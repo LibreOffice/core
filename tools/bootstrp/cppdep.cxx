51ba8a0f555fe5d31e2b8d221e3766810dc741b4
Martin Hollmichel <mh@openoffice.org>
2000-09-19 22:37:08 +0000

    Reverted one MACOSX macro back to MAC macro.

--- tools/bootstrp/cppdep.cxx
+++ tools/bootstrp/cppdep.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cppdep.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mh $ $Date: 2000-09-19 22:50:08 $
+ *  last change: $Author: mh $ $Date: 2000-09-19 23:37:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -80,7 +80,7 @@
 #endif
 #include <unistd.h>
 #endif
-#ifndef MACOSX
+#ifdef MACOSX
 #include <sys/stat.h>
 #else
 #include <stat.h>
