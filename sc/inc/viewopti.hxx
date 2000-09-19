58f16995ababae0d143f728884ff93cbebd170eb
Niklas Nebel <nn@openoffice.org>
2000-09-19 16:56:04 +0000

    ScLinkConfigItem: parent methods reimplemented

--- sc/inc/viewopti.hxx
+++ sc/inc/viewopti.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewopti.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:50 $
+ *  last change: $Author: nn $ $Date: 2000-09-19 17:56:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -221,10 +221,15 @@ public:
     virtual void    Notify( const com::sun::star::uno::Sequence<rtl::OUString>& aPropertyNames );
     virtual void    Commit();
 
-    ConfigItem::GetProperties;
-    ConfigItem::EnableNotification;
-    ConfigItem::PutProperties;
-    ConfigItem::SetModified;
+    void    SetModified()   { ConfigItem::SetModified(); }
+    com::sun::star::uno::Sequence< com::sun::star::uno::Any>
+            GetProperties(const com::sun::star::uno::Sequence< rtl::OUString >& rNames)
+                            { return ConfigItem::GetProperties( rNames ); }
+    sal_Bool PutProperties( const com::sun::star::uno::Sequence< rtl::OUString >& rNames,
+                            const com::sun::star::uno::Sequence< com::sun::star::uno::Any>& rValues)
+                            { return ConfigItem::PutProperties( rNames, rValues ); }
+    sal_Bool EnableNotification(com::sun::star::uno::Sequence< rtl::OUString >& rNames)
+                            { return ConfigItem::EnableNotification( rNames ); }
 };
 
 
