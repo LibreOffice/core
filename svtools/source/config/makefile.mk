0cec32e38abc6083c2c597c6ec5e29902ccae513
Mathias Bauer <mba@openoffice.org>
2000-09-20 11:29:52 +0000

    SaveOptions added

--- svtools/source/config/makefile.mk
+++ svtools/source/config/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:58:56 $
+#   last change: $Author: mba $ $Date: 2000-09-20 12:29:52 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -72,9 +72,8 @@ TARGET=config
 
 # --- Files --------------------------------------------------------
 
-SLOFILES = \
-        $(SLO)$/pathoptions.obj		\
-        $(SLO)$/useroptions.obj
+SLOFILES =      $(SLO)$/pathoptions.obj \
+                $(SLO)$/saveopt.obj
 
 # --- Targets ------------------------------------------------------
 
