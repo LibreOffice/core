28f1e8a870925a90f36c7af2680eed2a5c382ab7
Niklas Nebel <nn@openoffice.org>
2000-09-25 10:42:56 +0000

    #78631# #78943# UpdateAllComments

--- sc/inc/detfunc.hxx
+++ sc/inc/detfunc.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: detfunc.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:48 $
+ *  last change: $Author: nn $ $Date: 2000-09-25 11:42:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -139,6 +139,8 @@ public:
                                     const Rectangle& rVisible, BOOL bLeft,
                                     BOOL bForce, SdrPage* pDestPage );
     BOOL        HideComment( USHORT nCol, USHORT nRow );
+
+    void        UpdateAllComments();        // on all tables
 
     static BOOL IsNonAlienArrow( SdrObject* pObject );
 };
