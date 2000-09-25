8cd9c86cde2765b5e580c9ee834c73462f180410
Patrick Luby <patrick.luby@openoffice.org>
2000-09-25 04:25:51 +0000

    Added initialization of static data members for template classes for Mac OS X

--- connectivity/source/drivers/dbase/makefile.mk
+++ connectivity/source/drivers/dbase/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:14:21 $
+#   last change: $Author: patrick.luby $ $Date: 2000-09-25 05:25:51 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -94,7 +94,12 @@ SLOFILES=\
     $(SLO)$/Dservices.obj			\
     $(SLO)$/DDriver.obj
 
-        
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX" 
+SLOFILES+=$(SLO)$/staticmbdbase.obj
+.ENDIF
 
 # --- Library -----------------------------------
 
