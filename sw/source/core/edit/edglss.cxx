37312f2e00e7bb170ce976518469f96a8eae99bb
jp <jp@openoffice.org>
2000-09-25 18:23:00 +0000

    Bug #78534#: GetSelectedText - append an unicode zero to the stream

--- sw/source/core/edit/edglss.cxx
+++ sw/source/core/edit/edglss.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edglss.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:08:18 $
+ *  last change: $Author: jp $ $Date: 2000-09-25 19:23:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -415,7 +415,7 @@ BOOL SwEditShell::GetSelectedText( String &rBuf, int nHndlParaBrk )
                 STRING_MAXLEN > (( lLen  = aStream.GetSize() )
                                         / sizeof( sal_Unicode )) + 1 )
             {
-                aStream << '\0';
+                aStream << (sal_Unicode)'\0';
 
                 const sal_Unicode *p = (sal_Unicode*)aStream.GetBuffer();
                 if( p )
