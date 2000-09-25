b7fde0964d6a3a340932a438eb0646164b0f8d3f
Kai Ahrens <ka@openoffice.org>
2000-09-25 10:14:41 +0000

    not needed member removed (maCropRect)

--- goodies/inc/grfmgr.hxx
+++ goodies/inc/grfmgr.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: grfmgr.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ka $ $Date: 2000-09-22 14:23:44 $
+ *  last change: $Author: ka $ $Date: 2000-09-25 11:14:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -142,7 +142,6 @@ private:
     BOOL            mbInvert;
     BYTE            mcTransparency;
     GraphicDrawMode meDrawMode;
-    Rectangle       maCropRect;
 
     void*           mpDummy;
 
