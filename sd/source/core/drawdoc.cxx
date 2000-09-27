d2f68113664ff330062f15de80b939dc7ca87d4b
Christian Lippka <cl@openoffice.org>
2000-09-27 08:06:50 +0000

    #78567# initialize international members

--- sd/source/core/drawdoc.cxx
+++ sd/source/core/drawdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drawdoc.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:16:46 $
+ *  last change: $Author: cl $ $Date: 2000-09-27 09:06:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -234,6 +234,9 @@ SdDrawDocument::SdDrawDocument(DocumentType eType, SfxObjectShell* pDrDocSh) :
     bInitialOnlineSpellingEnabled(TRUE),
     bHasOnlineSpellErrors(FALSE),
     pOnlineSearchItem(NULL),
+    mpInternational(NULL),
+    mpLocale(NULL),
+    mpCharClass(NULL),
 #ifndef SVX_LIGHT
     bAllocDocSh(FALSE),
 #endif
