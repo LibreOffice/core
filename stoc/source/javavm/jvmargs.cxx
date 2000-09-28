1915b827947bec0c85158548df4c14eee9705ae6
Kay Ramme <kr@openoffice.org>
2000-09-28 16:35:21 +0000

    cleaned; parses properties

--- stoc/source/javavm/jvmargs.cxx
+++ stoc/source/javavm/jvmargs.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: jvmargs.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 15:29:34 $
+ *  last change: $Author: kr $ $Date: 2000-09-28 17:35:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,245 +59,276 @@
  *
  ************************************************************************/
 
+#ifdef UNIX
+#define CLASSPATH_DELIMETER ":"
+
+#else
+#define CLASSPATH_DELIMETER ";"
+
+#endif
+
+
 #include "jvmargs.hxx"
 
+#include <osl/diagnose.h>
 
-JVM::JVM(JNI_InitArgs_Type * pVMInitArgs)
-    : debug(sal_False)
-{
-    /*
-    ** The following is used to specify that we require at least
-    ** JNI version 1.1. Currently, this field is not checked but
-    ** will be starting with JDK/JRE 1.2. The value returned after
-    ** calling JNI_GetDefaultJavaVMInitArgs() is the actual JNI version
-    ** supported, and is always higher that the requested version.
-    */
+#include <rtl/ustring.hxx>
 
+using namespace rtl;
 
-    jDK1_1InitArgs.version = JNI_VERSION_1_1;
-    pVMInitArgs(&jDK1_1InitArgs);
+namespace stoc_javavm {
 
-    javaVMInitArgs.nOptions = 0;
-    javaVMInitArgs.options = NULL;
-    jiDebugPort = 1199;
-}
-
-JVM::~JVM()
-{
-    if(javaVMInitArgs.options)
+    JVM::JVM() throw()
+        : _is_debugPort(sal_False),
+          _is_disableAsyncGC(sal_False),
+          _is_enableClassGC(sal_False),
+          _is_enableVerboseGC(sal_False),
+          _is_checkSource(sal_False),
+          _is_nativeStackSize(sal_False),
+          _is_javaStackSize(sal_False),
+          _is_minHeapSize(sal_False),
+          _is_maxHeapSize(sal_False),
+          _enabled(sal_False),
+          _is_verifyMode(sal_False),
+          _is_print(sal_False),
+          _is_exit(sal_False),
+          _is_abort(sal_False)
     {
-        for(size_t i = 0; i < javaVMInitArgs.nOptions; i++)
-        {
-            free(javaVMInitArgs.options[i].optionString);
+    }
+
+    void JVM::pushProp(const OUString & property) throw() {
+        sal_Int32 index = property.indexOf((sal_Unicode)'=');
+        OUString left = property.copy(0, index).trim();
+        OUString right(property.copy(index + 1).trim());
+
+//  #ifdef DEBUG
+//      OString left_tmp = OUStringToOString(left, RTL_TEXTENCODING_ASCII_US);
+//      OSL_TRACE("javavm - left %s", left_tmp.getStr());
+//      OString right_tmp = OUStringToOString(right, RTL_TEXTENCODING_ASCII_US);
+//      OSL_TRACE("javam - right %s", right_tmp.getStr());
+//  #endif
+
+        if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("Debug"))))
+            setDebugPort(1199);
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("Java"))))
+            setEnabled(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("Version"))))
+            ;
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("Home"))))
+            ;
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("SystemClasspath"))))
+            setSystemClasspath(right);
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("UserClasspath"))))
+            setUserClasspath(right);
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("DisableAsyncGC"))))
+            setDisableAsyncGC(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("EnableClassGC"))))
+            setEnableClassGC(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("EnableVerboseGC"))))
+            setEnableVerboseGC(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("NativeStackSize"))))
+            setNativeStackSize(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("JavaStackSize"))))
+            setJavaStackSize(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("VerifyMode"))))
+            setVerifyMode(right);
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("MinHeapSize"))))
+            setMinHeapSize(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("MaxHeapSize"))))
+            setMaxHeapSize(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("DebugPort"))))
+            setDebugPort(right.toInt32());
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("RuntimeLib"))))
+            setRuntimeLib(right);
+
+        else if(left.equals(OUString(RTL_CONSTASCII_USTRINGPARAM("CheckSource"))))
+            setCheckSource(right.toInt32());
+
+        else
+            _props.push_back(property);
+    }
+
+    void JVM::setEnabled(sal_Bool sbFlag) throw() {
+        _enabled = sbFlag;
+    }
+
+    void JVM::setDisableAsyncGC(jint jiValue) throw() {
+        _is_disableAsyncGC = sal_True;
+        _disableAsyncGC = jiValue;
+    }
+
+    void JVM::setEnableClassGC(jint jiValue) throw() {
+        _is_enableClassGC = sal_True;
+        _enableClassGC = jiValue;
+    }
+
+    void JVM::setEnableVerboseGC(jint jiValue) throw() {
+        _is_enableVerboseGC = sal_True;
+        _enableVerboseGC = jiValue;
+    }
+
+    void JVM::setCheckSource(jint jiValue) throw() {
+        OSL_TRACE("JVM::setCheckSource: %i", jiValue);
+
+        _is_checkSource = sal_True;
+        _checkSource = jiValue;
+    }
+
+    void JVM::setNativeStackSize(jint jiValue) throw() {
+        _is_nativeStackSize = sal_True;
+        _nativeStackSize = jiValue;
+    }
+
+    void JVM::setJavaStackSize(jint jiValue) throw() {
+        _is_javaStackSize = sal_True;
+        _javaStackSize = jiValue;
+    }
+
+    void JVM::setVerifyMode(const OUString & mode) throw() {
+        _is_verifyMode = sal_True;
+
+        if(mode.compareToAscii("none") == 0)
+            _verifyMode = 0;
+
+        else if(mode.compareToAscii("remote") == 0)
+            _verifyMode = 1;
+
+        else if(mode.compareToAscii("all") == 0 )
+            _verifyMode = 2;
+    }
+
+    void JVM::setMinHeapSize(jint jiValue) throw() {
+        _is_minHeapSize = sal_True;
+        _minHeapSize = jiValue;
+    }
+
+    void JVM::setMaxHeapSize(jint jiValue) throw() {
+        _is_maxHeapSize = sal_True;
+        _maxHeapSize = jiValue;
+    }
+
+    void JVM::setDebugPort(jint jiValue) throw() {
+        _is_debugPort = sal_True;
+        _debugPort = jiValue;
+    }
+
+    void JVM::setSystemClasspath(const OUString & classpath) throw() {
+        OString tmp = OUStringToOString(classpath, RTL_TEXTENCODING_ASCII_US);
+        OSL_TRACE("JVM::setSystemClasspath: %s", tmp.getStr());
+
+        _systemClasspath = classpath;
+    }
+
+    void JVM::setUserClasspath(const OUString & classpath) throw() {
+        OString tmp = OUStringToOString(classpath, RTL_TEXTENCODING_ASCII_US);
+        OSL_TRACE("JVM::setUserClasspath: %s", tmp.getStr());
+
+        _userClasspath = classpath;
+    }
+
+    void JVM::setPrint(JNIvfprintf vfprintf) throw() {
+        _is_print = sal_True;
+        _print = vfprintf;
+    }
+
+    void JVM::setExit(JNIexit exit) throw() {
+        _is_exit = sal_True;
+        _exit = exit;
+    }
+
+    void JVM::setAbort(JNIabort abort) throw() {
+        _is_abort = sal_True;
+        _abort = abort;
+    }
+
+    void JVM::setArgs(JDK1_1InitArgs * pargs) const throw() {
+        OUString classpath = _systemClasspath;
+        classpath += OUString(RTL_CONSTASCII_USTRINGPARAM(CLASSPATH_DELIMETER));
+        classpath += _userClasspath;
+
+        pargs->classpath = strdup(OUStringToOString(classpath, RTL_TEXTENCODING_ASCII_US));
+
+        if(_is_debugPort) {
+            pargs->debugging = JNI_TRUE;
+            pargs->debugPort = _debugPort;
         }
 
-        free(javaVMInitArgs.options);
-        javaVMInitArgs.nOptions = 0;
-        javaVMInitArgs.options = NULL;
-    }
+        if(_is_disableAsyncGC)
+            pargs->disableAsyncGC = _disableAsyncGC;
 
-    size_t i = 0;
-    if(jDK1_1InitArgs.properties)
-    {
-        while(jDK1_1InitArgs.properties[i])
-            free(jDK1_1InitArgs.properties[i ++]);
+        if(_is_enableClassGC)
+            pargs->enableClassGC = _enableClassGC;
 
-        free(jDK1_1InitArgs.properties);
-    }
-}
+        if(_is_enableVerboseGC)
+            pargs->enableVerboseGC = _enableVerboseGC;
 
-void JVM::pushPProp(OUString uString, void * extraInfo)
-{
-    JavaVMOption javaVMOption;
+        if(_is_checkSource)
+            pargs->checkSource = _checkSource;
 
-    javaVMOption.optionString = strdup(OUStringToOString(uString, RTL_TEXTENCODING_ASCII_US));
-    javaVMOption.extraInfo = extraInfo;
+        if(_is_nativeStackSize)
+            pargs->nativeStackSize = _nativeStackSize;
 
-    p_props.push_back(javaVMOption);
-}
+        if(_is_javaStackSize)
+            pargs->javaStackSize = _javaStackSize;
 
-void JVM::pushProp(const OUString & uString)
-{
-    props.push_back(uString);
-}
-
-void JVM::disableAsyncGC(jboolean jbFlag)
-{
-    jDK1_1InitArgs.disableAsyncGC = jbFlag;
-}
-
-void JVM::enableClassGC(jboolean jbFlag)
-{
-    jDK1_1InitArgs.enableClassGC = jbFlag;
-    if(!jbFlag)
-    {
-        OUString tmpStr( RTL_CONSTASCII_USTRINGPARAM("-Xnoclassgc") );
-        pushPProp(tmpStr);
-    }
-}
-
-void JVM::enableVerboseGC(jboolean jbFlag)
-{
-    jDK1_1InitArgs.enableVerboseGC = jbFlag;
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("-verbose:gc")));
-}
-
-void JVM::verbose(jboolean jbFlag)
-{
-    jDK1_1InitArgs.verbose = jbFlag;
-    pushPProp(OUString(RTL_CONSTASCII_USTRINGPARAM("-verbose:class")));
-}
-
-void JVM::nativeStackSize(jint jiSize)
-{
-    jDK1_1InitArgs.nativeStackSize = jiSize;
-}
-
-void JVM::javaStackSize(jint jiSize)
-{
-    jDK1_1InitArgs.javaStackSize = jiSize;
-}
-
-void JVM::verifyMode(OUString uStr)
-{
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("-Xverify:")) + uStr );
-
-    if( uStr.compareToAscii("none") == 0 )
-        jDK1_1InitArgs.verifyMode = 0;
-    else if( uStr.compareToAscii("remote") == 0)
-        jDK1_1InitArgs.verifyMode = 1;
-    else if( uStr.compareToAscii("all") == 0 )
-        jDK1_1InitArgs.verifyMode = 2;
-}
-
-void JVM::minHeapSize(jint jiSize)
-{
-    // Workaround ! Der neue Wert wird nur uebernommen, wenn dieser ueber dem
-    // DefaultSize ( 1 MB ) liegt. Ein zu kleiner initialer HeapSize f’hrt unter Solaris Sparc zum Deadlock.
-    if(jiSize > (sal_uInt32) jDK1_1InitArgs.minHeapSize)
-        jDK1_1InitArgs.minHeapSize = jiSize;
-
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("-Xms")) + OUString::valueOf((sal_Int32) jiSize) );
-}
-
-void JVM::maxHeapSize(jint jiSize)
-{
-    jDK1_1InitArgs.maxHeapSize = jiSize;
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("-Xmx")) + OUString::valueOf((sal_Int32)jiSize) );
-}
-
-void JVM::setDebug(sal_Bool flag)
-{
-    fprintf(stderr, "#### JVM::setDebug: %d\n", flag);
-    debug = flag;
-}
-
-sal_Bool JVM::getDebug()
-{
-    return debug;
-}
-
-void JVM::setDebugPort(jint jiPort)
-{
-    jiDebugPort = jiPort;
-}
-
-void JVM::setCompiler(const OUString & usCompiler)
-{
-    this->usCompiler = usCompiler;
-}
-
-OUString JVM::getCompiler()
-{
-    return usCompiler;
-}
-
-void JVM::classPath( OString str )
-{
-//    jDK1_1InitArgs.classpath = strdup(str.GetStr());
-      jDK1_1InitArgs.classpath = strdup(str);
-      pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("java.class.path=")) + OStringToOUString(str, RTL_TEXTENCODING_ASCII_US ) );
-}
-
-void JVM::vfprintf(JNIvfprintf vfprintf)
-{
-    jDK1_1InitArgs.vfprintf = vfprintf;
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("vfprintf")), (void *)vfprintf );
-}
-
-void JVM::exit(JNIexit exit)
-{
-    jDK1_1InitArgs.exit = exit;
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("exit")), (void *)exit );
-}
-
-void JVM::abort(JNIabort abort)
-{
-    jDK1_1InitArgs.abort = abort;
-    pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("abort=")), (void *)abort );
-}
-
-
-const JDK1_1InitArgs * JVM::getJDK1_1InitArgs()
-{
-    if( usCompiler.getLength() )
-        pushProp( OUString(RTL_CONSTASCII_USTRINGPARAM("java.compiler=")) + usCompiler);
-
-    if(debug)
-    {
-        jDK1_1InitArgs.debugging = JNI_TRUE;
-        jDK1_1InitArgs.debugPort = jiDebugPort;
-        pushPProp( OUString(RTL_CONSTASCII_USTRINGPARAM("-Xdebug")), &jiDebugPort);
-    }
-
-    if(!jDK1_1InitArgs.properties)
-    {
-        size_t size = props.size() + 1;
-        jDK1_1InitArgs.properties = (char **) calloc(sizeof(const char *), props.size() + 1);
-
-        for(size_t i = 0; i < props.size(); i++)
-        {
-            OUString str = props[i];
-
-            jDK1_1InitArgs.properties[i] = strdup( OUStringToOString(str, RTL_TEXTENCODING_ASCII_US) );
+        if(_is_minHeapSize) {
+            // Workaround ! Der neue Wert wird nur uebernommen, wenn dieser ueber der
+            // DefaultSize ( 1 MB ) liegt. Ein zu kleiner initialer HeapSize fuehrt unter Solaris Sparc zum Deadlock.
+            if(_minHeapSize > (sal_uInt32) pargs->minHeapSize)
+                pargs->minHeapSize = _minHeapSize;
         }
 
-        jDK1_1InitArgs.properties[props.size()] = NULL;
+        if(_is_maxHeapSize)
+            pargs->maxHeapSize = _maxHeapSize;
+
+        if(_is_verifyMode)
+            pargs->verifyMode = _verifyMode;
+
+        if(_is_print)
+            pargs->vfprintf = _print;
+
+        if(_is_exit)
+            pargs->exit = _exit;
+
+        if(_is_abort)
+            pargs->abort = _abort;
+
+        size_t size = _props.size();
+        pargs->properties = (char **)calloc(sizeof(const char *), size + 1);
+
+        for(size_t i = 0; i < size; ++ i) {
+            const OUString & str = _props[i];
+
+            pargs->properties[i] = strdup(OUStringToOString(str, RTL_TEXTENCODING_ASCII_US));
+        }
+
+        pargs->properties[size] = NULL;
     }
 
-    return &jDK1_1InitArgs;
+    void JVM::setRuntimeLib(const OUString & libName) throw() {
+        _runtimeLib = libName;
+    }
+
+    const OUString & JVM::getRuntimeLib() const throw() {
+        return _runtimeLib;
+    }
+
+    sal_Bool JVM::isEnabled() const throw() {
+        return _enabled && _systemClasspath.getLength() && _runtimeLib.getLength();
+    }
 }
-
-const JavaVMInitArgs * JVM::getJavaVMInitArgs()
-{
-    if(usCompiler.getLength())
-        pushProp( OUString(RTL_CONSTASCII_USTRINGPARAM("java.compiler=")) + usCompiler);
-
-    javaVMInitArgs.version              = JNI_VERSION_1_2;
-    javaVMInitArgs.nOptions             = props.size() + p_props.size();
-    javaVMInitArgs.ignoreUnrecognized   = JNI_TRUE;
-
-    javaVMInitArgs.options = (JavaVMOption *)calloc(sizeof(JavaVMOption), javaVMInitArgs.nOptions);
-
-    size_t i;
-
-    for(i = 0; i < p_props.size(); i++)
-    {
-        javaVMInitArgs.options[i] = p_props[i];
-    }
-
-
-    for(;i < javaVMInitArgs.nOptions; i++)
-    {
-        OUString str = props[i - p_props.size()];
-
-        if( str.copy(0, 2).compareToAscii("-D") != 0  && str.copy(0, 2).compareToAscii("-X") != 0 )
-            str = OUString(RTL_CONSTASCII_USTRINGPARAM("-D")) + str;
-
-        javaVMInitArgs.options[i].optionString = strdup( OUStringToOString(str, RTL_TEXTENCODING_ASCII_US) );
-        javaVMInitArgs.options[i].extraInfo = NULL;
-    }
-
-    return &javaVMInitArgs;
-}
-
