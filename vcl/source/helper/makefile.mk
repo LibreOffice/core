9c09c7e48f54ee2ee7f7ab395fa632c5c2e5fa33
Patrick Luby <pluby@openoffice.org>
2000-10-04 14:37:04 +0000

    Removed use of staticmb*.cxx files for Mac OS X since they instantiate static data members that no longer exist in this project.

--- vcl/source/helper/makefile.mk
+++ vcl/source/helper/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: pl $ $Date: 2000-10-04 14:28:48 $
+#   last change: $Author: pluby $ $Date: 2000-10-04 15:37:04 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,13 +88,6 @@ SLOFILES+=\
 
 .IF "$(remote)" != ""
 SLOFILES+=$(SLO)$/atom.obj
-.ENDIF
-
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
-# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
-SLOFILES+=$(SLO)$/staticmbhelper.obj
 .ENDIF
 
 # --- Targets ------------------------------------------------------
