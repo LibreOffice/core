60d4893f6af99c0363e330b41008820ecb9e3d21
Jens-Heiner Rechtien <hr@openoffice.org>
2000-10-05 13:07:02 +0000

    #65293#: included <tools/urlobj-hxx>

--- svx/source/xoutdev/xtabcolr.cxx
+++ svx/source/xoutdev/xtabcolr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xtabcolr.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:01:28 $
+ *  last change: $Author: hr $ $Date: 2000-10-05 14:07:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 #pragma hdrstop
 
 #include <sfx2/docfile.hxx>
+#include <tools/urlobj.hxx>
 #include "dialogs.hrc"
 #include "dialmgr.hxx"
 #include "xtable.hxx"
