a8fa2d6966007875a294f7855f77641df8b0e025
Patrick Luby <pluby@openoffice.org>
2000-10-07 21:17:37 +0000

    Removed static data members that are now initialized in earlier projects.

--- unotools/source/misc/makefile.mk
+++ unotools/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: bmahbod $ $Date: 2000-10-06 23:53:15 $
+#   last change: $Author: pluby $ $Date: 2000-10-07 22:17:37 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,16 +104,6 @@ SLOFILES=	$(SLO)$/types.obj         \
         $(SLO)$/sequence.obj      \
         $(SLO)$/querydeep.obj     \
         $(SLO)$/regpathhelper.obj
-
-
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism 
-# for GCC won't work.  SCO and MACOSX: the linker does know about weak 
-# symbols, but we can't ignore multiple defined symbols
-
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
-SLOFILES+=$(SLO)$/staticmbmisc.obj
-.ENDIF
 
 # --- Targets ----------------------------------
 
