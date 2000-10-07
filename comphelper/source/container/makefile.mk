d6b44eb02e53e6e5f4e7d8d390768fbfd64b815a
Patrick Luby <pluby@openoffice.org>
2000-10-07 20:13:53 +0000

    Added initialization of static data members for template classes for Mac OS X

--- comphelper/source/container/makefile.mk
+++ comphelper/source/container/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: fs $ $Date: 2000-09-29 11:28:15 $
+#   last change: $Author: pluby $ $Date: 2000-10-07 21:13:51 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -76,6 +76,13 @@ ENABLE_EXCEPTIONS=TRUE
 SLOFILES=\
             $(SLO)$/enumhelper.obj	\
             $(SLO)$/container.obj
+
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SLOFILES+=$(SLO)$/staticmbcontainer.obj
+.ENDIF
 
 # --- Targets ----------------------------------
 
