6a0154a48d3b83ddae648ace70c991200f85d6f3
Patrick Luby <pluby@openoffice.org>
2000-10-08 01:48:40 +0000

    Added initialization of Sequence<X>::s_pType static data members for Mac OS X

--- toolkit/source/awt/makefile.mk
+++ toolkit/source/awt/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:02:08 $
+#   last change: $Author: pluby $ $Date: 2000-10-08 02:48:40 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -90,6 +90,13 @@ SLOFILES=   \
             $(SLO)$/vclxtopwindow.obj 				\
             $(SLO)$/vclxwindow.obj 					\
             $(SLO)$/vclxwindows.obj			
+
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SLOFILES+=$(SLO)$/staticmbawt.obj
+.ENDIF
 
 # --- Targets ------------------------------------------------------
 
