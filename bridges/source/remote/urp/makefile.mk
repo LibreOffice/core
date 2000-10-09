4bae27caf8ce4fc710ffd217a57ef1dc8cfc0652
Patrick Luby <pluby@openoffice.org>
2000-10-09 15:29:42 +0000

    Added initialization of static data members for template classes for Mac OS X

--- bridges/source/remote/urp/makefile.mk
+++ bridges/source/remote/urp/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: jbu $ $Date: 2000-09-29 08:42:05 $
+#   last change: $Author: pluby $ $Date: 2000-10-09 16:29:42 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -103,6 +103,13 @@ SLOFILES= 	\
         $(SLO)$/urp_log.obj \
         $(SLO)$/urp_bridgeimpl.obj \
         $(SLO)$/urp_propertyobject.obj
+
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SLOFILES+=$(SLO)$/staticmbremoteurp.obj
+.ENDIF
 
 SHL1TARGET= $(TARGET)
 
