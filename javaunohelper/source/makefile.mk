75c5d408572002000c9f1c7a832ecc351da43eaf
Patrick Luby <pluby@openoffice.org>
2000-10-10 02:19:11 +0000

    Added initialization of static data members for template classes for Mac OS X

--- javaunohelper/source/makefile.mk
+++ javaunohelper/source/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:31:32 $
+#   last change: $Author: pluby $ $Date: 2000-10-10 03:19:11 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,6 +104,13 @@ UNOTYPES= \
 
 SLOFILES= \
         $(SLO)$/javaunohelper.obj
+
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SLOFILES+=$(SLO)$/staticmb.obj
+.ENDIF
 
 SHL1TARGET=	$(TARGET)
 
