b915a811100ee814cca4666ebcd520d3c1f322c9
John Marmion <jmarmion@openoffice.org>
2000-10-13 07:42:05 +0000

    shellio uses exceptions handling

--- sw/source/filter/basflt/makefile.mk
+++ sw/source/filter/basflt/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: jp $ $Date: 2000-09-28 12:07:10 $
+#   last change: $Author: jmarmion $ $Date: 2000-10-13 08:42:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -85,6 +85,9 @@ SLOFILES =  \
         $(SLO)$/fltini.obj \
         $(SLO)$/shellio.obj \
         $(SLO)$/w4wflt.obj
+
+EXCEPTIONSFILES =  \
+        $(SLO)$/shellio.obj \
 
 OBJFILES = $(OBJ)$/w4wflt.obj
 
