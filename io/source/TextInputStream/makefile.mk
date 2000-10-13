9bdb6240aad7c2d9cf89508c309879807ccbac02
Gene Anaya <ganaya@openoffice.org>
2000-10-13 17:00:29 +0000

    Added several staticmbXXX.cxx files for mac os X build

--- io/source/TextInputStream/makefile.mk
+++ io/source/TextInputStream/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2000-09-18 23:16:57 $
+#   last change: $Author: ganaya $ $Date: 2000-10-13 18:00:28 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -75,6 +75,12 @@ NO_BSYMBOLIC=TRUE
 
 SLOFILES= \
     $(SLO)$/TextInputStream.obj
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX" 
+SLOFILES+=$(SLO)$/staticmbTextInputStream.obj
+.ENDIF
 
 SHL1TARGET= $(TARGET)
 
