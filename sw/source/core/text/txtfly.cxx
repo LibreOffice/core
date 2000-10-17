ef74e1369c7583b492031ec44d34bbc14706e205
Andreas Martens <ama@openoffice.org>
2000-10-17 09:21:27 +0000

    Fix: Ignore fly frames during multi-portion formatting

--- sw/source/core/text/txtfly.cxx
+++ sw/source/core/text/txtfly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtfly.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:08:26 $
+ *  last change: $Author: ama $ $Date: 2000-10-17 10:21:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -396,6 +396,8 @@ sal_Bool SwTxtFormatter::ChkFlyUnderflow( SwTxtFormatInfo &rInf ) const
 
 void SwTxtFormatter::CalcFlyWidth( SwTxtFormatInfo &rInf ) const
 {
+    if( GetMulti() )
+        return;
     SwTxtFly *pTxtFly = rInf.GetTxtFly();
     if( !pTxtFly->IsOn() || rInf.IsIgnoreFly() )
         return;
