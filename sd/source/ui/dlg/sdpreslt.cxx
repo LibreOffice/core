e608971544250a28acd12ecbb2d2960b4d62f1a5
Christian Lippka <cl@openoffice.org>
2000-10-18 13:30:41 +0000

    #76587# set correct default for inserting in master page mode

--- sd/source/ui/dlg/sdpreslt.cxx
+++ sd/source/ui/dlg/sdpreslt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sdpreslt.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:33 $
+ *  last change: $Author: cl $ $Date: 2000-10-18 14:30:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -147,6 +147,7 @@ void SdPresLayoutDlg::Reset()
     {
         BOOL bMasterPage = ( (const SfxBoolItem*) pPoolItem)->GetValue();
         aCbxMasterPage.Enable( !bMasterPage );
+        aCbxMasterPage.Check( bMasterPage );
     }
 
     // Nicht verwendete MasterPages entfernen
