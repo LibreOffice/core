962e3b2e0f32a5af4505a9af17d7866800756162
Stephan Bergmann <sb@openoffice.org>
2000-10-18 09:04:31 +0000

    Added fileidentifierconverter.obj.

--- ucbhelper/source/client/makefile.mk
+++ ucbhelper/source/client/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: bmahbod $ $Date: 2000-10-09 21:59:31 $
+#   last change: $Author: sb $ $Date: 2000-10-18 10:04:31 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -81,7 +81,8 @@ ENABLE_EXCEPTIONS=TRUE
 SLOFILES=\
         $(SLO)$/content.obj		\
         $(SLO)$/contentbroker.obj	\
-        $(SLO)$/commandenvironment.obj
+        $(SLO)$/commandenvironment.obj	\
+        $(SLO)$/fileidentifierconverter.obj
 
 .ENDIF
 
