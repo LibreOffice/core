e3d4ceea2f1fca276faeae526a5206f15dd4f311
Andreas Martens <ama@openoffice.org>
2000-10-19 11:38:41 +0000

    Fix #78742#: No braodcast during formatting

--- sw/source/core/text/porfly.cxx
+++ sw/source/core/text/porfly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: porfly.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:08:25 $
+ *  last change: $Author: ama $ $Date: 2000-10-19 12:38:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -511,7 +511,7 @@ void SwFlyCntPortion::SetBase( const Point &rBase, long nLnAscent,
             }
             Point aDiff = aRelPos + aBase - pSdrObj->GetSnapRect().TopLeft();
             pSdrObj->ImpSetAnchorPos( aBase );
-            pSdrObj->Move( Size( aDiff.X(), aDiff.Y() ) );
+            pSdrObj->NbcMove( Size( aDiff.X(), aDiff.Y() ) );
         }
     }
     else
