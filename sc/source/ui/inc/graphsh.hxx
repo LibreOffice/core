e84041a93944b0a9e7368e3d9b0b7d0b3892c469
Niklas Nebel <nn@openoffice.org>
2000-10-20 17:24:10 +0000

    Execute/GetAttrState for graphic object functions

--- sc/source/ui/inc/graphsh.hxx
+++ sc/source/ui/inc/graphsh.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: graphsh.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:59 $
+ *  last change: $Author: nn $ $Date: 2000-10-20 18:24:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,6 +88,8 @@ public:
                 ScGraphicShell(ScViewData* pData);
     virtual     ~ScGraphicShell();
 
+    void    Execute(SfxRequest& rReq);
+    void    GetAttrState(SfxItemSet &rSet);
 };
 
 #endif
