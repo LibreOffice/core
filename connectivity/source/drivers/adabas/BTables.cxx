0a207c871aca297c3c8c7db8791b342369e73b72
Ocke Janssen <oj@openoffice.org>
2000-10-24 15:04:47 +0000

    make strings unique for lib's

--- connectivity/source/drivers/adabas/BTables.cxx
+++ connectivity/source/drivers/adabas/BTables.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: BTables.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:14:20 $
+ *  last change: $Author: oj $ $Date: 2000-10-24 16:04:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -86,6 +86,7 @@
 #ifndef _CONNECTIVITY_ADABAS_BCONNECTION_HXX_
 #include "adabas/BConnection.hxx"
 #endif
+#define CONNECTIVITY_PROPERTY_NAME_SPACE adabas
 #ifndef _CONNECTIVITY_PROPERTYIDS_HXX_
 #include "propertyids.hxx"
 #endif
