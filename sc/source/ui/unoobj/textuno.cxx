54761119a0894bf5347afa46aebc5ebe86e550b0
Niklas Nebel <nn@openoffice.org>
2000-10-24 11:06:46 +0000

    include for PropertyAttribute

--- sc/source/ui/unoobj/textuno.cxx
+++ sc/source/ui/unoobj/textuno.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: textuno.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:45:08 $
+ *  last change: $Author: nn $ $Date: 2000-10-24 12:06:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,6 +79,8 @@
 #ifndef _COM_SUN_STAR_AWT_FONTSLANT_HPP_
 #include <com/sun/star/awt/FontSlant.hpp>
 #endif
+
+#include <com/sun/star/beans/PropertyAttribute.hpp>
 
 #include "textuno.hxx"
 #include "fielduno.hxx"
