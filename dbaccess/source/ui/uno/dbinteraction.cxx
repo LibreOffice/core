66318aa6475b57e4ca90e39569c6262752d2114c
Frank Schönheit <fs@openoffice.org>
2000-10-25 12:00:01 +0000

    initial checkin - InteractionHandler for common database related interaction requests

---
+++ dbaccess/source/ui/uno/dbinteraction.cxx
@@ -0,0 +1,363 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dbinteraction.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2000-10-25 12:59:42 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _DBAUI_INTERACTION_HXX_
+#include "dbinteraction.hxx"
+#endif
+#ifndef _DBA_REGISTRATION_HELPER_HXX_
+#include "registrationhelper.hxx"
+#endif
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#ifndef _SV_MSGBOX_HXX
+#include <vcl/msgbox.hxx>
+#endif
+#ifndef _DBHELPER_DBEXCEPTION_HXX_
+#include <connectivity/dbexception.hxx>
+#endif
+#ifndef _DBAUI_SQLMESSAGE_HXX_
+#include "sqlmessage.hxx"
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONAPPROVE_HPP_
+#include <com/sun/star/task/XInteractionApprove.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONDISAPPROVE_HPP_
+#include <com/sun/star/task/XInteractionDisapprove.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONRETRY_HPP_
+#include <com/sun/star/task/XInteractionRetry.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONABORT_HPP_
+#include <com/sun/star/task/XInteractionAbort.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UCB_XINTERACTIONSUPPLYAUTHENTICATION_HPP_
+#include <com/sun/star/ucb/XInteractionSupplyAuthentication.hpp>
+#endif
+#ifndef _SVTOOLS_LOGINDLG_HXX_
+#include <svtools/logindlg.hxx>
+#endif
+#ifndef _DBU_RESOURCE_HRC_
+#include "dbu_resource.hrc"
+#endif
+
+//==========================================================================
+
+extern "C" void SAL_CALL createRegistryInfo_OInteractionHandler()
+{
+    static OMultiInstanceAutoRegistration< ::dbaui::OInteractionHandler > aOInteractionHandler_AutoRegistration;
+}
+
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::ucb;
+    using namespace ::com::sun::star::lang;
+    using namespace ::com::sun::star::task;
+    using namespace ::dbtools;
+
+    //=========================================================================
+    //= OInteractionHandler
+    //=========================================================================
+    //-------------------------------------------------------------------------
+    OInteractionHandler::OInteractionHandler(const Reference< XMultiServiceFactory >& _rxORB)
+        :m_xORB(_rxORB)
+    {
+    }
+
+    //-------------------------------------------------------------------------
+    IMPLEMENT_SERVICE_INFO1_STATIC(OInteractionHandler, "com.sun.star.comp.dbaccess.OInteractionHandler", "com.sun.star.sdb.InteractionHandler");
+
+    //-------------------------------------------------------------------------
+    void SAL_CALL OInteractionHandler::handle(const Reference< XInteractionRequest >& _rxRequest) throw(RuntimeException)
+    {
+        Any aRequest;
+        if (_rxRequest.is())
+        {
+            try { aRequest = _rxRequest->getRequest(); }
+            catch(RuntimeException&) { }
+        }
+        DBG_ASSERT(aRequest.hasValue(), "OInteractionHandler::handle: invalid request!");
+        if (!aRequest.hasValue())
+            // no request -> no handling
+            return;
+
+        Sequence< Reference< XInteractionContinuation > > aContinuations;
+        try { aContinuations = _rxRequest->getContinuations(); }
+        catch(RuntimeException&) { }
+
+        // try to extract an SQLException (or one of it's derived members
+        SQLExceptionInfo aInfo(aRequest);
+        if (aInfo.isValid())
+        {
+            implHandle(aInfo, aContinuations);
+            return;
+        }
+
+        AuthenticationRequest aAuthentRequest;
+        if (aRequest >>= aAuthentRequest)
+        {   // it's an authentification request
+            implHandle(aAuthentRequest, aContinuations);
+            return;
+        }
+
+        DBG_ERROR("OInteractionHandler::handle: unsupported request type!");
+    }
+
+    //-------------------------------------------------------------------------
+    void OInteractionHandler::implHandle(const AuthenticationRequest& _rAuthRequest, const Sequence< Reference< XInteractionContinuation > >& _rContinuations)
+    {
+        // search the continuations we can handle
+        sal_Int32 nAbortPos = getContinuation(ABORT, _rContinuations);
+        sal_Int32 nRetryPos = getContinuation(RETRY, _rContinuations);
+        sal_Int32 nAuthentPos = getContinuation(SUPPLY_AUTHENTICATION, _rContinuations);
+
+        // we strongly need an XInteractionSupplyAuthentication (else we can't return the input given by the user)
+        Reference< XInteractionSupplyAuthentication > xSuppAuthent;
+        if (-1 != nAuthentPos)
+            xSuppAuthent = Reference< XInteractionSupplyAuthentication >(_rContinuations[nAuthentPos], UNO_QUERY);
+        DBG_ASSERT(xSuppAuthent.is(), "OInteractionHandler::implHandle: need an XInteractionSupplyAuthentication to return the results!");
+
+        // check which "remember password" modes are allowed
+        sal_Bool bRemember(sal_False);
+        sal_Bool bRememberPersistent(sal_False);
+        if (xSuppAuthent.is())
+        {
+            RememberAuthentication eDefault;
+            Sequence< RememberAuthentication > aModes(xSuppAuthent->getRememberPasswordModes(eDefault));
+            const RememberAuthentication* pModes = aModes.getConstArray();
+            bRemember = eDefault != RememberAuthentication_NO;
+            for (sal_Int32 i=0; i<aModes.getLength(); ++i, ++pModes)
+                if (*pModes == RememberAuthentication_PERSISTENT)
+                {
+                    bRememberPersistent = sal_True;
+                    break;
+                }
+        }
+
+        // extract some infor from the authentication request
+        // use the account as realm
+        String sRealm;
+        if (_rAuthRequest.HasRealm)
+            sRealm = _rAuthRequest.Realm.getStr();
+
+        // determine the flags for
+        sal_uInt16 nFlags = 0;
+        nFlags |= LF_NO_PATH;   // the AuthenticationRequest does not support a path
+
+        if (0 == _rAuthRequest.Diagnostic.getLength())
+            nFlags |= LF_NO_ERRORTEXT;
+        if (!_rAuthRequest.HasAccount)
+            nFlags |= LF_NO_ACCOUNT;
+        if (!_rAuthRequest.HasUserName || !xSuppAuthent.is() || !xSuppAuthent->canSetUserName())
+            nFlags |= LF_USERNAME_READONLY;
+
+        // create the dialog
+        ::svt::LoginDialog aLogin(NULL, nFlags, _rAuthRequest.ServerName, sRealm.Len() ? &sRealm : NULL);
+
+        // initialize it
+        aLogin.SetErrorText(_rAuthRequest.Diagnostic.getStr());
+        aLogin.SetName(_rAuthRequest.UserName);
+        if (_rAuthRequest.HasAccount)
+            aLogin.ClearAccount();
+        else
+            aLogin.ClearPassword();
+
+        aLogin.SetPassword(_rAuthRequest.Password.getStr());
+
+        aLogin.SetSavePassword(bRemember);
+        aLogin.SetSavePasswordText(ModuleRes(bRememberPersistent ? STR_REMEMBERPASSWORD_PERSISTENT : STR_REMEMBERPASSWORD_SESSION));    // TODO
+
+        // execute
+        sal_Int32 nResult = aLogin.Execute();
+
+        // dispatch the result
+        try
+        {
+            switch (nResult)
+            {
+                case RET_OK:
+                    if (xSuppAuthent.is())
+                    {
+                        xSuppAuthent->setUserName(aLogin.GetName());
+                        xSuppAuthent->setPassword(aLogin.GetPassword());
+                        xSuppAuthent->setRememberPassword(
+                                    aLogin.IsSavePassword()
+                                    ?
+                                            bRememberPersistent
+                                        ?   RememberAuthentication_PERSISTENT
+                                        :   RememberAuthentication_SESSION
+                                    :   RememberAuthentication_NO);
+                        xSuppAuthent->setAccount(aLogin.GetAccount());
+                        xSuppAuthent->select();
+                    }
+                    break;
+                case RET_RETRY:
+                    if (-1 != nRetryPos)
+                        _rContinuations[nRetryPos]->select();
+                    break;
+                default:
+                    if (-1 != nAbortPos)
+                        _rContinuations[nAbortPos]->select();
+                    break;
+            }
+        }
+        catch(Exception&)
+        {
+            DBG_ERROR("OInteractionHandler::implHandle(AuthenticationRequest): error while calling back into the InteractionContinuation!");
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    void OInteractionHandler::implHandle(const SQLExceptionInfo& _rSqlInfo, const Sequence< Reference< XInteractionContinuation > >& _rContinuations)
+    {
+        sal_Int32 nApprovePos = getContinuation(APPROVE, _rContinuations);
+        sal_Int32 nAbortPos = getContinuation(ABORT, _rContinuations);
+        sal_Int32 nRetryPos = getContinuation(RETRY, _rContinuations);
+#ifdef DBG_UTIL
+        sal_Int32 nDisapprovePos = getContinuation(DISAPPROVE, _rContinuations);
+        sal_Int32 nAuthentPos = getContinuation(SUPPLY_AUTHENTICATION, _rContinuations);
+        DBG_ASSERT((-1 == nDisapprovePos) && (-1 == nAuthentPos), "OInteractionHandler::implHandle(SQLExceptionInfo): unsupported continuation type!");
+            // "Retry" and "Authenticate" do not make sense if the request refered to an SQLException
+#endif
+        // determine the style of the dialog, dependent in the present continuation types
+        WinBits nDialogStyle = WB_OK | WB_DEF_OK;
+        if (-1 != nAbortPos)
+            nDialogStyle = WB_OK_CANCEL;
+        if (-1 != nRetryPos)
+            nDialogStyle |= WB_RETRY_CANCEL | WB_DEF_RETRY;
+
+        // excute the dialog
+        OSQLMessageBox aDialog(NULL, _rSqlInfo, nDialogStyle);
+            // TODO: need a way to specify the parent window
+        sal_Int16 nResult = aDialog.Execute();
+        try
+        {
+            switch (nResult)
+            {
+                case RET_OK:
+                    if (-1 != nApprovePos)
+                        _rContinuations[nApprovePos]->select();
+                    break;
+                case RET_CANCEL:
+                    if (-1 != nAbortPos)
+                        _rContinuations[nAbortPos]->select();
+                    break;
+                case RET_RETRY:
+                    if (-1 != nRetryPos)
+                        _rContinuations[nRetryPos]->select();
+                    break;
+            }
+        }
+        catch(RuntimeException&)
+        {
+            DBG_ERROR("OInteractionHandler::implHandle: caught a RuntimeException while calling the continuation callback!");
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    sal_Int32 OInteractionHandler::getContinuation(Continuation _eCont, const Sequence< Reference< XInteractionContinuation > >& _rContinuations)
+    {
+        const Reference< XInteractionContinuation >* pContinuations = _rContinuations.getConstArray();
+        for (sal_Int32 i=0; i<_rContinuations.getLength(); ++i, ++pContinuations)
+        {
+            switch (_eCont)
+            {
+                case APPROVE:
+                    if (Reference< XInteractionApprove >(*pContinuations, UNO_QUERY).is())
+                        return i;
+                    break;
+                case DISAPPROVE:
+                    if (Reference< XInteractionDisapprove >(*pContinuations, UNO_QUERY).is())
+                        return i;
+                    break;
+                case RETRY:
+                    if (Reference< XInteractionRetry >(*pContinuations, UNO_QUERY).is())
+                        return i;
+                    break;
+                case ABORT:
+                    if (Reference< XInteractionAbort >(*pContinuations, UNO_QUERY).is())
+                        return i;
+                    break;
+                case SUPPLY_AUTHENTICATION:
+                    if (Reference< XInteractionSupplyAuthentication >(*pContinuations, UNO_QUERY).is())
+                        return i;
+                    break;
+            }
+        }
+
+        return -1;
+    }
+
+
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 25.10.00 10:18:21  fs
+ ************************************************************************/
+
---
+++ dbaccess/source/ui/uno/dbinteraction.hxx
@@ -0,0 +1,173 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dbinteraction.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2000-10-25 12:59:51 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _DBAUI_INTERACTION_HXX_
+#define _DBAUI_INTERACTION_HXX_
+
+#ifndef _CPPUHELPER_IMPLBASE2_HXX_
+#include <cppuhelper/implbase2.hxx>
+#endif
+
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
+#include <com/sun/star/task/XInteractionHandler.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UCB_AUTHENTICATIONREQUEST_HPP_
+#include <com/sun/star/ucb/AuthenticationRequest.hpp>
+#endif
+#ifndef _DBASHARED_APITOOLS_HXX_
+#include "apitools.hxx"
+#endif
+
+#ifndef _DBAUI_MODULE_DBU_HXX_
+#include "moduledbu.hxx"
+#endif
+
+namespace dbtools
+{
+    class SQLExceptionInfo;
+}
+
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+
+    //=========================================================================
+    //= OInteractionHandler
+    //=========================================================================
+    typedef ::cppu::WeakImplHelper2 <   ::com::sun::star::lang::XServiceInfo
+                                    ,   ::com::sun::star::task::XInteractionHandler
+                                    >   OInteractionHandler_Base;
+    /** implements an <type scope="com.sun.star.task">XInteractionHandler</type> for
+        database related interaction requests.
+        <p/>
+        Supported interaction requests by now (specified by an exception: The appropriate exception
+        has to be returned by the getRequest method of the object implementing the
+        <type scope="com.sun.star.task">XInteractionRequest</type> interface.
+            <ul>
+                <li><b><type scope="com.sun.star.sdbc">SQLException</type></b>: requests to display a
+                    standard error dialog for the (maybe chained) exception given</li>
+            </ul>
+    */
+    class OInteractionHandler
+                :public OInteractionHandler_Base
+                ,public OModuleClient       // want to use resources
+    {
+        ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >
+                m_xORB;
+    public:
+        OInteractionHandler(const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB);
+
+    // XServiceInfo
+        DECLARE_SERVICE_INFO_STATIC();
+
+    // XInteractionHandler
+        virtual void SAL_CALL handle( const ::com::sun::star::uno::Reference< ::com::sun::star::task::XInteractionRequest >& Request ) throw(::com::sun::star::uno::RuntimeException);
+
+    protected:
+        /// handle SQLExceptions (and derived classes)
+        void    implHandle(
+                    const ::dbtools::SQLExceptionInfo& _rSqlInfo,
+                    const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::task::XInteractionContinuation > >& _rContinuations);
+
+        /// handle authentication requests
+        void    implHandle(
+                    const ::com::sun::star::ucb::AuthenticationRequest& _rAuthRequest,
+                    const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::task::XInteractionContinuation > >& _rContinuations);
+
+        /// known continuation types
+        enum Continuation
+        {
+            APPROVE,
+            DISAPPROVE,
+            RETRY,
+            ABORT,
+            SUPPLY_AUTHENTICATION
+        };
+        /** check if a given continuation sequence contains a given continuation type<p/>
+            @return     the index within <arg>_rContinuations</arg> of the first occurence of a continuation
+                        of the requested type, -1 of no such continuation exists
+        */
+        sal_Int32 getContinuation(
+            Continuation _eCont,
+            const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::task::XInteractionContinuation > >& _rContinuations);
+    };
+
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
+
+#endif // _DBAUI_INTERACTION_HXX_
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 25.10.00 10:18:27  fs
+ ************************************************************************/
+
