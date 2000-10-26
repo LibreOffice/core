d70b2f814e5d20d64c137c4d5587ff553b5e52f0
Niklas Nebel <nn@openoffice.org>
2000-10-26 18:02:44 +0000

    docsh8 needs exceptions

--- sc/source/ui/docshell/makefile.mk
+++ sc/source/ui/docshell/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:44:55 $
+#   last change: $Author: nn $ $Date: 2000-10-26 19:02:44 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -130,6 +130,7 @@ SLOFILES =  \
 
 
 EXCEPTIONSFILES= \
+        $(SLO)$/docsh8.obj \
         $(SLO)$/dbdocimp.obj
 
 
