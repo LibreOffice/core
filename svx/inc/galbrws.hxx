030f0ab1e5a6dd4d3664ebb7a1de4bed87aa7017
Armin Weiss <aw@openoffice.org>
2000-10-30 08:58:01 +0000

    change SdrObjects to use SfxItemSet instead of SfxSetItems. Removed TakeAttributes() and SetAttributes(), new ItemSet modification methods (GetItem[Set], SetItem[Set], ClearItem,...)

--- svx/inc/galbrws.hxx
+++ svx/inc/galbrws.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: galbrws.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:00:56 $
+ *  last change: $Author: aw $ $Date: 2000-10-30 09:54:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,6 +67,9 @@
 #endif
 #ifndef _SFXDOCKWIN_HXX
 #include <sfx2/dockwin.hxx>
+#endif
+#ifndef _SV_GRAPH_HXX
+#include <vcl/graph.hxx>
 #endif
 
 // -----------
