ad6c66a601695f1abb7ef69184eab6c4943b7ebd
Jens-Heiner Rechtien <hr@openoffice.org>
2000-10-31 15:00:08 +0000

    #65293#: moved include

--- sd/source/core/drawdoc4.cxx
+++ sd/source/core/drawdoc4.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drawdoc4.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: dl $ $Date: 2000-10-25 10:29:22 $
+ *  last change: $Author: hr $ $Date: 2000-10-31 16:00:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,11 +70,11 @@
 #include <offmgr/osplcfg.hxx>
 #endif
 #include "sdoutl.hxx"
+#include <unotools/processfactory.hxx>
 #else
 #ifndef _OUTLINER_HXX //autogen wg. Outliner
 #include <svx/outliner.hxx>
 #endif
-#include <unotools/processfactory.hxx>
 #endif // !SVX_LIGHT
 
 #ifndef _EEITEM_HXX //autogen
