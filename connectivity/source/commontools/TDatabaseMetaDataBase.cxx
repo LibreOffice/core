933d58abf2a157a499385020c6dff6cf0e8c0696
Ocke Janssen <oj@openoffice.org>
2000-11-03 12:26:24 +0000

    new base class for databasemetadata

---
+++ connectivity/source/commontools/TDatabaseMetaDataBase.cxx
@@ -0,0 +1,100 @@
+/*************************************************************************
+ *
+ *  $RCSfile: TDatabaseMetaDataBase.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: oj $ $Date: 2000-11-03 13:26:24 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _CONNECTIVITY_ODATABASEMETADATABASE_HXX_
+#include "TDatabaseMetaDataBase.hxx"
+#endif
+#ifndef COMPHELPER_EVENTLISTENERHELPER_HXX
+#include <comphelper/evtlistenerhlp.hxx>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
+#include <com/sun/star/lang/XComponent.hpp>
+#endif
+
+using namespace com::sun::star::uno;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::sdbc;
+using namespace com::sun::star::lang;
+using namespace comphelper;
+using namespace connectivity;
+
+
+ODatabaseMetaDataBase::ODatabaseMetaDataBase(const Reference< XConnection >& _rxConnection)
+    : m_xConnection(_rxConnection)
+{
+    osl_incrementInterlockedCount( &m_refCount );
+    m_xListenerHelper = new OEventListenerHelper(this);
+    Reference<XComponent> xCom(m_xConnection,UNO_QUERY);
+    if(xCom.is())
+        xCom->addEventListener(m_xListenerHelper);
+    osl_decrementInterlockedCount( &m_refCount );
+}
+// -------------------------------------------------------------------------
+ODatabaseMetaDataBase::~ODatabaseMetaDataBase()
+{
+}
+// -----------------------------------------------------------------------------
+void SAL_CALL ODatabaseMetaDataBase::disposing( const EventObject& Source ) throw(RuntimeException)
+{
+    // cut off all references to the connection
+    m_xConnection       = NULL;
+    m_xListenerHelper   = NULL;
+}
+
