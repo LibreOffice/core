683ba83ceb531d1c9502c5ad38cb1a0174a0ca7a
Thomas Benisch <tbe@openoffice.org>
2000-11-03 14:33:16 +0000

    removed typo in previous commit

--- basctl/source/basicide/macrodlg.cxx
+++ basctl/source/basicide/macrodlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: macrodlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tbe $ $Date: 2000-11-03 15:20:51 $
+ *  last change: $Author: tbe $ $Date: 2000-11-03 15:33:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
