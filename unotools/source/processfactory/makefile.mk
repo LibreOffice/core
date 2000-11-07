17eb1e7177aee312f64a46ac23652c4ca7d1f69e
Jens-Heiner Rechtien <hr@openoffice.org>
2000-11-07 09:00:22 +0000

    #65393#: componentfactory migrated to comphelper

--- unotools/source/processfactory/makefile.mk
+++ unotools/source/processfactory/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:03:55 $
+#   last change: $Author: hr $ $Date: 2000-11-07 10:00:22 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,8 +88,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Files -------------------------------------
 
-SLOFILES=	$(SLO)$/processfactory.obj	\
-        $(SLO)$/componentfactory.obj
+SLOFILES=	$(SLO)$/processfactory.obj
 
 # --- Targets ----------------------------------
 
