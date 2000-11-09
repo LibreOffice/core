f0ad747053c828e869c7a7df84152a437300bf7f
Andreas Martens <ama@openoffice.org>
2000-11-09 14:09:29 +0000

    Fix #78742#: Remove has to delete the format too

--- sw/source/core/draw/dcontact.cxx
+++ sw/source/core/draw/dcontact.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dcontact.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: jp $ $Date: 2000-10-30 17:57:05 $
+ *  last change: $Author: ama $ $Date: 2000-11-09 15:09:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -423,7 +423,6 @@ void SwDrawContact::_Changed(const SdrObject& rObj, SdrUserCallType eType,
                     lcl_Notify( this, pOldBoundRect );
                 DisconnectFromLayout( FALSE );
                 SetMaster( NULL );
-//              GetFmt()->GetDoc()->DelLayoutFmt( GetFmt() );
                 delete this;
                 break;
             }
@@ -438,6 +437,8 @@ void SwDrawContact::_Changed(const SdrObject& rObj, SdrUserCallType eType,
             {
                 if( bNotify )
                     lcl_Notify( this, pOldBoundRect );
+                if( GetAnchor() )
+                    GetFmt()->GetDoc()->DelLayoutFmt( GetFmt() );
                 DisconnectFromLayout( FALSE );
                 break;
             }
