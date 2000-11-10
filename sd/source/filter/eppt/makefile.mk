a57e356e6bb90f5831a22694f29305230f673091
Sven Jacobi <sj@openoffice.org>
2000-11-10 07:18:59 +0000

    added linking to unotoolslib

--- sd/source/filter/eppt/makefile.mk
+++ sd/source/filter/eppt/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: sj $ $Date: 2000-11-03 18:03:34 $
+#   last change: $Author: sj $ $Date: 2000-11-10 08:18:59 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -110,6 +110,7 @@ SHL1STDLIBS = \
                 $(SVTOOLLIB)		\
                 $(VCLLIB)			\
                 $(UCBHELPERLIB)		\
+                $(UNOTOOLSLIB)		\
                 $(SVXLIB)
 
 # --- Targets --------------------------------------------------------------
