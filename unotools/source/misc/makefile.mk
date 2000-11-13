661ed26adc1e3489491dc40eb4d68868c4c7b215
Patrick Luby <pluby@openoffice.org>
2000-11-13 22:11:47 +0000

    Eliminated stray quote character in SLOFILES macro

--- unotools/source/misc/makefile.mk
+++ unotools/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: obo $ $Date: 2000-11-08 17:15:06 $
+#   last change: $Author: pluby $ $Date: 2000-11-13 23:11:47 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,7 +104,7 @@ SLOFILES=	$(SLO)$/atom.obj \
 # NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
 # SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
 .IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
-SLOFILES+=$(SLO)$/staticmbatom".obj 
+SLOFILES+=$(SLO)$/staticmbatom.obj 
 .ENDIF
 
 
