c452dc78a8ffeb113357295cbab12d7563095ffc
Niklas Nebel <nn@openoffice.org>
2000-11-13 18:37:06 +0000

    #80222# less and greater added to RefFinder delimiters

--- sc/source/core/tool/reffind.cxx
+++ sc/source/core/tool/reffind.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: reffind.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:16:18 $
+ *  last change: $Author: nn $ $Date: 2000-11-13 19:37:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,7 +78,7 @@
 
 //  incl. Doppelpunkt -> Doppelte Referenzen werden einzeln behandelt
 const sal_Unicode __FAR_DATA ScRefFinder::pDelimiters[] = {
-    '=','(',')',';','+','-','*','/','^','&',' ','{','}',':', 0
+    '=','(',')',';','+','-','*','/','^','&',' ','{','}','<','>',':', 0
 };
 
 // =======================================================================
