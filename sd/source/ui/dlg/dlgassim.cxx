b55aaa6dadbfdb8b558d2b50fe3a8c7ff3f61dbc
Jens-Heiner Rechtien <hr@openoffice.org>
2000-11-14 17:07:51 +0000

    #65293#: includes

--- sd/source/ui/dlg/dlgassim.cxx
+++ sd/source/ui/dlg/dlgassim.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dlgassim.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ka $ $Date: 2000-11-10 16:48:33 $
+ *  last change: $Author: hr $ $Date: 2000-11-14 18:07:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,6 +65,7 @@
 #ifndef _URLOBJ_HXX //autogen
 #include <tools/urlobj.hxx>
 #endif
+#include <svtools/pathoptions.hxx>
 
 #include "sdpage.hxx"
 #include "sdoutl.hxx"
