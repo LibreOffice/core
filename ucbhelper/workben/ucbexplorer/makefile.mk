a70881f29658c22f53534b28124a4c235581ba49
Kai Sommerfeld <kso@openoffice.org>
2000-11-15 06:59:09 +0000

    Removed: UNOTOOLSLIB, Added: COMPHELPERLIB

--- ucbhelper/workben/ucbexplorer/makefile.mk
+++ ucbhelper/workben/ucbexplorer/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:03:38 $
+#   last change: $Author: kso $ $Date: 2000-11-15 07:59:09 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -101,7 +101,7 @@ APP1STDLIBS=$(SALLIB) 	 \
         $(VOSLIB) 	 \
         $(TOOLSLIB)    	 \
         $(SVTOOLLIB)	 \
-        $(UNOTOOLSLIB) 	 \
+        $(COMPHELPERLIB) \
         $(UCBHELPERLIB)  \
         $(SVLIB)
 
