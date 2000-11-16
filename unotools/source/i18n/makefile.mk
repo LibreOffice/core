9177036ac5f8b7e73518aa3ff53b49a66b7e76b0
jp <jp@openoffice.org>
2000-11-16 21:21:35 +0000

    helper class for the access to the text search interface

--- unotools/source/i18n/makefile.mk
+++ unotools/source/i18n/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: pluby $ $Date: 2000-10-19 19:44:18 $
+#   last change: $Author: jp $ $Date: 2000-11-16 22:17:01 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -98,14 +98,15 @@ ENABLE_EXCEPTIONS=TRUE
 SLOFILES=	\
     $(SLO)$/charclass.obj	\
     $(SLO)$/localedatawrapper.obj	\
-    $(SLO)$/numberformatcodewrapper.obj
+    $(SLO)$/numberformatcodewrapper.obj \
+    $(SLO)$/textsearch.obj
 
 # NETBSD: somewhere we have to instantiate the static data members.
 # NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism
 # for GCC won't work.  SCO and MACOSX: the linker does know about weak
 # symbols, but we can't ignore multiple defined symbols
 .IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
-SLOFILES+=$(SLO)$/staticmbi18n.obj 
+SLOFILES+=$(SLO)$/staticmbi18n.obj
 .ENDIF
 
 # --- Targets ----------------------------------
