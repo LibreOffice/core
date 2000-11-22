d87e087ac7b0d15267330a1c7faa5d3198697ff9
Jens-Heiner Rechtien <hr@openoffice.org>
2000-11-22 13:09:08 +0000

    #65293#: default parameters are not possible in typedef

--- sw/source/core/inc/pamtyp.hxx
+++ sw/source/core/inc/pamtyp.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pamtyp.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jp $ $Date: 2000-11-20 09:21:18 $
+ *  last change: $Author: hr $ $Date: 2000-11-22 14:09:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -117,7 +117,7 @@ typedef void (*GoSection)( SwPosition* );
 typedef FASTBOOL (SwPosition:: *CmpOp)( const SwPosition& ) const;
 typedef const SwTxtAttr* (*GetHint)( const SwpHints&, USHORT&, xub_StrLen );
 typedef int (utl::TextSearch:: *SearchTxt)( const String&, xub_StrLen*,
-                    xub_StrLen*, ::com::sun::star::util::SearchResult* =0 );
+                    xub_StrLen*, ::com::sun::star::util::SearchResult* );
 typedef void (SwNodes:: *MvSection)( SwNodeIndex * ) const;
 
 
