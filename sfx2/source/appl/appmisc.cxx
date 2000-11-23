f63c06c5d6ebadcafc0f386db4e99bd79473ba6c
Peter Burow <pb@openoffice.org>
2000-11-23 08:46:00 +0000

    fix: #80647# must convert the modules path to normalized path

--- sfx2/source/appl/appmisc.cxx
+++ sfx2/source/appl/appmisc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: appmisc.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: pb $ $Date: 2000-11-23 07:47:43 $
+ *  last change: $Author: pb $ $Date: 2000-11-23 09:46:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1043,8 +1043,8 @@ Config* SfxApplication::GetFilterIni()
         // load modules path from configuration
         String aIniFile( SvtPathOptions().GetModulePath() );
         ::rtl::OUString aTemp;
-        // it's an URL -> convert to normalized path
-        if ( ::osl::FileBase::getNormalizedPathFromFileURL( aIniFile, aTemp ) == osl::FileBase::E_None )
+        // convert to normalized path
+        if ( ::osl::FileBase::normalizePath( aIniFile, aTemp ) == osl::FileBase::E_None )
         {
             // append the name of the filter ini
             aIniFile = aTemp;
