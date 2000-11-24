ba1e6d031235950a55086e64d7abbceb6a11eb03
Frank Schönheit <fs@openoffice.org>
2000-11-24 09:14:25 +0000

    #80583# extended isValidNameCont so that '.' is allowed as a inner character of a name

--- configmgr/source/treemgr/configpath.cxx
+++ configmgr/source/treemgr/configpath.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: configpath.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2000-11-14 10:27:37 $
+ *  last change: $Author: fs $ $Date: 2000-11-24 10:14:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -154,8 +154,10 @@ namespace
     inline
     bool isValidNameCont(sal_Unicode ch)
     {
-        return (sal_Unicode('0') <= ch && ch <= sal_Unicode('9')) ||
-                sal_Unicode('-') == ch;
+        return  (   (sal_Unicode('0') <= ch && ch <= sal_Unicode('9'))
+                ||  (sal_Unicode('-') == ch)
+                ||  (sal_Unicode('.') == ch)
+                );
     }
 
     //-------------------------------------------------------------------------
