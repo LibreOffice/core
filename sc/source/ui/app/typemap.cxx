ffe38662ea22c31ad0726142a672563dfa0413f0
Niklas Nebel <nn@openoffice.org>
2000-11-25 13:34:49 +0000

    #80753# include grafctrl

--- sc/source/ui/app/typemap.cxx
+++ sc/source/ui/app/typemap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: typemap.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:53 $
+ *  last change: $Author: nn $ $Date: 2000-11-25 14:34:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#include <segmentc.hxx>
 
 #pragma hdrstop
 
@@ -137,6 +135,7 @@
 #include <svx/xftshxy.hxx>
 #include <svx/xftsfit.hxx>
 #include <svx/langitem.hxx>
+#include <svx/grafctrl.hxx>
 
 #include "attrib.hxx"
 
@@ -173,85 +172,7 @@
 #define SvxCellOrientationEnumItem  SfxUInt16Item
 #define SvxLanguage                 SfxUInt16Item
 
-#pragma code_seg("SCSTATICS")
-
 #define SFX_TYPEMAP
 #include "scslots.hxx"
 
-#pragma code_seg()
-
-SEG_EOFGLOBALS()
-
-
-/*************************************************************************
-
-    Source Code Control System - Update
-
-    $Log: not supported by cvs2svn $
-    Revision 1.18  2000/09/17 14:08:54  willem.vandorp
-    OpenOffice header added.
-
-    Revision 1.17  2000/08/31 16:38:18  willem.vandorp
-    Header and footer replaced
-
-    Revision 1.16  1999/09/24 16:01:52  nn
-    hlnkitem.hxx moved to svx
-
-    Revision 1.15  1998/02/06 13:15:50  HJS
-    includes
-
-
-      Rev 1.14   06 Feb 1998 14:15:50   HJS
-   includes
-
-      Rev 1.13   05 Dec 1997 20:07:56   ANK
-   Includes geaendert
-
-      Rev 1.12   03 Dec 1997 14:27:06   HJS
-   includes
-
-      Rev 1.11   05 Aug 1997 15:10:24   TJ
-   include svx/srchitem.hxx
-
-      Rev 1.10   02 May 1997 17:38:10   NF
-   includes...
-
-      Rev 1.9   18 Feb 1997 17:02:30   NN
-   dbcoll
-
-      Rev 1.8   01 Nov 1996 06:25:52   MH
-   chg: includes, defines
-
-      Rev 1.7   20 Sep 1996 11:02:36   NN
-   SCSTATICS Segment fuer statische Konstruktoren
-
-      Rev 1.6   13 Feb 1996 16:21:06   JN
-   Include fuer SvxAddressItem
-
-      Rev 1.5   26 Jan 1996 09:37:40   JN
-   Auch eigene Typen includen
-
-      Rev 1.4   19 Dec 1995 11:55:52   MD
-   ...
-
-      Rev 1.3   11 Dec 1995 18:41:00   MO
-   Text-Items hinzugefuegt
-
-      Rev 1.2   07 Dec 1995 17:11:12   MO
-   SvxDrawAlignItem
-
-      Rev 1.1   04 Dec 1995 14:22:22   JN
-   Zwischenstand fuer Updater
-
-      Rev 1.0   28 Nov 1995 19:04:06   MO
-   Initial revision.
-
-      Rev 1.0   28 Nov 1995 09:49:20   JN
-   Umstellung auf 303a
-
-   Initial revision.
-
-*************************************************************************/
-
-#pragma SEG_EOFMODULE
 
