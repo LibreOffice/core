2c277be649b78e725dd80db26f04ad1e1764ff61
Niklas Nebel <nn@openoffice.org>
2000-11-26 12:53:57 +0000

    #80747# Export: GetLanguage not needed

--- sc/source/filter/xml/xmlwrap.cxx
+++ sc/source/filter/xml/xmlwrap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlwrap.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: sab $ $Date: 2000-11-16 18:03:05 $
+ *  last change: $Author: nn $ $Date: 2000-11-26 13:53:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -264,7 +264,6 @@ sal_Bool ScXMLImportWrapper::Export()
     {
         pObjSh->UpdateDocInfoForSave();     // update information
 
-        LanguageType eDocLang = rDoc.GetLanguage();
         uno::Reference<frame::XModel> xModel = pObjSh->GetModel();
 
         ScXMLExport *pExp = new ScXMLExport( xModel, sFileName, xHandler, sal_False );
