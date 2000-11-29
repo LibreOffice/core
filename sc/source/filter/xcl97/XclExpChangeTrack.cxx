bb18f8eb17ef791c9d880aafe4aaf991fc37d7a8
Daniel Rentz <dr@openoffice.org>
2000-11-29 10:19:15 +0000

    initial revision, no functionality

---
+++ sc/source/filter/xcl97/XclExpChangeTrack.cxx
@@ -0,0 +1,104 @@
+/*************************************************************************
+ *
+ *  $RCSfile: XclExpChangeTrack.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: dr $ $Date: 2000-11-29 11:19:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifdef PCH
+#include "filt_pch.hxx"
+#endif
+
+#pragma hdrstop
+
+//___________________________________________________________________
+
+#ifndef _SC_XCLEXPCHANGETRACK_HXX
+#include "XclExpChangeTrack.hxx"
+#endif
+
+//___________________________________________________________________
+
+extern const sal_Char*  pRevLogStreamName;
+
+//___________________________________________________________________
+
+XclExpChangeTrack::XclExpChangeTrack( RootData* pRootData ) :
+    ExcRoot( pRootData )
+{
+    DBG_ASSERT( pRootData, "XclExpChangeTrack::XclExpChangeTrack - no root data" );
+}
+
+XclExpChangeTrack::~XclExpChangeTrack()
+{
+}
+
+void XclExpChangeTrack::Write()
+{
+    if( !aRecList.Count() )
+        return;
+
+    String sStreamName( RTL_CONSTASCII_STRINGPARAM( pRevLogStreamName ) );
+    SvStream* pStrm = pExcRoot->pRootStorage->OpenStream( sStreamName, STREAM_READWRITE | STREAM_TRUNC );
+    DBG_ASSERT( pStrm, "XclExpChangeTrack::Write - no stream" );
+    if( !pStrm ) return;
+
+    for( ExcRecord* pRec = aRecList.First(); pRec; pRec = aRecList.Next() )
+        pRec->Save( *pStrm );
+    delete pStrm;
+}
+
