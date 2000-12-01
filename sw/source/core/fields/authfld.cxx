0430421c361dfa59d9377f31d2dba9adbce15019
Daniel Vogelheim <dvo@openoffice.org>
2000-12-01 09:57:46 +0000

    #80616# authority field now accepts new data

--- sw/source/core/fields/authfld.cxx
+++ sw/source/core/fields/authfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: authfld.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: os $ $Date: 2000-11-28 08:58:23 $
+ *  last change: $Author: dvo $ $Date: 2000-12-01 10:57:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -898,7 +898,7 @@ BOOL    SwAuthorityField::PutValue( const Any& rVal, const String& rProperty )
     }
 
     ((SwAuthorityFieldType*)GetTyp())->RemoveField(nHandle);
-    ((SwAuthorityFieldType*)GetTyp())->AddField(sToSet);
+    nHandle = ((SwAuthorityFieldType*)GetTyp())->AddField(sToSet);
 
     return FALSE;
 }
