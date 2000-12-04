431dc03de9716a3c1e3b2ee92c02e1eb1e863606
Stephan Bergmann <sb@openoffice.org>
2000-12-04 16:45:16 +0000

    #80720# Added remotecontentproviderscontrol.obj.

--- ucbhelper/source/provider/makefile.mk
+++ ucbhelper/source/provider/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: sb $ $Date: 2000-11-09 13:21:10 $
+#   last change: $Author: sb $ $Date: 2000-12-04 17:45:16 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -86,6 +86,7 @@ SLOFILES=\
         $(SLO)$/contentinfo.obj        \
         $(SLO)$/propertyvalueset.obj   \
         $(SLO)$/registerucb.obj        \
+        $(SLO)$/remotecontentproviderscontrol.obj \
         $(SLO)$/resultsetmetadata.obj  \
         $(SLO)$/resultset.obj          \
         $(SLO)$/resultsethelper.obj    \
