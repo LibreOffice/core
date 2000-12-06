9e2f95d2af3dbb979dfb67c506485ecbf518f01a
Armin Weiss <aw@openoffice.org>
2000-12-06 15:46:01 +0000

    #80594# added Clone() operators to 3d items

--- svx/source/engine3d/svx3ditems.cxx
+++ svx/source/engine3d/svx3ditems.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx3ditems.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: aw $ $Date: 2000-11-24 16:22:01 $
+ *  last change: $Author: aw $ $Date: 2000-12-06 16:46:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -94,6 +94,207 @@ using namespace ::rtl;
 using namespace ::com::sun::star;
 
 //////////////////////////////////////////////////////////////////////////////
+Svx3DPercentDiagonalItem::Svx3DPercentDiagonalItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_PERCENT_DIAGONAL, nVal)
+{}
+
+Svx3DBackscaleItem::Svx3DBackscaleItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_BACKSCALE, nVal)
+{}
+
+Svx3DDepthItem::Svx3DDepthItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DOBJ_DEPTH, nVal)
+{}
+
+Svx3DHorizontalSegmentsItem::Svx3DHorizontalSegmentsItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DOBJ_HORZ_SEGS, nVal)
+{}
+
+Svx3DVerticalSegmentsItem::Svx3DVerticalSegmentsItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DOBJ_VERT_SEGS, nVal)
+{}
+
+Svx3DEndAngleItem::Svx3DEndAngleItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DOBJ_END_ANGLE, nVal)
+{}
+
+Svx3DDoubleSidedItem::Svx3DDoubleSidedItem(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DOBJ_DOUBLE_SIDED, bVal)
+{}
+
+Svx3DNormalsKindItem::Svx3DNormalsKindItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_NORMALS_KIND, nVal)
+{}
+
+Svx3DNormalsInvertItem::Svx3DNormalsInvertItem(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DOBJ_NORMALS_INVERT, bVal)
+{}
+
+Svx3DTextureProjectionXItem::Svx3DTextureProjectionXItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_TEXTURE_PROJ_X, nVal)
+{}
+
+Svx3DTextureProjectionYItem::Svx3DTextureProjectionYItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_TEXTURE_PROJ_Y, nVal)
+{}
+
+Svx3DShadow3DItem::Svx3DShadow3DItem(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DOBJ_SHADOW_3D, bVal)
+{}
+
+Svx3DMaterialColorItem::Svx3DMaterialColorItem(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DOBJ_MAT_COLOR)
+{}
+
+Svx3DMaterialEmissionItem::Svx3DMaterialEmissionItem(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DOBJ_MAT_EMISSION)
+{}
+
+Svx3DMaterialSpecularItem::Svx3DMaterialSpecularItem(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DOBJ_MAT_SPECULAR)
+{}
+
+Svx3DMaterialSpecularIntensityItem::Svx3DMaterialSpecularIntensityItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_MAT_SPECULAR_INTENSITY, nVal)
+{}
+
+Svx3DTextureKindItem::Svx3DTextureKindItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_TEXTURE_KIND, nVal)
+{}
+
+Svx3DTextureModeItem::Svx3DTextureModeItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DOBJ_TEXTURE_MODE, nVal)
+{}
+
+Svx3DTextureFilterItem::Svx3DTextureFilterItem(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DOBJ_TEXTURE_FILTER, bVal)
+{}
+
+Svx3DPerspectiveItem::Svx3DPerspectiveItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DSCENE_PERSPECTIVE, nVal)
+{}
+
+Svx3DDistanceItem::Svx3DDistanceItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DSCENE_DISTANCE, nVal)
+{}
+
+Svx3DFocalLengthItem::Svx3DFocalLengthItem(sal_uInt32 nVal)
+:   SfxUInt32Item(SDRATTR_3DSCENE_FOCAL_LENGTH, nVal)
+{}
+
+Svx3DTwoSidedLightingItem::Svx3DTwoSidedLightingItem(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_TWO_SIDED_LIGHTING, bVal)
+{}
+
+Svx3DLightcolor1Item::Svx3DLightcolor1Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_1)
+{}
+
+Svx3DLightcolor2Item::Svx3DLightcolor2Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_2)
+{}
+
+Svx3DLightcolor3Item::Svx3DLightcolor3Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_3)
+{}
+
+Svx3DLightcolor4Item::Svx3DLightcolor4Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_4)
+{}
+
+Svx3DLightcolor5Item::Svx3DLightcolor5Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_5)
+{}
+
+Svx3DLightcolor6Item::Svx3DLightcolor6Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_6)
+{}
+
+Svx3DLightcolor7Item::Svx3DLightcolor7Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_7)
+{}
+
+Svx3DLightcolor8Item::Svx3DLightcolor8Item(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_LIGHTCOLOR_8)
+{}
+
+Svx3DAmbientcolorItem::Svx3DAmbientcolorItem(const Color& rCol)
+:   SvxColorItem(rCol, SDRATTR_3DSCENE_AMBIENTCOLOR)
+{}
+
+Svx3DLightOnOff1Item::Svx3DLightOnOff1Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_1, bVal)
+{}
+
+Svx3DLightOnOff2Item::Svx3DLightOnOff2Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_2, bVal)
+{}
+
+Svx3DLightOnOff3Item::Svx3DLightOnOff3Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_3, bVal)
+{}
+
+Svx3DLightOnOff4Item::Svx3DLightOnOff4Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_4, bVal)
+{}
+
+Svx3DLightOnOff5Item::Svx3DLightOnOff5Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_5, bVal)
+{}
+
+Svx3DLightOnOff6Item::Svx3DLightOnOff6Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_6, bVal)
+{}
+
+Svx3DLightOnOff7Item::Svx3DLightOnOff7Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_7, bVal)
+{}
+
+Svx3DLightOnOff8Item::Svx3DLightOnOff8Item(BOOL bVal)
+:   SfxBoolItem(SDRATTR_3DSCENE_LIGHTON_8, bVal)
+{}
+
+Svx3DLightDirection1Item::Svx3DLightDirection1Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_1, rVec)
+{}
+
+Svx3DLightDirection2Item::Svx3DLightDirection2Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_2, rVec)
+{}
+
+Svx3DLightDirection3Item::Svx3DLightDirection3Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_3, rVec)
+{}
+
+Svx3DLightDirection4Item::Svx3DLightDirection4Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_4, rVec)
+{}
+
+Svx3DLightDirection5Item::Svx3DLightDirection5Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_5, rVec)
+{}
+
+Svx3DLightDirection6Item::Svx3DLightDirection6Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_6, rVec)
+{}
+
+Svx3DLightDirection7Item::Svx3DLightDirection7Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_7, rVec)
+{}
+
+Svx3DLightDirection8Item::Svx3DLightDirection8Item(const Vector3D& rVec)
+:   SvxVector3DItem(SDRATTR_3DSCENE_LIGHTDIRECTION_8, rVec)
+{}
+
+Svx3DShadowSlantItem::Svx3DShadowSlantItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DSCENE_SHADOW_SLANT, nVal)
+{}
+
+Svx3DShadeModeItem::Svx3DShadeModeItem(sal_uInt16 nVal)
+:   SfxUInt16Item(SDRATTR_3DSCENE_SHADE_MODE, nVal)
+{}
+
+//////////////////////////////////////////////////////////////////////////////
 
 // Svx3DNormalsKindItem: use drawing::NormalsKind
 sal_Bool Svx3DNormalsKindItem::QueryValue( uno::Any& rVal, BYTE nMemberId) const
@@ -109,6 +310,11 @@ sal_Bool Svx3DNormalsKindItem::PutValue( const uno::Any& rVal, BYTE nMemberId)
         return sal_False;
     SetValue((sal_Int16)eVar);
     return sal_True;
+}
+
+SfxPoolItem* Svx3DNormalsKindItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DNormalsKindItem(*this);
 }
 
 // Svx3DTextureProjectionXItem: use drawing::TextureProjectionMode
@@ -127,6 +333,11 @@ sal_Bool Svx3DTextureProjectionXItem::PutValue( const uno::Any& rVal, BYTE nMemb
     return sal_True;
 }
 
+SfxPoolItem* Svx3DTextureProjectionXItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DTextureProjectionXItem(*this);
+}
+
 // Svx3DTextureProjectionYItem: use drawing::TextureProjectionMode
 sal_Bool Svx3DTextureProjectionYItem::QueryValue( uno::Any& rVal, BYTE nMemberId) const
 {
@@ -141,6 +352,11 @@ sal_Bool Svx3DTextureProjectionYItem::PutValue( const uno::Any& rVal, BYTE nMemb
         return sal_False;
     SetValue((sal_Int16)eVar);
     return sal_True;
+}
+
+SfxPoolItem* Svx3DTextureProjectionYItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DTextureProjectionYItem(*this);
 }
 
 // Svx3DTextureKindItem: use drawing::TextureKind
@@ -159,6 +375,11 @@ sal_Bool Svx3DTextureKindItem::PutValue( const uno::Any& rVal, BYTE nMemberId)
     return sal_True;
 }
 
+SfxPoolItem* Svx3DTextureKindItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DTextureKindItem(*this);
+}
+
 // Svx3DTextureModeItem: use drawing:TextureMode
 sal_Bool Svx3DTextureModeItem::QueryValue( uno::Any& rVal, BYTE nMemberId) const
 {
@@ -173,6 +394,11 @@ sal_Bool Svx3DTextureModeItem::PutValue( const uno::Any& rVal, BYTE nMemberId)
         return sal_False;
     SetValue((sal_Int16)eVar);
     return sal_True;
+}
+
+SfxPoolItem* Svx3DTextureModeItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DTextureModeItem(*this);
 }
 
 // Svx3DPerspectiveItem: use drawing::ProjectionMode
@@ -191,6 +417,11 @@ sal_Bool Svx3DPerspectiveItem::PutValue( const uno::Any& rVal, BYTE nMemberId)
     return sal_True;
 }
 
+SfxPoolItem* Svx3DPerspectiveItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DPerspectiveItem(*this);
+}
+
 // Svx3DShadeModeItem: use drawing::ShadeMode
 sal_Bool Svx3DShadeModeItem::QueryValue( uno::Any& rVal, BYTE nMemberId) const
 {
@@ -205,6 +436,11 @@ sal_Bool Svx3DShadeModeItem::PutValue( const uno::Any& rVal, BYTE nMemberId)
         return sal_False;
     SetValue((sal_Int16)eVar);
     return sal_True;
+}
+
+SfxPoolItem* Svx3DShadeModeItem::Clone(SfxItemPool* pPool) const
+{
+    return new Svx3DShadeModeItem(*this);
 }
 
 // EOF
