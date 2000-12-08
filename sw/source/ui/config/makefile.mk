3f4b4885103a224c56faea01203da1a20ffd98d9
Oliver Specht <os@openoffice.org>
2000-12-08 09:22:43 +0000

    cfgstr.src not needed anymore

--- sw/source/ui/config/makefile.mk
+++ sw/source/ui/config/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:14:33 $
+#   last change: $Author: os $ $Date: 2000-12-08 10:22:43 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -98,8 +98,7 @@ SRCFILES =	\
         optdlg.src \
         optins.src \
         optload.src \
-        redlopt.src \
-        cfgstr.src
+        redlopt.src
 
 SLOFILES =	\
         $(SLO)$/barcfg.obj \
