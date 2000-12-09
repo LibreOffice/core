d3e5e246d2770ac10109d65aef336ec7dcb25ca5
Björn Milcke <bm@openoffice.org>
2000-12-09 14:59:31 +0000

    #80712# no export of properties that are set to "auto" (min, max, stepmain, stephelp, origin)

--- xmloff/source/chart/PropertyMaps.cxx
+++ xmloff/source/chart/PropertyMaps.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: PropertyMaps.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: bm $ $Date: 2000-12-06 17:07:10 $
+ *  last change: $Author: bm $ $Date: 2000-12-09 15:59:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -150,7 +150,7 @@ using namespace com::sun::star;
 #define XML_SCH_TYPE_TEXT_CROSSEDOUT        ( XML_SCH_TYPES_START + 7 )
 
 #define MAP_ENTRY( a, ns, nm, t ) { a, XML_NAMESPACE_##ns, sXML_##nm, t }
-#define MAP_CONTEXT( a, ns, nm, t, c ) { a, XML_NAMESPACE_##ns, sXML_##nm, t, c }
+#define MAP_CONTEXT( a, ns, nm, t, c ) { a, XML_NAMESPACE_##ns, sXML_##nm, t, XML_SCH_CONTEXT_##c }
 #define MAP_SPECIAL( a, ns, nm, t, c ) { a, XML_NAMESPACE_##ns, sXML_##nm, t | MID_FLAG_SPECIAL_ITEM, XML_SCH_SPECIAL_##c }
 #define MAP_ENTRY_END { 0,0,0,0 }
 
@@ -164,8 +164,15 @@ using namespace com::sun::star;
 #define XML_SCH_SPECIAL_DATA_LABEL_NUMBER 8
 #define XML_SCH_SPECIAL_DATA_LABEL_TEXT 9
 #define XML_SCH_SPECIAL_DATA_LABEL_SYMBOL 10
+#define XML_SCH_SPECIAL_AXIS_MIN 11
+#define XML_SCH_SPECIAL_AXIS_MAX 12
 
-#define XML_SCH_USER_SYMBOL 1001
+#define XML_SCH_CONTEXT_USER_SYMBOL 1001
+#define XML_SCH_CONTEXT_MIN 1002
+#define XML_SCH_CONTEXT_MAX 1003
+#define XML_SCH_CONTEXT_STEP_MAIN 1004
+#define XML_SCH_CONTEXT_STEP_HELP 1005
+#define XML_SCH_CONTEXT_ORIGIN 1006
 
 const XMLPropertyMapEntry aXMLChartPropMap[] =
 {
@@ -179,7 +186,7 @@ const XMLPropertyMapEntry aXMLChartPropMap[] =
     MAP_ENTRY( "SolidType", CHART, solid_type, XML_SCH_TYPE_SOLID_TYPE ),
     MAP_ENTRY( "SplineType", CHART, splines, XML_TYPE_NUMBER ),
     MAP_ENTRY( "Stacked", CHART, stacked, XML_TYPE_BOOL ),
-    MAP_CONTEXT( "SymbolType", CHART, symbol, XML_TYPE_NUMBER, XML_SCH_USER_SYMBOL ),
+    MAP_CONTEXT( "SymbolType", CHART, symbol, XML_TYPE_NUMBER, USER_SYMBOL ),
     MAP_ENTRY( "Vertical", CHART, vertical, XML_TYPE_BOOL ),
     MAP_ENTRY( "NumberOfLines", CHART, lines_used, XML_TYPE_NUMBER ),
     MAP_ENTRY( "StackedBarsConnected", CHART, connect_bars, XML_TYPE_BOOL ),
@@ -191,11 +198,11 @@ const XMLPropertyMapEntry aXMLChartPropMap[] =
     MAP_SPECIAL( "HelpMarks", CHART, tick_marks_minor_inner, XML_TYPE_NUMBER, TICKS_MIN_INNER ),        // see above
     MAP_SPECIAL( "HelpMarks", CHART, tick_marks_minor_outer, XML_TYPE_NUMBER, TICKS_MIN_OUTER ),
     MAP_ENTRY( "Logarithmic", CHART, logarithmic, XML_TYPE_BOOL ),
-    MAP_ENTRY( "Max", CHART, maximum, XML_TYPE_DOUBLE ),
-    MAP_ENTRY( "Min", CHART, minimum, XML_TYPE_DOUBLE ),
-    MAP_ENTRY( "Origin", CHART, origin, XML_TYPE_DOUBLE ),
-    MAP_ENTRY( "Step", CHART, interval_major, XML_TYPE_DOUBLE ),
-    MAP_ENTRY( "StepHelp", CHART, interval_minor, XML_TYPE_DOUBLE ),
+    MAP_CONTEXT( "Min", CHART, minimum, XML_TYPE_DOUBLE, MIN ),
+    MAP_CONTEXT( "Max", CHART, maximum, XML_TYPE_DOUBLE, MAX ),
+    MAP_CONTEXT( "Origin", CHART, origin, XML_TYPE_DOUBLE, ORIGIN ),
+    MAP_CONTEXT( "StepMain", CHART, interval_major, XML_TYPE_DOUBLE, STEP_MAIN ),
+    MAP_CONTEXT( "StepHelp", CHART, interval_minor, XML_TYPE_DOUBLE, STEP_HELP ),
     MAP_ENTRY( "GapWidth", CHART, gap_width, XML_TYPE_NUMBER ),
     MAP_ENTRY( "Overlap", CHART, overlap, XML_TYPE_NUMBER ),
     MAP_ENTRY( "TextBreak", TEXT, line_break, XML_TYPE_BOOL ),
@@ -422,6 +429,9 @@ void XMLChartExportPropertyMapper::ContextFilter(
     std::vector< XMLPropertyState >& rProperties,
     uno::Reference< beans::XPropertySet > rPropSet ) const
 {
+    ::rtl::OUString aAutoPropName;
+    sal_Bool bCheckAuto = sal_False;
+
     // filter properties
     for( std::vector< XMLPropertyState >::iterator property = rProperties.begin();
          property != rProperties.end();
@@ -431,7 +441,7 @@ void XMLChartExportPropertyMapper::ContextFilter(
         // to prevent writing this property set mnIndex member to -1
         switch( getPropertySetMapper()->GetEntryContextId( property->mnIndex ))
         {
-            case XML_SCH_USER_SYMBOL:
+            case XML_SCH_CONTEXT_USER_SYMBOL:
                 {
                     sal_Int32 nIndex = chart::ChartSymbolType::AUTO;
                     property->maValue >>= nIndex;
@@ -439,6 +449,47 @@ void XMLChartExportPropertyMapper::ContextFilter(
                         property->mnIndex = -1;
                 }
                 break;
+
+            // if Auto... is set the corresponding properties mustn't be exported
+            case XML_SCH_CONTEXT_MIN:
+                bCheckAuto = sal_True;
+                aAutoPropName = ::rtl::OUString::createFromAscii( "AutoMin" );
+                break;
+            case XML_SCH_CONTEXT_MAX:
+                bCheckAuto = sal_True;
+                aAutoPropName = ::rtl::OUString::createFromAscii( "AutoMax" );
+                break;
+            case XML_SCH_CONTEXT_STEP_MAIN:
+                bCheckAuto = sal_True;
+                aAutoPropName = ::rtl::OUString::createFromAscii( "AutoStepMain" );
+                break;
+            case XML_SCH_CONTEXT_STEP_HELP:
+                bCheckAuto = sal_True;
+                aAutoPropName = ::rtl::OUString::createFromAscii( "AutoStepHelp" );
+                break;
+            case XML_SCH_CONTEXT_ORIGIN:
+                bCheckAuto = sal_True;
+                aAutoPropName = ::rtl::OUString::createFromAscii( "AutoOrigin" );
+                break;
+        }
+
+        if( bCheckAuto )
+        {
+            if( rPropSet.is())
+            {
+                try
+                {
+                    sal_Bool bAuto;
+                    uno::Any aAny = rPropSet->getPropertyValue( aAutoPropName );
+                    aAny >>= bAuto;
+                    if( bAuto )
+                        property->mnIndex = -1;
+                }
+                catch( beans::UnknownPropertyException )
+                {
+                }
+            }
+            bCheckAuto = sal_False;
         }
     }
 
@@ -538,9 +589,12 @@ void XMLChartExportPropertyMapper::handleSpecialItem(
                 SvXMLUnitConverter::convertBool( sValueBuffer, bValue );
                 break;
         }
-        sValue = sValueBuffer.makeStringAndClear();
-        sAttrName = rNamespaceMap.GetQNameByKey( nNameSpace, sAttrName );
-        rAttrList.AddAttribute( sAttrName, msCDATA, sValue );
+        if( sValueBuffer.getLength())
+        {
+            sValue = sValueBuffer.makeStringAndClear();
+            sAttrName = rNamespaceMap.GetQNameByKey( nNameSpace, sAttrName );
+            rAttrList.AddAttribute( sAttrName, msCDATA, sValue );
+        }
     }
     else
     {
