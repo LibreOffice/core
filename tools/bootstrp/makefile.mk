a068ad5f8a628a650e29413cb235402049b13e2d
Nils Fuhrmann <nf@openoffice.org>
2000-12-11 11:58:44 +0000

    Fixes for static link of stl

--- tools/bootstrp/makefile.mk
+++ tools/bootstrp/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:02:59 $
+#   last change: $Author: nf $ $Date: 2000-12-11 12:58:44 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,6 +65,8 @@ PRJ=..
 PRJNAME=tools
 TARGET=bootstrp
 TARGETTYPE=CUI
+NO_DEFAULT_STL=TRUE
+
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  svpre.mk
