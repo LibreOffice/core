05eb4f872f3f9384c6392350a0c1218caa15c8a5
Ocke Janssen <oj@openoffice.org>
2000-12-12 11:33:41 +0000

    #80933# new dialogs for size and other attribs

---
+++ dbaccess/source/ui/dlg/dlgattr.cxx
@@ -0,0 +1,171 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dlgattr.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: oj $ $Date: 2000-12-12 12:33:09 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifdef MAC
+#ifdef ITEMID_NUMBERINFO
+#undef ITEMID_NUMBERINFO
+#endif
+#endif
+
+#define ITEMID_NUMBERINFO 0
+
+#ifndef DBAUI_SBATTRDLG_HXX
+#include "dlgattr.hxx"
+#endif
+
+#ifndef _SFXTABDLG_HXX //autogen
+#include <sfx2/tabdlg.hxx>
+#endif
+
+#ifndef _SVX_NUMINF_HXX //autogen
+#include <svx/numinf.hxx>
+#endif
+
+#ifndef _SVX_CHARDLG_HXX //autogen
+#include <svx/chardlg.hxx>
+#endif
+
+#ifndef _SVX_NUMFMT_HXX //autogen
+#include <svx/numfmt.hxx>
+#endif
+#ifndef _SVX_NUMINF_HXX
+#include <svx/numinf.hxx>
+#endif
+
+#ifndef _SVX_ALIGN_HXX //autogen
+#include <svx/align.hxx>
+#endif
+
+#ifndef _SVX_DIALOGS_HRC
+#include <svx/dialogs.hrc>
+#endif
+
+#ifndef _DBAUI_MODULE_DBU_HXX_
+#include "moduledbu.hxx"
+#endif
+#ifndef _DBU_RESOURCE_HRC_
+#include "dbu_resource.hrc"
+#endif
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+#define _ZFORLIST_DECLARE_TABLE
+#ifndef _ZFORLIST_HXX
+#include <svtools/zforlist.hxx>
+#endif
+
+using namespace dbaui;
+
+//==================================================================
+SbaSbAttrDlg::SbaSbAttrDlg(Window* pParent, const SfxItemSet* pCellAttrs, SvNumberFormatter* pFormatter, USHORT nFlags, BOOL bRow)
+             : SfxTabDialog(pParent, ModuleRes( DLG_ATTR ), pCellAttrs )
+             ,aTitle(ResId(ST_ROW))
+{
+    pNumberInfoItem = new SvxNumberInfoItem( pFormatter );
+
+    if (bRow)
+        SetText(aTitle);
+    if( nFlags & TP_ATTR_CHAR )
+        AddTabPage( RID_SVXPAGE_CHAR_STD,String(ResId(TP_ATTR_CHAR)),SvxCharStdPage::Create,            0 );
+    if( nFlags & TP_ATTR_NUMBER )
+        AddTabPage( RID_SVXPAGE_NUMBERFORMAT,String(ResId(TP_ATTR_NUMBER)),SvxNumberFormatTabPage::Create,  0 );
+    if( nFlags & TP_ATTR_ALIGN )
+        AddTabPage( RID_SVXPAGE_ALIGNMENT,String(ResId(TP_ATTR_ALIGN)),SvxAlignmentTabPage::Create, 0 );
+    FreeResource();
+}
+
+// -----------------------------------------------------------------------
+SbaSbAttrDlg::~SbaSbAttrDlg()
+{
+    delete pNumberInfoItem;
+}
+
+// -----------------------------------------------------------------------
+void SbaSbAttrDlg::PageCreated( sal_uInt16 nPageId, SfxTabPage& rTabPage )
+{
+    switch ( nPageId )
+    {
+        case RID_SVXPAGE_NUMBERFORMAT:
+        {
+            ((SvxNumberFormatTabPage&)rTabPage).
+                SetNumberFormatList( *pNumberInfoItem );
+        }
+        break;
+
+        case RID_SVXPAGE_CHAR_STD:
+        {
+            //  ((SvxCharStdPage&)rTabPage).SetFontList(SBA_MOD_EXT()->FontListItem());
+        }
+        break;
+
+        case RID_SVXPAGE_ALIGNMENT:
+        {
+            ((SvxAlignmentTabPage&)rTabPage).SetFlags(WBA_NO_ORIENTATION|WBA_NO_LINEBREAK|WBA_NO_GRIDLINES|WBA_NO_VERTICAL|WBA_NO_LEFTINDENT);
+        }
+        break;
+
+        default:
+        break;
+    }
+}
+
+
---
+++ dbaccess/source/ui/dlg/dlgsize.cxx
@@ -0,0 +1,154 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dlgsize.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: oj $ $Date: 2000-12-12 12:33:26 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef DBAUI_DLGSIZE_HRC
+#include "dlgsize.hrc"
+#endif
+#ifndef _DBAUI_DLGSIZE_HXX
+#include "dlgsize.hxx"
+#endif
+#ifndef _DBAUI_MODULE_DBU_HXX_
+#include "moduledbu.hxx"
+#endif
+#ifndef _DBU_RESOURCE_HRC_
+#include "dbu_resource.hrc"
+#endif
+
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+
+
+#define DEF_ROW_HEIGHT  45
+#define DEF_COL_WIDTH   227
+
+//==================================================================
+DlgSize::DlgSize(Window * pParent, sal_Int32 nVal, sal_Bool bRow)
+        :ModalDialog( pParent, ModuleRes(bRow ? DLG_ROWHEIGHT : DLG_COLWIDTH))
+        ,aFT_VALUE(this,    ResId( FT_VALUE))
+        ,aMF_VALUE(this,    ResId( MF_VALUE))
+        ,aCB_STANDARD(this, ResId(CB_STANDARD))
+        ,aPB_OK(this,       ResId(PB_OK))
+        ,aPB_CANCEL(this,   ResId(PB_CANCEL))
+        ,aPB_HELP(this,     ResId(PB_HELP))
+        ,m_nPrevValue(nVal)
+        ,m_nStandard(bRow ? DEF_ROW_HEIGHT : DEF_COL_WIDTH)
+{
+    aCB_STANDARD.SetClickHdl(LINK(this,DlgSize,CbClickHdl));
+
+    aMF_VALUE.EnableEmptyFieldValue(sal_True);
+    sal_Bool bDefault = -1 == nVal;
+    aCB_STANDARD.Check(bDefault);
+    if (bDefault)
+    {
+        SetValue(m_nStandard);
+        m_nPrevValue = m_nStandard;
+    }
+    LINK(this,DlgSize,CbClickHdl).Call(&aCB_STANDARD);
+
+    FreeResource();
+}
+
+//------------------------------------------------------------------------------
+DlgSize::~DlgSize()
+{
+}
+
+//------------------------------------------------------------------------------
+void DlgSize::SetValue( sal_Int32 nVal )
+{
+    aMF_VALUE.SetValue(nVal, FUNIT_CM );
+}
+
+//------------------------------------------------------------------------------
+sal_Int32 DlgSize::GetValue()
+{
+    if (aCB_STANDARD.IsChecked())
+        return -1;
+    return (sal_Int32)aMF_VALUE.GetValue( FUNIT_CM );
+}
+
+//------------------------------------------------------------------------------
+IMPL_LINK( DlgSize, CbClickHdl, Button *, pButton )
+{
+
+    if( pButton == &aCB_STANDARD )
+    {
+        aMF_VALUE.Enable(!aCB_STANDARD.IsChecked());
+        if (aCB_STANDARD.IsChecked())
+        {
+            m_nPrevValue = aMF_VALUE.GetValue(FUNIT_CM);
+                // don't use getValue as this will use aCB_STANDARD.to determine if we're standard
+            aMF_VALUE.SetEmptyFieldValue();
+        }
+        else
+        {
+            SetValue( m_nPrevValue );
+        }
+    }
+    return 0;
+}
+// -----------------------------------------------------------------------------
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
+
+
