f6a176dc6af13e85e76f1ce3ec910df54d0caa12
Juergen Schmidt <jsc@openoffice.org>
2000-12-13 14:53:51 +0000

    #81235# new script which change hrefs in docu files to relativ links

---
+++ solenv/bin/convertlinks.pl
@@ -0,0 +1,52 @@
+#
+# convertlinks - a perl script to make hrefs to
+# http://api.openoffice.org/common/ref relativ.
+#
+# Copyright (c) 2000 Sun Microsystems, Inc.
+#
+
+use File::Find;
+
+# for the convenience of &wanted calls, including -eval statements:
+use vars qw/*name *dir/;
+*name   = *File::Find::name;
+*dir    = *File::Find::dir;
+@files = ();
+
+find(\&wanted, "$ARGV[0]");
+
+$return = 1;
+foreach $i (@files)
+{
+    open ( FILEIN, $i->{filename} ) || die "could not open $i->{filename} for reading";
+
+    $relPath = ".";
+    if( $i->{directory} =~ /.*www((\/|\\)(.*))/ )
+    {
+        $relPath = $3;
+        $relPath =~ s#\w+#\.\.#go
+    }
+
+    @lines = <FILEIN>;
+    close( FILEIN );
+    open( FILEOUT, ">$i->{filename}.tmp" ) || die "could not open $i->{filename} for writing";
+    foreach $_ (@lines)
+    {
+        s#((http:\/\/api\.openoffice\.org\/)(common\/ref[^\"]+))#$relPath\/$3#go;
+        print FILEOUT $_;
+    }
+    close FILEOUT;
+    rename "$i->{filename}.tmp", $i->{filename} || die "could not rename $i->{filename}.tmp to $i->{filename}";
+    $return = 0;
+}
+
+exit $return;
+
+
+sub wanted {
+    %file = (
+        directory => $dir,
+        filename  => $name
+    );
+    push @files, {%file} if /^.*\.html\z/s;
+}
