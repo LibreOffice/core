4ad1ccb1caee8d43518ae32c07f03e960442c14d
Dieter Loeschky <dl@openoffice.org>
2000-12-14 08:02:09 +0000

    #77153# GetIdBySubId(): consider SID_TEXTEDIT

--- sd/source/ui/view/drviewsc.cxx
+++ sd/source/ui/view/drviewsc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drviewsc.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ka $ $Date: 2000-11-24 18:54:34 $
+ *  last change: $Author: dl $ $Date: 2000-12-14 09:02:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -758,6 +758,7 @@ USHORT SdDrawViewShell::GetIdBySubId( USHORT nSId )
         case SID_ATTR_CHAR:
         case SID_TEXT_FITTOSIZE:
         case SID_DRAW_CAPTION:
+        case SID_TEXTEDIT:
         {
             nMappedSId = SID_DRAWTBX_TEXT;
         }
