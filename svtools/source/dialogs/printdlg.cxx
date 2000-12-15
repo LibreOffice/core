836f01669e8695f1b41a56d5ff66fa13035dd955
Philipp Lohmann <pl@openoffice.org>
2000-12-15 12:02:32 +0000

    #78578# print to file on serverside UCB

--- svtools/source/dialogs/printdlg.cxx
+++ svtools/source/dialogs/printdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: printdlg.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pl $ $Date: 2000-10-19 11:09:59 $
+ *  last change: $Author: pl $ $Date: 2000-12-15 13:02:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -366,8 +366,15 @@ IMPL_LINK( PrintDialog, ImplBrowseHdl, void*, EMPTYARG )
 
         if ( nRet == RET_OK )
         {
-            INetURLObject aObj( pVclFileDlg->GetPath() );
-            maFiPrintFile.SetText( aObj.PathToFileName() );
+            if( Application::IsRemoteServer() )
+            {
+                maFiPrintFile.SetText( pVclFileDlg->GetPath() );
+            }
+            else
+            {
+                INetURLObject aObj( pVclFileDlg->GetPath() );
+                maFiPrintFile.SetText( aObj.PathToFileName() );
+            }
             ImplCheckOK();
         }
 
