fecdaf8a56f0240888e5f1651f8382af02d942a7
Mathias Bauer <mba@openoffice.org>
2000-12-15 14:50:57 +0000

    #81910#: wrong usage of SaveWorkingSet

--- sfx2/source/appl/app.cxx
+++ sfx2/source/appl/app.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: app.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: mba $ $Date: 2000-12-13 09:30:25 $
+ *  last change: $Author: mba $ $Date: 2000-12-15 15:50:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1349,10 +1349,7 @@ sal_uInt16 SfxApplication::Exception( sal_uInt16 nError )
 
     // save all modified documents and close all documents
     // Do it only, if it's allowed! Ask configuration for right flag.
-    if(
-        ( Application::IsInExecute()                                    ) &&
-        ( SvtSaveOptions().IsSaveWorkingSet() == sal_True   )
-    )
+    if( Application::IsInExecute() )
     {
         SfxObjectShell *pIter, *pNext;
         sal_uInt16 n = 0;
