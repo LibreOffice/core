a6638865cef7434dc96290e865e1f432aaf90596
Thomas Lange <tl@openoffice.org>
2000-12-22 11:41:23 +0000

    lngmisc.cxx added

--- svtools/source/misc/makefile.mk
+++ svtools/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: jp $ $Date: 2000-09-27 09:38:32 $
+#   last change: $Author: tl $ $Date: 2000-12-22 12:41:23 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -131,6 +131,7 @@ LIB3OBJFILES=\
     $(SLO)$/imap3.obj \
     $(SLO)$/iniman.obj \
     $(SLO)$/iniprop.obj \
+    $(SLO)$/lngmisc.obj \
     $(SLO)$/ownlist.obj \
     $(SLO)$/pver.obj \
     $(SLO)$/urihelper.obj \
