51d52a7b505bd6d34be9d3b97d5c77268c8abc26
Thomas Lange <tl@openoffice.org>
2000-12-22 11:45:48 +0000

    linguistic functionality for handling words with hyphens and control chars

---
+++ svtools/inc/lngmisc.hxx
@@ -0,0 +1,110 @@
+/*************************************************************************
+ *
+ *  $RCSfile: lngmisc.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: tl $ $Date: 2000-12-22 12:40:40 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SVTOOLS_LNGMISC_HXX_
+#define _SVTOOLS_LNGMISC_HXX_
+
+#ifndef _SOLAR_H
+#include <tools/solar.h>
+#endif
+#ifndef _SAL_TYPES_H_
+#include <sal/types.h>
+#endif
+
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+///////////////////////////////////////////////////////////////////////////
+
+#define SVT_SOFT_HYPHEN ((sal_Unicode) 0x00AD)
+#define SVT_HARD_HYPHEN ((sal_Unicode) 0x2011)
+
+namespace linguistic
+{
+
+inline BOOL IsHyphen( sal_Unicode cChar )
+{
+    return cChar == SVT_SOFT_HYPHEN  ||  cChar == SVT_HARD_HYPHEN;
+}
+
+
+inline BOOL IsControlChar( sal_Unicode cChar )
+{
+    return cChar < (sal_Unicode) ' ';
+}
+
+
+inline BOOL HasHyphens( const rtl::OUString &rTxt )
+{
+    return  rTxt.indexOf( SVT_SOFT_HYPHEN ) != -1  ||
+            rTxt.indexOf( SVT_HARD_HYPHEN ) != -1;
+}
+
+
+BOOL    HasHyphens( const rtl::OUString &rTxt );
+INT32   GetNumControlChars( const rtl::OUString &rTxt );
+BOOL    RemoveHyphens( rtl::OUString &rTxt );
+BOOL    RemoveControlChars( rtl::OUString &rTxt );
+
+} // namespace linguistic
+
+#endif
