ed905b8df08703407ab59ddf5f647ad7712a84d5
Daniel Boelzle <dbo@openoffice.org>
2001-01-08 15:56:03 +0000

    now compiling not only for C50I

--- bridges/source/cpp_uno/cc50_solaris_intel/makefile.mk
+++ bridges/source/cpp_uno/cc50_solaris_intel/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 15:28:48 $
+#   last change: $Author: dbo $ $Date: 2001-01-08 16:56:03 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -76,7 +76,7 @@ NO_BSYMBOLIC=TRUE
 
 # --- Files --------------------------------------------------------
 
-.IF "$(COM)$(CPU)" == "C50I"
+#.IF "$(COM)$(CPU)" == "C50I"
 
 CFLAGS += -O5 -xO5
 
@@ -101,7 +101,7 @@ SHL1STDLIBS= \
     $(CPPULIB)	\
     $(SALLIB)
 
-.ENDIF
+#.ENDIF
 
 .INCLUDE :	target.mk
 
