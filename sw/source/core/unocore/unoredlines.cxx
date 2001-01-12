adf8cff06c7b636d0105b72dc529acc163cf9f62
Oliver Specht <os@openoffice.org>
2001-01-12 15:25:01 +0000

    Redline container

---
+++ sw/source/core/unocore/unoredlines.cxx
@@ -0,0 +1,294 @@
+/*************************************************************************
+ *
+ *  $RCSfile: unoredlines.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: os $ $Date: 2001-01-12 16:25:01 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifdef PRECOMPILED
+#include "core_pch.hxx"
+#endif
+
+#pragma hdrstop
+
+#ifndef _VOS_MUTEX_HXX_ //autogen
+#include <vos/mutex.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX //autogen
+#include <vcl/svapp.hxx>
+#endif
+#ifndef _UNOREDLINES_HXX
+#include <unoredlines.hxx>
+#endif
+#ifndef _UNOREDLINE_HXX
+#include <unoredline.hxx>
+#endif
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#ifndef _PAGEDESC_HXX //autogen
+#include <pagedesc.hxx>
+#endif
+#include "poolfmt.hxx"
+#ifndef _DOC_HXX
+#include <doc.hxx>
+#endif
+#ifndef _DOCARY_HXX
+#include <docary.hxx>
+#endif
+#ifndef _REDLINE_HXX
+#include <redline.hxx>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::text;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::beans;
+using namespace ::rtl;
+
+/*-- 11.01.01 15:28:54---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SwXRedlines::SwXRedlines(SwDoc* pDoc) :
+    SwUnoCollection(pDoc)
+{
+}
+/*-- 11.01.01 15:28:55---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SwXRedlines::~SwXRedlines()
+{
+}
+/*-- 11.01.01 15:28:55---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+sal_Int32 SwXRedlines::getCount(  ) throw(RuntimeException)
+{
+    vos::OGuard aGuard(Application::GetSolarMutex());
+    if(!IsValid())
+        throw uno::RuntimeException();
+    const SwRedlineTbl& rRedTbl = GetDoc()->GetRedlineTbl();
+    return rRedTbl.Count();
+}
+/*-- 11.01.01 15:28:55---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Any SwXRedlines::getByIndex(sal_Int32 nIndex)
+    throw( IndexOutOfBoundsException, WrappedTargetException, RuntimeException )
+{
+    vos::OGuard aGuard(Application::GetSolarMutex());
+    if(!IsValid())
+        throw uno::RuntimeException();
+    const SwRedlineTbl& rRedTbl = GetDoc()->GetRedlineTbl();
+    Any aRet;
+    if(rRedTbl.Count() > nIndex && nIndex >= 0)
+    {
+        Reference <XPropertySet> xRet = SwXRedlines::GetObject( *rRedTbl.GetObject((USHORT)nIndex), *GetDoc() );
+        aRet <<= xRet;
+    }
+    else
+        throw IndexOutOfBoundsException();
+    return aRet;
+}
+/*-- 11.01.01 15:28:55---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Reference< XEnumeration >  SwXRedlines::createEnumeration(void)
+    throw( RuntimeException )
+{
+    vos::OGuard aGuard(Application::GetSolarMutex());
+    if(!IsValid())
+        throw uno::RuntimeException();
+    const SwRedlineTbl& rRedTbl = GetDoc()->GetRedlineTbl();
+    return Reference< XEnumeration >(new SwXRedlineEnumeration(*GetDoc()));
+}
+/*-- 11.01.01 15:28:55---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Type SwXRedlines::getElementType(  ) throw(RuntimeException)
+{
+    return ::getCppuType((Reference<XPropertySet>*)0);
+}
+/*-- 11.01.01 15:28:56---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+sal_Bool SwXRedlines::hasElements(  ) throw(RuntimeException)
+{
+    vos::OGuard aGuard(Application::GetSolarMutex());
+    if(!IsValid())
+        throw uno::RuntimeException();
+    const SwRedlineTbl& rRedTbl = GetDoc()->GetRedlineTbl();
+    return rRedTbl.Count() > 0;
+}
+/*-- 11.01.01 15:28:56---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+OUString SwXRedlines::getImplementationName(void) throw( RuntimeException )
+{
+    return C2U("SwXRedlines");
+}
+/*-- 11.01.01 15:28:56---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+BOOL SwXRedlines::supportsService(const rtl::OUString& ServiceName)
+    throw( RuntimeException )
+{
+    DBG_ERROR("not implemented")
+    return FALSE;
+}
+/*-- 11.01.01 15:28:57---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Sequence< OUString > SwXRedlines::getSupportedServiceNames(void)
+    throw( RuntimeException )
+{
+    DBG_ERROR("not implemented")
+    return Sequence< OUString >();
+}
+/*-- 11.01.01 15:28:57---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+XPropertySet*   SwXRedlines::GetObject( SwRedline& rRedline, SwDoc& rDoc )
+{
+    SwPageDesc* pStdDesc = rDoc.GetPageDescFromPool(RES_POOLPAGE_STANDARD);
+    SwClientIter aIter(*pStdDesc);
+    SwXRedline* pxRedline = (SwXRedline*)aIter.First( TYPE( SwXRedline ));
+    while(pxRedline)
+    {
+        if(pxRedline->GetRedline() == &rRedline)
+            break;
+        pxRedline = (SwXRedline*)aIter.Next();
+    }
+    if( !pxRedline )
+        pxRedline = new SwXRedline(rRedline, rDoc);
+    return pxRedline;
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SwXRedlineEnumeration::SwXRedlineEnumeration(SwDoc& rDoc) :
+    pDoc(&rDoc),
+    nCurrentIndex(0)
+{
+    pDoc->GetPageDescFromPool(RES_POOLPAGE_STANDARD)->Add(this);
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SwXRedlineEnumeration::~SwXRedlineEnumeration()
+{
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+BOOL SwXRedlineEnumeration::hasMoreElements(void) throw( RuntimeException )
+{
+    if(!pDoc)
+        throw RuntimeException();
+    return pDoc->GetRedlineTbl().Count() > nCurrentIndex;
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Any SwXRedlineEnumeration::nextElement(void)
+    throw( NoSuchElementException, WrappedTargetException, RuntimeException )
+{
+    if(!pDoc)
+        throw RuntimeException();
+    const SwRedlineTbl& rRedTbl = pDoc->GetRedlineTbl();
+    if(!(rRedTbl.Count() > nCurrentIndex))
+        throw NoSuchElementException();
+    Reference <XPropertySet> xRet = SwXRedlines::GetObject( *rRedTbl.GetObject(nCurrentIndex++), *pDoc );
+    Any aRet;
+    aRet <<= xRet;
+    return aRet;
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+rtl::OUString SwXRedlineEnumeration::getImplementationName(void) throw( RuntimeException )
+{
+    return C2U("SwXRedlineEnumeration");
+}
+/*-- 12.01.01 15:06:10---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+BOOL SwXRedlineEnumeration::supportsService(const rtl::OUString& ServiceName) throw( RuntimeException )
+{
+    return FALSE;
+}
+/*-- 12.01.01 15:06:11---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+Sequence< OUString > SwXRedlineEnumeration::getSupportedServiceNames(void) throw( RuntimeException )
+{
+    return Sequence< OUString >();
+}
+/*-- 12.01.01 15:06:11---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+void SwXRedlineEnumeration::Modify( SfxPoolItem *pOld, SfxPoolItem *pNew)
+{
+    ClientModify(this, pOld, pNew);
+    if(!GetRegisteredIn())
+        pDoc = 0;
+}
