a86ebc69d6800b6cafb1568a3314126aa000b4d7
Hans-Joachim Lankenau <hjs@openoffice.org>
2001-01-16 09:29:11 +0000

    syntax

--- cppuhelper/source/shlib.cxx
+++ cppuhelper/source/shlib.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: shlib.cxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: dbo $ $Date: 2000-12-19 15:01:16 $
+ *  last change: $Author: hjs $ $Date: 2001-01-16 10:29:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -321,7 +321,7 @@ Reference< XSingleServiceFactory > SAL_CALL loadSharedLibComponentFactory(
 {
 #ifdef MACOSX
     OUString aComponentName;
-    OUString aModulePath( makeComponentPath( rLibName, rPath, aComponentName ) );
+    OUString aModulePath( makeComponentPath( rLibName, rPath, &aComponentName ) );
 #else
     OUString aModulePath( makeComponentPath( rLibName, rPath ) );
 #endif
@@ -493,7 +493,7 @@ void SAL_CALL writeSharedLibComponentInfo(
 {
 #ifdef MACOSX
     OUString aComponentName;
-    OUString aModulePath( makeComponentPath( rLibName, rPath, aComponentName ) );
+    OUString aModulePath( makeComponentPath( rLibName, rPath, &aComponentName ) );
 #else
     OUString aModulePath( makeComponentPath( rLibName, rPath ) );
 #endif
