76ea92e48c908bc70e65a44a0cd15e4ed0cb20aa
Peter Burow <pb@openoffice.org>
2001-01-18 07:13:02 +0000

    fix: #82076# options of default pathes

---
+++ svtools/source/config/defaultoptions.cxx
@@ -0,0 +1,405 @@
+/*************************************************************************
+ *
+ *  $RCSfile: defaultoptions.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: pb $ $Date: 2001-01-18 08:13:02 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#pragma hdrstop
+
+#include "defaultoptions.hxx"
+#include "pathoptions.hxx"
+
+#ifndef _UTL_CONFIGITEM_HXX_
+#include <unotools/configitem.hxx>
+#endif
+#ifndef _UTL_CONFIGMGR_HXX_
+#include <unotools/configmgr.hxx>
+#endif
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#ifndef _TOOLS_RESMGR_HXX
+#include <tools/resmgr.hxx>
+#endif
+#ifndef _ISOLANG_HXX
+#include <tools/isolang.hxx>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
+#include <com/sun/star/uno/Any.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+#include <vos/process.hxx>
+
+using namespace osl;
+using namespace utl;
+using namespace rtl;
+using namespace com::sun::star::uno;
+
+// define ----------------------------------------------------------------
+
+#define ASCII_STR(s)                OUString( RTL_CONSTASCII_USTRINGPARAM(s) )
+
+#define DEFAULTPATH__ADDIN          0
+#define DEFAULTPATH__AUTOCORRECT    1
+#define DEFAULTPATH__AUTOPILOT      2
+#define DEFAULTPATH__AUTOTEXT       3
+#define DEFAULTPATH__BACKUP         4
+#define DEFAULTPATH__BASIC          5
+#define DEFAULTPATH__BITMAP         6
+#define DEFAULTPATH__CONFIG         7
+#define DEFAULTPATH__DICTIONARY     8
+#define DEFAULTPATH__FAVORITES      9
+#define DEFAULTPATH__FILTER         10
+#define DEFAULTPATH__GALLERY        11
+#define DEFAULTPATH__GRAPHIC        12
+#define DEFAULTPATH__HELP           13
+#define DEFAULTPATH__LINGUISTIC     14
+#define DEFAULTPATH__MODULE         15
+#define DEFAULTPATH__NEWMENU        16
+#define DEFAULTPATH__PALETTE        17
+#define DEFAULTPATH__PLUGIN         18
+#define DEFAULTPATH__STORAGE        19
+#define DEFAULTPATH__TEMPLATE       20
+#define DEFAULTPATH__USERCONFIG     21
+#define DEFAULTPATH__USERDICTIONARY 22
+#define DEFAULTPATH__WORK           23
+
+// class SvtDefaultOptions_Impl ------------------------------------------
+
+class SvtDefaultOptions_Impl : public utl::ConfigItem
+{
+public:
+    String          m_aAddinPath;
+    String          m_aAutoCorrectPath;
+    String          m_aAutoPilotPath;
+    String          m_aAutoTextPath;
+    String          m_aBackupPath;
+    String          m_aBasicPath;
+    String          m_aBitmapPath;
+    String          m_aConfigPath;
+    String          m_aDictionaryPath;
+    String          m_aFavoritesPath;
+    String          m_aFilterPath;
+    String          m_aGalleryPath;
+    String          m_aGraphicPath;
+    String          m_aHelpPath;
+    String          m_aLinguisticPath;
+    String          m_aModulePath;
+    String          m_aNewMenuPath;
+    String          m_aPalettePath;
+    String          m_aPluginPath;
+    String          m_aStoragePath;
+    String          m_aTemplatePath;
+    String          m_aUserConfigPath;
+    String          m_aUserDictionaryPath;
+    String          m_aWorkPath;
+
+                    SvtDefaultOptions_Impl();
+
+    String          GetDefaultPath( USHORT nId ) const;
+};
+
+// global ----------------------------------------------------------------
+
+static SvtDefaultOptions_Impl*  pOptions = NULL;
+static sal_Int32                nRefCount = 0;
+static ::osl::Mutex             aMutex;
+
+typedef String SvtDefaultOptions_Impl:: *PathStrPtr;
+
+struct PathToDefaultMapping_Impl
+{
+    SvtPathOptions::Pathes  _ePath;
+    PathStrPtr              _pDefaultPath;
+};
+
+static PathToDefaultMapping_Impl __READONLY_DATA PathMap_Impl[] =
+{
+    SvtPathOptions::PATH_ADDIN,         &SvtDefaultOptions_Impl::m_aAddinPath,
+    SvtPathOptions::PATH_AUTOCORRECT,   &SvtDefaultOptions_Impl::m_aAutoCorrectPath,
+    SvtPathOptions::PATH_AUTOPILOT,     &SvtDefaultOptions_Impl::m_aAutoPilotPath,
+    SvtPathOptions::PATH_AUTOTEXT,      &SvtDefaultOptions_Impl::m_aAutoTextPath,
+    SvtPathOptions::PATH_BACKUP,        &SvtDefaultOptions_Impl::m_aBackupPath,
+    SvtPathOptions::PATH_BASIC,         &SvtDefaultOptions_Impl::m_aBasicPath,
+    SvtPathOptions::PATH_BITMAP,        &SvtDefaultOptions_Impl::m_aBitmapPath,
+    SvtPathOptions::PATH_CONFIG,        &SvtDefaultOptions_Impl::m_aConfigPath,
+#if SUPD<615
+    SvtPathOptions::PATH_DATABASE,      NULL,
+#endif
+    SvtPathOptions::PATH_DICTIONARY,    &SvtDefaultOptions_Impl::m_aDictionaryPath,
+    SvtPathOptions::PATH_FAVORITES,     &SvtDefaultOptions_Impl::m_aFavoritesPath,
+    SvtPathOptions::PATH_FILTER,        &SvtDefaultOptions_Impl::m_aFilterPath,
+    SvtPathOptions::PATH_GALLERY,       &SvtDefaultOptions_Impl::m_aGalleryPath,
+    SvtPathOptions::PATH_GRAPHIC,       &SvtDefaultOptions_Impl::m_aGraphicPath,
+    SvtPathOptions::PATH_HELP,          &SvtDefaultOptions_Impl::m_aHelpPath,
+    SvtPathOptions::PATH_LINGUISTIC,    &SvtDefaultOptions_Impl::m_aLinguisticPath,
+    SvtPathOptions::PATH_MODULE,        &SvtDefaultOptions_Impl::m_aModulePath,
+    SvtPathOptions::PATH_NEWMENU,       &SvtDefaultOptions_Impl::m_aNewMenuPath,
+    SvtPathOptions::PATH_PALETTE,       &SvtDefaultOptions_Impl::m_aPalettePath,
+    SvtPathOptions::PATH_PLUGIN,        &SvtDefaultOptions_Impl::m_aPluginPath,
+    SvtPathOptions::PATH_STORAGE,       &SvtDefaultOptions_Impl::m_aStoragePath,
+    SvtPathOptions::PATH_TEMP,          NULL,
+    SvtPathOptions::PATH_TEMPLATE,      &SvtDefaultOptions_Impl::m_aTemplatePath,
+#if SUPD<615
+    SvtPathOptions::PATH_TRASH,         NULL,
+#endif
+    SvtPathOptions::PATH_USERCONFIG,    &SvtDefaultOptions_Impl::m_aUserConfigPath,
+    SvtPathOptions::PATH_USERDICTIONARY,&SvtDefaultOptions_Impl::m_aUserDictionaryPath,
+    SvtPathOptions::PATH_WORK,          &SvtDefaultOptions_Impl::m_aWorkPath
+};
+
+// functions -------------------------------------------------------------
+
+Sequence< OUString > GetDefaultPropertyNames()
+{
+    static const char* aPropNames[] =
+    {
+        "Addin",            // PATH_ADDIN
+        "AutoCorrect",      // PATH_AUTOCORRECT
+        "AutoPilot",        // PATH_AUTOPILOT
+        "AutoText",         // PATH_AUTOTEXT
+        "Backup",           // PATH_BACKUP
+        "Basic",            // PATH_BASIC
+        "Bitmap",           // PATH_BITMAP
+        "Config",           // PATH_CONFIG
+        "Dictionary",       // PATH_DICTIONARY
+        "Favorite",         // PATH_FAVORITES
+        "Filter",           // PATH_FILTER
+        "Gallery",          // PATH_GALLERY
+        "Graphic",          // PATH_GRAPHIC
+        "Help",             // PATH_HELP
+        "Linguistic",       // PATH_LINGUISTIC
+        "Module",           // PATH_MODULE
+        "New",              // PATH_NEWMENU
+        "Palette",          // PATH_PALETTE
+        "Plugin",           // PATH_PLUGIN
+        "Storage",          // PATH_STORAGE
+        "Template",         // PATH_TEMPLATE
+        "UserConfig",       // PATH_USERCONFIG
+        "UserDictionary",   // PATH_USERDICTIONARY
+        "Work"              // PATH_WORK
+    };
+
+    const int nCount = sizeof( aPropNames ) / sizeof( const char* );
+    Sequence< OUString > aNames( nCount );
+    OUString* pNames = aNames.getArray();
+    for ( int i = 0; i < nCount; i++ )
+        pNames[i] = OUString::createFromAscii( aPropNames[i] );
+
+    return aNames;
+}
+
+// class SvtDefaultOptions_Impl ------------------------------------------
+
+String SvtDefaultOptions_Impl::GetDefaultPath( USHORT nId ) const
+{
+    String aRet;
+    USHORT nIdx = 0;
+
+    while ( PathMap_Impl[nIdx]._ePath <= SvtPathOptions::PATH_WORK )
+    {
+        if ( nId == PathMap_Impl[nIdx]._ePath && PathMap_Impl[nIdx]._pDefaultPath )
+        {
+            aRet = this->*(PathMap_Impl[nIdx]._pDefaultPath);
+            break;
+        }
+        ++nIdx;
+    }
+
+    return aRet;
+}
+
+// -----------------------------------------------------------------------
+
+SvtDefaultOptions_Impl::SvtDefaultOptions_Impl() : ConfigItem( ASCII_STR("Office.Common/Path/Default") )
+{
+    ConfigManager* pCfgMgr = ConfigManager::GetConfigManager();
+    Sequence< OUString > aNames = GetDefaultPropertyNames();
+    Sequence< Any > aValues = GetProperties( aNames );
+    EnableNotification( aNames );
+    const Any* pValues = aValues.getConstArray();
+    DBG_ASSERT( aValues.getLength() == aNames.getLength(), "GetProperties failed" );
+    if ( aValues.getLength() == aNames.getLength() )
+    {
+        SvtPathOptions aPathOpt;
+        OUString aTempStr, aFullPath;
+
+        for ( int nProp = 0; nProp < aNames.getLength(); nProp++ )
+        {
+            if ( pValues[nProp].hasValue() )
+            {
+                switch ( pValues[nProp].getValueTypeClass() )
+                {
+                    case ::com::sun::star::uno::TypeClass_STRING :
+                    {
+                        // multi pathes
+                        if ( pValues[nProp] >>= aTempStr )
+                            aFullPath = aPathOpt.SubstituteVariable( aTempStr );
+                        else
+                        {
+                            DBG_ERRORFILE( "any operator >>= failed" );
+                        }
+                        break;
+                    }
+
+                    case ::com::sun::star::uno::TypeClass_SEQUENCE :
+                    {
+                        // single pathes
+                        aFullPath = OUString();
+                        Sequence < OUString > aList;
+                        if ( pValues[nProp] >>= aList )
+                        {
+                            sal_Int32 nCount = aList.getLength();
+                            for ( sal_Int32 nPosition = 0; nPosition < nCount; ++nPosition )
+                            {
+                                aTempStr = aPathOpt.SubstituteVariable( aList[ nPosition ] );
+                                aFullPath += aTempStr;
+                                if ( nPosition < nCount-1 )
+                                    aFullPath += OUString( RTL_CONSTASCII_USTRINGPARAM(";") );
+                            }
+                        }
+                        else
+                        {
+                            DBG_ERRORFILE( "any operator >>= failed" );
+                        }
+                        break;
+                    }
+
+                    default:
+                    {
+                        DBG_ERRORFILE( "Wrong any type" );
+                    }
+                }
+
+                switch ( nProp )
+                {
+                    case DEFAULTPATH__ADDIN:            m_aAddinPath = String( aFullPath );         break;
+                    case DEFAULTPATH__AUTOCORRECT:      m_aAutoCorrectPath = String( aFullPath );   break;
+                    case DEFAULTPATH__AUTOPILOT:        m_aAutoPilotPath = String( aFullPath );     break;
+                    case DEFAULTPATH__AUTOTEXT:         m_aAutoTextPath = String( aFullPath );      break;
+                    case DEFAULTPATH__BACKUP:           m_aBackupPath = String( aFullPath );        break;
+                    case DEFAULTPATH__BASIC:            m_aBasicPath = String( aFullPath );         break;
+                    case DEFAULTPATH__BITMAP:           m_aBitmapPath = String( aFullPath );        break;
+                    case DEFAULTPATH__CONFIG:           m_aConfigPath = String( aFullPath );        break;
+                    case DEFAULTPATH__DICTIONARY:       m_aDictionaryPath = String( aFullPath );    break;
+                    case DEFAULTPATH__FAVORITES:        m_aFavoritesPath = String( aFullPath );     break;
+                    case DEFAULTPATH__FILTER:           m_aFilterPath = String( aFullPath );        break;
+                    case DEFAULTPATH__GALLERY:          m_aGalleryPath = String( aFullPath );       break;
+                    case DEFAULTPATH__GRAPHIC:          m_aGraphicPath = String( aFullPath );       break;
+                    case DEFAULTPATH__HELP:             m_aHelpPath = String( aFullPath );          break;
+                    case DEFAULTPATH__LINGUISTIC:       m_aLinguisticPath = String( aFullPath );    break;
+                    case DEFAULTPATH__MODULE:           m_aModulePath = String( aFullPath );        break;
+                    case DEFAULTPATH__NEWMENU:          m_aNewMenuPath = String( aFullPath );       break;
+                    case DEFAULTPATH__PALETTE:          m_aPalettePath = String( aFullPath );       break;
+                    case DEFAULTPATH__PLUGIN:           m_aPluginPath = String( aFullPath );        break;
+                    case DEFAULTPATH__STORAGE:          m_aStoragePath = String( aFullPath );       break;
+                    case DEFAULTPATH__TEMPLATE:         m_aTemplatePath = String( aFullPath );      break;
+                    case DEFAULTPATH__USERCONFIG:       m_aUserConfigPath = String( aFullPath );    break;
+                    case DEFAULTPATH__USERDICTIONARY:   m_aUserDictionaryPath = String( aFullPath );break;
+                    case DEFAULTPATH__WORK:             m_aWorkPath = String( aFullPath );          break;
+
+                    default:
+                        DBG_ERRORFILE( "invalid index to load a default path" );
+                }
+            }
+        }
+    }
+}
+
+// class SvtDefaultOptions -----------------------------------------------
+
+SvtDefaultOptions::SvtDefaultOptions()
+{
+    // Global access, must be guarded (multithreading)
+    ::osl::MutexGuard aGuard( aMutex );
+    if ( !pOptions )
+        pOptions = new SvtDefaultOptions_Impl;
+    ++nRefCount;
+    pImp = pOptions;
+}
+
+// -----------------------------------------------------------------------
+
+SvtDefaultOptions::~SvtDefaultOptions()
+{
+    // Global access, must be guarded (multithreading)
+    ::osl::MutexGuard aGuard( aMutex );
+    if ( !--nRefCount )
+    {
+        if ( pOptions->IsModified() )
+            pOptions->Commit();
+        DELETEZ( pOptions );
+    }
+}
+
+// -----------------------------------------------------------------------
+
+String SvtDefaultOptions::GetDefaultPath( USHORT nId ) const
+{
+    return pImp->GetDefaultPath( nId );
+}
+
