fea5ee7cf7b53bb70ea564b28f0db27364bbb6e8
Daniel Boelzle <dbo@openoffice.org>
2001-01-19 09:42:18 +0000

    #77512# found bug, there shared libs are more often loaded than needed

--- cppuhelper/source/shlib.cxx
+++ cppuhelper/source/shlib.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: shlib.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hjs $ $Date: 2001-01-16 10:29:11 $
+ *  last change: $Author: dbo $ $Date: 2001-01-19 10:42:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -363,7 +363,7 @@ Reference< XSingleServiceFactory > SAL_CALL loadSharedLibComponentFactory(
         OUString aEnvTypeName( OUString::createFromAscii( pEnvTypeName ) );
 
         sal_Bool bNeedsMapping =
-            (pEnv || aEnvTypeName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM(CPPU_CURRENT_LANGUAGE_BINDING_NAME ) ));
+            (pEnv || !aEnvTypeName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM(CPPU_CURRENT_LANGUAGE_BINDING_NAME ) ));
 
         if (bNeedsMapping)
         {
@@ -535,7 +535,7 @@ void SAL_CALL writeSharedLibComponentInfo(
         OUString aEnvTypeName( OUString::createFromAscii( pEnvTypeName ) );
 
         sal_Bool bNeedsMapping =
-            (pEnv || aEnvTypeName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM(CPPU_CURRENT_LANGUAGE_BINDING_NAME ) ));
+            (pEnv || !aEnvTypeName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM(CPPU_CURRENT_LANGUAGE_BINDING_NAME ) ));
 
         if (bNeedsMapping)
         {
