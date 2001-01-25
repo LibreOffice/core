881f4eed41b737afc0ea3aad4bf377c2891679fc
Sven Jacobi <sj@openoffice.org>
2001-01-25 14:42:39 +0000

    #83082# added catch: CommandCreationException

--- svtools/source/filter.vcl/filter/filter.cxx
+++ svtools/source/filter.vcl/filter/filter.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: filter.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: sj $ $Date: 2000-12-19 13:37:59 $
+ *  last change: $Author: sj $ $Date: 2001-01-25 15:42:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -248,6 +248,10 @@ BOOL ImplDirEntryHelper::Exists( const INetURLObject& rObj )
     catch( ::com::sun::star::ucb::CommandAbortedException& )
     {
         DBG_ERRORFILE( "CommandAbortedException" );
+    }
+    catch( ::com::sun::star::ucb::CommandCreationException& )
+    {
+        DBG_ERRORFILE( "CommandCreationException" );
     }
     catch( ... )
     {
