7471d8360e53679a9a6e28772bdd1a7d48e63d1b
Gene Anaya <ganaya@openoffice.org>
2001-01-30 15:48:38 +0000

    Added staticmbfile.cxx for Mac OS X

--- connectivity/source/drivers/file/makefile.mk
+++ connectivity/source/drivers/file/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: fs $ $Date: 2000-10-05 08:39:40 $
+#   last change: $Author: ganaya $ $Date: 2001-01-30 16:48:38 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -153,6 +153,12 @@ SLOFILES=\
     $(SLO)$/fanalyzer.obj			\
     $(SLO)$/fcode.obj				\
     $(SLO)$/fcomp.obj
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SLOFILES+=$(SLO)$/staticmbfile.obj
+.ENDIF
 
 # --- Targets ----------------------------------
 
