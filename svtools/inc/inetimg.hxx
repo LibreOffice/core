e851cb29e8fa817cd4b7b096404acdc64a567d88
jp <jp@openoffice.org>
2001-01-30 07:39:50 +0000

    moved from the source\urlobj directory to here

---
+++ svtools/inc/inetimg.hxx
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  $RCSfile: inetimg.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: jp $ $Date: 2001-01-30 08:39:50 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _INETIMG_HXX
+#define _INETIMG_HXX
+
+#ifndef _STRING_HXX //autogen
+#include <tools/string.hxx>
+#endif
+#ifndef _SV_GEN_HXX
+#include <tools/gen.hxx>
+#endif
+
+class SvData;
+class SotDataObject;
+class SotDataMemberObject;
+
+//=========================================================================
+
+class INetImage
+{
+    String          aImageURL;
+    String          aTargetURL;
+    String          aTargetFrame;
+    String          aAlternateText;
+    Size            aSizePixel;
+
+protected:
+    String          CopyExchange() const;
+    void            PasteExchange( String aString );
+
+    void            SetImageURL( const String& rS )     { aImageURL = rS; }
+    void            SetTargetURL( const String& rS )    { aTargetURL = rS; }
+    void            SetTargetFrame( const String& rS )  { aTargetFrame = rS; }
+    void            SetAlternateText( const String& rS ){ aAlternateText = rS; }
+    void            SetSizePixel( const Size& rSize )   { aSizePixel = rSize; }
+
+public:
+                    INetImage(
+                        const String& rImageURL,
+                        const String& rTargetURL,
+                        const String& rTargetFrame,
+                        const String& rAlternateText,
+                        const Size& rSizePixel )
+                    :   aImageURL( rImageURL ),
+                        aTargetURL( rTargetURL ),
+                        aTargetFrame( rTargetFrame ),
+                        aAlternateText( rAlternateText ),
+                        aSizePixel( rSizePixel )
+                    {}
+                    INetImage()
+                    {}
+
+    const String&   GetImageURL() const { return aImageURL; }
+    const String&   GetTargetURL() const { return aTargetURL; }
+    const String&   GetTargetFrame() const { return aTargetFrame; }
+    const String&   GetAlternateText() const { return aAlternateText; }
+    const Size&     GetSizePixel() const { return aSizePixel; }
+
+    static ULONG    HasFormat( SotDataObject& rObj );
+    BOOL            SetData( SvData& rData ) const;
+    BOOL            Copy( SotDataMemberObject& rObj ) const;
+    BOOL            CopyFormats( SotDataObject& rObj ) const;
+    BOOL            Paste( SotDataObject& rObj, ULONG nFormat );
+};
+
+#endif // #ifndef _INETIMG_HXX
+
+
