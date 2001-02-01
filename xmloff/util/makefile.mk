5df7c5c1273777f1ad0af407f12fff8a93dee1e0
Gene Anaya <ganaya@openoffice.org>
2001-02-01 23:04:17 +0000

    Added library to link line on MacOSX to resolve static member functions

--- xmloff/util/makefile.mk
+++ xmloff/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: cl $ $Date: 2001-01-12 16:08:04 $
+#   last change: $Author: ganaya $ $Date: 2001-02-02 00:04:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -112,6 +112,12 @@ SHL1STDLIBS= \
         $(ONELIB)		\
         $(GOODIESLIB)
 
+# NETBSD: somewhere we have to instantiate the static data members.
+# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
+# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
+.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+SHL1STDLIBS+=-licg617mxp
+.ENDIF
 
 SHL1DEF=    $(MISC)$/$(SHL1TARGET).def
 SHL1LIBS=   $(LIB1TARGET)
