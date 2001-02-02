4e993542b805821b6dd25a78b73a96ecfe7d3707
Thomas Lange <tl@openoffice.org>
2001-02-02 10:16:51 +0000

    definition of property names and handles moved to svtools/linguprops.hxx. List of which-ID's changed to corresponding handles

--- linguistic/inc/lngprops.hxx
+++ linguistic/inc/lngprops.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: lngprops.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tl $ $Date: 2000-11-22 15:54:32 $
+ *  last change: $Author: tl $ $Date: 2001-02-02 11:16:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,84 +62,37 @@
 #ifndef _LINGUISTIC_LNGPROPS_HHX_
 #define _LINGUISTIC_LNGPROPS_HHX_
 
-
-#define UPN_IS_GERMAN_PRE_REFORM            "IsGermanPreReform"
-#define UPN_IS_USE_DICTIONARY_LIST          "IsUseDictionaryList"
-#define UPN_IS_IGNORE_CONTROL_CHARACTERS    "IsIgnoreControlCharacters"
-
-// UNO property names for SpellChecker
-#define UPN_IS_SPELL_UPPER_CASE             "IsSpellUpperCase"
-#define UPN_IS_SPELL_WITH_DIGITS            "IsSpellWithDigits"
-#define UPN_IS_SPELL_CAPITALIZATION         "IsSpellCapitalization"
-
-// UNO property names for Hyphenator
-#define UPN_HYPH_MIN_LEADING                "HyphMinLeading"
-#define UPN_HYPH_MIN_TRAILING               "HyphMinTrailing"
-#define UPN_HYPH_MIN_WORD_LENGTH            "HyphMinWordLength"
-
-// UNO property names for OtherLingu (foreign Linguistik)
-#define UPN_IS_STANDARD_HYPHENATOR          "IsStandardHyphenator"
-#define UPN_IS_STANDARD_SPELL_CHECKER       "IsStandardSpellChecker"
-#define UPN_IS_STANDARD_THESAURUS           "IsStandardThesaurus"
-#define UPN_OTHER_LINGU_INDEX               "OtherLinguIndex"
-
-// UNO property names for Lingu
-// (those not covered by the SpellChecker, Hyphenator and OtherLingu properties
-// and more likely used in other modules only)
-#define UPN_DEFAULT_LANGUAGE                "DefaultLanguage"
-#define UPN_DEFAULT_LOCALE                  "DefaultLocale"
-#define UPN_DEFAULT_LOCALE_CJK              "DefaultLocale_CJK"
-#define UPN_DEFAULT_LOCALE_CTL              "DefaultLocale_CTL"
-#define UPN_IS_HYPH_AUTO                    "IsHyphAuto"
-#define UPN_IS_HYPH_SPECIAL                 "IsHyphSpecial"
-#define UPN_IS_SPELL_AUTO                   "IsSpellAuto"
-#define UPN_IS_SPELL_HIDE                   "IsSpellHide"
-#define UPN_IS_SPELL_IN_ALL_LANGUAGES       "IsSpellInAllLanguages"
-#define UPN_IS_SPELL_SPECIAL                "IsSpellSpecial"
-#define UPN_IS_WRAP_REVERSE                 "IsWrapReverse"
+#ifndef _SVTOOLS_LINGUPROPS_HXX_
+#include <svtools/linguprops.hxx>
+#endif
 
 // WIDs for property names
 //!! Don't change values! They are used as the property handles in
 //!! the service description
-#define WID_IS_GERMAN_PRE_REFORM             0
-#define WID_IS_USE_DICTIONARY_LIST           1
-#define WID_IS_IGNORE_CONTROL_CHARACTERS     2
-#define WID_IS_SPELL_UPPER_CASE              3
-#define WID_IS_SPELL_WITH_DIGITS             4
-#define WID_IS_SPELL_CAPITALIZATION          5
-#define WID_HYPH_MIN_LEADING                 6
-#define WID_HYPH_MIN_TRAILING                7
-#define WID_HYPH_MIN_WORD_LENGTH             8
-#define WID_DEFAULT_LOCALE                   9
-#define WID_IS_SPELL_AUTO                   10
-#define WID_IS_SPELL_HIDE                   11
-#define WID_IS_SPELL_IN_ALL_LANGUAGES       12
-#define WID_IS_SPELL_SPECIAL                13
-#define WID_IS_HYPH_AUTO                    14
-#define WID_IS_HYPH_SPECIAL                 15
-#define WID_IS_WRAP_REVERSE                 16
-#define WID_IS_STANDARD_HYPHENATOR          17
-#define WID_IS_STANDARD_SPELL_CHECKER       18
-#define WID_IS_STANDARD_THESAURUS           19
-#define WID_OTHER_LINGU_INDEX               20
-#define WID_DEFAULT_LANGUAGE                21
-#define WID_DEFAULT_LOCALE_CJK              22
-#define WID_DEFAULT_LOCALE_CTL              23
-
-
-// UNO property handles
-#define UPH_IS_GERMAN_PRE_REFORM            WID_IS_GERMAN_PRE_REFORM
-#define UPH_IS_USE_DICTIONARY_LIST          WID_IS_USE_DICTIONARY_LIST
-#define UPH_IS_IGNORE_CONTROL_CHARACTERS    WID_IS_IGNORE_CONTROL_CHARACTERS
-//
-#define UPH_IS_SPELL_UPPER_CASE             WID_IS_SPELL_UPPER_CASE
-#define UPH_IS_SPELL_WITH_DIGITS            WID_IS_SPELL_WITH_DIGITS
-#define UPH_IS_SPELL_CAPITALIZATION         WID_IS_SPELL_CAPITALIZATION
-//
-#define UPH_HYPH_MIN_LEADING                WID_HYPH_MIN_LEADING
-#define UPH_HYPH_MIN_TRAILING               WID_HYPH_MIN_TRAILING
-#define UPH_HYPH_MIN_WORD_LENGTH            WID_HYPH_MIN_WORD_LENGTH
-
+#define WID_IS_GERMAN_PRE_REFORM            UPH_IS_GERMAN_PRE_REFORM
+#define WID_IS_USE_DICTIONARY_LIST          UPH_IS_USE_DICTIONARY_LIST
+#define WID_IS_IGNORE_CONTROL_CHARACTERS    UPH_IS_IGNORE_CONTROL_CHARACTERS
+#define WID_IS_SPELL_UPPER_CASE             UPH_IS_SPELL_UPPER_CASE
+#define WID_IS_SPELL_WITH_DIGITS            UPH_IS_SPELL_WITH_DIGITS
+#define WID_IS_SPELL_CAPITALIZATION         UPH_IS_SPELL_CAPITALIZATION
+#define WID_HYPH_MIN_LEADING                UPH_HYPH_MIN_LEADING
+#define WID_HYPH_MIN_TRAILING               UPH_HYPH_MIN_TRAILING
+#define WID_HYPH_MIN_WORD_LENGTH            UPH_HYPH_MIN_WORD_LENGTH
+#define WID_DEFAULT_LOCALE                  UPH_DEFAULT_LOCALE
+#define WID_IS_SPELL_AUTO                   UPH_IS_SPELL_AUTO
+#define WID_IS_SPELL_HIDE                   UPH_IS_SPELL_HIDE
+#define WID_IS_SPELL_IN_ALL_LANGUAGES       UPH_IS_SPELL_IN_ALL_LANGUAGES
+#define WID_IS_SPELL_SPECIAL                UPH_IS_SPELL_SPECIAL
+#define WID_IS_HYPH_AUTO                    UPH_IS_HYPH_AUTO
+#define WID_IS_HYPH_SPECIAL                 UPH_IS_HYPH_SPECIAL
+#define WID_IS_WRAP_REVERSE                 UPH_IS_WRAP_REVERSE
+#define WID_IS_STANDARD_HYPHENATOR          UPH_IS_STANDARD_HYPHENATOR
+#define WID_IS_STANDARD_SPELL_CHECKER       UPH_IS_STANDARD_SPELL_CHECKER
+#define WID_IS_STANDARD_THESAURUS           UPH_IS_STANDARD_THESAURUS
+#define WID_OTHER_LINGU_INDEX               UPH_OTHER_LINGU_INDEX
+#define WID_DEFAULT_LANGUAGE                UPH_DEFAULT_LANGUAGE
+#define WID_DEFAULT_LOCALE_CJK              UPH_DEFAULT_LOCALE_CJK
+#define WID_DEFAULT_LOCALE_CTL              UPH_DEFAULT_LOCALE_CTL
 
 #endif
 
