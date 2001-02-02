dbdfbb60ff769775a3133582f25a75c5e9caa332
Sander Vesik <svesik@openoffice.org>
2001-02-02 18:16:28 +0000

    Merge NetBSD changes by Michael Rauch from OO614B to HEAD.

--- cppuhelper/source/makefile.mk
+++ cppuhelper/source/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: dbo $ $Date: 2000-12-19 15:01:51 $
+#   last change: $Author: svesik $ $Date: 2001-02-02 19:01:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -124,10 +124,8 @@ SLOFILES=	\
         $(SLO)$/component.obj		\
         $(SLO)$/shlib.obj
 
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
 # SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+.IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 SLOFILES+=$(SLO)$/staticmb.obj
 #OBJFILES+=$(OBJ)$/staticmb.obj
 .ENDIF
