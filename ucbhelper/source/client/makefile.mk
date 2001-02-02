aae96eba981365488ece3019a1d014a29ba46e57
Sander Vesik <svesik@openoffice.org>
2001-02-02 18:16:28 +0000

    Merge NetBSD changes by Michael Rauch from OO614B to HEAD.

--- ucbhelper/source/client/makefile.mk
+++ ucbhelper/source/client/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: sb $ $Date: 2000-10-18 10:04:31 $
+#   last change: $Author: svesik $ $Date: 2001-02-02 18:58:20 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -86,12 +86,10 @@ SLOFILES=\
 
 .ENDIF
 
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism
-# for GCC won't work.  SCO and MACOSX: the linker does know about weak
+# SCO and MACOSX: the linker does know about weak
 # symbols, but we can't ignore multiple defined symbols
 
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+.IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 SLOFILES+=$(SLO)$/staticmbclient.obj
 .ENDIF
 
--- ucbhelper/source/provider/makefile.mk
+++ ucbhelper/source/provider/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: sb $ $Date: 2000-12-04 17:45:16 $
+#   last change: $Author: svesik $ $Date: 2001-02-02 18:58:20 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -94,12 +94,10 @@ SLOFILES=\
 
 .ENDIF
 
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism 
-# for GCC won't work.  SCO and MACOSX: the linker does know about weak 
+# SCO and MACOSX: the linker does know about weak 
 # symbols, but we can't ignore multiple defined symbols
 
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+.IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 SLOFILES+=$(SLO)$/staticmbprovider.obj
 .ENDIF
 
