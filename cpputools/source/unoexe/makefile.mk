164046680cbd18ba4dc5e4434f933bca77c0aeac
Sander Vesik <svesik@openoffice.org>
2001-02-02 16:34:02 +0000

    Merge NetBSD changes by Michael Rauch from OO614B to HEAD.

--- cpputools/source/unoexe/makefile.mk
+++ cpputools/source/unoexe/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2000-11-13 15:43:24 $
+#   last change: $Author: svesik $ $Date: 2001-02-02 17:34:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -107,10 +107,8 @@ CXXFILES= \
 APP1TARGET=$(TARGET)
 APP1OBJS=$(OBJ)$/unoexe.obj 
 
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
 # SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+.IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 APP1OBJS+=$(OBJ)$/staticmb.obj
 .ENDIF
 
