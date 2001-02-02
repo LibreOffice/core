46a96b719721b97c38654969d6d689c934405f92
Sander Vesik <svesik@openoffice.org>
2001-02-02 18:16:28 +0000

    Merge NetBSD changes by Michael Rauch from OO614B to HEAD.

--- eventattacher/source/makefile.mk
+++ eventattacher/source/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:15:25 $
+#   last change: $Author: svesik $ $Date: 2001-02-02 19:06:24 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,10 +104,8 @@ UNOTYPES= \
 
 SLOFILES= \
         $(SLO)$/eventattacher.obj
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
 # SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
+.IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 SLOFILES+=$(SLO)$/staticmb.obj
 .ENDIF
 
