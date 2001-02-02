04b4687a6a048aa68a010a84011d69023f643be1
Ocke Janssen <oj@openoffice.org>
2001-02-02 14:58:34 +0000

    file removed

--- dbaccess/source/core/misc/makefile.mk
+++ dbaccess/source/core/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: fs $ $Date: 2000-10-18 16:17:22 $
+#   last change: $Author: oj $ $Date: 2001-02-02 15:58:34 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,14 +78,13 @@ NOOPTFILES=$(SLO)$/registryhelper.obj
 .ENDIF
 
 SLOFILES=	\
-        $(SLO)$/confignode.obj	\
-        $(SLO)$/commandbase.obj	\
-        $(SLO)$/seqoutputstream.obj	\
+        $(SLO)$/confignode.obj				\
+        $(SLO)$/commandbase.obj				\
         $(SLO)$/configurationflushable.obj	\
-        $(SLO)$/propertyhelper.obj	\
-        $(SLO)$/registryhelper.obj	\
-        $(SLO)$/services.obj	\
-        $(SLO)$/userinformation.obj	\
+        $(SLO)$/propertyhelper.obj			\
+        $(SLO)$/registryhelper.obj			\
+        $(SLO)$/services.obj				\
+        $(SLO)$/userinformation.obj
 
 # --- Targets ----------------------------------
 
