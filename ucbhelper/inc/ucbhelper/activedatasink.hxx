287395befa9858bf1de95d056cba7acd825b9a1f
Kai Sommerfeld <kso@openoffice.org>
2001-02-05 14:49:26 +0000

    #83045# - Initial revision.

---
+++ ucbhelper/inc/ucbhelper/activedatasink.hxx
@@ -0,0 +1,117 @@
+/*************************************************************************
+ *
+ *  $RCSfile: activedatasink.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: kso $ $Date: 2001-02-05 15:49:26 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _UCBHELPER_ACTIVEDATASINK_HXX
+#define _UCBHELPER_ACTIVEDATASINK_HXX
+
+#ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#endif
+#ifndef _COM_SUN_STAR_IO_XACTIVEDATASINK_HPP_
+#include <com/sun/star/io/XActiveDataSink.hpp>
+#endif
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+
+#ifndef _UCBHELPER_MACROS_HXX
+#include <ucbhelper/macros.hxx>
+#endif
+
+namespace ucb
+{
+
+//=========================================================================
+
+/**
+  * This class implements the interface com::sun::star::io::XActiveDataSink.
+  * Instances of this class can be passed with the parameters of an
+  * "open" command.
+  */
+
+class ActiveDataSink : public cppu::OWeakObject,
+                       public com::sun::star::lang::XTypeProvider,
+                       public com::sun::star::io::XActiveDataSink
+{
+    com::sun::star::uno::Reference<
+        com::sun::star::io::XInputStream > m_xStream;
+
+public:
+     // XInterface
+    XINTERFACE_DECL()
+
+    // XTypeProvider
+    XTYPEPROVIDER_DECL()
+
+    // XActiveDataSink methods.
+    virtual void SAL_CALL
+    setInputStream( const com::sun::star::uno::Reference<
+                            com::sun::star::io::XInputStream >& aStream )
+        throw( com::sun::star::uno::RuntimeException );
+    virtual com::sun::star::uno::Reference<
+        com::sun::star::io::XInputStream > SAL_CALL
+    getInputStream()
+        throw( com::sun::star::uno::RuntimeException );
+};
+
+} /* namespace ucb */
+
+#endif /* !_UCBHELPER_ACTIVEDATASINK_HXX */
