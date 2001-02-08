efc33f01edd3d3a7b97c3af35198ed08f7fd7361
Niklas Nebel <nn@openoffice.org>
2001-02-08 14:58:43 +0000

    transfer object for drawing objects

---
+++ sc/source/ui/app/drwtrans.cxx
@@ -0,0 +1,454 @@
+/*************************************************************************
+ *
+ *  $RCSfile: drwtrans.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: nn $ $Date: 2001-02-08 15:58:43 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+// INCLUDE ---------------------------------------------------------------
+
+#ifdef PCH
+#include "ui_pch.hxx"
+#endif
+
+#pragma hdrstop
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/beans/XPropertySetInfo.hpp>
+#include <com/sun/star/form/FormButtonType.hpp>
+
+#include <sot/storage.hxx>
+#include <so3/svstor.hxx>
+#include <vcl/virdev.hxx>
+#include <svx/fmglob.hxx>
+#include <svx/svditer.hxx>
+#include <svx/svdograf.hxx>
+#include <svx/svdouno.hxx>
+#include <svx/svdpage.hxx>
+#include <svx/svdxcgv.hxx>
+#include <sfx2/docfile.hxx>
+#include <svtools/itempool.hxx>
+#include <svtools/urlbmk.hxx>
+#include <tools/urlobj.hxx>
+
+#include "drwtrans.hxx"
+#include "docsh.hxx"
+#include "drwlayer.hxx"
+#include "viewdata.hxx"
+
+using namespace com::sun::star;
+
+// -----------------------------------------------------------------------
+
+#define SCDRAWTRANS_TYPE_EMBOBJ         1
+#define SCDRAWTRANS_TYPE_DRAWMODEL      2
+
+// -----------------------------------------------------------------------
+
+// -----------------------------------------------------------------------
+
+ScDrawTransferObj::ScDrawTransferObj( SdrModel* pClipModel, ScDocShell* pContainerShell,
+                                        const TransferableObjectDescriptor& rDesc ) :
+    aObjDesc( rDesc ),
+    pModel( pClipModel ),
+    pBookmark( NULL ),
+    bGraphic( FALSE ),
+    bGrIsBit( FALSE ),
+    bOleObj( FALSE )
+{
+    //
+    //  check what kind of objects are contained
+    //
+
+    SdrPage* pPage = pModel->GetPage(0);
+    if (pPage)
+    {
+        SdrObjListIter aIter( *pPage, IM_FLAT );
+        SdrObject* pObject = aIter.Next();
+        if (pObject && !aIter.Next())               // exactly one object?
+        {
+            //
+            //  OLE object
+            //
+
+            UINT16 nSdrObjKind = pObject->GetObjIdentifier();
+            if (nSdrObjKind == OBJ_OLE2)
+            {
+                bOleObj = TRUE;
+            }
+
+            //
+            //  Graphic object
+            //
+
+            if (nSdrObjKind == OBJ_GRAF)
+            {
+                bGraphic = TRUE;
+                if ( ((SdrGrafObj*)pObject)->GetGraphic().GetType() == GRAPHIC_BITMAP )
+                    bGrIsBit = TRUE;
+            }
+
+            //
+            //  URL button
+            //
+
+            SdrUnoObj* pUnoCtrl = PTR_CAST(SdrUnoObj, pObject);
+            if (pUnoCtrl && FmFormInventor == pUnoCtrl->GetObjInventor())
+            {
+                uno::Reference<awt::XControlModel> xControlModel = pUnoCtrl->GetUnoControlModel();
+                DBG_ASSERT( xControlModel.is(), "uno control without model" );
+                if ( xControlModel.is() )
+                {
+                    uno::Reference< beans::XPropertySet > xPropSet( xControlModel, uno::UNO_QUERY );
+                    uno::Reference< beans::XPropertySetInfo > xInfo = xPropSet->getPropertySetInfo();
+
+                    rtl::OUString sPropButtonType = rtl::OUString::createFromAscii( "ButtonType" );
+                    rtl::OUString sPropTargetURL  = rtl::OUString::createFromAscii( "TargetURL" );
+                    rtl::OUString sPropLabel      = rtl::OUString::createFromAscii( "Label" );
+
+                    if(xInfo->hasPropertyByName( sPropButtonType ))
+                    {
+                        uno::Any aAny = xPropSet->getPropertyValue( sPropButtonType );
+                        form::FormButtonType eTmp;
+                        if ( (aAny >>= eTmp) && eTmp == form::FormButtonType_URL )
+                        {
+                            // URL
+                            if(xInfo->hasPropertyByName( sPropTargetURL ))
+                            {
+                                aAny = xPropSet->getPropertyValue( sPropTargetURL );
+                                rtl::OUString sTmp;
+                                if ( (aAny >>= sTmp) && sTmp.len() )
+                                {
+                                    String aUrl = sTmp;
+                                    String aAbs;
+                                    const SfxMedium* pMedium;
+                                    if (pContainerShell && (pMedium = pContainerShell->GetMedium()))
+                                    {
+                                        bool bWasAbs = true;
+                                        aAbs = pMedium->GetURLObject().smartRel2Abs( aUrl, bWasAbs ).GetMainURL();
+                                    }
+                                    else
+                                        aAbs = aUrl;
+
+                                    // Label
+                                    String aLabel;
+                                    if(xInfo->hasPropertyByName( sPropLabel ))
+                                    {
+                                        aAny = xPropSet->getPropertyValue( sPropLabel );
+                                        if ( (aAny >>= sTmp) && sTmp.len() )
+                                        {
+                                            aLabel = String(sTmp);
+                                        }
+                                    }
+                                    pBookmark = new INetBookmark( aAbs, aLabel );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //
+    //  get size for object descriptor
+    //
+
+    SdrExchangeView aView(pModel);
+    Point aPos;
+    SdrPageView* pPv = aView.ShowPagePgNum(0,aPos);
+    aView.MarkAll(pPv);
+    aSrcSize = aView.GetAllMarkedRect().GetSize();
+    aObjDesc.maSize = aSrcSize;
+
+    //
+    //
+    //
+
+    ScGlobal::SetClipDraw(pModel);      // remove when global ClipDraw is no longer used
+}
+
+ScDrawTransferObj::~ScDrawTransferObj()
+{
+    delete pModel;
+    delete pBookmark;
+}
+
+void ScDrawTransferObj::AddSupportedFormats()
+{
+    if ( bGrIsBit )             // single bitmap graphic
+    {
+        AddFormat( SOT_FORMATSTR_ID_SVXB );
+        AddFormat( SOT_FORMAT_BITMAP );
+        AddFormat( SOT_FORMAT_GDIMETAFILE );
+    }
+    else if ( bGraphic )        // other graphic
+    {
+        AddFormat( SOT_FORMATSTR_ID_SVXB );
+        AddFormat( SOT_FORMAT_GDIMETAFILE );
+        AddFormat( SOT_FORMAT_BITMAP );
+    }
+    else if ( pBookmark )       // url button
+    {
+//      AddFormat( SOT_FORMATSTR_ID_EMBED_SOURCE );
+        AddFormat( SOT_FORMATSTR_ID_OBJECTDESCRIPTOR );
+        AddFormat( SOT_FORMATSTR_ID_SOLK );
+        AddFormat( SOT_FORMAT_STRING );
+        AddFormat( SOT_FORMATSTR_ID_UNIFORMRESOURCELOCATOR );
+        AddFormat( SOT_FORMATSTR_ID_NETSCAPE_BOOKMARK );
+        AddFormat( SOT_FORMATSTR_ID_DRAWING );
+    }
+    else if ( bOleObj )         // single OLE object
+    {
+        AddFormat( SOT_FORMATSTR_ID_EMBED_SOURCE );
+        AddFormat( SOT_FORMATSTR_ID_OBJECTDESCRIPTOR );
+        AddFormat( SOT_FORMAT_GDIMETAFILE );
+    }
+    else                        // any drawing objects
+    {
+        AddFormat( SOT_FORMATSTR_ID_EMBED_SOURCE );
+        AddFormat( SOT_FORMATSTR_ID_OBJECTDESCRIPTOR );
+        AddFormat( SOT_FORMATSTR_ID_DRAWING );
+        AddFormat( SOT_FORMAT_BITMAP );
+        AddFormat( SOT_FORMAT_GDIMETAFILE );
+    }
+
+//  if( pImageMap )
+//      AddFormat( SOT_FORMATSTR_ID_SVIM );
+}
+
+sal_Bool ScDrawTransferObj::GetData( const ::com::sun::star::datatransfer::DataFlavor& rFlavor )
+{
+    sal_uInt32  nFormat = SotExchange::GetFormat( rFlavor );
+    sal_Bool    bOK = sal_False;
+
+    if( HasFormat( nFormat ) )
+    {
+        if ( nFormat == SOT_FORMATSTR_ID_LINKSRCDESCRIPTOR || nFormat == SOT_FORMATSTR_ID_OBJECTDESCRIPTOR )
+        {
+            bOK = SetTransferableObjectDescriptor( aObjDesc, rFlavor );
+        }
+        else if ( nFormat == SOT_FORMATSTR_ID_DRAWING )
+        {
+            bOK = SetObject( pModel, SCDRAWTRANS_TYPE_DRAWMODEL, rFlavor );
+        }
+        else if ( nFormat == SOT_FORMAT_BITMAP || nFormat == SOT_FORMAT_GDIMETAFILE )
+        {
+            SdrExchangeView aView( pModel );
+            Point aPos;
+            SdrPageView* pPv = aView.ShowPagePgNum( 0, aPos );
+            DBG_ASSERT( pPv, "pPv not there..." );
+            aView.MarkAll( pPv );
+            if ( nFormat == SOT_FORMAT_GDIMETAFILE )
+                bOK = SetGDIMetaFile( aView.GetAllMarkedMetaFile( TRUE ), rFlavor );
+            else
+                bOK = SetBitmap( aView.GetAllMarkedBitmap( TRUE ), rFlavor );
+        }
+        else if ( nFormat == SOT_FORMATSTR_ID_SVXB )
+        {
+            // only enabled for single graphics object
+
+            SdrPage* pPage = pModel->GetPage(0);
+            if (pPage)
+            {
+                SdrObjListIter aIter( *pPage, IM_FLAT );
+                SdrObject* pObject = aIter.Next();
+                if (pObject && pObject->GetObjIdentifier() == OBJ_GRAF)
+                {
+                    SdrGrafObj* pGraphObj = (SdrGrafObj*) pObject;
+                    bOK = SetGraphic( pGraphObj->GetGraphic(), rFlavor );
+                }
+            }
+        }
+        else if ( nFormat == SOT_FORMATSTR_ID_EMBED_SOURCE )
+        {
+            if ( bOleObj )              // single OLE object
+            {
+                //! initialize aOleData
+                bOK = SetAny( aOleData.GetAny( rFlavor ), rFlavor );
+            }
+            else                        // create object from contents
+            {
+                InitDocShell();         // set aDocShellRef
+
+                SvEmbeddedObject* pEmbObj = aDocShellRef;
+                bOK = SetObject( pEmbObj, SCDRAWTRANS_TYPE_EMBOBJ, rFlavor );
+            }
+        }
+        else if( pBookmark )
+        {
+            bOK = SetINetBookmark( *pBookmark, rFlavor );
+        }
+    }
+    return bOK;
+}
+
+sal_Bool ScDrawTransferObj::WriteObject( SotStorageStreamRef& rxOStm, void* pUserObject, sal_uInt32 nUserObjectId,
+                                        const ::com::sun::star::datatransfer::DataFlavor& rFlavor )
+{
+    // called from SetObject, put data into stream
+
+    sal_Bool bRet = sal_False;
+    switch (nUserObjectId)
+    {
+        case SCDRAWTRANS_TYPE_DRAWMODEL:
+            {
+                SdrModel* pDrawModel = (SdrModel*)pUserObject;
+
+                pDrawModel->SetStreamingSdrModel(TRUE);
+                rxOStm->SetVersion(SOFFICE_FILEFORMAT_NOW);
+                rxOStm->SetBufferSize( 0xff00 );
+                pDrawModel->PreSave();
+                pDrawModel->GetItemPool().SetFileFormatVersion( (USHORT)rxOStm->GetVersion() );
+                pDrawModel->GetItemPool().Store( *rxOStm );
+                *rxOStm << *pDrawModel;
+                pDrawModel->PostSave();
+                rxOStm->Commit();
+                pDrawModel->SetStreamingSdrModel(FALSE);
+                bRet = ( rxOStm->GetError() == ERRCODE_NONE );
+            }
+            break;
+
+        case SCDRAWTRANS_TYPE_EMBOBJ:
+            {
+                SvEmbeddedObject* pEmbObj = (SvEmbeddedObject*)pUserObject;
+
+                SvStorageRef xWorkStore( new SvStorage( *rxOStm ) );
+                rxOStm->SetBufferSize( 0xff00 );
+
+                // write document storage
+                pEmbObj->SetupStorage( xWorkStore );
+                bRet = pEmbObj->DoSaveAs( xWorkStore );
+                pEmbObj->DoSaveCompleted();
+                xWorkStore->Commit();
+                rxOStm->Commit();
+
+                bRet = ( rxOStm->GetError() == ERRCODE_NONE );
+            }
+            break;
+
+        default:
+            DBG_ERROR("unknown object id");
+    }
+    return bRet;
+}
+
+void ScDrawTransferObj::ObjectReleased()
+{
+    if (ScGlobal::GetClipModel() == pModel)     // remove when global ClipDoc is no longer used
+        ScGlobal::ReleaseClip();                // (still owner of pModel - deleted in dtor)
+
+    TransferableHelper::ObjectReleased();
+}
+
+//
+//  initialize aDocShellRef with a live document from the ClipDoc
+//
+
+void ScDrawTransferObj::InitDocShell()
+{
+    if ( !aDocShellRef.Is() )
+    {
+        ScDocShell* pDocSh = new ScDocShell;
+        aDocShellRef = pDocSh;      // ref must be there before InitNew
+
+        pDocSh->DoInitNew(NULL);
+
+        ScDocument* pDestDoc = pDocSh->GetDocument();
+        pDestDoc->InitDrawLayer( pDocSh );
+
+        SdrModel* pDestModel = pDestDoc->GetDrawLayer();
+        SdrExchangeView aDestView( pDestModel );
+        Point aPos;
+        aDestView.ShowPagePgNum( 0, aPos );
+        aDestView.Paste( *pModel, Point( aSrcSize.Width()/2, aSrcSize.Height()/2 ) );
+
+        // put objects to right layer (see ScViewFunc::PasteDataFormat for SOT_FORMATSTR_ID_DRAWING)
+
+        SdrPage* pPage = pDestModel->GetPage(0);
+        if (pPage)
+        {
+            SdrObjListIter aIter( *pPage, IM_DEEPWITHGROUPS );
+            SdrObject* pObject = aIter.Next();
+            while (pObject)
+            {
+                if ( pObject->ISA(SdrUnoObj) )
+                    pObject->NbcSetLayer(SC_LAYER_CONTROLS);
+                else
+                    pObject->NbcSetLayer(SC_LAYER_FRONT);
+                pObject = aIter.Next();
+            }
+        }
+
+        Point aTmpPoint;
+        Rectangle aDestArea( aTmpPoint, aSrcSize );
+        pDocSh->SetVisArea( aDestArea );
+
+        ScViewOptions aViewOpt( pDestDoc->GetViewOptions() );
+        aViewOpt.SetOption( VOPT_GRID, FALSE );
+        pDestDoc->SetViewOptions( aViewOpt );
+
+        ScViewData aViewData( pDocSh, NULL );
+        aViewData.SetTabNo( 0 );
+        aViewData.SetScreen( aDestArea );
+        aViewData.SetCurX( 0 );
+        aViewData.SetCurY( 0 );
+        pDocSh->UpdateOle(&aViewData, TRUE);
+    }
+}
+
+
