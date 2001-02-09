7b0ffa5a555f5c44fc04b9f0e74738060e8aa8c8
Ocke Janssen <oj@openoffice.org>
2001-02-09 11:55:27 +0000

    line- and fill color were mixed

--- svtools/source/brwbox/brwbox2.cxx
+++ svtools/source/brwbox/brwbox2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: brwbox2.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hjs $ $Date: 2000-11-03 18:24:29 $
+ *  last change: $Author: oj $ $Date: 2001-02-09 12:55:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -652,8 +652,8 @@ void BrowseBox::Paint( const Rectangle& rRect )
             DrawRect( Rectangle(
                 Point( nX, 0 ),
                 Point( rRect.Right(), GetTitleHeight() - 2 ) ) );
-            SetFillColor( aOldLineColor );
-            SetLineColor( aOldFillColor );
+            SetFillColor( aOldFillColor); // aOldLineColor );  oj 09.02.00 seems to be a copy&paste bug
+            SetLineColor( aOldLineColor); // aOldFillColor );
         }
     }
 }
