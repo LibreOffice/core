99a7408945981339df08465f8867aa5f8b045b37
Hans-Joachim Lankenau <hjs@openoffice.org>
2001-02-09 10:54:20 +0000

    fixed output file

--- vcl/unx/source/app/makefile.mk
+++ vcl/unx/source/app/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hjs $ $Date: 2001-02-09 11:48:39 $
+#   last change: $Author: hjs $ $Date: 2001-02-09 11:54:20 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -136,5 +136,5 @@ EXCEPTIONSFILES=$(SLO)$/salmain.obj	\
 
 
 $(SLO)$/%.obj: %.s
-    CC -c -o $(@:b).o $< && touch $@
+    CC -c -o $(@:s/.obj/.o/) $< && touch $@
 
