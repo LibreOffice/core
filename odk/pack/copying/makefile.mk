7891abf4a7f46fb47c8dc574b576c54bd7764518
Jörg Budischewski <jbu@openoffice.org>
2001-02-14 07:36:04 +0000

    #83676# added java samples

--- odk/pack/copying/makefile.mk
+++ odk/pack/copying/makefile.mk
@@ -11,14 +11,35 @@ IDLDIRLIST={$(subst,/,$/ $(shell $(FIND) $(IDLOUT) -type d -print))}
 
 DIRLIST = \
     $(DESTDIREXAMPLES)$/officeclient 	\
+    $(DESTDIRJAVAEXAMPLES)		 	\
+    $(DESTDIRJAVAEXAMPLES)$/draw		\
+    $(DESTDIRJAVAEXAMPLES)$/calc		\
+    $(DESTDIRJAVAEXAMPLES)$/writer		\
     {$(subst,$(IDLOUT),$(DESTDIRIDL) $(IDLDIRLIST))}
 
 EXAMPLESLIST= \
-    $(DESTDIREXAMPLES)$/officeclient$/officeclient.cxx \
-    $(DESTDIREXAMPLES)$/officeclient$/README           \
-    $(DESTDIREXAMPLES)$/officeclient$/makefile.mk      \
-    $(DESTDIREXAMPLES)$/officeclient$/Makefile         \
-    $(DESTDIREXAMPLES)$/officeclient$/exports.dxp      \
+    $(DESTDIREXAMPLES)$/officeclient$/officeclient.cxx 		\
+    $(DESTDIREXAMPLES)$/officeclient$/README           		\
+    $(DESTDIREXAMPLES)$/officeclient$/makefile.mk      		\
+    $(DESTDIREXAMPLES)$/officeclient$/Makefile         		\
+    $(DESTDIREXAMPLES)$/officeclient$/exports.dxp      		\
+    $(DESTDIRJAVAEXAMPLES)$/readme.txt		   		\
+    $(DESTDIRJAVAEXAMPLES)$/draw$/SDraw.java	   		\
+    $(DESTDIRJAVAEXAMPLES)$/draw$/makefile.mk	   		\
+    $(DESTDIRJAVAEXAMPLES)$/draw$/Makefile	  	 		\
+    $(DESTDIRJAVAEXAMPLES)$/calc$/SCalc.java		   	\
+    $(DESTDIRJAVAEXAMPLES)$/calc$/EuroAdaption.java			\
+    $(DESTDIRJAVAEXAMPLES)$/calc$/makefile.mk	   		\
+    $(DESTDIRJAVAEXAMPLES)$/calc$/Makefile	  	 		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/SWriter.java	   		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/StyleInitialization.java 	\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/TextDocumentStructure.java 	\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/TextReplace.java 		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/BookmarkInsertion.java 		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/HardFormatting.java 		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/StyleCreation.java  		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/makefile.mk	   		\
+    $(DESTDIRJAVAEXAMPLES)$/writer$/Makefile	  	 	
 
 IDLLIST={$(subst,/,$/ $(shell $(FIND) $(IDLOUT) -type f -print))}
 DESTIDLLIST={$(subst,$(IDLOUT),$(DESTDIRIDL) $(IDLLIST))}
@@ -36,6 +57,10 @@ $(DIRLIST) :
      -$(MKDIRHIER) 	$@
 
 $(DESTDIREXAMPLES)$/% : $(PRJ)$/examples$/cpp$/% $(DIRLIST) $(BIN)$/$(UDKNAME).zip
+    +-rm -f $@ >& $(NULLDEV)
+    $(MY_TEXTCOPY) $(MY_TEXTCOPY_SOURCEPRE) $? $(MY_TEXTCOPY_TARGETPRE) $@
+
+$(DESTDIRJAVAEXAMPLES)$/% : $(PRJ)$/examples$/java$/% $(DIRLIST) $(BIN)$/$(UDKNAME).zip
     +-rm -f $@ >& $(NULLDEV)
     $(MY_TEXTCOPY) $(MY_TEXTCOPY_SOURCEPRE) $? $(MY_TEXTCOPY_TARGETPRE) $@
 
