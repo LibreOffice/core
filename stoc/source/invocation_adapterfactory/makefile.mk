1a3201d73905483a1c943ddd7df2276a61eb43da
Patrick Luby <pluby@openoffice.org>
2001-02-15 00:51:12 +0000

    Disabled optimization for Mac OS X to eliminate core dumps

--- stoc/source/invocation_adapterfactory/makefile.mk
+++ stoc/source/invocation_adapterfactory/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: svesik $ $Date: 2001-02-02 14:14:05 $
+#   last change: $Author: pluby $ $Date: 2001-02-15 01:51:12 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,6 +78,16 @@ COMP1TYPELIST=$(TARGET)
 
 SLOFILES=	\
         $(SLO)$/iafactory.obj
+
+# gcc on Mac OS X optimizes out some temporary variables when optimization is
+# turned on for compiling 
+.IF "$(OS)"=="MACOSX"
+NOOPTFILES+=$(SLOFILES)
+.IF "$(NOOPT_FLAG)"!=""
+CFLAGSNOOPT=
+.ENDIF
+.ENDIF
+
 # SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
 .IF "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
 SLOFILES+=$(SLO)$/staticmbinvocation_adapterfactory.obj
