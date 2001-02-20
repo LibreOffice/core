925bd8e9e0575f79129bec240034d4078abcd301
Frank Schönheit <fs@openoffice.org>
2001-02-20 13:33:09 +0000

    #82877# +ErrorMessageDialog.idl

--- offapi/com/sun/star/sdb/makefile.mk
+++ offapi/com/sun/star/sdb/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: mi $ $Date: 2000-11-08 12:43:31 $
+#   last change: $Author: fs $ $Date: 2001-02-20 14:33:09 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -91,6 +91,7 @@ IDLFILES=\
     DataSourceBrowser.idl \
     DefinitionContainer.idl \
     DocumentDefinition.idl \
+    ErrorMessageDialog.idl \
     InteractionHandler.idl \
     ParametersRequest.idl \
     PreparedStatement.idl \
