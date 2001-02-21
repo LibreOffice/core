aabf194b5f530ba3448938c1683f67b317fdd3f4
Andreas Bregas <ab@openoffice.org>
2001-02-21 16:40:06 +0000

    Container for event descriptions

---
+++ toolkit/source/controls/eventcontainer.cxx
@@ -0,0 +1,209 @@
+/*************************************************************************
+ *
+ *  $RCSfile: eventcontainer.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: ab $ $Date: 2001-02-21 17:40:06 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+#ifndef _CPPUHELPER_QUERYINTERFACE_HXX_
+#include <cppuhelper/queryinterface.hxx>
+#endif
+#ifndef _CPPUHELER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+#ifndef _CPPUHELPER_FACTORY_HXX_
+#include <cppuhelper/factory.hxx>
+#endif
+#include <cppuhelper/interfacecontainer.hxx>
+
+#include "toolkit/controls/eventcontainer.hxx"
+#include <com/sun/star/script/ScriptEventDescriptor.hpp>
+
+
+using namespace com::sun::star::uno;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::container;
+using namespace com::sun::star::registry;
+using namespace com::sun::star::script;
+using namespace cppu;
+using namespace osl;
+using namespace rtl;
+using namespace std;
+
+
+// Methods XElementAccess
+Type NameContainer_Impl::getElementType()
+    throw(RuntimeException)
+{
+    return mType;
+}
+
+sal_Bool NameContainer_Impl::hasElements()
+    throw(RuntimeException)
+{
+    sal_Bool bRet = (mnElementCount > 0);
+    return bRet;
+}
+
+// Methods XNameAccess
+Any NameContainer_Impl::getByName( const OUString& aName )
+    throw(NoSuchElementException, WrappedTargetException, RuntimeException)
+{
+    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
+    if( aIt == mHashMap.end() )
+    {
+        throw NoSuchElementException();
+    }
+    sal_Int32 iHashResult = (*aIt).second;
+    Any aRetAny = mValues.getConstArray()[ iHashResult ];
+    return aRetAny;
+}
+
+Sequence< OUString > NameContainer_Impl::getElementNames()
+    throw(RuntimeException)
+{
+    return mNames;
+}
+
+sal_Bool NameContainer_Impl::hasByName( const OUString& aName )
+    throw(RuntimeException)
+{
+    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
+    sal_Bool bRet = ( aIt != mHashMap.end() );
+    return bRet;
+}
+
+
+// Methods XNameReplace
+void NameContainer_Impl::replaceByName( const OUString& aName, const Any& aElement )
+    throw(IllegalArgumentException, NoSuchElementException, WrappedTargetException, RuntimeException)
+{
+    Type aAnyType = aElement.getValueType();
+    if( mType != aAnyType )
+        throw IllegalArgumentException();
+
+    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
+    if( aIt == mHashMap.end() )
+    {
+        throw NoSuchElementException();
+    }
+    sal_Int32 iHashResult = (*aIt).second;
+    mValues.getArray()[ iHashResult ] = aElement;
+}
+
+
+// Methods XNameContainer
+void NameContainer_Impl::insertByName( const OUString& aName, const Any& aElement )
+    throw(IllegalArgumentException, ElementExistException, WrappedTargetException, RuntimeException)
+{
+    Type aAnyType = aElement.getValueType();
+    if( mType != aAnyType )
+        throw IllegalArgumentException();
+
+    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
+    if( aIt != mHashMap.end() )
+    {
+        throw ElementExistException();
+    }
+
+    sal_Int32 nCount = mNames.getLength();
+    mNames.realloc( nCount + 1 );
+    mValues.realloc( nCount + 1 );
+    mNames.getArray()[ nCount ] = aName;
+    mValues.getArray()[ nCount ] = aElement;
+    mHashMap[ aName ] = nCount;
+}
+
+void NameContainer_Impl::removeByName( const OUString& Name )
+    throw(NoSuchElementException, WrappedTargetException, RuntimeException)
+{
+    NameContainerNameMap::iterator aIt = mHashMap.find( Name );
+    if( aIt == mHashMap.end() )
+    {
+        throw NoSuchElementException();
+    }
+
+    sal_Int32 iHashResult = (*aIt).second;
+    mHashMap.erase( aIt );
+    sal_Int32 iLast = mNames.getLength() - 1;
+    if( iLast != iHashResult )
+    {
+        OUString* pNames = mNames.getArray();
+        Any* pValues = mValues.getArray();
+        pNames[ iHashResult ] = pNames[ iLast ];
+        pValues[ iHashResult ] = pValues[ iLast ];
+        mHashMap[ pNames[ iHashResult ] ] = iHashResult;
+    }
+    mNames.realloc( iLast );
+    mValues.realloc( iLast );
+}
+
+
+ScriptEventContainer::ScriptEventContainer( void )
+    : NameContainer_Impl( getCppuType( (ScriptEventDescriptor*) NULL ) )
+{
+}
+
+
+
+
+
