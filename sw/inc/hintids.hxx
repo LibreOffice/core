abaa7d97e696dc9e5d9cfe3c1ea1ae615433ac05
jp <jp@openoffice.org>
2001-02-21 11:30:50 +0000

    new: Which mapping for ScriptType

--- sw/inc/hintids.hxx
+++ sw/inc/hintids.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hintids.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: jp $ $Date: 2001-02-15 20:10:39 $
+ *  last change: $Author: jp $ $Date: 2001-02-21 12:30:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -394,6 +394,8 @@ inline const SfxPoolItem* GetDfltAttr( USHORT nWhich )
     return aAttrTab[ nWhich - POOLATTR_BEGIN ];
 }
 #endif
+
+USHORT GetWhichOfScript( USHORT nWhich, USHORT nScript );
 
 // return for the given TextAttribut without an end the correct character.
 // This function returns
