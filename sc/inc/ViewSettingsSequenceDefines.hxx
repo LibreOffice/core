4efc03bf439b85a2817ea8bd080b35c647e85341
Sascha Ballach <sab@openoffice.org>
2001-02-21 08:44:05 +0000

    add the defines, which are used to get the view settings out of a sequence. the sequence is used to give the XML Export/Import the view settings

---
+++ sc/inc/ViewSettingsSequenceDefines.hxx
@@ -0,0 +1,96 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ViewSettingsSequenceDefines.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: sab $ $Date: 2001-02-21 09:44:05 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SC_VIEWSETTINGSSEQUENCEDEFINES_HXX
+#define _SC_VIEWSETTINGSSEQUENCEDEFINES_HXX
+
+// this are the defines for the position of the settings in the
+// ViewSettingsSequence
+
+#define SC_VIEWSETTINGS_COUNT               6
+
+#define SC_TABLE_VIEWSETTINGS               0
+#define SC_ACTIVE_TABLE                     1
+#define SC_HORIZONTAL_SCROLL_BAR_WIDTH      2
+#define SC_ZOOM_TYPE                        3
+#define SC_ZOOM_VALUE                       4
+#define SC_PAGE_VIEW_ZOOM_VALUE             5
+
+
+// this are the defines for the position of the settings in the
+// TableViewSettingsSequence
+
+#define SC_TABLE_VIEWSETTINGS_COUNT         11
+
+#define SC_CURSOR_X                         0
+#define SC_CURSOR_Y                         1
+#define SC_HORIZONTAL_SPLIT_MODE            2
+#define SC_VERTICAL_SPLIT_MODE              3
+#define SC_HORIZONTAL_SPLIT_POSITION        4
+#define SC_VERTICAL_SPLIT_POSITION          5
+#define SC_ACTIVE_SPLIT_RANGE               6
+#define SC_POSITION_LEFT                    7
+#define SC_POSITION_RIGHT                   8
+#define SC_POSITION_TOP                     9
+#define SC_POSITION_BOTTOM                  10
+
+
+#endif
