6b0600f3ba2fff4177042b598346b59740a80c8e
Patrick Luby <pluby@openoffice.org>
2001-02-23 02:44:30 +0000

    Correct Mac OS X "tempnam" bug

--- dmake/unix/macosx/gnu/make.sh
+++ dmake/unix/macosx/gnu/make.sh
@@ -51,10 +51,13 @@ cc -c -I. -Iunix -Iunix/macosx -Iunix/macosx/gnu -O unix/runargv.c
 mv runargv.o objects
 cc -c -I. -Iunix -Iunix/macosx -Iunix/macosx/gnu -O unix/dcache.c
 mv dcache.o objects
+cc -c -I. -Iunix -Iunix/macosx -Iunix/macosx/gnu -O unix/macosx/tempnam.c
+mv tempnam.o objects
 cc -O -o dmake  objects/infer.o objects/make.o objects/stat.o objects/expand.o \
 objects/dmstring.o objects/hash.o objects/dag.o objects/dmake.o objects/path.o \
 objects/imacs.o objects/sysintf.o objects/parse.o objects/getinp.o \
 objects/quit.o objects/state.o objects/dmdump.o objects/macparse.o \
 objects/rulparse.o objects/percent.o objects/function.o objects/arlib.o \
-objects/dirbrk.o objects/rmprq.o objects/ruletab.o objects/runargv.o objects/dcache.o
+objects/dirbrk.o objects/rmprq.o objects/ruletab.o objects/runargv.o \
+objects/dcache.o objects/tempnam.o
 cp unix/macosx/gnu/template.mk startup/config.mk
