9176179eead9597ca43c02609bc34f64a0b282f0
Andreas Bregas <ab@openoffice.org>
2001-03-03 13:56:17 +0000

    D_DLGEDITOR define activated

--- basctl/source/basicide/makefile.mk
+++ basctl/source/basicide/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: tbe $ $Date: 2001-02-26 11:14:44 $
+#   last change: $Author: ab $ $Date: 2001-03-03 14:56:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -80,7 +80,7 @@ PROJECTPCHSOURCE=ide_pch
 CDEFS+=-DBASICDEBUG
 .ENDIF
 
-#CDEFS+=-D_DLGEDITOR_
+CDEFS+=-D_DLGEDITOR_
 
 # --- Allgemein ----------------------------------------------------------
 
