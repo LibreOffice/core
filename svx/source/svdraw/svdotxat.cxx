b4fe12432526983c7a7fd11d246f81fd05ff16f7
Dieter Loeschky <dl@openoffice.org>
2001-03-06 06:35:55 +0000

    Textdirection

--- svx/source/svdraw/svdotxat.cxx
+++ svx/source/svdraw/svdotxat.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svdotxat.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: dl $ $Date: 2001-03-05 12:26:51 $
+ *  last change: $Author: dl $ $Date: 2001-03-06 07:35:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -299,24 +299,10 @@ void SdrTextObj::ItemSetChanged()
 
 void SdrTextObj::ItemChange(const sal_uInt16 nWhich, const SfxPoolItem* pNewItem)
 {
-    if( pNewItem )
+    if( pNewItem && nWhich == SDRATTR_TEXTDIRECTION_LEFT_TO_RIGHT )
     {
-        switch( nWhich )
-        {
-            case SID_TEXTDIRECTION_LEFT_TO_RIGHT:
-            {
-                if( pOutlinerParaObject )
-                    pOutlinerParaObject->SetVertical( FALSE );
-            }
-            break;
-
-            case SID_TEXTDIRECTION_TOP_TO_BOTTOM:
-            {
-                if( pOutlinerParaObject )
-                    pOutlinerParaObject->SetVertical( TRUE );
-            }
-            break;
-        }
+        if( pOutlinerParaObject )
+            pOutlinerParaObject->SetVertical( !( (SfxBoolItem*) pNewItem )->GetValue() );
     }
 
     SdrAttrObj::ItemChange( nWhich, pNewItem );
