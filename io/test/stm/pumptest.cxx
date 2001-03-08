659fdc1d97eed886cf92ce2033eef90efe0329b9
Armin Theissen <armin@openoffice.org>
2001-03-08 08:29:47 +0000

    changed stl includes

--- io/test/stm/pumptest.cxx
+++ io/test/stm/pumptest.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pumptest.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jbu $ $Date: 2001-01-25 14:06:36 $
+ *  last change: $Author: armin $ $Date: 2001-03-08 09:27:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,7 +78,7 @@
 #include <cppuhelper/factory.hxx>
 #include <osl/mutex.hxx>
 #include <osl/thread.h>
-#include <stl/list>
+#include <list>
 
 
 
--- sal/workben/clipboardwben/testcopy/XTDataObject.cxx
+++ sal/workben/clipboardwben/testcopy/XTDataObject.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XTDataObject.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tra $ $Date: 2000-11-13 08:34:09 $
+ *  last change: $Author: armin $ $Date: 2001-03-08 09:29:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -73,7 +73,7 @@
 
 #include <windows.h>
 #include <ole2.h>
-#include <stl/memory>
+#include <memory>
 
 //------------------------------------------------------------------------
 // namespace directives
