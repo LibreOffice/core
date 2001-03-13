733abfbb8f02a2667a77fb679d9a8298f80b1d39
Juergen Schmidt <jsc@openoffice.org>
2001-03-13 11:43:15 +0000

    remove using namespace vos

--- stoc/source/defaultregistry/defaultregistry.cxx
+++ stoc/source/defaultregistry/defaultregistry.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: defaultregistry.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jl $ $Date: 2001-03-12 15:32:37 $
+ *  last change: $Author: jsc $ $Date: 2001-03-13 12:43:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -97,7 +97,6 @@ using namespace com::sun::star::uno;
 using namespace com::sun::star::registry;
 using namespace com::sun::star::lang;
 using namespace cppu;
-using namespace vos;
 using namespace osl;
 using namespace rtl;
 
