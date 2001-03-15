aa447e8851119774555c0366dace8915cc12ca92
Frank Schönheit <fs@openoffice.org>
2001-03-15 08:09:46 +0000

    cppuhelper/extract -> comphelper/extract

--- dbaccess/source/ui/misc/DExport.cxx
+++ dbaccess/source/ui/misc/DExport.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DExport.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: oj $ $Date: 2001-02-23 15:10:55 $
+ *  last change: $Author: fs $ $Date: 2001-03-15 09:09:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,8 +112,8 @@
 #ifndef _CONNECTIVITY_DBTOOLS_HXX_
 #include <connectivity/dbtools.hxx>
 #endif
-#ifndef _CPPUHELPER_EXTRACT_HXX_
-#include <cppuhelper/extract.hxx>
+#ifndef _COMPHELPER_EXTRACT_HXX_
+#include <comphelper/extract.hxx>
 #endif
 #ifndef DBAUI_TYPEINFO_HXX
 #include "TypeInfo.hxx"
