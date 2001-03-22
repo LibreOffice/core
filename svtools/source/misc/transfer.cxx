5fdd95a0641b94dac09b919bcfaa5ff49604163d
jp <jp@openoffice.org>
2001-03-22 12:35:54 +0000

    inlne GetDataFlavorExVector

--- svtools/source/misc/transfer.cxx
+++ svtools/source/misc/transfer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: transfer.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: ka $ $Date: 2001-03-21 11:34:09 $
+ *  last change: $Author: jp $ $Date: 2001-03-22 13:35:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1067,13 +1067,6 @@ DataFlavor TransferableDataHelper::GetFormatDataFlavor( sal_uInt32 nFormat ) con
         aRet = (DataFlavor&) (*mpFormats)[ nFormat ];
 
     return aRet;
-}
-
-// -----------------------------------------------------------------------------
-
-DataFlavorExVector& TransferableDataHelper::GetDataFlavorExVector() const
-{
-    return( *mpFormats );
 }
 
 // -----------------------------------------------------------------------------
--- svtools/source/misc/transfer2.cxx
+++ svtools/source/misc/transfer2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: transfer2.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ka $ $Date: 2001-03-21 11:34:10 $
+ *  last change: $Author: jp $ $Date: 2001-03-22 13:35:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -388,9 +388,3 @@ sal_Bool DropTargetHelper::IsDropFormatSupported( const DataFlavor& rFlavor )
     return bRet;
 }
 
-// -----------------------------------------------------------------------------
-
-DataFlavorExVector& DropTargetHelper::GetDataFlavorExVector() const
-{
-    return( *mpFormats );
-}
