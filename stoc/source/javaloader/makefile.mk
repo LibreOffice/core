2bf317a6fd74a761af9abd66289c837862c2569a
Patrick Luby <pluby@openoffice.org>
2001-03-23 22:39:02 +0000

    Mac OS X updates

--- stoc/source/javaloader/makefile.mk
+++ stoc/source/javaloader/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: jsc $ $Date: 2001-03-07 10:05:16 $
+#   last change: $Author: pluby $ $Date: 2001-03-23 23:39:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,13 +78,6 @@ COMP1TYPELIST=$(TARGET)
 .INCLUDE :  ..$/cppumaker.mk
 
 SLOFILES= 	$(SLO)$/javaloader.obj
-
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
-# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX"
-SLOFILES+=$(SLO)$/staticmbjavaloader.obj
-.ENDIF
 
 SHL1TARGET= $(TARGET)
 
