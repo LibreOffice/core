a3908b90c1b4d2746e088cf4d48087a08893e8da
Patrick Luby <pluby@openoffice.org>
2001-03-23 22:39:02 +0000

    Mac OS X updates

--- connectivity/source/drivers/calc/makefile.mk
+++ connectivity/source/drivers/calc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: oj $ $Date: 2001-03-19 09:35:28 $
+#   last change: $Author: pluby $ $Date: 2001-03-23 23:38:23 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -90,13 +90,6 @@ SLOFILES=\
     $(SLO)$/CConnection.obj			\
     $(SLO)$/Cservices.obj			\
     $(SLO)$/CDriver.obj
-
-# NETBSD: somewhere we have to instantiate the static data members.
-# NETBSD-1.2.1 doesn't know about weak symbols so the default mechanism for GCC won't work.
-# SCO and MACOSX: the linker does know about weak symbols, but we can't ignore multiple defined symbols
-.IF "$(OS)"=="NETBSD" || "$(OS)"=="SCO" || "$(OS)$(COM)"=="OS2GCC" || "$(OS)"=="MACOSX" 
-SLOFILES+=$(SLO)$/staticmbcalc.obj
-.ENDIF
 
 .IF "$(OS)"=="MACOSX"
 .ELSE
