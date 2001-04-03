9c6210e18eee44000ca9ee297f25ec59fa3c12bb
Martin Gallwey <mtg@openoffice.org>
2001-04-03 11:48:35 +0000

    #82744# correctly handle a missing printer

--- sw/source/ui/uno/unotxdoc.cxx
+++ sw/source/ui/uno/unotxdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unotxdoc.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: mtg $ $Date: 2001-03-30 14:54:14 $
+ *  last change: $Author: mtg $ $Date: 2001-04-03 12:48:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -2210,7 +2210,8 @@ Any SwXTextDocument::getPropertyValue(const OUString& rPropertyName)
         case WID_DOC_PRINTER_NAME:
         {
             SfxPrinter *pPrinter = pDocShell->GetDoc()->GetPrt ( sal_False );
-            aAny <<= OUString ( pPrinter->GetName());
+            aAny <<= pPrinter ? OUString ( pPrinter->GetName()) : OUString();
+
         }
         break;
         case WID_DOC_IS_KERN_ASIAN_PUNCTUATION:
