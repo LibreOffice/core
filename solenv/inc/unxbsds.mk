830ec7ee3273edcbed4de9f6dba3c627200028f8
Sander Vesik <svesik@openoffice.org>
2001-04-09 15:25:23 +0000

    Merge 619 enhancements to trunk.

--- solenv/inc/unxbsds.mk
+++ solenv/inc/unxbsds.mk
@@ -1,77 +1,191 @@
-# mak file fuer unxbsds
 
+#*************************************************************************
+#
+#   $RCSfile: unxbsds.mk,v $
+#
+#   $Revision: 1.3 $
+#
+#   last change: $Author: svesik $ $Date: 2001-04-09 16:25:23 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+# mk file for unxbsds
 ASM=
 AFLAGS=
 
-cc=gcc -c 
-CC=g++ -c 
-CDEFS+=-D_PTHREADS -D_REENTRANT -DOSL_USE_SYS_V_SEMAPHORE
-CDEFS+=-D_STD_NO_NAMESPACE -D_VOS_NO_NAMESPACE -D_UNO_NO_NAMESPACE -DSTLPORT_VERSION=321
-CDEFS+=-DNO_INET_ON_DEMAND -DNEW_SOLAR -DNCIfeature
-CFLAGS+=-w -c $(INCLUDE) 
-CFLAGSCC=-pipe 
-CFLAGSEXCEPTIONS=-fexceptions
-CFLAGS_NO_EXCEPTIONS=-fno-exceptions
-CFLAGSCXX=-pipe -fguiding-decls -fno-for-scope -frtti -fpermissive
+SOLAR_JAVA=TRUE
+JAVAFLAGSDEBUG=-g
 
-CFLAGSOBJGUIST=
-CFLAGSOBJCUIST=
-CFLAGSOBJGUIMT= 
-CFLAGSOBJCUIMT= 
+# filter for supressing verbose messages from linker
+#not needed at the moment
+#LINKOUTPUT_FILTER=" |& $(SOLARENV)$/bin$/msg_filter"
+
+# _PTHREADS is needed for the stl
+CDEFS+=-D_PTHREADS -D_REENTRANT -DNEW_SOLAR -D_USE_NAMESPACE=1 -DSTLPORT_VERSION=400 -DOSL_USE_SYS_V_SEMAPHORE
+
+# this is a platform with JAVA support
+.IF "$(SOLAR_JAVA)"!=""
+JAVADEF=-DSOLAR_JAVA
+.IF "$(debug)"==""
+JAVA_RUNTIME=-ljava
+.ELSE
+JAVA_RUNTIME=-ljava_g
+.ENDIF
+.ENDIF 
+
+# name of C++ Compiler
+CC=g++ 
+# name of C Compiler
+cc=gcc 
+# flags for C and C++ Compiler
+CFLAGS=-w -c $(INCLUDE) -I$(SOLARENV)/unxbsds/usr/include
+# flags for the C++ Compiler
+CFLAGSCC= -pipe 
+
+# Flags for enabling exception handling
+CFLAGSEXCEPTIONS=-fexceptions
+# Flags for disabling exception handling
+CFLAGS_NO_EXCEPTIONS=-fno-exceptions
+
+# -fpermissive should be removed as soon as possible
+CFLAGSCXX= -pipe -fno-for-scope -fpermissive 
+
+# Compiler flags for compiling static object in single threaded environment with graphical user interface
+CFLAGSOBJGUIST= -fPIC
+# Compiler flags for compiling static object in single threaded environment with character user interface
+CFLAGSOBJCUIST= -fPIC
+# Compiler flags for compiling static object in multi threaded environment with graphical user interface
+CFLAGSOBJGUIMT= -fPIC
+# Compiler flags for compiling static object in multi threaded environment with character user interface
+CFLAGSOBJCUIMT= -fPIC
+# Compiler flags for compiling shared object in multi threaded environment with graphical user interface
 CFLAGSSLOGUIMT=	-fPIC
+# Compiler flags for compiling shared object in multi threaded environment with character user interface
 CFLAGSSLOCUIMT=	-fPIC
-CFLAGSPROF=     -pg
-CFLAGSDEBUG=	-g
+# Compiler flags for profiling
+CFLAGSPROF=
+# Compiler flags for debugging
+CFLAGSDEBUG=-g
 CFLAGSDBGUTIL=
-# die zusaetzlichen Optimierungsschalter schalten alle Optimierungen ein, die zwischen -O und -O2 liegen und
-# per Schalter einschaltbar sind. Dennoch gibt es einen Unterschied: einige Files im Writer werden
-# misoptimiert wenn -O2 eingeschaltet ist und waehrend die untenstehenden Schalter funktionieren.
-CFLAGSOPT=-O -fcse-follow-jumps -fcse-skip-blocks -fexpensive-optimizations -fstrength-reduce -fforce-mem -fcaller-saves -fgcse -frerun-cse-after-loop -frerun-loop-opt -fschedule-insns2 -fregmove -foptimize-register-move 
-#CFLAGSOPT=-O2
+# Compiler flags for enabling optimazations
+CFLAGSOPT=-O2
+# Compiler flags for disabling optimazations
 CFLAGSNOOPT=-O
+# Compiler flags for discibing the output path
 CFLAGSOUTOBJ=-o
 
-STATIC=			-Wl,-Bstatic
-DYNAMIC=		-Wl,-Bdynamic
+# switches for dynamic and static linking
+STATIC		= -Wl,-Bstatic
+DYNAMIC		= -Wl,-Bdynamic
 
-THREADLIB=
-LINK= gcc
+# name of linker
+LINK=gcc
+# default linker flags
 LINKFLAGS=
-.IF "$(PRJNAME)"=="osl" ||  "$(PRJNAME)"=="rtl" 
-LINKFLAGSSHLGUI= -shared -nostdlib -Wl,-shared
-LINKFLAGSSHLCUI= -shared -nostdlib -Wl,-shared
-.ELSE
-LINKFLAGSSHLGUI= -shared -nostdlib -Wl,-shared
-LINKFLAGSSHLCUI= -shared -nostdlib -Wl,-shared
-.ENDIF
-LINKFLAGSAPPGUI= -lpthread
-LINKFLAGSAPPCUI= -lpthread
+
+# linker flags for linking applications
+LINKFLAGSAPPGUI= -Wl,-export-dynamic 
+LINKFLAGSAPPCUI= -Wl,-export-dynamic
+# linker flags for linking shared libraries
+LINKFLAGSSHLGUI= -shared
+LINKFLAGSSHLCUI= -shared
+
 LINKFLAGSTACK=
 LINKFLAGSPROF=
-LINKFLAGSDEBUG=
+LINKFLAGSDEBUG=-g
 LINKFLAGSOPT= 
 
-_SYSLIBS= -lc -lm 
-_X11LIBS= -L/usr/X11R6/lib -lXext -lXt -lX11
+.IF "$(NO_BSYMBOLIC)"==""
+.IF "$(PRJNAME)" != "envtest"
+LINKFLAGSSHLGUI+=-Wl,-Bsymbolic
+LINKFLAGSSHLCUI+=-Wl,-Bsymbolic
+.ENDIF
+.ENDIF				# "$(NO_BSYMBOLIC)"==""
 
-STDLIBCPP= -lstdc++
+LINKVERSIONMAPFLAG=-Wl,--version-script
 
+# Sequence of libs does matter !
+
+STDLIBCPP=-lstdc++
+
+# default objectfilenames to link
 STDOBJGUI=
 STDSLOGUI=
 STDOBJCUI=
 STDSLOCUI=
 
-STDLIBGUIST=  ${_X11LIBS} ${_SYSLIBS}
-STDLIBCUIST=              ${_SYSLIBS}
-STDLIBGUIMT=  ${_X11LIBS} ${_SYSLIBS}
-STDLIBCUIMT=              ${_SYSLIBS}
-STDSHLGUIMT=
-STDSHLCUIMT=
+# libraries for linking applications
+STDLIBCUIST=-lm
+STDLIBGUIMT=-lXaw -lXt -lX11 -lpthread -lm
+STDLIBCUIMT=-lpthread -lm
+STDLIBGUIST=-lXaw -lXt -lX11 -lm
+# libraries for linking shared libraries
+STDSHLGUIMT=-lXaw -lXt -lX11 -lXext -lpthread -lm
+STDSHLCUIMT=-lpthread -lm
 
-LIBMGR=			ar
-LIBFLAGS=		-r
-LIBEXT=			.a
+# STLport always needs pthread. This may yield some redundant -lpthread
+# but that doesn't matter. 
+LIBSTLPORT=$(DYNAMIC) -lstlport_gcc -lpthread
+LIBSTLPORTST=$(STATIC) -lstlport_gcc $(DYNAMIC) -lpthread
 
+
+# name of library manager
+LIBMGR=ar
+LIBFLAGS=-r
+
+# tool for generating import libraries
 IMPLIB=
 IMPLIBFLAGS=
 
@@ -79,14 +193,13 @@ MAPSYM=
 MAPSYMFLAGS=
 
 RC=irc
-RCFLAGS=		-fo$@ $(RCFILES)
+RCFLAGS=-fo$@ $(RCFILES)
 RCLINK=
 RCLINKFLAGS=
 RCSETVERSION=
 
-DLLPOSTFIX=		bs
-DLLPRE=			lib
-DLLPOST=		.so.1.0
-
-LDUMP=
+# platform specific identifier for shared libs
+DLLPOSTFIX=bs
+DLLPRE=lib
+DLLPOST=.so
 
