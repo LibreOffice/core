60886139d4d5c407247e588d68495b6c37523bae
Björn Milcke <bm@openoffice.org>
2001-04-10 11:37:57 +0000

    #85929# too much comitted

--- xmloff/source/chart/SchXMLTableContext.cxx
+++ xmloff/source/chart/SchXMLTableContext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SchXMLTableContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: bm $ $Date: 2001-04-10 12:27:25 $
+ *  last change: $Author: bm $ $Date: 2001-04-10 12:37:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -109,12 +109,10 @@ using namespace com::sun::star;
 SchXMLTableContext::SchXMLTableContext( SchXMLImportHelper& rImpHelper,
                                         SvXMLImport& rImport,
                                         const rtl::OUString& rLName,
-                                        SchXMLTable& aTable,
-                                        ::rtl::OUString& rTableName ) :
+                                        SchXMLTable& aTable ) :
         SvXMLImportContext( rImport, XML_NAMESPACE_TABLE, rLName ),
         mrImportHelper( rImpHelper ),
-        mrTable( aTable ),
-        mrTableName( rTableName )
+        mrTable( aTable )
 {
     mrTable.nColumnIndex = -1;
     mrTable.nMaxColumnIndex = -1;
@@ -124,26 +122,6 @@ SchXMLTableContext::SchXMLTableContext( SchXMLImportHelper& rImpHelper,
 
 SchXMLTableContext::~SchXMLTableContext()
 {
-}
-
-void SchXMLTableContext::StartElement( const uno::Reference< xml::sax::XAttributeList >& xAttrList )
-{
-    // get number-columns-repeated attribute
-    sal_Int16 nAttrCount = xAttrList.is()? xAttrList->getLength(): 0;
-
-    for( sal_Int16 i = 0; i < nAttrCount; i++ )
-    {
-        rtl::OUString sAttrName = xAttrList->getNameByIndex( i );
-        rtl::OUString aLocalName;
-        USHORT nPrefix = GetImport().GetNamespaceMap().GetKeyByAttrName( sAttrName, &aLocalName );
-
-        if( nPrefix == XML_NAMESPACE_TABLE &&
-            aLocalName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( sXML_name )))
-        {
-            mrTableName = xAttrList->getValueByIndex( i );
-            break;   // we only need this attribute
-        }
-    }
 }
 
 SvXMLImportContext *SchXMLTableContext::CreateChildContext(
