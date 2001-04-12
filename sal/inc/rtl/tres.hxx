cb1371ac777b2cd8ae1fea343f1b30d969254730
sz <sz@openoffice.org>
2001-04-12 05:16:12 +0000

    added header, macro TST_BOOM

--- sal/inc/rtl/tres.hxx
+++ sal/inc/rtl/tres.hxx
@@ -1,3 +1,63 @@
+/*************************************************************************
+ *
+ *  $RCSfile: tres.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: sz $ $Date: 2001-04-12 06:16:12 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 #ifndef _RTL_TRES_HXX_
 #define _RTL_TRES_HXX_
 
@@ -5,17 +65,16 @@
 #include <osl/diagnose.h>
 #endif
 
-#ifndef _OSL_THREAD_H_
-#include    <osl/thread.h>
-#endif
+#define TST_BOOM(c, m)  OSL_ENSURE(c, m)
+
 
 namespace rtl {
 class tRes {
 
     sal_Char* m_name;
     sal_Char* m_result;
-    sal_Bool m_boom;
     sal_Bool m_state;
+    sal_Bool m_boom;
 
     tRes();
     tRes( const tRes& oRes );
@@ -72,9 +131,9 @@ public:
     //> c'tor
     tRes( const sal_Char* meth, sal_Bool boom = sal_False )
             : m_name(0)
-            , m_boom( boom )
             , m_result(0)
-            , m_state(sal_False) {
+            , m_state( sal_False )
+            , m_boom( boom ) {
 
 
         cpy( &m_name, meth );
@@ -96,7 +155,7 @@ public:
             if(! msg ) {
                 cpy( &msg, m_name );
             }
-            OSL_ENSURE( m_state, msg );
+            TST_BOOM( m_state, msg );
         }
     } ///< state
 
