2f2fd6db7c3ad6f21342504abf68af7b72a15d79
Frank Schönheit <fs@openoffice.org>
2001-04-19 15:14:04 +0000

    #86147# +eventlistenercontainer

--- unotools/source/misc/makefile.mk
+++ unotools/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: pluby $ $Date: 2001-03-02 07:20:48 $
+#   last change: $Author: fs $ $Date: 2001-04-19 16:14:04 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -98,7 +98,8 @@ UNOUCROUT=$(OUT)$/inc
 # --- Files -------------------------------------
 
 SLOFILES=	$(SLO)$/atom.obj \
-            $(SLO)$/datetime.obj
+            $(SLO)$/datetime.obj \
+            $(SLO)$/eventlisteneradapter.obj
 
 # --- Targets ----------------------------------
 
