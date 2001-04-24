025a71d4d73dc4844524846bc34b34c669dd1b77
Oliver Specht <os@openoffice.org>
2001-04-24 10:20:29 +0000

    enable exceptions in docufld.cxx

--- sw/source/core/fields/makefile.mk
+++ sw/source/core/fields/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-19 00:08:19 $
+#   last change: $Author: os $ $Date: 2001-04-24 11:20:29 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -117,7 +117,9 @@ SLOFILES =	\
         $(SLO)$/tblcalc.obj \
         $(SLO)$/usrfld.obj
 
-EXCEPTIONSFILES=$(SLO)$/usrfld.obj
+EXCEPTIONSFILES= \
+        $(SLO)$/docufld.obj \
+        $(SLO)$/usrfld.obj
 
 # --- Tagets -------------------------------------------------------
 
