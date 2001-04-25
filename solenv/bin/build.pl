3ad9f00c247bb0510dc3a4d00957c72c0aa38bc0
Vladimir Glazounov <vg@openoffice.org>
2001-04-25 16:16:47 +0000

    Added copyright comments

--- solenv/bin/build.pl
+++ solenv/bin/build.pl
@@ -1,9 +1,83 @@
-#!/usr/solar/bin/perl
+:
+eval 'exec perl -wS $0 ${1+"$@"}'
+    if 0;
+#*************************************************************************
+#
+#   $RCSfile: build.pl,v $
+#
+#   $Revision: 1.13 $
+#
+#   last change: $Author: vg $ $Date: 2001-04-25 17:16:47 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
 #
 #
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+#
+# build - build entire project
 #
 
 use Cwd;
+
+#### script id #####
+
+( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
+
+$id_str = ' $Revision: 1.13 $ ';
+$id_str =~ /Revision:\s+(\S+)\s+\$/
+  ? ($script_rev = $1) : ($script_rev = "-");
+
+print "$script_name -- version: $script_rev\n";
 
 #########################
 #                       #
@@ -25,6 +99,9 @@ $StandDir = GetStandDir();
 $QuantityToBuild = GetQuantityToBuild();
 $BuildAllParents = HowToBuild();
 $ENV{mk_tmp} = "1";
+
+#### main ####
+
 $dmake = GetDmakeCommando();
 BuildAll();
 @TotenEltern = keys %DeadParents;
@@ -36,6 +113,7 @@ if ($#TotenEltern != -1) {
     };
     print "\nnot found and couldn't be built. Correct build.lsts.\n";
 };
+$ENV{mk_tmp} = "";
 
 
 #########################
@@ -102,7 +180,6 @@ sub BuildAll {
     } else {
         BuildPrj(".");
     };
-    $ENV{mk_tmp} = "";
 };
 
 
@@ -131,7 +208,7 @@ sub MakeDir {
 
 
 #
-# Get string of parent projects to build
+# Get string (list) of parent projects to build
 #
 sub GetParentsString {
     my ($PrjDir);
