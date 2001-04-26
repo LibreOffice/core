06a7ad094d6b90c135bdd8f7a7339a04424c4fa1
Frank Schönheit <fs@openoffice.org>
2001-04-26 11:11:34 +0000

    added support for data source associated bookmarks

--- dbaccess/source/ui/misc/makefile.mk
+++ dbaccess/source/ui/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: fs $ $Date: 2001-04-11 12:57:56 $
+#   last change: $Author: fs $ $Date: 2001-04-26 12:08:48 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -82,6 +82,9 @@ SRCFILES =	\
 # ... object files ............................
 
 SLOFILES=	\
+        $(SLO)$/documentautolinker.obj		\
+        $(SLO)$/linkeddocuments.obj			\
+        $(SLO)$/filenotation.obj			\
         $(SLO)$/indexcollection.obj			\
         $(SLO)$/UITools.obj					\
         $(SLO)$/WCPage.obj					\
