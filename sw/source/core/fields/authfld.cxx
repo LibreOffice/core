af831166301be23f32f3e20a346f67ff0780d8ff
jp <jp@openoffice.org>
2001-04-26 19:46:28 +0000

    Bug #86433#: DTOR - remove not valid ASSERT

--- sw/source/core/fields/authfld.cxx
+++ sw/source/core/fields/authfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: authfld.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: os $ $Date: 2001-02-14 10:40:53 $
+ *  last change: $Author: jp $ $Date: 2001-04-26 20:46:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -223,7 +223,7 @@ SwAuthorityFieldType::SwAuthorityFieldType( const SwAuthorityFieldType& rFType)
  --------------------------------------------------*/
 SwAuthorityFieldType::~SwAuthorityFieldType()
 {
-    DBG_ASSERT(!m_pDataArr->Count(), "Array is not empty")
+//  DBG_ASSERT(!m_pDataArr->Count(), "Array is not empty")
     m_pSortKeyArr->DeleteAndDestroy(0, m_pSortKeyArr->Count());
     delete m_pSortKeyArr;
     delete m_pSequArr;
