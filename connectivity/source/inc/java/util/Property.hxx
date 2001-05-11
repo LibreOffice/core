681d15793f86936291aa2616771433f7288e6a16
Ocke Janssen <oj@openoffice.org>
2001-05-11 08:25:55 +0000

    new line missing

--- connectivity/source/inc/java/util/Property.hxx
+++ connectivity/source/inc/java/util/Property.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Property.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: oj $ $Date: 2001-05-09 12:59:06 $
+ *  last change: $Author: oj $ $Date: 2001-05-11 09:25:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
