b239a4467a8eb13dc28c0a361ae00c76589cb67a
jp <jp@openoffice.org>
2001-05-11 12:02:27 +0000

    comphelper lib added

--- starmath/util/makefile.mk
+++ starmath/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2000-11-22 12:29:32 $
+#   last change: $Author: jp $ $Date: 2001-05-11 13:02:27 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -107,7 +107,8 @@ SHL1STDLIBS= \
             $(SVXLIB) \
             $(SALLIB) \
             $(VOSLIB) \
-            $(UCBHELPERLIB)
+                        $(UCBHELPERLIB) \
+                        $(COMPHELPERLIB)
 
 SHL1DEPN=               makefile.mk \
                         $(MISC)$/$(SHL1TARGET).flt
