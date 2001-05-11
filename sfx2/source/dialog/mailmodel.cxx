74cd4013081390e5131783182bda96aa22c24b9f
Dirk Völzke <dv@openoffice.org>
2001-05-11 14:02:29 +0000

    #86645# Send document via creating an url and dispatching it

--- sfx2/source/dialog/mailmodel.cxx
+++ sfx2/source/dialog/mailmodel.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mailmodel.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: pb $ $Date: 2001-02-07 09:46:53 $
+ *  last change: $Author: dv $ $Date: 2001-05-11 15:02:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,20 +61,32 @@
 
 // includes --------------------------------------------------------------
 
-#ifndef _COM_SUN_STAR_IO_XACTIVEDATASINK_HPP_
+#ifndef  _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#ifndef  _COM_SUN_STAR_FRAME_XFRAME_HPP_
+#include <com/sun/star/frame/XFrame.hpp>
+#endif
+#ifndef  _COM_SUN_STAR_IO_XACTIVEDATASINK_HPP_
 #include <com/sun/star/io/XActiveDataSink.hpp>
 #endif
-#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#ifndef  _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #endif
-#ifndef _COM_SUN_STAR_UCB_XDATACONTAINER_HPP_
+#ifndef  _COM_SUN_STAR_MOZILLA_XPLUGININSTANCE_HPP_
+#include <com/sun/star/mozilla/XPluginInstance.hpp>
+#endif
+#ifndef  _COM_SUN_STAR_UCB_XDATACONTAINER_HPP_
 #include <com/sun/star/ucb/XDataContainer.hpp>
 #endif
-#ifndef _COM_SUN_STAR_UCB_COMMANDABORTEDEXCEPTION_HPP_
+#ifndef  _COM_SUN_STAR_UCB_COMMANDABORTEDEXCEPTION_HPP_
 #include <com/sun/star/ucb/CommandAbortedException.hpp>
 #endif
-#ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
+#ifndef  _COM_SUN_STAR_UNO_REFERENCE_H_
 #include <com/sun/star/uno/Reference.h>
+#endif
+#ifndef  _COM_SUN_STAR_UTIL_XURLTRANSFORMER_HPP_
+#include <com/sun/star/util/XURLTransformer.hpp>
 #endif
 
 #ifndef _UNOTOOLS_STREAMHELPER_HXX_
@@ -94,16 +106,33 @@
 #include "sfxsids.hrc"
 
 #include <unotools/tempfile.hxx>
+#include <vcl/svapp.hxx>
 #include <svtools/stritem.hxx>
 #include <svtools/eitem.hxx>
 #include <comphelper/processfactory.hxx>
 #include <ucbhelper/content.hxx>
 #include <tools/urlobj.hxx>
 
-using namespace com::sun::star::io;
-using namespace com::sun::star::lang;
-using namespace com::sun::star::ucb;
-using namespace com::sun::star::uno;
+// --------------------------------------------------------------
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::frame;
+using namespace ::com::sun::star::io;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::mozilla;
+using namespace ::com::sun::star::ucb;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::util;
+using namespace ::rtl;
+
+// --------------------------------------------------------------
+#define SEND_MAIL_COMMAND   "cmd.executeMail=1"
+#define SEND_MAIL_FROM      "MESSAGE_FROM="
+#define SEND_MAIL_TO        "MESSAGE_TO="
+#define SEND_MAIL_CC        "MESSAGE_CC="
+#define SEND_MAIL_BCC       "MESSAGE_BCC="
+#define SEND_MAIL_SUBJECT   "MESSAGE_SUBJECT="
+#define SEND_MAIL_FILEURL   "selectedURL="
+#define SEND_MAIL_SEP       '&'
 
 // class AddressList_Impl ------------------------------------------------
 
@@ -161,6 +190,7 @@ sal_Bool SfxMailModel_Impl::SaveDocument( String& rFileName, String& rType )
             SfxFilterMatcher aFilterMatcher( xDocShell->GetFactory().GetFilterContainer() );
             pFilter = aFilterMatcher.GetDefaultFilter();
         }
+#if 0
         // create temp file name with leading chars and extension
         String aLeadingStr( DEFINE_CONST_UNICODE("smail") );
         sal_Bool bHasName = xDocShell->HasName();
@@ -175,25 +205,30 @@ sal_Bool SfxMailModel_Impl::SaveDocument( String& rFileName, String& rType )
         }
 
         ::utl::TempFile aTempFile( aLeadingStr, pExt );
-        rFileName = aTempFile.GetURL();
 
+        delete pExt;
+
+        rFileName = aTempFile.GetURL();
         // save document to temp file
         SfxStringItem aFileName( SID_FILE_NAME, rFileName );
         SfxBoolItem aPicklist( SID_PICKLIST, FALSE );
-        SfxBoolItem aSaveTo( SID_SAVETO, TRUE );
+        SfxBoolItem aSaveTo( SID_SAVETO, FALSE );
+#endif
         SfxStringItem* pFilterName = NULL;
         if ( pFilter && bHasFilter )
             pFilterName = new SfxStringItem( SID_FILTER_NAME, pFilter->GetName() );
-        pDisp->Execute( SID_SAVEASDOC, SFX_CALLMODE_SYNCHRON | SFX_CALLMODE_API,
-                        &aFileName, &aPicklist, &aSaveTo, pFilterName, 0L );
+        pDisp->Execute( SID_SAVEDOC, SFX_CALLMODE_SYNCHRON,
+                        pFilterName, 0L );
+
+        rFileName = xDocShell->GetMedium()->GetName();
+
         delete pFilterName;
         if ( pFilter )
         {
             // detect content type and expand with the file name
             rType = pFilter->GetMimeType();
             rType += DEFINE_CONST_UNICODE("; name =\"");
-            INetURLObject aFileObj = bHasName ? xDocShell->GetMedium()->GetURLObject()
-                                              : INetURLObject( rFileName );
+            INetURLObject aFileObj = xDocShell->GetMedium()->GetURLObject();
             rType += aFileObj.getName( INetURLObject::LAST_SEGMENT, true, INetURLObject::DECODE_WITH_CHARSET );
             rType += '\"';
         }
@@ -281,121 +316,206 @@ sal_Bool SfxMailModel_Impl::Send()
 {
     sal_Bool bSend = sal_False;
     String aFileName, aContentType;
-    String aFileNameText, aContentTypeText;
+
     if ( SaveDocument( aFileName, aContentType ) )
     {
-        SvStream* pStream = new SvFileStream( aFileName, STREAM_STD_READ );
-        Reference < XMultiServiceFactory > xMgr = ::comphelper::getProcessServiceFactory();
-        Reference < XDataContainer > xData(
-            xMgr->createInstance( ::rtl::OUString::createFromAscii("com.sun.star.ucb.DataContainer") ), UNO_QUERY );
-        if ( xData.is() )
+        SfxFrame* pViewFrm = mpBindings->GetDispatcher_Impl()->GetFrame()->GetFrame();
+        Reference < XPluginInstance > xPlugin;
+
+        if ( pViewFrm )
+            xPlugin = Reference < XPluginInstance > ( pViewFrm->GetFrameInterface(), UNO_QUERY );
+
+        if ( xPlugin.is() )
         {
-            xData->setContentType( aContentType );
-            SvLockBytesRef xLockBytes = new SvLockBytes( pStream );
-            Reference < XInputStream > xStream = new ::utl::OInputStreamHelper( xLockBytes, 8192 );
-            Reference < XActiveDataSink > xSink( xData, UNO_QUERY );
-            if ( xSink.is() )
-                xSink->setInputStream( xStream );
-            else
+            OUString aURL, aHostName, aPrefix;
+            USHORT   nPort;
+
+            xPlugin->getHttpServerURL( aHostName, nPort, aPrefix );
+
+            aURL = aHostName;
+            aURL += aPrefix;
+
+            INetURLObject aObj( aURL, INET_PROT_HTTP );
+            aObj.SetPort( nPort );
+
+            // Create the parameter
+            ULONG i, nCount;
+            OUString aSep( SEND_MAIL_SEP );
+            OUString aParam( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_COMMAND ) );
+            aParam += aSep;
+
+            aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_FROM ) );
+            aParam += maFromAddress;
+            aParam += aSep;
+
+            nCount = mpToList ? mpToList->Count() : 0;
+            for ( i = 0; i < nCount; ++i )
             {
-                try
-                {
-                    Sequence< sal_Int8 > aData( 65536 );
-                    sal_Int8* pData = aData.getArray();
-                    Sequence< sal_Int8 > aBuffer;
-                    sal_Int32 nPos = 0, nRead = xStream->readSomeBytes( aBuffer, 65536 );
-                    while ( nRead > 0 )
-                    {
-                        if ( aData.getLength() < ( nPos + nRead ) )
-                            aData.realloc( nPos + nRead );
-
-                        aBuffer.realloc( nRead );
-                        rtl_copyMemory( (void*)pData, (const void*)aBuffer.getConstArray(), nRead );
-                        pData += nRead;
-                        nPos += nRead;
-
-                        aBuffer.realloc( 0 );
-                        nRead = xStream->readSomeBytes( aBuffer, 65536 );
-                    }
-
-                    aData.realloc( nPos );
-                    xData->setData( aData );
-                }
-                catch ( NotConnectedException const & ) {}
-                catch ( BufferSizeExceededException const & ) {}
-                catch ( IOException const & ) {}
+                aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_TO ) );
+                aParam += *mpToList->GetObject(i);;
+                aParam += aSep;
             }
 
-            try
+            nCount = mpCcList ? mpCcList->Count() : 0;
+            for ( i = 0; i < nCount; ++i )
             {
-                Reference< XCommandEnvironment > aCmdEnv;
-                String aURL = ::rtl::OUString( DEFINE_CONST_UNICODE("vnd.sun.staroffice.out:///~") );
-                ::ucb::Content aOutbox( aURL, aCmdEnv );
-                ::ucb::Content aMessage( aURL, aCmdEnv );
-
-                sal_Int32 nIdx = 0, nCount = 4;
-                sal_Bool bCc = sal_False, bBcc = sal_False;
-                if ( mpCcList && mpCcList->Count() > 0 )
-                {
-                    bCc = sal_True;
-                    nCount++;
-                }
-                if ( mpBccList && mpBccList->Count() > 0 )
-                {
-                    bBcc = sal_True;
-                    nCount++;
-                }
-                Sequence < ::rtl::OUString > aNamesList(nCount);
-                ::rtl::OUString* pNames = aNamesList.getArray();
-                pNames[nIdx++] = ::rtl::OUString::createFromAscii( "Title" );
-                pNames[nIdx++] = ::rtl::OUString::createFromAscii( "MessageFrom" );
-                pNames[nIdx++] = ::rtl::OUString::createFromAscii( "MessageTo" );
-                pNames[nIdx++] = ::rtl::OUString::createFromAscii( "MessageBody" );
-                if ( bCc )
-                    pNames[nIdx++] = ::rtl::OUString::createFromAscii( "MessageCC" );
-                if ( bBcc )
-                    pNames[nIdx++] = ::rtl::OUString::createFromAscii( "MessageBCC" );
-
-                nIdx = 0;
-                Sequence < Any > aValuesList(nCount);
-                Any* pValues = aValuesList.getArray();
-                pValues[nIdx++] = makeAny( ::rtl::OUString( maSubject ) );
-                pValues[nIdx++] = makeAny( ::rtl::OUString( maFromAddress ) );
-                String aValueList;
-                MakeValueList( mpToList, aValueList );
-                pValues[nIdx++] = makeAny( ::rtl::OUString( aValueList ) );
-                pValues[nIdx++] = makeAny( xData );
-                if ( bCc )
-                {
-                    MakeValueList( mpCcList, aValueList );
-                    pValues[nIdx++] = makeAny( ::rtl::OUString( aValueList ) );
-                }
-                if ( bBcc )
-                {
-                    MakeValueList( mpBccList, aValueList );
-                    pValues[nIdx++] = makeAny( ::rtl::OUString( aValueList ) );
-                }
-
-                bSend = aOutbox.insertNewContent(
-                    ::rtl::OUString( DEFINE_CONST_UNICODE("application/vnd.sun.staroffice.message") ),
-                    aNamesList, aValuesList, aMessage );
+                aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_CC ) );
+                aParam += *mpCcList->GetObject(i);;
+                aParam += aSep;
             }
-            catch( com::sun::star::ucb::ContentCreationException& )
+
+            nCount = mpBccList ? mpBccList->Count() : 0;
+            for ( i = 0; i < nCount; ++i )
             {
-                DBG_ERRORFILE( "ContentCreationException" );
+                aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_BCC ) );
+                aParam += *mpBccList->GetObject(i);;
+                aParam += aSep;
             }
-            catch( CommandAbortedException& e )
+
+            aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_SUBJECT ) );
+            aParam += maSubject;
+            aParam += aSep;
+
+            aParam += OUString( RTL_CONSTASCII_USTRINGPARAM( SEND_MAIL_FILEURL ) );
+            aParam += aFileName;
+            aParam += aSep;
+
+            aObj.SetParam( aParam, INetURLObject::ENCODE_ALL );
+
+            // now we dispatch the new created URL so the document will be send.
+            aURL = aObj.GetMainURL( INetURLObject::NO_DECODE );
+
+            URL aTargetURL;
+            aTargetURL.Complete = aURL;
+            Reference < XURLTransformer > xTrans( ::comphelper::getProcessServiceFactory()->createInstance( OUString::createFromAscii("com.sun.star.util.URLTransformer" )), UNO_QUERY );
+            xTrans->parseStrict( aTargetURL );
+
+            Reference < XDispatchProvider > xProv( xPlugin, UNO_QUERY );
+            Reference < XDispatch > xDisp;
+            if ( xProv.is() )
+                xDisp = xProv->queryDispatch( aTargetURL, OUString(), 0 );
+
+            if ( xDisp.is() )
             {
-                ByteString aError( UniString( e.Message ), RTL_TEXTENCODING_MS_1252 );
-                DBG_ERRORFILE( aError.GetBuffer() );
-            }
-            catch( Exception& )
-            {
-                DBG_ERRORFILE( "Any other exception" );
+                Sequence < PropertyValue > aArgs;
+                xDisp->dispatch( aTargetURL, aArgs );
+                bSend = sal_True;
             }
         }
+        else
+        {
+            SvStream* pStream = new SvFileStream( aFileName, STREAM_STD_READ );
+            Reference < XMultiServiceFactory > xMgr = ::comphelper::getProcessServiceFactory();
+            Reference < XDataContainer > xData(
+                xMgr->createInstance( OUString::createFromAscii("com.sun.star.ucb.DataContainer") ), UNO_QUERY );
+            if ( xData.is() )
+            {
+                xData->setContentType( aContentType );
+                SvLockBytesRef xLockBytes = new SvLockBytes( pStream );
+                Reference < XInputStream > xStream = new ::utl::OInputStreamHelper( xLockBytes, 8192 );
+                Reference < XActiveDataSink > xSink( xData, UNO_QUERY );
+                if ( xSink.is() )
+                    xSink->setInputStream( xStream );
+                else
+                {
+                    try
+                    {
+                        Sequence< sal_Int8 > aData( 65536 );
+                        sal_Int8* pData = aData.getArray();
+                        Sequence< sal_Int8 > aBuffer;
+                        sal_Int32 nPos = 0, nRead = xStream->readSomeBytes( aBuffer, 65536 );
+                        while ( nRead > 0 )
+                        {
+                            if ( aData.getLength() < ( nPos + nRead ) )
+                                aData.realloc( nPos + nRead );
 
-        delete pStream;
+                            aBuffer.realloc( nRead );
+                            rtl_copyMemory( (void*)pData, (const void*)aBuffer.getConstArray(), nRead );
+                            pData += nRead;
+                            nPos += nRead;
+
+                            aBuffer.realloc( 0 );
+                            nRead = xStream->readSomeBytes( aBuffer, 65536 );
+                        }
+
+                        aData.realloc( nPos );
+                        xData->setData( aData );
+                    }
+                    catch ( NotConnectedException const & ) {}
+                    catch ( BufferSizeExceededException const & ) {}
+                    catch ( IOException const & ) {}
+                }
+
+                try
+                {
+                    Reference< XCommandEnvironment > aCmdEnv;
+                    String aURL = OUString( DEFINE_CONST_UNICODE("vnd.sun.staroffice.out:///~") );
+                    ::ucb::Content aOutbox( aURL, aCmdEnv );
+                    ::ucb::Content aMessage( aURL, aCmdEnv );
+
+                    sal_Int32 nIdx = 0, nCount = 4;
+                    sal_Bool bCc = sal_False, bBcc = sal_False;
+                    if ( mpCcList && mpCcList->Count() > 0 )
+                    {
+                        bCc = sal_True;
+                        nCount++;
+                    }
+                    if ( mpBccList && mpBccList->Count() > 0 )
+                    {
+                        bBcc = sal_True;
+                        nCount++;
+                    }
+                    Sequence < OUString > aNamesList(nCount);
+                    OUString* pNames = aNamesList.getArray();
+                    pNames[nIdx++] = OUString::createFromAscii( "Title" );
+                    pNames[nIdx++] = OUString::createFromAscii( "MessageFrom" );
+                    pNames[nIdx++] = OUString::createFromAscii( "MessageTo" );
+                    pNames[nIdx++] = OUString::createFromAscii( "MessageBody" );
+                    if ( bCc )
+                        pNames[nIdx++] = OUString::createFromAscii( "MessageCC" );
+                    if ( bBcc )
+                        pNames[nIdx++] = OUString::createFromAscii( "MessageBCC" );
+
+                    nIdx = 0;
+                    Sequence < Any > aValuesList(nCount);
+                    Any* pValues = aValuesList.getArray();
+                    pValues[nIdx++] = makeAny( OUString( maSubject ) );
+                    pValues[nIdx++] = makeAny( OUString( maFromAddress ) );
+                    String aValueList;
+                    MakeValueList( mpToList, aValueList );
+                    pValues[nIdx++] = makeAny( OUString( aValueList ) );
+                    pValues[nIdx++] = makeAny( xData );
+                    if ( bCc )
+                    {
+                        MakeValueList( mpCcList, aValueList );
+                        pValues[nIdx++] = makeAny( OUString( aValueList ) );
+                    }
+                    if ( bBcc )
+                    {
+                        MakeValueList( mpBccList, aValueList );
+                        pValues[nIdx++] = makeAny( OUString( aValueList ) );
+                    }
+
+                    bSend = aOutbox.insertNewContent(
+                        OUString( DEFINE_CONST_UNICODE("application/vnd.sun.staroffice.message") ),
+                        aNamesList, aValuesList, aMessage );
+                }
+                catch( com::sun::star::ucb::ContentCreationException& )
+                {
+                    DBG_ERRORFILE( "ContentCreationException" );
+                }
+                catch( CommandAbortedException& e )
+                {
+                    ByteString aError( UniString( e.Message ), RTL_TEXTENCODING_MS_1252 );
+                    DBG_ERRORFILE( aError.GetBuffer() );
+                }
+                catch( Exception& )
+                {
+                    DBG_ERRORFILE( "Any other exception" );
+                }
+            }
+            delete pStream;
+        }
     }
 
     return bSend;
