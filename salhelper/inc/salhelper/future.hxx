b2000e8491f044d80c077dc007457469c2b3fbaf
Matthias Huetsch <mhu@openoffice.org>
2001-05-14 10:52:34 +0000

    Template class salhelper::Future<T>.

---
+++ salhelper/inc/salhelper/future.hxx
@@ -0,0 +1,164 @@
+/*************************************************************************
+ *
+ *  $RCSfile: future.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: mhu $ $Date: 2001-05-14 11:52:34 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SALHELPER_FUTURE_HXX_
+#define _SALHELPER_FUTURE_HXX_
+
+#ifndef _SAL_TYPES_H_
+#include <sal/types.h>
+#endif
+
+#ifndef _OSL_DIAGNOSE_H_
+#include <osl/diagnose.h>
+#endif
+#ifndef _OSL_CONDITN_HXX_
+#include <osl/conditn.hxx>
+#endif
+
+#ifndef _SALHELPER_REFOBJ_HXX_
+#include <salhelper/refobj.hxx>
+#endif
+
+namespace salhelper
+{
+
+//----------------------------------------------------------------------------
+
+#ifndef SALHELPER_COPYCTOR_API
+#define SALHELPER_COPYCTOR_API(C) C (const C&); C& operator= (const C&)
+#endif
+
+//----------------------------------------------------------------------------
+
+template<class value_type>
+class FutureValue : protected osl::Condition
+{
+    /** Representation.
+     */
+    value_type m_aValue;
+
+    /** Not implemented.
+     */
+    SALHELPER_COPYCTOR_API(FutureValue<value_type>);
+
+public:
+    inline FutureValue (const value_type& value = value_type()) SAL_THROW(())
+        : m_aValue (value)
+    {
+        Condition::reset();
+    }
+
+    inline ~FutureValue() SAL_THROW(())
+    {}
+
+    inline sal_Bool is() const SAL_THROW(())
+    {
+        return const_cast<FutureValue*>(this)->check();
+    }
+
+    inline void set (const value_type& value) SAL_THROW(())
+    {
+        m_aValue = value;
+        Condition::set();
+    }
+
+    inline value_type& get() SAL_THROW(())
+    {
+        Condition::wait();
+        return m_aValue;
+    }
+};
+
+//----------------------------------------------------------------------------
+
+template<class value_type>
+class Future : public salhelper::ReferenceObject
+{
+    /** Representation.
+     */
+    FutureValue<value_type> m_aValue;
+
+    /** Not implemented.
+     */
+    SALHELPER_COPYCTOR_API(Future<value_type>);
+
+public:
+    inline Future (const value_type& value = value_type()) SAL_THROW(())
+        : m_aValue (value)
+    {}
+
+    inline void set (const value_type& value) SAL_THROW(())
+    {
+        OSL_PRECOND(!m_aValue.is(), "Future::set(): value already set");
+        m_aValue.set (value);
+    }
+
+    inline value_type& get() SAL_THROW(())
+    {
+        return m_aValue.get();
+    }
+};
+
+//----------------------------------------------------------------------------
+
+} // namespace salhelper
+
+#endif /* !_SALHELPER_FUTURE_HXX_ */
