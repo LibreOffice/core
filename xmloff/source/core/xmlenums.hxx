24e6908fa5f97a26c84cccb3e7eca3be548aaecb
Martin Gallwey <mtg@openoffice.org>
2001-05-16 10:46:28 +0000

    #87071# moved forbidden character enums to here and added symbol descriptor enums

---
+++ xmloff/source/core/xmlenums.hxx
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlenums.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: mtg $ $Date: 2001-05-16 11:46:28 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLENUMS_HXX_
+#define _XMLENUMS_HXX_
+
+enum XMLForbiddenCharactersEnum
+{
+    XML_FORBIDDEN_CHARACTER_LANGUAGE,
+    XML_FORBIDDEN_CHARACTER_COUNTRY,
+    XML_FORBIDDEN_CHARACTER_VARIANT,
+    XML_FORBIDDEN_CHARACTER_BEGIN_LINE,
+    XML_FORBIDDEN_CHARACTER_END_LINE,
+    XML_FORBIDDEN_CHARACTER_MAX
+};
+
+enum XMLSymbolDescriptorsEnum
+{
+    XML_SYMBOL_DESCRIPTOR_NAME,
+    XML_SYMBOL_DESCRIPTOR_EXPORT_NAME,
+    XML_SYMBOL_DESCRIPTOR_SYMBOL_SET,
+    XML_SYMBOL_DESCRIPTOR_CHARACTER,
+    XML_SYMBOL_DESCRIPTOR_FONT_NAME,
+    XML_SYMBOL_DESCRIPTOR_CHAR_SET,
+    XML_SYMBOL_DESCRIPTOR_FAMILY,
+    XML_SYMBOL_DESCRIPTOR_PITCH,
+    XML_SYMBOL_DESCRIPTOR_WEIGHT,
+    XML_SYMBOL_DESCRIPTOR_ITALIC,
+    XML_SYMBOL_DESCRIPTOR_MAX
+};
+#endif
