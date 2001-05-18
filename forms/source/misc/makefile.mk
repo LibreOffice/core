273991770f1bc8ac0428cff491079b954470107c
Frank Schönheit <fs@openoffice.org>
2001-05-18 13:48:32 +0000

    #86712# +limitedformats

--- forms/source/misc/makefile.mk
+++ forms/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:29:06 $
+#   last change: $Author: fs $ $Date: 2001-05-18 14:48:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,7 +78,8 @@ INCPRE+=$(SOLARINCDIR)$/offuh
 
 # --- Files -------------------------------------
 
-SLOFILES=	$(SLO)$/property.obj \
+SLOFILES=	$(SLO)$/limitedformats.obj \
+            $(SLO)$/property.obj \
             $(SLO)$/services.obj \
             $(SLO)$/InterfaceContainer.obj \
             $(SLO)$/ids.obj \
