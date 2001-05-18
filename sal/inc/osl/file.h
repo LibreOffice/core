5366c9e159b7272ace032c34229662d089bfd3df
Patrick Luby <pluby@openoffice.org>
2001-05-18 18:30:16 +0000

    Restored version 1.9 as the real problem with Linux was in sal/osl/unx/file.c.

--- sal/inc/osl/file.h
+++ sal/inc/osl/file.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: file.h,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: pluby $ $Date: 2001-05-18 15:52:46 $
+ *  last change: $Author: pluby $ $Date: 2001-05-18 19:30:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -628,6 +628,8 @@ oslFileError SAL_CALL osl_openFile( rtl_uString *strPath, oslFileHandle *pHandle
     @see    osl_openFile
     @see    osl_getFilePos
 */
+
+oslFileError SAL_CALL osl_setFilePos( oslFileHandle Handle, sal_uInt32 uHow, sal_Int64 uPos );
 
 
 /** Retrieves the current position of the internal pointer of an open file.
