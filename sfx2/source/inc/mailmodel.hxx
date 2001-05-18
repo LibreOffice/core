af816c5daeb4f361252682cf37cba18ceb02a217
Mathias Bauer <mba@openoffice.org>
2001-05-18 12:37:36 +0000

    #86645#: send mails without UI

---
+++ sfx2/source/inc/mailmodel.hxx
@@ -0,0 +1,121 @@
+/*************************************************************************
+ *
+ *  $RCSfile: mailmodel.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: mba $ $Date: 2001-05-18 13:37:36 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef INCLUDED_SFX_MAILMODEL_HXX
+#define INCLUDED_SFX_MAILMODEL_HXX
+
+#include "objsh.hxx"
+
+// class SfxMailModel_Impl -----------------------------------------------
+
+class AddressList_Impl;
+class SfxBindings;
+
+class SfxMailModel_Impl
+{
+public:
+    enum MailPriority
+    {
+        PRIO_HIGHEST,
+        PRIO_HIGH,
+        PRIO_NORMAL,
+        PRIO_LOW,
+        PRIO_LOWEST
+    };
+
+    enum AddressRole
+    {
+        ROLE_TO,
+        ROLE_CC,
+        ROLE_BCC
+    };
+
+private:
+    AddressList_Impl*   mpToList;
+    AddressList_Impl*   mpCcList;
+    AddressList_Impl*   mpBccList;
+    SfxBindings*        mpBindings;
+    String              maFromAddress;
+    String              maSubject;
+    MailPriority        mePriority;
+
+    sal_Bool            mbLoadDone;
+
+    void                ClearList( AddressList_Impl* pList );
+    void                MakeValueList( AddressList_Impl* pList, String& rValueList );
+    BOOL                SaveDocument( String& rFileName, String& rType );
+
+    DECL_LINK( DoneHdl, void* );
+
+public:
+    SfxMailModel_Impl( SfxBindings* pBinds );
+    ~SfxMailModel_Impl();
+
+    void                AddAddress( const String& rAddress, AddressRole eRole );
+    void                SetFromAddress( const String& rAddress )    { maFromAddress = rAddress; }
+    void                SetSubject( const String& rSubject )        { maSubject = rSubject; }
+    void                SetPriority( MailPriority ePrio )           { mePriority = ePrio; }
+
+    sal_Bool            Send();
+};
+
+#endif // INCLUDED_SFX_MAILMODEL_HXX
+
+
