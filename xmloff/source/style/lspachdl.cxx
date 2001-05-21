7501b091fb890d14a7af3209d783bdba18138964
Michael Brauer <mib@openoffice.org>
2001-05-21 05:54:15 +0000

    #87195#: import of fixed line height

--- xmloff/source/style/lspachdl.cxx
+++ xmloff/source/style/lspachdl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: lspachdl.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:07:05 $
+ *  last change: $Author: mib $ $Date: 2001-05-21 06:54:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,7 @@ sal_Bool XMLLineHeightHdl::importXML( const OUString& rStrImpValue, uno::Any& rV
             return sal_False;
         aLSp.Height = nTemp;
     }
-    else if( rStrImpValue.compareToAscii( sXML_casemap_normal ) )
+    else if( rStrImpValue.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM(sXML_casemap_normal) ) )
     {
         aLSp.Mode = style::LineSpacingMode::PROP;
         aLSp.Height = 100;
