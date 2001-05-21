ef68adb44e916789f2748cedc58ea77c7384b02d
Andreas Schlüns <as@openoffice.org>
2001-05-21 05:09:40 +0000

    #86171# new query for graphic filters

--- framework/inc/queries.h
+++ framework/inc/queries.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: queries.h,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: as $ $Date: 2001-04-04 13:28:32 $
+ *  last change: $Author: as $ $Date: 2001-05-21 06:09:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -93,16 +93,6 @@ namespace framework{
                     They are used by our FilterFactory or ouer TypeDetection to return
                     subsets of our cached configuration.
 *//*-*************************************************************************************************************/
-
-#define FILTERQUERY_TEXTDOCUMENT                        DECLARE_ASCII("_filterquery_textdocument"                       )
-#define FILTERQUERY_WEBDOCUMENT                         DECLARE_ASCII("_filterquery_webdocument"                        )
-#define FILTERQUERY_GLOBALDOCUMENT                      DECLARE_ASCII("_filterquery_globaldocument"                     )
-#define FILTERQUERY_CHARTDOCUMENT                       DECLARE_ASCII("_filterquery_chartdocument"                      )
-#define FILTERQUERY_SPREADSHEETDOCUMENT                 DECLARE_ASCII("_filterquery_spreadsheetdocument"                )
-#define FILTERQUERY_PRESENTATIONDOCUMENT                DECLARE_ASCII("_filterquery_presentationdocument"               )
-#define FILTERQUERY_DRAWINGDOCUMENT                     DECLARE_ASCII("_filterquery_drawingdocument"                    )
-#define FILTERQUERY_FORMULARPROPERTIES                  DECLARE_ASCII("_filterquery_formulaproperties"                  )
-
 #define FILTERQUERY_TEXTDOCUMENT_WITHDEFAULT            DECLARE_ASCII("_filterquery_textdocument_withdefault"           )
 #define FILTERQUERY_WEBDOCUMENT_WITHDEFAULT             DECLARE_ASCII("_filterquery_webdocument_withdefault"            )
 #define FILTERQUERY_GLOBALDOCUMENT_WITHDEFAULT          DECLARE_ASCII("_filterquery_globaldocument_withdefault"         )
@@ -111,13 +101,12 @@ namespace framework{
 #define FILTERQUERY_PRESENTATIONDOCUMENT_WITHDEFAULT    DECLARE_ASCII("_filterquery_presentationdocument_withdefault"   )
 #define FILTERQUERY_DRAWINGDOCUMENT_WITHDEFAULT         DECLARE_ASCII("_filterquery_drawingdocument_withdefault"        )
 #define FILTERQUERY_FORMULARPROPERTIES_WITHDEFAULT      DECLARE_ASCII("_filterquery_formulaproperties_withdefault"      )
-
-#define FILTERQUERY_DEFAULTFILTER                       DECLARE_ASCII("_filterquery_defaultfilter"                      )
+#define FILTERQUERY_GRAPHICFILTERS                      DECLARE_ASCII("_filterquery_graphicfilters"                     )
+#define FILTERQUERY_DEFAULTFILTERS                      DECLARE_ASCII("_filterquery_defaultfilters"                     )
 
 /*-************************************************************************************************************//**
     @short          These values describe properties which represent all items of a query.
 *//*-*************************************************************************************************************/
-
 // compare it with string Filter.DocumentService
 #define DETECTPROPERTY_TEXTDOCUMENT                     DECLARE_ASCII("com.sun.star.text.TextDocument"                  )
 #define DETECTPROPERTY_WEBDOCUMENT                      DECLARE_ASCII("com.sun.star.text.WebDocument"                   )
@@ -128,8 +117,12 @@ namespace framework{
 #define DETECTPROPERTY_DRAWINGDOCUMENT                  DECLARE_ASCII("com.sun.star.drawing.DrawingDocument"            )
 #define DETECTPROPERTY_FORMULARPROPERTIES               DECLARE_ASCII("com.sun.star.formula.FormulaProperties"          )
 
-// compare it with string Filter.DocumentService
-#define DETECTPROPERTY_DEFAULTFILTER                    FILTERFLAG_DEFAULT
+// compare it with first string of Filter.UserData!
+// (temp. special!)
+#define DETECTPROPERTY_GRAPHICFILTERS                   DECLARE_ASCII("OO")
+
+// compare it with int Filter.Flags
+#define DETECTPROPERTY_DEFAULTFILTERS                   FILTERFLAG_DEFAULT
 
 //_________________________________________________________________________________________________________________
 //  methods
@@ -138,18 +131,9 @@ namespace framework{
 /*-************************************************************************************************************//**
     @short          This method checks if given string match to a supported query.
 *//*-*************************************************************************************************************/
-
 inline sal_Bool c_isQuery( const ::rtl::OUString& sQuery )
 {
     return  (
-                ( sQuery == FILTERQUERY_TEXTDOCUMENT                        )   ||
-                ( sQuery == FILTERQUERY_WEBDOCUMENT                         )   ||
-                ( sQuery == FILTERQUERY_GLOBALDOCUMENT                      )   ||
-                ( sQuery == FILTERQUERY_CHARTDOCUMENT                       )   ||
-                ( sQuery == FILTERQUERY_SPREADSHEETDOCUMENT                 )   ||
-                ( sQuery == FILTERQUERY_PRESENTATIONDOCUMENT                )   ||
-                ( sQuery == FILTERQUERY_DRAWINGDOCUMENT                     )   ||
-                ( sQuery == FILTERQUERY_FORMULARPROPERTIES                  )   ||
                 ( sQuery == FILTERQUERY_TEXTDOCUMENT_WITHDEFAULT            )   ||
                 ( sQuery == FILTERQUERY_WEBDOCUMENT_WITHDEFAULT             )   ||
                 ( sQuery == FILTERQUERY_GLOBALDOCUMENT_WITHDEFAULT          )   ||
@@ -158,7 +142,8 @@ inline sal_Bool c_isQuery( const ::rtl::OUString& sQuery )
                 ( sQuery == FILTERQUERY_PRESENTATIONDOCUMENT_WITHDEFAULT    )   ||
                 ( sQuery == FILTERQUERY_DRAWINGDOCUMENT_WITHDEFAULT         )   ||
                 ( sQuery == FILTERQUERY_FORMULARPROPERTIES_WITHDEFAULT      )   ||
-                ( sQuery == FILTERQUERY_DEFAULTFILTER                       )
+                ( sQuery == FILTERQUERY_GRAPHICFILTERS                      )   ||
+                ( sQuery == FILTERQUERY_DEFAULTFILTERS                      )
             );
 }
 
