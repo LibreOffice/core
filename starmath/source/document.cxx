19d9a3ff57b3293d6510b6ce04072cf5d94f22b5
jp <jp@openoffice.org>
2001-05-22 14:20:15 +0000

    Bug #81916#: CopyFromClipboard with Window pointer

--- starmath/source/document.cxx
+++ starmath/source/document.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: document.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: jp $ $Date: 2001-05-21 07:48:57 $
+ *  last change: $Author: jp $ $Date: 2001-05-22 15:20:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1316,8 +1316,9 @@ void SmDocShell::Execute(SfxRequest& rReq)
 
     case SID_PASTEOBJECT:
         {
-            TransferableDataHelper aData(
-                        TransferableDataHelper::CreateFromSystemClipboard() );
+            TransferableDataHelper aData( TransferableDataHelper::
+                CreateFromSystemClipboard(pViewSh ? pViewSh->GetEditWindow()
+                                                  : 0) );
             SotStorageStreamRef xStrm;
             SotFormatStringId nId;
             if( aData.GetTransferable().is() &&
