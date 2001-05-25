2eea835a28c63057a2e08fe4339bdaca843ddf3a
Frank Schönheit <fs@openoffice.org>
2001-05-25 09:56:23 +0000

    initial checkin - driver rerouting it's connect through the connection pool

---
+++ connectivity/source/cpool/ZDriverWrapper.cxx
@@ -0,0 +1,172 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ZDriverWrapper.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2001-05-25 10:56:17 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _CONNECTIVITY_CPOOL_ZDRIVERWRAPPER_HXX_
+#include "ZDriverWrapper.hxx"
+#endif
+#ifndef _CONNECTIVITY_ZCONNECTIONPOOL_HXX_
+#include "ZConnectionPool.hxx"
+#endif
+#ifndef _OSL_DIAGNOSE_H_
+#include <osl/diagnose.h>
+#endif
+
+//........................................................................
+namespace connectivity
+{
+//........................................................................
+
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::sdbc;
+    using namespace ::com::sun::star::beans;
+
+    //====================================================================
+    //= ODriverWrapper
+    //====================================================================
+    //--------------------------------------------------------------------
+    ODriverWrapper::ODriverWrapper( Reference< XAggregation >& _rxAggregateDriver, OConnectionPool* _pPool )
+        :m_pConnectionPool(_pPool)
+    {
+        OSL_ENSURE(_rxAggregateDriver.is(), "ODriverWrapper::ODriverWrapper: invalid aggregate!");
+        OSL_ENSURE(m_pConnectionPool, "ODriverWrapper::ODriverWrapper: invalid connection pool!");
+
+        if (m_pConnectionPool)
+            m_pConnectionPool->acquire();
+
+        osl_incrementInterlockedCount( &m_refCount );
+        if (_rxAggregateDriver.is())
+        {
+            // transfer the (one and only) real ref to the aggregate to our member
+            m_xDriverAggregate = _rxAggregateDriver;
+            _rxAggregateDriver = NULL;
+
+            // a second "real" reference
+            m_xDriver = Reference< XDriver >(m_xDriverAggregate, UNO_QUERY);
+            OSL_ENSURE(m_xDriver.is(), "ODriverWrapper::ODriverWrapper: invalid aggregate (no XDriver)!");
+
+            // set ourself as delegator
+            m_xDriverAggregate->setDelegator( static_cast< XWeak* >( this ) );
+        }
+        osl_decrementInterlockedCount( &m_refCount );
+    }
+
+    //--------------------------------------------------------------------
+    ODriverWrapper::~ODriverWrapper()
+    {
+        if (m_xDriverAggregate.is())
+            m_xDriverAggregate->setDelegator(NULL);
+
+        if (m_pConnectionPool)
+            m_pConnectionPool->release();
+        m_pConnectionPool = NULL;
+    }
+
+    //--------------------------------------------------------------------
+    Reference< XConnection > SAL_CALL ODriverWrapper::connect( const ::rtl::OUString& url, const Sequence< PropertyValue >& info ) throw (SQLException, RuntimeException)
+    {
+        Reference< XConnection > xConnection;
+        if (m_pConnectionPool)
+            // route this through the pool
+            xConnection = m_pConnectionPool->getConnectionWithInfo( url, info );
+        else if (m_xDriver.is())
+            xConnection = m_xDriver->connect( url, info );
+
+        return xConnection;
+    }
+
+    //--------------------------------------------------------------------
+    sal_Bool SAL_CALL ODriverWrapper::acceptsURL( const ::rtl::OUString& url ) throw (SQLException, RuntimeException)
+    {
+        return m_xDriver.is() && m_xDriver->acceptsURL(url);
+    }
+
+    //--------------------------------------------------------------------
+    Sequence< DriverPropertyInfo > SAL_CALL ODriverWrapper::getPropertyInfo( const ::rtl::OUString& url, const Sequence< PropertyValue >& info ) throw (SQLException, RuntimeException)
+    {
+        Sequence< DriverPropertyInfo > aInfo;
+        if (m_xDriver.is())
+            aInfo = m_xDriver->getPropertyInfo(url, info);
+        return aInfo;
+    }
+
+    //--------------------------------------------------------------------
+    sal_Int32 SAL_CALL ODriverWrapper::getMajorVersion(  ) throw (RuntimeException)
+    {
+        return m_xDriver.is() ? m_xDriver->getMajorVersion() : 0;
+    }
+
+    //--------------------------------------------------------------------
+    sal_Int32 SAL_CALL ODriverWrapper::getMinorVersion(  ) throw (RuntimeException)
+    {
+        return m_xDriver.is() ? m_xDriver->getMinorVersion() : 0;
+    }
+
+//........................................................................
+}   // namespace connectivity
+//........................................................................
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 25.05.01 11:10:52  fs
+ ************************************************************************/
+
---
+++ connectivity/source/cpool/ZDriverWrapper.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ZDriverWrapper.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2001-05-25 10:56:23 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _CONNECTIVITY_CPOOL_ZDRIVERWRAPPER_HXX_
+#define _CONNECTIVITY_CPOOL_ZDRIVERWRAPPER_HXX_
+
+#ifndef _COM_SUN_STAR_SDBC_XDRIVER_HPP_
+#include <com/sun/star/sdbc/XDriver.hpp>
+#endif
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_XAGGREGATION_HPP_
+#include <com/sun/star/uno/XAggregation.hpp>
+#endif
+
+//........................................................................
+namespace connectivity
+{
+//........................................................................
+
+    class OConnectionPool;
+    //====================================================================
+    //= ODriverWrapper
+    //====================================================================
+    typedef ::cppu::WeakImplHelper1 <   ::com::sun::star::sdbc::XDriver
+                                    >   ODriverWrapper_BASE;
+
+    class ODriverWrapper : public ODriverWrapper_BASE
+    {
+    protected:
+        ::com::sun::star::uno::Reference< ::com::sun::star::uno::XAggregation >
+                            m_xDriverAggregate;
+        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDriver >
+                            m_xDriver;
+        OConnectionPool*    m_pConnectionPool;
+
+    public:
+        /** creates a new wrapper for a driver
+            @param _rxAggregateDriver
+                the driver to aggregate. The object will be reset to <NULL/> when returning from the ctor.
+        */
+        ODriverWrapper(
+            ::com::sun::star::uno::Reference< ::com::sun::star::uno::XAggregation >& _rxAggregateDriver,
+            OConnectionPool* _pPool
+            );
+
+        /// dtor
+        ~ODriverWrapper();
+
+    protected:
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection > SAL_CALL connect( const ::rtl::OUString& url, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& info ) throw (::com::sun::star::sdbc::SQLException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool SAL_CALL acceptsURL( const ::rtl::OUString& url ) throw (::com::sun::star::sdbc::SQLException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::sdbc::DriverPropertyInfo > SAL_CALL getPropertyInfo( const ::rtl::OUString& url, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& info ) throw (::com::sun::star::sdbc::SQLException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getMajorVersion(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getMinorVersion(  ) throw (::com::sun::star::uno::RuntimeException);
+    };
+
+//........................................................................
+}   // namespace connectivity
+//........................................................................
+
+#endif // _CONNECTIVITY_CPOOL_ZDRIVERWRAPPER_HXX_
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 25.05.01 11:03:45  fs
+ ************************************************************************/
+
