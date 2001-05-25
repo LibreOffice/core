70b57048543aa767156b61abb3d80b05634b8900
Frank Schönheit <fs@openoffice.org>
2001-05-25 10:02:04 +0000

    #86996# +ZDriverWrapper

--- connectivity/source/cpool/makefile.mk
+++ connectivity/source/cpool/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: oj $ $Date: 2001-05-17 09:13:25 $
+#   last change: $Author: fs $ $Date: 2001-05-25 11:02:04 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -79,6 +79,7 @@ ENVCFLAGS+=/FR$(SLO)$/
 # --- Files -------------------------------------
 
 SLOFILES=\
+        $(SLO)$/ZDriverWrapper.obj			\
         $(SLO)$/ZConnectionWrapper.obj			\
         $(SLO)$/ZPooledConnection.obj			\
         $(SLO)$/ZConnectionPool.obj				\
