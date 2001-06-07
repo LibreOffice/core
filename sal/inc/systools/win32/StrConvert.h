d784228a21f7c73d0e0dd5608a887bdb65bf0c07
Oliver Braun <obr@openoffice.org>
2001-06-07 08:21:10 +0000

    #85841# added new unicode converter functions and macros

--- sal/inc/systools/win32/StrConvert.h
+++ sal/inc/systools/win32/StrConvert.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: StrConvert.h,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 15:17:15 $
+ *  last change: $Author: obr $ $Date: 2001-06-07 09:21:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 #define _STRCONVERT_H_
 
 #include <windows.h>
+#include <assert.h>
 
 #ifdef __cplusplus
 extern "C"{
@@ -72,6 +73,76 @@ int  AllocSpaceAndCopyWStr2Str( LPCWSTR lpcwstrString, DWORD nWCharsToCopy, LPST
 int  CalcLenDblNullTerminatedWStr( LPCWSTR lpcwstrString );
 int  CalcLenDblNullTerminatedStr( LPCSTR lpcstrString );
 void FreeSpaceStr( LPSTR lpszString );
+
+/* WC2MB allocates a sufficient amount of memory on stack and converts
+   the wide char parameter to multi byte string using the actual code
+   page.
+
+   @Param: wcStr - a wide char string
+           mbStr - the corresponding multi byte string
+
+   NOTE: due to the use of _alloca, this must be a macro and no function
+*/
+
+#define WC2MB( wcStr, mbStr ) \
+if( wcStr ) \
+{ \
+    int needed = WideCharToMultiByte( CP_ACP, 0, wcStr, -1, NULL, 0, NULL, NULL ); \
+    if( needed > 0 ) \
+    { \
+        int copied; \
+        mbStr = _alloca( needed * sizeof( CHAR ) ); \
+        copied = WideCharToMultiByte( CP_ACP, 0, wcStr, -1, mbStr, needed, NULL, NULL ); \
+        assert( copied == needed ); \
+    } \
+}
+
+
+/* WideCharListGetMultiByteLength
+   calculates the needed length of a corresponding the multi byte string
+   list for a wide char string list.
+
+   @Param: cp - the code page to use for convertion.
+           wcList - a double '\0' terminated wide char string list.
+*/
+
+int WideCharListGetMultiByteLength( UINT codepage, LPCWSTR wcList );
+
+/* WideCharListToMultiByteList
+   converts a double '\0' terminated list of wide char strings to a
+   multi byte string list.
+
+   @Param: cp - the code page to use for convertion.
+           wcList - a double '\0' terminated wide char string list.
+           mbList - a double '\0' terminated multi byte string list.
+           dwSize - size of buffer for multi byte string list.
+*/
+
+int WideCharListToMultiByteList( UINT codepage, LPCWSTR wcList, LPSTR mbList, DWORD dwSize );
+
+
+/* WCL2MBL allocates a sufficient amount of memory on stack and converts
+   the wide char list parameter to multi byte string list using the actual
+   code page.
+
+   @Param: wcList - a wide char string list
+           mbList - the corresponding multi byte string list
+
+   NOTE: due to the use of _alloca, this must be a macro and no function
+*/
+
+#define WCL2MBL( wcList, mbList ) \
+if( wcList ) \
+{ \
+    int needed = WideCharListGetMultiByteLength( CP_ACP, wcList ); \
+    if( needed > 0 ) \
+    { \
+        int copied; \
+        mbList = _alloca( needed * sizeof( CHAR ) ); \
+        copied = WideCharListToMultiByteList( CP_ACP, wcList, mbList, needed ); \
+        assert( copied == needed ); \
+    } \
+}
 
 #ifdef __cplusplus
 }
