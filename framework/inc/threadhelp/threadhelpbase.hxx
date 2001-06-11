c432c78bf735c6087e69a19f6466303bca5aa4a0
Andreas Schlüns <as@openoffice.org>
2001-06-11 09:10:57 +0000

    #85529# new base member to simulate different lock mechanism

---
+++ framework/inc/threadhelp/threadhelpbase.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ *  $RCSfile: threadhelpbase.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: as $ $Date: 2001-06-11 10:10:57 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#define __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_THREADHELP_LOCKHELPER_HXX_
+#include <threadhelp/lockhelper.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+//_________________________________________________________________________________________________________________
+//  namespace
+//_________________________________________________________________________________________________________________
+
+namespace framework{
+
+//_________________________________________________________________________________________________________________
+//  const
+//_________________________________________________________________________________________________________________
+
+//_________________________________________________________________________________________________________________
+//  declarations
+//_________________________________________________________________________________________________________________
+
+/*-************************************************************************************************************//**
+    @short          "baseclass" to make own classes threadsafe
+    @descr          Sometimes you must share your lock- or mutex member with any other baseclasses.
+                    And baseclasses are initialized erlier then members! That's why you should use
+                    this struct as first of your baseclasses!!!
+                    Then you will get a public member "m_aLock" which can be used by special guard implementations
+                    to make your code threadsafe.
+
+    @seealso        class LockHelper
+
+    @implements     -
+    @base           -
+
+    @devstatus      ready to use
+*//*-*************************************************************************************************************/
+struct ThreadHelpBase
+{
+    //-------------------------------------------------------------------------------------------------------------
+    //  public methods
+    //-------------------------------------------------------------------------------------------------------------
+    public:
+        ThreadHelpBase( ::vos::IMutex* pSolarMutex = NULL )
+                :   m_aLock( pSolarMutex )
+        {
+        }
+
+    //-------------------------------------------------------------------------------------------------------------
+    //  public member
+    //  Make it mutable for using in const functions!
+    //-------------------------------------------------------------------------------------------------------------
+    public:
+
+        mutable LockHelper m_aLock;
+};
+
+}       //  namespace framework
+
+#endif  //  #ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
