1ae4fe1ef84b5e889c6db31e1c5d0cfd9a2553b9
Andreas Bille <abi@openoffice.org>
2001-06-11 09:55:01 +0000

    Building ucpchelp.dll ( c++ help provider )

---
+++ xmlhelp/util/makefile.mk
@@ -0,0 +1,119 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.1 $
+#
+#   last change: $Author: abi $ $Date: 2001-06-11 10:55:01 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+UCP_VERSION=1
+UCP_NAME=chelp
+
+PRJ=..
+PRJNAME=xmlhelp
+TARGET=ucp$(UCP_NAME)
+UCPHELP_MAJOR=1
+
+ENABLE_EXCEPTIONS=TRUE
+USE_DEFFILE=TRUE
+NO_BSYMBOLIC=TRUE
+
+# --- Settings ---------------------------------------------------------
+
+.INCLUDE: settings.mk
+
+.IF "$(GUI)"=="WNT"
+
+CFLAGS+=/GR
+# --- Shared-Library ---------------------------------------------------
+
+SHL1TARGET=$(TARGET)$(UCP_VERSION)
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+SHL1IMPLIB=i$(TARGET)
+SHL1VERSIONMAP=exports.map
+
+# Add additional libs here.
+SHL1STDLIBS=                 \
+    $(CPPUHELPERLIB)         \
+    $(CPPULIB)               \
+    $(SALLIB)                \
+    $(VOSLIB)                \
+    $(EXPATASCII3RDLIB)      \
+    sablot.lib               \
+    $(UCBHELPERLIB)          \
+    $(BERKELEYLIB)           \
+    $(BERKELEYCPPLIB)
+
+SHL1LIBS =                   \
+    $(SLB)$/jaqe.lib         \
+    $(SLB)$/jautil.lib       \
+    $(SLB)$/jadb.lib         \
+    $(SLB)$/chelp.lib
+
+# --- Def-File ---------------------------------------------------------
+
+DEF1NAME=$(SHL1TARGET)
+DEF1EXPORTFILE=exports.dxp
+DEF1DES=UCB Help Content Provider
+
+# --- Targets ----------------------------------------------------------
+.ENDIF
+
+
+
+.INCLUDE: target.mk
+
