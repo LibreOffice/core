c7b037f8cdde94d807a45ce821428edda2a97f74
Hans-Joachim Lankenau <hjs@openoffice.org>
2001-06-13 12:59:43 +0000

    backslash...

--- tools/bootstrp/rscdep.cxx
+++ tools/bootstrp/rscdep.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rscdep.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hjs $ $Date: 2001-06-13 13:42:22 $
+ *  last change: $Author: hjs $ $Date: 2001-06-13 13:59:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -359,7 +359,7 @@ main( int argc, char **argv )
     {
         ByteString *pStr = pLst->GetObject(j);
 #ifdef UNX
-        pStr->SearchAndReplaceAll('\', ByteString( aDelim,  RTL_TEXTENCODING_ASCII_US ));
+        pStr->SearchAndReplaceAll('\\', ByteString( aDelim,  RTL_TEXTENCODING_ASCII_US ));
 #endif
 #ifdef WNT
         pStr->SearchAndReplaceAll('/', ByteString( aDelim,  RTL_TEXTENCODING_ASCII_US ));
