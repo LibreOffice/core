1d4d6d028482ca46d5df9e29836add9f83aafecc
Tino Rachui <tra@openoffice.org>
2001-06-27 06:16:57 +0000

    #86986#exeption handling enabled

--- sysui/source/win32/misc/makefile.mk
+++ sysui/source/win32/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: tra $ $Date: 2001-06-26 18:37:51 $
+#   last change: $Author: tra $ $Date: 2001-06-27 07:16:57 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,6 +70,8 @@ LIBTARGET=NO
 .INCLUDE : settings.mk
 
 .INCLUDE : ..$/..$/cppumaker.mk
+
+CFLAGS+=/GX
 
 # --- Files -------------------------------------
 
