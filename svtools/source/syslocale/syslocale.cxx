5a3047efc947b40adb58a6a5c9bb2b13a44705d2
Eike Rathke <er@openoffice.org>
2001-07-02 08:37:27 +0000

    SvtSysLocale provides a single instance of an application wide LocaleDataWrapper which always follows the locale as it is configured by the user

---
+++ svtools/source/syslocale/syslocale.cxx
@@ -0,0 +1,191 @@
+/*************************************************************************
+ *
+ *  $RCSfile: syslocale.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: er $ $Date: 2001-07-02 09:37:27 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#pragma hdrstop
+
+#include "syslocale.hxx"
+
+#ifndef _SVT_BROADCAST_HXX
+#include <broadcast.hxx>
+#endif
+#ifndef _SVT_LISTENER_HXX
+#include <listener.hxx>
+#endif
+#ifndef _SFXSMPLHINT_HXX
+#include <smplhint.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+#ifndef _ISOLANG_HXX
+#include <tools/isolang.hxx>
+#endif
+#ifndef _STRING_HXX
+#include <tools/string.hxx>
+#endif
+#ifndef INCLUDED_SVTOOLS_SYSLOCALEOPTIONS_HXX
+#include <svtools/syslocaleoptions.hxx>
+#endif
+#ifndef _UNOTOOLS_LOCALEDATAWRAPPER_HXX
+#include <unotools/localedatawrapper.hxx>
+#endif
+#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
+#include <comphelper/processfactory.hxx>
+#endif
+
+
+using namespace osl;
+using namespace com::sun::star;
+
+
+SvtSysLocale_Impl*  SvtSysLocale::pImpl = NULL;
+sal_Int32           SvtSysLocale::nRefCount = 0;
+
+
+class SvtSysLocale_Impl : public SvtListener
+{
+    friend class SvtSysLocale;
+
+        SvtSysLocaleOptions     aSysLocaleOptions;
+        LocaleDataWrapper*      pLocaleData;
+
+public:
+                                SvtSysLocale_Impl();
+    virtual                     ~SvtSysLocale_Impl();
+
+    virtual void                Notify( SvtBroadcaster& rBC, const SfxHint& rHint );
+
+};
+
+
+// -----------------------------------------------------------------------
+
+SvtSysLocale_Impl::SvtSysLocale_Impl()
+{
+    const lang::Locale& rLocale = Application::GetSettings().GetLocale();
+    pLocaleData = new LocaleDataWrapper(
+        ::comphelper::getProcessServiceFactory(), rLocale );
+    aSysLocaleOptions.AddListener( *this );
+}
+
+
+SvtSysLocale_Impl::~SvtSysLocale_Impl()
+{
+    aSysLocaleOptions.RemoveListener( *this );
+    delete pLocaleData;
+}
+
+
+void SvtSysLocale_Impl::Notify( SvtBroadcaster& rBC, const SfxHint& rHint )
+{
+    const SfxSimpleHint* p = PTR_CAST( SfxSimpleHint, &rHint );
+    if( p && (p->GetId() & SYSLOCALEOPTIONS_HINT_LOCALE) )
+    {
+        MutexGuard aGuard( SvtSysLocale::GetMutex() );
+        const lang::Locale& rLocale = Application::GetSettings().GetLocale();
+        pLocaleData->setLocale( rLocale );
+    }
+}
+
+
+// ====================================================================
+
+SvtSysLocale::SvtSysLocale()
+{
+    MutexGuard aGuard( GetMutex() );
+    if ( !pImpl )
+        pImpl = new SvtSysLocale_Impl;
+    ++nRefCount;
+}
+
+
+SvtSysLocale::~SvtSysLocale()
+{
+    MutexGuard aGuard( GetMutex() );
+    if ( !--nRefCount )
+    {
+        delete pImpl;
+        pImpl = NULL;
+    }
+}
+
+
+// static
+Mutex& SvtSysLocale::GetMutex()
+{
+    static Mutex* pMutex = NULL;
+    if( !pMutex )
+    {
+        MutexGuard aGuard( Mutex::getGlobalMutex() );
+        if( !pMutex )
+        {
+            static Mutex aMutex;
+            pMutex = &aMutex;
+        }
+    }
+    return *pMutex;
+}
+
+
+const LocaleDataWrapper& SvtSysLocale::GetLocaleData() const
+{
+    return *(pImpl->pLocaleData);
+}
