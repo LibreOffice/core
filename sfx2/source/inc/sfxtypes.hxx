8dcc2593b45dff7863d0187bec4750eb8ac7eaa3
Dirk Völzke <dv@openoffice.org>
2001-07-03 11:16:57 +0000

    #89176# AsynchronLink moved to svtools

--- sfx2/source/inc/sfxtypes.hxx
+++ sfx2/source/inc/sfxtypes.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sfxtypes.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:52:34 $
+ *  last change: $Author: dv $ $Date: 2001-07-03 12:16:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -194,38 +194,6 @@ String SfxStringEncode( const String &rSource,
                         const char *pKey = SFX_PASSWORD_CODE );
 String SfxStringDecode( const String &rSource,
                         const char *pKey = SFX_PASSWORD_CODE );
-
-
-class NAMESPACE_VOS( OMutex );
-class AsynchronLink
-{
-    Link   _aLink;
-    ULONG  _nEventId;
-    Timer* _pTimer;
-    BOOL   _bInCall;
-    BOOL*  _pDeleted;
-    void*  _pArg;
-    NAMESPACE_VOS( OMutex )* _pMutex;
-
-    DECL_STATIC_LINK( AsynchronLink, HandleCall, void* );
-    void Call_Impl( void* pArg );
-public:
-    AsynchronLink( const Link& rLink ) :
-        _pTimer( 0 ), _aLink( rLink ), _nEventId( 0 ), _bInCall( FALSE ),
-        _pDeleted( 0 ), _pMutex( 0 ){}
-    AsynchronLink() : _nEventId( 0 ), _pTimer( 0 ), _bInCall( FALSE ),
-            _pDeleted( 0 ), _pMutex( 0 ){}
-    ~AsynchronLink();
-
-    void CreateMutex();
-    void operator=( const Link& rLink ) { _aLink = rLink; }
-    void Call( void* pObj, BOOL bAllowDoubles = FALSE,
-               BOOL bUseTimer = FALSE );
-    void ForcePendingCall( );
-    void ClearPendingCall( );
-    BOOL IsSet() const { return _aLink.IsSet(); }
-    Link GetLink() const { return _aLink; }
-};
 
 
 struct StringList_Impl : private Resource
--- sfx2/source/notify/hintpost.cxx
+++ sfx2/source/notify/hintpost.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hintpost.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:52:35 $
+ *  last change: $Author: dv $ $Date: 2001-07-03 12:16:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -138,108 +138,3 @@ void SfxHintPoster::SetEventHdl( const GenLink& rLink )
     aStrm << x.GetStr() << '\n';                                \
 }
 #endif
-
-
-//--------------------------------------------------------------------
-
-void AsynchronLink::CreateMutex()
-{
-    if( !_pMutex ) _pMutex = new NAMESPACE_VOS( OMutex );
-}
-
-void AsynchronLink::Call( void* pObj, BOOL bAllowDoubles, BOOL bUseTimer )
-{
-#ifdef DBG_UTIL
-    if ( bUseTimer || !_bInCall )
-        DBG_WARNING( "Recursives Call. Eher ueber Timer. TLX Fragen" );
-#endif
-    if( _aLink.IsSet() )
-    {
-        _pArg = pObj;
-        DBG_ASSERT( bAllowDoubles ||
-                    ( !_nEventId && ( !_pTimer || !_pTimer->IsActive() ) ),
-                    "Schon ein Call unterwegs" );
-        if( _nEventId )
-        {
-            if( _pMutex ) _pMutex->acquire();
-            LOG( String( "Remove " ) + String( _nEventId ) );
-            Application::RemoveUserEvent( _nEventId );
-            if( _pMutex ) _pMutex->release();
-        }
-        if( _pTimer )_pTimer->Stop();
-        if( bUseTimer )
-        {
-            if( !_pTimer )
-            {
-                _pTimer = new Timer;
-                _pTimer->SetTimeout( 0 );
-                _pTimer->SetTimeoutHdl( STATIC_LINK(
-                    this, AsynchronLink, HandleCall) );
-            }
-            _pTimer->Start();
-        }
-        else
-        {
-            if( _pMutex ) _pMutex->acquire();
-            Application::PostUserEvent( _nEventId, STATIC_LINK( this, AsynchronLink, HandleCall), 0 );
-            LOG( String( "Posted " ) + String( _nEventId ) );
-            if( _pMutex ) _pMutex->release();
-        }
-    }
-}
-
-AsynchronLink::~AsynchronLink()
-{
-    if( _nEventId )
-    {
-        LOG( String( "Remove " ) + String( _nEventId ) );
-        Application::RemoveUserEvent( _nEventId );
-    }
-    delete _pTimer;
-    if( _pDeleted ) *_pDeleted = TRUE;
-    delete _pMutex;
-}
-
-IMPL_STATIC_LINK( AsynchronLink, HandleCall, void*, EMPTYARG )
-{
-    if( pThis->_pMutex ) pThis->_pMutex->acquire();
-    pThis->_nEventId = 0;
-    if( pThis->_pMutex ) pThis->_pMutex->release();
-    pThis->Call_Impl( pThis->_pArg );
-    return 0;
-}
-
-void AsynchronLink::ForcePendingCall()
-{
-    ClearPendingCall();
-    Call_Impl( _pArg );
-}
-
-void AsynchronLink::ClearPendingCall()
-{
-    if( _pMutex ) _pMutex->acquire();
-    if( _nEventId )
-    {
-        Application::RemoveUserEvent( _nEventId );
-        _nEventId = 0;
-    }
-    if( _pMutex ) _pMutex->release();
-    if( _pTimer ) _pTimer->Stop();
-}
-
-void AsynchronLink::Call_Impl( void* pArg )
-{
-    _bInCall = TRUE;
-    BOOL bDeleted = FALSE;
-    _pDeleted = &bDeleted;
-    _aLink.Call( pArg );
-    if( !bDeleted )
-    {
-        _bInCall = FALSE;
-        _pDeleted = 0;
-    }
-}
-
-SV_IMPL_PTRARR( AsynchronLinkArr_Impl, AsynchronLink* );
-
-
