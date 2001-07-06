6a5a81c05ffb645b95f9d613bb8c48e2a9880cdb
Oliver Bolte <obo@openoffice.org>
2001-07-06 12:25:43 +0000

    #65293# linux

--- sfx2/source/menu/mnumgr.cxx
+++ sfx2/source/menu/mnumgr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mnumgr.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: mba $ $Date: 2001-06-26 08:15:46 $
+ *  last change: $Author: obo $ $Date: 2001-07-06 13:25:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1150,10 +1150,6 @@ SfxMenuBarManager::~SfxMenuBarManager()
         pWindow->SetMenuBar( 0 );
 }
 
-String SfxMenuBarManager::GetStreamName()
-{
-    return String::CreateFromAscii("MenuBar.xml");
-}
 
 /*
 SvStream* SfxMenuBarManager::GetDefaultStream( StreamMode nMode )
