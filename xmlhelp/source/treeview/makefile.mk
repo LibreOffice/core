e5e0eca1abba73537ce6b54806e765129d0487e6
Rüdiger Timm <rt@openoffice.org>
2001-07-18 09:30:44 +0000

    #87576# dependency on tvhlp1.flt removed, this file does not get built any more

--- xmlhelp/source/treeview/makefile.mk
+++ xmlhelp/source/treeview/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: abi $ $Date: 2001-07-17 14:46:52 $
+#   last change: $Author: rt $ $Date: 2001-07-18 10:30:44 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -92,7 +92,6 @@ SHL1STDLIBS=\
     $(UCBHELPERLIB)
 SHL1VERSIONMAP=	$(TARGET).map
 
-DEF1DEPN=$(MISC)$/$(SHL1TARGET).flt
 DEF1NAME=$(SHL1TARGET)
 DEF1DES=UCB : Treeview help
 
