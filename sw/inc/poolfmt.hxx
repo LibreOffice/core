0113ca492c0290556f84094ea9f19438d7b18072
Martin Gallwey <mtg@openoffice.org>
2001-07-19 15:16:40 +0000

    #89999# remove old function declaration...instead use the static methods in the SwStyleNameMapper class for Programmatic Name <-> UI Name <-> Pool Id conversion

--- sw/inc/poolfmt.hxx
+++ sw/inc/poolfmt.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: poolfmt.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jp $ $Date: 2000-10-23 11:58:55 $
+ *  last change: $Author: mtg $ $Date: 2001-07-19 16:16:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -458,10 +458,6 @@ RES_POOLCOLL_HTML_END
 //              USHRT_MAX   -> kein Parent
 //              sonst       -> den Parent
 USHORT GetPoolParent( USHORT nId );
-
-// erfrage zu einer PoolId den Namen (steht im poolfmt.cxx)
-class String;
-String& GetDocPoolNm( USHORT nId, String& rFillNm );
 
 inline BOOL IsConditionalByPoolId(USHORT nId)
     {
