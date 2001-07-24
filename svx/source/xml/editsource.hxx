411608e45c43e6e9f1212502bdb031f26f9d7aa4
Rüdiger Timm <rt@openoffice.org>
2001-07-24 10:41:10 +0000

    #87576# missing newline at end of file added.

--- svx/source/xml/editsource.hxx
+++ svx/source/xml/editsource.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: editsource.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: cl $ $Date: 2001-07-20 12:57:19 $
+ *  last change: $Author: rt $ $Date: 2001-07-24 11:41:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
