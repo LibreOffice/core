f20bac52281514dae23c392ad84fd05b3545bd20
Frank Schönheit <fs@openoffice.org>
2001-07-25 12:28:46 +0000

    initial checkin - main factory for load-on-demand usage of OSQLParser

---
+++ connectivity/source/simpledbt/parser_s.hxx
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *
+ *  $RCSfile: parser_s.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2001-07-25 13:28:46 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef CONNECTIVITY_DBTOOLS_PARSER_SIMPLE_HXX
+#define CONNECTIVITY_DBTOOLS_PARSER_SIMPLE_HXX
+
+#ifndef CONNECTIVITY_VIRTUAL_DBTOOLS_HXX
+#include <connectivity/virtualdbtools.hxx>
+#endif
+#ifndef CONNECTIVITY_DBTOOLS_REFBASE_HXX
+#include "refbase.hxx"
+#endif
+#ifndef _CONNECTIVITY_SQLPARSE_HXX
+#include <connectivity/sqlparse.hxx>
+#endif
+
+//........................................................................
+namespace connectivity
+{
+//........................................................................
+
+    //================================================================
+    //= OSimpleSQLParser
+    //================================================================
+    class OSimpleSQLParser
+            :public simple::ISQLParser
+            ,public ORefBase
+    {
+    protected:
+        OSQLParser      m_aFullParser;
+
+    public:
+        OSimpleSQLParser(const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxServiceFactory);
+
+        // ISQLParser
+        virtual ::rtl::Reference< simple::ISQLParseNode > predicateTree(
+            ::rtl::OUString& rErrorMessage,
+            const ::rtl::OUString& rStatement,
+            const ::com::sun::star::uno::Reference< ::com::sun::star::util::XNumberFormatter >& _rxFormatter,
+            const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _rxField
+        ) const;
+
+        virtual const OParseContext& getContext() const;
+
+        // disambiguate IReference
+        virtual oslInterlockedCount SAL_CALL acquire();
+        virtual oslInterlockedCount SAL_CALL release();
+    };
+
+//........................................................................
+}   // namespace connectivity
+//........................................................................
+
+#endif // CONNECTIVITY_DBTOOLS_PARSER_SIMPLE_HXX
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 24.07.01 16:32:42  fs
+ ************************************************************************/
+
