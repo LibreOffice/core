9f276ccf7ef0fc75264ce8fb6903cac5741880a0
Martin Gallwey <mtg@openoffice.org>
2001-07-27 12:26:07 +0000

    #89906# intial revision - new service for print preview settings

---
+++ sw/source/ui/uno/SwXPrintPreviewSettings.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ *  $RCSfile: SwXPrintPreviewSettings.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: mtg $ $Date: 2001-07-27 13:26:07 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): Martin Gallwey ( gallwey@sun.com )
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SW_XPRINTPREVIEWSETTINGS_HXX_
+#define _SW_XPRINTPREVIEWSETTINGS_HXX_
+
+#ifdef PRECOMPILED
+#include "ui_pch.hxx"
+#endif
+#pragma hdrstop
+
+#ifndef _COMPHELPER_CHAINABLEPROPERTYSET_HXX_
+#include <comphelper/ChainablePropertySet.hxx>
+#endif
+#ifndef _COM_SUN_STAR_TEXT_XTEXTDOCUMENT_HPP
+#include <com/sun/star/text/XTextDocument.hpp>
+#endif
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+class SwXTextDocument;
+class SwDocShell;
+class SwDoc;
+class SwPagePreViewPrtData;
+
+class SwXPrintPreviewSettings :public comphelper::ChainablePropertySet,
+                        public cppu::OWeakObject,
+                        public com::sun::star::lang::XServiceInfo
+{
+    friend class SwXDocumentSettings;
+protected:
+    sal_Bool        mbPreviewDataChanged;
+    SwDoc*          mpDoc;
+    SwPagePreViewPrtData *mpPreViewData;
+    const SwPagePreViewPrtData *mpConstPreViewData;
+
+    virtual void _preSetValues ()
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException );
+    virtual void _setSingleValue( const comphelper::PropertyInfo & rInfo, const ::com::sun::star::uno::Any &rValue )
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException );
+    virtual void _postSetValues ()
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException );
+
+    virtual void _preGetValues ()
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException );
+    virtual void _getSingleValue( const comphelper::PropertyInfo & rInfo, ::com::sun::star::uno::Any & rValue )
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException );
+    virtual void _postGetValues ()
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException );
+public:
+    SwXPrintPreviewSettings(SwDoc *pDoc);
+    virtual ~SwXPrintPreviewSettings();
+
+    // XInterface
+    virtual ::com::sun::star::uno::Any SAL_CALL queryInterface( const ::com::sun::star::uno::Type& aType )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL acquire(  )
+        throw ();
+    virtual void SAL_CALL release(  )
+        throw ();
+
+    //XServiceInfo
+    virtual rtl::OUString SAL_CALL getImplementationName(void)
+        throw( ::com::sun::star::uno::RuntimeException );
+    virtual sal_Bool SAL_CALL supportsService(const rtl::OUString& ServiceName)
+        throw( ::com::sun::star::uno::RuntimeException );
+    virtual ::com::sun::star::uno::Sequence< rtl::OUString > SAL_CALL getSupportedServiceNames(void)
+        throw( ::com::sun::star::uno::RuntimeException );
+};
+#endif
