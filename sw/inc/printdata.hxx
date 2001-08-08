24c4e42c2fe0851e65aa1db7c27e1968b29ce058
Martin Gallwey <mtg@openoffice.org>
2001-08-08 20:40:10 +0000

    #90674# moved this functionality here from source\ui\inc\prtopt.hxx

--- sw/inc/printdata.hxx
+++ sw/inc/printdata.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: printdata.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: os $ $Date: 2001-05-10 08:40:24 $
+ *  last change: $Author: mtg $ $Date: 2001-08-08 21:40:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -68,45 +68,36 @@
 #ifndef _RTL_USTRING_
 #include <rtl/ustring>
 #endif
+
 struct SwPrintData
 {
-    sal_Bool
-         bPrintGraphic       ,
-         bPrintTable         ,
-         bPrintDraw          ,
-         bPrintControl       ,
-         bPrintPageBackground,
-         bPrintBlackFont     ,
-
-         bPrintLeftPage      ,
-         bPrintRightPage     ,
-         bPrintReverse       ,
-         bPrintProspect      ,
-         bPrintSingleJobs    ,
-
-         bPaperFromSetup     ;
+    sal_Bool bPrintGraphic, bPrintTable, bPrintDraw, bPrintControl, bPrintPageBackground,
+             bPrintBlackFont, bPrintLeftPage, bPrintRightPage, bPrintReverse, bPrintProspect,
+             bPrintSingleJobs, bPaperFromSetup, bModified;
 
     sal_Int16           nPrintPostIts;
     rtl::OUString       sFaxName;
 
-         SwPrintData()
-            {
-                bPrintGraphic           =
-                bPrintTable             =
-                bPrintDraw              =
-                bPrintControl           =
-                bPrintLeftPage          =
-                bPrintRightPage         =
-                bPrintPageBackground    = sal_True;
+    SwPrintData()
+    {
+        bPrintGraphic           =
+        bPrintTable             =
+        bPrintDraw              =
+        bPrintControl           =
+        bPrintLeftPage          =
+        bPrintRightPage         =
+        bPrintPageBackground    = sal_True;
 
-                bPaperFromSetup         =
-                bPrintReverse           =
-                bPrintProspect          =
-                bPrintSingleJobs        =
-                bPrintBlackFont         = sal_False;
+        bPaperFromSetup         =
+        bPrintReverse           =
+        bPrintProspect          =
+        bPrintSingleJobs        =
+        bModified               =
+        bPrintBlackFont         = sal_False;
 
-                nPrintPostIts           = 0;
-            }
+        nPrintPostIts           = 0;
+    }
+
     sal_Bool operator==(const SwPrintData& rData)const
     {
         return
@@ -125,6 +116,37 @@ struct SwPrintData
         nPrintPostIts       ==   rData.nPrintPostIts       &&
         sFaxName            ==   rData.sFaxName;
     }
+    sal_Bool IsPrintGraphic()   const { return bPrintGraphic; }
+    sal_Bool IsPrintTable()     const { return bPrintTable; }
+    sal_Bool IsPrintDraw()      const { return bPrintDraw; }
+    sal_Bool IsPrintControl()   const { return bPrintControl; }
+    sal_Bool IsPrintLeftPage()  const { return bPrintLeftPage; }
+    sal_Bool IsPrintRightPage() const { return bPrintRightPage; }
+    sal_Bool IsPrintReverse()   const { return bPrintReverse; }
+    sal_Bool IsPaperFromSetup() const { return bPaperFromSetup; }
+    sal_Bool IsPrintProspect()  const { return bPrintProspect; }
+    sal_Bool IsPrintPageBackground() const { return bPrintPageBackground; }
+    sal_Bool IsPrintBlackFont() const { return bPrintBlackFont;}
+    sal_Bool IsPrintSingleJobs() const { return bPrintSingleJobs;}
+    sal_Int16 GetPrintPostIts() const { return nPrintPostIts; }
+    const rtl::OUString     GetFaxName() const{return sFaxName;}
+
+    void SetPrintGraphic  ( sal_Bool b ) { doSetModified(); bPrintGraphic = b;}
+    void SetPrintTable    ( sal_Bool b ) { doSetModified(); bPrintTable = b;}
+    void SetPrintDraw     ( sal_Bool b ) { doSetModified(); bPrintDraw = b;}
+    void SetPrintControl  ( sal_Bool b ) { doSetModified(); bPrintControl = b; }
+    void SetPrintLeftPage ( sal_Bool b ) { doSetModified(); bPrintLeftPage = b;}
+    void SetPrintRightPage( sal_Bool b ) { doSetModified(); bPrintRightPage = b;}
+    void SetPrintReverse  ( sal_Bool b ) { doSetModified(); bPrintReverse = b;}
+    void SetPaperFromSetup( sal_Bool b ) { doSetModified(); bPaperFromSetup = b;}
+    void SetPrintPostIts    ( sal_Int16 n){ doSetModified(); nPrintPostIts = n; }
+    void SetPrintProspect   ( sal_Bool b ) { doSetModified(); bPrintProspect = b; }
+    void SetPrintPageBackground(sal_Bool b){ doSetModified(); bPrintPageBackground = b;}
+    void SetPrintBlackFont(sal_Bool b){ doSetModified(); bPrintBlackFont = b;}
+    void SetPrintSingleJobs(sal_Bool b){ doSetModified(); bPrintSingleJobs = b;}
+    void SetFaxName(const rtl::OUString& rSet){sFaxName = rSet;}
+    virtual void doSetModified () { bModified = sal_True;}
+    sal_Bool WasModified () { return bModified; }
 };
 
 #endif  //_SW_PRINTDATA_HXX
