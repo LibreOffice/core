6b52df25e1d13586be46d98d465a7c71c27cf38d
Stephan Bergmann <sb@openoffice.org>
2001-08-21 09:12:41 +0000

    Added '.cxx' to ucbhelper.flt to filter out names from unnamed namespaces.

--- ucbhelper/util/makefile.mk
+++ ucbhelper/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: sb $ $Date: 2001-06-20 07:55:28 $
+#   last change: $Author: sb $ $Date: 2001-08-21 10:12:41 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -122,6 +122,7 @@ $(MISC)$/$(SHL1TARGET).flt : ucbhelper.flt
     @echo ??_G>>$@
     @echo ??_H>>$@
     @echo ??_I>>$@
+    @echo .cxx>>$@
 .ENDIF # MSC
 .IF "$(GUI)"=="WNT"
     @echo ?CreateType@>>$@
