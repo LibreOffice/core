a133547178acb583e4823f94c281ecdb52c8e9ad
Eike Rathke <er@openoffice.org>
2001-08-22 10:22:12 +0000

    #91013# GetDataArea: undo of last change, no data is a special case here

--- sc/source/filter/rtf/expbase.cxx
+++ sc/source/filter/rtf/expbase.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: expbase.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: er $ $Date: 2001-08-09 11:06:23 $
+ *  last change: $Author: er $ $Date: 2001-08-22 11:22:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -105,10 +105,6 @@ BOOL ScExportBase::GetDataArea( USHORT nTab, USHORT& nStartCol,
 {
     pDoc->GetDataStart( nTab, nStartCol, nStartRow );
     pDoc->GetPrintArea( nTab, nEndCol, nEndRow, TRUE );
-    if ( nStartCol > nEndCol )
-        nStartCol = nEndCol;
-    if ( nStartRow > nEndRow )
-        nStartRow = nEndRow;
     return TrimDataArea( nTab, nStartCol, nStartRow, nEndCol, nEndRow );
 }
 
