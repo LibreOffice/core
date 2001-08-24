c58b388a0a2b6d016582f2338782083c70a7831a
Hans-Joachim Lankenau <hjs@openoffice.org>
2001-08-24 13:22:26 +0000

    #87136# no ALL target for L10N-framework

--- sw/source/ui/makefile.mk
+++ sw/source/ui/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: os $ $Date: 2000-10-13 10:08:04 $
+#   last change: $Author: hjs $ $Date: 2001-08-24 14:22:26 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -153,6 +153,7 @@ SUBLIBS2= \
 
 # -----------------------------------------------------------
 
+.IF "$(L10N-framework)" == ""
 .IF "$(RC_SUBDIRS)" == ""
 .IF "$(depend)" == ""
 ui:						\
@@ -164,6 +165,7 @@ ui:
     @+echo Doing nothing in source\ui
 .ENDIF
 .ENDIF
+.ENDIF          # "$(L10N-framework)" == ""
 
 ################################################################
 
