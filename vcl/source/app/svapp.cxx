f3a72302105e76a7aa7964bc37fcab67ff381157
Carsten Driesner <cd@openoffice.org>
2001-08-24 06:45:57 +0000

    #91460# Don't call use MergeSystemSettings implementation in webtop case

--- vcl/source/app/svapp.cxx
+++ vcl/source/app/svapp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svapp.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: th $ $Date: 2001-08-13 11:35:37 $
+ *  last change: $Author: cd $ $Date: 2001-08-24 07:45:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -893,11 +893,11 @@ void Application::SystemSettingsChanging( AllSettings& rSettings,
 
 void Application::MergeSystemSettings( AllSettings& rSettings )
 {
-    Window* pWindow = ImplGetDefaultWindow();
 #ifndef REMOTE_APPSERVER
+    Window* pWindow = ImplGetDefaultWindow();
     pWindow->ImplGetFrame()->UpdateSettings( rSettings );
-#endif
     pWindow->ImplUpdateGlobalSettings( rSettings, FALSE );
+#endif
 }
 
 // -----------------------------------------------------------------------
