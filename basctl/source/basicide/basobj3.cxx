7d54af9bc54dc0301f05cf9bfae7af4b064f6944
Oliver Bolte <obo@openoffice.org>
2001-09-04 10:52:52 +0000

    #65293# includes missing

--- basctl/source/basicide/basobj3.cxx
+++ basctl/source/basicide/basobj3.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: basobj3.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: tbe $ $Date: 2001-09-03 11:48:16 $
+ *  last change: $Author: obo $ $Date: 2001-09-04 11:52:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,8 @@
 #include <ide_pch.hxx>
 
 #pragma hdrstop
-
+#include <vector>
+#include <algorithm>
 #ifndef _SBXCLASS_HXX //autogen
 #include <svtools/sbx.hxx>
 #endif
