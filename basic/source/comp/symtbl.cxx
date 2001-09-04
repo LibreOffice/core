5d5e9cf6024b77fa1fa685534ba796b6abc13cbe
Andreas Bregas <ab@openoffice.org>
2001-09-04 09:07:46 +0000

    #88329# Initialise SbiSymDef global flag

--- basic/source/comp/symtbl.cxx
+++ basic/source/comp/symtbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: symtbl.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:12:10 $
+ *  last change: $Author: ab $ $Date: 2001-09-04 10:07:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -334,7 +334,8 @@ SbiSymDef::SbiSymDef( const String& rName ) : aName( rName )
     bStatic  =
     bOpt     =
     bByVal   =
-    bChained = FALSE;
+    bChained =
+    bGlobal  = FALSE;
     pIn      =
     pPool    = NULL;
 }
