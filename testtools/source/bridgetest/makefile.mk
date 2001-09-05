6f8e3dab5ef203ccd2d28861b7682cf0909b8ceb
Jörg Budischewski <jbu@openoffice.org>
2001-09-05 09:29:53 +0000

    #91714# now using .map-file instead of dxp-files

--- testtools/source/bridgetest/makefile.mk
+++ testtools/source/bridgetest/makefile.mk
@@ -71,7 +71,7 @@ SHL1STDLIBS= \
 SHL1LIBS=	$(LIB1TARGET)
 SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
 DEF1NAME=	$(SHL1TARGET)
-DEF1EXPORTFILE=	exports.dxp
+SAL1VERSIONMAP=$(SOLARENV)$/src$/component.map
 
 # ---- test object ----
 
@@ -88,7 +88,7 @@ SHL2STDLIBS= \
 SHL2LIBS=	$(LIB2TARGET)
 SHL2DEF=	$(MISC)$/$(SHL2TARGET).def
 DEF2NAME=	$(SHL2TARGET)
-DEF2EXPORTFILE=	exports.dxp
+SAL2VERSIONMAP=$(SOLARENV)$/src$/component.map
 
 # --- Targets ------------------------------------------------------
 ALL : \
