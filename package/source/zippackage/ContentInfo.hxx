ff5a40c661ccb228dcf18c8e6daf8948140f0e34
Martin Gallwey <mtg@openoffice.org>
2001-09-14 13:33:38 +0000

    #89303# Small struct to allow me to avoid using Anys as much as possible

---
+++ package/source/zippackage/ContentInfo.hxx
@@ -0,0 +1,100 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ContentInfo.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: mtg $ $Date: 2001-09-14 14:33:38 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): Martin Gallwey (gallwey@sun.com)
+ *
+ *
+ ************************************************************************/
+#ifndef _CONTENT_INFO_HXX_
+#define _CONTENT_INFO_HXX_
+
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMECONTAINER_HPP_
+#include <com/sun/star/container/XNameContainer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XUNOTUNNEl_HPP_
+#include <com/sun/star/lang/XUnoTunnel.hpp>
+#endif
+#include <hash_map>
+#ifndef _ZIP_PACKAGE_FOLDER_HXX
+#include <ZipPackageFolder.hxx>
+#endif
+#ifndef _ZIP_PACKAGE_STREAM_HXX
+#include <ZipPackageStream.hxx>
+#endif
+
+struct ContentInfo
+{
+    com::sun::star::uno::Reference < com::sun::star::lang::XUnoTunnel > xTunnel;
+    bool bFolder;
+    union
+    {
+        ZipPackageFolder *pFolder;
+        ZipPackageStream *pStream;
+    };
+    ContentInfo ( ZipPackageStream * pNewStream )
+    : bFolder ( false )
+    , pStream ( pNewStream )
+    , xTunnel ( pNewStream )
+    {
+    }
+    ContentInfo ( ZipPackageFolder * pNewFolder )
+    : bFolder ( true )
+    , pFolder ( pNewFolder )
+    , xTunnel ( pNewFolder )
+    {
+    }
+};
+#endif
