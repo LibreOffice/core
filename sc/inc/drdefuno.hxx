4611b6257ec22ea206e07b8aaecdf16d465c82e5
Vladimir Glazounov <vg@openoffice.org>
2001-09-18 14:15:52 +0000

    #92075# exception specificaion

--- sc/inc/drdefuno.hxx
+++ sc/inc/drdefuno.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drdefuno.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: nn $ $Date: 2001-03-05 11:30:27 $
+ *  last change: $Author: vg $ $Date: 2001-09-18 15:15:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,7 +79,7 @@ private:
 
 public:
                             ScDrawDefaultsObj(ScDocShell* pDocSh);
-    virtual                 ~ScDrawDefaultsObj();
+    virtual                 ~ScDrawDefaultsObj() throw ();
 
     virtual void            Notify( SfxBroadcaster& rBC, const SfxHint& rHint );
 
