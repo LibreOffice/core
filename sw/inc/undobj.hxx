a2214aaab0e227e8a987269352a39efcf5e91dbc
jp <jp@openoffice.org>
2001-09-27 12:27:58 +0000

    Task #87374#: SwUndoCompDoc - need an additionl member

--- sw/inc/undobj.hxx
+++ sw/inc/undobj.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: undobj.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: jp $ $Date: 2001-03-02 14:35:07 $
+ *  last change: $Author: jp $ $Date: 2001-09-27 13:27:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1596,7 +1596,7 @@ public:
 class SwUndoCompDoc : public SwUndo, public SwUndRng
 {
     SwRedlineData* pRedlData;
-    SwUndoDelete* pUnDel;
+    SwUndoDelete* pUnDel, *pUnDel2;
     SwRedlineSaveDatas* pRedlSaveData;
     BOOL bInsert;
 public:
