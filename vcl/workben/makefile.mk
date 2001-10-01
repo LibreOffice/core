34bae24808f47be2f98d4e7ba2bf368f32dd9419
Rüdiger Timm <rt@openoffice.org>
2001-10-01 11:33:42 +0000

    #65293# svdem needs to be linked against CPPUHELPERLIB and COMPHELPERLIB

--- vcl/workben/makefile.mk
+++ vcl/workben/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: ssa $ $Date: 2001-08-10 11:20:10 $
+#   last change: $Author: rt $ $Date: 2001-10-01 12:33:42 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -87,6 +87,8 @@ APP1OBJS=		$(OBJFILES)
 #				$(OBJ)$/salmain.obj
 
 APP1STDLIBS=	$(CPPULIB)			\
+                $(CPPUHELPERLIB)	\
+                $(COMPHELPERLIB)	\
                 $(VCLLIB)			\
                 $(TOOLSLIB) 		\
                 $(SALLIB)			\
@@ -96,10 +98,8 @@ APP1STDLIBS=	$(CPPULIB)			\
 
 .IF "$(remote)"!=""
 APP1STDLIBS+=	$(UNOLIBS)			\
-                $(CPPUHELPERLIB)	\
                 $(UNOTOOLSLIB)		\
                 $(UCBHELPERLIB)		\
-                $(COMPHELPERLIB)	\
                 $(SALHELPERLIB)
 .ENDIF
 
