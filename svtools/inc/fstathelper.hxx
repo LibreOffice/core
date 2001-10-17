7d2ad279764d18dd6b7e56720dc34e4100191a43
jp <jp@openoffice.org>
2001-10-17 12:28:31 +0000

    Task #88180#: add new method for detect if a folder exist

--- svtools/inc/fstathelper.hxx
+++ svtools/inc/fstathelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fstathelper.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: jp $ $Date: 2000-09-27 09:33:34 $
+ *  last change: $Author: jp $ $Date: 2001-10-17 13:28:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,6 +89,11 @@ sal_Bool GetModifiedDateTimeOfFile( const UniString& rURL,
     UCB.IsContent.
 */
 sal_Bool IsDocument( const UniString& rURL );
+
+/** Return if under the URL a folder exist. This is only a wrapper for the
+    UCB.isFolder.
+*/
+sal_Bool IsFolder( const UniString& rURL );
 
 };
 
