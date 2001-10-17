3656246823fdc6ac444eb6c2034d7a8a3d001aca
Peter Burow <pb@openoffice.org>
2001-10-17 09:57:34 +0000

    fix: #92010# new class HelpDispatch_Impl

---
+++ sfx2/source/appl/helpdispatch.cxx
@@ -0,0 +1,138 @@
+/*************************************************************************
+ *
+ *  $RCSfile: helpdispatch.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: pb $ $Date: 2001-10-17 10:57:34 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "helpdispatch.hxx"
+#include "sfxuno.hxx"
+#include "newhelp.hxx"
+
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::frame;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::util;
+
+// class HelpInterceptor_Impl --------------------------------------------
+
+HelpDispatch_Impl::HelpDispatch_Impl( HelpInterceptor_Impl& _rInterceptor,
+                                      const ::com::sun::star::uno::Reference<
+                                          ::com::sun::star::frame::XDispatch >& _xDisp ) :
+
+    m_rInterceptor  ( _rInterceptor ),
+    m_xRealDispatch ( _xDisp )
+
+{
+}
+
+// -----------------------------------------------------------------------
+
+HelpDispatch_Impl::~HelpDispatch_Impl()
+{
+}
+
+// -----------------------------------------------------------------------
+// XDispatch
+
+void SAL_CALL HelpDispatch_Impl::dispatch(
+
+    const URL& aURL, const Sequence< PropertyValue >& aArgs ) throw( RuntimeException )
+
+{
+    DBG_ASSERT( m_xRealDispatch.is(), "invalid dispatch" );
+    URL aNewURL( aURL );
+    sal_Int32 nIdx = aURL.Complete.indexOf( DEFINE_CONST_OUSTRING("&Keyword=") );
+    if ( nIdx != -1 )
+    {
+        String aKeyword = aURL.Complete.copy( nIdx + 9 );
+        aNewURL.Complete = aURL.Complete.copy( 0, nIdx );
+    }
+    m_rInterceptor.addURL( aNewURL.Complete );
+    m_rInterceptor.GetHelpWindow()->AddURLListener( aNewURL, m_xRealDispatch );
+    m_xRealDispatch->dispatch( aNewURL, aArgs );
+}
+
+// -----------------------------------------------------------------------
+
+void SAL_CALL HelpDispatch_Impl::addStatusListener(
+
+    const Reference< XStatusListener >& xControl, const URL& aURL ) throw( RuntimeException )
+
+{
+    DBG_ASSERT( m_xRealDispatch.is(), "invalid dispatch" );
+    m_xRealDispatch->addStatusListener( xControl, aURL );
+}
+
+// -----------------------------------------------------------------------
+
+void SAL_CALL HelpDispatch_Impl::removeStatusListener(
+
+    const Reference< XStatusListener >& xControl, const URL& aURL ) throw( RuntimeException )
+
+{
+    DBG_ASSERT( m_xRealDispatch.is(), "invalid dispatch" );
+    m_xRealDispatch->removeStatusListener( xControl, aURL );
+}
+
