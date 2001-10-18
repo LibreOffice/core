c762f4182ea23d26fd0aad0f81c92b2ee2657580
Jens-Heiner Rechtien <hr@openoffice.org>
2001-10-18 11:10:19 +0000

    #92924#: exception specifications

--- starmath/inc/unomodel.hxx
+++ starmath/inc/unomodel.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unomodel.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2001-09-13 08:24:44 $
+ *  last change: $Author: hr $ $Date: 2001-10-18 12:10:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -102,10 +102,8 @@ public:
 
     //XInterface
     virtual     ::com::sun::star::uno::Any SAL_CALL queryInterface( const ::com::sun::star::uno::Type& aType ) throw(::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL acquire(  )
-        throw(::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL release(  )
-        throw(::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL acquire(  ) throw();
+    virtual void SAL_CALL release(  ) throw();
 
     //XTypeProvider
     virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type > SAL_CALL getTypes(  ) throw(::com::sun::star::uno::RuntimeException);
