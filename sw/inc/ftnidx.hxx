811f945d00c38fb95621f980a2e8facd61201ceb
Jens-Heiner Rechtien <hr@openoffice.org>
2001-10-18 14:41:49 +0000

    #92924#: constness

--- sw/inc/ftnidx.hxx
+++ sw/inc/ftnidx.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ftnidx.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:14:26 $
+ *  last change: $Author: hr $ $Date: 2001-10-18 15:41:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -83,7 +83,7 @@ class SwFtnIdxs : public _SwFtnIdxs
 public:
     SwFtnIdxs() {}
 
-    void UpdateFtn( SwNodeIndex& rStt );    // ab Pos. alle Updaten
+    void UpdateFtn( const SwNodeIndex& rStt );      // ab Pos. alle Updaten
     void UpdateAllFtn();                    // alle Fussnoten updaten
     void UpdateFtnInSections();
 
--- sw/source/core/doc/ftnidx.cxx
+++ sw/source/core/doc/ftnidx.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ftnidx.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:08:16 $
+ *  last change: $Author: hr $ $Date: 2001-10-18 15:40:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -139,7 +139,7 @@ BOOL _SwFtnIdxs::Seek_Entry( const SwTxtFtnPtr rSrch, USHORT* pFndPos ) const
 }
 
 
-void SwFtnIdxs::UpdateFtn( SwNodeIndex& rStt )
+void SwFtnIdxs::UpdateFtn( const SwNodeIndex& rStt )
 {
     if( !Count() )
         return;
