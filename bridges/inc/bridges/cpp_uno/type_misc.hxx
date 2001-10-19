d546db13c9fb418af64d65cb5aab4226de0562a5
Daniel Boelzle <dbo@openoffice.org>
2001-10-19 12:30:16 +0000

    #92951# added another cppu_isSimpleType()

--- bridges/inc/bridges/cpp_uno/type_misc.hxx
+++ bridges/inc/bridges/cpp_uno/type_misc.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: type_misc.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dbo $ $Date: 2000-12-21 14:44:41 $
+ *  last change: $Author: dbo $ $Date: 2001-10-19 13:30:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,7 +74,7 @@
     @param pTypeDescr type description of type
     @return true if type might relate to an interface, false otherwise
 */
-inline sal_Bool SAL_CALL cppu_relatesToInterface( typelib_TypeDescription * pTypeDescr ) throw ()
+inline bool cppu_relatesToInterface( typelib_TypeDescription * pTypeDescr ) SAL_THROW( () )
 {
     switch (pTypeDescr->eTypeClass)
     {
@@ -86,19 +86,19 @@ inline sal_Bool SAL_CALL cppu_relatesToInterface( typelib_TypeDescription * pTyp
         case typelib_TypeClass_INTERFACE:
         case typelib_TypeClass_UNION: // might relate to interface
         case typelib_TypeClass_ANY: // might relate to interface
-            return sal_True;
+            return true;
         case typelib_TypeClass_SEQUENCE:
         case typelib_TypeClass_STRUCT:
         case typelib_TypeClass_EXCEPTION:
         {
             typelib_TypeDescription * pTD = 0;
             TYPELIB_DANGER_GET( &pTD, ((typelib_IndirectTypeDescription *)pTypeDescr)->pType );
-            sal_Bool bRel = cppu_relatesToInterface( pTD );
+            bool bRel = cppu_relatesToInterface( pTD );
             TYPELIB_DANGER_RELEASE( pTD );
             return bRel;
         }
         }
-        return sal_False;
+        return false;
     }
     case typelib_TypeClass_STRUCT:
     case typelib_TypeClass_EXCEPTION:
@@ -113,7 +113,7 @@ inline sal_Bool SAL_CALL cppu_relatesToInterface( typelib_TypeDescription * pTyp
             case typelib_TypeClass_INTERFACE:
             case typelib_TypeClass_UNION: // might relate to interface
             case typelib_TypeClass_ANY: // might relate to interface
-                return sal_True;
+                return true;
 //              case typelib_TypeClass_TYPEDEF:
             case typelib_TypeClass_SEQUENCE:
             case typelib_TypeClass_STRUCT:
@@ -121,10 +121,10 @@ inline sal_Bool SAL_CALL cppu_relatesToInterface( typelib_TypeDescription * pTyp
             {
                 typelib_TypeDescription * pTD = 0;
                 TYPELIB_DANGER_GET( &pTD, pTypes[nPos] );
-                sal_Bool bRel = cppu_relatesToInterface( pTD );
+                bool bRel = cppu_relatesToInterface( pTD );
                 TYPELIB_DANGER_RELEASE( pTD );
                 if (bRel)
-                    return sal_True;
+                    return true;
             }
             }
         }
@@ -135,21 +135,29 @@ inline sal_Bool SAL_CALL cppu_relatesToInterface( typelib_TypeDescription * pTyp
     case typelib_TypeClass_UNION: // might relate to interface
     case typelib_TypeClass_ANY: // might relate to interface
     case typelib_TypeClass_INTERFACE:
-        return sal_True;
+        return true;
     }
-    return sal_False;
+    return false;
 }
 
+/** Determines whether given type is a cpp simple type, e.g. int, enum.<br>
+    @param eTypeClass type class of type
+    @return true if type is a cpp simple type, false otherwise
+*/
+inline bool cppu_isSimpleType( typelib_TypeClass eTypeClass ) SAL_THROW( () )
+{
+    return (eTypeClass <= typelib_TypeClass_ENUM &&
+            eTypeClass != typelib_TypeClass_STRING &&
+            eTypeClass != typelib_TypeClass_ANY &&
+            eTypeClass != typelib_TypeClass_TYPE);
+}
 /** Determines whether given type is a cpp simple type, e.g. int, enum.<br>
     @param pTypeDescr type description of type
     @return true if type is a cpp simple type, false otherwise
 */
-inline sal_Bool SAL_CALL cppu_isSimpleType( typelib_TypeDescription * pTypeDescr ) throw ()
+inline bool cppu_isSimpleType( typelib_TypeDescription * pTypeDescr ) SAL_THROW( () )
 {
-    return (pTypeDescr->eTypeClass <= typelib_TypeClass_ENUM &&
-            pTypeDescr->eTypeClass != typelib_TypeClass_STRING &&
-            pTypeDescr->eTypeClass != typelib_TypeClass_ANY &&
-            pTypeDescr->eTypeClass != typelib_TypeClass_TYPE);
+    return cppu_isSimpleType( pTypeDescr->eTypeClass );
 }
 
 #endif
