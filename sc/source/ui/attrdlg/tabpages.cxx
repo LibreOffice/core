7416709dcc04605d76c7cfbf930da4fe8a909a7b
Martin Hollmichel <mh@openoffice.org>
2001-10-23 10:02:58 +0000

    del: segmentc.hxx

--- sc/source/ui/attrdlg/tabpages.cxx
+++ sc/source/ui/attrdlg/tabpages.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabpages.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-17 12:14:02 $
+ *  last change: $Author: mh $ $Date: 2001-10-23 11:02:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,11 +63,6 @@
 #include "ui_pch.hxx"
 #endif
 
-#pragma hdrstop
-
-#ifndef PCH
-#include <segmentc.hxx>
-#endif
 
 #include "global.hxx"
 #include "document.hxx"
@@ -87,12 +82,9 @@ static USHORT pProtectionRanges[] =
     0
 };
 
-SEG_EOFGLOBALS()
-
 //========================================================================
 // Zellschutz-Tabpage:
 //========================================================================
-#pragma SEG_FUNCDEF(tabpages_18)
 
 ScTabPageProtection::ScTabPageProtection( Window*           pParent,
                                           const SfxItemSet& rCoreAttrs )
@@ -124,14 +116,12 @@ ScTabPageProtection::ScTabPageProtection( Window*           pParent,
 }
 
 // -----------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_19)
 
 __EXPORT ScTabPageProtection::~ScTabPageProtection()
 {
 }
 
 //------------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_20)
 
 USHORT* __EXPORT ScTabPageProtection::GetRanges()
 {
@@ -139,7 +129,6 @@ USHORT* __EXPORT ScTabPageProtection::GetRanges()
 }
 
 // -----------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_1a)
 
 SfxTabPage* __EXPORT ScTabPageProtection::Create( Window*           pParent,
                                                   const SfxItemSet& rAttrSet )
@@ -148,7 +137,6 @@ SfxTabPage* __EXPORT ScTabPageProtection::Create( Window*           pParent,
 }
 
 //------------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_1c)
 
 void __EXPORT ScTabPageProtection::Reset( const SfxItemSet& rCoreAttrs )
 {
@@ -193,7 +181,6 @@ void __EXPORT ScTabPageProtection::Reset( const SfxItemSet& rCoreAttrs )
 }
 
 // -----------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_1b)
 
 BOOL __EXPORT ScTabPageProtection::FillItemSet( SfxItemSet& rCoreAttrs )
 {
@@ -228,7 +215,6 @@ BOOL __EXPORT ScTabPageProtection::FillItemSet( SfxItemSet& rCoreAttrs )
 }
 
 //------------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_22)
 
 int __EXPORT ScTabPageProtection::DeactivatePage( SfxItemSet* pSet )
 {
@@ -239,7 +225,6 @@ int __EXPORT ScTabPageProtection::DeactivatePage( SfxItemSet* pSet )
 }
 
 //------------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_1d)
 
 IMPL_LINK( ScTabPageProtection, ButtonClickHdl, TriStateBox*, pBox )
 {
@@ -269,7 +254,6 @@ IMPL_LINK( ScTabPageProtection, ButtonClickHdl, TriStateBox*, pBox )
 }
 
 //------------------------------------------------------------------------
-#pragma SEG_FUNCDEF(tabpages_23)
 
 void ScTabPageProtection::UpdateButtons()
 {
@@ -294,228 +278,3 @@ void ScTabPageProtection::UpdateButtons()
         aBtnHideFormula.Enable( bEnable );
     }
 }
-
-/*------------------------------------------------------------------------
-
-    $Log: not supported by cvs2svn $
-    Revision 1.1.1.1  2000/09/18 16:44:53  hr
-    initial import
-
-    Revision 1.73  2000/09/17 14:08:55  willem.vandorp
-    OpenOffice header added.
-
-    Revision 1.72  2000/08/31 16:38:19  willem.vandorp
-    Header and footer replaced
-
-    Revision 1.71  1997/12/05 19:05:16  ANK
-    Includes geaendert
-
-
-      Rev 1.70   05 Dec 1997 20:05:16   ANK
-   Includes geaendert
-
-      Rev 1.69   08 Apr 1997 18:53:42   NN
-   #38543# DontCare fuer alle Flags zusammen ein/aus
-
-      Rev 1.68   29 Oct 1996 14:04:38   NN
-   ueberall ScResId statt ResId
-
-      Rev 1.67   23 Jan 1996 11:53:46   MO
-   GetOldItem() fuer Standard-Button
-
-      Rev 1.66   04 Dec 1995 10:09:28   MO
-   neuer Link
-
-      Rev 1.65   28 Nov 1995 19:02:40   MO
-   303a: HasExchangeSupport entfernt
-
-      Rev 1.64   08 Nov 1995 13:04:38   MO
-   301-Aenderungen
-
-      Rev 1.63   12 Sep 1995 10:56:14   MO
-   FixedText jetzt FixedInfo
-
-      Rev 1.62   01 Aug 1995 20:23:04   MO
-   Reset-Enable/Disable korrigiert
-
-      Rev 1.61   28 Jul 1995 11:40:34   MO
-   FillItemSet korrigiert
-
-      Rev 1.60   24 Jul 1995 14:00:58   MO
-   EXPORT
-
-      Rev 1.59   05 Jul 1995 22:24:42   MO
-   ExchangeSupport
-
-      Rev 1.58   30 Jun 1995 17:56:00   HJS
-   exports fuer create
-
-      Rev 1.57   22 May 1995 17:33:12   MO
-   Schuetzen/Ausgabe Gruppen
-
-      Rev 1.56   22 May 1995 13:03:40   MO
-   InfoText in BoldLight
-
-      Rev 1.55   18 May 1995 14:13:24   MO
-   Alignment Seite entfernt
-
-      Rev 1.54   21 Apr 1995 18:48:40   MO
-   ImageButtons
-
-      Rev 1.53   15 Mar 1995 11:41:20   NN
-   SaveValue-Aufruf am TriState-Button bei Alignment::Reset
-
-      Rev 1.52   13 Mar 1995 16:27:08   MO
-   * x10-Korrektur fuer MetricFields entfernt
-
-      Rev 1.51   23 Feb 1995 08:27:44   KH
-   Wer braucht "\" in Funktionsaufrufen ?
-
-      Rev 1.50   07 Feb 1995 09:21:00   MO
-   * Fix beim Normalize der Metrikfeldwerte bis CL es in SV fixed.
-
-      Rev 1.49   04 Feb 1995 13:58:02   MO
-   * Reset()-Handler aufgeraeumt
-   * Suche, ob Einstellungen verloren gehen (noch nicht beendet!)
-
-      Rev 1.48   27 Jan 1995 13:06:54   TRI
-   __EXPORT bei virtuellen Methoden eingebaut
-
-      Rev 1.47   27 Jan 1995 10:29:08   MO
-   * NumberFormat-Seite entfernt (jetzt Svx-Page)
-   + GetRanges()-Methode
-
-      Rev 1.46   22 Jan 1995 15:09:14   SC
-   2.37 Anpassung
-
-      Rev 1.45   19 Jan 1995 15:19:24   TRI
-   __EXPORT vor verschiedene LinkHandler gesetzt
-
-      Rev 1.44   18 Jan 1995 13:07:42   TRI
-   Pragmas zur Segementierung eingebaut
-
-      Rev 1.43   18 Jan 1995 09:56:48   MO
-   Bedingte Auswertung in ScTabPageNumber::Reset() durch if-else ersetzt
-
-      Rev 1.42   10 Jan 1995 11:37:46   MO
-   Erste verallgemeinerungs-faehige Version der Zahlenformat-Page.
-   ScNumberInfoItem ersetzt ScDocumentItem und ScCellItem.
-   Core wird nicht mehr ueber das Dokument aktualisiert,
-   sondern die zu aktualisierenden Daten werden ueber das InfoItem
-   wieder in den Set gesteckt.
-
-      Rev 1.41   22 Dec 1994 15:52:20   MO
-   switches haben jetzt alle ein default (Mac-Compiler)
-
-      Rev 1.40   21 Dec 1994 17:56:50   MO
-   SVX_ORIENTATION_STACKED aktiviert
-
-      Rev 1.39   14 Dec 1994 13:50:30   MO
-   Globale ResIds jetzt in sc.hrc
-
-      Rev 1.38   07 Dec 1994 09:54:08   MO
-   SfxMap -> SfxWhichMap (Sfx 233.b)
-
-      Rev 1.37   21 Nov 1994 15:56:38   MO
-   FontTabpage entfernt
-
-      Rev 1.36   10 Nov 1994 20:45:02   MO
-   EditHdl-Aufruf nur mit Edit-Objekt
-   0 = UpdateOptions nicht ausfuehren
-
-      Rev 1.35   10 Nov 1994 17:24:52   MO
-   Zahlenformate nur bei Aenderungen schreiben
-
-      Rev 1.34   09 Nov 1994 17:55:34   MO
-
-      Rev 1.33   09 Nov 1994 11:16:04   MO
-   Options-Update aus Format-String
-
-      Rev 1.32   09 Nov 1994 09:27:18   MO
-   Zeilenumbruch-Attribut wird jetzt wieder geschrieben
-
-      Rev 1.31   08 Nov 1994 19:44:00   MO
-
-      Rev 1.30   07 Nov 1994 19:17:18   MO
-
-      Rev 1.28   07 Nov 1994 09:30:22   MO
-   Zahlenformat in Arbeit
-
-      Rev 1.27   03 Nov 1994 15:14:12   MO
-   Fehler beim Init behoben
-
-      Rev 1.26   02 Nov 1994 21:13:48   MO
-   Zahlenformatseite ohne Hinzufuegen/Entfernen
-
-      Rev 1.25   27 Oct 1994 14:35:04   MO
-   Attribute in die Core und umgekehrt
-
-      Rev 1.24   27 Oct 1994 10:47:32   MO
-   Attribute aus der Core lesen
-
-      Rev 1.23   25 Oct 1994 11:46:16   MO
-   Umrandungs-Vorgaben eingeaut
-
-      Rev 1.22   21 Oct 1994 17:03:30   MO
-   Neuen FrameSelektor eingebaut
-
-      Rev 1.21   19 Oct 1994 10:59:54   MO
-   Voreinstellungen und Schatten anwaehlbar
-
-      Rev 1.20   17 Oct 1994 17:31:46   MO
-
-      Rev 1.19   17 Oct 1994 13:23:56   MO
-   Neuer FrameSelektor
-
-      Rev 1.18   14 Oct 1994 09:26:44   MO
-   'Reanpassung' SV229
-
-      Rev 1.17   13 Oct 1994 12:05:24   MO
-   FramSelector in Umrandungs-Seite eingebaut
-
-      Rev 1.16   12 Oct 1994 18:14:12   NN
-   MarginAttr-Werte als Twips interpretiert
-
-      Rev 1.15   06 Oct 1994 11:43:52   MO
-   Anpassungen an Sfx 228g
-
-      Rev 1.14   05 Oct 1994 19:59:34   MO
-   BitmapRadioButtons fuer Ausrichtungs-Seite
-
-      Rev 1.13   05 Oct 1994 12:23:44   MO
-   Sfx-Anpassungen (228f)
-
-      Rev 1.12   04 Oct 1994 15:56:58   MO
-   Umstellung auf allgemeine Sfx-Loesung
-
-      Rev 1.11   27 Sep 1994 16:10:58   MO
-
-      Rev 1.9   22 Sep 1994 15:31:26   MO
-   Neuer Zahlenformat-Entwurf
-
-      Rev 1.8   20 Sep 1994 19:53:46   MO
-   No change.
-
-      Rev 1.7   20 Sep 1994 11:33:00   MO
-   aFont-Problem gefixed
-
-      Rev 1.6   19 Sep 1994 13:18:20   MO
-
-      Rev 1.5   14 Sep 1994 12:18:22   MO
-
-      Rev 1.4   13 Sep 1994 16:31:00   MO
-   Zellschutzseite implementiert
-
-      Rev 1.3   12 Sep 1994 10:05:20   MO
-   Fontdialog arbeitet mit Fonthoehe in Twips
-
-      Rev 1.2   09 Sep 1994 12:57:00   MO
-   Hintergrund- und Zeichenseiten rudimentaer implementiert
-
-      Rev 1.0   01 Sep 1994 12:45:24   MO
-   Initial revision.
-
-------------------------------------------------------------------------*/
-
-#pragma SEG_EOFMODULE
-
