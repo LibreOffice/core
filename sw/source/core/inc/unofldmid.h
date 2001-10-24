5477839b4eaac1e628b8e0d56ee90b463e427684
jp <jp@openoffice.org>
2001-10-24 17:46:05 +0000

    Task #88180#: uno-memberids for fields

---
+++ sw/source/core/inc/unofldmid.h
@@ -0,0 +1,95 @@
+/*************************************************************************
+ *
+ *  $RCSfile: unofldmid.h,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: jp $ $Date: 2001-10-24 18:46:05 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef SW_UNOFLDMID_H
+#define SW_UNOFLDMID_H
+
+class String;
+namespace com { namespace sun { namespace star { namespace uno {
+    class Any;
+} } } }
+
+/******************************************************************************
+ *
+ ******************************************************************************/
+//Abbildung der Properties auf den Descriptor
+#define FIELD_PROP_PAR1             10
+#define FIELD_PROP_PAR2             11
+#define FIELD_PROP_PAR3             12
+#define FIELD_PROP_FORMAT           13
+#define FIELD_PROP_SUBTYPE          14
+#define FIELD_PROP_BOOL1            15
+#define FIELD_PROP_BOOL2            16
+#define FIELD_PROP_DATE             17
+#define FIELD_PROP_USHORT1          18
+#define FIELD_PROP_USHORT2          19
+#define FIELD_PROP_BYTE1            20
+#define FIELD_PROP_DOUBLE           21
+#define FIELD_PROP_BOOL3            22
+#define FIELD_PROP_PAR4             23
+#define FIELD_PROP_SHORT1           24
+#define FIELD_PROP_DATE_TIME        25
+#define FIELD_PROP_PROP_SEQ         26
+#define FIELD_PROP_LOCALE           27
+
+
+String& GetString( const com::sun::star::uno::Any&, String& rStr );
+
+#endif
