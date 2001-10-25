7a19970c485c3ef1b30e47a57202d484cd69969c
Martin Hollmichel <mh@openoffice.org>
2001-10-25 15:54:08 +0000

    del: segmentc.hxx

--- sw/source/ui/dochdl/selglos.cxx
+++ sw/source/ui/dochdl/selglos.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: selglos.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: fme $ $Date: 2001-06-01 10:52:08 $
+ *  last change: $Author: mh $ $Date: 2001-10-25 16:54:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,7 +66,6 @@
 #pragma hdrstop
 
 #include "swtypes.hxx"
-#include "segmentc.hxx"
 
 #include "selglos.hxx"
 
@@ -110,64 +109,3 @@ IMPL_LINK(SwSelGlossaryDlg, DoubleClickHdl, ListBox*, pBox)
     EndDialog(RET_OK);
     return 0;
 }
-/*------------------------------------------------------------------------
-
-    $Log: not supported by cvs2svn $
-    Revision 1.1.1.1  2000/09/18 17:14:35  hr
-    initial import
-
-    Revision 1.16  2000/09/18 16:05:22  willem.vandorp
-    OpenOffice header added.
-
-    Revision 1.15  2000/04/13 08:22:23  os
-    UNICODE
-
-    Revision 1.14  1999/10/25 06:44:30  os
-    #68967# HelpButton and DoubleClickHdl
-
-    Revision 1.13  1997/11/03 12:13:06  MA
-    precomp entfernt
-
-
-      Rev 1.12   03 Nov 1997 13:13:06   MA
-   precomp entfernt
-
-      Rev 1.11   14 Nov 1996 15:23:02   TRI
-   includes
-
-      Rev 1.10   11 Nov 1996 09:35:36   MA
-   ResMgr
-
-      Rev 1.9   28 Aug 1996 10:27:36   OS
-   includes
-
-      Rev 1.8   24 Nov 1995 16:58:42   OM
-   PCH->PRECOMPILED
-
-      Rev 1.7   25 Oct 1994 17:08:08   ER
-   add: PCH
-
-      Rev 1.6   26 Feb 1994 01:59:36   ER
-   virt. dtor mit export
-
-      Rev 1.5   25 Feb 1994 20:51:54   ER
-   virtuellen dtor wegen export ueberladen
-
-      Rev 1.4   17 Feb 1994 08:41:20   MI
-   SEG_FUNCDEFS ausgefuellt
-
-      Rev 1.3   16 Feb 1994 20:02:32   MI
-   SEG_FUNCDEFS ausgefuellt
-
-      Rev 1.2   16 Feb 1994 16:21:24   MI
-   Pragmas zurechtgerueckt
-
-      Rev 1.1   28 Jan 1994 11:41:02   MI
-   TCOV() entfernt, SW_... nach SEG_... umbenannt
-
-      Rev 1.0   25 Jan 1994 16:01:06   VB
-   Initial revision.
-
-------------------------------------------------------------------------*/
-
-
