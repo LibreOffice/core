eb0b369d09c66dd413cd6874c93f6181d77204af
Daniel Boelzle <dbo@openoffice.org>
2001-10-26 13:23:30 +0000

    #93739#

--- bridges/source/cpp_uno/gcc3_linux_intel/makefile.mk
+++ bridges/source/cpp_uno/gcc3_linux_intel/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: dbo $ $Date: 2001-10-19 13:32:39 $
+#   last change: $Author: dbo $ $Date: 2001-10-26 14:23:30 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -76,7 +76,7 @@ NO_BSYMBOLIC=TRUE
 
 # --- Files --------------------------------------------------------
 
-.IF "$(COM)$(OS)$(CPU)" == "GCCLINUXI"
+.IF "$(COM)$(OS)$(CPU)$(COMNAME)" == "GCCLINUXIgcc3"
 
 .IF "$(cppu_no_leak)" == ""
 CFLAGS += -DLEAK_STATIC_DATA
