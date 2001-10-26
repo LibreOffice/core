fb8b533ad7ae44a4eaa846bb9216466b1530038d
Frank Schönheit <fs@openoffice.org>
2001-10-26 07:48:11 +0000

    #92988# +ControlActions.idl

--- offapi/com/sun/star/ui/dialogs/makefile.mk
+++ offapi/com/sun/star/ui/dialogs/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: fs $ $Date: 2001-09-14 09:52:04 $
+#   last change: $Author: fs $ $Date: 2001-10-26 08:48:11 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ PACKAGE=com$/sun$/star$/ui$/dialogs
 # ------------------------------------------------------------------------
 
 IDLFILES=\
+    ControlActions.idl\
     ExecutableDialogException.idl\
     ExecutableDialogResults.idl\
     XExecutableDialog.idl\
