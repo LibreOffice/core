57ee93b8e0ce5d7ddc73a68ce0d4d8da9ee815ff
Jens-Heiner Rechtien <hr@openoffice.org>
2001-10-26 11:33:35 +0000

    #92924# exception specifications

--- dbaccess/source/ui/inc/sbamultiplex.hxx
+++ dbaccess/source/ui/inc/sbamultiplex.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sbamultiplex.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2001-09-13 14:14:47 $
+ *  last change: $Author: hr $ $Date: 2001-10-26 12:33:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -151,8 +151,8 @@ namespace dbaui
     public:
         OSbaWeakSubObject(::cppu::OWeakObject& rParent) : m_rParent(rParent) { }
 
-        virtual void SAL_CALL acquire() throw(::com::sun::star::uno::RuntimeException) { m_rParent.acquire(); }
-        virtual void SAL_CALL release() throw(::com::sun::star::uno::RuntimeException) { m_rParent.release(); }
+        virtual void SAL_CALL acquire() throw() { m_rParent.acquire(); }
+        virtual void SAL_CALL release() throw() { m_rParent.release(); }
     };
 
     //------------------------------------------------------------------
