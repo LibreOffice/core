1b24384a783b02bf2fd72e41fe9752a264dd0d20
Jens-Heiner Rechtien <hr@openoffice.org>
2001-10-26 11:55:08 +0000

    #92924# exception specifications

--- dbaccess/source/ui/inc/sbagrid.hxx
+++ dbaccess/source/ui/inc/sbagrid.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sbagrid.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2001-10-26 12:43:21 $
+ *  last change: $Author: hr $ $Date: 2001-10-26 12:55:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -134,11 +134,11 @@ namespace dbaui
 
         // XTypeProvider
         virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type > SAL_CALL getTypes(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< sal_Int8 > SAL_CALL getImplementationId(  ) throw ();
+        virtual ::com::sun::star::uno::Sequence< sal_Int8 > SAL_CALL getImplementationId(  ) throw (::com::sun::star::uno::RuntimeException);
 
         // ::com::sun::star::lang::XServiceInfo
-        ::rtl::OUString SAL_CALL getImplementationName() throw( ::com::sun::star::uno::RuntimeException );
-        virtual ::comphelper::StringSequence SAL_CALL getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException);
+        ::rtl::OUString SAL_CALL getImplementationName() throw();
+        virtual ::comphelper::StringSequence SAL_CALL getSupportedServiceNames() throw();
         // need by registration
         static ::rtl::OUString getImplementationName_Static() throw( ::com::sun::star::uno::RuntimeException );
         static ::com::sun::star::uno::Sequence< ::rtl::OUString > getSupportedServiceNames_Static(void) throw( ::com::sun::star::uno::RuntimeException );
