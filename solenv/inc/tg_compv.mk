5eb6f60e1db9e4346b3db240199f596a88d703a2
Hans-Joachim Lankenau <hjs@openoffice.org>
2001-10-26 10:57:55 +0000

    #93739# provide COMID and COMNAME depending on compiler version

---
+++ solenv/inc/tg_compv.mk
@@ -0,0 +1,112 @@
+#*************************************************************************
+#
+#   $RCSfile: tg_compv.mk,v $
+#
+#   $Revision: 1.1 $
+#
+#   last change: $Author: hjs $ $Date: 2001-10-26 11:57:55 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+.IF "$(COM)"=="GCC"
+CFLAGSVERSION=-dumpversion
+CFLAGSNUMVERSION=-dumpversion |& awk -v num=true -f $(SOLARENV)$/bin$/getcompver.awk
+.ENDIF
+
+.IF "$(COM)"=="MSC"
+CFLAGSVERSION= |& $(AWK) -f $(SOLARENV)$/bin$/getcompver.awk
+CFLAGSNUMVERSION= |& $(AWK) -v num=true -f $(SOLARENV)$/bin$/getcompver.awk
+.ENDIF
+
+.IF "$(COM)"=="C52" || "$(COM)"=="C40" || "$(COM)"=="sunpro"
+CFLAGSVERSION= -V |& nawk -f $(SOLARENV)$/bin$/getcompver.awk
+CFLAGSNUMVERSION= -V |& nawk -v num=true -f $(SOLARENV)$/bin$/getcompver.awk
+.ENDIF
+
+# that's the version known by the specific
+#compiler
+CCVER:=$(shell $(CC) $(CFLAGSVERSION))
+
+# and a computed integer for comparing
+# each point seperated token blown up to 4 digits
+CCNUMVER:=$(shell $(CC) $(CFLAGSNUMVERSION))
+
+.IF "$(COM)"=="MSC"
+.IF "$(CCNUMVER)">="001200000000"
+COMID=MSC
+COMNAME=msci
+.ENDIF
+.ENDIF
+
+.IF "$(COM)"=="GCC"
+.IF "$(CCNUMVER)">="000200950000"
+COMID=GCC
+COMNAME=gcc2
+.ENDIF
+.IF "$(CCNUMVER)">="000300000001"
+COMID=gcc3
+COMNAME=gcc3
+.ENDIF
+.ENDIF
+
+.IF "$(COM)"=="C52" || "$(COM)"=="C40" || "$(COM)"=="sunpro"
+.IF "$(CCNUMVER)">="00050002"
+COMID=C52
+COMNAME=sunpro5
+.ENDIF
+.ENDIF
+
+CDEFS+=-DCPPU_ENV=$(COMNAME)
+
