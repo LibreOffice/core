9f6c0f56c2fe04566fb805ef68943e3dc487583d
Joachim Lingner <jl@openoffice.org>
2001-10-31 14:38:50 +0000

    *** empty log message ***

---
+++ stoc/test/javavm/testcomponent/makefile.mk
@@ -0,0 +1,16 @@
+#*************************************************************************
+#*
+#*    $RCSfile: makefile.mk,v $
+#*
+#*    Creation date     09/19/2001
+#*    last change       $Author: jl $ $Date: 2001-10-31 15:38:50 $
+#*    $Revision: 1.1 $
+#*
+#*    Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+#*
+#*************************************************************************
+TARGET=JavaTestComponent
+PRJ=../../..
+
+.INCLUDE : ant.mk
+ALLTAR: ANTBUILD
