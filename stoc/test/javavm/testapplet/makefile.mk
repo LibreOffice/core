92eb40e6b2324c904eba2c0d37b74aafe02e79d4
Joachim Lingner <jl@openoffice.org>
2001-11-22 12:12:28 +0000

    *** empty log message ***

---
+++ stoc/test/javavm/testapplet/makefile.mk
@@ -0,0 +1,16 @@
+#*************************************************************************
+#*
+#*    $RCSfile: makefile.mk,v $
+#*
+#*    Creation date     09/19/2001
+#*    last change       $Author: jl $ $Date: 2001-11-22 13:12:08 $
+#*    $Revision: 1.1 $
+#*
+#*    Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+#*
+#*************************************************************************
+TARGET=TestApplet
+PRJ=../../..
+
+.INCLUDE : ant.mk
+ALLTAR: ANTBUILD
