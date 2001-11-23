e1646f8563f05a1f1869767e380121d6e9f4ec6e
Andreas Bille <abi@openoffice.org>
2001-11-23 12:59:19 +0000

    Adding rtti-support, which was previously the default

--- xmlhelp/util/makefile.mk
+++ xmlhelp/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: abi $ $Date: 2001-06-13 16:24:24 $
+#   last change: $Author: abi $ $Date: 2001-11-23 13:59:19 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,6 +78,9 @@ NO_BSYMBOLIC=TRUE
 
 .IF "$(GUI)"=="WNT"
 CFLAGS+=/GR
+.ENDIF
+.IF "$(COM)"=="GCC"
+CFLAGSCXX+=-frtti
 .ENDIF
 
 .IF "$(SABLOT3RDLIB)"==""
