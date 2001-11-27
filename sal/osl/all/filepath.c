94a44a2259a72b6b515ed7842879c538cf17619e
Martin Hollmichel <mh@openoffice.org>
2001-11-27 17:34:27 +0000

    chg: Syntax #65293#

--- sal/osl/all/filepath.c
+++ sal/osl/all/filepath.c
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: filepath.c,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hro $ $Date: 2001-11-22 12:03:11 $
+ *  last change: $Author: mh $ $Date: 2001-11-27 18:34:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -68,7 +68,7 @@ static sal_uInt32 SAL_CALL osl_calcTextWidthWrapper( rtl_uString *ustrText, void
 {
     if ( pvContext )
     {
-        return ((oslCalcTextWidthFunc)pvContext)( ustrText )
+        return ((oslCalcTextWidthFunc)pvContext)( ustrText );
     }
     else
         return osl_defCalcTextWidthEx( ustrText, NULL );
