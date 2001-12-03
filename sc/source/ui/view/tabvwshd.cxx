f5b8b2f7cf543b94754b3d2520367d01201c7c11
Niklas Nebel <nn@openoffice.org>
2001-12-03 19:32:45 +0000

    #95513# GetDialogParent: if a ref-dialog is open, use it as parent

--- sc/source/ui/view/tabvwshd.cxx
+++ sc/source/ui/view/tabvwshd.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabvwshd.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:45:10 $
+ *  last change: $Author: nn $ $Date: 2001-12-03 20:32:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -73,6 +73,7 @@
 
 // INCLUDE ---------------------------------------------------------------
 
+#include <sfx2/childwin.hxx>
 #include <sfx2/request.hxx>
 #include <sfx2/topfrm.hxx>
 #include <so3/ipenv.hxx>
@@ -98,6 +99,23 @@
 
 Window* ScTabViewShell::GetDialogParent()
 {
+    //  #95513# if a ref-input dialog is open, use it as parent
+    //  (necessary when a slot is executed from the dialog's OK handler)
+    if ( nCurRefDlgId && nCurRefDlgId == SC_MOD()->GetCurRefDlgId() )
+    {
+        SfxViewFrame* pViewFrm = GetViewFrame();
+        if ( pViewFrm->HasChildWindow(nCurRefDlgId) )
+        {
+            SfxChildWindow* pChild = pViewFrm->GetChildWindow(nCurRefDlgId);
+            if (pChild)
+            {
+                Window* pWin = pChild->GetWindow();
+                if (pWin && pWin->IsVisible())
+                    return pWin;
+            }
+        }
+    }
+
     ScDocShell* pDocSh = GetViewData()->GetDocShell();
     if ( pDocSh->IsOle() )
     {
