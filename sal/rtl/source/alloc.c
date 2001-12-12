60cff8a195c7e3ed02f0c0f7fcb6befbfcc4602a
Christof Pintaske <cp@openoffice.org>
2001-12-12 18:47:19 +0000

    #95880# undefine MAP_ANON to enable build on Solaris 8 (X86)

--- sal/rtl/source/alloc.c
+++ sal/rtl/source/alloc.c
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: alloc.c,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: mhu $ $Date: 2001-12-04 15:10:51 $
+ *  last change: $Author: cp $ $Date: 2001-12-12 19:47:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -126,6 +126,14 @@ static sal_uInt32 __rtl_memory_vmpagesize (void)
 
 #ifndef PROT_HEAP
 #define PROT_HEAP (PROT_READ | PROT_WRITE | PROT_EXEC)
+#endif
+
+/* #95880# building on Solaris 8 provides MAP_ANON, but it
+   is not available on Solaris 7 */
+#if defined (SOLARIS) && defined (INTEL)
+#ifdef MAP_ANON
+#undef MAP_ANON
+#endif
 #endif
 
 #ifndef MAP_ANON
