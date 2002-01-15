b55762059b13585c922f860cbd08c1cddc9bde0b
Jens-Heiner Rechtien <hr@openoffice.org>
2002-01-15 12:06:46 +0000

    #95549#: fix security check

--- solenv/bin/deliver.pl
+++ solenv/bin/deliver.pl
@@ -5,9 +5,9 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 #
 #   $RCSfile: deliver.pl,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: hr $ $Date: 2002-01-08 10:58:46 $
+#   last change: $Author: hr $ $Date: 2002-01-15 13:06:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,7 +77,7 @@ use File::Path;
 
 ( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
-$id_str = ' $Revision: 1.18 $ ';
+$id_str = ' $Revision: 1.19 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -313,7 +313,7 @@ sub init_globals {
     my $work_stamp    = $ENV{'WORK_STAMP'};
 
     # special security check for release engineers
-    if ( $updater eq 'TRUE' && !defined($build_sosl) && !$opt_force) {
+    if ( defined($updater) && !defined($build_sosl) && !$opt_force) {
         my $path = cwd();
         if ( $path !~ /$work_stamp/o ) {
             print_error("can't deliver from local directory to SOLARVERSION");
