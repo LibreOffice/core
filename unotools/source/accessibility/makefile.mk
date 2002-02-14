0bc6a0e165e8d2577dc84dab772ee28c1462a2a6
Sascha Ballach <sab@openoffice.org>
2002-02-14 12:54:20 +0000

    #95584#; put this stuff in a own lib

--- unotools/source/accessibility/makefile.mk
+++ unotools/source/accessibility/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: sab $ $Date: 2002-02-05 16:38:10 $
+#   last change: $Author: sab $ $Date: 2002-02-14 13:54:20 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -63,7 +63,7 @@
 PRJ=..$/..
 PRJINC=..$/..$/inc
 PRJNAME=unotools
-TARGET=property
+TARGET=accessibility
 
 ENABLE_EXCEPTIONS=TRUE
 
