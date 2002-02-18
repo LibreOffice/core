fa233bba5b77a9427578d7dd3fbb3ead5068eb0e
Armin Weiss <aw@openoffice.org>
2002-02-18 13:58:18 +0000

    #97016# Added ImpForceQuadratic(...) to have central support for keeping the aspect ratio for a rectangle.

--- sd/source/ui/inc/fupoor.hxx
+++ sd/source/ui/inc/fupoor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fupoor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2002-02-15 17:02:42 $
+ *  last change: $Author: aw $ $Date: 2002-02-18 14:58:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -179,6 +179,8 @@ class FuPoor
 
     // #97016#
     virtual SdrObject* CreateDefaultObject(const sal_uInt16 nID, const Rectangle& rRectangle);
+protected:
+    void ImpForceQuadratic(Rectangle& rRect);
 };
 
 
