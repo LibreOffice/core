a585185e84dbc4dc984780dfb39ce1e418c240c4
Jens-Heiner Rechtien <hr@openoffice.org>
2002-02-18 16:58:50 +0000

    #65293#: security check should be case insensitive

--- solenv/bin/deliver.pl
+++ solenv/bin/deliver.pl
@@ -5,9 +5,9 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 #
 #   $RCSfile: deliver.pl,v $
 #
-#   $Revision: 1.20 $
+#   $Revision: 1.21 $
 #
-#   last change: $Author: hr $ $Date: 2002-01-23 12:44:35 $
+#   last change: $Author: hr $ $Date: 2002-02-18 17:58:50 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,7 +77,7 @@ use File::Path;
 
 ( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
-$id_str = ' $Revision: 1.20 $ ';
+$id_str = ' $Revision: 1.21 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -331,7 +331,7 @@ sub init_globals {
     # special security check for release engineers
     if ( defined($updater) && !defined($build_sosl) && !$opt_force) {
         my $path = cwd();
-        if ( $path !~ /$work_stamp/o ) {
+        if ( $path !~ /$work_stamp/io ) {
             print_error("can't deliver from local directory to SOLARVERSION");
             print STDERR "\nDANGER! Release Engineer:\n";
             print STDERR "do you really want to deliver from $path to SOLARVERSION?\n";
