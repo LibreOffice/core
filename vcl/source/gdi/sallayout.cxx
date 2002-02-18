d9a5a5a558340c9451367c2174f25990170c96ef
Herbert Duerr <hdu@openoffice.org>
2002-02-18 08:08:18 +0000

    #97942# initial CTL support for VCL

---
+++ vcl/source/gdi/sallayout.cxx
@@ -0,0 +1,541 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sallayout.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: hdu $ $Date: 2002-02-18 09:08:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <cstdio>
+#include <math.h>
+
+#if defined(WIN32)
+#define M_PI 3.1415926536
+#include <malloc.h>
+#define alloca _alloca
+#endif
+
+//#define _SV_OUTDEV_CXX
+
+#ifndef REMOTE_APPSERVER
+#ifndef _SV_SVSYS_HXX
+#include <svsys.h>
+#endif
+#endif
+
+#ifndef REMOTE_APPSERVER
+#ifndef _SV_SALGDI_HXX
+#include <salgdi.hxx>
+#endif
+#else
+#ifndef _SV_RMOUTDEV_HXX
+#include <rmoutdev.hxx>
+#endif
+#endif // REMOTE_APPSERVER
+
+#ifndef _SV_SALLAYOUT_HXX
+#include <sallayout.hxx>
+#endif // _SV_SALLAYOUT_HXX
+
+#include <limits.h>
+
+// =======================================================================
+
+ImplLayoutArgs::ImplLayoutArgs( const xub_Unicode* pStr, int nLength,
+    int nFirstCharIndex, int nEndCharIndex )
+:   mpStr( pStr ),
+    mnLength( nLength ),
+    mnFirstCharIndex( nFirstCharIndex ),
+    mnEndCharIndex( nEndCharIndex ),
+    mnFlags( 0 ),
+    mnLayoutWidth( 0 ),
+    mpDXArray( NULL ),
+    maDrawPosition( 0, 0 ),
+    mnOrientation( 0 )
+{}
+
+// =======================================================================
+
+SalLayout::SalLayout( const ImplLayoutArgs& rArgs )
+:   mnRefCount( 1 ),
+    mnFirstCharIndex( rArgs.mnFirstCharIndex ),
+    mnEndCharIndex( rArgs.mnEndCharIndex ),
+    maDrawPosition( rArgs.maDrawPosition ),
+    mnOrientation( rArgs.mnOrientation )
+{}
+
+// -----------------------------------------------------------------------
+
+SalLayout::~SalLayout()
+{}
+
+// -----------------------------------------------------------------------
+
+void SalLayout::Reference() const
+{
+    ++mnRefCount;
+}
+
+// -----------------------------------------------------------------------
+
+void SalLayout::Release() const
+{
+    if( --mnRefCount <= 0 )
+    {
+        // const_cast because some compilers violate ANSI C++ spec
+        delete const_cast<SalLayout*>(this);
+    }
+}
+
+// -----------------------------------------------------------------------
+
+Point SalLayout::GetDrawPosition( const Point& rRelative ) const
+{
+    Point aPos = maDrawPosition;
+
+    if( mnOrientation == 0 )
+        aPos += rRelative;
+    else
+    {
+        // cache trigonometric results
+        static int nOldOrientation = 0;
+        static double fCos = 1.0, fSin = 0.0;
+        if( nOldOrientation != mnOrientation )
+        {
+            nOldOrientation = mnOrientation;
+            double fRad = mnOrientation * (M_PI / 1800.0);
+            fCos = cos( fRad );
+            fSin = cos( fRad );
+        }
+
+        long nX0 = rRelative.X();
+        long nY0 = rRelative.Y();
+        long nX = static_cast<long>( +fCos * nX0 - fSin * nY0 );
+        long nY = static_cast<long>( +fCos * nY0 + fSin * nX0 );
+        aPos += Point( nX, nY );
+    }
+
+    return aPos;
+}
+
+// =======================================================================
+
+GenericSalLayout::GenericSalLayout( const ImplLayoutArgs& rArgs )
+:   SalLayout( rArgs ),
+    mnGlyphCapacity(0), mnGlyphCount(0), mpGlyphItems( NULL )
+{}
+
+// -----------------------------------------------------------------------
+
+GenericSalLayout::~GenericSalLayout()
+{
+    delete[] mpGlyphItems ;
+}
+
+// -----------------------------------------------------------------------
+
+void GenericSalLayout::SetGlyphItems( GlyphItem* pGlyphItems, int nGlyphCount )
+{
+    mpGlyphItems = pGlyphItems;
+    mnGlyphCount = nGlyphCount;
+}
+
+// -----------------------------------------------------------------------
+
+int* GenericSalLayout::GetCharWidths() const
+{
+    // allocate char width array
+    int nCharCapacity = mnEndCharIndex - mnFirstCharIndex;
+    int* pCharWidths = new int[ nCharCapacity ];
+
+    // initialize character extents buffer
+    long* pMinPos = (long*)alloca( 2*nCharCapacity * sizeof(long) );
+    long* pMaxPos = pMinPos + nCharCapacity;
+
+    int i;
+    for( i = 0; i < nCharCapacity; ++i )
+    {
+        pMinPos[i] = LONG_MAX;
+        pMaxPos[i] = -1;
+    }
+
+    // determine cluster extents
+    const GlyphItem* pG = mpGlyphItems;
+    int nClusterIndex = 0;
+    for( i = mnGlyphCount; --i >= 0; ++pG )
+    {
+        // use cluster start to get char index
+        if( pG->mnFlags & GlyphItem::CLUSTER_START )
+            nClusterIndex = pG->mnCharIndex;
+
+        int n = nClusterIndex - mnFirstCharIndex;
+        if( n < 0 || n >= nCharCapacity )
+            continue;
+
+        // maximum left extent of character
+        long nXPos = pG->maLinearPos.X();
+        if( pMinPos[n] > nXPos )
+            pMinPos[n] = nXPos;
+
+        // maximum right extent of character
+        nXPos += pG->mnWidth;
+        if( pMaxPos[n] < nXPos )
+            pMaxPos[n] = nXPos;
+
+        // special case for zero width glyphs in cluster, e.g. in Thai
+        if( !pG->mnWidth )
+        {
+            int m = nClusterIndex - mnFirstCharIndex;
+            if( n != m )
+                pMinPos[m] = pMaxPos[m] = nXPos;
+        }
+    }
+
+    // set char width array
+    for( i = 0; i < nCharCapacity; )
+    {
+        long nClusterWidth = pMaxPos[i] - pMinPos[i];
+
+        // ligature glyphs correspond to more than one sal_Unicode, so
+        // some character widths are still uninitialized. This is solved
+        // by distributing the cluster width to the involved characters
+        int j = i;
+        while( ++j<nCharCapacity && pMaxPos[j]<0 );
+        int nClusterSize = j - i;
+        if( nClusterSize > 1 )
+        {
+            int nCharWidth = (nClusterWidth + (nClusterSize/2)) / nClusterSize;
+            while( --j > i )
+            {
+                pCharWidths[j] = nCharWidth;
+                nClusterWidth -= nCharWidth;
+            }
+        }
+
+        pCharWidths[i] = nClusterWidth;
+        i += nClusterSize;
+    }
+
+    return pCharWidths;
+}
+
+// -----------------------------------------------------------------------
+
+long GenericSalLayout::FillDXArray( long* pDXArray ) const
+{
+    int* pCharWidths = GetCharWidths();
+
+    long nWidth = 0;
+    for( int i = mnFirstCharIndex; i < mnEndCharIndex; ++i )
+    {
+        nWidth += pCharWidths[ i - mnFirstCharIndex ];
+        if( pDXArray )
+            pDXArray[ i - mnFirstCharIndex ] = nWidth;
+    }
+    delete[] pCharWidths;
+
+    return nWidth;
+}
+
+// -----------------------------------------------------------------------
+
+void GenericSalLayout::ApplyDXArray( const long* pDXArray )
+{
+    // get reference positions
+    int nChars = mnEndCharIndex - mnFirstCharIndex;
+    long* pNewDX = (long*)alloca( nChars * sizeof(long) );
+    FillDXArray( pNewDX );
+
+    // initialize flags for touched character
+    int i;
+    char* pTouched = (char*)alloca( nChars );
+    for( i = 0; i < nChars; ++i )
+        pTouched[ i ] = 0;
+
+    // get x base offset
+    GlyphItem* pG = mpGlyphItems;
+    long nBasePointX = -1;
+    for( i = 0; i < mnGlyphCount; ++i, ++pG )
+    {
+        int n = pG->mnCharIndex;
+        if( n < mnFirstCharIndex || n >= mnEndCharIndex )
+            continue;
+        if( nBasePointX >= 0 )  // break when initalized
+            break;
+        // initialize x base offset with first match glyph position
+        nBasePointX = pG->maLinearPos.X();
+    }
+
+    // adjust to requested positions
+    long nDelta = 0;
+    for(; i < mnGlyphCount; ++i, ++pG )
+    {
+        int n = pG->mnCharIndex;
+        if( n < mnEndCharIndex )
+        {
+            n -= mnFirstCharIndex;
+            if( (n >= 0) && !pTouched[ n ] )
+            {
+                // mark first character in cluster as touched
+                pTouched[ n ] = 1;
+
+                long nNewDelta = (n <= 0) ? 0 : pDXArray[ n-1 ];
+fprintf(stderr,"move p%d %d->%d\n",n,pG->maLinearPos.X(),nNewDelta);//###
+                nNewDelta += nBasePointX - pG->maLinearPos.X();
+                nDelta = nNewDelta;
+            }
+        }
+
+        pG->maLinearPos += Point( nDelta, 0 );
+    }
+
+    // adjust remaining glyphs
+    if( nDelta )
+        for(; i < mnGlyphCount; ++i, ++pG )
+            pG->maLinearPos += Point( nDelta, 0 );
+}
+
+// -----------------------------------------------------------------------
+
+void GenericSalLayout::Justify( long nNewWidth )
+{
+    int* pCharWidths = GetCharWidths();
+
+    int nOldWidth = FillDXArray( NULL );
+    if( !nOldWidth || nNewWidth==nOldWidth )
+        return;
+
+    double fFactor = (double)nNewWidth / nOldWidth;
+
+    GlyphItem* pG = mpGlyphItems;
+    const long nBasePos = maBasePoint.X();
+    for( int i = mnGlyphCount; --i >= 0; ++pG )
+    {
+        if( (pG->mnCharIndex >= mnFirstCharIndex)
+        &&  (pG->mnCharIndex < mnEndCharIndex) )
+        {
+            long nOldPos = pG->maLinearPos.X();
+            long nNewPos = nBasePos + (long)(fFactor * (nOldPos - nBasePos) + 0.5);
+            if( nNewPos != nOldPos )
+                pG->maLinearPos += Point( nNewPos-nOldPos, 0 );
+        }
+    }
+}
+
+// -----------------------------------------------------------------------
+
+int GenericSalLayout::GetTextBreak( long nMaxWidth ) const
+{
+    int* pCharWidths = GetCharWidths();
+
+    long nWidth = 0;
+    for( int i = mnFirstCharIndex; i < mnEndCharIndex; ++i )
+    {
+        nWidth += pCharWidths[ i - mnFirstCharIndex ];
+        if( nWidth >= nMaxWidth )
+            return i;
+    }
+    delete[] pCharWidths;
+
+    return STRING_LEN;
+}
+
+// -----------------------------------------------------------------------
+
+int GenericSalLayout::GetNextGlyphs( int nLen, long* pGlyphs, Point& rPos, int& nStart ) const
+{
+    const GlyphItem* pG = mpGlyphItems + nStart;
+
+    // find next glyph in substring
+    bool bFirst = (nStart == 0);
+    for(; nStart < mnGlyphCount; ++nStart, ++pG )
+    {
+        int n = pG->mnCharIndex;
+        if( n < mnFirstCharIndex || n >= mnEndCharIndex )
+            continue;
+        if( bFirst )   // set base point if not yet initialized
+            maBasePoint = Point( pG->maLinearPos.X(), 0 );
+        break;
+    }
+
+    // return zero if no more glyph found
+    if( nStart >= mnGlyphCount )
+        return 0;
+
+    // calculate absolute position
+    rPos = GetDrawPosition( pG->maLinearPos - maBasePoint );
+
+    // find more glyphs which can be merged into one drawing instruction
+    int nCount = 0;
+    while( nCount < nLen )
+    {
+        *(pGlyphs++) = pG->mnGlyphIndex;
+        ++nCount;
+
+        if( ++nStart >= mnGlyphCount )
+            break;
+
+        Point aNewPos = pG->maLinearPos;
+        ++pG;
+        int n = pG->mnCharIndex;
+        if( (n < mnFirstCharIndex) || (n >= mnEndCharIndex) )
+            break;
+
+        aNewPos += Point( pG->mnWidth, 0 );
+        if( aNewPos != pG->maLinearPos )
+            break;
+    }
+
+    return nCount;
+}
+
+// -----------------------------------------------------------------------
+
+#if 0
+SalLayout* GenericSalLayout::ExtractLayout( int nXorFlags, int nAndFlags )
+{
+    int nNewSize = 0;
+    int nUsableSize = 0;
+
+    int i;
+    const GlyphItem* pSrc = mpGlyphItems;
+    for( i = mnGlyphCount; --i >= 0; ++pSrc )
+    {
+        if( ((pSrc->mnFlags ^ nXorFlags) & nAndFlags) != 0 )
+            continue;
+        ++nUsableSize;
+        if( (pSrc->mnCharIndex >= mnFirstCharIndex)
+        &&  (pSrc->mnCharIndex < mnEndCharIndex) )
+            ++nNewSize;
+    }
+
+    SalLayout* pDstLayout = NULL;
+
+    if( !nNewSize )
+        return NULL;
+
+//###    if( nUsableSize == mnGlyphCount )
+    {
+        pDstLayout = this;
+        Reference();
+    }
+/*###
+    else
+    {
+        pDstLayout = new SalLayout( nNewSize );
+        bool bWantFallback = false;
+        if( nNewSize > 0 )
+        {
+            GlyphItem* pDst = pDstLayout->mpGlyphItems;
+            pSrc = mpGlyphItems;
+            bool bFirst = true;
+            for( i = mnSize; --i >= 0; ++pSrc )
+            {
+                if( (pSrc->mnCharIndex >= mnFirstCharIndex)
+                &&  (pSrc->mnCharIndex < mnEndCharIndex)
+                &&  !((pSrc->mnFlags ^ nXorFlags) & nAndFlags) )
+                {
+                    *(pDst++) = *pSrc;
+                    if( !pSrc->mnGlyphIndex )
+                        bWantFallback = true;
+                }
+            }
+        }
+
+        pDstLayout->maAdvance       = maAdvance;
+        pDstLayout->maBasePoint     = maBasePoint;
+        pDstLayout->mnFirstCharIndex= mnFirstCharIndex;
+        pDstLayout->mnEndCharIndex  = mnEndCharIndex;
+        pDstLayout->mnOrientation   = mnOrientation;
+        pDstLayout->mbWantFallback  = bWantFallback;
+    }
+
+    pDstLayout->SetDrawPosition( maDrawPosition );
+###*/
+
+    return pDstLayout;
+}
+#endif
+
+// -----------------------------------------------------------------------
+
+#if 0
+void GenericSalLayout::MergeLayout( int nFlags, const GenericSalLayout& rSalLayout )
+{
+    GlyphItem* pG = mpGlyphItems;
+    for( int i = mnGlyphCount; --i >= 0; ++pG )
+    {
+        // use only when we need glyph fallback
+        if( pG->mnGlyphIndex != 0 )
+            continue;
+        int nCharIndex = pG->mnCharIndex;
+        const GlyphItem* pSrc = rSalLayout.mpGlyphItems;
+        for( int j = rSalLayout.mnGlyphCount; --j >= 0; ++pSrc )
+        {
+            if( !pSrc->mnGlyphIndex )
+                continue;
+            if( nCharIndex != pSrc->mnCharIndex )
+                continue;
+            pG->mnGlyphIndex = pSrc->mnGlyphIndex;
+            pG->mnFlags      = nFlags;
+            //### TODO: adjust position of following glyphs
+            break;
+        }
+    }
+}
+#endif
+
+// =======================================================================
