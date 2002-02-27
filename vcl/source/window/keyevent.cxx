a1e86b46eace2a8feef4f5619fa5a81219972271
Christof Pintaske <cp@openoffice.org>
2002-02-27 11:30:16 +0000

    #97803# create new key event with logical directionality

---
+++ vcl/source/window/keyevent.cxx
@@ -0,0 +1,104 @@
+/*************************************************************************
+ *
+ *  $RCSfile: keyevent.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: cp $ $Date: 2002-02-27 12:30:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <event.hxx>
+
+KeyEvent::KeyEvent (const KeyEvent& rKeyEvent) :
+    maKeyCode (rKeyEvent.maKeyCode),
+    mnRepeat  (rKeyEvent.mnRepeat),
+    mnCharCode(rKeyEvent.mnCharCode)
+{}
+
+KeyEvent KeyEvent::LogicalTextDirectionality (TextDirectionality eMode) const
+{
+    KeyEvent aClone(*this);
+
+    USHORT nCode = maKeyCode.GetCode();
+    USHORT nMod  = maKeyCode.GetAllModifier();
+
+    switch (eMode)
+    {
+        case TextDirectionality_RightToLeft_TopToBottom:
+            switch (nCode)
+            {
+                case KEY_LEFT:  aClone.maKeyCode = KeyCode(KEY_RIGHT, nMod); break;
+                case KEY_RIGHT: aClone.maKeyCode = KeyCode(KEY_LEFT,  nMod); break;
+            }
+            break;
+
+        case TextDirectionality_TopToBottom_RightToLeft:
+            switch (nCode)
+            {
+                case KEY_DOWN:  aClone.maKeyCode = KeyCode(KEY_RIGHT, nMod); break;
+                case KEY_UP:    aClone.maKeyCode = KeyCode(KEY_LEFT,  nMod); break;
+                case KEY_LEFT:  aClone.maKeyCode = KeyCode(KEY_DOWN,  nMod); break;
+                case KEY_RIGHT: aClone.maKeyCode = KeyCode(KEY_UP,    nMod); break;
+            }
+            break;
+
+        case TextDirectionality_LeftToRight_TopToBottom:
+            /* do nothing */
+            break;
+    }
+
+    return aClone;
+}
+
