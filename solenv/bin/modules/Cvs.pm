d5175c1e6a070afa0d96e4878c0c8dcc5291fff4
dc <dc@openoffice.org>
2002-03-05 15:22:27 +0000

    cvs-binary overloading via environment variable

--- solenv/bin/modules/Cvs.pm
+++ solenv/bin/modules/Cvs.pm
@@ -1,7 +1,69 @@
+#*************************************************************************
+#
+#   $RCSfile: Cvs.pm,v $
+#
+#   $Revision: 1.4 $
+#
+#   last change: $Author: dc $ $Date: 2002-03-05 16:22:27 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+
 #
 # Cvs.pm - package for manipulating CVS archives
 #
-# $Id: Cvs.pm,v 1.3 2002-01-18 17:51:05 hr Exp $
 
 package Cvs;
 use strict;
@@ -15,10 +77,14 @@ sub new {
     $self->{NAME} = undef;
     $self->{HEAD} = undef;
     $self->{FLAGS} = undef;
-    if ($^O eq "MSWin32" || $^O eq "os2" ) {
-    $self->{CVS_BINARY} = "cvsclt2.exe";
+    if ( $ENV{CVS_BINARY} ) {
+        $self->{CVS_BINARY} = $ENV{CVS_BINARY};
     } else {
-    $self->{CVS_BINARY} = "cvs.clt2";
+        if ($^O eq "MSWin32" || $^O eq "os2" ) {
+            $self->{CVS_BINARY} = "cvsclt2.exe";
+        } else {
+            $self->{CVS_BINARY} = "cvs.clt2";
+        }
     }
     $self->{REV_DATA} = {};
     $self->{REV_SORTED} = [];
@@ -322,4 +388,3 @@ sub parse_log {
 ####
 
 1; # needed by "use" or "require"
-
