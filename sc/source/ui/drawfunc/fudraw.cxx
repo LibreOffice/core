5cddc86510cca4357913554202fc0ffc3de69d1e
Armin Weiss <aw@openoffice.org>
2002-03-13 15:00:50 +0000

    #97016# Added all necessary stuff to implement draw object keyboard control

--- sc/source/ui/drawfunc/fudraw.cxx
+++ sc/source/ui/drawfunc/fudraw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fudraw.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: nn $ $Date: 2001-09-24 17:37:41 $
+ *  last change: $Author: aw $ $Date: 2002-03-13 16:00:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,6 +78,10 @@
 #include "drwlayer.hxx"
 #include "scresid.hxx"
 
+// #97016#
+#ifndef SC_DOCSHELL_HXX
+#include "docsh.hxx"
+#endif
 
 /*************************************************************************
 |*
@@ -257,10 +261,23 @@ BOOL __EXPORT FuDraw::KeyInput(const KeyEvent& rKEvt)
             }
             else if ( pView->HasMarkedObj() )
             {
-                pView->UnmarkAll();
+                // #97016# III
+                const SdrHdlList& rHdlList = pView->GetHdlList();
+                SdrHdl* pHdl = rHdlList.GetFocusHdl();
+
+                if(pHdl)
+                {
+                    ((SdrHdlList&)rHdlList).ResetFocusHdl();
+                }
+                else
+                {
+                    pView->UnmarkAll();
+                }
+
                 //  Beim Bezier-Editieren ist jetzt wieder das Objekt selektiert
                 if (!pView->HasMarkedObj())
                     pViewShell->SetDrawShell( FALSE );
+
                 bReturn = TRUE;
             }
             break;
@@ -269,6 +286,327 @@ BOOL __EXPORT FuDraw::KeyInput(const KeyEvent& rKEvt)
             pView->DeleteMarked();
             bReturn = TRUE;
             break;
+
+        // #97016#
+        case KEY_TAB:
+        {
+            // in calc do NOT start draw object selection using TAB/SHIFT-TAB when
+            // there is not yet a object selected
+            if(pView->HasMarkedObj())
+            {
+                KeyCode aCode = rKEvt.GetKeyCode();
+
+                if ( !aCode.IsMod1() && !aCode.IsMod2() )
+                {
+                    // changeover to the next object
+                    if(!pView->MarkNextObj( !aCode.IsShift() ))
+                    {
+                        // #97016# No next object: go over open end and
+                        // get first from the other side
+                        pView->UnmarkAllObj();
+                        pView->MarkNextObj(!aCode.IsShift());
+                    }
+
+                    // #97016# II
+                    if(pView->HasMarkedObj())
+                        pView->MakeVisible(pView->GetAllMarkedRect(), *pWindow);
+
+                    bReturn = TRUE;
+                }
+
+                if(rKEvt.GetKeyCode().IsMod1())
+                {
+                    // #97016# II do something with a selected handle?
+                    const SdrHdlList& rHdlList = pView->GetHdlList();
+                    sal_Bool bForward(!rKEvt.GetKeyCode().IsShift());
+
+                    ((SdrHdlList&)rHdlList).TravelFocusHdl(bForward);
+
+                    // guarantee visibility of focused handle
+                    SdrHdl* pHdl = rHdlList.GetFocusHdl();
+
+                    if(pHdl)
+                    {
+                        Point aHdlPosition(pHdl->GetPos());
+                        Rectangle aVisRect(aHdlPosition - Point(100, 100), Size(200, 200));
+                        pView->MakeVisible(aVisRect, *pWindow);
+                    }
+
+                    // consumed
+                    bReturn = TRUE;
+                }
+            }
+        }
+        break;
+
+        // #97016#
+        case KEY_END:
+        {
+            // in calc do NOT select the last draw object when
+            // there is not yet a object selected
+            if(pView->HasMarkedObj())
+            {
+                KeyCode aCode = rKEvt.GetKeyCode();
+
+                if ( aCode.IsMod1() )
+                {
+                    // #97016# mark last object
+                    pView->UnmarkAllObj();
+                    pView->MarkNextObj(FALSE);
+
+                    // #97016# II
+                    if(pView->HasMarkedObj())
+                        pView->MakeVisible(pView->GetAllMarkedRect(), *pWindow);
+
+                    bReturn = TRUE;
+                }
+            }
+        }
+        break;
+
+        // #97016#
+        case KEY_HOME:
+        {
+            // in calc do NOT select the first draw object when
+            // there is not yet a object selected
+            if(pView->HasMarkedObj())
+            {
+                KeyCode aCode = rKEvt.GetKeyCode();
+
+                if ( aCode.IsMod1() )
+                {
+                    // #97016# mark first object
+                    pView->UnmarkAllObj();
+                    pView->MarkNextObj(TRUE);
+
+                    // #97016# II
+                    if(pView->HasMarkedObj())
+                        pView->MakeVisible(pView->GetAllMarkedRect(), *pWindow);
+
+                    bReturn = TRUE;
+                }
+            }
+        }
+        break;
+
+        // #97016#
+        case KEY_UP:
+        case KEY_DOWN:
+        case KEY_LEFT:
+        case KEY_RIGHT:
+        {
+            // in calc do cursor travelling of draw objects only when
+            // there is a object selected yet
+            if(pView->HasMarkedObj())
+            {
+                long nX = 0;
+                long nY = 0;
+                USHORT nCode = rKEvt.GetKeyCode().GetCode();
+
+                if (nCode == KEY_UP)
+                {
+                    // Scroll nach oben
+                    nX = 0;
+                    nY =-1;
+                }
+                else if (nCode == KEY_DOWN)
+                {
+                    // Scroll nach unten
+                    nX = 0;
+                    nY = 1;
+                }
+                else if (nCode == KEY_LEFT)
+                {
+                    // Scroll nach links
+                    nX =-1;
+                    nY = 0;
+                }
+                else if (nCode == KEY_RIGHT)
+                {
+                    // Scroll nach rechts
+                    nX = 1;
+                    nY = 0;
+                }
+
+                ScViewData* pViewData = pViewShell->GetViewData();
+                BOOL bReadOnly = pViewData ? pViewData->GetDocShell()->IsReadOnly() : FALSE;
+
+                if(!rKEvt.GetKeyCode().IsMod1() && !bReadOnly)
+                {
+                    if(rKEvt.GetKeyCode().IsMod2())
+                    {
+                        // #97016# move in 1 pixel distance
+                        Size aLogicSizeOnePixel = (pWindow) ? pWindow->PixelToLogic(Size(1,1)) : Size(100, 100);
+                        nX *= aLogicSizeOnePixel.Width();
+                        nY *= aLogicSizeOnePixel.Height();
+                    }
+                    else
+                    {
+                        // old, fixed move distance
+                        nX *= 100;
+                        nY *= 100;
+                    }
+
+                    // is there a movement to do?
+                    if(0 != nX || 0 != nY)
+                    {
+                        // #97016# II
+                        const SdrHdlList& rHdlList = pView->GetHdlList();
+                        SdrHdl* pHdl = rHdlList.GetFocusHdl();
+
+                        if(0L == pHdl)
+                        {
+                            // #90129# restrict movement to WorkArea
+                            const Rectangle& rWorkArea = pView->GetWorkArea();
+
+                            if(!rWorkArea.IsEmpty())
+                            {
+                                Rectangle aMarkRect(pView->GetMarkedObjRect());
+                                aMarkRect.Move(nX, nY);
+
+                                if(!aMarkRect.IsInside(rWorkArea))
+                                {
+                                    if(aMarkRect.Left() < rWorkArea.Left())
+                                    {
+                                        nX += rWorkArea.Left() - aMarkRect.Left();
+                                    }
+
+                                    if(aMarkRect.Right() > rWorkArea.Right())
+                                    {
+                                        nX -= aMarkRect.Right() - rWorkArea.Right();
+                                    }
+
+                                    if(aMarkRect.Top() < rWorkArea.Top())
+                                    {
+                                        nY += rWorkArea.Top() - aMarkRect.Top();
+                                    }
+
+                                    if(aMarkRect.Bottom() > rWorkArea.Bottom())
+                                    {
+                                        nY -= aMarkRect.Bottom() - rWorkArea.Bottom();
+                                    }
+                                }
+                            }
+
+                            // now move the selected draw objects
+                            pView->MoveAllMarked(Size(nX, nY));
+
+                            // #97016# II
+                            pView->MakeVisible(pView->GetAllMarkedRect(), *pWindow);
+
+                            bReturn = TRUE;
+                        }
+                        else
+                        {
+                            // move handle with index nHandleIndex
+                            if(pHdl && (nX || nY))
+                            {
+                                // now move the Handle (nX, nY)
+                                Point aStartPoint(pHdl->GetPos());
+                                Point aEndPoint(pHdl->GetPos() + Point(nX, nY));
+                                const SdrDragStat& rDragStat = pView->GetDragStat();
+
+                                // start dragging
+                                pView->BegDragObj(aStartPoint, 0, pHdl, 0);
+
+                                if(pView->IsDragObj())
+                                {
+                                    FASTBOOL bWasNoSnap = rDragStat.IsNoSnap();
+                                    BOOL bWasSnapEnabled = pView->IsSnapEnabled();
+
+                                    // switch snapping off
+                                    if(!bWasNoSnap)
+                                        ((SdrDragStat&)rDragStat).SetNoSnap(TRUE);
+                                    if(bWasSnapEnabled)
+                                        pView->SetSnapEnabled(FALSE);
+
+                                    pView->MovAction(aEndPoint);
+                                    pView->EndDragObj();
+
+                                    // restore snap
+                                    if(!bWasNoSnap)
+                                        ((SdrDragStat&)rDragStat).SetNoSnap(bWasNoSnap);
+                                    if(bWasSnapEnabled)
+                                        pView->SetSnapEnabled(bWasSnapEnabled);
+                                }
+
+                                // make moved handle visible
+                                Rectangle aVisRect(aEndPoint - Point(100, 100), Size(200, 200));
+                                pView->MakeVisible(aVisRect, *pWindow);
+
+                                bReturn = TRUE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        break;
+
+        // #97016#
+        case KEY_SPACE:
+        {
+            // in calc do only something when draw objects are selected
+            if(pView->HasMarkedObj())
+            {
+                const SdrHdlList& rHdlList = pView->GetHdlList();
+                SdrHdl* pHdl = rHdlList.GetFocusHdl();
+
+                if(pHdl)
+                {
+                    if(pHdl->GetKind() == HDL_POLY)
+                    {
+                        // rescue ID of point with focus
+                        sal_uInt16 nPol(pHdl->GetPolyNum());
+                        sal_uInt16 nPnt(pHdl->GetPointNum());
+
+                        if(pView->IsPointMarked(*pHdl))
+                        {
+                            if(rKEvt.GetKeyCode().IsShift())
+                            {
+                                pView->UnmarkPoint(*pHdl);
+                            }
+                        }
+                        else
+                        {
+                            if(!rKEvt.GetKeyCode().IsShift())
+                            {
+                                pView->UnmarkAllPoints();
+                            }
+
+                            pView->MarkPoint(*pHdl);
+                        }
+
+                        if(0L == rHdlList.GetFocusHdl())
+                        {
+                            // restore point with focus
+                            SdrHdl* pNewOne = 0L;
+
+                            for(sal_uInt32 a(0); !pNewOne && a < rHdlList.GetHdlCount(); a++)
+                            {
+                                SdrHdl* pAct = rHdlList.GetHdl(a);
+
+                                if(pAct
+                                    && pAct->GetKind() == HDL_POLY
+                                    && pAct->GetPolyNum() == nPol
+                                    && pAct->GetPointNum() == nPnt)
+                                {
+                                    pNewOne = pAct;
+                                }
+                            }
+
+                            if(pNewOne)
+                            {
+                                ((SdrHdlList&)rHdlList).SetFocusHdl(pNewOne);
+                            }
+                        }
+
+                        bReturn = TRUE;
+                    }
+                }
+            }
+        }
+        break;
     }
 
     if (!bReturn)
@@ -277,6 +615,13 @@ BOOL __EXPORT FuDraw::KeyInput(const KeyEvent& rKEvt)
     }
 
     return (bReturn);
+}
+
+// #97016# II
+void FuDraw::SelectionHasChanged()
+{
+    const SdrHdlList& rHdlList = pView->GetHdlList();
+    ((SdrHdlList&)rHdlList).ResetFocusHdl();
 }
 
 /*************************************************************************
