8100fe8e5b06147e29ea603d715bcd7246f21a2a
Thomas Benisch <tbe@openoffice.org>
2002-03-18 16:50:15 +0000

    #97222# added tkresmgr.obj

--- toolkit/source/helper/makefile.mk
+++ toolkit/source/helper/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:02:09 $
+#   last change: $Author: tbe $ $Date: 2002-03-18 17:50:15 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -83,7 +83,8 @@ SLOFILES=   \
             $(SLO)$/unomemorystream.obj 		\
             $(SLO)$/unopropertyarrayhelper.obj	\
             $(SLO)$/unowrapper.obj 				\
-            $(SLO)$/vclunohelper.obj
+            $(SLO)$/vclunohelper.obj			\
+            $(SLO)$/tkresmgr.obj
 
 # --- Targets ------------------------------------------------------
 
