ab75e38100d1d52ed1388d4124d080b5bed78fa9
Andre Fischer <af@openoffice.org>
2002-04-15 14:18:15 +0000

    #95585# Initial revision.

---
+++ sd/source/ui/inc/AccessiblePageShape.hxx
@@ -0,0 +1,228 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessiblePageShape.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: af $ $Date: 2002-04-15 15:18:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifndef _SD_ACCESSIBILITY_ACCESSIBLE_PAGE_SHAPE_HXX
+#define _SD_ACCESSIBILITY_ACCESSIBLE_PAGE_SHAPE_HXX
+
+#ifndef _SVX_ACCESSIBILITY_ACCESSIBLE_SHAPE_HXX
+#include <svx/AccessibleShape.hxx>
+#endif
+#ifndef _SVX_ACCESSIBILITY_ACCESSIBLE_SHAPE_TREE_INFO_HXX
+#include <svx/AccessibleShapeTreeInfo.hxx>
+#endif
+#ifndef _SVX_ACCESSIBILITY_IACCESSIBLE_VIEW_FORWARDER_LISTENER_HXX
+#include <svx/IAccessibleViewForwarderListener.hxx>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLE_HPP_
+#include <drafts/com/sun/star/accessibility/XAccessible.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLEEXTENDEDCOMPONENT_HPP_
+#include <drafts/com/sun/star/accessibility/XAccessibleExtendedComponent.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
+#include <drafts/com/sun/star/accessibility/AccessibleRole.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_XDRAWPAGE_HPP_
+#include <com/sun/star/drawing/XDrawPage.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XEVENTLISTENER_HPP_
+#include <com/sun/star/lang/XEventListener.hpp>
+#endif
+
+#ifndef _SVX_UNOEDACC_HXX_
+#include <svx/unoedacc.hxx>
+#endif
+
+namespace accessibility {
+
+/** A page shape represents the actual page as seen on the screen.
+*/
+class AccessiblePageShape
+    :   public AccessibleShape
+{
+public:
+    //=====  internal  ========================================================
+
+    /** Create a new accessible object that makes the given shape accessible.
+        @param rxParent
+            The accessible parent object.  It will be used, for example when
+            the <member>getIndexInParent</member> method is called.
+        @param rShapeTreeInfo
+            Bundel of information passed to this shape and all of its desendants.
+        @param nIndex
+            Index used to disambiguate between objects that have the same
+            name.  Passing a value of -1 leads to the use of the object's
+            z-order instead.  Because that is not a good substitute, better
+            pass an ever increasing counter.
+        @attention
+            Always call the <member>init</member> method after creating a
+            new accessible shape.  This is one way to overcome the potential
+            problem of registering the new object with e.g. event
+            broadcasters.  That would delete the new object if a broadcaster
+            would not keep a strong reference to the new object.
+    */
+    AccessiblePageShape (
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::drawing::XDrawPage>& rxPage,
+        const ::com::sun::star::uno::Reference<
+            ::drafts::com::sun::star::accessibility::XAccessible>& rxParent,
+        AccessibleShapeTreeInfo& rShapeTreeInfo,
+        long nIndex = -1);
+
+    virtual ~AccessiblePageShape (void);
+
+    /** Initialize a new shape.  See the documentation of the constructor
+        for the reason of this method's existence.
+    */
+    virtual void Init (void);
+
+    //=====  XAccessibleContext  ==============================================
+
+    /// Returns always 0 because there can be no children.
+    virtual long SAL_CALL
+        getAccessibleChildCount (void)
+        throw ();
+
+    /** Return the specified child.
+        @param nIndex
+            Index of the requested child.
+        @return
+            Reference of the requested child which is the accessible object
+            of a visible shape.
+        @raises IndexOutOfBoundsException
+            Throws always an exception because there are no children.
+    */
+    virtual ::com::sun::star::uno::Reference<
+            ::drafts::com::sun::star::accessibility::XAccessible> SAL_CALL
+        getAccessibleChild (long nIndex)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    //=====  XAccessibleComponent  ============================================
+
+    virtual ::com::sun::star::awt::Rectangle SAL_CALL getBounds (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    //=====  XAccessibleExtendedComponent  ====================================
+
+    virtual sal_Int32 SAL_CALL getForeground (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual sal_Int32 SAL_CALL getBackground (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    //=====  XComponent  ======================================================
+
+    virtual void SAL_CALL
+        dispose (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    //=====  XServiceInfo  ====================================================
+
+    /** Returns an identifier for the implementation of this object.
+    */
+    virtual ::rtl::OUString SAL_CALL
+        getImplementationName (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    //=====  lang::XEventListener  ============================================
+
+    virtual void SAL_CALL
+        disposing (const ::com::sun::star::lang::EventObject& Source)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+protected:
+    /** Create a base name string that contains the accessible name.
+    */
+    virtual ::rtl::OUString
+        CreateAccessibleBaseName (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual ::rtl::OUString
+        CreateAccessibleName (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    /// Create a description string that contains the accessible description.
+    virtual ::rtl::OUString
+        CreateAccessibleDescription (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+private:
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::drawing::XDrawPage> mxPage;
+
+    /** Don't use the default constructor.  Use the public constructor that
+        takes the original shape and the parent as arguments instead.
+    */
+    explicit AccessiblePageShape (void);
+    /// Don't use the copy constructor.  Is there any use for it?
+    explicit AccessiblePageShape (const AccessiblePageShape&);
+    /// Don't use the assignment operator.  Do we need this?
+    AccessibleShape& operator= (const AccessiblePageShape&);
+};
+
+} // end of namespace accessibility
+
+#endif
