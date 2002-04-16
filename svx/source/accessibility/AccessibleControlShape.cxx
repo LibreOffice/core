afe098fa21e710df2a604694e4481847c9a1fca5
Andre Fischer <af@openoffice.org>
2002-04-16 11:08:19 +0000

    #95585# Initial revision.

---
+++ svx/source/accessibility/AccessibleControlShape.cxx
@@ -0,0 +1,186 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessibleControlShape.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: af $ $Date: 2002-04-16 12:08:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+
+#ifndef _SVX_ACCESSIBILITY_ACCESSIBLE_CONTROL_SHAPE_HXX
+#include "AccessibleControlShape.hxx"
+#endif
+#ifndef _SVX_ACCESSIBILITY_DESCRIPTION_GENERATOR_HXX
+#include "DescriptionGenerator.hxx"
+#endif
+
+#include "ShapeTypeHandler.hxx"
+#include "SvxShapeTypes.hxx"
+
+using namespace accessibility;
+using namespace ::rtl;
+using namespace ::com::sun::star;
+using namespace ::drafts::com::sun::star::accessibility;
+using ::com::sun::star::uno::Reference;
+
+//=====  internal  ============================================================
+
+AccessibleControlShape::AccessibleControlShape (const ::com::sun::star::uno::Reference<
+        ::com::sun::star::drawing::XShape>& rxShape,
+    const ::com::sun::star::uno::Reference<
+        ::drafts::com::sun::star::accessibility::XAccessible>& rxParent,
+    AccessibleShapeTreeInfo& rShapeTreeInfo,
+    long nIndex)
+    :      AccessibleShape (rxShape, rxParent, rShapeTreeInfo, nIndex)
+{
+}
+
+
+
+
+AccessibleControlShape::~AccessibleControlShape (void)
+{
+}
+
+
+
+
+//=====  XAccessible  =========================================================
+
+Reference<XAccessibleContext> SAL_CALL
+    AccessibleControlShape::getAccessibleContext (void)
+    throw (::com::sun::star::uno::RuntimeException)
+{
+    // Here will later be the control shape asked for its control and that
+    // for its accessibility object.
+    return AccessibleShape::getAccessibleContext ();
+}
+
+
+
+
+//=====  XServiceInfo  ========================================================
+
+::rtl::OUString SAL_CALL
+    AccessibleControlShape::getImplementationName (void)
+    throw (::com::sun::star::uno::RuntimeException)
+{
+    return ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("AccessibleControlShape"));
+}
+
+
+
+
+/// Set this object's name if is different to the current name.
+::rtl::OUString
+    AccessibleControlShape::CreateAccessibleBaseName (void)
+    throw (::com::sun::star::uno::RuntimeException)
+{
+    ::rtl::OUString sName;
+
+    ShapeTypeId nShapeType = ShapeTypeHandler::Instance().GetTypeId (mxShape);
+    switch (nShapeType)
+    {
+        case DRAWING_CONTROL:
+            sName = ::rtl::OUString (RTL_CONSTASCII_USTRINGPARAM("ControlShape"));
+            break;
+        default:
+            sName = ::rtl::OUString (RTL_CONSTASCII_USTRINGPARAM("UnknownAccessibleControlShape"));
+            uno::Reference<drawing::XShapeDescriptor> xDescriptor (mxShape, uno::UNO_QUERY);
+            if (xDescriptor.is())
+                sName += ::rtl::OUString (RTL_CONSTASCII_USTRINGPARAM(": "))
+                    + xDescriptor->getShapeType();
+    }
+
+    return sName;
+}
+
+
+
+
+::rtl::OUString
+    AccessibleControlShape::CreateAccessibleDescription (void)
+    throw (::com::sun::star::uno::RuntimeException)
+{
+    DescriptionGenerator aDG (mxShape);
+    ShapeTypeId nShapeType = ShapeTypeHandler::Instance().GetTypeId (mxShape);
+    switch (nShapeType)
+    {
+        case DRAWING_CONTROL:
+            aDG.Initialize (OUString::createFromAscii("Control"));
+            aDG.AddProperty (OUString::createFromAscii ("ControlBackground"),
+                DescriptionGenerator::COLOR,
+                OUString());
+            aDG.AddProperty (OUString::createFromAscii ("ControlBorder"),
+                DescriptionGenerator::INTEGER,
+                OUString());
+            break;
+        default:
+            aDG.Initialize (::rtl::OUString::createFromAscii (
+                "Unknown accessible control shape"));
+            uno::Reference<drawing::XShapeDescriptor> xDescriptor (mxShape, uno::UNO_QUERY);
+            if (xDescriptor.is())
+            {
+                aDG.AppendString (::rtl::OUString (RTL_CONSTASCII_USTRINGPARAM("service name=")));
+                aDG.AppendString (xDescriptor->getShapeType());
+            }
+    }
+
+    return aDG();
+}
+
+
