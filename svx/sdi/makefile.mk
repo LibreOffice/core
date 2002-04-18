a93ca73ef56ec1f87eb1f5810c1ee6df52411940
Mathias Bauer <mba@openoffice.org>
2002-04-18 13:18:10 +0000

    #98405#: export declaration missing

--- svx/sdi/makefile.mk
+++ svx/sdi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: mba $ $Date: 2002-03-28 16:24:02 $
+#   last change: $Author: mba $ $Date: 2002-04-18 14:18:10 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,7 +73,7 @@ TARGET=svxslots
 .INCLUDE :  sv.mk
 
 SDINAME=$(TARGET)
-#SDI1NAME=baside
+SDIEXPORT=svx
 
 # --- Files --------------------------------------------------------
 
