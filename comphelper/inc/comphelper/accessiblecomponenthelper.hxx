26c1bd7a865f947d84f7849ccde597d0782087ab
Frank Schönheit <fs@openoffice.org>
2002-04-23 10:10:21 +0000

    initial checkin - helper for implementing an XAccessible(Extended)Component

---
+++ comphelper/inc/comphelper/accessiblecomponenthelper.hxx
@@ -0,0 +1,186 @@
+/*************************************************************************
+ *
+ *  $RCSfile: accessiblecomponenthelper.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2002-04-23 11:08:57 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef COMPHELPER_ACCESSIBLE_COMPONENT_HELPER_HXX
+#define COMPHELPER_ACCESSIBLE_COMPONENT_HELPER_HXX
+
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLECOMPONENT_HPP_
+#include <drafts/com/sun/star/accessibility/XAccessibleComponent.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLEEXTENDEDCOMPONENT_HPP_
+#include <drafts/com/sun/star/accessibility/XAccessibleExtendedComponent.hpp>
+#endif
+
+#ifndef COMPHELPER_ACCESSIBLE_CONTEXT_HELPER_HXX
+#include <comphelper/accessiblecontexthelper.hxx>
+#endif
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+
+//.........................................................................
+namespace comphelper
+{
+//.........................................................................
+
+    //=====================================================================
+    //= OCommonAccessibleComponent
+    //=====================================================================
+    /** base class encapsulating common functionality for the helper classes implementing
+        the XAccessibleComponent respectively XAccessibleExtendendComponent
+    */
+    class OCommonAccessibleComponent : public OAccessibleContextHelper
+    {
+    protected:
+        OCommonAccessibleComponent();
+
+    protected:
+        /// implements the calculation of the bounding rectangle - still waiting to be overwritten
+        virtual ::com::sun::star::awt::Rectangle SAL_CALL implGetBounds(  ) throw (::com::sun::star::uno::RuntimeException) = 0;
+
+    protected:
+        /** non-virtual versions of the methods which can be implemented using <method>implGetBounds</method>
+            note: getLocationOnScreen relies on a valid parent (XAccessibleContext::getParent()->getAccessibleContext()),
+                 which itself implements XAccessibleComponent
+        */
+        sal_Bool SAL_CALL contains( const ::com::sun::star::awt::Point& aPoint ) throw (::com::sun::star::uno::RuntimeException);
+        ::com::sun::star::awt::Point SAL_CALL getLocation(  ) throw (::com::sun::star::uno::RuntimeException);
+        ::com::sun::star::awt::Point SAL_CALL getLocationOnScreen(  ) throw (::com::sun::star::uno::RuntimeException);
+        ::com::sun::star::awt::Size SAL_CALL getSize(  ) throw (::com::sun::star::uno::RuntimeException);
+        ::com::sun::star::awt::Rectangle SAL_CALL getBounds(  ) throw (::com::sun::star::uno::RuntimeException);
+    };
+
+    //=====================================================================
+    //= OAccessibleComponentHelper
+    //=====================================================================
+
+    typedef ::cppu::ImplHelper1 <   ::drafts::com::sun::star::accessibility::XAccessibleComponent
+                                >   OAccessibleComponentHelper_Base;
+
+    /** a helper class for implementing an AccessibleContext which at the same time
+        supports an XAccessibleComponent interface.
+    */
+    class OAccessibleComponentHelper
+            :public OCommonAccessibleComponent
+            ,public OAccessibleComponentHelper_Base
+    {
+    protected:
+        OAccessibleComponentHelper( );
+
+    public:
+        // XInterface
+        DECLARE_XINTERFACE( )
+        DECLARE_XTYPEPROVIDER( )
+
+        // XAccessibleComponent - default implementations
+        virtual sal_Bool SAL_CALL contains( const ::com::sun::star::awt::Point& aPoint ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Point SAL_CALL getLocation(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Point SAL_CALL getLocationOnScreen(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Size SAL_CALL getSize(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Rectangle SAL_CALL getBounds(  ) throw (::com::sun::star::uno::RuntimeException);
+    };
+
+    //=====================================================================
+    //= OAccessibleExtendedComponentHelper
+    //=====================================================================
+
+    typedef ::cppu::ImplHelper1 <   ::drafts::com::sun::star::accessibility::XAccessibleExtendedComponent
+                                >   OAccessibleExtendedComponentHelper_Base;
+
+    /** a helper class for implementing an AccessibleContext which at the same time
+        supports an XAccessibleExtendedComponent interface.
+    */
+    class OAccessibleExtendedComponentHelper
+            :public OCommonAccessibleComponent
+            ,public OAccessibleExtendedComponentHelper_Base
+    {
+    protected:
+        OAccessibleExtendedComponentHelper( );
+
+    public:
+        // XInterface
+        DECLARE_XINTERFACE( )
+        DECLARE_XTYPEPROVIDER( )
+
+        // XAccessibleComponent - default implementations
+        virtual sal_Bool SAL_CALL contains( const ::com::sun::star::awt::Point& aPoint ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Point SAL_CALL getLocation(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Point SAL_CALL getLocationOnScreen(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Size SAL_CALL getSize(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::awt::Rectangle SAL_CALL getBounds(  ) throw (::com::sun::star::uno::RuntimeException);
+    };
+
+//.........................................................................
+}   // namespace comphelper
+//.........................................................................
+
+#endif // COMPHELPER_ACCESSIBLE_COMPONENT_HELPER_HXX
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 18.04.2002 11:47:39  fs
+ ************************************************************************/
+
---
+++ comphelper/source/misc/accessiblecomponenthelper.cxx
@@ -0,0 +1,235 @@
+/*************************************************************************
+ *
+ *  $RCSfile: accessiblecomponenthelper.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: fs $ $Date: 2002-04-23 11:10:21 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef COMPHELPER_ACCESSIBLE_COMPONENT_HELPER_HXX
+#include <comphelper/accessiblecomponenthelper.hxx>
+#endif
+
+//.........................................................................
+namespace comphelper
+{
+//.........................................................................
+
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::awt;
+    using namespace ::com::sun::star::lang;
+    using namespace ::drafts::com::sun::star::accessibility;
+
+    //=====================================================================
+    //= OCommonAccessibleComponent
+    //=====================================================================
+    //---------------------------------------------------------------------
+    OCommonAccessibleComponent::OCommonAccessibleComponent( )
+    {
+    }
+
+    //--------------------------------------------------------------------
+    sal_Bool SAL_CALL OCommonAccessibleComponent::contains( const Point& _rPoint ) throw (RuntimeException)
+    {
+        OContextEntryGuard aGuard( this );
+        Rectangle aBounds( implGetBounds() );
+        return  ( _rPoint.X >= 0 )
+            &&  ( _rPoint.Y >= 0 )
+            &&  ( _rPoint.X < aBounds.Width - aBounds.X )
+            &&  ( _rPoint.Y < aBounds.Height - aBounds.Y );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OCommonAccessibleComponent::getLocation(  ) throw (RuntimeException)
+    {
+        OContextEntryGuard aGuard( this );
+        Rectangle aBounds( implGetBounds() );
+        return Point( aBounds.X, aBounds.Y );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OCommonAccessibleComponent::getLocationOnScreen(  ) throw (RuntimeException)
+    {
+        OContextEntryGuard aGuard( this );
+        Rectangle aBounds( implGetBounds() );
+
+        Point aScreenLoc( 0, 0 );
+
+        Reference< XAccessibleComponent > xParentComponent( implGetParentContext(), UNO_QUERY );
+        OSL_ENSURE( xParentComponent.is(), "OCommonAccessibleComponent::getLocationOnScreen: no parent component!" );
+        if ( xParentComponent.is() )
+        {
+            Point aParentScreenLoc( xParentComponent->getLocationOnScreen() );
+            Point aOwnRelativeLoc( getLocation() );
+            aScreenLoc.X = aParentScreenLoc.X + aOwnRelativeLoc.X;
+            aScreenLoc.Y = aParentScreenLoc.Y + aOwnRelativeLoc.Y;
+        }
+
+        return aScreenLoc;
+    }
+
+    //--------------------------------------------------------------------
+    Size SAL_CALL OCommonAccessibleComponent::getSize(  ) throw (RuntimeException)
+    {
+        OContextEntryGuard aGuard( this );
+        Rectangle aBounds( implGetBounds() );
+        return Size( aBounds.Width - aBounds.X, aBounds.Height - aBounds.Y );
+    }
+
+    //--------------------------------------------------------------------
+    Rectangle SAL_CALL OCommonAccessibleComponent::getBounds(  ) throw (RuntimeException)
+    {
+        OContextEntryGuard aGuard( this );
+        return implGetBounds();
+    }
+
+    //=====================================================================
+    //= OAccessibleComponentHelper
+    //=====================================================================
+    //---------------------------------------------------------------------
+    OAccessibleComponentHelper::OAccessibleComponentHelper( )
+    {
+    }
+
+    //--------------------------------------------------------------------
+    IMPLEMENT_FORWARD_XINTERFACE2( OAccessibleComponentHelper, OCommonAccessibleComponent, OAccessibleComponentHelper_Base )
+    IMPLEMENT_FORWARD_XTYPEPROVIDER2( OAccessibleComponentHelper, OCommonAccessibleComponent, OAccessibleComponentHelper_Base )
+        // (order matters: the first is the class name, the second is the class doing the ref counting)
+
+    //--------------------------------------------------------------------
+    sal_Bool SAL_CALL OAccessibleComponentHelper::contains( const Point& _rPoint ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::contains( _rPoint );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OAccessibleComponentHelper::getLocation(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getLocation( );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OAccessibleComponentHelper::getLocationOnScreen(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getLocationOnScreen( );
+    }
+
+    //--------------------------------------------------------------------
+    Size SAL_CALL OAccessibleComponentHelper::getSize(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getSize( );
+    }
+
+    //--------------------------------------------------------------------
+    Rectangle SAL_CALL OAccessibleComponentHelper::getBounds(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getBounds( );
+    }
+
+    //=====================================================================
+    //= OAccessibleExtendedComponentHelper
+    //=====================================================================
+    //---------------------------------------------------------------------
+    OAccessibleExtendedComponentHelper::OAccessibleExtendedComponentHelper( )
+    {
+    }
+
+    //--------------------------------------------------------------------
+    IMPLEMENT_FORWARD_XINTERFACE2( OAccessibleExtendedComponentHelper, OCommonAccessibleComponent, OAccessibleExtendedComponentHelper_Base )
+    IMPLEMENT_FORWARD_XTYPEPROVIDER2( OAccessibleExtendedComponentHelper, OCommonAccessibleComponent, OAccessibleExtendedComponentHelper_Base )
+        // (order matters: the first is the class name, the second is the class doing the ref counting)
+
+    //--------------------------------------------------------------------
+    sal_Bool SAL_CALL OAccessibleExtendedComponentHelper::contains( const Point& _rPoint ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::contains( _rPoint );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OAccessibleExtendedComponentHelper::getLocation(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getLocation( );
+    }
+
+    //--------------------------------------------------------------------
+    Point SAL_CALL OAccessibleExtendedComponentHelper::getLocationOnScreen(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getLocationOnScreen( );
+    }
+
+    //--------------------------------------------------------------------
+    Size SAL_CALL OAccessibleExtendedComponentHelper::getSize(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getSize( );
+    }
+
+    //--------------------------------------------------------------------
+    Rectangle SAL_CALL OAccessibleExtendedComponentHelper::getBounds(  ) throw (RuntimeException)
+    {
+        return OCommonAccessibleComponent::getBounds( );
+    }
+
+//.........................................................................
+}   // namespace comphelper
+//.........................................................................
+
+/*************************************************************************
+ * history:
+ *  $Log: not supported by cvs2svn $
+ *
+ *  Revision 1.0 18.04.2002 11:54:56  fs
+ ************************************************************************/
+
