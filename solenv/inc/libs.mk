50eb88d487b66bc67306cd7fb1d6e003a440f637
Bustamam Harun <bustamam@openoffice.org>
2002-04-25 21:05:34 +0000

    #97582# Update to ICU 2.1

--- solenv/inc/libs.mk
+++ solenv/inc/libs.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: libs.mk,v $
 #
-#   $Revision: 1.45 $
+#   $Revision: 1.46 $
 #
-#   last change: $Author: hjs $ $Date: 2002-04-02 12:08:17 $
+#   last change: $Author: bustamam $ $Date: 2002-04-25 22:05:34 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -60,7 +60,7 @@
 #
 #*************************************************************************
 
-LIBSMKREV!:="$$Revision: 1.45 $$"
+LIBSMKREV!:="$$Revision: 1.46 $$"
 
 .IF "$(COM)"=="WTC"
 LIBPRE=libr
@@ -72,14 +72,7 @@ LIBPRE=libr
 #externe libs in plattform.mk
 #
 
-ICUI18NLIB=-licui18n -licuuc -licule
-# Sparc, PowerPC platforms are big-endian otherwise assume little-endian.
-# should we put this in unx*.mk platform dependent files?
-.IF "$(CPU)"=="S" || "$(CPU)"=="P"
-ICUI18NLIB+=-licudt20b
-.ELSE
-ICUI18NLIB+=-licudt20l
-.ENDIF
+ICUI18NLIB=-licui18n -licuuc -licule -licudata
 GPC3RDLIB=-lgpc
 SALHELPERLIB=-lsalhelper$(UDK_MAJOR)$(COMID)
 XMLSCRIPTLIB =-lxcr$(OFFICEUPD)$(DLLPOSTFIX)
