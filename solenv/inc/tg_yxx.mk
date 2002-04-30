f58dc586670253878ced14d7982e6a43ac910230
Hans-Joachim Lankenau <hjs@openoffice.org>
2002-04-30 14:52:06 +0000

    one more rule - generate to inc

--- solenv/inc/tg_yxx.mk
+++ solenv/inc/tg_yxx.mk
@@ -8,5 +8,14 @@ $(MISC)$/%.cxx : %.y
     @+-$(COPY:s/-f//) $@.h $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
     @+-$(COPY:s/-f//) $(@:d)$/$(@:b).hxx $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
     +$(TYPE) $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
+
+$(INCCOM)$/yy%.cxx : %.y
+    @echo ------------------------------
+    @echo Making: $@
+    $(BISON) $(YACCFLAGS) $(YACCTARGET) $(YACCFILES)
+# removing -f switch - avoid getting nothing when copying a file to itself
+    @+-$(COPY:s/-f//) $@.h $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
+    @+-$(COPY:s/-f//) $(@:d)$/$(@:b).hxx $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
+    +$(TYPE) $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
 .ENDIF
 
