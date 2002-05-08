2f4a19f02094759b507f7626fd91833d0ac1fba0
Juergen Schmidt <jsc@openoffice.org>
2002-05-08 15:02:26 +0000

    #99053# extend PATH with OFFICE_PROGRAM_PATH

--- odk/setsdkenv_windows.bat
+++ odk/setsdkenv_windows.bat
@@ -44,7 +44,7 @@ REM Set office program path.
 set OFFICE_PROGRAM_PATH=%OFFICE_HOME%\program
 
 REM Add directory of the SDK tools to the path.
-set PATH=%PATH%;%SDK_HOME%\windows\bin
+set PATH=%PATH%;%SDK_HOME%\windows\bin;%OFFICE_PROGRAM_PATH
 
 REM Add directory of the command make to the path, if necessary.
 if defined SDK_MAKE set PATH=%PATH%;%SDK_MAKE%
