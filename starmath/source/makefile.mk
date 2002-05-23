0cdd43592dd776acbb6538a3820a7723ac8dfc58
Vladimir Glazounov <vg@openoffice.org>
2002-05-23 13:18:44 +0000

    #65293# new exceptions entry

--- starmath/source/makefile.mk
+++ starmath/source/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: tl $ $Date: 2002-05-15 13:47:45 $
+#   last change: $Author: vg $ $Date: 2002-05-23 14:18:44 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -95,7 +95,7 @@ SLOFILES =  \
         $(SLO)$/symbol.obj \
         $(SLO)$/toolbox.obj \
         $(SLO)$/action.obj \
-                $(SLO)$/accessibility.obj \
+        $(SLO)$/accessibility.obj \
         $(SLO)$/cfgitem.obj \
         $(SLO)$/config.obj \
         $(SLO)$/dialog.obj \
@@ -116,6 +116,7 @@ SLOFILES =  \
         $(SLO)$/xchar.obj
 
 EXCEPTIONSFILES =   \
+        $(SLO)$/accessibility.obj \
         $(SLO)$/register.obj  \
         $(SLO)$/mathml.obj \
         $(SLO)$/viewhdl.obj \
