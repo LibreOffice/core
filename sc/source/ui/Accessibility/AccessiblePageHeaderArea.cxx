05f1275964f5f4e4effd8203ddd47d487daa6de6
Sascha Ballach <sab@openoffice.org>
2002-05-24 14:12:02 +0000

    #95584#; make every area (left, center, right) of the header/footer a own child

---
+++ sc/source/ui/Accessibility/AccessiblePageHeaderArea.cxx
@@ -0,0 +1,221 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessiblePageHeaderArea.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: sab $ $Date: 2002-05-24 15:12:02 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifndef _SC_ACCESSIBLEPAGEHEADERAREA_HXX
+#include "AccessiblePageHeaderArea.hxx"
+#endif
+#ifndef _SC_ACCESSIBLETEXT_HXX
+#include "AccessibleText.hxx"
+#endif
+#ifndef SC_UNOGUARD_HXX
+#include "unoguard.hxx"
+#endif
+#ifndef SC_EDITSRC_HXX
+#include "editsrc.hxx"
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLEROLE_HPP_
+#include <drafts/com/sun/star/accessibility/AccessibleRole.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLESTATETYPE_HPP_
+#include <drafts/com/sun/star/accessibility/AccessibleStateType.hpp>
+#endif
+
+#ifndef _EDITOBJ_HXX
+#include <svx/editobj.hxx>
+#endif
+#ifndef _SVX_ACCESSILE_TEXT_HELPER_HXX_
+#include <svx/AccessibleTextHelper.hxx>
+#endif
+#ifndef _RTL_UUID_H_
+#include <rtl/uuid.h>
+#endif
+#ifndef _UTL_ACCESSIBLESTATESETHELPER_HXX_
+#include <unotools/accessiblestatesethelper.hxx>
+#endif
+
+using namespace ::com::sun::star;
+using namespace ::drafts::com::sun::star::accessibility;
+
+    //=====  internal  ========================================================
+
+ScAccessiblePageHeaderArea::ScAccessiblePageHeaderArea(
+        const uno::Reference<XAccessible>& rxParent,
+        ScPreviewShell* pViewShell,
+        const EditTextObject* pEditObj,
+        sal_Bool bHeader,
+        SvxAdjust eAdjust)
+        : ScAccessibleContextBase(rxParent, AccessibleRole::TEXT),
+        mpEditObj(pEditObj->Clone()),
+        mpTextHelper(NULL),
+        mpViewShell(pViewShell),
+        mbHeader(bHeader),
+        meAdjust(eAdjust)
+{
+}
+
+ScAccessiblePageHeaderArea::~ScAccessiblePageHeaderArea(void)
+{
+    if (!ScAccessibleContextBase::IsDefunc() && !rBHelper.bInDispose)
+    {
+        // increment refcount to prevent double call off dtor
+        osl_incrementInterlockedCount( &m_refCount );
+        dispose();
+    }
+}
+
+void SAL_CALL ScAccessiblePageHeaderArea::disposing()
+{
+    if (mpTextHelper)
+        DELETEZ(mpTextHelper);
+
+    ScAccessibleContextBase::disposing();
+}
+
+    //=====  XAccessibleComponent  ============================================
+
+uno::Reference< XAccessible > SAL_CALL ScAccessiblePageHeaderArea::getAccessibleAt(
+        const awt::Point& rPoint )
+        throw (uno::RuntimeException)
+{
+     ScUnoGuard aGuard;
+    if(!mpTextHelper)
+        CreateTextHelper();
+
+    return mpTextHelper->GetAt(rPoint);
+}
+
+    //=====  XAccessibleContext  ==============================================
+
+sal_Int32 SAL_CALL
+    ScAccessiblePageHeaderArea::getAccessibleChildCount(void)
+                    throw (uno::RuntimeException)
+{
+    ScUnoGuard aGuard;
+    if (!mpTextHelper)
+        CreateTextHelper();
+    return mpTextHelper->GetChildCount();
+}
+
+uno::Reference< XAccessible > SAL_CALL
+    ScAccessiblePageHeaderArea::getAccessibleChild(sal_Int32 nIndex)
+        throw (uno::RuntimeException,
+        lang::IndexOutOfBoundsException)
+{
+    ScUnoGuard aGuard;
+    if (!mpTextHelper)
+        CreateTextHelper();
+    return mpTextHelper->GetChild(nIndex);
+}
+
+uno::Reference<XAccessibleStateSet> SAL_CALL
+    ScAccessiblePageHeaderArea::getAccessibleStateSet(void)
+    throw (uno::RuntimeException)
+{
+    ScUnoGuard aGuard;
+    uno::Reference<XAccessibleStateSet> xParentStates;
+    if (getAccessibleParent().is())
+    {
+        uno::Reference<XAccessibleContext> xParentContext = getAccessibleParent()->getAccessibleContext();
+        xParentStates = xParentContext->getAccessibleStateSet();
+    }
+    utl::AccessibleStateSetHelper* pStateSet = new utl::AccessibleStateSetHelper();
+//  if (IsDefunc(xParentStates))
+//      pStateSet->AddState(AccessibleStateType::DEFUNC);
+    pStateSet->AddState(AccessibleStateType::ENABLED);
+    pStateSet->AddState(AccessibleStateType::MULTILINE);
+    if (isShowing())
+        pStateSet->AddState(AccessibleStateType::SHOWING);
+    if (isVisible())
+        pStateSet->AddState(AccessibleStateType::VISIBLE);
+    return pStateSet;
+}
+
+//=====  XTypeProvider  =======================================================
+
+uno::Sequence<sal_Int8> SAL_CALL
+    ScAccessiblePageHeaderArea::getImplementationId(void)
+    throw (uno::RuntimeException)
+{
+    ScUnoGuard aGuard;
+    static uno::Sequence<sal_Int8> aId;
+    if (aId.getLength() == 0)
+    {
+        aId.realloc (16);
+        rtl_createUuid (reinterpret_cast<sal_uInt8 *>(aId.getArray()), 0, sal_True);
+    }
+    return aId;
+}
+
+//===== internal ==============================================================
+
+void ScAccessiblePageHeaderArea::CreateTextHelper()
+{
+    if (!mpTextHelper)
+    {
+        ::std::auto_ptr < ScAccessibleTextData > pAccessibleHeaderTextData
+            (new ScAccessibleHeaderTextData(mpViewShell, mpEditObj, mbHeader, meAdjust));
+        ::std::auto_ptr< SvxEditSource > pEditSource (new ScAccessibilityEditSource(pAccessibleHeaderTextData));
+
+        mpTextHelper = new accessibility::AccessibleTextHelper(this, pEditSource );
+    }
+}
---
+++ sc/source/ui/inc/AccessiblePageHeaderArea.hxx
@@ -0,0 +1,147 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessiblePageHeaderArea.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: sab $ $Date: 2002-05-24 15:11:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifndef _SC_ACCESSIBLEPAGEHEADERAREA_HXX
+#define _SC_ACCESSIBLEPAGEHEADERAREA_HXX
+
+#ifndef _SC_ACCESSIBLE_CONTEXT_BASE_HXX
+#include "AccessibleContextBase.hxx"
+#endif
+#ifndef _SVX_SVXENUM_HXX
+#include <svx/svxenum.hxx>
+#endif
+
+class EditTextObject;
+namespace accessibility
+{
+    class AccessibleTextHelper;
+}
+class ScPreviewShell;
+
+class ScAccessiblePageHeaderArea
+    :   public ScAccessibleContextBase
+{
+public:
+    //=====  internal  ========================================================
+    ScAccessiblePageHeaderArea(
+        const ::com::sun::star::uno::Reference<
+        ::drafts::com::sun::star::accessibility::XAccessible>& rxParent,
+        ScPreviewShell* pViewShell,
+        const EditTextObject* pEditObj,
+        sal_Bool bHeader,
+        SvxAdjust eAdjust);
+protected:
+    virtual ~ScAccessiblePageHeaderArea (void);
+public:
+    const EditTextObject* GetEditTextObject() const { return mpEditObj; }
+
+    virtual void SAL_CALL disposing();
+
+    ///=====  XAccessibleComponent  ============================================
+
+    virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::accessibility::XAccessible >
+        SAL_CALL getAccessibleAt(
+        const ::com::sun::star::awt::Point& rPoint )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    ///=====  XAccessibleContext  ==============================================
+
+    /// Return the number of currently visible children.
+    // is overloaded to calculate this on demand
+    virtual sal_Int32 SAL_CALL
+        getAccessibleChildCount(void)
+                    throw (::com::sun::star::uno::RuntimeException);
+
+    /// Return the specified child or NULL if index is invalid.
+    // is overloaded to calculate this on demand
+    virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::accessibility::XAccessible> SAL_CALL
+        getAccessibleChild(sal_Int32 nIndex)
+        throw (::com::sun::star::uno::RuntimeException,
+                ::com::sun::star::lang::IndexOutOfBoundsException);
+
+    /// Return the set of current states.
+    virtual ::com::sun::star::uno::Reference<
+            ::drafts::com::sun::star::accessibility::XAccessibleStateSet> SAL_CALL
+        getAccessibleStateSet(void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    ///=====  XTypeProvider  ===================================================
+
+    /** Returns a implementation id.
+    */
+    virtual ::com::sun::star::uno::Sequence<sal_Int8> SAL_CALL
+        getImplementationId(void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+private:
+    EditTextObject* mpEditObj;
+    accessibility::AccessibleTextHelper* mpTextHelper;
+    ScPreviewShell* mpViewShell;
+    sal_Bool mbHeader;
+    SvxAdjust meAdjust;
+
+    void CreateTextHelper();
+};
+
+
+#endif
+
