7cdaf323263af808c02dae32d12241204a78722a
Cyrille Moureaux <cyrillem@openoffice.org>
2002-05-27 16:09:16 +0000

    Implementation of SingleBackend for local file system

---
+++ configmgr/source/localbe/localsinglebackend.cxx
@@ -0,0 +1,301 @@
+/*************************************************************************
+ *
+ *  $RCSfile: localsinglebackend.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: cyrillem $ $Date: 2002-05-27 17:09:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
+#include "localsinglebackend.hxx"
+#endif // CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
+
+#ifndef CONFIGMGR_LOCALBE_LOCALFILELAYER_HXX_
+#include "localfilelayer.hxx"
+#endif // CONFIGMGR_LOCALBE_LOCALFILELAYER_HXX_
+
+#ifndef _CONFIGMGR_OSLSTREAM_HXX_
+#include "oslstream.hxx"
+#endif // _CONFIGMGR_OSLSTREAM_HXX_
+
+#ifndef _RTL_USTRBUF_HXX_
+#include <rtl/ustrbuf.hxx>
+#endif // _RTL_USTRBUF_HXX_
+
+namespace configmgr {
+
+//==============================================================================
+
+//------------------------------------------------------------------------------
+
+LocalSingleBackend::LocalSingleBackend(
+        const uno::Reference<uno::XComponentContext>& aContext)
+: SingleBackendBase(mMutex), mContext(aContext) {
+}
+//------------------------------------------------------------------------------
+
+void SAL_CALL LocalSingleBackend::initialize(
+        const uno::Sequence<uno::Any>& aParameters) {
+}
+//------------------------------------------------------------------------------
+
+static sal_Unicode kComponentSeparator = '.' ;
+static sal_Unicode kPathSeparator = '/' ;
+
+static rtl::OUString componentToPath(const rtl::OUString& aComponent) {
+    rtl::OUStringBuffer retCode ;
+
+    retCode.append(kPathSeparator) ;
+    retCode.append(aComponent.replace(kComponentSeparator, kPathSeparator)) ;
+    return retCode.makeStringAndClear() ;
+}
+//------------------------------------------------------------------------------
+
+static const rtl::OUString kDefaultsSubPath(
+                                        RTL_CONSTASCII_USTRINGPARAM("data")) ;
+static const rtl::OUString kDataSuffix(RTL_CONSTASCII_USTRINGPARAM(".xcu")) ;
+
+uno::Sequence<rtl::OUString> SAL_CALL LocalSingleBackend::listLayerIds(
+        const rtl::OUString& aComponent, const rtl::OUString& aEntity)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    if (!aEntity.equals(mOwnId)) {
+        throw lang::IllegalArgumentException(
+                rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                        "Can only access user data")),
+                *this, 1) ;
+    }
+    uno::Sequence<rtl::OUString> retCode(2) ;
+    rtl::OUString componentSubPath = componentToPath(aComponent) + kDataSuffix ;
+    // First, the defaults...
+    rtl::OUStringBuffer buffer(mSharedDataUrl) ;
+
+    buffer.append(kDefaultsSubPath).append(componentSubPath) ;
+    retCode [0] = buffer.makeStringAndClear() ;
+    // Then the user delta.
+    buffer.append(mUserDataUrl).append(componentSubPath) ;
+    retCode [1] = buffer.makeStringAndClear() ;
+    return retCode ;
+}
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL LocalSingleBackend::getUpdateLayerId(
+        const rtl::OUString& aComponent, const rtl::OUString& aEntity)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    if (!aEntity.equals(mOwnId)) {
+        throw lang::IllegalArgumentException(
+                rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                        "Can only access user data")),
+                *this, 1) ;
+    }
+    rtl::OUStringBuffer retCode(mUserDataUrl) ;
+
+    retCode.append(componentToPath(aComponent)).append(kDataSuffix) ;
+    return retCode.makeStringAndClear() ;
+}
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL LocalSingleBackend::getOwnId(void) {
+    return mOwnId ;
+}
+//------------------------------------------------------------------------------
+
+/**
+  Tests whether a file defined by its URL is more recent than a
+  date provided as a timestamp.
+  The timestamp is formatted YYYYMMDDhhmmssZ.
+
+  @param aFileUrl   URL of the file to test
+  @param aTimestamp timestamp to test against
+  @return   sal_True if the file is more recent, sal_False otherwise
+  */
+static sal_Bool isMoreRecent(const rtl::OUString& aFileUrl,
+                             const rtl::OUString& aTimestamp) {
+    return (LocalFileLayer::getTimestamp(aFileUrl).compareTo(aTimestamp) > 0) ;
+}
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XLayer> SAL_CALL LocalSingleBackend::getLayer(
+        const rtl::OUString& aLayerId, const rtl::OUString& aTimestamp)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    if (!isMoreRecent(aLayerId, aTimestamp)) { return NULL ; }
+    return new LocalFileLayer(mContext, aLayerId) ;
+}
+//------------------------------------------------------------------------------
+
+uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
+LocalSingleBackend::getLayers(const uno::Sequence<rtl::OUString>& aLayerIds,
+                              const rtl::OUString& aTimestamp)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    uno::Sequence<uno::Reference<backend::XLayer> >
+                                                retCode(aLayerIds.getLength()) ;
+
+    for (sal_Int32 i = 0 ; i < aLayerIds.getLength() ; ++ i) {
+        retCode [i] = getLayer(aLayerIds [i], aTimestamp) ;
+    }
+    return retCode ;
+}
+//------------------------------------------------------------------------------
+
+uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
+LocalSingleBackend::getMultipleLayers(
+                        const uno::Sequence<rtl::OUString>& aLayerIds,
+                        const uno::Sequence<rtl::OUString>& aTimestamps)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    if (aLayerIds.getLength() != aTimestamps.getLength()) {
+        throw lang::IllegalArgumentException(
+                rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                        "Not enough or too many timestamps")),
+                *this, 0) ;
+    }
+    uno::Sequence<uno::Reference<backend::XLayer> >
+                                                retCode(aLayerIds.getLength()) ;
+
+    for (sal_Int32 i = 0 ; i < aLayerIds.getLength() ; ++ i) {
+        retCode [i] = getLayer(aLayerIds [i], aTimestamps [i]) ;
+    }
+    return retCode ;
+}
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XUpdatableLayer> SAL_CALL
+LocalSingleBackend::getUpdatableLayer(const rtl::OUString& aLayerId)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    return new LocalFileLayer(mContext, aLayerId) ;
+}
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XLayerHandler> SAL_CALL
+LocalSingleBackend::getWriteHandler(const rtl::OUString& aLayerId)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    return getUpdatableLayer(aLayerId)->getWriteHandler() ;
+}
+//------------------------------------------------------------------------------
+
+static const rtl::OUString kSchemaSubPath(
+                                    RTL_CONSTASCII_USTRINGPARAM("/schema")) ;
+static const rtl::OUString kSchemaSuffix(RTL_CONSTASCII_USTRINGPARAM(".xcs")) ;
+static const rtl::OUString kXMLSchemaParser(RTL_CONSTASCII_USTRINGPARAM(
+            "com.sun.star.configuration.backend.xml.SchemaParser")) ;
+
+uno::Reference<backend::XSchema> SAL_CALL LocalSingleBackend::getSchema(
+                                              const rtl::OUString& aComponent)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    rtl::OUString subPath = componentToPath(aComponent) ;
+    rtl::OUStringBuffer schemaUrl(mSharedDataUrl) ;
+
+    schemaUrl.append(kSchemaSubPath).append(subPath).append(kSchemaSuffix) ;
+    osl::File *schemaFile = new osl::File(schemaUrl.makeStringAndClear()) ;
+    osl::FileBase::RC errorCode = schemaFile->open(OpenFlag_Read) ;
+
+    if (errorCode != osl_File_E_None) {
+        throw backend::BackendAccessException(
+                FileHelper::createOSLErrorString(errorCode), *this) ;
+    }
+    uno::Sequence<uno::Any> arguments(1) ;
+    uno::Reference<io::XInputStream> stream =
+                            new OSLInputStreamWrapper(schemaFile, sal_True) ;
+
+    arguments [0] <<= stream ;
+    return uno::Reference<backend::XSchema>::query(
+        mContext->getServiceManager()->createInstanceWithArgumentsAndContext(
+            kXMLSchemaParser, arguments, mContext)) ;
+}
+//------------------------------------------------------------------------------
+
+static const rtl::OUString kImplementationName(
+        RTL_CONSTASCII_USTRINGPARAM(
+            "com.sun.star.comp.configuration.backend.LocalSingleBackend")) ;
+
+rtl::OUString SAL_CALL LocalSingleBackend::getName(void) {
+    return kImplementationName ;
+}
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL LocalSingleBackend::getImplementationName(void) {
+    return getName() ;
+}
+//------------------------------------------------------------------------------
+
+static const rtl::OUString kSingleBackendServiceName(
+        RTL_CONSTASCII_USTRINGPARAM(
+            "com.sun.star.configuration.backend.SingleBackend")) ;
+
+sal_Bool SAL_CALL LocalSingleBackend::supportsService(
+                                        const rtl::OUString& aServiceName) {
+    return aServiceName.equals(kSingleBackendServiceName) ;
+}
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL LocalSingleBackend::getServices(void) {
+    return uno::Sequence<rtl::OUString>(&kSingleBackendServiceName, 1) ;
+}
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString>
+SAL_CALL LocalSingleBackend::getSupportedServiceNames(void) {
+    return getServices() ;
+}
+//------------------------------------------------------------------------------
+
+} // configmgr
---
+++ configmgr/source/localbe/localsinglebackend.hxx
@@ -0,0 +1,191 @@
+/*************************************************************************
+ *
+ *  $RCSfile: localsinglebackend.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: cyrillem $ $Date: 2002-05-27 17:09:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
+#define CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
+
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSINGLEBACKEND_HPP_
+#include <drafts/com/sun/star/configuration/backend/XSingleBackend.hpp>
+#endif // _COM_SUN_STAR_CONFIGURATION_BACKEND_XSINGLEBACKEND_HPP_
+
+#ifndef _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
+#include <com/sun/star/lang/XInitialization.hpp>
+#endif // _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif // _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif // _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+
+#ifndef _CPPUHELPER_COMPBASE3_HXX_
+#include <cppuhelper/compbase3.hxx>
+#endif // _CPPUHELPER_COMPBASE3_HXX_
+
+namespace configmgr {
+
+namespace css = com::sun::star ;
+namespace uno = css::uno ;
+namespace lang = css::lang ;
+//namespace backend = css::configuration::backend ;
+namespace backend = drafts::com::sun::star::configuration::backend ;
+
+typedef cppu::WeakComponentImplHelper3<backend::XSingleBackend,
+                                       lang::XInitialization,
+                                       lang::XServiceInfo> SingleBackendBase ;
+
+/**
+  Implements the SingleBackend service for local file access.
+  Layer identifiers in that backend are file URLs.
+  */
+class LocalSingleBackend : public SingleBackendBase {
+    public :
+        /**
+          Service constructor from a component context.
+
+          @param xContext   component context
+          */
+        LocalSingleBackend(
+                const uno::Reference<uno::XComponentContext>& xContext) ;
+        /** Destructor */
+        ~LocalSingleBackend(void) ;
+
+        // XInitialize
+        virtual void SAL_CALL initialize(
+                                const uno::Sequence<uno::Any>& aParameters) ;
+        // XSingleBackend
+        virtual uno::Sequence<rtl::OUString> SAL_CALL listLayerIds(
+                                                const rtl::OUString& aComponent,
+                                                const rtl::OUString& aEntity)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual rtl::OUString SAL_CALL getUpdateLayerId(
+                                                const rtl::OUString& aComponent,
+                                                const rtl::OUString& aEntity)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual rtl::OUString SAL_CALL getOwnId(void) ;
+        virtual uno::Reference<backend::XLayer> SAL_CALL getLayer(
+                                            const rtl::OUString& aLayerId,
+                                            const rtl::OUString& aTimestamp)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
+            getLayers(const uno::Sequence<rtl::OUString>& aLayerIds,
+                      const rtl::OUString& aTimestamp)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
+            getMultipleLayers(const uno::Sequence<rtl::OUString>& aLayerIds,
+                              const uno::Sequence<rtl::OUString>& aTimestamps)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual uno::Reference<backend::XUpdatableLayer> SAL_CALL
+            getUpdatableLayer(const rtl::OUString& aLayerId)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual uno::Reference<backend::XLayerHandler> SAL_CALL
+            getWriteHandler(const rtl::OUString& aLayerId)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        virtual uno::Reference<backend::XSchema> SAL_CALL getSchema(
+                                            const rtl::OUString& aComponent)
+            throw (backend::BackendAccessException,
+                    lang::IllegalArgumentException) ;
+        // XServiceInfo
+        virtual rtl::OUString SAL_CALL getImplementationName(void) ;
+        virtual sal_Bool SAL_CALL supportsService(
+                                            const rtl::OUString& aServiceName) ;
+        virtual uno::Sequence<rtl::OUString> SAL_CALL
+            getSupportedServiceNames(void) ;
+
+        /**
+          Provides the implementation name.
+
+          @return   implementation name
+          */
+        static rtl::OUString SAL_CALL getName(void) ;
+        /**
+          Provides the supported services names
+
+          @return   service names
+          */
+        static uno::Sequence<rtl::OUString> SAL_CALL getServices(void) ;
+
+    protected :
+    private :
+        /** Component context */
+        const uno::Reference<uno::XComponentContext>& mContext ;
+        /** Mutex for resources protection */
+        osl::Mutex mMutex ;
+        /** Identifier of the user of the backend */
+        rtl::OUString mOwnId ;
+        /** Base of the shared data */
+        rtl::OUString mSharedDataUrl ;
+        /** Base of the user data */
+        rtl::OUString mUserDataUrl ;
+} ;
+
+} // configmgr
+
+#endif // CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
