64f4577f3ab59c4f3657d6269a4a09d47d7b3810
Juergen Schmidt <jsc@openoffice.org>
2002-05-28 12:27:27 +0000

    #99053# change productname for openoffice build

--- odk/util/makefile.mk
+++ odk/util/makefile.mk
@@ -9,6 +9,9 @@ TARGET=odk
 
 all: zipit 
 
+.IF "$(BUILD_SOSL)"==""
+# for SUN build without minor
+
 zipit .SETDIR=$(DESTDIR)$/.. .PHONY:
     .IF "$(OS)"=="WNT"
         +zip -ur $(ODKNAME).zip $(ODKNAME)
@@ -16,5 +19,19 @@ zipit .SETDIR=$(DESTDIR)$/.. .PHONY:
 # 		tar does not properly support update
         +tar cf - $(ODKNAME) | gzip - > $(ODKNAME).tar.gz
     .ENDIF
+
+.ELSE
+# for OpenOffice build with minor 
+
+zipit .SETDIR=$(DESTDIR)$/.. .PHONY:
+    .IF "$(OS)"=="WNT"
+        +zip -ur $(PRODUCT_NAME).zip $(PRODUCT_NAME)
+    .ELIF "$(GUI)"=="UNX"
+# 		tar does not properly support update
+        +tar cf - $(PRODUCT_NAME) | gzip - > $(PRODUCT_NAME).tar.gz
+    .ENDIF
+
+.ENDIF
+
 
 
