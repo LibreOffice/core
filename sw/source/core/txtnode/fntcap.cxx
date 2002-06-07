665748c737d60eb89449600bfe6abe0cea5d2380
Frank Meies <fme@openoffice.org>
2002-06-07 14:47:51 +0000

    Fix #99962# DrawStretchText must consider COL_AUTO

--- sw/source/core/txtnode/fntcap.cxx
+++ sw/source/core/txtnode/fntcap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fntcap.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: fme $ $Date: 2002-03-19 09:01:41 $
+ *  last change: $Author: fme $ $Date: 2002-06-07 15:47:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -298,8 +298,11 @@ void SwDoDrawCapital::DrawSpace( Point &rPos )
     static sal_Char __READONLY_DATA sDoubleSpace[] = "  ";
     const USHORT nDiff = (USHORT)( rInf.GetPos().X() - rPos.X() );
     if ( nDiff )
+    {
+        rInf.ApplyAutoColor();
         GetOut()->DrawStretchText( rPos, nDiff,
             XubString( sDoubleSpace, RTL_TEXTENCODING_MS_1252 ), 0, 2 );
+    }
     rPos.X() = rInf.GetPos().X() + rInf.GetWidth();
 }
 
