b4dd99437bb00068e473c7abc35f07f0bae250d2
Hans-Joachim Lankenau <hjs@openoffice.org>
2002-06-12 09:09:15 +0000

    replaced / in compiler switches to avoid tcsh problems

--- fpicker/source/win32/misc/makefile.mk
+++ fpicker/source/win32/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: tra $ $Date: 2001-06-28 11:11:15 $
+#   last change: $Author: hjs $ $Date: 2002-06-12 10:09:15 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -71,7 +71,7 @@ LIBTARGET=NO
 
 .INCLUDE : ..$/..$/cppumaker.mk
 
-CFLAGS+=/GX
+CFLAGS+=-GX
 
 # --- Files -------------------------------------
 
