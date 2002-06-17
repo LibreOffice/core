16192bf2af10ca653b49f92ac00e68599b6d864f
Rüdiger Timm <rt@openoffice.org>
2002-06-17 10:54:34 +0000

    #i5536# There is no GUI MACOSX. Max-X11 builds use GUI=UNX and identifier 'u', the (historic) identifier 'm' for Mac OS 9 had GUI MAC.

--- solenv/bin/build.pl
+++ solenv/bin/build.pl
@@ -5,9 +5,9 @@ eval 'exec perl -S $0 ${1+"$@"}'
 #
 #   $RCSfile: build.pl,v $
 #
-#   $Revision: 1.55 $
+#   $Revision: 1.56 $
 #
-#   last change: $Author: hr $ $Date: 2002-05-07 16:25:10 $
+#   last change: $Author: rt $ $Date: 2002-06-17 11:54:34 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -75,7 +75,7 @@ use Cwd;
 
 ( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
-$id_str = ' $Revision: 1.55 $ ';
+$id_str = ' $Revision: 1.56 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -545,7 +545,7 @@ sub CheckPlatform {
         return 1;
     } elsif (($ENV{GUI} eq 'UNX') && ($Platform eq 'u')) {
         return 1;
-    } elsif (($ENV{GUI} eq 'MACOSX') && ($Platform eq 'm')) {
+    } elsif (($ENV{GUI} eq 'MAC') && ($Platform eq 'm')) {
         return 1;
     } elsif (($ENV{GUI} eq 'OS2') && ($Platform eq 'p')) {
         return 1;
