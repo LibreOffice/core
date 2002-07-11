ccff6eaf54aa7d576854bf02e63e2cc316d63492
Martin Hollmichel <mh@openoffice.org>
2002-07-11 07:44:25 +0000

    join: from OOO_STABLE_1

--- dmake/autogen.sh
+++ dmake/autogen.sh
@@ -12,18 +12,33 @@ FILE=make.c
 
 DIE=0
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+# cvs may leave some files unwritable
+(chmod -Rf u+rw $srcdir) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "You must have autoconf installed to compile $PROJECT."
-    echo "Download the appropriate package for your distribution,"
-    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    echo "You must have owner permissions for all files and subdirectories"
+    echo "in $srcdir"
+    DIE=1
+}
+
+(autoconf --version | grep '2.13' ) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf version 2.13 installed to compile $PROJECT."
+    echo "Get ftp://ftp.gnu.org/gnu/gettext/autoconf/autoconf-2.13.tar.gz"
     DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "You must have automake installed to compile $PROJECT."
-    echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+    echo "Get ftp://ftp.gnu.org/gnu/gettext/automake/automake-1.4.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+}
+
+(gettextize --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have gettextize installed to compile $PROJECT."
+    echo "Get ftp://ftp.gnu.org/gnu/gettext/gettext-0.10.38.tar.gz"
     echo "(or a newer version if it is available)"
     DIE=1
 }
@@ -41,10 +56,6 @@ if test -z "$*"; then
     echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
 fi
-
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
 
 if test -z "$ACLOCAL_FLAGS"; then
 
@@ -83,11 +94,11 @@ aclocal $ACLOCAL_FLAGS
 # optionally feature autoheader
 (autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
 
-automake --foreign $am_opt
+automake -a --include-deps $am_opt
 autoconf
 cd $ORIGDIR
 
-$srcdir/configure --enable-maintainer-mode  --prefix=/usr/local/test "$@"
+$srcdir/configure --enable-maintainer-mode  --prefix=/usr/local "$@"
 
 echo
-echo "Now type 'make' to compile $PROJECT."
+echo "Now exucute GNU make to compile $PROJECT."
