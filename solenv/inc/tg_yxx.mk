203eb6f11c3d6209f064547f523d5c27d6bbbf86
Hans-Joachim Lankenau <hjs@openoffice.org>
2002-07-17 14:34:25 +0000

    #101448# stripping dos lineends from all *.y and *.yy files

--- solenv/inc/tg_yxx.mk
+++ solenv/inc/tg_yxx.mk
@@ -3,7 +3,12 @@
 $(MISC)$/%.cxx : %.y
     @echo ------------------------------
     @echo Making: $@
-    $(BISON) $(YACCFLAGS) $(YACCTARGET) $(YACCFILES)
+.IF "$(GUI)"=="UNX" || "$(USE_SHELL)" != "4nt"
+    +tr -d "\015" < $< > $(MISC)$/stripped_$<
+.ELSE
+    +cat $< > $(MISC)$/stripped_$<
+.ENDIF
+    $(BISON) $(YACCFLAGS) $(YACCTARGET) $(MISC)$/stripped_$<
 # removing -f switch - avoid getting nothing when copying a file to itself
     @+-$(COPY:s/-f//) $@.h $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
     @+-$(COPY:s/-f//) $(@:d)$/$(@:b).hxx $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
@@ -12,7 +17,12 @@ $(MISC)$/%.cxx : %.y
 $(INCCOM)$/yy%.cxx : %.y
     @echo ------------------------------
     @echo Making: $@
-    $(BISON) $(YACCFLAGS) $(YACCTARGET) $(YACCFILES)
+.IF "$(GUI)"=="UNX" || "$(USE_SHELL)" != "4nt"
+    +tr -d "\015" < $< > $(MISC)$/stripped_$<
+.ELSE
+    +cat $< > $(MISC)$/stripped_$<
+.ENDIF
+    $(BISON) $(YACCFLAGS) $(YACCTARGET) $(MISC)$/stripped_$<
 # removing -f switch - avoid getting nothing when copying a file to itself
     @+-$(COPY:s/-f//) $@.h $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
     @+-$(COPY:s/-f//) $(@:d)$/$(@:b).hxx $(INCCOM)$/$(@:b).hxx >& $(NULLDEV)
