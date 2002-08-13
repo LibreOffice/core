fcc0b40d3b4f2dbf4894dbf9a5ef9c54617c2b11
Martin Hollmichel <mh@openoffice.org>
2002-08-13 11:29:52 +0000

    join: from OOO_STABLE_1_PORTS, #101685#

--- cosv/source/strings/streamstr.cxx
+++ cosv/source/strings/streamstr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: streamstr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: np $ $Date: 2002-05-14 08:08:46 $
+ *  last change: $Author: mh $ $Date: 2002-08-13 12:29:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,7 +65,7 @@
 // NOT FULLY DECLARED SERVICES
 #include <string.h>
 #include <stdio.h>
-#if defined(WNT) || defined(LINUX)
+#if defined(WNT) || defined(LINUX) || defined(MACOSX)
 #include <stdarg.h>
 #else
 #include <sys/varargs.h>
