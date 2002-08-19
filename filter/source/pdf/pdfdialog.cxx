d1aa00e19c219f8bd6ff25eff48e9a525ac8c29d
Kai Ahrens <ka@openoffice.org>
2002-08-19 05:41:18 +0000

    #101570#: initial revision

---
+++ filter/source/pdf/pdfdialog.cxx
@@ -0,0 +1,264 @@
+ /*************************************************************************
+ *
+ *  $RCSfile: pdfdialog.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: ka $ $Date: 2002-08-19 06:41:17 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "pdfdialog.hxx"
+#include "impdialog.hxx"
+#include <vcl/svapp.hxx>
+#include <vcl/dialog.hxx>
+#include <svtools/solar.hrc>
+
+// -----------------------
+// - PDFDialog functions -
+// -----------------------
+
+#define SERVICE_NAME "com.sun.star.document.PDFDialog"
+
+// -----------------------------------------------------------------------------
+
+OUString PDFDialog_getImplementationName ()
+    throw (RuntimeException)
+{
+    return OUString ( RTL_CONSTASCII_USTRINGPARAM ( "com.sun.star.comp.PDF.PDFDialog" ) );
+}
+
+// -----------------------------------------------------------------------------
+
+Sequence< sal_Int8 > SAL_CALL PDFDialog_getImplementationId()
+    throw(RuntimeException)
+{
+    static ::cppu::OImplementationId aId;
+    return aId.getImplementationId();
+}
+
+// -----------------------------------------------------------------------------
+
+Sequence< OUString > SAL_CALL PDFDialog_getSupportedServiceNames()
+    throw (RuntimeException)
+{
+    Sequence < OUString > aRet(1);
+    OUString* pArray = aRet.getArray();
+    pArray[0] =  OUString ( RTL_CONSTASCII_USTRINGPARAM ( SERVICE_NAME ) );
+    return aRet;
+}
+
+// -----------------------------------------------------------------------------
+
+Reference< XInterface > SAL_CALL PDFDialog_createInstance( const Reference< XMultiServiceFactory > & rSMgr)
+    throw( Exception )
+{
+    return (cppu::OWeakObject*) new PDFDialog( rSMgr );
+}
+
+// -----------------------------------------------------------------------------
+
+#undef SERVICE_NAME
+
+// -------------
+// - PDFDialog -
+// -------------
+
+PDFDialog::PDFDialog( const Reference< XMultiServiceFactory > &rxMSF ) :
+    OGenericUnoDialog( rxMSF )
+{
+    ByteString aResMgrName( "pdffilter" );
+
+    aResMgrName.Append( ByteString::CreateFromInt32( SOLARUPD ) );
+    mpResMgr = ResMgr::CreateResMgr( aResMgrName.GetBuffer(), Application::GetSettings().GetUILanguage() );
+}
+
+// -----------------------------------------------------------------------------
+
+PDFDialog::~PDFDialog()
+{
+    delete mpResMgr;
+}
+
+// -----------------------------------------------------------------------------
+
+Any SAL_CALL PDFDialog::queryInterface( const Type& rType )
+    throw (RuntimeException)
+{
+    Any aReturn = OGenericUnoDialog::queryInterface( rType );
+
+    if( !aReturn.hasValue() )
+        aReturn = ::cppu::queryInterface( rType, static_cast< XPropertyAccess* >( this ) );
+
+    return aReturn;
+}
+
+// -----------------------------------------------------------------------------
+
+void SAL_CALL PDFDialog::acquire()
+    throw ()
+{
+    OWeakObject::acquire();
+}
+
+// -----------------------------------------------------------------------------
+
+void SAL_CALL PDFDialog::release()
+    throw ()
+{
+    OWeakObject::release();
+}
+
+// -----------------------------------------------------------------------------
+
+Sequence< sal_Int8 > SAL_CALL PDFDialog::getImplementationId()
+    throw(RuntimeException)
+{
+    return PDFDialog_getImplementationId();
+}
+
+// -----------------------------------------------------------------------------
+
+OUString SAL_CALL PDFDialog::getImplementationName()
+    throw (RuntimeException)
+{
+    return PDFDialog_getImplementationName();
+}
+
+// -----------------------------------------------------------------------------
+
+Sequence< OUString > SAL_CALL PDFDialog::getSupportedServiceNames()
+    throw (RuntimeException)
+{
+    return PDFDialog_getSupportedServiceNames();
+}
+
+// -----------------------------------------------------------------------------
+
+Dialog* PDFDialog::createDialog( Window* pParent )
+{
+    return( mpResMgr ? ( new ImpPDFDialog( pParent, *mpResMgr, maFilterData ) ) : NULL );
+}
+
+// -----------------------------------------------------------------------------
+
+void PDFDialog::executedDialog( sal_Int16 nExecutionResult )
+{
+    if( nExecutionResult && m_pDialog )
+        maFilterData = static_cast< ImpPDFDialog* >( m_pDialog )->GetFilterData();
+
+    destroyDialog();
+}
+
+// -----------------------------------------------------------------------------
+
+Reference< XPropertySetInfo > SAL_CALL PDFDialog::getPropertySetInfo()
+    throw(RuntimeException)
+{
+    Reference< XPropertySetInfo >  xInfo( createPropertySetInfo( getInfoHelper() ) );
+    return xInfo;
+}
+
+//-------------------------------------------------------------------------
+::cppu::IPropertyArrayHelper& PDFDialog::getInfoHelper()
+{
+    return *const_cast<PDFDialog*>(this)->getArrayHelper();
+}
+
+//------------------------------------------------------------------------------
+::cppu::IPropertyArrayHelper* PDFDialog::createArrayHelper() const
+{
+    Sequence< Property > aProps;
+    describeProperties(aProps);
+    return new ::cppu::OPropertyArrayHelper( aProps );
+}
+
+// -----------------------------------------------------------------------------
+
+Sequence< PropertyValue > SAL_CALL PDFDialog::getPropertyValues()
+    throw ( RuntimeException )
+{
+    sal_Int32 i, nCount;
+
+    for( i = 0, nCount = maMediaDescriptor.getLength(); i < nCount; i++ )
+    {
+        if( maMediaDescriptor[ i ].Name.equalsAscii( "FilterData" ) )
+            break;
+    }
+
+    if( i == nCount )
+        maMediaDescriptor.realloc( ++nCount );
+
+    maMediaDescriptor[ i ].Name = String( RTL_CONSTASCII_USTRINGPARAM( "FilterData" ) );
+    maMediaDescriptor[ i ].Value <<= maFilterData;
+
+    return maMediaDescriptor;
+}
+
+// -----------------------------------------------------------------------------
+
+void SAL_CALL PDFDialog::setPropertyValues( const Sequence< PropertyValue >& rProps )
+    throw ( UnknownPropertyException, PropertyVetoException, IllegalArgumentException, WrappedTargetException, RuntimeException )
+{
+    maMediaDescriptor = rProps;
+
+    for( sal_Int32 i = 0, nCount = maMediaDescriptor.getLength(); i < nCount; i++ )
+    {
+        if ( maMediaDescriptor[ i ].Name.equalsAscii( "FilterData" ) )
+        {
+            maMediaDescriptor[ i ].Value >>= maFilterData;
+            break;
+        }
+    }
+}
---
+++ filter/source/pdf/pdfdialog.hxx
@@ -0,0 +1,119 @@
+/*************************************************************************
+ *
+ *  $RCSfile: pdfdialog.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: ka $ $Date: 2002-08-19 06:41:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef PDFDIALOG_HXX
+#define PDFDIALOG_HXX
+
+#include "pdffilter.hxx"
+#include <svtools/genericunodialog.hxx>
+
+// -------------
+// - PDFDialog -
+// -------------
+
+class Window;
+class ResMgr;
+
+class PDFDialog : public ::svt::OGenericUnoDialog,
+                  public ::comphelper::OPropertyArrayUsageHelper< PDFDialog >,
+                  public XPropertyAccess
+{
+private:
+
+    ResMgr*                     mpResMgr;
+    Sequence< PropertyValue >   maMediaDescriptor;
+    Sequence< PropertyValue >   maFilterData;
+
+protected:
+
+    // XInterface
+    virtual Any SAL_CALL queryInterface( const Type& aType ) throw (RuntimeException);
+    virtual void SAL_CALL acquire() throw ();
+    virtual void SAL_CALL release() throw ();
+
+    // OGenericUnoDialog
+    virtual Sequence< sal_Int8 > SAL_CALL getImplementationId() throw(RuntimeException);
+    virtual OUString SAL_CALL getImplementationName() throw (RuntimeException);
+    virtual Sequence< OUString > SAL_CALL getSupportedServiceNames() throw (RuntimeException);
+    virtual Dialog* createDialog( Window* pParent );
+    virtual void executedDialog( sal_Int16 nExecutionResult );
+    virtual Reference< XPropertySetInfo>  SAL_CALL getPropertySetInfo() throw(RuntimeException);
+    virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
+    virtual ::cppu::IPropertyArrayHelper* createArrayHelper( ) const;
+
+    // XPropertyAccess
+    virtual Sequence< PropertyValue > SAL_CALL getPropertyValues(  ) throw (RuntimeException);
+    virtual void SAL_CALL setPropertyValues( const Sequence< PropertyValue >& aProps ) throw (UnknownPropertyException, PropertyVetoException, IllegalArgumentException, WrappedTargetException, RuntimeException);
+
+public:
+
+                PDFDialog( const Reference< XMultiServiceFactory >& rxMSF );
+    virtual     ~PDFDialog();
+};
+
+// -----------------------------------------------------------------------------
+
+OUString PDFDialog_getImplementationName () throw (RuntimeException);
+Sequence< sal_Int8 > SAL_CALL PDFDialog_getImplementationId() throw(RuntimeException);
+Sequence< OUString > SAL_CALL PDFDialog_getSupportedServiceNames() throw (RuntimeException);
+Reference< XInterface > SAL_CALL PDFDialog_createInstance( const Reference< XMultiServiceFactory > & rSMgr) throw( Exception );
+
+#endif // PDFDIALOG_HXX
