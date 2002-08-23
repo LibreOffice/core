62106c646389c8fbe97a46da373fb9b1b98917d6
Jens-Heiner Rechtien <hr@openoffice.org>
2002-08-23 15:18:39 +0000

    #101685#,#i6886#: merge OOO_STABLE_1_PORTS (1.3-1.3.16.1) -> HEAD

--- sw/source/core/doc/makefile.mk
+++ sw/source/core/doc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: mtg $ $Date: 2001-07-19 16:21:30 $
+#   last change: $Author: hr $ $Date: 2002-08-23 16:18:39 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -126,6 +126,10 @@ SLOFILES =	\
         $(SLO)$/tblrwcl.obj \
         $(SLO)$/visiturl.obj \
         $(SLO)$/htmltbl.obj
+
+.IF "$(OS)"=="MACOSX"
+SLOFILES += $(SLO)$/gcc_outl.obj
+.ENDIF
 
 # --- Tagets -------------------------------------------------------
 
