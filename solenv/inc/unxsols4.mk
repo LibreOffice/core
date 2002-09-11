8ffa31dc4ebf9653085c3e1d74248dd3e4a46202
Oliver Bolte <obo@openoffice.org>
2002-09-11 12:26:36 +0000

    Intial revision by HR

---
+++ solenv/inc/unxsols4.mk
@@ -0,0 +1,143 @@
+
+ASM=/usr/ccs/bin/as
+AFLAGS=-P -q
+
+
+CDEFS+=-D_PTHREADS -DSYSV -DSUN -DSUN4 -D_REENTRANT -D_POSIX_PTHREAD_SEMANTICS -DSTLPORT_VERSION=400
+
+.IF "$(SOLAR_JAVA)"!=""
+JAVADEF=-DSOLAR_JAVA
+JAVAFLAGSDEBUG=-g
+.IF "$(debug)"==""
+JAVA_RUNTIME=-ljava
+.ELSE
+JAVA_RUNTIME=-ljava_g
+.ENDIF
+.ENDIF
+
+CC=CC
+cc=cc
+
+CFLAGS=$(PREENVCFLAGS) -c -temp=/tmp $(INCLUDE)
+CFLAGSCC=-xCC
+CFLAGSCXX=-features=no%altspell,no%localfor
+
+CFLAGSEXCEPTIONS=
+CFLAGS_NO_EXCEPTIONS=-noex
+
+CFLAGSOBJGUIST=-KPIC
+CFLAGSOBJCUIST=-KPIC
+CFLAGSOBJGUIMT=-KPIC
+CFLAGSOBJCUIMT=-KPIC
+CFLAGSSLOGUIMT=-KPIC
+CFLAGSSLOCUIMT=-KPIC
+CFLAGSPROF=-xpg
+CFLAGSDEBUG=-g
+CFLAGSDBGUTIL=
+CFLAGSOPT=-O
+CFLAGSNOOPT=
+CFLAGSOUTOBJ=-o
+
+# Enable all warnings
+CFLAGSWALL=+w2
+# Set default warn level
+CFLAGSDFLTWARN=-w
+
+STDOBJVCL=$(L)$/salmain.o
+
+THREADLIB=-lposix4
+.IF "$(PURIFY)"!=""
+LINK=/usr/local/purify-4.2-solaris2/purify CC
+.ELSE
+LINK=CC
+.ENDIF
+
+# -z combreloc combines multiple relocation sections. Reduces overhead on startup
+# -norunpath prevents the compiler from recording his own libs in the runpath
+LINKFLAGS=-w -z combreloc -PIC -temp=/tmp -norunpath -library=no%Crun
+
+# -z text force fatal error if non PIC code is linked into shared library. Such code 
+#    would be expensive on startup
+CHECKFORPIC =-z text
+LINKFLAGSSHLGUI=$(CHECKFORPIC) -G
+LINKFLAGSSHLCUI=$(CHECKFORPIC) -G
+
+.IF "$(NO_BSYMBOLIC)"==""
+.IF "$(PRJNAME)"!="schedule"
+#.IF "$(PRJNAME)"!="envtest"
+LINKFLAGSSHLGUI+=-Bsymbolic
+LINKFLAGSSHLCUI+=-Bsymbolic
+#.ENDIF
+.ENDIF
+.ENDIF				# "$(NO_BSYMBOLIC)"==""
+
+# switches for dynamic and static linking
+STATIC		= -Bstatic
+DYNAMIC		= -Bdynamic
+
+LINKFLAGSTACK=
+LINKFLAGSPROF=-L$(COMPATH)/WS6U1/lib/libp -xpg -z allextract
+LINKFLAGSDEBUG=
+LINKFLAGSOPT=
+LINKVERSIONMAPFLAG=-M
+APPLINKSTATIC=-Bstatic
+APPLINKSHARED=-Bdynamic
+APP_LINKTYPE=
+
+# reihenfolge der libs NICHT egal!
+STDOBJGUI=
+.IF "DBG_UTIL" != ""
+STDSLOGUI=#-lpthread
+.ELSE
+STDSLOGUI=
+.ENDIF
+STDOBJCUI=
+STDSLOCUI=
+STDLIBGUIST=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDLIBCUIST=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDLIBGUIMT=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDLIBCUIMT=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDSHLGUIST=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDSHLCUIST=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDSHLGUIMT=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+STDSHLCUIMT=-Bdynamic -lnsl -lsocket -ldl -lm -lCrun
+
+STDLIBGUIST+=-lX11
+STDLIBGUIMT+=-lX11
+
+LIBSTLPORT=$(DYNAMIC) -lstlport_sunpro
+LIBSTLPORTST=$(STATIC) -lstlport_sunpro $(DYNAMIC)
+
+
+LIBMGR=CC
+LIBFLAGS=-xar -o 
+
+IMPLIB=
+IMPLIBFLAGS=
+
+MAPSYM=
+MAPSYMFLAGS=
+
+RC=irc
+RCFLAGS=-fo$@ $(RCFILES)
+RCLINK=
+RCLINKFLAGS=
+RCSETVERSION=
+
+DLLPOSTFIX=ss
+
+DLLPRE=lib
+DLLPOST=.so
+
+LDUMP=cppfilt /b /n /o /p
+
+CDEFS += -D_PTHREADS  
+#CFLAGS += -U_REENTRANT -U_POSIX_PTHREAD_SEMANTICS
+CFLAGSCXXSLO += -instances=static
+CFLAGSCXXOBJ += -instances=static
+
+LINKFLAGSAPPGUI+= -instances=static
+LINKFLAGSSHLGUI+= -instances=static
+LINKFLAGSAPPCUI+= -instances=static
+LINKFLAGSSHLCUI+= -instances=static
+
