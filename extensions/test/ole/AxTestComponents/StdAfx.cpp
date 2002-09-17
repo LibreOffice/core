b228f80b343c3912062199edeea473cddb25cd16
Joachim Lingner <jl@openoffice.org>
2002-09-17 10:20:28 +0000

    #99957# convert to VS .NET

--- extensions/test/ole/AxTestComponents/StdAfx.cpp
+++ extensions/test/ole/AxTestComponents/StdAfx.cpp
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: StdAfx.cpp,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:16:55 $
+ *  last change: $Author: jl $ $Date: 2002-09-17 11:20:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,7 @@
 
 #ifdef _ATL_STATIC_REGISTRY
 #include <statreg.h>
-#include <statreg.cpp>
+//#include <statreg.cpp>
 #endif
 
-#include <atlimpl.cpp>
+//#include <atlimpl.cpp>
