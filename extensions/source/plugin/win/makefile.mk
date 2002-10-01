9db64696eec32045f21ea8e745549b5f54066e19
Jörg Budischewski <jbu@openoffice.org>
2002-10-01 07:10:47 +0000

    #103263# removed obsolete applicat.rdb

--- extensions/source/plugin/win/makefile.mk
+++ extensions/source/plugin/win/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 16:16:52 $
+#   last change: $Author: jbu $ $Date: 2002-10-01 08:10:47 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -72,9 +72,6 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :  ..$/util$/makefile.pmk
 
 # --- Types -------------------------------------
-
-UNOUCRDEP=$(SOLARBINDIR)$/applicat.rdb
-UNOUCRRDB=$(SOLARBINDIR)$/applicat.rdb
 
 UNOUCROUT=$(OUT)$/inc$/$(PRJNAME)
 INCPRE+=$(UNOUCROUT)
