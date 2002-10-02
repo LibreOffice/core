9a44c2a1a6e2f81a85551ea6fb92e879ad17c8cf
Christian Lippka <cl@openoffice.org>
2002-10-02 15:19:56 +0000

    #103668# don't support placeware for draw

--- sd/source/ui/inc/filedlg.hxx
+++ sd/source/ui/inc/filedlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: filedlg.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: thb $ $Date: 2001-09-04 16:36:05 $
+ *  last change: $Author: cl $ $Date: 2002-10-02 16:19:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,7 +89,7 @@ private:
     const std::auto_ptr< SdFileDialog_Imp > mpImpl;
 
 public:
-                            SdExportFileDialog( BOOL haveCheckbox );
+                            SdExportFileDialog( BOOL haveCheckbox, bool ispresentation = TRUE );
                             ~SdExportFileDialog();
 
     ErrCode                 Execute();
