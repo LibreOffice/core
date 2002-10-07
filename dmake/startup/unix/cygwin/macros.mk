07a2c0410798623fa689e98587785840c2c7972a
Ken Foskey <waratah@openoffice.org>
2002-10-07 12:40:32 +0000

    dmake: Add the cygwin configure files

---
+++ dmake/startup/unix/cygwin/macros.mk
@@ -0,0 +1,10 @@
+# Define additional Cygwin specific macros.
+#
+
+# Process environment-specific refinements, if any.
+.IF $(OSENVIRONMENT)
+   .INCLUDE .NOINFER .IGNORE : $(INCFILENAME:d)$(OSENVIRONMENT)$/macros.mk
+.ENDIF
+
+# Make OS-release-specific settings
+#PRINT *:= lpr
