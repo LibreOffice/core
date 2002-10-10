eeebd32631553b90bdf4930ed07f893267294209
Oliver Bolte <obo@openoffice.org>
2002-10-10 12:32:48 +0000

    #100000# convert MAc format to UNIX format (SOLARIS)

--- filter/source/xmlfilteradaptor/XmlFilterAdaptor.hxx
+++ filter/source/xmlfilteradaptor/XmlFilterAdaptor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XmlFilterAdaptor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aidan $ $Date: 2002-10-02 11:46:43 $
+ *  last change: $Author: obo $ $Date: 2002-10-10 13:32:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,100 +58,178 @@
  *
  *
  ************************************************************************/
+
 #ifndef _XMLFILTERADAPTOR_HXX
 #define _XMLFILTERADAPTOR_HXX
+
 
 #ifndef _COM_SUN_STAR_DOCUMENT_XFILTER_HPP_
 #include <com/sun/star/document/XFilter.hpp>
 #endif
+
 #ifndef _COM_SUN_STAR_DOCUMENT_XEXPORTER_HPP_
 #include <com/sun/star/document/XExporter.hpp>
 #endif
+
 #ifndef _COM_SUN_STAR_DOCUMENT_XIMPORTER_HPP_
 #include <com/sun/star/document/XImporter.hpp>
 #endif
+
 #ifndef _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
 #include <com/sun/star/lang/XInitialization.hpp>
 #endif
+
 #ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #endif
+
 #ifndef _CPPUHELPER_IMPLBASE5_HXX_
 #include <cppuhelper/implbase5.hxx>
 #endif
 
-enum FilterType 
+
+
+enum FilterType
 {
     FILTER_IMPORT,
     FILTER_EXPORT
 };
+
 /* This component will be instantiated for both import or export. Whether it calls
  * setSourceDocument or setTargetDocument determines which Impl function the filter
  * member calls */
-class XmlFilterAdaptor : public cppu::WeakImplHelper5 
-< 
+
+class XmlFilterAdaptor : public cppu::WeakImplHelper5
+
+<
+
     com::sun::star::document::XFilter,
+
     com::sun::star::document::XExporter,
+
     com::sun::star::document::XImporter,
+
     com::sun::star::lang::XInitialization,
+
     com::sun::star::lang::XServiceInfo
+
 >
+
 {
+
 protected:
+
   ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > mxMSF;
+
   ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent > mxDoc;
+
   ::rtl::OUString msFilterName;
+
   ::com::sun::star::uno::Sequence< ::rtl::OUString > msUserData;
+
    ::rtl::OUString msTemplateName;
+
     FilterType meType;
-    sal_Bool SAL_CALL exportImpl( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor ) 
+
+    sal_Bool SAL_CALL exportImpl( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor )
+
         throw (::com::sun::star::uno::RuntimeException);
-    sal_Bool SAL_CALL importImpl( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor ) 
+
+    sal_Bool SAL_CALL importImpl( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor )
+
         throw (::com::sun::star::uno::RuntimeException);
+
+
 
 public:
+
     XmlFilterAdaptor( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > &rxMSF)
+
         : mxMSF( rxMSF ) {}
+
     virtual ~XmlFilterAdaptor() {}
 
+
+
     // XFilter
-    virtual sal_Bool SAL_CALL filter( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor ) 
+
+    virtual sal_Bool SAL_CALL filter( const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aDescriptor )
+
         throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL cancel(  ) 
+
+    virtual void SAL_CALL cancel(  )
+
         throw (::com::sun::star::uno::RuntimeException);
+
+
 
     // XExporter
-    virtual void SAL_CALL setSourceDocument( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent >& xDoc ) 
+
+    virtual void SAL_CALL setSourceDocument( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent >& xDoc )
+
         throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+
+
 
     // XImporter
-    virtual void SAL_CALL setTargetDocument( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent >& xDoc ) 
+
+    virtual void SAL_CALL setTargetDocument( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent >& xDoc )
+
         throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
 
+
+
     // XInitialization
-    virtual void SAL_CALL initialize( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments ) 
+
+    virtual void SAL_CALL initialize( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments )
+
         throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
 
+
+
     // XServiceInfo
-    virtual ::rtl::OUString SAL_CALL getImplementationName(  ) 
+
+    virtual ::rtl::OUString SAL_CALL getImplementationName(  )
+
         throw (::com::sun::star::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) 
+
+    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName )
+
         throw (::com::sun::star::uno::RuntimeException);
-    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames(  ) 
+
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames(  )
+
         throw (::com::sun::star::uno::RuntimeException);
+
 };
 
+
+
 ::rtl::OUString XmlFilterAdaptor_getImplementationName()
+
     throw ( ::com::sun::star::uno::RuntimeException );
 
-sal_Bool SAL_CALL XmlFilterAdaptor_supportsService( const ::rtl::OUString& ServiceName ) 
+
+
+sal_Bool SAL_CALL XmlFilterAdaptor_supportsService( const ::rtl::OUString& ServiceName )
+
     throw ( ::com::sun::star::uno::RuntimeException );
 
-::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL XmlFilterAdaptor_getSupportedServiceNames(  ) 
+
+
+::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL XmlFilterAdaptor_getSupportedServiceNames(  )
+
     throw ( ::com::sun::star::uno::RuntimeException );
+
+
 
 ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >
+
 SAL_CALL XmlFilterAdaptor_createInstance( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > & rSMgr)
+
     throw ( ::com::sun::star::uno::Exception );
 
+
+
 #endif
+
