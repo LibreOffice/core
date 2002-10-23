5941abc52f6a78b98ce74fa4710f1c81049e5e2d
Daniel Rentz <dr@openoffice.org>
2002-10-23 10:03:34 +0000

    #104430# paper size conversion

--- sc/source/filter/excel/excrecds.cxx
+++ sc/source/filter/excel/excrecds.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: excrecds.cxx,v $
  *
- *  $Revision: 1.47 $
+ *  $Revision: 1.48 $
  *
- *  last change: $Author: dr $ $Date: 2002-08-09 12:05:49 $
+ *  last change: $Author: dr $ $Date: 2002-10-23 11:03:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -3855,7 +3855,9 @@ ExcSetup::ExcSetup( RootData* pExcRoot ) :
 
         nGrbit = bLandscape? 0 : 0x0002;    // !fLandscape / fLandscape
 
-        // direct copy from ImportExcel::Setup()!
+#define TWIPS_FROM_INCH( v )    ((sal_Int32)((v)*72.0*TWIPS_PER_POINT+0.5))
+#define TWIPS_FROM_CM( v )      ((sal_Int32)((v)*72.0*TWIPS_PER_POINT/CM_PER_INCH+0.5))
+
         struct PAPER_SIZE
         {
             INT32                   nH;
@@ -3864,48 +3866,48 @@ ExcSetup::ExcSetup( RootData* pExcRoot ) :
         static const                nAnzSizes = 42;
         static const PAPER_SIZE     pPS[ nAnzSizes ] =
         {
-            {(INT32)(0),(INT32)(0)},                                        // undefined
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*11)},       // Letter 8 1/2 x 11 in
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*11)},       // Letter Small 8 1/2 x 11 in
-            {(INT32)(TWIPS_PER_INCH*11),(INT32)(TWIPS_PER_INCH*17)},        // Tabloid 11 x 17 in
-            {(INT32)(TWIPS_PER_INCH*17),(INT32)(TWIPS_PER_INCH*11)},        // Ledger 17 x 11 in
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*14)},       // Legal 8 1/2 x 14 in
-            {(INT32)(TWIPS_PER_INCH*5.5),(INT32)(TWIPS_PER_INCH*8.5)},      // Statement 5 1/2 x 8 1/2 in
-            {(INT32)(TWIPS_PER_INCH*7.25),(INT32)(TWIPS_PER_INCH*10.5)},    // Executive 7 1/4 x 10 1/2 in
-            {(INT32)(TWIPS_PER_CM*29.7),(INT32)(TWIPS_PER_CM*42.0)},        // A3 297 x 420 mm
-            {(INT32)(TWIPS_PER_CM*21.0),(INT32)(TWIPS_PER_CM*29.7)},        // A4 210 x 297 mm
-            {(INT32)(TWIPS_PER_CM*21.0),(INT32)(TWIPS_PER_CM*29.7)},        // A4 Small 210 x 297 mm
-            {(INT32)(TWIPS_PER_CM*14.8),(INT32)(TWIPS_PER_CM*21.0)},        // A5 148 x 210 mm
-            {(INT32)(TWIPS_PER_CM*25.0),(INT32)(TWIPS_PER_CM*35.4)},        // B4 250 x 354
-            {(INT32)(TWIPS_PER_CM*18.2),(INT32)(TWIPS_PER_CM*25.7)},        // B5 182 x 257 mm
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*13)},       // Folio 8 1/2 x 13 in
-            {(INT32)(TWIPS_PER_CM*21.5),(INT32)(TWIPS_PER_CM*27.5)},        // Quarto 215 x 275 mm
-            {(INT32)(TWIPS_PER_INCH*10),(INT32)(TWIPS_PER_INCH*14)},        // 10x14 in
-            {(INT32)(TWIPS_PER_INCH*11),(INT32)(TWIPS_PER_INCH*17)},        // 11x17 in
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*11)},       // Note 8 1/2 x 11 in
-            {(INT32)(TWIPS_PER_INCH*3.875),(INT32)(TWIPS_PER_INCH*8.875)},  // Envelope #9 3 7/8 x 8 7/8
-            {(INT32)(TWIPS_PER_INCH*4.125),(INT32)(TWIPS_PER_INCH*9.5)},    // Envelope #10 4 1/8 x 9 1/2
-            {(INT32)(TWIPS_PER_INCH*4.5),(INT32)(TWIPS_PER_INCH*10.375)},   // Envelope #11 4 1/2 x 10 3/8
-            {(INT32)(TWIPS_PER_INCH*4.03),(INT32)(TWIPS_PER_INCH*11)},      // Envelope #12 4 \276 x 11
-            {(INT32)(TWIPS_PER_INCH*14.5),(INT32)(TWIPS_PER_INCH*11.5)},    // Envelope #14 5 x 11 1/2
-            {(INT32)(0),(INT32)(0)},                                        // C size sheet
-            {(INT32)(0),(INT32)(0)},                                        // D size sheet
-            {(INT32)(0),(INT32)(0)},                                        // E size sheet
-            {(INT32)(TWIPS_PER_CM*11.0),(INT32)(TWIPS_PER_CM*22.0)},        // Envelope DL 110 x 220mm
-            {(INT32)(TWIPS_PER_CM*16.2),(INT32)(TWIPS_PER_CM*22.9)},        // Envelope C5 162 x 229 mm
-            {(INT32)(TWIPS_PER_CM*32.4),(INT32)(TWIPS_PER_CM*45.8)},        // Envelope C3  324 x 458 mm
-            {(INT32)(TWIPS_PER_CM*22.9),(INT32)(TWIPS_PER_CM*32.4)},        // Envelope C4  229 x 324 mm
-            {(INT32)(TWIPS_PER_CM*11.4),(INT32)(TWIPS_PER_CM*16.2)},        // Envelope C6  114 x 162 mm
-            {(INT32)(TWIPS_PER_CM*11.4),(INT32)(TWIPS_PER_CM*22.9)},        // Envelope C65 114 x 229 mm
-            {(INT32)(TWIPS_PER_CM*25.0),(INT32)(TWIPS_PER_CM*35.3)},        // Envelope B4  250 x 353 mm
-            {(INT32)(TWIPS_PER_CM*17.6),(INT32)(TWIPS_PER_CM*25.0)},        // Envelope B5  176 x 250 mm
-            {(INT32)(TWIPS_PER_CM*17.6),(INT32)(TWIPS_PER_CM*12.5)},        // Envelope B6  176 x 125 mm
-            {(INT32)(TWIPS_PER_CM*11.0),(INT32)(TWIPS_PER_CM*23.0)},        // Envelope 110 x 230 mm
-            {(INT32)(TWIPS_PER_INCH*3.875),(INT32)(TWIPS_PER_INCH*7.5)},    // Envelope Monarch 3.875 x 7.5 in
-            {(INT32)(TWIPS_PER_INCH*3.625),(INT32)(TWIPS_PER_INCH*6.5)},    // 6 3/4 Envelope 3 5/8 x 6 1/2 in
-            {(INT32)(TWIPS_PER_INCH*14.875),(INT32)(TWIPS_PER_INCH*11)},    // US Std Fanfold 14 7/8 x 11 in
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*12)},       // German Std Fanfold 8 1/2 x 12 in
-            {(INT32)(TWIPS_PER_INCH*8.5),(INT32)(TWIPS_PER_INCH*13)}        // German Legal Fanfold 8 1/2 x 13 in
+            { 0,                        0                           },  // undefined
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 11 )       },  // Letter 8 1/2 x 11 in
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 11 )       },  // Letter Small 8 1/2 x 11 in
+            { TWIPS_FROM_INCH( 11 ),    TWIPS_FROM_INCH( 17 )       },  // Tabloid 11 x 17 in
+            { TWIPS_FROM_INCH( 17 ),    TWIPS_FROM_INCH( 11 )       },  // Ledger 17 x 11 in
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 14 )       },  // Legal 8 1/2 x 14 in
+            { TWIPS_FROM_INCH( 5.5 ),   TWIPS_FROM_INCH( 8.5 )      },  // Statement 5 1/2 x 8 1/2 in
+            { TWIPS_FROM_INCH( 7.25 ),  TWIPS_FROM_INCH( 10.5 )     },  // Executive 7 1/4 x 10 1/2 in
+            { TWIPS_FROM_CM( 29.7 ),    TWIPS_FROM_CM( 42.0 )       },  // A3 297 x 420 mm
+            { TWIPS_FROM_CM( 21.0 ),    TWIPS_FROM_CM( 29.7 )       },  // A4 210 x 297 mm
+            { TWIPS_FROM_CM( 21.0 ),    TWIPS_FROM_CM( 29.7 )       },  // A4 Small 210 x 297 mm
+            { TWIPS_FROM_CM( 14.8 ),    TWIPS_FROM_CM( 21.0 )       },  // A5 148 x 210 mm
+            { TWIPS_FROM_CM( 25.0 ),    TWIPS_FROM_CM( 35.4 )       },  // B4 250 x 354
+            { TWIPS_FROM_CM( 18.2 ),    TWIPS_FROM_CM( 25.7 )       },  // B5 182 x 257 mm
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 13 )       },  // Folio 8 1/2 x 13 in
+            { TWIPS_FROM_CM( 21.5 ),    TWIPS_FROM_CM( 27.5 )       },  // Quarto 215 x 275 mm
+            { TWIPS_FROM_INCH( 10 ),    TWIPS_FROM_INCH( 14 )       },  // 10x14 in
+            { TWIPS_FROM_INCH( 11 ),    TWIPS_FROM_INCH( 17 )       },  // 11x17 in
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 11 )       },  // Note 8 1/2 x 11 in
+            { TWIPS_FROM_INCH( 3.875 ), TWIPS_FROM_INCH( 8.875 )    },  // Envelope #9 3 7/8 x 8 7/8
+            { TWIPS_FROM_INCH( 4.125 ), TWIPS_FROM_INCH( 9.5 )      },  // Envelope #10 4 1/8 x 9 1/2
+            { TWIPS_FROM_INCH( 4.5 ),   TWIPS_FROM_INCH( 10.375 )   },  // Envelope #11 4 1/2 x 10 3/8
+            { TWIPS_FROM_INCH( 4.03 ),  TWIPS_FROM_INCH( 11 )       },  // Envelope #12 4 \276 x 11
+            { TWIPS_FROM_INCH( 14.5 ),  TWIPS_FROM_INCH( 11.5 )     },  // Envelope #14 5 x 11 1/2
+            { 0,                        0                           },  // C size sheet
+            { 0,                        0                           },  // D size sheet
+            { 0,                        0                           },  // E size sheet
+            { TWIPS_FROM_CM( 11.0 ),    TWIPS_FROM_CM( 22.0 )       },  // Envelope DL 110 x 220mm
+            { TWIPS_FROM_CM( 16.2 ),    TWIPS_FROM_CM( 22.9 )       },  // Envelope C5 162 x 229 mm
+            { TWIPS_FROM_CM( 32.4 ),    TWIPS_FROM_CM( 45.8 )       },  // Envelope C3  324 x 458 mm
+            { TWIPS_FROM_CM( 22.9 ),    TWIPS_FROM_CM( 32.4 )       },  // Envelope C4  229 x 324 mm
+            { TWIPS_FROM_CM( 11.4 ),    TWIPS_FROM_CM( 16.2 )       },  // Envelope C6  114 x 162 mm
+            { TWIPS_FROM_CM( 11.4 ),    TWIPS_FROM_CM( 22.9 )       },  // Envelope C65 114 x 229 mm
+            { TWIPS_FROM_CM( 25.0 ),    TWIPS_FROM_CM( 35.3 )       },  // Envelope B4  250 x 353 mm
+            { TWIPS_FROM_CM( 17.6 ),    TWIPS_FROM_CM( 25.0 )       },  // Envelope B5  176 x 250 mm
+            { TWIPS_FROM_CM( 17.6 ),    TWIPS_FROM_CM( 12.5 )       },  // Envelope B6  176 x 125 mm
+            { TWIPS_FROM_CM( 11.0 ),    TWIPS_FROM_CM( 23.0 )       },  // Envelope 110 x 230 mm
+            { TWIPS_FROM_INCH( 3.875 ), TWIPS_FROM_INCH( 7.5 )      },  // Envelope Monarch 3.875 x 7.5 in
+            { TWIPS_FROM_INCH( 3.625 ), TWIPS_FROM_INCH( 6.5 )      },  // 6 3/4 Envelope 3 5/8 x 6 1/2 in
+            { TWIPS_FROM_INCH( 14.875 ),TWIPS_FROM_INCH( 11 )       },  // US Std Fanfold 14 7/8 x 11 in
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 12 )       },  // German Std Fanfold 8 1/2 x 12 in
+            { TWIPS_FROM_INCH( 8.5 ),   TWIPS_FROM_INCH( 13 )       }   // German Legal Fanfold 8 1/2 x 13 in
         };
 
         UINT16              n = 0;
