da77832c0d02d43ce97f915a793f7e80bac3118f
Duncan Foster <dfoster@openoffice.org>
2002-10-24 06:33:39 +0000

    Scripting framework refactor. Code moved from scripturi

---
+++ scripting/source/storage/ScriptURI.cxx
@@ -0,0 +1,255 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ScriptURI.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: dfoster $ $Date: 2002-10-24 07:33:39 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#include <osl/diagnose.h>
+#include <osl/file.h>
+
+#ifdef _DEBUG
+#include <stdio.h>
+#endif
+
+//Local headers
+#include "ScriptURI.hxx"
+#include <util/util.hxx>
+
+using namespace ::rtl;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+
+
+namespace scripting_impl {
+
+static const OUString schema = OUString::createFromAscii("script://");
+
+/**
+ *  Constructor
+ *
+ *  @param scriptURI the string script URI
+ */
+ScriptURI::ScriptURI( const ::rtl::OUString& scriptURI )
+    throw ( IllegalArgumentException ) : m_uri( scriptURI )
+{
+#ifdef _DEBUG
+    printf("received uri: %s\n",::rtl::OUStringToOString(m_uri,RTL_TEXTENCODING_ASCII_US).pData->buffer);
+#endif
+    set_values(parseIt());
+    if(!isValid())
+    {
+        OSL_TRACE("ScriptURI ctor: throwing IllegalArgException");
+        throw IllegalArgumentException(
+            OUSTR( "Failed to parse invalid URI: " ).concat( scriptURI ), Reference < XInterface > (), 1 );
+    }
+}
+
+/**
+ *  Destuctor
+ *
+ */
+// dtor should never throw exceptions, so ensure this by specifying it
+ScriptURI::~ScriptURI() SAL_THROW( () )
+{
+    OSL_TRACE( "< ScriptURI dtor called >\n" );
+}
+
+/**
+ *  This function is used to determine if this object represents a valid URI
+ *
+ */
+bool ScriptURI::isValid(  ) {
+    return m_valid;
+}
+
+/**
+ *  This function returns the location of the script
+ *
+ */
+::rtl::OUString ScriptURI::getLocation(  )
+{
+    return m_location;
+}
+
+/**
+ *  This function returns the language of the script, eg. java, StarBasic,...
+ *
+ */
+::rtl::OUString ScriptURI::getLanguage(  )
+{
+    return m_language;
+}
+
+/**
+ *  This function returns the language dependent function name of the script
+ *
+ */
+::rtl::OUString ScriptURI::getFunctionName(  )
+{
+    return m_functionName;
+}
+
+/**
+ *  This function returns the language independent logical name of the script
+ *
+ */
+::rtl::OUString ScriptURI::getLogicalName(  )
+{
+    return m_logicalName;
+}
+
+/**
+ *  This function returns the full URI
+ *
+ */
+::rtl::OUString ScriptURI::getURI(  )
+{
+    return m_uri;
+}
+
+//Private mutex guarded method for setting the members
+void ScriptURI::set_values(scripting_impl::Uri values)
+{
+    osl::Guard< ::osl::Mutex > aGuard( m_mutex );
+    m_valid=values.valid;
+    m_location=values.location;
+    m_language=values.language;
+    m_functionName=values.functionName;
+    m_logicalName=values.logicalName;
+
+}
+/**
+ *  This is a private method used for parsing the URI received by the
+ * initialization.
+ *
+ */
+// rather naive parsing?
+Uri ScriptURI::parseIt()
+{
+    sal_Int32 schemaLen = schema.getLength();
+    scripting_impl::Uri results;
+
+    //attempt to parse
+    // check that it starts script://
+    // better check for OBO errors here
+    if(m_uri.indexOf(schema)!=0)
+    {
+        OSL_TRACE("wrong schema");
+        results.valid=sal_False;
+        return results;
+    }
+
+    // substr from here to the '?' and set the logical name
+    sal_Int32 len=m_uri.indexOf('?');
+    if(len==-1)
+    {
+        // no queries so just set the logical name
+        results.logicalName=m_uri.copy(schemaLen);
+        results.valid=sal_True;
+        return results;
+    }
+    results.logicalName=m_uri.copy(schemaLen,len-schemaLen);
+#ifdef _DEBUG
+
+    printf("log name: %s\n",::rtl::OUStringToOString(results.logicalName,RTL_TEXTENCODING_ASCII_US).pData->buffer);
+#endif
+
+    len++;
+    // now get the attributes
+    OUString attr;
+    do
+    {
+        attr=m_uri.getToken(0,';',len);
+#ifdef _DEBUG
+
+        printf("chunk: %s\n",::rtl::OUStringToOString(attr,RTL_TEXTENCODING_ASCII_US).pData->buffer);
+#endif
+
+        if(attr.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("language"))==sal_True)
+        {
+            sal_Int32 len2=attr.indexOf('=');
+            results.language=attr.copy(len2+1);
+            continue;
+        }
+        else if (attr.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("location"))==sal_True)
+        {
+            sal_Int32 len2=attr.indexOf('=');
+            results.location=attr.copy(len2+1);
+            continue;
+        }
+        else if (attr.matchAsciiL(RTL_CONSTASCII_STRINGPARAM("function"))==sal_True)
+        {
+            sal_Int32 len2=attr.indexOf('=');
+            results.functionName=attr.copy(len2+1);
+            continue;
+        }
+        else
+        {
+#ifdef _DEBUG
+            printf("Unknown attribute?\n");
+#endif
+
+        }
+    }
+    while (len>=0)
+        ;
+    // parse is good
+    return results;
+}
+
+} // namespace script_uri
---
+++ scripting/source/storage/ScriptURI.hxx
@@ -0,0 +1,154 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ScriptURI.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: dfoster $ $Date: 2002-10-24 07:33:39 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_STORAGE_SCRIPTURI_HXX_
+#define __FRAMEWORK_STORAGE_SCRIPTURI_HXX_
+
+#include <osl/mutex.hxx>
+#include <rtl/ustring>
+
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+
+namespace scripting_impl {
+// for simplification
+#define css ::com::sun::star
+#define dcsssf ::drafts::com::sun::star::script::framework
+
+struct Uri {
+    bool valid;
+    ::rtl::OUString uri;
+    ::rtl::OUString location;
+    ::rtl::OUString language;
+    ::rtl::OUString functionName;
+    ::rtl::OUString logicalName;
+};
+/**
+ * Helper class for dealing with script URIs.
+ */
+class ScriptURI
+{
+public:
+    ScriptURI( const ::rtl::OUString& scriptURI )
+        throw (css::lang::IllegalArgumentException);
+    ~ScriptURI()  SAL_THROW ( () );
+
+    /**
+     *  This function returns the location of the script
+     *
+     */
+    virtual ::rtl::OUString  getLocation();
+
+    /**
+     *  This function returns the language of the script, eg. java,
+     *  StarBasic,...
+     *
+     */
+    virtual ::rtl::OUString  getLanguage();
+
+    /**
+     *  This function returns the language dependent function name of
+     *  the script
+     */
+    virtual ::rtl::OUString  getFunctionName();
+
+    /**
+     *  This function returns the language independent logical name of
+     *  the script
+     */
+    virtual ::rtl::OUString  getLogicalName();
+
+    /**
+     *  This function returns the full URI
+     *
+     */
+    virtual ::rtl::OUString  getURI();
+
+private:
+    ::osl::Mutex m_mutex;
+
+    /** @internal */
+    sal_Bool m_valid;
+
+    //the private strings
+    /** the string representation of the this objects URI */
+    ::rtl::OUString m_uri;
+    /** the location of the script referred to by this URI */
+    ::rtl::OUString m_location;
+    /** the language of the script referred to by this URI */
+    ::rtl::OUString m_language;
+    /** the language dependent function name of the script referred to by this URI */
+    ::rtl::OUString m_functionName;
+    /** the language independent logical name of the script referred to by this URI */
+    ::rtl::OUString m_logicalName;
+
+    //attempt to parse the URI provided
+    /** @internal */
+    Uri parseIt();
+    //set the members
+    /** @internal */
+    void set_values(Uri);
+    bool isValid();
+}
+; // class ScriptURI
+
+} //namespace script_uri
+
+#endif // define __FRAMEWORK_STORAGE_SCRIPTURI_HXX_
