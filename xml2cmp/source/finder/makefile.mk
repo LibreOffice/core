61e97b5303ecf722a73d9b9f09a857f612a7fcce
Nikolai Pretzell <np@openoffice.org>
2002-11-14 13:11:24 +0000

    #100000# Workaround to: Bug in STLPort configuration leads to inconsistent exception specifications.

--- xml2cmp/source/finder/makefile.mk
+++ xml2cmp/source/finder/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: np $ $Date: 2001-03-23 13:32:07 $
+#   last change: $Author: np $ $Date: 2002-11-14 14:11:24 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -69,6 +69,8 @@ TARGETTYPE=CUI
 
 
 # --- Settings -----------------------------------------------------
+
+ENABLE_EXCEPTIONS=TRUE
 
 .INCLUDE :  settings.mk
 
