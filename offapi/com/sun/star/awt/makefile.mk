467f50bbf87dc6f3024da9545025639c30d12fcc
Malte Timmermann <mt@openoffice.org>
2002-11-15 10:45:32 +0000

    #104991# UnoControlDialogElement.idl

--- offapi/com/sun/star/awt/makefile.mk
+++ offapi/com/sun/star/awt/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: mt $ $Date: 2002-09-11 14:44:20 $
+#   last change: $Author: mt $ $Date: 2002-11-15 11:45:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -143,6 +143,7 @@ IDLFILES=\
     UnoControlDateField.idl\
     UnoControlDateFieldModel.idl\
     UnoControlDialog.idl\
+    UnoControlDialogElement.idl\
     UnoControlDialogModel.idl\
     UnoControlEdit.idl\
     UnoControlEditModel.idl\
