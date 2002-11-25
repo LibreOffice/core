b180348a22b401e41811a7fa7b27ffa551e6dd4f
Gregor Hartmann <gh@openoffice.org>
2002-11-25 11:23:43 +0000

    #105307#

--- basic/source/app/status.cxx
+++ basic/source/app/status.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: status.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: gh $ $Date: 2002-11-12 16:53:40 $
+ *  last change: $Author: gh $ $Date: 2002-11-25 12:23:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -152,6 +152,7 @@ void StatusLine::LoadTaskToolBox()
     }
 
     pTaskToolBox->EndUpdateTask();
+    Resize();
 }
 
 
