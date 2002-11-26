57f66a76a114dedfa6de98e0ee4c8151c7d61458
Ocke Janssen <oj@openoffice.org>
2002-11-26 11:53:34 +0000

    #105538# set the type of the SvLBox to WINDOW_LISTBOX to allow accessible label

--- svtools/source/contnr/svlbox.cxx
+++ svtools/source/contnr/svlbox.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svlbox.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: pb $ $Date: 2002-07-04 11:57:21 $
+ *  last change: $Author: oj $ $Date: 2002-11-26 12:53:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -782,6 +782,7 @@ SvLBox::SvLBox( Window* pParent, const ResId& rResId ) :
     Font aFont( "Geneva", Size( 0, 10 ) );
     SetFont( aFont );
 #endif
+    SetType(WINDOW_LISTBOX);
 }
 
 
