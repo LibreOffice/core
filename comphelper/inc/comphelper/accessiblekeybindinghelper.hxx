acf99805d0efa3a430070f3d2202a8aa6d85dc3b
Thomas Benisch <tbe@openoffice.org>
2002-12-10 16:19:42 +0000

    #106154# helper for implementing an XAccessibleKeyBinding

---
+++ comphelper/inc/comphelper/accessiblekeybindinghelper.hxx
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *
+ *  $RCSfile: accessiblekeybindinghelper.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: tbe $ $Date: 2002-12-10 17:18:32 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef COMPHELPER_ACCESSIBLE_KEYBINDING_HELPER_HXX
+#define COMPHELPER_ACCESSIBLE_KEYBINDING_HELPER_HXX
+
+#ifndef _DRAFTS_COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLEKEYBINDING_HPP_
+#include <drafts/com/sun/star/accessibility/XAccessibleKeyBinding.hpp>
+#endif
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+#include <vector>
+
+
+//..............................................................................
+namespace comphelper
+{
+//..............................................................................
+
+    //==============================================================================
+    // OAccessibleKeyBindingHelper
+    //==============================================================================
+
+    typedef ::cppu::WeakImplHelper1 <   ::drafts::com::sun::star::accessibility::XAccessibleKeyBinding
+                                    >   OAccessibleKeyBindingHelper_Base;
+
+    /** a helper class for implementing an accessible keybinding
+     */
+    class OAccessibleKeyBindingHelper : public OAccessibleKeyBindingHelper_Base
+    {
+    private:
+        typedef ::std::vector< ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::awt::KeyStroke > > KeyBindings;
+
+        KeyBindings     m_aKeyBindings;
+
+    protected:
+        ::osl::Mutex    m_aMutex;
+
+        virtual ~OAccessibleKeyBindingHelper();
+
+    public:
+        OAccessibleKeyBindingHelper();
+        OAccessibleKeyBindingHelper( const OAccessibleKeyBindingHelper& rHelper );
+
+        void AddKeyBinding( const ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::awt::KeyStroke >& rKeyBinding ) throw (::com::sun::star::uno::RuntimeException);
+        void AddKeyBinding( const ::drafts::com::sun::star::awt::KeyStroke& rKeyStroke ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XAccessibleKeyBinding
+        virtual sal_Int32 SAL_CALL getAccessibleKeyBindingCount() throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::awt::KeyStroke > SAL_CALL getAccessibleKeyBinding( sal_Int32 nIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+    };
+
+//..............................................................................
+}   // namespace comphelper
+//..............................................................................
+
+#endif // COMPHELPER_ACCESSIBLE_KEYBINDING_HELPER_HXX
---
+++ comphelper/source/misc/accessiblekeybindinghelper.cxx
@@ -0,0 +1,147 @@
+/*************************************************************************
+ *
+ *  $RCSfile: accessiblekeybindinghelper.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: tbe $ $Date: 2002-12-10 17:19:42 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+// includes --------------------------------------------------------------
+
+#ifndef COMPHELPER_ACCESSIBLE_KEYBINDING_HELPER_HXX
+#include <comphelper/accessiblekeybindinghelper.hxx>
+#endif
+
+
+//..............................................................................
+namespace comphelper
+{
+//..............................................................................
+
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::lang;
+    using namespace ::drafts::com::sun::star;
+    using namespace ::drafts::com::sun::star::accessibility;
+
+    //==============================================================================
+    // OAccessibleKeyBindingHelper
+    //==============================================================================
+
+    OAccessibleKeyBindingHelper::OAccessibleKeyBindingHelper()
+    {
+    }
+
+    // -----------------------------------------------------------------------------
+
+    OAccessibleKeyBindingHelper::OAccessibleKeyBindingHelper( const OAccessibleKeyBindingHelper& rHelper )
+        :m_aKeyBindings( rHelper.m_aKeyBindings )
+    {
+    }
+
+    // -----------------------------------------------------------------------------
+
+    OAccessibleKeyBindingHelper::~OAccessibleKeyBindingHelper()
+    {
+    }
+
+    // -----------------------------------------------------------------------------
+
+    void OAccessibleKeyBindingHelper::AddKeyBinding( const Sequence< awt::KeyStroke >& rKeyBinding ) throw (RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        m_aKeyBindings.push_back( rKeyBinding );
+    }
+
+    // -----------------------------------------------------------------------------
+
+    void OAccessibleKeyBindingHelper::AddKeyBinding( const awt::KeyStroke& rKeyStroke ) throw (RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        Sequence< awt::KeyStroke > aSeq(1);
+        aSeq[0] = rKeyStroke;
+        m_aKeyBindings.push_back( aSeq );
+    }
+
+    // -----------------------------------------------------------------------------
+    // XAccessibleKeyBinding
+    // -----------------------------------------------------------------------------
+
+    sal_Int32 OAccessibleKeyBindingHelper::getAccessibleKeyBindingCount() throw (RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        return m_aKeyBindings.size();
+    }
+
+    // -----------------------------------------------------------------------------
+
+    Sequence< awt::KeyStroke > OAccessibleKeyBindingHelper::getAccessibleKeyBinding( sal_Int32 nIndex ) throw (IndexOutOfBoundsException, RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        if ( nIndex < 0 || nIndex >= (sal_Int32)m_aKeyBindings.size() )
+            throw IndexOutOfBoundsException();
+
+        return m_aKeyBindings[nIndex];
+    }
+
+    // -----------------------------------------------------------------------------
+
+//..............................................................................
+}   // namespace comphelper
+//..............................................................................
