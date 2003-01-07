9c97267ce8a97ce029a94bfc60407d9e91795f24
Juergen Schmidt <jsc@openoffice.org>
2003-01-07 07:12:27 +0000

    #99053# new

---
+++ odk/pack/gendocu/makefile.mk
@@ -0,0 +1,31 @@
+PRJ=..$/..
+PRJNAME=odk
+TARGET=gendocu
+
+#----------------------------------------------------------------
+.INCLUDE: settings.mk
+.INCLUDE: $(PRJ)$/util$/makefile.pmk
+#----------------------------------------------------------------
+
+DOCU_INDEX_FILE = $(DESTDIRGENDOCU)$/module-ix.html
+
+IDLFILES={$(subst,/,$/ $(shell $(FIND) $(IDLOUT) -type f -print))}
+
+DIRLIST = \
+    $(DESTDIRCOMMONDOCU) \
+    $(DESTDIRGENDOCU) \
+
+
+all: $(DOCU_INDEX_FILE)
+
+$(DIRLIST) :
+     -$(MKDIRHIER) 	$@
+    
+$(DOCU_INDEX_FILE) : $(IDLFILES) $(DIRLIST)
+.IF "$(BUILD_SOSL)"==""
+    +autodoc -html $(DESTDIRGENDOCU) -dvgroot ..$/..$/DevelopersGuide -name "StarOffice6.0 API" -lg \
+    idl -dvgfile idl_chapter_refs.txt -t $(DESTDIRIDL)
+.ELSE
+    +autodoc -html $(DESTDIRGENDOCU) -dvgroot ..$/..$/DevelopersGuide -name "OpenOffice.org 1.0.2 API" -lg \
+    idl -dvgfile idl_chapter_refs.txt -t $(DESTDIRIDL)
+.ENDIF
