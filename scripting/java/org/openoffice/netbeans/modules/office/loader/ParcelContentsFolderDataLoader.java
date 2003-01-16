7e83a71ea36c15f5720ee5e287d4ee6d4ac906f4
Tomas O'Connor <toconnor@openoffice.org>
2003-01-16 16:58:24 +0000

    IssueZilla 10518 - Add the New menu to the Contents directory of Parcel recipes

--- scripting/java/org/openoffice/netbeans/modules/office/loader/ParcelContentsFolderDataLoader.java
+++ scripting/java/org/openoffice/netbeans/modules/office/loader/ParcelContentsFolderDataLoader.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ParcelContentsFolderDataLoader.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: toconnor $ $Date: 2002-11-13 17:44:32 $
+ *  last change: $Author: toconnor $ $Date: 2003-01-16 17:58:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,6 +99,7 @@ public class ParcelContentsFolderDataLoader extends UniFileLoader {
     protected SystemAction[] defaultActions() {
         return new SystemAction[] {
             SystemAction.get(PasteAction.class),
+            SystemAction.get(NewTemplateAction.class),
             // null,
             // SystemAction.get(PropertiesAction.class),
         };
