ee4e413987919edbada9cf127c262890615adf0e
Martin Maher <mmaher@openoffice.org>
2003-01-23 10:16:49 +0000

    #104852# Added workbook caching a test to see in the case of NameDefinition whether the definitiono is of type expression or range.

--- xmerge/java/org/openoffice/xmerge/converter/xml/sxc/pexcel/records/formula/FormulaHelper.java
+++ xmerge/java/org/openoffice/xmerge/converter/xml/sxc/pexcel/records/formula/FormulaHelper.java
@@ -60,22 +60,41 @@ import java.io.IOException;
 import java.util.Vector;
 import java.util.Enumeration;
 
+import org.openoffice.xmerge.util.Debug;
+import org.openoffice.xmerge.converter.xml.sxc.pexcel.records.Workbook;
+
 /**
  * This Helper class provides a simplified interface to conversion between PocketXL formula representation
  * and Calc formula representation.<p>
  * The class is used by {@link org.openoffice.xmerge.converter.xml.sxc.pexcel.Records.Formula}
  */
 public class FormulaHelper {
+
     private static FormulaParser parser;
     private static FormulaCompiler compiler;
     private static TokenEncoder encoder;
     private static TokenDecoder decoder;
+    private boolean rangeType = false;
+    private boolean expressionType = false;
 
     static {
         parser   = new FormulaParser();
         compiler = new FormulaCompiler();
         encoder = new TokenEncoder();
         decoder  = new TokenDecoder();
+    }
+
+    /**
+     * Sets the workbook cache so that global data such as
+     * <code>DefinedNames</code>, <code>Boundsheets</code> can be read
+     *
+     * @param wb Wrokbook object containing all the global data
+     */
+    public void setWorkbook(Workbook wb) {
+
+        encoder.setWorkbook(wb);
+        decoder.setWorkbook(wb);
+        parser.setWorkbook(wb);
     }
 
     /**
@@ -87,6 +106,7 @@ public class FormulaHelper {
      *
      */
     public byte[] convertCalcToPXL(String formula) throws UnsupportedFunctionException, FormulaParsingException {
+
         Vector parseTokens = parser.parse(formula);
         Vector rpnTokens = compiler.infix2RPN(parseTokens);
 
@@ -94,7 +114,8 @@ public class FormulaHelper {
         try {
             bytes = new ByteArrayOutputStream();
             for (Enumeration e = rpnTokens.elements(); e.hasMoreElements();) {
-                bytes.write(encoder.getByte((Token)e.nextElement()));
+                Token t = (Token)e.nextElement();
+                bytes.write(encoder.getByte(t));
             }
         } catch (IOException e) {
         }
@@ -108,13 +129,53 @@ public class FormulaHelper {
      *
      */
     public String convertPXLToCalc(byte[] formula) {
+
         Vector parseTokens = decoder.getTokenVector(formula);
         Vector infixTokens = compiler.RPN2Infix(parseTokens);
 
         StringBuffer buff = new StringBuffer();
         for (Enumeration e = infixTokens.elements();e.hasMoreElements();) {
-            buff.append(((Token)e.nextElement()).toString());
+            Token t = (Token)e.nextElement();
+            buff.append(t.toString());
+            // If we are parsing a Name definition we need to know if it is of
+            // type range or expression
+            if(!t.isOperand()) {
+                expressionType = true;
+            }
+        }
+        if(!expressionType) {
+            rangeType = true;
         }
         return "=" + buff.toString();
+    }
+
+    /**
+     * Returns a boolean indicating whether or not the byte[] parsed is of
+     * type range. This means it contains only a cell reference and no
+     * operators. This is necessry because the syntax for range and expression
+     * types differs. This is only of interest when dealing with
+     * <code>DefinedNames</code> and not <code>Formula</code>
+     *
+     * @return a boolean true if of type range otherwise false
+     *
+     */
+     public boolean isRangeType() {
+
+        return rangeType;
+    }
+
+    /**
+     * Returns a boolean indicating whether or not the byte[] parsed is of
+     * type expression. This means it contains operators. This is necessry
+     * because the syntax for range and expression types differs. This is
+     * only of interest when dealing with <code>DefinedNames</code> and not
+     * <code>Formula</code>
+     *
+     * @return a boolean true if of type expression otherwise false
+     *
+     */
+     public boolean isExpressionType() {
+
+        return expressionType;
     }
 }
