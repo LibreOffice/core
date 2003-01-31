4af95888deb97a88ab3d56afb52e3854b5d2563e
Stephan Wunderlich <sw@openoffice.org>
2003-01-31 08:51:42 +0000

    CHG: using AnyConverter instead of simple cast

--- qadevOOo/tests/java/mod/_cached/CachedContentResultSetFactory.java
+++ qadevOOo/tests/java/mod/_cached/CachedContentResultSetFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CachedContentResultSetFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:14:22 $
+ *  last change:$Date: 2003-01-31 09:51:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -77,6 +77,8 @@ import com.sun.star.ucb.XContentProvider;
 import com.sun.star.ucb.XDynamicResultSet;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
+import com.sun.star.uno.Type;
+import com.sun.star.uno.AnyConverter;
 import java.io.PrintWriter;
 import lib.StatusException;
 import lib.TestCase;
@@ -189,8 +191,14 @@ public class CachedContentResultSetFactory extends TestCase {
                 (OpenMode.ALL, 10000, null, new Property[] {prop},
                  new NumberedSortingInfo[0])) ;
 
-            XDynamicResultSet dynResSet = (XDynamicResultSet)
-                cmdProc.execute(cmd, 0, null) ;
+            XDynamicResultSet dynResSet = null;
+            try {
+                dynResSet = (XDynamicResultSet)
+                    AnyConverter.toObject(new Type(XDynamicResultSet.class),
+                                        cmdProc.execute(cmd, 0, null));
+            } catch (com.sun.star.lang.IllegalArgumentException iae) {
+                throw new StatusException("Couldn't convert Any ",iae);
+            }
 
             XResultSet resSet = dynResSet.getStaticResultSet() ;
 
