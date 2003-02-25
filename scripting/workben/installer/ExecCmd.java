51ee7d2de5353ae0f43226400a884b00241887e3
Noel Power <npower@openoffice.org>
2003-02-25 15:15:43 +0000

    ScriptBinding.xba, enabled dynamic config changes ExecCmd, put close of streams in final block

--- scripting/workben/installer/ExecCmd.java
+++ scripting/workben/installer/ExecCmd.java
@@ -16,9 +16,10 @@ public class ExecCmd
            new Thread(new Runnable() {
                public void run()
                {
+                   BufferedReader br_in = null;
                    try
                    {
-                       BufferedReader br_in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                       br_in = new BufferedReader(new InputStreamReader(p.getInputStream()));
                        String buff = null;
                        while ((buff = br_in.readLine()) != null)
                        {
@@ -29,7 +30,6 @@ public class ExecCmd
                            }
                            catch(Exception e) {}*/
                        }
-                       br_in.close();
                        System.out.println("finished reading out");
                     }
                     catch (IOException ioe)
@@ -37,22 +37,44 @@ public class ExecCmd
                         System.out.println("Exception caught printing javac result");
                         ioe.printStackTrace();
                     }
+                    finally
+                    {
+                       if ( br_in != null )
+                       {
+                           try
+                           {
+                               br_in.close();
+                           }
+                           catch( Exception e ) {} // nothing can be done
+                       }
+                    }
                } } ).start();
 
             new Thread(new Runnable() {
                 public void run() {
+                BufferedReader br_err = null;
                 try {
-                    BufferedReader br_err = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                    br_err = new BufferedReader(new InputStreamReader(p.getErrorStream()));
                     String buff = null;
                     while ((buff = br_err.readLine()) != null) {
                     System.out.println("Process err :" + buff);
                     /*try {Thread.sleep(100); } catch(Exception e) {}*/
                  }
-                 br_err.close();
                  System.out.println("finished reading err");
                  } catch (IOException ioe) {
                     System.out.println("Exception caught printing javac result");
                     ioe.printStackTrace();
+                 }
+                 finally
+                 {
+                    if ( br_err != null )
+                    {
+                        try
+                        {
+                            br_err.close();
+                        }
+                        catch( Exception e ) {} // nothing can be done
+                    }
                  }
             } }).start();
             int exitcode = p.waitFor();
