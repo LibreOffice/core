6e704847b83beda434054c97842ec9ae74a0b6a3
Stephan Wunderlich <sw@openoffice.org>
2003-02-28 15:11:26 +0000

    NEW: initial version

---
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleStatusBarItem.java
@@ -0,0 +1,217 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessibleStatusBarItem.java,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change:$Date: 2003-02-28 16:11:26 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._toolkit;
+
+import java.io.PrintWriter;
+
+import com.sun.star.awt.XWindow;
+import com.sun.star.frame.XController;
+import com.sun.star.frame.XDesktop;
+import com.sun.star.frame.XModel;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import drafts.com.sun.star.accessibility.AccessibleRole;
+import drafts.com.sun.star.accessibility.XAccessible;
+import drafts.com.sun.star.accessibility.XAccessibleComponent;
+import drafts.com.sun.star.accessibility.XAccessibleContext;
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.AccessibilityTools;
+import util.DesktopTools;
+import util.SOfficeFactory;
+
+/**
+ * Test for object that implements the following interfaces :
+ * <ul>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster
+ *  </code></li>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleExtendedComponent
+ *  </code></li>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleValue</code></li>
+ *  <li><code>
+ *  drafts::com::sun::star::accessibility::XAccessibleAction</code></li>
+ * </ul> <p>
+ *
+ * @see drafts.com.sun.star.accessibility.XAccessibleEventBroadcaster
+ * @see drafts.com.sun.star.accessibility.XAccessibleContext
+ * @see drafts.com.sun.star.accessibility.XAccessibleComponent
+ * @see drafts.com.sun.star.accessibility.XAccessibleExtendedComponent
+ * @see drafts.com.sun.star.accessibility.XAccessibleValue
+ * @see drafts.com.sun.star.accessibility.XAccessibleAction
+ * @see ifc.accessibility._XAccessibleEventBroadcaster
+ * @see ifc.accessibility._XAccessibleContext
+ * @see ifc.accessibility._XAccessibleComponent
+ * @see ifc.accessibility.XAccessibleExtendedComponent
+ * @see ifc.accessibility.XAccessibleValue
+ * @see ifc.accessibility.XAccessibleAction
+ */
+public class AccessibleStatusBarItem extends TestCase {
+
+    XDesktop the_Desk;
+    XTextDocument xTextDoc;
+
+    /**
+     * Creates the Desktop service (<code>com.sun.star.frame.Desktop</code>).
+     */
+    protected void initialize(TestParameters Param, PrintWriter log) {
+        the_Desk = (XDesktop) UnoRuntime.queryInterface(
+                    XDesktop.class, DesktopTools.createDesktop(Param.getMSF()));
+    }
+
+    /**
+     * Disposes the document, if exists, created in
+     * <code>createTestEnvironment</code> method.
+     */
+    protected void cleanup( TestParameters Param, PrintWriter log) {
+        log.println("disposing xTextDoc");
+
+        if (xTextDoc != null) {
+            xTextDoc.dispose();
+        }
+    }
+
+    /**
+     * Creates a text document.
+     * Then obtains an accessible object with
+     * the role <code>AccessibleRole.SCROLLBAR</code>.
+     * Object relations created :
+     * <ul>
+     *  <li> <code>'EventProducer'</code> for
+     *      {@link ifc.accessibility._XAccessibleEventBroadcaster}:
+     *      grabs focus </li>
+     * </ul>
+     *
+     * @param tParam test parameters
+     * @param log writer to log information while testing
+     *
+     * @see com.sun.star.awt.Toolkit
+     * @see drafts.com.sun.star.accessibility.AccessibleRole
+     * @see ifc.accessibility._XAccessibleEventBroadcaster
+     * @see drafts.com.sun.star.accessibility.XAccessibleEventBroadcaster
+     */
+    protected TestEnvironment createTestEnvironment(
+        TestParameters tParam, PrintWriter log) {
+
+        log.println( "creating a test environment" );
+
+        if (xTextDoc != null) xTextDoc.dispose();
+
+        // get a soffice factory object
+        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+
+        try {
+            log.println( "creating a text document" );
+            xTextDoc = SOF.createTextDoc(null);
+        } catch ( com.sun.star.uno.Exception e ) {
+            // Some exception occures.FAILED
+            e.printStackTrace( log );
+            throw new StatusException( "Couldn't create document", e );
+        }
+
+        XModel aModel = (XModel)
+                    UnoRuntime.queryInterface(XModel.class, xTextDoc);
+
+        XController xController = aModel.getCurrentController();
+
+        XInterface oObj = null;
+
+        AccessibilityTools at = new AccessibilityTools();
+
+        XWindow xWindow = at.getCurrentWindow(tParam.getMSF(), aModel);
+
+        XAccessible xRoot = at.getAccessibleObject(xWindow);
+
+        XAccessibleContext statusbar = at.getAccessibleObjectForRole(xRoot,
+            AccessibleRole.STATUSBAR);
+
+        try {
+            oObj = statusbar.getAccessibleChild(6);
+        } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
+
+        }
+
+        log.println("ImplementationName: "+ util.utils.getImplName(oObj));
+
+        TestEnvironment tEnv = new TestEnvironment(oObj);
+
+        final XTextDocument doc = xTextDoc;
+
+        tEnv.addObjRelation("EventProducer",
+            new ifc.accessibility._XAccessibleEventBroadcaster.EventProducer(){
+                public void fireEvent() {
+                    doc.getText().setString("AccessibleStatusBarItem");
+                }
+            });
+
+        return tEnv;
+    }
+}
