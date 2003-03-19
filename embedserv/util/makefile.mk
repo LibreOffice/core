9f679f482de6350a8caf4d58e001140e2e8a4fc5
Mikhail Voitenko <mav@openoffice.org>
2003-03-19 09:58:50 +0000

    #i2822# let the project be independent component

--- embedserv/util/makefile.mk
+++ embedserv/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: mav $ $Date: 2003-03-19 10:56:49 $
+#   last change: $Author: mav $ $Date: 2003-03-19 10:58:50 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -92,7 +92,6 @@ SHL1STDLIBS=\
     $(SALLIB) 	\
     $(CPPULIB)	\
     $(CPPUHELPERLIB)	\
-    $(LIBCIMT) 	\
     ole32.lib 	\
     gdi32.lib 	\
     uuid.lib 	\
