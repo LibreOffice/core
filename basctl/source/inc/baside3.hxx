4e16c0a5d4fc652feae98bf86d7f49d84fdae217
Vladimir Glazounov <vg@openoffice.org>
2003-03-26 11:50:30 +0000

    INTEGRATION: CWS tbe3 (1.1.2); FILE ADDED 2003/02/28 17:21:08 tbe 1.1.2.2: #103595# accessibility for dialog editor 2003/02/27 14:10:39 tbe 1.1.2.1: #103595# moved from basctl/source/basicide to basctl/source/inc

---
+++ basctl/source/inc/baside3.hxx
@@ -0,0 +1,141 @@
+/*************************************************************************
+ *
+ *  $RCSfile: baside3.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2003-03-26 12:50:30 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _BASIDE3_HXX
+#define _BASIDE3_HXX
+
+#ifndef _SVHEADER_HXX
+//#include <svheader.hxx>
+#endif
+
+#include <bastypes.hxx>
+#include <svtools/undo.hxx>
+
+#ifndef _COM_SUN_STAR_SCRIPT_XLIBRYARYCONTAINER_HPP_
+#include <com/sun/star/script/XLibraryContainer.hpp>
+#endif
+
+class StarBASIC;
+class SfxItemSet;
+class DlgEditor;
+class DlgEdModel;
+class DlgEdPage;
+class DlgEdView;
+class SfxUndoManager;
+
+class DialogWindow: public IDEBaseWindow
+{
+private:
+    DlgEditor*          pEditor;
+    SfxUndoManager*     pUndoMgr;
+    Link                aOldNotifyUndoActionHdl;
+
+protected:
+    virtual void        Paint( const Rectangle& );
+    virtual void        Resize();
+
+    virtual void        MouseButtonDown( const MouseEvent& rMEvt );
+    virtual void        MouseButtonUp( const MouseEvent& rMEvt );
+    virtual void        MouseMove( const MouseEvent& rMEvt );
+    virtual void        KeyInput( const KeyEvent& rKEvt );
+    virtual void        Command( const CommandEvent& rCEvt );
+    virtual void        LoseFocus();
+
+    DECL_LINK( NotifyUndoActionHdl, SfxUndoAction * );
+    virtual void        DoInit();
+    virtual void        DoScroll( ScrollBar* pCurScrollBar );
+    virtual void        DataChanged( const DataChangedEvent& rDCEvt );
+    void                InitSettings(BOOL bFont,BOOL bForeground,BOOL bBackground);
+
+public:
+                        TYPEINFO();
+                        DialogWindow( Window* pParent, StarBASIC* pBasic,
+                            SfxObjectShell* pShell, String aLibName, String aName,
+                            const com::sun::star::uno::Reference< com::sun::star::container::XNameContainer >& xDialogModel );
+                        DialogWindow( DialogWindow* pCurView );
+                        ~DialogWindow();
+
+    virtual void        ExecuteCommand( SfxRequest& rReq );
+    virtual void        GetState( SfxItemSet& );
+    DlgEditor*          GetEditor() const   { return pEditor; }
+    ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameContainer > GetDialog() const;
+    DlgEdModel*         GetModel() const;
+    DlgEdPage*          GetPage() const;
+    DlgEdView*          GetView() const;
+    BOOL                RenameDialog( const String& rNewName );
+    void                DisableBrowser();
+    void                UpdateBrowser();
+    virtual String      GetTitle();
+    virtual void        SetReadOnly( BOOL bReadOnly );
+    virtual BOOL        IsReadOnly();
+
+    virtual void        StoreData();
+    virtual BOOL        IsModified();
+    virtual BOOL        IsPasteAllowed();
+
+    virtual SfxUndoManager* GetUndoManager();
+    virtual void        PrintData( Printer* pPrinter );
+    virtual void        Deactivating();
+
+    virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::accessibility::XAccessible > CreateAccessible();
+};
+
+#endif  // _BASIDE3_HXX
