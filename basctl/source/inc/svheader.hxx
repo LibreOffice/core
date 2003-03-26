bd5a8715f1dfe9508677134baa62e6d0e4a06765
Vladimir Glazounov <vg@openoffice.org>
2003-03-26 11:51:56 +0000

    INTEGRATION: CWS tbe3 (1.1.2); FILE ADDED 2003/02/27 14:12:10 tbe 1.1.2.1: #103595# moved from basctl/source/basicide to basctl/source/inc

---
+++ basctl/source/inc/svheader.hxx
@@ -0,0 +1,123 @@
+/*************************************************************************
+ *
+ *  $RCSfile: svheader.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2003-03-26 12:51:56 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SVHEADER_HXX
+#define _SVHEADER_HXX
+
+
+// SV
+#define _COLDLG_HXX
+#define _CONFIG_HXX
+#define _CURSOR_HXX
+#define _FONTDLG_HXX
+#define _PRVWIN_HXX
+//#define _SELENG_HXX
+#define _VIRTDEV_HXX
+
+// Wenn Brkdlg ohne PCH:
+// #define _SPIN_HXX
+// #define _FIELD_HXX
+
+// SVTOOLS
+#define _CTRLBOX_HXX
+#define _EXTATTR_HXX
+#define _CTRLTOOL_HXX
+#define _CTRLBOX_HXX
+#define _STDMENU_HXX
+#define _VALUESET_HXX
+#define _RULER_HXX
+#define _SCRWIN_HXX
+
+// SFX
+#define _SFXAPPWIN_HXX
+#define _SFX_RESMGR_HXX
+#define _SFX_PRNMON_HXX
+#define _INTRO_HXX
+#define _SFXMSGDESCR_HXX
+#define _SFXMSGPOOL_HXX
+// #define _PASSWD_HXX
+#define _SFXMNUMGR_HXX
+
+// SFXDLG
+// #define _SFXTABDLG_HXX
+// #define _SFX_DINFDLG_HXX
+#define _SFX_MGETEMPL_HXX
+#define _SFX_STYLEDLG_HXX
+#define _SFX_TPLPITEM_HXX
+#define _NEWSTYLE_HXX
+#define _SFXDOCTEMPL_HXX
+#define _SFXDOCTDLG_HXX
+#define _SFX_TEMPLDLG_HXX
+#define _SFXNEW_HXX
+
+// SFXDOC
+// #define _SFXDOCINF_HXX
+//#define _SFX_DOCFILT_HXX
+#define _SFX_IPFRM_HXX
+#define _SFX_INTERNO_HXX
+
+// SVCONTNR
+#define _SVICNVW_HXX
+
+#define _SBSTDOBJ1_HXX
+#define _SBXMSTRM_HXX
+
+#endif
+
