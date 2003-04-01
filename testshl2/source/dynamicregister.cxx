17e26533455a25364f635d41e44994913c11bbff
Vladimir Glazounov <vg@openoffice.org>
2003-04-01 12:16:35 +0000

    INTEGRATION: CWS qadev3 (1.3.16); FILE MERGED 2003/03/31 09:09:42 lla 1.3.16.1: #108453# better version number handling

--- testshl2/source/dynamicregister.cxx
+++ testshl2/source/dynamicregister.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dynamicregister.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: lla $ $Date: 2003-01-21 13:19:58 $
+ *  last change: $Author: vg $ $Date: 2003-04-01 13:16:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,7 +78,10 @@ DynamicLibraryHelper::DynamicLibraryHelper(rtl::OUString const& _sDLLName, GetOp
     // create and load the module (shared library)
     rtl::OUString suFile = FileHelper::convertPath( _sDLLName );
     rtl::OString sDLLName = rtl::OUStringToOString(suFile, RTL_TEXTENCODING_ASCII_US);
-    fprintf(stderr, "Try to load '%s'.\n", sDLLName.getStr());
+    if (_aOptions.hasOpt("-verbose"))
+    {
+        fprintf(stderr, "Try to load '%s'.\n", sDLLName.getStr());
+    }
 
     if (! m_pModule->load( suFile ))
     {
