3b7c92cd9eca4a7fd70eb2ec0645e8dd89e14005
Vladimir Glazounov <vg@openoffice.org>
2003-04-01 14:25:26 +0000

    INTEGRATION: CWS sw008 (1.3.146); FILE MERGED 2003/03/07 11:13:03 oj 1.3.146.1: #97593# change BaseClass member method

--- sw/inc/unoevent.hxx
+++ sw/inc/unoevent.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoevent.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: dvo $ $Date: 2001-04-17 13:52:25 $
+ *  last change: $Author: vg $ $Date: 2003-04-01 15:25:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,12 +88,13 @@ class SwHyperlinkEventDescriptor : public SvDetachedEventDescriptor
     //XServiceInfo
     virtual rtl::OUString SAL_CALL getImplementationName(void)
         throw( ::com::sun::star::uno::RuntimeException );
-
+protected:
+    virtual ~SwHyperlinkEventDescriptor();
 public:
 
      SwHyperlinkEventDescriptor();
 
-    virtual ~SwHyperlinkEventDescriptor();
+
 
     void copyMacrosFromINetFmt(const SwFmtINetFmt& aFmt);
     void copyMacrosIntoINetFmt(SwFmtINetFmt& aFmt);
