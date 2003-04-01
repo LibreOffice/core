f9d33ab84dd547db2ba03e70533e3936aae12d25
Vladimir Glazounov <vg@openoffice.org>
2003-04-01 14:31:25 +0000

    INTEGRATION: CWS sw008 (1.12.116); FILE MERGED 2003/03/10 09:59:28 hbrinkm 1.12.116.1: #107993# ParkCursor->ParkCursorTab

--- sw/source/core/frmedt/fews.cxx
+++ sw/source/core/frmedt/fews.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fews.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: fme $ $Date: 2002-10-10 08:46:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-01 15:31:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -689,7 +689,8 @@ BOOL SwFEShell::Sort(const SwSortOptions& rOpt)
         while( !pFrm->IsCellFrm() )
             pFrm = pFrm->GetUpper();
         {
-            ParkCrsr( SwNodeIndex( *((SwCellFrm*)pFrm)->GetTabBox()->GetSttNd() ));
+            /* #107993# ParkCursor->ParkCursorTab */
+            ParkCursorInTab();
         }
 
         // Sorting am Dokument aufrufen
