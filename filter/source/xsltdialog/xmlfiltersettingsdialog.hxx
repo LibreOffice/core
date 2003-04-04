ba8bf77c47576afeab5a0be27461a8accef92afb
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-04 15:34:22 +0000

    INTEGRATION: CWS xmlfilter01 (1.1.2); FILE ADDED 2003/03/31 15:58:14 cl 1.1.2.7: #107159# fixed typedetection 2003/03/29 18:58:34 cl 1.1.2.6: #107159# ui freeze and fixes 2003/03/27 23:09:30 cl 1.1.2.5: #108138# added filter package feature and bugfixes 2003/03/24 17:31:18 cl 1.1.2.4: #108138# fixed use of FilterName and InterfaceName 2003/03/23 16:41:07 cl 1.1.2.3: #108138# reworked export testing to use optional ui dtd 2003/03/22 21:25:07 cl 1.1.2.2: #108138# initial checkin of the xslt filter dialog 2003/03/21 13:58:00 cl 1.1.2.1: #108138# initial checkin of the xslt filter dialog

---
+++ filter/source/xsltdialog/xmlfiltersettingsdialog.hxx
@@ -0,0 +1,211 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlfiltersettingsdialog.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2003-04-04 16:34:22 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _XMLFILTERSETTINGSDIALOG_HXX_
+#define _XMLFILTERSETTINGSDIALOG_HXX_
+
+#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
+#include <com/sun/star/lang/XComponent.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XHIERARCHICALNAME_HPP_
+#include <com/sun/star/container/XHierarchicalName.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMECONTAINER_HPP_
+#include <com/sun/star/container/XNameContainer.hpp>
+#endif
+
+#ifndef _SV_WRKWIN_HXX
+#include <vcl/wrkwin.hxx>
+#endif
+
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+
+#ifndef _SVTABBX_HXX
+#include <svtools/svtabbx.hxx>
+#endif
+
+#ifndef _SFXPOOLITEM_HXX
+#include <svtools/poolitem.hxx>
+#endif
+
+#include "xmlfiltercommon.hxx"
+
+// --------------------------------------------------------------------
+
+class SvxPathControl_Impl : public Control
+{
+private:
+    Control*        m_pFocusCtrl;
+
+public:
+    SvxPathControl_Impl( Window* pParent, const ResId& rId ) :
+        Control( pParent, rId ), m_pFocusCtrl( NULL ) {}
+
+    void            SetFocusControl( Control* pCtrl ) { m_pFocusCtrl = pCtrl; }
+
+    virtual long    Notify( NotifyEvent& rNEvt );
+};
+
+// --------------------------------------------------------------------
+
+class HeaderBar;
+
+class XMLFilterListBox : public SvTabListBox
+{
+private:
+    bool        mbFirstPaint;
+    HeaderBar*  mpHeaderBar;
+
+    DECL_LINK( TabBoxScrollHdl_Impl, SvTabListBox* );
+    DECL_LINK( HeaderSelect_Impl, HeaderBar* );
+    DECL_LINK( HeaderEndDrag_Impl, HeaderBar* );
+
+    String getEntryString( const filter_info_impl* pInfo ) const;
+
+public:
+    XMLFilterListBox( SvxPathControl_Impl* pParent );
+    ~XMLFilterListBox();
+
+    void Reset();
+
+    /** adds a new filter info entry to the ui filter list */
+    void addFilterEntry( const filter_info_impl* pInfo );
+
+    void changeEntry( const filter_info_impl* pInfo );
+
+    virtual void    Paint( const Rectangle& rRect );
+};
+
+// --------------------------------------------------------------------
+
+class XMLFilterTestDialog;
+
+class XMLFilterSettingsDialog : public WorkWindow
+{
+public:
+    XMLFilterSettingsDialog( Window* pParent, ResMgr& rResMgr, const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& rxMSF  );
+    virtual ~XMLFilterSettingsDialog();
+
+    DECL_LINK(ClickHdl_Impl, PushButton * );
+    DECL_LINK(SelectionChangedHdl_Impl, void * );
+    DECL_LINK(DoubleClickHdl_Impl, void * );
+
+    void Show();
+
+    void    onNew();
+    void    onEdit();
+    void    onTest();
+    void    onDelete();
+    void    onSave();
+    void    onOpen();
+    void    onClose();
+
+    void    updateStates();
+
+    virtual long    Notify( NotifyEvent& rNEvt );
+
+    bool    isClosable();
+
+    static ResMgr* mpResMgr;
+
+private:
+    void    initFilterList();
+    void    disposeFilterList();
+
+    bool    insertOrEdit( filter_info_impl* pNewInfo, const filter_info_impl* pOldInfo = NULL );
+
+    rtl::OUString createUniqueFilterName( const rtl::OUString& rUIName );
+    rtl::OUString createUniqueTypeName( const rtl::OUString& rTypeName );
+    rtl::OUString createUniqueInterfaceName( const rtl::OUString& rInterfaceName );
+
+private:
+    bool    mbIsClosable;
+
+    com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory > mxMSF;
+    com::sun::star::uno::Reference< com::sun::star::container::XNameContainer > mxFilterContainer;
+    com::sun::star::uno::Reference< com::sun::star::container::XNameContainer > mxTypeDetection;
+    com::sun::star::uno::Reference< com::sun::star::container::XNameContainer > mxExtendedTypeDetection;
+
+    std::vector< filter_info_impl* > maFilterVector;
+
+    XMLFilterListBox*   mpFilterListBox;
+    SvxPathControl_Impl maCtrlFilterList;
+    PushButton  maPBNew;
+    PushButton  maPBEdit;
+    PushButton  maPBTest;
+    PushButton  maPBDelete;
+    PushButton  maPBSave;
+    PushButton  maPBOpen;
+    HelpButton  maPBHelp;
+    PushButton  maPBClose;
+
+    ::rtl::OUString sTemplatePath;
+    ::rtl::OUString sDocTypePrefix;
+};
+
+#endif
