8bc4e4fe1d713eac5d6e9108e0e186510d6c5087
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-04 15:34:57 +0000

    INTEGRATION: CWS xmlfilter01 (1.1.2); FILE ADDED 2003/03/27 23:09:30 cl 1.1.2.3: #108138# added filter package feature and bugfixes 2003/03/24 17:31:19 cl 1.1.2.2: #108138# fixed use of FilterName and InterfaceName 2003/03/21 13:58:03 cl 1.1.2.1: #108138# initial checkin of the xslt filter dialog

---
+++ filter/source/xsltdialog/xmlfiltertabdialog.hxx
@@ -0,0 +1,114 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlfiltertabdialog.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2003-04-04 16:34:57 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _XMLFILTERTABDIALOG_HXX_
+#define _XMLFILTERTABDIALOG_HXX_
+
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+#ifndef _SFXTABDLG_HXX
+#include <sfx2/tabdlg.hxx>
+#endif
+
+class Window;
+class ResMgr;
+
+class filter_info_impl;
+class XMLFilterTabPageBasic;
+class XMLFilterTabPageXSLT;
+
+class XMLFilterTabDialog: public TabDialog
+{
+public:
+    XMLFilterTabDialog( Window *pParent, ResMgr& rResMgr, const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& rxMSF, const filter_info_impl* pInfo );
+    virtual ~XMLFilterTabDialog();
+
+    ResMgr& getResMgr() { return mrResMgr; }
+
+    bool onOk();
+
+    filter_info_impl* getNewFilterInfo() const;
+
+private:
+    com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory > mxMSF;
+
+    DECL_LINK( ActivatePageHdl, TabControl * );
+    DECL_LINK( DeactivatePageHdl, TabControl * );
+    DECL_LINK( OkHdl, Button * );
+    DECL_LINK( CancelHdl, Button * );
+
+    ResMgr& mrResMgr;
+
+    const filter_info_impl* mpOldInfo;
+    filter_info_impl* mpNewInfo;
+
+    TabControl      maTabCtrl;
+    OKButton        maOKBtn;
+    CancelButton    maCancelBtn;
+    HelpButton      maHelpBtn;
+
+    XMLFilterTabPageBasic*  mpBasicPage;
+    XMLFilterTabPageXSLT* mpXSLTPage;
+};
+
+
+#endif
