c3f9226543ca3bb2183403059e75be4fbfa1c514
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-04 15:00:51 +0000

    INTEGRATION: CWS xmlfilter01 (1.2.76); FILE MERGED 2003/03/06 13:57:05 as 1.2.76.1: #107711# full read/write access for types, filters, detect service

--- framework/inc/filterflags.h
+++ framework/inc/filterflags.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: filterflags.h,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: as $ $Date: 2001-03-29 13:17:08 $
+ *  last change: $Author: hr $ $Date: 2003-04-04 16:00:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,26 +70,30 @@ namespace framework{
                     in flag combined values!
 *//*-*************************************************************************************************************/
 
-#define FILTERFLAGNAME_IMPORT                               DECLARE_ASCII("Import"          )
-#define FILTERFLAGNAME_EXPORT                               DECLARE_ASCII("Export"          )
-#define FILTERFLAGNAME_INTERNAL                             DECLARE_ASCII("Internal"        )
-#define FILTERFLAGNAME_TEMPLATE                             DECLARE_ASCII("Template"        )
-#define FILTERFLAGNAME_TEMPLATEPATH                         DECLARE_ASCII("TemplatePath"    )
-#define FILTERFLAGNAME_OWN                                  DECLARE_ASCII("Own"             )
-#define FILTERFLAGNAME_ALIEN                                DECLARE_ASCII("Alien"           )
-#define FILTERFLAGNAME_ASYNCHRON                            DECLARE_ASCII("Asynchron"       )
-#define FILTERFLAGNAME_READONLY                             DECLARE_ASCII("Readonly"        )
-#define FILTERFLAGNAME_USESOPTIONS                          DECLARE_ASCII("UsesOptions"     )
-#define FILTERFLAGNAME_DEFAULT                              DECLARE_ASCII("Default"         )
-#define FILTERFLAGNAME_NOTINSTALLED                         DECLARE_ASCII("NotInstalled"    )
-#define FILTERFLAGNAME_CONSULTSERVICE                       DECLARE_ASCII("ConsultService"  )
-#define FILTERFLAGNAME_3RDPARTYFILTER                       DECLARE_ASCII("3rdPartyFilter"  )
-#define FILTERFLAGNAME_NOTINCHOOSER                         DECLARE_ASCII("NotInChooser"    )
-#define FILTERFLAGNAME_NOTINFILEDIALOG                      DECLARE_ASCII("NotInFileDialog" )
-#define FILTERFLAGNAME_PACKED                               DECLARE_ASCII("Packed"          )
-#define FILTERFLAGNAME_SILENTEXPORT                         DECLARE_ASCII("SilentExport"    )
-#define FILTERFLAGNAME_BROWSERPREFERED                      DECLARE_ASCII("BrowserPrefered" )
-#define FILTERFLAGNAME_PREFERED                             DECLARE_ASCII("Prefered"        )
+#define FILTERFLAGNAME_IMPORT                               DECLARE_ASCII("Import"           )  // x
+#define FILTERFLAGNAME_EXPORT                               DECLARE_ASCII("Export"           )  // x
+#define FILTERFLAGNAME_TEMPLATE                             DECLARE_ASCII("Template"         )  // x
+#define FILTERFLAGNAME_INTERNAL                             DECLARE_ASCII("Internal"         )  // x
+#define FILTERFLAGNAME_TEMPLATEPATH                         DECLARE_ASCII("TemplatePath"     )  // x
+#define FILTERFLAGNAME_OWN                                  DECLARE_ASCII("Own"              )  // x
+#define FILTERFLAGNAME_ALIEN                                DECLARE_ASCII("Alien"            )  // x
+#define FILTERFLAGNAME_USESOPTIONS                          DECLARE_ASCII("UsesOptions"      )  // x
+#define FILTERFLAGNAME_DEFAULT                              DECLARE_ASCII("Default"          )  // x
+#define FILTERFLAGNAME_EXECUTABLE                           DECLARE_ASCII("Executable"       )  // deprecated
+#define FILTERFLAGNAME_SUPPORTSSELECTION                    DECLARE_ASCII("SupportsSelection")  // x
+#define FILTERFLAGNAME_MAPTOAPPPLUG                         DECLARE_ASCII("MapToAppPlug"     )  // deprecated
+#define FILTERFLAGNAME_NOTINFILEDIALOG                      DECLARE_ASCII("NotInFileDialog"  )  // x
+#define FILTERFLAGNAME_NOTINCHOOSER                         DECLARE_ASCII("NotInChooser"     )  // x
+#define FILTERFLAGNAME_ASYNCHRON                            DECLARE_ASCII("Asynchron"        )  // x
+#define FILTERFLAGNAME_CREATOR                              DECLARE_ASCII("Creator"          )  // deprecated
+#define FILTERFLAGNAME_READONLY                             DECLARE_ASCII("Readonly"         )  // x
+#define FILTERFLAGNAME_NOTINSTALLED                         DECLARE_ASCII("NotInstalled"     )  // deprecated
+#define FILTERFLAGNAME_CONSULTSERVICE                       DECLARE_ASCII("ConsultService"   )  // deprecated
+#define FILTERFLAGNAME_3RDPARTYFILTER                       DECLARE_ASCII("3rdPartyFilter"   )  // x
+#define FILTERFLAGNAME_PACKED                               DECLARE_ASCII("Packed"           )  // x
+#define FILTERFLAGNAME_SILENTEXPORT                         DECLARE_ASCII("SilentExport"     )  // x
+#define FILTERFLAGNAME_BROWSERPREFERED                      DECLARE_ASCII("BrowserPrefered"  )  // deprecated
+#define FILTERFLAGNAME_PREFERED                             DECLARE_ASCII("Prefered"         )  // x
 
 #define FILTERFLAG_IMPORT                                   0x00000001L     // 1
 #define FILTERFLAG_EXPORT                                   0x00000002L     // 2
@@ -100,9 +104,13 @@ namespace framework{
 #define FILTERFLAG_ALIEN                                    0x00000040L     // 64
 #define FILTERFLAG_USESOPTIONS                              0x00000080L     // 128
 #define FILTERFLAG_DEFAULT                                  0x00000100L     // 256
+#define FILTERFLAG_EXECUTABLE                               0x00000200L     // 512
+#define FILTERFLAG_SUPPORTSSELECTION                        0x00000400L     // 1024
+#define FILTERFLAG_MAPTOAPPPLUG                             0x00000800L     // 2048
 #define FILTERFLAG_NOTINFILEDIALOG                          0x00001000L     // 4096
 #define FILTERFLAG_NOTINCHOOSER                             0x00002000L     // 8192
 #define FILTERFLAG_ASYNCHRON                                0x00004000L     // 16384
+#define FILTERFLAG_CREATOR                                  0x00008000L     // 32768
 #define FILTERFLAG_READONLY                                 0x00010000L     // 65536
 #define FILTERFLAG_NOTINSTALLED                             0x00020000L     // 131072
 #define FILTERFLAG_CONSULTSERVICE                           0x00040000L     // 262144
@@ -110,29 +118,89 @@ namespace framework{
 #define FILTERFLAG_PACKED                                   0x00100000L     // 1048576
 #define FILTERFLAG_SILENTEXPORT                             0x00200000L     // 2097152
 #define FILTERFLAG_BROWSERPREFERED                          0x00400000L     // 4194304
+//FREE! ... 0x00800000L
 #define FILTERFLAG_PREFERED                                 0x10000000L     // 268435456
 
-#define DISABLE_FILTERFLAG_IMPORT                           0xFFFFFFFEL
-#define DISABLE_FILTERFLAG_EXPORT                           0xFFFFFFFDL
-#define DISABLE_FILTERFLAG_TEMPLATE                         0xFFFFFFFBL
-#define DISABLE_FILTERFLAG_INTERNAL                         0xFFFFFFF7L
-#define DISABLE_FILTERFLAG_TEMPLATEPATH                     0xFFFFFFEFL
-#define DISABLE_FILTERFLAG_OWN                              0xFFFFFFDFL
-#define DISABLE_FILTERFLAG_ALIEN                            0xFFFFFFBFL
-#define DISABLE_FILTERFLAG_USESOPTIONS                      0xFFFFFF7FL
-#define DISABLE_FILTERFLAG_DEFAULT                          0xFFFFFEFFL
-#define DISABLE_FILTERFLAG_NOTINFILEDIALOG                  0xFFFFEFFFL
-#define DISABLE_FILTERFLAG_NOTINCHOOSER                     0xFFFFDFFFL
-#define DISABLE_FILTERFLAG_ASYNCHRON                        0xFFFFBFFFL
-#define DISABLE_FILTERFLAG_READONLY                         0xFFFEFFFFL
-#define DISABLE_FILTERFLAG_NOTINSTALLED                     0xFFFDFFFFL
-#define DISABLE_FILTERFLAG_CONSULTSERVICE                   0xFFFBFFFFL
-#define DISABLE_FILTERFLAG_3RDPARTYFILTER                   0xFFF7FFFFL
-#define DISABLE_FILTERFLAG_PACKED                           0xFFEFFFFFL
-#define DISABLE_FILTERFLAG_SILENTEXPORT                     0xFFDFFFFFL
-#define DISABLE_FILTERFLAG_BROWSERPREFERED                  0xFFBFFFFFL
-#define DISABLE_FILTERFLAG_PREFERED                         0xEFFFFFFFL
+class FlagCheck
+{
+    public:
 
-}       //  namespace framework
+    //___________________________________________
 
-#endif  //  #ifndef __FRAMEWORK_FILTERFLAGS_H_
+    /** @short  checks if the given flag mask is set.
+
+        @param  nFlags  the flag field, which should be checked.
+        @param  nMask   this mask field is searched inside parameter nFlags.
+
+        @return TRUE if mask match to the given flag field.
+     */
+    static sal_Bool isMaskSet( sal_Int32 nFlags, sal_Int32 nMask )
+    {
+        return((nFlags & nMask) == nMask);
+    }
+
+    //___________________________________________
+
+    /** @short  checks if the given flag field contains unknown flags.
+
+        @descr  Of course it can work only, if not the whole range of an int32
+                is used!
+
+        @param  nFlags  the flag field, which should be checked.
+
+        @return TRUE if only well known flags are set.
+     */
+    static sal_Bool isValid( sal_Int32 nFlags )
+    {
+        sal_Int32 nCheck = nFlags;
+        nCheck &= ~FILTERFLAG_IMPORT;
+        nCheck &= ~FILTERFLAG_EXPORT;
+        nCheck &= ~FILTERFLAG_TEMPLATE;
+        nCheck &= ~FILTERFLAG_INTERNAL;
+        nCheck &= ~FILTERFLAG_TEMPLATEPATH;
+        nCheck &= ~FILTERFLAG_OWN;
+        nCheck &= ~FILTERFLAG_ALIEN;
+        nCheck &= ~FILTERFLAG_USESOPTIONS;
+        nCheck &= ~FILTERFLAG_DEFAULT;
+        nCheck &= ~FILTERFLAG_EXECUTABLE;
+        nCheck &= ~FILTERFLAG_SUPPORTSSELECTION;
+        nCheck &= ~FILTERFLAG_MAPTOAPPPLUG;
+        nCheck &= ~FILTERFLAG_NOTINFILEDIALOG;
+        nCheck &= ~FILTERFLAG_NOTINCHOOSER;
+        nCheck &= ~FILTERFLAG_ASYNCHRON;
+        nCheck &= ~FILTERFLAG_CREATOR;
+        nCheck &= ~FILTERFLAG_READONLY;
+        nCheck &= ~FILTERFLAG_NOTINSTALLED;
+        nCheck &= ~FILTERFLAG_CONSULTSERVICE;
+        nCheck &= ~FILTERFLAG_3RDPARTYFILTER;
+        nCheck &= ~FILTERFLAG_PACKED;
+        nCheck &= ~FILTERFLAG_SILENTEXPORT;
+        nCheck &= ~FILTERFLAG_BROWSERPREFERED;
+        nCheck &= ~FILTERFLAG_PREFERED;
+        return(nCheck == 0);
+    }
+
+    //___________________________________________
+
+    /** @short  checks if the given flag field uses deprecated flag values.
+
+        @param  nFlags  the flag field, which should be checked.
+
+        @return TRUE if one deprecated flag item could be found.
+     */
+    static sal_Bool useDeprecated( sal_Int32 nFlags )
+    {
+        return(
+                isMaskSet(nFlags, FILTERFLAG_EXECUTABLE       ) ||
+                isMaskSet(nFlags, FILTERFLAG_MAPTOAPPPLUG     ) ||
+                isMaskSet(nFlags, FILTERFLAG_CREATOR          ) ||
+                isMaskSet(nFlags, FILTERFLAG_NOTINSTALLED     ) ||
+                isMaskSet(nFlags, FILTERFLAG_CONSULTSERVICE   ) ||
+                isMaskSet(nFlags, FILTERFLAG_BROWSERPREFERED  )
+              );
+    }
+};
+
+} // namespace framework
+
+#endif // #ifndef __FRAMEWORK_FILTERFLAGS_H_
