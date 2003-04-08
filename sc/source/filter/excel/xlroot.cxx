eacf52b7a2f34d1497525dd443ebf7737b1f51e3
Rüdiger Timm <rt@openoffice.org>
2003-04-08 15:25:45 +0000

    INTEGRATION: CWS calc06 (1.1.4.2.10); FILE MERGED 2003/03/31 15:46:42 sab 1.1.4.2.10.2: RESYNC: (1.1.4.2-1.1.4.3); FILE MERGED 2003/03/27 16:07:26 dr 1.1.4.2.10.1: #107688# import/export of cell styles

--- sc/source/filter/excel/xlroot.cxx
+++ sc/source/filter/excel/xlroot.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xlroot.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-26 18:04:38 $
+ *  last change: $Author: rt $ $Date: 2003-04-08 16:25:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -197,6 +197,11 @@ SfxPrinter* XclRoot::GetPrinter() const
 SvNumberFormatter& XclRoot::GetFormatter() const
 {
     return *GetDoc().GetFormatTable();
+}
+
+ScStyleSheetPool& XclRoot::GetStyleSheetPool() const
+{
+    return *GetDoc().GetStyleSheetPool();
 }
 
 ScRangeName& XclRoot::GetNamedRanges() const
