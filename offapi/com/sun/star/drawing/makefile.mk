777a55e6b340d1526dfc49b49a42ba46442e1c05
Rüdiger Timm <rt@openoffice.org>
2003-04-08 14:39:22 +0000

    INTEGRATION: CWS draw8 (1.15.86); FILE MERGED 2003/03/20 08:58:26 af 1.15.86.1: #108294# Added new service AccessibleGraphControl.

--- offapi/com/sun/star/drawing/makefile.mk
+++ offapi/com/sun/star/drawing/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-26 13:53:46 $
+#   last change: $Author: rt $ $Date: 2003-04-08 15:39:22 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ PACKAGE=com$/sun$/star$/drawing
 # ------------------------------------------------------------------------
 
 IDLFILES=\
+    AccessibleGraphControl.idl\
     Alignment.idl\
     AppletShape.idl\
     Arrangement.idl\
