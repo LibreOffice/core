78e4fc7cc1594dd544f4500c2c57b0d8c2c9da69
Rüdiger Timm <rt@openoffice.org>
2003-04-08 14:30:28 +0000

    INTEGRATION: CWS draw8 (1.3.108); FILE MERGED 2003/03/24 14:41:59 thb 1.3.108.1: #107878# Corrected to array delete

--- xmloff/source/text/XMLTextListAutoStylePool.cxx
+++ xmloff/source/text/XMLTextListAutoStylePool.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLTextListAutoStylePool.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: cl $ $Date: 2000-12-01 19:05:32 $
+ *  last change: $Author: rt $ $Date: 2003-04-08 15:30:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -353,7 +353,7 @@ void XMLTextListAutoStylePool::exportXML() const
         aNumRuleExp.exportNumberingRule( pEntry->GetName(),
                                          pEntry->GetNumRules() );
     }
-    delete aExpEntries;
+    delete [] aExpEntries;
 }
 
 
