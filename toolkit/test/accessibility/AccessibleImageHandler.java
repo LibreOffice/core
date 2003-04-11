51851125b8d797bf65850b7303b9006f054fdf60
Vladimir Glazounov <vg@openoffice.org>
2003-04-11 16:09:15 +0000

    INTEGRATION: CWS vcl07 (1.4.2); FILE ADDED 2003/04/08 14:28:33 obr 1.4.2.1: re-added accessibility workbench

---
+++ toolkit/test/accessibility/AccessibleImageHandler.java
@@ -0,0 +1,51 @@
+
+import com.sun.star.uno.UnoRuntime;
+import drafts.com.sun.star.accessibility.XAccessibleContext;
+import drafts.com.sun.star.accessibility.XAccessibleImage;
+
+
+class AccessibleImageHandler extends NodeHandler
+{
+    public NodeHandler createHandler (XAccessibleContext xContext)
+    {
+        XAccessibleImage xImage =
+            (XAccessibleImage) UnoRuntime.queryInterface (
+                XAccessibleImage.class, xContext);
+        if (xImage != null)
+            return new AccessibleImageHandler (xImage);
+        else
+            return null;
+    }
+
+    public AccessibleImageHandler ()
+    {
+    }
+
+    public AccessibleImageHandler (XAccessibleImage xImage)
+    {
+        if (xImage != null)
+            maChildList.setSize (1);
+    }
+
+    protected static XAccessibleImage getImage (AccTreeNode aNode)
+    {
+        return (XAccessibleImage) UnoRuntime.queryInterface (
+            XAccessibleImage.class, aNode.getContext());
+    }
+
+    public AccessibleTreeNode createChild (AccessibleTreeNode aParent, int nIndex)
+    {
+        if (aParent instanceof AccTreeNode)
+        {
+            XAccessibleImage xImage = getImage ((AccTreeNode)aParent);
+            if (xImage != null)
+                return new StringNode (
+                    "Image: " +
+                    xImage.getAccessibleImageDescription() + " (" +
+                    xImage.getAccessibleImageWidth() + "x" +
+                    xImage.getAccessibleImageHeight() + ")",
+                    aParent);
+        }
+        return null;
+    }
+}
