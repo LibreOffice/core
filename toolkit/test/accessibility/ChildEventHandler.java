0d268335c9af56b097c6979ceb34971b760c2226
Vladimir Glazounov <vg@openoffice.org>
2003-04-11 16:11:01 +0000

    INTEGRATION: CWS vcl07 (1.2.2); FILE ADDED 2003/04/08 14:28:40 obr 1.2.2.1: re-added accessibility workbench

---
+++ toolkit/test/accessibility/ChildEventHandler.java
@@ -0,0 +1,46 @@
+import drafts.com.sun.star.accessibility.XAccessible;
+import drafts.com.sun.star.accessibility.AccessibleEventObject;
+import com.sun.star.uno.UnoRuntime;
+
+import java.io.PrintStream;
+
+class ChildEventHandler
+    extends EventHandler
+{
+    public ChildEventHandler (AccessibleEventObject aEvent, AccessibilityTreeModel aTreeModel)
+    {
+        super (aEvent, aTreeModel);
+        mxOldChild = (XAccessible)UnoRuntime.queryInterface(
+            XAccessible.class, aEvent.OldValue);
+        mxNewChild = (XAccessible)UnoRuntime.queryInterface(
+            XAccessible.class, aEvent.NewValue);
+    }
+
+    public void PrintOldAndNew (PrintStream out)
+    {
+        if (mxOldChild != null)
+            out.println ("   removing child " + mxOldChild);
+        if (mxNewChild != null)
+            out.println ("   adding child " + mxNewChild);
+    }
+
+    public void Process ()
+    {
+        // Insertion and removal of children should be mutually exclusive.
+        // But this is a test tool and should take everything into account.
+        if (mxOldChild != null)
+        {
+            maTreeModel.removeNode (mxOldChild.getAccessibleContext());
+            maTreeModel.updateNode (mxEventSource, AccessibleTreeHandler.class);
+        }
+
+        if (mxNewChild != null)
+        {
+            maTreeModel.addChild (mxEventSource, mxNewChild);
+        }
+    }
+
+
+    private XAccessible mxOldChild;
+    private XAccessible mxNewChild;
+}
---
+++ toolkit/test/accessibility/ContextEventHandler.java
@@ -0,0 +1,50 @@
+import drafts.com.sun.star.accessibility.XAccessible;
+import drafts.com.sun.star.accessibility.AccessibleEventObject;
+import drafts.com.sun.star.accessibility.AccessibleEventId;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.AnyConverter;
+
+import java.io.PrintStream;
+
+class ContextEventHandler
+    extends EventHandler
+{
+    public ContextEventHandler (AccessibleEventObject aEvent, AccessibilityTreeModel aTreeModel)
+    {
+        super (aEvent, aTreeModel);
+    }
+
+    public void PrintOldAndNew (PrintStream out)
+    {
+        switch (mnEventId)
+        {
+            case AccessibleEventId.ACCESSIBLE_STATE_EVENT:
+                try
+                {
+                    int nOldValue = AnyConverter.toInt (maEvent.OldValue);
+                    out.println ("    turning off state " + nOldValue + " ("
+                        + NameProvider.getStateName (nOldValue) + ")");
+                }
+                catch (com.sun.star.lang.IllegalArgumentException e)
+                {}
+                try
+                {
+                    int nNewValue = AnyConverter.toInt (maEvent.NewValue);
+                    out.println ("    turning on state " + nNewValue + " ("
+                        + NameProvider.getStateName (nNewValue) + ")");
+                }
+                catch (com.sun.star.lang.IllegalArgumentException e)
+                {}
+                break;
+
+            default:
+                super.PrintOldAndNew (out);
+        }
+
+    }
+
+    public void Process ()
+    {
+        maTreeModel.updateNode (mxEventSource, AccessibleContextHandler.class);
+    }
+}
