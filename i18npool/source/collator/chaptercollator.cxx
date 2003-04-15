9ecf53b774e3792377319854e78691e236701f14
Vladimir Glazounov <vg@openoffice.org>
2003-04-15 16:07:52 +0000

    INTEGRATION: CWS dbgmacros1 (1.3.46); FILE MERGED 2003/04/09 11:54:43 kso 1.3.46.1: #108413# - debug macro unification.

--- i18npool/source/collator/chaptercollator.cxx
+++ i18npool/source/collator/chaptercollator.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: chaptercollator.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-26 10:54:38 $
+ *  last change: $Author: vg $ $Date: 2003-04-15 17:07:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#include <assert.h>
 
 // prevent internal compiler error with MSVC6SP3
 #include <stl/utility>
