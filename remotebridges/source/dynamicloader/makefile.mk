f2a11b056ac5c35ef35fcba44b22980bed9fad93
Vladimir Glazounov <vg@openoffice.org>
2003-04-15 12:36:03 +0000

    INTEGRATION: CWS tune01 (1.5.50); FILE MERGED 2003/03/28 10:02:10 kso 1.5.50.1: #i12685# - added map file.

--- remotebridges/source/dynamicloader/makefile.mk
+++ remotebridges/source/dynamicloader/makefile.mk
@@ -1,15 +1,63 @@
 #*************************************************************************
-#*
-#*	  $Workfile:   makefile  $
-#*
-#*	  Ersterstellung	JSC 24.06.99
-#*	  Letzte Aenderung	$Author: pluby $ $Date: 2001-03-02 07:16:26 $
-#*	  $Revision: 1.5 $
-#*
-#*	  $Logfile:$
-#*
-#*	  Copyright (c) 1990-1994, STAR DIVISION
-#*
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.6 $
+#
+#   last change: $Author: vg $ $Date: 2003-04-15 13:36:03 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
 #*************************************************************************
 
 PRJ=..$/..
@@ -17,7 +65,6 @@ PRJ=..$/..
 PRJNAME=remotebridges
 TARGET=dynamicloader
 ENABLE_EXCEPTIONS=TRUE
-USE_DEFFILE=TRUE
 NO_BSYMBOLIC=TRUE
 COMP1TYPELIST=$(TARGET)
 
@@ -29,7 +76,7 @@ COMP1TYPELIST=$(TARGET)
 
 CPPUMAKERFLAGS += -C
 UNOUCRDEP=$(SOLARBINDIR)$/udkapi.rdb
-UNOUCRRDB=$(SOLARBINDIR)$/udkapi.rdb 
+UNOUCRRDB=$(SOLARBINDIR)$/udkapi.rdb
 
 # output directory (one dir for each project)
 UNOUCROUT=$(OUT)$/inc
@@ -39,7 +86,7 @@ INCPRE+=$(UNOUCROUT)
 
 
 SLOFILES= \
-    $(SLO)$/dynamicloader.obj 
+    $(SLO)$/dynamicloader.obj
 
 SHL1TARGET= $(TARGET)
 
@@ -49,11 +96,12 @@ SHL1STDLIBS=\
         $(SALLIB)
 
 SHL1IMPLIB=	i$(TARGET)
-SHL1LIBS=$(SLB)$/$(TARGET).lib 
-SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+SHL1LIBS=$(SLB)$/$(TARGET).lib
+
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+SHL1VERSIONMAP=$(SHL1TARGET).map
 
 DEF1NAME=	$(SHL1TARGET)
-DEF1EXPORTFILE=	exports.dxp
 
 
 # --- Targets ------------------------------------------------------
