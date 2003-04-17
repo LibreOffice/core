bbe5249df55da65e48002f7e7ba65f9a593652b7
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:23:07 +0000

    INTEGRATION: CWS os8 (1.61.2.7.6); FILE MERGED 2003/04/03 07:13:59 os 1.61.2.7.6.1: #108583# precompiled headers removed

--- sw/source/ui/docvw/edtwin.cxx
+++ sw/source/ui/docvw/edtwin.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edtwin.cxx,v $
  *
- *  $Revision: 1.64 $
+ *  $Revision: 1.65 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:02:21 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:23:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
