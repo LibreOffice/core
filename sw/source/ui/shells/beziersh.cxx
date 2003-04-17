eca361089bc3a4aab15f5179052a0888d159ca1a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:40:41 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:14:59 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/ui/shells/beziersh.cxx
+++ sw/source/ui/shells/beziersh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: beziersh.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:56:51 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:40:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
