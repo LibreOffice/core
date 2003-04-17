574ea29a420b1aa2493ea4ccdb74015723688ed2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:39:29 +0000

    INTEGRATION: CWS os8 (1.8.142); FILE MERGED 2003/04/03 07:14:54 os 1.8.142.1: #108583# precompiled headers removed

--- sw/source/ui/ribbar/inputwin.cxx
+++ sw/source/ui/ribbar/inputwin.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: inputwin.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:16:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:39:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
