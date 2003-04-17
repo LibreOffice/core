fc3919d91ab910e043d17b72e21a8973a48c5ac5
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:41:10 +0000

    INTEGRATION: CWS os8 (1.2.178); FILE MERGED 2003/04/03 07:09:31 os 1.2.178.1: #108583# precompiled headers removed

--- sw/source/core/attr/swatrset.cxx
+++ sw/source/core/attr/swatrset.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swatrset.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jp $ $Date: 2000-11-20 09:37:36 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:41:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
