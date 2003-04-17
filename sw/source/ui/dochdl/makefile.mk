d11861993f3be1b6b04239c65f31964a95ec3230
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:22:16 +0000

    INTEGRATION: CWS os8 (1.3.172); FILE MERGED 2003/04/03 07:13:58 os 1.3.172.1: #108583# precompiled headers removed

--- sw/source/ui/dochdl/makefile.mk
+++ sw/source/ui/dochdl/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: jp $ $Date: 2001-03-23 16:00:53 $
+#   last change: $Author: vg $ $Date: 2003-04-17 15:22:16 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=dochdl
 
 AUTOSEG=true
-
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\ui_1st\ui_pch
 
 # --- Settings -----------------------------------------------------
 
