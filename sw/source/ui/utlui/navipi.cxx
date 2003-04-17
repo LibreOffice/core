8e20aa833b37e62cb09763d67a01d6236746006b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:56:23 +0000

    INTEGRATION: CWS os8 (1.22.146); FILE MERGED 2003/04/03 07:15:49 os 1.22.146.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/navipi.cxx
+++ sw/source/ui/utlui/navipi.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: navipi.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: fs $ $Date: 2002-07-19 15:36:16 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:56:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 
 #pragma hdrstop
