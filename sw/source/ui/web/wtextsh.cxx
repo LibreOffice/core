6fc6d901f9929d6e7fbc894aafb292d664bda7c4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:59:22 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:15:59 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/ui/web/wtextsh.cxx
+++ sw/source/ui/web/wtextsh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wtextsh.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:59:37 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:59:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
