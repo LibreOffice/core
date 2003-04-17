efd0b2244b66834b501c83b5de71492931582074
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:26:46 +0000

    INTEGRATION: CWS os8 (1.12.140); FILE MERGED 2003/04/03 07:11:34 os 1.12.140.1: #108583# precompiled headers removed

--- sw/source/core/text/itradj.cxx
+++ sw/source/core/text/itradj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: itradj.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:53:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:26:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
