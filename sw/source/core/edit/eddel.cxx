c36f2791859976f25da97dd98640c3d887be8411
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:00:16 +0000

    INTEGRATION: CWS os8 (1.1.1.1.182); FILE MERGED 2003/04/03 07:10:28 os 1.1.1.1.182.1: #108583# precompiled headers removed

--- sw/source/core/edit/eddel.cxx
+++ sw/source/core/edit/eddel.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: eddel.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:49:54 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:00:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
