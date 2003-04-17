7bd720a2862f6c8e766caa8324ec66274cb5949f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:09:03 +0000

    INTEGRATION: CWS os8 (1.12.140); FILE MERGED 2003/04/07 22:17:22 os 1.12.140.2: RESYNC: (1.12-1.13); FILE MERGED 2003/04/03 07:10:49 os 1.12.140.1: #108583# precompiled headers removed

--- sw/source/core/frmedt/fews.cxx
+++ sw/source/core/frmedt/fews.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fews.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:11:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:09:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
