125f15b852f4735feffc5acaf18c0159d1cb5f0d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:14:10 +0000

    INTEGRATION: CWS os8 (1.17.148); FILE MERGED 2003/04/03 07:11:01 os 1.17.148.1: #108583# precompiled headers removed

--- sw/source/core/layout/newfrm.cxx
+++ sw/source/core/layout/newfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: newfrm.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: mib $ $Date: 2002-05-03 12:36:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:14:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
