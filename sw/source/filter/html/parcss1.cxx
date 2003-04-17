1fc11cf1f2b4acbf3ef8e0bd728e64dfeb4f8c76
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:57:31 +0000

    INTEGRATION: CWS os8 (1.2.160); FILE MERGED 2003/04/03 07:12:52 os 1.2.160.1: #108583# precompiled headers removed

--- sw/source/filter/html/parcss1.cxx
+++ sw/source/filter/html/parcss1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: parcss1.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2001-09-28 06:27:53 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:57:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
