e22039691539cfb63428d66e4fb9ad2f13673633
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:05:45 +0000

    INTEGRATION: CWS os8 (1.12.138); FILE MERGED 2003/04/07 21:57:46 os 1.12.138.2: RESYNC: (1.12-1.13); FILE MERGED 2003/04/03 07:13:16 os 1.12.138.1: #108583# precompiled headers removed

--- sw/source/filter/xml/XMLRedlineImportHelper.cxx
+++ sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLRedlineImportHelper.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:38:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:05:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
