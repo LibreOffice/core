6dd4e952c26c44298604a96ef787f064b9657c1c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:22:26 +0000

    INTEGRATION: CWS os8 (1.3.140); FILE MERGED 2003/04/03 07:13:58 os 1.3.140.1: #108583# precompiled headers removed

--- sw/source/ui/dochdl/selglos.cxx
+++ sw/source/ui/dochdl/selglos.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: selglos.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: mh $ $Date: 2001-10-25 16:54:08 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:22:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
