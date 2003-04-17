4a266a472c2a324a63e57fd5263dbc8641ec20d5
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:48:12 +0000

    INTEGRATION: CWS os8 (1.3.166); FILE MERGED 2003/04/03 07:09:52 os 1.3.166.1: #108583# precompiled headers removed

--- sw/source/core/doc/docbasic.cxx
+++ sw/source/core/doc/docbasic.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docbasic.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: mba $ $Date: 2001-06-11 09:16:58 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:48:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
