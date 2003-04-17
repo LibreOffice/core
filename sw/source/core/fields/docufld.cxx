755e7efb80bac059970f2f60d4911b107aa2855f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:05:16 +0000

    INTEGRATION: CWS os8 (1.28.138); FILE MERGED 2003/04/07 22:17:37 os 1.28.138.2: RESYNC: (1.28-1.29); FILE MERGED 2003/04/03 07:10:40 os 1.28.138.1: #108583# precompiled headers removed

--- sw/source/core/fields/docufld.cxx
+++ sw/source/core/fields/docufld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docufld.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:20:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:05:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
