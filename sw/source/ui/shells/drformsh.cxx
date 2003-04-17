fb5b330421709990f466cc3d0552b4c465cf5675
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:41:14 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:15:01 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/ui/shells/drformsh.cxx
+++ sw/source/ui/shells/drformsh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drformsh.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:56:52 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:41:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
