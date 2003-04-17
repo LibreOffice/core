3b09e275ece5b1a7339990f3a26ab5c2f8d63ee1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:33:41 +0000

    INTEGRATION: CWS os8 (1.1.1.1.182); FILE MERGED 2003/04/03 07:09:16 os 1.1.1.1.182.1: #108583# precompiled headers removed

--- sw/inc/swpre.mk
+++ sw/inc/swpre.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: swpre.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:14:28 $
+#   last change: $Author: vg $ $Date: 2003-04-17 13:33:41 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,9 +77,5 @@ optimize=true
 OPTIMIZE=TRUE
 .ENDIF
 .ENDIF
-
-# falls dependencies auf s:\solver\... erstellt werden sollen
-# MKDEPENDSOLVER=YES
-PROJECTPCH4DLL=TRUE
 
 
