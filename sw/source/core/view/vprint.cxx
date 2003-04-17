56c3755c464dde4fca53acd7fc6d2312ec959d16
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:48:12 +0000

    INTEGRATION: CWS os8 (1.19.2.5.46); FILE MERGED 2003/04/07 22:07:33 os 1.19.2.5.46.2: RESYNC: (1.19.2.5-1.22); FILE MERGED 2003/04/03 07:12:30 os 1.19.2.5.46.1: #108583# precompiled headers removed

--- sw/source/core/view/vprint.cxx
+++ sw/source/core/view/vprint.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: vprint.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:37:09 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:48:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
