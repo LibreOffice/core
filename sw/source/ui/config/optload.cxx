94a763edb7114a5e3e8b3d1d0e5004724a0e142d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:18:31 +0000

    INTEGRATION: CWS os8 (1.14.166); FILE MERGED 2003/04/07 21:54:06 os 1.14.166.2: RESYNC: (1.14-1.15); FILE MERGED 2003/04/03 07:13:45 os 1.14.166.1: #108583# precompiled headers removed

--- sw/source/ui/config/optload.cxx
+++ sw/source/ui/config/optload.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optload.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 10:12:21 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:18:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
