8896f08785689941ff22ef2b0884ce38fb747b52
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:07:19 +0000

    INTEGRATION: CWS os8 (1.9.138); FILE MERGED 2003/04/03 07:10:43 os 1.9.138.1: #108583# precompiled headers removed

--- sw/source/core/fields/tblcalc.cxx
+++ sw/source/core/fields/tblcalc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tblcalc.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: os $ $Date: 2002-11-15 11:08:43 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:07:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
