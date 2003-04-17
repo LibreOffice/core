18200568bc513dc23c7943dce9d4ed281d513d3a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:12:19 +0000

    INTEGRATION: CWS os8 (1.19.142); FILE MERGED 2003/04/03 07:10:57 os 1.19.142.1: #108583# precompiled headers removed

--- sw/source/core/layout/flylay.cxx
+++ sw/source/core/layout/flylay.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: flylay.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: ama $ $Date: 2002-09-12 09:22:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:12:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
