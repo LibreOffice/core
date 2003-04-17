299d4ba1e2f67e0e3248d3a96f22eca82434d23e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:36:47 +0000

    INTEGRATION: CWS os8 (1.3.8.1.48); FILE MERGED 2003/04/03 07:09:22 os 1.3.8.1.48.1: #108583# precompiled headers removed

--- sw/source/core/access/accgraphic.cxx
+++ sw/source/core/access/accgraphic.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accgraphic.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:18 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:36:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
