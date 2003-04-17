b01267f5b18dbe513337f5d9a453af190fbb122e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:47:08 +0000

    INTEGRATION: CWS os8 (1.7.2.1.20); FILE MERGED 2003/04/03 07:12:28 os 1.7.2.1.20.1: #108583# precompiled headers removed

--- sw/source/core/view/scrrect.cxx
+++ sw/source/core/view/scrrect.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: scrrect.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:41:36 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:47:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
