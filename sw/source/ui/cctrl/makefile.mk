e3fe4e7b2982e62b1b0e8adda07b3a63aec9a207
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:14:36 +0000

    INTEGRATION: CWS os8 (1.1.1.1.182); FILE MERGED 2003/04/03 07:13:37 os 1.1.1.1.182.1: #108583# precompiled headers removed

--- sw/source/ui/cctrl/makefile.mk
+++ sw/source/ui/cctrl/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-18 17:14:32 $
+#   last change: $Author: vg $ $Date: 2003-04-17 15:14:36 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=cctrl
 
 AUTOSEG=true
-
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\ui_1st\ui_pch
 
 # --- Settings -----------------------------------------------------
 
