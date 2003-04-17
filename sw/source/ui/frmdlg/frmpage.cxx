24ecc8cff13e9046c7334a6c7465ccdae8fc57e7
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:30:56 +0000

    INTEGRATION: CWS os8 (1.33.2.3.48); FILE MERGED 2003/04/03 07:14:28 os 1.33.2.3.48.1: #108583# precompiled headers removed

--- sw/source/ui/frmdlg/frmpage.cxx
+++ sw/source/ui/frmdlg/frmpage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: frmpage.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:16:02 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:30:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
