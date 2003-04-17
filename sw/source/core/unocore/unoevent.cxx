4726beff0c1a1b4ae56812a304111a441d5c3afe
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:42:33 +0000

    INTEGRATION: CWS os8 (1.10.170); FILE MERGED 2003/04/03 07:12:16 os 1.10.170.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoevent.cxx
+++ sw/source/core/unocore/unoevent.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoevent.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: dvo $ $Date: 2001-04-17 13:52:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:42:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
