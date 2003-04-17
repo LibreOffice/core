3d92b037263e6184b510b1509db42a265a4b012f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:53:54 +0000

    INTEGRATION: CWS os8 (1.6.24.1.112); FILE MERGED 2003/04/03 07:12:46 os 1.6.24.1.112.1: #108583# precompiled headers removed

--- sw/source/filter/html/htmlbas.cxx
+++ sw/source/filter/html/htmlbas.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: htmlbas.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:41:46 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:53:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
