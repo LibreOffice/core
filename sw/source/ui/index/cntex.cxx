6f8ba6fc6e33d74af9881e098c86a94238c22f28
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:32:57 +0000

    INTEGRATION: CWS os8 (1.2.4.1.48); FILE MERGED 2003/04/03 07:14:33 os 1.2.4.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/index/cntex.cxx
+++ sw/source/ui/index/cntex.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cntex.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:03:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:32:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
