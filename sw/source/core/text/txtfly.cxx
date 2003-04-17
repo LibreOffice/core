adca0046084d04fe61b6b27fb7b7095ca0c4cfc6
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:30:27 +0000

    INTEGRATION: CWS os8 (1.30.2.1.58); FILE MERGED 2003/04/07 22:12:39 os 1.30.2.1.58.2: RESYNC: (1.30.2.1-1.32); FILE MERGED 2003/04/03 07:11:42 os 1.30.2.1.58.1: #108583# precompiled headers removed

--- sw/source/core/text/txtfly.cxx
+++ sw/source/core/text/txtfly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtfly.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:56:03 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:30:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
