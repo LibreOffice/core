ddad1b2abcc37a5eb30dab176bd24f48ca84b8ca
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:41:21 +0000

    INTEGRATION: CWS os8 (1.2.20.1.54); FILE MERGED 2003/04/03 07:09:32 os 1.2.20.1.54.1: #108583# precompiled headers removed

--- sw/source/core/bastyp/bparr.cxx
+++ sw/source/core/bastyp/bparr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bparr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:41:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
