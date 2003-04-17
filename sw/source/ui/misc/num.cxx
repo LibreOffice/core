fcd998a769341a6363cea33dc7d2ba58bf5fc02e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:37:07 +0000

    INTEGRATION: CWS os8 (1.6.138); FILE MERGED 2003/04/03 07:14:46 os 1.6.138.1: #108583# precompiled headers removed

--- sw/source/ui/misc/num.cxx
+++ sw/source/ui/misc/num.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: num.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:03:28 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:37:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
