690acf0f01f565411089f5719571cec43c433d5c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:32:31 +0000

    INTEGRATION: CWS os8 (1.4.146); FILE MERGED 2003/04/03 07:11:52 os 1.4.146.1: #108583# precompiled headers removed

--- sw/source/core/tox/tox.cxx
+++ sw/source/core/tox/tox.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tox.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: fme $ $Date: 2002-06-06 13:16:41 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:32:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
