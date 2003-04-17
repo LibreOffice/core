66c88d54b8fd0735dbce6a5d90e85b2226b04e34
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:00:05 +0000

    INTEGRATION: CWS os8 (1.27.2.2.48); FILE MERGED 2003/04/07 22:18:03 os 1.27.2.2.48.2: RESYNC: (1.27.2.2-1.29); FILE MERGED 2003/04/03 07:10:28 os 1.27.2.2.48.1: #108583# precompiled headers removed

--- sw/source/core/edit/edattr.cxx
+++ sw/source/core/edit/edattr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edattr.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:10:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:00:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
