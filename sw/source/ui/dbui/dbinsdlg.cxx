a1305d5391cf2c4a1862ec166d9641a14d8cfe6b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:19:50 +0000

    INTEGRATION: CWS os8 (1.37.140); FILE MERGED 2003/04/03 07:13:51 os 1.37.140.1: #108583# precompiled headers removed

--- sw/source/ui/dbui/dbinsdlg.cxx
+++ sw/source/ui/dbui/dbinsdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbinsdlg.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: os $ $Date: 2002-09-27 13:18:44 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:19:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
