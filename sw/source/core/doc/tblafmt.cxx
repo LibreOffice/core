8d6d1cc5a60b67593704a4bc3070664fb9186d0d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:54:52 +0000

    INTEGRATION: CWS os8 (1.9.140); FILE MERGED 2003/04/03 07:10:08 os 1.9.140.1: #108583# precompiled headers removed

--- sw/source/core/doc/tblafmt.cxx
+++ sw/source/core/doc/tblafmt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tblafmt.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: dr $ $Date: 2002-09-25 15:56:20 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:54:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,9 +60,6 @@
  ************************************************************************/
 
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
