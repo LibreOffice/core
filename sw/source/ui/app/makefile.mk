286045004a0b98fbb079351d5df1a1958a623ccd
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:12:43 +0000

    INTEGRATION: CWS os8 (1.4.4.1.48); FILE MERGED 2003/04/03 07:13:32 os 1.4.4.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/app/makefile.mk
+++ sw/source/ui/app/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-27 15:42:34 $
+#   last change: $Author: vg $ $Date: 2003-04-17 15:12:43 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=app
 
 AUTOSEG=true
-
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..$/ui_1st$/ui_pch
 
 LIBTARGET=NO
 
