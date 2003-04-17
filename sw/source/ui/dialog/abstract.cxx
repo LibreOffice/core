f24bd7e9b952fd442505a080977f606c0df2a1df
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:20:43 +0000

    INTEGRATION: CWS os8 (1.3.138); FILE MERGED 2003/04/03 07:13:54 os 1.3.138.1: #108583# precompiled headers removed

--- sw/source/ui/dialog/abstract.cxx
+++ sw/source/ui/dialog/abstract.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: abstract.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:39:15 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:20:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
