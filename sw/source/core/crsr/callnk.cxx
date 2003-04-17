e6faa743ac7ed4e5fa6240735c48e806a0829f3c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:44:07 +0000

    INTEGRATION: CWS os8 (1.5.142); FILE MERGED 2003/04/03 07:09:39 os 1.5.142.1: #108583# precompiled headers removed

--- sw/source/core/crsr/callnk.cxx
+++ sw/source/core/crsr/callnk.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: callnk.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: fme $ $Date: 2002-09-09 09:26:48 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:44:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
