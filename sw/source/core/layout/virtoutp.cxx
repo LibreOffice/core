3d2e0705a3ef3a1999168ccfd611dd255dde3c9d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:16:27 +0000

    INTEGRATION: CWS os8 (1.6.138); FILE MERGED 2003/04/03 07:11:05 os 1.6.138.1: #108583# precompiled headers removed

--- sw/source/core/layout/virtoutp.cxx
+++ sw/source/core/layout/virtoutp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: virtoutp.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: od $ $Date: 2002-11-20 09:50:02 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:16:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
