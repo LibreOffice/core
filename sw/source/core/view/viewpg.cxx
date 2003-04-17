389ad43a0f8a888c35937fb4616d87ed9fe4681a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:47:40 +0000

    INTEGRATION: CWS os8 (1.16.2.2.54); FILE MERGED 2003/04/07 22:07:21 os 1.16.2.2.54.2: RESYNC: (1.16.2.2-1.18); FILE MERGED 2003/04/03 07:12:29 os 1.16.2.2.54.1: #108583# precompiled headers removed

--- sw/source/core/view/viewpg.cxx
+++ sw/source/core/view/viewpg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewpg.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 09:59:18 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:47:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
