fffd5742aad0d76f8933eae0704c3bbb2a443475
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:41:36 +0000

    INTEGRATION: CWS os8 (1.20.2.1.48); FILE MERGED 2003/04/03 07:15:02 os 1.20.2.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/shells/drwtxtex.cxx
+++ sw/source/ui/shells/drwtxtex.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drwtxtex.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:44:20 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:41:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
