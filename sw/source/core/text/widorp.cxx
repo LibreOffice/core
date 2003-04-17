372680a132ce16f1574f8fced9a204f0169eeba5
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:31:56 +0000

    INTEGRATION: CWS os8 (1.9.138); FILE MERGED 2003/04/03 07:11:46 os 1.9.138.1: #108583# precompiled headers removed

--- sw/source/core/text/widorp.cxx
+++ sw/source/core/text/widorp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: widorp.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: fme $ $Date: 2002-12-12 09:04:44 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:31:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
