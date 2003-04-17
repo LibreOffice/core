6d77689c3dca78d74806ee348ec3f4c665a0ec3d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:43:55 +0000

    INTEGRATION: CWS os8 (1.3.174); FILE MERGED 2003/04/03 07:09:39 os 1.3.174.1: #108583# precompiled headers removed

--- sw/source/core/crsr/bookmrk.cxx
+++ sw/source/core/crsr/bookmrk.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bookmrk.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: jp $ $Date: 2001-03-13 18:30:19 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:43:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
