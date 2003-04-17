8435f9b9fd3de09d8a3bd74bcf61e7b964adf5b0
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:02:13 +0000

    INTEGRATION: CWS os8 (1.4.34.1.86); FILE MERGED 2003/04/03 07:10:32 os 1.4.34.1.86.1: #108583# precompiled headers removed

--- sw/source/core/edit/edsect.cxx
+++ sw/source/core/edit/edsect.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edsect.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:54 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:02:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
