fba14cabdf35294dd888c08fcff198cbab55d31a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:15:09 +0000

    INTEGRATION: CWS os8 (1.56.2.9.18); FILE MERGED 2003/04/07 22:14:58 os 1.56.2.9.18.2: RESYNC: (1.56.2.9-1.58); FILE MERGED 2003/04/03 07:11:02 os 1.56.2.9.18.1: #108583# precompiled headers removed

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: paintfrm.cxx,v $
  *
- *  $Revision: 1.59 $
+ *  $Revision: 1.60 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:51:53 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:15:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
