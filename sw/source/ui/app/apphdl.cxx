ee9df7ff6d9015c4413eefc48b054a91ed20cef0
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:10:33 +0000

    INTEGRATION: CWS os8 (1.29.2.3.48); FILE MERGED 2003/04/03 07:13:28 os 1.29.2.3.48.1: #108583# precompiled headers removed

--- sw/source/ui/app/apphdl.cxx
+++ sw/source/ui/app/apphdl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: apphdl.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 15:32:51 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:10:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
