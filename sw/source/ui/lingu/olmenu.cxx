c75505668b65c63eff77b3d79a0f1704adaefc8a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:34:35 +0000

    INTEGRATION: CWS os8 (1.9.6.2.4.1.138); FILE MERGED 2003/04/03 07:14:38 os 1.9.6.2.4.1.138.1: #108583# precompiled headers removed

--- sw/source/ui/lingu/olmenu.cxx
+++ sw/source/ui/lingu/olmenu.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: olmenu.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:43:55 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:34:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
