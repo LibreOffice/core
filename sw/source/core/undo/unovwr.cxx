c1290a2b7d093a9c03cde39b06731d66075e037d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:39:46 +0000

    INTEGRATION: CWS os8 (1.10.156); FILE MERGED 2003/04/03 07:12:09 os 1.10.156.1: #108583# precompiled headers removed

--- sw/source/core/undo/unovwr.cxx
+++ sw/source/core/undo/unovwr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unovwr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: jp $ $Date: 2001-12-12 12:32:30 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:39:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
