ff05ee8881070eab35f0214d3fae6f050e3c03f5
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:19:12 +0000

    INTEGRATION: CWS os8 (1.7.174); FILE MERGED 2003/04/03 07:13:46 os 1.7.174.1: #108583# precompiled headers removed

--- sw/source/ui/config/uinums.cxx
+++ sw/source/ui/config/uinums.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: uinums.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: os $ $Date: 2001-02-23 12:45:29 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:19:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
