25d454145530bb8e6fafe69eb9768ba2398e880d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:13:08 +0000

    INTEGRATION: CWS os8 (1.9.148); FILE MERGED 2003/04/03 07:13:32 os 1.9.148.1: #108583# precompiled headers removed

--- sw/source/ui/app/swdll.cxx
+++ sw/source/ui/app/swdll.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swdll.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: os $ $Date: 2002-04-19 09:38:00 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:13:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
