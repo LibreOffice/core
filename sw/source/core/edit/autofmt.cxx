e703381ed6700b61f91f2ef08b9c04f4327c954a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:59:44 +0000

    INTEGRATION: CWS os8 (1.15.2.1.86); FILE MERGED 2003/04/07 22:17:57 os 1.15.2.1.86.2: RESYNC: (1.15.2.1-1.17); FILE MERGED 2003/04/03 07:10:27 os 1.15.2.1.86.1: #108583# precompiled headers removed

--- sw/source/core/edit/autofmt.cxx
+++ sw/source/core/edit/autofmt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: autofmt.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:49:43 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:59:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
