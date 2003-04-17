ec8f1fb0624adddb2cda53b7d6abf43afbe354ae
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:24:04 +0000

    INTEGRATION: CWS os8 (1.9.2.3.20); FILE MERGED 2003/04/03 07:14:02 os 1.9.2.3.20.1: #108583# precompiled headers removed

--- sw/source/ui/docvw/srcedtw.cxx
+++ sw/source/ui/docvw/srcedtw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: srcedtw.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:02:32 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:24:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
