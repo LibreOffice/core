de79247647106edc688d26cccbea60b02ec6c5ce
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:08:51 +0000

    INTEGRATION: CWS os8 (1.2.178); FILE MERGED 2003/04/03 07:13:22 os 1.2.178.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmlscript.cxx
+++ sw/source/filter/xml/xmlscript.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlscript.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ab $ $Date: 2000-12-05 09:34:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:08:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
