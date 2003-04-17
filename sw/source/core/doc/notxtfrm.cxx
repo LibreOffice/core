9d89fc250d02be68adee97593ddb4991ec019148
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:53:30 +0000

    INTEGRATION: CWS os8 (1.18.2.2.54); FILE MERGED 2003/04/07 22:20:19 os 1.18.2.2.54.2: RESYNC: (1.18.2.2-1.20); FILE MERGED 2003/04/03 07:10:05 os 1.18.2.2.54.1: #108583# precompiled headers removed

--- sw/source/core/doc/notxtfrm.cxx
+++ sw/source/core/doc/notxtfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: notxtfrm.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 09:53:15 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:53:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
