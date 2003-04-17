0cb41cdf8663df6c6d7b35227bb9720e925113a2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:26:15 +0000

    INTEGRATION: CWS os8 (1.33.2.2.58); FILE MERGED 2003/04/07 22:10:35 os 1.33.2.2.58.2: RESYNC: (1.33.2.2-1.35); FILE MERGED 2003/04/03 07:11:33 os 1.33.2.2.58.1: #108583# precompiled headers removed

--- sw/source/core/text/frmpaint.cxx
+++ sw/source/core/text/frmpaint.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: frmpaint.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:53:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:26:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
