cb0e45c916cfb8200f6d115a4df9e17e283a5edd
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:35:50 +0000

    INTEGRATION: CWS os8 (1.5.146); FILE MERGED 2003/04/03 07:09:21 os 1.5.146.1: #108583# precompiled headers removed

--- sw/source/core/access/accframebase.cxx
+++ sw/source/core/access/accframebase.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accframebase.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mib $ $Date: 2002-07-24 13:14:50 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:35:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
