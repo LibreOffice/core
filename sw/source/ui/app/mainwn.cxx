d7c6f878466f7e04a0ff061e71b3231ffae6bea2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:12:33 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:13:31 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/ui/app/mainwn.cxx
+++ sw/source/ui/app/mainwn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mainwn.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 13:02:03 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:12:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
