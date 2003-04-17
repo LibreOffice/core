fd2954baaa1191f75af81910751cca2a91f12d0d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:46:22 +0000

    INTEGRATION: CWS os8 (1.51.140); FILE MERGED 2003/04/03 07:12:22 os 1.51.140.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unostyle.cxx
+++ sw/source/core/unocore/unostyle.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unostyle.cxx,v $
  *
- *  $Revision: 1.52 $
+ *  $Revision: 1.53 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:13:27 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:46:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
