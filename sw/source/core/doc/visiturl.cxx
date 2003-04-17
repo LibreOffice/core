67fd1d957d2c2965c977d7c8b7eee21cc5bdfb8f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:55:30 +0000

    INTEGRATION: CWS os8 (1.2.160); FILE MERGED 2003/04/03 07:10:09 os 1.2.160.1: #108583# precompiled headers removed

--- sw/source/core/doc/visiturl.cxx
+++ sw/source/core/doc/visiturl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: visiturl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:49:09 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:55:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
