26031400342ed23cc6e14b6531376a85c826204c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:29:20 +0000

    INTEGRATION: CWS os8 (1.29.144); FILE MERGED 2003/04/07 22:12:16 os 1.29.144.2: RESYNC: (1.29-1.30); FILE MERGED 2003/04/03 07:11:40 os 1.29.144.1: #108583# precompiled headers removed

--- sw/source/core/text/porrst.cxx
+++ sw/source/core/text/porrst.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: porrst.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 09:58:05 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:29:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
