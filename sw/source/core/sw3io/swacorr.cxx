b1f817384f5605a6f66c78c5473768bdcb165724
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:21:32 +0000

    INTEGRATION: CWS os8 (1.5.170); FILE MERGED 2003/04/03 07:11:20 os 1.5.170.1: #108583# precompiled headers removed

--- sw/source/core/sw3io/swacorr.cxx
+++ sw/source/core/sw3io/swacorr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swacorr.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mtg $ $Date: 2001-05-02 16:43:23 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:21:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
