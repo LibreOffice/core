48bdf91cc93199cc89f9d7d4735e1bb6d26a1d5d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:54:44 +0000

    INTEGRATION: CWS os8 (1.9.138); FILE MERGED 2003/04/03 07:15:44 os 1.9.138.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/attrdesc.cxx
+++ sw/source/ui/utlui/attrdesc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: attrdesc.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:18:02 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:54:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
