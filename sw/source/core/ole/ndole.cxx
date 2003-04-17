88ec60435f95bbcaaf1d9bde1793199f0c9477e1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:17:05 +0000

    INTEGRATION: CWS os8 (1.10.148); FILE MERGED 2003/04/03 07:11:11 os 1.10.148.1: #108583# precompiled headers removed

--- sw/source/core/ole/ndole.cxx
+++ sw/source/core/ole/ndole.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ndole.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: jp $ $Date: 2002-02-22 15:36:14 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:17:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
