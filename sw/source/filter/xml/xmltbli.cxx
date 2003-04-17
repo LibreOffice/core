0283c0435240d58367414ba3e58af198d43ce841
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:09:15 +0000

    INTEGRATION: CWS os8 (1.34.8.5.2.1.48); FILE MERGED 2003/04/07 21:58:38 os 1.34.8.5.2.1.48.2: RESYNC: (1.34.8.5.2.1-1.42); FILE MERGED 2003/04/03 07:13:23 os 1.34.8.5.2.1.48.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmltbli.cxx
+++ sw/source/filter/xml/xmltbli.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmltbli.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:39:15 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:09:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
