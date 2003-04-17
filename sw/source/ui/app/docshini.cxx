d8b0848d150a208541af07fc2e19828f423c13fe
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:11:52 +0000

    INTEGRATION: CWS os8 (1.27.2.7.2.2.18); FILE MERGED 2003/04/03 07:13:30 os 1.27.2.7.2.2.18.1: #108583# precompiled headers removed

--- sw/source/ui/app/docshini.cxx
+++ sw/source/ui/app/docshini.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docshini.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:15:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:11:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,9 +60,6 @@
  ************************************************************************/
 
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
