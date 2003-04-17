de8002346925c1ca8679ff94b00995514ead5cd1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:46:09 +0000

    INTEGRATION: CWS os8 (1.4.14.1.58); FILE MERGED 2003/04/03 07:09:43 os 1.4.14.1.58.1: #108583# precompiled headers removed

--- sw/source/core/crsr/pam.cxx
+++ sw/source/core/crsr/pam.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pam.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:09:31 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:46:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
