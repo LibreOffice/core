02ba5add675a630f724897a0ee32101e2c2042fd
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:54:09 +0000

    INTEGRATION: CWS os8 (1.20.142); FILE MERGED 2003/04/03 07:15:41 os 1.20.142.1: #108583# precompiled headers removed

--- sw/source/ui/uno/unomod.cxx
+++ sw/source/ui/uno/unomod.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unomod.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: os $ $Date: 2002-09-20 12:10:10 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:54:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
