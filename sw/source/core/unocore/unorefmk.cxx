2c5dc15b2af9a17df860b05a9481de46db47ce6d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:45:36 +0000

    INTEGRATION: CWS os8 (1.6.2.1.48); FILE MERGED 2003/04/07 22:09:12 os 1.6.2.1.48.2: RESYNC: (1.6.2.1-1.8); FILE MERGED 2003/04/03 07:12:21 os 1.6.2.1.48.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unorefmk.cxx
+++ sw/source/core/unocore/unorefmk.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unorefmk.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:35:27 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:45:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
