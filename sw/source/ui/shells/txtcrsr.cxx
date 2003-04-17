bd930969daf81cdbaa944581031f2777f0121780
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:44:56 +0000

    INTEGRATION: CWS os8 (1.10.138); FILE MERGED 2003/04/03 07:15:10 os 1.10.138.1: #108583# precompiled headers removed

--- sw/source/ui/shells/txtcrsr.cxx
+++ sw/source/ui/shells/txtcrsr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtcrsr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: os $ $Date: 2002-12-12 16:36:44 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:44:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
