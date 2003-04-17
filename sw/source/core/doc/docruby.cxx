d7f3b8b18fabeac8784ab113b34da053a3e5e2f7
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:51:29 +0000

    INTEGRATION: CWS os8 (1.6.150); FILE MERGED 2003/04/03 07:09:59 os 1.6.150.1: #108583# precompiled headers removed

--- sw/source/core/doc/docruby.cxx
+++ sw/source/core/doc/docruby.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docruby.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: jp $ $Date: 2002-02-01 12:39:14 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:51:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
