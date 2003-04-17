a99c0ad5bd5cd570d272067989786cd13abdd000
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:57:38 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:10:19 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/core/docnode/node2lay.cxx
+++ sw/source/core/docnode/node2lay.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: node2lay.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: od $ $Date: 2002-11-15 11:06:37 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:57:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
