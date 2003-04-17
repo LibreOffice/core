219fdc8de7f133f7fc9a0386c8135f125423ea47
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:57:34 +0000

    INTEGRATION: CWS os8 (1.13.160); FILE MERGED 2003/04/07 21:15:57 os 1.13.160.2: RESYNC: (1.13-1.14); FILE MERGED 2003/04/03 07:15:53 os 1.13.160.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/unotools.cxx
+++ sw/source/ui/utlui/unotools.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unotools.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:44:12 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:57:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
