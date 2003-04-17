eb7e025d534fc2feff805cb6fdc13b082f5d3571
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:57:43 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:15:54 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/zoomctrl.cxx
+++ sw/source/ui/utlui/zoomctrl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: zoomctrl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 13:01:17 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:57:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
