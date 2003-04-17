5d8414dd38e589ff68e337ecb42d8ff4518788ed
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:45:48 +0000

    INTEGRATION: CWS os8 (1.40.138); FILE MERGED 2003/04/07 22:09:19 os 1.40.138.2: RESYNC: (1.40-1.41); FILE MERGED 2003/04/03 07:12:21 os 1.40.138.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unosect.cxx
+++ sw/source/core/unocore/unosect.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unosect.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:35:45 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:45:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
