aaa20c21fad4ac16fa49b813e2214e2d6951c999
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:50:56 +0000

    INTEGRATION: CWS os8 (1.16.138); FILE MERGED 2003/04/07 21:18:47 os 1.16.138.2: RESYNC: (1.16-1.17); FILE MERGED 2003/04/03 07:15:30 os 1.16.138.1: #108583# precompiled headers removed

--- sw/source/ui/uiview/viewdraw.cxx
+++ sw/source/ui/uiview/viewdraw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewdraw.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 10:13:49 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:50:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
