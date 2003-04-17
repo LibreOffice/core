81f882ef3b315a4898329a1663be0f441681369e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:54:06 +0000

    INTEGRATION: CWS os8 (1.21.2.2.20); FILE MERGED 2003/04/03 07:10:06 os 1.21.2.2.20.1: #108583# precompiled headers removed

--- sw/source/core/doc/poolfmt.cxx
+++ sw/source/core/doc/poolfmt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: poolfmt.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:40 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:54:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
