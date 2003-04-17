c87060a6826fcfb9aa04f29d47ebe96bbc294261
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 15:04:53 +0000

    INTEGRATION: CWS os8 (1.5.138); FILE MERGED 2003/04/07 21:12:43 os 1.5.138.2: RESYNC: (1.5-1.6); FILE MERGED 2003/04/03 07:16:17 os 1.5.138.1: #108583# precompiled headers removed

--- sw/source/ui/wrtsh/move.cxx
+++ sw/source/ui/wrtsh/move.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: move.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:25:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 16:04:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
