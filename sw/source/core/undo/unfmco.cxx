51d2a292a42af5aab726c14e7e8e91e71a003e78
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:38:50 +0000

    INTEGRATION: CWS os8 (1.2.160); FILE MERGED 2003/04/03 07:12:07 os 1.2.160.1: #108583# precompiled headers removed

--- sw/source/core/undo/unfmco.cxx
+++ sw/source/core/undo/unfmco.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unfmco.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2001-09-28 07:32:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:38:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
