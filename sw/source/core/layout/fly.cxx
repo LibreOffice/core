1223a91e61f7fe3db55f179b7080150c06070943
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:11:40 +0000

    INTEGRATION: CWS os8 (1.37.2.2.48); FILE MERGED 2003/04/03 07:10:56 os 1.37.2.2.48.1: #108583# precompiled headers removed

--- sw/source/core/layout/fly.cxx
+++ sw/source/core/layout/fly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fly.cxx,v $
  *
- *  $Revision: 1.39 $
+ *  $Revision: 1.40 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:12:00 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:11:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
