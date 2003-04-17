316aacc8d32e190c5a9d6033b945666b8a772b03
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:35:11 +0000

    INTEGRATION: CWS os8 (1.2.160); FILE MERGED 2003/04/03 07:11:57 os 1.2.160.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/ndhints.cxx
+++ sw/source/core/txtnode/ndhints.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ndhints.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ama $ $Date: 2001-09-05 09:39:46 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:35:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
