627c4976d4d25a130fffc7c7fd0939015807e191
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:57:43 +0000

    INTEGRATION: CWS os8 (1.5.138); FILE MERGED 2003/04/03 07:12:52 os 1.5.138.1: #108583# precompiled headers removed

--- sw/source/filter/html/svxcss1.cxx
+++ sw/source/filter/html/svxcss1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svxcss1.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mib $ $Date: 2002-11-21 13:11:48 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:57:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
