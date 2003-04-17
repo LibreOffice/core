5d6f417b87f07bb1c3175f57af0936944ca4c81d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:46:40 +0000

    INTEGRATION: CWS os8 (1.16.4.7.2.3.58); FILE MERGED 2003/04/03 07:09:45 os 1.16.4.7.2.3.58.1: #108583# precompiled headers removed

--- sw/source/core/crsr/swcrsr.cxx
+++ sw/source/core/crsr/swcrsr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swcrsr.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:09:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:46:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
