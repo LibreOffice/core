76c4f53aa2b3494b21d58b6acb229f45e8e95f84
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:13:53 +0000

    INTEGRATION: CWS os8 (1.2.166); FILE MERGED 2003/04/03 07:13:33 os 1.2.166.1: #108583# precompiled headers removed

--- sw/source/ui/app/swwait.cxx
+++ sw/source/ui/app/swwait.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swwait.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ama $ $Date: 2001-07-05 13:20:57 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:13:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
