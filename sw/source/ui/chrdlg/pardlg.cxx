7c155174e107b37556bc6cb09f0b7f10fbcad8a4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:16:22 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:13:41 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/ui/chrdlg/pardlg.cxx
+++ sw/source/ui/chrdlg/pardlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pardlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 13:06:28 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:16:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
