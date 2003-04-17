ed49cad2fa9c78f3c70866146d7dea3c2fa3dd2e
Rüdiger Timm <rt@openoffice.org>
2003-04-17 14:14:10 +0000

    INTEGRATION: CWS vcl08 (1.1.1.1.114); FILE MERGED 2003/04/07 13:19:07 pl 1.1.1.1.114.1: #93756# start grid dialog on button click, allocator mismatch

--- extensions/source/scanner/grid.cxx
+++ extensions/source/scanner/grid.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: grid.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:16:52 $
+ *  last change: $Author: rt $ $Date: 2003-04-17 15:14:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -148,7 +148,7 @@ GridWindow::GridWindow(
 GridWindow::~GridWindow()
 {
     if( m_pNewYValues )
-        delete m_pNewYValues;
+        delete [] m_pNewYValues;
 }
 
 // ---------------------------------------------------------------------
@@ -355,9 +355,9 @@ void GridWindow::computeNew()
         }
     }
 
-    delete nodex;
-    delete nodey;
-    delete pList;
+    delete [] nodex;
+    delete [] nodey;
+    delete [] pList;
 }
 
 // ---------------------------------------------------------------------
