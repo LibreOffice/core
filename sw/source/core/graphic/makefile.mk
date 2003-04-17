9347daaab22fe06a0e0df88c006abd041a95cdf2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:09:51 +0000

    INTEGRATION: CWS os8 (1.5.160); FILE MERGED 2003/04/03 07:10:52 os 1.5.160.1: #108583# precompiled headers removed

--- sw/source/core/graphic/makefile.mk
+++ sw/source/core/graphic/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: jp $ $Date: 2001-10-18 12:12:58 $
+#   last change: $Author: vg $ $Date: 2003-04-17 14:09:51 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=graphic
 
 AUTOSEG=true
-
-PROJECTPCH=core_pch
-PDBTARGET=core_pch
-PROJECTPCHSOURCE=..\core_1st\core_pch
 
 # --- Settings -----------------------------------------------------
 
