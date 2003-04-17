ee31cfe532810522cb7a63cff04bd8b4aea48d35
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:30:20 +0000

    INTEGRATION: CWS os8 (1.4.142); FILE MERGED 2003/04/03 07:14:27 os 1.4.142.1: #108583# precompiled headers removed

--- sw/source/ui/frmdlg/frmdlg.cxx
+++ sw/source/ui/frmdlg/frmdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: frmdlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: os $ $Date: 2002-08-22 10:03:50 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:30:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
