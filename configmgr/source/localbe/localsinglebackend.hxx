6486f321569527959cf76d1e45f49c3a8ba6e969
Rüdiger Timm <rt@openoffice.org>
2003-04-17 12:30:34 +0000

    INTEGRATION: CWS configapi01 (1.5.2.2.14); FILE MERGED 2003/04/15 10:25:34 jb 1.5.2.2.14.4: #i11893# backend\updatedata.cxx 2003/04/11 11:58:36 jb 1.5.2.2.14.3: #107954# Revised exception specifications to allow backend accesses in XBackendEntities 2003/04/10 15:47:24 jb 1.5.2.2.14.2: #1077715# Move configuration backend API out of drafts; adjust to API changes 2003/04/01 16:30:03 cyrillem 1.5.2.2.14.1: #107957# #107960# Adaptation to new location of interfaces and exceptions

--- configmgr/source/localbe/localsinglebackend.hxx
+++ configmgr/source/localbe/localsinglebackend.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: localsinglebackend.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-19 16:19:20 $
+ *  last change: $Author: rt $ $Date: 2003-04-17 13:30:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,15 @@
 #ifndef CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
 #define CONFIGMGR_LOCALBE_LOCALSINGLEBACKEND_HXX_
 
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSINGLEBACKEND_HPP_
-#include <drafts/com/sun/star/configuration/backend/XSingleBackend.hpp>
-#endif // _COM_SUN_STAR_CONFIGURATION_BACKEND_XSINGLEBACKEND_HPP_
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSCHEMASUPPLIER_HPP_
+#include <com/sun/star/configuration/backend/XSchemaSupplier.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XMULTILAYERSTRATUM_HPP_
+#include <com/sun/star/configuration/backend/XMultiLayerStratum.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XBACKENDENTITIES_HPP_
+#include <com/sun/star/configuration/backend/XBackendEntities.hpp>
+#endif
 
 #ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
 #include <com/sun/star/uno/XComponentContext.hpp>
@@ -82,31 +88,30 @@
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #endif // _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 
-#ifndef _CPPUHELPER_COMPBASE3_HXX_
-#include <cppuhelper/compbase3.hxx>
-#endif // _CPPUHELPER_COMPBASE3_HXX_
-
 #ifndef _COM_SUN_STAR_CONFIGURATION_INVALIDBOOTSTRAPFILEEXCEPTION_HPP_
 #include <com/sun/star/configuration/InvalidBootstrapFileException.hpp>
 #endif
 
-#ifndef _DRAFTS_COM_SUN_STAR_CONFIGURATION_BACKEND_CANNOTCONNECTEXCEPTION_HPP_
-#include <drafts/com/sun/star/configuration/backend/CannotConnectException.hpp>
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_CANNOTCONNECTEXCEPTION_HPP_
+#include <com/sun/star/configuration/backend/CannotConnectException.hpp>
 #endif
+
+#ifndef _CPPUHELPER_COMPBASE5_HXX_
+#include <cppuhelper/compbase5.hxx>
+#endif // _CPPUHELPER_COMPBASE5_HXX_
 
 namespace configmgr { namespace localbe {
 
 namespace css = com::sun::star ;
 namespace uno = css::uno ;
 namespace lang = css::lang ;
-//namespace backend = css::configuration::backend ;
-namespace backend = drafts::com::sun::star::configuration::backend ;
+namespace backend = css::configuration::backend ;
 
-typedef cppu::WeakComponentImplHelper3<backend::XSingleBackend,
+typedef cppu::WeakComponentImplHelper5<backend::XSchemaSupplier,
+                                       backend::XMultiLayerStratum,
+                                       backend::XBackendEntities,
                                        lang::XInitialization,
                                        lang::XServiceInfo> SingleBackendBase ;
-
-class LocalFileLayer ;
 
 /**
   Implements the SingleBackend service for local file access.
@@ -119,75 +124,95 @@ class LocalSingleBackend : public SingleBackendBase {
 
           @param xFactory   service factory
           */
-        LocalSingleBackend(
-                const uno::Reference<uno::XComponentContext>& xContext) ;
+        LocalSingleBackend(const uno::Reference<uno::XComponentContext>& xContext) ;
+
         /** Destructor */
         ~LocalSingleBackend(void) ;
 
+
         // XInitialize
-        virtual void SAL_CALL initialize(
-                                const uno::Sequence<uno::Any>& aParameters)
-            throw (uno::RuntimeException, uno::Exception,
-                   css::configuration::InvalidBootstrapFileException,
-                   backend::CannotConnectException,
-                   backend::BackendSetupException);
-        // XSingleBackend
-        virtual uno::Sequence<rtl::OUString> SAL_CALL listLayerIds(
-                                                const rtl::OUString& aComponent,
-                                                const rtl::OUString& aEntity)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
-        virtual rtl::OUString SAL_CALL getUpdateLayerId(
-                                                const rtl::OUString& aComponent,
-                                                const rtl::OUString& aEntity)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
-        virtual rtl::OUString SAL_CALL getOwnId(void)
-            throw (uno::RuntimeException) ;
-        virtual uno::Reference<backend::XLayer> SAL_CALL getLayer(
-                                            const rtl::OUString& aLayerId,
-                                            const rtl::OUString& aTimestamp)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
+        virtual void SAL_CALL
+            initialize( const uno::Sequence<uno::Any>& aParameters)
+                throw (uno::RuntimeException, uno::Exception,
+                       css::configuration::InvalidBootstrapFileException,
+                       backend::CannotConnectException,
+                       backend::BackendSetupException);
+
+        // XSchemaSupplier
+        virtual uno::Reference<backend::XSchema> SAL_CALL
+            getComponentSchema( const rtl::OUString& aComponent )
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
+        // XMultiLayerStratum
+        virtual uno::Sequence<rtl::OUString> SAL_CALL
+            listLayerIds( const rtl::OUString& aComponent, const rtl::OUString& aEntity )
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
+        virtual rtl::OUString SAL_CALL
+            getUpdateLayerId( const rtl::OUString& aComponent, const rtl::OUString& aEntity )
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
+        virtual uno::Reference<backend::XLayer> SAL_CALL
+            getLayer( const rtl::OUString& aLayerId, const rtl::OUString& aTimestamp )
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
         virtual uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
             getLayers(const uno::Sequence<rtl::OUString>& aLayerIds,
                       const rtl::OUString& aTimestamp)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
         virtual uno::Sequence<uno::Reference<backend::XLayer> > SAL_CALL
             getMultipleLayers(const uno::Sequence<rtl::OUString>& aLayerIds,
                               const uno::Sequence<rtl::OUString>& aTimestamps)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
         virtual uno::Reference<backend::XUpdatableLayer> SAL_CALL
-            getUpdatableLayer(const rtl::OUString& aLayerId)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
-        virtual uno::Reference<backend::XLayerHandler> SAL_CALL
-            getWriteHandler(const rtl::OUString& aLayerId)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
-        virtual uno::Reference<backend::XSchema> SAL_CALL getSchema(
-                                            const rtl::OUString& aComponent)
-            throw (backend::BackendAccessException,
-                    lang::IllegalArgumentException,
-                    uno::RuntimeException) ;
+            getUpdatableLayer( const rtl::OUString& aLayerId )
+                throw (backend::BackendAccessException,
+                        lang::IllegalArgumentException,
+                        uno::RuntimeException) ;
+
+        // XBackendEntities
+        virtual rtl::OUString SAL_CALL
+            getOwnerEntity(  )
+                throw (uno::RuntimeException);
+
+        virtual rtl::OUString SAL_CALL
+            getAdminEntity(  )
+                throw (uno::RuntimeException);
+
+        virtual sal_Bool SAL_CALL
+            supportsEntity( const rtl::OUString& aEntity )
+                throw (backend::BackendAccessException, uno::RuntimeException);
+
+        virtual sal_Bool SAL_CALL
+            isEqualEntity( const rtl::OUString& aEntity, const rtl::OUString& aOtherEntity )
+                throw (backend::BackendAccessException, lang::IllegalArgumentException, uno::RuntimeException);
+
         // XServiceInfo
-        virtual rtl::OUString SAL_CALL getImplementationName(void)
-            throw (uno::RuntimeException) ;
-        virtual sal_Bool SAL_CALL supportsService(
-                                            const rtl::OUString& aServiceName)
-            throw (uno::RuntimeException) ;
+        virtual rtl::OUString SAL_CALL
+            getImplementationName(  )
+                throw (uno::RuntimeException) ;
+
+        virtual sal_Bool SAL_CALL
+            supportsService( const rtl::OUString& aServiceName )
+                throw (uno::RuntimeException) ;
+
         virtual uno::Sequence<rtl::OUString> SAL_CALL
-            getSupportedServiceNames(void)
-            throw (uno::RuntimeException) ;
+            getSupportedServiceNames(  )
+                throw (uno::RuntimeException) ;
 
         /**
           Provides the implementation name.
@@ -206,9 +231,9 @@ class LocalSingleBackend : public SingleBackendBase {
         /**
           Locates the main layer data and localized data directories in a layer directory hierarchy
           */
-        static void LocalSingleBackend::getLayerSubDirectories(rtl::OUString const & aLayerBaseUrl,
-                                     rtl::OUString& aMainLayerUrl,
-                                     rtl::OUString& aSubLayerUrl);
+        static void getLayerSubDirectories( rtl::OUString const & aLayerBaseUrl,
+                                             rtl::OUString& aMainLayerUrl,
+                                             rtl::OUString& aSubLayerUrl);
         /**
           Creates a simple readonly non-composite layer for a component in a base directory
           */
@@ -228,8 +253,11 @@ class LocalSingleBackend : public SingleBackendBase {
         uno::Reference<lang::XMultiServiceFactory> mFactory ;
         /** Mutex for resources protection */
         osl::Mutex mMutex ;
-        /** Base of the schema data */
-        rtl::OUString mSchemaDataUrl ;
+        /**
+          Base of the schema data. Is a list to allow
+          for multiple schema directories.
+          */
+        uno::Sequence<rtl::OUString> mSchemaDataUrls ;
         /**
           Base of the default data. Is a list to allow
           for multiple layers of default data.
@@ -237,6 +265,11 @@ class LocalSingleBackend : public SingleBackendBase {
         uno::Sequence<rtl::OUString> mDefaultDataUrls ;
         /** Base of the user data */
         rtl::OUString mUserDataUrl ;
+
+        /** special index for entity */
+        sal_Int32 findEntity(rtl::OUString const & _aEntity);
+        /** parse and translate layer-id */
+        sal_Int32 resolveLayerId(rtl::OUString const & _aLayerId, rtl::OUString & _aFile);
 
         /**
           Builds a LocalFileLayer object given a layer id.
@@ -250,7 +283,7 @@ class LocalSingleBackend : public SingleBackendBase {
           @throws com::sun::star::lang::IllegalArgumentException
                   if the layer id is invalid.
           */
-        LocalFileLayer *getFileLayer(const rtl::OUString& aLayerId)
+        uno::Reference<backend::XUpdatableLayer> getFileLayer(const rtl::OUString& aLayerId)
             throw (lang::IllegalArgumentException) ;
         /**
           Same as above, but using a component URL and layer index
@@ -260,8 +293,8 @@ class LocalSingleBackend : public SingleBackendBase {
           @param aLayerIndex    layer index
           @return   local file layer
           */
-        LocalFileLayer *getFileLayer(const rtl::OUString& aComponent,
-                                     sal_Int32 aLayerIndex) ;
+        uno::Reference<backend::XUpdatableLayer> getFileLayer(const rtl::OUString& aComponent,
+                                                                sal_Int32 aLayerIndex) ;
         /**
           Maps a layer index (-1 for user layer, 0-x for defaults)
           to the appropriate layer and sublayers base directories.
