0db1a5510ad4adcfcc7dab6fb9247520ee7ff652
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:10:46 +0000

    INTEGRATION: CWS os8 (1.16.22.1.48); FILE MERGED 2003/04/03 07:13:28 os 1.16.22.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/app/applab.cxx
+++ sw/source/ui/app/applab.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: applab.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:42:31 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:10:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
