fed927df3423e0b24465ed2abda7babec3d065d0
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:22:05 +0000

    INTEGRATION: CWS os8 (1.11.14.1.58); FILE MERGED 2003/04/03 07:13:57 os 1.11.14.1.58.1: #108583# precompiled headers removed

--- sw/source/ui/dochdl/gloshdl.cxx
+++ sw/source/ui/dochdl/gloshdl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: gloshdl.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:43:00 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:22:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 #ifndef _HINTIDS_HXX
