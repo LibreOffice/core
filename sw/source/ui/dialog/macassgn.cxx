1493f6cf61e1ef439d3ffa5106758d1db0866ce1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:21:31 +0000

    INTEGRATION: CWS os8 (1.3.138); FILE MERGED 2003/04/03 07:13:55 os 1.3.138.1: #108583# precompiled headers removed

--- sw/source/ui/dialog/macassgn.cxx
+++ sw/source/ui/dialog/macassgn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: macassgn.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:38:52 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:21:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
