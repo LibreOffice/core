ff0d0b991513fd5d0b176ebd5a0abc1feb3c8a92
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:00:41 +0000

    INTEGRATION: CWS os8 (1.5.138); FILE MERGED 2003/04/03 07:10:29 os 1.5.138.1: #108583# precompiled headers removed

--- sw/source/core/edit/edfld.cxx
+++ sw/source/core/edit/edfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edfld.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: os $ $Date: 2002-11-29 12:09:16 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:00:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
