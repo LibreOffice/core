aa787dccab816e8e843b2a0de538565b32e11861
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:57:14 +0000

    INTEGRATION: CWS os8 (1.4.160); FILE MERGED 2003/04/03 07:15:52 os 1.4.160.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/uiitems.cxx
+++ sw/source/ui/utlui/uiitems.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: uiitems.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:18:22 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:57:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
