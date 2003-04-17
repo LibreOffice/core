cfdca3488f970c7433274f62486691fc6b9a7367
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:54:17 +0000

    INTEGRATION: CWS os8 (1.4.170); FILE MERGED 2003/04/03 07:10:07 os 1.4.170.1: #108583# precompiled headers removed

--- sw/source/core/doc/sortopt.cxx
+++ sw/source/core/doc/sortopt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sortopt.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: jp $ $Date: 2001-04-06 08:57:53 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:54:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
