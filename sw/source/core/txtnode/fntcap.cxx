1fd35ca64ad390b7ab9fdba7a1bd341cb921659b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:34:48 +0000

    INTEGRATION: CWS os8 (1.12.10.1.58); FILE MERGED 2003/04/03 07:11:56 os 1.12.10.1.58.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/fntcap.cxx
+++ sw/source/core/txtnode/fntcap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fntcap.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 15:32:19 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:34:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
