c97480fc8b6cf923a31737f7cbc0bd14dd75e42a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:57:23 +0000

    INTEGRATION: CWS os8 (1.10.142); FILE MERGED 2003/04/03 07:15:53 os 1.10.142.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/uitool.cxx
+++ sw/source/ui/utlui/uitool.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: uitool.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: os $ $Date: 2002-09-09 09:04:03 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:57:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
