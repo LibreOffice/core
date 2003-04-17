c4e96e74bfea82bb4001797b9569436c50a6282b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:30:49 +0000

    INTEGRATION: CWS os8 (1.28.2.1.58); FILE MERGED 2003/04/03 07:11:43 os 1.28.2.1.58.1: #108583# precompiled headers removed

--- sw/source/core/text/txtftn.cxx
+++ sw/source/core/text/txtftn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtftn.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:56:28 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:30:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
