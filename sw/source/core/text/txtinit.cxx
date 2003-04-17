f6f07254aacfcc2522ac9ec4df0dd90ebaf58ed9
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:31:14 +0000

    INTEGRATION: CWS os8 (1.3.148); FILE MERGED 2003/04/03 07:11:44 os 1.3.148.1: #108583# precompiled headers removed

--- sw/source/core/text/txtinit.cxx
+++ sw/source/core/text/txtinit.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtinit.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: os $ $Date: 2002-04-12 10:33:47 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:31:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
