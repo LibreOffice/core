27d4eb20e0d284523b83429827121c7ed027fe1b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:14:24 +0000

    INTEGRATION: CWS os8 (1.24.2.2.48); FILE MERGED 2003/04/03 07:11:01 os 1.24.2.2.48.1: #108583# precompiled headers removed

--- sw/source/core/layout/pagechg.cxx
+++ sw/source/core/layout/pagechg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pagechg.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:40:28 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:14:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
