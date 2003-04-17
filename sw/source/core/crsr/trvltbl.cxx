b1150d8c6bad294d3b65414ae34d8f48e4f69325
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:47:18 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:09:46 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/core/crsr/trvltbl.cxx
+++ sw/source/core/crsr/trvltbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: trvltbl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: fme $ $Date: 2002-11-01 13:28:21 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:47:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
