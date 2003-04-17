c82d7d1b621e42214eb40f4542b1248304b37663
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:21:54 +0000

    INTEGRATION: CWS os8 (1.18.8.6.142); FILE MERGED 2003/04/03 07:13:56 os 1.18.8.6.142.1: #108583# precompiled headers removed

--- sw/source/ui/dialog/regionsw.cxx
+++ sw/source/ui/dialog/regionsw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: regionsw.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:42:55 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:21:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
