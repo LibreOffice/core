6fc1f1a24c221dedc4fd121f213a3dddddfe15d9
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:04:05 +0000

    INTEGRATION: CWS os8 (1.19.2.1.70); FILE MERGED 2003/04/03 07:10:37 os 1.19.2.1.70.1: #108583# precompiled headers removed

--- sw/source/core/fields/authfld.cxx
+++ sw/source/core/fields/authfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: authfld.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:57 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:04:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
