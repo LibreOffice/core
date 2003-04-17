51b947701fd23d1d9262776d4beeb36bf9ae44d4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:08:11 +0000

    INTEGRATION: CWS os8 (1.8.6.1.48); FILE MERGED 2003/04/03 07:10:47 os 1.8.6.1.48.1: #108583# precompiled headers removed

--- sw/source/core/frmedt/fefly1.cxx
+++ sw/source/core/frmedt/fefly1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fefly1.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:40:01 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:08:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
