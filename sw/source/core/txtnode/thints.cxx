4c673f355263597dc82b8b80b94cb1a915245052
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:35:58 +0000

    INTEGRATION: CWS os8 (1.28.138); FILE MERGED 2003/04/03 07:11:59 os 1.28.138.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/thints.cxx
+++ sw/source/core/txtnode/thints.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: thints.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:57:36 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:35:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
