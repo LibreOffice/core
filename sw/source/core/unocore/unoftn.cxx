b7683da3b85953384e13c6d413c611c3e70c4500
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:43:24 +0000

    INTEGRATION: CWS os8 (1.18.4.2.48); FILE MERGED 2003/04/07 22:08:27 os 1.18.4.2.48.2: RESYNC: (1.18.4.2-1.20); FILE MERGED 2003/04/03 07:12:17 os 1.18.4.2.48.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoftn.cxx
+++ sw/source/core/unocore/unoftn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoftn.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:34:13 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:43:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
