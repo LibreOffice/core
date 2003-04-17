5811383b8b4b2686bdaa1c1b6283ecd2ab536632
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:21:20 +0000

    INTEGRATION: CWS os8 (1.5.138); FILE MERGED 2003/04/03 07:13:55 os 1.5.138.1: #108583# precompiled headers removed

--- sw/source/ui/dialog/docstdlg.cxx
+++ sw/source/ui/dialog/docstdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docstdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: os $ $Date: 2002-12-04 12:18:40 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:21:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
