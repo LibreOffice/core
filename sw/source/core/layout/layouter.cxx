564017141715bde3d1970ab76a073c8e4000450a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:13:47 +0000

    INTEGRATION: CWS os8 (1.2.170); FILE MERGED 2003/04/03 07:11:00 os 1.2.170.1: #108583# precompiled headers removed

--- sw/source/core/layout/layouter.cxx
+++ sw/source/core/layout/layouter.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: layouter.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:51:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:13:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
