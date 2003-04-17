f8ae5f01eb31fbba31bdf9fb94412e28a6a422e8
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:50:17 +0000

    INTEGRATION: CWS os8 (1.9.158); FILE MERGED 2003/04/03 07:09:56 os 1.9.158.1: #108583# precompiled headers removed

--- sw/source/core/doc/docglbl.cxx
+++ sw/source/core/doc/docglbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docglbl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: jp $ $Date: 2001-10-25 17:21:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:50:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
