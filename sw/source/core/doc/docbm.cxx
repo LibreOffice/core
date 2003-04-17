eaf749475488bf7c34a00e2d62f3b988047c5dae
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:48:21 +0000

    INTEGRATION: CWS os8 (1.3.174); FILE MERGED 2003/04/03 07:09:52 os 1.3.174.1: #108583# precompiled headers removed

--- sw/source/core/doc/docbm.cxx
+++ sw/source/core/doc/docbm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docbm.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:48:48 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:48:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
