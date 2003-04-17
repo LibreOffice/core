5af5d2e52149f5b52ec0beb310dd4fa6248ee1de
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:55:41 +0000

    INTEGRATION: CWS os8 (1.1.1.1.182); FILE MERGED 2003/04/03 07:10:15 os 1.1.1.1.182.1: #108583# precompiled headers removed

--- sw/source/core/docnode/makefile.mk
+++ sw/source/core/docnode/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1.1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: hr $ $Date: 2000-09-19 00:08:17 $
+#   last change: $Author: vg $ $Date: 2003-04-17 13:55:41 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=docnode
 
 AUTOSEG=true
-
-PROJECTPCH=core_pch
-PDBTARGET=core_pch
-PROJECTPCHSOURCE=..$/core_1st$/core_pch
 
 # --- Settings -----------------------------------------------------
 
