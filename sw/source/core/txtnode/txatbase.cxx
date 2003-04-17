390ee7c9969673f6c0cd80f844373041cfbee53f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:36:11 +0000

    INTEGRATION: CWS os8 (1.6.160); FILE MERGED 2003/04/03 07:11:59 os 1.6.160.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/txatbase.cxx
+++ sw/source/core/txtnode/txatbase.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txatbase.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: jp $ $Date: 2001-08-27 15:51:01 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:36:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
