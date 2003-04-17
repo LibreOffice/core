3c8547e8b6c6fcfb4c1f5040d24e983674d0cbb7
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:52:13 +0000

    INTEGRATION: CWS os8 (1.16.4.1.96); FILE MERGED 2003/04/03 07:15:33 os 1.16.4.1.96.1: #108583# precompiled headers removed

--- sw/source/ui/uiview/viewtab.cxx
+++ sw/source/ui/uiview/viewtab.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewtab.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:16:51 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:52:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 
 #pragma hdrstop
