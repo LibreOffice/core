727971029bd234046c47740ad188c5d50804ea60
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:44:33 +0000

    INTEGRATION: CWS os8 (1.21.12.4.140); FILE MERGED 2003/04/03 07:12:19 os 1.21.12.4.140.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoport.cxx
+++ sw/source/core/unocore/unoport.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoport.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:41:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:44:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
