5135910ec3e4d3b8c365114d651e0f42f8885b0b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:10:21 +0000

    INTEGRATION: CWS os8 (1.13.10.2.46); FILE MERGED 2003/04/07 21:56:27 os 1.13.10.2.46.2: RESYNC: (1.13.10.2-1.15); FILE MERGED 2003/04/03 07:13:27 os 1.13.10.2.46.1: #108583# precompiled headers removed

--- sw/source/ui/app/appenv.cxx
+++ sw/source/ui/app/appenv.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: appenv.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:14:29 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:10:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
