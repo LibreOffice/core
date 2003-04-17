2892ad5d9ad331b65f7c9404f6b2703c9b70db36
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:39:39 +0000

    INTEGRATION: CWS os8 (1.6.8.1.48); FILE MERGED 2003/04/03 07:09:25 os 1.6.8.1.48.1: #108583# precompiled headers removed

--- sw/source/core/access/acctextframe.cxx
+++ sw/source/core/access/acctextframe.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: acctextframe.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:22 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:39:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
