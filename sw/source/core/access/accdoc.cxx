e861a275f7b382588c6777668dce73185a980628
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:34:53 +0000

    INTEGRATION: CWS os8 (1.19.4.1.48); FILE MERGED 2003/04/03 07:09:19 os 1.19.4.1.48.1: #108583# precompiled headers removed

--- sw/source/core/access/accdoc.cxx
+++ sw/source/core/access/accdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accdoc.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:16 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:34:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #ifndef _SV_WINDOW_HXX
 #include <vcl/window.hxx>
