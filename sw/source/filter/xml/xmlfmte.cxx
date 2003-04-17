5b59f774e40601610e3179094699687a8b055064
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:07:26 +0000

    INTEGRATION: CWS os8 (1.36.138); FILE MERGED 2003/04/03 07:13:20 os 1.36.138.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmlfmte.cxx
+++ sw/source/filter/xml/xmlfmte.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlfmte.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: fs $ $Date: 2002-10-28 10:08:47 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:07:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
