935bc8d43bce6d571b36a8ff73bf1050e51de5e7
Rüdiger Timm <rt@openoffice.org>
2003-04-17 13:19:39 +0000

    INTEGRATION: CWS configapi01 (1.1.2); FILE ADDED 2003/04/15 08:18:56 jb 1.1.2.3: #107715# Add new service descriptions 2003/04/10 14:18:23 jb 1.1.2.2: #107716# Add new interfaces and services moved from drafts 2003/04/01 14:41:39 cyrillem 1.1.2.1: #107960# Schema and layer related interfaces and exceptions

---
+++ offapi/com/sun/star/configuration/backend/makefile.mk
@@ -0,0 +1,118 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2003-04-17 14:19:39 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..$/..$/..
+PRJNAME = offapi
+
+TARGET = csscbackend
+PACKAGE = com$/sun$/star$/configuration$/backend
+
+.INCLUDE: $(PRJ)$/util$/makefile.pmk
+
+IDLFILES = \
+    Backend.idl				\
+    SingleBackend.idl	    \
+    LocalSingleBackend.idl	\
+    LdapSingleBackend.idl	\
+    BackendAdapter.idl		\
+    SingleBackendAdapter.idl \
+    OfflineBackend.idl		\
+    OnlineBackend.idl		\
+    HierarchyBrowser.idl	\
+    LocalHierarchyBrowser.idl	\
+    DataImporter.idl	\
+    LocalDataImporter.idl	\
+    LayerUpdateMerger.idl	\
+    Importer.idl			\
+    MergeImporter.idl		\
+    CopyImporter.idl		\
+    Schema.idl				\
+    Layer.idl				\
+    UpdatableLayer.idl      \
+    XBackend.idl			\
+    XBackendEntities.idl	\
+    XSchemaSupplier.idl		\
+    XSingleLayerStratum.idl	\
+    XMultiLayerStratum.idl	\
+    XSchema.idl				\
+    XLayer.idl				\
+    XCompositeLayer.idl     \
+    XUpdatableLayer.idl     \
+    XSchemaHandler.idl		\
+    XLayerHandler.idl		\
+    XUpdateHandler.idl      \
+    XLayerImporter.idl      \
+    TemplateIdentifier.idl  \
+    SchemaAttribute.idl     \
+    NodeAttribute.idl       \
+    MalformedDataException.idl	\
+    BackendSetupException.idl	\
+    AuthenticationFailedException.idl	\
+    CannotConnectException.idl	\
+    InvalidAuthenticationMechanismException.idl \
+    BackendAccessException.idl	\
+    ConnectionLostException.idl	\
+    InsufficientAccessRightsException.idl	\
+
+
+.INCLUDE: target.mk
+.INCLUDE: $(PRJ)$/util$/target.pmk
