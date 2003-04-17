5701094f1932bf3a1c46b17c98d18184ee9a6e79
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:08:38 +0000

    INTEGRATION: CWS os8 (1.10.138); FILE MERGED 2003/04/07 21:58:26 os 1.10.138.2: RESYNC: (1.10-1.11); FILE MERGED 2003/04/03 07:13:22 os 1.10.138.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmlmeta.cxx
+++ sw/source/filter/xml/xmlmeta.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlmeta.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:38:44 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:08:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
