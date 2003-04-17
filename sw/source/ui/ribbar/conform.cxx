91cca5e53a59a39d0136033d15465fd7334248ca
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:38:37 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:14:52 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/ui/ribbar/conform.cxx
+++ sw/source/ui/ribbar/conform.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: conform.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: os $ $Date: 2002-10-25 13:09:48 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:38:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
