dbd3ca46cab2acb365994f137b1450221f5438b1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:49:18 +0000

    INTEGRATION: CWS os8 (1.14.138); FILE MERGED 2003/04/07 22:19:26 os 1.14.138.2: RESYNC: (1.14-1.15); FILE MERGED 2003/04/03 07:09:54 os 1.14.138.1: #108583# precompiled headers removed

--- sw/source/core/doc/docdraw.cxx
+++ sw/source/core/doc/docdraw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docdraw.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 09:52:49 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:49:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
