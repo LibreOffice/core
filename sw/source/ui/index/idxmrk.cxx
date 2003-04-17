d425eafb9de17db7f0bf7829300147b428b8de91
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:33:22 +0000

    INTEGRATION: CWS os8 (1.26.140); FILE MERGED 2003/04/03 07:14:34 os 1.26.140.1: #108583# precompiled headers removed

--- sw/source/ui/index/idxmrk.cxx
+++ sw/source/ui/index/idxmrk.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: idxmrk.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 18:16:38 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:33:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
