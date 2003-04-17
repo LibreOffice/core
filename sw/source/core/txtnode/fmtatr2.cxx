5a7832e553ee8471cb93cd774eda233ebd22fa1e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:34:26 +0000

    INTEGRATION: CWS os8 (1.19.140); FILE MERGED 2003/04/07 22:09:46 os 1.19.140.2: RESYNC: (1.19-1.20); FILE MERGED 2003/04/03 07:11:55 os 1.19.140.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/fmtatr2.cxx
+++ sw/source/core/txtnode/fmtatr2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtatr2.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:32:17 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:34:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
