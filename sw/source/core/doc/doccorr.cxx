79f116650299392c0a41edf46f1c06a268b6cd67
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:48:49 +0000

    INTEGRATION: CWS os8 (1.3.138); FILE MERGED 2003/04/03 07:09:53 os 1.3.138.1: #108583# precompiled headers removed

--- sw/source/core/doc/doccorr.cxx
+++ sw/source/core/doc/doccorr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: doccorr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hbrinkm $ $Date: 2002-11-21 14:44:36 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:48:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
