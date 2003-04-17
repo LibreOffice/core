ed2c1642ef6cbc28ec20bc610347475eb68e896e
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:36:30 +0000

    INTEGRATION: CWS os8 (1.4.146); FILE MERGED 2003/04/03 07:14:44 os 1.4.146.1: #108583# precompiled headers removed

--- sw/source/ui/misc/insfnote.cxx
+++ sw/source/ui/misc/insfnote.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: insfnote.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: mba $ $Date: 2002-07-01 08:59:56 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:36:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
