ece74c666e712076571631db173ab820322a03a3
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:44:10 +0000

    INTEGRATION: CWS os8 (1.31.2.2.48); FILE MERGED 2003/04/07 22:08:50 os 1.31.2.2.48.2: RESYNC: (1.31.2.2-1.34); FILE MERGED 2003/04/03 07:12:18 os 1.31.2.2.48.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoobj2.cxx
+++ sw/source/core/unocore/unoobj2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoobj2.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:58:31 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:44:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
