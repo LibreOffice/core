e72acf3ce3a302dc7cec70cefc22fb2591f3725a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:40:47 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:12:10 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/core/undo/untbl.cxx
+++ sw/source/core/undo/untbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: untbl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:13:56 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:40:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
