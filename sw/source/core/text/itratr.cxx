0ab856ccc0beec5ca8b35b94872e15f26d40bd71
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:26:57 +0000

    INTEGRATION: CWS os8 (1.22.148); FILE MERGED 2003/04/07 22:10:58 os 1.22.148.2: RESYNC: (1.22-1.23); FILE MERGED 2003/04/03 07:11:34 os 1.22.148.1: #108583# precompiled headers removed

--- sw/source/core/text/itratr.cxx
+++ sw/source/core/text/itratr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: itratr.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:53:50 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:26:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
