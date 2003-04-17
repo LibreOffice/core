20afacd6f480b2bea176e623e23333692d0397db
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:23:52 +0000

    INTEGRATION: CWS os8 (1.13.138); FILE MERGED 2003/04/03 07:14:01 os 1.13.138.1: #108583# precompiled headers removed

--- sw/source/ui/docvw/romenu.cxx
+++ sw/source/ui/docvw/romenu.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: romenu.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2002-12-03 10:42:05 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:23:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
