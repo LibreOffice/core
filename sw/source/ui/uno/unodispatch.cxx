3e04b3e214e3120327636c1145eac8789fc13081
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:53:46 +0000

    INTEGRATION: CWS os8 (1.12.142); FILE MERGED 2003/04/03 07:15:40 os 1.12.142.1: #108583# precompiled headers removed

--- sw/source/ui/uno/unodispatch.cxx
+++ sw/source/ui/uno/unodispatch.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unodispatch.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: os $ $Date: 2002-09-09 07:03:11 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:53:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
