eba92a8aee0bd150e6b304ccd139ac686dc2d7c9
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:44:21 +0000

    INTEGRATION: CWS os8 (1.26.140); FILE MERGED 2003/04/03 07:12:19 os 1.26.140.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoparagraph.cxx
+++ sw/source/core/unocore/unoparagraph.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoparagraph.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: cmc $ $Date: 2002-10-16 09:19:04 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:44:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
