1ae7b0f42c4d7f4748254ee7976643361fd3a571
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:07:15 +0000

    INTEGRATION: CWS os8 (1.23.138); FILE MERGED 2003/04/07 21:58:08 os 1.23.138.2: RESYNC: (1.23-1.24); FILE MERGED 2003/04/03 07:13:19 os 1.23.138.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmlfmt.cxx
+++ sw/source/filter/xml/xmlfmt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlfmt.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:38:21 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:07:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
