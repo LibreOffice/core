6dff005e7ea6915b0d6460bdc2fca2d6d8d64ca8
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:31:07 +0000

    INTEGRATION: CWS os8 (1.2.38.1.76); FILE MERGED 2003/04/03 07:14:29 os 1.2.38.1.76.1: #108583# precompiled headers removed

--- sw/source/ui/frmdlg/makefile.mk
+++ sw/source/ui/frmdlg/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-27 15:43:35 $
+#   last change: $Author: vg $ $Date: 2003-04-17 15:31:07 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=frmdlg
 
 AUTOSEG=true
-
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\ui_1st\ui_pch
 
 # --- Settings -----------------------------------------------------
 
