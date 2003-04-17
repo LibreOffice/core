707838c48bbb6834dc83ff2718b3d1b57d98e05c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:26:45 +0000

    INTEGRATION: CWS os8 (1.9.148); FILE MERGED 2003/04/03 07:14:11 os 1.9.148.1: #108583# precompiled headers removed

--- sw/source/ui/fldui/changedb.cxx
+++ sw/source/ui/fldui/changedb.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: changedb.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: os $ $Date: 2002-06-06 13:12:58 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:26:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
