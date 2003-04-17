cea44befe5e4abe528836b9463f0ea8bd52ccd90
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:03:17 +0000

    INTEGRATION: CWS os8 (1.4.160); FILE MERGED 2003/04/03 07:10:33 os 1.4.160.1: #108583# precompiled headers removed

--- sw/source/core/edit/makefile.mk
+++ sw/source/core/edit/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: jp $ $Date: 2001-10-18 12:18:12 $
+#   last change: $Author: vg $ $Date: 2003-04-17 14:03:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=edit
 
 AUTOSEG=true
-
-PROJECTPCH=core_pch
-PDBTARGET=core_pch
-PROJECTPCHSOURCE=..$/core_1st$/core_pch
 
 # --- Settings -----------------------------------------------------
 
