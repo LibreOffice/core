1292d0b5526893683c7c4a581111495e791a368a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:01:37 +0000

    INTEGRATION: CWS os8 (1.4.178); FILE MERGED 2003/04/03 07:10:31 os 1.4.178.1: #108583# precompiled headers removed

--- sw/source/core/edit/edlingu.cxx
+++ sw/source/core/edit/edlingu.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edlingu.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:50:05 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:01:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
