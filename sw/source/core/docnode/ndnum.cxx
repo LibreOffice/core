922669fe31474449dba0ecad86657ecdbf43f010
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:56:43 +0000

    INTEGRATION: CWS os8 (1.4.138); FILE MERGED 2003/04/03 07:10:17 os 1.4.138.1: #108583# precompiled headers removed

--- sw/source/core/docnode/ndnum.cxx
+++ sw/source/core/docnode/ndnum.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ndnum.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: od $ $Date: 2002-12-09 10:20:11 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:56:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
