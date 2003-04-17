4fa07db7b7f4ae7d777ffc4a181254dcbeb8c580
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:22:54 +0000

    INTEGRATION: CWS os8 (1.14.138); FILE MERGED 2003/04/03 07:13:59 os 1.14.138.1: #108583# precompiled headers removed

--- sw/source/ui/docvw/edtdd.cxx
+++ sw/source/ui/docvw/edtdd.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: edtdd.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 13:05:32 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:22:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
