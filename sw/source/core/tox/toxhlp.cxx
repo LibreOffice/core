161c83355d6edee30ef6eb45f13b5fd5738216ce
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:32:43 +0000

    INTEGRATION: CWS os8 (1.4.142); FILE MERGED 2003/04/03 07:11:52 os 1.4.142.1: #108583# precompiled headers removed

--- sw/source/core/tox/toxhlp.cxx
+++ sw/source/core/tox/toxhlp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: toxhlp.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: fme $ $Date: 2002-08-23 08:13:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:32:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
