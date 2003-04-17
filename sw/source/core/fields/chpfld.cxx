505222ed26893c056f026b010278dca9d311787f
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:04:28 +0000

    INTEGRATION: CWS os8 (1.5.148); FILE MERGED 2003/04/03 07:10:38 os 1.5.148.1: #108583# precompiled headers removed

--- sw/source/core/fields/chpfld.cxx
+++ sw/source/core/fields/chpfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: chpfld.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mba $ $Date: 2002-05-27 14:32:25 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:04:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
