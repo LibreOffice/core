b7a931af5d84242f1d91a31ffafa0360c2fed7d8
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:37:28 +0000

    INTEGRATION: CWS os8 (1.5.158); FILE MERGED 2003/04/03 07:12:04 os 1.5.158.1: #108583# precompiled headers removed

--- sw/source/core/undo/rolbck.cxx
+++ sw/source/core/undo/rolbck.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rolbck.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: jp $ $Date: 2001-10-31 20:49:00 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:37:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
