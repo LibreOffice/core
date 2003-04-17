15208586e462e0cb5b21ba8894c13d6d63f85654
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:34:26 +0000

    INTEGRATION: CWS os8 (1.10.2.1.48); FILE MERGED 2003/04/03 07:09:18 os 1.10.2.1.48.1: #108583# precompiled headers removed

--- sw/source/core/access/acccell.cxx
+++ sw/source/core/access/acccell.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: acccell.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:15 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:34:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
