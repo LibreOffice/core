d59d4a3dc034d042e61c3fda997ce45c55d7abe2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:50:37 +0000

    INTEGRATION: CWS os8 (1.2.138); FILE MERGED 2003/04/03 07:15:29 os 1.2.138.1: #108583# precompiled headers removed

--- sw/source/ui/uiview/viewdlg.cxx
+++ sw/source/ui/uiview/viewdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewdlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-12-10 14:26:22 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:50:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
