dbe93901650dc216ede7346621970b08967e0152
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:45:37 +0000

    INTEGRATION: CWS os8 (1.1.1.1.182); FILE MERGED 2003/04/03 07:09:42 os 1.1.1.1.182.1: #108583# precompiled headers removed

--- sw/source/core/crsr/findfmt.cxx
+++ sw/source/core/crsr/findfmt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: findfmt.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-19 00:08:17 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:45:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
