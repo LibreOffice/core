10bb376790f7e2284231f0a754b45cfed703347a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:59:53 +0000

    INTEGRATION: CWS os8 (1.3.32.1.96); FILE MERGED 2003/04/03 07:13:00 os 1.3.32.1.96.1: #108583# precompiled headers removed

--- sw/source/filter/rtf/rtfnum.cxx
+++ sw/source/filter/rtf/rtfnum.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rtfnum.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 08:43:04 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:59:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
