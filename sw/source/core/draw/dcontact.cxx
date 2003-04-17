4c29c6d93869abb08ec779ed3c045409377058db
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:58:22 +0000

    INTEGRATION: CWS os8 (1.15.138); FILE MERGED 2003/04/03 07:10:23 os 1.15.138.1: #108583# precompiled headers removed

--- sw/source/core/draw/dcontact.cxx
+++ sw/source/core/draw/dcontact.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dcontact.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: fme $ $Date: 2002-10-29 08:55:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:58:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
