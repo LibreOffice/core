3544e66b8cd5676b45fd80e3ccd8591b0ac08731
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:37:16 +0000

    INTEGRATION: CWS os8 (1.39.6.2.54); FILE MERGED 2003/04/07 22:22:24 os 1.39.6.2.54.2: RESYNC: (1.39.6.2-1.41); FILE MERGED 2003/04/03 07:09:23 os 1.39.6.2.54.1: #108583# precompiled headers removed

--- sw/source/core/access/accmap.cxx
+++ sw/source/core/access/accmap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accmap.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:48:15 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:37:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
