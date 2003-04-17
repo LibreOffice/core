0ca5cd78df0c73cbec0a68b5dd430bd5581431fa
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:09:53 +0000

    INTEGRATION: CWS os8 (1.28.138); FILE MERGED 2003/04/07 21:58:59 os 1.28.138.2: RESYNC: (1.28-1.29); FILE MERGED 2003/04/03 07:13:24 os 1.28.138.1: #108583# precompiled headers removed

--- sw/source/filter/xml/xmltexti.cxx
+++ sw/source/filter/xml/xmltexti.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmltexti.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:39:28 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:09:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "filt_pch.hxx"
-#endif
 
 #pragma hdrstop
 
