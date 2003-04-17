edb324aaaa6e7c060eb86f791c94716b441f02ba
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:42:44 +0000

    INTEGRATION: CWS os8 (1.1.176); FILE MERGED 2003/04/03 07:12:16 os 1.1.176.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoevtlstnr.cxx
+++ sw/source/core/unocore/unoevtlstnr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoevtlstnr.cxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: os $ $Date: 2000-12-19 15:49:21 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:42:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
