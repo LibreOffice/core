5278e51024ddf003218eff29d374c25a109d323c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:52:46 +0000

    INTEGRATION: CWS os8 (1.3.144); FILE MERGED 2003/04/03 07:15:38 os 1.3.144.1: #108583# precompiled headers removed

--- sw/source/ui/uno/SwXFilterOptions.cxx
+++ sw/source/ui/uno/SwXFilterOptions.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXFilterOptions.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: mav $ $Date: 2002-08-13 07:33:55 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:52:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
