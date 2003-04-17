fd6d10f48323ed299593c55e2ac71278ea79c34a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:34:37 +0000

    INTEGRATION: CWS os8 (1.64.2.3.18); FILE MERGED 2003/04/07 22:09:51 os 1.64.2.3.18.2: RESYNC: (1.64.2.3-1.66); FILE MERGED 2003/04/03 07:11:56 os 1.64.2.3.18.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/fntcache.cxx
+++ sw/source/core/txtnode/fntcache.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fntcache.cxx,v $
  *
- *  $Revision: 1.68 $
+ *  $Revision: 1.69 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 10:13:45 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:34:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
