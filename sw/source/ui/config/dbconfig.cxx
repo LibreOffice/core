10a5b91154446b16f526abfef8f09c17191edcdb
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:17:30 +0000

    INTEGRATION: CWS os8 (1.3.138); FILE MERGED 2003/04/03 07:13:44 os 1.3.138.1: #108583# precompiled headers removed

--- sw/source/ui/config/dbconfig.cxx
+++ sw/source/ui/config/dbconfig.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbconfig.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: os $ $Date: 2002-11-29 12:10:02 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:17:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
