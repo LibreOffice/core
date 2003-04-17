f00ec3322a8b85c4177a881c3449e50dcfa5131c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:28:23 +0000

    INTEGRATION: CWS os8 (1.16.6.1.58); FILE MERGED 2003/04/07 22:11:48 os 1.16.6.1.58.2: RESYNC: (1.16.6.1-1.18); FILE MERGED 2003/04/03 07:11:38 os 1.16.6.1.58.1: #108583# precompiled headers removed

--- sw/source/core/text/porfly.cxx
+++ sw/source/core/text/porfly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: porfly.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 09:57:26 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:28:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
