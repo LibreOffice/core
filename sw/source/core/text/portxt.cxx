50ddbe36c7339c903c7ff3eadccb58b933f1dacc
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:29:39 +0000

    INTEGRATION: CWS os8 (1.29.4.2.20); FILE MERGED 2003/04/03 07:11:40 os 1.29.4.2.20.1: #108583# precompiled headers removed

--- sw/source/core/text/portxt.cxx
+++ sw/source/core/text/portxt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: portxt.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:55:18 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:29:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
