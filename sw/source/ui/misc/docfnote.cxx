ac45bac7f3a672f150f8e073c43049300cbab9d1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:35:22 +0000

    INTEGRATION: CWS os8 (1.10.138); FILE MERGED 2003/04/03 07:14:42 os 1.10.138.1: #108583# precompiled headers removed

--- sw/source/ui/misc/docfnote.cxx
+++ sw/source/ui/misc/docfnote.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docfnote.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:45:03 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:35:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
