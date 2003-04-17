72d5a309fde1227f616dff7d3d624283c926269a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:55:07 +0000

    INTEGRATION: CWS os8 (1.25.138); FILE MERGED 2003/04/03 07:15:46 os 1.25.138.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/content.cxx
+++ sw/source/ui/utlui/content.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: content.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:04:47 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:55:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
