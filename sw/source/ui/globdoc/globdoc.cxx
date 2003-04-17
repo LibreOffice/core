a06d4d485c12d73efd7ea4b7caed3b5bacbb1f64
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:31:51 +0000

    INTEGRATION: CWS os8 (1.6.138); FILE MERGED 2003/04/03 07:14:32 os 1.6.138.1: #108583# precompiled headers removed

--- sw/source/ui/globdoc/globdoc.cxx
+++ sw/source/ui/globdoc/globdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: globdoc.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 13:07:08 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:31:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
