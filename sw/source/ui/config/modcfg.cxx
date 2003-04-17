f7b9801e0baec0a3cf658d5c48b303c1277df230
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:18:03 +0000

    INTEGRATION: CWS os8 (1.18.160); FILE MERGED 2003/04/03 07:13:44 os 1.18.160.1: #108583# precompiled headers removed

--- sw/source/ui/config/modcfg.cxx
+++ sw/source/ui/config/modcfg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: modcfg.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: os $ $Date: 2001-09-03 14:50:42 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:18:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
