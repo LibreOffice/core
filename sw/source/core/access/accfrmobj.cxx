5e499968137716663ebea6555d77c97fc2209b0b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:36:05 +0000

    INTEGRATION: CWS os8 (1.2.148); FILE MERGED 2003/04/03 07:09:21 os 1.2.148.1: #108583# precompiled headers removed

--- sw/source/core/access/accfrmobj.cxx
+++ sw/source/core/access/accfrmobj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accfrmobj.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mib $ $Date: 2002-05-15 13:17:31 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:36:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
