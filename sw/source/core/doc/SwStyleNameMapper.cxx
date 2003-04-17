be3e0e891fc535c3f8bf5e1d8f12ef2102804eb1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:47:45 +0000

    INTEGRATION: CWS os8 (1.7.12.1.48); FILE MERGED 2003/04/03 07:09:50 os 1.7.12.1.48.1: #108583# precompiled headers removed

--- sw/source/core/doc/SwStyleNameMapper.cxx
+++ sw/source/core/doc/SwStyleNameMapper.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwStyleNameMapper.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:39:34 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:47:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 #pragma hdrstop
 
 #ifndef _SWSTYLENAMEMAPPER_HXX
