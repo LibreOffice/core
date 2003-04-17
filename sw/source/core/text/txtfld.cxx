765fd8e98044fe72e1b5b875cb4d9fed4c0c1a49
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:30:17 +0000

    INTEGRATION: CWS os8 (1.12.148); FILE MERGED 2003/04/03 07:11:42 os 1.12.148.1: #108583# precompiled headers removed

--- sw/source/core/text/txtfld.cxx
+++ sw/source/core/text/txtfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtfld.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:55:52 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:30:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
