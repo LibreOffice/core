cf9988e58f80ab88e592427647fabda2892a6417
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 15:05:42 +0000

    INTEGRATION: CWS os8 (1.4.2.1.48); FILE MERGED 2003/04/03 07:16:19 os 1.4.2.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/wrtsh/wrtsh3.cxx
+++ sw/source/ui/wrtsh/wrtsh3.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrtsh3.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:45:17 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 16:05:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
