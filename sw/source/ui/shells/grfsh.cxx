8a3678ca77b6f910cf19737610d81891e82a9321
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:42:10 +0000

    INTEGRATION: CWS os8 (1.14.148); FILE MERGED 2003/04/03 07:15:03 os 1.14.148.1: #108583# precompiled headers removed

--- sw/source/ui/shells/grfsh.cxx
+++ sw/source/ui/shells/grfsh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: grfsh.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: os $ $Date: 2002-05-27 13:02:32 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:42:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
