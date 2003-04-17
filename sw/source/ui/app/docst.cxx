e326eda12f26a5c56517f04d866265496307375c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:12:06 +0000

    INTEGRATION: CWS os8 (1.12.2.2.20); FILE MERGED 2003/04/03 07:13:30 os 1.12.2.2.20.1: #108583# precompiled headers removed

--- sw/source/ui/app/docst.cxx
+++ sw/source/ui/app/docst.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docst.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:42:33 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:12:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
