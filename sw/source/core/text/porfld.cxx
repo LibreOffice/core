dfc54778cfcd0a00e6a3f0aa5064b16ae680a635
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:28:13 +0000

    INTEGRATION: CWS os8 (1.34.2.2.20); FILE MERGED 2003/04/03 07:11:37 os 1.34.2.2.20.1: #108583# precompiled headers removed

--- sw/source/core/text/porfld.cxx
+++ sw/source/core/text/porfld.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: porfld.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:41:01 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:28:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
