b137def764348501c1ff51f7acf77684d6ef4f87
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:49:51 +0000

    INTEGRATION: CWS os8 (1.5.12.1.48); FILE MERGED 2003/04/03 07:15:28 os 1.5.12.1.48.1: #108583# precompiled headers removed

--- sw/source/ui/uiview/view1.cxx
+++ sw/source/ui/uiview/view1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: view1.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:44:46 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:49:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
