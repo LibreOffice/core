df36bf3f4af955b3bc29cd4a30aa830908eccdff
Rüdiger Timm <rt@openoffice.org>
2003-04-17 08:38:00 +0000

    INTEGRATION: CWS sdk01 (1.7.14.1.6); FILE MERGED 2003/03/12 09:55:28 jsc 1.7.14.1.6.1: #107901# insert title

--- odk/setsdkenv_windows.bat
+++ odk/setsdkenv_windows.bat
@@ -3,22 +3,20 @@
 REM This script sets all enviroment variables, which
 REM are necessary for building the examples of the Office Development Kit.
 REM The Script was developed for the operating systems Windows.
-REM Before the script could be executed the following variables have to be
-REM adjusted:
 
 REM Installation directory of the Software Development Kit.
-REM Example: set OO_SDK_HOME=D:\api\odk
+REM Example: set OO_SDK_HOME=D:\StarOffice6.1SDK
 set OO_SDK_HOME=
 
 REM Office installation directory.
-REM Example: set OFFICE_HOME=C:\Programme\StarOffice6.0
+REM Example: set OFFICE_HOME=C:\Programme\StarOffice6.1
 set OFFICE_HOME=
 
 REM Directory of the make command.
-REM Example: set OO_SDK_MAKE=D:\NextGenerationMake\make
-set OO_SDK_MAKE=
+REM Example: set OO_SDK_MAKE_HOME=D:\NextGenerationMake
+set OO_SDK_MAKE_HOME=
 
-REM Directory of the C++ compiler.
+REM Directory of the C++ tools.
 REM Example:set OO_SDK_CPP_HOME=C:\Programme\Microsoft Visual Studio\VC98\bin
 set OO_SDK_CPP_HOME=
 
@@ -26,27 +24,45 @@ REM STLPORT installation directory.
 REM Example: set OO_STLPORT_HOME=D:\stlport-4.5.3
 set OO_STLPORT_HOME=
 
-REM Java SDK installation directory.
-REM Example: set OO_SDK_JAVA_HOME=C:\jdk1.4.0
+REM Java installation directory.
+REM Example: set OO_SDK_JAVA_HOME=c:\j2sdk1.4.1_01
 set OO_SDK_JAVA_HOME=
-
-REM Environment variable to enable auto deployment of example components
-REM Example: set SDK_AUTO_DEPLOYMENT=YES
-set SDK_AUTO_DEPLOYMENT=YES
 
 REM ANT installation directory.
 REM Example: set OO_SDK_ANT_HOME=D:\jakarta-ant-1.4
 REM set OO_SDK_ANT_HOME=
 
+REM Automatic deployment
+REM Example: set SDK_AUTO_DEPLOYMENT=YES
+set SDK_AUTO_DEPLOYMENT=YES
+
 
 REM Check installation path for the StarOffice Development Kit.
-if not defined OO_SDK_HOME goto error
+if not defined OO_SDK_HOME (
+  echo Error: the variable OO_SDK_HOME is missing!
+  goto :error
+)
 
 REM Check installation path for the office.
-if not defined OFFICE_HOME goto error
+if not defined OFFICE_HOME (
+  echo Error: the variable OFFICE_HOME is missing!
+  goto :error
+)
+
+REM Check installation path for GNU make.
+if not defined OO_SDK_MAKE_HOME (
+  echo Error: the variable OO_SDK_MAKE_HOME is missing!
+  goto :error
+)
+
+REM Check installation path for Java.
+if not defined OO_SDK_JAVA_HOME (
+  echo Error: the variable OO_SDK_JAVA_HOME is missing!
+  goto :error
+)
 
 REM Set library path.
-set LIB=%OO_SDK_HOME%\windows\lib;%OO_SDK_HOME%\WINexample.out\lib;%LIB
+set LIB=%OO_SDK_HOME%\windows\lib;%OO_SDK_HOME%\WINexample.out\lib;%LIB%
 
 REM Add directory of the STLPORT library to the library path.
 if defined OO_STLPORT_HOME set LIB=%OO_STLPORT_HOME%\lib;%LIB%
@@ -55,10 +71,10 @@ REM Set office program path.
 set OFFICE_PROGRAM_PATH=%OFFICE_HOME%\program
 
 REM Add directory of the SDK tools to the path.
-set PATH=%OO_SDK_HOME%\windows\bin;%OFFICE_PROGRAM_PATH%;%OO_SDK_HOME%\WINexample.out\bin;%PATH%
+set PATH=%OO_SDK_HOME%\windows\bin;%OFFICE_HOME%\program;%OO_SDK_HOME%\WINexample.out\bin;%PATH%
 
 REM Add directory of the command make to the path, if necessary.
-if defined OO_SDK_MAKE set PATH=%OO_SDK_MAKE%;%PATH%
+if defined OO_SDK_MAKE_HOME set PATH=%OO_SDK_MAKE_HOME%;%PATH%
 
 REM Add directory of the C++ tools to the path, if necessary.
 if defined OO_SDK_CPP_HOME set PATH=%OO_SDK_CPP_HOME%;%PATH%
@@ -73,11 +89,14 @@ REM Add directory of the ANT tools to the path, if necessary.
 if defined OO_SDK_ANT_HOME set PATH=%OO_SDK_ANT_HOME%\bin;%PATH%
 
 REM Set environment for C++ tools, if necessary.
-if defined OO_SDK_CPP_HOME call "%OO_SDK_CPP_HOME%"\VCVARS32.bat
+if defined OO_SDK_CPP_HOME call "%OO_SDK_CPP_HOME%\VCVARS32.bat"
+
+REM Set tilte to identify the prepared shell.
+title Shell prepared for SDK
 
 goto end
 
 :error
-echo Error: Please insert the necessary environment variables into the batch file.
+Error: Please insert the necessary environment variables into the batch file.
 
 :end
