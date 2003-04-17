b4a4c1e106490760d7a456627fb7c98486f9a47b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:31:35 +0000

    INTEGRATION: CWS os8 (1.5.146); FILE MERGED 2003/04/03 07:11:45 os 1.5.146.1: #108583# precompiled headers removed

--- sw/source/core/text/txtpaint.cxx
+++ sw/source/core/text/txtpaint.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtpaint.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:56:50 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:31:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
