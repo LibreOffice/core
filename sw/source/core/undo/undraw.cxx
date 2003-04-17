8e39f5ca6902c5e37a984571602e43555991dd0d
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:38:40 +0000

    INTEGRATION: CWS os8 (1.4.148); FILE MERGED 2003/04/03 07:12:07 os 1.4.148.1: #108583# precompiled headers removed

--- sw/source/core/undo/undraw.cxx
+++ sw/source/core/undo/undraw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: undraw.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:57:56 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:38:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
