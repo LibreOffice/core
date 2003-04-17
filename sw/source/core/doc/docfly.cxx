edd3a1492eb15d46467d4049748875364e2ffec4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:49:48 +0000

    INTEGRATION: CWS os8 (1.13.138); FILE MERGED 2003/04/03 07:09:55 os 1.13.138.1: #108583# precompiled headers removed

--- sw/source/core/doc/docfly.cxx
+++ sw/source/core/doc/docfly.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docfly.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: fme $ $Date: 2002-11-05 12:33:39 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:49:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
