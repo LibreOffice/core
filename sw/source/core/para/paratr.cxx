7fdf332e81e83fa78405919f9bce1454d60695e2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:17:33 +0000

    INTEGRATION: CWS os8 (1.5.146); FILE MERGED 2003/04/03 07:11:12 os 1.5.146.1: #108583# precompiled headers removed

--- sw/source/core/para/paratr.cxx
+++ sw/source/core/para/paratr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: paratr.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: os $ $Date: 2002-07-04 14:46:09 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:17:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
