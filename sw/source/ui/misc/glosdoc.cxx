fa3004b1317f7b55a2afa009566a34b69014dc8b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:35:46 +0000

    INTEGRATION: CWS os8 (1.13.140); FILE MERGED 2003/04/07 21:23:40 os 1.13.140.2: RESYNC: (1.13-1.14); FILE MERGED 2003/04/03 07:14:43 os 1.13.140.1: #108583# precompiled headers removed

--- sw/source/ui/misc/glosdoc.cxx
+++ sw/source/ui/misc/glosdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: glosdoc.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:41:23 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:35:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 
 #pragma hdrstop
