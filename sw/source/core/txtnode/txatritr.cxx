3f50e8e24c32d72a7059619f9658fb21d6107c1a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:36:24 +0000

    INTEGRATION: CWS os8 (1.4.144); FILE MERGED 2003/04/03 07:11:59 os 1.4.144.1: #108583# precompiled headers removed

--- sw/source/core/txtnode/txatritr.cxx
+++ sw/source/core/txtnode/txatritr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txatritr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: fme $ $Date: 2002-08-02 09:53:02 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:36:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
