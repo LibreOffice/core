2349d602f9b24fe9bc908afac2492f8de351c28b
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:15:46 +0000

    INTEGRATION: CWS os8 (1.7.138); FILE MERGED 2003/04/07 21:55:25 os 1.7.138.2: RESYNC: (1.7-1.8); FILE MERGED 2003/04/03 07:13:40 os 1.7.138.1: #108583# precompiled headers removed

--- sw/source/ui/chrdlg/drpcps.cxx
+++ sw/source/ui/chrdlg/drpcps.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drpcps.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:23:47 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:15:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
