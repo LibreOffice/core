3126e2b2df7faba3317ac9a54f55c08afd629122
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:45:26 +0000

    INTEGRATION: CWS os8 (1.2.142); FILE MERGED 2003/04/03 07:09:42 os 1.2.142.1: #108583# precompiled headers removed

--- sw/source/core/crsr/findcoll.cxx
+++ sw/source/core/crsr/findcoll.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: findcoll.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-09-13 13:01:41 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:45:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
