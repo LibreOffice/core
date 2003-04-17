befcdacf28e62f219d9bef53db662112ecc8d7de
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:13:22 +0000

    INTEGRATION: CWS os8 (1.23.2.1.48); FILE MERGED 2003/04/07 22:14:40 os 1.23.2.1.48.2: RESYNC: (1.23.2.1-1.25); FILE MERGED 2003/04/03 07:10:59 os 1.23.2.1.48.1: #108583# precompiled headers removed

--- sw/source/core/layout/layact.cxx
+++ sw/source/core/layout/layact.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: layact.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:51:16 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:13:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
