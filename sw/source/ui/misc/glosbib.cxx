6f02550146a277c7c9f78fd125787d0494506543
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:35:35 +0000

    INTEGRATION: CWS os8 (1.12.138); FILE MERGED 2003/04/03 07:14:42 os 1.12.138.1: #108583# precompiled headers removed

--- sw/source/ui/misc/glosbib.cxx
+++ sw/source/ui/misc/glosbib.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: glosbib.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: os $ $Date: 2002-12-05 12:45:04 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:35:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
