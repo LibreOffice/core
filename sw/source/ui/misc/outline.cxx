4b96acfd1124a86b8a920efa83b13bfbc7a856c4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:37:35 +0000

    INTEGRATION: CWS os8 (1.20.138); FILE MERGED 2003/04/03 07:14:47 os 1.20.138.1: #108583# precompiled headers removed

--- sw/source/ui/misc/outline.cxx
+++ sw/source/ui/misc/outline.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: outline.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: fme $ $Date: 2002-12-10 09:46:06 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:37:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
