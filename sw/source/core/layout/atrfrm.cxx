f7fc4c03ae2848c46cdca08c5f7ed3b3464b7ed1
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:10:28 +0000

    INTEGRATION: CWS os8 (1.36.4.1.48); FILE MERGED 2003/04/03 07:10:53 os 1.36.4.1.48.1: #108583# precompiled headers removed

--- sw/source/core/layout/atrfrm.cxx
+++ sw/source/core/layout/atrfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: atrfrm.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:40:23 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:10:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
