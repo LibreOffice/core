f18a4a40587772dbf63312ca982a1fe215e908d4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:05:04 +0000

    INTEGRATION: CWS os8 (1.2.176); FILE MERGED 2003/04/03 07:10:39 os 1.2.176.1: #108583# precompiled headers removed

--- sw/source/core/fields/ddetbl.cxx
+++ sw/source/core/fields/ddetbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ddetbl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dvo $ $Date: 2000-12-11 20:14:38 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:05:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
