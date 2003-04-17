eb09f167ecaa374b852fb42105a68309fb6e566c
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:38:18 +0000

    INTEGRATION: CWS os8 (1.7.138); FILE MERGED 2003/04/03 07:12:06 os 1.7.138.1: #108583# precompiled headers removed

--- sw/source/core/undo/undobj.cxx
+++ sw/source/core/undo/undobj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: undobj.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: dvo $ $Date: 2002-11-28 17:41:08 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:38:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
