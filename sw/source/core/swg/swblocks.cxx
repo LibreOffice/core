f6bbb03e41f0a83a8b74e89744fb9f5cb436f6fc
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:24:41 +0000

    INTEGRATION: CWS os8 (1.15.160); FILE MERGED 2003/04/03 07:11:27 os 1.15.160.1: #108583# precompiled headers removed

--- sw/source/core/swg/swblocks.cxx
+++ sw/source/core/swg/swblocks.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swblocks.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: jp $ $Date: 2001-10-19 08:19:59 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:24:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
