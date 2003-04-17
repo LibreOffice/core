71e088a3c6c43d764a810268d9c7970dfcf5a6f2
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:56:27 +0000

    INTEGRATION: CWS os8 (1.5.146); FILE MERGED 2003/04/03 07:10:17 os 1.5.146.1: #108583# precompiled headers removed

--- sw/source/core/docnode/ndnotxt.cxx
+++ sw/source/core/docnode/ndnotxt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ndnotxt.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mib $ $Date: 2002-07-25 15:47:22 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:56:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
