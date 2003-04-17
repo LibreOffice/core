7a88ed447a086ccb782f887d44bf369ac279fa62
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:56:01 +0000

    INTEGRATION: CWS os8 (1.4.160); FILE MERGED 2003/04/03 07:15:48 os 1.4.160.1: #108583# precompiled headers removed

--- sw/source/ui/utlui/makefile.mk
+++ sw/source/ui/utlui/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: jp $ $Date: 2001-10-18 12:22:35 $
+#   last change: $Author: vg $ $Date: 2003-04-17 15:56:01 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,10 +66,6 @@ PRJNAME=sw
 TARGET=utlui
 
 AUTOSEG=true
-
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\ui_1st\ui_pch
 
 # --- Settings -----------------------------------------------------
 
