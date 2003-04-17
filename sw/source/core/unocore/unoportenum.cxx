9c40acfa7316e4937445b97c8aba0cb0a1bed53a
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 13:44:48 +0000

    INTEGRATION: CWS os8 (1.20.14.1.48); FILE MERGED 2003/04/03 07:12:20 os 1.20.14.1.48.1: #108583# precompiled headers removed

--- sw/source/core/unocore/unoportenum.cxx
+++ sw/source/core/unocore/unoportenum.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoportenum.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:58:41 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 14:44:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
