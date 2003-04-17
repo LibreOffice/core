3f8d482b93396f526158858dce76a044bc00d3d4
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:38:12 +0000

    INTEGRATION: CWS os8 (1.5.144); FILE MERGED 2003/04/03 07:09:25 os 1.5.144.1: #108583# precompiled headers removed

--- sw/source/core/access/accselectionhelper.cxx
+++ sw/source/core/access/accselectionhelper.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accselectionhelper.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mib $ $Date: 2002-08-15 09:29:35 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:38:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
