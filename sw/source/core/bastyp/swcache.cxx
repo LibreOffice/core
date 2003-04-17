4ba4eb4f415b1a263e410142e3564914e2f40834
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 12:42:44 +0000

    INTEGRATION: CWS os8 (1.5.174); FILE MERGED 2003/04/03 07:09:35 os 1.5.174.1: #108583# precompiled headers removed

--- sw/source/core/bastyp/swcache.cxx
+++ sw/source/core/bastyp/swcache.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swcache.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:48:38 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 13:42:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "core_pch.hxx"
-#endif
 
 #pragma hdrstop
 
