059a01e769a868ced94f9697a4020b7d404f82c5
Vladimir Glazounov <vg@openoffice.org>
2003-04-17 14:41:25 +0000

    INTEGRATION: CWS os8 (1.9.2.1.68); FILE MERGED 2003/04/03 07:15:01 os 1.9.2.1.68.1: #108583# precompiled headers removed

--- sw/source/ui/shells/drwbassh.cxx
+++ sw/source/ui/shells/drwbassh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drwbassh.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:44:20 $
+ *  last change: $Author: vg $ $Date: 2003-04-17 15:41:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,6 @@
  *
  ************************************************************************/
 
-#ifdef PRECOMPILED
-#include "ui_pch.hxx"
-#endif
 
 #pragma hdrstop
 
