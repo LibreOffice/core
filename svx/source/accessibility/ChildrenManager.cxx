84a9768ec4c74fd9c0e45d39fb29d8dc044bd740
Vladimir Glazounov <vg@openoffice.org>
2003-04-24 15:56:19 +0000

    INTEGRATION: CWS uaa02 (1.10.182); FILE MERGED 2003/04/17 17:32:48 mt 1.10.182.2: RESYNC: (1.10-1.11); FILE MERGED 2003/04/11 17:06:57 mt 1.10.182.1: #108656# Moved accessibility from drafts to final

--- svx/source/accessibility/ChildrenManager.cxx
+++ svx/source/accessibility/ChildrenManager.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ChildrenManager.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 15:23:46 $
+ *  last change: $Author: vg $ $Date: 2003-04-24 16:56:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -71,7 +71,7 @@
 
 using namespace ::rtl;
 using namespace ::com::sun::star;
-using namespace ::drafts::com::sun::star::accessibility;
+using namespace ::com::sun::star::accessibility;
 using ::com::sun::star::uno::Reference;
 
 namespace accessibility {
