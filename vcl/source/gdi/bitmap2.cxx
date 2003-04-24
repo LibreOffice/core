4a309a0645163171f028ad5e725ad281584b2fbe
Rüdiger Timm <rt@openoffice.org>
2003-04-24 12:17:58 +0000

    INTEGRATION: CWS mav4 (1.3.164); FILE MERGED 2003/04/16 12:22:23 sj 1.3.164.1: #108967# removed forbidden memory access

--- vcl/source/gdi/bitmap2.cxx
+++ vcl/source/gdi/bitmap2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bitmap2.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: cp $ $Date: 2001-06-28 13:10:36 $
+ *  last change: $Author: rt $ $Date: 2003-04-24 13:17:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1155,7 +1155,7 @@ BOOL Bitmap::ImplWriteRLE( SvStream& rOStm, BitmapReadAccess& rAcc, BOOL bRLE4 )
     ULONG       nSaveIndex;
     ULONG       nCount;
     ULONG       nBufCount;
-    BYTE*       pBuf = new BYTE[ nWidth << 1 ];
+    BYTE*       pBuf = new BYTE[ ( nWidth << 1 ) + 2 ];
     BYTE*       pTmp;
     BYTE        cPix;
     BYTE        cLast;
