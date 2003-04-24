d461809d26a6cba5f295879588985b5ee98871e2
Rüdiger Timm <rt@openoffice.org>
2003-04-24 13:48:45 +0000

    INTEGRATION: CWS draw9 (1.29.10); FILE MERGED 2003/04/10 11:24:36 thb 1.29.10.1: #108759# Factored out background color determination to SdrPaintView

--- svx/source/svdraw/svdedxv.cxx
+++ svx/source/svdraw/svdedxv.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svdedxv.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:04:28 $
+ *  last change: $Author: rt $ $Date: 2003-04-24 14:48:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -133,8 +133,6 @@
 #ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
 #endif
-
-#define SPOTCOUNT   5
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -435,6 +433,7 @@ void SdrObjEditView::ImpPaintOutlinerView(OutlinerView& rOutlView, const Rectang
 
 Color SdrObjEditView::ImpGetTextEditBackgroundColor() const
 {
+    // #108759# Extracted significant parts to SdrPaintView::CalcBackgroundColor()
     svtools::ColorConfig aColorConfig;
     Color aBackground(aColorConfig.GetColorValue(svtools::DOCCOLOR).nColor);
 
@@ -446,7 +445,6 @@ Color SdrObjEditView::ImpGetTextEditBackgroundColor() const
 
     if(!rStyleSettings.GetHighContrastMode())
     {
-        // original non-contrast code here
         BOOL bFound=FALSE;
         SdrTextObj* pText=PTR_CAST(SdrTextObj,pTextEditObj);
         if (pText!=NULL && pText->IsClosedObj())
@@ -455,117 +453,12 @@ Color SdrObjEditView::ImpGetTextEditBackgroundColor() const
         }
         if (!bFound && pTextEditPV!=NULL && pTextEditObj!=NULL)
         {
-            // Ok, dann eben die Page durchsuchen!
             Point aPvOfs(pTextEditPV->GetOffset());
             const SdrPage* pPg=pTextEditPV->GetPage();
-
-            Point aSpotPos[SPOTCOUNT];
-            Color aSpotColor[SPOTCOUNT];
             Rectangle aSnapRect( pTextEditObj->GetSnapRect() );
-            ULONG nHeight( aSnapRect.GetSize().Height() );
-            ULONG nWidth( aSnapRect.GetSize().Width() );
-            ULONG nWidth14  = nWidth / 4;
-            ULONG nHeight14 = nHeight / 4;
-            ULONG nWidth34  = ( 3 * nWidth ) / 4;
-            ULONG nHeight34 = ( 3 * nHeight ) / 4;
+            aSnapRect.Move(aPvOfs.X(), aPvOfs.Y());
 
-            const SetOfByte& rVisiLayers = pTextEditPV->GetVisibleLayers();
-
-            USHORT i;
-            for ( i = 0; i < SPOTCOUNT; i++ )
-            {
-                // Es wird anhand von fuenf Spots die Farbe untersucht
-                switch ( i )
-                {
-                    case 0 :
-                    {
-                        // Center-Spot
-                        aSpotPos[i] = aSnapRect.Center();
-                        aSpotPos[i] += aPvOfs;
-                    }
-                    break;
-
-                    case 1 :
-                    {
-                        // TopLeft-Spot
-                        aSpotPos[i] = aSnapRect.TopLeft();
-                        aSpotPos[i].X() += nWidth14;
-                        aSpotPos[i].Y() += nHeight14;
-                        aSpotPos[i] += aPvOfs;
-                    }
-                    break;
-
-                    case 2 :
-                    {
-                        // TopRight-Spot
-                        aSpotPos[i] = aSnapRect.TopLeft();
-                        aSpotPos[i].X() += nWidth34;
-                        aSpotPos[i].Y() += nHeight14;
-                        aSpotPos[i] += aPvOfs;
-                    }
-                    break;
-
-                    case 3 :
-                    {
-                        // BottomLeft-Spot
-                        aSpotPos[i] = aSnapRect.TopLeft();
-                        aSpotPos[i].X() += nWidth14;
-                        aSpotPos[i].Y() += nHeight34;
-                        aSpotPos[i] += aPvOfs;
-                    }
-                    break;
-
-                    case 4 :
-                    {
-                        // BottomRight-Spot
-                        aSpotPos[i] = aSnapRect.TopLeft();
-                        aSpotPos[i].X() += nWidth34;
-                        aSpotPos[i].Y() += nHeight34;
-                        aSpotPos[i] += aPvOfs;
-                    }
-                    break;
-
-                }
-
-                aSpotColor[i] = Color( COL_WHITE );
-                pPg->GetFillColor(aSpotPos[i], rVisiLayers, bLayerSortedRedraw, aSpotColor[i]);
-            }
-
-            USHORT aMatch[SPOTCOUNT];
-
-            for ( i = 0; i < SPOTCOUNT; i++ )
-            {
-                // Wurden gleiche Spot-Farben gefuden?
-                aMatch[i] = 0;
-
-                for ( USHORT j = 0; j < SPOTCOUNT; j++ )
-                {
-                    if( j != i )
-                    {
-                        if( aSpotColor[i] == aSpotColor[j] )
-                        {
-                            aMatch[i]++;
-                        }
-                    }
-                }
-            }
-
-            // Das hoechste Gewicht hat der Spot in der Mitte
-            aBackground = aSpotColor[0];
-
-            for ( USHORT nMatchCount = SPOTCOUNT - 1; nMatchCount > 1; nMatchCount-- )
-            {
-                // Welche Spot-Farbe wurde am haeufigsten gefunden?
-                for ( USHORT i = 0; i < SPOTCOUNT; i++ )
-                {
-                    if( aMatch[i] == nMatchCount )
-                    {
-                        aBackground = aSpotColor[i];
-                        nMatchCount = 1;   // Abbruch auch der aeusseren for-Schleife
-                        break;
-                    }
-                }
-            }
+            return CalcBackgroundColor( aSnapRect, pTextEditPV->GetVisibleLayers(), *pPg );
         }
     }
 
