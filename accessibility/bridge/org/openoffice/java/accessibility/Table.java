81d7259d8110892f5c5c6e9032aae833edda63cb
Vladimir Glazounov <vg@openoffice.org>
2003-04-24 17:16:46 +0000

    INTEGRATION: CWS uaa02 (1.3.6); FILE MERGED 2003/04/11 11:01:08 obr 1.3.6.1: #108656# accessibility API moved from drafts to final

--- accessibility/bridge/org/openoffice/java/accessibility/Table.java
+++ accessibility/bridge/org/openoffice/java/accessibility/Table.java
@@ -61,7 +61,7 @@ import javax.accessibility.AccessibleState;
 
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.UnoRuntime;
-import drafts.com.sun.star.accessibility.*;
+import com.sun.star.accessibility.*;
 
 public class Table extends DescendantManager implements javax.accessibility.Accessible {
 
@@ -148,10 +148,10 @@ public class Table extends DescendantManager implements javax.accessibility.Acce
         /** Called by OpenOffice process to notify property changes */
         public void notifyEvent(AccessibleEventObject event) {
             switch (event.EventId) {
-                case AccessibleEventId.ACCESSIBLE_ACTIVE_DESCENDANT_EVENT:
+                case AccessibleEventId.ACTIVE_DESCENDANT_CHANGED:
                     setActiveDescendant(event.NewValue);
                     break;
-                case AccessibleEventId.ACCESSIBLE_CHILD_EVENT:
+                case AccessibleEventId.CHILD:
                     if (AnyConverter.isObject(event.OldValue)) {
                         remove(event.OldValue);
                     }
@@ -411,7 +411,7 @@ public class Table extends DescendantManager implements javax.accessibility.Acce
                     if (unoAccessibleStateSet.contains(AccessibleStateType.SINGLE_LINE)) {
                         stateSet.add(javax.accessibility.AccessibleState.SINGLE_LINE);
                     }
-                    if (unoAccessibleStateSet.contains(AccessibleStateType.MULTILINE)) {
+                    if (unoAccessibleStateSet.contains(AccessibleStateType.MULTI_LINE)) {
                         stateSet.add(javax.accessibility.AccessibleState.MULTI_LINE);
                     }
                     if (unoAccessibleStateSet.contains(AccessibleStateType.SELECTABLE)) {
