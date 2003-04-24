2d3dece122eb88014000f25087f0d8e2abf5be75
Rüdiger Timm <rt@openoffice.org>
2003-04-24 12:28:02 +0000

    INTEGRATION: CWS mav4 (1.8.12); FILE MERGED 2003/04/15 12:39:13 abi 1.8.12.2: #88865# added exception handling under windows ( and explicit linking to cppu.lib) 2003/04/15 10:38:53 abi 1.8.12.1: #88865# a hack in an method which will get removed

--- tools/util/makefile.mk
+++ tools/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-27 17:05:08 $
+#   last change: $Author: rt $ $Date: 2003-04-24 13:28:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -165,7 +165,9 @@ LIB1FILES+=  \
             $(SLB)$/zcodec.lib      \
             $(SLB)$/inet.lib
 
-SHL1STDLIBS += $(ZLIB3RDLIB)
+SHL1STDLIBS += $(ZLIB3RDLIB) \
+               $(UCBHELPERLIB) \
+               $(CPPULIB)
 
 LIB1FILES+=  $(SLB)$/dll.lib
 
