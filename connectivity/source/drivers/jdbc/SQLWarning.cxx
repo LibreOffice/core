4def11af42e094ec24c5c1b6dda43c73235eab0d
Rüdiger Timm <rt@openoffice.org>
2003-04-24 12:22:18 +0000

    INTEGRATION: CWS mav4 (1.1.1.1.98); FILE MERGED 2003/04/15 12:22:15 oj 1.1.1.1.98.1: #108943# merge from apps61beta2 and statement fix, concurrency

--- connectivity/source/drivers/jdbc/SQLWarning.cxx
+++ connectivity/source/drivers/jdbc/SQLWarning.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SQLWarning.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:14:23 $
+ *  last change: $Author: rt $ $Date: 2003-04-24 13:22:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -87,7 +87,7 @@ jclass java_sql_SQLWarning_BASE::getMyClass()
 
 void java_sql_SQLWarning_BASE::saveClassRef( jclass pClass )
 {
-    if( SDBThreadAttach::IsJavaErrorOccured() || pClass==NULL  )
+    if( pClass==NULL  )
         return;
     // der uebergebe Klassen-Handle ist schon global, daher einfach speichern
     theClass = pClass;
