a7bdbab14378462b720bc6752eb8ba3c7a8dd9a0
Rüdiger Timm <rt@openoffice.org>
2003-04-24 12:19:00 +0000

    INTEGRATION: CWS mav4 (1.3.84); FILE MERGED 2003/04/15 12:22:02 oj 1.3.84.1: #108943# merge from apps61beta2 and statement fix, concurrency

--- connectivity/source/drivers/jdbc/Array.cxx
+++ connectivity/source/drivers/jdbc/Array.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Array.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: oj $ $Date: 2001-05-31 08:29:15 $
+ *  last change: $Author: rt $ $Date: 2003-04-24 13:19:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -94,7 +94,7 @@ jclass java_sql_Array::getMyClass()
 
 void java_sql_Array::saveClassRef( jclass pClass )
 {
-    if( SDBThreadAttach::IsJavaErrorOccured() || pClass==NULL  )
+    if( pClass==NULL  )
         return;
     // der uebergebe Klassen-Handle ist schon global, daher einfach speichern
     theClass = pClass;
