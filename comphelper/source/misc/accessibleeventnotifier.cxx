a9fd75dbfd748288ad2a7dcf855820f3244ae0b9
Vladimir Glazounov <vg@openoffice.org>
2003-04-24 16:27:22 +0000

    INTEGRATION: CWS uaa02 (1.4.8); FILE MERGED 2003/04/10 11:59:40 mt 1.4.8.1: #108656# Moved Accessibility module from drafts to final

--- comphelper/source/misc/accessibleeventnotifier.cxx
+++ comphelper/source/misc/accessibleeventnotifier.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accessibleeventnotifier.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-19 15:58:36 $
+ *  last change: $Author: vg $ $Date: 2003-04-24 17:27:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,7 +78,7 @@ namespace comphelper
 
     using namespace ::com::sun::star::uno;
     using namespace ::com::sun::star::lang;
-    using namespace ::drafts::com::sun::star::accessibility;
+    using namespace ::com::sun::star::accessibility;
     using namespace ::comphelper;
 
     //=====================================================================
