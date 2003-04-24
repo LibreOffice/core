ac802f279df5abbe0f23cfde859db09e7eaee454
Vladimir Glazounov <vg@openoffice.org>
2003-04-24 10:10:32 +0000

    INTEGRATION: CWS i18napi (1.3.8); FILE MERGED 2003/04/19 19:41:12 er 1.3.8.1: #107686# drafts.com.sun.star.i18n to com.sun.star.i18n

--- i18npool/source/transliteration/transliteration_Numeric.cxx
+++ i18npool/source/transliteration/transliteration_Numeric.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: transliteration_Numeric.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 16:07:08 $
+ *  last change: $Author: vg $ $Date: 2003-04-24 11:10:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,6 @@
 #include <nativenumbersupplier.hxx>
 #include <defaultnumberingprovider.hxx>
 
-using namespace drafts::com::sun::star::i18n;
 using namespace com::sun::star::uno;
 using namespace rtl;
 
