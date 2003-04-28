b949093e826e056e70c9dd8d4810137cc2806473
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:51:00 +0000

    INTEGRATION: CWS ooo20030412 (1.2.44); FILE MERGED 2003/04/28 13:37:09 mh 1.2.44.3: RESYNC: (1.5-1.6); FILE MERGED 2003/04/12 22:29:41 mh 1.2.44.2: RESYNC: (1.2-1.5); FILE MERGED 2003/04/11 11:38:38 mh 1.2.44.1: join: from ooo11beta

--- i18npool/source/transliteration/ignoreIterationMark_ja_JP.cxx
+++ i18npool/source/transliteration/ignoreIterationMark_ja_JP.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ignoreIterationMark_ja_JP.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-24 11:08:51 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:51:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 // prevent internal compiler error with MSVC6SP3
-#include <stl/utility>
+#include <utility>
 
 #include <i18nutil/oneToOneMapping.hxx>
 #define TRANSLITERATION_IterationMark_ja_JP
