9b3ee1ab79a249bdf27bf78de54f5ef3b722c0da
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:29:53 +0000

    INTEGRATION: CWS ooo20030412 (1.3.44); FILE MERGED 2003/04/10 05:34:04 mh 1.3.44.1: chg: use sal alloca.h, #i6842#

--- bridges/source/remote/static/proxy.cxx
+++ bridges/source/remote/static/proxy.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: proxy.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:28:29 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:29:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,14 +62,7 @@
 #define NDEBUG
 #endif
 #include <assert.h>
-#ifdef SOLARIS
-#include <alloca.h>
-#elif defined MACOSX
-#include <sys/types.h>
-#include <sys/malloc.h>
-#else
-#include <malloc.h>
-#endif
+#include <sal/alloca.h>
 
 #include <bridges/remote/proxy.hxx>
 #include <bridges/remote/context.h>
