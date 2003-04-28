eab45698958c8a680f715f49c6b7c779bf2c58b8
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:52:09 +0000

    INTEGRATION: CWS ooo20030412 (1.2.44); FILE MERGED 2003/04/12 22:31:31 mh 1.2.44.2: RESYNC: (1.2-1.5); FILE MERGED 2003/04/11 11:38:40 mh 1.2.44.1: join: from ooo11beta

--- i18npool/source/transliteration/ignoreSeparator_ja_JP.cxx
+++ i18npool/source/transliteration/ignoreSeparator_ja_JP.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ignoreSeparator_ja_JP.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 16:03:47 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:52:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 // prevent internal compiler error with MSVC6SP3
-#include <stl/utility>
+#include <utility>
 
 #define TRANSLITERATION_Separator_ja_JP
 #include <transliteration_Ignore.hxx>
