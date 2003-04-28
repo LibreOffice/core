067b82a04c50aa50b7cf787c3607628f08a2d26c
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:52:49 +0000

    INTEGRATION: CWS ooo20030412 (1.2.44); FILE MERGED 2003/04/12 22:29:08 mh 1.2.44.2: RESYNC: (1.2-1.5); FILE MERGED 2003/04/11 11:38:41 mh 1.2.44.1: join: from ooo11beta

--- i18npool/source/transliteration/ignoreTraditionalKana_ja_JP.cxx
+++ i18npool/source/transliteration/ignoreTraditionalKana_ja_JP.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ignoreTraditionalKana_ja_JP.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 16:04:32 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:52:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 // prevent internal compiler error with MSVC6SP3
-#include <stl/utility>
+#include <utility>
 
 #define TRANSLITERATION_TraditionalKana_ja_JP
 #include <transliteration_Ignore.hxx>
