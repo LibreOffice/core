a6baf1ab6fde263026d50f13b109a7fcb1cbd794
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:47:54 +0000

    INTEGRATION: CWS ooo20030412 (1.3.36.1.34); FILE MERGED 2003/04/11 09:25:18 mh 1.3.36.1.34.2: join: from ooo11beta 2003/03/27 11:37:07 waratah 1.3.36.1.34.1: 11233: enable standard warnings from the gcc compiler on latest 644 branch

--- solenv/inc/unxlnxi.mk
+++ solenv/inc/unxlnxi.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxlnxi.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-27 11:48:19 $
+#   last change: $Author: hr $ $Date: 2003-04-28 16:47:54 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -82,9 +82,9 @@ JAVA_RUNTIME=-ljava_g
 CXX*=g++
 CC*=gcc
 .IF "$(GLIBC)"=="2"
-CFLAGS=-w -c -I. -I$(INC) -I$(INCLOCAL) -I$(INCGUI) -I$(INCCOM) $(SOLARINC)
+CFLAGS=-c -I. -I$(INC) -I$(INCLOCAL) -I$(INCGUI) -I$(INCCOM) $(SOLARINC)
 .ELSE
-CFLAGS=-w -c -I. -I/usr/solar/inc/pthread_provenzano -I$(INC) -I$(INCLOCAL) -I$(INCGUI) -I$(INCCOM) $(SOLARINC)
+CFLAGS=-c -I. -I/usr/solar/inc/pthread_provenzano -I$(INC) -I$(INCLOCAL) -I$(INCGUI) -I$(INCCOM) $(SOLARINC)
 .ENDIF
 CFLAGSCC=-pipe -mpentium -fguiding-decls
 CFLAGSCXX=-pipe -mpentium -fguiding-decls
