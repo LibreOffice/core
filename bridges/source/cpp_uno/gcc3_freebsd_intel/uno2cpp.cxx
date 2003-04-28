71137b13aa1ad14683ec2ba101de944f6674174f
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:28:35 +0000

    INTEGRATION: CWS ooo20030412 (1.1.4.1.12); FILE MERGED 2003/04/10 05:34:00 mh 1.1.4.1.12.2: chg: use sal alloca.h, #i6842# 2003/04/07 01:16:36 waratah 1.1.4.1.12.1: 12879: Free bsd patches to intergate patches into the tree

--- bridges/source/cpp_uno/gcc3_freebsd_intel/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_freebsd_intel/uno2cpp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: uno2cpp.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-23 16:30:27 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:28:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,7 @@
  *
  ************************************************************************/
 
-#include <malloc.h>
+#include <sal/alloca.h>
 #include <rtl/alloc.h>
 
 #include <uno/data.h>
