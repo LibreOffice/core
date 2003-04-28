bfeafc1d7942b7268fdb09ab4ce9c5ccfcf27b91
Jens-Heiner Rechtien <hr@openoffice.org>
2003-04-28 15:50:39 +0000

    INTEGRATION: CWS ooo20030412 (1.2.44); FILE MERGED 2003/04/12 22:30:26 mh 1.2.44.2: RESYNC: (1.2-1.5); FILE MERGED 2003/04/11 11:38:37 mh 1.2.44.1: join: from ooo11beta

--- i18npool/source/transliteration/ignoreHyuByu_ja_JP.cxx
+++ i18npool/source/transliteration/ignoreHyuByu_ja_JP.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ignoreHyuByu_ja_JP.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-08 16:02:10 $
+ *  last change: $Author: hr $ $Date: 2003-04-28 16:50:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 // prevent internal compiler error with MSVC6SP3
-#include <stl/utility>
+#include <utility>
 
 #define TRANSLITERATION_HyuByu_ja_JP
 #include <transliteration_Ignore.hxx>
