86a277def0d9c9d9f60626aab36730d56ff9f44c
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 08:43:27 +0000

    INTEGRATION: CWS aig01 (1.9.20); FILE MERGED 2003/05/15 15:22:10 fme 1.9.20.1: #108778# Fly frame and drawing positioning

--- sw/source/core/draw/dview.cxx
+++ sw/source/core/draw/dview.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dview.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-24 14:54:11 $
+ *  last change: $Author: vg $ $Date: 2003-05-22 09:43:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -569,7 +569,7 @@ const SwFrm *SwDrawView::CalcAnchor()
         }
     }
     if( pAnch && !pAnch->IsProtected() )
-        aAnchorPoint = pAnch->GetAnchorPos();
+        aAnchorPoint = pAnch->GetFrmAnchorPos( ::HasWrap( pObj ) );
     else
         pAnch = 0;
     return pAnch;
