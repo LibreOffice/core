b5961ae5c6dd29bed2f8b68d7cd060f6116e8dd5
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 07:55:11 +0000

    INTEGRATION: CWS os9 (1.1.2); FILE ADDED 2003/04/29 11:15:33 lo 1.1.2.1: #107420# new place for OEM dialog

---
+++ desktop/source/so_comp/services.cxx
@@ -0,0 +1,204 @@
+/*************************************************************************
+ *
+ *  $RCSfile: services.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2003-05-22 08:55:11 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "evaluation.hxx"
+
+#ifndef _COM_SUN_STAR_BEANS_NAMEDVALUE_HPP_
+#include <com/sun/star/beans/NamedValue.hpp>
+#endif
+#ifndef _COM_SUN_STAR_REGISTRY_XREGISTRYKEY_HPP_
+#include <com/sun/star/registry/XRegistryKey.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_DATE_HPP_
+#include <com/sun/star/util/Date.hpp>
+#endif
+
+#ifndef _UNO_ENVIRONMENT_H_
+#include <uno/environment.h>
+#endif
+#ifndef _CPPUHELPER_FACTORY_HXX_
+#include <cppuhelper/factory.hxx>
+#endif
+#ifndef _UTL_CONFIGMGR_HXX_
+#include <unotools/configmgr.hxx>
+#endif
+
+#include "oemjob.hxx"
+#include "evaluation.hxx"
+
+
+using namespace rtl;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::registry;
+using namespace ::desktop;
+
+static const char* pServices[] =
+{
+    SOEvaluation::serviceName,
+    OEMPreloadJob::serviceName,
+    NULL
+};
+
+static const char* pImplementations[] =
+{
+    SOEvaluation::implementationName,
+    OEMPreloadJob::implementationName,
+    NULL
+};
+
+typedef Reference<XInterface>(* fProvider)(const Reference<XMultiServiceFactory>&);
+
+static const fProvider pInstanceProviders[] =
+{
+    SOEvaluation::CreateInstance,
+    OEMPreloadJob::CreateInstance,
+    NULL
+};
+
+
+static const char** pSupportedServices[] =
+{
+    SOEvaluation::interfaces,
+    OEMPreloadJob::interfaces,
+    NULL
+};
+
+static Sequence<OUString>
+getSupportedServiceNames(int p) {
+    const char **names = pSupportedServices[p];
+    Sequence<OUString> aSeq;
+    for(int i = 0; names[i] != NULL; i++) {
+        aSeq.realloc(i+1);
+        aSeq[i] = OUString::createFromAscii(names[i]);
+    }
+    return aSeq;
+}
+
+extern "C"
+{
+void SAL_CALL
+component_getImplementationEnvironment(
+    const sal_Char** ppEnvironmentTypeName,
+    uno_Environment** ppEnvironment)
+{
+    *ppEnvironmentTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME ;
+}
+
+sal_Bool SAL_CALL
+component_writeInfo(
+    void* pServiceManager,
+    void* pRegistryKey)
+{
+    Reference<XMultiServiceFactory> xMan(
+        reinterpret_cast< XMultiServiceFactory* >( pServiceManager ) ) ;
+    Reference<XRegistryKey> xKey(
+        reinterpret_cast< XRegistryKey* >( pRegistryKey ) ) ;
+
+    // iterate over service names and register them...
+    OUString aImpl;
+    const char* pServiceName = NULL;
+    const char* pImplName = NULL;
+    for (int i = 0; (pServices[i]!=NULL)&&(pImplementations[i]!=NULL); i++) {
+        pServiceName= pServices[i];
+        pImplName = pImplementations[i];
+        aImpl = OUString::createFromAscii("/")
+              + OUString::createFromAscii(pImplName)
+              + OUString::createFromAscii("/UNO/SERVICES");
+        Reference<XRegistryKey> xNewKey = xKey->createKey(aImpl);
+        xNewKey->createKey(OUString::createFromAscii(pServiceName));
+    }
+    return sal_True;
+}
+
+void* SAL_CALL
+component_getFactory(
+    const sal_Char* pImplementationName,
+    void* pServiceManager,
+    void* pRegistryKey)
+{
+    // Set default return value for this operation - if it failed.
+    if  ( pImplementationName && pServiceManager )
+    {
+        Reference< XSingleServiceFactory > xFactory;
+        Reference< XMultiServiceFactory > xServiceManager(
+            reinterpret_cast< XMultiServiceFactory* >( pServiceManager ) ) ;
+
+        // search implementation
+        for (int i = 0; (pImplementations[i]!=NULL); i++) {
+            if ( strcmp(pImplementations[i], pImplementationName ) == 0 ) {
+                // found implementation
+                xFactory = Reference<XSingleServiceFactory>(cppu::createSingleFactory(
+                    xServiceManager, OUString::createFromAscii(pImplementationName),
+                    pInstanceProviders[i], getSupportedServiceNames(i)));
+                if ( xFactory.is() ) {
+                    // Factory is valid - service was found.
+                    xFactory->acquire();
+                    return xFactory.get();
+                }
+            }
+        } // for()
+    }
+    // Return with result of this operation.
+    return NULL;
+}
+} // extern "C"
