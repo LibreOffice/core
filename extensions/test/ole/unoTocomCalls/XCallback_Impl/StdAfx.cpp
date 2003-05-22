142cb75b7577b2bbb929ad8d811d240e2c76bacd
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 09:08:00 +0000

    INTEGRATION: CWS uno4 (1.1.1.1.122); FILE MERGED 2003/05/08 07:39:14 jl 1.1.1.1.122.1: *** empty log message ***

--- extensions/test/ole/unoTocomCalls/XCallback_Impl/StdAfx.cpp
+++ extensions/test/ole/unoTocomCalls/XCallback_Impl/StdAfx.cpp
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: StdAfx.cpp,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:16:56 $
+ *  last change: $Author: vg $ $Date: 2003-05-22 10:08:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,3 +70,4 @@
 #endif
 
 #include <atlimpl.cpp>
+
