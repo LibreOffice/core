00e10393a2b26aa22abb66638ad960bdec6179a6
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 08:42:16 +0000

    INTEGRATION: CWS aig01 (1.17.20); FILE MERGED 2003/05/15 15:21:49 fme 1.17.20.1: #108778# Fly frame and drawing positioning

--- sw/source/core/doc/docdraw.cxx
+++ sw/source/core/doc/docdraw.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docdraw.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-24 14:53:49 $
+ *  last change: $Author: vg $ $Date: 2003-05-22 09:42:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -223,7 +223,7 @@ SwDrawContact* SwDoc::GroupSelection( SdrView& rDrawView )
 
             //Ankerpos und Relpos synchronisieren, damit die Position der
             //Objekte erhalten bleibt.
-            const Point aAbs( pContact->GetAnchor()->GetAnchorPos() +
+            const Point aAbs( pContact->GetAnchor()->GetFrmAnchorPos( ::HasWrap( pObj ) ) +
                               pObj->GetRelativePos() );
 
             pFmt = (SwDrawFrmFmt*)pContact->GetFmt();
