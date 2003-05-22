5e3c5cb05137586144000a4317aa67fb697796d7
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 12:31:46 +0000

    INTEGRATION: CWS uaa03 (1.5.8); FILE MERGED 2003/05/22 11:02:46 sw 1.5.8.1: #i14623# UAA API clean up

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenu.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenu.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleMenu.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Date: 2003-04-28 11:21:38 $
+ *  last change: $Date: 2003-05-22 13:31:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -197,6 +197,8 @@ public class AccessibleMenu extends TestCase {
         tEnv.addObjRelation("XAccessibleText.Text", text.getText());
 
         tEnv.addObjRelation("EditOnly","Can't change or select Text in Menu");
+
+        tEnv.addObjRelation("LimitedBounds", "yes");
 
         return tEnv;
 
