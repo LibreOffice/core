9bdf78bac151aa332c8bc237fb1f096ad642248e
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 08:24:32 +0000

    INTEGRATION: CWS obo02 (1.3.94); FILE MERGED 2003/05/14 15:39:14 mav 1.3.94.1: #i14324# adopt to new atl version

--- extensions/source/activex/main/SOComWindowPeer.cpp
+++ extensions/source/activex/main/SOComWindowPeer.cpp
@@ -16,7 +16,11 @@ STDMETHODIMP SOComWindowPeer::InterfaceSupportsErrorInfo(REFIID riid)
 
     for (int i=0;i<sizeof(arr)/sizeof(arr[0]);i++)
     {
+#if defined(_MSC_VER) && (_MSC_VER >= 1300)
+        if (InlineIsEqualGUID(*arr[i],riid))
+#else
         if (::ATL::InlineIsEqualGUID(*arr[i],riid))
+#endif
             return S_OK;
     }
     return S_FALSE;
