c37b08557bd891333c5a006e40dccb202016317d
Vladimir Glazounov <vg@openoffice.org>
2003-05-22 07:39:06 +0000

    INTEGRATION: CWS uno4 (1.1.2); FILE ADDED 2003/05/09 12:22:09 sb 1.1.2.1: #108642# Moved Java remote bridge tests from jurt/tests/bugs here to avoid cyclic project dependencies.

---
+++ bridges/test/com/sun/star/lib/uno/bridges/java_remote/Bug97697_Test.java
@@ -0,0 +1,139 @@
+/*************************************************************************
+ *
+ *  $RCSfile: Bug97697_Test.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2003-05-22 08:39:06 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2002 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package com.sun.star.lib.uno.bridges.java_remote;
+
+import com.sun.star.bridge.XBridge;
+import com.sun.star.bridge.XInstanceProvider;
+import com.sun.star.lang.DisposedException;
+import com.sun.star.lib.TestBed;
+import com.sun.star.lib.uno.typeinfo.MethodTypeInfo;
+import com.sun.star.lib.uno.typeinfo.TypeInfo;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import complexlib.ComplexTestCase;
+
+/**
+ * Test case for bug #97697#.
+ *
+ * <p>Bug #97697# "GPF in java-uno bridge in bugdoc scenario" shows that sending
+ * a plain <code>Object</code> as an <code>Any</code> over the URP bridge lead
+ * to a <code>StackOverflowError</code> on the writer thread, which was silently
+ * discarded (and the bridge was not disposed).</p>
+ *
+ * <p>This test has to detect whether the spawned client process indeed hangs,
+ * which can not be done reliably.  As an approximation, it waits for 10 sec and
+ * considers the process hanging if it has not completed by then.</p>
+ */
+public final class Bug97697_Test extends ComplexTestCase {
+    public String getTestObjectName() {
+        return getClass().getName();
+    }
+
+    public String[] getTestMethodNames() {
+        return new String[] { "test" };
+    }
+
+    public void test() throws Exception {
+        TestBed t = new TestBed();
+        assure("test", t.execute(new Provider(t), true, Client.class, 10000));
+    }
+
+    public static final class Client extends TestBed.Client {
+        public static void main(String[] args) {
+            new Client().execute();
+        }
+
+        protected boolean run(XBridge bridge) throws Throwable {
+            XTransport transport = (XTransport) UnoRuntime.queryInterface(
+                XTransport.class, bridge.getInstance("Transport"));
+            try {
+                transport.getAny();
+            } catch (DisposedException e) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static final class Provider implements XInstanceProvider {
+        public Provider(TestBed testBed) {
+            this.testBed = testBed;
+        }
+
+        public Object getInstance(String instanceName) {
+            return new XTransport() {
+                    public Object getAny() {
+                        testBed.serverDone(true);
+                        return new Object();
+                    }
+                };
+        }
+
+        private final TestBed testBed;
+    }
+
+    public interface XTransport extends XInterface {
+        Object getAny();
+
+        TypeInfo[] UNOTYPEINFO = { new MethodTypeInfo("getAny", 0, 0) };
+    }
+}
