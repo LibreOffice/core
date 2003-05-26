0d67aa593cc34a241bd4e6158c374a40e37eb229
Vladimir Glazounov <vg@openoffice.org>
2003-05-26 07:52:35 +0000

    INTEGRATION: CWS draw12 (1.2.28); FILE MERGED 2003/05/16 11:53:34 cl 1.2.28.1: #109253# use correct resource manager for error strings

--- filter/source/xsltdialog/xmlfiltertabdialog.cxx
+++ filter/source/xsltdialog/xmlfiltertabdialog.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlfiltertabdialog.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 16:34:39 $
+ *  last change: $Author: vg $ $Date: 2003-05-26 08:52:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -335,7 +335,7 @@ bool XMLFilterTabDialog::onOk()
         maTabCtrl.SetCurPageId( (USHORT)nErrorPage );
         ActivatePageHdl( &maTabCtrl );
 
-        ResId aResId( nErrorId );
+        ResId aResId( nErrorId, &mrResMgr );
         String aMessage( aResId );
 
         if( aReplace2.Len() )
