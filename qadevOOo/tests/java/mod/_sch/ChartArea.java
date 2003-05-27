ec2ad933894300e478f36c1b7b53225e5e81e3f7
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:20:44 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:57:32 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sch/ChartArea.java
+++ qadevOOo/tests/java/mod/_sch/ChartArea.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ChartArea.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:17:14 $
+ *  last change:$Date: 2003-05-27 13:20:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sch;
 
 import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.chart.XChartDocument;
 import com.sun.star.lang.XComponent;
 import com.sun.star.uno.UnoRuntime;
@@ -104,7 +105,7 @@ public class ChartArea extends TestCase {
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a chartdocument" );
