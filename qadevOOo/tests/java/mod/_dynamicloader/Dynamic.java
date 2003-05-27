07626045cc49dc0a6af919d805be98b5e0f77728
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:38:48 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:55:45 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_dynamicloader/Dynamic.java
+++ qadevOOo/tests/java/mod/_dynamicloader/Dynamic.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Dynamic.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:14:47 $
+ *  last change:$Date: 2003-05-27 12:38:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,7 @@ public class Dynamic extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance( "com.sun.star.loader.Dynamic" );
         }
         catch( com.sun.star.uno.Exception e ) {
