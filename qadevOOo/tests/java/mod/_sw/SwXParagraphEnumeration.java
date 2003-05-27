8825380c7ca15f7337bf1ac9a1a45fee6e5f4682
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:47:22 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:58:27 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXParagraphEnumeration.java
+++ qadevOOo/tests/java/mod/_sw/SwXParagraphEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXParagraphEnumeration.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:18:34 $
+ *  last change:$Date: 2003-05-27 13:47:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sw;
 
 import com.sun.star.container.XEnumeration;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.text.ControlCharacter;
 import com.sun.star.text.XText;
@@ -95,7 +96,7 @@ public class SwXParagraphEnumeration extends TestCase {
     * Creates text document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         try {
             log.println( "creating a textdocument" );
             xTextDoc = SOF.createTextDoc( null );
