c487248ec8062abb56be24fdb44c55b244c62b3f
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:13:13 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:46 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_xmloff/Draw/XMLSettingsExporter.java
+++ qadevOOo/tests/java/mod/_xmloff/Draw/XMLSettingsExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLSettingsExporter.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:20:00 $
+ *  last change:$Date: 2003-05-27 14:13:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,7 +112,7 @@ public class XMLSettingsExporter extends TestCase {
     protected void initialize( TestParameters tParam, PrintWriter log ) {
 
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a drawdocument" );
@@ -156,7 +156,7 @@ public class XMLSettingsExporter extends TestCase {
     protected synchronized TestEnvironment createTestEnvironment
             (TestParameters tParam, PrintWriter log) {
 
-        XMultiServiceFactory xMSF = tParam.getMSF() ;
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)tParam.getMSF() ;
         XInterface oObj = null;
 
         FilterChecker filter = new FilterChecker(log) ;
--- qadevOOo/tests/java/mod/_xmloff/Draw/XMLSettingsImporter.java
+++ qadevOOo/tests/java/mod/_xmloff/Draw/XMLSettingsImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLSettingsImporter.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:20:00 $
+ *  last change:$Date: 2003-05-27 14:13:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -110,7 +110,7 @@ public class XMLSettingsImporter extends TestCase {
     protected void initialize( TestParameters tParam, PrintWriter log ) {
 
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a drawdocument" );
@@ -160,7 +160,7 @@ public class XMLSettingsImporter extends TestCase {
         // first we write what we are intend to do to log file
         log.println( "creating a test environment" );
 
-        XMultiServiceFactory xMSF = tParam.getMSF() ;
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)tParam.getMSF() ;
 
         try {
             oInt = xMSF.createInstance
