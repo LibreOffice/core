a2b1a7da165198fdacd7f85d125b8d9dcd2ca549
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:06:06 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:56:53 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
+++ qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScFunctionListObj.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:16:29 $
+ *  last change:$Date: 2003-05-27 13:06:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -100,7 +100,7 @@ public class ScFunctionListObj extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
@@ -140,7 +140,7 @@ public class ScFunctionListObj extends TestCase {
 
         log.println("Getting test object ") ;
 
-        XMultiServiceFactory oDocMSF = Param.getMSF();
+        XMultiServiceFactory oDocMSF = (XMultiServiceFactory)Param.getMSF();
         try {
             oObj =  (XInterface)oDocMSF.createInstance(
                 "com.sun.star.sheet.FunctionDescriptions");
