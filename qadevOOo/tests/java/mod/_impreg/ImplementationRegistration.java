a182f0e61744e8497d7e931025411c2733550a3b
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:51:08 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:56:20 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_impreg/ImplementationRegistration.java
+++ qadevOOo/tests/java/mod/_impreg/ImplementationRegistration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ImplementationRegistration.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:15:42 $
+ *  last change:$Date: 2003-05-27 12:51:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,7 +96,7 @@ public class ImplementationRegistration extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ( "com.sun.star.registry.ImplementationRegistration" );
         } catch( com.sun.star.uno.Exception e ) {
