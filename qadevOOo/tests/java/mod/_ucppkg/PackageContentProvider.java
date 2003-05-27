7de807179548468f6f6761bd73360b3e28c17567
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:09:34 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:35 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_ucppkg/PackageContentProvider.java
+++ qadevOOo/tests/java/mod/_ucppkg/PackageContentProvider.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: PackageContentProvider.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:19:49 $
+ *  last change:$Date: 2003-05-27 14:09:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -81,7 +81,7 @@ public class PackageContentProvider extends TestCase {
         Object ucb = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ( "com.sun.star.comp.ucb.PackageContentProvider" );
             ucb = xMSF.createInstance
