7284beb50d2e5f1a376d1b38db14fc28af014ae9
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 14:09:43 +0000

    INTEGRATION: CWS calc11 (1.10.46); FILE MERGED 2003/05/26 19:05:44 nn 1.10.46.1: #109031# set OutputDevice digit language according to CTL settings

--- sc/source/ui/view/tabview5.cxx
+++ sc/source/ui/view/tabview5.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabview5.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-26 18:06:49 $
+ *  last change: $Author: vg $ $Date: 2003-05-27 15:09:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -570,6 +570,14 @@ void ScTabView::UpdateDrawTextOutliner()
         if (pOL)
             aViewData.UpdateOutlinerFlags( *pOL );
     }
+}
+
+void ScTabView::DigitLanguageChanged()
+{
+    LanguageType eNewLang = SC_MOD()->GetOptDigitLanguage();
+    for (USHORT i=0; i<4; i++)
+        if ( pGridWin[i] )
+            pGridWin[i]->SetDigitLanguage( eNewLang );
 }
 
 //---------------------------------------------------------------
