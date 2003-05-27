5e2cf3acd483623a67f92ca0b33e48cede477e42
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:12:31 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:57:04 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScSpreadsheetSettings.java
+++ qadevOOo/tests/java/mod/_sc/ScSpreadsheetSettings.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScSpreadsheetSettings.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:16:15 $
+ *  last change:$Date: 2003-05-27 13:12:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.uno.XInterface;
+import com.sun.star.lang.XMultiServiceFactory;
 import java.io.PrintWriter;
 import lib.StatusException;
 import lib.TestCase;
@@ -99,7 +100,7 @@ public class ScSpreadsheetSettings extends TestCase {
 
         // create testobject here
         try {
-            oObj = (XInterface)tParam.getMSF().
+            oObj = (XInterface)((XMultiServiceFactory)tParam.getMSF()).
                 createInstance("com.sun.star.sheet.GlobalSheetSettings");
         } catch (com.sun.star.uno.Exception e) {
             e.printStackTrace(log);
