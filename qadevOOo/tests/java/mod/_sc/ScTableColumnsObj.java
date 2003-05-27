e90c751fa385639e98827b91fd111a835acbeff4
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:14:21 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:57:07 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScTableColumnsObj.java
+++ qadevOOo/tests/java/mod/_sc/ScTableColumnsObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScTableColumnsObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-04 15:44:25 $
+ *  last change:$Date: 2003-05-27 13:14:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XNameAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.frame.XDesktop;
 import com.sun.star.lang.XComponent;
@@ -111,7 +112,7 @@ public class ScTableColumnsObj extends TestCase {
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a sheetdocument" );
