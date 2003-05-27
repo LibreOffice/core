1070c85836898b393c5c3b400b7e51738226a4b8
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:43:41 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:55:57 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_forms/OHiddenModel.java
+++ qadevOOo/tests/java/mod/_forms/OHiddenModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: OHiddenModel.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-10 10:48:19 $
+ *  last change:$Date: 2003-05-27 12:43:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._forms;
 
 import com.sun.star.container.XNameContainer;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.drawing.XDrawPage;
 import com.sun.star.lang.XComponent;
 import com.sun.star.uno.UnoRuntime;
@@ -128,7 +129,7 @@ public class OHiddenModel extends TestCase {
     * Creates Drawing document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a draw document" );
@@ -169,7 +170,7 @@ public class OHiddenModel extends TestCase {
         log.println( "creating a test environment" );
 
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF());
         String objName = "HiddenControl";
         XInterface ctrl = SOF.createControl(xDrawDoc, objName) ;
 
