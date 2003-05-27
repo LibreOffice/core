ed5e99ce7cdab3e4466059c220e02cd87a9716d6
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:19:36 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:57:30 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sch/ChXChartAxis.java
+++ qadevOOo/tests/java/mod/_sch/ChXChartAxis.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ChXChartAxis.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:17:17 $
+ *  last change:$Date: 2003-05-27 13:19:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sch;
 
 import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.chart.XAxisYSupplier;
 import com.sun.star.chart.XChartDocument;
 import com.sun.star.chart.XDiagram;
@@ -101,7 +102,7 @@ public class ChXChartAxis extends TestCase {
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a chartdocument" );
