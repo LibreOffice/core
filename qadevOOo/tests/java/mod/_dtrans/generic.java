5201e952bf7533f5a8587f689864ebf35b417d5d
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:38:39 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:55:44 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_dtrans/generic.java
+++ qadevOOo/tests/java/mod/_dtrans/generic.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: generic.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:14:46 $
+ *  last change:$Date: 2003-05-27 12:38:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -84,7 +84,7 @@ public class generic extends TestCase {
     protected synchronized TestEnvironment createTestEnvironment
             (TestParameters Param, PrintWriter log) {
 
-        XMultiServiceFactory xMSF = Param.getMSF();
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
         XInterface oObj = null;
 
         try {
