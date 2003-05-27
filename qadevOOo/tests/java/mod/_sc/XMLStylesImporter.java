1fceb8708a755a04a3a7789cba9c1ebe4c7f6efa
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:17:16 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:57:13 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/XMLStylesImporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLStylesImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLStylesImporter.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:16:32 $
+ *  last change:$Date: 2003-05-27 13:17:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -110,7 +110,7 @@ public class XMLStylesImporter extends TestCase {
     * New spreadsheet document created.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
@@ -164,7 +164,7 @@ public class XMLStylesImporter extends TestCase {
         // first we write what we are intend to do to log file
         log.println( "creating a test environment" );
 
-        XMultiServiceFactory xMSF = tParam.getMSF() ;
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)tParam.getMSF() ;
 
         try {
             oInt = xMSF.createInstance
