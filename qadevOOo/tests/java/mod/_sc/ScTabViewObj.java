375a75cd99754d4ebc08f57c631a18910c56e7cb
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:13:47 +0000

    INTEGRATION: CWS qadev6 (1.4.8); FILE MERGED 2003/05/21 10:57:06 sg 1.4.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScTabViewObj.java
+++ qadevOOo/tests/java/mod/_sc/ScTabViewObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScTabViewObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-02-28 09:35:41 $
+ *  last change:$Date: 2003-05-27 13:13:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.frame.XModel;
 import com.sun.star.lang.XComponent;
 import com.sun.star.sheet.XSpreadsheet;
@@ -126,7 +127,7 @@ public class ScTabViewObj extends TestCase {
     public void initialize( TestParameters Param, PrintWriter log ) {
         // get a soffice factory object
 
-        SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF());
 
         try {
             log.println("creating a spreadsheetdocument");
