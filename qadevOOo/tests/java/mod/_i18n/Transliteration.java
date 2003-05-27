41063abc6d63e664271ad4a81f2be30e86ece162
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:50:41 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:56:18 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_i18n/Transliteration.java
+++ qadevOOo/tests/java/mod/_i18n/Transliteration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Transliteration.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:15:34 $
+ *  last change:$Date: 2003-05-27 12:50:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -94,7 +94,7 @@ public class Transliteration extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance( "com.sun.star.i18n.Transliteration" );
         }
         catch( com.sun.star.uno.Exception e ) {
