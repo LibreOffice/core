6f8ea72233a7bc5e67539d176a9d865b26b4010e
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:42:26 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:58:19 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXAutoTextEntry.java
+++ qadevOOo/tests/java/mod/_sw/SwXAutoTextEntry.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXAutoTextEntry.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-06 11:07:36 $
+ *  last change:$Date: 2003-05-27 13:42:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -116,7 +116,7 @@ public class SwXAutoTextEntry extends TestCase {
     * Creates text document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         try {
             log.println( "creating a textdocument" );
             xTextDoc = SOF.createTextDoc( null );
@@ -170,7 +170,7 @@ public class SwXAutoTextEntry extends TestCase {
 
         log.println( "creating a test environment" );
         try {
-            XMultiServiceFactory myMSF = Param.getMSF();
+            XMultiServiceFactory myMSF = (XMultiServiceFactory)Param.getMSF();
             Object oInst = myMSF.createInstance
                 ("com.sun.star.text.AutoTextContainer");
             oContainer = (XAutoTextContainer)
