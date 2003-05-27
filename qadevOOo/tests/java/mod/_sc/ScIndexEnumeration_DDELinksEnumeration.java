9e97c3b26e4afad581b95040f02089f80e25a82c
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:07:53 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:56:56 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DDELinksEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DDELinksEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_DDELinksEnumeration.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-03 12:56:10 $
+ *  last change:$Date: 2003-05-27 13:07:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,7 +92,7 @@ public class ScIndexEnumeration_DDELinksEnumeration extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
@@ -127,7 +127,7 @@ public class ScIndexEnumeration_DDELinksEnumeration extends TestCase {
 
         // create testobject here
 
-        XMultiServiceFactory oMSF = Param.getMSF();
+        XMultiServiceFactory oMSF = (XMultiServiceFactory)Param.getMSF();
         try {
             oInterface = oMSF.createInstance("com.sun.star.frame.Desktop" );
         } catch(com.sun.star.uno.Exception e) {
--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DataPilotTablesEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DataPilotTablesEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_DataPilotTablesEnumeration.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-03 13:01:15 $
+ *  last change:$Date: 2003-05-27 13:07:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.lang.XComponent;
 import com.sun.star.sheet.XDataPilotDescriptor;
@@ -91,7 +92,7 @@ public class ScIndexEnumeration_DataPilotTablesEnumeration extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
