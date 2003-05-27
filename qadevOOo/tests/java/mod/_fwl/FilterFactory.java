daa069b413294a6c160526acc12afd59f4624d44
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:48:38 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:56:12 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_fwl/FilterFactory.java
+++ qadevOOo/tests/java/mod/_fwl/FilterFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: FilterFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:15:33 $
+ *  last change:$Date: 2003-05-27 12:48:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._fwl;
 
 import com.sun.star.beans.PropertyValue;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XNameAccess;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
@@ -111,7 +112,7 @@ public class FilterFactory extends TestCase {
 
         //now get the OButtonControl
         try {
-            oInterface = Param.getMSF().createInstance
+            oInterface = ((XMultiServiceFactory)Param.getMSF()).createInstance
                 ("com.sun.star.comp.framework.FilterFactory") ;
         } catch (com.sun.star.uno.Exception e) {
             log.println("Couldn't get service");
