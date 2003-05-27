603095b39b6c1b1f53f42bc9c584b7b623dfc934
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:21:23 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:57:34 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sch/ChartTitle.java
+++ qadevOOo/tests/java/mod/_sch/ChartTitle.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ChartTitle.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:17:12 $
+ *  last change:$Date: 2003-05-27 13:21:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sch;
 
 import com.sun.star.chart.XChartDocument;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.drawing.XShape;
 import java.io.PrintWriter;
 import lib.StatusException;
@@ -106,7 +107,7 @@ public class ChartTitle extends TestCase {
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
         // get a soffice factory object
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
 
         try {
             log.println( "creating a chartdocument" );
