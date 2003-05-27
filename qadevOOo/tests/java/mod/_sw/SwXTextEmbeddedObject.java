85934216c0111a8191ef209249fd6c86f62f99c0
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:50:24 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:58:33 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXTextEmbeddedObject.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextEmbeddedObject.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextEmbeddedObject.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-06 14:28:21 $
+ *  last change:$Date: 2003-05-27 13:50:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sw;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XNameAccess;
 import com.sun.star.lang.XComponent;
 import com.sun.star.text.XTextDocument;
@@ -108,7 +109,7 @@ public class SwXTextEmbeddedObject extends TestCase {
      *
      */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a textdocument" );
@@ -154,7 +155,7 @@ public class SwXTextEmbeddedObject extends TestCase {
 
         XInterface oObj = null;
         // create testobject here
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         String testdoc = utils.getFullTestURL("SwXTextEmbeddedObject.sdw");
         System.out.println(testdoc);
         try {
