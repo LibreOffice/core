b849bdbe4039e3127e7b34859953281ed841a396
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:41:37 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:55:53 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_forms/ODateControl.java
+++ qadevOOo/tests/java/mod/_forms/ODateControl.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ODateControl.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:15:11 $
+ *  last change:$Date: 2003-05-27 12:41:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._forms;
 
 import com.sun.star.awt.XControlModel;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.awt.XDevice;
 import com.sun.star.awt.XGraphics;
 import com.sun.star.awt.XTextComponent;
@@ -136,7 +137,7 @@ public class ODateControl extends TestCase {
      * Creates a new text document.
      */
     protected void initialize ( TestParameters Param, PrintWriter log) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF() );
 
         try {
             log.println( "creating a textdocument" );
