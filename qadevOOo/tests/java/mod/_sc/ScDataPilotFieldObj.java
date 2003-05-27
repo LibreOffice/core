265e06c74ef4767eb2f5ac6a4bf844f3c39ed920
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:04:42 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:56:50 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScDataPilotFieldObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotFieldObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDataPilotFieldObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-03 10:40:51 $
+ *  last change:$Date: 2003-05-27 13:04:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.lang.XComponent;
 import com.sun.star.sheet.XDataPilotDescriptor;
 import com.sun.star.sheet.XDataPilotTables;
@@ -107,7 +108,7 @@ public class ScDataPilotFieldObj extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
--- qadevOOo/tests/java/mod/_sc/ScDataPilotFieldsObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotFieldsObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDataPilotFieldsObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-03 10:44:41 $
+ *  last change:$Date: 2003-05-27 13:04:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.lang.XComponent;
 import com.sun.star.sheet.XDataPilotDescriptor;
 import com.sun.star.sheet.XDataPilotTables;
@@ -104,7 +105,7 @@ public class ScDataPilotFieldsObj extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
