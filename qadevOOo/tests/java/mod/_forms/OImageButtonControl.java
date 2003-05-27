d359629e853470e7dbbfad3e4b678b318325a281
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:43:53 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:55:57 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_forms/OImageButtonControl.java
+++ qadevOOo/tests/java/mod/_forms/OImageButtonControl.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: OImageButtonControl.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:15:05 $
+ *  last change:$Date: 2003-05-27 12:43:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._forms;
 
 import com.sun.star.awt.XControlModel;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.awt.XDevice;
 import com.sun.star.awt.XGraphics;
 import com.sun.star.awt.XToolkit;
@@ -120,7 +121,7 @@ public class OImageButtonControl extends TestCase {
      * Creates a new text document.
      */
     protected void initialize ( TestParameters Param, PrintWriter log) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF() );
 
         try {
             log.println( "creating a textdocument" );
