dd04b785ab58e6cb0b762bea0b345f6dbf695d8d
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:42:37 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:58:20 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXAutoTextGroup.java
+++ qadevOOo/tests/java/mod/_sw/SwXAutoTextGroup.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXAutoTextGroup.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-06 11:11:07 $
+ *  last change:$Date: 2003-05-27 13:42:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,7 +112,7 @@ public class SwXAutoTextGroup extends TestCase {
     * Creates text document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         try {
             log.println( "creating a textdocument" );
             xTextDoc = SOF.createTextDoc( null );
@@ -148,7 +148,7 @@ public class SwXAutoTextGroup extends TestCase {
 
         log.println( "creating a test environment" );
         try {
-            XMultiServiceFactory myMSF = Param.getMSF();
+            XMultiServiceFactory myMSF = (XMultiServiceFactory)Param.getMSF();
             Object oInst = myMSF.createInstance
                 ("com.sun.star.text.AutoTextContainer");
             oContainer = (XAutoTextContainer)
