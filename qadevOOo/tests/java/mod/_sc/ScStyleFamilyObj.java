bc440b853089ee34abf830269d16cf7768c06140
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:13:13 +0000

    INTEGRATION: CWS qadev6 (1.2.8); FILE MERGED 2003/05/21 10:57:05 sg 1.2.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sc/ScStyleFamilyObj.java
+++ qadevOOo/tests/java/mod/_sc/ScStyleFamilyObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScStyleFamilyObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-04 14:30:22 $
+ *  last change:$Date: 2003-05-27 13:13:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sc;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XNameAccess;
 import com.sun.star.container.XNameContainer;
 import com.sun.star.lang.XComponent;
@@ -109,7 +110,7 @@ public class ScStyleFamilyObj extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
@@ -190,7 +191,7 @@ public class ScStyleFamilyObj extends TestCase {
                 "Exception occured while getting StyleFamily", e);
         }
 
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF());
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
         XComponent xComp = (XComponent)
             UnoRuntime.queryInterface(XComponent.class, xSpreadsheetDoc);
 
--- qadevOOo/tests/java/mod/_sc/ScStyleObj.java
+++ qadevOOo/tests/java/mod/_sc/ScStyleObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScStyleObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-04 14:37:51 $
+ *  last change:$Date: 2003-05-27 13:13:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,7 +112,7 @@ public class ScStyleObj extends TestCase {
     * Creates Spreadsheet document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
 
         try {
             log.println( "creating a Spreadsheet document" );
