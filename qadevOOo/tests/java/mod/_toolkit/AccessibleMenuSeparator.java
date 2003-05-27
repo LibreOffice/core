6f1b1cb61a86a1e5f6f48c04872b0d1c879503b0
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:59:03 +0000

    INTEGRATION: CWS qadev6 (1.2.2); FILE MERGED 2003/05/21 10:59:00 sg 1.2.2.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenuSeparator.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenuSeparator.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleMenuSeparator.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Date: 2003-04-28 11:21:49 $
+ *  last change: $Date: 2003-05-27 13:59:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._toolkit;
 
 import com.sun.star.awt.XWindow;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.text.XTextDocument;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
@@ -116,7 +117,7 @@ public class AccessibleMenuSeparator extends TestCase {
         XInterface oObj = null;
 
         try {
-            oObj = (XInterface) Param.getMSF().createInstance
+            oObj = (XInterface) ((XMultiServiceFactory)Param.getMSF()).createInstance
                 ("com.sun.star.awt.Toolkit") ;
         } catch (com.sun.star.uno.Exception e) {
             log.println("Couldn't get toolkit");
@@ -167,7 +168,7 @@ public class AccessibleMenuSeparator extends TestCase {
      */
     protected void initialize(TestParameters Param, PrintWriter log) {
         try {
-            SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF());
+            SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF());
             xTextDoc = SOF.createTextDoc(null);
         } catch (com.sun.star.uno.Exception e) {
             throw new StatusException("Can't create document", e);
