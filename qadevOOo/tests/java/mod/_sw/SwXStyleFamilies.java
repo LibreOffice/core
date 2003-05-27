c0eb3ed300e9c0cccdc5d157ee3bc5555fe563f9
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:48:47 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:58:30 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXStyleFamilies.java
+++ qadevOOo/tests/java/mod/_sw/SwXStyleFamilies.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXStyleFamilies.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:18:31 $
+ *  last change:$Date: 2003-05-27 13:48:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sw;
 
 import com.sun.star.container.XNameAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.style.XStyleFamiliesSupplier;
 import com.sun.star.text.XTextDocument;
 import com.sun.star.uno.UnoRuntime;
@@ -100,7 +101,7 @@ public class SwXStyleFamilies extends TestCase {
     * Creates text document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         try {
             log.println( "creating a textdocument" );
             xTextDoc = SOF.createTextDoc( null );
