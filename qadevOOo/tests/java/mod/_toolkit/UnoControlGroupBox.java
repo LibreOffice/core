deffdc083c46a2ae74b02eb73e6cdda8679feb14
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:05:34 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:15 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_toolkit/UnoControlGroupBox.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlGroupBox.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoControlGroupBox.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:19:16 $
+ *  last change:$Date: 2003-05-27 14:05:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._toolkit;
 
 import com.sun.star.awt.XControl;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.awt.XControlModel;
 import com.sun.star.awt.XDevice;
 import com.sun.star.awt.XGraphics;
@@ -89,7 +90,7 @@ public class UnoControlGroupBox extends TestCase {
     XTextDocument xTextDoc;
 
     protected void initialize ( TestParameters Param, PrintWriter log) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( Param.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF() );
 
         try {
             log.println( "creating a textdocument" );
