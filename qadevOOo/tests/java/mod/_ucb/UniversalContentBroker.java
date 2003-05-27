777a33263fb4d204bd3014feb23120b60b36fc15
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:08:48 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:32 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_ucb/UniversalContentBroker.java
+++ qadevOOo/tests/java/mod/_ucb/UniversalContentBroker.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UniversalContentBroker.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:19:43 $
+ *  last change:$Date: 2003-05-27 14:08:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,7 +89,7 @@ public class UniversalContentBroker extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ( "com.sun.star.comp.ucb.UniversalContentBroker" );
         } catch( com.sun.star.uno.Exception e ) {
