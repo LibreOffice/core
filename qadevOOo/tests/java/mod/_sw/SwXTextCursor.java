5d7578af12e1eeaeab54c71e3eb21cf5e58898ff
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 12:50:01 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:58:32 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_sw/SwXTextCursor.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextCursor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextCursor.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:18:29 $
+ *  last change:$Date: 2003-05-27 13:49:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 package mod._sw;
 
 import com.sun.star.beans.Property;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.beans.PropertyAttribute;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.text.ControlCharacter;
@@ -137,7 +138,7 @@ public class SwXTextCursor extends TestCase {
     * Creates text document.
     */
     protected void initialize( TestParameters tParam, PrintWriter log ) {
-        SOfficeFactory SOF = SOfficeFactory.getFactory( tParam.getMSF() );
+        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF() );
         try {
             log.println( "creating a textdocument" );
             xTextDoc = SOF.createTextDoc( null );
