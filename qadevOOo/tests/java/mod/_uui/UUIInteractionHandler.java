60d6ba087c101a2226b8d4d10609d3ee0f794aa3
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:10:12 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:37 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_uui/UUIInteractionHandler.java
+++ qadevOOo/tests/java/mod/_uui/UUIInteractionHandler.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UUIInteractionHandler.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:19:52 $
+ *  last change:$Date: 2003-05-27 14:10:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -94,7 +94,7 @@ public class UUIInteractionHandler extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ( "com.sun.star.uui.InteractionHandler" );
         }
