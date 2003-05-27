37ddc1f79ef0a317b74da82c27b9a892f15617fd
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 13:08:22 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:59:30 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_ucb/UcbContentProviderProxyFactory.java
+++ qadevOOo/tests/java/mod/_ucb/UcbContentProviderProxyFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UcbContentProviderProxyFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:19:44 $
+ *  last change:$Date: 2003-05-27 14:08:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -87,7 +87,7 @@ public class UcbContentProviderProxyFactory extends TestCase {
         Object oInterface = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ( "com.sun.star.comp.ucb.UcbContentProviderProxyFactory" );
         } catch( com.sun.star.uno.Exception e ) {
