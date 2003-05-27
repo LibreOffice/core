275925ee0ef8093d048d04829d58f12bf88b7b69
Vladimir Glazounov <vg@openoffice.org>
2003-05-27 11:39:27 +0000

    INTEGRATION: CWS qadev6 (1.1.8); FILE MERGED 2003/05/21 10:55:48 sg 1.1.8.1: #109819# prepare devide of runner

--- qadevOOo/tests/java/mod/_fileacc/SimpleFileAccess.java
+++ qadevOOo/tests/java/mod/_fileacc/SimpleFileAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SimpleFileAccess.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:14:54 $
+ *  last change:$Date: 2003-05-27 12:39:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,7 +96,7 @@ public class SimpleFileAccess extends TestCase {
         Object oIH = null;
 
         try {
-            XMultiServiceFactory xMSF = Param.getMSF();
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
             oInterface = xMSF.createInstance
                 ("com.sun.star.comp.ucb.SimpleFileAccess");
             oIH = xMSF.createInstance("com.sun.star.sdb.InteractionHandler");
