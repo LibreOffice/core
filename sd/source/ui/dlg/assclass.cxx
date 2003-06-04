41955aa02d8380d6b1d986ae362f23c251555b94
Vladimir Glazounov <vg@openoffice.org>
2003-06-04 11:27:14 +0000

    INTEGRATION: CWS draw14 (1.1.1.1.182); FILE MERGED 2003/05/28 13:44:52 af 1.1.1.1.182.1: #109384# Fixed delete of pPageStatus in destructor.

--- sd/source/ui/dlg/assclass.cxx
+++ sd/source/ui/dlg/assclass.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: assclass.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:31 $
+ *  last change: $Author: vg $ $Date: 2003-06-04 12:27:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -225,7 +225,7 @@ Assistent::~Assistent()
         delete pPages[i];
     }
 
-    delete pPageStatus;
+    delete [] pPageStatus;
 }
 
 BOOL Assistent::IsEnabled( UINT8 nPage )
