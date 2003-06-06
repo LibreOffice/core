7052967a439e549bb3941554d7a61983443e0a57
Vladimir Glazounov <vg@openoffice.org>
2003-06-06 09:44:23 +0000

    INTEGRATION: CWS dba07 (1.10.82); FILE MERGED 2003/05/26 16:04:35 fs 1.10.82.1: #109905# lock the SdrUnoControlRec while it's being painted - this prevents unnecessary add/removePropertyChangeListener calls at the control model during painting, plus a deadlock

--- svx/source/svdraw/svdouno.cxx
+++ svx/source/svdraw/svdouno.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svdouno.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-28 12:46:00 $
+ *  last change: $Author: vg $ $Date: 2003-06-06 10:44:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -312,6 +312,8 @@ FASTBOOL SdrUnoObj::Paint(ExtOutputDevice& rXOut, const SdrPaintInfoRec& rInfoRe
 
     if (pControlRec && pControlRec->GetControl().is())
     {
+        SdrUnoControlPaintGuard aLockForPaint( *const_cast< SdrUnoControlRec* >( pControlRec ) );
+
         uno::Reference< awt::XControl > xUnoControl = pControlRec->GetControl();
 
         uno::Reference< awt::XView > xView(xUnoControl, uno::UNO_QUERY);
