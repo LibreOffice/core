e657d76891e0a303e3349540737da58967bfcc0f
Vladimir Glazounov <vg@openoffice.org>
2003-06-10 10:32:55 +0000

    INTEGRATION: CWS sdk02 (1.1.32); FILE MERGED 2003/05/26 16:42:14 np 1.1.32.1: #108925# Enable tag @since and text for tag @deprecated.

--- autodoc/source/display/idl/hfi_doc.cxx
+++ autodoc/source/display/idl/hfi_doc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hfi_doc.cxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: np $ $Date: 2002-11-01 17:14:25 $
+ *  last change: $Author: vg $ $Date: 2003-06-10 11:32:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -102,6 +102,15 @@ HF_IdlDocu::Produce_byData( const client &  i_ce,
     if ( i_pDocu->IsDeprecated() )
     {
         rOut.Produce_Term("[ DEPRECATED ]");
+
+        if (NOT i_pDocu->DeprecatedText().IsEmpty())
+        {
+            HF_IdlDocuTextDisplay
+                aDescription( Env(), 0, i_ce);
+            aDescription.Out().Enter( rOut.Produce_Definition() );
+            i_pDocu->DeprecatedText().DisplayAt( aDescription );
+            aDescription.Out().Leave();
+        }
     }
     if ( i_pDocu->IsOptional() )
     {
