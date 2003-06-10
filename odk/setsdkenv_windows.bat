7a55e9aafcb7b5356f8deadd5cf67dc24c7f418e
Vladimir Glazounov <vg@openoffice.org>
2003-06-10 09:02:40 +0000

    INTEGRATION: CWS sdk02 (1.11.6); FILE MERGED 2003/06/05 14:27:13 jsc 1.11.6.2: #109909# remove version number in setsdk* files also 2003/05/14 10:41:13 jsc 1.11.6.1: #109152,108987# change version number and remove STLPORT

--- odk/setsdkenv_windows.bat
+++ odk/setsdkenv_windows.bat
@@ -5,11 +5,11 @@ REM are necessary for building the examples of the Office Development Kit.
 REM The Script was developed for the operating systems Windows.
 
 REM Installation directory of the Software Development Kit.
-REM Example: set OO_SDK_HOME=D:\StarOffice6.1SDK
+REM Example: set OO_SDK_HOME=D:\StarOfficeSDK
 set OO_SDK_HOME=
 
 REM Office installation directory.
-REM Example: set OFFICE_HOME=C:\Programme\StarOffice6.1
+REM Example: set OFFICE_HOME=C:\Programme\StarOffice
 set OFFICE_HOME=
 
 REM Directory of the make command.
@@ -20,17 +20,9 @@ REM Directory of the C++ tools.
 REM Example:set OO_SDK_CPP_HOME=C:\Programme\Microsoft Visual Studio\VC98\bin
 set OO_SDK_CPP_HOME=
 
-REM STLPORT installation directory.
-REM Example: set OO_STLPORT_HOME=D:\stlport-4.5.3
-set OO_STLPORT_HOME=
-
 REM Java installation directory.
 REM Example: set OO_SDK_JAVA_HOME=c:\j2sdk1.4.1_01
 set OO_SDK_JAVA_HOME=
-
-REM ANT installation directory.
-REM Example: set OO_SDK_ANT_HOME=D:\jakarta-ant-1.4
-REM set OO_SDK_ANT_HOME=
 
 REM Automatic deployment
 REM Example: set SDK_AUTO_DEPLOYMENT=YES
@@ -64,9 +56,6 @@ if not defined OO_SDK_JAVA_HOME (
 REM Set library path.
 set LIB=%OO_SDK_HOME%\windows\lib;%OO_SDK_HOME%\WINexample.out\lib;%LIB%
 
-REM Add directory of the STLPORT library to the library path.
-if defined OO_STLPORT_HOME set LIB=%OO_STLPORT_HOME%\lib;%LIB%
-
 REM Set office program path.
 set OFFICE_PROGRAM_PATH=%OFFICE_HOME%\program
 
@@ -79,14 +68,8 @@ if defined OO_SDK_MAKE_HOME set PATH=%OO_SDK_MAKE_HOME%;%PATH%
 REM Add directory of the C++ tools to the path, if necessary.
 if defined OO_SDK_CPP_HOME set PATH=%OO_SDK_CPP_HOME%;%PATH%
 
-REM Add directory of the STLPORT library to the path, if necessary.
-if defined OO_STLPORT_HOME set PATH=%OO_STLPORT_HOME%\lib;%PATH%
-
 REM Add directory of the Java tools to the path, if necessary.
 if defined OO_SDK_JAVA_HOME set PATH=%OO_SDK_JAVA_HOME%\bin;%OO_SDK_JAVA_HOME%\jre\bin;%PATH%
-
-REM Add directory of the ANT tools to the path, if necessary.
-if defined OO_SDK_ANT_HOME set PATH=%OO_SDK_ANT_HOME%\bin;%PATH%
 
 REM Set environment for C++ tools, if necessary.
 if defined OO_SDK_CPP_HOME call "%OO_SDK_CPP_HOME%\VCVARS32.bat"
