066e851228bbd2253d1335f8bb965353478013e3
Vladimir Glazounov <vg@openoffice.org>
2003-06-10 09:20:16 +0000

    INTEGRATION: CWS sdk02 (1.1.2); FILE ADDED 2003/05/22 15:01:07 jsc 1.1.2.2: #109793# insert changes of SJ 2003/05/09 11:23:10 jsc 1.1.2.1: #109045# insert new and remove example zip file

---
+++ odk/examples/DevelopersGuide/Drawing/ChangeOrderDemo.java
@@ -0,0 +1,156 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ChangeOrderDemo.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2003-06-10 10:20:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+// __________ Imports __________
+
+// base classes
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.lang.*;
+
+// property access
+import com.sun.star.beans.*;
+
+// name access
+import com.sun.star.container.*;
+
+// text
+import com.sun.star.text.*;
+import com.sun.star.style.*;
+
+
+// application specific classes
+import com.sun.star.drawing.*;
+
+// presentation specific classes
+import com.sun.star.presentation.*;
+
+// Point, Size, ..
+import com.sun.star.awt.*;
+import java.io.File;
+
+
+// __________ Implementation __________
+
+/** ChangeOrderDemo
+    @author Sven Jacobi
+ */
+
+public class ChangeOrderDemo
+{
+    public static void main( String args[] )
+    {
+        XComponent xDrawDoc = null;
+        try
+        {
+            String sConnection;
+            if ( args.length >= 1 )
+                sConnection = args[ 1 ];
+            else
+                sConnection = "uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager";
+            XMultiServiceFactory xServiceFactory =
+                Helper.connect( sConnection );
+
+            // suppress Presentation Autopilot when opening the document
+            // properties are the same as described for com.sun.star.document.MediaDescriptor
+            PropertyValue[] pPropValues = new PropertyValue[ 1 ];
+            pPropValues[ 0 ] = new PropertyValue();
+            pPropValues[ 0 ].Name = "Silent";
+            pPropValues[ 0 ].Value = new Boolean( true );
+
+            xDrawDoc = Helper.createDocument( xServiceFactory,
+                "private:factory/sdraw", "_blank", 0, pPropValues );
+
+            // create two rectangles
+            XDrawPage xPage = PageHelper.getDrawPageByIndex( xDrawDoc, 0 );
+            XShapes xShapes = (XShapes)
+                    UnoRuntime.queryInterface( XShapes.class, xPage );
+
+            XShape xShape1 = ShapeHelper.createShape( xDrawDoc,
+                new Point( 1000, 1000 ), new Size( 5000, 5000 ),
+                    "com.sun.star.drawing.RectangleShape" );
+
+            XShape xShape2 = ShapeHelper.createShape( xDrawDoc,
+                new Point( 2000, 2000 ), new Size( 5000, 5000 ),
+                    "com.sun.star.drawing.EllipseShape" );
+
+            xShapes.add( xShape1 );
+            ShapeHelper.addPortion( xShape1, "     this shape was inserted first", false );
+            ShapeHelper.addPortion( xShape1, "by changing the ZOrder it lie now on top", true );
+            xShapes.add( xShape2 );
+
+            XPropertySet xPropSet1 = (XPropertySet)
+                    UnoRuntime.queryInterface( XPropertySet.class, xShape1 );
+            XPropertySet xPropSet2 = (XPropertySet)
+                    UnoRuntime.queryInterface( XPropertySet.class, xShape2 );
+
+            int nOrderOfShape1 = ((Integer)xPropSet1.getPropertyValue( "ZOrder" )).intValue();
+            int nOrderOfShape2 = ((Integer)xPropSet2.getPropertyValue( "ZOrder" )).intValue();
+
+            xPropSet1.setPropertyValue( "ZOrder", new Integer( nOrderOfShape2 ) );
+            xPropSet2.setPropertyValue( "ZOrder", new Integer( nOrderOfShape1 ) );
+        }
+        catch( Exception ex )
+        {
+            System.out.println( ex );
+        }
+        System.exit( 0 );
+    }
+}
