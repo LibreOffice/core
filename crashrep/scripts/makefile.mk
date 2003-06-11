1a01c5e1b690733ab026c9c13f1dc3da9a2e9147
Hennes Rohling <hro@openoffice.org>
2003-06-11 18:24:53 +0000

    #110104# Own crash_report shel startup script

--- crashrep/scripts/makefile.mk
+++ crashrep/scripts/makefile.mk
@@ -2,10 +2,6 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
-#
-#   last change: $Author: hro $ $Date: 2003-06-11 19:24:53 $
-#
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
 #
@@ -60,53 +56,19 @@
 #
 #*************************************************************************
 
-PRJ=..$/..
+PRJ=..
 
 PRJNAME=crashrep
-TARGET=soreport
-LIBTARGET=NO
-ENABLE_EXCEPTIONS=TRUE
-TARGETTYPE=GUI
+TARGET=scripts
 
-# --- Settings -----------------------------------------------------
+# --- Settings -----------------------------------------------------------
 
 .INCLUDE :  settings.mk
 
-# --- Resources ----------------------------------------------------
+# --- Targets -------------------------------------------------------------
 
-RCFILES=$(TARGET).rc
+UNIXTEXT= \
+    $(MISC)$/crash_report.sh
 
-# --- Files --------------------------------------------------------
+.INCLUDE :  target.mk
 
-OBJFILES=\
-    $(OBJ)$/soreport.obj\
-    $(OBJ)$/base64.obj
-    
-LNGFILES=..$/all$/crashrep.lng
-
-APP1OBJS=$(OBJFILES)
-
-APP1NOSAL=TRUE
-                        
-APP1TARGET=$(TARGET)
-
-STDLIB1=\
-    gdi32.lib\
-    comctl32.lib\
-    comdlg32.lib\
-    advapi32.lib\
-    wsock32.lib\
-    dbghelp.lib
-
-APP1NOSVRES=$(RES)$/$(TARGET).res
-
-# --- Targets ------------------------------------------------------
-
-.INCLUDE :	target.mk
-
-# Generate the native Windows resource file
-# using lngconvex.exe 
-
-$(RCFILES) : $(LNGFILES) makefile.mk rcfooter.txt rcheader.txt rctemplate.txt ctrylnglist.txt
-    +lngconvex.exe -lng ..\all\crashrep.lng -rc $(TARGET).rc -c  ctrylnglist.txt  -rct rctemplate.txt -rch rcheader.txt -rcf rcfooter.txt
-    
