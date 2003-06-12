b82fefb492d236af598df327debf3d0ae18fc438
Vladimir Glazounov <vg@openoffice.org>
2003-06-12 09:02:43 +0000

    INTEGRATION: CWS mh11rc (1.3.16); FILE MERGED 2003/06/05 14:30:40 mh 1.3.16.1: join: from beta2

--- solenv/inc/unxlngs3904.mk
+++ solenv/inc/unxlngs3904.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxlngs3904.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2003-04-28 16:47:46 $
+#   last change: $Author: vg $ $Date: 2003-06-12 10:02:43 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -107,8 +107,8 @@ CFLAGSEXCEPTIONS=-fexceptions -fno-enforce-eh-specs
 CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
 # -fpermissive should be removed as soon as possible
-#CFLAGSCXX= -fsigned-char -pipe -fno-for-scope -fpermissive -fno-rtti 
-CFLAGSCXX= -fsigned-char -pipe -fno-for-scope -fpermissive
+CFLAGSCXX= -fsigned-char -pipe -fno-for-scope -fpermissive -fno-rtti 
+#CFLAGSCXX= -fsigned-char -pipe -fno-for-scope -fpermissive
 
 
 # Compiler flags for compiling static object in single threaded environment with graphical user interface
