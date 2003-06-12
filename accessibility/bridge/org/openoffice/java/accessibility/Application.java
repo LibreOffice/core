4f74af7c9f401a443d0d83ddcec5d34f8ac524d7
Rüdiger Timm <rt@openoffice.org>
2003-06-12 06:58:47 +0000

    INTEGRATION: CWS uaa04 (1.1.46); FILE MERGED 2003/06/04 07:21:06 obr 1.1.46.1: finished transition from tabs to spaces

--- accessibility/bridge/org/openoffice/java/accessibility/Application.java
+++ accessibility/bridge/org/openoffice/java/accessibility/Application.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Application.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: obr $ $Date: 2002-12-06 11:25:33 $
+ *  last change: $Author: rt $ $Date: 2003-06-12 07:58:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
