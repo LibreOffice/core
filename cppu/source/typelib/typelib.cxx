7d3186672bf2010bae18756aeec8ab514e4fe370
Vladimir Glazounov <vg@openoffice.org>
2003-06-12 09:33:51 +0000

    INTEGRATION: CWS mh11rc (1.17.8); FILE MERGED 2003/06/05 08:24:21 dbo 1.17.8.1: #i11781# compiler warnings

--- cppu/source/typelib/typelib.cxx
+++ cppu/source/typelib/typelib.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: typelib.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:37:12 $
+ *  last change: $Author: vg $ $Date: 2003-06-12 10:33:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,7 +74,7 @@
 #ifdef SOLARIS
 #include <alloca.h>
 #endif
-#include <new.h>
+#include <new>
 
 #ifndef _OSL_INTERLCK_H_
 #include <osl/interlck.h>
