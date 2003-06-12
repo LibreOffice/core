bd2e4457787971f0cd0e120f5690e8edc14b314d
Rüdiger Timm <rt@openoffice.org>
2003-06-12 06:58:37 +0000

    INTEGRATION: CWS uaa04 (1.2.12); FILE MERGED 2003/06/04 07:21:06 obr 1.2.12.1: finished transition from tabs to spaces

--- accessibility/bridge/org/openoffice/java/accessibility/AccessibleValueImpl.java
+++ accessibility/bridge/org/openoffice/java/accessibility/AccessibleValueImpl.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleValueImpl.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-24 18:13:35 $
+ *  last change: $Author: rt $ $Date: 2003-06-12 07:58:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
