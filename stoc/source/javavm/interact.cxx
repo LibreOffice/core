af627f09c21a49ce031655e3c2e119d46bd83dc5
Vladimir Glazounov <vg@openoffice.org>
2003-06-12 10:08:24 +0000

    INTEGRATION: CWS mh11rc (1.4.50); FILE MERGED 2003/06/06 08:16:55 mh 1.4.50.1: join: from beta2

--- stoc/source/javavm/interact.cxx
+++ stoc/source/javavm/interact.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: interact.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: sb $ $Date: 2002-12-06 10:48:58 $
+ *  last change: $Author: vg $ $Date: 2003-06-12 11:08:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -129,7 +129,7 @@ InteractionRequest::InteractionRequest(css::uno::Any const & rRequest):
     m_aRequest(rRequest)
 {
     bool bRetry;
-#if defined LINUX
+#if defined LINUX || defined FREEBSD
     // Only if Java is disabled we allow retry:
     bRetry = m_aRequest.isExtractableTo(
         getCppuType(static_cast< css::java::JavaDisabledException * >(0)));
