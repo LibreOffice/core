38ab7316f1e7a6bf692132193ba74773992969c0
Rüdiger Timm <rt@openoffice.org>
2003-06-12 06:56:56 +0000

    INTEGRATION: CWS uaa04 (1.2.12); FILE MERGED 2003/06/04 07:20:59 obr 1.2.12.1: finished transition from tabs to spaces

--- accessibility/bridge/org/openoffice/java/accessibility/AccessibleEditableTextImpl.java
+++ accessibility/bridge/org/openoffice/java/accessibility/AccessibleEditableTextImpl.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleEditableTextImpl.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-24 18:12:12 $
+ *  last change: $Author: rt $ $Date: 2003-06-12 07:56:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
