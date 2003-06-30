dbbc7ae2325e3b1e6943447a5f9143a93521e810
Jens-Heiner Rechtien <hr@openoffice.org>
2003-06-30 15:02:17 +0000

    INTEGRATION: CWS sdk04 (1.2.6); FILE MERGED 2003/06/25 08:39:44 jsc 1.2.6.1: #110423# add gif

--- odk/util/check.pl
+++ odk/util/check.pl
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: check.pl,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: vg $ $Date: 2003-06-10 10:51:20 $
+#   last change: $Author: hr $ $Date: 2003-06-30 16:02:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,7 +104,7 @@ if (-d "$StartDir") {
     if (-d "$StartDir/docs") {
     my @filelist = ( "install.html","DevelopersGuide_intro.html","notsupported.html",
              "sdk_styles.css", "tools.html", "images/black_dot.gif",
-             "images/logo.gif", "images/logo.gif");
+             "images/logo.gif", "images/sdk_logo.gif", "images/nada.gif");
 
     foreach $i (@filelist)
     {
