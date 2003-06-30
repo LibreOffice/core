dd524c043060fee487a70804b8c0ecceb8cbf0c1
Jens-Heiner Rechtien <hr@openoffice.org>
2003-06-30 14:51:25 +0000

    INTEGRATION: CWS aig04 (1.2.216); FILE MERGED 2003/06/12 12:41:45 hbrinkm 1.2.216.1: #108791# new: SwDropDownField{Type}

--- sw/source/core/inc/unofldmid.h
+++ sw/source/core/inc/unofldmid.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unofldmid.h,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: os $ $Date: 2002-11-15 11:09:20 $
+ *  last change: $Author: hr $ $Date: 2003-06-30 15:51:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,6 +89,7 @@ namespace com { namespace sun { namespace star { namespace uno {
 #define FIELD_PROP_PROP_SEQ         26
 #define FIELD_PROP_LOCALE           27
 #define FIELD_PROP_BOOL4            28
+#define FIELD_PROP_STRINGS          29
 
 String& GetString( const com::sun::star::uno::Any&, String& rStr );
 
