8f9a29ebf7e4cc9c9c5a6c92c33a8ed2e3436ec4
Vladimir Glazounov <vg@openoffice.org>
2003-07-02 12:39:07 +0000

    INTEGRATION: CWS ooo11rc (1.15.2); FILE MERGED 2003/07/01 00:52:25 khendricks 1.15.2.1: adding luxi sans before Vera in UI_FALLBACK Issue 15238 for iso-8859-2 using locales

--- vcl/source/gdi/fontcfg.cxx
+++ vcl/source/gdi/fontcfg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fontcfg.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 14:29:06 $
+ *  last change: $Author: vg $ $Date: 2003-07-02 13:39:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -406,7 +406,7 @@ const OUString& DefaultFontConfigItem::getDefaultFont( int nLanguage, int nType 
 
 const OUString& DefaultFontConfigItem::getUserInterfaceFont( int nLanguage ) const
 {
-    #define FALLBACKFONT_UI_SANS "Andale Sans UI;Tahoma;Arial Unicode MS;Arial;Bitstream Vera Sans;Interface User;Geneva;WarpSans;Dialog;Swiss;Lucida;Helvetica;Charcoal;Chicago;MS Sans Serif;Helv;Times;Times New Roman;Interface System"
+    #define FALLBACKFONT_UI_SANS "Andale Sans UI;Tahoma;Arial Unicode MS;Arial;Luxi Sans;Bitstream Vera Sans;Interface User;Geneva;WarpSans;Dialog;Swiss;Lucida;Helvetica;Charcoal;Chicago;MS Sans Serif;Helv;Times;Times New Roman;Interface System"
     #define FALLBACKFONT_UI_SANS_ARABIC "Tahoma;Traditional Arabic;Simplified Arabic;Lucidasans;Lucida Sans;Supplement;Andale Sans UI;Interface User;Arial Unicode MS;Lucida Sans Unicode;WarpSans;Geneva;MS Sans Serif;Helv;Dialog;Albany;Lucida;Helvetica;Charcoal;Chicago;Arial;Helmet;Interface System;Sans Serif"
     #define FALLBACKFONT_UI_SANS_THAI "OONaksit;Tahoma;Lucidasans;Arial Unicode MS"
     #define FALLBACKFONT_UI_SANS_KOREAN "SunGulim;Gulim;Roundgothic;Arial Unicode MS;Lucida Sans Unicode;Tahoma;Andale Sans UI"
