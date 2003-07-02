e34fa64393275c8601ef6845dfaf7e889a96559b
Vladimir Glazounov <vg@openoffice.org>
2003-07-02 12:35:06 +0000

    INTEGRATION: CWS ooo11rc (1.17.34); FILE MERGED 2003/06/21 14:38:05 pjanik 1.17.34.2: #i15895#: Fix preprocessor usage. 2003/06/18 13:00:55 mh 1.17.34.1: join: from ooo11beta2

--- sal/osl/unx/system.h
+++ sal/osl/unx/system.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: system.h,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-28 17:13:35 $
+ *  last change: $Author: vg $ $Date: 2003-07-02 13:35:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -212,12 +212,19 @@ extern struct ps_strings *__ps_strings;
 #   include <dlfcn.h>
 #   include <sys/filio.h>
 #   include <sys/ioctl.h>
+#   include <sys/param.h>
 #   include <sys/time.h>
 #   include <sys/uio.h>
+#   include <sys/exec.h>
+#   include <vm/vm.h>
+#   include <vm/vm_param.h>
+#   include <vm/pmap.h>
+#   include <vm/swap_pager.h>
 #   include <sys/un.h>
 #   include <netinet/tcp.h>
 #   define  IORESOURCE_TRANSFER_BSD
 #   include <machine/endian.h>
+#if __FreeBSD_version < 500000
 #   if BYTE_ORDER == LITTLE_ENDIAN
 #       define _LITTLE_ENDIAN
 #   elif BYTE_ORDER == BIG_ENDIAN
@@ -225,12 +232,14 @@ extern struct ps_strings *__ps_strings;
 #   elif BYTE_ORDER == PDP_ENDIAN
 #       define _PDP_ENDIAN
 #   endif
-#   define  sched_yield()               pthread_yield()
-#   define  pthread_testcancel()
+#endif
 #   define  NO_PTHREAD_RTL
-#   define  NO_PTHREAD_PRIORITY
-#   define  CMD_ARG_PRG                 __progname
-#   define  CMD_ARG_ENV                 environ
+/* __progname isn't sufficient here. We need the full path as well
+ * for e.g. setup and __progname only points to the binary name.
+ */
+#   define  CMD_ARG_PRG_IS_DEFINED
+#   define  CMD_ARG_PRG         *((struct ps_strings *)PS_STRINGS)->ps_argvstr
+#   define  CMD_ARG_ENV         environ
 #endif
 
 #ifdef SCO
@@ -556,12 +565,14 @@ extern int sem_post(sem_t* sem);
 #endif
 
 #ifdef NO_PTHREAD_RTL
+#if !defined FREEBSD || (__FreeBSD_version < 500112)
 struct passwd *getpwent_r(struct passwd *pwd, char *buffer,  int buflen);
 extern struct spwd *getspnam_r(const char *name, struct spwd *result,
                                char *buffer, int buflen);
 
 struct tm *localtime_r(const time_t *timep, struct tm *buffer);
 struct tm *gmtime_r(const time_t *timep, struct tm *buffer);
+#endif /* !defined FREEBSD || (__FreeBSD_version < 500112) */
 struct hostent *gethostbyname_r(const char *name, struct hostent *result,
                                 char *buffer, int buflen, int *h_errnop);
 #endif
