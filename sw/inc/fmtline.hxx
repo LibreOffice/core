22e36684496aa122e0373beb38d29ef7847be94e
Jens-Heiner Rechtien <hr@openoffice.org>
2003-07-16 17:06:05 +0000

    INTEGRATION: CWS ooo11rc2 (1.4.268); FILE MERGED 2003/07/15 03:08:53 fa 1.4.268.1: Conditionalize OS X + gcc 2.95 hacks for gcc 2.95 only.
    http://www.openoffice.org/issues/show_bug.cgi?id=16849
    
    Contributor:
    Kevin Hendricks
    
    Dan
    fa@ooo

--- sw/inc/fmtline.hxx
+++ sw/inc/fmtline.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtline.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2002-08-23 13:32:13 $
+ *  last change: $Author: hr $ $Date: 2003-07-16 18:06:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -105,7 +105,7 @@ public:
     void SetCountLines( BOOL b )     { bCountLines = b;    }
 };
 
-#ifndef MACOSX
+#if !(defined(MACOSX) && ( __GNUC__ < 3 ))
 // GrP moved to gcc_outl.cxx; revisit with gcc3
 inline const SwFmtLineNumber &SwAttrSet::GetLineNumber(BOOL bInP) const
     { return (const SwFmtLineNumber&)Get( RES_LINENUMBER,bInP); }
