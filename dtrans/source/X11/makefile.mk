7c2986291a0d8e795878b753fb41ae2cb8904128
Jens-Heiner Rechtien <hr@openoffice.org>
2003-07-16 16:33:36 +0000

    INTEGRATION: CWS ooo11rc2 (1.7.18); FILE MERGED 2003/07/15 03:38:24 fa 1.7.18.1: Fixes for OS X.
    http://www.openoffice.org/issues/show_bug.cgi?id=16859
    
    Contributor:
    Kevin Hendricks
    
    Dan
    fa@ooo

--- dtrans/source/X11/makefile.mk
+++ dtrans/source/X11/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-15 14:05:38 $
+#   last change: $Author: hr $ $Date: 2003-07-16 17:33:36 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,12 +77,12 @@ LIBTARGET=NO
 
 # ------------------------------------------------------------------
 
-.IF "$(OS)"=="MACOSX"
+.IF "$(GUIBASE)"=="aqua"
 
 dummy:
     @echo "Nothing to build for Mac OS X"
  
-.ELSE		# "$(OS)"=="MACOSX"
+.ELSE		# "$(GUIBASE)"=="aqua"
 
 .IF "$(COM)$(CPU)" == "C50I" || "$(COM)$(CPU)" == "C52I"
 NOOPTFILES=\
