5b9f2f7bebd66e844e2fd9bd8639215719b43001
Jens-Heiner Rechtien <hr@openoffice.org>
2003-07-16 16:51:37 +0000

    INTEGRATION: CWS ooo11rc2 (1.7.16); FILE MERGED 2003/07/14 15:18:52 fa 1.7.16.1: Conditionalize OS X + gcc 2.95 hacks for gcc 2.95 only.
    http://www.openoffice.org/project/www/issues/show_bug.cgi?id=16611
    
    Dan
    fa@ooo

--- cppuhelper/inc/cppuhelper/implbase9.hxx
+++ cppuhelper/inc/cppuhelper/implbase9.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: implbase9.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: dbo $ $Date: 2001-11-09 13:49:15 $
+ *  last change: $Author: hr $ $Date: 2003-07-16 17:51:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,7 +61,8 @@
 #ifndef _CPPUHELPER_IMPLBASE9_HXX_
 #define _CPPUHELPER_IMPLBASE9_HXX_
 
-#ifdef MACOSX /* use old impl helpers for macosx */
+#if defined( MACOSX ) && ( __GNUC__ < 3 )
+/* use old impl helpers for macosx */
 
 #define __IFC9 Ifc1, Ifc2, Ifc3, Ifc4, Ifc5, Ifc6, Ifc7, Ifc8, Ifc9
 #define __CLASS_IFC9 class Ifc1, class Ifc2, class Ifc3, class Ifc4, class Ifc5, class Ifc6, class Ifc7, class Ifc8, class Ifc9
