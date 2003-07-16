ee3956bc4946fc998584e9353370daba4d9df709
Jens-Heiner Rechtien <hr@openoffice.org>
2003-07-16 16:43:29 +0000

    INTEGRATION: CWS ooo11rc2 (1.3.80); FILE MERGED 2003/07/15 02:51:25 fa 1.3.80.1: Conditionalize OS X + gcc 2.95 hacks for gcc 2.95 only.
    http://www.openoffice.org/issues/show_bug.cgi?id=16836
    
    Dan
    fa@ooo

--- i18npool/source/registerservices/makefile.mk
+++ i18npool/source/registerservices/makefile.mk
@@ -2,9 +2,9 @@
 #*
 #*  $RCSfile: makefile.mk,v $
 #*
-#*  $Revision: 1.3 $
+#*  $Revision: 1.4 $
 #*
-#*  last change: $Author: bustamam $ $Date: 2002-03-26 13:29:54 $
+#*  last change: $Author: hr $ $Date: 2003-07-16 17:43:29 $
 #*
 #*  The Contents of this file are made available subject to the terms of
 #*  either of the following licenses
@@ -79,11 +79,7 @@ SLOFILES=   \
 
 # Make symbol renaming match library name for Mac OS X
 .IF "$(OS)"=="MACOSX"
-.IF $(UPD)>=625
 SYMBOLPREFIX=i18n$(UPD)$(DLLPOSTFIX)
-.ELSE
-SYMBOLPREFIX=int$(UPD)$(DLLPOSTFIX)
-.ENDIF
 .ENDIF
 
 # --- Targets ------------------------------------------------------
