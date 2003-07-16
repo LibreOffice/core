e740b60ee9dd4fbe1478e971f28650a30d81b218
Jens-Heiner Rechtien <hr@openoffice.org>
2003-07-16 17:05:31 +0000

    INTEGRATION: CWS ooo11rc2 (1.5.272); FILE MERGED 2003/07/15 03:08:52 fa 1.5.272.1: Conditionalize OS X + gcc 2.95 hacks for gcc 2.95 only.
    http://www.openoffice.org/issues/show_bug.cgi?id=16849
    
    Contributor:
    Kevin Hendricks
    
    Dan
    fa@ooo

--- sw/inc/fmtfsize.hxx
+++ sw/inc/fmtfsize.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtfsize.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2002-08-23 13:21:17 $
+ *  last change: $Author: hr $ $Date: 2003-07-16 18:05:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -151,7 +151,7 @@ public:
                                       const SvxULSpaceItem *pULSpace ) const;
 };
 
-#ifndef MACOSX
+#if !(defined(MACOSX) &&  ( __GNUC__ < 3 ))
 // GrP moved to gcc_outl.cxx; revisit with gcc3
 inline const SwFmtFrmSize &SwAttrSet::GetFrmSize(BOOL bInP) const
     { return (const SwFmtFrmSize&)Get( RES_FRM_SIZE,bInP); }
