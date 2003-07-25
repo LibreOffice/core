380699bb88c43c31bce5e47bfaeea0f97115feb2
Vladimir Glazounov <vg@openoffice.org>
2003-07-25 10:37:05 +0000

    INTEGRATION: CWS abi3 (1.6.38); FILE MERGED 2003/07/17 15:17:18 abi 1.6.38.1: #110335# download in the background

--- ucbhelper/util/makefile.mk
+++ ucbhelper/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: hjs $ $Date: 2002-03-28 16:15:16 $
+#   last change: $Author: vg $ $Date: 2003-07-25 11:37:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -81,6 +81,7 @@ LIB1FILES=	$(SLB)$/client.lib \
 
 SHL1TARGET=	$(TARGET)$(UCBHELPER_MAJOR)$(COMID)
 SHL1STDLIBS = \
+    $(COMPHELPERLIB)  \
     $(CPPUHELPERLIB) \
     $(CPPULIB) \
     $(SALHELPERLIB) \
