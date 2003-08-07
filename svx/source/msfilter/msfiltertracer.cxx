d2e291fcbbd339d2bf6c9882fa5a714196056b78
Jens-Heiner Rechtien <hr@openoffice.org>
2003-08-07 14:25:19 +0000

    INTEGRATION: CWS filtertracer01 (1.2.140); FILE MERGED 2003/07/28 13:38:49 sj 1.2.140.3: DocumentURL attribute is now containing the name of the document which is traced instead of the name of the logfile 2003/07/14 12:45:51 sj 1.2.140.2: not using wchart* if initializing rtl::OUString, because wchart* is 32bit on some platforms 2003/07/11 13:47:01 sj 1.2.140.1: implementing filter tracing

--- svx/source/msfilter/msfiltertracer.cxx
+++ svx/source/msfilter/msfiltertracer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: msfiltertracer.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:03:42 $
+ *  last change: $Author: hr $ $Date: 2003-08-07 15:25:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,12 @@
 #ifndef _MS_FILTERTRACER_HXX
 #include "msfiltertracer.hxx"
 #endif
-
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
 #ifndef _COM_SUN_STAR_UNO_SEQUENCE_H_
 #include <com/sun/star/uno/Sequence.h>
 #endif
@@ -81,9 +86,22 @@
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #endif
+#ifndef _COM_SUN_STAR_IO_XACTIVEDATASOURCE_HPP_
+#include <com/sun/star/io/XActiveDataSource.hpp>
+#endif
 #ifndef _FILTER_CONFIG_ITEM_HXX_
 #include <svtools/FilterConfigItem.hxx>
 #endif
+#ifndef _UNOTOOLS_LOCALFILEHELPER_HXX
+#include <unotools/localfilehelper.hxx>
+#endif
+#ifndef _UTL_STREAM_WRAPPER_HXX_
+#include <unotools/streamwrap.hxx>
+#endif
+#ifndef _UNTOOLS_UCBSTREAMHELPER_HXX
+#include <unotools/ucbstreamhelper.hxx>
+#endif
+
 
 // --------------
 // - Namespaces -
@@ -91,98 +109,199 @@
 
 using namespace ::com::sun::star;
 
-MSFilterTracer::MSFilterTracer() :
-    mpCfgItem( NULL )
+MSFilterTracer::MSFilterTracer( const ::rtl::OUString& rConfigPath, ::uno::Sequence< ::beans::PropertyValue >* pConfigData ) :
+    mpCfgItem( new FilterConfigItem( rConfigPath, pConfigData ) ),
+    mpAttributeList( new SvXMLAttributeList() ),
+    mpStream( NULL ),
+    mbEnabled( sal_False )  // will be set to true in StartTracing()
 {
-
-};
-
-MSFilterTracer::~MSFilterTracer()
-{
-    delete mpCfgItem;
-};
-
-MSFilterTracer& MSFilterTracer::operator=( const MSFilterTracer& rFilterTracer )
-{
-    delete mpCfgItem, mpCfgItem = new FilterConfigItem( *rFilterTracer.mpCfgItem );
-    mbEnabled = rFilterTracer.mbEnabled;
-    mxFilterTracer = rFilterTracer.mxFilterTracer;
-    mxLogger = rFilterTracer.mxLogger;
-    return *this;
-}
-
-void MSFilterTracer::StartTracing( const ::rtl::OUString& rConfigPath )
-{
-    delete mpCfgItem, mpCfgItem = new FilterConfigItem( rConfigPath );
-
-    mbEnabled = mpCfgItem->ReadBool( rtl::OUString::createFromAscii( "Enabled" ), sal_False );
-    if ( mbEnabled )
+    if ( mpCfgItem->ReadBool( rtl::OUString::createFromAscii( "On" ), sal_False ) )
     {
-        sal_Int32 nLogLevel = mpCfgItem->ReadInt32( rtl::OUString::createFromAscii( "LogLevel" ), util::logging::LogLevel::ALL );
-        ::rtl::OUString aEmptyString;
-        ::rtl::OUString aClassFilter( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "ClassFilter" ), aEmptyString ) );
-        ::rtl::OUString aMethodFilter( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "MethodFilter" ), aEmptyString ) );
-        ::rtl::OUString aMessageFilter( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "MessageFilter" ), aEmptyString ) );
-        ::rtl::OUString aURL( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "URL" ), aEmptyString ) );
-        util::SearchAlgorithms eSearchAlgorithm = (util::SearchAlgorithms)
-            mpCfgItem->ReadInt32( rtl::OUString::createFromAscii( "SearchAlgorithm" ), util::SearchAlgorithms_ABSOLUTE );
-
-        uno::Sequence< uno::Any > aArgument( 1 );
-        uno::Sequence< beans::PropertyValue > aPropValues( 5 );
-        aPropValues[ 0 ].Name  = ::rtl::OUString::createFromAscii( "LogLevel" );
-        aPropValues[ 0 ].Value <<= nLogLevel;
-        aPropValues[ 1 ].Name  = ::rtl::OUString::createFromAscii( "ClassFilter" );
-        aPropValues[ 1 ].Value <<= aClassFilter;
-        aPropValues[ 2 ].Name  = ::rtl::OUString::createFromAscii( "MethodFilter" );
-        aPropValues[ 2 ].Value <<= aMethodFilter;
-        aPropValues[ 3 ].Name  = ::rtl::OUString::createFromAscii( "MessageFilter" );
-        aPropValues[ 3 ].Value <<= aMessageFilter;
-        aPropValues[ 4 ].Name  = ::rtl::OUString::createFromAscii( "URL" );
-        aPropValues[ 4 ].Value <<= aURL;
-        aArgument[ 0 ] <<= aPropValues;
-
         uno::Reference< lang::XMultiServiceFactory > xMgr( ::comphelper::getProcessServiceFactory() );
         if ( xMgr.is() )
         {
-            mxFilterTracer = xMgr->createInstanceWithArguments( ::rtl::OUString::createFromAscii( "com.sun.star.util.FilterTracer" ), aArgument );
+            /* the following methods try to read a property, if it is not available it will put the second
+            parameter as default into the property sequence of the FilterConfigItem. It means we ensure that
+            the property is available by trying to read it (the return value of the method is ignored) */
+            ::rtl::OUString aEmptyString;
+            mpCfgItem->ReadInt32( rtl::OUString::createFromAscii( "LogLevel" ), util::logging::LogLevel::ALL );
+            mpCfgItem->ReadString( rtl::OUString::createFromAscii( "ClassFilter" ), aEmptyString );
+            mpCfgItem->ReadString( rtl::OUString::createFromAscii( "MethodFilter" ), aEmptyString );
+            mpCfgItem->ReadString( rtl::OUString::createFromAscii( "MessageFilter" ), aEmptyString );
+            util::SearchAlgorithms eSearchAlgorithm = (util::SearchAlgorithms)
+                mpCfgItem->ReadInt32( rtl::OUString::createFromAscii( "SearchAlgorithm" ), util::SearchAlgorithms_ABSOLUTE );
+
+            // creating the name of the log file
+            String aPath( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "Path" ), aEmptyString ) );
+            String aName( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "Name" ), aEmptyString ) );
+            String aDocumentURL( mpCfgItem->ReadString( rtl::OUString::createFromAscii( "DocumentURL" ), aEmptyString ) );
+            INetURLObject aLogFile( aDocumentURL );
+            if ( aLogFile.GetMainURL( INetURLObject::NO_DECODE ).Len() )
+            {
+                if ( aPath.Len() )
+                {
+                    String aOldName( aLogFile.getName( INetURLObject::LAST_SEGMENT, true, INetURLObject::NO_DECODE ) );
+                    aLogFile = INetURLObject( aPath );
+                    aLogFile.insertName( aOldName );
+                }
+                if ( aName.Len() )
+                    aLogFile.setName( aName );
+            }
+            else
+            {
+                if ( aPath.Len() )
+                    aLogFile = INetURLObject( aPath );
+                else
+                {
+                    String aURLStr;
+                    if( ::utl::LocalFileHelper::ConvertPhysicalNameToURL( Application::GetAppFileName(), aURLStr ) )
+                    {
+                        aLogFile = aURLStr;
+                        aLogFile .removeSegment();
+                        aLogFile .removeFinalSlash();
+                    }
+                }
+                if ( !aName.Len() )
+                    aName = rtl::OUString::createFromAscii( "tracer" );
+                aLogFile.insertName( aName );
+            }
+            aLogFile.setExtension( rtl::OUString::createFromAscii( "log" ) );
+
+            // creating the file stream
+            mpStream = ::utl::UcbStreamHelper::CreateStream( aLogFile.GetMainURL( INetURLObject::NO_DECODE ), STREAM_WRITE | STREAM_TRUNC | STREAM_SHARE_DENYNONE );
+            if ( mpStream )
+            {
+                // creating a wrapper for our stream
+                utl::OOutputStreamWrapper* pHelper = new ::utl::OOutputStreamWrapper( *mpStream );
+                uno::Reference< io::XOutputStream > xOutputStream( pHelper );
+
+                // instanciating the DocumentHandler, then setting the OutputStream
+                mxHandler = uno::Reference< xml::sax::XDocumentHandler >( xMgr->createInstance( rtl::OUString::createFromAscii( "com.sun.star.xml.sax.Writer" ) ), uno::UNO_QUERY );
+                uno::Reference< io::XActiveDataSource > xDocSrc( mxHandler, uno::UNO_QUERY );
+                xDocSrc->setOutputStream( xOutputStream );
+                mxHandler->startDocument();
+                mxHandler->ignorableWhitespace ( rtl::OUString::createFromAscii( " " ) );
+
+                // writing the "DocumentHandler" property, so the FilterTracer component
+                // will use it for the output
+                uno::Any aAny;
+                aAny <<= xDocSrc;
+                mpCfgItem->WriteAny( rtl::OUString::createFromAscii( "DocumentHandler" ), aAny );
+
+                SvXMLAttributeList* pAttrList = new SvXMLAttributeList;
+                pAttrList->AddAttribute( rtl::OUString::createFromAscii( "DocumentURL" ), aDocumentURL );
+                uno::Reference < xml::sax::XAttributeList > xAttributeList(pAttrList);
+                mxHandler->startElement( rtl::OUString::createFromAscii( "Document" ), xAttributeList );
+            }
+
+            uno::Sequence< uno::Any > aArgument( 1 );
+            uno::Sequence< beans::PropertyValue > aPropValues( mpCfgItem->GetFilterData() );
+            aArgument[ 0 ] <<= aPropValues;
+            mxFilterTracer = xMgr->createInstanceWithArguments( rtl::OUString::createFromAscii( "com.sun.star.util.FilterTracer" ), aArgument );
             if ( mxFilterTracer.is() )
             {
-                uno::Reference< util::XTextSearch >
-                    xTextSearch( mxFilterTracer, uno::UNO_QUERY );
+                mxTextSearch = uno::Reference< util::XTextSearch >( mxFilterTracer, uno::UNO_QUERY );
                 mxLogger = uno::Reference< util::logging::XLogger >( mxFilterTracer, uno::UNO_QUERY );
-
-                if ( xTextSearch.is() )
+                if ( mxTextSearch.is() )
                 {
-                    util::SearchOptions aOptions;
-                    aOptions.algorithmType = eSearchAlgorithm;
-                    xTextSearch->setOptions( aOptions );
+                    maSearchOptions.algorithmType = eSearchAlgorithm;
+                    mxTextSearch->setOptions( maSearchOptions );
                 }
             }
         }
     }
 }
 
+MSFilterTracer::~MSFilterTracer()
+{
+    mxLogger = NULL;
+    mxFilterTracer = NULL;
+    if ( mxHandler.is() )
+    {
+        mxHandler->ignorableWhitespace ( rtl::OUString::createFromAscii( " " ) );
+        mxHandler->endElement( rtl::OUString::createFromAscii( "Document" ) );
+        mxHandler->ignorableWhitespace ( rtl::OUString::createFromAscii( " " ) );
+        mxHandler->endDocument();
+        mxHandler = NULL;
+    }
+    delete mpAttributeList;
+    delete mpCfgItem;
+    delete mpStream;
+}
+
+void MSFilterTracer::StartTracing()
+{
+    mbEnabled = mpCfgItem->ReadBool( rtl::OUString::createFromAscii( "On" ), sal_False );
+}
+
 void MSFilterTracer::EndTracing()
 {
     mbEnabled = sal_False;
-    uno::Reference< uno::XInterface > x;
-    mxFilterTracer = x;
-    delete mpCfgItem, mpCfgItem = NULL;
 }
 
-void MSFilterTracer::Trace( const rtl::OUString& rMessage ) const
+void MSFilterTracer::StartElement( const rtl::OUString& rName, uno::Reference< xml::sax::XAttributeList > xAttribs )
+{
+    mxHandler->startElement( rName, xAttribs );
+}
+
+void MSFilterTracer::EndElement( const rtl::OUString& rName )
+{
+    mxHandler->endElement( rName );
+}
+
+void MSFilterTracer::Trace( const rtl::OUString& rElement, const rtl::OUString& rMessage )
 {
     if ( mbEnabled && mxLogger.is() )
     {
-        rtl::OUString aEmpty;
-        mxLogger->logp( 0, aEmpty, aEmpty, rMessage );
+        sal_Bool bFilter = sal_False;
+        if ( rMessage.getLength() && mxTextSearch.is() )
+        {
+            maSearchOptions.searchString = rMessage;
+            mxTextSearch->setOptions(  maSearchOptions );
+            util::SearchResult aSearchResult = mxTextSearch->searchForward( rMessage, 0, rMessage.getLength() );
+            bFilter = aSearchResult.subRegExpressions != 0;
+        }
+        if ( !bFilter )
+        {
+            uno::Reference < xml::sax::XAttributeList > xAttrList( new SvXMLAttributeList( *mpAttributeList ) );
+            mxHandler->startElement( rElement, xAttrList );
+            if ( rMessage.getLength() )
+            {
+                rtl::OUString aEmpty;
+                mxLogger->logp( 0, aEmpty, aEmpty, rMessage );
+            }
+            mxHandler->endElement( rElement );
+        }
     }
 }
 
-uno::Any MSFilterTracer::GetProperty( const rtl::OUString& rPropName ) const
+void MSFilterTracer::AddAttribute( const ::rtl::OUString& sName , const ::rtl::OUString& sValue )
 {
-    uno::Any aAny;
-    if ( mpCfgItem )
-        aAny = mpCfgItem->ReadAny( rPropName, aAny );
-    return aAny;
+    if ( mbEnabled )
+        mpAttributeList->AddAttribute( sName, sValue );
 }
+void MSFilterTracer::ClearAttributes()
+{
+    if ( mbEnabled )
+        mpAttributeList->Clear();
+}
+
+void MSFilterTracer::RemoveAttribute( const ::rtl::OUString& sName )
+{
+    if ( mbEnabled )
+        mpAttributeList->RemoveAttribute( sName );
+}
+
+uno::Any MSFilterTracer::GetProperty( const rtl::OUString& rPropName, const uno::Any* pDefault ) const
+{
+    uno::Any aDefault;
+    if ( pDefault )
+        aDefault = *pDefault;
+    return mpCfgItem->ReadAny( rPropName, aDefault );
+}
+
+void MSFilterTracer::SetProperty( const ::rtl::OUString& rPropName, const uno::Any& rProperty )
+{
+    mpCfgItem->WriteAny( rPropName, rProperty );
+}
+
