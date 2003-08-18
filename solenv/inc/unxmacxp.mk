4c9c1957cfc7c9de3def35dab4dcd07e76992ddd
Hans-Joachim Lankenau <hjs@openoffice.org>
2003-08-18 13:49:54 +0000

    INTEGRATION: CWS geordi2q01 (1.43.14); FILE MERGED 2003/08/18 13:25:23 hr 1.43.14.1: #i18350#: join changes from CWS ooo11rc3

--- solenv/inc/unxmacxp.mk
+++ solenv/inc/unxmacxp.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxmacxp.mk,v $
 #
-#   $Revision: 1.43 $
+#   $Revision: 1.44 $
 #
-#   last change: $Author: hr $ $Date: 2003-07-16 18:21:12 $
+#   last change: $Author: hjs $ $Date: 2003-08-18 14:49:54 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -75,11 +75,11 @@ LINKOUTPUT_FILTER=
 # Definitions that we may need on the compile line.
 # -D_PTHREADS and -D_REENTRANT are needed for STLport, and must be specified when
 #  compiling STLport sources too, either internally or externally.
-CDEFS+=-DGLIBC=2 -D_PTHREADS -D_REENTRANT -DNO_PTHREAD_PRIORITY -DSTLPORT_VERSION=400 -D_USE_NAMESPACE=1
+CDEFS+=-DGLIBC=2 -D_PTHREADS -D_REENTRANT -DNO_PTHREAD_PRIORITY -DPOWERPC -DPPC -DSTLPORT_VERSION=400 -D_USE_NAMESPACE=1
 
 # Name of library where static data members are initialized
-STATICLIBNAME=static$(DLLPOSTFIX)
-STATICLIB=-l$(STATICLIBNAME)
+# STATICLIBNAME=static$(DLLPOSTFIX)
+# STATICLIB=-l$(STATICLIBNAME)
 
 # MacOS X specific Java compilation/link flags
 .IF "$(SOLAR_JAVA)"!=""
@@ -94,134 +94,69 @@ STATICLIB=-l$(STATICLIBNAME)
 # CC = C++ compiler to use
 # cc = C compiler to use
 # objc = Objective C compiler to use
-#CC=c++
-#cc=cc
+CXX*=g++
+CC*=gcc
 objc=cc
 
-# [ed] 6/15/02 Some .. bleep .. decided to make a directory within a module that has the same
-# name as one of the STL headers.  This sucks since, with Apple's compilers, if a directory
-# with a name appears in a directory specified with a -I directive, the compiler will flag
-# an error and not search for a file with that name in a following -I directive on the
-# command line.  Therefore...this ugly hack...put STL as the first include on the linkline.
-
-# Remeber:  STLPORT4=="NO_STLPORT4" generally indicates that we are using the _INTERNAL_ stlport
-# in OOo and not an external one.
-
-.IF "$(STLPORT4)"!=""
-    .IF "$(STLPORT4)"=="NO_STLPORT4"
-        # [ed] We need to add in the STLport include directory included with the standard OOo sources,
-        # currently STLPORT 4.5.
-        CFLAGS=-c -I$(SOLARVERSION)$/$(GVERDIR)$/inc/stl $(INCLUDE)
-    .ELSE
-        # [ed] Add in external STLport includes directory if not using internal stlport
-        CFLAGS=-c -I$(STLPORT4)$/stlport $(INCLUDE)
-    .ENDIF
-.ELSE
-    # Otherwise, some indeterminiate STLport...
-    CFLAGS=-c -I$(SOLARVERSION)$/$(GVERDIR)$/inc/stl $(INCLUDE)
-.ENDIF
+CFLAGS=-fsigned-char -fmessage-length=0 -malign-natural -c $(INCLUDE)
 
 # ---------------------------------
 #  Compilation flags
 # ---------------------------------
 # Normal C compilation flags
-CFLAGSCC=-pipe -fno-common -malign-natural
+CFLAGSCC=-pipe -fsigned-char -malign-natural
 
 # Normal Objective C compilation flags
 OBJCFLAGS=-no-precomp
 
 # Comp Flags for files that need exceptions enabled (C and C++)
-CFLAGSEXCEPTIONS=-fexceptions
+CFLAGSEXCEPTIONS=-fexceptions -fno-enforce-eh-specs
+
 # Comp Flags for files that do not need exceptions enabled (C and C++)
 CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
 # Normal C++ compilation flags
-CFLAGSCXX=-pipe -malign-natural -fno-for-scope -fpermissive -fno-operator-names -fno-common -Wno-long-double -fno-strict-aliasing
+CFLAGSCXX=-pipe -malign-natural -fsigned-char -fno-for-scope -fpermissive -Wno-long-double 
 
 # Other flags
-CFLAGSOBJGUIST=-fPIC
-CFLAGSOBJCUIST=-fPIC
-CFLAGSOBJGUIMT=-fPIC
-CFLAGSOBJCUIMT=-fPIC
-CFLAGSSLOGUIMT=-fPIC
-CFLAGSSLOCUIMT=-fPIC
+CFLAGSOBJGUIST=-fPIC -fno-common
+CFLAGSOBJCUIST=-fPIC -fno-common
+CFLAGSOBJGUIMT=-fPIC -fno-common
+CFLAGSOBJCUIMT=-fPIC -fno-common
+CFLAGSSLOGUIMT=-fPIC -fno-common
+CFLAGSSLOCUIMT=-fPIC -fno-common
 CFLAGSPROF=
+
 # Flag for including debugging information in object files
 CFLAGSDEBUG=-g
 CFLAGSDBGUTIL=
+
 # Flag to specify output file to compiler/linker
 CFLAGSOUTOBJ=-o
 
 # ---------------------------------
 #  Optimization flags
 # ---------------------------------
-# Workaround for the compiler's buggy opitmizer when exceptions are turned on.
-# Note that we have to define NO_OPTIMIZE for all files as once you turn off
-# optimizing for one file, we need to adjust our compiling of the remaining
-# optimized files.
-CDEFS+=-DNO_OPTIMIZE
-.IF "$(ENABLE_EXCEPTIONS)"==""
-    # Opt flags when exceptions are _dis_abled
-    CFLAGSOPT=-O2
-    CFLAGSNOOPT=-O
-.ELSE
-    # Opt flags when exceptions are enabled
-    CFLAGSOPT=
-    CFLAGSNOOPT=
-.ENDIF
+CFLAGSOPT=-O2 -fno-strict-aliasing
+CFLAGSNOOPT=-O0
 
 # Currently, there is no nas support for OS X...
 CDEFS+= -DNO_AUDIO
 
 # ---------------------------------
-#  Shared Library names
-# ---------------------------------
-# Add all libraries to the shared lib list that ARE NOT lib*static or cppuhelper (to prevent multiply defined errors)
-SOLARVERSHLLIBS=$(shell -/bin/sh -c "ls $(SOLARLIBDIR)$/*$(DLLPOST) $(SOLARLIBDIR)$/*$(DLLPOST).*[0-9] $(LB)$/*$(DLLPOST) $(LB)$/*$(DLLPOST).*[0-9] $(MISC)$/*$(DLLPOST) $(MISC)$/*$(DLLPOST).*[0-9] 2>/dev/null | grep -E -v 'lib\w+static' | grep -v cppuhelper")
-
-# ---------------------------------
 #  STLport library names
 # ---------------------------------
-# Option for adding STLport _dynamic_ library to the link line
-LIBSTLPORT=-lstlport_gcc
-
-# Get the name of the static STLport library
-.IF "$(STLPORT4)"=="" || "$(STLPORT4)"=="NO_STLPORT4"
-    LIBSTLPORTST=$(SOLARVERSION)$/$(INPATH)$/lib/libstlport_gcc.a
-.ELSE
-    LIBSTLPORTST=$(STLPORT4)$/lib/libstlport_gcc.a
-.ENDIF
-
-# Get the name of the dynamic STLport library and add it to the shared lib list
-.IF "$(STLPORT4)"!=""
-    .IF "$(STLPORT4)"=="NO_STLPORT4"
-        # Look for STLport 4.5 libraries in SOLARLIBDIR
-        SOLARVERSHLLIBS+=$(shell -/bin/sh -c "ls $(SOLARLIBDIR)$/$(DLLPRE)$(LIBSTLPORT:s/-l//)$(DLLPOST) 2>/dev/null")
-    .ELSE
-        # Look for STLport 4.0 libraries in the STLport4 home directory
-        SOLARVERSHLLIBS+=$(shell -/bin/sh -c "ls $(STLPORT4)$/lib$/*$(DLLPOST) $(STLPORT4)$/lib$/*$(DLLPOST).*[0-9] 2>/dev/null")
-    .ENDIF
-.ENDIF
-
-# ---------------------------------
-#  Module specific shared lib requirements
-# ---------------------------------
-.IF "$(PRJNAME)"!="cppuhelper"
-    # For all projects _except_ cppuhelper, add cppuhelper's library to the shared lib list.
-    # Note this only adds cppuhelper and no other libraries.
-    # Attempting to link cppuhelper to itself results in multiple defines errors
-    SOLARVERSHLLIBS+=$(shell -/bin/sh -c "ls $(SOLARLIBDIR)$/*$(DLLPOST) $(SOLARLIBDIR)$/*$(DLLPOST).*[0-9] $(LB)$/*$(DLLPOST) $(LB)$/*$(DLLPOST).*[0-9] $(MISC)$/*$(DLLPOST) $(MISC)$/*$(DLLPOST).*[0-9] 2>/dev/null | grep -E -v 'lib\w+static' | grep cppuhelper")
-.ENDIF
+LIBSTLPORT=-lstlport_gcc -lstdc++
+LIBSTLPORTST=$(SOLARVERSION)$/$(INPATH)$/lib/libstlport_gcc.a -lstdc++
 
 # ---------------------------------
 #  Link stage flags
 # ---------------------------------
-# For MacOS X, always use C++ compiler for linking because it does some library
-# order magic behind the scenes
-LINK=c++
-LINKFLAGS=-dynamic -framework System -Wl,-multiply_defined,suppress -lcc_dynamic -lstdc++ -L$(MISC)
-#LINKFLAGS=-dynamic -framework System -framework CoreFoundation -lcc_dynamic -lstdc++ \
-#	$(foreach,i,$(SOLARVERSHLLIBS) -dylib_file @executable_path$/$(i:f):$i) -L$(MISC)
+# always link with gcc since you may be linking c code and don't want -lstdc++ linked in!
+LINK=gcc
+LINKFLAGS=-Wl,-multiply_defined,suppress
+
+# LINKFLAGS=-dynamic -framework System -Wl,-multiply_defined,suppress -lcc_dynamic -lstdc++ -L$(MISC)
 
 # [ed] 5/14/02 If we're building for aqua, add in the objc runtime library into our link line
 .IF "$(GUIBASE)" == "aqua"
@@ -232,23 +167,22 @@ LINKFLAGS=-dynamic -framework System -Wl,-multiply_defined,suppress -lcc_dynamic
 .ENDIF
 
 # Random link flags dealing with different cases of linking
-# UNIXVERSIONNAMES specifies whether or not to use library versioning
-#   (ie libstlport.dylib.4.5) rather than no versioning.  We currently
-#   use this on MacOS X.
-LINKFLAGSAPPGUI=
+
+LINKFLAGSAPPGUI=-bind_at_load
 .IF "$(UNIXVERSIONNAMES)"!=""
-    LINKFLAGSSHLGUI=-dynamiclib -install_name '@executable_path$/$(@:f:b:b)'
+    LINKFLAGSSHLGUI=-dynamiclib -single_module -install_name '@executable_path$/$(@:f:b:b)'
 .ELSE
-    LINKFLAGSSHLGUI=-dynamiclib -install_name '@executable_path$/$(@:f)'
+    LINKFLAGSSHLGUI=-dynamiclib -single_module -install_name '@executable_path$/$(@:f)'
 .ENDIF
-LINKFLAGSAPPCUI=-Wl,-u,__objcInit
+LINKFLAGSAPPCUI=-bind_at_load -Wl,-u,__objcInit
 .IF "$(UNIXVERSIONNAMES)"!=""
-    LINKFLAGSSHLCUI=-dynamiclib -install_name '@executable_path$/$(@:f:b:b)'
+    LINKFLAGSSHLCUI=-dynamiclib -single_module -install_name '@executable_path$/$(@:f:b:b)'
 .ELSE
-    LINKFLAGSSHLCUI=-dynamiclib -install_name '@executable_path$/$(@:f)'
+    LINKFLAGSSHLCUI=-dynamiclib -single_module -install_name '@executable_path$/$(@:f)'
 .ENDIF
 LINKFLAGSTACK=
 LINKFLAGSPROF=
+
 # Flag to add debugging information to final products
 LINKFLAGSDEBUG=-g
 LINKFLAGSOPT=
@@ -264,24 +198,26 @@ DLLPRE=lib
 DLLPOST=.dylib
 
 # We don't use mapping on MacOS X
-LINKVERSIONMAPFLAG=
+LINKVERSIONMAPFLAG=-Wl,--version-script
 
-# Reihenfolge der libs NICHT egal!
+SONAME_SWITCH=-Wl,-h
+
+STDLIBCPP=-lstdc++
 
 STDOBJGUI=
 STDSLOGUI=
 STDOBJCUI=
 STDSLOCUI=
 
-STDLIBGUIST=
-STDLIBCUIST=
-STDLIBGUIMT=
-STDLIBCUIMT=
-STDSHLGUIMT=
-STDSHLCUIMT=
+STDLIBGUIST=-lm
+STDLIBCUIST=-lX11 -lpthread -lm
+STDLIBGUIMT=-lpthread -lm
+STDLIBCUIMT=-lX11 -lm
+STDSHLGUIMT=-lX11 -lXext -lpthread -m
+STDSHLCUIMT=-lpthread -lm
 
-LIBMGR=libtool
-LIBFLAGS=-o
+LIBMGR=ar
+LIBFLAGS=-r
 
 IMPLIB=
 IMPLIBFLAGS=
