a3b746cab7dee21db2d14d3cf3a630eb93d0b23a
Noel Power <npower@openoffice.org>
2003-08-19 08:46:43 +0000

    #17307# Adding duncancs changes for Runtme->Providers

---
+++ scripting/source/provider/MasterScriptProvider.hxx
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  $RCSfile: MasterScriptProvider.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: npower $ $Date: 2003-08-19 09:46:43 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _FRAMEWORK_SCRIPT_PROVIDER_XFUNCTIONPROVIDER_HXX_
+#define _FRAMEWORK_SCRIPT_PROVIDER_XFUNCTIONPROVIDER_HXX_
+
+#include <rtl/ustring>
+#include <cppuhelper/implbase3.hxx>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/uno/RuntimeException.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
+
+#include <drafts/com/sun/star/script/framework/provider/XScriptProvider.hpp>
+#include <drafts/com/sun/star/script/framework/runtime/XScriptInvocation.hpp>
+#include <drafts/com/sun/star/script/framework/storage/XScriptStorageManager.hpp>
+#include "ScriptingContext.hxx"
+
+namespace func_provider
+{
+// for simplification
+#define css ::com::sun::star
+#define dcsssf ::drafts::com::sun::star::script::framework
+
+class MasterScriptProvider :
+            public ::cppu::WeakImplHelper3 < dcsssf::provider::XScriptProvider,
+                css::lang::XServiceInfo, css::lang::XInitialization >
+{
+public:
+    MasterScriptProvider(
+        const css::uno::Reference< css::uno::XComponentContext >
+        & xContext );
+    ~MasterScriptProvider();
+
+    // XServiceInfo implementation
+    virtual ::rtl::OUString SAL_CALL getImplementationName( )
+        throw( css::uno::RuntimeException );
+    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName )
+        throw( css::uno::RuntimeException );
+    virtual css::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames( )
+        throw( css::uno::RuntimeException );
+
+    // XScriptInvocation implementation
+    virtual css::uno::Reference < dcsssf::provider::XScript > SAL_CALL
+        getScript( const ::rtl::OUString& scriptURI )
+        throw( css::uno::RuntimeException );
+
+    /**
+     *  XInitialise implementation
+     *
+     * @param args expected to contain a single ::rtl::OUString
+     * containing the URI
+     */
+    virtual void SAL_CALL initialize( css::uno::Sequence < css::uno::Any > const & args )
+        throw ( css::uno::Exception, css::uno::RuntimeException);
+private:
+    void addStorageAsListener();
+    /* to obtain other services if needed */
+    css::uno::Reference< css::uno::XComponentContext > m_xContext;
+    css::uno::Reference< css::lang::XMultiComponentFactory > m_xMgr;
+    css::uno::Reference< css::frame::XModel > m_xModel;
+    css::uno::Reference < dcsssf::storage::XScriptStorageManager > m_xScriptStorageMgr;
+    bool m_bInitialised;
+    css::uno::Reference< css::beans::XPropertySet > m_XScriptingContext;
+
+    osl::Mutex m_mutex;
+};
+} // namespace func_provider
+#endif //_FRAMEWORK_SCRIPT_PROVIDER_XFUNCTIONPROVIDER_HXX_
