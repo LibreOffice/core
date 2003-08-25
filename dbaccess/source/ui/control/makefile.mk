150b7d0cfe8ff67d6e11153b0e4d5ab4df2442b7
Kurt Zenker <kz@openoffice.org>
2003-08-25 14:02:55 +0000

    INTEGRATION: CWS ause04 (1.11.108); FILE MERGED 2003/07/10 14:21:05 hjs 1.11.108.1: #108794# replace SRCFILES with SRC1FILES

--- dbaccess/source/ui/control/makefile.mk
+++ dbaccess/source/ui/control/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: oj $ $Date: 2002-03-26 09:20:37 $
+#   last change: $Author: kz $ $Date: 2003-08-25 15:02:55 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,7 +73,8 @@ TARGET=uicontrols
 
 # ... resource files ............................
 
-SRCFILES =	\
+SRS1NAME=$(TARGET)
+SRC1FILES =	\
         TableGrantCtrl.src	\
         undosqledit.src		\
         tabletree.src		\
