9422f6441fabd2c2179aa4a739e6a78218987dd7
Kurt Zenker <kz@openoffice.org>
2003-08-25 13:57:46 +0000

    INTEGRATION: CWS ause04 (1.13.112); FILE MERGED 2003/07/11 10:09:22 hjs 1.13.112.1: #108794# replace SRCFILES with SRC1FILES

--- sw/source/ui/uno/makefile.mk
+++ sw/source/ui/uno/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-17 15:53:16 $
+#   last change: $Author: kz $ $Date: 2003-08-25 14:57:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,8 +64,6 @@ PRJ=..$/..$/..
 
 PRJNAME=sw
 TARGET=unoidl
-
-AUTOSEG=true
 
 # --- Settings -----------------------------------------------------
 
