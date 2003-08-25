c38f100174f2fb188e9dda00461a778519a3e1cb
Kurt Zenker <kz@openoffice.org>
2003-08-25 14:02:45 +0000

    INTEGRATION: CWS ause04 (1.12.102); FILE MERGED 2003/07/10 14:21:03 hjs 1.12.102.1: #108794# replace SRCFILES with SRC1FILES

--- dbaccess/source/ui/browser/makefile.mk
+++ dbaccess/source/ui/browser/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: fs $ $Date: 2002-11-08 13:11:37 $
+#   last change: $Author: kz $ $Date: 2003-08-25 15:02:45 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -95,7 +95,8 @@ SLOFILES =\
             $(EXCEPTIONSFILES)\
             $(SLO)$/AsyncronousLink.obj
 
-SRCFILES =  sbabrw.src	\
+SRS1NAME=$(TARGET)
+SRC1FILES =  sbabrw.src	\
             sbagrid.src
 
 # --- Targets -------------------------------------------------------
