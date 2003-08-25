aa559019f5298f65a8508356252e678944b1f2ba
Kurt Zenker <kz@openoffice.org>
2003-08-25 14:21:34 +0000

    INTEGRATION: CWS ause04 (1.2.180); FILE MERGED 2003/07/09 18:42:47 hjs 1.2.180.1: #108794# replace SRCFILES with SRC1FILES

--- sc/source/ui/styleui/makefile.mk
+++ sc/source/ui/styleui/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: nn $ $Date: 2002-05-31 18:04:06 $
+#   last change: $Author: kz $ $Date: 2003-08-25 15:21:34 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -90,7 +90,8 @@ CXXFILES =  \
 SLOFILES =  \
     $(SLO)$/styledlg.obj \
 
-SRCFILES =  \
+SRS1NAME=$(TARGET)
+SRC1FILES =  \
     scstyles.src \
     styledlg.src
 
