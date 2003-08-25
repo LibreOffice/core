dfa356be5be586b4ae081948757d3918ed3bfe3d
Kurt Zenker <kz@openoffice.org>
2003-08-25 13:57:13 +0000

    INTEGRATION: CWS ause04 (1.2.112); FILE MERGED 2003/07/11 10:09:19 hjs 1.2.112.1: #108794# replace SRCFILES with SRC1FILES

--- sw/source/ui/ribbar/makefile.mk
+++ sw/source/ui/ribbar/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-17 15:39:39 $
+#   last change: $Author: kz $ $Date: 2003-08-25 14:57:13 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,8 +65,6 @@ PRJ=..$/..$/..
 PRJNAME=sw
 TARGET=ribbar
 
-AUTOSEG=true
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  $(PRJ)$/inc$/swpre.mk
@@ -78,7 +76,8 @@ BMP_IN=$(PRJ)$/win/imglst
 
 # --- Files --------------------------------------------------------
 
-SRCFILES =  \
+SRS1NAME=$(TARGET)
+SRC1FILES =  \
         tbxanchr.src \
         inputwin.src \
         tblctrl.src \
