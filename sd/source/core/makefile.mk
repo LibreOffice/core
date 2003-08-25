f2fd88f0a5ddd170111901a4b3c70d6f09bc4394
Kurt Zenker <kz@openoffice.org>
2003-08-25 14:14:07 +0000

    INTEGRATION: CWS ause04 (1.3.196); FILE MERGED 2003/07/11 10:02:06 hjs 1.3.196.1: #108794# replace SRCFILES with SRC1FILES

--- sd/source/core/makefile.mk
+++ sd/source/core/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hjs $ $Date: 2001-11-21 19:42:17 $
+#   last change: $Author: kz $ $Date: 2003-08-25 15:14:07 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -67,20 +67,17 @@ PROJECTPCHSOURCE=$(PRJ)$/util$/sd
 PRJNAME=sd
 TARGET=core
 
-AUTOSEG=true
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 IENV+=-I..\ui\inc
 
 # --- Files --------------------------------------------------------
 
-
-SRCFILES = glob.src
+SRS1NAME=$(TARGET)
+SRC1FILES = glob.src
 
 SLOFILES = $(SLO)$/stlsheet.obj  \
            $(SLO)$/stlpool.obj	\
