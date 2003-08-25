aa7c07dcd59046b5ed6567b9072af42ec4979c3e
Kurt Zenker <kz@openoffice.org>
2003-08-25 13:55:26 +0000

    INTEGRATION: CWS ause04 (1.5.112); FILE MERGED 2003/07/11 10:09:12 hjs 1.5.112.1: #108794# replace SRCFILES with SRC1FILES

--- sw/source/ui/dbui/makefile.mk
+++ sw/source/ui/dbui/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-17 15:20:31 $
+#   last change: $Author: kz $ $Date: 2003-08-25 14:55:26 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,8 +65,6 @@ PRJ=..$/..$/..
 PRJNAME=sw
 TARGET=dbui
 
-AUTOSEG=true
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  $(PRJ)$/inc$/swpre.mk
@@ -75,13 +73,8 @@ AUTOSEG=true
 
 # --- Files --------------------------------------------------------
 
-CXXFILES = \
-        dbinsdlg.cxx \
-        dbmgr.cxx \
-        dbtree.cxx \
-        dbui.cxx
-
-SRCFILES =  \
+SRS1NAME=$(TARGET)
+SRC1FILES =  \
         dbinsdlg.src	\
         dbui.src
 
