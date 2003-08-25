c1e22531a41ea73812ded2ea51501dd885c89a89
Kurt Zenker <kz@openoffice.org>
2003-08-25 13:55:17 +0000

    INTEGRATION: CWS ause04 (1.7.112); FILE MERGED 2003/07/11 10:09:11 hjs 1.7.112.1: #108794# replace SRCFILES with SRC1FILES

--- sw/source/ui/config/makefile.mk
+++ sw/source/ui/config/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-17 15:17:53 $
+#   last change: $Author: kz $ $Date: 2003-08-25 14:55:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,8 +65,6 @@ PRJ=..$/..$/..
 PRJNAME=sw
 TARGET=config
 
-AUTOSEG=true
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  $(PRJ)$/inc$/swpre.mk
@@ -75,22 +73,8 @@ AUTOSEG=true
 
 # --- Files --------------------------------------------------------
 
-CXXFILES = \
-        barcfg.cxx \
-        caption.cxx \
-        cfgitems.cxx \
-        dbconfig.cxx \
-        fontcfg.cxx \
-        modcfg.cxx \
-        optload.cxx \
-        optpage.cxx \
-        prtopt.cxx \
-        uinums.cxx \
-        usrpref.cxx \
-        viewopt.cxx \
-        swlinguconfig.cxx
-
-SRCFILES =	\
+SRS1NAME=$(TARGET)
+SRC1FILES =	\
         optdlg.src \
         optload.src \
         redlopt.src
