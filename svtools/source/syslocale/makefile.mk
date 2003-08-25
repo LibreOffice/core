bd838690250a3205e6f192667230cc8b95a2b247
Kurt Zenker <kz@openoffice.org>
2003-08-25 14:44:50 +0000

    INTEGRATION: CWS ause04 (1.1.282); FILE MERGED 2003/07/10 16:19:44 hjs 1.1.282.1: #108794# replace SRCFILES with SRC1FILES

--- svtools/source/syslocale/makefile.mk
+++ svtools/source/syslocale/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: er $ $Date: 2001-07-02 09:37:54 $
+#   last change: $Author: kz $ $Date: 2003-08-25 15:44:50 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -61,14 +61,12 @@
 #*************************************************************************
 PRJ=..$/..
 
-PRJNAME=SVTOOLS
+PRJNAME=svtools
 TARGET=syslocale
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- Files --------------------------------------------------------
 
