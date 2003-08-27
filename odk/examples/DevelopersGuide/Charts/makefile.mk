f335d51e6504283091683ea064948faebde4cfd5
Kurt Zenker <kz@openoffice.org>
2003-08-27 15:43:22 +0000

    INTEGRATION: CWS geordi2q03 (1.2.32); FILE MERGED 2003/08/27 15:05:44 hr 1.2.32.1: #111934#: join from CWS sdk07

--- odk/examples/DevelopersGuide/Charts/makefile.mk
+++ odk/examples/DevelopersGuide/Charts/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: vg $ $Date: 2003-06-10 10:10:05 $
+#   last change: $Author: kz $ $Date: 2003-08-27 16:43:22 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,18 +88,23 @@ CHARTS_FILES=\
     $(DESTDIRDEVGUIDEEXAMPLES)$/Charts$/bullet.gif \
     $(DESTDIRDEVGUIDEEXAMPLES)$/Charts$/Makefile
 
-EXAMPLESLIST= \
+DIR_FILE_LIST= \
     $(CHARTS_FILES) \
 
+DIR_DIRECTORY_LIST=$(uniq $(DIR_FILE_LIST:d))
+DIR_CREATE_FLAG=$(MISC)$/devguide_charts_dirs_created.txt
+DIR_FILE_FLAG=$(MISC)$/devguide_charts.txt
 
 #--------------------------------------------------
 # TARGETS
 #--------------------------------------------------
 all : \
-    $(EXAMPLESLIST)
+    $(DIR_FILE_LIST) \
+    $(DIR_FILE_FLAG)
 
 #--------------------------------------------------
 # use global rules
 #--------------------------------------------------   
 .INCLUDE: $(PRJ)$/util$/odk_rules.pmk
     
+
