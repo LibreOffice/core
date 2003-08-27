e3e5c9b74c4e6b89a21568e6816d185557b18ff1
Kurt Zenker <kz@openoffice.org>
2003-08-27 15:46:29 +0000

    INTEGRATION: CWS geordi2q03 (1.2.32); FILE MERGED 2003/08/27 15:06:10 hr 1.2.32.1: #111934#: join from CWS sdk07

--- odk/examples/basic/makefile.mk
+++ odk/examples/basic/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: vg $ $Date: 2003-06-10 10:45:11 $
+#   last change: $Author: kz $ $Date: 2003-08-27 16:46:29 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -100,22 +100,27 @@ TEXT_FILES=\
     $(DESTDIRBASICEXAMPLES)$/text$/modifying_text_automatically$/replacing_text.sxw \
     $(DESTDIRBASICEXAMPLES)$/text$/modifying_text_automatically$/using_regular_expressions.sxw
 
-EXAMPLESLIST= \
+DIR_FILE_LIST= \
     $(DRAWING_FILES) \
     $(FORMSANDCONTROLS_FILES) \
     $(SHEET_FILES) \
     $(STOCKQUOTEUPDATER_FILES) \
     $(TEXT_FILES) \
 
+DIR_DIRECTORY_LIST=$(uniq $(DIR_FILE_LIST:d))
+DIR_CREATE_FLAG=$(MISC)$/ex_basic_dirs_created.txt
+DIR_FILE_FLAG=$(MISC)$/ex_basic_files.txt
 
 #--------------------------------------------------
 # TARGETS
 #--------------------------------------------------
 all : \
-    $(EXAMPLESLIST)
+    $(DIR_FILE_LIST) \
+    $(DIR_FILE_FLAG)
 
 #--------------------------------------------------
 # use global rules
 #--------------------------------------------------   
 .INCLUDE: $(PRJ)$/util$/odk_rules.pmk
     
+
