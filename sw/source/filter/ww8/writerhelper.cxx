e8cb294563ed4b1b0bd0340765add8b4d48a3b6f
Oliver Bolte <obo@openoffice.org>
2003-09-01 11:39:39 +0000

    INTEGRATION: CWS limerickfilterteam08 (1.1.2); FILE ADDED 2003/08/14 15:30:22 cmc 1.1.2.5: #i17056# word doesn't have title case character property, work around as hard uppercased text 2003/07/16 16:49:18 cmc 1.1.2.4: #i6991# better mechanism to ensure objects are unloaded while being transferred from SdrOle2Obj into SwDoc's Persist 2003/07/15 09:48:50 cmc 1.1.2.3: #108795# If more than one outline style for a level, then when building TOC add extra ones in as extra styles 2003/07/01 12:14:33 cmc 1.1.2.2: Clean GetAttr casting to make it readable 2003/07/01 08:38:29 cmc 1.1.2.1: rename to avoid name collision

---
+++ sw/source/filter/ww8/writerhelper.cxx
@@ -0,0 +1,231 @@
+/*************************************************************************
+ *
+ *  $RCSfile: writerhelper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2003-09-01 12:39:39 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
+
+#ifndef SW_WRITERHELPER
+#include "writerhelper.hxx"
+#endif
+
+#include <algorithm>        //std::swap
+#include <functional>       //std::binary_function
+
+#ifndef _DOC_HXX
+#include <doc.hxx>          //SwDoc
+#endif
+#ifndef _SVDOBJ_HXX
+#include <svx/svdobj.hxx>   //SdrObject
+#endif
+#ifndef _SVDOOLE2_HXX
+#include <svx/svdoole2.hxx> //SdrOle2Obj
+#endif
+#ifndef _SVX_FMGLOB_HXX
+#include <svx/fmglob.hxx>   //FmFormInventor
+#endif
+
+namespace
+{
+    //Utility to sort SwTxtFmtColl's by their outline numbering level
+    class outlinecmp : public
+        std::binary_function<const SwTxtFmtColl*, const SwTxtFmtColl*, bool>
+    {
+    public:
+        bool operator()(const SwTxtFmtColl *pA, const SwTxtFmtColl *pB) const
+        {
+            return pB->GetOutlineLevel() < pB->GetOutlineLevel();
+        }
+    };
+}
+
+namespace sw
+{
+    namespace hack
+    {
+        void SetLayer::SendObjectToHell(SdrObject &rObject) const
+        {
+            SetObjectLayer(rObject, eHell);
+        }
+
+        void SetLayer::SendObjectToHeaven(SdrObject &rObject) const
+        {
+            SetObjectLayer(rObject, eHeaven);
+        }
+
+        void SetLayer::SetObjectLayer(SdrObject &rObject, Layer eLayer) const
+        {
+            if (FmFormInventor == rObject.GetObjInventor())
+                rObject.SetLayer(mnFormLayer);
+            else
+            {
+                switch (eLayer)
+                {
+                    case eHeaven:
+                        rObject.SetLayer(mnHeavenLayer);
+                        break;
+                    case eHell:
+                        rObject.SetLayer(mnHellLayer);
+                        break;
+                }
+            }
+        }
+
+        //SetLayer boilerplate begin
+        void SetLayer::Swap(SetLayer& rOther) throw()
+        {
+            std::swap(mnHeavenLayer, rOther.mnHeavenLayer);
+            std::swap(mnHellLayer, rOther.mnHellLayer);
+            std::swap(mnFormLayer, rOther.mnFormLayer);
+        }
+
+        SetLayer::SetLayer(const SwDoc &rDoc)
+            : mnHeavenLayer(rDoc.GetHeavenId()),
+            mnHellLayer(rDoc.GetHellId()),
+            mnFormLayer(rDoc.GetControlsId())
+        {
+        }
+
+        SetLayer::SetLayer(const SetLayer& rOther) throw()
+            : mnHeavenLayer(rOther.mnHeavenLayer),
+            mnHellLayer(rOther.mnHellLayer),
+            mnFormLayer(rOther.mnFormLayer)
+        {
+        }
+
+        SetLayer& SetLayer::operator=(const SetLayer& rOther) throw()
+        {
+            SetLayer aTemp(rOther);
+            Swap(aTemp);
+            return *this;
+        }
+        //SetLayer boilerplate end
+
+
+        DrawingOLEAdaptor::DrawingOLEAdaptor(SdrOle2Obj &rObj,
+            SvPersist &rPers)
+            : mxIPRef(rObj.GetObjRef()),
+            msOrigPersistName(rObj.GetPersistName()), mrPers(rPers)
+        {
+            rObj.SetPersistName(String());
+            rObj.SetObjRef(SvInPlaceObjectRef());
+        }
+
+        bool DrawingOLEAdaptor::TransferToDoc(const String &rName)
+        {
+            ASSERT(mxIPRef.Is(), "Transferring invalid object to doc");
+            if (!mxIPRef.Is())
+                return false;
+
+            SvInfoObjectRef refObj = new SvEmbeddedInfoObject(mxIPRef, rName);
+            bool bSuccess = mrPers.Move(refObj, rName);
+            if (bSuccess)
+            {
+                SvPersist *pO = mxIPRef;
+                ASSERT(!pO->IsModified(), "Not expected to be modified here");
+                if (pO->IsModified())
+                {
+                    pO->DoSave();
+                    pO->DoSaveCompleted();
+                }
+                mxIPRef.Clear();
+                bSuccess = mrPers.Unload(pO);
+            }
+
+            return bSuccess;
+        }
+
+        DrawingOLEAdaptor::~DrawingOLEAdaptor()
+        {
+            if (mxIPRef.Is())
+            {
+                if (SvInfoObject* pInfo = mrPers.Find(msOrigPersistName))
+                {
+                    pInfo->SetDeleted(TRUE);
+                    pInfo->SetObj(0);
+                }
+                mxIPRef->DoClose();
+                mrPers.Remove(mxIPRef);
+                mxIPRef.Clear();
+            }
+        }
+    }
+
+    namespace util
+    {
+        ParaStyles GetParaStyles(const SwDoc &rDoc)
+        {
+            ParaStyles aStyles;
+            typedef ParaStyles::size_type mysizet;
+
+            const SwTxtFmtColls *pColls = rDoc.GetTxtFmtColls();
+            mysizet nCount = pColls ? pColls->Count() : 0;
+            aStyles.reserve(nCount);
+            for (mysizet nI = 0; nI < nCount; ++nI)
+                aStyles.push_back((*pColls)[nI]);
+            return aStyles;
+        }
+
+        void SortByOutline(ParaStyles &rStyles)
+        {
+            std::sort(rStyles.begin(), rStyles.end(), outlinecmp());
+        }
+    }
+}
+/* vi:set tabstop=4 shiftwidth=4 expandtab: */
