87d6502d07aeb3b35719df133c26db706d926f9c
Oliver Bolte <obo@openoffice.org>
2003-09-04 08:16:29 +0000

    INTEGRATION: CWS cliuno01 (1.2.24); FILE MERGED 2003/07/04 06:44:55 jl 1.2.24.3: *** empty log message *** 2003/06/05 13:31:27 dbo 1.2.24.2: #107130# fixing tests 2003/04/11 17:15:20 dbo 1.2.24.1: #107130# CLI tests

--- testtools/source/bridgetest/cppobj.cxx
+++ testtools/source/bridgetest/cppobj.cxx
@@ -1,7 +1,7 @@
 /**************************************************************************
 #*
-#*    last change   $Author: dbo $ $Date: 2002-11-27 10:06:09 $
-#*    $Revision: 1.2 $
+#*    last change   $Author: obo $ $Date: 2003-09-04 09:16:29 $
+#*    $Revision: 1.3 $
 #*
 #*    $Logfile: $
 #*
@@ -24,7 +24,7 @@
 #include <com/sun/star/lang/XComponent.hpp>
 #include <com/sun/star/registry/XRegistryKey.hpp>
 
-#include <com/sun/star/test/bridge/XBridgeTest.hpp>
+#include <com/sun/star/test/bridge/XBridgeTest2.hpp>
 
 using namespace rtl;
 using namespace osl;
@@ -93,7 +93,7 @@ static void assign( TestData & rData,
 }
 
 //==================================================================================================
-class Test_Impl : public WeakImplHelper3< XBridgeTest, XServiceInfo , XRecursiveCall >
+class Test_Impl : public WeakImplHelper3< XBridgeTest2, XServiceInfo , XRecursiveCall >
 {
     TestData _aData, _aStructData;
     sal_Int32 m_nLastCallId;
@@ -101,13 +101,43 @@ class Test_Impl : public WeakImplHelper3< XBridgeTest, XServiceInfo , XRecursive
     sal_Bool m_bSequenceOfCallTestPassed;
     Mutex m_mutex;
 
+    Sequence<sal_Bool> _arBool;
+    Sequence<sal_Unicode> _arChar;
+    Sequence<sal_Int8> _arByte;
+    Sequence<sal_Int16> _arShort;
+    Sequence<sal_uInt16> _arUShort;
+    Sequence<sal_Int32> _arLong;
+    Sequence<sal_uInt32> _arULong;
+    Sequence<sal_Int64> _arHyper;
+    Sequence<sal_uInt64> _arUHyper;
+    Sequence<OUString> _arString;
+    Sequence<float> _arFloat;
+    Sequence<double> _arDouble;
+    Sequence<TestEnum> _arEnum;
+    Sequence<Reference<XInterface> > _arObject;
+    Sequence<Sequence<sal_Int32> > _arLong2;
+    Sequence<Sequence<Sequence<sal_Int32> > > _arLong3;
+    Sequence<Any> _arAny;
+    Sequence<TestElement> _arStruct;
+
 public:
     Test_Impl() : m_nLastCallId( 0 ),
                   m_bFirstCall( sal_True ),
                   m_bSequenceOfCallTestPassed( sal_True )
         {}
     virtual ~Test_Impl()
-        { OSL_TRACE( "> scalar Test_Impl dtor <\n" ); }
+        {
+            OSL_TRACE( "> scalar Test_Impl dtor <\n" );
+        }
+
+    void SAL_CALL acquire() throw ()
+    {
+        OWeakObject::acquire();
+    }
+    void SAL_CALL release() throw ()
+    {
+        OWeakObject::release();
+     }
 
     // XServiceInfo
     virtual OUString SAL_CALL getImplementationName() throw (RuntimeException);
@@ -245,6 +275,82 @@ public: // XBridgeTest
 
     virtual sal_Int32 SAL_CALL getRuntimeException() throw(::com::sun::star::uno::RuntimeException);
     virtual void SAL_CALL setRuntimeException( sal_Int32 _runtimeexception ) throw(::com::sun::star::uno::RuntimeException);
+
+    // XBridgeTest2
+    virtual Sequence< sal_Bool > SAL_CALL setSequenceBool(
+        const Sequence< sal_Bool >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_Unicode > SAL_CALL setSequenceChar(
+        const Sequence< sal_Unicode >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_Int8 > SAL_CALL setSequenceByte(
+        const Sequence< sal_Int8 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_Int16 > SAL_CALL setSequenceShort(
+        const Sequence< sal_Int16 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_uInt16 > SAL_CALL setSequenceUShort(
+        const Sequence< sal_uInt16 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_Int32 > SAL_CALL setSequenceLong(
+        const Sequence< sal_Int32 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_uInt32 > SAL_CALL setSequenceULong(
+        const Sequence< sal_uInt32 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_Int64 > SAL_CALL setSequenceHyper(
+        const Sequence< sal_Int64 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< sal_uInt64 > SAL_CALL setSequenceUHyper(
+        const Sequence< sal_uInt64 >& aSeq ) throw (RuntimeException);
+    virtual Sequence< float > SAL_CALL setSequenceFloat(
+        const Sequence< float >& aSeq ) throw (RuntimeException);
+    virtual Sequence< double > SAL_CALL setSequenceDouble(
+        const Sequence< double >& aSeq ) throw (RuntimeException);
+    virtual Sequence< TestEnum > SAL_CALL setSequenceEnum(
+        const Sequence< TestEnum >& aSeq ) throw (RuntimeException) ;
+    virtual Sequence< OUString > SAL_CALL setSequenceString(
+        const Sequence< OUString >& aString ) throw (RuntimeException);
+    virtual Sequence< Reference< XInterface > > SAL_CALL setSequenceXInterface(
+        const Sequence< Reference< XInterface > >& aSeq )
+        throw (RuntimeException);
+    virtual Sequence<Any > SAL_CALL setSequenceAny(
+        const Sequence<Any >& aSeq ) throw (RuntimeException);
+    virtual Sequence<TestElement > SAL_CALL setSequenceStruct(
+        const Sequence< TestElement >& aSeq ) throw (RuntimeException);
+    virtual Sequence< Sequence< sal_Int32 > > SAL_CALL setDim2(
+        const Sequence<Sequence< sal_Int32 > >& aSeq ) throw (RuntimeException);
+    virtual Sequence< Sequence< Sequence< sal_Int32 > > > SAL_CALL setDim3(
+        const Sequence< Sequence< Sequence< sal_Int32 > > >& aSeq )
+        throw (RuntimeException);
+    virtual void SAL_CALL setSequencesInOut(Sequence< sal_Bool >& aSeqBoolean,
+                                Sequence< sal_Unicode >& aSeqChar,
+                                Sequence< sal_Int8 >& aSeqByte,
+                                Sequence< sal_Int16 >& aSeqShort,
+                                Sequence< sal_uInt16 >& aSeqUShort,
+                                Sequence< sal_Int32 >& aSeqLong,
+                                Sequence< sal_uInt32 >& aSeqULong,
+                                Sequence< sal_Int64 >& aSeqHyper,
+                                Sequence< sal_uInt64 >& aSeqUHyper,
+                                Sequence< float >& aSeqFloat,
+                                Sequence< double >& aSeqDouble,
+                                Sequence< TestEnum >& aSeqTestEnum,
+                                Sequence< OUString >& aSeqString,
+                                Sequence<Reference<XInterface > >& aSeqXInterface,
+                                Sequence< Any >& aSeqAny,
+                                Sequence< Sequence< sal_Int32 > >& aSeqDim2,
+                                Sequence< Sequence< Sequence< sal_Int32 > > >& aSeqDim3 )
+        throw (RuntimeException);
+    virtual void SAL_CALL setSequencesOut( Sequence< sal_Bool >& aSeqBoolean,
+                             Sequence< sal_Unicode >& aSeqChar,
+                             Sequence< sal_Int8 >& aSeqByte,
+                             Sequence< sal_Int16 >& aSeqShort,
+                             Sequence< sal_uInt16 >& aSeqUShort,
+                             Sequence< sal_Int32 >& aSeqLong,
+                             Sequence< sal_uInt32 >& aSeqULong,
+                             Sequence< sal_Int64 >& aSeqHyper,
+                             Sequence< sal_uInt64 >& aSeqUHyper,
+                             Sequence< float >& aSeqFloat,
+                             Sequence< double >& aSeqDouble,
+                             Sequence< TestEnum >& aSeqEnum,
+                             Sequence< OUString >& aSeqString,
+                             Sequence< Reference< XInterface > >& aSeqXInterface,
+                             Sequence< Any >& aSeqAny,
+                             Sequence< Sequence< sal_Int32 > >& aSeqDim2,
+                             Sequence< Sequence< Sequence< sal_Int32 > > >& aSeqDim3 )
+        throw (RuntimeException);
 
 public:
     virtual void SAL_CALL callRecursivly( const ::com::sun::star::uno::Reference< ::com::sun::star::test::bridge::XRecursiveCall >& xCall, sal_Int32 nToCall ) throw(::com::sun::star::uno::RuntimeException);
@@ -427,6 +533,215 @@ void Test_Impl::setRuntimeException( sal_Int32 _runtimeexception ) throw(::com::
     throw aExc;
 }
 
+// XBridgeTest2 -------------------------------------------------------------
+Sequence< sal_Bool > SAL_CALL Test_Impl::setSequenceBool(
+        const Sequence< sal_Bool >& aSeq ) throw (RuntimeException)
+{
+    _arBool = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_Unicode > SAL_CALL Test_Impl::setSequenceChar(
+        const Sequence< sal_Unicode >& aSeq ) throw (RuntimeException)
+{
+    _arChar = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_Int8 > SAL_CALL Test_Impl::setSequenceByte(
+        const Sequence< sal_Int8 >& aSeq ) throw (RuntimeException)
+{
+    _arByte = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_Int16 > SAL_CALL Test_Impl::setSequenceShort(
+        const Sequence< sal_Int16 >& aSeq ) throw (RuntimeException)
+{
+    _arShort = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_uInt16 > SAL_CALL Test_Impl::setSequenceUShort(
+        const Sequence< sal_uInt16 >& aSeq ) throw (RuntimeException)
+{
+    _arUShort = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_Int32 > SAL_CALL Test_Impl::setSequenceLong(
+        const Sequence< sal_Int32 >& aSeq ) throw (RuntimeException)
+{
+    _arLong = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_uInt32 > SAL_CALL Test_Impl::setSequenceULong(
+        const Sequence< sal_uInt32 >& aSeq ) throw (RuntimeException)
+{
+    _arULong = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_Int64 > SAL_CALL Test_Impl::setSequenceHyper(
+        const Sequence< sal_Int64 >& aSeq ) throw (RuntimeException)
+{
+    _arHyper = aSeq;
+    return aSeq;
+}
+
+Sequence< sal_uInt64 > SAL_CALL Test_Impl::setSequenceUHyper(
+        const Sequence< sal_uInt64 >& aSeq ) throw (RuntimeException)
+{
+    _arUHyper = aSeq;
+    return aSeq;
+}
+
+Sequence< float > SAL_CALL Test_Impl::setSequenceFloat(
+        const Sequence< float >& aSeq ) throw (RuntimeException)
+{
+    _arFloat = aSeq;
+    return aSeq;
+}
+
+Sequence< double > SAL_CALL Test_Impl::setSequenceDouble(
+    const Sequence< double >& aSeq ) throw (RuntimeException)
+{
+    _arDouble = aSeq;
+    return aSeq;
+}
+
+Sequence< TestEnum > SAL_CALL Test_Impl::setSequenceEnum(
+    const Sequence< TestEnum >& aSeq ) throw (RuntimeException)
+{
+    _arEnum = aSeq;
+    return aSeq;
+}
+
+Sequence< OUString > SAL_CALL Test_Impl::setSequenceString(
+    const Sequence< OUString >& aSeq ) throw (RuntimeException)
+{
+    _arString = aSeq;
+    return aSeq;
+}
+
+Sequence< Reference< XInterface > > SAL_CALL Test_Impl::setSequenceXInterface(
+        const Sequence< Reference< XInterface > >& aSeq )
+        throw (RuntimeException)
+{
+    _arObject = aSeq;
+    return aSeq;
+}
+
+Sequence<Any > SAL_CALL Test_Impl::setSequenceAny(
+    const Sequence<Any >& aSeq ) throw (RuntimeException)
+{
+    _arAny = aSeq;
+    return aSeq;
+}
+
+Sequence<TestElement > SAL_CALL Test_Impl::setSequenceStruct(
+    const Sequence< TestElement >& aSeq ) throw (RuntimeException)
+{
+    _arStruct = aSeq;
+    return aSeq;
+}
+
+Sequence< Sequence< sal_Int32 > > SAL_CALL Test_Impl::setDim2(
+        const Sequence<Sequence< sal_Int32 > >& aSeq ) throw (RuntimeException)
+{
+    _arLong2 = aSeq;
+    return aSeq;
+}
+
+Sequence< Sequence< Sequence< sal_Int32 > > > SAL_CALL Test_Impl::setDim3(
+        const Sequence< Sequence< Sequence< sal_Int32 > > >& aSeq )
+        throw (RuntimeException)
+{
+    _arLong3 = aSeq;
+    return aSeq;
+}
+
+void SAL_CALL Test_Impl::setSequencesInOut(Sequence< sal_Bool >& aSeqBoolean,
+                                Sequence< sal_Unicode >& aSeqChar,
+                                Sequence< sal_Int8 >& aSeqByte,
+                                Sequence< sal_Int16 >& aSeqShort,
+                                Sequence< sal_uInt16 >& aSeqUShort,
+                                Sequence< sal_Int32 >& aSeqLong,
+                                Sequence< sal_uInt32 >& aSeqULong,
+                                Sequence< sal_Int64 >& aSeqHyper,
+                                Sequence< sal_uInt64 >& aSeqUHyper,
+                                Sequence< float >& aSeqFloat,
+                                Sequence< double >& aSeqDouble,
+                                Sequence< TestEnum >& aSeqTestEnum,
+                                Sequence< OUString >& aSeqString,
+                                Sequence<Reference<XInterface > >& aSeqXInterface,
+                                Sequence< Any >& aSeqAny,
+                                Sequence< Sequence< sal_Int32 > >& aSeqDim2,
+                                Sequence< Sequence< Sequence< sal_Int32 > > >& aSeqDim3 )
+        throw (RuntimeException)
+{
+    _arBool = aSeqBoolean;
+    _arChar = aSeqChar;
+    _arByte = aSeqByte;
+    _arShort = aSeqShort;
+    _arUShort = aSeqUShort;
+    _arLong = aSeqLong;
+    _arULong = aSeqULong;
+    _arHyper  = aSeqHyper;
+    _arUHyper = aSeqUHyper;
+    _arFloat = aSeqFloat;
+    _arDouble = aSeqDouble;
+    _arEnum = aSeqTestEnum;
+    _arString = aSeqString;
+    _arObject = aSeqXInterface;
+    _arAny = aSeqAny;
+    _arLong2 = aSeqDim2;
+    _arLong3 = aSeqDim3;
+}
+
+void SAL_CALL Test_Impl::setSequencesOut( Sequence< sal_Bool >& aSeqBoolean,
+                             Sequence< sal_Unicode >& aSeqChar,
+                             Sequence< sal_Int8 >& aSeqByte,
+                             Sequence< sal_Int16 >& aSeqShort,
+                             Sequence< sal_uInt16 >& aSeqUShort,
+                             Sequence< sal_Int32 >& aSeqLong,
+                             Sequence< sal_uInt32 >& aSeqULong,
+                             Sequence< sal_Int64 >& aSeqHyper,
+                             Sequence< sal_uInt64 >& aSeqUHyper,
+                             Sequence< float >& aSeqFloat,
+                             Sequence< double >& aSeqDouble,
+                             Sequence< TestEnum >& aSeqEnum,
+                             Sequence< OUString >& aSeqString,
+                             Sequence< Reference< XInterface > >& aSeqXInterface,
+                             Sequence< Any >& aSeqAny,
+                             Sequence< Sequence< sal_Int32 > >& aSeqDim2,
+                             Sequence< Sequence< Sequence< sal_Int32 > > >& aSeqDim3 )
+        throw (RuntimeException)
+{
+    aSeqBoolean = _arBool;
+    aSeqChar = _arChar;
+    aSeqByte = _arByte;
+    aSeqShort = _arShort;
+    aSeqUShort = _arUShort;
+    aSeqLong = _arLong;
+    aSeqULong = _arULong;
+    aSeqHyper = _arHyper;
+    aSeqUHyper = _arUHyper;
+    aSeqFloat = _arFloat;
+    aSeqDouble = _arDouble;
+    aSeqEnum = _arEnum;
+    aSeqString = _arString;
+    aSeqXInterface = _arObject;
+    aSeqAny = _arAny;
+    aSeqDim2 = _arLong2;
+    aSeqDim3 = _arLong3;
+}
+
+
+
+
+
 // XServiceInfo
 //__________________________________________________________________________________________________
 OUString Test_Impl::getImplementationName()
@@ -521,6 +836,18 @@ void * SAL_CALL component_getFactory(
 
 /**************************************************************************
     $Log: not supported by cvs2svn $
+    Revision 1.2.24.3  2003/07/04 06:44:55  jl
+    *** empty log message ***
+
+    Revision 1.2.24.2  2003/06/05 13:31:27  dbo
+    #107130# fixing tests
+
+    Revision 1.2.24.1  2003/04/11 17:15:20  dbo
+    #107130# CLI tests
+
+    Revision 1.2  2002/11/27 10:06:09  dbo
+    #104312# extended tests
+
     Revision 1.1  2001/05/04 07:05:17  kr
     moved from grande to openoffice
 
