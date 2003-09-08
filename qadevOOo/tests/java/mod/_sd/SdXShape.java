a959b897f9a97cb59c0354d19f9ab5ce0712c284
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:27:52 +0000

    INTEGRATION: CWS qadev11 (1.3.14); FILE MERGED 2003/09/03 15:34:01 sw 1.3.14.1: #112049#

--- qadevOOo/tests/java/mod/_sd/SdXShape.java
+++ qadevOOo/tests/java/mod/_sd/SdXShape.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdXShape.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-05-27 13:24:57 $
+ *  last change:$Date: 2003-09-08 12:27:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,14 @@
 
 package mod._sd;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.container.XIndexAccess;
 import com.sun.star.drawing.XDrawPage;
@@ -71,17 +79,10 @@ import com.sun.star.drawing.XShapes;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.style.XStyle;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
-
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
