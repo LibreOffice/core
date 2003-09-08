e24e5f32aa4564d9e605359117d8de6d9998c328
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:25:37 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 14:54:25 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/ifc/ucb/_XRemoteContentProviderAcceptor.java
+++ qadevOOo/tests/java/ifc/ucb/_XRemoteContentProviderAcceptor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XRemoteContentProviderAcceptor.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:30:09 $
+ *  last change:$Date: 2003-09-08 11:25:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,11 @@
 
 package ifc.ucb;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.ucb.XRemoteContentProviderAcceptor;
 import com.sun.star.ucb.XRemoteContentProviderDoneListener;
-import lib.MultiMethodTest;
 
 /**
  * Testing <code>com.sun.star.ucb.XRemoteContentProviderAcceptor</code>
