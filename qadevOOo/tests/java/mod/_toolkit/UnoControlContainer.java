39eac5e2002bed2b316dea06a03e895bb1c9c4f8
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:04:40 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:29:53 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_toolkit/UnoControlContainer.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlContainer.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoControlContainer.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:02:11 $
+ *  last change:$Date: 2003-09-08 13:04:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,25 +58,10 @@
  *
  *
  ************************************************************************/
-
 package mod._toolkit;
 
-import com.sun.star.awt.XControl;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.awt.XControlModel;
-import com.sun.star.awt.XDevice;
-import com.sun.star.awt.XToolkit;
-import com.sun.star.awt.XWindow;
-import com.sun.star.awt.XWindowPeer;
-import com.sun.star.drawing.XControlShape;
-import com.sun.star.drawing.XShape;
-import com.sun.star.frame.XController;
-import com.sun.star.frame.XFrame;
-import com.sun.star.text.XTextDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
-import com.sun.star.view.XControlAccess;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
@@ -85,9 +70,24 @@ import util.FormTools;
 import util.WriterTools;
 import util.utils;
 
+import com.sun.star.awt.XControl;
+import com.sun.star.awt.XControlModel;
+import com.sun.star.awt.XToolkit;
+import com.sun.star.awt.XWindow;
+import com.sun.star.awt.XWindowPeer;
+import com.sun.star.drawing.XControlShape;
+import com.sun.star.drawing.XShape;
+import com.sun.star.frame.XController;
+import com.sun.star.frame.XFrame;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.util.XCloseable;
+import com.sun.star.view.XControlAccess;
+
 
 public class UnoControlContainer extends TestCase {
-
     XTextDocument xTextDoc;
     XTextDocument xTD2;
     XControl xCtrl1;
@@ -95,93 +95,113 @@ public class UnoControlContainer extends TestCase {
 
     protected void initialize(TestParameters param, PrintWriter log) {
         try {
-            log.println( "creating a textdocument" );
-            xTD2 = WriterTools.createTextDoc((XMultiServiceFactory)param.getMSF());
-            xTextDoc = WriterTools.createTextDoc((XMultiServiceFactory)param.getMSF());
-        } catch ( Exception e ) {
+            log.println("creating a textdocument");
+            xTD2 = WriterTools.createTextDoc( (XMultiServiceFactory) param.getMSF());
+            xTextDoc = WriterTools.createTextDoc( (XMultiServiceFactory) param.getMSF());
+        } catch (Exception e) {
             // Some exception occures.FAILED
-            e.printStackTrace( log );
-            throw new StatusException( "Couldn't create document", e );
+            e.printStackTrace(log);
+            throw new StatusException("Couldn't create document", e);
         }
     }
 
-    protected void cleanup(TestParameters param, PrintWriter log) {
-        log.println( "disposing xTextDoc" );
-        xTextDoc.dispose();
-        xTD2.dispose();
+    protected void cleanup(TestParameters tParam, PrintWriter log) {
+        log.println("    disposing xTextDoc ");
+
+        try {
+            XCloseable closer = (XCloseable) UnoRuntime.queryInterface(
+                                        XCloseable.class, xTextDoc);
+            closer.close(true);
+
+            closer = (XCloseable) UnoRuntime.queryInterface(XCloseable.class,
+                                                            xTD2);
+            closer.close(true);
+        } catch (com.sun.star.util.CloseVetoException e) {
+            log.println("couldn't close document");
+        } catch (com.sun.star.lang.DisposedException e) {
+            log.println("couldn't close document");
+        }
     }
 
     public TestEnvironment createTestEnvironment(TestParameters param,
-                                                    PrintWriter log) {
+                                                 PrintWriter log) {
         // create Object Relations -------------------------------------------
         XInterface oObj = null;
         XControlShape shape = null;
         XControlModel model = null;
         XControlAccess access = null;
         XWindow anotherWindow = null;
+
         // for XControl
         XWindowPeer the_win = null;
         XToolkit the_kit = null;
-        XDevice aDevice = null;
+
+
         // create 2 XControls
         // create first XControl
-        shape = FormTools.createControlShape(xTextDoc,3000,4500,
-                                        15000,10000,"TextField");
+        shape = FormTools.createControlShape(xTextDoc, 3000, 4500, 15000,
+                                             10000, "TextField");
         WriterTools.getDrawPage(xTextDoc).add((XShape) shape);
         model = shape.getControl();
-        access = (XControlAccess) UnoRuntime.queryInterface
-                (XControlAccess.class,xTextDoc.getCurrentController());
+        access = (XControlAccess) UnoRuntime.queryInterface(
+                         XControlAccess.class, xTextDoc.getCurrentController());
+
         try {
             xCtrl1 = access.getControl(model);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace(log);
             throw new StatusException("Couldn't create XControl", e);
         }
+
+
         // create second XControl
-        shape = FormTools.createControlShape(xTextDoc,3000,4500,
-                                        15000,10000,"CommandButton");
+        shape = FormTools.createControlShape(xTextDoc, 3000, 4500, 15000,
+                                             10000, "CommandButton");
         WriterTools.getDrawPage(xTextDoc).add((XShape) shape);
         model = shape.getControl();
-        access = (XControlAccess) UnoRuntime.queryInterface
-                (XControlAccess.class,xTextDoc.getCurrentController());
+        access = (XControlAccess) UnoRuntime.queryInterface(
+                         XControlAccess.class, xTextDoc.getCurrentController());
+
         try {
             xCtrl2 = access.getControl(model);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace(log);
             throw new StatusException("Couldn't create XControl", e);
         }
+
         // create XToolkit, XWindowPeer, XDevice
         //Insert a ControlShape and get the ControlModel
-        XControlShape aShape = FormTools.createUnoControlShape(
-                                xTextDoc,3000,4500,15000,10000,"CommandButton",
-                                "UnoControlButton");
+        XControlShape aShape = FormTools.createUnoControlShape(xTextDoc, 3000,
+                                                               4500, 15000,
+                                                               10000,
+                                                               "CommandButton",
+                                                               "UnoControlButton");
 
         WriterTools.getDrawPage(xTD2).add((XShape) aShape);
+
         XControlModel the_Model = aShape.getControl();
 
         //Try to query XControlAccess
         XControlAccess the_access = (XControlAccess) UnoRuntime.queryInterface(
-                        XControlAccess.class,xTD2.getCurrentController());
+                                            XControlAccess.class,
+                                            xTD2.getCurrentController());
 
         //get the ButtonControl for the needed Object relations
         try {
             the_win = the_access.getControl(the_Model).getPeer();
             the_kit = the_win.getToolkit();
-            aDevice = the_kit.createScreenCompatibleDevice(200,200);
+            //aDevice = the_kit.createScreenCompatibleDevice(200, 200);
         } catch (Exception e) {
             log.println("Couldn't get ButtonControl");
             e.printStackTrace(log);
-            throw new StatusException("Couldn't get ButtonControl", e );
+            throw new StatusException("Couldn't get ButtonControl", e);
         }
 
         try {
             XController aController = xTD2.getCurrentController();
             XFrame aFrame = aController.getFrame();
             anotherWindow = aFrame.getComponentWindow();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace(log);
             throw new StatusException("Couldn't create XWindow", e);
         }
@@ -189,37 +209,42 @@ public class UnoControlContainer extends TestCase {
         // finished create Object Relations -----------------------------------
         // create the UnoControlContainer
         try {
-            oObj = (XInterface) ((XMultiServiceFactory)param.getMSF()).createInstance(
-                                    "com.sun.star.awt.UnoControlContainer");
-            XControl xCtrl = (XControl)
-                UnoRuntime.queryInterface(XControl.class, oObj);
+            oObj = (XInterface) ( (XMultiServiceFactory) param.getMSF())
+                                     .createInstance("com.sun.star.awt.UnoControlContainer");
+
+            XControl xCtrl = (XControl) UnoRuntime.queryInterface(
+                                     XControl.class, oObj);
             xCtrl.setModel(the_Model);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace(log);
             throw new StatusException("Couldn't create UnoControlContainer", e);
         }
 
-        log.println( "creating a new environment for UnoControlContainer object");
-        TestEnvironment tEnv = new TestEnvironment( oObj );
+        log.println(
+                "creating a new environment for UnoControlContainer object");
+
+        TestEnvironment tEnv = new TestEnvironment(oObj);
 
         String objName = "UnoControlContainer";
         tEnv.addObjRelation("OBJNAME", "toolkit." + objName);
+
+
         // Object Relation for XControlContainer
         tEnv.addObjRelation("CONTROL1", xCtrl1);
         tEnv.addObjRelation("CONTROL2", xCtrl2);
+
+
         // Object Relation for XControl
         tEnv.addObjRelation("CONTEXT", xTD2);
-        tEnv.addObjRelation("WINPEER",the_win);
-        tEnv.addObjRelation("TOOLKIT",the_kit);
-        tEnv.addObjRelation("MODEL",the_Model);
+        tEnv.addObjRelation("WINPEER", the_win);
+        tEnv.addObjRelation("TOOLKIT", the_kit);
+        tEnv.addObjRelation("MODEL", the_Model);
+
+
         // Object Relation for XWindow
         tEnv.addObjRelation("XWindow.AnotherWindow", anotherWindow);
-        System.out.println("ImplementationName: " + utils.getImplName(oObj ));
+        System.out.println("ImplementationName: " + utils.getImplName(oObj));
 
         return tEnv;
-
     }
-
 }
-
