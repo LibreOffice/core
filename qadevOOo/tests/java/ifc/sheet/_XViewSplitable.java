860f7a809c1511ca9876cde61a0c00c3cab5f138
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:07:44 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:33:42 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/sheet/_XViewSplitable.java
+++ qadevOOo/tests/java/ifc/sheet/_XViewSplitable.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XViewSplitable.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:12:12 $
+ *  last change:$Date: 2003-09-08 11:07:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,8 +61,9 @@
 
 package ifc.sheet;
 
-import com.sun.star.sheet.XViewSplitable;
 import lib.MultiMethodTest;
+
+import com.sun.star.sheet.XViewSplitable;
 
 /**
 * Testing <code>com.sun.star.sheet.XViewSplitable</code>
