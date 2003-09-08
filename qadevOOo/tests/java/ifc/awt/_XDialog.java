d311de35b6a6a325da9c880a2d3b4efb49575c7d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:11:19 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:23 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_XDialog.java
+++ qadevOOo/tests/java/ifc/awt/_XDialog.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XDialog.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:36 $
+ *  last change:$Date: 2003-09-08 10:11:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,8 +61,9 @@
 
 package ifc.awt;
 
-import com.sun.star.awt.XDialog;
 import lib.MultiMethodTest;
+
+import com.sun.star.awt.XDialog;
 
 /**
 * Testing <code>com.sun.star.awt.XDialog</code>
--- qadevOOo/tests/java/ifc/awt/_XFixedText.java
+++ qadevOOo/tests/java/ifc/awt/_XFixedText.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XFixedText.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:36 $
+ *  last change:$Date: 2003-09-08 10:11:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,10 @@
 package ifc.awt;
 
 
+import lib.MultiMethodTest;
+
 import com.sun.star.awt.TextAlign;
 import com.sun.star.awt.XFixedText;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.awt.XFixedText</code>
