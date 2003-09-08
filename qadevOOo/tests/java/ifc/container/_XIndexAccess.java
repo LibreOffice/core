7ef2d4b62cb96c430f466349176ce1dfa96a680d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:23:07 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:40:16 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/container/_XIndexAccess.java
+++ qadevOOo/tests/java/ifc/container/_XIndexAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XIndexAccess.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:38 $
+ *  last change:$Date: 2003-09-08 10:22:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,11 @@
 
 package ifc.container;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.container.XIndexAccess;
 import com.sun.star.lang.IndexOutOfBoundsException;
 import com.sun.star.lang.WrappedTargetException;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.container.XIndexAccess</code>
--- qadevOOo/tests/java/ifc/container/_XIndexContainer.java
+++ qadevOOo/tests/java/ifc/container/_XIndexContainer.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XIndexContainer.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:37 $
+ *  last change:$Date: 2003-09-08 10:23:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.container;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.container.XIndexContainer;
 import com.sun.star.lang.IndexOutOfBoundsException;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.container.XIndexContainer</code>
