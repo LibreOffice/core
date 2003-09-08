a73ae093b0bf06682fb7cd8a5d8d00fcf654e651
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:06:37 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 14:32:25 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/ifc/sheet/_XSubTotalCalculatable.java
@@ -0,0 +1,159 @@
+/*************************************************************************
+ *
+ *  $RCSfile: _XSubTotalCalculatable.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 11:06:37 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+package ifc.sheet;
+
+import lib.MultiMethodTest;
+import lib.Status;
+import lib.StatusException;
+
+import com.sun.star.sheet.SubTotalColumn;
+import com.sun.star.sheet.XSpreadsheet;
+import com.sun.star.sheet.XSubTotalCalculatable;
+import com.sun.star.sheet.XSubTotalDescriptor;
+import com.sun.star.uno.UnoRuntime;
+
+
+public class _XSubTotalCalculatable extends MultiMethodTest {
+    public XSubTotalCalculatable oObj;
+    protected XSubTotalDescriptor desc;
+    protected XSpreadsheet oSheet;
+
+    protected void before() {
+        oSheet = (XSpreadsheet) tEnv.getObjRelation("SHEET");
+
+        if (oSheet == null) {
+            log.println("Object relation oSheet is missing");
+            log.println("Trying to query the needed Interface");
+            oSheet = (XSpreadsheet) UnoRuntime.queryInterface(
+                             XSpreadsheet.class, tEnv.getTestObject());
+
+            if (oSheet == null) {
+                throw new StatusException(Status.failed(
+                                                  "Object relation oSheet is missing"));
+            }
+        }
+    }
+
+    public void _applySubTotals() {
+        requiredMethod("createSubTotalDescriptor()");
+
+        boolean res = true;
+
+        try {
+            oSheet.getCellByPosition(0, 0).setFormula("first");
+            oSheet.getCellByPosition(1, 0).setFormula("second");
+            oSheet.getCellByPosition(0, 3).setFormula("");
+            oSheet.getCellByPosition(0, 1).setValue(5);
+            oSheet.getCellByPosition(0, 2).setValue(5);
+            oSheet.getCellByPosition(1, 1).setValue(17);
+            oSheet.getCellByPosition(1, 2).setValue(25);
+            oObj.applySubTotals(desc, true);
+
+            String formula = oSheet.getCellByPosition(0, 3).getFormula();
+            String expected = "=SUBTOTAL(9;$A$2:$A$3)";
+            res = formula.equals(expected);
+
+            if (!res) {
+                log.println("getting: " + formula);
+                log.println("expected: " + expected);
+            }
+        } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
+            log.println("Couldn't fill cells" + e.getLocalizedMessage());
+            res = false;
+        }
+
+        tRes.tested("applySubTotals()", res);
+    }
+
+    public void _createSubTotalDescriptor() {
+        desc = oObj.createSubTotalDescriptor(true);
+
+        SubTotalColumn[] columns = new SubTotalColumn[1];
+        columns[0] = new SubTotalColumn();
+        columns[0].Column = 0;
+        columns[0].Function = com.sun.star.sheet.GeneralFunction.SUM;
+        desc.addNew(columns, 0);
+        tRes.tested("createSubTotalDescriptor()", true);
+    }
+
+    public void _removeSubTotals() {
+        requiredMethod("applySubTotals()");
+
+        boolean res = true;
+
+        try {
+            oObj.removeSubTotals();
+
+            String formula = oSheet.getCellByPosition(0, 3).getFormula();
+            String expected = "";
+            res = formula.equals(expected);
+
+            if (!res) {
+                log.println("getting: " + formula);
+                log.println("expected: " + expected);
+            }
+        } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
+            log.println("Couldn't get cell" + e.getLocalizedMessage());
+        }
+
+        tRes.tested("removeSubTotals()", res);
+    }
+}
