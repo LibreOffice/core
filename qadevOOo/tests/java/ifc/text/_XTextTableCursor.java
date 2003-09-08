160691c9f6e3b02d763d38e51a59950e28c86e2d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:22:11 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:53:25 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/text/_XTextTableCursor.java
+++ qadevOOo/tests/java/ifc/text/_XTextTableCursor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XTextTableCursor.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:13:07 $
+ *  last change:$Date: 2003-09-08 11:21:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.text;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.table.XCell;
 import com.sun.star.text.XTextTableCursor;
-import lib.MultiMethodTest;
 
 
 /**
