68a61f04007008dd19eead1826f013c36bef38cc
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:28:32 +0000

    INTEGRATION: CWS qadev11 (1.2.28); FILE MERGED 2003/09/03 14:01:42 sw 1.2.28.1: #112049#

--- qadevOOo/tests/java/ifc/drawing/_FillProperties.java
+++ qadevOOo/tests/java/ifc/drawing/_FillProperties.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _FillProperties.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-05 10:35:54 $
+ *  last change:$Date: 2003-09-08 10:28:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,8 +61,9 @@
 
 package ifc.drawing;
 
-import com.sun.star.awt.XBitmap;
 import lib.MultiPropertyTest;
+
+import com.sun.star.awt.XBitmap;
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
 
