0620f532865621b50e4421277d5208f12fe910fa
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:15:53 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 15:21:42 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/mod/_sc/ScSortDescriptorBase.java
@@ -0,0 +1,185 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ScSortDescriptorBase.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 12:15:53 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._sc;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.sheet.XSpreadsheets;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.util.XSortable;
+
+
+/**
+ *
+ * initial description
+ * @see com.sun.star.beans.XPropertySet
+ * @see com.sun.star.sheet.SheetSortDescriptor
+ *
+ */
+public class ScSortDescriptorBase extends TestCase {
+
+    XSpreadsheetDocument xSpreadsheetDoc;
+
+    /**
+     * in general this method creates a testdocument
+     *
+     *  @param tParam    class which contains additional test parameters
+     *  @param log        class to log the test state and result
+     *
+     *
+     *  @see TestParameters
+     *    @see PrintWriter
+     *
+     */
+    protected void initialize( TestParameters tParam, PrintWriter log ) {
+        SOfficeFactory SOF = SOfficeFactory.getFactory(  (XMultiServiceFactory) tParam.getMSF() );
+
+        try {
+            log.println( "creating a Spreadsheet document" );
+            xSpreadsheetDoc = SOF.createCalcDoc(null);
+        } catch ( com.sun.star.uno.Exception e ) {
+            // Some exception occures.FAILED
+            e.printStackTrace( log );
+            throw new StatusException( "Couldn't create document", e );
+        }
+
+    }
+
+    /**
+     * in general this method disposes the testenvironment and document
+     *
+     *  @param tParam    class which contains additional test parameters
+     *  @param log        class to log the test state and result
+     *
+     *
+     *  @see TestParameters
+     *    @see PrintWriter
+     *
+     */
+    protected void cleanup( TestParameters tParam, PrintWriter log ) {
+        log.println( "    disposing xSheetDoc " );
+        XComponent oComp = (XComponent) UnoRuntime.queryInterface
+            (XComponent.class, xSpreadsheetDoc) ;
+        oComp.dispose();
+    }
+
+
+
+    /**
+     *    creating a Testenvironment for the interfaces to be tested
+     *
+     *  @param tParam    class which contains additional test parameters
+     *  @param log        class to log the test state and result
+     *
+     *  @return    Status class
+     *
+     *  @see TestParameters
+     *    @see PrintWriter
+     */
+    public TestEnvironment createTestEnvironment( TestParameters tParam,
+                                                  PrintWriter log )
+                                                    throws StatusException {
+
+        XInterface oObj = null;
+        XSortable xSORT = null;
+
+        // creation of testobject here
+        // first we write what we are intend to do to log file
+        log.println( "creating a test environment" );
+
+        // create testobject here
+
+        log.println("getting sheets");
+        XSpreadsheets xSpreadsheets = (XSpreadsheets)xSpreadsheetDoc.getSheets();
+        if (xSpreadsheets == null) log.println("FAILED"); else log.println("OK");
+
+        log.println("getting a sheet");
+        XIndexAccess oIndexAccess = (XIndexAccess)
+                    UnoRuntime.queryInterface(XIndexAccess.class, xSpreadsheets);
+
+        try {
+            oObj = (XInterface) UnoRuntime.queryInterface(XInterface.class,oIndexAccess.getByIndex(0));
+        } catch (Exception e) {
+            throw new StatusException( "Couldn't get a spreadsheet", e);
+        }
+
+        xSORT = (XSortable) UnoRuntime.queryInterface(XSortable.class,oObj);
+
+        TestEnvironment tEnv = new TestEnvironment(oObj);
+        tEnv.addObjRelation("xSORT",xSORT);
+        return tEnv;
+
+    } // finish method getTestEnvironment
+
+}    // finish class ScSortDescriptorBase
+
