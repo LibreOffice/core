6455ee70f3f1b9e414e869996c2c15e31f007fca
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:31:16 +0000

    INTEGRATION: CWS qadev11 (1.2.28); FILE MERGED 2003/09/03 14:01:46 sw 1.2.28.1: #112049#

--- qadevOOo/tests/java/ifc/drawing/_XDrawView.java
+++ qadevOOo/tests/java/ifc/drawing/_XDrawView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XDrawView.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-28 08:51:51 $
+ *  last change:$Date: 2003-09-08 10:31:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,11 @@
 
 package ifc.drawing;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.drawing.XDrawPage;
 import com.sun.star.drawing.XDrawPages;
 import com.sun.star.drawing.XDrawView;
-import lib.MultiMethodTest;
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
 
