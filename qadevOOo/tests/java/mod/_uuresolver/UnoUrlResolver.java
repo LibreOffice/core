08d4ff605d9fcb7a4b95229fc70ff9dfe068b9e6
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:14:29 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:33 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_uuresolver/UnoUrlResolver.java
+++ qadevOOo/tests/java/mod/_uuresolver/UnoUrlResolver.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoUrlResolver.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:10:23 $
+ *  last change:$Date: 2003-09-08 13:14:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,14 +61,16 @@
 
 package mod._uuresolver;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.Exception;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.Exception;
+import com.sun.star.uno.XInterface;
 
 public class UnoUrlResolver extends TestCase {
 
