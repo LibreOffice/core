ce797a7a5de0f3ee0de48ab742bf09b1914f7c6a
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:48:37 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:43 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sw/SwXReferenceMark.java
+++ qadevOOo/tests/java/mod/_sw/SwXReferenceMark.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXReferenceMark.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:48:06 $
+ *  last change:$Date: 2003-09-08 12:48:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,14 @@
 
 package mod._sw;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.container.XNamed;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.text.XText;
@@ -69,12 +77,6 @@ import com.sun.star.text.XTextCursor;
 import com.sun.star.text.XTextDocument;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
 
 /**
  * Test for object which is represented by service
