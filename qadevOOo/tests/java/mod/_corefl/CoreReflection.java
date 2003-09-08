6a4d53e7db75acef18d34ddb2b86f8238376b52b
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:40:48 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:32:44 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_corefl/CoreReflection.java
+++ qadevOOo/tests/java/mod/_corefl/CoreReflection.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CoreReflection.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:35:06 $
+ *  last change:$Date: 2003-09-08 11:40:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,13 +61,15 @@
 
 package mod._corefl;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
