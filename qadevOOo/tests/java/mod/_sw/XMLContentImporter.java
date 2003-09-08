e8a002891f5fe914696304768df3b22a68feba55
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:55:49 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:56 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sw/XMLContentImporter.java
+++ qadevOOo/tests/java/mod/_sw/XMLContentImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLContentImporter.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:54:54 $
+ *  last change:$Date: 2003-09-08 12:55:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,17 +61,18 @@
 
 package mod._sw;
 
-import com.sun.star.document.XImporter;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.text.XTextDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
  * Test for object which is represented by service
@@ -161,8 +162,8 @@ public class XMLContentImporter extends TestCase {
         try {
             oInt = xMSF.createInstance
                 ("com.sun.star.comp.Writer.XMLContentImporter") ;
-            XImporter imp = (XImporter) UnoRuntime.queryInterface
-                (XImporter.class, oInt) ;
+            //XImporter imp = (XImporter) UnoRuntime.queryInterface
+            //    (XImporter.class, oInt) ;
             //imp.setTargetDocument(xTextDoc) ;
         } catch (com.sun.star.uno.Exception e) {
             e.printStackTrace(log) ;
