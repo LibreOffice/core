bb7f209cde9535774141dfa617219fe0717ead3d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:12:18 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:59:10 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/task/_XStatusIndicatorFactory.java
+++ qadevOOo/tests/java/ifc/task/_XStatusIndicatorFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XStatusIndicatorFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:13:03 $
+ *  last change:$Date: 2003-09-08 11:12:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.task;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.task.XStatusIndicator;
 import com.sun.star.task.XStatusIndicatorFactory;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.task.XStatusIndicatorFactory</code>
