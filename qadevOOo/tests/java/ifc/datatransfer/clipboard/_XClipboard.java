20b64a8e73aa2bd2989d5135f22c48eec5dd8177
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:24:50 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:58:21 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/datatransfer/clipboard/_XClipboard.java
+++ qadevOOo/tests/java/ifc/datatransfer/clipboard/_XClipboard.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XClipboard.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:49 $
+ *  last change:$Date: 2003-09-08 10:24:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,12 +61,13 @@
 
 package ifc.datatransfer.clipboard;
 
+import lib.MultiMethodTest;
+import lib.StatusException;
+
 import com.sun.star.datatransfer.DataFlavor;
 import com.sun.star.datatransfer.XTransferable;
 import com.sun.star.datatransfer.clipboard.XClipboard;
 import com.sun.star.datatransfer.clipboard.XClipboardOwner;
-import lib.MultiMethodTest;
-import lib.StatusException;
 
 /**
 * Testing <code>com.sun.star.datatransfer.clipboard.XClipboard</code>
@@ -135,7 +136,7 @@ public class _XClipboard extends MultiMethodTest {
      */
     public void _getName() {
         String name = oObj.getName();
-        tRes.tested("getName()", oObj.getName() != null);
+        tRes.tested("getName()", name != null);
     }
 
     /**
