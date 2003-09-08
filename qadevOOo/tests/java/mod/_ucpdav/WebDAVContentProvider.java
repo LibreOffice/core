d6c4dce501bd13d844a6a366d33d75e635d3cc0b
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:12:12 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:25 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ucpdav/WebDAVContentProvider.java
+++ qadevOOo/tests/java/mod/_ucpdav/WebDAVContentProvider.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: WebDAVContentProvider.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:08:57 $
+ *  last change:$Date: 2003-09-08 13:12:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,16 +61,18 @@
 
 package mod._ucpdav;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.ucb.XContentIdentifierFactory;
 import com.sun.star.uno.Exception;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
 
 /**
  * Creates a <code>com.sun.star.ucb.WebDAVContentProvider</code> instance.
