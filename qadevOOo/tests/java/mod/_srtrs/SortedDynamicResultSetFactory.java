c94901521ce3f51b0fac0247cc1ebbd6b0eb7beb
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:31:04 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:14 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_srtrs/SortedDynamicResultSetFactory.java
+++ qadevOOo/tests/java/mod/_srtrs/SortedDynamicResultSetFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SortedDynamicResultSetFactory.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:29:01 $
+ *  last change:$Date: 2003-09-08 12:31:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,12 +61,14 @@
 
 package mod._srtrs;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
  * Test for object which is represented by service
