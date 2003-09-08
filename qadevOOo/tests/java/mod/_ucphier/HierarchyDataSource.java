ca79ce2eb5a9588dfc9616d3f079e534eab8b49d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:13:01 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:28 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ucphier/HierarchyDataSource.java
+++ qadevOOo/tests/java/mod/_ucphier/HierarchyDataSource.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: HierarchyDataSource.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:09:25 $
+ *  last change:$Date: 2003-09-08 13:13:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,12 +61,14 @@
 
 package mod._ucphier;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
  * Creates <code>HierarchyDataSource</code> implementation of
