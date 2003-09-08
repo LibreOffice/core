938169ee36c83a13e252203b436c565aaba965a4
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:29:46 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:09 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sm/XMLExporter.java
+++ qadevOOo/tests/java/mod/_sm/XMLExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLExporter.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:27:31 $
+ *  last change:$Date: 2003-09-08 12:29:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,15 @@
 
 package mod._sm;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+import util.XMLTools;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XExporter;
 import com.sun.star.lang.XComponent;
@@ -70,13 +79,6 @@ import com.sun.star.uno.Type;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 import com.sun.star.xml.sax.XDocumentHandler;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
-import util.XMLTools;
 
 /**
  * Test for object which is represented by service
