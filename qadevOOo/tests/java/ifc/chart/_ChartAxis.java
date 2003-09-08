0742289cc597f447725946a69807d031e124a704
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:17:22 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:58:07 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/chart/_ChartAxis.java
+++ qadevOOo/tests/java/ifc/chart/_ChartAxis.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _ChartAxis.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:20 $
+ *  last change:$Date: 2003-09-08 10:17:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.chart;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 import lib.StatusException;
 import util.ValueChanger;
 
