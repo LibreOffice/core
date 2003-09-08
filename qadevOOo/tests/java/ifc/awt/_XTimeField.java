db41b3594cb07721fad691a2943b6aa8634ab96c
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:13:43 +0000

    INTEGRATION: CWS qadev11 (1.2.28); FILE MERGED 2003/09/03 13:36:28 sw 1.2.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_XTimeField.java
+++ qadevOOo/tests/java/ifc/awt/_XTimeField.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XTimeField.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-25 11:16:38 $
+ *  last change:$Date: 2003-09-08 10:13:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,8 +62,9 @@
 package ifc.awt;
 
 
-import com.sun.star.awt.XTimeField;
 import lib.MultiMethodTest;
+
+import com.sun.star.awt.XTimeField;
 
 /**
 * Testing <code>com.sun.star.awt.XTimeField</code>
