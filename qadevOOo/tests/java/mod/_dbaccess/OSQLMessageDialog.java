ef41e734dffc5e96afc6ff3fd37bb374b1a13950
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:43:28 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:32:49 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_dbaccess/OSQLMessageDialog.java
+++ qadevOOo/tests/java/mod/_dbaccess/OSQLMessageDialog.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: OSQLMessageDialog.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:37:48 $
+ *  last change:$Date: 2003-09-08 11:43:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,14 +61,16 @@
 
 package mod._dbaccess;
 
-import com.sun.star.uno.XInterface;
-import com.sun.star.lang.XMultiServiceFactory;
 import java.io.PrintWriter;
+
 import lib.Status;
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
  * Test for object which is represented by service
