f3394af72ae9136a394fcda2d4a54224ba499329
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:13:33 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:30 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ucprmt/ContentProvider.java
+++ qadevOOo/tests/java/mod/_ucprmt/ContentProvider.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ContentProvider.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:09:43 $
+ *  last change:$Date: 2003-09-08 13:13:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,16 +61,18 @@
 
 package mod._ucprmt;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.ucb.XContentIdentifierFactory;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.Status;
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.ucb.XContentIdentifierFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
@@ -116,7 +118,6 @@ public class ContentProvider extends TestCase {
     public TestEnvironment createTestEnvironment
             (TestParameters Param, PrintWriter log ) {
         XInterface oObj = null;
-        Object oInterface = null;
         XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
         XContentIdentifierFactory cntIDFac = null ;
 
--- qadevOOo/tests/java/mod/_ucprmt/ProviderAcceptor.java
+++ qadevOOo/tests/java/mod/_ucprmt/ProviderAcceptor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ProviderAcceptor.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:09:51 $
+ *  last change:$Date: 2003-09-08 13:13:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,15 +61,17 @@
 
 package mod._ucprmt;
 
-import com.sun.star.ucb.XRemoteContentProviderAcceptor;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.ucb.XRemoteContentProviderAcceptor;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 public class ProviderAcceptor extends TestCase {
 
