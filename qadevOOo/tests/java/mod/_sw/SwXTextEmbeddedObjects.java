75fae5f73d44320ffc9d93bf2bba342980ff4f09
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:51:30 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:48 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sw/SwXTextEmbeddedObjects.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextEmbeddedObjects.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextEmbeddedObjects.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:50:35 $
+ *  last change:$Date: 2003-09-08 12:51:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,20 @@
 
 package mod._sw;
 
-import com.sun.star.text.XTextDocument;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.text.XTextEmbeddedObjectsSupplier;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
 import util.utils;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.text.XTextEmbeddedObjectsSupplier;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 
 public class SwXTextEmbeddedObjects extends TestCase {
--- qadevOOo/tests/java/mod/_sw/SwXTextFieldMasters.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextFieldMasters.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextFieldMasters.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:50:57 $
+ *  last change:$Date: 2003-09-08 12:51:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,17 +61,19 @@
 
 package mod._sw;
 
-import com.sun.star.text.XTextDocument;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.text.XTextFieldsSupplier;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.text.XTextFieldsSupplier;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
  *
