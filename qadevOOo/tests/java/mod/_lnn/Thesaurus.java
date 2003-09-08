cfb1cae433fc107a05a8805978d76837950ba24f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:59:07 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:33:37 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_lnn/Thesaurus.java
+++ qadevOOo/tests/java/mod/_lnn/Thesaurus.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Thesaurus.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:54:50 $
+ *  last change:$Date: 2003-09-08 11:59:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,13 +61,15 @@
 
 package mod._lnn;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
  * Test for object which is represented by service
