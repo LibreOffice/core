8ce8ab6625a37319ae98794394100fd5d231760f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:42:40 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:32 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sw/SwXAutoTextContainer.java
+++ qadevOOo/tests/java/mod/_sw/SwXAutoTextContainer.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXAutoTextContainer.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:42:15 $
+ *  last change:$Date: 2003-09-08 12:42:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,15 +61,16 @@
 
 package mod._sw;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.text.XAutoTextContainer;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XAutoTextContainer;
+import com.sun.star.uno.UnoRuntime;
 
 /**
 * Test for object which is represented by service
@@ -97,7 +98,6 @@ public class SwXAutoTextContainer extends TestCase {
     */
     public synchronized TestEnvironment createTestEnvironment(
             TestParameters Param, PrintWriter log ) throws StatusException {
-        XInterface oInterface = null;
         XAutoTextContainer  oContainer = null;
 
         // creation of testobject here
