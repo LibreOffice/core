541b9ec41ab0c340a874a6f0b255fa9551cf061f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:09:07 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:22:17 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/ScDrawPagesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDrawPagesObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDrawPagesObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:05:45 $
+ *  last change:$Date: 2003-09-08 12:09:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,19 +61,21 @@
 
 package mod._sc;
 
-import com.sun.star.drawing.XDrawPages;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.drawing.XDrawPagesSupplier;
-import com.sun.star.lang.XComponent;
-import com.sun.star.sheet.XSpreadsheetDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.drawing.XDrawPages;
+import com.sun.star.drawing.XDrawPagesSupplier;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
