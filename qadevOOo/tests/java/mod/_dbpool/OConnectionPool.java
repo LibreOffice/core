faee25158e9f17d4652e9b4d5c0febfc1e8e3a77
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:44:01 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:32:52 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_dbpool/OConnectionPool.java
+++ qadevOOo/tests/java/mod/_dbpool/OConnectionPool.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: OConnectionPool.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:38:18 $
+ *  last change:$Date: 2003-09-08 11:44:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,16 +61,18 @@
 
 package mod._dbpool;
 
-import lib.TestCase;
-import lib.Status;
-import lib.TestParameters;
 import java.io.PrintWriter;
-import lib.TestEnvironment;
+
+import lib.Status;
 import lib.StatusException;
-import com.sun.star.uno.XInterface;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.beans.PropertyValue;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
 import util.DBTools;
+
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
