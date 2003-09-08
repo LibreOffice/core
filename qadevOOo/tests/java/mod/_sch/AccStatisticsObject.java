f1b53184f4f8b1d8ddb5de125636c5f9bd1aba98
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:22:06 +0000

    INTEGRATION: CWS qadev11 (1.3.14); FILE MERGED 2003/09/03 15:33:45 sw 1.3.14.1: #112049#

--- qadevOOo/tests/java/mod/_sch/AccStatisticsObject.java
+++ qadevOOo/tests/java/mod/_sch/AccStatisticsObject.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccStatisticsObject.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Date: 2003-05-27 13:18:52 $
+ *  last change: $Date: 2003-09-08 12:22:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,32 +60,27 @@
  ************************************************************************/
 package mod._sch;
 
-import lib.TestCase;
-import com.sun.star.lang.XMultiServiceFactory;
-import lib.TestParameters;
 import java.io.PrintWriter;
-import lib.TestEnvironment;
-import com.sun.star.uno.XInterface;
-import util.SOfficeFactory;
+
 import lib.StatusException;
-import com.sun.star.frame.XController;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
 import util.AccessibilityTools;
-import com.sun.star.accessibility.AccessibleRole;
-import com.sun.star.accessibility.XAccessibleContext;
-import com.sun.star.accessibility.XAccessibleValue;
-import com.sun.star.accessibility.XAccessibleComponent;
-import com.sun.star.beans.XPropertySet;
-import com.sun.star.frame.XModel;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.awt.XWindow;
-import com.sun.star.accessibility.XAccessible;
-import util.utils;
 import util.SOfficeFactory;
+import util.utils;
+
+import com.sun.star.accessibility.AccessibleRole;
+import com.sun.star.accessibility.XAccessible;
+import com.sun.star.accessibility.XAccessibleComponent;
+import com.sun.star.accessibility.XAccessibleContext;
+import com.sun.star.awt.XWindow;
+import com.sun.star.beans.XPropertySet;
 import com.sun.star.chart.XChartDocument;
-import com.sun.star.drawing.XShape;
-import com.sun.star.awt.Rectangle;
-import com.sun.star.awt.PosSize;
-import com.sun.star.chart.XAxisXSupplier;
+import com.sun.star.frame.XModel;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 public class AccStatisticsObject extends TestCase {
 
