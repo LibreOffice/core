3a1b969d664b1f0a82ec671f581a59dfb2f75f18
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:09:13 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:18 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_UnoControlPatternFieldModel.java
+++ qadevOOo/tests/java/ifc/awt/_UnoControlPatternFieldModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _UnoControlPatternFieldModel.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:41 $
+ *  last change:$Date: 2003-09-08 10:09:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.awt;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 import util.utils;
 
 /**
@@ -106,6 +105,18 @@ public class _UnoControlPatternFieldModel extends MultiPropertyTest {
     */
     public void _TextColor() {
         testProperty("TextColor", new PropertyTester() {
+            protected Object getNewValue(String p, Object old) {
+                return utils.isVoid(old) ? new Integer(4321) : null ;
+            }
+        }) ;
+    }
+
+    /**
+    * This property can be VOID, and in case if it is so new
+    * value must defined.
+    */
+    public void _TextLineColor() {
+        testProperty("TextLineColor", new PropertyTester() {
             protected Object getNewValue(String p, Object old) {
                 return utils.isVoid(old) ? new Integer(4321) : null ;
             }
