a5fed552139dda50e97f0e4049b7b3e52ba328d7
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:06:57 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:33:40 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/sheet/_XSpreadsheets.java
+++ qadevOOo/tests/java/ifc/sheet/_XSpreadsheets.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XSpreadsheets.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:12:15 $
+ *  last change:$Date: 2003-09-08 11:06:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,8 +61,9 @@
 
 package ifc.sheet;
 
-import com.sun.star.sheet.XSpreadsheets;
 import lib.MultiMethodTest;
+
+import com.sun.star.sheet.XSpreadsheets;
 
 /**
 * Testing <code>com.sun.star.sheet.XSpreadsheets</code>
--- qadevOOo/tests/java/ifc/sheet/_XSubTotalField.java
+++ qadevOOo/tests/java/ifc/sheet/_XSubTotalField.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XSubTotalField.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:12:14 $
+ *  last change:$Date: 2003-09-08 11:06:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,11 @@
 
 package ifc.sheet;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.sheet.GeneralFunction;
 import com.sun.star.sheet.SubTotalColumn;
 import com.sun.star.sheet.XSubTotalField;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.sheet.XSubTotalField</code>
