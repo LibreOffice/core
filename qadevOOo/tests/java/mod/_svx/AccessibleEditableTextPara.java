9c63755ac6a0ae28fcab7c9399e2ff23cabb713d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:35:12 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 15:28:15 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/mod/_svx/AccessibleEditableTextPara.java
@@ -0,0 +1,195 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessibleEditableTextPara.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._svx;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.AccessibilityTools;
+import util.SOfficeFactory;
+import util.utils;
+
+import com.sun.star.accessibility.AccessibleRole;
+import com.sun.star.accessibility.XAccessible;
+import com.sun.star.accessibility.XAccessibleContext;
+import com.sun.star.accessibility.XAccessibleEditableText;
+import com.sun.star.awt.XWindow;
+import com.sun.star.frame.XModel;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+
+
+public class AccessibleEditableTextPara extends TestCase {
+
+    XComponent xSpreadsheetDoc = null;
+
+    /**
+    * Called to create an instance of <code>TestEnvironment</code>
+    * with an object to test and related objects.
+    * Obtains accissible object for the spreadsheet document.
+    *
+    * @param tParam test parameters
+    * @param log writer to log information while testing
+    *
+    * @see TestEnvironment
+    * @see #getTestEnvironment()
+    */
+    protected TestEnvironment createTestEnvironment(
+        TestParameters Param, PrintWriter log) {
+
+        XInterface oObj = null;
+
+
+        XModel aModel = (XModel)
+            UnoRuntime.queryInterface(XModel.class, xSpreadsheetDoc);
+
+        AccessibilityTools at = new AccessibilityTools();
+
+        XWindow xWindow = at.getCurrentWindow( (XMultiServiceFactory) Param.getMSF(), aModel);
+        XAccessible xRoot = at.getAccessibleObject(xWindow);
+
+        XAccessibleContext InputLine = at.getAccessibleObjectForRole(xRoot, AccessibleRole.PANEL,"Input line");
+        try {
+            oObj = InputLine.getAccessibleChild(0);
+            XAccessibleEditableText et = (XAccessibleEditableText) UnoRuntime.queryInterface(XAccessibleEditableText.class, oObj);
+            et.setText("AccessibleEditablePara");
+        } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
+
+        }
+        //at.printAccessibleTree(log, xRoot);
+        log.println("ImplementationName " + utils.getImplName(oObj));
+
+        TestEnvironment tEnv = new TestEnvironment(oObj);
+
+        final XAccessibleEditableText edText = (XAccessibleEditableText)
+            UnoRuntime.queryInterface(XAccessibleEditableText.class,oObj) ;
+
+        tEnv.addObjRelation("EventProducer",
+            new ifc.accessibility._XAccessibleEventBroadcaster.EventProducer() {
+                public void fireEvent() {
+                    try {
+                    int l = new String("AccessibleEditablePara").length();
+                    edText.deleteText(0, l);
+                    edText.setText("Event");
+                    edText.setText("AccessibleEditablePara");
+                    }catch(com.sun.star.lang.IndexOutOfBoundsException e) {}
+                }
+            });
+
+        return tEnv;
+
+    }
+
+    /**
+    * Called while disposing a <code>TestEnvironment</code>.
+    * Disposes text document.
+    * @param tParam test parameters
+    * @param tEnv the environment to cleanup
+    * @param log writer to log information while testing
+    */
+    protected void cleanup( TestParameters Param, PrintWriter log) {
+        log.println( "    disposing xSheetDoc " );
+        xSpreadsheetDoc.dispose();
+    }
+
+    /**
+     * Called while the <code>TestCase</code> initialization. In the
+     * implementation does nothing. Subclasses can override to initialize
+     * objects shared among all <code>TestEnvironment</code>s.
+     *
+     * @param tParam test parameters
+     * @param log writer to log information while testing
+     *
+     * @see #initializeTestCase()
+     */
+    protected void initialize(TestParameters Param, PrintWriter log) {
+        // get a soffice factory object
+        SOfficeFactory SOF = SOfficeFactory.getFactory(  (XMultiServiceFactory) Param.getMSF());
+
+        try {
+            log.println("creating a spreadsheetdocument");
+            xSpreadsheetDoc = (XComponent) UnoRuntime.queryInterface(XComponent.class,SOF.createCalcDoc(null));
+            shortWait();
+        } catch (com.sun.star.uno.Exception e) {
+            e.printStackTrace( log );
+            throw new StatusException( "Couldn't create document ", e );
+        }
+    }
+
+    /**
+    * Sleeps for 0.5 sec. to allow StarOffice to react on <code>
+    * reset</code> call.
+    */
+    private void shortWait() {
+        try {
+            Thread.sleep(500) ;
+        } catch (InterruptedException e) {
+            log.println("While waiting :" + e) ;
+        }
+    }
+
+}
