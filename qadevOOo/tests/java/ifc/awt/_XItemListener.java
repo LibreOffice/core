3d3d9a0e02e5b47610d4a38e14da1c31953fdfc5
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:11:53 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:24 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_XItemListener.java
+++ qadevOOo/tests/java/ifc/awt/_XItemListener.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XItemListener.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:36 $
+ *  last change:$Date: 2003-09-08 10:11:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,11 +62,12 @@
 package ifc.awt;
 
 
-import com.sun.star.awt.ItemEvent;
-import com.sun.star.awt.XItemListener;
 import lib.MultiMethodTest;
 import lib.Status;
 import lib.StatusException;
+
+import com.sun.star.awt.ItemEvent;
+import com.sun.star.awt.XItemListener;
 
 /**
 * Testing <code>com.sun.star.awt.XItemListener</code>
