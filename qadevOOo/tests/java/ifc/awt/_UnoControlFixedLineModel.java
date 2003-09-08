b9c3e80d33a0306c30e0c390b1dd2de414b28ada
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:07:55 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:15 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_UnoControlFixedLineModel.java
+++ qadevOOo/tests/java/ifc/awt/_UnoControlFixedLineModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _UnoControlFixedLineModel.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:45 $
+ *  last change:$Date: 2003-09-08 10:07:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.awt;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 import util.utils;
 
 /**
