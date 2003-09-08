524ae0d228ee371b2da806d5355bb99b76ee2b99
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:20:40 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:58:14 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/chart/_XDiagram.java
+++ qadevOOo/tests/java/ifc/chart/_XDiagram.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XDiagram.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:08 $
+ *  last change:$Date: 2003-09-08 10:20:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,11 +61,12 @@
 
 package ifc.chart;
 
-import com.sun.star.beans.XPropertySet;
-import com.sun.star.chart.XDiagram;
 import lib.MultiMethodTest;
 import lib.Status;
 import lib.StatusException;
+
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.chart.XDiagram;
 
 /**
 * Testing <code>com.sun.star.chart.XDiagram</code>
--- qadevOOo/tests/java/ifc/chart/_XTwoAxisXSupplier.java
+++ qadevOOo/tests/java/ifc/chart/_XTwoAxisXSupplier.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XTwoAxisXSupplier.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:07 $
+ *  last change:$Date: 2003-09-08 10:20:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.chart;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.chart.XTwoAxisXSupplier;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.chart.XTwoAxisXSupplier</code>
