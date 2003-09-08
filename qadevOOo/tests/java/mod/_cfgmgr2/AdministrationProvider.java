1febc2ba33554bc17ee1d69fb28969ce9ce40cc3
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:35:59 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 15:03:26 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/mod/_cfgmgr2/AdministrationProvider.java
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AdministrationProvider.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 11:35:59 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._cfgmgr2;
+
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+import com.sun.star.beans.PropertyState;
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
+
+
+public class AdministrationProvider extends TestCase {
+
+    /** Called to create an instance of <code>TestEnvironment</code> with an
+     * object to test and related objects.
+     *
+     * For this an instance of com.sun.star.comp.configuration.AdministrationProvider
+     * is created at the MultiServiceFactory of the Office
+     *
+     * @param tParam test parameters
+     * @param log writer to log information while testing
+     *
+     * @see TestEnvironment
+     * @see #getTestEnvironment()
+     *
+     */
+    protected TestEnvironment createTestEnvironment(TestParameters tParam,
+                                                    PrintWriter log) {
+        XInterface oObj = null;
+
+        try {
+            oObj = (XInterface) ((XMultiServiceFactory)tParam.getMSF())
+                                      .createInstance("com.sun.star.comp.configuration.AdministrationProvider");
+        } catch (com.sun.star.uno.Exception e) {
+        }
+
+        util.dbg.printInterfaces(oObj);
+
+        PropertyValue[] nodeArgs = new PropertyValue[1];
+        PropertyValue nodepath = new PropertyValue();
+        nodepath.Name = "nodepath";
+        nodepath.Value = "org.openoffice.Setup";
+        nodepath.Handle = -1;
+        nodepath.State = PropertyState.DEFAULT_VALUE;
+        nodeArgs[0] = nodepath;
+
+        Vector args = new Vector();
+
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+        args.add(0, nodeArgs);
+
+        TestEnvironment tEnv = new TestEnvironment(oObj);
+
+        tEnv.addObjRelation("XMSF.Args",
+                            args.toArray(new Object[args.size()][]));
+        tEnv.addObjRelation("needArgs", "AdministrationProvider");
+
+        return tEnv;
+    }
+}
