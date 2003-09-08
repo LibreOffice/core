859f754672b48161623f076fd1d78e1f3a63d481
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:03:35 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 15:20:42 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/mod/_sc/ScAnnotationTextCursor.java
@@ -0,0 +1,186 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ScAnnotationTextCursor.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 12:03:35 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._sc;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sheet.XSheetAnnotation;
+import com.sun.star.sheet.XSheetAnnotationAnchor;
+import com.sun.star.sheet.XSpreadsheet;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.sheet.XSpreadsheets;
+import com.sun.star.table.CellAddress;
+import com.sun.star.table.XCell;
+import com.sun.star.table.XCellRange;
+import com.sun.star.text.XSimpleText;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+
+
+/**
+ *
+ * initial description
+ * @see com.sun.star.beans.XPropertySet
+ * @see com.sun.star.beans.XPropertyState
+ * @see com.sun.star.document.XDocumentInsertable
+ * @see com.sun.star.style.CharacterProperties
+ * @see com.sun.star.style.ParagraphProperties
+ * @see com.sun.star.text.XParagraphCursor
+ * @see com.sun.star.text.XSentenceCursor
+ * @see com.sun.star.text.XTextCursor
+ * @see com.sun.star.text.XTextRange
+ * @see com.sun.star.text.XWordCursor
+ * @see com.sun.star.util.XSortable
+ *
+ */
+public class ScAnnotationTextCursor extends TestCase {
+
+        XSpreadsheetDocument xSheetDoc = null;
+
+    protected void initialize( TestParameters tParam, PrintWriter log ) {
+        SOfficeFactory SOF = SOfficeFactory.getFactory(  (XMultiServiceFactory) tParam.getMSF() );
+
+        try {
+            log.println( "creating a Spreadsheet document" );
+            xSheetDoc = SOF.createCalcDoc(null);
+        } catch ( com.sun.star.uno.Exception e ) {
+            // Some exception occures.FAILED
+            e.printStackTrace( log );
+            throw new StatusException( "Couldn³t create document", e );
+        }
+
+    }
+
+    protected void cleanup( TestParameters tParam, PrintWriter log ) {
+        log.println( "    disposing xSheetDoc " );
+        XComponent oComp = (XComponent) UnoRuntime.queryInterface
+            (XComponent.class, xSheetDoc) ;
+        oComp.dispose();
+    }
+
+
+    /**
+     *    creating a Testenvironment for the interfaces to be tested
+     */
+    public synchronized TestEnvironment createTestEnvironment
+            ( TestParameters Param, PrintWriter log )
+            throws StatusException {
+
+        XInterface oObj = null;
+
+        // creation of testobject here
+        // first we write what we are intend to do to log file
+        log.println( "Creating a test environment" );
+
+        CellAddress cellPos = new CellAddress((short)0, 1, 2);
+
+        try {
+            log.println("Getting test object ") ;
+
+            XSpreadsheetDocument xArea = (XSpreadsheetDocument)
+                UnoRuntime.queryInterface(XSpreadsheetDocument.class, xSheetDoc);
+
+            XSpreadsheets oSheets = (XSpreadsheets) xArea.getSheets();
+
+            XIndexAccess XAccess = (XIndexAccess)
+                        UnoRuntime.queryInterface(XIndexAccess.class, oSheets);
+
+            XSpreadsheet oSheet = (XSpreadsheet)XAccess.getByIndex(cellPos.Sheet);
+
+            XCellRange oCRange = (XCellRange)
+                            UnoRuntime.queryInterface(XCellRange.class, oSheet);
+
+            XCell oCell = oCRange.getCellByPosition(cellPos.Column, cellPos.Row);
+
+            XSheetAnnotationAnchor oAnnoA = (XSheetAnnotationAnchor)
+                UnoRuntime.queryInterface(XSheetAnnotationAnchor.class, oCell);
+
+            XSheetAnnotation oAnno = oAnnoA.getAnnotation();
+            XSimpleText aText = (XSimpleText)
+                            UnoRuntime.queryInterface(XSimpleText.class,oAnno);
+            aText.setString("A nice little Test");
+            oObj = aText.createTextCursor();
+
+        } catch (Exception e) {
+            e.printStackTrace(log) ;
+            throw new StatusException(
+                    "Error getting test object from spreadsheet document",e) ;
+        }
+
+        TestEnvironment tEnv = new TestEnvironment( oObj );
+
+        // Other parameters required for interface tests
+
+        return tEnv;
+    }
+
+}    // finish class ScAnnotationTextCursor
+
