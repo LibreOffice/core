8631e88856bfd5fe48450560f1b5aa87badeb8e2
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:13:37 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 14:49:36 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/ifc/text/_TextEmbeddedObject.java
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  $RCSfile: _TextEmbeddedObject.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 11:13:37 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package ifc.text;
+
+import lib.MultiPropertyTest;
+
+/**
+ * Testing <code>com.sun.star.text.TextEmbeddedObject</code>
+ * service properties :
+ * <ul>
+ *  <li><code> AnchorPageNo          </code></li>
+ *  <li><code> AnchorType            </code></li>
+ *  <li><code> BackColor             </code></li>
+ *  <li><code> BorderDistance        </code></li>
+ *  <li><code> BottomBorder          </code></li>
+ *  <li><code> BottomMargin          </code></li>
+ *  <li><code> ChainNextName         </code></li>
+ *  <li><code> ChainPrevName         </code></li>
+ *  <li><code> ContentProtected      </code></li>
+ *  <li><code> EditInReadonly        </code></li>
+ *  <li><code> FrameStyle            </code></li>
+ *  <li><code> GraphicFilter         </code></li>
+ *  <li><code> GraphicLocation       </code></li>
+ *  <li><code> GraphicURL            </code></li>
+ *  <li><code> Height                </code></li>
+ *  <li><code> HoriOrient            </code></li>
+ *  <li><code> HoriOrientPosition    </code></li>
+ *  <li><code> HoriOrientRelation    </code></li>
+ *  <li><code> HyperLinkName         </code></li>
+ *  <li><code> HyperLinkTarget       </code></li>
+ *  <li><code> HyperLinkURL          </code></li>
+ *  <li><code> LeftBorder            </code></li>
+ *  <li><code> LeftMargin            </code></li>
+ *  <li><code> Opaque                </code></li>
+ *  <li><code> PageToggle            </code></li>
+ *  <li><code> PositionProtected     </code></li>
+ *  <li><code> Print                 </code></li>
+ *  <li><code> RelativeHeight        </code></li>
+ *  <li><code> RelativeWidth         </code></li>
+ *  <li><code> RightBorder           </code></li>
+ *  <li><code> RightMargin           </code></li>
+ *  <li><code> ServerMap             </code></li>
+ *  <li><code> ShadowFormat          </code></li>
+ *  <li><code> Size                  </code></li>
+ *  <li><code> SizeProtected         </code></li>
+ *  <li><code> SizeRelative          </code></li>
+ *  <li><code> SizeType              </code></li>
+ *  <li><code> Surround              </code></li>
+ *  <li><code> SurroundAnchoronly    </code></li>
+ *  <li><code> TextColumns           </code></li>
+ *  <li><code> TopBorder             </code></li>
+ *  <li><code> TopMargin             </code></li>
+ *  <li><code> TransparentBackground </code></li>
+ *  <li><code> VertOrient            </code></li>
+ *  <li><code> VertOrientPosition    </code></li>
+ *  <li><code> VertOrientRelation    </code></li>
+ *  <li><code> Width                 </code></li>
+ * </ul> <p>
+ * Properties testing is automated by <code>lib.MultiPropertyTest</code>.
+ * @see com.sun.star.text.TextEmbeddedObject
+ */
+public class _TextEmbeddedObject extends MultiPropertyTest {
+
+}  // finish class _TextEmbeddedObject
+
