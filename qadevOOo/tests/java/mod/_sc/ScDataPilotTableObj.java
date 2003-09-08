a54f9bf942450b3a9137f89432c745920524a10d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:08:14 +0000

    INTEGRATION: CWS qadev11 (1.3.14); FILE MERGED 2003/09/03 15:22:15 sw 1.3.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/ScDataPilotTableObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotTableObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDataPilotTableObj.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-05-27 13:04:53 $
+ *  last change:$Date: 2003-09-08 12:08:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,18 @@
 
 package mod._sc;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.beans.XPropertySet;
-import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.container.XIndexAccess;
 import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.sheet.TableFilterField;
 import com.sun.star.sheet.XDataPilotDescriptor;
 import com.sun.star.sheet.XDataPilotTables;
@@ -75,16 +83,10 @@ import com.sun.star.sheet.XSpreadsheetDocument;
 import com.sun.star.sheet.XSpreadsheets;
 import com.sun.star.table.CellAddress;
 import com.sun.star.table.CellRangeAddress;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
@@ -166,7 +168,6 @@ public class ScDataPilotTableObj extends TestCase {
     */
     protected synchronized TestEnvironment createTestEnvironment(TestParameters Param, PrintWriter log) {
 
-        Object oInterface = null;
         XInterface oObj = null;
 
         CellAddress sCellAddress = new CellAddress();
