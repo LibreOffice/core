2a53934b96124b533b7e11c95cde3ea8e80ffdd8
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:11:13 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:22 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ucb/UcbContentProviderProxyFactory.java
+++ qadevOOo/tests/java/mod/_ucb/UcbContentProviderProxyFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UcbContentProviderProxyFactory.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:08:22 $
+ *  last change:$Date: 2003-09-08 13:11:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,13 +61,15 @@
 
 package mod._ucb;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
