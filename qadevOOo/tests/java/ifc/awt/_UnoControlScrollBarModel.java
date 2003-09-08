8a2034d6f829e6c62f3d98bedca1ad6ac4f533f0
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:09:32 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:19 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_UnoControlScrollBarModel.java
+++ qadevOOo/tests/java/ifc/awt/_UnoControlScrollBarModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _UnoControlScrollBarModel.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:40 $
+ *  last change:$Date: 2003-09-08 10:09:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.awt;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 import util.utils;
 
 /**
