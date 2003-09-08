4be61102736c2658e4cbf3051142220ccd017652
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:16:23 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:22:33 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/ScStyleFamiliesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScStyleFamiliesObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScStyleFamiliesObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:12:53 $
+ *  last change:$Date: 2003-09-08 12:16:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,20 @@
 
 package mod._sc;
 
-import com.sun.star.container.XNameAccess;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.lang.XComponent;
-import com.sun.star.sheet.XSpreadsheetDocument;
-import com.sun.star.style.XStyleFamiliesSupplier;
-import com.sun.star.uno.UnoRuntime;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.container.XNameAccess;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.style.XStyleFamiliesSupplier;
+import com.sun.star.uno.UnoRuntime;
 
 /**
 * Test for object which is represented by service
