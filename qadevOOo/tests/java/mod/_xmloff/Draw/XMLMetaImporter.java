95a36b4e3b642269709bb69d57a0508c46f8327f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:16:52 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:40 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_xmloff/Draw/XMLMetaImporter.java
+++ qadevOOo/tests/java/mod/_xmloff/Draw/XMLMetaImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLMetaImporter.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:12:52 $
+ *  last change:$Date: 2003-09-08 13:16:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,20 @@
 
 package mod._xmloff.Draw;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XDocumentInfoSupplier;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
 
 /**
  * Test for object which is represented by service
@@ -152,7 +154,6 @@ public class XMLMetaImporter extends TestCase {
 
         XInterface oObj = null;
         Object oInt = null ;
-        final String impValue = "XMLImporter_test" ;
 
         // creation of testobject here
         // first we write what we are intend to do to log file
