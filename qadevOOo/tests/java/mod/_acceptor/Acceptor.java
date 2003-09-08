44a53734d1fe912f391034425b1ffd59360e1db9
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:34:12 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:32:36 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_acceptor/Acceptor.java
+++ qadevOOo/tests/java/mod/_acceptor/Acceptor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Acceptor.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:31:49 $
+ *  last change:$Date: 2003-09-08 11:34:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,14 +61,16 @@
 
 package mod._acceptor;
 
-import com.sun.star.uno.XInterface;
-import com.sun.star.lang.XMultiServiceFactory;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.utils;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
 * Here <code>com.sun.star.connection.Acceptor</code> service is tested.<p>
@@ -80,7 +82,6 @@ import util.utils;
 */
 public class Acceptor extends TestCase {
 
-    private XInterface acceptor = null ;
 
     /**
     * Acceptor chooses the first port after <code>basePort</code>
