b0dfa15f344f252b88a2dac79029c71539a88519
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:25:01 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:54:23 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/ucb/_XParameterizedContentProvider.java
+++ qadevOOo/tests/java/ifc/ucb/_XParameterizedContentProvider.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XParameterizedContentProvider.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:13:36 $
+ *  last change:$Date: 2003-09-08 11:25:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.ucb;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.ucb.XContentProvider;
 import com.sun.star.ucb.XParameterizedContentProvider;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.ucb.XParameterizedContentProvider</code>
