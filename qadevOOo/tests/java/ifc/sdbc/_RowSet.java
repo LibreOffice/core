43a740d8e956e1d0734894be2afa20227699f835
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:52:39 +0000

    INTEGRATION: CWS qadev11 (1.2.28); FILE MERGED 2003/09/03 14:58:47 sw 1.2.28.1: #112049#

--- qadevOOo/tests/java/ifc/sdbc/_RowSet.java
+++ qadevOOo/tests/java/ifc/sdbc/_RowSet.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _RowSet.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-27 12:24:35 $
+ *  last change:$Date: 2003-09-08 10:52:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,6 @@ package ifc.sdbc;
 
 import lib.MultiPropertyTest;
 
-import com.sun.star.container.XNameAccess;
 import com.sun.star.uno.Any;
 
 
