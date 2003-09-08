dbcae846b3602221e12ded177d46b13551d49396
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:28:45 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:06 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sfx/FrameLoader.java
+++ qadevOOo/tests/java/mod/_sfx/FrameLoader.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: FrameLoader.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:25:45 $
+ *  last change:$Date: 2003-09-08 12:28:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,10 +61,8 @@
 
 package mod._sfx;
 
-import com.sun.star.uno.XInterface;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.lang.XMultiServiceFactory;
 import java.io.PrintWriter;
+
 import lib.Status;
 import lib.StatusException;
 import lib.TestCase;
@@ -72,6 +70,10 @@ import lib.TestEnvironment;
 import lib.TestParameters;
 import util.dbg;
 import util.utils;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
