f3e28dceae80f7f173256fec780a330126225d17
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:09:26 +0000

    INTEGRATION: CWS qadev11 (1.2.28); FILE MERGED 2003/09/03 14:59:03 sw 1.2.28.1: #112049#

--- qadevOOo/tests/java/ifc/table/_CellProperties.java
+++ qadevOOo/tests/java/ifc/table/_CellProperties.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _CellProperties.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-04 13:00:14 $
+ *  last change:$Date: 2003-09-08 11:09:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,11 +62,11 @@
 package ifc.table;
 
 import lib.MultiPropertyTest;
+
 import com.sun.star.container.XNameContainer;
-import com.sun.star.xml.AttributeData;
-import com.sun.star.uno.Any;
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
+import com.sun.star.xml.AttributeData;
 
 /**
 * Testing <code>com.sun.star.table.CellProperties</code>
