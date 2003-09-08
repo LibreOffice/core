9616626168ef87dd3aa8fd5c8a94269706bb515f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:37:48 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:11:11 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/frame/_XComponentLoader.java
+++ qadevOOo/tests/java/ifc/frame/_XComponentLoader.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XComponentLoader.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:10:24 $
+ *  last change:$Date: 2003-09-08 10:37:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,11 +61,12 @@
 
 package ifc.frame;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.beans.PropertyState;
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.lang.XComponent;
-import lib.MultiMethodTest;
 
 
 /**
