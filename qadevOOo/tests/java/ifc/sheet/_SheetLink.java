1a744b2623fffccbb5c1bdcbfeac05ae0a45055d
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:56:57 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:33:22 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/sheet/_SheetLink.java
+++ qadevOOo/tests/java/ifc/sheet/_SheetLink.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _SheetLink.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:12:36 $
+ *  last change:$Date: 2003-09-08 10:56:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.sheet;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 import util.ValueChanger;
 
 /**
