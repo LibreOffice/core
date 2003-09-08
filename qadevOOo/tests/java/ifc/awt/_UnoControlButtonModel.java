e5f0c54e620359412befae9fa8de2fab648437b7
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:06:34 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:36:11 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/awt/_UnoControlButtonModel.java
+++ qadevOOo/tests/java/ifc/awt/_UnoControlButtonModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _UnoControlButtonModel.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:07:49 $
+ *  last change:$Date: 2003-09-08 10:06:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 package ifc.awt;
 
 import lib.MultiPropertyTest;
-import lib.MultiPropertyTest$PropertyTester;
 
 /*
 * Testing <code>com.sun.star.awt.UnoControlButtonModel</code>
