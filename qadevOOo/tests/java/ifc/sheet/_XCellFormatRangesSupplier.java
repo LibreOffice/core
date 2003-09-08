c7fabca3380a452066dce52516f7acddf55c6070
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:58:36 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 14:25:22 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/ifc/sheet/_XCellFormatRangesSupplier.java
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  $RCSfile: _XCellFormatRangesSupplier.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 10:58:36 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+package ifc.sheet;
+
+import lib.MultiMethodTest;
+
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.sheet.XCellFormatRangesSupplier;
+
+/**
+ * Interface test to check the interface XCellFormatRangesSupplier
+ *
+ * methods:
+ *
+ *  getCellFormatRanges()
+ *
+ */
+
+public class _XCellFormatRangesSupplier extends MultiMethodTest {
+
+    public XCellFormatRangesSupplier oObj = null;
+
+    /**
+     * call the method getCellFormatRanges and returns OK result if
+     * the gained XIndexAccess isn't null and the method checkIndexAccess
+     * returns true.
+     */
+
+    public void _getCellFormatRanges() {
+        boolean res = true;
+        XIndexAccess xIA = oObj.getCellFormatRanges();
+        if (xIA != null) {
+            res = checkIndexAccess(xIA);
+        } else {
+            log.println("The gained IndexAccess is null");
+            res = false;
+        }
+        tRes.tested("getCellFormatRanges()",res);
+    }
+
+    /**
+     * calls the method getCount at the IndexAccess, returns true is it is >0
+     * and getByIndex() doesn't throw an exception for Indexes between 0 and count
+     */
+
+    protected boolean checkIndexAccess(XIndexAccess xIA) {
+        boolean res = true;
+        int count = xIA.getCount();
+        log.println("Found "+count+" Elements");
+        res &= count>0;
+        for (int k=0; k<count; k++) {
+            try {
+                Object element = xIA.getByIndex(k);
+                log.println("Element "+k+" = "+element);
+            } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
+                log.println("Unexpected Exception while getting by Index ("+k+")"+e.getMessage());
+                res &=false;
+            } catch (com.sun.star.lang.WrappedTargetException e) {
+                log.println("Unexpected Exception while getting by Index ("+k+")"+e.getMessage());
+                res &=false;
+            }
+        }
+        return res;
+    }
+
+}
