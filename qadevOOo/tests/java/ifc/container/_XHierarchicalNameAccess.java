1ce78c34c28a7a6dc3632c67dd85ccce3e01032b
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:22:45 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 13:40:01 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/container/_XHierarchicalNameAccess.java
+++ qadevOOo/tests/java/ifc/container/_XHierarchicalNameAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XHierarchicalNameAccess.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:38 $
+ *  last change:$Date: 2003-09-08 10:22:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.container;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.container.NoSuchElementException;
 import com.sun.star.container.XHierarchicalNameAccess;
-import lib.MultiMethodTest;
 
 /**
 * Testing <code>com.sun.star.container.XHierarchicalNameAccess</code>
