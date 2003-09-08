56009430cfb1c5ef0a3380a0068bd0211683279c
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:19:33 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:22:39 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/XMLImporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLImporter.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:16:20 $
+ *  last change:$Date: 2003-09-08 12:19:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,14 @@
 
 package mod._sc;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.document.XImporter;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
@@ -68,12 +76,6 @@ import com.sun.star.sheet.XSpreadsheetDocument;
 import com.sun.star.sheet.XSpreadsheets;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
 /**
  * Test for object which is represented by service
  * <code>com.sun.star.comp.Calc.XMLImporter</code>. <p>
