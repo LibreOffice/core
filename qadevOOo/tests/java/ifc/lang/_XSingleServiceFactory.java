55f55b1b57e453d74013aac884435f6543a81568
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:46:17 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:15:25 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/lang/_XSingleServiceFactory.java
+++ qadevOOo/tests/java/ifc/lang/_XSingleServiceFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XSingleServiceFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:10:55 $
+ *  last change:$Date: 2003-09-08 10:46:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.lang;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.lang.XSingleServiceFactory;
 import com.sun.star.uno.UnoRuntime;
-import lib.MultiMethodTest;
 
 
 /**
