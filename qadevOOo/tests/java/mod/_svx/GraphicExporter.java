aa6ec6641c54f7cc687c3e139e98e466e19dd4c1
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:36:39 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:28:32 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_svx/GraphicExporter.java
+++ qadevOOo/tests/java/mod/_svx/GraphicExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: GraphicExporter.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:35:51 $
+ *  last change:$Date: 2003-09-08 12:36:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,16 @@
 
 package mod._svx;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.DrawTools;
+import util.SOfficeFactory;
+import util.XMLTools;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XExporter;
 import com.sun.star.drawing.XShape;
@@ -70,14 +80,6 @@ import com.sun.star.ucb.XSimpleFileAccess;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 import com.sun.star.util.URL;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.DrawTools;
-import util.SOfficeFactory;
-import util.XMLTools;
 
 /**
  * Test for object which is represented by service
