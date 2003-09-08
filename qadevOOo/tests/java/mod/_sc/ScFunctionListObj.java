784fa8bc28e231d1fe00d52791d1f6f037f7cb16
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:09:36 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:22:18 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
+++ qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScFunctionListObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:06:06 $
+ *  last change:$Date: 2003-09-08 12:09:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,17 +61,19 @@
 
 package mod._sc;
 
-import com.sun.star.lang.XComponent;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.sheet.XSpreadsheetDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
