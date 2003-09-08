6e69c095bf5fc01e4c93df54008983924e69fa60
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:28:13 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:59:13 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/ui/dialogs/_XFilterManager.java
+++ qadevOOo/tests/java/ifc/ui/dialogs/_XFilterManager.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XFilterManager.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:13:44 $
+ *  last change:$Date: 2003-09-08 11:28:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,8 +61,9 @@
 
 package ifc.ui.dialogs;
 
-import com.sun.star.ui.dialogs.XFilterManager;
 import lib.MultiMethodTest;
+
+import com.sun.star.ui.dialogs.XFilterManager;
 
 /**
 * Testing <code>com.sun.star.ui.XFilterManager</code>
