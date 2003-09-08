a920fa32c56ff92806e61b1a19420975fe995ef1
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:34:39 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:32:37 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ado/ODriver.java
+++ qadevOOo/tests/java/mod/_ado/ODriver.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ODriver.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 12:32:29 $
+ *  last change:$Date: 2003-09-08 11:34:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,15 +61,17 @@
 
 package mod._ado;
 
-import lib.TestCase;
-import com.sun.star.lang.XMultiServiceFactory;
-import lib.TestParameters;
 import java.io.PrintWriter;
-import lib.TestEnvironment;
-import lib.StatusException;
+
 import lib.Status;
-import com.sun.star.uno.XInterface;
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
 import com.sun.star.beans.PropertyValue;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
 
 /**
 * Here <code>com.sun.star.sdbc.Driver</code> service is tested.<p>
