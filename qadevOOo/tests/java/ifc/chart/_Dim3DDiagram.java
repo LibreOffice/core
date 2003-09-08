b295bdb557191808a5826ee8dc3a9f224e5825a4
Rüdiger Timm <rt@openoffice.org>
2003-09-08 09:18:42 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:58:10 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/chart/_Dim3DDiagram.java
+++ qadevOOo/tests/java/ifc/chart/_Dim3DDiagram.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _Dim3DDiagram.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:08:14 $
+ *  last change:$Date: 2003-09-08 10:18:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,12 +61,13 @@
 
 package ifc.chart;
 
-import com.sun.star.beans.XPropertySet;
-import com.sun.star.chart.XChartDocument;
-import com.sun.star.uno.UnoRuntime;
 import lib.MultiPropertyTest;
 import lib.Status;
 import lib.StatusException;
+
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.chart.XChartDocument;
+import com.sun.star.uno.UnoRuntime;
 
 /**
 * Testing <code>com.sun.star.chart.Dim3DDiagram</code>
