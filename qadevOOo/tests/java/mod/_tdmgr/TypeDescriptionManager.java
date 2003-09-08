b4e40dba83bf38c6fc8b4db27ea297b21eeceac8
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:58:15 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:21 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_tdmgr/TypeDescriptionManager.java
+++ qadevOOo/tests/java/mod/_tdmgr/TypeDescriptionManager.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TypeDescriptionManager.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:56:58 $
+ *  last change:$Date: 2003-09-08 12:58:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,16 +61,18 @@
 
 package mod._tdmgr;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
 
 public class TypeDescriptionManager extends TestCase {
 
