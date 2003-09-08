8053233f5470119531a9eae644fa844749d400c6
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:25:47 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:33:56 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sd/SdDocLinkTargets.java
+++ qadevOOo/tests/java/mod/_sd/SdDocLinkTargets.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdDocLinkTargets.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:22:05 $
+ *  last change:$Date: 2003-09-08 12:25:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,17 +61,19 @@
 
 package mod._sd;
 
-import com.sun.star.document.XLinkTargetSupplier;
-import com.sun.star.lang.XComponent;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.document.XLinkTargetSupplier;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
--- qadevOOo/tests/java/mod/_sd/SdDrawPagesAccess.java
+++ qadevOOo/tests/java/mod/_sd/SdDrawPagesAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdDrawPagesAccess.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:22:28 $
+ *  last change:$Date: 2003-09-08 12:25:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,20 @@
 
 package mod._sd;
 
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+import util.SOfficeFactory;
+
 import com.sun.star.drawing.XDrawPages;
 import com.sun.star.drawing.XDrawPagesSupplier;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
-import java.io.PrintWriter;
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.SOfficeFactory;
 
 /**
 * Test for object which is represented by service
