76deb75ff8d5bdd03a4193f576883857a9ff8f6f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:13:13 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:35:29 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_ucppkg/PackageContentProvider.java
+++ qadevOOo/tests/java/mod/_ucppkg/PackageContentProvider.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: PackageContentProvider.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 14:09:34 $
+ *  last change:$Date: 2003-09-08 13:13:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,15 +61,17 @@
 
 package mod._ucppkg;
 
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.ucb.XContentIdentifierFactory;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.ucb.XContentIdentifierFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 public class PackageContentProvider extends TestCase {
 
