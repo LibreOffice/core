7950cee4e97328d84cc1e8b70b49ad6b9967d29f
Rüdiger Timm <rt@openoffice.org>
2003-09-08 12:12:33 +0000

    INTEGRATION: CWS qadev11 (1.1.2); FILE ADDED 2003/09/03 15:11:44 sw 1.1.2.1: #112049#

---
+++ qadevOOo/tests/java/mod/_ucpftp/FTPContentProvider.java
@@ -0,0 +1,115 @@
+/*************************************************************************
+ *
+ *  $RCSfile: FTPContentProvider.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-09-08 13:12:33 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._ucpftp;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.ucb.XContentIdentifierFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+
+public class FTPContentProvider extends TestCase {
+
+    public TestEnvironment createTestEnvironment
+            ( TestParameters Param,PrintWriter log ) {
+
+        XInterface oObj = null;
+        Object oInterface = null;
+        Object aUCB = null;
+
+        try {
+            XMultiServiceFactory xMSF = (XMultiServiceFactory)Param.getMSF();
+            oInterface = xMSF.createInstance
+                ( "com.sun.star.ucb.FTPContentProvider" );
+            aUCB = xMSF.createInstance
+                ( "com.sun.star.comp.ucb.UniversalContentBroker" );
+        } catch( com.sun.star.uno.Exception e ) {
+            log.println("Can't create an object." );
+            throw new StatusException( "Can't create an object", e );
+        }
+
+        oObj = (XInterface) oInterface;
+
+        XContentIdentifierFactory CIF = (XContentIdentifierFactory)
+                UnoRuntime.queryInterface(XContentIdentifierFactory.class,aUCB);
+
+        log.println("ImplementationName: "+util.utils.getImplName(oObj));
+        util.dbg.printInterfaces(oObj);
+        TestEnvironment tEnv = new TestEnvironment( oObj );
+
+        //Adding ObjRelation for XContentProvider
+        tEnv.addObjRelation("FACTORY",CIF);
+        tEnv.addObjRelation("CONTENT1","ftp:///ftp.sun.com");
+        tEnv.addObjRelation("CONTENT2","ftp:///ftp.openoffice.org");
+
+        return tEnv;
+
+    } // finish method getTestEnvironment
+
+}    // finish class FileProvider
+
