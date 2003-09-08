b0054e212347697b6e6f63435e3734595d01353e
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:04:20 +0000

    INTEGRATION: CWS qadev11 (1.3.14); FILE MERGED 2003/09/03 15:22:07 sw 1.3.14.1: #112049#

--- qadevOOo/tests/java/mod/_sc/ScAutoFormatFieldObj.java
+++ qadevOOo/tests/java/mod/_sc/ScAutoFormatFieldObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScAutoFormatFieldObj.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-05-27 13:01:02 $
+ *  last change:$Date: 2003-09-08 12:04:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,18 @@
 
 package mod._sc;
 
-import com.sun.star.container.XIndexAccess;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
-import util.SOfficeFactory;
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
 * Test for object which is represented by service
@@ -113,7 +113,6 @@ public class ScAutoFormatFieldObj extends TestCase {
 
         try {
 
-            SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF() );
                   log.println ("create Object ...") ;
             // creation of testobject here
             XInterface formats = (XInterface)((XMultiServiceFactory)Param.getMSF()).createInstance
