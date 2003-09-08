7b37909e83013876833c1d283050407691879be9
Rüdiger Timm <rt@openoffice.org>
2003-09-08 11:49:09 +0000

    INTEGRATION: CWS qadev11 (1.2.14); FILE MERGED 2003/09/03 15:34:44 sw 1.2.14.1: #112049#

--- qadevOOo/tests/java/mod/_sw/SwXShape.java
+++ qadevOOo/tests/java/mod/_sw/SwXShape.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXShape.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:48:27 $
+ *  last change:$Date: 2003-09-08 12:48:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,20 +61,22 @@
 
 package mod._sw;
 
-import com.sun.star.drawing.XDrawPage;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.drawing.XDrawPageSupplier;
-import com.sun.star.drawing.XShape;
-import com.sun.star.drawing.XShapes;
-import com.sun.star.text.XTextDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.drawing.XDrawPage;
+import com.sun.star.drawing.XDrawPageSupplier;
+import com.sun.star.drawing.XShape;
+import com.sun.star.drawing.XShapes;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
 
 /**
  * Test for object which is represented by service
--- qadevOOo/tests/java/mod/_sw/SwXStyleFamilies.java
+++ qadevOOo/tests/java/mod/_sw/SwXStyleFamilies.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXStyleFamilies.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-05-27 13:48:47 $
+ *  last change:$Date: 2003-09-08 12:49:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,19 @@
 
 package mod._sw;
 
-import com.sun.star.container.XNameAccess;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.style.XStyleFamiliesSupplier;
-import com.sun.star.text.XTextDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import java.io.PrintWriter;
+
 import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
+
+import com.sun.star.container.XNameAccess;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.style.XStyleFamiliesSupplier;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
 
 
 /**
@@ -126,8 +127,7 @@ public class SwXStyleFamilies extends TestCase {
     */
     public synchronized TestEnvironment createTestEnvironment(
             TestParameters Param, PrintWriter log ) throws StatusException {
-        XInterface oInterface = null;
-        XInterface oObj = null;
+
 
         log.println( "Creating a test environment" );
         XTextDocument xArea = (XTextDocument)
