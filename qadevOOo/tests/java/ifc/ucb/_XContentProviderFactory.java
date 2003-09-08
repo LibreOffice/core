ae7db5a3504f9d8491d065498037f240f0308a74
Rüdiger Timm <rt@openoffice.org>
2003-09-08 10:24:10 +0000

    INTEGRATION: CWS qadev11 (1.1.28); FILE MERGED 2003/09/03 14:54:20 sw 1.1.28.1: #112049#

--- qadevOOo/tests/java/ifc/ucb/_XContentProviderFactory.java
+++ qadevOOo/tests/java/ifc/ucb/_XContentProviderFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XContentProviderFactory.java,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change:$Date: 2003-01-27 18:13:38 $
+ *  last change:$Date: 2003-09-08 11:24:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,10 @@
 
 package ifc.ucb;
 
+import lib.MultiMethodTest;
+
 import com.sun.star.ucb.XContentProvider;
 import com.sun.star.ucb.XContentProviderFactory;
-import lib.MultiMethodTest;
 
 
 /**
