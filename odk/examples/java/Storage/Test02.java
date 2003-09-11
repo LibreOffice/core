7cc48dd0b821e2c2e8fd9f3d0e4de74d2a98dc97
Kurt Zenker <kz@openoffice.org>
2003-09-11 09:36:12 +0000

    INTEGRATION: CWS mav05 (1.1.2); FILE ADDED 2003/07/22 15:10:35 mav 1.1.2.1: #i15929# new storage api testing

---
+++ odk/examples/java/Storage/Test02.java
@@ -0,0 +1,151 @@
+package storagetesting;
+
+import com.sun.star.uno.XInterface;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.lang.XSingleServiceFactory;
+
+import com.sun.star.bridge.XUnoUrlResolver;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.io.XStream;
+import com.sun.star.io.XInputStream;
+
+import com.sun.star.embed.*;
+
+import storagetesting.TestHelper;
+import storagetesting.StorageTest;
+
+public class Test02 implements StorageTest {
+
+    XMultiServiceFactory m_xMSF;
+    XSingleServiceFactory m_xStorageFactory;
+    TestHelper m_aTestHelper;
+
+    public Test02( XMultiServiceFactory xMSF, XSingleServiceFactory xStorageFactory )
+    {
+        m_xMSF = xMSF;
+        m_xStorageFactory = xStorageFactory;
+        m_aTestHelper = new TestHelper( "Test02: " );
+    }
+
+    public boolean test()
+    {
+        try
+        {
+            XStream xTempFileStream = m_aTestHelper.CreateTempFileStream( m_xMSF );
+            if ( xTempFileStream == null )
+                return false;
+
+            // create storage based on the temporary stream
+            Object pArgs[] = new Object[2];
+            pArgs[0] = (Object) xTempFileStream;
+            pArgs[1] = new Integer( ElementModes.ELEMENT_WRITE );
+
+            Object oTempStorage = m_xStorageFactory.createInstanceWithArguments( pArgs );
+            XStorage xTempStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oTempStorage );
+            if ( xTempStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            // open a new substorage
+            XStorage xTempSubStorage = m_aTestHelper.openSubStorage( xTempStorage,
+                                                                    "SubStorage1",
+                                                                    ElementModes.ELEMENT_WRITE );
+            if ( xTempSubStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create substorage!" );
+                return false;
+            }
+
+            byte pBytes1[] = { 1, 1, 1, 1, 1 };
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xTempSubStorage, "SubStream1", "MediaType1", true, pBytes1 ) )
+                return false;
+
+            // set "MediaType" property for storages and check that "IsRoot" and "OpenMode" properties are set correctly
+            if ( !m_aTestHelper.setStorageTypeAndCheckProps( xTempStorage,
+                                                            "MediaType2",
+                                                            true,
+                                                            ElementModes.ELEMENT_WRITE ) )
+                return false;
+
+            // set "MediaType" property for storages and check that "IsRoot" and "OpenMode" properties are set correctly
+            if ( !m_aTestHelper.setStorageTypeAndCheckProps( xTempSubStorage,
+                                                            "MediaType3",
+                                                            false,
+                                                            ElementModes.ELEMENT_WRITE ) )
+                return false;
+
+            // commit substorage first
+            if ( !m_aTestHelper.commitStorage( xTempSubStorage ) )
+                return false;
+
+            // commit the root storage so the contents must be stored now
+            if ( !m_aTestHelper.commitStorage( xTempStorage ) )
+                return false;
+
+            // dispose used storage to free resources
+            if ( !m_aTestHelper.disposeStorage( xTempStorage ) )
+                return false;
+
+
+            // ================================================
+            // now check all the written information
+            // ================================================
+
+            // close the output part of the temporary stream
+            // the output part must present since we already wrote to the stream
+            if ( !m_aTestHelper.closeOutput( xTempFileStream ) )
+                return false;
+
+            XInputStream xTempInStream = m_aTestHelper.getInputStream( xTempFileStream );
+            if ( xTempInStream == null )
+                return false;
+
+
+            // open input stream
+            // since no mode is provided the result storage must be opened readonly
+            Object pOneArg[] = new Object[1];
+            pOneArg[0] = (Object) xTempInStream;
+
+            Object oResultStorage = m_xStorageFactory.createInstanceWithArguments( pOneArg );
+            XStorage xResultStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oResultStorage );
+            if ( xResultStorage == null )
+            {
+                m_aTestHelper.Error( "Can't open storage based on input stream!" );
+                return false;
+            }
+
+            if ( !m_aTestHelper.checkStorageProperties( xResultStorage, "MediaType2", true, ElementModes.ELEMENT_READ ) )
+                return false;
+
+            // open existing substorage
+            XStorage xResultSubStorage = m_aTestHelper.openSubStorage( xResultStorage,
+                                                                        "SubStorage1",
+                                                                        ElementModes.ELEMENT_READ );
+            if ( xResultSubStorage == null )
+            {
+                m_aTestHelper.Error( "Can't open existing substorage!" );
+                return false;
+            }
+
+            if ( !m_aTestHelper.checkStorageProperties( xResultSubStorage, "MediaType3", false, ElementModes.ELEMENT_READ ) )
+                return false;
+
+            if ( !m_aTestHelper.checkStream( xResultSubStorage, "SubStream1", "MediaType1", pBytes1 ) )
+                return false;
+
+            return true;
+        }
+        catch( Exception e )
+        {
+            m_aTestHelper.Error( "Exception: " + e );
+            return false;
+        }
+    }
+
+}
+
---
+++ odk/examples/java/Storage/Test03.java
@@ -0,0 +1,206 @@
+package storagetesting;
+
+import com.sun.star.uno.XInterface;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.lang.XSingleServiceFactory;
+
+import com.sun.star.bridge.XUnoUrlResolver;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+
+import com.sun.star.embed.*;
+import com.sun.star.container.XNameAccess;
+
+import storagetesting.TestHelper;
+import storagetesting.StorageTest;
+
+public class Test03 implements StorageTest {
+
+    XMultiServiceFactory m_xMSF;
+    XSingleServiceFactory m_xStorageFactory;
+    TestHelper m_aTestHelper;
+
+    public Test03( XMultiServiceFactory xMSF, XSingleServiceFactory xStorageFactory )
+    {
+        m_xMSF = xMSF;
+        m_xStorageFactory = xStorageFactory;
+        m_aTestHelper = new TestHelper( "Test03: " );
+    }
+
+    public boolean test()
+    {
+        try
+        {
+            // create temporary storage based on arbitrary medium
+            // after such a storage is closed it is lost
+            Object oTempStorage = m_xStorageFactory.createInstance();
+            XStorage xTempStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oTempStorage );
+            if ( xTempStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            // open a new substorage
+            XStorage xTempSubStorage = m_aTestHelper.openSubStorage( xTempStorage,
+                                                                        "SubStorage1",
+                                                                        ElementModes.ELEMENT_WRITE );
+            if ( xTempSubStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create substorage!" );
+                return false;
+            }
+
+            byte pBytes1[] = { 1, 1, 1, 1, 1 };
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xTempStorage, "SubStream1", "MediaType1", true, pBytes1 ) )
+                return false;
+
+            byte pBytes2[] = { 2, 2, 2, 2, 2 };
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xTempSubStorage, "SubStream2", "MediaType2", false, pBytes2 ) )
+                return false;
+
+            // set "MediaType" property for storages and check that "IsRoot" and "OpenMode" properties are set correctly
+            if ( !m_aTestHelper.setStorageTypeAndCheckProps( xTempSubStorage,
+                                                            "MediaType3",
+                                                            false,
+                                                            ElementModes.ELEMENT_WRITE ) )
+                return false;
+
+            if ( !m_aTestHelper.disposeStorage( xTempSubStorage ) )
+                return false;
+
+            // ================================================
+            // check storage hyerarchy tree
+            // ================================================
+
+            // check that isStorageElement() and isStreamElement reacts to nonexisting object correctly
+            try {
+                xTempStorage.isStorageElement( "does not exist" );
+                m_aTestHelper.Error( "Nonexisting element doesn't detected by isStorageElement() call!" );
+                return false;
+            }
+            catch( com.sun.star.container.NoSuchElementException ne )
+            {
+            }
+            catch( Exception e )
+            {
+                m_aTestHelper.Error( "Wrong exception is thrown by isStorageElement() call: " + e );
+                return false;
+            }
+
+            try {
+                xTempStorage.isStreamElement( "does not exist" );
+                m_aTestHelper.Error( "Nonexisting element doesn't detected by isStreamElement() call!" );
+                return false;
+            }
+            catch( com.sun.star.container.NoSuchElementException ne )
+            {
+            }
+            catch( Exception e )
+            {
+                m_aTestHelper.Error( "Wrong exception is thrown by isStreamElement() call: " + e );
+                return false;
+            }
+
+            XNameAccess xRootNameAccess = (XNameAccess) UnoRuntime.queryInterface( XNameAccess.class, xTempStorage );
+            if ( xRootNameAccess == null )
+            {
+                m_aTestHelper.Error( "Root storage doesn't support XNameAccess!" );
+                return false;
+            }
+
+            try {
+                if ( !xTempStorage.isStorageElement( "SubStorage1" ) || xTempStorage.isStreamElement( "SubStorage1" ) )
+                {
+                    m_aTestHelper.Error( "Child 'SubStorage1' can not be detected as storage!" );
+                    return false;
+                }
+
+                if ( xTempStorage.isStorageElement( "SubStream1" ) || !xTempStorage.isStreamElement( "SubStream1" ) )
+                {
+                    m_aTestHelper.Error( "Child 'SubStream1' can not be detected as stream!" );
+                    return false;
+                }
+            }
+            catch( Exception e )
+            {
+                m_aTestHelper.Error( "Child's type can not be detected, exception: " + e );
+                return false;
+            }
+
+
+            // check that root storage contents are represented correctly
+            String sRootCont[] = xRootNameAccess.getElementNames();
+
+            if ( sRootCont.length != 2 )
+            {
+                m_aTestHelper.Error( "Root storage contains wrong amount of children!" );
+                return false;
+            }
+
+            if ( !( sRootCont[0].equals( "SubStorage1" ) && sRootCont[1].equals( "SubStream1" )
+                 || sRootCont[0].equals( "SubStream1" ) && sRootCont[1].equals( "SubStorage1" ) )
+              || !( xRootNameAccess.hasByName( "SubStream1" ) && xRootNameAccess.hasByName( "SubStorage1" ) ) )
+            {
+                m_aTestHelper.Error( "Root storage contains wrong list of children!" );
+                return false;
+            }
+
+            // get storage through XNameAccess
+            XStorage xResultSubStorage = getStorageFromNameAccess( xRootNameAccess, "SubStorage1" );
+            if ( xResultSubStorage == null )
+                return false;
+
+            if ( !m_aTestHelper.checkStorageProperties( xResultSubStorage, "MediaType3", false, ElementModes.ELEMENT_READ ) )
+                return false;
+
+            XNameAccess xChildAccess = (XNameAccess) UnoRuntime.queryInterface( XNameAccess.class, xResultSubStorage );
+            if ( xChildAccess == null )
+            {
+                m_aTestHelper.Error( "Child storage doesn't support XNameAccess!" );
+                return false;
+            }
+
+            if ( !xChildAccess.hasByName( "SubStream2" )
+              || !xResultSubStorage.isStreamElement( "SubStream2" )
+              || xResultSubStorage.isStorageElement( "SubStream2" ) )
+            {
+                m_aTestHelper.Error( "'SubStream2' can not be detected as child stream element of 'SubStorage1'!" );
+                return false;
+            }
+
+            return true;
+        }
+        catch( Exception e )
+        {
+            m_aTestHelper.Error( "Exception: " + e );
+            return false;
+        }
+    }
+
+    public XStorage getStorageFromNameAccess( XNameAccess xAccess, String sName )
+    {
+        try
+        {
+            Object oStorage = xAccess.getByName( sName );
+            XStorage xResult = (XStorage) UnoRuntime.queryInterface( XStorage.class, oStorage );
+
+            if ( xResult != null )
+                return xResult;
+            else
+                m_aTestHelper.Error( "Can't retrieve substorage '" + sName + "' through XNameAccess!" );
+        }
+        catch( Exception e )
+        {
+            m_aTestHelper.Error( "Can't retrieve substorage '" + sName + "' through XNameAccess, exception: " + e );
+        }
+
+        return null;
+    }
+
+}
+
