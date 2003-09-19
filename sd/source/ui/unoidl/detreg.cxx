d05946ee221b24fa4826dab5382094ec99899d6b
Rüdiger Timm <rt@openoffice.org>
2003-09-19 07:17:53 +0000

    INTEGRATION: CWS fwkq1 (1.1.2); FILE ADDED 2003/07/14 17:51:34 mba 1.1.2.1: #110843#: get rid of factories

---
+++ sd/source/ui/unoidl/detreg.cxx
@@ -0,0 +1,152 @@
+/*************************************************************************
+ *
+ *  $RCSfile: detreg.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-09-19 08:17:53 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+#ifndef _COM_SUN_STAR_REGISTRY_XREGISTRYKEY_HPP_
+#include <com/sun/star/registry/XRegistryKey.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_H_
+#include <com/sun/star/uno/Sequence.h>
+#endif
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+#include "sddetect.hxx"
+
+using namespace ::rtl;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+
+extern "C" {
+
+void SAL_CALL component_getImplementationEnvironment(
+        const  sal_Char**   ppEnvironmentTypeName,
+        uno_Environment**   ppEnvironment           )
+{
+    *ppEnvironmentTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME ;
+}
+
+sal_Bool SAL_CALL component_writeInfo(  void*   pServiceManager ,
+                                        void*   pRegistryKey    )
+{
+    Reference< ::registry::XRegistryKey >
+            xKey( reinterpret_cast< ::registry::XRegistryKey* >( pRegistryKey ) ) ;
+
+    OUString aDelimiter( RTL_CONSTASCII_USTRINGPARAM("/") );
+    OUString aUnoServices( RTL_CONSTASCII_USTRINGPARAM( "/UNO/SERVICES") );
+
+    // Eigentliche Implementierung und ihre Services registrieren
+    sal_Int32 i;
+    Reference< ::registry::XRegistryKey >  xNewKey;
+
+    xNewKey = xKey->createKey( aDelimiter + SdFilterDetect::impl_getStaticImplementationName() +
+                               aUnoServices );
+
+    Sequence< OUString > aServices = SdFilterDetect::impl_getStaticSupportedServiceNames();
+    for(i = 0; i < aServices.getLength(); i++ )
+        xNewKey->createKey( aServices.getConstArray()[i] );
+
+    return sal_True;
+}
+
+void* SAL_CALL component_getFactory( const sal_Char* pImplementationName,
+                                     void* pServiceManager,
+                                     void* pRegistryKey )
+{
+    // Set default return value for this operation - if it failed.
+    void* pReturn = NULL ;
+
+    if  (
+            ( pImplementationName   !=  NULL ) &&
+            ( pServiceManager       !=  NULL )
+        )
+    {
+        // Define variables which are used in following macros.
+        Reference< XSingleServiceFactory >   xFactory                                                                                                ;
+        Reference< XMultiServiceFactory >    xServiceManager( reinterpret_cast< XMultiServiceFactory* >( pServiceManager ) ) ;
+
+        if( SdFilterDetect::impl_getStaticImplementationName().equalsAscii( pImplementationName ) )
+        {
+            xFactory = ::cppu::createSingleFactory( xServiceManager,
+            SdFilterDetect::impl_getStaticImplementationName(),
+            SdFilterDetect::impl_createInstance,
+            SdFilterDetect::impl_getStaticSupportedServiceNames() );
+        }
+
+        // Factory is valid - service was found.
+        if ( xFactory.is() )
+        {
+            xFactory->acquire();
+            pReturn = xFactory.get();
+        }
+    }
+
+    // Return with result of this operation.
+    return pReturn ;
+}
+} // extern "C"
+
+
+
