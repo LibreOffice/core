dff4158c3037fbd0108d85d52b4b6263389bd234
Rüdiger Timm <rt@openoffice.org>
2003-09-19 07:52:41 +0000

    INTEGRATION: CWS fwkq1 (1.6.124); FILE MERGED 2003/07/15 06:25:23 mba 1.6.124.1: #110843#: get rid of factories

--- starmath/source/math_pch.cxx
+++ starmath/source/math_pch.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: math_pch.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2001-08-10 10:07:19 $
+ *  last change: $Author: rt $ $Date: 2003-09-19 08:52:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -159,7 +159,6 @@
 #include <vos/object.hxx>
 #include <vos/macros.hxx>
 #include <smdll.hxx>
-#include <smdll0.hxx>
 #include <sfx2/sfxsids.hrc>
 #include <chaos/cntids.hrc>
 #include <svtools/cntwids.hrc>
