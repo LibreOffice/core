e4348db24966fe395f844d8b4d70cb0ff7384ac0
Rüdiger Timm <rt@openoffice.org>
2003-09-19 07:28:52 +0000

    INTEGRATION: CWS fwkq1 (1.2.104); FILE MERGED 2003/07/14 16:09:48 mba 1.2.104.1: #110843#: get rid of factories

--- basctl/inc/iderdll.hxx
+++ basctl/inc/iderdll.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: iderdll.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tbe $ $Date: 2001-07-25 11:39:43 $
+ *  last change: $Author: rt $ $Date: 2003-09-19 08:28:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -80,8 +80,6 @@ public:
     BasicIDEData*   GetExtraData();
     static void     Init();
     static void     Exit();
-    static void     LibInit();
-    static void     LibExit();
     static BasicIDEDLL* GetDLL();
 };
 
