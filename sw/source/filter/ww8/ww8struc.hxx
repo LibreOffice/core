d51cb430b92615eebf8641f826e35571e396f66a
Rüdiger Timm <rt@openoffice.org>
2003-09-25 06:46:37 +0000

    INTEGRATION: CWS killarneyfilterteam13 (1.24.142); FILE MERGED 2003/08/20 12:50:26 cmc 1.24.142.1: retab everything to 4spaces per tab for once and for all

--- sw/source/filter/ww8/ww8struc.hxx
+++ sw/source/filter/ww8/ww8struc.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ww8struc.hxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-19 12:29:08 $
+ *  last change: $Author: rt $ $Date: 2003-09-25 07:46:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
