a53d3834c1bc609bea60210395352c1b073d34ef
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:00:07 +0000

    INTEGRATION: CWS rt02 (1.3.96); FILE MERGED 2003/10/01 12:33:59 rt 1.3.96.1: #i19697# No newline at end of file

--- desktop/source/so_comp/evaluation.cxx
+++ desktop/source/so_comp/evaluation.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: evaluation.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-22 08:53:39 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:00:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
