4f4e40f420305097fcf20eaf01cd50d7096ca6ad
Vladimir Glazounov <vg@openoffice.org>
2003-10-06 11:55:55 +0000

    INTEGRATION: CWS unopkg (1.4.90); FILE MERGED 2003/09/30 13:37:59 dbo 1.4.90.2: #112449# 2003/09/26 13:01:38 dbo 1.4.90.1: #112449# added ::cppu::getCaughtException()

--- cppuhelper/source/exc_thrower.cxx
+++ cppuhelper/source/exc_thrower.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: exc_thrower.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: jl $ $Date: 2001-03-12 13:39:32 $
+ *  last change: $Author: vg $ $Date: 2003-10-06 12:55:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,234 +59,268 @@
  *
  ************************************************************************/
 
-#ifndef _OSL_DIAGNOSE_H_
-#include <osl/diagnose.h>
-#endif
-#ifndef _OSL_INTERLOCK_H_
-#include <osl/interlck.h>
-#endif
-#ifndef _UNO_DISPATCHER_H_
-#include <uno/dispatcher.h>
-#endif
-#ifndef _UNO_MAPPING_HXX_
-#include <uno/mapping.hxx>
-#endif
+#include "osl/diagnose.h"
+#include "osl/doublecheckedlocking.h"
+#include "osl/mutex.hxx"
+#include "uno/dispatcher.hxx"
+#include "uno/mapping.hxx"
+#include "cppu/XExceptionThrower.hpp"
 
-#include <com/sun/star/uno/Any.hxx>
-#include <com/sun/star/uno/Exception.hpp>
-#include <com/sun/star/uno/RuntimeException.hpp>
-#include <com/sun/star/uno/XInterface.hpp>
-
-using namespace com::sun::star::uno;
-using namespace rtl;
+#define OUSTR(x) ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM(x) )
 
 
-namespace cppu
+using namespace ::rtl;
+using namespace ::osl;
+using namespace ::cppu;
+using namespace ::com::sun::star::uno;
+
+namespace
 {
 
-//************************************** generated, slightly modified for unicode revision
-class XThrower : public ::com::sun::star::uno::XInterface
+//==============================================================================
+struct ExceptionThrower : public uno_Interface, XExceptionThrower
 {
+    inline ExceptionThrower();
+
 public:
+    static ExceptionThrower * get();
+    static inline Type const & getCppuType()
+    {
+        return ::getCppuType(
+            reinterpret_cast< Reference< XExceptionThrower > const * >(0) );
+    }
 
-    // Methods
-    virtual void SAL_CALL throwException( const ::com::sun::star::uno::Any& rExc )
-        SAL_THROW( (::com::sun::star::uno::Exception) ) = 0;
+    // XInterface
+    virtual Any SAL_CALL queryInterface( Type const & type )
+        throw (RuntimeException);
+    virtual void SAL_CALL acquire() throw ();
+    virtual void SAL_CALL release() throw ();
+
+    // XExceptionThrower
+    virtual void SAL_CALL throwException( Any const & exc ) throw (Exception);
+    virtual void SAL_CALL rethrowException() throw (Exception);
 };
 
-static
-const ::com::sun::star::uno::Type& getCppuType( const ::com::sun::star::uno::Reference< ::cppu::XThrower >* )
-    SAL_THROW( () )
-{
-    static ::com::sun::star::uno::Type * pType_cppu_XThrower = 0;
-
-    if ( !pType_cppu_XThrower )
-    {
-    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
-    if ( !pType_cppu_XThrower )
-    {
-        // Start inline typedescription generation
-        typelib_InterfaceTypeDescription * pTD = 0;
-
-        const Type & rSuperType = ::getCppuType( ( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > *)0 );
-        typelib_TypeDescriptionReference * pMembers[1] = { 0 };
-        OUString aMemberTypeName( RTL_CONSTASCII_USTRINGPARAM("cppu.XThrower::throwException") );
-        typelib_typedescriptionreference_new( &pMembers[0],
-                                              (typelib_TypeClass)::com::sun::star::uno::TypeClass_INTERFACE_METHOD,
-                                              aMemberTypeName.pData );
-
-        OUString aTypeName( RTL_CONSTASCII_USTRINGPARAM("cppu.XThrower") );
-        typelib_typedescription_newInterface(
-            &pTD,
-            aTypeName.pData, 0x3313e220, 0xb491, 0xb491, 0x9ede0050, 0x04d7677e,
-            rSuperType.getTypeLibType(),
-            1,
-            pMembers );
-
-        typelib_typedescription_register( (typelib_TypeDescription**)&pTD );
-        typelib_typedescriptionreference_release( pMembers[0] );
-        typelib_typedescription_release( (typelib_TypeDescription*)pTD );
-
-        static ::com::sun::star::uno::Type aType_cppu_XThrower(
-            ::com::sun::star::uno::TypeClass_INTERFACE, aTypeName.pData );
-        ::getCppuType( (const ::com::sun::star::uno::Exception*)0 );
-
-        typelib_InterfaceMethodTypeDescription * pMethod = 0;
-        {
-            typelib_Parameter_Init aParameters[1];
-            OUString aParamName( RTL_CONSTASCII_USTRINGPARAM("rExc") );
-            aParameters[0].pParamName = aParamName.pData;
-            aParameters[0].eTypeClass = (typelib_TypeClass)::com::sun::star::uno::TypeClass_ANY;
-            OUString aParamTypeName( RTL_CONSTASCII_USTRINGPARAM("any") );
-            aParameters[0].pTypeName = aParamTypeName.pData;
-            aParameters[0].bIn = sal_True;
-            aParameters[0].bOut = sal_False;
-            OUString aExceptionName( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.uno.Exception") );
-            OUString aVoidName( RTL_CONSTASCII_USTRINGPARAM("void") );
-            typelib_typedescription_newInterfaceMethod( &pMethod,
-                3, sal_False,
-                aMemberTypeName.pData,
-                (typelib_TypeClass)::com::sun::star::uno::TypeClass_VOID, aVoidName.pData,
-                1, aParameters,
-                1, &aExceptionName.pData );
-            typelib_typedescription_register( (typelib_TypeDescription**)&pMethod );
-        }
-        typelib_typedescription_release( (typelib_TypeDescription*)pMethod );
-        // End inline typedescription generation
-        pType_cppu_XThrower = &aType_cppu_XThrower;
-    }
-    }
-    return *pType_cppu_XThrower;
-}
-//**************************************
-
-
-//==================================================================================================
-struct ExceptionThrower : public uno_Interface
-{
-    oslInterlockedCount         nRef;
-
-    inline ExceptionThrower() SAL_THROW( () );
-};
 extern "C"
 {
-//--------------------------------------------------------------------------------------------------
-static void SAL_CALL ExceptionThrower_acquire( uno_Interface * pUnoI ) SAL_THROW( () )
+
+//------------------------------------------------------------------------------
+static void SAL_CALL ExceptionThrower_acquire_release_nop( uno_Interface * )
 {
-    osl_incrementInterlockedCount( & SAL_STATIC_CAST( ExceptionThrower *, pUnoI )->nRef );
 }
-//--------------------------------------------------------------------------------------------------
-static void SAL_CALL ExceptionThrower_release( uno_Interface * pUnoI ) SAL_THROW( () )
-{
-    if (! osl_decrementInterlockedCount( & SAL_STATIC_CAST( ExceptionThrower *, pUnoI )->nRef ))
-        delete SAL_STATIC_CAST( ExceptionThrower *, pUnoI );
-}
-//--------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
 static void SAL_CALL ExceptionThrower_dispatch(
-    uno_Interface * pUnoI, const typelib_TypeDescription * pMemberType,
-    void * pReturn, void * pArgs[], uno_Any ** ppException )
-    SAL_THROW( () )
+    uno_Interface * pUnoI, typelib_TypeDescription const * pMemberType,
+    void * pReturn, void * pArgs [], uno_Any ** ppException )
 {
     OSL_ASSERT( pMemberType->eTypeClass == typelib_TypeClass_INTERFACE_METHOD );
 
-    switch (((typelib_InterfaceMemberTypeDescription *)pMemberType)->nPosition)
+    switch (reinterpret_cast< typelib_InterfaceMemberTypeDescription * >(
+                const_cast< typelib_TypeDescription * >( pMemberType ) )->
+            nPosition)
     {
     case 0: // queryInterace()
     {
-        if (::getCppuType( (const Reference< XInterface > *)0 ).equals( *(const Type *)pArgs[0] ))
+        Type const & rType_demanded =
+            *reinterpret_cast< Type const * >( pArgs[ 0 ] );
+        if (rType_demanded.equals(
+                ::getCppuType( reinterpret_cast<
+                               Reference< XInterface > const * >(0) ) ) ||
+            rType_demanded.equals( ExceptionThrower::getCppuType() ))
         {
-            const Type & rType = ::getCppuType( (const Reference< XInterface > *)0 );
             typelib_TypeDescription * pTD = 0;
-            TYPELIB_DANGER_GET( &pTD, rType.getTypeLibType() );
-            uno_any_construct( (uno_Any *)pReturn, &pUnoI, pTD, 0 );
-            TYPELIB_DANGER_RELEASE( pTD );
-        }
-        else if (getCppuType( (const Reference< XThrower > *)0 ).equals( *(const Type *)pArgs[0] ))
-        {
-            const Type & rType = getCppuType( (const Reference< XThrower > *)0 );
-            typelib_TypeDescription * pTD = 0;
-            TYPELIB_DANGER_GET( &pTD, rType.getTypeLibType() );
-            uno_any_construct( (uno_Any *)pReturn, &pUnoI, pTD, 0 );
+            TYPELIB_DANGER_GET( &pTD, rType_demanded.getTypeLibType() );
+            uno_any_construct(
+                reinterpret_cast< uno_Any * >( pReturn ), &pUnoI, pTD, 0 );
             TYPELIB_DANGER_RELEASE( pTD );
         }
         else
         {
-            uno_any_construct( (uno_Any *)pReturn, 0, 0, 0 );
+            uno_any_construct(
+                reinterpret_cast< uno_Any * >( pReturn ), 0, 0, 0 );
         }
         *ppException = 0;
         break;
     }
     case 1: // acquire()
-        ExceptionThrower_acquire( pUnoI );
-        *ppException = 0;
-        break;
     case 2: // release()
-        ExceptionThrower_release( pUnoI );
         *ppException = 0;
         break;
     case 3: // throwException()
     {
-        OSL_ASSERT( ((uno_Any *)pArgs[0])->pType->eTypeClass == typelib_TypeClass_EXCEPTION );
-
-        uno_type_any_construct(
-            *ppException, ((uno_Any *)pArgs[0])->pData, ((uno_Any *)pArgs[0])->pType, 0 );
+        uno_Any * pAny = reinterpret_cast< uno_Any * >( pArgs[ 0 ] );
+        OSL_ASSERT( pAny->pType->eTypeClass == typelib_TypeClass_EXCEPTION );
+        uno_type_any_construct( *ppException, pAny->pData, pAny->pType, 0 );
         break;
     }
     default:
-        OSL_ENSURE( sal_False, "### illegal member called!" );
+    {
+        OSL_ASSERT( 0 );
+        RuntimeException exc(
+            OUSTR("not implemented!"), Reference< XInterface >() );
+        uno_type_any_construct(
+            *ppException, &exc, ::getCppuType( &exc ).getTypeLibType(), 0 );
+        break;
+    }
     }
 }
+
 } // extern "C"
-//__________________________________________________________________________________________________
-inline ExceptionThrower::ExceptionThrower() SAL_THROW( () )
-    : nRef( 0 )
+
+//______________________________________________________________________________
+Any ExceptionThrower::queryInterface( Type const & type )
+    throw (RuntimeException)
 {
-    uno_Interface::acquire = ExceptionThrower_acquire;
-    uno_Interface::release = ExceptionThrower_release;
+    if (type.equals( ::getCppuType( reinterpret_cast<
+                                    Reference< XInterface > const * >(0) ) ) ||
+        type.equals( ExceptionThrower::getCppuType() ))
+    {
+        XExceptionThrower * that = static_cast< XExceptionThrower * >( this );
+        return Any( &that, type );
+    }
+    return Any();
+}
+
+//______________________________________________________________________________
+void ExceptionThrower::acquire() throw ()
+{
+}
+//______________________________________________________________________________
+void ExceptionThrower::release() throw ()
+{
+}
+
+//______________________________________________________________________________
+void ExceptionThrower::throwException( Any const & exc ) throw (Exception)
+{
+    OSL_ENSURE( 0, "unexpected!" );
+    throwException( exc );
+}
+
+//______________________________________________________________________________
+void ExceptionThrower::rethrowException() throw (Exception)
+{
+    throw;
+}
+
+//______________________________________________________________________________
+inline ExceptionThrower::ExceptionThrower()
+{
+    uno_Interface::acquire = ExceptionThrower_acquire_release_nop;
+    uno_Interface::release = ExceptionThrower_acquire_release_nop;
     uno_Interface::pDispatcher = ExceptionThrower_dispatch;
 }
 
-//==================================================================================================
-void SAL_CALL throwException( const Any & rExc ) SAL_THROW( (Exception) )
+//______________________________________________________________________________
+ExceptionThrower * ExceptionThrower::get()
 {
-    if (rExc.getValueTypeClass() == TypeClass_EXCEPTION)
+    ExceptionThrower * s_pThrower = 0;
+    if (s_pThrower == 0)
     {
-        OUString aExceptionName( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.uno.Exception") );
-        typelib_TypeDescription * pComp = 0;
-        TYPELIB_DANGER_GET( &pComp, rExc.getValueTypeRef() );
-        // must be derived from com.sun.star.uno.Exception
-        for ( typelib_CompoundTypeDescription * pTD = (typelib_CompoundTypeDescription *)pComp;
-              pTD; pTD = pTD->pBaseTypeDescription )
-        {
-            if (aExceptionName == rtl::OUString(((typelib_TypeDescription *)pTD)->pTypeName))
-            {
-                Mapping aUno2Cpp( OUString( RTL_CONSTASCII_USTRINGPARAM(UNO_LB_UNO) ),
-                                  OUString( RTL_CONSTASCII_USTRINGPARAM(CPPU_CURRENT_LANGUAGE_BINDING_NAME) ) );
-                OSL_ENSURE( aUno2Cpp.is(), "### cannot get uno to cpp mapping!" );
-                if (aUno2Cpp.is())
-                {
-                    uno_Interface * pUnoI = new ExceptionThrower();
-                    (*pUnoI->acquire)( pUnoI );
-                    Reference< XThrower > xThrower;
-                    aUno2Cpp.mapInterface(
-                        reinterpret_cast< void ** >( &xThrower ),
-                        pUnoI, ::cppu::getCppuType( (const Reference< XThrower > *)0 ) );
-                    (*pUnoI->release)( pUnoI );
-
-                    if (xThrower.is())
-                    {
-                        TYPELIB_DANGER_RELEASE( pComp );
-                        xThrower->throwException( rExc );
-                    }
-                }
-                break;
-            }
-        }
-        TYPELIB_DANGER_RELEASE( pComp );
+        MutexGuard guard( Mutex::getGlobalMutex() );
+        static ExceptionThrower s_thrower;
+        OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
+        s_pThrower = &s_thrower;
     }
-    throw RuntimeException(
-        OUString( RTL_CONSTASCII_USTRINGPARAM("failed throwing exception generically!") ),
-        Reference< XInterface >() );
+    else
+    {
+        OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
+    }
+    return s_pThrower;
+}
+
+} // anonymous namespace
+
+
+namespace cppu
+{
+
+//==============================================================================
+void SAL_CALL throwException( Any const & exc ) SAL_THROW( (Exception) )
+{
+    if (exc.getValueTypeClass() != TypeClass_EXCEPTION)
+    {
+        throw RuntimeException(
+            OUSTR("no UNO exception given "
+                  "(must be derived from com::sun::star::uno::Exception)!"),
+            Reference< XInterface >() );
+    }
+
+    Mapping uno2cpp(
+        OUSTR(UNO_LB_UNO), OUSTR(CPPU_CURRENT_LANGUAGE_BINDING_NAME) );
+    if (! uno2cpp.is())
+    {
+        throw RuntimeException(
+            OUSTR("cannot get binary UNO to C++ mapping!"),
+            Reference< XInterface >() );
+    }
+
+    Reference< XExceptionThrower > xThrower;
+    uno2cpp.mapInterface(
+        reinterpret_cast< void ** >( &xThrower ),
+        static_cast< uno_Interface * >( ExceptionThrower::get() ),
+        ExceptionThrower::getCppuType() );
+    OSL_ASSERT( xThrower.is() );
+    xThrower->throwException( exc );
+}
+
+//==============================================================================
+Any SAL_CALL getCaughtException()
+{
+    Mapping cpp2uno(
+        OUSTR(CPPU_CURRENT_LANGUAGE_BINDING_NAME), OUSTR(UNO_LB_UNO) );
+    if (! cpp2uno.is())
+    {
+        throw RuntimeException(
+            OUSTR("cannot get C++ to binary UNO mapping!"),
+            Reference< XInterface >() );
+    }
+    Mapping uno2cpp(
+        OUSTR(UNO_LB_UNO), OUSTR(CPPU_CURRENT_LANGUAGE_BINDING_NAME) );
+    if (! uno2cpp.is())
+    {
+        throw RuntimeException(
+            OUSTR("cannot get binary UNO to C++ mapping!"),
+            Reference< XInterface >() );
+    }
+
+    typelib_TypeDescription * pTD = 0;
+    TYPELIB_DANGER_GET(
+        &pTD, ExceptionThrower::getCppuType().getTypeLibType() );
+
+    UnoInterfaceReference unoI;
+    cpp2uno.mapInterface(
+        reinterpret_cast< void ** >( &unoI.m_pUnoI ),
+        static_cast< XExceptionThrower * >( ExceptionThrower::get() ), pTD );
+    OSL_ASSERT( unoI.is() );
+
+    typelib_TypeDescription * pMemberTD = 0;
+    TYPELIB_DANGER_GET(
+        &pMemberTD,
+        reinterpret_cast< typelib_InterfaceTypeDescription * >( pTD )->
+        ppMembers[ 1 ] /* rethrowException() */ );
+
+    uno_Any exc_mem;
+    uno_Any * exc = &exc_mem;
+    unoI.dispatch( pMemberTD, 0, 0, &exc );
+
+    TYPELIB_DANGER_RELEASE( pMemberTD );
+    TYPELIB_DANGER_RELEASE( pTD );
+
+    if (exc == 0)
+    {
+        throw RuntimeException(
+            OUSTR("rethrowing C++ exception failed!"),
+            Reference< XInterface >() );
+    }
+
+    Any ret;
+    uno_any_destruct( &ret, cpp_release );
+    uno_type_any_constructAndConvert(
+        &ret, exc->pData, exc->pType, uno2cpp.get() );
+    uno_any_destruct( exc, 0 );
+    return ret;
 }
 
 }
