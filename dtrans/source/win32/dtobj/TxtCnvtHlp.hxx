5b88cb98fbcf3b8e4829d7e322f86f98fc06fb57
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:41:04 +0000

    INTEGRATION: CWS rt02 (1.2.100); FILE MERGED 2003/10/01 12:06:18 rt 1.2.100.1: #i19697# No newline at end of file

--- dtrans/source/win32/dtobj/TxtCnvtHlp.hxx
+++ dtrans/source/win32/dtobj/TxtCnvtHlp.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TxtCnvtHlp.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tra $ $Date: 2001-03-20 09:26:01 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:41:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
