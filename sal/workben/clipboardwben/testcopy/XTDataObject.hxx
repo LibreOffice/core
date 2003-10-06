2ab34c34f5dc4540a1eb868233d38be7f7b8fb84
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:22:31 +0000

    INTEGRATION: CWS rt02 (1.2.188); FILE MERGED 2003/10/01 09:15:54 rt 1.2.188.1: #i19697# No newline at end of file

--- sal/workben/clipboardwben/testcopy/XTDataObject.hxx
+++ sal/workben/clipboardwben/testcopy/XTDataObject.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XTDataObject.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tra $ $Date: 2000-11-13 08:34:09 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:22:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
