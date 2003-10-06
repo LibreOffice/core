1b0a7efb3350356694efdc885d1b79e345a2826d
Vladimir Glazounov <vg@openoffice.org>
2003-10-06 11:36:54 +0000

    INTEGRATION: CWS qadev12 (1.1.2); FILE ADDED 2003/09/24 16:08:38 sg 1.1.2.3: #110481#CHG: implemented change to use database out producer 2003/09/15 09:53:58 sg 1.1.2.2: CHG: removed unused variable 'debug' 2003/09/12 15:56:26 sg 1.1.2.1: #112239# CHG: enable Runner to execute Java component tests

---
+++ qadevOOo/runner/base/java_cmp.java
@@ -0,0 +1,146 @@
+/*************************************************************************
+ *
+ *  $RCSfile: java_cmp.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Date: 2003-10-06 12:36:54 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package base;
+
+
+import java.io.PrintWriter;
+
+import stats.OutProducerFactory;
+import lib.TestParameters;
+import lib.TestCase;
+import lib.Status;
+import lib.MultiMethodTest;
+import lib.TestEnvironment;
+import util.DynamicClassLoader;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.connection.XConnector;
+import com.sun.star.connection.XConnection;
+
+import com.sun.star.bridge.XUnoUrlResolver;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.uno.XNamingService;
+import com.sun.star.uno.XComponentContext;
+
+import share.DescEntry;
+import share.DescGetter;
+import helper.APIDescGetter;
+import helper.OfficeProvider;
+import helper.AppProvider;
+import base.TestBase;
+
+import share.LogWriter;
+import stats.Summarizer;
+
+/**
+ *
+ * this class handles tests written in java without running an Office with
+ * own bootstrapping of UNO
+ */
+public class java_cmp implements TestBase {
+
+    private TestBase mWrappedTestBase = new java_fat();
+
+    public boolean executeTest(lib.TestParameters param) {
+        param.put("OfficeProvider", "helper.UnoProvider");
+        return mWrappedTestBase.executeTest(param);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
