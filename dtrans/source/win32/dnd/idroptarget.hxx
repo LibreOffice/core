12843767cbeaa748cac3041be04013c0e951c8f8
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:38:20 +0000

    INTEGRATION: CWS rt02 (1.1.102); FILE MERGED 2003/10/01 12:06:08 rt 1.1.102.1: #i19697# No newline at end of file

--- dtrans/source/win32/dnd/idroptarget.hxx
+++ dtrans/source/win32/dnd/idroptarget.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: idroptarget.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: jl $ $Date: 2001-02-26 16:00:59 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:38:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
