6b38430c08d69ca6ef2cd608460765cf0839574a
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:38:06 +0000

    INTEGRATION: CWS rt02 (1.7.94); FILE MERGED 2003/10/01 12:06:05 rt 1.7.94.1: #i19697# No newline at end of file

--- dtrans/source/win32/clipb/WinClipboard.hxx
+++ dtrans/source/win32/clipb/WinClipboard.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: WinClipboard.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: tra $ $Date: 2001-07-24 07:53:23 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:38:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
