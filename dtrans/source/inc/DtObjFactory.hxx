792ec317a097d6d4d43c3951f7bab62071b87c39
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:36:30 +0000

    INTEGRATION: CWS rt02 (1.1.102); FILE MERGED 2003/10/01 12:05:57 rt 1.1.102.1: #i19697# No newline at end of file

--- dtrans/source/inc/DtObjFactory.hxx
+++ dtrans/source/inc/DtObjFactory.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DtObjFactory.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: tra $ $Date: 2001-03-02 11:51:44 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:36:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
