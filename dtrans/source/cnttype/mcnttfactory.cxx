d2b68e2fcbf5533cb391bb5d173d58547de8bc88
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:35:57 +0000

    INTEGRATION: CWS rt02 (1.2.102); FILE MERGED 2003/10/01 12:05:55 rt 1.2.102.1: #i19697# No newline at end of file

--- dtrans/source/cnttype/mcnttfactory.cxx
+++ dtrans/source/cnttype/mcnttfactory.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mcnttfactory.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tra $ $Date: 2001-02-13 13:04:11 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:35:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
