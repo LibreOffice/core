914a047aef98c523cf3a992c91e2410708b3a355
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:42:03 +0000

    INTEGRATION: CWS rt02 (1.4.98); FILE MERGED 2003/10/01 12:06:25 rt 1.4.98.1: #i19697# No newline at end of file

--- dtrans/source/win32/misc/ImplHelper.hxx
+++ dtrans/source/win32/misc/ImplHelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ImplHelper.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: tra $ $Date: 2001-05-10 09:43:48 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:42:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
