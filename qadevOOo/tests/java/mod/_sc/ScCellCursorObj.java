f2d2572abb14992a703a0c8c7b85af550c1c7120
Vladimir Glazounov <vg@openoffice.org>
2003-10-06 12:33:19 +0000

    INTEGRATION: CWS qadev12 (1.4.2); FILE MERGED 2003/09/30 09:29:16 sg 1.4.2.2: CHG: old version again 2003/09/22 15:39:04 sg 1.4.2.1: CHG: added support for the XCellRangesQuery interface

--- qadevOOo/tests/java/mod/_sc/ScCellCursorObj.java
+++ qadevOOo/tests/java/mod/_sc/ScCellCursorObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScCellCursorObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:04:54 $
+ *  last change:$Date: 2003-10-06 13:33:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
