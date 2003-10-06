9ecba606834ddc66e203ffa3f0527f77c236a576
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:53:09 +0000

    INTEGRATION: CWS rt02 (1.10.42); FILE MERGED 2003/10/01 10:41:53 rt 1.10.42.1: #i19697# No newline at end of file

--- fpicker/source/win32/filepicker/asynceventnotifier.cxx
+++ fpicker/source/win32/filepicker/asynceventnotifier.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: asynceventnotifier.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: tra $ $Date: 2002-11-26 09:21:59 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:53:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
