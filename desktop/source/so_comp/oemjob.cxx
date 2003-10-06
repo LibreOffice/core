e965824d87c2fdcc0f08466bf1762f897a9199c1
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:00:19 +0000

    INTEGRATION: CWS rt02 (1.2.96); FILE MERGED 2003/10/01 12:34:00 rt 1.2.96.1: #i19697# No newline at end of file

--- desktop/source/so_comp/oemjob.cxx
+++ desktop/source/so_comp/oemjob.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: oemjob.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-22 08:54:36 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:00:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
