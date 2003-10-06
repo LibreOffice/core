ad628fe7697cb6fa090abe8a397d84603bc555a4
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:52:13 +0000

    INTEGRATION: CWS rt02 (1.19.24); FILE MERGED 2003/10/01 10:41:52 rt 1.19.24.1: #i19697# No newline at end of file

--- fpicker/source/win32/filepicker/FilePicker.cxx
+++ fpicker/source/win32/filepicker/FilePicker.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: FilePicker.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-25 18:04:56 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:52:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
