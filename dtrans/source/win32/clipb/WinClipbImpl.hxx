206eb6a8ac4bd21ded02bf1eb23b6c7880ffeb80
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:37:37 +0000

    INTEGRATION: CWS rt02 (1.7.88); FILE MERGED 2003/10/01 12:06:03 rt 1.7.88.1: #i19697# No newline at end of file

--- dtrans/source/win32/clipb/WinClipbImpl.hxx
+++ dtrans/source/win32/clipb/WinClipbImpl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: WinClipbImpl.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: tra $ $Date: 2001-09-28 12:23:22 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:37:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
