db484ad904cd18ab2be807edb6c74719cd44a4b0
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:57:29 +0000

    INTEGRATION: CWS rt02 (1.1.52); FILE MERGED 2003/10/01 10:42:00 rt 1.1.52.1: #i19697# No newline at end of file

--- fpicker/source/win32/filepicker/previewbase.hxx
+++ fpicker/source/win32/filepicker/previewbase.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: previewbase.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: tra $ $Date: 2002-03-21 07:12:10 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:57:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
