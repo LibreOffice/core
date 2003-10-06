da45eaf77a74bb1865c24557a78ce804f9f711fc
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:46:15 +0000

    INTEGRATION: CWS rt02 (1.3.20); FILE MERGED 2003/10/01 13:15:18 rt 1.3.20.1: #i19697# Fixed compiler warnings

--- configmgr/source/backend/binarywriter.cxx
+++ configmgr/source/backend/binarywriter.cxx
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: binarywriter.cxx,v $
 *
-*  $Revision: 1.3 $
+*  $Revision: 1.4 $
 *
-*  last change: $Author: vg $ $Date: 2003-06-04 10:19:14 $
+*  last change: $Author: rt $ $Date: 2003-10-06 14:46:15 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
