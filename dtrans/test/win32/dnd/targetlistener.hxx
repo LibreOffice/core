7557cb86db3d01f930d55912191f77013e139d68
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:43:21 +0000

    INTEGRATION: CWS rt02 (1.2.102); FILE MERGED 2003/10/01 12:06:30 rt 1.2.102.1: #i19697# No newline at end of file

--- dtrans/test/win32/dnd/targetlistener.hxx
+++ dtrans/test/win32/dnd/targetlistener.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: targetlistener.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: jl $ $Date: 2001-02-12 13:14:37 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:43:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
