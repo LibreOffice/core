487761cf7da3ead0332aa7fd28244c14e25b9fc4
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:40:48 +0000

    INTEGRATION: CWS rt02 (1.2.100); FILE MERGED 2003/10/01 12:06:17 rt 1.2.100.1: #i19697# No newline at end of file

--- dtrans/source/win32/dtobj/MimeAttrib.hxx
+++ dtrans/source/win32/dtobj/MimeAttrib.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MimeAttrib.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tra $ $Date: 2001-03-16 09:00:32 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:40:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
