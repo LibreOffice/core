67c601650d59d29ca21f33d22fe233f5b0c1e9e4
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:37:01 +0000

    INTEGRATION: CWS rt02 (1.3.42); FILE MERGED 2003/10/01 12:06:00 rt 1.3.42.1: #i19697# No newline at end of file

--- dtrans/source/win32/clipb/APNDataObject.hxx
+++ dtrans/source/win32/clipb/APNDataObject.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: APNDataObject.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-25 14:05:33 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:37:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
