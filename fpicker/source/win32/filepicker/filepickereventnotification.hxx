06aaaf530ce0d9480bb82af228209a9a86c33323
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:56:10 +0000

    INTEGRATION: CWS rt02 (1.1.52); FILE MERGED 2003/10/01 10:41:58 rt 1.1.52.1: #i19697# No newline at end of file

--- fpicker/source/win32/filepicker/filepickereventnotification.hxx
+++ fpicker/source/win32/filepicker/filepickereventnotification.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: filepickereventnotification.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: tra $ $Date: 2002-03-21 07:10:09 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:56:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
