4025c354e4ffdeffa3332354f8037bbbe4f12b76
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:30:45 +0000

    INTEGRATION: CWS rt02 (1.2.262); FILE MERGED 2003/09/30 15:44:03 rt 1.2.262.1: #i19697# Fixed comment after preprocessor directive

--- sw/inc/TextCursorHelper.hxx
+++ sw/inc/TextCursorHelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TextCursorHelper.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:24:04 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:30:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -87,6 +87,6 @@ public:
     virtual SwDoc*              GetDoc()        = 0;
 };
 
-#endif _SW_TEXTCURSORHELPER_HXX
+#endif // _SW_TEXTCURSORHELPER_HXX
 
 
