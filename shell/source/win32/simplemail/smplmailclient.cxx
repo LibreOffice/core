52b10db8cdb8e3d8b9ce02b4c1f0fc169967b469
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:44:41 +0000

    INTEGRATION: CWS rt02 (1.8.52); FILE MERGED 2003/10/01 07:48:27 rt 1.8.52.1: #i19697# No newline at end of file

--- shell/source/win32/simplemail/smplmailclient.cxx
+++ shell/source/win32/simplemail/smplmailclient.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: smplmailclient.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 11:16:20 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:44:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
