a367d14241d71261023ec15b8a3310037b5fc4a6
Rüdiger Timm <rt@openoffice.org>
2003-10-06 13:17:53 +0000

    INTEGRATION: CWS rt02 (1.2.96); FILE MERGED 2003/10/01 10:52:38 rt 1.2.96.1: #i19697# No newline at end of file

--- filter/source/xsltdialog/xmlfiltercommon.hxx
+++ filter/source/xsltdialog/xmlfiltercommon.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlfiltercommon.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 16:33:01 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 14:17:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
