8eed6c04d0c1ac00b316cde2370a7d65fb01d150
Rüdiger Timm <rt@openoffice.org>
2003-10-06 14:54:53 +0000

    INTEGRATION: CWS rt02 (1.2.24); FILE MERGED 2003/10/01 10:41:56 rt 1.2.24.1: #i19697# No newline at end of file

--- fpicker/source/win32/filepicker/customcontrolfactory.cxx
+++ fpicker/source/win32/filepicker/customcontrolfactory.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: customcontrolfactory.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-25 18:04:58 $
+ *  last change: $Author: rt $ $Date: 2003-10-06 15:54:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
