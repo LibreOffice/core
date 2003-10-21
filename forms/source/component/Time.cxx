7182bb67bb6f2ddae393e45536d1fb874539beb1
Oliver Bolte <obo@openoffice.org>
2003-10-21 08:00:30 +0000

    INTEGRATION: CWS formcelllinkage (1.12.38); FILE MERGED 2003/10/01 09:18:35 fs 1.12.38.1: #i18994# merging the changes from the CWS fs002

--- forms/source/component/Time.cxx
+++ forms/source/component/Time.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Time.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-19 13:10:17 $
+ *  last change: $Author: obo $ $Date: 2003-10-21 09:00:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -137,8 +137,6 @@ StringSequence SAL_CALL OTimeControl::getSupportedServiceNames() throw()
 //==================================================================
 //= OTimeModel
 //==================================================================
-sal_Int32   OTimeModel::nTimeHandle = -1;
-
 //------------------------------------------------------------------
 InterfaceRef SAL_CALL OTimeModel_CreateInstance(const Reference<XMultiServiceFactory>& _rxFactory)
 {
@@ -168,16 +166,14 @@ Sequence<Type> OTimeModel::_getTypes()
 DBG_NAME( OTimeModel )
 //------------------------------------------------------------------
 OTimeModel::OTimeModel(const Reference<XMultiServiceFactory>& _rxFactory)
-            :OEditBaseModel(_rxFactory, VCL_CONTROLMODEL_TIMEFIELD, FRM_CONTROL_TIMEFIELD )
+            :OEditBaseModel( _rxFactory, VCL_CONTROLMODEL_TIMEFIELD, FRM_CONTROL_TIMEFIELD, sal_False )
                                     // use the old control name for compytibility reasons
             ,OLimitedFormats(_rxFactory, FormComponentType::TIMEFIELD)
 {
     DBG_CTOR( OTimeModel, NULL );
 
     m_nClassId = FormComponentType::TIMEFIELD;
-    m_sDataFieldConnectivityProperty = PROPERTY_TIME;
-    if (OTimeModel::nTimeHandle == -1)
-        OTimeModel::nTimeHandle = getOriginalHandle(PROPERTY_ID_TIME);
+    initValueProperty( PROPERTY_TIME, PROPERTY_ID_TIME );
 
     setAggregateSet(m_xAggregateFastSet, getOriginalHandle(PROPERTY_ID_TIMEFORMAT));
 }
@@ -281,9 +277,9 @@ void SAL_CALL OTimeModel::setFastPropertyValue_NoBroadcast(sal_Int32 _nHandle, c
 
 // XLoadListener
 //------------------------------------------------------------------------------
-void OTimeModel::_loaded(const EventObject& rEvent)
+void OTimeModel::onConnectedDbColumn( const Reference< XInterface >& _rxForm )
 {
-    OBoundControlModel::_loaded(rEvent);
+    OBoundControlModel::onConnectedDbColumn( _rxForm );
     Reference<XPropertySet> xField = getField();
     if (xField.is())
     {
@@ -300,24 +296,23 @@ void OTimeModel::_loaded(const EventObject& rEvent)
     }
 }
 
-// XBoundComponent
 //------------------------------------------------------------------------------
-sal_Bool OTimeModel::_commit()
+sal_Bool OTimeModel::commitControlValueToDbColumn( bool _bPostReset )
 {
-    Any aNewValue = m_xAggregateFastSet->getFastPropertyValue( OTimeModel::nTimeHandle );
-    if (!compare(aNewValue, m_aSaveValue))
+    Any aControlValue( m_xAggregateFastSet->getFastPropertyValue( getValuePropertyAggHandle() ) );
+    if ( !compare( aControlValue, m_aSaveValue ) )
     {
-        if (!aNewValue.hasValue())
+        if ( !aControlValue.hasValue() )
             m_xColumnUpdate->updateNull();
         else
         {
             try
             {
                 starutil::Time aTime;
-                if (!(aNewValue >>= aTime))
+                if ( !( aControlValue >>= aTime ) )
                 {
                     sal_Int32 nAsInt(0);
-                    aNewValue >>= nAsInt;
+                    aControlValue >>= nAsInt;
                     aTime = DBTypeConversion::toTime(nAsInt);
                 }
 
@@ -338,32 +333,26 @@ sal_Bool OTimeModel::_commit()
                 return sal_False;
             }
         }
-        m_aSaveValue = aNewValue;
+        m_aSaveValue = aControlValue;
     }
     return sal_True;
 }
 
 //------------------------------------------------------------------------------
-void OTimeModel::_onValueChanged()
+Any OTimeModel::translateDbColumnToControlValue()
 {
     starutil::Time aTime = m_xColumn->getTime();
-    if (m_xColumn->wasNull())
+    if ( m_xColumn->wasNull() )
         m_aSaveValue.clear();
     else
         // the aggregated set expects an Int32 as value ...
-        m_aSaveValue <<= DBTypeConversion::toINT32(aTime);
+        m_aSaveValue <<= DBTypeConversion::toINT32( aTime );
 
-    {   // release our mutex once (it's acquired in the calling method !), as setting aggregate properties
-        // may cause any uno controls belonging to us to lock the solar mutex, which is potentially dangerous with
-        // our own mutex locked
-        // FS - 72451 - 31.01.00
-        MutexRelease aRelease(m_aMutex);
-        m_xAggregateFastSet->setFastPropertyValue(OTimeModel::nTimeHandle, m_aSaveValue);
-    }
+    return m_aSaveValue;
 }
 
 //------------------------------------------------------------------------------
-void OTimeModel::_reset()
+Any OTimeModel::getDefaultForReset() const
 {
     Any aValue;
     if  (m_aDefault.getValueType().getTypeClass() == TypeClass_LONG)
@@ -374,13 +363,7 @@ void OTimeModel::_reset()
         aValue <<= (sal_Int32)aCurrentTime.GetTime();
     }
 
-    {   // release our mutex once (it's acquired in the calling method !), as setting aggregate properties
-        // may cause any uno controls belonging to us to lock the solar mutex, which is potentially dangerous with
-        // our own mutex locked
-        // FS - 72451 - 31.01.00
-        MutexRelease aRelease(m_aMutex);
-        m_xAggregateFastSet->setFastPropertyValue(OTimeModel::nTimeHandle, aValue);
-    }
+    return aValue;
 }
 
 //.........................................................................
