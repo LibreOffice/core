b5288be9e8f7aed26c978791be3a96f7ad51e4a8
Rüdiger Timm <rt@openoffice.org>
2003-10-27 12:25:53 +0000

    INTEGRATION: CWS aw005 (1.11.286); FILE MERGED 2003/10/08 16:58:21 aw 1.11.286.1: #112587#

--- svx/source/engine3d/extrud3d.cxx
+++ svx/source/engine3d/extrud3d.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: extrud3d.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 15:02:11 $
+ *  last change: $Author: rt $ $Date: 2003-10-27 13:25:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -630,7 +630,7 @@ void E3dExtrudeObj::ReadData(const SdrObjIOHeader& rHead, SvStream& rIn)
             double fTransDepth(rFirstPoint.Z());
             Matrix4D aTransMat;
             aTransMat.TranslateZ(fTransDepth);
-            NbcSetTransform(aTransMat * GetTransform());
+            NbcSetTransform(GetTransform() * aTransMat); // #112587#
 
             // correct polygon itself
             aTransMat.Identity();
