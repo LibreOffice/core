1e28f61c6bd498fc6710a15256b48031e13be592
Rüdiger Timm <rt@openoffice.org>
2003-10-30 08:47:21 +0000

    INTEGRATION: CWS mav08 (1.1.2); FILE ADDED 2003/10/28 07:46:58 mav 1.1.2.1: #i21121# unit test for storages

---
+++ package/qa/storages/Test07.java
@@ -0,0 +1,138 @@
+package complex.storages;
+
+import com.sun.star.uno.XInterface;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.lang.XSingleServiceFactory;
+
+import com.sun.star.bridge.XUnoUrlResolver;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+
+import com.sun.star.embed.*;
+
+import share.LogWriter;
+import complex.storages.TestHelper;
+import complex.storages.StorageTest;
+
+public class Test07 implements StorageTest {
+
+    XMultiServiceFactory m_xMSF;
+    XSingleServiceFactory m_xStorageFactory;
+    TestHelper m_aTestHelper;
+
+    public Test07( XMultiServiceFactory xMSF, XSingleServiceFactory xStorageFactory, LogWriter aLogWriter )
+    {
+        m_xMSF = xMSF;
+        m_xStorageFactory = xStorageFactory;
+        m_aTestHelper = new TestHelper( aLogWriter, "Test07: " );
+    }
+
+    public boolean test()
+    {
+        try
+        {
+            String sTempFileURL = m_aTestHelper.CreateTempFile( m_xMSF );
+            if ( sTempFileURL == null || sTempFileURL == "" )
+            {
+                m_aTestHelper.Error( "No valid temporary file was created!" );
+                return false;
+            }
+
+            // create temporary storage based on arbitrary medium
+            // after such a storage is closed it is lost
+            Object oTempStorage = m_xStorageFactory.createInstance();
+            XStorage xTempStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oTempStorage );
+            if ( xTempStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            byte pBytes1[] = { 1, 1, 1, 1, 1 };
+            byte pPass1[] = { 1, 2, 3, 4, 5 };
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToEncrSubstream( xTempStorage, "SubStream1", "MediaType1", true, pBytes1, pPass1 ) )
+                return false;
+
+            byte pBytes2[] = { 2, 2, 2, 2, 2 };
+            byte pPass2[] = { 5, 4, 3, 2, 1 };
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToEncrSubstream( xTempStorage, "SubStream2", "MediaType2", false, pBytes2, pPass2 ) )
+                return false;
+
+            // create temporary storage based on a previously created temporary file
+            Object pArgs[] = new Object[2];
+            pArgs[0] = (Object) sTempFileURL;
+            pArgs[1] = new Integer( ElementModes.ELEMENT_WRITE );
+
+            Object oTempFileStorage = m_xStorageFactory.createInstanceWithArguments( pArgs );
+            XStorage xTempFileStorage = (XStorage)UnoRuntime.queryInterface( XStorage.class, oTempFileStorage );
+            if ( xTempFileStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create storage based on temporary file!" );
+                return false;
+            }
+
+            // copy xTempStorage to xTempFileStorage
+            // xTempFileStorage will be automatically commited
+            if ( !m_aTestHelper.copyStorage( xTempStorage, xTempFileStorage ) )
+                return false;
+
+            // dispose used storages to free resources
+            if ( !m_aTestHelper.disposeStorage( xTempStorage ) || !m_aTestHelper.disposeStorage( xTempFileStorage ) )
+                return false;
+
+            // ================================================
+            // now check all the written and copied information
+            // ================================================
+
+            // the temporary file must not be locked any more after storage disposing
+            pArgs[1] = new Integer( ElementModes.ELEMENT_READWRITE );
+            Object oResultStorage = m_xStorageFactory.createInstanceWithArguments( pArgs );
+            XStorage xResultStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oResultStorage );
+            if ( xResultStorage == null )
+            {
+                m_aTestHelper.Error( "Can't reopen storage based on temporary file!" );
+                return false;
+            }
+
+            Object o2CopyStorage = m_xStorageFactory.createInstance();
+            XStorage x2CopyStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, o2CopyStorage );
+            if ( x2CopyStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            if ( !m_aTestHelper.copyStorage( xResultStorage, x2CopyStorage ) )
+                return false;
+
+            if ( !m_aTestHelper.checkEncrStream( xResultStorage, "SubStream1", "MediaType1", pBytes1, pPass1 ) )
+                return false;
+
+            if ( !m_aTestHelper.checkEncrStream( xResultStorage, "SubStream2", "MediaType2", pBytes2, pPass2 ) )
+                return false;
+
+            if ( !m_aTestHelper.checkEncrStream( x2CopyStorage, "SubStream1", "MediaType1", pBytes1, pPass1 ) )
+                return false;
+
+            if ( !m_aTestHelper.checkEncrStream( x2CopyStorage, "SubStream2", "MediaType2", pBytes2, pPass2 ) )
+                return false;
+
+            // dispose used storages to free resources
+            if ( !m_aTestHelper.disposeStorage( xResultStorage ) )
+                return false;
+
+            return true;
+        }
+        catch( Exception e )
+        {
+            m_aTestHelper.Error( "Exception: " + e );
+            return false;
+        }
+    }
+
+}
+
