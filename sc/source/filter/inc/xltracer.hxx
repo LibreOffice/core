c96883f299805152bb6a4ec44e99d6af738c0434
Jens-Heiner Rechtien <hr@openoffice.org>
2003-11-05 12:43:53 +0000

    INTEGRATION: CWS calc17 (1.2.4); FILE MERGED 2003/09/09 09:58:55 jmarmion 1.2.4.1: #112154# add selected trace features to excel import.

--- sc/source/filter/inc/xltracer.hxx
+++ sc/source/filter/inc/xltracer.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xltracer.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-08-07 15:31:18 $
+ *  last change: $Author: hr $ $Date: 2003-11-05 13:43:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,9 +64,43 @@
 #ifndef SC_XLTRACER_HXX
 #define SC_XLTRACER_HXX
 
+#ifndef SC_SCGLOB_HXX
+#include "global.hxx"        // ScAddress
+#endif
+
 #ifndef SC_XLTOOLS_HXX
 #include "xltools.hxx"
 #endif
+
+enum XclTracerId
+{
+    eUnKnown  = 0,          /// unused but allows us to set the correct index
+    eRowLimitExceeded = 1,
+    eTabLimitExceeded = 2,
+    ePassword = 3,
+    ePrintRange = 4,
+    eHorizontalPrint = 5,
+    eShortDate = 6,
+    eShrinkToFit = 7,
+    eAlignmentFill = 8,
+    eDiagonalBorder = 9,
+    eBorderLineStyle = 10,
+    eFillPattern = 11,
+    eInvisibleGrid = 12,
+    eFormattedNote = 13,
+    eFormulaExtName = 14,
+    ePivotDataSource = 15,
+    eTraceLength = 16        /// this *should* always be the final entry
+};
+
+struct XclTracerDetails
+{
+    XclTracerId                 meProblemId;    /// Excel Import Trace index.
+    const sal_Char*             mpContext;      /// Context for problem e.g. Limits
+    const sal_Char*             mpDetail;       /// Context Detail e.g. SheetX
+    const sal_Char*             mpProblem;      /// Description of problem
+};
+
 
 // ============================================================================
 
@@ -94,15 +128,43 @@ public:
         @descr  Removes all attributes after the element is traced. */
     void                        Trace( const ::rtl::OUString& rElementID, sal_Int32 nMessage );
 
+    /** Calls Trace() with a known document properties problem. */
+    void                        TraceLog( XclTracerId eProblem, sal_Int32 nValue = 0 );
+
+    /** Calls AddAttribute() to create the Context & Detail for known problems. */
+    void                        Context( XclTracerId eProblem, sal_uInt16 nTab = 0 );
+
+    /** Ensure that particular traces are logged once per document. */
+    void                        ProcessTraceOnce(XclTracerId eProblem, sal_uInt16 nTab = 0);
+
+    void                        TraceInvalidAddress(const ScAddress& rPos, const ScAddress& rMaxPos);
+    void                        TraceInvalidRow( sal_uInt16 nTab,  sal_uInt32 nRow, sal_uInt32 nMaxrow );
+    void                        TraceInvalidTab( sal_uInt16 nTab, sal_uInt16 nMaxTab);
+    void                        TracePrintRange();
+    void                        TracePrintFitToPages(sal_uInt16 nFitWidth);
+    void                        TraceDates(sal_uInt16 nNumFmt);
+    void                        TraceShrinkToFit(bool bShrinkToFit);
+    void                        TraceAlignmentFill(bool bAlignmentFill);
+    void                        TraceDiagonalBorder(bool bDiagonalBorder);
+    void                        TraceBorderLineStyle(bool bBorderLineStyle);
+    void                        TraceFillPattern(bool bFillPattern);
+    void                        TraceInvisibleGrid(bool bVisibleGrid);
+    void                        TraceFormattedNote(bool bFormattedNote);
+    void                        TraceFormulaExtName();
+    void                        TracePivotDataSource(bool bExternal);
+
     /** Returns the SVX filter tracer for usage in external code (i.e. Escher). */
     inline MSFilterTracer&      GetBaseTracer() { return *mpTracer; }
 
 private:
     typedef ::std::auto_ptr< MSFilterTracer > MSFilterTracerPtr;
-
     MSFilterTracerPtr           mpTracer;
     bool                        mbEnabled;
+    typedef ::std::vector< bool >     BoolVec;
+    /** array of flags corresponding to each entry in the XclTracerDetails table. */
+    BoolVec                     maFirstTimes;
 };
+
 
 // ============================================================================
 
