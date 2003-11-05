d89a239fca8c4c5585c14b7d8908677da64fef00
Jens-Heiner Rechtien <hr@openoffice.org>
2003-11-05 11:39:18 +0000

    INTEGRATION: CWS tbe7 (1.2.98); FILE MERGED 2003/07/30 08:35:59 tbe 1.2.98.1: #i17353# pass count in Basic IDE break point dialog is not restored

--- basctl/source/basicide/brkdlg.hxx
+++ basctl/source/basicide/brkdlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: brkdlg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: sb $ $Date: 2002-07-09 13:25:24 $
+ *  last change: $Author: hr $ $Date: 2003-11-05 12:39:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -113,7 +113,7 @@ protected:
 public:
             BreakPointDialog( Window* pParent, BreakPointList& rBrkList );
 
-    void    SetCurrentBreakPoint( const BreakPoint& rBrk );
+    void    SetCurrentBreakPoint( BreakPoint* pBrk );
 };
 
 #endif  // _BRKDLG_HXX
