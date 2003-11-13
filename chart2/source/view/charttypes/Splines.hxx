3ef211ee62c556616ce83e3f3467d9f2718f2b47
Ingrid Halama <iha@openoffice.org>
2003-11-13 09:01:24 +0000

    corrected include saver

--- chart2/source/view/charttypes/Splines.hxx
+++ chart2/source/view/charttypes/Splines.hxx
@@ -1,5 +1,5 @@
-#ifndef _CHART2__HXX
-#define _CHART2__HXX
+#ifndef _CHART2_SPLINECALCULATOR_HXX
+#define _CHART2_SPLINECALCULATOR_HXX
 
 #ifndef _COM_SUN_STAR_DRAWING_POLYPOLYGONSHAPE3D_HPP_
 #include <com/sun/star/drawing/PolyPolygonShape3D.hpp>
