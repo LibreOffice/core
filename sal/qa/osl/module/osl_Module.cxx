8da7eab6102ebfbb8ac81ff2ef009cfe72818d8b
Kurt Zenker <kz@openoffice.org>
2003-11-18 15:39:02 +0000

    INTEGRATION: CWS qadev13 (1.2.18); FILE MERGED 2003/11/05 03:05:03 mindyliu 1.2.18.2: #112598# module::load_002 updated 2003/11/04 10:47:52 mindyliu 1.2.18.1: #112598# module::load_002 added

--- sal/qa/osl/module/osl_Module.cxx
+++ sal/qa/osl/module/osl_Module.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: osl_Module.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $  $Date: 2003-09-08 13:21:15 $
+ *  last change: $Author: kz $  $Date: 2003-11-18 16:39:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -110,6 +110,91 @@ inline ::rtl::OUString getDllURL( void )
     osl::FileBase::getAbsoluteFileURL( dirPath, libPath, dllPath );
 
     return dllPath;
+}
+
+/** print a UNI_CODE file name.
+*/
+inline void printFileName( const ::rtl::OUString & str )
+{
+    rtl::OString aString;
+
+    printf( "#printFileName_u# " );
+    aString = ::rtl::OUStringToOString( str, RTL_TEXTENCODING_ASCII_US );
+    printf( "%s\n", aString.getStr( ) );
+}
+
+inline sal_Bool isURL( const ::rtl::OUString pathname )
+{
+    ::rtl::OUString aPreURL( rtl::OUString::createFromAscii( "file:///" ) );
+    return ( ( pathname.indexOf( aPreURL ) == 0 ) ? sal_True : sal_False );
+}
+
+/** create a temp test directory using OUString name of full qualified URL or system path.
+*/
+inline void createTestDirectory( const ::rtl::OUString dirname )
+{
+    ::rtl::OUString     aPathURL   = dirname.copy( 0 );
+    ::osl::FileBase::RC nError;
+
+    if ( !isURL( dirname ) )
+        ::osl::FileBase::getFileURLFromSystemPath( dirname, aPathURL ); //convert if not full qualified URL
+    nError = ::osl::Directory::create( aPathURL );
+    CPPUNIT_ASSERT_MESSAGE( "In createTestDirectory Function: creation: ", ( ::osl::FileBase::E_None == nError ) || ( nError == ::osl::FileBase::E_EXIST ) );
+}
+
+/** delete a temp test directory using OUString name of full qualified URL or system path.
+*/
+inline void deleteTestDirectory( const ::rtl::OUString dirname )
+{
+    ::rtl::OUString     aPathURL   = dirname.copy( 0 );
+    ::osl::FileBase::RC nError;
+    if ( !isURL( dirname ) )
+        ::osl::FileBase::getFileURLFromSystemPath( dirname, aPathURL ); //convert if not full qualified URL
+
+    ::osl::Directory testDir( aPathURL );
+    if ( testDir.isOpen( ) == sal_True )
+    {
+            testDir.close( );  //close if still open.
+        }
+
+    nError = ::osl::Directory::remove( aPathURL );
+     CPPUNIT_ASSERT_MESSAGE( "In deleteTestDirectory function: remove ", ( ::osl::FileBase::E_None == nError ) || ( nError == ::osl::FileBase::E_NOENT ) );
+}
+
+//check if the file exist
+inline sal_Bool ifFileExist( const ::rtl::OUString & str )
+{
+    ::rtl::OUString     aUStr;
+    if ( isURL( str ) )
+        ::osl::FileBase::getSystemPathFromFileURL( str, aUStr );
+    else
+        return sal_False;
+
+    ::osl::File strFile( aUStr );
+    sal_Bool nError = strFile.open( OpenFlag_Read );
+    if ( ::File::E_NOENT == nError )
+        return sal_False;
+    else{
+        strFile.close( );
+        return sal_True;
+    }
+}
+
+/** detete a temp test file using OUString name.
+*/
+inline void deleteTestFile( const ::rtl::OUString filename )
+{
+    ::rtl::OUString aPathURL   = filename.copy( 0 );
+    ::osl::FileBase::RC nError;
+
+    if ( !isURL( filename ) )
+        ::osl::FileBase::getFileURLFromSystemPath( filename, aPathURL ); //convert if not full qualified URL
+
+    nError = ::osl::File::setAttributes( aPathURL, Attribute_GrpWrite| Attribute_OwnWrite| Attribute_OthWrite ); // if readonly, make writtenable.
+    CPPUNIT_ASSERT_MESSAGE( "In deleteTestFile Function: set writtenable ", ( ::osl::FileBase::E_None == nError ) || ( ::osl::FileBase::E_NOENT == nError ) );
+
+    nError = ::osl::File::remove( aPathURL );
+    CPPUNIT_ASSERT_MESSAGE( "In deleteTestFile Function: remove ", ( ::osl::FileBase::E_None == nError ) || ( nError == ::osl::FileBase::E_NOENT ) );
 }
 
 
@@ -238,9 +323,53 @@ namespace osl_Module
             CPPUNIT_ASSERT_MESSAGE( "#test comment#: load function should do the same thing as constructor with library name.",
                                     sal_True == bRes  );
         }
+        // load lib which is under a CJK directory
+        void load_002( )
+        {
+#ifdef UNX
+            //Can not get a CJK directory already exist, so here create one. Perhaps reason is encoding problem.
+            ::rtl::OUString aPidDirURL = rtl::OUString::createFromAscii( "file:///tmp/" ) + ::rtl::OUString::valueOf( ( long )getpid( ) );
+            ::rtl::OUString aMyDirURL = aPidDirURL + aKname;
+            createTestDirectory( aPidDirURL );
+            createTestDirectory( aMyDirURL );
+
+            ::rtl::OUString aDLLURL = aMyDirURL + rtl::OUString::createFromAscii( "/libModule_DLL.so" );
+            //check if the lib exist.
+            //FIXME: if assert condition is false, the case will return, so the directory will not be clean-up
+            CPPUNIT_ASSERT_MESSAGE( "#Source file is not exist. please manually clean-up directory and file under /tmp", ifFileExist( getDllURL( ) ) == sal_True );
+            ::osl::FileBase::RC nError = ::osl::File::copy( getDllURL( ), aDLLURL );
+            CPPUNIT_ASSERT_MESSAGE( "#copy failed. please manually clean-up directory and file under /tmp", nError == ::osl::FileBase::E_None );
+            //ifFileExist returned false but the file exist
+            CPPUNIT_ASSERT_MESSAGE( "#This file is not exist, copy failed. please manually clean-up directory and file under /tmp", ifFileExist( aDLLURL ) == sal_True );
+
+            //test if can create a normal file
+            ::rtl::OUString aFileURL = aMyDirURL + rtl::OUString::createFromAscii( "/test_file" );
+            ::osl::File testFile( aFileURL );
+            nError = testFile.open( OpenFlag_Create );
+            CPPUNIT_ASSERT_MESSAGE( "#create failed. please manually clean-up directory and file under /tmp", nError == ::osl::FileBase::E_None );
+            CPPUNIT_ASSERT_MESSAGE( "#This file is not exist, create failed. please manually clean-up directory and file under /tmp", ifFileExist( aFileURL ) == sal_True );
+
+            //load the copied dll
+            ::osl::Module aMod( aDLLURL );
+            ::osl::Module aMod1;
+
+            sal_Bool bOK = aMod1.load( aDLLURL );
+            bRes = oslModule(aMod) == oslModule(aMod1);
+            aMod.unload( );
+            aMod1.unload( );
+            deleteTestFile( aFileURL );
+            deleteTestFile( aDLLURL );
+            deleteTestDirectory( aMyDirURL );
+            deleteTestDirectory( aPidDirURL );
+
+            CPPUNIT_ASSERT_MESSAGE( "#test comment#: load lib which is under a CJK directory.",
+                                    sal_True == bRes && bOK == sal_True );
+#endif
+        }
 
         CPPUNIT_TEST_SUITE( load );
         CPPUNIT_TEST( load_001 );
+        CPPUNIT_TEST( load_002 );
         CPPUNIT_TEST_SUITE_END( );
     }; // class load
 
