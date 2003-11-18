c76c7f3eae7dc2278ea12593886ae5f8b83d6aaf
Kurt Zenker <kz@openoffice.org>
2003-11-18 16:18:05 +0000

    INTEGRATION: CWS scriptingf1 (1.32.64); FILE MERGED 2003/10/06 14:51:09 mav 1.32.64.1: #i19303# XDocumentSubStorageSupplier.idl

--- offapi/com/sun/star/document/makefile.mk
+++ offapi/com/sun/star/document/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.32 $
+#   $Revision: 1.33 $
 #
-#   last change: $Author: rt $ $Date: 2003-04-24 13:29:37 $
+#   last change: $Author: kz $ $Date: 2003-11-18 17:18:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -105,6 +105,7 @@ IDLFILES=\
     XDocumentInfo.idl\
     XDocumentInfoSupplier.idl\
     XDocumentInsertable.idl\
+    XDocumentSubStorageSupplier.idl\
     XEmbeddedObjectResolver.idl\
     XEmbeddedObjectSupplier.idl\
     XEventBroadcaster.idl\
