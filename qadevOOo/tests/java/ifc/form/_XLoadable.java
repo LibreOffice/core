c4b799f8c2f8fad957bfb61e40e6ee9f57964e85
Kurt Zenker <kz@openoffice.org>
2003-11-18 15:22:45 +0000

    INTEGRATION: CWS qadev13 (1.2.6); FILE MERGED 2003/11/12 12:42:39 sg 1.2.6.1: #112280#CHG:fixed bu

--- qadevOOo/tests/java/ifc/form/_XLoadable.java
+++ qadevOOo/tests/java/ifc/form/_XLoadable.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XLoadable.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-09-08 10:35:28 $
+ *  last change:$Date: 2003-11-18 16:22:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -272,6 +272,10 @@ public class _XLoadable extends MultiMethodTest {
         result = ! loadListener.loadedCalled ;
 
         tRes.tested("removeLoadListener()", result) ;
+    }
+
+    protected void after() {
+        disposeEnvironment();
     }
 }
 
