6b6ecc7f43a37b73528ef3dcda528f1c18a8acc5
Kurt Zenker <kz@openoffice.org>
2003-11-18 15:28:02 +0000

    INTEGRATION: CWS qadev13 (1.1.2); FILE ADDED 2003/10/17 09:33:19 cn 1.1.2.1: NEW: #112566# initial final version

---
+++ qadevOOo/tests/java/mod/_fwk/JobHandler.java
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  $RCSfile: JobHandler.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change:$Date: 2003-11-18 16:28:02 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._fwk;
+
+import java.io.PrintWriter;
+
+import lib.Status;
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.XInterface;
+import com.sun.star.util.URL;
+import util.utils;
+/**
+* Test for object that implements the following interfaces :
+* <ul>
+*  <li><code>com::sun::star::frame::XDispatchProvider</code></li>
+*  <li><code>com::sun::star::lang::XInitialization</code></li>
+* </ul><p>
+* @see com.sun.star.frame.XDispatchProvider
+* @see com.sun.star.lang.XInitialization
+* @see ifc.frame._XDispatchProvider
+* @see ifc.lang._XInitialization
+*/
+public class JobHandler extends TestCase {
+
+    /**
+    * Creating a Testenvironment for the interfaces to be tested.
+    */
+    public TestEnvironment createTestEnvironment( TestParameters Param,
+        PrintWriter log ) throws StatusException {
+        boolean serviceRegistered = false;
+
+        try {
+            Object obj = ((XMultiServiceFactory)Param.getMSF()).createInstance("test.Job");
+            serviceRegistered = obj != null;
+        } catch(com.sun.star.uno.Exception e) {}
+
+        log.println("Service test.Job is "
+            + (serviceRegistered ? "already" : "not yet")  + " registered.");
+        if (! serviceRegistered){
+            String message = "You have to register 'test.Job' before office is stared.\n";
+            message += "Please run '$OFFICEPATH/program/pkgchk $DOCPTH/qadevlibs/JobExecutor.jar'";
+            throw new StatusException(message, new Exception());
+        }
+
+        XInterface oObj = null;
+
+        try {
+            oObj = (XInterface)((XMultiServiceFactory)Param.getMSF()).createInstance(
+                "com.sun.star.comp.framework.jobs.JobDispatch");
+        } catch(com.sun.star.uno.Exception e) {
+            e.printStackTrace(log);
+            throw new StatusException(
+                Status.failed("Couldn't create instance"));
+        }
+
+        TestEnvironment tEnv = new TestEnvironment( oObj );
+
+        tEnv.addObjRelation("XDispatchProvider.URL", "vnd.sun.star.job:alias=TestJob");
+        return tEnv;
+    } // finish method getTestEnvironment
+
+}
