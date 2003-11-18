d69b55de2cfa73452db00e78af11dfcffb9183cc
Kurt Zenker <kz@openoffice.org>
2003-11-18 15:23:30 +0000

    INTEGRATION: CWS qadev13 (1.2.6); FILE MERGED 2003/11/12 12:59:42 sg 1.2.6.1: #113689#CHG: fixed failed test

--- qadevOOo/tests/java/ifc/sdb/_XRowSetApproveBroadcaster.java
+++ qadevOOo/tests/java/ifc/sdb/_XRowSetApproveBroadcaster.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XRowSetApproveBroadcaster.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-09-08 10:52:27 $
+ *  last change:$Date: 2003-11-18 16:23:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -240,7 +240,7 @@ public class _XRowSetApproveBroadcaster extends MultiMethodTest {
     * Disposes object environment.
     */
     public void after() {
-        //disposeEnvironment() ;
+        disposeEnvironment() ;
     }
 
 }  // finish class _XRowSetApproveBroadcaster
