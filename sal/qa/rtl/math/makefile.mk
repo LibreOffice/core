c37ebf776af4498987318c1fd4ce1ab4c972e033
Kurt Zenker <kz@openoffice.org>
2003-11-18 15:41:52 +0000

    INTEGRATION: CWS qadev13 (1.3.26); FILE MERGED 2003/10/31 09:29:22 lla 1.3.26.1: #112598# changes in makefiles, to use dmake test

--- sal/qa/rtl/math/makefile.mk
+++ sal/qa/rtl/math/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2003-08-07 15:12:08 $
+#   last change: $Author: kz $ $Date: 2003-11-18 16:41:52 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -102,4 +102,5 @@ SLOFILES=$(SHL1OBJS)
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
+.INCLUDE : _cppunit.mk
 
