ca806f146f0dbf4563599430fa0cc6750cc56495
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:51:27 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:14:33 aw 1.1.2.6: #111097# 2003/07/24 14:25:53 aw 1.1.2.5: #110094# Adaptions for DrawingLayer changes 2003/06/30 13:27:26 aw 1.1.2.4: #110094# 2003/06/06 12:59:27 aw 1.1.2.3: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:53:56 aw 1.1.2.2: #109820# Removed all itemset handling stuff, migrated to new properties implementations completely. I had to change from base class Properties to BaseProperties due to a solaris compiler bug. 2003/05/21 11:31:58 aw 1.1.2.1: #109820#

---
+++ svx/source/sdr/properties/properties.cxx
@@ -0,0 +1,230 @@
+/*************************************************************************
+ *
+ *  $RCSfile: properties.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:51:27 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_PROPERTIES_HXX
+#include <svx/sdr/properties/properties.hxx>
+#endif
+
+#ifndef _SDR_PROPERTIES_ITEMSETTOOLS_HXX
+#include <svx/sdr/properties/itemsettools.hxx>
+#endif
+
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+
+#ifndef _SVDOGRP_HXX
+#include <svdogrp.hxx>
+#endif
+
+#ifndef _SVDITER_HXX
+#include <svditer.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        BaseProperties::BaseProperties(SdrObject& rObj)
+        :   mrObject(rObj)
+        {
+        }
+
+        BaseProperties::BaseProperties(const BaseProperties& rProps, SdrObject& rObj)
+        :   mrObject(rObj)
+        {
+        }
+
+        BaseProperties::~BaseProperties()
+        {
+        }
+
+        const SfxItemSet& BaseProperties::GetMergedItemSet() const
+        {
+            // default implementation falls back to GetObjectItemSet()
+            return GetObjectItemSet();
+        }
+
+        void BaseProperties::SetMergedItemSet(const SfxItemSet& rSet, sal_Bool bClearAllItems)
+        {
+            // clear items if requested
+            if(bClearAllItems)
+            {
+                ClearObjectItem();
+            }
+
+            // default implementation falls back to SetObjectItemSet()
+            SetObjectItemSet(rSet);
+        }
+
+        void BaseProperties::SetMergedItem(const SfxPoolItem& rItem)
+        {
+            // default implementation falls back to SetObjectItem()
+            SetObjectItem(rItem);
+        }
+
+        void BaseProperties::ClearMergedItem(const sal_uInt16 nWhich)
+        {
+            // default implementation falls back to ClearObjectItem()
+            ClearObjectItem(nWhich);
+        }
+
+        void BaseProperties::Scale(const Fraction& rScale)
+        {
+            // default implementation does nothing; overload where
+            // an ItemSet is implemented.
+        }
+
+        void BaseProperties::MoveToItemPool(SfxItemPool* pSrcPool, SfxItemPool* pDestPool, SdrModel* pNewModel)
+        {
+            // Move properties to a new ItemPool. Default implementation does nothing.
+            // Overload where an ItemSet is implemented.
+        }
+
+        void BaseProperties::SetModel(SdrModel* pOldModel, SdrModel* pNewModel)
+        {
+            // Set new model. Default implementation does nothing.
+            // Overload where an ItemSet is implemented.
+        }
+
+        void BaseProperties::ForceStyleToHardAttributes(sal_Bool bPseudoSheetsOnly)
+        {
+            // force all attributes which come from styles to hard attributes
+            // to be able to live without the style. Default implementation does nothing.
+            // Overload where an ItemSet is implemented.
+        }
+
+        //void BaseProperties::SetItemAndBroadcast(const SfxPoolItem& rItem)
+        //{
+        //  ItemChangeBroadcaster aC(GetSdrObject());
+        //  SetObjectItem(rItem);
+        //  BroadcastItemChange(aC);
+        //}
+
+        //void BaseProperties::ClearItemAndBroadcast(const sal_uInt16 nWhich)
+        //{
+        //  ItemChangeBroadcaster aC(GetSdrObject());
+        //  ClearObjectItem(nWhich);
+        //  BroadcastItemChange(aC);
+        //}
+
+        void BaseProperties::SetMergedItemSetAndBroadcast(const SfxItemSet& rSet, sal_Bool bClearAllItems)
+        {
+            ItemChangeBroadcaster aC(GetSdrObject());
+
+            if(bClearAllItems)
+            {
+                ClearObjectItem();
+            }
+
+            SetMergedItemSet(rSet);
+            BroadcastItemChange(aC);
+        }
+
+        const SfxPoolItem& BaseProperties::GetItem(const sal_uInt16 nWhich) const
+        {
+            return GetObjectItemSet().Get(nWhich);
+        }
+
+        void BaseProperties::BroadcastItemChange(const ItemChangeBroadcaster& rChange)
+        {
+            const sal_uInt32 nCount(rChange.GetRectangleCount());
+
+            // #110094#-14 Reduce to do only second change
+            //// invalidate all remembered rectangles
+            //for(sal_uInt32 a(0); a < nCount; a++)
+            //{
+            //  GetSdrObject().BroadcastObjectChange(rChange.GetRectangle(a));
+            //}
+
+            // invalidate all new rectangles
+            if(GetSdrObject().ISA(SdrObjGroup))
+            {
+                SdrObjListIter aIter((SdrObjGroup&)GetSdrObject(), IM_DEEPNOGROUPS);
+
+                while(aIter.IsMore())
+                {
+                    SdrObject* pObj = aIter.Next();
+                    // This is done with ItemSetChanged
+                    // pObj->SetChanged();
+                    pObj->BroadcastObjectChange();
+                }
+            }
+            else
+            {
+                // This is done with ItemSetChanged
+                // GetSdrObject().SetChanged();
+                GetSdrObject().BroadcastObjectChange();
+            }
+
+            // also send the user calls
+            for(sal_uInt32 a(0L); a < nCount; a++)
+            {
+                GetSdrObject().SendUserCall(SDRUSERCALL_CHGATTR, rChange.GetRectangle(a));
+            }
+        }
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
