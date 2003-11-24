8efefdc846af85ef98bf3999b951932091dd4b52
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:35:33 +0000

    INTEGRATION: CWS aw003 (1.1.1.1.236); FILE MERGED 2003/07/24 14:38:06 aw 1.1.1.1.236.1: #110094# Adaptions for DrawingLayer changes

--- svx/source/engine3d/dragmt3d.cxx
+++ svx/source/engine3d/dragmt3d.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dragmt3d.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:01:15 $
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:35:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -233,7 +233,8 @@ IMPL_LINK(E3dDragMethod, TimerInterruptHdl, void*, EMPTYARG)
                     nOldQual = 255;
 
                 pScene->SetDisplayQuality((UINT8)nOldQual);
-                pScene->SendRepaintBroadcast();
+                pScene->SetChanged();
+                pScene->BroadcastObjectChange();
 
                 if(nOldQual != 255)
                 {
