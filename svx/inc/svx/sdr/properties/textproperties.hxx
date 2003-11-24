9dfe86fb5c752d837ae9580931f0fe0a5ec48a4f
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:32:24 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:01:52 aw 1.1.2.4: #111097# 2003/06/30 13:24:43 aw 1.1.2.3: #110094# 2003/06/06 12:58:04 aw 1.1.2.2: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:48:02 aw 1.1.2.1: #109820# The new includes for the isolated properties. They are in /svx/inc/svx/sdr/properties since they can then be included in other projects without the need to use the hedabu mechanism. Hedabu cannot be used when delivering to non-simple inc dest dirs, had a long discussion with RT. Thus, the old tree in svx/sdr/properties is no longer used.

---
+++ svx/inc/svx/sdr/properties/textproperties.hxx
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  $RCSfile: textproperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:32:24 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_TEXTPROPERTIES_HXX
+#define _SDR_PROPERTIES_TEXTPROPERTIES_HXX
+
+#ifndef _SDR_PROPERTIES_ATTRIBUTEPROPERTIES_HXX
+#include <svx/sdr/properties/attributeproperties.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        class TextProperties : public AttributeProperties
+        {
+        protected:
+            // create a new itemset
+            virtual SfxItemSet& CreateObjectSpecificItemSet(SfxItemPool& rPool);
+
+            // Do the ItemChange, may do special handling
+            virtual void ItemChange(const sal_uInt16 nWhich, const SfxPoolItem* pNewItem = 0);
+
+            // react on ItemSet changes
+            virtual void ItemSetChanged(const SfxItemSet& rSet);
+
+        public:
+            // basic constructor
+            TextProperties(SdrObject& rObj);
+
+            // constructor for copying, but using new object
+            TextProperties(const TextProperties& rProps, SdrObject& rObj);
+
+            // destructor
+            virtual ~TextProperties();
+
+            // Clone() operator, normally just calls the local copy constructor
+            virtual BaseProperties& Clone(SdrObject& rObj) const;
+
+            // set a new StyleSheet and broadcast
+            virtual void SetStyleSheet(SfxStyleSheet* pNewStyleSheet, sal_Bool bDontRemoveHardAttr);
+
+            // pre/post-process saving
+            virtual void PreProcessSave();
+
+            // force default attributes for a specific object type, called from
+            // DefaultProperties::GetObjectItemSet() if a new ItemSet is created
+            virtual void ForceDefaultAttributes();
+
+            // force all attributes which come from styles to hard attributes
+            // to be able to live without the style.
+            virtual void ForceStyleToHardAttributes(sal_Bool bPseudoSheetsOnly = sal_False);
+
+            // This is the notifyer from SfxListener
+            virtual void Notify(SfxBroadcaster& rBC, const SfxHint& rHint);
+        };
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_PROPERTIES_TEXTPROPERTIES_HXX
+
+// eof
