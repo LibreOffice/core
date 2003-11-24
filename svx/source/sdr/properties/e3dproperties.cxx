f82af47f8d5bd93a7c16fd72ef5da3ab885f689a
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:49:33 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:14:17 aw 1.1.2.5: #111097# 2003/06/30 13:27:23 aw 1.1.2.4: #110094# 2003/06/06 12:59:24 aw 1.1.2.3: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:53:53 aw 1.1.2.2: #109820# Removed all itemset handling stuff, migrated to new properties implementations completely. I had to change from base class Properties to BaseProperties due to a solaris compiler bug. 2003/05/21 11:31:50 aw 1.1.2.1: #109820#

---
+++ svx/source/sdr/properties/e3dproperties.cxx
@@ -0,0 +1,149 @@
+/*************************************************************************
+ *
+ *  $RCSfile: e3dproperties.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:49:33 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_E3DPROPERTIES_HXX
+#include <svx/sdr/properties/e3dproperties.hxx>
+#endif
+
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+
+#ifndef _SVDDEF_HXX
+#include <svddef.hxx>
+#endif
+
+#ifndef _E3D_OBJ3D_HXX
+#include <obj3d.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        // create a new itemset
+        SfxItemSet& E3dProperties::CreateObjectSpecificItemSet(SfxItemPool& rPool)
+        {
+            return *(new SfxItemSet(rPool,
+
+                // ranges from SdrAttrObj
+                SDRATTR_START, SDRATTRSET_SHADOW,
+                SDRATTRSET_OUTLINER, SDRATTRSET_MISC,
+                SDRATTR_TEXTDIRECTION, SDRATTR_TEXTDIRECTION,
+
+                // ranges from E3dObject, contains object and scene because of GetMergedItemSet()
+                SDRATTR_3D_FIRST, SDRATTR_3D_LAST,
+
+                // end
+                0, 0));
+        }
+
+        E3dProperties::E3dProperties(SdrObject& rObj)
+        :   AttributeProperties(rObj)
+        {
+        }
+
+        E3dProperties::E3dProperties(const E3dProperties& rProps, SdrObject& rObj)
+        :   AttributeProperties(rProps, rObj)
+        {
+        }
+
+        E3dProperties::~E3dProperties()
+        {
+        }
+
+        BaseProperties& E3dProperties::Clone(SdrObject& rObj) const
+        {
+            return *(new E3dProperties(*this, rObj));
+        }
+
+        void E3dProperties::ItemSetChanged(const SfxItemSet& rSet)
+        {
+            E3dObject& rObj = (E3dObject&)GetSdrObject();
+
+            // call parent
+            AttributeProperties::ItemSetChanged(rSet);
+
+            // local changes
+            rObj.StructureChanged(&rObj);
+        }
+
+        void E3dProperties::SetStyleSheet(SfxStyleSheet* pNewStyleSheet, sal_Bool bDontRemoveHardAttr)
+        {
+            // call parent
+            AttributeProperties::SetStyleSheet(pNewStyleSheet, bDontRemoveHardAttr);
+
+            // propagate call to contained objects
+            const SdrObjList* pSub = ((const E3dObject&)GetSdrObject()).GetSubList();
+            const sal_uInt32 nCount(pSub->GetObjCount());
+
+            for(sal_uInt32 a(0L); a < nCount; a++)
+            {
+                pSub->GetObj(a)->GetProperties().SetStyleSheet(pNewStyleSheet, bDontRemoveHardAttr);
+            }
+        }
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
