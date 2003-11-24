baae8ecf5f2b695fc6a4a92a6c3fe4dc1abe61ff
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:29:23 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:01:07 aw 1.1.2.4: #111097# 2003/06/30 13:24:37 aw 1.1.2.3: #110094# 2003/06/06 12:57:58 aw 1.1.2.2: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:47:50 aw 1.1.2.1: #109820# The new includes for the isolated properties. They are in /svx/inc/svx/sdr/properties since they can then be included in other projects without the need to use the hedabu mechanism. Hedabu cannot be used when delivering to non-simple inc dest dirs, had a long discussion with RT. Thus, the old tree in svx/sdr/properties is no longer used.

---
+++ svx/inc/svx/sdr/properties/defaultproperties.hxx
@@ -0,0 +1,151 @@
+/*************************************************************************
+ *
+ *  $RCSfile: defaultproperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:29:23 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_DEFAULTPROPERTIES_HXX
+#define _SDR_PROPERTIES_DEFAULTPROPERTIES_HXX
+
+#ifndef _SDR_PROPERTIES_PROPERTIES_HXX
+#include <svx/sdr/properties/properties.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        class DefaultProperties : public BaseProperties
+        {
+        protected:
+            // the to be used ItemSet
+            SfxItemSet*                                     mpItemSet;
+
+            // create a new itemset
+            virtual SfxItemSet& CreateObjectSpecificItemSet(SfxItemPool& rPool);
+
+            // test changeability for a single item
+            virtual sal_Bool AllowItemChange(const sal_uInt16 nWhich, const SfxPoolItem* pNewItem = 0) const;
+
+            // Do the ItemChange, may do special handling
+            virtual void ItemChange(const sal_uInt16 nWhich, const SfxPoolItem* pNewItem = 0);
+
+            // Called after ItemChange() is done for all items.
+            virtual void PostItemChange(const sal_uInt16 nWhich);
+
+            // react on ItemSet changes
+            virtual void ItemSetChanged(const SfxItemSet& rSet);
+
+        public:
+            // basic constructor
+            DefaultProperties(SdrObject& rObj);
+
+            // constructor for copying, but using new object
+            DefaultProperties(const DefaultProperties& rProps, SdrObject& rObj);
+
+            // destructor
+            virtual ~DefaultProperties();
+
+            // Clone() operator, normally just calls the local copy constructor
+            virtual BaseProperties& Clone(SdrObject& rObj) const;
+
+            // get itemset
+            virtual const SfxItemSet& GetObjectItemSet() const;
+
+            // set single item
+            virtual void SetObjectItem(const SfxPoolItem& rItem);
+
+            // set single item direct, do not do any notifies or things like that
+            virtual void SetObjectItemDirect(const SfxPoolItem& rItem);
+
+            // clear single item
+            virtual void ClearObjectItem(const sal_uInt16 nWhich = 0);
+
+            // clear single item direct, do not do any notifies or things like that.
+            // Also supports complete deleteion of items when default parameter 0 is used.
+            virtual void ClearObjectItemDirect(const sal_uInt16 nWhich = 0);
+
+            // set complete item set
+            virtual void SetObjectItemSet(const SfxItemSet& rSet);
+
+            // set a new StyleSheet and broadcast
+            virtual void SetStyleSheet(SfxStyleSheet* pNewStyleSheet, sal_Bool bDontRemoveHardAttr);
+
+            // get the installed StyleSheet
+            virtual SfxStyleSheet* GetStyleSheet() const;
+
+            // pre/post-process saving
+            virtual void PreProcessSave();
+            virtual void PostProcessSave();
+
+            // force default attributes for a specific object type, called from
+            // DefaultProperties::GetObjectItemSet() if a new ItemSet is created.
+            // Default implementation does nothing.
+            virtual void ForceDefaultAttributes();
+
+            // Scale the included ItemSet.
+            virtual void Scale(const Fraction& rScale);
+        };
+    } // end of namespace properties
+} // end of namespace sdr
+
+#endif //_SDR_PROPERTIES_DEFAULTPROPERTIES_HXX
+
+// eof
