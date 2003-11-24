6a631e5accc4cf2f6c997ee3c045c0c4de67b9cc
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:27:15 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/20 10:41:43 aw 1.1.2.7: #111111# 2003/10/07 12:00:12 aw 1.1.2.6: #111097# 2003/08/19 16:37:12 aw 1.1.2.5: #111096# 2003/07/30 16:07:10 aw 1.1.2.4: #110094# Further DrawingLayer adaptions 2003/07/25 10:13:36 aw 1.1.2.3: #110094# Further changes 2003/07/24 14:23:55 aw 1.1.2.2: #110094# Adaptions for DrawingLayer changes 2003/06/30 13:24:09 aw 1.1.2.1: #110094#

---
+++ svx/inc/svx/sdr/contact/viewcontactofsdrpage.hxx
@@ -0,0 +1,160 @@
+/*************************************************************************
+ *
+ *  $RCSfile: viewcontactofsdrpage.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:27:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFSDRPAGE_HXX
+#define _SDR_CONTACT_VIEWCONTACTOFSDRPAGE_HXX
+
+#ifndef _SAL_TYPES_H_
+#include <sal/types.h>
+#endif
+
+#ifndef _SDR_CONTACT_VIEWCONTACT_HXX
+#include <svx/sdr/contact/viewcontact.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class SdrPage;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class ViewContactOfSdrPage : public ViewContact
+        {
+        protected:
+            // the owner of this ViewContact. Set from constructor and not
+            // to be changed in any way.
+            SdrPage&                                        mrPage;
+
+            // internal access to SdrObject
+            SdrPage& GetSdrPage() const
+            {
+                return mrPage;
+            }
+
+            // method to create a AnimationInfo. Needs to give a result if
+            // SupportsAnimation() is overloaded and returns sal_True.
+            virtual sdr::animation::AnimationInfo* CreateAnimationInfo();
+
+            // Create a Object-Specific ViewObjectContact, set ViewContact and
+            // ObjectContact. Always needs to return something.
+            virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact);
+
+            // method to recalculate the PaintRectangle if the validity flag shows that
+            // it is invalid. The flag is set from GetPaintRectangle, thus the implementation
+            // only needs to refresh maPaintRectangle itself.
+            virtual void CalcPaintRectangle();
+
+        private:
+            // Because of old correction hacks there may be MasterPages (better:
+            // MasterPageDescriptors) set at the page, but actually no MasterPages
+            // exist at the model. To correctly handle that cases it is necessary to
+            // not only get the MasterPageCount() form the page, but also to correct
+            // that number if the model does not have any MasterPages.
+            sal_uInt32 ImpGetCorrectedMasterPageCount() const;
+
+        protected:
+            // local paint methods
+            void DrawPaper(DisplayInfo& rDisplayInfo);
+            void DrawPaperBorder(DisplayInfo& rDisplayInfo);
+            void DrawBorder(DisplayInfo& rDisplayInfo);
+            void DrawHelplines(DisplayInfo& rDisplayInfo);
+            void DrawGrid(DisplayInfo& rDisplayInfo);
+
+        public:
+            // basic constructor, used from SdrPage.
+            ViewContactOfSdrPage(SdrPage& rObj);
+
+            // The destructor. When PrepareDelete() was not called before (see there)
+            // warnings will be generated in debug version if there are still contacts
+            // existing.
+            virtual ~ViewContactOfSdrPage();
+
+            // When ShouldPaintObject() returns sal_True, the object itself is painted and
+            // PaintObject() is called.
+            virtual sal_Bool ShouldPaintObject(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
+
+            // Pre- and Post-Paint this object. Is used e.g. for page background/foreground painting.
+            virtual void PrePaintObject(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
+            virtual void PostPaintObject(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
+
+            // Access to possible sub-hierarchy
+            virtual sal_uInt32 GetObjectCount() const;
+            virtual ViewContact& GetViewContact(sal_uInt32 nIndex) const;
+            // Since MasterPages are part of the hierarchy of a DrawPage, the
+            // link to ParentContacts may be 1:n
+            virtual sal_Bool GetParentContacts(ViewContactVector& rVContacts) const;
+
+            // Does this ViewContact support animation?
+            virtual sal_Bool SupportsAnimation() const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWCONTACTOFSDRPAGE_HXX
+
+// eof
