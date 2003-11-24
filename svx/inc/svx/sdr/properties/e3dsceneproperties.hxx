2d649cac1fd8c5214904b11107e70b51c141cd54
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:30:22 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:01:25 aw 1.1.2.4: #111097# 2003/06/30 13:24:39 aw 1.1.2.3: #110094# 2003/06/06 12:58:00 aw 1.1.2.2: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:47:55 aw 1.1.2.1: #109820# The new includes for the isolated properties. They are in /svx/inc/svx/sdr/properties since they can then be included in other projects without the need to use the hedabu mechanism. Hedabu cannot be used when delivering to non-simple inc dest dirs, had a long discussion with RT. Thus, the old tree in svx/sdr/properties is no longer used.

---
+++ svx/inc/svx/sdr/properties/e3dsceneproperties.hxx
@@ -0,0 +1,140 @@
+/*************************************************************************
+ *
+ *  $RCSfile: e3dsceneproperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:30:22 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_E3DSCENEPROPERTIES_HXX
+#define _SDR_PROPERTIES_E3DSCENEPROPERTIES_HXX
+
+#ifndef _SDR_PROPERTIES_E3DPROPERTIES_HXX
+#include <svx/sdr/properties/e3dproperties.hxx>
+#endif
+
+// predeclarations
+class B3dLightGroup;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        class E3dSceneProperties : public E3dProperties
+        {
+        protected:
+            // Called after ItemChange() is done for all items.
+            virtual void PostItemChange(const sal_uInt16 nWhich);
+
+        public:
+            // basic constructor
+            E3dSceneProperties(SdrObject& rObj);
+
+            // constructor for copying, but using new object
+            E3dSceneProperties(const E3dSceneProperties& rProps, SdrObject& rObj);
+
+            // destructor
+            virtual ~E3dSceneProperties();
+
+            // Clone() operator, normally just calls the local copy constructor
+            virtual BaseProperties& Clone(SdrObject& rObj) const;
+
+            // get itemset
+            virtual const SfxItemSet& GetObjectItemSet() const;
+
+            // get merged ItemSet. Normappl, this maps directly to GetObjectItemSet(), but may
+            // be overloaded e.g for group objects to return a merged ItemSet of the object.
+            // When using this method the returned ItemSet may contain items in the state
+            // SFX_ITEM_DONTCARE which means there were several such items with different
+            // values.
+            virtual const SfxItemSet& GetMergedItemSet() const;
+
+            // Set merged ItemSet. Normally, this maps to SetObjectItemSet().
+            virtual void SetMergedItemSet(const SfxItemSet& rSet, sal_Bool bClearAllItems = sal_False);
+
+            // Set a single item, iterate over hierarchies if necessary.
+            virtual void SetMergedItem(const SfxPoolItem& rItem);
+
+            // Clear a single item, iterate over hierarchies if necessary.
+            virtual void ClearMergedItem(const sal_uInt16 nWhich = 0);
+
+            // set a new StyleSheet and broadcast
+            virtual void SetStyleSheet(SfxStyleSheet* pNewStyleSheet, sal_Bool bDontRemoveHardAttr);
+
+            // get the installed StyleSheet
+            virtual SfxStyleSheet* GetStyleSheet() const;
+
+            // pre/post-process saving
+            virtual void PreProcessSave();
+            virtual void PostProcessSave();
+
+            // Move properties to a new ItemPool. Default implementation does nothing.
+            virtual void MoveToItemPool(SfxItemPool* pSrcPool, SfxItemPool* pDestPool, SdrModel* pNewModel = 0L);
+
+            // Special for scene:
+            void SetLightItemsFromLightGroup(B3dLightGroup& rLightGroup);
+            void SetSceneItemsFromCamera();
+        };
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // _SDR_PROPERTIES_E3DSCENEPROPERTIES_HXX
+
+// eof
