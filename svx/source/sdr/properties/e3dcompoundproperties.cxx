a87038ad647412bea65404620d0ce743c124a380
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:48:58 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:14:09 aw 1.1.2.5: #111097# 2003/06/30 13:27:21 aw 1.1.2.4: #110094# 2003/06/06 12:59:24 aw 1.1.2.3: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:53:52 aw 1.1.2.2: #109820# Removed all itemset handling stuff, migrated to new properties implementations completely. I had to change from base class Properties to BaseProperties due to a solaris compiler bug. 2003/05/21 11:31:48 aw 1.1.2.1: #109820#

---
+++ svx/source/sdr/properties/e3dcompoundproperties.cxx
@@ -0,0 +1,193 @@
+/*************************************************************************
+ *
+ *  $RCSfile: e3dcompoundproperties.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:48:58 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_E3DCOMPOUNDPROPERTIES_HXX
+#include <svx/sdr/properties/e3dcompoundproperties.hxx>
+#endif
+
+#ifndef _E3D_OBJ3D_HXX
+#include <obj3d.hxx>
+#endif
+
+#ifndef _E3D_SCENE3D_HXX
+#include <scene3d.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        E3dCompoundProperties::E3dCompoundProperties(SdrObject& rObj)
+        :   E3dProperties(rObj)
+        {
+        }
+
+        E3dCompoundProperties::E3dCompoundProperties(const E3dCompoundProperties& rProps, SdrObject& rObj)
+        :   E3dProperties(rProps, rObj)
+        {
+        }
+
+        E3dCompoundProperties::~E3dCompoundProperties()
+        {
+        }
+
+        BaseProperties& E3dCompoundProperties::Clone(SdrObject& rObj) const
+        {
+            return *(new E3dCompoundProperties(*this, rObj));
+        }
+
+        const SfxItemSet& E3dCompoundProperties::GetObjectItemSet() const
+        {
+            //DBG_ASSERT(sal_False, "E3dCompoundProperties::GetObjectItemSet() maybe the wrong call (!)");
+            return E3dProperties::GetObjectItemSet();
+        }
+
+        const SfxItemSet& E3dCompoundProperties::GetMergedItemSet() const
+        {
+            // include Items of scene this object belongs to
+            E3dCompoundObject& rObj = (E3dCompoundObject&)GetSdrObject();
+            E3dScene* pScene = rObj.GetScene();
+
+            if(pScene)
+            {
+                // add filtered scene properties (SDRATTR_3DSCENE_) to local itemset
+                GetObjectItemSet();
+                SfxItemSet aSet(*mpItemSet->GetPool(), SDRATTR_3DSCENE_FIRST, SDRATTR_3DSCENE_LAST);
+                aSet.Put(pScene->GetProperties().GetObjectItemSet());
+                mpItemSet->Put(aSet);
+            }
+
+            // call parent
+            return E3dProperties::GetMergedItemSet();
+        }
+
+        void E3dCompoundProperties::SetMergedItemSet(const SfxItemSet& rSet, sal_Bool bClearAllItems)
+        {
+            // Set scene specific items at scene
+            E3dCompoundObject& rObj = (E3dCompoundObject&)GetSdrObject();
+            E3dScene* pScene = rObj.GetScene();
+
+            if(pScene)
+            {
+                // Generate filtered scene properties (SDRATTR_3DSCENE_) itemset
+                GetObjectItemSet();
+                SfxItemSet aSet(*mpItemSet->GetPool(), SDRATTR_3DSCENE_FIRST, SDRATTR_3DSCENE_LAST);
+                aSet.Put(rSet);
+
+                if(bClearAllItems)
+                {
+                    pScene->GetProperties().ClearObjectItem();
+                }
+
+                if(aSet.Count())
+                {
+                    pScene->GetProperties().SetObjectItemSet(aSet);
+                }
+            }
+
+            // call parent. This will set items on local object, too.
+            E3dProperties::SetMergedItemSet(rSet, bClearAllItems);
+        }
+
+        void E3dCompoundProperties::PostItemChange(const sal_uInt16 nWhich)
+        {
+            // call parent
+            E3dProperties::PostItemChange(nWhich);
+
+            // handle value change
+            E3dCompoundObject& rObj = (E3dCompoundObject&)GetSdrObject();
+
+            switch(nWhich)
+            {
+                case SDRATTR_3DOBJ_DOUBLE_SIDED:
+                {
+                    rObj.InvalidateGeometry();
+                    break;
+                }
+                case SDRATTR_3DOBJ_NORMALS_KIND:
+                {
+                    rObj.InvalidateGeometry();
+                    break;
+                }
+                case SDRATTR_3DOBJ_NORMALS_INVERT:
+                {
+                    rObj.InvalidateGeometry();
+                    break;
+                }
+                case SDRATTR_3DOBJ_TEXTURE_PROJ_X:
+                {
+                    rObj.InvalidateGeometry();
+                    break;
+                }
+                case SDRATTR_3DOBJ_TEXTURE_PROJ_Y:
+                {
+                    rObj.InvalidateGeometry();
+                    break;
+                }
+            }
+        }
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
