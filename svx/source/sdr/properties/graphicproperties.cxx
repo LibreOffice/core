0dc43e61f828c106625f53c7d61e569969b333f5
Rüdiger Timm <rt@openoffice.org>
2003-11-24 15:50:15 +0000

    INTEGRATION: CWS aw003 (1.1.2); FILE ADDED 2003/10/07 12:14:22 aw 1.1.2.5: #111097# 2003/06/30 13:27:24 aw 1.1.2.4: #110094# 2003/06/06 12:59:25 aw 1.1.2.3: #109820# 2nd run of changes for ItemSet isolation 2003/05/26 12:53:54 aw 1.1.2.2: #109820# Removed all itemset handling stuff, migrated to new properties implementations completely. I had to change from base class Properties to BaseProperties due to a solaris compiler bug. 2003/05/21 11:31:54 aw 1.1.2.1: #109820#

---
+++ svx/source/sdr/properties/graphicproperties.cxx
@@ -0,0 +1,206 @@
+/*************************************************************************
+ *
+ *  $RCSfile: graphicproperties.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2003-11-24 16:50:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_PROPERTIES_GRAPHICPROPERTIES_HXX
+#include <svx/sdr/properties/graphicproperties.hxx>
+#endif
+
+#ifndef _SFXSTYLE_HXX
+#include <svtools/style.hxx>
+#endif
+
+#ifndef _SVDDEF_HXX
+#include <svddef.hxx>
+#endif
+
+#ifndef _EEITEM_HXX
+#include <eeitem.hxx>
+#endif
+
+#ifndef _SVDOGRAF_HXX
+#include <svdograf.hxx>
+#endif
+
+#define ITEMID_GRF_CROP 0
+
+#ifndef _SDGCPITM_HXX
+#include <sdgcpitm.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace properties
+    {
+        // create a new itemset
+        SfxItemSet& GraphicProperties::CreateObjectSpecificItemSet(SfxItemPool& rPool)
+        {
+            return *(new SfxItemSet(rPool,
+
+                // range from SdrAttrObj
+                SDRATTR_START, SDRATTRSET_SHADOW,
+                SDRATTRSET_OUTLINER, SDRATTRSET_MISC,
+                SDRATTR_TEXTDIRECTION, SDRATTR_TEXTDIRECTION,
+
+                // range from SdrGrafObj
+                SDRATTR_GRAF_FIRST, SDRATTRSET_GRAF,
+
+                // range from SdrTextObj
+                EE_ITEMS_START, EE_ITEMS_END,
+
+                // end
+                0, 0));
+        }
+
+        GraphicProperties::GraphicProperties(SdrObject& rObj)
+        :   RectangleProperties(rObj)
+        {
+        }
+
+        GraphicProperties::GraphicProperties(const GraphicProperties& rProps, SdrObject& rObj)
+        :   RectangleProperties(rProps, rObj)
+        {
+        }
+
+        GraphicProperties::~GraphicProperties()
+        {
+        }
+
+        BaseProperties& GraphicProperties::Clone(SdrObject& rObj) const
+        {
+            return *(new GraphicProperties(*this, rObj));
+        }
+
+        void GraphicProperties::ItemSetChanged(const SfxItemSet& rSet)
+        {
+            SdrGrafObj& rObj = (SdrGrafObj&)GetSdrObject();
+
+            // local changes
+            rObj.SetXPolyDirty();
+
+            // call parent
+            RectangleProperties::ItemSetChanged(rSet);
+        }
+
+        void GraphicProperties::SetStyleSheet(SfxStyleSheet* pNewStyleSheet, sal_Bool bDontRemoveHardAttr)
+        {
+            SdrGrafObj& rObj = (SdrGrafObj&)GetSdrObject();
+
+            // local changes
+            rObj.SetXPolyDirty();
+
+            // call parent
+            RectangleProperties::SetStyleSheet(pNewStyleSheet, bDontRemoveHardAttr);
+
+            // local changes
+            rObj.ImpSetAttrToGrafInfo();
+        }
+
+        void GraphicProperties::PreProcessSave()
+        {
+            // call parent
+            RectangleProperties::PreProcessSave();
+
+            // prepare SetItems for storage
+            const SfxItemSet& rSet = *mpItemSet;
+            const SfxItemSet* pParent = mpStyleSheet ? &(mpStyleSheet->GetItemSet()) : 0L;
+
+            SdrGrafSetItem aGrafAttr(rSet.GetPool());
+            aGrafAttr.GetItemSet().Put(rSet);
+            aGrafAttr.GetItemSet().SetParent(pParent);
+            mpItemSet->Put(aGrafAttr);
+        }
+
+        void GraphicProperties::PostProcessSave()
+        {
+            // call parent
+            RectangleProperties::PostProcessSave();
+
+            // remove SetItems from local itemset
+            if(mpItemSet)
+            {
+                mpItemSet->ClearItem(SDRATTRSET_GRAF);
+            }
+        }
+
+        void GraphicProperties::ForceDefaultAttributes()
+        {
+            // call parent
+            RectangleProperties::ForceDefaultAttributes();
+
+            GetObjectItemSet();
+            mpItemSet->Put( SdrGrafLuminanceItem( 0 ) );
+            mpItemSet->Put( SdrGrafContrastItem( 0 ) );
+            mpItemSet->Put( SdrGrafRedItem( 0 ) );
+            mpItemSet->Put( SdrGrafGreenItem( 0 ) );
+            mpItemSet->Put( SdrGrafBlueItem( 0 ) );
+            mpItemSet->Put( SdrGrafGamma100Item( 100 ) );
+            mpItemSet->Put( SdrGrafTransparenceItem( 0 ) );
+            mpItemSet->Put( SdrGrafInvertItem( FALSE ) );
+            mpItemSet->Put( SdrGrafModeItem( GRAPHICDRAWMODE_STANDARD ) );
+            mpItemSet->Put( SdrGrafCropItem( 0, 0, 0, 0 ) );
+        }
+    } // end of namespace properties
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
