dba0b964adf1ba4f4778d9fe5a64666d17848c1e
Rüdiger Timm <rt@openoffice.org>
2003-12-01 12:31:54 +0000

    INTEGRATION: CWS vclcleanup01 (1.1.1.1.384); FILE MERGED 2003/11/28 07:32:53 mt 1.1.1.1.384.1: #i22952# Removed App Server code

--- vcl/source/window/accmgr.cxx
+++ vcl/source/window/accmgr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accmgr.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:05:40 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 13:31:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -78,7 +78,7 @@
 #include <accmgr.hxx>
 #endif
 
-#pragma hdrstop
+
 
 // =======================================================================
 
