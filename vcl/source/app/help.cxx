214f72e3a31feeba2e7d156e138adccc2c729b8f
Rüdiger Timm <rt@openoffice.org>
2003-12-01 12:05:23 +0000

    INTEGRATION: CWS vclcleanup01 (1.23.152); FILE MERGED 2003/11/28 07:32:22 mt 1.23.152.1: #i22952# Removed App Server code

--- vcl/source/app/help.cxx
+++ vcl/source/app/help.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: help.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 09:53:12 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 13:05:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -90,7 +90,7 @@
 
 #include <stdio.h>
 
-#pragma hdrstop
+
 
 
 // =======================================================================
