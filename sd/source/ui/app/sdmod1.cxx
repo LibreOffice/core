22589a83365138c8ac6b69031f0f25f98d355454
Rüdiger Timm <rt@openoffice.org>
2003-12-01 09:08:37 +0000

    INTEGRATION: CWS geordi2q10 (1.25.28); FILE MERGED 2003/11/26 16:09:30 rt 1.25.28.1: #111934#: join CWS draw20pp1

--- sd/source/ui/app/sdmod1.cxx
+++ sd/source/ui/app/sdmod1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sdmod1.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2003-11-05 14:34:36 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 10:08:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
