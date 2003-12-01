4d37ad40e759c73ea8d9c77f5182a46488eef7b3
Rüdiger Timm <rt@openoffice.org>
2003-12-01 17:25:01 +0000

    INTEGRATION: CWS ooo20031110 (1.27.146); FILE MERGED 2003/11/06 12:10:54 waratah 1.27.146.1: #i21906# remove unused inline function definition

--- sfx2/source/menu/mnumgr.cxx
+++ sfx2/source/menu/mnumgr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mnumgr.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-04 17:38:08 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 18:25:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -913,8 +913,6 @@ void SfxMenuManager::LeavePopup()
 //--------------------------------------------------------------------
 
 // executes the function for the selected item
-extern Select_Impl( void*, void* ) ;
-
 IMPL_LINK( SfxMenuManager, Select, Menu *, pMenu )
 {
     DBG_MEMTEST();
