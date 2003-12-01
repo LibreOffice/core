fe05f0ec1daf8c02af3f583e3a103424e595dc46
Rüdiger Timm <rt@openoffice.org>
2003-12-01 14:49:55 +0000

    INTEGRATION: CWS ooo20031110 (1.1.1.1.380); FILE MERGED 2003/11/04 10:44:13 waratah 1.1.1.1.380.1: #i21906# add class after friend definition required to remove permissive from gcc compile

--- svtools/source/dialogs/mcvmath.hxx
+++ svtools/source/dialogs/mcvmath.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mcvmath.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:58:58 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 15:49:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -107,8 +107,8 @@ class ColWheel;
 class Fix
 {
 private:
-    friend  FixCpx;
-    friend  ColWheel;
+    friend  class FixCpx;
+    friend  class ColWheel;
 
 //  friend  Fix ImpMultBig2( const Fix& a, const Fix& b );
 
