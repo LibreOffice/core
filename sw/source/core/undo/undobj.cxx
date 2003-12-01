b942e5ca27ac9e70abbc65f80d499518e5814166
Rüdiger Timm <rt@openoffice.org>
2003-12-01 08:41:35 +0000

    INTEGRATION: CWS geordi2q10 (1.8.298); FILE MERGED 2003/11/26 16:20:41 rt 1.8.298.1: #111934#: join CWS sw7pp1a

--- sw/source/core/undo/undobj.cxx
+++ sw/source/core/undo/undobj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: undobj.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 14:38:18 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 09:41:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -601,36 +601,50 @@ void SwUndoSaveCntnt::DelCntntIndex( const SwPosition& rMark,
                     }
                     break;
                 case FLY_AT_CNTNT:
-
-                    if( 0 != (pAPos = pAnchor->GetCntntAnchor() ) &&
-                        (( DELCNT_CHKNOCNTNT & nDelCntntType )
-                        ? ( pStt->nNode <= pAPos->nNode &&
-                            pAPos->nNode < pEnd->nNode )
-                        : ( bDelFwrd
-                            ? rMark.nNode < pAPos->nNode &&
-                                        pAPos->nNode <= rPoint.nNode
-                            : rPoint.nNode <= pAPos->nNode &&
-                                        pAPos->nNode < rMark.nNode )) )
                     {
-                        if( !pHistory )
-                            pHistory = new SwHistory;
+                        pAPos =  pAnchor->GetCntntAnchor();
 
-                        // nur den Anker verchieben ??
-                        if( !( DELCNT_CHKNOCNTNT & nDelCntntType ) &&
-                            rPoint.nNode.GetIndex() == pAPos->nNode.GetIndex())
-                        {
-                            pHistory->Add( *pFmt );
-
-                            SwFmtAnchor aAnch( *pAnchor );
-                            SwPosition aPos( rMark.nNode );
-                            aAnch.SetAnchor( &aPos );
-                            pFmt->SetAttr( aAnch );
-                        }
+                        bool bTmp;
+                        if (0 != pAPos &&
+                            ( DELCNT_CHKNOCNTNT & nDelCntntType ))
+                            bTmp = pStt->nNode <= pAPos->nNode &&
+                                pAPos->nNode < pEnd->nNode;
                         else
                         {
-                            pHistory->Add( *pFmt, nChainInsPos );
-                            // n wieder zurueck, damit nicht ein Format uebesprungen wird !
-                            n = n >= rSpzArr.Count() ? rSpzArr.Count() : n+1;
+                            if (bDelFwrd)
+                                bTmp = rMark.nNode < pAPos->nNode &&
+                                    pAPos->nNode <= rPoint.nNode;
+                            else
+                                bTmp = rPoint.nNode <= pAPos->nNode &&
+                                    pAPos->nNode < rMark.nNode;
+                        }
+
+                        if (bTmp)
+                        {
+                            if( !pHistory )
+                                pHistory = new SwHistory;
+
+                            // nur den Anker verchieben ??
+                            if( 0 != pAPos && // #i9456#
+                                ( DELCNT_CHKNOCNTNT & nDelCntntType ) &&
+                                rPoint.nNode.GetIndex() ==
+                                pAPos->nNode.GetIndex())
+                            {
+                                pHistory->Add( *pFmt );
+
+                                SwFmtAnchor aAnch( *pAnchor );
+                                SwPosition aPos( rMark.nNode );
+                                aAnch.SetAnchor( &aPos );
+                                pFmt->SetAttr( aAnch );
+                            }
+                            else
+                            {
+                                pHistory->Add( *pFmt, nChainInsPos );
+                                // n wieder zurueck, damit nicht ein
+                                // Format uebesprungen wird !
+                                n = n >= rSpzArr.Count() ?
+                                    rSpzArr.Count() : n+1;
+                            }
                         }
                     }
                     break;
