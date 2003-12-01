e89afad1326fa91e0438ee906d33241eadd18ebc
Rüdiger Timm <rt@openoffice.org>
2003-12-01 12:13:47 +0000

    INTEGRATION: CWS vclcleanup01 (1.5.366); FILE MERGED 2003/11/28 07:32:33 mt 1.5.366.1: #i22952# Removed App Server code

--- vcl/source/control/longcurr.cxx
+++ vcl/source/control/longcurr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: longcurr.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mt $ $Date: 2001-06-29 10:39:19 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 13:13:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -81,7 +81,7 @@
 #include <svdata.hxx>
 #include <longcurr.hxx>
 
-#pragma hdrstop
+
 
 #ifndef _UNOTOOLS_LOCALEDATAWRAPPER_HXX
 #include <unotools/localedatawrapper.hxx>
