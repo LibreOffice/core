16b84dc165026bfa100d3eb06d797f163a4cfe63
Rüdiger Timm <rt@openoffice.org>
2003-12-01 12:07:14 +0000

    INTEGRATION: CWS vclcleanup01 (1.1.1.1.382); FILE MERGED 2003/11/28 07:32:23 mt 1.1.1.1.382.1: #i22952# Removed App Server code

--- vcl/source/app/stdtext.cxx
+++ vcl/source/app/stdtext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: stdtext.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:05:35 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 13:07:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,7 +74,7 @@
 #include <stdtext.hxx>
 #endif
 
-#pragma hdrstop
+
 
 // =======================================================================
 
