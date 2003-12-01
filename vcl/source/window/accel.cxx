3bf080b38f2d0211ac7635f2028ea1b455ec9738
Rüdiger Timm <rt@openoffice.org>
2003-12-01 12:31:37 +0000

    INTEGRATION: CWS vclcleanup01 (1.1.1.1.384); FILE MERGED 2003/11/28 07:32:52 mt 1.1.1.1.384.1: #i22952# Removed App Server code

--- vcl/source/window/accel.cxx
+++ vcl/source/window/accel.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: accel.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:05:39 $
+ *  last change: $Author: rt $ $Date: 2003-12-01 13:31:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -84,7 +84,7 @@
 #include <rc.h>
 #endif
 
-#pragma hdrstop
+
 
 // =======================================================================
 
