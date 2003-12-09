5d24c67023bcead769842bd859d76ad44991ef09
Kurt Zenker <kz@openoffice.org>
2003-12-09 10:53:41 +0000

    INTEGRATION: CWS portlaoisefilterteam16 (1.3.22); FILE MERGED 2003/11/11 11:03:42 cmc 1.3.22.6: #i22206# ww6 documents with no charset set use the default charset implied by the base language set ? 2003/11/10 09:27:08 cmc 1.3.22.5: RESYNC: (1.3-1.4); FILE MERGED 2003/11/04 15:01:10 cmc 1.3.22.4: an Anachronism apparently 2003/10/31 18:01:30 cmc 1.3.22.3: #i21881# Add support for character styles as well as paragraph styles. Cunning (probably overcunning and it will bite me with some compiler or other) use made of template to have a single algorithm for char and para style mapping without all sorts of boolean flags and random hairy casting 2003/10/31 14:06:30 cmc 1.3.22.2: #i21881# remove dependancies of paragraph style mapping code on WW8 import filter classes, document the suckers. Move it all into one friendly utility class 2003/10/30 14:45:51 cmc 1.3.22.1: #i11717# make the effort to put humpy dumpty back together again if a section with a title page was imported from word see if our page follow formats are equivalent to a word section and export that as a single section. Split the various bits of code up into sensible self contained entities, and actually document the key bits.

--- sw/source/filter/ww8/writerwordglue.cxx
+++ sw/source/filter/ww8/writerwordglue.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: writerwordglue.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2003-11-05 14:15:32 $
+ *  last change: $Author: kz $ $Date: 2003-12-09 11:53:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,44 +66,346 @@
 #include <msfilter.hxx>
 #endif
 #ifndef SW_WRITERWORDGLUE
-#include "writerwordglue.hxx"
+#   include "writerwordglue.hxx"
+#endif
+#ifndef SW_MS_MSFILTER_HXX
+#   include "../inc/msfilter.hxx"
+#endif
+#ifndef SW_WRITERHELPER
+#   include "writerhelper.hxx"
 #endif
 
-#include <algorithm>        //std::find_if
-#include <functional>       //std::unary_function
+#include <algorithm>            //std::find_if
+#include <functional>           //std::unary_function
 
-#ifndef _SVX_PAPERINF_HXX
-#include <svx/paperinf.hxx> //lA0Width...
-#endif
 #ifndef _RTL_TENCINFO_H
-#include <rtl/tencinfo.h>   //rtl_getBestWindowsCharsetFromTextEncoding
+#   include <rtl/tencinfo.h>    //rtl_getBestWindowsCharsetFromTextEncoding
 #endif
-#ifndef _ERRHDL_HXX
-#include <errhdl.hxx>       // ASSERT()
+#ifndef _HINTIDS_HXX
+#   include <hintids.hxx>       //ITEMID_LRSPACE...
 #endif
+#ifndef _SVX_PAPERINF_HXX
+#   include <svx/paperinf.hxx>  //lA0Width...
+#endif
+#ifndef _SVX_LRSPITEM_HXX
+#   include <svx/lrspitem.hxx>  //SvxLRSpaceItem
+#endif
+#ifndef _SVX_ULSPITEM_HXX
+#   include <svx/ulspitem.hxx>  //SvxULSpaceItem
+#endif
+#ifndef _SVX_BOXITEM_HXX
+#   include <svx/boxitem.hxx>   //SvxBoxItem
+#endif
+#ifndef _FRMFMT_HXX
+#   include <frmfmt.hxx>        //SwFrmFmt
+#endif
+#ifndef _FMTCLDS_HXX
+#   include <fmtclds.hxx>       //SwFmtCol
+#endif
+#ifndef _SW_HF_EAT_SPACINGITEM_HXX
+#   include <hfspacingitem.hxx> //SwHeaderAndFooterEatSpacingItem
+#endif
+#ifndef _FMTFSIZE_HXX
+#   include <fmtfsize.hxx>      //SwFmtFrmSize
+#endif
+#ifndef _SWRECT_HXX
+#   include <swrect.hxx>        //SwRect
+#endif
+#ifndef _FMTHDFT_HXX
+#   include <fmthdft.hxx>       //SwFmtHeader/SwFmtFooter
+#endif
+#ifndef _POOLFMT_HXX
+#   include <poolfmt.hxx>       //RES_POOL_COLLFMT_TYPE
+#endif
+#ifndef _FRMATR_HXX
+#   include <frmatr.hxx>    //GetLRSpace...
+#endif
+
+namespace
+{
+    class closeenough : public std::unary_function<long, bool>
+    {
+    private:
+        long mnValue;
+        long mnWriggleRoom;
+    public:
+        closeenough(long nValue, long nWriggleRoom)
+            : mnValue(nValue), mnWriggleRoom(nWriggleRoom) {}
+        bool operator() (long nTest) const
+        {
+            return (
+                    (mnValue - nTest < mnWriggleRoom) &&
+                    (mnValue - nTest > -mnWriggleRoom)
+                   );
+        }
+    };
+
+    long CalcHdFtDist(const SwFrmFmt& rFmt, long nSpacing)
+    {
+        /*
+        #98506#
+        The normal case for reexporting word docs is to have dynamic spacing,
+        as this is word's only setting, and the reason for the existance of the
+        dynamic spacing features. If we have dynamic spacing active then we can
+        add its spacing to the value height of the h/f and get the wanted total
+        size for word.
+
+        Otherwise we have to get the real layout rendered
+        height, which is totally nonoptimum, but the best we can do.
+        */
+        long nDist=0;
+        const SwFmtFrmSize& rSz = rFmt.GetFrmSize();
+
+        const SwHeaderAndFooterEatSpacingItem &rSpacingCtrl =
+            sw::util::ItemGet<SwHeaderAndFooterEatSpacingItem>
+            (rFmt, RES_HEADER_FOOTER_EAT_SPACING);
+        if (rSpacingCtrl.GetValue())
+            nDist += rSz.GetHeight();
+        else
+        {
+            SwRect aRect(rFmt.FindLayoutRect(false));
+            if (aRect.Height())
+                nDist += aRect.Height();
+            else
+            {
+                const SwFmtFrmSize& rSz = rFmt.GetFrmSize();
+                if (ATT_VAR_SIZE != rSz.GetSizeType())
+                    nDist += rSz.GetHeight();
+                else
+                {
+                    nDist += 274;       // default for 12pt text
+                    nDist += nSpacing;
+                }
+            }
+        }
+        return nDist;
+    }
+
+    long CalcHdDist(const SwFrmFmt& rFmt)
+    {
+        return CalcHdFtDist(rFmt, rFmt.GetULSpace().GetUpper());
+    }
+
+    long CalcFtDist(const SwFrmFmt& rFmt)
+    {
+        return CalcHdFtDist(rFmt, rFmt.GetULSpace().GetLower());
+    }
+
+
+    /*
+     SwTxtFmtColl and SwCharFmt are quite distinct types and how they are
+     gotten is also distinct, but the algorithm to match word's eqivalents into
+     them is the same, so we put the different stuff into two seperate helper
+     implementations and a core template that uses the helpers that uses the
+     same algorithm to do the work. We'll make the helpers specializations of a
+     non existing template so I can let the compiler figure out the right one
+     to use from a simple argument to the algorithm class
+    */
+    template <class C> class MapperImpl;
+    template<> class MapperImpl<SwTxtFmtColl>
+    {
+    private:
+        SwDoc &mrDoc;
+    public:
+        MapperImpl(SwDoc &rDoc) : mrDoc(rDoc) {}
+        SwTxtFmtColl* GetBuiltInStyle(ww::sti eSti);
+        SwTxtFmtColl* GetStyle(const String &rName);
+        SwTxtFmtColl* MakeStyle(const String &rName);
+    };
+
+    SwTxtFmtColl* MapperImpl<SwTxtFmtColl>::GetBuiltInStyle(ww::sti eSti)
+    {
+        const RES_POOL_COLLFMT_TYPE RES_NONE  = RES_POOLCOLL_DOC_END;
+        static const RES_POOL_COLLFMT_TYPE aArr[]=
+        {
+            RES_POOLCOLL_STANDARD, RES_POOLCOLL_HEADLINE1,
+            RES_POOLCOLL_HEADLINE2, RES_POOLCOLL_HEADLINE3,
+            RES_POOLCOLL_HEADLINE4, RES_POOLCOLL_HEADLINE5,
+            RES_POOLCOLL_HEADLINE6, RES_POOLCOLL_HEADLINE7,
+            RES_POOLCOLL_HEADLINE8, RES_POOLCOLL_HEADLINE9,
+            RES_POOLCOLL_TOX_IDX1, RES_POOLCOLL_TOX_IDX2,
+            RES_POOLCOLL_TOX_IDX3, RES_NONE, RES_NONE, RES_NONE, RES_NONE,
+            RES_NONE, RES_NONE, RES_POOLCOLL_TOX_CNTNT1,
+            RES_POOLCOLL_TOX_CNTNT2, RES_POOLCOLL_TOX_CNTNT3,
+            RES_POOLCOLL_TOX_CNTNT4, RES_POOLCOLL_TOX_CNTNT5,
+            RES_POOLCOLL_TOX_CNTNT6, RES_POOLCOLL_TOX_CNTNT7,
+            RES_POOLCOLL_TOX_CNTNT8, RES_POOLCOLL_TOX_CNTNT9, RES_NONE,
+            RES_POOLCOLL_FOOTNOTE, RES_NONE, RES_POOLCOLL_HEADER,
+            RES_POOLCOLL_FOOTER, RES_POOLCOLL_TOX_IDXH, RES_NONE, RES_NONE,
+            RES_POOLCOLL_JAKETADRESS, RES_POOLCOLL_SENDADRESS, RES_NONE,
+            RES_NONE, RES_NONE, RES_NONE, RES_NONE, RES_POOLCOLL_ENDNOTE,
+            RES_NONE, RES_NONE, RES_NONE, RES_POOLCOLL_LISTS_BEGIN,
+            RES_NONE, RES_NONE, RES_NONE, RES_NONE, RES_NONE, RES_NONE,
+            RES_NONE, RES_NONE, RES_NONE, RES_NONE, RES_NONE, RES_NONE,
+            RES_NONE, RES_NONE, RES_POOLCOLL_DOC_TITEL, RES_NONE,
+            RES_POOLCOLL_SIGNATURE, RES_NONE, RES_POOLCOLL_TEXT,
+            RES_POOLCOLL_TEXT_MOVE, RES_NONE, RES_NONE, RES_NONE, RES_NONE,
+            RES_NONE, RES_NONE, RES_POOLCOLL_DOC_SUBTITEL
+        };
+
+        const size_t nArrSize = (sizeof(aArr) / sizeof(aArr[0]));
+        ASSERT(nArrSize == 75, "Style Array has false size");
+
+        SwTxtFmtColl* pRet = 0;
+        //If this is a built-in word style that has a built-in writer
+        //equivalent, then map it to one of our built in styles regardless
+        //of its name
+        if (eSti < nArrSize && aArr[eSti] != RES_NONE)
+            pRet = mrDoc.GetTxtCollFromPoolSimple(aArr[eSti], false);
+        return pRet;
+    }
+
+    SwTxtFmtColl* MapperImpl<SwTxtFmtColl>::GetStyle(const String &rName)
+    {
+        return sw::util::GetParaStyle(mrDoc, rName);
+    }
+
+    SwTxtFmtColl* MapperImpl<SwTxtFmtColl>::MakeStyle(const String &rName)
+    {
+        return mrDoc.MakeTxtFmtColl(rName,
+            const_cast<SwTxtFmtColl *>(mrDoc.GetDfltTxtFmtColl()));
+    }
+
+    template<> class MapperImpl<SwCharFmt>
+    {
+    private:
+        SwDoc &mrDoc;
+    public:
+        MapperImpl(SwDoc &rDoc) : mrDoc(rDoc) {}
+        SwCharFmt* GetBuiltInStyle(ww::sti eSti);
+        SwCharFmt* GetStyle(const String &rName);
+        SwCharFmt* MakeStyle(const String &rName);
+    };
+
+    SwCharFmt* MapperImpl<SwCharFmt>::GetBuiltInStyle(ww::sti eSti)
+    {
+        RES_POOL_CHRFMT_TYPE eLookup = RES_POOLCHR_NORMAL_END;
+        switch (eSti)
+        {
+            case ww::stiFtnRef:
+                eLookup = RES_POOLCHR_FOOTNOTE;
+                break;
+            case ww::stiLnn:
+                eLookup = RES_POOLCHR_LINENUM;
+                break;
+            case ww::stiPgn:
+                eLookup = RES_POOLCHR_PAGENO;
+                break;
+            case ww::stiEdnRef:
+                eLookup = RES_POOLCHR_ENDNOTE;
+                break;
+            case ww::stiHyperlink:
+                eLookup = RES_POOLCHR_INET_NORMAL;
+                break;
+            case ww::stiHyperlinkFollowed:
+                eLookup = RES_POOLCHR_INET_VISIT;
+                break;
+            case ww::stiStrong:
+                eLookup = RES_POOLCHR_HTML_STRONG;
+                break;
+            case ww::stiEmphasis:
+                eLookup = RES_POOLCHR_HTML_EMPHASIS;
+                break;
+            default:
+                eLookup = RES_POOLCHR_NORMAL_END;
+                break;
+        }
+        SwCharFmt *pRet = 0;
+        if (eLookup != RES_POOLCHR_NORMAL_END)
+            pRet = mrDoc.GetCharFmtFromPool(eLookup);
+        return pRet;
+    }
+
+    SwCharFmt* MapperImpl<SwCharFmt>::GetStyle(const String &rName)
+    {
+        return sw::util::GetCharStyle(mrDoc, rName);
+    }
+
+    SwCharFmt* MapperImpl<SwCharFmt>::MakeStyle(const String &rName)
+    {
+        return mrDoc.MakeCharFmt(rName, mrDoc.GetDfltCharFmt());
+    }
+
+    template<class C> class StyleMapperImpl
+    {
+    private:
+        MapperImpl<C> maHelper;
+        std::set<const C*> maUsedStyles;
+        C* MakeNonCollidingStyle(const String& rName);
+    public:
+        typedef std::pair<C*, bool> StyleResult;
+        StyleMapperImpl(SwDoc &rDoc) : maHelper(rDoc) {}
+        StyleResult GetStyle(const String& rName, ww::sti eSti);
+    };
+
+    template<class C>
+    typename StyleMapperImpl<C>::StyleResult
+    StyleMapperImpl<C>::GetStyle(const String& rName, ww::sti eSti)
+    {
+        C *pRet = maHelper.GetBuiltInStyle(eSti);
+
+        //If we've used it once, don't reuse it
+        if (pRet && (maUsedStyles.end() != maUsedStyles.find(pRet)))
+            pRet = 0;
+
+        if (!pRet)
+        {
+            pRet = maHelper.GetStyle(rName);
+            //If we've used it once, don't reuse it
+            if (pRet && (maUsedStyles.end() != maUsedStyles.find(pRet)))
+                pRet = 0;
+        }
+
+        bool bStyExist = pRet ? true : false;
+
+        if (!pRet)
+        {
+            String aName(rName);
+            xub_StrLen nPos = aName.Search(',');
+            // No commas allow in SW style names
+            if (STRING_NOTFOUND != nPos)
+                aName.Erase(nPos);
+            pRet = MakeNonCollidingStyle(aName);
+        }
+
+        if (pRet)
+            maUsedStyles.insert(pRet);
+
+        return StyleResult(pRet, bStyExist);
+    }
+
+    template<class C>
+    C* StyleMapperImpl<C>::MakeNonCollidingStyle(const String& rName)
+    {
+        String aName(rName);
+        C* pColl = 0;
+
+        if (pColl = maHelper.GetStyle(aName))
+        {
+            //If the style collides first stick WW- in front of it, unless
+            //it already has it and then successively add a larger and
+            //larger number after it, its got to work at some stage!
+            if (!aName.EqualsIgnoreCaseAscii("WW-", 0, 3))
+                aName.InsertAscii("WW-" , 0);
+
+            sal_Int32 nI = 1;
+            while (
+                    (pColl = maHelper.GetStyle(aName)) &&
+                    (nI < SAL_MAX_INT32)
+                  )
+            {
+                aName += String::CreateFromInt32(nI++);
+            }
+        }
+
+        return pColl ? 0 : maHelper.MakeStyle(aName);
+    }
+}
 
 namespace sw
 {
     namespace types
     {
-
-        class closeenough : public std::unary_function<long, bool>
-        {
-        private:
-            long mnValue;
-            long mnWriggleRoom;
-        public:
-            closeenough(long nValue, long nWriggleRoom)
-                : mnValue(nValue), mnWriggleRoom(nWriggleRoom) {}
-            bool operator() (long nTest) const
-            {
-                return (
-                        (mnValue - nTest < mnWriggleRoom) &&
-                        (mnValue - nTest > -mnWriggleRoom)
-                       );
-            }
-        };
-
         long SnapPageDimension(long nSize) throw()
         {
             static const long aSizes[] =
@@ -132,7 +434,114 @@ namespace sw
 
             return nSize;
         }
+    }
 
+    namespace util
+    {
+
+        bool IsPlausableSingleWordSection(const SwFrmFmt &rTitleFmt,
+            const SwFrmFmt &rFollowFmt)
+        {
+            bool bPlausableTitlePage = true;
+
+            const SwFmtCol& rFirstCols = rTitleFmt.GetCol();
+            const SwFmtCol& rFollowCols = rFollowFmt.GetCol();
+            const SwColumns& rFirstColumns = rFirstCols.GetColumns();
+            const SwColumns& rFollowColumns = rFollowCols.GetColumns();
+            const SvxLRSpaceItem &rOneLR = rTitleFmt.GetLRSpace();
+            const SvxLRSpaceItem &rTwoLR= rFollowFmt.GetLRSpace();
+
+            if (rFirstColumns.Count() != rFollowColumns.Count())
+            {
+                //e.g. #i4320#
+                bPlausableTitlePage = false;
+            }
+            else if (rOneLR != rTwoLR)
+                bPlausableTitlePage = false;
+            else
+            {
+                HdFtDistanceGlue aOne(rTitleFmt.GetAttrSet());
+                HdFtDistanceGlue aTwo(rFollowFmt.GetAttrSet());
+                //e.g. #i14509#
+                if (!aOne.EqualTopBottom(aTwo))
+                    bPlausableTitlePage = false;
+            }
+            return bPlausableTitlePage;
+        }
+
+        HdFtDistanceGlue::HdFtDistanceGlue(const SfxItemSet &rPage)
+        {
+            if (const SvxBoxItem *pBox = HasItem<SvxBoxItem>(rPage, RES_BOX))
+            {
+                dyaHdrTop = pBox->CalcLineSpace(BOX_LINE_TOP);
+                dyaHdrBottom = pBox->CalcLineSpace(BOX_LINE_BOTTOM);
+            }
+            else
+                dyaHdrTop = dyaHdrBottom = 0;
+            const SvxULSpaceItem &rUL =
+                ItemGet<SvxULSpaceItem>(rPage, RES_UL_SPACE);
+            dyaHdrTop += rUL.GetUpper();
+            dyaHdrBottom += rUL.GetLower();
+
+            dyaTop = dyaHdrTop;
+            dyaBottom = dyaHdrBottom;
+
+            const SwFmtHeader *pHd = HasItem<SwFmtHeader>(rPage, RES_HEADER);
+            if (pHd && pHd->IsActive() && pHd->GetHeaderFmt())
+            {
+                mbHasHeader = true;
+                dyaTop += CalcHdDist(*(pHd->GetHeaderFmt()));
+            }
+
+            const SwFmtFooter *pFt = HasItem<SwFmtFooter>(rPage, RES_FOOTER);
+            if (pFt && pFt->IsActive() && pFt->GetFooterFmt())
+            {
+                mbHasFooter = true;
+                dyaBottom += CalcFtDist(*(pFt->GetFooterFmt()));
+            }
+        }
+
+        bool HdFtDistanceGlue::EqualTopBottom(const HdFtDistanceGlue &rOther)
+            const
+        {
+            return (dyaTop == rOther.dyaTop && dyaBottom == rOther.dyaBottom);
+        }
+
+        ParaStyleMapper::ParaStyleMapper(SwDoc &rDoc)
+            : mpImpl(new StyleMapperImpl<SwTxtFmtColl>(rDoc))
+        {
+        }
+
+        ParaStyleMapper::~ParaStyleMapper()
+        {
+            delete mpImpl;
+        }
+
+        ParaStyleMapper::StyleResult ParaStyleMapper::GetStyle(
+            const String& rName, ww::sti eSti)
+        {
+            return mpImpl->GetStyle(rName, eSti);
+        }
+
+        CharStyleMapper::CharStyleMapper(SwDoc &rDoc)
+            : mpImpl(new StyleMapperImpl<SwCharFmt>(rDoc))
+        {
+        }
+
+        CharStyleMapper::~CharStyleMapper()
+        {
+            delete mpImpl;
+        }
+
+        CharStyleMapper::StyleResult CharStyleMapper::GetStyle(
+            const String& rName, ww::sti eSti)
+        {
+            return mpImpl->GetStyle(rName, eSti);
+        }
+    }
+
+    namespace ms
+    {
         sal_uInt8 rtl_TextEncodingToWinCharset(rtl_TextEncoding eTextEncoding)
         {
             sal_uInt8 nRet =
