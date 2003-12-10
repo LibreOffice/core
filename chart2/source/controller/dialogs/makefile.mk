66f1db6d60a0b96e73364b3c22bfbf77e5cda936
Ingrid Halama <iha@openoffice.org>
2003-12-10 17:03:21 +0000

    new Dialog for Position And Size

--- chart2/source/controller/dialogs/makefile.mk
+++ chart2/source/controller/dialogs/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: iha $ $Date: 2003-12-09 17:06:36 $
+#   last change: $Author: iha $ $Date: 2003-12-10 18:03:21 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,6 +88,7 @@ SLOFILES=   	$(SLO)$/tp_AxisLabel.obj \
                 $(SLO)$/dlg_ObjectProperties.obj \
                 $(SLO)$/dlg_ChartType.obj \
                 $(SLO)$/dlg_RotateDiagram.obj \
+                $(SLO)$/dlg_PositionAndSize.obj \
                 $(SLO)$/dlg_InsertAxis_Grid.obj \
                 $(SLO)$/dlg_InsertDataLabel.obj \
                 $(SLO)$/dlg_InsertLegend.obj \
