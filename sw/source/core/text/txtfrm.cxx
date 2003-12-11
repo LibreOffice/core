0774ab016524209ecb654867ffe812cd1127f7a5
Kurt Zenker <kz@openoffice.org>
2003-12-11 09:22:44 +0000

    INTEGRATION: CWS swq01 (1.65.30); FILE MERGED 2003/12/02 08:57:57 fme 1.65.30.2: RESYNC: (1.65-1.66); FILE MERGED 2003/11/27 09:01:34 fme 1.65.30.1: #109071# Provide fallback value for footnote line so that GetFtnLine does not have to invalidate in case that the are no paragraph information available

--- sw/source/core/text/txtfrm.cxx
+++ sw/source/core/text/txtfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: txtfrm.cxx,v $
  *
- *  $Revision: 1.66 $
+ *  $Revision: 1.67 $
  *
- *  last change: $Author: rt $ $Date: 2003-11-24 16:09:47 $
+ *  last change: $Author: kz $ $Date: 2003-12-11 10:22:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -430,6 +430,7 @@ void SwTxtFrm::InitCtor()
     nThisLines = 0;
     mnFlyAnchorOfst = 0;
     mnFlyAnchorOfstNoWrap = 0;
+    mnFtnLine = 0;
 
     nType = FRMC_TXT;
     bLocked = bFormatted = bWidow = bUndersized = bJustWidow =
