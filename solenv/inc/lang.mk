6f9eb440bdf0490524fda2870dae7cb0460d1e69
Vladimir Glazounov <vg@openoffice.org>
2003-12-17 17:05:05 +0000

    INTEGRATION: CWS geordi2q11 (1.11.100); FILE MERGED 2003/12/16 16:14:09 hr 1.11.100.1: #111934#: join CWS ooo111fix1

--- solenv/inc/lang.mk
+++ solenv/inc/lang.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: lang.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: hr $ $Date: 2003-04-28 16:42:06 $
+#   last change: $Author: vg $ $Date: 2003-12-17 18:05:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -79,7 +79,8 @@ pol$(LANG_GUI)=-CHARSET_microsoft-cp1250
 slovak$(LANG_GUI)=-CHARSET_UTF8
 russ$(LANG_GUI)=-CHARSET_microsoft-cp1251
 czech$(LANG_GUI)=-CHARSET_UTF8
-hung$(LANG_GUI)=-CHARSET_microsoft-cp1250
+slovenian$(LANG_GUI)=-CHARSET_UTF8
+hung$(LANG_GUI)=-CHARSET_UTF8
 japn$(LANG_GUI)=-CHARSET_UTF8
 chinsim$(LANG_GUI)=-CHARSET_UTF8
 chintrad$(LANG_GUI)=-CHARSET_UTF8
