2a90ac1aa17b57a384f3e77a21bad8e21f3c9668
Björn Milcke <bm@openoffice.org>
2003-12-17 13:31:17 +0000

    error

--- chart2/source/inc/LinearRegressionCurveCalculator.hxx
+++ chart2/source/inc/LinearRegressionCurveCalculator.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: LinearRegressionCurveCalculator.hxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: bm $ $Date: 2003-12-17 14:14:00 $
+ *  last change: $Author: bm $ $Date: 2003-12-17 14:31:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,9 +70,6 @@
 #endif
 
 namespace chart
-{
-
-namespace impl
 {
 
 class LinearRegressionCurveCalculator : public
