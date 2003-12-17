6162b430036045f97fb4eb0c1bb1dfef158c4c88
Vladimir Glazounov <vg@openoffice.org>
2003-12-17 16:46:34 +0000

    INTEGRATION: CWS geordi2q11 (1.2.56); FILE MERGED 2003/12/16 14:35:23 hr 1.2.56.1: #111934#: join CWS ooo111fix1

--- readlicense_oo/docs/readme/makefile.mk
+++ readlicense_oo/docs/readme/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2003-04-28 17:16:37 $
+#   last change: $Author: vg $ $Date: 2003-12-17 17:46:34 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -68,6 +68,7 @@ TARGET  = ooo
 
 .INCLUDE : settings.mk
 
+#SOLAR_JAVA=
 # --- Files --------------------------------------------------------
 
 READMEFILES = \
