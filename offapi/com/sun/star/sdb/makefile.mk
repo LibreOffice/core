b7e3bc4a4d688787b401d666479b447e444a8433
Vladimir Glazounov <vg@openoffice.org>
2003-12-19 10:05:41 +0000

    #100000# cvs problems workaround

--- offapi/com/sun/star/sdb/makefile.mk
+++ offapi/com/sun/star/sdb/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: hr $ $Date: 2003-04-04 17:55:19 $
+#   last change: $Author: vg $ $Date: 2003-12-19 11:05:41 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -138,6 +138,12 @@ IDLFILES=\
     XSQLErrorBroadcaster.idl \
     XSQLErrorListener.idl \
     XSQLQueryComposer.idl \
+    XSingleSelectQueryComposer.idl \
+    OrderColumn.idl \
+    SingleSelectQueryAnalyzer.idl \
+    SingleSelectQueryComposer.idl \
+    SQLFilterOperator.idl \
+    XSingleSelectQueryAnalyzer.idl \
     XSQLQueryComposerFactory.idl
 
 # ------------------------------------------------------------------
