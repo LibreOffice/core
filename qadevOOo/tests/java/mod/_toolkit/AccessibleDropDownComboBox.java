66b6d5e33d660d8eb5667a3dc7a0a170bb6edeb4
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:32:37 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/18 17:20:00 sw 1.4.18.2: #114449# 2003/12/18 14:44:26 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleDropDownComboBox.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleDropDownComboBox.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleDropDownComboBox.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Date: 2003-09-08 12:59:11 $
+ *  last change: $Date: 2004-01-05 20:32:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,16 +60,6 @@
  ************************************************************************/
 package mod._toolkit;
 
-import java.io.PrintWriter;
-
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.AccessibilityTools;
-import util.SOfficeFactory;
-import util.utils;
-
 import com.sun.star.accessibility.AccessibleRole;
 import com.sun.star.accessibility.XAccessible;
 import com.sun.star.accessibility.XAccessibleAction;
@@ -78,6 +68,18 @@ import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.text.XTextDocument;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+import util.AccessibilityTools;
+import util.SOfficeFactory;
+import util.utils;
+
 
 /**
  * Test for object which is represented by accessible component
@@ -117,7 +119,9 @@ public class AccessibleDropDownComboBox extends TestCase {
 
         AccessibilityTools at = new AccessibilityTools();
 
-        XWindow xWindow = at.getCurrentWindow( (XMultiServiceFactory) Param.getMSF(), xTextDoc);
+        XWindow xWindow = at.getCurrentWindow(
+                                  (XMultiServiceFactory) Param.getMSF(),
+                                  xTextDoc);
 
         XAccessible xRoot = at.getAccessibleObject(xWindow);
 
@@ -151,7 +155,8 @@ public class AccessibleDropDownComboBox extends TestCase {
      */
     protected void initialize(TestParameters Param, PrintWriter log) {
         try {
-            SOfficeFactory SOF = SOfficeFactory.getFactory((XMultiServiceFactory) Param.getMSF());
+            SOfficeFactory SOF = SOfficeFactory.getFactory(
+                                         (XMultiServiceFactory) Param.getMSF());
             xTextDoc = SOF.createTextDoc(null);
         } catch (com.sun.star.uno.Exception e) {
             throw new StatusException("Can't create document", e);
@@ -162,7 +167,7 @@ public class AccessibleDropDownComboBox extends TestCase {
      * Disposes writer document.
      */
     protected void cleanup(TestParameters Param, PrintWriter log) {
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
+        ;
     }
-
 }
