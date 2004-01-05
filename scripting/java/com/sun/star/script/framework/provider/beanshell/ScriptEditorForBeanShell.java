c82adfb53997508a121572a5e832e056c0207a4b
Rüdiger Timm <rt@openoffice.org>
2004-01-05 12:14:15 +0000

    INTEGRATION: CWS scriptingf2 (1.2.2); FILE MERGED 2003/12/16 13:54:39 npower 1.2.2.2: #i23335# Add code completion and syntax highlighting for BeanShell scripts Updates to editor to display NetBeansSourceView ( which includes sytax highlighting and code completion ) or PlainSourceView ( old beanshell editor window ) 2003/12/15 16:04:36 npower 1.2.2.1: #i22568#, #i22564# Updated edit classes to save ( direct support in ScriptEditorForBeanShell ) to document using embed Storage API. ScriptEditorForJavaScript modified to pass URL to rhino debugger.

--- scripting/java/com/sun/star/script/framework/provider/beanshell/ScriptEditorForBeanShell.java
+++ scripting/java/com/sun/star/script/framework/provider/beanshell/ScriptEditorForBeanShell.java
@@ -1,25 +1,73 @@
+/*************************************************************************
+*
+*  $RCSfile: ScriptEditorForBeanShell.java,v $
+*
+*  $Revision: 1.3 $
+*
+*  last change: $Author: rt $ $Date: 2004-01-05 13:14:15 $
+*
+*  The Contents of this file are made available subject to the terms of
+*  either of the following licenses
+*
+*         - GNU Lesser General Public License Version 2.1
+*         - Sun Industry Standards Source License Version 1.1
+*
+*  Sun Microsystems Inc., October, 2000
+*
+*  GNU Lesser General Public License Version 2.1
+*  =============================================
+*  Copyright 2000 by Sun Microsystems, Inc.
+*  901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License version 2.1, as published by the Free Software Foundation.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*  MA  02111-1307  USA
+*
+*
+*  Sun Industry Standards Source License Version 1.1
+*  =================================================
+*  The contents of this file are subject to the Sun Industry Standards
+*  Source License Version 1.1 (the "License"); You may not use this file
+*  except in compliance with the License. You may obtain a copy of the
+*  License at http://www.openoffice.org/license.html.
+*
+*  Software provided under this License is provided on an "AS IS" basis,
+*  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+*  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+*  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+*  See the License for the specific provisions governing your rights and
+*  obligations concerning the Software.
+*
+*  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+*
+*  Copyright: 2000 by Sun Microsystems, Inc.
+*
+*  All Rights Reserved.
+*
+*  Contributor(s): _______________________________________
+*
+*
+************************************************************************/
 package com.sun.star.script.framework.provider.beanshell;
 
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.text.Document;
-import javax.swing.event.DocumentListener;
-import javax.swing.event.DocumentEvent;
 
 import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -27,101 +75,86 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
+import java.net.URL;
 
-import drafts.com.sun.star.script.provider.XScriptContext;
 import bsh.Interpreter;
 
-public class ScriptEditorForBeanShell
-    implements ActionListener, DocumentListener {
+import drafts.com.sun.star.script.provider.XScriptContext;
+import com.sun.star.script.framework.provider.PathUtils;
+import com.sun.star.script.framework.browse.ScriptMetaData;
+
+
+public class ScriptEditorForBeanShell implements ActionListener {
 
     private JFrame frame;
-    private JTextArea ta;
-    private GlyphGutter gg;
+    private String filename;
+
+    private ScriptSourceModel model;
+    private ScriptSourceView view;
+
     private XScriptContext context;
-    private int currentPosition = -1;
-    private int linecount;
-    private Interpreter sessionInterpreter;
-    private Thread execThread = null;
-    private String filename = null;
-
-    public void edit(XScriptContext context, String path) {
-        go(context, path);
-    }
-
-    /* Entry point for script execution */
-    public void go(XScriptContext context, String filename) {
-        if (filename != null && filename != "") {
-            try {
-                FileInputStream fis = new FileInputStream(filename);
-                this.filename = filename;
-                go(context, fis);
-            }
-            catch (IOException ioe) {
-                JOptionPane.showMessageDialog(frame,
-                    "Error loading file: " + ioe.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
-
-    /* Entry point for script execution */
-    public void go(XScriptContext context, InputStream in) {
+    private URL scriptURL = null;
+    public void edit(XScriptContext context, ScriptMetaData entry) {
         this.context = context;
-        initUI();
-
-        if (in != null) {
+        if (entry != null ) {
             try {
-                loadFile(in);
+                String sUrl = entry.getParcelLocation();
+                if ( !sUrl.endsWith( "/" ) )
+                {
+                    sUrl += "/";
+                }
+                sUrl +=  entry.getLanguageName();
+                scriptURL = PathUtils.createScriptURL( sUrl );
+                this.filename = scriptURL.getFile();
             }
             catch (IOException ioe) {
-                JOptionPane.showMessageDialog(frame,
-                    "Error loading stream: " + ioe.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+
+                showErrorMessage( "Error loading file: " + ioe.getMessage() );
             }
         }
-    }
-
-    public void loadFile(InputStream in) throws IOException {
-
-        /* Remove ourselves as a DocumentListener while loading the file
-           so we don't get a storm of DocumentEvents during loading */
-        ta.getDocument().removeDocumentListener(this);
-
-        byte[] contents = new byte[1024];
-        int len = 0, pos = 0;
-
-        while ((len = in.read(contents, 0, 1024)) != -1) {
-            ta.insert(new String(contents, 0, len), pos);
-            pos += len;
-        }
+        this.model = new ScriptSourceModel(scriptURL);
 
         try {
-            in.close();
+            Class c = Class.forName(
+                "org.openoffice.netbeans.editor.NetBeansSourceView");
+
+            Class[] types = new Class[] { ScriptSourceModel.class };
+
+            java.lang.reflect.Constructor ctor = c.getConstructor(types);
+
+            if (ctor != null) {
+                Object[] args = new Object[] { this.model };
+                this.view = (ScriptSourceView) ctor.newInstance(args);
+            }
+            else {
+                this.view = new PlainSourceView(model);
+            }
         }
-        catch (IOException ignore) {
+        catch (java.lang.Error err) {
+            this.view = new PlainSourceView(model);
+        }
+        catch (Exception e) {
+            this.view = new PlainSourceView(model);
         }
 
-        /* Update the GlyphGutter and add back the DocumentListener */
-        gg.update();
-        ta.getDocument().addDocumentListener(this);
+        this.model.setView(this.view);
+        initUI();
+        frame.show();
+    }
+
+    public ScriptEditorForBeanShell() {
+    }
+
+    public void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(frame, message,
+            "Error", JOptionPane.ERROR_MESSAGE);
     }
 
     private void initUI() {
-        frame = new JFrame("BeanShell Debug Window");
-        ta = new JTextArea();
-        ta.setRows(15);
-        ta.setColumns(40);
-        ta.setLineWrap(false);
-        linecount = ta.getLineCount();
+        this.frame = new JFrame("BeanShell Debug Window");
 
-        gg = new GlyphGutter(this);
-
-        final JScrollPane sp = new JScrollPane();
-        sp.setViewportView(ta);
-        sp.setRowHeaderView(gg);
-
-        ta.getDocument().addDocumentListener(this);
         String[] labels = {"Run", "Clear", "Save", "Close"};
         JPanel p = new JPanel();
         p.setLayout(new FlowLayout());
@@ -136,74 +169,11 @@ public class ScriptEditorForBeanShell
             }
         }
 
-        frame.getContentPane().add(sp, "Center");
+        frame.getContentPane().add((JComponent)view, "Center");
         frame.getContentPane().add(p, "South");
         frame.pack();
-        frame.show();
-    }
-
-    /* Implementation of DocumentListener interface */
-    public void insertUpdate(DocumentEvent e) {
-        doChanged(e);
-    }
-
-    public void removeUpdate(DocumentEvent e) {
-        doChanged(e);
-    }
-
-    public void changedUpdate(DocumentEvent e) {
-        doChanged(e);
-    }
-
-    /* If the number of lines in the JTextArea has changed then update the
-       GlyphGutter */
-    public void doChanged(DocumentEvent e) {
-        if (linecount != ta.getLineCount()) {
-            gg.update();
-            linecount = ta.getLineCount();
-        }
-    }
-
-    private void startExecution() {
-        execThread = new Thread() {
-            public void run() {
-                Interpreter interpreter = new Interpreter();
-                interpreter.getNameSpace().clear();
-
-                // reset position and repaint gutter so no red arrow appears
-                currentPosition = -1;
-                gg.repaint();
-
-                try {
-                    interpreter.set("context", context);
-                    interpreter.eval(ta.getText());
-                }
-                catch (bsh.EvalError err) {
-                    currentPosition = err.getErrorLineNumber() - 1;
-                    try {
-                        // scroll to line of the error
-                        int line = ta.getLineStartOffset(currentPosition);
-                        Rectangle rect = ta.modelToView(line);
-                        ta.scrollRectToVisible(rect);
-                    }
-                    catch (Exception e) {
-                        // couldn't scroll to line, do nothing
-                    }
-                    gg.repaint();
-
-                    JOptionPane.showMessageDialog(frame, "Error at line " +
-                        String.valueOf(err.getErrorLineNumber()) +
-                        "\n\n: " + err.getErrorText(),
-                        "Error", JOptionPane.ERROR_MESSAGE);
-                }
-                catch (Exception e) {
-                    JOptionPane.showMessageDialog(frame,
-                        "Error: " + e.getMessage(),
-                        "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        };
-        execThread.start();
+        frame.setSize(590, 480);
+        frame.setLocation(300, 200);
     }
 
     private void promptForSaveName() {
@@ -233,38 +203,63 @@ public class ScriptEditorForBeanShell
     }
 
     private void saveTextArea() {
-        if (filename == null) {
-            promptForSaveName();
+        if (!view.isModified()) {
+            return;
         }
 
-        FileOutputStream fos = null;
-        if (filename != null) {
-            try {
-                File f = new File(filename);
-                fos = new FileOutputStream(f);
-                String s = ta.getText();
-                fos.write(s.getBytes(), 0, s.length());
+        /*if (filename == null) {
+            promptForSaveName();
+        }*/
+        OutputStream fos = null;
+        try {
+            String s = view.getText();
+            if ( scriptURL.getProtocol().equals("file") )
+            {
+                fos = new FileOutputStream( filename );
             }
-            catch (IOException ioe) {
-                JOptionPane.showMessageDialog(frame,
-                    "Error saving file: " + ioe.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+            else
+            {
+                fos = scriptURL.openConnection().getOutputStream();
             }
-            finally {
-                if (fos != null) {
-                    try {
+            if ( fos  != null) {
+                fos.write(s.getBytes());
+            }
+            else
+            {
+                showErrorMessage( "Error saving file: couldn't open stream for file" );
+            }
+
+       }
+       catch (IOException ioe) {
+           showErrorMessage( "Error saving file: " + ioe.getMessage() );
+        }
+       catch (Exception e) {
+           showErrorMessage( "Error saving file: " + e.getMessage() );
+       }
+        finally {
+            if (fos != null) {
+                try {
+                    fos.flush();
+                    if ( fos != null )
+                    {
                         fos.close();
                     }
-                    catch (IOException ignore) {
-                    }
+                }
+                catch (IOException ignore) {
                 }
             }
         }
+
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Run")) {
-            startExecution();
+            try {
+                model.execute(context);
+            }
+            catch (java.lang.reflect.InvocationTargetException ite) {
+                showErrorMessage(ite.getMessage());
+            }
         }
         else if (e.getActionCommand().equals("Close")) {
             frame.dispose();
@@ -273,109 +268,7 @@ public class ScriptEditorForBeanShell
             saveTextArea();
         }
         else if (e.getActionCommand().equals("Clear")) {
-            ta.setText("");
+            view.clear();
         }
-    }
-
-    public JTextArea getTextArea() {
-        return ta;
-    }
-
-    public int getCurrentPosition() {
-        return currentPosition;
     }
 }
-
-class GlyphGutter extends JComponent {
-
-    private ScriptEditorForBeanShell debugger;
-    private final String DUMMY_STRING = "99";
-
-    GlyphGutter(ScriptEditorForBeanShell debugger) {
-        this.debugger = debugger;
-        update();
-    }
-
-    public void update() {
-        JTextArea textArea = debugger.getTextArea();
-        Font font = textArea.getFont();
-        setFont(font);
-
-        FontMetrics metrics = getFontMetrics(font);
-        int h = metrics.getHeight();
-        int lineCount = textArea.getLineCount() + 1;
-
-        String dummy = Integer.toString(lineCount);
-        if (dummy.length() < 2) {
-            dummy = DUMMY_STRING;
-        }
-
-        Dimension d = new Dimension();
-        d.width = metrics.stringWidth(dummy) + 16;
-        d.height = lineCount * h + 100;
-        setPreferredSize(d);
-        setSize(d);
-    }
-
-    public void paintComponent(Graphics g) {
-        JTextArea textArea = debugger.getTextArea();
-
-        Font font = textArea.getFont();
-        g.setFont(font);
-
-        FontMetrics metrics = getFontMetrics(font);
-        Rectangle clip = g.getClipBounds();
-
-        g.setColor(getBackground());
-        g.fillRect(clip.x, clip.y, clip.width, clip.height);
-
-        int ascent = metrics.getMaxAscent();
-        int h = metrics.getHeight();
-        int lineCount = textArea.getLineCount() + 1;
-
-        int startLine = clip.y / h;
-        int endLine = (clip.y + clip.height) / h + 1;
-        int width = getWidth();
-        if (endLine > lineCount) {
-            endLine = lineCount;
-        }
-
-        for (int i = startLine; i < endLine; i++) {
-            String text;
-            text = Integer.toString(i + 1) + " ";
-            int w = metrics.stringWidth(text);
-            int y = i * h;
-            g.setColor(Color.blue);
-            g.drawString(text, 0, y + ascent);
-            int x = width - ascent;
-
-            // if currentPosition is not -1 then a red arrow will be drawn
-            if (i == debugger.getCurrentPosition()) {
-                drawArrow(g, ascent, x, y);
-            }
-        }
-    }
-
-    private void drawArrow(Graphics g, int ascent, int x, int y) {
-        Polygon arrow = new Polygon();
-        int dx = x;
-        y += ascent - 10;
-        int dy = y;
-        arrow.addPoint(dx, dy + 3);
-        arrow.addPoint(dx + 5, dy + 3);
-        for (x = dx + 5; x <= dx + 10; x++, y++) {
-            arrow.addPoint(x, y);
-        }
-        for (x = dx + 9; x >= dx + 5; x--, y++) {
-            arrow.addPoint(x, y);
-        }
-        arrow.addPoint(dx + 5, dy + 7);
-        arrow.addPoint(dx, dy + 7);
-
-        g.setColor(Color.red);
-        g.fillPolygon(arrow);
-        g.setColor(Color.black);
-        g.drawPolygon(arrow);
-    }
-};
-
