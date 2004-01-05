488bce31500e8cd10ac9cbb28e12f02c31227a77
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:33:10 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/16 09:14:36 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/XMLStylesImporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLStylesImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLStylesImporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:20:24 $
+ *  last change:$Date: 2004-01-05 19:33:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -131,7 +131,7 @@ public class XMLStylesImporter extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing document " );
-        comp.dispose();
+        util.DesktopTools.closeDoc(comp);
     }
 
     /**
