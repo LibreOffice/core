494d240963d5a4bdf183f917cdb49129b9987188
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:06:06 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/16 09:14:05 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScHeaderFieldsObj.java
+++ qadevOOo/tests/java/mod/_sc/ScHeaderFieldsObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScHeaderFieldsObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:09:59 $
+ *  last change:$Date: 2004-01-05 19:06:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -131,7 +131,7 @@ public class ScHeaderFieldsObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSpreadsheetDoc);
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
