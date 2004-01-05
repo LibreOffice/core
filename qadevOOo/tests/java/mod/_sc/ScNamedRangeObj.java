9daf8bd5406c56d92ff1c897a26f9004dd737be0
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:20:20 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/16 09:14:20 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScNamedRangeObj.java
+++ qadevOOo/tests/java/mod/_sc/ScNamedRangeObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScNamedRangeObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:14:58 $
+ *  last change:$Date: 2004-01-05 19:20:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -125,7 +125,7 @@ public class ScNamedRangeObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface(XComponent.class, xSheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
