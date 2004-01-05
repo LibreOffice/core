4a2b847b659ac31dd74da9ee2cb395bb9aacd6df
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:55:41 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/18 10:03:17 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_svx/SvxUnoTextRangeEnumeration.java
+++ qadevOOo/tests/java/mod/_svx/SvxUnoTextRangeEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SvxUnoTextRangeEnumeration.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:40:13 $
+ *  last change:$Date: 2004-01-05 19:55:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,7 @@ public class SvxUnoTextRangeEnumeration extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xDrawDoc " );
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);
     }
 
 
