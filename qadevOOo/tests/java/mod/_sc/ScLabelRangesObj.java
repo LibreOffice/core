edc8985ac09a3e8978178c42b116969856db4eaa
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:19:33 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/16 09:14:19 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScLabelRangesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScLabelRangesObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScLabelRangesObj.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:14:37 $
+ *  last change:$Date: 2004-01-05 19:19:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -122,7 +122,7 @@ public class ScLabelRangesObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
