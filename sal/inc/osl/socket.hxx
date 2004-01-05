1663a71dfe2b5ba2a7ab2c0c4cd6b8546d9d35d2
Oliver Bolte <obo@openoffice.org>
2004-01-05 20:18:35 +0000

    INTEGRATION: CWS qadev15 (1.3.220); FILE MERGED 2003/12/18 11:21:30 lla 1.3.220.1: #i23666# turner in variable assignment

--- sal/inc/osl/socket.hxx
+++ sal/inc/osl/socket.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: socket.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: jbu $ $Date: 2001-03-19 16:54:15 $
+ *  last change: $Author: obo $ $Date: 2004-01-05 21:18:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -209,7 +209,7 @@ namespace osl
         ::rtl::OUString hostname;
         oslSocketResult result = osl_getLocalHostname( &(hostname.pData) );
         if(pResult )
-            result = *pResult;
+            *pResult = result;
         return hostname;
     }
 
