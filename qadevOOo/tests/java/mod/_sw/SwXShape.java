19869d8975f1e3e426c5b562d1da97eba4c543d1
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:13:32 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:59:04 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXShape.java
+++ qadevOOo/tests/java/mod/_sw/SwXShape.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXShape.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:48:49 $
+ *  last change:$Date: 2004-01-05 20:13:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -125,7 +125,7 @@ public class SwXShape extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
