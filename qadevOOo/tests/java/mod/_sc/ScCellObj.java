e22dc8fbd07149f121507a4bc921fa94f08139e0
Oliver Bolte <obo@openoffice.org>
2004-01-05 17:52:44 +0000

    INTEGRATION: CWS qadev15 (1.5.8); FILE MERGED 2003/12/16 09:13:52 sw 1.5.8.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScCellObj.java
+++ qadevOOo/tests/java/mod/_sc/ScCellObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScCellObj.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2003-11-18 16:29:48 $
+ *  last change:$Date: 2004-01-05 18:52:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -150,7 +150,7 @@ public class ScCellObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSheetDoc);
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
