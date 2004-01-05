07941f22df2b40e34983f01c0ab6da8226f68be9
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:48:28 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/18 10:03:10 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_svx/SvxShapeCircle.java
+++ qadevOOo/tests/java/mod/_svx/SvxShapeCircle.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SvxShapeCircle.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:37:31 $
+ *  last change:$Date: 2004-01-05 19:48:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -177,7 +177,7 @@ public class SvxShapeCircle extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xDrawDoc " );
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);
     }
 
 
