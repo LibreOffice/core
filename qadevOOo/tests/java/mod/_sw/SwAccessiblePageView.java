1ebfa23f286d44e3517c548736f5fce7754a4fe0
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:58:53 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/17 08:58:50 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwAccessiblePageView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessiblePageView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessiblePageView.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $
+ *  last change: $Author: obo $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -231,7 +231,7 @@ public class SwAccessiblePageView extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("dispose text document");
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
--- qadevOOo/tests/java/mod/_sw/SwAccessibleParagraphView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessibleParagraphView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessibleParagraphView.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $
+ *  last change: $Author: obo $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -154,7 +154,7 @@ public class SwAccessibleParagraphView extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("dispose text document");
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
