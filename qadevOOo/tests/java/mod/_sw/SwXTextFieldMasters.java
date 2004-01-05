bbf80b247828c7a92402b1cdd37855c65827f1b8
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:18:49 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:59:09 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXTextFieldMasters.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextFieldMasters.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextFieldMasters.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:51:30 $
+ *  last change:$Date: 2004-01-05 20:18:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -126,7 +126,7 @@ public class SwXTextFieldMasters extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
 
