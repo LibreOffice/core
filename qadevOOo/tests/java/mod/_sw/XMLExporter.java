94f9a6ef4967bfbfeaf7a86343c4151be24db020
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:28:44 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:59:18 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/XMLExporter.java
+++ qadevOOo/tests/java/mod/_sw/XMLExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLExporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:56:03 $
+ *  last change:$Date: 2004-01-05 20:28:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -128,7 +128,7 @@ public class XMLExporter extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
--- qadevOOo/tests/java/mod/_sw/XMLImporter.java
+++ qadevOOo/tests/java/mod/_sw/XMLImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLImporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:56:19 $
+ *  last change:$Date: 2004-01-05 20:28:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -125,7 +125,7 @@ public class XMLImporter extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
