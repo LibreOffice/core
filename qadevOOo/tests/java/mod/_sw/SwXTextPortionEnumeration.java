60e454221689008015b52cc02512f17b67304626
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:21:53 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/17 08:59:12 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXTextPortionEnumeration.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextPortionEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextPortionEnumeration.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:52:57 $
+ *  last change:$Date: 2004-01-05 20:21:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -106,7 +106,7 @@ public class SwXTextPortionEnumeration extends TestCase {
 
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
