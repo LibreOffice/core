4d664e090297522e4d7e9f7741ac9bd7b4892340
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:24:47 +0000

    INTEGRATION: CWS qadev15 (1.4.8); FILE MERGED 2003/12/17 08:59:15 sw 1.4.8.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXTextTableCursor.java
+++ qadevOOo/tests/java/mod/_sw/SwXTextTableCursor.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTextTableCursor.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-11-18 16:31:07 $
+ *  last change:$Date: 2004-01-05 20:24:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,7 @@ public class SwXTextTableCursor extends TestCase {
 
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
 
