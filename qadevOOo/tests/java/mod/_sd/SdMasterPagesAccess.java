acaf930168e22dc2dcff4a02af65b890e273b025
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:37:49 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/18 08:21:00 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sd/SdMasterPagesAccess.java
+++ qadevOOo/tests/java/mod/_sd/SdMasterPagesAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdMasterPagesAccess.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:26:34 $
+ *  last change:$Date: 2004-01-05 19:37:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -119,7 +119,7 @@ public class SdMasterPagesAccess extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("disposing xDrawDoc");
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);;
     }
 
     /**
--- qadevOOo/tests/java/mod/_sd/SdPageLinkTargets.java
+++ qadevOOo/tests/java/mod/_sd/SdPageLinkTargets.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdPageLinkTargets.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:26:42 $
+ *  last change:$Date: 2004-01-05 19:37:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -120,7 +120,7 @@ public class SdPageLinkTargets extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("disposing xDrawDoc");
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);;
     }
 
     /**
