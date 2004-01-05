37893c1ed5e9858d60e9cab117f4e251c15868be
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:10:07 +0000

    INTEGRATION: CWS qadev15 (1.6.18); FILE MERGED 2003/12/17 08:59:00 sw 1.6.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXHeadFootText.java
+++ qadevOOo/tests/java/mod/_sw/SwXHeadFootText.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXHeadFootText.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change:$Date: 2003-09-08 12:46:29 $
+ *  last change:$Date: 2004-01-05 20:10:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -138,7 +138,7 @@ public class SwXHeadFootText extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
