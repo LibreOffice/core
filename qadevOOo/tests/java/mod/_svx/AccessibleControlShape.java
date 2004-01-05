f3780557e2c5574c01a11cd3098c55b6e15f1733
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:41:33 +0000

    INTEGRATION: CWS qadev15 (1.5.18); FILE MERGED 2003/12/18 10:03:02 sw 1.5.18.1: #114449#

--- qadevOOo/tests/java/mod/_svx/AccessibleControlShape.java
+++ qadevOOo/tests/java/mod/_svx/AccessibleControlShape.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleControlShape.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2003-09-08 12:35:02 $
+ *  last change:$Date: 2004-01-05 19:41:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -110,7 +110,7 @@ public class AccessibleControlShape extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xDrawDoc " );
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);
     }
 
     protected TestEnvironment createTestEnvironment
