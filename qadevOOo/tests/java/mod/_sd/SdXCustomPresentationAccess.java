599689056453908073504b44f4d13aaabdd343f9
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:39:56 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/18 08:21:02 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sd/SdXCustomPresentationAccess.java
+++ qadevOOo/tests/java/mod/_sd/SdXCustomPresentationAccess.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdXCustomPresentationAccess.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:27:25 $
+ *  last change:$Date: 2004-01-05 19:39:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -126,7 +126,7 @@ public class SdXCustomPresentationAccess extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("disposing xImpressDoc");
-        xImpressDoc.dispose();
+        util.DesktopTools.closeDoc(xImpressDoc);;
     }
 
     /**
