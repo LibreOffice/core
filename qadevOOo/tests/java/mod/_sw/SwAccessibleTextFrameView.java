843f76a443071d98a37037d980c96c4cdc24593e
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:01:08 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/17 08:58:52 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwAccessibleTextFrameView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessibleTextFrameView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessibleTextFrameView.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $
+ *  last change: $Author: obo $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -201,7 +201,7 @@ public class SwAccessibleTextFrameView extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("dispose text document");
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
