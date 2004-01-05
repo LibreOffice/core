6b87ee44abc8553ade77f13e766a2bd0ba969ab3
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:16:10 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/16 09:14:15 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SubTotalFieldsEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SubTotalFieldsEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_SubTotalFieldsEnumeration.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:13:16 $
+ *  last change:$Date: 2004-01-05 19:16:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -115,7 +115,7 @@ public class ScIndexEnumeration_SubTotalFieldsEnumeration extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSpreadsheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     protected synchronized TestEnvironment createTestEnvironment(TestParameters Param, PrintWriter log) {
