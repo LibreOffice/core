ca6411f1487fdbdbc01ee1c21309ea40e4b4e4f4
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:15:16 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:59:06 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXTableColumns.java
+++ qadevOOo/tests/java/mod/_sw/SwXTableColumns.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXTableColumns.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:49:43 $
+ *  last change:$Date: 2004-01-05 20:15:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,7 @@ public class SwXTableColumns extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
