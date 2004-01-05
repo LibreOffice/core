3cfa45959e5a019373de79d25b40ec1b9a3aedde
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:00:06 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/16 09:13:59 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScDataPilotTablesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotTablesObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDataPilotTablesObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:08:25 $
+ *  last change:$Date: 2004-01-05 18:59:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -129,7 +129,7 @@ public class ScDataPilotTablesObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface(XComponent.class, xSheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
