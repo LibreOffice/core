ad80b4c93970ea9657f4cd8bc6251b3e901fbfe4
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:05:52 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:58:56 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXDocumentIndexes.java
+++ qadevOOo/tests/java/mod/_sw/SwXDocumentIndexes.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXDocumentIndexes.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:44:40 $
+ *  last change:$Date: 2004-01-05 20:05:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -118,7 +118,7 @@ public class SwXDocumentIndexes extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
 
