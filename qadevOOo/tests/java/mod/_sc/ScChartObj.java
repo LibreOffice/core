65b01e50238a7ceb3a51e035839152b62174cfc8
Oliver Bolte <obo@openoffice.org>
2004-01-05 17:56:33 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/16 09:13:56 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScChartObj.java
+++ qadevOOo/tests/java/mod/_sc/ScChartObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScChartObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:07:01 $
+ *  last change:$Date: 2004-01-05 18:56:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -132,7 +132,7 @@ public class ScChartObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface(XComponent.class, xSheetDoc);
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
