0fcabb1ef4dfeae0e347468c4ce03a5ca0a471cf
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:13:55 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/17 08:59:04 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXStyle.java
+++ qadevOOo/tests/java/mod/_sw/SwXStyle.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXStyle.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:48:59 $
+ *  last change:$Date: 2004-01-05 20:13:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -127,7 +127,7 @@ public class SwXStyle extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
