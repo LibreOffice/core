636d8cd347c37568589794dfea95ab1e025de62b
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:12:55 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/16 09:14:12 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_LabelRangesEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_LabelRangesEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_LabelRangesEnumeration.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:12:05 $
+ *  last change:$Date: 2004-01-05 19:12:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -106,7 +106,7 @@ public class ScIndexEnumeration_LabelRangesEnumeration extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     protected synchronized TestEnvironment createTestEnvironment(TestParameters Param, PrintWriter log) {
