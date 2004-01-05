1d8e5e39a0130d7b0223ad992fd6c78764cfeac4
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:41:12 +0000

    INTEGRATION: CWS qadev15 (1.4.18); FILE MERGED 2003/12/18 08:21:03 sw 1.4.18.1: #114449#

--- qadevOOo/tests/java/mod/_sd/SdXShape.java
+++ qadevOOo/tests/java/mod/_sd/SdXShape.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdXShape.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:27:52 $
+ *  last change:$Date: 2004-01-05 19:41:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -131,7 +131,7 @@ public class SdXShape extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("disposing xDrawDoc");
-        xDrawDoc.dispose();
+        util.DesktopTools.closeDoc(xDrawDoc);;
     }
 
     /**
