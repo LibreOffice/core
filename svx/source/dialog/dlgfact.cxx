982a7b54a80d0f3438404e816365f952960c3e6b
Rüdiger Timm <rt@openoffice.org>
2004-01-05 10:31:42 +0000

    INTEGRATION: CWS dialogfactory (1.1.4); FILE ADDED 2003/12/16 09:25:12 mba 1.1.4.1: #i23574#: new dialogfactory

---
+++ svx/source/dialog/dlgfact.cxx
@@ -0,0 +1,165 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dlgfact.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-01-05 11:31:42 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "dlgfact.hxx"
+
+#include <sfx2/tabdlg.hxx>
+
+// class ResId
+#include <tools/rc.hxx>
+
+// AbstractTabDialog implementations just forwards everything to the dialog
+USHORT AbstractTabDialog::Execute()
+{
+    return pDlg->Execute();
+}
+
+void AbstractTabDialog::Show( BOOL bShow )
+{
+    pDlg->Show( bShow );
+}
+
+String AbstractTabDialog::GetData()
+{
+    return String();
+}
+
+void AbstractTabDialog::SetCurrentPageId( USHORT nId )
+{
+    pDlg->SetCurPageId( nId );
+}
+
+const SfxItemSet* AbstractTabDialog::GetOutputItemSet() const
+{
+    return pDlg->GetOutputItemSet();
+}
+
+AbstractTabDialog::~AbstractTabDialog()
+{
+    delete pDlg;
+}
+
+USHORT AbstractDialog::Execute()
+{
+    return pDlg->Execute();
+}
+
+void AbstractDialog::Show( BOOL bShow )
+{
+    pDlg->Show( bShow );
+}
+
+String AbstractDialog::GetData()
+{
+    return String();
+}
+
+AbstractDialog::~AbstractDialog()
+{
+    delete pDlg;
+}
+
+// Create dialogs with simplest interface
+VclAbstractDialog* AbstractDialogFactory::CreateVclDialog( Window* pParent, const ResId& rResId )
+{
+    Dialog* pDlg=NULL;
+    return 0;
+}
+
+// dialogs that use SfxBindings
+VclAbstractDialog* AbstractDialogFactory::CreateSfxDialog( Window* pParent, const SfxBindings& rBindings, const ResId& rResId )
+{
+    return 0;
+}
+
+// TabDialog outside the drawing layer
+SfxAbstractTabDialog* AbstractDialogFactory::CreateTabDialog( const ResId& rResId,
+                                                Window* pParent,
+                                                const SfxItemSet* pAttrSet,
+                                                SfxViewFrame* pViewFrame,
+                                                bool bEditFmt,
+                                                const String *pUserButtonText )
+{
+    SfxTabDialog* pDlg=NULL;
+    return 0;
+}
+
+// TabDialog that use functionality of the drawing layer
+SfxAbstractTabDialog* AbstractDialogFactory::CreateDialog( Window* pParent,
+                                            const SfxItemSet* pAttrSet,
+                                            const ResId& rResId,
+                                            SdrView* pView,
+                                            SdrModel* pModel )
+{
+    SfxTabDialog* pDlg=NULL;
+    return 0;
+}
+
+// Factories for TabPages
+CreateTabPage AbstractDialogFactory::GetTabPageCreatorFunc( USHORT nId )
+{
+    return 0;
+}
+
+GetTabPageRanges AbstractDialogFactory::GetTabPageRangesFunc( USHORT nId )
+{
+    return 0;
+}
