5ecb71cb62264a901d9013e02686f7d4c4b9c416
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:32:01 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/16 09:14:34 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/XMLSettingsExporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLSettingsExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLSettingsExporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:19:53 $
+ *  last change:$Date: 2004-01-05 19:32:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -129,7 +129,7 @@ public class XMLSettingsExporter extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xCalcDoc " );
-        xSheetDoc.dispose();
+        util.DesktopTools.closeDoc(xSheetDoc);
     }
 
     /**
