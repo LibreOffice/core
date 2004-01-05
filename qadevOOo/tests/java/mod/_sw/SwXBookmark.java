62cdaa80f477983c2d395f69f802c9014e295c8d
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:03:25 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:58:54 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/SwXBookmark.java
+++ qadevOOo/tests/java/mod/_sw/SwXBookmark.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXBookmark.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:43:24 $
+ *  last change:$Date: 2004-01-05 20:02:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -119,7 +119,7 @@ public class SwXBookmark extends TestCase {
     */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
         log.println( "    disposing xTextDoc " );
-        xTextDoc.dispose();
+        util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
