0823721ed7f6b152b9dba99944c51f681654b2de
Oliver Bolte <obo@openoffice.org>
2004-01-05 19:29:29 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/17 08:59:19 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sw/XMLMetaExporter.java
+++ qadevOOo/tests/java/mod/_sw/XMLMetaExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLMetaExporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:56:31 $
+ *  last change:$Date: 2004-01-05 20:29:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -129,7 +129,7 @@ public class XMLMetaExporter extends TestCase {
      */
     protected void cleanup( TestParameters tParam, PrintWriter log ) {
       log.println( "    disposing xTextDoc " );
-      xTextDoc.dispose();
+      util.DesktopTools.closeDoc(xTextDoc);
     }
 
     /**
