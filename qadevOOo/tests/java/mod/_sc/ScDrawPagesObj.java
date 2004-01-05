b0772957f4c58d957b1a4a9e6cb7c4311849e21a
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:01:51 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/16 09:14:01 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sc/ScDrawPagesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDrawPagesObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDrawPagesObj.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:09:07 $
+ *  last change:$Date: 2004-01-05 19:01:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -128,7 +128,7 @@ public class ScDrawPagesObj extends TestCase {
         log.println( "    disposing xSheetDoc " );
         XComponent oComp = (XComponent)
             UnoRuntime.queryInterface (XComponent.class, xSheetDoc) ;
-        oComp.dispose();
+        util.DesktopTools.closeDoc(oComp);
     }
 
     /**
