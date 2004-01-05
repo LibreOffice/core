209a934c6f4612ef428d1b0e475b7cbb69f3895b
Oliver Bolte <obo@openoffice.org>
2004-01-05 18:40:49 +0000

    INTEGRATION: CWS qadev15 (1.3.18); FILE MERGED 2003/12/18 08:21:03 sw 1.3.18.1: #114449#

--- qadevOOo/tests/java/mod/_sd/SdXPresentation.java
+++ qadevOOo/tests/java/mod/_sd/SdXPresentation.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SdXPresentation.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 12:27:44 $
+ *  last change:$Date: 2004-01-05 19:40:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -119,7 +119,7 @@ public class SdXPresentation extends TestCase {
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
         log.println("disposing xImpressDoc");
-        xImpressDoc.dispose();
+        util.DesktopTools.closeDoc(xImpressDoc);;
     }
 
     /**
