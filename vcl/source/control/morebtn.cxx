6b2f4bd97a74ed053f1e430d13e128cb73486066
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:23:15 +0000

    INTEGRATION: CWS vclcleanup02 (1.2.4); FILE MERGED 2003/12/17 16:03:53 mt 1.2.4.2: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings 2003/12/10 15:59:16 mt 1.2.4.1: #i23061# VCL cleanup, removed headers, methods and types...

--- vcl/source/control/morebtn.cxx
+++ vcl/source/control/morebtn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: morebtn.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 13:14:46 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:23:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,14 +59,12 @@
  *
  ************************************************************************/
 
-#define _SV_MOREBTN_CXX
-
 #ifndef _SV_MOREBTN_HXX
 #include <morebtn.hxx>
 #endif
 
 #ifndef _SV_RD_H
-#include <rc.h>
+#include <tools/rc.h>
 #endif
 
 
