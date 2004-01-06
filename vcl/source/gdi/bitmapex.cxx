16cb03fdd0dabf11de00fa49b27b9635f36ae8b1
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:29:58 +0000

    INTEGRATION: CWS vclcleanup02 (1.7.4); FILE MERGED 2003/12/17 16:04:09 mt 1.7.4.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bitmapex.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 13:17:22 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:29:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_BITMAPEX_CXX
 
 #ifndef _RTL_CRC_H_
 #include <rtl/crc.h>
