4144011873a71faa898d54c9d78c82d99d8006ab
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:15:29 +0000

    INTEGRATION: CWS vclcleanup02 (1.3.4); FILE MERGED 2003/12/17 16:03:31 mt 1.3.4.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings

--- vcl/source/app/timer.cxx
+++ vcl/source/app/timer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: timer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 13:09:18 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:15:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_TIMER_CXX
 
 #ifndef _SV_SVSYS_HXX
 #include <svsys.h>
