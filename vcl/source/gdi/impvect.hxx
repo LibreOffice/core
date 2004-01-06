f2148c8c3d7b4412adcfb7b841967335f383fb96
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:45:39 +0000

    INTEGRATION: CWS vclcleanup02 (1.1.1.1.386); FILE MERGED 2003/12/10 15:59:27 mt 1.1.1.1.386.1: #i23061# VCL cleanup, removed headers, methods and types...

--- vcl/source/gdi/impvect.hxx
+++ vcl/source/gdi/impvect.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: impvect.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:05:38 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:45:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,8 +62,8 @@
 #ifndef _SV_IMPVECT_HXX
 #define _SV_IMPVECT_HXX
 
-#ifndef _SV_POLY_HXX
-#include <poly.hxx>
+#ifndef _TL_POLY_HXX
+#include <tools/poly.hxx>
 #endif
 #ifndef _SV_GDIMTF_HXX
 #include <gdimtf.hxx>
