82b8b6898d17282cd9afed9d020da45d54165b2f
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 11:47:39 +0000

    INTEGRATION: CWS vclcleanup02 (1.3.384); FILE MERGED 2003/12/18 09:45:24 mt 1.3.384.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed some windows compiler warnings

--- vcl/aqua/source/gdi/salogl.cxx
+++ vcl/aqua/source/gdi/salogl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: salogl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pluby $ $Date: 2000-11-01 22:12:32 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 12:47:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_SALOGL_CXX
 
 #ifndef _SV_SALGDI_HXX
 #include <salgdi.hxx>
