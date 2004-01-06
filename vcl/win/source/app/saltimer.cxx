06a54225f4d891038e87d74c19cea6b4fdbf16b9
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 13:53:36 +0000

    INTEGRATION: CWS vclcleanup02 (1.3.12); FILE MERGED 2003/12/18 09:45:43 mt 1.3.12.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed some windows compiler warnings

--- vcl/win/source/app/saltimer.cxx
+++ vcl/win/source/app/saltimer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: saltimer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2003-11-18 14:51:01 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 14:53:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,8 +62,6 @@
 #ifndef _SVWIN_H
 #include <tools/svwin.h>
 #endif
-
-#define _SV_SALTIMER_CXX
 
 #ifndef _SV_SALDATA_HXX
 #include <saldata.hxx>
