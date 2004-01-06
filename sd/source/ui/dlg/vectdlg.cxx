80528a9614b8dd25070447032e31eea31fc5bd09
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 17:44:44 +0000

    INTEGRATION: CWS vclcleanup02 (1.3.322); FILE MERGED 2003/12/11 09:17:26 mt 1.3.322.1: #i23061# VCL cleanup, removed headers, methods and types...

--- sd/source/ui/dlg/vectdlg.cxx
+++ sd/source/ui/dlg/vectdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: vectdlg.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: thb $ $Date: 2001-06-15 18:16:50 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 18:44:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -75,8 +75,8 @@
 #include "vectdlg.hxx"
 #include "vectdlg.hrc"
 
-#ifndef _SV_CONFIG_HXX //autogen
-#include <vcl/config.hxx>
+#ifndef _CONFIG_HXX
+#include <tools/config.hxx>
 #endif
 #ifndef _SV_OCTREE_HXX //autogen
  #include <vcl/octree.hxx>
