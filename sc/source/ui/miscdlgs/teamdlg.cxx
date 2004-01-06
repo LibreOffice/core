9fca21ec7542c603ed5f5d49aa9a80754901a771
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 18:01:49 +0000

    INTEGRATION: CWS vclcleanup02 (1.2.348); FILE MERGED 2003/12/04 16:56:56 mt 1.2.348.1: #i23061# Removed/Changed old StarView stuff

--- sc/source/ui/miscdlgs/teamdlg.cxx
+++ sc/source/ui/miscdlgs/teamdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: teamdlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: nn $ $Date: 2001-05-22 12:17:33 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 19:01:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,7 +67,6 @@
 
 //------------------------------------------------------------------
 
-#include <vcl/system.hxx>
 #include <tools/shl.hxx>
 
 #include "teamdlg.hxx"
