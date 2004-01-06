2a054efb95fb33381ae1636020b0b158c7d27a86
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 18:26:00 +0000

    INTEGRATION: CWS vclcleanup02 (1.24.62); FILE MERGED 2003/12/11 09:07:20 mt 1.24.62.1: #i23061# VCL cleanup, removed headers, methods and types...

--- svtools/source/edit/svmedit.cxx
+++ svtools/source/edit/svmedit.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svmedit.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: obo $ $Date: 2003-09-04 07:46:03 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 19:26:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,7 +61,7 @@
 
 #include <memory>
 
-#include <vcl/rc.h>
+#include <tools/rc.h>
 #include <vcl/decoview.hxx>
 #include <vcl/svapp.hxx>
 
