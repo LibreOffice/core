230bd80dca36dfdee47379a12afd0e2e51a046a9
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 11:47:59 +0000

    INTEGRATION: CWS vclcleanup02 (1.5.376); FILE MERGED 2003/12/18 09:45:24 mt 1.5.376.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed some windows compiler warnings

--- vcl/aqua/source/gdi/salpixmaputils.cxx
+++ vcl/aqua/source/gdi/salpixmaputils.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: salpixmaputils.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: bmahbod $ $Date: 2001-03-12 23:15:32 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 12:47:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_SALPIXMAPUTILS_CXX
 
 #ifndef _SV_SALPIXMAPUTILS_HXX
     #include <salpixmaputils.hxx>
