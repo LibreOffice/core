9e67ceb7f5609c04807cee38837dfd3218f10bb7
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 11:46:03 +0000

    INTEGRATION: CWS vclcleanup02 (1.3.374); FILE MERGED 2003/12/18 09:45:21 mt 1.3.374.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed some windows compiler warnings

--- vcl/aqua/source/gdi/salcolorutils.cxx
+++ vcl/aqua/source/gdi/salcolorutils.cxx
@@ -2,8 +2,8 @@
  *
  *  $RCSfile: salcolorutils.cxx,v $
  *
- *  $Revision: 1.3 $
- *  last change: $Author: bmahbod $ $Date: 2001-03-12 23:15:32 $
+ *  $Revision: 1.4 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 12:46:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -57,8 +57,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_SALCOLORUTILS_CXX
 
 #ifndef _SV_SALCOLORUTILS_HXX
     #include <salcolorutils.hxx>
