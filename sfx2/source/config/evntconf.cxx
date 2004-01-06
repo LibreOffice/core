3a7330951916760e7acc7cc49c55046498e9318a
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 15:25:10 +0000

    INTEGRATION: CWS vclcleanup02 (1.16.158); FILE MERGED 2003/12/11 09:08:27 mt 1.16.158.1: #i23061# VCL cleanup, removed headers, methods and types...

--- sfx2/source/config/evntconf.cxx
+++ sfx2/source/config/evntconf.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: evntconf.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-26 08:28:45 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 16:25:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,7 @@
 #include <vcl/msgbox.hxx>
 #endif
 #ifndef _SV_RESARY_HXX
-#include <vcl/resary.hxx>
+#include <tools/resary.hxx>
 #endif
 #ifndef _SFXLSTNER_HXX //autogen
 #include <svtools/lstner.hxx>
