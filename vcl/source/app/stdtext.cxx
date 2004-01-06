5e203b40fe4bf7d80005eec69c1ca46f06e3e11c
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:13:14 +0000

    INTEGRATION: CWS vclcleanup02 (1.2.4); FILE MERGED 2003/12/17 16:03:26 mt 1.2.4.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings

--- vcl/source/app/stdtext.cxx
+++ vcl/source/app/stdtext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: stdtext.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 13:07:14 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:13:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_STDTEXT_CXX
 
 #ifndef _SV_SVIDS_HRC
 #include <svids.hrc>
