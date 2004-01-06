90d3529ab85ebeb33831c2e23e82a2fcf1dfcd7f
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 12:22:49 +0000

    INTEGRATION: CWS vclcleanup02 (1.2.4); FILE MERGED 2003/12/17 16:03:52 mt 1.2.4.2: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings 2003/12/10 15:59:16 mt 1.2.4.1: #i23061# VCL cleanup, removed headers, methods and types...

--- vcl/source/control/menubtn.cxx
+++ vcl/source/control/menubtn.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: menubtn.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 13:14:25 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 13:22:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,10 +59,8 @@
  *
  ************************************************************************/
 
-#define _SV_MENUBTN_CXX
-
 #ifndef _SV_RC_H
-#include <rc.h>
+#include <tools/rc.h>
 #endif
 #ifndef _SV_DECOVIEW_HXX
 #include <decoview.hxx>
