f6ea81a0f93b9d6962713786ba6e790adc4c1ac0
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 15:46:58 +0000

    INTEGRATION: CWS vclcleanup02 (1.6.88); FILE MERGED 2003/12/04 15:45:35 mt 1.6.88.1: #i23061# Removed/Changed old StarView stuff

--- dbaccess/source/ui/inc/TableGrantCtrl.hxx
+++ dbaccess/source/ui/inc/TableGrantCtrl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TableGrantCtrl.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2003-05-19 12:55:09 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 16:46:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -128,7 +128,7 @@ public:
 protected:
     virtual void Resize();
 
-    virtual long PreParentNotify(NotifyEvent& rNEvt );
+    virtual long PreNotify(NotifyEvent& rNEvt );
 
     virtual BOOL IsTabAllowed(BOOL bForward) const;
     virtual void InitController( ::svt::CellControllerRef& rController, long nRow, USHORT nCol );
