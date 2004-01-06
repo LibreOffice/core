7b2d4af3c7a4bf7b574710ce9d762b47f949e218
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 13:04:53 +0000

    INTEGRATION: CWS vclcleanup02 (1.4.148); FILE MERGED 2003/12/17 16:05:27 mt 1.4.148.1: #i23061# header cleanup, remove #ifdef ???_CXX and #define ???_CXX, also removed .impl files and fixed soke windows compiler warnings

--- vcl/source/window/cmdevt.cxx
+++ vcl/source/window/cmdevt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cmdevt.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 14:30:09 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 14:04:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define _SV_CMDEVT_CXX
 
 #ifndef _STRING_H
 #include <string.h>
