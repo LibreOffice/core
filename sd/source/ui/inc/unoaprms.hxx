03c0b755b4883a256b69e83c21d040af6f7f15cd
Vladimir Glazounov <vg@openoffice.org>
2004-01-06 17:47:31 +0000

    INTEGRATION: CWS vclcleanup02 (1.1.1.1.336); FILE MERGED 2003/12/11 09:17:30 mt 1.1.1.1.336.1: #i23061# VCL cleanup, removed headers, methods and types...

--- sd/source/ui/inc/unoaprms.hxx
+++ sd/source/ui/inc/unoaprms.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoaprms.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:42 $
+ *  last change: $Author: vg $ $Date: 2004-01-06 18:47:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -83,8 +83,8 @@
 #include <svx/svdopath.hxx>
 #endif
 
-#ifndef _SV_COLOR_HXX //autogen
-#include <vcl/color.hxx>
+#ifndef _TOOLS_COLOR_HXX
+#include <tools/color.hxx>
 #endif
 
 
