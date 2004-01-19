b509f86ebcfb59ee58c40103ff4a8decad58b63b
Kurt Zenker <kz@openoffice.org>
2004-01-19 17:27:03 +0000

    INTEGRATION: CWS sb12 (1.19.198); FILE MERGED 2003/12/17 15:59:08 sb 1.19.198.2: #i19971# Moved URI processing stuff to udkapi/com/sun/star/uri. 2003/12/17 08:35:55 sb 1.19.198.1: #i19971# New URI processing API.

--- offapi/com/sun/star/util/makefile.mk
+++ offapi/com/sun/star/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.19 $
+#   $Revision: 1.20 $
 #
-#   last change: $Author: rt $ $Date: 2003-04-17 14:21:31 $
+#   last change: $Author: kz $ $Date: 2004-01-19 18:27:03 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
