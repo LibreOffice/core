6139ad1683abdf99ceca1c1c2aeaf5beda70e157
Oliver Bolte <obo@openoffice.org>
2004-01-20 10:39:18 +0000

    INTEGRATION: CWS impress1 (1.1.2); FILE ADDED 2004/01/09 15:57:48 af 1.1.2.3: #111996# Using the FrameView directly in the constructors of the view shells. 2003/12/04 09:01:41 af 1.1.2.2: #111996# Re-inserted the pOldShell argument into the constructor. 2003/09/16 14:33:41 af 1.1.2.1: #1119996# Taken from presvish.hxx.

---
+++ sd/source/ui/inc/PresentationViewShell.hxx
@@ -0,0 +1,112 @@
+/*************************************************************************
+ *
+ *  $RCSfile: PresentationViewShell.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-01-20 11:39:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SD_PRESENTATION_VIEW_SHELL_HXX
+#define SD_PRESENTATION_VIEW_SHELL_HXX
+
+#ifndef SD_DRAW_VIEW_SHELL
+#include "DrawViewShell.hxx"
+#endif
+
+namespace sd {
+
+/** This view shell is responsible for showing the presentation of an
+    Impress document.
+*/
+class PresentationViewShell
+    : public DrawViewShell
+{
+public:
+    TYPEINFO();
+
+    SFX_DECL_VIEWFACTORY(PresViewShell);
+    SFX_DECL_INTERFACE( SD_IF_SDPRESVIEWSHELL );
+
+    /** Create a new view shell for the full screen presentation.
+        @param rViewShellBase
+            The new object will be stacked on this view shell base.
+        @param pFrameView
+            The frame view that makes it possible to pass information from
+            one view shell to the next.
+    */
+    PresentationViewShell (
+        SfxViewFrame* pFrame,
+        ViewShellBase& rViewShellBase,
+        FrameView* pFrameView);
+
+    static void CreateFullScreenShow(ViewShell* pOriginShell, SfxRequest& rReq );
+
+private:
+
+    Rectangle       maOldVisArea;
+    sal_Bool        mbShowStarted;
+
+
+    PresentationViewShell (SfxViewFrame* pFrame, const DrawViewShell& rShell);
+    virtual ~PresentationViewShell (void);
+
+    virtual void    Activate( BOOL bIsMDIActivate );
+    virtual void    Paint( const Rectangle& rRect, ::sd::Window* pWin );
+};
+
+} // end of namespace sd
+
+#endif
