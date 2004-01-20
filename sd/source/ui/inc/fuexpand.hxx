507c4740e92d5293992404a304b8581823f95bbd
Oliver Bolte <obo@openoffice.org>
2004-01-20 11:01:37 +0000

    INTEGRATION: CWS impress1 (1.1.1.1.262); FILE MERGED 2003/09/16 13:35:58 af 1.1.1.1.262.1: #111996# Introduction of namespace sd. Use of sub-shells.

--- sd/source/ui/inc/fuexpand.hxx
+++ sd/source/ui/inc/fuexpand.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fuexpand.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:39 $
+ *  last change: $Author: obo $ $Date: 2004-01-20 12:01:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,22 +59,31 @@
  *
  ************************************************************************/
 
-#ifndef _SD_FUEXPAND_HXX
-#define _SD_FUEXPAND_HXX
+#ifndef SD_FU_EXPAND_PAGE_HXX
+#define SD_FU_EXPAND_PAGE_HXX
 
-#ifndef _SD_FUPOOR_HXX
+#ifndef SD_FU_POOR_HXX
 #include "fupoor.hxx"
 #endif
 
-class FuExpandPage : public FuPoor
+namespace sd {
+
+class FuExpandPage
+    : public FuPoor
 {
  public:
     TYPEINFO();
 
-    FuExpandPage(SdViewShell* pViewSh, SdWindow* pWin, SdView* pView,
-            SdDrawDocument* pDoc, SfxRequest& rReq);
-    ~FuExpandPage() {}
+    FuExpandPage (
+        ViewShell* pViewSh,
+        ::sd::Window* pWin,
+        ::sd::View* pView,
+        SdDrawDocument* pDoc,
+        SfxRequest& rReq);
+    virtual ~FuExpandPage (void) {}
 };
 
-#endif // _SD_FUEXPAND_HXX
+} // end of namespace sd
+
+#endif
 
--- sd/source/ui/inc/fugrid.hxx
+++ sd/source/ui/inc/fugrid.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fugrid.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:39 $
+ *  last change: $Author: obo $ $Date: 2004-01-20 12:01:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,25 +59,35 @@
  *
  ************************************************************************/
 
-#ifndef _SD_FUGRID_HXX
-#define _SD_FUGRID_HXX
+#ifndef SD_FU_GRID_HXX
+#define SD_FU_GRID_HXX
 
-#ifndef _SD_FUPOOR_HXX
+#ifndef SD_FU_POOR_HXX
 #include "fupoor.hxx"
 #endif
 
-class FuGrid : public FuPoor
+namespace sd {
+
+class FuGrid
+    : public FuPoor
 {
- public:
+public:
     TYPEINFO();
 
-    FuGrid(SdViewShell* pViewSh, SdWindow* pWin, SdView* pView,
-            SdDrawDocument* pDoc, SfxRequest& rReq);
-    ~FuGrid() {}
+    FuGrid (
+        ViewShell* pViewSh,
+        ::sd::Window* pWin,
+        ::sd::View* pView,
+        SdDrawDocument* pDoc,
+        SfxRequest& rReq);
+    virtual ~FuGrid (void) {}
 
     virtual void Activate() {}         // Function aktivieren
     virtual void Deactivate() {}           // Function deaktivieren
 };
 
-#endif // _SD_FUGRID_HXX
+} // end of namespace sd
+
+#endif
+
 
