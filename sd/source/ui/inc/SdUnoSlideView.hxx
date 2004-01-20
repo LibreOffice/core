ddebd0ece0c568253217eb17d7148c71629d2214
Oliver Bolte <obo@openoffice.org>
2004-01-20 10:43:23 +0000

    INTEGRATION: CWS impress1 (1.3.228); FILE MERGED 2003/10/28 09:42:53 af 1.3.228.3: RESYNC: (1.3-1.4); FILE MERGED 2003/09/24 16:25:04 af 1.3.228.2: #111996# Modified architecture of controllers. 2003/09/16 13:39:00 af 1.3.228.1: #111996# Introduction of namespace sd. Use of sub-shells.

--- sd/source/ui/inc/SdUnoSlideView.hxx
+++ sd/source/ui/inc/SdUnoSlideView.hxx
@@ -1,6 +1,70 @@
-#ifndef _SD_UNOSLIDEVIEW_HXX
-#define _SD_UNOSLIDEVIEW_HXX
+/*************************************************************************
+ *
+ *  $RCSfile: SdUnoSlideView.hxx,v $
+ *
+ *  $Revision: 1.5 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-01-20 11:43:23 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 
+#ifndef SD_UNO_SLIDE_VIEW_HXX
+#define SD_UNO_SLIDE_VIEW_HXX
+
+#ifndef SD_DRAW_SUB_CONTROLLER_HXX
+#include "DrawSubController.hxx"
+#endif
 #ifndef _COM_SUN_STAR_DRAWING_XDRAWVIEW_HPP_
 #include <com/sun/star/drawing/XDrawView.hpp>
 #endif
@@ -31,142 +95,31 @@
 #include <svx/unoshape.hxx>
 #endif
 
-class SdView;
-class SdSlideViewShell;
-class SdXImpressDocument;
 
-struct SdUnoSlideViewBase
-{
-    osl::Mutex aMutex;
-};
+namespace sd {
+
+class View;
+class SlideViewShell;
+
 
 /**
  * This class implements the view component for a SdOutlineViewShell
  */
-class SdUnoSlideView :public SdUnoSlideViewBase,
-                        public ::cppu::OBroadcastHelper,
-                        public ::cppu::OPropertySetHelper,
-                        public ::com::sun::star::view::XSelectionSupplier,
-                        public ::com::sun::star::drawing::XDrawView,
-                        public ::com::sun::star::lang::XServiceInfo,
-                        public ::com::sun::star::awt::XWindow,
-                        public SfxBaseController
+class SdUnoSlideView
+    : public DrawSubController
 {
 public:
-    SdUnoSlideView(SdView* pSdView, SdSlideViewShell* pSdViewSh) throw();
+    SdUnoSlideView(View& rView, ViewShell& rViewShell) throw();
     virtual ~SdUnoSlideView() throw();
 
-    void fireSelectionChangeListener() throw();
-    void fireVisAreaChanged( const Rectangle& rVisArea ) throw();
-
-    // XInterface
-    virtual ::com::sun::star::uno::Any SAL_CALL queryInterface( const ::com::sun::star::uno::Type & rType ) throw(::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL acquire() throw();
-    virtual void SAL_CALL release() throw();
-
-    // XComponent
-    virtual void SAL_CALL dispose() throw( ::com::sun::star::uno::RuntimeException );
-    virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
-
     // XTypeProvider
-    virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type > SAL_CALL getTypes(  ) throw(::com::sun::star::uno::RuntimeException);
     virtual ::com::sun::star::uno::Sequence< sal_Int8 > SAL_CALL getImplementationId(  ) throw(::com::sun::star::uno::RuntimeException);
 
     // XServiceInfo
-    virtual ::rtl::OUString SAL_CALL getImplementationName() throw(::com::sun::star::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) throw(::com::sun::star::uno::RuntimeException);
-    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException);
-
-    // XSelectionSupplier
-    virtual sal_Bool SAL_CALL select( const ::com::sun::star::uno::Any& aSelection ) throw(::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
-    virtual ::com::sun::star::uno::Any SAL_CALL getSelection(  ) throw(::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addSelectionChangeListener( const ::com::sun::star::uno::Reference< ::com::sun::star::view::XSelectionChangeListener >& xListener ) throw(::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeSelectionChangeListener( const ::com::sun::star::uno::Reference< ::com::sun::star::view::XSelectionChangeListener >& xListener ) throw(::com::sun::star::uno::RuntimeException);
-
-    // XDrawView
-    virtual void SAL_CALL setCurrentPage( const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage >& xPage ) throw(::com::sun::star::uno::RuntimeException);
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage > SAL_CALL getCurrentPage(  ) throw(::com::sun::star::uno::RuntimeException);
-
-    // XPropertySet
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo(  ) throw(::com::sun::star::uno::RuntimeException);
-
-protected:
-    /**
-     * This method must return the name to index table. This table contains all property
-     * names and types of this object.
-     */
-    virtual ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelper();
-
-    /**
-     * Converted the value rValue and return the result in rConvertedValue and the
-     * old value in rOldValue. A IllegalArgumentException is thrown.
-     * The method is not implemented in this class. After this call the vetoable
-     * listeners are notified.
-     *
-     * @param rConvertedValue the converted value. Only set if return is true.
-     * @param rOldValue the old value. Only set if return is true.
-     * @param nHandle the handle of the proberty.
-     * @return true if the value converted.
-     */
-    virtual sal_Bool SAL_CALL convertFastPropertyValue(
-        ::com::sun::star::uno::Any & rConvertedValue,
-        ::com::sun::star::uno::Any & rOldValue,
-        sal_Int32 nHandle,
-        const ::com::sun::star::uno::Any& rValue )
-        throw (::com::sun::star::lang::IllegalArgumentException);
-    /**
-     * The same as setFastProperyValue, but no exception is thrown and nHandle
-     * is always valid. You must not broadcast the changes in this method.<BR>
-     * <B>You type is correct you need't test it.</B>
-     */
-    virtual void SAL_CALL setFastPropertyValue_NoBroadcast(
-        sal_Int32 nHandle,
-        const ::com::sun::star::uno::Any& rValue )
-        throw (::com::sun::star::uno::Exception);
-    /**
-     * The same as getFastProperyValue, but return the value through
-     * rValue and nHandle is always valid.
-     */
-    virtual void SAL_CALL getFastPropertyValue(
-        ::com::sun::star::uno::Any& rValue,
-        sal_Int32 nHandle ) const;
-
-    // XWindow
-    virtual void SAL_CALL setPosSize( sal_Int32 X, sal_Int32 Y, sal_Int32 Width, sal_Int32 Height, sal_Int16 Flags ) throw (::com::sun::star::uno::RuntimeException);
-    virtual ::com::sun::star::awt::Rectangle SAL_CALL getPosSize(  ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL setVisible( sal_Bool Visible ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL setEnable( sal_Bool Enable ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL setFocus(  ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addWindowListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindowListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeWindowListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindowListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addFocusListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeFocusListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addKeyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XKeyListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeKeyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XKeyListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addMouseListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeMouseListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addMouseMotionListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseMotionListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removeMouseMotionListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseMotionListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL addPaintListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XPaintListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL removePaintListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XPaintListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
-
-    SdXImpressDocument* getModel() const throw();
-
-    sal_Bool getMasterPageMode(void) const throw();
-    void setMasterPageMode(sal_Bool MasterPageMode_) throw();
-    sal_Bool getLayerMode(void) const throw();
-    void setLayerMode(sal_Bool LayerMode_) throw();
-
-private:
-    com::sun::star::uno::Reference< ::com::sun::star::awt::XWindow > getWindow();
-
-    SdView*             mpView;
-    SdSlideViewShell*   mpViewSh;
-
-    sal_Bool            mbDisposing;
-
-    Rectangle           maLastVisArea;
+    virtual ::rtl::OUString SAL_CALL getImplementationName()
+        throw(::com::sun::star::uno::RuntimeException);
 };
+
+} // end of namespace sd
 
 #endif
