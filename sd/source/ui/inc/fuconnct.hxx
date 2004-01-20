9f35a1c27f2fde67ff21a981f0a4a943df4eab15
Oliver Bolte <obo@openoffice.org>
2004-01-20 10:57:17 +0000

    INTEGRATION: CWS impress1 (1.1.1.1.262); FILE MERGED 2003/09/16 13:35:52 af 1.1.1.1.262.1: #111996# Introduction of namespace sd. Use of sub-shells.

--- sd/source/ui/inc/fuconnct.hxx
+++ sd/source/ui/inc/fuconnct.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fuconnct.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:48:38 $
+ *  last change: $Author: obo $ $Date: 2004-01-20 11:57:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,23 +59,31 @@
  *
  ************************************************************************/
 
+#ifndef SD_FU_CONNECTION_DLG_HXX
+#define SD_FU_CONNECTION_DLG_HXX
 
-#ifndef _SD_FUCONNCT_HXX
-#define _SD_FUCONNCT_HXX
-
-#ifndef _SD_FUPOOR_HXX
+#ifndef SD_FU_POOR_HXX
 #include "fupoor.hxx"
 #endif
 
-class FuConnectionDlg : public FuPoor
+namespace sd {
+
+class FuConnectionDlg
+    : public FuPoor
 {
- public:
+public:
     TYPEINFO();
 
-    FuConnectionDlg( SdViewShell* pViewSh, SdWindow* pWin, SdView* pView,
-                    SdDrawDocument* pDoc, SfxRequest& rReq);
-    ~FuConnectionDlg() {}
+    FuConnectionDlg (
+        ViewShell* pViewSh,
+        ::sd::Window* pWin,
+        ::sd::View* pView,
+        SdDrawDocument* pDoc,
+        SfxRequest& rReq);
+    virtual ~FuConnectionDlg (void) {}
 };
 
-#endif // _SD_FUCONNCT_HXX
+} // end of namespace sd
+
+#endif
 
