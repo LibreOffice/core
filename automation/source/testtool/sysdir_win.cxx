d0e34cffc72bbf51d820086fbcd472b1e2549a8f
Oliver Bolte <obo@openoffice.org>
2004-01-20 15:16:34 +0000

    INTEGRATION: CWS networker2 (1.1.2); FILE ADDED 2003/12/19 10:15:29 gh 1.1.2.1: #i22604#add new command GetCommonApplicationPath

---
+++ automation/source/testtool/sysdir_win.cxx
@@ -0,0 +1,129 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sysdir_win.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-01-20 16:16:34 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2002
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2002 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2002 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////
+////
+////                  Windows ONLY
+////
+////////////////////////////////////////////////////////////////////////////
+
+
+#include <tools/prewin.h>
+#ifndef _SHOBJ_H
+#include <shlobj.h>
+#endif
+#include <tools/postwin.h>
+// as we define it ourselves further down the line we remove it here
+#ifdef IS_ERROR
+    #undef IS_ERROR
+#endif
+
+#include <tchar.h>
+#include "sysdir_win.hxx"
+
+
+////////  copied this from setup2\win\source\system\winos.cxx
+
+void _SHFree( void *pv )
+{
+    IMalloc *pMalloc;
+    if( NOERROR == SHGetMalloc(&pMalloc) )
+    {
+        pMalloc->Free( pv );
+        pMalloc->Release();
+    }
+}
+
+#define ALLOC(type, n) ((type *) HeapAlloc(GetProcessHeap(), 0, sizeof(type) * n ))
+#define FREE(p) HeapFree(GetProcessHeap(), 0, p)
+
+UniString _SHGetSpecialFolder( int nFolderID )
+{
+
+    LPITEMIDLIST    pidl;
+    HRESULT         hHdl = SHGetSpecialFolderLocation( NULL, nFolderID, &pidl );
+    UniString       aFolder;
+
+    if( hHdl == NOERROR )
+    {
+        WCHAR *lpFolderA;
+        lpFolderA = ALLOC( WCHAR, 16000 );
+
+        SHGetPathFromIDListW( pidl, lpFolderA );
+        aFolder = UniString( lpFolderA );
+
+        FREE( lpFolderA );
+        _SHFree( pidl );
+    }
+    return aFolder;
+}
+
+
+/////////////// end of copy
+
+
+
+String _SHGetSpecialFolder_COMMON_APPDATA()
+{
+    return _SHGetSpecialFolder( CSIDL_COMMON_APPDATA );
+}
+
+
