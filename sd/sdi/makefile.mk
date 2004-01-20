befbfa84826527d17a59b8423d7cfc47b7726a96
Oliver Bolte <obo@openoffice.org>
2004-01-20 09:23:16 +0000

    INTEGRATION: CWS impress1 (1.2.226); FILE MERGED 2003/10/28 09:49:31 af 1.2.226.2: RESYNC: (1.2-1.3); FILE MERGED 2003/09/16 12:23:40 af 1.2.226.1: #111996# Added new file.

--- sd/sdi/makefile.mk
+++ sd/sdi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:13:57 $
+#   last change: $Author: obo $ $Date: 2004-01-20 10:23:16 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -93,6 +93,7 @@ SVSDI1DEPEND= \
     drviewsh.sdi \
     outlnvsh.sdi \
     slidvish.sdi \
+    ViewShellBase.sdi \
     sdslots.hrc \
     $(PRJ)$/inc$/app.hrc
 
@@ -109,6 +110,7 @@ SVSDI2DEPEND= \
     grviewsh.sdi \
     outlnvsh.sdi \
     slidvish.sdi \
+    ViewShellBase.sdi \
     sdslots.hrc \
     $(PRJ)$/inc$/app.hrc
 
