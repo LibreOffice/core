16cffe748bdf71760ec4eb430c688b52df82c198
Oliver Bolte <obo@openoffice.org>
2004-01-20 09:25:01 +0000

    INTEGRATION: CWS impress1 (1.1.2); FILE ADDED 2003/12/11 12:48:48 af 1.1.2.1: #111996# Moved class definition from drawdoc.hxx into its own file.

---
+++ sd/source/core/PageListWatcher.cxx
@@ -0,0 +1,235 @@
+/*************************************************************************
+ *
+ *  $RCSfile: PageListWatcher.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-01-20 10:25:01 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "PageListWatcher.hxx"
+
+#include "sdpage.hxx"
+#include <tools/debug.hxx>
+#include <svx/svdmodel.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// #109538#
+
+void ImpPageListWatcher::ImpRecreateSortedPageListOnDemand()
+{
+    // clear vectors
+    maPageVectorStandard.clear();
+    maPageVectorNotes.clear();
+    mpHandoutPage = 0L;
+
+    // build up vectors again
+    const sal_uInt32 nPageCount(ImpGetPageCount());
+
+    for(sal_uInt32 a(0L); a < nPageCount; a++)
+    {
+        SdPage* pCandidate = ImpGetPage(a);
+        DBG_ASSERT(pCandidate, "ImpPageListWatcher::ImpRecreateSortedPageListOnDemand: Invalid PageList in Model (!)");
+
+        switch(pCandidate->GetPageKind())
+        {
+            case PK_STANDARD:
+            {
+                maPageVectorStandard.push_back(pCandidate);
+                break;
+            }
+            case PK_NOTES:
+            {
+                maPageVectorNotes.push_back(pCandidate);
+                break;
+            }
+            case PK_HANDOUT:
+            {
+                DBG_ASSERT(!mpHandoutPage, "ImpPageListWatcher::ImpRecreateSortedPageListOnDemand: Two Handout pages in PageList of Model (!)");
+                mpHandoutPage = pCandidate;
+                break;
+            }
+        }
+    }
+
+    // set to valid
+    mbPageListValid = sal_True;
+}
+
+ImpPageListWatcher::ImpPageListWatcher(const SdrModel& rModel)
+:   mrModel(rModel),
+    mpHandoutPage(0L),
+    mbPageListValid(sal_False)
+{
+}
+
+ImpPageListWatcher::~ImpPageListWatcher()
+{
+}
+
+SdPage* ImpPageListWatcher::GetSdPage(PageKind ePgKind, sal_uInt32 nPgNum)
+{
+    SdPage* pRetval(0L);
+
+    if(!mbPageListValid)
+    {
+        ImpRecreateSortedPageListOnDemand();
+    }
+
+    switch(ePgKind)
+    {
+        case PK_STANDARD:
+        {
+            DBG_ASSERT(nPgNum <= maPageVectorStandard.size(), "ImpPageListWatcher::GetSdPage: access out of range (!)");
+            if (nPgNum>=0 && nPgNum<maPageVectorStandard.size())
+                pRetval = maPageVectorStandard[nPgNum];
+            break;
+        }
+        case PK_NOTES:
+        {
+            DBG_ASSERT(nPgNum <= maPageVectorNotes.size(), "ImpPageListWatcher::GetSdPage: access out of range (!)");
+            if (nPgNum>=0 && nPgNum<maPageVectorNotes.size())
+                pRetval = maPageVectorNotes[nPgNum];
+            break;
+        }
+        case PK_HANDOUT:
+        {
+            DBG_ASSERT(mpHandoutPage, "ImpPageListWatcher::GetSdPage: access to non existing handout page (!)");
+            DBG_ASSERT(nPgNum == 0L, "ImpPageListWatcher::GetSdPage: access to non existing handout page (!)");
+            if (nPgNum == 0)
+                pRetval = mpHandoutPage;
+            break;
+        }
+    }
+
+    return pRetval;
+}
+
+sal_uInt32 ImpPageListWatcher::GetSdPageCount(PageKind ePgKind)
+{
+    sal_uInt32 nRetval(0L);
+
+    if(!mbPageListValid)
+    {
+        ImpRecreateSortedPageListOnDemand();
+    }
+
+    switch(ePgKind)
+    {
+        case PK_STANDARD:
+        {
+            nRetval = maPageVectorStandard.size();
+            break;
+        }
+        case PK_NOTES:
+        {
+            nRetval = maPageVectorNotes.size();
+            break;
+        }
+        case PK_HANDOUT:
+        {
+            if(mpHandoutPage)
+            {
+                nRetval = 1L;
+            }
+
+            break;
+        }
+    }
+
+    return nRetval;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+sal_uInt32 ImpDrawPageListWatcher::ImpGetPageCount() const
+{
+    return (sal_uInt32)mrModel.GetPageCount();
+}
+
+SdPage* ImpDrawPageListWatcher::ImpGetPage(sal_uInt32 nIndex) const
+{
+    return (SdPage*)mrModel.GetPage((sal_uInt16)nIndex);
+}
+
+ImpDrawPageListWatcher::ImpDrawPageListWatcher(const SdrModel& rModel)
+:   ImpPageListWatcher(rModel)
+{
+}
+
+ImpDrawPageListWatcher::~ImpDrawPageListWatcher()
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+sal_uInt32 ImpMasterPageListWatcher::ImpGetPageCount() const
+{
+    return (sal_uInt32)mrModel.GetMasterPageCount();
+}
+
+SdPage* ImpMasterPageListWatcher::ImpGetPage(sal_uInt32 nIndex) const
+{
+    return (SdPage*)mrModel.GetMasterPage((sal_uInt16)nIndex);
+}
+
+ImpMasterPageListWatcher::ImpMasterPageListWatcher(const SdrModel& rModel)
+:   ImpPageListWatcher(rModel)
+{
+}
+
+ImpMasterPageListWatcher::~ImpMasterPageListWatcher()
+{
+}
