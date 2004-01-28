fb5c5b83eb17e999b3d6477b39ebe79316f0c8d9
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:11:38 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/12/18 05:18:01 as 1.1.2.6: #102620# seq. helper uses vector instead of deque 2003/11/04 10:45:45 as 1.1.2.5: #102620# split sequenceadapter into different files/classes 2003/08/12 05:09:31 as 1.1.2.4: #102620# correct build dependencies; update container queries 2003/08/04 08:23:10 as 1.1.2.3: #102620# convert flag names into values; change some constants; repair query 2003/08/04 05:44:05 as 1.1.2.2: #102620# new container queries 2003/08/01 07:21:36 as 1.1.2.1: first revison of moved filtercache and related services

---
+++ filter/source/config/cache/contenthandlerfactory.cxx
@@ -0,0 +1,238 @@
+/*************************************************************************
+ *
+ *  $RCSfile: contenthandlerfactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-01-28 15:11:38 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "contenthandlerfactory.hxx"
+#include "querytokenizer.hxx"
+#include "macros.hxx"
+#include "constant.hxx"
+#include "versions.hxx"
+
+//_______________________________________________
+// includes
+
+#ifndef _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
+#include <com/sun/star/lang/XInitialization.hpp>
+#endif
+
+#ifndef _COMPHELPER_ENUMHELPER_HXX_
+#include <comphelper/enumhelper.hxx>
+#endif
+
+//_______________________________________________
+// namespace
+
+namespace filter{
+    namespace config{
+
+namespace css = ::com::sun::star;
+
+//_______________________________________________
+// definitions
+
+/*-----------------------------------------------
+    09.07.2003 07:43
+-----------------------------------------------*/
+ContentHandlerFactory::ContentHandlerFactory(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR)
+{
+    BaseContainer::init(xSMGR                                                 ,
+                        ContentHandlerFactory::impl_getImplementationName()   ,
+                        ContentHandlerFactory::impl_getSupportedServiceNames(),
+                        FilterCache::E_CONTENTHANDLER                         );
+}
+
+/*-----------------------------------------------
+    09.07.2003 07:43
+-----------------------------------------------*/
+ContentHandlerFactory::~ContentHandlerFactory()
+{
+}
+
+/*-----------------------------------------------
+    16.07.2003 13:38
+-----------------------------------------------*/
+css::uno::Reference< css::uno::XInterface > SAL_CALL ContentHandlerFactory::createInstance(const ::rtl::OUString& sHandler)
+    throw(css::uno::Exception       ,
+          css::uno::RuntimeException)
+{
+    return createInstanceWithArguments(sHandler, css::uno::Sequence< css::uno::Any >());
+}
+
+/*-----------------------------------------------
+    16.07.2003 13:40
+-----------------------------------------------*/
+css::uno::Reference< css::uno::XInterface > SAL_CALL ContentHandlerFactory::createInstanceWithArguments(const ::rtl::OUString&                     sHandler  ,
+                                                                                                        const css::uno::Sequence< css::uno::Any >& lArguments)
+    throw(css::uno::Exception       ,
+          css::uno::RuntimeException)
+{
+    css::uno::Reference< css::uno::XInterface > xHandler;
+
+    // SAFE ->
+    ::osl::ResettableMutexGuard aLock(m_aLock);
+
+    ::rtl::OUString sRealHandler = sHandler;
+
+    #ifdef _FILTER_CONFIG_MIGRATION_Q_
+
+        /* -> TODO - HACK
+            check if the given handler name realy exist ...
+            Because our old implementation worked with an internal
+            type name instead of a handler name. For a small migration time
+            we must simulate this old feature :-( */
+
+        if (!m_rCache->hasItem(FilterCache::E_CONTENTHANDLER, sHandler) && m_rCache->hasItem(FilterCache::E_TYPE, sHandler))
+        {
+            _FILTER_CONFIG_LOG_("ContentHandlerFactory::createInstanceWithArguments() ... simulate old type search functionality!\n");
+
+            css::uno::Sequence< ::rtl::OUString > lTypes(1);
+            lTypes[0] = sHandler;
+
+            css::uno::Sequence< css::beans::NamedValue > lQuery(1);
+            lQuery[0].Name    = PROPNAME_TYPES;
+            lQuery[0].Value <<= lTypes;
+
+            css::uno::Reference< css::container::XEnumeration > xSet = createSubSetEnumerationByProperties(lQuery);
+            while(xSet->hasMoreElements())
+            {
+                ::comphelper::SequenceAsHashMap lHandlerProps(xSet->nextElement());
+                if (!(lHandlerProps[PROPNAME_NAME] >>= sRealHandler))
+                    continue;
+            }
+
+            // prevent outside code against NoSuchElementException!
+            // But dont implement such defensive strategy for our new create handling :-)
+            if (!m_rCache->hasItem(FilterCache::E_CONTENTHANDLER, sRealHandler))
+                return css::uno::Reference< css::uno::XInterface>();
+        }
+
+        /* <- HACK */
+
+    #endif // _FILTER_CONFIG_MIGRATION_Q_
+
+    // search handler on cache
+    CacheItem aHandler = m_rCache->getItem(FilterCache::E_CONTENTHANDLER, sRealHandler);
+
+    // create service instance
+    xHandler = m_xSMGR->createInstance(sRealHandler);
+
+    // initialize filter
+    css::uno::Reference< css::lang::XInitialization > xInit(xHandler, css::uno::UNO_QUERY);
+    if (xInit.is())
+    {
+        // format: lInitData[0] = seq<PropertyValue>, which contains all configuration properties of this handler
+        //         lInitData[1] = lArguments[0]
+        //         ...
+        //         lInitData[n] = lArguments[n-1]
+        css::uno::Sequence< css::beans::PropertyValue > lConfig;
+        aHandler >> lConfig;
+
+        ::comphelper::SequenceAsVector< css::uno::Any > stlArguments(lArguments);
+        stlArguments.insert(stlArguments.begin(), css::uno::makeAny(lConfig));
+
+        css::uno::Sequence< css::uno::Any > lInitData;
+        stlArguments >> lInitData;
+
+        xInit->initialize(lInitData);
+    }
+
+    return xHandler;
+    // <- SAFE
+}
+
+/*-----------------------------------------------
+    09.07.2003 07:46
+-----------------------------------------------*/
+css::uno::Sequence< ::rtl::OUString > SAL_CALL ContentHandlerFactory::getAvailableServiceNames()
+    throw(css::uno::RuntimeException)
+{
+    // must be the same list as ((XNameAccess*)this)->getElementNames() return!
+    return getElementNames();
+}
+
+/*-----------------------------------------------
+    09.07.2003 07:43
+-----------------------------------------------*/
+::rtl::OUString ContentHandlerFactory::impl_getImplementationName()
+{
+    return ::rtl::OUString::createFromAscii("com.sun.star.comp.filter.config.ContentHandlerFactory");
+}
+
+/*-----------------------------------------------
+    09.07.2003 07:43
+-----------------------------------------------*/
+css::uno::Sequence< ::rtl::OUString > ContentHandlerFactory::impl_getSupportedServiceNames()
+{
+    css::uno::Sequence< ::rtl::OUString > lServiceNames(1);
+    lServiceNames[0] = ::rtl::OUString::createFromAscii("com.sun.star.frame.ContentHandlerFactory");
+    return lServiceNames;
+}
+
+/*-----------------------------------------------
+    09.07.2003 07:43
+-----------------------------------------------*/
+css::uno::Reference< css::uno::XInterface > SAL_CALL ContentHandlerFactory::impl_createInstance(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR)
+{
+    ContentHandlerFactory* pNew = new ContentHandlerFactory(xSMGR);
+    return css::uno::Reference< css::uno::XInterface >(static_cast< css::lang::XMultiServiceFactory* >(pNew), css::uno::UNO_QUERY);
+}
+
+    } // namespace config
+} // namespace filter
