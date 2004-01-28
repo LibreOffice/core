cbd8ae9facc1c1e50255164789662d8ad72f86c1
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:15:02 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/04 10:45:46 as 1.1.2.2: #102620# split sequenceadapter into different files/classes 2003/08/01 07:21:41 as 1.1.2.1: first revison of moved filtercache and related services

---
+++ filter/source/config/cache/filterfactory.hxx
@@ -0,0 +1,197 @@
+/*************************************************************************
+ *
+ *  $RCSfile: filterfactory.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-01-28 15:15:02 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FILTER_CONFIG_FILTERFACTORY_HXX_
+#define __FILTER_CONFIG_FILTERFACTORY_HXX_
+
+//_______________________________________________
+// includes
+
+#include "basecontainer.hxx"
+
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XSingleServiceFactory.hpp>
+#endif
+
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+
+//_______________________________________________
+// namespace
+
+namespace filter{
+    namespace config{
+
+namespace css = ::com::sun::star;
+
+//_______________________________________________
+// definitions
+
+//_______________________________________________
+
+/** @short      implements the service <type scope="com.sun.star.document">FilterFactory</type>.
+ */
+class FilterFactory : public ::cppu::ImplInheritanceHelper1< BaseContainer                   ,
+                                                             css::lang::XMultiServiceFactory >
+{
+    //-------------------------------------------
+    // native interface
+
+    public:
+
+        //---------------------------------------
+        // ctor/dtor
+
+        /** @short  standard ctor to connect this interface wrapper to
+                    the global filter cache instance ...
+
+            @param  xSMGR
+                    reference to the uno service manager, which created this service instance.
+         */
+        FilterFactory(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR);
+
+        //---------------------------------------
+
+        /** @short  standard dtor.
+         */
+        virtual ~FilterFactory();
+
+    //-------------------------------------------
+    // uno interface
+
+    public:
+
+        //---------------------------------------
+        // XMultiServiceFactory
+
+        virtual css::uno::Reference< css::uno::XInterface > SAL_CALL createInstance(const ::rtl::OUString& sFilter)
+            throw(css::uno::Exception       ,
+                  css::uno::RuntimeException);
+
+        virtual css::uno::Reference< css::uno::XInterface > SAL_CALL createInstanceWithArguments(const ::rtl::OUString&                     sFilter   ,
+                                                                                                 const css::uno::Sequence< css::uno::Any >& lArguments)
+            throw(css::uno::Exception       ,
+                  css::uno::RuntimeException);
+
+        virtual css::uno::Sequence< ::rtl::OUString > SAL_CALL getAvailableServiceNames()
+            throw(css::uno::RuntimeException);
+
+        //---------------------------------------
+        // XContainerQuery
+
+        virtual css::uno::Reference< css::container::XEnumeration > SAL_CALL createSubSetEnumerationByQuery(const ::rtl::OUString& sQuery)
+            throw (css::uno::RuntimeException);
+
+    //-------------------------------------------
+    // static uno helper!
+
+    public:
+
+        //---------------------------------------
+
+        /** @short  return the uno implementation name of this class.
+
+            @descr  Because this information is used at several places
+                    (and mostly an object instance of this class is not possible)
+                    its implemented as a static function!
+
+            @return The fix uno implementation name of this class.
+         */
+        static ::rtl::OUString impl_getImplementationName();
+
+        //---------------------------------------
+
+        /** @short  return the list of supported uno services of this class.
+
+            @descr  Because this information is used at several places
+                    (and mostly an object instance of this class is not possible)
+                    its implemented as a static function!
+
+            @return The fix list of uno services supported by this class.
+         */
+        static css::uno::Sequence< ::rtl::OUString > impl_getSupportedServiceNames();
+
+        //---------------------------------------
+
+        /** @short  return a new intsnace of this class.
+
+            @descr  This method is used by the uno service manager, to create
+                    a new instance of this service if needed.
+
+            @param  xSMGR
+                    reference to the uno service manager, which require
+                    this new instance. It should be passed to the new object
+                    so it can be used internaly to create own needed uno resources.
+
+            @return The new instance of this service as an uno reference.
+         */
+        static css::uno::Reference< css::uno::XInterface > impl_createInstance(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR);
+};
+
+    } // namespace config
+} // namespace filter
+
+#endif // __FILTER_CONFIG_FILTERFACTORY_HXX_
