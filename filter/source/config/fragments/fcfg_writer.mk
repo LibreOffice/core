2f5d054d9fae0cbe3bf4c4fc6ac2d7a1a4da9636
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:29:16 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:21 as 1.1.2.5: #102620# repair dependencies 2003/11/04 10:55:47 as 1.1.2.4: #102620# rollback dependency fix 2003/10/28 14:04:40 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/15 06:44:27 as 1.1.2.2: #102620# support changed config format ... update packages 2003/10/02 12:44:52 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_writer.mk
@@ -0,0 +1,91 @@
+# -----------------------------------------------
+# count = 31
+T4_WRITER = \
+    writer_web_HTML \
+    writer_Lotus_1_2_3_10_DOS_StarWriter \
+    writer_Lotus_1_2_3_10_WIN_StarWriter \
+    calc_MS_Excel_40 \
+    calc_MS_Excel_5095 \
+    calc_MS_Excel_95 \
+    writer_MS_WinWord_5 \
+    writer_MS_WinWord_60 \
+    writer_MS_Word_95 \
+    writer_MS_Word_95_Vorlage \
+    writer_MS_Word_97 \
+    writer_MS_Word_97_Vorlage \
+    writer_Rich_Text_Format \
+    writer_StarOffice_XML_Writer \
+    writer_StarWriter_10 \
+    writer_StarWriter_20 \
+    writer_StarWriter_30 \
+    writer_StarWriter_30_VorlageTemplate \
+    writer_StarWriter_40 \
+    writer_StarWriter_40_VorlageTemplate \
+    writer_StarWriter_50 \
+    writer_StarWriter_50_VorlageTemplate \
+    writer_StarWriter_DOS \
+    writer_Text \
+    writer_Text_encoded \
+    writer_JustSystem_Ichitaro_10 \
+    writer_JustSystem_Ichitaro_10_template \
+    writer_MIZI_Hwp_97 \
+    writer_StarOffice_XML_Writer_Template \
+    writer_WPSSystem_WPS2000_10 \
+    pdf_Portable_Document_Format
+
+# -----------------------------------------------
+# count = 31
+F4_WRITER = \
+    HTML__StarWriter_ \
+    Lotus_1_2_3_1_0__DOS___StarWriter_ \
+    Lotus_1_2_3_1_0__WIN___StarWriter_ \
+    MS_Excel_4_0__StarWriter_ \
+    MS_Excel_5_0__StarWriter_ \
+    MS_Excel_95__StarWriter_ \
+    MS_WinWord_5 \
+    MS_WinWord_6_0 \
+    MS_Word_95 \
+    MS_Word_95_Vorlage \
+    MS_Word_97 \
+    MS_Word_97_Vorlage \
+    Rich_Text_Format \
+    StarOffice_XML__Writer_ \
+    StarWriter_1_0 \
+    StarWriter_2_0 \
+    StarWriter_3_0 \
+    StarWriter_3_0_Vorlage_Template \
+    StarWriter_4_0 \
+    StarWriter_4_0_Vorlage_Template \
+    StarWriter_5_0 \
+    StarWriter_5_0_Vorlage_Template \
+    StarWriter_DOS \
+    Text \
+    Text__encoded_ \
+    writer_JustSystem_Ichitaro_10 \
+    writer_JustSystem_Ichitaro_10_template \
+    writer_MIZI_Hwp_97 \
+    writer_StarOffice_XML_Writer_Template \
+    writer_WPSSystem_WPS2000_10 \
+    writer_pdf_Export
+
+# -----------------------------------------------
+# count = 0
+L4_WRITER =
+
+# -----------------------------------------------
+# count = 0
+C4_WRITER =
+
+# -----------------------------------------------
+TYPES_4fcfg_writer           = $(foreach,i,$(T4_WRITER) types$/$i.xcu          )
+FILTERS_4fcfg_writer         = $(foreach,i,$(F4_WRITER) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_writer    = $(foreach,i,$(L4_WRITER) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_writer = $(foreach,i,$(C4_WRITER) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_writer = \
+    $(TYPES_4fcfg_writer) \
+    $(FILTERS_4fcfg_writer) \
+    $(FRAMELOADERS_4fcfg_writer) \
+    $(CONTENTHANDLERS_4fcfg_writer)
