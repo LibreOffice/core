99f1202b6e27fc9c410ecba83ce9544853dfaa00
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:26:57 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:19 as 1.1.2.3: #102620# repair dependencies 2003/10/28 14:04:38 as 1.1.2.2: #102620# try new dependency mechanism 2003/10/20 12:34:46 as 1.1.2.1: #102620# new packages

---
+++ filter/source/config/fragments/fcfg_palm.mk
@@ -0,0 +1,31 @@
+# -----------------------------------------------
+# count = 1
+T4_PALM = \
+    writer_AportisDoc_PalmDB_File
+
+# -----------------------------------------------
+# count = 1
+F4_PALM = \
+    AportisDoc_Palm_DB
+
+# -----------------------------------------------
+# count = 0
+L4_PALM =
+
+# -----------------------------------------------
+# count = 0
+C4_PALM =
+
+# -----------------------------------------------
+TYPES_4fcfg_palm           = $(foreach,i,$(T4_PALM) types$/$i.xcu          )
+FILTERS_4fcfg_palm         = $(foreach,i,$(F4_PALM) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_palm    = $(foreach,i,$(L4_PALM) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_palm = $(foreach,i,$(C4_PALM) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_palm = \
+    $(TYPES_4fcfg_palm) \
+    $(FILTERS_4fcfg_palm) \
+    $(FRAMELOADERS_4fcfg_palm) \
+    $(CONTENTHANDLERS_4fcfg_palm)
