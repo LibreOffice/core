05f2bcdd090be8913cb886d2898d406db5d15e06
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:28:50 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:21 as 1.1.2.4: #102620# repair dependencies 2003/10/28 14:04:39 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/15 06:44:27 as 1.1.2.2: #102620# support changed config format ... update packages 2003/10/02 12:44:51 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_web.mk
@@ -0,0 +1,52 @@
+# -----------------------------------------------
+# count = 11
+T4_WEB = \
+    writer_web_HTML \
+    writer_StarWriter_30 \
+    writer_StarWriter_40 \
+    writer_StarWriter_50 \
+    writer_web_StarWriterWeb_40_VorlageTemplate \
+    writer_web_StarWriterWeb_50_VorlageTemplate \
+    writer_Text \
+    writer_web_HTML_help \
+    writer_StarOffice_XML_Writer \
+    writer_web_StarOffice_XML_Writer_Web_Template \
+    pdf_Portable_Document_Format
+
+# -----------------------------------------------
+# count = 12
+F4_WEB = \
+    HTML \
+    StarWriter_3_0__StarWriter_Web_ \
+    StarWriter_4_0__StarWriter_Web_ \
+    StarWriter_5_0__StarWriter_Web_ \
+    StarWriter_Web_4_0_Vorlage_Template \
+    StarWriter_Web_5_0_Vorlage_Template \
+    Text__StarWriter_Web_ \
+    Text__encoded___StarWriter_Web_ \
+    writer_web_HTML_help \
+    writer_web_StarOffice_XML_Writer \
+    writer_web_StarOffice_XML_Writer_Web_Template \
+    writer_web_pdf_Export
+
+# -----------------------------------------------
+# count = 0
+L4_WEB =
+
+# -----------------------------------------------
+# count = 0
+C4_WEB =
+
+# -----------------------------------------------
+TYPES_4fcfg_web           = $(foreach,i,$(T4_WEB) types$/$i.xcu          )
+FILTERS_4fcfg_web         = $(foreach,i,$(F4_WEB) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_web    = $(foreach,i,$(L4_WEB) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_web = $(foreach,i,$(C4_WEB) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_web = \
+    $(TYPES_4fcfg_web) \
+    $(FILTERS_4fcfg_web) \
+    $(FRAMELOADERS_4fcfg_web) \
+    $(CONTENTHANDLERS_4fcfg_web)
