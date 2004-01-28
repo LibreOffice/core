34947427e2a8e20bd3320f388abcadae9a2f9e08
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:22:29 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:18 as 1.1.2.4: #102620# repair dependencies 2003/10/28 14:04:35 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/15 06:44:25 as 1.1.2.2: #102620# support changed config format ... update packages 2003/10/02 12:44:32 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_chart.mk
@@ -0,0 +1,37 @@
+# -----------------------------------------------
+# count = 4
+T4_CHART = \
+    chart_StarChart_30 \
+    chart_StarChart_40 \
+    chart_StarChart_50 \
+    chart_StarOffice_XML_Chart
+
+# -----------------------------------------------
+# count = 4
+F4_CHART = \
+    StarChart_3_0 \
+    StarChart_4_0 \
+    StarChart_5_0 \
+    StarOffice_XML__Chart_
+
+# -----------------------------------------------
+# count = 0
+L4_CHART =
+
+# -----------------------------------------------
+# count = 0
+C4_CHART =
+
+# -----------------------------------------------
+TYPES_4fcfg_chart           = $(foreach,i,$(T4_CHART) types$/$i.xcu          )
+FILTERS_4fcfg_chart         = $(foreach,i,$(F4_CHART) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_chart    = $(foreach,i,$(L4_CHART) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_chart = $(foreach,i,$(C4_CHART) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_chart = \
+    $(TYPES_4fcfg_chart) \
+    $(FILTERS_4fcfg_chart) \
+    $(FRAMELOADERS_4fcfg_chart) \
+    $(CONTENTHANDLERS_4fcfg_chart)
