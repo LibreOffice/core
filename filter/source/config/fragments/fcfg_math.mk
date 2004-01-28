ed4a923de06ec3b4230e4ac8aea1b6067a2920e6
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:26:31 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:19 as 1.1.2.4: #102620# repair dependencies 2003/10/28 14:04:38 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/15 06:44:26 as 1.1.2.2: #102620# support changed config format ... update packages 2003/10/02 12:44:48 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_math.mk
@@ -0,0 +1,45 @@
+# -----------------------------------------------
+# count = 8
+T4_MATH = \
+    math_MathML_XML_Math \
+    math_MathType_3x \
+    math_StarMath_20 \
+    math_StarMath_30 \
+    math_StarMath_40 \
+    math_StarMath_50 \
+    math_StarOffice_XML_Math \
+    pdf_Portable_Document_Format
+
+# -----------------------------------------------
+# count = 8
+F4_MATH = \
+    MathML_XML__Math_ \
+    MathType_3_x \
+    StarMath_2_0 \
+    StarMath_3_0 \
+    StarMath_4_0 \
+    StarMath_5_0 \
+    StarOffice_XML__Math_ \
+    math_pdf_Export
+
+# -----------------------------------------------
+# count = 0
+L4_MATH =
+
+# -----------------------------------------------
+# count = 0
+C4_MATH =
+
+# -----------------------------------------------
+TYPES_4fcfg_math           = $(foreach,i,$(T4_MATH) types$/$i.xcu          )
+FILTERS_4fcfg_math         = $(foreach,i,$(F4_MATH) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_math    = $(foreach,i,$(L4_MATH) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_math = $(foreach,i,$(C4_MATH) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_math = \
+    $(TYPES_4fcfg_math) \
+    $(FILTERS_4fcfg_math) \
+    $(FRAMELOADERS_4fcfg_math) \
+    $(CONTENTHANDLERS_4fcfg_math)
