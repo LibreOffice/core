c76dd558dae5e400ce1793b020d8cf553b48d6c9
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:23:52 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:18 as 1.1.2.4: #102620# repair dependencies 2003/10/28 14:04:37 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/15 06:44:25 as 1.1.2.2: #102620# support changed config format ... update packages 2003/10/02 12:44:38 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_global.mk
@@ -0,0 +1,47 @@
+# -----------------------------------------------
+# count = 9
+T4_GLOBAL = \
+    writer_StarWriter_30 \
+    writer_StarWriter_40 \
+    writer_globaldocument_StarWriter_40GlobalDocument \
+    writer_StarWriter_50 \
+    writer_globaldocument_StarWriter_50GlobalDocument \
+    writer_Text \
+    writer_StarOffice_XML_Writer \
+    writer_globaldocument_StarOffice_XML_Writer_GlobalDocument \
+    pdf_Portable_Document_Format
+
+# -----------------------------------------------
+# count = 9
+F4_GLOBAL = \
+    StarWriter_3_0__StarWriter_GlobalDocument_ \
+    StarWriter_4_0__StarWriter_GlobalDocument_ \
+    StarWriter_4_0_GlobalDocument \
+    StarWriter_5_0__StarWriter_GlobalDocument_ \
+    StarWriter_5_0_GlobalDocument \
+    Text__encoded___StarWriter_GlobalDocument_ \
+    writer_globaldocument_StarOffice_XML_Writer \
+    writer_globaldocument_StarOffice_XML_Writer_GlobalDocument \
+    writer_globaldocument_pdf_Export
+
+# -----------------------------------------------
+# count = 0
+L4_GLOBAL =
+
+# -----------------------------------------------
+# count = 0
+C4_GLOBAL =
+
+# -----------------------------------------------
+TYPES_4fcfg_global           = $(foreach,i,$(T4_GLOBAL) types$/$i.xcu          )
+FILTERS_4fcfg_global         = $(foreach,i,$(F4_GLOBAL) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_global    = $(foreach,i,$(L4_GLOBAL) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_global = $(foreach,i,$(C4_GLOBAL) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_global = \
+    $(TYPES_4fcfg_global) \
+    $(FILTERS_4fcfg_global) \
+    $(FRAMELOADERS_4fcfg_global) \
+    $(CONTENTHANDLERS_4fcfg_global)
