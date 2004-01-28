4a5b4675bab231d991d6a793e479ae11a74314eb
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:30:16 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:22 as 1.1.2.3: #102620# repair dependencies 2003/10/28 14:04:41 as 1.1.2.2: #102620# try new dependency mechanism 2003/10/20 12:34:36 as 1.1.2.1: #102620# new packages

---
+++ filter/source/config/fragments/fcfg_xslt.mk
@@ -0,0 +1,35 @@
+# -----------------------------------------------
+# count = 3
+T4_XSLT = \
+    writer_DocBook_File \
+    writer_MS_Word_2003 \
+    writer_XHTML_File
+
+# -----------------------------------------------
+# count = 3
+F4_XSLT = \
+    DocBook_File \
+    Microsoft_Word_2003_XML \
+    XHTML_File
+
+# -----------------------------------------------
+# count = 0
+L4_XSLT =
+
+# -----------------------------------------------
+# count = 0
+C4_XSLT =
+
+# -----------------------------------------------
+TYPES_4fcfg_xslt           = $(foreach,i,$(T4_XSLT) types$/$i.xcu          )
+FILTERS_4fcfg_xslt         = $(foreach,i,$(F4_XSLT) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_xslt    = $(foreach,i,$(L4_XSLT) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_xslt = $(foreach,i,$(C4_XSLT) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_xslt = \
+    $(TYPES_4fcfg_xslt) \
+    $(FILTERS_4fcfg_xslt) \
+    $(FRAMELOADERS_4fcfg_xslt) \
+    $(CONTENTHANDLERS_4fcfg_xslt)
