762c3bac894708bd309d4425c90ec1ef7d68568e
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:22:59 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:18 as 1.1.2.4: #102620# repair dependencies 2003/10/28 14:04:36 as 1.1.2.3: #102620# try new dependency mechanism 2003/10/17 04:36:45 as 1.1.2.2: #102620# rework some packages 2003/10/02 12:44:37 as 1.1.2.1: #102620# first revision

---
+++ filter/source/config/fragments/fcfg_draw.mk
@@ -0,0 +1,43 @@
+# -----------------------------------------------
+# count = 7
+T4_DRAW = \
+    draw_StarDraw_30 \
+    draw_StarDraw_30_Vorlage \
+    draw_StarDraw_50 \
+    draw_StarDraw_50_Vorlage \
+    draw_StarOffice_XML_Draw \
+    draw_StarOffice_XML_Draw_Template \
+    pdf_Portable_Document_Format
+
+# -----------------------------------------------
+# count = 7
+F4_DRAW = \
+    StarDraw_3_0 \
+    StarDraw_3_0_Vorlage \
+    StarDraw_5_0 \
+    StarDraw_5_0_Vorlage \
+    StarOffice_XML__Draw_ \
+    draw_StarOffice_XML_Draw_Template \
+    draw_pdf_Export
+
+# -----------------------------------------------
+# count = 0
+L4_DRAW =
+
+# -----------------------------------------------
+# count = 0
+C4_DRAW =
+
+# -----------------------------------------------
+TYPES_4fcfg_draw           = $(foreach,i,$(T4_DRAW) types$/$i.xcu          )
+FILTERS_4fcfg_draw         = $(foreach,i,$(F4_DRAW) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_draw    = $(foreach,i,$(L4_DRAW) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_draw = $(foreach,i,$(C4_DRAW) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_draw = \
+    $(TYPES_4fcfg_draw) \
+    $(FILTERS_4fcfg_draw) \
+    $(FRAMELOADERS_4fcfg_draw) \
+    $(CONTENTHANDLERS_4fcfg_draw)
