0c3038ab24bdf0188f7e316aea437bcb4e5338b9
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:27:25 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/14 08:04:20 as 1.1.2.3: #102620# repair dependencies 2003/10/28 14:04:38 as 1.1.2.2: #102620# try new dependency mechanism 2003/10/20 12:34:54 as 1.1.2.1: #102620# new packages

---
+++ filter/source/config/fragments/fcfg_pocketexcel.mk
@@ -0,0 +1,31 @@
+# -----------------------------------------------
+# count = 1
+T4_POCKETEXCEL = \
+    calc_Pocket_Excel_File
+
+# -----------------------------------------------
+# count = 1
+F4_POCKETEXCEL = \
+    Pocket_Excel
+
+# -----------------------------------------------
+# count = 0
+L4_POCKETEXCEL =
+
+# -----------------------------------------------
+# count = 0
+C4_POCKETEXCEL =
+
+# -----------------------------------------------
+TYPES_4fcfg_pocketexcel           = $(foreach,i,$(T4_POCKETEXCEL) types$/$i.xcu          )
+FILTERS_4fcfg_pocketexcel         = $(foreach,i,$(F4_POCKETEXCEL) filters$/$i.xcu        )
+FRAMELOADERS_4fcfg_pocketexcel    = $(foreach,i,$(L4_POCKETEXCEL) frameloaders$/$i.xcu   )
+CONTENTHANDLERS_4fcfg_pocketexcel = $(foreach,i,$(C4_POCKETEXCEL) contenthandlers$/$i.xcu)
+
+# -----------------------------------------------
+# needed to get dependencies inside global makefile work!
+ALL_4fcfg_pocketexcel = \
+    $(TYPES_4fcfg_pocketexcel) \
+    $(FILTERS_4fcfg_pocketexcel) \
+    $(FRAMELOADERS_4fcfg_pocketexcel) \
+    $(CONTENTHANDLERS_4fcfg_pocketexcel)
