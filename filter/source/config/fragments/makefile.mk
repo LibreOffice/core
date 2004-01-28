4a5a1c980d3dac4d87838791f33ec0861485f128
Kurt Zenker <kz@openoffice.org>
2004-01-28 14:30:42 +0000

    INTEGRATION: CWS filtercfg (1.1.2); FILE ADDED 2003/11/21 13:43:43 as 1.1.2.12: #102620# use right includes 2003/11/14 08:04:22 as 1.1.2.11: #102620# repair dependencies 2003/11/04 10:55:47 as 1.1.2.10: #102620# rollback dependency fix 2003/10/28 14:04:42 as 1.1.2.9: #102620# try new dependency mechanism 2003/10/24 09:37:47 as 1.1.2.8: #102620# seperate frame loader & content handler again 2003/10/21 13:16:31 as 1.1.2.7: #102620# split TypeDetection package into different packages for types and filters 2003/10/20 12:39:23 as 1.1.2.6: #102620# add write support; new atomic file copy mechanism for Merger; new packages 2003/10/17 10:35:44 as 1.1.2.5: #102620# use right command alias 2003/10/15 06:44:28 as 1.1.2.4: #102620# support changed config format ... update packages 2003/10/14 06:37:29 as 1.1.2.3: #102620# content handler now property of type 2003/10/02 12:49:54 as 1.1.2.2: #102620# renamed packages 2003/09/26 06:30:28 as 1.1.2.1: #102620# new mechanism to build filter packages

---
+++ filter/source/config/fragments/makefile.mk
@@ -0,0 +1,147 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Date: 2004-01-28 15:30:42 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+PRJ     = ..$/..$/..
+TARGET  = FCFGPkg
+PRJNAME = $(TARGET)
+
+# -----------------------------------------------------------------------------
+# include all package definition files
+# -----------------------------------------------------------------------------
+
+.INCLUDE: fcfg_base.mk
+.INCLUDE: fcfg_writer.mk
+.INCLUDE: fcfg_web.mk
+.INCLUDE: fcfg_global.mk
+.INCLUDE: fcfg_w4w.mk
+.INCLUDE: fcfg_calc.mk
+.INCLUDE: fcfg_draw.mk
+.INCLUDE: fcfg_impress.mk
+.INCLUDE: fcfg_chart.mk
+.INCLUDE: fcfg_math.mk
+.INCLUDE: fcfg_xslt.mk
+.INCLUDE: fcfg_palm.mk
+.INCLUDE: fcfg_pocketword.mk
+.INCLUDE: fcfg_pocketexcel.mk
+.INCLUDE: fcfg_internalgraphics.mk
+.INCLUDE: fcfg_drawgraphics.mk
+.INCLUDE: fcfg_impressgraphics.mk
+
+# -----------------------------------------------------------------------------
+# include global settings
+# -----------------------------------------------------------------------------
+
+.INCLUDE: settings.mk
+
+# -----------------------------------------------------------------------------
+# list of all required destination files
+# -----------------------------------------------------------------------------
+
+REALFILTERPACKAGES  =   \
+    $(BIN)$/fcfg_base.zip               \
+    $(BIN)$/fcfg_writer.zip             \
+    $(BIN)$/fcfg_web.zip                \
+    $(BIN)$/fcfg_global.zip             \
+    $(BIN)$/fcfg_w4w.zip                \
+    $(BIN)$/fcfg_calc.zip               \
+    $(BIN)$/fcfg_draw.zip               \
+    $(BIN)$/fcfg_impress.zip            \
+    $(BIN)$/fcfg_chart.zip              \
+    $(BIN)$/fcfg_math.zip               \
+    $(BIN)$/fcfg_xslt.zip               \
+    $(BIN)$/fcfg_palm.zip               \
+    $(BIN)$/fcfg_pocketexcel.zip        \
+    $(BIN)$/fcfg_pocketword.zip         \
+    $(BIN)$/fcfg_drawgraphics.zip       \
+    $(BIN)$/fcfg_impressgraphics.zip
+
+INTERNALFILTERPACKAGES  =   \
+    $(BIN)$/fcfg_internalgraphics.zip
+
+# -----------------------------------------------------------------------------
+# build all
+# -----------------------------------------------------------------------------
+
+.INCLUDE: target.mk
+
+ALLTAR: $(REALFILTERPACKAGES) $(INTERNALFILTERPACKAGES)
+
+# -----------------------------------------------------------------------------
+# build every module seperated
+# -----------------------------------------------------------------------------
+
+$(REALFILTERPACKAGES) : $$(ALL_4$$(@:b))
+     +@echo -------------------------------------------------------------------
+     +@echo Building package $@
+     +@$(JAVA) -jar $(CLASSDIR)$/FCFGMerge.jar fragmentsdir=. tempdir=$(TEMP) outdir=$(MISC) pkg=$(@:b)_types   xmlpackage=Types  tcfg=$(mktmp items=$(TYPES_4$(@:b):b:t",":s/.xcu/))
+     +@$(JAVA) -jar $(CLASSDIR)$/FCFGMerge.jar fragmentsdir=. tempdir=$(TEMP) outdir=$(MISC) pkg=$(@:b)_filters xmlpackage=Filter fcfg=$(mktmp items=$(FILTERS_4$(@:b):b:t",":s/.xcu/))
+     +@$(JAVA) -jar $(CLASSDIR)$/FCFGMerge.jar fragmentsdir=. tempdir=$(TEMP) outdir=$(MISC) pkg=$(@:b)_others  xmlpackage=Misc   lcfg=$(mktmp items=$(FRAMELOADERS_4$(@:b):b:t",":s/.xcu/)) ccfg=$(mktmp items=$(CONTENTHANDLERS_4$(@:b):b:t",":s/.xcu/))
+     +@zip -j $@ $(MISC)$/$(@:b)_*.xcu
+     +@$(RM) $(MISC)$/$(@:b)_*.xcu
+
+$(INTERNALFILTERPACKAGES) : $$(ALL_4$$(@:b))
+     +@echo -------------------------------------------------------------------
+     +@echo Building special packages $@
+     +@$(JAVA) -jar $(CLASSDIR)$/FCFGMerge.jar fragmentsdir=. tempdir=$(TEMP) outdir=$(MISC) pkg=$(@:b)_types   xmlpackage=Types  tcfg=$(mktmp items=$(TYPES_4$(@:b):b:t",":s/.xcu/))
+     +@$(JAVA) -jar $(CLASSDIR)$/FCFGMerge.jar fragmentsdir=. tempdir=$(TEMP) outdir=$(MISC) pkg=$(@:b)_filters xmlpackage=GraphicFilter fcfg=$(mktmp items=$(FILTERS_4$(@:b):b:t",":s/.xcu/)) subdir_filters=internalgraphicfilters
+     +@zip -j $(BIN)$/$(@:b).zip $(MISC)$/$(@:b)_*.xcu
+     +@$(RM) $(MISC)$/$(@:b)_*.xcu
