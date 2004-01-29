d2e1412fd5862a389052f74bcddd9a6e53c3c0a6
Kurt Zenker <kz@openoffice.org>
2004-01-29 10:49:41 +0000

    INTEGRATION: CWS newscptools (1.1.2); FILE ADDED 2004/01/20 16:06:37 is 1.1.2.1: #i24601# new scp tooling

---
+++ solenv/bin/modules/pre2par/pathanalyzer.pm
@@ -0,0 +1,110 @@
+#*************************************************************************
+#
+#   $RCSfile: pathanalyzer.pm,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2004-01-29 11:49:16 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+
+package pre2par::pathanalyzer;
+
+use pre2par::globals;
+
+###########################################
+# Path analyzer
+###########################################
+
+sub get_path_from_fullqualifiedname
+{
+    my ($longfilenameref) = @_;
+
+    if ( $$longfilenameref =~ /\Q$pre2par::globals::separator\E/ )  # Is there a separator in the path? Otherwise the path is empty.
+    {
+        if ( $$longfilenameref =~ /^\s*(\S.*\S\Q$pre2par::globals::separator\E)(\S.+?\S)/ )
+        {
+            $$longfilenameref = $1;
+        }
+    }
+    else
+    {
+        $$longfilenameref = ""; # there is no path
+    }
+}
+
+sub make_absolute_filename_to_relative_filename
+{
+    my ($longfilenameref) = @_;
+
+    if ( $pre2par::globals::isunix )
+    {
+        if ( $$longfilenameref =~ /^.*\/(\S.+\S?)/ )
+        {
+            $$longfilenameref = $1;
+        }
+    }
+
+    if ( $pre2par::globals::iswin )
+    {
+        if ( $$longfilenameref =~ /^.*\\(\S.+\S?)/ )
+        {
+            $$longfilenameref = $1;
+        }
+    }
+}
+
+1;
