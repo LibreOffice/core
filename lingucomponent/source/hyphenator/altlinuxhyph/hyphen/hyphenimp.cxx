240f54797eb97b10466891916c3bffd49fb152fa
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 19:17:06 +0000

    INTEGRATION: CWS geordi2q14 (1.11.8); FILE MERGED 2004/01/29 17:19:57 hr 1.11.8.1: #111934#: merge CWS ooo111fix2

--- lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hyphenimp.cxx
+++ lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hyphenimp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hyphenimp.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 16:13:27 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 20:17:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -149,8 +149,8 @@ Hyphenator::Hyphenator() :
 {
     bDisposing = FALSE;
     pPropHelper = NULL;
+        aDicts = NULL;
         numdict = 0;
-
 }
 
 
@@ -158,7 +158,7 @@ Hyphenator::~Hyphenator()
 {
     if (pPropHelper)
         pPropHelper->RemoveAsPropListener();
-        if (numdict) {
+        if ((aDicts) || (numdict)) {
             if (aDicts) delete[] aDicts;
         aDicts = NULL;
             numdict = 0;
@@ -320,9 +320,9 @@ sal_Bool SAL_CALL Hyphenator::hasLocale(const Locale& rLocale)
         getLocales();
 
     INT32 nLen = aSuppLocales.getLength();
+    const Locale *pLocale = aSuppLocales.getConstArray();
     for (INT32 i = 0;  i < nLen;  ++i)
     {
-        const Locale *pLocale = aSuppLocales.getConstArray();
         if (rLocale == pLocale[i])
         {
             bRes = TRUE;
