79042e1c939cc575b1509e52887f50f43c67dbe6
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 19:02:13 +0000

    INTEGRATION: CWS geordi2q14 (1.3.72); FILE MERGED 2004/01/30 13:11:33 hr 1.3.72.1: #111934#: merge CWS ooo111fix2

--- odk/examples/DevelopersGuide/OfficeDev/PathSubstitution/PathSubstitutionTest.java
+++ odk/examples/DevelopersGuide/OfficeDev/PathSubstitution/PathSubstitutionTest.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: PathSubstitutionTest.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:42:52 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 20:02:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -88,7 +88,7 @@ public class PathSubstitutionTest extends java.lang.Object {
             XUnoUrlResolver xUnoUrlResolver = (XUnoUrlResolver) UnoRuntime.queryInterface(
                 XUnoUrlResolver.class, urlResolver );
             Object initialObject = xUnoUrlResolver.resolve(
-                "uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" );
+                "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager" );
             XPropertySet xPropertySet = (XPropertySet)UnoRuntime.queryInterface(
                 XPropertySet.class, initialObject);
             Object context = xPropertySet.getPropertyValue("DefaultContext");
