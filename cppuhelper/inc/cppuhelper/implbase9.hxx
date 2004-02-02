0b642d15c1f2d0f40fc6657818b67dc823aa2269
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 22:14:07 +0000

    INTEGRATION: CWS geordi2q14 (1.8.40); FILE MERGED 2004/01/29 15:09:55 hr 1.8.40.1: #111934#: merge CWS ooo111fix2

--- cppuhelper/inc/cppuhelper/implbase9.hxx
+++ cppuhelper/inc/cppuhelper/implbase9.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: implbase9.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2003-07-16 17:51:37 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 23:14:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,25 +60,6 @@
  ************************************************************************/
 #ifndef _CPPUHELPER_IMPLBASE9_HXX_
 #define _CPPUHELPER_IMPLBASE9_HXX_
-
-#if defined( MACOSX ) && ( __GNUC__ < 3 )
-/* use old impl helpers for macosx */
-
-#define __IFC9 Ifc1, Ifc2, Ifc3, Ifc4, Ifc5, Ifc6, Ifc7, Ifc8, Ifc9
-#define __CLASS_IFC9 class Ifc1, class Ifc2, class Ifc3, class Ifc4, class Ifc5, class Ifc6, class Ifc7, class Ifc8, class Ifc9
-#define __PUBLIC_IFC9 public Ifc1, public Ifc2, public Ifc3, public Ifc4, public Ifc5, public Ifc6, public Ifc7, public Ifc8, public Ifc9
-
-#ifndef _CPPUHELPER_IMPLBASE_HXX_
-#include <cppuhelper/implbase.hxx>
-#endif
-
-__DEF_IMPLHELPER_PRE( 9 )
-    __IFC_WRITEOFFSET( 1 ) __IFC_WRITEOFFSET( 2 ) __IFC_WRITEOFFSET( 3 ) __IFC_WRITEOFFSET( 4 )
-    __IFC_WRITEOFFSET( 5 ) __IFC_WRITEOFFSET( 6 ) __IFC_WRITEOFFSET( 7 ) __IFC_WRITEOFFSET( 8 )
-    __IFC_WRITEOFFSET( 9 )
-__DEF_IMPLHELPER_POST( 9 )
-
-#else /* ! MACOSX */
 
 #ifndef _CPPUHELPER_IMPLBASE_EX_HXX_
 #include <cppuhelper/implbase_ex.hxx>
@@ -323,7 +304,5 @@ namespace cppu
             }
     };
 }
-
-#endif /* MACOSX */
 
 #endif
