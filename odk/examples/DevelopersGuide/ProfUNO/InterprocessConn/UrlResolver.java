d27366f475d1bd3a7461ec53ab456eb3f67b8b88
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 19:03:31 +0000

    INTEGRATION: CWS geordi2q14 (1.3.72); FILE MERGED 2004/01/30 13:11:36 hr 1.3.72.1: #111934#: merge CWS ooo111fix2

--- odk/examples/DevelopersGuide/ProfUNO/InterprocessConn/UrlResolver.java
+++ odk/examples/DevelopersGuide/ProfUNO/InterprocessConn/UrlResolver.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UrlResolver.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:44:30 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 20:03:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -55,7 +55,7 @@ class UrlResolver
             System.out.println( "usage: UrlResolver uno-url\n" +
                                 "       uno-url  The uno-url identifying the object to\n" +
                                 "                be imported, for instance\n" +
-                                "                uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" +
+                                "                uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager" +
                                 "                (use \" on unix shells to avoid ;-problems" );
             System.exit( 1 );
         }
