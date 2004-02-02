eaa6a2437f0857d47c6a0ebe887984346d85c9c6
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 22:06:32 +0000

    INTEGRATION: CWS geordi2q14 (1.4.76); FILE MERGED 2004/01/29 16:05:48 hr 1.4.76.1: #111934#: merge CWS ooo111fix2

--- framework/test/threadtest/makefile.mk
+++ framework/test/threadtest/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: hr $ $Date: 2003-07-16 17:40:25 $
+#   last change: $Author: hr $ $Date: 2004-02-02 23:06:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -86,10 +86,6 @@ APP1OBJS=		$(SLO)$/threadtest.obj							\
                 $(SLO)$/lockhelper.obj
 
 # [ed] 6/16/02 Add the transaction manager library on OS X
-
-.IF "$(OS)$(CVER)"=="MACOSXC295"
-APP1OBJS+=	$(SLO)$/transactionmanager.obj
-.ENDIF
 
 APP1STDLIBS=	$(CPPULIB)										\
                 $(CPPUHELPERLIB)								\
