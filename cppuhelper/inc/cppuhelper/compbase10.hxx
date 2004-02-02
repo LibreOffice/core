88db834132f27e27f03d951a4c3bc3cfc970cada
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 22:07:53 +0000

    INTEGRATION: CWS geordi2q14 (1.8.40); FILE MERGED 2004/01/29 15:09:06 hr 1.8.40.1: #111934#: merge CWS ooo111fix2

--- cppuhelper/inc/cppuhelper/compbase10.hxx
+++ cppuhelper/inc/cppuhelper/compbase10.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: compbase10.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2003-07-16 17:48:04 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 23:07:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,17 +64,6 @@
 #ifndef _CPPUHELPER_IMPLBASE10_HXX_
 #include <cppuhelper/implbase10.hxx>
 #endif
-
-#if defined( MACOSX ) && ( __GNUC__ < 3 )
-/* use old impl helpers for macosx */
-
-#ifndef _CPPUHELPER_COMPBASE_HXX_
-#include <cppuhelper/compbase.hxx>
-#endif
-
-__DEF_COMPIMPLHELPER( 10 )
-
-#else /* ! MACOSX */
 
 #ifndef _CPPUHELPER_COMPBASE_EX_HXX_
 #include <cppuhelper/compbase_ex.hxx>
@@ -179,7 +168,5 @@ namespace cppu
             }
     };
 }
-
-#endif /* MACOSX */
 
 #endif
