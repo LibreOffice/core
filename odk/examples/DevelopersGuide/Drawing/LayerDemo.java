226e053a4e036ecf0c003c40471686fe769fedf9
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 18:55:22 +0000

    INTEGRATION: CWS geordi2q14 (1.3.72); FILE MERGED 2004/01/30 13:11:19 hr 1.3.72.1: #111934#: merge CWS ooo111fix2

--- odk/examples/DevelopersGuide/Drawing/LayerDemo.java
+++ odk/examples/DevelopersGuide/Drawing/LayerDemo.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: LayerDemo.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:24:01 $
+ *  last change: $Author: hr $ $Date: 2004-02-02 19:55:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -83,7 +83,7 @@ public class LayerDemo
             if ( args.length >= 1 )
                 sConnection = args[ 1 ];
             else
-                sConnection = "uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager";
+                sConnection = "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager";
             XMultiServiceFactory xServiceFactory =
                 Helper.connect( sConnection );
 
