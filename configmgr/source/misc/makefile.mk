e962a5adddfc61424313701ed8f001504992f921
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 19:21:27 +0000

    INTEGRATION: CWS geordi2q14 (1.21.84); FILE MERGED 2004/01/29 14:58:27 hr 1.21.84.1: #111934#: merge CWS ooo111fix2

--- configmgr/source/misc/makefile.mk
+++ configmgr/source/misc/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.21 $
+#   $Revision: 1.22 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-19 16:19:23 $
+#   last change: $Author: hr $ $Date: 2004-02-02 20:21:27 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -96,10 +96,6 @@ OBJFILES=	\
         $(OBJ)$/filehelper.obj	\
         $(OBJ)$/trivialbufferedfile.obj \
 
-
-.IF "$(OS)"=="MACOSX"
-SYMBOLPREFIX=$(CFGMGR_TARGET)$(CFGMGR_MAJOR)
-.ENDIF
 
 # --- Targets ----------------------------------
 
