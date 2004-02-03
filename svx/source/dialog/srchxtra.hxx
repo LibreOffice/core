11fc7a2839e54bb10b6fdb11a1f81dd80ae92671
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 17:51:31 +0000

    INTEGRATION: CWS dialogdiet (1.1.2); FILE ADDED 2004/01/13 03:52:42 mwu 1.1.2.1: DialogDiet 2004_01_13

---
+++ svx/source/dialog/srchxtra.hxx
@@ -0,0 +1,148 @@
+/*************************************************************************
+ *
+ *  $RCSfile: srchxtra.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-03 18:51:31 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SVX_SRCHXTRA_HXX
+#define _SVX_SRCHXTRA_HXX
+
+// include ---------------------------------------------------------------
+
+#ifndef _SV_FIELD_HXX
+#include <vcl/field.hxx>
+#endif
+#ifndef _CTRLTOOL_HXX
+#include <svtools/ctrltool.hxx>
+#endif
+#ifndef _SFXTABDLG_HXX
+#include <sfx2/tabdlg.hxx>
+#endif
+
+#include "checklbx.hxx"
+#include "srchdlg.hxx"
+
+// class SvxSearchFormatDialog -------------------------------------------
+
+class SvxSearchFormatDialog : public SfxTabDialog
+{
+public:
+    SvxSearchFormatDialog( Window* pParent, const SfxItemSet& rSet );
+    ~SvxSearchFormatDialog();
+
+protected:
+    virtual void    PageCreated( USHORT nId, SfxTabPage &rPage );
+
+private:
+    FontList*       pFontList;
+};
+
+// class SvxSearchFormatDialog -------------------------------------------
+
+class SvxSearchAttributeDialog : public ModalDialog
+{
+public:
+    SvxSearchAttributeDialog( Window* pParent, SearchAttrItemList& rLst,
+                              const USHORT* pWhRanges );
+
+private:
+    FixedText           aAttrFL;
+    SvxCheckListBox     aAttrLB;
+
+    OKButton            aOKBtn;
+    CancelButton        aEscBtn;
+    HelpButton          aHelpBtn;
+
+    SearchAttrItemList& rList;
+
+    DECL_LINK( OKHdl, Button * );
+};
+
+// class SvxSearchSimilarityDialog ---------------------------------------
+
+class SvxSearchSimilarityDialog : public ModalDialog
+{
+private:
+    FixedLine           aFixedLine;
+    FixedText           aOtherTxt;
+    NumericField        aOtherFld;
+    FixedText           aLongerTxt;
+    NumericField        aLongerFld;
+    FixedText           aShorterTxt;
+    NumericField        aShorterFld;
+    CheckBox            aRelaxBox;
+
+    OKButton            aOKBtn;
+    CancelButton        aEscBtn;
+    HelpButton          aHelpBtn;
+
+public:
+    SvxSearchSimilarityDialog(  Window* pParent,
+                                BOOL bRelax,
+                                USHORT nOther,
+                                USHORT nShorter,
+                                USHORT nLonger );
+
+    USHORT  GetOther()      { return (USHORT)aOtherFld.GetValue(); }
+    USHORT  GetShorter()    { return (USHORT)aShorterFld.GetValue(); }
+    USHORT  GetLonger()     { return (USHORT)aLongerFld.GetValue(); }
+    BOOL    IsRelaxed()     { return aRelaxBox.IsChecked(); }
+};
+
+
+#endif
+
