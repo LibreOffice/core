a36a55464f1814547197edc710a22e239c6bbdab
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 10:28:56 +0000

    INTEGRATION: CWS geordi2q14 (1.3.72); FILE MERGED 2004/01/30 13:11:19 hr 1.3.72.1: #111934#: merge CWS ooo111fix2

--- odk/examples/DevelopersGuide/Drawing/GraphicExportDemo.java
+++ odk/examples/DevelopersGuide/Drawing/GraphicExportDemo.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: GraphicExportDemo.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:23:37 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 11:28:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -81,7 +81,7 @@ public class GraphicExportDemo
                 if ( args.length >= 4 )
                     sConnection = args[ 3 ];
                 else
-                    sConnection = "uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager";
+                    sConnection = "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager";
                 XMultiServiceFactory xServiceFactory =
                     Helper.connect( sConnection );
 
