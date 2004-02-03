c2188a6bc5c11aa8c99d9892300c45c6ae54eac0
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 11:41:30 +0000

    INTEGRATION: CWS calcrtl (1.10.28); FILE MERGED 2003/10/08 09:25:59 nn 1.10.28.2: RESYNC: (1.10-1.11); FILE MERGED 2003/07/29 18:12:14 nn 1.10.28.1: #106948# RTL scroll bar handling

--- sc/source/ui/inc/tabview.hxx
+++ sc/source/ui/inc/tabview.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabview.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2003-09-04 08:03:34 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 12:41:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -222,6 +222,9 @@ private:
     void            UpdateHeaderWidth( const ScVSplitPos* pWhich = NULL,
                                         const USHORT* pPosY = NULL );
     void            UpdateVarZoom();
+
+    static void     SetScrollBar( ScrollBar& rScroll, long nRangeMax, long nVisible, long nPos, BOOL bLayoutRTL );
+    static long     GetScrollBarPos( ScrollBar& rScroll, BOOL bLayoutRTL );
 
 protected:
     void            HideTip();
