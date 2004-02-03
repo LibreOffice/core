a77a63b4eeed5e78a16a23c367d76b6483e3eb41
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 18:43:22 +0000

    INTEGRATION: CWS dialogdiet (1.24.10); FILE MERGED 2004/01/12 14:44:30 mba 1.24.10.1: #i14041#: crash when document is closed while basic works on drawpage

--- svx/source/unodraw/unopage.cxx
+++ svx/source/unodraw/unopage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unopage.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: rt $ $Date: 2003-11-24 17:03:34 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 19:43:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -212,6 +212,7 @@ void SvxDrawPage::Notify( SfxBroadcaster& rBC, const SfxHint& rHint )
             pModel = NULL;
             delete pView;
             pView = NULL;
+            pPage = NULL;
         }
     }
 }
