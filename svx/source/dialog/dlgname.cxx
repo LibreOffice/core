fce9f72f651566e6ba04e8554ea9e1bc9d33cd2d
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 17:25:46 +0000

    INTEGRATION: CWS dialogdiet (1.2.490); FILE MERGED 2004/01/19 21:02:47 mba 1.2.490.2: RESYNC: (1.2-1.3); FILE MERGED 2004/01/13 03:12:03 mwu 1.2.490.1: DialogDiet 2004_01_13

--- svx/source/dialog/dlgname.cxx
+++ svx/source/dialog/dlgname.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dlgname.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-01-05 16:24:46 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 18:25:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -76,6 +76,7 @@
 #include "dialogs.hrc"
 
 #include "dlgname.hxx"
+#include "defdlgname.hxx" //CHINA001
 #include "dlgname.hrc"
 #include "dialmgr.hxx"
 
