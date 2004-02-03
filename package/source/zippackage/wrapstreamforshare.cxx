96c4513d6fdba4735b5b8b4c39083d4abd3a0028
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 17:02:34 +0000

    INTEGRATION: CWS readstorage (1.1.2); FILE ADDED 2004/01/06 14:29:00 mav 1.1.2.1: #114521# instead of staream return wrappers that share mutex and remember own position

---
+++ package/source/zippackage/wrapstreamforshare.cxx
@@ -0,0 +1,213 @@
+/*************************************************************************
+ *
+ *  $RCSfile: wrapstreamforshare.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-03 18:02:34 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _OSL_DIAGNOSE_H_
+#include <osl/diagnose.h>
+#endif
+
+#include "wrapstreamforshare.hxx"
+
+using namespace ::com::sun::star;
+
+
+WrapStreamForShare::WrapStreamForShare( const uno::Reference< io::XInputStream >& xInStream,
+                                        const SotMutexHolderRef& rMutexRef )
+: m_xInStream( xInStream )
+, m_rMutexRef( rMutexRef )
+, m_nCurPos( 0 )
+{
+    m_xSeekable = uno::Reference< io::XSeekable >( m_xInStream, uno::UNO_QUERY );
+    if ( !m_rMutexRef.Is() || !m_xInStream.is() || !m_xSeekable.is() )
+    {
+        OSL_ENSURE( sal_False, "Wrong initialization of wrapping stream!\n" );
+        throw uno::RuntimeException();
+    }
+}
+
+WrapStreamForShare::~WrapStreamForShare()
+{
+}
+
+// XInputStream
+sal_Int32 SAL_CALL WrapStreamForShare::readBytes( uno::Sequence< sal_Int8 >& aData, sal_Int32 nBytesToRead )
+        throw ( io::NotConnectedException,
+                io::BufferSizeExceededException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    m_xSeekable->seek( m_nCurPos );
+
+    sal_Int32 nRead = m_xInStream->readBytes( aData, nBytesToRead );
+    m_nCurPos += nRead;
+
+    return nRead;
+}
+
+sal_Int32 SAL_CALL WrapStreamForShare::readSomeBytes( uno::Sequence< sal_Int8 >& aData, sal_Int32 nMaxBytesToRead )
+        throw ( io::NotConnectedException,
+                io::BufferSizeExceededException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    m_xSeekable->seek( m_nCurPos );
+
+    sal_Int32 nRead = m_xInStream->readSomeBytes( aData, nMaxBytesToRead );
+    m_nCurPos += nRead;
+
+    return nRead;
+}
+
+void SAL_CALL WrapStreamForShare::skipBytes( sal_Int32 nBytesToSkip )
+        throw ( io::NotConnectedException,
+                io::BufferSizeExceededException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    m_xSeekable->seek( m_nCurPos );
+
+    m_xInStream->skipBytes( nBytesToSkip );
+    m_nCurPos = m_xSeekable->getPosition();
+}
+
+sal_Int32 SAL_CALL WrapStreamForShare::available()
+        throw ( io::NotConnectedException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    return m_xInStream->available();
+}
+
+void SAL_CALL WrapStreamForShare::closeInput()
+        throw ( io::NotConnectedException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    m_xInStream->closeInput();
+    m_xInStream = uno::Reference< io::XInputStream >();
+    m_xSeekable = uno::Reference< io::XSeekable >();
+}
+
+// XSeekable
+void SAL_CALL WrapStreamForShare::seek( sal_Int64 location )
+        throw ( lang::IllegalArgumentException,
+                io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    // let stream implementation do all the checking
+    m_xSeekable->seek( location );
+
+    m_nCurPos = m_xSeekable->getPosition();
+}
+
+sal_Int64 SAL_CALL WrapStreamForShare::getPosition()
+        throw ( io::IOException,
+                uno::RuntimeException)
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    return m_nCurPos;
+}
+
+sal_Int64 SAL_CALL WrapStreamForShare::getLength()
+        throw ( io::IOException,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_rMutexRef->GetMutex() );
+
+    if ( !m_xInStream.is() )
+        throw io::IOException(); // TODO
+
+    return m_xSeekable->getLength();
+}
+
