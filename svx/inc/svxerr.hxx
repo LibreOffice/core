14e244c83d529a5464b8e13fdda80030b785cae1
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 16:45:45 +0000

    INTEGRATION: CWS dialogdiet (1.2.506); FILE MERGED 2004/01/26 14:31:39 mba 1.2.506.1: #i22972#: SvxErrorHandler needs to be created by apps

--- svx/inc/svxerr.hxx
+++ svx/inc/svxerr.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svxerr.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: tl $ $Date: 2001-08-23 14:45:58 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 17:45:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -122,6 +122,7 @@ class SvxErrorHandler : private SfxErrorHandler
 {
   public:
     SvxErrorHandler();
+    static void Get();
 };
 
 #endif
