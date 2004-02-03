5e6499eb247ce851692ff920a27ca7f6d8ff7e09
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 17:44:42 +0000

    INTEGRATION: CWS dialogdiet (1.1.2); FILE ADDED 2003/12/05 07:02:59 cd 1.1.2.2: #i22972# Fixed include problem 2003/11/28 17:00:08 mba 1.1.2.1: #i22969#: dialogs and tabpages moved from offmgr

---
+++ svx/source/dialog/opthtml.hxx
@@ -0,0 +1,138 @@
+/*************************************************************************
+ *
+ *  $RCSfile: opthtml.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-03 18:44:42 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _OFA_OPTHTML_HXX
+#define _OFA_OPTHTML_HXX
+
+
+#ifndef _BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+#ifndef _FIELD_HXX
+#include <vcl/field.hxx>
+#endif
+#ifndef _FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
+#ifndef _VCL_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
+#ifndef _GROUP_HXX
+#include <vcl/group.hxx>
+#endif
+#ifndef _SFXTABDLG_HXX
+#include <sfx2/tabdlg.hxx>
+#endif
+#ifndef _SVX_TXENCBOX_HXX
+#include "txencbox.hxx"
+#endif
+
+
+class OfaHtmlTabPage : public SfxTabPage
+{
+    FixedLine       aFontSizeGB;
+    FixedText       aSize1FT;
+    NumericField    aSize1NF;
+    FixedText       aSize2FT;
+    NumericField    aSize2NF;
+    FixedText       aSize3FT;
+    NumericField    aSize3NF;
+    FixedText       aSize4FT;
+    NumericField    aSize4NF;
+    FixedText       aSize5FT;
+    NumericField    aSize5NF;
+    FixedText       aSize6FT;
+    NumericField    aSize6NF;
+    FixedText       aSize7FT;
+    NumericField    aSize7NF;
+
+    FixedLine       aImportGB;
+    CheckBox        aUnknownTagCB;
+    CheckBox        aIgnoreFontNamesCB;
+
+    FixedLine       aExportGB;
+    ListBox         aExportLB;
+    CheckBox        aStarBasicCB;
+    CheckBox        aStarBasicWarningCB;
+    CheckBox        aPrintExtensionCB;
+    CheckBox        aSaveGrfLocalCB;
+    FixedText       aCharSetFT;
+    SvxTextEncodingBox aCharSetLB;
+
+    DECL_LINK(ExportHdl_Impl, ListBox*);
+    DECL_LINK(CheckBoxHdl_Impl, CheckBox*);
+
+    OfaHtmlTabPage(Window* pParent, const SfxItemSet& rSet);
+    virtual ~OfaHtmlTabPage();
+public:
+
+    static SfxTabPage*  Create( Window* pParent,
+                                const SfxItemSet& rAttrSet );
+
+    virtual BOOL        FillItemSet( SfxItemSet& rSet );
+    virtual void        Reset( const SfxItemSet& rSet );
+
+};
+
+
+#endif //
+
+
+
