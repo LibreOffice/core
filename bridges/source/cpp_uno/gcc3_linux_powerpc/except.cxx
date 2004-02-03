d7eae0931884e40059d73ccf80bda5174f601bb1
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 11:40:19 +0000

    INTEGRATION: CWS sb10 (1.4.52); FILE MERGED 2004/01/17 18:40:44 khendricks 1.4.52.1: Stephan,
    I am adding in the new ppc linux bridge code to your branch
    in the hopes it will get merged in when your branch
    tag is merged in
    
    it passes the multinherit tests in all forms
    
    I will try to fix the MacOSX stuff next.

--- bridges/source/cpp_uno/gcc3_linux_powerpc/except.cxx
+++ bridges/source/cpp_uno/gcc3_linux_powerpc/except.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: except.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 16:26:17 $
+ *  last change: $Author: hr $ $Date: 2004-02-03 12:40:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -69,7 +69,7 @@
 #include <osl/diagnose.h>
 #include <osl/mutex.hxx>
 
-#include <bridges/cpp_uno/bridge.hxx>
+#include <com/sun/star/uno/genfunc.hxx>
 #include <typelib/typedescription.hxx>
 #include <uno/any2.h>
 
