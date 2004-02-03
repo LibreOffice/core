c8014c9fb7d131b50184ac33153e66d67ea1d78f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 11:50:40 +0000

    INTEGRATION: CWS sb10 (1.1.2); FILE ADDED 2004/01/08 13:43:30 sb 1.1.2.4: #114000# Factored out more shared code. 2003/12/19 16:14:58 sb 1.1.2.3: #114000# Factored out even more shared behaviour. 2003/12/18 08:08:11 sb 1.1.2.2: #114000# Fixed MSVC compile problems. 2003/12/10 10:50:16 sb 1.1.2.1: #114000# Adapted to multiple-inheritance interface types.

---
+++ bridges/source/cpp_uno/shared/cppinterfaceproxy.cxx
@@ -0,0 +1,203 @@
+/*************************************************************************
+ *
+ *  $RCSfile: cppinterfaceproxy.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-03 12:50:40 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "bridges/cpp_uno/shared/cppinterfaceproxy.hxx"
+
+#include "guardedarray.hxx"
+
+#include "bridges/cpp_uno/shared/bridge.hxx"
+#include "bridges/cpp_uno/shared/vtablefactory.hxx"
+
+#include "com/sun/star/uno/XInterface.hpp"
+#include "osl/diagnose.h"
+#include "osl/getglobalmutex.hxx"
+#include "osl/interlck.h"
+#include "osl/mutex.hxx"
+#include "rtl/instance.hxx"
+#include "typelib/typedescription.h"
+
+#include <cstddef>
+#include <new>
+
+using bridges::cpp_uno::shared::CppInterfaceProxy;
+
+namespace {
+
+struct InitVtableFactory {
+    bridges::cpp_uno::shared::VtableFactory * operator()() {
+        static bridges::cpp_uno::shared::VtableFactory instance;
+        return &instance;
+    }
+};
+
+bridges::cpp_uno::shared::VtableFactory * getVtableFactory() {
+    return rtl_Instance<
+        bridges::cpp_uno::shared::VtableFactory, InitVtableFactory,
+        osl::MutexGuard, osl::GetGlobalMutex >::create(
+            InitVtableFactory(), osl::GetGlobalMutex());
+}
+
+}
+
+com::sun::star::uno::XInterface * CppInterfaceProxy::create(
+    bridges::cpp_uno::shared::Bridge * pBridge, uno_Interface * pUnoI,
+    typelib_InterfaceTypeDescription * pTypeDescr, rtl::OUString const & rOId)
+    SAL_THROW(())
+{
+    typelib_typedescription_complete(
+        reinterpret_cast< typelib_TypeDescription ** >(&pTypeDescr));
+    bridges::cpp_uno::shared::VtableFactory::Vtables aVtables(
+        getVtableFactory()->getVtables(pTypeDescr));
+    bridges::cpp_uno::shared::GuardedArray< char > pMemory(
+        new char[
+            sizeof (CppInterfaceProxy)
+            + (aVtables.count - 1) * sizeof (void **)]);
+    new(pMemory.get()) CppInterfaceProxy(pBridge, pUnoI, pTypeDescr, rOId);
+    CppInterfaceProxy * pProxy = reinterpret_cast< CppInterfaceProxy * >(
+        pMemory.release());
+    for (sal_Int32 i = 0; i < aVtables.count; ++i) {
+        pProxy->vtables[i] = VtableFactory::mapBlockToVtable(
+            aVtables.blocks[i]);
+    }
+    return castProxyToInterface(pProxy);
+}
+
+void CppInterfaceProxy::free(uno_ExtEnvironment * pEnv, void * pInterface)
+    SAL_THROW(())
+{
+    CppInterfaceProxy * pThis = castInterfaceToProxy(pInterface);
+    OSL_ASSERT( pEnv == pThis->pBridge->getCppEnv() );
+
+    (*pThis->pBridge->getUnoEnv()->revokeInterface)(
+        pThis->pBridge->getUnoEnv(), pThis->pUnoI );
+    (*pThis->pUnoI->release)( pThis->pUnoI );
+    ::typelib_typedescription_release(
+        (typelib_TypeDescription *)pThis->pTypeDescr );
+    pThis->pBridge->release();
+
+#if OSL_DEBUG_LEVEL > 1
+    *(int *)pInterface = 0xdeadbabe;
+#endif
+    pThis->~CppInterfaceProxy();
+    delete[] reinterpret_cast< char * >(pThis);
+}
+
+void CppInterfaceProxy::acquireProxy() SAL_THROW(())
+{
+    if (1 == osl_incrementInterlockedCount( &nRef ))
+    {
+        // rebirth of proxy zombie
+        // register at cpp env
+        void * pThis = castProxyToInterface( this );
+        (*pBridge->getCppEnv()->registerProxyInterface)(
+            pBridge->getCppEnv(), &pThis, free, oid.pData, pTypeDescr );
+        OSL_ASSERT( pThis == castProxyToInterface( this ) );
+    }
+}
+
+void CppInterfaceProxy::releaseProxy() SAL_THROW(())
+{
+    if (! osl_decrementInterlockedCount( &nRef )) // last release
+    {
+        // revoke from cpp env
+        (*pBridge->getCppEnv()->revokeInterface)(
+            pBridge->getCppEnv(), castProxyToInterface( this ) );
+    }
+}
+
+CppInterfaceProxy::CppInterfaceProxy(
+    bridges::cpp_uno::shared::Bridge * pBridge_, uno_Interface * pUnoI_,
+    typelib_InterfaceTypeDescription * pTypeDescr_, rtl::OUString const & rOId_)
+    SAL_THROW(())
+    : nRef( 1 )
+    , pBridge( pBridge_ )
+    , pUnoI( pUnoI_ )
+    , pTypeDescr( pTypeDescr_ )
+    , oid( rOId_ )
+{
+    pBridge->acquire();
+    ::typelib_typedescription_acquire( (typelib_TypeDescription *)pTypeDescr );
+    (*pUnoI->acquire)( pUnoI );
+    (*pBridge->getUnoEnv()->registerInterface)(
+        pBridge->getUnoEnv(), reinterpret_cast< void ** >( &pUnoI ), oid.pData,
+        pTypeDescr );
+}
+
+CppInterfaceProxy::~CppInterfaceProxy()
+{}
+
+com::sun::star::uno::XInterface * CppInterfaceProxy::castProxyToInterface(
+    CppInterfaceProxy * pProxy)
+{
+    return reinterpret_cast< com::sun::star::uno::XInterface * >(
+        &pProxy->vtables);
+}
+
+CppInterfaceProxy * CppInterfaceProxy::castInterfaceToProxy(void * pInterface)
+{
+    // pInterface == &pProxy->vtables (this emulated offsetof is not truly
+    // portable):
+    char const * const base = reinterpret_cast< char const * >(16);
+    std::ptrdiff_t const offset = reinterpret_cast< char const * >(
+        &reinterpret_cast< CppInterfaceProxy const * >(base)->vtables) - base;
+    return reinterpret_cast< CppInterfaceProxy * >(
+        static_cast< char * >(pInterface) - offset);
+}
