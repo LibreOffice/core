f530f2f3801a17238cfbbbfad44366e9b28e5743
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-03 19:44:59 +0000

    INTEGRATION: CWS dialogdiet (1.1.2); FILE ADDED 2003/11/28 16:37:47 mba 1.1.2.1: #i22972#: FilterOptions moved to svt

---
+++ svtools/inc/fltrcfg.hxx
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  $RCSfile: fltrcfg.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-03 20:44:59 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SVT_FLTRCFG_HXX
+#define _SVT_FLTRCFG_HXX
+
+// -----------------------------------------------------------------------
+
+#ifndef _UTL_CONFIGITEM_HXX_
+#include <unotools/configitem.hxx>
+#endif
+
+struct SvtFilterOptions_Impl;
+class SvtFilterOptions : public utl::ConfigItem
+{
+    SvtFilterOptions_Impl* pImp;
+
+    const com::sun::star::uno::Sequence<rtl::OUString>& GetPropertyNames();
+public:
+                    SvtFilterOptions();
+    virtual        ~SvtFilterOptions();
+
+    virtual void            Notify( const com::sun::star::uno::Sequence<rtl::OUString>& aPropertyNames);
+    virtual void            Commit();
+    void                    Load();
+
+    void SetLoadWordBasicCode( sal_Bool bFlag );
+    sal_Bool IsLoadWordBasicCode() const;
+    void SetLoadWordBasicStorage( sal_Bool bFlag );
+    sal_Bool IsLoadWordBasicStorage() const;
+
+    void SetLoadExcelBasicCode( sal_Bool bFlag );
+    sal_Bool IsLoadExcelBasicCode() const;
+    void SetLoadExcelBasicStorage( sal_Bool bFlag );
+    sal_Bool IsLoadExcelBasicStorage() const;
+
+    void SetLoadPPointBasicCode( sal_Bool bFlag );
+    sal_Bool IsLoadPPointBasicCode() const;
+    void SetLoadPPointBasicStorage( sal_Bool bFlag );
+    sal_Bool IsLoadPPointBasicStorage() const;
+
+    sal_Bool IsMathType2Math() const;
+    void SetMathType2Math( sal_Bool bFlag );
+    sal_Bool IsMath2MathType() const;
+    void SetMath2MathType( sal_Bool bFlag );
+
+    sal_Bool IsWinWord2Writer() const;
+    void SetWinWord2Writer( sal_Bool bFlag );
+    sal_Bool IsWriter2WinWord() const;
+    void SetWriter2WinWord( sal_Bool bFlag );
+
+    sal_Bool IsExcel2Calc() const;
+    void SetExcel2Calc( sal_Bool bFlag );
+    sal_Bool IsCalc2Excel() const;
+    void SetCalc2Excel( sal_Bool bFlag );
+
+    sal_Bool IsPowerPoint2Impress() const;
+    void SetPowerPoint2Impress( sal_Bool bFlag );
+    sal_Bool IsImpress2PowerPoint() const;
+    void SetImpress2PowerPoint( sal_Bool bFlag );
+
+    static SvtFilterOptions* SvtFilterOptions::Get();
+};
+
+#endif
+
+
+
+
