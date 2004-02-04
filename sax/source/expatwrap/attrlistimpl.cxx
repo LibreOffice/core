2d9587b3790fc41a8fa686ba139a079b713f3f19
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-04 12:40:21 +0000

    INTEGRATION: CWS ooo20031216 (1.3.32); FILE MERGED 2003/12/13 14:07:44 waratah 1.3.32.1: #i1858# test for previous definition of macro to remove warning

--- sax/source/expatwrap/attrlistimpl.cxx
+++ sax/source/expatwrap/attrlistimpl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: attrlistimpl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:32:30 $
+ *  last change: $Author: hr $ $Date: 2004-02-04 13:40:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,9 @@
 #include <vector>
 
 #if OSL_DEBUG_LEVEL == 0
-#define NDEBUG
+#  ifndef NDEBUG
+#    define NDEBUG
+#  endif
 #endif
 #include <assert.h>
 
