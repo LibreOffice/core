fb8edf6711bf88ada9a8161d71cd1ff80c65822c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-04 10:58:43 +0000

    INTEGRATION: CWS ooo20031216 (1.3.38); FILE MERGED 2004/01/22 16:52:40 mh 1.3.38.1: chg: compile for win, #i10000#

--- rsc/source/rscpp/cpp.h
+++ rsc/source/rscpp/cpp.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cpp.h,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-28 16:39:26 $
+ *  last change: $Author: hr $ $Date: 2004-02-04 11:58:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -372,7 +372,7 @@ int outputEval( int c );
 /* cpp2.c */
 int control( int counter );
 doinclude();
-dodefine();
+void dodefine();
 doif( int hash );
 openinclude( char *, int );
 hasdirectory(char *, char * );
