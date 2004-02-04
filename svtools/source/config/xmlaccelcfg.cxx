34eaeec3b653ea9453909fedfaa9fd3324980af6
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-04 12:57:11 +0000

    INTEGRATION: CWS ooo20031216 (1.2.228); FILE MERGED 2003/12/13 13:56:56 waratah 1.2.228.1: #i1858# test for previous definition of macro to remove warning

--- svtools/source/config/xmlaccelcfg.cxx
+++ svtools/source/config/xmlaccelcfg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlaccelcfg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:52:51 $
+ *  last change: $Author: hr $ $Date: 2004-02-04 13:57:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,9 @@
 
 #include <vector>
 #if OSL_DEBUG_LEVEL == 0
-#define NDEBUG
+#  ifndef NDEBUG
+#    define NDEBUG
+#  endif
 #endif
 #include <assert.h>
 
