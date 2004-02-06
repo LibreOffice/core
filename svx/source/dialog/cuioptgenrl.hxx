dfa8fc7dbc2943ed6c59cf34ee3d88111e39a70f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-06 10:35:50 +0000

    #100000,i24117#: formerly optgenrl.hxx

---
+++ svx/source/dialog/cuioptgenrl.hxx
@@ -0,0 +1,151 @@
+/*************************************************************************
+ *
+ *  $RCSfile: cuioptgenrl.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-02-06 11:35:50 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SVX_CUIOPTGENRL_HXX
+#define _SVX_CUIOPTGENRL_HXX
+
+// include ---------------------------------------------------------------
+
+#ifndef _SVX_OPTGENRL_HXX
+#include <optgenrl.hxx>
+#endif
+#ifndef _SFXTABDLG_HXX //autogen
+#include <sfx2/tabdlg.hxx>
+#endif
+#ifndef _FIXED_HXX //autogen
+#include <vcl/fixed.hxx>
+#endif
+#ifndef _EDIT_HXX //autogen
+#include <vcl/edit.hxx>
+#endif
+#ifndef _GROUP_HXX //autogen
+#include <vcl/group.hxx>
+#endif
+
+// forward ---------------------------------------------------------------
+
+class SvxAddressItem;
+struct GeneralTabPage_Impl;
+
+#define SfxGeneralTabPage SvxGeneralTabPage
+
+// class SvxGeneralTabPage -----------------------------------------------
+
+class SvxGeneralTabPage : public SfxTabPage
+{
+private:
+    FixedText           aCompanyLbl;
+    Edit                aCompanyEdit;
+    FixedText           aNameLbl;
+    FixedText           aNameLblRuss;
+    Edit                aFirstName;
+    Edit                aFatherName;
+    Edit                aName;
+    Edit                aShortName;
+    FixedText           aStreetLbl;
+    FixedText           aStreetLblRuss;
+    Edit                aStreetEdit;
+    Edit                aApartmentNrEdit;
+    FixedText           aCityLbl;
+    Edit                aPLZEdit;
+    Edit                aCityEdit;
+    Edit                aUsCityEdit;
+    Edit                aUsStateEdit;
+    Edit                aUsZipEdit;
+    FixedText           aCountryLbl;
+    Edit                aCountryEdit;
+    FixedText           aTitlePosLbl;
+    Edit                aTitleEdit;
+    Edit                aPositionEdit;
+    FixedText           aPhoneLbl;
+    Edit                aTelPrivEdit;
+    Edit                aTelCompanyEdit;
+    FixedText           aFaxMailLbl;
+    Edit                aFaxEdit;
+    Edit                aEmailEdit;
+    FixedLine           aAddrFrm;
+    CheckBox            aUseDataCB;
+
+    GeneralTabPage_Impl*    pImpl;
+
+
+#ifdef _SVX_OPTGENRL_CXX
+    DECL_LINK( ModifyHdl_Impl, Edit * );
+
+    String              GetAddress_Impl();
+    void                SetAddress_Impl( const SvxAddressItem& rAddress );
+#endif
+
+protected:
+    virtual int         DeactivatePage( SfxItemSet* pSet );
+
+public:
+    SvxGeneralTabPage( Window* pParent, const SfxItemSet& rSet );
+    ~SvxGeneralTabPage();
+
+    static SfxTabPage*  Create( Window* pParent, const SfxItemSet& rAttrSet );
+
+    virtual BOOL        FillItemSet( SfxItemSet& rSet );
+    virtual void        Reset( const SfxItemSet& rSet );
+};
+
+#endif // #ifndef _SVX_CUIOPTGENRL_HXX
+
+
