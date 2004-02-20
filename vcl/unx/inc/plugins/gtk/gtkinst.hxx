9072e25014399ef543542d0dbad0a7ff5f823eb7
Oliver Bolte <obo@openoffice.org>
2004-02-20 07:57:32 +0000

    INTEGRATION: CWS vcl17 (1.1.2); FILE ADDED 2004/01/22 12:52:41 pl 1.1.2.8: solved an access problem 2004/01/19 18:55:30 pl 1.1.2.7: add foundations for GtkSalObject 2004/01/19 15:45:32 pl 1.1.2.6: allow for recursive Grab/Ungrab 2003/12/16 23:08:41 mmeeks 1.1.2.5: Pass oslModule into plugin for symbol lookups Dig out & hook gdk_threads_enter/leave Add hooked locking sub-class 2003/12/15 18:12:09 pl 1.1.2.4: first cut at key events 2003/12/12 14:56:03 pl 1.1.2.3: adding gtk frame: first steps 2003/12/12 13:44:51 mmeeks 1.1.2.2: Split out SalSystem stuff into a shared header, virtualize the native dialog stuff to bin the nasty msgbox code.
    Don't g_thread_init if we have already - theoretical crasher
    add SAL_DEBUG_UPDATES
    
    ** Disable timeout ** - causing gtk+ rendering to barf, will fix next pass.
    
    re-hash native dialog code to make it more pleasant.
    2003/12/11 12:47:31 pl 1.1.2.1: move to more sensible dir structure

---
+++ vcl/unx/inc/plugins/gtk/gtkinst.hxx
@@ -0,0 +1,137 @@
+/*************************************************************************
+ *
+ *  $RCSfile: gtkinst.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-02-20 08:57:32 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _VCL_GTKINST_HXX
+#define _VCL_GTKINST_HXX
+
+#include <salinst.h>
+#include <salsys.h>
+
+class GtkYieldMutex : public SalYieldMutex
+{
+public:
+                                                GtkYieldMutex();
+    virtual void                                acquire();
+    virtual void                                release();
+    virtual sal_Bool                            tryToAcquire();
+
+    virtual int Grab();
+    virtual void Ungrab( int );
+
+    class GtkYieldGuard
+    {
+        GtkYieldMutex*  m_pMutex;
+        int             m_nGrab;
+    public:
+        GtkYieldGuard( GtkYieldMutex* pMutex )
+                : m_pMutex( pMutex )
+        {
+            m_nGrab = m_pMutex->Grab();
+        }
+        ~GtkYieldGuard()
+        {
+            m_pMutex->Ungrab( m_nGrab );
+        }
+    };
+};
+
+class GtkHookedYieldMutex : public GtkYieldMutex
+{
+    virtual int     Grab()              { return 0; };
+    virtual void        Ungrab(int )        {};
+    std::list<ULONG> aYieldStack;
+public:
+    GtkHookedYieldMutex();
+    virtual void      acquire() { SalYieldMutex::acquire(); }
+    virtual void      release() { SalYieldMutex::release(); }
+    virtual sal_Bool  tryToAcquire() { return SalYieldMutex::tryToAcquire(); }
+    void ThreadsEnter();
+    void ThreadsLeave();
+};
+
+
+#define GTK_YIELD_GRAB() GtkYieldMutex::GtkYieldGuard aLocalGtkYieldGuard( static_cast<GtkYieldMutex*>(GetSalData()->pInstance_->GetYieldMutex()) )
+
+class GtkInstance : public X11SalInstance
+{
+public:
+    GtkInstance( SalYieldMutex* pMutex )
+            : X11SalInstance( pMutex )
+    {}
+    virtual ~GtkInstance();
+
+    virtual SalFrame*           CreateFrame( SalFrame* pParent, ULONG nStyle );
+    virtual SalObject*          CreateObject( SalFrame* pParent );
+    virtual SalSystem*          CreateSalSystem();
+};
+
+class GtkSalSystem : public X11SalSystem
+{
+public:
+    GtkSalSystem() : X11SalSystem() {}
+    virtual ~GtkSalSystem();
+    virtual int ShowNativeDialog( const String& rTitle,
+                                  const String& rMessage,
+                                  const std::list< String >& rButtons,
+                                  int nDefButton );
+};
+
+#endif // _VCL_GTKINST_HXX
