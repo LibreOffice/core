50b3cc532cf15cf2230b21050a525e6cea89938a
Oliver Bolte <obo@openoffice.org>
2004-02-20 07:57:14 +0000

    INTEGRATION: CWS vcl17 (1.1.2); FILE ADDED 2004/02/12 12:34:06 pl 1.1.2.9: dispatch mouse events to the correct VCL frame 2003/12/18 13:09:58 mmeeks 1.1.2.8: cleanup cursors fix grab problems. 2003/12/17 21:28:29 mmeeks 1.1.2.7: Split shared cursor includes out, Implement GdkCursor support nicely 2003/12/17 13:13:18 mmeeks 1.1.2.6: Cursor bits .. Flush/Sync Don't return TRUE from ConfigureEvent (eg.) SetPointerPos Use widget's mapped state not separate boolean 2003/12/16 16:01:17 mmeeks 1.1.2.5: Create SalX11Display sub-class Bin 'bool' to constructor, and do via sub-classing Fixup XCloseDisplay duplication in destructor. 2003/12/15 18:12:08 pl 1.1.2.4: first cut at key events 2003/12/15 14:12:09 mmeeks 1.1.2.3: re-hash glib timeout code for efficiency Don't add the X socket to the poll a 2nd time (using un-virtualized callbacks) in SalDisplay 2003/12/12 14:56:01 pl 1.1.2.2: adding gtk frame: first steps 2003/12/11 12:47:30 pl 1.1.2.1: move to more sensible dir structure

---
+++ vcl/unx/inc/plugins/gtk/gtkdata.hxx
@@ -0,0 +1,112 @@
+/*************************************************************************
+ *
+ *  $RCSfile: gtkdata.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-02-20 08:57:14 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _VCL_GTKDATA_HXX
+#define _VCL_GTKDATA_HXX
+
+#include <prex.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkx.h>
+#include <gtk/gtk.h>
+#include <postx.h>
+
+#include <saldisp.hxx>
+#include <saldata.hxx>
+#include <ptrstyle.hxx>
+
+#include <list>
+
+class GtkData : public SalData
+{
+public:
+    GtkData() {}
+    virtual ~GtkData();
+
+    virtual void Init();
+};
+
+class GtkSalFrame;
+
+class GtkSalDisplay : public SalDisplay
+{
+    GdkDisplay*                     m_pGdkDisplay;
+    std::list< GtkSalFrame* >       m_aFrames;
+    GdkCursor                      *m_aCursors[ POINTER_COUNT ];
+    GdkCursor *getFromXPM( const char *pBitmap, const char *pMask,
+                           int nWidth, int nHeight, int nXHot, int nYHot );
+public:
+             GtkSalDisplay( GdkDisplay* pDisplay, Visual* pVis, Colormap aCol );
+    virtual ~GtkSalDisplay();
+
+    GdkDisplay* GetGdkDisplay() const { return m_pGdkDisplay; }
+
+    void registerFrame( GtkSalFrame* pFrame );
+    void deregisterFrame( GtkSalFrame* pFrame );
+    GdkCursor *getCursor( PointerStyle ePointerStyle );
+    virtual int CaptureMouse( SalFrame* pFrame );
+
+    static GdkFilterReturn filterGdkEvent( GdkXEvent* sys_event,
+                                           GdkEvent* event,
+                                           gpointer data );
+};
+
+
+#endif // _VCL_GTKDATA_HXX
