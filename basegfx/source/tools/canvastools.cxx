da430538da14849174861ee4fb3e2d2a2fcfb7b2
Thorsten Behrens <thb@openoffice.org>
2004-02-24 20:45:29 +0000

    Initial revisio

---
+++ basegfx/source/tools/canvastools.cxx
@@ -0,0 +1,353 @@
+/*************************************************************************
+ *
+ *  $RCSfile: canvastools.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: thb $ $Date: 2004-02-24 21:45:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_REALSIZE2D_HPP__
+#include <drafts/com/sun/star/geometry/RealSize2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_REALPOINT2D_HPP__
+#include <drafts/com/sun/star/geometry/RealPoint2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_REALRECTANGLE2D_HPP__
+#include <drafts/com/sun/star/geometry/RealRectangle2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_REALBEZIERSEGMENT2D_HPP__
+#include <drafts/com/sun/star/geometry/RealBezierSegment2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_AFFINEMATRIX2D_HPP_
+#include <drafts/com/sun/star/geometry/AffineMatrix2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_INTEGERSIZE2D_HPP__
+#include <drafts/com/sun/star/geometry/IntegerSize2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_INTEGERPOINT2D_HPP__
+#include <drafts/com/sun/star/geometry/IntegerPoint2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_GEOMETRY_INTEGERRECTANGLE2D_HPP__
+#include <drafts/com/sun/star/geometry/IntegerRectangle2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XPOLYPOLYGON2D_HPP__
+#include <drafts/com/sun/star/rendering/XPolyPolygon2D.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XGRAPHICDEVICE_HPP__
+#include <drafts/com/sun/star/rendering/XGraphicDevice.hpp>
+#endif
+
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/vector/b2dsize.hxx>
+#include <basegfx/point/b2dpoint.hxx>
+#include <basegfx/range/b2drectangle.hxx>
+#include <basegfx/vector/b2isize.hxx>
+#include <basegfx/point/b2ipoint.hxx>
+#include <basegfx/range/b2irectangle.hxx>
+#include <basegfx/polygon/b2dpolygon.hxx>
+#include <basegfx/polygon/b2dpolypolygon.hxx>
+
+#include <basegfx/tools/canvastools.hxx>
+
+
+
+using namespace ::drafts::com::sun::star;
+using namespace ::com::sun::star;
+
+namespace basegfx
+{
+
+    namespace unotools
+    {
+        namespace
+        {
+            uno::Sequence< geometry::RealBezierSegment2D > bezierSequenceFromB2DPolygon( const ::basegfx::B2DPolygon& rPoly )
+            {
+                const sal_uInt32 nNumPoints( rPoly.count() );
+
+                uno::Sequence< geometry::RealBezierSegment2D > outputSequence( nNumPoints );
+
+                // fill sequence from polygon
+                sal_uInt32 i;
+                for( i=0; i<nNumPoints; ++i )
+                {
+                    const ::basegfx::B2DPoint   aStartPoint( rPoly.getB2DPoint(i) );
+                    const ::basegfx::B2DVector  aCtrl1( rPoly.getControlVectorA(i) );
+                    const ::basegfx::B2DVector  aCtrl2( rPoly.getControlVectorB(i) );
+
+                    if( aCtrl1.equalZero() &&
+                        aCtrl2.equalZero() )
+                    {
+                        outputSequence[i] = geometry::RealBezierSegment2D( aStartPoint.getX(),
+                                                                           aStartPoint.getY(),
+                                                                           aStartPoint.getX(),
+                                                                           aStartPoint.getY(),
+                                                                           aStartPoint.getX(),
+                                                                           aStartPoint.getY() );
+                    }
+                    else
+                    {
+                        outputSequence[i] = geometry::RealBezierSegment2D( aStartPoint.getX(),
+                                                                           aStartPoint.getY(),
+                                                                           aCtrl1.getX(),
+                                                                           aCtrl1.getY(),
+                                                                           aCtrl2.getX(),
+                                                                           aCtrl2.getY() );
+                    }
+                }
+
+                return outputSequence;
+            }
+
+            uno::Sequence< geometry::RealPoint2D > pointSequenceFromB2DPolygon( const ::basegfx::B2DPolygon& rPoly )
+            {
+                const sal_uInt32 nNumPoints( rPoly.count() );
+
+                uno::Sequence< geometry::RealPoint2D > outputSequence( nNumPoints );
+
+                // fill sequence from polygon
+                sal_uInt32 i;
+                for( i=0; i<nNumPoints; ++i )
+                {
+                    const ::basegfx::B2DPoint   aPoint( rPoly.getB2DPoint(i) );
+
+                    outputSequence[i] = geometry::RealPoint2D( aPoint.getX(),
+                                                               aPoint.getY() );
+                }
+
+                return outputSequence;
+            }
+        }
+
+        //---------------------------------------------------------------------------------------
+
+        uno::Reference< rendering::XPolyPolygon2D > xPolyPolygonFromB2DPolygon( const uno::Reference< rendering::XGraphicDevice >&  xGraphicDevice,
+                                                                                const ::basegfx::B2DPolygon&                        rPoly    )
+        {
+            if( !xGraphicDevice.is() )
+                return uno::Reference< rendering::XPolyPolygon2D >();
+
+            if( rPoly.areControlPointsUsed() )
+            {
+                uno::Sequence< uno::Sequence< geometry::RealBezierSegment2D > > outputSequence( 1 );
+                outputSequence[0] = bezierSequenceFromB2DPolygon( rPoly );
+
+                return uno::Reference< rendering::XPolyPolygon2D >(
+                    xGraphicDevice->createCompatibleBezierPolyPolygon( outputSequence ),
+                    uno::UNO_QUERY );
+            }
+            else
+            {
+                uno::Sequence< uno::Sequence< geometry::RealPoint2D > > outputSequence( 1 );
+                outputSequence[0] = pointSequenceFromB2DPolygon( rPoly );
+
+                return uno::Reference< rendering::XPolyPolygon2D >(
+                    xGraphicDevice->createCompatibleLinePolyPolygon( outputSequence ),
+                    uno::UNO_QUERY );
+            }
+        }
+
+        //---------------------------------------------------------------------------------------
+
+        uno::Reference< rendering::XPolyPolygon2D > xPolyPolygonFromB2DPolyPolygon( const uno::Reference< rendering::XGraphicDevice >&  xGraphicDevice,
+                                                                                    const ::basegfx::B2DPolyPolygon&                    rPolyPoly    )
+        {
+            if( !xGraphicDevice.is() )
+                return uno::Reference< rendering::XPolyPolygon2D >();
+
+            const sal_uInt32 nNumPolies( rPolyPoly.count() );
+            sal_uInt32 i;
+
+            if( rPolyPoly.areControlPointsUsed() )
+            {
+                uno::Sequence< uno::Sequence< geometry::RealBezierSegment2D > > outputSequence( nNumPolies );
+
+                for( i=0; i<nNumPolies; ++i )
+                {
+                    outputSequence[i] = bezierSequenceFromB2DPolygon( rPolyPoly.getB2DPolygon(i) );
+                }
+
+                return uno::Reference< rendering::XPolyPolygon2D >(
+                    xGraphicDevice->createCompatibleBezierPolyPolygon( outputSequence ),
+                    uno::UNO_QUERY );
+            }
+            else
+            {
+                uno::Sequence< uno::Sequence< geometry::RealPoint2D > > outputSequence( nNumPolies );
+
+                for( i=0; i<nNumPolies; ++i )
+                {
+                    outputSequence[i] = pointSequenceFromB2DPolygon( rPolyPoly.getB2DPolygon(i) );
+                }
+
+                return uno::Reference< rendering::XPolyPolygon2D >(
+                    xGraphicDevice->createCompatibleLinePolyPolygon( outputSequence ),
+                    uno::UNO_QUERY );
+            }
+        }
+
+        //---------------------------------------------------------------------------------------
+
+        ::basegfx::B2DHomMatrix& homMatrixFromAffineMatrix( ::basegfx::B2DHomMatrix&        output,
+                                                            const geometry::AffineMatrix2D& input )
+        {
+            // ensure last row is [0,0,1] (and optimized away)
+            output.identity();
+
+            output.set(0,0, input.m00);
+            output.set(0,1, input.m01);
+            output.set(0,2, input.m02);
+            output.set(1,0, input.m10);
+            output.set(1,1, input.m11);
+            output.set(1,2, input.m12);
+
+            return output;
+        }
+
+        geometry::AffineMatrix2D& affineMatrixFromHomMatrix( geometry::AffineMatrix2D&      output,
+                                                             const ::basegfx::B2DHomMatrix& input)
+        {
+            output.m00 = input.get(0,0);
+            output.m01 = input.get(0,1);
+            output.m02 = input.get(0,2);
+            output.m10 = input.get(1,0);
+            output.m11 = input.get(1,1);
+            output.m12 = input.get(1,2);
+
+            return output;
+        }
+
+        //---------------------------------------------------------------------------------------
+
+        geometry::RealSize2D size2DFromB2DSize( const ::basegfx::B2DVector& rVec )
+        {
+            return geometry::RealSize2D( rVec.getX(),
+                                         rVec.getY() );
+        }
+
+        geometry::RealPoint2D point2DFromB2DPoint( const ::basegfx::B2DPoint& rPoint )
+        {
+            return geometry::RealPoint2D( rPoint.getX(),
+                                          rPoint.getY() );
+        }
+
+        geometry::RealRectangle2D rectangle2DFromB2DRectangle( const ::basegfx::B2DRange& rRect )
+        {
+            return geometry::RealRectangle2D( rRect.getMinX(),
+                                              rRect.getMinY(),
+                                              rRect.getMaxX(),
+                                              rRect.getMaxY() );
+        }
+
+        ::basegfx::B2DVector b2DSizeFromRealSize2D( const geometry::RealSize2D& rSize )
+        {
+            return ::basegfx::B2DVector( rSize.Width,
+                                         rSize.Height );
+        }
+
+        ::basegfx::B2DPoint b2DPointFromRealPoint2D( const geometry::RealPoint2D& rPoint )
+        {
+            return ::basegfx::B2DPoint( rPoint.X,
+                                        rPoint.Y );
+        }
+
+        ::basegfx::B2DRange b2DRectangleFromRealRectangle2D( const geometry::RealRectangle2D& rRect )
+        {
+            return ::basegfx::B2DRange( rRect.X1,
+                                        rRect.Y1,
+                                        rRect.X2,
+                                        rRect.Y2 );
+        }
+
+        geometry::IntegerSize2D integerSize2DFromB2ISize( const ::basegfx::B2IVector& rSize )
+        {
+            return geometry::IntegerSize2D( rSize.getX(),
+                                            rSize.getY() );
+        }
+
+        geometry::IntegerPoint2D integerPoint2DFromB2IPoint( const ::basegfx::B2IPoint& rPoint )
+        {
+            return geometry::IntegerPoint2D( rPoint.getX(),
+                                             rPoint.getY() );
+        }
+
+        geometry::IntegerRectangle2D integerRectangle2DFromB2IRectangle( const ::basegfx::B2IRange& rRectangle )
+        {
+            return geometry::IntegerRectangle2D( rRectangle.getMinX(), rRectangle.getMinY(),
+                                                 rRectangle.getMaxX(), rRectangle.getMaxY() );
+        }
+
+        ::basegfx::B2IVector b2ISizeFromIntegerSize2D( const geometry::IntegerSize2D& rSize )
+        {
+            return ::basegfx::B2IVector( rSize.Width,
+                                         rSize.Height );
+        }
+
+        ::basegfx::B2IPoint b2IPointFromIntegerPoint2D( const geometry::IntegerPoint2D& rPoint )
+        {
+            return ::basegfx::B2IPoint( rPoint.X,
+                                        rPoint.Y );
+        }
+
+        ::basegfx::B2IRange b2IRectangleFromIntegerRectangle2D( const geometry::IntegerRectangle2D& rRectangle )
+        {
+            return ::basegfx::B2IRange( rRectangle.X1, rRectangle.Y1,
+                                        rRectangle.X2, rRectangle.Y2 );
+        }
+
+    } // namespace bgfxtools
+
+} // namespace canvas
