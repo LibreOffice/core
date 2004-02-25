002b475d640335955a314dec2f642d2bb5dfdcdb
Kurt Zenker <kz@openoffice.org>
2004-02-25 16:08:29 +0000

    INTEGRATION: CWS layoutmanager (1.1.2); FILE ADDED 2003/11/18 15:05:26 abi 1.1.2.2: recycling Frame,Window and Document 2003/10/29 13:40:59 abi 1.1.2.1: new files for inplace activation

---
+++ embedserv/source/embed/ed_iinplace.cxx
@@ -0,0 +1,110 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ed_iinplace.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-02-25 17:08:29 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "embeddoc.hxx"
+
+#ifndef _OSL_DIAGNOSE_H_
+#include <ols/diagnose.h>
+#endif
+
+
+STDMETHODIMP EmbedDocument_Impl::GetWindow(HWND *hWnd)
+{
+    OSL_ENSURE(m_pDocHolder,"no document for inplace activation");
+
+    *hWnd = m_pDocHolder->GetTopMostWinHandle();
+    if(*hWnd != NULL)
+        return NOERROR;
+    else
+        return ERROR;
+}
+
+STDMETHODIMP EmbedDocument_Impl::ContextSensitiveHelp(BOOL)
+{
+    return NOERROR;
+}
+
+STDMETHODIMP EmbedDocument_Impl::InPlaceDeactivate(void)
+{
+    m_pDocHolder->InPlaceDeactivate();
+    return NOERROR;
+}
+
+STDMETHODIMP EmbedDocument_Impl::UIDeactivate(void)
+{
+    m_pDocHolder->UIDeactivate();
+    return NOERROR;
+}
+
+STDMETHODIMP EmbedDocument_Impl::SetObjectRects(LPCRECT aRect, LPCRECT aClip)
+{
+    OSL_ENSURE(m_pDocHolder,"no document for inplace activation");
+
+    return m_pDocHolder->SetObjectRects(aRect,aClip);
+}
+
+STDMETHODIMP EmbedDocument_Impl::ReactivateAndUndo(void)
+{
+    return E_NOTIMPL;
+}
+
+
+
