2adcce02f12b9f94b15f04c65681ca8ad9e8ca95
Kurt Zenker <kz@openoffice.org>
2004-02-25 16:37:47 +0000

    INTEGRATION: CWS layoutmanager (1.1.2); FILE ADDED 2004/01/30 16:47:19 cd 1.1.2.7: #111899# Uses command description service correctly 2004/01/28 16:18:48 cd 1.1.2.6: #111899# index container implementation for mulit-thread env 2004/01/19 12:57:33 cd 1.1.2.5: #111899# Some bugfixes 2003/10/31 16:37:47 cd 1.1.2.4: #111899# Fixed some problems with OLE inplace editing 2003/10/28 08:27:12 cd 1.1.2.3: #111899# Some changes for user interface configuration API 2003/10/08 14:43:45 cd 1.1.2.2: #111899# Provide interface to retrieve system dependent menu handle 2003/09/12 08:47:59 cd 1.1.2.1: #111899# Added classes for new user interface implementation

---
+++ framework/inc/uielement/menubarmanager.hxx
@@ -0,0 +1,273 @@
+/*************************************************************************
+ *
+ *  $RCSfile: menubarmanager.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-02-25 17:37:47 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_UIELEMENT_MENUBARMANAGER_HXX_
+#define __FRAMEWORK_UIELEMENT_MENUBARMANAGER_HXX_
+
+/** Attention: stl headers must(!) be included at first. Otherwhise it can make trouble
+               with solaris headers ...
+*/
+#include <vector>
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#include <threadhelp/threadhelpbase.hxx>
+#endif
+#ifndef __FRAMEWORK_MACROS_DEBUG_HXX_
+#include <macros/debug.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_FRAME_XFRAME_HPP_
+#include <com/sun/star/frame/XFrame.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCH_HPP_
+#include <com/sun/star/frame/XDispatch.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_FEATURESTATEEVENT_HPP_
+#include <com/sun/star/frame/FeatureStateEvent.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XFRAMEACTIONLISTENER_HPP_
+#include <com/sun/star/frame/XFrameActionListener.hpp>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_FRAME_XPOPUPMENUCONTROLLER_HPP_
+#include <drafts/com/sun/star/frame/XPopupMenuController.hpp>
+#endif
+#ifndef _COM_SUN_STAR_AWT_XSYSTEMDEPENDENTMENUPEER_HPP_
+#include <com/sun/star/awt/XSystemDependentMenuPeer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXACCESS_HPP_
+#include <com/sun/star/container/XIndexAccess.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMEACCESS_HPP_
+#include <com/sun/star/container/XNameAccess.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
+#include <com/sun/star/lang/XComponent.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTICOMPONENTFACTORY_HPP_
+#include <com/sun/star/lang/XMultiComponentFactory.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_FRAME_XUICONTROLLERREGISTRATION_HPP_
+#include <drafts/com/sun/star/frame/XUIControllerRegistration.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _RTL_USTRING_
+#include <rtl/ustring.hxx>
+#endif
+#ifndef _SV_MENU_HXX
+#include <vcl/menu.hxx>
+#endif
+#ifndef _SV_ACCEL_HXX
+#include <vcl/accel.hxx>
+#endif
+#ifndef _TOOLKIT_AWT_VCLXMENU_HXX_
+#include <toolkit/awt/vclxmenu.hxx>
+#endif
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+#ifndef _CPPUHELPER_INTERFACECONTAINER_HXX_
+#include <cppuhelper/interfacecontainer.hxx>
+#endif
+
+namespace framework
+{
+
+class BmkMenu;
+class AddonMenu;
+class AddonPopupMenu;
+class MenuBarManager : public com::sun::star::frame::XStatusListener        ,
+                       public com::sun::star::frame::XFrameActionListener   ,
+                       public com::sun::star::lang::XComponent              ,
+                       public com::sun::star::awt::XSystemDependentMenuPeer ,
+                       public ThreadHelpBase                                ,
+                       public ::cppu::OWeakObject
+{
+    protected:
+        MenuBarManager( com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame,
+                        BmkMenu*            pBmkMenu,
+                        sal_Bool            bDelete,
+                        sal_Bool            bDeleteChildren );
+
+        MenuBarManager( com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame,
+                        AddonMenu*          pAddonMenu,
+                        sal_Bool            bDelete,
+                        sal_Bool            bDeleteChildren );
+
+        MenuBarManager( com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame,
+                        AddonPopupMenu*     pAddonMenu,
+                        sal_Bool            bDelete,
+                        sal_Bool            bDeleteChildren );
+
+    public:
+        MenuBarManager( com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame,
+                        Menu* pMenu,
+                        sal_Bool bDelete,
+                        sal_Bool bDeleteChildren );
+
+        virtual ~MenuBarManager();
+
+        // XInterface
+        virtual void SAL_CALL acquire() throw();
+        virtual void SAL_CALL release() throw();
+        virtual ::com::sun::star::uno::Any SAL_CALL queryInterface( const ::com::sun::star::uno::Type & rType ) throw( ::com::sun::star::uno::RuntimeException );
+
+        // XComponent
+        virtual void SAL_CALL dispose() throw (::com::sun::star::uno::RuntimeException);
+        virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+        virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XStatusListener
+        virtual void SAL_CALL statusChanged( const com::sun::star::frame::FeatureStateEvent& Event ) throw ( ::com::sun::star::uno::RuntimeException );
+
+        // XFrameActionListener
+        virtual void SAL_CALL frameAction( const com::sun::star::frame::FrameActionEvent& Action ) throw ( ::com::sun::star::uno::RuntimeException );
+
+        // XEventListener
+        virtual void SAL_CALL disposing( const ::com::sun::star::lang::EventObject& Source ) throw ( ::com::sun::star::uno::RuntimeException );
+
+        // XSystemDependentMenuPeer
+        virtual ::com::sun::star::uno::Any SAL_CALL getMenuHandle( const ::com::sun::star::uno::Sequence< sal_Int8 >& ProcessId, sal_Int16 SystemType ) throw (::com::sun::star::uno::RuntimeException);
+
+        DECL_LINK( Select, Menu * );
+
+        Menu*   GetMenuBar() const { return m_pVCLMenu; }
+
+        // Configuration methods
+        static void FillMenu( USHORT& nId, Menu* pMenu, const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& rItemContainer );
+        void FillMenuManager( Menu* pMenu, ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >& rFrame, sal_Bool bDelete, sal_Bool bDeleteChildren );
+        void SetItemContainer( const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& rItemContainer );
+
+    protected:
+        DECL_LINK( Highlight, Menu * );
+        DECL_LINK( Activate, Menu * );
+        DECL_LINK( Deactivate, Menu * );
+
+        void RemoveListener();
+
+    private:
+        String RetrieveLabelFromCommand( const String& aCmdURL );
+        void UpdateSpecialFileMenu( Menu* pMenu );
+        void UpdateSpecialWindowMenu( Menu* pMenu );
+
+
+        struct MenuItemHandler
+        {
+            MenuItemHandler( USHORT             aItemId,
+                             ::com::sun::star::uno::Reference< ::com::sun::star::frame::XStatusListener >& xManager,
+                             ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatch >& rDispatch ) :
+                             nItemId( aItemId ),
+                             xSubMenuManager( xManager ),
+                             xMenuItemDispatch( rDispatch ) {}
+
+            USHORT                                                                                      nItemId;
+            ::rtl::OUString                                                                             aTargetFrame;
+            ::rtl::OUString                                                                             aMenuItemURL;
+            ::rtl::OUString                                                                             aFilter;
+            ::rtl::OUString                                                                             aPassword;
+            ::rtl::OUString                                                                             aTitle;
+            ::com::sun::star::uno::Reference< ::com::sun::star::frame::XStatusListener >                xSubMenuManager;
+            ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatch >                      xMenuItemDispatch;
+            ::com::sun::star::uno::Reference< ::drafts::com::sun::star::frame::XPopupMenuController >   xPopupMenuController;
+            ::com::sun::star::uno::Reference< ::com::sun::star::awt::XPopupMenu >                       xPopupMenu;
+        };
+
+        void             CreatePicklistArguments(
+                            ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aArgsList,
+                            const MenuItemHandler* );
+
+        MenuItemHandler* GetMenuItemHandler( USHORT nItemId );
+
+        sal_Bool                                                                                       m_bDisposed : 1,
+                                                                                                       m_bInitialized : 1,
+                                                                                                       m_bDeleteMenu : 1,
+                                                                                                       m_bDeleteChildren : 1,
+                                                                                                       m_bActive : 1,
+                                                                                                       m_bIsBookmarkMenu : 1,
+                                                                                                       m_bWasHiContrast : 1,
+                                                                                                       m_bShowMenuImages : 1;
+        ::rtl::OUString                                                                                m_aMenuItemCommand;
+        ::rtl::OUString                                                                                m_aModuleIdentifier;
+        Menu*                                                                                          m_pVCLMenu;
+        ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >                            m_xFrame;
+        ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameAccess >                   m_xUICommandLabels;
+        ::com::sun::star::uno::Reference< ::drafts::com::sun::star::frame::XUIControllerRegistration > m_xPopupMenuControllerRegistration;
+        ::std::vector< MenuItemHandler* >                                                              m_aMenuItemHandlerVector;
+        ::cppu::OMultiTypeInterfaceContainerHelper                                                     m_aListenerContainer;   /// container for ALL Listener
+};
+
+} // namespace
+
+#endif
