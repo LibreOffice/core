d6085f34c22ec6264c24a883875afd404f752c87
Kurt Zenker <kz@openoffice.org>
2004-02-25 16:47:19 +0000

    INTEGRATION: CWS layoutmanager (1.1.2); FILE ADDED 2004/02/19 09:04:50 cd 1.1.2.4: #i25017# Check reference before usage 2003/11/04 12:06:39 cd 1.1.2.3: #111899# Menus use activate to get status updates 2003/10/31 16:38:01 cd 1.1.2.2: #111899# Fixed some problems with OLE inplace editing 2003/09/12 08:50:52 cd 1.1.2.1: #111899# Added base class for new user interface implementation

---
+++ framework/source/helper/popupmenucontrollerbase.cxx
@@ -0,0 +1,309 @@
+/*************************************************************************
+ *
+ *  $RCSfile: popupmenucontrollerbase.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-02-25 17:47:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_HELPER_POPUPMENUCONTROLLERBASE_HXX_
+#include <helper/popupmenucontrollerbase.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_THREADHELP_RESETABLEGUARD_HXX_
+#include <threadhelp/resetableguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_SERVICES_H_
+#include "services.h"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_AWT_XDEVICE_HPP_
+#include <com/sun/star/awt/XDevice.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_AWT_MENUITEMSTYLE_HPP_
+#include <com/sun/star/awt/MenuItemStyle.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UTIL_XURLTRANSFORMER_HPP_
+#include <com/sun/star/util/XURLTransformer.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDER_HPP_
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#endif
+
+
+//_________________________________________________________________________________________________________________
+//  includes of other projects
+//_________________________________________________________________________________________________________________
+
+#ifndef _VCL_MENU_HXX_
+#include <vcl/menu.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  Defines
+//_________________________________________________________________________________________________________________
+//
+
+using namespace com::sun::star::uno;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::frame;
+using namespace com::sun::star::beans;
+using namespace com::sun::star::util;
+
+namespace framework
+{
+
+//*****************************************************************************************************************
+//  XInterface, XTypeProvider, XServiceInfo
+//*****************************************************************************************************************
+DEFINE_XINTERFACE_8                     (   PopupMenuControllerBase                                                     ,
+                                            OWeakObject                                                                 ,
+                                            DIRECT_INTERFACE( css::lang::XTypeProvider                                  ),
+                                            DIRECT_INTERFACE( css::lang::XServiceInfo                                   ),
+                                            DIRECT_INTERFACE( drafts::com::sun::star::frame::XPopupMenuController       ),
+                                            DIRECT_INTERFACE( css::lang::XInitialization                                ),
+                                            DIRECT_INTERFACE( css::frame::XStatusListener                               ),
+                                            DIRECT_INTERFACE( css::awt::XMenuListener                                   ),
+                                            DERIVED_INTERFACE( css::lang::XEventListener, css::frame::XStatusListener   ),
+                                            DERIVED_INTERFACE( css::lang::XEventListener, css::awt::XMenuListener       )
+                                        )
+
+DEFINE_XTYPEPROVIDER_7                  (   PopupMenuControllerBase                             ,
+                                            css::lang::XTypeProvider                            ,
+                                            css::lang::XServiceInfo                             ,
+                                            drafts::com::sun::star::frame::XPopupMenuController ,
+                                            css::lang::XInitialization                          ,
+                                            css::frame::XStatusListener                         ,
+                                            css::awt::XMenuListener                             ,
+                                            css::lang::XEventListener
+                                        )
+
+PopupMenuControllerBase::PopupMenuControllerBase( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceManager ) :
+    ThreadHelpBase(),
+    m_xServiceManager( xServiceManager ),
+    m_bInitialized( sal_False )
+{
+}
+
+PopupMenuControllerBase::~PopupMenuControllerBase()
+{
+}
+
+// protected function
+void PopupMenuControllerBase::resetPopupMenu( com::sun::star::uno::Reference< com::sun::star::awt::XPopupMenu >& rPopupMenu )
+{
+    VCLXPopupMenu* pPopupMenu = 0;
+    if ( rPopupMenu.is() && rPopupMenu->getItemCount() > 0 )
+    {
+         pPopupMenu = (VCLXPopupMenu *)VCLXMenu::GetImplementation( rPopupMenu );
+         if ( pPopupMenu )
+         {
+            vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+            PopupMenu* pVCLPopupMenu = (PopupMenu *)pPopupMenu->GetMenu();
+            pVCLPopupMenu->Clear();
+         }
+    }
+}
+
+// XStatusListener
+void SAL_CALL PopupMenuControllerBase::statusChanged( const FeatureStateEvent& Event ) throw ( RuntimeException )
+{
+    // must be implemented by sub class
+}
+
+// XEventListener
+void SAL_CALL PopupMenuControllerBase::disposing( const EventObject& Source ) throw ( RuntimeException )
+{
+    ResetableGuard aLock( m_aLock );
+    m_xFrame.clear();
+    m_xDispatch.clear();
+    m_xPopupMenu.clear();
+}
+
+// XMenuListener
+void SAL_CALL PopupMenuControllerBase::highlight( const css::awt::MenuEvent& rEvent ) throw (RuntimeException)
+{
+}
+
+void SAL_CALL PopupMenuControllerBase::select( const css::awt::MenuEvent& rEvent ) throw (RuntimeException)
+{
+    Reference< css::awt::XPopupMenu >   xPopupMenu;
+    Reference< XDispatch >              xDispatch;
+    Reference< XMultiServiceFactory >   xServiceManager;
+
+    ResetableGuard aLock( m_aLock );
+    xPopupMenu      = m_xPopupMenu;
+    xDispatch       = m_xDispatch;
+    xServiceManager = m_xServiceManager;
+    aLock.unlock();
+
+    if ( xPopupMenu.is() && xDispatch.is() )
+    {
+        VCLXPopupMenu* pPopupMenu = (VCLXPopupMenu *)VCLXPopupMenu::GetImplementation( xPopupMenu );
+        if ( pPopupMenu )
+        {
+            css::util::URL               aTargetURL;
+            Sequence<PropertyValue>      aArgs;
+            Reference< XURLTransformer > xURLTransformer( xServiceManager->createInstance(
+                                                            rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.util.URLTransformer" ))),
+                                                        UNO_QUERY );
+
+            {
+                vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+                PopupMenu* pVCLPopupMenu = (PopupMenu *)pPopupMenu->GetMenu();
+                aTargetURL.Complete = pVCLPopupMenu->GetItemCommand( rEvent.MenuId );
+            }
+
+            xURLTransformer->parseStrict( aTargetURL );
+            xDispatch->dispatch( aTargetURL, aArgs );
+        }
+    }
+}
+
+void SAL_CALL PopupMenuControllerBase::activate( const css::awt::MenuEvent& rEvent ) throw (RuntimeException)
+{
+}
+
+void SAL_CALL PopupMenuControllerBase::deactivate( const css::awt::MenuEvent& rEvent ) throw (RuntimeException)
+{
+}
+
+// XPopupMenuController
+void SAL_CALL PopupMenuControllerBase::setPopupMenu( const Reference< css::awt::XPopupMenu >& PopupMenu ) throw (RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_xFrame.is() && !m_xPopupMenu.is() )
+        m_xPopupMenu = PopupMenu;
+}
+
+void SAL_CALL PopupMenuControllerBase::updatePopupMenu() throw ( ::com::sun::star::uno::RuntimeException )
+{
+    ResetableGuard aLock( m_aLock );
+    Reference< XStatusListener > xStatusListener( static_cast< OWeakObject* >( this ), UNO_QUERY );
+    Reference< XDispatch > xDispatch( m_xDispatch );
+    Reference< XURLTransformer > xURLTransformer( m_xServiceManager->createInstance(
+                                                    rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.util.URLTransformer" ))),
+                                                UNO_QUERY );
+    com::sun::star::util::URL aTargetURL;
+    aTargetURL.Complete = m_aCommandURL;
+    xURLTransformer->parseStrict( aTargetURL );
+    aLock.unlock();
+
+    // Add/remove status listener to get a status update once
+    if ( xDispatch.is() )
+    {
+        xDispatch->addStatusListener( xStatusListener, aTargetURL );
+        xDispatch->removeStatusListener( xStatusListener, aTargetURL );
+    }
+}
+
+// XInitialization
+void SAL_CALL PopupMenuControllerBase::initialize( const Sequence< Any >& aArguments ) throw ( Exception, RuntimeException )
+{
+    const rtl::OUString aFrameName( RTL_CONSTASCII_USTRINGPARAM( "Frame" ));
+    const rtl::OUString aCommandURLName( RTL_CONSTASCII_USTRINGPARAM( "CommandURL" ));
+
+    ResetableGuard aLock( m_aLock );
+
+    sal_Bool bInitalized( m_bInitialized );
+    if ( !bInitalized )
+    {
+        PropertyValue       aPropValue;
+        rtl::OUString       aCommandURL;
+        Reference< XFrame > xFrame;
+
+        for ( int i = 0; i < aArguments.getLength(); i++ )
+        {
+            if ( aArguments[i] >>= aPropValue )
+            {
+                if ( aPropValue.Name.equalsAscii( "Frame" ))
+                    aPropValue.Value >>= xFrame;
+                else if ( aPropValue.Name.equalsAscii( "CommandURL" ))
+                    aPropValue.Value >>= aCommandURL;
+            }
+        }
+
+        if ( xFrame.is() && aCommandURL.getLength() )
+        {
+            ResetableGuard aLock( m_aLock );
+            m_xFrame        = xFrame;
+            m_aCommandURL   = aCommandURL;
+        }
+    }
+}
+
+}
