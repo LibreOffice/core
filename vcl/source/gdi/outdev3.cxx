10454a8a1621a88aa85ec1114133832f2b803c6f
Kurt Zenker <kz@openoffice.org>
2004-02-25 13:28:49 +0000

    INTEGRATION: CWS geordi2q15 (1.167.18); FILE MERGED 2004/02/20 13:09:53 hr 1.167.18.1: #111934#: merge CWS ooo111fix3

--- vcl/source/gdi/outdev3.cxx
+++ vcl/source/gdi/outdev3.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: outdev3.cxx,v $
  *
- *  $Revision: 1.168 $
+ *  $Revision: 1.169 $
  *
- *  last change: $Author: obo $ $Date: 2004-02-20 08:51:14 $
+ *  last change: $Author: kz $ $Date: 2004-02-25 14:28:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -2665,7 +2665,7 @@ ImplFontEntry* ImplFontCache::GetFallback( ImplDevFontList* pFontList,
     {
         // TODO: implement dynamic lists or improve static lists
         #define FALLBACKFONT_NAMELIST \
-            "arialunicodems;andalesansui;cyberbit;starsymbol;lucidatypeWriter;"  \
+            "arialunicodems;andalesansui;cyberbit;starsymbol;opensymbol;lucidatypewriter;"  \
             "fzmingti;sunbatang;sundotum;baekmukdotum;"                     \
             "hgmincholightj;msunglightsc;msunglighttc;hymyeongjolightk;"    \
             "lucidasans;tahoma;"                                            \
