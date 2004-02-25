c1cad8c0c2807c892e02787089221d598881473a
Kurt Zenker <kz@openoffice.org>
2004-02-25 16:36:13 +0000

    INTEGRATION: CWS layoutmanager (1.1.2); FILE ADDED 2004/01/28 16:27:10 cd 1.1.2.4: #111899# Adapt code for new index container implementation 2004/01/21 18:10:43 cd 1.1.2.3: #111899# Some bugfixes 2004/01/20 08:11:54 cd 1.1.2.2: #111899# Fixed compile problems 2004/01/19 12:55:51 cd 1.1.2.1: #111899# Added ui configuration managers for documents and modules

---
+++ framework/inc/uiconfiguration/uiconfigurationmanager.hxx
@@ -0,0 +1,291 @@
+/*************************************************************************
+ *
+ *  $RCSfile: uiconfigurationmanager.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-02-25 17:36:13 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_UICONFIGURATION_UICONFIGMANAGER_HXX_
+#define __FRAMEWORK_UICONFIGURATION_UICONFIGMANAGER_HXX_
+
+
+/** Attention: stl headers must(!) be included at first. Otherwhise it can make trouble
+               with solaris headers ...
+*/
+#include <vector>
+#include <list>
+#include <hash_map>
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#include <threadhelp/threadhelpbase.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_GENERIC_HXX_
+#include <macros/generic.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XINTERFACE_HXX_
+#include <macros/xinterface.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XTYPEPROVIDER_HXX_
+#include <macros/xtypeprovider.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XSERVICEINFO_HXX_
+#include <macros/xserviceinfo.hxx>
+#endif
+
+#ifndef __FRAMEWORK_STDTYPES_H_
+#include <stdtypes.h>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
+#include <com/sun/star/lang/XComponent.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_XUICONFIGURATION_HPP_
+#include <drafts/com/sun/star/ui/XUIConfiguration.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_XUICONFIGURATIONPERSISTENCE_HPP_
+#include <drafts/com/sun/star/ui/XUIConfigurationPersistence.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_XUICONFIGURATIONSTORGAE_HPP_
+#include <drafts/com/sun/star/ui/XUIConfigurationStorage.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_XUICONFIGURATIONMANAGER_HPP_
+#include <drafts/com/sun/star/ui/XUIConfigurationManager.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_CONFIGURATIONEVENT_HPP_
+#include <drafts/com/sun/star/ui/ConfigurationEvent.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_UIELEMENTTYPE_HPP_
+#include <drafts/com/sun/star/ui/UIElementType.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXCONTAINER_HPP_
+#include <com/sun/star/container/XIndexContainer.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+
+#ifndef _CPPUHELPER_INTERFACECONTAINER_HXX_
+#include <cppuhelper/interfacecontainer.hxx>
+#endif
+
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+
+namespace framework
+{
+    class UIConfigurationManager :   public com::sun::star::lang::XTypeProvider ,
+                                     public com::sun::star::lang::XServiceInfo  ,
+                                     public com::sun::star::lang::XComponent    ,
+                                     public drafts::com::sun::star::ui::XUIConfiguration             ,
+                                     public drafts::com::sun::star::ui::XUIConfigurationManager      ,
+                                     public drafts::com::sun::star::ui::XUIConfigurationPersistence  ,
+                                     public drafts::com::sun::star::ui::XUIConfigurationStorage      ,
+                                     private ThreadHelpBase                     ,   // Struct for right initalization of mutex member! Must be first of baseclasses.
+                                     public ::cppu::OWeakObject
+    {
+        public:
+            //  XInterface, XTypeProvider, XServiceInfo
+            DECLARE_XINTERFACE
+            DECLARE_XTYPEPROVIDER
+            DECLARE_XSERVICEINFO
+
+            UIConfigurationManager( com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory > xServiceManager );
+            virtual ~UIConfigurationManager();
+
+            // XComponent
+            virtual void SAL_CALL dispose() throw (::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+            // XUIConfiguration
+            virtual void SAL_CALL addConfigurationListener( const ::com::sun::star::uno::Reference< ::drafts::com::sun::star::ui::XUIConfigurationListener >& Listener ) throw (::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL removeConfigurationListener( const ::com::sun::star::uno::Reference< ::drafts::com::sun::star::ui::XUIConfigurationListener >& Listener ) throw (::com::sun::star::uno::RuntimeException);
+
+            // XUIConfigurationManager
+            virtual void SAL_CALL reset() throw (::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > > SAL_CALL getUIElementsInfo( sal_Int16 ElementType ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexContainer > SAL_CALL createSettings(  ) throw (::com::sun::star::uno::RuntimeException);
+            virtual sal_Bool SAL_CALL hasSettings( const ::rtl::OUString& ResourceURL ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess > SAL_CALL getSettings( const ::rtl::OUString& ResourceURL, sal_Bool bWriteable ) throw (::com::sun::star::container::NoSuchElementException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL replaceSettings( const ::rtl::OUString& ResourceURL, const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& aNewData ) throw (::com::sun::star::container::NoSuchElementException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::IllegalAccessException, ::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL removeSettings( const ::rtl::OUString& ResourceURL ) throw (::com::sun::star::container::NoSuchElementException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::IllegalAccessException, ::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL insertSettings( const ::rtl::OUString& NewResourceURL, const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& aNewData ) throw (::com::sun::star::container::ElementExistException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::IllegalAccessException, ::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL getImageManager() throw (::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL getShortCutManager() throw (::com::sun::star::uno::RuntimeException);
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL getEventsManager() throw (::com::sun::star::uno::RuntimeException);
+
+            // XUIConfigurationPersistence
+            virtual void SAL_CALL reload() throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL store() throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+            virtual void SAL_CALL storeToStorage( const ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage >& Storage ) throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+            virtual sal_Bool SAL_CALL isModified() throw (::com::sun::star::uno::RuntimeException);
+            virtual sal_Bool SAL_CALL isReadOnly() throw (::com::sun::star::uno::RuntimeException);
+
+            // XUIConfigurationStorage
+            virtual void SAL_CALL setStorage( const ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage >& Storage ) throw (::com::sun::star::uno::RuntimeException);
+            virtual sal_Bool SAL_CALL hasStorage() throw (::com::sun::star::uno::RuntimeException);
+
+        private:
+            // private data types
+            enum NotifyOp
+            {
+                NotifyOp_Remove,
+                NotifyOp_Insert,
+                NotifyOp_Replace
+            };
+
+            struct UIElementInfo
+            {
+                UIElementInfo( const rtl::OUString& rResourceURL, const rtl::OUString& rUIName ) :
+                    aResourceURL( rResourceURL), aUIName( rUIName ) {}
+                rtl::OUString   aResourceURL;
+                rtl::OUString   aUIName;
+            };
+
+            struct UIElementData
+            {
+                UIElementData() : bModified( false ), bDefault( true ) {};
+
+                rtl::OUString aResourceURL;
+                rtl::OUString aName;
+                bool          bModified;        // has been changed since last storing
+                bool          bDefault;         // default settings
+                com::sun::star::uno::Reference< com::sun::star::container::XIndexAccess > xSettings;
+            };
+
+            struct UIElementType;
+            friend UIElementType;
+            typedef ::std::hash_map< rtl::OUString, UIElementData, OUStringHashCode, ::std::equal_to< rtl::OUString > > UIElementDataHashMap;
+
+            struct UIElementType
+            {
+                UIElementType() : nElementType( drafts::com::sun::star::ui::UIElementType::UNKNOWN ), bLoaded( false ), bModified( false ), bDefaultLayer( false ) {}
+
+                bool                                                              bModified;
+                bool                                                              bLoaded;
+                bool                                                              bDefaultLayer;
+                sal_Int16                                                         nElementType;
+                UIElementDataHashMap                                              aElementsHashMap;
+                com::sun::star::uno::Reference< com::sun::star::embed::XStorage > xStorage;
+            };
+
+            typedef ::std::vector< UIElementType > UIElementTypesVector;
+            typedef ::std::vector< drafts::com::sun::star::ui::ConfigurationEvent > ConfigEventNotifyContainer;
+            typedef ::std::hash_map< rtl::OUString, UIElementInfo, OUStringHashCode, ::std::equal_to< rtl::OUString > > UIElementInfoHashMap;
+
+            // private methods
+            void            impl_Initialize();
+            void            implts_notifyContainerListener( const drafts::com::sun::star::ui::ConfigurationEvent& aEvent, NotifyOp eOp );
+            void            impl_fillSequenceWithElementTypeInfo( UIElementInfoHashMap& aUIElementInfoCollection, sal_Int16 nElementType );
+            void            impl_preloadUIElementTypeList( sal_Int16 nElementType );
+            UIElementData*  impl_findUIElementData( const rtl::OUString& aResourceURL, sal_Int16 nElementType, bool bLoad = true );
+            void            impl_requestUIElementData( sal_Int16 nElementType, UIElementData& aUIElementData );
+            void            impl_storeElementTypeData( com::sun::star::uno::Reference< com::sun::star::embed::XStorage >& xStorage, UIElementType& rElementType, bool bResetModifyState = true );
+            void            impl_resetElementTypeData( UIElementType& rDocElementType, ConfigEventNotifyContainer& rRemoveNotifyContainer );
+            void            impl_reloadElementTypeData( UIElementType& rDocElementType, ConfigEventNotifyContainer& rRemoveNotifyContainer, ConfigEventNotifyContainer& rReplaceNotifyContainer );
+
+            UIElementTypesVector                                                            m_aUIElements;
+            com::sun::star::uno::Reference< com::sun::star::embed::XStorage >               m_xDocConfigStorage;
+            bool                                                                            m_bReadOnly;
+            bool                                                                            m_bInitialized;
+            bool                                                                            m_bModified;
+            bool                                                                            m_bConfigRead;
+            bool                                                                            m_bDisposed;
+            rtl::OUString                                                                   m_aXMLPostfix;
+            rtl::OUString                                                                   m_aPropUIName;
+            rtl::OUString                                                                   m_aPropResourceURL;
+            rtl::OUString                                                                   m_aModuleIdentifier;
+            com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >    m_xServiceManager;
+            ::cppu::OMultiTypeInterfaceContainerHelper                                      m_aListenerContainer;   /// container for ALL Listener
+   };
+}
+
+#endif // __FRAMEWORK_UICONFIGURATION_UICONFIGMANAGER_HXX_
