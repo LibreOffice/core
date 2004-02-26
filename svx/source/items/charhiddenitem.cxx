5cce2b49d1c16e114ce0b4f4fce561b9c53de7a6
Kurt Zenker <kz@openoffice.org>
2004-02-26 14:54:16 +0000

    INTEGRATION: CWS hiddentext (1.1.2); FILE ADDED 2003/12/17 15:07:29 os 1.1.2.1: #i23614# new Feature: Hidden text

---
+++ svx/source/items/charhiddenitem.cxx
@@ -0,0 +1,117 @@
+/*************************************************************************
+ *
+ *  $RCSfile: charhiddenitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-02-26 15:54:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#pragma hdrstop
+
+#define ITEMID_CHARHIDDEN            0
+
+#ifndef _SVX_CHARHIDDENITEM_HXX
+#include <charhiddenitem.hxx>
+#endif
+#include "svxitems.hrc"
+#include "dialmgr.hxx"
+
+TYPEINIT1_AUTOFACTORY(SvxCharHiddenItem, SfxBoolItem);
+
+/*-- 16.12.2003 15:24:25---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SvxCharHiddenItem::SvxCharHiddenItem( const sal_Bool bHidden, const USHORT nId ) :
+    SfxBoolItem( nId, bHidden )
+{
+}
+/*-- 16.12.2003 15:24:25---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SfxPoolItem* SvxCharHiddenItem::Clone( SfxItemPool * ) const
+{
+    return new SvxCharHiddenItem( *this );
+}
+/*-- 16.12.2003 15:24:25---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+SfxItemPresentation SvxCharHiddenItem::GetPresentation
+(
+    SfxItemPresentation ePres,
+    SfxMapUnit          eCoreUnit,
+    SfxMapUnit          ePresUnit,
+    XubString&          rText, const IntlWrapper *pIntl
+)   const
+{
+    switch ( ePres )
+    {
+        case SFX_ITEM_PRESENTATION_NONE:
+            rText.Erase();
+            return ePres;
+        case SFX_ITEM_PRESENTATION_NAMELESS:
+        case SFX_ITEM_PRESENTATION_COMPLETE:
+        {
+            USHORT nId = RID_SVXITEMS_CHARHIDDEN_FALSE;
+
+            if ( GetValue() )
+                nId = RID_SVXITEMS_CHARHIDDEN_TRUE;
+            rText = SVX_RESSTR(nId);
+            return ePres;
+        }
+    }
+    return SFX_ITEM_PRESENTATION_NONE;
+}
+
