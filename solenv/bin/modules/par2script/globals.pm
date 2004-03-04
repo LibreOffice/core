5b2bcb4dc298386958d45d2f7d5b12df8609d0c9
Rüdiger Timm <rt@openoffice.org>
2004-03-04 13:04:03 +0000

    INTEGRATION: CWS scp2fix1 (1.2.22); FILE MERGED 2004/02/20 16:32:14 is 1.2.22.1: #i25475# matching MSWin instead of win Issue number: Submitted by: Reviewed by:

--- solenv/bin/modules/par2script/globals.pm
+++ solenv/bin/modules/par2script/globals.pm
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: globals.pm,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2004-01-29 11:44:05 $
+#   last change: $Author: rt $ $Date: 2004-03-04 14:04:03 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -92,7 +92,7 @@ BEGIN
 
     $plat = $^O;
 
-    if ( $plat =~ /win/i )
+    if ( $plat =~ /MSWin/i )
     {
         $separator = "\\";
         $pathseparator = "\;";
