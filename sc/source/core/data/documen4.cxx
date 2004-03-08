734eaa99601577b13318138134e19d5b7c2d7a40
Jens-Heiner Rechtien <hr@openoffice.org>
2004-03-08 10:43:52 +0000

    INTEGRATION: CWS cac (1.11.48); FILE MERGED 2003/07/01 15:49:02 er 1.11.48.1: #i4070# untangle compiler.hxx -> tokenarray.hxx, errorcodes.hxx

--- sc/source/core/data/documen4.cxx
+++ sc/source/core/data/documen4.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documen4.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2003-04-28 15:31:14 $
+ *  last change: $Author: hr $ $Date: 2004-03-08 11:43:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -86,6 +86,7 @@
 #include "patattr.hxx"
 #include "chgtrack.hxx"
 #include "progress.hxx"
+#include "compiler.hxx"
 
 // -----------------------------------------------------------------------
 
