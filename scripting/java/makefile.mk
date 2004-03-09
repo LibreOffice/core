a0f09ce972d204fff17059c9dc92a8561aba52a6
Jens-Heiner Rechtien <hr@openoffice.org>
2004-03-09 11:07:25 +0000

    INTEGRATION: CWS ooo20040225 (1.2.16); FILE MERGED 2004/02/25 10:38:33 haggai 1.2.16.1: #i25829# Fix scripting build without Java support ($SOLAR_JAVA!=TRUE)

--- scripting/java/makefile.mk
+++ scripting/java/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: dfoster $ $Date: 2002-09-23 15:02:08 $
+#   last change: $Author: hr $ $Date: 2004-03-09 12:07:25 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -66,5 +66,6 @@ TARGET=scriptruntimeforjava
 
 .INCLUDE : ant.mk
 
+.IF "$(SOLAR_JAVA)"!=""
 ALLTAR : ANTBUILD
-
+.ENDIF
