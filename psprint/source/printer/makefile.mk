eba0890d1286b9b36d67c5a76ff48e70ddc76376
Oliver Bolte <obo@openoffice.org>
2004-03-17 09:51:30 +0000

    INTEGRATION: CWS cups01 (1.2.74); FILE MERGED 2004/02/13 17:03:27 pl 1.2.74.4: RESYNC: (1.2-1.3); FILE MERGED 2003/08/21 13:39:14 pl 1.2.74.3: #i12087# only compile in cups if ENABLE_CUPS is set 2003/08/15 12:41:04 pl 1.2.74.2: #i12087# add authentication callback 2003/08/05 13:35:47 pl 1.2.74.1: #i12087# add CUPS support

--- psprint/source/printer/makefile.mk
+++ psprint/source/printer/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2004-02-02 18:55:54 $
+#   last change: $Author: obo $ $Date: 2004-03-17 10:51:30 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,6 +70,10 @@ TARGET=printer
 
 .INCLUDE :  settings.mk
 
+.IF "$(ENABLE_CUPS)" != ""
+CDEFS += -DENABLE_CUPS
+.ENDIF
+
 # --- Files --------------------------------------------------------
 
 .IF "$(GUIBASE)"=="aqua"
@@ -81,10 +85,20 @@ dummy:
 
 SLOFILES=\
     $(SLO)$/printerinfomanager.obj	\
-    $(SLO)$/jobdata.obj
+    $(SLO)$/jobdata.obj				\
+    $(SLO)$/cupsmgr.obj
 
 .ENDIF # GUIBASE = aqua
 
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
+
+XSALSETLIBNAME=$(DLLPRE)spa$(UPD)$(DLLPOSTFIX)$(DLLPOST)
+
+$(INCCOM)$/rtsname.hxx:
+    rm -f $(INCCOM)$/rtsname.hxx ; \
+    echo "#define _XSALSET_LIBNAME "\"$(XSALSETLIBNAME)\" > $(INCCOM)$/rtsname.hxx
+
+$(SLO)$/cupsmgr.obj : $(INCCOM)$/rtsname.hxx
+
