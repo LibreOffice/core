995c2f94eea28e335ef7e1d8214ef239dfa2d04c
Oliver Bolte <obo@openoffice.org>
2004-03-19 11:51:34 +0000

    INTEGRATION: CWS os28 (1.11.68); FILE MERGED 2004/01/26 11:05:17 tl 1.11.68.1: #114838# suppress linguistic events during creation of dictionary-list

--- linguistic/source/dlistimp.cxx
+++ linguistic/source/dlistimp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dlistimp.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-26 12:51:41 $
+ *  last change: $Author: obo $ $Date: 2004-03-19 12:51:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -173,6 +173,7 @@ public:
     INT16   BeginCollectEvents();
     INT16   EndCollectEvents();
     INT16   FlushEvents();
+    void    ClearEvents()   { nCondensedEvt = 0; }
 };
 
 
@@ -761,6 +762,11 @@ void DicList::_CreateDicList()
                 xDic->setActive( TRUE );
         }
     }
+
+    // suppress collected events during creation of the dictionary list.
+    // there should be no events during creation.
+    pDicEvtLstnrHelper->ClearEvents();
+
     pDicEvtLstnrHelper->EndCollectEvents();
 }
 
