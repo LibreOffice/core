2d477594f8557c084beba6bf72f6166b3aa5080e
Oliver Bolte <obo@openoffice.org>
2004-03-19 11:05:16 +0000

    INTEGRATION: CWS formdesign01 (1.1.2); FILE ADDED 2003/11/27 10:20:37 fs 1.1.2.1: initial checkin - UNO wrapper for dialogs provided in this module

---
+++ extensions/source/propctrlr/pcrunodialogs.hxx
@@ -0,0 +1,137 @@
+/*************************************************************************
+ *
+ *  $RCSfile: pcrunodialogs.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-03-19 12:05:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef EXTENSIONS_SOURCE_PROPCTRLR_PCRUNODIALOGS_HXX
+#define EXTENSIONS_SOURCE_PROPCTRLR_PCRUNODIALOGS_HXX
+
+#ifndef _SVT_GENERICUNODIALOG_HXX_
+#include <svtools/genericunodialog.hxx>
+#endif
+#ifndef _EXTENSIONS_COMPONENT_MODULE_HXX_
+#include "componentmodule.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_AWT_XTABCONTROLLERMODEL_HPP_
+#include <com/sun/star/awt/XTabControllerModel.hpp>
+#endif
+#ifndef _COM_SUN_STAR_AWT_XCONTROLCONTAINER_HPP_
+#include <com/sun/star/awt/XControlContainer.hpp>
+#endif
+
+//........................................................................
+namespace pcr
+{
+//........................................................................
+
+    //====================================================================
+    //= OTabOrderDialog
+    //====================================================================
+    class OTabOrderDialog;
+    typedef ::svt::OGenericUnoDialog                                    OTabOrderDialog_DBase;
+    typedef ::comphelper::OPropertyArrayUsageHelper< OTabOrderDialog >  OTabOrderDialog_PBase;
+
+    class OTabOrderDialog
+                :public OTabOrderDialog_DBase
+                ,public OTabOrderDialog_PBase
+                ,public OModuleResourceClient
+    {
+    protected:
+        // <properties>
+        ::com::sun::star::uno::Reference< ::com::sun::star::awt::XTabControllerModel >
+                m_xTabbingModel;
+        ::com::sun::star::uno::Reference< ::com::sun::star::awt::XControlContainer >
+                m_xControlContext;
+        // </properties>
+
+    public:
+        OTabOrderDialog( const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& _rxORB );
+        ~OTabOrderDialog();
+
+        // XTypeProvider
+        virtual ::com::sun::star::uno::Sequence<sal_Int8> SAL_CALL getImplementationId(  ) throw(::com::sun::star::uno::RuntimeException);
+
+        // XServiceInfo
+        virtual ::rtl::OUString SAL_CALL getImplementationName() throw(::com::sun::star::uno::RuntimeException);
+        virtual ::comphelper::StringSequence SAL_CALL getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException);
+
+        // XServiceInfo - static methods
+        static ::com::sun::star::uno::Sequence< ::rtl::OUString > getSupportedServiceNames_Static(void) throw( ::com::sun::star::uno::RuntimeException );
+        static ::rtl::OUString getImplementationName_Static(void) throw( ::com::sun::star::uno::RuntimeException );
+        static ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >
+                SAL_CALL Create(const ::com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >&);
+
+        // XPropertySet
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo>  SAL_CALL getPropertySetInfo() throw(::com::sun::star::uno::RuntimeException);
+        virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
+
+        // OPropertyArrayUsageHelper
+        virtual ::cppu::IPropertyArrayHelper* createArrayHelper( ) const;
+
+    protected:
+    // OGenericUnoDialog overridables
+        virtual Dialog* createDialog(Window* _pParent);
+    };
+
+//........................................................................
+} // namespacepcr
+//........................................................................
+
+#endif // EXTENSIONS_SOURCE_PROPCTRLR_PCRUNODIALOGS_HXX
+
