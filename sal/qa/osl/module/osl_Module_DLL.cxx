886cd29111d333487ec898deda977ef4a31c9ba4
Oliver Bolte <obo@openoffice.org>
2004-03-19 13:49:08 +0000

    INTEGRATION: CWS qadev16 (1.2.40); FILE MERGED 2004/02/11 07:30:14 lla 1.2.40.2: #114785# change back t_print to printf here, which was wrong 2004/02/11 06:07:03 lla 1.2.40.1: #114785# printf replacement, now t_print  is same as printf

--- sal/qa/osl/module/osl_Module_DLL.cxx
+++ sal/qa/osl/module/osl_Module_DLL.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: osl_Module_DLL.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2003-09-08 13:21:30 $
+ *  last change: $Author: obo $ $Date: 2004-03-19 14:49:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,7 @@
 
 #include <stdio.h>
 #include <sal/types.h>
+#include <cppunit/simpleheader.hxx>
 
 extern "C" sal_Bool SAL_CALL firstfunc( sal_Bool bRes )
 {
