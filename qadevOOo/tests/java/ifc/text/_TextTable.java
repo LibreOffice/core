60f89e7900b983d84e68a5e08301258578fc6989
Oliver Bolte <obo@openoffice.org>
2004-03-19 13:36:07 +0000

    INTEGRATION: CWS qadev16 (1.2.22); FILE MERGED 2004/01/14 15:03:56 sw 1.2.22.1: #i23301#

--- qadevOOo/tests/java/ifc/text/_TextTable.java
+++ qadevOOo/tests/java/ifc/text/_TextTable.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _TextTable.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-09-08 11:14:39 $
+ *  last change:$Date: 2004-03-19 14:36:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -104,7 +104,7 @@ public class _TextTable extends MultiPropertyTest {
      * strings.
      */
     public void _PageDescName() {
-        testProperty("PageDescName", "", "Standard");
+        testProperty("PageDescName", "Endnote", "Standard");
     }
 
     /**
