f0e5122f20073af321544c672e6f88ca7601b530
Oliver Bolte <obo@openoffice.org>
2004-03-19 13:37:22 +0000

    INTEGRATION: CWS qadev16 (1.4.2); FILE MERGED 2004/02/13 16:54:36 sw 1.4.2.1: #23458#

--- qadevOOo/tests/java/mod/_svx/GraphicExporter.java
+++ qadevOOo/tests/java/mod/_svx/GraphicExporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: GraphicExporter.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2004-01-05 19:45:36 $
+ *  last change:$Date: 2004-03-19 14:37:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,18 +58,7 @@
  *
  *
  ************************************************************************/
-
 package mod._svx;
-
-import java.io.PrintWriter;
-
-import lib.StatusException;
-import lib.TestCase;
-import lib.TestEnvironment;
-import lib.TestParameters;
-import util.DrawTools;
-import util.SOfficeFactory;
-import util.XMLTools;
 
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XExporter;
@@ -80,6 +69,18 @@ import com.sun.star.ucb.XSimpleFileAccess;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 import com.sun.star.util.URL;
+
+import java.io.PrintWriter;
+
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+import util.DrawTools;
+import util.SOfficeFactory;
+import util.XMLTools;
+
 
 /**
  * Test for object which is represented by service
@@ -109,23 +110,22 @@ import com.sun.star.util.URL;
  * @see ifc.document._XExporter
  */
 public class GraphicExporter extends TestCase {
-
     XComponent xDrawDoc;
 
     /**
      * Creates a new draw document.
      */
-    protected void initialize( TestParameters tParam, PrintWriter log ) {
-
-        log.println( "creating a drawdoc" );
-        xDrawDoc = DrawTools.createDrawDoc((XMultiServiceFactory)tParam.getMSF());
+    protected void initialize(TestParameters tParam, PrintWriter log) {
+        log.println("creating a drawdoc");
+        xDrawDoc = DrawTools.createDrawDoc(
+                           (XMultiServiceFactory) tParam.getMSF());
     }
 
     /**
      * Disposes the draw document created before
      */
-    protected void cleanup( TestParameters tParam, PrintWriter log ) {
-        log.println( "    disposing xDrawDoc " );
+    protected void cleanup(TestParameters tParam, PrintWriter log) {
+        log.println("    disposing xDrawDoc ");
         util.DesktopTools.closeDoc(xDrawDoc);
     }
 
@@ -158,101 +158,114 @@ public class GraphicExporter extends TestCase {
      *   </li>
      * </ul>
      */
-    protected TestEnvironment createTestEnvironment(TestParameters tParam, PrintWriter log) {
-
+    protected TestEnvironment createTestEnvironment(TestParameters tParam,
+                                                    PrintWriter log) {
         XInterface oObj = null;
         XShape oShape = null;
-        Object go=null;
+        Object go = null;
+
 
         // creation of testobject here
         // first we write what we are intend to do to log file
-        log.println( "creating a test environment" );
+        log.println("creating a test environment");
+
         try {
-            go = ((XMultiServiceFactory)tParam.getMSF()).createInstance
-                ("com.sun.star.drawing.GraphicExportFilter");
+            go = ((XMultiServiceFactory) tParam.getMSF()).createInstance(
+                         "com.sun.star.drawing.GraphicExportFilter");
         } catch (com.sun.star.uno.Exception e) {
             log.println("Couldn't create instance");
             e.printStackTrace(log);
         }
 
         // create testobject here
-        SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)tParam.getMSF());
-        oShape = SOF.createShape(xDrawDoc,5000,5000,1500,1000,"GraphicObject");
-        DrawTools.getShapes(DrawTools.getDrawPage(xDrawDoc,0)).add(oShape);
-        XPropertySet oShapeProps = (XPropertySet)
-            UnoRuntime.queryInterface(XPropertySet.class,oShape);
+        SOfficeFactory SOF = SOfficeFactory.getFactory(
+                                     (XMultiServiceFactory) tParam.getMSF());
+        oShape = SOF.createShape(xDrawDoc, 5000, 5000, 1500, 1000,
+                                 "GraphicObject");
+        DrawTools.getShapes(DrawTools.getDrawPage(xDrawDoc, 0)).add(oShape);
+
+        XPropertySet oShapeProps = (XPropertySet) UnoRuntime.queryInterface(
+                                           XPropertySet.class, oShape);
         XComponent xComp = null;
+
         try {
-            oShapeProps.setPropertyValue(
-                "GraphicURL",util.utils.getFullTestURL("space-metal.jpg"));
-            xComp = (XComponent) UnoRuntime.queryInterface
-                (XComponent.class,oShape);
-            XExporter xEx = (XExporter) UnoRuntime.queryInterface
-                (XExporter.class,(XInterface) go);
+            oShapeProps.setPropertyValue("GraphicURL",
+                                         util.utils.getFullTestURL(
+                                                 "space-metal.jpg"));
+            xComp = (XComponent) UnoRuntime.queryInterface(XComponent.class,
+                                                           oShape);
+
+            XExporter xEx = (XExporter) UnoRuntime.queryInterface(
+                                    XExporter.class, (XInterface) go);
             xEx.setSourceDocument(xComp);
         } catch (com.sun.star.lang.WrappedTargetException e) {
-            e.printStackTrace(log) ;
-            throw new StatusException("Error while preparing component", e) ;
+            e.printStackTrace(log);
+            throw new StatusException("Error while preparing component", e);
         } catch (com.sun.star.lang.IllegalArgumentException e) {
-            e.printStackTrace(log) ;
-            throw new StatusException("Error while preparing component", e) ;
+            e.printStackTrace(log);
+            throw new StatusException("Error while preparing component", e);
         } catch (com.sun.star.beans.PropertyVetoException e) {
-            e.printStackTrace(log) ;
-            throw new StatusException("Error while preparing component", e) ;
+            e.printStackTrace(log);
+            throw new StatusException("Error while preparing component", e);
         } catch (com.sun.star.beans.UnknownPropertyException e) {
-            e.printStackTrace(log) ;
-            throw new StatusException("Error while preparing component", e) ;
+            e.printStackTrace(log);
+            throw new StatusException("Error while preparing component", e);
         }
 
-        final URL aURL = new URL() ;
-        aURL.Complete = util.utils.getOfficeTemp((XMultiServiceFactory)tParam.getMSF())+"picture.jpg";
+        final URL aURL = new URL();
+        aURL.Complete = util.utils.getOfficeTemp(
+                                (XMultiServiceFactory) tParam.getMSF()) +
+                        "picture.jpg";
 
-        final XSimpleFileAccess fAcc ;
+        final XSimpleFileAccess fAcc;
+
         try {
-            Object oFAcc = ((XMultiServiceFactory)tParam.getMSF()).createInstance
-                ("com.sun.star.ucb.SimpleFileAccess") ;
-            fAcc = (XSimpleFileAccess) UnoRuntime.queryInterface
-                (XSimpleFileAccess.class, oFAcc) ;
+            Object oFAcc = ((XMultiServiceFactory) tParam.getMSF()).createInstance(
+                                   "com.sun.star.ucb.SimpleFileAccess");
+            fAcc = (XSimpleFileAccess) UnoRuntime.queryInterface(
+                           XSimpleFileAccess.class, oFAcc);
+
             if (fAcc.exists(aURL.Complete)) {
                 fAcc.kill(aURL.Complete);
             }
         } catch (com.sun.star.uno.Exception e) {
             log.println("Error accessing file system :");
             e.printStackTrace(log);
-            throw new StatusException("Error accessing file system.", e) ;
+            throw new StatusException("Error accessing file system.", e);
         }
 
         oObj = (XInterface) go;
-        log.println("ImplName "+ util.utils.getImplName(oObj));
+        log.println("ImplName " + util.utils.getImplName(oObj));
 
-        TestEnvironment tEnv = new TestEnvironment( oObj );
-        tEnv.addObjRelation("MediaDescriptor", XMLTools.createMediaDescriptor(
-            new String[] {"FilterName","URL", "MediaType"},
-            new Object[] {"JPG",aURL, "image/jpeg"}));
-        tEnv.addObjRelation("SourceDocument",xComp);
+        TestEnvironment tEnv = new TestEnvironment(oObj);
+        tEnv.addObjRelation("MediaDescriptor",
+                            XMLTools.createMediaDescriptor(
+                                    new String[] {
+            "FilterName", "URL", "MediaType"
+        }, new Object[] { "JPG", aURL, "image/jpeg" }));
+        tEnv.addObjRelation("SourceDocument", xComp);
 
         log.println("adding ObjRelation for XFilter");
         log.println("This Component doesn't really support the cancel method");
         log.println("See #101725");
-        tEnv.addObjRelation("CANCEL", new Boolean(false));
+        tEnv.addObjRelation("NoFilter.cancel()", new Boolean(true));
 
-
-        final String hideMode = (String) tParam.get("soapi.test.hidewindows") ;
+        final String hideMode = (String) tParam.get("soapi.test.hidewindows");
         tEnv.addObjRelation("XFilter.Checker",
-            new ifc.document._XFilter.FilterChecker() {
-                public boolean checkFilter() {
-                    try {
-                        if (hideMode != null && hideMode.equals("true"))
-                            return true ;
-                        return fAcc.exists(aURL.Complete) ;
-                    } catch (com.sun.star.uno.Exception e) {
-                        return false ;
+                            new ifc.document._XFilter.FilterChecker() {
+            public boolean checkFilter() {
+                try {
+                    if ((hideMode != null) && hideMode.equals("true")) {
+                        return true;
                     }
+
+                    return fAcc.exists(aURL.Complete);
+                } catch (com.sun.star.uno.Exception e) {
+                    return false;
                 }
-            }) ;
+            }
+        });
 
         return tEnv;
     } // finish method getTestEnvironment
-
-}    // finish class GraphicExporter
-
+} // finish class GraphicExporter
