22f39842035dc751547fb74ed784a167f81e8353
Oliver Bolte <obo@openoffice.org>
2004-03-19 13:49:21 +0000

    INTEGRATION: CWS qadev16 (1.3.20); FILE MERGED 2004/01/16 07:36:56 lla 1.3.20.1: #114786# makefile TARGET renames

--- sal/qa/osl/mutex/makefile.mk
+++ sal/qa/osl/mutex/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-11-18 16:39:24 $
+#   last change: $Author: obo $ $Date: 2004-03-19 14:49:21 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -63,7 +63,7 @@
 PRJ=..$/..$/..
 
 PRJNAME=sal
-TARGET=qa
+TARGET=qa_osl_mutex
 
 ENABLE_EXCEPTIONS=TRUE
 
