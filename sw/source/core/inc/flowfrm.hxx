ca541bb833b5a91d6a5bcc807d4a4043cf7b92b6
Kurt Zenker <kz@openoffice.org>
2004-03-23 10:24:41 +0000

    INTEGRATION: CWS swformatcells (1.3.72); FILE MERGED 2004/03/02 15:16:16 od 1.3.72.1: #106629# class <SwFlowFrm> - new method <CalcLowerSpace(..)>

--- sw/source/core/inc/flowfrm.hxx
+++ sw/source/core/inc/flowfrm.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: flowfrm.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-01-13 11:10:43 $
+ *  last change: $Author: kz $ $Date: 2004-03-23 11:24:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -190,6 +190,13 @@ public:
     BOOL HasParaSpaceAtPages( BOOL bSct ) const;
     SwTwips CalcUpperSpace( const SwBorderAttrs *pAttrs = NULL,
                             const SwFrm* pPr = NULL ) const;
+    /** calculation of lower space
+
+        OD 2004-03-02 #106629#
+
+        @author OD
+    */
+    SwTwips CalcLowerSpace( const SwBorderAttrs* _pAttrs = 0L ) const;
 
     void CheckKeep();
 
