ad35fc0384e9090004914e716604358086d972a9
Kurt Zenker <kz@openoffice.org>
2004-03-23 09:28:33 +0000

    INTEGRATION: CWS cfgcleanup (1.8.56); FILE MERGED 2004/02/09 15:30:27 jb 1.8.56.1: #i25025# Eliminate warnings from gcc

--- configmgr/source/registry/cfgregistrykey.cxx
+++ configmgr/source/registry/cfgregistrykey.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cfgregistrykey.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:18:08 $
+ *  last change: $Author: kz $ $Date: 2004-03-23 10:28:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -149,7 +149,7 @@ namespace {
     inline
     bool isAscii(sal_Unicode ch)
     {
-        return 0 <= ch && ch < 128;
+        return 0 < ch && ch < 128;
     }
     inline
     bool isAscii(sal_Unicode const * ps, sal_Int32 nLen )
