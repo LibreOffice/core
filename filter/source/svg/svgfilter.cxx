55da2052a533b05972cfabfcbf12a876aecca173
Kurt Zenker <kz@openoffice.org>
2004-03-25 13:59:45 +0000

    INTEGRATION: CWS jl3 (1.3.126); FILE MERGED 2004/02/09 14:21:45 jl 1.3.126.2: RESYNC: (1.3-1.5); FILE MERGED 2003/11/21 08:56:17 jl 1.3.126.1: #111907# replace TKTThreadAttach by jvmaccess::VirtualMachine

--- filter/source/svg/svgfilter.cxx
+++ filter/source/svg/svgfilter.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svgfilter.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-12-17 15:25:33 $
+ *  last change: $Author: kz $ $Date: 2004-03-25 14:59:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,7 @@
 
 #include "svgfilter.hxx"
 
+using namespace ::rtl;
 // -------------
 // - SVGFilter -
 // -------------
