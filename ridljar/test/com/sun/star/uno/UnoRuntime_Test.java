fe9cc40148491595ebafb03ca1df46526e80c42e
Rüdiger Timm <rt@openoffice.org>
2004-03-30 15:39:04 +0000

    INTEGRATION: CWS sb14 (1.1.2); FILE ADDED 2004/02/19 14:20:12 sb 1.1.2.1: #i21150# Moved Java types from jurt to ridljar, as javamaker now generates code that uses UnoRuntime; cleaned up; deprecated environment/bridge/mapping stuff.

---
+++ ridljar/test/com/sun/star/uno/UnoRuntime_Test.java
@@ -0,0 +1,212 @@
+/*************************************************************************
+ *
+ *  $RCSfile: UnoRuntime_Test.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-03-30 16:39:04 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package com.sun.star.uno;
+
+import complexlib.ComplexTestCase;
+
+public final class UnoRuntime_Test extends ComplexTestCase {
+    public String getTestObjectName() {
+        return getClass().getName();
+    }
+
+    public String[] getTestMethodNames() {
+        return new String[] {
+            "test_generateOid", "test_queryInterface", "test_areSame",
+            "test_currentContext" };
+    }
+
+    public void test_generateOid() {
+        // Test if UnoRuntime generates an OID for a simple class:
+        assure("Test1", UnoRuntime.generateOid(new Test1()) != null);
+
+        // Test if UnoRuntime generates an OID for a class implementing
+        // IQueryInterface and returning null from getOid:
+        assure("Test2", UnoRuntime.generateOid(new Test2()) != null);
+
+        // Test if a delegator object has the same OID as its creator:
+        Test4 test4 = new Test4();
+        Ifc ifc = (Ifc) UnoRuntime.queryInterface(Ifc.class, test4);
+        assure(
+            "Test4",
+            UnoRuntime.generateOid(test4).equals(UnoRuntime.generateOid(ifc)));
+    }
+
+    public void test_queryInterface() {
+        // Test if a query for an interface which is not supported returns null:
+        assure(
+            "Test1",
+            (Ifc) UnoRuntime.queryInterface(Ifc.class, new Test1()) == null);
+
+        // Test if a query for an interface which is supported through
+        // IQueryInterface succeeds:
+        assure(
+            "Test2",
+            (Ifc) UnoRuntime.queryInterface(Ifc.class, new Test2()) != null);
+
+        // Test if a query for an interface which is directly supported (through
+        // inheritance) succeeds:
+        assure(
+            "Test3",
+            (Ifc) UnoRuntime.queryInterface(Ifc.class, new Test3()) != null);
+    }
+
+    public void test_areSame() {
+        // Test if two independent instances are not the same:
+        assure("Test1", !UnoRuntime.areSame(new Test1(), new Test2()));
+
+        // Test if a delegator object is the same as its creator:
+        Test2 test2 = new Test2();
+        Ifc ifc = (Ifc) UnoRuntime.queryInterface(Ifc.class, test2);
+        assure("Test2", UnoRuntime.areSame(ifc, test2));
+    }
+
+    public void test_currentContext() throws InterruptedException {
+        TestThread t1 = new TestThread();
+        TestThread t2 = new TestThread();
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        Object v1 = t1.context.getValueByName("");
+        Object v2 = t2.context.getValueByName("");
+        assure("", t1.context != t2.context);
+        assure("", v1 == t1);
+        assure("", v2 == t2);
+        assure("", v1 != v2);
+    }
+
+    private interface Ifc extends XInterface {}
+
+    private static class Test1 {}
+
+    private static class Test2 implements IQueryInterface {
+        public String getOid() {
+            return null;
+        }
+
+        public Object queryInterface(Type type) {
+            return type.equals(new Type(Ifc.class)) ? t2 : null;
+        }
+
+        public boolean isSame(Object object) {
+            return object == t2;
+        }
+
+        private static final class T2 implements Ifc {}
+
+        private final T2 t2 = new T2();
+    }
+
+    private static class Test3 implements Ifc {}
+
+    private static class Test4 implements IQueryInterface {
+        public String getOid() {
+            return null;
+        }
+
+        public Object queryInterface(Type type) {
+            return type.equals(new Type(Ifc.class)) ? t4 : null;
+        }
+
+        public boolean isSame(Object object) {
+            return object == t4;
+        }
+
+        private final class T4 implements Ifc, IQueryInterface {
+            public String getOid() {
+                return UnoRuntime.generateOid(Test4.this);
+            }
+
+            public Object queryInterface(Type type) {
+                return Test4.this.queryInterface(type);
+            }
+
+            public boolean isSame(Object object) {
+                return UnoRuntime.areSame(Test4.this, object);
+            }
+        }
+
+        private final T4 t4 = new T4();
+    }
+
+    private final class TestThread extends Thread {
+        public void run() {
+            assure("", UnoRuntime.getCurrentContext() == null);
+            context = new TestCurrentContext();
+            UnoRuntime.setCurrentContext(context);
+            assure("", UnoRuntime.getCurrentContext() == context);
+            assure("", context.getValueByName("") == this);
+            UnoRuntime.setCurrentContext(null);
+            assure("", UnoRuntime.getCurrentContext() == null);
+        }
+
+        public XCurrentContext context = null;
+    }
+
+    private static final class TestCurrentContext implements XCurrentContext {
+        public Object getValueByName(String name) {
+            return value;
+        }
+
+        private final Object value = Thread.currentThread();
+    }
+}
