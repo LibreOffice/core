3eb9a2ee54edccfb8556c9fa9e73b4ab1383c7ba
Rüdiger Timm <rt@openoffice.org>
2004-03-30 15:40:37 +0000

    INTEGRATION: CWS sb14 (1.2.4); FILE MERGED 2004/03/15 16:01:58 sb 1.2.4.3: #i21150# Dumping extended interface attributes. 2004/03/15 09:53:52 sb 1.2.4.2: #i21150# Adapted to new extensible type writer interface; added support for bound interface attributes. 2004/03/01 12:53:49 sb 1.2.4.1: #i21150# Added optional interface inheritance; added -stdin switch; do not warn about bad member names of com.sun.star.uno.Uik; some general clean up and added const qualifiers.

--- idlc/inc/idlc/astinterface.hxx
+++ idlc/inc/idlc/astinterface.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: astinterface.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 11:55:31 $
+ *  last change: $Author: rt $ $Date: 2004-03-30 16:40:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,21 +67,41 @@
 #ifndef _IDLC_ASTSCOPE_HXX_
 #include <idlc/astscope.hxx>
 #endif
+#include "idlc/inheritedinterface.hxx"
 
+#include <map>
+#include <vector>
 
 class AstInterface : public AstType
                    , public AstScope
 {
 public:
-    AstInterface(const ::rtl::OString& name, AstDeclaration* pInherits, AstScope* pScope);
+    typedef std::vector< InheritedInterface > InheritedInterfaces;
+
+    typedef std::vector< AstInterface const * > DoubleInterfaceDeclarations;
+
+    struct DoubleMemberDeclaration {
+        AstDeclaration const * first;
+        AstDeclaration const * second;
+    };
+
+    typedef std::vector< DoubleMemberDeclaration > DoubleMemberDeclarations;
+
+    struct DoubleDeclarations {
+        DoubleInterfaceDeclarations interfaces;
+        DoubleMemberDeclarations members;
+    };
+
+    AstInterface(
+        const ::rtl::OString& name, AstInterface const * pInherits,
+        AstScope* pScope);
     virtual ~AstInterface();
 
-    const DeclList& getInheritedInterfaces()
-        { return m_inheritedInterfaces; }
-    sal_Int32   nInheritedInterfaces()
-        {
-            return m_inheritedInterfaces.size();
-        }
+    InheritedInterfaces const & getAllInheritedInterfaces() const
+    { return m_inheritedInterfaces; }
+
+    bool hasMandatoryInheritedInterfaces() const
+    { return m_mandatoryInterfaces > 0; }
 
     void setForwarded(sal_Bool bForwarded)
         { m_bForwarded = bForwarded; }
@@ -98,18 +118,64 @@ public:
 
     bool usesSingleInheritance() const { return m_bSingleInheritance; }
 
-    bool addInheritedInterface(AstInterface * pInherits);
+    DoubleDeclarations checkInheritedInterfaceClashes(
+        AstInterface const * ifc, bool optional) const;
+
+    void addInheritedInterface(
+        AstInterface const * ifc, bool optional,
+        rtl::OUString const & documentation);
+
+    DoubleMemberDeclarations checkMemberClashes(
+        AstDeclaration const * member) const;
+
+    void addMember(AstDeclaration /*TODO: const*/ * member);
 
     void forwardDefined(AstInterface const & def);
 
-    virtual sal_Bool dump(RegistryKey& rKey, RegistryTypeWriterLoader* pLoader);
+    virtual sal_Bool dump(RegistryKey& rKey);
+
 private:
-    DeclList    m_inheritedInterfaces;
+    enum InterfaceKind {
+        INTERFACE_INDIRECT_OPTIONAL, INTERFACE_DIRECT_OPTIONAL,
+        INTERFACE_INDIRECT_MANDATORY, INTERFACE_DIRECT_MANDATORY };
+
+    struct VisibleMember {
+        explicit VisibleMember(AstDeclaration const * theMandatory = 0):
+            mandatory(theMandatory) {}
+
+        typedef std::map< rtl::OString, AstDeclaration const * > Optionals;
+
+        AstDeclaration const * mandatory;
+        Optionals optionals;
+    };
+
+    typedef std::map< rtl::OString, InterfaceKind > VisibleInterfaces;
+    typedef std::map< rtl::OString, VisibleMember > VisibleMembers;
+
+    void checkInheritedInterfaceClashes(
+        DoubleDeclarations & doubleDeclarations,
+        std::set< rtl::OString > & seenInterfaces, AstInterface const * ifc,
+        bool optional, bool direct, bool mainOptional) const;
+
+    void checkMemberClashes(
+        DoubleMemberDeclarations & doubleMembers, AstDeclaration const * member,
+        bool checkOptional) const;
+
+    void addVisibleInterface(
+        AstInterface const * ifc, bool direct, bool optional);
+
+    void addOptionalVisibleMembers(AstInterface const * ifc);
+
+    bool increment(sal_uInt16 * counter, char const * sort) const;
+
+    InheritedInterfaces m_inheritedInterfaces;
+    InheritedInterfaces::size_type m_mandatoryInterfaces;
     sal_Bool    m_bIsDefined;
     sal_Bool    m_bForwarded;
     sal_Bool    m_bForwardedInSameFile;
     bool m_bSingleInheritance;
+    VisibleInterfaces m_visibleInterfaces;
+    VisibleMembers m_visibleMembers;
 };
 
 #endif // _IDLC_ASTINTERFACE_HXX_
-
