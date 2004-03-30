047314707627f62aa295b928363d510e21e39897
Rüdiger Timm <rt@openoffice.org>
2004-03-30 14:39:29 +0000

    INTEGRATION: CWS interop12 (1.2.426); FILE MERGED 2003/11/11 16:21:09 cl 1.2.426.1: #i20949# set paint info rec at SdrOutliner so app has a better clue in CalcFieldValueHdl

--- svx/source/svdraw/svdoutl.cxx
+++ svx/source/svdraw/svdoutl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svdoutl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: cl $ $Date: 2001-12-05 15:19:38 $
+ *  last change: $Author: rt $ $Date: 2004-03-30 15:39:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -91,7 +91,8 @@
 \************************************************************************/
 SdrOutliner::SdrOutliner( SfxItemPool* pItemPool, USHORT nMode ):
 Outliner( pItemPool, nMode ),
-pTextObj( NULL )
+pTextObj( NULL ),
+mpPaintInfoRec( NULL )
 {
 }
 
