e9f5782dea7d28426f1e46cb8527a676f064c8d1
Rüdiger Timm <rt@openoffice.org>
2004-03-30 15:28:19 +0000

    INTEGRATION: CWS sb14 (1.9.38); FILE MERGED 2004/03/15 09:09:26 sb 1.9.38.2: #i21150# Improved documentation. 2004/03/11 14:41:30 sb 1.9.38.1: #i21150# rtl_string2UString and OUString::OUString(sal_Char const *, sal_Int32, rtl_TextEncoding, sal_uInt32) handle out-of-memory conditions.

--- sal/inc/rtl/ustring.h
+++ sal/inc/rtl/ustring.h
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ustring.h,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 16:09:43 $
+ *  last change: $Author: rt $ $Date: 2004-03-30 16:28:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1360,8 +1360,10 @@ sal_Int32 SAL_CALL rtl_uString_getToken( rtl_uString ** newStr , rtl_uString * s
     for double-byte encodings, UTF-7, UTF-8).
 
     If the length of the byte string is greater than zero, the reference count
-    of the new string will be 1.  This function does not handle out-of-memory
-    conditions.
+    of the new string will be 1.
+
+    If an out-of-memory condition occurs, newStr will point to a null pointer
+    upon return.
 
     @param newStr
     pointer to the new string.  The pointed-to data must be null or a valid
