fc2967c431fbfe0962a10c19c2c7a379a81dc05f
Rüdiger Timm <rt@openoffice.org>
2004-04-02 12:55:53 +0000

    INTEGRATION: CWS sj05 (1.1.2); FILE ADDED 2004/01/23 14:54:07 cl 1.1.2.1: #i20484# adding autoshape ui

---
+++ svx/inc/fontworkgallery.hxx
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  $RCSfile: fontworkgallery.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-04-02 13:55:53 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifndef _SVX_FONTWORK_GALLERY_DIALOG_HXX
+#define _SVX_FONTWORK_GALLERY_DIALOG_HXX
+
+#ifndef _FIXED_HXX //autogen
+#include <vcl/fixed.hxx>
+#endif
+#ifndef _VALUESET_HXX //autogen
+#include <svtools/valueset.hxx>
+#endif
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+#ifndef _SV_DIALOG_HXX
+#include <vcl/dialog.hxx>
+#endif
+
+#include <vector>
+
+class FmFormModel;
+class SdrView;
+class Window;
+
+//------------------------------------------------------------------------
+namespace svx
+{
+
+class FontWorkGalleryDialog : public ModalDialog
+{
+    ValueSet            maCtlFavorites;
+    FixedLine           maFLFavorites;
+    ImageButton         maBtnLTR;
+    ImageButton         maBtnTTB;
+    OKButton            maOKButton;
+    CancelButton        maCancelButton;
+
+    sal_uInt16          mnThemeId;
+
+    SdrView*            mpSdrView;
+    FmFormModel*        mpModel;
+
+    String              maStrClickToAddText;
+
+    DECL_LINK( DoubleClickFavoriteHdl, void * );
+    DECL_LINK( ClickOKHdl, void * );
+    DECL_LINK( ClickTextDirectionHdl, ImageButton * );
+
+    void            initfavorites(sal_uInt16 nThemeId, std::vector< Bitmap * >& rFavorites);
+    void            insertSelectedFontwork();
+    void            changeText( SdrTextObj* pObj );
+    void            fillFavorites( sal_uInt16 nThemeId, std::vector< Bitmap * >& rFavorites );
+
+    std::vector< Bitmap * > maFavoritesVertical;
+    std::vector< Bitmap * > maFavoritesHorizontal;
+
+public:
+    FontWorkGalleryDialog( SdrView* pView, Window* pParent, sal_uInt16 nSID );
+    ~FontWorkGalleryDialog();
+};
+
+};
+
+#endif
