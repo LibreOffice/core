e3a6fa3ce142ca5f94dbfd456e89667676a52a67
Rüdiger Timm <rt@openoffice.org>
2004-04-02 12:19:09 +0000

    INTEGRATION: CWS sj05 (1.4.44); FILE MERGED 2004/02/13 14:48:58 sj 1.4.44.2: RESYNC: (1.4-1.5); FILE MERGED 2004/01/23 17:16:12 cl 1.4.44.1: #i20484# adding autoshape ui

--- sd/inc/glob.hxx
+++ sd/inc/glob.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: glob.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2004-01-20 10:15:42 $
+ *  last change: $Author: rt $ $Date: 2004-04-02 13:19:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,6 +89,8 @@
 #define SD_IF_SDPRESVIEWSHELL           SFX_INTERFACE_SD_START + 14
 #define SD_IF_SDPREVIEWVIEWSHELL        SFX_INTERFACE_SD_START + 15
 #define SD_IF_SDVIEWSHELLBASE           SFX_INTERFACE_SD_START + 16
+#define SD_IF_SD3DOBJECTBAR             SFX_INTERFACE_SD_START + 17
+#define SD_IF_SDFONTWORKOBJECTBAR       SFX_INTERFACE_SD_START + 18
 
 // Inventor-Id fuer StarDraw UserData
 const UINT32 SdUDInventor=UINT32('S')*0x00000001+
