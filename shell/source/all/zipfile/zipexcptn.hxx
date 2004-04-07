21fb1b352f775acab049aabfbbebb296216265e0
Jens-Heiner Rechtien <hr@openoffice.org>
2004-04-07 09:58:03 +0000

    INTEGRATION: CWS desktintgr01 (1.1.2); FILE ADDED 2004/02/26 13:29:35 deuce 1.1.2.1: Issue number: 97686, 115531, 108219  restructure shxthdl module Submitted by: Gorden Lin/gorden.lin@sun.com Reviewed by: Tino Rachui/tino.rachui@sun.com

---
+++ shell/source/all/zipfile/zipexcptn.hxx
@@ -0,0 +1,78 @@
+// ZipException.h: interface for the ZipException class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef ZIPEXCPTN_HXX_INCLUDED
+#define ZIPEXCPTN_HXX_INCLUDED
+
+#include <stdexcept>
+
+//------------------------------------------
+/**
+*/
+class RuntimeException : public std::exception
+{
+public:
+    RuntimeException(int Error);
+    virtual ~RuntimeException();
+
+    int GetErrorCode() const;
+
+private:
+    int m_Error;
+};
+
+//------------------------------------------
+/**
+*/
+class ZipException : public RuntimeException
+{
+public:
+    ZipException(int Error);
+
+    virtual const char* what() const throw();
+};
+
+//------------------------------------------
+/**
+*/
+class Win32Exception : public RuntimeException
+{
+public:
+    Win32Exception(int Error);
+    virtual ~Win32Exception();
+
+    virtual const char* what() const throw();
+
+private:
+    void* m_MsgBuff;
+};
+
+//------------------------------------------
+/**
+*/
+class ZipContentMissException : public ZipException
+{
+public:
+    ZipContentMissException(int Error);
+};
+
+//------------------------------------------
+/**
+*/
+class AccessViolationException : public Win32Exception
+{
+public:
+    AccessViolationException(int Error);
+};
+
+//------------------------------------------
+/**
+*/
+class IOException : public Win32Exception
+{
+public:
+    IOException(int Error);
+};
+
+#endif
