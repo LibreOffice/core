5f95a9ccfdc3be356ee6b01df41aea8a5d06506c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-04-13 11:21:52 +0000

    INTEGRATION: CWS unopkg1 (1.37.10); FILE MERGED 2004/01/19 15:16:27 mav 1.37.10.1: #114733# do not create storage opened in readonly mode

--- sot/source/sdstor/storage.cxx
+++ sot/source/sdstor/storage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: storage.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: kz $ $Date: 2003-11-18 16:52:37 $
+ *  last change: $Author: hr $ $Date: 2004-04-13 12:21:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -833,7 +833,10 @@ uno::Reference< embed::XStorage > SotStorage::GetUNOAPIDuplicate( const String& 
     if ( GetError() == ERRCODE_NONE )
     {
         StreamMode nMode = ( ( nUNOStorageMode & embed::ElementModes::ELEMENT_WRITE ) == embed::ElementModes::ELEMENT_WRITE ) ?
-                                    STREAM_WRITE : STREAM_READ;
+                                    STREAM_WRITE : STREAM_READ | STREAM_NOCREATE;
+
+        if ( nUNOStorageMode & embed::ElementModes::ELEMENT_NOCREATE )
+            nMode |= STREAM_NOCREATE;
 
         sal_Bool bStorageReady = !IsStorage( rEleName );
         SotStorageRef pChildStorage = OpenUCBStorage( rEleName, nMode, STORAGE_TRANSACTED );
