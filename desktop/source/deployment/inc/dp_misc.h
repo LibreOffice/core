06a93c878ffd69515afe852c2d60070ac2185719
Jens-Heiner Rechtien <hr@openoffice.org>
2004-04-13 11:05:36 +0000

    INTEGRATION: CWS unopkg1 (1.1.2); FILE ADDED 2004/03/31 11:21:50 dbo 1.1.2.7: #115884# Issue number: Submitted by: Reviewed by: 2004/03/26 14:16:04 dbo 1.1.2.6: #115884# minor fixes Issue number: Submitted by: Reviewed by: 2004/03/19 16:12:54 dbo 1.1.2.5: #115884# fixes Issue number: Submitted by: Reviewed by: 2004/03/12 18:02:45 dbo 1.1.2.4: #115884# misc fixes Issue number: Submitted by: Reviewed by: 2004/02/27 16:01:13 dbo 1.1.2.3: #i20304# revised Issue number: Submitted by: Reviewed by: 2004/01/09 17:38:29 dbo 1.1.2.2: #i20304# API revision; source saved 2003/11/28 13:28:47 dbo 1.1.2.1: #i20304# new

---
+++ desktop/source/deployment/inc/dp_misc.h
@@ -0,0 +1,135 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dp_misc.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-04-13 12:05:36 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2002 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#if ! defined INCLUDED_DP_MISC_H
+#define INCLUDED_DP_MISC_H
+
+#include "rtl/ustrbuf.hxx"
+#include "osl/mutex.hxx"
+#include "com/sun/star/uno/XComponentContext.hpp"
+#include "com/sun/star/lang/XComponent.hpp"
+
+#define OUSTR(x) ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM(x) )
+#define ARLEN(x) sizeof (x) / sizeof *(x)
+
+
+namespace css = ::com::sun::star;
+
+namespace dp_misc
+{
+
+//==============================================================================
+class MutexHolder
+{
+    mutable ::osl::Mutex m_mutex;
+protected:
+    inline ::osl::Mutex & getMutex() const { return m_mutex; }
+};
+
+//==============================================================================
+inline void try_dispose( css::uno::Reference< css::uno::XInterface > const & x )
+{
+    css::uno::Reference< css::lang::XComponent > xComp(
+        x, css::uno::UNO_QUERY );
+    if (xComp.is())
+        xComp->dispose();
+}
+
+//##############################################################################
+
+//==============================================================================
+template< typename T >
+inline T extract_throw( css::uno::Any const & a )
+{
+    T v;
+    if (! (a >>= v))
+    {
+        throw css::uno::RuntimeException(
+            OUSTR("expected ") + ::getCppuType( &v ).getTypeName(),
+            css::uno::Reference< css::uno::XInterface >() );
+    }
+    return v;
+}
+
+//==============================================================================
+template< typename T >
+inline void extract_throw( T * p, css::uno::Any const & a )
+{
+    if (! (a >>= *p))
+    {
+        throw css::uno::RuntimeException(
+            OUSTR("expected ") + ::getCppuType( p ).getTypeName(),
+            css::uno::Reference< css::uno::XInterface >() );
+    }
+}
+
+//==============================================================================
+::rtl::OUString expand_reg_url(
+    ::rtl::OUString const & url,
+    css::uno::Reference< css::uno::XComponentContext > const & xContext );
+
+//==============================================================================
+bool office_is_running(
+    css::uno::Reference< css::uno::XComponentContext > const & xContext );
+
+}
+
+#endif
