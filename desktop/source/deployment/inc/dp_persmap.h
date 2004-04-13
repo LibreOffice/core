24e780c0d203a69a87d8b242a8f6774799102fa9
Jens-Heiner Rechtien <hr@openoffice.org>
2004-04-13 11:05:47 +0000

    INTEGRATION: CWS unopkg1 (1.1.2); FILE ADDED 2004/03/19 16:12:54 dbo 1.1.2.2: #115884# fixes Issue number: Submitted by: Reviewed by: 2004/03/05 20:18:41 dbo 1.1.2.1: #i20303# source save Issue number: Submitted by: Reviewed by:

---
+++ desktop/source/deployment/inc/dp_persmap.h
@@ -0,0 +1,101 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dp_persmap.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-04-13 12:05:47 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2002 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "com/sun/star/uno/XComponentContext.hpp"
+#include "berkeleydb/db_cxx.h"
+#include <hash_map>
+
+
+namespace css = ::com::sun::star;
+
+namespace dp_misc
+{
+
+typedef ::std::hash_map<
+    ::rtl::OUString, ::rtl::OUString, ::rtl::OUStringHash > t_string2string_map;
+
+//==============================================================================
+class PersistentMap
+{
+    ::rtl::OUString m_sysPath;
+    mutable Db m_db;
+    void throw_rtexc( char const * msg ) const;
+
+public:
+    ~PersistentMap();
+    PersistentMap(
+        ::rtl::OUString const & url,
+        css::uno::Reference< css::uno::XComponentContext > const & xContext );
+    /** in mem db */
+    PersistentMap();
+
+    void flush() const;
+
+    bool has( ::rtl::OUString const & key ) const;
+    bool get( ::rtl::OUString * value, ::rtl::OUString const & key ) const;
+    t_string2string_map getEntries() const;
+    bool put( ::rtl::OUString const & key, ::rtl::OUString & value,
+              bool overwrite = true, bool flush_immediately = true );
+    bool erase( ::rtl::OUString const & key, bool flush_immediately = true );
+};
+
+}
+
