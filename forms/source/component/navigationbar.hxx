199aeca90013a1327ca93540f6e3a35f0d68abb9
Jens-Heiner Rechtien <hr@openoffice.org>
2004-04-13 10:15:51 +0000

    INTEGRATION: CWS frmcontrols02 (1.1.2); FILE ADDED 2004/01/23 17:51:46 fs 1.1.2.1: control/model for navigation bar form control

---
+++ forms/source/component/navigationbar.hxx
@@ -0,0 +1,192 @@
+/*************************************************************************
+ *
+ *  $RCSfile: navigationbar.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-04-13 11:15:51 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef FORMS_COMPONENT_NAVIGATION_BAR_HXX
+#define FORMS_COMPONENT_NAVIGATION_BAR_HXX
+
+#ifndef _FORMS_FORMCOMPONENT_HXX_
+#include "FormComponent.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_IO_XPERSISTOBJECT_HPP_
+#include <com/sun/star/io/XPersistObject.hpp>
+#endif
+#ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
+#include <comphelper/proparrhlp.hxx>
+#endif
+#ifndef COMPHELPER_PROPERTYCONTAINERHELPER_HXX
+#include <comphelper/propertycontainerhelper.hxx>
+#endif
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+#ifndef _CPPUHELPER_INTERFACECONTAINER_HXX_
+#include <cppuhelper/interfacecontainer.hxx>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XSET_HPP_
+#include <com/sun/star/container/XSet.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XCONTAINER_HPP_
+#include <com/sun/star/container/XContainer.hpp>
+#endif
+#ifndef FORMS_SOURCE_COMPONENT_FORMCONTROLFONT_HXX
+#include "formcontrolfont.hxx"
+#endif
+
+#include <set>
+
+//.........................................................................
+namespace frm
+{
+//.........................................................................
+
+    //==================================================================
+    // ONavigationBarModel
+    //==================================================================
+    typedef ::cppu::ImplHelper1 <   ::com::sun::star::awt::XControlModel
+                                > ONavigationBarModel_BASE;
+
+    class ONavigationBarModel
+                        :public OControlModel
+                        ,public FontControlModel
+                        ,public OPropertyContainerHelper
+                        ,public OAggregationArrayUsageHelper< ONavigationBarModel >
+                            // though we don't use aggregation, we're derived from an OPropertySetAggregationHelper,
+                            // which expects that we use an OPropertyArrayAggregationHelper, which we ensure
+                            // with deriving from OAggregationArrayUsageHelper
+                        ,public ONavigationBarModel_BASE
+    {
+        // <properties>
+        ::com::sun::star::uno::Any          m_aTabStop;
+        ::com::sun::star::uno::Any          m_aBackgroundColor;
+        ::rtl::OUString                     m_sDefaultControl;
+        ::rtl::OUString                     m_sHelpText;
+        ::rtl::OUString                     m_sHelpURL;
+        sal_Int16                           m_nIconSize;
+        sal_Int16                           m_nBorder;
+        sal_Int32                           m_nDelay;
+        sal_Bool                            m_bEnabled;
+        sal_Bool                            m_bShowPosition;
+        sal_Bool                            m_bShowNavigation;
+        sal_Bool                            m_bShowActions;
+        sal_Bool                            m_bShowFilterSort;
+        // </properties>
+
+    public:
+        DECLARE_DEFAULT_LEAF_XTOR( ONavigationBarModel );
+
+        // XServiceInfo - static version
+        static  ::rtl::OUString SAL_CALL getImplementationName_Static();
+        static  ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames_Static();
+        static  ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL Create( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxFactory );
+
+    protected:
+        // UNO
+        DECLARE_UNO3_AGG_DEFAULTS( ONavigationBarModel, OControlModel );
+        virtual ::com::sun::star::uno::Any SAL_CALL queryAggregation( const ::com::sun::star::uno::Type& _rType ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XServiceInfo
+        virtual ::rtl::OUString SAL_CALL getImplementationName()  throw(::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames()  throw(::com::sun::star::uno::RuntimeException);
+
+        // XTypeProvider
+        DECLARE_XTYPEPROVIDER()
+
+        // OComponentHelper
+        virtual void SAL_CALL disposing();
+
+        // XPersistObject
+        virtual ::rtl::OUString SAL_CALL getServiceName() throw ( ::com::sun::star::uno::RuntimeException);
+        virtual void SAL_CALL write(const ::com::sun::star::uno::Reference< ::com::sun::star::io::XObjectOutputStream>& _rxOutStream) throw ( ::com::sun::star::io::IOException, ::com::sun::star::uno::RuntimeException);
+        virtual void SAL_CALL read(const ::com::sun::star::uno::Reference< ::com::sun::star::io::XObjectInputStream>& _rxInStream) throw ( ::com::sun::star::io::IOException, ::com::sun::star::uno::RuntimeException);
+
+        // XPropertySet
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo> SAL_CALL getPropertySetInfo() throw(::com::sun::star::uno::RuntimeException);
+        virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
+        virtual void SAL_CALL getFastPropertyValue(::com::sun::star::uno::Any& rValue, sal_Int32 nHandle ) const;
+        virtual sal_Bool SAL_CALL convertFastPropertyValue(::com::sun::star::uno::Any& rConvertedValue, ::com::sun::star::uno::Any& rOldValue,
+                                              sal_Int32 nHandle, const ::com::sun::star::uno::Any& rValue )
+                                            throw(::com::sun::star::lang::IllegalArgumentException);
+        virtual void SAL_CALL setFastPropertyValue_NoBroadcast(sal_Int32 nHandle, const ::com::sun::star::uno::Any& rValue) throw ( ::com::sun::star::uno::Exception);
+
+        // ::com::sun::star::beans::XPropertyState
+        virtual ::com::sun::star::uno::Any getPropertyDefaultByHandle( sal_Int32 nHandle ) const;
+
+        // OAggregationArrayUsageHelper
+        virtual void fillProperties(
+            ::com::sun::star::uno::Sequence< ::com::sun::star::beans::Property >& /* [out] */ _rProps,
+            ::com::sun::star::uno::Sequence< ::com::sun::star::beans::Property >& /* [out] */ _rAggregateProps
+            ) const;
+        IMPLEMENT_INFO_SERVICE()
+
+    protected:
+        DECLARE_XCLONEABLE();
+
+    private:
+        void implInitPropertyContainer();
+    };
+
+//.........................................................................
+}   // namespace frm
+//.........................................................................
+
+#endif // FORMS_COMPONENT_NAVIGATION_BAR_HXX
+
