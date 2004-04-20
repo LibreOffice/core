91d8b7df3d8d124e4088f6ceec8daf40ce4d1633
Sander Vesik <svesik@openoffice.org>
2004-04-20 11:38:07 +0000

    INTEGRATION: CWS newscpzip2 (1.1.2); FILE ADDED 2004/03/16 13:42:36 dv 1.1.2.1: #i25671# New setup loader for Windows Installer (native installer)

---
+++ desktop/win32/source/setup/Resource.h
@@ -0,0 +1,66 @@
+//+-------------------------------------------------------------------------
+//
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+//  File:       resource.h
+//
+//--------------------------------------------------------------------------
+
+#ifndef _RESOURCE_H
+#define _RESOURCE_H
+
+#define IDS_APP_TITLE   11
+#define IDS_OUTOFMEM    12
+#define IDS_NOMSI   13
+#define IDS_REQUIRES_NEWER_VERSION  14
+#define IDS_INVALID_PATH    15
+#define IDS_USER_CANCELLED  16
+#define IDS_REQUIRES_ADMIN_PRIV 17
+#define IDS_FILE_NOT_FOUND  18
+#define IDS_FAILED_TO_UPGRADE_MSI   19
+#define IDS_REBOOT_REQUIRED 20
+#define IDS_INVALID_PARAM   21
+#define IDS_INSTALL_ERROR   22
+#define IDS_ALLOW_MSI_UPDATE    23
+#define IDS_USAGE   24
+#define IDS_ALREADY_RUNNING 25
+#define IDS_UNKNOWN_ERROR   26
+#define IDS_INVALID_PROFILE 27
+#define IDS_STRING39    28
+#define IDS_UNKNOWN_LANG    29
+#define IDS_LANGUAGE_ENGLISH    30
+#define IDS_LANGUAGE_SPAIN  31
+#define IDS_LANGUAGE_GERMAN 32
+#define IDS_LANGUAGE_ZH_TW  33
+#define IDS_LANGUAGE_CS 34
+#define IDS_LANGUAGE_DA 35
+#define IDS_LANGUAGE_DE_DE  36
+#define IDS_LANGUAGE_EL 37
+#define IDS_LANGUAGE_EN_US  38
+#define IDS_LANGUAGE_ES 39
+#define IDS_LANGUAGE_FI 40
+#define IDS_LANGUAGE_FR_FR  41
+#define IDS_LANGUAGE_HE 42
+#define IDS_LANGUAGE_HU 43
+#define IDS_LANGUAGE_IT_IT  44
+#define IDS_LANGUAGE_JA 45
+#define IDS_LANGUAGE_KO 46
+#define IDS_LANGUAGE_NL_NL  47
+#define IDS_LANGUAGE_NO_NO  48
+#define IDS_LANGUAGE_PL 49
+#define IDS_LANGUAGE_PT_BR  50
+#define IDS_LANGUAGE_RU 51
+#define IDS_LANGUAGE_SK 52
+#define IDS_LANGUAGE_SV_SE  53
+#define IDS_LANGUAGE_TH 54
+#define IDS_LANGUAGE_TR 55
+#define IDS_LANGUAGE_ET 56
+#define IDS_LANGUAGE_ZH_CN  57
+#define IDS_LANGUAGE_PT_PT  58
+
+#define IDD_LANGUAGE                    101
+#define IDC_COMBO1                      1001
+
+#define IDI_INSTALLER                  99
+
+#endif
