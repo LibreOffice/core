a8cd3672598efe65d6a30020fe41fb05ea88ebdc
Sander Vesik <svesik@openoffice.org>
2004-04-20 11:27:12 +0000

    INTEGRATION: CWS newscpzip2 (1.1.2); FILE ADDED 2004/03/31 12:31:07 is 1.1.2.1: #i25325# new native packaging process

---
+++ solenv/bin/modules/installer/exiter.pm
@@ -0,0 +1,133 @@
+#*************************************************************************
+#
+#   $RCSfile: exiter.pm,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: svesik $ $Date: 2004-04-20 12:27:01 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+package installer::exiter;
+
+use installer::files;
+use installer::globals;
+
+############################################
+# Exiting the program with an error
+# This function is used instead of "die"
+############################################
+
+sub exit_program
+{
+    my ($message, $function) = @_;
+
+    # If @installer::globals::logfileinfo is not empty, it can be used.
+    # Otherwise the content of @installer::globals::globallogfileinfo has to be used.
+
+    my $infoline;
+
+    $installer::globals::logfilename = $installer::globals::exitlog . $installer::globals::logfilename;
+
+    if ( $#installer::globals::logfileinfo > -1 )
+    {
+        $infoline = "\n***************************************************************\n";
+        push(@installer::globals::logfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "$message\n";
+        push(@installer::globals::logfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "in function: $function\n";
+        push(@installer::globals::logfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "***************************************************************\n";
+        push(@installer::globals::logfileinfo, $infoline);
+
+        installer::files::save_file($installer::globals::logfilename ,\@installer::globals::logfileinfo);
+    }
+    else
+    {
+        $infoline = "\n***************************************************************\n";
+        push(@installer::globals::globallogfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "$message\n";
+        push(@installer::globals::globallogfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "in function: $function\n";
+        push(@installer::globals::globallogfileinfo, $infoline);
+        print("$infoline");
+
+        $infoline = "***************************************************************\n";
+        push(@installer::globals::globallogfileinfo, $infoline);
+
+        installer::files::save_file($installer::globals::logfilename ,\@installer::globals::globallogfileinfo);
+    }
+
+    print("Saved logfile: $installer::globals::logfilename\n");
+    print("$infoline");
+
+    # Saving the debug info
+
+    if ( $installer::globals::debug ) { installer::logger::savedebug($installer::globals::exitlog); }
+
+    exit(-1);
+}
+
+1;
