f1b26634db79f6316fb7c5c07ffbefd004ceda00
Sander Vesik <svesik@openoffice.org>
2004-04-21 13:14:55 +0000

    INTEGRATION: CWS ooo20040329 (1.22.378); FILE MERGED 2004/03/17 12:16:51 waratah 1.22.378.1: #i1858# bracket hdrstop pragma to stop wall warning in gcc

--- svx/source/unoedit/unofored.cxx
+++ svx/source/unoedit/unofored.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unofored.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: vg $ $Date: 2003-06-24 07:40:11 $
+ *  last change: $Author: svesik $ $Date: 2004-04-21 14:14:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,9 @@
  *
  *
  ************************************************************************/
-
+#ifndef GCC
 #pragma hdrstop
+#endif
 
 #include <algorithm>
 
