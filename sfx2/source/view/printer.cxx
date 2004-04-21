9b1a749316163f3269ebd5049a3716a79f89a325
Sander Vesik <svesik@openoffice.org>
2004-04-21 12:20:44 +0000

    INTEGRATION: CWS ooo20040329 (1.10.62); FILE MERGED 2004/03/15 12:40:17 waratah 1.10.62.1: #i1858# Include guards around windows hdrstop pragma

--- sfx2/source/view/printer.cxx
+++ sfx2/source/view/printer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: printer.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-04 13:37:14 $
+ *  last change: $Author: svesik $ $Date: 2004-04-21 13:20:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,7 +79,9 @@
 #endif
 #include <vector>
 
+#ifndef GCC
 #pragma hdrstop
+#endif
 
 #include "printer.hxx"
 #include "printopt.hxx"
