15f51f3b5ffa875b6a79709cb89ce3ceb797c1a9
Sander Vesik <svesik@openoffice.org>
2004-04-21 12:06:27 +0000

    INTEGRATION: CWS ooo20040329 (1.1.1.1.480); FILE MERGED 2004/03/18 10:34:17 waratah 1.1.1.1.480.1: :#i1858# exclude pragma hdrstop by bracketting for gcc

--- sfx2/source/bastyp/misc.cxx
+++ sfx2/source/bastyp/misc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: misc.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:52:28 $
+ *  last change: $Author: svesik $ $Date: 2004-04-21 13:06:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,9 @@
  *
  ************************************************************************/
 
+#ifndef GCC
 #pragma hdrstop
+#endif
 
 #include "sfxtypes.hxx"
 
