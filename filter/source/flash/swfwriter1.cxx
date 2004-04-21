65357a2326d69d5f45a764fb09b0840eeeb4ab32
Sander Vesik <svesik@openoffice.org>
2004-04-21 13:00:55 +0000

    INTEGRATION: CWS ooo20040329 (1.14.46); FILE MERGED 2004/03/25 11:17:34 mmeeks 1.14.46.1: Prune redundant code causing compile issues. Issue number: #24917

--- filter/source/flash/swfwriter1.cxx
+++ filter/source/flash/swfwriter1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swfwriter1.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: vg $ $Date: 2003-12-17 15:24:09 $
+ *  last change: $Author: svesik $ $Date: 2004-04-21 14:00:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,6 @@
  *
  *
  ************************************************************************/
-
 #ifndef _SWF_WRITER_HXX_
 #include "swfwriter.hxx"
 #endif
@@ -2073,46 +2072,4 @@ void Writer::Impl_quadBezierApprox( BitStream& rBits,
             }
         }
     }
-}
-
-/////////////////////////////////////////////////////////////////////////
-
-// TODO: Why do I need this?
-
-extern "C"
-{
-
-int ZEXPORT compress2 ( Bytef * dest, uLongf * destLen, const Bytef * source, uLong sourceLen, int level)
-{
-    z_stream stream;
-    int err;
-
-    stream.next_in = (Bytef*)source;
-    stream.avail_in = (uInt)sourceLen;
-#ifdef MAXSEG_64K
-    /* Check for source > 64K on 16-bit machine: */
-    if ((uLong)stream.avail_in != sourceLen) return Z_BUF_ERROR;
-#endif
-    stream.next_out = dest;
-    stream.avail_out = (uInt)*destLen;
-    if ((uLong)stream.avail_out != *destLen) return Z_BUF_ERROR;
-
-    stream.zalloc = (alloc_func)0;
-    stream.zfree = (free_func)0;
-    stream.opaque = (voidpf)0;
-
-    err = deflateInit(&stream, level);
-    if (err != Z_OK) return err;
-
-    err = deflate(&stream, Z_FINISH);
-    if (err != Z_STREAM_END) {
-        deflateEnd(&stream);
-        return err == Z_OK ? Z_BUF_ERROR : err;
-    }
-    *destLen = stream.total_out;
-
-    err = deflateEnd(&stream);
-    return err;
-}
-
 }
