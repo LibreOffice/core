fa27647343ccb62da056d513eac6f52bb18bdf12
Oliver Bolte <obo@openoffice.org>
2004-04-27 15:02:48 +0000

    INTEGRATION: CWS tl01 (1.1.2); FILE ADDED 2004/03/04 10:27:25 tl 1.1.2.4: #112058# fixed as suggested by AF 2004/02/19 13:33:53 af 1.1.2.3: #112058# Incorporated changes made in sdoutl.cxx for Hangul/Hanja conversion. 2003/10/08 12:27:54 cl 1.1.2.2: #112058# added undo for Hangul/Hanja conversion 2003/10/02 10:28:15 cl 1.1.2.1: #112058# added Hangul/Hanja conversion

---
+++ sd/source/ui/func/fuhhconv.cxx
@@ -0,0 +1,175 @@
+/*************************************************************************
+ *
+ *  $RCSfile: fuhhconv.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-04-27 16:02:48 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <fuhhconv.hxx>
+#include "drawdoc.hxx"
+#include "Outliner.hxx"
+#include "DrawViewShell.hxx"
+#include "OutlineViewShell.hxx"
+
+#ifndef SD_VIEW_SHELL_BASE_HXX
+#include "ViewShellBase.hxx"
+#endif
+
+#include "sdresid.hxx"
+#include "strings.hrc"
+
+class SfxRequest;
+
+namespace sd {
+
+class ViewShell;
+
+TYPEINIT1( FuHangulHanjaConversion, FuPoor );
+
+/*************************************************************************
+|*
+|* Konstruktor
+|*
+\************************************************************************/
+
+FuHangulHanjaConversion::FuHangulHanjaConversion (
+    ViewShell* pViewSh,
+    ::sd::Window* pWin,
+    ::sd::View* pView,
+    SdDrawDocument* pDocument,
+    SfxRequest& rReq )
+       : FuPoor(pViewSh, pWin, pView, pDocument, rReq),
+    pSdOutliner(NULL),
+    bOwnOutliner(FALSE)
+{
+    if ( pViewShell->ISA(DrawViewShell) )
+    {
+        bOwnOutliner = TRUE;
+        pSdOutliner = new Outliner( pDoc, OUTLINERMODE_TEXTOBJECT );
+    }
+    else if ( pViewShell->ISA(OutlineViewShell) )
+    {
+        bOwnOutliner = FALSE;
+        pSdOutliner = pDoc->GetOutliner();
+    }
+
+    if (pSdOutliner)
+       pSdOutliner->PrepareSpelling();
+}
+
+
+
+/*************************************************************************
+|*
+|* Destruktor
+|*
+\************************************************************************/
+
+FuHangulHanjaConversion::~FuHangulHanjaConversion()
+{
+    if (pSdOutliner)
+        pSdOutliner->EndConversion();
+
+    if (bOwnOutliner)
+        delete pSdOutliner;
+}
+
+
+/*************************************************************************
+|*
+|* Suchen&Ersetzen
+|*
+\************************************************************************/
+
+void FuHangulHanjaConversion::StartConversion( INT16 nLanguage )
+{
+
+    String aString( SdResId(STR_UNDO_HANGULHANJACONVERSION) );
+    pView->BegUndo( aString );
+
+    ViewShellBase* pBase = PTR_CAST(ViewShellBase, SfxViewShell::Current());
+    if (pBase != NULL)
+        pViewShell = pBase->GetSubShellManager().GetMainSubShell();
+
+    if( pViewShell )
+    {
+        if ( pSdOutliner && pViewShell->ISA(DrawViewShell) && !bOwnOutliner )
+        {
+            pSdOutliner->EndConversion();
+
+            bOwnOutliner = TRUE;
+            pSdOutliner = new Outliner( pDoc, OUTLINERMODE_TEXTOBJECT );
+            pSdOutliner->BeginConversion();
+        }
+        else if ( pSdOutliner && pViewShell->ISA(OutlineViewShell) && bOwnOutliner )
+        {
+            pSdOutliner->EndConversion();
+            delete pSdOutliner;
+
+            bOwnOutliner = FALSE;
+            pSdOutliner = pDoc->GetOutliner();
+            pSdOutliner->BeginConversion();
+        }
+
+        if (pSdOutliner)
+            pSdOutliner->StartTextConversion(nLanguage);
+    }
+
+    pView->EndUndo();
+}
+
+} // end of namespace
