b05666689ede627548a4c89d632ca9cc139f0395
Oliver Bolte <obo@openoffice.org>
2004-04-27 15:07:21 +0000

    INTEGRATION: CWS tl01 (1.3.80); FILE MERGED 2003/07/25 13:19:42 tl 1.3.80.1: #110762# API for Hangul/Hanja text conversion user-dictionaries

--- linguistic/source/dicimp.hxx
+++ linguistic/source/dicimp.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dicimp.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: tl $ $Date: 2001-03-19 14:51:06 $
+ *  last change: $Author: obo $ $Date: 2004-04-27 16:07:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -135,7 +135,6 @@ class DictionaryNeo :
     BOOL                    addEntry_Impl(const ::com::sun::star::uno::Reference<
         ::com::sun::star::linguistic2::XDictionaryEntry > xDicEntry,
                                           BOOL bIsLoadEntries = FALSE);
-    BOOL                    isReadonly_Impl();
 
 public:
     DictionaryNeo();
