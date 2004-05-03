4a3e8d45aca1738bf8d4be053a7f338a998d2316
Rüdiger Timm <rt@openoffice.org>
2004-05-03 06:41:40 +0000

    INTEGRATION: CWS comptrans1 (1.4.122); FILE MERGED 2004/04/07 09:54:28 tbe 1.4.122.2: #115421# Render Office Components Transparently Usable 2004/04/06 09:56:43 tbe 1.4.122.1: #115421# Render Office Components Transparently Usable

--- odk/settings/std.mk
+++ odk/settings/std.mk
@@ -1,4 +1,5 @@
-OUT=$(PRJ)/$(OS)example.out
+OSOUT=$(OS)example.out
+OUT=$(PRJ)/$(OSOUT)
 OUT_BIN=$(OUT)/bin
 OUT_INC=$(OUT)/inc
 OUT_SLO=$(OUT)/slo
@@ -11,6 +12,23 @@ IDL_DIR=$(PRJ)/idl
 BIN_DIR=$(PRJ)/$(PLATFORM)/bin
 CLASSES_DIR=$(PRJ)/classes
 OFFICE_CLASSES_DIR=$(subst \,/,$(OFFICE_PROGRAM_PATH))/classes
-DEPLOYTOOL="$(OFFICE_PROGRAM_PATH)$(PS)pkgchk" -f
 COMPONENT_PACKAGE_DIR=$(OO_SDK_HOME)$(PS)$(OS)example.out$(PS)bin
-SDK_JAVA=$(OO_SDK_JAVA_HOME)/bin/java
+
+DEPLOYTOOL="$(OFFICE_PROGRAM_PATH)$(PS)pkgchk" -f
+SDK_JAVA="$(OO_SDK_JAVA_HOME)/bin/java"
+SDK_JAVAC="$(OO_SDK_JAVA_HOME)/bin/javac"
+SDK_JAR="$(OO_SDK_JAVA_HOME)/bin/jar"
+IDLC="$(OO_SDK_HOME)/$(PLATFORM)/bin/idlc"
+CPPUMAKER="$(OO_SDK_HOME)/$(PLATFORM)/bin/cppumaker"
+JAVAMAKER="$(OO_SDK_HOME)/$(PLATFORM)/bin/javamaker"
+REGMERGE="$(OO_SDK_HOME)/$(PLATFORM)/bin/regmerge"
+XML2CMP="$(OO_SDK_HOME)/$(PLATFORM)/bin/xml2cmp"
+
+SDK_JAVA_UNO_BOOTSTRAP_FILES=\
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/Loader.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/Loader$$CustomURLClassLoader.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/InstallationFinder.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/InstallationFinder$$StreamGobbler.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/WinRegKey.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)com/sun/star/lib/loader/WinRegKeyException.class$(SQM) \
+    -C $(CLASSES_DIR) $(SQM)win/unowinreg.dll$(SQM)
