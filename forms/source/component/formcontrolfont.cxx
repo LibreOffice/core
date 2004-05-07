7417a7df13d383091363b840ce574a737fe36d20
Rüdiger Timm <rt@openoffice.org>
2004-05-07 15:08:13 +0000

    INTEGRATION: CWS frmcontrols03 (1.1.4); FILE MERGED 2004/04/29 14:57:28 fs 1.1.4.3: #i10000# 2004/03/30 12:05:06 fs 1.1.4.2: #100000# 2004/03/01 16:51:11 fs 1.1.4.1: helper class for font handling (copied hereing from CWS frmcontrols02)

--- forms/source/component/formcontrolfont.cxx
+++ forms/source/component/formcontrolfont.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: formcontrolfont.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-04-14 09:32:36 $
+ *  last change: $Author: rt $ $Date: 2004-05-07 16:08:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -82,6 +82,10 @@
 #include <tools/color.hxx>
 #endif
 
+#ifndef _TOOLKIT_HELPER_EMPTYFONTDESCRIPTOR_HXX_
+#include <toolkit/helper/emptyfontdescriptor.hxx>
+#endif
+
 #ifndef _COM_SUN_STAR_AWT_FONTRELIEF_HPP_
 #include <com/sun/star/awt/FontRelief.hpp>
 #endif
@@ -100,13 +104,94 @@ namespace frm
     using namespace ::com::sun::star::lang;
     using namespace ::com::sun::star::beans;
 
+    //------------------------------------------------------------------------------
+    namespace
+    {
+        Any lcl_extractFontDescriptorAggregate( sal_Int32 _nHandle, const FontDescriptor& _rFont )
+        {
+            Any aValue;
+            switch ( _nHandle )
+            {
+            case PROPERTY_ID_FONT_NAME:
+                aValue <<= _rFont.Name;
+                break;
+
+            case PROPERTY_ID_FONT_STYLENAME:
+                aValue <<= _rFont.StyleName;
+                break;
+
+            case PROPERTY_ID_FONT_FAMILY:
+                aValue <<= (sal_Int16)_rFont.Family;
+                break;
+
+            case PROPERTY_ID_FONT_CHARSET:
+                aValue <<= (sal_Int16)_rFont.CharSet;
+                break;
+
+            case PROPERTY_ID_FONT_CHARWIDTH:
+                aValue <<= _rFont.CharacterWidth;
+                break;
+
+            case PROPERTY_ID_FONT_KERNING:
+                aValue <<= _rFont.Kerning;
+                break;
+
+            case PROPERTY_ID_FONT_ORIENTATION:
+                aValue <<= _rFont.Orientation;
+                break;
+
+            case PROPERTY_ID_FONT_PITCH:
+                aValue <<= _rFont.Pitch;
+                break;
+
+            case PROPERTY_ID_FONT_TYPE:
+                aValue <<= _rFont.Type;
+                break;
+
+            case PROPERTY_ID_FONT_WIDTH:
+                aValue <<= _rFont.Width;
+                break;
+
+            case PROPERTY_ID_FONT_HEIGHT:
+                aValue <<= (float)( _rFont.Height );
+                break;
+
+            case PROPERTY_ID_FONT_WEIGHT:
+                aValue <<= (float)_rFont.Weight;
+                break;
+
+            case PROPERTY_ID_FONT_SLANT:
+                aValue = makeAny(_rFont.Slant);
+                break;
+
+            case PROPERTY_ID_FONT_UNDERLINE:
+                aValue <<= (sal_Int16)_rFont.Underline;
+                break;
+
+            case PROPERTY_ID_FONT_STRIKEOUT:
+                aValue <<= (sal_Int16)_rFont.Strikeout;
+                break;
+
+            case PROPERTY_ID_FONT_WORDLINEMODE:
+                aValue = makeAny( (sal_Bool)_rFont.WordLineMode );
+                break;
+
+            default:
+                OSL_ENSURE( sal_False, "lcl_extractFontDescriptorAggregate: invalid handle!" );
+                break;
+            }
+            return aValue;
+        }
+    }
+
     //=====================================================================
     //= FontControlModel
     //=====================================================================
     //---------------------------------------------------------------------
-    FontControlModel::FontControlModel()
+    FontControlModel::FontControlModel( bool _bToolkitCompatibleDefaults )
         :m_nFontRelief( FontRelief::NONE )
         ,m_nFontEmphasis( FontEmphasisMark::NONE )
+        ,m_bToolkitCompatibleDefaults( _bToolkitCompatibleDefaults )
     {
     }
 
@@ -115,6 +200,7 @@ namespace frm
     {
         m_nFontRelief = _pOriginal->m_nFontRelief;
         m_nFontEmphasis = _pOriginal->m_nFontEmphasis;
+        m_bToolkitCompatibleDefaults = _pOriginal->m_bToolkitCompatibleDefaults;
     }
 
     //---------------------------------------------------------------------
@@ -163,15 +249,21 @@ namespace frm
     void FontControlModel::describeFontRelatedProperties( Sequence< Property >& /* [out] */ _rProps ) const
     {
         sal_Int32 nPos = _rProps.getLength();
-        _rProps.realloc( nPos + 15 );
+        _rProps.realloc( nPos + 21 );
         Property* pProperties = _rProps.getArray();
 
         DECL_PROP2      ( FONT,               FontDescriptor,   BOUND, MAYBEDEFAULT );
         DECL_PROP2      ( FONTEMPHASISMARK,   sal_Int16,        BOUND, MAYBEDEFAULT );
         DECL_PROP2      ( FONTRELIEF,         sal_Int16,        BOUND, MAYBEDEFAULT );
         DECL_PROP3      ( TEXTCOLOR,          sal_Int32,        BOUND, MAYBEDEFAULT, MAYBEVOID );
-        DECL_PROP3      ( TEXTLINECOLOR,       sal_Int32,       BOUND, MAYBEDEFAULT, MAYBEVOID );
+        DECL_PROP3      ( TEXTLINECOLOR,      sal_Int32,        BOUND, MAYBEDEFAULT, MAYBEVOID );
 
+        DECL_PROP1      ( FONT_CHARWIDTH,     float,            MAYBEDEFAULT );
+        DECL_BOOL_PROP1 ( FONT_KERNING,                         MAYBEDEFAULT );
+        DECL_PROP1      ( FONT_ORIENTATION,   float,            MAYBEDEFAULT );
+        DECL_PROP1      ( FONT_PITCH,         sal_Int16,        MAYBEDEFAULT );
+        DECL_PROP1      ( FONT_TYPE,          sal_Int16,        MAYBEDEFAULT );
+        DECL_PROP1      ( FONT_WIDTH,         sal_Int16,        MAYBEDEFAULT );
         DECL_PROP1      ( FONT_NAME,          ::rtl::OUString,  MAYBEDEFAULT );
         DECL_PROP1      ( FONT_STYLENAME,     ::rtl::OUString,  MAYBEDEFAULT );
         DECL_PROP1      ( FONT_FAMILY,        sal_Int16,        MAYBEDEFAULT );
@@ -209,48 +301,9 @@ namespace frm
             _rValue = makeAny( m_aFont );
             break;
 
-        case PROPERTY_ID_FONT_NAME:
-            _rValue <<= m_aFont.Name;
-            break;
-
-        case PROPERTY_ID_FONT_STYLENAME:
-            _rValue <<= m_aFont.StyleName;
-            break;
-
-        case PROPERTY_ID_FONT_FAMILY:
-            _rValue <<= (sal_Int16)m_aFont.Family;
-            break;
-
-        case PROPERTY_ID_FONT_CHARSET:
-            _rValue <<= (sal_Int16)m_aFont.CharSet;
-            break;
-
-        case PROPERTY_ID_FONT_HEIGHT:
-            _rValue <<= (float)( m_aFont.Height );
-            break;
-
-        case PROPERTY_ID_FONT_WEIGHT:
-            _rValue <<= (float)m_aFont.Weight;
-            break;
-
-        case PROPERTY_ID_FONT_SLANT:
-            _rValue = makeAny(m_aFont.Slant);
-            break;
-
-        case PROPERTY_ID_FONT_UNDERLINE:
-            _rValue <<= (sal_Int16)m_aFont.Underline;
-            break;
-
-        case PROPERTY_ID_FONT_STRIKEOUT:
-            _rValue <<= (sal_Int16)m_aFont.Strikeout;
-            break;
-
-        case PROPERTY_ID_FONT_WORDLINEMODE:
-            _rValue = makeAny( (sal_Bool)m_aFont.WordLineMode );
-            break;
-
         default:
-            DBG_ERROR( "FontControlModel::getFastPropertyValue: no font aggregate!" );
+            _rValue = lcl_extractFontDescriptorAggregate( _nHandle, m_aFont );
+            break;
         }
     }
 
@@ -279,8 +332,8 @@ namespace frm
 
         case PROPERTY_ID_FONT:
         {
-            Any aAny( makeAny( m_aFont ) );
-            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, aAny, ::getCppuType( &m_aFont ) );
+            Any aWorkAroundGccLimitation = makeAny( m_aFont );
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, aWorkAroundGccLimitation, ::getCppuType( &m_aFont ) );
         }
         break;
 
@@ -298,6 +351,30 @@ namespace frm
 
         case PROPERTY_ID_FONT_CHARSET:
             bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, (sal_Int16)m_aFont.CharSet );
+            break;
+
+        case PROPERTY_ID_FONT_CHARWIDTH:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, float( m_aFont.CharacterWidth ) );
+            break;
+
+        case PROPERTY_ID_FONT_KERNING:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, (sal_Int16)m_aFont.Kerning );
+            break;
+
+        case PROPERTY_ID_FONT_ORIENTATION:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, float( m_aFont.Orientation ) );
+            break;
+
+        case PROPERTY_ID_FONT_PITCH:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, (sal_Int16)m_aFont.Pitch );
+            break;
+
+        case PROPERTY_ID_FONT_TYPE:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, (sal_Int16)m_aFont.Type );
+            break;
+
+        case PROPERTY_ID_FONT_WIDTH:
+            bModified = tryPropertyValue( _rConvertedValue, _rOldValue, _rValue, (sal_Int16)m_aFont.Width );
             break;
 
         case PROPERTY_ID_FONT_HEIGHT:
@@ -371,6 +448,30 @@ namespace frm
             _rValue >>= m_aFont.CharSet;
             break;
 
+        case PROPERTY_ID_FONT_CHARWIDTH:
+            _rValue >>= m_aFont.CharacterWidth;
+            break;
+
+        case PROPERTY_ID_FONT_KERNING:
+            _rValue >>= m_aFont.Kerning;
+            break;
+
+        case PROPERTY_ID_FONT_ORIENTATION:
+            _rValue >>= m_aFont.Orientation;
+            break;
+
+        case PROPERTY_ID_FONT_PITCH:
+            _rValue >>= m_aFont.Pitch;
+            break;
+
+        case PROPERTY_ID_FONT_TYPE:
+            _rValue >>= m_aFont.Type;
+            break;
+
+        case PROPERTY_ID_FONT_WIDTH:
+            _rValue >>= m_aFont.Width;
+            break;
+
         case PROPERTY_ID_FONT_HEIGHT:
         {
             float nHeight = 0;
@@ -412,23 +513,42 @@ namespace frm
     Any FontControlModel::getPropertyDefaultByHandle( sal_Int32 _nHandle ) const
     {
         Any aReturn;
+        // some defaults which are the same, not matter if we have toolkit-compatible
+        // defaults or not
+        bool bHandled = false;
         switch( _nHandle )
         {
         case PROPERTY_ID_TEXTCOLOR:
         case PROPERTY_ID_TEXTLINECOLOR:
             // void
-            break;
-
-        case PROPERTY_ID_FONT:
-            aReturn <<= ::comphelper::getDefaultFont();
+            bHandled = true;
             break;
 
         case PROPERTY_ID_FONTEMPHASISMARK:
             aReturn <<= FontEmphasisMark::NONE;
+            bHandled = true;
             break;
 
         case PROPERTY_ID_FONTRELIEF:
             aReturn <<= FontRelief::NONE;
+            bHandled = true;
+            break;
+        }
+        if ( bHandled )
+            return aReturn;
+
+        if ( m_bToolkitCompatibleDefaults )
+        {
+            EmptyFontDescriptor aEmpty;
+            if ( PROPERTY_ID_FONT == _nHandle )
+                return makeAny( (FontDescriptor)aEmpty );
+            return lcl_extractFontDescriptorAggregate( _nHandle, aEmpty );
+        }
+
+        switch( _nHandle )
+        {
+        case PROPERTY_ID_FONT:
+            aReturn <<= ::comphelper::getDefaultFont();
             break;
 
         case PROPERTY_ID_FONT_WORDLINEMODE:
@@ -444,9 +564,22 @@ namespace frm
         case PROPERTY_ID_FONT_UNDERLINE:
         case PROPERTY_ID_FONT_STRIKEOUT:
             aReturn <<= (sal_Int16)1;
+            break;
+
+        case PROPERTY_ID_FONT_KERNING:
+            aReturn = makeBoolAny(sal_False);
+            break;
+
+        case PROPERTY_ID_FONT_PITCH:
+        case PROPERTY_ID_FONT_TYPE:
+        case PROPERTY_ID_FONT_WIDTH:
+            aReturn <<= (sal_Int16)0;
+            break;
 
         case PROPERTY_ID_FONT_HEIGHT:
         case PROPERTY_ID_FONT_WEIGHT:
+        case PROPERTY_ID_FONT_CHARWIDTH:
+        case PROPERTY_ID_FONT_ORIENTATION:
             aReturn <<= (float)0;
             break;
 
