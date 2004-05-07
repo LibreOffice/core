38120f10d71d26827e7b026beca66955cd687b01
Rüdiger Timm <rt@openoffice.org>
2004-05-07 15:12:12 +0000

    INTEGRATION: CWS frmcontrols03 (1.1.2); FILE ADDED 2004/02/23 11:35:42 fs 1.1.2.2: some more implementations (mainly CJK relevant) 2004/02/18 15:29:17 fs 1.1.2.1: dispatching features to a rich text control which correspond with character or paragraph attributes, and require parameters

---
+++ forms/source/richtext/parametrizedattributedispatcher.hxx
@@ -0,0 +1,111 @@
+/*************************************************************************
+ *
+ *  $RCSfile: parametrizedattributedispatcher.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-05-07 16:12:12 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef FORMS_SOURCE_RICHTEXT_PARAMETRIZEDATTRIBUTEDISPATCHER_HXX
+#define FORMS_SOURCE_RICHTEXT_PARAMETRIZEDATTRIBUTEDISPATCHER_HXX
+
+#ifndef FORMS_SOURCE_RICHTEXT_ATTRIBUTEDISPATCHER_HXX
+#include "attributedispatcher.hxx"
+#endif
+
+class SfxPoolItem;
+//........................................................................
+namespace frm
+{
+//........................................................................
+
+    //====================================================================
+    //= OParametrizedAttributeDispatcher
+    //====================================================================
+    class OParametrizedAttributeDispatcher  :public OAttributeDispatcher
+    {
+    public:
+        OParametrizedAttributeDispatcher(
+            EditView&                           _rView,
+            AttributeId                         _nAttributeId,
+            const ::com::sun::star::util::URL&  _rURL,
+            IMultiAttributeDispatcher*          _pMasterDispatcher
+        );
+
+    protected:
+        ~OParametrizedAttributeDispatcher();
+
+        // XDispatch
+        virtual void SAL_CALL dispatch( const ::com::sun::star::util::URL& URL, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& Arguments ) throw (::com::sun::star::uno::RuntimeException);
+
+        // OAttributeDispatcher
+        virtual void    fillFeatureEventFromAttributeState( ::com::sun::star::frame::FeatureStateEvent& _rEvent, const AttributeState& _rState ) const;
+
+    protected:
+        // own overridables
+        /** convert the arguments as got in a XDispatch::dispatch call into an SfxPoolItem, which can
+            be used with a IMultiAttributeDispatcher::executeAttribute
+        */
+        virtual const SfxPoolItem* convertDispatchArgsToItem(
+            const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& _rArguments );
+    };
+
+//........................................................................
+} // namespace frm
+//........................................................................
+
+#endif // FORMS_SOURCE_RICHTEXT_PARAMETRIZEDATTRIBUTEDISPATCHER_HXX
+
