8c0702848d8710ffb542147aff5f8ad5418d4e19
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 15:29:53 +0000

    INTEGRATION: CWS dialogdiet01 (1.1.1.1.620); FILE MERGED 2004/03/26 09:12:05 mwu 1.1.1.1.620.1: sw model converted. 20040326

--- sw/source/ui/inc/redlndlg.hxx
+++ sw/source/ui/inc/redlndlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: redlndlg.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:14:41 $
+ *  last change: $Author: hr $ $Date: 2004-05-10 16:29:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,9 +66,145 @@
 #ifndef _BASEDLGS_HXX //autogen
 #include <sfx2/basedlgs.hxx>
 #endif
-
+#ifndef _REDLINE_HXX
+#include <redline.hxx> //CHINA001
+#endif
+#ifndef _REDLENUM_HXX
+#include <redlenum.hxx>
+#endif
+#ifndef _TOOLS_INTN_HXX //autogen
+#include <tools/intn.hxx>
+#endif
+#ifndef _DATETIME_HXX //autogen
+#include <tools/datetime.hxx>
+#endif
+#ifndef _SV_MSGBOX_HXX //autogen
+#include <vcl/msgbox.hxx>
+#endif
+#ifndef _SVSTDARR_HXX
+#include <svtools/svstdarr.hxx>
+#endif
+#ifndef _SFXENUMITEM_HXX //autogen
+#include <svtools/eitem.hxx>
+#endif
+#ifndef _SFXVIEWFRM_HXX //autogen
+#include <sfx2/viewfrm.hxx>
+#endif
+#ifndef _SFXDISPATCH_HXX //autogen
+#include <sfx2/dispatch.hxx>
+#endif
+#ifndef _SVX_CTREDLIN_HXX //autogen
+#include <svx/ctredlin.hxx>
+#endif
+#ifndef _SVX_POSTATTR_HXX //autogen
+#include <svx/postattr.hxx>
+#endif
 class SwChildWinWrapper;
-class SwRedlineAcceptDlg;
+
+//CHINA001 class SwRedlineAcceptDlg;
+//add CHINA001 begin this part is extracted from sw\source\ui\misc\redlndlg.cxx
+struct SwRedlineDataChild
+{
+    const SwRedlineData*        pChild;     // Verweis auf originale gestackte Daten
+    const SwRedlineDataChild*   pNext;      // Verweis auf gestackte Daten
+    SvLBoxEntry*                pTLBChild;  // zugehoeriger TreeListBox-Eintrag
+};
+
+struct SwRedlineDataParent
+{
+    const SwRedlineData*        pData;      // RedlineDataPtr
+    const SwRedlineDataChild*   pNext;      // Verweis auf gestackte Daten
+    SvLBoxEntry*                pTLBParent; // zugehoeriger TreeListBox-Eintrag
+    String                      sComment;   // Redline-Kommentar
+
+    inline BOOL operator==( const SwRedlineDataParent& rObj ) const
+                        { return (pData && pData->GetSeqNo() == rObj.pData->GetSeqNo()); }
+    inline BOOL operator< ( const SwRedlineDataParent& rObj ) const
+                        { return (pData && pData->GetSeqNo() <  rObj.pData->GetSeqNo()); }
+};
+
+typedef SwRedlineDataParent* SwRedlineDataParentPtr;
+SV_DECL_PTRARR_DEL(SwRedlineDataParentArr, SwRedlineDataParentPtr, 10, 20)
+
+
+SV_DECL_PTRARR_SORT(SwRedlineDataParentSortArr, SwRedlineDataParentPtr, 10, 20)
+
+
+typedef SwRedlineDataChild* SwRedlineDataChildPtr;
+SV_DECL_PTRARR_DEL(SwRedlineDataChildArr, SwRedlineDataChildPtr, 4, 4)
+
+
+typedef SvLBoxEntry* SvLBoxEntryPtr;
+SV_DECL_PTRARR(SvLBoxEntryArr, SvLBoxEntryPtr, 100, 100)
+
+class SwRedlineAcceptDlg
+{
+    Dialog*                 pParentDlg;
+    SwRedlineDataParentArr  aRedlineParents;
+    SwRedlineDataChildArr   aRedlineChilds;
+    SwRedlineDataParentSortArr aUsedSeqNo;
+    SvxAcceptChgCtr         aTabPagesCTRL;
+    PopupMenu               aPopup;
+    Timer                   aDeselectTimer;
+    Timer                   aSelectTimer;
+    String                  sInserted;
+    String                  sDeleted;
+    String                  sFormated;
+    String                  sTableChgd;
+    String                  sFmtCollSet;
+    String                  sFilterAction;
+    String                  sAutoFormat;
+    Size                    aBorderSz;
+    SvxTPView*              pTPView;
+    SvxRedlinTable*         pTable;
+    Link                    aOldSelectHdl;
+    Link                    aOldDeselectHdl;
+    BOOL                    bOnlyFormatedRedlines;
+    BOOL                    bHasReadonlySel;
+    BOOL                    bRedlnAutoFmt;
+
+    // prevent update dialog data during longer operations (cf #102657#)
+    bool                    bInhibitActivate;
+
+    DECL_LINK( AcceptHdl,       void* );
+    DECL_LINK( AcceptAllHdl,    void* );
+    DECL_LINK( RejectHdl,       void* );
+    DECL_LINK( RejectAllHdl,    void* );
+    DECL_LINK( UndoHdl,         void* );
+    DECL_LINK( DeselectHdl,     void* );
+    DECL_LINK( SelectHdl,       void* );
+    DECL_LINK( GotoHdl,         void* );
+    DECL_LINK( CommandHdl,      void* );
+
+    USHORT          CalcDiff(USHORT nStart, BOOL bChild);
+    void            InsertChilds(SwRedlineDataParent *pParent, const SwRedline& rRedln, const USHORT nAutoFmt);
+    void            InsertParents(USHORT nStart, USHORT nEnd = USHRT_MAX);
+    void            RemoveParents(USHORT nStart, USHORT nEnd);
+    void            InitAuthors();
+
+    String          GetRedlineText(const SwRedline& rRedln, DateTime &rDateTime, USHORT nStack = 0);
+    const String&   GetActionText(const SwRedline& rRedln, USHORT nStack = 0);
+    USHORT          GetRedlinePos( const SvLBoxEntry& rEntry) const;
+
+public:
+    SwRedlineAcceptDlg(Dialog *pParent, BOOL bAutoFmt = FALSE);
+    ~SwRedlineAcceptDlg();
+
+    DECL_LINK( FilterChangedHdl, void *pDummy = 0 );
+
+    inline SvxAcceptChgCtr* GetChgCtrl()        { return &aTabPagesCTRL; }
+    inline BOOL     HasRedlineAutoFmt() const   { return bRedlnAutoFmt; }
+
+    void            Init(USHORT nStart = 0);
+    void            CallAcceptReject( BOOL bSelect, BOOL bAccept );
+
+    void            Initialize(const String &rExtraData);
+    void            FillInfo(String &rExtraData) const;
+
+    virtual void    Activate();
+    virtual void    Resize();
+};
+//add CHINA001 end
 
 /*------------------------------------------------------------------------
     Beschreibung:
@@ -94,19 +230,19 @@ public:
     Beschreibung:
 ------------------------------------------------------------------------*/
 
-class SwModalRedlineAcceptDlg : public SfxModalDialog
-{
-    SwRedlineAcceptDlg*     pImplDlg;
-
-    virtual void    Resize();
-
-public:
-    SwModalRedlineAcceptDlg(Window *pParent);
-    ~SwModalRedlineAcceptDlg();
-
-    void            AcceptAll( BOOL bAccept );
-    virtual void    Activate();
-};
+//CHINA001 class SwModalRedlineAcceptDlg : public SfxModalDialog
+//CHINA001 {
+//CHINA001 SwRedlineAcceptDlg*      pImplDlg;
+//CHINA001
+//CHINA001 virtual void Resize();
+//CHINA001
+//CHINA001 public:
+//CHINA001 SwModalRedlineAcceptDlg(Window *pParent);
+//CHINA001 ~SwModalRedlineAcceptDlg();
+//CHINA001
+//CHINA001 void         AcceptAll( BOOL bAccept );
+//CHINA001 virtual void Activate();
+//CHINA001 };
 
 /*------------------------------------------------------------------------
     Beschreibung:
