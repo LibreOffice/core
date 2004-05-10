26ff1df5829867845cd9f9502e0cb1c54cbaa2d4
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 16:53:50 +0000

    INTEGRATION: CWS fwkbugfix02 (1.5.10); FILE MERGED 2004/04/19 13:35:48 mav 1.5.10.2: #116056# interfaces review 2004/04/07 16:34:51 mav 1.5.10.1: #116056# adopt to incompatible changes

--- embeddedobj/source/msole/oleembed.cxx
+++ embeddedobj/source/msole/oleembed.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: oleembed.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: mav $ $Date: 2003-12-15 15:44:26 $
+ *  last change: $Author: hr $ $Date: 2004-05-10 17:53:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -98,16 +98,16 @@ using namespace ::com::sun::star;
 
 //----------------------------------------------
 uno::Sequence< sal_Int32 > OleEmbeddedObject::GetReachableStatesList_Impl(
-                                                        const uno::Sequence< embed::VerbDescr >& aVerbList )
+                                                        const uno::Sequence< embed::VerbDescriptor >& aVerbList )
 {
     uno::Sequence< sal_Int32 > aStates(2);
-    aStates[0] = embed::EmbedStates::EMBED_LOADED;
-    aStates[1] = embed::EmbedStates::EMBED_RUNNING;
+    aStates[0] = embed::EmbedStates::LOADED;
+    aStates[1] = embed::EmbedStates::RUNNING;
     for ( sal_Int32 nInd = 0; nInd < aVerbList.getLength(); nInd++ )
         if ( aVerbList[nInd].VerbID == embed::EmbedVerbs::MS_OLEVERB_OPEN )
         {
             aStates.realloc(3);
-            aStates[2] = embed::EmbedStates::EMBED_ACTIVE;
+            aStates[2] = embed::EmbedStates::ACTIVE;
         }
 
     return aStates;
@@ -116,10 +116,10 @@ uno::Sequence< sal_Int32 > OleEmbeddedObject::GetReachableStatesList_Impl(
 //----------------------------------------------
 uno::Sequence< sal_Int32 > OleEmbeddedObject::GetIntermediateVerbsSequence_Impl( sal_Int32 nNewState )
 {
-    OSL_ENSURE( m_nObjectState != embed::EmbedStates::EMBED_LOADED, "Loaded object is switched to running state without verbs using!" );
+    OSL_ENSURE( m_nObjectState != embed::EmbedStates::LOADED, "Loaded object is switched to running state without verbs using!" );
 
     // actually there will be only one verb
-    if ( m_nObjectState == embed::EmbedStates::EMBED_RUNNING && nNewState == embed::EmbedStates::EMBED_ACTIVE )
+    if ( m_nObjectState == embed::EmbedStates::RUNNING && nNewState == embed::EmbedStates::ACTIVE )
     {
         uno::Sequence< sal_Int32 > aVerbs( 1 );
         aVerbs[0] = embed::EmbedVerbs::MS_OLEVERB_OPEN;
@@ -151,7 +151,7 @@ void SAL_CALL OleEmbeddedObject::changeState( sal_Int32 nNewState )
     // TODO: additional verbs can be a problem, since nobody knows how the object
     //       will behave after activation
 
-    if ( nNewState == embed::EmbedStates::EMBED_LOADED )
+    if ( nNewState == embed::EmbedStates::LOADED )
     {
         // This means just closing of the current object
         // If component can not be closed the object stays in loaded state
@@ -160,9 +160,9 @@ void SAL_CALL OleEmbeddedObject::changeState( sal_Int32 nNewState )
         // the component will become "complete"
         GetRidOfComponent();
     }
-    else if ( nNewState == embed::EmbedStates::EMBED_RUNNING || nNewState == embed::EmbedStates::EMBED_ACTIVE )
+    else if ( nNewState == embed::EmbedStates::RUNNING || nNewState == embed::EmbedStates::ACTIVE )
     {
-        if ( m_nObjectState == embed::EmbedStates::EMBED_LOADED )
+        if ( m_nObjectState == embed::EmbedStates::LOADED )
         {
             // if the target object is in loaded state and a different state is specified
             // as a new one the object first must be switched to running state.
@@ -172,18 +172,18 @@ void SAL_CALL OleEmbeddedObject::changeState( sal_Int32 nNewState )
             CreateOleComponentAndLoad_Impl( m_pOleComponent );
 
             m_pOleComponent->RunObject();
-            m_nObjectState = embed::EmbedStates::EMBED_RUNNING;
+            m_nObjectState = embed::EmbedStates::RUNNING;
             if ( m_nObjectState == nNewState )
                 return;
         }
 
         // so now the object is either switched from Active to Running state or vise versa
-        if ( m_nObjectState == embed::EmbedStates::EMBED_RUNNING && nNewState == embed::EmbedStates::EMBED_ACTIVE )
+        if ( m_nObjectState == embed::EmbedStates::RUNNING && nNewState == embed::EmbedStates::ACTIVE )
         {
             // execute OPEN verb, if object does not reach active state it is an object's problem
             m_pOleComponent->ExecuteVerb( embed::EmbedVerbs::MS_OLEVERB_OPEN );
         }
-        else if ( m_nObjectState == embed::EmbedStates::EMBED_ACTIVE && nNewState == embed::EmbedStates::EMBED_RUNNING )
+        else if ( m_nObjectState == embed::EmbedStates::ACTIVE && nNewState == embed::EmbedStates::RUNNING )
         {
             m_pOleComponent->CloseObject();
             m_pOleComponent->RunObject();
@@ -213,7 +213,7 @@ uno::Sequence< sal_Int32 > SAL_CALL OleEmbeddedObject::getReachableStates()
                                         uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 
 #ifdef WNT
-    if ( m_nObjectState == embed::EmbedStates::EMBED_LOADED )
+    if ( m_nObjectState == embed::EmbedStates::LOADED )
     {
         // the list of supported verbs can be retrieved only when object is in running state
         throw embed::NeedsRunningStateException(); // TODO:
@@ -264,7 +264,7 @@ void SAL_CALL OleEmbeddedObject::doVerb( sal_Int32 nVerbID )
                                         uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 
 #ifdef WNT
-    if ( m_nObjectState == embed::EmbedStates::EMBED_LOADED )
+    if ( m_nObjectState == embed::EmbedStates::LOADED )
     {
         // if the target object is in loaded state
         // it must be switched to running state to execute verb
@@ -273,7 +273,7 @@ void SAL_CALL OleEmbeddedObject::doVerb( sal_Int32 nVerbID )
         // it can be created during loading
         CreateOleComponentAndLoad_Impl( m_pOleComponent );
         m_pOleComponent->RunObject();
-        m_nObjectState = embed::EmbedStates::EMBED_RUNNING;
+        m_nObjectState = embed::EmbedStates::RUNNING;
     }
 
     if ( !m_pOleComponent )
@@ -286,7 +286,7 @@ void SAL_CALL OleEmbeddedObject::doVerb( sal_Int32 nVerbID )
 }
 
 //----------------------------------------------
-uno::Sequence< embed::VerbDescr > SAL_CALL OleEmbeddedObject::getSupportedVerbs()
+uno::Sequence< embed::VerbDescriptor > SAL_CALL OleEmbeddedObject::getSupportedVerbs()
         throw ( embed::WrongStateException,
                 uno::RuntimeException )
 {
@@ -298,7 +298,7 @@ uno::Sequence< embed::VerbDescr > SAL_CALL OleEmbeddedObject::getSupportedVerbs(
         throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The object has no persistence!\n" ),
                                         uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 #ifdef WNT
-    if ( m_nObjectState == embed::EmbedStates::EMBED_LOADED )
+    if ( m_nObjectState == embed::EmbedStates::LOADED )
     {
         // the list of supported verbs can be retrieved only when object is in running state
         throw embed::NeedsRunningStateException(); // TODO:
@@ -309,7 +309,7 @@ uno::Sequence< embed::VerbDescr > SAL_CALL OleEmbeddedObject::getSupportedVerbs(
 
     return m_pOleComponent->GetVerbList();
 #else
-    return uno::Sequence< embed::VerbDescr >();
+    return uno::Sequence< embed::VerbDescriptor >();
 #endif
 }
 
@@ -360,14 +360,14 @@ void SAL_CALL OleEmbeddedObject::update()
         throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The object has no persistence!\n" ),
                                         uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 
-    if ( m_nUpdateMode == embed::EmbedUpdateModes::EMBED_EXPLICIT_UPDATE )
+    if ( m_nUpdateMode == embed::EmbedUpdateModes::EXPLICIT_UPDATE )
     {
         // TODO: update view representation
     }
     else
     {
         // the object must be up to date
-        OSL_ENSURE( m_nUpdateMode == embed::EmbedUpdateModes::EMBED_ALWAYS_UPDATE, "Unknown update mode!\n" );
+        OSL_ENSURE( m_nUpdateMode == embed::EmbedUpdateModes::ALWAYS_UPDATE, "Unknown update mode!\n" );
     }
 }
 
@@ -384,8 +384,8 @@ void SAL_CALL OleEmbeddedObject::setUpdateMode( sal_Int32 nMode )
         throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The object has no persistence!\n" ),
                                         uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 
-    OSL_ENSURE( nMode == embed::EmbedUpdateModes::EMBED_ALWAYS_UPDATE
-                    || nMode == embed::EmbedUpdateModes::EMBED_EXPLICIT_UPDATE,
+    OSL_ENSURE( nMode == embed::EmbedUpdateModes::ALWAYS_UPDATE
+                    || nMode == embed::EmbedUpdateModes::EXPLICIT_UPDATE,
                 "Unknown update mode!\n" );
     m_nUpdateMode = nMode;
 }
@@ -399,7 +399,7 @@ sal_Int64 SAL_CALL OleEmbeddedObject::getStatus( sal_Int64 nAspect )
     if ( m_bDisposed )
         throw lang::DisposedException(); // TODO
 
-    if ( m_nObjectState == -1 || m_nObjectState == embed::EmbedStates::EMBED_LOADED )
+    if ( m_nObjectState == -1 || m_nObjectState == embed::EmbedStates::LOADED )
         throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The object must be in running state!\n" ),
                                     uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 
@@ -413,4 +413,16 @@ sal_Int64 SAL_CALL OleEmbeddedObject::getStatus( sal_Int64 nAspect )
                                     uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
 #endif
 }
+
+//----------------------------------------------
+void SAL_CALL OleEmbeddedObject::setContainerName( const ::rtl::OUString& sName )
+        throw ( uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    m_aContainerName = sName;
+}
+
 
