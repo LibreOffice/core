b5c1710c8eea57472be5ba0644364a95ba2325ef
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 15:26:23 +0000

    INTEGRATION: CWS dialogdiet01 (1.3.196); FILE MERGED 2004/03/26 09:12:02 mwu 1.3.196.1: sw model converted. 20040326

--- sw/source/ui/fmtui/makefile.mk
+++ sw/source/ui/fmtui/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 14:56:20 $
+#   last change: $Author: hr $ $Date: 2004-05-10 16:26:23 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,7 +64,7 @@ PRJ=..$/..$/..
 
 PRJNAME=sw
 TARGET=fmtui
-
+LIBTARGET=NO
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  $(PRJ)$/inc$/swpre.mk
