ad42b2210acad9fd12593b84e0b8f72f6bde110f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 17:11:08 +0000

    INTEGRATION: CWS fwkbugfix02 (1.3.8); FILE MERGED 2004/04/07 16:06:40 mav 1.3.8.1: #116056# adopt to incompatible changes

--- sot/source/sdstor/unostorageholder.cxx
+++ sot/source/sdstor/unostorageholder.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unostorageholder.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2004-02-25 17:28:18 $
+ *  last change: $Author: hr $ $Date: 2004-05-10 18:11:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -162,7 +162,7 @@ void SAL_CALL UNOStorageHolder::commited( const lang::EventObject& aEvent )
 
     uno::Sequence< uno::Any > aArg( 2 );
     aArg[0] <<= ::rtl::OUString( aTmpStorFile.GetURL() );
-    aArg[1] <<= embed::ElementModes::ELEMENT_READWRITE;
+    aArg[1] <<= embed::ElementModes::READWRITE;
     uno::Reference< embed::XStorage > xTempStorage( xStorageFactory->createInstanceWithArguments( aArg ), uno::UNO_QUERY );
 
     OSL_ENSURE( xTempStorage.is(), "Can't open storage!\n" );
