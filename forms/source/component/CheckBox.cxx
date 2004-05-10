b97d8a0e65bdd27e7a3c2efb9df87897f8402be3
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 11:43:52 +0000

    INTEGRATION: CWS dba09 (1.10.44); FILE MERGED 2004/04/27 06:13:42 fs 1.10.44.2: RESYNC: (1.10-1.11); FILE MERGED 2004/03/17 11:51:02 fs 1.10.44.1: #92831# at runtime, don't use 'stardiv.one.form.control.*' service names - translate when writing old (binary) format

--- forms/source/component/CheckBox.cxx
+++ forms/source/component/CheckBox.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CheckBox.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2004-04-02 10:49:08 $
+ *  last change: $Author: hr $ $Date: 2004-05-10 12:43:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -136,7 +136,7 @@ InterfaceRef SAL_CALL OCheckBoxModel_CreateInstance(const Reference<XMultiServic
 DBG_NAME( OCheckBoxModel )
 //------------------------------------------------------------------
 OCheckBoxModel::OCheckBoxModel(const Reference<XMultiServiceFactory>& _rxFactory)
-    :OBoundControlModel( _rxFactory, VCL_CONTROLMODEL_CHECKBOX, FRM_CONTROL_CHECKBOX, sal_False, sal_True, sal_True )
+    :OBoundControlModel( _rxFactory, VCL_CONTROLMODEL_CHECKBOX, FRM_SUN_CONTROL_CHECKBOX, sal_False, sal_True, sal_True )
                     // use the old control name for compytibility reasons
 {
     DBG_CTOR( OCheckBoxModel, NULL );
