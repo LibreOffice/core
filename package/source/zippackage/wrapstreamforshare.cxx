b8bc2f2f6b257f4f9b6c8a0073b8cca48f9a5699
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 16:30:21 +0000

    INTEGRATION: CWS fwkbugfix02 (1.2.12); FILE MERGED 2004/03/17 11:39:25 mav 1.2.12.1: #115957# Extend the cloning functionality

--- package/source/zippackage/wrapstreamforshare.cxx
+++ package/source/zippackage/wrapstreamforshare.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrapstreamforshare.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:02:34 $
+ *  last change: $Author: hr $ $Date: 2004-05-10 17:30:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -165,7 +165,8 @@ void SAL_CALL WrapStreamForShare::closeInput()
     if ( !m_xInStream.is() )
         throw io::IOException(); // TODO
 
-    m_xInStream->closeInput();
+    // the package is the owner so it will close the stream
+    // m_xInStream->closeInput();
     m_xInStream = uno::Reference< io::XInputStream >();
     m_xSeekable = uno::Reference< io::XSeekable >();
 }
