389374af12f3f822d947eaf6fd7768c317698504
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 15:19:05 +0000

    INTEGRATION: CWS dialogdiet01 (1.3.196); FILE MERGED 2004/03/26 09:11:56 mwu 1.3.196.1: sw model converted. 20040326

--- sw/source/ui/chrdlg/makefile.mk
+++ sw/source/ui/chrdlg/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 14:55:06 $
+#   last change: $Author: hr $ $Date: 2004-05-10 16:19:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,7 +64,7 @@ PRJ=..$/..$/..
 
 PRJNAME=sw
 TARGET=chrdlg
-
+LIBTARGET=NO
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  $(PRJ)$/inc$/swpre.mk
@@ -88,9 +88,15 @@ SLOFILES =	\
         $(SLO)$/chardlg.obj \
         $(SLO)$/drpcps.obj \
         $(SLO)$/ccoll.obj \
+        $(SLO)$/swuiccoll.obj \
         $(SLO)$/pardlg.obj \
         $(SLO)$/tblnumfm.obj \
         $(SLO)$/numpara.obj
+        
+LIB1TARGET = $(SLB)$/$(TARGET).lib
+
+LIB1OBJFILES =  \
+    $(SLO)$/ccoll.obj
 
 # --- Tagets -------------------------------------------------------
 
