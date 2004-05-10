a9c4f79b5bad48cf640141968037be46a79964a2
Jens-Heiner Rechtien <hr@openoffice.org>
2004-05-10 15:01:45 +0000

    INTEGRATION: CWS dialogdiet01 (1.1.2); FILE ADDED 2004/04/22 01:40:26 mwu 1.1.2.1: dialogdiet01

---
+++ sc/source/ui/inc/scui_def.hxx
@@ -0,0 +1,88 @@
+/*************************************************************************
+ *
+ *  $RCSfile: scui_def.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-05-10 16:01:45 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SCUI_DEF_HXX
+#define SCUI_DEF_HXX
+
+#define SCRET_COLS 0x42
+#define SCRET_ROWS 0x43
+
+#define FDS_OPT_NONE        0  // from filldlg.hxx
+#define FDS_OPT_HORZ        1   // from filldlg.hxx
+#define FDS_OPT_VERT        2  // from filldlg.hxx
+
+#define INS_CONT_NOEMPTY    0x0100 //from inscodlg.hxx
+#define INS_CONT_TRANS      0x0200 //from inscodlg.hxx
+#define INS_CONT_LINK       0x0400 //from inscodlg.hxx
+
+#define SC_CELL_SHIFT_DISABLE_DOWN  0x01 //from inscodlg.hxx
+#define SC_CELL_SHIFT_DISABLE_RIGHT 0x02 //from inscodlg.hxx
+
+#define NAME_TOP        1 //from namecrea.hxx
+#define NAME_LEFT       2 //from namecrea.hxx
+#define NAME_BOTTOM     4  //from namecrea.hxx
+#define NAME_RIGHT      8 //from namecrea.hxx
+
+#define BTN_PASTE_NAME  100  // from namepast.hxx
+#define BTN_PASTE_LIST  101  // from namepast.hxx
+
+#define SCRET_REMOVE    0x42 //from subtdlg.hxx
+#endif
