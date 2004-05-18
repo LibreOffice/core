d2ff8be54105455b74dfeb0d5fadfd7350b48576
Rüdiger Timm <rt@openoffice.org>
2004-05-18 12:33:19 +0000

    INTEGRATION: CWS sdk13 (1.4.118); FILE MERGED 2004/05/11 12:16:47 jsc 1.4.118.2: #116705# ask for optional output directory 2004/05/05 09:32:50 jsc 1.4.118.1: #116658# merge cinnabar changes

--- odk/settings/std.mk
+++ odk/settings/std.mk
@@ -1,5 +1,9 @@
-OSOUT=$(OS)example.out
-OUT=$(PRJ)/$(OSOUT)
+ifneq "$(OO_SDK_OUTPUT_DIR)" ""
+OUT=$(subst \,/,$(OO_SDK_OUTPUT_DIR))/$(SDKNAME)/$(OS)example.out
+else
+OUT=$(PRJ)/$(OS)example.out
+endif
+
 OUT_BIN=$(OUT)/bin
 OUT_INC=$(OUT)/inc
 OUT_SLO=$(OUT)/slo
@@ -12,7 +16,11 @@ IDL_DIR=$(PRJ)/idl
 BIN_DIR=$(PRJ)/$(PLATFORM)/bin
 CLASSES_DIR=$(PRJ)/classes
 OFFICE_CLASSES_DIR=$(subst \,/,$(OFFICE_PROGRAM_PATH))/classes
-COMPONENT_PACKAGE_DIR=$(OO_SDK_HOME)$(PS)$(OS)example.out$(PS)bin
+COMPONENT_PACKAGE_DIR=$(subst /,$(PS),$(OUT_BIN))
+
+OFFICE_TYPE_LIBRARY=$(subst \\,\,"$(OFFICE_PROGRAM_PATH)$(PS)types.rdb")
+# DKREGISTRYNAME is only for compatibility reasons 
+DKREGISTRYNAME=$(OFFICE_TYPE_LIBRARY)
 
 DEPLOYTOOL="$(OFFICE_PROGRAM_PATH)$(PS)pkgchk" -f
 SDK_JAVA="$(OO_SDK_JAVA_HOME)/bin/java"
@@ -22,6 +30,7 @@ IDLC="$(OO_SDK_HOME)/$(PLATFORM)/bin/idlc"
 CPPUMAKER="$(OO_SDK_HOME)/$(PLATFORM)/bin/cppumaker"
 JAVAMAKER="$(OO_SDK_HOME)/$(PLATFORM)/bin/javamaker"
 REGMERGE="$(OO_SDK_HOME)/$(PLATFORM)/bin/regmerge"
+REGCOMP="$(OO_SDK_HOME)/$(PLATFORM)/bin/regcomp"
 XML2CMP="$(OO_SDK_HOME)/$(PLATFORM)/bin/xml2cmp"
 
 SDK_JAVA_UNO_BOOTSTRAP_FILES=\
