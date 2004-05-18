9059b8dc2d23c121220fb975f1d54dba690b5f93
Kurt Zenker <kz@openoffice.org>
2004-05-18 12:59:09 +0000

    INTEGRATION: CWS swundo01 (1.1.1.1.336); FILE MERGED 2003/08/15 13:21:37 hbrinkm 1.1.1.1.336.1: #111827#

--- sw/inc/rolbck.hxx
+++ sw/inc/rolbck.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rolbck.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-17 16:11:02 $
+ *  last change: $Author: kz $ $Date: 2004-05-18 13:59:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -291,6 +291,7 @@ public:
     OUT_HSTR_HINT(Bookmark)
 
     BOOL IsEqualBookmark( const SwBookmark& );
+    const String & GetName() const;
 };
 
 class SwHstrySetAttrSet : public SwHstryHint
