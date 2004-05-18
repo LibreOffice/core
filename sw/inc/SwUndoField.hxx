2f657bc70ffe2da188255781f55296e862fd51fe
Kurt Zenker <kz@openoffice.org>
2004-05-18 12:55:49 +0000

    INTEGRATION: CWS swundo01 (1.1.2); FILE ADDED 2004/01/07 15:13:31 hbrinkm 1.1.2.1: #111840#

---
+++ sw/inc/SwUndoField.hxx
@@ -0,0 +1,122 @@
+/*************************************************************************
+ *
+ *  $RCSfile: SwUndoField.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-05-18 13:55:49 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SW_UNDO_FIELD_HXX
+#define _SW_UNDO_FIELD_HXX
+
+#include <doc.hxx>
+#include <undobj.hxx>
+#include <pam.hxx>
+
+class SwTxtFld;
+class SwField;
+class SwMsgPoolItem;
+
+class SwUndoField : public SwUndo
+{
+    ULONG nNodeIndex;
+    xub_StrLen nOffset;
+
+protected:
+    SwDoc * pDoc;
+    SwPosition GetPosition();
+
+    void RestoreFieldType(SwField * pField, SwFieldType * pNewType);
+
+public:
+    SwUndoField(const SwPosition & rPos);
+    virtual ~SwUndoField();
+};
+
+class SwUndoFieldFromDoc : public SwUndoField
+{
+    SwField * pOldField, * pNewField;
+    SwMsgPoolItem * pHnt;
+    BOOL bUpdate;
+
+public:
+    SwUndoFieldFromDoc(const SwPosition & rPos, const SwField & aOldField,
+                       const SwField & aNewField,
+                       SwMsgPoolItem * pHnt, BOOL bUpdate);
+    virtual ~SwUndoFieldFromDoc();
+
+    virtual void Undo(SwUndoIter & rIt);
+    virtual void Redo(SwUndoIter & rIt);
+    virtual void Repeat(SwUndoIter & rIt);
+};
+
+class SwUndoFieldFromAPI : public SwUndoField
+{
+    com::sun::star::uno::Any aOldVal, aNewVal;
+    BYTE nMId;
+
+public:
+    SwUndoFieldFromAPI(const SwPosition & rPos,
+                       const com::sun::star::uno::Any & rOldVal,
+                       const com::sun::star::uno::Any & rNewVal,
+                       BYTE nMId);
+    virtual ~SwUndoFieldFromAPI();
+
+    virtual void Undo(SwUndoIter & rIt);
+    virtual void Redo(SwUndoIter & rIt);
+    virtual void Repeat(SwUndoIter & rIt);
+};
+
+#endif // _SW_UNDO_FIELD_HXX
