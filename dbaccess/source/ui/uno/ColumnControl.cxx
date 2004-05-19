aebd64c832b66157f5f4f36227cca36709868970
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:56:37 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/01/14 09:19:01 oj 1.1.2.1: #i24380# new sverices for column descriptor control and model

---
+++ dbaccess/source/ui/uno/ColumnControl.cxx
@@ -0,0 +1,180 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ColumnControl.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-05-19 13:56:37 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef DBAUI_COLUMNCONTROL_HXX
+#include "ColumnControl.hxx"
+#endif
+#ifndef DBAUI_COLUMNPEER_HXX
+#include "ColumnPeer.hxx"
+#endif
+#ifndef DBACCESS_SHARED_DBUSTRINGS_HRC
+#include "dbustrings.hrc"
+#endif
+#ifndef _DBASHARED_APITOOLS_HXX_
+#include "apitools.hxx"
+#endif
+#ifndef _COM_SUN_STAR_AWT_POSSIZE_HPP_
+#include <com/sun/star/awt/PosSize.hpp>
+#endif
+#ifndef _DBU_REGHELPER_HXX_
+#include "dbu_reghelper.hxx"
+#endif
+
+extern "C" void SAL_CALL createRegistryInfo_OColumnControl()
+{
+    static ::dbaui::OMultiInstanceAutoRegistration< ::dbaui::OColumnControl> aAutoRegistration;
+}
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::sdbc;
+
+OColumnControl::OColumnControl(const Reference<XMultiServiceFactory>& _rxFactory)
+: m_xORB(_rxFactory)
+{
+}
+// -----------------------------------------------------------------------------
+IMPLEMENT_SERVICE_INFO2_STATIC(OColumnControl,SERVICE_CONTROLDEFAULT,"com.sun.star.awt.UnoControl","com.sun.star.sdb.ColumnDescriptorControl")
+// -----------------------------------------------------------------------------
+::rtl::OUString OColumnControl::GetComponentServiceName()
+{
+    return ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.sdb.ColumnDescriptorControl"));
+}
+// -----------------------------------------------------------------------------
+void SAL_CALL OColumnControl::createPeer(const Reference< XToolkit >& rToolkit, const Reference< XWindowPeer >& rParentPeer) throw( RuntimeException )
+{
+    ::osl::ClearableMutexGuard aGuard( GetMutex() );
+    if ( !getPeer().is() )
+    {
+        mbCreatingPeer = sal_True;
+
+        Window* pParentWin = NULL;
+        if (rParentPeer.is())
+        {
+            VCLXWindow* pParent = VCLXWindow::GetImplementation(rParentPeer);
+            if (pParent)
+                pParentWin = pParent->GetWindow();
+        }
+
+        OColumnPeer* pPeer = new OColumnPeer(pParentWin,m_xORB);
+        OSL_ENSURE(pPeer != NULL, "FmXGridControl::createPeer : imp_CreatePeer didn't return a peer !");
+        setPeer( pPeer );
+
+        UnoControlComponentInfos aComponentInfos(maComponentInfos);
+        sal_Bool bDesignMode(mbDesignMode);
+        Reference< XGraphics > xGraphics( mxGraphics );
+        Reference< XView >  xV(getPeer(), UNO_QUERY);
+        Reference< XWindow >    xW(getPeer(), UNO_QUERY);
+
+        aGuard.clear();
+
+        updateFromModel();
+
+        xV->setZoom( aComponentInfos.nZoomX, aComponentInfos.nZoomY );
+        setPosSize( aComponentInfos.nX, aComponentInfos.nY, aComponentInfos.nWidth, aComponentInfos.nHeight, ::com::sun::star::awt::PosSize::POSSIZE );
+
+        Reference<XPropertySet> xProp(getModel(), UNO_QUERY);
+        if ( xProp.is() )
+        {
+            Reference<XConnection> xCon(xProp->getPropertyValue(PROPERTY_ACTIVECONNECTION),UNO_QUERY);
+            pPeer->setConnection(xCon);
+            Reference<XPropertySet> xColumn(xProp->getPropertyValue(PROPERTY_COLUMN),UNO_QUERY);
+            pPeer->setColumn(xColumn);
+        }
+
+        if (aComponentInfos.bVisible)
+            xW->setVisible(sal_True);
+
+        if (!aComponentInfos.bEnable)
+            xW->setEnable(sal_False);
+
+        if (maWindowListeners.getLength())
+            xW->addWindowListener( &maWindowListeners );
+
+        if (maFocusListeners.getLength())
+            xW->addFocusListener( &maFocusListeners );
+
+        if (maKeyListeners.getLength())
+            xW->addKeyListener( &maKeyListeners );
+
+        if (maMouseListeners.getLength())
+            xW->addMouseListener( &maMouseListeners );
+
+        if (maMouseMotionListeners.getLength())
+            xW->addMouseMotionListener( &maMouseMotionListeners );
+
+        if (maPaintListeners.getLength())
+            xW->addPaintListener( &maPaintListeners );
+
+        Reference< ::com::sun::star::awt::XView >  xPeerView(getPeer(), UNO_QUERY);
+        xPeerView->setZoom( maComponentInfos.nZoomX, maComponentInfos.nZoomY );
+        xPeerView->setGraphics( xGraphics );
+
+        mbCreatingPeer = sal_False;
+    }
+}
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
