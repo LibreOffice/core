6106cdbbaaf59880c59605dd776c2ba5900905da
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:05:15 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/30 13:36:05 tv 1.1.2.10: added support for HelpURLs Issue number: Submitted by: Reviewed by: 2004/03/23 15:56:31 tv 1.1.2.9: fixed bug in PathSelection and changed CallStoreDialog to pure URL handling 2004/03/02 09:14:22 tv 1.1.2.8: made txtPath public Issue number: Submitted by: Reviewed by: 2004/02/13 10:32:42 tv 1.1.2.7: correct TabIndex Issue number: Submitted by: Reviewed by: 2004/02/03 14:59:49 rpiterman 1.1.2.6: solved merge conflicts 2004/02/02 11:29:44 tv 1.1.2.5: formatted with autoformatter (indents now use TAB) 2004/01/22 19:44:14 bc 1.1.2.4: #111603# querywizard now short before final 2003/11/28 09:36:28 rpiterman 1.1.2.3: Changed to support changes in class SystemDialog (using now static method to instanciate) 2003/11/14 13:08:01 tv 1.1.2.2: changed resources to dbwizres 2003/10/17 13:52:27 tv 1.1.2.1: Path Selection control

---
+++ wizards/com/sun/star/wizards/ui/PathSelection.java
@@ -0,0 +1,120 @@
+package com.sun.star.wizards.ui;
+
+import com.sun.star.awt.XTextComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.Exception;
+import com.sun.star.wizards.common.FileAccess;
+import com.sun.star.wizards.common.SystemDialog;
+
+public class PathSelection {
+
+    UnoDialog2 CurUnoDialog;
+    XMultiServiceFactory xMSF;
+    int iDialogType;
+    int iTransferMode;
+
+    public String sDefaultDirectory = "";
+    public String sDefaultName = "";
+    public String sDefaultFilter = "";
+
+    public XPathSelectionListener xAction;
+    public XTextComponent xSaveTextBox;
+
+    private final int CMDSELECTPATH = 1;
+    private final int TXTSAVEPATH = 1;
+
+    public static class DialogTypes {
+        public static final int FOLDER = 0;
+        public static final int FILE = 1;
+    }
+
+    public static class TransferMode {
+        public static final int SAVE = 0;
+        public static final int LOAD = 1;
+    }
+
+    public PathSelection(XMultiServiceFactory xMSF, UnoDialog2 CurUnoDialog, int TransferMode, int DialogType) {
+        this.CurUnoDialog = CurUnoDialog;
+        this.xMSF = xMSF;
+        this.iDialogType = DialogType;
+        this.iTransferMode = TransferMode;
+
+    }
+
+    public void insert(int DialogStep, int XPos, int YPos, int Width, short CurTabIndex, String LabelText, boolean Enabled, String TxtHelpURL, String BtnHelpURL) {
+
+        CurUnoDialog.insertControlModel("com.sun.star.awt.UnoControlFixedTextModel", "lblSaveAs", new String[] { "Enabled", "Height", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width" }, new Object[] { new Boolean(Enabled), new Integer(8), LabelText, new Integer(XPos), new Integer(YPos), new Integer(DialogStep), new Short(CurTabIndex), new Integer(Width)});
+
+        xSaveTextBox = CurUnoDialog.insertTextField("txtSavePath", "callXPathSelectionListener", this, new String[] { "Enabled", "Height", "HelpURL", "PositionX", "PositionY", "Step", "TabIndex", "Width" }, new Object[] { new Boolean(Enabled), new Integer(12), TxtHelpURL, new Integer(XPos), new Integer(YPos + 10), new Integer(DialogStep), new Short((short)(CurTabIndex + 1)), new Integer(Width - 26)});
+
+        CurUnoDialog.insertButton("cmdSelectPath", "triggerPathPicker", this, new String[] { "Enabled", "Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width" }, new Object[] { new Boolean(Enabled), new Integer(14), BtnHelpURL, "...", new Integer(XPos + Width - 16), new Integer(YPos + 9), new Integer(DialogStep), new Short((short)(CurTabIndex + 2)), new Integer(16)});
+
+    }
+
+    public void addSelectionListener(XPathSelectionListener xAction) {
+        this.xAction = xAction;
+    }
+
+    public String getSelectedPath() {
+        return xSaveTextBox.getText();
+    }
+
+    public void initializePath() {
+        try {
+            FileAccess myFA = new FileAccess(xMSF);
+            xSaveTextBox.setText(myFA.getPath(sDefaultDirectory + "/" + sDefaultName, null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void triggerPathPicker() {
+        try {
+            switch (iTransferMode) {
+                case TransferMode.SAVE :
+                    switch (iDialogType) {
+                        case DialogTypes.FOLDER :
+                            //TODO: write code for picking a folder for saving
+                            break;
+                        case DialogTypes.FILE :
+                            SystemDialog myFilePickerDialog = SystemDialog.createStoreDialog(xMSF);
+                            myFilePickerDialog.callStoreDialog(sDefaultDirectory, sDefaultName, sDefaultFilter);
+                            String sStorePath = myFilePickerDialog.sStorePath;
+                            if (sStorePath != null) {
+                                FileAccess myFA = new FileAccess(xMSF);
+                                xSaveTextBox.setText(myFA.getPath(sStorePath,null));
+                                sDefaultDirectory = FileAccess.getParentDir(sStorePath);
+                                sDefaultName = myFA.getFilename(sStorePath);
+                            }
+                            break;
+                        default :
+                            break;
+                    }
+                    break;
+                case TransferMode.LOAD :
+                    switch (iDialogType) {
+                        case DialogTypes.FOLDER :
+                            //TODO: write code for picking a folder for loading
+                            break;
+                        case DialogTypes.FILE :
+                            //TODO: write code for picking a file for loading
+                            break;
+                        default :
+                            break;
+                    }
+                    break;
+                default :
+                    break;
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void callXPathSelectionListener() {
+        if (xAction != null)
+            xAction.validatePath();
+    }
+
+}
