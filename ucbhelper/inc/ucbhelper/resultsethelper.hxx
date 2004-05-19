b6eb07fdedd0e8122b1803aab5abfccfeb302fb9
Rüdiger Timm <rt@openoffice.org>
2004-05-19 12:12:39 +0000

    INTEGRATION: CWS uno8eabugs01 (1.1.1.1.134); FILE MERGED 2004/05/17 14:30:09 kso 1.1.1.1.134.1: #i27607# - Corrected dynamic resultset service name.

--- ucbhelper/inc/ucbhelper/resultsethelper.hxx
+++ ucbhelper/inc/ucbhelper/resultsethelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: resultsethelper.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:03:37 $
+ *  last change: $Author: rt $ $Date: 2004-05-19 13:12:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -101,8 +101,7 @@ namespace ucb {
 
 //=========================================================================
 
-#define DYNAMICRESULTSET_SERVICE_NAME \
-                            "com.sun.star.ucb.DynamicContentResultSet"
+#define DYNAMICRESULTSET_SERVICE_NAME "com.sun.star.ucb.DynamicResultSet"
 
 //=========================================================================
 
