19c65983e769a95406b6f24bab16a69272716074
Rüdiger Timm <rt@openoffice.org>
2004-05-19 07:33:41 +0000

    INTEGRATION: CWS scriptingf4 (1.1.2); FILE ADDED 2004/03/24 13:34:22 dfoster 1.1.2.1: #i26923# Implmentation of the Script Organizer spec.

---
+++ sfx2/source/control/sorgitm.cxx
@@ -0,0 +1,167 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sorgitm.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-05-19 08:33:41 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#pragma hdrstop
+
+#include "sfxsids.hrc"
+#include "sorgitm.hxx"
+// STATIC DATA -----------------------------------------------------------
+
+TYPEINIT1_AUTOFACTORY(SfxScriptOrganizerItem, SfxStringItem);
+
+//------------------------------------------------------------------------
+
+SfxScriptOrganizerItem::SfxScriptOrganizerItem() :
+
+    SfxStringItem()
+
+{
+}
+
+//------------------------------------------------------------------------
+
+SfxScriptOrganizerItem::SfxScriptOrganizerItem( const String& rLanguage ) :
+
+     SfxStringItem( SID_SCRIPTORGANIZER, rLanguage ),
+
+     aLanguage( rLanguage )
+
+{
+}
+
+//------------------------------------------------------------------------
+
+SfxScriptOrganizerItem::SfxScriptOrganizerItem( const SfxScriptOrganizerItem& rItem ) :
+
+    SfxStringItem( rItem ),
+
+    aLanguage( rItem.aLanguage )
+
+{
+}
+
+//------------------------------------------------------------------------
+
+SfxScriptOrganizerItem::~SfxScriptOrganizerItem()
+{
+}
+
+//------------------------------------------------------------------------
+
+SfxPoolItem* SfxScriptOrganizerItem::Clone( SfxItemPool * ) const
+{
+    return new SfxScriptOrganizerItem( *this );
+}
+
+//------------------------------------------------------------------------
+
+int SfxScriptOrganizerItem::operator==( const SfxPoolItem& rItem) const
+{
+     return rItem.Type() == Type() &&
+         SfxStringItem::operator==(rItem) &&
+         aLanguage == ((const SfxScriptOrganizerItem &)rItem).aLanguage;
+}
+
+
+sal_Bool SfxScriptOrganizerItem::QueryValue( com::sun::star::uno::Any& rVal, BYTE nMemberId ) const
+{
+    String aValue;
+    BOOL bIsString = FALSE;
+    sal_Bool bValue = sal_False;
+    nMemberId &= ~CONVERT_TWIPS;
+    switch ( nMemberId )
+    {
+        case MID_SCRIPT_ORGANIZER_LANGUAGE:
+            bIsString = TRUE;
+            aValue = aLanguage;
+            break;
+        default:
+            DBG_ERROR("Wrong MemberId!");
+               return sal_False;
+     }
+
+    if ( bIsString )
+        rVal <<= ::rtl::OUString( aValue );
+    else
+        rVal <<= bValue;
+    return sal_True;
+}
+
+sal_Bool SfxScriptOrganizerItem::PutValue( const com::sun::star::uno::Any& rVal, BYTE nMemberId )
+{
+    ::rtl::OUString aValue;
+    sal_Bool bRet = sal_False;
+    nMemberId &= ~CONVERT_TWIPS;
+    switch ( nMemberId )
+    {
+        case MID_SCRIPT_ORGANIZER_LANGUAGE:
+            bRet = (rVal >>= aValue);
+            if ( bRet )
+                aLanguage = aValue;
+            break;
+        default:
+            DBG_ERROR("Wrong MemberId!");
+            return sal_False;
+    }
+
+    return bRet;
+}
+
