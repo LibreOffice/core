08a062add98fe235129626325879624908fcac09
Rüdiger Timm <rt@openoffice.org>
2004-05-19 07:33:31 +0000

    INTEGRATION: CWS scriptingf4 (1.3.102); FILE MERGED 2004/03/24 13:34:21 dfoster 1.3.102.1: #i26923# Implmentation of the Script Organizer spec.

--- sfx2/source/control/makefile.mk
+++ sfx2/source/control/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:28:36 $
+#   last change: $Author: rt $ $Date: 2004-05-19 08:33:31 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -91,6 +91,7 @@ SLOFILES = \
     $(SLO)$/bindings.obj \
     $(SLO)$/minfitem.obj \
     $(SLO)$/srchitem.obj \
+    $(SLO)$/sorgitm.obj \
         $(SLO)$/macro.obj \
     $(SLO)$/macrconf.obj
 
