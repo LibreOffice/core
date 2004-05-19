1055e7ed960c1da7e7ff066578728b0b5a116918
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:20:19 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:17:01 rpiterman 1.1.2.3: documentation and small implementation-fixes 2004/02/13 15:45:22 rpiterman 1.1.2.2: added GNU license 2004/02/11 17:09:09 rpiterman 1.1.2.1: The interface definision of an exporter.

---
+++ wizards/com/sun/star/wizards/web/export/Exporter.java
@@ -0,0 +1,87 @@
+/*************************************************************************
+ *
+ *  $RCSfile: Exporter.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $  $Date: 2004-05-19 13:20:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ */
+package com.sun.star.wizards.web.export;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.wizards.ui.event.Task;
+import com.sun.star.wizards.web.data.*;
+
+
+/**
+ *
+ * @author  rpiterman
+ */
+public interface Exporter {
+    /**
+     * This method exports a document with a specified filter.<br/>
+     * The method is responsible for exporting the given source document
+     * to the given target directory.
+     * @param source is a CGDocument object.
+     * @param target contains the URL of a directory to which the
+     * file should be exported to.
+     * @param xmsf this is a basic multiServiceFactory.
+     * @param task - a task monitoring object. This should advance
+     * (call task.advacne(true) ) 3 times, while exporting.
+     */
+    public boolean export(CGDocument source, String targetDirectory ,XMultiServiceFactory xmsf, Task task);
+
+    public void init(CGExporter exporter);
+}
+
