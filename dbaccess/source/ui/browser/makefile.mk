b6b8071f3e77b9c6df4044d79db191f3473bd734
Kurt Zenker <kz@openoffice.org>
2004-05-19 03:35:36 +0000

    INTEGRATION: CWS bmpres01 (1.13.74); FILE MERGED 2004/04/27 14:31:15 ka 1.13.74.1: using new resource structure for images

--- dbaccess/source/ui/browser/makefile.mk
+++ dbaccess/source/ui/browser/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:02:45 $
+#   last change: $Author: kz $ $Date: 2004-05-19 04:35:36 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,12 +65,10 @@ PRJINC=$(PRJ)$/source
 PRJNAME=dbaccess
 TARGET=browser
 
-BMP_IN=$(PRJ)$/res
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-IMGLST_SRS=$(SRS)$/$(TARGET).srs
+
 # --- Files --------------------------------------------------------
 
 EXCEPTIONSFILES=\
