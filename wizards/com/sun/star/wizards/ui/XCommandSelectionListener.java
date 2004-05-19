508ccee72d4bb6a688121ed5a5af7047e2298d26
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:07:07 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/02/02 11:29:51 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2004/01/22 19:40:58 bc 1.1.2.1: #111603# interface for dialog component added

---
+++ wizards/com/sun/star/wizards/ui/XCommandSelectionListener.java
@@ -0,0 +1,15 @@
+/*
+ * XDSFieldSelection.java
+ *
+ * Created on July 23, 2003, 5:05 PM
+ */
+
+package com.sun.star.wizards.ui;
+
+/**
+ *
+ * @author  bc93774
+ */
+public interface XCommandSelectionListener {
+    public void CommandSelected(String CommandName);
+}
