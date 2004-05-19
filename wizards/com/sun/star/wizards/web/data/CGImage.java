0eec600fe8ec20273bb600468e0b2b1141cd6b20
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:17:45 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:16:59 rpiterman 1.1.2.3: documentation and small implementation-fixes 2004/02/11 17:07:21 rpiterman 1.1.2.2: durther developement 2003/10/21 13:27:59 rpiterman 1.1.2.1: webwizard configuration-access objects

---
+++ wizards/com/sun/star/wizards/web/data/CGImage.java
@@ -0,0 +1,19 @@
+/*
+ * IconSet.java
+ *
+ * Created on 12. September 2003, 17:16
+ */
+
+package com.sun.star.wizards.web.data;
+
+import com.sun.star.wizards.common.ConfigGroup;
+
+/**
+ *
+ * @author  rpiterman
+ */
+
+
+public class CGImage       extends ConfigGroup {
+  public String cp_Href;
+}
