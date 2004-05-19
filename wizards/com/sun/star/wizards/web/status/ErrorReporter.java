8a4895742411908ef198acce7ba8e6a7247d483f
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:21:22 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:17:02 rpiterman 1.1.2.2: documentation and small implementation-fixes 2003/11/05 14:16:06 rpiterman 1.1.2.1: An Interface which defines an object which saves errors (exceptions) during a process.

---
+++ wizards/com/sun/star/wizards/web/status/ErrorReporter.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 14.10.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.sun.star.wizards.web.status;
+
+/**
+ * @author rpiterman
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface ErrorReporter {
+  public void error(Exception ex, Object arg, String st);
+}
