2573fd1408a3c9523f61bda487cd5fb153369ef5
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:45:44 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/02/02 11:28:26 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2003/11/18 15:43:23 bc 1.1.2.1: #111603# Querywizard added

---
+++ wizards/com/sun/star/wizards/query/XCallQueryWizard.java
@@ -0,0 +1,9 @@
+package com.sun.star.wizards.query;
+
+public interface XCallQueryWizard extends com.sun.star.uno.XInterface {
+    // Methods
+    public void CallQueryDialog();
+
+    // static Member
+    public static final com.sun.star.lib.uno.typeinfo.TypeInfo UNOTYPEINFO[] = { new com.sun.star.lib.uno.typeinfo.MethodTypeInfo("CallQueryDialog", 0, 0)};
+}
