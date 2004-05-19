621f06d7bcf484d61b3bd133ec9c19cdf3bdf11e
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:09:06 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:16:53 rpiterman 1.1.2.3: documentation and small implementation-fixes 2004/02/02 11:29:58 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2003/12/02 15:19:18 rpiterman 1.1.2.1: Used by AbstractListener to invoke a method in response to an event

---
+++ wizards/com/sun/star/wizards/ui/event/MethodInvocation.java
@@ -0,0 +1,69 @@
+/*
+ * MethodInvokation.java
+ *
+ * Created on 1. September 2003, 12:58
+ */
+package com.sun.star.wizards.ui.event;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Encapsulate a Method invocation.
+ * In the constructor one defines a method, a target object and an optional
+ * Parameter.
+ * Then one calls "invoke", with or without a parameter. <br/>
+ * Limitations: I do not check anything myself. If the param is not ok, from the
+ * wrong type, or the mothod doesnot exist on the given object.
+ * You can trick this class howmuch you want: it will all throw exceptions
+ * on the java level. i throw no error warnings or my own excceptions...
+ * @author  rpiterman
+ */
+public class MethodInvocation {
+    static final Class[] EMPTY_ARRAY = {
+    };
+    //the method to invoke.
+    Method mMethod;
+    //the object to invoke the method on.
+    Object mObject;
+    //with one Parameter / without
+    boolean mWithParam;
+
+    /** Creates a new instance of MethodInvokation */
+    public MethodInvocation(String methodName, Object obj) throws NoSuchMethodException {
+        this(methodName, obj, null);
+    }
+
+    public MethodInvocation(Method method, Object obj) {
+        this(method, obj, null);
+    }
+
+    public MethodInvocation(String methodName, Object obj, Class paramClass) throws NoSuchMethodException {
+        this(paramClass == null ? obj.getClass().getMethod(methodName, null) : obj.getClass().getMethod(methodName, new Class[] { paramClass }), obj, paramClass);
+    }
+
+    public MethodInvocation(Method method, Object obj, Class paramClass) {
+        mMethod = method;
+        mObject = obj;
+        mWithParam = !(paramClass == null);
+    }
+
+    /**
+     * Returns the result of calling the method on the object, or null, if no result.
+     */
+    public Object invoke(Object param) throws IllegalAccessException, InvocationTargetException {
+        if (mWithParam)
+            return mMethod.invoke(mObject, new Object[] {(Object) param });
+        else
+            return mMethod.invoke(mObject, EMPTY_ARRAY);
+    }
+
+    /**
+     * This method is a convenience method.
+     * It is the same as calling invoke(null);
+     */
+    public Object invoke() throws IllegalAccessException, InvocationTargetException {
+        return invoke(null);
+    }
+
+}
