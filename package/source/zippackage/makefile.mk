fa76bf2a7fb237a310baf331c95b514f7511634a
Kurt Zenker <kz@openoffice.org>
2004-05-19 08:24:19 +0000

    INTEGRATION: CWS bmpres01 (1.20.24); FILE MERGED 2004/04/13 16:04:06 mav 1.20.24.1: #116335# ZipFileAccess

--- package/source/zippackage/makefile.mk
+++ package/source/zippackage/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.21 $
+#   $Revision: 1.22 $
 #
-#   last change: $Author: hr $ $Date: 2004-05-10 17:30:05 $
+#   last change: $Author: kz $ $Date: 2004-05-19 09:24:19 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -85,7 +85,8 @@ SLOFILES= \
         $(SLO)$/ZipPackageFolderEnumeration.obj	\
         $(SLO)$/ZipPackageSink.obj		\
         $(SLO)$/ZipPackageStream.obj	\
-        $(SLO)$/wrapstreamforshare.obj
+        $(SLO)$/wrapstreamforshare.obj	\
+        $(SLO)$/zipfileaccess.obj
 
 #		$(SLO)$/InteractionRequest.obj  \
 #		$(SLO)$/InteractionContinuation.obj
