326760a48a0c439873a55e032ad4ad20b815bc9b
Kurt Zenker <kz@openoffice.org>
2004-05-19 12:54:17 +0000

    INTEGRATION: CWS qwizards1 (1.13.42); FILE MERGED 2004/01/14 09:16:05 oj 1.13.42.1: #i24380# new services for column descriptor control and model

--- dbaccess/source/ui/control/makefile.mk
+++ dbaccess/source/ui/control/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: obo $ $Date: 2003-09-04 08:32:23 $
+#   last change: $Author: kz $ $Date: 2004-05-19 13:54:17 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -86,6 +86,7 @@ EXCEPTIONSFILES=\
         $(SLO)$/tabletree.obj			\
         $(SLO)$/TableGrantCtrl.obj		\
         $(SLO)$/sqledit.obj			\
+        $(SLO)$/ColumnControlWindow.obj	\
         $(SLO)$/FieldDescControl.obj
 
 # ... object files ............................
