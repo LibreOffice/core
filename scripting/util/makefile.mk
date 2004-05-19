3f8e4f2e46b03c38a1b8c07a47ecb6486605eb4d
Rüdiger Timm <rt@openoffice.org>
2004-05-19 07:29:14 +0000

    INTEGRATION: CWS scriptingf4 (1.4.4); FILE MERGED 2004/05/06 12:56:08 npower 1.4.4.1: #i28812# MiscUtils.hxx file adds requirment for UCBHelper libs, so this is added to link

--- scripting/util/makefile.mk
+++ scripting/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2004-01-05 14:42:07 $
+#   last change: $Author: rt $ $Date: 2004-05-19 08:29:14 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,6 +88,7 @@ SHL1STDLIBS+=\
     $(CPPULIB) \
     $(CPPUHELPERLIB) \
     $(COMPHELPERLIB) \
+    $(UCBHELPERLIB) \
     $(SALLIB)
 
 
