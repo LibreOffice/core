c535d5eb81447421caf1971c03cb63718418ee3f
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:42:48 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/04/21 12:19:25 tv 1.1.2.6: added support for SenderState 2004/03/02 14:55:39 tv 1.1.2.5: correct support for double in the configuration 2004/03/02 09:13:13 tv 1.1.2.4: added some configuration modifications 2004/02/13 10:35:01 tv 1.1.2.3: correct TabIndex & added Configuration (Part1) 2004/02/02 11:28:07 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2004/01/28 15:59:15 tv 1.1.2.1: initial revision

---
+++ wizards/com/sun/star/wizards/letter/CGPaperElementLocation.java
@@ -0,0 +1,13 @@
+package com.sun.star.wizards.letter;
+
+import com.sun.star.wizards.common.ConfigGroup;
+
+public class CGPaperElementLocation extends ConfigGroup {
+
+    public boolean cp_Display;
+    public double cp_Width;
+    public double cp_Height;
+    public double cp_X;
+    public double cp_Y;
+
+}
