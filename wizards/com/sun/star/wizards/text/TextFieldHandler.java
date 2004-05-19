e3d1a81b750f65920be4ab52d0a2d44d19fc8334
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:48:50 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 15:25:32 bc 1.1.2.4: #111603# several changes 2004/03/09 12:06:03 rpiterman 1.1.2.3: solved conflict and removed unneeded imports 2004/02/02 11:28:35 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2003/08/21 16:24:24 tv 1.1.2.1: recommit after reorganization

---
+++ wizards/com/sun/star/wizards/text/TextFieldHandler.java
@@ -0,0 +1,234 @@
+/*************************************************************************
+*
+*  $RCSfile: TextFieldHandler.java,v $
+*
+*  $Revision: 1.2 $
+*
+*  last change: $Author: kz $ $Date: 2004-05-19 12:48:50 $
+*
+*  The Contents of this file are made available subject to the terms of
+*  either of the following licenses
+*
+*         - GNU Lesser General Public License Version 2.1
+*         - Sun Industry Standards Source License Version 1.1
+*
+*  Sun Microsystems Inc., October, 2000
+*
+*  GNU Lesser General Public License Version 2.1
+*  =============================================
+*  Copyright 2000 by Sun Microsystems, Inc.
+*  901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License version 2.1, as published by the Free Software Foundation.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*  MA  02111-1307  USA
+*
+*
+*  Sun Industry Standards Source License Version 1.1
+*  =================================================
+*  The contents of this file are subject to the Sun Industry Standards
+*  Source License Version 1.1 (the "License"); You may not use this file
+*  except in compliance with the License. You may obtain a copy of the
+*  License at http://www.openoffice.org/license.html.
+*
+*  Software provided under this License is provided on an "AS IS" basis,
+*  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+*  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+*  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+*  See the License for the specific provisions governing your rights and
+*  obligations concerning the Software.
+*
+*  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+*
+*  Copyright: 2000 by Sun Microsystems, Inc.
+*
+*  All Rights Reserved.
+*
+*  Contributor(s): _______________________________________
+*
+*/
+
+package com.sun.star.wizards.text;
+
+import java.util.Vector;
+
+import com.sun.star.text.XDependentTextField;
+import com.sun.star.text.XTextContent;
+import com.sun.star.text.XTextCursor;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.text.XTextFieldsSupplier;
+import com.sun.star.text.XTextRange;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.container.XEnumeration;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.Any;
+import com.sun.star.uno.AnyConverter;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.util.XRefreshable;
+import com.sun.star.wizards.common.Helper;
+
+public class TextFieldHandler {
+
+    public XTextFieldsSupplier xTextFieldsSupplier;
+    private XMultiServiceFactory xMSFDoc;
+
+
+    /** Creates a new instance of TextFieldHandler */
+    public TextFieldHandler(XMultiServiceFactory xMSF, XTextDocument xTextDocument) {
+        this.xMSFDoc = xMSF;
+        xTextFieldsSupplier = (XTextFieldsSupplier) UnoRuntime.queryInterface(XTextFieldsSupplier.class, xTextDocument);
+    }
+
+
+    public void refreshTextFields(){
+        XRefreshable xUp = (XRefreshable) UnoRuntime.queryInterface(XRefreshable.class, xTextFieldsSupplier.getTextFields());
+        xUp.refresh();
+    }
+
+
+    public String getUserFieldContent(XTextCursor xTextCursor) {
+        try {
+            XTextRange xTextRange = xTextCursor.getEnd();
+            Object oTextField = Helper.getUnoPropertyValue(xTextRange, "TextField");
+            if (com.sun.star.uno.AnyConverter.isVoid(oTextField))
+                return "";
+            else {
+                XDependentTextField xDependent = (XDependentTextField) UnoRuntime.queryInterface(XDependentTextField.class, oTextField);
+                XPropertySet xMaster = xDependent.getTextFieldMaster();
+                String UserFieldContent = (String) xMaster.getPropertyValue("Content");
+                return UserFieldContent;
+            }
+        } catch (com.sun.star.uno.Exception exception) {
+            exception.printStackTrace(System.out);
+            return "";
+        }
+    }
+
+    public void insertUserField(XTextCursor xTextCursor, String FieldName, String FieldTitle) {
+        try {
+            XInterface xField = (XInterface) xMSFDoc.createInstance("com.sun.star.text.TextField.User");
+            XDependentTextField xDepField = (XDependentTextField) UnoRuntime.queryInterface(XDependentTextField.class, xField);
+            XTextContent xFieldContent = (XTextContent) UnoRuntime.queryInterface(XTextContent.class, xField);
+            if (xTextFieldsSupplier.getTextFieldMasters().hasByName("com.sun.star.text.FieldMaster.User." + FieldName)) {
+                Object oMaster = xTextFieldsSupplier.getTextFieldMasters().getByName("com.sun.star.text.FieldMaster.User." + FieldName);
+                XComponent xComponent = (XComponent) UnoRuntime.queryInterface(XComponent.class, oMaster);
+                xComponent.dispose();
+            }
+            XPropertySet xPSet = createUserField(FieldName, FieldTitle);
+            xDepField.attachTextFieldMaster(xPSet);
+            xTextCursor.getText().insertTextContent(xTextCursor, xFieldContent, false);
+        } catch (com.sun.star.uno.Exception exception) {
+            exception.printStackTrace(System.out);
+        }
+    }
+
+
+    public XPropertySet createUserField(String FieldName, String FieldTitle) throws com.sun.star.uno.Exception{
+        Object oMaster = xMSFDoc.createInstance("com.sun.star.text.FieldMaster.User");
+        XPropertySet xPSet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, oMaster);
+        xPSet.setPropertyValue("Name", FieldName);
+        xPSet.setPropertyValue("Content", FieldTitle);
+        return xPSet;
+    }
+
+
+
+    private XDependentTextField[] getTextFieldsByProperty(String _PropertyName, Object _aPropertyValue, String _TypeName) throws Exception{
+    try {
+        XDependentTextField[] xDependentFields;
+        Vector xDependentVector = new Vector();
+        if (xTextFieldsSupplier.getTextFields().hasElements()) {
+            XPropertySet xPSet;
+            XEnumeration xEnum = xTextFieldsSupplier.getTextFields().createEnumeration();
+            while (xEnum.hasMoreElements()) {
+                Object oTextField = xEnum.nextElement();
+                XDependentTextField xDependent = (XDependentTextField) UnoRuntime.queryInterface(XDependentTextField.class, oTextField);
+                xPSet = xDependent.getTextFieldMaster();
+                if (xPSet.getPropertySetInfo().hasPropertyByName(_PropertyName)){
+                    Object oValue = xPSet.getPropertyValue(_PropertyName);
+                    // TODO replace the following comparison via com.sun.star.uno.Any.Type
+                    if(AnyConverter.isString(oValue)){
+                        if (_TypeName.equals("String")){
+                            String sValue = AnyConverter.toString(oValue);
+                            if (sValue.equals(_aPropertyValue))
+                                xDependentVector.addElement(xDependent);
+                        }
+                    }
+                    else if (AnyConverter.isShort(oValue)){
+                        if (_TypeName.equals("Short")){
+                            short iShortParam = ((Short) _aPropertyValue).shortValue();
+                            short ishortValue = AnyConverter.toShort(oValue);
+                            if (ishortValue == iShortParam)
+                                xDependentVector.addElement(xDependent);
+                        }
+                    }
+                }
+            }
+        }
+        if (xDependentVector.size() > 0){
+            xDependentFields = new XDependentTextField[xDependentVector.size()];
+            xDependentVector.toArray(xDependentFields);
+            return xDependentFields;
+        }
+    } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace(System.out);
+    }
+    return null;
+    }
+
+
+
+    public void changeUserFieldContent(String _FieldName, String _FieldContent){
+        try {
+            XDependentTextField[] xDependentTextFields = getTextFieldsByProperty("Name", _FieldName, "String");
+            if (xDependentTextFields != null){
+                for (int i = 0; i < xDependentTextFields.length; i++){
+                    xDependentTextFields[i].getTextFieldMaster().setPropertyValue("Content", _FieldContent);
+                }
+                refreshTextFields();
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+
+    public void removeUserFieldByContent(String _FieldContent) {
+    try {
+        XDependentTextField[] xDependentTextFields = getTextFieldsByProperty("Content", _FieldContent, "String");
+        if (xDependentTextFields != null)
+            for (int i = 0; i < xDependentTextFields.length; i++)
+                xDependentTextFields[i].dispose();
+    } catch (Exception e) {
+        e.printStackTrace(System.out);
+    }}
+
+
+
+    public void changeExtendedUserFieldContent(short UserDataPart, String _FieldContent) {
+        try {
+            XDependentTextField[] xDependentTextFields = getTextFieldsByProperty("UserDataType", new Short(UserDataPart), "Short");
+            if (xDependentTextFields != null){
+                for (int i = 0; i < xDependentTextFields.length; i++){
+                    xDependentTextFields[i].getTextFieldMaster().setPropertyValue("Content", _FieldContent);
+                }
+            }
+            refreshTextFields();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
