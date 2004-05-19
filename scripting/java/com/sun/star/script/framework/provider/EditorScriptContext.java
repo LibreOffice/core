748004a012a688a8e9845d2a76d00f2488a43455
Rüdiger Timm <rt@openoffice.org>
2004-05-19 07:22:50 +0000

    INTEGRATION: CWS scriptingf4 (1.1.2); FILE ADDED 2004/05/06 13:48:25 npower 1.1.2.1: #i28818# New ScriptingContext implementation, will always return the currently selected OO document when getDocument() is called.
    Issue number:
    Submitted by:
    Reviewed by:

---
+++ scripting/java/com/sun/star/script/framework/provider/EditorScriptContext.java
@@ -0,0 +1,142 @@
+/*************************************************************************
+*
+*  $RCSfile: EditorScriptContext.java,v $
+*
+*  $Revision: 1.2 $
+*
+*  last change: $Author: rt $ $Date: 2004-05-19 08:22:50 $
+*
+*  The Contents of this file are made available subject to the terms of
+*  either of the following licenses
+*
+*         - GNU Lesser General Public License Version 2.1
+*         - Sun Industry Standards Source License Version 1.1
+*
+*  Sun Microsystems Inc., October, 2000
+*
+*  GNU Lesser General Public License Version 2.1
+*  =============================================
+*  Copyright 2000 by Sun Microsystems, Inc.
+*  901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License version 2.1, as published by the Free Software Foundation.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*  MA  02111-1307  USA
+*
+*
+*  Sun Industry Standards Source License Version 1.1
+*  =================================================
+*  The contents of this file are subject to the Sun Industry Standards
+*  Source License Version 1.1 (the "License"); You may not use this file
+*  except in compliance with the License. You may obtain a copy of the
+*  License at http://www.openoffice.org/license.html.
+*
+*  Software provided under this License is provided on an "AS IS" basis,
+*  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+*  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+*  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+*  See the License for the specific provisions governing your rights and
+*  obligations concerning the Software.
+*
+*  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+*
+*  Copyright: 2000 by Sun Microsystems, Inc.
+*
+*  All Rights Reserved.
+*
+*  Contributor(s): _______________________________________
+*
+*
+************************************************************************/
+package com.sun.star.script.framework.provider;
+
+import com.sun.star.frame.XModel;
+import com.sun.star.frame.XDesktop;
+import com.sun.star.uno.XComponentContext;
+import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.beans.XPropertyChangeListener;
+import com.sun.star.beans.XVetoableChangeListener;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.AnyConverter;
+import com.sun.star.beans.PropertyAttribute;
+import com.sun.star.lib.uno.helper.PropertySet;
+import com.sun.star.uno.Type;
+
+import java.util.HashMap;
+
+import drafts.com.sun.star.script.provider.XScriptContext;
+
+import com.sun.star.script.framework.log.LogUtils;
+
+
+/**
+ *  Description of the Class
+ *
+ * @author     Noel Power
+ * @created    August 2, 2002
+ */
+public class EditorScriptContext implements XScriptContext
+{
+    private XDesktop m_xDeskTop;
+    private XComponentContext  m_xComponentContext;
+    private XDesktop m_xCtx;
+    public EditorScriptContext( XComponentContext xmComponentContext,
+        XDesktop xDesktop )
+    {
+        this.m_xComponentContext = xmComponentContext;
+        this.m_xDeskTop = xDesktop;
+    }
+
+    //----------------------------------------------------------------------
+    /**
+        Obtain the document reference on which the script can operate
+
+        @returns
+          XModel interface
+    */
+    public XModel getDocument()
+    {
+        XModel xModel = ( XModel ) UnoRuntime.queryInterface( XModel.class,
+             m_xDeskTop.getCurrentComponent() );
+
+        return xModel;
+    }
+
+    /**
+        Obtain the desktop reference on which the script can operate
+
+        @returns
+          XDesktop interface
+    */
+    public XDesktop getDesktop()
+    {
+        return m_xDeskTop;
+    }
+
+    /**
+        Obtain the component context which the script can use to create other uno components
+
+        @returns
+          XComponentContext interface
+    */
+    public XComponentContext getComponentContext()
+    {
+       return m_xComponentContext;
+    }
+
+}
