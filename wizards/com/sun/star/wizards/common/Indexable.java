1714309c992477b63de96a2cbc81549e52d1e643
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:36:38 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:16:50 rpiterman 1.1.2.3: documentation and small implementation-fixes 2004/02/02 11:27:40 tv 1.1.2.2: formatted with autoformatter (indents now use TAB) 2003/10/21 13:33:36 rpiterman 1.1.2.1: For configuration sets, indexable represents an item which has a swequantial access (not only name...)

---
+++ wizards/com/sun/star/wizards/common/Indexable.java
@@ -0,0 +1,17 @@
+/*
+ * Indexable.java
+ *
+ * Created on 16. September 2003, 11:38
+ */
+
+package com.sun.star.wizards.common;
+
+/**
+ *
+ * @author  rpiterman
+ */
+public interface Indexable {
+
+    public int getIndex();
+
+}
