cfb6899fa119cf3803cd81bd75a7ed58b7677503
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:35:29 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/12 16:16:49 rpiterman 1.1.2.4: documentation and small implementation-fixes 2004/02/02 11:27:36 tv 1.1.2.3: formatted with autoformatter (indents now use TAB) 2004/01/14 19:45:58 rpiterman 1.1.2.2: added header 2003/12/19 15:58:57 rpiterman 1.1.2.1: An Interface which defines an object that reads and writes itseld to the OO Configuration. A Successor of ConfigReader.java

---
+++ wizards/com/sun/star/wizards/common/ConfigNode.java
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ConfigNode.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $  $Date: 2004-05-19 12:35:29 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ */
+package com.sun.star.wizards.common;
+
+/**
+ * This Interface specifies a method of an object which is
+ * capable of reading adn writing its data out of the
+ * OO Configuration. <br/>
+ * There are 2 direct implementations: ConfigGroup and ConfigSet.
+ * The root is the first Java Object in the configuration hirarchie.
+ * @author  rpiterman
+ */
+public interface ConfigNode {
+
+    /**
+     * reads the object data out of the configuration.
+     * @param  configurationView is a ::com::sun::star::configuration::HierarchyElement
+     * which represents the node corresponding to the Object.
+     * @param param a free parameter. Since the intension of this interface is
+     * to be used in a tree like way, reading objects and subobjects and so on,
+     * it might be practical to be able to pass an extra parameter, for a free use.
+     */
+    public void readConfiguration(Object configurationView, Object param);
+    public void writeConfiguration(Object configurationView, Object param);
+    public void setRoot(Object root);
+
+}
