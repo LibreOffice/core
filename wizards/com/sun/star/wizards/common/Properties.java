1ff5fa4ca99e76fc3756fc44c631dd360c43a5ab
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:37:24 +0000

    INTEGRATION: CWS qwizards1 (1.1.2); FILE ADDED 2004/03/16 13:38:41 bc 1.1.2.9: #111603# final changes 2004/02/03 15:06:11 rpiterman 1.1.2.8: Merge Conflicts again 2004/02/02 11:27:48 tv 1.1.2.7: formatted with autoformatter (indents now use TAB) 2004/01/22 19:45:45 bc 1.1.2.6: #111603# several changes for Querywizard 2003/12/17 13:32:48 rpiterman 1.1.2.5: Change argument order in static createProperty method, so that name comes first, value second... :-) 2003/11/04 16:19:25 rpiterman 1.1.2.4: Bug fixed 2003/10/01 15:42:41 rpiterman 1.1.2.3: bug fix 2003/10/01 15:40:37 rpiterman 1.1.2.2: *** empty log message *** 2003/10/01 15:37:52 rpiterman 1.1.2.1: Simplifies handling arrays of PropertyValue.

---
+++ wizards/com/sun/star/wizards/common/Properties.java
@@ -0,0 +1,84 @@
+/*
+ * Properties.java
+ *
+ * Created on 1. Oktober 2003, 17:16
+ */
+
+package com.sun.star.wizards.common;
+
+import com.sun.star.beans.PropertyValue;
+import java.util.*;
+
+/**
+ * Simplifies handling Arrays of PropertyValue.
+ * To make a use of this class, instantiate it, and call
+ * the put(propName,propValue) method.
+ * caution: propName should always be a String.
+ * When finished, call the getProperties() method to get an array of the set properties.
+ * @author  rp
+ */
+public class Properties extends Hashtable {
+
+    public static Object getPropertyValue(PropertyValue[] props, String propName) {
+        for (int i = 0; i<props.length; i++)
+              if (propName.equals(props[i].Name))
+                  return props[i].Value;
+        throw new IllegalArgumentException("Property '" + propName + "' not found.");
+    }
+
+
+    public static boolean hasPropertyValue(PropertyValue[] props, String propName){
+        for (int i = 0; i<props.length; i++){
+            if (propName.equals(props[i].Name))
+                return true;
+        }
+        return false;
+    }
+
+
+
+    public PropertyValue[] getProperties() {
+        return getProperties(this);
+    }
+
+    public static PropertyValue[] getProperties(Map map) {
+        PropertyValue[] pv = new PropertyValue[map.size()];
+
+        Iterator it = map.keySet().iterator();
+        for (int i = 0; i < pv.length; i++)
+            pv[i] = createProperty((String) it.next(), map);
+
+        return pv;
+    }
+
+    public static PropertyValue createProperty(String name, Map map) {
+        return createProperty(name, map.get(name));
+    }
+
+    public static PropertyValue createProperty(String name, Object value) {
+        PropertyValue pv = new PropertyValue();
+        pv.Name = name;
+        pv.Value = value;
+        return pv;
+    }
+
+    public static PropertyValue createProperty(String name, Object value, int handle) {
+        PropertyValue pv = createProperty(name, value);
+        pv.Handle = handle;
+        return pv;
+    }
+
+
+    public static PropertyValue[] convertToPropertyValueArray(Object[] _oObjectArray){
+        PropertyValue[] retproperties = null;
+        if (_oObjectArray != null){
+            if (_oObjectArray.length > 0){
+                 retproperties = new PropertyValue[_oObjectArray.length];
+                for (int i = 0; i < _oObjectArray.length; i++){
+                    retproperties[i] = (PropertyValue) _oObjectArray[i];
+                }
+            }
+        }
+        return retproperties;
+    }
+}
