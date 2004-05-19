6fb1797fa4c0d858d117481ff7f314ec1e71a25b
Kurt Zenker <kz@openoffice.org>
2004-05-19 11:48:15 +0000

    INTEGRATION: CWS qwizards1 (1.4.58); FILE MERGED 2004/02/02 11:28:33 tv 1.4.58.3: formatted with autoformatter (indents now use TAB) 2003/08/21 16:24:21 tv 1.4.58.2: recommit after reorganization 2003/08/21 15:28:58 tv 1.4.58.1: removed due to reorganization

--- wizards/com/sun/star/wizards/report/XCallReportWizard.java
+++ wizards/com/sun/star/wizards/report/XCallReportWizard.java
@@ -1,13 +1,9 @@
 package com.sun.star.wizards.report;
 
-public interface XCallReportWizard extends com.sun.star.uno.XInterface
-{
+public interface XCallReportWizard extends com.sun.star.uno.XInterface {
     // Methods
-    public void CallReportDialog(  );
+    public void CallReportDialog();
 
     // static Member
-    public static final com.sun.star.lib.uno.typeinfo.TypeInfo UNOTYPEINFO[] = {
-        new com.sun.star.lib.uno.typeinfo.MethodTypeInfo( "CallReportDialog", 0, 0 )
-     };
+    public static final com.sun.star.lib.uno.typeinfo.TypeInfo UNOTYPEINFO[] = { new com.sun.star.lib.uno.typeinfo.MethodTypeInfo("CallReportDialog", 0, 0)};
 }
-
