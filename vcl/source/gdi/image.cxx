587fdfd760bd3a6f4cea12c9bf57984c8788930e
Rüdiger Timm <rt@openoffice.org>
2004-05-21 15:23:20 +0000

    INTEGRATION: CWS bmpres01 (1.9.34); FILE MERGED 2004/04/27 13:28:04 ka 1.9.34.7: using new image resource structure 2004/04/23 14:39:41 ka 1.9.34.6: added support for ZIPped image directory 2004/04/21 14:25:36 ka 1.9.34.5: added support for external compressed image tree 2004/04/15 11:48:58 ka 1.9.34.4: added support for MONOCHROME_BLACK AND ..._WHITE images/bitmaps 2004/02/18 09:58:55 ka 1.9.34.3: #i22149#: added support for alpha masks 2004/02/12 11:39:37 ka 1.9.34.2: #i22149# support for alpha masks 2004/02/11 16:15:08 ka 1.9.34.1: #i22149#: added alpha mask support for Images/ImageLists

--- vcl/source/gdi/image.cxx
+++ vcl/source/gdi/image.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: image.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-21 14:38:36 $
+ *  last change: $Author: rt $ $Date: 2004-05-21 16:23:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
