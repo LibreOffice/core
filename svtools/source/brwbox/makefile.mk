a6f5c122341f351680e05640c23a43796d921edb
Rüdiger Timm <rt@openoffice.org>
2004-05-21 10:39:21 +0000

    INTEGRATION: CWS bmpres01 (1.11.124); FILE MERGED 2004/04/27 14:33:15 ka 1.11.124.1: using new resource structure for images

--- svtools/source/brwbox/makefile.mk
+++ svtools/source/brwbox/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:40:57 $
+#   last change: $Author: rt $ $Date: 2004-05-21 11:39:21 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,11 +70,6 @@ TARGET=browse
 .INCLUDE :  settings.mk
 
 # --- Files --------------------------------------------------------
-
-BMP=$(BIN)$/bmp
-
-IMGLST_SRS=$(SRS)$/browse.srs
-BMP_IN=$(PRJ)$/res
 
 SRS1NAME=$(TARGET)
 SRC1FILES=		editbrowsebox.src
