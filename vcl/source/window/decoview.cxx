c5d7f9c5b71e762dee4fed0e87650f8b036c9b84
Rüdiger Timm <rt@openoffice.org>
2004-05-21 15:28:29 +0000

    INTEGRATION: CWS bmpres01 (1.7.36); FILE MERGED 2004/02/13 13:06:15 ka 1.7.36.2: #i22149#: support for alpha masks (cleanup) 2004/02/11 16:15:09 ka 1.7.36.1: #i22149#: added alpha mask support for Images/ImageLists

--- vcl/source/window/decoview.cxx
+++ vcl/source/window/decoview.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: decoview.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 15:49:48 $
+ *  last change: $Author: rt $ $Date: 2004-05-21 16:28:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
