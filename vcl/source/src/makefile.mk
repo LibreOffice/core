bfbee919b95571b5c6216ef92c7fa1a180d364a2
Rüdiger Timm <rt@openoffice.org>
2004-05-21 15:26:00 +0000

    INTEGRATION: CWS bmpres01 (1.4.100); FILE MERGED 2004/04/27 13:28:04 ka 1.4.100.1: using new image resource structure

--- vcl/source/src/makefile.mk
+++ vcl/source/src/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2004-05-21 14:43:09 $
+#   last change: $Author: rt $ $Date: 2004-05-21 16:26:00 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
