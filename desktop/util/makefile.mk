5da4deaff917bd1acc25b03ed8ff61cb0de7713c
Rüdiger Timm <rt@openoffice.org>
2004-05-21 15:18:46 +0000

    INTEGRATION: CWS bmpres01 (1.39.32); FILE MERGED 2004/05/04 14:20:55 ka 1.39.32.1: new resource structure for images

--- desktop/util/makefile.mk
+++ desktop/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.41 $
+#   $Revision: 1.42 $
 #
-#   last change: $Author: rt $ $Date: 2004-05-21 14:26:37 $
+#   last change: $Author: rt $ $Date: 2004-05-21 16:18:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
