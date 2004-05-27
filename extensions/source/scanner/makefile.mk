7a9aad2814feaba6f441059c060d0ff603d81caf
Rüdiger Timm <rt@openoffice.org>
2004-05-27 10:19:48 +0000

    #i10000# correct path delimiter

--- extensions/source/scanner/makefile.mk
+++ extensions/source/scanner/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2004-05-21 13:56:45 $
+#   last change: $Author: rt $ $Date: 2004-05-27 11:19:48 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -118,7 +118,7 @@ SRC1FILES=\
     grid.src
 
 RESLIB1NAME=san
-RESLIB1IMAGES=$(PRJ)$/source/scanner
+RESLIB1IMAGES=$(PRJ)$/source$/scanner
 RESLIB1SRSFILES= $(SRS)$/scn.srs
 RESLIB1DEPN= sanedlg.src sanedlg.hrc grid.src grid.hrc
 .ENDIF
