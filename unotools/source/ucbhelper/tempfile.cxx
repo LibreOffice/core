bb3f36a8d3c5c4be999d0af7f8319d8ab6e1fe86
Oliver Bolte <obo@openoffice.org>
2004-05-28 14:18:45 +0000

    INTEGRATION: CWS tdocfixes1 (1.15.8); FILE MERGED 2004/05/14 15:19:18 kso 1.15.8.1: #i29204# - Avoid NameClashException by using special UcbStreamHelper::CreateStream

--- unotools/source/ucbhelper/tempfile.cxx
+++ unotools/source/ucbhelper/tempfile.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tempfile.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2004-04-13 12:16:53 $
+ *  last change: $Author: obo $ $Date: 2004-05-28 15:18:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -388,7 +388,7 @@ SvStream* TempFile::GetStream( StreamMode eMode )
     if ( !pImp->pStream )
     {
         if ( GetURL().Len() )
-            pImp->pStream = UcbStreamHelper::CreateStream( pImp->aURL, eMode );
+            pImp->pStream = UcbStreamHelper::CreateStream( pImp->aURL, eMode, sal_True /* bFileExists */ );
         else
             pImp->pStream = new SvFileStream( pImp->aName, eMode );
     }
