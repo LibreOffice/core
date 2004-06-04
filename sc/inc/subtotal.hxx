2c120345b01c13b46b9f94cd48f2dc0c15193947
Oliver Bolte <obo@openoffice.org>
2004-06-04 09:16:21 +0000

    INTEGRATION: CWS rowlimit (1.1.1.1.346); FILE MERGED 2004/03/15 17:41:56 er 1.1.1.1.346.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2004/03/11 12:05:48 er 1.1.1.1.346.1: #i1967# type correctness

--- sc/inc/subtotal.hxx
+++ sc/inc/subtotal.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: subtotal.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-03-08 11:42:11 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 10:16:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -82,7 +82,7 @@ private:
     USHORT  nProgress;
 
 public:
-    USHORT  nIndex;         // Test
+    SCSIZE  nIndex;         // Test
 
 public:
             SubTotal();
