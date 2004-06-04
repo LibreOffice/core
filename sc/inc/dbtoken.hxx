328d2ae534850ef0e8eea35b74021493242a4278
Oliver Bolte <obo@openoffice.org>
2004-06-04 09:06:28 +0000

    INTEGRATION: CWS rowlimit (1.1.1.1.346); FILE MERGED 2004/01/12 17:14:55 er 1.1.1.1.346.1: #i1967# SCCOL,SCROW,SCTAB replace USHORT; SCsCOL,SCsROW,SCsTAB replace short

--- sc/inc/dbtoken.hxx
+++ sc/inc/dbtoken.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbtoken.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:48 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 10:06:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -75,11 +75,11 @@
 
 BOOL DbgToken( char );
 ScTokenArray* DbgMakeTokenArray
-    ( ScDocument* pDoc, USHORT nTab, const String& r, USHORT& rErr );
+    ( ScDocument* pDoc, SCTAB nTab, const String& r, USHORT& rErr );
 void DbgDumpTokenArray( const BYTE* pArr, USHORT nLen, const char* pMsg );
 void DbgDelTokenArray( ScTokenArray* );
 StackVar DbgInterpret( ScDocument* pDok, const ScTokenArray* pToken,
-                       USHORT nCOL, USHORT nROW, USHORT nTAB,
+                       SCCOL nCOL, SCROW nROW, SCTAB nTAB,
                                   StackVar eStackVar,
                                   char* &rStringErgPtr,
                                   double& rDoubleErg,
