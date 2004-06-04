8e505218815997bf0c122bda707de91bc4a4ee19
Oliver Bolte <obo@openoffice.org>
2004-06-04 09:17:30 +0000

    INTEGRATION: CWS rowlimit (1.2.18); FILE MERGED 2004/03/18 10:22:17 er 1.2.18.1: #i1967# type correctness

--- sc/inc/tokenarray.hxx
+++ sc/inc/tokenarray.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tokenarray.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $  $Date: 2004-03-08 11:42:37 $
+ *  last change: $Author: obo $  $Date: 2004-06-04 10:17:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -226,7 +226,7 @@ public:
                             /** Determines the extent of direct adjacent
                                 references. Only use with real functions, e.g.
                                 GetOuterFuncOpCode() == ocSum ! */
-    BOOL                    GetAdjacentExtendOfOuterFuncRefs( USHORT& nExtend,
+    BOOL                    GetAdjacentExtendOfOuterFuncRefs( SCCOLROW& nExtend,
                                 const ScAddress& rPos, ScDirection );
 
                             /** Operators +,-,*,/,^,&,=,<>,<,>,<=,>=
