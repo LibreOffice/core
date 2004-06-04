8048c4db1110ec1c9799c8a93cfd30942a2f29a1
Oliver Bolte <obo@openoffice.org>
2004-06-04 02:03:00 +0000

    INTEGRATION: CWS sb18 (1.2.52); FILE MERGED 2004/05/12 09:11:24 sb 1.2.52.1: #i21150# Adapted to instantiated polymorphic struct types.

--- bridges/test/com/sun/star/lib/uno/bridges/java_remote/Bug111153_Test.java
+++ bridges/test/com/sun/star/lib/uno/bridges/java_remote/Bug111153_Test.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Bug111153_Test.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-08-13 17:20:56 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 03:03:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,7 @@
  *
  ************************************************************************/
 
-package com.sun.star.lib.uno.bridges.java_remote;
+package com.sun.star.lib.uno.bridges.javaremote;
 
 import com.sun.star.bridge.XBridge;
 import com.sun.star.bridge.XInstanceProvider;
