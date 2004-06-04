cc78393166504468d0d5bc391171c4ab55048646
Oliver Bolte <obo@openoffice.org>
2004-06-04 13:02:42 +0000

    INTEGRATION: CWS fieldoptions (1.8.22); FILE MERGED 2004/05/12 11:41:06 dr 1.8.22.1: #i23447# reimpl of pivot table import completed

--- sc/source/filter/excel/xiroot.cxx
+++ sc/source/filter/excel/xiroot.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xiroot.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2004-03-02 09:38:54 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 14:02:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -86,6 +86,9 @@
 #endif
 #ifndef SC_XIESCHER_HXX
 #include "xiescher.hxx"
+#endif
+#ifndef SC_XIPIVOT_HXX
+#include "xipivot.hxx"
 #endif
 
 #include "root.hxx"
@@ -211,6 +214,13 @@ XclImpWebQueryBuffer& XclImpRoot::GetWebQueryBuffer() const
     if( !mrImpData.mpWebQBuffer.get() )
         mrImpData.mpWebQBuffer.reset( new XclImpWebQueryBuffer( GetRoot() ) );
     return *mrImpData.mpWebQBuffer;
+}
+
+XclImpPivotTableManager& XclImpRoot::GetPivotTableManager() const
+{
+    if( !mrImpData.mpPTManager.get() )
+        mrImpData.mpPTManager.reset( new XclImpPivotTableManager( GetRoot() ) );
+    return *mrImpData.mpPTManager;
 }
 
 ExcelToSc& XclImpRoot::GetFmlaConverter() const
