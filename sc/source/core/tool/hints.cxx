b6003d1ec3070f989c04b17f0585c9b599227f9a
Oliver Bolte <obo@openoffice.org>
2004-06-04 09:36:10 +0000

    INTEGRATION: CWS rowlimit (1.1.1.1.346); FILE MERGED 2004/04/29 16:23:27 er 1.1.1.1.346.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2004/01/12 17:14:03 er 1.1.1.1.346.1: #i1967# SCCOL,SCROW,SCTAB replace USHORT; SCsCOL,SCsROW,SCsTAB replace short

--- sc/source/core/tool/hints.cxx
+++ sc/source/core/tool/hints.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hints.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-03-19 16:08:12 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 10:36:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -103,7 +103,7 @@ ScPaintHint::~ScPaintHint()
 // -----------------------------------------------------------------------
 
 ScUpdateRefHint::ScUpdateRefHint( UpdateRefMode eMode, const ScRange& rR,
-                                    short nX, short nY, short nZ ) :
+                                    SCsCOL nX, SCsROW nY, SCsTAB nZ ) :
     eUpdateRefMode( eMode ),
     aRange( rR ),
     nDx( nX ),
