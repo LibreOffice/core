aa29d810bcfb986c5f57965ebf9f6fb645a7d8a8
Oliver Bolte <obo@openoffice.org>
2004-06-04 07:45:32 +0000

    INTEGRATION: CWS fmebugs02 (1.33.26); FILE MERGED 2004/05/24 12:20:00 fme 1.33.26.2: #i29438# Incomplete fix. Undo 2004/05/24 12:03:39 fme 1.33.26.1: #i29438# Condition ContaintCntnt changed to ContainsAny on check for zombie section due to table frames without content

--- sw/source/core/layout/sectfrm.cxx
+++ sw/source/core/layout/sectfrm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sectfrm.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-03 14:23:44 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 08:45:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
