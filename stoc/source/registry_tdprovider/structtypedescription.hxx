14425cca49ac02e0842b57a182476688f13cf400
Oliver Bolte <obo@openoffice.org>
2004-06-04 01:32:55 +0000

    INTEGRATION: CWS sb18 (1.1.2); FILE ADDED 2004/04/14 08:00:00 sb 1.1.2.1: #i21150# Added support for polymorphic struct types.

---
+++ stoc/source/registry_tdprovider/structtypedescription.hxx
@@ -0,0 +1,133 @@
+/*************************************************************************
+ *
+ *  $RCSfile: structtypedescription.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-06-04 02:32:55 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_stoc_source_registry_tdprovider_structtypedescription_hxx
+#define INCLUDED_stoc_source_registry_tdprovider_structtypedescription_hxx
+
+#include "com/sun/star/reflection/XStructTypeDescription.hpp"
+#include "com/sun/star/uno/Reference.hxx"
+#include "com/sun/star/uno/Sequence.hxx"
+#include "cppuhelper/implbase1.hxx"
+#include "sal/types.h"
+
+namespace com { namespace sun { namespace star {
+    namespace container { class XHierarchicalNameAccess; }
+    namespace reflection { class XCompoundTypeDescription; }
+} } }
+namespace rtl { class OUString; }
+
+namespace stoc { namespace registry_tdprovider {
+
+class StructTypeDescription:
+    public cppu::WeakImplHelper1<
+        com::sun::star::reflection::XStructTypeDescription >
+{
+public:
+    StructTypeDescription(
+        com::sun::star::uno::Reference<
+            com::sun::star::container::XHierarchicalNameAccess > const &
+            manager,
+        rtl::OUString const & name, rtl::OUString const & baseTypeName,
+        com::sun::star::uno::Sequence< sal_Int8 > const & data);
+
+    virtual ~StructTypeDescription();
+
+    virtual com::sun::star::uno::TypeClass SAL_CALL getTypeClass()
+        throw (com::sun::star::uno::RuntimeException);
+
+    virtual rtl::OUString SAL_CALL getName()
+        throw (com::sun::star::uno::RuntimeException);
+
+    virtual
+    com::sun::star::uno::Reference<
+        com::sun::star::reflection::XTypeDescription >
+    SAL_CALL getBaseType() throw (com::sun::star::uno::RuntimeException);
+
+    virtual
+    com::sun::star::uno::Sequence<
+        com::sun::star::uno::Reference<
+            com::sun::star::reflection::XTypeDescription > >
+    SAL_CALL getMemberTypes() throw (com::sun::star::uno::RuntimeException);
+
+    virtual com::sun::star::uno::Sequence< rtl::OUString > SAL_CALL
+    getMemberNames() throw (com::sun::star::uno::RuntimeException);
+
+    virtual com::sun::star::uno::Sequence< rtl::OUString > SAL_CALL
+    getTypeParameters() throw (com::sun::star::uno::RuntimeException);
+
+    virtual
+    com::sun::star::uno::Sequence<
+        com::sun::star::uno::Reference<
+            com::sun::star::reflection::XTypeDescription > >
+    SAL_CALL getTypeArguments() throw (com::sun::star::uno::RuntimeException);
+
+private:
+    StructTypeDescription(StructTypeDescription &); // not implemented
+    void operator =(StructTypeDescription); // not implemented
+
+    com::sun::star::uno::Sequence< sal_Int8 > m_data;
+    com::sun::star::uno::Reference<
+        com::sun::star::reflection::XCompoundTypeDescription > m_base;
+};
+
+} }
+
+#endif
