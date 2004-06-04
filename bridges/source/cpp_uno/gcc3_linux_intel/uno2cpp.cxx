3fa7c899d87183c505ad349fff6b5adcf07c6156
Oliver Bolte <obo@openoffice.org>
2004-06-04 02:00:11 +0000

    INTEGRATION: CWS sb18 (1.5.20); FILE MERGED 2004/05/12 09:35:22 sb 1.5.20.1: #i21150# Missing includes.

--- bridges/source/cpp_uno/gcc3_linux_intel/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_linux_intel/uno2cpp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: uno2cpp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 12:38:54 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 03:00:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,6 +62,7 @@
 #include <malloc.h>
 
 #include <com/sun/star/uno/genfunc.hxx>
+#include "com/sun/star/uno/RuntimeException.hpp"
 #include <uno/data.h>
 
 #include "bridges/cpp_uno/shared/bridge.hxx"
