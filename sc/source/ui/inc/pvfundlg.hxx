eb0fda26b7cd2fe888a7ef212ab6decb53e660f0
Oliver Bolte <obo@openoffice.org>
2004-06-04 13:12:07 +0000

    INTEGRATION: CWS fieldoptions (1.3.332); FILE MERGED 2004/05/27 08:29:24 dr 1.3.332.7: #100000# dialogdiet changes 2004/05/07 11:49:30 dr 1.3.332.6: #i23447# DP subtotal options dialog: used hierarchy updates members list 2004/05/07 09:49:22 dr 1.3.332.5: #i23447# hidden members and used hierarchy 2004/05/06 11:14:25 dr 1.3.332.4: #i23447# auto show options in subtotal dialog 2004/05/05 16:02:22 dr 1.3.332.3: #i23447# layout options in subtotal dialog 2004/05/05 14:11:13 dr 1.3.332.2: #i23447# sort options in subtotal dialog 2004/05/04 16:32:03 dr 1.3.332.1: #i23447# DP data field options dialog

--- sc/source/ui/inc/pvfundlg.hxx
+++ sc/source/ui/inc/pvfundlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pvfundlg.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: dr $ $Date: 2002-03-01 11:33:44 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 14:12:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,69 +62,200 @@
 #ifndef SC_PVFUNDLG_HXX
 #define SC_PVFUNDLG_HXX
 
-#ifndef _SV_HXX
+#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDREFERENCE_HPP_
+#include <com/sun/star/sheet/DataPilotFieldReference.hpp>
 #endif
-//#ifndef _SVTOOL_HXX
-//#include <svtool.hxx>
-//#endif
-//#include <svtfixed.hxx>
+#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDSORTINFO_HPP_
+#include <com/sun/star/sheet/DataPilotFieldSortInfo.hpp>
+#endif
 
-#ifndef _STDCTRL_HXX //autogen
-#include <svtools/stdctrl.hxx>
-#endif
-#ifndef _FIXED_HXX //autogen
+#ifndef _FIXED_HXX
 #include <vcl/fixed.hxx>
 #endif
-#ifndef _LSTBOX_HXX //autogen
+#ifndef _LSTBOX_HXX
 #include <vcl/lstbox.hxx>
 #endif
-#ifndef _DIALOG_HXX //autogen
+#ifndef _DIALOG_HXX
 #include <vcl/dialog.hxx>
 #endif
-#ifndef _BUTTON_HXX //autogen
+#ifndef _BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
+#ifndef _MOREBTN_HXX
+#include <vcl/morebtn.hxx>
+#endif
+#ifndef _SV_FIELD_HXX
+#include <vcl/field.hxx>
+#endif
+#ifndef _STDCTRL_HXX
+#include <svtools/stdctrl.hxx>
+#endif
+#ifndef _SVX_CHECKLBX_HXX
+#include <svx/checklbx.hxx>
+#endif
+#ifndef SFX_ITEMCONNECT_HXX
+#include <sfx2/itemconnect.hxx>
+#endif
 
-//===================================================================
+#ifndef SC_PIVOT_HXX
+#include "pivot.hxx"
+#endif
+
+// ============================================================================
+
+typedef sfx::ListBoxWrapper< sal_Int32 > ScDPListBoxWrapper;
+
+class ScDPObject;
+
+// ============================================================================
+
+class ScDPFunctionListBox : public MultiListBox
+{
+public:
+    explicit            ScDPFunctionListBox( Window* pParent, const ResId& rResId );
+    explicit            ScDPFunctionListBox( Window* pParent, WinBits nStyle = 0 );
+
+    void                SetSelection( USHORT nFuncMask );
+    USHORT              GetSelection() const;
+
+private:
+    void                FillFunctionNames();
+};
+
+// ============================================================================
 
 class ScDPFunctionDlg : public ModalDialog
 {
 public:
-        ScDPFunctionDlg(    Window*         pParent,
-                            BOOL            bSubTotalFunc,
-                            const String&   rName,
-                            USHORT          nFunctions,
-                            BOOL            bIsShowAll );
-        ~ScDPFunctionDlg();
+    explicit            ScDPFunctionDlg( Window* pParent, const ScDPLabelDataVec& rLabelVec,
+                            const ScDPLabelData& rLabelData, const ScDPFuncData& rFuncData );
 
-    USHORT  GetFuncMask() const { return nFuncMask; }
-    BOOL    GetShowAll() const;
+    USHORT              GetFuncMask() const;
+    ::com::sun::star::sheet::DataPilotFieldReference GetFieldRef() const;
 
 private:
-    FixedLine           aFlFunc;
-    RadioButton         aBtnNone;
-    RadioButton         aBtnAuto;
-    RadioButton         aBtnUser;
-    MultiListBox        aLbFunc;
-    CheckBox            aCbShowAll;
-    OKButton            aBtnOk;
-    CancelButton        aBtnCancel;
-    HelpButton          aBtnHelp;
-    FixedText           aFtNameLabel;
-    FixedInfo           aFtName;
+    void                Init( const ScDPLabelData& rLabelData, const ScDPFuncData& rFuncData );
 
-    USHORT              nFuncMask;
+    DECL_LINK( SelectHdl, ListBox* );
+    DECL_LINK( DblClickHdl, MultiListBox* );
 
 private:
-    void InitFuncLb     ( USHORT nFunctions );
-    void SetUI      ( BOOL bSubTotals );
-    // Handler
-    DECL_LINK( ClickHdl, PushButton * );
-    DECL_LINK( DblClickHdl, MultiListBox * );
-    DECL_LINK( RadioClickHdl, RadioButton * );
+    FixedLine           maFlFunc;
+    ScDPFunctionListBox maLbFunc;
+    FixedText           maFtNameLabel;
+    FixedInfo           maFtName;
+    FixedLine           maFlDisplay;
+    FixedText           maFtType;
+    ListBox             maLbType;
+    FixedText           maFtBaseField;
+    ListBox             maLbBaseField;
+    FixedText           maFtBaseItem;
+    ListBox             maLbBaseItem;
+    OKButton            maBtnOk;
+    CancelButton        maBtnCancel;
+    HelpButton          maBtnHelp;
+    MoreButton          maBtnMore;
+
+    ScDPListBoxWrapper  maLbTypeWrp;        /// Wrapper for direct usage of API constants.
+
+    const ScDPLabelDataVec& mrLabelVec;     /// Data of all labels.
+    bool                mbEmptyItem;        /// true = Empty base item in listbox.
 };
 
+// ============================================================================
 
-#endif // SC_PVFUNDLG_HXX
+class ScDPSubtotalDlg : public ModalDialog
+{
+public:
+    explicit            ScDPSubtotalDlg( Window* pParent, ScDPObject& rDPObj,
+                            const ScDPLabelData& rLabelData, const ScDPFuncData& rFuncData,
+                            const ScDPNameVec& rDataFields, bool bEnableLayout );
 
+    USHORT              GetFuncMask() const;
+
+    void                FillLabelData( ScDPLabelData& rLabelData ) const;
+
+private:
+    void                Init( const ScDPLabelData& rLabelData, const ScDPFuncData& rFuncData );
+
+    DECL_LINK( DblClickHdl, MultiListBox* );
+    DECL_LINK( RadioClickHdl, RadioButton* );
+    DECL_LINK( ClickHdl, PushButton* );
+
+private:
+    FixedLine           maFlSubt;
+    RadioButton         maRbNone;
+    RadioButton         maRbAuto;
+    RadioButton         maRbUser;
+    ScDPFunctionListBox maLbFunc;
+    FixedText           maFtNameLabel;
+    FixedInfo           maFtName;
+    CheckBox            maCbShowAll;
+    OKButton            maBtnOk;
+    CancelButton        maBtnCancel;
+    HelpButton          maBtnHelp;
+    PushButton          maBtnOptions;
+
+    ScDPObject&         mrDPObj;            /// The DataPilot object (for member names).
+    const ScDPNameVec&  mrDataFields;       /// The list of all data field names.
+
+    ScDPLabelData       maLabelData;        /// Cache for sub dialog.
+    bool                mbEnableLayout;     /// true = Enable Layout mode controls.
+};
+
+// ============================================================================
+
+class ScDPSubtotalOptDlg : public ModalDialog
+{
+public:
+    explicit            ScDPSubtotalOptDlg( Window* pParent, ScDPObject& rDPObj,
+                            const ScDPLabelData& rLabelData, const ScDPNameVec& rDataFields,
+                            bool bEnableLayout );
+
+    void                FillLabelData( ScDPLabelData& rLabelData ) const;
+
+private:
+    void                Init( const ScDPNameVec& rDataFields, bool bEnableLayout );
+    void                InitHideListBox();
+
+    DECL_LINK( RadioClickHdl, RadioButton* );
+    DECL_LINK( CheckHdl, CheckBox* );
+    DECL_LINK( SelectHdl, ListBox* );
+
+private:
+    FixedLine           maFlSortBy;
+    ListBox             maLbSortBy;
+    RadioButton         maRbSortAsc;
+    RadioButton         maRbSortDesc;
+    RadioButton         maRbSortMan;
+    FixedLine           maFlLayout;
+    FixedText           maFtLayout;
+    ListBox             maLbLayout;
+    CheckBox            maCbLayoutEmpty;
+    FixedLine           maFlAutoShow;
+    CheckBox            maCbShow;
+    NumericField        maNfShow;
+    FixedText           maFtShow;
+    FixedText           maFtShowFrom;
+    ListBox             maLbShowFrom;
+    FixedText           maFtShowUsing;
+    ListBox             maLbShowUsing;
+    FixedLine           maFlHide;
+    SvxCheckListBox     maLbHide;
+    FixedText           maFtHierarchy;
+    ListBox             maLbHierarchy;
+    OKButton            maBtnOk;
+    CancelButton        maBtnCancel;
+    HelpButton          maBtnHelp;
+
+    ScDPListBoxWrapper  maLbLayoutWrp;      /// Wrapper for direct usage of API constants.
+    ScDPListBoxWrapper  maLbShowFromWrp;    /// Wrapper for direct usage of API constants.
+
+    ScDPObject&         mrDPObj;            /// The DataPilot object (for member names).
+    ScDPLabelData       maLabelData;        /// Cache for members data.
+};
+
+// ============================================================================
+
+#endif
 
