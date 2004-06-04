5862d886fca8ca3cf6301d5ecb376c057a0a0b03
Oliver Bolte <obo@openoffice.org>
2004-06-04 02:04:25 +0000

    INTEGRATION: CWS sb18 (1.2.8); FILE MERGED 2004/05/12 09:11:25 sb 1.2.8.1: #i21150# Adapted to instantiated polymorphic struct types.

--- bridges/test/com/sun/star/lib/uno/bridges/java_remote/MethodIdTest.java
+++ bridges/test/com/sun/star/lib/uno/bridges/java_remote/MethodIdTest.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MethodIdTest.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-03-25 14:58:06 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 03:04:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,7 @@
  *
  ************************************************************************/
 
-package com.sun.star.lib.uno.bridges.java_remote;
+package com.sun.star.lib.uno.bridges.javaremote;
 
 import com.sun.star.bridge.XBridge;
 import com.sun.star.bridge.XInstanceProvider;
