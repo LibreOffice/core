6a8e4d90747e37aff723b9b232aff6274e3d6c31
Oliver Bolte <obo@openoffice.org>
2004-06-04 11:03:36 +0000

    INTEGRATION: CWS rowlimit (1.9.26); FILE MERGED 2004/03/03 21:36:24 er 1.9.26.4: #i1967# type correctness 2004/02/11 13:46:08 er 1.9.26.3: RESYNC: (1.10-1.11); FILE MERGED 2004/01/21 14:17:49 er 1.9.26.2: RESYNC: (1.9-1.10); FILE MERGED 2004/01/16 17:43:01 er 1.9.26.1: #i1967# SCCOL,SCROW,SCTAB replace USHORT; SCsCOL,SCsROW,SCsTAB replace short

--- sc/source/ui/view/output3.cxx
+++ sc/source/ui/view/output3.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: output3.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 12:58:04 $
+ *  last change: $Author: obo $ $Date: 2004-06-04 12:03:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -116,8 +116,8 @@ void ScOutputData::DrawingLayer(const sal_uInt16 nLayer, const sal_uInt16 nPaint
     MapMode aOldMode = pDev->GetMapMode();
 
                             // Area auch fuer Metafiles
-    USHORT nCol;
-    USHORT nRow;
+    SCCOL nCol;
+    SCROW nRow;
 
     long nLayoutSign = bLayoutRTL ? -1 : 1;
 
@@ -417,7 +417,7 @@ void ScOutputData::DrawingSingle(const sal_uInt16 nLayer, const sal_uInt16 nPain
 
     BOOL    bHad    = FALSE;
     long    nPosY   = nScrY;
-    USHORT  nArrY;
+    SCSIZE  nArrY;
     for (nArrY=1; nArrY+1<nArrCount; nArrY++)
     {
         BOOL bChanged = FALSE;
