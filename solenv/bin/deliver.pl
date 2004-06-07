a98acc6ea1807fae27dad46787fe7ff77ab8f1f5
Rüdiger Timm <rt@openoffice.org>
2004-06-07 08:34:09 +0000

    #i29666# Create directory %COMMON_DEST%\res%_EXT%\img before copy into into, even in case of empty .ilst filed.

--- solenv/bin/deliver.pl
+++ solenv/bin/deliver.pl
@@ -5,9 +5,9 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 #
 #   $RCSfile: deliver.pl,v $
 #
-#   $Revision: 1.67 $
+#   $Revision: 1.68 $
 #
-#   last change: $Author: rt $ $Date: 2004-05-27 15:36:07 $
+#   last change: $Author: rt $ $Date: 2004-06-07 09:34:09 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -78,7 +78,7 @@ use File::Spec;
 
 ( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
-$id_str = ' $Revision: 1.67 $ ';
+$id_str = ' $Revision: 1.68 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -610,8 +610,9 @@ sub parse_imagelists
             push(@action_data, ['copy', $dlst_line]);
         }
         close(IMGLST);
-        push(@action_data, ['copy', "..\\%__SRC%\\bin\\*.$ilst_ext $destdir"]);
     }
+    push(@action_data, ['mkdir', $destdir]);
+    push(@action_data, ['copy', "..\\%__SRC%\\bin\\*.$ilst_ext $destdir"]);
     return;
 }
 
