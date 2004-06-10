3b177db2809b84ad334aa4331d406086ea7d9c6e
Kurt Zenker <kz@openoffice.org>
2004-06-10 12:19:11 +0000

    INTEGRATION: CWS fwk02ea (1.3.38); FILE MERGED 2004/05/13 08:41:03 as 1.3.38.1: #111074# make frame empty before you check the environment

--- framework/inc/dispatch/closedispatcher.hxx
+++ framework/inc/dispatch/closedispatcher.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: closedispatcher.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2004-02-25 17:33:40 $
+ *  last change: $Author: kz $ $Date: 2004-06-10 13:19:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,8 @@
 #ifndef __FRAMEWORK_DISPATCH_CLOSEDISPATCHER_HXX_
 #define __FRAMEWORK_DISPATCH_CLOSEDISPATCHER_HXX_
 
-//_________________________________________________________________________________________________________________
-//  my own includes
-//_________________________________________________________________________________________________________________
+//_______________________________________________
+// my own includes
 
 #ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
 #include <threadhelp/threadhelpbase.hxx>
@@ -94,9 +93,8 @@
 #include <general.h>
 #endif
 
-//_________________________________________________________________________________________________________________
-//  interface includes
-//_________________________________________________________________________________________________________________
+//_______________________________________________
+// interface includes
 
 #ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
 #include <com/sun/star/lang/XTypeProvider.hpp>
@@ -130,9 +128,8 @@
 #include <com/sun/star/frame/DispatchResultState.hpp>
 #endif
 
-//_________________________________________________________________________________________________________________
-//  other includes
-//_________________________________________________________________________________________________________________
+//_______________________________________________
+// other includes
 
 #ifndef _CPPUHELPER_WEAK_HXX_
 #include <cppuhelper/weak.hxx>
@@ -142,29 +139,22 @@
 #include <vcl/evntpost.hxx>
 #endif
 
-//_________________________________________________________________________________________________________________
-//  namespace
-//_________________________________________________________________________________________________________________
+//_______________________________________________
+// namespace
 
 namespace framework{
 
-//_________________________________________________________________________________________________________________
-//  exported const
-//_________________________________________________________________________________________________________________
+//-----------------------------------------------
+/**
+    @short          helper to dispatch the URLs ".uno:CloseDoc"/".uno:CloseWin"/".uno:CloseFrame"
+                    to close a frame/document or the whole application implicitly in case it was the last frame
 
-//_________________________________________________________________________________________________________________
-//  exported definitions
-//_________________________________________________________________________________________________________________
-
-/*-************************************************************************************************************//**
-    @short          helper to dispatch the URLs ".uno:CloseDoc" and ".uno:CloseWin" to close a targeted frame/document
     @descr          These URLs implements a special functionality to close a document or the whole frame ...
-                    and handle the state, it was the last frame or document. Then we create the default backing document
-                    which can be used to open new ones using the file open dialog or some other menu entries.
-
-    @devstatus      ready to use
-    @threadsafe     yes
-*//*-*************************************************************************************************************/
+                    and handle the state, it was the last frame or document. Then we create the
+                    default backing document which can be used to open new ones using the file open dialog
+                    or some other menu entries. Or we terminate the whole application in case this backing mode shouldnt
+                    be used.
+ */
 class CloseDispatcher : public css::lang::XTypeProvider
                       , public css::frame::XNotifyingDispatch   // => XDispatch
                       , public css::frame::XStatusListener      // => XEventListener
@@ -172,67 +162,102 @@ class CloseDispatcher : public css::lang::XTypeProvider
                       , private ThreadHelpBase
                       , public  ::cppu::OWeakObject
 {
-    //_____________________________________________
+    //-------------------------------------------
     // types
 
     private:
 
+        //---------------------------------------
+        /** @short  describe, which request must be done here.
+        @descr      The incoming URLs {.uno:CloseDoc/CloseWin and CloseFrame
+                    can be classified so and checked later performant.}*/
         enum EOperation
         {
-            E_ESTABLISH_BACKINGMODE,
-            E_CLOSE_TARGET,
-            E_EXIT_APP
+            E_CLOSE_DOC,
+            E_CLOSE_FRAME,
+            E_CLOSE_WIN
         };
 
-    //_____________________________________________
+    //-------------------------------------------
     // member
 
     private:
 
-        /** reference to the uno service manager */
+        //---------------------------------------
+        /** @short reference to an uno service manager,
+                   which can be used to create own needed
+                   uno resources. */
         css::uno::Reference< css::lang::XMultiServiceFactory > m_xSMGR;
 
-        /** reference to the target frame */
-        css::uno::Reference< css::frame::XFrame > m_xTarget;
+        //---------------------------------------
+        /** @short  reference to the target frame, which should be
+                    closed by this dispatch. */
+        css::uno::Reference< css::frame::XFrame > m_xCloseFrame;
 
-        /** used for asynchronous callbacks within the main thread, to
-            establish the backing mode or close the target frame. */
+        //---------------------------------------
+        /** @short  used for asynchronous callbacks within the main thread.
+            @descr  Internaly we work asynchronous. Because our callis
+                    are not aware, that her request can kill its own environment ... */
         ::vcl::EventPoster m_aAsyncCallback;
 
-        /** used inside asyncronous callback to decide, which operation must be executed. */
-        EOperation m_eAsyncOperation;
+        //---------------------------------------
+        /** @short  used inside asyncronous callback to decide,
+                    which operation must be executed. */
+        EOperation m_eOperation;
 
-        /** for asynchronous operations we must hold us self alive! */
+        //---------------------------------------
+        /** @short  for asynchronous operations we must hold us self alive! */
         css::uno::Reference< css::uno::XInterface > m_xSelfHold;
 
-        /** list of registered status listener */
+        //---------------------------------------
+        /** @short  list of registered status listener */
         ListenerHash m_lStatusListener;
 
-        /** holded alive for internaly asynchronous operations! */
+        //---------------------------------------
+        /** @short  holded alive for internaly asynchronous operations! */
         css::uno::Reference< css::frame::XDispatchResultListener > m_xResultListener;
 
-    //_____________________________________________
+    //-------------------------------------------
     // native interface
 
     public:
 
-                 CloseDispatcher( const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR   ,
-                                  const css::uno::Reference< css::frame::XFrame >&              xTarget );
-        virtual ~CloseDispatcher(                                                                       );
+        //---------------------------------------
+        /** @short  connect a new CloseDispatcher instance to its frame.
+            @descr  One CloseDispatcher instance is bound to onw frame only.
+                    That makes an implementation (e.g. of listener support)
+                    much more easier .-)
 
-    //_____________________________________________
+            @param  xSMGR
+                    an un oservice manager, which is needed to create uno resource
+                    internaly.
+
+            @param  xCloseFrame
+                    the frame, where we must work on.
+         */
+        CloseDispatcher(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR      ,
+                        const css::uno::Reference< css::frame::XFrame >&              xCloseFrame);
+
+        //---------------------------------------
+        /** @short  does nothing real. */
+        virtual ~CloseDispatcher();
+
+    //-------------------------------------------
     // uno interface
 
     public:
 
+        //---------------------------------------
         DECLARE_XINTERFACE
         DECLARE_XTYPEPROVIDER
 
+        //---------------------------------------
         // XNotifyingDispatch
         virtual void SAL_CALL dispatchWithNotification( const css::util::URL&                                             aURL      ,
                                                         const css::uno::Sequence< css::beans::PropertyValue >&            lArguments,
                                                         const css::uno::Reference< css::frame::XDispatchResultListener >& xListener ) throw(css::uno::RuntimeException);
 
+        //---------------------------------------
         // XDispatch
         virtual void SAL_CALL dispatch            ( const css::util::URL&                                     aURL      ,
                                                     const css::uno::Sequence< css::beans::PropertyValue >&    lArguments) throw(css::uno::RuntimeException);
@@ -241,31 +266,111 @@ class CloseDispatcher : public css::lang::XTypeProvider
         virtual void SAL_CALL removeStatusListener( const css::uno::Reference< css::frame::XStatusListener >& xListener ,
                                                     const css::util::URL&                                     aURL      ) throw(css::uno::RuntimeException);
 
+        //---------------------------------------
         // XStatusListener
         virtual void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& aState ) throw(css::uno::RuntimeException);
 
+        //---------------------------------------
         // XEventListener
         virtual void SAL_CALL disposing( const css::lang::EventObject& aSource ) throw(css::uno::RuntimeException);
 
-    //_____________________________________________
+    //-------------------------------------------
     // internal helper
 
     private:
 
-        void     impl_dispatchCloseDoc    ( const css::uno::Sequence< css::beans::PropertyValue >&                 lArguments   ,
-                                            const css::uno::Reference< css::frame::XDispatchResultListener >&      xListener    );
-        void     impl_dispatchCloseWin    ( const css::uno::Sequence< css::beans::PropertyValue >&                 lArguments   ,
-                                            const css::uno::Reference< css::frame::XDispatchResultListener >&      xListener    );
-        void     impl_dispatchCloseFrame  ( const css::uno::Sequence< css::beans::PropertyValue >&                 lArguments   ,
-                                            const css::uno::Reference< css::frame::XDispatchResultListener >&      xListener    );
-        sal_Bool impl_closeFrame          (       css::uno::Reference< css::frame::XFrame >&                       xFrame       ,
-                                                  sal_Bool                                                         bCallSuspend );
-        sal_Bool impl_exitApp             (       css::uno::Reference< css::frame::XFrame >&                       xTarget      );
-        void     impl_notifyResultListener( const css::uno::Reference< css::frame::XDispatchResultListener >&      xListener    ,
-                                                  sal_Int16                                                        nState       ,
-                                            const css::uno::Any&                                                   aResult      );
-        sal_Bool impl_establishBackingMode(                                                                                     );
+        //---------------------------------------
+        /** @short  a callback for asynchronous started operations.
+
+            @descr  As already mentione, we make internaly all operations
+                    asynchronous. Otherwhise our callis kill its own environment
+                    during they call us ...
+        */
         DECL_LINK( impl_asyncCallback, void* );
+
+        //---------------------------------------
+        /** @short  close the document view of our m_xCloseFrame.
+
+            @descr  Thats needed to be shure, that the document cant disagree
+                    later with e.g. an office termination.
+                    The problem: Closing of documents can show UI. If the user
+                    ignores it and open/close other documents, we cant know
+                    which state the office has after closing of this frame.
+
+            @param  bAllowSuspend
+                    force calling of XController->suspend().
+
+            @param  bCloseAllOtherViewsToo
+                    if there are other top level frames, which
+                    contains views to the same document then our m_xCloseFrame,
+                    they are forced to be closed too.
+                    We need it to implement the CLOSE_DOC semantic.
+
+            @return [boolean]
+                    TRUE if closing was successfully.
+         */
+        sal_Bool implts_closeView(sal_Bool bAllowSuspend         ,
+                                  sal_Bool bCloseAllOtherViewsToo);
+
+        //---------------------------------------
+        /** @short  close the member m_xCloseFrame.
+
+            @descr  This method does not look for any document
+                    inside this frame. Such views must be cleared
+                    before (e.g. by calling implts_closeView()!
+
+                    Otherwhise e.g. the XController->suspend()
+                    call isnt made and no UI warn the user about
+                    loosing document changes. Because the
+                    frame is closed ....
+
+            @return [bool]
+                    TRUE if closing was successfully.
+         */
+        sal_Bool implts_closeFrame();
+
+        //---------------------------------------
+        /** @short  set the special BackingComponent (now StartModule)
+                    as new component of our m_xCloseFrame.
+
+            @return [bool]
+                    TRUE if operation was successfully.
+         */
+        sal_Bool implts_establishBackingMode();
+
+        //---------------------------------------
+        /** @short  calls XDesktop->terminate().
+
+            @descr  No office code has to be called
+                    afterwards! Because the process is dieing ...
+                    The only exception is a might be registered
+                    listener at this instance here.
+                    Because he should know, that such things will happen :-)
+
+            @return [bool]
+                    TRUE if termination of the application was started ...
+         */
+        sal_Bool implts_terminateApplication();
+
+        //---------------------------------------
+        /** @short  notify a DispatchResultListener.
+
+            @descr  We check the listener reference before we use it.
+                    So this method can be called everytimes!
+
+            @parama xListener
+                    the listener, which should be notified.
+                    Can be null!
+
+            @param  nState
+                    directly used as css::frame::DispatchResultState value.
+
+            @param  aResult
+                    not used yet realy ...
+         */
+        void implts_notifyResultListener(const css::uno::Reference< css::frame::XDispatchResultListener >& xListener,
+                                               sal_Int16                                                   nState   ,
+                                         const css::uno::Any&                                              aResult  );
 
 }; // class CloseDispatcher
 
