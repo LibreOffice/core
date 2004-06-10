c1289abebf61f6ccfff20fbcfb2efac8efe32ade
Kurt Zenker <kz@openoffice.org>
2004-06-10 14:57:13 +0000

    INTEGRATION: CWS fwk02ea (1.2.58); FILE MERGED 2004/06/10 07:24:07 as 1.2.58.1: remove UTF-8 problematic signs

--- filter/source/config/tools/utils/Logger.java
+++ filter/source/config/tools/utils/Logger.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Logger.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-01-28 19:01:29 $
+ *  last change: $Author: kz $ $Date: 2004-06-10 15:57:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -73,7 +73,7 @@ import java.util.*;
 /** can be used to print out some debug messages
  *  and group it into warnings/errors or info statements.
  *
- *  @author Andreas Schlüns
+ *
  */
 public class Logger
 {
