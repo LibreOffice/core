a695508859ced2831ef705b10513844827895784
Rüdiger Timm <rt@openoffice.org>
2004-06-11 15:06:36 +0000

    INTEGRATION: CWS hebnum (1.17.14); FILE MERGED 2004/05/07 17:54:54 khong 1.17.14.4: #i23372# add Hebrew native numbering system 2004/05/04 21:42:22 khong 1.17.14.3: #i23372# add Hebrew native numbering system 2004/05/04 00:24:38 khong 1.17.14.2: #i23372# add Hebrew native numbering system 2004/04/14 22:00:31 khong 1.17.14.1: #i23372# add Hebrew native numbering system

--- i18npool/source/nativenumber/nativenumbersupplier.cxx
+++ i18npool/source/nativenumber/nativenumbersupplier.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: nativenumbersupplier.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-04 13:24:53 $
+ *  last change: $Author: rt $ $Date: 2004-06-11 16:06:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -104,6 +104,8 @@ typedef struct {
 
 namespace com { namespace sun { namespace star { namespace i18n {
 
+OUString SAL_CALL getHebrewNativeNumberString(const OUString& aNumberString, sal_Bool useGeresh);
+
 static sal_Unicode thousandSeparator = NUMBER_COMMA;    // default separator
 
 OUString SAL_CALL AsciiToNativeChar( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
@@ -116,9 +118,18 @@ OUString SAL_CALL AsciiToNativeChar( const OUString& inStr, sal_Int32 startPos, 
 
         for (sal_Int32 i = 0; i < nCount; i++) {
             sal_Unicode ch = src[i];
-            newStr->buffer[i] = (isNumber(ch) ? NumberChar[number][ ch - NUMBER_ZERO ] :
-                isDecimal(ch) ? DecimalChar[number] : isMinus(ch) ? MinusChar[number] :
-                ! isSeparator(ch) ? ch : number == NumberChar_FullWidth ? thousandSeparator + 0xFEE0 : SeparatorChar[number]);
+            if (isNumber(ch))
+                newStr->buffer[i] = NumberChar[number][ ch - NUMBER_ZERO ];
+            else if (i+1 < nCount && isNumber(src[i+1])) {
+                if (i > 0 && isNumber(src[i-1]) && isSeparator(ch))
+                    newStr->buffer[i] = (number == NumberChar_FullWidth) ?
+                            thousandSeparator + 0xFEE0 : SeparatorChar[number];
+                else
+                    newStr->buffer[i] = isDecimal(ch) ? DecimalChar[number] :
+                            isMinus(ch) ? MinusChar[number] : ch;
+            }
+            else
+                newStr->buffer[i] = ch;
             if (useOffset)
                 offset[i] = startPos + i;
         }
@@ -456,16 +467,16 @@ static Number natnum10 = { NumberChar_Hangul_ko, MultiplierChar_6_CJK[Multiplier
 static Number natnum11 = { NumberChar_Hangul_ko, MultiplierChar_6_CJK[Multiplier_Hangul_ko], NUMBER_OMIT_ALL,
                 ExponentCount_6_CJK, MultiplierExponent_6_CJK };
 
-static sal_Char *natnum1Locales[] = { "zh_CN", "zh_TW", "ja", "ko", "ar", "th", "hi", "or", "mr", "bn", "pa", "gu", "ta", "te", "kn", "ml", "lo", "bo", "my", "km", "mn" };
+static const sal_Char *natnum1Locales[] = { "zh_CN", "zh_TW", "ja", "ko", "he", "ar", "th", "hi", "or", "mr", "bn", "pa", "gu", "ta", "te", "kn", "ml", "lo", "bo", "my", "km", "mn" };
 static sal_Int16 nbOfLocale = sizeof(natnum1Locales)/sizeof(natnum1Locales[0]);
 
-static sal_Int16 natnum1[] = { NumberChar_Lower_zh, NumberChar_Lower_zh, NumberChar_Modern_ja, NumberChar_Lower_ko,
+static sal_Int16 natnum1[] = { NumberChar_Lower_zh, NumberChar_Lower_zh, NumberChar_Modern_ja, NumberChar_Lower_ko, NumberChar_he,
                 NumberChar_Indic_ar, NumberChar_th, NumberChar_hi, NumberChar_or, NumberChar_mr, NumberChar_bn,
                 NumberChar_pa, NumberChar_gu, NumberChar_ta, NumberChar_te, NumberChar_kn, NumberChar_ml,
                 NumberChar_lo, NumberChar_bo, NumberChar_my, NumberChar_km, NumberChar_mn };
 static sal_Int16 sizeof_natnum1 = sizeof(natnum1)/sizeof(natnum1[0]);
 static sal_Int16 natnum2[] = { NumberChar_Upper_zh, NumberChar_Upper_zh_TW, NumberChar_Traditional_ja,
-                NumberChar_Upper_ko };
+                NumberChar_Upper_ko, NumberChar_he };
 static sal_Int16 sizeof_natnum2 = sizeof(natnum2)/sizeof(natnum2[0]);
 
 #define isLang(lang) rLocale.Language.equalsAsciiL(lang, 2)
@@ -488,17 +499,6 @@ OUString SAL_CALL NativeNumberSupplier::getNativeNumberString(const OUString& aN
 {
         Number *number = 0;
         sal_Int16 num = -1;
-        if (!aLocale.Language.equals(rLocale.Language) ||
-                !aLocale.Country.equals(rLocale.Country) ||
-                !aLocale.Variant.equals(rLocale.Variant)) {
-            LocaleDataItem item = LocaleData().getLocaleItem( rLocale );
-            aLocale = rLocale;
-            DecimalChar[NumberChar_HalfWidth] = item.decimalSeparator.toChar();
-            if (DecimalChar[NumberChar_HalfWidth] > 0x7E || DecimalChar[NumberChar_HalfWidth] < 0x21)
-                DecimalChar[NumberChar_HalfWidth] = 0x002C;
-            DecimalChar[NumberChar_FullWidth] = DecimalChar[NumberChar_HalfWidth] + 0xFEE0;
-            thousandSeparator = item.thousandSeparator.toChar();
-        }
 
         if (isValidNatNum(rLocale, nNativeNumberMode)) {
             sal_Int16 langnum = getLanguageNumber(rLocale);
@@ -543,10 +543,27 @@ OUString SAL_CALL NativeNumberSupplier::getNativeNumberString(const OUString& aN
                 break;
             }
         }
-        if (number)
-            return AsciiToNative( aNumberString, 0, aNumberString.getLength(), offset, useOffset, number );
-        else if (num >= 0)
-            return AsciiToNativeChar(aNumberString, 0, aNumberString.getLength(), offset, useOffset, num);
+
+        if (number || num >= 0) {
+            if (!aLocale.Language.equals(rLocale.Language) ||
+                    !aLocale.Country.equals(rLocale.Country) ||
+                    !aLocale.Variant.equals(rLocale.Variant)) {
+                LocaleDataItem item = LocaleData().getLocaleItem( rLocale );
+                aLocale = rLocale;
+                DecimalChar[NumberChar_HalfWidth] = item.decimalSeparator.toChar();
+                if (DecimalChar[NumberChar_HalfWidth] > 0x7E || DecimalChar[NumberChar_HalfWidth] < 0x21)
+                    DecimalChar[NumberChar_HalfWidth] = 0x002C;
+                DecimalChar[NumberChar_FullWidth] = DecimalChar[NumberChar_HalfWidth] + 0xFEE0;
+                thousandSeparator = item.thousandSeparator.toChar();
+            }
+            if (number)
+                return AsciiToNative( aNumberString, 0, aNumberString.getLength(), offset, useOffset, number );
+            else if (num == NumberChar_he)
+                return getHebrewNativeNumberString(aNumberString,
+                                nNativeNumberMode == NativeNumberMode::NATNUM2);
+            else
+                return AsciiToNativeChar(aNumberString, 0, aNumberString.getLength(), offset, useOffset, num);
+        }
         else
             return aNumberString;
 }
@@ -593,9 +610,9 @@ sal_Unicode SAL_CALL NativeNumberSupplier::getNativeNumberChar( const sal_Unicod
         return inChar;
 }
 
-sal_Bool SAL_CALL NativeNumberSupplier::isValidNatNum( const Locale& aLocale, sal_Int16 nNativeNumberMode ) throw (RuntimeException)
+sal_Bool SAL_CALL NativeNumberSupplier::isValidNatNum( const Locale& rLocale, sal_Int16 nNativeNumberMode ) throw (RuntimeException)
 {
-        sal_Int16 langnum = getLanguageNumber(aLocale);
+        sal_Int16 langnum = getLanguageNumber(rLocale);
 
         switch (nNativeNumberMode) {
             case NativeNumberMode::NATNUM0:     // Ascii
@@ -606,33 +623,35 @@ sal_Bool SAL_CALL NativeNumberSupplier::isValidNatNum( const Locale& aLocale, sa
                 return (langnum >= 0);
             break;
             case NativeNumberMode::NATNUM2:     // Char, Upper
+                if (langnum == 4) // Hebrew numbering
+                    return sal_True;
             case NativeNumberMode::NATNUM4:     // Text, Lower, Long
             case NativeNumberMode::NATNUM5:     // Text, Upper, Long
             case NativeNumberMode::NATNUM6:     // Text, FullWidth
             case NativeNumberMode::NATNUM7:     // Text. Lower, Short
             case NativeNumberMode::NATNUM8:     // Text, Upper, Short
-                return (langnum >= 0 && langnum < 4);
+                return (langnum >= 0 && langnum < 4); // CJK numbering
             break;
             case NativeNumberMode::NATNUM9:     // Char, Hangul
             case NativeNumberMode::NATNUM10:    // Text, Hangul, Long
             case NativeNumberMode::NATNUM11:    // Text, Hangul, Short
-                return (langnum == 3);
+                return (langnum == 3); // Korean numbering
             break;
         }
         return sal_False;
 }
 
-NativeNumberXmlAttributes SAL_CALL NativeNumberSupplier::convertToXmlAttributes( const Locale& aLocale, sal_Int16 nNativeNumberMode ) throw (RuntimeException)
+NativeNumberXmlAttributes SAL_CALL NativeNumberSupplier::convertToXmlAttributes( const Locale& rLocale, sal_Int16 nNativeNumberMode ) throw (RuntimeException)
 {
         static const sal_Int16 attShort         = 0;
         static const sal_Int16 attMedium        = 1;
         static const sal_Int16 attLong          = 2;
-        static sal_Char *attType[] = { "short", "medium", "long" };
+        static const sal_Char *attType[] = { "short", "medium", "long" };
 
         sal_Int16 number = NumberChar_HalfWidth, type = attShort;
 
-        if (isValidNatNum(aLocale, nNativeNumberMode)) {
-            sal_Int16 langnum = getLanguageNumber(aLocale);
+        if (isValidNatNum(rLocale, nNativeNumberMode)) {
+            sal_Int16 langnum = getLanguageNumber(rLocale);
             switch (nNativeNumberMode) {
                 case NativeNumberMode::NATNUM0: // Ascii
                     number = NumberChar_HalfWidth;
@@ -644,7 +663,7 @@ NativeNumberXmlAttributes SAL_CALL NativeNumberSupplier::convertToXmlAttributes(
                 break;
                 case NativeNumberMode::NATNUM2: // Char, Upper
                     number = natnum2[langnum];
-                    type = attShort;
+                    type = number == NumberChar_he ? attMedium : attShort;
                 break;
                 case NativeNumberMode::NATNUM3: // Char, FullWidth
                     number = NumberChar_FullWidth;
@@ -686,7 +705,7 @@ NativeNumberXmlAttributes SAL_CALL NativeNumberSupplier::convertToXmlAttributes(
                 break;
             }
         }
-        NativeNumberXmlAttributes att(aLocale, OUString(NumberChar[number] + 1, 1),
+        NativeNumberXmlAttributes att(rLocale, OUString(&NumberChar[number][1], 1),
                                             OUString::createFromAscii(attType[type]));
         return att;
 }
@@ -720,6 +739,8 @@ sal_Int16 SAL_CALL NativeNumberSupplier::convertFromXmlAttributes( const NativeN
         } else if (aAttr.Style.equalsAscii("medium")) {
             if (num == NumberChar_Hangul_ko)
                 return NativeNumberMode::NATNUM11;
+            else if (num == NumberChar_he)
+                return NativeNumberMode::NATNUM2;
             else if (natNumIn(num, natnum1, sizeof_natnum1))
                 return NativeNumberMode::NATNUM7;
             else if (natNumIn(num, natnum2, sizeof_natnum2))
@@ -739,7 +760,112 @@ sal_Int16 SAL_CALL NativeNumberSupplier::convertFromXmlAttributes( const NativeN
         return NativeNumberMode::NATNUM0;
 }
 
-static sal_Char* implementationName = "com.sun.star.i18n.NativeNumberSupplier";
+
+// Following code generates Hebrew Number,
+// see numerical system in the Hebrew Numbering System in following link for details,
+// http://people.netscape.com/smontagu/writings/HebrewNumbers.html
+
+struct HebrewNumberChar {
+    sal_Unicode code;
+    sal_Int16 value;
+} HebrewNumberCharArray[] = {
+    { 0x05ea, 400 },
+    { 0x05ea, 400 },
+    { 0x05e9, 300 },
+    { 0x05e8, 200 },
+    { 0x05e7, 100 },
+    { 0x05e6, 90 },
+    { 0x05e4, 80 },
+    { 0x05e2, 70 },
+    { 0x05e1, 60 },
+    { 0x05e0, 50 },
+    { 0x05de, 40 },
+    { 0x05dc, 30 },
+    { 0x05db, 20 },
+    { 0x05d9, 10 },
+    { 0x05d8, 9 },
+    { 0x05d7, 8 },
+    { 0x05d6, 7 },
+    { 0x05d5, 6 },
+    { 0x05d4, 5 },
+    { 0x05d3, 4 },
+    { 0x05d2, 3 },
+    { 0x05d1, 2 },
+    { 0x05d0, 1 }
+};
+
+static sal_Int16 nbOfHebrewNumberChar = sizeof(HebrewNumberCharArray)/sizeof(HebrewNumberChar);
+
+static sal_Unicode thousand[] = {0x05d0, 0x05dc, 0x05e3, 0x0};
+static sal_Unicode thousands[] = {0x05d0, 0x05dc, 0x05e4, 0x05d9, 0x0};
+static sal_Unicode thousands_last[] = {0x05d0, 0x05dc, 0x05e4, 0x05d9, 0x05dd, 0x0};
+static sal_Unicode geresh = 0x05f3;
+static sal_Unicode gershayim = 0x05f4;
+
+void makeHebrewNumber(sal_Int64 value, OUStringBuffer& output, sal_Bool isLast, sal_Bool useGeresh)
+{
+    sal_Int16 num = value % 1000;
+
+    if (value > 1000) {
+        makeHebrewNumber(value / 1000, output, num != 0, useGeresh);
+        output.appendAscii(" ");
+    }
+    if (num == 0) {
+        output.append(value == 1000 ? thousand : isLast ? thousands_last : thousands);
+    } else {
+        sal_Int16 nbOfChar = 0;
+        for (sal_Int32 j = 0; num > 0 && j < nbOfHebrewNumberChar; j++) {
+            if (num - HebrewNumberCharArray[j].value >= 0) {
+                nbOfChar++;
+                if (num == 15 || num == 16) // substitution for 15 and 16
+                    j++;
+                num-= HebrewNumberCharArray[j].value;
+                output.append(HebrewNumberCharArray[j].code);
+            }
+        }
+        if (useGeresh) {
+            if (nbOfChar > 1)   // a number is written as more than one character
+                output.insert(output.getLength() - 1, gershayim);
+            else if (nbOfChar == 1) // a number is written as a single character
+                output.append(geresh);
+        }
+    }
+}
+
+OUString SAL_CALL getHebrewNativeNumberString(const OUString& aNumberString, sal_Bool useGeresh)
+{
+    sal_Int64 value = 0;
+    sal_Int32 i, count = 0, len = aNumberString.getLength();
+    const sal_Unicode *src = aNumberString.getStr();
+    sal_Bool neg = sal_False;
+
+    for (i = 0; i < len; i++) {
+        sal_Unicode ch = src[i];
+        if (isNumber(ch)) {
+            if (++count >= 20) // Number is too long, could not be handled.
+                return aNumberString;
+            value = value * 10 + (ch - NUMBER_ZERO);
+        }
+        else if (isSeparator(ch) && count > 0) continue;
+        else if (isMinus(ch) && count == 0) neg = sal_True;
+        else break;
+    }
+
+    if (value > 0) {
+        OUStringBuffer output(count*2 + 2 + len - i);
+
+        makeHebrewNumber(value, output, sal_True, useGeresh);
+
+        if (i < len)
+            output.append(aNumberString.copy(i));
+
+        return output.makeStringAndClear();
+    }
+    else
+        return aNumberString;
+}
+
+static const sal_Char* implementationName = "com.sun.star.i18n.NativeNumberSupplier";
 
 OUString SAL_CALL NativeNumberSupplier::getImplementationName() throw( RuntimeException )
 {
