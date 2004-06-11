1a920d3f270143243d3eac6a44db9a74342f3ae5
Kurt Zenker <kz@openoffice.org>
2004-06-11 10:52:50 +0000

    INTEGRATION: CWS unopkg2 (1.15.312); FILE MERGED 2004/05/05 11:07:59 dbo 1.15.312.1: #115885# added XAbortChannel

--- offapi/com/sun/star/task/makefile.mk
+++ offapi/com/sun/star/task/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: hr $ $Date: 2003-04-04 17:44:16 $
+#   last change: $Author: kz $ $Date: 2004-06-11 11:52:50 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -99,7 +99,8 @@ IDLFILES=\
     XPasswordContainer.idl\
     XStatusIndicator.idl\
     XStatusIndicatorFactory.idl\
-    XStatusIndicatorSupplier.idl
+    XStatusIndicatorSupplier.idl\
+    XAbortChannel.idl
 
 
 # ------------------------------------------------------------------
