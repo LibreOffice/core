5b881381d70d5c9c0f0f4b45371378dbbfabde4c
Kurt Zenker <kz@openoffice.org>
2004-06-11 11:09:03 +0000

    INTEGRATION: CWS unopkg2 (1.2.4); FILE MERGED 2004/06/03 13:15:31 dbo 1.2.4.5: #i20304# decoupling from vcl Issue number: Submitted by: Reviewed by: 2004/05/27 13:23:06 dbo 1.2.4.4: #i20304# cleanup process in case of error Issue number: Submitted by: Reviewed by: 2004/05/19 12:53:44 dbo 1.2.4.3: #i20304# minor fixes 2004/05/14 16:56:24 dbo 1.2.4.2: #i20304# live deployment, abort feature 2004/05/11 15:14:12 dbo 1.2.4.1: #i20304# major API revision, GUI Issue number: Submitted by: Reviewed by:

--- desktop/source/deployment/misc/dp_misc.cxx
+++ desktop/source/deployment/misc/dp_misc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dp_misc.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: svesik $ $Date: 2004-04-20 11:04:00 $
+ *  last change: $Author: kz $ $Date: 2004-06-11 12:09:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,55 +60,108 @@
  ************************************************************************/
 
 #include "dp_misc.h"
+#include "dp_interact.h"
 #include "rtl/uri.hxx"
 #include "rtl/digest.h"
+#include "rtl/random.h"
+#include "rtl/bootstrap.hxx"
 #include "osl/file.hxx"
 #include "osl/pipe.hxx"
-#include "com/sun/star/util/XMacroExpander.hpp"
+#include "osl/security.hxx"
+#include "osl/thread.hxx"
+#include "osl/mutex.hxx"
+#include "com/sun/star/ucb/CommandAbortedException.hpp"
+#include "com/sun/star/bridge/XUnoUrlResolver.hpp"
 
 
+using ::rtl::OUString;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
-using ::rtl::OUString;
 
 namespace dp_misc
 {
 
+static OUString s_platform, s_os = OUSTR("$_OS"), s_arch = OUSTR("$_ARCH");
+
 //==============================================================================
-OUString expand_reg_url(
-    OUString const & url, Reference< XComponentContext > const & xContext )
+OUString const & getPlatformString()
 {
-    Reference< util::XMacroExpander > xMacroExpander(
-        xContext->getValueByName(
-            OUSTR("/singletons/com.sun.star.util.theMacroExpander") ),
-        UNO_QUERY_THROW );
+    if (s_platform.getLength() == 0)
+    {
+        ::osl::MutexGuard guard( ::osl::Mutex::getGlobalMutex() );
+        ::rtl::Bootstrap::expandMacros( s_os );
+        ::rtl::Bootstrap::expandMacros( s_arch );
+        ::rtl::OUStringBuffer buf;
+        buf.append( s_os );
+        buf.append( static_cast<sal_Unicode>('_') );
+        buf.append( s_arch );
+        s_platform = buf.makeStringAndClear();
+    }
+    return s_platform;
+}
+//==============================================================================
+bool platform_fits( OUString const & platform_string )
+{
+    OUString const & thisPlatform = getPlatformString();
+    sal_Int32 index = 0;
+    for (;;)
+    {
+        OUString token( platform_string.getToken( 0, ',', index ).trim() );
+        // check if this platform:
+        if (token.equalsIgnoreAsciiCase( thisPlatform ) ||
+            (token.indexOf( '_' ) < 0 && /* check OS part only */
+             token.equalsIgnoreAsciiCase( s_os )))
+        {
+            return true;
+        }
+        if (index < 0)
+            break;
+    }
+    return false;
+}
+
+//==============================================================================
+OUString make_url( OUString const & base_url, OUString const & url )
+{
+    // xxx todo: wait for SB's api
+    ::rtl::OUStringBuffer buf;
+    buf.append( base_url );
+    if (base_url.getLength() > 0 &&
+        base_url[ base_url.getLength() - 1 ] != '/')
+    {
+        buf.append( static_cast< sal_Unicode >('/') );
+    }
+    if (url.getLength() > 0 && url[ 0 ] == '/')
+        buf.append( url.copy( 1 ) );
+    else
+        buf.append( url );
+    return buf.makeStringAndClear();
+}
+
+//==============================================================================
+OUString expand_url( OUString const & url )
+{
     if (url.matchIgnoreAsciiCaseAsciiL(
             RTL_CONSTASCII_STRINGPARAM("vnd.sun.star.expand:") ))
     {
+        static ::rtl::Bootstrap * s_punorc = 0;
+        if (s_punorc == 0)
+        {
+            OUString unorc = OUSTR("$ORIGIN/" SAL_CONFIGFILE("uno"));
+            ::rtl::Bootstrap::expandMacros( unorc );
+            static ::rtl::Bootstrap s_unorc( unorc );
+            OSL_ASSERT( s_unorc.getHandle() != 0 );
+            s_punorc = &s_unorc;
+        }
+
         // cut protocol:
-        OUString macro( url.copy( sizeof ("vnd.sun.star.expand:") -1 ) );
+        OUString macro( url.copy( sizeof ("vnd.sun.star.expand:") - 1 ) );
         // decode uric class chars:
         macro = ::rtl::Uri::decode(
             macro, rtl_UriDecodeWithCharset, RTL_TEXTENCODING_UTF8 );
         // expand macro string:
-        OUString ret( xMacroExpander->expandMacros( macro ) );
-// #if OSL_DEBUG_LEVEL > 1
-//         {
-//         OUStringBuffer buf( 128 );
-//         buf.appendAscii(
-//             RTL_CONSTASCII_STRINGPARAM(__FILE__" expand_reg_url(): ") );
-//         buf.append( url );
-//         buf.appendAscii( RTL_CONSTASCII_STRINGPARAM(" => ") );
-//         buf.append( macro );
-//         buf.appendAscii( RTL_CONSTASCII_STRINGPARAM(" => ") );
-//         buf.append( ret );
-//         OString cstr(
-//             OUStringToOString(
-//                 buf.makeStringAndClear(), osl_getThreadTextEncoding() ) );
-//         OSL_TRACE( "%s", cstr.getStr() );
-//         }
-// #endif
-        return ret;
+        s_punorc->expandMacrosFrom( macro );
+        return macro;
     }
     else
     {
@@ -116,31 +169,27 @@ OUString expand_reg_url(
     }
 }
 
-enum t_status { RUNNING, NOT_RUNNING, INIT_ME };
 //==============================================================================
-bool office_is_running( Reference< XComponentContext > const & xContext )
+bool office_is_running()
 {
-    static t_status s_status = INIT_ME;
-    if (s_status == INIT_ME)
+    static OUString s_pipeId;
+    if (s_pipeId.getLength() == 0)
     {
-        Reference< util::XMacroExpander > xMacroExpander(
-            xContext->getValueByName(
-                OUSTR("/singletons/com.sun.star.util.theMacroExpander") ),
-            UNO_QUERY_THROW );
-        OUString user_path(
-            xMacroExpander->expandMacros( OUSTR( "${$SYSBINDIR/"
-                                                 SAL_CONFIGFILE("bootstrap")
-                                                 ":UserInstallation}") ) );
+        OUString userPath = OUSTR("${$SYSBINDIR/" SAL_CONFIGFILE("bootstrap")
+                                  ":UserInstallation}");
+        ::rtl::Bootstrap::expandMacros( userPath );
+        OSL_ASSERT( userPath.getLength() > 0 );
+
         // normalize path:
         ::osl::FileStatus status( FileStatusMask_FileURL );
         ::osl::DirectoryItem dirItem;
-        if (::osl::DirectoryItem::get( user_path, dirItem )
+        if (::osl::DirectoryItem::get( userPath, dirItem )
             != ::osl::DirectoryItem::E_None ||
             dirItem.getFileStatus( status )
             != ::osl::DirectoryItem::E_None ||
             !status.isValid( FileStatusMask_FileURL ) ||
             ::osl::FileBase::getAbsoluteFileURL(
-                OUString(), status.getFileURL(), user_path )
+                OUString(), status.getFileURL(), userPath )
             != ::osl::FileBase::E_None)
         {
             return false;
@@ -148,19 +197,17 @@ bool office_is_running( Reference< XComponentContext > const & xContext )
 
         rtlDigest digest = rtl_digest_create( rtl_Digest_AlgorithmMD5 );
         if (digest <= 0)
-        {
             throw RuntimeException(
                 OUSTR("cannot get digest rtl_Digest_AlgorithmMD5!"), 0 );
-        }
 
         sal_uInt8 const * data =
-            reinterpret_cast< sal_uInt8 const * >(user_path.getStr());
-        sal_Size size = (user_path.getLength() * sizeof (sal_Unicode));
+            reinterpret_cast< sal_uInt8 const * >(userPath.getStr());
+        sal_Size size = (userPath.getLength() * sizeof (sal_Unicode));
         sal_uInt32 md5_key_len = rtl_digest_queryLength( digest );
         sal_uInt8 * md5_buf = new sal_uInt8 [ md5_key_len ];
 
-        rtl_digest_init( digest, data, static_cast< sal_uInt32 >(size) );
-        rtl_digest_update( digest, data, static_cast< sal_uInt32 >(size) );
+        rtl_digest_init( digest, data, static_cast<sal_uInt32>(size) );
+        rtl_digest_update( digest, data, static_cast<sal_uInt32>(size) );
         rtl_digest_get( digest, md5_buf, md5_key_len );
         rtl_digest_destroy( digest );
 
@@ -169,16 +216,104 @@ bool office_is_running( Reference< XComponentContext > const & xContext )
         ::rtl::OUStringBuffer buf;
         buf.appendAscii( RTL_CONSTASCII_STRINGPARAM("SingleOfficeIPC_") );
         for ( sal_uInt32 i = 0; i < md5_key_len; ++i )
-            buf.append( static_cast< sal_Int32 >(md5_buf[ i ]), 0x10 );
+            buf.append( static_cast<sal_Int32>(md5_buf[ i ]), 0x10 );
 
         delete [] md5_buf;
-
-        OUString pipe_id( buf.makeStringAndClear() );
-        ::osl::Security sec;
-        ::osl::Pipe pipe( pipe_id, osl_Pipe_OPEN, sec );
-        s_status = (pipe.is() ? RUNNING : NOT_RUNNING);
+        s_pipeId = buf.makeStringAndClear();
     }
-    return (s_status == RUNNING);
+
+    ::osl::Security sec;
+    ::osl::Pipe pipe( s_pipeId, osl_Pipe_OPEN, sec );
+    return pipe.is();
+}
+
+//==============================================================================
+oslProcess raiseProcess(
+    OUString const & appURL, Sequence<OUString> const & args )
+{
+    ::osl::Security sec;
+    sal_Int32 pos = args.getLength();
+    OUString const * pargs = args.getConstArray();
+    rtl_uString ** parArgs = new rtl_uString * [ pos ];
+    for ( ; pos--; )
+        parArgs[ pos ] = pargs[ pos ].pData;
+
+    oslProcess hProcess = 0;
+    oslProcessError rc = osl_executeProcess(
+        appURL.pData, parArgs, args.getLength(),
+        osl_Process_DETACHED,
+        sec.getHandle(),
+        0, // => current working dir
+        0, 0, // => no env vars
+        &hProcess );
+    delete [] parArgs;
+
+    switch (rc)
+    {
+    case osl_Process_E_None:
+        break;
+    case osl_Process_E_NotFound:
+        throw RuntimeException( OUSTR("image not found!"), 0 );
+    case osl_Process_E_TimedOut:
+        throw RuntimeException( OUSTR("timout occured!"), 0 );
+    case osl_Process_E_NoPermission:
+        throw RuntimeException( OUSTR("permission denied!"), 0 );
+    case osl_Process_E_Unknown:
+        throw RuntimeException( OUSTR("unknown error!"), 0 );
+    case osl_Process_E_InvalidError:
+    default:
+        throw RuntimeException( OUSTR("unmapped error!"), 0 );
+    }
+
+    return hProcess;
+}
+
+//==============================================================================
+OUString generateRandomPipeId()
+{
+    // compute some good pipe id:
+    static rtlRandomPool s_hPool = rtl_random_createPool();
+    if (s_hPool == 0)
+        throw RuntimeException( OUSTR("cannot create random pool!?"), 0 );
+    sal_uInt8 bytes[ 32 ];
+    if (rtl_random_getBytes(
+            s_hPool, bytes, ARLEN(bytes) ) != rtl_Random_E_None)
+        throw RuntimeException( OUSTR("random pool error!?"), 0 );
+
+    ::rtl::OUStringBuffer buf;
+    for ( sal_uInt32 i = 0; i < ARLEN(bytes); ++i )
+        buf.append( static_cast<sal_Int32>(bytes[ i ]), 0x10 );
+
+    return buf.makeStringAndClear();
+}
+
+//==============================================================================
+Reference<XInterface> resolveUnoURL(
+    OUString const & connectString,
+    Reference<XComponentContext> const & xLocalContext,
+    AbortChannel * abortChannel )
+{
+    Reference<bridge::XUnoUrlResolver> xUnoUrlResolver(
+        xLocalContext->getServiceManager()->createInstanceWithContext(
+            OUSTR("com.sun.star.bridge.UnoUrlResolver"), xLocalContext ),
+        UNO_QUERY_THROW );
+
+    for (;;)
+    {
+        if (abortChannel != 0 && abortChannel->isAborted())
+            throw ucb::CommandAbortedException(
+                OUSTR("abort!"), Reference<XInterface>() );
+
+        try
+        {
+            return xUnoUrlResolver->resolve( connectString );
+        }
+        catch (connection::NoConnectException &)
+        {
+            TimeValue tv = { 0 /* secs */, 500000000 /* nanosecs */ };
+            ::osl::Thread::wait( tv );
+        }
+    }
 }
 
 }
