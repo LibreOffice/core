67ef91a2e91cf58bae4d4560005cfd91edbe72d3
Rüdiger Timm <rt@openoffice.org>
2004-06-16 09:06:39 +0000

    INTEGRATION: CWS ooo20040509 (1.2.384); FILE MERGED 2004/05/06 14:04:24 waratah 1.2.384.1: Bracket pragma not valid for gcc

--- svtools/source/config/fontoptions.cxx
+++ svtools/source/config/fontoptions.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fontoptions.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-15 17:51:45 $
+ *  last change: $Author: rt $ $Date: 2004-06-16 10:06:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,8 +58,9 @@
  *
  *
  ************************************************************************/
-
+#ifndef GCC
 #pragma hdrstop
+#endif
 
 //_________________________________________________________________________________________________________________
 //  includes
