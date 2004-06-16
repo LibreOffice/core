c4a43c323610b47ecdd29fd0ec4089466817282a
Rüdiger Timm <rt@openoffice.org>
2004-06-16 09:05:05 +0000

    INTEGRATION: CWS ooo20040509 (1.3.548); FILE MERGED 2004/05/06 13:53:14 waratah 1.3.548.1: Bracket pragma not valid for gcc

--- svtools/source/brwbox/brwhead.cxx
+++ svtools/source/brwbox/brwhead.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: brwhead.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: oj $ $Date: 2002-08-19 07:20:30 $
+ *  last change: $Author: rt $ $Date: 2004-06-16 10:05:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,9 @@
 #include "brwhead.hxx"
 #include "brwbox.hxx"
 
+#ifndef GCC
 #pragma hdrstop
+#endif
 
 //===================================================================
 
