4a90763a24c4d9aa66b48816b2e7dd4ff7b73c7c
Rüdiger Timm <rt@openoffice.org>
2004-06-16 09:53:56 +0000

    INTEGRATION: CWS ooo20040509 (1.2.226); FILE MERGED 2004/05/07 00:23:04 vq 1.2.226.1: #i27968# Only use SOLARINC, remove the use of INCLUDE and SOLARINCLUDES.

--- extensions/source/activex/msidl/makefile.mk
+++ extensions/source/activex/msidl/makefile.mk
@@ -1,12 +1,12 @@
 #**************************************************************************
 #
-#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.2 2003-03-25 16:01:04 hr Exp $
+#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.3 2004-06-16 10:53:56 rt Exp $
 #
 # =========================================================================
 #
-#     $Date: 2003-03-25 16:01:04 $
-#     $Author: hr $
-#     $Revision: 1.2 $
+#     $Date: 2004-06-16 10:53:56 $
+#     $Author: rt $
+#     $Revision: 1.3 $
 #
 # =========================================================================
 #
@@ -36,10 +36,10 @@ MTL_PROJ=
 
 SOURCE=so_activex.idl
 
-MTL_SWITCHES=/tlb $(MISC)\so_activex.tlb /h $(MISC)\so_activex.h /iid $(MISC)\so_activex_i.c /dlldata $(MISC)\so_activex_dll.c /proxy $(MISC)\so_activex_p.c /Oicf
+MTL_SWITCHES=-tlb $(MISC)$/so_activex.tlb -h $(MISC)$/so_activex.h -iid $(MISC)$/so_activex_i.c -dlldata $(MISC)$/so_activex_dll.c -proxy $(MISC)$/so_activex_p.c -Oicf
 
 activex_component : $(SOURCE) 
-    $(MTL) $(MTL_SWITCHES) $(SOURCE) 
+    $(WRAPCMD) $(MTL) $(MTL_SWITCHES) $(INCLUDE) $(SOURCE) 
 
 .ENDIF
 
