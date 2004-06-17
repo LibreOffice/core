fb72fc5544ff2161b2f93499297e2384f956fbc0
Rüdiger Timm <rt@openoffice.org>
2004-06-17 14:42:00 +0000

    INTEGRATION: CWS defaultmailer (1.6.6); FILE MERGED 2004/06/08 10:32:22 obr 1.6.6.1: #i21321#,#i28378#,103347 cmdmail is now unix only and auto-detects the command line profile to use

--- shell/source/unix/exec/makefile.mk
+++ shell/source/unix/exec/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: hr $ $Date: 2004-05-10 13:06:32 $
+#   last change: $Author: rt $ $Date: 2004-06-17 15:42:00 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -76,12 +76,14 @@ COMPRDB=$(SOLARBINDIR)$/types.rdb
 
 # --- Files --------------------------------------------------------
 
+DLLPRE=
+
 SLOFILES=$(SLO)$/shellexec.obj\
     $(SLO)$/shellexecentry.obj
     
 SHL1OBJS=$(SLOFILES) 
     
-SHL1TARGET=syssh
+SHL1TARGET=syssh.uno
 SHL1IMPLIB=
 
 SHL1VERSIONMAP=exports.map
