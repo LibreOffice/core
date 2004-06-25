735ca1162e502814258be415652a27003a94206e
Hans-Joachim Lankenau <hjs@openoffice.org>
2004-06-25 15:27:59 +0000

    INTEGRATION: CWS mergebuild (1.3.64); FILE MERGED 2004/05/10 18:33:33 hjs 1.3.64.1: #i8252# lngfiles to ulffiles

--- scp2/source/xsltfilter/makefile.mk
+++ scp2/source/xsltfilter/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2004-03-09 10:14:13 $
+#   last change: $Author: hjs $ $Date: 2004-06-25 16:27:59 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -79,8 +79,8 @@ PARFILES= \
         file_xsltfilter.par  \
         starregistryitem_xsltfilter.par
 
-LNGFILES= \
-        module_xsltfilter.lng
+ULFFILES= \
+        module_xsltfilter.ulf
 
 
 # --- File ---------------------------------------------------------
