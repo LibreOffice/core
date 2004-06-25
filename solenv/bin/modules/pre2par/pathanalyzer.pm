dd2b1f31556179cdecd71c71479be3d82209bb3e
Hans-Joachim Lankenau <hjs@openoffice.org>
2004-06-25 15:10:18 +0000

    INTEGRATION: CWS mergebuild (1.2.48); FILE MERGED 2004/05/11 09:50:48 hjs 1.2.48.1: #i8252# find merged ulf files

--- solenv/bin/modules/pre2par/pathanalyzer.pm
+++ solenv/bin/modules/pre2par/pathanalyzer.pm
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: pathanalyzer.pm,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2004-01-29 11:49:16 $
+#   last change: $Author: hjs $ $Date: 2004-06-25 16:10:18 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
