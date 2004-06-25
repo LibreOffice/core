7b786e3c5ba025d2a416a5a420aa5bbd2015dd2b
Hans-Joachim Lankenau <hjs@openoffice.org>
2004-06-25 15:09:38 +0000

    INTEGRATION: CWS mergebuild (1.3.32); FILE MERGED 2004/05/18 11:47:44 hjs 1.3.32.2: RESYNC: (1.3-1.4); FILE MERGED 2004/05/05 15:32:24 hjs 1.3.32.1: #i8252# switch to iso codes and ulf format

--- solenv/bin/modules/pre2par/globals.pm
+++ solenv/bin/modules/pre2par/globals.pm
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: globals.pm,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: svesik $ $Date: 2004-04-20 12:35:57 $
+#   last change: $Author: hjs $ $Date: 2004-06-25 16:09:38 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ BEGIN
 
     $prefilename = "";
     $parfilename = "";
+    $ulffilename = "";
 
     @allitems = ("Installation", "ScpAction", "HelpText", "Directory", "DataCarrier", "StarRegistry", "File",
                  "Shortcut", "Custom", "Procedure", "Module", "ConfigurationItem", "Profile", "ProfileItem",
