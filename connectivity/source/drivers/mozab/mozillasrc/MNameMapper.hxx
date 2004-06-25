aef2171332725abe456f51af333533254a8ddd5d
Hans-Joachim Lankenau <hjs@openoffice.org>
2004-06-25 17:33:16 +0000

    INTEGRATION: CWS mozab04 (1.2.2); FILE MERGED 2004/04/12 10:15:55 windly 1.2.2.1: #i6883# make mozab driver threadsafe

--- connectivity/source/drivers/mozab/mozillasrc/MNameMapper.hxx
+++ connectivity/source/drivers/mozab/mozillasrc/MNameMapper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MNameMapper.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-03-17 10:42:36 $
+ *  last change: $Author: hjs $ $Date: 2004-06-25 18:33:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
