90a6d1b9c8db916f970e6c85a784e4121548143e
Hans-Joachim Lankenau <hjs@openoffice.org>
2004-06-25 11:36:05 +0000

    INTEGRATION: CWS mergebuild (1.7.40); FILE MERGED 2004/04/22 14:25:26 hjs 1.7.40.1: #i8252# missing libarary

--- goodies/source/filter.vcl/etiff/makefile.mk
+++ goodies/source/filter.vcl/etiff/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2003-04-15 14:10:05 $
+#   last change: $Author: hjs $ $Date: 2004-06-25 12:36:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -82,7 +82,7 @@ SLOFILES =	$(SLO)$/etiff.obj
 
 SHL1TARGET= 	eti$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB= 	etiff
-SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB)
+SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB)
 SHL1DEPN=		$(LB)$/etiff.lib
 SHL1LIBS=		$(SLB)$/etiff.lib
 
