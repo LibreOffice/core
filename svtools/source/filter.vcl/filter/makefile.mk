d1a51005fd8939283f519a1f8a4fd065d0fe9a51
Kurt Zenker <kz@openoffice.org>
2004-06-28 15:12:24 +0000

    INTEGRATION: CWS impress14ea (1.8.234); FILE MERGED 2004/06/11 16:33:40 sj 1.8.234.1: #i30102# png export dialog moved from module goodies

--- svtools/source/filter.vcl/filter/makefile.mk
+++ svtools/source/filter.vcl/filter/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:42:23 $
+#   last change: $Author: kz $ $Date: 2004-06-28 16:12:24 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -104,14 +104,16 @@ UNOTYPES=	com.sun.star.beans.PropertyValue					\
 # --- Files --------------------------------------------------------
 
 SRS1NAME=$(TARGET)
-SRC1FILES= strings.src		  \
-          dlgexpor.src		  \
+SRC1FILES= strings.src			\
+          dlgexpor.src			\
+          dlgepng.src			\
           dlgejpg.src
 
 SLOFILES= $(SLO)$/filter.obj				\
           $(SLO)$/filter2.obj				\
           $(SLO)$/dlgexpor.obj				\
           $(SLO)$/dlgejpg.obj				\
+          $(SLO)$/dlgepng.obj				\
           $(SLO)$/sgfbram.obj				\
           $(SLO)$/sgvmain.obj				\
           $(SLO)$/sgvtext.obj				\
