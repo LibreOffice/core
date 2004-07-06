4396431863d6f648869711208544af93cd447843
Oliver Bolte <obo@openoffice.org>
2004-07-06 15:55:06 +0000

    INTEGRATION: CWS docking1 (1.2.4); FILE MERGED 2004/06/01 10:55:35 cd 1.2.4.3: #i10000# Fixed merge problems 2004/05/31 04:18:11 cd 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2004/04/05 07:57:37 cd 1.2.4.1: #i26252# New internal structure to transport toolbar data

--- framework/inc/xml/toolboxconfiguration.hxx
+++ framework/inc/xml/toolboxconfiguration.hxx
@@ -10,14 +10,18 @@
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
 #endif
-#ifndef _STREAM_HXX
-#include <tools/stream.hxx>
+
+#ifndef _COM_SUN_STAR_IO_XINPUTSTREAM_HPP_
+#include <com/sun/star/io/XInputStream.hpp>
 #endif
-#ifndef _SV_TOOLBOX_HXX
-#include <vcl/toolbox.hxx>
+#ifndef _COM_SUN_STAR_IO_XOUPUTSTREAM_HPP_
+#include <com/sun/star/io/XOutputStream.hpp>
 #endif
-#ifndef _SV_TOOLBOX_HXX
-#include <vcl/toolbox.hxx>
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXCONTAINER_HPP_
+#include <com/sun/star/container/XIndexContainer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXACCESS_HPP_
+#include <com/sun/star/container/XIndexAccess.hpp>
 #endif
 
 // #110897#
@@ -28,79 +32,20 @@
 namespace framework
 {
 
-struct ToolBoxItemDescriptor
-{
-    Bitmap* pBmp;                       // Bitmap ptr not used by xml configuration
-    String  aBitmapName;                // bitmap name => use to find correct bmp file
-    String  aItemText;                  // label for this toolbox item
-    String  aURL;                       // URL command to dispatch
-    USHORT  nId;                        // internal id not used by xml configuration
-    USHORT  nItemBits;                  // properties for this toolbox item (WinBits)
-    USHORT  nItemType;                  // toolbox item type (BUTTON, SPACE, BREAK, SEPARATOR)
-    USHORT  nVisible;                   // toolbox item visible?
-    USHORT  nWidth;                     // width of a toolbox window (edit field, etc.)
-    USHORT  nUserDef;                   // user defined toolbox item (1=yes/0=no)
-    String  aHelpId;                    // A help id associated with this toolbox item
-
-    public:
-
-        ToolBoxItemDescriptor() : pBmp( 0 )
-                                 ,nId( 0 )
-                                 ,nItemBits( 0 )
-                                 ,nItemType( (USHORT)TOOLBOXITEM_SPACE )
-                                 ,nVisible( sal_True )
-                                 ,nWidth( 0 )
-                                 ,nUserDef( sal_False ) {}
-};
-
-typedef ToolBoxItemDescriptor* ToolBoxItemDescriptorPtr;
-SV_DECL_PTRARR_DEL( ToolBoxDescriptor, ToolBoxItemDescriptorPtr, 10, 2)
-
-struct ToolBoxLayoutItemDescriptor
-{
-    String                      aName;          // Unique name of the toolbox ( Objectbar, Toolbar etc. )
-    String                      aUserName;      // Userspecified name for this toolbar
-    Point                       aFloatingPos;   // Position in floating mode
-    USHORT                      nFloatingLines; // Number of lines in floating mode
-    USHORT                      nLines;         // Number of lines in docking mode
-    WindowAlign                 eAlign;         // Aligned position in docking mode
-    BOOL                        bVisible;       // Visible or not
-    BOOL                        bFloating;      // Floating mode on/off
-    ButtonType                  eType;          // text, symbol or text+symbol
-
-    ToolBoxLayoutItemDescriptor() : nFloatingLines( 0 )
-                                    ,nLines( 1 )
-                                    ,eAlign( WINDOWALIGN_LEFT )
-                                    ,bVisible( sal_False )
-                                    ,bFloating( sal_False )
-                                    ,eType( BUTTON_SYMBOL ) {}
-};
-
-typedef ToolBoxLayoutItemDescriptor* ToolBoxLayoutItemDescriptorPtr;
-SV_DECL_PTRARR_DEL( ToolBoxLayoutDescriptor, ToolBoxLayoutItemDescriptorPtr, 10, 2)
-
 class ToolBoxConfiguration
 {
     public:
         // #110897#
         static sal_Bool LoadToolBox(
             const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
-            SvStream& rInStream, ToolBoxDescriptor& aItems );
+            const ::com::sun::star::uno::Reference< ::com::sun::star::io::XInputStream >& rInputStream,
+            const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexContainer >& rToolbarConfiguration );
 
         // #110897#
         static sal_Bool StoreToolBox(
             const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
-            SvStream& rOutStream, const ToolBoxDescriptor& aItems );
-
-        // #110897#
-        static sal_Bool LoadToolBoxLayout(
-            const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
-            SvStream& rInStream, ToolBoxLayoutDescriptor& aItems );
-
-        // #110897#
-        static sal_Bool StoreToolBoxLayout(
-            const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
-            SvStream& rOutStream, ToolBoxLayoutDescriptor& aItems );
+            const ::com::sun::star::uno::Reference< ::com::sun::star::io::XOutputStream >& rOutputStream,
+            const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& rToolbarConfiguration );
 };
 
 } // namespace framework
