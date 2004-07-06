0446c57cb38232002af2f20ff30d4963c67045d0
Oliver Bolte <obo@openoffice.org>
2004-07-06 16:03:47 +0000

    INTEGRATION: CWS docking1 (1.3.20); FILE MERGED 2004/06/16 13:43:11 cd 1.3.20.1: #i30169# First implementation of an UNO based module image manager

--- framework/source/xml/imagesconfiguration.cxx
+++ framework/source/xml/imagesconfiguration.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: imagesconfiguration.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-03 13:23:14 $
+ *  last change: $Author: obo $ $Date: 2004-07-06 17:03:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -206,6 +206,78 @@ sal_Bool ImagesConfiguration::StoreImages(
     try
     {
         OWriteImagesDocumentHandler aWriteImagesDocumentHandler( aItems, xWriter );
+        aWriteImagesDocumentHandler.WriteImagesDocument();
+        return sal_True;
+    }
+    catch ( RuntimeException& )
+    {
+        return sal_False;
+    }
+    catch ( SAXException& )
+    {
+        return sal_False;
+    }
+    catch ( ::com::sun::star::io::IOException& )
+    {
+        return sal_False;
+    }
+
+    return sal_False;
+}
+
+sal_Bool ImagesConfiguration::LoadImages(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
+    const ::com::sun::star::uno::Reference< ::com::sun::star::io::XInputStream >& rInputStream,
+    ImageListsDescriptor& rItems )
+{
+    Reference< XParser > xParser( GetSaxParser( xServiceFactory ) );
+
+    // connect stream to input stream to the parser
+    InputSource aInputSource;
+
+    aInputSource.aInputStream = rInputStream;
+
+    // create namespace filter and set document handler inside to support xml namespaces
+    Reference< XDocumentHandler > xDocHandler( new OReadImagesDocumentHandler( rItems ));
+    Reference< XDocumentHandler > xFilter( new SaxNamespaceFilter( xDocHandler ));
+
+    // connect parser and filter
+    xParser->setDocumentHandler( xFilter );
+
+    try
+    {
+        xParser->parseStream( aInputSource );
+        return sal_True;
+    }
+    catch ( RuntimeException& )
+    {
+        return sal_False;
+    }
+    catch( SAXException& )
+    {
+        return sal_False;
+    }
+    catch( ::com::sun::star::io::IOException& )
+    {
+        return sal_False;
+    }
+
+    return sal_False;
+}
+
+sal_Bool ImagesConfiguration::StoreImages(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
+    const ::com::sun::star::uno::Reference< ::com::sun::star::io::XOutputStream >& rOutputStream,
+    const ImageListsDescriptor& rItems )
+{
+    Reference< XDocumentHandler > xWriter( GetSaxWriter( xServiceFactory ) );
+
+    Reference< ::com::sun::star::io::XActiveDataSource> xDataSource( xWriter , UNO_QUERY );
+    xDataSource->setOutputStream( rOutputStream );
+
+    try
+    {
+        OWriteImagesDocumentHandler aWriteImagesDocumentHandler( rItems, xWriter );
         aWriteImagesDocumentHandler.WriteImagesDocument();
         return sal_True;
     }
