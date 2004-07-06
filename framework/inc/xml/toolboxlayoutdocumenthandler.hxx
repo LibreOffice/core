18a6bba8dfe0a365af0658572971ead3e221b842
Oliver Bolte <obo@openoffice.org>
2004-07-06 15:55:50 +0000

    INTEGRATION: CWS docking1 (1.2.4); FILE MERGED 2004/04/05 07:57:37 cd 1.2.4.1: #i26252# New internal structure to transport toolbar data

--- framework/inc/xml/toolboxlayoutdocumenthandler.hxx
+++ framework/inc/xml/toolboxlayoutdocumenthandler.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: toolboxlayoutdocumenthandler.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-02-25 17:41:43 $
+ *  last change: $Author: obo $ $Date: 2004-07-06 16:55:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -103,140 +103,6 @@
 //_________________________________________________________________________________________________________________
 
 namespace framework{
-
-//*****************************************************************************************************************
-// Hash code function for using in all hash maps of follow implementation.
-
-class OReadToolBoxLayoutDocumentHandler : public ::com::sun::star::xml::sax::XDocumentHandler,
-                                          private ThreadHelpBase,   // Struct for right initalization of lock member! Must be first of baseclasses.
-                                          public ::cppu::OWeakObject
-{
-    public:
-        enum ToolBoxLayout_XML_Entry
-        {
-            TBL_ELEMENT_TOOLBARLAYOUTS,
-            TBL_ELEMENT_TOOLBARLAYOUT,
-            TBL_ELEMENT_TOOLBARCONFIGITEMS,
-            TBL_ELEMENT_TOOLBARCONFIGITEM,
-            TBL_ATTRIBUTE_ID,
-            TBL_ATTRIBUTE_TOOLBARNAME,
-            TBL_ATTRIBUTE_CONTEXT,
-            TBL_ATTRIBUTE_FLOATINGLINES,
-            TBL_ATTRIBUTE_DOCKINGLINES,
-            TBL_ATTRIBUTE_ALIGN,
-            TBL_ATTRIBUTE_FLOATING,
-            TBL_ATTRIBUTE_FLOATINGPOSLEFT,
-            TBL_ATTRIBUTE_FLOATINGPOSTOP,
-            TBL_ATTRIBUTE_VISIBLE,
-            TBL_ATTRIBUTE_STYLE,
-            TBL_ATTRIBUTE_USERDEFNAME,
-            TBL_XML_ENTRY_COUNT
-        };
-
-        enum ToolBox_XML_Namespace
-        {
-            TBL_NS_TOOLBAR,
-            TBL_XML_NAMESPACES_COUNT
-        };
-
-        OReadToolBoxLayoutDocumentHandler( ToolBoxLayoutDescriptor& aToolBoxLayoutItems );
-        virtual ~OReadToolBoxLayoutDocumentHandler();
-
-        // XInterface
-        virtual void SAL_CALL acquire() throw()
-            { OWeakObject::acquire(); }
-        virtual void SAL_CALL release() throw()
-            { OWeakObject::release(); }
-        virtual ::com::sun::star::uno::Any SAL_CALL queryInterface(
-            const ::com::sun::star::uno::Type & rType ) throw( ::com::sun::star::uno::RuntimeException );
-
-        // XDocumentHandler
-        virtual void SAL_CALL startDocument(void)
-        throw ( ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL endDocument(void)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL startElement(
-            const rtl::OUString& aName,
-            const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList > &xAttribs)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL endElement(const rtl::OUString& aName)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL characters(const rtl::OUString& aChars)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL ignorableWhitespace(const rtl::OUString& aWhitespaces)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL processingInstruction(const rtl::OUString& aTarget,
-                                                    const rtl::OUString& aData)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-        virtual void SAL_CALL setDocumentLocator(
-            const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XLocator > &xLocator)
-        throw(  ::com::sun::star::xml::sax::SAXException,
-                ::com::sun::star::uno::RuntimeException );
-
-    private:
-        ::rtl::OUString getErrorLineString();
-
-        class ToolBoxLayoutHashMap : public ::std::hash_map< ::rtl::OUString                ,
-                                                             ToolBoxLayout_XML_Entry        ,
-                                                             OUStringHashCode               ,
-                                                             ::std::equal_to< ::rtl::OUString > >
-        {
-            public:
-                inline void free()
-                {
-                    ToolBoxLayoutHashMap().swap( *this );
-                }
-        };
-
-        sal_Bool                                                                    m_bToolBarLayoutsStartFound;
-        sal_Bool                                                                    m_bToolBarLayoutsEndFound;
-        sal_Bool                                                                    m_bToolBarLayoutStartFound;
-        sal_Bool                                                                    m_bToolBarConfigListStartFound;
-        sal_Bool                                                                    m_bToolBarConfigItemStartFound;
-        ToolBoxLayoutHashMap                                                        m_aToolBoxMap;
-        ToolBoxLayoutDescriptor&                                                    m_aToolBoxItems;
-        ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XLocator >    m_xLocator;
-};
-
-class OWriteToolBoxLayoutDocumentHandler : private ThreadHelpBase // Struct for right initalization of lock member! Must be first of baseclasses.
-{
-    public:
-        OWriteToolBoxLayoutDocumentHandler(
-            const ToolBoxLayoutDescriptor& aToolBoxLayoutItems,
-            ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XDocumentHandler > );
-        virtual ~OWriteToolBoxLayoutDocumentHandler();
-
-        void WriteToolBoxLayoutDocument() throw
-            ( ::com::sun::star::xml::sax::SAXException,
-              ::com::sun::star::uno::RuntimeException );
-
-    protected:
-        virtual void WriteToolBoxLayoutItem( const ToolBoxLayoutItemDescriptor* ) throw
-            ( ::com::sun::star::xml::sax::SAXException,
-              ::com::sun::star::uno::RuntimeException );
-
-        const ToolBoxLayoutDescriptor&                                                      m_aToolBoxLayoutItems;
-        ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XDocumentHandler >    m_xWriteDocumentHandler;
-        ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >      m_xEmptyList;
-        ::rtl::OUString                                                                     m_aXMLToolbarNS;
-        ::rtl::OUString                                                                     m_aAttributeType;
-        ::rtl::OUString                                                                     m_aTrueValue;
-        ::rtl::OUString                                                                     m_aFalseValue;
-};
 
 } // namespace framework
 
