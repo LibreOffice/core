6a8f6d23dce92cb026752861a4c91fdff2a559c7
Oliver Bolte <obo@openoffice.org>
2004-07-06 16:02:18 +0000

    INTEGRATION: CWS docking1 (1.1.2); FILE ADDED 2004/04/23 11:57:29 cd 1.1.2.4: #i25238# Changes to the UI options must be respected 2004/04/07 09:43:49 cd 1.1.2.3: #i25238# Better docking according to toolbar specification 2004/04/05 08:03:54 cd 1.1.2.2: #i24937# Programmatic control of toolbar items 2004/03/08 06:42:20 cd 1.1.2.1: #i24937# Programmatic control of toolbar items

---
+++ framework/source/uielement/toolbarwrapper.cxx
@@ -0,0 +1,360 @@
+/*************************************************************************
+ *
+ *  $RCSfile: toolbarwrapper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-07-06 17:02:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+
+#ifndef __FRAMEWORK_UIELEMENT_TOOLBARWRAPPER_HXX_
+#include <uielement/toolbarwrapper.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_RESETABLEGUARD_HXX_
+#include <threadhelp/resetableguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_HELPER_ACTIONTRIGGERHELPER_HXX_
+#include <helper/actiontriggerhelper.hxx>
+#endif
+
+#ifndef __FRAMEWORK_UIELEMENT_CONSTITEMCONTAINER_HXX_
+#include <uielement/constitemcontainer.hxx>
+#endif
+
+#ifndef __FRAMEWORK_UIELEMENT_ROOTITEMCONTAINER_HXX_
+#include <uielement/rootitemcontainer.hxx>
+#endif
+
+#ifndef __FRAMEWORK_UIELEMENT_TOOLBARMANAGER_HXX_
+#include <uielement/toolbarmanager.hxx>
+#endif
+
+#ifndef __FRAMEWORK_UIELEMENT_TOOLBARW_HXX_
+#include <uielement/toolbar.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_AWT_XSYSTEMDEPENDENTMENUPEER_HPP_
+#include <com/sun/star/awt/XSystemDependentMenuPeer.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_AWT_XMENUBAR_HPP_
+#include <com/sun/star/awt/XMenuBar.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXCONTAINER_HPP_
+#include <com/sun/star/container/XIndexContainer.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMEACCESS_HPP_
+#include <com/sun/star/container/XNameAccess.hpp>
+#endif
+
+#ifndef _DRAFTS_COM_SUN_STAR_UI_UIELEMENTTYPE_HPP_
+#include <drafts/com/sun/star/ui/UIElementType.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
+#include <toolkit/unohlp.hxx>
+#endif
+#ifndef _TOOLKIT_AWT_VCLXWINDOW_HXX_
+#include <toolkit/awt/vclxwindow.hxx>
+#endif
+#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
+#include <comphelper/processfactory.hxx>
+#endif
+
+#include <svtools/miscopt.hxx>
+#include <vcl/svapp.hxx>
+#include <vcl/toolbox.hxx>
+#include <rtl/logfile.hxx>
+
+using namespace rtl;
+using namespace com::sun::star::uno;
+using namespace com::sun::star::beans;
+using namespace com::sun::star::frame;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::container;
+using namespace com::sun::star::awt;
+using namespace drafts::com::sun::star::ui;
+
+namespace framework
+{
+
+ToolBarWrapper::ToolBarWrapper( const Reference< XMultiServiceFactory >& xServiceManager ) :
+    UIConfigElementWrapperBase( UIElementType::TOOLBAR ),
+    m_xServiceManager( xServiceManager )
+{
+}
+
+ToolBarWrapper::~ToolBarWrapper()
+{
+}
+
+// XComponent
+void SAL_CALL ToolBarWrapper::dispose() throw ( RuntimeException )
+{
+    Reference< XComponent > xThis( static_cast< OWeakObject* >(this), UNO_QUERY );
+
+    com::sun::star::lang::EventObject aEvent( xThis );
+    m_aListenerContainer.disposeAndClear( aEvent );
+
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_xToolBarManager.is() )
+        m_xToolBarManager->dispose();
+    m_xToolBarManager.clear();
+    m_xConfigSource.clear();
+    m_xConfigData.clear();
+    m_xToolBarWindow.clear();
+
+    m_bDisposed = sal_True;
+}
+
+// XInitialization
+void SAL_CALL ToolBarWrapper::initialize( const Sequence< Any >& aArguments ) throw ( Exception, RuntimeException )
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    if ( !m_bInitialized )
+    {
+        UIConfigElementWrapperBase::initialize( aArguments );
+
+        Reference< XFrame > xFrame( m_xWeakFrame );
+        if ( xFrame.is() && m_xConfigSource.is() )
+        {
+            // Create VCL based toolbar which will be filled with settings data
+            ToolBar* pToolBar = 0;
+            ToolBarManager* pToolBarManager = 0;
+            {
+                vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+                Window* pWindow = VCLUnoHelper::GetWindow( xFrame->getContainerWindow() );
+                if ( pWindow )
+                {
+                    ULONG nStyles = WB_LINESPACING | WB_BORDER | WB_SCROLL | WB_MOVEABLE | WB_3DLOOK | WB_DOCKABLE | WB_SIZEABLE | WB_CLOSEABLE;
+
+                    pToolBar = new ToolBar( pWindow, nStyles );
+                    m_xToolBarWindow = VCLUnoHelper::GetInterface( pToolBar );
+                    pToolBarManager = new ToolBarManager( m_xServiceManager, xFrame, m_aResourceURL, pToolBar );
+                    pToolBar->SetToolBarManager( pToolBarManager );
+                    m_xToolBarManager = Reference< XComponent >( static_cast< OWeakObject *>( pToolBarManager ), UNO_QUERY );
+                }
+            }
+
+            try
+            {
+                m_xConfigData = m_xConfigSource->getSettings( m_aResourceURL, sal_False );
+                if ( m_xConfigData.is() && pToolBar && pToolBarManager )
+                {
+                    // Fill toolbar with container contents
+                    pToolBarManager->FillToolbar( m_xConfigData );
+                    pToolBar->SetOutStyle( SvtMiscOptions().GetToolboxStyle() );
+                    pToolBar->EnableCustomize( TRUE );
+                    ::Size aActSize( pToolBar->GetSizePixel() );
+                    ::Size aSize( pToolBar->CalcWindowSizePixel() );
+                    aSize.Width() = aActSize.Width();
+                    pToolBar->SetSizePixel( aSize );
+                }
+            }
+            catch ( NoSuchElementException& )
+            {
+            }
+        }
+    }
+}
+
+// XUpdatable
+void SAL_CALL ToolBarWrapper::update() throw (::com::sun::star::uno::RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    ToolBarManager* pToolBarManager = static_cast< ToolBarManager *>( m_xToolBarManager.get() );
+    if ( pToolBarManager )
+        pToolBarManager->CheckAndUpdateImages();
+}
+
+// XUIElementSettings
+void SAL_CALL ToolBarWrapper::updateSettings() throw (::com::sun::star::uno::RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    if ( m_bPersistent &&
+         m_xConfigSource.is() &&
+         m_xToolBarManager.is() )
+    {
+        try
+        {
+            ToolBarManager* pToolBarManager = static_cast< ToolBarManager *>( m_xToolBarManager.get() );
+
+            m_xConfigData = m_xConfigSource->getSettings( m_aResourceURL, sal_False );
+            if ( m_xConfigData.is() )
+                pToolBarManager->FillToolbar( m_xConfigData );
+        }
+        catch ( NoSuchElementException& )
+        {
+        }
+    }
+}
+
+Reference< XIndexAccess > SAL_CALL ToolBarWrapper::getSettings( sal_Bool bWriteable ) throw (::com::sun::star::uno::RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    if ( bWriteable )
+        return Reference< XIndexAccess >( static_cast< OWeakObject * >( new RootItemContainer( m_xConfigData ) ), UNO_QUERY );
+    else
+        return m_xConfigData;
+}
+
+void SAL_CALL ToolBarWrapper::setSettings( const ::com::sun::star::uno::Reference< ::com::sun::star::container::XIndexAccess >& xSettings ) throw (::com::sun::star::uno::RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    if ( xSettings.is() )
+    {
+        // Create a copy of the data if the container is not const
+        Reference< XIndexReplace > xReplace( xSettings, UNO_QUERY );
+        if ( xReplace.is() )
+            m_xConfigData = Reference< XIndexAccess >( static_cast< OWeakObject * >( new ConstItemContainer( xSettings ) ), UNO_QUERY );
+        else
+            m_xConfigData = xSettings;
+
+        if ( m_xConfigSource.is() && m_bPersistent )
+        {
+            OUString aResourceURL( m_aResourceURL );
+            Reference< XUIConfigurationManager > xUICfgMgr( m_xConfigSource );
+
+            aLock.unlock();
+
+            try
+            {
+                xUICfgMgr->replaceSettings( aResourceURL, m_xConfigData );
+            }
+            catch( NoSuchElementException& )
+            {
+            }
+        }
+    }
+}
+
+//  XUIConfigurationListener
+void SAL_CALL ToolBarWrapper::elementInserted( const ::drafts::com::sun::star::ui::ConfigurationEvent& Event ) throw (::com::sun::star::uno::RuntimeException)
+{
+}
+
+void SAL_CALL ToolBarWrapper::elementRemoved( const ::drafts::com::sun::star::ui::ConfigurationEvent& Event ) throw (::com::sun::star::uno::RuntimeException)
+{
+}
+
+void SAL_CALL ToolBarWrapper::elementReplaced( const ::drafts::com::sun::star::ui::ConfigurationEvent& Event ) throw (::com::sun::star::uno::RuntimeException)
+{
+}
+
+// XUIElement interface
+Reference< XInterface > SAL_CALL ToolBarWrapper::getRealInterface(  ) throw (::com::sun::star::uno::RuntimeException)
+{
+    ResetableGuard aLock( m_aLock );
+
+    if ( m_xToolBarManager.is() )
+    {
+        ToolBarManager* pToolBarManager = static_cast< ToolBarManager *>( m_xToolBarManager.get() );
+        if ( pToolBarManager )
+        {
+            Window* pWindow = (Window *)pToolBarManager->GetToolBar();
+            return VCLUnoHelper::GetInterface( pWindow );
+        }
+    }
+
+    return Reference< XInterface >();
+}
+
+} // namespace framework
