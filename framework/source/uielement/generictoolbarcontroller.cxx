4767520965fab9d9ab193e4a7ee43e1f009da103
Oliver Bolte <obo@openoffice.org>
2004-07-06 16:00:58 +0000

    INTEGRATION: CWS docking1 (1.1.2); FILE ADDED 2004/05/27 13:15:32 cd 1.1.2.4: #i25238# New shrink algorithm for docked toolbars 2004/04/21 07:06:21 cd 1.1.2.3: #i26252# Additional work on generic toolbar controller 2004/04/05 08:03:54 cd 1.1.2.2: #i24937# Programmatic control of toolbar items 2004/03/08 06:48:34 cd 1.1.2.1: #i24937# Programmatic control of toolbar items

---
+++ framework/source/uielement/generictoolbarcontroller.cxx
@@ -0,0 +1,193 @@
+/*************************************************************************
+ *
+ *  $RCSfile: generictoolbarcontroller.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-07-06 17:00:58 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_UIELEMENT_GENERICTOOLBARCONTROLLER_HXX
+#include "uielement/generictoolbarcontroller.hxx"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_TOOLBAR_HXX_
+#include "uielement/toolbar.hxx"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_UTIL_XURLTRANSFORMER_HPP_
+#include <com/sun/star/util/XURLTransformer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDER_HPP_
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
+#include <com/sun/star/lang/DisposedException.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_STATUS_ITEMSTATUS_HPP_
+#include <com/sun/star/frame/status/ItemStatus.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_STATUS_ITEMSTATE_HPP_
+#include <com/sun/star/frame/status/ItemState.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _SVTOOLS_TOOLBOXCONTROLLER_HXX
+#include <svtools/toolboxcontroller.hxx>
+#endif
+#ifndef _VOS_MUTEX_HXX_
+#include <vos/mutex.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+
+using namespace ::rtl;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::frame;
+using namespace ::com::sun::star::frame::status;
+using namespace ::com::sun::star::util;
+
+namespace framework
+{
+
+GenericToolbarController::GenericToolbarController( const Reference< XMultiServiceFactory >& rServiceManager,
+                                                    const Reference< XFrame >&               rFrame,
+                                                    ToolBar*                                 pToolbar,
+                                                    USHORT                                   nID,
+                                                    const OUString&                          aCommand ) :
+    svt::ToolboxController( rServiceManager, rFrame, aCommand )
+    ,   m_pToolbar( pToolbar )
+    ,   m_nID( nID )
+{
+}
+
+GenericToolbarController::~GenericToolbarController()
+{
+}
+
+void SAL_CALL GenericToolbarController::dispose()
+throw ( RuntimeException )
+{
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+    svt::ToolboxController::dispose();
+
+    m_pToolbar = 0;
+    m_nID = 0;
+}
+
+void GenericToolbarController::statusChanged( const FeatureStateEvent& Event )
+throw ( RuntimeException )
+{
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+    if ( m_bDisposed )
+        return;
+
+    if ( m_pToolbar )
+    {
+        m_pToolbar->EnableItem( m_nID, Event.IsEnabled );
+
+        USHORT nItemBits = m_pToolbar->GetItemBits( m_nID );
+        nItemBits &= ~TIB_CHECKABLE;
+        TriState eTri = STATE_NOCHECK;
+
+        sal_Bool        bValue;
+        rtl::OUString   aStrValue;
+        ItemStatus      aItemState;
+
+        if ( Event.State >>= bValue )
+        {
+            // Boolean, treat it as checked/unchecked
+            m_pToolbar->SetItemBits( m_nID, nItemBits );
+            m_pToolbar->CheckItem( m_nID, bValue );
+            if ( bValue )
+                eTri = STATE_CHECK;
+            nItemBits |= TIB_CHECKABLE;
+        }
+        else if ( Event.State >>= aStrValue )
+        {
+            m_pToolbar->SetItemText( m_nID, aStrValue );
+        }
+        else if ( Event.State >>= aItemState )
+        {
+            eTri = STATE_DONTKNOW;
+            nItemBits |= TIB_CHECKABLE;
+        }
+
+        m_pToolbar->SetItemState( m_nID, eTri );
+        m_pToolbar->SetItemBits( m_nID, nItemBits );
+    }
+}
+
+} // namespace
