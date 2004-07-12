3c71d91249334f2ec540b7fe5d4957e2fd7b58c4
Rüdiger Timm <rt@openoffice.org>
2004-07-12 13:30:58 +0000

    INTEGRATION: CWS aw013 (1.3.194); FILE MERGED 2004/06/28 14:54:34 aw 1.3.194.1: #117254#

--- svx/inc/svx/sdr/contact/viewcontactofsdrpage.hxx
+++ svx/inc/svx/sdr/contact/viewcontactofsdrpage.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewcontactofsdrpage.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2004-02-26 17:45:41 $
+ *  last change: $Author: rt $ $Date: 2004-07-12 14:30:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,7 +72,6 @@
 
 //////////////////////////////////////////////////////////////////////////////
 // predeclarations
-
 class SdrPage;
 
 //////////////////////////////////////////////////////////////////////////////
