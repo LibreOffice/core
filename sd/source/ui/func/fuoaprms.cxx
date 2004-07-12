dc4937ab1e1607a6612ede808bdd9bcb73c2a621
Rüdiger Timm <rt@openoffice.org>
2004-07-12 14:04:21 +0000

    INTEGRATION: CWS aw013 (1.5.36); FILE MERGED 2004/06/24 09:33:11 aw 1.5.36.1: #i29181#

--- sd/source/ui/func/fuoaprms.cxx
+++ sd/source/ui/func/fuoaprms.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fuoaprms.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 15:47:26 $
+ *  last change: $Author: rt $ $Date: 2004-07-12 15:04:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -145,7 +145,7 @@ FuObjectAnimationParameters::FuObjectAnimationParameters (
     SfxUndoManager* pUndoMgr = pViewSh->GetViewFrame()->GetObjectShell()->
                                 GetUndoManager();
 
-    const SdrMarkList& rMarkList  = pView->GetMarkList();
+    const SdrMarkList& rMarkList  = pView->GetMarkedObjectList();
     ULONG        nCount     = rMarkList.GetMarkCount();
     ULONG        nObject    = 0;
 
