3c56fd89cc74a60518e28bdcd0df6243d5a54d5d
Rüdiger Timm <rt@openoffice.org>
2004-07-12 12:05:26 +0000

    INTEGRATION: CWS jl9 (1.1.2); FILE ADDED 2004/06/21 15:02:00 jl 1.1.2.1: #i20317#

---
+++ cli_ure/source/native/assembly.cxx
@@ -0,0 +1,6 @@
+#using <mscorlib.dll>
+
+[assembly:System::Reflection::AssemblyProduct( "CLI-UNO Language Binding" )];
+[assembly:System::Reflection::AssemblyDescription( "CLI-UNO Helper Library" )];
+[assembly:System::Reflection::AssemblyDelaySign(false)];
+[assembly:System::Reflection::AssemblyCompany( "OpenOffice.org" )];
