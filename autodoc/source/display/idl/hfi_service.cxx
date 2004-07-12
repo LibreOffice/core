c47643d5c7a480a7cd9891d845084ad8896e0f74
Rüdiger Timm <rt@openoffice.org>
2004-07-12 14:28:08 +0000

    INTEGRATION: CWS adc8 (1.2.66); FILE MERGED 2004/07/06 14:04:27 np 1.2.66.2: #i25608# 2004/07/02 10:52:21 np 1.2.66.1: #116899#

--- autodoc/source/display/idl/hfi_service.cxx
+++ autodoc/source/display/idl/hfi_service.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hfi_service.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-18 14:11:38 $
+ *  last change: $Author: rt $ $Date: 2004-07-12 15:28:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,6 +66,7 @@
 
 // NOT FULLY DEFINED SERVICES
 #include <ary/idl/i_ce.hxx>
+#include <ary/idl/ik_property.hxx>
 #include <ary/idl/ik_service.hxx>
 #include <ary_i/codeinf2.hxx>
 #include <toolkit/hf_docentry.hxx>
@@ -130,7 +131,7 @@ HF_IdlService::~HF_IdlService()
 
 typedef ::ary::idl::ifc_service::attr
     ServiceAttr;
-typedef ::ary::Dyn_StdConstIterator< ::ary::idl::CommentedReference >
+typedef ::ary::Dyn_StdConstIterator< ::ary::idl::CommentedRelation >
     dyn_comref_list;
 
 void
@@ -155,7 +156,7 @@ HF_IdlService::Produce_byData( const client & i_ce ) const
     dyn_comref_list
         dpIncludedServices;
     ServiceAttr::Get_IncludedServices(dpIncludedServices, i_ce);
-    if ( BOOL_OF(*dpIncludedServices) )
+    if ( (*dpIncludedServices).operator bool() )
     {
         produce_IncludedServices( i_ce, *dpIncludedServices );
         pNaviSubRow->SwitchOn(sli_IncludedServices);
@@ -164,7 +165,7 @@ HF_IdlService::Produce_byData( const client & i_ce ) const
     dyn_comref_list
         dpExportedInterfaces;
     ServiceAttr::Get_ExportedInterfaces(dpExportedInterfaces, i_ce);
-    if ( BOOL_OF(*dpExportedInterfaces) )
+    if ( (*dpExportedInterfaces).operator bool() )
     {
         produce_ExportedInterfaces( i_ce, *dpExportedInterfaces );
         pNaviSubRow->SwitchOn(sli_ExportedInterfaces);
@@ -173,7 +174,7 @@ HF_IdlService::Produce_byData( const client & i_ce ) const
     dyn_ce_list
         dpProperties;
     ServiceAttr::Get_Properties(dpProperties, i_ce);
-    if ( BOOL_OF(*dpProperties) )
+    if ( (*dpProperties).operator bool() )
     {
         produce_Members( *dpProperties,
                          C_sList_Properties,
@@ -186,6 +187,54 @@ HF_IdlService::Produce_byData( const client & i_ce ) const
 
     pNaviSubRow->Produce_Row();
     CurOut() << new Xml::XmlCode("<br>&nbsp;");
+}
+
+typedef ary::idl::ifc_property::attr    PropertyAttr;
+
+void
+HF_IdlService::produce_SummaryDeclaration( Xml::Element &      o_row,
+                                           const client &      i_property ) const
+{
+    // KORR
+    // Put this in to HF_IdlProperty!
+
+    Xml::Element &
+        rCell = o_row
+                    >> *new Html::TableCell
+                        << new Html::ClassAttr( C_sCellStyle_SummaryLeft );
+
+    if (PropertyAttr::HasAnyStereotype(i_property))
+    {
+        rCell << "[ ";
+        if (PropertyAttr::IsReadOnly(i_property))
+            rCell << "readonly ";
+        if (PropertyAttr::IsBound(i_property))
+            rCell << "bound ";
+        if (PropertyAttr::IsConstrained(i_property))
+            rCell << "constrained ";
+        if (PropertyAttr::IsMayBeAmbiguous(i_property))
+            rCell << "maybeambiguous ";
+        if (PropertyAttr::IsMayBeDefault(i_property))
+            rCell << "maybedefault ";
+        if (PropertyAttr::IsMayBeVoid(i_property))
+            rCell << "maybevoid ";
+        if (PropertyAttr::IsRemovable(i_property))
+            rCell << "removable ";
+        if (PropertyAttr::IsTransient(i_property))
+            rCell << "transient ";
+        rCell << "] ";
+    }   // end if
+
+    HF_IdlTypeText
+        aType( Env(), rCell, true );
+    aType.Produce_byData( PropertyAttr::Type(i_property) );
+
+    StreamLock aLocalLink(100);
+    aLocalLink() << "#" << i_property.LocalName();
+    rCell
+        << new Html::LineBreak
+        >> *new Html::Link( aLocalLink().c_str() )
+            << i_property.LocalName();
 }
 
 DYN HF_NaviSubRow &
@@ -218,11 +267,11 @@ HF_IdlService::produce_IncludedServices( const client & i_ce,
                 C_sList_IncludedServices,
                 2 );
 
-    for ( ; BOOL_OF(it_list); ++it_list )
+    for ( ; it_list.operator bool(); ++it_list )
     {
         Xml::Element &
             rRow = aTable.Add_Row();
-        produce_Link(rRow, (*it_list).first);
+        produce_Link(rRow, (*it_list).Type());
         produce_LinkDoc(i_ce, rRow, it_list);
     }   // end for
 }
@@ -243,7 +292,7 @@ HF_IdlService::produce_ExportedInterfaces( const client &   i_ce,
     {
         Xml::Element &
             rRow = aTable.Add_Row();
-        produce_Link(rRow, (*it_list).first);
+        produce_Link(rRow, (*it_list).Type());
         produce_LinkDoc(i_ce, rRow, it_list);
     }   // end for
 }
@@ -271,39 +320,35 @@ HF_IdlService::produce_LinkDoc( const client &     i_ce,
                 >> *new Html::TableCell
                     << new Html::ClassAttr(C_sCellStyle_SummaryRight);
 
-
     HF_DocEntryList
         aDocList(rCell);
-    if ( (*i_commentedRef).second != 0 )
+
+    if ( (*i_commentedRef).Info() != 0 )
     {
+//      aDocList.Produce_Term("Comment on Reference");
+
         HF_IdlDocu
             aDocuDisplay(Env(), aDocList);
-        aDocuDisplay.Produce_byData(i_ce, (*i_commentedRef).second );
+        aDocuDisplay.Produce_byDocu4Reference(*(*i_commentedRef).Info(), i_ce);
     }
     else
     {
         const client *
-            pCe = Env().Linker().Search_CeFromType((*i_commentedRef).first);
+            pCe = Env().Linker().Search_CeFromType((*i_commentedRef).Type());
         const ce_info *
             pShort = pCe != 0
                         ?   pCe->Docu()
                         :   0;
         if ( pShort != 0 )
         {
-            if (pShort->IsDeprecated())
-            {
-                rCell << "[ DEPRECATED ]" << new Html::LineBreak;
-            }
-            if (pShort->IsOptional())
-            {
-                rCell << "[ OPTIONAL ]" << new Html::LineBreak;
-            }
-
+            aDocList.Produce_NormalTerm("(referenced entity's summary:)");
+            Xml::Element &
+                rDef = aDocList.Produce_Definition();
             HF_IdlDocuTextDisplay
-                aShortDisplay( Env(), &rCell, *pCe);
+                aShortDisplay( Env(), &rDef, *pCe);
             pShort->Short().DisplayAt(aShortDisplay);
-        }
-    }   // endif ( (*i_commentedRef).second != 0 ) else
+        }   // end if (pShort != 0)
+    }   // endif ( (*i_commentedRef).Info() != 0 ) else
 }
 
 
