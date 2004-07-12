43626af12b936596d138a1d07c8337ce9ca599a6
Rüdiger Timm <rt@openoffice.org>
2004-07-12 14:43:07 +0000

    INTEGRATION: CWS adc8 (1.2.54); FILE MERGED 2004/07/02 11:04:18 np 1.2.54.1: #i26261#

--- autodoc/source/parser_i/idoc/cx_dsapi.cxx
+++ autodoc/source/parser_i/idoc/cx_dsapi.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cx_dsapi.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-06-10 11:34:58 $
+ *  last change: $Author: rt $ $Date: 2004-07-12 15:43:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
