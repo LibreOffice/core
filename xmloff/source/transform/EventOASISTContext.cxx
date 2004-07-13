ee4ddf5f2ba8860e0483e4d7fc348a01f72ee526
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:48:41 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/06/03 09:04:31 mib 1.1.2.2: - #i20153#: draw/impress bug fixes 2004/06/02 10:55:57 mib 1.1.2.1: - #i20153#: continued events

---
+++ xmloff/source/transform/EventOASISTContext.cxx
@@ -0,0 +1,270 @@
+/*************************************************************************
+ *
+ *  $RCSfile: EventOASISTContext.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:48:41 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_EVENTOASISTCONTEXT_HXX
+#include "EventOASISTContext.hxx"
+#endif
+#ifndef _XMLOFF_EVENTMAP_HXX
+#include "EventMap.hxx"
+#endif
+#ifndef _XMLOFF_MUTABLEATTRLIST_HXX
+#include "MutableAttrList.hxx"
+#endif
+#ifndef _XMLOFF_XMLNMSPE_HXX
+#include "xmlnmspe.hxx"
+#endif
+#ifndef _XMLOFF_ACTIONMAPTYPESOASIS_HXX
+#include "ActionMapTypesOASIS.hxx"
+#endif
+#ifndef _XMLOFF_ATTRTRANSFORMERACTION_HXX
+#include "AttrTransformerAction.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERACTIONS_HXX
+#include "TransformerActions.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERBASE_HXX
+#include "TransformerBase.hxx"
+#endif
+
+#include <hash_map>
+
+using namespace ::rtl;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+using namespace ::xmloff::token;
+
+class XMLTransformerOASISEventMap_Impl:
+    public ::std::hash_map< NameKey_Impl, ::rtl::OUString,
+                            NameHash_Impl, NameHash_Impl >
+{
+public:
+    XMLTransformerOASISEventMap_Impl( XMLTransformerEventMapEntry *pInit );
+    ~XMLTransformerOASISEventMap_Impl();
+};
+
+XMLTransformerOASISEventMap_Impl::XMLTransformerOASISEventMap_Impl( XMLTransformerEventMapEntry *pInit )
+{
+    if( pInit )
+    {
+        XMLTransformerOASISEventMap_Impl::key_type aKey;
+        XMLTransformerOASISEventMap_Impl::data_type aData;
+        while( pInit->m_pOASISName )
+        {
+            aKey.m_nPrefix = pInit->m_nOASISPrefix;
+            aKey.m_aLocalName = OUString::createFromAscii(pInit->m_pOASISName);
+
+            OSL_ENSURE( find( aKey ) == end(), "duplicate event map entry" );
+
+            aData = OUString::createFromAscii(pInit->m_pOOoName);
+
+            XMLTransformerOASISEventMap_Impl::value_type aVal( aKey, aData );
+
+            insert( aVal );
+            ++pInit;
+        }
+    }
+}
+
+XMLTransformerOASISEventMap_Impl::~XMLTransformerOASISEventMap_Impl()
+{
+}
+
+// -----------------------------------------------------------------------------
+
+TYPEINIT1( XMLEventOASISTransformerContext, XMLRenameElemTransformerContext);
+
+XMLEventOASISTransformerContext::XMLEventOASISTransformerContext(
+        XMLTransformerBase& rImp,
+        const OUString& rQName ) :
+    XMLRenameElemTransformerContext( rImp, rQName,
+         rImp.GetNamespaceMap().GetKeyByAttrName( rQName ), XML_EVENT )
+{
+}
+
+XMLEventOASISTransformerContext::~XMLEventOASISTransformerContext()
+{
+}
+
+XMLTransformerOASISEventMap_Impl
+    *XMLEventOASISTransformerContext::CreateEventMap()
+{
+    return new XMLTransformerOASISEventMap_Impl( aTransformerEventMap );
+}
+
+void XMLEventOASISTransformerContext::FlushEventMap(
+        XMLTransformerOASISEventMap_Impl *p )
+{
+    delete p;
+}
+
+OUString XMLEventOASISTransformerContext::GetEventName(
+        sal_uInt16 nPrefix,
+        const OUString& rName,
+           XMLTransformerOASISEventMap_Impl& rMap )
+{
+    XMLTransformerOASISEventMap_Impl::key_type aKey( nPrefix, rName );
+    XMLTransformerOASISEventMap_Impl::const_iterator aIter = rMap.find( aKey );
+    if( aIter == rMap.end() )
+        return rName;
+    else
+        return (*aIter).second;
+}
+
+
+void XMLEventOASISTransformerContext::StartElement(
+    const Reference< XAttributeList >& rAttrList )
+{
+    XMLTransformerActions *pActions =
+        GetTransformer().GetUserDefinedActions( OASIS_EVENT_ACTIONS );
+    OSL_ENSURE( pActions, "go no actions" );
+
+    Reference< XAttributeList > xAttrList( rAttrList );
+    XMLMutableAttributeList *pMutableAttrList = 0;
+    sal_Int16 nAttrCount = xAttrList.is() ? xAttrList->getLength() : 0;
+    for( sal_Int16 i=0; i < nAttrCount; i++ )
+    {
+        const OUString& rAttrName = xAttrList->getNameByIndex( i );
+        OUString aLocalName;
+        sal_uInt16 nPrefix =
+            GetTransformer().GetNamespaceMap().GetKeyByAttrName( rAttrName,
+                                                                 &aLocalName );
+        XMLTransformerActions::key_type aKey( nPrefix, aLocalName );
+        XMLTransformerActions::const_iterator aIter =
+            pActions->find( aKey );
+        if( !(aIter == pActions->end() ) )
+        {
+            if( !pMutableAttrList )
+            {
+                pMutableAttrList =
+                        new XMLMutableAttributeList( xAttrList );
+                xAttrList = pMutableAttrList;
+            }
+            const OUString& rAttrValue = xAttrList->getValueByIndex( i );
+            switch( (*aIter).second.m_nActionType )
+            {
+            case XML_ATACTION_HREF:
+                // TODO
+                break;
+            case XML_ATACTION_EVENT_NAME:
+                pMutableAttrList->SetValueByIndex( i,
+                               GetTransformer().GetEventName( rAttrValue ) );
+                break;
+            case XML_ATACTION_REMOVE_NAMESPACE_PREFIX:
+                {
+                    OUString aAttrValue( rAttrValue );
+                    sal_uInt16 nValPrefix =
+                        static_cast<sal_uInt16>((*aIter).second.m_nParam1);
+                    if( GetTransformer().RemoveNamespacePrefix(
+                                aAttrValue, nValPrefix ) )
+                        pMutableAttrList->SetValueByIndex( i, aAttrValue );
+                }
+                break;
+            case XML_ATACTION_MACRO_NAME:
+                {
+                    const OUString& rApp = GetXMLToken( XML_APPLICATION );
+                    const OUString& rDoc = GetXMLToken( XML_DOCUMENT );
+                    OUString aLocation;
+                    OUString aAttrValue;
+                    if( rAttrValue.getLength() > rApp.getLength()+1 &&
+                        rAttrValue.copy(0,rApp.getLength()).
+                                    equalsIgnoreAsciiCase( rApp ) &&
+                        ':' == rAttrValue[rApp.getLength()] )
+                    {
+                        aLocation = rApp;
+                        aAttrValue = rAttrValue.copy( rApp.getLength()+1 );
+                    }
+                    else if( rAttrValue.getLength() > rDoc.getLength()+1 &&
+                             rAttrValue.copy(0,rDoc.getLength()).
+                                     equalsIgnoreAsciiCase( rDoc ) &&
+                        ':' == rAttrValue[rDoc.getLength()] )
+                    {
+                        aLocation= rDoc;
+                        aAttrValue = rAttrValue.copy( rDoc.getLength()+1 );
+                    }
+                    if( aAttrValue.getLength() )
+                        pMutableAttrList->SetValueByIndex( i,
+                                            aAttrValue );
+                    if( aLocation.getLength() )
+                    {
+                        OUString aAttrQName( GetTransformer().GetNamespaceMap().
+                                GetQNameByKey( XML_NAMESPACE_SCRIPT,
+                            ::xmloff::token::GetXMLToken( XML_LOCATION ) ) );
+                        pMutableAttrList->AddAttribute( aAttrQName, aLocation );
+                        // draw bug
+                        aAttrQName = GetTransformer().GetNamespaceMap().
+                                GetQNameByKey( XML_NAMESPACE_SCRIPT,
+                            ::xmloff::token::GetXMLToken( XML_LIBRARY ) );
+                        pMutableAttrList->AddAttribute( aAttrQName, aLocation );
+                    }
+
+                }
+                break;
+            case XML_ATACTION_COPY:
+                break;
+            default:
+                OSL_ENSURE( !this, "unknown action" );
+                break;
+            }
+        }
+    }
+
+    XMLRenameElemTransformerContext::StartElement( xAttrList );
+}
