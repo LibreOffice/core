c0582f3277d4f306e8b43a754297aec5bd113fb6
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:36:48 +0000

    INTEGRATION: CWS oasis (1.17.248); FILE MERGED 2004/05/18 06:48:17 mib 1.17.248.1: - #i20153#: added <draw:frame>

--- xmloff/source/text/XMLTextFrameContext.hxx
+++ xmloff/source/text/XMLTextFrameContext.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLTextFrameContext.hxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2004-05-18 15:04:50 $
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:36:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,10 +62,6 @@
 #ifndef _XMLTEXTFRAMECONTEXT_HXX
 #define _XMLTEXTFRAMECONTEXT_HXX
 
-#ifndef __SGI_STL_MAP
-#include <map>
-#endif
-
 #ifndef _COM_SUN_STAR_TEXT_TEXTCONTENTANCHORTYPE_HPP
 #include <com/sun/star/text/TextContentAnchorType.hpp>
 #endif
@@ -74,118 +70,31 @@
 #include "xmlictxt.hxx"
 #endif
 
-#if SUPD > 632 || DVO_TEST
-#ifndef _COMPHELPER_STLTYPES_HXX_
-#include <comphelper/stl_types.hxx>
-#endif
-#else
-#ifndef _XMLOFF_FUNCTIONAL_HXX
-#include <xmloff/functional.hxx>
-#endif
-#endif
-
-
 namespace com { namespace sun { namespace star {
     namespace text { class XTextCursor; class XTextContent; }
-    namespace beans { class XPropertySet; }
-    namespace io { class XOutputStream; }
 } } }
 
-#define XML_TEXT_FRAME_TEXTBOX 1
-#define XML_TEXT_FRAME_GRAPHIC 2
-#define XML_TEXT_FRAME_OBJECT 3
-#define XML_TEXT_FRAME_OBJECT_OLE 4
-#define XML_TEXT_FRAME_APPLET 5
-#define XML_TEXT_FRAME_PLUGIN 6
-#define XML_TEXT_FRAME_FLOATING_FRAME 7
-
-#if SUPD > 632 || DVO_TEST
-typedef ::std::map < const ::rtl::OUString, ::rtl::OUString, ::comphelper::UStringLess> ParamMap;
-#else
-typedef ::std::map < const ::rtl::OUString, ::rtl::OUString, less_functor> ParamMap;
-#endif
-
+class SvXMLAttributeList;
 class XMLTextFrameContextHyperlink_Impl;
 
 class XMLTextFrameContext : public SvXMLImportContext
 {
-    ::com::sun::star::uno::Reference <
-        ::com::sun::star::text::XTextCursor > xOldTextCursor;
-    ::com::sun::star::uno::Reference <
-        ::com::sun::star::beans::XPropertySet > xPropSet;
-    ::com::sun::star::uno::Reference <
-        ::com::sun::star::io::XOutputStream > xBase64Stream;
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::xml::sax::XAttributeList > m_xAttrList;
 
-    /// old list item and block (#89891#)
-    SvXMLImportContextRef xListBlock;
-    SvXMLImportContextRef xListItem;
+    SvXMLImportContextRef m_xImplContext;
+    SvXMLAttributeList *m_pAttrList;
 
-    const ::rtl::OUString sWidth;
-    const ::rtl::OUString sRelativeWidth;
-    const ::rtl::OUString sHeight;
-    const ::rtl::OUString sRelativeHeight;
-    const ::rtl::OUString sSizeType;
-    const ::rtl::OUString sIsSyncWidthToHeight;
-    const ::rtl::OUString sIsSyncHeightToWidth;
-    const ::rtl::OUString sHoriOrient;
-    const ::rtl::OUString sHoriOrientPosition;
-    const ::rtl::OUString sVertOrient;
-    const ::rtl::OUString sVertOrientPosition;
-    const ::rtl::OUString sChainNextName;
-    const ::rtl::OUString sAnchorType;
-    const ::rtl::OUString sAnchorPageNo;
-    const ::rtl::OUString sGraphicURL;
-    const ::rtl::OUString sGraphicFilter;
-    const ::rtl::OUString sAlternativeText;
-    const ::rtl::OUString sFrameStyleName;
-    const ::rtl::OUString sGraphicRotation;
-    const ::rtl::OUString sTextBoxServiceName;
-    const ::rtl::OUString sGraphicServiceName;
-    const ::rtl::OUString sWidthType;
+    XMLTextFrameContextHyperlink_Impl   *m_pHyperlink;
+    ::rtl::OUString m_sDesc;
 
-    ::rtl::OUString sName;
-    ::rtl::OUString sStyleName;
-    ::rtl::OUString sNextName;
-    ::rtl::OUString sHRef;
-    ::rtl::OUString sFilterName;
-    ::rtl::OUString sCode;
-    ::rtl::OUString sObject;
-    ::rtl::OUString sArchive;
-    ::rtl::OUString sMimeType;
-    ::rtl::OUString sFrameName;
-    ::rtl::OUString sAppletName;
-    ::rtl::OUString sDesc;
-    ::rtl::OUString sFilterService;
-    ::rtl::OUString sBase64CharsLeft;
-    ::rtl::OUString sTblName;
+    ::com::sun::star::text::TextContentAnchorType   m_eDefaultAnchorType;
 
-    ParamMap aParamMap;
+    sal_Bool m_bHasName;
 
-    XMLTextFrameContextHyperlink_Impl   *pHyperlink;
-
-    sal_Int32   nX;
-    sal_Int32   nY;
-    sal_Int32   nWidth;
-    sal_Int32   nHeight;
-    sal_Int32   nZIndex;
-    sal_Int16   nPage;
-    sal_Int16   nRotation;
-    sal_Int16   nRelWidth;
-    sal_Int16   nRelHeight;
-
-    sal_uInt16 nType;
-    ::com::sun::star::text::TextContentAnchorType   eAnchorType;
-
-    sal_Bool    bMayScript : 1;
-    sal_Bool    bMinWidth : 1;
-    sal_Bool    bMinHeight : 1;
-    sal_Bool    bSyncWidth : 1;
-    sal_Bool    bSyncHeight : 1;
-    sal_Bool    bCreateFailed : 1;
-    sal_Bool    bOwnBase64Stream : 1;
-
-    void Create( sal_Bool bHRefOrBase64 );
     sal_Bool CreateIfNotThere();
+    sal_Bool CreateIfNotThere( ::com::sun::star::uno::Reference <
+        ::com::sun::star::beans::XPropertySet >& rPropSet );
 
 public:
 
@@ -196,13 +105,10 @@ public:
             const ::rtl::OUString& rLName,
             const ::com::sun::star::uno::Reference<
                 ::com::sun::star::xml::sax::XAttributeList > & xAttrList,
-            ::com::sun::star::text::TextContentAnchorType eAnchorType,
-            sal_uInt16 nType );
+            ::com::sun::star::text::TextContentAnchorType eDfltAnchorType );
     virtual ~XMLTextFrameContext();
 
     virtual void EndElement();
-
-    virtual void Characters( const ::rtl::OUString& rChars );
 
     SvXMLImportContext *CreateChildContext( sal_uInt16 nPrefix,
                 const ::rtl::OUString& rLocalName,
@@ -214,7 +120,7 @@ public:
                        const ::rtl::OUString& rTargetFrameName,
                        sal_Bool bMap );
 
-    ::com::sun::star::text::TextContentAnchorType GetAnchorType() const { return eAnchorType; }
+    ::com::sun::star::text::TextContentAnchorType GetAnchorType() const;
 
     ::com::sun::star::uno::Reference <
         ::com::sun::star::text::XTextContent > GetTextContent() const;
