ab0b9d9f2cf70c271ca0efc2892177f15c9a5686
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:58:07 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/07/01 13:08:12 sab 1.1.2.10: #i20153#; oasis changes 2004/06/25 14:23:44 cl 1.1.2.9: some fixes for impress 2004/06/25 11:51:40 bm 1.1.2.8: chart property: interval-minor-divisor 2004/06/18 14:56:50 bm 1.1.2.7: #i20158#: Replace chart:splines with chart:interpolation 2004/05/24 12:43:48 mib 1.1.2.6: - #i20153#: some attributes renamed 2004/05/21 14:59:18 mib 1.1.2.5: - #i20153#: changed underline, line-through and tab-stop 2004/05/18 06:48:22 mib 1.1.2.4: - #i20153#: added <draw:frame> 2004/04/19 12:33:36 mib 1.1.2.3: - changed "graphics" style family to "graphic" (#i20153#) - replaced "inch" with "in" (#i18170#) 2004/04/08 08:49:34 mib 1.1.2.2: - renamed memeber variables to m_ - renamed <text:tab-stop> to <text:tab> - replaced fo:score-spaces with style:text-underline-mode and   style:text-line-through-mode 2004/04/07 11:43:08 mib 1.1.2.1: splitted <properties> elements

---
+++ xmloff/source/transform/PropertyActionsOOo.hxx
@@ -0,0 +1,108 @@
+/*************************************************************************
+ *
+ *  $RCSfile: PropertyActionsOOo.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:58:07 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_PROPERTYACTIONSOOO_HXX
+#define _XMLOFF_PROPERTYACTIONSOOO_HXX
+
+#ifndef _XMLOFF_TRANSFORMERACTION_HXX
+#include "TransformerAction.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERACTIONINIT_HXX
+#include "TransformerActionInit.hxx"
+#endif
+#ifndef _XMLOFF_ATTRTRANSFORMERACTION_HXX
+#include "AttrTransformerAction.hxx"
+#endif
+
+enum XMLPropOOOTransformerAction
+{
+    XML_PTACTION_LINE_MODE=XML_ATACTION_USER_DEFINED,
+    XML_PTACTION_UNDERLINE,
+    XML_PTACTION_LINETHROUGH,
+    XML_PTACTION_KEEP_WITH_NEXT,
+    XML_PTACTION_SPLINES,
+    XML_ATACTION_INCH2IN_DUPLICATE,
+    XML_PTACTION_INTERVAL_MAJOR,
+    XML_PTACTION_INTERVAL_MINOR,
+    XML_ATACTION_COPY_DUPLICATE,
+    XML_PTACTION_END=XML_TACTION_END
+};
+
+extern XMLTransformerActionInit aGraphicPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aGraphicPropertyOOoElemActionTable[];
+extern XMLTransformerActionInit aDrawingPagePropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aPageLayoutPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aHeaderFooterPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTextPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTextPropertyOOoElemActionTable[];
+extern XMLTransformerActionInit aParagraphPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aParagraphPropertyOOoElemActionTable[];
+extern XMLTransformerActionInit aSectionPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTablePropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTableColumnPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTableRowPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTableCellPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aTableCellPropertyOOoElemActionTable[];
+extern XMLTransformerActionInit aListLevelPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aChartPropertyOOoAttrActionTable[];
+extern XMLTransformerActionInit aChartPropertyOOoElemActionTable[];
+
+#endif  //  _XMLOFF_PROPERTYACTIONSOOO_HXX
