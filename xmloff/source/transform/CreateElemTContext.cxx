e28922057044f8b3b3009dbca863794bfef36685
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:46:37 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/05/19 07:16:15 mib 1.1.2.2: #i20153#: removed Reference ambiguities 2004/05/12 10:59:52 mib 1.1.2.1: - #i20153#: changed <office:annotation> and <office:change-info>

---
+++ xmloff/source/transform/CreateElemTContext.cxx
@@ -0,0 +1,177 @@
+/*************************************************************************
+ *
+ *  $RCSfile: CreateElemTContext.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:46:37 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_CREATEELEMTCONTEXT_HXX
+#include "CreateElemTContext.hxx"
+#endif
+#ifndef _XMLOFF_MUTABLEATTRLIST_HXX
+#include "MutableAttrList.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERBASE_HXX
+#include "TransformerBase.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERACTIONS_HXX
+#include "TransformerActions.hxx"
+#endif
+#ifndef _XMLOFF_TCONTEXTVECTOR_HXX
+#include "TContextVector.hxx"
+#endif
+#ifndef _XMLOFF_FLATTCONTEXT_HXX
+#include "FlatTContext.hxx"
+#endif
+#ifndef _XMLOFF_ATTRTRANSFORMERACTION_HXX
+#include "AttrTransformerAction.hxx"
+#endif
+#ifndef _XMLOFF_NMSPMAP_HXX
+#include "nmspmap.hxx"
+#endif
+
+using ::rtl::OUString;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+using namespace ::xmloff::token;
+
+TYPEINIT1( XMLCreateElemTransformerContext, XMLTransformerContext );
+
+XMLCreateElemTransformerContext::XMLCreateElemTransformerContext(
+        XMLTransformerBase& rImp,
+        const OUString& rQName,
+       sal_uInt16 nActionMap ) :
+    XMLTransformerContext( rImp, rQName ),
+    m_nActionMap( nActionMap )
+{
+}
+
+XMLCreateElemTransformerContext::~XMLCreateElemTransformerContext()
+{
+}
+
+void XMLCreateElemTransformerContext::StartElement(
+        const Reference< XAttributeList >& rAttrList )
+{
+    Reference< XAttributeList > xAttrList( rAttrList );
+
+    XMLTransformerContextVector aChildContexts;
+
+    XMLMutableAttributeList *pMutableAttrList = 0;
+    XMLTransformerActions *pActions =
+        GetTransformer().GetUserDefinedActions( m_nActionMap );
+    OSL_ENSURE( pActions, "go no actions" );
+    if( pActions )
+    {
+        sal_Int16 nAttrCount = xAttrList.is() ? xAttrList->getLength() : 0;
+        for( sal_Int16 i=0; i < nAttrCount; ++i )
+        {
+            const OUString& rAttrName = xAttrList->getNameByIndex( i );
+            const OUString& rAttrValue = xAttrList->getValueByIndex( i );
+            OUString aLocalName;
+            sal_uInt16 nPrefix =
+                GetTransformer().GetNamespaceMap().GetKeyByAttrName( rAttrName,
+                                                           &aLocalName );
+
+            XMLTransformerActions::key_type aKey( nPrefix, aLocalName );
+            XMLTransformerActions::const_iterator aIter =
+                    pActions->find( aKey );
+            if( !(aIter == pActions->end() ) )
+            {
+                if( !pMutableAttrList )
+                {
+                    pMutableAttrList = new XMLMutableAttributeList( xAttrList );
+                    xAttrList = pMutableAttrList;
+                }
+                sal_uInt32 nAction = (*aIter).second.m_nActionType;
+                switch( nAction )
+                {
+                case XML_ATACTION_MOVE_TO_ELEM:
+                    {
+                        OUString aElemQName(
+                            GetTransformer().GetNamespaceMap().GetQNameByKey(
+                                (*aIter).second.GetQNamePrefixFromParam1(),
+                                ::xmloff::token::GetXMLToken(
+                                (*aIter).second.GetQNameTokenFromParam1()) ) );
+                        XMLTransformerContext *pContext =
+                            new XMLPersTextContentTContext( GetTransformer(),
+                                                       aElemQName );
+                        pContext->Characters( rAttrValue );
+                        XMLTransformerContextVector::value_type aVal(
+                                pContext );
+                        aChildContexts.push_back( aVal );
+                        pMutableAttrList->RemoveAttributeByIndex( i );
+                        --i;
+                        --nAttrCount;
+                    }
+                    break;
+                default:
+                    OSL_ENSURE( !this, "unknown action" );
+                    break;
+                }
+            }
+        }
+    }
+    XMLTransformerContext::StartElement( xAttrList );
+
+    XMLTransformerContextVector::iterator aIter = aChildContexts.begin();
+
+    for( ; aIter != aChildContexts.end(); ++aIter )
+    {
+        (*aIter)->Export();
+    }
+}
