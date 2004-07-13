5bd318d65ac1179b3de1d6137c61db0697f8c1c3
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:58:53 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/06/03 09:04:36 mib 1.1.2.3: - #i20153#: draw/impress bug fixes 2004/04/08 08:49:35 mib 1.1.2.2: - renamed memeber variables to m_ - renamed <text:tab-stop> to <text:tab> - replaced fo:score-spaces with style:text-underline-mode and   style:text-line-through-mode 2004/04/07 11:44:13 mib 1.1.2.1: splitted <properties>

---
+++ xmloff/source/transform/StyleOASISTContext.hxx
@@ -0,0 +1,113 @@
+/*************************************************************************
+ *
+ *  $RCSfile: StyleOASISTContext.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:58:53 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_STYLEOASISTCONTEXT_HXX
+#define _XMLOFF_STYLEOASISTCONTEXT_HXX
+
+#ifndef _RTL_REF_HXX_
+#include <rtl/ref.hxx>
+#endif
+
+#ifndef _XMLOFF_DEEPTCONTEXT_HXX
+#include "DeepTContext.hxx"
+#endif
+
+
+class XMLTransformerActions;
+class XMLPropertiesTContext_Impl;
+class XMLStyleOASISTContext : public XMLPersElemContentTContext
+{
+    ::rtl::Reference< XMLPropertiesTContext_Impl > m_xPropContext;
+
+    sal_Bool m_bPersistent;
+
+public:
+    TYPEINFO();
+
+    XMLStyleOASISTContext( XMLTransformerBase& rTransformer,
+                           const ::rtl::OUString& rQName,
+                              sal_Bool bPersistent=sal_False );
+    XMLStyleOASISTContext( XMLTransformerBase& rTransformer,
+                           const ::rtl::OUString& rQName,
+                              sal_uInt16 nPrefix,
+                              ::xmloff::token::XMLTokenEnum eToken,
+                              sal_Bool bPersistent=sal_False );
+
+    virtual ~XMLStyleOASISTContext();
+
+    virtual XMLTransformerContext *CreateChildContext( sal_uInt16 nPrefix,
+                                   const ::rtl::OUString& rLocalName,
+                                   const ::rtl::OUString& rQName,
+                                   const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
+
+    virtual void StartElement( const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
+    virtual void EndElement();
+    virtual void Characters( const ::rtl::OUString& rChars );
+
+    virtual void ExportContent();
+
+    virtual sal_Bool IsPersistent() const;
+
+    static XMLTransformerActions *CreateTransformerActions( sal_uInt16 nType );
+};
+
+#endif  //  _XMLOFF_STYLEOASISTCONTEXT_HXX
+
