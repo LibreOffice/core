e2255a4a6f881c9acd45e8741657d710860c2d06
Rüdiger Timm <rt@openoffice.org>
2004-07-13 13:01:51 +0000

    INTEGRATION: CWS impress2 (1.2.26); FILE MERGED 2004/06/22 11:09:10 af 1.2.26.5: #i22705# Added overloading GetController() method. 2004/06/18 00:18:58 af 1.2.26.4: RESYNC: (1.3-1.4); FILE MERGED 2004/06/12 12:17:08 af 1.2.26.3: #i22705# Replaced OuterResizePixel(), InnerResizePixel(), AdjustPosSizePixel() by GetBorder() and Resize(). 2004/05/19 18:38:33 af 1.2.26.2: RESYNC: (1.2-1.3); FILE MERGED 2004/02/25 16:38:23 af 1.2.26.1: #i22705# Added argument to constructors.

--- sd/source/ui/inc/SlideViewShell.hxx
+++ sd/source/ui/inc/SlideViewShell.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SlideViewShell.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2004-06-03 11:55:06 $
+ *  last change: $Author: rt $ $Date: 2004-07-13 14:01:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -102,8 +102,6 @@ public:
     SFX_DECL_VIEWFACTORY(SlideViewShell);
     SFX_DECL_INTERFACE(SD_IF_SDSLIDEVIEWSHELL);
 
-    SlideViewShell(SfxViewFrame *pFrame, const SlideViewShell& rShell);
-
     /** Create a new view shell for the slide view.
         @param rViewShellBase
             The new object will be stacked on this view shell base.
@@ -111,34 +109,18 @@ public:
             The frame view that makes it possible to pass information from
             one view shell to the next.
     */
-    SlideViewShell (
-        SfxViewFrame* pFrame,
+    SlideViewShell(SfxViewFrame* pFrame,
         ViewShellBase& rViewShellBase,
+        ::Window* pParentWindow,
         FrameView* pFrameView = NULL);
+
+    SlideViewShell(SfxViewFrame *pFrame,
+        ::Window* pParentWindow,
+        const SlideViewShell& rShell);
 
     virtual ~SlideViewShell (void);
 
     virtual void    Paint(const Rectangle& rRect, ::sd::Window* pWin);
-
-    /** Set the position and size of the area which contains the GUI
-        elements like rulers, sliders, and buttons as well as the document
-        view.  Both size and position are expected to be in pixel
-        coordinates.  The positions and sizes of the mentioned GUI elements
-        are updated as well.
-
-        <p> This method is implemented by first setting copying the given
-        values to internal variables and then calling the
-        <type>ArrangeGUIElements</type> method which performs the actual
-        work of sizeing and arranging the UI elements accordingly.</p>
-        @param rPos
-            The position of the enclosing window relative to the document
-            window.  This is only interesting if a Draw/Impress document
-            view is embedded as OLE object into another document view.  For
-            normal documents this position is (0,0).
-        @param rSize
-            The new size in pixel.
-    */
-    virtual void    AdjustPosSizePixel(const Point &rPos, const Size &rSize);
 
     /** Arrange and resize the GUI elements like rulers, sliders, and
         buttons as well as the actual document view according to the size of
@@ -212,9 +194,9 @@ public:
     */
     virtual void Activate (BOOL IsMDIActivate);
 
+    virtual DrawController* GetController (void);
+
 protected:
-    virtual void    InnerResizePixel(const Point &rPos, const Size &rSize);
-    virtual void    OuterResizePixel(const Point &rPos, const Size &rSize);
     virtual Size    GetOptimalSizePixel() const;
     virtual long    VirtHScrollHdl(ScrollBar* pHScroll);
     virtual long    VirtVScrollHdl(ScrollBar* pVHScroll);
