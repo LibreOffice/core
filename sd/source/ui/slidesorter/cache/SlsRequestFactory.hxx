7dd6ac24223d202026c05269c3b07d19c3b80a17
Rüdiger Timm <rt@openoffice.org>
2004-07-13 13:12:25 +0000

    INTEGRATION: CWS impress2 (1.1.2); FILE ADDED 2004/06/14 13:49:23 af 1.1.2.3: #i22705# Resolved compiler problems. 2004/02/25 14:21:40 af 1.1.2.2: #i22705# Corrected spelling of included header. 2004/02/13 12:17:05 af 1.1.2.1: #i22705# Initial revision.

---
+++ sd/source/ui/slidesorter/cache/SlsRequestFactory.hxx
@@ -0,0 +1,151 @@
+/*************************************************************************
+ *
+ *  $RCSfile: SlsRequestFactory.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 14:12:25 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SD_SLIDESORTER_REQUEST_FACTORY_HXX
+#define SD_SLIDESORTER_REQUEST_FACTORY_HXX
+
+#include "model/SlideSorterModel.hxx"
+#include "model/SlsPageDescriptor.hxx"
+#include "view/SlideSorterView.hxx"
+#include <svx/svdpagv.hxx>
+
+#include <svx/sdr/contact/viewcontact.hxx>
+
+namespace sd { namespace slidesorter { namespace view {
+class PageObjectViewObjectContact;
+} } }
+
+namespace sd { namespace slidesorter { namespace cache {
+
+template <class Queue, bool UseAheadOfTimeRequests>
+class RequestFactory
+{
+public:
+    void operator() (
+        const model::SlideSorterModel& rModel,
+        const view::SlideSorterView& rView,
+        Queue& mrQueue);
+};
+
+
+
+
+//=============================================================================
+// Implementation
+
+//=====  RequestFactory  ======================================================
+
+template <class Queue,bool UseAheadOfTimeRequests>
+    void RequestFactory<Queue,UseAheadOfTimeRequests>::operator() (
+        const model::SlideSorterModel& rModel,
+        const view::SlideSorterView& rView,
+        Queue& rQueue)
+{
+    // Fill the queues with the new content.  Visible page objects go into
+    // the regular queue, non-visible page objects go into the ahead-of-time
+    // queue.
+    SdrPageView* pPageView = rView.GetPageViewPvNum(0);
+    SdrPageViewWindow* pPageViewWindow = NULL;
+    if (pPageView != NULL)
+        pPageViewWindow = pPageView->GetWindow(0);
+    if (pPageViewWindow != NULL)
+    {
+        ::sdr::contact::ObjectContact& rObjectContact (
+            rView.GetPageViewPvNum(0)->GetWindow(0)->GetObjectContact());
+
+        // Add the requests for the visible pages.
+        model::SlideSorterModel::Enumeration aPageEnumeration (
+            rModel.GetVisiblePagesEnumeration());
+        while (aPageEnumeration.HasMoreElements())
+        {
+            model::PageDescriptor& rDescriptor (
+                aPageEnumeration.GetNextElement());
+            view::PageObjectViewObjectContact* pRequest (
+                rDescriptor.GetViewObjectContact());
+            if (pRequest != NULL)
+                rQueue.InsertFrontRequest (*pRequest, 0);
+        }
+
+        if (UseAheadOfTimeRequests)
+        {
+            // Add the requests for the non-visible pages.
+            aPageEnumeration = rModel.GetAllPagesEnumeration();
+            while (aPageEnumeration.HasMoreElements())
+            {
+                model::PageDescriptor& rDescriptor (
+                    aPageEnumeration.GetNextElement());
+                view::PageObjectViewObjectContact* pRequest =
+                    static_cast<view::PageObjectViewObjectContact*>(
+                        &rDescriptor.GetPageObject()->GetViewContact()
+                        .GetViewObjectContact(rObjectContact));
+                if ( ! rDescriptor.IsVisible())
+                    rQueue.InsertFrontRequest (*pRequest, 1);
+            }
+        }
+    }
+}
+
+
+
+} } } // end of namespace ::sd::slidesorter::cache
+
+
+#endif
