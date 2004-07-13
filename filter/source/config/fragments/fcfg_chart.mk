b51f1c4ac482d13ee9aa4ff0365d86e12e42ef8c
Rüdiger Timm <rt@openoffice.org>
2004-07-13 08:17:29 +0000

    INTEGRATION: CWS oasis (1.3.54); FILE MERGED 2004/05/07 15:08:47 cl 1.3.54.1: added oasis filter configurations

--- filter/source/config/fragments/fcfg_chart.mk
+++ filter/source/config/fragments/fcfg_chart.mk
@@ -1,19 +1,21 @@
 # -----------------------------------------------
-# count = 4
+# count = 5
 T4_CHART = \
     chart_StarChart_30 \
     chart_StarChart_40 \
     chart_StarChart_50 \
     chart_StarOffice_XML_Chart \
-    chart2_StarOffice_XML_Chart
+    chart2_StarOffice_XML_Chart\
+    chart8
 
 # -----------------------------------------------
-# count = 4
+# count = 5
 F4_CHART = \
     StarChart_3_0 \
     StarChart_4_0 \
     StarChart_5_0 \
-    StarOffice_XML__Chart_
+    StarOffice_XML__Chart_\
+    chart8
 
 # -----------------------------------------------
 # count = 0
