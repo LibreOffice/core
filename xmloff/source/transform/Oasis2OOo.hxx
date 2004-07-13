0e5b8e18a29a8136c4ee3d22d899eef373fc70b0
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:55:18 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/06/02 10:54:04 mib 1.1.2.5: - #i20153#: continued events 2004/04/19 12:33:35 mib 1.1.2.4: - changed "graphics" style family to "graphic" (#i20153#) - replaced "inch" with "in" (#i18170#) 2004/04/08 08:49:34 mib 1.1.2.3: - renamed memeber variables to m_ - renamed <text:tab-stop> to <text:tab> - replaced fo:score-spaces with style:text-underline-mode and   style:text-line-through-mode 2004/04/07 11:49:06 mib 1.1.2.2: splitted <properties> 2004/03/31 14:05:26 mib 1.1.2.1: First steps of OOo->OASIS transformation and back

---
+++ xmloff/source/transform/Oasis2OOo.hxx
@@ -0,0 +1,107 @@
+/*************************************************************************
+ *
+ *  $RCSfile: Oasis2OOo.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:55:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_OASIS2OOO_HXX
+#define _XMLOFF_OASIS2OOO_HXX
+
+#ifndef _XMLOFF_ACTIONMAPTYPESOASIS_HXX
+#include "ActionMapTypesOASIS.hxx"
+#endif
+#ifndef _XMLOFF_TRANSFORMERBASE_HXX_
+#include "TransformerBase.hxx"
+#endif
+
+class XMLTransformerOASISEventMap_Impl;
+
+class Oasis2OOoTransformer : public XMLTransformerBase
+{
+    XMLTransformerActions       *m_aActions[MAX_OASIS_ACTIONS];
+    XMLTransformerOASISEventMap_Impl *m_pEventMap;
+
+protected:
+
+    virtual XMLTransformerContext *CreateUserDefinedContext(
+                                      const TransformerAction_Impl& rAction,
+                                      const ::rtl::OUString& rQName,
+                                         sal_Bool bPersistent=sal_False );
+
+    virtual XMLTransformerActions *GetUserDefinedActions( sal_uInt16 n );
+
+public:
+    Oasis2OOoTransformer () throw();
+    virtual ~Oasis2OOoTransformer() throw();
+
+    static const ::com::sun::star::uno::Sequence< sal_Int8 > & getUnoTunnelId() throw();
+    static Oasis2OOoTransformer * getImplementation( ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > ) throw();
+
+
+    // XServiceInfo
+    virtual ::rtl::OUString SAL_CALL getImplementationName(  ) throw(::com::sun::star::uno::RuntimeException);
+    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) throw(::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames(  ) throw(::com::sun::star::uno::RuntimeException);
+
+    // XUnoTunnel
+    virtual sal_Int64 SAL_CALL getSomething( const ::com::sun::star::uno::Sequence< sal_Int8 >& aIdentifier ) throw(::com::sun::star::uno::RuntimeException);
+
+    virtual ::rtl::OUString GetEventName( const ::rtl::OUString& rName );
+};
+
+#endif  //  _XMLOFF_OASIS2OOO_HXX
