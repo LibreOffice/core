3a79db9fd192a07e222ef1bb03869d7e76384531
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:53:26 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/04/19 12:33:34 mib 1.1.2.3: - changed "graphics" style family to "graphic" (#i20153#) - replaced "inch" with "in" (#i18170#) 2004/04/08 08:49:32 mib 1.1.2.2: - renamed memeber variables to m_ - renamed <text:tab-stop> to <text:tab> - replaced fo:score-spaces with style:text-underline-mode and   style:text-line-through-mode 2004/03/31 14:05:30 mib 1.1.2.1: First steps of OOo->OASIS transformation and back

---
+++ xmloff/source/transform/MetaTContext.hxx
@@ -0,0 +1,121 @@
+/*************************************************************************
+ *
+ *  $RCSfile: MetaTContext.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:53:26 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_METATCONTEXT_HXX
+#define _XMLOFF_METATCONTEXT_HXX
+
+#ifndef _RTL_REF_HXX_
+#include <rtl/ref.hxx>
+#endif
+#ifndef _XMLOFF_FUNCTIONAL_HXX
+#include "functional.hxx"
+#endif
+
+#include <map>
+
+#ifndef _XMLOFF_FLATTRCONTEXT_HXX
+#include "FlatTContext.hxx"
+#endif
+
+typedef ::std::multimap< ::rtl::OUString,
+                    ::rtl::Reference< XMLPersTextContentTContext >,
+                    less_functor > XMLMetaContexts_Impl;
+
+
+class XMLMetaTransformerContext : public XMLTransformerContext
+{
+    XMLMetaContexts_Impl m_aContexts;
+
+public:
+    TYPEINFO();
+
+    // A contexts constructor does anything that is required if an element
+    // starts. Namespace processing has been done already.
+    // Note that virtual methods cannot be used inside constructors. Use
+    // StartElement instead if this is required.
+    XMLMetaTransformerContext( XMLTransformerBase& rTransformer,
+                           const ::rtl::OUString& rQName );
+
+    // A contexts destructor does anything that is required if an element
+    // ends. By default, nothing is done.
+    // Note that virtual methods cannot be used inside destructors. Use
+    // EndElement instead if this is required.
+    virtual ~XMLMetaTransformerContext();
+
+    // Create a childs element context. By default, the import's
+    // CreateContext method is called to create a new default context.
+    virtual XMLTransformerContext *CreateChildContext( sal_uInt16 nPrefix,
+                                   const ::rtl::OUString& rLocalName,
+                                   const ::rtl::OUString& rQName,
+                                   const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
+
+    // EndElement is called before a context will be destructed, but
+    // after a elements context has been parsed. It may be used for actions
+    // that require virtual methods. The default is to do nothing.
+    virtual void EndElement();
+
+    // This method is called for all characters that are contained in the
+    // current element. The default is to ignore them.
+    virtual void Characters( const ::rtl::OUString& rChars );
+};
+
+#endif  //  _XMLOFF_METATCONTEXT_HXX
+
