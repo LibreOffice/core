99ba4c67b38a2d49753434b24f472f26786f265d
Rüdiger Timm <rt@openoffice.org>
2004-07-13 15:48:59 +0000

    INTEGRATION: CWS ooo20040620 (1.3.38); FILE MERGED 2004/06/15 20:38:29 vq 1.3.38.1: #i30156# Change switches of rsc/rscdep with filename to -xx=<filename>.

--- solenv/inc/_tg_srs.mk
+++ solenv/inc/_tg_srs.mk
@@ -21,7 +21,7 @@ $(MISC)$/$(PWD:f).$(SRS1NAME).dprr: $(SRC1FILES) $(HIDSRS1PARTICLE) $(HID1FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS1NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS1NAME).srs} -fo$@ $(SRC1FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS1NAME).srs} -fo=$@ $(SRC1FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC1FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -65,7 +65,7 @@ $(SRS)$/$(SRS1NAME).srs: $(foreach,i,$(SRC1FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC1FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -95,7 +95,7 @@ $(MISC)$/$(PWD:f).$(SRS2NAME).dprr: $(SRC2FILES) $(HIDSRS2PARTICLE) $(HID2FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS2NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS2NAME).srs} -fo$@ $(SRC2FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS2NAME).srs} -fo=$@ $(SRC2FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC2FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -139,7 +139,7 @@ $(SRS)$/$(SRS2NAME).srs: $(foreach,i,$(SRC2FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC2FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -169,7 +169,7 @@ $(MISC)$/$(PWD:f).$(SRS3NAME).dprr: $(SRC3FILES) $(HIDSRS3PARTICLE) $(HID3FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS3NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS3NAME).srs} -fo$@ $(SRC3FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS3NAME).srs} -fo=$@ $(SRC3FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC3FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -213,7 +213,7 @@ $(SRS)$/$(SRS3NAME).srs: $(foreach,i,$(SRC3FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC3FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -243,7 +243,7 @@ $(MISC)$/$(PWD:f).$(SRS4NAME).dprr: $(SRC4FILES) $(HIDSRS4PARTICLE) $(HID4FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS4NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS4NAME).srs} -fo$@ $(SRC4FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS4NAME).srs} -fo=$@ $(SRC4FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC4FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -287,7 +287,7 @@ $(SRS)$/$(SRS4NAME).srs: $(foreach,i,$(SRC4FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC4FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -317,7 +317,7 @@ $(MISC)$/$(PWD:f).$(SRS5NAME).dprr: $(SRC5FILES) $(HIDSRS5PARTICLE) $(HID5FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS5NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS5NAME).srs} -fo$@ $(SRC5FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS5NAME).srs} -fo=$@ $(SRC5FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC5FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -361,7 +361,7 @@ $(SRS)$/$(SRS5NAME).srs: $(foreach,i,$(SRC5FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC5FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -391,7 +391,7 @@ $(MISC)$/$(PWD:f).$(SRS6NAME).dprr: $(SRC6FILES) $(HIDSRS6PARTICLE) $(HID6FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS6NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS6NAME).srs} -fo$@ $(SRC6FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS6NAME).srs} -fo=$@ $(SRC6FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC6FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -435,7 +435,7 @@ $(SRS)$/$(SRS6NAME).srs: $(foreach,i,$(SRC6FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC6FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -465,7 +465,7 @@ $(MISC)$/$(PWD:f).$(SRS7NAME).dprr: $(SRC7FILES) $(HIDSRS7PARTICLE) $(HID7FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS7NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS7NAME).srs} -fo$@ $(SRC7FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS7NAME).srs} -fo=$@ $(SRC7FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC7FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -509,7 +509,7 @@ $(SRS)$/$(SRS7NAME).srs: $(foreach,i,$(SRC7FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC7FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -539,7 +539,7 @@ $(MISC)$/$(PWD:f).$(SRS8NAME).dprr: $(SRC8FILES) $(HIDSRS8PARTICLE) $(HID8FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS8NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS8NAME).srs} -fo$@ $(SRC8FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS8NAME).srs} -fo=$@ $(SRC8FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC8FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -583,7 +583,7 @@ $(SRS)$/$(SRS8NAME).srs: $(foreach,i,$(SRC8FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC8FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -613,7 +613,7 @@ $(MISC)$/$(PWD:f).$(SRS9NAME).dprr: $(SRC9FILES) $(HIDSRS9PARTICLE) $(HID9FILES)
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS9NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS9NAME).srs} -fo$@ $(SRC9FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS9NAME).srs} -fo=$@ $(SRC9FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC9FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -657,7 +657,7 @@ $(SRS)$/$(SRS9NAME).srs: $(foreach,i,$(SRC9FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC9FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
@@ -687,7 +687,7 @@ $(MISC)$/$(PWD:f).$(SRS10NAME).dprr: $(SRC10FILES) $(HIDSRS10PARTICLE) $(HID10FI
     @echo ------------------------------
     @echo Making: $@
     +-$(RM) $(MISC)$/$(PWD:f).$(SRS10NAME).dprr >& $(NULLDEV)
-    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp{$(SRS)$/$(SRS10NAME).srs} -fo$@ $(SRC10FILES)
+    +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS10NAME).srs} -fo=$@ $(SRC10FILES)
 
 .IF "$(common_build_srs)"!=""
 $(foreach,i,$(SRC10FILES) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
@@ -731,7 +731,7 @@ $(SRS)$/$(SRS10NAME).srs: $(foreach,i,$(SRC10FILES) $(MISC)$/$(TARGET)$/$i)
     +$(RSC) -presponse @$(mktmp \
         $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) \
         $(INCLUDE) $(RSCDEFS) $(RSCUPDVERDEF) \
-        -fp$@ \
+        -fp=$@ \
         $(foreach,i,$(SRC10FILES) $(MISC)$/$(TARGET)$/$i) \
     )
 .ENDIF          # "$(common_build_srs)"!=""
