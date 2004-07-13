796492b18366ea779140990cf9a2fde4833b9a26
Rüdiger Timm <rt@openoffice.org>
2004-07-13 12:56:23 +0000

    INTEGRATION: CWS impress2 (1.1.2); FILE ADDED 2004/06/04 06:42:47 af 1.1.2.2: #i22705# Renamed the implementation class. 2004/05/19 13:19:15 af 1.1.2.1: #i22705# Initial revision.

---
+++ sd/source/ui/inc/MasterPageObserver.hxx
@@ -0,0 +1,176 @@
+/*************************************************************************
+ *
+ *  $RCSfile: MasterPageObserver.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 13:56:23 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SD_VIEW_MASTER_PAGE_OBSERVER_HXX
+#define SD_VIEW_MASTER_PAGE_OBSERVER_HXX
+
+#include <osl/mutex.hxx>
+#include <tools/link.hxx>
+#include <memory>
+#include <set>
+
+class SdDrawDocument;
+class String;
+
+namespace sd {
+
+/** This singleton observes all registered documents for changes in the used
+    master pages and in turn informs its listeners about it.  One such
+    listener is the master page selector control in the tool panel that
+    shows the recently used master pages.
+*/
+class MasterPageObserver
+{
+public:
+    typedef ::std::set<String> MasterPageNameSet;
+
+    /** Return the single instance of this class.
+    */
+    static MasterPageObserver& Instance (void);
+
+    /** The master page observer will listen to events of this document and
+        detect changes of the use of master pages.
+    */
+    void RegisterDocument (SdDrawDocument& rDocument);
+
+    /** The master page observer will stop to listen to events of this
+        document.
+    */
+    void UnregisterDocument (SdDrawDocument& rDocument);
+
+    /** Add a listener that is informed of master pages that are newly
+        assigned to slides or become unassigned.
+        @param rEventListener
+            The event listener to call for future events.  Call
+            RemoveEventListener() before the listener is destroyed.
+    */
+    void AddEventListener (const Link& rEventListener);
+
+    /** Remove the given listener from the list of listeners.
+        @param rEventListener
+            After this method returns the given listener is not called back
+            from this object.  Passing a listener that has not
+            been registered before is safe and is silently ignored.
+    */
+    void RemoveEventListener (const Link& rEventListener);
+
+    /** Return a set of the names of master pages for the given document.
+        This convenience method exists because this set is part of the
+        internal data structure and thus takes no time to create.
+    */
+    MasterPageNameSet GetMasterPageNames (SdDrawDocument& rDocument);
+
+private:
+    static ::osl::Mutex maMutex;
+
+    /** The single instance of this class.  It is created on demand when
+        Instance() is called for the first time.
+    */
+    static MasterPageObserver* mpInstance;
+
+    class Implementation;
+    ::std::auto_ptr<Implementation> mpImpl;
+
+    MasterPageObserver (void);
+
+    /// The copy constructor is not implemented.  Do not use!
+    MasterPageObserver (const MasterPageObserver&);
+
+    /// The assignment operator is not implemented.  Do not use!
+    MasterPageObserver& operator= (const MasterPageObserver&);
+};
+
+
+
+
+/** Objects of this class are sent to listeners of the MasterPageObserver
+    singleton when the list of master pages of one document has changed.
+*/
+class MasterPageObserverEvent
+{
+public:
+    enum EventType {
+        /// Master page already exists when document is registered.
+        ET_MASTER_PAGE_EXISTS,
+        /// Master page has been added to a document.
+        ET_MASTER_PAGE_ADDED,
+        /// Master page has been removed from to a document.
+        ET_MASTER_PAGE_REMOVED
+    };
+
+    EventType meType;
+    SdDrawDocument& mrDocument;
+    const String& mrMasterPageName;
+
+    MasterPageObserverEvent (
+        EventType eType,
+        SdDrawDocument& rDocument,
+        const String& rMasterPageName)
+        : meType(eType),
+          mrDocument(rDocument),
+          mrMasterPageName(rMasterPageName)
+    {}
+
+};
+
+} // end of namespace sd
+
+#endif
