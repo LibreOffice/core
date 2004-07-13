34fa3977821aa15629b9ef39a1b2fb643f0f9384
Rüdiger Timm <rt@openoffice.org>
2004-07-13 13:23:23 +0000

    INTEGRATION: CWS impress2 (1.1.2); FILE ADDED 2004/06/22 11:24:13 af 1.1.2.2: #i22705# Moved the storing of the page borders to model/PageDescriptor. 2004/05/05 12:58:28 af 1.1.2.1: #i22705# Moved here from ../../view.

---
+++ sd/source/ui/slidesorter/inc/view/SlsPageObjectViewContact.hxx
@@ -0,0 +1,139 @@
+/*************************************************************************
+ *
+ *  $RCSfile: SlsPageObjectViewContact.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 14:23:23 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SD_SLIDESORTER_PAGE_OBJECT_VIEW_CONTACT_HXX
+#define SD_SLIDESORTER_PAGE_OBJECT_VIEW_CONTACT_HXX
+
+#include <svx/sdr/contact/viewcontactofpageobj.hxx>
+
+class SdrPageObj;
+
+namespace sdr {namespace contact {
+class ViewObjectContact;
+class ObjectContact;
+} }
+
+namespace sd { namespace slidesorter { namespace model {
+class PageDescriptor;
+} } }
+
+
+namespace sd { namespace slidesorter { namespace view {
+
+/** Details:
+    This class has to provide the bounding box but can not determine it
+    fully because it has no access to the output device.  It therefore
+    retrieves some of the necessary data, the border, from the
+    PageDescriptor which acts here as persistent storage.
+*/
+class PageObjectViewContact
+    : public ::sdr::contact::ViewContactOfPageObj
+{
+public:
+    PageObjectViewContact (
+        SdrPageObj& rPageObj,
+        model::PageDescriptor& rDescriptor);
+
+    /** Create a ViewObjectContact object that buffers its output in a
+        bitmap.
+        @return
+            Ownership of the new object passes to the caller.
+    */
+    virtual ::sdr::contact::ViewObjectContact&
+        CreateObjectSpecificViewObjectContact(
+            ::sdr::contact::ObjectContact& rObjectContact);
+
+    const SdrPage* GetPage (void) const;
+
+    SdrPageObj& GetPageObject (void) const;
+
+    Rectangle GetPageObjectBoundingBox (void) const;
+
+    /** Return the original bounding box of the page objects, not the
+        enlarged rectangle that encloses the frames, indicators, and the
+        title (as returned by GetPaintRectangle()).
+    */
+    virtual Rectangle GetPageRectangle (void);
+
+protected:
+    /** Enlarge the paint rectangle of the base class by the space that is
+        used to paint the focus and selection indicators, the fade effect
+        indicator, and the slide name.
+    */
+    virtual void CalcPaintRectangle (void);
+
+    virtual void PrepareDelete (void);
+
+private:
+    // The bounding box that is calculated by the base class implementation
+    // of the CalcPaintRectangle() method.
+    Rectangle maPageObjectBoundingBox;
+
+    /** This flag is set as long as PrepareDelete() has not been called.
+    */
+    bool mbIsValid;
+
+    model::PageDescriptor& mrDescriptor;
+};
+
+} } } // end of namespace ::sd::slidesorter::view
+
+#endif
