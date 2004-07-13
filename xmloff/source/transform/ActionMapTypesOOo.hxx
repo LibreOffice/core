f5ea5804244c879a7b6aacee9b4a53e028aa5797
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:43:16 +0000

    INTEGRATION: CWS oasis (1.1.2); FILE ADDED 2004/06/24 15:47:06 sab 1.1.2.15: #i20153#; oasis changes 2004/06/21 09:12:38 mib 1.1.2.14: #i20153#: meta generator, service names for chart and forms 2004/06/16 07:54:02 mib 1.1.2.13: #i20153#: new package URI schema, namespaced conditions 2004/06/08 14:49:22 mib 1.1.2.12: - #i20153#: form controls 2004/06/02 10:53:59 mib 1.1.2.11: - #i20153#: continued events 2004/05/24 12:43:46 mib 1.1.2.10: - #i20153#: some attributes renamed 2004/05/24 09:16:08 mib 1.1.2.9: - #i20153#: replaced transparency with opacity 2004/05/21 14:59:13 mib 1.1.2.8: - #i20153#: changed underline, line-through and tab-stop 2004/05/18 06:48:20 mib 1.1.2.7: - #i20153#: added <draw:frame> 2004/05/12 11:00:39 mib 1.1.2.6: - #i20153#: changed <office:annotation> and <office:change-info> 2004/05/11 11:11:26 mib 1.1.2.5: - #i20153#: encode/decode style names finished 2004/04/22 15:17:02 mib 1.1.2.4: - renamed most *:value attributes to office:value (#i20153#) - renamed some more formatting properties (#i20153#) 2004/04/19 12:33:31 mib 1.1.2.3: - changed "graphics" style family to "graphic" (#i20153#) - replaced "inch" with "in" (#i18170#) 2004/04/08 08:49:30 mib 1.1.2.2: - renamed memeber variables to m_ - renamed <text:tab-stop> to <text:tab> - replaced fo:score-spaces with style:text-underline-mode and   style:text-line-through-mode 2004/04/07 11:46:16 mib 1.1.2.1: splitted <properties>

---
+++ xmloff/source/transform/ActionMapTypesOOo.hxx
@@ -0,0 +1,119 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ActionMapTypesOOo.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:43:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_ACTIONMAPTYPESOOO_HXX
+#define _XMLOFF_ACTIONMAPTYPESOOO_HXX
+
+enum ActionMapTypesOOo
+{
+    PROP_OOO_GRAPHIC_ATTR_ACTIONS,
+    PROP_OOO_GRAPHIC_ELEM_ACTIONS,
+    PROP_OOO_DRAWING_PAGE_ATTR_ACTIONS,
+    PROP_OOO_PAGE_LAYOUT_ATTR_ACTIONS,
+    PROP_OOO_HEADER_FOOTER_ATTR_ACTIONS,
+    PROP_OOO_TEXT_ATTR_ACTIONS,
+    PROP_OOO_TEXT_ELEM_ACTIONS,
+    PROP_OOO_PARAGRAPH_ATTR_ACTIONS,
+    PROP_OOO_PARAGRAPH_ELEM_ACTIONS,
+    PROP_OOO_SECTION_ATTR_ACTIONS,
+    PROP_OOO_TABLE_ATTR_ACTIONS,
+    PROP_OOO_TABLE_COLUMN_ATTR_ACTIONS,
+    PROP_OOO_TABLE_ROW_ATTR_ACTIONS,
+    PROP_OOO_TABLE_CELL_ATTR_ACTIONS,
+    PROP_OOO_TABLE_CELL_ELEM_ACTIONS,
+    PROP_OOO_LIST_LEVEL_ATTR_ACTIONS,
+    PROP_OOO_CHART_ATTR_ACTIONS,
+    PROP_OOO_CHART_ELEM_ACTIONS,
+    MAX_OOO_PROP_ACTIONS,
+    OOO_STYLE_ACTIONS = MAX_OOO_PROP_ACTIONS,
+    OOO_FONT_DECL_ACTIONS,
+    OOO_SHAPE_ACTIONS,
+    OOO_INDEX_ENTRY_TAB_STOP_ACTIONS,
+    OOO_TAB_STOP_ACTIONS,
+    OOO_LINENUMBERING_ACTIONS,
+    OOO_FOOTNOTE_SEP_ACTIONS,
+    OOO_DROP_CAP_ACTIONS,
+    OOO_COLUMNS_ACTIONS,
+    OOO_TEXT_VALUE_TYPE_ACTIONS,
+    OOO_TABLE_VALUE_TYPE_ACTIONS,
+    OOO_PARA_ACTIONS,
+    OOO_STYLE_REF_ACTIONS,
+    OOO_MASTER_PAGE_ACTIONS,
+    OOO_ANNOTATION_ACTIONS,
+    OOO_CHANGE_INFO_ACTIONS,
+    OOO_FRAME_ELEM_ACTIONS,
+    OOO_FRAME_ATTR_ACTIONS,
+    OOO_BACKGROUND_IMAGE_ACTIONS,
+    OOO_DDE_CONNECTION_DECL_ACTIONS,
+    OOO_EVENT_ACTIONS,
+    OOO_FORM_CONTROL_ACTIONS,
+    OOO_FORM_COLUMN_ACTIONS,
+    OOO_FORM_PROP_ACTIONS,
+    OOO_XLINK_ACTIONS,
+    OOO_CONFIG_ITEM_SET_ACTIONS,
+    OOO_FORMULA_ACTIONS,
+    OOO_CHART_ACTIONS,
+    OOO_ERROR_MACRO_ACTIONS,
+    OOO_DDE_CONV_MODE_ACTIONS,
+    MAX_OOO_ACTIONS
+};
+
+#endif  //  _XMLOFF_ACTIONMAPTYPESOOO_HXX
