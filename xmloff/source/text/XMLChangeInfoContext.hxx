7d5e7550d9073ff116d6f187dc6dc9d80b1b1826
Rüdiger Timm <rt@openoffice.org>
2004-07-13 07:31:07 +0000

    INTEGRATION: CWS oasis (1.2.288); FILE MERGED 2004/05/12 11:00:37 mib 1.2.288.1: - #i20153#: changed <office:annotation> and <office:change-info>

--- xmloff/source/text/XMLChangeInfoContext.hxx
+++ xmloff/source/text/XMLChangeInfoContext.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLChangeInfoContext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dvo $ $Date: 2001-01-24 16:49:51 $
+ *  last change: $Author: rt $ $Date: 2004-07-13 08:31:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,8 +96,8 @@ class XMLChangeInfoContext : public SvXMLImportContext
 {
     const ::rtl::OUString& rType;
 
-    ::rtl::OUString sAuthor;
-    ::rtl::OUString sDateTime;
+    ::rtl::OUStringBuffer sAuthorBuffer;
+    ::rtl::OUStringBuffer sDateTimeBuffer;
     ::rtl::OUStringBuffer sCommentBuffer;
 
     XMLChangedRegionImportContext& rChangedRegion;
