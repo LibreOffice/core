f8ceb18df9a64a49fea567aab8347988b18a0575
Michael Mi <mmi@openoffice.org>
2004-07-14 09:44:22 +0000

    Issue number: Submitted by:  Andrew Fan Reviewed by:
    Add a flag for xpcom linking.

--- xmlsecurity/util/makefile.mk
+++ xmlsecurity/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: mmi $ $Date: 2004-07-14 03:56:55 $
+#   last change: $Author: mmi $ $Date: 2004-07-14 10:44:22 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,6 +64,9 @@ PRJ=..
 
 PRJNAME=xmlsecurity
 TARGET=xmlsecurity
+
+# Disable '-z defs' due to broken libxpcom.
+LINKFLAGSDEFS=$(0)
 
 # --- Settings -----------------------------------------------------
 
