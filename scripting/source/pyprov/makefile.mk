39c0cc0acd00f95cfc59c77b25d7d347182e8754
Jens-Heiner Rechtien <hr@openoffice.org>
2004-07-23 13:11:32 +0000

    INTEGRATION: CWS scriptingf7 (1.3.22); FILE MERGED 2004/07/02 09:01:47 npower 1.3.22.1: #i25269# add UCBHELP to link list, pyprovider has been enabled to be initialise from a string ( support for pkgchk, MasterScriptProvider factory, tdoc ) Issue number: Submitted by: Reviewed by:

--- scripting/source/pyprov/makefile.mk
+++ scripting/source/pyprov/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-02-10 16:15:18 $
+#   last change: $Author: hr $ $Date: 2004-07-23 14:11:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -100,6 +100,7 @@ SHL1STDLIBS= \
         $(COMPHELPERLIB) \
         $(CPPUHELPERLIB) \
         $(CPPULIB) \
+        $(UCBHELPERLIB) \
         $(SALLIB)
 
 SHL1DEPN=
