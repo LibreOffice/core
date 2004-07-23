a2ed626cfd7f5658d5782ee71fbbd326e21f29da
Jens-Heiner Rechtien <hr@openoffice.org>
2004-07-23 12:00:19 +0000

    INTEGRATION: CWS tableoptions (1.8.36); FILE MERGED 2004/07/06 10:20:44 dr 1.8.36.1: #i25111# UI for new DataPilot table options

--- sc/source/ui/inc/pvlaydlg.hxx
+++ sc/source/ui/inc/pvlaydlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pvlaydlg.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2004-06-04 14:35:17 $
+ *  last change: $Author: hr $ $Date: 2004-07-23 13:00:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -176,6 +176,8 @@ private:
     CheckBox                aBtnDetectCat;
     CheckBox                aBtnTotalCol;
     CheckBox                aBtnTotalRow;
+    CheckBox                aBtnFilter;
+    CheckBox                aBtnDrillDown;
 
     OKButton                aBtnOk;
     CancelButton            aBtnCancel;
