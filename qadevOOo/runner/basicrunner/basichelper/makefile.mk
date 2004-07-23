200cfeb4bc4d66c345aaa59e5586a7458f7dacc3
Rüdiger Timm <rt@openoffice.org>
2004-07-23 12:40:48 +0000

    INTEGRATION: CWS sb20 (1.2.120); FILE MERGED 2004/06/14 15:05:42 sb 1.2.120.1: #i29119# sandbox.jar no longer needed by UNO.

--- qadevOOo/runner/basicrunner/basichelper/makefile.mk
+++ qadevOOo/runner/basicrunner/basichelper/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Date: 2003-03-31 16:23:49 $
+#   last change: $Date: 2004-07-23 13:40:48 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -72,7 +72,7 @@ TARGET = runner_basichelper
 
 # --- Files --------------------------------------------------------
 
-JARFILES       = sandbox.jar ridl.jar jurt.jar unoil.jar
+JARFILES       = ridl.jar jurt.jar unoil.jar
 JAVAFILES      = AttributeList.java DocumentHandler.java Filter.java \
                  Connector.java DispatchProviderInterceptor.java \
                  ThreadRunner.java
