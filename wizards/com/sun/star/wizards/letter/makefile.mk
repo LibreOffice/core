ff71e0d08b0111f4ccfb41842115102ae502e202
Rüdiger Timm <rt@openoffice.org>
2004-07-23 13:22:53 +0000

    INTEGRATION: CWS sb20 (1.2.12); FILE MERGED 2004/06/15 09:07:00 sb 1.2.12.1: #i29119# sandbox.jar no longer needed by UNO.

--- wizards/com/sun/star/wizards/letter/makefile.mk
+++ wizards/com/sun/star/wizards/letter/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2004-05-19 12:44:36 $
+#   last change: $Author: rt $ $Date: 2004-07-23 14:22:53 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,7 +70,7 @@ PACKAGE = com$/sun$/star$/wizards$/letter
 .INCLUDE : settings.mk
 #.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
-JARFILES= unoil.jar jurt.jar ridl.jar sandbox.jar juh.jar jut.jar java_uno.jar java_uno_accessbridge commonwizards.jar
+JARFILES= unoil.jar jurt.jar ridl.jar juh.jar jut.jar java_uno.jar java_uno_accessbridge commonwizards.jar
 
 CUSTOMMANIFESTFILE= MANIFEST.MF
 
