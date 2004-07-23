8678c20e1bae73756e6c0770a6bae363d63481ab
Rüdiger Timm <rt@openoffice.org>
2004-07-23 13:32:32 +0000

    INTEGRATION: CWS sb20 (1.2.102); FILE MERGED 2004/06/15 08:08:50 sb 1.2.102.1: #i29119# sandbox.jar no longer needed by UNO.

--- sfx2/qa/complex/DocHelper/makefile.mk
+++ sfx2/qa/complex/DocHelper/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Date: 2004-03-08 16:26:06 $
+#   last change: $Date: 2004-07-23 14:32:32 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -71,7 +71,7 @@ PACKAGE = complex$/framework$/dochelper
 
 #----- compile .java files -----------------------------------------
 
-JARFILES        = sandbox.jar ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar OOoRunner.jar
+JARFILES        = ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar OOoRunner.jar
 JAVAFILES       = WriterHelper.java DialogThread.java
 JAVACLASSFILES	= $(foreach,i,$(JAVAFILES) $(CLASSDIR)$/$(PACKAGE)$/$(i:b).class)
 
