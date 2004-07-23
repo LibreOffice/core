9e3cc0d8c6537a8d75e7ff20a21838671fe23a9e
Rüdiger Timm <rt@openoffice.org>
2004-07-23 14:15:51 +0000

    INTEGRATION: CWS sb20 (1.2.10); FILE MERGED 2004/06/14 13:25:27 sb 1.2.10.1: #i29119# sandbox.jar no longer needed by UNO.

--- javaunohelper/com/sun/star/comp/helper/UnoInfo.java
+++ javaunohelper/com/sun/star/comp/helper/UnoInfo.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoInfo.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-03 07:36:39 $
+ *  last change: $Author: rt $ $Date: 2004-07-23 15:15:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,7 +88,6 @@ public final class UnoInfo {
             "jurt.jar",
             "unoil.jar",
             "ridl.jar",
-            "sandbox.jar",
             "juh.jar",
             "unoinfo.jar" };
 
