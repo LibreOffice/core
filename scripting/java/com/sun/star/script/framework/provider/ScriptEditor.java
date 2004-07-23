0d8d3ef9eaa1e8020252b4dd1615f1e6e9e6f694
Jens-Heiner Rechtien <hr@openoffice.org>
2004-07-23 13:02:01 +0000

    INTEGRATION: CWS scriptingf7 (1.2.14); FILE MERGED 2004/07/14 09:07:08 npower 1.2.14.2: #i25260# Editors changes to support new methods in ScriptEditor interface { execute & indicateErrorLine }. ScriptProviders now raise editors with line of error highlighted. 2004/07/12 16:34:36 npower 1.2.14.1: #i25260# Changes to support error displayed in IDE for javascript and beanshell when script that is invoked is opened in IDE Issue number: Submitted by: Reviewed by:

--- scripting/java/com/sun/star/script/framework/provider/ScriptEditor.java
+++ scripting/java/com/sun/star/script/framework/provider/ScriptEditor.java
@@ -5,6 +5,8 @@ import com.sun.star.script.framework.container.ScriptMetaData;
 
 public interface ScriptEditor
 {
+    public Object execute() throws Exception;
+    public void indicateErrorLine( int lineNum );
     public void edit(XScriptContext context, ScriptMetaData entry);
     public String getTemplate();
     public String getExtension();
