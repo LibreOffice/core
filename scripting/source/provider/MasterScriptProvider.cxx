dde8cd7c9a2e6d25f560e0f77ba46006c089762a
Kurt Zenker <kz@openoffice.org>
2004-07-26 15:30:29 +0000

    #i10000# return value was missing

--- scripting/source/provider/MasterScriptProvider.cxx
+++ scripting/source/provider/MasterScriptProvider.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MasterScriptProvider.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-07-23 14:10:18 $
+ *  last change: $Author: kz $ $Date: 2004-07-26 16:30:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -898,6 +898,7 @@ sal_Bool SAL_CALL MasterScriptProvider::hasElements(  ) throw ( RuntimeException
         throw RuntimeException(  OUSTR("hasElements not implemented!!!!") ,
                 Reference< XInterface >() );
     }
+    return false;
 }
 
 //*************************************************************************
