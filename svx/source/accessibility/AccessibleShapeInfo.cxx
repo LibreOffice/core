c74ef36bf22512240c037192b40430810853d4d8
Kurt Zenker <kz@openoffice.org>
2004-07-30 14:45:07 +0000

    INTEGRATION: CWS gcc340fixes01 (1.2.576); FILE MERGED 2004/07/16 12:16:26 hr 1.2.576.1: #i31568#: gcc.3.4.x needs copy ctor for AccesibleShapeInfo, see http://gcc.gnu.org/bugs.html#cxx_rvalbind

--- svx/source/accessibility/AccessibleShapeInfo.cxx
+++ svx/source/accessibility/AccessibleShapeInfo.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleShapeInfo.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-24 16:55:01 $
+ *  last change: $Author: kz $ $Date: 2004-07-30 15:45:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,14 @@ AccessibleShapeInfo::AccessibleShapeInfo (
     // empty.
 }
 
-
+AccessibleShapeInfo::AccessibleShapeInfo (const AccessibleShapeInfo &rOther)
+    : mxShape (rOther.mxShape),
+      mxParent (rOther.mxParent),
+      mpChildrenManager (rOther.mpChildrenManager),
+      mnIndex (rOther.mnIndex)
+{
+    // empty.
+}
 
 
 AccessibleShapeInfo::~AccessibleShapeInfo (void)
