71e7e18c51f70941939b1aa9fdfb44fa5244d206
Kurt Zenker <kz@openoffice.org>
2004-07-30 14:07:45 +0000

    INTEGRATION: CWS gcc340fixes01 (1.3.110); FILE MERGED 2004/07/13 20:22:10 fa 1.3.110.1: #i31261# Specify namespaces/explicitly scope variables for gcc 3.4

--- configmgr/source/inc/treeaccessor.hxx
+++ configmgr/source/inc/treeaccessor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: treeaccessor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 13:34:24 $
+ *  last change: $Author: kz $ $Date: 2004-07-30 15:07:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -160,7 +160,7 @@ namespace configmgr
         {
             sharable::Address aAddr = p.addressValue();
 
-            if (aAddr) aAddr += offsetof(DataType,nodes);
+            if (aAddr) aAddr += offsetof(TreeAddress::DataType,nodes);
 
             return NodeAddress( memory::Pointer(aAddr) );
         }
