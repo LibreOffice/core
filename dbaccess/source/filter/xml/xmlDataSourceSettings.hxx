3ab74f07213d98c8fc723232ebe3f161b4ea8e1c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:20:25 +0000

    INTEGRATION: CWS insight01 (1.1.2); FILE ADDED 2003/10/24 06:13:00 oj 1.1.2.1: #i21643# initial revision of the import filter

---
+++ dbaccess/source/filter/xml/xmlDataSourceSettings.hxx
@@ -0,0 +1,92 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlDataSourceSettings.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-08-02 15:20:25 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef DBA_XMLDATASOURCESETTINGS_HXX
+#define DBA_XMLDATASOURCESETTINGS_HXX
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include <xmloff/xmlictxt.hxx>
+#endif
+
+namespace dbaxml
+{
+    class ODBFilter;
+    class OXMLDataSource;
+    class OXMLDataSourceSettings : public SvXMLImportContext
+    {
+        OXMLDataSource& m_rParent;
+        ODBFilter& GetOwnImport();
+    public:
+
+        OXMLDataSourceSettings( ODBFilter& rImport, sal_uInt16 nPrfx,
+                    const ::rtl::OUString& rLName,
+                    const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList > & xAttrList
+                    ,OXMLDataSource& _rParent);
+        virtual ~OXMLDataSourceSettings();
+
+        virtual SvXMLImportContext *CreateChildContext( sal_uInt16 nPrefix,
+                    const ::rtl::OUString& rLocalName,
+                    const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList > & xAttrList );
+    };
+// -----------------------------------------------------------------------------
+} // namespace dbaxml
+// -----------------------------------------------------------------------------
+
+#endif // DBA_XMLDATASOURCESETTINGS_HXX
