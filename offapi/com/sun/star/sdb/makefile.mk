63775b4d87a8a4025b1fc7debad9cc48df64418c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 13:07:05 +0000

    INTEGRATION: CWS insight01 (1.10.196); FILE MERGED 2004/05/28 18:58:25 oj 1.10.196.3: RESYNC: (1.11-1.12); FILE MERGED 2004/01/02 20:06:04 oj 1.10.196.2: RESYNC: (1.10-1.11); FILE MERGED 2003/11/26 12:37:54 oj 1.10.196.1: #111075# new save request

--- offapi/com/sun/star/sdb/makefile.mk
+++ offapi/com/sun/star/sdb/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: kz $ $Date: 2004-05-19 14:06:12 $
+#   last change: $Author: hr $ $Date: 2004-08-02 14:07:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -93,6 +93,7 @@ IDLFILES=\
     DataSourceBrowser.idl \
     DefinitionContainer.idl \
     DocumentDefinition.idl \
+    DocumentSaveRequest.idl \
     ErrorMessageDialog.idl \
     InteractionHandler.idl \
     ParametersRequest.idl \
@@ -126,6 +127,7 @@ IDLFILES=\
     XDatabaseAccessListener.idl \
     XDatabaseEnvironment.idl \
     XFormDocumentsSupplier.idl \
+    XInteractionDocumentSave.idl \
     XInteractionSupplyParameters.idl \
     XParametersSupplier.idl \
     XQueriesSupplier.idl \
