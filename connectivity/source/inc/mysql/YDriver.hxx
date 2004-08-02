975326933e560157e8208e7f70449e458846359b
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 16:14:52 +0000

    INTEGRATION: CWS insight01 (1.3.92); FILE MERGED 2003/08/01 11:52:28 oj 1.3.92.1: #111090# getPropertyInfo at driver

--- connectivity/source/inc/mysql/YDriver.hxx
+++ connectivity/source/inc/mysql/YDriver.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: YDriver.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: oj $ $Date: 2002-12-05 14:00:49 $
+ *  last change: $Author: hr $ $Date: 2004-08-02 17:14:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -54,7 +54,7 @@
  *
  *  All Rights Reserved.
  *
- *  Contributor(s): _______________________________________
+ *  Contributor(s): Ocke Janssen
  *
  *
  ************************************************************************/
