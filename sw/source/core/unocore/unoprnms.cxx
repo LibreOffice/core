d3d17efdbb39806ef37013b92d2ddc74117ac028
Kurt Zenker <kz@openoffice.org>
2004-08-02 13:18:42 +0000

    INTEGRATION: CWS swobjpos04 (1.95.46); FILE MERGED 2004/08/02 11:11:58 od 1.95.46.4: RESYNC: (1.97-1.98); FILE MERGED resolve merge conflict. 2004/07/14 08:29:00 od 1.95.46.3: correct syntax 2004/07/13 01:45:38 od 1.95.46.2: RESYNC: (1.95-1.97); FILE MERGED resolve merge conflict. 2004/05/07 15:37:56 od 1.95.46.1: #i28701# - add uno name for new attribute 'WrapInfluenceOnPosition'

--- sw/source/core/unocore/unoprnms.cxx
+++ sw/source/core/unocore/unoprnms.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoprnms.cxx,v $
  *
- *  $Revision: 1.98 $
+ *  $Revision: 1.99 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-12 13:34:40 $
+ *  last change: $Author: kz $ $Date: 2004-08-02 14:18:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -748,7 +748,8 @@ const SwPropNameTab aPropNameTab = {
 /* 0666 UNO_NAME_WIDTH_TYPE*/ {MAP_CHAR_LEN("WidthType")},
 /* 0667 UNO_NAME_SCRIPT_URL */            {MAP_CHAR_LEN("ScriptURL")},
 /* 0668 UNO_NAME_RUNTIME_UID*/ {MAP_CHAR_LEN("RuntimeUID")},
-/* 0669 UNO_NAME_COLLAPSING_BORDERS*/ {MAP_CHAR_LEN("CollapsingBorders")}
+/* 0669 UNO_NAME_COLLAPSING_BORDERS*/ {MAP_CHAR_LEN("CollapsingBorders")},
+/* 0670 UNO_NAME_WRAP_INFLUENCE_ON_POSITION*/ {MAP_CHAR_LEN("WrapInfluenceOnPosition")}
 };
 
 #ifndef PRODUCT
