e41bbb62c2e082dd0746784d35f36a2a0fcfa952
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:10:25 +0000

    INTEGRATION: CWS insight01 (1.5.132); FILE MERGED 2004/07/16 12:34:07 oj 1.5.132.9: #i31520# make sure that the name doesn't contain any / 2004/03/30 12:54:41 oj 1.5.132.8: share storage 2004/03/12 12:50:07 oj 1.5.132.7: #111075# implement commit 2004/03/12 11:55:21 oj 1.5.132.6: #111075# implement commit 2004/02/12 16:15:26 oj 1.5.132.5: #111075# fix refcount problem 2003/12/17 09:46:39 oj 1.5.132.4: #111075# ongoing work 2003/11/26 12:14:31 oj 1.5.132.3: #111075# ongoing work 2003/11/17 09:39:11 oj 1.5.132.2: #111075# ongoning work 2003/10/24 06:27:28 oj 1.5.132.1: #i21643# import filter changes

--- dbaccess/source/core/dataaccess/documentcontainer.hxx
+++ dbaccess/source/core/dataaccess/documentcontainer.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documentcontainer.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: fs $ $Date: 2001-06-18 11:49:37 $
+ *  last change: $Author: hr $ $Date: 2004-08-02 15:10:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,35 +65,116 @@
 #ifndef _DBA_CORE_DEFINITIONCONTAINER_HXX_
 #include "definitioncontainer.hxx"
 #endif
+#ifndef _CPPUHELPER_IMPLBASE4_HXX_
+#include <cppuhelper/implbase4.hxx>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XCOMPONENTLOADER_HPP_
+#include <com/sun/star/frame/XComponentLoader.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XHIERARCHICALNAMECONTAINER_HPP_
+#include <com/sun/star/container/XHierarchicalNameContainer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_XTRANSACTEDOBJECT_HPP_
+#include <com/sun/star/embed/XTransactedObject.hpp>
+#endif
+#ifndef COMPHELPER_PROPERTYSTATECONTAINER_HXX
+#include <comphelper/propertystatecontainer.hxx>
+#endif
+#ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
+#include <comphelper/proparrhlp.hxx>
+#endif
 
 //........................................................................
 namespace dbaccess
 {
 //........................................................................
-
+typedef ::cppu::ImplHelper4<        ::com::sun::star::frame::XComponentLoader
+                                ,   ::com::sun::star::lang::XMultiServiceFactory
+                                ,   ::com::sun::star::container::XHierarchicalNameContainer
+                                ,   ::com::sun::star::embed::XTransactedObject
+                                >   ODocumentContainer_Base;
 //==========================================================================
 //= ODocumentContainer -    collections of database documents (reports/forms)
 //==========================================================================
-class ODocumentContainer    :public ODefinitionContainer
+class ODocumentContainer    : public ODefinitionContainer
+                            , public ODocumentContainer_Base
+                            , public ::comphelper::OPropertyStateContainer
+                            , public ::comphelper::OPropertyArrayUsageHelper< ODocumentContainer >
 {
+    DECLARE_STL_USTRINGACCESS_MAP(bool, PersistentNames);
+    PersistentNames m_aObjectList;
+    sal_Bool        m_bFormsContainer;
 public:
     /** constructs the container.<BR>
-        @param      _rParent                the parent object which is used for ref counting
-        @param      _rMutex                 the parent's mutex object for access safety
     */
     ODocumentContainer(
-        ::cppu::OWeakObject& _rParent,
-        ::osl::Mutex& _rMutex
+          const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _xORB
+        , const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >&  _xParentContainer
+        ,const TContentPtr& _pImpl
+        , sal_Bool _bFormsContainer
         );
-    virtual ~ODocumentContainer();
+
+    // ::com::sun::star::uno::XInterface
+    DECLARE_XINTERFACE( )
+    // com::sun::star::lang::XTypeProvider
+    DECLARE_TYPEPROVIDER( );
+    // ::com::sun::star::lang::XServiceInfo
+    DECLARE_SERVICE_INFO();
+
+    // XComponentLoader
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::lang::XComponent > SAL_CALL loadComponentFromURL( const ::rtl::OUString& URL, const ::rtl::OUString& TargetFrameName, sal_Int32 SearchFlags, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& Arguments ) throw (::com::sun::star::io::IOException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+
+    // ::com::sun::star::lang::XMultiServiceFactory
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL createInstance( const ::rtl::OUString& aServiceSpecifier ) throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL createInstanceWithArguments( const ::rtl::OUString& ServiceSpecifier, const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& Arguments ) throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getAvailableServiceNames(  ) throw (::com::sun::star::uno::RuntimeException);
+
+    // XCommandProcessor
+    virtual ::com::sun::star::uno::Any SAL_CALL execute( const ::com::sun::star::ucb::Command& aCommand, sal_Int32 CommandId, const ::com::sun::star::uno::Reference< ::com::sun::star::ucb::XCommandEnvironment >& Environment ) throw (::com::sun::star::uno::Exception, ::com::sun::star::ucb::CommandAbortedException, ::com::sun::star::uno::RuntimeException) ;
+
+    // XHierarchicalNameAccess
+    virtual ::com::sun::star::uno::Any SAL_CALL getByHierarchicalName( const ::rtl::OUString& _sName ) throw (::com::sun::star::container::NoSuchElementException, ::com::sun::star::uno::RuntimeException);
+    virtual sal_Bool SAL_CALL hasByHierarchicalName( const ::rtl::OUString& _sName ) throw (::com::sun::star::uno::RuntimeException);
+
+    // XHierarchicalNameContainer
+    virtual void SAL_CALL insertByHierarchicalName( const ::rtl::OUString& aName, const ::com::sun::star::uno::Any& aElement ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::container::ElementExistException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeByHierarchicalName( const ::rtl::OUString& Name ) throw (::com::sun::star::container::NoSuchElementException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    // XHierarchicalNameReplace
+    virtual void SAL_CALL replaceByHierarchicalName( const ::rtl::OUString& aName, const ::com::sun::star::uno::Any& aElement ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::container::NoSuchElementException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    // ::com::sun::star::beans::XPropertySet
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo(  ) throw(::com::sun::star::uno::RuntimeException);
+
+    // XTransactedObject
+    virtual void SAL_CALL commit(  ) throw (::com::sun::star::io::IOException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL revert(  ) throw (::com::sun::star::io::IOException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    // OPropertySetHelper
+    virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
+
+    // helper
+    ::rtl::Reference<OContentHelper> getContent(const ::rtl::OUString& _sName) const;
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage> getStorage() const;
 
 protected:
+    virtual ~ODocumentContainer();
 // ODefinitionContainer
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createObject(
-        const ::rtl::OUString& _rName,
-        const ::utl::OConfigurationNode& _rObjectNode
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::ucb::XContent > createObject(
+        const ::rtl::OUString& _rName
     );
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createObject();
+
+    virtual ::com::sun::star::uno::Any getPropertyDefaultByHandle( sal_Int32 _nHandle ) const;
+
+    // OPropertyArrayUsageHelper
+    virtual ::cppu::IPropertyArrayHelper* createArrayHelper( ) const;
+    /** approve that the object given may be inserted into the container. Should be overloaded by derived classes,
+        the default implementation just checks the object to be non-void.
+    */
+    virtual sal_Bool approveNewObject(const ::rtl::OUString& _sName,const ::com::sun::star::uno::Reference< ::com::sun::star::ucb::XContent >& _rxObject) const;
 };
 
 //........................................................................
