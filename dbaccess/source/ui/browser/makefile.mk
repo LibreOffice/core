8f6454346aa6bf1b5266b4feddaa9836b350559d
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:34:33 +0000

    INTEGRATION: CWS insight01 (1.12.104); FILE MERGED 2004/06/01 08:49:55 oj 1.12.104.4: RESYNC: (1.13-1.14); FILE MERGED 2003/08/28 10:04:31 oj 1.12.104.3: RESYNC: (1.12-1.13); FILE MERGED 2003/08/15 08:52:44 oj 1.12.104.2: #111075# ongoing new dbaapp work 2003/07/29 12:23:18 oj 1.12.104.1: #111075# insight changes

--- dbaccess/source/ui/browser/makefile.mk
+++ dbaccess/source/ui/browser/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: kz $ $Date: 2004-05-19 04:35:36 $
+#   last change: $Author: hr $ $Date: 2004-08-02 15:34:33 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -68,7 +68,6 @@ TARGET=browser
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-
 # --- Files --------------------------------------------------------
 
 EXCEPTIONSFILES=\
@@ -84,7 +83,6 @@ EXCEPTIONSFILES=\
             $(SLO)$/unodatbr.obj				\
             $(SLO)$/sbamultiplex.obj			\
             $(SLO)$/exsrcbrw.obj                \
-            $(SLO)$/dbtreelistbox.obj           \
             $(SLO)$/dbtreemodel.obj				\
             $(SLO)$/dsEntriesNoExp.obj			\
             $(SLO)$/dbtreeview.obj
