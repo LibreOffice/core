5bb1745a773c25932cdce0405850441f64c9cd7b
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:54:59 +0000

    INTEGRATION: CWS insight01 (1.1.2); FILE ADDED 2004/04/26 10:24:57 oj 1.1.2.3: show error message when user admin does not exist 2004/03/17 12:18:05 oj 1.1.2.2: #111075# new method to set title 2004/03/15 12:31:38 oj 1.1.2.1: #111075# ongoing work

---
+++ dbaccess/source/ui/inc/UserAdminDlg.hxx
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  $RCSfile: UserAdminDlg.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-08-02 15:54:59 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): Ocke Janssen
+ *
+ *
+ ************************************************************************/
+
+#ifndef DBAUI_USERADMINDLG_HXX
+#define DBAUI_USERADMINDLG_HXX
+
+#ifndef _SFXTABDLG_HXX
+#include <sfx2/tabdlg.hxx>
+#endif
+#ifndef _DBAUI_DSNTYPES_HXX_
+#include "dsntypes.hxx"
+#endif
+#ifndef DBAUI_ITEMSETHELPER_HXX
+#include "IItemSetHelper.hxx"
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+#ifndef _DBAUI_MODULE_DBU_HXX_
+#include "moduledbu.hxx"
+#endif
+#include <memory>
+
+FORWARD_DECLARE_INTERFACE(beans,XPropertySet)
+FORWARD_DECLARE_INTERFACE(sdbc,XConnection)
+FORWARD_DECLARE_INTERFACE(lang,XMultiServiceFactory)
+
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+    class ODbDataSourceAdministrationHelper;
+    //=========================================================================
+    //= OUserAdminDlg
+    //=========================================================================
+
+    /** implements the user adin dialog
+    */
+    class OUserAdminDlg : public SfxTabDialog, public IItemSetHelper, public IAdminHelper,public dbaui::OModuleClient
+    {
+        ::std::auto_ptr<ODbDataSourceAdministrationHelper>  m_pImpl;
+        SfxItemSet* m_pItemSet;
+        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection>          m_xConnection;
+    protected:
+        virtual void PageCreated(USHORT _nId, SfxTabPage& _rPage);
+    public:
+        OUserAdminDlg( Window* _pParent
+                            ,SfxItemSet* _pItems
+                            ,const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB
+                            ,const ::com::sun::star::uno::Any& _aDataSourceName);
+
+        virtual ~OUserAdminDlg();
+
+        virtual const SfxItemSet* getOutputSet() const;
+        virtual SfxItemSet* getWriteOutputSet();
+
+        virtual short   Execute();
+
+        // forwards to ODbDataSourceAdministrationHelper
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > getORB();
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection > createConnection();
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDriver > getDriver();
+        virtual DATASOURCE_TYPE getDatasourceType(const SfxItemSet& _rSet) const;
+        virtual void clearPassword();
+        virtual sal_Bool saveDatasource();
+        virtual void setTitle(const ::rtl::OUString& _sTitle);
+    };
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
+
+#endif // DBAUI_USERADMINDLG_HXX
