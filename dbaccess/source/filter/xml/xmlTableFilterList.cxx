d8201688d6185fcc9753d270d95decdfb2958716
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:23:30 +0000

    INTEGRATION: CWS insight01 (1.1.2); FILE ADDED 2003/12/17 10:01:27 oj 1.1.2.3: #111075# ongoing work 2003/11/28 06:59:09 oj 1.1.2.2: #111075# ongoing work 2003/10/24 06:15:24 oj 1.1.2.1: #i21643# initial revision of the import filter

---
+++ dbaccess/source/filter/xml/xmlTableFilterList.cxx
@@ -0,0 +1,152 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlTableFilterList.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-08-02 15:23:30 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef DBA_XMLTABLEFILTERLIST_HXX
+#include "xmlTableFilterList.hxx"
+#endif
+#ifndef DBA_XMLTABLEFILTERPATTERN_HXX
+#include "xmlTableFilterPattern.hxx"
+#endif
+#ifndef DBA_XMLENUMS_HXX
+#include "xmlEnums.hxx"
+#endif
+#ifndef _XMLOFF_XMLTOKEN_HXX
+#include <xmloff/xmltoken.hxx>
+#endif
+#ifndef _XMLOFF_XMLNMSPE_HXX
+#include <xmloff/xmlnmspe.hxx>
+#endif
+#ifndef _XMLOFF_NMSPMAP_HXX
+#include <xmloff/nmspmap.hxx>
+#endif
+#ifndef _XMLOFF_XMLTOKEN_HXX
+#include <xmloff/xmltoken.hxx>
+#endif
+#ifndef DBACCESS_SHARED_XMLSTRINGS_HRC
+#include "xmlstrings.hrc"
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+#ifndef _XMLOFF_XMLIMP_HXX
+#include <xmloff/xmlimp.hxx>
+#endif
+#ifndef DBA_XMLFILTER_HXX
+#include "xmlfilter.hxx"
+#endif
+
+namespace dbaxml
+{
+    using namespace ::rtl;
+    using namespace ::xmloff::token;
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::beans;
+    using namespace ::com::sun::star::xml::sax;
+
+OXMLTableFilterList::OXMLTableFilterList( SvXMLImport& rImport,
+                sal_uInt16 nPrfx
+                , const OUString& _sLocalName
+                ,sal_Bool _bNameFilter) :
+    SvXMLImportContext( rImport, nPrfx, _sLocalName )
+{
+}
+// -----------------------------------------------------------------------------
+
+OXMLTableFilterList::~OXMLTableFilterList()
+{
+}
+// -----------------------------------------------------------------------------
+
+SvXMLImportContext* OXMLTableFilterList::CreateChildContext(
+        sal_uInt16 nPrefix,
+        const OUString& rLocalName,
+        const Reference< XAttributeList > & xAttrList )
+{
+    SvXMLImportContext *pContext = 0;
+
+    if ( XML_NAMESPACE_DB == nPrefix )
+    {
+        GetImport().GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
+        if ( IsXMLToken( rLocalName, XML_TABLE_FILTER_PATTERN ) )
+            pContext = new OXMLTableFilterPattern( GetImport(), nPrefix, rLocalName,sal_True,*this);
+        else if ( IsXMLToken( rLocalName, XML_TABLE_TYPE ) )
+            pContext = new OXMLTableFilterPattern( GetImport(), nPrefix, rLocalName,sal_False,*this);
+    }
+
+    if( !pContext )
+        pContext = new SvXMLImportContext( GetImport(), nPrefix, rLocalName );
+
+    return pContext;
+}
+// -----------------------------------------------------------------------------
+void OXMLTableFilterList::EndElement()
+{
+    Reference<XPropertySet> xDataSource(GetImport().GetModel(),UNO_QUERY);
+    if ( xDataSource.is() )
+    {
+        if ( !m_aPatterns.empty() )
+            xDataSource->setPropertyValue(PROPERTY_TABLEFILTER,makeAny(Sequence< ::rtl::OUString>(m_aPatterns.begin(),m_aPatterns.size())));
+        if ( !m_aTypes.empty() )
+            xDataSource->setPropertyValue(PROPERTY_TABLETYPEFILTER,makeAny(Sequence< ::rtl::OUString>(m_aTypes.begin(),m_aTypes.size())));
+    }
+}
+//----------------------------------------------------------------------------
+} // namespace dbaxml
+// -----------------------------------------------------------------------------
