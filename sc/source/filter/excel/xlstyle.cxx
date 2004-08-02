6e99a2edf8e6c345d50330ac8d44abc6a7f4258d
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 15:58:51 +0000

    INTEGRATION: CWS dr14 (1.8.24); FILE MERGED 2004/06/03 13:57:22 dr 1.8.24.4: RESYNC: (1.9-1.10); FILE MERGED 2004/01/05 18:58:25 dr 1.8.24.3: RESYNC: (1.8-1.9); FILE MERGED 2003/12/18 13:54:47 dr 1.8.24.2: #i22640# SvxOrientationItem replaced by SfxBoolItem for stacked state #i23675# Preparations for import/export of diagonal lines 2003/11/26 14:56:52 dr 1.8.24.1: #i22640# new cell attribute 'shrink to fit'

--- sc/source/filter/excel/xlstyle.cxx
+++ sc/source/filter/excel/xlstyle.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xlstyle.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2004-07-30 16:21:25 $
+ *  last change: $Author: hr $ $Date: 2004-08-02 16:58:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1168,17 +1168,18 @@ XclCellAlign::XclCellAlign() :
     meOrient( xlTextOrient_Default ),
     mnRotation( 0 ),
     mnIndent( 0 ),
-    mbWrapped( false )
+    mbWrapped( false ),
+    mbShrink( false )
 {
 }
 
 bool operator==( const XclCellAlign& rLeft, const XclCellAlign& rRight )
 {
     return
-        (rLeft.meHorAlign == rRight.meHorAlign) && (rLeft.meVerAlign == rRight.meVerAlign) &&
-        (rLeft.meTextDir  == rRight.meTextDir)  &&
-        (rLeft.meOrient   == rRight.meOrient)   && (rLeft.mnRotation == rRight.mnRotation) &&
-        (rLeft.mnIndent   == rRight.mnIndent)   && (rLeft.mbWrapped  == rRight.mbWrapped);
+        (rLeft.meHorAlign == rRight.meHorAlign) && (rLeft.meVerAlign == rRight.meVerAlign)   &&
+        (rLeft.meTextDir  == rRight.meTextDir)  && (rLeft.meOrient   == rRight.meOrient)     &&
+        (rLeft.mnRotation == rRight.mnRotation) && (rLeft.mnIndent   == rRight.mnIndent)     &&
+        (rLeft.mbWrapped  == rRight.mbWrapped)  && (rLeft.mbShrink   == rRight.mbShrink);
 }
 
 // ----------------------------------------------------------------------------
@@ -1188,20 +1189,26 @@ XclCellBorder::XclCellBorder() :
     mnRightColor( 0 ),
     mnTopColor( 0 ),
     mnBottomColor( 0 ),
+    mnDiagColor( 0 ),
     mnLeftLine( EXC_LINE_NONE ),
     mnRightLine( EXC_LINE_NONE ),
     mnTopLine( EXC_LINE_NONE ),
-    mnBottomLine( EXC_LINE_NONE )
+    mnBottomLine( EXC_LINE_NONE ),
+    mnDiagLine( EXC_LINE_NONE ),
+    mbDiagTLtoBR( false ),
+    mbDiagBLtoTR( false )
 {
 }
 
 bool operator==( const XclCellBorder& rLeft, const XclCellBorder& rRight )
 {
     return
-        (rLeft.mnLeftColor == rRight.mnLeftColor) && (rLeft.mnRightColor  == rRight.mnRightColor)  &&
-        (rLeft.mnTopColor  == rRight.mnTopColor)  && (rLeft.mnBottomColor == rRight.mnBottomColor) &&
-        (rLeft.mnLeftLine  == rRight.mnLeftLine)  && (rLeft.mnRightLine   == rRight.mnRightLine)   &&
-        (rLeft.mnTopLine   == rRight.mnTopLine)   && (rLeft.mnBottomLine  == rRight.mnBottomLine);
+        (rLeft.mnLeftColor  == rRight.mnLeftColor)  && (rLeft.mnRightColor  == rRight.mnRightColor)  &&
+        (rLeft.mnTopColor   == rRight.mnTopColor)   && (rLeft.mnBottomColor == rRight.mnBottomColor) &&
+        (rLeft.mnLeftLine   == rRight.mnLeftLine)   && (rLeft.mnRightLine   == rRight.mnRightLine)   &&
+        (rLeft.mnTopLine    == rRight.mnTopLine)    && (rLeft.mnBottomLine  == rRight.mnBottomLine)  &&
+        (rLeft.mnDiagColor  == rRight.mnDiagColor)  && (rLeft.mnDiagLine    == rRight.mnDiagLine)    &&
+        (rLeft.mbDiagTLtoBR == rRight.mbDiagTLtoBR) && (rLeft.mbDiagBLtoTR  == rRight.mbDiagBLtoTR);
 }
 
 // ----------------------------------------------------------------------------
