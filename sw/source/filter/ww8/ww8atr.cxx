dddd7da63e622da1cf0aaba978a618d086850776
Kurt Zenker <kz@openoffice.org>
2004-08-02 13:20:52 +0000

    INTEGRATION: CWS swobjpos04 (1.77.14); FILE MERGED 2004/07/13 01:50:34 od 1.77.14.2: RESYNC: (1.77-1.81); FILE MERGED 2004/05/07 15:25:34 od 1.77.14.1: #i28701# - use which id of new attribute 'WrapInfluenceOnObjPos'

--- sw/source/filter/ww8/ww8atr.cxx
+++ sw/source/filter/ww8/ww8atr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ww8atr.cxx,v $
  *
- *  $Revision: 1.81 $
+ *  $Revision: 1.82 $
  *
- *  last change: $Author: rt $ $Date: 2004-06-17 13:48:42 $
+ *  last change: $Author: kz $ $Date: 2004-08-02 14:20:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -4843,7 +4843,7 @@ SwAttrFnTab aWW8AttrFnTab = {
 /* RES_FRMATR_DUMMY8 */             0, // Dummy:
 /* RES_FRMATR_DUMMY9 */             0, // Dummy:
 /* RES_FOLLOW_TEXT_FLOW */          0,
-/* RES_FRMATR_DUMMY1 */             0, // Dummy:
+/* RES_WRAP_INFLUENCE_ON_OBJPOS */  0,
 /* RES_FRMATR_DUMMY2 */             0, // Dummy:
 /* RES_FRMATR_DUMMY3 */             0, // Dummy:
 /* RES_FRMATR_DUMMY4 */             0, // Dummy:
