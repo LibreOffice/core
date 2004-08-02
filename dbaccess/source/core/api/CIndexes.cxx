89ca5feda804f48baf2a1d68bff7421724fc3b08
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 13:58:26 +0000

    INTEGRATION: CWS insight01 (1.11.104); FILE MERGED 2003/11/05 11:36:10 oj 1.11.104.1: #111075# ongoing work

--- dbaccess/source/core/api/CIndexes.cxx
+++ dbaccess/source/core/api/CIndexes.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CIndexes.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2002-10-30 14:56:51 $
+ *  last change: $Author: hr $ $Date: 2004-08-02 14:58:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,7 @@ Reference< XNamed > OIndexes::createObject(const ::rtl::OUString& _rName)
 {
     Reference< XNamed > xRet = NULL;
     if ( m_xIndexes.is() && m_xIndexes->hasByName(_rName) )
-        m_xIndexes->getByName(_rName) >>= xRet;
+        xRet.set(m_xIndexes->getByName(_rName),UNO_QUERY);
     else
         xRet = OIndexesHelper::createObject(_rName);
 
