4ee8cffb6ccbb6c7e74b82934ccd7170f487438e
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 13:31:49 +0000

    INTEGRATION: CWS insight01 (1.1.2); FILE ADDED 2004/07/07 08:37:30 oj 1.1.2.2: merge conflicts 2004/01/07 13:55:34 oj 1.1.2.1: #111075# ongoing work

---
+++ svtools/inc/DocumentInfoPreview.hxx
@@ -0,0 +1,98 @@
+/*************************************************************************
+ *
+ *  $RCSfile: DocumentInfoPreview.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-08-02 14:31:49 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): Ocke Janssen
+ *
+ *
+ ************************************************************************/
+#ifndef SVTOOLS_DOCUMENTINFOPREVIEW_HXX
+#define SVTOOLS_DOCUMENTINFOPREVIEW_HXX
+
+#ifndef _SV_WINDOW_HXX
+#include <vcl/window.hxx>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_LOCALE_HPP_
+#include <com/sun/star/lang/Locale.hpp>
+#endif
+
+class SvtExtendedMultiLineEdit_Impl;
+class SvtDocInfoTable_Impl;
+namespace svtools
+{
+    class ODocumentInfoPreview : public Window
+    {
+        SvtExtendedMultiLineEdit_Impl*  m_pEditWin;
+        SvtDocInfoTable_Impl*           m_pInfoTable;
+        com::sun::star::lang::Locale    m_aLocale;
+
+    public:
+        ODocumentInfoPreview( Window* pParent ,WinBits _nBits);
+        virtual ~ODocumentInfoPreview();
+
+        virtual void Resize();
+        void    Clear();
+        void    fill(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet>& _xDocInfo
+                    ,const String& rURL);
+        void    InsertEntry( const String& rTitle, const String& rValue );
+        void SetAutoScroll(BOOL _bAutoScroll);
+    };
+}
+
+#endif // SVTOOLS_DOCUMENTINFOPREVIEW_HXX
+
