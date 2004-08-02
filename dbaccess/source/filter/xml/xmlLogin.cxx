1ac6bed0aeb2df10928f2e10095622089b67e17b
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 14:22:18 +0000

    INTEGRATION: CWS insight01 (1.1.2); FILE ADDED 2004/04/06 11:01:45 oj 1.1.2.3: ongoing work 2004/04/06 10:59:49 oj 1.1.2.2: ongoing work 2003/10/24 06:16:52 oj 1.1.2.1: #i21643# initial revision of the import filter

---
+++ dbaccess/source/filter/xml/xmlLogin.cxx
@@ -0,0 +1,139 @@
+/*************************************************************************
+ *
+ *  $RCSfile: xmlLogin.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-08-02 15:22:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the License); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an AS IS basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef DBA_XMLLOGIN_HXX
+#include "xmlLogin.hxx"
+#endif
+#ifndef DBA_XMLFILTER_HXX
+#include "xmlfilter.hxx"
+#endif
+#ifndef _XMLOFF_XMLTOKEN_HXX
+#include <xmloff/xmltoken.hxx>
+#endif
+#ifndef _XMLOFF_XMLNMSPE_HXX
+#include <xmloff/xmlnmspe.hxx>
+#endif
+#ifndef _XMLOFF_NMSPMAP_HXX
+#include <xmloff/nmspmap.hxx>
+#endif
+#ifndef DBA_XMLENUMS_HXX
+#include "xmlEnums.hxx"
+#endif
+#ifndef DBACCESS_SHARED_XMLSTRINGS_HRC
+#include "xmlstrings.hrc"
+#endif
+
+
+#include <vector>
+
+namespace dbaxml
+{
+    using namespace ::rtl;
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::xml::sax;
+
+OXMLLogin::OXMLLogin( ODBFilter& rImport,
+                sal_uInt16 nPrfx, const OUString& _sLocalName,
+                const Reference< XAttributeList > & _xAttrList ) :
+    SvXMLImportContext( rImport, nPrfx, _sLocalName )
+{
+    OSL_ENSURE(_xAttrList.is(),"Attribute list is NULL!");
+    const SvXMLNamespaceMap& rMap = rImport.GetNamespaceMap();
+    const SvXMLTokenMap& rTokenMap = rImport.GetLoginElemTokenMap();
+
+    Reference<XPropertySet> xDataSource(rImport.GetModel(),UNO_QUERY);
+
+    sal_Int16 nLength = (xDataSource.is() && _xAttrList.is()) ? _xAttrList->getLength() : 0;
+    static const ::rtl::OUString s_sTRUE = ::xmloff::token::GetXMLToken(XML_TRUE);
+    for(sal_Int16 i = 0; i < nLength; ++i)
+    {
+        OUString sLocalName;
+        rtl::OUString sAttrName = _xAttrList->getNameByIndex( i );
+        sal_uInt16 nPrefix = rMap.GetKeyByAttrName( sAttrName,&sLocalName );
+        rtl::OUString sValue = _xAttrList->getValueByIndex( i );
+
+        try
+        {
+            switch( rTokenMap.Get( nPrefix, sLocalName ) )
+            {
+                case XML_TOK_USER_NAME:
+                    xDataSource->setPropertyValue(PROPERTY_USER,makeAny(sValue));
+                    break;
+                case XML_TOK_IS_PASSWORD_REQUIRED:
+                    xDataSource->setPropertyValue(PROPERTY_ISPASSWORDREQUIRED,makeAny((sValue == s_sTRUE ? sal_True : sal_False)));
+                    break;
+            }
+        }
+        catch(Exception)
+        {
+            OSL_ENSURE(0,"OXMLLogin::OXMLLogin: Could not set login!");
+        }
+    }
+}
+// -----------------------------------------------------------------------------
+
+OXMLLogin::~OXMLLogin()
+{
+}
+// -----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+} // namespace dbaxml
+// -----------------------------------------------------------------------------
