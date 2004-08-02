73ca905129f8b20b474cdb258b993e1cc357be78
Kurt Zenker <kz@openoffice.org>
2004-08-02 13:21:07 +0000

    INTEGRATION: CWS swobjpos04 (1.116.16); FILE MERGED 2004/07/13 01:50:41 od 1.116.16.3: RESYNC: (1.116-1.119); FILE MERGED 2004/07/02 14:59:59 od 1.116.16.2: #i28701# - further adjustments for the new object positioning 2004/06/30 12:42:37 od 1.116.16.1: #i28701# - merging changes of cws swdrawpositioning

--- sw/source/filter/ww8/ww8graf.cxx
+++ sw/source/filter/ww8/ww8graf.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ww8graf.cxx,v $
  *
- *  $Revision: 1.120 $
+ *  $Revision: 1.121 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-12 13:34:57 $
+ *  last change: $Author: kz $ $Date: 2004-08-02 14:21:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
