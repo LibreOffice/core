a653b9386f296755da3a6a528a2b53631bf1182d
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-02 16:33:37 +0000

    INTEGRATION: CWS insight01 (1.3.182); FILE MERGED 2004/04/16 12:07:05 oj 1.3.182.3: correct alignment and default 2004/03/05 10:58:11 oj 1.3.182.2: WizardPage changes 2004/03/05 07:38:47 oj 1.3.182.1: #111090# changes for the new prop dialogs

--- extensions/source/abpilot/abpfinalpage.cxx
+++ extensions/source/abpilot/abpfinalpage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: abpfinalpage.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-25 16:00:40 $
+ *  last change: $Author: hr $ $Date: 2004-08-02 17:33:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -71,12 +71,42 @@
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
 #endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+#ifndef _UNOTOOLS_UCBHELPER_HXX
+#include <unotools/ucbhelper.hxx>
+#endif
+#ifndef _FILEDLGHELPER_HXX
+#include <sfx2/filedlghelper.hxx>
+#endif
+#ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
+#include <svtools/pathoptions.hxx>
+#endif
+#ifndef SVTOOLS_FILENOTATION_HXX_
+#include <svtools/filenotation.hxx>
+#endif
+#ifndef _SFX_DOCFILT_HACK_HXX
+#include <sfx2/docfilt.hxx>
+#endif
+#ifndef _SV_MSGBOX_HXX
+#include <vcl/msgbox.hxx>
+#endif
 
 //.........................................................................
 namespace abp
 {
 //.........................................................................
+    using namespace ::svt;
+    using namespace ::utl;
 
+    const SfxFilter* lcl_getBaseFilter()
+    {
+        static const String s_sDatabaseType = String::CreateFromAscii("StarOffice XML (Base)");
+        const SfxFilter* pFilter = SfxFilter::GetFilterByName( s_sDatabaseType);
+        OSL_ENSURE(pFilter,"Filter: StarOffice XML (Base) could not be found!");
+        return pFilter;
+    }
     //=====================================================================
     //= FinalPage
     //=====================================================================
@@ -86,11 +116,26 @@ namespace abp
         ,m_aExplanation         ( this, ResId( FT_FINISH_EXPL ) )
         ,m_aNameLabel           ( this, ResId( FT_NAME_EXPL ) )
         ,m_aName                ( this, ResId( ET_DATASOURCENAME ) )
+        ,m_aLocationLabel       ( this, ResId( FT_LOCATION ) )
+        ,m_aLocation            ( this, ResId( CBB_LOCATION ) )
+        ,m_aBrowse              ( this, ResId( PB_BROWSE ) )
+        ,m_aRegisterName        ( this, ResId( CB_REGISTER_DS ) )
         ,m_aDuplicateNameError  ( this, ResId( FT_DUPLICATENAME ) )
+        ,m_bCheckFileName       (sal_True)
     {
         FreeResource();
 
         m_aName.SetModifyHdl( LINK(this, FinalPage, OnNameModified) );
+        m_aLocation.SetModifyHdl( LINK(this, FinalPage, OnNameModified) );
+        m_aBrowse.SetClickHdl( LINK( this, FinalPage, OnBrowse ) );
+        m_aRegisterName.SetClickHdl( LINK( this, FinalPage, OnRegister ) );
+        m_aRegisterName.Check(TRUE);
+
+        const SfxFilter* pFilter = lcl_getBaseFilter();
+        if ( pFilter )
+        {
+            m_aLocation.SetFilter(pFilter->GetDefaultExtension());
+        }
     }
 
     //---------------------------------------------------------------------
@@ -110,14 +155,47 @@ namespace abp
     }
 
     //---------------------------------------------------------------------
+    void FinalPage::setFields()
+    {
+        AddressSettings& rSettings = getSettings();
+
+        INetURLObject aURL( rSettings.sDataSourceName );
+        if( aURL.GetProtocol() == INET_PROT_NOT_VALID )
+        {
+            String sPath = SvtPathOptions().GetWorkPath();
+            sPath += '/';
+            sPath += String(rSettings.sDataSourceName);
+
+            const SfxFilter* pFilter = lcl_getBaseFilter();
+            if ( pFilter )
+            {
+                String sExt = pFilter->GetDefaultExtension();
+                sPath += sExt.GetToken(1,'*');
+            }
+
+            aURL.SetURL(sPath);
+        }
+        OSL_ENSURE( aURL.GetProtocol() != INET_PROT_NOT_VALID ,"No valid file name!");
+        rSettings.sDataSourceName = aURL.GetMainURL( INetURLObject::NO_DECODE );
+        OFileNotation aFileNotation( rSettings.sDataSourceName );
+        m_aLocation.SetText(aFileNotation.get(OFileNotation::N_SYSTEM));
+        String sName = aURL.getName( );
+        xub_StrLen nPos = sName.Search(aURL.GetExtension());
+        if ( nPos != STRING_NOTFOUND )
+        {
+            sName.Erase(nPos-1,4);
+        }
+        m_aName.SetText(sName);
+
+        OnRegister(&m_aRegisterName);
+    }
+
+    //---------------------------------------------------------------------
     void FinalPage::initializePage()
     {
         AddressBookSourcePage::initializePage();
 
-        const AddressSettings& rSettings = getSettings();
-        m_aName.SetText(rSettings.sDataSourceName);
-
-        implCheckName();
+        setFields();
     }
 
     //---------------------------------------------------------------------
@@ -126,8 +204,24 @@ namespace abp
         if (!AddressBookSourcePage::commitPage(_eReason))
             return sal_False;
 
+        OFileNotation aFileNotation( m_aLocation.GetText() );
+        ::rtl::OUString sURL = aFileNotation.get(OFileNotation::N_URL);
+        // check if the name exists
+        if ( m_bCheckFileName )
+        {
+            if ( ::utl::UCBContentHelper::Exists( sURL ) )
+            {
+                QueryBox aBox( this, WB_YES_NO, ModuleRes( RID_STR_ALREADYEXISTOVERWRITE ) );
+                if ( aBox.Execute() != RET_YES )
+                    return sal_False;
+            }
+        }
+
         AddressSettings& rSettings = getSettings();
-        rSettings.sDataSourceName = m_aName.GetText();
+        rSettings.sDataSourceName = sURL;
+        rSettings.bRegisterDataSource = m_aRegisterName.IsChecked();
+        if ( rSettings.bRegisterDataSource )
+            rSettings.sRegisteredDataSourceName = m_aName.GetText();
 
         return sal_True;
     }
@@ -143,17 +237,11 @@ namespace abp
 
         // in real, the data source which this dialog should create, is already part of the context
         // as it's name is - of course - a valid name, we have to remove it from the bag
-        DBG_ASSERT( getDialog()->getDataSource().getName() == getSettings().sDataSourceName,
-            "FinalPage::ActivatePage: inconsistent names!" );
-            // only we ourself or the "admin dialog invokation" page are allowed to produce such an inconsistence,
-            // and both should - immediately after doing so - rename the data source
-        StringBagIterator aPos = m_aInvalidDataSourceNames.find( getSettings().sDataSourceName );
-        DBG_ASSERT( m_aInvalidDataSourceNames.end() != aPos, "FinalPage::ActivatePage: invalid settings!" );
-        if ( m_aInvalidDataSourceNames.end() != aPos )
-            m_aInvalidDataSourceNames.erase( aPos );
+//      DBG_ASSERT( getDialog()->getDataSource().getName() == getSettings().sDataSourceName,
+//          "FinalPage::ActivatePage: inconsistent names!" );
 
         // give the name edit the focus
-        m_aName.GrabFocus();
+        m_aLocation.GrabFocus();
 
         // default the finish button
         getDialog()->defaultButton( WZB_FINISH );
@@ -181,21 +269,64 @@ namespace abp
     {
         sal_Bool bValidName = isValidName();
         sal_Bool bEmptyName = 0 == m_aName.GetText().Len();
+        sal_Bool bEmptyLocation = 0 == m_aLocation.GetText().Len();
 
         // enable or disable the finish button
-        getDialog()->enableButtons( WZB_FINISH, bValidName );
+        getDialog()->enableButtons( WZB_FINISH, !bEmptyLocation && (!m_aRegisterName.IsChecked() || bValidName) );
 
         // show the error message for an invalid name
         m_aDuplicateNameError.Show( !bValidName && !bEmptyName );
     }
 
     //---------------------------------------------------------------------
-    IMPL_LINK( FinalPage, OnNameModified, void*, NOTINTERESTEDIN )
+    IMPL_LINK( FinalPage, OnNameModified, Edit*, _pEdit)
     {
+        if ( _pEdit == &m_aLocation )
+            m_bCheckFileName = sal_True;
+
         implCheckName();
         return 0L;
     }
+    // -----------------------------------------------------------------------------
+    IMPL_LINK( FinalPage, OnBrowse, PushButton*, EMPTYARG )
+    {
+        OFileNotation aOldFile( m_aLocation.GetText() );
+        WinBits nBits(WB_STDMODAL|WB_SAVEAS);
+        ::sfx2::FileDialogHelper aFileDlg( ::sfx2::FILESAVE_AUTOEXTENSION,static_cast<sal_uInt32>(nBits) ,this);
+        aFileDlg.SetDisplayDirectory( aOldFile.get(OFileNotation::N_URL) );
 
+        static const String s_sDatabaseType = String::CreateFromAscii("StarOffice XML (Base)");
+        const SfxFilter* pFilter = SfxFilter::GetFilterByName( s_sDatabaseType);
+        OSL_ENSURE(pFilter,"Filter: StarOffice XML (Base) could not be found!");
+        if ( pFilter )
+        {
+            aFileDlg.AddFilter(pFilter->GetFilterName(),pFilter->GetDefaultExtension());
+            aFileDlg.SetCurrentFilter(pFilter->GetFilterName());
+        }
+
+        if ( aFileDlg.Execute() == ERRCODE_NONE )
+        {
+            INetURLObject aURL( aFileDlg.GetPath() );
+            if( aURL.GetProtocol() != INET_PROT_NOT_VALID )
+            {
+                OFileNotation aFileNotation( aURL.GetMainURL( INetURLObject::NO_DECODE ) );
+                m_aLocation.SetText(aFileNotation.get(OFileNotation::N_SYSTEM));
+                implCheckName();
+                m_bCheckFileName = sal_False;
+            }
+        }
+
+        return 0L;
+    }
+    // -----------------------------------------------------------------------------
+    IMPL_LINK( FinalPage, OnRegister, CheckBox*, EMPTYARG )
+    {
+        BOOL bEnable = m_aRegisterName.IsChecked();
+        m_aNameLabel.Enable(bEnable);
+        m_aName.Enable(bEnable);
+        implCheckName();
+        return 0L;
+    }
 //.........................................................................
 }   // namespace abp
 //.........................................................................
