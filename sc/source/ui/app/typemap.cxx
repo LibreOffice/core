73e9f6be5862797dbfca4c7e4d49aa8e763de8cd
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 12:28:08 +0000

    INTEGRATION: CWS aw017 (1.11.64); FILE MERGED 2004/07/29 14:09:09 aw 1.11.64.1: #i25616#

--- sc/source/ui/app/typemap.cxx
+++ sc/source/ui/app/typemap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: typemap.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-06 12:53:34 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 13:28:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -149,6 +149,10 @@
 #include <svtools/ilstitem.hxx>
 #include <svtools/globalnameitem.hxx>
 #include <svx/chrtitem.hxx>
+
+// #i25616#
+#include <svx/sdshitm.hxx>
+
 #include <svtools/aeitem.hxx>
 #include "attrib.hxx"
 
