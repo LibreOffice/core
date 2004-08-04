b584fd1745e0499cadcad36fa44d050592dee31b
Rüdiger Timm <rt@openoffice.org>
2004-08-04 07:58:57 +0000

    INTEGRATION: CWS impress18ea (1.2.10); FILE MERGED 2004/07/28 14:54:24 af 1.2.10.1: #i31401# Removed inclusion of unused Decorator.hxx header.

--- sd/source/ui/toolpanel/TitledControl.cxx
+++ sd/source/ui/toolpanel/TitledControl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TitledControl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-13 14:40:12 $
+ *  last change: $Author: rt $ $Date: 2004-08-04 08:58:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,6 @@
 #include "TitledControl.hxx"
 
 #include "AccessibleTitledControl.hxx"
-#include "Decorator.hxx"
 #include "ControlContainer.hxx"
 #include "TaskPaneFocusManager.hxx"
 
