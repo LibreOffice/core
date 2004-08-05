847602b56418e2e9855f317f2f07f00c7181aab8
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-05 10:08:37 +0000

    #i10000#: use GetActiveWindow() instead of pWindow

--- sd/source/ui/view/drviews6.cxx
+++ sd/source/ui/view/drviews6.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drviews6.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2004-08-02 10:10:16 $
+ *  last change: $Author: hr $ $Date: 2004-08-05 11:08:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -617,7 +617,7 @@ void DrawViewShell::FuTemp04(SfxRequest& rReq)
                     }
 
                     pFormatClipboard->Copy( *pDrView, bPersistentCopy );
-                    pFuActual = new FuFormatPaintBrush( this, pWindow, pDrView, GetDoc(), rReq );
+                    pFuActual = new FuFormatPaintBrush( this, GetActiveWindow(), pDrView, GetDoc(), rReq );
                     GetViewFrame()->GetBindings().Invalidate(SID_FORMATPAINTBRUSH);
                 }
             }
