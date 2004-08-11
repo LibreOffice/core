123668af37c0bcf84356d2a7fe382ec472da350a
Oliver Bolte <obo@openoffice.org>
2004-08-11 14:40:42 +0000

    INTEGRATION: CWS tleamisc (1.2.154); FILE MERGED 2004/08/10 13:28:04 sw 1.2.154.1: #i23294#

--- sw/qa/unoapi/makefile.mk
+++ sw/qa/unoapi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Date: 2004-01-05 17:00:35 $
+#   last change: $Date: 2004-08-11 15:40:42 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -59,7 +59,6 @@
 #
 #
 #*************************************************************************
-
 PRJ=..$/..
 
 PRJNAME=sw
