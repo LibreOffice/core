d5071e3be6a406e6796189741f9463ac196cad46
Oliver Bolte <obo@openoffice.org>
2004-08-11 08:03:13 +0000

    INTEGRATION: CWS encryption (1.2.280); FILE MERGED 2004/07/16 17:43:07 jmarmion 1.2.280.2: #115980# - complete password stuff for calc. 2004/03/23 17:40:19 dr 1.2.280.1: #115980# using interaction handler for passwords

--- sc/source/filter/ftools/fapihelper.cxx
+++ sc/source/filter/ftools/fapihelper.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fapihelper.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-26 18:04:50 $
+ *  last change: $Author: obo $ $Date: 2004-08-11 09:03:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,16 +59,39 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
-#pragma hdrstop
-
 // ============================================================================
 
 #ifndef SC_FAPIHELPER_HXX
 #include "fapihelper.hxx"
 #endif
+
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
+#include <com/sun/star/task/XInteractionHandler.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONREQUEST_HPP_
+#include <com/sun/star/task/XInteractionRequest.hpp>
+#endif
+
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+// no include guard in this header!!!
+//#ifndef ...
+#include <svtools/docpasswdrequest.hxx>
+//#endif
+#ifndef _SFXDOCFILE_HXX
+#include <sfx2/docfile.hxx>
+#endif
+#ifndef _SFXSTRITEM_HXX //SfxStringItem
+#include <svtools/stritem.hxx>
+#endif
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+#ifndef _SFXSIDS_HRC
+#include <sfx2/sfxsids.hrc>
+#endif
+
 
 using ::rtl::OUString;
 using ::com::sun::star::uno::Any;
@@ -76,7 +99,8 @@ using ::com::sun::star::uno::Reference;
 using ::com::sun::star::uno::Exception;
 using ::com::sun::star::beans::XPropertySet;
 using ::com::sun::star::beans::XPropertySetInfo;
-
+using ::com::sun::star::task::XInteractionHandler;
+using ::com::sun::star::task::XInteractionRequest;
 
 // Set properties =============================================================
 
@@ -93,7 +117,6 @@ void setPropAny( const Reference< XPropertySet >& xProp, const OUString& rName, 
     {
     }
 }
-
 
 // Get properties =============================================================
 
@@ -114,6 +137,42 @@ bool getPropAny( Any& rAny, const Reference< XPropertySet >& rxProp, const OUStr
     {
     }
     return bSuccess;
+}
+
+// Static helper functions ====================================================
+
+String ScfApiHelper::QueryPasswordForMedium( SfxMedium& rMedium )
+{
+    String aPassw;
+    const SfxItemSet* pSet = rMedium.GetItemSet();
+    const SfxPoolItem *pPasswordItem;
+
+    if(pSet && SFX_ITEM_SET == pSet->GetItemState(SID_PASSWORD, TRUE, &pPasswordItem))
+        aPassw = ((const SfxStringItem *)pPasswordItem)->GetValue();
+    else
+    {
+        try
+        {
+            Reference< XInteractionHandler > xHandler( rMedium.GetInteractionHandler() );
+            if( xHandler.is() )
+            {
+                RequestDocumentPassword* pRequest = new RequestDocumentPassword(
+                    ::com::sun::star::task::PasswordRequestMode_PASSWORD_ENTER,
+                    INetURLObject( rMedium.GetOrigURL() ).GetName( INetURLObject::DECODE_WITH_CHARSET ) );
+                Reference< XInteractionRequest > xRequest( pRequest );
+
+               xHandler->handle( xRequest );
+
+               if( pRequest->isPassword() )
+                   aPassw = pRequest->getPassword();
+            }
+        }
+        catch( Exception& )
+        {
+        }
+    }
+
+    return aPassw;
 }
 
 
@@ -188,5 +247,5 @@ bool ScfMultiPSHelper::setPropertyValues(
 }
 
 
-//___________________________________________________________________
+// ============================================================================
 
