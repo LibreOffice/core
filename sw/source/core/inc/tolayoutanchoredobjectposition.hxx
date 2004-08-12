dd1e47e20b6e2355e0f38ddd80bcef4f40f6edf2
Oliver Bolte <obo@openoffice.org>
2004-08-12 11:29:13 +0000

    INTEGRATION: CWS tune05 (1.2.162); FILE MERGED 2004/07/23 16:05:45 mhu 1.2.162.2: RESYNC: (1.2-1.3); FILE MERGED 2004/07/22 10:59:31 cmc 1.2.162.1: #i30554# unused SwToLayoutAnchoredObjectPosition::GetFlyLayFrmOfObj

--- sw/source/core/inc/tolayoutanchoredobjectposition.hxx
+++ sw/source/core/inc/tolayoutanchoredobjectposition.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tolayoutanchoredobjectposition.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hjs $ $Date: 2004-06-28 13:37:15 $
+ *  last change: $Author: obo $ $Date: 2004-08-12 12:29:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -84,7 +84,8 @@ namespace objectpositioning
     {
         private:
             // calculated data for object position type TO_LAYOUT
-            Point maRelPos;
+            Point       maRelPos;
+
             // --> OD 2004-06-17 #i26791#
             // determine offset to frame anchor position according to the
             // positioning alignments
