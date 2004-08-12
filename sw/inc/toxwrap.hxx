e2f30a729e665c1c3bbda67e0ae6c9f00e6c56bc
Oliver Bolte <obo@openoffice.org>
2004-08-12 11:07:50 +0000

    INTEGRATION: CWS tune05 (1.2.564); FILE MERGED 2004/07/21 15:55:17 cmc 1.2.564.1: #i30554# GetLocaleList only usage in cnttab.cxx is gone

--- sw/inc/toxwrap.hxx
+++ sw/inc/toxwrap.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: toxwrap.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-24 10:52:51 $
+ *  last change: $Author: obo $ $Date: 2004-08-12 12:07:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -106,9 +106,6 @@ public:
 
     STAR_NMSPC::uno::Sequence< ::rtl::OUString >
     GetAlgorithmList( const STAR_NMSPC::lang::Locale& rLcl ) const;
-
-    STAR_NMSPC::uno::Sequence < com::sun::star::lang::Locale >
-    GetLocaleList() const;
 
     sal_Bool LoadAlgorithm( const STAR_NMSPC::lang::Locale& rLcl,
                             const String& sSortAlgorithm, long nOptions ) const;
