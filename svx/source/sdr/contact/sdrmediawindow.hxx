202555fd961ff683dc440eb7f9418678911baa36
Oliver Bolte <obo@openoffice.org>
2004-08-12 08:04:25 +0000

    INTEGRATION: CWS jmf2 (1.1.2); FILE ADDED 2004/07/11 14:34:59 ka 1.1.2.1: #i3316#: initial revision

---
+++ svx/source/sdr/contact/sdrmediawindow.hxx
@@ -0,0 +1,103 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sdrmediawindow.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-08-12 09:04:25 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_SDRMEDIAWIMNDOW_HXX
+
+#include <avmedia/mediawindow.hxx>
+
+namespace sdr { namespace contact {
+
+// ------------------
+// - SdrMediaWindow -
+// ------------------
+
+class ViewObjectContactOfSdrMediaObj;
+
+class SdrMediaWindow : public ::avmedia::MediaWindow
+{
+public:
+
+                            SdrMediaWindow( Window* pParent, ViewObjectContactOfSdrMediaObj& rViewObjContact );
+                            ~SdrMediaWindow();
+
+        virtual void        MouseMove( const MouseEvent& rMEvt );
+        virtual void        MouseButtonDown( const MouseEvent& rMEvt );
+        virtual void        MouseButtonUp( const MouseEvent& rMEvt );
+
+        virtual void        KeyInput( const KeyEvent& rKEvt );
+        virtual void        KeyUp( const KeyEvent& rKEvt );
+
+        virtual void        Command( const CommandEvent& rCEvt );
+
+        virtual sal_Int8    AcceptDrop( const AcceptDropEvent& rEvt );
+        virtual sal_Int8    ExecuteDrop( const ExecuteDropEvent& rEvt );
+
+        virtual void        StartDrag( sal_Int8 nAction, const Point& rPosPixel );
+
+private:
+
+    ViewObjectContactOfSdrMediaObj& mrViewObjectContactOfSdrMediaObj;
+};
+
+} }
+
+#endif
+
