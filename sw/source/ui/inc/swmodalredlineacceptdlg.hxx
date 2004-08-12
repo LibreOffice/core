c8d20c9f2107e5779b71117245191a2c7b4a5a3b
Oliver Bolte <obo@openoffice.org>
2004-08-12 12:07:31 +0000

    INTEGRATION: CWS tune05 (1.2.68); FILE MERGED 2004/07/21 15:32:57 cmc 1.2.68.1: #i30554# remove new newline warnings

--- sw/source/ui/inc/swmodalredlineacceptdlg.hxx
+++ sw/source/ui/inc/swmodalredlineacceptdlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swmodalredlineacceptdlg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:30:04 $
+ *  last change: $Author: obo $ $Date: 2004-08-12 13:07:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
