3f920858880ab0b105ff654c5230e1b2d867a8e1
Oliver Bolte <obo@openoffice.org>
2004-08-12 09:17:04 +0000

    INTEGRATION: CWS jmf2 (1.22.156); FILE MERGED 2004/07/21 11:16:37 ka 1.22.156.1: #i3316#: media support

--- sw/source/ui/uiview/viewstat.cxx
+++ sw/source/ui/uiview/viewstat.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewstat.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: kz $ $Date: 2004-08-02 13:11:25 $
+ *  last change: $Author: obo $ $Date: 2004-08-12 10:17:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -382,6 +382,7 @@ void SwView::GetState(SfxItemSet &rSet)
             }
             break;
         case SID_GALLERY :
+        case SID_AVMEDIA_PLAYER :
         case FN_REDLINE_ACCEPT:
             {
                 SfxViewFrame* pVFrame = GetViewFrame();
