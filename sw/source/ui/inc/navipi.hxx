e96efccc5160e9302e6ee622496b2c1ecaa65ca5
Oliver Bolte <obo@openoffice.org>
2004-08-12 12:05:45 +0000

    INTEGRATION: CWS tune05 (1.9.740); FILE MERGED 2004/07/23 16:44:20 mhu 1.9.740.2: RESYNC: (1.9-1.10); FILE MERGED 2004/07/22 08:12:46 cmc 1.9.740.1: #i30554# SwNavigationPI::IsInDrag unused

--- sw/source/ui/inc/navipi.hxx
+++ sw/source/ui/inc/navipi.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: navipi.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 15:09:55 $
+ *  last change: $Author: obo $ $Date: 2004-08-12 13:05:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -218,7 +218,6 @@ public:
 
     USHORT          GetRegionDropMode() const {return nRegionMode;}
     void            SetRegionDropMode(USHORT nNewMode);
-    BOOL            IsInDrag() const;
 
     sal_Int8        AcceptDrop( const AcceptDropEvent& rEvt );
     sal_Int8        ExecuteDrop( const ExecuteDropEvent& rEvt );
