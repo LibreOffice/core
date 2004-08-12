20b09bb52950c570c6f4345c290e0d542016747b
Oliver Bolte <obo@openoffice.org>
2004-08-12 08:00:29 +0000

    INTEGRATION: CWS jmf2 (1.1.2); FILE ADDED 2004/07/14 17:23:20 ka 1.1.2.2: #i3316#: uno support for media objects 2004/07/01 15:30:56 ka 1.1.2.1: #i3316#: initial revision

---
+++ svx/inc/svx/sdr/contact/viewcontactofsdrmediaobj.hxx
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  $RCSfile: viewcontactofsdrmediaobj.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-08-12 09:00:29 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFSDRMEDIAOBJ_HXX
+#define _SDR_CONTACT_VIEWCONTACTOFSDRMEDIAOBJ_HXX
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFSDROBJ_HXX
+#include <svx/sdr/contact/viewcontactofsdrobj.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class SdrMediaObj;
+namespace avmedia { class MediaItem; }
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class ViewContactOfSdrMediaObj : public ViewContactOfSdrObj
+        {
+            friend class ViewObjectContactOfSdrMediaObj;
+
+        public:
+
+            // basic constructor, used from SdrObject.
+            ViewContactOfSdrMediaObj( SdrMediaObj& rMediaObj );
+
+            // The destructor. When PrepareDelete() was not called before (see there)
+            // warnings will be generated in debug version if there are still contacts
+            // existing.
+            virtual ~ViewContactOfSdrMediaObj();
+
+            // Paint this object. This is before evtl. SubObjects get painted. It needs to return
+            // sal_True when something was pained and the paint output rectangle in rPaintRectangle.
+            virtual sal_Bool PaintObject(DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC);
+
+        public:
+
+            bool    hasPreferredSize() const;
+            Size    getPreferredSize() const;
+
+            void    updateMediaItem( ::avmedia::MediaItem& rItem ) const;
+            void    executeMediaItem( const ::avmedia::MediaItem& rItem );
+
+        protected:
+
+            // Create a Object-Specific ViewObjectContact, set ViewContact and
+            // ObjectContact. Always needs to return something.
+            virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact);
+
+            // Does this ViewContact support animation?
+            virtual sal_Bool SupportsAnimation() const;
+
+            // get notified if some properties have changed
+            virtual void mediaPropertiesChanged( const ::avmedia::MediaItem& rNewState );
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWCONTACTOFSDRMEDIAOBJ_HXX
