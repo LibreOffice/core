10313a6b2e6d10ac24e6add0db0d2e9f8c975efe
Oliver Bolte <obo@openoffice.org>
2004-08-12 11:02:50 +0000

    INTEGRATION: CWS unotlc (1.3.20); FILE MERGED 2004/07/30 13:19:22 dbo 1.3.20.1: #i31111# API revision Issue number: Submitted by: Reviewed by:

--- offapi/com/sun/star/deployment/makefile.mk
+++ offapi/com/sun/star/deployment/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2004-06-11 11:51:33 $
+#   last change: $Author: obo $ $Date: 2004-08-12 12:02:50 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -74,7 +74,6 @@ IDLFILES = \
         XPackage.idl                                            \
         XPackageRegistry.idl                                    \
         PackageRegistryBackend.idl                              \
-        PackageRegistry.idl                                     \
         XPackageManager.idl                                     \
         XPackageManagerFactory.idl                              \
         thePackageManagerFactory.idl
