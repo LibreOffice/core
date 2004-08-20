2ea01a518e4d4c79618818bd34525208de40845a
Rüdiger Timm <rt@openoffice.org>
2004-08-20 12:00:14 +0000

    INTEGRATION: CWS cfgruggedised (1.2.406); FILE MERGED 2004/07/23 13:44:59 jb 1.2.406.1: #i28654# Provide an InteractionHandler to the configuration during startup

--- desktop/source/app/configinit.hxx
+++ desktop/source/app/configinit.hxx
@@ -32,4 +32,45 @@ extern
 com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >
     CreateApplicationConfigurationProvider( );
 
+//-----------------------------------------------------------------------------
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
+#include <com/sun/star/task/XInteractionHandler.hpp>
+#endif
+
+/**
+    sets an InteractionHandler for configuration errors in the current context.
+
+  */
+    class ConfigurationErrorHandler
+    {
+    public:
+        typedef com::sun::star::uno::Reference< com::sun::star::task::XInteractionHandler > InteractionHandler;
+
+        ConfigurationErrorHandler()
+        : m_pContext(0), m_xHandler( getDefaultInteractionHandler())
+        {}
+
+        ConfigurationErrorHandler(const InteractionHandler & xHandler)
+        : m_pContext(0), m_xHandler( xHandler )
+        {}
+
+        ~ConfigurationErrorHandler();
+
+        static InteractionHandler getDefaultInteractionHandler();
+
+        /// installs the handler into the current context
+        void activate();
+        /// deinstalls the handler from the current context, restoring the previous context
+        void deactivate();
+    private:
+        class Context;
+        Context * m_pContext;
+        InteractionHandler m_xHandler;
+    private:
+        // not implemented - suppress copy
+        ConfigurationErrorHandler(const ConfigurationErrorHandler&);
+        void operator=(const ConfigurationErrorHandler&);
+    };
+
+//-----------------------------------------------------------------------------
 #endif
