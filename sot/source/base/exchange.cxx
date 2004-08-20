2bc19b5f93786e9950855f3299cb69b09b19cfe4
Rüdiger Timm <rt@openoffice.org>
2004-08-20 07:34:52 +0000

    INTEGRATION: CWS oasis2 (1.30.4); FILE MERGED 2004/08/02 15:35:12 mib 1.30.4.1: #i32453#: spec for MIME types changed

--- sot/source/base/exchange.cxx
+++ sot/source/base/exchange.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: exchange.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-13 09:17:00 $
+ *  last change: $Author: rt $ $Date: 2004-08-20 08:34:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -245,12 +245,12 @@ namespace
             /*123 SOT_FORMATSTR_ID_HTML_NO_COMMENT */       { "application/x-openoffice-html-no-comment;windows_formatname=\"HTML Format\"", "HTML (no comment)", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*124 SOT_FORMATSTR_ID_STARWRITER_8*/          { "application/x-vnd.oasis.openoffice.text", "Writer 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*125 SOT_FORMATSTR_ID_STARWRITERWEB_8*/       { "application/x-vnd.oasis.openoffice.text-web", "Writer/Web 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
-            /*126 SOT_FORMATSTR_ID_STARWRITERGLOB_8*/      { "application/x-vnd.oasis.openoffice.text-global", "Writer/Global 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
+            /*126 SOT_FORMATSTR_ID_STARWRITERGLOB_8*/      { "application/x-vnd.oasis.openoffice.text-master", "Writer/Global 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*127 SOT_FORMATSTR_ID_STARWDRAW_8*/           { "application/x-vnd.oasis.openoffice.drawing", "Draw 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*128 SOT_FORMATSTR_ID_STARIMPRESS_8*/         { "application/x-vnd.oasis.openoffice.presentation", "Impress 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*129 SOT_FORMATSTR_ID_STARCALC_8*/            { "application/x-vnd.oasis.openoffice.spreadsheet", "Calc 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
             /*130 SOT_FORMATSTR_ID_STARCHART_8*/           { "application/x-vnd.oasis.openoffice.chart", "Chart 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) },
-            /*131 SOT_FORMATSTR_ID_STARMATH_8*/            { "application/x-vnd.oasis.openoffice.math", "Math 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) }
+            /*131 SOT_FORMATSTR_ID_STARMATH_8*/            { "application/x-vnd.oasis.openoffice.formula", "Math 8", &::getCppuType( (const Sequence< sal_Int8 >*) 0 ) }
             };
         return &aInstance[0];
         }
