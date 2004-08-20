a079f2480fa93021afac3c3359427179252bac98
Rüdiger Timm <rt@openoffice.org>
2004-08-20 13:05:55 +0000

    INTEGRATION: CWS syssettings01 (1.2.70); FILE MERGED 2004/07/28 09:14:38 obr 1.2.70.1: #i21191#,#i8452#,#b4932088# made desktop locale settings available through configuration

--- officecfg/registry/schema/org/openoffice/makefile.mk
+++ officecfg/registry/schema/org/openoffice/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2004-07-05 13:35:03 $
+#   last change: $Author: rt $ $Date: 2004-08-20 14:05:55 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ PACKAGE=org.openoffice
 XCSFILES= \
     Inet.xcs \
     Setup.xcs \
+    System.xcs \
     UserProfile.xcs \
     VCL.xcs
 
