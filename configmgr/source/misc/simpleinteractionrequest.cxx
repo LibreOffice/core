d4751777b6489dddbd9ef9330ab180f12c1c4ec6
Rüdiger Timm <rt@openoffice.org>
2004-08-20 11:56:56 +0000

    INTEGRATION: CWS cfgruggedised (1.1.2); FILE ADDED 2004/07/23 13:29:45 jb 1.1.2.1: #i28654#,#i29838# Add InteractionHandler support to configuration

---
+++ configmgr/source/misc/simpleinteractionrequest.cxx
@@ -0,0 +1,128 @@
+/*************************************************************************
+ *
+ *  $RCSfile: simpleinteractionrequest.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-08-20 12:56:56 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2004 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "simpleinteractionrequest.hxx"
+
+namespace configmgr { namespace apihelper {
+
+namespace uno = com::sun::star::uno;
+namespace task = com::sun::star::task;
+//=========================================================================
+SimpleInteractionRequest::SimpleInteractionRequest(
+                                    const uno::Any & rRequest,
+                                    const Continuation nContinuations )
+: InteractionRequest( rRequest )
+{
+    // Set continuations.
+    OSL_ENSURE( nContinuations != CONTINUATION_UNKNOWN,
+                "SimpleInteractionRequest - No continuation!" );
+
+    sal_Int32 nLength = 0;
+
+    const sal_uInt32 k_NumContinuationTypes = 4;
+    uno::Reference< task::XInteractionContinuation > xContinuations[ k_NumContinuationTypes ];
+
+    if ( nContinuations & CONTINUATION_ABORT )
+        xContinuations[nLength++] = new InteractionAbort( this );
+
+    if ( nContinuations & CONTINUATION_RETRY )
+        xContinuations[nLength++] = new InteractionRetry( this );
+
+    if ( nContinuations & CONTINUATION_APPROVE )
+        xContinuations[nLength++] = new InteractionApprove( this );
+
+    if (  nContinuations & CONTINUATION_DISAPPROVE )
+        xContinuations[nLength++] = new InteractionDisapprove( this );
+
+    OSL_ENSURE( nLength > 0,
+                "SimpleInteractionRequest - No continuation!" );
+
+    uno::Sequence< uno::Reference< task::XInteractionContinuation > >
+        aContinuations( xContinuations, nLength );
+
+    this->setContinuations( aContinuations );
+}
+
+//=========================================================================
+const SimpleInteractionRequest::Continuation SimpleInteractionRequest::getResponse() const
+{
+    uno::Reference< task::XInteractionContinuation > xSelection = this->getSelection();
+    if ( xSelection.is() )
+    {
+        if ( uno::Reference< task::XInteractionApprove >::query(xSelection).is() )
+            return CONTINUATION_APPROVE;
+
+        if ( uno::Reference< task::XInteractionDisapprove >::query(xSelection).is() )
+            return CONTINUATION_DISAPPROVE;
+
+        if ( uno::Reference< task::XInteractionRetry >::query(xSelection).is() )
+            return CONTINUATION_RETRY;
+
+        if ( uno::Reference< task::XInteractionAbort >::query(xSelection).is() )
+            return CONTINUATION_ABORT;
+
+        OSL_ENSURE( sal_False,
+            "SimpleInteractionRequest::getResponse - Unknown continuation!" );
+    }
+    return CONTINUATION_UNKNOWN;
+}
+
+} }
