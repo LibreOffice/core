2a58c9145a35d948ccd8a2f6ee6149868e5276ca
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:08:16 +0000

    INTEGRATION: CWS tune03 (1.2.82); FILE MERGED 2004/07/19 19:11:40 mhu 1.2.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/misc/swmodalredlineacceptdlg.cxx
+++ sw/source/ui/misc/swmodalredlineacceptdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swmodalredlineacceptdlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:34:25 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:08:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
