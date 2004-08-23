5000aacd74db433ecf7a5b86a47bef4db73be20d
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:32:50 +0000

    INTEGRATION: CWS tune03 (1.2.516); FILE MERGED 2004/07/08 16:45:09 mhu 1.2.516.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/docshell/tpstat.cxx
+++ sc/source/ui/docshell/tpstat.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tpstat.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-30 13:48:11 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:32:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
