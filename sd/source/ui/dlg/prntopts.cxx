9f66a4eea92b1644a2ad4dc88040df66e93a5379
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:18:33 +0000

    INTEGRATION: CWS tune03 (1.9.110); FILE MERGED 2004/08/08 12:54:09 mhu 1.9.110.1: #i29979# Added SD_DLLPUBLIC/PRIVATE (see sddllapi.h) to exported symbols/classes.

--- sd/source/ui/dlg/prntopts.cxx
+++ sd/source/ui/dlg/prntopts.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: prntopts.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 15:44:42 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:18:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SD_DLLIMPLEMENTATION
+#undef SD_DLLIMPLEMENTATION
+#endif
 
 #pragma hdrstop
 
