3a12aa62f1499e650ec31a74b0b71dbbd342d7ea
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:46:54 +0000

    INTEGRATION: CWS tune03 (1.8.82); FILE MERGED 2004/08/09 21:46:12 mhu 1.8.82.2: RESYNC: (1.8-1.9); FILE MERGED 2004/07/19 19:11:01 mhu 1.8.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/chrdlg/pardlg.cxx
+++ sw/source/ui/chrdlg/pardlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pardlg.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-05 14:42:00 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:46:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 #pragma hdrstop
 
