835bb5917a773ba55a52244b1cb2b15ccefd96a1
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:37:21 +0000

    INTEGRATION: CWS tune03 (1.2.518); FILE MERGED 2004/07/08 16:45:13 mhu 1.2.518.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/miscdlgs/lbseldlg.cxx
+++ sc/source/ui/miscdlgs/lbseldlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: lbseldlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-17 15:17:52 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:37:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
