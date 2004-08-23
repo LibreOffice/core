7ce6c793f398152aa640f9d63c8579767c2d59b6
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:18:21 +0000

    INTEGRATION: CWS tune03 (1.11.110); FILE MERGED 2004/08/08 12:54:09 mhu 1.11.110.1: #i29979# Added SD_DLLPUBLIC/PRIVATE (see sddllapi.h) to exported symbols/classes.

--- sd/source/ui/dlg/prltempl.cxx
+++ sd/source/ui/dlg/prltempl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: prltempl.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-12 15:12:14 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:18:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SD_DLLIMPLEMENTATION
+#undef SD_DLLIMPLEMENTATION
+#endif
 
 #pragma hdrstop
 
