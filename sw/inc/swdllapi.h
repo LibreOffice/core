158987bf535129ec1ccb32f9cb40b88bc7df7183
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:39:17 +0000

    INTEGRATION: CWS tune03 (1.1.2); FILE ADDED 2004/07/19 19:10:42 mhu 1.1.2.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

---
+++ sw/inc/swdllapi.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_SWDLLAPI_H
+#define INCLUDED_SWDLLAPI_H
+
+#ifndef _SAL_TYPES_H_
+#include "sal/types.h"
+#endif
+
+#if defined(SW_DLLIMPLEMENTATION)
+#define SW_DLLPUBLIC  SAL_DLLPUBLIC_EXPORT
+#else
+#define SW_DLLPUBLIC  SAL_DLLPUBLIC_IMPORT
+#endif
+#define SW_DLLPRIVATE SAL_DLLPRIVATE
+
+#endif /* INCLUDED_SWDLLAPI_H */
