f26fd1355069685482f5be7c5637e64844401373
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:07:36 +0000

    INTEGRATION: CWS tune03 (1.5.580); FILE MERGED 2004/07/19 19:11:39 mhu 1.5.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/misc/pgfnote.cxx
+++ sw/source/ui/misc/pgfnote.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pgfnote.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:37:43 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:07:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
