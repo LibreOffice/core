abb9490cbe13a70d1fb79a5d00edb942df8e07a6
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:09:35 +0000

    INTEGRATION: CWS tune03 (1.4.580); FILE MERGED 2004/07/19 19:11:44 mhu 1.4.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/table/mergetbl.cxx
+++ sw/source/ui/table/mergetbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: mergetbl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:46:50 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:09:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
