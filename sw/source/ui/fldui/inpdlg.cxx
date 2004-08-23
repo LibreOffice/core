ee3df6c13c19e860dd1d6ecdd362f27c2200d432
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:54:22 +0000

    INTEGRATION: CWS tune03 (1.5.580); FILE MERGED 2004/07/19 19:11:17 mhu 1.5.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/fldui/inpdlg.cxx
+++ sw/source/ui/fldui/inpdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: inpdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:28:37 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:54:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
