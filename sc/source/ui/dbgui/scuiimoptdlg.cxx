b87f8537482d7dc56c0f2667b68d87ebff61ad79
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:31:26 +0000

    INTEGRATION: CWS tune03 (1.2.72); FILE MERGED 2004/07/08 16:45:07 mhu 1.2.72.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/dbgui/scuiimoptdlg.cxx
+++ sc/source/ui/dbgui/scuiimoptdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: scuiimoptdlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 15:57:44 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:31:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
