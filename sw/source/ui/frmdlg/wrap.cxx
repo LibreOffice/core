02120f934ba8592a75303f11fe0dc9139c8785fd
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:56:19 +0000

    INTEGRATION: CWS tune03 (1.10.6); FILE MERGED 2004/08/09 21:49:24 mhu 1.10.6.2: RESYNC: (1.10-1.11); FILE MERGED 2004/07/19 19:11:23 mhu 1.10.6.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/frmdlg/wrap.cxx
+++ sw/source/ui/frmdlg/wrap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrap.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-05 15:46:14 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:56:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
