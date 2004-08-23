84b644e1a5023758b6430a033df19f3d5f729504
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:51:36 +0000

    INTEGRATION: CWS tune03 (1.6.82); FILE MERGED 2004/07/19 19:11:12 mhu 1.6.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/envelp/labprt.cxx
+++ sw/source/ui/envelp/labprt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: labprt.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:24:37 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:51:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
