e78c0adb67a133b72c6d0cfd111a13f8c917c397
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:34:17 +0000

    INTEGRATION: CWS tune03 (1.6.222); FILE MERGED 2004/07/19 19:10:35 mhu 1.6.222.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/inc/fmtline.hxx
+++ sw/inc/fmtline.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtline.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-02 17:57:48 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:34:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -71,9 +71,13 @@
 #ifndef _FORMAT_HXX //autogen
 #include <format.hxx>
 #endif
+#ifndef INCLUDED_SWDLLAPI_H
+#include "swdllapi.h"
+#endif
+
 class IntlWrapper;
 
-class SwFmtLineNumber: public SfxPoolItem
+class SW_DLLPUBLIC SwFmtLineNumber: public SfxPoolItem
 {
     ULONG nStartValue   :24; //Startwert fuer den Absatz, 0 == kein Startwert
     ULONG bCountLines   :1;  //Zeilen des Absatzes sollen mitgezaehlt werden.
