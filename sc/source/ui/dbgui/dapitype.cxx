c382988dcb0786b04569bbc52c86cc9c03fbed0f
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:29:56 +0000

    INTEGRATION: CWS tune03 (1.2.516); FILE MERGED 2004/07/08 16:45:06 mhu 1.2.516.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/dbgui/dapitype.cxx
+++ sc/source/ui/dbgui/dapitype.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dapitype.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-25 15:27:22 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:29:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
