18c7d332e1b3ccada3a0bc658667111c29ac1452
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:36:11 +0000

    INTEGRATION: CWS tune03 (1.2.516); FILE MERGED 2004/07/08 16:45:12 mhu 1.2.516.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/miscdlgs/crdlg.cxx
+++ sc/source/ui/miscdlgs/crdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: crdlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-30 13:32:40 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:35:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
--- sc/source/ui/miscdlgs/delcldlg.cxx
+++ sc/source/ui/miscdlgs/delcldlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: delcldlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: dr $ $Date: 2001-05-23 10:50:15 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:35:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
