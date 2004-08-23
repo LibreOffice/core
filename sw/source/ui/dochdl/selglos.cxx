5ece58b93e57c1a70b0c7929deee6869262544b9
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:50:18 +0000

    INTEGRATION: CWS tune03 (1.4.580); FILE MERGED 2004/07/19 19:11:09 mhu 1.4.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/dochdl/selglos.cxx
+++ sw/source/ui/dochdl/selglos.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: selglos.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:22:26 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:50:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
