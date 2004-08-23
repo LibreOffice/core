d735dd57efdb40dfd0e0e9f6673a3cfe53ec9cb0
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:47:15 +0000

    INTEGRATION: CWS tune03 (1.5.82); FILE MERGED 2004/07/19 19:11:01 mhu 1.5.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/chrdlg/tblnumfm.cxx
+++ sw/source/ui/chrdlg/tblnumfm.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tblnumfm.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:19:37 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:47:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
