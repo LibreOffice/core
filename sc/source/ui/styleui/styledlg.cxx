d074ff02b144d9ded7e054f5f321f9ee7f972fb8
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:41:04 +0000

    INTEGRATION: CWS tune03 (1.6.70); FILE MERGED 2004/08/09 21:03:21 mhu 1.6.70.2: RESYNC: (1.6-1.7); FILE MERGED 2004/07/08 16:45:18 mhu 1.6.70.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/styleui/styledlg.cxx
+++ sc/source/ui/styleui/styledlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: styledlg.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 17:04:27 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:41:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
