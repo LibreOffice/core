593b1f986a055845a382f0600301d8f04c18d8c9
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:10:13 +0000

    INTEGRATION: CWS tune03 (1.4.580); FILE MERGED 2004/07/19 19:11:45 mhu 1.4.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/table/splittbl.cxx
+++ sw/source/ui/table/splittbl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: splittbl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:47:22 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:10:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
