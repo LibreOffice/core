6e655c5c3f1f8762dbb836660aba0ebcdf56e9b1
Rüdiger Timm <rt@openoffice.org>
2004-08-23 09:49:34 +0000

    INTEGRATION: CWS tune03 (1.23.82); FILE MERGED 2004/07/19 19:11:39 mhu 1.23.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/misc/outline.cxx
+++ sw/source/ui/misc/outline.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: outline.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 13:10:43 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 10:49:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
