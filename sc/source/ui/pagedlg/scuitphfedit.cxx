b9953ccdd161cf8084b3ed6f79bbd8d6665b323a
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:40:25 +0000

    INTEGRATION: CWS tune03 (1.2.72); FILE MERGED 2004/08/09 21:02:52 mhu 1.2.72.2: RESYNC: (1.2-1.3); FILE MERGED 2004/07/08 16:45:17 mhu 1.2.72.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/pagedlg/scuitphfedit.cxx
+++ sc/source/ui/pagedlg/scuitphfedit.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: scuitphfedit.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-23 14:26:45 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:40:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
