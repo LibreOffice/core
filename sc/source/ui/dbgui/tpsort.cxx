9ddfc622ace65743dc52a47022db32d96e3f81a9
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:32:01 +0000

    INTEGRATION: CWS tune03 (1.7.52); FILE MERGED 2004/07/08 16:45:07 mhu 1.7.52.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/dbgui/tpsort.cxx
+++ sc/source/ui/dbgui/tpsort.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tpsort.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2004-06-04 11:21:49 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:32:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
