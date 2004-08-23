629e88da7a8eb540049dc43af120bf465c0aa41e
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:19:19 +0000

    INTEGRATION: CWS tune03 (1.10.110); FILE MERGED 2004/08/08 12:54:11 mhu 1.10.110.1: #i29979# Added SD_DLLPUBLIC/PRIVATE (see sddllapi.h) to exported symbols/classes.

--- sd/source/ui/dlg/tabtempl.cxx
+++ sd/source/ui/dlg/tabtempl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabtempl.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-12 15:13:50 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:19:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SD_DLLIMPLEMENTATION
+#undef SD_DLLIMPLEMENTATION
+#endif
 
 #include <svx/flstitem.hxx>
 
