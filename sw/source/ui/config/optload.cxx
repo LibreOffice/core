eb21d417d02bbd2d9ff718e3991dbc93e636211a
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:47:38 +0000

    INTEGRATION: CWS tune03 (1.17.298); FILE MERGED 2004/07/19 19:11:02 mhu 1.17.298.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/config/optload.cxx
+++ sw/source/ui/config/optload.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optload.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2003-10-15 10:00:54 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:47:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
