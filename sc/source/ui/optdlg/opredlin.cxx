df80b4f08bd8c041ee867fcdf843b3cd753697fb
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:39:15 +0000

    INTEGRATION: CWS tune03 (1.3.156); FILE MERGED 2004/07/08 16:45:16 mhu 1.3.156.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/optdlg/opredlin.cxx
+++ sc/source/ui/optdlg/opredlin.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: opredlin.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 20:34:54 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:39:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
