c432780aca6923d032156c44f225981e2488af04
Rüdiger Timm <rt@openoffice.org>
2004-08-23 09:52:13 +0000

    INTEGRATION: CWS tune03 (1.42.6); FILE MERGED 2004/08/09 21:48:58 mhu 1.42.6.2: RESYNC: (1.42-1.44); FILE MERGED 2004/07/19 19:11:21 mhu 1.42.6.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/frmdlg/frmpage.cxx
+++ sw/source/ui/frmdlg/frmpage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: frmpage.cxx,v $
  *
- *  $Revision: 1.45 $
+ *  $Revision: 1.46 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 13:03:12 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 10:52:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 #ifndef _CMDID_H
 #include <cmdid.h>
