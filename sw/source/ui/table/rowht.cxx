cc4e588239735dbeb61ac79fd8f674eb644d4030
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:09:48 +0000

    INTEGRATION: CWS tune03 (1.7.70); FILE MERGED 2004/07/19 19:11:44 mhu 1.7.70.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/table/rowht.cxx
+++ sw/source/ui/table/rowht.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rowht.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2004-05-18 15:01:41 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:09:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
