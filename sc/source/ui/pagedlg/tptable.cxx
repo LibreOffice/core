3b32f0fcf1591ec30660d71b84b09a1ac79da01a
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:40:51 +0000

    INTEGRATION: CWS tune03 (1.6.126); FILE MERGED 2004/07/08 16:45:18 mhu 1.6.126.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/pagedlg/tptable.cxx
+++ sc/source/ui/pagedlg/tptable.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tptable.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2004-03-19 16:14:34 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:40:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
