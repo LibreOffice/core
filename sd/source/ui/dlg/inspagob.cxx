326140833ca989effb734dc7b99642ca56dd1b84
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:16:45 +0000

    INTEGRATION: CWS tune03 (1.5.186); FILE MERGED 2004/08/08 12:54:06 mhu 1.5.186.1: #i29979# Added SD_DLLPUBLIC/PRIVATE (see sddllapi.h) to exported symbols/classes.

--- sd/source/ui/dlg/inspagob.cxx
+++ sd/source/ui/dlg/inspagob.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: inspagob.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2004-01-20 10:45:47 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:16:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SD_DLLIMPLEMENTATION
+#undef SD_DLLIMPLEMENTATION
+#endif
 
 #pragma hdrstop
 
