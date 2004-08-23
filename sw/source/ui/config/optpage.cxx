3908d6ce3f0e71cceeca5b9cd1d1126875a0f2dc
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:47:49 +0000

    INTEGRATION: CWS tune03 (1.40.82); FILE MERGED 2004/07/19 19:11:02 mhu 1.40.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/config/optpage.cxx
+++ sw/source/ui/config/optpage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optpage.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 12:58:55 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:47:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 #pragma hdrstop
 
