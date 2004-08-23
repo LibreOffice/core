cc6b33b71c505479fa67b75825a13daa564ad309
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:39:27 +0000

    INTEGRATION: CWS tune03 (1.11.72); FILE MERGED 2004/08/11 13:07:53 mhu 1.11.72.3: #i29979# Cleaned up resync merge leftovers. 2004/08/09 21:02:29 mhu 1.11.72.2: RESYNC: (1.11-1.12); FILE MERGED 2004/07/08 16:45:16 mhu 1.11.72.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/optdlg/tpcalc.cxx
+++ sc/source/ui/optdlg/tpcalc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tpcalc.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-03 11:37:52 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:39:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
