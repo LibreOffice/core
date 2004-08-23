13ef3239c26b19bb8199b75a5e292f9989e9b515
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:54:32 +0000

    INTEGRATION: CWS tune03 (1.7.580); FILE MERGED 2004/07/19 19:11:17 mhu 1.7.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/fldui/javaedit.cxx
+++ sw/source/ui/fldui/javaedit.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: javaedit.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 13:02:27 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:54:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
