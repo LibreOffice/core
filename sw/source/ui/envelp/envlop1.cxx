f4e33f3d6ba89251bc48593016e8258a3aa511ab
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:50:39 +0000

    INTEGRATION: CWS tune03 (1.14.580); FILE MERGED 2004/07/19 19:11:10 mhu 1.14.580.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/envelp/envlop1.cxx
+++ sw/source/ui/envelp/envlop1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: envlop1.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:24:38 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:50:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
