9a14e52ddb60c321e7c7c1407811835a81aa6e9f
Rüdiger Timm <rt@openoffice.org>
2004-08-23 07:46:28 +0000

    INTEGRATION: CWS tune03 (1.13.82); FILE MERGED 2004/07/19 19:11:01 mhu 1.13.82.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/chrdlg/drpcps.cxx
+++ sw/source/ui/chrdlg/drpcps.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drpcps.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:18:53 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 08:46:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
