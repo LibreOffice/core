9ba2c091870a4fffecc98d01746eccedbef91847
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:06:14 +0000

    INTEGRATION: CWS tune03 (1.14.268); FILE MERGED 2004/07/19 19:11:36 mhu 1.14.268.1: #i29979# Added SW_DLLPUBLIC/PRIVATE (see swdllapi.h) to exported symbols/classes.

--- sw/source/ui/misc/glosbib.cxx
+++ sw/source/ui/misc/glosbib.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: glosbib.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 13:10:28 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:06:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SW_DLLIMPLEMENTATION
+#undef SW_DLLIMPLEMENTATION
+#endif
 
 
 #pragma hdrstop
