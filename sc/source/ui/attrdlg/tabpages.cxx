16c1b8ea1b9ba3a5ab45cb00efb7a02fa1f293f4
Rüdiger Timm <rt@openoffice.org>
2004-08-23 08:28:38 +0000

    INTEGRATION: CWS tune03 (1.3.508); FILE MERGED 2004/07/08 16:45:04 mhu 1.3.508.1: #i29979# Added SC_DLLPUBLIC/PRIVATE (see scdllapi.h) to exported symbols/classes.

--- sc/source/ui/attrdlg/tabpages.cxx
+++ sc/source/ui/attrdlg/tabpages.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabpages.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: mh $ $Date: 2001-10-23 11:02:58 $
+ *  last change: $Author: rt $ $Date: 2004-08-23 09:28:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,8 @@
  *
  *
  ************************************************************************/
+
+#undef SC_DLLIMPLEMENTATION
 
 #ifdef PCH
 #include "ui_pch.hxx"
