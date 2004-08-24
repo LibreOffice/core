d5e3f78b49ac244a99b58d2a765d2b518448e06e
Michael Meeks <mmeeks@openoffice.org>
2004-08-24 09:44:16 +0000

    final cleans. Issue number: 32648

--- libwpd/makefile.mk
+++ libwpd/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: mmeeks $ $Date: 2004-08-24 08:28:04 $
+#   last change: $Author: mmeeks $ $Date: 2004-08-24 10:44:15 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
