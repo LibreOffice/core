0436b0349e7bcc8392a07dcbf928940df9387c48
Rüdiger Timm <rt@openoffice.org>
2004-09-08 15:02:08 +0000

    INTEGRATION: CWS ooo20040704 (1.3.38); FILE MERGED 2004/07/02 09:59:46 cmc 1.3.38.2: #i30891# revert header and namespace change 2004/06/28 14:05:21 cmc 1.3.38.1: #i30801# allow using system stl if possible

--- stoc/source/corereflection/lrucache.hxx
+++ stoc/source/corereflection/lrucache.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: lrucache.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-04 14:31:00 $
+ *  last change: $Author: rt $ $Date: 2004-09-08 16:02:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,7 +72,6 @@
 #endif
 
 #include <hash_map>
-
 
 /** Implementation of a least recently used (lru) cache.
     <br>
