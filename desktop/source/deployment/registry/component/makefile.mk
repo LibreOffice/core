7bfe504bef8cc9534f75b05fc901504b3a0234c4
Rüdiger Timm <rt@openoffice.org>
2004-09-08 15:49:26 +0000

    INTEGRATION: CWS ooo20040704 (1.3.10); FILE MERGED 2004/07/02 11:19:50 cmc 1.3.10.2: #i30891# revert header and namespace change 2004/06/28 13:13:43 cmc 1.3.10.1: #i30801# allow using system stl if possible

--- desktop/source/deployment/registry/component/makefile.mk
+++ desktop/source/deployment/registry/component/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2004-06-11 12:14:15 $
+#   last change: $Author: rt $ $Date: 2004-09-08 16:49:26 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -67,6 +67,10 @@ TARGET = deployment_registry_component
 ENABLE_EXCEPTIONS = TRUE
 
 .INCLUDE : settings.mk
+
+.IF "$(SYSTEM_DB3)" == "YES"
+CFLAGS+=-DSYSTEM_DB3
+.ENDIF
 
 SRS1NAME = $(TARGET)
 SRC1FILES = \
