2aeeb05425a6769601ab977c80fccb7d8685f952
Rüdiger Timm <rt@openoffice.org>
2004-09-08 15:15:48 +0000

    INTEGRATION: CWS ooo20040704 (1.2.12); FILE MERGED 2004/06/28 14:46:09 cmc 1.2.12.1: #i30801# allow using system libs if possible

--- unoxml/source/events/makefile.mk
+++ unoxml/source/events/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: lo $ $Date: 2004-03-01 13:24:19 $
+#   last change: $Author: rt $ $Date: 2004-09-08 16:15:48 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,6 +70,10 @@ ENABLE_EXCEPTIONS=TRUE
 
 .INCLUDE :  settings.mk
 RSCUPDVER=$(RSCREVISION)(SV$(UPD)$(UPDMINOR))
+
+.IF "$(SYSTEM_LIBXML)" == "YES"
+CFLAGS+=-DSYSTEM_LIBXML $(LIBXML_CFLAGS)
+.ENDIF
 
 # --- Files --------------------------------------------------------
 
