bd1de74fa9a1d742f370f484198e8b3eb8d879d9
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 14:49:21 +0000

    INTEGRATION: CWS dr20 (1.2.194); FILE MERGED 2004/08/11 10:43:54 dr 1.2.194.2: #i12577# #i16277# #i24129# #i31482# #i24672# #i27407# #i30411# rework of cell table export - default row/column formats and shared formulas 2004/06/15 16:40:31 dr 1.2.194.1: #i30205# Excel filter code cleanup - rowlimit changes: SCCOL/SCROW/SCTAB usage - type correctness: USHORT vs. sal_uInt16, ULONG/size_t vs. sal_uInt32 - removed compiler warnings

--- sc/source/filter/inc/xlformula.hxx
+++ sc/source/filter/inc/xlformula.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xlformula.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hjs $ $Date: 2003-08-19 11:38:18 $
+ *  last change: $Author: hr $ $Date: 2004-09-08 15:49:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,20 +59,68 @@
  *
  ************************************************************************/
 
-// ============================================================================
-
 #ifndef SC_XLFORMULA_HXX
 #define SC_XLFORMULA_HXX
 
-#ifndef SC_COMPILER_HXX
-#include "compiler.hxx"
+#ifndef SC_ADDRESS_HXX
+#include "address.hxx"
 #endif
 #ifndef SC_FTOOLS_HXX
 #include "ftools.hxx"
 #endif
 
+// Constants ==================================================================
+
+const sal_uInt8 EXC_TOKID_EXP               = 0x01;
+const sal_uInt8 EXC_TOKID_TBL               = 0x02;
+const sal_uInt8 EXC_TOKID_ADD               = 0x03;
+const sal_uInt8 EXC_TOKID_SUB               = 0x04;
+const sal_uInt8 EXC_TOKID_MUL               = 0x05;
+const sal_uInt8 EXC_TOKID_DIV               = 0x06;
+const sal_uInt8 EXC_TOKID_POWER             = 0x07;
+const sal_uInt8 EXC_TOKID_CONCAT            = 0x08;
+const sal_uInt8 EXC_TOKID_LT                = 0x09;
+const sal_uInt8 EXC_TOKID_LE                = 0x0A;
+const sal_uInt8 EXC_TOKID_EQ                = 0x0B;
+const sal_uInt8 EXC_TOKID_GE                = 0x0C;
+const sal_uInt8 EXC_TOKID_GT                = 0x0D;
+const sal_uInt8 EXC_TOKID_NE                = 0x0E;
+const sal_uInt8 EXC_TOKID_ISECT             = 0x0F;
+const sal_uInt8 EXC_TOKID_LIST              = 0x10;
+const sal_uInt8 EXC_TOKID_RANGE             = 0x11;
+const sal_uInt8 EXC_TOKID_UPLUS             = 0x12;
+const sal_uInt8 EXC_TOKID_UMINUS            = 0x13;
+const sal_uInt8 EXC_TOKID_PERCENT           = 0x14;
+const sal_uInt8 EXC_TOKID_PAREN             = 0x15;
+const sal_uInt8 EXC_TOKID_MISSARG           = 0x16;
+const sal_uInt8 EXC_TOKID_STR               = 0x17;
+const sal_uInt8 EXC_TOKID_EXTENDED          = 0x18;
+const sal_uInt8 EXC_TOKID_ATTR              = 0x19;
+const sal_uInt8 EXC_TOKID_SHEET             = 0x1A;
+const sal_uInt8 EXC_TOKID_ENDSHEET          = 0x1B;
+const sal_uInt8 EXC_TOKID_ERR               = 0x1C;
+const sal_uInt8 EXC_TOKID_BOOL              = 0x1D;
+const sal_uInt8 EXC_TOKID_INT               = 0x1E;
+const sal_uInt8 EXC_TOKID_NUM               = 0x1F;
 
 // Token array ================================================================
+
+/** Contains all cell references that can be extracted from a multiple operations formula. */
+struct XclMultipleOpRefs
+{
+    ScAddress           maFmlaScPos;        /// Position of the (first) formula cell.
+    ScAddress           maColFirstScPos;
+    ScAddress           maColRelScPos;
+    ScAddress           maRowFirstScPos;
+    ScAddress           maRowRelScPos;
+    bool                mbDblRefMode;       /// true = One formula with row and column values.
+};
+
+// ----------------------------------------------------------------------------
+
+class ScTokenArray;
+class ScRangeData;
+class XclRoot;
 
 /** A helper that extends an ScTokenArray with additional Excel specific functions.
     @descr  The purpose to not add these functions to ScTokenArray is to prevent code
@@ -87,7 +135,7 @@ public:
     /** Parses the passed formula and tries to find a single string token, i.e. "abc".
         @param rString  (out-parameter) The string contained in the formula.
         @return  true = String token found, rString parameter is valid. */
-    static bool                 GetString( String& rString, const ScTokenArray& rTokenArray );
+    static bool         GetString( String& rString, const ScTokenArray& rScTokArr );
 
     /** Parses the passed formula and tries to find a string token list, i.e. "abc";"def";"ghi".
         @descr  Returns the unquoted (!) strings in a single string, separated with the
@@ -96,31 +144,31 @@ public:
         @param rStringList  (out-parameter) All strings contained in the formula as list.
         @param cSep  List separator character.
         @return  true = String token list found, rString parameter is valid. */
-    static bool                 GetStringList( String& rStringList, const ScTokenArray& rTokenArray, sal_Unicode cSep );
+    static bool         GetStringList( String& rStringList, const ScTokenArray& rScTokArr, sal_Unicode cSep );
 
     /** Tries to convert a formula that consists of a single string token to a list of strings.
         @descr  Example: The formula ="abc\ndef\nghi" will be converted to the formula
         ="abc";"def";"ghi", if the LF character is specified as separator.
-        @param rTokenArray  (in/out-parameter) The token array to modify.
+        @param rScTokArr  (in/out-parameter) The token array to modify.
         @param cStringSep  The separator in the source string. */
-    static void                 ConvertStringToList( ScTokenArray& rTokenArray, sal_Unicode cStringSep );
+    static void         ConvertStringToList( ScTokenArray& rScTokArr, sal_Unicode cStringSep );
+
+    // shared formulas --------------------------------------------------------
+
+    /** Tries to extract the definition of a shared formula from the passed token array.
+        @descr  Shared formulas are stored as hidden defined names in Calc. This
+        function looks if the passed token array consists of the reference to
+        such a hidden defined name and returns its definition on success. */
+    static const ScRangeData* GetSharedFormula( const XclRoot& rRoot, const ScTokenArray& rScTokArr );
 
     // multiple operations ----------------------------------------------------
 
     /** Parses the passed formula and tries to extract references of a multiple operation.
         @descr  Requires that the formula contains a single MULTIPLE.OPERATION function call.
         Spaces in the formula are silently ignored.
-        @param rbIsDoubleRefMode  (out-parameter) true, if the multiple operation contains one
-        formula, and row and column values.
         @return  true = Multiple operation found, and all references successfully extracted. */
-    static bool                 GetMultipleOpRefs(
-                                    ScAddress& rFormula,
-                                    ScAddress& rColFirstPos, ScAddress& rColRelPos,
-                                    ScAddress& rRowFirstPos, ScAddress& rRowRelPos,
-                                    bool& rbIsDoubleRefMode,
-                                    const ScTokenArray& rTokenArray );
+    static bool         GetMultipleOpRefs( XclMultipleOpRefs& rRefs, const ScTokenArray& rScTokArr );
 };
-
 
 // ============================================================================
 
