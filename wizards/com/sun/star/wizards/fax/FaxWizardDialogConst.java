067be25f167a8cbfcb239c1ef3df4e71c11125cd
Oliver Bolte <obo@openoffice.org>
2004-09-08 13:03:26 +0000

    INTEGRATION: CWS qwizards2 (1.1.2); FILE ADDED 2004/07/21 15:54:06 tv 1.1.2.2: several small bugfixes, incl. exchanging some listboxes with combo boxes 2004/06/04 10:34:41 tv 1.1.2.1: initial revision of the new fax wizard

---
+++ wizards/com/sun/star/wizards/fax/FaxWizardDialogConst.java
@@ -0,0 +1,88 @@
+package com.sun.star.wizards.fax;
+public interface FaxWizardDialogConst {
+
+  public static final String OPTBUSINESSFAX_ITEM_CHANGED = "optBusinessFaxItemChanged";
+  public static final String LSTBUSINESSSTYLE_ACTION_PERFORMED = null; // "lstBusinessStyleActionPerformed";
+  public static final String LSTBUSINESSSTYLE_ITEM_CHANGED = "lstBusinessStyleItemChanged";
+  public static final String OPTPRIVATEFAX_ITEM_CHANGED = "optPrivateFaxItemChanged";
+  public static final String LSTPRIVATESTYLE_ACTION_PERFORMED = null; // "lstPrivateStyleActionPerformed";
+  public static final String LSTPRIVATESTYLE_ITEM_CHANGED = "lstPrivateStyleItemChanged";
+  public static final String CHKUSELOGO_ITEM_CHANGED = "chkUseLogoItemChanged";
+  public static final String CHKUSEDATE_ITEM_CHANGED = "chkUseDateItemChanged";
+  public static final String CHKUSECOMMUNICATIONTYPE_ITEM_CHANGED = "chkUseCommunicationItemChanged";
+  public static final String LSTCOMMUNICATIONTYPE_ACTION_PERFORMED = null; // "lstCommunicationActionPerformed";
+  public static final String LSTCOMMUNICATIONTYPE_ITEM_CHANGED = "lstCommunicationItemChanged";
+  public static final String LSTCOMMUNICATIONTYPE_TEXT_CHANGED = "lstCommunicationTextChanged";
+  public static final String CHKUSESUBJECT_ITEM_CHANGED = "chkUseSubjectItemChanged";
+  public static final String CHKUSESALUTATION_ITEM_CHANGED = "chkUseSalutationItemChanged";
+  public static final String LSTSALUTATION_ACTION_PERFORMED = null; // "lstSalutationActionPerformed";
+  public static final String LSTSALUTATION_ITEM_CHANGED = "lstSalutationItemChanged";
+  public static final String LSTSALUTATION_TEXT_CHANGED = "lstSalutationTextChanged";
+  public static final String CHKUSEGREETING_ITEM_CHANGED = "chkUseGreetingItemChanged";
+  public static final String LSTGREETING_ACTION_PERFORMED = null; // "lstGreetingActionPerformed";
+  public static final String LSTGREETING_ITEM_CHANGED = "lstGreetingItemChanged";
+  public static final String LSTGREETING_TEXT_CHANGED = "lstGreetingTextChanged";
+  public static final String CHKUSEFOOTER_ITEM_CHANGED = "chkUseFooterItemChanged";
+  public static final String OPTSENDERPLACEHOLDER_ITEM_CHANGED = "optSenderPlaceholderItemChanged";
+  public static final String OPTSENDERDEFINE_ITEM_CHANGED = "optSenderDefineItemChanged";
+  public static final String TXTSENDERNAME_TEXT_CHANGED = "txtSenderNameTextChanged";
+  public static final String TXTSENDERSTREET_TEXT_CHANGED = "txtSenderStreetTextChanged";
+  public static final String TXTSENDERPOSTCODE_TEXT_CHANGED = "txtSenderPostCodeTextChanged";
+  public static final String TXTSENDERSTATE_TEXT_CHANGED = "txtSenderStateTextChanged";
+  public static final String TXTSENDERCITY_TEXT_CHANGED = "txtSenderCityTextChanged";
+  public static final String TXTSENDERFAX_TEXT_CHANGED = "txtSenderFaxTextChanged";
+  public static final String OPTRECEIVERPLACEHOLDER_ITEM_CHANGED = "optReceiverPlaceholderItemChanged";
+  public static final String OPTRECEIVERDATABASE_ITEM_CHANGED = "optReceiverDatabaseItemChanged";
+  public static final String TXTFOOTER_TEXT_CHANGED = "txtFooterTextChanged";
+  public static final String CHKFOOTERNEXTPAGES_ITEM_CHANGED = "chkFooterNextPagesItemChanged";
+  public static final String CHKFOOTERPAGENUMBERS_ITEM_CHANGED = "chkFooterPageNumbersItemChanged";
+  public static final String TXTTEMPLATENAME_TEXT_CHANGED = "txtTemplateNameTextChanged";
+  public static final String FILETEMPLATEPATH_TEXT_CHANGED = null; // "fileTemplatePathTextChanged";
+  public static final String OPTCREATEFAX_ITEM_CHANGED = "optCreateFaxItemChanged";
+  public static final String OPTMAKECHANGES_ITEM_CHANGED = "optMakeChangesItemChanged";
+  public static final String imageURLImageControl2 = null; //"images/ImageControl2";
+  public static final String imageURLImageControl3 = null; //"images/ImageControl3";
+
+  /* ====================================
+   *                 Help IDs
+   * ====================================
+   */
+  public final static int HID = 0; //TODO enter first hid here
+  public static final int HIDMAIN = 40820;
+
+  public final String OPTBUSINESSFAX_HID = "HID:" + (HID + 1);
+  public final String LSTBUSINESSSTYLE_HID = "HID:" + (HID + 2);
+  public final String OPTPRIVATEFAX_HID = "HID:" + (HID + 3);
+  public final String LSTPRIVATESTYLE_HID = "HID:" + (HID + 4);
+  public final String IMAGECONTROL3_HID = "HID:" + (HID + 9);
+  public final String CHKUSELOGO_HID = "HID:" + (HID + 10);
+  public final String CHKUSEDATE_HID = "HID:" + (HID + 11);
+  public final String CHKUSECOMMUNICATIONTYPE_HID = "HID:" + (HID + 12);
+  public final String LSTCOMMUNICATIONTYPE_HID = "HID:" + (HID + 13);
+  public final String CHKUSESUBJECT_HID = "HID:" + (HID + 14);
+  public final String CHKUSESALUTATION_HID = "HID:" + (HID + 15);
+  public final String LSTSALUTATION_HID = "HID:" + (HID + 16);
+  public final String CHKUSEGREETING_HID = "HID:" + (HID + 17);
+  public final String LSTGREETING_HID = "HID:" + (HID + 18);
+  public final String CHKUSEFOOTER_HID = "HID:" + (HID + 19);
+  public final String OPTSENDERPLACEHOLDER_HID = "HID:" + (HID + 21);
+  public final String OPTSENDERDEFINE_HID = "HID:" + (HID + 22);
+  public final String TXTSENDERNAME_HID = "HID:" + (HID + 23);
+  public final String TXTSENDERSTREET_HID = "HID:" + (HID + 24);
+  public final String TXTSENDERPOSTCODE_HID = "HID:" + (HID + 25);
+  public final String TXTSENDERSTATE_HID = "HID:" + (HID + 26);
+  public final String TXTSENDERCITY_HID = "HID:" + (HID + 27);
+  public final String TXTSENDERFAX_HID = "HID:" + (HID + 28);
+  public final String OPTRECEIVERPLACEHOLDER_HID = "HID:" + (HID + 29);
+  public final String OPTRECEIVERDATABASE_HID = "HID:" + (HID + 30);
+  public final String TXTFOOTER_HID = "HID:" + (HID + 39);
+  public final String CHKFOOTERNEXTPAGES_HID = "HID:" + (HID + 40);
+  public final String CHKFOOTERPAGENUMBERS_HID = "HID:" + (HID + 41);
+  public final String TXTTEMPLATENAME_HID = "HID:" + (HID + 44);
+  public final String FILETEMPLATEPATH_HID = "HID:" + (HID + 45);
+  public final String OPTCREATEFAX_HID = "HID:" + (HID + 46);
+  public final String OPTMAKECHANGES_HID = "HID:" + (HID + 47);
+  public final String IMAGECONTROL2_HID = "HID:" + (HID + 51);
+
+}
+  
