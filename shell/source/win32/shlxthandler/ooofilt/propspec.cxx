25a557588308072abea1955590487a984c1bb5df
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 13:33:18 +0000

    INTEGRATION: CWS desktintgr02 (1.1.2); FILE ADDED 2004/08/02 12:43:23 tra 1.1.2.3: #i21110# 2004/07/30 09:56:57 deuce 1.1.2.2: Issue number:  i21110 Submitted by:  Gorden Lin {gorden.lin@sun.com} Reviewed by:   Tino Rachui {tino.rachui@sun.com} 2004/07/29 09:28:30 deuce 1.1.2.1: Issue number:  21110 Submitted by:  Gorden Lin {gorden.lin@sun.com} Reviewed by:   Tino Rachui {tino.rachui@sun.com}

---
+++ shell/source/win32/shlxthandler/ooofilt/propspec.cxx
@@ -0,0 +1,256 @@
+/*************************************************************************
+ *
+ *  $RCSfile: propspec.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-09-08 14:33:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+//+-------------------------------------------------------------------------
+//
+//  File:       propspec.cxx
+//
+//  Contents:   C++ wrappers for FULLPROPSPEC
+//
+//--------------------------------------------------------------------------
+#include <windows.h>
+#pragma warning(disable: 4512)
+#include <filter.h>
+#pragma hdrstop
+
+#include "propspec.hxx"
+
+//GUID CLSID_Storage = PSGUID_STORAGE;
+//
+//
+//refer to ms-help://MS.VSCC/MS.MSDNVS.2052/com/stgasstg_7agk.htm
+//FMTID_SummaryInformation
+//
+//GUID CLSID_SummaryInforation = {
+//    0xF29F85E0,
+//    0x4FF9,
+//    0x1068,
+//    { 0xAB, 0x91, 0x08, 0x00, 0x2B, 0x27, 0xB3, 0xD9 }
+//};
+//+-------------------------------------------------------------------------
+//
+//  Member:     CFullPropSpec::CFullPropSpec, public
+//
+//  Synopsis:   Default constructor
+//
+//  Effects:    Defines property with null guid and propid 0
+//
+//--------------------------------------------------------------------------
+
+CFullPropSpec::CFullPropSpec()
+{
+    memset( &_guidPropSet, 0, sizeof( _guidPropSet ) );
+    _psProperty.ulKind = PRSPEC_PROPID;
+    _psProperty.propid = 0;
+}
+//+-------------------------------------------------------------------------
+//
+//  Member:     CFullPropSpec::CFullPropSpec, public
+//
+//  Synopsis:   Construct propid based propspec
+//
+//  Arguments:  [guidPropSet]  -- Property set
+//              [pidProperty] -- Property
+//
+//--------------------------------------------------------------------------
+CFullPropSpec::CFullPropSpec( GUID const & guidPropSet, PROPID pidProperty ) :
+    _guidPropSet( guidPropSet )
+{
+    _psProperty.ulKind = PRSPEC_PROPID;
+    _psProperty.propid = pidProperty;
+}
+//+-------------------------------------------------------------------------
+//
+//  Member:     CFullPropSpec::CFullPropSpec, public
+//
+//  Synopsis:   Construct name based propspec
+//
+//  Arguments:  [guidPropSet] -- Property set
+//              [wcsProperty] -- Property
+//
+//--------------------------------------------------------------------------
+CFullPropSpec::CFullPropSpec( GUID const & guidPropSet,
+                              WCHAR const * wcsProperty ) :
+    _guidPropSet( guidPropSet )
+{
+    _psProperty.ulKind = PRSPEC_PROPID;
+    SetProperty( wcsProperty );
+}
+//+-------------------------------------------------------------------------
+//
+//  Member:     CFullPropSpec::CFullPropSpec, public
+//
+//  Synopsis:   Copy constructor
+//
+//  Arguments:  [src] -- Source property spec
+//
+//--------------------------------------------------------------------------
+CFullPropSpec::CFullPropSpec( CFullPropSpec const & src ) :     _guidPropSet( src._guidPropSet )
+{
+    _psProperty.ulKind = src._psProperty.ulKind;
+    if ( _psProperty.ulKind == PRSPEC_LPWSTR )
+    {
+        if ( src._psProperty.lpwstr )
+        {
+            _psProperty.ulKind = PRSPEC_PROPID;
+            SetProperty( src._psProperty.lpwstr );
+        }
+        else
+            _psProperty.lpwstr = 0;
+    }
+    else
+    {
+        _psProperty.propid = src._psProperty.propid;
+    }
+}
+inline void * operator new( size_t /*size*/, void * p )
+{
+    return( p );
+}
+//+-------------------------------------------------------------------------
+//
+//  Member:     CFullPropSpec::operator=, public
+//
+//  Synopsis:   Assignment operator
+//
+//  Arguments:  [Property] -- Source property
+//
+//--------------------------------------------------------------------------
+CFullPropSpec & CFullPropSpec::operator=( CFullPropSpec const & Property )
+{
+    // Clean up.
+    CFullPropSpec::~CFullPropSpec();
+#pragma warning( disable : 4291 )           // unmatched operator new
+    new (this) CFullPropSpec( Property );
+#pragma warning( default : 4291 )
+    return *this;
+}
+CFullPropSpec::~CFullPropSpec()
+{
+    if ( _psProperty.ulKind == PRSPEC_LPWSTR &&
+         _psProperty.lpwstr )
+    {
+        CoTaskMemFree( _psProperty.lpwstr );
+    }
+}
+void CFullPropSpec::SetProperty( PROPID pidProperty )
+{
+    if ( _psProperty.ulKind == PRSPEC_LPWSTR &&
+         0 != _psProperty.lpwstr )
+    {
+        CoTaskMemFree( _psProperty.lpwstr );
+    }
+    _psProperty.ulKind = PRSPEC_PROPID;
+    _psProperty.propid = pidProperty;
+}
+BOOL CFullPropSpec::SetProperty( WCHAR const * wcsProperty )
+{
+    if ( _psProperty.ulKind == PRSPEC_LPWSTR &&
+         0 != _psProperty.lpwstr )
+    {
+        CoTaskMemFree( _psProperty.lpwstr );
+    }
+    _psProperty.ulKind = PRSPEC_LPWSTR;
+    int len = (int) ( (wcslen( wcsProperty ) + 1) * sizeof( WCHAR ) );
+    _psProperty.lpwstr = (WCHAR *)CoTaskMemAlloc( len );
+    if ( 0 != _psProperty.lpwstr )
+    {
+        memcpy( _psProperty.lpwstr,
+                wcsProperty,
+                len );
+        return( TRUE );
+    }
+    else
+    {
+        _psProperty.lpwstr = 0;
+        return( FALSE );
+    }
+}
+int CFullPropSpec::operator==( CFullPropSpec const & prop ) const
+{
+    if ( memcmp( &prop._guidPropSet,
+                 &_guidPropSet,
+                 sizeof( _guidPropSet ) ) != 0 ||
+         prop._psProperty.ulKind != _psProperty.ulKind )
+    {
+        return( 0 );
+    }
+    switch( _psProperty.ulKind )
+    {
+    case PRSPEC_LPWSTR:
+        return( _wcsicmp( GetPropertyName(), prop.GetPropertyName() ) == 0 );
+        break;
+    case PRSPEC_PROPID:
+        return( GetPropertyPropid() == prop.GetPropertyPropid() );
+        break;
+    default:
+        return( 0 );
+        break;
+    }
+}
+int CFullPropSpec::operator!=( CFullPropSpec const & prop ) const
+{
+    if (*this == prop)
+        return( 0 );
+    else
+        return( 1 );
+}
+
