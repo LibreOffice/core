75db4ceaba9cda1cb0c3c6d7a0fb6c54555a8fba
Rüdiger Timm <rt@openoffice.org>
2004-09-08 14:05:36 +0000

    INTEGRATION: CWS ooo20040704 (1.11.34); FILE MERGED 2004/06/28 14:49:06 cmc 1.11.34.1: #i30801# allow using system stl if possible

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bitmapex.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-24 15:25:04 $
+ *  last change: $Author: rt $ $Date: 2004-09-08 15:05:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,6 @@
  *
  ************************************************************************/
 
-#include <hash_map>
 #include <ctype.h>
 
 #ifndef _RTL_CRC_H_
