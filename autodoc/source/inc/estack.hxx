ea34fbf986a396276e52b34438b39ad35654cc9d
Rüdiger Timm <rt@openoffice.org>
2004-09-08 13:17:03 +0000

    INTEGRATION: CWS ooo20040704 (1.2.70); FILE MERGED 2004/07/02 14:17:53 cmc 1.2.70.2: #i30891# revert header and namespace change 2004/06/28 11:49:46 cmc 1.2.70.1: #i30801 allow using system stl if possible

--- autodoc/source/inc/estack.hxx
+++ autodoc/source/inc/estack.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: estack.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2004-07-30 15:22:07 $
+ *  last change: $Author: rt $ $Date: 2004-09-08 14:17:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
