8a135b066ba1da1290800f35e330254861ca2a53
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 12:44:40 +0000

    INTEGRATION: CWS dr12 (1.1.2); FILE ADDED 2004/08/26 15:38:08 jmarmion 1.1.2.5: #i32667# - support EE_PARA_JUST in notes. 2004/07/23 20:58:06 sab 1.1.2.4: #i21253#; add formatted notes 2004/07/09 16:41:06 jmarmion 1.1.2.3: #i21255# add support for creation of DrawLayer() for notes. 2004/04/19 17:53:48 jmarmion 1.1.2.2: #i25335,i26291# note sizing and text box enhancements. 2004/02/24 10:06:16 jmarmion 1.1.2.1: #i21255# text formatting for cell notes

---
+++ sc/source/core/data/postit.cxx
@@ -0,0 +1,289 @@
+/*************************************************************************
+ *
+ *  $RCSfile: postit.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-09-08 13:44:40 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#ifdef PCH
+#include "core_pch.hxx"
+#endif
+
+#pragma hdrstop
+
+// INCLUDE ---------------------------------------------------------------
+
+#ifndef SC_POSTIT_HXX
+#include "postit.hxx"
+#endif
+
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+#ifndef _SFX_OBJSH_HXX
+#include <sfx2/objsh.hxx>
+#endif
+#ifndef _EDITOBJ_HXX
+#include <svx/editobj.hxx>
+#endif
+#ifndef _UNOTOOLS_CHARCLASS_HXX
+#include <unotools/charclass.hxx>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+
+#ifndef SC_ITEMS_HXX
+#include "scitems.hxx"
+#endif
+
+#ifndef SC_DOCUMENT_HXX
+#include "document.hxx"
+#endif
+#ifndef SC_EDITUTIL_HXX
+#include "editutil.hxx"
+#endif
+#ifndef SC_SCATTR_HXX
+#include "attrib.hxx"
+#endif
+#ifndef SC_DETFUNC_HXX
+#include "detfunc.hxx"
+#endif
+#ifndef SC_SCPATATR_HXX
+#include "patattr.hxx"
+#endif
+#ifndef SC_DRWLAYER_HXX
+#include "drwlayer.hxx"
+#endif
+#ifndef SC_DOCUNO_HXX
+#include "docuno.hxx"
+#endif
+#ifndef _SVDPAGE_HXX
+#include <svx/svdpage.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
+#include <com/sun/star/uno/Reference.hxx>
+#endif
+
+namespace com { namespace sun { namespace star { namespace frame { class XModel; } } } }
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::frame::XModel;
+
+//========================================================================
+// class ScPostIt
+//========================================================================
+
+ScPostIt::ScPostIt(ScDocument* pDoc):
+    mpDoc(pDoc),
+    mbShown(FALSE),
+    maItemSet(pDoc->GetNoteItemPool(), SDRATTR_START,  SDRATTR_END, EE_ITEMS_START, EE_ITEMS_END, 0,0)
+{
+    // this 'default' ctor  does not set the Author & Date stamp.
+    // maRectangle is not initialised as it can be tested using IsEmpty().
+}
+
+ScPostIt::ScPostIt( const String& rText, ScDocument* pDoc ):
+    mpDoc(pDoc),
+    mbShown(FALSE),
+    maItemSet(pDoc->GetNoteItemPool(), SDRATTR_START,  SDRATTR_END, EE_ITEMS_START, EE_ITEMS_END, 0,0)
+{
+    // maRectangle is not initialised as it can be tested using IsEmpty().
+    SetText( rText);
+}
+
+ScPostIt::ScPostIt( const EditTextObject* pTextObj, ScDocument* pDoc ):
+    mpDoc ( pDoc ),
+    mbShown(FALSE),
+    maItemSet(pDoc->GetNoteItemPool(), SDRATTR_START,  SDRATTR_END, EE_ITEMS_START, EE_ITEMS_END, 0,0)
+{
+    // maRectangle is not initialised as it can be tested using IsEmpty().
+    SetEditTextObject( pTextObj);
+}
+
+ScPostIt::~ScPostIt()
+{
+}
+
+// Support existing functionality - create simple text string from the
+// EditTextObject.
+String ScPostIt::GetText() const
+{
+    String aText;
+    const EditTextObject* pEditText;
+    if(mpDoc && ((pEditText = GetEditTextObject()) != 0))
+    {
+        ScNoteEditEngine& rEE = mpDoc->GetNoteEngine();
+        rEE.SetText( *pEditText );
+        aText = rEE.GetText( LINEEND_LF );
+        aText.SearchAndReplaceAll(CHAR_CR, ' ');
+    }
+    return aText;
+}
+
+void ScPostIt::SetEditTextObject( const EditTextObject* pTextObj)
+{
+    if(pTextObj)
+    {
+        mpEditObj.reset(pTextObj->Clone());
+        AutoStamp( );
+    }
+    else
+        mpEditObj.reset();
+}
+
+// Support existing functionality - create EditTextObject from a simple
+// text string
+void ScPostIt::SetText( const String& rText)
+{
+    if(mpDoc && rText.Len())
+    {
+        ScNoteEditEngine& rEE = mpDoc->GetNoteEngine();
+        rEE.SetText( rText );
+        mpEditObj.reset(rEE.CreateTextObject());
+        AutoStamp( );
+    }
+    else
+        mpEditObj.reset();
+
+}
+
+void ScPostIt::AutoStamp( )
+{
+    maStrDate   = ScGlobal::pLocaleData->getDate( Date() );
+
+    SvtUserOptions aUserOpt;
+    maStrAuthor = aUserOpt.GetID();
+}
+
+BOOL ScPostIt::IsEmpty() const
+{
+    return (!mpEditObj.get());
+}
+
+Rectangle ScPostIt::DefaultRectangle(const ScAddress& rPos) const
+{
+    BOOL bNegativePage = mpDoc->IsNegativePage( rPos.Tab() );
+    long nPageSign = bNegativePage ? -1 : 1;
+
+    SCCOL nNextCol = rPos.Col()+1;
+    const ScMergeAttr* pMerge = (const ScMergeAttr*) mpDoc->GetAttr( rPos.Col(), rPos.Row(), rPos.Tab(), ATTR_MERGE );
+    if ( pMerge->GetColMerge() > 1 )
+        nNextCol = rPos.Col() + pMerge->GetColMerge();
+
+    Point aRectPos = ScDetectiveFunc(mpDoc, rPos.Tab()).GetDrawPos( nNextCol, rPos.Row(), FALSE );
+
+    aRectPos.X() += 600 * nPageSign;
+    aRectPos.Y() -= 1500;
+    if ( aRectPos.Y() < 0 ) aRectPos.Y() = 0;
+
+    String aText = GetText();
+    long nDefWidth = ( aText.Len() > SC_NOTE_SMALLTEXT ) ? 5800 : 2900;
+    Size aRectSize( nDefWidth, 1800 );
+
+    long nMaxWidth = aRectSize.Width();
+
+    if ( bNegativePage )
+        aRectPos.X() -= aRectSize.Width();
+    else
+        if ( aRectPos.X() < 0 ) aRectPos.X() = 0;
+
+    return Rectangle(aRectPos, aRectSize);
+}
+
+void ScPostIt::SetItemSet(const SfxItemSet& rItemSet)
+{
+    maItemSet.PutExtended(rItemSet, SFX_ITEM_DONTCARE, SFX_ITEM_DEFAULT);
+}
+
+void ScPostIt::InsertObject(SdrCaptionObj* pObj, ScDocument& rDoc, SCTAB nTab) const
+{
+    SdrPage* pPage = NULL;
+    ScDrawLayer* pDrawModel = rDoc.GetDrawLayer();
+    // Copying note cells between documents is one user scenario where the
+    // DrawLayer may not exist. Use the ScModelObj() to create
+    // and ensure there is a DrawLayer.
+    if(!pDrawModel)
+    {
+        ScModelObj* pScModelObj = NULL;
+        SfxObjectShell* pDocShell = rDoc.GetDocumentShell();
+        if(pDocShell)
+            pScModelObj = ScModelObj::getImplementation( Reference< XModel >( pDocShell->GetModel() ) );
+        if(pScModelObj)
+            pDrawModel= pScModelObj->MakeDrawLayer();
+        if(pDrawModel)
+            pPage = pDrawModel->GetPage(nTab);
+    }
+    else
+        pPage = pDrawModel->GetPage(nTab);
+
+    if(pPage && pObj)
+        pPage->InsertObject(pObj);
+}
+
+void ScPostIt::RemoveObject(SdrCaptionObj* pObj, ScDocument& rDoc, SCTAB nTab) const
+{
+    SdrPage* pPage = NULL;
+    ScDrawLayer* pDrawModel = rDoc.GetDrawLayer();
+    if(pDrawModel)
+        pPage = pDrawModel->GetPage(nTab);
+    if(pPage && pObj)
+        pPage->RemoveObject(pObj->GetOrdNum());
+}
+
+//========================================================================
