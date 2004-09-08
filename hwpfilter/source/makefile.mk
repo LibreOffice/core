3d8a8afb2cd00cf07233a6bc9ccf5e736ad3c99b
Rüdiger Timm <rt@openoffice.org>
2004-09-08 15:17:28 +0000

    INTEGRATION: CWS ooo20040704 (1.2.18); FILE MERGED 2004/06/28 07:21:32 cmc 1.2.18.1: #i30801 allow using system libs if possible

--- hwpfilter/source/makefile.mk
+++ hwpfilter/source/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hjs $ $Date: 2004-06-28 16:39:57 $
+#   last change: $Author: rt $ $Date: 2004-09-08 16:17:28 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,6 +77,7 @@ ENABLE_EXCEPTIONS=TRUE
 .IF "$(SYSTEM_ZLIB)" == "YES"
 CFLAGS+=-DSYSTEM_ZLIB
 .ENDIF
+
 SLOFILES =	\
         $(SLO)$/attributes.obj \
         $(SLO)$/cspline.obj \
