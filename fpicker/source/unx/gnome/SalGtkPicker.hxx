7af8ae1585949c6529d99a07df1c668ac26e0c50
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 16:08:43 +0000

    INTEGRATION: CWS gtkfpicker1 (1.1.2); FILE ADDED 2004/09/08 09:42:09 cmc 1.1.2.1: #i32555# use system gtk folderpicker like gtk filepicker

---
+++ fpicker/source/unx/gnome/SalGtkPicker.hxx
@@ -0,0 +1,116 @@
+/*************************************************************************
+ *
+ *  $RCSfile: SalGtkPicker.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-09-08 17:08:43 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): Anil Bhatia
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SALGTKFPICKER_HXX_
+#define _SALGTKFPICKER_HXX_
+
+//_____________________________________________________________________________
+//  includes of other projects
+//_____________________________________________________________________________
+
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_UI_DIALOGS_XFILEPICKER_HPP_
+#include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UI_XFOLDERPICKER_HPP_
+#include <com/sun/star/ui/dialogs/XFolderPicker.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UTIL_XCANCELLABLE_HPP_
+#include <com/sun/star/util/XCancellable.hpp>
+#endif
+
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+
+//----------------------------------------------------------
+// class declaration
+//----------------------------------------------------------
+
+class SalGtkPicker
+{
+    public:
+        SalGtkPicker() : m_pDialog(0) {}
+        virtual ~SalGtkPicker();
+    protected:
+        osl::Mutex m_rbHelperMtx;
+        GtkWidget  *m_pDialog;
+    protected:
+        virtual void SAL_CALL implsetTitle( const ::rtl::OUString& aTitle )
+            throw( ::com::sun::star::uno::RuntimeException );
+
+        virtual void SAL_CALL implsetDisplayDirectory( const rtl::OUString& rDirectory )
+            throw( com::sun::star::lang::IllegalArgumentException, com::sun::star::uno::RuntimeException );
+
+        virtual rtl::OUString SAL_CALL implgetDisplayDirectory(  )
+            throw( com::sun::star::uno::RuntimeException );
+
+};
+
+#endif
