5d439c31278af2703d3ff980266f4933c6d636a9
Oliver Bolte <obo@openoffice.org>
2004-09-08 13:02:24 +0000

    INTEGRATION: CWS qwizards2 (1.1.2); FILE ADDED 2004/07/21 15:54:05 tv 1.1.2.2: several small bugfixes, incl. exchanging some listboxes with combo boxes 2004/06/04 10:34:35 tv 1.1.2.1: initial revision of the new fax wizard

---
+++ wizards/com/sun/star/wizards/fax/CGFax.java
@@ -0,0 +1,33 @@
+package com.sun.star.wizards.fax;
+
+import com.sun.star.wizards.common.ConfigGroup;
+
+public class CGFax extends ConfigGroup {
+
+    public int cp_Style;
+    public boolean cp_PrintCompanyLogo;
+    public boolean cp_PrintDate;
+    public boolean cp_PrintSubjectLine;
+    public boolean cp_PrintSalutation;
+    public boolean cp_PrintCommunicationType;
+    public boolean cp_PrintGreeting;
+    public boolean cp_PrintFooter;
+    public String cp_CommunicationType;
+    public String cp_Salutation;
+    public String cp_Greeting;
+    public int cp_SenderAddressType;
+    public String cp_SenderCompanyName;
+    public String cp_SenderStreet;
+    public String cp_SenderPostCode;
+    public String cp_SenderState;
+    public String cp_SenderCity;
+    public String cp_SenderFax;
+    public int cp_ReceiverAddressType;
+    public String cp_Footer;
+    public boolean cp_FooterOnlySecondPage;
+    public boolean cp_FooterPageNumbers;
+    public int cp_CreationType;
+    public String cp_TemplateName;
+    public String cp_TemplatePath;
+
+}
