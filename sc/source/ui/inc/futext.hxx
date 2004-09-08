bb71617e7f5d8db81b32e3facf9293a8971ec00f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 12:55:37 +0000

    INTEGRATION: CWS dr12 (1.3.280); FILE MERGED 2004/04/19 17:52:48 jmarmion 1.3.280.1: #i25335,i26291# note sizing and text box enhancements.

--- sc/source/ui/inc/futext.hxx
+++ sc/source/ui/inc/futext.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: futext.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: nn $ $Date: 2002-05-07 17:38:47 $
+ *  last change: $Author: hr $ $Date: 2004-09-08 13:55:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -102,6 +102,7 @@ public:
     void    SetInEditMode( SdrObject* pObj = NULL, const Point* pMousePixel = NULL,
                             BOOL bCursorToEnd = FALSE, const KeyEvent* pInitialKey = NULL );
     void    StopEditMode();
+    void    StopDragMode(SdrObject* pObject);
 
     // #98185# Create default drawing objects via keyboard
     virtual SdrObject* CreateDefaultObject(const sal_uInt16 nID, const Rectangle& rRectangle);
