662768b9b192f07f82547083ab84fd299dfea2f8
Oliver Bolte <obo@openoffice.org>
2004-09-08 13:19:28 +0000

    INTEGRATION: CWS qwizards2 (1.3.2); FILE MERGED 2004/07/30 14:55:55 rpiterman 1.3.2.1: bugfixes Web Wizard

--- wizards/com/sun/star/wizards/web/data/makefile.mk
+++ wizards/com/sun/star/wizards/web/data/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-07-23 14:26:09 $
+#   last change: $Author: obo $ $Date: 2004-09-08 14:19:28 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -82,6 +82,7 @@ JAVAFILES=			\
     CGDesign.java \
     CGDocument.java \
     CGExporter.java \
+    CGFilter.java \
     CGGeneralInfo.java \
     CGIconSet.java \
     CGImage.java \
