06abaa6370df3fb6ede53fb91e74f56c4e7d5092
Rüdiger Timm <rt@openoffice.org>
2004-09-08 14:55:16 +0000

    INTEGRATION: CWS ooo20040704 (1.2.64); FILE MERGED 2004/07/02 14:43:04 haggai 1.2.64.1: #i31044# Add missing EOLs to keep javac happy

--- qadevOOo/tests/java/ifc/sdb/_XSingleSelectQueryComposer.java
+++ qadevOOo/tests/java/ifc/sdb/_XSingleSelectQueryComposer.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XSingleSelectQueryComposer.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-12-16 12:36:28 $
+ *  last change:$Date: 2004-09-08 15:55:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
