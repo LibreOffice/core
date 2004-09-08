924157e929265e8dce1a3f98d9b775dd4f53500a
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 13:28:38 +0000

    INTEGRATION: CWS desktintgr02 (1.1.2); FILE ADDED 2004/08/02 03:22:03 deuce 1.1.2.2: Issue number: i21110 Submitted by: Gorden Lin {Gorden.Lin@Sun.com} Reviewed by: Tino Rachui {Tino.Rachui@Sun.com} 2004/07/29 09:28:25 deuce 1.1.2.1: Issue number:  21110 Submitted by:  Gorden Lin {gorden.lin@sun.com} Reviewed by:   Tino Rachui {tino.rachui@sun.com}

---
+++ shell/source/all/ooofilereader/basereader.cxx
@@ -0,0 +1,127 @@
+/*************************************************************************
+ *
+ *  $RCSfile: basereader.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-09-08 14:28:38 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef BASEREADER_HXX_INCLUDED
+#include "internal/basereader.hxx"
+#endif
+
+#ifndef XML_PARSER_HXX_INCLUDED
+#include "internal/xml_parser.hxx"
+#endif
+
+#include "assert.h"
+#include <memory>
+
+/**  constructor of CBaseReader.
+*/
+CBaseReader::CBaseReader(const std::string& DocumentName):
+m_ZipFile( DocumentName )
+{
+}
+
+//------------------------------
+//
+//------------------------------
+
+CBaseReader::~CBaseReader()
+{
+}
+
+//------------------------------
+//
+//------------------------------
+
+void CBaseReader::start_document()
+{
+}
+
+//------------------------------
+//
+//------------------------------
+
+void CBaseReader::end_document()
+{
+}
+
+/** Read interested tag content into respective structure then start parsing process.
+    @param ContentName
+    the xml file name in the zipped document which we interest.
+*/
+void CBaseReader::Initialize( const std::string& ContentName)
+{
+    try
+    {
+        if (m_ZipContent.empty())
+            m_ZipFile.GetUncompressedContent( ContentName, m_ZipContent );
+
+        xml_parser parser;
+        parser.set_document_handler(this);  // pass current reader as reader to the sax parser
+        parser.parse(&m_ZipContent[0], m_ZipContent.size());
+    }
+    catch(std::exception& ex)
+    {
+        ENSURE( false, ex.what() );
+    }
+    catch(...)
+    {
+        ENSURE(false, "Unknown error");
+    }
+}
