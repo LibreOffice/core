40df8bcc69f2fef03bec99faf42bfc2cacd20afa
Rüdiger Timm <rt@openoffice.org>
2004-09-08 15:16:02 +0000

    INTEGRATION: CWS ooo20040704 (1.2.12); FILE MERGED 2004/07/03 13:50:53 vq 1.2.12.2: #i27032# Thanks to the partly *crossmerge* of 27032 this change has also to be crossmerged. This touches all lines of the file because the lineendings were corrected to unix style. 2004/06/28 14:46:09 cmc 1.2.12.1: #i30801# allow using system libs if possible

--- unoxml/source/service/makefile.mk
+++ unoxml/source/service/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2004-07-23 11:03:50 $
+#   last change: $Author: rt $ $Date: 2004-09-08 16:16:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -72,6 +72,9 @@ ENABLE_EXCEPTIONS=TRUE
 
 .INCLUDE :  settings.mk
 RSCUPDVER=$(RSCREVISION)(SV$(UPD)$(UPDMINOR))
+.IF "$(SYSTEM_LIBXML)" == "YES"
+CFLAGS+=-DSYSTEM_LIBXML $(LIBXML_CFLAGS)
+.ENDIF
 
 # --- Files --------------------------------------------------------
 
