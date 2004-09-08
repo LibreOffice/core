c2f8149f0275acaa00da250d206ba55eec9204e4
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-08 13:21:58 +0000

    INTEGRATION: CWS desktintgr02 (1.3.52); FILE MERGED 2004/08/18 13:30:13 is 1.3.52.1: #i33152# shellnew files into template folder

--- solenv/bin/modules/installer/windows/component.pm
+++ solenv/bin/modules/installer/windows/component.pm
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: component.pm,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2004-06-11 18:18:38 $
+#   last change: $Author: hr $ $Date: 2004-09-08 14:21:45 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -118,6 +118,15 @@ sub get_file_component_directory
     if ( $localstyles =~ /\bFONT\b/ )   # special handling for font files
     {
         return $installer::globals::fontsfolder;
+    }
+
+    my $destdir = "";
+
+    if ( $onefile->{'Dir'} ) { $destdir = $onefile->{'Dir'}; }
+
+    if ( $destdir =~ /\bPREDEFINED_OSSHELLNEWDIR\b/ )   # special handling for shellnew files
+    {
+        return $installer::globals::templatefolder;
     }
 
     my $destination = $onefile->{'destination'};
--- solenv/bin/modules/installer/windows/directory.pm
+++ solenv/bin/modules/installer/windows/directory.pm
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: directory.pm,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2004-06-11 18:19:04 $
+#   last change: $Author: hr $ $Date: 2004-09-08 14:21:58 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -207,6 +207,9 @@ sub add_root_directories
         # push(@{$directorytableref}, $oneline);
 
         $oneline = "$installer::globals::startupfolder\tTARGETDIR\t.\n";
+        push(@{$directorytableref}, $oneline);
+
+        $oneline = "$installer::globals::templatefolder\tTARGETDIR\t.\n";
         push(@{$directorytableref}, $oneline);
 
         $oneline = "$installer::globals::fontsfolder\tTARGETDIR\t$installer::globals::fontsfoldername\n";
