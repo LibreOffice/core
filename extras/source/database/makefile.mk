6bbf79cfb99dc6c27993e55de6b5e6027f6fa091
Rüdiger Timm <rt@openoffice.org>
2004-09-09 08:13:21 +0000

    INTEGRATION: CWS insight02 (1.3.2); FILE MERGED 2004/08/11 13:11:35 oj 1.3.2.2: moved again to biblio 2004/08/11 13:02:31 oj 1.3.2.1: biblio.odb

--- extras/source/database/makefile.mk
+++ extras/source/database/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2004-08-02 14:31:17 $
+#   last change: $Author: rt $ $Date: 2004-09-09 09:13:21 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,11 +70,9 @@ TARGET=databasegeneral
 
 .INCLUDE : $(PRJ)$/util$/target.pmk
 
-
-
 ZIP1TARGET      = $(DATABASE_USER_TARGET)
 
-ZIP1LIST        = *.odb
+ZIP1LIST        = biblio.odb
 
 ZIP2TARGET      = $(DATABASE_USER_BIBLIO_TARGET)
 
