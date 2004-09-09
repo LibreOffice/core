697831056c669331e45f99646eeebcb000ea162c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-09-09 10:15:54 +0000

    INTEGRATION: CWS ooo20040815 (1.9.72); FILE MERGED 2004/07/28 10:29:29 waratah 1.9.72.2: Remove -fno-for-scope and -fpermissive from all parameters 2004/07/28 07:18:13 pjanik 1.9.72.1: #i31791#: Remove -fno-for-scope which was deprecated for OpenOffice.org builds.

--- solenv/inc/unxfbsdi.mk
+++ solenv/inc/unxfbsdi.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxfbsdi.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: svesik $ $Date: 2004-04-21 13:54:46 $
+#   last change: $Author: hr $ $Date: 2004-09-09 11:15:54 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -98,7 +98,7 @@ CFLAGS+= -I/usr/X11R6/include
 
 # flags for the C++ Compiler
 CFLAGSCC= -pipe -fno-rtti
-CFLAGSCXX= -pipe -fno-for-scope -fpermissive -fno-rtti
+CFLAGSCXX= -pipe -fno-rtti
 
 # Flags for enabling exception handling
 CFLAGSEXCEPTIONS= -fexceptions
