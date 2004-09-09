1a847bd38cd546b43b5eebb94176d0f96398b7cc
Rüdiger Timm <rt@openoffice.org>
2004-09-09 08:43:30 +0000

    INTEGRATION: CWS insight02 (1.2.2); FILE MERGED 2004/08/13 09:01:05 oj 1.2.2.1: #i32563# toolbox changes

--- dbaccess/source/ui/imagelists/makefile.mk
+++ dbaccess/source/ui/imagelists/makefile.mk
@@ -2,9 +2,9 @@
  #
  #  $RCSfile: makefile.mk,v $
  #
- #  $Revision: 1.2 $
+ #  $Revision: 1.3 $
  #
- #  last change: $Author: hr $ $Date: 2004-08-02 15:51:32 $
+ #  last change: $Author: rt $ $Date: 2004-09-09 09:43:30 $
  #
  #  The Contents of this file are made available subject to the terms of
  #  either of the following licenses
@@ -70,8 +70,6 @@ IMGLST_SRS=$(SRS)$/$(TARGET).srs
 # --- Files -------------------------------------
 
 # ... resource files ............................
-
-BMP_IN=$(PRJ)$/res
 
 SRS1NAME=$(TARGET)
 SRC1FILES=dbimagelists.src
