c06cad7e7a8e607646ebd886ddb61b9030bde0ce
Rüdiger Timm <rt@openoffice.org>
2004-09-17 12:32:09 +0000

    INTEGRATION: CWS os34 (1.3.314); FILE MERGED 2004/09/16 08:13:48 tl 1.3.314.2: #i30303# merged to m53 2004/08/16 11:47:41 tl 1.3.314.1: #i30303# simplified/traditional Chinese conversion

--- sw/source/ui/uiview/makefile.mk
+++ sw/source/ui/uiview/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: kz $ $Date: 2004-08-02 09:58:50 $
+#   last change: $Author: rt $ $Date: 2004-09-17 13:32:09 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -103,7 +103,8 @@ SLOFILES =  \
 
 EXCEPTIONSFILES= \
         $(SLO)$/srcview.obj \
-        $(SLO)$/uivwimp.obj
+        $(SLO)$/uivwimp.obj \
+        $(SLO)$/viewling.obj
 
 # --- Tagets -------------------------------------------------------
 
