e7c17fa9d8965e64e749c1020b5ba85444efd303
Rüdiger Timm <rt@openoffice.org>
2004-09-17 12:47:37 +0000

    INTEGRATION: CWS os34 (1.1.2); FILE ADDED 2004/09/07 12:26:48 tl 1.1.2.5: #117820# hide and disable 'use variants...' checkbox 2004/08/30 15:42:57 iha 1.1.2.4: #i33568# missing help ids for chinese conversion dialogs 2004/08/17 14:17:17 iha 1.1.2.3: #i31579# chinese simplified traditional translation dialogs 2004/08/15 09:09:11 iha 1.1.2.2: #i31579# chinese simplified traditional translation dialogs - setDirection to Dictionary Dialog 2004/08/13 12:13:56 iha 1.1.2.1: #i31579# chinese simplified traditional translation dialogs

---
+++ svx/source/unodialogs/textconversiondlgs/chinese_translationdialog.cxx
@@ -0,0 +1,241 @@
+/*************************************************************************
+ *
+ *  $RCSfile: chinese_translationdialog.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-09-17 13:47:37 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2004 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "chinese_translationdialog.hxx"
+#include "chinese_translationdialog.hrc"
+#include "resid.hxx"
+#include "chinese_dictionarydialog.hxx"
+
+#ifndef _COM_SUN_STAR_I18N_TEXTCONVERSIONOPTION_HPP_
+#include <com/sun/star/i18n/TextConversionOption.hpp>
+#endif
+// header for define RET_OK
+#ifndef _SV_MSGBOX_HXX
+#include <vcl/msgbox.hxx>
+#endif
+// header for class SvtLinguConfigItem
+#ifndef _SVTOOLS_LINGUCFG_HXX_
+#include <svtools/lingucfg.hxx>
+#endif
+#ifndef _SVTOOLS_LINGUPROPS_HXX_
+#include <svtools/linguprops.hxx>
+#endif
+#ifndef _SVX_HELPID_HRC
+#include "helpid.hrc"
+#endif
+
+//disable compiler warning C4355: 'this' : used in base member initializer list
+#ifdef _MSC_VER
+#  pragma warning (disable : 4355)
+#endif
+
+//.............................................................................
+namespace textconversiondlgs
+{
+//.............................................................................
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+
+ChineseTranslationDialog::ChineseTranslationDialog( Window* pParent )
+    : ModalDialog( pParent, TextConversionDlgs_ResId( DLG_CHINESETRANSLATION ) )
+    , m_aFL_Direction( this, ResId( FL_DIRECTION ) )
+    , m_aRB_To_Simplified( this, ResId( RB_TO_SIMPLIFIED ) )
+    , m_aRB_To_Traditional( this, ResId( RB_TO_TRADITIONAL ) )
+    , m_aCB_Use_Variants( this, ResId( CB_USE_VARIANTS ) )
+    , m_aFL_Commonterms( this, ResId( FL_COMMONTERMS ) )
+    , m_aCB_Translate_Commonterms( this, ResId( CB_TRANSLATE_COMMONTERMS ) )
+    , m_aPB_Editterms( this, ResId( PB_EDITTERMS ) )
+    , m_aFL_Bottomline( this, ResId( FL_BOTTOMLINE ) )
+    , m_aBP_OK( this, ResId( PB_OK ) )
+    , m_aBP_Cancel( this, ResId( PB_CANCEL ) )
+    , m_aBP_Help( this, ResId( PB_HELP ) )
+    , m_pDictionaryDialog(0)
+{
+    FreeResource();
+
+    m_aRB_To_Simplified.SetHelpId( HID_SVX_CHINESE_TRANSLATION_RB_CONVERSION_TO_SIMPLIFIED );
+    m_aRB_To_Traditional.SetHelpId( HID_SVX_CHINESE_TRANSLATION_RB_CONVERSION_TO_TRADITIONAL );
+    m_aCB_Use_Variants.SetHelpId( HID_SVX_CHINESE_TRANSLATION_CB_USE_VARIANTS );
+
+    SvtLinguConfig  aLngCfg;
+    sal_Bool bValue;
+    Any aAny( aLngCfg.GetProperty( rtl::OUString::createFromAscii( UPN_IS_DIRECTION_TO_SIMPLIFIED ) ) );
+    aAny >>= bValue;
+    if( bValue )
+        m_aRB_To_Simplified.Check();
+    else
+        m_aRB_To_Traditional.Check();
+
+    aAny = aLngCfg.GetProperty( rtl::OUString::createFromAscii( UPN_IS_USE_CHARACTER_VARIANTS ) );
+    if( aAny >>= bValue )
+        m_aCB_Use_Variants.Check( bValue );
+
+    // #117820# (search for other occurences!)
+    // disable and hide that checkbox until it is decided if it is needed or not.
+    // If it is to be removed later the respective code needs to be removed as
+    // well, otherwise we just have to remove the next lines again.
+    m_aCB_Use_Variants.Check( sal_False );
+    m_aCB_Use_Variants.Enable( sal_False );
+    m_aCB_Use_Variants.Show( sal_False );
+
+    aAny = aLngCfg.GetProperty( rtl::OUString::createFromAscii( UPN_IS_TRANSLATE_COMMON_TERMS ) );
+    if( aAny >>= bValue )
+        m_aCB_Translate_Commonterms.Check( bValue );
+
+    m_aPB_Editterms.SetClickHdl( LINK( this, ChineseTranslationDialog, DictionaryHdl ) );
+    m_aRB_To_Simplified.SetClickHdl( LINK( this, ChineseTranslationDialog, DirectionHdl ) );
+    m_aRB_To_Traditional.SetClickHdl( LINK( this, ChineseTranslationDialog, DirectionHdl ) );
+    m_aCB_Translate_Commonterms.SetClickHdl( LINK( this, ChineseTranslationDialog, CommonTermsHdl ) );
+    m_aBP_OK.SetClickHdl( LINK( this, ChineseTranslationDialog, OkHdl ) );
+
+    impl_UpdateVariantsCheckBox();
+}
+
+ChineseTranslationDialog::~ChineseTranslationDialog()
+{
+    if(m_pDictionaryDialog)
+    {
+        if(m_pDictionaryDialog->IsInExecute())
+            m_pDictionaryDialog->EndDialog();
+        delete m_pDictionaryDialog;
+    }
+}
+
+void ChineseTranslationDialog::getSettings( sal_Bool& rbDirectionToSimplified
+                                          , sal_Bool& rbUseCharacterVariants
+                                          , sal_Bool& rbTranslateCommonTerms ) const
+{
+    rbDirectionToSimplified = m_aRB_To_Simplified.IsChecked();
+    rbUseCharacterVariants = m_aCB_Use_Variants.IsChecked();
+    rbTranslateCommonTerms = m_aCB_Translate_Commonterms.IsChecked();
+}
+
+void ChineseTranslationDialog::impl_UpdateVariantsCheckBox()
+{
+// #117820# (search for other occurences!)
+//    m_aCB_Use_Variants.Enable( m_aRB_To_Traditional.IsChecked() );
+}
+
+IMPL_LINK( ChineseTranslationDialog, DirectionHdl, void*, EMPTYARG )
+{
+    impl_UpdateVariantsCheckBox();
+    return 0;
+}
+
+IMPL_LINK( ChineseTranslationDialog, CommonTermsHdl, void*, EMPTYARG )
+{
+// #117820# (search for other occurences!)
+//    if( m_aCB_Translate_Commonterms.IsChecked() && m_aRB_To_Traditional.IsChecked() )
+//        m_aCB_Use_Variants.Check( true );
+    return 0;
+}
+
+IMPL_LINK( ChineseTranslationDialog, OkHdl, void*, EMPTYARG )
+{
+    //save settings to configuration
+    SvtLinguConfig  aLngCfg;
+    Any aAny;
+    aAny <<= sal_Bool( !!m_aRB_To_Simplified.IsChecked() );
+    aLngCfg.SetProperty( rtl::OUString::createFromAscii( UPN_IS_DIRECTION_TO_SIMPLIFIED ), aAny );
+    aAny <<= sal_Bool( !!m_aCB_Use_Variants.IsChecked() );
+    aLngCfg.SetProperty( rtl::OUString::createFromAscii( UPN_IS_USE_CHARACTER_VARIANTS ), aAny );
+    aAny <<= sal_Bool( !!m_aCB_Translate_Commonterms.IsChecked() );
+    aLngCfg.SetProperty( rtl::OUString::createFromAscii( UPN_IS_TRANSLATE_COMMON_TERMS ), aAny );
+
+    EndDialog( RET_OK );
+    return 0;
+}
+
+IMPL_LINK( ChineseTranslationDialog, DictionaryHdl, void*, EMPTYARG )
+{
+    if( !m_pDictionaryDialog )
+    {
+        m_pDictionaryDialog = new ChineseDictionaryDialog(this);
+    }
+    if( m_pDictionaryDialog )
+    {
+        if( m_pDictionaryDialog->IsInExecute() )
+        {
+            if( !m_pDictionaryDialog->IsReallyVisible() )
+            {
+                m_pDictionaryDialog->ToTop();
+                m_pDictionaryDialog->GrabFocusToFirstControl();
+            }
+        }
+        else
+        {
+            sal_Int32 nTextConversionOptions = i18n::TextConversionOption::NONE;
+            if( !m_aCB_Translate_Commonterms.IsChecked() )
+                nTextConversionOptions = nTextConversionOptions | i18n::TextConversionOption::CHARACTER_BY_CHARACTER;
+            if( m_aCB_Use_Variants.IsChecked() )
+                nTextConversionOptions = nTextConversionOptions | i18n::TextConversionOption::USE_CHARACTER_VARIANTS;
+
+            m_pDictionaryDialog->setDirectionAndTextConversionOptions( m_aRB_To_Simplified.IsChecked(), nTextConversionOptions );
+            m_pDictionaryDialog->Execute();
+        }
+    }
+    return 0;
+}
+
+//.............................................................................
+} //end namespace
+//.............................................................................
