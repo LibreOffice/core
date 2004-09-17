f6cb67e0a77f28370a8bd35b5181d1567ac61d1e
Rüdiger Timm <rt@openoffice.org>
2004-09-17 12:02:52 +0000

    INTEGRATION: CWS syssettings02 (1.2.2); FILE MERGED 2004/09/03 06:24:25 obr 1.2.2.1: #i20364#,#i20369# renamed componentdef to localebecdef due to dependency conflicts

--- shell/source/backends/localebe/makefile.mk
+++ shell/source/backends/localebe/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2004-08-20 14:10:32 $
+#   last change: $Author: rt $ $Date: 2004-09-17 13:02:52 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -83,7 +83,7 @@ DLLPRE =
 
 
 SLOFILES=\
-    $(SLO)$/componentdef.obj \
+    $(SLO)$/localebecdef.obj \
     $(SLO)$/localebackend.obj \
     $(SLO)$/localelayer.obj
         
