5b544855842a12cbfea853d488fc97498c2e2fcc
Rüdiger Timm <rt@openoffice.org>
2004-09-20 12:35:21 +0000

    INTEGRATION: CWS impress21ea (1.2.60); FILE MERGED 2004/09/17 11:29:09 af 1.2.60.1: #i33470# Made GetViewObjectContact() const.

--- sd/source/ui/slidesorter/model/SlsPageDescriptor.cxx
+++ sd/source/ui/slidesorter/model/SlsPageDescriptor.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SlsPageDescriptor.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-07-13 14:24:32 $
+ *  last change: $Author: rt $ $Date: 2004-09-20 13:35:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -219,7 +219,8 @@ void PageDescriptor::RemoveFocus (void)
 
 
 
-view::PageObjectViewObjectContact* PageDescriptor::GetViewObjectContact (void)
+view::PageObjectViewObjectContact*
+    PageDescriptor::GetViewObjectContact (void) const
 {
     return mpViewObjectContact;
 }
