6ab34a9ae8414c1377a1ee9269c6a613ff74758a
Rüdiger Timm <rt@openoffice.org>
2004-09-20 12:22:48 +0000

    INTEGRATION: CWS swmailmerge (1.1.2); FILE ADDED 2004/08/23 09:42:26 os 1.1.2.14: #i25107# change Roadmap entry dynamically 2004/08/12 14:29:09 os 1.1.2.13: #i25107# use skipUntil() instead of GotoPage() 2004/07/06 11:25:42 os 1.1.2.12: #i25107# weekly check-in 2004/07/02 09:15:16 os 1.1.2.11: #i25107# weekly check-in 2004/06/25 13:39:15 os 1.1.2.10: #i25107# weekly check-in 2004/06/17 07:02:33 os 1.1.2.9: #i25107# weekly check-in 2004/06/08 06:55:46 os 1.1.2.8: #i25107# buttons resorted 2004/06/07 07:14:18 os 1.1.2.7: #i25107# RoadMap added 2004/06/04 10:26:39 os 1.1.2.6: #i25107# mail merge dialogs move to swcui library 2004/06/03 06:45:06 os 1.1.2.5: #i25107# signature of createPage has changed 2004/05/28 12:46:06 os 1.1.2.4: #i25107# ChildWindow to return to the mail merge wizard 2004/04/30 14:32:31 os 1.1.2.3: #i25107# intermediate check-in 2004/04/16 12:47:17 os 1.1.2.2: #i25107# config item added; Layout page added 2004/04/08 08:43:07 os 1.1.2.1: #i25107# new mail merge wizard

---
+++ sw/source/ui/inc/mailmergewizard.hxx
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  $RCSfile: mailmergewizard.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-09-20 13:22:48 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _MAILMERGEWIZARD_HXX
+#define _MAILMERGEWIZARD_HXX
+
+#ifndef SVTOOLS_INC_ROADMAPWIZARD_HXX
+#include <svtools/roadmapwizard.hxx>
+#endif
+
+class SwView;
+class SwMailMergeConfigItem;
+
+#define MM_DOCUMENTSELECTPAGE   0
+#define MM_OUTPUTTYPETPAGE      1
+#define MM_ADDRESSBLOCKPAGE     2
+#define MM_GREETINGSPAGE        3
+#define MM_LAYOUTPAGE           4
+#define MM_PREPAREMERGEPAGE     5
+#define MM_MERGEPAGE            6
+#define MM_OUTPUTPAGE           7
+
+class SwMailMergeWizard : public ::svt::RoadmapWizard
+{
+    SwView*                 m_pSwView;
+    String                  sDocumentURL;
+    SwMailMergeConfigItem&  m_rConfigItem;
+
+    String                  m_sStarting;
+    String                  m_sDocumentType;
+    String                  m_sAddressBlock;
+    String                  m_sAddressList;
+    String                  m_sGreetingsLine;
+    String                  m_sLayout;
+    String                  m_sPrepareMerge;
+    String                  m_sMerge;
+    String                  m_sOutput;
+    String                  m_sFinish;
+
+    sal_uInt16              m_nRestartPage;
+
+protected:
+    virtual svt::OWizardPage*       createPage(WizardState _nState);
+    virtual void                    enterState( WizardState _nState );
+
+// roadmap feature ??
+//    virtual sal_Bool            prepareLeaveCurrentState( CommitPageReason _eReason );
+    virtual String                  getStateDisplayName( WizardState _nState );
+    virtual void                    StateChanged( StateChangedType nStateChange );
+
+public:
+    SwMailMergeWizard(SwView& rView, SwMailMergeConfigItem& rConfigItem);
+    ~SwMailMergeWizard();
+
+    SwView*                     GetSwView() {return m_pSwView;}
+    SwMailMergeConfigItem&      GetConfigItem() { return m_rConfigItem;}
+
+    void                    SetReloadDocument(const String& rURL){sDocumentURL = rURL;}
+    const String&           GetReloadDocument() const {return sDocumentURL;}
+
+    void                    UpdateRoadmap();
+    void                    CreateTargetDocument();
+
+    sal_uInt16              GetRestartPage() const {return m_nRestartPage;}
+    void                    SetRestartPage(sal_uInt16 nPage) { m_nRestartPage = nPage;}
+
+    sal_Bool                skipUntil( sal_uInt16 nPage)
+                                {return ::svt::RoadmapWizard::skipUntil(WizardState(nPage));}
+
+    void                    updateRoadmapItemLabel( WizardState _nState );
+};
+#endif
+
