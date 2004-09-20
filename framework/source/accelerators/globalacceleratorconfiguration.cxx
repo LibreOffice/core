9b6a1c7cb3d6e44f46da1107b5d97e7b0b0e7892
Rüdiger Timm <rt@openoffice.org>
2004-09-20 09:05:55 +0000

    INTEGRATION: CWS keyconfig01 (1.1.2); FILE ADDED 2004/07/20 09:48:46 as 1.1.2.5: #i29863# support load/save of acc cfg from/to documents, update all caches immediatly after changing one 2004/07/17 10:43:07 as 1.1.2.4: #i29863# close streams immediatly after using, patch res2xml tool, fix bugs inside read/write xml, establish preset handler 2004/07/09 08:53:45 as 1.1.2.3: #i29863# use new helper to cache and share storages based on her relativ path names 2004/07/08 06:45:40 as 1.1.2.2: #i29863# support localized UI config, support new UI directory structure, adopt converter tool to support accelerators 2004/06/22 07:53:25 as 1.1.2.1: #i29863# first revision

---
+++ framework/source/accelerators/globalacceleratorconfiguration.cxx
@@ -0,0 +1,183 @@
+/*************************************************************************
+ *
+ *  $RCSfile: globalacceleratorconfiguration.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-09-20 10:05:55 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_ACCELERATORS_GLOBALACCELERATORCONFIGURATION_HXX_
+#include <accelerators/globalacceleratorconfiguration.hxx>
+#endif
+
+//_______________________________________________
+// own includes
+
+#ifndef __FRAMEWORK_THREADHELP_READGUARD_HXX_
+#include <threadhelp/readguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_WRITEGUARD_HXX_
+#include <threadhelp/writeguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_ACCELERATORCONST_H_
+#include <acceleratorconst.h>
+#endif
+
+#ifndef __FRAMEWORK_SERVICES_H_
+#include <services.h>
+#endif
+
+//_______________________________________________
+// interface includes
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XSingleServiceFactory.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
+#include <com/sun/star/embed/ElementModes.hpp>
+#endif
+
+//_______________________________________________
+// other includes
+
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+
+#ifndef _COMPHELPER_LOCALE_HXX
+#include <comphelper/locale.hxx>
+#endif
+
+//_______________________________________________
+// const
+
+namespace framework
+{
+
+//-----------------------------------------------
+// XInterface, XTypeProvider, XServiceInfo
+DEFINE_XINTERFACE_1(GlobalAcceleratorConfiguration           ,
+                    AcceleratorConfiguration                 ,
+                    DIRECT_INTERFACE(css::lang::XServiceInfo))
+DEFINE_XTYPEPROVIDER_1_WITH_BASECLASS(GlobalAcceleratorConfiguration,
+                                      AcceleratorConfiguration      ,
+                                      css::lang::XServiceInfo       )
+
+DEFINE_XSERVICEINFO_MULTISERVICE(GlobalAcceleratorConfiguration                   ,
+                                 ::cppu::OWeakObject                              ,
+                                 SERVICENAME_GLOBALACCELERATORCONFIGURATION       ,
+                                 IMPLEMENTATIONNAME_GLOBALACCELERATORCONFIGURATION)
+
+DEFINE_INIT_SERVICE(GlobalAcceleratorConfiguration,
+                    {
+                        /*Attention
+                        I think we don't need any mutex or lock here ... because we are called by our own static method impl_createInstance()
+                        to create a new instance of this class by our own supported service factory.
+                        see macro DEFINE_XSERVICEINFO_MULTISERVICE and "impl_initService()" for further informations!
+                        */
+                        impl_ts_fillCache();
+                    }
+                   )
+
+//-----------------------------------------------
+GlobalAcceleratorConfiguration::GlobalAcceleratorConfiguration(const css::uno::Reference< css::lang::XMultiServiceFactory > xSMGR)
+    : AcceleratorConfiguration(xSMGR)
+{
+}
+
+//-----------------------------------------------
+GlobalAcceleratorConfiguration::~GlobalAcceleratorConfiguration()
+{
+    m_aPresetHandler.removeStorageListener(this);
+}
+
+//-----------------------------------------------
+void GlobalAcceleratorConfiguration::impl_ts_fillCache()
+{
+    // get current office locale ... but dont cache it.
+    // Otherwise we must be listener on the configuration layer
+    // which seems to superflous for this small implementation .-)
+    ::comphelper::Locale aLocale = impl_ts_getLocale();
+
+    // Note: The used preset class is threadsafe by itself ... and live if we live!
+    // We do not need any mutex here.
+
+    // open the folder, where the configuration exists
+    m_aPresetHandler.connectToResource(
+        PresetHandler::E_GLOBAL,
+        PresetHandler::RESOURCETYPE_ACCELERATOR(),
+        ::rtl::OUString(),
+        css::uno::Reference< css::embed::XStorage >(),
+        aLocale);
+
+    // check if the user already has a current configuration
+    // if not - se the default preset as new current one.
+    // means: copy "share/default.xml" => "user/current.xml"
+    if (!m_aPresetHandler.existsTarget(PresetHandler::TARGET_CURRENT()))
+        m_aPresetHandler.copyPresetToTarget(PresetHandler::PRESET_DEFAULT(), PresetHandler::TARGET_CURRENT());
+
+    AcceleratorConfiguration::reload();
+    m_aPresetHandler.addStorageListener(this);
+}
+
+} // namespace framework
