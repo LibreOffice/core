572fa5bda5ca8783d498acee5c5321fd0fa95f61
Rüdiger Timm <rt@openoffice.org>
2004-09-20 12:35:12 +0000

    INTEGRATION: CWS swmailmerge (1.2.8); FILE MERGED 2004/08/18 14:54:23 os 1.2.8.1: i33130 textencoding now saved (OJ)

--- dbaccess/source/filter/xml/xmlDataSourceInfo.cxx
+++ dbaccess/source/filter/xml/xmlDataSourceInfo.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlDataSourceInfo.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 15:19:37 $
+ *  last change: $Author: rt $ $Date: 2004-09-20 13:35:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -140,7 +140,7 @@ OXMLDataSourceInfo::OXMLDataSourceInfo( ODBFilter& rImport
             case XML_TOK_THOUSAND:
                 aProperty.Name = INFO_THOUSANDSDELIMITER;
                 break;
-            case XML_TOK_FONT_CHARSET:
+            case XML_TOK_ENCODING:
                 aProperty.Name = INFO_CHARSET;
                 break;
         }
