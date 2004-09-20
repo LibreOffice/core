806ef9b7139535e6359cf0254c2a66c58a3319b3
Rüdiger Timm <rt@openoffice.org>
2004-09-20 13:35:24 +0000

    INTEGRATION: CWS jl12 (1.1.2); FILE ADDED 2004/09/06 15:33:48 jl 1.1.2.3: #i33639# bound attribute renamed 2004/08/27 05:28:17 jl 1.1.2.2: #i30973# 2004/08/20 12:35:37 jl 1.1.2.1: #i33102#

---
+++ cli_ure/source/basetypes/makefile.mk
@@ -0,0 +1,123 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2004-09-20 14:35:24 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): _______________________________________
+#
+#
+#
+#*************************************************************************
+
+PRJ = ..$/..
+PRJNAME = cli_ure
+
+# for dummy
+TARGET = basetypes
+
+.INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
+
+.INCLUDE : $(PRJ)$/util$/target.pmk
+.INCLUDE : target.mk
+
+.IF "$(USE_SHELL)"!="4nt"
+ECHOQUOTE='
+.ELSE
+ECHOQUOTE=
+.ENDIF
+
+.IF "$(BUILD_FOR_CLI)" != ""
+
+#!!! Always change version if code has changed. Provide a publisher
+#policy assembly!!!
+ASSEMBLY_VERSION="1.0.0.0"
+
+ASSEMBLY_ATTRIBUTES = $(MISC)$/assembly_ure.cs
+
+ALLTAR : \
+    $(ASSEMBLY_ATTRIBUTES) \
+    $(OUT)$/bin$/cli_basetypes.dll
+
+CSFILES = \
+    uno$/Any.cs			\
+    uno$/BoundAttribute.cs \
+    uno$/ExceptionAttribute.cs	\
+    uno$/ParameterizedTypeAttribute.cs	\
+    uno$/TypeParametersAttribute.cs \
+    uno$/TypeArgumentsAttribute.cs \
+    uno$/OnewayAttribute.cs	\
+    uno$/PolymorphicType.cs \
+    $(ASSEMBLY_ATTRIBUTES)
+
+#	uno$/MethodAtrribute.cs				\
+
+$(ASSEMBLY_ATTRIBUTES) .PHONY :
+    $(GNUCOPY) -p assembly.cs $@
+    +echo $(ECHOQUOTE) \
+    [assembly:System.Reflection.AssemblyVersion( $(ASSEMBLY_VERSION))] \
+    [assembly:System.Reflection.AssemblyKeyFile(@"$(BIN)$/cliuno.snk")]$(ECHOQUOTE) \
+    >> $@
+
+$(OUT)$/bin$/cli_basetypes.dll : $(CSFILES)
+    +$(WRAPCMD) csc $(CSCFLAGS) \
+        -target:library \
+        -out:$@ \
+        -reference:System.dll \
+        $(CSFILES)
+    @echo "If code has changed then provide a policy assembly and change the version!"
+
+.ENDIF
+    
+
