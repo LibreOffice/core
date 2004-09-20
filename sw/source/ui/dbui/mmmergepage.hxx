95e06dd2115633b5d35e3a13275cc7b6641067f1
Rüdiger Timm <rt@openoffice.org>
2004-09-20 12:17:48 +0000

    INTEGRATION: CWS swmailmerge (1.1.2); FILE ADDED 2004/08/10 08:06:54 os 1.1.2.3: #i25107# commit() removed 2004/05/28 12:43:38 os 1.1.2.2: #i25107# edit document impl. 2004/04/08 08:37:01 os 1.1.2.1: #i25107# new mail merge wizard

---
+++ sw/source/ui/dbui/mmmergepage.hxx
@@ -0,0 +1,112 @@
+/*************************************************************************
+ *
+ *  $RCSfile: mmmergepage.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-09-20 13:17:48 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _MAILMERGEMERGEPAGE_HXX
+#define _MAILMERGEMERGEPAGE_HXX
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+#ifndef _SV_EDIT_HXX
+#include <vcl/edit.hxx>
+#endif
+#ifndef _STDCTRL_HXX
+#include <svtools/stdctrl.hxx>
+#endif
+#ifndef _MAILMERGEHELPER_HXX
+#include <mailmergehelper.hxx>
+#endif
+class SwMailMergeWizard;
+/*-- 02.04.2004 09:21:06---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+class SwMailMergeMergePage : public svt::OWizardPage
+{
+    SwBoldFixedInfo m_aHeaderFI;
+
+    FixedInfo       m_aEditFI;
+    PushButton      m_aEditPB;
+
+    FixedLine       m_aFindFL;
+    FixedText       m_aFineFT;
+    Edit            m_aFindED;
+    PushButton      m_aFindPB;
+
+    CheckBox        m_aWholeWordsCB;
+    CheckBox        m_aBackwardsCB;
+    CheckBox        m_aMatchCaseCB;
+
+    SwMailMergeWizard*  m_pWizard;
+
+    DECL_LINK(EditDocumentHdl_Impl, PushButton*);
+    DECL_LINK(FindHdl_Impl, PushButton*);
+public:
+        SwMailMergeMergePage( SwMailMergeWizard* _pParent);
+        ~SwMailMergeMergePage();
+
+};
+
+
+#endif
+
+
