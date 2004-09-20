841537ef2790adef0c3966e63fe096bad8d416c5
Rüdiger Timm <rt@openoffice.org>
2004-09-20 12:18:32 +0000

    INTEGRATION: CWS swmailmerge (1.1.2); FILE ADDED 2004/08/27 09:44:44 os 1.1.2.15: #i25107# Send mail dialog now modal 2004/08/26 13:50:30 os 1.1.2.14: #i25107# QueryClose() has to be connected to the frame 2004/08/18 15:17:57 os 1.1.2.13: #i25107# mail merge leftovers 2004/07/13 08:30:37 os 1.1.2.12: #i25107# changed mail API 2004/07/07 13:13:00 os 1.1.2.11: #i25107# static StopSendMails handler and paused-label added 2004/07/07 11:26:37 os 1.1.2.10: #i25107# content type added to body and attachment 2004/07/06 11:25:40 os 1.1.2.9: #i25107# weekly check-in 2004/07/02 09:15:14 os 1.1.2.8: #i25107# weekly check-in 2004/06/23 14:06:55 os 1.1.2.7: #i25107# weekly check-in 2004/06/17 07:02:30 os 1.1.2.6: #i25107# weekly check-in 2004/05/28 12:44:24 os 1.1.2.5: #i25107# some output functions impl. 2004/05/18 06:22:52 os 1.1.2.4: #i25107# some output types implemented 2004/04/30 14:32:28 os 1.1.2.3: #i25107# intermediate check-in 2004/04/13 15:33:11 os 1.1.2.2: #i25107# mail merge progress 2004/04/08 07:57:42 os 1.1.2.1: #i25107# new mail merge wizard

---
+++ sw/source/ui/dbui/mmoutputpage.hxx
@@ -0,0 +1,297 @@
+/*************************************************************************
+ *
+ *  $RCSfile: mmoutputpage.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-09-20 13:18:32 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _MAILMERGEOUTPUTPAGE_HXX
+#define _MAILMERGEOUTPUTPAGE_HXX
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+#ifndef _STDCTRL_HXX
+#include <svtools/stdctrl.hxx>
+#endif
+#ifndef _SV_COMBOBOX_HXX
+#include <vcl/combobox.hxx>
+#endif
+#ifndef _SV_FIELD_HXX
+#include <vcl/field.hxx>
+#endif
+#ifndef _SV_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
+#ifndef _MAILMERGEHELPER_HXX
+#include <mailmergehelper.hxx>
+#endif
+#ifndef _SFX_OBJSH_HXX
+#include <sfx2/objsh.hxx>
+#endif
+#ifndef _BASEDLGS_HXX
+#include <sfx2/basedlgs.hxx>
+#endif
+#ifndef _SVTABBX_HXX
+#include <svtools/svtabbx.hxx>
+#endif
+#ifndef _HEADBAR_HXX
+#include <svtools/headbar.hxx>
+#endif
+#ifndef _PRGSBAR_HXX
+#include <svtools/prgsbar.hxx>
+#endif
+
+
+class SwMailMergeWizard;
+class SfxPrinter;
+
+namespace com{ namespace sun{ namespace star{
+    namespace mail{
+        class XMailMessage;
+    }
+}}}
+/*-- 02.04.2004 09:21:06---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+class SwMailMergeOutputPage : public svt::OWizardPage
+{
+    SwBoldFixedInfo m_aHeaderFI;
+    FixedInfo       m_aOptionsFI;
+    RadioButton     m_aSaveStartDocRB;
+    RadioButton     m_aSaveMergedDocRB;
+    RadioButton     m_aPrintRB;
+    RadioButton     m_aSendMailRB;
+
+    FixedLine       m_aSeparatorFL;
+
+    PushButton      m_aSaveStartDocPB;
+
+    RadioButton     m_aSaveAsOneRB;
+    RadioButton     m_aSaveIndividualRB;
+    RadioButton     m_aPrintAllRB; //has to be here for tab control reasons
+    RadioButton     m_aSendAllRB;  //has to be here for tab control reasons
+    //this group is used in save and print
+    RadioButton     m_aFromRB;
+    NumericField    m_aFromNF;
+    FixedText       m_aToFT;
+    NumericField    m_aToNF;
+    PushButton      m_aSaveNowPB;
+
+    FixedText       m_aPrinterFT;
+    ListBox         m_aPrinterLB;
+    PushButton      m_aPrinterSettingsPB;
+    PushButton      m_aPrintNowPB;
+
+    FixedText       m_aMailToFT;
+    ListBox         m_aMailToLB;
+    PushButton      m_aCopyToPB;
+    FixedText       m_aSubjectFT;
+    Edit            m_aSubjectED;
+    FixedText       m_aSendAsFT;
+    ListBox         m_aSendAsLB;
+    FixedText       m_aAttachmentFT;
+    Edit            m_aAttachmentED;
+    PushButton      m_aSendAsPB;
+    PushButton      m_aSendDocumentsPB;
+
+    SwMailMergeWizard*  m_pWizard;
+
+
+    //some FixedLine labels
+    String          m_sSaveStartST;
+    String          m_sSaveMergedST;
+    String          m_sPrintST;
+    String          m_sSendMailST;
+
+    //misc strings
+    String          m_sDefaultAttachmentST;
+    String          m_sNoSubjectQueryST;
+    String          m_sNoSubjectST;
+    String          m_sNoAttachmentNameST;
+    String          m_sConfigureMail;
+
+    String          m_sBody;
+
+    long            m_nFromToRBPos;
+    long            m_nFromToFTPos;
+    long            m_nFromToNFPos;
+    long            m_nRBOffset;
+
+    //some dialog data
+    Printer*        m_pTempPrinter;
+    SfxPrinter*     m_pDocumentPrinterCopy;
+    String          m_sCC;
+    String          m_sBCC;
+
+
+    DECL_LINK(OutputTypeHdl_Impl, RadioButton*);
+    DECL_LINK(CopyToHdl_Impl, PushButton*);
+    DECL_LINK(SaveStartHdl_Impl, PushButton* );
+    DECL_LINK(SaveOutputHdl_Impl, PushButton* );
+    DECL_LINK(PrinterChangeHdl_Impl, ListBox* );
+    DECL_LINK(PrintHdl_Impl, PushButton* );
+    DECL_LINK(PrinterSetupHdl_Impl, PushButton* );
+    DECL_LINK(SendTypeHdl_Impl, ListBox*);
+    DECL_LINK(SendAsHdl_Impl, PushButton*);
+    DECL_LINK(SendDocumentsHdl_Impl, PushButton*);
+
+protected:
+        virtual sal_Bool determineNextButtonState();
+public:
+        SwMailMergeOutputPage( SwMailMergeWizard* _pParent);
+        ~SwMailMergeOutputPage();
+
+};
+/*-- 21.05.2004 12:48:50---------------------------------------------------
+
+  -----------------------------------------------------------------------*/
+struct SwMailDescriptor
+{
+    ::rtl::OUString                             sEMail;
+    ::rtl::OUString                             sAttachmentURL;
+    ::rtl::OUString                             sAttachmentName;
+    ::rtl::OUString                             sMimeType;
+    ::rtl::OUString                             sSubject;
+    ::rtl::OUString                             sBodyMimeType;
+    ::rtl::OUString                             sBodyContent;
+
+
+    ::rtl::OUString                             sCC;
+    ::rtl::OUString                             sBCC;
+};
+struct SwSendMailDialog_Impl;
+class MailProgressBar_Impl;
+class SwMailMergeConfigItem;
+//class SwSendMailDialog : public SfxFloatingWindow
+class SwSendMailDialog : public SfxModalDialog
+{
+    FixedLine               m_aStatusFL;
+    FixedText               m_aStatusFT;
+
+    FixedLine               m_aTransferStatusFL;
+    FixedText               m_aTransferStatusFT;
+    FixedInfo               m_PausedFI;
+    ProgressBar             m_aProgressBar;
+    FixedText               m_aErrorStatusFT;
+
+    PushButton              m_aDetailsPB;
+    HeaderBar               m_aStatusHB;
+    SvTabListBox            m_aStatusLB;
+
+    FixedLine               m_aSeparatorFL;
+
+    PushButton              m_aStopPB;
+    PushButton              m_aClosePB;
+
+    String                  m_sMore;
+    String                  m_sLess;
+    String                  m_sContinue;
+    String                  m_sStop;
+    String                  m_sSend;
+    String                  m_sTransferStatus;
+    String                  m_sErrorStatus;
+    String                  m_sSendingTo;
+    String                  m_sCompleted;
+    String                  m_sFailed;
+    String                  m_sTerminateQuery;
+
+    bool                    m_bCancel;
+
+    ImageList               m_aImageList;
+    ImageList               m_aImageListHC;
+
+    SwSendMailDialog_Impl*  m_pImpl;
+    SwMailMergeConfigItem*  m_pConfigItem;
+    sal_Int32               m_nStatusHeight;
+    sal_Int32               m_nSendCount;
+    sal_Int32               m_nErrorCount;
+
+    DECL_LINK( DetailsHdl_Impl, PushButton* );
+    DECL_LINK( StopHdl_Impl, PushButton* );
+    DECL_LINK( CloseHdl_Impl, PushButton* );
+    DECL_STATIC_LINK( SwSendMailDialog, StartSendMails, SwSendMailDialog* );
+    DECL_STATIC_LINK( SwSendMailDialog, StopSendMails, SwSendMailDialog* );
+
+    void        SendMails();
+    void        UpdateTransferStatus();
+public:
+//    SwSendMailDialog(SfxBindings*, SfxChildWindow*, Window *pParent);
+    SwSendMailDialog( Window* pParent, SwMailMergeConfigItem& );
+    ~SwSendMailDialog();
+
+//    virtual void        FillInfo(SfxChildWinInfo&) const;
+
+//    void                SetConfigItem( SwMailMergeConfigItem& rConfigItem )
+//                            {m_pConfigItem = &rConfigItem;}
+
+    void                AddDocument( SwMailDescriptor& rDesc );
+//    void                Start();
+    virtual short       Execute();
+
+    void                DocumentSent( ::com::sun::star::uno::Reference< ::com::sun::star::mail::XMailMessage>,
+                                        bool bResult,
+                                        const ::rtl::OUString* pError );
+    void                AllMailsSent();
+
+//    sal_Bool            QueryTermination();
+};
+#endif
+
+
