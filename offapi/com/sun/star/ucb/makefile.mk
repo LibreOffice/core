3421feb5bd114041c5fe15905481e870160d2a72
Rüdiger Timm <rt@openoffice.org>
2004-09-20 10:50:54 +0000

    INTEGRATION: CWS fwk01ea (1.41.222); FILE MERGED 2004/04/27 15:21:18 abi 1.41.222.2: RESYNC: (1.41-1.42); FILE MERGED 2004/04/27 13:54:52 abi 1.41.222.1: Initial revision GnomeVFS

--- offapi/com/sun/star/ucb/makefile.mk
+++ offapi/com/sun/star/ucb/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.42 $
+#   $Revision: 1.43 $
 #
-#   last change: $Author: hr $ $Date: 2004-04-13 11:58:16 $
+#   last change: $Author: rt $ $Date: 2004-09-20 11:50:54 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -130,6 +130,9 @@ IDLFILES=\
     FTPContent.idl\
     FTPContentProvider.idl\
     GlobalTransferCommandArgument.idl\
+    GnomeVFSContentProvider.idl\
+    GnomeVFSDocumentContent.idl\
+    GnomeVFSFolderContent.idl\
     HandleCookiesRequest.idl\
     HelpContent.idl\
     HelpContentProvider.idl\
