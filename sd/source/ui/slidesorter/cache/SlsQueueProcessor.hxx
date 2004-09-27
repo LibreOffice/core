bcbc09f34ef7d9cede28366f8487596a0142f741
Rüdiger Timm <rt@openoffice.org>
2004-09-27 11:43:52 +0000

    #i10000# Inlude view/SlideSorterView.hxx and svx/svdpagv.hxx

--- sd/source/ui/slidesorter/cache/SlsQueueProcessor.hxx
+++ sd/source/ui/slidesorter/cache/SlsQueueProcessor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SlsQueueProcessor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-09-20 13:33:33 $
+ *  last change: $Author: rt $ $Date: 2004-09-27 12:43:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,9 @@
 #define SD_SLIDESORTER_QUEUE_PROCESSOR_HXX
 
 #include "view/SlsPageObject.hxx"
+#include "view/SlideSorterView.hxx"
 #include "TextLogger.hxx"
+#include <svx/svdpagv.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/timer.hxx>
 
