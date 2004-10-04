4d8622ababdcef3a59106b5156432ac7729a05ff
Kurt Zenker <kz@openoffice.org>
2004-10-04 19:59:31 +0000

    INTEGRATION: CWS mav09 (1.1.1.1.424); FILE MERGED 2004/04/14 11:47:32 mba 1.1.1.1.424.1: #i27773#: remove so3

--- sfx2/source/inc/preview.hxx
+++ sfx2/source/inc/preview.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: preview.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:52:34 $
+ *  last change: $Author: kz $ $Date: 2004-10-04 20:59:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,7 +66,7 @@
 #include "basedlgs.hxx"
 
 class SfxObjectShell;
-class SvStorageRef;
+//REMOVE    class SvStorageRef;
 class SfxFrameWindow;
 
 class SfxFrame;
