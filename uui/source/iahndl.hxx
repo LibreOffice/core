e4b7c1f6d9a3cd7000a84de362c64ffb162e78e8
Kurt Zenker <kz@openoffice.org>
2004-10-04 17:07:47 +0000

    INTEGRATION: CWS mav09 (1.11.114); FILE MERGED 2004/07/01 17:32:21 mav 1.11.114.1: #i27773# handle document error code

--- uui/source/iahndl.hxx
+++ uui/source/iahndl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: iahndl.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-27 17:44:57 $
+ *  last change: $Author: kz $ $Date: 2004-10-04 18:07:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -91,9 +91,6 @@
 #endif
 #ifndef _COM_SUN_STAR_DOCUMENT_AMBIGOUSFILTERREQUEST_HPP_
 #include "com/sun/star/document/AmbigousFilterRequest.hpp"
-#endif
-#ifndef _COM_SUN_STAR_TASK_ERRORCODEREQUEST_HPP_
-#include "com/sun/star/task/ErrorCodeRequest.hpp"
 #endif
 #ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include "com/sun/star/uno/Sequence.hxx"
@@ -333,7 +330,7 @@ private:
 
     void
     handleGenericErrorRequest(
-        com::sun::star::task::ErrorCodeRequest const & rRequest,
+        sal_Int32 nErrorCode,
         com::sun::star::uno::Sequence<
                 com::sun::star::uno::Reference<
                     com::sun::star::task::XInteractionContinuation > > const &
