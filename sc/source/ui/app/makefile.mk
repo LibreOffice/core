fffc778495b1cd642d3178cda2fbd66e3b01074f
Kurt Zenker <kz@openoffice.org>
2004-10-04 19:13:18 +0000

    INTEGRATION: CWS mav09 (1.12.138); FILE MERGED 2004/05/04 14:01:40 mba 1.12.138.1: #i27773#: remove so3

--- sc/source/ui/app/makefile.mk
+++ sc/source/ui/app/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: rt $ $Date: 2003-09-19 08:22:27 $
+#   last change: $Author: kz $ $Date: 2004-10-04 20:13:18 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -101,6 +101,7 @@ SLOFILES =  \
     $(SLO)$/client.obj
 
 EXCEPTIONSFILES= \
+    $(SLO)$/drwtrans.obj \
     $(SLO)$/scmod2.obj \
     $(SLO)$/scmod.obj
 
