2a51627aa19428509e8cc0b2151e4d9ebdb3cadf
Kurt Zenker <kz@openoffice.org>
2004-10-04 19:25:04 +0000

    INTEGRATION: CWS mav09 (1.8.412); FILE MERGED 2004/09/16 23:18:42 mav 1.8.412.3: RESYNC: (1.9-1.11); FILE MERGED 2004/07/09 03:02:51 mav 1.8.412.2: RESYNC: (1.8-1.9); FILE MERGED 2004/05/04 14:02:00 mba 1.8.412.1: #i27773#: remove so3

--- sc/source/ui/view/tabview2.cxx
+++ sc/source/ui/view/tabview2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tabview2.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-09-08 15:55:34 $
+ *  last change: $Author: kz $ $Date: 2004-10-04 20:25:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,6 +74,7 @@
 #include <vcl/timer.hxx>
 #include <vcl/msgbox.hxx>
 #include <sfx2/app.hxx>
+#include <sfx2/viewfrm.hxx>
 #include <sfx2/bindings.hxx>
 #include <sfx2/childwin.hxx>
 
