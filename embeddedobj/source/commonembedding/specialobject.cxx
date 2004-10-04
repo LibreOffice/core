2151fb5f9cf03ab0a9088e63b6fba7968b290e6c
Kurt Zenker <kz@openoffice.org>
2004-10-04 18:50:09 +0000

    INTEGRATION: CWS mav09 (1.1.2); FILE ADDED 2004/07/02 08:48:10 mba 1.1.2.2: #i27773#: more stuff for SpecialObjects 2004/06/18 14:18:26 mba 1.1.2.1: #i27773#: special objects implementation

---
+++ embeddedobj/source/commonembedding/specialobject.cxx
@@ -0,0 +1,285 @@
+/*************************************************************************
+ *
+ *  $RCSfile: specialobject.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-10-04 19:50:09 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef  _COM_SUN_STAR_UI_DIALOGS_XEXECUTABLEDIALOG_HPP_
+#include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_EMBEDSTATES_HPP_
+#include <com/sun/star/embed/EmbedStates.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_EMBEDVERBS_HPP_
+#include <com/sun/star/embed/EmbedVerbs.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_EMBEDUPDATEMODES_HPP_
+#include <com/sun/star/embed/EmbedUpdateModes.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_XEMBEDDEDCLIENT_HPP_
+#include <com/sun/star/embed/XEmbeddedClient.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_XINPLACECLIENT_HPP_
+#include <com/sun/star/embed/XInplaceClient.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_XWINDOWSUPPLIER_HPP_
+#include <com/sun/star/embed/XWindowSupplier.hpp>
+#endif
+#ifndef _COM_SUN_STAR_AWT_XWINDOWPEER_HPP_
+#include <com/sun/star/awt/XWindowPeer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XCLOSEBROADCASTER_HPP_
+#include <com/sun/star/util/XCloseBroadcaster.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XCLOSEABLE_HPP_
+#include <com/sun/star/util/XCloseable.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XMODIFIABLE_HPP_
+#include <com/sun/star/util/XModifiable.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XFRAME_HPP_
+#include <com/sun/star/frame/XFrame.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XCOMPONENTLOADER_HPP_
+#include <com/sun/star/frame/XComponentLoader.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDERINTERCEPTION_HPP_
+#include <com/sun/star/frame/XDispatchProviderInterception.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
+#include <com/sun/star/lang/DisposedException.hpp>
+#endif
+#ifndef _COM_SUN_STAR_EMBED_EMBEDMAPUNITS_HPP_
+#include <com/sun/star/embed/EmbedMapUnits.hpp>
+#endif
+
+#include <cppuhelper/typeprovider.hxx>
+
+#include "specialobject.hxx"
+#include "convert.hxx"
+#include "intercept.hxx"
+
+using namespace ::com::sun::star;
+
+
+OSpecialEmbeddedObject::OSpecialEmbeddedObject( const uno::Reference< lang::XMultiServiceFactory >& xFactory, const uno::Sequence< beans::NamedValue >& aObjectProps )
+: OCommonEmbeddedObject( xFactory, aObjectProps )
+{
+    maSize.Width = maSize.Height = 10000;
+    m_nObjectState = embed::EmbedStates::LOADED;
+}
+
+//------------------------------------------------------
+uno::Any SAL_CALL OSpecialEmbeddedObject::queryInterface( const uno::Type& rType )
+        throw( uno::RuntimeException )
+{
+    uno::Any aReturn;
+
+    aReturn <<= ::cppu::queryInterface( rType,
+                                        static_cast< embed::XEmbeddedObject* >( this ),
+                                        static_cast< embed::XInplaceObject* >( this ),
+                                        static_cast< embed::XVisualObject* >( this ),
+                                        static_cast< embed::XClassifiedObject* >( this ),
+                                        static_cast< embed::XComponentSupplier* >( this ),
+                                        static_cast< util::XCloseable* >( this ),
+                                        static_cast< document::XEventBroadcaster* >( this ) );
+    if ( aReturn.hasValue() )
+        return aReturn;
+    else
+        return ::cppu::OWeakObject::queryInterface( rType ) ;
+
+}
+
+//------------------------------------------------------
+uno::Sequence< uno::Type > SAL_CALL OSpecialEmbeddedObject::getTypes()
+        throw( uno::RuntimeException )
+{
+    static ::cppu::OTypeCollection* pTypeCollection = NULL;
+
+    if ( !pTypeCollection )
+    {
+        ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+        if ( !pTypeCollection )
+        {
+            static ::cppu::OTypeCollection aTypeCollection(
+                                        ::getCppuType( (const uno::Reference< lang::XTypeProvider >*)NULL ),
+                                        ::getCppuType( (const uno::Reference< embed::XEmbeddedObject >*)NULL ),
+                                        ::getCppuType( (const uno::Reference< embed::XInplaceObject >*)NULL ) );
+
+            pTypeCollection = &aTypeCollection ;
+        }
+    }
+
+    return pTypeCollection->getTypes() ;
+
+}
+
+embed::VisualRepresentation SAL_CALL OSpecialEmbeddedObject::getPreferredVisualRepresentation( sal_Int64 nAspect )
+        throw ( lang::IllegalArgumentException,
+                embed::WrongStateException,
+                uno::Exception,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    // TODO: if object is in loaded state it should switch itself to the running state
+    if ( m_nObjectState == -1 || m_nObjectState == embed::EmbedStates::LOADED )
+        throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The own object has no model!\n" ),
+                                    uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
+
+    OSL_ENSURE( m_pDocHolder->GetComponent().is(), "Running or Active object has no component!\n" );
+
+    // TODO: return for the aspect of the document
+    embed::VisualRepresentation aVisualRepresentation;
+    /*
+    uno::Reference< datatransfer::XTransferable > xTransferable( m_pDocHolder->GetComponent(), uno::UNO_QUERY );
+    if ( !xTransferable.is() )
+        throw uno::RuntimeException();
+
+    datatransfer::DataFlavor aDataFlavor(
+            ::rtl::OUString::createFromAscii( "application/x-openoffice-gdimetafile;windows_formatname=\"GDIMetaFile\"" ),
+            ::rtl::OUString::createFromAscii( "GDIMetaFile" ),
+            ::getCppuType( (const uno::Sequence< sal_Int8 >*) NULL ) );
+
+    aVisualRepresentation.Data = xTransferable->getTransferData( aDataFlavor );
+    aVisualRepresentation.Flavor = aDataFlavor;*/
+    return aVisualRepresentation;
+}
+
+void SAL_CALL OSpecialEmbeddedObject::setVisualAreaSize( sal_Int64 nAspect, const awt::Size& aSize )
+        throw ( lang::IllegalArgumentException,
+                embed::WrongStateException,
+                uno::Exception,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    maSize = aSize;
+}
+
+awt::Size SAL_CALL OSpecialEmbeddedObject::getVisualAreaSize( sal_Int64 nAspect )
+        throw ( lang::IllegalArgumentException,
+                embed::WrongStateException,
+                uno::Exception,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    if ( m_nObjectState == -1 )
+        throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The own object has no model!\n" ),
+                                    uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
+    awt::Size aResult;
+    return maSize;
+}
+
+sal_Int32 SAL_CALL OSpecialEmbeddedObject::getMapUnit( sal_Int64 nAspect )
+        throw ( uno::Exception,
+                uno::RuntimeException)
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    return embed::EmbedMapUnits::ONE_100TH_MM;
+}
+
+void SAL_CALL OSpecialEmbeddedObject::changeState( sal_Int32 nNewState )
+        throw ( ::com::sun::star::embed::UnreachableStateException,
+                ::com::sun::star::embed::WrongStateException,
+                ::com::sun::star::uno::Exception,
+                ::com::sun::star::uno::RuntimeException )
+{
+    if ( nNewState == embed::EmbedStates::UI_ACTIVE )
+        nNewState = embed::EmbedStates::INPLACE_ACTIVE;
+    OCommonEmbeddedObject::changeState( nNewState );
+}
+
+void SAL_CALL OSpecialEmbeddedObject::doVerb( sal_Int32 nVerbID )
+        throw ( lang::IllegalArgumentException,
+                embed::WrongStateException,
+                embed::UnreachableStateException,
+                uno::Exception,
+                uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( m_aMutex );
+    if ( m_bDisposed )
+        throw lang::DisposedException(); // TODO
+
+    if ( m_nObjectState == -1 )
+        throw embed::WrongStateException( ::rtl::OUString::createFromAscii( "The object has no persistence!\n" ),
+                                        uno::Reference< uno::XInterface >( reinterpret_cast< ::cppu::OWeakObject* >(this) ) );
+
+    if ( nVerbID == -7 )
+    {
+
+        uno::Reference < ui::dialogs::XExecutableDialog > xDlg( m_pDocHolder->GetComponent(), uno::UNO_QUERY );
+        if ( xDlg.is() )
+            xDlg->execute();
+        else
+            throw embed::UnreachableStateException();
+    }
+    else
+        OCommonEmbeddedObject::doVerb( nVerbID );
+}
+
