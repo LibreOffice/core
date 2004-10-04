7f01427a54b1ba6287035ff754f102798602fb43
Kurt Zenker <kz@openoffice.org>
2004-10-04 17:43:49 +0000

    INTEGRATION: CWS mav09 (1.16.60); FILE MERGED 2004/09/16 18:46:48 mav 1.16.60.5: RESYNC: (1.21-1.22); FILE MERGED 2004/08/09 20:24:40 mav 1.16.60.4: RESYNC: (1.17-1.21); FILE MERGED 2004/04/29 18:18:33 mav 1.16.60.3: RESYNC: (1.16-1.17); FILE MERGED 2004/04/27 17:02:06 mba 1.16.60.2: #i27773#: new embedding API 2004/04/14 14:12:24 mba 1.16.60.1: #i27773#: remove so3; new storage API

--- sd/source/ui/view/drviews4.cxx
+++ sd/source/ui/view/drviews4.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drviews4.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 09:17:55 $
+ *  last change: $Author: kz $ $Date: 2004-10-04 18:43:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -947,7 +947,7 @@ void DrawViewShell::ShowMousePosInfo(const Rectangle& rRect,
 
     // StatusBar Koordinatenanzeige
     OSL_ASSERT (GetViewShell()!=NULL);
-    if ( ! GetViewShell()->GetIPClient())
+    if ( !GetViewShell()->GetUIActiveClient() )
     {
         SfxItemSet aSet(GetPool(), SID_CONTEXT, SID_CONTEXT,
                                    SID_ATTR_POSITION, SID_ATTR_POSITION,
