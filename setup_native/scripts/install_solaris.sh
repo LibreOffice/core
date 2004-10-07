4b325b0f20ce574296c865b4825e584097933bf4
Christof Pintaske <cp@openoffice.org>
2004-10-07 16:22:21 +0000

    #34894# don't delete a directory unconditionally, ask the user to do so

--- setup_native/scripts/install_solaris.sh
+++ setup_native/scripts/install_solaris.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
 
-# First parameter: Path to the packages
-# Second parameter: Path to the local root directory
-# PACKAGE_PATH=/cws/so-cwsserv05/cnpsdk/unxsoli4.pro/StarOffice/install/en-US/packages
-# MY_ROOT=/export/home/is/root
+error()
+{
+    echo
+    printf "ERROR:\t$1\n"
+    echo
+    echo "User Mode Installation script for developer and knowledgeable early access tester"
+    echo
+    echo "This installation method is not intended for use in a production environment!"
+    echo "Using this script is unsupported and completely at your own risk"
+    echo
+    echo "Usage:" $0 "<pkg-source-dir> <inst-destination-dir> [-l]"
+    echo "    <pkg-source-dir>:       directory *only* containing the Solaris pkg packages to be installed"
+    echo "    <inst-destination-dir>: absolute path to where the office and the pkg database will get installed"
+    echo "    -l:                     optional parameter to create a link \"soffice\" in $HOME"
+    echo
+    exit 2
+}
+
+cannot_install()
+{
+    echo
+    printf "ERROR:\tCannot install to directory $MY_ROOT\n"
+    printf "\t$1\n"
+    printf "\tPlease check/cleanup $MY_ROOT or choose a different directory\n"
+    echo
+    exit 2
+}
+
+#
+# expect either two or three arguments
+#
 
 if [ \( $# -ne 2 -a $# -ne 3 \) -o -z "$1" -o -z "$2" ]
 then
-  echo "Two (optional three) parameters required"
-  echo "Usage:"
-  echo "1. parameter: Path to the packages"
-  echo "2. parameter: Path to the local root directory"
-  echo "3. parameter (optional): \"-l\" (Creates a link \"soffice\" in $HOME)"
-  exit 2
+    error "Wrong number of arguments"
 fi
+
+#
+# this script is for userland not for root
+#
+
+if [ $UID -eq 0 ]
+then
+    error "This script is for installation without administrative rights only\n\tPlease use pkgadd to install as root"
+fi
+
+#
+# Evaluate command line arguments
+#
 
 PACKAGE_PATH=$1
 MY_ROOT=$2
@@ -26,33 +61,35 @@ fi
 
 if [ ! -d $PACKAGE_PATH ]
 then
-  echo "$PACKAGE_PATH is not a valid directory"
-  echo "Usage:"
-  echo "1. parameter: Path to the packages"
-  echo "2. parameter: Path to the local root directory"
-  echo "3. parameter (optional): \"-l\" (Creates a link \"soffice\" in $HOME)"
-  exit 2
+    error "Directory $PACKAGE_PATH does not exist"
 fi
 
-cd `dirname $0`
-DIRECTORY=`pwd`
+if [ ! "${MY_ROOT:0:1}" = "/" ]
+then
+    error "Invalid installation directory $MY_ROOT, has to be an absolute path"
+fi
 
+#
+# the admin file is expected to be in the same directory as this script
+#
+
+DIRECTORY=`dirname $0`
 ADMINFILE=$DIRECTORY/admin
-GETUID_SO=/tmp/libgetuid.so.$$
-linenum=???
-tail +$linenum $0 > $GETUID_SO
+if [ ! -r $ADMINFILE ]
+then
+    error "Admin file $ADMINFILE does not exist or is not readable"
+fi
 
-COREPKG=`find $PACKAGE_PATH/* -type d -prune -name "*-core-*" -print`
-PKGLIST=`find $PACKAGE_PATH/* -type d -prune ! -name "*adabas*" ! -name "*j3*" ! -name "*-gnome-*" ! -name "*-cde-*" ! -name "*-core-*" -print`
+#
+# Check and get the list of packages to install
+#
+
+COREPKG=`find $PACKAGE_PATH/* -type d -prune -name "*-core*" -print`
+PKGLIST=`find $PACKAGE_PATH/* -type d -prune ! -name "*adabas*" ! -name "*j3*" ! -name "*-gnome*" ! -name "*-cde*" ! -name "*-core*" -print`
 
 if [ -z "$COREPKG" ]
 then
-  echo "No core package found in $PACKAGE_PATH"
-  echo "Usage:"
-  echo "1. parameter: Path to the packages"
-  echo "2. parameter: Path to the local root directory"
-  echo "3. parameter (optional): \"-l\" \(Creates a link \"soffice\" in $HOME\)"
-  exit 2
+    error "No core package found in directory $PACKAGE_PATH"
 fi
 
 echo "Packages found:"
@@ -60,51 +97,81 @@ for i in $COREPKG $PKGLIST; do
   echo `basename $i`
 done
 
-# Removing old directories
-rm -rf $MY_ROOT/opt
-rm -rf $MY_ROOT/var
-
-# Creating the installation directory (otherwise the user would be asked)
+#
+# Check/Create installation directory
+#
 
 CORENAME=`basename $COREPKG`
-INSTALLDIR=`pkgparam -d $PACKAGE_PATH $CORENAME BASEDIR`
+INSTALLDIR=$MY_ROOT`pkgparam -d $PACKAGE_PATH $CORENAME BASEDIR`
 
-if [ ! -d $MY_ROOT$INSTALLDIR ]
+# We expect that $INSTALLDIR does not exist, however if it is empty we ignore it
+if [ -d $INSTALLDIR ]
 then
-  mkdir -p $MY_ROOT$INSTALLDIR
+    # if it is not empty we cannot rm it (might be a permission problem as well)
+    rmdir $INSTALLDIR
+fi
+if [ -d $INSTALLDIR ]
+then
+    cannot_install "Directory $INSTALLDIR exists and is not empty or cannot be removed"
 fi
 
-#Ausgaben...
-clear
+if [ -d $MY_ROOT/var/sadm ]
+then
+    cannot_install "pkg database does already exist $MY_ROOT/var/sadm"
+fi
+
+# Create the installation directory (otherwise the user would be asked)
+mkdir -p $INSTALLDIR
+if [ ! -d $INSTALLDIR ]
+then
+    cannot_install "Unable to create directory $INSTALLDIR"
+fi
+
+#
+# the tail of the script contains a shared object for overloading the getuid() call
+#
+
+GETUID_SO=/tmp/libgetuid.so.$$
+linenum=???
+tail +$linenum $0 > $GETUID_SO
+if [ ! -r $GETUID_SO ]
+then
+    error "Cannot create getuid wrapper library $GETUID_SO"
+fi
+
+#
+# Perform the installation
+#
+
 echo "####################################################################"
-echo "#     Installation of the StarOffice packages                      #"
+echo "#     Installation of the found packages                           #"
 echo "####################################################################"
 echo
 echo "Path to the packages       : " $PACKAGE_PATH
 echo "Path to the installation   : " $MY_ROOT
 
-LD_PRELOAD=$GETUID_SO
-export LD_PRELOAD
+export LD_PRELOAD=$GETUID_SO
 
 for i in $COREPKG $PKGLIST; do
   echo /usr/sbin/pkgadd -a $ADMINFILE -d $PACKAGE_PATH -R $MY_ROOT `basename $i`
   /usr/sbin/pkgadd -a $ADMINFILE -d $PACKAGE_PATH -R $MY_ROOT `basename $i`
 done
 
-# creating a link into the user home directory
-
-if [ $LINK = "-l" ]
-then
-  echo
-  echo rm -f $HOME/soffice
-  rm -f $HOME/soffice
-  echo ln -s $MY_ROOT$INSTALLDIR/program/soffice $HOME/soffice
-  ln -s $MY_ROOT$INSTALLDIR/program/soffice $HOME/soffice
-fi
-
+export -n LD_PRELOAD
 rm -f $GETUID_SO
 
-#clear
+#
+# Create a link into the users home directory
+#
+
+if [ "$LINK" = "-l" ]
+then
+  echo
+  echo "Creating link from $INSTALLDIR/program/soffice to $HOME/soffice"
+  rm -f $HOME/soffice
+  ln -s $INSTALLDIR/program/soffice $HOME/soffice
+fi
+
 echo
 echo "Installation done ..."
 
