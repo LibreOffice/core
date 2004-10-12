e27a5ac0a9b5ef727daf15c99944a11d369e64a6
Jens-Heiner Rechtien <hr@openoffice.org>
2004-10-12 12:24:05 +0000

    INTEGRATION: CWS sj09 (1.1.2); FILE ADDED 2004/09/16 13:16:55 sj 1.1.2.2: AutoGrowSize has been removed 2004/08/20 11:04:30 sj 1.1.2.1: #i33240# CustomShape integration for Writer and Calc

---
+++ sw/source/ui/ribbar/concustomshape.cxx
@@ -0,0 +1,295 @@
+/*************************************************************************
+ *
+ *  $RCSfile: concustomshape.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-10-12 13:24:05 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+
+#pragma hdrstop
+
+
+#ifndef _SFX_BINDINGS_HXX //autogen
+#include <sfx2/bindings.hxx>
+#endif
+#ifndef _SVX_HTMLMODE_HXX //autogen
+#include <svx/htmlmode.hxx>
+#endif
+#ifndef sdtacitm_hxx //autogen
+#include <svx/sdtacitm.hxx>
+#endif
+#ifndef _SVDOBJ_HXX //autogen
+#include <svx/svdobj.hxx>
+#endif
+#ifndef _SDTAGITM_HXX //autogen
+#include <svx/sdtagitm.hxx>
+#endif
+#ifndef SDTAKITM_HXX //autogen
+#include <svx/sdtakitm.hxx>
+#endif
+#ifndef _SDTADITM_HXX //autogen
+#include <svx/sdtaditm.hxx>
+#endif
+#ifndef _SDTAAITM_HXX //autogen
+#include <svx/sdtaaitm.hxx>
+#endif
+#ifndef _SVDVIEW_HXX //autogen
+#include <svx/svdview.hxx>
+#endif
+#ifndef _SFXVIEWFRM_HXX
+#include <sfx2/viewfrm.hxx>
+#endif
+#ifndef _SVDCAPT_HXX
+#include <svx/svdocapt.hxx>
+#endif
+#ifndef _OUTLOBJ_HXX
+#include <svx/outlobj.hxx>
+#endif
+
+#ifndef _CMDID_H
+#include <cmdid.h>
+#endif
+#ifndef _VIEW_HXX
+#include <view.hxx>
+#endif
+#ifndef _EDTWIN_HXX
+#include <edtwin.hxx>
+#endif
+#ifndef _WRTSH_HXX
+#include <wrtsh.hxx>
+#endif
+#ifndef _VIEWOPT_HXX
+#include <viewopt.hxx>
+#endif
+#ifndef _DRAWBASE_HXX
+#include <drawbase.hxx>
+#endif
+#ifndef _SW_CONCUSTOMSHAPE_HXX
+#include <concustomshape.hxx>
+#endif
+#ifndef _GALLERY_HXX_
+#include <svx/gallery.hxx>
+#endif
+#ifndef _SFXREQUEST_HXX //autogen
+#include <sfx2/request.hxx>
+#endif
+#ifndef _FM_FMMODEL_HXX
+#include <svx/fmmodel.hxx>
+#endif
+#ifndef _SFXITEMPOOL_HXX
+#include <svtools/itempool.hxx>
+#endif
+#ifndef _SVDPAGE_HXX
+#include <svx/svdpage.hxx>
+#endif
+#ifndef _SVDOASHP_HXX
+#include <svx/svdoashp.hxx>
+#endif
+#ifndef _SDTAGITM_HXX
+#include <svx/sdtagitm.hxx>
+#endif
+
+
+/*************************************************************************
+|*
+|* C'Tor
+|*
+\************************************************************************/
+ConstCustomShape::ConstCustomShape( SwWrtShell* pWrtShell, SwEditWin* pEditWin, SwView* pSwView, SfxRequest& rReq )
+    : SwDrawBase( pWrtShell, pEditWin, pSwView )
+{
+    const SfxItemSet* pArgs = rReq.GetArgs();
+    if ( pArgs )
+    {
+        const SfxStringItem& rItm = (const SfxStringItem&)pArgs->Get( rReq.GetSlot() );
+        aCustomShape = rItm.GetValue();
+    }
+}
+
+/*************************************************************************
+|*
+|* MouseButtonDown-event
+|*
+\************************************************************************/
+
+BOOL ConstCustomShape::MouseButtonDown(const MouseEvent& rMEvt)
+{
+    BOOL bReturn = SwDrawBase::MouseButtonDown(rMEvt);
+    if ( bReturn )
+    {
+        SdrView *pSdrView = pSh->GetDrawView();
+        if ( pSdrView )
+        {
+            SdrObject* pObj = pSdrView->GetCreateObj();
+            if ( pObj )
+            {
+                SetAttributes( pObj );
+                sal_Bool bForceFillStyle = sal_True;
+                sal_Bool bForceNoFillStyle = sal_False;
+                if ( ((SdrObjCustomShape*)pObj)->UseNoFillStyle() )
+                {
+                    bForceFillStyle = sal_False;
+                    bForceNoFillStyle = sal_True;
+                }
+
+                SfxItemSet aAttr( pView->GetPool() );
+                if ( bForceNoFillStyle )
+                    aAttr.Put( XFillStyleItem( XFILL_NONE ) );
+                pObj->SetMergedItemSet(aAttr);
+            }
+        }
+    }
+    return bReturn;
+}
+
+/*************************************************************************
+|*
+|* MouseButtonUp-event
+|*
+\************************************************************************/
+
+BOOL ConstCustomShape::MouseButtonUp(const MouseEvent& rMEvt)
+{
+    return SwDrawBase::MouseButtonUp(rMEvt);
+}
+
+/*************************************************************************
+|*
+|* activate function
+|*
+\************************************************************************/
+
+void ConstCustomShape::Activate(const USHORT nSlotId)
+{
+    pWin->SetDrawMode( OBJ_CUSTOMSHAPE );
+
+    SwDrawBase::Activate(nSlotId);
+}
+
+/*************************************************************************
+|*
+|* applying attributes
+|*
+\************************************************************************/
+
+void ConstCustomShape::SetAttributes( SdrObject* pObj )
+{
+    sal_Bool bAttributesAppliedFromGallery = sal_False;
+
+    SdrCustomShapeGeometryItem aGeometryItem( (SdrCustomShapeGeometryItem&)pObj->GetMergedItem( SDRATTR_CUSTOMSHAPE_GEOMETRY ) );
+    if ( GalleryExplorer::GetSdrObjCount( GALLERY_THEME_POWERPOINT ) )
+    {
+        std::vector< rtl::OUString > aObjList;
+        if ( GalleryExplorer::FillObjListTitle( GALLERY_THEME_POWERPOINT, aObjList ) )
+        {
+            sal_uInt16 i;
+            for ( i = 0; i < aObjList.size(); i++ )
+            {
+                if ( aObjList[ i ].equalsIgnoreAsciiCase( aCustomShape ) )
+                {
+                    FmFormModel aFormModel;
+                    SfxItemPool& rPool = aFormModel.GetItemPool();
+                    rPool.FreezeIdRanges();
+                    if ( GalleryExplorer::GetSdrObj( GALLERY_THEME_POWERPOINT, i, &aFormModel ) )
+                    {
+                        const SdrObject* pSourceObj = aFormModel.GetPage( 0 )->GetObj( 0 );
+                        if( pSourceObj )
+                        {
+                            const SfxItemSet& rSource = pSourceObj->GetMergedItemSet();
+                            SfxItemSet aDest( pObj->GetModel()->GetItemPool(),              // ranges from SdrAttrObj
+                            SDRATTR_START, SDRATTRSET_SHADOW,
+                            SDRATTRSET_OUTLINER, SDRATTRSET_MISC,
+                            SDRATTR_TEXTDIRECTION, SDRATTR_TEXTDIRECTION,
+                            // Graphic Attributes
+                            SDRATTR_GRAF_FIRST, SDRATTRSET_GRAF,
+                            // 3d Properties
+                            SDRATTR_3D_FIRST, SDRATTR_3D_LAST,
+                            // CustomShape properties
+                            SDRATTR_CUSTOMSHAPE_FIRST, SDRATTR_CUSTOMSHAPE_LAST,
+                            // range from SdrTextObj
+                            EE_ITEMS_START, EE_ITEMS_END,
+                            // end
+                            0, 0);
+                            aDest.Set( rSource );
+                            pObj->SetMergedItemSet( aDest );
+                            sal_Int32 nAngle = pSourceObj->GetRotateAngle();
+                            if ( nAngle )
+                            {
+                                double a = nAngle * F_PI18000;
+                                pObj->NbcRotate( pObj->GetSnapRect().Center(), nAngle, sin( a ), cos( a ) );
+                            }
+                            bAttributesAppliedFromGallery = sal_True;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    if ( !bAttributesAppliedFromGallery )
+    {
+        const rtl::OUString sPredefinedType( RTL_CONSTASCII_USTRINGPARAM ( "PredefinedType" ) );
+        com::sun::star::beans::PropertyValue aPropVal;
+        aPropVal.Name = sPredefinedType;
+        aPropVal.Value <<= aCustomShape;
+        aGeometryItem.SetPropertyValue( aPropVal );
+        pObj->SetMergedItem( aGeometryItem );
+        pObj->SetMergedItem( SdrTextAutoGrowHeightItem( sal_False ) );
+    }
+}
+
+
