d14f60b7837a030aac4f38b6941964e84b465333
Jens-Heiner Rechtien <hr@openoffice.org>
2004-10-12 13:12:59 +0000

    INTEGRATION: CWS sj09 (1.1.2); FILE ADDED 2004/09/16 18:57:09 sj 1.1.2.5: fixed shape names for toolbar 2004/07/21 18:07:04 sj 1.1.2.4: controller is now generic and able to handle each customshape slot 2004/07/19 18:23:22 sj 1.1.2.3: changed classname 2004/07/19 18:08:38 sj 1.1.2.2: added toolbar controller for basic shapes 2004/05/10 17:15:48 sj 1.1.2.1: added customshape ui

---
+++ svx/source/customshapes/tbxcustomshapes.cxx
@@ -0,0 +1,231 @@
+/*************************************************************************
+ *
+ *  $RCSfile: tbxcustomshapes.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-10-12 14:12:59 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <string> // HACK: prevent conflict between STLPORT and Workshop headers
+#ifndef _SVX_SVXIDS_HRC
+#include <svxids.hrc>
+#endif
+#ifndef _SHL_HXX
+#include <tools/shl.hxx>
+#endif
+#include <svtools/stritem.hxx>
+#ifndef _SFXDISPATCH_HXX //autogen
+#include <sfx2/dispatch.hxx>
+#endif
+#ifndef _SFXVIEWSH_HXX
+#include <sfx2/viewsh.hxx>
+#endif
+#include <sfx2/imagemgr.hxx>
+#include <sfx2/viewfrm.hxx>
+#include <vcl/toolbox.hxx>
+
+#pragma hdrstop
+
+#ifndef _SVX_TBXCUSTOMSHAPES_HXX
+#include "tbxcustomshapes.hxx"
+#endif
+
+SFX_IMPL_TOOLBOX_CONTROL(SvxTbxCtlCustomShapes, SfxStringItem);
+
+/*************************************************************************
+|*
+|*
+|*
+\************************************************************************/
+
+SvxTbxCtlCustomShapes::SvxTbxCtlCustomShapes( USHORT nSlotId, USHORT nId, ToolBox& rTbx ) :
+    aSubTbxResName( RTL_CONSTASCII_USTRINGPARAM( "private:resource/toolbar/" ) ),
+    SfxToolBoxControl( nSlotId, nId, rTbx ),
+    nSlotId( nSlotId )
+{
+    const rtl::OUString sBasicShape     ( RTL_CONSTASCII_USTRINGPARAM( "diamond" ) ) ;
+    const rtl::OUString sSymbolShape    ( RTL_CONSTASCII_USTRINGPARAM( "smiley" ) ) ;
+    const rtl::OUString sArrowShape     ( RTL_CONSTASCII_USTRINGPARAM( "left-right-arrow" ) ) ;
+    const rtl::OUString sFlowChartShape ( RTL_CONSTASCII_USTRINGPARAM( "flowchart-internal-storage" ) ) ;
+    const rtl::OUString sCalloutShape   ( RTL_CONSTASCII_USTRINGPARAM( "round-rectangular-callout" ) ) ;
+    const rtl::OUString sStarShape      ( RTL_CONSTASCII_USTRINGPARAM( "star5" ) ) ;
+
+    switch( nSlotId )
+    {
+        default :
+        {
+            DBG_ASSERT( false, "SvxTbxCtlCustomShapes: unknown slot executed. ?" );
+        }
+        case SID_DRAWTBX_CS_BASIC :
+        {
+            aLastAction = sBasicShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "basicshapes" ) );
+        }
+        break;
+
+        case SID_DRAWTBX_CS_SYMBOL :
+        {
+            aLastAction = sSymbolShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "symbolshapes" ) );
+        }
+        break;
+
+        case SID_DRAWTBX_CS_ARROW :
+        {
+            aLastAction = sArrowShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "arrowshapes" ) );
+        }
+        break;
+        case SID_DRAWTBX_CS_FLOWCHART :
+        {
+            aLastAction = sFlowChartShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "flowchartshapes" ) );
+        }
+        break;
+        case SID_DRAWTBX_CS_CALLOUT :
+        {
+            aLastAction = sCalloutShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "calloutshapes" ) );
+        }
+        break;
+        case SID_DRAWTBX_CS_STAR :
+        {
+            aLastAction = sStarShape;
+            aSubTbxResName += rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "starshapes" ) );
+        }
+        break;
+    }
+    rTbx.SetItemBits( nId, TIB_DROPDOWN | rTbx.GetItemBits( nId ) );
+    rTbx.Invalidate();
+}
+
+/*************************************************************************
+|*
+|* Benachrichtigung, wenn sich der Applikationsstatus geaendert hat
+|*
+\************************************************************************/
+
+void SvxTbxCtlCustomShapes::StateChanged( USHORT nSID, SfxItemState eState,
+                                  const SfxPoolItem* pState )
+{
+/*
+    GetToolBox().EnableItem( GetId(), ( eState != SFX_ITEM_DISABLED ) );
+    if ( SFX_ITEM_AVAILABLE == eState )
+    {
+        USHORT nTemp = ( (SfxEnumItem*)pState )->GetValue();
+
+        if( GetSlotId() == SID_INSERT_DRAW && nTemp != USHRT_MAX )
+        {
+            // Check whether we are in high contrast mode or not!
+            BOOL bHiContrast = GetToolBox().GetBackground().GetColor().IsDark();
+
+            nLastAction = nTemp;
+
+            USHORT nImage = nLastAction ? nLastAction : GetSlotId();
+            rtl::OUString aSlotURL( RTL_CONSTASCII_USTRINGPARAM( "slot:" ));
+            aSlotURL += rtl::OUString::valueOf( sal_Int32( nImage ));
+            Image aImage = GetImage( m_xFrame,
+                                     aSlotURL,
+                                     hasBigImages(),
+                                     GetToolBox().GetDisplayBackground().GetColor().IsDark() );
+
+//            SfxViewFrame* pFrame = GetBindings().GetDispatcher()->GetFrame();
+//            Image aImage = pFrame->GetImageManager()->GetImage( nImage, bHiContrast );
+            GetToolBox().SetItemImage( GetId(), aImage );
+        }
+    }
+*/
+    SfxToolBoxControl::StateChanged( nSID, eState, pState );
+}
+
+/*************************************************************************
+|*
+|* Wenn man ein PopupWindow erzeugen will
+|*
+\************************************************************************/
+
+SfxPopupWindowType SvxTbxCtlCustomShapes::GetPopupWindowType() const
+{
+    return( aLastAction.getLength() == 0 ? SFX_POPUPWINDOW_ONCLICK : SFX_POPUPWINDOW_ONTIMEOUT);
+}
+
+/*************************************************************************
+|*
+|* Hier wird das Fenster erzeugt
+|* Lage der Toolbox mit GetToolBox() abfragbar
+|* rItemRect sind die Screen-Koordinaten
+|*
+\************************************************************************/
+
+SfxPopupWindow* SvxTbxCtlCustomShapes::CreatePopupWindow()
+{
+    createAndPositionSubToolBar( aSubTbxResName );
+    return NULL;
+}
+
+// -----------------------------------------------------------------------
+
+void SvxTbxCtlCustomShapes::Select( BOOL bMod1 )
+{
+     if ( aLastAction.getLength() > 0 )
+    {
+        com::sun::star::uno::Sequence< com::sun::star::beans::PropertyValue > aParamSeq( 1 );
+        aParamSeq[ 0 ].Name = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "CustomShape" ) );
+        aParamSeq[ 0 ].Value <<= aLastAction;
+        Dispatch( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( ".uno:CustomShape" ) ), aParamSeq );
+    }
+}
+
