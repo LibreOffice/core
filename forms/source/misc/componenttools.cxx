02a40f35c7fc8edf1f05a360a1f2af2cfa1da82e
Pascal Junck <pjunck@openoffice.org>
2004-10-22 10:41:12 +0000

    INTEGRATION: CWS dba17 (1.1.2); FILE ADDED 2004/09/09 12:12:45 fs 1.1.2.1: helper

---
+++ forms/source/misc/componenttools.cxx
@@ -0,0 +1,143 @@
+/*************************************************************************
+ *
+ *  $RCSfile: componenttools.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: pjunck $ $Date: 2004-10-22 11:41:12 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef FORMS_SOURCE_INC_COMPONENTTOOLS_HXX
+#include "componenttools.hxx"
+#endif
+
+/** === begin UNO includes === **/
+/** === end UNO includes === **/
+
+#include <algorithm>
+#include <iterator>
+
+//........................................................................
+namespace frm
+{
+//........................................................................
+
+    //====================================================================
+    //= TypeBag
+    //====================================================================
+    //--------------------------------------------------------------------
+    TypeBag::TypeBag( const TypeSequence& _rTypes1 )
+    {
+        addTypes( _rTypes1 );
+    }
+
+    //--------------------------------------------------------------------
+    TypeBag::TypeBag( const TypeSequence& _rTypes1, const TypeSequence& _rTypes2 )
+    {
+        addTypes( _rTypes1 );
+        addTypes( _rTypes2 );
+    }
+
+    //--------------------------------------------------------------------
+    TypeBag::TypeBag( const TypeSequence& _rTypes1, const TypeSequence& _rTypes2, const TypeSequence& _rTypes3 )
+    {
+        addTypes( _rTypes1 );
+        addTypes( _rTypes2 );
+        addTypes( _rTypes3 );
+    }
+
+    //--------------------------------------------------------------------
+    TypeBag::TypeBag( const TypeSequence& _rTypes1, const TypeSequence& _rTypes2, const TypeSequence& _rTypes3,
+        const TypeSequence& _rTypes4 )
+    {
+        addTypes( _rTypes1 );
+        addTypes( _rTypes2 );
+        addTypes( _rTypes3 );
+        addTypes( _rTypes4 );
+    }
+
+    //--------------------------------------------------------------------
+    TypeBag::TypeBag( const TypeSequence& _rTypes1, const TypeSequence& _rTypes2, const TypeSequence& _rTypes3,
+        const TypeSequence& _rTypes4, const TypeSequence& _rTypes5 )
+    {
+        addTypes( _rTypes1 );
+        addTypes( _rTypes2 );
+        addTypes( _rTypes3 );
+        addTypes( _rTypes4 );
+        addTypes( _rTypes5 );
+    }
+
+    //--------------------------------------------------------------------
+    void TypeBag::addTypes( const TypeSequence& _rTypes )
+    {
+        ::std::copy(
+            _rTypes.getConstArray(),
+            _rTypes.getConstArray() + _rTypes.getLength(),
+            ::std::insert_iterator< TypeSet >( m_aTypes, m_aTypes.begin() )
+        );
+    }
+
+    //--------------------------------------------------------------------
+    TypeBag::TypeSequence TypeBag::getTypes() const
+    {
+        TypeSequence aTypes( m_aTypes.size() );
+        ::std::copy( m_aTypes.begin(), m_aTypes.end(), aTypes.getArray() );
+        return aTypes;
+    }
+
+//........................................................................
+} // namespace frm
+//........................................................................
+
