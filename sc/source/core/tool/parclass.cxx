782a30b19f45bf3c34b8e78e1398ce876e3515f9
Rüdiger Timm <rt@openoffice.org>
2004-10-22 06:59:41 +0000

    INTEGRATION: CWS calc23 (1.2.220); FILE MERGED 2004/09/21 21:19:33 er 1.2.220.1: #115823# parameter classification for ocCell

--- sc/source/core/tool/parclass.cxx
+++ sc/source/core/tool/parclass.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: parclass.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $  $Date: 2004-03-08 11:49:13 $
+ *  last change: $Author: rt $  $Date: 2004-10-22 07:59:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,6 +112,7 @@ const ScParameterClassification::RawData ScParameterClassification::pRawData[] =
     { ocAveDev,          {{ Reference                              },   true }},
     { ocAverage,         {{ Reference                              },   true }},
     { ocAverageA,        {{ Reference                              },   true }},
+    { ocCell,            {{ Value, Reference                       }}},
     { ocColumn,          {{ Reference                              }}},
     { ocColumns,         {{ Reference                              },   true }},
     { ocCorrel,          {{ ForceArray, ForceArray                 }}},
