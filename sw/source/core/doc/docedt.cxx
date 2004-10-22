f3e736fa17745bd03bac88fae515238e3d9ea7d3
Rüdiger Timm <rt@openoffice.org>
2004-10-22 07:11:03 +0000

    INTEGRATION: CWS swqbugfixes07 (1.17.182); FILE MERGED 2004/10/12 14:22:13 fme 1.17.182.3: #i30314# Minor corrections for the convert-tab-stops-into-numbering-indent feature 2004/10/11 10:29:09 fme 1.17.182.2: RESYNC: (1.17-1.19); FILE MERGED 2004/09/20 10:16:35 hbrinkm 1.17.182.1: #i30314# RemoveLeadingChars

--- sw/source/core/doc/docedt.cxx
+++ sw/source/core/doc/docedt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: docedt.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2004-09-17 14:47:44 $
+ *  last change: $Author: rt $ $Date: 2004-10-22 08:11:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -2469,3 +2469,25 @@ void SwDoc::CountWords( const SwPaM& rPaM, SwDocStat& rStat ) const
         pTNd->CountWords( rStat, nSttCnt, nEndCnt );
 }
 
+void SwDoc::RemoveLeadingChars(const SwPosition & rPos, sal_Unicode sChar)
+{
+    const SwTxtNode* pTNd = rPos.nNode.GetNode().GetTxtNode();
+    if ( pTNd )
+    {
+        const String& rTxt = pTNd->GetTxt();
+        xub_StrLen nIdx = 0;
+        while( nIdx < rTxt.Len() && sChar == rTxt.GetChar( nIdx ) )
+        {
+            ++nIdx;
+        }
+
+        if ( nIdx > 0 )
+        {
+            SwPaM aPam(rPos);
+            aPam.GetPoint()->nContent = 0;
+            aPam.SetMark();
+            aPam.GetMark()->nContent = nIdx;
+            Delete( aPam );
+        }
+    }
+}
