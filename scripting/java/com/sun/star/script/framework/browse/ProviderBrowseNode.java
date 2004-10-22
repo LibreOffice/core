0b51201d106d3f0012da3f036351f3877cd82b31
Rüdiger Timm <rt@openoffice.org>
2004-10-22 12:56:11 +0000

    INTEGRATION: CWS scriptingf6 (1.4.2); FILE MERGED 2004/08/03 14:44:53 dfoster 1.4.2.1: #i32502#

--- scripting/java/com/sun/star/script/framework/browse/ProviderBrowseNode.java
+++ scripting/java/com/sun/star/script/framework/browse/ProviderBrowseNode.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ProviderBrowseNode.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2004-07-23 13:56:06 $
+ *  last change: $Author: rt $ $Date: 2004-10-22 13:56:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -76,8 +76,8 @@ import com.sun.star.ucb.XSimpleFileAccess;
 import com.sun.star.beans.XIntrospectionAccess;
 import com.sun.star.script.XInvocation;
 
-import drafts.com.sun.star.script.browse.XBrowseNode;
-import drafts.com.sun.star.script.browse.BrowseNodeTypes;
+import com.sun.star.script.browse.XBrowseNode;
+import com.sun.star.script.browse.BrowseNodeTypes;
 
 import com.sun.star.script.framework.provider.ScriptProvider;
 import com.sun.star.script.framework.log.*;
