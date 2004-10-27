5e3b1610fb573c196ad67ac603905176b449dcf2
Pascal Junck <pjunck@openoffice.org>
2004-10-27 12:29:33 +0000

    INTEGRATION: CWS dbwizard1 (1.2.34); FILE MERGED 2004/10/01 12:38:57 bc 1.2.34.2: ## several changes in dbwizards 2004/08/13 15:00:39 bc 1.2.34.1: #111602# several changes for Formwizard and TableWizard

--- wizards/com/sun/star/wizards/common/SystemDialog.java
+++ wizards/com/sun/star/wizards/common/SystemDialog.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SystemDialog.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $  $Date: 2004-09-08 14:01:57 $
+ *  last change: $Author: pjunck $  $Date: 2004-10-27 13:29:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -367,7 +367,7 @@ public class SystemDialog {
             iMessage = xMsgbox.execute();
         } catch (Exception e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            e.printStackTrace(System.out);
         }
         return iMessage;
     }
