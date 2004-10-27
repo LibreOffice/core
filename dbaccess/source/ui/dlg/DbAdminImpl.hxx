fcca49353d72443b4edc689f5948aa756a6ce819
Pascal Junck <pjunck@openoffice.org>
2004-10-27 11:59:51 +0000

    INTEGRATION: CWS dbwizard1 (1.2.6); FILE MERGED 2004/08/16 10:50:08 bc 1.2.6.1: #i20313#Classes for Datasource wizard checked in

--- dbaccess/source/ui/dlg/DbAdminImpl.hxx
+++ dbaccess/source/ui/dlg/DbAdminImpl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DbAdminImpl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 15:41:02 $
+ *  last change: $Author: pjunck $ $Date: 2004-10-27 12:59:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -161,6 +161,10 @@ namespace dbaui
         /** returns the data source the dialog is currently working with
         */
         ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >   getCurrentDataSource();
+
+        // returns the Url of a database document
+        String              getDocumentUrl(SfxItemSet& _rDest);
+
 
 
         void setCurrentDataSourceName(const ::com::sun::star::uno::Any& _aDataSourceName);
