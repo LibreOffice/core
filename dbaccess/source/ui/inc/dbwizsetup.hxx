0b65babf5f6077c6f253af7a4a82a14ba0d2bdf0
Pascal Junck <pjunck@openoffice.org>
2004-10-27 12:06:19 +0000

    INTEGRATION: CWS dbwizard1 (1.1.2); FILE ADDED 2004/09/14 22:52:10 bc 1.1.2.2: ## several changes 2004/08/16 10:54:47 bc 1.1.2.1: #i20313#Classes for Datasource wizard checked in

---
+++ dbaccess/source/ui/inc/dbwizsetup.hxx
@@ -0,0 +1,228 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dbwizsetup.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: pjunck $ $Date: 2004-10-27 13:06:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef DBAUI_DBWIZ2_HXX
+#define DBAUI_DBWIZ2_HXX
+
+#ifndef _SFXTABDLG_HXX
+#include <sfx2/tabdlg.hxx>
+#endif
+#ifndef _DBAUI_DSNTYPES_HXX_
+#include "dsntypes.hxx"
+#endif
+#ifndef DBAUI_ITEMSETHELPER_HXX
+#include "IItemSetHelper.hxx"
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+#ifndef _DBAUI_MODULE_DBU_HXX_
+#include "moduledbu.hxx"
+#endif
+#include <memory>
+#ifndef SVTOOLS_INC_ROADMAPWIZARD_HXX
+#include <svtools/roadmapwizard.hxx>
+#endif
+#ifndef _CONNECTIVITY_DBTOOLS_HXX_
+#include <connectivity/dbtools.hxx>
+#endif
+
+
+FORWARD_DECLARE_INTERFACE(beans,XPropertySet)
+FORWARD_DECLARE_INTERFACE(sdbc,XConnection)
+FORWARD_DECLARE_INTERFACE(lang,XMultiServiceFactory)
+
+//.........................................................................
+namespace dbaui
+{
+//.........................................................................
+
+class ODsnTypeCollection;
+class OGenericAdministrationPage;
+
+//=========================================================================
+//= ODbTypeWizDialogSetup
+//=========================================================================
+class OGeneralPage;
+struct OPageSettings;
+class ODbDataSourceAdministrationHelper;
+/** tab dialog for administrating the office wide registered data sources
+*/
+class OMySQLIntroPageSetup;
+
+class ODbTypeWizDialogSetup : public svt::RoadmapWizard , public IItemSetHelper, public IAdminHelper,public dbaui::OModuleClient
+{
+
+private:
+    ::std::auto_ptr<ODbDataSourceAdministrationHelper>  m_pImpl;
+    SfxItemSet*             m_pOutSet;
+    DATASOURCE_TYPE         m_eType;
+    DATASOURCE_TYPE         m_eOldType;
+    sal_Bool                m_bResetting : 1;   /// sal_True while we're resetting the pages
+    sal_Bool                m_bApplied : 1;     /// sal_True if any changes have been applied while the dialog was executing
+    sal_Bool                m_bUIEnabled : 1;   /// <TRUE/> if the UI is enabled, false otherwise. Cannot be switched back to <TRUE/>, once it is <FALSE/>
+    sal_Bool                m_bIsConnectable : 1;
+    String                  m_sRM_IntroText;
+    String                  m_sRM_dBaseText;
+    String                  m_sRM_TextText;
+    String                  m_sRM_MSAccessText;
+    String                  m_sRM_LDAPText;
+    String                  m_sRM_ADABASText;
+    String                  m_sRM_ADOText;
+    String                  m_sRM_JDBCText;
+    String                  m_sRM_OracleText;
+    String                  m_sRM_MySQLText;
+    String                  m_sRM_ODBCText;
+    String                  m_sRM_SpreadSheetText;
+    String                  m_sRM_AuthentificationText;
+    String                  m_sRM_FinalText;
+//    String                  m_sWizardTitle;
+    INetURLObject           m_aDocURL;
+    String                  m_sWorkPath;
+    OGeneralPage*           m_pGeneralPage;
+    OMySQLIntroPageSetup*   m_pMySQLIntroPage;
+    ODsnTypeCollection*     m_pCollection;  /// the DSN type collection instance
+
+
+
+public:
+    /** ctor. The itemset given should have been created by <method>createItemSet</method> and should be destroyed
+        after the dialog has been destroyed
+    */
+    ODbTypeWizDialogSetup(Window* pParent
+        ,SfxItemSet* _pItems
+        ,const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB
+        ,const ::com::sun::star::uno::Any& _aDataSourceName
+        );
+    virtual ~ODbTypeWizDialogSetup();
+
+    virtual const SfxItemSet* getOutputSet() const;
+    virtual SfxItemSet* getWriteOutputSet();
+
+    // forwards to ODbDataSourceAdministrationHelper
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > getORB();
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection > createConnection();
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDriver > getDriver();
+    virtual DATASOURCE_TYPE     getDatasourceType(const SfxItemSet& _rSet) const;
+    virtual void clearPassword();
+    virtual void setTitle(const ::rtl::OUString& _sTitle);
+    virtual sal_Bool saveDatasource();
+    virtual String  getStateDisplayName( WizardState _nState );
+    virtual short Execute();
+
+protected:
+    /// to override to create new pages
+    virtual TabPage*    createPage(WizardState _nState);
+//  virtual ::svt::WizardTypes::WizardState determineNextState(::svt::WizardTypes::WizardState _nCurrentState);
+    virtual sal_Bool    leaveState(WizardState _nState);
+    virtual void enterState(WizardState _nState);
+    virtual ::svt::IWizardPage* getWizardPage(TabPage* _pCurrentPage) const;
+    virtual sal_Bool onFinish(sal_Int32 _nResult);
+
+protected:
+    inline sal_Bool isUIEnabled() const { return m_bUIEnabled; }
+    inline void     disabledUI() { m_bUIEnabled = sal_False; }
+
+    /// select a datasource with a given name, adjust the item set accordingly, and everything like that ..
+    void implSelectDatasource(const ::rtl::OUString& _rRegisteredName);
+    void resetPages(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _rxDatasource);
+
+    enum ApplyResult
+    {
+        AR_LEAVE_MODIFIED,      // somthing was modified and has successfully been committed
+        AR_LEAVE_UNCHANGED,     // no changes were made
+        AR_KEEP                 // don't leave the page (e.g. because an error occured)
+    };
+    /** apply all changes made
+    */
+    ApplyResult implApplyChanges();
+
+private:
+    sal_Bool StartTableWizard(); //const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB
+    void RegisterDataSourceByLocation(::rtl::OUString sPath);
+    void OpenDatabaseDocument(::rtl::OUString sPath);
+    sal_Bool SaveDatabaseDocument();
+    void activateDatabasePath(OGeneralPage* _pTabpage);
+    void createUniqueFileName(INetURLObject* pURL);
+    ::rtl::OUString createUniqueName(com::sun::star::uno::Sequence< ::rtl::OUString > sContent, ::rtl::OUString _ElementName);
+    void CreateDatabase();
+    void createUniqueFolderName(INetURLObject* pURL);
+    DATASOURCE_TYPE VerifyDataSourceType(const DATASOURCE_TYPE _DatabaseType) const;
+
+    void ToggleFollowingRoadmapSteps();
+    sal_Bool callSaveAsDialog();
+//    sal_Bool DocUrlHasValue();
+    sal_Bool IsConnectionUrlRequired();
+    DECL_LINK(OnTypeSelected, OGeneralPage*);
+    DECL_LINK(ImplCreateDBHdl, OGeneralPage*);
+    DECL_LINK(ImplClickHdl, OMySQLIntroPageSetup*);
+    DECL_LINK(ImplModifiedHdl, OGenericAdministrationPage*);
+};
+
+//.........................................................................
+}   // namespace dbaui
+//.........................................................................
+
+#endif // DBAUI_DBWIZ2_HXX
+
