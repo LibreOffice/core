c3b019147f12e5cb4ae854dac7119547dd587a04
Pascal Junck <pjunck@openoffice.org>
2004-10-27 12:39:15 +0000

    INTEGRATION: CWS dbwizard1 (1.1.2); FILE ADDED 2004/08/13 15:14:49 bc 1.1.2.1: #111602# several changes for Formwizard and TableWizard

---
+++ wizards/com/sun/star/wizards/table/XCallTableWizard.java
@@ -0,0 +1,9 @@
+package com.sun.star.wizards.table;
+
+public interface XCallTableWizard extends com.sun.star.uno.XInterface {
+    // Methods
+    public void CallTableDialog();
+
+    // static Member
+    public static final com.sun.star.lib.uno.typeinfo.TypeInfo UNOTYPEINFO[] = { new com.sun.star.lib.uno.typeinfo.MethodTypeInfo("CallTableDialog", 0, 0)};
+}
