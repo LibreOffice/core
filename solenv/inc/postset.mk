95cd1ecbb2d4b043bb3908c9c00684d95e9a9822
Pascal Junck <pjunck@openoffice.org>
2004-10-27 14:21:52 +0000

    INTEGRATION: CWS withlang (1.21.92); FILE MERGED 2004/10/24 19:03:43 pjanik 1.21.92.2: #i31784#: en-US should always be the first ISO code. 2004/10/23 19:26:29 pjanik 1.21.92.1: #i31784#: simplify handling of languages.

--- solenv/inc/postset.mk
+++ solenv/inc/postset.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: postset.mk,v $
 #
-#   $Revision: 1.21 $
+#   $Revision: 1.22 $
 #
-#   last change: $Author: hjs $ $Date: 2004-06-25 16:11:46 $
+#   last change: $Author: pjunck $ $Date: 2004-10-27 15:21:52 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -61,162 +61,11 @@
 #*************************************************************************
 
 
-
-.IF "$(RES_GER)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=de
-.ENDIF
-completelangiso+=de
-
-# keep this one!!! #################################################################################
-#
-#
-####################################################################################################
-#
-
-.IF "$(RES_ENUS)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=en-US
-.ENDIF
-completelangiso+=en-US
-
-# keep this one!!! #################################################################################
-#
+# Default ISO code used when no languages specified
+# This language will be always included!
 defaultlangiso=en-US
-#
-####################################################################################################
-#
 
-.IF "$(RES_ITAL)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=it
-.ENDIF
-completelangiso+=it
+# Complete list of all supported ISO codes
+completelangiso=ar ca cs da de el en-US es et fi fr he hi-IN hu it ja ko nl pl pt pt-BR ru sk sl sv th tr zh-CN zh-TW
 
-.IF "$(RES_SPAN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=es
-.ENDIF
-completelangiso+=es
-
-.IF "$(RES_FREN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=fr
-.ENDIF
-completelangiso+=fr
-
-.IF "$(RES_DTCH)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=nl
-.ENDIF
-completelangiso+=nl
-
-.IF "$(RES_DAN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=da
-.ENDIF
-completelangiso+=da
-
-.IF "$(RES_SWED)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=sv
-.ENDIF
-completelangiso+=sv
-
-.IF "$(RES_PORT)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=pt
-.ENDIF 
-completelangiso+=pt
-
-.IF "$(RES_JAPN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=ja
-.ENDIF
-completelangiso+=ja
-
-.IF "$(RES_CHINSIM)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=zh-CN
-.ENDIF 
-completelangiso+=zh-CN
-
-.IF "$(RES_CHINTRAD)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=zh-TW
-.ENDIF 
-completelangiso+=zh-TW
-
-.IF "$(RES_RUSS)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=ru
-.ENDIF
-completelangiso+=ru
-
-.IF "$(RES_POL)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=pl
-.ENDIF 
-completelangiso+=pl
-
-.IF "$(RES_ARAB)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=ar
-.ENDIF 
-completelangiso+=ar
-
-.IF "$(RES_GREEK)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=el
-.ENDIF 
-completelangiso+=el
-
-.IF "$(RES_TURK)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=tr
-.ENDIF 
-completelangiso+=tr
-
-.IF "$(RES_KOREAN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=ko
-.ENDIF 
-completelangiso+=ko
-
-.IF "$(RES_CAT)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=ca
-.ENDIF 
-completelangiso+=ca
-
-.IF "$(RES_FINN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=fi
-.ENDIF          # "$(RES_FINN)"!="" || "$(give_me_all_languages)"!=""
-completelangiso+=fi
-
-.IF "$(RES_HUNG)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=hu
-.ENDIF
-completelangiso+=hu
-
-.IF "$(RES_THAI)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=th
-.ENDIF          # "$(RES_THAI)"!="" || "$(give_me_all_languages)"!=""
-completelangiso+=th
-
-.IF "$(RES_HINDI)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=hi-IN
-.ENDIF          # "$(RES_HINDI)"!="" || "$(give_me_all_languages)"!=""
-completelangiso+=hi-IN
-
-.IF "$(RES_PORTBR)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=pt-BR
-.ENDIF 
-completelangiso+=pt-BR
-
-.IF "$(RES_CZECH)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=cs
-.ENDIF 
-completelangiso+=cs
-
-.IF "$(RES_SLOVAK)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=sk
-.ENDIF 
-completelangiso+=sk
-
-.IF "$(RES_SLOVENIAN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=sl
-.ENDIF 
-completelangiso+=sl
-
-.IF "$(RES_ESTONIAN)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=et
-.ENDIF 
-completelangiso+=et
-
-.IF "$(RES_HEBREW)"!="" || "$(give_me_all_languages)"!=""
-alllangiso+=he
-.ENDIF 
-completelangiso+=he
-
+alllangiso=$(strip $(defaultlangiso) $(subst,$(defaultlangiso), $(uniq $(subst,ALL,$(completelangiso) $(WITH_LANG)))))
