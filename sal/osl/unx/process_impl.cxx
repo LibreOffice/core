e2b3ebfb9ff10f35d71740b71e3acc95bac02a91
Rüdiger Timm <rt@openoffice.org>
2004-10-28 15:25:26 +0000

    INTEGRATION: CWS valgrind02 (1.6.32); FILE MERGED 2004/10/11 17:13:37 mhu 1.6.32.2: #i35209# Adapted executable file path resolution to also handle the PATH search case. 2004/10/08 15:53:24 mhu 1.6.32.1: #i35209# Added function 'osl_setCommandArgs()', adapted 'osl_getExecutableFile()' and 'osl_getCommandArg[Count]()', cleaned up previous 'getCmdLine()' mess, removed unused / obsolete code.

--- sal/osl/unx/process_impl.cxx
+++ sal/osl/unx/process_impl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: process_impl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hjs $ $Date: 2004-06-25 17:14:34 $
+ *  last change: $Author: rt $ $Date: 2004-10-28 16:25:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,182 +59,299 @@
  *
  ************************************************************************/
 
- #ifndef _STRINGS_H
- #include <strings.h>
- #endif
+#include "osl/process.h"
 
- #ifndef _STDLIB_H
- #include <stdlib.h>
- #endif
-
- #ifndef _OSL_PROCESS_H_
- #include <osl/process.h>
- #endif
-
- #ifndef _RTL_USTRING_HXX_
- #include <rtl/ustring.hxx>
- #endif
-
- #ifndef _OSL_THREAD_H_
- #include <osl/thread.h>
- #endif
-
- #ifndef _OSL_FILE_HXX_
- #include <osl/file.hxx>
- #endif
-
- #ifndef _OSL_UUNXAPI_HXX_
- #include "uunxapi.hxx"
- #endif
-
- #ifndef _OSL_FILE_PATH_HELPER_HXX_
- #include "file_path_helper.hxx"
- #endif
-
- #include <string.h>
-
- //------------------------------------
- // forward
- //------------------------------------
-
- extern "C" sal_Char *getCmdLine();
- extern "C" char* osl_impl_getExecutableName(char * buffer, size_t n);
-
- //------------------------------------
- // private stuff
- //------------------------------------
-
- namespace /* private */
- {
-
-    const sal_Unicode UNICHAR_SLASH = ((sal_Unicode)'/');
-
-    //-------------------------------------------------------
-     // if the command line arg 0 contains a '/' somewhere it
-    // has been probably invoked relatively to the current
-    // working dir
-    //-------------------------------------------------------
-
-     inline bool is_relative_to_cwd(const rtl::OUString& path)
-    {
-        return (path.indexOf(UNICHAR_SLASH) > -1);
-    }
-
-    //---------------------------------------------------------
-    // make a relative path absolute using the current working
-    // directory
-    // no return value because osl_getExecutableFile either
-    // returns osl_Process_E_None or osl_Process_E_Unknown
-    //---------------------------------------------------------
-
-    void make_absolute_to_cwd(const rtl::OUString& relative_path, rtl::OUString& absolute_path)
-    {
-        rtl::OUString cwd_url;
-
-        if (osl_Process_E_None == osl_getProcessWorkingDir(&cwd_url.pData))
-        {
-            rtl::OUString cwd;
-#if OSL_DEBUG_LEVEL > 0
-            osl::FileBase::RC rc =
+#ifndef INCLUDED_LIMITS_H
+#include <limits.h>
+#define INCLUDED_LIMITS_H
 #endif
-                osl::FileBase::getSystemPathFromFileURL(cwd_url, cwd);
-            OSL_ASSERT(osl::FileBase::E_None == rc);
 
-            osl::systemPathMakeAbsolutePath(cwd, relative_path, absolute_path);
-        }
-    }
+#ifndef INCLUDED_PTHREAD_H
+#include <pthread.h>
+#define INCLUDED_PTHREAD_H
+#endif
 
-    //---------------------------------------------------------
-    // search for the file using the PATH environment variable
-    // no return value because osl_getExecutableFile either
-    // returns osl_Process_E_None or osl_Process_E_Unknown
-    //---------------------------------------------------------
+#ifndef INCLUDED_STDLIB_H
+#include <stdlib.h>
+#define INCLUDED_STDLIB_H
+#endif
 
-    void find_in_PATH(const rtl::OUString& file_path, rtl::OUString& path_found)
-    {
-        rtl::OUString   PATH   = rtl::OUString::createFromAscii("PATH");
-        rtl::OUString   env_path;
+#ifndef INCLUDED_STRING_H
+#include <string.h>
+#define INCLUDED_STRING_H
+#endif
 
-        if (osl_getEnvironment(PATH.pData, &env_path.pData) == osl_Process_E_None)
-            osl::searchPath(file_path, env_path, path_found);
-    }
+#ifndef _OSL_DIAGNOSE_H_
+#include "osl/diagnose.h"
+#endif
 
- } // namespace /private */
+#ifndef _OSL_FILE_H_
+#include <osl/file.h>
+#endif
 
+#ifndef _OSL_THREAD_H_
+#include "osl/thread.h"
+#endif
+
+#ifndef _RTL_USTRING_HXX_
+#include "rtl/ustring.hxx"
+#endif
+
+#ifndef _OSL_FILE_PATH_HELPER_H_
+#include "file_path_helper.h"
+#endif
+
+#ifndef _OSL_UUNXAPI_H_
+#include "uunxapi.h"
+#endif
 
 /***************************************
-  osl_getExecutableFile
+ CommandArgs_Impl.
  **************************************/
-
-oslProcessError SAL_CALL osl_getExecutableFile(rtl_uString** ppustrFile)
+struct CommandArgs_Impl
 {
-    sal_Char*       p_cmdline = getCmdLine();
-    oslProcessError osl_error = osl_Process_E_Unknown;
+    pthread_mutex_t m_mutex;
+    sal_uInt32      m_nCount;
+    rtl_uString **  m_ppArgs;
+};
 
-    if (p_cmdline != 0)
+static struct CommandArgs_Impl g_command_args =
+{
+    PTHREAD_MUTEX_INITIALIZER,
+    0,
+    0
+};
+
+/***************************************
+  osl_getExecutableFile().
+ **************************************/
+oslProcessError SAL_CALL osl_getExecutableFile (rtl_uString ** ppustrFile)
+{
+    oslProcessError result = osl_Process_E_NotFound;
+
+    pthread_mutex_lock (&(g_command_args.m_mutex));
+    OSL_ENSURE (g_command_args.m_nCount > 0, "osl_getExecutableFile(): CommandArgs not set.");
+    if (g_command_args.m_nCount > 0)
     {
-        rtl::OUString cmdline(p_cmdline, (sal_Int32)(strlen(p_cmdline)), osl_getThreadTextEncoding());
-
-        free(p_cmdline);
-
-        rtl::OUString abs_path;
-
-        if (osl::systemPathIsRelativePath(cmdline))
-        {
-            if (is_relative_to_cwd(cmdline))
-                make_absolute_to_cwd(cmdline, abs_path);
-            else
-                find_in_PATH(cmdline, abs_path);
-        }
-        else
-        {
-            abs_path = cmdline;
-        }
-
-        rtl::OUString path_resolved;
-
-        if ((abs_path.getLength() > 0) && osl::realpath(abs_path, path_resolved))
-        {
-            rtl::OUString furl;
-#if OSL_DEBUG_LEVEL > 0
-            osl::FileBase::RC rc =
-#endif
-                osl::FileBase::getFileURLFromSystemPath(path_resolved.pData, furl);
-
-            OSL_ASSERT(osl::FileBase::E_None == rc);
-
-            rtl_uString_assign(ppustrFile, furl.pData);
-            osl_error = osl_Process_E_None;
-        }
+        /* CommandArgs set. Obtain argv[0]. */
+        rtl_uString_assign (ppustrFile, g_command_args.m_ppArgs[0]);
+        result = osl_Process_E_None;
     }
-    return osl_error;
+    pthread_mutex_unlock (&(g_command_args.m_mutex));
+
+    return (result);
 }
 
 /***************************************
- Necessary for signal.c
+ osl_getCommandArgCount().
  **************************************/
-
-char* osl_impl_getExecutableName(char * buffer, size_t n)
+sal_uInt32 SAL_CALL osl_getCommandArgCount (void)
 {
-    sal_Char*     p_cmdline = getCmdLine();
-    char*         pChrRet   = NULL;
+    sal_uInt32 result = 0;
 
-    if (p_cmdline != 0)
+    pthread_mutex_lock (&(g_command_args.m_mutex));
+    OSL_ENSURE (g_command_args.m_nCount > 0, "osl_getCommandArgCount(): CommandArgs not set.");
+    if (g_command_args.m_nCount > 0)
+        result = g_command_args.m_nCount - 1;
+    pthread_mutex_unlock (&(g_command_args.m_mutex));
+
+    return (result);
+}
+
+/***************************************
+ osl_getCommandArg().
+ **************************************/
+oslProcessError SAL_CALL osl_getCommandArg (sal_uInt32 nArg, rtl_uString ** strCommandArg)
+{
+    oslProcessError result = osl_Process_E_NotFound;
+
+    pthread_mutex_lock (&(g_command_args.m_mutex));
+    OSL_ENSURE (g_command_args.m_nCount > 0, "osl_getCommandArg(): CommandArgs not set.");
+    if (g_command_args.m_nCount > (nArg + 1))
     {
-        rtl::OUString cmdline(p_cmdline, (sal_Int32)(strlen(p_cmdline)), osl_getThreadTextEncoding());
+        rtl_uString_assign (strCommandArg, g_command_args.m_ppArgs[nArg + 1]);
+        result = osl_Process_E_None;
+    }
+    pthread_mutex_unlock (&(g_command_args.m_mutex));
 
-        free(p_cmdline);
+    return (result);
+}
 
-        rtl::OUString exename_u;
-        osl::systemPathGetFileNameOrLastDirectoryPart(cmdline, exename_u);
-
-        rtl::OString exename_a = rtl::OUStringToOString(exename_u, osl_getThreadTextEncoding());
-
-        if (exename_a.getLength() < static_cast<int>(n))
+/***************************************
+ osl_setCommandArgs().
+ **************************************/
+void SAL_CALL osl_setCommandArgs (int argc, char ** argv)
+{
+    pthread_mutex_lock (&(g_command_args.m_mutex));
+    OSL_ENSURE (g_command_args.m_nCount == 0, "osl_setCommandArgs(): CommandArgs already set.");
+    if (g_command_args.m_nCount == 0)
+    {
+        rtl_uString** ppArgs = (rtl_uString**)rtl_allocateZeroMemory (argc * sizeof(rtl_uString*));
+        if (ppArgs != 0)
         {
-            strcpy(buffer, exename_a.getStr());
-            pChrRet = buffer;
+            rtl_TextEncoding encoding = osl_getThreadTextEncoding();
+            for (int i = 0; i < argc; i++)
+            {
+                rtl_string2UString (
+                    &(ppArgs[i]),
+                    argv[i], rtl_str_getLength (argv[i]), encoding,
+                    OSTRING_TO_OUSTRING_CVTFLAGS);
+            }
+            if (ppArgs[0] != 0)
+            {
+                /* see @ osl_getExecutableFile(). */
+                if (rtl_ustr_indexOfChar (rtl_uString_getStr(ppArgs[0]), sal_Unicode('/')) == -1)
+                {
+                    const rtl::OUString PATH (rtl::OUString::createFromAscii ("PATH"));
+
+                    rtl_uString * pSearchPath = 0;
+                    osl_getEnvironment (PATH.pData, &pSearchPath);
+                    if (pSearchPath)
+                    {
+                        rtl_uString * pSearchResult = 0;
+                        osl_searchPath (ppArgs[0], pSearchPath, &pSearchResult);
+                        if (pSearchResult)
+                        {
+                            rtl_uString_assign (&(ppArgs[0]), pSearchResult);
+                            rtl_uString_release (pSearchResult);
+                        }
+                        rtl_uString_release (pSearchPath);
+                    }
+                }
+
+                rtl_uString * pArg0 = 0;
+                if (realpath_u (ppArgs[0], &pArg0))
+                {
+                    osl_getFileURLFromSystemPath (pArg0, &(ppArgs[0]));
+                    rtl_uString_release (pArg0);
+                }
+            }
+            g_command_args.m_nCount = argc;
+            g_command_args.m_ppArgs = ppArgs;
         }
     }
-    return pChrRet;
+    pthread_mutex_unlock (&(g_command_args.m_mutex));
+}
+
+/***************************************
+ osl_getEnvironment().
+ **************************************/
+oslProcessError SAL_CALL osl_getEnvironment(rtl_uString* pustrEnvVar, rtl_uString** ppustrValue)
+{
+    oslProcessError  result   = osl_Process_E_NotFound;
+    rtl_TextEncoding encoding = osl_getThreadTextEncoding();
+    rtl_String* pstr_env_var  = 0;
+
+    OSL_PRECOND(pustrEnvVar, "osl_getEnvironment(): Invalid parameter");
+    OSL_PRECOND(ppustrValue, "osl_getEnvironment(): Invalid parameter");
+
+    rtl_uString2String(
+        &pstr_env_var,
+        rtl_uString_getStr(pustrEnvVar), rtl_uString_getLength(pustrEnvVar), encoding,
+        OUSTRING_TO_OSTRING_CVTFLAGS);
+    if (pstr_env_var != 0)
+    {
+        const char* p_env_var = getenv (rtl_string_getStr (pstr_env_var));
+        if (p_env_var != 0)
+        {
+            rtl_string2UString(
+                ppustrValue,
+                p_env_var, strlen(p_env_var), encoding,
+                OSTRING_TO_OUSTRING_CVTFLAGS);
+            OSL_ASSERT(*ppustrValue != NULL);
+
+            result = osl_Process_E_None;
+        }
+        rtl_string_release(pstr_env_var);
+    }
+
+    return (result);
+}
+
+/***************************************
+ osl_getProcessWorkingDir().
+ **************************************/
+oslProcessError SAL_CALL osl_getProcessWorkingDir(rtl_uString **ppustrWorkingDir)
+{
+    oslProcessError result = osl_Process_E_Unknown;
+    char buffer[PATH_MAX];
+
+    OSL_PRECOND(ppustrWorkingDir, "osl_getProcessWorkingDir(): Invalid parameter");
+
+    if (getcwd (buffer, sizeof(buffer)) != 0)
+    {
+        rtl_uString* ustrTmp = 0;
+
+        rtl_string2UString(
+            &ustrTmp,
+            buffer, strlen(buffer), osl_getThreadTextEncoding(),
+            OSTRING_TO_OUSTRING_CVTFLAGS);
+        if (ustrTmp != 0)
+        {
+            if (osl_getFileURLFromSystemPath (ustrTmp, ppustrWorkingDir) == osl_File_E_None)
+                result = osl_Process_E_None;
+            rtl_uString_release (ustrTmp);
+        }
+    }
+
+    return (result);
+}
+
+/******************************************************************************
+ *
+ *              new functions to set/return the current process locale
+ *
+ *****************************************************************************/
+
+struct ProcessLocale_Impl
+{
+    pthread_mutex_t m_mutex;
+    rtl_Locale *    m_pLocale;
+};
+
+static struct ProcessLocale_Impl g_process_locale =
+{
+    PTHREAD_MUTEX_INITIALIZER,
+    0
+};
+
+extern "C" void _imp_getProcessLocale( rtl_Locale ** );
+extern "C" int  _imp_setProcessLocale( rtl_Locale * );
+
+/**********************************************
+ osl_getProcessLocale().
+ *********************************************/
+oslProcessError SAL_CALL osl_getProcessLocale( rtl_Locale ** ppLocale )
+{
+    OSL_PRECOND(ppLocale, "osl_getProcessLocale(): Invalid parameter.");
+
+    pthread_mutex_lock(&(g_process_locale.m_mutex));
+
+    if (g_process_locale.m_pLocale == 0)
+        _imp_getProcessLocale (&(g_process_locale.m_pLocale));
+    *ppLocale = g_process_locale.m_pLocale;
+
+    pthread_mutex_unlock (&(g_process_locale.m_mutex));
+
+    return (osl_Process_E_None);
+}
+
+/**********************************************
+ osl_setProcessLocale().
+ *********************************************/
+oslProcessError SAL_CALL osl_setProcessLocale( rtl_Locale * pLocale )
+{
+    oslProcessError result = osl_Process_E_Unknown;
+
+    OSL_PRECOND(pLocale, "osl_setProcessLocale(): Invalid parameter.");
+
+    pthread_mutex_lock(&(g_process_locale.m_mutex));
+    if (_imp_setProcessLocale (pLocale) == 0)
+    {
+        g_process_locale.m_pLocale = pLocale;
+        result = osl_Process_E_None;
+    }
+    pthread_mutex_unlock (&(g_process_locale.m_mutex));
+
+    return (result);
 }
