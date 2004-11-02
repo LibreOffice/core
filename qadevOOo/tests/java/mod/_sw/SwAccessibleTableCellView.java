6863704c224415bd25debc5767ab58be84b5c449
Pascal Junck <pjunck@openoffice.org>
2004-11-02 11:11:05 +0000

    INTEGRATION: CWS qadev19 (1.5.70); FILE MERGED 2004/10/21 12:06:20 cn 1.5.70.1: #i35923# at.printAccessibleTree(log, xRoot,tParam.getBool(util.PropertyName.DEBUG_IS_ACTIVE)) added

--- qadevOOo/tests/java/mod/_sw/SwAccessibleTableCellView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessibleTableCellView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessibleTableCellView.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $
+ *  last change: $Author: pjunck $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -143,7 +143,7 @@ public class SwAccessibleTableCellView extends TestCase {
 
         log.println("ImplementationName " + utils.getImplName(oObj));
 
-        //at.printAccessibleTree(log, xRoot);
+        at.printAccessibleTree(log, xRoot, Param.getBool(util.PropertyName.DEBUG_IS_ACTIVE));
         TestEnvironment tEnv = new TestEnvironment(oObj);
 
         final XAccessibleSelection accSel = (XAccessibleSelection) UnoRuntime.queryInterface(
