117b3777a6b08690b0d23382ef08a0f5a3c222a4
Pascal Junck <pjunck@openoffice.org>
2004-11-02 10:48:09 +0000

    INTEGRATION: CWS qadev19 (1.1.2); FILE ADDED 2004/09/17 12:01:29 sg 1.1.2.1: #i32878#NEW: initial vesion

---
+++ qadevOOo/runner/util/PropertyName.java
@@ -0,0 +1,21 @@
+package util;
+
+public interface PropertyName {
+    final public static String APP_EXECUTION_COMMAND = "AppExecutionCommand";
+    final public static String CONNECTION_STRING = "ConnectionString";
+    final public static String TEST_BASE = "TestBase";
+    final public static String TEST_DOCUMENT_PATH = "TestDocumentPath";
+    final public static String LOGGING_IS_ACTIVE = "LoggingIsActive";
+    final public static String DEBUG_IS_ACTIVE = "DebugIsActive";
+    final public static String OUT_PRODUCER = "OutProducer";
+    final public static String SHORT_WAIT= "ShortWait";
+    final public static String OFFICE_PROVIDER = "OfficeProvider";
+    final public static String LOG_WRITER = "LogWriter";
+    final public static String TIME_OUT = "TimeOut";
+    final public static String OPERATING_SYSTEM = "OperatingSystem";
+
+    final public static String WNTMSCI = "wntmsci";
+    final public static String UNXLNGI = "unxlngi";
+    final public static String UNXSOLS = "unxsols";
+    final public static String UNXSOLI = "unxsoli";
+}
