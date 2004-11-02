09b68162d4c0b9d2f638a4e542188a77c48a1a75
Pascal Junck <pjunck@openoffice.org>
2004-11-02 10:39:59 +0000

    INTEGRATION: CWS qadev19 (1.2.138); FILE MERGED 2004/09/17 12:00:28 sg 1.2.138.1: #i32878#CHG: fixed bugs

--- qadevOOo/runner/lib/TestCase.java
+++ qadevOOo/runner/lib/TestCase.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TestCase.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2003-02-13 17:33:38 $
+ *  last change:$Date: 2004-11-02 11:39:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -173,7 +173,11 @@ public abstract class TestCase {
                 tEnv.setTestCase(this);
             }
         } catch (Exception e) {
-            System.out.println("Exception while getting Environment "+e.getMessage());
+            String message = e.getMessage();
+            if (message == null)
+                message = e.toString();
+            System.out.println("Exception while getting Environment "+message);
+            e.printStackTrace();
         }
         return tEnv;
     }
