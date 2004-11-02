2f8cc5117fb27a417ebc9590e187a075e7ab540d
Pascal Junck <pjunck@openoffice.org>
2004-11-02 11:13:38 +0000

    INTEGRATION: CWS qadev19 (1.7.70); FILE MERGED 2004/10/21 12:08:56 cn 1.7.70.1: #i35923# at.printAccessibleTree(log, xRoot,tParam.getBool(util.PropertyName.DEBUG_IS_ACTIVE)) added

--- qadevOOo/tests/java/mod/_toolkit/AccessibleFixedText.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleFixedText.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleFixedText.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Date: 2004-01-05 20:34:08 $
+ *  last change: $Date: 2004-11-02 12:13:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -196,7 +196,7 @@ public class AccessibleFixedText extends TestCase {
 
         XAccessible xRoot = at.getAccessibleObject(xWindow);
 
-        at.printAccessibleTree(log, xRoot);
+        at.printAccessibleTree(log, xRoot, Param.getBool(util.PropertyName.DEBUG_IS_ACTIVE));
 
         oObj = at.getAccessibleObjectForRole(xRoot, AccessibleRole.LABEL);
 
