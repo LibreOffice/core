ce5ad3e6d07c78bac28d43ebe0bf1a767acecc35
Pascal Junck <pjunck@openoffice.org>
2004-11-02 10:13:31 +0000

    INTEGRATION: CWS qadev19 (1.1.2); FILE ADDED 2004/09/22 08:21:31 lla 1.1.2.1: #i31243# new helper class for Images

---
+++ qadevOOo/runner/convwatch/ImageHelper.java
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ImageHelper.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Date: 2004-11-02 11:13:31 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package convwatch;
+
+import java.awt.Image;
+import java.awt.image.PixelGrabber;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+class ImageHelper
+{
+    Image m_aImage;
+    int[] m_aPixels;
+    int m_w = 0;
+    int m_h = 0;
+    boolean m_bGrabbed = false;
+
+    public ImageHelper(Image _aImage)
+    {
+         m_aImage = _aImage;
+
+         // grab all (consume much memory)
+         m_w = getWidth();
+         m_h = getHeight();
+         int x = 0;
+         int y = 0;
+         m_aPixels = new int[m_w * m_h];
+         PixelGrabber pg = new PixelGrabber(m_aImage, x, y, m_w, m_h, m_aPixels, 0, m_w);
+         try {
+             pg.grabPixels();
+         } catch (InterruptedException e) {
+             System.err.println("interrupted waiting for pixels!");
+             return;
+         }
+         if ((pg.getStatus() & ImageObserver.ABORT) != 0) {
+             System.err.println("image fetch aborted or errored");
+             return;
+         }
+         m_bGrabbed = true;
+    }
+    public int getWidth() {return m_aImage.getWidth(null);}
+    public int getHeight() {return m_aImage.getHeight(null);}
+    // direct access to a pixel
+    public int getPixel(int x, int y)
+        {
+            return m_aPixels[y * m_w + x];
+        }
+
+    // Write down the current image to a file.
+    // public void storeImage(String _sFilename)
+    //    {
+    //    }
+
+    public static ImageHelper createImageHelper(String _sFilename)
+        throws java.io.IOException
+        {
+            Image aImage = null;
+            File aFile = new File(_sFilename);
+            aImage = ImageIO.read(aFile);
+
+            return new ImageHelper(aImage);
+        }
+}
