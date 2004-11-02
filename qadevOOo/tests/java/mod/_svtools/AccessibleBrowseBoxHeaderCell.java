e1c531a924262df8737041b6b0a3f5c3181318d2
Pascal Junck <pjunck@openoffice.org>
2004-11-02 11:06:25 +0000

    INTEGRATION: CWS qadev19 (1.5.62); FILE MERGED 2004/10/21 12:08:35 cn 1.5.62.1: #i35923# at.printAccessibleTree(log, xRoot,tParam.getBool(util.PropertyName.DEBUG_IS_ACTIVE)) added

--- qadevOOo/tests/java/mod/_svtools/AccessibleBrowseBoxHeaderCell.java
+++ qadevOOo/tests/java/mod/_svtools/AccessibleBrowseBoxHeaderCell.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleBrowseBoxHeaderCell.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-02-04 11:23:41 $
+ *  last change:$Date: 2004-11-02 12:06:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -245,7 +245,7 @@ public class AccessibleBrowseBoxHeaderCell extends TestCase {
 
         XAccessible xRoot = at.getAccessibleObject(xWindow);
 
-        //at.printAccessibleTree(log,xRoot);
+        at.printAccessibleTree(log, xRoot, tParam.getBool(util.PropertyName.DEBUG_IS_ACTIVE));
 
         oObj = at.getAccessibleObjectForRole(xRoot,
             AccessibleRole.ROW_HEADER);
