faa3983e163d29336d4305e5629ed29ec1295283
Pascal Junck <pjunck@openoffice.org>
2004-11-03 07:00:21 +0000

    INTEGRATION: CWS desktintgr03 (1.2.2); FILE MERGED 2004/10/21 13:54:52 tra 1.2.2.1: #i34294#

--- shell/source/win32/shlxthandler/ooofilt/makefile.mk
+++ shell/source/win32/shlxthandler/ooofilt/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2004-09-08 14:32:45 $
+#   last change: $Author: pjunck $ $Date: 2004-11-03 08:00:21 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -72,41 +72,39 @@ USE_DEFFILE=TRUE
 .INCLUDE :  settings.mk
 
 CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501 
-CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
+CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820 
 CDEFS+=-D_WIN32_IE=0x501
 
 # --- Files --------------------------------------------------------
 
-SLOFILES=$(SLO)$/ooofilt.obj\
-     $(SLO)$/utilities.obj\
-     $(SLO)$/dbgmacros.obj\
-     $(SLO)$/propspec.obj
-    
-SHL1TARGET=$(TARGET)
+#UWINAPILIB=
 
+SLOFILES=$(SLO)$/ooofilt.obj\
+        $(SLO)$/utilities.obj\
+        $(SLO)$/dbgmacros.obj\
+        $(SLO)$/propspec.obj
+        
+SHL1TARGET=$(TARGET)
 SHL1STDLIBS=ole32.lib\
      advapi32.lib\
      comctl32.lib\
-     uwinapi.lib\
      uuid.lib\
      shell32.lib\
      kernel32.lib\
      OldNames.Lib
-    
+     
 SHL1LIBS=$(SOLARLIBDIR)$/zlib.lib\
     $(SOLARLIBDIR)$/expat_xmlparse.lib\
     $(SOLARLIBDIR)$/expat_xmltok.lib\
     $(SLB)$/util.lib\
     $(SLB)$/ooofilereader.lib
-
+    
 SHL1DEPN=
-
 SHL1OBJS=$(SLOFILES) 
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
-
 DEF1NAME=$(SHL1TARGET)
 DEF1EXPORTFILE=exports.dxp
-
+     
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :	target.mk
