69ab935cb4eaf428d31419147418070c0c4f749a
Pascal Junck <pjunck@openoffice.org>
2004-11-03 09:52:48 +0000

    INTEGRATION: CWS aw019 (1.12.62); FILE MERGED 2004/10/18 19:42:06 aw 1.12.62.2: RESYNC: (1.12-1.13); FILE MERGED 2004/10/06 16:18:16 aw 1.12.62.1: #i34831#

--- svx/source/svdraw/makefile.mk
+++ svx/source/svdraw/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: kz $ $Date: 2004-10-04 17:52:53 $
+#   last change: $Author: pjunck $ $Date: 2004-11-03 10:52:48 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ TARGET=svdraw
 
 SLOFILES=\
          $(SLO)$/svdobj.obj          \
+         $(SLO)$/svdoimp.obj         \
          $(SLO)$/svdoattr.obj        \
          $(SLO)$/svdotext.obj        \
          $(SLO)$/svdotxat.obj        \
