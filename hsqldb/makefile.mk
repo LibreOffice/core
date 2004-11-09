0e99359c489f16a9f14dca570ab2b011dd09be5e
Ocke Janssen <oj@openoffice.org>
2004-11-09 12:59:27 +0000

    moved from external/util

---
+++ hsqldb/makefile.mk
@@ -0,0 +1,108 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.1 $
+#
+#   last change: $Author: oj $ $Date: 2004-11-09 13:57:41 $
+#
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+#
+#          - GNU Lesser General Public License Version 2.1
+#          - Sun Industry Standards Source License Version 1.1
+#
+#   Sun Microsystems Inc., October, 2000
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#
+#   Sun Industry Standards Source License Version 1.1
+#   =================================================
+#   The contents of this file are subject to the Sun Industry Standards
+#   Source License Version 1.1 (the "License"); You may not use this file
+#   except in compliance with the License. You may obtain a copy of the
+#   License at http://www.openoffice.org/license.html.
+#
+#   Software provided under this License is provided on an "AS IS" basis,
+#   WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#   WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#   MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#   See the License for the specific provisions governing your rights and
+#   obligations concerning the Software.
+#
+#   The Initial Developer of the Original Code is: Ralph Thomas
+#
+#   Copyright: 2000 by Sun Microsystems, Inc.
+#
+#   All Rights Reserved.
+#
+#   Contributor(s): Ocke Janssen
+#
+#*************************************************************************
+
+PRJ=.
+
+PRJNAME=so_hsqldb
+TARGET=so_hsqldb
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+.INCLUDE :  version.mk
+
+.IF "$(SOLAR_JAVA)" != ""
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=hsqldb_$(HSQLDB_VERSION)
+PATCH_FILE_NAME=hsqldb_$(HSQLDB_VERSION).patch
+      
+TARFILE_ROOTDIR=hsqldb
+ADDITIONAL_FILES=src$/org$/hsqldb$/Storage.java\
+                src$/org$/hsqldb$/lib$/FileAccess.java \
+                src$/org$/hsqldb$/lib$/FileStorage.java
+
+
+.IF "$(ANT_HOME)" == ""
+ANT_HOME*:=$(COMMON_BUILD_TOOLS)$/apache-ant-1.6.1
+.EXPORT : ANT_HOME
+.ENDIF
+ANT_LIB*:=$(ANT_HOME)$/lib
+
+ANT_CLASSPATH:=$(ANT_LIB)$/xercesImpl.jar$(PATH_SEPERATOR)$(ANT_LIB)$/xml-apis.jar$(PATH_SEPERATOR)$(ANT_LIB)$/ant.jar
+PATH!:=$(ANT_HOME)$/bin$(PATH_SEPERATOR)$(PATH)
+
+.IF "$(ANT)" == ""
+ANT*:=$(ANT_HOME)$/bin$/ant
+.ENDIF
+
+ANT_BUILDFILE=build$/build.xml
+
+BUILD_ACTION=$(ANT) -f $(ANT_BUILDFILE) jar				
+
+.ENDIF
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+.INCLUDE : tg_ext.mk
+
+
---
+++ hsqldb/version.mk
@@ -0,0 +1,7 @@
+# when you want to change the HSQLDB version, you must update the d.lst
+# in the HSQLDB_thon project accordingly !!!
+HSQLDB_MAJOR=1
+HSQLDB_MINOR=7
+HSQLDB_MICRO=2
+HSQLDB_MICRO_MICRO=4
+HSQLDB_VERSION=$(HSQLDB_MAJOR)_$(HSQLDB_MINOR)_$(HSQLDB_MICRO)_$(HSQLDB_MICRO_MICRO)
