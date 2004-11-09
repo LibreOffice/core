fee97bbe5c9b25b26968e263cbf0e53cf79b567f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-09 11:36:36 +0000

    INTEGRATION: CWS oasisbf1 (1.3.638); FILE MERGED 2004/08/18 15:32:50 mib 1.3.638.1: #i32616#: document events were not saved

--- sw/source/filter/xml/xmlscript.cxx
+++ sw/source/filter/xml/xmlscript.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlscript.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-17 15:08:51 $
+ *  last change: $Author: hr $ $Date: 2004-11-09 12:36:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -77,10 +77,6 @@
 
 #ifndef _XMLOFF_XMLMETAI_HXX
 #include <xmloff/xmlscripti.hxx>
-#endif
-
-#ifndef _XMLOFF_XMLMETAE_HXX
-#include <xmloff/xmlscripte.hxx>
 #endif
 
 #ifndef _SVX_LANGITEM_HXX
