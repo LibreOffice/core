346074ec5407101723a3c77a0586abe3de924efc
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-09 15:41:01 +0000

    INTEGRATION: CWS vcl28 (1.1.4); FILE ADDED 2004/09/27 14:07:22 pl 1.1.4.1: #i34684# make selection timeout configurable

---
+++ dtrans/source/X11/config.cxx
@@ -0,0 +1,182 @@
+/*************************************************************************
+ *
+ *  $RCSfile: config.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-11-09 16:41:01 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <cstdio>
+
+#ifndef _UTL_CONFIGITEM_HXX_
+#include <unotools/configitem.hxx>
+#endif
+
+#include "X11_selection.hxx"
+
+#define SETTINGS_CONFIGNODE "VCL/Settings/Transfer"
+#define SELECTION_PROPERTY "SelectionTimeout"
+
+namespace x11
+{
+
+class DtransX11ConfigItem : public ::utl::ConfigItem
+{
+    sal_Int32           m_nSelectionTimeout;
+
+    virtual void Notify( const ::com::sun::star::uno::Sequence< ::rtl::OUString >& rPropertyNames );
+    virtual void Commit();
+public:
+    DtransX11ConfigItem();
+    virtual ~DtransX11ConfigItem();
+
+    sal_Int32 getSelectionTimeout() const { return m_nSelectionTimeout; }
+};
+
+}
+
+using namespace com::sun::star::lang;
+using namespace com::sun::star::uno;
+using namespace rtl;
+using namespace x11;
+
+sal_Int32 SelectionManager::getSelectionTimeout()
+{
+    if( m_nSelectionTimeout < 1 )
+    {
+        DtransX11ConfigItem aCfg;
+        m_nSelectionTimeout = aCfg.getSelectionTimeout();
+#if OSL_DEBUG_LEVEL > 1
+        fprintf( stderr, "initialized selection timeout to %ld seconds\n", m_nSelectionTimeout );
+#endif
+    }
+    return m_nSelectionTimeout;
+}
+
+/*
+ *  DtransX11ConfigItem constructor
+ */
+
+DtransX11ConfigItem::DtransX11ConfigItem() :
+    ConfigItem( OUString( RTL_CONSTASCII_USTRINGPARAM( SETTINGS_CONFIGNODE ) ),
+                CONFIG_MODE_DELAYED_UPDATE ),
+    m_nSelectionTimeout( 3 )
+{
+    if( IsValidConfigMgr() )
+    {
+        Sequence< OUString > aKeys( 1 );
+        aKeys.getArray()[0] = OUString( RTL_CONSTASCII_USTRINGPARAM( SELECTION_PROPERTY ) );
+        Sequence< Any > aValues = GetProperties( aKeys );
+#if OSL_DEBUG_LEVEL > 1
+        fprintf( stderr, "found %ld properties for %s\n", aValues.getLength(), SELECTION_PROPERTY );
+#endif
+        Any* pValue = aValues.getArray();
+        for( int i = 0; i < aValues.getLength(); i++, pValue++ )
+        {
+            if( pValue->getValueTypeClass() == TypeClass_STRING )
+            {
+                const OUString* pLine = (const OUString*)pValue->getValue();
+                if( pLine->getLength() )
+                {
+                    m_nSelectionTimeout = pLine->toInt32();
+                    if( m_nSelectionTimeout < 1 )
+                        m_nSelectionTimeout = 1;
+                }
+#if OSL_DEBUG_LEVEL > 1
+                fprintf( stderr, "found SelectionTimeout \"%s\"\n",
+                OUStringToOString( *pLine, osl_getThreadTextEncoding() ).getStr() );
+#endif
+            }
+#if OSL_DEBUG_LEVEL > 1
+            else
+                fprintf( stderr, "found SelectionTimeout of type \"%s\"\n",
+                OUStringToOString( pValue->getValueType().getTypeName(), osl_getThreadTextEncoding() ).getStr() );
+#endif
+        }
+    }
+#if OSL_DEBUG_LEVEL > 1
+    else
+        fprintf( stderr, "no valid configmanager, could not read timeout setting\n" );
+#endif
+}
+
+/*
+ *  DtransX11ConfigItem destructor
+ */
+
+DtransX11ConfigItem::~DtransX11ConfigItem()
+{
+}
+
+/*
+ *  DtransX11ConfigItem::Commit
+ */
+
+void DtransX11ConfigItem::Commit()
+{
+    // for the clipboard service this is readonly, so
+    // there is nothing to commit
+}
+
+/*
+ *  DtransX11ConfigItem::Notify
+ */
+
+void DtransX11ConfigItem::Notify( const Sequence< OUString >& rPropertyNames )
+{
+}
+
+
