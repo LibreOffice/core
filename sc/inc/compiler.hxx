bc6c7fe2d5e3a93cd9e075d1fdfd8824dc9bb503
Rüdiger Timm <rt@openoffice.org>
2004-11-09 13:58:16 +0000

    INTEGRATION: CWS dr27 (1.22.6); FILE MERGED 2004/11/04 15:36:33 dr 1.22.6.1: #110618# #i3724# #i25653# new formula export, step 20: unsupported functions

--- sc/inc/compiler.hxx
+++ sc/inc/compiler.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: compiler.hxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2004-10-22 07:56:48 $
+ *  last change: $Author: rt $ $Date: 2004-11-09 14:58:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -362,6 +362,8 @@ public:
     ScTokenArray* CompileString( const String& rFormula );
     BOOL  CompileTokenArray();
     short GetNumFormatType() { return nNumFmt; }
+
+    static const String& GetStringFromOpCode( OpCode eOpCode, bool bEnglish = false );
 
     ScToken* CreateStringFromToken( String& rFormula, ScToken* pToken,
                                     BOOL bAllowArrAdvance = FALSE );
