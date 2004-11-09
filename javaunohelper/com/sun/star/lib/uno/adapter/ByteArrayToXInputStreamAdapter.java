c7f2d41faf99e3cfb3714d7aa4315fbcf2a18e14
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-09 10:18:36 +0000

    INTEGRATION: CWS beansplit (1.1.2); FILE ADDED 2004/11/02 16:02:17 mi 1.1.2.3: raedBytes did not move stream pointer 2004/09/23 14:55:57 mi 1.1.2.2: ByteArray...Adapter moved from bean 2004/09/23 11:35:33 mi 1.1.2.1: ByteArray adapters moved from bean project

---
+++ javaunohelper/com/sun/star/lib/uno/adapter/ByteArrayToXInputStreamAdapter.java
@@ -0,0 +1,196 @@
+/*************************************************************************
+ *
+ *  $RCSfile: ByteArrayToXInputStreamAdapter.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-11-09 11:18:36 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., September, 2004
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2004 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2004 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+/*
+ * ByteArrayXInputStram.java
+ *
+ * Created on 10. April 2003, 15:45
+ */
+
+package com.sun.star.lib.uno.adapter;
+
+/**
+ *
+ * @author  lo119109
+ */
+
+import com.sun.star.io.XInputStream;
+import com.sun.star.io.XSeekable;
+import  com.sun.star.lib.uno.helper.ComponentBase;
+import java.io.*;
+
+public class ByteArrayToXInputStreamAdapter
+    extends ComponentBase
+    implements XInputStream, XSeekable
+{
+
+    byte[] m_bytes;
+    int m_length;
+    int m_pos;
+
+    boolean m_open;
+
+    /** Creates a new instance of ByteArrayXInputStram */
+    public ByteArrayToXInputStreamAdapter(byte[] bytes) {
+        init(bytes);
+    }
+
+    public void init(byte[] bytes) {
+        // System.err.println("ByteArrayXInputStream");
+        m_bytes = bytes;
+        m_length = bytes.length;
+        m_pos = 0;
+        m_open = true;
+    }
+
+    private void _check() throws com.sun.star.io.NotConnectedException, com.sun.star.io.IOException {
+        if (m_bytes == null) {
+            // System.err.println("check failed no bytes!");
+            throw new com.sun.star.io.NotConnectedException("no bytes");
+        }
+        if(!m_open) {
+            // System.err.println("check failed: closed");
+            throw new com.sun.star.io.IOException("input closed");
+        }
+    }
+
+    public int available() throws com.sun.star.io.NotConnectedException, com.sun.star.io.IOException {
+        _check();
+        long a = m_length - m_pos;
+        if (a != (int)a)
+            throw new com.sun.star.io.IOException("integer overflow");
+        else {
+            // System.err.println("available() -> "+a);
+            return (int)a;
+        }
+    }
+
+    public void closeInput() throws com.sun.star.io.NotConnectedException, com.sun.star.io.IOException {
+        // System.err.println("closeInput()");
+        _check();
+        m_open = false;
+    }
+
+    public int readBytes(byte[][] values, int param) throws com.sun.star.io.NotConnectedException, com.sun.star.io.BufferSizeExceededException, com.sun.star.io.IOException {
+        // System.err.println("readbytes(..., "+param+")");
+        _check();
+    try {
+            int remain = (int)(m_length - m_pos);
+            if (param > remain) param = remain;
+            /* ARGH!!! */
+            if (values[0] == null){
+                values[0] = new byte[param];
+                // System.err.println("allocated new buffer of "+param+" bytes");
+            }
+            System.arraycopy(m_bytes, m_pos, values[0], 0, param);
+            // System.err.println("readbytes() -> "+param);
+            m_pos += param;
+            return param;
+        } catch (ArrayIndexOutOfBoundsException ae) {
+            // System.err.println("readbytes() -> ArrayIndexOutOfBounds");
+            ae.printStackTrace();
+            throw new com.sun.star.io.BufferSizeExceededException("buffer overflow");
+        } catch (Exception e) {
+            // System.err.println("readbytes() -> Exception: "+e.getMessage());
+            e.printStackTrace();
+            throw new com.sun.star.io.IOException("error accessing buffer");
+        }
+    }
+
+    public int readSomeBytes(byte[][] values, int param) throws com.sun.star.io.NotConnectedException, com.sun.star.io.BufferSizeExceededException, com.sun.star.io.IOException {
+        // System.err.println("readSomebytes()");
+        return readBytes(values, param);
+    }
+
+    public void skipBytes(int param) throws com.sun.star.io.NotConnectedException, com.sun.star.io.BufferSizeExceededException, com.sun.star.io.IOException {
+        // System.err.println("skipBytes("+param+")");
+        _check();
+        if (param > (m_length - m_pos))
+            throw new com.sun.star.io.BufferSizeExceededException("buffer overflow");
+        m_pos += param;
+    }
+
+    public long getLength() throws com.sun.star.io.IOException {
+        // System.err.println("getLength() -> "+m_length);
+        if (m_bytes != null) return m_length;
+        else throw new com.sun.star.io.IOException("no bytes");
+    }
+
+    public long getPosition() throws com.sun.star.io.IOException {
+        // System.err.println("getPosition() -> "+m_pos);
+        if (m_bytes != null) return m_pos;
+        else throw new com.sun.star.io.IOException("no bytes");
+    }
+
+    public void seek(long param) throws com.sun.star.lang.IllegalArgumentException, com.sun.star.io.IOException {
+        // System.err.println("seek("+param+")");
+        if (m_bytes != null){
+            if (param < 0 || param > m_length) throw new com.sun.star.lang.IllegalArgumentException("invalid seek position");
+            else m_pos = (int)param;
+        }else throw new com.sun.star.io.IOException("no bytes");
+    }
+
+    public void finalize() throws Throwable{
+        // System.err.println("finalizer called for ByteArrayXInputStream!");
+        super.finalize();
+    }
+
+}
