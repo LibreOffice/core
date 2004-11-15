bbdb3007c758f18683189546a20335bdddd55801
Oliver Bolte <obo@openoffice.org>
2004-11-15 12:08:54 +0000

    INTEGRATION: CWS pythonupgrade (1.1.78); FILE MERGED 2004/10/31 10:48:50 pjanik 1.1.78.2: Fix DOS line endings. 2004/09/30 20:33:17 jbu 1.1.78.1: #i32901#+#i31866# upgrade to version 2.3.4, new build env on windows using nmake instead of msdev

--- python/pyversion.mk
+++ python/pyversion.mk
@@ -1,6 +1,14 @@
 # when you want to change the python version, you must update the d.lst
 # in the python project accordingly !!!
 PYMAJOR=2
-PYMINOR=2
-PYMICRO=2
+PYMINOR=3
+PYMICRO=4
 PYVERSION=$(PYMAJOR).$(PYMINOR).$(PYMICRO)
+
+.IF "$(GUI)" == "UNX"
+PY_FULL_DLL_NAME=libpython$(PYMAJOR).$(PYMINOR).so.1.0
+PYTHONLIB=-lpython$(PYMAJOR).$(PYMINOR)
+.ELSE
+PY_FULL_DLL_NAME=python$(PYMAJOR)$(PYMINOR).dll
+PYTHONLIB=python$(PYMAJOR)$(PYMINOR).lib
+.ENDIF
