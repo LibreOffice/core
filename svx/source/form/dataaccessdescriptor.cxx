83954814efe76cf4486b5b85598fad0fc8e92e69
Oliver Bolte <obo@openoffice.org>
2004-11-16 10:21:25 +0000

    INTEGRATION: CWS eforms2 (1.11.32); FILE MERGED 2004/08/23 16:46:35 pb 1.11.32.1: fix: #i32842# continue DataNavigator

--- svx/source/form/dataaccessdescriptor.cxx
+++ svx/source/form/dataaccessdescriptor.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dataaccessdescriptor.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 16:44:28 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 11:21:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
