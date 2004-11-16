08df29f6f1317b27c39bf9b95d915f42a12f9a45
Oliver Bolte <obo@openoffice.org>
2004-11-16 09:14:04 +0000

    INTEGRATION: CWS eforms2 (1.1.2); FILE ADDED 2004/08/06 14:46:20 dvo 1.1.2.3: #i31958# use "ID" property for instance IDs Issue number: Submitted by: Reviewed by: 2004/05/26 15:14:06 dvo 1.1.2.2: #114856# adopt to new XModel API; also put XForms attributes into correct (none) namespace Issue number: Submitted by: Reviewed by: 2004/01/28 14:53:52 dvo 1.1.2.1: #114856# begin XForms import/export

---
+++ xmloff/source/xforms/XFormsInstanceContext.cxx
@@ -0,0 +1,189 @@
+/*************************************************************************
+ *
+ *  $RCSfile: XFormsInstanceContext.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-11-16 10:14:04 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "XFormsInstanceContext.hxx"
+
+#include "DomBuilderContext.hxx"
+#include "xformsapi.hxx"
+
+#include <rtl/ustring.hxx>
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/beans/PropertyValue.hpp>
+#include <com/sun/star/xml/dom/XDocument.hpp>
+#include <com/sun/star/xforms/XModel.hpp>
+#include <tools/debug.hxx>
+
+#include <xmlnmspe.hxx>
+#include <xmltoken.hxx>
+#include <xmlimp.hxx>
+#include <xmlerror.hxx>
+#include <nmspmap.hxx>
+
+
+using rtl::OUString;
+using com::sun::star::uno::Reference;
+using com::sun::star::uno::makeAny;
+using com::sun::star::uno::UNO_QUERY;
+using com::sun::star::uno::Sequence;
+using com::sun::star::xforms::XModel;
+using com::sun::star::beans::XPropertySet;
+using com::sun::star::beans::PropertyValue;;
+using com::sun::star::xml::sax::XAttributeList;
+
+using xmloff::token::IsXMLToken;
+using xmloff::token::XML_INSTANCE;
+using xmloff::token::XML_SRC;
+using xmloff::token::XML_ID;
+
+static SvXMLTokenMapEntry aAttributes[] =
+{
+    TOKEN_MAP_ENTRY( NONE, SRC ),
+    TOKEN_MAP_ENTRY( NONE, ID ),
+    XML_TOKEN_MAP_END
+};
+
+XFormsInstanceContext::XFormsInstanceContext(
+    SvXMLImport& rImport,
+    USHORT nPrefix,
+    const OUString& rLocalName,
+    Reference<XPropertySet> xModel ) :
+        TokenContext( rImport, nPrefix, rLocalName, aAttributes, aEmptyMap ),
+        mxModel( Reference<XModel>( xModel, UNO_QUERY ) )
+{
+    DBG_ASSERT( mxModel.is(), "need model" );
+}
+
+XFormsInstanceContext::~XFormsInstanceContext()
+{
+}
+
+SvXMLImportContext* XFormsInstanceContext::CreateChildContext(
+    USHORT nPrefix,
+    const OUString& rLocalName,
+    const Reference<XAttributeList>& xAttrList )
+{
+    SvXMLImportContext* pContext = NULL;
+
+    // only the first element child of an xforms:instance element
+    // is used as an instance. The other children remainder must be
+    // ignored.
+    if( mxInstance.is() )
+    {
+        GetImport().SetError( XMLERROR_XFORMS_ONLY_ONE_INSTANCE_ELEMENT, rLocalName );
+        pContext = new SvXMLImportContext( GetImport(), nPrefix, rLocalName );
+    }
+    else
+    {
+        // create new DomBuilderContext. Save reference to tree in Model.
+        DomBuilderContext* pInstance =
+            new DomBuilderContext( GetImport(), nPrefix, rLocalName );
+        mxInstance = pInstance->getTree();
+        pContext = pInstance;
+    }
+
+    DBG_ASSERT( pContext != NULL, "no context!" );
+    return pContext;
+
+}
+
+void XFormsInstanceContext::EndElement()
+{
+    Sequence<PropertyValue> aSequence( 3 );
+    PropertyValue* pSequence = aSequence.getArray();
+    pSequence[0].Name = OUString( RTL_CONSTASCII_USTRINGPARAM("Instance") );
+    pSequence[0].Value <<= mxInstance;
+    pSequence[1].Name = OUString( RTL_CONSTASCII_USTRINGPARAM("ID") );
+    pSequence[1].Value <<= msId;
+    pSequence[2].Name = OUString( RTL_CONSTASCII_USTRINGPARAM("URL") );
+    pSequence[2].Value <<= msURL;
+
+    mxModel->getInstances()->insert( makeAny( aSequence ) );
+}
+
+
+void XFormsInstanceContext::HandleAttribute(
+    sal_uInt16 nToken,
+    const rtl::OUString& rValue )
+{
+    switch( nToken )
+    {
+    case XML_SRC:
+        msURL = rValue;
+        break;
+    case XML_ID:
+        msId = rValue;
+        break;
+    default:
+        DBG_ERROR( "should not happen" );
+        break;
+    }
+}
+
+SvXMLImportContext* XFormsInstanceContext::HandleChild(
+    sal_uInt16 nToken,
+    sal_uInt16,
+    const OUString&,
+    const Reference<XAttributeList>& )
+{
+    DBG_ERROR( "to be handled by CreateChildContext" );
+    return NULL;
+}
