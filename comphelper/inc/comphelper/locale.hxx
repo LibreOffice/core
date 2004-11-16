ca02f3cff1602083eb8d86a5d545ad570f4ecff6
Oliver Bolte <obo@openoffice.org>
2004-11-16 14:30:08 +0000

    INTEGRATION: CWS docking4 (1.3.20); FILE MERGED 2004/10/27 12:54:46 as 1.3.20.1: #i35699# repair locale fallback mechanism

--- comphelper/inc/comphelper/locale.hxx
+++ comphelper/inc/comphelper/locale.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: locale.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-09-20 10:16:57 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 15:30:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -105,6 +105,7 @@ class Locale
 
         /** @short some predefined Locale objects. */
         static const Locale EN_US();
+        static const Locale EN();
         static const Locale DE_DE();
         static const Locale DE_CH();
         static const Locale DE_AT();
