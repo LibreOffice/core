053a480cbccaf4f44837d6493dc43b6ecb99aa31
Oliver Bolte <obo@openoffice.org>
2004-11-16 11:07:52 +0000

    INTEGRATION: CWS eforms2 (1.4.22); FILE MERGED 2004/06/29 14:23:40 dvo 1.4.22.2: RESYNC: (1.4-1.5); FILE MERGED 2004/04/26 11:33:47 fs 1.4.22.1: some cleanup/consolidation: now separate classes for property UI descriptor and line descriptor (which is a UI descriptor plus values)

--- extensions/source/propctrlr/linedescriptor.hxx
+++ extensions/source/propctrlr/linedescriptor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: linedescriptor.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: svesik $ $Date: 2004-04-21 14:20:12 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 12:07:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,54 +67,131 @@
 #ifndef _VCL_FLDUNIT_HXX
 #include <vcl/fldunit.hxx>
 #endif
+#ifndef _SV_IMAGE_HXX
+#include <vcl/image.hxx>
+#endif
 
 //............................................................................
 namespace pcr
 {
 //............................................................................
 
+#define LINE_FLAG_EVENT             0x0001
+    // TODO: this could be handled with virtual properties, and a IPropertyHandler implementation, and PROP_FLAGs
+
+    struct PropertyUIDescriptor
+    {
+        /// the type of the control which should be used to represent this property at the UI
+        BrowserControlType          eControlType;
+
+        /** a user-defined control, if the property cannot be represented by a standard control
+            Only evaluated if eControlType equals BCT_USERDEFINED
+        */
+        IBrowserControl*            pControl;
+
+        /** a list of possible values for the property
+            Only evaluated if eControlType equals BCT_LISTBOX or BCT_COMBOBOX
+        */
+        ::std::vector< ::rtl::OUString >
+                                    aListValues;
+
+        /** a help id to give to the browse button, if necessary.
+            The line has a browse button if and only if this is not 0.
+        */
+        sal_uInt32                  nButtonHelpId;
+
+        /** if the property supports two buttons, this is the help id of the second one
+            The line has a second button if and only if this is not 0.
+            Only evaluated if nButtonHelpId is not 0.
+        */
+        sal_uInt32                  nAdditionalButtonHelpId;
+
+        /** an image to display on the browse button. If not set, the button
+            will contain the usual "..." text for browse buttons.
+            Only evaluated if nButtonHelpId is not 0.
+        */
+        Image                       aButtonImage;
+
+        /** an image to display on the second button.
+            Only evaluated if nAdditionalButtonHelpId is not 0.
+        */
+        Image                       aAdditionalButtonImage;
+
+        /** should the UI for this property get an indent, so it looks like a sub-aspect
+            of some super-ordinate property?
+        */
+        sal_Bool                    bIndent : 1;
+
+        /// is the UI for this property locked (i.e. read-only)?
+        sal_Bool                    bIsLocked : 1;
+
+        /** does the (numeric) property have a min- and max value
+            If this is false, nMinValue and nMaxValue are to be ignored
+        */
+        sal_Bool                    bHaveMinMax : 1;
+
+        /// for numeric fields: number of digits
+        sal_uInt16                  nDigits;
+        /// for numeric fields only: minimum value
+        sal_Int32                   nMinValue;
+        /// for numeric fields only: maximum value
+        sal_Int32                   nMaxValue;
+        /// for numeric fields only: measurement unit to display values
+        FieldUnit                   eDisplayUnit;
+        /// for numeric fields only: measurement unit to transfer values
+        FieldUnit                   eValueUnit;
+
+        PropertyUIDescriptor()
+        {
+            clear();
+        }
+
+        void clear()
+        {
+            eControlType = BCT_UNDEFINED;
+            pControl = NULL;
+            aListValues.resize( 0 );
+            nButtonHelpId = nAdditionalButtonHelpId = 0;
+            aButtonImage = aAdditionalButtonImage = Image();
+            bIndent = bIsLocked = bHaveMinMax = sal_False;
+            nDigits = 0;
+            nMinValue = 0;
+            nMaxValue = -1;
+            eDisplayUnit = eValueUnit = FUNIT_NONE;
+        }
+    };
+
     class IBrowserControl;
     //========================================================================
     //= OLineDescriptor
     //========================================================================
-    struct OLineDescriptor
+    struct OLineDescriptor : public PropertyUIDescriptor
     {
-        String                      sName;
-        String                      sTitle;
-        String                      sValue;
-        ::std::vector< String>      aListValues;
-        void*                       pDataPtr;
-        IBrowserControl*            pControl;
-        BrowserControlType          eControlType;
-        sal_uInt32                  nHelpId;
-        sal_uInt32                  nUniqueButtonId;
-        sal_uInt16                  nDigits;        // for numeric fields
-        sal_Int32                   nMinValue;      // for numeric fields only
-        sal_Int32                   nMaxValue;      // for numeric fields only
-        FieldUnit                   eDisplayUnit;   // for numeric fields only
-        FieldUnit                   eValueUnit;     // for numeric fields only
+        String                      sName;          // the name of the property
+        String                      sTitle;         // the human readable label for the property
+        String                      sValue;         // the current value of the property, in it's string representation
+        sal_uInt32                  nHelpId;        // a help id to give to the input control
 
-        sal_Bool                    bUnknownValue   :1;
-        sal_Bool                    bHasDefaultValue:1;
-        sal_Bool                    bIsLocked       :1;
-        sal_Bool                    bHaveMinMax     :1;
+        sal_uInt16                  nFlags;                 // special flag, see LINE_FLAG_*
+        sal_Bool                    bUnknownValue    : 1;   // is the property value currently "unknown"?
+        sal_Bool                    bHasDefaultValue : 1;   // does the property support a "standard" value?
+
+        // TODO: this class is a wild mix of property meta data, and information about a current property
+        // value. Can this be separated? Doesn't part of this belong into IPropertyInfoService?
 
         OLineDescriptor()
-            :pDataPtr(NULL)
-            ,pControl(NULL)
-            ,eControlType(BCT_UNDEFINED)
-            ,nHelpId(0)
-            ,nUniqueButtonId(0)
-            ,nDigits(0)
-            ,nMinValue(0)
-            ,nMaxValue(-1)
-            ,bUnknownValue(sal_False)
-            ,bHasDefaultValue(sal_False)
-            ,bIsLocked(sal_False)
-            ,bHaveMinMax(sal_False)
-            ,eDisplayUnit( FUNIT_NONE )
-            ,eValueUnit( FUNIT_NONE )
         {
+            clear();
+        }
+
+        void clear()
+        {
+            PropertyUIDescriptor::clear();
+
+            sName = sTitle = sValue = String();
+            nHelpId = 0;
+            nFlags = 0;
+            bUnknownValue = bHasDefaultValue = sal_False;
         }
     };
 
