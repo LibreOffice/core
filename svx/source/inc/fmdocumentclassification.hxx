4d7bf0b57348b370437c567a469e7f6ca8371cb0
Oliver Bolte <obo@openoffice.org>
2004-11-16 10:30:02 +0000

    INTEGRATION: CWS eforms2 (1.2.86); FILE MERGED 2004/08/03 14:00:51 fs 1.2.86.1: XForms now named EnhancedForms

--- svx/source/inc/fmdocumentclassification.hxx
+++ svx/source/inc/fmdocumentclassification.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmdocumentclassification.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-05 15:52:21 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 11:30:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -83,7 +83,7 @@ namespace svxform
         eSpreadsheetDocument,
         eDrawingDocument,
         ePresentationDocument,
-        eElectronicForm,
+        eEnhancedForm,
         eDatabaseForm,
 
         eUnknownDocumentType
