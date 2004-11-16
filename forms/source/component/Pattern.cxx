b6d77ede7eca565c1fe9870b44e1267537807083
Oliver Bolte <obo@openoffice.org>
2004-11-16 09:40:13 +0000

    INTEGRATION: CWS eforms2 (1.11.26); FILE MERGED 2004/07/19 15:18:42 fs 1.11.26.1: during #114856#: properly determine who instigated a setControlValue, so that we can propagate the values to appropriate interested parties / while we were here: make some attributes private ...

--- forms/source/component/Pattern.cxx
+++ forms/source/component/Pattern.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Pattern.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 12:46:56 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 10:40:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -214,7 +214,7 @@ sal_Bool OPatternModel::commitControlValueToDbColumn( bool _bPostReset )
 
     if ( sNewValue != m_aSaveValue )
     {
-        if ( !sNewValue.getLength() && !m_bRequired && m_bEmptyIsNull )
+        if ( !sNewValue.getLength() && !isRequired() && m_bEmptyIsNull )
             m_xColumnUpdate->updateNull();
         else
         {
