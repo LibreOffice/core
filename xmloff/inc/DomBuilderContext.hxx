7d1a9b1b0ccff92bb4a473fa31b550a97ad2628d
Oliver Bolte <obo@openoffice.org>
2004-11-16 09:02:43 +0000

    INTEGRATION: CWS eforms2 (1.1.2); FILE ADDED 2004/01/28 14:38:45 dvo 1.1.2.1: #114856# load/save DOM trees

---
+++ xmloff/inc/DomBuilderContext.hxx
@@ -0,0 +1,149 @@
+/*************************************************************************
+ *
+ *  $RCSfile: DomBuilderContext.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2004-11-16 10:02:43 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_DOMBUILDERCONTEXT_HXX
+#define _XMLOFF_DOMBUILDERCONTEXT_HXX
+
+
+//
+// include for parent class and members
+//
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include "xmlictxt.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
+#include "Reference.hxx"
+#endif
+
+
+//
+// forward declarations
+//
+
+namespace com { namespace sun { namespace star {
+    namespace xml { namespace dom {
+        class XNode;
+        class XDocument;
+    } }
+    namespace xml { namespace sax {
+        class XAttributeList;
+    } }
+} } }
+namespace rtl {
+    class OUString;
+}
+class SvXMLImport;
+class SvXMLImportContext;
+
+/**
+ * DomBuilderContext creates a DOM tree suitable for in-memory processing of
+ * XML data from a sequence of SAX events */
+class DomBuilderContext : public SvXMLImportContext
+{
+    com::sun::star::uno::Reference<com::sun::star::xml::dom::XNode> mxNode;
+
+public:
+
+    /** default constructor: create new DOM tree */
+    DomBuilderContext( SvXMLImport& rImport,
+                       USHORT nPrefix,
+                       const ::rtl::OUString& rLocalName );
+
+    /** constructor: create DOM subtree under the given node */
+    DomBuilderContext( SvXMLImport& rImport,
+                       USHORT nPrefix,
+                       const ::rtl::OUString& rLocalName,
+                       com::sun::star::uno::Reference<com::sun::star::xml::dom::XNode>& );
+
+    virtual ~DomBuilderContext();
+
+
+    //
+    // access to the DOM tree
+    //
+
+    /** access the DOM tree */
+    com::sun::star::uno::Reference<com::sun::star::xml::dom::XDocument> getTree();
+
+    /** access this context's DOM sub-tree */
+    com::sun::star::uno::Reference<com::sun::star::xml::dom::XNode> getNode();
+
+
+    //
+    // implement SvXMLImportContext methods:
+    //
+
+    virtual SvXMLImportContext* CreateChildContext(
+        USHORT nPrefix,
+        const rtl::OUString& rLocalName,
+        const com::sun::star::uno::Reference<com::sun::star::xml::sax::XAttributeList >& xAttrList );
+
+    virtual void StartElement(
+        const com::sun::star::uno::Reference<com::sun::star::xml::sax::XAttributeList>& xAttrList );
+
+    virtual void EndElement();
+
+    virtual void Characters( const ::rtl::OUString& rChars );
+};
+
+#endif
