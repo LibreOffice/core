9c42eddd4900f2a1142fed1a52cf882457639e25
Oliver Bolte <obo@openoffice.org>
2004-11-17 11:36:33 +0000

    INTEGRATION: CWS fwkbugfix03 (1.2.68); FILE MERGED 2004/10/19 12:50:28 cd 1.2.68.1: #i30041# Added new state to support show/hide controllers with status update

--- offapi/com/sun/star/frame/status/makefile.mk
+++ offapi/com/sun/star/frame/status/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2004-07-06 11:21:23 $
+#   last change: $Author: obo $ $Date: 2004-11-17 12:36:33 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -74,7 +74,8 @@ PACKAGE=com$/sun$/star$/frame$/status
 
 IDLFILES=\
     ItemStatus.idl\
-    ItemState.idl
+    ItemState.idl\
+    Visibility.idl
 
 # ------------------------------------------------------------------
 
