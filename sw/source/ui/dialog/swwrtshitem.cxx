4bfcbe0289598a2c7dc7c51635bf3a42f216ddd3
Rüdiger Timm <rt@openoffice.org>
2004-11-17 07:23:01 +0000

    INTEGRATION: CWS os41 (1.3.108); FILE MERGED 2004/10/26 15:01:23 os 1.3.108.1: #i35864# Copy CTor must copy all members

--- sw/source/ui/dialog/swwrtshitem.cxx
+++ sw/source/ui/dialog/swwrtshitem.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swwrtshitem.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 13:00:18 $
+ *  last change: $Author: rt $ $Date: 2004-11-17 08:23:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,8 +65,9 @@ SwWrtShellItem::SwWrtShellItem( USHORT nWhich, SwWrtShell* pSh )
 {
 
 }
-SwWrtShellItem::SwWrtShellItem( const SwWrtShellItem& rItem)
-    : SfxPoolItem( rItem.Which() )
+SwWrtShellItem::SwWrtShellItem( const SwWrtShellItem& rItem) :
+    SfxPoolItem( rItem.Which() ),
+    pWrtSh( rItem.pWrtSh )
 {
 }
 
