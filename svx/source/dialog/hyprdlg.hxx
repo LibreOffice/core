13992edb020a3f555203b44725c053478f765e57
Oliver Bolte <obo@openoffice.org>
2004-11-17 17:07:50 +0000

    INTEGRATION: CWS oooicons (1.2.418); FILE MERGED 2004/10/31 07:30:03 cd 1.2.418.2: #i10000# Removed obsolete include 2004/10/29 16:12:56 cd 1.2.418.1: #i35368# Removed all obsolete occurrences of SfxImageManager - added a static image list manager

--- svx/source/dialog/hyprdlg.hxx
+++ svx/source/dialog/hyprdlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hyprdlg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:31:13 $
+ *  last change: $Author: obo $ $Date: 2004-11-17 18:07:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
