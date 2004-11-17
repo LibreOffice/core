8c3861b1fcd961a09254b584b4e74e6a7c0b06a4
Oliver Bolte <obo@openoffice.org>
2004-11-17 14:36:09 +0000

    INTEGRATION: CWS fwkp2fix02 (1.2.34); FILE MERGED 2004/11/12 12:14:17 mba 1.2.34.1: #i36981#: math objects couldn't be loaded

--- comphelper/source/misc/storagehelper.cxx
+++ comphelper/source/misc/storagehelper.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: storagehelper.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-10-04 21:07:02 $
+ *  last change: $Author: obo $ $Date: 2004-11-17 15:36:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -263,7 +263,7 @@ sal_Int32 OStorageHelper::GetXStorageFormat(
       || aMediaType.equalsIgnoreAsciiCase( ::rtl::OUString::createFromAscii( "application/x-vnd.oasis.openoffice.presentation" ) )
       || aMediaType.equalsIgnoreAsciiCase( ::rtl::OUString::createFromAscii( "application/x-vnd.oasis.openoffice.spreadsheet" ) )
       || aMediaType.equalsIgnoreAsciiCase( ::rtl::OUString::createFromAscii( "application/x-vnd.oasis.openoffice.chart" ) )
-      || aMediaType.equalsIgnoreAsciiCase( ::rtl::OUString::createFromAscii( "application/x-vnd.oasis.openoffice.math" ) ) )
+      || aMediaType.equalsIgnoreAsciiCase( ::rtl::OUString::createFromAscii( "application/x-vnd.oasis.openoffice.formula" ) ) )
     {
         nResult = SOFFICE_FILEFORMAT_8;
     }
