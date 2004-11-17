cd2e49f1fae47737216f4255e6227d25b9097d37
Oliver Bolte <obo@openoffice.org>
2004-11-17 11:43:51 +0000

    INTEGRATION: CWS fwkbugfix03 (1.3.78); FILE MERGED 2004/09/23 15:48:30 mav 1.3.78.1: #i32557# the activex control can not be compiled by .NET 2003 Toolkit

--- extensions/source/activex/msidl/makefile.mk
+++ extensions/source/activex/msidl/makefile.mk
@@ -1,12 +1,12 @@
 #**************************************************************************
 #
-#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.3 2004-06-16 10:53:56 rt Exp $
+#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.4 2004-11-17 12:43:51 obo Exp $
 #
 # =========================================================================
 #
-#     $Date: 2004-06-16 10:53:56 $
-#     $Author: rt $
-#     $Revision: 1.3 $
+#     $Date: 2004-11-17 12:43:51 $
+#     $Author: obo $
+#     $Revision: 1.4 $
 #
 # =========================================================================
 #
@@ -25,7 +25,7 @@ TARGET=msidl
 
 .INCLUDE : settings.mk
 
-.IF "$(GUI)" == "WNT"
+.IF "$(GUI)" == "WNT" && "$(NETTOOLKIT)"==""
 
 # --- Targets ----------------------------------
 
