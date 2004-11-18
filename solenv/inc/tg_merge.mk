a96282a413d3dde5624d91581719214179b6cfe0
Oliver Bolte <obo@openoffice.org>
2004-11-18 07:44:28 +0000

    INTEGRATION: CWS nativefixer1 (1.4.2); FILE MERGED 2004/10/22 12:48:09 is 1.4.2.1: #i34221# solving merge conflicts in header

--- solenv/inc/tg_merge.mk
+++ solenv/inc/tg_merge.mk
@@ -2,9 +2,6 @@
 #
 #   $RCSfile: tg_merge.mk,v $
 #
-#   $Revision: 1.4 $
-#
-#   last change: $Author: obo $ $Date: 2004-10-18 13:56:03 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
