e42d2e6bfbaac131ecce1deab335ee6a159e7d42
Oliver Bolte <obo@openoffice.org>
2004-11-18 07:33:08 +0000

    INTEGRATION: CWS nativefixer1 (1.3.164); FILE MERGED 2004/11/01 12:39:46 is 1.3.164.1: #i35930# introducing regmerge

--- solenv/bin/par2script.pl
+++ solenv/bin/par2script.pl
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: par2script.pl,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-02-10 14:27:03 $
+#   last change: $Author: obo $ $Date: 2004-11-18 08:33:08 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -102,7 +102,7 @@ par2script::check::check_directories_in_item_definitions($setupscript, "File");
 par2script::check::check_directories_in_item_definitions($setupscript, "Shortcut");
 par2script::check::check_directories_in_item_definitions($setupscript, "Profile");
 par2script::check::check_module_existence($setupscript);
-par2script::check::check_registry_at_files($setupscript);
+# par2script::check::check_registry_at_files($setupscript);
 par2script::check::check_moduleid_at_items($setupscript);
 
 # saving the script
