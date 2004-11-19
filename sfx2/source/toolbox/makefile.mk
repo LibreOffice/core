12ef372065bd77d34253f8caf52828caebd29fea
Oliver Bolte <obo@openoffice.org>
2004-11-19 10:37:49 +0000

    INTEGRATION: CWS oooicons (1.3.58); FILE MERGED 2004/10/29 15:56:50 cd 1.3.58.1: #i35368# Removed all obsolete occurrences of SfxImageManager - added a static image list manager

--- sfx2/source/toolbox/makefile.mk
+++ sfx2/source/toolbox/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2004-08-02 15:04:43 $
+#   last change: $Author: obo $ $Date: 2004-11-19 11:37:49 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -87,8 +87,6 @@ SLOFILES =	\
         $(SLO)$/imgmgr.obj \
         $(SLO)$/tbxconf.obj \
         $(SLO)$/tbxitem.obj
-
-#        $(SLO)$/tbxcust.obj \
 
 # --- Tagets -------------------------------------------------------
 
