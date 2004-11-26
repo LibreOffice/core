5c2f48ad653ac5faa759e218a4d60cce20ccb123
Rüdiger Timm <rt@openoffice.org>
2004-11-26 11:59:37 +0000

    INTEGRATION: CWS oasisbf2 (1.6.416); FILE MERGED 2004/10/27 17:23:36 dvo 1.6.416.1: #i35802# image maps should use draw:points (instead of svg:points) on   <draw:area-polygon>   update filter + OASIS transformer

--- xmloff/source/draw/XMLImageMapExport.cxx
+++ xmloff/source/draw/XMLImageMapExport.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLImageMapExport.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: dvo $ $Date: 2001-06-29 21:07:13 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 12:59:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -457,6 +457,6 @@ void XMLImageMapExport::ExportPolygon(
     awt::Size aSize(nWidth, nHeight);
     SdXMLImExPointsElement aPoints( &aPoly, aViewBox, aPoint, aSize,
                                     rExport.GetMM100UnitConverter() );
-    rExport.AddAttribute( XML_NAMESPACE_SVG, XML_POINTS,
+    rExport.AddAttribute( XML_NAMESPACE_DRAW, XML_POINTS,
                           aPoints.GetExportString());
 }
