775387fcfe8f9b06a634edfad050bfffa40deb34
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:08:16 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/11/23 20:21:41 thb 1.1.2.9: #110496# Now using the full shape area when blowing up the clip polygon (such that it also includes transformed and anti-aliased shapes 2004/10/08 20:08:16 thb 1.1.2.8: #i34997# Fixed several node state inconsistencies, which led to start/end activities not correctly updating the shapes; added proper updating behaviour to LayerManager (several display-changing cases were missing); fixed SLIDEWIPE for shape transitions; improved docs and structure slightly 2004/09/22 17:58:00 thb 1.1.2.7: #110496# Fixed various issues reported by QA: shape positions, when controlled via animations, are now all relative to the shape center; unified scheduleActivationEvent, to interpret missing begin times as 0 for all nodes (not only leaf nodes, as before); added two new function keywords to the expression parser (min and max), this was needed for Zoom from bottom effect; temporarily implemented SLIDEWIPE by BARWIPE for shapes; corrected various transition mapping issues in the transitionfactorytab.cxx; clarified documentation at various places 2004/09/03 17:56:20 thb 1.1.2.6: #110496# Added support for TransitionType::RANDOM 2004/09/03 16:36:22 thb 1.1.2.5: #110496# Changed assertion messages, slightly adapted transitionfactory error handling, changed dissolve to scale isotrophically 2004/09/03 11:00:25 thb 1.1.2.4: #110496# Slide event handling was completely broken, fixed that (the reason was that ultimately, the presentation did not call prefetchShow before asking isAnimated) 2004/09/02 17:07:18 dbo 1.1.2.3: #110496# added doublediamondwipe, doublebarndoorwipe, figure wipes, sweepwipes, misc modifications Issue number: Submitted by: Reviewed by: 2004/09/01 20:26:45 thb 1.1.2.2: #110496# Implemented fade, slide and push special transition effects (had to pass Slide ptrs to transitionfactory again) 2004/08/31 14:48:26 thb 1.1.2.1: #110496# Broke up transitionfactory.cxx into two parts

---
+++ slideshow/source/engine/transitions/shapetransitionfactory.cxx
@@ -0,0 +1,396 @@
+/*************************************************************************
+ *
+ *  $RCSfile: shapetransitionfactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 19:08:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <basegfx/numeric/ftools.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/polygon/b2dpolypolygontools.hxx>
+#include <canvas/debug.hxx>
+#include <transitionfactory.hxx>
+#include <transitiontools.hxx>
+#include <parametricpolypolygonfactory.hxx>
+#include <animationfactory.hxx>
+#include <clippingfunctor.hxx>
+#include <com/sun/star/animations/TransitionType.hpp>
+#include <com/sun/star/animations/TransitionSubType.hpp>
+
+#ifndef BOOST_BIND_HPP_INCLUDED
+#include <boost/bind.hpp>
+#endif
+
+
+using namespace ::com::sun::star;
+
+namespace presentation {
+namespace internal {
+
+/***************************************************
+ ***                                             ***
+ ***          Shape Transition Effects           ***
+ ***                                             ***
+ ***************************************************/
+
+namespace {
+
+class ClippingAnimation : public NumberAnimation
+{
+public:
+    ClippingAnimation(
+        const ParametricPolyPolygonSharedPtr&   rPolygon,
+        const LayerManagerSharedPtr&            rLayerManager,
+        const TransitionInfo&                   rTransitionInfo,
+        bool                                    bDirectionForward,
+        bool                                    bModeIn );
+
+    // Animation interface
+    // -------------------
+    virtual void start( const AnimatableShapeSharedPtr&     rShape,
+                        const ShapeAttributeLayerSharedPtr& rAttrLayer );
+    virtual void end();
+
+    // NumberAnimation interface
+    // -----------------------
+    virtual bool operator()( double nValue );
+    virtual double getUnderlyingValue() const;
+
+private:
+    AnimatableShapeSharedPtr           mpShape;
+    ShapeAttributeLayerSharedPtr       mpAttrLayer;
+    LayerManagerSharedPtr              mpLayerManager;
+    ClippingFunctor                    maClippingFunctor;
+    bool                               mbSpriteActive;
+};
+
+ClippingAnimation::ClippingAnimation(
+    const ParametricPolyPolygonSharedPtr&   rPolygon,
+    const LayerManagerSharedPtr&            rLayerManager,
+    const TransitionInfo&                   rTransitionInfo,
+    bool                                    bDirectionForward,
+    bool                                    bModeIn ) :
+        mpShape(),
+        mpAttrLayer(),
+        mpLayerManager( rLayerManager ),
+        maClippingFunctor( rPolygon,
+                           rTransitionInfo,
+                           bDirectionForward,
+                           bModeIn ),
+        mbSpriteActive(false)
+{
+    ENSURE_AND_THROW(
+        rLayerManager.get(),
+        "ClippingAnimation::ClippingAnimation(): Invalid LayerManager" );
+}
+
+void ClippingAnimation::start( const AnimatableShapeSharedPtr&      rShape,
+                               const ShapeAttributeLayerSharedPtr&  rAttrLayer )
+{
+    OSL_ENSURE( !mpShape.get(),
+                "ClippingAnimation::start(): Shape already set" );
+    OSL_ENSURE( !mpAttrLayer.get(),
+                "ClippingAnimation::start(): Attribute layer already set" );
+
+    mpShape = rShape;
+    mpAttrLayer = rAttrLayer;
+
+    ENSURE_AND_THROW( rShape.get(),
+                      "ClippingAnimation::start(): Invalid shape" );
+    ENSURE_AND_THROW( rAttrLayer.get(),
+                      "ClippingAnimation::start(): Invalid attribute layer" );
+
+    mpShape = rShape;
+    mpAttrLayer = rAttrLayer;
+
+    if( !mbSpriteActive )
+    {
+        mpLayerManager->enterAnimationMode( mpShape );
+        mbSpriteActive = true;
+    }
+}
+
+void ClippingAnimation::end()
+{
+    if( mbSpriteActive )
+    {
+        mbSpriteActive = false;
+        mpLayerManager->leaveAnimationMode( mpShape );
+    }
+}
+
+bool ClippingAnimation::operator()( double nValue )
+{
+    ENSURE_AND_RETURN(
+        mpAttrLayer.get() && mpShape.get(),
+        "ClippingAnimation::operator(): Invalid ShapeAttributeLayer" );
+
+    // set new clip
+    mpAttrLayer->setClip( maClippingFunctor( nValue,
+                                             mpShape->getUpdateArea().getRange() ) );
+
+    if( mpShape->isUpdateNecessary() )
+        mpLayerManager->notifyShapeUpdate( mpShape );
+
+    return true;
+}
+
+double ClippingAnimation::getUnderlyingValue() const
+{
+    ENSURE_AND_THROW(
+        mpAttrLayer.get(),
+        "ClippingAnimation::getUnderlyingValue(): Invalid ShapeAttributeLayer" );
+
+    return 0.0;     // though this should be used in concert with
+                    // ActivitiesFactory::createSimpleActivity, better
+                    // explicitely name our start value.
+                    // Permissible range for operator() above is [0,1]
+}
+
+} // anon namespace
+
+
+AnimationActivitySharedPtr TransitionFactory::createShapeTransition(
+    const ActivitiesFactory::CommonParameters&          rParms,
+    const AnimatableShapeSharedPtr&                     rShape,
+    const LayerManagerSharedPtr&                        rLayerManager,
+    uno::Reference< animations::XTransitionFilter >&    xTransition )
+{
+    return createShapeTransition( rParms,
+                                  rShape,
+                                  rLayerManager,
+                                  xTransition,
+                                  xTransition->getTransition(),
+                                  xTransition->getSubtype() );
+}
+
+AnimationActivitySharedPtr TransitionFactory::createShapeTransition(
+    const ActivitiesFactory::CommonParameters&              rParms,
+    const AnimatableShapeSharedPtr&                         rShape,
+    const LayerManagerSharedPtr&                            rLayerManager,
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::animations::XTransitionFilter >&  xTransition,
+    sal_Int16                                               nType,
+    sal_Int16                                               nSubType )
+{
+    ENSURE_AND_THROW(
+        xTransition.is(),
+        "TransitionFactory::createShapeTransition(): Invalid XTransition" );
+
+    const TransitionInfo* pTransitionInfo(
+        getTransitionInfo( nType, nSubType ) );
+
+    AnimationActivitySharedPtr pGeneratedActivity;
+    if( pTransitionInfo != NULL )
+    {
+        switch( pTransitionInfo->meTransitionClass )
+        {
+            default:
+            case TransitionInfo::TRANSITION_INVALID:
+                OSL_ENSURE( false,
+                            "TransitionFactory::createShapeTransition(): Invalid transition type. "
+                            "Don't ask me for a 0 TransitionType, have no XTransitionFilter node instead!" );
+                return AnimationActivitySharedPtr();
+
+
+            case TransitionInfo::TRANSITION_CLIP_POLYPOLYGON:
+            {
+                // generate parametric poly-polygon
+                ParametricPolyPolygonSharedPtr pPoly(
+                    ParametricPolyPolygonFactory::createClipPolyPolygon(
+                        nType, nSubType ) );
+
+                // create a clip activity from that
+                pGeneratedActivity = ActivitiesFactory::createSimpleActivity(
+                    rParms,
+                    NumberAnimationSharedPtr(
+                        new ClippingAnimation(
+                            pPoly,
+                            rLayerManager,
+                            *pTransitionInfo,
+                            xTransition->getDirection(),
+                            xTransition->getMode() ) ),
+                    true );
+            }
+            break;
+
+            case TransitionInfo::TRANSITION_SPECIAL:
+            {
+                switch( nType )
+                {
+                    case animations::TransitionType::RANDOM:
+                    {
+                        // select randomly one of the effects from the
+                        // TransitionFactoryTable
+
+                        const TransitionInfo* pRandomTransitionInfo( getRandomTransitionInfo() );
+
+                        ENSURE_AND_THROW( pRandomTransitionInfo != NULL,
+                                          "TransitionFactory::createShapeTransition(): Got invalid random transition info" );
+
+                        ENSURE_AND_THROW( pRandomTransitionInfo->mnTransitionType != animations::TransitionType::RANDOM,
+                                          "TransitionFactory::createShapeTransition(): Got random again for random input!" );
+
+                        // and recurse
+                        pGeneratedActivity = createShapeTransition( rParms,
+                                                                    rShape,
+                                                                    rLayerManager,
+                                                                    xTransition,
+                                                                    pRandomTransitionInfo->mnTransitionType,
+                                                                    pRandomTransitionInfo->mnTransitionSubType );
+                    }
+                    break;
+
+                    // TODO(F3): Implement slidewipe for shape
+                    case animations::TransitionType::SLIDEWIPE:
+                    {
+                        sal_Int16 nBarWipeSubType(0);
+                        bool      bDirectionForward(true);
+
+                        // map slidewipe to BARWIPE, for now
+                        switch( nSubType )
+                        {
+                            case animations::TransitionSubType::FROMLEFT:
+                                nBarWipeSubType = animations::TransitionSubType::LEFTTORIGHT;
+                                bDirectionForward = true;
+                                break;
+
+                            case animations::TransitionSubType::FROMRIGHT:
+                                nBarWipeSubType = animations::TransitionSubType::LEFTTORIGHT;
+                                bDirectionForward = false;
+                                break;
+
+                            case animations::TransitionSubType::FROMTOP:
+                                nBarWipeSubType = animations::TransitionSubType::TOPTOBOTTOM;
+                                bDirectionForward = true;
+                                break;
+
+                            case animations::TransitionSubType::FROMBOTTOM:
+                                nBarWipeSubType = animations::TransitionSubType::TOPTOBOTTOM;
+                                bDirectionForward = false;
+                                break;
+
+                            default:
+                                ENSURE_AND_THROW( false,
+                                                  "TransitionFactory::createShapeTransition(): Unexpected subtype for SLIDEWIPE" );
+                                break;
+                        }
+
+                        // generate parametric poly-polygon
+                        ParametricPolyPolygonSharedPtr pPoly(
+                            ParametricPolyPolygonFactory::createClipPolyPolygon(
+                                animations::TransitionType::BARWIPE,
+                                nBarWipeSubType ) );
+
+                        // create a clip activity from that
+                        pGeneratedActivity = ActivitiesFactory::createSimpleActivity(
+                            rParms,
+                            NumberAnimationSharedPtr(
+                                new ClippingAnimation(
+                                    pPoly,
+                                    rLayerManager,
+                                    *getTransitionInfo( animations::TransitionType::BARWIPE,
+                                                        nBarWipeSubType ),
+                                    bDirectionForward,
+                                    xTransition->getMode() ) ),
+                            true );
+                    }
+                    break;
+
+                    default:
+                    {
+                        // TODO(F1): Check whether there's anything left, anyway,
+                        // for _shape_ transitions. AFAIK, there are no special
+                        // effects for shapes...
+
+                        // for now, map all to fade effect
+                        pGeneratedActivity = ActivitiesFactory::createSimpleActivity(
+                            rParms,
+                            AnimationFactory::createNumberPropertyAnimation(
+                                ::rtl::OUString(
+                                    RTL_CONSTASCII_USTRINGPARAM("Opacity") ),
+                                rShape,
+                                rLayerManager ),
+                            xTransition->getMode() );
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    if( !pGeneratedActivity.get() )
+    {
+        // No animation generated, maybe no table entry for given
+        // transition?
+        OSL_TRACE(
+            "TransitionFactory::createShapeTransition(): Unknown type/subtype (%d/%d) "
+            "combination encountered",
+            xTransition->getTransition(),
+            xTransition->getSubtype() );
+        OSL_ENSURE(
+            false,
+            "TransitionFactory::createShapeTransition(): Unknown type/subtype "
+            "combination encountered" );
+    }
+
+    return pGeneratedActivity;
+}
+
+}
+}
