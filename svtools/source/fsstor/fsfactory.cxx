d88e0aadec63ea80e0ed05a864cc1969a00a62a8
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-26 19:38:40 +0000

    INTEGRATION: CWS storagechange01 (1.1.2); FILE ADDED 2004/11/01 09:36:37 mav 1.1.2.1: #i26882# file system storages implementation

---
+++ svtools/source/fsstor/fsfactory.cxx
@@ -0,0 +1,304 @@
+/*************************************************************************
+ *
+ *  $RCSfile: fsfactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2004-11-26 20:38:40 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _COM_SUN_STAR_UCB_XSIMPLEFILEACCESS_HPP_
+#include <com/sun/star/ucb/XSimpleFileAccess.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
+#include <com/sun/star/embed/ElementModes.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_IO_XSEEKABLE_HPP_
+#include <com/sun/star/io/XSeekable.hpp>
+#endif
+
+
+#include <ucbhelper/fileidentifierconverter.hxx>
+#include <ucbhelper/contentbroker.hxx>
+#include <ucbhelper/content.hxx>
+
+#include <unotools/tempfile.hxx>
+#include <unotools/ucbhelper.hxx>
+
+#include <fsfactory.hxx>
+#include "fsstorage.hxx"
+
+
+using namespace ::com::sun::star;
+
+//-------------------------------------------------------------------------
+// Checks whether it is a file URL that represents a folder
+sal_Bool isLocalNotFile_Impl( ::rtl::OUString aURL )
+{
+    sal_Bool bResult = sal_False;
+
+    ::rtl::OUString aSystemPath;
+    ::ucb::ContentBroker* pBroker = ::ucb::ContentBroker::get();
+    if ( !pBroker )
+        throw uno::RuntimeException();
+
+    uno::Reference< ::com::sun::star::ucb::XContentProviderManager > xManager =
+            pBroker->getContentProviderManagerInterface();
+    try
+    {
+           aSystemPath = ::ucb::getSystemPathFromFileURL( xManager, aURL );
+    }
+    catch ( uno::Exception& )
+    {
+    }
+
+    if ( aSystemPath.getLength() != 0 )
+    {
+        // it is a local file URL, check that it is not a file
+        try
+        {
+            uno::Reference< ::com::sun::star::ucb::XCommandEnvironment > xDummyEnv;
+            ::ucb::Content aContent( aURL, xDummyEnv );
+            bResult = aContent.isFolder();
+        }
+        catch( uno::Exception& )
+        {
+            bResult = sal_True;
+        }
+    }
+
+    return bResult;
+}
+
+//-------------------------------------------------------------------------
+uno::Sequence< ::rtl::OUString > SAL_CALL FSStorageFactory::impl_staticGetSupportedServiceNames()
+{
+    uno::Sequence< ::rtl::OUString > aRet(2);
+    aRet[0] = ::rtl::OUString::createFromAscii("com.sun.star.embed.FileSystemStorageFactory");
+    aRet[1] = ::rtl::OUString::createFromAscii("com.sun.star.comp.embed.FileSystemStorageFactory");
+    return aRet;
+}
+
+//-------------------------------------------------------------------------
+::rtl::OUString SAL_CALL FSStorageFactory::impl_staticGetImplementationName()
+{
+    return ::rtl::OUString::createFromAscii("com.sun.star.comp.embed.FileSystemStorageFactory");
+}
+
+//-------------------------------------------------------------------------
+uno::Reference< uno::XInterface > SAL_CALL FSStorageFactory::impl_staticCreateSelfInstance(
+            const uno::Reference< lang::XMultiServiceFactory >& xServiceManager )
+{
+    return uno::Reference< uno::XInterface >( *new FSStorageFactory( xServiceManager ) );
+}
+
+//-------------------------------------------------------------------------
+uno::Reference< uno::XInterface > SAL_CALL FSStorageFactory::createInstance()
+    throw ( uno::Exception,
+            uno::RuntimeException )
+{
+    ::rtl::OUString aTempURL;
+
+    aTempURL = ::utl::TempFile( NULL, sal_True ).GetURL();
+
+    if ( !aTempURL.getLength() )
+        throw uno::RuntimeException(); // TODO: can not create tempfile
+
+    ::ucb::Content aResultContent( aTempURL, uno::Reference< ::com::sun::star::ucb::XCommandEnvironment >() );
+
+    return uno::Reference< uno::XInterface >(
+                static_cast< OWeakObject* >( new FSStorage( aResultContent,
+                                                            embed::ElementModes::READWRITE,
+                                                            uno::Sequence< beans::PropertyValue >(),
+                                                            m_xFactory ) ),
+                uno::UNO_QUERY );
+}
+
+//-------------------------------------------------------------------------
+uno::Reference< uno::XInterface > SAL_CALL FSStorageFactory::createInstanceWithArguments(
+            const uno::Sequence< uno::Any >& aArguments )
+    throw ( uno::Exception,
+            uno::RuntimeException )
+{
+    // The request for storage can be done with up to three arguments
+
+    // The first argument specifies a source for the storage
+    // it must be URL.
+    // The second value is a mode the storage should be open in.
+    // And the third value is a media descriptor.
+
+    sal_Int32 nArgNum = aArguments.getLength();
+    OSL_ENSURE( nArgNum < 4, "Wrong parameter number" );
+
+    if ( !nArgNum )
+        return createInstance();
+
+    // first try to retrieve storage open mode if any
+    // by default the storage will be open in readonly mode
+    sal_Int32 nStorageMode = embed::ElementModes::READ;
+    if ( nArgNum >= 2 )
+    {
+        if( !( aArguments[1] >>= nStorageMode ) )
+        {
+            OSL_ENSURE( sal_False, "Wrong second argument!\n" );
+            throw uno::Exception(); // TODO: Illegal argument
+        }
+        // it's allways possible to read written storage in this implementation
+        nStorageMode |= embed::ElementModes::READ;
+    }
+
+    // retrieve storage source URL
+    ::rtl::OUString aURL;
+
+    if ( aArguments[0] >>= aURL )
+    {
+        if ( !aURL.getLength() )
+        {
+            OSL_ENSURE( sal_False, "Empty URL is provided!\n" );
+            throw uno::Exception(); // TODO: illegal argument
+        }
+    }
+    else
+    {
+        OSL_ENSURE( sal_False, "Wrong first argument!\n" );
+        throw uno::Exception(); // TODO: Illegal argument
+    }
+
+    // retrieve mediadescriptor and set storage properties
+    uno::Sequence< beans::PropertyValue > aDescr;
+    uno::Sequence< beans::PropertyValue > aPropsToSet;
+
+    if ( nArgNum >= 3 )
+    {
+        if( aArguments[2] >>= aDescr )
+        {
+            aPropsToSet.realloc(1);
+            aPropsToSet[0].Name = ::rtl::OUString::createFromAscii( "URL" );
+            aPropsToSet[0].Value <<= aURL;
+
+            for ( sal_Int32 nInd = 0, nNumArgs = 1; nInd < aDescr.getLength(); nInd++ )
+            {
+                if ( aDescr[nInd].Name.equalsAscii( "InteractionHandler" ) )
+                {
+                    aPropsToSet.realloc( ++nNumArgs );
+                    aPropsToSet[nNumArgs-1].Name = aDescr[nInd].Name;
+                    aPropsToSet[nNumArgs-1].Value = aDescr[nInd].Value;
+                    break;
+                }
+                else
+                    OSL_ENSURE( sal_False, "Unacceptable property, will be ignored!\n" );
+            }
+        }
+        else
+        {
+            OSL_ENSURE( sal_False, "Wrong third argument!\n" );
+            throw uno::Exception(); // TODO: Illegal argument
+        }
+    }
+
+    // allow to use other ucp's
+    // if ( !isLocalNotFile_Impl( aURL ) )
+    if ( aURL.equalsIgnoreAsciiCaseAsciiL( "vnd.sun.star.pkg", 16 )
+      || aURL.equalsIgnoreAsciiCaseAsciiL( "vnd.sun.star.zip", 16 )
+      || ::utl::UCBContentHelper::IsDocument( aURL ) )
+    {
+        OSL_ENSURE( sal_False, "File system storages can be based only on file URLs!\n" ); // ???
+        throw uno::Exception(); // TODO: illegal argument
+    }
+
+    if ( nStorageMode & embed::ElementModes::WRITE )
+        FSStorage::MakeFolderNoUI( aURL, sal_False );
+
+    ::ucb::Content aResultContent( aURL, uno::Reference< ::com::sun::star::ucb::XCommandEnvironment >() );
+
+    // create storage based on source
+    return uno::Reference< uno::XInterface >(
+                static_cast< OWeakObject* >( new FSStorage( aResultContent, nStorageMode, aPropsToSet, m_xFactory ) ),
+                uno::UNO_QUERY );
+}
+
+//-------------------------------------------------------------------------
+::rtl::OUString SAL_CALL FSStorageFactory::getImplementationName()
+    throw ( uno::RuntimeException )
+{
+    return impl_staticGetImplementationName();
+}
+
+//-------------------------------------------------------------------------
+sal_Bool SAL_CALL FSStorageFactory::supportsService( const ::rtl::OUString& ServiceName )
+    throw ( uno::RuntimeException )
+{
+    uno::Sequence< ::rtl::OUString > aSeq = impl_staticGetSupportedServiceNames();
+
+    for ( sal_Int32 nInd = 0; nInd < aSeq.getLength(); nInd++ )
+        if ( ServiceName.compareTo( aSeq[nInd] ) == 0 )
+            return sal_True;
+
+    return sal_False;
+}
+
+//-------------------------------------------------------------------------
+uno::Sequence< ::rtl::OUString > SAL_CALL FSStorageFactory::getSupportedServiceNames()
+    throw ( uno::RuntimeException )
+{
+    return impl_staticGetSupportedServiceNames();
+}
+
