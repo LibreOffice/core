76c9e063b60b2085c014e9fe5dd54070cd26e285
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:03:28 +0000

    INTEGRATION: CWS presentationengine01 (1.2.2); FILE MERGED 2004/05/27 14:36:52 thb 1.2.2.2: #110496# verbose tracing is now guarded also with DBG_UTIL. Unified verbose trace output between directx and VCL canvas 2004/04/05 15:57:53 thb 1.2.2.1: Resync with canvas01 changes

--- canvas/inc/canvas/verbosetrace.hxx
+++ canvas/inc/canvas/verbosetrace.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: verbosetrace.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: thb $ $Date: 2004-03-18 10:38:26 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:03:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,7 @@
 #ifndef _CANVAS_VERBOSETRACE_HXX
 #define _CANVAS_VERBOSETRACE_HXX
 
-#ifdef VERBOSE
+#if defined(VERBOSE) && defined(DBG_UTIL)
 /// Wrap OSL_TRACE with a verbosity switch
 #define VERBOSE_TRACE     OSL_TRACE
 #else
