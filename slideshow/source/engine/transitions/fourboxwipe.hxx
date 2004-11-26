53105e6712c4b6ad5ae3054f05cf62fc2bfbf52e
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:06:17 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/08/13 14:47:41 dbo 1.1.2.3: #110496# more wipes Issue number: Submitted by: Reviewed by: 2004/08/11 17:02:11 dbo 1.1.2.2: #110496# more transitions, modified file/class structure Issue number: Submitted by: Reviewed by: 2004/08/10 16:43:08 dbo 1.1.2.1: #110496# added transistions Issue number: Submitted by: Reviewed by:

---
+++ slideshow/source/engine/transitions/fourboxwipe.hxx
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *
+ *  $RCSfile: fourboxwipe.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 19:06:17 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#if ! defined INCLUDED_SLIDESHOW_FOURBOXWIPE_HXX
+#define INCLUDED_SLIDESHOW_FOURBOXWIPE_HXX
+
+#include "parametricpolypolygon.hxx"
+#include "transitiontools.hxx"
+#include "basegfx/polygon/b2dpolygon.hxx"
+
+
+namespace presentation {
+namespace internal {
+
+/// Generate a 4-box wipe
+class FourBoxWipe : public ParametricPolyPolygon
+{
+public:
+    FourBoxWipe( bool cornersOut ) : m_cornersOut(cornersOut),
+                                     m_unitRect( createUnitRect() )
+        {}
+    virtual ::basegfx::B2DPolyPolygon operator () ( double t );
+private:
+    const bool m_cornersOut;
+    const ::basegfx::B2DPolygon m_unitRect;
+};
+
+}
+}
+
+#endif /* INCLUDED_SLIDESHOW_FOURBOXWIPE_HXX */
