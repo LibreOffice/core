d5a7ed0bd517a81dcd669ead4443043da3dc5a96
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:24:00 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/08/05 20:00:01 thb 1.1.2.3: #110496# Moved from source/engine 2004/08/01 20:08:14 thb 1.1.2.2: #110496# Changed slideshow API to match the needs of sd more closely 2004/07/22 19:34:50 thb 1.1.2.1: #110496# Initial revision

---
+++ slideshow/source/inc/viewlayer.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ *  $RCSfile: viewlayer.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 19:24:00 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SLIDESHOW_VIEWLAYER_HXX
+#define _SLIDESHOW_VIEWLAYER_HXX
+
+#ifndef _CPPCANVAS_SPRITECANVAS_HXX
+#include <cppcanvas/spritecanvas.hxx>
+#endif
+
+#ifndef _BGFX_RANGE_B2IRECTANGLE_HXX
+#include <basegfx/range/b2irectangle.hxx>
+#endif
+
+#ifndef BOOST_SHARED_PTR_HPP_INCLUDED
+#include <boost/shared_ptr.hpp>
+#endif
+
+
+/* Definition of ViewLayer interface */
+
+namespace presentation
+{
+    namespace internal
+    {
+        class ViewLayer
+        {
+        public:
+            virtual ~ViewLayer() {}
+
+            /** Get the associated canvas of this layer.
+
+                The canvas returned by this method must not change, as
+                long as this object is alive.
+            */
+            virtual ::cppcanvas::CanvasSharedPtr getCanvas() const = 0;
+
+            /** Create a sprite for this layer
+
+                @param rSpriteSizePixel
+                Sprite size in device pixel
+
+                @return the sprite, or NULL on failure (or if this canvas
+                does not support sprites)
+            */
+            virtual ::cppcanvas::CustomSpriteSharedPtr createSprite( const ::basegfx::B2DSize& rSpriteSizePixel ) const = 0;
+
+            /** Compute a sprite priority relative to this layer.
+
+                @param nSpritePrio
+                Priority of the Sprite within this layer
+
+                @return absolute sprite priority, to be set at a ::cppcanvas::Sprite
+            */
+            virtual double getSpritePriority( double nSpritePrio ) const = 0;
+
+            /** Set the layer priority
+
+                This method influences the relative priority of this
+                layer, i.e. the z position in relation to other layers on
+                the parent view. The higher the priority, the further in front
+                the layer resides.
+
+                @param nPrio
+                Priority, must be in the range [0,1]
+            */
+            virtual void setPriority( double nPrio ) = 0;
+        };
+
+        typedef ::boost::shared_ptr< ViewLayer > ViewLayerSharedPtr;
+    }
+}
+
+#endif /* _SLIDESHOW_VIEWLAYER_HXX */
