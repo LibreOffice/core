cdd6917fedd03559fc87d0cf7fadcea23e912576
Rüdiger Timm <rt@openoffice.org>
2004-11-26 15:18:22 +0000

    INTEGRATION: CWS leanobjects (1.19.74); FILE MERGED 2004/11/18 11:11:09 mba 1.19.74.1: #i37278#: make objects loadable on demand

--- sd/source/ui/view/sdview4.cxx
+++ sd/source/ui/view/sdview4.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sdview4.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: kz $ $Date: 2004-10-04 18:47:31 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 16:18:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -491,6 +491,9 @@ IMPL_LINK( View, DropInsertFileHdl, Timer*, pTimer )
                     uno::Reference < embed::XEmbedPersist > xPersist( xObj, uno::UNO_QUERY );
                     if ( xPersist.is())
                     {
+                        // TODO/LEAN: VisualArea currently needs running state
+                        svt::EmbeddedObjectRef::TryRunningState( xObj );
+
                         sal_Int64 nAspect = embed::Aspects::MSOLE_CONTENT;
                         xPersist->storeOwn();
 
