e812993267e57c4640cded03ca7a162bb695b4a3
Rüdiger Timm <rt@openoffice.org>
2004-11-26 15:23:41 +0000

    INTEGRATION: CWS cd03 (1.24.58); FILE MERGED 2004/11/17 17:31:19 mav 1.24.58.1: #i37232# allow to provide some arguments on save operation

--- offapi/com/sun/star/frame/makefile.mk
+++ offapi/com/sun/star/frame/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.26 $
+#   $Revision: 1.27 $
 #
-#   last change: $Author: obo $ $Date: 2004-11-17 14:31:10 $
+#   last change: $Author: rt $ $Date: 2004-11-26 16:23:41 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -158,6 +158,7 @@ IDLFILES=\
     XStatusbarController.idl\
     XStorable.idl\
     XSubToolbarController.idl\
+    XStorable2.idl\
     XSynchronousDispatch.idl\
     XSynchronousFrameLoader.idl\
     XTask.idl\
