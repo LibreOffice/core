9c465c76490774f33f6687231f6fdefef8368a2b
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:08:48 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/11/17 17:00:26 thb 1.1.2.4: #118514# Canvas module reorg 2004/10/26 23:36:57 thb 1.1.2.3: #110496# Added dedicated alpha bitmap support: factory methods at the XGraphicDevice, and optimized handling internally 2004/09/27 20:12:30 thb 1.1.2.2: #110496# Fixed problem with CanvasBitmap instances with alpha channel (simply ignored that before) 2004/07/20 19:22:44 thb 1.1.2.1: #110496# Removed self-references to various interface implementations, along the lines, factored out common base implementation for all c++ canvases

---
+++ canvas/source/vcl/backbuffer.hxx
@@ -0,0 +1,108 @@
+/*************************************************************************
+ *
+ *  $RCSfile: backbuffer.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:08:48 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _VCLCANVAS_BACKBUFFER_HXX_
+#define _VCLCANVAS_BACKBUFFER_HXX_
+
+#ifndef _SV_VIRDEV_HXX
+#include <vcl/virdev.hxx>
+#endif
+
+#include <canvas/vclwrapper.hxx>
+
+#include "outdevprovider.hxx"
+
+
+namespace vclcanvas
+{
+    /// OutDevProvider implementation for backbuffer VDev
+    class BackBuffer : public OutDevProvider
+    {
+    public:
+        /** Create a backbuffer for given reference device
+
+            @param bMonochromeBuffer
+            When false, default depth of reference device is
+            chosen. When true, the buffer will be monochrome, i.e. one
+            bit deep.
+
+            @param bAlphaVDev
+            If true, the generated backbuffer will have an alpha channel
+         */
+        BackBuffer( const OutputDevice& rRefDevice,
+                    bool                bMonochromeBuffer=false,
+                    bool                bAlphaVDev=false );
+
+        virtual OutputDevice&           getOutDev();
+        virtual const OutputDevice&     getOutDev() const;
+
+        VirtualDevice&                  getVirDev();
+        const VirtualDevice&            getVirDev() const;
+
+    private:
+        ::canvas::vcltools::VCLObject<VirtualDevice>    maVDev;
+    };
+
+    typedef ::boost::shared_ptr< BackBuffer > BackBufferSharedPtr;
+
+}
+
+#endif /* #ifndef _VCLCANVAS_BACKBUFFER_HXX_ */
