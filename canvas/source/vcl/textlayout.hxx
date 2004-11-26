978a3a880adfa59cec99d20e3bd4d8db0f8ca762
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:16:16 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/11/17 17:00:35 thb 1.1.2.3: #118514# Canvas module reorg 2004/07/20 19:23:58 thb 1.1.2.2: #110496# Removed self-references to various interface implementations, along the lines, factored out common base implementation for all c++ canvases 2004/04/12 15:11:38 thb 1.1.2.1: Initial revision

---
+++ canvas/source/vcl/textlayout.hxx
@@ -0,0 +1,157 @@
+/*************************************************************************
+ *
+ *  $RCSfile: textlayout.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:16:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _TEXTLAYOUT_HXX
+#define _TEXTLAYOUT_HXX
+
+#ifndef _CPPUHELPER_COMPBASE2_HXX_
+#include <cppuhelper/compbase2.hxx>
+#endif
+#ifndef _COMPHELPER_BROADCASTHELPER_HXX_
+#include <comphelper/broadcasthelper.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XTEXTLAYOUT_HPP_
+#include <drafts/com/sun/star/rendering/XTextLayout.hpp>
+#endif
+
+#include <canvas/vclwrapper.hxx>
+
+#include "canvasfont.hxx"
+#include "impltools.hxx"
+
+
+#define TEXTLAYOUT_IMPLEMENTATION_NAME "VCLCanvas::TextLayout"
+
+/* Definition of TextLayout class */
+
+namespace vclcanvas
+{
+    typedef ::cppu::WeakComponentImplHelper2< ::drafts::com::sun::star::rendering::XTextLayout,
+                                               ::com::sun::star::lang::XServiceInfo > TextLayout_Base;
+
+    class TextLayout : public ::comphelper::OBaseMutex, public TextLayout_Base
+    {
+    public:
+        TextLayout( const ::drafts::com::sun::star::rendering::StringContext&   aText,
+                    sal_Int8                                                    nDirection,
+                    sal_Int64                                                   nRandomSeed,
+                    const CanvasFont::ImplRef&                                  rFont,
+                    const OutDevProviderSharedPtr&                              rRefDevice );
+
+        /// Dispose all internal references
+        virtual void SAL_CALL disposing();
+
+        // XTextLayout
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > > SAL_CALL queryTextShapes(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryInkMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< double > SAL_CALL queryLogicalAdvancements(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual void SAL_CALL applyLogicalAdvancements( const ::com::sun::star::uno::Sequence< double >& aAdvancements ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual ::drafts::com::sun::star::geometry::RealRectangle2D SAL_CALL queryTextBounds(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual double SAL_CALL justify( double nSize ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual double SAL_CALL combinedJustify( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XTextLayout > >& aNextLayouts, double nSize ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual ::drafts::com::sun::star::rendering::TextHit SAL_CALL getTextHit( const ::drafts::com::sun::star::geometry::RealPoint2D& aHitPoint ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::drafts::com::sun::star::rendering::Caret SAL_CALL getCaret( sal_Int32 nInsertionIndex, sal_Bool bExcludeLigatures ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Int32 SAL_CALL getNextInsertionIndex( sal_Int32 nStartIndex, sal_Int32 nCaretAdvancement, sal_Bool bExcludeLigatures ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryVisualHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryLogicalHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual double SAL_CALL getBaselineOffset(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Int8 SAL_CALL getMainTextDirection(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XCanvasFont > SAL_CALL getFont(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::drafts::com::sun::star::rendering::StringContext SAL_CALL getText(  ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XServiceInfo
+        virtual ::rtl::OUString SAL_CALL getImplementationName() throw( ::com::sun::star::uno::RuntimeException );
+        virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) throw( ::com::sun::star::uno::RuntimeException );
+        virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames()  throw( ::com::sun::star::uno::RuntimeException );
+
+        bool draw( OutputDevice&                                           rOutDev,
+                   const Point&                                            rOutpos,
+                   const ::drafts::com::sun::star::rendering::ViewState&   viewState,
+                   const ::drafts::com::sun::star::rendering::RenderState& renderState ) const;
+
+    protected:
+        ~TextLayout(); // we're a ref-counted UNO class. _We_ destroy ourselves.
+
+    private:
+        // default: disabled copy/assignment
+        TextLayout(const TextLayout&);
+        TextLayout& operator=( const TextLayout& );
+
+        void setupTextOffsets( long*                                                    outputOffsets,
+                               const ::com::sun::star::uno::Sequence< double >&         inputOffsets,
+                               const ::drafts::com::sun::star::rendering::ViewState&    viewState,
+                               const ::drafts::com::sun::star::rendering::RenderState&  renderState     ) const;
+
+        ::drafts::com::sun::star::rendering::StringContext maText;
+        ::com::sun::star::uno::Sequence< double >          maLogicalAdvancements;
+        CanvasFont::ImplRef                                mpFont;
+        OutDevProviderSharedPtr                            mpRefDevice;
+        sal_Int8                                           mnTextDirection;
+    };
+
+}
+
+#endif /* _TEXTLAYOUT_HXX */
