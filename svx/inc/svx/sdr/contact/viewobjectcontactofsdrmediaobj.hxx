3a453174daa53756350dcf571f729daeb0474355
Rüdiger Timm <rt@openoffice.org>
2004-11-26 17:11:22 +0000

    INTEGRATION: CWS presentationengine01 (1.2.6); FILE MERGED 2004/11/22 11:45:50 ka 1.2.6.1: correct handling of media repaint

--- svx/inc/svx/sdr/contact/viewobjectcontactofsdrmediaobj.hxx
+++ svx/inc/svx/sdr/contact/viewobjectcontactofsdrmediaobj.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: viewobjectcontactofsdrmediaobj.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-08-12 09:00:41 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 18:11:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -113,7 +113,6 @@ namespace sdr
 
         private:
 
-            Rectangle                       maLastPaintRect;
             ::sdr::contact::SdrMediaWindow* mpMediaWindow;
         };
     } // end of namespace contact
