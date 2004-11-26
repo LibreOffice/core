14a38068a9c50d786fcf93005acbec433ce68c1b
Rüdiger Timm <rt@openoffice.org>
2004-11-26 12:25:35 +0000

    INTEGRATION: CWS oasisbf2 (1.2.122); FILE MERGED 2004/10/20 06:58:35 od 1.2.122.1: #i35017# - adjustments for changed com::sun::star::text::WrapInfluenceOnPosition

--- sw/source/core/inc/sortedobjsimpl.hxx
+++ sw/source/core/inc/sortedobjsimpl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sortedobjsimpl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-08-02 14:06:58 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 13:25:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -69,9 +69,6 @@
 
 #ifndef _FMTANCHR_HXX
 #include <fmtanchr.hxx>
-#endif
-#ifndef _FMTWRAPINFLUENCEONOBJPOS_HXX
-#include <fmtwrapinfluenceonobjpos.hxx>
 #endif
 
 class SwAnchoredObject;
