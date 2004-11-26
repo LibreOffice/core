0df9a10d87145d9278eaacc0b925e771fa9a3552
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:06:28 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/11/06 19:58:01 thb 1.1.2.4: #110496# Moved pruneScaleValue to basegfx 2004/08/20 17:02:38 dbo 1.1.2.3: #110496# adopted polygon clipping order, added snake wipe, parallel snake wipe Issue number: Submitted by: Reviewed by: 2004/08/11 17:02:11 dbo 1.1.2.2: #110496# more transitions, modified file/class structure Issue number: Submitted by: Reviewed by: 2004/08/10 16:43:08 dbo 1.1.2.1: #110496# added transistions Issue number: Submitted by: Reviewed by:

---
+++ slideshow/source/engine/transitions/iriswipe.cxx
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *
+ *  $RCSfile: iriswipe.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 19:06:28 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "canvas/debug.hxx"
+#include "iriswipe.hxx"
+#include "basegfx/matrix/b2dhommatrix.hxx"
+
+
+namespace presentation {
+namespace internal {
+
+::basegfx::B2DPolyPolygon IrisWipe::operator () ( double t )
+{
+    ::basegfx::B2DHomMatrix aTransform;
+    aTransform.translate( -0.5, -0.5 );
+    const double d = ::basegfx::pruneScaleValue(t);
+    aTransform.scale( d, d );
+    aTransform.translate( 0.5, 0.5 );
+    ::basegfx::B2DPolyPolygon res( m_unitRect );
+    res.transform( aTransform );
+    return res;
+}
+
+}
+}
