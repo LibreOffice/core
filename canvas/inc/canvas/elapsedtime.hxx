5ed7177efde2e305dad7af3ed175315708c71475
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:02:42 +0000

    INTEGRATION: CWS presentationengine01 (1.2.2); FILE MERGED 2004/07/16 17:44:24 thb 1.2.2.2: #110496# Made ElapsedTime const-correct 2004/04/05 15:57:53 thb 1.2.2.1: Resync with canvas01 changes

--- canvas/inc/canvas/elapsedtime.hxx
+++ canvas/inc/canvas/elapsedtime.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: elapsedtime.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: thb $ $Date: 2004-03-18 10:38:25 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:02:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -101,7 +101,7 @@ namespace canvas
 
                 @return the elapsed time in seconds.
              */
-            double getElapsedTime();
+            double getElapsedTime() const;
 
         private:
             sal_uInt64  mnStartTime;    // implementation-dependent start time representation
