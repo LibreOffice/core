fcef4cdac59da15547dfb9d37b340a727cb46511
Kurt Zenker <kz@openoffice.org>
2004-11-26 15:00:51 +0000

    INTEGRATION: CWS nsplugin3 (1.3.24); FILE MERGED 2004/10/12 03:10:22 jmeng 1.3.24.1: add dynamic OpenOffice.org / StarOffice support

--- extensions/source/nsplugin/source/npshell.hxx
+++ extensions/source/nsplugin/source/npshell.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: npshell.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: mba $ $Date: 2004-09-02 14:01:05 $
+ *  last change: $Author: kz $ $Date: 2004-11-26 16:00:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,9 +58,6 @@
  *
  *
  ************************************************************************/
-
-#define PLUGIN_NAME         "StarOffice Plugin"
-#define PLUGIN_DESCRIPTION  "The StarOffice plugin handles all staroffice documents"
 
 #ifdef UNIX
 
