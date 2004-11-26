477fd6b8c6c013a843844d837cc3329aa37c563d
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:11:46 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/08/05 19:59:50 thb 1.1.2.1: #110496# Moved from source/engine

---
+++ slideshow/source/inc/activity.hxx
@@ -0,0 +1,131 @@
+/*************************************************************************
+ *
+ *  $RCSfile: activity.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 19:11:46 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SLIDESHOW_ACTIVITY_HXX
+#define _SLIDESHOW_ACTIVITY_HXX
+
+#ifndef _SAL_TYPES_H_
+#include <sal/types.h>
+#endif
+
+#ifndef BOOST_SHARED_PTR_HPP_INCLUDED
+#include <boost/shared_ptr.hpp>
+#endif
+
+#include <disposable.hxx>
+
+
+/* Definition of Activity interface */
+
+namespace presentation
+{
+    namespace internal
+    {
+
+        class Activity : public Disposable
+        {
+        public:
+            /** Perform the activity associated with this interface's
+                implementation.
+
+                @return true, if activity continues, or false, if activity has
+                ended.
+            */
+            virtual bool perform() = 0;
+
+            /** Query whether this activity is still continuing
+
+                @return true, if this activity still
+                continues. Returns false, if activity has ended. It is
+                required that operator() returns false, when
+                isActive() returns false. Furthermore, it is required
+                that the inactive state is persistent; an activity
+                that has become inactive (i.e. isActive() once
+                returned false) must stay in that state eternally.
+            */
+            virtual bool isActive() const = 0;
+
+            /** Query whether this activity, while active, needs a
+                screen update after perform() calls.
+
+                A prominent example for activities that need screen
+                updates are animations.
+
+                @return true, if this activity, while active, needs a
+                screen update after perform() calls.
+             */
+            virtual bool needsScreenUpdate() const = 0;
+
+            /** End the activity prematurely
+
+                This method ends the activity prematurely, i.e. before
+                time or number of iterations is reached. The next time
+                perform() or isActive() are called, false is returned.
+             */
+            virtual void end() = 0;
+        };
+
+        typedef ::boost::shared_ptr< Activity > ActivitySharedPtr;
+
+    }
+}
+
+#endif /* _SLIDESHOW_ACTIVITY_HXX */
