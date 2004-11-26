0b62cff074fee6c5d5015fd80fc541a210af36af
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:10:25 +0000

    INTEGRATION: CWS presentationengine01 (1.2.2); FILE MERGED 2004/11/17 17:00:27 thb 1.2.2.8: #118514# Canvas module reorg 2004/10/26 23:36:58 thb 1.2.2.7: #110496# Added dedicated alpha bitmap support: factory methods at the XGraphicDevice, and optimized handling internally 2004/09/30 15:10:03 thb 1.2.2.6: #110496# Temporarily removed creation of palette bitmaps for <=8 bit displays, VCL does not seem to handle 8bit+alpha correctly 2004/09/27 20:12:31 thb 1.2.2.5: #110496# Fixed problem with CanvasBitmap instances with alpha channel (simply ignored that before) 2004/07/20 19:23:54 thb 1.2.2.4: #110496# Removed self-references to various interface implementations, along the lines, factored out common base implementation for all c++ canvases 2004/05/27 20:51:24 thb 1.2.2.3: #110496# Added classification code to all TODO/HACK/FIXME comments. There are four categories:  - code quality (C)  - performance (P)  - missing functionality (F)  - and missing/incomplete error handling (E)
    Furthermore, every category has a severity number between
    1 and 3 associated, where 1 is lowest and 3 highest
    severity
    2004/04/12 15:12:24 thb 1.2.2.2: #110496# Adaptions after canvas01 merge
    2004/04/05 15:57:57 thb 1.2.2.1: Resync with canvas01 changes

--- canvas/source/vcl/canvasbitmap.cxx
+++ canvas/source/vcl/canvasbitmap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: canvasbitmap.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: thb $ $Date: 2004-03-18 10:38:39 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:10:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,369 +59,104 @@
  *
  ************************************************************************/
 
+#include <canvas/debug.hxx>
+#include "canvasbitmap.hxx"
+
+#ifndef _SV_BITMAPEX_HXX
+#include <vcl/bitmapex.hxx>
+#endif
 #ifndef _SV_BMPACC_HXX
 #include <vcl/bmpacc.hxx>
 #endif
-#ifndef _VCL_CANVASTOOLS_HXX
-#include <vcl/canvastools.hxx>
-#endif
-
-#include <canvas/canvastools.hxx>
-
-#include "canvasbitmap.hxx"
-#include "bitmapcanvas.hxx"
-#include "impltools.hxx"
 
 using namespace ::com::sun::star;
 using namespace ::drafts::com::sun::star;
+
 
 namespace vclcanvas
 {
     // Currently, the only way to generate an XBitmap is from
     // XGraphicDevice.getCompatibleBitmap(). Therefore, we don't even
     // take a bitmap here, but a VDev directly.
-    CanvasBitmap::CanvasBitmap( const ::Size&                   rSize,
-                                const OutDevProvider::ImplRef&  rReferenceCanvas ) :
-        mpReferenceCanvas( rReferenceCanvas ),
-        mpBitmapCanvas( new BitmapCanvas( rSize,
-                                          rReferenceCanvas ) )
+    CanvasBitmap::CanvasBitmap( const ::Size&                       rSize,
+                                bool                                bAlphaBitmap,
+                                const WindowGraphicDevice::ImplRef& rDevice )
     {
+        ENSURE_AND_THROW( rDevice->getOutDev(),
+                          "CanvasBitmap::CanvasBitmap(): Invalid reference outdev" );
+
+        OutputDevice&            rOutDev( *rDevice->getOutDev() );
+        tools::OutDevStateKeeper aStateKeeper( rOutDev );
+
+        // create bitmap for given reference device
+        // ========================================
+        const USHORT nBitCount( (USHORT)24U );
+        const BitmapPalette*    pPalette = NULL;
+
+#if 0
+        // TODO(P2): Seems like VCL doesn't handle palette bitmap with
+        // alpha channel. Devise other ways to handle this, or fix VCL
+        const USHORT nBitCount( ::std::min( (USHORT)24U,
+                                            (USHORT)rOutDev.GetBitCount() ) );
+
+        if( nBitCount <= 8 )
+        {
+            rOutDev.EnableMapMode( FALSE );
+
+            // try to determine palette from output device (by
+            // extracting a 1,1 bitmap, and querying it)
+            const Point aEmptyPoint;
+            const Size  aSize(1,1);
+            Bitmap aTmpBitmap( rOutDev.GetBitmap( aEmptyPoint,
+                                                  aSize ) );
+
+            ScopedBitmapReadAccess pReadAccess( aTmpBitmap.AcquireReadAccess(),
+                                                aTmpBitmap );
+
+            pPalette = &pReadAccess->GetPalette();
+        }
+#endif
+
+        Bitmap aBitmap( rSize, nBitCount, pPalette );
+
+        // only create alpha channel bitmap, if factory requested
+        // that. Providing alpha-channeled bitmaps by default has,
+        // especially under VCL, a huge performance penalty (have to
+        // use alpha VDev, then).
+        if( bAlphaBitmap )
+        {
+            AlphaMask   aAlpha ( rSize );
+
+            maCanvasHelper.setBitmap( BitmapEx( aBitmap, aAlpha ),
+                                      rDevice );
+        }
+        else
+        {
+            maCanvasHelper.setBitmap( BitmapEx( aBitmap ),
+                                      rDevice );
+        }
     }
 
-    CanvasBitmap::CanvasBitmap( const BitmapEx&                 rBitmap,
-                                const OutDevProvider::ImplRef&  rReferenceCanvas ) :
-        mpReferenceCanvas( rReferenceCanvas ),
-        mpBitmapCanvas( new BitmapCanvas( rBitmap.GetSizePixel(),
-                                          rReferenceCanvas ) )
+    CanvasBitmap::CanvasBitmap( const BitmapEx&                     rBitmap,
+                                const WindowGraphicDevice::ImplRef& rDevice )
     {
-        // TODO: Support alpha canvas here
-        VirtualDevice& rVDev( getVirDev() );
-        rVDev.EnableMapMode( FALSE );
-        const Point aEmptyPoint(0,0);
-        rVDev.DrawBitmapEx(aEmptyPoint, rBitmap);
+        ENSURE_AND_THROW( rDevice->getOutDev(),
+                          "CanvasBitmap::CanvasBitmap(): Invalid reference outdev" );
+
+        maCanvasHelper.setBitmap( rBitmap,
+                                  rDevice );
     }
 
     CanvasBitmap::~CanvasBitmap()
     {
     }
 
-    geometry::IntegerSize2D SAL_CALL CanvasBitmap::getSize(  ) throw (uno::RuntimeException)
-    {
-        tools::LocalGuard aGuard;
-        return ::vcl::unotools::integerSize2DFromSize( getVirDev().GetOutputSizePixel() );
-    }
-
-    uno::Reference< rendering::XBitmapCanvas > SAL_CALL CanvasBitmap::queryBitmapCanvas(  ) throw (uno::RuntimeException)
-    {
-        return mpBitmapCanvas.getRef();
-    }
-
-    uno::Reference< rendering::XBitmap > SAL_CALL CanvasBitmap::getScaledBitmap( const geometry::RealSize2D& newSize, sal_Bool beFast ) throw (uno::RuntimeException)
+    void SAL_CALL CanvasBitmap::disposing()
     {
         tools::LocalGuard aGuard;
 
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        BitmapEx aRes( *maBitmap );
-
-        aRes.Scale( ::canvas::tools::sizeFromRealSize2D(newSize),
-                     beFast ? BMP_SCALE_FAST : BMP_SCALE_INTERPOLATE );
-
-        return uno::Reference< rendering::XBitmap >( new CanvasBitmap( aRes ) );
-#else
-        // TODO: Support alpha canvas here
-        VirtualDevice& rVDev( getVirDev() );
-        const Point aEmptyPoint(0,0);
-        const Size  aBmpSize( rVDev.GetOutputSizePixel() );
-
-        Bitmap aBitmap( rVDev.GetBitmap(aEmptyPoint, aBmpSize) );
-
-        aBitmap.Scale( ::vcl::unotools::sizeFromRealSize2D(newSize),
-                       beFast ? BMP_SCALE_FAST : BMP_SCALE_INTERPOLATE );
-
-        return uno::Reference< rendering::XBitmap >( new CanvasBitmap( aBitmap,
-                                                                       mpReferenceCanvas ) );
-#endif
-    }
-
-    uno::Sequence< sal_Int8 > SAL_CALL CanvasBitmap::getData( const geometry::IntegerRectangle2D& rect ) throw (lang::IndexOutOfBoundsException, rendering::VolatileContentDestroyedException,uno::RuntimeException)
-    {
-        tools::LocalGuard aGuard;
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        Bitmap aBitmap( maBitmap->GetBitmap() );
-        Bitmap aAlpha( maBitmap->GetAlpha().GetBitmap() );
-#else
-        // TODO: Support alpha canvas here
-        VirtualDevice& rVDev( getVirDev() );
-        const Point aEmptyPoint(0,0);
-        const Size  aBmpSize( rVDev.GetOutputSizePixel() );
-
-        Bitmap aBitmap( rVDev.GetBitmap(aEmptyPoint, aBmpSize) );
-        Bitmap aAlpha( Bitmap( aBmpSize, 8) );
-#endif
-
-        ScopedBitmapReadAccess pReadAccess( aBitmap.AcquireReadAccess(),
-                                            aBitmap );
-        ScopedBitmapReadAccess pAlphaReadAccess( aAlpha.AcquireReadAccess(),
-                                                 aAlpha );
-
-        if( pReadAccess.get() != NULL &&
-            pAlphaReadAccess.get() != NULL )
-        {
-            // TODO: Support more formats.
-            const Size aBmpSize( aBitmap.GetSizePixel() );
-
-            // for the time being, always return as BGRA
-            uno::Sequence< sal_Int8 > aRes( 4*aBmpSize.Width()*aBmpSize.Height() );
-
-            int nCurrPos(0);
-            for( int y=rect.Y1;
-                 y<aBmpSize.Height() && y<rect.Y2;
-                 ++y )
-            {
-                for( int x=rect.X1;
-                     x<aBmpSize.Width() && x<rect.X2;
-                     ++x )
-                {
-                    aRes[ nCurrPos++ ] = pReadAccess->GetColor( y, x ).GetBlue();
-                    aRes[ nCurrPos++ ] = pReadAccess->GetColor( y, x ).GetGreen();
-                    aRes[ nCurrPos++ ] = pReadAccess->GetColor( y, x ).GetRed();
-                    aRes[ nCurrPos++ ] = pAlphaReadAccess->GetPixel( y, x ).GetIndex();
-                }
-            }
-
-            return aRes;
-        }
-
-        return uno::Sequence< sal_Int8 >();
-    }
-
-    void SAL_CALL CanvasBitmap::setData( const uno::Sequence< sal_Int8 >& data, const geometry::IntegerRectangle2D& rect ) throw (lang::IllegalArgumentException, lang::IndexOutOfBoundsException, uno::RuntimeException)
-    {
-        tools::LocalGuard aGuard;
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-
-        // retrieve local copies from the BitmapEx, which are later
-        // stored back. Unfortunately, the BitmapEx does not permit
-        // in-place modifications, as they are necessary here.
-        Bitmap aBitmap( maBitmap->GetBitmap() );
-        Bitmap aAlpha( maBitmap->GetAlpha().GetBitmap() );
-#else
-        // TODO: Support alpha canvas here
-        VirtualDevice& rVDev( getVirDev() );
-        const Point aEmptyPoint(0,0);
-        const Size  aBmpSize( rVDev.GetOutputSizePixel() );
-
-        Bitmap aBitmap( rVDev.GetBitmap(aEmptyPoint, aBmpSize) );
-        Bitmap aAlpha( Bitmap( aBmpSize, 8) );
-#endif
-
-        bool bCopyBack( false ); // only copy something back, if we
-                                 // actually changed pixel
-
-        {
-            ScopedBitmapWriteAccess pWriteAccess( aBitmap.AcquireWriteAccess(),
-                                                  aBitmap );
-            ScopedBitmapWriteAccess pAlphaWriteAccess( aAlpha.AcquireWriteAccess(),
-                                                       aAlpha );
-
-            if( pWriteAccess.get() != NULL &&
-                pAlphaWriteAccess.get() != NULL )
-            {
-                // TODO: Support more formats.
-                const Size aBmpSize( aBitmap.GetSizePixel() );
-
-                // for the time being, always read as BGRA
-                int nCurrPos(0);
-                for( int y=rect.Y1;
-                     y<aBmpSize.Height() && y<rect.Y2;
-                     ++y )
-                {
-                    for( int x=rect.X1;
-                         x<aBmpSize.Width() && x<rect.X2;
-                         ++x )
-                    {
-                        pWriteAccess->SetPixel( y, x, BitmapColor( data[ nCurrPos+2 ],
-                                                                   data[ nCurrPos+1 ],
-                                                                   data[ nCurrPos ] ) );
-                        nCurrPos += 3;
-
-                        pAlphaWriteAccess->SetPixel( y, x, BitmapColor( 255 - data[ nCurrPos++ ] ) );
-                    }
-                }
-
-                bCopyBack = true;
-            }
-        }
-
-        // copy back only here, since the BitmapAccessors must be
-        // destroyed beforehand
-        if( bCopyBack )
-        {
-#if 0
-            // TODO: To be used when dealing with real bitmaps here
-            maBitmap = BitmapEx( aBitmap,
-                                 AlphaMask( aAlpha ) );
-#else
-            // TODO: Support alpha canvas here
-            VirtualDevice& rVDev( getVirDev() );
-            rVDev.EnableMapMode( FALSE );
-            const Point aEmptyPoint(0,0);
-            rVDev.DrawBitmap(aEmptyPoint, aBitmap);
-#endif
-        }
-    }
-
-    void SAL_CALL CanvasBitmap::setPixel( const uno::Sequence< sal_Int8 >& color, const geometry::IntegerPoint2D& pos ) throw (lang::IllegalArgumentException, lang::IndexOutOfBoundsException, uno::RuntimeException)
-    {
-        tools::LocalGuard aGuard;
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        const Size aBmpSize( maBitmap->GetSizePixel() );
-#else
-        const Size aBmpSize( getVirDev().GetOutputSizePixel() );
-#endif
-
-        OSL_ENSURE( pos.X >= 0 && pos.X < aBmpSize.Width(), "CanvasBitmap::setPixel: X coordinate out of bounds" );
-        OSL_ENSURE( pos.Y >= 0 && pos.Y < aBmpSize.Height(), "CanvasBitmap::setPixel: Y coordinate out of bounds" );
-        OSL_ENSURE( color.getLength() > 3, "CanvasBitmap::setPixel: not enough color components" );
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        // retrieve local copies from the BitmapEx, which are later
-        // stored back. Unfortunately, the BitmapEx does not permit
-        // in-place modifications, as they are necessary here.
-        Bitmap aBitmap( maBitmap->GetBitmap() );
-        Bitmap aAlpha( maBitmap->GetAlpha().GetBitmap() );
-
-        bool bCopyBack( false ); // only copy something back, if we
-                                 // actually changed a pixel
-
-        {
-            ScopedBitmapWriteAccess pWriteAccess( aBitmap.AcquireWriteAccess(),
-                                                  aBitmap );
-            ScopedBitmapWriteAccess pAlphaWriteAccess( aAlpha.AcquireWriteAccess(),
-                                                       aAlpha );
-
-            if( pWriteAccess.get() != NULL &&
-                pAlphaWriteAccess.get() != NULL )
-            {
-                pWriteAccess->SetPixel( pos.Y, pos.X, BitmapColor( color[ 2 ],
-                                                                   color[ 1 ],
-                                                                   color[ 0 ] ) );
-                pAlphaWriteAccess->SetPixel( pos.Y, pos.X, BitmapColor( 255 - color[ 3 ] ) );
-            }
-        }
-
-        // copy back only here, since the BitmapAccessors must be
-        // destroyed beforehand
-        if( bCopyBack )
-        {
-            maBitmap = BitmapEx( aBitmap,
-                                 AlphaMask( aAlpha ) );
-        }
-#else
-        VirtualDevice& rVDev( getVirDev() );
-        rVDev.EnableMapMode( FALSE );
-
-        uno::Reference< rendering::XCanvas > xCanvas( mpBitmapCanvas.getRef(),
-                                                      uno::UNO_QUERY );
-        OSL_ENSURE( xCanvas.is(), "CanvasBitmap::setPixel(): Invalid reference canvas" );
-
-        getVirDev().DrawPixel( ::vcl::unotools::pointFromIntegerPoint2D( pos ),
-                               ::vcl::unotools::sequenceToColor(
-                                   xCanvas->getDevice(),
-                                   color ) );
-#endif
-    }
-
-    uno::Sequence< sal_Int8 > SAL_CALL CanvasBitmap::getPixel( const geometry::IntegerPoint2D& pos ) throw (lang::IndexOutOfBoundsException, rendering::VolatileContentDestroyedException, uno::RuntimeException)
-    {
-        tools::LocalGuard aGuard;
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        const Size aBmpSize( maBitmap->GetSizePixel() );
-#else
-        const Size aBmpSize( getVirDev().GetOutputSizePixel() );
-#endif
-
-        OSL_ENSURE( pos.X >= 0 && pos.X < aBmpSize.Width(), "CanvasBitmap::getPixel: X coordinate out of bounds" );
-        OSL_ENSURE( pos.Y >= 0 && pos.Y < aBmpSize.Height(), "CanvasBitmap::getPixel: Y coordinate out of bounds" );
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        Bitmap aBitmap( maBitmap->GetBitmap() );
-        Bitmap aAlpha( maBitmap->GetAlpha().GetBitmap() );
-        ScopedBitmapReadAccess pReadAccess( aBitmap.AcquireReadAccess(),
-                                            aBitmap );
-        ScopedBitmapReadAccess pAlphaReadAccess( aAlpha.AcquireReadAccess(),
-                                                 aAlpha );
-
-        if( pReadAccess.get() != NULL &&
-            pAlphaReadAccess.get() != NULL )
-        {
-            // for the time being, always return as BGRA
-            uno::Sequence< sal_Int8 > aRes( 4 );
-
-            const BitmapColor aColor( pReadAccess->GetColor( pos.Y, pos.X ) );
-            aRes[ 3 ] = aColor.GetRed();
-            aRes[ 2 ] = aColor.GetGreen();
-            aRes[ 1 ] = aColor.GetBlue();
-
-            aRes[ 3 ] = pAlphaReadAccess->GetPixel( pos.Y, pos.X ).GetIndex();
-
-            return aRes;
-        }
-
-        return uno::Sequence< sal_Int8 >();
-#else
-        VirtualDevice& rVDev( getVirDev() );
-        rVDev.EnableMapMode( FALSE );
-
-        uno::Reference< rendering::XCanvas > xCanvas(mpBitmapCanvas.getRef(),
-                                                     uno::UNO_QUERY);
-
-        OSL_ENSURE( xCanvas.is(), "CanvasBitmap::getPixel(): Invalid reference canvas" );
-
-        return ::vcl::unotools::colorToIntSequence(
-            xCanvas->getDevice(),
-            getVirDev().GetPixel( ::vcl::unotools::pointFromIntegerPoint2D( pos ) ) );
-#endif
-    }
-
-    uno::Reference< rendering::XBitmapPalette > SAL_CALL CanvasBitmap::getPalette(  ) throw (uno::RuntimeException)
-    {
-        return uno::Reference< rendering::XBitmapPalette >();
-    }
-
-    rendering::IntegerBitmapLayout SAL_CALL CanvasBitmap::getMemoryLayout(  ) throw (uno::RuntimeException)
-    {
-        // TODO: finish that one
-        rendering::IntegerBitmapLayout aLayout;
-
-#if 0
-        // TODO: To be used when dealing with real bitmaps here
-        const Size aBmpSize( maBitmap->GetSizePixel() );
-#else
-        const Size aBmpSize( getVirDev().GetOutputSizePixel() );
-#endif
-
-        aLayout.ScanLines = aBmpSize.Height();
-        aLayout.ScanLineBytes = aBmpSize.Width()*4;
-        aLayout.ScanLineStride = aLayout.ScanLineBytes;
-        aLayout.Format = 0;
-        aLayout.NumComponents = 4;
-        aLayout.ComponentMasks = uno::Sequence<sal_Int64>();
-        aLayout.Endianness = 0;
-        aLayout.IsPseudoColor = false;
-
-        return aLayout;
+        // forward to parent
+        CanvasBitmap_Base::disposing();
     }
 
 #define SERVICE_NAME "drafts.com.sun.star.rendering.CanvasBitmap"
@@ -444,8 +179,8 @@ namespace vclcanvas
         return aRet;
     }
 
-    VirtualDevice& CanvasBitmap::getVirDev()
+    BitmapEx CanvasBitmap::getBitmap() const
     {
-        return mpBitmapCanvas->getVirDev();
+        return maCanvasHelper.getBitmap();
     }
 }
