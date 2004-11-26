ffeef185a02711e44520d033bfb43e2132d26b8e
Rüdiger Timm <rt@openoffice.org>
2004-11-26 20:08:50 +0000

    INTEGRATION: CWS presentationengine01 (1.2.52); FILE MERGED 2004/11/19 23:56:02 thb 1.2.52.3: #110496# Removed isEmpty due to resync. FS obviously encountered the same shortcoming, but named it the STL way 2004/11/17 20:12:48 thb 1.2.52.2: RESYNC: (1.2-1.3); FILE MERGED 2004/09/06 13:23:00 thb 1.2.52.1: #110496# Added isEmpty query on the ListenerContainer

--- comphelper/source/misc/listenernotification.cxx
+++ comphelper/source/misc/listenernotification.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: listenernotification.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pjunck $ $Date: 2004-10-22 12:46:17 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 21:08:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
