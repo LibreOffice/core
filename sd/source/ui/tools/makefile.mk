bd9f946cde28b9e3c0bcaea3c8aa3046f72df0fe
Rüdiger Timm <rt@openoffice.org>
2004-11-26 19:27:16 +0000

    INTEGRATION: CWS presentationengine01 (1.2.18); FILE MERGED 2004/10/12 23:20:31 thb 1.2.18.2: RESYNC: (1.2-1.3); FILE MERGED 2004/09/29 16:41:10 af 1.2.18.1: #i34804# Added new class EventMultiplexer.

--- sd/source/ui/tools/makefile.mk
+++ sd/source/ui/tools/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-09-20 13:36:52 $
+#   last change: $Author: rt $ $Date: 2004-11-26 20:27:16 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -80,6 +80,7 @@ AUTOSEG=true
 
 SLOFILES =      							\
     $(SLO)$/IconCache.obj					\
+    $(SLO)$/EventMultiplexer.obj			\
     $(SLO)$/PreviewRenderer.obj				\
     $(SLO)$/SdGlobalResourceContainer.obj
 
