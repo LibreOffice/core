ff9e392ab22a3a07890db9397f4ecbdc79daff8a
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-26 14:09:58 +0000

    INTEGRATION: CWS impress16 (1.5.1052); FILE MERGED 2004/11/10 15:37:19 af 1.5.1052.1: #i33716# Overriding default parameter of SvxUnoNameItemTable.

--- svx/source/unodraw/unogtabl.cxx
+++ svx/source/unodraw/unogtabl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unogtabl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: cl $ $Date: 2001-01-28 16:24:30 $
+ *  last change: $Author: hr $ $Date: 2004-11-26 15:09:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,6 +79,7 @@
 #include "svdmodel.hxx"
 #include "xdef.hxx"
 #include "xflgrit.hxx"
+#include "unomid.hxx"
 
 using namespace ::com::sun::star;
 using namespace ::rtl;
@@ -101,7 +102,7 @@ public:
 };
 
 SvxUnoGradientTable::SvxUnoGradientTable( SdrModel* pModel ) throw()
-: SvxUnoNameItemTable( pModel, XATTR_FILLGRADIENT )
+    : SvxUnoNameItemTable( pModel, XATTR_FILLGRADIENT, MID_FILLGRADIENT )
 {
 }
 
