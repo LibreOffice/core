b42c7e1823697d5d854a5d6bae4ce9139f7c70c9
Rüdiger Timm <rt@openoffice.org>
2004-11-26 16:11:53 +0000

    INTEGRATION: CWS presentationengine01 (1.2.2); FILE MERGED 2004/11/17 17:00:29 thb 1.2.2.5: #118514# Canvas module reorg 2004/07/20 19:23:56 thb 1.2.2.4: #110496# Removed self-references to various interface implementations, along the lines, factored out common base implementation for all c++ canvases 2004/05/10 09:36:02 hdu 1.2.2.3: #116716# improve text handling for VCL canvas 2004/04/12 15:12:25 thb 1.2.2.2: #110496# Adaptions after canvas01 merge 2004/04/05 15:57:58 thb 1.2.2.1: Resync with canvas01 changes

--- canvas/source/vcl/canvasfont.hxx
+++ canvas/source/vcl/canvasfont.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: canvasfont.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: thb $ $Date: 2004-03-18 10:38:40 $
+ *  last change: $Author: rt $ $Date: 2004-11-26 17:11:53 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,17 @@
 #ifndef _CANVASFONT_HXX
 #define _CANVASFONT_HXX
 
-#ifndef _CPPUHELPER_IMPLBASE2_HXX_
-#include <cppuhelper/implbase2.hxx>
+#ifndef _COMPHELPER_IMPLEMENTATIONREFERENCE_HXX
+#include <comphelper/implementationreference.hxx>
 #endif
+
+#ifndef _CPPUHELPER_COMPBASE2_HXX_
+#include <cppuhelper/compbase2.hxx>
+#endif
+#ifndef _COMPHELPER_BROADCASTHELPER_HXX_
+#include <comphelper/broadcasthelper.hxx>
+#endif
+
 #ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #endif
@@ -79,8 +87,7 @@
 
 #include <canvas/vclwrapper.hxx>
 
-#include "canvasbase.hxx"
-#include "outdevprovider.hxx"
+#include "canvashelper.hxx"
 #include "impltools.hxx"
 
 
@@ -90,24 +97,30 @@
 
 namespace vclcanvas
 {
-    class SpriteCanvas;
+    typedef ::cppu::WeakComponentImplHelper2< ::drafts::com::sun::star::rendering::XCanvasFont,
+                                               ::com::sun::star::lang::XServiceInfo > CanvasFont_Base;
 
-    class CanvasFont : public ::cppu::WeakImplHelper2< ::drafts::com::sun::star::rendering::XCanvasFont,
-                         ::com::sun::star::lang::XServiceInfo >
+    class CanvasFont : public ::comphelper::OBaseMutex, public CanvasFont_Base
     {
     public:
-        CanvasFont( const ::drafts::com::sun::star::rendering::FontRequest& fontRequest,
-                    const OutDevProvider::ImplRef&                          rCanvas );
+        typedef ::comphelper::ImplementationReference<
+            CanvasFont,
+            ::drafts::com::sun::star::rendering::XCanvasFont > ImplRef;
+
+        CanvasFont( const ::drafts::com::sun::star::rendering::FontRequest&                                 fontRequest,
+                    const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >&        extraFontProperties,
+                    const ::drafts::com::sun::star::geometry::Matrix2D&                                     rFontMatrix,
+                    const OutDevProviderSharedPtr&                                                          rDevice );
+
+        /// Dispose all internal references
+        virtual void SAL_CALL disposing();
 
         // XCanvasFont
-        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > > SAL_CALL queryTextShapes( const ::drafts::com::sun::star::rendering::StringContext& text, const ::drafts::com::sun::star::rendering::ViewState& viewState, const ::drafts::com::sun::star::rendering::RenderState& renderState, sal_Int8 direction ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryTightMeasures( const ::drafts::com::sun::star::rendering::StringContext& text, const ::drafts::com::sun::star::rendering::ViewState& viewState, const ::drafts::com::sun::star::rendering::RenderState& renderState, sal_Int8 direction ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryTextMeasures( const ::drafts::com::sun::star::rendering::StringContext& text, const ::drafts::com::sun::star::rendering::ViewState& viewState, const ::drafts::com::sun::star::rendering::RenderState& renderState, sal_Int8 direction ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< double > SAL_CALL queryTextOffsets( const ::drafts::com::sun::star::rendering::StringContext& text, const ::drafts::com::sun::star::rendering::ViewState& viewState, const ::drafts::com::sun::star::rendering::RenderState& renderState, sal_Int8 direction ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::drafts::com::sun::star::geometry::RealRectangle2D SAL_CALL queryTextBounds( const ::drafts::com::sun::star::rendering::StringContext& text, const ::drafts::com::sun::star::rendering::ViewState& viewState, const ::drafts::com::sun::star::rendering::RenderState& renderState, sal_Int8 direction ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XTextLayout > SAL_CALL createTextLayout( const ::drafts::com::sun::star::rendering::StringContext& aText, sal_Int8 nDirection, sal_Int64 nRandomSeed ) throw (::com::sun::star::uno::RuntimeException);
         virtual ::drafts::com::sun::star::rendering::FontRequest SAL_CALL getFontRequest(  ) throw (::com::sun::star::uno::RuntimeException);
         virtual ::drafts::com::sun::star::rendering::FontMetrics SAL_CALL getFontMetrics(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XCanvas > SAL_CALL getAssociatedCanvas(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< double > SAL_CALL getAvailableSizes(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > SAL_CALL getExtraFontProperties(  ) throw (::com::sun::star::uno::RuntimeException);
 
         // XServiceInfo
         virtual ::rtl::OUString SAL_CALL getImplementationName() throw( ::com::sun::star::uno::RuntimeException );
@@ -126,7 +139,7 @@ namespace vclcanvas
 
         ::canvas::vcltools::VCLObject<Font>                 maFont;
         ::drafts::com::sun::star::rendering::FontRequest    maFontRequest;
-        OutDevProvider::ImplRef                             mpCanvas;
+        OutDevProviderSharedPtr                             mpRefDevice;
     };
 
 }
