ff2f1d1a40803a72e531688478700b4b57fa8384
Rüdiger Timm <rt@openoffice.org>
2004-11-26 13:21:18 +0000

    INTEGRATION: CWS recovery04 (1.1.2); FILE ADDED 2004/09/03 04:54:28 hro 1.1.2.1: #i28480# Sending only mode for crash reporter

---
+++ svx/source/dialog/sendreportgen.cxx
@@ -0,0 +1,23 @@
+#include "docrecovery.hxx"
+
+namespace svx{
+    namespace DocRecovery{
+
+        bool ErrorRepSendDialog::ReadParams()
+        {
+            return false;
+        }
+
+        bool ErrorRepSendDialog::SaveParams()
+        {
+            return false;
+        }
+
+        bool ErrorRepSendDialog::SendReport()
+        {
+            return false;
+        }
+
+
+    }   // namespace DocRecovery
+}   // namespace svx
