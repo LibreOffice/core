c76f80404e140aad0d832098865cafb85eac43cb
Rüdiger Timm <rt@openoffice.org>
2004-11-26 11:54:10 +0000

    INTEGRATION: CWS oasisbf2 (1.1.2); FILE ADDED 2004/11/02 17:20:51 mib 1.1.2.1: #i35133#: replacement image for embedded objects

---
+++ xmloff/inc/XMLReplacementImageContext.hxx
@@ -0,0 +1,111 @@
+/*************************************************************************
+ *
+ *  $RCSfile: XMLReplacementImageContext.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2004-11-26 12:54:10 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_XMLREPLACEMENTIMAGECONTEXT_HXX
+#define _XMLOFF_XMLREPLACEMENTIMAGECONTEXT_HXX
+
+#ifndef _COM_SUN_STAR_XML_SAX_XDOCUMENTHANDLER_HPP_
+#include <com/sun/star/xml/sax/XDocumentHandler.hpp>
+#endif
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include "xmlictxt.hxx"
+#endif
+
+
+namespace com { namespace sun { namespace star {
+    namespace beans { XPropertySet; }
+    namespace io { class XOutputStream; } } } }
+
+class XMLReplacementImageContext : public SvXMLImportContext
+{
+    ::com::sun::star::uno::Reference <
+        ::com::sun::star::io::XOutputStream > m_xBase64Stream;
+::com::sun::star::uno::Reference <
+        ::com::sun::star::beans::XPropertySet > m_xPropSet;
+
+    ::rtl::OUString m_sHRef;
+    const ::rtl::OUString m_sGraphicURL;
+
+public:
+
+    TYPEINFO();
+
+    XMLReplacementImageContext( SvXMLImport& rImport,
+            sal_uInt16 nPrfx,
+            const ::rtl::OUString& rLName,
+            const ::com::sun::star::uno::Reference<
+                ::com::sun::star::xml::sax::XAttributeList > & rAttrList,
+            const ::com::sun::star::uno::Reference <
+                ::com::sun::star::beans::XPropertySet >& rPropSet );
+    virtual ~XMLReplacementImageContext();
+
+    virtual void EndElement();
+
+    SvXMLImportContext *CreateChildContext( sal_uInt16 nPrefix,
+                const ::rtl::OUString& rLocalName,
+                 const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::xml::sax::XAttributeList > & xAttrList );
+
+};
+
+#endif  //  _XMLOFF_XMLREPLACEMENTIMAGECONTEXT_HXX
+
