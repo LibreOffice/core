7158c0bee4e50347c7ac676df213c4a19e0cb770
Rüdiger Timm <rt@openoffice.org>
2004-11-26 18:30:10 +0000

    INTEGRATION: CWS presentationengine01 (1.1.2); FILE ADDED 2004/04/16 13:46:04 cl 1.1.2.1: smil support for impress

---
+++ xmloff/source/core/unointerfacetouniqueidentifiermapper.cxx
@@ -0,0 +1,135 @@
+#ifndef __COMPHELPER_UNOINTERFACETOUNIQUEIDENTIFIERMAPPER__
+#include "unointerfacetouniqueidentifiermapper.hxx"
+#endif
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::XInterface;
+using ::rtl::OUString;
+
+namespace comphelper
+{
+
+UnoInterfaceToUniqueIdentifierMapper::UnoInterfaceToUniqueIdentifierMapper()
+: mnNextId( 1 )
+{
+}
+
+/** returns a unique identifier for the given uno object. IF a uno object is
+    registered more than once, the returned identifier is always the same.
+*/
+const OUString& UnoInterfaceToUniqueIdentifierMapper::registerReference( const Reference< XInterface >& rInterface )
+{
+    IdMap_t::const_iterator aIter;
+    if( findReference( rInterface, aIter ) )
+    {
+        return (*aIter).first;
+    }
+    else
+    {
+        return (*maEntries.insert( IdMap_t::value_type( OUString::valueOf( mnNextId++ ), rInterface ) ).first).first;
+    }
+}
+
+/** registers the given uno object with the given identifier.
+
+    @returns
+        false, if the given identifier already exists and is not associated with the given interface
+*/
+bool UnoInterfaceToUniqueIdentifierMapper::registerReference( const OUString& rIdentifier, const Reference< XInterface >& rInterface )
+{
+    IdMap_t::const_iterator aIter;
+    if( findReference( rInterface, aIter ) )
+    {
+        return rIdentifier != (*aIter).first;
+    }
+    else if( findIdentifier( rIdentifier, aIter ) )
+    {
+        return false;
+    }
+    else
+    {
+        maEntries.insert( IdMap_t::value_type( rIdentifier, rInterface ) );
+
+        // see if this is a reference like something we would generate in the future
+        const sal_Unicode *p = rIdentifier.getStr();
+        sal_Int32 nLength = rIdentifier.getLength();
+
+        // see if the identifier is a pure integer value
+        while(nLength--)
+        {
+            if( (*p < '0') || (*p > '9') )
+                return true; // a custom id, that will never conflict with genereated id's
+
+            p++;
+        }
+
+        // the identifier is a pure integer value
+        // so we make sure we will never generate
+        // an integer value like this one
+        sal_Int32 nId = rIdentifier.toInt32();
+        if( mnNextId <= nId )
+            mnNextId = nId + 1;
+
+        return true;
+    }
+}
+
+/** @returns
+        the identifier for the given uno object. If this uno object is not already
+        registered, an empty string is returned
+*/
+const OUString& UnoInterfaceToUniqueIdentifierMapper::getIdentifier( const Reference< XInterface >& rInterface ) const
+{
+    IdMap_t::const_iterator aIter;
+    if( findReference( rInterface, aIter ) )
+    {
+        return (*aIter).first;
+    }
+    else
+    {
+        static const OUString aEmpty;
+        return aEmpty;
+    }
+}
+
+/** @returns
+    the uno object that is registered with the given identifier. If no uno object
+    is registered with the given identifier, an empty reference is returned.
+*/
+const Reference< XInterface >& UnoInterfaceToUniqueIdentifierMapper::getReference( const OUString& rIdentifier ) const
+{
+    IdMap_t::const_iterator aIter;
+    if( findIdentifier( rIdentifier, aIter ) )
+    {
+        return (*aIter).second;
+    }
+    else
+    {
+        static const Reference< XInterface > aEmpty;
+        return aEmpty;
+    }
+}
+
+bool UnoInterfaceToUniqueIdentifierMapper::findReference( const Reference< XInterface >& rInterface, IdMap_t::const_iterator& rIter ) const
+{
+    rIter = maEntries.begin();
+    const IdMap_t::const_iterator aEnd( maEntries.end() );
+    while( rIter != aEnd )
+    {
+        if( (*rIter).second == rInterface )
+            return true;
+
+        rIter++;
+    }
+
+    return false;
+}
+
+bool UnoInterfaceToUniqueIdentifierMapper::findIdentifier( const OUString& rIdentifier, IdMap_t::const_iterator& rIter ) const
+{
+    rIter = maEntries.find( rIdentifier );
+    return rIter != maEntries.end();
+}
+
+};
+
