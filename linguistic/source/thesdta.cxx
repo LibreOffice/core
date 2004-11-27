4d10af3b07bef5e3edfa40d206fc9e4655378ef9
Kurt Zenker <kz@openoffice.org>
2004-11-27 12:21:21 +0000

    INTEGRATION: CWS tl04 (1.1.2); FILE ADDED 2004/11/08 10:00:30 tl 1.1.2.1: #118281# switch to Proximity linguistic

---
+++ linguistic/source/thesdta.cxx
@@ -0,0 +1,123 @@
+/*************************************************************************
+ *
+ *  $RCSfile: thesdta.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2004-11-27 13:21:21 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _LANG_HXX //autogen wg. LANGUAGE_ENGLISH_US
+#include <tools/lang.hxx>
+#endif
+#ifndef _TOOLS_DEBUG_HXX //autogen wg. DBG_ASSERT
+#include <tools/debug.hxx>
+#endif
+
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+#include <linguistic/misc.hxx>
+
+#include "thesdta.hxx"
+
+//using namespace utl;
+using namespace osl;
+using namespace rtl;
+using namespace com::sun::star;
+using namespace com::sun::star::beans;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::uno;
+using namespace com::sun::star::linguistic2;
+using namespace linguistic;
+
+
+namespace linguistic
+{
+
+///////////////////////////////////////////////////////////////////////////
+
+
+ThesaurusMeaning::ThesaurusMeaning(const OUString &rText,
+                 const OUString &rLookUpText, INT16 nLookUpLang ) :
+    aText               (rText),
+    aLookUpText         (rLookUpText),
+    nLookUpLanguage     (nLookUpLang)
+{
+}
+
+ThesaurusMeaning::~ThesaurusMeaning()
+{
+}
+
+OUString SAL_CALL
+        ThesaurusMeaning::getMeaning() throw(RuntimeException)
+{
+    MutexGuard  aGuard( GetLinguMutex() );
+    return aText;
+}
+
+/*
+uno::Sequence< OUString > SAL_CALL ThesaurusMeaning::querySynonyms()
+        throw(RuntimeException)
+{
+}
+*/
+
+///////////////////////////////////////////////////////////////////////////
+
+}   // namespace linguistic
+
