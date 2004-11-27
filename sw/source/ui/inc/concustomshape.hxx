29e1b79a868e8b421f1ec077b829ec92b0b3c3f5
Jens-Heiner Rechtien <hr@openoffice.org>
2004-11-27 11:31:10 +0000

    INTEGRATION: CWS os42 (1.2.80); FILE MERGED 2004/11/27 08:55:59 os 1.2.80.2: RESYNC: (1.2-1.3); FILE MERGED 2004/11/11 12:33:18 iha 1.2.80.1: #i36242# custom shapes could not be selected if another was selected before

--- sw/source/ui/inc/concustomshape.hxx
+++ sw/source/ui/inc/concustomshape.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: concustomshape.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-17 09:38:46 $
+ *  last change: $Author: hr $ $Date: 2004-11-27 12:31:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -86,6 +86,9 @@ class ConstCustomShape : public SwDrawBase
     virtual BOOL MouseButtonDown(const MouseEvent& rMEvt);
 
     virtual void Activate(const USHORT nSlotId);    // Function aktivieren
+
+    rtl::OUString GetShapeType() const;
+    static rtl::OUString GetShapeTypeFromRequest( SfxRequest& rReq );
 
     // #i33136#
     virtual bool doConstructOrthogonal() const;
