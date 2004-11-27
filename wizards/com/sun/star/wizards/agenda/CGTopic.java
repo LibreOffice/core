8654e2768fcf1b78645f99abb1bf906eeee9a181
Kurt Zenker <kz@openoffice.org>
2004-11-27 08:04:54 +0000

    INTEGRATION: CWS qwizardsbf1 (1.2.2); FILE MERGED 2004/09/30 10:51:00 rpiterman 1.2.2.1: documentation

--- wizards/com/sun/star/wizards/agenda/CGTopic.java
+++ wizards/com/sun/star/wizards/agenda/CGTopic.java
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: CGTopic.java,v $
 *
-*  $Revision: 1.2 $
+*  $Revision: 1.3 $
 *
-*  last change: $Author: obo $  $Date: 2004-09-08 13:59:39 $
+*  last change: $Author: kz $  $Date: 2004-11-27 09:04:54 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
@@ -65,17 +65,31 @@ import com.sun.star.wizards.common.ConfigGroup;
 import com.sun.star.wizards.common.Indexable;
 
 /**
-*
-*/
+ * CGTopic means: Configuration Group Topic.
+ * This object encapsulates a configuration group with topic information.
+ * Since the topics gui conftrol uses its own data model, there is
+ * also code here to convert from the data model to CGTopic object (the constructor)
+ * and vice versa (setDataToRow method - used when loading the last session...)
+ */
 public class CGTopic extends ConfigGroup implements Indexable {
 
+    /** sort order  */
     public int cp_Index;
+    /** topic name  */
     public String cp_Topic;
+    /** responsible */
     public String cp_Responsible;
+    /** time */
     public String cp_Time;
 
     public CGTopic() {}
 
+    /**
+     * create a new CGTopic object with data from the given row.
+     * the row object is a PropertyValue array, as used
+     * by the TopicsControl's data model.
+     * @param row PropertyValue array as used by the TopicsControl data model.
+     */
     public CGTopic( Object row) {
         PropertyValue[] pv = (PropertyValue[])row;
         String num = (String)pv[0].Value;
@@ -85,6 +99,12 @@ public class CGTopic extends ConfigGroup implements Indexable {
         cp_Time = (String)pv[3].Value;
     }
 
+    /**
+     * copies the data in this CGTopic object
+     * to the given row.
+     * @param row the row object (PropertyValue array) to
+     * copy the data to.
+     */
     public void setDataToRow(Object row) {
         PropertyValue[] pv = (PropertyValue[])row;
         pv[0].Value = "" + cp_Index + ".";
@@ -96,6 +116,5 @@ public class CGTopic extends ConfigGroup implements Indexable {
     public int getIndex() {
         return cp_Index;
     }
-
 
 }
