d656e0cc33c64ecb451804dfe0beca8d93cd2c6a
Kurt Zenker <kz@openoffice.org>
2004-11-27 08:04:42 +0000

    INTEGRATION: CWS qwizardsbf1 (1.2.2); FILE MERGED 2004/09/30 10:50:27 rpiterman 1.2.2.1: documentation

--- wizards/com/sun/star/wizards/agenda/CGAgenda.java
+++ wizards/com/sun/star/wizards/agenda/CGAgenda.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CGAgenda.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $  $Date: 2004-09-08 13:59:27 $
+ *  last change: $Author: kz $  $Date: 2004-11-27 09:04:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,37 +64,60 @@ import com.sun.star.wizards.common.ConfigGroup;
 import com.sun.star.wizards.common.ConfigSet;
 
 /**
- *
+ * CGAgenda means Configuration Group Agenda.
+ * This object encapsulates a OOo configuration group, used
+ * to save the state of the agenda wizard for reloading uppon
+ * next start.
  */
 public class CGAgenda extends ConfigGroup {
 
+    /** step 1 - design*/
     public int cp_AgendaType;
+    /** step 1 - include minutes*/
     public boolean cp_IncludeMinutes;
 
-
+    /** step 2  - title */
     public String cp_Title = "";
+    /** step 2  - date */
     public String cp_Date;
+    /** step 2  - time */
     public String cp_Time;
+    /** step 2  - location */
     public String cp_Location = "";
 
+    /** step 3  - show meeting type */
     public boolean cp_ShowMeetingType;
+    /** step 3  - show read */
     public boolean cp_ShowRead;
+    /** step 3  - show bring */
     public boolean cp_ShowBring;
+    /** step 3  - show notes */
     public boolean cp_ShowNotes;
 
+    /** step 4 - show called by */
     public boolean cp_ShowCalledBy;
+    /** step 4 - show facilitator */
     public boolean cp_ShowFacilitator;
+    /** step 4 - show notes taker */
     public boolean cp_ShowNotetaker;
+    /** step 4 - show time keeper */
     public boolean cp_ShowTimekeeper;
+    /** step 4 - show attendees */
     public boolean cp_ShowAttendees;
+    /** step 4 - show observers */
     public boolean cp_ShowObservers;
+    /** step 4 - show resource persons */
     public boolean cp_ShowResourcePersons;
 
+    /** page 6 - template title   */
     public String cp_TemplateName;
+    /** page 6 - template url    */
     public String cp_TemplatePath;
 
+    /** page 6 - how to proceed  */
     public int cp_ProceedMethod;
 
+    /** page 5 - topics ( a set )*/
     public ConfigSet cp_Topics = new ConfigSet(CGTopic.class);
 
 }
