a9e73c94e2220737d7efc9a31f4fca823f9f5dc2
Jens-Heiner Rechtien <hr@openoffice.org>
2004-12-10 17:40:08 +0000

    INTEGRATION: CWS ause012 (1.5.82); FILE MERGED 2004/11/16 14:29:49 hjs 1.5.82.1: #i37200# remove unused RSCUPDVER redefinition

--- desktop/source/splash/makefile.mk
+++ desktop/source/splash/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hjs $ $Date: 2004-06-25 12:27:01 $
+#   last change: $Author: hr $ $Date: 2004-12-10 18:40:08 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -71,7 +71,6 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-RSCUPDVER=$(RSCREVISION)(SV$(UPD)$(UPDMINOR))
 
 # --- Files --------------------------------------------------------
 
