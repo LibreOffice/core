2e3976eab2f5365ce284013121c0a968dfabc214
Jens-Heiner Rechtien <hr@openoffice.org>
2004-12-10 17:06:24 +0000

    INTEGRATION: CWS ause012 (1.3.6); FILE MERGED 2004/11/16 15:13:53 hjs 1.3.6.1: #i37200# remove unused RSCUPDVER redefinition

--- unoxml/source/events/makefile.mk
+++ unoxml/source/events/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-09-08 16:15:48 $
+#   last change: $Author: hr $ $Date: 2004-12-10 18:06:24 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -62,14 +62,13 @@
 
 PRJ=../..
 
-PRJNAME=libxml2
+PRJNAME=unoxml
 TARGET=eventsimpl
 ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-RSCUPDVER=$(RSCREVISION)(SV$(UPD)$(UPDMINOR))
 
 .IF "$(SYSTEM_LIBXML)" == "YES"
 CFLAGS+=-DSYSTEM_LIBXML $(LIBXML_CFLAGS)
