61457d5d5b916758269f28c179363d8afd7670a2
Jens-Heiner Rechtien <hr@openoffice.org>
2004-12-10 16:59:12 +0000

    INTEGRATION: CWS ause012 (1.3.144); FILE MERGED 2004/11/24 08:05:23 os 1.3.144.1: #i37200# RID_BUILDVERSION removed

--- sw/source/ui/inc/initui.hxx
+++ sw/source/ui/inc/initui.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: initui.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-08-23 08:59:40 $
+ *  last change: $Author: hr $ $Date: 2004-12-10 17:59:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -82,7 +82,6 @@ class SvStringsDtor;
  * Extern Definitions
  */
 extern  SwThesaurus*    pThes;
-extern  String          GetSWGVersion();
 
 extern  String*         pOldGrfCat;
 extern  String*         pOldTabCat;
