c128137b79be5a97d5d43c7d846fb2ee832685c1
Jens-Heiner Rechtien <hr@openoffice.org>
2004-12-10 17:03:23 +0000

    INTEGRATION: CWS ause012 (1.1.1.1.406); FILE MERGED 2004/11/23 13:03:12 hjs 1.1.1.1.406.1: #i37699# return error if makefile not found

--- solenv/inc/found_no.mk
+++ solenv/inc/found_no.mk
@@ -2,3 +2,4 @@ no_traget:
     @+echo -----------------------------------------------
     @+echo         Error: No makefile.mk found!
     @+echo -----------------------------------------------
+    force_dmake_to_error
