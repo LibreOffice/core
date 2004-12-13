972025001ea94c87c4f40aa641fb10eca68b351a
Jens-Heiner Rechtien <hr@openoffice.org>
2004-12-13 11:15:45 +0000

    INTEGRATION: CWS ineturl1 (1.24.458); FILE MERGED 2004/09/30 16:04:10 cmc 1.24.458.1: #i34006# modify INetURLObject to use rtl::OUString and rtl::OUStringBuffer

--- svx/source/dialog/imapdlg.cxx
+++ svx/source/dialog/imapdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: imapdlg.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: rt $ $Date: 2003-11-24 16:34:29 $
+ *  last change: $Author: hr $ $Date: 2004-12-13 12:15:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -771,7 +771,7 @@ BOOL SvxIMapDlg::DoSave()
         }
         else
         {
-            if( !aURL.getExtension().Len() )
+            if( !aURL.getExtension().getLength() )
                 aURL.setExtension( aExt );
 
             SvStream* pOStm = ::utl::UcbStreamHelper::CreateStream( aURL.GetMainURL( INetURLObject::NO_DECODE ), STREAM_WRITE | STREAM_TRUNC );
