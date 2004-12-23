47be31f831dbef0d758305f657a35a11d28c4467
Vladimir Glazounov <vg@openoffice.org>
2004-12-23 09:48:37 +0000

    INTEGRATION: CWS dr30 (1.143.32); FILE MERGED 2004/12/03 10:48:01 dr 1.143.32.3: RESYNC: (1.144-1.145); FILE MERGED 2004/12/02 17:29:42 dr 1.143.32.2: RESYNC: (1.143-1.144); FILE MERGED 2004/11/12 10:35:36 dr 1.143.32.1: #i31239# always disable form design mode

--- sw/source/filter/ww8/ww8par.cxx
+++ sw/source/filter/ww8/ww8par.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ww8par.cxx,v $
  *
- *  $Revision: 1.145 $
+ *  $Revision: 1.146 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 13:29:07 $
+ *  last change: $Author: vg $ $Date: 2004-12-23 10:48:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,7 +58,6 @@
  *
  *
  ************************************************************************/
-
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
 #ifdef PCH
@@ -1624,7 +1623,9 @@ void SwWW8ImplReader::ImportDop()
     if (pWwFib->nFib > 105)
         ImportDopTypography(pWDop->doptypography);
 
-    if (pWDop->fProtEnabled)
+    // #110055# disable form design mode to be able to use imported controls directly
+    // #i31239# always disable form design mode, not only in protected docs
+//    if (pWDop->fProtEnabled)
     {
         using namespace com::sun::star;
 
