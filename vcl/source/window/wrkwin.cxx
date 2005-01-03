cdae651d0d3fb35526b644b14ff5bbe084f7d1f1
Oliver Bolte <obo@openoffice.org>
2005-01-03 16:43:37 +0000

    INTEGRATION: CWS res32bit (1.11.114); FILE MERGED 2004/11/05 14:27:32 pl 1.11.114.1: #i34513# more adjustments for resource 32bit change

--- vcl/source/window/wrkwin.cxx
+++ vcl/source/window/wrkwin.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrkwin.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2004-06-17 11:44:01 $
+ *  last change: $Author: obo $ $Date: 2005-01-03 17:43:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -218,7 +218,7 @@ void WorkWindow::ImplLoadRes( const ResId& rResId )
 {
     SystemWindow::ImplLoadRes( rResId );
 
-    ReadShortRes();
+    ReadLongRes();
     if ( !(rResId.aWinBits & WB_HIDE) && (RSC_WORKWIN == rResId.GetRT()) )
         Show();
 }
