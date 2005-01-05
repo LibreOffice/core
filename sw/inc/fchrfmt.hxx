3234a585aa11c3b47f48a0d5c3528afb09ef452e
Rüdiger Timm <rt@openoffice.org>
2005-01-05 14:47:36 +0000

    INTEGRATION: CWS os44 (1.5.162); FILE MERGED 2004/11/24 13:57:05 os 1.5.162.1: #i37761# sw3io files removed

--- sw/inc/fchrfmt.hxx
+++ sw/inc/fchrfmt.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fchrfmt.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2004-08-23 08:31:09 $
+ *  last change: $Author: rt $ $Date: 2005-01-05 15:47:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -102,8 +102,6 @@ public:
     // "pure virtual Methoden" vom SfxPoolItem
     virtual int             operator==( const SfxPoolItem& ) const;
     virtual SfxPoolItem*    Clone( SfxItemPool* pPool = 0 ) const;
-    virtual SfxPoolItem*    Create(SvStream &, USHORT nVer) const;
-    virtual SvStream&       Store(SvStream &, USHORT nIVer ) const;
     virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
                                     SfxMapUnit eCoreMetric,
                                     SfxMapUnit ePresMetric,
