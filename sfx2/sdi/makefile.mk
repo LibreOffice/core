9779e0dbb482bfd99c71494d9541c3b9a605b10c
Rüdiger Timm <rt@openoffice.org>
2005-01-11 13:55:28 +0000

    INTEGRATION: CWS sb25 (1.3.356); FILE MERGED 2004/11/12 17:03:15 sb 1.3.356.1: #i37077# Reduce number of exported symbols of sfx dynamic library.

--- sfx2/sdi/makefile.mk
+++ sfx2/sdi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:27:49 $
+#   last change: $Author: rt $ $Date: 2005-01-11 14:55:28 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,11 +64,11 @@ PRJ=..
 
 PRJNAME=sfx2
 TARGET=sfxslots
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 SDINAME=$(TARGET)
 SDIEXPORT=sfx
