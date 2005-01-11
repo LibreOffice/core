be2ae68608b02ea3a3a9345fcbccb9dc21d1d20a
Rüdiger Timm <rt@openoffice.org>
2005-01-11 12:16:11 +0000

    INTEGRATION: CWS sb19 (1.1.1.1.604); FILE MERGED 2004/10/27 07:31:02 mba 1.1.1.1.604.1: #110407#: remove static BaseURL

--- sc/inc/rtfimp.hxx
+++ sc/inc/rtfimp.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rtfimp.hxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:44:50 $
+ *  last change: $Author: rt $ $Date: 2005-01-11 13:16:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
