a39d30696080331d845a17d7aec7aa61c741edb2
Rüdiger Timm <rt@openoffice.org>
2005-01-11 13:56:46 +0000

    INTEGRATION: CWS sb25 (1.7.346); FILE MERGED 2004/11/12 17:03:42 sb 1.7.346.1: #i37077# Reduce number of exported symbols of sfx dynamic library.

--- sfx2/source/bastyp/makefile.mk
+++ sfx2/source/bastyp/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2003-09-19 07:58:36 $
+#   last change: $Author: rt $ $Date: 2005-01-11 14:56:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,11 +65,11 @@ PRJ=..$/..
 PRJNAME=sfx2
 TARGET=bastyp
 ENABLE_EXCEPTIONS=TRUE
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
