a11bca8419081f00574d65df85d9eaffc2129e96
Rüdiger Timm <rt@openoffice.org>
2005-01-11 13:59:02 +0000

    INTEGRATION: CWS sb25 (1.3.356); FILE MERGED 2004/11/12 17:03:45 sb 1.3.356.1: #i37077# Reduce number of exported symbols of sfx dynamic library.

--- sfx2/source/inet/makefile.mk
+++ sfx2/source/inet/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:29:12 $
+#   last change: $Author: rt $ $Date: 2005-01-11 14:59:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,11 +65,11 @@ PRJ=..$/..
 PRJNAME=sfx2
 TARGET=inet
 ENABLE_EXCEPTIONS=TRUE
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
