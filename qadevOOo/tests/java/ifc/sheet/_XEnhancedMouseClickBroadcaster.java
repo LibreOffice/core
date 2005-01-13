2fe27b4f6d74f60444b04a86a131716fe2202f44
Kurt Zenker <kz@openoffice.org>
2005-01-13 16:40:27 +0000

    INTEGRATION: CWS notesapi (1.2.58); FILE MERGED 2005/01/07 14:58:35 sw 1.2.58.2: #i36871# 2004/12/17 15:52:33 sw 1.2.58.1: #i36871#

--- qadevOOo/tests/java/ifc/sheet/_XEnhancedMouseClickBroadcaster.java
+++ qadevOOo/tests/java/ifc/sheet/_XEnhancedMouseClickBroadcaster.java
@@ -1,9 +1,63 @@
-/*
- * _XEnhancedMouseClickBroadcaster.java
+/*************************************************************************
  *
- * Created on 26. Februar 2004, 16:27
- */
-
+ *  $RCSfile: _XEnhancedMouseClickBroadcaster.java,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change:$Date: 2005-01-13 17:40:27 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 package ifc.sheet;
 
 import com.sun.star.accessibility.AccessibleRole;
@@ -20,121 +74,121 @@ import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.sheet.XEnhancedMouseClickBroadcaster;
 import com.sun.star.uno.UnoRuntime;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
+
 import lib.MultiMethodTest;
 import lib.StatusException;
+
 import util.AccessibilityTools;
 import util.utils;
 
-/**
- *
- * @author  sw93809
- */
-public class _XEnhancedMouseClickBroadcaster extends MultiMethodTest {
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
+
+public class _XEnhancedMouseClickBroadcaster extends MultiMethodTest {
     public XEnhancedMouseClickBroadcaster oObj;
-    protected boolean mousePressed=false;
-    protected boolean mouseReleased=false;
+    protected boolean mousePressed = false;
+    protected boolean mouseReleased = false;
+    protected XEnhancedMouseClickHandler listener = new MyListener();
 
     public void _addEnhancedMouseClickHandler() {
         oObj.addEnhancedMouseClickHandler(listener);
         clickOnSheet();
+
         //make sure that the listener is removed even if the test fails
         if ((!mousePressed) || (!mouseReleased)) {
             oObj.removeEnhancedMouseClickHandler(listener);
         }
-        tRes.tested("addEnhancedMouseClickHandler()",mousePressed && mouseReleased);
+
+        tRes.tested("addEnhancedMouseClickHandler()",
+            mousePressed && mouseReleased);
     }
 
     public void _removeEnhancedMouseClickHandler() {
         requiredMethod("addEnhancedMouseClickHandler()");
-        mousePressed=false;
-        mouseReleased=false;
+        mousePressed = false;
+        mouseReleased = false;
         oObj.removeEnhancedMouseClickHandler(listener);
         clickOnSheet();
-        tRes.tested("removeEnhancedMouseClickHandler()",(!mousePressed) && (!mouseReleased));
-    }
-
-
-    protected XEnhancedMouseClickHandler listener = new MyListener();
-
-    protected class MyListener implements XEnhancedMouseClickHandler {
-
-        public void disposing(com.sun.star.lang.EventObject eventObject) {
-        }
-
-        public boolean mousePressed(com.sun.star.awt.EnhancedMouseEvent enhancedMouseEvent) {
-            log.println("mousePressed");
-            mousePressed=true;
-            return true;
-        }
-
-        public boolean mouseReleased(com.sun.star.awt.EnhancedMouseEvent enhancedMouseEvent) {
-            log.println("mouseReleased");
-            mouseReleased=true;
-            return true;
-        }
-
+        tRes.tested("removeEnhancedMouseClickHandler()",
+            (!mousePressed) && (!mouseReleased));
     }
 
     protected boolean clickOnSheet() {
         log.println("Trying to get AccessibleSpreadsheet");
+
         AccessibilityTools at = new AccessibilityTools();
-        XComponent xSheetDoc = (XComponent) tEnv.getObjRelation("DOCUMENT");
-        XModel xModel = (XModel)
-            UnoRuntime.queryInterface(XModel.class, xSheetDoc);
-       XWindow xWindow = at.getCurrentWindow((XMultiServiceFactory)tParam.getMSF(), xModel);
+        XComponent xSheetDoc =
+            (XComponent) tEnv.getObjRelation("DOCUMENT");
+        XModel xModel =
+            (XModel) UnoRuntime.queryInterface(XModel.class, xSheetDoc);
+        XWindow xWindow =
+            at.getCurrentWindow((XMultiServiceFactory) tParam.getMSF(),
+                xModel);
         XAccessible xRoot = at.getAccessibleObject(xWindow);
 
-        at.getAccessibleObjectForRole(xRoot, AccessibleRole.TABLE  );
+        at.getAccessibleObjectForRole(xRoot, AccessibleRole.TABLE);
 
-        XAccessibleComponent AccessibleSpreadsheet = (XAccessibleComponent) UnoRuntime.queryInterface(XAccessibleComponent.class,AccessibilityTools.SearchedContext);
+        XAccessibleComponent AccessibleSpreadsheet =
+            (XAccessibleComponent) UnoRuntime.queryInterface(XAccessibleComponent.class,
+                AccessibilityTools.SearchedContext);
 
         log.println("Got " + utils.getImplName(AccessibleSpreadsheet));
 
         Object toolkit = null;
 
         try {
-            toolkit = ((XMultiServiceFactory)tParam.getMSF()).createInstance("com.sun.star.awt.Toolkit");
+            toolkit =
+                ((XMultiServiceFactory) tParam.getMSF())
+                .createInstance("com.sun.star.awt.Toolkit");
         } catch (com.sun.star.uno.Exception e) {
             log.println("Couldn't get toolkit");
             e.printStackTrace(log);
             throw new StatusException("Couldn't get toolkit", e);
         }
 
-        XExtendedToolkit tk = (XExtendedToolkit) UnoRuntime.queryInterface(
-                                      XExtendedToolkit.class, toolkit);
+        XExtendedToolkit tk =
+            (XExtendedToolkit) UnoRuntime.queryInterface(XExtendedToolkit.class,
+                toolkit);
 
         XTopWindow tw = null;
 
         int k = tk.getTopWindowCount();
-        for (int i=0;i<k;i++) {
-        try {
-            XTopWindow tw_temp = tk.getTopWindow(11);
-            XAccessible xacc = (XAccessible) UnoRuntime.queryInterface(XAccessible.class,  tw_temp);
-            if (xacc != null) {
-                if (xacc.getAccessibleContext().getAccessibleName().indexOf("d2")>0) {
-                    tw=tw_temp;
-                };
-            } else {
-                log.println("\t unknown window");
+
+        for (int i = 0; i < k; i++) {
+            try {
+                XTopWindow tw_temp = tk.getTopWindow(i);
+                XAccessible xacc =
+                    (XAccessible) UnoRuntime.queryInterface(XAccessible.class,
+                        tw_temp);
+
+                if (xacc != null) {
+                    if (xacc.getAccessibleContext().getAccessibleName()
+                                .indexOf("d2") > 0) {
+                        tw = tw_temp;
+                    }
+
+                    ;
+                } else {
+                    log.println("\t unknown window");
+                }
+            } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
             }
-
-        } catch (com.sun.star.lang.IndexOutOfBoundsException e) {
-
         }
-        }
+
         if (tw == null) {
             System.out.println("No TopWindow :-(");
+
             return false;
         }
+
         tw.toFront();
 
         Point point = AccessibleSpreadsheet.getLocationOnScreen();
         Rectangle rect = AccessibleSpreadsheet.getBounds();
 
-        log.println("Clicking in the center of the AccessibleSpreadsheet");
+        log.println(
+            "Clicking in the center of the AccessibleSpreadsheet");
 
         try {
             Robot rob = new Robot();
@@ -157,6 +211,28 @@ public class _XEnhancedMouseClickBroadcaster extends MultiMethodTest {
             Thread.currentThread().sleep(500);
         } catch (InterruptedException e) {
             System.out.println("While waiting :" + e);
+        }
+    }
+
+    protected class MyListener implements XEnhancedMouseClickHandler {
+        public void disposing(
+            com.sun.star.lang.EventObject eventObject) {
+        }
+
+        public boolean mousePressed(
+            com.sun.star.awt.EnhancedMouseEvent enhancedMouseEvent) {
+            log.println("mousePressed");
+            mousePressed = true;
+
+            return true;
+        }
+
+        public boolean mouseReleased(
+            com.sun.star.awt.EnhancedMouseEvent enhancedMouseEvent) {
+            log.println("mouseReleased");
+            mouseReleased = true;
+
+            return true;
         }
     }
 }
