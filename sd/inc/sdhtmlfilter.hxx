fc06564c447d10ca5bec917e71f79151c8c37682
Kurt Zenker <kz@openoffice.org>
2005-01-13 16:24:59 +0000

    INTEGRATION: CWS impress26 (1.2.304); FILE MERGED 2004/12/16 13:40:39 cl 1.2.304.1: #i39045# #i21440# hide spell checker marks during graphic and html export

--- sd/inc/sdhtmlfilter.hxx
+++ sd/inc/sdhtmlfilter.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sdhtmlfilter.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-01-20 10:17:58 $
+ *  last change: $Author: kz $ $Date: 2005-01-13 17:24:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -82,6 +82,8 @@ public:
 
 private:
     DECL_LINK( IOProgressHdl, USHORT* );
+    bool mbHideSpell;
+
 };
 
 #endif // _SD_SDHTMLFILTER_HXX
