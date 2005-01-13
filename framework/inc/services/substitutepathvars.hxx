c6e977dbf83bd5839ef40b5418e19c7b786c190f
Kurt Zenker <kz@openoffice.org>
2005-01-13 18:14:01 +0000

    INTEGRATION: CWS babelfish (1.5.160); FILE MERGED 2004/12/01 18:08:52 ihi 1.5.160.2: RESYNC: (1.5-1.6); FILE MERGED 2004/11/18 19:38:16 ihi 1.5.160.1: #i35265# Remove REPLACEMENT_*, use ISO codes instead

--- framework/inc/services/substitutepathvars.hxx
+++ framework/inc/services/substitutepathvars.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: substitutepathvars.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-17 08:16:09 $
+ *  last change: $Author: kz $ $Date: 2005-01-13 19:14:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -358,7 +358,6 @@ class SubstitutePathVariables :     public com::sun::star::lang::XTypeProvider  
          rtl::OUString  GetPathVariableValue() const;
 
         rtl::OUString   GetHomeVariableValue() const;
-        rtl::OUString   GetLanguageString( LanguageType eLanguageType ) const;
 
         // XStringSubstitution implementation methods
         rtl::OUString impl_substituteVariable( const ::rtl::OUString& aText, sal_Bool bSustRequired )
