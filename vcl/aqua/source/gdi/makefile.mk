61765c5ab2e87eabf45a68c6bfe691b05620d4ec
Kurt Zenker <kz@openoffice.org>
2005-01-13 16:24:23 +0000

    INTEGRATION: CWS vclcompact (1.8.262); FILE MERGED 2004/11/29 10:07:07 dv 1.8.262.1: #i37763# Reduce vcl exports

--- vcl/aqua/source/gdi/makefile.mk
+++ vcl/aqua/source/gdi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: vg $ $Date: 2004-01-06 12:45:01 $
+#   last change: $Author: kz $ $Date: 2005-01-13 17:24:23 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -71,6 +71,7 @@ TARGET=salgdi
 .INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
 .INCLUDE :  sv.mk
+.INCLUDE :	$(PRJ)$/util$/makefile2.pmk
 
 # --- Files --------------------------------------------------------
 
