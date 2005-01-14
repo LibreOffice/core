cb02e5545f9af872dd74cc46697d832808d91726
Kurt Zenker <kz@openoffice.org>
2005-01-14 10:33:30 +0000

    INTEGRATION: CWS ause015 (1.12.20); FILE MERGED 2005/01/06 14:26:01 hjs 1.12.20.1: #40027# add postprocess to get updated uiconfig.zip on build --all

--- solenv/bin/cwsresync.pl
+++ solenv/bin/cwsresync.pl
@@ -5,9 +5,9 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 #
 #   $RCSfile: cwsresync.pl,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: hr $ $Date: 2004-12-13 17:26:41 $
+#   last change: $Author: kz $ $Date: 2005-01-14 11:33:30 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -108,7 +108,7 @@ use CwsConfig;
 ( my $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
 my $script_rev;
-my $id_str = ' $Revision: 1.12 $ ';
+my $id_str = ' $Revision: 1.13 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -124,6 +124,7 @@ my %obligatory_modules = ();
 $obligatory_modules{'solenv'}++;
 $obligatory_modules{'default_images'}++;
 $obligatory_modules{'custom_images'}++;
+$obligatory_modules{'postprocess'}++;
 $obligatory_modules{'instset_native'}++;
 $obligatory_modules{'instsetoo_native'}++;
 $obligatory_modules{'smoketest_native'}++;
