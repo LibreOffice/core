474fc7f8b50d78a67a2ee803c2e20ce09837cf13
Kurt Zenker <kz@openoffice.org>
2005-01-14 11:01:12 +0000

    INTEGRATION: CWS impress25 (1.25.16); FILE MERGED 2004/12/17 14:06:01 cl 1.25.16.2: #i38753# fixed header/footer-decl export position 2004/12/10 16:58:09 cl 1.25.16.1: #i38514# do not crash when no document was set

--- xmloff/source/draw/ximpbody.cxx
+++ xmloff/source/draw/ximpbody.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ximpbody.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 19:33:51 $
+ *  last change: $Author: kz $ $Date: 2005-01-14 12:01:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -382,6 +382,13 @@ SvXMLImportContext *SdXMLBodyContext::CreateChildContext(
 
     switch(rTokenMap.Get(nPrefix, rLocalName))
     {
+        case XML_TOK_BODY_HEADER_DECL:
+        case XML_TOK_BODY_FOOTER_DECL:
+        case XML_TOK_BODY_DATE_TIME_DECL:
+        {
+            pContext = new SdXMLHeaderFooterDeclContext( GetImport(), nPrefix, rLocalName, xAttrList );
+            break;
+        }
         case XML_TOK_BODY_PAGE:
         {
             // only read the first page in preview mode
@@ -390,6 +397,9 @@ SvXMLImportContext *SdXMLBodyContext::CreateChildContext(
                 // import this page
                 uno::Reference< drawing::XDrawPage > xNewDrawPage;
                 uno::Reference< drawing::XDrawPages > xDrawPages(GetSdImport().GetLocalDrawPages(), uno::UNO_QUERY);
+
+                if( !xDrawPages.is() )
+                    break;
 
                 if(GetSdImport().GetNewPageCount() + 1 > xDrawPages->getCount())
                 {
