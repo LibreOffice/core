58c654b4d7b4101cd48351259251e83a6a469de6
Kurt Zenker <kz@openoffice.org>
2005-01-18 14:48:21 +0000

    INTEGRATION: CWS fwkbugfix04 (1.2.214); FILE MERGED 2004/12/15 13:09:42 mba 1.2.214.1: obsolete sfx headers removed

--- sc/source/ui/inc/tbinsert.hxx
+++ sc/source/ui/inc/tbinsert.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tbinsert.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-06 12:54:04 $
+ *  last change: $Author: kz $ $Date: 2005-01-18 15:48:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,10 +64,6 @@
 
 #ifndef _SFXTBXCTRL_HXX //autogen
 #include <sfx2/tbxctrl.hxx>
-#endif
-
-#ifndef _SFXTBXMGR_HXX //autogen
-#include <sfx2/tbxmgr.hxx>
 #endif
 
 //----------------------------------------------------------------------------
