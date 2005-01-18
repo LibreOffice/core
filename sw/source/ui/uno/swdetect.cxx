1471150bdba676d007a06f2ff6483656eb6dd2fd
Kurt Zenker <kz@openoffice.org>
2005-01-18 14:27:29 +0000

    INTEGRATION: CWS fwkbugfix04 (1.5.38); FILE MERGED 2005/01/09 12:53:00 mba 1.5.38.1: #i33168#: wrong template flag used

--- sw/source/ui/uno/swdetect.cxx
+++ sw/source/ui/uno/swdetect.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swdetect.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-17 15:20:43 $
+ *  last change: $Author: kz $ $Date: 2005-01-18 15:27:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -336,7 +336,7 @@ SwFilterDetect::~SwFilterDetect()
                             aTypeName = pFilter->GetTypeName();
                         }
 
-                        aTypeName = SfxFilter::GetTypeFromStorage( xStorage, pFilter ? pFilter->IsAllowedAsTemplate() : FALSE, &aFilterName );
+                        aTypeName = SfxFilter::GetTypeFromStorage( xStorage, pFilter ? pFilter->IsOwnTemplateFormat() : FALSE, &aFilterName );
                     }
                     catch( lang::WrappedTargetException& aWrap )
                     {
