cfb3d6badc2a3d7902df7f1339444f879f3b8f14
Kurt Zenker <kz@openoffice.org>
2005-01-18 14:46:59 +0000

    INTEGRATION: CWS fwkbugfix04 (1.6.140); FILE MERGED 2004/12/15 12:58:52 mba 1.6.140.1: #i33585#: remove obsolete resources

--- sc/sdi/makefile.mk
+++ sc/sdi/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: obo $ $Date: 2004-08-12 09:27:17 $
+#   last change: $Author: kz $ $Date: 2005-01-18 15:46:59 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -100,10 +100,6 @@ SVSDIDEPEND= \
     pgbrksh.sdi \
     scslots.hrc \
     $(INC)$/sc.hrc
-
-SRS1NAME=$(TARGET)
-SRC1FILES= \
-    scslots.src
 
 # --- Targets -------------------------------------------------------
 
