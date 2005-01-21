1a47619b4552cefaa40ec631b679eb14e82469d3
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:42:55 +0000

    INTEGRATION: CWS visibility01 (1.41.286); FILE MERGED 2004/11/01 20:17:23 mhu 1.41.286.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/numpages.cxx
+++ svx/source/dialog/numpages.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: numpages.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: rt $ $Date: 2005-01-11 12:57:03 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:42:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #include <numpages.hxx>
 #include <numpages.hrc>
