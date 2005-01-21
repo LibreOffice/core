d6a7edd9841d018d3a192f4bb6ac1518fdd97d2b
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:34:02 +0000

    INTEGRATION: CWS visibility01 (1.2.142); FILE MERGED 2004/11/15 20:39:17 mhu 1.2.142.2: RESYNC: (1.2-1.3); FILE MERGED 2004/11/01 20:17:17 mhu 1.2.142.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/dbregisterednamesconfig.cxx
+++ svx/source/dialog/dbregisterednamesconfig.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbregisterednamesconfig.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-10-22 08:52:41 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:34:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,11 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
+
 #include "dbregisterednamesconfig.hxx"
 
 
