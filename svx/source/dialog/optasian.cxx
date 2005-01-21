fd202b7f4c68c17b3a81e562070dce0be1c0a6ef
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:43:39 +0000

    INTEGRATION: CWS visibility01 (1.2.434); FILE MERGED 2004/11/01 20:17:24 mhu 1.2.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/optasian.cxx
+++ svx/source/dialog/optasian.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optasian.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:38:40 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:43:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #ifndef _SVX_OPTASIAN_HXX
 #include <optasian.hxx>
