c5d6918237e32725adc42ffe978e8b828b4093fb
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:56:37 +0000

    INTEGRATION: CWS visibility01 (1.5.278); FILE MERGED 2004/12/22 03:24:42 mnicel 1.5.278.1: Part of symbol visibility work - #i35758#

--- svx/source/gallery2/makefile.mk
+++ svx/source/gallery2/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2004-05-21 09:43:13 $
+#   last change: $Author: kz $ $Date: 2005-01-21 16:56:37 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -67,6 +67,7 @@ TARGET=gal
 # --- Settings -----------------------------------------------------
 
 .INCLUDE : settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
