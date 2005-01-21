e5a579354923e97d05ea29200b26ad7277eb3e5a
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:37:30 +0000

    INTEGRATION: CWS visibility01 (1.16.820); FILE MERGED 2004/11/01 20:17:20 mhu 1.16.820.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/hlmailtp.cxx
+++ svx/source/dialog/hlmailtp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hlmailtp.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2003-04-24 14:46:43 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:37:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDER_HPP_
 #include <com/sun/star/frame/XDispatchProvider.hpp>
