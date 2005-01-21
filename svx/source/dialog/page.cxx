966b0464a6dcbca4d69d178c5892bf635021e3da
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:47:18 +0000

    INTEGRATION: CWS visibility01 (1.25.286); FILE MERGED 2004/11/01 20:17:28 mhu 1.25.286.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/page.cxx
+++ svx/source/dialog/page.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: page.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2004-05-10 16:53:17 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:47:18 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,11 @@
  *
  ************************************************************************/
 
-// include ---------------------------------------------------------------
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
-#pragma hdrstop
+// include ---------------------------------------------------------------
 
 #define ITEMID_BRUSH        0
 #define ITEMID_BOX          0
