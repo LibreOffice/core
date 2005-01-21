dcf8e24ba601558aa6d7d397d2ddb088d4703512
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:26:34 +0000

    INTEGRATION: CWS visibility01 (1.17.140); FILE MERGED 2004/11/01 20:17:11 mhu 1.17.140.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/align.cxx
+++ svx/source/dialog/align.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: align.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 17:39:45 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:26:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
