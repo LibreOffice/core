1df0b484cb9a23871bbbc0dc684b31253b736f19
Kurt Zenker <kz@openoffice.org>
2005-01-21 08:47:59 +0000

    INTEGRATION: CWS ause016 (1.3.124); FILE MERGED 2005/01/18 10:51:57 hjs 1.3.124.1: #i35263# avoid language specific lines in d.lst

--- readlicense_oo/makefile.mk
+++ readlicense_oo/makefile.mk
@@ -1,17 +1,42 @@
 PRJ=.
-PRJNAME=readlicense
+PRJNAME=readlicense_oo
 TARGET=source
 
 # ------------------------------------------------------------------
 .INCLUDE: settings.mk
 # ------------------------------------------------------------------
 
-$MAKEALLTARGETS: convert
+.IF "$(GUI)"=="WNT"
+SYSLICBASE=license.txt license.html license.rtf
+SYSLICDEST=$(MISC)$/license$/wnt
+.ELSE          # "$(GUI)"=="WNT"
+SYSLICBASE=LICENSE LICENSE.html
+SYSLICDEST=$(MISC)$/license$/unx
+.ENDIF          # "$(GUI)"=="WNT"
 
-convert: 
-    @+-$(PERL) conv.pl -o $(MISC)
+fallbacklicenses=$(foreach,i,{$(subst,$(defaultlangiso), $(alllangiso))} $(foreach,j,$(SYSLICBASE) $(SYSLICDEST)$/$(j:b)_$i$(j:e)))
 
 # ------------------------------------------------------------------
 .INCLUDE: target.mk
 # ------------------------------------------------------------------
+
+ALLTAR: convert $(fallbacklicenses) just_for_nice_optics
+
+.IF "$(fallbacklicenses)"!=""
+$(fallbacklicenses) : convert
+    @+$(ECHON) .
+    @+$(COPY) $(@:d)$(@:b:s/_/./:b)_$(defaultlangiso)$(@:e) $@ $(noout)
+.ENDIF          # "$(fallbacklicenses)"!=""
+
+just_for_nice_optics: $(fallbacklicenses)
+    @+$(ECHONL)
+    @+echo done.
+
+convert:
+    @+echo converting license files
+    @+-$(PERL) conv.pl -o $(MISC)
+# no conversion for *.rtf
+.IF "$(GUI)"=="WNT"
+    @+$(COPY) source$/license$/wnt$/*.rtf $(SYSLICDEST) $(noout)
+.ENDIF          # "$(GUI)"=="WNT"
 
