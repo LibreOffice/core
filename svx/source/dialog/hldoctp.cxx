849b08907b783579a5a5dd56857dabd7fc21fd2e
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:36:57 +0000

    INTEGRATION: CWS visibility01 (1.20.434); FILE MERGED 2004/11/01 20:17:20 mhu 1.20.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/hldoctp.cxx
+++ svx/source/dialog/hldoctp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hldoctp.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:28:46 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:36:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #include "cuihyperdlg.hxx"
 
