21e26b37edd134aef026a023fc12ea0bc140a3f1
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:31:45 +0000

    INTEGRATION: CWS visibility01 (1.3.118); FILE MERGED 2005/01/12 12:17:01 mnicel 1.3.118.2: RESYNC: (1.3-1.4); FILE MERGED 2004/11/01 20:17:15 mhu 1.3.118.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/cuigaldlg.cxx
+++ svx/source/dialog/cuigaldlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cuigaldlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2004-12-13 12:14:26 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:31:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #include <algorithm>
 #include <ucbhelper/content.hxx>
