d2fadf6395bf0bcdd88c866f4094776cb6d0cda5
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:53:11 +0000

    INTEGRATION: CWS visibility01 (1.17.434); FILE MERGED 2005/01/12 12:15:05 mnicel 1.17.434.2: RESYNC: (1.17-1.18); FILE MERGED 2004/11/01 20:17:32 mhu 1.17.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/tphatch.cxx
+++ svx/source/dialog/tphatch.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tphatch.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2004-12-13 12:17:27 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:53:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
