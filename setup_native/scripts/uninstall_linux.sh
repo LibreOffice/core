c5c15eff945be4e9fc5dc3d9cc8c3d915813cf6a
Kurt Zenker <kz@openoffice.org>
2005-01-21 10:34:46 +0000

    INTEGRATION: CWS sysui03 (1.3.2); FILE MERGED 2005/01/18 12:56:30 obr 1.3.2.2: #i39410# switched back to rpm from environment due to problems on baseline machines 2005/01/14 07:08:53 obr 1.3.2.1: #i39410# check for gnome packages and other enhancements

--- setup_native/scripts/uninstall_linux.sh
+++ setup_native/scripts/uninstall_linux.sh
@@ -4,14 +4,19 @@
 # No parameter required, all RPMs listed in $HOME/.RPM_OFFICEDATABASE
 # will be removed.
 
-if [ $# -ne 0 ]
+if [ $# -ne 1 ]
 then
-  echo "No parameters required"
-  echo "All RPMs listed in $HOME/.RPM_OFFICEDATABASE will be removed"
+  echo
+  echo "Usage:" $0 "<office-installation-dir>"
+  echo "    <inst-destination-dir>: directory where the office to be removed is installed"
+  echo
   exit 2
 fi
 
-RPM_DB_PATH=$HOME/.RPM_OFFICEDATABASE
+#RPM_DB_PATH=$HOME/.RPM_OFFICEDATABASE
+RPM_DB_PATH=$1/.RPM_OFFICEDATABASE
+
+RPMLIST=`rpm --dbpath $RPM_DB_PATH --query --all`
 
 # Output ...
 clear
@@ -21,12 +26,15 @@ echo "#########################################"
 echo
 echo "Path to the RPM database: " $RPM_DB_PATH
 echo "RPMs to deinstall:"
-echo "`rpm --dbpath $RPM_DB_PATH --query --all`"
+echo "$RPMLIST"
 echo "===================================================================="
 echo
-rpm --dbpath $RPM_DB_PATH --erase `rpm --dbpath $RPM_DB_PATH --query --all`
 
-echo
+# Restore original bootstraprc
+mv -f $1/program/bootstraprc.orig $1/program/bootstraprc
+
+rpm --dbpath $RPM_DB_PATH --erase $RPMLIST || exit 2
+
 echo "Deleting directories"
 rm -rf $RPM_DB_PATH
 
