8cf936389be00c2dcab789eb417ab1199b6cf108
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:27:23 +0000

    INTEGRATION: CWS visibility01 (1.1.1.1.1050); FILE MERGED 2004/11/01 20:17:12 mhu 1.1.1.1.1050.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/bbdlg.cxx
+++ svx/source/dialog/bbdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bbdlg.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 17:01:06 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:27:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
