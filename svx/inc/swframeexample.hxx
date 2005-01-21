d667557c75924b13864722f08ae65ece4269df87
Kurt Zenker <kz@openoffice.org>
2005-01-21 14:35:56 +0000

    INTEGRATION: CWS visibility01 (1.2.224); FILE MERGED 2004/12/06 08:10:49 mnicel 1.2.224.1: Part of symbol visibility markup - #i35758#

--- svx/inc/swframeexample.hxx
+++ svx/inc/swframeexample.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: swframeexample.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hjs $ $Date: 2004-06-28 14:02:27 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 15:35:56 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,9 +72,13 @@
 #include <tools/gen.hxx>
 #endif
 
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
 // class SwFrmPagePreview -------------------------------------------------------
 
-class SvxSwFrameExample : public Window
+class SVX_DLLPUBLIC SvxSwFrameExample : public Window
 {
     Color       m_aTransColor;      // transparency
     Color       m_aBgCol;           // background
