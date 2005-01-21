0f117d53d9b75438bbdf2c2e70629a1d3557c225
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:45:48 +0000

    INTEGRATION: CWS visibility01 (1.4.118); FILE MERGED 2005/01/12 12:20:27 mnicel 1.4.118.3: RESYNC: (1.5-1.6); FILE MERGED 2004/11/15 20:41:55 mhu 1.4.118.2: RESYNC: (1.4-1.5); FILE MERGED 2004/11/01 20:17:27 mhu 1.4.118.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/optjava.cxx
+++ svx/source/dialog/optjava.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optjava.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2004-12-10 17:26:00 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:45:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
