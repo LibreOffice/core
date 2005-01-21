48e57a6e8a675e805ec1dd2a70c7ae9823eb8994
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:39:57 +0000

    INTEGRATION: CWS visibility01 (1.2.434); FILE MERGED 2004/11/01 20:17:22 mhu 1.2.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/internationaloptions.cxx
+++ svx/source/dialog/internationaloptions.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: internationaloptions.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:34:16 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:39:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #ifndef _OFFAPP_INTERNATIONALOPTIONS_HXX_
 #include "internationaloptions.hxx"
