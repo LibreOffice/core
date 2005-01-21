0745d837d4890d837d11ae02de8fedff1c550636
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:59:17 +0000

    INTEGRATION: CWS impress23 (1.1.2); FILE ADDED 2004/12/08 12:44:35 thb 1.1.2.1: #i36214# Restructured activities factory into several files, to increase maintainability (in preparation for implementing global time handling)

---
+++ slideshow/source/engine/activities/continuouskeytimeactivitybase.hxx
@@ -0,0 +1,117 @@
+/*************************************************************************
+ *
+ *  $RCSfile: continuouskeytimeactivitybase.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:59:17 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SLIDESHOW_CONTINUOUSKEYTIMEACTIVITYBASE_HXX
+#define _SLIDESHOW_CONTINUOUSKEYTIMEACTIVITYBASE_HXX
+
+#include <simplecontinuousactivitybase.hxx>
+
+#include <vector>
+
+
+namespace presentation
+{
+    namespace internal
+    {
+        /** Interpolated, key-times animation.
+
+            This class implements an interpolated key-times
+            animation, with continuous time.
+        */
+        class ContinuousKeyTimeActivityBase : public SimpleContinuousActivityBase
+        {
+        public:
+            ContinuousKeyTimeActivityBase( const ActivityParameters& rParms );
+
+            /** Hook for derived classes
+
+                This method will be called from perform(), already
+                equipped with the modified time (nMinNumberOfFrames, repeat,
+                acceleration and deceleration taken into account).
+
+                @param nIndex
+                Current index of the key times/key values.
+
+                @param nFractionalIndex
+                Fractional value from the [0,1] range, specifying
+                the position between nIndex and nIndex+1.
+
+                @param nRepeatCount
+                Number of full repeats already performed
+            */
+            virtual void perform( sal_uInt32    nIndex,
+                                  double        nFractionalIndex,
+                                  sal_uInt32    nRepeatCount ) const = 0;
+
+            /// From SimpleContinuousActivityBase class
+            virtual void simplePerform( double      nSimpleTime,
+                                        sal_uInt32  nRepeatCount ) const;
+
+        protected:
+            const ::std::vector< double >   maKeyTimes;
+
+            /// last active index in maKeyTimes (to avoid frequent searching)
+            mutable ::std::size_t           mnLastIndex;
+        };
+    }
+}
+
+#endif /* _SLIDESHOW_CONTINUOUSKEYTIMEACTIVITYBASE_HXX */
