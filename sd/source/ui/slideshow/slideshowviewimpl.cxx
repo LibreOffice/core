a75b6ef7f2a26d6881e68e25f6ce6c3231d9901f
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:35:27 +0000

    INTEGRATION: CWS impress23 (1.3.22); FILE MERGED 2005/01/11 14:48:12 dbo 1.3.22.1: #i39098# making slideshow creation/startShow()/startPreview() more robust

--- sd/source/ui/slideshow/slideshowviewimpl.cxx
+++ sd/source/ui/slideshow/slideshowviewimpl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: slideshowviewimpl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2004-12-09 16:11:32 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:35:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
