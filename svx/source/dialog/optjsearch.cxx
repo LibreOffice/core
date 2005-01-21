85890a33a858b0b4a05e71248855bc5129289918
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:46:12 +0000

    INTEGRATION: CWS visibility01 (1.2.434); FILE MERGED 2004/11/01 20:17:27 mhu 1.2.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/optjsearch.cxx
+++ svx/source/dialog/optjsearch.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optjsearch.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:45:59 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:46:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 
 #ifndef _SHL_HXX
