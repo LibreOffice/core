fd6284caf8da0c03eb22dbd8fd1655f1eccb1fe8
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:44:51 +0000

    INTEGRATION: CWS visibility01 (1.15.84); FILE MERGED 2005/01/12 12:21:52 mnicel 1.15.84.3: RESYNC: (1.16-1.17); FILE MERGED 2004/11/15 20:41:28 mhu 1.15.84.2: RESYNC: (1.15-1.16); FILE MERGED 2004/11/01 20:17:26 mhu 1.15.84.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/optgdlg.cxx
+++ svx/source/dialog/optgdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optgdlg.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-18 13:29:19 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:44:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #ifndef _ZFORLIST_HXX //autogen
 #include <svtools/zforlist.hxx>
