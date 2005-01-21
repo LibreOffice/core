d5da0767e5cdef4cac9a39f6e29defc40d01908b
Kurt Zenker <kz@openoffice.org>
2005-01-21 13:56:11 +0000

    INTEGRATION: CWS visibility01 (1.3.26); FILE MERGED 2004/12/06 08:10:27 mnicel 1.3.26.1: Part of symbol visibility markup - #i35758#

--- svx/inc/measctrl.hxx
+++ svx/inc/measctrl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: measctrl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pjunck $ $Date: 2004-11-03 10:10:08 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 14:56:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,6 +67,10 @@
 #include <vcl/ctrl.hxx>
 #endif
 
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
 
 class SfxItemSet;
 
@@ -79,7 +83,7 @@ class SdrModel;
 |* SvxXMeasurePreview
 |*
 \************************************************************************/
-class SvxXMeasurePreview : public Control
+class SVX_DLLPUBLIC SvxXMeasurePreview : public Control
 {
  friend class SvxMeasurePage;
 
