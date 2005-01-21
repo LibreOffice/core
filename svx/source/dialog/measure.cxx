538292bdfc86053ca02134b892efb8068f67f413
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:40:49 +0000

    INTEGRATION: CWS visibility01 (1.9.434); FILE MERGED 2004/11/01 20:17:22 mhu 1.9.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/measure.cxx
+++ svx/source/dialog/measure.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: measure.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:36:21 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:40:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 
 // include ---------------------------------------------------------------
