84287b1835db373d2488e4aed958f9fea54ff873
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:48:01 +0000

    INTEGRATION: CWS visibility01 (1.2.74); FILE MERGED 2004/11/01 20:17:29 mhu 1.2.74.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/pastedlg.cxx
+++ svx/source/dialog/pastedlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pastedlg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-10-04 17:48:27 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:47:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,14 +58,10 @@
  *
  *
  ************************************************************************/
-/*
-#ifdef WNT
-#include <tools/prewin.h>
-#include <windows.h>
-#include <ole2.h>
-#include <tools/postwin.h>
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
 #endif
-*/
 
 #ifndef _COM_SUN_STAR_EMBED_ASPECTS_HPP_
 #include <com/sun/star/embed/Aspects.hpp>
--- svx/source/dialog/plfilter.cxx
+++ svx/source/dialog/plfilter.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: plfilter.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2004-10-04 17:48:47 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:48:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #include <rope>
 #include <set>
