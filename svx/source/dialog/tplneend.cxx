57ae94766951637770e0901199b8b7c9e31fd1f0
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:54:01 +0000

    INTEGRATION: CWS visibility01 (1.16.434); FILE MERGED 2005/01/12 12:23:25 mnicel 1.16.434.3: RESYNC: (1.17-1.18); FILE MERGED 2004/11/15 20:43:31 mhu 1.16.434.2: RESYNC: (1.16-1.17); FILE MERGED 2004/11/01 20:17:33 mhu 1.16.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/tplneend.cxx
+++ svx/source/dialog/tplneend.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tplneend.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2004-12-13 12:17:57 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:54:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
