c37b8959cbd373c0e948238335fa2d46ec3c1485
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:52:22 +0000

    INTEGRATION: CWS visibility01 (1.19.434); FILE MERGED 2005/01/12 12:18:33 mnicel 1.19.434.2: RESYNC: (1.19-1.20); FILE MERGED 2004/11/01 20:17:32 mhu 1.19.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/tpbitmap.cxx
+++ svx/source/dialog/tpbitmap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tpbitmap.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2004-12-13 12:16:41 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:52:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
