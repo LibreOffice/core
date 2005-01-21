e7f9607d7f984cb5f6f15218aa9d162bfc58e47d
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:57:58 +0000

    INTEGRATION: CWS visibility01 (1.12.118); FILE MERGED 2005/01/12 12:45:00 mnicel 1.12.118.2: RESYNC: (1.12-1.13); FILE MERGED 2004/12/22 03:24:43 mnicel 1.12.118.1: Part of symbol visibility work - #i35758#

--- svx/source/msfilter/makefile.mk
+++ svx/source/msfilter/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: rt $ $Date: 2004-11-09 09:38:47 $
+#   last change: $Author: kz $ $Date: 2005-01-21 16:57:58 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -69,6 +69,7 @@ AUTOSEG=true
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
