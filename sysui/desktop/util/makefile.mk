4b11c5ceb8a2fce6429a10892e272289e860fc56
Kurt Zenker <kz@openoffice.org>
2005-01-21 10:26:03 +0000

    INTEGRATION: CWS sysui03 (1.5.52); FILE MERGED 2005/01/13 08:12:19 obr 1.5.52.3: #i36339# modification of /etc/mailcap and /etc/mime.types now done in the desktop integration packages 2004/12/21 12:59:08 obr 1.5.52.2: #i35698# replaced obsolete gnomeint by gnome-set-default-application 2004/12/14 10:02:58 obr 1.5.52.1: removed obsolete 1.x desktop integration

--- sysui/desktop/util/makefile.mk
+++ sysui/desktop/util/makefile.mk
@@ -61,34 +61,49 @@ PRJ=..$/..
 PRJNAME=sysui
 TARGET=launcher
 
+TARGETTYPE=CUI
+LIBTARGET=NO
+NO_DEFAULT_STL=TRUE
+LIBSALCPPRT=$(0)
+
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
 
+.IF "$(ENABLE_GNOMEVFS)"!=""
+COMPILER_WARN_ALL=TRUE
+PKGCONFIG_MODULES=gnome-vfs-2.0
+.INCLUDE: pkg_config.mk
+
+.IF "$(OS)" == "SOLARIS"
+LINKFLAGS+=-z nodefs
+.ENDIF # "$(OS)" == "SOLARIS"
+
+# Disable lazy binding of symbols
+.IF "$(OS)" == "LINUX" || "$(OS)" == "SOLARIS"
+LINKFLAGS+=-z now
+.ENDIF  
+
+.IF "$(OS)" == "LINUX"
+# hack for faked SO environment
+CFLAGS+=-gdwarf-2
+PKGCONFIG_LIBS!:=-Wl,--export-dynamic $(PKGCONFIG_LIBS)
+.ENDIF
+
+.ENDIF          # "$(ENABLE_GNOMEVFS)"!=""
+
 # --- Files --------------------------------------------------------
 
-ZIPFLAGS= -u -r
+.IF "$(ENABLE_GNOMEVFS)"!=""
+OBJFILES += $(OBJ)$/gnome-set-default-application.obj
 
-ZIP1TARGET = kdeapp
-ZIP1LIST   = share/applnk/*
-ZIP1DIR    = $(MISC)$/kde
-
-ZIP2TARGET = kdemime
-ZIP2LIST   = share/mimelnk/*
-ZIP2DIR    = $(MISC)$/kde
-
-ZIP3TARGET = gnome2apps
-ZIP3LIST   = *.desktop
-ZIP3DIR    = $(MISC)$/gnome
+APP1TARGET=gnome-set-default-application
+APP1OBJS=$(OBJ)$/gnome-set-default-application.obj
+APP1LIBS=
+APP1STDLIBS+=$(PKGCONFIG_LIBS)
+.ENDIF          # "$(ENABLE_GNOMEVFS)"!=""
 
 SCRIPTS= \
-    $(BIN)$/update-mime-data.sh \
-    $(BIN)$/update-user-mime-data.sh \
-    $(BIN)$/update-redhat-user-menus.sh \
-    $(BIN)$/update-suse-user-menus.sh \
-    $(BIN)$/update-user-menus.sh \
-    $(BIN)$/functions.sh \
-    $(BIN)$/gnomeint.sh \
     $(BIN)$/viewdoc.sh
 
 # --- Targets ------------------------------------------------------
