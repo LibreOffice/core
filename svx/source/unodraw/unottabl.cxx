57ebf30cc0621108c4787c9ee07e1d24817fd858
Kurt Zenker <kz@openoffice.org>
2005-01-21 16:02:42 +0000

    INTEGRATION: CWS visibility01 (1.5.1040); FILE MERGED 2005/01/12 12:31:43 mnicel 1.5.1040.2: RESYNC: (1.5-1.6); FILE MERGED Resolved the line conflict because of the introduction of another header. 2004/12/06 08:11:29 mnicel 1.5.1040.1: Part of symbol visibility markup - #i35758#

--- svx/source/unodraw/unottabl.cxx
+++ svx/source/unodraw/unottabl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unottabl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2004-11-26 15:10:35 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 17:02:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -80,6 +80,7 @@
 
 #include "svdmodel.hxx"
 #include "xflhtit.hxx"
+#include "unofill.hxx"
 #include "unomid.hxx"
 
 #ifndef _SVX_UNONAMEITEMTABLE_HXX_
