8a30adaae0ab66a7d139787bbb3874ddb7de7fc0
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:40:32 +0000

    INTEGRATION: CWS visibility01 (1.2.74); FILE MERGED 2005/01/12 12:08:35 mnicel 1.2.74.3: RESYNC: (1.3-1.5); FILE MERGED 2004/11/15 20:40:16 mhu 1.2.74.2: RESYNC: (1.2-1.3); FILE MERGED 2004/11/01 20:17:22 mhu 1.2.74.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/linkdlg.cxx
+++ svx/source/dialog/linkdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: linkdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2004-12-13 12:15:58 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:40:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #include <linkdlg.hxx>
 
