968723a3e9a7bfc4b3937198721950cc362b5604
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:45:19 +0000

    INTEGRATION: CWS visibility01 (1.2.434); FILE MERGED 2004/11/01 20:17:26 mhu 1.2.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/opthtml.cxx
+++ svx/source/dialog/opthtml.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: opthtml.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:44:02 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:45:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,11 @@
  *
  ************************************************************************/
 
-// include ---------------------------------------------------------------
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
-#pragma hdrstop
+// include ---------------------------------------------------------------
 
 #include "htmlcfg.hxx"
 #include "opthtml.hxx"
