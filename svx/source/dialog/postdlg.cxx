1d811ba00d46e830a9ced7a62c64e488ce2e0ab2
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:48:25 +0000

    INTEGRATION: CWS visibility01 (1.5.1008); FILE MERGED 2004/11/01 20:17:29 mhu 1.5.1008.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/postdlg.cxx
+++ svx/source/dialog/postdlg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: postdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: os $ $Date: 2002-06-10 14:42:30 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:48:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
