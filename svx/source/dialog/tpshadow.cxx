aea6b1a6dd44af71dd980d92a1227c1f643a7592
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:54:20 +0000

    INTEGRATION: CWS visibility01 (1.9.434); FILE MERGED 2004/11/01 20:17:33 mhu 1.9.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/tpshadow.cxx
+++ svx/source/dialog/tpshadow.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: tpshadow.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:58:44 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:54:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
