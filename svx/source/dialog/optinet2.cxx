ff6026b8ce3e672c2b2cf3292cff923ae8d01fa7
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:45:32 +0000

    INTEGRATION: CWS visibility01 (1.9.76); FILE MERGED 2005/01/12 12:08:21 mnicel 1.9.76.2: RESYNC: (1.9-1.10); FILE MERGED 2004/11/01 20:17:26 mhu 1.9.76.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/optinet2.cxx
+++ svx/source/dialog/optinet2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: optinet2.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 14:48:07 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:45:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 // include ---------------------------------------------------------------
 
