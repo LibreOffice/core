8c217763693b23d1a175a633f25fdeca0963e65a
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:49:28 +0000

    INTEGRATION: CWS visibility01 (1.2.434); FILE MERGED 2004/11/01 20:17:30 mhu 1.2.434.1: #i35758# Undefine SVX_DLLIMPLEMENTATION for objects going into 'cui' library.

--- svx/source/dialog/sdbcdriverenum.cxx
+++ svx/source/dialog/sdbcdriverenum.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sdbcdriverenum.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-03 18:49:13 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:49:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,10 @@
  *
  *
  ************************************************************************/
+
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
 
 #ifndef _OFFMGR_SDBCDRIVERENUMERATION_HXX_
 #include "sdbcdriverenum.hxx"
