3fa07c9cdfb70569029ecab4bb457157e49f379e
Kurt Zenker <kz@openoffice.org>
2005-01-21 15:57:59 +0000

    INTEGRATION: CWS impress23 (1.1.2); FILE ADDED 2004/12/08 12:44:16 thb 1.1.2.1: #i36214# Restructured activities factory into several files, to increase maintainability (in preparation for implementing global time handling)

---
+++ slideshow/source/engine/activities/activitybase.cxx
@@ -0,0 +1,269 @@
+/*************************************************************************
+ *
+ *  $RCSfile: activitybase.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-01-21 16:57:59 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+// must be first
+#include <canvas/debug.hxx>
+#include <canvas/verbosetrace.hxx>
+
+#include <activitybase.hxx>
+
+
+namespace presentation
+{
+    namespace internal
+    {
+        // TODO(P1): Elide some virtual function calls, by templifying this
+        // static hierarchy
+
+        ActivityBase::ActivityBase( const ActivityParameters& rParms ) :
+            mpEndEvent( rParms.mrEndEvent ),
+            mrEventQueue( rParms.mrEventQueue ),
+            mpShape(),
+            mpAttributeLayer(),
+            maRepeats( rParms.mrRepeats ),
+            mnAccelerationFraction( rParms.mnAccelerationFraction ),
+            mnDecelerationFraction( rParms.mnDecelerationFraction ),
+            mbAutoReverse( rParms.mbAutoReverse ),
+            mbFirstPerformCall( true ),
+            mbIsActive( true )
+        {
+        }
+
+        void ActivityBase::dispose()
+        {
+            // dispose event
+            if( mpEndEvent.get() )
+                mpEndEvent->dispose();
+
+            // release references
+            mpEndEvent.reset();
+            mpShape.reset();
+            mpAttributeLayer.reset();
+
+            // deactivate
+            mbIsActive = false;
+        }
+
+        bool ActivityBase::perform()
+        {
+            // still active?
+            if( !isActive() )
+                return false; // no, early exit.
+
+            if( mbFirstPerformCall )
+            {
+                mbFirstPerformCall = false;
+
+                // notify derived classes that we're
+                // starting now
+                start();
+            }
+
+            return true;
+        }
+
+        bool ActivityBase::isActive() const
+        {
+            return mbIsActive;
+        }
+
+        bool ActivityBase::needsScreenUpdate() const
+        {
+            // animations, while active, _do_ need screen updates
+            // after perform() calls. This is because
+            // e.g. SlideChanger internally uses XCustomSprites,
+            // which don't repaint by themselves.
+            //
+            // What is actually a feature, because then, the
+            // activities thread can issue synchronized repaints,
+            // updating all visible sprites at once.
+
+            // FIXME(P1):
+            // Since by definition, the animation must not change the
+            // sprite when it is no longer active, we could possibly
+            // return true here only as long as mbIsActive is true.
+            // Unfortunately, the ActivitiesQueue asks for necessary
+            // screen updates after the perform calls, and this is a
+            // problem for the very last perform call of an activity
+            // (the one that ends the activity): this last one usually
+            // _does_ need a screen update, but has already set
+            // mbIsActive to false. I would consider delayed false returns
+            // here a hack, since it relies on call order. For the time
+            // being, we thus return true here, always (note that a
+            // finished Activity will normally be not queried for screen
+            // updates, anyway, since it's removed from the queues).
+            return true;
+        }
+
+        void ActivityBase::end()
+        {
+            mbIsActive = false;
+
+            // Activity is ending, queue event, then
+            if( mpEndEvent.get() )
+                mrEventQueue.addEvent( mpEndEvent );
+
+            // release references
+            mpEndEvent.reset();
+        }
+
+        void ActivityBase::setTargets( const AnimatableShapeSharedPtr&      rShape,
+                                       const ShapeAttributeLayerSharedPtr&  rAttrLayer )
+        {
+            ENSURE_AND_THROW( rShape.get(),
+                              "ActivityBase::setTargets(): Invalid shape" );
+            ENSURE_AND_THROW( rAttrLayer.get(),
+                              "ActivityBase::setTargets(): Invalid attribute layer" );
+
+            mpShape = rShape;
+            mpAttributeLayer = rAttrLayer;
+        }
+
+        double ActivityBase::calcAcceleratedTime( double nT ) const
+        {
+            // Handle acceleration/deceleration
+            // ================================
+
+            // clamp nT to permissible [0,1] range
+            nT = ::std::max( 0.0, ::std::min( 1.0, nT ) );
+
+            // take acceleration/deceleration into account. if the sum
+            // of mnAccelerationFraction and mnDecelerationFraction
+            // exceeds 1.0, ignore both (that's according to SMIL spec)
+            if( (mnAccelerationFraction > 0.0 ||
+                 mnDecelerationFraction > 0.0) &&
+                mnAccelerationFraction + mnDecelerationFraction <= 1.0 )
+            {
+                // calc accelerated/decelerated time.
+                //
+                // We have three intervals:
+                // 1 [0,a]
+                // 2 [a,d]
+                // 3 [d,1] (with a and d being acceleration/deceleration
+                // fraction, resp.)
+                //
+                // The change rate during interval 1 is constantly
+                // increasing, reaching 1 at a. It then stays at 1,
+                // starting a linear decrease at d, ending with 0 at
+                // time 1. The integral of this function is the
+                // required new time nT'.
+                //
+                // As we arbitrarily assumed 1 as the upper value of
+                // the change rate, the integral must be normalized to
+                // reach nT'=1 at the end of the interval. This
+                // normalization constant is:
+                //
+                // c = 1 - 0.5a - 0.5d
+                //
+                // The integral itself then amounts to:
+                //
+                // 0.5 nT^2 / a + (nT-a) + (nT - 0.5 nT^2 / d)
+                //
+                // (where each of the three summands correspond to the
+                // three intervals above, and are applied only if nT
+                // has reached the corresponding interval)
+                //
+                // The graph of the change rate is a trapezoid:
+                //
+                //   |
+                //  1|      /--------------\
+                //   |     /                \
+                //   |    /                  \
+                //   |   /                    \
+                //   -----------------------------
+                //      0   a              d  1
+                //
+                //
+                const double nC( 1.0 - 0.5*mnAccelerationFraction - 0.5*mnDecelerationFraction );
+
+                // this variable accumulates the new time value
+                double nTPrime(0.0);
+
+                if( nT < mnAccelerationFraction )
+                {
+                    nTPrime += 0.5*nT*nT/mnAccelerationFraction; // partial first interval
+                }
+                else
+                {
+                    nTPrime += 0.5*mnAccelerationFraction; // full first interval
+
+                    if( nT <= 1.0-mnDecelerationFraction )
+                    {
+                        nTPrime += nT-mnAccelerationFraction; // partial second interval
+                    }
+                    else
+                    {
+                        nTPrime += 1.0 - mnAccelerationFraction - mnDecelerationFraction; // full second interval
+
+                        const double nTRelative( nT - 1.0 + mnDecelerationFraction );
+
+                        nTPrime += nTRelative - 0.5*nTRelative*nTRelative / mnDecelerationFraction;
+                    }
+                }
+
+                // normalize, and assign to work variable
+                nT = nTPrime / nC;
+            }
+
+            return nT;
+        }
+    }
+}
