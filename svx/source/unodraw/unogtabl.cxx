0a5b3354cdb03c4207a5b846718010becf7c63ec
Kurt Zenker <kz@openoffice.org>
2005-01-21 16:01:49 +0000

    INTEGRATION: CWS visibility01 (1.5.1040); FILE MERGED 2005/01/12 12:30:58 mnicel 1.5.1040.2: RESYNC: (1.5-1.6); FILE MERGED Resolved the line conflict becuase of the introduction of another header. 2004/12/06 08:11:28 mnicel 1.5.1040.1: Part of symbol visibility markup - #i35758#

--- svx/source/unodraw/unogtabl.cxx
+++ svx/source/unodraw/unogtabl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unogtabl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2004-11-26 15:09:58 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 17:01:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,6 +79,7 @@
 #include "svdmodel.hxx"
 #include "xdef.hxx"
 #include "xflgrit.hxx"
+#include "unofill.hxx"
 #include "unomid.hxx"
 
 using namespace ::com::sun::star;
