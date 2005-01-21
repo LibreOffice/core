9efa173128f459d99556651b0ae499b712ab7908
Kurt Zenker <kz@openoffice.org>
2005-01-21 14:01:33 +0000

    INTEGRATION: CWS visibility01 (1.3.218); FILE MERGED 2004/11/19 12:54:20 mmeeks 1.3.218.1: Issue number: #i35758# Submitted by: mnicel Reviewed by:  mmeeks
    	Add lots of visibility attributes to APIs.

--- svx/inc/ofaitem.hxx
+++ svx/inc/ofaitem.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ofaitem.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-07-06 13:05:56 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 15:01:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,9 +67,13 @@
 #include <svtools/poolitem.hxx>
 #endif
 
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
 // class OfaPtrItem ------------------------------------------------------
 
-class OfaPtrItem : public SfxPoolItem
+class SVX_DLLPUBLIC OfaPtrItem : public SfxPoolItem
 {
 private:
     void* pPtr;
