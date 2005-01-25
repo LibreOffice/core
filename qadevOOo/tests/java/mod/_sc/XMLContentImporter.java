efd9ac1427b5382670a2fe5cc9c1e2a3ca6b3990
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:53:15 +0000

    INTEGRATION: CWS gcj1 (1.4.86); FILE MERGED 2004/12/01 15:55:58 cmc 1.4.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/XMLContentImporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLContentImporter.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: XMLContentImporter.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2004-01-05 19:30:03 $
+ *  last change:$Date: 2005-01-25 15:52:40 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,7 @@ public class XMLContentImporter extends TestCase {
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
