0af7272e78fa94c7e88efae757b82a6a72f24d89
Oliver Bolte <obo@openoffice.org>
2005-01-25 15:04:11 +0000

    INTEGRATION: CWS gcj1 (1.5.86); FILE MERGED 2004/12/01 15:56:08 cmc 1.5.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sw/SwXStyle.java
+++ qadevOOo/tests/java/mod/_sw/SwXStyle.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwXStyle.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-01-05 20:13:55 $
+ *  last change:$Date: 2005-01-25 16:04:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -118,7 +118,7 @@ public class SwXStyle extends TestCase {
             xTextDoc = SOF.createTextDoc( null );
         } catch ( com.sun.star.uno.Exception e ) {
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
