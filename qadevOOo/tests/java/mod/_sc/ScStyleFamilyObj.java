725d4a03c7c91210c54eafacad62c0f9f1d40539
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:51:59 +0000

    INTEGRATION: CWS gcj1 (1.5.86); FILE MERGED 2004/12/01 15:55:57 cmc 1.5.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScStyleFamilyObj.java
+++ qadevOOo/tests/java/mod/_sc/ScStyleFamilyObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScStyleFamilyObj.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-01-05 19:24:19 $
+ *  last change:$Date: 2005-01-25 15:51:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -119,7 +119,7 @@ public class ScStyleFamilyObj extends TestCase {
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
--- qadevOOo/tests/java/mod/_sc/ScStyleObj.java
+++ qadevOOo/tests/java/mod/_sc/ScStyleObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScStyleObj.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-01-05 19:24:43 $
+ *  last change:$Date: 2005-01-25 15:51:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,7 @@ public class ScStyleObj extends TestCase {
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
