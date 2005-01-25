a95ce671b499e07c546879310f91d9fdd0d95120
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:45:25 +0000

    INTEGRATION: CWS gcj1 (1.2.50); FILE MERGED 2004/12/01 15:55:52 cmc 1.2.50.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScDataPilotItemsObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotItemsObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScDataPilotItemsObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2004-04-13 12:20:01 $
+ *  last change:$Date: 2005-01-25 15:45:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -132,7 +132,7 @@ public class ScDataPilotItemsObj extends TestCase {
         } catch (com.sun.star.uno.Exception e) {
             // Some exception occures.FAILED
             e.printStackTrace(log);
-            throw new StatusException("Couldn³t create document", e);
+            throw new StatusException("Couldn't create document", e);
         }
     }
 
