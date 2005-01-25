533271249a91f050b1b81e79d26e72e8feebabc3
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:33:23 +0000

    INTEGRATION: CWS gcj1 (1.3.52); FILE MERGED 2004/12/01 13:42:45 cmc 1.3.52.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize german umlautes

--- javaunohelper/com/sun/star/lib/uno/helper/UnoUrl.java
+++ javaunohelper/com/sun/star/lib/uno/helper/UnoUrl.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoUrl.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-08-13 17:20:23 $
+ *  last change: $Author: obo $ $Date: 2005-01-25 15:33:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -95,7 +95,7 @@ import java.util.Vector;
  *   UnoUrl url = UnoUrl.parseUnoUrl("socket,host=localhost,port=2002;urp;StarOffice.ServiceManager");
  * </code>
  *
- * @author Jörg Brunsmann
+ * @author Joerg Brunsmann
  */
 public class UnoUrl {
 
