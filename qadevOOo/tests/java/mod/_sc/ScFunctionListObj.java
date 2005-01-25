0719d5f9aa1a8a5f36541e29f7f7cce3a5481db6
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:46:45 +0000

    INTEGRATION: CWS gcj1 (1.4.86); FILE MERGED 2004/12/01 15:55:53 cmc 1.4.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
+++ qadevOOo/tests/java/mod/_sc/ScFunctionListObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScFunctionListObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2004-01-05 19:04:33 $
+ *  last change:$Date: 2005-01-25 15:46:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -110,7 +110,7 @@ public class ScFunctionListObj extends TestCase {
         } catch (com.sun.star.uno.Exception e) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
 
     }
