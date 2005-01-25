b7ace2f51d2508275a90c483e3516c771e276f62
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:51:04 +0000

    INTEGRATION: CWS gcj1 (1.6.86); FILE MERGED 2004/12/01 15:55:57 cmc 1.6.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScSheetLinksObj.java
+++ qadevOOo/tests/java/mod/_sc/ScSheetLinksObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScSheetLinksObj.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change:$Date: 2004-01-05 19:22:44 $
+ *  last change:$Date: 2005-01-25 15:51:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -111,7 +111,7 @@ public class ScSheetLinksObj extends TestCase {
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
