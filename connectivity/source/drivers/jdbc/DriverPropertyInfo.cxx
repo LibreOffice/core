776fad793a924589d57925e7f17be0131a1a2350
Oliver Bolte <obo@openoffice.org>
2005-01-25 15:58:08 +0000

    INTEGRATION: CWS gcj1 (1.4.174); FILE MERGED 2005/01/05 08:54:42 cmc 1.4.174.1: #i38188# small gcc parse help required

--- connectivity/source/drivers/jdbc/DriverPropertyInfo.cxx
+++ connectivity/source/drivers/jdbc/DriverPropertyInfo.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DriverPropertyInfo.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-21 16:41:58 $
+ *  last change: $Author: obo $ $Date: 2005-01-25 16:58:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -184,12 +184,10 @@ Sequence< ::rtl::OUString> java_sql_DriverPropertyInfo::choices() const
     {
         jfieldID id = t.pEnv->GetFieldID(java_sql_DriverPropertyInfo::getMyClass(),"choices","[Ljava/lang/String;");
         if(id)
-            return copyArrayAndDelete(
-                t.pEnv,
-                (jobjectArray)t.pEnv->GetObjectField( object, id),
-                static_cast< const ::rtl::OUString* >( NULL ),
-                static_cast< const java_lang_String* >( NULL )
-            );
+        {
+            java_lang_String aEmpty(NULL,NULL);
+            return copyArrayAndDelete(t.pEnv,(jobjectArray)t.pEnv->GetObjectField( object, id), ::rtl::OUString(),aEmpty);
+        }
     } //t.pEnv
     return Sequence< ::rtl::OUString>();
 }
