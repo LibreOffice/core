56555e2b18549abc4259da4be0f1998b2d61ec6d
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:47:53 +0000

    INTEGRATION: CWS gcj1 (1.5.86); FILE MERGED 2004/12/01 15:55:54 cmc 1.5.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DataPilotFieldsEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_DataPilotFieldsEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_DataPilotFieldsEnumeration.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-01-05 19:10:51 $
+ *  last change:$Date: 2005-01-25 15:47:26 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -101,7 +101,7 @@ public class ScIndexEnumeration_DataPilotFieldsEnumeration extends TestCase {
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
 
     }
