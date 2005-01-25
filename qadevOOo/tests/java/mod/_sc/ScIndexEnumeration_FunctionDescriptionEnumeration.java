a80201e9b59e269766c75e22541289c5090eefee
Oliver Bolte <obo@openoffice.org>
2005-01-25 14:49:11 +0000

    INTEGRATION: CWS gcj1 (1.4.86); FILE MERGED 2004/12/01 15:55:55 cmc 1.4.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_FunctionDescriptionEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_FunctionDescriptionEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_FunctionDescriptionEnumeration.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2004-01-05 19:12:26 $
+ *  last change:$Date: 2005-01-25 15:48:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -91,7 +91,7 @@ public class ScIndexEnumeration_FunctionDescriptionEnumeration extends TestCase 
         } catch (com.sun.star.uno.Exception e) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
 
     }
--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_TableAutoFormatEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_TableAutoFormatEnumeration.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_TableAutoFormatEnumeration.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2004-01-05 19:16:41 $
+ *  last change:$Date: 2005-01-25 15:49:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,7 +92,7 @@ public class ScIndexEnumeration_TableAutoFormatEnumeration extends TestCase{
         } catch ( com.sun.star.uno.Exception e ) {
             // Some exception occures.FAILED
             e.printStackTrace( log );
-            throw new StatusException( "Couldn³t create document", e );
+            throw new StatusException( "Couldn't create document", e );
         }
     }
 
