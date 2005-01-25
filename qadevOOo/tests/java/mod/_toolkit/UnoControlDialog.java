ffb5da8ddbc5bc71f4dbcabe07c3aea08babc465
Oliver Bolte <obo@openoffice.org>
2005-01-25 15:15:48 +0000

    INTEGRATION: CWS gcj1 (1.5.86); FILE MERGED 2004/12/01 15:56:18 cmc 1.5.86.1: #i38188# gcj is picky about using non ascii characters in java source, ascii-ize multi-copied bad single quote symbol, handle some uncompilable directories

--- qadevOOo/tests/java/mod/_toolkit/UnoControlDialog.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlDialog.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UnoControlDialog.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change:$Date: 2004-01-05 20:48:17 $
+ *  last change:$Date: 2005-01-25 16:15:48 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -283,7 +283,7 @@ public class UnoControlDialog extends TestCase {
         } catch (com.sun.star.uno.Exception e) {
             // Some exception occures.FAILED
             e.printStackTrace(log);
-            throw new StatusException("Couldn³t create document", e);
+            throw new StatusException("Couldn't create document", e);
         }
     }
 }
