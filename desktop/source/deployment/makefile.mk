4bc1499797e96c2b36ff0adf70ac6d21ba79a0b3
Rüdiger Timm <rt@openoffice.org>
2005-01-27 09:20:30 +0000

    INTEGRATION: CWS cd04 (1.5.52); FILE MERGED 2005/01/19 10:17:48 dbo 1.5.52.1: #i35195##i40553# - focus takeover - invalid bootstraprc crash - resmgr mutex cleanup Issue number: Submitted by: Reviewed by:

--- desktop/source/deployment/makefile.mk
+++ desktop/source/deployment/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hr $ $Date: 2004-11-09 14:04:42 $
+#   last change: $Author: rt $ $Date: 2005-01-27 10:20:30 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -96,7 +96,6 @@ SHL1STDLIBS = \
         $(UCBHELPERLIB) \
         $(COMPHELPERLIB) \
         $(TOOLSLIB) \
-        $(VOSLIB) \
         $(UNOTOOLSLIB) \
         $(XMLSCRIPTLIB) \
         $(BERKELEYLIB) \
