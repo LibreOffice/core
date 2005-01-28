931b2734556b5d239e2d48d2a016ed0dfffe7d90
Rüdiger Timm <rt@openoffice.org>
2005-01-28 15:30:16 +0000

    INTEGRATION: CWS aw023 (1.2.610); FILE MERGED 2004/12/21 16:00:00 aw 1.2.610.2: RESYNC: (1.2-1.3); FILE MERGED 2004/12/16 11:40:19 aw 1.2.610.1: #i38135#

--- svx/inc/svx/sdr/animation/scheduler.hxx
+++ svx/inc/svx/sdr/animation/scheduler.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: scheduler.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-12-13 08:53:39 $
+ *  last change: $Author: rt $ $Date: 2005-01-28 16:30:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -167,6 +167,9 @@ namespace sdr
 
             // get time
             sal_uInt32 GetTime();
+
+            // #i38135#
+            void SetTime(sal_uInt32 nTime);
 
             // reset
             void Reset(sal_uInt32 nTime);
