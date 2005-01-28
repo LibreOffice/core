1c15603c9fc7c4ef88d0b1133f741b966307d3dd
Rüdiger Timm <rt@openoffice.org>
2005-01-28 16:22:01 +0000

    INTEGRATION: CWS fwkbeta03 (1.2.258); FILE MERGED 2005/01/25 12:20:23 as 1.2.258.1: #i22205# new configuration to support sorted filter lists

--- officecfg/registry/schema/org/openoffice/TypeDetection/makefile.mk
+++ officecfg/registry/schema/org/openoffice/TypeDetection/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2004-07-05 13:46:45 $
+#   last change: $Author: rt $ $Date: 2005-01-28 17:22:01 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -74,7 +74,8 @@ XCSFILES= \
     Types.xcs \
     Filter.xcs \
     GraphicFilter.xcs \
-    Misc.xcs
+    Misc.xcs \
+    UISort.xcs
 
 .INCLUDE :  target.mk
 .INCLUDE :  $(PRJ)$/registry$/schema$/localtarget.mk
