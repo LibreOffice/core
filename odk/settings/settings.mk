5b00093c1a67febaf6ab74098ca872dd753815e9
Rüdiger Timm <rt@openoffice.org>
2005-01-31 16:22:22 +0000

    INTEGRATION: CWS sdksample (1.9.14); FILE MERGED 2005/01/20 10:25:18 jsc 1.9.14.18: #i24142# correct typo 2005/01/20 08:20:31 jsc 1.9.14.17: #i24142# integrate patch for FreeBSD 2004/12/09 15:44:41 jsc 1.9.14.16: #i38747# take care of compiler verison and adjust link option, solaris only 2004/11/19 14:35:27 jsc 1.9.14.15: #i29308# insert debug link option for windows 2004/11/16 09:02:55 jsc 1.9.14.14: RESYNC: (1.9-1.10); FILE MERGED 2004/11/11 10:31:51 jsc 1.9.14.13: #i29308# clean up compiler option 2004/10/28 10:07:36 jsc 1.9.14.12: #i29308# insert new linker option --allow_shlib_undefined for binaries 2004/10/25 12:58:55 jsc 1.9.14.11: #i29308# minimize exported symbols 2004/10/12 15:02:39 jsc 1.9.14.10: #i29308# insert macro P2BG for starting processes in the background 2004/10/08 13:59:45 jsc 1.9.14.9: #i29308# insert macro for package platform descr 2004/08/05 12:44:37 jsc 1.9.14.8: #i29308# remove DEBUG notmapped link option, not longer supported 2004/08/05 12:39:31 jsc 1.9.14.7: #i29308# remove DEBUG notmapped link option, not longer supported 2004/08/04 12:37:08 jsc 1.9.14.6: #i29308# ensure that correct stl import library is used, regression 2004/07/19 16:12:24 jsc 1.9.14.5: #i29308# insert newlione at the end 2004/06/30 09:09:04 jsc 1.9.14.4: #i29308# insert linker switch -Wl,-z,defs for Linux 2004/06/16 11:40:06 jsc 1.9.14.3: #i24142# integrate support for FreeBSD 2004/06/11 12:52:41 jsc 1.9.14.2: #26337# special work for gcc - GXX_INCLUDE_APTH 2004/06/03 11:57:47 jsc 1.9.14.1: #i26335# use new script templates

--- odk/settings/settings.mk
+++ odk/settings/settings.mk
@@ -68,12 +68,15 @@ COPY=copy
 SHAREDLIB_EXT=dll
 SHAREDLIB_OUT=$(OUT_BIN)
 PACKAGE_LIB_DIR=windows.plt
+UNOPKG_PLATFORM=Windows
     
 OSEP=^<
 CSEP=^>
 QUOTE=
 QM=
 SQM=
+ECHOLINE=@echo.
+P2BG=
 
 DEL=del
 DELRECURSIVE=rd /S /Q
@@ -83,7 +86,9 @@ SALLIB=isal.lib
 CPPULIB=icppu.lib
 CPPUHELPERLIB=icppuhelper.lib
 SALHELPERLIB=isalhelper.lib
-STLPORTLIB=stlport_vc7.lib
+REGLIB=ireg.lib
+STORELIB=istore.lib
+STLPORTLIB=stlport_vc71.lib
 
 BLANK= 
 EMPTYSTRING=
@@ -102,11 +107,16 @@ SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/
 # define for used compiler necessary for UNO
 # -DCPPU_ENV=msci -- windows msvc 4.x - 7.x
 
-CC_DEFINES=-DWIN32 -DCPPU_ENV=msci
+CC_DEFINES=-DWIN32 -DWNT -DCPPU_ENV=msci
 CC_OUTPUT_SWITCH=-Fo
 
-LIBRARY_LINK_FLAGS=/NODEFAULTLIB /DLL /DEBUG:notmapped,full /DEBUGTYPE:cv
-EXE_LINK_FLAGS=/MAP /OPT:NOREF /SUBSYSTEM:CONSOLE /BASE:0x1100000 /DEBUG:notmapped,full /DEBUGTYPE:cv
+LIBRARY_LINK_FLAGS=/NODEFAULTLIB /DLL /DEBUGTYPE:cv
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) /DEF:$(PRJ)/settings/component.uno.def
+EXE_LINK_FLAGS=/MAP /OPT:NOREF /SUBSYSTEM:CONSOLE /BASE:0x1100000 /DEBUGTYPE:cv
+ifeq "$(DEBUG)" "yes"
+LIBRARY_LINK_FLAGS+=/DEBUG
+EXE_LINK_FLAGS+=/DEBUG
+endif
 
 LINK_JAVA_LIBS=/LIBPATH:$(OO_SDK_JAVA_HOME)/lib
 
@@ -131,10 +141,12 @@ PROCTYPE := $(shell uname -p)
 ifeq "$(PROCTYPE)" "sparc"
 PLATFORM=solsparc
 PACKAGE_LIB_DIR=solaris_sparc.plt
+UNOPKG_PLATFORM=Solaris_SPARC
 JAVA_PROC_TYPE=sparc
 else
 PLATFORM=solintel
 PACKAGE_LIB_DIR=solaris_x86.plt
+UNOPKG_PLATFORM=Solaris_x86
 JAVA_PROC_TYPE=i386
 endif
 
@@ -156,6 +168,8 @@ CSEP=\>
 QUOTE=$(subst S,\,S)
 QM=\"
 SQM='
+ECHOLINE=@echo
+P2BG=&
 
 DEL=rm -f
 DELRECURSIVE=rm -rf
@@ -169,6 +183,8 @@ SALLIB=-luno_sal
 CPPULIB=-luno_cppu
 CPPUHELPERLIB=-luno_cppuhelperC52
 SALHELPERLIB=-luno_salhelperC52
+REGLIB=-lreg
+STORELIB=-lstore
 STLPORTLIB=-lstlport_sunpro
 
 EMPTYSTRING=
@@ -178,7 +194,7 @@ CC_FLAGS=-c -KPIC
 ifeq "$(DEBUG)" "yes"
 CC_FLAGS+=-g
 endif
-CC_INCLUDES=-I. -I/usr/include -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I$(OO_SDK_HOME)/include/stl
 SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/solaris
 
@@ -188,7 +204,13 @@ SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/
 CC_DEFINES=-DUNX -DSOLARIS -DSPARC -DCPPU_ENV=sunpro5
 CC_OUTPUT_SWITCH=-o 
 
-LIBRARY_LINK_FLAGS=-w -PIC -temp=/tmp '-R $$ORIGIN' -norunpath -G -lthread -instances=static 
+LIBRARY_LINK_FLAGS=-w -mt -z combreloc -PIC -temp=/tmp '-R $$ORIGIN' -z text -z defs -norunpath -G -Bdirect -Bdynamic -lpthread -lCrun -lc -lm
+# means if used CC is lower then version 5.5 use option -instance=static
+ifeq ($(OO_SDK_CC_55_OR_HIGHER),)
+LIBRARY_LINK_FLAGS+=-instances=static
+endif
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -M $(PRJ)/settings/component.uno.map
+
 LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L$(OFFICE_PROGRAM_PATH)
 LINK_JAVA_LIBS=-L$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)
 
@@ -206,22 +228,23 @@ ifeq "$(PLATFORM)" "Linux"
 
 PROCTYPE := $(shell uname -m)
 
-    
 # Default is linux on a intel machine    
 PLATFORM=linux
 PACKAGE_LIB_DIR=linux_x86.plt
+UNOPKG_PLATFORM=Linux_x86
 JAVA_PROC_TYPE=i386
-    
+
 ifeq "$(PROCTYPE)" "ppc"
 PACKAGE_LIB_DIR=linux_powerpc.plt
+UNOPKG_PLATFORM=Linux_PowerPC
 JAVA_PROC_TYPE=ppc
 endif
 
 OS=LINUX
 PS=/
 CC=gcc
-LINK=gcc
-LIB=gcc
+LINK=g++
+LIB=g++
 ECHO=@echo
 MKDIR=mkdir -p
 CAT=cat
@@ -229,7 +252,7 @@ OBJ_EXT=o
 SHAREDLIB_EXT=so
 SHAREDLIB_PRE=lib
 SHAREDLIB_OUT=$(OUT_LIB)
-    
+
 GCC_VERSION=$(shell $(CC) -dumpversion)
 
 ifeq "$(shell echo $(GCC_VERSION) | cut -c 1)" "3"
@@ -245,6 +268,8 @@ CSEP=\>
 QUOTE=$(subst S,\,S)
 QM=\"
 SQM='
+ECHOLINE=@echo
+P2BG=&
     
 DEL=rm -f
 DELRECURSIVE=rm -rf
@@ -258,6 +283,8 @@ SALLIB=-luno_sal
 CPPULIB=-luno_cppu
 CPPUHELPERLIB=-luno_cppuhelper$(COMID)
 SALHELPERLIB=-luno_salhelper$(COMID)
+REGLIB=-lreg
+STORELIB=-lstore
 STLPORTLIB=-lstlport_gcc
 
 EMPTYSTRING=
@@ -265,9 +292,9 @@ PATH_SEPARATOR=:
 
 # -O is necessary for inlining (see gcc documentation)
 ifeq "$(DEBUG)" "yes"
-CC_FLAGS=-c -g -fpic -fno-rtti
+CC_FLAGS=-c -g -fpic
 else
-CC_FLAGS=-c -O -fpic -fno-rtti
+CC_FLAGS=-c -O -fpic
 endif
 
 ifeq "$(PROCTYPE)" "ppc"
@@ -275,7 +302,7 @@ CC_FLAGS+=-fPIC
 endif
 
 SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/linux
-CC_INCLUDES=-I. -I/usr/include -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I$(OO_SDK_HOME)/include/stl
 CC_DEFINES=-DUNX -DGCC -DLINUX -DCPPU_ENV=$(CPPU_ENV)
 
@@ -285,13 +312,14 @@ CC_DEFINES=-DUNX -DGCC -DLINUX -DCPPU_ENV=$(CPPU_ENV)
 
 CC_OUTPUT_SWITCH=-o
 
-LIBRARY_LINK_FLAGS=-shared '-Wl,-rpath,$$ORIGIN'
+LIBRARY_LINK_FLAGS=-shared -Wl,-rpath,$$ORIGIN
 
 ifeq "$(PROCTYPE)" "ppc"
 LIBRARY_LINK_FLAGS+=-fPIC
 endif
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -Wl,--version-script,$(PRJ)/settings/component.uno.map
 
-EXE_LINK_FLAGS=-Wl -export-dynamic
+EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs
 LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L$(OFFICE_PROGRAM_PATH)
 LINK_JAVA_LIBS=-L$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)
 
@@ -308,13 +336,14 @@ ifeq "$(PLATFORM)" "Darwin"
 # Default is MacOSX on a ppc machine    
 PLATFORM=macosx
 PACKAGE_LIB_DIR=macosx_ppc.plt
+UNOPKG_PLATFORM=MacOSX_PowerPC
 JAVA_PROC_TYPE=ppc
 
 OS=MACOSX
 PS=/
 CC=gcc
-LINK=gcc
-LIB=gcc
+LINK=g++
+LIB=g++
 ECHO=@echo
 MKDIR=mkdir -p
 CAT=cat
@@ -322,7 +351,7 @@ OBJ_EXT=o
 SHAREDLIB_EXT=dylib
 SHAREDLIB_PRE=lib
 SHAREDLIB_OUT=$(OUT_LIB)
-    
+
 GCC_VERSION=$(shell $(CC) -dumpversion)
 
 COMID=gcc3
@@ -333,6 +362,8 @@ CSEP=\>
 QUOTE=$(subst S,\,S)
 QM=\"
 SQM='
+ECHOLINE=@echo
+P2BG=&
     
 DEL=rm -f
 DELRECURSIVE=rm -rf
@@ -346,6 +377,8 @@ SALLIB=-luno_sal
 CPPULIB=-luno_cppu
 CPPUHELPERLIB=-luno_cppuhelper$(COMID)
 SALHELPERLIB=-luno_salhelper$(COMID)
+REGLIB=-lreg
+STORELIB=-lstore
 STLPORTLIB=-lstlport_gcc -lstdc++
 
 EMPTYSTRING=
@@ -353,27 +386,115 @@ PATH_SEPARATOR=:
 
 # -O is necessary for inlining (see gcc documentation)
 ifeq "$(DEBUG)" "yes"
-CC_FLAGS=-malign-natural -c -g -fPIC -fno-common -fno-rtti
+CC_FLAGS=-malign-natural -c -g -fPIC -fno-common
 else
-CC_FLAGS=-malign-natural -c -O -fPIC -fno-common -fno-rtti
+CC_FLAGS=-malign-natural -c -O -fPIC -fno-common
 endif
 
 SDK_JAVA_INCLUDES = -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -I/System/Library/Frameworks/JavaVM.framework/Headers
-CC_INCLUDES=-I. -I/usr/include -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I$(OO_SDK_HOME)/include/stl
 CC_DEFINES=-DUNX -DGCC -DMACOSX -DCPPU_ENV=$(CPPU_ENV)
 
 CC_OUTPUT_SWITCH=-o
 
-LIBRARY_LINK_FLAGS=-dynamiclib -single_module
+LIBRARY_LINK_FLAGS=-dynamiclib -single_module -fPIC -fno-common
 
 # install_name '@executable_path$/(@:f)'
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) 
+#-Wl,--version-script,$(PRJ)/settings/component.uno.map
 
-LIBRARY_LINK_FLAGS+=-fPIC -fno-common
-
-EXE_LINK_FLAGS=
+#EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs
 LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L$(OFFICE_PROGRAM_PATH)
 LINK_JAVA_LIBS=-framework JavaVM
 
 endif
 
+###########################################################################
+#
+# FreeBSD specific settings
+#
+###########################################################################
+ifeq "$(PLATFORM)" "FreeBSD"
+# Settings for FreeBSD using gcc compiler
+
+PROCTYPE := $(shell uname -m)
+
+# Default is freebsd on a intel machine    
+PLATFORM=FreeBSD
+PACKAGE_LIB_DIR=freebsd_x86.plt
+UNOPKG_PLATFORM=FreeBSD_x86
+JAVA_PROC_TYPE=i386
+
+OS=FREEBSD
+PS=/
+CC=gcc
+LINK=g++
+LIB=g++
+ECHO=@echo
+MKDIR=mkdir -p
+CAT=cat
+OBJ_EXT=o
+SHAREDLIB_EXT=so
+SHAREDLIB_PRE=lib
+SHAREDLIB_OUT=$(OUT_LIB)
+ 
+GCC_VERSION=$(shell $(CC) -dumpversion)
+
+ifeq "$(shell echo $(GCC_VERSION) | cut -c 1)" "3"
+COMID=gcc3
+CPPU_ENV=gcc3
+else
+COMID=GCC
+CPPU_ENV=gcc2
+endif
+
+OSEP=\<
+CSEP=\>
+QUOTE=$(subst S,\,S)
+QM=\"
+ECHOLINE=@echo
+P2BG=&
+   
+DEL=rm -f
+DELRECURSIVE=rm -rf
+COPY=cp
+URLPREFIX=file://
+
+# Include UDK version numbers
+include $(PRJ)/include/udkversion.mk
+
+SALLIB=-lsal
+CPPULIB=-lcppu
+CPPUHELPERLIB=-lcppuhelper$(COMID)
+SALHELPERLIB=-lsalhelper$(COMID)
+REGLIB=-lreg
+STORELIB=-lstore
+STLPORTLIB=-lstlport_gcc
+
+EMPTYSTRING=
+PATH_SEPARATOR=:
+
+# -O is necessary for inlining (see gcc documentation)
+ifeq "$(DEBUG)" "yes"
+CC_FLAGS=-c -g -fPIC -DPIC $(PTHREAD_CFLAGS)
+else
+CC_FLAGS=-c -O -fPIC -DPIC $(PTHREAD_CFLAGS)
+endif
+
+SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/freebsd
+CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+STL_INCLUDES=-I$(OO_SDK_HOME)/include/stl
+CC_DEFINES=-DUNX -DGCC -DFREEBSD -DCPPU_ENV=$(CPPU_ENV)
+
+CC_OUTPUT_SWITCH=-o
+
+LIBRARY_LINK_FLAGS=-shared '-Wl,-rpath,$$ORIGIN'
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -Wl,--version-script,$(PRJ)/settings/component.uno.map
+
+EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined 
+#EXE_LINK_FLAGS+=-Wl,-export-dynamic -Wl,-z,defs
+LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L$(OFFICE_PROGRAM_PATH) $(PTHREAD_LIBS)
+LINK_JAVA_LIBS=-L$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)
+
+endif
