e789e5b364e2cf50069b47c509c0733b20cc239b
Rüdiger Timm <rt@openoffice.org>
2005-01-31 15:33:10 +0000

    INTEGRATION: CWS sdksample (1.1.2); FILE ADDED 2004/10/28 15:31:59 jsc 1.1.2.2: #i29308# use new bootstrap mechanism 2004/10/07 08:52:50 jsc 1.1.2.1: #i29308# new

---
+++ odk/examples/DevelopersGuide/Forms/TableCellTextBinding.java
@@ -0,0 +1,244 @@
+/*************************************************************************
+ *
+ *  $RCSfile: TableCellTextBinding.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-01-31 16:33:10 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+import com.sun.star.uno.Type;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.table.XCell;
+import com.sun.star.util.XModifyListener;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.text.XTextRange;
+import com.sun.star.form.binding.IncompatibleTypesException;
+
+/** a value binding to be connected to a form control
+
+    This binding synchronizes the text contained in a table cell (which you must
+    pass upon construction) to the text in an XBindableValue.
+
+    Well, in real it does not synchronize both directions. The ValueBinding
+    service has not much room for own activity: It allows notification of changes
+    in the own value, and it allows external instances to set the current value.
+
+    Note that we implement this binding as a separate thread, which is (more or
+    less permanently) polling for a new text at the cell. This is unfortunate, but
+    sadly the Writer table cells do not support actively notifying changes in their
+    content to other interested parties.
+*/
+public class TableCellTextBinding
+                extends     java.lang.Thread
+                implements  com.sun.star.form.binding.XValueBinding,
+                            com.sun.star.util.XModifyBroadcaster
+{
+    private XTextRange  m_cellText;
+    private Object      m_writeSignal;
+    private String      m_newCellText;
+    private String      m_lastKnownCellText;
+    private boolean     m_haveNewCellText;
+    private java.util.List  m_listeners;
+
+    /** Creates a new instance of TableCellTextBinding */
+    public TableCellTextBinding( XCell cell )
+    {
+        m_cellText = (XTextRange)UnoRuntime.queryInterface( XTextRange.class, cell );
+
+        m_newCellText = new String();
+        m_listeners = new java.util.LinkedList();
+
+        start();
+    }
+
+    /** retrieves the list of data types which this binding can exchange
+    */
+    public com.sun.star.uno.Type[] getSupportedValueTypes()
+    {
+        try
+        {
+            // well, only strings here ...
+            return new Type[] {
+                getStringType()
+            };
+        }
+        catch( java.lang.Exception e )
+        {
+        }
+        return new Type[] { };
+    }
+
+    /** retrieves the current value
+    */
+    public Object getValue(com.sun.star.uno.Type type) throws com.sun.star.form.binding.IncompatibleTypesException
+    {
+        if ( !type.equals( getStringType() ) )
+            throw new com.sun.star.form.binding.IncompatibleTypesException();
+
+        return m_cellText.getString();
+    }
+
+    /** sets a new value
+    */
+    public void setValue(Object obj) throws com.sun.star.form.binding.IncompatibleTypesException
+    {
+        String text;
+        try
+        {
+            text = (String)obj;
+        }
+        catch( java.lang.ClassCastException e )
+        {
+            throw new com.sun.star.form.binding.IncompatibleTypesException();
+        }
+        // remember the new text
+        synchronized( m_newCellText )
+        {
+            m_newCellText = text;
+            m_haveNewCellText = true;
+        }
+        // and wake up the thread which is waiting for it
+        synchronized( m_writeSignal )
+        {
+            m_writeSignal.notify();
+        }
+    }
+
+    /** determines whether a given value type is supported
+    */
+    public boolean supportsType(com.sun.star.uno.Type type)
+    {
+        return type.equals( getStringType() );
+    }
+
+    /** retrieves the UNO type for the string class
+    */
+    private static final Type getStringType()
+    {
+        return new com.sun.star.uno.Type( String.class );
+    }
+
+    /** runs the thread
+    */
+    public void run()
+    {
+        try
+        {
+            m_writeSignal = new Object();
+            while ( true )
+            {
+                // go sleep a while
+                synchronized( m_writeSignal )
+                {
+                    m_writeSignal.wait( 200 );
+                }
+
+                // if there's new text in the control, propagate it to the cell
+                synchronized ( m_newCellText )
+                {
+                    if ( m_haveNewCellText )
+                    {
+                        m_cellText.setString( m_newCellText );
+                        m_lastKnownCellText = m_newCellText;
+                    }
+                    m_haveNewCellText = false;
+                }
+
+                // if there's new text in the cell, propagate it to the control
+                String currentCellText = m_cellText.getString();
+                if ( !currentCellText.equals( m_lastKnownCellText ) )
+                {
+                    m_lastKnownCellText = currentCellText;
+                    // notify the modification
+                    synchronized( m_listeners )
+                    {
+                        com.sun.star.lang.EventObject eventSource = new com.sun.star.lang.EventObject( this );
+
+                        java.util.Iterator loop = m_listeners.iterator();
+                        while ( loop.hasNext() )
+                        {
+                            ((XModifyListener)loop.next()).modified( eventSource );
+                        }
+                    }
+                }
+            }
+        }
+        catch( java.lang.Exception e )
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public void addModifyListener(com.sun.star.util.XModifyListener xModifyListener)
+    {
+        synchronized( m_listeners )
+        {
+            m_listeners.add( xModifyListener );
+        }
+    }
+
+    public void removeModifyListener(com.sun.star.util.XModifyListener xModifyListener)
+    {
+        synchronized( m_listeners )
+        {
+            m_listeners.remove( xModifyListener );
+        }
+    }
+
+    public void disposing(com.sun.star.lang.EventObject eventObject)
+    {
+        // not interested in
+    }
+}
