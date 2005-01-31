20eaa883c06b3b00a3822f25c7d3c55ef51cf532
Rüdiger Timm <rt@openoffice.org>
2005-01-31 15:26:09 +0000

    INTEGRATION: CWS sdksample (1.4.40); FILE MERGED 2004/08/06 14:37:01 jsc 1.4.40.2: #i29308# use System.err for error output 2004/06/10 13:24:35 jsc 1.4.40.1: #i29308# use of new UNO bootstrap feature

--- odk/examples/DevelopersGuide/FirstSteps/FirstConnection.java
+++ odk/examples/DevelopersGuide/FirstSteps/FirstConnection.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: FirstConnection.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-02 19:58:02 $
+ *  last change: $Author: rt $ $Date: 2005-01-31 16:26:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -38,22 +38,21 @@
  *
  *************************************************************************/
 
-import com.sun.star.bridge.XUnoUrlResolver;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XComponentContext;
-import com.sun.star.lang.XMultiComponentFactory;
-import com.sun.star.beans.XPropertySet;
-
-
 public class FirstConnection extends java.lang.Object {
 
-    private XComponentContext xRemoteContext = null;
-    private XMultiComponentFactory xRemoteServiceManager = null;
-
     public static void main(String[] args) {
-        FirstConnection firstConnection1 = new FirstConnection();
         try {
-            firstConnection1.useConnection();
+            // get the remote office component context
+            com.sun.star.uno.XComponentContext xContext =
+                com.sun.star.comp.helper.Bootstrap.bootstrap();
+
+            System.out.println("Connected to a running office ...");
+
+            com.sun.star.lang.XMultiComponentFactory xMCF =
+                xContext.getServiceManager();
+
+            String available = (null != xMCF ? "available" : "not available");
+            System.out.println( "remote ServiceManager is " + available );
         }
         catch (java.lang.Exception e){
             e.printStackTrace();
@@ -61,46 +60,5 @@ public class FirstConnection extends java.lang.Object {
         finally {
             System.exit(0);
         }
-    }
-
-    protected void useConnection() throws java.lang.Exception {
-        try {
-            xRemoteServiceManager = this.getRemoteServiceManager(
-                    "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager");
-            String available = (null != xRemoteServiceManager ? "available" : "not available");
-            System.out.println( "remote ServiceManager is " + available );
-        }
-        catch( com.sun.star.lang.DisposedException e ) { //works from Patch 1
-            xRemoteContext = null;
-            throw e;
-        }
-    }
-
-    protected XMultiComponentFactory getRemoteServiceManager(String unoUrl) throws java.lang.Exception {
-        if (xRemoteContext == null) {
-            // First step: create local component context, get local servicemanager and
-            // ask it to create a UnoUrlResolver object with an XUnoUrlResolver interface
-            XComponentContext xLocalContext =
-                com.sun.star.comp.helper.Bootstrap.createInitialComponentContext(null);
-
-            XMultiComponentFactory xLocalServiceManager = xLocalContext.getServiceManager();
-
-            Object urlResolver  = xLocalServiceManager.createInstanceWithContext(
-                "com.sun.star.bridge.UnoUrlResolver", xLocalContext );
-            // query XUnoUrlResolver interface from urlResolver object
-            XUnoUrlResolver xUnoUrlResolver = (XUnoUrlResolver) UnoRuntime.queryInterface(
-                XUnoUrlResolver.class, urlResolver );
-            //XUnoUrlResolver xUnoUrlResolver = (XUnoUrlResolver)urlResolver;
-
-            // Second step: use xUrlResolver interface to import the remote StarOffice.ServiceManager,
-            // retrieve its property DefaultContext and get the remote servicemanager
-            Object initialObject = xUnoUrlResolver.resolve( unoUrl );
-            XPropertySet xPropertySet = (XPropertySet)UnoRuntime.queryInterface(
-                XPropertySet.class, initialObject);
-            Object context = xPropertySet.getPropertyValue("DefaultContext");
-            xRemoteContext = (XComponentContext)UnoRuntime.queryInterface(
-                XComponentContext.class, context);
-        }
-        return xRemoteContext.getServiceManager();
     }
 }
