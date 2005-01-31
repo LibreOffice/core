da7f4870a93ebfaf75818672c59a6cfb511b6a3f
Rüdiger Timm <rt@openoffice.org>
2005-01-31 15:36:21 +0000

    INTEGRATION: CWS sdksample (1.4.40); FILE MERGED 2004/10/15 11:04:10 jsc 1.4.40.3: #i29308# updated 2004/08/06 14:37:03 jsc 1.4.40.2: #i29308# use System.err for error output 2004/06/17 11:02:25 jsc 1.4.40.1: #i29308# use of new UNO bootstrap feature

--- odk/examples/DevelopersGuide/OfficeDev/Clipboard/Clipboard.java
+++ odk/examples/DevelopersGuide/OfficeDev/Clipboard/Clipboard.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Clipboard.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2004-02-02 20:00:11 $
+ *  last change: $Author: rt $ $Date: 2005-01-31 16:36:21 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -38,7 +38,6 @@
  *
  *************************************************************************/
 
-import com.sun.star.bridge.XUnoUrlResolver;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiComponentFactory;
 import com.sun.star.uno.XComponentContext;
@@ -46,8 +45,12 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.beans.XPropertySet;
-import com.sun.star.datatransfer.*;
-import com.sun.star.datatransfer.clipboard.*;
+import com.sun.star.datatransfer.DataFlavor;
+import com.sun.star.datatransfer.UnsupportedFlavorException;
+import com.sun.star.datatransfer.XTransferable;
+import com.sun.star.datatransfer.clipboard.XClipboard;
+import com.sun.star.datatransfer.clipboard.XClipboardNotifier;
+import com.sun.star.text.XTextDocument;
 import com.sun.star.uno.AnyConverter;
 
 //-------------------------------------------------
@@ -60,55 +63,50 @@ public class Clipboard
     {
         try
         {
-            String sConnection;
-            if ( args.length >= 2 )
-                sConnection = "uno:" + args[0] + ";urp;StarOffice.ServiceManager" ;
-            else
-                sConnection = "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager";
+            // get the remote office context. If necessary a new office
+            // process is started
+            XComponentContext xOfficeContext =
+                com.sun.star.comp.helper.Bootstrap.bootstrap();
+            System.out.println("Connected to a running office ...");
+            // get the service manager from the office context
+            XMultiComponentFactory xServiceManager =
+                xOfficeContext.getServiceManager();
 
-            /* Bootstraps a component context with the jurt base components
-               registered. Component context to be granted to a component for running.
-               Arbitrary values can be retrieved from the context.
-            */
+            // create a new test document
+            Object oDesktop = xServiceManager.createInstanceWithContext(
+                "com.sun.star.frame.Desktop", xOfficeContext);
 
-            XComponentContext xComponentContext =
-                com.sun.star.comp.helper.Bootstrap.createInitialComponentContext(null);
+            XComponentLoader xCompLoader =(XComponentLoader)
+                UnoRuntime.queryInterface(XComponentLoader.class, oDesktop);
 
-            /* Gets the service manager instance to be used (or null). This method has
-               been added for convenience, because the service manager is a often used
-               object.
-            */
+            com.sun.star.lang.XComponent xComponent =
+                xCompLoader.loadComponentFromURL("private:factory/swriter",
+                    "_blank", 0, new com.sun.star.beans.PropertyValue[0]);
+            {
+            XTextDocument xDoc =(XTextDocument)
+                UnoRuntime.queryInterface(XTextDocument.class, xComponent);
+            xDoc.getText().setString("In the first step, paste the current content of the clipboard in the document!\nThe text \"Hello world!\" shall be insert at the current cursor position below.\n\nIn the second step, please select some words and put it into the clipboard! ...\n\nCurrent clipboard content = ");
 
-            XMultiComponentFactory xMultiComponentFactory =
-                xComponentContext.getServiceManager();
+            // ensure that the document content is optimal visible
+            com.sun.star.frame.XModel xModel =
+                (com.sun.star.frame.XModel)UnoRuntime.queryInterface(
+                    com.sun.star.frame.XModel.class, xDoc);
+            // get the frame for later usage
+            com.sun.star.frame.XFrame xFrame =
+                xModel.getCurrentController().getFrame();
 
-            /* Creates an instance of the component UnoUrlResolver which
-               supports the services specified by the factory.
-            */
+            com.sun.star.view.XViewSettingsSupplier xViewSettings =
+                (com.sun.star.view.XViewSettingsSupplier)UnoRuntime.queryInterface(
+                    com.sun.star.view.XViewSettingsSupplier.class,
+                    xModel.getCurrentController());
+            xViewSettings.getViewSettings().setPropertyValue(
+                "ZoomType", new Short((short)0));
+            }
+            // test document will be closed later
 
-            Object objectUrlResolver =
-                xMultiComponentFactory.createInstanceWithContext(
-                    "com.sun.star.bridge.UnoUrlResolver",
-                    xComponentContext );
-
-            // Create a new url resolver
-
-            XUnoUrlResolver xurlresolver = (XUnoUrlResolver)
-                UnoRuntime.queryInterface(XUnoUrlResolver.class, objectUrlResolver);
-
-            // Resolves an object that is specified as follow:
-            // uno:<connection description>;<protocol description>;<initial object name>
-
-            Object objectInitial = xurlresolver.resolve(sConnection);
-
-            // Create a service manager from the initial object
-            xMultiComponentFactory = (XMultiComponentFactory)
-            UnoRuntime.queryInterface(XMultiComponentFactory.class, objectInitial);
-
-            Object oClipboard =
-            xMultiComponentFactory.createInstanceWithContext(
+            Object oClipboard = xServiceManager.createInstanceWithContext(
                 "com.sun.star.datatransfer.clipboard.SystemClipboard",
-                xComponentContext);
+                xOfficeContext);
 
             XClipboard xClipboard = (XClipboard)
                 UnoRuntime.queryInterface(XClipboard.class, oClipboard);
@@ -124,52 +122,8 @@ public class Clipboard
 
             xClipNotifier.addClipboardListener(aClipListener);
 
-            //---------------------------------------------------
-            // get a list of formats currently on the clipboard
-            //---------------------------------------------------
-
-            XTransferable xTransferable = xClipboard.getContents();
-
-            DataFlavor[] aDflvArr = xTransferable.getTransferDataFlavors();
-
-            // print all available formats
-
-            System.out.println("Reading the clipboard...");
-            System.out.println("Available clipboard formats:");
-
-            DataFlavor aUniFlv = null;
-
-            for (int i=0;i<aDflvArr.length;i++)
-            {
-                System.out.println( "MimeType: " +
-                                    aDflvArr[i].MimeType +
-                                    " HumanPresentableName: " +
-                                    aDflvArr[i].HumanPresentableName );
-
-                // if there is the format unicode text on the clipboard save the
-                // corresponding DataFlavor so that we can later output the string
-
-                if ( aDflvArr[i].MimeType.equals("text/plain;charset=utf-16") )
-                {
-                    aUniFlv = aDflvArr[i];
-                }
-            }
-
-            System.out.println("");
-
-            try
-            {
-                if (aUniFlv != null)
-                {
-                    System.out.println("Unicode text on the clipboard...");
-                    Object aData = xTransferable.getTransferData(aUniFlv);
-                    System.out.println(AnyConverter.toString(aData));
-                }
-            }
-            catch( UnsupportedFlavorException ex )
-            {
-                System.err.println( "Requested format is not available on the clipboard!" );
-            }
+            // Read ClipBoard
+            readClipBoard(xClipboard);
 
             //---------------------------------------------------
             // becoming a clipboard owner
@@ -198,17 +152,84 @@ public class Clipboard
                 Thread.sleep(1000);
             }
 
+            // Read ClipBoard again
+            readClipBoard(xClipboard);
+
             //---------------------------------------------------
             // unregistering as clipboard listener
             //---------------------------------------------------
-
             xClipNotifier.removeClipboardListener(aClipListener);
+
+            // close test document
+            com.sun.star.util.XCloseable xCloseable = (com.sun.star.util.XCloseable)
+                UnoRuntime.queryInterface(com.sun.star.util.XCloseable.class,
+                                          xComponent );
+
+            if (xCloseable != null ) {
+                xCloseable.close(false);
+            } else
+            {
+                xComponent.dispose();
+            }
 
             System.exit(0);
         }
-        catch( Exception exception )
+        catch( Exception ex )
         {
-            System.err.println(exception);
+            ex.printStackTrace();
+        }
+    }
+
+    public static void readClipBoard(XClipboard xClipboard)
+        throws java.lang.Exception
+    {
+        //---------------------------------------------------
+        // get a list of formats currently on the clipboard
+        //---------------------------------------------------
+
+        XTransferable xTransferable = xClipboard.getContents();
+
+        DataFlavor[] aDflvArr = xTransferable.getTransferDataFlavors();
+
+        // print all available formats
+
+        System.out.println("Reading the clipboard...");
+        System.out.println("Available clipboard formats:");
+
+        DataFlavor aUniFlv = null;
+
+        for (int i=0;i<aDflvArr.length;i++)
+        {
+            System.out.println( "MimeType: " +
+                                aDflvArr[i].MimeType +
+                                " HumanPresentableName: " +
+                                aDflvArr[i].HumanPresentableName );
+
+            // if there is the format unicode text on the clipboard save the
+            // corresponding DataFlavor so that we can later output the string
+
+            if ( aDflvArr[i].MimeType.equals("text/plain;charset=utf-16") )
+            {
+                aUniFlv = aDflvArr[i];
+            }
+        }
+
+        System.out.println("");
+
+        try
+        {
+            if (aUniFlv != null)
+            {
+                System.out.print("Unicode text on the clipboard ...\nYour selected text \"");
+                Object aData = xTransferable.getTransferData(aUniFlv);
+                System.out.println(AnyConverter.toString(aData)
+                                   + "\" is now in the clipboard.\n");
+            }
+        }
+        catch( UnsupportedFlavorException ex )
+        {
+            System.err.println( "Requested format is not available on the clipboard!" );
+            ex.printStackTrace();
         }
     }
 }
