6873b3be47d71f94c38262003101576081acd241
Rüdiger Timm <rt@openoffice.org>
2005-01-31 08:49:09 +0000

    INTEGRATION: CWS jre5issues (1.3.18); FILE MERGED 2005/01/18 12:48:48 jl 1.3.18.1: #i40879# in build environment the JRE is not tested for accessibility, because of potential X server problems

--- jvmfwk/plugins/sunmajor/pluginlib/JREProperties.java
+++ jvmfwk/plugins/sunmajor/pluginlib/JREProperties.java
@@ -17,15 +17,31 @@ public class JREProperties
 {
     static public void main(String[] args)
     {
-        try
+         try
         {
-            try{
-                //This line is needed to get the accessibility properties
-                Toolkit tk = java.awt.Toolkit.getDefaultToolkit();
-            }
-            catch(Throwable e)
+            boolean bNoAccess = false;
+            if(args.length > 0)
             {
-                System.err.println(e);
+                if (args[0].equals("noaccessibility"))
+                    bNoAccess = true;
+            }
+
+            //We need to be able to switch this part off because
+            //it causes an exception if the DISPLAY variable has
+            //a false value. Setting the noaccessibility argument
+            //can be done by providing a sunjavaplugin.ini with
+            //the bootstrap parameter JFW_PLUGIN_NO_NOT_CHECK_ACCESSIBILITY
+            //set to "1"
+            if (bNoAccess == false)
+            {
+                try{
+                    //This line is needed to get the accessibility properties
+                    Toolkit tk = java.awt.Toolkit.getDefaultToolkit();
+                }
+                catch(Throwable e)
+                {
+                    System.err.println(e);
+                }
             }
 
 
@@ -50,4 +66,7 @@ public class JREProperties
 
         System.exit(0);
     }
+
+
+
 }
