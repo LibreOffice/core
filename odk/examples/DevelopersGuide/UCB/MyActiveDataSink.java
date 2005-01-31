2ab15918e30c2847a35f0793973b448258ccc37d
Rüdiger Timm <rt@openoffice.org>
2005-01-31 15:58:13 +0000

    INTEGRATION: CWS sdksample (1.3.124); FILE MERGED 2004/06/16 09:43:23 jsc 1.3.124.1: #i29308# explicit imports

--- odk/examples/DevelopersGuide/UCB/MyActiveDataSink.java
+++ odk/examples/DevelopersGuide/UCB/MyActiveDataSink.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MyActiveDataSink.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:48:04 $
+ *  last change: $Author: rt $ $Date: 2005-01-31 16:58:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -38,7 +38,6 @@
  *
  *************************************************************************/
 
-// base classes
 import com.sun.star.io.XActiveDataSink;
 import com.sun.star.io.XInputStream;
 
