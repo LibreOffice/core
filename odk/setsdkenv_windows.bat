8d5aa325fa39a123ef96b2ffe4c12ca496c5da81
Rüdiger Timm <rt@openoffice.org>
2005-01-31 14:55:32 +0000

    INTEGRATION: CWS sdksample (1.13.4); FILE MERGED 2004/10/27 09:13:38 jsc 1.13.4.7: #i36006# insert bootstrap variables for Java framework 2004/10/08 14:01:18 jsc 1.13.4.6: #i29308# adapt CLI settings 2004/10/04 13:45:32 jsc 1.13.4.5: RESYNC: (1.13-1.14); FILE MERGED 2004/08/24 14:59:06 jsc 1.13.4.4: #i29308# improved 2004/08/04 12:35:55 jsc 1.13.4.3: #i29308# check UNO_PATH settings 2004/07/01 13:48:00 jsc 1.13.4.2: #i29308# set UNO_PATH 2004/06/02 12:50:08 jsc 1.13.4.1: #i26335# #i26337# prepare new scripts, configure zip tool

--- odk/setsdkenv_windows.bat
+++ odk/setsdkenv_windows.bat
@@ -16,15 +16,19 @@ REM Example: set OFFICE_HOME=C:\Programme\StarOffice
 set OFFICE_HOME=
 
 REM Directory of the make command.
-REM Example: set OO_SDK_MAKE_HOME=D:\NextGenerationMake
+REM Example: set OO_SDK_MAKE_HOME=D:\NextGenerationMake\make
 set OO_SDK_MAKE_HOME=
 
-REM Directory of the C++ tools.
+REM Directory of the zip tool.
+REM Example: set OO_SDK_ZIP_HOME=D:\infozip\bin
+set OO_SDK_ZIP_HOME=
+
+REM Directory of the C++ compiler.
 REM Example:set OO_SDK_CPP_HOME=C:\Programme\Microsoft Visual Studio\VC98\bin
 set OO_SDK_CPP_HOME=
 
 REM Directory of the C# and VB.NET compilers.
-REM Example:set OO_SDK_CLI_HOME=C:\WINXP\Microsoft.NET\Framework\v1.0.3705
+REM Example:set OO_SDK_CLI_HOME=C:\WINXP\Microsoft.NET\Framework\v1.1.4322
 set OO_SDK_CLI_HOME=
 
 REM Java installation directory.
@@ -33,12 +37,23 @@ set OO_SDK_JAVA_HOME=
 
 REM Special output directory
 REM Example: set OO_SDK_OUTPUT_DIR=c:\temp
-REM set OO_SDK_OUTPUT_DIR=
+set OO_SDK_OUTPUT_DIR=
 
 REM Automatic deployment
 REM Example: set SDK_AUTO_DEPLOYMENT=YES
 set SDK_AUTO_DEPLOYMENT=YES
 
+REM bootstrap variable, needed by the java framework
+REM Example: set UNO_JAVA_JFW_JREHOME=file:///d:/java/jdk1.5
+set UNO_JAVA_JFW_JREHOME=
+
+REM bootstrap variable, needed by the java framework
+REM Example: set UNO_JAVA_JFW_ENV_CLASSPATH=true
+set UNO_JAVA_JFW_ENV_CLASSPATH=true
+
+REM bootstrap variable, needed by the java framework
+REM Example: set UNO_JAVA_JFW_VENDOR_SETTINGS=file:///c:/program files/StarOffice%%208/share/config/javavendors.xml
+set UNO_JAVA_JFW_VENDOR_SETTINGS=
 
 REM Check installation path for the StarOffice Development Kit.
 if not defined OO_SDK_HOME (
@@ -58,9 +73,9 @@ if not defined OO_SDK_MAKE_HOME (
   goto :error
 )
 
-REM Check installation path for Java.
-if not defined OO_SDK_JAVA_HOME (
-  echo Error: the variable OO_SDK_JAVA_HOME is missing!
+REM Check installation path for the zip tool.
+if not defined OO_SDK_ZIP_HOME (
+  echo Error: the variable OO_SDK_ZIP_HOME is missing!
   goto :error
 )
 
@@ -70,11 +85,15 @@ set LIB=%OO_SDK_HOME%\windows\lib;%OO_SDK_HOME%\WINexample.out\lib;%LIB%
 REM Set office program path.
 set OFFICE_PROGRAM_PATH=%OFFICE_HOME%\program
 
+REM Set UNO path, necessary to ensure that the cpp examples using the
+REM new UNO bootstrap mechanism use the configured office installation.
+set UNO_PATH=%OFFICE_PROGRAM_PATH%
+
 REM Set classpath
 set CLASSPATH=%OFFICE_PROGRAM_PATH%\classes\jurt.jar;%OFFICE_PROGRAM_PATH%\classes\unoil.jar;%OFFICE_PROGRAM_PATH%\classes\ridl.jar;%OFFICE_PROGRAM_PATH%\classes\sandbox.jar;%OFFICE_PROGRAM_PATH%\classes\juh.jar
 
 REM Add directory of the SDK tools to the path.
-set PATH=%OO_SDK_HOME%\windows\bin;%OFFICE_HOME%\program;%PATH%
+set PATH=%OO_SDK_HOME%\windows\bin;%OFFICE_HOME%\program;%OO_SDK_HOME%\WINexample.out\bin;%PATH%
 
 REM Set PATH appropriate to the output directory
 if defined OO_SDK_OUTPUT_DIR (
@@ -86,7 +105,10 @@ if defined OO_SDK_OUTPUT_DIR (
 REM Add directory of the command make to the path, if necessary.
 if defined OO_SDK_MAKE_HOME set PATH=%OO_SDK_MAKE_HOME%;%PATH%
 
-REM Add directory of the C++ tools to the path, if necessary.
+REM Add directory of the zip tool to the path, if necessary.
+if defined OO_SDK_ZIP_HOME set PATH=%OO_SDK_ZIP_HOME%;%PATH%
+
+REM Add directory of the C++ compiler to the path, if necessary.
 if defined OO_SDK_CPP_HOME set PATH=%OO_SDK_CPP_HOME%;%PATH%
 
 REM Add directory of the C# and VB.NET compilers to the path, if necessary.
@@ -95,11 +117,29 @@ if defined OO_SDK_CLI_HOME set PATH=%OO_SDK_CLI_HOME%;%PATH%
 REM Add directory of the Java tools to the path, if necessary.
 if defined OO_SDK_JAVA_HOME set PATH=%OO_SDK_JAVA_HOME%\bin;%OO_SDK_JAVA_HOME%\jre\bin;%PATH%
 
-REM Set environment for C++ tools, if necessary.
+REM Set environment for C++ compiler tools, if necessary.
 if defined OO_SDK_CPP_HOME call "%OO_SDK_CPP_HOME%\VCVARS32.bat"
 
 REM Set tilte to identify the prepared shell.
 title Shell prepared for SDK
+
+REM Prepare shell with all necessary environment variables.
+echo. 
+echo  ************************************************************************
+echo  *
+echo  * SDK environment is prepared for Windows
+echo  *       
+echo  * SDK = %OO_SDK_HOME%
+echo  * Office = %OFFICE_HOME%
+echo  * Make = %OO_SDK_MAKE_HOME%
+echo  * Zip = %OO_SDK_ZIP_HOME%
+echo  * C++ Compiler = %OO_SDK_CPP_HOME%
+echo  * C# and VB.NET compilers = %OO_SDK_CLI_HOME%
+echo  * Java = %OO_SDK_JAVA_HOME%
+echo  * Special Output directory = %OO_SDK_OUTPUT_DIR%
+echo  * Auto deployment = %SDK_AUTO_DEPLOYMENT%
+echo  *
+echo  ************************************************************************
 
 goto end
 
