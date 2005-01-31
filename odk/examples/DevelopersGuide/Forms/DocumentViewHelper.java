47126f75108c4f7a6e1a4f2a1c6a92d08c725dbf
Rüdiger Timm <rt@openoffice.org>
2005-01-31 15:29:57 +0000

    INTEGRATION: CWS sdksample (1.3.124); FILE MERGED 2004/10/07 09:20:57 jsc 1.3.124.1: #i29208# updated

--- odk/examples/DevelopersGuide/Forms/DocumentViewHelper.java
+++ odk/examples/DevelopersGuide/Forms/DocumentViewHelper.java
@@ -2,42 +2,64 @@
  *
  *  $RCSfile: DocumentViewHelper.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:27:36 $
+ *  last change: $Author: rt $ $Date: 2005-01-31 16:29:57 $
  *
  *  The Contents of this file are made available subject to the terms of
- *  the BSD license.
+ *  either of the following licenses
  *
- *  Copyright (c) 2003 by Sun Microsystems, Inc.
- *  All rights reserved.
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
  *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *  1. Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *  3. Neither the name of Sun Microsystems, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
+ *  Sun Microsystems Inc., October, 2000
  *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *************************************************************************/
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 
+/**************************************************************************/
 import com.sun.star.uno.*;
 import com.sun.star.frame.*;
 import com.sun.star.lang.*;
@@ -53,26 +75,38 @@ import com.sun.star.form.*;
 */
 class DocumentViewHelper
 {
-    private XController         m_xController;
-    protected   XMultiServiceFactory    m_xMSF;
-    protected   DocumentHelper      m_aDocument;
+    private     XMultiServiceFactory    m_orb;
+    private     XController             m_controller;
+    private     DocumentHelper      m_document;
 
     /* ------------------------------------------------------------------ */
-    public DocumentViewHelper( XMultiServiceFactory xMSF, DocumentHelper aDocument, XController xController )
+    final protected XController getController()
     {
-        m_xMSF = xMSF;
-        m_aDocument = aDocument;
-        m_xController = xController;
+        return m_controller;
+    }
+
+    /* ------------------------------------------------------------------ */
+    final protected DocumentHelper getDocument()
+    {
+        return m_document;
+    }
+
+    /* ------------------------------------------------------------------ */
+    public DocumentViewHelper( XMultiServiceFactory orb, DocumentHelper document, XController controller )
+    {
+        m_orb = orb;
+        m_document = document;
+        m_controller = controller;
     }
 
     /* ------------------------------------------------------------------ */
     /** Quick access to a given interface of the view
         @param aInterfaceClass
-            the class of the interface which shall be returned
+                the class of the interface which shall be returned
     */
     public Object get( Class aInterfaceClass )
     {
-        return UnoRuntime.queryInterface( aInterfaceClass, m_xController );
+        return UnoRuntime.queryInterface( aInterfaceClass, m_controller );
     }
 
     /* ------------------------------------------------------------------ */
@@ -97,7 +131,7 @@ class DocumentViewHelper
         {
             // need an URLTransformer
             XURLTransformer xTransformer = (XURLTransformer)UnoRuntime.queryInterface(
-                XURLTransformer.class, m_xMSF.createInstance( "com.sun.star.util.URLTransformer" ) );
+                    XURLTransformer.class, m_orb.createInstance( "com.sun.star.util.URLTransformer" ) );
             xTransformer.parseStrict( aURL );
 
             xReturn = xProvider.queryDispatch( aURL[0], new String( ), 0 );
@@ -118,7 +152,7 @@ class DocumentViewHelper
     /* ------------------------------------------------------------------ */
     /** retrieves a control within the current view of a document
         @param xModel
-            specifies the control model which's control should be located
+            specifies the control model whose control should be located
         @return
             the control tied to the model
     */
@@ -133,14 +167,14 @@ class DocumentViewHelper
     /* ------------------------------------------------------------------ */
     public XControl getControl( Object aModel ) throws com.sun.star.uno.Exception
     {
-        XControlModel xModel = UNO.queryControlModel( aModel );
+        XControlModel xModel = (XControlModel)UnoRuntime.queryInterface( XControlModel.class, aModel );
         return getControl( xModel );
     }
 
     /* ------------------------------------------------------------------ */
     public Object getControl( Object aModel, Class aInterfaceClass ) throws com.sun.star.uno.Exception
     {
-        XControlModel xModel = UNO.queryControlModel( aModel );
+        XControlModel xModel = (XControlModel)UnoRuntime.queryInterface( XControlModel.class, aModel );
         return UnoRuntime.queryInterface( aInterfaceClass, getControl( xModel ) );
     }
 
@@ -184,7 +218,7 @@ class DocumentViewHelper
     protected void grabControlFocus( ) throws java.lang.Exception
     {
         // the forms container of our document
-        XIndexContainer xForms = UNO.queryIndexContainer( m_aDocument.getFormComponentTreeRoot( ) );
+        XIndexContainer xForms = UNO.queryIndexContainer( m_document.getFormComponentTreeRoot( ) );
         // the first form
         XIndexContainer xForm = UNO.queryIndexContainer( xForms.getByIndex( 0 ) );
 
@@ -194,7 +228,8 @@ class DocumentViewHelper
             XPropertySet xControlProps = UNO.queryPropertySet( xForm.getByIndex( i ) );
             if ( FormComponentType.FIXEDTEXT != ((Short)xControlProps.getPropertyValue( "ClassId" )).shortValue() )
             {
-                XControlModel xControlModel = UNO.queryControlModel( xControlProps );
+                XControlModel xControlModel = (XControlModel)UnoRuntime.queryInterface(
+                    XControlModel.class, xControlProps );
                 // set the focus to this control
                 grabControlFocus( xControlModel );
                 // outta here
