193a1d63ca993ba9a53f103c106c1c771cdb72c7
Rüdiger Timm <rt@openoffice.org>
2005-02-02 12:50:04 +0000

    INTEGRATION: CWS imprec01 (1.1.2); FILE ADDED 2004/12/15 06:50:26 as 1.1.2.1: #i35842# use condition inside thread for intelligent sleep

---
+++ framework/inc/helper/wakeupthread.hxx
@@ -0,0 +1,148 @@
+/*************************************************************************
+ *
+ *  $RCSfile: wakeupthread.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-02-02 13:50:04 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_HELPER_WAKEUPTHREAD_HXX_
+#define __FRAMEWORK_HELPER_WAKEUPTHREAD_HXX_
+
+//_______________________________________________
+// include files of own module
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#include <threadhelp/threadhelpbase.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_DEBUG_HXX_
+#include <macros/debug.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_GENERIC_HXX_
+#include <macros/generic.hxx>
+#endif
+
+#ifndef __FRAMEWORK_GENERAL_HXX_
+#include <general.h>
+#endif
+
+//_______________________________________________
+// include UNO interfaces
+
+#ifndef _COM_SUN_STAR_URTIL_XUPDATABLE_HPP_
+#include <com/sun/star/util/XUpdatable.hpp>
+#endif
+
+//_______________________________________________
+// include all others
+
+#ifndef _CPPUHELPER_WEAKREF_HXX_
+#include <cppuhelper/weakref.hxx>
+#endif
+
+#ifndef _THREAD_HXX_
+#include <osl/thread.hxx>
+#endif
+
+//_______________________________________________
+// namespace
+
+namespace framework{
+
+//_______________________________________________
+// definitions
+
+//===============================================
+/** @short  implements a "sleeping" thread, which try to sleep
+            without a using cpu consumption :-) */
+class WakeUpThread : public ThreadHelpBase
+                   , public ::osl::Thread
+{
+    //-------------------------------------------
+    // member
+    private:
+
+        /** @short  this listener will be notified if this thread
+                    waked up. */
+        css::uno::WeakReference< css::util::XUpdatable > m_xListener;
+
+    //-------------------------------------------
+    // interface
+    public:
+
+        /** @short  Register a new listener on this thread.
+
+            @descr  The listener is holded as a weak reference.
+                    If the thread detects, that no listener exists ...
+                    he will terminate itself.
+         */
+        WakeUpThread(const css::uno::Reference< css::util::XUpdatable >& xListener);
+
+        /** @descr  The thread waits on a condition using a fix timeout value.
+                    If the thread wakes up he notify the internal set listener.
+                    The listener can use this "timeout" info for it's own purpose.
+                    The thread itself will wait on the condition again.
+         */
+        virtual void SAL_CALL run();
+
+        virtual void SAL_CALL onTerminated();
+};
+
+} // namespace framework
+
+#endif // __FRAMEWORK_HELPER_WAKEUPTHREAD_HXX_
