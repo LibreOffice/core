13ce137f967738e1c1c5394c0fd2c75d10064bbf
Rüdiger Timm <rt@openoffice.org>
2005-02-02 12:53:40 +0000

    INTEGRATION: CWS imprec01 (1.1.2); FILE ADDED 2005/01/12 13:09:12 as 1.1.2.3: #i35842# reschedule 40x/sec only 2005/01/06 07:06:58 as 1.1.2.2: #i35842# remove RTL_LOGFILE messages 2004/12/15 06:50:50 as 1.1.2.1: #i35842# use condition inside thread for intelligent sleep

---
+++ framework/source/helper/wakeupthread.cxx
@@ -0,0 +1,123 @@
+/*************************************************************************
+ *
+ *  $RCSfile: wakeupthread.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-02-02 13:53:40 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRUNTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRUNTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+//_______________________________________________
+// include files of own module
+
+#ifndef __FRAMEWORK_HELPER_WAKEUPTHREAD_HXX_
+#include <helper/wakeupthread.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_READGUARD_HXX_
+#include <threadhelp/readguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_WRITEGUARD_HXX_
+#include <threadhelp/writeguard.hxx>
+#endif
+
+//_______________________________________________
+// namespace
+
+namespace framework{
+
+//_______________________________________________
+// declarations
+
+//***********************************************
+WakeUpThread::WakeUpThread(const css::uno::Reference< css::util::XUpdatable >& xListener)
+    : ThreadHelpBase(         )
+    , m_xListener   (xListener)
+{
+}
+
+//***********************************************
+void SAL_CALL WakeUpThread::run()
+{
+    ::osl::Condition aSleeper;
+
+    TimeValue aTime;
+    aTime.Seconds =     0;
+    aTime.Nanosec = 25000;
+
+    while(schedule())
+    {
+        aSleeper.reset();
+        aSleeper.wait(&aTime);
+
+        // SAFE ->
+        ReadGuard aReadLock(m_aLock);
+        css::uno::Reference< css::util::XUpdatable > xListener(m_xListener.get(), css::uno::UNO_QUERY);
+        aReadLock.unlock();
+        // <- SAFE
+
+        if (xListener.is())
+            xListener->update();
+    }
+}
+
+//***********************************************
+void SAL_CALL WakeUpThread::onTerminated()
+{
+    delete this;
+}
+
+} // namespace framework
