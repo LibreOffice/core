d5625f3e1ea7b12ef04c2dd7e7ee90d548a24d38
Jens-Heiner Rechtien <hr@openoffice.org>
2005-02-11 14:27:27 +0000

    INTEGRATION: CWS fpic1 (1.5.158); FILE MERGED 2005/02/01 14:40:40 cmc 1.5.158.1: #i41014# centralize -fpic/-fPIC into one variable

--- solenv/inc/unxbsdi2.mk
+++ solenv/inc/unxbsdi2.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxbsdi2.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2004-09-20 08:36:51 $
+#   last change: $Author: hr $ $Date: 2005-02-11 15:27:27 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -107,6 +107,7 @@ CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
 CFLAGSCXX= -pipe -frtti
 CFLAGSCXX+= -Wno-ctor-dtor-privacy
+PICSWITCH:=-fpic
 
 # Compiler flags for compiling static object in single threaded environment with graphical user interface
 CFLAGSOBJGUIST=
@@ -117,9 +118,9 @@ CFLAGSOBJGUIMT=
 # Compiler flags for compiling static object in multi threaded environment with character user interface
 CFLAGSOBJCUIMT=
 # Compiler flags for compiling shared object in multi threaded environment with graphical user interface
-CFLAGSSLOGUIMT=-fpic
+CFLAGSSLOGUIMT=$(PICSWITCH)
 # Compiler flags for compiling shared object in multi threaded environment with character user interface
-CFLAGSSLOCUIMT=-fpic
+CFLAGSSLOCUIMT=$(PICSWITCH)
 # Compiler flags for profiling
 CFLAGSPROF=
 # Compiler flags for debugging
