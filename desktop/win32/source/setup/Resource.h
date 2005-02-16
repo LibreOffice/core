7665787520ecf64a2dbf6816ffbf3a889fa11056
Vladimir Glazounov <vg@openoffice.org>
2005-02-16 15:51:55 +0000

    INTEGRATION: CWS installer05 (1.4.10); FILE MERGED 2005/02/03 09:14:51 dv 1.4.10.1: cleaned up resources

--- desktop/win32/source/setup/Resource.h
+++ desktop/win32/source/setup/Resource.h
@@ -1,36 +1,81 @@
-//+-------------------------------------------------------------------------
-//
-//  Copyright (c) Microsoft Corporation.  All rights reserved.
-//
-//  File:       resource.h
-//
-//--------------------------------------------------------------------------
+/*************************************************************************
+ *
+ *  $RCSfile: Resource.h,v $
+ *
+ *  $Revision: 1.5 $
+ *
+ *  last change: $Author: vg $ $Date: 2005-02-16 16:51:55 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2004 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 
 #ifndef _RESOURCE_H
 #define _RESOURCE_H
 
 #define IDS_APP_TITLE   10
 #define IDS_APP_PROD_TITLE  11
-#define IDS_APP_OK  12
-#define IDS_APP_CANCEL  13
-#define IDS_OUTOFMEM    14
-#define IDS_NOMSI   15
-#define IDS_REQUIRES_NEWER_VERSION  16
-#define IDS_INVALID_PATH    17
-#define IDS_USER_CANCELLED  18
-#define IDS_REQUIRES_ADMIN_PRIV 19
-#define IDS_FILE_NOT_FOUND  20
-#define IDS_FAILED_TO_UPGRADE_MSI   21
-#define IDS_REBOOT_REQUIRED 22
-#define IDS_INVALID_PARAM   23
-#define IDS_INSTALL_ERROR   24
-#define IDS_ALLOW_MSI_UPDATE    25
-#define IDS_USAGE   26
-#define IDS_ALREADY_RUNNING 27
-#define IDS_UNKNOWN_ERROR   28
-#define IDS_INVALID_PROFILE 29
-#define IDS_CHOOSE_LANG 30
-#define IDS_UNKNOWN_LANG    31
+#define IDS_OUTOFMEM    12
+#define IDS_NOMSI   13
+#define IDS_USER_CANCELLED  14
+#define IDS_REQUIRES_ADMIN_PRIV 15
+#define IDS_FILE_NOT_FOUND  16
+#define IDS_INVALID_PARAM   17
+#define IDS_ALLOW_MSI_UPDATE    18
+#define IDS_USAGE   19
+#define IDS_ALREADY_RUNNING 20
+#define IDS_UNKNOWN_ERROR   21
+#define IDS_INVALID_PROFILE 22
+#define IDS_UNKNOWN_LANG    23
 #define IDS_LANGUAGE_ENGLISH    50
 #define IDS_LANGUAGE_SPAIN  51
 #define IDS_LANGUAGE_GERMAN 52
@@ -60,10 +105,6 @@
 #define IDS_LANGUAGE_ET 76
 #define IDS_LANGUAGE_ZH_CN  77
 #define IDS_LANGUAGE_PT_PT  78
-
-#define IDD_LANGUAGE                    101
-#define IDC_COMBO1                      1001
-#define IDC_TEXT01                      1002
 
 #define IDI_INSTALLER                  99
 
