3b705471a099d8841eed72b75b652929bbd87e81
Vladimir Glazounov <vg@openoffice.org>
2005-02-16 14:48:02 +0000

    INTEGRATION: CWS visibility02 (1.10.38); FILE MERGED 2004/12/22 04:02:27 mnicel 1.10.38.1: Issue number:38608 Symbol visibility work.

--- ucbhelper/source/client/makefile.mk
+++ ucbhelper/source/client/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: svesik $ $Date: 2004-04-21 12:34:54 $
+#   last change: $Author: vg $ $Date: 2005-02-16 15:48:02 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :	svpre.mk
 .INCLUDE :	settings.mk
 .INCLUDE :	sv.mk
+.INCLUDE :	$(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
