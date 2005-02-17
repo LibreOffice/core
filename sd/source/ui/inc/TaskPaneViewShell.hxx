1336ea4a69e1c7325709963520bea347cd4bdda6
Vladimir Glazounov <vg@openoffice.org>
2005-02-17 08:42:55 +0000

    INTEGRATION: CWS impress31 (1.3.72); FILE MERGED 2005/01/28 09:13:05 cl 1.3.72.1: #i32920# reworked FuPage to work with master pages without a slide

--- sd/source/ui/inc/TaskPaneViewShell.hxx
+++ sd/source/ui/inc/TaskPaneViewShell.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TaskPaneViewShell.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 20:14:46 $
+ *  last change: $Author: vg $ $Date: 2005-02-17 09:42:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -135,6 +135,7 @@ public:
     virtual void KeyInput (const KeyEvent& rEvent);
 
     virtual SdPage* GetActualPage (void);
+    virtual SdPage* getCurrentPage (void) const;
 
     void Execute (SfxRequest& rRequest);
     void GetState (SfxItemSet& rItemSet);
