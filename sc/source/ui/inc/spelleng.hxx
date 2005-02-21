5f9906ad8968c54eab8ebbe1f9047a3788147b87
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 12:50:58 +0000

    INTEGRATION: CWS dr32 (1.7.148); FILE MERGED 2005/01/12 10:41:00 dr 1.7.148.1: #i34769# conversion parameter handling; restore cursor after conversion

--- sc/source/ui/inc/spelleng.hxx
+++ sc/source/ui/inc/spelleng.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: spelleng.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2004-09-24 10:31:36 $
+ *  last change: $Author: vg $ $Date: 2005-02-21 13:50:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,14 +58,17 @@
  *
  *
  ************************************************************************/
-
 #ifndef SC_SPELLENG_HXX
 #define SC_SPELLENG_HXX
 
+#ifndef SC_EDITUTIL_HXX
 #include "editutil.hxx"
-
+#endif
 #ifndef SC_SELECTIONSTATE_HXX
 #include "selectionstate.hxx"
+#endif
+#ifndef SC_SPELLPARAM_HXX
+#include "spellparam.hxx"
 #endif
 
 class ScViewData;
@@ -79,60 +82,62 @@ class SfxItemPool;
 class ScConversionEngineBase : public ScEditEngineDefaulter
 {
 public:
-    explicit                ScConversionEngineBase(
-                                SfxItemPool* pEnginePool, ScViewData& rViewData,
-                                ScDocument* pUndoDoc, ScDocument* pRedoDoc );
+    explicit            ScConversionEngineBase(
+                            SfxItemPool* pEnginePool, ScViewData& rViewData,
+                            ScDocument* pUndoDoc, ScDocument* pRedoDoc );
 
-    virtual                 ~ScConversionEngineBase();
+    virtual             ~ScConversionEngineBase();
 
     /** Derived classes implement to convert all cells in the selection or sheet. */
-    virtual void            ConvertAll( EditView& rEditView ) = 0;
+    virtual void        ConvertAll( EditView& rEditView ) = 0;
 
     /** Returns true, if at least one cell has been modified. */
-    inline bool             IsAnyModified() const { return mbIsAnyModified; }
+    inline bool         IsAnyModified() const { return mbIsAnyModified; }
     /** Returns true, if the entire document/selection has been finished. */
-    inline bool             IsFinished() const { return mbFinished; }
+    inline bool         IsFinished() const { return mbFinished; }
 
 protected:
     /** Implementation of cell iteration. Finds a cell that needs conversion.
         @return  true = Current cell needs conversion (i.e. spelling error found). */
-    bool                    FindNextConversionCell();
+    bool                FindNextConversionCell();
+    /** Restores the initial cursor position. */
+    void                RestoreCursorPos();
 
     /** Derived classes return, if the current text needs conversion (i.e. spelling error found).
         @return  true = Current edit text needs conversion. */
-    virtual bool            NeedsConversion() = 0;
+    virtual bool        NeedsConversion() = 0;
 
     /** Derived classes may show a query box that asks whether to restart at top of the sheet.
         @descr  Default here is no dialog and restart always.
         @return  true = Restart at top, false = Stop the conversion. */
-    virtual bool            ShowTableWrapDialog();
+    virtual bool        ShowTableWrapDialog();
     /** Derived classes may show a message box stating that the conversion is finished.
         @descr  Default here is no dialog. */
-    virtual void            ShowFinishDialog();
+    virtual void        ShowFinishDialog();
 
 private:
     /** Fills the edit engine from a document cell. */
-    void                    FillFromCell( SCCOL nCol, SCROW nRow, SCTAB nTab );
+    void                FillFromCell( SCCOL nCol, SCROW nRow, SCTAB nTab );
 
 protected:  // for usage in derived classes
-    ScViewData&             mrViewData;
-    ScDocShell&             mrDocShell;
-    ScDocument&             mrDoc;
+    ScViewData&         mrViewData;
+    ScDocShell&         mrDocShell;
+    ScDocument&         mrDoc;
 
 private:
-    ScSelectionState        maSelState;         /// Selection data of the document.
-    ScDocument*             mpUndoDoc;          /// Document stores all old cells for UNDO action.
-    ScDocument*             mpRedoDoc;          /// Document stores all new cells for REDO action.
-    LanguageType            meCurrLang;         /// Current cell language.
-    SCCOL                   mnStartCol;         /// Initial column index.
-    SCROW                   mnStartRow;         /// Initial row index.
-    SCTAB                   mnStartTab;         /// Initial sheet index.
-    SCCOL                   mnCurrCol;          /// Current column index.
-    SCROW                   mnCurrRow;          /// Current row index.
-    bool                    mbIsAnyModified;    /// true = At least one cell has been changed.
-    bool                    mbInitialState;     /// true = Not searched for a cell yet.
-    bool                    mbWrappedInTable;   /// true = Already restarted at top of the sheet.
-    bool                    mbFinished;         /// true = Entire document/selection finished.
+    ScSelectionState    maSelState;         /// Selection data of the document.
+    ScDocument*         mpUndoDoc;          /// Document stores all old cells for UNDO action.
+    ScDocument*         mpRedoDoc;          /// Document stores all new cells for REDO action.
+    LanguageType        meCurrLang;         /// Current cell language.
+    SCCOL               mnStartCol;         /// Initial column index.
+    SCROW               mnStartRow;         /// Initial row index.
+    SCTAB               mnStartTab;         /// Initial sheet index.
+    SCCOL               mnCurrCol;          /// Current column index.
+    SCROW               mnCurrRow;          /// Current row index.
+    bool                mbIsAnyModified;    /// true = At least one cell has been changed.
+    bool                mbInitialState;     /// true = Not searched for a cell yet.
+    bool                mbWrappedInTable;   /// true = Already restarted at top of the sheet.
+    bool                mbFinished;         /// true = Entire document/selection finished.
 };
 
 // ============================================================================
@@ -143,32 +148,32 @@ class ScSpellingEngine : public ScConversionEngineBase
 public:
     typedef ::com::sun::star::uno::Reference< ::com::sun::star::linguistic2::XSpellChecker1 > XSpellCheckerRef;
 
-    explicit                ScSpellingEngine(
-                                SfxItemPool* pEnginePool,
-                                ScViewData& rViewData,
-                                ScDocument* pUndoDoc,
-                                ScDocument* pRedoDoc,
-                                XSpellCheckerRef xSpeller );
+    explicit            ScSpellingEngine(
+                            SfxItemPool* pEnginePool,
+                            ScViewData& rViewData,
+                            ScDocument* pUndoDoc,
+                            ScDocument* pRedoDoc,
+                            XSpellCheckerRef xSpeller );
 
     /** Checks spelling of all cells in the selection or sheet. */
-    virtual void            ConvertAll( EditView& rEditView );
+    virtual void        ConvertAll( EditView& rEditView );
 
 protected:
     /** Callback from edit engine to check the next cell. */
-    virtual BOOL            SpellNextDocument();
+    virtual BOOL        SpellNextDocument();
 
     /** Returns true, if the current text contains a spelling error. */
-    virtual bool            NeedsConversion();
+    virtual bool        NeedsConversion();
 
     /** Show a query box that asks whether to restart at top of the sheet.
         @return  true = Restart at top, false = Stop the conversion. */
-    virtual bool            ShowTableWrapDialog();
+    virtual bool        ShowTableWrapDialog();
     /** Show a message box stating that spell checking is finished. */
-    virtual void            ShowFinishDialog();
+    virtual void        ShowFinishDialog();
 
 private:
     /** Returns the spelling dialog if it is open. */
-    Window*                 GetDialogParent();
+    Window*             GetDialogParent();
 };
 
 // ============================================================================
@@ -177,34 +182,25 @@ private:
 class ScTextConversionEngine : public ScConversionEngineBase
 {
 public:
-    explicit                ScTextConversionEngine(
-                                SfxItemPool* pEnginePool,
-                                ScViewData& rViewData,
-                                ScDocument* pUndoDoc,
-                                ScDocument* pRedoDoc,
-                                LanguageType eSourceLanguage,   // source language for conversion
-                                LanguageType eTargetLanguage,   // target language for conversion
-                                const Font *pTargetFont,        // target font to be used if language has to be changed
-                                sal_Int32 nOptions,             // misc conversion flags
-                                sal_Bool bIsInteractive         // textconversiob has (specific) dialog that may be raised
-                            );
+    explicit            ScTextConversionEngine(
+                            SfxItemPool* pEnginePool,
+                            ScViewData& rViewData,
+                            const ScConversionParam& rConvParam,
+                            ScDocument* pUndoDoc,
+                            ScDocument* pRedoDoc );
 
     /** Converts all cells in the selection or sheet according to set language. */
-    virtual void            ConvertAll( EditView& rEditView );
+    virtual void        ConvertAll( EditView& rEditView );
 
 protected:
     /** Callback from edit engine to convert the next cell. */
-    virtual BOOL            ConvertNextDocument();
+    virtual BOOL        ConvertNextDocument();
 
     /** Returns true, if the current text contains text to convert. */
-    virtual bool            NeedsConversion();
+    virtual bool        NeedsConversion();
 
 private:
-    LanguageType    meSourceLang;       // source language for conversion
-    LanguageType    meTargetLang;       // target language for conversion
-    const Font *    mpTargetFont;       // target font to be used if language has to be changed
-    sal_Int32       mnOptions;          // misc conversion flags
-    sal_Bool        mbIsInteractive;    // textconversiob has (specific) dialog that may be raised
+    ScConversionParam   maConvParam;        /// Conversion parameters.
 };
 
 // ============================================================================
