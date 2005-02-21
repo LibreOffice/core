1c4f77de36fc7e33ff2c6417fc6abc97f1bdaf71
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 11:41:52 +0000

    INTEGRATION: CWS mozab05 (1.3.28); FILE MERGED 2005/01/14 10:39:04 windly 1.3.28.1: #i20088# add browse support

--- dbaccess/source/ui/dlg/DriverSettings.hxx
+++ dbaccess/source/ui/dlg/DriverSettings.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DriverSettings.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pjunck $ $Date: 2004-10-27 13:00:07 $
+ *  last change: $Author: vg $ $Date: 2005-02-21 12:41:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -164,6 +164,7 @@ namespace dbaui
             @return SfxTabPage*
         */
         static  SfxTabPage* CreateLDAP( Window* _pParent, const SfxItemSet& _rAttrSet );
+
 
         /** Creates the detail page for Text
             @param  _pParent
