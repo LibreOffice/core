d0de4bad9dd9ccc450f91d09287ebc27d978edaf
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 12:44:51 +0000

    INTEGRATION: CWS dr32 (1.11.2); FILE MERGED 2005/02/04 14:03:08 dr 1.11.2.3: #i37725# add missing documentation 2005/02/02 14:14:21 dr 1.11.2.2: #b6219324# #i23079# #i27871# #i35812# #i37725# new address converter, import/export of view settings 2005/01/19 15:03:05 dr 1.11.2.1: #i40570# removed/changed constants and enums

--- sc/source/filter/inc/xihelper.hxx
+++ sc/source/filter/inc/xihelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xihelper.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2005-02-16 18:09:00 $
+ *  last change: $Author: vg $ $Date: 2005-02-21 13:44:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -69,6 +69,12 @@
 #include "scmatrix.hxx"
 #endif
 
+#ifndef SC_XLADDRESS_HXX
+#include "xladdress.hxx"
+#endif
+#ifndef SC_XLSTRING_HXX
+#include "xlstring.hxx"
+#endif
 #ifndef SC_XIROOT_HXX
 #include "xiroot.hxx"
 #endif
@@ -76,17 +82,96 @@
 #include "xistream.hxx"
 #endif
 
-class ScMatrix;
+// Excel->Calc cell address/range conversion ==================================
+
+/** Provides functions to convert Excel cell addresses to Calc cell addresses. */
+class XclImpAddressConverter : public XclAddressConverterBase
+{
+public:
+    explicit            XclImpAddressConverter( const XclImpRoot& rRoot );
+
+    // cell address -----------------------------------------------------------
+
+    /** Checks if the passed Excel cell address is valid.
+        @param rXclPos  The Excel cell address to check.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell address is not valid.
+        @return  true = Cell address in rXclPos is valid. */
+    bool                CheckAddress( const XclAddress& rXclPos, bool bWarn );
+
+    /** Converts the passed Excel cell address to a Calc cell address.
+        @param rScPos  (Out) The converted Calc cell address, if valid.
+        @param rXclPos  The Excel cell address to convert.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell address is invalid.
+        @return  true = Cell address returned in rScPos is valid. */
+    bool                ConvertAddress( ScAddress& rScPos,
+                            const XclAddress& rXclPos, SCTAB nScTab, bool bWarn );
+
+    /** Returns a valid cell address by moving it into allowed dimensions.
+        @param rXclPos  The Excel cell address to convert.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell address is invalid.
+        @return  The converted Calc cell address. */
+    ScAddress           CreateValidAddress( const XclAddress& rXclPos,
+                            SCTAB nScTab, bool bWarn );
+
+    // cell range -------------------------------------------------------------
+
+    /** Checks if the passed cell range is valid (checks start and end position).
+        @param rXclRange  The Excel cell range to check.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell range is not valid.
+        @return  true = Cell range in rXclRange is valid. */
+    bool                CheckRange( const XclRange& rXclRange, bool bWarn );
+
+    /** Converts the passed Excel cell range to a Calc cell range.
+        @param rScRange  (Out) The converted Calc cell range, if valid.
+        @param rXclRange  The Excel cell range to convert.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell range contains invalid cells.
+        @return  true = Cell range returned in rScRange is valid (original or cropped). */
+    bool                ConvertRange( ScRange& rScRange, const XclRange& rXclRange,
+                            SCTAB nScTab1, SCTAB nScTab2, bool bWarn );
+
+    /** Returns a valid cell range by moving it into allowed dimensions.
+        @descr  The start and/or end position of the range may be modified.
+        @param rXclRange  The Excel cell range to convert.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell range contains invalid cells.
+        @return  The converted Calc cell range. */
+    ScRange             CreateValidRange( const XclRange& rXclRange,
+                            SCTAB nScTab1, SCTAB nScTab2, bool bWarn );
+
+    // cell range list --------------------------------------------------------
+
+    /** Checks if the passed cell range list is valid.
+        @param rXclRanges  The Excel cell range list to check.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if the cell range list contains at
+            least one invalid range.
+        @return  true = Cell range list in rScRanges is completly valid. */
+    bool                CheckRangeList( const XclRangeList& rXclRanges, bool bWarn );
+
+    /** Converts the passed Excel cell range list to a Calc cell range list.
+        @descr  The start position of the ranges will not be modified. Cell
+            ranges that fit partly into valid dimensions are cropped
+            accordingly. Cell ranges that do not fit at all, are not inserted
+            into the Calc cell range list.
+        @param rScRanges  (Out) The converted Calc cell range list.
+        @param rXclRanges  The Excel cell range list to convert.
+        @param bWarn  true = Sets the internal flag that produces a warning box
+            after loading/saving the file, if at least one of the cell ranges
+            contains invalid cells. */
+    void                ConvertRangeList( ScRangeList& rScRanges,
+                            const XclRangeList& rXclRanges, SCTAB nScTab, bool bWarn );
+};
 
 // Byte/Unicode strings =======================================================
 
 /** This class represents an unformatted or formatted string and provides importing from stream. */
 class XclImpString
 {
-private:
-    String              maString;       /// The text data of the string.
-    XclFormatRunVec     maFormats;      /// All formatting runs.
-
 public:
     /** Constructs an unformatted string. */
     explicit            XclImpString( const String& rString );
@@ -108,6 +193,10 @@ public:
     inline bool         IsRich() const { return !maFormats.empty(); }
     /** Returns the formatting run vector. */
     inline const XclFormatRunVec& GetFormats() const { return maFormats; }
+
+private:
+    String              maString;       /// The text data of the string.
+    XclFormatRunVec     maFormats;      /// All formatting runs.
 };
 
 // String->EditEngine conversion ==============================================
@@ -303,16 +392,6 @@ class ScTokenArray;
     CRN, EXTERNNAME, tArray). */
 class XclImpCachedValue : ScfNoCopy
 {
-protected:
-    typedef ::std::auto_ptr< String >               StringPtr;
-    typedef ::std::auto_ptr< const ScTokenArray >   ScTokenArrayPtr;
-
-    StringPtr           mxStr;      /// Cached value is a string.
-    double              mfValue;    /// Cached value is a double.
-    ScTokenArrayPtr     mxTokArr;   /// Cached value is a formula or error code or Boolean.
-    sal_uInt8           mnBoolErr;  /// Boolean value or Excel error code.
-    sal_uInt8           mnType;     /// The type of the cached value (EXC_CACHEDVAL_*).
-
 public:
     /** Creates a cached value and reads contents from stream and stores it with its array address. */
     explicit            XclImpCachedValue( XclImpStream& rStrm );
@@ -330,6 +409,16 @@ public:
     USHORT              GetError() const;
     /** Returns the token array if this is a Boolean value or error value, else 0. */
     inline const ScTokenArray* GetBoolErrFmla() const { return mxTokArr.get(); }
+
+protected:
+    typedef ::std::auto_ptr< String >               StringPtr;
+    typedef ::std::auto_ptr< const ScTokenArray >   ScTokenArrayPtr;
+
+    StringPtr           mxStr;      /// Cached value is a string.
+    double              mfValue;    /// Cached value is a double.
+    ScTokenArrayPtr     mxTokArr;   /// Cached value is a formula or error code or Boolean.
+    sal_uInt8           mnBoolErr;  /// Boolean value or Excel error code.
+    sal_uInt8           mnType;     /// The type of the cached value (EXC_CACHEDVAL_*).
 };
 
 // ----------------------------------------------------------------------------
@@ -350,7 +439,6 @@ private:
     XclImpValueList     maValueList;    /// List of cached cell values.
     SCSIZE              mnScCols;       /// Number of cached columns.
     SCSIZE              mnScRows;       /// Number of cached rows.
-
 };
 
 // ============================================================================
