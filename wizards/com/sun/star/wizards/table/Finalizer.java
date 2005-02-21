ad0a0ab39f30b8320494e0e3dadcce34ffe40725
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 13:01:16 +0000

    INTEGRATION: CWS dbwizard3 (1.2.20); FILE MERGED 2005/01/16 20:52:56 bc 1.2.20.7: #i40282#Thread handling in Webwizard removed 2005/01/12 10:01:00 bc 1.2.20.6: #i37933#initialize of Aggregatecomponent.java modified 2005/01/10 20:34:21 bc 1.2.20.5: #i38555# Automatic Autovalue checkbox now also toggled 2005/01/07 16:52:20 bc 1.2.20.4: ##lables for schema and catalog added Issue number: Submitted by: Reviewed by: 2005/01/07 15:17:07 bc 1.2.20.3: #i40082#new NumberFormatter class added 2004/12/18 21:55:01 bc 1.2.20.2: ##code for including schemas and catalogs in tablewizard added 2004/12/06 14:55:48 bc 1.2.20.1: ##several bugfixes

--- wizards/com/sun/star/wizards/table/Finalizer.java
+++ wizards/com/sun/star/wizards/table/Finalizer.java
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: Finalizer.java,v $
 *
-*  $Revision: 1.2 $
+*  $Revision: 1.3 $
 *
-*  last change: $Author: pjunck $ $Date: 2004-10-27 13:38:12 $
+*  last change: $Author: vg $ $Date: 2005-02-21 14:01:16 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
@@ -57,19 +57,25 @@
 *  Contributor(s): _______________________________________
 *
 */
-
 package com.sun.star.wizards.table;
 
 import com.sun.star.awt.FocusEvent;
+import com.sun.star.awt.TextEvent;
 import com.sun.star.awt.VclWindowPeerAttribute;
 import com.sun.star.awt.XFocusListener;
+import com.sun.star.awt.XListBox;
 import com.sun.star.awt.XRadioButton;
 import com.sun.star.awt.XTextComponent;
+import com.sun.star.awt.XTextListener;
 import com.sun.star.awt.XWindow;
+import com.sun.star.beans.UnknownPropertyException;
 import com.sun.star.lang.EventObject;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.sdbc.SQLException;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.wizards.common.Configuration;
 import com.sun.star.wizards.common.Desktop;
+import com.sun.star.wizards.common.Helper;
 import com.sun.star.wizards.common.JavaTools;
 import com.sun.star.wizards.common.SystemDialog;
 import com.sun.star.wizards.db.DBMetaData;
@@ -78,20 +84,23 @@ import com.sun.star.wizards.ui.UIConsts;
 import com.sun.star.wizards.ui.*;
 
 
-public class Finalizer implements XFocusListener{
-    WizardDialog CurUnoDialog;
+public class Finalizer{
+    TableWizard CurUnoDialog;
     short curtabindex;
     XRadioButton optModifyTable;
     XRadioButton optWorkWithTable;
     XRadioButton optStartFormWizard;
     XTextComponent txtTableName;
-    DBMetaData curtabledescriptor;
-    public String TOGGLEFINISHBUTTON = "toggleFinishButton";
+    XListBox xCatalogListBox;
+    XListBox xSchemaListBox;
+    TableDescriptor curtabledescriptor;
+    public String SETCOMPLETIONFLAG = "setCompletionFlag";
     public static int WORKWITHTABLEMODE = 0;
     public static int MODIFYTABLEMODE = 1;
     public static int STARTFORMWIZARDMODE = 2;
 
-    public Finalizer(WizardDialog _CurUnoDialog, TableDescriptor _curtabledescriptor) {
+
+    public Finalizer(TableWizard _CurUnoDialog, TableDescriptor _curtabledescriptor) {
         this.CurUnoDialog = _CurUnoDialog;
         this.curtabledescriptor = _curtabledescriptor;
         curtabindex = (short) (TableWizard.SOFINALPAGE * 100);
@@ -102,41 +111,111 @@ public class Finalizer implements XFocusListener{
         String sStartFormWizard = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 39);
         String sModifyTable = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 37);
         String sCongratulations  = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 35);
+        String slblCatalog = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 49);
+        String slblSchema = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 50);
+        String[] sCatalogNames = curtabledescriptor.getCatalogNames();
+        String[] sSchemaNames = curtabledescriptor.getSchemaNames();
+        int nListBoxPosX = 97;
+        int ndiffPosY = 0;
         CurUnoDialog.insertLabel("lblTableName",
           new String[] {"Height", "Label", "PositionX", "PositionY", "Step", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], slblTableName, new Integer(97), new Integer(25), IFINALSTEP, new Integer(111)}
+          new Object[] { UIConsts.INTEGERS[8], slblTableName, new Integer(97), new Integer(25), IFINALSTEP, new Integer(200)}
         );
-        txtTableName = CurUnoDialog.insertTextField("txtTableName", TOGGLEFINISHBUTTON, this,
+        txtTableName = CurUnoDialog.insertTextField("txtTableName", SETCOMPLETIONFLAG, this,
           new String[] {"Height", "HelpURL", "PositionX", "PositionY", "Step", "TabIndex", "Text", "Width"},
-          new Object[] { UIConsts.INTEGER_12, "HID:41240", new Integer(97), new Integer(35),IFINALSTEP, new Short(curtabindex++),"", new Integer(185)}
+          new Object[] { UIConsts.INTEGER_12, "HID:41240", new Integer(97), new Integer(35),IFINALSTEP, new Short(curtabindex++),"", new Integer(183)}
         );
-        XWindow xWindow = (XWindow) UnoRuntime.queryInterface(XWindow.class, txtTableName);
-        xWindow.addFocusListener(this);
+        txtTableName.addTextListener(CurUnoDialog);
+        txtTableName.setMaxTextLen((short) this.curtabledescriptor.getMaxTableNameLength());
 
-        CurUnoDialog.insertLabel("lblcongratulations",
-          new String[] {"Height", "Label", "MultiLine", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], sCongratulations, Boolean.TRUE, new Integer(97), new Integer(62),IFINALSTEP, new Short(curtabindex++), new Integer(154)}
-        );
+        if (sCatalogNames != null){
+            if (sCatalogNames.length > 0){
+                String sCatalog = "";
+                try {
+                    sCatalog = curtabledescriptor.DBConnection.getCatalog();
+                } catch (SQLException e1) {
+                    e1.printStackTrace(System.out);
+                }
+                CurUnoDialog.insertLabel("lblCatalog",
+                        new String[] {"Height", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width" },
+                        new Object[] {new Integer(8), slblCatalog, new Integer(nListBoxPosX), new Integer(52), IFINALSTEP, new Short(curtabindex++), new Integer(100) });
+
+                try {
+                    xCatalogListBox = CurUnoDialog.insertListBox("lstCatalog", null, null,
+                        new String[] { "Dropdown", "Height", "HelpURL", "LineCount", "PositionX", "PositionY", "Step", "StringItemList", "TabIndex", "Width" },
+                        new Object[] { Boolean.TRUE, new Integer(12), "HID:41244", new Short("7"), new Integer(nListBoxPosX), new Integer(62), IFINALSTEP, sCatalogNames, new Short(curtabindex++), new Integer(80)});
+                    int isel = JavaTools.FieldInList(sCatalogNames, sCatalog);
+                    if (isel < 0)
+                        isel = 0;
+                    CurUnoDialog.setControlProperty("lstCatalog", "SelectedItems", new short[]{(short) isel});
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
+                nListBoxPosX = 200;
+            }
+        }
+
+        if (sSchemaNames != null){
+            if (sSchemaNames.length > 0){
+                String sSchema = "";
+                try {
+                    sSchema = (String) curtabledescriptor.xDataSourcePropertySet.getPropertyValue("User");
+                } catch (Exception e1) {
+                    e1.printStackTrace(System.out);
+                }
+                CurUnoDialog.insertLabel("lblSchema",
+                        new String[] {"Height", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width" },
+                        new Object[] {new Integer(8), slblSchema, new Integer(nListBoxPosX), new Integer(52), IFINALSTEP, new Short(curtabindex++), new Integer(80) });
+
+                try {
+                    xSchemaListBox = CurUnoDialog.insertListBox("lstSchema", null, null,
+                                new String[] { "Dropdown", "Height", "HelpURL", "LineCount", "PositionX", "PositionY", "Step", "StringItemList", "TabIndex", "Width" },
+                                new Object[] { Boolean.TRUE, new Integer(12), "HID:41245", new Short("7"), new Integer(nListBoxPosX), new Integer(62), IFINALSTEP, sSchemaNames, new Short(curtabindex++), new Integer(80)});
+                    int isel = JavaTools.FieldInList(sSchemaNames, sSchema);
+                    if (isel < 0)
+                        isel = 0;
+                    CurUnoDialog.setControlProperty("lstSchema", "SelectedItems", new short[]{(short) isel});
+                } catch (Exception e){
+                    e.printStackTrace(System.out);
+                }
+            }
+        }
+
+        if ((sCatalogNames == null) && (sSchemaNames == null)){
+            CurUnoDialog.insertLabel("lblcongratulations",
+                  new String[] {"Height", "Label", "MultiLine", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
+                  new Object[] { new Integer(16), sCongratulations, Boolean.TRUE, new Integer(97), new Integer(62),IFINALSTEP, new Short(curtabindex++), new Integer(206)});
+        }
+        else{
+            if ((sCatalogNames.length == 0) && (sSchemaNames.length == 0)){
+                CurUnoDialog.insertLabel("lblcongratulations",
+                          new String[] {"Height", "Label", "MultiLine", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
+                          new Object[] { new Integer(16), sCongratulations, Boolean.TRUE, new Integer(97), new Integer(62),IFINALSTEP, new Short(curtabindex++), new Integer(206)});
+            }
+            else
+                ndiffPosY = 10;
+        }
+
+
         CurUnoDialog.insertLabel("lblProceed",
           new String[] {"Height", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], slblProceed, new Integer(97), new Integer(82),IFINALSTEP, new Short(curtabindex++), new Integer(154)}
+          new Object[] { UIConsts.INTEGERS[8], slblProceed, new Integer(97), new Integer(82+ndiffPosY),IFINALSTEP, new Short(curtabindex++), new Integer(207)}
         );
         optWorkWithTable = CurUnoDialog.insertRadioButton("optWorkWithTable", null,
           new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "State", "Step", "TabIndex", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], "HID:41242", sWorkWithTable, new Integer(101), new Integer(97), new Short((short)1),IFINALSTEP, new Short(curtabindex++), new Integer(107)}
+          new Object[] { UIConsts.INTEGERS[8], "HID:41242", sWorkWithTable, new Integer(101), new Integer(97+ndiffPosY), new Short((short)1),IFINALSTEP, new Short(curtabindex++), new Integer(157)}
         );
         optModifyTable = CurUnoDialog.insertRadioButton("optModifyTable", null,
           new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], "HID:41241", sModifyTable, new Integer(101), new Integer(109),IFINALSTEP, new Short(curtabindex++), new Integer(107)}
+          new Object[] { UIConsts.INTEGERS[8], "HID:41241", sModifyTable, new Integer(101), new Integer(109+ndiffPosY),IFINALSTEP, new Short(curtabindex++), new Integer(157)}
         );
         optStartFormWizard = CurUnoDialog.insertRadioButton("optStartFormWizard", null,
           new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-          new Object[] { UIConsts.INTEGERS[8], "HID:41243", sStartFormWizard, new Integer(101), new Integer(121),IFINALSTEP, new Short(curtabindex++), new Integer(107)}
+          new Object[] { UIConsts.INTEGERS[8], "HID:41243", sStartFormWizard, new Integer(101), new Integer(121+ndiffPosY),IFINALSTEP, new Short(curtabindex++), new Integer(157)}
         );
     }
 
     public void initialize(String _firsttablename){
-        toggleFinishButton();
         setTableName(_firsttablename);
     }
 
@@ -149,11 +228,29 @@ public class Finalizer implements XFocusListener{
             return STARTFORMWIZARDMODE;
     }
 
+
+    public String getComposedTableName(String _stablename){
+        String scatalogname = null;
+        String sschemaname = null;
+        if (xCatalogListBox != null)
+            scatalogname = xCatalogListBox.getSelectedItem();
+        if (xSchemaListBox != null)
+            sschemaname = xSchemaListBox.getSelectedItem();
+        return curtabledescriptor.getComposedTableName(scatalogname, sschemaname, _stablename);
+    }
+
+
     public void setTableName(String _tablename){
         if (txtTableName.getText().equals("")){
-            String tablename = Desktop.getUniqueName(curtabledescriptor.getTableNames(), _tablename);
-            txtTableName.setText(tablename);
+            String ssuffix = Desktop.getIncrementSuffix(curtabledescriptor.xTableNames, getComposedTableName(_tablename));
+            txtTableName.setText(_tablename + ssuffix);
+            setCompletionFlag();
         }
+    }
+
+
+    public String getTableName(){
+        return txtTableName.getText();
     }
 
     public String getTableName(String _firsttablename){
@@ -162,29 +259,35 @@ public class Finalizer implements XFocusListener{
         return txtTableName.getText();
     }
 
-    public void toggleFinishButton(){
-        CurUnoDialog.enableFinishButton(txtTableName.getText().length() > 0);
+
+    public String getSchemaName(){
+        if (xSchemaListBox != null)
+            return this.xSchemaListBox.getSelectedItem();
+        else
+            return "";
     }
 
 
-    public void focusGained(FocusEvent arg0) {
+    public String getCatalogName(){
+        if (xCatalogListBox != null)
+            return this.xCatalogListBox.getSelectedItem();
+        else
+            return "";
     }
 
 
-
-    public void focusLost(FocusEvent arg0) {
-        String tablename = txtTableName.getText();
-        int i = Desktop.checkforfirstSpecialCharacter(curtabledescriptor.xMSF, tablename, Configuration.getOfficeLocale(curtabledescriptor.xMSF));
-        if (i < tablename.length()){
-            //TODO better take the locale of the database
-            String sSpecialCharWarning = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 41);
-            sSpecialCharWarning = JavaTools.replaceSubString(sSpecialCharWarning, tablename, "%TABLENAME");
-            sSpecialCharWarning = JavaTools.replaceSubString(sSpecialCharWarning, tablename.substring(i, i+1), "%SPECIALCHAR");
-            CurUnoDialog.showMessageBox("WarningBox", VclWindowPeerAttribute.OK, sSpecialCharWarning);
-        }
+    public boolean iscompleted(){
+        return (txtTableName.getText().length() > 0);
     }
 
-    public void disposing(EventObject arg0) {
+
+    public void setCompletionFlag(){
+        CurUnoDialog.setcompleted(TableWizard.SOFINALPAGE, iscompleted());
     }
+
+    public void setFocusToTableNameControl(){
+        CurUnoDialog.setFocus("txtTableName");
+    }
+
 
 }
