3b4afcbe9fdbc3a21277c336d81b125182fd4740
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 16:12:19 +0000

    INTEGRATION: CWS fwkpostbeta02 (1.3.44); FILE MERGED 2005/02/14 16:45:10 sb 1.3.44.1: #i42746# Removed commented-out code that contained non-ASCII characters.

--- sal/qa/rtl_strings/rtl_old_testowstring.cxx
+++ sal/qa/rtl_strings/rtl_old_testowstring.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rtl_old_testowstring.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: pjunck $ $Date: 2004-11-02 10:31:31 $
+ *  last change: $Author: vg $ $Date: 2005-02-21 17:12:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -435,15 +435,6 @@ void oldtests::test_OString2OUStringAndViceVersa()
     TEST_ENSURE( s2.equals("Hallo jetzt komm ich"), "test_OString2OWStringAndViceVersa error 7" );
     s2 = OUStringToOString(u1, RTL_TEXTENCODING_ASCII_US);
     TEST_ENSURE( s2.equals("Hallo jetzt komm ich"), "test_OString2OWStringAndViceVersa error 8" );
-
-    /*
-    s1 = ", 1 2 3 ! \" § $ % & ( ) = ? ";
-    u1 = OStringToOUString(s1, RTL_TEXTENCODING_MS_1252);
-    TEST_ENSURE( u1.equals(OUString::createFromAscii(", 1 2 3 ! \" § $ % & ( ) = ? ")), "test_OString2OWStringAndViceVersa error 9" );
-
-    s2 = OUStringToOString(u1, RTL_TEXTENCODING_MS_1252);
-    TEST_ENSURE( s2.equals(", 1 2 3 ! \" § $ % & ( ) = ? "), "test_OString2OWStringAndViceVersa error 10" );
-    */
 
     printf("test_OString2OWStringAndViceVersa OK !!!\n");
 }
