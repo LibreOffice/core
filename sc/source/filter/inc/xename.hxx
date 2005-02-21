388e38b94ea3cbf4804bf651a364c1681928d1f0
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 12:42:33 +0000

    INTEGRATION: CWS dr32 (1.2.2); FILE MERGED 2005/02/02 14:14:20 dr 1.2.2.1: #b6219324# #i23079# #i27871# #i35812# #i37725# new address converter, import/export of view settings

--- sc/source/filter/inc/xename.hxx
+++ sc/source/filter/inc/xename.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xename.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-14 12:09:52 $
+ *  last change: $Author: vg $ $Date: 2005-02-21 13:42:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,6 +74,7 @@
 
 // ============================================================================
 
+class ScRangeList;
 class XclExpNameManagerImpl;
 
 /** Manager that stores all internal defined names (NAME records) of the document. */
