a8a9629282f0fe4ac6c5d1f5f14b8a5621b90a40
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 13:00:45 +0000

    INTEGRATION: CWS dbwizard3 (1.2.20); FILE MERGED 2005/01/07 15:17:06 bc 1.2.20.1: #i40082#new NumberFormatter class added

--- wizards/com/sun/star/wizards/table/FieldDescription.java
+++ wizards/com/sun/star/wizards/table/FieldDescription.java
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: FieldDescription.java,v $
 *
-*  $Revision: 1.2 $
+*  $Revision: 1.3 $
 *
-*  last change: $Author: pjunck $ $Date: 2004-10-27 13:37:50 $
+*  last change: $Author: vg $ $Date: 2005-02-21 14:00:45 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
@@ -57,7 +57,6 @@
 *  Contributor(s): _______________________________________
 *
 */
-
 
 package com.sun.star.wizards.table;
 
@@ -182,109 +181,4 @@ public class FieldDescription{
         }
         return null;
     }
-
-//  public void setFieldProperties(XNameAccess _xNameAccessFieldNode, String _fieldname){
-//  try {
-//      xPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, _xNameAccessFieldNode);
-//      Name = _fieldname;
-////        Integer Index = (Integer) xPropertySet.getPropertyValue("Index");
-//      if (propertyexists("Type"))
-//          aPropertyValues.addElement(Properties.createProperty("Type", (Integer) xPropertySet.getPropertyValue("Type")) );
-//
-//          Type = (Integer) xPropertySet.getPropertyValue("Type");
-//      if (propertyexists("Scale"))
-//          Scale = (Integer) xPropertySet.getPropertyValue("Scale");
-//      if (propertyexists("Precision"))
-//          Precision = (Integer) xPropertySet.getPropertyValue("Precision");
-//      if (propertyexists("DefaultValue"))
-//          DefaultValue = (Boolean) xPropertySet.getPropertyValue("DefaultValue");
-//      //Type =  new Integer(4); // TODO wo ist der Fehler?(Integer) xPropertySet.getPropertyValue("Type");
-//  } catch (Exception e) {
-//      e.printStackTrace(System.out);
-//  }}
-
-
-
 }
-
-
-
-
-//public class CGField extends DBField{
-//
-//  XMultiServiceFactory xMSF;
-//  XNameAccess xNameAccessFieldNode;
-//  XNameAccess xNameAccessPropertiesNode;
-//  XNameAccess xNameAccessTypeNode;
-//  XPropertySet xPropertySet;
-//  boolean bgetbyShortName;
-//
-//  private Object oconfigView;
-//
-//  public CGField(XMultiServiceFactory _xMSF){
-//      super(_xMSF);
-//  }
-//
-//
-//  public void initialize(XNameAccess _xNameAccessParentNode, String _fieldname, String _keyname, boolean _bgetbyShortName){
-//      bgetbyShortName = _bgetbyShortName;
-//      if (_bgetbyShortName)
-//          xNameAccessFieldNode = Configuration.getChildNodebyDisplayName(_xNameAccessParentNode, _keyname, "ShortName");
-//      else
-//          xNameAccessFieldNode = Configuration.getChildNodebyDisplayName(_xNameAccessParentNode,_keyname, "Name");
-//      setFieldProperties(_fieldname);
-//  }
-//
-//
-//  public void initialize(XNameAccess _xNameAccessParentNode, String _fieldname, int _index){
-//      xNameAccessFieldNode = Configuration.getChildNodebyIndex( _xNameAccessParentNode, _index);
-//      setFieldProperties(_fieldname);
-//  }
-//
-//  private boolean propertyexists(String _propertyname){
-//      boolean bexists = false;
-//      try {
-//          if (xPropertySet.getPropertySetInfo().hasPropertyByName(_propertyname)){
-//              Object oValue = xPropertySet.getPropertyValue(_propertyname);
-//              bexists = (!com.sun.star.uno.AnyConverter.isVoid(oValue));
-//          }
-//      } catch (Exception e) {
-//          e.printStackTrace(System.out);
-//      }
-//      return bexists;
-//  }
-//
-//  private void setFieldProperties(String _fieldname){
-//  try {
-//      xPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, xNameAccessFieldNode);
-//      Name = _fieldname;
-//      Index = (Integer) xPropertySet.getPropertyValue("Index");
-//      if (propertyexists("Type"))
-//          Type = (Integer) xPropertySet.getPropertyValue("Type");
-//      if (propertyexists("Scale"))
-//          Scale = (Integer) xPropertySet.getPropertyValue("Scale");
-//      if (propertyexists("Precision"))
-//          Precision = (Integer) xPropertySet.getPropertyValue("Precision");
-//      if (propertyexists("DefaultValue"))
-//          DefaultValue = (Boolean) xPropertySet.getPropertyValue("DefaultValue");
-//      //Type =  new Integer(4); // TODO wo ist der Fehler?(Integer) xPropertySet.getPropertyValue("Type");
-//  } catch (Exception e) {
-//      e.printStackTrace(System.out);
-//  }}
-//
-//
-//  public void getProperties(){
-//  try {
-//      String[] spropnames = Configuration.getNodeDisplayNames(xNameAccessPropertiesNode);
-//      for (int i = 0; i < spropnames.length; i++){
-//          XPropertySet xPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, xNameAccessPropertiesNode);
-//          String sname = (String) xPropertySet.getPropertyValue("Name");
-//          String svalue = (String) xPropertySet.getPropertyValue("Value");
-//      }
-//      int i = 0;
-//  } catch (Exception e) {
-//      e.printStackTrace(System.out);
-////        return null;
-//  }}
-//
-//}
