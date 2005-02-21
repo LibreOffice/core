3a1d9ae1d914dbad59f8280243afce8f7bb45d62
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 16:09:33 +0000

    INTEGRATION: CWS fwkpostbeta02 (1.37.30); FILE MERGED 2005/02/11 12:01:22 mba 1.37.30.1: #i42433#: version im/export moved to XMLOFF

--- sfx2/util/makefile.mk
+++ sfx2/util/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.37 $
+#   $Revision: 1.38 $
 #
-#   last change: $Author: kz $ $Date: 2005-01-18 16:21:07 $
+#   last change: $Author: vg $ $Date: 2005-02-21 17:09:33 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -108,7 +108,6 @@ SHL1USE_EXPORTS=ordinal
 SHL1STDLIBS+=\
         $(FWELIB) \
         $(BASICLIB) \
-        $(XMLOFFLIB) \
         $(XMLSCRIPTLIB) \
         $(SVTOOLLIB) \
         $(TKLIB) \
