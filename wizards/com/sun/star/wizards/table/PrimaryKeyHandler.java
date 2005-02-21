fc59aba6385b8c59a3d23a16c429c272148838e2
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 13:01:29 +0000

    INTEGRATION: CWS dbwizard3 (1.2.20); FILE MERGED 2005/01/27 15:20:23 bc 1.2.20.6: #i37837# TypeInspector modified 2005/01/11 21:38:49 bc 1.2.20.5: #i37839#new class ColumnPropertySet introduced 2005/01/10 20:34:21 bc 1.2.20.4: #i38555# Automatic Autovalue checkbox now also toggled 2005/01/07 15:17:07 bc 1.2.20.3: #i40082#new NumberFormatter class added 2004/12/08 11:27:15 bc 1.2.20.2: #i38555# PrimaryKey page adapted to the task 2004/12/06 14:55:48 bc 1.2.20.1: ##several bugfixes

--- wizards/com/sun/star/wizards/table/PrimaryKeyHandler.java
+++ wizards/com/sun/star/wizards/table/PrimaryKeyHandler.java
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: PrimaryKeyHandler.java,v $
 *
-*  $Revision: 1.2 $
+*  $Revision: 1.3 $
 *
-*  last change: $Author: pjunck $ $Date: 2004-10-27 13:38:36 $
+*  last change: $Author: vg $ $Date: 2005-02-21 14:01:29 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
@@ -58,7 +58,6 @@
 *
 */
 
-
 package com.sun.star.wizards.table;
 
 import com.sun.star.awt.XCheckBox;
@@ -66,9 +65,17 @@ import com.sun.star.awt.XFixedText;
 import com.sun.star.awt.XListBox;
 import com.sun.star.awt.XRadioButton;
 import com.sun.star.awt.XTextComponent;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.sdbc.DataType;
+import com.sun.star.uno.AnyConverter;
 import com.sun.star.wizards.common.Helper;
 import com.sun.star.wizards.common.JavaTools;
+import com.sun.star.wizards.db.DBMetaData;
 import com.sun.star.wizards.db.TableDescriptor;
+import com.sun.star.wizards.db.TypeInspector;
 import com.sun.star.wizards.ui.FieldSelection;
 import com.sun.star.wizards.ui.UIConsts;
 import com.sun.star.wizards.ui.UnoDialog;
@@ -78,7 +85,7 @@ import com.sun.star.wizards.ui.XFieldSelectionListener;
 
 public class PrimaryKeyHandler implements XFieldSelectionListener {
 
-    WizardDialog CurUnoDialog;
+    TableWizard CurUnoDialog;
     short curtabindex;
     String SPRIMEKEYMODE = "togglePrimeKeyFields";
     String SSINGLEKEYMODE = "toggleSinglePrimeKeyFields";
@@ -88,14 +95,21 @@ public class PrimaryKeyHandler implements XFieldSelectionListener {
     XRadioButton optUseSeveral;
     XCheckBox chkUsePrimaryKey;
     XCheckBox chkcreatePrimaryKey;
-    XCheckBox chkApplyAutoValue;
+    XCheckBox chkApplyAutoValueExisting;
+    XCheckBox chkApplyAutoValueAutomatic;
+
     XListBox lstSinglePrimeKey;
     XFixedText lblPrimeFieldName;
     FieldSelection curPrimaryKeySelection;
     String[] fieldnames;
+    TableDescriptor curTableDescriptor;
+    int nAutoPrimeKeyDataType;
+    boolean bAutoPrimaryKeysupportsAutoIncrmentation;
 
-    public PrimaryKeyHandler(WizardDialog _CurUnoDialog){
+    public PrimaryKeyHandler(TableWizard _CurUnoDialog, TableDescriptor _curTableDescriptor){
         this.CurUnoDialog = _CurUnoDialog;
+        curTableDescriptor = _curTableDescriptor;
+        bAutoPrimaryKeysupportsAutoIncrmentation = isAutoPrimeKeyAutoIncrementationsupported();
         curtabindex = (short) ((TableWizard.SOPRIMARYKEYPAGE * 100) - 20);
         Integer IPRIMEKEYSTEP = new Integer(TableWizard.SOPRIMARYKEYPAGE);
         String sExplanations  = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 26);
@@ -109,49 +123,53 @@ public class PrimaryKeyHandler implements XFieldSelectionListener {
         String slblSelPrimaryFields = CurUnoDialog.oResource.getResText(UIConsts.RID_TABLE + 32);
         CurUnoDialog.insertLabel("lblExplanation",
               new String[] {"Height", "Label", "MultiLine", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-              new Object[] { new Integer(40), sExplanations,  Boolean.TRUE, new Integer(97), new Integer(18),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(207)}
+              new Object[] { new Integer(40), sExplanations,  Boolean.TRUE, new Integer(91), new Integer(27),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(213)}
         );
 
         chkcreatePrimaryKey = CurUnoDialog.insertCheckBox("chkcreatePrimaryKey", SPRIMEKEYMODE, this,
             new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "State", "Step", "TabIndex", "Width"},
-            new Object[] {UIConsts.INTEGERS[8], "HID:41229", screatePrimaryKey, new Integer(97), new Integer(58), new Short((short)1), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(160)});
+            new Object[] {UIConsts.INTEGERS[8], "HID:41229", screatePrimaryKey, new Integer(97), new Integer(70), new Short((short)1), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(160)});
 
         optAddAutomatically = CurUnoDialog.insertRadioButton("optAddAutomatically", SPRIMEKEYMODE, this,
               new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "State", "Step", "TabIndex", "Width"},
-              new Object[] { UIConsts.INTEGERS[8], "HID:41231", sAddAutomatically, new Integer(103), new Integer(70), new Short((short)1),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
+              new Object[] { UIConsts.INTEGERS[8], "HID:41231", sAddAutomatically, new Integer(106), new Integer(82), new Short((short)1),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
         );
 
         optUseExisting = CurUnoDialog.insertRadioButton("optUseExisting", SPRIMEKEYMODE, this,
-              new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-              new Object[] { UIConsts.INTEGERS[8], "HID:41230", sUseExisting, new Integer(103), new Integer(82),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
+              new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"}, //94
+              new Object[] { UIConsts.INTEGERS[8], "HID:41230", sUseExisting, new Integer(106), new Integer(104),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
         );
 
         optUseSeveral = CurUnoDialog.insertRadioButton("optUseSeveral", SPRIMEKEYMODE, this,
               new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-              new Object[] { UIConsts.INTEGERS[8], "HID:41234", sUseSeveral, new Integer(103), new Integer(110),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
+              new Object[] { UIConsts.INTEGERS[8], "HID:41234", sUseSeveral, new Integer(106), new Integer(132),IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(200)}
         );
 
-        lblPrimeFieldName = CurUnoDialog.insertLabel("lbPrimeFieldName",
+        chkApplyAutoValueAutomatic = CurUnoDialog.insertCheckBox("chkApplyAutoValueAutomatic", SPRIMEKEYMODE, this,
+                new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"}, //107
+                new Object[] {UIConsts.INTEGERS[8], "HID:41233", sApplyAutoValue, new Integer(116), new Integer(92), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(68)});
+
+        lblPrimeFieldName = CurUnoDialog.insertLabel("lblPrimeFieldName",
             new String[] {"Enabled", "Height", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-            new Object[] { Boolean.FALSE, UIConsts.INTEGERS[8], slblPrimeFieldName, new Integer(110), new Integer(95), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(49)}
+            new Object[] { Boolean.FALSE, UIConsts.INTEGERS[8], slblPrimeFieldName, new Integer(116), new Integer(117), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(46)}
         );
 
         lstSinglePrimeKey = CurUnoDialog.insertListBox("lstSinglePrimeKey", "onPrimeKeySelected", null, this,
             new String[] {"Dropdown", "Enabled", "Height", "HelpURL", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-            new Object[] {Boolean.TRUE, Boolean.FALSE, new Integer(12), "HID:41232", new Integer(160), new Integer(93), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(70)});
+            new Object[] {Boolean.TRUE, Boolean.FALSE, new Integer(12), "HID:41232", new Integer(162), new Integer(115), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(70)});
 
-        chkApplyAutoValue = CurUnoDialog.insertCheckBox("chkApplyAutoValue", SPRIMEKEYMODE, this,
-            new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"},
-            new Object[] {UIConsts.INTEGERS[8], "HID:41233", sApplyAutoValue, new Integer(232), new Integer(95), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(68)});
-        curPrimaryKeySelection = new FieldSelection(CurUnoDialog, IPRIMEKEYSTEP.intValue(), 114, 122, 190, 56, slblAvailableFields, slblSelPrimaryFields, 41235, false);
+        chkApplyAutoValueExisting = CurUnoDialog.insertCheckBox("chkApplyAutoValueExisting", SPRIMEKEYMODE, this,
+                new String[] {"Height", "HelpURL", "Label", "PositionX", "PositionY", "Step", "TabIndex", "Width"}, //107
+                new Object[] {UIConsts.INTEGERS[8], "HID:41233", sApplyAutoValue, new Integer(238), new Integer(117), IPRIMEKEYSTEP, new Short(curtabindex++), new Integer(66)});
+        curPrimaryKeySelection = new FieldSelection(CurUnoDialog, IPRIMEKEYSTEP.intValue(), 116, 142, 188, 39, slblAvailableFields, slblSelPrimaryFields, 41235, false);
         curPrimaryKeySelection.addFieldSelectionListener(this);
+
     }
 
 
-    public void initialize(TableDescriptor curTableDescriptor){
+    public void initialize(){
         boolean breselect;
-        fieldnames = curTableDescriptor.getFieldnames();
-//      fieldnames = setNumericFields();
+        fieldnames = curTableDescriptor.getNonBinaryFieldNames();
         String[] skeyfieldnames = curPrimaryKeySelection.getSelectedFieldNames();
         curPrimaryKeySelection.initialize(fieldnames, false);
         if (skeyfieldnames != null){
@@ -166,59 +184,133 @@ public class PrimaryKeyHandler implements XFieldSelectionListener {
             if (JavaTools.FieldInList(fieldnames, selfield) > -1)
                 lstSinglePrimeKey.selectItem(selfield, true);
         }
-
         togglePrimeKeyFields();
     }
 
 
+    private boolean isAutoPrimeKeyAutoIncrementationsupported(){
+        TypeInspector.TypeInfo aAutoPrimeTypeInfo;
+        aAutoPrimeTypeInfo = curTableDescriptor.oTypeInspector.findAutomaticPrimaryKeyType();
+        return aAutoPrimeTypeInfo.bisAutoIncrementable;
+    }
+
+    public boolean iscompleted(){
+        if (chkcreatePrimaryKey.getState() == 0)
+            return true;
+        if (this.optAddAutomatically.getState())
+            return true;
+        if (optUseExisting.getState()){
+            fieldnames = curTableDescriptor.getNonBinaryFieldNames();
+            String selfield = lstSinglePrimeKey.getSelectedItem();
+            if ( selfield != null){
+                return (JavaTools.FieldInList(fieldnames, selfield) > -1);
+            }
+        }
+        if (optUseSeveral.getState()){
+            fieldnames = curTableDescriptor.getNonBinaryFieldNames();
+            String[] skeyfieldnames = curPrimaryKeySelection.getSelectedFieldNames();
+            String[] snewkeyfieldnames = JavaTools.removeOutdatedFields(skeyfieldnames, fieldnames);
+            return (snewkeyfieldnames.length > 0);
+        }
+        return false;
+    }
+
     public void togglePrimeKeyFields(){
         boolean bdoEnable = (this.chkcreatePrimaryKey.getState() == 1);
+
         Helper.setUnoPropertyValue(UnoDialog.getModel(optAddAutomatically), "Enabled", new Boolean(bdoEnable));
+        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValueAutomatic), "Enabled", new Boolean(bAutoPrimaryKeysupportsAutoIncrmentation && bdoEnable));
         Helper.setUnoPropertyValue(UnoDialog.getModel(optUseExisting), "Enabled", new Boolean(bdoEnable));
         Helper.setUnoPropertyValue(UnoDialog.getModel(optUseSeveral), "Enabled", new Boolean(bdoEnable));
+        //toggle subcontrols of the radiobuttons...
+        toggleAutomaticAutoValueCheckBox();
         boolean benableSinglePrimekeyControls = bdoEnable && optUseExisting.getState();
         toggleSinglePrimeKeyFields(benableSinglePrimekeyControls);
         boolean benableSeveralPrimekeyControls = bdoEnable && optUseSeveral.getState();
         curPrimaryKeySelection.toggleListboxControls(new Boolean(benableSeveralPrimekeyControls));
+        // toggle the following steps of the dialog...
         if (!bdoEnable)
-            CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, true);
+            CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, true);
         else{
             if (benableSeveralPrimekeyControls)
-                CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
+                CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
             else if (benableSinglePrimekeyControls)
-                CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, lstSinglePrimeKey.getSelectedItemPos() != -1);
+                CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, lstSinglePrimeKey.getSelectedItemPos() != -1);
             else if (optAddAutomatically.getState())
-                CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, true);
+                CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, true);
         }
     }
 
 
+    private boolean isAutoIncrementatable(String _fieldname){
+    boolean bisAutoIncrementable = false;
+    try {
+        XPropertySet xColPropertySet = curTableDescriptor.getColumnbyName(_fieldname);
+        if (xColPropertySet != null){
+            int itype = AnyConverter.toInt(xColPropertySet.getPropertyValue("Type"));
+            if (curTableDescriptor.getDBDataTypeInspector() != null)
+                return curTableDescriptor.getDBDataTypeInspector().isAutoIncrementable(xColPropertySet);
+        }
+    } catch (Exception e) {
+        e.printStackTrace(System.out);
+    }
+    return false;
+    }
+
+
+    public boolean isAutoIncremented(){
+    boolean bischecked = false;
+    try {
+        boolean bisAutomaticMode = AnyConverter.toBoolean(Helper.getUnoPropertyValue(UnoDialog.getModel(optAddAutomatically), "Enabled"));
+        boolean bisExistingMode = AnyConverter.toBoolean(Helper.getUnoPropertyValue(UnoDialog.getModel(optUseExisting), "Enabled"));
+        if (bisAutomaticMode)
+            bischecked = chkApplyAutoValueAutomatic.getState() == (short)1;
+        else if (bisExistingMode)
+            bischecked = chkApplyAutoValueExisting.getState() == (short)1;
+    } catch (IllegalArgumentException e) {
+        e.printStackTrace(System.out);
+    }
+    return bischecked;
+    }
+
+
     public void onPrimeKeySelected(){
-        CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, lstSinglePrimeKey.getSelectedItemPos() != -1);
+        boolean bdoenable = isAutoIncrementatable(lstSinglePrimeKey.getSelectedItem());
+        CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, lstSinglePrimeKey.getSelectedItemPos() != -1);
+        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValueExisting), "Enabled", new Boolean(bdoenable));
     }
 
 
-    public void toggleSinglePrimeKeyFields(boolean _bdoEnable){
-        Helper.setUnoPropertyValue(UnoDialog.getModel(lblPrimeFieldName), "Enabled", new Boolean(_bdoEnable));
-        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValue), "Enabled", new Boolean(_bdoEnable));
-        Helper.setUnoPropertyValue(UnoDialog.getModel(lstSinglePrimeKey), "Enabled", new Boolean(_bdoEnable));
+    private void toggleAutomaticAutoValueCheckBox(){
+    try {
+        boolean bisAutomaticMode = AnyConverter.toBoolean(Helper.getUnoPropertyValue(UnoDialog.getModel(optAddAutomatically), "Enabled"));
+        boolean bdoenable = bAutoPrimaryKeysupportsAutoIncrmentation && optAddAutomatically.getState() && bisAutomaticMode;
+        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValueAutomatic), "Enabled", new Boolean(bdoenable));
+    } catch (IllegalArgumentException e) {
+        e.printStackTrace(System.out);
+    }}
+
+    private void toggleSinglePrimeKeyFields(boolean _bdoenable){
+        Helper.setUnoPropertyValue(UnoDialog.getModel(lblPrimeFieldName), "Enabled", new Boolean(_bdoenable));
+        Helper.setUnoPropertyValue(UnoDialog.getModel(lstSinglePrimeKey), "Enabled", new Boolean(_bdoenable));
+        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValueExisting), "Enabled", new Boolean(_bdoenable));
+        boolean bdoenableAutoValueCheckBox = (isAutoIncrementatable(lstSinglePrimeKey.getSelectedItem()) && _bdoenable);
+        Helper.setUnoPropertyValue(UnoDialog.getModel(chkApplyAutoValueExisting), "Enabled", new Boolean(bdoenableAutoValueCheckBox));
     }
 
 
-    public void toggleSeveralPrimeKeyFields(){
+    private void toggleSeveralPrimeKeyFields(){
         boolean bdoEnable = (this.optUseSeveral.getState());
         curPrimaryKeySelection.toggleListboxControls(new Boolean(bdoEnable));
     }
 
 
-    public boolean IsAutoIncrement(){
-        return false;
-    }
-
-
     public String[] getPrimaryKeyFields(TableDescriptor _curtabledescriptor){
+        if (chkcreatePrimaryKey.getState() == 0){
+            return null;
+        }
         if (fieldnames == null)
-            initialize(_curtabledescriptor);
+            initialize();
         if (optUseSeveral.getState())
             return curPrimaryKeySelection.getSelectedFieldNames();
         else if (optUseExisting.getState())
@@ -247,11 +339,10 @@ public class PrimaryKeyHandler implements XFieldSelectionListener {
     }
 
     public void shiftFromLeftToRight(String[] SelItems, String[] NewItems) {
-        CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
+        CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
     }
 
     public void shiftFromRightToLeft(String[] OldSelItems, String[] NewItems) {
-        CurUnoDialog.enablefromStep(TableWizard.SOFINALPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
+        CurUnoDialog.setcompleted(TableWizard.SOPRIMARYKEYPAGE, (curPrimaryKeySelection.getSelectedFieldNames().length > 0));
     }
-
 }
