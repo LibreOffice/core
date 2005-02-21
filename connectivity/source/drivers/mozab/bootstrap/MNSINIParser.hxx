5e1c5ca90d39e6c4e38ac9161808e4bb0a90e308
Vladimir Glazounov <vg@openoffice.org>
2005-02-21 11:26:27 +0000

    INTEGRATION: CWS mozab05 (1.1.2); FILE ADDED 2005/01/27 11:30:10 windly 1.1.2.3: #i41444# mozab crash on solaris sparc 2005/01/24 08:04:57 windly 1.1.2.2: #i20088# fix build bugs on solaris sparc 2005/01/17 05:30:46 windly 1.1.2.1: #i20088# Detect Mozilla Thunderbird Address Book: patchs for connectivity

---
+++ connectivity/source/drivers/mozab/bootstrap/MNSINIParser.hxx
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *
+ *  $RCSfile: MNSINIParser.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2005-02-21 12:26:27 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef __MNSINIPARSER_HXX__
+#define __MNSINIPARSER_HXX__
+
+#include <rtl/ustring>
+#include <com/sun/star/io/IOException.hpp>
+#ifndef _OSL_PROCESS_H_
+#include <osl/process.h>
+#endif
+using namespace rtl;
+
+#include <map>
+#include <list>
+
+#if OSL_DEBUG_LEVEL > 0
+#include <stdio.h>
+#endif
+
+struct ini_NameValue
+{
+    rtl::OUString sName;
+    rtl::OUString sValue;
+
+    inline ini_NameValue() SAL_THROW( () )
+        {}
+    inline ini_NameValue(
+        OUString const & name, OUString const & value ) SAL_THROW( () )
+        : sName( name ),
+          sValue( value )
+        {}
+};
+
+typedef std::list<
+    ini_NameValue
+> NameValueList;
+
+struct ini_Section
+{
+    rtl::OUString sName;
+    NameValueList lList;
+};
+typedef std::map<rtl::OUString,
+                ini_Section
+                >IniSectionMap;
+
+
+class IniParser
+{
+    IniSectionMap mAllSection;
+public:
+    IniSectionMap * getAllSection(){return &mAllSection;};
+    ini_Section *  getSection(OUString const & secName);
+    IniParser(OUString const & rIniName) throw(com::sun::star::io::IOException );
+#if OSL_DEBUG_LEVEL > 0
+    void Dump();
+#endif
+
+};
+
+#endif
+
