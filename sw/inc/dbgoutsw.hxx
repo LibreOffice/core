f2b454948874866c1d2e4ff6138a6920307f01a0
Vladimir Glazounov <vg@openoffice.org>
2005-02-22 09:02:14 +0000

    INTEGRATION: CWS swqcore06 (1.9.60); FILE MERGED 2005/02/10 13:07:20 hbrinkm 1.9.60.2: RESYNC: (1.9-1.10); FILE MERGED 2005/02/10 11:58:01 hbrinkm 1.9.60.1: #1000000# resync

--- sw/inc/dbgoutsw.hxx
+++ sw/inc/dbgoutsw.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbgoutsw.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2005-02-21 16:01:31 $
+ *  last change: $Author: vg $ $Date: 2005-02-22 10:02:14 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -79,6 +79,7 @@ class SwUndo;
 class SwUndos;
 class SwRect;
 class SwFrmFmt;
+class SwFrmFmts;
 class SwNodes;
 class SwRewriter;
 class SwNumRuleTbl;
