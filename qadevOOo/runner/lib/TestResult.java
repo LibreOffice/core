d77a8e5c141f96110f70b3744c7db10f2a809c49
Vladimir Glazounov <vg@openoffice.org>
2005-02-24 16:22:22 +0000

    INTEGRATION: CWS qadev21 (1.2.14); FILE MERGED 2005/02/18 12:56:42 sw 1.2.14.1: i42888

--- qadevOOo/runner/lib/TestResult.java
+++ qadevOOo/runner/lib/TestResult.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TestResult.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: pjunck $ $Date: 2004-11-02 11:43:46 $
+ *  last change: $Author: vg $ $Date: 2005-02-24 17:22:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,6 +89,7 @@ public class TestResult {
      * @see #tested(String, Status)
      */
     public boolean tested( String method, boolean result) {
+        System.out.println("Method "+method+" finished with state "+(result?"OK":"FAILED"));
         return tested( method, Status.passed( result ) );
     }
 
