7000813fd2accf69f6198ff1ebf1683e1eccb8df
Vladimir Glazounov <vg@openoffice.org>
2005-02-24 16:27:42 +0000

    INTEGRATION: CWS qadev21 (1.2.8); FILE MERGED 2005/01/20 09:17:21 sw 1.2.8.1: #i38247#

--- qadevOOo/tests/java/ifc/form/submission/_XSubmission.java
+++ qadevOOo/tests/java/ifc/form/submission/_XSubmission.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _XSubmission.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2004-11-16 12:50:10 $
+ *  last change:$Date: 2005-02-24 17:27:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,94 +62,167 @@ package ifc.form.submission;
 
 import com.sun.star.form.submission.XSubmission;
 import com.sun.star.form.submission.XSubmissionVetoListener;
+import com.sun.star.task.XInteractionHandler;
 import com.sun.star.uno.UnoRuntime;
 
 import lib.MultiMethodTest;
 
 
-public class _XSubmission extends MultiMethodTest
-{
+public class _XSubmission extends MultiMethodTest {
     public XSubmission oObj = null;
+    public boolean HandlerWasCalled = false;
+
+    public void _addSubmissionVetoListener() {
+        log.println(
+            "submitting with VetoListener ... exception should appear");
+
+        boolean res = true;
+        XSubmissionVetoListener aListener = new MyListener();
+
+        try {
+            oObj.addSubmissionVetoListener(aListener);
+            oObj.submit();
+            res = false;
+            log.println(
+                "the expected exception wasn't thrown ... FAILED");
+        } catch (com.sun.star.lang.WrappedTargetException e) {
+            log.println(
+                "Expected exception was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        } catch (com.sun.star.lang.NoSupportException e) {
+            log.println(
+                "NoSupportExpected exception was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        } catch (com.sun.star.util.VetoException e) {
+            log.println(
+                "VetoException was thrown while calling submit() "
+                + e.getMessage() + "OK");
+        }
+
+        try {
+            oObj.removeSubmissionVetoListener(aListener);
+        } catch (com.sun.star.lang.NoSupportException e) {
+            log.println(
+                "NoSupportExpected exception was thrown while removing the listener) "
+                + e.getMessage() + "FAILED");
+            res = false;
+        }
+
+        tRes.tested("addSubmissionVetoListener()", res);
+    }
+
+    public void _removeSubmissionVetoListener() {
+        log.println(
+            "submitting with VetoListener ... exception should appear");
+
+        boolean res = true;
+        XSubmissionVetoListener aListener = new MyListener();
+
+        try {
+            oObj.addSubmissionVetoListener(aListener);
+            oObj.submit();
+            res = false;
+            log.println(
+                "the expected exception wasn't thrown ... FAILED");
+        } catch (com.sun.star.lang.WrappedTargetException e) {
+            log.println(
+                "WrappedTargetException exception was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        } catch (com.sun.star.lang.NoSupportException e) {
+            log.println(
+                "NoSupportExpected exception was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        } catch (com.sun.star.util.VetoException e) {
+            log.println(
+                "VetoException was thrown while calling submit() "
+                + e.getMessage() + "OK");
+        }
+
+        log.println("removing the listener");
+
+        try {
+            oObj.removeSubmissionVetoListener(aListener);
+        } catch (com.sun.star.lang.NoSupportException e) {
+            log.println(
+                "NoSupportExpected exception was thrown while removing the listener) "
+                + e.getMessage() + "FAILED");
+            res = false;
+        }
+
+        log.println("Sleeping 2s");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // sleeping didn't work
+        }
+
+        log.println("... done");
+
+        log.println(
+            "submitting after VetoListener has been removed... no exception should appear");
+
+        try {
+            oObj.submit();
+            log.println("No Exception ... OK");
+        } catch (com.sun.star.lang.WrappedTargetException e) {
+            log.println(
+                "WrappedTargetException was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        } catch (com.sun.star.util.VetoException e) {
+            log.println(
+                "VetoException was thrown while calling submit() "
+                + e.getMessage() + "FAILED");
+            res = false;
+        }
+
+        tRes.tested("removeSubmissionVetoListener()", res);
+    }
 
     /** Calls submit and returns true if no exception was thrown
      *  then adds a SubmissionVetoListener and checks if the
      *  exception is thrown in case of a veto of this listener.
      */
-    public void _submit()
-    {
+    public void _submit() {
         boolean res = true;
-        log.println("submitting without VetoListener ... no exception should appear");
-        try
-        {
+        log.println(
+            "submitting without VetoListener ... no exception should appear");
+
+        try {
             oObj.submit();
             log.println("No Exception ... OK");
-        }
-        catch (com.sun.star.lang.WrappedTargetException e)
-        {
+        } catch (com.sun.star.lang.WrappedTargetException e) {
             log.println(
                 "Exception was thrown while calling submit() "
-                + e.getMessage() + "FAILED"
-            );
+                + e.getMessage() + "FAILED");
             res = false;
-        }
-        catch (com.sun.star.util.VetoException e)
-        {
+        } catch (com.sun.star.util.VetoException e) {
             log.println(
                 "VetoException was thrown while calling submit() "
-                + e.getMessage() + "FAILED"
-            );
+                + e.getMessage() + "FAILED");
             res = false;
-        }
-
-        log.println("submitting with VetoListener ... exception should appear");
-
-        try
-        {
-            oObj.addSubmissionVetoListener(new MyListener());
-            oObj.submit();
-            res = false;
-            log.println("the expected exception wasn't thrown ... FAILED");
-        }
-        catch (com.sun.star.lang.WrappedTargetException e)
-        {
-            log.println(
-                "Expected exception was thrown while calling submit() "
-                + e.getMessage() + "FAILED"
-            );
-            res = false;
-        }
-        catch (com.sun.star.lang.NoSupportException e)
-        {
-            log.println(
-                "NoSupportExpected exception was thrown while calling submit() "
-                + e.getMessage() + "FAILED"
-            );
-            res = false;
-        }
-        catch (com.sun.star.util.VetoException e)
-        {
-            log.println(
-                "VetoException was thrown while calling submit() "
-                + e.getMessage() + "OK"
-            );
         }
 
         tRes.tested("submit()", res);
     }
 
-    public class MyListener implements XSubmissionVetoListener
-    {
-        public void disposing(com.sun.star.lang.EventObject eventObject)
-        {
+    public class MyListener implements XSubmissionVetoListener {
+        public void disposing(
+            com.sun.star.lang.EventObject eventObject) {
         }
 
-        public void submitting(com.sun.star.lang.EventObject eventObject)
-                throws com.sun.star.util.VetoException
-        {
+        public void submitting(
+            com.sun.star.lang.EventObject eventObject)
+                throws com.sun.star.util.VetoException {
             log.println("MyVetoListener was called");
             throw new com.sun.star.util.VetoException(
-                "don't dare to submit :-)"
-            );
+                "submission isn't allowed ...");
         }
     }
+
 }
