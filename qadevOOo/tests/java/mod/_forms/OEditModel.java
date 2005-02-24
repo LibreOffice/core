c4ad7b4bf0e796097804f5b2c0ee9696d49f67e7
Vladimir Glazounov <vg@openoffice.org>
2005-02-24 16:43:02 +0000

    INTEGRATION: CWS qadev21 (1.3.106); FILE MERGED 2005/01/19 17:33:58 cn 1.3.106.1: #i40688# redesign of test: uses GenericModelTest

--- qadevOOo/tests/java/mod/_forms/OEditModel.java
+++ qadevOOo/tests/java/mod/_forms/OEditModel.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: OEditModel.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change:$Date: 2003-09-08 11:47:42 $
+ *  last change:$Date: 2005-02-24 17:43:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,27 +60,13 @@
  ************************************************************************/
 package mod._forms;
 
+import com.sun.star.beans.NamedValue;
 import java.io.PrintWriter;
 
-import lib.StatusException;
-import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
 import util.DBTools;
-import util.FormTools;
-import util.WriterTools;
 
-import com.sun.star.beans.XPropertySet;
-import com.sun.star.drawing.XControlShape;
-import com.sun.star.drawing.XShape;
-import com.sun.star.form.XBoundComponent;
-import com.sun.star.form.XLoadable;
-import com.sun.star.lang.XMultiServiceFactory;
-import com.sun.star.sdbc.XResultSetUpdate;
-import com.sun.star.text.XTextDocument;
-import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
-import com.sun.star.util.XCloseable;
 
 
 /**
@@ -155,171 +141,60 @@ import com.sun.star.util.XCloseable;
 * @see ifc.form._XLoadListener
 * @see ifc.container._XChild
 */
-public class OEditModel extends TestCase {
-    XTextDocument xTextDoc;
+public class OEditModel extends GenericModelTest {
 
     /**
-    * Creates Writer document where controls are placed.
-    */
+     * Set some member variable of the super class <CODE>GenericModelTest</CODE>:
+     * <pre>
+     *    <super.m_ChangePropertyName</CODE> = "Text";
+     *    super.m_kindOfControl</CODE>="TextField";
+     *    super.m_ObjectName</CODE> = "Edit";
+     *    NamedValue myProp = new NamedValue();
+     *        myProp.Name = "DataField";
+     *        myProp.Value = DBTools.TST_STRING_F;
+     *        super.m_propertiesToSet.add(myProp);
+     *        super.m_LCShape_Type = "FixedText";
+     * </pre>
+     * Then <CODE>super.initialize()</CODE> was called.
+     * @param tParam the test parameter
+     * @param log the log writer
+     */
+
     protected void initialize(TestParameters tParam, PrintWriter log) {
-        log.println("creating a textdocument");
-        xTextDoc = WriterTools.createTextDoc(((XMultiServiceFactory) tParam.getMSF()));
+        super.m_ChangePropertyName = "Text";
+
+        super.m_kindOfControl="TextField";
+
+        super.m_ObjectName = "stardiv.one.form.component.Edit";
+
+        NamedValue myProp = new NamedValue();
+        myProp.Name = "DataField";
+        myProp.Value = DBTools.TST_STRING_F;
+        super.m_propertiesToSet.add(myProp);
+
+        super.m_LCShape_Type = "FixedText";
+
+        super.initialize(tParam, log);
     }
 
     /**
-    * Disposes Writer document.
-    */
+     * calls <CODE>cleanup()</CODE> from it's super class
+     * @param tParam the test parameter
+     * @param log the log writer
+     */
     protected void cleanup(TestParameters tParam, PrintWriter log) {
-        log.println("    disposing xTextDoc ");
-
-        try {
-            XCloseable closer = (XCloseable) UnoRuntime.queryInterface(
-                                        XCloseable.class, xTextDoc);
-            closer.close(true);
-        } catch (com.sun.star.util.CloseVetoException e) {
-            log.println("couldn't close document");
-        } catch (com.sun.star.lang.DisposedException e) {
-            log.println("couldn't close document");
-        }
+        super.cleanup(tParam, log);
     }
 
+
     /**
-    * Creating a Testenvironment for the interfaces to be tested.
-    * First <code>TestDB</code> database is registered.
-    * Creates TextField in the Form, then binds it to TestDB
-    * database and returns Field's control. <p>
-    *     Object relations created :
-    * <ul>
-    *  <li> <code>'OBJNAME'</code> for
-    *      {@link ifc.io._XPersistObject} : name of service which is
-    *    represented by this object. </li>
-    *  <li> <code>'LC'</code> for {@link ifc.form._DataAwareControlModel}.
-    *    Specifies the value for LabelControl property. It is
-    *    <code>FixedText</code> component added to the document.</li>
-    *  <li> <code>'FL'</code> for
-    *      {@link ifc.form._DataAwareControlModel} interface.
-    *    Specifies XLoadable implementation which connects form to
-    *    the data source.</li>
-    *  <li> <code>'XUpdateBroadcaster.Checker'</code> : <code>
-    *    _XUpdateBroadcaster.UpdateChecker</code> interface implementation
-    *    which can update, commit data and check if the data was successfully
-    *    commited.</li>
-    *  <li> <code>'DataAwareControlModel.NewFieldName'</code> : for
-    *    <code>com.sun.star.form.DataAwareControlModel</code> service
-    *    which contains new name of the field ('_TEXT') to bind control to.
-    *  </li>
-    * </ul>
-    * @see ifc.form._XUpdateBroadcaster
-    */
+     * calls <CODE>createTestEnvironment()</CODE> from it's super class
+     * @param Param the test parameter
+     * @param log the log writer
+     * @return lib.TestEnvironment
+     */
     protected synchronized TestEnvironment createTestEnvironment(TestParameters Param,
                                                                  PrintWriter log) {
-        XInterface oObj = null;
-
-        XControlShape aShape = FormTools.createControlShape(xTextDoc, 3000,
-                                                            4500, 15000, 10000,
-                                                            "TextField");
-
-        WriterTools.getDrawPage(xTextDoc).add((XShape) aShape);
-        oObj = aShape.getControl();
-
-        XLoadable formLoader = null;
-
-        try {
-            DBTools dbTools = new DBTools(((XMultiServiceFactory) Param.getMSF()));
-            dbTools.registerTestDB((String) Param.get("DOCPTH"));
-
-            formLoader = FormTools.bindForm(xTextDoc, "APITestDatabase",
-                                            "TestDB");
-        } catch (com.sun.star.uno.Exception e) {
-            log.println("!!! Can't access TestDB !!!");
-            e.printStackTrace(log);
-            throw new StatusException("Can't access TestDB", e);
-        }
-
-        log.println("creating a new environment for OEditModel object");
-
-        TestEnvironment tEnv = new TestEnvironment(oObj);
-        String objName = "Edit";
-        tEnv.addObjRelation("OBJNAME", "stardiv.one.form.component." +
-                            objName);
-        aShape = FormTools.createControlShape(xTextDoc, 6000, 4500, 15000,
-                                              10000, "FixedText");
-        WriterTools.getDrawPage(xTextDoc).add((XShape) aShape);
-
-        final XPropertySet ps = (XPropertySet) UnoRuntime.queryInterface(
-                                        XPropertySet.class, oObj);
-
-        try {
-            //ps.setPropertyValue("DefaultDate", new Integer(777));
-            ps.setPropertyValue("DataField", DBTools.TST_STRING_F);
-        } catch (com.sun.star.lang.WrappedTargetException e) {
-            e.printStackTrace(log);
-            throw new StatusException("Couldn't set Default Date", e);
-        } catch (com.sun.star.lang.IllegalArgumentException e) {
-            e.printStackTrace(log);
-            throw new StatusException("Couldn't set Default Date", e);
-        } catch (com.sun.star.beans.PropertyVetoException e) {
-            e.printStackTrace(log);
-            throw new StatusException("Couldn't set Default Date", e);
-        } catch (com.sun.star.beans.UnknownPropertyException e) {
-            e.printStackTrace(log);
-            throw new StatusException("Couldn't set Default Date", e);
-        }
-
-
-        // added LabelControl for 'DataAwareControlModel'
-        tEnv.addObjRelation("LC", aShape.getControl());
-
-
-        // added FormLoader for 'DataAwareControlModel'
-        tEnv.addObjRelation("FL", formLoader);
-
-
-        //adding ObjRelation for XPersistObject
-        tEnv.addObjRelation("PSEUDOPERSISTENT", new Boolean(true));
-
-        // adding relation for XUpdateBroadcaster
-        final XInterface ctrl = oObj;
-        final XLoadable formLoaderF = formLoader;
-
-        tEnv.addObjRelation("XUpdateBroadcaster.Checker",
-                            new ifc.form._XUpdateBroadcaster.UpdateChecker() {
-            private String lastText = "";
-
-            public void update() throws com.sun.star.uno.Exception {
-                if (!formLoaderF.isLoaded()) {
-                    formLoaderF.load();
-                }
-
-                lastText = "_" + ps.getPropertyValue("Text");
-                ps.setPropertyValue("Text", lastText);
-            }
-
-            public void commit() throws com.sun.star.sdbc.SQLException {
-                XBoundComponent bound = (XBoundComponent) UnoRuntime.queryInterface(
-                                                XBoundComponent.class, ctrl);
-                XResultSetUpdate update = (XResultSetUpdate) UnoRuntime.queryInterface(
-                                                  XResultSetUpdate.class,
-                                                  formLoaderF);
-
-                bound.commit();
-                update.updateRow();
-            }
-
-            public boolean wasCommited() throws com.sun.star.uno.Exception {
-                formLoaderF.reload();
-
-                String getS = (String) ps.getPropertyValue("Text");
-
-                return lastText.equals(getS);
-            }
-        });
-
-
-        // adding relation for DataAwareControlModel service
-        tEnv.addObjRelation("DataAwareControlModel.NewFieldName",
-                            DBTools.TST_STRING_F);
-
-        return tEnv;
-    } // finish method getTestEnvironment
+        return super.createTestEnvironment(Param, log);
+    }
 } // finish class OEditModel
