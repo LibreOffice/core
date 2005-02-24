2105d55b7c70cc670c9d7f6e366cea07d216717d
Vladimir Glazounov <vg@openoffice.org>
2005-02-24 15:55:31 +0000

    INTEGRATION: CWS tbe19 (1.7.344); FILE MERGED 2005/02/04 15:42:14 tbe 1.7.344.2: RESYNC: (1.7-1.8); FILE MERGED 2005/01/27 16:59:54 tbe 1.7.344.1: #i40118# undo & restore in basic

--- svx/inc/lboxctrl.hxx
+++ svx/inc/lboxctrl.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: lboxctrl.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-21 14:53:59 $
+ *  last change: $Author: vg $ $Date: 2005-02-24 16:55:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -106,6 +106,7 @@ public:
 class SVX_DLLPUBLIC SvxUndoRedoControl : public SvxListBoxControl
 {
     std::vector< ::rtl::OUString > aUndoRedoList;
+    ::rtl::OUString                aDefaultText;
 
 public:
     SFX_DECL_TOOLBOX_CONTROL();
