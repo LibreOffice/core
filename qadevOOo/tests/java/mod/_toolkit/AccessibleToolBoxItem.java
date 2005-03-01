fedfee7ff62935348e780c013f7a5c23387c8f61
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:41:37 +0000

    INTEGRATION: CWS removedrafts (1.12.22); FILE MERGED 2005/02/18 12:51:10 sw 1.12.22.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleToolBoxItem.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleToolBoxItem.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleToolBoxItem.java,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change:$Date: 2004-11-02 12:16:38 $
+ *  last change:$Date: 2005-03-01 20:41:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,21 +88,21 @@ import util.SOfficeFactory;
  * Test for object that implements the following interfaces :
  * <ul>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleContext</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster
+ *  ::com::sun::star::accessibility::XAccessibleEventBroadcaster
  *  </code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleComponent</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleExtendedComponent
+ *  ::com::sun::star::accessibility::XAccessibleExtendedComponent
  *  </code></li>
  * <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleAction</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleAction</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleText</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleText</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleValue</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleValue</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleEventBroadcaster
