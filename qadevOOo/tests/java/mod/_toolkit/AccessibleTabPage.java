a9400e2d439d60f016fd71ee8835222e68d740a9
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:40:55 +0000

    INTEGRATION: CWS removedrafts (1.6.30); FILE MERGED 2005/02/18 12:51:09 sw 1.6.30.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleTabPage.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleTabPage.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleTabPage.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change:$Date: 2004-07-23 10:49:36 $
+ *  last change:$Date: 2005-03-01 20:40:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -101,14 +101,14 @@ import util.SOfficeFactory;
  * Test for object that implements the following interfaces :
  * <ul>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleContext</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster
+ *  ::com::sun::star::accessibility::XAccessibleEventBroadcaster
  *  </code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  ::com::sun::star::accessibility::XAccessibleComponent</code></li>
  *  <li><code>
- *  drafts::com::sun::star::accessibility::XAccessibleExtendedComponent
+ *  ::com::sun::star::accessibility::XAccessibleExtendedComponent
  *  </code></li>
  * </ul> <p>
  *
