3f7fd1497ccd69ac2d55e8e305f8e49d2ea092f4
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:34:15 +0000

    INTEGRATION: CWS removedrafts (1.5.92); FILE MERGED 2005/02/18 12:51:02 sw 1.5.92.1: #i43060#

--- qadevOOo/tests/java/mod/_sw/SwAccessibleParagraphView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessibleParagraphView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessibleParagraphView.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $
+ *  last change: $Author: kz $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -83,7 +83,7 @@ import com.sun.star.uno.XInterface;
 * Test of accessible object for paragraph of a text document.<p>
 * Object implements the following interfaces :
 * <ul>
-*  <li> <code>drafts::com::sun::star::accessibility::XAccessible</code></li>
+*  <li> <code>::com::sun::star::accessibility::XAccessible</code></li>
 * </ul>
 * @see com.sun.star.accessibility.XAccessible
 */
