4d0b6627ff9cbb940bd9f8e1f09b4bad7cfc43c7
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:32:41 +0000

    INTEGRATION: CWS removedrafts (1.4.108); FILE MERGED 2005/02/18 12:50:59 sw 1.4.108.1: #i43060#

--- qadevOOo/tests/java/mod/_svtools/AccessibleTreeListBox.java
+++ qadevOOo/tests/java/mod/_svtools/AccessibleTreeListBox.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleTreeListBox.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change:$Date: 2003-09-08 12:34:38 $
+ *  last change:$Date: 2005-03-01 20:32:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,13 +92,13 @@ import com.sun.star.util.URL;
 /**
  * Test for object that implements the following interfaces :
  * <ul>
- *  <li><code>drafts::com::sun::star::accessibility::XAccessibleComponent
+ *  <li><code>::com::sun::star::accessibility::XAccessibleComponent
  *  </code></li>
- *  <li><code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster
+ *  <li><code>::com::sun::star::accessibility::XAccessibleEventBroadcaster
  *  </code></li>
- *  <li><code>drafts::com::sun::star::accessibility::XAccessibleContext
+ *  <li><code>::com::sun::star::accessibility::XAccessibleContext
  *  </code></li>
- *  <li><code>drafts::com::sun::star::accessibility::XAccessibleSelection
+ *  <li><code>::com::sun::star::accessibility::XAccessibleSelection
  *  </code></li>
  * </ul> <p>
  *
