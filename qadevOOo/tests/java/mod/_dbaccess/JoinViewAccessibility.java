01a258ed5cb28b0d187a4edaced5aa32fb160676
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:22:42 +0000

    INTEGRATION: CWS removedrafts (1.6.30); FILE MERGED 2005/02/18 12:50:47 sw 1.6.30.1: #i43060#

--- qadevOOo/tests/java/mod/_dbaccess/JoinViewAccessibility.java
+++ qadevOOo/tests/java/mod/_dbaccess/JoinViewAccessibility.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: JoinViewAccessibility.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change:$Date: 2004-08-02 17:56:12 $
+ *  last change:$Date: 2005-03-01 20:22:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -103,10 +103,10 @@ import util.utils;
 /**
 * Object implements the following interfaces :
 * <ul>
-*   <li><code>drafts::com::sun::star::accessibility::XAccessible</code></li>
-*   <li><code>drafts::com::sun::star::accessibility::XAccessibleContext
+*   <li><code>::com::sun::star::accessibility::XAccessible</code></li>
+*   <li><code>::com::sun::star::accessibility::XAccessibleContext
 *   </code></li>
-*   <li><code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster
+*   <li><code>::com::sun::star::accessibility::XAccessibleEventBroadcaster
 *   </code></li>
 * </ul><p>
 * @see com.sun.star.accessibility.XAccessible
