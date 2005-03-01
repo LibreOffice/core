d18ccb35fa2beae284b49e646c6b030d6a03f2e7
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:37:24 +0000

    INTEGRATION: CWS removedrafts (1.5.92); FILE MERGED 2005/02/18 12:51:05 sw 1.5.92.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleDropDownListBox.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleDropDownListBox.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleDropDownListBox.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Date: 2004-01-05 20:33:02 $
+ *  last change: $Date: 2005-03-01 20:37:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -87,11 +87,11 @@ import util.utils;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleAction</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleAction</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleExtendedComponent
--- qadevOOo/tests/java/mod/_toolkit/AccessibleEdit.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleEdit.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleEdit.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Date: 2004-01-05 20:33:34 $
+ *  last change: $Date: 2005-03-01 20:37:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -97,12 +97,12 @@ import util.utils;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEditableText</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleText</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEditableText</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleText</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleExtendedComponent
