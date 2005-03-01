6a3634fd1ea610da2e675e79cfd92d0c191c46e1
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:38:51 +0000

    INTEGRATION: CWS removedrafts (1.7.22); FILE MERGED 2005/02/18 12:51:06 sw 1.7.22.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenuBar.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenuBar.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleMenuBar.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Date: 2004-11-02 12:14:45 $
+ *  last change: $Date: 2005-03-01 20:38:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -89,11 +89,11 @@ import util.utils;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleSelection</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleSelection</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleExtendedComponent
