1147ef3db3d2fcce0933ae1bc051cd0191a39969
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:27:21 +0000

    INTEGRATION: CWS removedrafts (1.6.22); FILE MERGED 2005/02/18 12:50:53 sw 1.6.22.1: #i43060#

--- qadevOOo/tests/java/mod/_sc/ScAccessibleDocument.java
+++ qadevOOo/tests/java/mod/_sc/ScAccessibleDocument.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScAccessibleDocument.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: pjunck $
+ *  last change: $Author: kz $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,10 +88,10 @@ import com.sun.star.uno.XInterface;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleSelection</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleSelection</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleComponent
