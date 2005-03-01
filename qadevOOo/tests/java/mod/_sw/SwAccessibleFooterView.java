eed99c446f2ae45016b04947d9736c0662e79793
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:33:31 +0000

    INTEGRATION: CWS removedrafts (1.7.22); FILE MERGED 2005/02/18 12:51:00 sw 1.7.22.1: #i43060#

--- qadevOOo/tests/java/mod/_sw/SwAccessibleFooterView.java
+++ qadevOOo/tests/java/mod/_sw/SwAccessibleFooterView.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SwAccessibleFooterView.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Date: 2004-11-02 12:10:07 $
+ *  last change: $Date: 2005-03-01 20:33:31 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -91,7 +91,7 @@ import com.sun.star.view.XViewSettingsSupplier;
 * Test of accessible object for a footer of a text document.<p>
 * Object implements the following interfaces :
 * <ul>
-*  <li> <code>drafts::com::sun::star::accessibility::XAccessible</code></li>
+*  <li> <code>::com::sun::star::accessibility::XAccessible</code></li>
 * </ul>
 * @see com.sun.star.accessibility.XAccessible
 */
