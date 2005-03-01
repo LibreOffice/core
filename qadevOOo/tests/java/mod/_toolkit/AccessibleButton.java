ed583678ccaa4e82ed6816e98d94e3be88fe7d50
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:36:04 +0000

    INTEGRATION: CWS removedrafts (1.9.16); FILE MERGED 2005/02/18 12:51:04 sw 1.9.16.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleButton.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleButton.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleButton.java,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Date: 2004-12-10 17:05:07 $
+ *  last change: $Date: 2005-03-01 20:36:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,13 +92,13 @@ import util.utils;
 /**
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleValue</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleAction</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleText</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleValue</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleAction</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleText</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleExtendedComponent
