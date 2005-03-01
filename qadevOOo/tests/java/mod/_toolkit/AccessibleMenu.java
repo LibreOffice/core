209c14fe5f2a4798e7c9ffd1bce747c74c45b5bd
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:38:38 +0000

    INTEGRATION: CWS removedrafts (1.11.22); FILE MERGED 2005/02/18 12:51:06 sw 1.11.22.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenu.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenu.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleMenu.java,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Date: 2004-11-02 12:14:31 $
+ *  last change: $Date: 2005-03-01 20:38:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -90,14 +90,14 @@ import util.utils;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleAction</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleSelection</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleValue</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleText</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleAction</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleSelection</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleValue</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleText</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleEventBroadcaster
