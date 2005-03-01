a5099896df42833713c6164479bb08ef9427348f
Kurt Zenker <kz@openoffice.org>
2005-03-01 16:09:50 +0000

    INTEGRATION: CWS libwpdupgrade (1.1.2); FILE MERGED 2005/02/02 20:44:46 rene 1.1.2.1: upgrade writerperfect to new libwpd API and implement --with-system-libwpd. Remove old libwpd/ stuff, tarball will come when the actual release happened :-)

--- writerperfect/source/stream/makefile.mk
+++ writerperfect/source/stream/makefile.mk
@@ -6,8 +6,12 @@ ENABLE_EXCEPTIONS=true
 
 .INCLUDE :  settings.mk
 
+.IF "$(SYSTEM_LIBWPD)" == "YES"
+INCPRE+= $(LIBWPD_CFLAGS)
+.ELSE
 # broken but ... necessary, internal include shafted ...
 INCPRE+=$(SOLARVER)$/$(UPD)$/$(INPATH)$/inc$/libwpd
+.ENDIF
 
 SLOFILES= $(SLO)$/WPXSvStream.obj
 
