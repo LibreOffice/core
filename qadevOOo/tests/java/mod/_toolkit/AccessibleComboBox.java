a319c3ae7acdefa3d63a5729455652d8fbd6223b
Kurt Zenker <kz@openoffice.org>
2005-03-01 19:36:35 +0000

    INTEGRATION: CWS removedrafts (1.8.16); FILE MERGED 2005/02/18 12:51:04 sw 1.8.16.1: #i43060#

--- qadevOOo/tests/java/mod/_toolkit/AccessibleComboBox.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleComboBox.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleComboBox.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Date: 2004-12-10 17:05:44 $
+ *  last change: $Date: 2005-03-01 20:36:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,10 +96,10 @@ import util.utils;
  *
  * Object implements the following interfaces :
  * <ul>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleComponent</code></li>
- *  <li> <code>drafts::com::sun::star::accessibility::XAccessibleContext</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleExtendedComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleEventBroadcaster</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleComponent</code></li>
+ *  <li> <code>::com::sun::star::accessibility::XAccessibleContext</code></li>
  * </ul> <p>
  *
  * @see com.sun.star.accessibility.XAccessibleExtendedComponent
