7dcd3e488e2696850e446d3bd21069723aba606d
Vladimir Glazounov <vg@openoffice.org>
2005-03-08 14:41:15 +0000

    INTEGRATION: CWS calc30 (1.47.96); FILE MERGED 2005/02/21 18:36:01 sab 1.47.96.2: RESYNC: (1.47-1.48); FILE MERGED 2005/02/14 16:26:14 nn 1.47.96.1: #i42666# copy doc options to clipboard/OLE object

--- sc/source/core/data/documen2.cxx
+++ sc/source/core/data/documen2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documen2.cxx,v $
  *
- *  $Revision: 1.49 $
+ *  $Revision: 1.50 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-08 11:29:42 $
+ *  last change: $Author: vg $ $Date: 2005-03-08 15:41:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -638,6 +638,11 @@ void ScDocument::InitClipPtrs( ScDocument* pSourceDoc )
     }
     else
         pClipData = NULL;
+
+    // Options pointers exist (ImplCreateOptions) for any document.
+    // Must be copied for correct results in OLE objects (#i42666#).
+    SetDocOptions( pSourceDoc->GetDocOptions() );
+    SetViewOptions( pSourceDoc->GetViewOptions() );
 }
 
 SvNumberFormatter* ScDocument::GetFormatTable() const
