104707ee908391d3dfe53364dcf31d6ab99ed1b8
Vladimir Glazounov <vg@openoffice.org>
2005-03-08 14:05:30 +0000

    INTEGRATION: CWS oasisbf5 (1.2.210); FILE MERGED 2005/02/18 11:41:52 mib 1.2.210.1: #i43065#: wrong values for fo:keep-with-next in tables

--- sw/source/filter/xml/xmlitmap.hxx
+++ sw/source/filter/xml/xmlitmap.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlitmap.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2004-11-09 12:36:22 $
+ *  last change: $Author: vg $ $Date: 2005-03-08 15:05:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,7 @@ struct SvXMLItemMapEntry
     enum ::xmloff::token::XMLTokenEnum eLocalName;
                                 // the local name for the item inside
                                 // the Namespace (as an XMLTokenEnum)
-    sal_uInt32 nWhichId;        // the WichId to identify the item
+    sal_uInt16 nWhichId;        // the WichId to identify the item
                                 // in the pool
     sal_uInt32 nMemberId;       // the memberid specifies wich part
                                 // of the item should be imported or
