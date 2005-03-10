df4a4353cf28f24d82528ee546ceff9131c7ee2d
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 14:31:16 +0000

    INTEGRATION: CWS dba24 (1.10.44); FILE MERGED 2005/03/08 07:54:05 fs 1.10.44.2: RESYNC: (1.10-1.11); FILE MERGED 2005/02/04 08:43:35 oj 1.10.44.1: STLPort fix from thb

--- connectivity/source/drivers/mysql/YDriver.cxx
+++ connectivity/source/drivers/mysql/YDriver.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: YDriver.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2005-02-24 14:40:05 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 15:31:16 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -350,7 +350,7 @@ namespace connectivity
                         );
             }
         }
-        return Sequence< DriverPropertyInfo >(&(aDriverInfo[0]),aDriverInfo.size());
+        return Sequence< DriverPropertyInfo >(&aDriverInfo[0],aDriverInfo.size());
     }
 
     //--------------------------------------------------------------------
