7338b4f2ab8f9eb8cb4cf9358ba9520e773ed3fe
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 12:45:00 +0000

    INTEGRATION: CWS presfixes01 (1.3.6); FILE MERGED 2005/02/16 11:17:45 fs 1.3.6.1: #i42558# drafts.com.sun.star.drawing/rendering/geometry moved to com.sun.star.*

--- slideshow/source/engine/slidebitmap.cxx
+++ slideshow/source/engine/slidebitmap.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: slidebitmap.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 18:59:01 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 13:45:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,11 +63,11 @@
 #include <canvas/debug.hxx>
 #include <slidebitmap.hxx>
 
-#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XCANVAS_HPP_
-#include <drafts/com/sun/star/rendering/XCanvas.hpp>
+#ifndef _COM_SUN_STAR_RENDERING_XCANVAS_HPP_
+#include <com/sun/star/rendering/XCanvas.hpp>
 #endif
-#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XBITMAP_HPP_
-#include <drafts/com/sun/star/rendering/XBitmap.hpp>
+#ifndef _COM_SUN_STAR_RENDERING_XBITMAP_HPP_
+#include <com/sun/star/rendering/XBitmap.hpp>
 #endif
 
 #ifndef _BGFX_MATRIX_B2DHOMMATRIX_HXX
@@ -78,7 +78,7 @@
 #include <basegfx/tools/canvastools.hxx>
 
 
-using namespace ::drafts::com::sun::star;
+using namespace ::com::sun::star;
 using namespace ::com::sun::star;
 
 namespace presentation
