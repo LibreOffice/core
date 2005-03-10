16ff6cfce57576ea56953e0928b37c03101f9b68
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 12:30:36 +0000

    INTEGRATION: CWS presfixes01 (1.3.6); FILE MERGED 2005/02/16 11:14:38 fs 1.3.6.1: #i42558# drafts.com.sun.star.drawing/rendering/geometry moved to com.sun.star.*

--- cppcanvas/source/wrapper/implfont.cxx
+++ cppcanvas/source/wrapper/implfont.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: implfont.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 21:01:11 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 13:30:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,7 @@
 #include <canvas/canvastools.hxx>
 
 
-using namespace ::drafts::com::sun::star;
+using namespace ::com::sun::star;
 using namespace ::com::sun::star;
 
 /* Definition of Font class */
