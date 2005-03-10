d71cd2b0359aa78e99c647913a415caa7f53e989
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 12:54:06 +0000

    INTEGRATION: CWS presfixes01 (1.2.6); FILE MERGED 2005/02/15 12:45:10 thb 1.2.6.1: #i42440# Moved sprite-growing code to slideshow, we already handle the case there for update areas of antialiased shapes; Improved/reformatted documentation; (Hopefully) fixed the problem that sometimes, the first effect preview did not work. The reason there was presumably the clearing of the EventQueue before every slide, and the fact that EventMultiplexer only generated new tick events when it thought ticking was switched off.

--- slideshow/source/inc/event.hxx
+++ slideshow/source/inc/event.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: event.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 19:15:36 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 13:54:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,10 +62,7 @@
 #ifndef _SLIDESHOW_EVENT_HXX
 #define _SLIDESHOW_EVENT_HXX
 
-#ifndef BOOST_SHARED_PTR_HPP_INCLUDED
 #include <boost/shared_ptr.hpp>
-#endif
-
 #include <vector>
 
 #include <disposable.hxx>
