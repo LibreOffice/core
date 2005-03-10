d80bfeb2b29fde9d89c7103602168a62219a2f12
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 15:23:03 +0000

    INTEGRATION: CWS dba24 (1.2.186); FILE MERGED 2005/02/11 12:56:11 fs 1.2.186.1: #i41976# data navigator docked at the right hand side by default

--- svx/source/form/datanavi.cxx
+++ svx/source/form/datanavi.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: datanavi.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-16 11:21:49 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 16:23:03 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -2182,7 +2182,8 @@ namespace svxform
 
     {
         pWindow = new DataNavigator( pBindings, this, pParent );
-        eChildAlignment = SFX_ALIGN_NOALIGNMENT;
+        eChildAlignment = SFX_ALIGN_RIGHT;
+        pWindow->SetSizePixel( Size( 250, 400 ) );
         ( (SfxDockingWindow*)pWindow )->Initialize( pInfo );
     }
 
