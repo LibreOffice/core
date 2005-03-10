407b2dbaaa017360ae9b19e95e09deb28c6b9bee
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 11:01:35 +0000

    INTEGRATION: CWS presfixes01 (1.2.10); FILE MERGED 2005/02/16 11:14:17 fs 1.2.10.1: #i42558# drafts.com.sun.star.drawing/rendering/geometry moved to com.sun.star.*

--- canvas/source/vcl/textlayout.hxx
+++ canvas/source/vcl/textlayout.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: textlayout.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-26 17:16:16 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 12:01:35 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,8 +72,8 @@
 #ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #endif
-#ifndef _DRAFTS_COM_SUN_STAR_RENDERING_XTEXTLAYOUT_HPP_
-#include <drafts/com/sun/star/rendering/XTextLayout.hpp>
+#ifndef _COM_SUN_STAR_RENDERING_XTEXTLAYOUT_HPP_
+#include <com/sun/star/rendering/XTextLayout.hpp>
 #endif
 
 #include <canvas/vclwrapper.hxx>
@@ -88,13 +88,13 @@
 
 namespace vclcanvas
 {
-    typedef ::cppu::WeakComponentImplHelper2< ::drafts::com::sun::star::rendering::XTextLayout,
+    typedef ::cppu::WeakComponentImplHelper2< ::com::sun::star::rendering::XTextLayout,
                                                ::com::sun::star::lang::XServiceInfo > TextLayout_Base;
 
     class TextLayout : public ::comphelper::OBaseMutex, public TextLayout_Base
     {
     public:
-        TextLayout( const ::drafts::com::sun::star::rendering::StringContext&   aText,
+        TextLayout( const ::com::sun::star::rendering::StringContext&   aText,
                     sal_Int8                                                    nDirection,
                     sal_Int64                                                   nRandomSeed,
                     const CanvasFont::ImplRef&                                  rFont,
@@ -104,23 +104,23 @@ namespace vclcanvas
         virtual void SAL_CALL disposing();
 
         // XTextLayout
-        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > > SAL_CALL queryTextShapes(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryInkMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< ::drafts::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XPolyPolygon2D > > SAL_CALL queryTextShapes(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryInkMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::geometry::RealRectangle2D > SAL_CALL queryMeasures(  ) throw (::com::sun::star::uno::RuntimeException);
         virtual ::com::sun::star::uno::Sequence< double > SAL_CALL queryLogicalAdvancements(  ) throw (::com::sun::star::uno::RuntimeException);
         virtual void SAL_CALL applyLogicalAdvancements( const ::com::sun::star::uno::Sequence< double >& aAdvancements ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
-        virtual ::drafts::com::sun::star::geometry::RealRectangle2D SAL_CALL queryTextBounds(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::geometry::RealRectangle2D SAL_CALL queryTextBounds(  ) throw (::com::sun::star::uno::RuntimeException);
         virtual double SAL_CALL justify( double nSize ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
-        virtual double SAL_CALL combinedJustify( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XTextLayout > >& aNextLayouts, double nSize ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
-        virtual ::drafts::com::sun::star::rendering::TextHit SAL_CALL getTextHit( const ::drafts::com::sun::star::geometry::RealPoint2D& aHitPoint ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::drafts::com::sun::star::rendering::Caret SAL_CALL getCaret( sal_Int32 nInsertionIndex, sal_Bool bExcludeLigatures ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual double SAL_CALL combinedJustify( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XTextLayout > >& aNextLayouts, double nSize ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::rendering::TextHit SAL_CALL getTextHit( const ::com::sun::star::geometry::RealPoint2D& aHitPoint ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::rendering::Caret SAL_CALL getCaret( sal_Int32 nInsertionIndex, sal_Bool bExcludeLigatures ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
         virtual sal_Int32 SAL_CALL getNextInsertionIndex( sal_Int32 nStartIndex, sal_Int32 nCaretAdvancement, sal_Bool bExcludeLigatures ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryVisualHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryLogicalHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryVisualHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XPolyPolygon2D > SAL_CALL queryLogicalHighlighting( sal_Int32 nStartIndex, sal_Int32 nEndIndex ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
         virtual double SAL_CALL getBaselineOffset(  ) throw (::com::sun::star::uno::RuntimeException);
         virtual sal_Int8 SAL_CALL getMainTextDirection(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Reference< ::drafts::com::sun::star::rendering::XCanvasFont > SAL_CALL getFont(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::drafts::com::sun::star::rendering::StringContext SAL_CALL getText(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XCanvasFont > SAL_CALL getFont(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::rendering::StringContext SAL_CALL getText(  ) throw (::com::sun::star::uno::RuntimeException);
 
         // XServiceInfo
         virtual ::rtl::OUString SAL_CALL getImplementationName() throw( ::com::sun::star::uno::RuntimeException );
@@ -129,8 +129,8 @@ namespace vclcanvas
 
         bool draw( OutputDevice&                                           rOutDev,
                    const Point&                                            rOutpos,
-                   const ::drafts::com::sun::star::rendering::ViewState&   viewState,
-                   const ::drafts::com::sun::star::rendering::RenderState& renderState ) const;
+                   const ::com::sun::star::rendering::ViewState&   viewState,
+                   const ::com::sun::star::rendering::RenderState& renderState ) const;
 
     protected:
         ~TextLayout(); // we're a ref-counted UNO class. _We_ destroy ourselves.
@@ -142,10 +142,10 @@ namespace vclcanvas
 
         void setupTextOffsets( long*                                                    outputOffsets,
                                const ::com::sun::star::uno::Sequence< double >&         inputOffsets,
-                               const ::drafts::com::sun::star::rendering::ViewState&    viewState,
-                               const ::drafts::com::sun::star::rendering::RenderState&  renderState     ) const;
+                               const ::com::sun::star::rendering::ViewState&    viewState,
+                               const ::com::sun::star::rendering::RenderState&  renderState     ) const;
 
-        ::drafts::com::sun::star::rendering::StringContext maText;
+        ::com::sun::star::rendering::StringContext maText;
         ::com::sun::star::uno::Sequence< double >          maLogicalAdvancements;
         CanvasFont::ImplRef                                mpFont;
         OutDevProviderSharedPtr                            mpRefDevice;
