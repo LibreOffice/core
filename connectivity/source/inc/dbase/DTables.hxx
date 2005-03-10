8b566579b04e093aab160e8b725f0b5f55e026a2
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 14:39:01 +0000

    INTEGRATION: CWS dba24 (1.4.262); FILE MERGED 2005/02/09 08:07:54 oj 1.4.262.1: #i26950# remove the need for XNamed

--- connectivity/source/inc/dbase/DTables.hxx
+++ connectivity/source/inc/dbase/DTables.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: DTables.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: oj $ $Date: 2001-10-12 11:54:43 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 15:39:01 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -76,10 +76,9 @@ namespace connectivity
         class ODbaseTables : public ODbaseTables_BASE
         {
         protected:
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > createObject(const ::rtl::OUString& _rName);
+            virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
             virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
             virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
         public:
