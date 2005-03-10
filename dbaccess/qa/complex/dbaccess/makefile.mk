2cb92b71ac35900ff6b53d1e92463ec0dadfc21b
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 15:26:46 +0000

    INTEGRATION: CWS dba24 (1.3.84); FILE MERGED 2005/02/22 11:41:54 oj 1.3.84.1: #i43296# remove find and append java names directly

--- dbaccess/qa/complex/dbaccess/makefile.mk
+++ dbaccess/qa/complex/dbaccess/makefile.mk
@@ -3,9 +3,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Date: 2004-07-23 14:34:02 $
+#   last change: $Date: 2005-03-10 16:26:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,7 +73,7 @@ PACKAGE = complex$/dbaccess
 #----- compile .java files -----------------------------------------
 
 JARFILES        = ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar OOoRunner.jar
-JAVAFILES       = $(shell $(FIND) .$/*.java)
+JAVAFILES       = SingleSelectQueryComposer.java
 JAVACLASSFILES	= $(foreach,i,$(JAVAFILES) $(CLASSDIR)$/$(PACKAGE)$/$(i:b).class)
 
 #----- make a jar from compiled files ------------------------------
