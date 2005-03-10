7f3fa9a8c96232018759568f79861cc4e78db41b
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 14:35:30 +0000

    INTEGRATION: CWS dba24 (1.5.260); FILE MERGED 2005/02/09 08:07:50 oj 1.5.260.1: #i26950# remove the need for XNamed

--- connectivity/source/inc/adabas/BViews.hxx
+++ connectivity/source/inc/adabas/BViews.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: BViews.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: oj $ $Date: 2001-10-30 11:03:59 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 15:35:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -77,10 +77,9 @@ namespace connectivity
             sal_Bool m_bInDrop;
             //  OCatalog*                                           m_pParent;
         protected:
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > createObject(const ::rtl::OUString& _rName);
+            virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
             virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
             virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
 
--- connectivity/source/inc/ado/AGroups.hxx
+++ connectivity/source/inc/ado/AGroups.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AGroups.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: oj $ $Date: 2001-11-09 06:59:00 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 15:35:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -87,11 +87,9 @@ namespace connectivity
             OCatalog*   m_pCatalog;
         protected:
 
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > createObject(const ::rtl::OUString& _rName);
+            virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
             virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            // return a object which is the copy of the descriptor
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::container::XNamed > cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
             virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
         public:
