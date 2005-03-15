118d7de48f0355f479e259dffc45ddda2a3b612f
Oliver Bolte <obo@openoffice.org>
2005-03-15 08:58:05 +0000

    INTEGRATION: CWS ause019 (1.9.10); FILE MERGED 2005/03/14 11:12:42 hjs 1.9.10.1: #i42716# solaris/gcc

--- solenv/inc/unxsogs.mk
+++ solenv/inc/unxsogs.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxsogs.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: kz $ $Date: 2005-03-03 17:37:44 $
+#   last change: $Author: obo $ $Date: 2005-03-15 09:58:05 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -106,7 +106,9 @@ CFLAGSOUTOBJ=-o
 STATIC		= -Wl,-Bstatic
 DYNAMIC		= -Wl,-Bdynamic
 
-LINK=gcc
+LINK*=$(CXX)
+LINKC*=$(CC)
+
 LINKFLAGS=
 LINKFLAGSAPPGUI=-Wl,-export-dynamic
 LINKFLAGSSHLGUI=-shared
@@ -134,15 +136,15 @@ STDSLOGUI=
 STDOBJCUI=
 STDSLOCUI=
 
-STDLIBGUIST=$(DYNAMIC) -lstdc++ -lm
-STDLIBCUIST=$(DYNAMIC) -lstdc++ -lm
-STDLIBGUIMT=$(DYNAMIC) -lpthread -lthread -lstdc++ -lm
-STDLIBCUIMT=$(DYNAMIC) -lpthread -lthread -lstdc++ -lm
+STDLIBGUIST=$(DYNAMIC) -lm
+STDLIBCUIST=$(DYNAMIC) -lm
+STDLIBGUIMT=$(DYNAMIC) -lpthread -lthread -lm
+STDLIBCUIMT=$(DYNAMIC) -lpthread -lthread -lm
 # libraries for linking shared libraries
-STDSHLGUIST=$(DYNAMIC) -lstdc++ -lm
-STDSHLCUIST=$(DYNAMIC) -lstdc++ -lm
-STDSHLGUIMT=$(DYNAMIC) -lpthread -lthread -lstdc++ -lm
-STDSHLCUIMT=$(DYNAMIC) -lpthread -lthread -lstdc++ -lm
+STDSHLGUIST=$(DYNAMIC) -lm
+STDSHLCUIST=$(DYNAMIC) -lm
+STDSHLGUIMT=$(DYNAMIC) -lpthread -lthread -lm
+STDSHLCUIMT=$(DYNAMIC) -lpthread -lthread -lm
 
 STDLIBGUIST+=-lX11
 STDLIBGUIMT+=-lX11
