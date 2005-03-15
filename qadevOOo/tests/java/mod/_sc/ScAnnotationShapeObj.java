56cb158754a4dd490c6157678b81505c6585c666
Oliver Bolte <obo@openoffice.org>
2005-03-15 08:22:17 +0000

    INTEGRATION: CWS gcj3 (1.2.14); FILE MERGED 2005/03/08 19:18:40 cmc 1.2.14.1: #i44359# more of qadevOOo now compilable with gcj

--- qadevOOo/tests/java/mod/_sc/ScAnnotationShapeObj.java
+++ qadevOOo/tests/java/mod/_sc/ScAnnotationShapeObj.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScAnnotationShapeObj.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2005-01-13 17:41:11 $
+ *  last change:$Date: 2005-03-15 09:22:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -133,7 +133,7 @@ public class ScAnnotationShapeObj extends TestCase {
         } catch (com.sun.star.uno.Exception e) {
             // Some exception occures.FAILED
             e.printStackTrace(log);
-            throw new StatusException("Couldn³t create document", e);
+            throw new StatusException("Couldn't create document", e);
         }
     }
 
