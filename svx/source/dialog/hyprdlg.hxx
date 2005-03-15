a033e746cfea2d416e5acf7872a883859858d117
Oliver Bolte <obo@openoffice.org>
2005-03-15 08:28:08 +0000

    INTEGRATION: CWS toolbars4 (1.3.220); FILE MERGED 2005/02/15 17:46:08 cd 1.3.220.1: #i39612# Set dropdown only flag correctly

--- svx/source/dialog/hyprdlg.hxx
+++ svx/source/dialog/hyprdlg.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: hyprdlg.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-17 18:07:50 $
+ *  last change: $Author: obo $ $Date: 2005-03-15 09:28:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -168,6 +168,7 @@ private:
     DECL_LINK(TBClickHdl, ToolBox *);
     DECL_LINK(TBSelectHdl, ToolBox *);
     DECL_LINK(TBDeactivateHdl, ToolBox *);
+    DECL_LINK(DropdownClick, ToolBox * );
     DECL_LINK(LinkPopupSelectHdl, Menu *);
     DECL_LINK(SearchPopupSelectHdl, Menu *);
     DECL_LINK(ComboSelectHdl, ComboBox *);
