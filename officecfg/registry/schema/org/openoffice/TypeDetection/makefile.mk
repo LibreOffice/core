2a5092fd3573dd6fd36f53a8926af7b0eb9d2051
Oliver Bolte <obo@openoffice.org>
2005-03-15 11:44:51 +0000

    INTEGRATION: CWS c01v005 (1.3.50); FILE MERGED 2005/03/02 12:09:40 hjs 1.3.50.1: #119245# move processing to solenv

--- officecfg/registry/schema/org/openoffice/TypeDetection/makefile.mk
+++ officecfg/registry/schema/org/openoffice/TypeDetection/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-01-28 17:22:01 $
+#   last change: $Author: obo $ $Date: 2005-03-15 12:44:51 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -62,9 +62,10 @@
 PRJ=..$/..$/..$/..$/..
 
 PRJNAME=officecfg
-TARGET=schema
+TARGET=schema_ooTypeDetection
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Targets ------------------------------------------------------
 
@@ -78,5 +79,4 @@ XCSFILES= \
     UISort.xcs
 
 .INCLUDE :  target.mk
-.INCLUDE :  $(PRJ)$/registry$/schema$/localtarget.mk
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
