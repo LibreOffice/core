e6ba6b72e93adcac28b93b3f894ae1274004f148
Oliver Bolte <obo@openoffice.org>
2005-03-15 08:21:51 +0000

    INTEGRATION: CWS gcj3 (1.3.10); FILE MERGED 2005/03/08 19:18:39 cmc 1.3.10.1: #i44359# more of qadevOOo now compilable with gcj

--- qadevOOo/runner/util/compare/makefile.mk
+++ qadevOOo/runner/util/compare/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Date: 2005-01-25 15:40:00 $
+#   last change: $Date: 2005-03-15 09:21:51 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -69,10 +69,6 @@ TARGET = runner_util_compare
 
 .INCLUDE :  settings.mk
 
-.IF "$(JDK)" == "gcj"
-all:
-        @echo This dir cannot be build with gcj because of convwatch
-.ELSE
 # --- Files --------------------------------------------------------
 
 JARFILES = ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar
@@ -91,4 +87,3 @@ JAVACLASSFILES=	$(foreach,i,$(JAVAFILES) $(CLASSDIR)$/$(PACKAGE)$/$(i:b).class)
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
-.ENDIF
