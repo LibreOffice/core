b3ee28ebe9303ce600762632b53cb01d27133baf
Kurt Zenker <kz@openoffice.org>
2005-03-18 15:25:43 +0000

    INTEGRATION: CWS dbwizard4 (1.2.78); FILE MERGED 2005/03/14 16:20:16 bc 1.2.78.2: ## merge conflict resolved Issue number: Submitted by: Reviewed by: 2005/02/17 13:50:27 bc 1.2.78.1: ##ControlArrangement in FormWizard modified

--- wizards/com/sun/star/wizards/ui/PeerConfig.java
+++ wizards/com/sun/star/wizards/ui/PeerConfig.java
@@ -2,9 +2,9 @@
 *
 *  $RCSfile: PeerConfig.java,v $
 *
-*  $Revision: 1.4 $
+*  $Revision: 1.5 $
 *
-*  last change: $Author: vg $ $Date: 2005-03-08 15:45:18 $
+*  last change: $Author: kz $ $Date: 2005-03-18 16:25:43 $
 *
 *  The Contents of this file are made available subject to the terms of
 *  either of the following licenses
@@ -66,7 +66,9 @@ import java.util.Vector;
 import com.sun.star.awt.*;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.lang.EventObject;
+import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.UnoRuntime;
+import com.sun.star.wizards.common.Helper;
 
 /**
  * @author rpiterman
@@ -76,10 +78,13 @@ import com.sun.star.uno.UnoRuntime;
  */
 public class PeerConfig implements XWindowListener{
     Vector aPeerTasks = new Vector();
+    Vector aControlTasks = new Vector();
+    Vector aImageUrlTasks = new Vector();
+    UnoDialog oUnoDialog = null;
 
-
-    public PeerConfig(XWindow xDialogWindow) {
-        xDialogWindow.addWindowListener(this);
+    public PeerConfig(UnoDialog _oUnoDialog) {
+        oUnoDialog = _oUnoDialog;
+        oUnoDialog.xWindow.addWindowListener(this);
     }
 
     class PeerTask{
@@ -95,25 +100,67 @@ public class PeerConfig implements XWindowListener{
     }
 
 
+    class ControlTask{
+        Object oModel;
+        String propname;
+        Object propvalue;
+
+        public ControlTask(Object _oModel, String _propName, Object _propValue){
+            propname = _propName;
+            propvalue = _propValue;
+            oModel = _oModel;
+        }
+    }
+
+
+    class ImageUrlTask{
+        Object oModel;
+        Object oResource;
+        Object oHCResource;
+
+        public ImageUrlTask(Object _oModel, Object _oResource, Object _oHCResource){
+            oResource = _oResource;
+            oHCResource = _oHCResource;
+            oModel = _oModel;
+        }
+    }
+
+
     public void windowResized(WindowEvent arg0) {
     }
 
     public void windowMoved(WindowEvent arg0) {
     }
 
+
     public void windowShown(EventObject arg0) {
-        try {
-            for (int i = 0; i < this.aPeerTasks.size(); i++){
-                PeerTask aPeerTask = (PeerTask) aPeerTasks.elementAt(i);
-                XVclWindowPeer xVclWindowPeer = (XVclWindowPeer) UnoRuntime.queryInterface(XVclWindowPeer.class, aPeerTask.xControl.getPeer());
-                for (int n = 0; i < aPeerTask.propnames.length; i++)
-                    xVclWindowPeer.setProperty(aPeerTask.propnames[n], aPeerTask.propvalues[n]);
-            }
-        } catch (RuntimeException re) {
-            re.printStackTrace();
-            throw re;
+    try {
+        for (int i = 0; i < this.aPeerTasks.size(); i++){
+            PeerTask aPeerTask = (PeerTask) aPeerTasks.elementAt(i);
+            XVclWindowPeer xVclWindowPeer = (XVclWindowPeer) UnoRuntime.queryInterface(XVclWindowPeer.class, aPeerTask.xControl.getPeer());
+            for (int n = 0; n < aPeerTask.propnames.length; n++)
+                xVclWindowPeer.setProperty(aPeerTask.propnames[n], aPeerTask.propvalues[n]);
         }
-    }
+        for (int i = 0; i < this.aControlTasks.size(); i++){
+            ControlTask aControlTask = (ControlTask) aControlTasks.elementAt(i);
+            Helper.setUnoPropertyValue(aControlTask.oModel, aControlTask.propname, aControlTask.propvalue);
+        }
+        for (int i = 0; i < this.aImageUrlTasks.size(); i++){
+            ImageUrlTask aImageUrlTask = (ImageUrlTask) aImageUrlTasks.elementAt(i);
+            String sImageUrl = "";
+            if (AnyConverter.isInt(aImageUrlTask.oResource))
+                sImageUrl = oUnoDialog.getWizardImageUrl(((Integer) aImageUrlTask.oResource).intValue(), ((Integer) aImageUrlTask.oHCResource).intValue());
+            else if (AnyConverter.isString(aImageUrlTask.oResource))
+                sImageUrl = oUnoDialog.getImageUrl(((String) aImageUrlTask.oResource), ((String) aImageUrlTask.oHCResource));
+            if (!sImageUrl.equals(""))
+                Helper.setUnoPropertyValue(aImageUrlTask.oModel, "ImageURL", sImageUrl);
+        }
+
+    } catch (RuntimeException re) {
+        re.printStackTrace(System.out);
+        throw re;
+    }}
+
 
     public void windowHidden(EventObject arg0) {
     }
@@ -153,4 +200,58 @@ public class PeerConfig implements XWindowListener{
         PeerTask  oPeerTask = new PeerTask(_xControl, propnames, propvalues);
         this.aPeerTasks.add(oPeerTask);
     }
+
+    /**
+     * assigns an arbitrary property to a control as soon as the peer is created
+     * Note: The property 'ImageUrl' should better be assigned with 'setImageurl(...)', to consider the High Contrast Mode
+     * @param _ocontrolmodel
+     * @param _spropname
+     * @param _propvalue
+     */
+    public void setControlProperty(Object _ocontrolmodel, String _spropname, Object _propvalue){
+        ControlTask oControlTask = new ControlTask(_ocontrolmodel, _spropname, _propvalue);
+        this.aControlTasks.add(oControlTask);
+    }
+
+    /**
+     * Assigns an image to the property 'ImageUrl' of a dialog control. The image id must be assigned in a resource file
+     * within the wizards project
+     * wizards project
+     * @param _ocontrolmodel
+     * @param _nResId
+     * @param _nhcResId
+     */
+    public void setImageUrl(Object _ocontrolmodel, int _nResId, int _nhcResId){
+        ImageUrlTask oImageUrlTask = new ImageUrlTask(_ocontrolmodel, new Integer(_nResId), new Integer(_nhcResId));
+        this.aImageUrlTasks.add(oImageUrlTask);
+    }
+
+    /**
+     * Assigns an image to the property 'ImageUrl' of a dialog control. The image ids that the Resource urls point to
+     * may be assigned in a Resource file outside the wizards project
+     * @param _ocontrolmodel
+     * @param _sResourceUrl
+     * @param _sHCResourceUrl
+     */
+    public void setImageUrl(Object _ocontrolmodel, String _sResourceUrl, String _sHCResourceUrl){
+        ImageUrlTask oImageUrlTask = new ImageUrlTask(_ocontrolmodel, _sResourceUrl, _sHCResourceUrl);
+        this.aImageUrlTasks.add(oImageUrlTask);
+    }
+
+
+    /**
+     * Assigns an image to the property 'ImageUrl' of a dialog control. The image id must be assigned in a resource file
+     * within the wizards project
+     * wizards project
+     * @param _ocontrolmodel
+     * @param _nResId
+     * @param _nhcResId
+     */
+    public void setImageUrl(Object _ocontrolmodel, Object _oResource, Object _oHCResource){
+        ImageUrlTask oImageUrlTask = new ImageUrlTask(_ocontrolmodel, _oResource, _oHCResource);
+        this.aImageUrlTasks.add(oImageUrlTask);
+    }
+
+
+
 }
