901f0d18679943b680561ffc80e41588d79306c2
Kurt Zenker <kz@openoffice.org>
2005-03-18 15:57:11 +0000

    INTEGRATION: CWS impress36 (1.3.214); FILE MERGED 2005/02/24 12:13:50 af 1.3.214.1: #i43335# Made controls constructable on demand.

--- sd/source/ui/toolpanel/TaskPaneTreeNode.cxx
+++ sd/source/ui/toolpanel/TaskPaneTreeNode.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TaskPaneTreeNode.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-08-04 08:58:06 $
+ *  last change: $Author: kz $ $Date: 2005-03-18 16:57:11 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,7 +59,7 @@
  *
  ************************************************************************/
 
-#include "TaskPaneTreeNode.hxx"
+#include "taskpane/TaskPaneTreeNode.hxx"
 
 #include "ControlContainer.hxx"
 #include "TitledControl.hxx"
