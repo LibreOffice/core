c73c57d70d8b51f4b826576e70a79020fd96e0ff
Kurt Zenker <kz@openoffice.org>
2005-03-18 15:26:07 +0000

    INTEGRATION: CWS dbwizard4 (1.2.78); FILE MERGED 2005/03/14 15:12:44 bc 1.2.78.3: ## merge conflicts resolved 2005/03/12 18:31:38 bc 1.2.78.2: RESYNC: (1.2-1.3); FILE MERGED 2005/02/17 13:50:27 bc 1.2.78.1: ##ControlArrangement in FormWizard modified

--- wizards/com/sun/star/wizards/ui/TitlesComponent.java
+++ wizards/com/sun/star/wizards/ui/TitlesComponent.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: TitlesComponent.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2005-02-21 14:06:18 $
+ *  last change: $Author: kz $ $Date: 2005-03-18 16:26:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -173,7 +173,7 @@ public class TitlesComponent extends ControlScroller{
         initializeScrollFields();
         super.initialize(fieldnames.length);
         adjustPosSizes();
-        setComponentMouseTransparent();
+//      setComponentMouseTransparent();
     }
 
 
