b7bab28a4057832ff9f48511c9cb09a7606032ed
Oliver Bolte <obo@openoffice.org>
2005-03-18 09:09:54 +0000

    INTEGRATION: CWS dba25 (1.3.102); FILE MERGED 2005/03/04 07:19:44 oj 1.3.102.1: #i43912# use set connection as activeconnection

--- dbaccess/source/ui/dlg/UserAdmin.hxx
+++ dbaccess/source/ui/dlg/UserAdmin.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: UserAdmin.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-02 15:41:57 $
+ *  last change: $Author: obo $ $Date: 2005-03-18 10:09:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -121,7 +121,6 @@ protected:
     DECL_LINK( UserHdl,         PushButton * );
 
     void        FillUserNames();
-    virtual int DeactivatePage(SfxItemSet* _pSet);
 
     OUserAdmin( Window* pParent, const SfxItemSet& _rCoreAttrs);
 public:
