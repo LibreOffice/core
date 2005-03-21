6f8b28a32f2d735e029ceb6a849d4931ef37b9f4
Kurt Zenker <kz@openoffice.org>
2005-03-21 10:48:00 +0000

    INTEGRATION: CWS fwkpostbeta1 (1.3.130); FILE MERGED 2005/03/03 10:49:26 as 1.3.130.2: resolve merge conflicts 2005/02/10 08:41:31 as 1.3.130.1: #i41696# seperate ui names from filter properties

--- filter/source/config/fragments/fcfg_web.mk
+++ filter/source/config/fragments/fcfg_web.mk
@@ -35,6 +35,19 @@ F4_WEB = \
     writerweb8_writer
 
 # -----------------------------------------------
+# count = 9
+F4_UI_WEB = \
+    HTML_ui \
+    StarWriter_Web_4_0_Vorlage_Template_ui \
+    StarWriter_Web_5_0_Vorlage_Template_ui \
+    Text__StarWriter_Web__ui \
+    Text__encoded___StarWriter_Web__ui \
+    writer_web_StarOffice_XML_Writer_ui \
+    writer_web_StarOffice_XML_Writer_Web_Template_ui \
+    writerweb8_writer_template_ui \
+    writerweb8_writer_ui
+    
+# -----------------------------------------------
 # count = 0
 L4_WEB =
 
@@ -43,15 +56,22 @@ L4_WEB =
 C4_WEB =
 
 # -----------------------------------------------
-TYPES_4fcfg_web           = $(foreach,i,$(T4_WEB) types$/$i.xcu          )
-FILTERS_4fcfg_web         = $(foreach,i,$(F4_WEB) filters$/$i.xcu        )
-FRAMELOADERS_4fcfg_web    = $(foreach,i,$(L4_WEB) frameloaders$/$i.xcu   )
-CONTENTHANDLERS_4fcfg_web = $(foreach,i,$(C4_WEB) contenthandlers$/$i.xcu)
+TYPES_4fcfg_web           = $(foreach,i,$(T4_WEB)    types$/$i.xcu                   )
+FILTERS_4fcfg_web         = $(foreach,i,$(F4_WEB)    filters$/$i.xcu                 )
+UI_FILTERS_4fcfg_web      = $(foreach,i,$(F4_UI_WEB) $(DIR_LOCFRAG)$/filters$/$i.xcu )
+FRAMELOADERS_4fcfg_web    = $(foreach,i,$(L4_WEB)    frameloaders$/$i.xcu            )
+CONTENTHANDLERS_4fcfg_web = $(foreach,i,$(C4_WEB)    contenthandlers$/$i.xcu         )
 
 # -----------------------------------------------
 # needed to get dependencies inside global makefile work!
 ALL_4fcfg_web = \
     $(TYPES_4fcfg_web) \
-    $(foreach,i,$(FILTERS_4fcfg_base) $(MISC)$/$i) \
+    $(FILTERS_4fcfg_web) \
+    $(UI_FILTERS_4fcfg_web) \
     $(FRAMELOADERS_4fcfg_web) \
     $(CONTENTHANDLERS_4fcfg_web)
+
+ALL_UI_FILTERS+=$(UI_FILTERS_4fcfg_web)
+    
+ALL_PACKAGES+=web
+    
