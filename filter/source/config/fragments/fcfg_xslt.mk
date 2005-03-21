5d24b2a4b2ef0da99b2cf080a7b379ab384d77c1
Kurt Zenker <kz@openoffice.org>
2005-03-21 10:48:40 +0000

    INTEGRATION: CWS fwkpostbeta1 (1.5.56); FILE MERGED 2005/03/03 10:49:27 as 1.5.56.2: resolve merge conflicts 2005/02/10 08:41:30 as 1.5.56.1: #i41696# seperate ui names from filter properties

--- filter/source/config/fragments/fcfg_xslt.mk
+++ filter/source/config/fragments/fcfg_xslt.mk
@@ -17,6 +17,10 @@ F4_XSLT = \
 
 # -----------------------------------------------
 # count = 0
+F4_UI_XSLT =
+    
+# -----------------------------------------------
+# count = 0
 L4_XSLT =
 
 # -----------------------------------------------
@@ -24,15 +28,22 @@ L4_XSLT =
 C4_XSLT =
 
 # -----------------------------------------------
-TYPES_4fcfg_xslt           = $(foreach,i,$(T4_XSLT) types$/$i.xcu          )
-FILTERS_4fcfg_xslt         = $(foreach,i,$(F4_XSLT) filters$/$i.xcu        )
-FRAMELOADERS_4fcfg_xslt    = $(foreach,i,$(L4_XSLT) frameloaders$/$i.xcu   )
-CONTENTHANDLERS_4fcfg_xslt = $(foreach,i,$(C4_XSLT) contenthandlers$/$i.xcu)
+TYPES_4fcfg_xslt           = $(foreach,i,$(T4_XSLT)    types$/$i.xcu                     )
+FILTERS_4fcfg_xslt         = $(foreach,i,$(F4_XSLT)    filters$/$i.xcu                   )
+UI_FILTERS_4fcfg_xslt      = $(foreach,i,$(F4_UI_XSLT) $(DIR_LOCFRAG)$/filters$/$i.xcu   )
+FRAMELOADERS_4fcfg_xslt    = $(foreach,i,$(L4_XSLT)    frameloaders$/$i.xcu              )
+CONTENTHANDLERS_4fcfg_xslt = $(foreach,i,$(C4_XSLT)    contenthandlers$/$i.xcu           )
 
 # -----------------------------------------------
 # needed to get dependencies inside global makefile work!
 ALL_4fcfg_xslt = \
     $(TYPES_4fcfg_xslt) \
-    $(foreach,i,$(FILTERS_4fcfg_base) $(MISC)$/$i) \
+    $(FILTERS_4fcfg_xslt) \
+    $(UI_FILTERS_4fcfg_xslt) \
     $(FRAMELOADERS_4fcfg_xslt) \
     $(CONTENTHANDLERS_4fcfg_xslt)
+    
+ALL_UI_FILTERS+=$(UI_FILTERS_4fcfg_xslt)    
+    
+ALL_PACKAGES+=xslt
+    
