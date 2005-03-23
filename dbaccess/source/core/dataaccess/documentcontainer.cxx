3510d882bc3ff23f1ff7423fd784700583ac5b05
Vladimir Glazounov <vg@openoffice.org>
2005-03-23 08:47:06 +0000

    INTEGRATION: CWS dba26 (1.10.60); FILE MERGED 2005/03/14 14:00:00 fs 1.10.60.2: RESYNC: (1.10-1.11); FILE MERGED 2005/03/11 13:35:58 fs 1.10.60.1: #i44786# createInstanceWithArguments: don't reuse the ContentImpl of an existing object, if we're creating a new one

--- dbaccess/source/core/dataaccess/documentcontainer.cxx
+++ dbaccess/source/core/dataaccess/documentcontainer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documentcontainer.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-10 16:34:52 $
+ *  last change: $Author: vg $ $Date: 2005-03-23 09:47:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -254,7 +254,7 @@ Reference< XInterface > SAL_CALL ODocumentContainer::createInstanceWithArguments
 
         ODefinitionContainer_Impl::Documents::iterator aFind = pItem->m_aDocumentMap.find(sName);
         TContentPtr pElementImpl;
-        if ( aFind == pItem->m_aDocumentMap.end() )
+        if ( bNew || ( aFind == pItem->m_aDocumentMap.end() ) )
         {
             pElementImpl.reset( new OContentHelper_Impl );
             if ( !bNew )
