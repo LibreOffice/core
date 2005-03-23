b208d83b9de5c5d326e05b36358e8c92132d731b
Vladimir Glazounov <vg@openoffice.org>
2005-03-23 10:31:19 +0000

    INTEGRATION: CWS eforms4 (1.9.6); FILE MERGED 2004/12/10 09:11:33 fs 1.9.6.2: #i36359# some method constness 2004/12/08 16:16:58 fs 1.9.6.1: #i36359# #i36303# proper handling when bound to external values

--- forms/source/component/Time.hxx
+++ forms/source/component/Time.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: Time.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-16 10:41:17 $
+ *  last change: $Author: vg $ $Date: 2005-03-23 11:31:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -124,7 +124,9 @@ protected:
     virtual sal_Bool        commitControlValueToDbColumn( bool _bPostReset );
 
     virtual ::com::sun::star::uno::Any
-                            translateControlValueToValidatableValue( ) const;
+                            translateControlValueToExternalValue( ) const;
+    virtual ::com::sun::star::uno::Any
+                            translateExternalValueToControlValue( ) const;
 
     virtual ::com::sun::star::uno::Any
                             getDefaultForReset() const;
