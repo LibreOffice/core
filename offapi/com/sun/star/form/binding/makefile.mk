ecb8c9706904a7d349d2b1b4fc20a97b2bd47ad1
Vladimir Glazounov <vg@openoffice.org>
2005-03-23 10:43:49 +0000

    INTEGRATION: CWS eforms4 (1.3.6); FILE MERGED 2004/12/08 16:23:55 fs 1.3.6.1: +InvalidBindingStateException

--- offapi/com/sun/star/form/binding/makefile.mk
+++ offapi/com/sun/star/form/binding/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: obo $ $Date: 2004-11-16 11:08:12 $
+#   last change: $Author: vg $ $Date: 2005-03-23 11:43:49 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -96,6 +96,7 @@ IDLFILES=\
     BindableDatabaseTimeField.idl   \
     \
     IncompatibleTypesException.idl   \
+    InvalidBindingStateException.idl   \
 
 # ------------------------------------------------------------------
 
