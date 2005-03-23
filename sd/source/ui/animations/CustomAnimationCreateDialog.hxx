6d772d835ad6cd04398d0f2a166125f9ae57df8b
Vladimir Glazounov <vg@openoffice.org>
2005-03-23 12:56:37 +0000

    INTEGRATION: CWS impress37 (1.3.56); FILE MERGED 2005/03/08 14:15:33 bm 1.3.56.1: #i39279# open at right side and remember last position

--- sd/source/ui/animations/CustomAnimationCreateDialog.hxx
+++ sd/source/ui/animations/CustomAnimationCreateDialog.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CustomAnimationCreateDialog.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-21 18:18:13 $
+ *  last change: $Author: vg $ $Date: 2005-03-23 13:56:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -95,6 +95,8 @@ public:
 private:
     CustomAnimationCreateTabPage* getCurrentPage() const;
     void preview( const CustomAnimationPresetPtr& pPreset ) const;
+    void setPosition();
+    void storePosition();
 
     DECL_LINK( implActivatePagekHdl, Control* );
     DECL_LINK( implDeactivatePagekHdl, Control* );
