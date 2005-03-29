30b9229cdb4a4704e9d9e9f9fe7d6d8726a97389
Rüdiger Timm <rt@openoffice.org>
2005-03-29 11:52:19 +0000

    INTEGRATION: CWS vcl37 (1.5.8); FILE MERGED 2005/03/04 15:30:48 obr 1.5.8.1: #i20314# don't return AccessibleText when disposed

--- accessibility/bridge/org/openoffice/java/accessibility/ToolTip.java
+++ accessibility/bridge/org/openoffice/java/accessibility/ToolTip.java
@@ -88,6 +88,10 @@ public class ToolTip extends Component implements javax.accessibility.Accessible
 
         /** Gets the AccessibleText associated with this object presenting text on the display */
         public javax.accessibility.AccessibleText getAccessibleText() {
+
+            if (disposed)
+                return null;
+
             try {
                 XAccessibleText unoAccessibleText = (XAccessibleText) UnoRuntime.queryInterface(XAccessibleText.class,
                         unoAccessibleComponent);
