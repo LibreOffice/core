fc4e0db8a14aed9a39ced02e41093aeebeecf901
Rüdiger Timm <rt@openoffice.org>
2005-03-29 12:39:47 +0000

    INTEGRATION: CWS xmlsec10 (1.2.130); FILE MERGED 2005/03/08 08:27:39 mmi 1.2.130.1: add key usage information Issue number: Submitted by: Reviewed by:

--- offapi/com/sun/star/security/makefile.mk
+++ offapi/com/sun/star/security/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2004-08-31 09:52:20 $
+#   last change: $Author: rt $ $Date: 2005-03-29 13:39:47 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -88,7 +88,8 @@ IDLFILES=\
     CertificateCharacters.idl           \
     CertificateValidity.idl             \
     XCertificateExtension.idl           \
-    XDocumentDigitalSignatures.idl
+    XDocumentDigitalSignatures.idl	\
+    KeyUsage.idl
 
 # ------------------------------------------------------------------
 
