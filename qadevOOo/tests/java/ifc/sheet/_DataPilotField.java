0635a8377882fa900aae14135674288becaba358
Rüdiger Timm <rt@openoffice.org>
2005-03-29 12:04:39 +0000

    INTEGRATION: CWS groupingapi (1.2.4); FILE MERGED 2005/03/24 10:43:46 sw 1.2.4.1: #i45915#

--- qadevOOo/tests/java/ifc/sheet/_DataPilotField.java
+++ qadevOOo/tests/java/ifc/sheet/_DataPilotField.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: _DataPilotField.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change:$Date: 2005-01-13 17:40:12 $
+ *  last change:$Date: 2005-03-29 13:04:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,6 +61,9 @@
 
 package ifc.sheet;
 
+import com.sun.star.sheet.DataPilotFieldAutoShowInfo;
+import com.sun.star.sheet.DataPilotFieldLayoutInfo;
+import com.sun.star.sheet.DataPilotFieldReference;
 import com.sun.star.sheet.DataPilotFieldSortInfo;
 import lib.MultiPropertyTest;
 
@@ -77,32 +80,123 @@ import lib.MultiPropertyTest;
 public class _DataPilotField extends MultiPropertyTest {
 
     /**
-     *This class is destined to custom test of property <code>PageStyle</code>.
+     *This class is destined to custom test of property <code>SortInfo</code>.
      */
-    protected PropertyTester infoTester = new PropertyTester() {
+    protected PropertyTester SortInfoTester = new PropertyTester() {
         protected Object getNewValue(String propName, Object oldValue) {
             DataPilotFieldSortInfo sortInfo = new DataPilotFieldSortInfo();
         sortInfo.Mode = com.sun.star.sheet.DataPilotFieldSortMode.MANUAL;
             sortInfo.IsAscending = false;
             sortInfo.Field = "COL1";
-            System.out.println("Having Value and returning a new one");
+            log.println("Having Value and returning a new one");
             return sortInfo.equals(oldValue) ? null : sortInfo;
         }
     };
 
     /**
-     * Test property <code>PageStyle</code> using custom <code>PropertyTest</code>.
+     * Test property <code>SortInfo</code> using custom <code>PropertyTest</code>.
      */
     public void _SortInfo() {
-        System.out.println("first trying to get it");
-        Object getting = null;
-        try {
-            oObj.getPropertyValue ("SortInfo");
-        } catch (Exception e) {
-            System.out.println("FAILED");
+        testProperty("SortInfo", SortInfoTester);
+    }
+
+    public void _HasSortInfo() {
+        requiredMethod("SortInfo");
+        testProperty("HasSortInfo");
+    }
+
+    /**
+     *This class is destined to custom test of property <code>LayoutInfo</code>.
+     */
+    protected PropertyTester LayoutInfoTester = new PropertyTester() {
+        protected Object getNewValue(String propName, Object oldValue) {
+            DataPilotFieldLayoutInfo layoutInfo = new DataPilotFieldLayoutInfo();
+            layoutInfo.LayoutMode = com.sun.star.sheet.DataPilotFieldLayoutMode.OUTLINE_SUBTOTALS_BOTTOM;
+            layoutInfo.AddEmptyLines = true;
+            log.println("Having Value and returning a new one");
+            return layoutInfo.equals(oldValue) ? null : layoutInfo;
         }
-        System.out.println("Getting: "+getting.getClass ().getName ());
-        testProperty("SortInfo", infoTester);
+    };
+
+    /**
+     * Test property <code>LayoutInfo</code> using custom <code>PropertyTest</code>.
+     */
+    public void _LayoutInfo() {
+        testProperty("LayoutInfo", LayoutInfoTester);
+    }
+
+    public void _HasLayoutInfo() {
+        requiredMethod("LayoutInfo");
+        testProperty("HasLayoutInfo");
+    }
+
+    /**
+     *This class is destined to custom test of property <code>AutoShowInfo</code>.
+     */
+    protected PropertyTester AutoShowInfoTester = new PropertyTester() {
+        protected Object getNewValue(String propName, Object oldValue) {
+            DataPilotFieldAutoShowInfo AutoShowInfo = new DataPilotFieldAutoShowInfo();
+            AutoShowInfo.DataField = "Col1";
+            AutoShowInfo.IsEnabled = true;
+            log.println("Having Value and returning a new one");
+            return AutoShowInfo.equals(oldValue) ? null : AutoShowInfo;
+        }
+    };
+
+    /**
+     * Test property <code>AutoShowInfo</code> using custom <code>PropertyTest</code>.
+     */
+    public void _AutoShowInfo() {
+        testProperty("AutoShowInfo", AutoShowInfoTester);
+    }
+
+    public void _HasAutoShowInfo() {
+        requiredMethod("AutoShowInfo");
+        testProperty("HasAutoShowInfo");
+    }
+
+    /**
+     *This class is destined to custom test of property <code>Reference</code>.
+     */
+    protected PropertyTester ReferenceTester = new PropertyTester() {
+        protected Object getNewValue(String propName, Object oldValue) {
+            DataPilotFieldReference Reference = new DataPilotFieldReference();
+            Reference.ReferenceField="Col1";
+            Reference.ReferenceItemType = com.sun.star.sheet.DataPilotFieldReferenceItemType.NAMED;
+            log.println("Having Value and returning a new one");
+            return Reference.equals(oldValue) ? null : Reference;
+        }
+    };
+
+    /**
+     * Test property <code>Reference</code> using custom <code>PropertyTest</code>.
+     */
+    public void _Reference() {
+        testProperty("Reference", ReferenceTester);
+    }
+
+    public void _HasReference() {
+        requiredMethod("Reference");
+        testProperty("HasReference");
+    }
+
+    /*
+     * this property can only set a true value to false, fo ungrouping purposes
+     */
+    public void _IsGroupField() {
+        boolean result = true;
+        try {
+            Boolean propValue = (Boolean) oObj.getPropertyValue ("IsGroupField");
+            if (propValue.booleanValue ()) {
+                oObj.setPropertyValue ("IsGroupField", Boolean.FALSE);
+                Boolean propV = (Boolean) oObj.getPropertyValue ("IsGroupField");
+                result = !propV.booleanValue ();
+            }
+        } catch (Exception e) {
+            log.println ("Exception while getting Property 'IsGroupField'"+e);
+            result = false;
+        }
+        tRes.tested ("IsGroupField",result);
     }
 }
 
