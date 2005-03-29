8c93fc8a7f757d00fbaf1dcbedc979442c1fba2c
Rüdiger Timm <rt@openoffice.org>
2005-03-29 13:20:52 +0000

    INTEGRATION: CWS effectmigration01 (1.2.12); FILE MERGED 2005/03/29 09:22:02 cl 1.2.12.3: RESYNC: (1.2-1.3); FILE MERGED 2005/03/17 16:56:47 iha 1.2.12.2: #i45012# dispose sound at deactivation 2005/03/14 17:14:50 iha 1.2.12.1: #i45012# to many player instances created at the same time

--- slideshow/source/engine/animationnodes/animationaudionode.hxx
+++ slideshow/source/engine/animationnodes/animationaudionode.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: animationaudionode.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2005-03-18 17:11:12 $
+ *  last change: $Author: rt $ $Date: 2005-03-29 14:20:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -113,7 +113,11 @@ namespace presentation
         private:
             ::com::sun::star::uno::Reference<
                 ::com::sun::star::animations::XAudio >  mxAudioNode;
-            SoundPlayerSharedPtr                        mpPlayer;
+            ::rtl::OUString                             maSoundURL;
+            mutable SoundPlayerSharedPtr                mpPlayer;
+
+            void createPlayer() const;
+            void resetPlayer() const;
         };
 
     }
