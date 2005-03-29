a4ed74dc6490189c1c4d75a18b55e108babe60f7
Rüdiger Timm <rt@openoffice.org>
2005-03-29 13:18:44 +0000

    INTEGRATION: CWS effectmigration01 (1.3.48); FILE MERGED 2005/03/29 08:54:52 cl 1.3.48.2: RESYNC: (1.3-1.4); FILE MERGED 2005/03/18 18:12:26 cl 1.3.48.1: #i41476# clear XAudio begin and end attributes for now

--- sd/source/filter/ppt/pptinanimations.cxx
+++ sd/source/filter/ppt/pptinanimations.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: pptinanimations.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-23 13:23:49 $
+ *  last change: $Author: rt $ $Date: 2005-03-29 14:18:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -2548,6 +2548,11 @@ void AnimationImporter::importAudioContainer( const Atom* pAtom, const Reference
 
             pChildAtom = pAtom->findNextChildAtom( pChildAtom );
         }
+
+        // TODO: What to do with them?
+        Any aEmpty;
+        xAudio->setBegin( aEmpty );
+        xAudio->setEnd( aEmpty );
     }
 }
 
