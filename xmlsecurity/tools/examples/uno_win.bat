7d1960d2dac143859fedb7610d210f75b800dc3e
Rüdiger Timm <rt@openoffice.org>
2005-03-29 12:32:53 +0000

    INTEGRATION: CWS xmlsec10 (1.2.4); FILE MERGED 2005/03/24 08:20:58 mmi 1.2.4.1: idl review
    Issue number:
    Submitted by:
    Reviewed by:

--- xmlsecurity/tools/examples/uno_win.bat
+++ xmlsecurity/tools/examples/uno_win.bat
@@ -1,6 +1,4 @@
-set WS_PATH=D:/My_work/SRC680
-set OUTPUTDIR=wntmsci10
-set SO_CLASSPATHPATH=d:/2000Soft/OOo1.1/program/classes
+call env_win.bat
 
 @echo off
 echo =============================================
@@ -20,6 +18,6 @@ echo =============================================
 @echo on
 
 cp %WS_PATH%/xmlsecurity/tools/uno/current.gif ./.
-java -classpath %WS_PATH%/xmlsecurity/wntmsci8.pro/class;%SO_CLASSPATHPATH%/rt.jar;%SO_CLASSPATHPATH%/java_uno.jar;%SO_CLASSPATHPATH%/sandbox.jar;%SO_CLASSPATHPATH%/jurt.jar;%SO_CLASSPATHPATH%/unoil.jar;%SO_CLASSPATHPATH%/ridl.jar;%SO_CLASSPATHPATH%/juh.jar com.sun.star.xml.security.uno.TestTool %WS_PATH%/xmlsecurity/tools/cryptoken/jks/testToken.jks %WS_PATH%/xmlsecurity/tools/cryptoken/nss
+java -classpath %WS_PATH%/xmlsecurity/%OUTPUTDIR%/class;%SO_CLASSPATHPATH%/rt.jar;%SO_CLASSPATHPATH%/java_uno.jar;%SO_CLASSPATHPATH%/sandbox.jar;%SO_CLASSPATHPATH%/jurt.jar;%SO_CLASSPATHPATH%/unoil.jar;%SO_CLASSPATHPATH%/ridl.jar;%SO_CLASSPATHPATH%/juh.jar com.sun.star.xml.security.uno.TestTool %WS_PATH%/xmlsecurity/tools/cryptoken/jks/testToken.jks ""
 rm ./current.gif
 
