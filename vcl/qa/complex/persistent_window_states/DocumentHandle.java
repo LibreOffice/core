b60b5d8330cf2f8527e6d16392bd51adc2288cc6
Rüdiger Timm <rt@openoffice.org>
2005-03-30 08:04:15 +0000

    INTEGRATION: CWS vcl38 (1.1.2); FILE ADDED 2005/03/22 15:43:38 sg 1.1.2.1: #i44325#NEW: initial version

---
+++ vcl/qa/complex/persistent_window_states/DocumentHandle.java
@@ -0,0 +1,203 @@
+/*************************************************************************
+ *
+ *  $RCSfile: DocumentHandle.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Date: 2005-03-30 09:04:15 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+package complex.persistent_window_states;
+
+
+import com.sun.star.awt.Rectangle;
+import com.sun.star.awt.PosSize;
+import com.sun.star.frame.XComponentLoader;
+import com.sun.star.lang.XComponent;
+import com.sun.star.awt.XWindow;
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.PropertyState;
+import com.sun.star.frame.XController;
+import com.sun.star.frame.FrameSearchFlag;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.frame.XFrame;
+import com.sun.star.frame.FrameSearchFlag;
+import com.sun.star.frame.XFramesSupplier;
+import helper.WindowListener;
+
+/**
+ * Load and resize a document.
+ *
+ */
+public class DocumentHandle {
+    // the component loader to load a document
+    XComponentLoader xCompLoader = null;
+    // the document
+    XComponent xComp = null;
+    // the current window
+    XWindow xWin = null;
+    // a own window listener
+    WindowListener wl = null;
+
+    /**
+     * Constructor
+     * @param xComponentLoader A loader to load a document
+     */
+    public DocumentHandle(XComponentLoader xCompLoader) {
+        this.xCompLoader = xCompLoader;
+        wl = new WindowListener();
+    }
+
+    /**
+     * Load/Create a document.
+     * @param docName The name of a document as file URL
+     * @param hidden If true, the document is loaded hidden.
+     * @return The size of the opened/created document.
+     */
+    public Rectangle loadDocument(String docName, boolean hidden)
+                                                            throws Exception{
+        wl.resetTrigger();
+        try {
+            PropertyValue [] szArgs = null;
+            if (hidden) {
+                szArgs = new PropertyValue [1];
+                PropertyValue Arg = new PropertyValue();
+                Arg.Name = "Hidden";
+                Arg.Value = hidden?"True":"False";
+                Arg.Handle = -1;
+                Arg.State = PropertyState.DEFAULT_VALUE;
+                szArgs[0] = Arg;
+            }
+            else {
+                szArgs = new PropertyValue [0];
+            }
+
+            // get the current active window
+            XFrame xCurFrame = (XFrame)UnoRuntime.queryInterface(XFrame.class, xCompLoader);
+
+            // create a new frame
+            XFrame xFrame = xCurFrame.findFrame("_blank", FrameSearchFlag.CREATE);
+
+            // load document in this frame
+            XComponentLoader xFrameLoader = (XComponentLoader)UnoRuntime.queryInterface(XComponentLoader.class, xFrame);
+            xComp = xFrameLoader.loadComponentFromURL(
+                                                docName, "_self", 0, szArgs);
+            // wait for the document to load.
+            try {
+                Thread.sleep(10000);
+            }
+            catch(java.lang.InterruptedException e) {}
+
+            xWin = xFrame.getContainerWindow();
+            xWin.addWindowListener(wl);
+        }
+        catch(com.sun.star.io.IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        catch(com.sun.star.lang.IllegalArgumentException e) {
+            e.printStackTrace();
+            return null;
+        }
+        catch(java.lang.Exception e) {
+            System.out.println("DH3");
+            e.printStackTrace();
+            throw e;
+        }
+        return xWin.getPosSize();
+
+    }
+
+    /**
+     * Get the size of the current window.
+     * @return The size of the window as Rectangle.
+     */
+    public Rectangle getDocumentPosSize() {
+        return xWin.getPosSize();
+    }
+
+    /**
+     * Resize the window in defined steps:
+     * width -10 pixel;
+     * height -10 pixel;
+     * X-Position +10 pixel;
+     * Y-Position +10 pixel
+     * @return True if resize worked.
+     */
+    public boolean resizeDocument() {
+        Rectangle newPosSize = xWin.getPosSize();
+        newPosSize.Width = newPosSize.Width - 20;
+        newPosSize.Height = newPosSize.Height - 20;
+        newPosSize.X = newPosSize.X + 80;
+        newPosSize.Y = newPosSize.Y + 80;
+        return resizeDocument(newPosSize);
+    }
+
+    /**
+     * Resize window to the given Rectangle
+     * @param newPosSize The new position and size of the window.
+     * @return True if resize worked.
+     */
+    public boolean resizeDocument(Rectangle newPosSize){
+        wl.resetTrigger();
+        xWin.setPosSize(newPosSize.X, newPosSize.Y, newPosSize.Width,
+                                    newPosSize.Height, PosSize.POSSIZE);
+        try {
+            Thread.sleep(3000);
+        }
+        catch(java.lang.InterruptedException e) {}
+        return wl.resizedTrigger;
+    }
+}
