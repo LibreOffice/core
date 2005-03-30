915733a23975a7bbf1fca2812f1f4198d533c241
Rüdiger Timm <rt@openoffice.org>
2005-03-30 06:55:42 +0000

    INTEGRATION: CWS presfixes02 (1.4.2); FILE MERGED 2005/03/14 16:12:25 thb 1.4.2.1: #i36914# #i37785# #i41477# #i43486# Merge from prefixes01 patches

--- slideshow/source/engine/rehearsetimingsactivity.cxx
+++ slideshow/source/engine/rehearsetimingsactivity.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: rehearsetimingsactivity.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-10 13:43:34 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 07:55:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -77,7 +77,6 @@
 #include <algorithm>
 
 
-using namespace ::com::sun::star;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 
