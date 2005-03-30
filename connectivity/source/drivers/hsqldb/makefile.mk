cda1a8d1d5cfbd36985e341de84a3344ea5963fd
Rüdiger Timm <rt@openoffice.org>
2005-03-30 10:52:49 +0000

    INTEGRATION: CWS dba28 (1.5.22); FILE MERGED 2005/03/23 14:56:16 fs 1.5.22.1: #i45314# +accesslog for tracing file operations in debug mode

--- connectivity/source/drivers/hsqldb/makefile.mk
+++ connectivity/source/drivers/hsqldb/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: vg $ $Date: 2005-02-17 10:15:18 $
+#   last change: $Author: rt $ $Date: 2005-03-30 11:52:49 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -87,7 +87,8 @@ SLOFILES=\
         $(SLO)$/StorageNativeOutputStream.obj	\
         $(SLO)$/StorageNativeInputStream.obj	\
         $(SLO)$/StorageFileAccess.obj			\
-        $(SLO)$/Hservices.obj
+        $(SLO)$/Hservices.obj                   \
+        $(SLO)$/accesslog.obj
         
 
 .IF "$(OS)"=="MACOSX"
