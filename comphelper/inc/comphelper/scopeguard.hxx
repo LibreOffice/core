770d25feee8b18a0af3c0554251ee921da8c72fe
Rüdiger Timm <rt@openoffice.org>
2005-03-30 07:17:58 +0000

    INTEGRATION: CWS presfixes02 (1.3.2); FILE MERGED 2005/03/17 09:31:35 dbo 1.3.2.1: #i39513# modified scopeguard exception handling switch, optional generators

--- comphelper/inc/comphelper/scopeguard.hxx
+++ comphelper/inc/comphelper/scopeguard.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: scopeguard.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-03-14 09:22:02 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 08:17:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,29 +62,31 @@
 #if ! defined(INCLUDED_COMPHELPER_SCOPEGUARD_HXX)
 #define INCLUDED_COMPHELPER_SCOPEGUARD_HXX
 
-#include "boost/function.hpp"
-#include "boost/noncopyable.hpp"
-
-#ifndef INCLUDED_COMPHELPERDLLAPI_H
+#if ! defined(INCLUDED_COMPHELPERDLLAPI_H)
 #include "comphelper/comphelperdllapi.h"
 #endif
+#include "boost/function.hpp"
+#include "boost/noncopyable.hpp"
 
 namespace comphelper {
 
 /** ScopeGuard to ease writing exception-safe code.
  */
-class COMPHELPER_DLLPUBLIC ScopeGuard : private boost::noncopyable // noncopyable until we have
-                                              // good reasons...
+class COMPHELPER_DLLPUBLIC ScopeGuard : private ::boost::noncopyable
+                                        // noncopyable until we have
+                                        // good reasons...
 {
 public:
+    enum exc_handling { IGNORE_EXCEPTIONS, ALLOW_EXCEPTIONS };
+
     /** @param func function object to be executed in dtor
-        @param bIgnoreExceptions if true (default),
-                                 thrown exceptions in dtor will be
-                                 silently ignored (but OSL_ asserted)
+        @param excHandling switches whether thrown exceptions in dtor will be
+                           silently ignored (but OSL_ asserted)
     */
-    template <typename FuncT>
-    explicit ScopeGuard( FuncT func, bool bIgnoreExceptions = true )
-        : m_func(func), m_bIgnoreExceptions(bIgnoreExceptions) {}
+    template <typename func_type>
+    explicit ScopeGuard( func_type const & func,
+                         exc_handling excHandling = IGNORE_EXCEPTIONS )
+        : m_func( func ), m_excHandling( excHandling ) {}
 
     ~ScopeGuard();
 
@@ -94,11 +96,11 @@ public:
     void dismiss();
 
 private:
-    ::boost::function0<void> m_func;
-    const bool m_bIgnoreExceptions;
+    ::boost::function0<void> m_func; // preferring portable syntax
+    exc_handling const m_excHandling;
 };
 
 } // namespace comphelper
 
-#endif
+#endif // ! defined(INCLUDED_COMPHELPER_SCOPEGUARD_HXX)
 
