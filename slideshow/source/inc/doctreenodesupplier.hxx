da8530e1410362df32b56c3d33165d8604b779b3
Rüdiger Timm <rt@openoffice.org>
2005-03-30 07:14:32 +0000

    INTEGRATION: CWS presfixes02 (1.1.2); FILE ADDED 2005/03/15 00:38:18 thb 1.1.2.1: #i41839# Reworked shape subsetting and DocTreeNode stuff, to properly support BiDi/CTL text

---
+++ slideshow/source/inc/doctreenodesupplier.hxx
@@ -0,0 +1,185 @@
+/*************************************************************************
+ *
+ *  $RCSfile: doctreenodesupplier.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-03-30 08:14:32 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SLIDESHOW_DOCTREENODESUPPLIER_HXX
+#define _SLIDESHOW_DOCTREENODESUPPLIER_HXX
+
+#include <doctreenode.hxx>
+
+
+/* Definition of DocTreeNodeSupplier interface */
+
+namespace presentation
+{
+    namespace internal
+    {
+        /** Interface to retrieve DocTreeNodes from subsettable
+            shapes.
+
+            Shapes which implement the AttributableShape interface
+            also provides this interface, providing methods to
+            retrieve specific DocTreeNode objects from the shape. The
+            methods mainly distinguish various ways on how to specify
+            the actual DocTreeNode to return.
+
+            If a requested DocTreeNode is not available when one of
+            the methods below is called, an empty DocTreeNode will be
+            returned (the predicate DocTreeNode::isEmpty() will return
+            true). If, on the other hand, the shape cannot determine,
+            for internal reasons, the internal tree node structure,
+            all those methods will throw an
+            ImportFailedException. This is, in fact, a delayed error
+            that could also have been reported during shape
+            construction, but might be postponed until the missing
+            information is actually requested.
+         */
+        class DocTreeNodeSupplier
+        {
+        public:
+            /** Query number of tree nodes of the given type this
+                shape contains.
+
+                The value returned by this method minus one is the
+                maximum value permissible at the getTreeNode()
+                method, for the given node type.
+
+                @throws ImportFailedException, if tree node structure
+                cannot be determined.
+             */
+            virtual sal_Int32 getNumberOfTreeNodes( DocTreeNode::NodeType eNodeType ) const = 0; // throw ImportFailedException;
+
+            /** Create DocTreeNode from shape.
+
+                This method creates a DocTreeNode from a shape, a
+                given node type and a running index into the shape's
+                DocTreeNodes of the given type.
+
+                @param nNodeIndex
+                Starting with 0, every DocTreeNode of the shape that
+                has type eNodeType is indexed. The DocTreeNode whose
+                index equals nNodeIndex will be returned.
+
+                @param eNodeType
+                Type of the node to return
+
+                @return the DocTreeNode found, or the empty
+                DocTreeNode, if nothing was found.
+
+                @throws ImportFailedException, if tree node structure
+                cannot be determined.
+            */
+            virtual DocTreeNode getTreeNode( sal_Int32              nNodeIndex,
+                                             DocTreeNode::NodeType  eNodeType ) const = 0; // throw ImportFailedException;
+
+            /** Query number of tree nodes of the given type this
+                subset contains.
+
+                The value returned by this method minus one is the
+                maximum value permissible at the
+                getSubsetTreeNode() method, for the given node
+                type.
+
+                @param rParentNode
+                The parent node, below which the number of tree nodes
+                of the given type shall be counted.
+
+                @param eNodeType
+                Node type to count.
+
+                @throws ImportFailedException, if tree node structure
+                cannot be determined.
+             */
+            virtual sal_Int32 getNumberOfSubsetTreeNodes( const DocTreeNode&    rParentNode,
+                                                          DocTreeNode::NodeType eNodeType ) const = 0; // throw ImportFailedException;
+
+            /** Create DocTreeNode from shape subset.
+
+                This method creates a DocTreeNode from a shape, a
+                parent tree node, a given node type and a running
+                index into the shape's DocTreeNodes of the given type.
+
+                @param rParentNode
+                Parent node, below which the tree node with the given
+                type shall be selected.
+
+                @param nNodeIndex
+                Starting with 0, every DocTreeNode of the shape that
+                has type eNodeType is indexed. The DocTreeNode whose
+                index equals nNodeIndex will be returned.
+
+                @param eNodeType
+                Type of the node to return
+
+                @return the DocTreeNode found, or the empty
+                DocTreeNode, if nothing was found.
+
+                @throws ImportFailedException, if tree node structure
+                cannot be determined.
+            */
+            virtual DocTreeNode getSubsetTreeNode( const DocTreeNode&       rParentNode,
+                                                   sal_Int32                nNodeIndex,
+                                                   DocTreeNode::NodeType    eNodeType ) const = 0; // throw ImportFailedException;
+        };
+
+    }
+}
+
+#endif /* _SLIDESHOW_DOCTREENODESUPPLIER_HXX */
