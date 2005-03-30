07a6df809b355296588a0fd565a1f8219b507a04
Rüdiger Timm <rt@openoffice.org>
2005-03-30 10:42:16 +0000

    INTEGRATION: CWS systemmozilla (1.5.70); FILE MERGED 2005/01/06 18:58:33 rene 1.5.70.1: #i17187# use system mozilla ... (patch)

--- extensions/source/plugin/base/makefile.mk
+++ extensions/source/plugin/base/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2004-09-08 14:04:32 $
+#   last change: $Author: rt $ $Date: 2005-03-30 11:42:16 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,11 +65,6 @@ PRJNAME=plugin
 TARGET=plbase
 TARGETTYPE=GUI
 ENABLE_EXCEPTIONS=TRUE
-
-.IF "$(WITH_MOZILLA)" == "NO"
-dummy:
-        @echo "mozilla disabled"
-.ENDIF
 
 .INCLUDE :  ..$/util$/makefile.pmk
 
