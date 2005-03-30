5f0e9040a1abd9bf73a8383b886b668822bc215d
Rüdiger Timm <rt@openoffice.org>
2005-03-30 06:41:06 +0000

    INTEGRATION: CWS presfixes02 (1.3.2); FILE MERGED 2005/03/15 15:32:54 mbu 1.3.2.1: added polypolygon triangulation

--- basegfx/inc/basegfx/tools/canvastools.hxx
+++ basegfx/inc/basegfx/tools/canvastools.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: canvastools.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-10 13:37:32 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 07:41:06 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
