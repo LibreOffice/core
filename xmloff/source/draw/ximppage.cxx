44f517a1ba7fcdbbf0a889a5b04af72825995b88
Rüdiger Timm <rt@openoffice.org>
2005-03-30 09:28:04 +0000

    INTEGRATION: CWS impress40 (1.13.136); FILE MERGED 2005/03/22 12:14:11 cl 1.13.136.1: #b6236864# fixed header&footer

--- xmloff/source/draw/ximppage.cxx
+++ xmloff/source/draw/ximppage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ximppage.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2004-11-03 16:41:04 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 10:28:04 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -233,8 +233,7 @@ void SdXMLGenericPageContext::EndElement()
 
                     if( bFixed )
                     {
-                        xSet->setPropertyValue( aStrDateTimeTextProp,
-                                            makeAny( GetSdImport().GetFooterDecl( maUseFooterDeclName ) ) );
+                        xSet->setPropertyValue( aStrDateTimeTextProp, makeAny( aText ) );
                     }
                     else if( aDateTimeFormat.getLength() )
                     {
