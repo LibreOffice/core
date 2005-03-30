55ea73a4d64418ede3eb7ace8316fc5a7c139e44
Rüdiger Timm <rt@openoffice.org>
2005-03-30 07:07:45 +0000

    INTEGRATION: CWS presfixes02 (1.2.8); FILE MERGED 2005/03/21 17:37:16 dbo 1.2.8.1: #i41476# schedule deactivation of parent node if no children are available Issue number: Submitted by: Reviewed by:

--- slideshow/source/engine/animationnodes/sequentialtimecontainer.cxx
+++ slideshow/source/engine/animationnodes/sequentialtimecontainer.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sequentialtimecontainer.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-21 17:07:44 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 08:07:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -93,7 +93,7 @@ namespace presentation
             if( getState() == ACTIVE )
                 return true; // avoid duplicate event generation
 
-            if( !BaseNode::activate() )
+            if( !BaseContainerNode::activate() )
                 return false;
 
             // resolve first child
