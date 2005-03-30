a5706d26458cf59454a9910c6aac2d93b7d94e70
Rüdiger Timm <rt@openoffice.org>
2005-03-30 07:35:49 +0000

    INTEGRATION: CWS presfixes02 (1.4.2); FILE MERGED 2005/03/14 16:04:59 thb 1.4.2.1: #i35136# #i36914# #i41113# #i44100# #i40115# #i41839# #i44404# Merge from presfixes01 patches

--- cppcanvas/source/wrapper/vclfactory.cxx
+++ cppcanvas/source/wrapper/vclfactory.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: vclfactory.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-10 13:32:58 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 08:35:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -94,7 +94,6 @@
 #include <implsprite.hxx>
 
 
-using namespace ::com::sun::star;
 using namespace ::com::sun::star;
 
 namespace cppcanvas
