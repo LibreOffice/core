bfd61ef81b21a5fcf418bac3894a568e2b007dcc
Rüdiger Timm <rt@openoffice.org>
2005-03-30 06:45:22 +0000

    INTEGRATION: CWS presfixes02 (1.3.2); FILE MERGED 2005/03/15 15:33:13 mbu 1.3.2.1: added polypolygon triangulation

--- basegfx/source/vector/b2ivector.cxx
+++ basegfx/source/vector/b2ivector.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: b2ivector.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-10 13:39:13 $
+ *  last change: $Author: rt $ $Date: 2005-03-30 07:45:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -69,9 +69,6 @@
 
 #ifndef _BGFX_NUMERIC_FTOOLS_HXX
 #include <basegfx/numeric/ftools.hxx>
-#endif
-#ifndef _BGFX_VECTOR_B2DVECTOR_HXX
-#include <basegfx/vector/b2dvector.hxx>
 #endif
 
 namespace basegfx
