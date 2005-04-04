725c491a6b430587ab218bf9152b1eda86299a2f
Rüdiger Timm <rt@openoffice.org>
2005-04-04 07:13:19 +0000

    INTEGRATION: CWS tl07 (1.1.2); FILE ADDED 2005/01/19 15:38:12 tl 1.1.2.1: #i37058# text conversion: improvments for secondary Chinese languages

---
+++ sw/inc/splargs.hxx
@@ -0,0 +1,217 @@
+/*************************************************************************
+ *
+ *  $RCSfile: splargs.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-04-04 08:13:19 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+#ifndef _SPLARGS_HXX
+#define _SPLARGS_HXX
+
+#ifndef _SOLAR_H
+#include <tools/solar.h>
+#endif
+#ifndef _GEN_HXX //autogen
+#include <tools/gen.hxx>
+#endif
+#include <limits.h>       // USHRT_MAX
+#ifndef _STRING_HXX //autogen
+#include <tools/string.hxx>
+#endif
+
+class SwTxtNode;
+class SwIndex;
+class SpellCheck;
+
+#ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLALTERNATIVES_HPP_
+#include <com/sun/star/linguistic2/XSpellAlternatives.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLCHECKER1_HPP_
+#include <com/sun/star/linguistic2/XSpellChecker1.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LINGUISTIC2_XHYPHENATEDWORD_HPP_
+#include <com/sun/star/linguistic2/XHyphenatedWord.hpp>
+#endif
+
+/*************************************************************************
+ *                      struct SwArgsBase
+ *************************************************************************/
+
+
+struct SwArgsBase     // used for text conversion (Hangul/Hanja, ...)
+{
+    SwTxtNode  *pStartNode;
+    SwIndex    *pStartIdx;
+    SwTxtNode  *pEndNode;
+    SwIndex    *pEndIdx;
+
+    SwArgsBase(
+            SwTxtNode* pStart, SwIndex& rStart,
+            SwTxtNode* pEnd, SwIndex& rEnd )
+        : pStartNode( pStart ), pStartIdx( &rStart ),
+          pEndNode( pEnd ), pEndIdx( &rEnd )
+        {}
+
+    void SetStart(SwTxtNode* pStart, SwIndex& rStart )
+    {
+        pStartNode = pStart;    pStartIdx = &rStart ;
+    }
+
+    void SetEnd( SwTxtNode* pEnd, SwIndex& rEnd  )
+    {
+        pEndNode = pEnd;        pEndIdx = &rEnd ;
+    }
+};
+
+/*************************************************************************
+ *                      struct SwConversionArgs
+ * used for text conversion (Hangul/Hanja, Simplified/Traditional Chinese, ...)
+ *************************************************************************/
+
+struct SwConversionArgs : SwArgsBase
+{
+    rtl::OUString   aConvText;
+    LanguageType    nConvSrcLang;       // (source) language to look for
+    LanguageType    nConvTextLang;      // language of aConvText if that one was found
+
+    SwConversionArgs( LanguageType nLang,
+            SwTxtNode* pStart, SwIndex& rStart,
+            SwTxtNode* pEnd, SwIndex& rEnd )
+        : SwArgsBase( pStart, rStart, pEnd, rEnd ),
+          nConvSrcLang( nLang ),
+          nConvTextLang( LANGUAGE_NONE )
+        {}
+};
+
+/*************************************************************************
+ *                      struct SwSpellArgs
+ *************************************************************************/
+
+struct SwSpellArgs : SwArgsBase
+{
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::linguistic2::XSpellChecker1 >     xSpeller;
+
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::linguistic2::XSpellAlternatives > xSpellAlt;
+
+    SwSpellArgs(::com::sun::star::uno::Reference<
+            ::com::sun::star::linguistic2::XSpellChecker1 > &rxSplChk,
+            SwTxtNode* pStart, SwIndex& rStart,
+            SwTxtNode* pEnd, SwIndex& rEnd )
+        :   SwArgsBase( pStart, rStart, pEnd, rEnd ),
+            xSpeller( rxSplChk )
+        {}
+};
+
+/*************************************************************************
+ *                      class SwInterHyphInfo
+ *************************************************************************/
+
+// Parameter-Klasse fuer Hyphenate
+// docedt.cxx:  SwDoc::Hyphenate()
+// txtedt.cxx:  SwTxtNode::Hyphenate()
+// txthyph.cxx: SwTxtFrm::Hyphenate()
+
+class SwInterHyphInfo
+{
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::linguistic2::XHyphenatedWord >    xHyphWord;
+    const   Point aCrsrPos;
+    sal_Bool    bAuto   : 1;
+    sal_Bool    bNoLang : 1;
+    sal_Bool    bCheck  : 1;
+public:
+    xub_StrLen nStart;
+    xub_StrLen nLen;
+    xub_StrLen nWordStart;
+    xub_StrLen nWordLen;
+    xub_StrLen nHyphPos;
+    sal_uInt16 nMinTrail;
+
+    inline SwInterHyphInfo( const Point &rCrsrPos,
+                            const sal_uInt16 nStart = 0,
+                            const sal_uInt16 nLen = USHRT_MAX )
+         : aCrsrPos( rCrsrPos ),
+           bAuto(sal_False), bNoLang(sal_False), bCheck(sal_False),
+           nStart(nStart), nLen(nLen),
+           nWordStart(0), nWordLen(0),
+           nHyphPos(0), nMinTrail(0)
+         { }
+    inline xub_StrLen GetStart() const { return nStart; }
+    inline xub_StrLen GetLen() const { return nLen; }
+    inline xub_StrLen GetEnd() const
+    { return STRING_LEN == nLen ? nLen : nStart + nLen; }
+    inline const Point *GetCrsrPos() const
+    { return aCrsrPos.X() || aCrsrPos.Y() ? &aCrsrPos : 0; }
+    inline sal_Bool IsAuto() const { return bAuto; }
+    inline void SetAuto( const sal_Bool bNew ) { bAuto = bNew; }
+    inline sal_Bool IsCheck() const { return bCheck; }
+    inline void SetCheck( const sal_Bool bNew ) { bCheck = bNew; }
+    inline sal_Bool IsNoLang() const { return bNoLang; }
+    inline void SetNoLang( const sal_Bool bNew ) { bNoLang = bNew; }
+
+    inline void
+            SetHyphWord(const ::com::sun::star::uno::Reference<
+                ::com::sun::star::linguistic2::XHyphenatedWord >  &rxHW)
+            { xHyphWord = rxHW; }
+    inline ::com::sun::star::uno::Reference<
+        ::com::sun::star::linguistic2::XHyphenatedWord >
+            GetHyphWord() { return xHyphWord; }
+};
+
+
+#endif
