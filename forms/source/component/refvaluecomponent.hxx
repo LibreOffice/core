529b8219db4e0f0d40a2f6b5fe52d619c3a68281
Rüdiger Timm <rt@openoffice.org>
2005-04-04 06:46:45 +0000

    INTEGRATION: CWS gccfour (1.2.18); FILE MERGED 2005/03/11 20:56:55 cmc 1.2.18.1: #i44718# name enums that are compared against ints

--- forms/source/component/refvaluecomponent.hxx
+++ forms/source/component/refvaluecomponent.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: refvaluecomponent.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-23 11:31:59 $
+ *  last change: $Author: rt $ $Date: 2005-04-04 07:46:45 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,7 +74,7 @@ namespace frm
 {
 //........................................................................
 
-    enum { STATE_NOCHECK, STATE_CHECK, STATE_DONTKNOW };
+    enum state { STATE_NOCHECK, STATE_CHECK, STATE_DONTKNOW };
 
     //====================================================================
     //= OReferenceValueComponent
