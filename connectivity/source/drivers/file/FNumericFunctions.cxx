8069587f07f036a09570ffed2ab1e53f7392314d
Rüdiger Timm <rt@openoffice.org>
2005-04-04 06:37:22 +0000

    INTEGRATION: CWS gccfour (1.3.104); FILE MERGED 2005/03/11 10:44:18 cmc 1.3.104.1: #i44718# move cmath include to keep gcc 4.0.0 happy

--- connectivity/source/drivers/file/FNumericFunctions.cxx
+++ connectivity/source/drivers/file/FNumericFunctions.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: FNumericFunctions.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-03-15 12:46:43 $
+ *  last change: $Author: rt $ $Date: 2005-04-04 07:37:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,8 +59,8 @@
  *
  ************************************************************************/
 
-#include "file/FNumericFunctions.hxx"
 #include <cmath>
+#include "file/FNumericFunctions.hxx"
 #include <rtl/math.hxx>
 
 using namespace connectivity;
