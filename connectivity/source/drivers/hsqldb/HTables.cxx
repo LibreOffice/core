04d7cc0461639475f738f78ac5b775acc2702094
Jens-Heiner Rechtien <hr@openoffice.org>
2005-04-06 09:35:50 +0000

    INTEGRATION: CWS hsqldb4 (1.1.2); FILE ADDED 2005/03/31 11:55:12 fs 1.1.2.1: #i41785# (provided by OJ) implementing an SDBCX driver

---
+++ connectivity/source/drivers/hsqldb/HTables.cxx
@@ -0,0 +1,290 @@
+/*************************************************************************
+ *
+ *  $RCSfile: HTables.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2005-04-06 10:35:50 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef CONNECTIVITY_HSQLDB_TABLES_HXX
+#include "hsqldb/HTables.hxx"
+#endif
+#ifndef _CONNECTIVITY_HSQLDB_VIEWS_HXX_
+#include "hsqldb/HViews.hxx"
+#endif
+#ifndef CONNECTIVITY_HSQLDB_TABLE_HXX
+#include "hsqldb/HTable.hxx"
+#endif
+#ifndef _COM_SUN_STAR_SDBC_XROW_HPP_
+#include <com/sun/star/sdbc/XRow.hpp>
+#endif
+#ifndef _COM_SUN_STAR_SDBC_XRESULTSET_HPP_
+#include <com/sun/star/sdbc/XResultSet.hpp>
+#endif
+#ifndef _COM_SUN_STAR_SDBC_COLUMNVALUE_HPP_
+#include <com/sun/star/sdbc/ColumnValue.hpp>
+#endif
+#ifndef _COM_SUN_STAR_SDBCX_PRIVILEGE_HPP_
+#include <com/sun/star/sdbcx/Privilege.hpp>
+#endif
+#ifndef _COM_SUN_STAR_SDBC_KEYRULE_HPP_
+#include <com/sun/star/sdbc/KeyRule.hpp>
+#endif
+#ifndef _COM_SUN_STAR_SDBCX_KEYTYPE_HPP_
+#include <com/sun/star/sdbcx/KeyType.hpp>
+#endif
+#ifndef CONNECTIVITY_HSQLDB_CATALOG_HXX
+#include "hsqldb/HCatalog.hxx"
+#endif
+#ifndef _COMPHELPER_EXTRACT_HXX_
+#include <comphelper/extract.hxx>
+#endif
+#ifndef _CONNECTIVITY_DBTOOLS_HXX_
+#include "connectivity/dbtools.hxx"
+#endif
+#ifndef _DBHELPER_DBEXCEPTION_HXX_
+#include "connectivity/dbexception.hxx"
+#endif
+#ifndef _CPPUHELPER_INTERFACECONTAINER_H_
+#include <cppuhelper/interfacecontainer.h>
+#endif
+#ifndef _COMPHELPER_TYPES_HXX_
+#include <comphelper/types.hxx>
+#endif
+#ifndef CONNECTIVITY_CONNECTION_HXX
+#include "TConnection.hxx"
+#endif
+
+using namespace ::comphelper;
+using namespace connectivity;
+using namespace ::cppu;
+using namespace connectivity::hsqldb;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::sdbcx;
+using namespace ::com::sun::star::sdbc;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::lang;
+using namespace dbtools;
+typedef connectivity::sdbcx::OCollection OCollection_TYPE;
+
+sdbcx::ObjectType OTables::createObject(const ::rtl::OUString& _rName)
+{
+    ::rtl::OUString sCatalog,sSchema,sTable;
+    ::dbtools::qualifiedNameComponents(m_xMetaData,_rName,sCatalog,sSchema,sTable,::dbtools::eInDataManipulation);
+
+    static const ::rtl::OUString s_sTableTypeView(RTL_CONSTASCII_USTRINGPARAM("VIEW"));
+    static const ::rtl::OUString s_sTableTypeTable(RTL_CONSTASCII_USTRINGPARAM("TABLE"));
+    static const ::rtl::OUString s_sAll(RTL_CONSTASCII_USTRINGPARAM("%"));
+
+    Sequence< ::rtl::OUString > sTableTypes(3);
+    sTableTypes[0] = s_sTableTypeView;
+    sTableTypes[1] = s_sTableTypeTable;
+    sTableTypes[2] = s_sAll;    // just to be sure to include anything else ....
+
+    Any aCatalog;
+    if ( sCatalog.getLength() )
+        aCatalog <<= sCatalog;
+    Reference< XResultSet > xResult = m_xMetaData->getTables(aCatalog,sSchema,sTable,sTableTypes);
+
+    sdbcx::ObjectType xRet = NULL;
+    if ( xResult.is() )
+    {
+        Reference< XRow > xRow(xResult,UNO_QUERY);
+        if ( xResult->next() ) // there can be only one table with this name
+        {
+//          Reference<XStatement> xStmt = m_xConnection->createStatement();
+//          if ( xStmt.is() )
+//          {
+//              Reference< XResultSet > xPrivRes = xStmt->executeQuery();
+//              Reference< XRow > xPrivRow(xPrivRes,UNO_QUERY);
+//              while ( xPrivRes.is() && xPrivRes->next() )
+//              {
+//                  if ( xPrivRow->getString(1) )
+//                  {
+//                  }
+//              }
+//          }
+            sal_Int32 nPrivileges = Privilege::DROP         |
+                                    Privilege::REFERENCE    |
+                                    Privilege::ALTER        |
+                                    Privilege::CREATE       |
+                                    Privilege::READ         |
+                                    Privilege::DELETE       |
+                                    Privilege::UPDATE       |
+                                    Privilege::INSERT       |
+                                    Privilege::SELECT;
+
+            OHSQLTable* pRet = new OHSQLTable( this
+                                                ,static_cast<OHCatalog&>(m_rParent).getConnection()
+                                                ,sTable
+                                                ,xRow->getString(4)
+                                                ,xRow->getString(5)
+                                                ,sSchema
+                                                ,sCatalog
+                                                ,nPrivileges);
+            xRet = pRet;
+        }
+        ::comphelper::disposeComponent(xResult);
+    }
+
+    return xRet;
+}
+// -------------------------------------------------------------------------
+void OTables::impl_refresh(  ) throw(RuntimeException)
+{
+    static_cast<OHCatalog&>(m_rParent).refreshTables();
+}
+// -------------------------------------------------------------------------
+void OTables::disposing(void)
+{
+    m_xMetaData = NULL;
+    OCollection::disposing();
+}
+// -------------------------------------------------------------------------
+Reference< XPropertySet > OTables::createEmptyObject()
+{
+    return new OHSQLTable(this,static_cast<OHCatalog&>(m_rParent).getConnection());
+}
+// -------------------------------------------------------------------------
+// XAppend
+void OTables::appendObject( const Reference< XPropertySet >& descriptor )
+{
+    ::rtl::OUString aName = getString(descriptor->getPropertyValue(OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_NAME)));
+    if(!aName.getLength())
+        ::dbtools::throwFunctionSequenceException(static_cast<XTypeProvider*>(this));
+
+    createTable(descriptor);
+}
+// -------------------------------------------------------------------------
+// XDrop
+void OTables::dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName)
+{
+    Reference< ::com::sun::star::lang::XUnoTunnel> xTunnel(getObject(_nPos),UNO_QUERY);
+    sal_Bool bIsNew = sal_False;
+    if(xTunnel.is())
+    {
+        connectivity::sdbcx::ODescriptor* pTable = (connectivity::sdbcx::ODescriptor*)xTunnel->getSomething(connectivity::sdbcx::ODescriptor::getUnoTunnelImplementationId());
+        if(pTable)
+            bIsNew = pTable->isNew();
+    }
+    if (!bIsNew)
+    {
+        Reference< XConnection > xConnection = static_cast<OHCatalog&>(m_rParent).getConnection();
+
+
+        ::rtl::OUString sCatalog,sSchema,sTable;
+        ::dbtools::qualifiedNameComponents(m_xMetaData,_sElementName,sCatalog,sSchema,sTable,::dbtools::eInDataManipulation);
+
+        ::rtl::OUString aSql = ::rtl::OUString::createFromAscii("DROP ");
+
+        Reference<XPropertySet> xProp(xTunnel,UNO_QUERY);
+        sal_Bool bIsView;
+        if(bIsView = (xProp.is() && ::comphelper::getString(xProp->getPropertyValue(OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_TYPE))) == ::rtl::OUString::createFromAscii("VIEW"))) // here we have a view
+            aSql += ::rtl::OUString::createFromAscii("VIEW ");
+        else
+            aSql += ::rtl::OUString::createFromAscii("TABLE ");
+
+        ::rtl::OUString sComposedName;
+        ::dbtools::composeTableName(m_xMetaData,sCatalog,sSchema,sTable,sComposedName,sal_True,::dbtools::eInDataManipulation);
+        aSql += sComposedName;
+        Reference< XStatement > xStmt = xConnection->createStatement(  );
+        if ( xStmt.is() )
+        {
+            xStmt->execute(aSql);
+            ::comphelper::disposeComponent(xStmt);
+        }
+        // if no exception was thrown we must delete it from the views
+        if ( bIsView )
+        {
+            OViews* pViews = static_cast<OViews*>(static_cast<OHCatalog&>(m_rParent).getPrivateViews());
+            if ( pViews && pViews->hasByName(_sElementName) )
+                pViews->dropByNameImpl(_sElementName);
+        }
+    }
+}
+// -------------------------------------------------------------------------
+void OTables::createTable( const Reference< XPropertySet >& descriptor )
+{
+    Reference< XConnection > xConnection = static_cast<OHCatalog&>(m_rParent).getConnection();
+    ::rtl::OUString aSql = ::dbtools::createSqlCreateTableStatement(descriptor,xConnection);
+
+    Reference< XStatement > xStmt = xConnection->createStatement(  );
+    if ( xStmt.is() )
+    {
+        xStmt->execute(aSql);
+        ::comphelper::disposeComponent(xStmt);
+    }
+}
+// -----------------------------------------------------------------------------
+void OTables::appendNew(const ::rtl::OUString& _rsNewTable)
+{
+    insertElement(_rsNewTable,NULL);
+
+    // notify our container listeners
+    ContainerEvent aEvent(static_cast<XContainer*>(this), makeAny(_rsNewTable), Any(), Any());
+    OInterfaceIteratorHelper aListenerLoop(m_aContainerListeners);
+    while (aListenerLoop.hasMoreElements())
+        static_cast<XContainerListener*>(aListenerLoop.next())->elementInserted(aEvent);
+}
+// -----------------------------------------------------------------------------
+::rtl::OUString OTables::getNameForObject(const sdbcx::ObjectType& _xObject)
+{
+    OSL_ENSURE(_xObject.is(),"OTables::getNameForObject: Object is NULL!");
+    return ::dbtools::composeTableName(m_xMetaData,_xObject,sal_False,::dbtools::eInDataManipulation);
+}
+// -----------------------------------------------------------------------------
+
