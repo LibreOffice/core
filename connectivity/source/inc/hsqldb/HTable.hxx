e6f243af728770f8e33c6be665c11bbfd1309ad1
Jens-Heiner Rechtien <hr@openoffice.org>
2005-04-06 09:38:35 +0000

    INTEGRATION: CWS hsqldb4 (1.1.2); FILE ADDED 2005/03/31 11:57:11 fs 1.1.2.1: #i41785# (provided by OJ) implementing an SDBCX driver

---
+++ connectivity/source/inc/hsqldb/HTable.hxx
@@ -0,0 +1,161 @@
+/*************************************************************************
+ *
+ *  $RCSfile: HTable.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2005-04-06 10:38:35 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef CONNECTIVITY_HSQLDB_TABLE_HXX
+#define CONNECTIVITY_HSQLDB_TABLE_HXX
+
+#ifndef CONNECTIVITY_TABLEHELPER_HXX
+#include "connectivity/TTableHelper.hxx"
+#endif
+#ifndef _COM_SUN_STAR_SDBC_XDATABASEMETADATA_HPP_
+#include <com/sun/star/sdbc/XDatabaseMetaData.hpp>
+#endif
+#ifndef COMPHELPER_IDPROPERTYARRAYUSAGEHELPER_HXX
+#include <comphelper/IdPropArrayHelper.hxx>
+#endif
+
+namespace connectivity
+{
+    namespace hsqldb
+    {
+
+        ::rtl::OUString getTypeString(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xColProp);
+
+        class OHSQLTable;
+        typedef ::comphelper::OIdPropertyArrayUsageHelper< OHSQLTable > OHSQLTable_PROP;
+        class OHSQLTable :  public OTableHelper
+                            ,public OHSQLTable_PROP
+        {
+            sal_Int32 m_nPrivileges; // we have to set our privileges by our own
+
+            /** executes the statmenmt.
+                @param  _rStatement
+                    The statement to execute.
+                */
+            void executeStatement(const ::rtl::OUString& _rStatement );
+        protected:
+
+            /** creates the column collection for the table
+                @param  _rNames
+                    The column names.
+            */
+            virtual sdbcx::OCollection* createColumns(const TStringVector& _rNames);
+
+            /** creates the key collection for the table
+                @param  _rNames
+                    The key names.
+            */
+            virtual sdbcx::OCollection* createKeys(const TStringVector& _rNames);
+
+            /** creates the index collection for the table
+                @param  _rNames
+                    The index names.
+            */
+            virtual sdbcx::OCollection* createIndexes(const TStringVector& _rNames);
+
+            /** used to implement the creation of the array helper which is shared amongst all instances of the class.
+                This method needs to be implemented in derived classes.
+                <BR>
+                The method gets called with s_aMutex acquired.
+                <BR>
+                as long as IPropertyArrayHelper has no virtual destructor, the implementation of ~OPropertyArrayUsageHelper
+                assumes that you created an ::cppu::OPropertyArrayHelper when deleting s_pProps.
+                @return                         an pointer to the newly created array helper. Must not be NULL.
+            */
+            virtual ::cppu::IPropertyArrayHelper* createArrayHelper(sal_Int32 nId) const;
+            virtual ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelper();
+
+        public:
+            OHSQLTable( sdbcx::OCollection* _pTables,
+                            const ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >& _xConnection);
+            OHSQLTable( sdbcx::OCollection* _pTables,
+                            const ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >& _xConnection,
+                            const ::rtl::OUString& _Name,
+                            const ::rtl::OUString& _Type,
+                            const ::rtl::OUString& _Description = ::rtl::OUString(),
+                            const ::rtl::OUString& _SchemaName = ::rtl::OUString(),
+                            const ::rtl::OUString& _CatalogName = ::rtl::OUString(),
+                            sal_Int32 _nPrivileges = 0
+                );
+
+            // ODescriptor
+            virtual void construct();
+            // com::sun::star::lang::XUnoTunnel
+            virtual sal_Int64 SAL_CALL getSomething( const ::com::sun::star::uno::Sequence< sal_Int8 >& aIdentifier ) throw(::com::sun::star::uno::RuntimeException);
+            static ::com::sun::star::uno::Sequence< sal_Int8 > getUnoTunnelImplementationId();
+
+            // XAlterTable
+            virtual void SAL_CALL alterColumnByName( const ::rtl::OUString& colName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor ) throw(::com::sun::star::sdbc::SQLException, ::com::sun::star::container::NoSuchElementException, ::com::sun::star::uno::RuntimeException);
+            /**
+                returns the ALTER TABLE XXX COLUMN statement
+            */
+            ::rtl::OUString getAlterTableColumnPart();
+
+            // some methods to alter table structures
+            void alterColumnType(sal_Int32 nNewType,const ::rtl::OUString& _rColName,const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
+            void alterDefaultValue(const ::rtl::OUString& _sNewDefault,const ::rtl::OUString& _rColName);
+            void dropDefaultValue(const ::rtl::OUString& _sNewDefault);
+
+        };
+    }
+}
+#endif // CONNECTIVITY_HSQLDB_TABLE_HXX
+
