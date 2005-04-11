99a22186e43ccc7e350f076827102787e7073645
Jens-Heiner Rechtien <hr@openoffice.org>
2005-04-11 07:41:25 +0000

    INTEGRATION: CWS macosx05 (1.18.6); FILE MERGED 2005/04/10 07:35:52 maho 1.18.6.1: enable mozilla connectivity stuff Issue number:  46546 Reviewed by:   pjanik

--- connectivity/source/drivers/mozab/makefile.mk
+++ connectivity/source/drivers/mozab/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: rt $ $Date: 2005-04-01 16:25:30 $
+#   last change: $Author: hr $ $Date: 2005-04-11 08:41:25 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -65,7 +65,7 @@ PRJNAME=connectivity
 TARGET=$(MOZAB_TARGET)
 TARGET2=$(MOZAB_TARGET)drv
 
-.IF  "$(OS)"=="MACOSX" || ( "$(SYSTEM_MOZILLA)" == "YES" && "$(WITH_MOZILLA)" == "YES") || "$(WITH_MOZILLA)" == "NO"
+.IF ( "$(SYSTEM_MOZILLA)" == "YES" && "$(WITH_MOZILLA)" == "YES") || "$(WITH_MOZILLA)" == "NO"
 all: 
     @echo "		Not building the mozabsrc stuff in OpenOffice.org build"
     @echo "		dependency to Mozilla developer snapshots not feasable at the moment"
