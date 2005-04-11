1ce001d7c9ea45452108b634d1ccd79bc1b0fa63
Jens-Heiner Rechtien <hr@openoffice.org>
2005-04-11 08:32:00 +0000

    INTEGRATION: CWS systemdb (1.2.2); FILE MERGED 2005/03/31 08:28:27 rene 1.2.2.1: fix build with system db, remove system-db3 flags, add system-db #i34430# copy db.jar into the tree

--- xmlhelp/source/cxxhelp/provider/db.hxx
+++ xmlhelp/source/cxxhelp/provider/db.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: db.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2005-04-11 08:42:09 $
+ *  last change: $Author: hr $ $Date: 2005-04-11 09:32:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,7 +61,11 @@
 #ifndef BERKELEYDBPROXY_DB_HXX_
 #define BERKELEYDBPROXY_DB_HXX_
 
+#ifdef SYSTEM_DB
+#include <db.h>
+#else
 #include <berkeleydb/db.h>
+#endif
 
 #include <rtl/string.hxx>
 
