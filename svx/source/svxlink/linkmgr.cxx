5a9bd4155e75e9661e449bd36372bf2ee8eb2903
Oliver Bolte <obo@openoffice.org>
2005-04-12 15:37:07 +0000

    INTEGRATION: CWS swqbugfixes22 (1.24.20); FILE MERGED 2005/04/11 05:55:55 od 1.24.20.1: #i46300# - undo changes for issue i45410

--- svx/source/svxlink/linkmgr.cxx
+++ svx/source/svxlink/linkmgr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: linkmgr.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: rt $ $Date: 2005-03-29 14:17:28 $
+ *  last change: $Author: obo $ $Date: 2005-04-12 16:37:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -163,12 +163,9 @@ BOOL SvxLinkManager::InsertFileLink( sfx2::SvBaseLink& rLink,
         return FALSE;
 
     String sCmd( rFileNm );
+    sCmd += ::sfx2::cTokenSeperator;
     if( pRange )
-    {
-        sCmd += ::sfx2::cTokenSeperator;
         sCmd += *pRange;
-    }
-
     if( pFilterNm )
         ( sCmd += ::sfx2::cTokenSeperator ) += *pFilterNm;
 
