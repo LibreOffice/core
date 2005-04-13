8299d315db832af8230e5181097710d38079de69
Oliver Bolte <obo@openoffice.org>
2005-04-13 10:00:58 +0000

    INTEGRATION: CWS visibility03 (1.3.912); FILE MERGED 2005/03/24 12:13:06 mhu 1.3.912.1: #i45006# Added (svl|svt).pmk; partial cleanup.

--- svtools/source/filter.vcl/ixpm/makefile.mk
+++ svtools/source/filter.vcl/ixpm/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: nf $ $Date: 2002-01-15 14:20:53 $
+#   last change: $Author: obo $ $Date: 2005-04-13 11:00:58 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,23 +64,16 @@ PRJ=..$/..$/..
 
 PRJNAME=svtools
 TARGET=ixpm
-DEPTARGET=vixpm
-VERSION=$(UPD)
-
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
-
+.INCLUDE :  $(PRJ)$/util$/svt.pmk
 
 # --- Files --------------------------------------------------------
-
 
 CXXFILES= xpmread.cxx
 
 SLOFILES= $(SLO)$/xpmread.obj
-
 
 .INCLUDE :  target.mk
