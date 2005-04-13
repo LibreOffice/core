a11b35302348c183b7b35a90fad7fd9d1affb7f3
Oliver Bolte <obo@openoffice.org>
2005-04-13 07:30:30 +0000

    INTEGRATION: CWS visibility03 (1.36.148); FILE MERGED 2005/04/06 14:33:12 mhu 1.36.148.2: RESYNC: (1.36-1.37); FILE MERGED 2005/03/26 16:38:19 mhu 1.36.148.1: #i45006# subst includes svtools/sbx with basic/sbx

--- svx/source/form/fmview.cxx
+++ svx/source/form/fmview.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmview.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-23 11:50:11 $
+ *  last change: $Author: obo $ $Date: 2005-04-13 08:30:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -118,7 +118,7 @@
 #endif
 
 #ifndef _SBXCLASS_HXX //autogen
-#include <svtools/sbx.hxx>
+#include <basic/sbx.hxx>
 #endif
 
 #ifndef _SVX_FMITEMS_HXX
