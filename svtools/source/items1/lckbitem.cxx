a119608c895942b96b1fd9a0be5e1dbea5ae2914
Oliver Bolte <obo@openoffice.org>
2005-04-13 10:17:18 +0000

    INTEGRATION: CWS visibility03 (1.1.2); FILE ADDED 2005/04/01 13:03:44 mhu 1.1.2.2: #i45006# Removed unused include(s). 2005/03/24 10:45:52 mhu 1.1.2.1: #i45006# Moved from svtools/source/items/ (for svl/items1.lib).

---
+++ svtools/source/items1/lckbitem.cxx
@@ -0,0 +1,235 @@
+/*************************************************************************
+ *
+ *  $RCSfile: lckbitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-04-13 11:17:18 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#define _LCKBITEM_CXX
+#include "lckbitem.hxx"
+
+#ifndef _SFXPOOLITEM_HXX
+#include "poolitem.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
+#include <com/sun/star/uno/Any.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+
+#ifndef _CACHESTR_HXX //autogen
+#include <tools/cachestr.hxx>
+#endif
+
+// STATIC DATA -----------------------------------------------------------
+
+
+// -----------------------------------------------------------------------
+
+TYPEINIT1_AUTOFACTORY(SfxLockBytesItem, SfxPoolItem);
+
+// -----------------------------------------------------------------------
+
+SfxLockBytesItem::SfxLockBytesItem()
+{
+}
+
+// -----------------------------------------------------------------------
+
+SfxLockBytesItem::SfxLockBytesItem( USHORT nW, SvLockBytes *pLockBytes )
+:   SfxPoolItem( nW ),
+    _xVal( pLockBytes )
+{
+}
+
+// -----------------------------------------------------------------------
+
+SfxLockBytesItem::SfxLockBytesItem( USHORT nW, SvStream &rStream )
+:   SfxPoolItem( nW )
+{
+    rStream.Seek( 0L );
+    _xVal = new SvLockBytes( new SvCacheStream(), TRUE );
+
+    SvStream aLockBytesStream( _xVal );
+    rStream >> aLockBytesStream;
+}
+
+// -----------------------------------------------------------------------
+
+SfxLockBytesItem::SfxLockBytesItem( const SfxLockBytesItem& rItem )
+:   SfxPoolItem( rItem ),
+    _xVal( rItem._xVal )
+{
+}
+
+// -----------------------------------------------------------------------
+
+SfxLockBytesItem::~SfxLockBytesItem()
+{
+}
+
+// -----------------------------------------------------------------------
+
+int SfxLockBytesItem::operator==( const SfxPoolItem& rItem ) const
+{
+    return ((SfxLockBytesItem&)rItem)._xVal == _xVal;
+}
+
+// -----------------------------------------------------------------------
+
+SfxPoolItem* SfxLockBytesItem::Clone(SfxItemPool *) const
+{
+    return new SfxLockBytesItem( *this );
+}
+
+// -----------------------------------------------------------------------
+
+#define MAX_BUF 32000
+
+SfxPoolItem* SfxLockBytesItem::Create( SvStream &rStream, USHORT nVersion ) const
+{
+    ULONG nSize = 0;
+    ULONG nActRead = 0;
+    sal_Char cTmpBuf[MAX_BUF];
+    SvMemoryStream aNewStream;
+    rStream >> nSize;
+
+    do {
+        ULONG nToRead;
+        if( (nSize - nActRead) > MAX_BUF )
+            nToRead = MAX_BUF;
+        else
+            nToRead = nSize - nActRead;
+        nActRead += rStream.Read( cTmpBuf, nToRead );
+        aNewStream.Write( cTmpBuf, nToRead );
+    } while( nSize > nActRead );
+
+    return new SfxLockBytesItem( Which(), aNewStream );
+}
+
+// -----------------------------------------------------------------------
+
+SvStream& SfxLockBytesItem::Store(SvStream &rStream, USHORT nItemVersion ) const
+{
+    SvStream aLockBytesStream( _xVal );
+    ULONG nSize = aLockBytesStream.Seek( STREAM_SEEK_TO_END );
+    aLockBytesStream.Seek( 0L );
+
+    rStream << nSize;
+    rStream << aLockBytesStream;
+
+    return rStream;
+}
+
+//----------------------------------------------------------------------------
+// virtual
+BOOL SfxLockBytesItem::PutValue( const com::sun::star::uno::Any& rVal,
+                                    BYTE nMemberId )
+{
+    com::sun::star::uno::Sequence< sal_Int8 > aSeq;
+    if ( rVal >>= aSeq )
+    {
+        if ( aSeq.getLength() )
+        {
+            SvCacheStream* pStream = new SvCacheStream;
+            pStream->Write( (void*)aSeq.getConstArray(), aSeq.getLength() );
+            pStream->Seek(0);
+
+            _xVal = new SvLockBytes( pStream, TRUE );
+        }
+        else
+            _xVal = NULL;
+
+        return TRUE;
+    }
+
+    DBG_ERROR( "SfxLockBytesItem::PutValue - Wrong type!" );
+    return FALSE;
+}
+
+//----------------------------------------------------------------------------
+// virtual
+BOOL SfxLockBytesItem::QueryValue( com::sun::star::uno::Any& rVal,
+                                    BYTE nMemberId ) const
+{
+    if ( _xVal.Is() )
+    {
+        sal_uInt32 nLen;
+        SvLockBytesStat aStat;
+
+        if ( _xVal->Stat( &aStat, SVSTATFLAG_DEFAULT ) == ERRCODE_NONE )
+            nLen = aStat.nSize;
+        else
+            return FALSE;
+
+        ULONG nRead = 0;
+        com::sun::star::uno::Sequence< sal_Int8 > aSeq( nLen );
+
+        _xVal->ReadAt( 0, aSeq.getArray(), nLen, &nRead );
+        rVal <<= aSeq;
+    }
+    else
+    {
+        com::sun::star::uno::Sequence< sal_Int8 > aSeq( 0 );
+        rVal <<= aSeq;
+    }
+
+    return TRUE;
+}
+
