6d4d29f4fc2970dbbccb1a237f7c4aaca25d7050
Oliver Bolte <obo@openoffice.org>
2005-04-13 11:09:56 +0000

    INTEGRATION: CWS visibility03 (1.2.270); FILE MERGED 2005/02/28 03:54:51 mnicel 1.2.270.1: Issue number:  40092 Part of visibility work

--- tools/source/datetime/makefile.mk
+++ tools/source/datetime/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2003-03-27 17:03:56 $
+#   last change: $Author: obo $ $Date: 2005-04-13 12:09:56 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -70,6 +70,7 @@ TARGET=datetime
 .INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
 .INCLUDE :  sv.mk
+.INCLUDE :      $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
