8290ddafa848c2776bb6d45c4f2b0a5563ed9766
Oliver Bolte <obo@openoffice.org>
2005-04-13 11:30:42 +0000

    INTEGRATION: CWS visibility03 (1.2.226); FILE MERGED 2005/02/17 10:31:45 mnicel 1.2.226.1: Issue number:  40092 symbol visibility

--- unotools/source/processfactory/makefile.mk
+++ unotools/source/processfactory/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2000-11-07 10:00:22 $
+#   last change: $Author: obo $ $Date: 2005-04-13 12:30:42 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -73,6 +73,7 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings ----------------------------------
 
 .INCLUDE : settings.mk
+.INCLUDE :      $(PRJ)$/util$/makefile.pmk
 
 # --- Types -------------------------------------
 
