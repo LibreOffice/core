c551ce53226e78e8bb33e3770584994b7731d9eb
Oliver Bolte <obo@openoffice.org>
2005-04-13 10:50:07 +0000

    INTEGRATION: CWS visibility03 (1.2.532); FILE MERGED 2005/03/24 12:13:13 mhu 1.2.532.1: #i45006# Added (svl|svt).pmk; partial cleanup.

--- svtools/source/urlobj/makefile.mk
+++ svtools/source/urlobj/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:45:26 $
+#   last change: $Author: obo $ $Date: 2005-04-13 11:50:07 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -68,13 +68,12 @@ TARGET=urlobj
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/svt.pmk
 
 # --- Files --------------------------------------------------------
 
 SLOFILES =	\
-        $(SLO)$/urlbmk.obj	\
         $(SLO)$/inetimg.obj
-
 
 # --- Tagets -------------------------------------------------------
 
