d482a30b740cfe73e327b65169e106a8cbfee8dc
Oliver Bolte <obo@openoffice.org>
2005-04-13 11:35:27 +0000

    INTEGRATION: CWS visibility03 (1.2.22); FILE MERGED 2005/02/17 10:45:05 mnicel 1.2.22.1: Issue number:  40092

--- sot/source/unoolestorage/makefile.mk
+++ sot/source/unoolestorage/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2004-10-04 20:32:04 $
+#   last change: $Author: obo $ $Date: 2005-04-13 12:35:27 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -64,13 +64,13 @@ PRJ=..$/..
 
 PRJNAME=sot
 TARGET=unoolestorage
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
 .INCLUDE :  sv.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
