e5aab1859af9114893ffcd312f6e09546d0e939f
Oliver Bolte <obo@openoffice.org>
2005-04-13 08:18:22 +0000

    INTEGRATION: CWS visibility03 (1.1.1.1.316); FILE MERGED 2005/03/24 18:01:24 mhu 1.1.1.1.316.1: #i45006# Adapted to moved sbx headers.

--- basic/source/sample/collelem.cxx
+++ basic/source/sample/collelem.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: collelem.cxx,v $
  *
- *  $Revision: 1.1.1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 16:12:11 $
+ *  last change: $Author: obo $ $Date: 2005-04-13 09:18:22 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,7 +66,7 @@
 #include <vcl/msgbox.hxx>
 #endif
 #ifndef _SBXCLASS_HXX //autogen
-#include <svtools/sbx.hxx>
+#include <sbx.hxx>
 #endif
 #pragma hdrstop
 #include "collelem.hxx"
