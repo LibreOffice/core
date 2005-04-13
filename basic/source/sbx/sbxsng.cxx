e6f106a6dfae429c2d03ec9653afb8503a0d3c4f
Oliver Bolte <obo@openoffice.org>
2005-04-13 08:25:57 +0000

    INTEGRATION: CWS visibility03 (1.1.2); FILE ADDED 2005/03/24 18:04:32 mhu 1.1.2.1: #i45006# Moved from svtools/source/sbx/

---
+++ basic/source/sbx/sbxsng.cxx
@@ -0,0 +1,368 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sbxsng.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-04-13 09:25:44 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _ERRCODE_HXX //autogen
+#include <tools/errcode.hxx>
+#endif
+#include "sbx.hxx"
+#include "sbxconv.hxx"
+
+float ImpGetSingle( const SbxValues* p )
+{
+    SbxValues aTmp;
+    float nRes;
+start:
+    switch( p->eType )
+    {
+        case SbxNULL:
+            SbxBase::SetError( SbxERR_CONVERSION );
+        case SbxEMPTY:
+            nRes = 0; break;
+        case SbxCHAR:
+            nRes = p->nChar; break;
+        case SbxBYTE:
+            nRes = p->nByte; break;
+        case SbxINTEGER:
+        case SbxBOOL:
+            nRes = p->nInteger; break;
+        case SbxERROR:
+        case SbxUSHORT:
+            nRes = p->nUShort; break;
+        case SbxLONG:
+            nRes = (float) p->nLong; break;
+        case SbxULONG:
+            nRes = (float) p->nULong; break;
+        case SbxSINGLE:
+            nRes = p->nSingle; break;
+        case SbxSALINT64:
+            nRes = (float) p->nInt64; break;
+        case SbxSALUINT64:
+            nRes = (float) ImpSalUInt64ToDouble( p->uInt64 ); break;
+        case SbxDECIMAL:
+        case SbxBYREF | SbxDECIMAL:
+            if( p->pDecimal )
+                p->pDecimal->getSingle( nRes );
+            else
+                nRes = 0.0;
+            break;
+        case SbxDATE:
+        case SbxDOUBLE:
+        case SbxLONG64:
+        case SbxULONG64:
+        case SbxCURRENCY:
+            {
+            double dVal;
+            if( p->eType == SbxCURRENCY )
+                dVal = ImpCurrencyToDouble( p->nLong64 );
+            else if( p->eType == SbxLONG64 )
+                dVal = ImpINT64ToDouble( p->nLong64 );
+            else if( p->eType == SbxULONG64 )
+                dVal = ImpUINT64ToDouble( p->nULong64 );
+            else
+                dVal = p->nDouble;
+
+            if( dVal > SbxMAXSNG )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMAXSNG;
+            }
+            else if( dVal < SbxMINSNG )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMINSNG;
+            }
+            else if( dVal > 0 && dVal < SbxMAXSNG2 )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMAXSNG2;
+            }
+            else if( dVal < 0 && dVal > SbxMINSNG2 )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMINSNG2;
+            }
+            else
+                nRes = (float) dVal;
+            break;
+            }
+        case SbxBYREF | SbxSTRING:
+        case SbxSTRING:
+        case SbxLPSTR:
+            if( !p->pString )
+                nRes = 0;
+            else
+            {
+                double d;
+                SbxDataType t;
+                if( ImpScan( *p->pString, d, t, NULL ) != SbxERR_OK )
+                    nRes = 0;
+                else if( d > SbxMAXSNG )
+                {
+                    SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMAXSNG;
+                }
+                else if( d < SbxMINSNG )
+                {
+                    SbxBase::SetError( SbxERR_OVERFLOW ); nRes = SbxMINSNG;
+                }
+                else
+                    nRes = (float) d;
+            }
+            break;
+        case SbxOBJECT:
+        {
+            SbxValue* pVal = PTR_CAST(SbxValue,p->pObj);
+            if( pVal )
+                nRes = pVal->GetSingle();
+            else
+            {
+                SbxBase::SetError( SbxERR_NO_OBJECT ); nRes = 0;
+            }
+            break;
+        }
+
+        case SbxBYREF | SbxCHAR:
+            nRes = *p->pChar; break;
+        case SbxBYREF | SbxBYTE:
+            nRes = *p->pByte; break;
+        case SbxBYREF | SbxINTEGER:
+        case SbxBYREF | SbxBOOL:
+            nRes = *p->pInteger; break;
+        case SbxBYREF | SbxLONG:
+            nRes = (float) *p->pLong; break;
+        case SbxBYREF | SbxULONG:
+            nRes = (float) *p->pULong; break;
+        case SbxBYREF | SbxERROR:
+        case SbxBYREF | SbxUSHORT:
+            nRes = *p->pUShort; break;
+        case SbxBYREF | SbxSINGLE:
+            nRes = *p->pSingle; break;
+        // ab hier muss getestet werden
+        case SbxBYREF | SbxDATE:
+        case SbxBYREF | SbxDOUBLE:
+            aTmp.nDouble = *p->pDouble; goto ref;
+        case SbxBYREF | SbxULONG64:
+            aTmp.nULong64 = *p->pULong64; goto ref;
+        case SbxBYREF | SbxLONG64:
+        case SbxBYREF | SbxSALINT64:
+            nRes = (float) *p->pnInt64; break;
+        case SbxBYREF | SbxSALUINT64:
+            nRes = (float) ImpSalUInt64ToDouble( *p->puInt64 ); break;
+        case SbxBYREF | SbxCURRENCY:
+            aTmp.nLong64 = *p->pLong64; goto ref;
+        ref:
+            aTmp.eType = SbxDataType( p->eType & 0x0FFF );
+            p = &aTmp; goto start;
+
+        default:
+            SbxBase::SetError( SbxERR_CONVERSION ); nRes = 0;
+    }
+    return nRes;
+}
+
+void ImpPutSingle( SbxValues* p, float n )
+{
+    SbxValues aTmp;
+start:
+    switch( p->eType )
+    {
+        case SbxCHAR:
+            aTmp.pChar = &p->nChar; goto direct;
+        case SbxBYTE:
+            aTmp.pByte = &p->nByte; goto direct;
+        case SbxINTEGER:
+        case SbxBOOL:
+            aTmp.pInteger = &p->nInteger; goto direct;
+        case SbxLONG:
+            aTmp.pLong = &p->nLong; goto direct;
+        case SbxULONG:
+            aTmp.pULong = &p->nULong; goto direct;
+        case SbxERROR:
+        case SbxUSHORT:
+            aTmp.pUShort = &p->nUShort; goto direct;
+        case SbxULONG64:
+            aTmp.pULong64 = &p->nULong64; goto direct;
+        case SbxLONG64:
+        case SbxCURRENCY:
+            aTmp.pLong64 = &p->nLong64; goto direct;
+        case SbxSALINT64:
+            aTmp.pnInt64 = &p->nInt64; goto direct;
+        case SbxSALUINT64:
+            aTmp.puInt64 = &p->uInt64; goto direct;
+        case SbxDECIMAL:
+        case SbxBYREF | SbxDECIMAL:
+            {
+            SbxDecimal* pDec = ImpCreateDecimal( p );
+            if( !pDec->setSingle( n ) )
+                SbxBase::SetError( SbxERR_OVERFLOW );
+            break;
+            }
+        direct:
+            aTmp.eType = SbxDataType( p->eType | SbxBYREF );
+            p = &aTmp; goto start;
+
+        // keine Tests ab hier
+        case SbxSINGLE:
+            p->nSingle = n; break;
+        case SbxDATE:
+        case SbxDOUBLE:
+            p->nDouble = n; break;
+
+        case SbxBYREF | SbxSTRING:
+        case SbxSTRING:
+        case SbxLPSTR:
+        {
+            if( !p->pString )
+                p->pString = new XubString;
+            ImpCvtNum( (double) n, 6, *p->pString );
+            break;
+        }
+        case SbxOBJECT:
+        {
+            SbxValue* pVal = PTR_CAST(SbxValue,p->pObj);
+            if( pVal )
+                pVal->PutSingle( n );
+            else
+                SbxBase::SetError( SbxERR_NO_OBJECT );
+            break;
+        }
+        case SbxBYREF | SbxCHAR:
+            if( n > SbxMAXCHAR )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXCHAR;
+            }
+            else if( n < SbxMINCHAR )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMINCHAR;
+            }
+            *p->pChar = (xub_Unicode) n; break;
+        case SbxBYREF | SbxBYTE:
+            if( n > SbxMAXBYTE )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXBYTE;
+            }
+            else if( n < 0 )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = 0;
+            }
+            *p->pByte = (BYTE) n; break;
+        case SbxBYREF | SbxINTEGER:
+        case SbxBYREF | SbxBOOL:
+            if( n > SbxMAXINT )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXINT;
+            }
+            else if( n < SbxMININT )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMININT;
+            }
+            *p->pInteger = (INT16) n; break;
+        case SbxBYREF | SbxERROR:
+        case SbxBYREF | SbxUSHORT:
+            if( n > SbxMAXUINT )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXUINT;
+            }
+            else if( n < 0 )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = 0;
+            }
+            *p->pUShort = (UINT16) n; break;
+        case SbxBYREF | SbxLONG:
+            if( n > SbxMAXLNG )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXLNG;
+            }
+            else if( n < SbxMINLNG )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMINLNG;
+            }
+            *p->pLong = (INT32) n; break;
+        case SbxBYREF | SbxULONG:
+            if( n > SbxMAXULNG )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXULNG;
+            }
+            else if( n < 0 )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = 0;
+            }
+            *p->pULong = (UINT32) n; break;
+        case SbxBYREF | SbxSINGLE:
+            *p->pSingle = n; break;
+        case SbxBYREF | SbxDATE:
+        case SbxBYREF | SbxDOUBLE:
+            *p->pDouble = (double) n; break;
+        case SbxBYREF | SbxSALINT64:
+            *p->pnInt64 = ImpDoubleToSalInt64( (double) n ); break;
+        case SbxBYREF | SbxSALUINT64:
+            *p->puInt64 = ImpDoubleToSalUInt64( (double) n ); break;
+        case SbxBYREF | SbxCURRENCY:
+            if( n > SbxMAXCURR )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMAXCURR;
+            }
+            else if( n < SbxMINCURR )
+            {
+                SbxBase::SetError( SbxERR_OVERFLOW ); n = SbxMINCURR;
+            }
+            *p->pLong64 = ImpDoubleToCurrency( (double)n ); break;
+
+        default:
+            SbxBase::SetError( SbxERR_CONVERSION );
+    }
+}
+
---
+++ basic/source/sbx/sbxstr.cxx
@@ -0,0 +1,281 @@
+/*************************************************************************
+ *
+ *  $RCSfile: sbxstr.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-04-13 09:25:57 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _ERRCODE_HXX //autogen
+#include <tools/errcode.hxx>
+#endif
+#include "sbx.hxx"
+#include "sbxconv.hxx"
+#include "sbxres.hxx"
+
+// AB 29.10.99 Unicode
+#ifndef _USE_NO_NAMESPACE
+using namespace rtl;
+#endif
+
+
+// Die Konversion eines Items auf String wird ueber die Put-Methoden
+// der einzelnen Datentypen abgewickelt, um doppelten Code zu vermeiden.
+
+XubString ImpGetString( const SbxValues* p )
+{
+    SbxValues aTmp;
+    XubString aRes;
+    aTmp.eType = SbxSTRING;
+    aTmp.pString = &aRes;
+    switch( p->eType )
+    {
+        case SbxNULL:
+            SbxBase::SetError( SbxERR_CONVERSION );
+        case SbxEMPTY:
+            break;
+        case SbxCHAR:
+            ImpPutChar( &aTmp, p->nChar ); break;
+        case SbxBYTE:
+            ImpPutByte( &aTmp, p->nByte ); break;
+        case SbxINTEGER:
+            ImpPutInteger( &aTmp, p->nInteger ); break;
+        case SbxBOOL:
+            ImpPutBool( &aTmp, p->nUShort ); break;
+        case SbxUSHORT:
+            ImpPutUShort( &aTmp, p->nUShort ); break;
+        case SbxLONG:
+            ImpPutLong( &aTmp, p->nLong ); break;
+        case SbxULONG:
+            ImpPutULong( &aTmp, p->nULong ); break;
+        case SbxSINGLE:
+            ImpPutSingle( &aTmp, p->nSingle ); break;
+        case SbxDOUBLE:
+            ImpPutDouble( &aTmp, p->nDouble ); break;
+        case SbxCURRENCY:
+            ImpPutCurrency( &aTmp, p->nLong64 ); break;
+        case SbxDECIMAL:
+        case SbxBYREF | SbxDECIMAL:
+            ImpPutDecimal( &aTmp, p->pDecimal ); break;
+        case SbxSALINT64:
+            ImpPutInt64( &aTmp, p->nInt64 ); break;
+        case SbxSALUINT64:
+            ImpPutUInt64( &aTmp, p->uInt64 ); break;
+        case SbxBYREF | SbxSTRING:
+        case SbxSTRING:
+        case SbxLPSTR:
+            if( p->pString )
+                aRes = *p->pString;
+            break;
+        case SbxOBJECT:
+        {
+            SbxValue* pVal = PTR_CAST(SbxValue,p->pObj);
+            if( pVal )
+                aRes = pVal->GetString();
+            else
+                SbxBase::SetError( SbxERR_NO_OBJECT );
+            break;
+        }
+        case SbxERROR:
+            // Hier wird der String "Error n" erzeugt
+            aRes = SbxRes( STRING_ERRORMSG );
+            aRes += p->nUShort; break;
+        case SbxDATE:
+            ImpPutDate( &aTmp, p->nDouble ); break;
+
+        case SbxBYREF | SbxCHAR:
+            ImpPutChar( &aTmp, *p->pChar ); break;
+        case SbxBYREF | SbxBYTE:
+            ImpPutByte( &aTmp, *p->pByte ); break;
+        case SbxBYREF | SbxINTEGER:
+        case SbxBYREF | SbxBOOL:
+            ImpPutInteger( &aTmp, *p->pInteger ); break;
+        case SbxBYREF | SbxLONG:
+            ImpPutLong( &aTmp, *p->pLong ); break;
+        case SbxBYREF | SbxULONG:
+            ImpPutULong( &aTmp, *p->pULong ); break;
+        case SbxBYREF | SbxERROR:
+        case SbxBYREF | SbxUSHORT:
+            ImpPutUShort( &aTmp, *p->pUShort ); break;
+        case SbxBYREF | SbxSINGLE:
+            ImpPutSingle( &aTmp, *p->pSingle ); break;
+        case SbxBYREF | SbxDATE:
+        case SbxBYREF | SbxDOUBLE:
+            ImpPutDouble( &aTmp, *p->pDouble ); break;
+        case SbxBYREF | SbxCURRENCY:
+            ImpPutCurrency( &aTmp, *p->pLong64 ); break;
+        case SbxBYREF | SbxSALINT64:
+            ImpPutInt64( &aTmp, *p->pnInt64 ); break;
+        case SbxBYREF | SbxSALUINT64:
+            ImpPutUInt64( &aTmp, *p->puInt64 ); break;
+        default:
+            SbxBase::SetError( SbxERR_CONVERSION );
+    }
+    return aRes;
+}
+
+// AB 10.4.97, neue Funktion fuer SbxValue::GetCoreString()
+XubString ImpGetCoreString( const SbxValues* p )
+{
+    // Vorerst nur fuer double
+    if( ( p->eType & (~SbxBYREF) ) == SbxDOUBLE )
+    {
+        SbxValues aTmp;
+        XubString aRes;
+        aTmp.eType = SbxSTRING;
+        aTmp.pString = &aRes;
+        if( p->eType == SbxDOUBLE )
+            ImpPutDouble( &aTmp, p->nDouble, /*bCoreString=*/TRUE );
+        else
+            ImpPutDouble( &aTmp, *p->pDouble, /*bCoreString=*/TRUE );
+        return aRes;
+    }
+    else
+        return ImpGetString( p );
+}
+
+void ImpPutString( SbxValues* p, const XubString* n )
+{
+    SbxValues aTmp;
+    aTmp.eType = SbxSTRING;
+    XubString* pTmp = NULL;
+    // Sicherheitshalber, falls ein NULL-Ptr kommt
+    if( !n )
+        n = pTmp = new XubString;
+    aTmp.pString = (XubString*) n;
+    switch( p->eType )
+    {
+        case SbxCHAR:
+            p->nChar = ImpGetChar( &aTmp ); break;
+        case SbxBYTE:
+            p->nByte = ImpGetByte( &aTmp ); break;
+        case SbxINTEGER:
+        case SbxBOOL:
+            p->nInteger = ImpGetInteger( &aTmp ); break;
+        case SbxLONG:
+            p->nLong = ImpGetLong( &aTmp ); break;
+        case SbxULONG:
+            p->nULong = ImpGetULong( &aTmp ); break;
+        case SbxERROR:
+        case SbxUSHORT:
+            p->nUShort = ImpGetUShort( &aTmp ); break;
+        case SbxSINGLE:
+            p->nSingle = ImpGetSingle( &aTmp ); break;
+        case SbxDATE:
+            p->nDouble = ImpGetDate( &aTmp ); break;
+        case SbxDOUBLE:
+            p->nDouble = ImpGetDouble( &aTmp ); break;
+        case SbxULONG64:
+            p->nLong64 = ImpGetCurrency( &aTmp ); break;
+        case SbxDECIMAL:
+        case SbxBYREF | SbxDECIMAL:
+            releaseDecimalPtr( p->pDecimal );
+            p->pDecimal = ImpGetDecimal( &aTmp ); break;
+        case SbxSALINT64:
+            p->nInt64 = ImpGetInt64( &aTmp ); break;
+        case SbxSALUINT64:
+            p->uInt64 = ImpGetUInt64( &aTmp ); break;
+
+        case SbxBYREF | SbxSTRING:
+        case SbxSTRING:
+        case SbxLPSTR:
+            if( n->Len() )
+            {
+                if( !p->pString )
+                    p->pString = new XubString;
+                *p->pString = *n;
+            }
+            else
+                delete p->pString, p->pString = NULL;
+            break;
+        case SbxOBJECT:
+        {
+            SbxValue* pVal = PTR_CAST(SbxValue,p->pObj);
+            if( pVal )
+                pVal->PutString( *n );
+            else
+                SbxBase::SetError( SbxERR_NO_OBJECT );
+            break;
+        }
+        case SbxBYREF | SbxCHAR:
+            *p->pChar = ImpGetChar( p ); break;
+        case SbxBYREF | SbxBYTE:
+            *p->pByte = ImpGetByte( p ); break;
+        case SbxBYREF | SbxINTEGER:
+            *p->pInteger = ImpGetInteger( p ); break;
+        case SbxBYREF | SbxBOOL:
+            *p->pUShort = ImpGetBool( p ); break;
+        case SbxBYREF | SbxERROR:
+        case SbxBYREF | SbxUSHORT:
+            *p->pUShort = ImpGetUShort( p ); break;
+        case SbxBYREF | SbxLONG:
+            *p->pLong = ImpGetLong( p ); break;
+        case SbxBYREF | SbxULONG:
+            *p->pULong = ImpGetULong( p ); break;
+        case SbxBYREF | SbxSINGLE:
+            *p->pSingle = ImpGetSingle( p ); break;
+        case SbxBYREF | SbxDATE:
+            *p->pDouble = ImpGetDate( p ); break;
+        case SbxBYREF | SbxDOUBLE:
+            *p->pDouble = ImpGetDouble( p ); break;
+        case SbxBYREF | SbxCURRENCY:
+            *p->pLong64 = ImpGetCurrency( p ); break;
+        default:
+            SbxBase::SetError( SbxERR_CONVERSION );
+    }
+    delete pTmp;
+}
+
