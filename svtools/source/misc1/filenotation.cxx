a5f91745eb0f3ebaf6d3c9074a2e33bd5615d6e3
Oliver Bolte <obo@openoffice.org>
2005-04-13 10:26:51 +0000

    INTEGRATION: CWS visibility03 (1.1.2); FILE ADDED 2005/03/24 10:05:56 mhu 1.1.2.1: #i45006# Moved from svtools/source/misc/ (for svl/misc1.lib)

---
+++ svtools/source/misc1/filenotation.cxx
@@ -0,0 +1,184 @@
+/*************************************************************************
+ *
+ *  $RCSfile: filenotation.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-04-13 11:26:51 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc..
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef SVTOOLS_FILENOTATION_HXX
+#include "filenotation.hxx"
+#endif
+#ifndef _OSL_FILE_H_
+#include <osl/file.h>
+#endif
+#ifndef _OSL_DIAGNOSE_H_
+#include <osl/diagnose.h>
+#endif
+#ifndef _URLOBJ_HXX
+#include <tools/urlobj.hxx>
+#endif
+
+//.........................................................................
+namespace svt
+{
+//.........................................................................
+
+    //=====================================================================
+    //= OFileNotation
+    //=====================================================================
+    //---------------------------------------------------------------------
+    OFileNotation::OFileNotation( const ::rtl::OUString& _rUrlOrPath )
+    {
+        construct( _rUrlOrPath );
+    }
+
+    //---------------------------------------------------------------------
+    OFileNotation::OFileNotation( const ::rtl::OUString& _rUrlOrPath, NOTATION _eInputNotation )
+    {
+        if ( _eInputNotation == N_URL )
+        {
+            INetURLObject aParser( _rUrlOrPath );
+            if ( aParser.GetProtocol() == INET_PROT_FILE )
+                implInitWithURLNotation( _rUrlOrPath );
+            else
+                m_sSystem = m_sFileURL = _rUrlOrPath;
+        }
+        else
+            implInitWithSystemNotation( _rUrlOrPath );
+    }
+
+    //---------------------------------------------------------------------
+    bool OFileNotation::implInitWithSystemNotation( const ::rtl::OUString& _rSystemPath )
+    {
+        bool bSuccess = false;
+
+        m_sSystem = _rSystemPath;
+        if  (  ( osl_File_E_None != osl_getFileURLFromSystemPath( m_sSystem.pData, &m_sFileURL.pData ) )
+            && ( 0 == m_sFileURL.getLength() )
+            )
+        {
+            if ( _rSystemPath.getLength() )
+            {
+                INetURLObject aSmartParser;
+                aSmartParser.SetSmartProtocol( INET_PROT_FILE );
+                if ( aSmartParser.SetSmartURL( _rSystemPath ) )
+                {
+                    m_sFileURL = aSmartParser.GetMainURL( INetURLObject::NO_DECODE );
+                    osl_getSystemPathFromFileURL( m_sFileURL.pData, &m_sSystem.pData );
+                    bSuccess = true;
+                }
+            }
+        }
+        else
+            bSuccess = true;
+        return bSuccess;
+    }
+
+    //---------------------------------------------------------------------
+    bool OFileNotation::implInitWithURLNotation( const ::rtl::OUString& _rURL )
+    {
+        m_sFileURL = _rURL;
+        osl_getSystemPathFromFileURL( _rURL.pData, &m_sSystem.pData );
+        return true;
+    }
+
+    //---------------------------------------------------------------------
+    void OFileNotation::construct( const ::rtl::OUString& _rUrlOrPath )
+    {
+        bool bSuccess = false;
+        // URL notation?
+        INetURLObject aParser( _rUrlOrPath );
+        switch ( aParser.GetProtocol() )
+        {
+            case INET_PROT_FILE:
+                // file URL
+                bSuccess = implInitWithURLNotation( _rUrlOrPath );
+                break;
+
+            case INET_PROT_NOT_VALID:
+                // assume system notation
+                bSuccess = implInitWithSystemNotation( _rUrlOrPath );
+                break;
+
+            default:
+                // it's a known scheme, but no file-URL -> assume that bothe the URL representation and the
+                // system representation are the URL itself
+                m_sSystem = m_sFileURL = _rUrlOrPath;
+                bSuccess = true;
+                break;
+        }
+
+        OSL_ENSURE( bSuccess, "OFileNotation::OFileNotation: could not detect the format!" );
+    }
+
+    //---------------------------------------------------------------------
+    ::rtl::OUString OFileNotation::get(NOTATION _eOutputNotation)
+    {
+        switch (_eOutputNotation)
+        {
+            case N_SYSTEM: return m_sSystem;
+            case N_URL: return m_sFileURL;
+        }
+
+        OSL_ENSURE(sal_False, "OFileNotation::get: inavlid enum value!");
+        return ::rtl::OUString();
+    }
+
+//.........................................................................
+}   // namespace svt
+//.........................................................................
+
