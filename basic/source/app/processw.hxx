de056efb2d454ed250aeee532946ee9822dc5d1e
Oliver Bolte <obo@openoffice.org>
2005-04-13 08:08:02 +0000

    INTEGRATION: CWS visibility03 (1.2.314); FILE MERGED 2005/03/24 18:01:14 mhu 1.2.314.1: #i45006# Adapted to moved sbx headers.

--- basic/source/app/processw.hxx
+++ basic/source/app/processw.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: processw.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2000-10-26 14:47:00 $
+ *  last change: $Author: obo $ $Date: 2005-04-13 09:08:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,13 +63,13 @@
 #define _PROCESSW_HXX
 
 #ifndef __SBX_SBX_FACTORY_HXX //autogen
-#include <svtools/sbxfac.hxx>
+#include <sbxfac.hxx>
 #endif
 #ifndef __SBX_SBXVARIABLE_HXX //autogen
-#include <svtools/sbxvar.hxx>
+#include <sbxvar.hxx>
 #endif
 #ifndef _SBX_SBXOBJECT_HXX //autogen
-#include <svtools/sbxobj.hxx>
+#include <sbxobj.hxx>
 #endif
 #include "process.hxx"
 
