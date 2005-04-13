b696e4ca9e27f2d30d7e13bc63f7e00c43f4e1ea
Oliver Bolte <obo@openoffice.org>
2005-04-13 10:31:31 +0000

    INTEGRATION: CWS visibility03 (1.4.532); FILE MERGED 2005/03/24 12:13:11 mhu 1.4.532.1: #i45006# Added (svl|svt).pmk; partial cleanup.

--- svtools/source/plugapp/makefile.mk
+++ svtools/source/plugapp/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: kz $ $Date: 2003-08-25 15:43:32 $
+#   last change: $Author: obo $ $Date: 2005-04-13 11:31:31 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -68,6 +68,7 @@ LIBTARGET=NO
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/svt.pmk
 
 # --- Files --------------------------------------------------------
 
