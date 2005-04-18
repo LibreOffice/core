c21b3bbc520bc32f8a02126ebafb89f678f469c5
Oliver Bolte <obo@openoffice.org>
2005-04-18 13:36:43 +0000

    INTEGRATION: CWS thaiblocksatz (1.15.500); FILE MERGED 2005/04/06 06:30:27 fme 1.15.500.1: #i41860# Thai justified alignment needs some more precision to prevent rounding errors

--- sw/source/core/text/itrtxt.hxx
+++ sw/source/core/text/itrtxt.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: itrtxt.hxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-17 16:23:18 $
+ *  last change: $Author: obo $ $Date: 2005-04-18 14:36:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -267,7 +267,7 @@ protected:
     // spannt beim Blocksatz die Glues auf.
     void CalcNewBlock( SwLineLayout *pCurr, const SwLinePortion *pStopAt,
         SwTwips nReal = 0 );
-    USHORT CalcKanaAdj( SwLineLayout *pCurr );
+    SwTwips CalcKanaAdj( SwLineLayout *pCurr );
 public:
     inline SwTxtAdjuster( SwTxtFrm *pFrm, SwTxtSizeInfo *pInf )
            { SwTxtMargin::CtorInit( pFrm, pInf ); }
