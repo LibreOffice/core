8f78e51e1f2a3682358c10cbe941a58a644a06c3
Oliver Bolte <obo@openoffice.org>
2005-04-18 13:42:43 +0000

    INTEGRATION: CWS olefix (1.2.4); FILE MERGED 2005/04/08 12:54:10 jl 1.2.4.1: #46778# Reloading of Java applet did not work, applet did not appear

--- bean/com/sun/star/comp/beans/CallWatchThread.java
+++ bean/com/sun/star/comp/beans/CallWatchThread.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CallWatchThread.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2005-04-18 11:55:10 $
+ *  last change: $Author: obo $ $Date: 2005-04-18 14:42:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,6 +65,10 @@ package com.sun.star.comp.beans;
 //---------------------------------------------------------------------------
 /** Helper class to watch calls into OOo with a timeout.
  */
+//Do not add the thread instances to a threadgroup. When testing the bean in
+//an applet it turned out the the ThreadGroup was in an inconsistent state
+//after navigating off the site that contains the applet and back to it.
+//That was tested with a Sun JRE 1.4.2_06
 public class CallWatchThread extends Thread
 {
     private static boolean DEBUG = false;
@@ -74,8 +78,6 @@ public class CallWatchThread extends Thread
     private boolean bAlive;
     private long nTimeout;
 
-
-
     public CallWatchThread(long nTimeout)
     {
         this(nTimeout, "");
@@ -83,7 +85,6 @@ public class CallWatchThread extends Thread
 
     public CallWatchThread( long nTimeout, String aTag )
     {
-
         super(aTag);
         this.aWatchedThread = Thread.currentThread();
         this.nTimeout = nTimeout;
