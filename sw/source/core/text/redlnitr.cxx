9795028bd79f5eddd598b32d19012646fd28f48a
Oliver Bolte <obo@openoffice.org>
2005-04-18 13:39:19 +0000

    INTEGRATION: CWS thaiblocksatz (1.35.598); FILE MERGED 2005/04/06 06:30:29 fme 1.35.598.1: #i41860# Thai justified alignment needs some more precision to prevent rounding errors

--- sw/source/core/text/redlnitr.cxx
+++ sw/source/core/text/redlnitr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: redlnitr.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: kz $ $Date: 2004-02-26 15:33:24 $
+ *  last change: $Author: obo $ $Date: 2005-04-18 14:39:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
