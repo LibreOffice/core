bcb63841e311cf69c604b45f798eb3d992b6fa42
Oliver Bolte <obo@openoffice.org>
2005-04-20 10:40:36 +0000

    INTEGRATION: CWS nativefixer6 (1.1.2); FILE ADDED 2005/04/12 10:19:15 is 1.1.2.2: #i46741# Linux patch 2005/04/12 10:14:00 is 1.1.2.1: #i46741# Linux patching

---
+++ setup_native/scripts/linuxpatchscript.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+MYUID=`id | sed "s/(.*//g" | sed "s/.*=//"`
+
+if [ $MYUID -ne 0 ]
+then
+    echo You need to have super-user rights to install this language package
+    exit 1
+fi
+
+echo
+echo "Searching for the PRODUCTNAMEPLACEHOLDER installation ..."
+
+RPMNAME=`rpm -qa | grep PRODUCTNAMEPLACEHOLDER-core01`
+
+if [ "x$RPMNAME" != "x" ]
+then
+  PRODUCTINSTALLLOCATION="`rpm -ql $RPMNAME | head -n 1`"
+else
+  echo "PRODUCTNAMEPLACEHOLDER is not installed"
+  exit 1
+fi
+
+# Asking for the installation directory
+
+echo
+echo "Where do you want to install the patch ? [$PRODUCTINSTALLLOCATION] "
+read reply leftover
+if [ "x$reply" != "x" ]
+then
+  PRODUCTINSTALLLOCATION="$reply"
+fi
+
+# Unpacking
+
+echo "Installing..."
+
+INSTALLLINES
+
+echo "Done..."
+
+exit 0
