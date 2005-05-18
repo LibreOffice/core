feae3db597cf1e3a55268a697d11bc2ff9ff3270
Kurt Zenker <kz@openoffice.org>
2005-05-18 09:54:16 +0000

    INTEGRATION: CWS readonlydoc1 (1.1.2); FILE ADDED 2005/05/12 14:20:57 sb 1.1.2.1: #i49149# Make sure that the java_remote_bridge is disposed whenever its MessageDispatcher thread terminates, even due to a thrown Error.

---
+++ bridges/test/com/sun/star/lib/uno/bridges/java_remote/StopMessageDispatcherTest.java
@@ -0,0 +1,142 @@
+/*************************************************************************
+ *
+ *  $RCSfile: StopMessageDispatcherTest.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-05-18 10:54:16 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2005 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package com.sun.star.lib.uno.bridges.javaremote;
+
+import com.sun.star.bridge.XBridge;
+import com.sun.star.bridge.XInstanceProvider;
+import com.sun.star.lang.DisposedException;
+import com.sun.star.lib.TestBed;
+import com.sun.star.lib.uno.typeinfo.MethodTypeInfo;
+import com.sun.star.lib.uno.typeinfo.TypeInfo;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import complexlib.ComplexTestCase;
+
+/* This test has to detect whether the spawned client process hangs, which can
+ * not be done reliably.  As an approximation, it waits for 10 sec and considers
+ * the process hanging if it has not terminated by then.
+ */
+public final class StopMessageDispatcherTest extends ComplexTestCase {
+    public StopMessageDispatcherTest() {}
+
+    public String[] getTestMethodNames() {
+        return new String[] { "test" };
+    }
+
+    public void test() throws Exception {
+        assure(
+            "test",
+            new TestBed().execute(new Provider(), false, Client.class, 10000));
+    }
+
+    public static final class Client extends TestBed.Client {
+        public static void main(String[] args) {
+            new Client().execute();
+        }
+
+        protected boolean run(XBridge bridge) throws Throwable {
+            XTest test = (XTest) UnoRuntime.queryInterface(
+                XTest.class, bridge.getInstance("Test"));
+            Thread[] threads = new Thread[101];
+            int n = Thread.enumerate(threads);
+            if (n > 100) {
+                System.err.println("ERROR: too many threads");
+                return false;
+            }
+            boolean stopped = false;
+            for (int i = 0; i < n; ++i) {
+                if (threads[i].getName().equals("MessageDispatcher")) {
+                    threads[i].stop();
+                    stopped = true;
+                    break;
+                }
+            }
+            if (!stopped) {
+                System.err.println("ERROR: thread not found");
+                return false;
+            }
+            try {
+                test.call();
+                System.err.println("ERROR: no DisposedException");
+                return false;
+            } catch (DisposedException e) {
+                return true;
+            }
+        }
+
+        private Client() {}
+    }
+
+    private static final class Provider implements XInstanceProvider {
+        public Object getInstance(String instanceName) {
+            return new XTest() {
+                    public void call() {}
+                };
+        }
+    }
+
+    public interface XTest extends XInterface {
+        void call();
+
+        TypeInfo[] UNOTYPEINFO = { new MethodTypeInfo("call", 0, 0) };
+    }
+}
