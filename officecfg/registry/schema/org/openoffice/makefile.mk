ebd7fa3e03dcff3be61fe3ee249c0e0ae5aa720b
Rüdiger Timm <rt@openoffice.org>
2005-05-24 12:59:38 +0000

    INTEGRATION: CWS c01v008 (1.5.34); FILE MERGED 2005/05/23 12:50:17 cd 1.5.34.1: #123059 make option to 'not register' configurable

--- officecfg/registry/schema/org/openoffice/makefile.mk
+++ officecfg/registry/schema/org/openoffice/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: obo $ $Date: 2005-03-15 12:44:11 $
+#   last change: $Author: rt $ $Date: 2005-05-24 13:59:38 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -77,7 +77,8 @@ XCSFILES= \
     Setup.xcs \
     System.xcs \
     UserProfile.xcs \
-    VCL.xcs
+    VCL.xcs \
+    FirstStartWizard.xcs
 
 .INCLUDE :  target.mk
 
