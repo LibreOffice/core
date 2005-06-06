a1c03d3c551108a5f4f2dfeef094f877d5282de2
Jens-Heiner Rechtien <hr@openoffice.org>
2005-06-06 16:40:27 +0000

    INTEGRATION: CWS gcj5 (1.4.14); FILE MERGED 2005/06/02 07:43:09 cmc 1.4.14.1: #i50224# gcj has the required bits to build java canvas now, while sun.awt imports are unnecessary

--- canvas/source/java/JavaCanvas.java
+++ canvas/source/java/JavaCanvas.java
@@ -75,10 +75,6 @@ import java.awt.*;
 import java.awt.image.*;
 import java.awt.geom.*;
 
-// system-dependent stuff
-import sun.awt.*;
-
-
 public class JavaCanvas
     extends CanvasBase
     implements com.sun.star.awt.XWindow,
