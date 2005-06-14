62691c85911c55a22c9213675b0550ec5a2b67bf
Oliver Bolte <obo@openoffice.org>
2005-06-14 14:50:33 +0000

    INTEGRATION: CWS qadev23 (1.7.10); FILE MERGED 2005/04/21 12:40:43 cn 1.7.10.1: #i46788# replace logging/comment string 'text doc' with 'calc doc'

--- qadevOOo/tests/java/mod/_sc/ScAccessibleDocument.java
+++ qadevOOo/tests/java/mod/_sc/ScAccessibleDocument.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ScAccessibleDocument.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $
+ *  last change: $Author: obo $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -155,7 +155,7 @@ public class ScAccessibleDocument extends TestCase {
 
     /**
     * Called while disposing a <code>TestEnvironment</code>.
-    * Disposes text document.
+    * Disposes calc document.
     * @param tParam test parameters
     * @param tEnv the environment to cleanup
     * @param log writer to log information while testing
