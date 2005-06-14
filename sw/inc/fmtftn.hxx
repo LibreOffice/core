c37aac7ce6651b4817f8e296c5f3498e7b2b2a5a
Oliver Bolte <obo@openoffice.org>
2005-06-14 15:26:34 +0000

    INTEGRATION: CWS gcc4fwdecl (1.3.284); FILE MERGED 2005/06/01 18:38:01 pmladek 1.3.284.1: #i50076# Fixed forward declarations for gcc4 in sw

--- sw/inc/fmtftn.hxx
+++ sw/inc/fmtftn.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtftn.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-01-05 15:51:33 $
+ *  last change: $Author: obo $ $Date: 2005-06-14 16:26:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -73,6 +73,7 @@
 #endif
 
 class SwDoc;
+class SwTxtFtn;
 
 // ATT_FTN **********************************************************
 
