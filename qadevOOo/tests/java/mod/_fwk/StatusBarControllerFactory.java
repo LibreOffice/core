c4d520f0631fc6fa11e1afecafa157ebc4cb75e6
Oliver Bolte <obo@openoffice.org>
2005-06-14 14:48:08 +0000

    INTEGRATION: CWS qadev23 (1.1.2); FILE ADDED 2005/06/09 13:54:32 cn 1.1.2.1: 2''#i50540# initial version

---
+++ qadevOOo/tests/java/mod/_fwk/StatusBarControllerFactory.java
@@ -0,0 +1,108 @@
+/*************************************************************************
+ *
+ *  $RCSfile: StatusBarControllerFactory.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Date: 2005-06-14 15:48:08 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package mod._fwk;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XComponentContext;
+import com.sun.star.uno.XInterface;
+import java.io.PrintWriter;
+import lib.StatusException;
+import lib.TestCase;
+import lib.TestEnvironment;
+import lib.TestParameters;
+
+/**
+ */
+public class StatusBarControllerFactory extends TestCase {
+    XInterface oObj = null;
+
+    protected TestEnvironment createTestEnvironment(TestParameters tParam, PrintWriter log) {
+        TestEnvironment tEnv = null;
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)tParam.getMSF();
+        try {
+            oObj = (XInterface)xMSF.createInstance("com.sun.star.comp.framework.StatusBarControllerFactory");
+
+            tEnv = new TestEnvironment(oObj);
+        }
+        catch(com.sun.star.uno.Exception e) {
+            throw new StatusException("Cannot create test object 'StatusBarControllerFactory'", e);
+        }
+        XPropertySet xProp = (XPropertySet)UnoRuntime.queryInterface(XPropertySet.class, xMSF);
+        try {
+            Object o = xProp.getPropertyValue("DefaultContext");
+            XComponentContext xContext = (XComponentContext)UnoRuntime.queryInterface(XComponentContext.class, o);
+            tEnv.addObjRelation("DC", xContext);
+        }
+        catch(com.sun.star.beans.UnknownPropertyException e) {
+            log.println("Cannot get the 'DefaultContext' for XMultiComponentFactory test.");
+            e.printStackTrace(log);
+        }
+        catch(com.sun.star.lang.WrappedTargetException e) {
+            log.println("Cannot get the 'DefaultContext' for XMultiComponentFactory test.");
+            e.printStackTrace(log);
+        }
+        return tEnv;
+    }
+}
+
+
