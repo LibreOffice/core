e565aa6568042e8d38c6ba32be67c4d03a0ab804
Oliver Bolte <obo@openoffice.org>
2005-06-14 15:36:43 +0000

    INTEGRATION: CWS gcc4fwdecl (1.3.448); FILE MERGED 2005/06/01 16:43:20 pmladek 1.3.448.1: #i50063# Fixed forward declarations for gcc4 in extensions

--- extensions/source/bibliography/loadlisteneradapter.hxx
+++ extensions/source/bibliography/loadlisteneradapter.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: loadlisteneradapter.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-03-25 16:03:02 $
+ *  last change: $Author: obo $ $Date: 2005-06-14 16:36:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -81,6 +81,8 @@
 namespace bib
 {
 //.........................................................................
+
+    class OComponentAdapterBase;
 
     //=====================================================================
     //= OComponentListener
