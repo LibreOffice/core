f2eb9faff7b351a9b99fb9a3c69e6d8e4ede9779
Oliver Bolte <obo@openoffice.org>
2005-06-17 08:57:49 +0000

    INTEGRATION: CWS sb31 (1.3.170); FILE MERGED 2005/03/30 12:45:43 sb 1.3.170.1: #120219# Cleaned up.

--- jurt/com/sun/star/comp/connections/makefile.mk
+++ jurt/com/sun/star/comp/connections/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: sb $ $Date: 2002-10-07 13:16:28 $
+#   last change: $Author: obo $ $Date: 2005-06-17 09:57:49 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -67,11 +67,11 @@ PACKAGE = com$/sun$/star$/comp$/connections
 
 .INCLUDE: $(PRJ)$/util$/makefile.pmk
 
-JAVACLASSFILES = \
-    $(CLASSDIR)$/$(PACKAGE)$/Acceptor.class \
-    $(CLASSDIR)$/$(PACKAGE)$/Connector.class \
-    $(CLASSDIR)$/$(PACKAGE)$/ConstantInstanceProvider.class \
-    $(CLASSDIR)$/$(PACKAGE)$/Implementation.class \
-    $(CLASSDIR)$/$(PACKAGE)$/PipedConnection.class
+JAVAFILES = \
+    Acceptor.java \
+    Connector.java \
+    ConstantInstanceProvider.java \
+    Implementation.java \
+    PipedConnection.java
 
 .INCLUDE:  target.mk
