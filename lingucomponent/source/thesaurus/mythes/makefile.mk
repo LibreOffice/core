33033ab1e98310e997b46e1537ae728a135b1a92
Oliver Bolte <obo@openoffice.org>
2005-06-17 08:22:56 +0000

    INTEGRATION: CWS moresystemstuff2 (1.2.18); FILE MERGED 2005/04/01 10:21:38 rene 1.2.18.1: #i46467# add more system stuff and options to link against pam/crypt/xrender

--- lingucomponent/source/thesaurus/mythes/makefile.mk
+++ lingucomponent/source/thesaurus/mythes/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2004-02-04 13:08:28 $
+#   last change: $Author: obo $ $Date: 2005-06-17 09:22:56 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -71,6 +71,11 @@ LIBTARGET=NO
 .INCLUDE : settings.mk
 
 # --- Files --------------------------------------------------------
+
+.IF "$(SYSTEM_MYTHES)" == "YES"
+@all:
+    @echo "Using system mythes..."
+.ENDIF
 
 all_target: ALLTAR
 
