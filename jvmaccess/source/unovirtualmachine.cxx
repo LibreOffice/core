5c155dafefd24272e42b26534ae0d56deb4ef343
Oliver Bolte <obo@openoffice.org>
2005-06-17 08:21:14 +0000

    INTEGRATION: CWS sb31 (1.1.2); FILE ADDED 2005/04/07 13:48:49 sb 1.1.2.2: #120220# Fixed copyright statement. 2005/04/07 12:17:07 sb 1.1.2.1: #120220# Added an encapsulating wrapper around a Java virtual machine and an appropriate UNO class loader.

---
+++ jvmaccess/source/unovirtualmachine.cxx
@@ -0,0 +1,129 @@
+/*************************************************************************
+ *
+ *  $RCSfile: unovirtualmachine.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-06-17 09:21:14 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2005 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "sal/config.h"
+
+#include "jvmaccess/unovirtualmachine.hxx"
+
+#include "osl/diagnose.h"
+
+#include "jvmaccess/virtualmachine.hxx"
+
+#if defined SOLAR_JAVA
+#include "jni.h"
+#endif
+
+namespace jvmaccess {
+
+UnoVirtualMachine::CreationException::CreationException() {}
+
+UnoVirtualMachine::CreationException::CreationException(
+    CreationException const &)
+{}
+
+UnoVirtualMachine::CreationException::~CreationException() {}
+
+UnoVirtualMachine::CreationException &
+UnoVirtualMachine::CreationException::operator =(CreationException const &) {
+    return *this;
+}
+
+UnoVirtualMachine::UnoVirtualMachine(
+    rtl::Reference< jvmaccess::VirtualMachine > const & virtualMachine,
+    jobject classLoader):
+    m_virtualMachine(virtualMachine),
+    m_classLoader(0)
+{
+#if defined SOLAR_JAVA
+    try {
+        m_classLoader =
+            jvmaccess::VirtualMachine::AttachGuard(m_virtualMachine).
+            getEnvironment()->NewGlobalRef(classLoader);
+    } catch (jvmaccess::VirtualMachine::AttachGuard::CreationException &) {}
+#endif
+    if (m_classLoader == 0) {
+        throw CreationException();
+    }
+}
+
+rtl::Reference< jvmaccess::VirtualMachine >
+UnoVirtualMachine::getVirtualMachine() const {
+    return m_virtualMachine;
+}
+
+jobject UnoVirtualMachine::getClassLoader() const {
+    return m_classLoader;
+}
+
+UnoVirtualMachine::~UnoVirtualMachine() {
+#if defined SOLAR_JAVA
+    try {
+        jvmaccess::VirtualMachine::AttachGuard(m_virtualMachine).
+            getEnvironment()->DeleteGlobalRef(m_classLoader);
+    } catch (jvmaccess::VirtualMachine::AttachGuard::CreationException &) {
+        OSL_TRACE(
+            "jvmaccess::UnoVirtualMachine::~UnoVirtualMachine:"
+            " jvmaccess::VirtualMachine::AttachGuard::CreationException" );
+    }
+#endif
+}
+
+}
