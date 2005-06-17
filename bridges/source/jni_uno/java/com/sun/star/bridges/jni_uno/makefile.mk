b3177897ad087ec6d1998f0563809a7d4b775053
Oliver Bolte <obo@openoffice.org>
2005-06-17 08:54:46 +0000

    INTEGRATION: CWS sb31 (1.3.44); FILE MERGED 2005/05/26 14:44:59 sb 1.3.44.3: #121932# Adaption for wntmsci10. 2005/04/18 14:21:54 sb 1.3.44.2: #121392# Extended manifest Class-Path so that native libraries can be found. 2005/03/30 12:37:37 sb 1.3.44.1: #120219# Added Class-Path and Sealed information to JAR file.

--- bridges/source/jni_uno/java/com/sun/star/bridges/jni_uno/makefile.mk
+++ bridges/source/jni_uno/java/com/sun/star/bridges/jni_uno/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2004-07-23 14:48:53 $
+#   last change: $Author: obo $ $Date: 2005-06-17 09:54:46 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -79,6 +79,8 @@ JAVACLASSFILES= \
 JARCLASSDIRS=$(PACKAGE)
 JARTARGET=$(TARGET).jar
 JARCOMPRESS=TRUE
+JARCLASSPATH = $(JARFILES) ../../lib/ ../bin/
+CUSTOMMANIFESTFILE = manifest
 
 # --- Targets ------------------------------------------------------
 
