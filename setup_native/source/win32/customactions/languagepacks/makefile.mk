ee94d781473b5ba0b08a023f0f310a2463e16fd4
Rüdiger Timm <rt@openoffice.org>
2005-07-01 11:18:58 +0000

    INTEGRATION: CWS nativefixer11 (1.2.56); FILE MERGED 2005/06/22 10:45:49 is 1.2.56.1: #123800# new custom action for patch and langpack

--- setup_native/source/win32/customactions/languagepacks/makefile.mk
+++ setup_native/source/win32/customactions/languagepacks/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2004-11-18 08:21:47 $
+#   last change: $Author: rt $ $Date: 2005-07-01 12:18:58 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -81,7 +81,8 @@ CFLAGS+=-D_STLP_USE_STATIC_LIB
 
 UWINAPILIB=
 
-SLOFILES =	$(SLO)$/lngpckinsthelper.obj
+SLOFILES =	$(SLO)$/lngpckinsthelper.obj \
+            $(SLO)$/respintest.obj
 
 SHL1STDLIBS=	kernel32.lib\
                 user32.lib\
