b243f18fac0b751a4b46a4f76201c8abef536dc9
Kurt Zenker <kz@openoffice.org>
2005-07-01 12:16:07 +0000

    INTEGRATION: CWS memory01 (1.3.238); FILE MERGED 2005/06/16 13:57:01 cd 1.3.238.1: #i50833# Correct wrong if/else statements to solve memory leakage for SfxVoidItem

--- sfx2/source/control/sfxstatuslistener.cxx
+++ sfx2/source/control/sfxstatuslistener.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sfxstatuslistener.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-11-17 13:35:38 $
+ *  last change: $Author: kz $ $Date: 2005-07-01 13:16:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -287,7 +287,7 @@ throw( RuntimeException )
             pItem = new SfxVoidItem( m_nSlotID );
             eState = SFX_ITEM_UNKNOWN;
         }
-        if ( pType == ::getBooleanCppuType() )
+        else if ( pType == ::getBooleanCppuType() )
         {
             sal_Bool bTemp ;
             rEvent.State >>= bTemp ;
