d496091740b2c84e58592f64d8037cdc8c7c7b09
Oliver Bolte <obo@openoffice.org>
2005-07-07 12:15:24 +0000

    INTEGRATION: CWS jl22 (1.2.26); FILE MERGED 2005/06/29 14:25:15 jl 1.2.26.1: #i51343#  deprecated methods

--- bean/com/sun/star/comp/beans/ContainerFactory.java
+++ bean/com/sun/star/comp/beans/ContainerFactory.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ContainerFactory.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mi $ $Date: 2004-10-14 10:37:12 $
+ *  last change: $Author: obo $ $Date: 2005-07-07 13:15:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,8 +66,8 @@ import java.awt.Container;
 /**
  * This interface reprecents an AWT container factory.
  *
- * @see OfficeBean
- * @since OOo 2.0.0
+ * @deprecated
+ *
  */
 
 public interface ContainerFactory
