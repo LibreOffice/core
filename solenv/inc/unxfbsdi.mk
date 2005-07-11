7b7e4f1414a06df18cb67b1ef96e4a439d69937b
Kurt Zenker <kz@openoffice.org>
2005-07-11 14:50:11 +0000

    INTEGRATION: CWS ause030 (1.14.6); FILE MERGED 2005/06/20 13:34:33 hjs 1.14.6.1: #i47954# introduce ARCH_FLAG

--- solenv/inc/unxfbsdi.mk
+++ solenv/inc/unxfbsdi.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxfbsdi.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: hr $ $Date: 2005-06-09 14:53:38 $
+#   last change: $Author: kz $ $Date: 2005-07-11 15:50:11 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -89,6 +89,15 @@ JAVA_RUNTIME=-ljava_g
 .ENDIF
 .ENDIF
 
+# architecture dependent flags for the C and C++ compiler that can be changed by
+# exporting the variable ARCH_FLAGS="..." in the shell, which is used to start build
+.IF "$(GCCNUMVER)">="000300040000"
+ARCH_FLAGS*=-mtune=pentiumpro
+.ELSE
+ARCH_FLAGS*=-mcpu=pentiumpro
+.ENDIF
+
+
 # name of C++ Compiler
 CXX*=g++
 # name of C Compiler
@@ -109,24 +118,16 @@ CFLAGSENABLESYMBOLS=-g # was temporarily commented out, reenabled before Beta
 .ENDIF
 
 # flags for the C++ Compiler
-CFLAGSCC= -pipe
+CFLAGSCC= -pipe $(ARCH_FLAGS)
 # Flags for enabling exception handling
 CFLAGSEXCEPTIONS=-fexceptions -fno-enforce-eh-specs
 # Flags for disabling exception handling
 CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
-CFLAGSCXX= -pipe
+CFLAGSCXX= -pipe $(ARCH_FLAGS)
 CFLAGSCXX+= -Wno-ctor-dtor-privacy
 
 # instruction scheduling 
-.IF "$(GCCNUMVER)">="000300040000"
-CFLAGSCC+=-mtune=pentiumpro
-CFLAGSCXX+=-mtune=pentiumpro
-.ELSE
-CFLAGSCC+=-mcpu=pentiumpro
-CFLAGSCXX+=-mcpu=pentiumpro
-.ENDIF
-
 PICSWITCH:=-fpic
 .IF "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
 CFLAGSCXX += -fvisibility-inlines-hidden
