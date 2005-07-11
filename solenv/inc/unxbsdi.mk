e8c22ca3e62aff7892b1741716dda489ec28ffca
Kurt Zenker <kz@openoffice.org>
2005-07-11 14:28:49 +0000

    INTEGRATION: CWS ause030 (1.9.130); FILE MERGED 2005/06/20 13:34:32 hjs 1.9.130.1: #i47954# introduce ARCH_FLAG

--- solenv/inc/unxbsdi.mk
+++ solenv/inc/unxbsdi.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: unxbsdi.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: hr $ $Date: 2005-02-11 15:27:14 $
+#   last change: $Author: kz $ $Date: 2005-07-11 15:28:49 $
 #
 #   The Contents of this file are made available subject to the terms of
 #   either of the following licenses
@@ -84,6 +84,10 @@ JAVA_RUNTIME=-ljava_g
 .ENDIF
 .ENDIF 
 
+# architecture dependent flags for the C and C++ compiler that can be changed by
+# exporting the variable ARCH_FLAGS="..." in the shell, which is used to start build
+ARCH_FLAGS*=-mpentiumpro
+
 # name of C++ Compiler
 CXX*=g++
 # name of C Compiler
@@ -91,13 +95,13 @@ CC*=gcc
 # flags for C and C++ Compiler
 CFLAGS+=-c $(INCLUDE)
 # flags for the C++ Compiler
-CFLAGSCC= -pipe 
+CFLAGSCC= -pipe $(ARCH_FLAGS)
 # Flags for enabling exception handling
 CFLAGSEXCEPTIONS=-fexceptions
 # Flags for disabling exception handling
 CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
-CFLAGSCXX= -pipe -mpentiumpro -frtti
+CFLAGSCXX= -pipe -frtti $(ARCH_FLAGS)
 CFLAGSCXX+= -Wno-ctor-dtor-privacy
 PICSWITCH:=-fpic
 
