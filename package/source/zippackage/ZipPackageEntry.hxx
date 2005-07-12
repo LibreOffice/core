ab2b480a713c82453c01b0f0ac8366603086a9e8
Kurt Zenker <kz@openoffice.org>
2005-07-12 11:31:20 +0000

    INTEGRATION: CWS mav18 (1.12.44); FILE MERGED 2005/05/27 14:52:22 mav 1.12.44.1: #i49755# fix incoplete commit problem

--- package/source/zippackage/ZipPackageEntry.hxx
+++ package/source/zippackage/ZipPackageEntry.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ZipPackageEntry.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2004-10-04 21:10:01 $
+ *  last change: $Author: kz $ $Date: 2005-07-12 12:31:20 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,6 +99,7 @@ class ZipPackageEntry : public cppu::WeakImplHelper5
 {
 protected:
     bool mbIsFolder:1;
+    bool mbAllowRemoveOnInsert:1;
     // com::sun::star::uno::Reference < com::sun::star::container::XNameContainer > xParent;
     ::rtl::OUString     sMediaType;
     ZipPackageFolder * pParent;
