d2b3c561d47c697ca9e193e67e102ba08544265d
Kurt Zenker <kz@openoffice.org>
2005-07-12 13:12:52 +0000

    INTEGRATION: CWS fwk16 (1.1.2); FILE ADDED 2005/07/04 07:14:03 as 1.1.2.1: #i50651# new api stress(!) test to check if normal GUI save and AutoSave can work together

---
+++ framework/qa/complex/framework/autosave/ConfigHelper.java
@@ -0,0 +1,124 @@
+package complex.framework.autosave;
+
+import java.lang.*;
+import com.sun.star.uno.*;
+import com.sun.star.lang.*;
+import com.sun.star.container.*;
+import com.sun.star.beans.*;
+import com.sun.star.util.*;
+
+class ConfigHelper
+{
+    private XMultiServiceFactory m_xSMGR = null;
+    private XHierarchicalNameAccess m_xConfig = null;
+
+    //-----------------------------------------------
+    public ConfigHelper(XMultiServiceFactory xSMGR       ,
+                        String               sConfigPath ,
+                        boolean              bReadOnly   )
+        throws com.sun.star.uno.Exception
+    {
+        m_xSMGR = xSMGR;
+
+        XMultiServiceFactory xConfigRoot = (XMultiServiceFactory)UnoRuntime.queryInterface(
+                                                XMultiServiceFactory.class,
+                                                m_xSMGR.createInstance("com.sun.star.configuration.ConfigurationProvider"));
+
+        PropertyValue[] lParams = new PropertyValue[1];
+        lParams[0] = new PropertyValue();
+        lParams[0].Name  = "nodepath";
+        lParams[0].Value = sConfigPath;
+
+        Object aConfig;
+        if (bReadOnly)
+            aConfig = xConfigRoot.createInstanceWithArguments(
+                            "com.sun.star.configuration.ConfigurationAccess",
+                            lParams);
+        else
+            aConfig = xConfigRoot.createInstanceWithArguments(
+                            "com.sun.star.configuration.ConfigurationUpdateAccess",
+                            lParams);
+
+        m_xConfig = (XHierarchicalNameAccess)UnoRuntime.queryInterface(
+                            XHierarchicalNameAccess.class,
+                            aConfig);
+
+        if (m_xConfig == null)
+            throw new com.sun.star.uno.Exception("Could not open configuration \""+sConfigPath+"\"");
+    }
+
+    //-----------------------------------------------
+    public Object readRelativeKey(String sRelPath,
+                                  String sKey    )
+        throws com.sun.star.container.NoSuchElementException
+    {
+        try
+        {
+            XPropertySet xPath = (XPropertySet)UnoRuntime.queryInterface(
+                                    XPropertySet.class,
+                                    m_xConfig.getByHierarchicalName(sRelPath));
+            return xPath.getPropertyValue(sKey);
+        }
+        catch(com.sun.star.uno.Exception ex)
+        {
+            throw new com.sun.star.container.NoSuchElementException(ex.getMessage());
+        }
+    }
+
+    //-----------------------------------------------
+    public void writeRelativeKey(String sRelPath,
+                                 String sKey    ,
+                                 Object aValue  )
+        throws com.sun.star.container.NoSuchElementException
+    {
+        try
+        {
+            XPropertySet xPath = (XPropertySet)UnoRuntime.queryInterface(
+                                    XPropertySet.class,
+                                    m_xConfig.getByHierarchicalName(sRelPath));
+            xPath.setPropertyValue(sKey, aValue);
+        }
+        catch(com.sun.star.uno.Exception ex)
+        {
+            throw new com.sun.star.container.NoSuchElementException(ex.getMessage());
+        }
+    }
+
+    //-----------------------------------------------
+    public void flush()
+    {
+        try
+        {
+            XChangesBatch xBatch = (XChangesBatch)UnoRuntime.queryInterface(
+                                        XChangesBatch.class,
+                                        m_xConfig);
+            xBatch.commitChanges();
+        }
+        catch(com.sun.star.uno.Exception ex)
+        {}
+    }
+
+    //-----------------------------------------------
+    public static Object readDirectKey(XMultiServiceFactory xSMGR      ,
+                                       String               sConfigFile,
+                                       String               sRelPath   ,
+                                       String               sKey       )
+        throws com.sun.star.uno.Exception
+    {
+        ConfigHelper aConfig = new ConfigHelper(xSMGR, sConfigFile, true);
+        return aConfig.readRelativeKey(sRelPath, sKey);
+    }
+
+    //-----------------------------------------------
+    public static void writeDirectKey(XMultiServiceFactory xSMGR      ,
+                                      String               sConfigFile,
+                                      String               sRelPath   ,
+                                      String               sKey       ,
+                                      Object               aValue     )
+        throws com.sun.star.uno.Exception
+    {
+        ConfigHelper aConfig = new ConfigHelper(xSMGR, sConfigFile, false);
+        aConfig.writeRelativeKey(sRelPath, sKey, aValue);
+        aConfig.flush();
+    }
+}
