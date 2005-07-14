03b0157d786740211480e12ce0e6fd0aa9949df6
Kurt Zenker <kz@openoffice.org>
2005-07-14 09:24:59 +0000

    INTEGRATION: CWS impress51 (1.3.46); FILE MERGED 2005/05/20 09:02:53 af 1.3.46.1: #i48247# Added CreateAccessibleObject() method.

--- sd/source/ui/toolpanel/controls/CustomAnimationPanel.hxx
+++ sd/source/ui/toolpanel/controls/CustomAnimationPanel.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: CustomAnimationPanel.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2005-03-18 17:00:55 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 10:24:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,6 +92,11 @@ public:
     virtual ::Window* GetWindow (void);
     virtual bool IsResizable (void);
     virtual bool IsExpandable (void) const;
+
+    virtual ::com::sun::star::uno::Reference<
+        ::com::sun::star::accessibility::XAccessible > CreateAccessibleObject (
+            const ::com::sun::star::uno::Reference<
+            ::com::sun::star::accessibility::XAccessible>& rxParent);
 
 private:
     Size maPreferredSize;
