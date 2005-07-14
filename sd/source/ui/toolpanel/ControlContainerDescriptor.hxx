976958c3cd82b917fe9b243255b27f5cefeb357a
Kurt Zenker <kz@openoffice.org>
2005-07-14 09:19:32 +0000

    INTEGRATION: CWS impress51 (1.3.46); FILE MERGED 2005/05/04 14:51:19 af 1.3.46.1: #i48247# Adaption to moving some headers to ../inc/taskpane.

--- sd/source/ui/toolpanel/ControlContainerDescriptor.hxx
+++ sd/source/ui/toolpanel/ControlContainerDescriptor.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: ControlContainerDescriptor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: kz $ $Date: 2005-03-18 16:54:26 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 10:19:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,7 +63,7 @@
 #define SD_TASKPANE_CONTROL_CONTAINER_DESCRIPTOR_HXX
 
 #include "taskpane/ILayoutableWindow.hxx"
-#include "TitleBar.hxx"
+#include "taskpane/TitleBar.hxx"
 
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
