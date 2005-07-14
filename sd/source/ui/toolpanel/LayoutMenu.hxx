6f47b0a279f477d7cfc6e2d1ce582ca054d34605
Kurt Zenker <kz@openoffice.org>
2005-07-14 09:20:08 +0000

    INTEGRATION: CWS impress51 (1.6.14); FILE MERGED 2005/06/20 12:37:14 af 1.6.14.1: #i50712# Added support for keyboard driven context menu.

--- sd/source/ui/toolpanel/LayoutMenu.hxx
+++ sd/source/ui/toolpanel/LayoutMenu.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: LayoutMenu.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2005-05-06 09:28:20 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 10:20:08 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -180,6 +180,10 @@ public:
     // DropTargetHelper
     virtual sal_Int8 AcceptDrop (const AcceptDropEvent& rEvent);
     virtual sal_Int8 ExecuteDrop (const ExecuteDropEvent& rEvent);
+
+    /** The context menu is requested over this Command() method.
+    */
+    virtual void Command (const CommandEvent& rEvent);
 
 private:
     ViewShellBase& mrBase;
