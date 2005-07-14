c8a2c7a0ce8906e62eb3726399befd0508d3aef7
Kurt Zenker <kz@openoffice.org>
2005-07-14 09:10:56 +0000

    INTEGRATION: CWS impress51 (1.1.2); FILE ADDED 2005/05/09 16:42:43 af 1.1.2.2: #i48247# Using correct include statement for base class. 2005/05/04 13:18:22 af 1.1.2.1: #i48703# Initial revision.

---
+++ sd/source/ui/accessibility/AccessibleTaskPane.cxx
@@ -0,0 +1,128 @@
+/*************************************************************************
+ *
+ *  $RCSfile: AccessibleTaskPane.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-07-14 10:10:56 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include "AccessibleTaskPane.hxx"
+
+#include "AccessibleTreeNode.hxx"
+
+#include "taskpane/ToolPanel.hxx"
+#include "taskpane/ControlContainer.hxx"
+#include "taskpane/TitledControl.hxx"
+
+#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
+#include <com/sun/star/accessibility/AccessibleRole.hpp>
+#endif
+
+#include <vos/mutex.hxx>
+#include <vcl/svapp.hxx>
+
+using namespace ::rtl;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::accessibility;
+using namespace ::com::sun::star::uno;
+
+namespace accessibility {
+
+AccessibleTaskPane::AccessibleTaskPane (
+    const ::com::sun::star::uno::Reference<
+        ::com::sun::star::accessibility::XAccessible> & rxParent,
+    const OUString& rsName,
+    const OUString& rsDescription,
+    ::sd::toolpanel::ToolPanel& rTaskPane)
+    : AccessibleTreeNode(
+        rxParent,
+        rTaskPane,
+        rsName,
+        rsDescription,
+        AccessibleRole::PANEL)
+{
+}
+
+
+
+
+AccessibleTaskPane::~AccessibleTaskPane (void)
+{
+}
+
+
+
+
+//=====  XServiceInfo  ========================================================
+
+OUString SAL_CALL
+    AccessibleTaskPane::getImplementationName (void)
+    throw (RuntimeException)
+{
+    return OUString(RTL_CONSTASCII_USTRINGPARAM("AccessibleTaskPane"));
+}
+
+
+
+
+void AccessibleTaskPane::UpdateStateSet (void)
+{
+    // The task pane itself can not be focused.
+    UpdateState(AccessibleStateType::FOCUSED, false);
+}
+
+
+} // end of namespace accessibility
