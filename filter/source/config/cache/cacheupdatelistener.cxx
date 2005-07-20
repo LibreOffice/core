11cd9ad5285bf040ade6c03c85a0979144610190
Oliver Bolte <obo@openoffice.org>
2005-07-20 08:28:00 +0000

    INTEGRATION: CWS fwk18 (1.3.274); FILE MERGED 2005/07/14 12:22:14 as 1.3.274.1: #123808# listen on config to be informed about changes

--- filter/source/config/cache/cacheupdatelistener.cxx
+++ filter/source/config/cache/cacheupdatelistener.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: cacheupdatelistener.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2004-04-29 13:39:12 $
+ *  last change: $Author: obo $ $Date: 2005-07-20 09:28:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,6 +60,7 @@
  ************************************************************************/
 
 #include "cacheupdatelistener.hxx"
+#include "constant.hxx"
 
 //_______________________________________________
 // includes
@@ -68,8 +69,16 @@
 #include <com/sun/star/util/XChangesNotifier.hpp>
 #endif
 
-#ifndef _COMPHELPER_SINGLETONREF_HXX_
-#include <comphelper/singletonref.hxx>
+#ifndef _COM_SUN_STAR_UTIL_XREFRESHABLE_HPP_
+#include <com/sun/star/util/XRefreshable.hpp>
+#endif
+
+#ifndef _SALHELPER_SINGLETONREF_HXX_
+#include <salhelper/singletonref.hxx>
+#endif
+
+#ifndef UNOTOOLS_CONFIGPATHES_HXX_INCLUDED
+#include <unotools/configpathes.hxx>
 #endif
 
 #ifndef _RTL_USTRING_HXX_
@@ -90,10 +99,14 @@ namespace css = ::com::sun::star;
 /*-----------------------------------------------
     05.03.2004 08:36
 -----------------------------------------------*/
-CacheUpdateListener::CacheUpdateListener(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR)
-    : BaseLock(     )
-    , m_xSMGR (xSMGR)
-    , m_rCache(     )
+CacheUpdateListener::CacheUpdateListener(const css::uno::Reference< css::lang::XMultiServiceFactory >& xSMGR        ,
+                                         const css::uno::Reference< css::uno::XInterface >&            xConfigAccess,
+                                               FilterCache::EItemType                                  eConfigType  )
+    : BaseLock     (             )
+    , m_xSMGR      (xSMGR        )
+    , m_rCache     (             )
+    , m_xConfig    (xConfigAccess)
+    , m_eConfigType(eConfigType  )
 {
 }
 
@@ -107,13 +120,18 @@ CacheUpdateListener::~CacheUpdateListener()
 /*-----------------------------------------------
     07.03.2004 07:59
 -----------------------------------------------*/
-void CacheUpdateListener::startListening(const css::uno::Reference< css::uno::XInterface >& xConfigAccess)
+void CacheUpdateListener::startListening()
 {
-    css::uno::Reference< css::util::XChangesNotifier > xNotifier(xConfigAccess, css::uno::UNO_QUERY);
+    // SAFE ->
+    ::osl::ResettableMutexGuard aLock(m_aLock);
+    css::uno::Reference< css::util::XChangesNotifier > xNotifier(m_xConfig, css::uno::UNO_QUERY);
+    aLock.clear();
+    // <- SAFE
+
     if (!xNotifier.is())
         return;
 
-    css::uno::Reference< css::util::XChangesListener > xThis(static_cast< css::util::XChangesListener* >(this), css::uno::UNO_QUERY);
+    css::uno::Reference< css::util::XChangesListener > xThis(static_cast< css::util::XChangesListener* >(this), css::uno::UNO_QUERY_THROW);
     xNotifier->addChangesListener(xThis);
 
 /*
@@ -129,9 +147,14 @@ void CacheUpdateListener::startListening(const css::uno::Reference< css::uno::XI
 /*-----------------------------------------------
     07.03.2004 07:59
 -----------------------------------------------*/
-void CacheUpdateListener::stopListening(const css::uno::Reference< css::uno::XInterface >& xConfigAccess)
+void CacheUpdateListener::stopListening()
 {
-    css::uno::Reference< css::util::XChangesNotifier > xNotifier(xConfigAccess, css::uno::UNO_QUERY);
+    // SAFE ->
+    ::osl::ResettableMutexGuard aLock(m_aLock);
+    css::uno::Reference< css::util::XChangesNotifier > xNotifier(m_xConfig, css::uno::UNO_QUERY);
+    aLock.clear();
+    // <- SAFE
+
     if (!xNotifier.is())
         return;
 
@@ -154,6 +177,101 @@ void CacheUpdateListener::stopListening(const css::uno::Reference< css::uno::XIn
 void SAL_CALL  CacheUpdateListener::changesOccurred(const css::util::ChangesEvent& aEvent)
     throw(css::uno::RuntimeException)
 {
+    // SAFE ->
+    ::osl::ResettableMutexGuard aLock(m_aLock);
+
+    // disposed ?
+    if ( ! m_xConfig.is())
+        return;
+
+    css::uno::Reference< css::lang::XMultiServiceFactory > xSMGR = m_xSMGR;
+    FilterCache::EItemType                                 eType = m_eConfigType;
+
+    aLock.clear();
+    // <- SAFE
+
+    OUStringList lChangedItems;
+    sal_Int32    c = aEvent.Changes.getLength();
+    sal_Int32    i = 0;
+
+    for (i=0; i<c; ++i)
+    {
+        const css::util::ElementChange& aChange = aEvent.Changes[i];
+
+        ::rtl::OUString sOrgPath ;
+        ::rtl::OUString sTempPath;
+
+        ::rtl::OUString sProperty;
+        ::rtl::OUString sNode    ;
+        ::rtl::OUString sLocale  ;
+
+        /*  at least we must be able to retrieve 2 path elements
+            But sometimes the original path can contain 3 of them ... in case
+            a localized value was changed.
+            =>
+            1) Filters/Filter["filtername"]/Property
+            2) Filters/Filter["filtername"]/LocalizedProperty/Locale
+        */
+
+        aChange.Accessor >>= sOrgPath;
+        if ( ! ::utl::splitLastFromConfigurationPath(sOrgPath, sTempPath, sLocale))
+            continue;
+        sOrgPath = sTempPath;
+        if ( ! ::utl::splitLastFromConfigurationPath(sOrgPath, sTempPath, sProperty))
+        {
+            sNode     = sLocale;
+            sProperty = ::rtl::OUString();
+            sLocale   = ::rtl::OUString();
+        }
+        else
+        {
+            sOrgPath = sTempPath;
+            if ( ! ::utl::splitLastFromConfigurationPath(sOrgPath, sTempPath, sNode))
+            {
+                sNode     = sProperty;
+                sProperty = sLocale;
+                sLocale   = ::rtl::OUString();
+            }
+        }
+
+        if ( ! sNode.getLength() )
+            continue;
+
+        OUStringList::const_iterator pIt = ::std::find(lChangedItems.begin(), lChangedItems.end(), sNode);
+        if (pIt == lChangedItems.end())
+            lChangedItems.push_back(sNode);
+    }
+
+    sal_Bool                     bNotifyRefresh = sal_False;
+    OUStringList::const_iterator pIt;
+    for (  pIt  = lChangedItems.begin();
+           pIt != lChangedItems.end()  ;
+         ++pIt                         )
+    {
+        const ::rtl::OUString& sItem = *pIt;
+        try
+        {
+            m_rCache->refreshItem(eType, sItem);
+        }
+        catch(const css::container::NoSuchElementException&)
+            {
+                // can be ignored! Because we must be aware that
+                // sItem was removed from the condfiguration and we forced an update of the cache.
+                // But we know, that the cache is up-to-date know and has thrown this exception afterwards .-)
+            }
+        // NO FLUSH! Otherwhise we start a never ending story here .-)
+        bNotifyRefresh = sal_True;
+    }
+
+    // notify sfx cache about the changed filter cache .-)
+    if (bNotifyRefresh)
+    {
+        css::uno::Reference< css::util::XRefreshable > xRefreshBroadcaster(
+            xSMGR->createInstance(SERVICE_FILTERCONFIGREFRESH),
+            css::uno::UNO_QUERY);
+        if (xRefreshBroadcaster.is())
+            xRefreshBroadcaster->refresh();
+    }
 }
 
 /*-----------------------------------------------
@@ -162,9 +280,12 @@ void SAL_CALL  CacheUpdateListener::changesOccurred(const css::util::ChangesEven
 void SAL_CALL CacheUpdateListener::disposing(const css::lang::EventObject& aEvent)
     throw(css::uno::RuntimeException)
 {
-    // We dont know the configuration access. Because it was given from outside and its lifetime
-    // is controlled there too. We live, till this external config access lives ...
-    // If we are registered for multiple access points, we live a little bit longer :-)
+    // SAFE ->
+    ::osl::ResettableMutexGuard aLock(m_aLock);
+    if (aEvent.Source == m_xConfig)
+        m_xConfig.clear();
+    aLock.clear();
+    // <- SAFE
 }
 
     } // namespace config
