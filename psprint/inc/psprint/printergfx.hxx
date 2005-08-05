dd2287fc1ca5402c2ac8d075a633e193bb2d61ac
Jens-Heiner Rechtien <hr@openoffice.org>
2005-08-05 12:21:15 +0000

    INTEGRATION: CWS cmcfixes12 (1.14.46); FILE MERGED 2005/07/26 10:42:56 cmc 1.14.46.1: #ii52482# change implicit forward decl to explicit

--- psprint/inc/psprint/printergfx.hxx
+++ psprint/inc/psprint/printergfx.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: printergfx.hxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2005-08-05 12:53:06 $
+ *  last change: $Author: hr $ $Date: 2005-08-05 13:21:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -218,6 +218,8 @@ struct GraphicsStatus
 
     GraphicsStatus();
 };
+
+class Font3;
 
 class PrinterGfx
 {
