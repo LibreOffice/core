ead34048570e33884c2aac2cdba06ebf00e4dc67
Jens-Heiner Rechtien <hr@openoffice.org>
2005-08-05 13:26:05 +0000

    INTEGRATION: CWS adc12p (1.2.6); FILE MERGED 2005/07/27 11:21:25 np 1.2.6.1: #i52495#

--- autodoc/source/display/inc/toolkit/htmlfile.hxx
+++ autodoc/source/display/inc/toolkit/htmlfile.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: htmlfile.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-23 09:04:33 $
+ *  last change: $Author: hr $ $Date: 2005-08-05 14:26:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -112,6 +112,7 @@ class DocuFile_Html
     String              sCopyright;
 
     Html::Body          aBodyData;
+    StreamStr           aBuffer;
 };
 
 
