ba028f33f43b72058eed8e995fb017ee106765f3
Rüdiger Timm <rt@openoffice.org>
2005-09-07 17:21:59 +0000

    INTEGRATION: CWS ooo19126 (1.2.90); FILE MERGED 2005/09/05 17:30:10 rt 1.2.90.1: #i54170# Change license header: remove SISSL

--- instsetoo_native/inc_openoffice/unix/makefile.mk
+++ instsetoo_native/inc_openoffice/unix/makefile.mk
@@ -1,44 +1,35 @@
 #*************************************************************************
 #
+#   OpenOffice.org - a multi-platform office productivity suite
+#
 #   $RCSfile: makefile.mk,v $
 #
+#   $Revision: 1.3 $
 #
-#   The Contents of this file are made available subject to the terms of
-#   either of the following licenses
+#   last change: $Author: rt $ $Date: 2005-09-07 18:21:59 $
 #
-#          - GNU Lesser General Public License Version 2.1
-#
-#   Sun Microsystems Inc., October, 2000
-#
-#   GNU Lesser General Public License Version 2.1
-#   =============================================
-#   Copyright 2000 by Sun Microsystems, Inc.
-#   901 San Antonio Road, Palo Alto, CA 94303, USA
-#
-#   This library is free software; you can redistribute it and/or
-#   modify it under the terms of the GNU Lesser General Public
-#   License version 2.1, as published by the Free Software Foundation.
-#
-#   This library is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#   Lesser General Public License for more details.
-#
-#   You should have received a copy of the GNU Lesser General Public
-#   License along with this library; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-#   MA  02111-1307  USA
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
 #
 #
-#   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
 #
-#   Copyright: 2000 by Sun Microsystems, Inc.
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
 #
-#   All Rights Reserved.
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
 #
-#   Contributor(s): _______________________________________
-#
-#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
 #
 #*************************************************************************
 
