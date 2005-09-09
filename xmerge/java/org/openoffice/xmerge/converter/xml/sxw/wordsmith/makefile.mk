de85f0e028561c0c1952103293d1d01c47dc85ea
Rüdiger Timm <rt@openoffice.org>
2005-09-09 10:23:54 +0000

    INTEGRATION: CWS ooo19126 (1.1.16); FILE MERGED 2005/09/05 14:03:00 rt 1.1.16.1: #i54170# Change license header: remove SISSL

--- xmerge/java/org/openoffice/xmerge/converter/xml/sxw/wordsmith/makefile.mk
+++ xmerge/java/org/openoffice/xmerge/converter/xml/sxw/wordsmith/makefile.mk
@@ -1,13 +1,36 @@
 #*************************************************************************
-#*
-#*    $RCSfile: makefile.mk,v $
-#*
-#*    Creation date     09/07/2001
-#*    last change       $Author: mmaher $ $Date: 2002-07-17 12:27:31 $
-#*    $Revision: 1.1 $
-#*
-#*    Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
-#*
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2005-09-09 11:23:25 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
 #*************************************************************************
 PRJNAME=converter
 TARGET=cv_jcsscdcxs_wordsmith
