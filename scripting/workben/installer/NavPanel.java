e3cfa58d8a5149570e966f72201d9cc918496b69
Rüdiger Timm <rt@openoffice.org>
2005-09-09 01:45:45 +0000

    INTEGRATION: CWS ooo19126 (1.2.90); FILE MERGED 2005/09/05 12:05:39 rt 1.2.90.1: #i54170# Change license header: remove SISSL

--- scripting/workben/installer/NavPanel.java
+++ scripting/workben/installer/NavPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class NavPanel extends JPanel implements ActionListener {
-    
+
     NavPanel(InstallWizard wizard, boolean bBack, boolean bNext, boolean bCancel, String prev, String next) {
         setBackground(Color.white);
         setBorder(new javax.swing.border.EtchedBorder(javax.swing.border.EtchedBorder.LOWERED));
@@ -15,19 +15,19 @@ public class NavPanel extends JPanel implements ActionListener {
         navNext = new javax.swing.JButton("Next >>");
         navCancel = new javax.swing.JButton("Cancel");
         setLayout(new GridBagLayout());
-        
+
         gridBagConstraints1 = new java.awt.GridBagConstraints();
         gridBagConstraints1.insets = new java.awt.Insets(1, 1, 1, 1);
-      gridBagConstraints1.anchor = gridBagConstraints1.WEST;    
+      gridBagConstraints1.anchor = gridBagConstraints1.WEST;
 
         gridBagConstraints2 = new java.awt.GridBagConstraints();
         gridBagConstraints2.gridx = 2;
         gridBagConstraints2.gridy = 0;
-        
+
         gridBagConstraints3 = new java.awt.GridBagConstraints();
         gridBagConstraints3.gridx = 6;
         gridBagConstraints3.gridy = 0;
-        
+
         navNext.setEnabled(bNext);
         navBack.setEnabled(bBack);
         navCancel.setEnabled(bCancel);
@@ -38,23 +38,23 @@ public class NavPanel extends JPanel implements ActionListener {
         add(navNext, gridBagConstraints2);
         add(navCancel, gridBagConstraints3);
     }
-    
+
     public void enableNext(boolean bEnable) {
         navNext.setEnabled(bEnable);
     }
-    
+
     public void enableBack(boolean bEnable) {
         navBack.setEnabled(bEnable);
     }
-    
+
     public void enableCancel(boolean bEnable) {
         navCancel.setEnabled(bEnable);
     }
-    
+
     public void enableIDE(boolean bEnable) {
     ideDetected = bEnable;
     }
-    
+
     public void actionPerformed(ActionEvent ev) {
         if ((ev.getSource() == navNext) && (next.length() != 0)) {
             wizard.show(next);
@@ -72,11 +72,11 @@ public class NavPanel extends JPanel implements ActionListener {
         enableIDE(false);
         }
     }
-    
+
     public void setNextListener(ActionListener listener) {
         navNext.addActionListener(listener);
     }
-    
+
     public void setBackListener(ActionListener listener) {
         navBack.addActionListener(listener);
     }
@@ -84,22 +84,22 @@ public class NavPanel extends JPanel implements ActionListener {
     public void setCancelListener(ActionListener listener) {
         navCancel.addActionListener(listener);
     }
-    
+
     public void removeNextListener(ActionListener listener)
     {
         navNext.removeActionListener(listener);
     }
-    
+
     public void removeBackListener(ActionListener listener)
     {
         navBack.removeActionListener(listener);
     }
-    
+
     public void removeCancelListener(ActionListener listener)
     {
         navCancel.removeActionListener(listener);
     }
-    
+
     public JButton navBack;
     public JButton navNext;
     public JButton navCancel;
