9d92ac972f29cb2e7aaff510a2706c9701fb83db
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-23 11:40:55 +0000

    INTEGRATION: CWS dba201b (1.23.6); FILE MERGED 2005/09/21 09:59:33 oj 1.23.6.2: RESYNC: (1.23-1.24); FILE MERGED 2005/07/20 10:16:19 fs 1.23.6.1: #i51255# +documentcontroller

--- dbaccess/source/ui/misc/makefile.mk
+++ dbaccess/source/ui/misc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.24 $
+#   $Revision: 1.25 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 16:15:33 $
+#   last change: $Author: hr $ $Date: 2005-09-23 12:40:55 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -73,7 +73,8 @@ EXCEPTIONSFILES=	\
         $(SLO)$/uiservices.obj				\
         $(SLO)$/RowSetDrop.obj				\
         $(SLO)$/TableCopyHelper.obj			\
-        $(SLO)$/moduledbu.obj
+        $(SLO)$/moduledbu.obj               \
+        $(SLO)$/documentcontroller.obj
 
 
 SLOFILES=	\
