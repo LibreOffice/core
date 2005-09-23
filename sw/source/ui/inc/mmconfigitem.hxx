52cc43763ce4bee4e9910ae9d6918b7e8a5b1bc9
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-23 10:48:52 +0000

    INTEGRATION: CWS dba201b (1.5.24); FILE MERGED 2005/09/21 15:15:14 oj 1.5.24.2: RESYNC: (1.5-1.6); FILE MERGED 2005/07/20 12:44:37 fs 1.5.24.1: #i51255# sharedunocomponent moved to unotools

--- sw/source/ui/inc/mmconfigitem.hxx
+++ sw/source/ui/inc/mmconfigitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmconfigitem.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:50:59 $
+ *  last change: $Author: hr $ $Date: 2005-09-23 11:48:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,8 +50,8 @@
 #ifndef INCLUDED_SWDLLAPI_H
 #include "swdllapi.h"
 #endif
-#ifndef SW_SHARED_UNO_COMPONENT_HXX
-#include "sharedunocomponent.hxx"
+#ifndef SW_UI_SHARED_CONNECTION_HXX
+#include "sharedconnection.hxx"
 #endif
 
 namespace com{namespace sun{namespace star{
