3865734257073ab9b53eca996c6442a8686509ca
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-23 11:28:31 +0000

    INTEGRATION: CWS dba201b (1.2.198); FILE MERGED 2005/09/21 08:27:32 oj 1.2.198.2: RESYNC: (1.2-1.3); FILE MERGED 2005/07/11 13:56:45 fs 1.2.198.1: merging CWS dba201a into CWS dba201b

--- dbaccess/source/ui/dlg/adodatalinks.cxx
+++ dbaccess/source/ui/dlg/adodatalinks.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: adodatalinks.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:52:25 $
+ *  last change: $Author: hr $ $Date: 2005-09-23 12:28:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -93,8 +93,6 @@ BSTR PromptNew(long hWnd)
         dlPrompt->Release( );
         return connstr;
     }
-
-    VARIANT_BOOL pbSuccess;
 
     // Prompt for connection information.
     hr = dlPrompt->PromptNew((IDispatch **)&piTmpConnection);
