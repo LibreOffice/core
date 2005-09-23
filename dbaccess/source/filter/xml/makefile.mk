d5eb84651a52bbce250f773a5fe56a50c2189823
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-23 11:08:08 +0000

    INTEGRATION: CWS dba201b (1.2.146); FILE MERGED 2005/09/21 07:22:56 oj 1.2.146.2: RESYNC: (1.2-1.3); FILE MERGED 2005/07/11 13:56:42 fs 1.2.146.1: merging CWS dba201a into CWS dba201b

--- dbaccess/source/filter/xml/makefile.mk
+++ dbaccess/source/filter/xml/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 14:00:19 $
+#   last change: $Author: hr $ $Date: 2005-09-23 12:08:08 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -106,6 +106,7 @@ SHL1STDLIBS=\
     $(VCLLIB)			\
     $(UNOTOOLSLIB)		\
     $(TOOLSLIB)			\
+    $(TKLIB)			\
     $(COMPHELPERLIB)	\
     $(CPPUHELPERLIB)	\
     $(CPPULIB)			\
