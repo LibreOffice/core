b9befd6c6aea6eb2f53b9f093f0d502f4826426f
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-23 10:48:24 +0000

    INTEGRATION: CWS dba201b (1.5.224); FILE MERGED 2005/09/21 13:13:53 oj 1.5.224.2: RESYNC: (1.5-1.6); FILE MERGED 2005/07/20 12:46:31 fs 1.5.224.1: #i51255# including another file which includes rtl/reference, don't do a 'using namespace rtl' since this conflicts now

--- sw/source/ui/dbui/mailmergehelper.cxx
+++ sw/source/ui/dbui/mailmergehelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mailmergehelper.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:55:11 $
+ *  last change: $Author: hr $ $Date: 2005-09-23 11:48:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -106,7 +106,7 @@ using namespace ::com::sun::star::container;
 using namespace ::com::sun::star::sdb;
 using namespace ::com::sun::star::sdbc;
 using namespace ::com::sun::star::sdbcx;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 namespace SwMailMergeHelper
 {
