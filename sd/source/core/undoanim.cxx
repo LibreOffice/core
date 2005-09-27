efb2725b524de3463e57a31e8421836aeaed86e2
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-27 08:29:50 +0000

    #i10000#: uncommitted changes from CWS impress49

--- sd/source/core/undoanim.cxx
+++ sd/source/core/undoanim.cxx
@@ -1,35 +1,61 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
- *
  *  $RCSfile: undoanim.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 10:43:19 $
+ *  last change: $Author: hr $ $Date: 2005-09-27 09:29:41 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
  *
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
  *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
  *
  ************************************************************************/
 
@@ -77,7 +103,7 @@ UndoAnimation::UndoAnimation( SdDrawDocument* pDoc, SdPage* pThePage )
     try
     {
         if( pThePage->mxAnimationNode.is() )
-            mpImpl->mxOldNode = sd::clone( pThePage->getAnimationNode() );
+            mpImpl->mxOldNode = ::sd::Clone( pThePage->getAnimationNode() );
     }
     catch( Exception& e )
     {
@@ -98,13 +124,13 @@ void UndoAnimation::Undo()
         if( !mpImpl->mbNewNodeSet )
         {
             if( mpImpl->mpPage->mxAnimationNode.is() )
-                mpImpl->mxNewNode.set( sd::clone( mpImpl->mpPage->mxAnimationNode ) );
+                mpImpl->mxNewNode.set( ::sd::Clone( mpImpl->mpPage->mxAnimationNode ) );
             mpImpl->mbNewNodeSet = true;
         }
 
         Reference< XAnimationNode > xOldNode;
         if( mpImpl->mxOldNode.is() )
-            xOldNode = sd::clone( mpImpl->mxOldNode );
+            xOldNode = ::sd::Clone( mpImpl->mxOldNode );
 
         mpImpl->mpPage->setAnimationNode( xOldNode );
     }
@@ -121,7 +147,7 @@ void UndoAnimation::Redo()
     {
         Reference< XAnimationNode > xNewNode;
         if( mpImpl->mxNewNode.is() )
-            xNewNode = sd::clone( mpImpl->mxNewNode );
+            xNewNode = ::sd::Clone( mpImpl->mxNewNode );
         mpImpl->mpPage->setAnimationNode( xNewNode );
     }
     catch( Exception& e )
