a2068fd32d38741c9038c8b9f17bd3de158452e4
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-28 14:45:44 +0000

    INTEGRATION: CWS cmcfixes19 (1.6.24); FILE MERGED 2005/09/22 15:27:56 cmc 1.6.24.1: #i54963# add dependancies for parallel build

--- svx/source/unodialogs/textconversiondlgs/makefile.mk
+++ svx/source/unodialogs/textconversiondlgs/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 00:59:07 $
+#   last change: $Author: hr $ $Date: 2005-09-28 15:45:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,8 @@ DEF1NAME=		$(SHL1TARGET)
 # --- Targets ------------------------------------------------------
 .INCLUDE :	target.mk
 
+$(SRS)$/chinese_dictionarydialog.srs: $(INCCOM)$/chinese_direction.hrc
+$(SRS)$/chinese_translationdialog.srs: $(INCCOM)$/chinese_direction.hrc
 $(SLO)$/chinese_dictionarydialog.obj: $(INCCOM)$/chinese_direction.hrc
 $(SLO)$/chinese_translationdialog.obj: $(INCCOM)$/chinese_direction.hrc
 
