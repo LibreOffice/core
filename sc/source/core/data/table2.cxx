0a4f19b27d2baf7a0505dee13e891f8227f30d14
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-28 10:33:12 +0000

    INTEGRATION: CWS dr37 (1.32.4); FILE MERGED 2005/07/04 13:31:04 nn 1.32.4.1: #i51445# handle merged cells extending from the deleted cells

--- sc/source/core/data/table2.cxx
+++ sc/source/core/data/table2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: table2.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:30:34 $
+ *  last change: $Author: hr $ $Date: 2005-09-28 11:33:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1110,7 +1110,10 @@ const ScPatternAttr* ScTable::GetPattern( SCCOL nCol, SCROW nRow ) const
     if (ValidColRow(nCol,nRow))
         return aCol[nCol].GetPattern( nRow );
     else
-        return NULL;
+    {
+        DBG_ERROR("wrong column or row");
+        return pDocument->GetDefPattern();      // for safety
+    }
 }
 
 
