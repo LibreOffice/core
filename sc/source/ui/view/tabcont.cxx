6640139d22430e295192c7db8d17d5a21f111e80
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-28 11:17:20 +0000

    INTEGRATION: CWS dr37 (1.10.220); FILE MERGED 2005/07/19 10:11:25 nn 1.10.220.1: #i52073# deactivate ole inplace for sheet tab context menu

--- sc/source/ui/view/tabcont.cxx
+++ sc/source/ui/view/tabcont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabcont.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:06:24 $
+ *  last change: $Author: hr $ $Date: 2005-09-28 12:17:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -399,6 +399,10 @@ void ScTabControl::Command( const CommandEvent& rCEvt )
                     Select();
                 }
             }
+
+            // #i52073# OLE inplace editing has to be stopped before showing the sheet tab context menu
+            pViewSh->DeactivateOle();
+
             //  Popup-Menu:
             //  get Dispatcher from ViewData (ViewFrame) instead of Shell (Frame), so it can't be null
             pViewData->GetDispatcher().ExecutePopup( ScResId(RID_POPUP_TAB) );
