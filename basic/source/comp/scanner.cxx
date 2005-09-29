230a4146019cfcc4b41832b8daf3c16e902d9db3
Jens-Heiner Rechtien <hr@openoffice.org>
2005-09-29 15:17:05 +0000

    #i10000#: Integration of CWS ab17fixes was broken. Reverse changes.

--- basic/source/comp/scanner.cxx
+++ basic/source/comp/scanner.cxx
@@ -1,61 +1,35 @@
 /*************************************************************************
  *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
  *  $RCSfile: scanner.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-29 12:49:24 $
+ *  last change: $Author: hr $ $Date: 2005-09-29 16:17:05 $
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
- *
- *         - GNU Lesser General Public License Version 2.1
- *         - Sun Industry Standards Source License Version 1.1
- *
- *  Sun Microsystems Inc., October, 2000
- *
- *  GNU Lesser General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
  *
  *
- *  Sun Industry Standards Source License Version 1.1
- *  =================================================
- *  The contents of this file are subject to the Sun Industry Standards
- *  Source License Version 1.1 (the "License"); You may not use this file
- *  except in compliance with the License. You may obtain a copy of the
- *  License at http://www.openoffice.org/license.html.
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  Software provided under this License is provided on an "AS IS" basis,
- *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
- *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
- *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
- *  See the License for the specific provisions governing your rights and
- *  obligations concerning the Software.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
  *
- *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  *
- *  Copyright: 2000 by Sun Microsystems, Inc.
- *
- *  All Rights Reserved.
- *
- *  Contributor(s): _______________________________________
- *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
  *
  ************************************************************************/
 
@@ -102,8 +76,7 @@ SbiScanner::SbiScanner( const ::rtl::OUString& rBuf, StarBASIC* p ) : aBuf( rBuf
     bNumber  =
     bSymbol  =
     bUsedForHilite =
-    bCompatible =
-    bPrevLineExtentsComment = FALSE;
+    bCompatible = FALSE;
     bHash    =
     bErrors  = TRUE;
 }
@@ -236,18 +209,9 @@ BOOL SbiScanner::NextSym()
     nCol1 = nCol;
 
     // nur Leerzeile?
-    if( !*pLine )
-        goto eoln;
+    if( !*pLine ) goto eoln;
 
-    if( bPrevLineExtentsComment )
-        goto PrevLineCommentLbl;
-
-    if( *pLine == '#' )
-    {
-        pLine++;
-        nCol++;
-        bHash = TRUE;
-    }
+    if( *pLine == '#' ) pLine++, nCol++, bHash = TRUE;
 
     // Symbol? Dann Zeichen kopieren.
     if( BasicSimpleCharClass::isAlpha( *pLine, bCompatible ) || *pLine == '_' )
@@ -485,17 +449,12 @@ BOOL SbiScanner::NextSym()
 
     nCol2 = nCol-1;
 
-PrevLineCommentLbl:
     // Kommentar?
-    if( bPrevLineExtentsComment || (eScanType != SbxSTRING &&
-        ( aSym.GetBuffer()[0] == '\'' || aSym.EqualsIgnoreCaseAscii( "REM" ) ) ) )
+    if( eScanType != SbxSTRING &&
+        ( aSym.GetBuffer()[0] == '\'' || aSym.EqualsIgnoreCaseAscii( "REM" ) ) )
     {
-        bPrevLineExtentsComment = FALSE;
         aSym = String::CreateFromAscii( "REM" );
-        USHORT nLen = String( pLine ).Len();
-        if( pLine[ nLen - 1 ] == '_' && pLine[ nLen - 2 ] == ' ' )
-            bPrevLineExtentsComment = TRUE;
-        nCol2 += nLen;
+        nCol2 += String( pLine ).Len();
         pLine = NULL;
     }
     return TRUE;
