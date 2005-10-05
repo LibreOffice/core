11f04b71de678516a15e9649ed69298216a33afb
Kurt Zenker <kz@openoffice.org>
2005-10-05 12:11:46 +0000

    INTEGRATION: CWS tl12 (1.5.226); FILE MERGED 2005/09/22 13:14:26 tl 1.5.226.3: RESYNC: (1.5-1.6); FILE MERGED 2005/08/18 10:15:13 tl 1.5.226.2: #i51153# include statement fixed 2005/08/15 16:39:10 cl 1.5.226.1: #i51153# changing styles also on chinese conversion

--- sd/source/ui/func/fuhhconv.cxx
+++ sd/source/ui/func/fuhhconv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuhhconv.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:43:18 $
+ *  last change: $Author: kz $ $Date: 2005-10-05 13:11:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,40 @@
  *
  ************************************************************************/
 
+#ifndef _COM_SUN_STAR_I18N_TEXTCONVERSIONOPTION_HPP_
+#include <com/sun/star/i18n/TextConversionOption.hpp>
+#endif
+
+#include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
+#include <com/sun/star/beans/PropertyValue.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <cppuhelper/bootstrap.hxx>
+#include <vcl/msgbox.hxx>
+
+#ifndef _SFXSTYLE_HXX
+#include <svtools/style.hxx>
+#endif
+
+#ifndef _EEITEM_HXX
+#include <svx/eeitem.hxx>
+#endif
+
+#define ITEMID_LANGUAGE EE_CHAR_LANGUAGE_CJK
+#ifndef _SVX_LANGITEM_HXX
+#include <svx/langitem.hxx>
+#endif
+#define ITEMID_FONT EE_CHAR_FONTINFO_CJK
+#ifndef _SVX_FONTITEM_HXX
+#include <svx/fontitem.hxx>
+#endif
+
 #include <fuhhconv.hxx>
 #include "drawdoc.hxx"
 #include "Outliner.hxx"
 #include "DrawViewShell.hxx"
 #include "OutlineViewShell.hxx"
+#include "Window.hxx"
 
 #ifndef SD_VIEW_SHELL_BASE_HXX
 #include "ViewShellBase.hxx"
@@ -47,6 +76,12 @@
 #include "strings.hrc"
 
 class SfxRequest;
+
+#define C2U(cChar)  rtl::OUString::createFromAscii(cChar)
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::uno;
 
 namespace sd {
 
@@ -162,4 +197,113 @@ void FuHangulHanjaConversion::StartConversion( INT16 nSourceLanguage, INT16 nTar
         pView->EndUndo();
 }
 
+
+void FuHangulHanjaConversion::ConvertStyles( INT16 nTargetLanguage, const Font *pTargetFont )
+{
+    if( !pDoc )
+        return;
+
+    SfxStyleSheetBasePool* pStyleSheetPool = pDoc->GetStyleSheetPool();
+    if( !pStyleSheetPool )
+        return;
+
+    SfxStyleSheetBase* pStyle = pStyleSheetPool->First();
+    while( pStyle )
+    {
+        SfxItemSet& rSet = pStyle->GetItemSet();
+
+        const bool bHasParent = pStyle->GetParent().Len() != 0;
+
+        if( !bHasParent || rSet.GetItemState( EE_CHAR_LANGUAGE_CJK, FALSE ) == SFX_ITEM_SET )
+            rSet.Put( SvxLanguageItem( nTargetLanguage, EE_CHAR_LANGUAGE_CJK ) );
+
+        if( pTargetFont &&
+            ( !bHasParent || rSet.GetItemState( EE_CHAR_FONTINFO_CJK, FALSE ) == SFX_ITEM_SET ) )
+        {
+            // set new font attribute
+            SvxFontItem aFontItem( (SvxFontItem&) rSet.Get( EE_CHAR_FONTINFO_CJK ) );
+            aFontItem.GetFamilyName()   = pTargetFont->GetName();
+            aFontItem.GetFamily()       = pTargetFont->GetFamily();
+            aFontItem.GetStyleName()    = pTargetFont->GetStyleName();
+            aFontItem.GetPitch()        = pTargetFont->GetPitch();
+            aFontItem.GetCharSet()      = pTargetFont->GetCharSet();
+            rSet.Put( aFontItem );
+        }
+
+        pStyle = pStyleSheetPool->Next();
+    }
+
+    pDoc->SetLanguage( EE_CHAR_LANGUAGE_CJK, nTargetLanguage );
+}
+
+void FuHangulHanjaConversion::StartChineseConversion()
+{
+    //open ChineseTranslationDialog
+    Reference< XComponentContext > xContext(
+        ::cppu::defaultBootstrap_InitialComponentContext() ); //@todo get context from calc if that has one
+    if(xContext.is())
+    {
+        Reference< lang::XMultiComponentFactory > xMCF( xContext->getServiceManager() );
+        if(xMCF.is())
+        {
+            Reference< ui::dialogs::XExecutableDialog > xDialog(
+                    xMCF->createInstanceWithContext(
+                        rtl::OUString::createFromAscii("com.sun.star.linguistic2.ChineseTranslationDialog")
+                        , xContext), UNO_QUERY);
+            Reference< lang::XInitialization > xInit( xDialog, UNO_QUERY );
+            if( xInit.is() )
+            {
+                //  initialize dialog
+                Reference< awt::XWindow > xDialogParentWindow(0);
+                Sequence<Any> aSeq(1);
+                Any* pArray = aSeq.getArray();
+                PropertyValue aParam;
+                aParam.Name = rtl::OUString::createFromAscii("ParentWindow");
+                aParam.Value <<= makeAny(xDialogParentWindow);
+                pArray[0] <<= makeAny(aParam);
+                xInit->initialize( aSeq );
+
+                //execute dialog
+                sal_Int16 nDialogRet = xDialog->execute();
+                if( RET_OK == nDialogRet )
+                {
+                    //get some parameters from the dialog
+                    sal_Bool bToSimplified = sal_True;
+                    sal_Bool bUseVariants = sal_True;
+                    sal_Bool bCommonTerms = sal_True;
+                    Reference< beans::XPropertySet >  xProp( xDialog, UNO_QUERY );
+                    if( xProp.is() )
+                    {
+                        try
+                        {
+                            xProp->getPropertyValue( C2U("IsDirectionToSimplified") ) >>= bToSimplified;
+                            xProp->getPropertyValue( C2U("IsUseCharacterVariants") ) >>= bUseVariants;
+                            xProp->getPropertyValue( C2U("IsTranslateCommonTerms") ) >>= bCommonTerms;
+                        }
+                        catch( Exception& )
+                        {
+                        }
+                    }
+
+                    //execute translation
+                    sal_Int16 nSourceLang = bToSimplified ? LANGUAGE_CHINESE_TRADITIONAL : LANGUAGE_CHINESE_SIMPLIFIED;
+                    sal_Int16 nTargetLang = bToSimplified ? LANGUAGE_CHINESE_SIMPLIFIED : LANGUAGE_CHINESE_TRADITIONAL;
+                    sal_Int32 nOptions    = bUseVariants ? i18n::TextConversionOption::USE_CHARACTER_VARIANTS : 0;
+                    if( !bCommonTerms )
+                        nOptions = nOptions | i18n::TextConversionOption::CHARACTER_BY_CHARACTER;
+
+                    Font aTargetFont = pWindow->GetDefaultFont(
+                                        DEFAULTFONT_CJK_PRESENTATION,
+                                        nTargetLang, DEFAULTFONT_FLAGS_ONLYONE );
+
+                    StartConversion( nSourceLang, nTargetLang, &aTargetFont, nOptions, sal_False );
+                    ConvertStyles( nTargetLang, &aTargetFont );
+                }
+            }
+            Reference< lang::XComponent > xComponent( xDialog, UNO_QUERY );
+            if( xComponent.is() )
+                xComponent->dispose();
+        }
+    }
+}
 } // end of namespace
