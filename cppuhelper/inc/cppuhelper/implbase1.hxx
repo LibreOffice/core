77226af43144a1c4a8a490ff1fe64bdf7a3f2ffe
Kurt Zenker <kz@openoffice.org>
2005-10-05 13:23:42 +0000

    INTEGRATION: CWS intptr (1.13.30); FILE MERGED 2005/09/13 14:50:00 kendy 1.13.30.1: #i54498# Introduce and use sal_IntPtr/sal_uIntPtr for ints where we have to store a pointer

--- cppuhelper/inc/cppuhelper/implbase1.hxx
+++ cppuhelper/inc/cppuhelper/implbase1.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implbase1.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 09:12:53 $
+ *  last change: $Author: kz $ $Date: 2005-10-05 14:23:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,8 +64,8 @@ namespace cppu
                 1 +1, sal_False, sal_False,
                 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
                 {
-                    { { Ifc1::static_type }, ((sal_Int32)(Ifc1 *) (Impl *) 16) - 16 },
-                    { { ::com::sun::star::lang::XTypeProvider::static_type }, ((sal_Int32)(::com::sun::star::lang::XTypeProvider *) (Impl *) 16) - 16 }
+                    { { Ifc1::static_type }, ((sal_IntPtr)(Ifc1 *) (Impl *) 16) - 16 },
+                    { { ::com::sun::star::lang::XTypeProvider::static_type }, ((sal_IntPtr)(::com::sun::star::lang::XTypeProvider *) (Impl *) 16) - 16 }
                 }
             };
             return reinterpret_cast< class_data * >(&s_cd);
