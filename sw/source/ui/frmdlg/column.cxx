c543a7f40dcdec2031ec46cf330945820ade828c
Kurt Zenker <kz@openoffice.org>
2005-10-06 09:53:59 +0000

    INTEGRATION: CWS intptr (1.27.608); FILE MERGED 2005/09/13 14:50:54 kendy 1.27.608.1: #i54498# Introduce and use sal_IntPtr/sal_uIntPtr for ints where we have to store a pointer

--- sw/source/ui/frmdlg/column.cxx
+++ sw/source/ui/frmdlg/column.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: column.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:49:17 $
+ *  last change: $Author: kz $ $Date: 2005-10-06 10:53:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -705,7 +705,7 @@ BOOL SwColumnPage::FillItemSet(SfxItemSet &rSet)
         ( nPos = aTextDirectionLB.GetSelectEntryPos() ) !=
                                             aTextDirectionLB.GetSavedValue() )
     {
-        sal_uInt32 nDirection = (sal_uInt32)aTextDirectionLB.GetEntryData( nPos );
+        sal_uInt32 nDirection = (sal_uInt32)(sal_IntPtr)aTextDirectionLB.GetEntryData( nPos );
         rSet.Put( SvxFrameDirectionItem( (SvxFrameDirection)nDirection, RES_FRAMEDIR));
     }
     return TRUE;
