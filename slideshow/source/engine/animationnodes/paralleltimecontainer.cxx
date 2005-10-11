3d45aeb1fbfc3766f09812121306201aed4a4e55
Oliver Bolte <obo@openoffice.org>
2005-10-11 07:44:01 +0000

    INTEGRATION: CWS presfixes08 (1.3.16); FILE MERGED 2005/06/13 08:55:20 dbo 1.3.16.1: #i45197# skip effect event Issue number: Submitted by: Reviewed by:

--- slideshow/source/engine/animationnodes/paralleltimecontainer.cxx
+++ slideshow/source/engine/animationnodes/paralleltimecontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: paralleltimecontainer.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:45:40 $
+ *  last change: $Author: obo $ $Date: 2005-10-11 08:44:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
