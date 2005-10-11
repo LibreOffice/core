e6ffb3773e49898b79f3ed3abc4424b091c8ad14
Oliver Bolte <obo@openoffice.org>
2005-10-11 08:01:10 +0000

    INTEGRATION: CWS presfixes08 (1.4.250); FILE MERGED 2005/09/14 16:29:23 dbo 1.4.250.1: #i46618# added get_pointer: support for boost::mem_fn and boost::bind

--- sal/inc/rtl/ref.hxx
+++ sal/inc/rtl/ref.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ref.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:40:48 $
+ *  last change: $Author: obo $ $Date: 2005-10-11 09:01:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -241,6 +241,14 @@ public:
     }
 };
 
+/** @internal
+    Enables boost::mem_fn and boost::bind to recognize Reference.
+*/
+template <typename T>
+inline T * get_pointer( Reference<T> const& r )
+{
+    return r.get();
+}
 
 } // namespace rtl
 
