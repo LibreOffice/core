13f5fe6d93a22abbe1470206ded9fdc13f27c555
Oliver Bolte <obo@openoffice.org>
2005-10-11 07:41:11 +0000

    INTEGRATION: CWS presfixes08 (1.5.10); FILE MERGED 2005/08/08 09:36:53 dbo 1.5.10.1: #i45197# dequeued() -> ActivityBase added end() Issue number: Submitted by: Reviewed by:

--- slideshow/source/engine/activities/simplecontinuousactivitybase.cxx
+++ slideshow/source/engine/activities/simplecontinuousactivitybase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: simplecontinuousactivitybase.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:37:43 $
+ *  last change: $Author: obo $ $Date: 2005-10-11 08:41:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -259,12 +259,5 @@ namespace presentation
 
             return isActive();
         }
-
-        void SimpleContinuousActivityBase::dequeued()
-        {
-            if (! isActive())
-                endAnimation();
-        }
-
     }
 }
