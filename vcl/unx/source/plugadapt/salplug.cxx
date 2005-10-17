945f89e1e1aee7ccee3d2188ac35bda2eff9b82f
Rüdiger Timm <rt@openoffice.org>
2005-10-17 13:23:45 +0000

    INTEGRATION: CWS macosxgcc4 (1.14.90); FILE MERGED 2005/09/20 02:54:07 fheckl 1.14.90.2: RESYNC: (1.14-1.15); FILE MERGED 2005/09/17 23:18:17 fheckl 1.14.90.1: adding necessary casts for compilation to succeed using gcc4 on MacOSX (i54461)

--- vcl/unx/source/plugadapt/salplug.cxx
+++ vcl/unx/source/plugadapt/salplug.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salplug.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:49:20 $
+ *  last change: $Author: rt $ $Date: 2005-10-17 14:23:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -327,7 +327,7 @@ static bool is_cde_desktop( Display* pDisplay )
     OUString aPathName( RTL_CONSTASCII_USTRINGPARAM( "file:///usr/dt/lib/libDtSvc.so" ) );
     if( nDtAtom && ( pLibrary = osl_loadModule( aPathName.pData, SAL_LOADMODULE_DEFAULT ) ) )
     {
-        osl_unloadModule( pLibrary );
+        osl_unloadModule( (_oslModule*)pLibrary );
         return true;
     }
 
