dd0593e0a4a1abbace2026dc224895d0716ccbda
Rüdiger Timm <rt@openoffice.org>
2005-10-18 12:53:43 +0000

    INTEGRATION: CWS os62 (1.9.84); FILE MERGED 2005/09/13 23:59:03 os 1.9.84.3: RESYNC: (1.9-1.10); FILE MERGED 2005/06/28 13:30:55 os 1.9.84.2: #i49921# invalid assertion removed 2005/06/15 13:43:42 os 1.9.84.1: #i49921# remove error when word is automatically replaced

--- svx/source/dialog/SpellDialog.cxx
+++ svx/source/dialog/SpellDialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SpellDialog.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:30:19 $
+ *  last change: $Author: rt $ $Date: 2005-10-18 13:53:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1071,6 +1071,7 @@ bool SpellDialog::ApplyChangeAllList_Impl(SpellPortions& rSentence, bool &bHasRe
             if(xEntry.is())
             {
                 aStart->sText = xEntry->getReplacementText();
+                aStart->xAlternatives = 0;
                 bHasReplaced = true;
             }
             else
@@ -1498,8 +1499,6 @@ bool SentenceEditWindow_Impl::MarkNextError()
         pAction->SetErrorMove(m_nErrorStart, m_nErrorEnd, nOldErrorStart, nOldErrorEnd);
         const SpellErrorAttrib* pOldAttrib = static_cast<const SpellErrorAttrib*>(
                 pTextEngine->FindAttrib( TextPaM(0, nOldErrorStart), TEXTATTR_SPELL_ERROR ));
-        //if the window has been modified then an error should have been found
-        DBG_ASSERT(!bModified || pOldAttrib, "old error could not be found")
         pAction->SetErrorLanguageSelected(pOldAttrib && pOldAttrib->GetAlternatives().is() &&
                 SvxLocaleToLanguage( pOldAttrib->GetAlternatives()->getLocale()) ==
                                         GetSpellDialog()->aLanguageLB.GetSelectLanguage());
