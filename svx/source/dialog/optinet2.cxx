f6dceed9e8246b292a58ef518a483b2f57e0b22c
Rüdiger Timm <rt@openoffice.org>
2005-10-19 07:40:38 +0000

    INTEGRATION: CWS os64 (1.13.228); FILE MERGED 2005/10/14 11:28:37 os 1.13.228.2: RESYNC: (1.13-1.15); FILE MERGED 2005/08/26 10:14:06 os 1.13.228.1: #124347# security page: set value of disabled CheckBoxes

--- svx/source/dialog/optinet2.cxx
+++ svx/source/dialog/optinet2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optinet2.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:45:14 $
+ *  last change: $Author: rt $ $Date: 2005-10-19 08:40:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1543,8 +1543,7 @@ namespace
         bool    b = _rOpt.IsOptionEnabled( _eOpt );
         _rCtrl.Enable( b );
         _rImg.Show( !b );
-        if( b )
-            _rCtrl.Check( _rOpt.IsOptionSet( _eOpt ) );
+        _rCtrl.Check( _rOpt.IsOptionSet( _eOpt ) );
         return b;
     }
 
