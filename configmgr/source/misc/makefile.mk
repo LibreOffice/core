3bed768797a5b7b107c1c4f5de7b97daafa1303e
Rüdiger Timm <rt@openoffice.org>
2005-10-19 11:17:21 +0000

    INTEGRATION: CWS cov2src (1.25.14); FILE MERGED 2005/10/18 08:10:33 rt 1.25.14.1: #126234# Join MWS COV680 m4 into SRC680

--- configmgr/source/misc/makefile.mk
+++ configmgr/source/misc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.25 $
+#   $Revision: 1.26 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 04:10:25 $
+#   last change: $Author: rt $ $Date: 2005-10-19 12:17:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ SLOFILES=	\
         $(SLO)$/mergechange.obj \
         $(SLO)$/oslstream.obj	\
         $(SLO)$/filehelper.obj	\
-        $(SLO)$/trivialbufferedfile.obj \
+        $(SLO)$/bufferedfile.obj \
         $(SLO)$/requestoptions.obj	\
         $(SLO)$/interactionrequest.obj	\
         $(SLO)$/configinteractionhandler.obj	\
@@ -72,7 +72,7 @@ SLOFILES=	\
 OBJFILES=	\
         $(OBJ)$/oslstream.obj	\
         $(OBJ)$/filehelper.obj	\
-        $(OBJ)$/trivialbufferedfile.obj \
+        $(OBJ)$/bufferedfile.obj \
 
 
 # --- Targets ----------------------------------
