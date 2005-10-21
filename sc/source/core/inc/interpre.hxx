8e9790937da60e2c8fcba1d09a81883512afa258
Rüdiger Timm <rt@openoffice.org>
2005-10-21 10:53:21 +0000

    INTEGRATION: CWS dr41 (1.21.78); FILE MERGED 2005/10/04 21:45:08 dr 1.21.78.2: RESYNC: (1.21-1.23); FILE MERGED 2005/08/25 14:13:00 dr 1.21.78.1: #i42727# BAHTTEXT

--- sc/source/core/inc/interpre.hxx
+++ sc/source/core/inc/interpre.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interpre.hxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 11:35:15 $
+ *  last change: $Author: rt $ $Date: 2005-10-21 11:53:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -451,6 +451,7 @@ void ScErrorType();
 void ScDBArea();
 void ScColRowNameAuto();
 void ScHyperLink();
+void ScBahtText();
 void ScCalcTeam();
 void ScAnswer();
 void ScTTT();
