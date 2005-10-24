6cf7d1a92386fa15e8a9dce5d36b850f8277dadf
Rüdiger Timm <rt@openoffice.org>
2005-10-24 06:39:54 +0000

    INTEGRATION: CWS impress69 (1.2.434); FILE MERGED 2005/10/14 11:01:20 af 1.2.434.3: RESYNC: (1.2-1.3); FILE MERGED 2005/09/21 15:54:45 af 1.2.434.2: #i54916# Support for bitmap compression. 2005/09/14 11:12:18 af 1.2.434.1: #i54589# Adaption to changes of the BitmapCache class.

--- sd/source/ui/slidesorter/cache/SlsCacheCompactor.cxx
+++ sd/source/ui/slidesorter/cache/SlsCacheCompactor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsCacheCompactor.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:09:41 $
+ *  last change: $Author: rt $ $Date: 2005-10-24 07:39:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,131 +35,207 @@
 
 #include "SlsCacheCompactor.hxx"
 
+#include "SlsBitmapCompressor.hxx"
 #include "SlsBitmapCache.hxx"
-#include <svx/svdpage.hxx>
+#include "SlsCacheCompactor.hxx"
+#include "SlsCacheConfiguration.hxx"
+
+#include <rtl/ustring.hxx>
+#include <com/sun/star/uno/Any.hxx>
 #include <set>
 
-using namespace std;
+using namespace ::com::sun::star::uno;
+
+// Uncomment the definition of VERBOSE to get some more OSL_TRACE messages.
+#ifdef DEBUG
+//#define VERBOSE
+#endif
+
+namespace {
+
+/** This is a trivial implementation of the CacheCompactor interface class.
+    It ignores calls to RequestCompaction() and thus will never decrease the
+    total size of off-screen preview bitmaps.
+*/
+class NoCacheCompaction
+    : public ::sd::slidesorter::cache::CacheCompactor
+{
+public:
+    NoCacheCompaction (
+        ::sd::slidesorter::cache::BitmapCache& rCache,
+        sal_Int32 nMaximalCacheSize)
+        : CacheCompactor(rCache, nMaximalCacheSize)
+    {}
+
+    virtual void RequestCompaction (void) { /* Ignored */ };
+
+protected:
+    virtual void Run (void) { /* Do nothing */ };
+};
+
+
+
+
+/** This implementation of the CacheCompactor interface class uses one of
+    several bitmap compression algorithms to reduce the number of the bytes
+    of the off-screen previews in the bitmap cache.  See the documentation
+    of CacheCompactor::Create() for more details on configuration properties
+    that control the choice of compression algorithm.
+*/
+class CacheCompactionByCompression
+    : public ::sd::slidesorter::cache::CacheCompactor
+{
+public:
+    CacheCompactionByCompression (
+        ::sd::slidesorter::cache::BitmapCache& rCache,
+        sal_Int32 nMaximalCacheSize,
+        const ::boost::shared_ptr< ::sd::slidesorter::cache::BitmapCompressor>& rpCompressor);
+
+protected:
+    virtual void Run (void);
+
+private:
+    ::boost::shared_ptr< ::sd::slidesorter::cache::BitmapCompressor>  mpCompressor;
+};
+
+
+} // end of anonymous namespace
 
 namespace sd { namespace slidesorter { namespace cache {
 
-/** Compare elements of the bitmap cache according to their last access
-    time.
-*/
-class AccessTimeComparator
+
+::std::auto_ptr<CacheCompactor> CacheCompactor::Create (
+    BitmapCache& rCache,
+    sal_Int32 nMaximalCacheSize)
 {
-public:
-    bool operator () (
-        const CacheBitmapContainer::value_type& e1,
-        const CacheBitmapContainer::value_type& e2)
-    {
-        return e1.second.mnLastAccessTime < e2.second.mnLastAccessTime;
-    }
-};
+    static const ::rtl::OUString sNone (RTL_CONSTASCII_USTRINGPARAM("None"));
+    static const ::rtl::OUString sCompress (RTL_CONSTASCII_USTRINGPARAM("Compress"));
+    static const ::rtl::OUString sErase (RTL_CONSTASCII_USTRINGPARAM("Erase"));
+    static const ::rtl::OUString sResolution (RTL_CONSTASCII_USTRINGPARAM("ResolutionReduction"));
+    static const ::rtl::OUString sPNGCompression (RTL_CONSTASCII_USTRINGPARAM("PNGCompression"));
 
-typedef set<CacheBitmapContainer::value_type, AccessTimeComparator>
-    SortedCache;
+    ::boost::shared_ptr<BitmapCompressor> pCompressor;
+    ::rtl::OUString sCompressionPolicy(sPNGCompression);
+    Any aCompressionPolicy (CacheConfiguration::Instance()->GetValue(
+        ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("CompressionPolicy"))));
+    if (aCompressionPolicy.has<rtl::OUString>())
+        aCompressionPolicy >>= sCompressionPolicy;
+    if (sCompressionPolicy == sNone)
+        pCompressor.reset(new NoBitmapCompression());
+    else if (sCompressionPolicy == sErase)
+        pCompressor.reset(new CompressionByDeletion());
+    else if (sCompressionPolicy == sResolution)
+        pCompressor.reset(new ResolutionReduction());
+    else
+        pCompressor.reset(new PngCompression());
 
+    ::std::auto_ptr<CacheCompactor> pCompactor (NULL);
+    ::rtl::OUString sCompactionPolicy(sCompress);
+    Any aCompactionPolicy (CacheConfiguration::Instance()->GetValue(
+        ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("CompactionPolicy"))));
+    if (aCompactionPolicy.has<rtl::OUString>())
+        aCompactionPolicy >>= sCompactionPolicy;
+    if (sCompactionPolicy == sNone)
+        pCompactor.reset(new NoCacheCompaction(rCache,nMaximalCacheSize));
+    else
+        pCompactor.reset(new CacheCompactionByCompression(rCache,nMaximalCacheSize,pCompressor));
 
-//=====  NoCompaction  ========================================================
-
-void NoCompaction::operator() (BitmapCache& rCache, sal_Int32 nMaximalSize)
-{
+    return pCompactor;
 }
 
 
 
 
-//=====  CompactionByRemoval ==================================================
-
-void CompactionByRemoval::operator() (
-    BitmapCache& rCache,
-    sal_Int32 nMaximalSize)
+void CacheCompactor::RequestCompaction (void)
 {
-    if (rCache.GetSize()  > nMaximalSize)
-    {
-        OSL_TRACE ("bitmap cache uses to much space: %d > %d",
-            rCache.GetSize(), nMaximalSize);
-
-        // Sort the entries in the cache by creating a new container.
-        SortedCache aSortedCache;
-        copy (
-            rCache.GetContainer().begin(),
-            rCache.GetContainer().end(),
-            inserter(aSortedCache, aSortedCache.end()));
-
-        // Free one bitmap at a time until there is enough free space.  The
-        // number of bitmaps in the cache is used as a guard against
-        // infinite loops when the maximal cache size is too small of the
-        // bitmaps are too large.
-        while (rCache.GetSize() > nMaximalSize && ! aSortedCache.empty())
-        {
-            CacheBitmapContainer::value_type aEntry (*aSortedCache.begin());
-            if ( ! aEntry.second.mbIsPrecious)
-            {
-                // Free the bitmap and thus increase the available space.
-                rCache.ReleaseBitmap (aEntry.first);
-
-                OSL_TRACE ("    there are now %d bytes occupied after releasing the bitmap for page %d",
-                    rCache.GetSize(), aEntry.first->GetPageNum());
-            }
-
-            aSortedCache.erase (aSortedCache.begin());
-        }
-    }
+    if ( ! mbIsCompactionRunning && ! maCompactionTimer.IsActive())
+        maCompactionTimer.Start();
 }
 
 
 
 
-//=====  CompactionByReduction  ===============================================
-
-void CompactionByReduction::operator() (
+CacheCompactor::CacheCompactor(
     BitmapCache& rCache,
-    sal_Int32 nMaximalSize)
+    sal_Int32 nMaximalCacheSize)
+    : mrCache(rCache),
+      mnMaximalCacheSize(nMaximalCacheSize),
+      mbIsCompactionRunning(false)
 {
-    if (rCache.GetSize()  > nMaximalSize)
+    maCompactionTimer.SetTimeout(100 /*ms*/);
+    maCompactionTimer.SetTimeoutHdl(LINK(this,CacheCompactor,CompactionCallback));
+
+}
+
+
+
+
+IMPL_LINK(CacheCompactor, CompactionCallback, Timer*, pTimer)
+{
+    mbIsCompactionRunning = true;
+
+    try
     {
-        OSL_TRACE ("bitmap cache uses to much space: %d > %d",
-            rCache.GetSize(), nMaximalSize);
-
-        // Sort the entries in the cache by creating a new container.
-        SortedCache aSortedCache;
-        copy (
-            rCache.GetContainer().begin(),
-            rCache.GetContainer().end(),
-            inserter(aSortedCache, aSortedCache.end()));
-
-        // Free one bitmap at a time until there is enough free space.  The
-        // number of bitmaps in the cache is used as a guard against
-        // infinite loops when the maximal cache size is too small or the
-        // bitmaps are too large.
-        while (rCache.GetSize() > nMaximalSize && ! aSortedCache.empty())
-        {
-            CacheBitmapContainer::value_type aEntry (*aSortedCache.begin());
-            if ( ! aEntry.second.mbIsPrecious)
-            {
-                Size aSize (aEntry.second.maBitmap.GetSizePixel());
-                if (aSize.Width() != 0)
-                {
-                    int nWidth = 100;
-                    int nHeight = aSize.Height() * nWidth / aSize.Width() ;
-                    aEntry.second.maBitmap.Scale (Size(nWidth,nHeight));
-                    rCache.SetBitmap (
-                        aEntry.first,
-                        aEntry.second.maBitmap,
-                        false);
-                }
-
-                OSL_TRACE ("    there are now %d bytes occupied after scaling down bitmap for page %d",
-                    rCache.GetSize(), aEntry.first->GetPageNum());
-            }
-            aSortedCache.erase (aSortedCache.begin());
-        }
+        Run();
     }
+    catch(::com::sun::star::uno::RuntimeException e) { }
+    catch(::com::sun::star::uno::Exception e) { }
+
+    mbIsCompactionRunning = false;
+    return 1;
 }
 
 
 
 
 } } } // end of namespace ::sd::slidesorter::cache
+
+
+
+
+namespace {
+
+//===== CacheCompactionByCompression ==========================================
+
+CacheCompactionByCompression::CacheCompactionByCompression (
+    ::sd::slidesorter::cache::BitmapCache& rCache,
+    sal_Int32 nMaximalCacheSize,
+    const ::boost::shared_ptr< ::sd::slidesorter::cache::BitmapCompressor>& rpCompressor)
+    : CacheCompactor(rCache,nMaximalCacheSize),
+      mpCompressor(rpCompressor)
+{
+}
+
+
+
+
+void CacheCompactionByCompression::Run (void)
+{
+    if (mrCache.GetSize() > mnMaximalCacheSize)
+    {
+#ifdef VERBOSE
+        OSL_TRACE ("bitmap cache uses to much space: %d > %d",
+            mrCache.GetSize(), mnMaximalCacheSize);
+#endif
+
+        ::std::auto_ptr< ::sd::slidesorter::cache::BitmapCache::CacheIndex> pIndex (
+            mrCache.GetCacheIndex(false,false));
+        ::sd::slidesorter::cache::BitmapCache::CacheIndex::iterator iIndex;
+        for (iIndex=pIndex->begin(); iIndex!=pIndex->end(); ++iIndex)
+        {
+            if (*iIndex == NULL)
+                continue;
+
+            mrCache.Compress(*iIndex, mpCompressor);
+            if (mrCache.GetSize() < mnMaximalCacheSize)
+                break;
+        }
+        mrCache.ReCalculateTotalCacheSize();
+#ifdef VERBOSE
+        OSL_TRACE ("    there are now %d bytes occupied", mrCache.GetSize());
+#endif
+    }
+}
+
+
+} // end of anonymous namespace
