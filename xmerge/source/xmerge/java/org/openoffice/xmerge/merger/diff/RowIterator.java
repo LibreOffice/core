d4b1b47901741ee1965433e74a7966ac121caf85
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-24 17:17:06 +0000

    INTEGRATION: CWS lo8 (1.1.2); FILE ADDED 2005/06/08 16:31:07 lo 1.1.2.1: restructuring of project and fix for #i44847#

---
+++ xmerge/source/xmerge/java/org/openoffice/xmerge/merger/diff/RowIterator.java
@@ -0,0 +1,112 @@
+/************************************************************************
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package org.openoffice.xmerge.merger.diff;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import org.openoffice.xmerge.ConverterCapabilities;
+import org.openoffice.xmerge.merger.Iterator;
+import org.openoffice.xmerge.converter.xml.OfficeConstants;
+import org.openoffice.xmerge.util.Debug;
+import org.openoffice.xmerge.util.Resources;
+
+
+/**
+ *  This is an implementation of the <code>Iterator</code> interface and extends
+ *  <code>NodeIterator</code>.  It will traverse the tree and find row sequences.
+ *
+ * @author smak
+ */
+public final class RowIterator extends NodeIterator {
+
+    private Resources res = Resources.getInstance();
+
+    // TODO: should compare the ConverterCapabilities supported feature only!
+    // otherwise even though one with a chart, one without, will still be
+    // considered to be not equivalent.
+
+    /**
+     *  Standard constructor.
+     *
+     *  @param  cc    The <code>ConverterCapabilities</code>.
+     *  @param  node  The initial root <code>Node</code>.
+     */
+    public RowIterator(ConverterCapabilities cc, Node node) {
+        super(cc, node);
+    }
+
+    /**
+     *  Overwrite the parent <code>nodeSupported</code> method.  Only
+     *  row <code>Node</code> objects are supported.
+     *
+     *  @param  node  <code>Node</code> to check.
+     *
+     *  @return  true if the <code>Node</code> is supported, false otherwise.
+     */
+    protected boolean nodeSupported(Node node) {
+
+        // can use an array later to check all possible tags for
+        // future expansion
+        if (node.getNodeType() == Node.ELEMENT_NODE &&
+            node.getNodeName().equals(OfficeConstants.TAG_TABLE_ROW)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
