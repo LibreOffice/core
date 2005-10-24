0766fe27979b4fa3094dc173ba111c7dda19ea04
Rüdiger Timm <rt@openoffice.org>
2005-10-24 06:41:10 +0000

    INTEGRATION: CWS impress69 (1.3.374); FILE MERGED 2005/10/14 11:02:05 af 1.3.374.7: RESYNC: (1.3-1.4); FILE MERGED 2005/09/22 07:47:17 af 1.3.374.6: #i54916# Support for bitmap compression. 2005/09/14 11:18:59 af 1.3.374.5: #i54589# Added support for persistent caches. 2005/09/02 08:29:00 af 1.3.374.4: #i54034# Moved the RequestPriorityClasses enum from SlsPageCache.cxx to its own file and removed it as template argument. 2005/09/01 14:31:38 af 1.3.374.3: #i54034# Extended request priority classes. 2005/09/01 13:30:40 af 1.3.374.2: #i54028# Moved configuration code to SlsCacheConfiguration.cxx. 2005/09/01 12:04:50 af 1.3.374.1: #i54097# Processing new MultiPaneGUI/SlideSorter/PreviewCache/CacheSize configuration entry.

--- sd/source/ui/slidesorter/cache/SlsPageCache.cxx
+++ sd/source/ui/slidesorter/cache/SlsPageCache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsPageCache.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:10:44 $
+ *  last change: $Author: rt $ $Date: 2005-10-24 07:41:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,136 +33,33 @@
  *
  ************************************************************************/
 
-/** All classes and template classes in this file belong to the preview
-    bitmap cache.  Its tasks are the creation and caching of preview bitmaps
-    for page objects.  The template class GenericPageCache is the facade
-    whose specialization will be visible to the using class.  Here is a list
-    of all collaborating classes.  Note that this is a work in progress.
-    The division into the classes is the first step of identifiying and
-    separation of the responsibilities.  Especially the choices of making a
-    class a template or not was made ad hoc and will likely change.
-
-    - BitmapCache
-    The low level cache of bitmaps.  Apart from storing bitmaps and keeping
-    track of how much memory they use the BitmapCache class will have the
-    responsibility of destroying bitmaps when it runs out of space.
-
-    - template <class Queue, bool UseAheadOfTimeRequests>
-    class RequestFactory
-    This function object class is responsible for creating requests after a
-    model change when all preview bitmaps become invalid.  It can be used to
-    generate ahead-of-time requests.
-
-    - class BitmapFactory
-    The static class creates preview bitmaps for page objects.
-
-    - template<class RequestData>
-    class Request
-    This simple class bundles the data that are necessary for the queue to
-    manage the requests.  Apart from the RequestData, typically a
-    view-object-contact object, that are the priority class and the priority
-    inside that class.
-
-    - template<class RequestData>
-    class RequestDataComparator
-    This function object compares request objects according to their request
-    data (pointer).  It is used for finding a request object that is
-    associated with a given request data.
-
-    - template<class RequestData>
-    class RequestComparator
-    This function object compares request objects according to their
-    priority.  Objects in a lower class (0) are processed earlier then
-    objects in a higher class (1).  When two objects belong to the same
-    priority class they are ordered according to their priority-in-class
-    value.  The function object is used to maintain the order of request
-    objects in a set data structure.
-
-    - template<class RequestData, int ClassCount>
-    class GenericRequestQueue
-    The request queue maintains a priority queue where each request for the
-    generation of a preview bitmap has a priority that consists of two
-    values: The priority class that is a rough classification that allows
-    the distinction of requests for visible and for non-visible page
-    objects.  The second value is the priority inside that class.
-
-    - class QueueProcessorBase
-    This class exists because the IMPL_LINK macro can not handle a template
-    class.
-
-    - template <class Queue, class RequestData, class BitmapCache,
-    class BitmapFactory>
-    class QueueProcessor
-    The queue processor is responsible for processing the requests in the
-    request queue.  In this class this is done asynchronously with a timer.
-    Alternative implementations may use threads or can even process requests
-    synchronously.
-
-    - template<class RequestData,
-    class CreationManager,
-    class BitmapCache,
-    class RequestQueue,
-    class QueueProcessor>
-    class GenericPageCache
-    The main class in this file uses all the ones above to implement a) a cache
-    and b) a factory of preview bitmaps.
-*/
-
-
 #include "cache/SlsPageCache.hxx"
+#include "cache/SlsPageCacheManager.hxx"
 #include "SlsBitmapCache.hxx"
 #include "SlsGenericPageCache.hxx"
 #include "SlsGenericRequestQueue.hxx"
 #include "SlsRequestFactory.hxx"
-#include "SlsCacheCompactor.hxx"
-
-
-// First we make some typedefs that define the arguments to the cache
-// templates.  Currently there are two arguments for which exist
-// alternatives:
-// 1.  The queue processor is implemented once with threads and once
-//     without.
 #include "SlsQueueProcessor.hxx"
-#define QUEUE_PROCESSOR QueueProcessor
-//#include "SlsQueueProcessorThread.hxx"
-//#define QUEUE_PROCESSOR QueueProcessorThread
-
-// 2.  There are two implementations of the preview factory.  One uses the
-// class PreviewRenderer (originally created for the toolpanel master page
-// controls.).  The other uses the new drawing layer more directly.
 #include "SlsPreviewBitmapFactory2.hxx"
-#define PREVIEW_BITMAP_FACTORY PreviewBitmapFactory2
-//#include "SlsPreviewBitmapFactory.hxx"
-//#define PREVIEW_BITMAP_FACTORY PreviewBitmapFactory
-
 
 
 using namespace ::sd::slidesorter::model;
 using namespace ::sd::slidesorter::view;
+using namespace ::com::sun::star;
 
 
 namespace sd { namespace slidesorter { namespace cache {
 
 
-typedef GenericRequestQueue<PageCache::RequestData,2> Queue;
-
-
-typedef QUEUE_PROCESSOR<
-    Queue,
-    PageCache::RequestData,
-    BitmapCache,
-    PREVIEW_BITMAP_FACTORY> Processor;
-
-typedef GenericPageCache<
-    PageCache::RequestData,
-    RequestFactory<Queue,false>,
-    BitmapCache,
-    Queue,
-    CompactionByReduction,
-    Processor
+typedef GenericRequestQueue<PageCache::RequestData> Queue;
+typedef QueueProcessor<Queue, PageCache::RequestData, PreviewBitmapFactory2> Processor;
+typedef GenericPageCache< PageCache::RequestData, RequestFactory<Queue,false>, Queue, Processor
     > BaseClass;
 
 
+/** The implementation class simply hides the actual GenericPageCache base
+    class.
+*/
 class PageCache::PageCacheImplementation
     : public BaseClass
 {
@@ -170,19 +67,23 @@ public:
     PageCacheImplementation (
         view::SlideSorterView& rView,
         model::SlideSorterModel& rModel,
-        sal_Int32 nMaximalCacheSize)
-        : BaseClass (rView,rModel, nMaximalCacheSize)
+        const Size& rPreviewSize)
+        : BaseClass (rView,rModel, rPreviewSize)
     {}
 };
 
 
+
+
+//===== PageCache =============================================================
+
 PageCache::PageCache (
     view::SlideSorterView& rView,
     model::SlideSorterModel& rModel,
-    sal_Int32 nMaximalCacheSize)
-    : mpImplementation (::std::auto_ptr<PageCacheImplementation>(
-        new PageCacheImplementation(rView,rModel,nMaximalCacheSize)))
+    const Size& rPreviewSize)
+    : mpImplementation(NULL)
 {
+    mpImplementation.reset(new PageCacheImplementation(rView,rModel,rPreviewSize));
 }
 
 
@@ -190,6 +91,14 @@ PageCache::PageCache (
 
 PageCache::~PageCache (void)
 {
+}
+
+
+
+
+void PageCache::ChangeSize(const Size& rPreviewSize)
+{
+    mpImplementation->ChangePreviewSize(rPreviewSize);
 }
 
 
@@ -223,22 +132,6 @@ void PageCache::InvalidatePreviewBitmap (const RequestData& rRequestData)
 
 
 
-void PageCache::DecreaseRequestPriority (RequestData& rRequestData)
-{
-    mpImplementation->DecreaseRequestPriority(rRequestData);
-}
-
-
-
-
-void PageCache::IncreaseRequestPriority (RequestData& rRequestData)
-{
-    mpImplementation->IncreaseRequestPriority(rRequestData);
-}
-
-
-
-
 void PageCache::ReleasePreviewBitmap (RequestData& rRequestData)
 {
     mpImplementation->ReleasePreviewBitmap(rRequestData);
@@ -247,9 +140,9 @@ void PageCache::ReleasePreviewBitmap (RequestData& rRequestData)
 
 
 
-void PageCache::InvalidateCache (void)
+void PageCache::InvalidateCache (bool bUpdateCache)
 {
-    mpImplementation->InvalidateCache();
+    mpImplementation->InvalidateCache(bUpdateCache);
 }
 
 
@@ -259,6 +152,8 @@ void PageCache::SetPreciousFlag (RequestData& rRequestData, bool bIsPrecious)
 {
     mpImplementation->SetPreciousFlag (rRequestData, bIsPrecious);
 }
+
+
 
 
 } } } // end of namespace ::sd::slidesorter::cache
