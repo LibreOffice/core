cb580f7ca63207767cd16be2e0f1fc00e8ddb33a
Rüdiger Timm <rt@openoffice.org>
2005-10-24 06:41:45 +0000

    INTEGRATION: CWS impress69 (1.4.204); FILE MERGED 2005/10/14 11:03:07 af 1.4.204.2: RESYNC: (1.4-1.5); FILE MERGED 2005/09/01 13:35:17 af 1.4.204.1: #i54028# Changed times between requests.  Looking into configuration for these values as well.

--- sd/source/ui/slidesorter/cache/SlsQueueProcessor.cxx
+++ sd/source/ui/slidesorter/cache/SlsQueueProcessor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsQueueProcessor.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:11:52 $
+ *  last change: $Author: rt $ $Date: 2005-10-24 07:41:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,6 +34,7 @@
  ************************************************************************/
 
 #include "SlsQueueProcessor.hxx"
+#include "SlsCacheConfiguration.hxx"
 
 namespace sd { namespace slidesorter { namespace cache {
 
@@ -41,10 +42,27 @@ namespace sd { namespace slidesorter { namespace cache {
 //===== QueueProcessorBase  ===================================================
 
 QueueProcessorBase::QueueProcessorBase (void)
-    : mnTimeBetweenHighPriorityRequests (50/*ms*/),
-      mnTimeBetweenLowPriorityRequests (250/*ms*/),
+    : mnTimeBetweenHighPriorityRequests (10/*ms*/),
+      mnTimeBetweenLowPriorityRequests (100/*ms*/),
       mnTimeBetweenRequestsWhenNotIdle (1000/*ms*/)
 {
+    // Look into the configuration if there for overriding values.
+    ::com::sun::star::uno::Any aTimeBetweenReqeusts;
+    aTimeBetweenReqeusts = CacheConfiguration::Instance()->GetValue(
+        ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("TimeBetweenHighPriorityRequests")));
+    if (aTimeBetweenReqeusts.has<sal_Int32>())
+        aTimeBetweenReqeusts >>= mnTimeBetweenHighPriorityRequests;
+
+    aTimeBetweenReqeusts = CacheConfiguration::Instance()->GetValue(
+        ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("TimeBetweenLowPriorityRequests")));
+    if (aTimeBetweenReqeusts.has<sal_Int32>())
+        aTimeBetweenReqeusts >>= mnTimeBetweenLowPriorityRequests;
+
+    aTimeBetweenReqeusts = CacheConfiguration::Instance()->GetValue(
+        ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("TimeBetweenRequestsDuringShow")));
+    if (aTimeBetweenReqeusts.has<sal_Int32>())
+        aTimeBetweenReqeusts >>= mnTimeBetweenRequestsWhenNotIdle;
+
     maTimer.SetTimeoutHdl (LINK(this,QueueProcessorBase,ProcessRequest));
     maTimer.SetTimeout (mnTimeBetweenHighPriorityRequests);
 }
