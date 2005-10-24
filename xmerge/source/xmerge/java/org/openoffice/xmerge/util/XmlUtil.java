8673db5d3bd988e033515e2f705633c67233a6e8
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-24 17:22:06 +0000

    INTEGRATION: CWS lo8 (1.1.2); FILE ADDED 2005/06/08 16:31:18 lo 1.1.2.1: restructuring of project and fix for #i44847#

---
+++ xmerge/source/xmerge/java/org/openoffice/xmerge/util/XmlUtil.java
@@ -0,0 +1,217 @@
+/************************************************************************
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+package org.openoffice.xmerge.util;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+
+/**
+ *  Class containing static util methods for handling XML trees.
+ *
+ *  @author     smak
+ */
+public final class XmlUtil {
+
+
+    /**
+     *  Perform a deep clone of certain <code>Node</code> which
+     *  will base on the document <code>Node</code> of the old
+     *  <code>Node</code>.
+     *
+     *  @param  oldNode  The <code>Document</code> of this
+     *                   <code>Node</code> is used to clone
+     *                   the <code>Node</code>
+     *  @param  newNode  The <code>Node</code> to clone.
+     *
+     *  @return  The cloned <code>Node</code>.
+     */
+    public static Node deepClone(Node oldNode, Node newNode) {
+        Document docNode = oldNode.getOwnerDocument();
+
+        // clone the starting node
+        Node clonedNode = cloneNode(docNode, newNode);
+
+        // then clone the sub-tree recursively
+        cloneTree(docNode, clonedNode, newNode);
+
+        return clonedNode;
+    }
+
+
+    /**
+     *  Clone the sub-tree under certain given <code>Node</code>
+     *
+     *  @param  docNode  The <code>Document</code> used to clone
+     *                   the <code>Node</code>.
+     *  @param  oldNode  The <code>Node</code> to clone.
+     *  @param  newNode  The destination <code>Node</code>.
+     */
+    private static void cloneTree(Document docNode, Node oldNode, Node newNode) {
+
+        NodeList nodeList = newNode.getChildNodes();
+        int nodeListLen = nodeList.getLength();
+
+        for (int i = 0; i < nodeListLen; i++) {
+            Node newClonedChild = cloneNode(docNode, nodeList.item(i));
+            if (newClonedChild != null) {
+                oldNode.appendChild(newClonedChild);
+                cloneTree(docNode, newClonedChild, nodeList.item(i));
+            }
+        }
+    }
+
+
+    /**
+     * Clone a <code>Node</code> (either text or element).
+     *
+     * @param  docNode  The <code>Document</code> used to
+     *                  clone the <code>Node</code>.
+     * @param  newNode  The <code>Node</code> to clone.
+     *
+     * @return  The cloned <code>Node</code>.
+     */
+    private static Node cloneNode(Document docNode, Node newNode) {
+
+        Node clonedNode = null;
+
+        // only support text node and element node (will copy the attributes)
+        switch (newNode.getNodeType()) {
+            case Node.TEXT_NODE:
+                String textStr = newNode.getNodeValue();
+                clonedNode = docNode.createTextNode(textStr);
+                break;
+            case Node.ELEMENT_NODE:
+                Element oldElem = (Element)newNode;
+                String tagName  = newNode.getNodeName();
+                Element newElem = (docNode.createElement(tagName));
+
+                // copy the attributes
+                NamedNodeMap attrs = oldElem.getAttributes();
+
+                for (int i = 0; i < attrs.getLength(); i++) {
+                    newElem.setAttribute(attrs.item(i).getNodeName(),
+                                         attrs.item(i).getNodeValue());
+                }
+                clonedNode = newElem;
+                break;
+        }
+        return clonedNode;
+    }
+
+
+    /**
+     *  Returns the name and type of an XML DOM <code>Node</code>.
+     *
+     *  @param  node  <code>Node</code> to query.
+     *
+     *  @return  Name and type of XML DOM <code>Node</code>.
+     */
+    public static String getNodeInfo(Node node) {
+
+        String str = null;
+        switch (node.getNodeType()) {
+
+            case Node.ELEMENT_NODE:
+                str = "ELEMENT";
+                break;
+            case Node.ATTRIBUTE_NODE:
+                str = "ATTRIBUTE";
+                break;
+            case Node.TEXT_NODE:
+                str = "TEXT";
+                break;
+            case Node.CDATA_SECTION_NODE:
+                str = "CDATA_SECTION";
+                break;
+            case Node.ENTITY_REFERENCE_NODE:
+                str = "ENTITY_REFERENCE";
+                break;
+            case Node.ENTITY_NODE:
+                str = "ENTITY";
+                break;
+            case Node.PROCESSING_INSTRUCTION_NODE:
+                str = "PROCESSING_INSTRUCTION";
+                break;
+            case Node.COMMENT_NODE:
+                str = "COMMENT";
+                break;
+            case Node.DOCUMENT_NODE:
+                str = "DOCUMENT";
+                break;
+            case Node.DOCUMENT_TYPE_NODE:
+                str = "DOCUMENT_TYPE";
+                break;
+            case Node.DOCUMENT_FRAGMENT_NODE:
+                str = "DOCUMENT_FRAGMENT";
+                break;
+            case Node.NOTATION_NODE:
+                str = "NOTATION";
+                break;
+        }
+
+        StringBuffer buffer = new StringBuffer("name=\"");
+        buffer.append(node.getNodeName());
+        buffer.append("\"  type=\"");
+        buffer.append(str);
+        buffer.append("\"");
+
+        return buffer.toString();
+    }
+}
+
