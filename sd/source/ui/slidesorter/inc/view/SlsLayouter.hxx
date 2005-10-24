df3a977a77e49f66842e39c586b776e77911ab0f
Rüdiger Timm <rt@openoffice.org>
2005-10-24 06:44:01 +0000

    INTEGRATION: CWS impress69 (1.4.96); FILE MERGED 2005/10/14 11:12:33 af 1.4.96.2: RESYNC: (1.4-1.5); FILE MERGED 2005/09/14 11:33:36 af 1.4.96.1: #i54589# Added support for persistent caches.

--- sd/source/ui/slidesorter/inc/view/SlsLayouter.hxx
+++ sd/source/ui/slidesorter/inc/view/SlsLayouter.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsLayouter.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:21:58 $
+ *  last change: $Author: rt $ $Date: 2005-10-24 07:44:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -180,6 +180,8 @@ public:
         output window.
     */
     Fraction GetScaleFactor (void) const;
+
+    Size GetPageObjectSize (void) const;
 
     /** Return the bounding box in model coordinates of the nIndex-th page
         object.
