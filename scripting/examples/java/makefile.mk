9f6c9ae8043fbff66420ea3dba92585f3c1dd167
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-25 10:18:56 +0000

    INTEGRATION: CWS morejava (1.4.14); FILE MERGED 2005/08/29 14:22:23 fridrich_strba 1.4.14.1: Issue number: Submitted by:  fridrich_strba Reviewed by:   fridrich_strba Allow building of OOo out-of-the-box with jdk 1.3.1

--- scripting/examples/java/makefile.mk
+++ scripting/examples/java/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:54:06 $
+#   last change: $Author: hr $ $Date: 2005-10-25 11:18:56 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,5 +40,9 @@ TARGET=javaexamples
 .INCLUDE : ant.mk
 
 .IF "$(SOLAR_JAVA)"!=""
+
+CLASSPATH+=$PATH_SEPERATOR$SOLARVER/$UPD/$INPATH/bin/crimson.jar
+.EXPORT : CLASSPATH
+
 ALLTAR : ANTBUILD
 .ENDIF
