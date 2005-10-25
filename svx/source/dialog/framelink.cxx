3edda6a47cef014aa09c558702341a5ff6f97ec7
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-25 12:15:37 +0000

    INTEGRATION: CWS calc201fix (1.3.88); FILE MERGED 2005/10/19 11:04:17 dr 1.3.88.1: #i56235# wrong Set function used

--- svx/source/dialog/framelink.cxx
+++ svx/source/dialog/framelink.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: framelink.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:07:35 $
+ *  last change: $Author: hr $ $Date: 2005-10-25 13:15:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1298,7 +1298,7 @@ void Style::Set( const SvxBorderLine* pBorder, double fScale, sal_uInt16 nMaxWid
 
 Style& Style::ScaleSelf( double fScale, sal_uInt16 nMaxWidth )
 {
-    Set( SCALEVALUE( mnPrim ), SCALEVALUE( mnDist ), SCALEVALUE( mnSecn ), mbDotted );
+    Set( SCALEVALUE( mnPrim ), SCALEVALUE( mnDist ), SCALEVALUE( mnSecn ) );
     return *this;
 }
 
