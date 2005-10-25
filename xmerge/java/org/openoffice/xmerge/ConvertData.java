bf8b5e18db54bf4003dfb63d322fb9554aab93f1
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-25 10:29:28 +0000

    INTEGRATION: CWS morejava (1.1.14); FILE MERGED 2005/08/26 14:03:42 fridrich_strba 1.1.14.1: Issue number: Submitted by:  kendy Reviewed by:   fridrich_strba Allow building with jdk 1.5.0 ("enum" is a reserved word in java 5)

--- xmerge/java/org/openoffice/xmerge/ConvertData.java
+++ xmerge/java/org/openoffice/xmerge/ConvertData.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ConvertData.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:18:54 $
+ *  last change: $Author: hr $ $Date: 2005-10-25 11:29:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -107,8 +107,8 @@ public class ConvertData {
      *           <code>Vector</code> of <code>Document</code> objects.
      */
     public Enumeration getDocumentEnumeration() {
-        Enumeration enum = v.elements();
-        return (enum);
+        Enumeration enumer = v.elements();
+        return (enumer);
     }
 
 
