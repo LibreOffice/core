01775660206a3330dfadeb94a140c3cb8b13984d
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-27 16:35:38 +0000

    INTEGRATION: CWS skeletonmaker (1.2.4); FILE MERGED 2005/10/14 15:26:42 jsc 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2005/08/11 12:24:30 jsc 1.2.4.1: #i52208# improve error output

--- codemaker/source/codemaker/codemaker.cxx
+++ codemaker/source/codemaker/codemaker.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: codemaker.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-17 13:20:04 $
+ *  last change: $Author: hr $ $Date: 2005-10-27 17:35:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
