960fee18e5fc6b4066632a9d7eca0eb6bccb481c
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-27 16:13:27 +0000

    INTEGRATION: CWS skeletonmaker (1.1.2); FILE ADDED 2005/10/25 08:46:03 jsc 1.1.2.6: #i52169# adapt docu for autodoc 2005/10/21 13:27:45 jsc 1.1.2.5: #i52169# remove needless include 2005/10/21 13:11:29 jsc 1.1.2.4: #i52169# rename member variable to m_aMutex 2005/10/20 08:09:51 jsc 1.1.2.3: #i52169# adapt docu 2005/09/06 16:07:43 jsc 1.1.2.2: #i51720# change licence header 2005/07/19 13:09:34 jsc 1.1.2.1: #i52169# insert new BaseMutex helper for usage with WeakComponentImplHelperXX

---
+++ cppuhelper/inc/cppuhelper/basemutex.hxx
@@ -0,0 +1,59 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: basemutex.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2005-10-27 17:13:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CPPUHELPER_BASEMUTEX_HXX_
+#define _CPPUHELPER_BASEMUTEX_HXX_
+
+#ifndef _OSL_MUTEX_HXX_
+#include <osl/mutex.hxx>
+#endif
+
+namespace cppu
+{
+    /** base class for all classes who want derive from
+     ::cppu::WeakComponentImplHelperXX.
+
+     Implmentation classes have first to derive from BaseMutex and then from
+     ::cppu::WeakComponentImplHelperXX to ensure that the BaseMutex is completely
+     initialized when the mutex is used to intialize the
+     ::cppu::WeakComponentImplHelperXX
+    */
+    class BaseMutex
+    {
+    protected:
+        mutable ::osl::Mutex m_aMutex;
+    };
+}
+#endif // _CPPUHELPER_BASEMUTEX_HXX_
