3e736d9eae67135db2163fea8a752b360440eaf5
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-27 14:51:29 +0000

    INTEGRATION: CWS mib02 (1.15.160); FILE MERGED 2005/09/28 17:59:47 mib 1.15.160.2: RESYNC: (1.15-1.16); FILE MERGED 2005/09/05 12:59:58 mib 1.15.160.1: #i48559#: Order of OutScript() parameters was wrong

--- svtools/source/svhtml/htmlout.cxx
+++ svtools/source/svhtml/htmlout.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlout.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:47:08 $
+ *  last change: $Author: hr $ $Date: 2005-10-27 15:51:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -744,8 +744,9 @@ SvStream& HTMLOutFuncs::Out_ImageMap( SvStream& rStream,
     return rStream;
 }
 
-SvStream& HTMLOutFuncs::OutScript( SvStream& rStrm, const String& rSource,
+SvStream& HTMLOutFuncs::OutScript( SvStream& rStrm,
                                    const String& rBaseURL,
+                                   const String& rSource,
                                    const String& rLanguage,
                                    ScriptType eScriptType,
                                    const String& rSrc,
