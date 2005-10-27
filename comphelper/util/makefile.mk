b87b56ccf800d0bc96bb69aa2a6384ee4deb19c3
Jens-Heiner Rechtien <hr@openoffice.org>
2005-10-27 16:25:58 +0000

    INTEGRATION: CWS skeletonmaker (1.9.42); FILE MERGED 2005/10/14 17:02:18 jsc 1.9.42.2: RESYNC: (1.9-1.10); FILE MERGED 2005/07/20 08:46:50 jsc 1.9.42.1: #i52207# modified for test

--- comphelper/util/makefile.mk
+++ comphelper/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 03:01:35 $
+#   last change: $Author: hr $ $Date: 2005-10-27 17:25:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
