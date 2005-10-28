ee4dec7a59e281f538859cfbca333782e3bdd04b
Armin Weiss <aw@openoffice.org>
2005-10-28 10:52:44 +0000

    #i39532#

---
+++ drawinglayer/util/makefile.mk
@@ -0,0 +1,78 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.1 $
+#
+#   last change: $Author: aw $ $Date: 2005-10-28 11:52:44 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..
+
+PRJNAME=drawinglayer
+TARGET=drawinglayer
+
+# --- Settings ---------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+# --- Allgemein ---------------------------------------------------
+
+LIB1TARGET=$(SLB)$/drawinglayer.lib
+LIB1FILES=\
+    $(SLB)$/primitive.lib
+
+SHL1TARGET= drawinglayer$(UPD)$(DLLPOSTFIX)
+SHL1IMPLIB= idrawinglayer
+
+SHL1STDLIBS=\
+        $(VCLLIB)			\
+        $(BASEGFXLIB)		\
+        $(TOOLSLIB)			\
+        $(SALLIB)			\
+        $(CPPUHELPERLIB)	\
+        $(CPPULIB)
+
+SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+SHL1LIBS=	$(SLB)$/drawinglayer.lib
+
+DEF1NAME	=$(SHL1TARGET)
+DEF1DEPN	=$(MISC)$/$(SHL1TARGET).flt \
+        $(LIB1TARGET)
+
+DEF1DES		=DrawingLayer
+DEFLIB1NAME	=drawinglayer
+
+# --- Targets -----------------------------------------------------------
+
+.INCLUDE :  target.mk
+
+$(MISC)$/$(SHL1TARGET).flt : makefile.mk
+    @+$(TYPE) $(TARGET).flt > $@
+
