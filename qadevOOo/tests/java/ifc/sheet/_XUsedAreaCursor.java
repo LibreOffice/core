575e22b05aa90abf74431fd96f31e45e76b23b32
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:49:09 +0000

    INTEGRATION: CWS qadev24 (1.2.114); FILE MERGED 2005/09/19 14:07:03 cn 1.2.114.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/13 10:25:34 cn 1.2.114.1: #i54539# replace 'tEnv.dispose()' with 'this.disposeEnvironment()'

--- qadevOOo/tests/java/ifc/sheet/_XUsedAreaCursor.java
+++ qadevOOo/tests/java/ifc/sheet/_XUsedAreaCursor.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _XUsedAreaCursor.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:59:41 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:49:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -140,7 +140,7 @@ public class _XUsedAreaCursor extends MultiMethodTest {
     * Forces object environment recreation.
     */
     protected void after() {
-        tEnv.dispose();
+        this.disposeEnvironment();
     }
 } // finished class _XUsedAreaCursor
 
