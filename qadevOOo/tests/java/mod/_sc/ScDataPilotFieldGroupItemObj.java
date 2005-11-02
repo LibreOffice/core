540c81d64a22f38b99a9c7549d940e7987d762d9
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:57:40 +0000

    INTEGRATION: CWS qadev24 (1.2.4); FILE MERGED 2005/09/19 15:26:30 cn 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/13 07:57:44 cn 1.2.4.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScDataPilotFieldGroupItemObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotFieldGroupItemObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScDataPilotFieldGroupItemObj.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:47:02 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:57:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,7 +70,7 @@ import util.SOfficeFactory;
 
 public class ScDataPilotFieldGroupItemObj extends TestCase
 {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
      * A field is filled some values. This integer determines the size of the
