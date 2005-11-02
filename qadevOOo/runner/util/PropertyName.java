6a13284f4bdce19a7ca17be6f7ac2d6083f8bac6
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:44:35 +0000

    INTEGRATION: CWS qadev24 (1.4.2); FILE MERGED 2005/08/30 13:09:33 cn 1.4.2.4: #i53977# the closing time for a self closing of the office get a new parameter 'OfficeCloseTimeOut' which is 5000 ms by default 2005/08/26 09:27:57 cn 1.4.2.3: #i52275# KEEP_DOCUMENT added 2005/08/15 13:18:30 cn 1.4.2.2: #i52234# new parameter NewOfficeInstance 2005/07/20 15:13:49 cn 1.4.2.1: #i52234# new property 'NewOfficeInstance'

--- qadevOOo/runner/util/PropertyName.java
+++ qadevOOo/runner/util/PropertyName.java
@@ -1,3 +1,63 @@
+/*************************************************************************
+ *
+ *  $RCSfile: PropertyName.java,v $
+ *
+ *  $Revision: 1.5 $
+ *
+ *  last change:$Date: 2005-11-02 17:44:35 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
 package util;
 
 public interface PropertyName {
@@ -13,8 +73,12 @@ public interface PropertyName {
     final public static String LOG_WRITER = "LogWriter";
     final public static String TIME_OUT = "TimeOut";
     final public static String THREAD_TIME_OUT = "ThreadTimeOut";
+    final public static String OFFICE_CLOSE_TIME_OUT = "OfficeCloseTimeOut";
     final public static String OPERATING_SYSTEM = "OperatingSystem";
     final public static String AUTO_RESTART = "AutoRestart";
+    final public static String NEW_OFFICE_INSTANCE = "NewOfficeInstance";
+    final public static String KEEP_DOCUMENT = "KeepDocument";
+
 
     final public static String WNTMSCI = "wntmsci";
     final public static String UNXLNGI = "unxlngi";
