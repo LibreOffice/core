e73c742fa5d8b278a04315ae8331a22537a1fe78
Kurt Zenker <kz@openoffice.org>
2005-11-02 12:53:57 +0000

    INTEGRATION: CWS canvas02 (1.12.20); FILE MERGED 2005/10/08 13:02:42 thb 1.12.20.3: RESYNC: (1.12-1.13); FILE MERGED 2005/08/30 22:56:07 thb 1.12.20.2: #i52876# Clarified docs on tools::isRectangle; added isRectangle also for poly-polygon 2005/07/28 10:10:43 thb 1.12.20.1: Join from cws_src680_aw024: #i48939# and new rendering subsystem need AW's clipper changes

--- basegfx/inc/basegfx/polygon/b2dpolypolygontools.hxx
+++ basegfx/inc/basegfx/polygon/b2dpolypolygontools.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dpolypolygontools.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:30:02 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 13:53:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,6 +48,10 @@
 #include <basegfx/polygon/b2dpolygon.hxx>
 #endif
 
+#ifndef _BGFX_POLYGON_B3DPOLYPOLYGON_HXX
+#include <basegfx/polygon/b3dpolypolygon.hxx>
+#endif
+
 #include <vector>
 
 namespace rtl
@@ -70,8 +74,7 @@ namespace basegfx
         // Check and evtl. correct orientations of all contained Polygons so that
         // the orientations of contained polygons will variate to express areas and
         // holes
-//BFS08     void correctOrientations(::basegfx::B2DPolyPolygon& rCandidate);
-        ::basegfx::B2DPolyPolygon correctOrientations(const ::basegfx::B2DPolyPolygon& rCandidate);
+        B2DPolyPolygon correctOrientations(const B2DPolyPolygon& rCandidate);
 
         // Remove all intersections, the self intersections and the in-between
         // polygon intersections. After this operation there are no more intersections
@@ -80,35 +83,33 @@ namespace basegfx
         // computed.
         // bForceOrientation: If true, the orientations of all contained polygons
         // is changed to ORIENTATION_POSITIVE before computing.
-//BFS08     // bInvertRemove: if true, created polygons which are inside others and
-//BFS08     // have the same orientation are removed (cleanup).
-//BFS08     void removeIntersections(::basegfx::B2DPolyPolygon& rCandidate,
-//BFS08         bool bForceOrientation = true, bool bInvertRemove = false);
-//BFS08     void removeIntersections(::basegfx::B2DPolyPolygon& rCandidate, bool bForceOrientation = true);
-//BFS08     void removeIntersections(::basegfx::B2DPolyPolygon& rCandidate);
-        ::basegfx::B2DPolyPolygon removeIntersections(const ::basegfx::B2DPolyPolygon& rCandidate);
-        ::basegfx::B2DPolyPolygon removeAllIntersections(const ::basegfx::B2DPolyPolygon& rCandidate);
-        ::basegfx::B2DPolyPolygon removeNeutralPolygons(const ::basegfx::B2DPolyPolygon& rCandidate, bool bUseOr);
+        B2DPolyPolygon removeIntersections(const B2DPolyPolygon& rCandidate);
+        B2DPolyPolygon removeAllIntersections(const B2DPolyPolygon& rCandidate);
+        B2DPolyPolygon removeNeutralPolygons(const B2DPolyPolygon& rCandidate, bool bUseOr);
 
         // Subdivide all contained curves. Use distanceBound value if given.
-        ::basegfx::B2DPolyPolygon adaptiveSubdivideByDistance(const ::basegfx::B2DPolyPolygon& rCandidate, double fDistanceBound = 0.0);
+        B2DPolyPolygon adaptiveSubdivideByDistance(const B2DPolyPolygon& rCandidate, double fDistanceBound = 0.0);
 
         // Subdivide all contained curves. Use distanceBound value if given. Else, a convenient one
         // is created.
-        ::basegfx::B2DPolyPolygon adaptiveSubdivideByAngle(const ::basegfx::B2DPolyPolygon& rCandidate, double fAngleBound = 0.0);
+        B2DPolyPolygon adaptiveSubdivideByAngle(const B2DPolyPolygon& rCandidate, double fAngleBound = 0.0);
 
         // Subdivide all contained curves. Use nCount divisions if given. Else, a convenient one
         // is created.
-        ::basegfx::B2DPolyPolygon adaptiveSubdivideByCount(const ::basegfx::B2DPolyPolygon& rCandidate, sal_uInt32 nCount = 0L);
+        B2DPolyPolygon adaptiveSubdivideByCount(const B2DPolyPolygon& rCandidate, sal_uInt32 nCount = 0L);
+
+        // isInside test for B2dPoint. On border is not inside as long as not true is given
+        // in bWithBorder flag. It is assumed that the orientations of the given polygon are correct.
+        bool isInside(const B2DPolyPolygon& rCandidate, const B2DPoint& rPoint, bool bWithBorder = false);
 
         // get size of PolyPolygon. Control vectors are included in that ranges.
-        ::basegfx::B2DRange getRange(const ::basegfx::B2DPolyPolygon& rCandidate);
+        B2DRange getRange(const B2DPolyPolygon& rCandidate);
 
         // Apply Line Dashing. This cuts every contained PolyPolygon into line pieces
         // which are inserted as single polygons into the result.
         // If fFullDashDotLen is not given it will be calculated from the given
         // raDashDotArray.
-        ::basegfx::B2DPolyPolygon applyLineDashing(const ::basegfx::B2DPolyPolygon& rCandidate, const ::std::vector<double>& raDashDotArray, double fFullDashDotLen = 0.0);
+        B2DPolyPolygon applyLineDashing(const B2DPolyPolygon& rCandidate, const ::std::vector<double>& raDashDotArray, double fFullDashDotLen = 0.0);
 
         // Merge contained Polygons to longer ones if the end point of one Polygon equals
         // the start point of the next one. Only direct successors are checked.
@@ -116,7 +117,7 @@ namespace basegfx
         // polygon edge after using applyLineDashing. This is necessary if the dashed line
         // parts shall be used for creating thick line geometry to be able to do correct
         // line joints.
-        ::basegfx::B2DPolyPolygon mergeDashedLines(const ::basegfx::B2DPolyPolygon& rCandidate);
+        B2DPolyPolygon mergeDashedLines(const B2DPolyPolygon& rCandidate);
 
         // test if point is inside epsilon-range around the given PolyPolygon. Can be used
         // for HitTesting. The epsilon-range is defined to be the tube around the PolyPolygon
@@ -137,8 +138,48 @@ namespace basegfx
 
             @return true, if the string was successfully parsed
          */
-        bool importFromSvgD( ::basegfx::B2DPolyPolygon& o_rPolyPoly,
+        bool importFromSvgD( B2DPolyPolygon& o_rPolyPoly,
                              const ::rtl::OUString&     rSvgDStatement );
+
+        // create 3d PolyPolygon from given 2d PolyPolygon. The given fZCoordinate is used to expand the
+        // third coordinate.
+        B3DPolyPolygon createB3DPolyPolygonFromB2DPolyPolygon(const B2DPolyPolygon& rCandidate, double fZCoordinate = 0.0);
+
+        // create 2d PolyPolygon from given 3d PolyPolygon. All coordinates are transformed using the given
+        // matrix and the resulting x,y is used to form the new polygon.
+        B2DPolyPolygon createB2DPolyPolygonFromB3DPolyPolygon(const B3DPolyPolygon& rCandidate, const B3DHomMatrix& rMat);
+
+        // for each contained edge in each contained polygon calculate the smallest distance. Return the index to the smallest
+        // edge in rEdgeIndex and the index to the polygon in rPolygonIndex. The relative position on the edge is returned in rCut.
+        // If nothing was found (e.g. empty input plygon), DBL_MAX is returned.
+        double getSmallestDistancePointToPolyPolygon(const B2DPolyPolygon& rCandidate, const B2DPoint& rTestPoint, sal_uInt32& rPolygonIndex, sal_uInt32& rEdgeIndex, double& rCut);
+
+        // distort PolyPolygon. rOriginal describes the original range, where the given points describe the distorted
+        // corresponding points.
+        B2DPolyPolygon distort(const B2DPolyPolygon& rCandidate, const B2DRange& rOriginal, const B2DPoint& rTopLeft, const B2DPoint& rTopRight, const B2DPoint& rBottomLeft, const B2DPoint& rBottomRight);
+
+        // rotate PolyPolygon around given point with given angle.
+        B2DPolyPolygon rotateAroundPoint(const B2DPolyPolygon& rCandidate, const B2DPoint& rCenter, double fAngle);
+
+        // expand all segments (which are not yet) to curve segments. This is done with setting the control
+        // vectors on the 1/3 resp. 2/3 distances on each segment.
+        B2DPolyPolygon expandToCurve(const B2DPolyPolygon& rCandidate);
+
+        // set continuity for the whole curve. If not a curve, nothing will change. Non-curve points are not changed, too.
+        B2DPolyPolygon setContinuity(const B2DPolyPolygon& rCandidate, B2VectorContinuity eContinuity);
+
+        /** Predicate whether a given poly-polygon is a rectangle.
+
+            @param rPoly
+            PolyPolygon to check
+
+            @return true, if the poly-polygon describes a rectangle
+            (contains exactly one polygon, polygon is closed, and the
+            points are either cw or ccw enumerations of a rectangle's
+            vertices). Note that intermediate points and duplicate
+            points are ignored.
+         */
+        bool isRectangle( const B2DPolyPolygon& rPoly );
 
     } // end of namespace tools
 } // end of namespace basegfx
