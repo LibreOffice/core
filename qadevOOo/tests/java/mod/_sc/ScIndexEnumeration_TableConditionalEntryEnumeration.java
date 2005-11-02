0cc9f482d3c781e9bb395cf89a69550fe9ebcded
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:05:30 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:15:48 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:18 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_TableConditionalEntryEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_TableConditionalEntryEnumeration.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_TableConditionalEntryEnumeration.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:57:25 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:05:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 
 public class ScIndexEnumeration_TableConditionalEntryEnumeration extends TestCase {
-    XSpreadsheetDocument xSpreadsheetDoc = null;
+    static XSpreadsheetDocument xSpreadsheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
@@ -134,7 +134,7 @@ public class ScIndexEnumeration_TableConditionalEntryEnumeration extends TestCas
                 UnoRuntime.queryInterface(XPropertySet.class, oSheet);
             CFormat = Props.getPropertyValue("ConditionalFormat");
             if (utils.isVoid(CFormat)) {
-                System.out.println("Property 'ConditionalFormat' is void");
+                log.println("Property 'ConditionalFormat' is void");
             }
         } catch (com.sun.star.lang.WrappedTargetException e){
             e.printStackTrace(log);
