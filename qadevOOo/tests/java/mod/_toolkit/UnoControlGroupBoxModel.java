34f9998b81403b161a9ebd1d2b6019c8f2cbbc3d
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:24:42 +0000

    INTEGRATION: CWS qadev24 (1.4.98); FILE MERGED 2005/09/19 17:22:37 cn 1.4.98.2: RESYNC: (1.4-1.5); FILE MERGED 2005/08/19 07:40:03 cn 1.4.98.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoControlGroupBoxModel.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlGroupBoxModel.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoControlGroupBoxModel.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:44:02 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:24:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ import util.utils;
 
 
 public class UnoControlGroupBoxModel extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     /**
     * Creates StarOffice Writer document.
