d7f77f24c575ed436a2271b3950024b9d31972e1
Kurt Zenker <kz@openoffice.org>
2005-11-02 12:33:29 +0000

    INTEGRATION: CWS canvas02 (1.10.160); FILE MERGED 2005/10/09 07:46:32 thb 1.10.160.2: RESYNC: (1.10-1.11); FILE MERGED 2005/07/28 15:00:30 thb 1.10.160.1: Join from cws_src680_glcanvas: needed the SystemChildWindow stuff also here

--- vcl/unx/inc/salinst.h
+++ vcl/unx/inc/salinst.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salinst.h,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:45:09 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 13:33:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,7 @@ public:
     virtual SalFrame*       CreateFrame( SalFrame* pParent, ULONG nStyle );
     virtual void                DestroyFrame( SalFrame* pFrame );
 
-    virtual SalObject*          CreateObject( SalFrame* pParent );
+    virtual SalObject*          CreateObject( SalFrame* pParent, SystemWindowData* pWindowData );
     virtual void                DestroyObject( SalObject* pObject );
 
     virtual SalVirtualDevice*   CreateVirtualDevice( SalGraphics* pGraphics,
