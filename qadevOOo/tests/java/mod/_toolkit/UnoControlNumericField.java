a92e23dbb687abcd28ce21a94bbadda5a4934ef7
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:25:24 +0000

    INTEGRATION: CWS qadev24 (1.5.72); FILE MERGED 2005/09/19 17:20:01 cn 1.5.72.2: RESYNC: (1.5-1.6); FILE MERGED 2005/08/19 07:40:04 cn 1.5.72.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoControlNumericField.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlNumericField.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoControlNumericField.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:47:39 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:25:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,7 +64,7 @@ import com.sun.star.view.XControlAccess;
 
 
 public class UnoControlNumericField extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     protected void initialize(TestParameters Param, PrintWriter log) {
         SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory) Param.getMSF());
