8ea9d5ea86a6f04e1d9c4283478f425b30d62388
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:06:22 +0000

    INTEGRATION: CWS qadev24 (1.4.98); FILE MERGED 2005/09/19 15:24:57 cn 1.4.98.2: RESYNC: (1.4-1.5); FILE MERGED 2005/09/13 07:58:22 cn 1.4.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScLabelRangesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScLabelRangesObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScLabelRangesObj.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:58:24 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:06:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,7 +70,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.container._XElementAccess
 */
 public class ScLabelRangesObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
