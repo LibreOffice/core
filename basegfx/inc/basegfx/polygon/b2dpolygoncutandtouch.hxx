36eb4a27aa201fae3f9c35350ff268b9eaac2e7e
Kurt Zenker <kz@openoffice.org>
2005-11-02 12:53:25 +0000

    INTEGRATION: CWS canvas02 (1.2.8); FILE MERGED 2005/10/08 13:01:13 thb 1.2.8.2: RESYNC: (1.2-1.3); FILE MERGED 2005/07/28 10:10:42 thb 1.2.8.1: Join from cws_src680_aw024: #i48939# and new rendering subsystem need AW's clipper changes

--- basegfx/inc/basegfx/polygon/b2dpolygoncutandtouch.hxx
+++ basegfx/inc/basegfx/polygon/b2dpolygoncutandtouch.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dpolygoncutandtouch.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:28:02 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 13:53:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,16 @@ namespace basegfx
         // look for polypolygon-intersections and polypolygon-touches (point of poly A on an edge of poly B) in given PolyPolygon and add
         // extra points there. Result will have no touches or intersections between contained polygons on an edge, only on points. For
         // convenience, the correction for self-intersections for each member polygon will be used, too.
-        B2DPolyPolygon addPointsAtCutsAndTouches(const B2DPolyPolygon& rCandidate);
+        // Changed: Self intersections are searched by default, but may be switched off by 2nd parameter.
+        B2DPolyPolygon addPointsAtCutsAndTouches(const B2DPolyPolygon& rCandidate, bool bSelfIntersections = true);
+
+        // look for intersections of rCandidate with all polygons from rMask and add extra points there. Do
+        // not change or add points to rMask.
+        B2DPolygon addPointsAtCutsAndTouches(const B2DPolyPolygon& rMask, const B2DPolygon& rCandidate);
+
+        // look for intersections of rCandidate with all polygons from rMask and add extra points there. Do
+        // not change or add points to rMask.
+        B2DPolyPolygon addPointsAtCutsAndTouches(const B2DPolyPolygon& rMask, const B2DPolyPolygon& rCandidate);
 
     } // end of namespace tools
 } // end of namespace basegfx
