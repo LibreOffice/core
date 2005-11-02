ca4eef47ec2273ccc67ee1794de92d0be5e6c896
Kurt Zenker <kz@openoffice.org>
2005-11-02 11:50:29 +0000

    INTEGRATION: CWS canvas02 (1.1.2); FILE ADDED 2005/10/11 15:41:07 thb 1.1.2.2: #i54170# Corrected license headers 2005/08/19 11:03:09 thb 1.1.2.1: #i53538# Added null canvas. This canvas will do nothing, except log all operations (to stderr, currently, but planned are various targets)

---
+++ canvas/source/null/null_spritehelper.cxx
@@ -0,0 +1,106 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: null_spritehelper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-11-02 12:50:29 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <canvas/debug.hxx>
+#include <canvas/verbosetrace.hxx>
+
+#include <rtl/logfile.hxx>
+#include <rtl/math.hxx>
+
+#include <canvas/canvastools.hxx>
+
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/point/b2dpoint.hxx>
+#include <basegfx/tools/canvastools.hxx>
+#include <basegfx/numeric/ftools.hxx>
+#include <basegfx/polygon/b2dpolypolygontools.hxx>
+#include <basegfx/polygon/b2dpolygontools.hxx>
+#include <basegfx/polygon/b2dpolypolygonrasterconverter.hxx>
+#include <basegfx/polygon/b2dpolygontriangulator.hxx>
+#include <basegfx/polygon/b2dpolygoncutandtouch.hxx>
+
+#include "null_canvascustomsprite.hxx"
+#include "null_spritehelper.hxx"
+
+#include <memory>
+
+
+using namespace ::com::sun::star;
+
+namespace nullcanvas
+{
+    SpriteHelper::SpriteHelper() :
+        mpSpriteCanvas(),
+        mbTextureDirty( true )
+    {
+    }
+
+    void SpriteHelper::init( const geometry::RealSize2D&    rSpriteSize,
+                             const SpriteCanvasRef&         rSpriteCanvas )
+    {
+        ENSURE_AND_THROW( rSpriteCanvas.get(),
+                          "SpriteHelper::init(): Invalid device, sprite canvas or surface" );
+
+        mpSpriteCanvas     = rSpriteCanvas;
+        mbTextureDirty     = true;
+
+        // also init base class
+        CanvasCustomSpriteHelper::init( rSpriteSize,
+                                        rSpriteCanvas.get() );
+    }
+
+    void SpriteHelper::disposing()
+    {
+        mpSpriteCanvas.clear();
+
+        // forward to parent
+        CanvasCustomSpriteHelper::disposing();
+    }
+
+    void SpriteHelper::redraw( bool& io_bSurfaceDirty ) const
+    {
+        // TODO
+    }
+
+    void SpriteHelper::clearSurface()
+    {
+        // TODO
+    }
+
+    ::basegfx::B2DPolyPolygon SpriteHelper::polyPolygonFromXPolyPolygon2D( uno::Reference< rendering::XPolyPolygon2D >& xPoly ) const
+    {
+        return ::canvas::tools::polyPolygonFromXPolyPolygon2D( xPoly );
+    }
+}
