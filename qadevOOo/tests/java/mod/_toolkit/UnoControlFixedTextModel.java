cbab17e7b18f916ee0d31756a38120214970c4c8
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:23:57 +0000

    INTEGRATION: CWS qadev24 (1.4.98); FILE MERGED 2005/09/19 17:18:22 cn 1.4.98.2: RESYNC: (1.4-1.5); FILE MERGED 2005/08/19 07:40:02 cn 1.4.98.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoControlFixedTextModel.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlFixedTextModel.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoControlFixedTextModel.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:43:08 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:23:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ import util.utils;
 
 
 public class UnoControlFixedTextModel extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     /**
     * Creates StarOffice Writer document.
