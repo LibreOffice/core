dde800da57dfbfbc82077b1b31e92c3c1092e0a7
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:53:26 +0000

    INTEGRATION: CWS qadev24 (1.3.6); FILE MERGED 2005/09/19 15:21:21 cn 1.3.6.2: RESYNC: (1.3-1.4); FILE MERGED 2005/09/13 07:57:28 cn 1.3.6.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScAnnotationShapeObj.java
+++ qadevOOo/tests/java/mod/_sc/ScAnnotationShapeObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScAnnotationShapeObj.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:41:09 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:53:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -86,7 +86,7 @@ import util.InstCreator;
 * @see ifc.sheet._XSheetAnnotation
 */
 public class ScAnnotationShapeObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates a spreadsheet document.
@@ -98,7 +98,7 @@ public class ScAnnotationShapeObj extends TestCase {
 
         try {
             log.println("creating a Spreadsheet document");
-            System.out.println("Loading: "+utils.getFullTestURL(
+            log.println("Loading: "+utils.getFullTestURL(
                             "ScAnnotationShapeObj.sxc"));
             xSheetDoc =
                 (XSpreadsheetDocument) UnoRuntime.queryInterface(XSpreadsheetDocument.class,
