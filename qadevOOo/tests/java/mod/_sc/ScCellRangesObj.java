32fbc9e4973e3eb2b4c81b406d292cdf9370e0c3
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:55:46 +0000

    INTEGRATION: CWS qadev24 (1.6.98); FILE MERGED 2005/09/19 15:11:36 cn 1.6.98.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:57:36 cn 1.6.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScCellRangesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScCellRangesObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScCellRangesObj.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:44:40 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:55:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -100,7 +100,7 @@ import ifc.sheet._XCellRangesQuery;
 * @see ifc.container._XNameAccess
 */
 public class ScCellRangesObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
