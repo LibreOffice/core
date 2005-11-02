9934f027bbc0c77ea09db568b1d685ed8a973301
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:03:46 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:17:49 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:12 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_ScenariosEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_ScenariosEnumeration.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_ScenariosEnumeration.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:55:10 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:03:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 
 public class ScIndexEnumeration_ScenariosEnumeration extends TestCase {
-    public XSpreadsheetDocument xSpreadsheetDoc;
+    public static XSpreadsheetDocument xSpreadsheetDoc;
 
     /**
     * Creates Spreadsheet document.
