b74b7c389f512f86053d7d7f8e146a26c412fff2
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:54:11 +0000

    INTEGRATION: CWS qadev24 (1.7.16); FILE MERGED 2005/09/19 15:14:22 cn 1.7.16.2: RESYNC: (1.7-1.8); FILE MERGED 2005/09/13 07:57:32 cn 1.7.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScAreaLinksObj.java
+++ qadevOOo/tests/java/mod/_sc/ScAreaLinksObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScAreaLinksObj.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:42:03 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:54:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,7 @@ import com.sun.star.uno.XInterface;
 * @see com.sun.star.sheet.CellAreaLinks
 */
 public class ScAreaLinksObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
