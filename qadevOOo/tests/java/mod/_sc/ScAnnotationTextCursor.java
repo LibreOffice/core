a0c5f237e9d876dedfeae6eabb97076ea4eace39
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:53:36 +0000

    INTEGRATION: CWS qadev24 (1.4.16); FILE MERGED 2005/09/19 15:20:22 cn 1.4.16.2: RESYNC: (1.4-1.5); FILE MERGED 2005/09/13 07:57:29 cn 1.4.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScAnnotationTextCursor.java
+++ qadevOOo/tests/java/mod/_sc/ScAnnotationTextCursor.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScAnnotationTextCursor.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:41:23 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:53:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,7 +77,7 @@ import com.sun.star.uno.XInterface;
  */
 public class ScAnnotationTextCursor extends TestCase {
 
-        XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     protected void initialize( TestParameters tParam, PrintWriter log ) {
         SOfficeFactory SOF = SOfficeFactory.getFactory(  (XMultiServiceFactory) tParam.getMSF() );
