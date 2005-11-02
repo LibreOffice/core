682c6ad7a17298f57ae148e5d585e2147b7d8553
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:26:19 +0000

    INTEGRATION: CWS qadev24 (1.4.22); FILE MERGED 2005/09/19 17:23:52 cn 1.4.22.2: RESYNC: (1.4-1.5); FILE MERGED 2005/08/19 07:40:06 cn 1.4.22.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoScrollBarControl.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoScrollBarControl.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoScrollBarControl.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:50:04 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:26:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,7 @@ import util.utils;
 
 
 public class UnoScrollBarControl extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     protected void initialize(TestParameters Param, PrintWriter log) {
         SOfficeFactory SOF = SOfficeFactory.getFactory(
