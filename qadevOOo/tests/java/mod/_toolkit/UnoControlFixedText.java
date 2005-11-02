2b80fc52452b61fb3e103a795fc6a961265dafec
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:23:45 +0000

    INTEGRATION: CWS qadev24 (1.4.98); FILE MERGED 2005/09/19 17:21:53 cn 1.4.98.2: RESYNC: (1.4-1.5); FILE MERGED 2005/08/19 07:40:02 cn 1.4.98.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoControlFixedText.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlFixedText.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoControlFixedText.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:42:55 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:23:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,7 +64,7 @@ import util.utils;
 
 
 public class UnoControlFixedText extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     protected void initialize(TestParameters Param, PrintWriter log) {
         SOfficeFactory SOF = SOfficeFactory.getFactory(
