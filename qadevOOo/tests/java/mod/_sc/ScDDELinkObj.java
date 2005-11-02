95bcb9a184467a7a38da2ab846f1f6b88b00076f
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:57:15 +0000

    INTEGRATION: CWS qadev24 (1.6.16); FILE MERGED 2005/09/19 15:13:59 cn 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:57:42 cn 1.6.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScDDELinkObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDDELinkObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScDDELinkObj.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:46:35 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:57:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,8 +77,8 @@ import com.sun.star.uno.XInterface;
 * @see ifc.sheet._XDDELink
 */
 public class ScDDELinkObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
-    XComponent oDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
+    static XComponent oDoc = null;
 
     /**
     * Creates Spreadsheet document.
