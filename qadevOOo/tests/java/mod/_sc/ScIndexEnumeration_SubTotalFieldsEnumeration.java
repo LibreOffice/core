56a834fff83e559406de84a32367708b99eaa1d3
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:04:44 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:13:34 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:16 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SubTotalFieldsEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SubTotalFieldsEnumeration.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_SubTotalFieldsEnumeration.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:56:28 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:04:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 
 public class ScIndexEnumeration_SubTotalFieldsEnumeration extends TestCase {
-    public XSpreadsheetDocument xSpreadsheetDoc;
+    public static XSpreadsheetDocument xSpreadsheetDoc;
 
     /**
     * Creates Spreadsheet document.
