6c808e1182e151150d2dc998b232fa6355c2f7a1
Kurt Zenker <kz@openoffice.org>
2005-11-02 11:56:53 +0000

    INTEGRATION: CWS canvas02 (1.3.22); FILE MERGED 2005/10/08 12:51:35 thb 1.3.22.2: RESYNC: (1.3-1.4); FILE MERGED 2005/09/27 09:22:14 thb 1.3.22.1: #i48939# Removed alpha VDev option from BackBuffer (wasn't used since ages); correctly setting backbuffer size (rounded _up_, like the sprite size itself is calculated, too)

--- canvas/source/vcl/backbuffer.cxx
+++ canvas/source/vcl/backbuffer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: backbuffer.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 23:15:45 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 12:56:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,13 +39,8 @@
 namespace vclcanvas
 {
     BackBuffer::BackBuffer( const OutputDevice& rRefDevice,
-                            bool                bMonochromeBuffer,
-                            bool                bAlphaVDev ) :
-        maVDev( bAlphaVDev ?
-                new VirtualDevice( rRefDevice,
-                                   bMonochromeBuffer,
-                                   bMonochromeBuffer ) :
-                new VirtualDevice( rRefDevice,
+                            bool                bMonochromeBuffer ) :
+        maVDev( new VirtualDevice( rRefDevice,
                                    bMonochromeBuffer ) )
     {
     }
