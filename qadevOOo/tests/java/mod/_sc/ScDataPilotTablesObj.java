a29ac6f978a7ba7f67431e83684ff7d6c56a2809
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:59:14 +0000

    INTEGRATION: CWS qadev24 (1.6.16); FILE MERGED 2005/09/19 15:09:39 cn 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:57:49 cn 1.6.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScDataPilotTablesObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDataPilotTablesObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScDataPilotTablesObj.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:48:58 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:59:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,7 +77,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.container._XElementAccess
 */
 public class ScDataPilotTablesObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
