e834bf9bffc837526a689b97b1eda2e64bcbb1a7
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:25:44 +0000

    INTEGRATION: CWS qadev24 (1.4.98); FILE MERGED 2005/09/19 17:20:12 cn 1.4.98.2: RESYNC: (1.4-1.5); FILE MERGED 2005/08/19 07:40:05 cn 1.4.98.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/UnoControlPatternField.java
+++ qadevOOo/tests/java/mod/_toolkit/UnoControlPatternField.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoControlPatternField.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:48:09 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:25:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@ import util.utils;
 
 
 public class UnoControlPatternField extends TestCase {
-    XTextDocument xTextDoc;
+    private static XTextDocument xTextDoc;
 
     protected void initialize(TestParameters Param, PrintWriter log) {
         SOfficeFactory SOF = SOfficeFactory.getFactory(
