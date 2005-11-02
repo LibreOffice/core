d8b483777f3405b5fbc79ba63d709c458f0c9bf1
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:56:11 +0000

    INTEGRATION: CWS qadev24 (1.6.16); FILE MERGED 2005/09/19 15:06:35 cn 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:57:38 cn 1.6.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScCellTextCursor.java
+++ qadevOOo/tests/java/mod/_sc/ScCellTextCursor.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScCellTextCursor.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:45:09 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:56:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -106,7 +106,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.style._CharacterPropertiesAsian
 */
 public class ScCellTextCursor extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
