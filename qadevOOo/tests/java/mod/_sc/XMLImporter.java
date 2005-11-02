89901b2e01fc6c18d5e3eef6d7ad51adf4f140af
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:12:24 +0000

    INTEGRATION: CWS qadev24 (1.5.16); FILE MERGED 2005/09/19 15:17:59 cn 1.5.16.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:59:05 cn 1.5.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/XMLImporter.java
+++ qadevOOo/tests/java/mod/_sc/XMLImporter.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLImporter.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:06:38 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:12:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -78,7 +78,7 @@ import com.sun.star.uno.XInterface;
  */
 public class XMLImporter extends TestCase {
 
-    XComponent xSheetDoc;
+    static XComponent xSheetDoc;
 
     /**
      * New spreadsheet document created.
