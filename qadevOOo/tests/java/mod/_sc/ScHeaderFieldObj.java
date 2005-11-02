ecc66272c798bf4416dfc54d107162eb203a5883
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:00:53 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:22:16 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:01 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScHeaderFieldObj.java
+++ qadevOOo/tests/java/mod/_sc/ScHeaderFieldObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScHeaderFieldObj.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:51:02 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:00:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -83,7 +83,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.text._TextContent
 */
 public class ScHeaderFieldObj extends TestCase {
-    XSpreadsheetDocument xSpreadsheetDoc;
+    static XSpreadsheetDocument xSpreadsheetDoc;
 
     /**
     * Creates Spreadsheet document.
