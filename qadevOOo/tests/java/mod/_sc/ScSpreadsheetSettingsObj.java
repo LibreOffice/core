224146b9d59280bb1b849981faefdef5a9bc1d43
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:08:20 +0000

    INTEGRATION: CWS qadev24 (1.5.16); FILE MERGED 2005/09/19 15:11:22 cn 1.5.16.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:31 cn 1.5.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScSpreadsheetSettingsObj.java
+++ qadevOOo/tests/java/mod/_sc/ScSpreadsheetSettingsObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScSpreadsheetSettingsObj.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:01:06 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:08:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.beans._XPropertySet
 */
 public class ScSpreadsheetSettingsObj extends TestCase {
-        XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
