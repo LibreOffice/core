82f9edcb6edbecbe908e0f6e9dc0f8c0ca831ab0
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:18:43 +0000

    INTEGRATION: CWS qadev24 (1.11.12); FILE MERGED 2005/09/19 17:27:08 cn 1.11.12.2: RESYNC: (1.11-1.12); FILE MERGED 2005/08/19 07:39:53 cn 1.11.12.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/AccessibleStatusBarItem.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleStatusBarItem.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleStatusBarItem.java,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:25:25 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:18:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -90,8 +90,8 @@ import util.SOfficeFactory;
  * @see ifc.accessibility.XAccessibleAction
  */
 public class AccessibleStatusBarItem extends TestCase {
-    XDesktop the_Desk;
-    XTextDocument xTextDoc;
+    private static XDesktop the_Desk;
+    private static XTextDocument xTextDoc;
 
     /**
      * Creates the Desktop service (<code>com.sun.star.frame.Desktop</code>).
