0d2c84fb810f878d76c80b63f971c8e9f21b0d33
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:17:17 +0000

    INTEGRATION: CWS qadev24 (1.8.12); FILE MERGED 2005/09/19 17:23:31 cn 1.8.12.2: RESYNC: (1.8-1.9); FILE MERGED 2005/08/19 07:39:52 cn 1.8.12.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenuBar.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenuBar.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleMenuBar.java,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:21:27 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:17:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -82,8 +82,8 @@ import util.utils;
  * @see ifc.accessibility._XAccessibleContext
  */
 public class AccessibleMenuBar extends TestCase {
-    XTextDocument xTextDoc = null;
-    XAccessibleAction action = null;
+    private static XTextDocument xTextDoc = null;
+    private static XAccessibleAction action = null;
 
     /**
      * Finds accessible component with role <code>MENUBAR</code>
