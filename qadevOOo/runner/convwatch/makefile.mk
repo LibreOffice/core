633ba86b451a5c378204c6e8ff06e886051b7c48
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:42:54 +0000

    INTEGRATION: CWS qadev24 (1.5.6); FILE MERGED 2005/09/19 22:02:55 cn 1.5.6.2: RESYNC: (1.5-1.6); FILE MERGED 2005/07/18 11:33:27 lla 1.5.6.1: #124348# status changes

--- qadevOOo/runner/convwatch/makefile.mk
+++ qadevOOo/runner/convwatch/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 17:17:06 $
+#   last change: $Author: kz $ $Date: 2005-11-02 17:42:54 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -52,6 +52,7 @@ JAVAFILES = \
  FilenameHelper.java \
  NameHelper.java \
  HTMLOutputter.java \
+ LISTOutputter.java \
  INIOutputter.java \
  PropertyName.java \
  StatusHelper.java \
