063712fe5189ed910202891be38f5adf7fdb276e
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:19:16 +0000

    INTEGRATION: CWS qadev24 (1.10.12); FILE MERGED 2005/09/19 17:24:03 cn 1.10.12.2: RESYNC: (1.10-1.11); FILE MERGED 2005/08/19 07:39:54 cn 1.10.12.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/AccessibleToolBox.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleToolBox.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleToolBox.java,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:27:31 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:19:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,8 +81,8 @@ import util.SOfficeFactory;
  * @see ifc.accessibility.XAccessibleExtendedComponent
  */
 public class AccessibleToolBox extends TestCase {
-    XDesktop the_Desk;
-    XTextDocument xTextDoc;
+    private static XDesktop the_Desk;
+    private static XTextDocument xTextDoc;
 
     /**
      * Creates the Desktop service (<code>com.sun.star.frame.Desktop</code>).
