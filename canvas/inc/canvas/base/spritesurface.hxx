67238dde54d41af43fbad6c3e721249a3ec7e11d
Kurt Zenker <kz@openoffice.org>
2005-11-02 11:44:21 +0000

    INTEGRATION: CWS canvas02 (1.1.2); FILE ADDED 2005/10/11 15:40:39 thb 1.1.2.2: #i54170# Corrected license headers 2005/06/17 23:36:53 thb 1.1.2.1: #i48939# Refactored canvas, these are base classes that encapsulate common functionality (like input checking, locking, and general behaviour common to all canvas implementations)

---
+++ canvas/inc/canvas/base/spritesurface.hxx
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: spritesurface.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-11-02 12:44:21 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_CANVAS_SPRITESURFACE_HXX
+#define INCLUDED_CANVAS_SPRITESURFACE_HXX
+
+#ifndef INCLUDED_CANVAS_SPRITE_HXX
+#include <canvas/base/sprite.hxx>
+#endif
+
+namespace canvas
+{
+    /* Definition of the SpriteSurface interface */
+
+    /** Canvas surface containing sprites
+
+        Every canvas surface that contains sprites must implement this
+        interface, when employing the canvas base framework. The
+        methods provided here are used from the individual sprites to
+        notify the canvas about necessary screen updates.
+     */
+    class SpriteSurface : public virtual ::com::sun::star::uno::XInterface
+    {
+    public:
+        typedef ::rtl::Reference< SpriteSurface > Reference;
+
+        /// Sprites should call this from XSprite::show()
+        virtual void showSprite( const Sprite::Reference& rSprite ) = 0;
+
+        /// Sprites should call this from XSprite::hide()
+        virtual void hideSprite( const Sprite::Reference& rSprite ) = 0;
+
+        /// Sprites should call this from XSprite::move()
+        virtual void moveSprite( const Sprite::Reference&       rSprite,
+                                 const ::basegfx::B2DPoint&     rOldPos,
+                                 const ::basegfx::B2DPoint&     rNewPos,
+                                 const ::basegfx::B2DVector&    rSpriteSize ) = 0;
+
+        /** Sprites should call this when some part of the content has
+            changed.
+
+            That includes show/hide, i.e. for show, both showSprite()
+            and updateSprite() must be called.
+        */
+        virtual void updateSprite( const Sprite::Reference&     rSprite,
+                                   const ::basegfx::B2DPoint&   rPos,
+                                   const ::basegfx::B2DRange&   rUpdateArea ) = 0;
+    };
+}
+
+#endif /* INCLUDED_CANVAS_SPRITESURFACE_HXX */
