e32f69079c988d23c421c8034f316969f48204fb
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:47:46 +0000

    INTEGRATION: CWS qadev24 (1.2.114); FILE MERGED 2005/09/19 12:12:36 cn 1.2.114.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/13 10:25:26 cn 1.2.114.1: #i54539# replace 'tEnv.dispose()' with 'this.disposeEnvironment()'

--- qadevOOo/tests/java/ifc/io/_XObjectInputStream.java
+++ qadevOOo/tests/java/ifc/io/_XObjectInputStream.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _XObjectInputStream.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:12:23 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:47:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -176,7 +176,7 @@ public class _XObjectInputStream extends MultiMethodTest {
     * Forces object environment recreation.
     */
     public void after() {
-        tEnv.dispose() ;
+        this.disposeEnvironment() ;
     }
 }
 
