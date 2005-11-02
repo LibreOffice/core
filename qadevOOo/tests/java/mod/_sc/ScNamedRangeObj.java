18a6a2306fddbdd4d8def9dda614529012151864
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:06:47 +0000

    INTEGRATION: CWS qadev24 (1.6.16); FILE MERGED 2005/09/19 15:14:41 cn 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:58:24 cn 1.6.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScNamedRangeObj.java
+++ qadevOOo/tests/java/mod/_sc/ScNamedRangeObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScNamedRangeObj.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:58:54 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:06:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -73,7 +73,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.sheet._XCellRangeReferrer
 */
 public class ScNamedRangeObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
