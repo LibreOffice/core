f345edf81c4222a05d99b3f465a5655c6f550b68
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:59:55 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:15:29 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:57:53 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScDrawPageObj.java
+++ qadevOOo/tests/java/mod/_sc/ScDrawPageObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScDrawPageObj.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:49:52 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:59:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ import com.sun.star.uno.XInterface;
 
 public class ScDrawPageObj extends TestCase {
 
-    XSpreadsheetDocument xDoc = null;
+    static XSpreadsheetDocument xDoc = null;
 
     /**
      * Creates a new Draw document.
