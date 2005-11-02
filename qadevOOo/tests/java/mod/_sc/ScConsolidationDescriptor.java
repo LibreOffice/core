67ca31c30bfb156e33b55873085ab9dee75a6ab3
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:57:05 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:25:39 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:57:42 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScConsolidationDescriptor.java
+++ qadevOOo/tests/java/mod/_sc/ScConsolidationDescriptor.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScConsolidationDescriptor.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:46:22 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:57:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.sheet._XConsolidationDescriptor
 */
 public class ScConsolidationDescriptor extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
