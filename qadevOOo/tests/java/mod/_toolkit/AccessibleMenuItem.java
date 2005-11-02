3b25c220dcf9c8cfbbae0a0f88da0b470e1c8c32
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:17:30 +0000

    INTEGRATION: CWS qadev24 (1.13.12); FILE MERGED 2005/09/19 17:26:30 cn 1.13.12.2: RESYNC: (1.13-1.14); FILE MERGED 2005/08/19 07:39:52 cn 1.13.12.1: #i52213# declare instance variables as 'privte static'

--- qadevOOo/tests/java/mod/_toolkit/AccessibleMenuItem.java
+++ qadevOOo/tests/java/mod/_toolkit/AccessibleMenuItem.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleMenuItem.java,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:22:04 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:17:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,9 +89,9 @@ import util.utils;
  * @see ifc.accessibility._XAccessibleText
  */
 public class AccessibleMenuItem extends TestCase {
-    XTextDocument xTextDoc = null;
-    XAccessibleAction action = null;
-    XMultiServiceFactory msf = null;
+    private static XTextDocument xTextDoc = null;
+    private static XAccessibleAction action = null;
+    private static XMultiServiceFactory msf = null;
 
     /**
      * Finds first accessible component with role <code>MENUITEM</code>
