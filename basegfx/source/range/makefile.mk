5710be892ee0a9b75e9d7818b1a37fe018c863a6
Kurt Zenker <kz@openoffice.org>
2005-11-02 13:00:33 +0000

    INTEGRATION: CWS canvas02 (1.2.24); FILE MERGED 2005/10/08 13:23:22 thb 1.2.24.2: RESYNC: (1.2-1.3); FILE MERGED 2005/06/17 23:08:14 thb 1.2.24.1: #i48939# Moved set difference calculation for ranges to basegfx (the computeSetDifference() methods); added more basic type conversion to canvastools

--- basegfx/source/range/makefile.mk
+++ basegfx/source/range/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 20:50:50 $
+#   last change: $Author: kz $ $Date: 2005-11-02 14:00:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -50,6 +50,7 @@ TARGET=range
 SLOFILES= \
                 $(SLO)$/b1drange.obj		\
                 $(SLO)$/b2drange.obj		\
+                $(SLO)$/b2xrange.obj		\
                 $(SLO)$/b2dmultirange.obj		\
                 $(SLO)$/b3drange.obj
 
