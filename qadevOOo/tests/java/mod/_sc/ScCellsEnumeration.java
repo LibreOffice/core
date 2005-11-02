e6bb953b773fa0f9450a24543d774689f2373fd9
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:56:21 +0000

    INTEGRATION: CWS qadev24 (1.6.16); FILE MERGED 2005/09/19 15:03:43 cn 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/13 07:57:38 cn 1.6.16.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScCellsEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScCellsEnumeration.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScCellsEnumeration.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:45:26 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:56:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,7 +70,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.container._XEnumeration
 */
 public class ScCellsEnumeration extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
