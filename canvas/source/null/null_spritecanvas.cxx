7e949bf6374ce58f47ee5e0fc9f75f063324e9e1
Kurt Zenker <kz@openoffice.org>
2005-11-02 11:49:45 +0000

    INTEGRATION: CWS canvas02 (1.1.2); FILE ADDED 2005/10/11 15:41:03 thb 1.1.2.3: #i54170# Corrected license headers 2005/08/22 12:08:51 thb 1.1.2.2: #i53538# Corrected service name 2005/08/19 11:03:14 thb 1.1.2.1: #i53538# Added null canvas. This canvas will do nothing, except log all operations (to stderr, currently, but planned are various targets)

---
+++ canvas/source/null/null_spritecanvas.cxx
@@ -0,0 +1,232 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: null_spritecanvas.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-11-02 12:49:45 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <canvas/debug.hxx>
+#include <canvas/verbosetrace.hxx>
+#include <canvas/canvastools.hxx>
+
+#include <osl/mutex.hxx>
+
+#include <com/sun/star/registry/XRegistryKey.hpp>
+#include <com/sun/star/lang/XSingleServiceFactory.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+
+#include <cppuhelper/factory.hxx>
+#include <cppuhelper/implementationentry.hxx>
+
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/point/b2dpoint.hxx>
+#include <basegfx/tools/canvastools.hxx>
+#include <basegfx/numeric/ftools.hxx>
+
+#include "null_spritecanvas.hxx"
+
+
+using namespace ::com::sun::star;
+
+#define IMPLEMENTATION_NAME "NullCanvas::SpriteCanvas"
+#define SERVICE_NAME "com.sun.star.rendering.NullCanvas"
+
+namespace
+{
+    static ::rtl::OUString SAL_CALL getImplementationName_SpriteCanvas()
+    {
+        return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( IMPLEMENTATION_NAME ) );
+    }
+
+    static uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames_SpriteCanvas()
+    {
+        uno::Sequence< ::rtl::OUString > aRet(1);
+        aRet[0] = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( SERVICE_NAME ) );
+
+        return aRet;
+    }
+
+}
+
+namespace nullcanvas
+{
+    SpriteCanvas::SpriteCanvas( const uno::Reference< uno::XComponentContext >& rxContext ) :
+        mxComponentContext( rxContext )
+    {
+    }
+
+    void SAL_CALL SpriteCanvas::disposing()
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        mxComponentContext.clear();
+
+        // forward to parent
+        SpriteCanvasBaseT::disposing();
+    }
+
+    ::sal_Bool SAL_CALL SpriteCanvas::showBuffer( ::sal_Bool bUpdateAll ) throw (uno::RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        // avoid repaints on hidden window (hidden: not mapped to
+        // screen). Return failure, since the screen really has _not_
+        // been updated (caller should try again later)
+        return !mbIsVisible ? false : SpriteCanvasBaseT::showBuffer( bUpdateAll );
+    }
+
+    ::sal_Bool SAL_CALL SpriteCanvas::switchBuffer( ::sal_Bool bUpdateAll ) throw (uno::RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        // avoid repaints on hidden window (hidden: not mapped to
+        // screen). Return failure, since the screen really has _not_
+        // been updated (caller should try again later)
+        return !mbIsVisible ? false : SpriteCanvasBaseT::switchBuffer( bUpdateAll );
+    }
+
+    sal_Bool SAL_CALL SpriteCanvas::updateScreen( sal_Bool bUpdateAll ) throw (uno::RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        // avoid repaints on hidden window (hidden: not mapped to
+        // screen). Return failure, since the screen really has _not_
+        // been updated (caller should try again later)
+        return !mbIsVisible ? false : maCanvasHelper.updateScreen(
+            ::basegfx::unotools::b2IRectangleFromAwtRectangle(maBounds),
+            bUpdateAll,
+            mbSurfaceDirty );
+
+        // avoid repaints on hidden window (hidden: not mapped to
+        // screen). Return failure, since the screen really has _not_
+        // been updated (caller should try again later)
+        return !mbIsVisible ? false : SpriteCanvasBaseT::updateScreen( bUpdateAll );
+    }
+
+    void SAL_CALL SpriteCanvas::initialize( const uno::Sequence< uno::Any >& aArguments ) throw( uno::Exception,
+                                                                                                 uno::RuntimeException)
+    {
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        VERBOSE_TRACE( "SpriteCanvas::initialize called" );
+
+        // At index 1, we expect a system window handle here,
+        // containing a pointer to a valid window, on which to output
+        // At index 2, we expect the current window bound rect
+        CHECK_AND_THROW( aArguments.getLength() >= 4 &&
+                         aArguments[1].getValueTypeClass() == uno::TypeClass_LONG,
+                         "SpriteCanvas::initialize: wrong number of arguments, or wrong types" );
+
+        awt::Rectangle aRect;
+        aArguments[2] >>= aRect;
+        const ::basegfx::B2ISize aSize(aRect.Width,
+                                       aRect.Height);
+
+        sal_Bool bIsFullscreen( sal_False );
+        aArguments[3] >>= bIsFullscreen;
+
+        // setup helper
+        maDeviceHelper.init( *this,
+                             aSize,
+                             bIsFullscreen );
+        maCanvasHelper.init( maRedrawManager,
+                             *this,
+                             aSize,
+                             false );
+    }
+
+    ::rtl::OUString SAL_CALL SpriteCanvas::getImplementationName() throw( uno::RuntimeException )
+    {
+        return getImplementationName_SpriteCanvas();
+    }
+
+    sal_Bool SAL_CALL SpriteCanvas::supportsService( const ::rtl::OUString& ServiceName ) throw( uno::RuntimeException )
+    {
+        return ServiceName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM ( SERVICE_NAME ) );
+    }
+
+    uno::Sequence< ::rtl::OUString > SAL_CALL SpriteCanvas::getSupportedServiceNames()  throw( uno::RuntimeException )
+    {
+        return getSupportedServiceNames_SpriteCanvas();
+    }
+
+    ::rtl::OUString SAL_CALL SpriteCanvas::getServiceName(  ) throw (uno::RuntimeException)
+    {
+        return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( SERVICE_NAME ) );
+    }
+
+    uno::Reference< uno::XInterface > SAL_CALL SpriteCanvas::createInstance( const uno::Reference< uno::XComponentContext >& xContext ) throw ( uno::Exception )
+    {
+        return uno::Reference< uno::XInterface >( static_cast<cppu::OWeakObject*>( new SpriteCanvas( xContext ) ) );
+    }
+}
+
+namespace
+{
+    /* shared lib exports implemented with helpers */
+    static struct ::cppu::ImplementationEntry s_component_entries [] =
+    {
+        {
+            nullcanvas::SpriteCanvas::createInstance, getImplementationName_SpriteCanvas,
+            getSupportedServiceNames_SpriteCanvas, ::cppu::createSingleComponentFactory,
+            0, 0
+        },
+        { 0, 0, 0, 0, 0, 0 }
+    };
+}
+
+
+/* Exported UNO methods for registration and object creation.
+   ==========================================================
+ */
+extern "C"
+{
+    void SAL_CALL component_getImplementationEnvironment( const sal_Char**  ppEnvTypeName,
+                                                          uno_Environment** ppEnv )
+    {
+        *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
+    }
+
+    sal_Bool SAL_CALL component_writeInfo( lang::XMultiServiceFactory*  xMgr,
+                                           registry::XRegistryKey*      xRegistry )
+    {
+        return ::cppu::component_writeInfoHelper(
+            xMgr, xRegistry, s_component_entries );
+    }
+
+    void * SAL_CALL component_getFactory( sal_Char const*               implName,
+                                          lang::XMultiServiceFactory*   xMgr,
+                                          registry::XRegistryKey*       xRegistry )
+    {
+        return ::cppu::component_getFactoryHelper(
+            implName, xMgr, xRegistry, s_component_entries );
+    }
+}
