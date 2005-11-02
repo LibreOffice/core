3c5ab50d64a673fc5b85cba83a7bf3909398d708
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:09:39 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:06:58 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:37 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScTableColumnObj.java
+++ qadevOOo/tests/java/mod/_sc/ScTableColumnObj.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScTableColumnObj.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:02:43 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,7 +77,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.beans._XPropertySet
 */
 public class ScTableColumnObj extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
