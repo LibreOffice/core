5ac1fc81ee896d0c7aea7c9d4812eafe58cde9cb
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:49:31 +0000

    INTEGRATION: CWS qadev24 (1.3.114); FILE MERGED 2005/09/19 12:14:48 cn 1.3.114.2: RESYNC: (1.3-1.4); FILE MERGED 2005/09/13 10:25:35 cn 1.3.114.1: #i54539# replace 'tEnv.dispose()' with 'this.disposeEnvironment()'

--- qadevOOo/tests/java/ifc/ui/dialogs/_XFilePickerNotifier.java
+++ qadevOOo/tests/java/ifc/ui/dialogs/_XFilePickerNotifier.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _XFilePickerNotifier.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:37:47 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:49:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,7 +79,7 @@ public class _XFilePickerNotifier extends MultiMethodTest {
             log.println("Cancelling Dialog");
             canc.cancel();
         } else {
-            tEnv.dispose();
+            this.disposeEnvironment();
         }
     }
 
