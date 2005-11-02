46abae76305979c9250dea0c8e328520cb0c01bd
Kurt Zenker <kz@openoffice.org>
2005-11-02 16:46:45 +0000

    INTEGRATION: CWS qadev24 (1.2.114); FILE MERGED 2005/09/19 12:11:08 cn 1.2.114.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/13 10:25:25 cn 1.2.114.1: #i54539# replace 'tEnv.dispose()' with 'this.disposeEnvironment()'

--- qadevOOo/tests/java/ifc/io/_XActiveDataSource.java
+++ qadevOOo/tests/java/ifc/io/_XActiveDataSource.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _XActiveDataSource.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:10:49 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 17:46:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -106,7 +106,7 @@ public class _XActiveDataSource extends MultiMethodTest {
     * Forces object environment recreation.
     */
     public void after() {
-        tEnv.dispose() ;
+        this.disposeEnvironment() ;
     }
 }
 
