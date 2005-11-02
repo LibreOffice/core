1a3afaeb947f0a4cc88a4d46391cdd4ce27925f4
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:01:30 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:14:52 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:02 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScHeaderFooterTextCursor.java
+++ qadevOOo/tests/java/mod/_sc/ScHeaderFooterTextCursor.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScHeaderFooterTextCursor.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:51:50 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:01:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -108,7 +108,7 @@ import com.sun.star.uno.XInterface;
 * @see ifc.style._CharacterPropertiesAsian
 */
 public class ScHeaderFooterTextCursor extends TestCase {
-    XSpreadsheetDocument xSpreadsheetDoc;
+    static XSpreadsheetDocument xSpreadsheetDoc;
 
     /**
     * Creates Spreadsheet document.
