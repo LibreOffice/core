486ec0e783c127829313c3d37c8f2977e5ff526b
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:08:09 +0000

    INTEGRATION: CWS qadev24 (1.3.98); FILE MERGED 2005/09/19 15:24:46 cn 1.3.98.2: RESYNC: (1.3-1.4); FILE MERGED 2005/09/13 07:58:30 cn 1.3.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScSortDescriptorBase.java
+++ qadevOOo/tests/java/mod/_sc/ScSortDescriptorBase.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScSortDescriptorBase.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:00:36 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:08:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ import com.sun.star.util.XSortable;
  */
 public class ScSortDescriptorBase extends TestCase {
 
-    XSpreadsheetDocument xSpreadsheetDoc;
+    static XSpreadsheetDocument xSpreadsheetDoc;
 
     /**
      * in general this method creates a testdocument
