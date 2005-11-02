13afa6953e1b5ce915de2759ad51190f34be4df2
Kurt Zenker <kz@openoffice.org>
2005-11-02 17:03:58 +0000

    INTEGRATION: CWS qadev24 (1.5.98); FILE MERGED 2005/09/19 15:19:34 cn 1.5.98.2: RESYNC: (1.5-1.6); FILE MERGED 2005/09/13 07:58:13 cn 1.5.98.1: #i54533# define class variables for documents as 'static'

--- qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SheetCellRangesEnumeration.java
+++ qadevOOo/tests/java/mod/_sc/ScIndexEnumeration_SheetCellRangesEnumeration.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScIndexEnumeration_SheetCellRangesEnumeration.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:55:26 $
+ *  last change: $Author: kz $ $Date: 2005-11-02 18:03:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,7 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XInterface;
 
 public class ScIndexEnumeration_SheetCellRangesEnumeration extends TestCase {
-    XSpreadsheetDocument xSheetDoc = null;
+    static XSpreadsheetDocument xSheetDoc = null;
 
     /**
     * Creates Spreadsheet document.
