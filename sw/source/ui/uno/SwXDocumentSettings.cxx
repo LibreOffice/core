b9673bbcffd736406b3a41ec9fd222d3b3d9545e
Rüdiger Timm <rt@openoffice.org>
2005-11-08 16:33:09 +0000

    INTEGRATION: CWS swnumtree (1.42.40); FILE MERGED 2005/10/26 11:53:56 od 1.42.40.3: RESYNC: (1.43-1.45); FILE MERGED 2005/09/14 01:56:05 hbrinkm 1.42.40.2: RESYNC: (1.42-1.43); FILE MERGED 2005/08/04 09:42:10 hbrinkm 1.42.40.1: #121445# OutlineLevelYieldsNumbering

--- sw/source/ui/uno/SwXDocumentSettings.cxx
+++ sw/source/ui/uno/SwXDocumentSettings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXDocumentSettings.cxx,v $
  *
- *  $Revision: 1.45 $
+ *  $Revision: 1.46 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-05 13:24:39 $
+ *  last change: $Author: rt $ $Date: 2005-11-08 17:33:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -150,6 +150,7 @@ enum SwDocumentSettingsPropertyHandles
     HANDLE_IS_ADD_FLY_OFFSET,
     HANDLE_IS_ADD_EXTERNAL_LEADING,
     HANDLE_OLD_NUMBERING, // #111955#
+    HANDLE_OUTLINELEVEL_YIELDS_NUMBERING,
     /* Stampit It disable the print cancel button of the shown progress dialog. */
     HANDLE_ALLOW_PRINTJOB_CANCEL,
     // DVO, OD 12.01.2004 #i11859#
@@ -203,6 +204,7 @@ MasterPropertySetInfo * lcl_createSettingsInfo()
         { RTL_CONSTASCII_STRINGPARAM("AddFrameOffsets"),            HANDLE_IS_ADD_FLY_OFFSET,               CPPUTYPE_BOOLEAN,           0,   0},
         { RTL_CONSTASCII_STRINGPARAM("AddExternalLeading"),         HANDLE_IS_ADD_EXTERNAL_LEADING,         CPPUTYPE_BOOLEAN,           0,   0},
         { RTL_CONSTASCII_STRINGPARAM("UseOldNumbering"),            HANDLE_OLD_NUMBERING,                   CPPUTYPE_BOOLEAN,           0,   0}, // #111955#
+        { RTL_CONSTASCII_STRINGPARAM("OutlineLevelYieldsNumbering"), HANDLE_OUTLINELEVEL_YIELDS_NUMBERING, CPPUTYPE_BOOLEAN,           0,   0},
         /* Stampit It disable the print cancel button of the shown progress dialog. */
         { RTL_CONSTASCII_STRINGPARAM("AllowPrintJobCancel"),        HANDLE_ALLOW_PRINTJOB_CANCEL,           CPPUTYPE_BOOLEAN,           0,   0},
         // DVO, OD 12.01.2004 #i11859#
@@ -576,6 +578,11 @@ void SwXDocumentSettings::_setSingleValue( const comphelper::PropertyInfo & rInf
             sal_Bool bTmp = *(sal_Bool*)rValue.getValue();
             mpDoc->SetOldNumbering(bTmp);
         }
+        case HANDLE_OUTLINELEVEL_YIELDS_NUMBERING:
+        {
+            sal_Bool bTmp = *(sal_Bool*)rValue.getValue();
+            mpDoc->SetOutlineYieldsOutlineRule(bTmp);
+        }
         break;
         case HANDLE_ALLOW_PRINTJOB_CANCEL:
         {
@@ -848,6 +855,12 @@ void SwXDocumentSettings::_getSingleValue( const comphelper::PropertyInfo & rInf
         case HANDLE_OLD_NUMBERING: // #111955#
         {
             sal_Bool bTmp = mpDoc->IsOldNumbering();
+            rValue.setValue( &bTmp, ::getBooleanCppuType() );
+        }
+        break;
+        case HANDLE_OUTLINELEVEL_YIELDS_NUMBERING: // #111955#
+        {
+            sal_Bool bTmp = mpDoc->IsOutlineLevelYieldsOutlineRule();
             rValue.setValue( &bTmp, ::getBooleanCppuType() );
         }
         break;
