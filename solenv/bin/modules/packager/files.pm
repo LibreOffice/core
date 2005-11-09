2342432b5ca09627b66549626f80b49d9e3872d7
Rüdiger Timm <rt@openoffice.org>
2005-11-09 08:12:02 +0000

    INTEGRATION: CWS sdkinstaller (1.6.48); FILE MERGED 2005/11/03 14:26:59 is 1.6.48.1: #124595# solving merge conflict

--- solenv/bin/modules/packager/files.pm
+++ solenv/bin/modules/packager/files.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: files.pm,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-14 08:03:11 $
+#   last change: $Author: rt $ $Date: 2005-11-09 09:12:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
