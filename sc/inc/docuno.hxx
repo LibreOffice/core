58d0b91f25ec7886fe4d8a565904c645638f7b6d
Rüdiger Timm <rt@openoffice.org>
2005-11-10 15:36:27 +0000

    INTEGRATION: CWS impress76 (1.21.58); FILE MERGED 2005/11/08 09:22:45 cl 1.21.58.1: #i56896# tunnel BuildId from meta.xml to ole objects

--- sc/inc/docuno.hxx
+++ sc/inc/docuno.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docuno.hxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:33:48 $
+ *  last change: $Author: rt $ $Date: 2005-11-10 16:36:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -172,6 +172,8 @@ private:
 
     BOOL                    FillRenderMarkData( const com::sun::star::uno::Any& aSelection,
                                                 ScMarkData& rMark, ScPrintSelectionStatus& rStatus ) const;
+
+    rtl::OUString           maBuildId;
 
 public:
                             ScModelObj(ScDocShell* pDocSh);
