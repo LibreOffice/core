b0728d9fb4c3386b724bde4fd670711efc6b0fde
Rüdiger Timm <rt@openoffice.org>
2005-11-11 08:10:07 +0000

    INTEGRATION: CWS xmlsec13 (1.3.14); FILE MERGED 2005/10/31 20:06:42 jl 1.3.14.2: RESYNC: (1.3-1.4); FILE MERGED 2005/10/24 15:59:36 jl 1.3.14.1: #54495# new DocumentSignatureInformation.idl

--- offapi/com/sun/star/security/makefile.mk
+++ offapi/com/sun/star/security/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 04:13:04 $
+#   last change: $Author: rt $ $Date: 2005-11-11 09:10:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ IDLFILES=\
     CertificateException.idl            \
     CryptographyException.idl           \
     DocumentDigitalSignatures.idl       \
-    DocumentSignaturesInformation.idl   \
+    DocumentSignatureInformation.idl   \
     EncryptionException.idl             \
     KeyException.idl                    \
     NoPasswordException.idl             \
