faba71b5d92c0c97e75fa114e878d5aad28400f8
Kurt Zenker <kz@openoffice.org>
2005-11-11 13:13:04 +0000

    INTEGRATION: CWS c05v001 (1.1.2); FILE ADDED 2005/09/29 09:24:24 as 1.1.2.1: #125916# new helper to forward URL's to the underlying OS

---
+++ framework/inc/dispatch/systemexec.hxx
@@ -0,0 +1,226 @@
+/*************************************************************************
+ *
+ *  $RCSfile: systemexec.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2005-11-11 14:13:04 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_DISPATCH_SYSTEMEXEC_HXX_
+#define __FRAMEWORK_DISPATCH_SYSTEMEXEC_HXX_
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_MACROS_GENERIC_HXX_
+#include <macros/generic.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XINTERFACE_HXX_
+#include <macros/xinterface.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XTYPEPROVIDER_HXX_
+#include <macros/xtypeprovider.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XSERVICEINFO_HXX_
+#include <macros/xserviceinfo.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_DEBUG_HXX_
+#include <macros/debug.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#include <threadhelp/threadhelpbase.hxx>
+#endif
+
+#ifndef __FRAMEWORK_GENERAL_H_
+#include <general.h>
+#endif
+
+#ifndef __FRAMEWORK_STDTYPES_H_
+#include <stdtypes.h>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XNOTIFYINGDISPATCH_HPP_
+#include <com/sun/star/frame/XNotifyingDispatch.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCH_HPP_
+#include <com/sun/star/frame/XDispatch.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDER_HPP_
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UTIL_URL_HPP_
+#include <com/sun/star/util/URL.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XSTATUSLISTENER_HPP_
+#include <com/sun/star/frame/XStatusListener.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  namespace
+//_________________________________________________________________________________________________________________
+
+namespace framework{
+
+//_________________________________________________________________________________________________________________
+//  exported const
+//_________________________________________________________________________________________________________________
+
+//_________________________________________________________________________________________________________________
+//  exported definitions
+//_________________________________________________________________________________________________________________
+
+/**
+    @short          protocol handler for "systemexec:*" URLs
+    @descr          It's a special dispatch/provider object which is registered for such URL pattern and will
+                    be automaticly used by the framework dispatch mechanism if such URL occured.
+                    It forwards all URL's to the underlying operating system.
+                    So it would e.g. be possible to open HTML files outside the office within a web browser.
+
+    @base           ThreadHelpBase
+                        exports a lock member to guarantee right initialize value of it
+    @base           OWeakObject
+                        provides XWeak and ref count mechanism
+
+    @devstatus      ready to use
+*/
+class SystemExec : // interfaces
+                       public  css::lang::XTypeProvider      ,
+                       public  css::lang::XServiceInfo       ,
+                       public  css::frame::XDispatchProvider ,
+                       public  css::frame::XNotifyingDispatch, // => XDispatch
+                       // baseclasses
+                       // Order is neccessary for right initialization!
+                       private ThreadHelpBase                ,
+                       public  cppu::OWeakObject
+{
+    /* member */
+    private:
+
+        /// reference to global uno service manager which had created us
+        css::uno::Reference< css::lang::XMultiServiceFactory > m_xFactory;
+
+    /* interface */
+    public:
+
+        // ctor/dtor
+                 SystemExec( const css::uno::Reference< css::lang::XMultiServiceFactory >& xFactory );
+        virtual ~SystemExec(                                                                        );
+
+        // XInterface, XTypeProvider, XServiceInfo
+        DECLARE_XINTERFACE
+        DECLARE_XTYPEPROVIDER
+        DECLARE_XSERVICEINFO
+
+        // XDispatchProvider
+        virtual css::uno::Reference< css::frame::XDispatch > SAL_CALL                       queryDispatch  ( const css::util::URL&                                       aURL        ,
+                                                                                                             const ::rtl::OUString&                                      sTarget     ,
+                                                                                                                   sal_Int32                                             nFlags      ) throw( css::uno::RuntimeException );
+        virtual css::uno::Sequence< css::uno::Reference< css::frame::XDispatch > > SAL_CALL queryDispatches( const css::uno::Sequence< css::frame::DispatchDescriptor >& lDescriptor ) throw( css::uno::RuntimeException );
+
+        // XNotifyingDispatch
+        virtual void SAL_CALL dispatchWithNotification( const css::util::URL&                                             aURL      ,
+                                                        const css::uno::Sequence< css::beans::PropertyValue >&            lArguments,
+                                                        const css::uno::Reference< css::frame::XDispatchResultListener >& xListener ) throw( css::uno::RuntimeException );
+
+        // XDispatch
+        virtual void SAL_CALL dispatch            ( const css::util::URL&                                     aURL       ,
+                                                    const css::uno::Sequence< css::beans::PropertyValue >&    lArguments ) throw( css::uno::RuntimeException );
+        virtual void SAL_CALL addStatusListener   ( const css::uno::Reference< css::frame::XStatusListener >& xListener  ,
+                                                    const css::util::URL&                                     aURL       ) throw( css::uno::RuntimeException );
+        virtual void SAL_CALL removeStatusListener( const css::uno::Reference< css::frame::XStatusListener >& xListener  ,
+                                                    const css::util::URL&                                     aURL       ) throw( css::uno::RuntimeException );
+
+    /* internal */
+    private:
+
+        void impl_notifyResultListener(const css::uno::Reference< css::frame::XDispatchResultListener >& xListener,
+                                       const sal_Int16                                                   nState   );
+
+};      //  class SystemExec
+
+}       //  namespace framework
+
+#endif  //  #ifndef __FRAMEWORK_DISPATCH_SYSTEMEXEC_HXX_
