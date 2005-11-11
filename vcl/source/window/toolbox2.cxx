343e117739bec1f331a16b353709828d9f71c27b
Kurt Zenker <kz@openoffice.org>
2005-11-11 16:20:53 +0000

    INTEGRATION: CWS c05v001 (1.37.232); FILE MERGED 2005/08/15 14:06:08 ssa 1.37.232.1: #124966# support for synchronous update for toolboxes that will open in popup mode

--- vcl/source/window/toolbox2.cxx
+++ vcl/source/window/toolbox2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toolbox2.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-11 11:55:25 $
+ *  last change: $Author: kz $ $Date: 2005-11-11 17:20:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -105,6 +105,7 @@ ImplToolBoxPrivateData::ImplToolBoxPrivateData() : m_pLayoutData( NULL )
     mbKeyInputDisabled = FALSE;
     mbMenubuttonSelected = FALSE;
     mbPageScroll = FALSE;
+    mbWillUsePopupMode = FALSE;
 }
 
 ImplToolBoxPrivateData::~ImplToolBoxPrivateData()
@@ -2354,3 +2355,15 @@ BOOL ToolBox::AlwaysLocked()
 
     return nAlwaysLocked == 1 ? TRUE : FALSE;
 }
+
+BOOL ToolBox::WillUsePopupMode() const
+{
+    return mpData->mbWillUsePopupMode;
+}
+
+void ToolBox::WillUsePopupMode( BOOL b )
+{
+    mpData->mbWillUsePopupMode = b;
+}
+
+// -----------------------------------------------------------------------
