765975e1702b77cf0e646dc07e46cdccaee9720f
Rüdiger Timm <rt@openoffice.org>
2005-11-11 11:03:20 +0000

    INTEGRATION: CWS c06 (1.4.496); FILE MERGED 2005/09/15 06:37:40 as 1.4.496.1: #i53536#, #i53630# limitate max count of open documents; mark windows as 'document windows' so vcl can handle it different

--- framework/inc/dispatch/interaction.hxx
+++ framework/inc/dispatch/interaction.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interaction.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:12:50 $
+ *  last change: $Author: rt $ $Date: 2005-11-11 12:03:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -369,6 +369,6 @@ class InteractionRequest : public ::cppu::WeakImplHelper1< ::com::sun::star::tas
 
 };  // class RequestFilterSelect
 
-#endif  // #define __FRAMEWORK_DISPATCH_INTERACTION_HXX_
-
 }       //  namespace framework
+
+#endif  // #define __FRAMEWORK_DISPATCH_INTERACTION_HXX_
