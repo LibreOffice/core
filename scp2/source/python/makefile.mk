5cb812eafb7a55d9af69f8e5a92b65f3756b1232
Rüdiger Timm <rt@openoffice.org>
2005-11-11 09:40:21 +0000

    INTEGRATION: CWS oooemailmerge (1.9.320); FILE MERGED 2005/09/09 13:33:57 cmc 1.9.320.1: #i51638# add python emailmerge backend

--- scp2/source/python/makefile.mk
+++ scp2/source/python/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-11-11 09:22:42 $
+#   last change: $Author: rt $ $Date: 2005-11-11 10:40:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ SCP_PRODUCT_TYPE=osl
 
 PARFILES=\
         module_python.par              \
+    module_python_mailmerge.par    \
         profileitem_python.par         \
         file_python.par                \
         shortcut_python.par
