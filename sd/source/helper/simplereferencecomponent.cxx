a11abd7f8e9a600cac9575b32a1acd3452b85fe0
Rüdiger Timm <rt@openoffice.org>
2005-12-14 15:52:50 +0000

    INTEGRATION: CWS impressfunctions (1.1.2); FILE ADDED 2005/10/28 11:38:22 cl 1.1.2.2: #125341# fixed unix build problems 2005/10/28 10:53:22 cl 1.1.2.1: #125341# reworked FuPoor classes to use refcounting

---
+++ sd/source/helper/simplereferencecomponent.cxx
@@ -0,0 +1,136 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: simplereferencecomponent.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2005-12-14 16:52:50 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SALHELPER_SIMPLEREFERENCECOMPONENT_HXX_
+#include "helper/simplereferencecomponent.hxx"
+#endif
+
+#include "com/sun/star/uno/RuntimeException.hpp"
+
+#ifndef _OSL_DIAGNOSE_H_
+#include "osl/diagnose.h"
+#endif
+
+#ifndef INCLUDED_NEW
+#include <new>
+#define INCLUDED_NEW
+#endif
+
+using com::sun::star::uno::RuntimeException;
+using sd::SimpleReferenceComponent;
+
+SimpleReferenceComponent::SimpleReferenceComponent()
+: m_nCount(0)
+, mbDisposed(false)
+{
+}
+
+SimpleReferenceComponent::~SimpleReferenceComponent()
+{
+    OSL_ASSERT(m_nCount == 0);
+    OSL_ASSERT(mbDisposed);
+}
+
+void SimpleReferenceComponent::acquire()
+{
+    osl_incrementInterlockedCount(&m_nCount);
+}
+
+void SimpleReferenceComponent::release()
+{
+    if((1 == m_nCount) && !mbDisposed)
+    {
+        try
+        {
+            Dispose();
+        }
+        catch (RuntimeException & exc) // don't break throw ()
+        {
+#if OSL_DEBUG_LEVEL > 0
+            rtl::OString msg( rtl::OUStringToOString( exc.Message, RTL_TEXTENCODING_ASCII_US ) );
+            OSL_ENSURE( 0, msg.getStr() );
+#endif
+        }
+    }
+
+    if(osl_decrementInterlockedCount(&m_nCount) == 0) delete this;
+}
+
+void SimpleReferenceComponent::Dispose()
+{
+    if( !mbDisposed )
+    {
+        mbDisposed = true;
+        disposing();
+    }
+}
+
+void SimpleReferenceComponent::disposing()
+{
+}
+
+void * SimpleReferenceComponent::operator new(std::size_t nSize)
+    SAL_THROW((std::bad_alloc))
+{
+    return ::operator new(nSize);
+}
+
+void * SimpleReferenceComponent::operator new(std::size_t nSize,
+                                           std::nothrow_t const & rNothrow)
+    SAL_THROW(())
+{
+#if defined WNT
+    return ::operator new(nSize);
+        // WNT lacks a global nothrow operator new...
+#else // WNT
+    return ::operator new(nSize, rNothrow);
+#endif // WNT
+}
+
+void SimpleReferenceComponent::operator delete(void * pPtr) SAL_THROW(())
+{
+    ::operator delete(pPtr);
+}
+
+void SimpleReferenceComponent::operator delete(void * pPtr,
+                                            std::nothrow_t const & rNothrow)
+    SAL_THROW(())
+{
+#if defined WNT || (defined IRIX && !defined GCC)
+    ::operator delete(pPtr); // WNT lacks a global nothrow operator delete...
+#else // WNT
+    ::operator delete(pPtr, rNothrow);
+#endif // WNT
+}
