deda9abf4a272dddcc9fbc491657b2e73a81c12c
Rüdiger Timm <rt@openoffice.org>
2005-12-14 16:14:02 +0000

    INTEGRATION: CWS impressfunctions (1.4.40); FILE MERGED 2005/10/28 10:56:51 cl 1.4.40.1: #125341# reworked FuPoor classes to use refcounting

--- sd/source/ui/inc/fuoaprms.hxx
+++ sd/source/ui/inc/fuoaprms.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuoaprms.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:36:09 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 17:13:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,16 +48,17 @@ class FuObjectAnimationParameters
 public:
     TYPEINFO();
 
+    static FunctionReference Create( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq );
+    virtual void DoExecute( SfxRequest& rReq );
+
+private:
+
     FuObjectAnimationParameters (
         ViewShell* pViewSh,
         ::sd::Window* pWin,
         ::sd::View* pView,
         SdDrawDocument* pDoc,
         SfxRequest& rReq);
-    virtual ~FuObjectAnimationParameters (void);
-
-    virtual void Activate();        // Function aktivieren
-    virtual void Deactivate();      // Function deaktivieren
 };
 
 } // end of namespace sd
--- sd/source/ui/inc/fuoltext.hxx
+++ sd/source/ui/inc/fuoltext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuoltext.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:36:39 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 17:14:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,13 +60,7 @@ class FuOutlineText
 public:
     TYPEINFO();
 
-    FuOutlineText (
-        ViewShell* pViewShell,
-        ::sd::Window* pWin,
-        ::sd::View* pView,
-        SdDrawDocument* pDoc,
-        SfxRequest& rReq);
-    virtual ~FuOutlineText (void);
+    static FunctionReference Create( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq );
 
     virtual BOOL KeyInput(const KeyEvent& rKEvt);
     virtual BOOL MouseMove(const MouseEvent& rMEvt);
@@ -85,6 +79,15 @@ public:
         preview in the slide sorter.
     */
     void UpdateForKeyPress (const KeyEvent& rEvent);
+
+protected:
+    FuOutlineText (
+        ViewShell* pViewShell,
+        ::sd::Window* pWin,
+        ::sd::View* pView,
+        SdDrawDocument* pDoc,
+        SfxRequest& rReq);
+
 };
 
 } // end of namespace sd
