b1675c906465dcf03db90cc500986a21483ff4ee
Rüdiger Timm <rt@openoffice.org>
2005-12-14 14:52:06 +0000

    INTEGRATION: CWS sj31 (1.7.24); FILE MERGED 2005/11/28 15:59:07 sj 1.7.24.1: #i58431# fixed gpf when saving certain kind of sound effects

--- sd/source/filter/eppt/pptexanimations.cxx
+++ sd/source/filter/eppt/pptexanimations.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pptexanimations.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 09:55:04 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 15:52:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -454,50 +454,53 @@ void AnimationExporter::processAfterEffectNodes( const Reference< XAnimationNode
     {
         Reference< XAnimationNode > xNode( xEnumeration->nextElement(), UNO_QUERY_THROW );
 
-        Reference< XEnumerationAccess > xEnumerationAccess2( xNode, UNO_QUERY_THROW );
-        Reference< XEnumeration > xEnumeration2( xEnumerationAccess2->createEnumeration(), UNO_QUERY_THROW );
-        while( xEnumeration2->hasMoreElements() )
+        Reference< XEnumerationAccess > xEnumerationAccess2( xNode, UNO_QUERY );
+        if ( xEnumerationAccess2.is() )
         {
-            Reference< XAnimationNode > xChildNode( xEnumeration2->nextElement(), UNO_QUERY_THROW );
-
-            Reference< XEnumerationAccess > xEnumerationAccess3( xChildNode, UNO_QUERY_THROW );
-            Reference< XEnumeration > xEnumeration3( xEnumerationAccess3->createEnumeration(), UNO_QUERY_THROW );
-            while( xEnumeration3->hasMoreElements() )
+            Reference< XEnumeration > xEnumeration2( xEnumerationAccess2->createEnumeration(), UNO_QUERY_THROW );
+            while( xEnumeration2->hasMoreElements() )
             {
-                Reference< XAnimationNode > xChildNode2( xEnumeration3->nextElement(), UNO_QUERY_THROW );
+                Reference< XAnimationNode > xChildNode( xEnumeration2->nextElement(), UNO_QUERY_THROW );
 
-                Reference< XEnumerationAccess > xEnumerationAccess4( xChildNode2, UNO_QUERY_THROW );
-                Reference< XEnumeration > xEnumeration4( xEnumerationAccess4->createEnumeration(), UNO_QUERY_THROW );
-                while( xEnumeration4->hasMoreElements() )
+                Reference< XEnumerationAccess > xEnumerationAccess3( xChildNode, UNO_QUERY_THROW );
+                Reference< XEnumeration > xEnumeration3( xEnumerationAccess3->createEnumeration(), UNO_QUERY_THROW );
+                while( xEnumeration3->hasMoreElements() )
                 {
-                    Reference< XAnimationNode > xChildNode3( xEnumeration4->nextElement(), UNO_QUERY_THROW );
+                    Reference< XAnimationNode > xChildNode2( xEnumeration3->nextElement(), UNO_QUERY_THROW );
 
-                    switch( xChildNode3->getType() )
+                    Reference< XEnumerationAccess > xEnumerationAccess4( xChildNode2, UNO_QUERY_THROW );
+                    Reference< XEnumeration > xEnumeration4( xEnumerationAccess4->createEnumeration(), UNO_QUERY_THROW );
+                    while( xEnumeration4->hasMoreElements() )
                     {
-                    // found an after effect
-                    case AnimationNodeType::SET:
-                    case AnimationNodeType::ANIMATECOLOR:
+                        Reference< XAnimationNode > xChildNode3( xEnumeration4->nextElement(), UNO_QUERY_THROW );
+
+                        switch( xChildNode3->getType() )
                         {
-                            Reference< XAnimationNode > xMaster;
-
-                            Sequence< NamedValue > aUserData( xChildNode3->getUserData() );
-                            sal_Int32 nLength = aUserData.getLength();
-                            const NamedValue* p = aUserData.getConstArray();
-
-                            while( nLength-- )
+                        // found an after effect
+                        case AnimationNodeType::SET:
+                        case AnimationNodeType::ANIMATECOLOR:
                             {
-                                if( p->Name.equalsAscii( "master-element" ) )
-                                {
-                                    p->Value >>= xMaster;
-                                    break;
-                                }
-                                p++;
-                            }
+                                Reference< XAnimationNode > xMaster;
 
-                            AfterEffectNodePtr pAfterEffectNode( new AfterEffectNode( xChildNode3, xMaster ) );
-                            maAfterEffectNodes.push_back( pAfterEffectNode );
+                                Sequence< NamedValue > aUserData( xChildNode3->getUserData() );
+                                sal_Int32 nLength = aUserData.getLength();
+                                const NamedValue* p = aUserData.getConstArray();
+
+                                while( nLength-- )
+                                {
+                                    if( p->Name.equalsAscii( "master-element" ) )
+                                    {
+                                        p->Value >>= xMaster;
+                                        break;
+                                    }
+                                    p++;
+                                }
+
+                                AfterEffectNodePtr pAfterEffectNode( new AfterEffectNode( xChildNode3, xMaster ) );
+                                maAfterEffectNodes.push_back( pAfterEffectNode );
+                            }
+                            break;
                         }
-                        break;
                     }
                 }
             }
