91463fdb460b8cb3dd7a3ee45680d8b2a9d31db4
Rüdiger Timm <rt@openoffice.org>
2005-12-14 16:18:37 +0000

    INTEGRATION: CWS impressfunctions (1.3.40); FILE MERGED 2005/10/28 10:56:56 cl 1.3.40.1: #125341# reworked FuPoor classes to use refcounting

--- sd/source/ui/inc/futempl.hxx
+++ sd/source/ui/inc/futempl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: futempl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:41:06 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 17:18:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,16 +48,19 @@ class FuTemplate
 public:
     TYPEINFO();
 
+    virtual void Activate();
+    virtual void Deactivate();
+
+    static FunctionReference Create( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq );
+    virtual void DoExecute( SfxRequest& rReq );
+
+private:
     FuTemplate (
         ViewShell* pViewSh,
         ::sd::Window* pWin,
         ::sd::View* pView,
         SdDrawDocument* pDoc,
         SfxRequest& rReq);
-    virtual ~FuTemplate (void) {}
-
-    virtual void Activate() {}
-    virtual void Deactivate() {}
 };
 
 } // end of namespace sd
--- sd/source/ui/inc/futhes.hxx
+++ sd/source/ui/inc/futhes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: futhes.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:41:33 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 17:18:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,13 +48,16 @@ class FuThesaurus
 public:
     TYPEINFO();
 
+    static FunctionReference Create( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq );
+    virtual void DoExecute( SfxRequest& rReq );
+
+private:
     FuThesaurus (
         ViewShell* pViewSh,
         ::sd::Window* pWin,
         ::sd::View* pView,
         SdDrawDocument* pDoc,
         SfxRequest& rReq);
-    virtual ~FuThesaurus (void);
 };
 
 } // end of namespace sd
