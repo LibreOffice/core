0df5688ce8625b091c7c4bc50661855e91a23097
Rüdiger Timm <rt@openoffice.org>
2005-12-14 16:13:27 +0000

    INTEGRATION: CWS impressfunctions (1.3.38); FILE MERGED 2005/10/28 10:56:50 cl 1.3.38.1: #125341# reworked FuPoor classes to use refcounting

--- sd/source/ui/inc/fumorph.hxx
+++ sd/source/ui/inc/fumorph.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fumorph.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:35:55 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 17:13:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,15 +58,17 @@ class FuMorph
 public:
     TYPEINFO();
 
+    static FunctionReference Create( ViewShell* pViewSh, ::sd::Window* pWin, ::sd::View* pView, SdDrawDocument* pDoc, SfxRequest& rReq );
+    virtual void DoExecute( SfxRequest& rReq );
+
+private:
     FuMorph (
         ViewShell* pViewSh,
         ::sd::Window* pWin,
         ::sd::View* pView,
         SdDrawDocument* pDoc,
         SfxRequest& rReq);
-    virtual ~FuMorph (void) {}
 
-private:
     void ImpInsertPolygons(List& rPolyPolyList3D, BOOL bAttributeFade,
         const SdrObject* pObj1, const SdrObject* pObj2);
     PolyPolygon3D* ImpCreateMorphedPolygon(
