c753404001f9dbcb8ce8b45668bcfe5f22d02709
Rüdiger Timm <rt@openoffice.org>
2005-12-14 11:06:34 +0000

    INTEGRATION: CWS buildstatushtml2 (1.4.292); FILE MERGED 2005/11/07 12:58:09 vg 1.4.292.2: RESYNC: (1.4-1.5); FILE MERGED 2005/09/16 13:31:03 vg 1.4.292.1: #125903#

--- solenv/bin/mkout.pl
+++ solenv/bin/mkout.pl
@@ -7,9 +7,9 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 #
 #   $RCSfile: mkout.pl,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 22:12:10 $
+#   last change: $Author: rt $ $Date: 2005-12-14 12:06:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,7 @@ use File::Path;
 
 ( $script_name = $0 ) =~ s/^.*\b(\w+)\.pl$/$1/;
 
-$id_str = ' $Revision: 1.5 $ ';
+$id_str = ' $Revision: 1.6 $ ';
 $id_str =~ /Revision:\s+(\S+)\s+\$/
   ? ($script_rev = $1) : ($script_rev = "-");
 
@@ -70,7 +70,7 @@ $opt_r      = 0;            # create 'remote' subdirs
                 'class' => 0,
                 'inc'   => 0,
                 'lib'   => 1,
-                'misc'  => 1,
+                'misc/logs'  => 1,
                 'obj'   => 1,
                 'res'   => 1,
                 'slb'   => 1,
@@ -123,6 +123,8 @@ sub get_base {
     # which in turn contains a file 'd.lst'
     my (@field, $base, $dlst);
     my $path = cwd();
+
+    print $path;
 
     @field = split(/\//, $path);
 
