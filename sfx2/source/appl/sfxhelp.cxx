2f0c26fb7788a19cc09f0cc87f54e57d466d05ba
Rüdiger Timm <rt@openoffice.org>
2005-12-14 14:21:39 +0000

    INTEGRATION: CWS helpnotfoundmsg (1.66.68); FILE MERGED 2005/11/08 20:44:20 rene 1.66.68.1: #i55841# make error message when no help is found more clear

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxhelp.cxx,v $
  *
- *  $Revision: 1.66 $
+ *  $Revision: 1.67 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:47:17 $
+ *  last change: $Author: rt $ $Date: 2005-12-14 15:21:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -151,7 +151,7 @@ public:
 
 NoHelpErrorBox::NoHelpErrorBox( Window* _pParent ) :
 
-    ErrorBox( _pParent, WB_OK, String( SfxResId( RID_STR_HLPAPPNOTSTARTED ) ) )
+    ErrorBox( _pParent, WB_OK, String( SfxResId( RID_STR_HLPFILENOTEXIST ) ) )
 {
     // Error message: "No help available"
 }
