ca122016f7911e4778d35393d7f827f5123d605a
Oliver Bolte <obo@openoffice.org>
2005-12-19 15:51:42 +0000

    INTEGRATION: CWS kaddrbook (1.1.2); FILE ADDED 2005/12/02 20:19:16 ebischoff 1.1.2.4: Issue number: Submitted by: Reviewed by: Making "Revision date" a "version" field. Such fields are not displayed, by default. 2005/11/30 13:56:41 kendy 1.1.2.3: #i54170# SISSL/LGPL -> LGPL for the KDE AddressBook files. 2005/08/30 15:36:25 ebischoff 1.1.2.2: Started applying Frank's remarks 2005/08/29 08:43:54 ebischoff 1.1.2.1: Initial checkin of KDE address book driver

---
+++ connectivity/source/drivers/kab/KTable.cxx
@@ -0,0 +1,107 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: KTable.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-12-19 16:51:42 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "KTable.hxx"
+#ifndef _CONNECTIVITY_KAB_TABLES_HXX_
+#include "KTables.hxx"
+#endif
+#ifndef _CONNECTIVITY_KAB_COLUMNS_HXX_
+#include "KColumns.hxx"
+#endif
+#ifndef _CONNECTIVITY_KAB_CATALOG_HXX_
+#include "KCatalog.hxx"
+#endif
+
+using namespace connectivity::kab;
+using namespace connectivity;
+using namespace ::comphelper;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::sdbcx;
+using namespace ::com::sun::star::sdbc;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::lang;
+
+// -------------------------------------------------------------------------
+KabTable::KabTable( sdbcx::OCollection* _pTables, KabConnection* _pConnection)
+    : KabTable_TYPEDEF(_pTables, sal_True),
+    m_pConnection(_pConnection)
+{
+    construct();
+}
+// -------------------------------------------------------------------------
+KabTable::KabTable( sdbcx::OCollection* _pTables,
+                KabConnection* _pConnection,
+                const ::rtl::OUString& _Name,
+                const ::rtl::OUString& _Type,
+                const ::rtl::OUString& _Description ,
+                const ::rtl::OUString& _SchemaName,
+                const ::rtl::OUString& _CatalogName
+                ) : KabTable_TYPEDEF(_pTables,sal_True,
+                                  _Name,
+                                  _Type,
+                                  _Description,
+                                  _SchemaName,
+                                  _CatalogName),
+                    m_pConnection(_pConnection)
+{
+    construct();
+}
+// -------------------------------------------------------------------------
+void KabTable::refreshColumns()
+{
+    TStringVector aVector;
+
+    if (!isNew())
+    {
+        Reference< XResultSet > xResult = m_pConnection->getMetaData()->getColumns(
+                Any(),
+                m_SchemaName,
+                m_Name,
+                ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("%")));
+
+        if (xResult.is())
+        {
+        Reference< XRow > xRow(xResult, UNO_QUERY);
+        while (xResult->next())
+                aVector.push_back(xRow->getString(4));
+        }
+    }
+
+    if (m_pColumns)
+        m_pColumns->reFill(aVector);
+    else
+        m_pColumns  = new KabColumns(this,m_aMutex,aVector);
+}
