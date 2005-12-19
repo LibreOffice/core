613e623a7179a8998149f54a1a5312750e3fd90d
Oliver Bolte <obo@openoffice.org>
2005-12-19 15:48:25 +0000

    INTEGRATION: CWS kaddrbook (1.1.2); FILE ADDED 2005/12/02 20:19:10 ebischoff 1.1.2.3: Issue number: Submitted by: Reviewed by: Making "Revision date" a "version" field. Such fields are not displayed, by default. 2005/11/30 13:56:35 kendy 1.1.2.2: #i54170# SISSL/LGPL -> LGPL for the KDE AddressBook files. 2005/08/29 08:43:47 ebischoff 1.1.2.1: Initial checkin of KDE address book driver

---
+++ connectivity/source/drivers/kab/KColumns.cxx
@@ -0,0 +1,111 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: KColumns.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-12-19 16:48:15 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "KColumns.hxx"
+#ifndef _CONNECTIVITY_KAB_TABLE_HXX_
+#include "KTable.hxx"
+#endif
+#ifndef _CONNECTIVITY_KAB_TABLES_HXX_
+#include "KTables.hxx"
+#endif
+#ifndef _CONNECTIVITY_KAB_CATALOG_HXX_
+#include "KCatalog.hxx"
+#endif
+
+#ifndef _CONNECTIVITY_SDBCX_COLUMN_HXX_
+#include "connectivity/sdbcx/VColumn.hxx"
+#endif
+
+using namespace connectivity::kab;
+using namespace connectivity::sdbcx;
+using namespace connectivity;
+using namespace ::comphelper;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::sdbc;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::lang;
+
+// -------------------------------------------------------------------------
+sdbcx::ObjectType KabColumns::createObject(const ::rtl::OUString& _rName)
+{
+    Reference< XResultSet > xResult = m_pTable->getConnection()->getMetaData()->getColumns(
+        Any(),
+        m_pTable->getSchema(),
+        m_pTable->getTableName(),
+        _rName);
+
+    sdbcx::ObjectType xRet = NULL;
+    if (xResult.is())
+    {
+        Reference< XRow > xRow(xResult,UNO_QUERY);
+
+        while (xResult->next())
+        {
+            if (xRow->getString(4) == _rName)
+            {
+                OColumn* pRet = new OColumn(
+                        _rName,
+                        xRow->getString(6),
+                        xRow->getString(13),
+                        xRow->getInt(11),
+                        xRow->getInt(7),
+                        xRow->getInt(9),
+                        xRow->getInt(5),
+                        sal_False,
+                        sal_False,
+                        sal_False,
+                        sal_True);
+                xRet = pRet;
+                break;
+            }
+        }
+    }
+
+    return xRet;
+}
+// -------------------------------------------------------------------------
+void KabColumns::impl_refresh() throw(RuntimeException)
+{
+    m_pTable->refreshColumns();
+}
+// -------------------------------------------------------------------------
+KabColumns::KabColumns( KabTable* _pTable,
+                        ::osl::Mutex& _rMutex,
+                        const TStringVector &_rVector)
+    : sdbcx::OCollection(*_pTable, sal_True, _rMutex, _rVector),
+      m_pTable(_pTable)
+{
+}
---
+++ connectivity/source/drivers/kab/KColumns.hxx
@@ -0,0 +1,67 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: KColumns.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2005-12-19 16:48:25 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CONNECTIVITY_KAB_COLUMNS_HXX_
+#define _CONNECTIVITY_KAB_COLUMNS_HXX_
+
+#ifndef _CONNECTIVITY_KAB_TABLE_HXX_
+#include "KTable.hxx"
+#endif
+
+#ifndef _CONNECTIVITY_SDBCX_COLLECTION_HXX_
+#include "connectivity/sdbcx/VCollection.hxx"
+#endif
+
+namespace connectivity
+{
+    namespace kab
+    {
+        class KabColumns : public sdbcx::OCollection
+        {
+        protected:
+            KabTable*   m_pTable;
+
+            virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
+            virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
+
+        public:
+            KabColumns( KabTable* _pTable,
+                        ::osl::Mutex& _rMutex,
+                        const TStringVector &_rVector);
+        };
+    }
+}
+
+#endif // _CONNECTIVITY_KAB_COLUMNS_HXX_
