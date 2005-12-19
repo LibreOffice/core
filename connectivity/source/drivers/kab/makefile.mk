78106b85bc8f33f52dfc00138f5e3b5358c05ea8
Oliver Bolte <obo@openoffice.org>
2005-12-19 15:54:12 +0000

    INTEGRATION: CWS kaddrbook (1.1.2); FILE ADDED 2005/12/04 11:28:48 ebischoff 1.1.2.9: Removing unnecessary library 2005/12/01 09:58:19 ebischoff 1.1.2.8: Issue number: #52449 Unneeded dependancy towards file driver removed 2005/11/30 13:56:44 kendy 1.1.2.7: #i54170# SISSL/LGPL -> LGPL for the KDE AddressBook files. 2005/11/28 10:59:30 ebischoff 1.1.2.6: Handling ORDER BY clauses 2005/09/13 18:20:42 ebischoff 1.1.2.5: - Added two new fields: "identifier" and "revision date".   Now handling TIMESTAMP data type in addition to CHAR(256). - Renamed KCondition.* to kcondition.* so that my own files   are clearly distinguished from the driver itself. 2005/08/31 08:30:37 ebischoff 1.1.2.4: These lines have moved to solenv/inc/unxlngi6.mk as planned. 2005/08/31 08:14:05 ebischoff 1.1.2.3: Whoops, forgot about the libs. This KDE_ROOT hack will be removed once it goes into a central place (see issue #54014) 2005/08/30 15:36:26 ebischoff 1.1.2.2: Started applying Frank's remarks 2005/08/29 08:43:57 ebischoff 1.1.2.1: Initial checkin of KDE address book driver

---
+++ connectivity/source/drivers/kab/makefile.mk
@@ -0,0 +1,121 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2005-12-19 16:54:12 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+PRJINC=..$/..
+PRJNAME=connectivity
+TARGET=kab
+
+# --- Settings ----------------------------------
+.IF "$(DBGUTIL_OJ)"!=""
+ENVCFLAGS+=/FR$(SLO)$/
+.ENDIF
+
+.INCLUDE : settings.mk
+.INCLUDE :  $(PRJ)$/version.mk
+
+.IF "$(GUI)" == "UNX"
+.IF "$(ENABLE_KDE)" == "TRUE"
+# --- Files -------------------------------------
+
+EXCEPTIONSFILES=\
+    $(SLO)$/KColumns.obj			\
+    $(SLO)$/KTable.obj				\
+    $(SLO)$/KTables.obj				\
+    $(SLO)$/KCatalog.obj			\
+    $(SLO)$/KResultSet.obj			\
+    $(SLO)$/KStatement.obj			\
+    $(SLO)$/KPreparedStatement.obj	\
+    $(SLO)$/KDatabaseMetaData.obj	\
+    $(SLO)$/KConnection.obj			\
+    $(SLO)$/KServices.obj			\
+    $(SLO)$/KResultSetMetaData.obj	\
+    $(SLO)$/KDriver.obj				\
+    $(SLO)$/kcondition.obj			\
+    $(SLO)$/korder.obj				\
+    $(SLO)$/kfields.obj				\
+ 
+CFLAGS+=$(KDE_CFLAGS)
+
+SLOFILES=\
+    $(EXCEPTIONSFILES)
+
+KAB_LIB=$(KDE_LIBS) -lkabc
+
+SHL1VERSIONMAP=$(TARGET).map
+
+
+# --- Library -----------------------------------
+
+SHL1TARGET=	$(KAB_TARGET)$(KAB_MAJOR)
+SHL1OBJS=$(SLOFILES)
+SHL1STDLIBS=\
+    $(CPPULIB)					\
+    $(CPPUHELPERLIB)			\
+    $(VOSLIB)					\
+    $(OSLLIB)					\
+    $(SVLLIB)					\
+    $(SVLIB)					\
+    $(TOOLSLIB)					\
+    $(SVTOOLLIB)				\
+    $(SVTLIB)					\
+    $(UNOTOOLSLIB)				\
+    $(UCBHELPERLIB)				\
+    $(SALLIB)					\
+    $(DBTOOLSLIB)				\
+    $(COMPHELPERLIB)			\
+    $(KAB_LIB)
+
+SHL1DEPN=
+SHL1IMPLIB=	i$(TARGET)
+
+SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+
+DEF1NAME=	$(SHL1TARGET)
+
+
+# --- Targets -----------------------------------
+.ELSE		# "$(ENABLE_KDE)" == "TRUE"
+dummy:
+    @echo KDE disabled - nothing to build
+.ENDIF
+
+.ELSE		# "$(GUI)" == "UNX"
+dummy:
+    @echo "Nothing to build for GUI $(GUI)"
+.ENDIF
+
+.INCLUDE : target.mk
+
