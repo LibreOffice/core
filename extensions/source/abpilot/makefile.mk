5ab5abb68c9229ccf332c3abec4bee70aa420017
Oliver Bolte <obo@openoffice.org>
2005-12-19 16:27:56 +0000

    INTEGRATION: CWS kaddrbook (1.8.54); FILE MERGED 2005/12/06 07:46:51 fs 1.8.54.1: don't provide Mozilla-based options if we're built without Mozilla support

--- extensions/source/abpilot/makefile.mk
+++ extensions/source/abpilot/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 19:08:58 $
+#   last change: $Author: obo $ $Date: 2005-12-19 17:27:56 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -69,6 +69,9 @@ SLOFILES=	\
         $(EXCEPTIONSFILES)
 
 
+.IF "WITH_MOZILLA" != ""
+CDEFS+=-DWITH_MOZILLA
+.ENDIF
 
 SRS1NAME=$(TARGET)
 SRC1FILES=			abspilot.src
