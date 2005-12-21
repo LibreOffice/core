7b28b6b3999457a9851b18af45325a764b0021f1
Oliver Bolte <obo@openoffice.org>
2005-12-21 11:49:49 +0000

    INTEGRATION: CWS nativefixer21 (1.9.8); FILE MERGED 2005/09/16 11:42:13 is 1.9.8.1: #i54688# Windows CustomActions into scp2

--- solenv/bin/modules/pre2par/globals.pm
+++ solenv/bin/modules/pre2par/globals.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: globals.pm,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:30:51 $
+#   last change: $Author: obo $ $Date: 2005-12-21 12:49:49 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@ BEGIN
 
     @allitems = ("Installation", "ScpAction", "HelpText", "Directory", "DataCarrier", "StarRegistry", "File",
                  "Shortcut", "Custom", "Procedure", "Module", "Profile", "ProfileItem",
-                 "Folder", "FolderItem", "RegistryItem", "StarRegistryItem");
+                 "Folder", "FolderItem", "RegistryItem", "StarRegistryItem", "WindowsCustomAction");
 
     $logging     = 0;
     $logfilename = "logfile.log";   # the default logfile name for global errors
