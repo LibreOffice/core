6b13de11972c1a15312d45ae9abf485db58a00f3
Oliver Bolte <obo@openoffice.org>
2005-12-21 16:54:26 +0000

    INTEGRATION: CWS pj45 (1.5.132); FILE MERGED 2005/12/17 18:02:22 pjanik 1.5.132.1: #i58068#: Add visibility markup (patch from Naren Devaiah, Intel, JCA).

--- svtools/inc/stdmenu.hxx
+++ svtools/inc/stdmenu.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stdmenu.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 13:57:48 $
+ *  last change: $Author: obo $ $Date: 2005-12-21 17:54:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -198,14 +198,14 @@ public:
 #define FONTSTYLEMENU_FIRSTID       62000
 #define FONTSTYLEMENU_LASTID        62999
 
-class FontStyleMenu : public PopupMenu
+class SVT_DLLPUBLIC FontStyleMenu : public PopupMenu
 {
 private:
     XubString       maCurStyle;
     Link            maSelectHdl;
     Link            maHighlightHdl;
 
-    BOOL            ImplIsAlreadyInserted( const XubString& rStyleName, USHORT nCount );
+    SVT_DLLPRIVATE BOOL         ImplIsAlreadyInserted( const XubString& rStyleName, USHORT nCount );
 
 public:
                     FontStyleMenu();
