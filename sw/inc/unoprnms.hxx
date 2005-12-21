eb73d75334e87071fe60ed6e78310739eaa77a72
Oliver Bolte <obo@openoffice.org>
2005-12-21 14:10:09 +0000

    INTEGRATION: CWS emptypage (1.111.58); FILE MERGED 2005/12/13 15:27:10 fme 1.111.58.1: #b6354161# Feature - Print empty pages

--- sw/inc/unoprnms.hxx
+++ sw/inc/unoprnms.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoprnms.hxx,v $
  *
- *  $Revision: 1.111 $
+ *  $Revision: 1.112 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-11 13:34:39 $
+ *  last change: $Author: obo $ $Date: 2005-12-21 15:10:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -768,7 +768,10 @@ enum SwPropNameIds
 /* 0701 */  UNO_NAME_IS_FIELD_DISPLAYED,
 /* 0702 */  UNO_NAME_BUILDID,
 /* 0703 */  UNO_NAME_PARA_STYLE_CONDITIONS,
-/* 0704 */  SW_PROPNAME_END
+// --> FME 2005-12-13 #b6354161# Print empty pages
+/* 0704 */  UNO_NAME_PRINT_EMPTY_PAGES,
+// <--
+/* 0705 */  SW_PROPNAME_END
 };
 
 
