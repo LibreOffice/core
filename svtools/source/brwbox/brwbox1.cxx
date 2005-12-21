534b84dcabc7a541038ac79e5190518f84db876c
Oliver Bolte <obo@openoffice.org>
2005-12-21 12:41:12 +0000

    INTEGRATION: CWS dba202a (1.35.90); FILE MERGED 2005/11/21 08:43:26 oj 1.35.90.1: #i56333# check column id for invalid

--- svtools/source/brwbox/brwbox1.cxx
+++ svtools/source/brwbox/brwbox1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: brwbox1.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:28:47 $
+ *  last change: $Author: obo $ $Date: 2005-12-21 13:41:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2055,7 +2055,7 @@ void BrowseBox::SelectColumnPos( USHORT nNewColPos, BOOL _bSelect, BOOL bMakeVis
 {
     DBG_CHKTHIS(BrowseBox,BrowseBoxCheckInvariants);
 
-    if ( !bColumnCursor )
+    if ( !bColumnCursor || nNewColPos == BROWSER_INVALIDID )
         return;
 
     if ( !bMultiSelection )
