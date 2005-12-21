e9a815865523bdbf28c6de1d3e717b8cd70a656e
Oliver Bolte <obo@openoffice.org>
2005-12-21 11:49:37 +0000

    INTEGRATION: CWS nativefixer21 (1.6.8); FILE MERGED 2005/09/16 11:42:42 is 1.6.8.1: #i54688# Windows CustomActions into scp2

--- solenv/bin/modules/par2script/globals.pm
+++ solenv/bin/modules/par2script/globals.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: globals.pm,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:26:29 $
+#   last change: $Author: obo $ $Date: 2005-12-21 12:49:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ BEGIN
 
     @allitems = ("Installation", "ScpAction", "HelpText", "Directory", "DataCarrier", "StarRegistry", "File",
                  "Shortcut", "Custom", "Procedure", "Module", "Profile", "ProfileItem",
-                 "Folder", "FolderItem", "RegistryItem", "StarRegistryItem");
+                 "Folder", "FolderItem", "RegistryItem", "StarRegistryItem", "WindowsCustomAction");
 
     @items_with_moduleid = ("Profile", "ProfileItem",
                              "FolderItem", "RegistryItem", "StarRegistryItem");
