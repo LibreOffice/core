4ed966c1fcab0609473260fd1144d8c89825aa95
Kurt Zenker <kz@openoffice.org>
2006-01-03 15:20:54 +0000

    INTEGRATION: CWS dba202c (1.18.54); FILE MERGED 2005/12/05 14:28:32 fs 1.18.54.1: #i48619 OMultilineEditControl is a CompoundControl

--- extensions/source/propctrlr/standardcontrol.cxx
+++ extensions/source/propctrlr/standardcontrol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: standardcontrol.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:28:18 $
+ *  last change: $Author: kz $ $Date: 2006-01-03 16:20:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -966,6 +966,8 @@ namespace pcr
             ,m_bDropdown(sal_False)
             ,m_bEdit(bEd)
     {
+        SetCompoundControl( TRUE );
+
         m_pImplEdit = new MultiLineEdit(this,WB_TABSTOP|WB_IGNORETAB| WB_NOBORDER| nWinStyle& WB_READONLY);
         SetSubEdit( m_pImplEdit );
         m_pImplEdit->Show();
