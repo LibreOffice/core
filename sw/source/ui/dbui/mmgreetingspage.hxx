6467e29d2622b1002fc7c79313a9eba696eadf2c
Kurt Zenker <kz@openoffice.org>
2006-01-06 12:02:50 +0000

    INTEGRATION: CWS os67 (1.2.574); FILE MERGED 2005/09/28 08:26:38 os 1.2.574.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/26 12:26:52 os 1.2.574.1: #ii51947# work flow in address page changed

--- sw/source/ui/dbui/mmgreetingspage.hxx
+++ sw/source/ui/dbui/mmgreetingspage.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmgreetingspage.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:00:27 $
+ *  last change: $Author: kz $ $Date: 2006-01-06 13:02:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,8 @@ class SwGreetingsHandler
 
     FixedText*          m_pNeutralFT;
     ComboBox*           m_pNeutralCB;
+
+    bool                m_bIsTabPage;
 
     SwMailMergeWizard*  m_pWizard;
 
