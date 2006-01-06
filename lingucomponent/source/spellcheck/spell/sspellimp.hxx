7ac428e23c242ddde9d41eaf82b6a54005b708cc
Kurt Zenker <kz@openoffice.org>
2006-01-06 12:13:16 +0000

    INTEGRATION: CWS hunspell (1.6.98); FILE MERGED 2005/09/19 22:13:18 nemeth 1.6.98.2: RESYNC: (1.6-1.7); FILE MERGED 2005/09/19 21:54:28 nemeth 1.6.98.1: i52383

--- lingucomponent/source/spellcheck/spell/sspellimp.hxx
+++ lingucomponent/source/spellcheck/spell/sspellimp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sspellimp.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:43:31 $
+ *  last change: $Author: kz $ $Date: 2006-01-06 13:13:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -96,7 +96,7 @@ class SpellChecker :
     >
 {
     Sequence< Locale >                 aSuppLocales;
-        MySpell **                         aDicts;
+        Hunspell **                         aDicts;
         rtl_TextEncoding *                 aDEncs;
         Locale *                           aDLocs;
         OUString *                         aDNames;
