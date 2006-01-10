91b854d9a4525a3ea9cbcdcb213dfd874193da6c
Rüdiger Timm <rt@openoffice.org>
2006-01-10 13:23:53 +0000

    INTEGRATION: CWS impresspresobjs (1.1.2); FILE ADDED 2005/11/24 11:59:19 cl 1.1.2.1: #123595# added application specific undo actions

---
+++ sd/inc/undo/undofactory.hxx
@@ -0,0 +1,60 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: undofactory.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-01-10 14:23:53 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SD_UNDOFACTORY_HXX
+#define _SD_UNDOFACTORY_HXX
+
+#ifndef _SVDUNDO_HXX
+#include <svx/svdundo.hxx>
+#endif
+
+namespace sd
+{
+
+class UndoFactory : public SdrUndoFactory
+{
+public:
+    virtual SdrUndoAction* CreateUndoRemoveObject( SdrObject& rObject, bool bOrdNumDirect = false);
+    virtual SdrUndoAction* CreateUndoDeleteObject( SdrObject& rObject, bool bOrdNumDirect = false);
+    virtual SdrUndoAction* CreateUndoObjectSetText( SdrObject& rNewObj );
+    virtual SdrUndoAction* CreateUndoReplaceObject( SdrObject& rOldObject, SdrObject& rNewObject, bool bOrdNumDirect = false );
+    virtual SdrUndoAction* CreateUndoGeoObject( SdrObject& rObject );
+    virtual SdrUndoAction* CreateUndoAttrObject( SdrObject& rObject, bool bStyleSheet1 = false, bool bSaveText = false );
+};
+
+
+}
+
+#endif     // _SD_UNDOMANAGER_HXX
