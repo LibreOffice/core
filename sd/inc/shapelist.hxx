3362125802ee54b407b584b429d0769158db1dfe
Rüdiger Timm <rt@openoffice.org>
2006-01-10 13:23:20 +0000

    INTEGRATION: CWS impresspresobjs (1.1.2); FILE ADDED 2005/11/24 12:00:21 cl 1.1.2.1: #123595# new helper

---
+++ sd/inc/shapelist.hxx
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: shapelist.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-01-10 14:23:20 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SHAPELIST_HXX
+#define _SHAPELIST_HXX
+
+#ifndef _SDR_OBJECTUSER_HXX
+#include <svx/sdrobjectuser.hxx>
+#endif
+
+#include <list>
+
+namespace sd
+{
+    class ShapeList : public sdr::ObjectUser
+    {
+    public:
+        virtual ~ShapeList();
+
+        /** adds the given shape to this list */
+        void addShape( SdrObject& rObject );
+
+        /** removes the shape from this list and returns
+            a pointer to the next shape in list or 0*/
+        SdrObject* removeShape( SdrObject& rObject );
+
+        /** removes all shapes from this list */
+        void clear();
+
+        /** returns true if this list is empty */
+        bool isEmpty() const;
+
+        /** returns true if given shape is part of this list */
+        bool hasShape( SdrObject& rObject ) const;
+
+        /** returns the shape following the given shape in the list or 0
+            returns the first shape if pObj is 0 */
+        SdrObject* getNextShape(SdrObject* pObj) const;
+
+        /** returns the shape prior to the given shape in the list or 0
+            returns the last shape if pObj is 0 */
+        SdrObject* getPreviousShape( SdrObject* pObj ) const;
+
+    private:
+        virtual void ObjectInDestruction(const SdrObject& rObject);
+
+        typedef std::list< SdrObject* > ListImpl;
+        ListImpl maShapeList;
+    };
+}
+
+#endif     // _SHAPELIST_HXX
