976e529f98603244064599b87f949abf60fd0823
Rüdiger Timm <rt@openoffice.org>
2006-01-13 16:00:47 +0000

    INTEGRATION: CWS dr43 (1.25.42); FILE MERGED 2005/12/02 14:53:59 nn 1.25.42.1: #i56873# check if AutoStyles exists

--- sc/source/filter/xml/xmlbodyi.cxx
+++ sc/source/filter/xml/xmlbodyi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlbodyi.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:03:19 $
+ *  last change: $Author: rt $ $Date: 2006-01-13 17:00:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -272,7 +272,7 @@ void ScXMLBodyContext::EndElement()
                         {
                             rtl::OUString sTableStyleName(aTableStyleNames[i]);
                             XMLTableStylesContext *pStyles = (XMLTableStylesContext *)GetScImport().GetAutoStyles();
-                            if (sTableStyleName.getLength())
+                            if ( pStyles && sTableStyleName.getLength() )
                             {
                                 XMLTableStyleContext* pStyle = (XMLTableStyleContext *)pStyles->FindStyleChildContext(
                                     XML_STYLE_FAMILY_TABLE_TABLE, sTableStyleName, sal_True);
