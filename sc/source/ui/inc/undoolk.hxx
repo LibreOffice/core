8f627d8fe96f1941a50a851ceb521207af090b76
Rüdiger Timm <rt@openoffice.org>
2006-01-13 16:04:38 +0000

    INTEGRATION: CWS dr43 (1.2.42); FILE MERGED 2005/12/21 11:52:58 nn 1.2.42.1: #125875# ensure right draw page count after undo if there was no drawing layer before

--- sc/source/ui/inc/undoolk.hxx
+++ sc/source/ui/inc/undoolk.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoolk.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:03:11 $
+ *  last change: $Author: rt $ $Date: 2006-01-13 17:04:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@ class SdrUndoAction;
 class ScDocument;
 
 SdrUndoAction*  GetSdrUndoAction    ( ScDocument* pDoc );
-void            DoSdrUndoAction     ( SdrUndoAction* pUndoAction );
+void            DoSdrUndoAction     ( SdrUndoAction* pUndoAction, ScDocument* pDoc );
 void            RedoSdrUndoAction   ( SdrUndoAction* pUndoAction );
 void            DeleteSdrUndoAction ( SdrUndoAction* pUndoAction );
 void            EnableDrawAdjust    ( ScDocument* pDoc, BOOL bEnable );
