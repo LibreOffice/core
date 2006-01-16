269217bdf530332e6dc7fcb5eed7280f0366119e
Oliver Bolte <obo@openoffice.org>
2006-01-16 12:23:45 +0000

    INTEGRATION: CWS allocacleanup (1.3.10); FILE MERGED 2005/11/06 08:52:21 maho 1.3.10.1: use include <sal/alloca.h> instead of include <alloca.h> Issue number:  #i53286#

--- bridges/source/cpp_uno/gcc3_solaris_sparc/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_solaris_sparc/uno2cpp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uno2cpp.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 22:32:10 $
+ *  last change: $Author: obo $ $Date: 2006-01-16 13:23:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 
 #include "share.hxx"
 
-#include <alloca.h>
+#include <sal/alloca.h>
 
 using namespace rtl;
 using namespace com::sun::star::uno;
