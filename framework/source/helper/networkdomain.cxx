68775446752071261a392b4a663321db8df38169
Oliver Bolte <obo@openoffice.org>
2006-01-16 12:17:55 +0000

    INTEGRATION: CWS allocacleanup (1.4.46); FILE MERGED 2005/11/06 09:11:56 maho 1.4.46.1: we use sal/alloca.h instead of alloca.h Issue number:  #i53286#

--- framework/source/helper/networkdomain.cxx
+++ framework/source/helper/networkdomain.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: networkdomain.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:24:10 $
+ *  last change: $Author: obo $ $Date: 2006-01-16 13:17:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -165,7 +165,7 @@ rtl::OUString NetworkDomain::GetNTDomainName()
 //_________________________________________________________________________________________________________________
 
 #include <sys/systeminfo.h>
-#include <alloca.h>
+#include <sal/alloca.h>
 
 static rtl_uString *getDomainName()
 {
