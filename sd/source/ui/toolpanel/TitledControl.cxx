2b8fbb096da15b733ca94d92af9cfcda3af07815
Oliver Bolte <obo@openoffice.org>
2006-01-19 11:55:22 +0000

    INTEGRATION: CWS impress78 (1.9.82); FILE MERGED 2005/12/15 16:49:45 af 1.9.82.1: #i57884# Changed role to LIST_ITEM.

--- sd/source/ui/toolpanel/TitledControl.cxx
+++ sd/source/ui/toolpanel/TitledControl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TitledControl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:36:11 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 12:55:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -470,7 +470,7 @@ TitleBar* TitledControl::GetTitleBar (void)
         *this,
         GetTitle(),
         GetTitle(),
-        ::com::sun::star::accessibility::AccessibleRole::PANEL);
+        ::com::sun::star::accessibility::AccessibleRole::LIST_ITEM);
 }
 
 
