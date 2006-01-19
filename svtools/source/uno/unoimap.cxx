f177218306e3b32129d72b010e9aee01f1633246
Oliver Bolte <obo@openoffice.org>
2006-01-19 17:35:18 +0000

    INTEGRATION: CWS gcc41 (1.10.116); FILE MERGED 2005/12/07 14:17:19 pmladek 1.10.116.1: #i58967# Removed extra qualification to fix build with gcc-4.1

--- svtools/source/uno/unoimap.cxx
+++ svtools/source/uno/unoimap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoimap.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:56:47 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 18:35:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -172,7 +172,7 @@ public:
     virtual Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw( RuntimeException );
 
 private:
-    static PropertySetInfo* SvUnoImageMapObject::createPropertySetInfo( UINT16 nType );
+    static PropertySetInfo* createPropertySetInfo( UINT16 nType );
 
 
     UINT16 mnType;
