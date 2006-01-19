5d671c2f0af794d4503fdf402044532c68973d6f
Oliver Bolte <obo@openoffice.org>
2006-01-19 11:53:39 +0000

    INTEGRATION: CWS impress78 (1.4.84); FILE MERGED 2005/12/12 13:08:42 af 1.4.84.1: #i58769# Added destructor.

--- sd/source/ui/slidesorter/inc/view/SlsPageObjectViewContact.hxx
+++ sd/source/ui/slidesorter/inc/view/SlsPageObjectViewContact.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsPageObjectViewContact.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:22:49 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 12:53:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -66,6 +66,7 @@ public:
     PageObjectViewContact (
         SdrPageObj& rPageObj,
         model::PageDescriptor& rDescriptor);
+    ~PageObjectViewContact (void);
 
     /** Create a ViewObjectContact object that buffers its output in a
         bitmap.
