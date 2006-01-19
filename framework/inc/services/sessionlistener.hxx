5d76a008eddd53a23ac2bfa416a719b61579f470
Oliver Bolte <obo@openoffice.org>
2006-01-19 16:50:05 +0000

    INTEGRATION: CWS gcc41 (1.3.64); FILE MERGED 2005/12/07 13:10:56 pmladek 1.3.64.1: #i58967# Removed extra qualification to fix build with gcc-4.1

--- framework/inc/services/sessionlistener.hxx
+++ framework/inc/services/sessionlistener.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sessionlistener.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:34:11 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 17:50:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -225,7 +225,7 @@ class SessionListener :   // interfaces
 
 
         // XInitialization
-        virtual void SAL_CALL SessionListener::initialize(const css::uno::Sequence< css::uno::Any  >& args) throw (css::uno::RuntimeException);
+        virtual void SAL_CALL initialize(const css::uno::Sequence< css::uno::Any  >& args) throw (css::uno::RuntimeException);
 
         // XSessionManagerListener
         virtual void SAL_CALL doSave( sal_Bool bShutdown, sal_Bool bCancelable )
