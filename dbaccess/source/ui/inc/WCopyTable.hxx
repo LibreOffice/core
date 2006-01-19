296d802dee03550d5671316ce150ac9b2669061a
Oliver Bolte <obo@openoffice.org>
2006-01-19 14:42:49 +0000

    INTEGRATION: CWS dba202e (1.18.64); FILE MERGED 2006/01/03 09:43:11 oj 1.18.64.1: #i59833# exception caught

--- dbaccess/source/ui/inc/WCopyTable.hxx
+++ dbaccess/source/ui/inc/WCopyTable.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WCopyTable.hxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:42:23 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 15:42:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -195,6 +195,7 @@ namespace dbaui
         ::rtl::OUString                 m_sSourceName;
         ::rtl::OUString                 m_aKeyName;
         TOTypeInfoSP                    m_pTypeInfo; // default type
+        sal_Bool                        m_bAddPKFirstTime;
     public:
         enum Wizard_Create_Style
         {
