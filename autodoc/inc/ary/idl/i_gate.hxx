206fddda73a6dbebf98ee8e6b89830aad75f5465
Oliver Bolte <obo@openoffice.org>
2006-01-19 16:56:40 +0000

    INTEGRATION: CWS gcc41 (1.2.14); FILE MERGED 2005/12/07 12:31:04 pmladek 1.2.14.1: #i58967# Removed extra qualification to fix build with gcc-4.1

--- autodoc/inc/ary/idl/i_gate.hxx
+++ autodoc/inc/ary/idl/i_gate.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: i_gate.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:09:09 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 17:56:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ class Gate : public ::ary::CommonGate
 {
   public:
     // LIFECYCLE
-    virtual             Gate::~Gate() {}
+    virtual             ~Gate() {}
 
     // OPERATIONS
 
