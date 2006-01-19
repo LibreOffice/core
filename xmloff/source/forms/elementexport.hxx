2d9e3b797be4e0b32d55fdb8e694c08a5166348e
Oliver Bolte <obo@openoffice.org>
2006-01-19 17:08:06 +0000

    INTEGRATION: CWS gcc41 (1.14.58); FILE MERGED 2005/12/07 15:27:23 pmladek 1.14.58.1: #i58967# Removed extra qualification to fix build with gcc-4.1

--- xmloff/source/forms/elementexport.hxx
+++ xmloff/source/forms/elementexport.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: elementexport.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:05:46 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 18:08:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -267,7 +267,7 @@ namespace xmloff
 
         /** retrieves the string specifying the ListSource of a list or combo box
         */
-        ::rtl::OUString OControlExport::getScalarListSourceValue() const;
+        ::rtl::OUString getScalarListSourceValue() const;
 
         /** determines whether the list entries (of a combo or list box) are supplied by the user
 
