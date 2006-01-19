336ed719ad58e41487458b5330432c26997965ce
Oliver Bolte <obo@openoffice.org>
2006-01-19 17:23:01 +0000

    INTEGRATION: CWS gcc41 (1.26.36); FILE MERGED 2006/01/06 14:25:52 pmladek 1.26.36.2: RESYNC: (1.26-1.27); FILE MERGED 2005/12/07 13:05:07 pmladek 1.26.36.1: #i58967# Removed extra qualification to fix build with gcc-4.1

--- forms/source/component/DatabaseForm.hxx
+++ forms/source/component/DatabaseForm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DatabaseForm.hxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: obo $ $Date: 2005-12-21 13:20:36 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 18:23:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -468,7 +468,7 @@ public:
     static  ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames_Static();
     static  ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getCurrentServiceNames_Static();
     static  ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getCompatibleServiceNames_Static();
-    static  ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL ODatabaseForm::Create( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxFactory );
+    static  ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL Create( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxFactory );
 
     // com::sun::star::io::XPersistObject
     virtual ::rtl::OUString SAL_CALL getServiceName() throw(::com::sun::star::uno::RuntimeException);
