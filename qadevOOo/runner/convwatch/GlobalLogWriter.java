38d4cca0ae72b3ec873a12579798dc3a41e93e3b
Oliver Bolte <obo@openoffice.org>
2006-01-19 13:18:44 +0000

    INTEGRATION: CWS qadev25 (1.1.2); FILE ADDED 2005/11/15 13:58:16 lla 1.1.2.1: #127811# write into log instead System.out.

---
+++ qadevOOo/runner/convwatch/GlobalLogWriter.java
@@ -0,0 +1,60 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: GlobalLogWriter.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-01-19 14:18:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+package convwatch;
+
+import share.LogWriter;
+import stats.SimpleLogWriter;
+
+public class GlobalLogWriter
+{
+    private static LogWriter m_aGlobalLogWriter = null;
+    public static LogWriter get()
+        {
+            if (m_aGlobalLogWriter == null)
+            {
+                SimpleLogWriter aLog = new SimpleLogWriter();
+                m_aGlobalLogWriter = aLog;
+            }
+            return m_aGlobalLogWriter;
+        }
+
+    public static void set(LogWriter _aLog)
+        {
+            m_aGlobalLogWriter = _aLog;
+        }
+
+}
+
