4dae570356df2766b6513dac0ad3f06b2d2dac2d
Oliver Bolte <obo@openoffice.org>
2006-01-20 10:39:48 +0000

    INTEGRATION: CWS sch13 (1.8.64); FILE MERGED 2005/12/19 15:12:42 bm 1.8.64.1: #i59288# adapt axis properties for percent charts

--- xmloff/inc/XMLChartPropertySetMapper.hxx
+++ xmloff/inc/XMLChartPropertySetMapper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLChartPropertySetMapper.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:46:40 $
+ *  last change: $Author: obo $ $Date: 2006-01-20 11:39:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -85,6 +85,7 @@ class XMLChartExportPropertyMapper : public SvXMLExportPropertyMapper
 private:
     const rtl::OUString msTrue;
     const rtl::OUString msFalse;
+    bool mbAdaptPercentage;
 
     SvXMLExport& mrExport;
 
@@ -113,6 +114,8 @@ public:
     XMLChartExportPropertyMapper( const UniReference< XMLPropertySetMapper >& rMapper,
                                   SvXMLExport& rExport );
     virtual ~XMLChartExportPropertyMapper();
+
+    void setAdaptPercentage( bool bNewValue );
 };
 
 // ----------------------------------------
