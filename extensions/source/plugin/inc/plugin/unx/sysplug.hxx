aa532dce5ab68f6a59388ef71d23a8e8c9339b74
Oliver Bolte <obo@openoffice.org>
2006-01-20 11:58:48 +0000

    INTEGRATION: CWS vcl49 (1.3.60); FILE MERGED 2005/12/14 15:44:29 pl 1.3.60.1: #i56878# use mozilla headers instead of npsdk, use OJI definition

--- extensions/source/plugin/inc/plugin/unx/sysplug.hxx
+++ extensions/source/plugin/inc/plugin/unx/sysplug.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sysplug.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:55:00 $
+ *  last change: $Author: obo $ $Date: 2006-01-20 12:58:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ public:
     virtual NPError NPP_Destroy( NPP instance, NPSavedData** save );
     virtual NPError NPP_DestroyStream( NPP instance, NPStream* stream,
                                        NPError reason );
-    virtual jref NPP_GetJavaClass();
+    virtual void* NPP_GetJavaClass();
     virtual NPError NPP_Initialize();
     virtual NPError NPP_New( NPMIMEType pluginType, NPP instance,
                              uint16 mode, int16 argc,
