e8775efb20994d2b008b0d7400501b1433782405
Oliver Bolte <obo@openoffice.org>
2006-01-20 09:50:50 +0000

    INTEGRATION: CWS cmcfixes23 (1.20.92); FILE MERGED 2005/11/29 13:56:41 cmc 1.20.92.1: #i44027# enable building out of the box with ntpl

--- solenv/inc/unxlngppc4.mk
+++ solenv/inc/unxlngppc4.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxlngppc4.mk,v $
 #
-#   $Revision: 1.20 $
+#   $Revision: 1.21 $
 #
-#   last change: $Author: kz $ $Date: 2005-10-05 11:35:00 $
+#   last change: $Author: obo $ $Date: 2006-01-20 10:50:50 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@ JAVAFLAGSDEBUG=-g
 #LINKOUTPUT_FILTER=" |& $(SOLARENV)$/bin$/msg_filter"
 
 # _PTHREADS is needed for the stl
-CDEFS+=-DGLIBC=2 -D_PTHREADS -D_REENTRANT -DNEW_SOLAR -D_USE_NAMESPACE=1 -DSTLPORT_VERSION=400 -DPOWERPC -DPPC
+CDEFS+=$(PTHREAD_CFLAGS) -DGLIBC=2 -D_PTHREADS -D_REENTRANT -DNEW_SOLAR -D_USE_NAMESPACE=1 -DSTLPORT_VERSION=400 -DPOWERPC -DPPC
 
 # enable visibility define in "sal/types.h"
 .IF "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
