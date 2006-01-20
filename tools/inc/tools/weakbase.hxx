7be38151ee2a22eb7ee0a298fdf1d8af1aa8fc34
Oliver Bolte <obo@openoffice.org>
2006-01-20 08:17:53 +0000

    INTEGRATION: CWS impress79 (1.1.2); FILE ADDED 2006/01/17 14:28:17 cl 1.1.2.4: fixed not yet used operator== 2006/01/06 10:08:08 cl 1.1.2.3: #124697# fixed getWeakConnection() 2005/12/29 09:58:14 cl 1.1.2.2: #124697# new helper for weak references to non recounted objects 2005/12/19 12:18:21 cl 1.1.2.1: #124697# new helper for weak references to non recounted objects

---
+++ tools/inc/tools/weakbase.hxx
@@ -0,0 +1,145 @@
+#ifndef _TOOLS_WEAKBASE_HXX_
+#define _TOOLS_WEAKBASE_HXX_
+
+#ifndef _TOOLS_WEAKBASE_H_
+#include <tools/weakbase.h>
+#endif
+
+/// see weakbase.h for documentation
+
+namespace tools
+{
+
+template< class reference_type >
+inline WeakReference< reference_type >::WeakReference()
+{
+    mpWeakConnection = new WeakConnection<reference_type>( 0 );
+    mpWeakConnection->acquire();
+}
+
+template< class reference_type >
+inline WeakReference< reference_type >::WeakReference( reference_type* pReference )
+{
+    if( pReference )
+        mpWeakConnection = pReference->getWeakConnection();
+    else
+        mpWeakConnection = new WeakConnection<reference_type>( 0 );
+
+    mpWeakConnection->acquire();
+}
+
+template< class reference_type >
+inline WeakReference< reference_type >::WeakReference( const WeakReference< reference_type >& rWeakRef )
+{
+    mpWeakConnection = rWeakRef.mpWeakConnection;
+    mpWeakConnection->acquire();
+}
+
+template< class reference_type >
+inline WeakReference< reference_type >::~WeakReference()
+{
+    mpWeakConnection->release();
+}
+
+template< class reference_type >
+inline bool WeakReference< reference_type >::is() const
+{
+    return mpWeakConnection->mpReference != 0;
+}
+
+template< class reference_type >
+inline reference_type * WeakReference< reference_type >::get() const
+{
+    return mpWeakConnection->mpReference;
+}
+
+template< class reference_type >
+inline void WeakReference< reference_type >::reset( reference_type* pReference )
+{
+    mpWeakConnection->release();
+
+    if( pReference )
+        mpWeakConnection = pReference->getWeakConnection();
+    else
+        mpWeakConnection = new WeakConnection<reference_type>( 0 );
+
+    mpWeakConnection->acquire();
+}
+
+template< class reference_type >
+inline reference_type * WeakReference< reference_type >::operator->() const
+{
+    OSL_PRECOND(m_pBody, "tools::WeakReference::operator->() : null body");
+    return mpWeakConnection->mpReference;
+}
+
+template< class reference_type >
+inline sal_Bool WeakReference< reference_type >::operator==(const reference_type * pReferenceObject) const
+{
+    OSL_PRECOND(m_pBody, "tools::WeakReference::operator==() : null body");
+    return mpWeakConnection->mpReference == pReferenceObject;
+}
+
+template< class reference_type >
+inline sal_Bool WeakReference< reference_type >::operator==(const WeakReference<reference_type> & handle) const
+{
+    return mpWeakConnection == handle.mpWeakConnection;
+}
+
+template< class reference_type >
+inline sal_Bool WeakReference< reference_type >::operator!=(const WeakReference<reference_type> & handle) const
+{
+    return mpWeakConnection != handle.mpWeakConnection;
+}
+
+template< class reference_type >
+inline sal_Bool WeakReference< reference_type >::operator<(const WeakReference<reference_type> & handle) const
+{
+    return mpWeakConnection->mpReference < handle.mpWeakConnection->mpReference;
+}
+
+template< class reference_type >
+inline sal_Bool WeakReference< reference_type >::operator>(const WeakReference<reference_type> & handle) const
+{
+    return mpWeakConnection->mpReference > handle.mpWeakConnection->mpReference;
+}
+
+template< class reference_type >
+inline WeakBase< reference_type >::WeakBase()
+{
+    mpWeakConnection = 0;
+}
+
+template< class reference_type >
+inline WeakBase< reference_type >::~WeakBase()
+{
+    if( mpWeakConnection )
+    {
+        mpWeakConnection->mpReference = 0;
+        mpWeakConnection->release();
+        mpWeakConnection = 0;
+    }
+}
+
+template< class reference_type >
+inline void WeakBase< reference_type >::clearWeak()
+{
+    if( mpWeakConnection )
+        mpWeakConnection->mpReference = 0;
+}
+
+template< class reference_type >
+inline WeakConnection< reference_type >* WeakBase< reference_type >::getWeakConnection()
+{
+    if( !mpWeakConnection )
+    {
+        mpWeakConnection = new WeakConnection< reference_type >( static_cast< reference_type* >( this ) );
+        mpWeakConnection->acquire();
+    }
+    return mpWeakConnection;
+}
+
+}
+
+#endif // _TOOLS_WEAKBASE_HXX_
+
