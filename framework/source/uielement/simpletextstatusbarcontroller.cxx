0cf7c65b5b41c5440c7c0d0ff5a3af7719cff673
Jens-Heiner Rechtien <hr@openoffice.org>
2006-01-24 14:12:56 +0000

    INTEGRATION: CWS c08v001 (1.1.2); FILE ADDED 2006/01/05 12:11:51 cd 1.1.2.1: #129365# Added simple text statusbar controller

---
+++ framework/source/uielement/simpletextstatusbarcontroller.cxx
@@ -0,0 +1,224 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: simpletextstatusbarcontroller.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-01-24 15:12:56 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_UIELEMENT_SIMPLETEXTSTATUSBARCONTROLLER_HXX_
+#include <uielement/simpletextstatusbarcontroller.hxx>
+#endif
+
+#ifndef __FRAMEWORK_CLASSES_FWKRESID_HXX_
+#include <classes/fwkresid.hxx>
+#endif
+#ifndef __FRAMEWORK_SERVICES_H_
+#include <services.h>
+#endif
+#include <classes/resource.hrc>
+
+#ifndef _VOS_MUTEX_HXX_
+#include <vos/mutex.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+#ifndef _SV_WINDOW_HXX
+#include <vcl/window.hxx>
+#endif
+#ifndef _SV_STATUS_HXX
+#include <vcl/status.hxx>
+#endif
+#ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
+#include <toolkit/unohlp.hxx>
+#endif
+#ifndef _TOOLKIT_HELPER_CONVERT_HXX_
+#include <toolkit/helper/convert.hxx>
+#endif
+
+using namespace ::rtl;
+using namespace ::cppu;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::frame;
+
+namespace framework
+{
+
+DEFINE_XSERVICEINFO_MULTISERVICE        (   SimpleTextStatusbarController           ,
+                                            OWeakObject                             ,
+                                            SERVICENAME_STATUSBARCONTROLLER         ,
+                                            IMPLEMENTATIONNAME_SIMPLETEXTSTATUSBARCONTROLLER
+                                        )
+
+DEFINE_INIT_SERVICE                     (   SimpleTextStatusbarController, {} )
+
+SimpleTextStatusbarController::SimpleTextStatusbarController()
+{
+}
+
+SimpleTextStatusbarController::SimpleTextStatusbarController( const uno::Reference< lang::XMultiServiceFactory >& xServiceManager ) :
+    svt::StatusbarController( xServiceManager, uno::Reference< frame::XFrame >(), rtl::OUString(), 0 )
+{
+}
+
+SimpleTextStatusbarController::~SimpleTextStatusbarController()
+{
+}
+
+// XInterface
+Any SAL_CALL SimpleTextStatusbarController::queryInterface( const Type& rType )
+throw ( RuntimeException )
+{
+    return svt::StatusbarController::queryInterface( rType );
+}
+
+void SAL_CALL SimpleTextStatusbarController::acquire() throw ()
+{
+    svt::StatusbarController::acquire();
+}
+
+void SAL_CALL SimpleTextStatusbarController::release() throw ()
+{
+    svt::StatusbarController::release();
+}
+
+void SAL_CALL SimpleTextStatusbarController::initialize( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments )
+throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException)
+{
+    const rtl::OUString aPropValueName( RTL_CONSTASCII_USTRINGPARAM( "Value" ));
+
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+    svt::StatusbarController::initialize( aArguments );
+
+    rtl::OUString        aValue;
+    beans::PropertyValue aPropValue;
+
+    // Check arguments for optional "Value" property. We need it
+    // to set our internal simple text.
+    for ( int i = 0; i < aArguments.getLength(); i++ )
+    {
+        if (( aArguments[i] >>= aPropValue ) && ( aPropValue.Name.equals( aPropValueName )))
+        {
+            aPropValue.Value >>= aValue;
+            break;
+        }
+    }
+
+    m_aText = aValue;
+    if ( m_xParentWindow.is() && m_nID > 0 )
+    {
+        Window* pWindow = VCLUnoHelper::GetWindow( m_xParentWindow );
+        if ( pWindow && ( pWindow->GetType() == WINDOW_STATUSBAR ))
+        {
+            StatusBar* pStatusBar = (StatusBar *)pWindow;
+            pStatusBar->SetItemText( m_nID, m_aText );
+        }
+    }
+}
+
+// XComponent
+void SAL_CALL SimpleTextStatusbarController::dispose()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    svt::StatusbarController::dispose();
+}
+
+// XEventListener
+void SAL_CALL SimpleTextStatusbarController::disposing( const EventObject& Source )
+throw ( RuntimeException )
+{
+    svt::StatusbarController::disposing( Source );
+}
+
+// XStatusListener
+void SAL_CALL SimpleTextStatusbarController::statusChanged( const FeatureStateEvent& Event )
+throw ( RuntimeException )
+{
+}
+
+// XStatusbarController
+::sal_Bool SAL_CALL SimpleTextStatusbarController::mouseButtonDown(
+    const ::com::sun::star::awt::MouseEvent& aMouseEvent )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    return sal_False;
+}
+
+::sal_Bool SAL_CALL SimpleTextStatusbarController::mouseMove(
+    const ::com::sun::star::awt::MouseEvent& aMouseEvent )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    return sal_False;
+}
+
+::sal_Bool SAL_CALL SimpleTextStatusbarController::mouseButtonUp(
+    const ::com::sun::star::awt::MouseEvent& aMouseEvent )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    return sal_False;
+}
+
+void SAL_CALL SimpleTextStatusbarController::command(
+    const ::com::sun::star::awt::Point& aPos,
+    ::sal_Int32 nCommand,
+    ::sal_Bool bMouseEvent,
+    const ::com::sun::star::uno::Any& aData )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    svt::StatusbarController::command( aPos, nCommand, bMouseEvent, aData );
+}
+
+void SAL_CALL SimpleTextStatusbarController::paint(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XGraphics >& xGraphics,
+    const ::com::sun::star::awt::Rectangle& rOutputRectangle,
+    ::sal_Int32 nItemId,
+    ::sal_Int32 nStyle )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    svt::StatusbarController::paint( xGraphics, rOutputRectangle, nItemId, nStyle );
+}
+
+void SAL_CALL SimpleTextStatusbarController::click()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    svt::StatusbarController::click();
+}
+
+void SAL_CALL SimpleTextStatusbarController::doubleClick() throw (::com::sun::star::uno::RuntimeException)
+{
+    svt::StatusbarController::doubleClick();
+}
+
+}
