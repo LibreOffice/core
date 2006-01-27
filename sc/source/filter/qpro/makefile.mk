3d96a8f4bed9c9fd2c0421c1c0667635207c8a51
Jens-Heiner Rechtien <hr@openoffice.org>
2006-01-27 14:51:00 +0000

    INTEGRATION: CWS qpro03 (1.1.4); FILE ADDED 2005/09/21 13:23:43 er 1.1.4.5: change license header: remove SISSL 2005/09/01 12:19:16 er 1.1.4.4: broken header 2005/05/18 07:56:37 mmeeks 1.1.4.3: fix broken entry 2005/05/13 16:47:49 mmeeks 1.1.4.2: Issue number: i#41688# Submitted by: shilpa Reviewed by:  mmeeks
    More qpro cleanup.
    2005/04/14 08:09:16 mnicel 1.1.4.1: Issue Number: 41688
    QPro Filter implementation

---
+++ sc/source/filter/qpro/makefile.mk
@@ -0,0 +1,72 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: hr $ $Date: 2006-01-27 15:51:00 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PRJNAME=sc
+TARGET=qpro
+
+PROJECTPCH4DLL=TRUE
+PROJECTPCH=filt_pch
+PROJECTPCHSOURCE=..\pch\filt_pch
+
+AUTOSEG=true
+
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  scpre.mk
+.INCLUDE :  settings.mk
+.INCLUDE :  sc.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+# --- Files --------------------------------------------------------
+
+CXXFILES = \
+            qpro.cxx        \
+            qproform.cxx \
+            qprostyle.cxx \
+            biff.cxx
+
+SLOFILES = \
+            $(SLO)$/qpro.obj        \
+            $(SLO)$/qproform.obj    \
+            $(SLO)$/qprostyle.obj \
+            $(SLO)$/biff.obj 
+
+# --- Tagets -------------------------------------------------------
+
+.INCLUDE :  target.mk
+
