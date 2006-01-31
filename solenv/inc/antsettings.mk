9056cb0d8fade6be1606ab41ce0475b752d2e6cc
Kurt Zenker <kz@openoffice.org>
2006-01-31 17:48:27 +0000

    INTEGRATION: CWS dba202d (1.1.2); FILE ADDED 2005/12/13 15:49:46 oj 1.1.2.1: #i57406# ant.mk patched by ause. thx

---
+++ solenv/inc/antsettings.mk
@@ -0,0 +1,92 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: antsettings.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2006-01-31 18:48:27 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+
+.IF "$(SOLAR_JAVA)"!=""
+
+.IF "$(ANT_HOME)" == ""
+ANT_HOME*:=$(COMMON_BUILD_TOOLS)$/apache-ant-1.6.1
+.EXPORT : ANT_HOME
+.ENDIF
+ANT_LIB*:=$(ANT_HOME)$/lib
+
+ANT_CLASSPATH:=$(ANT_LIB)$/xercesImpl.jar$(PATH_SEPERATOR)$(ANT_LIB)$/xml-apis.jar$(PATH_SEPERATOR)$(ANT_LIB)$/ant.jar
+PATH!:=$(ANT_HOME)$/bin$(PATH_SEPERATOR)$(PATH)
+
+.IF "$(ANT)" == ""
+ANT*:=$(ANT_HOME)$/bin$/ant
+.ENDIF
+
+.IF "$(ANT_BUILDFILE)"==""
+ANT_BUILDFILE=build.xml
+.ENDIF
+
+.IF "$(ANT_DEBUG)"==""
+.IF "$(debug)"==""
+ANT_DEBUG=off
+.ELSE
+ANT_DEBUG=on
+.ENDIF
+.ENDIF
+
+.IF "$(ANT_OPT)"==""
+.IF "$(optimize)"==""
+ANT_OPT=off
+.ELSE
+ANT_OPT=on
+.ENDIF
+.ENDIF
+
+.IF "$(JDK)"=="gcj"
+JAVA_HOME=
+.EXPORT : JAVA_HOME
+ANT_FLAGS!:=-Dbuild.compiler=gcj -Dprj=$(PRJ) -Dprjname=$(PRJNAME) -Ddebug=$(ANT_DEBUG) \
+ -Doptimize=$(ANT_OPT) -Dtarget=$(TARGET) -Dsolar.update=on -Dout=$(OUT) -Dinpath=$(INPATH) \
+ -Dproext="$(PROEXT)" -Dsolar.bin=$(SOLARBINDIR) -Dsolar.jar=$(SOLARBINDIR) \
+ -Dsolar.doc=$(SOLARDOCDIR) -Dcommon.jar=$(SOLARCOMMONBINDIR) \
+ -Dcommon.doc=$(SOLARCOMMONDOCDIR) -f $(ANT_BUILDFILE) $(ANT_FLAGS) -emacs
+.ELSE
+ANT_FLAGS!:=-Dprj=$(PRJ) -Dprjname=$(PRJNAME) -Ddebug=$(ANT_DEBUG) -Doptimize=$(ANT_OPT) \
+ -Dtarget=$(TARGET) -Dsolar.update=on -Dout=$(OUT) -Dinpath=$(INPATH) -Dproext="$(PROEXT)" \
+ -Dsolar.bin=$(SOLARBINDIR) -Dsolar.jar=$(SOLARBINDIR) -Dsolar.doc=$(SOLARDOCDIR) \
+ -Dcommon.jar=$(SOLARCOMMONBINDIR) -Dcommon.doc=$(SOLARCOMMONDOCDIR) \
+ -f $(ANT_BUILDFILE) $(ANT_FLAGS) -emacs
+.ENDIF
+.ELSE # No java
+ANT=
+ANT_FLAGS=
+.ENDIF
+ 
+ 
