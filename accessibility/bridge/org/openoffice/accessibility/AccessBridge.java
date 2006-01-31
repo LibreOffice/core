b098e94c1b5effa2e943c9eef4fc59ee80e07ddf
Kurt Zenker <kz@openoffice.org>
2006-01-31 17:21:53 +0000

    INTEGRATION: CWS accessalert (1.19.18); FILE MERGED 2006/01/18 10:15:09 obr 1.19.18.1: #i26277# changed role of [MESS|INFO|WARNING|ERROR|QUERY]BOX to ALERT

--- accessibility/bridge/org/openoffice/accessibility/AccessBridge.java
+++ accessibility/bridge/org/openoffice/accessibility/AccessBridge.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessBridge.java,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 15:28:14 $
+ *  last change: $Author: kz $ $Date: 2006-01-31 18:21:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -97,6 +97,7 @@ public class AccessBridge {
                     case AccessibleRole.WINDOW:
                     case AccessibleRole.FRAME:
                     case AccessibleRole.DIALOG:
+                    case AccessibleRole.ALERT:
                         return getTopWindowImpl(xAccessible);
 
                     default:
