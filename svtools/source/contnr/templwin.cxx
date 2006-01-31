8678840230abdf7c4f40e84b091ccac481e0d4ca
Kurt Zenker <kz@openoffice.org>
2006-01-31 17:47:22 +0000

    INTEGRATION: CWS dba202d (1.68.38); FILE MERGED 2006/01/24 09:57:50 fs 1.68.38.2: RESYNC: (1.68-1.69); FILE MERGED 2006/01/23 12:34:27 oj 1.68.38.1: merge from pb

--- svtools/source/contnr/templwin.cxx
+++ svtools/source/contnr/templwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: templwin.cxx,v $
  *
- *  $Revision: 1.69 $
+ *  $Revision: 1.70 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-05 18:13:11 $
+ *  last change: $Author: kz $ $Date: 2006-01-31 18:47:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -424,6 +424,7 @@ SvtIconWindow_Impl::SvtIconWindow_Impl( Window* pParent ) :
 {
     aDummyHeaderBar.Show();
 
+    aIconCtrl.SetAccessibleName( String( RTL_CONSTASCII_USTRINGPARAM("Groups") ) );
       aIconCtrl.SetHelpId( HID_TEMPLATEDLG_ICONCTRL );
     aIconCtrl.SetChoiceWithCursor( TRUE );
     aIconCtrl.SetSelectionMode( SINGLE_SELECTION );
