b0573befc6c0f163a28c5ec0021414bf1d0ddf09
Kurt Zenker <kz@openoffice.org>
2006-01-31 17:17:03 +0000

    INTEGRATION: CWS pj49 (1.8.2); FILE MERGED 2006/01/27 18:51:12 pjanik 1.8.2.1: #i61188#: Include system neon really properly.

--- ucb/source/ucp/webdav/LockSequence.cxx
+++ ucb/source/ucp/webdav/LockSequence.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LockSequence.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-20 14:19:52 $
+ *  last change: $Author: kz $ $Date: 2006-01-31 18:17:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,11 +36,7 @@
 #include <string.h>
 
 #ifndef NE_XML_H
-#ifdef SYSTEM_NEON
 #include <ne_xml.h>
-#else
-#include <neon/ne_xml.h>
-#endif
 #endif
 
 #ifndef _LOCKSEQUENCE_HXX_
