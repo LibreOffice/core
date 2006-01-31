5d39e449f8a9f72a3eccfba1e21700a9dcceee0d
Kurt Zenker <kz@openoffice.org>
2006-01-31 17:43:42 +0000

    INTEGRATION: CWS i18n24 (1.1.2); FILE ADDED 2006/01/10 23:09:08 khong 1.1.2.1: #i60331 extract index data to external library

---
+++ i18npool/source/indexentry/data/makefile.mk
@@ -0,0 +1,71 @@
+#*************************************************************************
+#*
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2006-01-31 18:43:42 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#************************************************************************
+PRJ=..$/..$/..
+
+PRJNAME=i18npool
+TARGET=index_data
+LIBTARGET=NO
+
+# --- Settings -----------------------------------------------------
+.INCLUDE :  settings.mk
+
+# Fix heap limit problem on MSC
+.IF "$(OS)" == "WNT"
+.IF  "$(COMEX)" != "8"
+CDEFS+=-Zm300
+.ENDIF
+.ENDIF
+
+# --- Files --------------------------------------------------------
+
+# index data library
+SHL1TARGET=$(TARGET)
+SHL1IMPLIB=i$(SHL1TARGET)
+
+SHL1VERSIONMAP=$(SHL1TARGET).map
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+
+DATA_TXTFILES:=$(shell ls ./*.txt)
+SHL1OBJS=$(subst,./,$(SLO)$/ $(DATA_TXTFILES:s/.txt/.obj/))
+
+LIB1TARGET=	$(SLB)$/$(SHL1TARGET).lib
+LIB1OBJFILES=$(SHL1OBJS)
+
+# --- Targets ------------------------------------------------------
+.INCLUDE :  target.mk
+
+$(MISC)$/%.cxx : %.txt $(BIN)$/genindex_data
+    +$(BIN)$/genindex_data $< $@ $*
