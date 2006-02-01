0793dccac6602c76cd297911a505f9173e71e017
Kurt Zenker <kz@openoffice.org>
2006-02-01 17:40:36 +0000

    INTEGRATION: CWS mav19 (1.22.34); FILE MERGED 2005/11/17 15:31:45 mav 1.22.34.2: RESYNC: (1.22-1.24); FILE MERGED 2005/11/10 13:57:26 mav 1.22.34.1: #i50429# allow to copy nonpersistent objects

--- sd/source/ui/view/makefile.mk
+++ sd/source/ui/view/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.24 $
+#   $Revision: 1.25 $
 #
-#   last change: $Author: rt $ $Date: 2005-11-08 16:31:09 $
+#   last change: $Author: kz $ $Date: 2006-02-01 18:40:36 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ EXCEPTIONSFILES=				\
         $(SLO)$/outlnvsh.obj \
         $(SLO)$/presvish.obj \
         $(SLO)$/sdview.obj	\
+        $(SLO)$/sdview2.obj	\
         $(SLO)$/sdview3.obj  \
         $(SLO)$/sdview4.obj  \
         $(SLO)$/slidvish.obj \
