53948a432d9b85ac131c222ff9ba81413db66c2c
Kurt Zenker <kz@openoffice.org>
2006-02-01 18:15:18 +0000

    INTEGRATION: CWS mav19 (1.4.18); FILE MERGED 2006/01/03 13:15:59 mav 1.4.18.1: #i59147# use central helper

--- package/source/zippackage/zipfileaccess.cxx
+++ package/source/zippackage/zipfileaccess.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: zipfileaccess.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:21:19 $
+ *  last change: $Author: kz $ $Date: 2006-02-01 19:15:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,8 +63,6 @@
 
 
 using namespace ::com::sun::star;
-
-extern void copyInputToOutput_Impl( uno::Reference< io::XInputStream >& aIn, uno::Reference< io::XOutputStream >& aOut );
 
 // ----------------------------------------------------------------
 OZipFileAccess::OZipFileAccess( const uno::Reference< lang::XMultiServiceFactory >& xFactory )
