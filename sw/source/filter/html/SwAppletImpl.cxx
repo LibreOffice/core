545716c0c8632b0e1fe919b9958d07ebafab92dd
Kurt Zenker <kz@openoffice.org>
2006-02-01 17:49:57 +0000

    INTEGRATION: CWS mav19 (1.11.76); FILE MERGED 2006/01/26 18:05:58 mav 1.11.76.1: #130736# switch floating frames, applets and plugins to running state when necessary

--- sw/source/filter/html/SwAppletImpl.cxx
+++ sw/source/filter/html/SwAppletImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwAppletImpl.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:40:03 $
+ *  last change: $Author: kz $ $Date: 2006-02-01 18:49:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -155,6 +155,7 @@ void SwApplet_Impl::CreateApplet( const String& rCode, const String& rName,
 
     // create Applet; it will be in running state
     xApplet = aCnt.CreateEmbeddedObject( SvGlobalName( SO3_APPLET_CLASSID ).GetByteSequence(), aName );
+    ::svt::EmbeddedObjectRef::TryRunningState( xApplet );
 
     INetURLObject aUrlBase(rDocumentBaseURL);
     aUrlBase.removeSegment();
