18906f753c1604aae11358d3c3537cf80bb5bbc9
Kurt Zenker <kz@openoffice.org>
2006-02-03 16:33:43 +0000

    INTEGRATION: CWS kabrefine (1.2.10); FILE MERGED 2006/01/05 09:49:50 ebischoff 1.2.10.1: Issue number: 59900 Submitted by: mkretzschmar Reviewed by: thb Not making assumptions whether internal or external STLP

--- connectivity/source/drivers/kab/korder.hxx
+++ connectivity/source/drivers/kab/korder.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: korder.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2005-12-19 16:53:56 $
+ *  last change: $Author: kz $ $Date: 2006-02-03 17:33:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,10 +39,8 @@
 #ifndef _RTL_USTRING_HXX_
 #include "rtl/ustring.hxx"
 #endif
-#ifndef _STLP_VECTOR
-#include "stl/vector"
-#endif
 
+#include <vector>
 #include <kabc/addressee.h>
 
 namespace connectivity
