979291db081f6c9c00d59f074c3caf5c5765a9f1
Rüdiger Timm <rt@openoffice.org>
2006-02-06 15:43:34 +0000

    INTEGRATION: CWS dba202f (1.1.2); FILE ADDED 2006/01/23 16:15:44 fs 1.1.2.2: make JAVAFILES find new files automatically 2006/01/23 12:17:00 fs 1.1.2.1: makefile

---
+++ connectivity/qa/connectivity/tools/makefile.mk
@@ -0,0 +1,67 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2006-02-06 16:43:34 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..
+TARGET  = ConnectivityTools
+PRJNAME = $(TARGET)
+PACKAGE = connectivity$/tools
+
+# --- Settings -----------------------------------------------------
+.INCLUDE: settings.mk
+
+
+#----- compile .java files -----------------------------------------
+
+JARFILES        = ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar
+JAVAFILES       = $(shell $(FIND) .$/*.java)
+JAVACLASSFILES	= $(foreach,i,$(JAVAFILES) $(CLASSDIR)$/$(PACKAGE)$/$(i:b).class)
+
+#----- make a jar from compiled files ------------------------------
+
+MAXLINELENGTH = 100000
+
+JARCLASSDIRS    = $(PACKAGE)
+JARTARGET       = $(TARGET).jar
+JARCOMPRESS 	= TRUE
+
+# --- Targets ------------------------------------------------------
+
+.IF "$(depend)" == ""
+ALL :   ALLTAR
+.ELSE
+ALL: 	ALLDEP
+.ENDIF
+
+.INCLUDE :  target.mk
