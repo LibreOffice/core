34935f8eceb11ee95ca91440516f629ebbd6fd6f
Rüdiger Timm <rt@openoffice.org>
2006-02-06 15:31:18 +0000

    INTEGRATION: CWS swqbf53 (1.42.4); FILE MERGED 2006/02/01 14:36:56 od 1.42.4.1: #130797# - correct fix for issue 126020

--- sw/source/core/layout/sectfrm.cxx
+++ sw/source/core/layout/sectfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sectfrm.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 14:25:19 $
+ *  last change: $Author: rt $ $Date: 2006-02-06 16:31:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1419,7 +1419,9 @@ void SwSectionFrm::Format( const SwBorderAttrs *pAttr )
                 {
                     FormatWidthCols( *pAttr, nRemaining, MINLAY );
                     // --> OD 2006-01-04 #126020# - adjust check for empty section
-                    while( HasFollow() && !GetFollow()->ContainsAny() )
+                    // --> OD 2006-02-01 #130797# - correct fix #126020#
+                    while( HasFollow() && !GetFollow()->ContainsCntnt() &&
+                           !GetFollow()->ContainsAny( true ) )
                     // <--
                     {
                         SwFrm* pOld = GetFollow();
