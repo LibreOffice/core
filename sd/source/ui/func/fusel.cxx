0a9145f06c489b7a92d03ebac8f4148233401439
Rüdiger Timm <rt@openoffice.org>
2006-02-07 09:13:31 +0000

    INTEGRATION: CWS fwk31 (1.35.8); FILE MERGED 2006/01/26 16:39:19 mav 1.35.8.2: RESYNC: (1.35-1.36); FILE MERGED 2006/01/13 08:30:53 cd 1.35.8.1: #126086# Ignore mouse button down, if context menu is open and an inplace object is active. Otherwise we would crash as the context menu execute remains on the stack and later calls on dead objects

--- sd/source/ui/func/fusel.cxx
+++ sd/source/ui/func/fusel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fusel.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 14:30:32 $
+ *  last change: $Author: rt $ $Date: 2006-02-07 10:13:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
