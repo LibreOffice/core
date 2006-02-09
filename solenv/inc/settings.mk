cd19027985434361437eb72ac547ea3b14611c99
Rüdiger Timm <rt@openoffice.org>
2006-02-09 13:21:41 +0000

    INTEGRATION: CWS iconswitching2 (1.183.8); FILE MERGED 2006/01/16 18:20:53 kendy 1.183.8.1: #i60434# Make use of Crystal icons.

--- solenv/inc/settings.mk
+++ solenv/inc/settings.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: settings.mk,v $
 #
-#   $Revision: 1.185 $
+#   $Revision: 1.186 $
 #
-#   last change: $Author: obo $ $Date: 2006-01-20 12:09:20 $
+#   last change: $Author: rt $ $Date: 2006-02-09 14:21:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -1087,7 +1087,7 @@ RSCDEFS+= -DDEBUG
 .ENDIF
 
 # additional image sets
-CUSTOM_IMAGE_SETS=hicontrast industrial
+CUSTOM_IMAGE_SETS=hicontrast industrial crystal
 
 UNOIDL=unoidl
 
