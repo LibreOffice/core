b792249fa4c5b239fd9797979e5c419c7d9c7bb8
Rüdiger Timm <rt@openoffice.org>
2006-02-09 12:49:51 +0000

    INTEGRATION: CWS fwk32 (1.11.6); FILE MERGED 2006/01/30 10:58:34 mav 1.11.6.1: #120410# let the document be locked from closing while the control is running, use DocumentCloser to close the frame

--- extensions/source/activex/main/makefile.mk
+++ extensions/source/activex/main/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2006-01-10 15:58:16 $
+#   last change: $Author: rt $ $Date: 2006-02-09 13:49:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -68,6 +68,7 @@ SLOFILES=\
     $(SLO)$/SOActiveX.obj \
     $(SLO)$/SOComWindowPeer.obj \
     $(SLO)$/SODispatchInterceptor.obj \
+    $(SLO)$/SOActionsApproval.obj \
     $(SLO)$/StdAfx2.obj
 
 SHL1TARGET=$(TARGET)
