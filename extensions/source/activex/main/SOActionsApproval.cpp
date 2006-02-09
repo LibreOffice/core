c1b9ea53cdd5a7c23fe6d23bc8608a4c0fc36cda
Rüdiger Timm <rt@openoffice.org>
2006-02-09 12:48:46 +0000

    INTEGRATION: CWS fwk32 (1.1.2); FILE ADDED 2006/01/30 11:00:15 mav 1.1.2.1: #120410# let the document be locked from closing while the control is running, use DocumentCloser to close the frame

---
+++ extensions/source/activex/main/SOActionsApproval.cpp
@@ -0,0 +1,28 @@
+// SOActionsApproval.cpp : Implementation of CHelpApp and DLL registration.
+
+#include "stdafx2.h"
+#include "so_activex.h"
+#include "SOActionsApproval.h"
+
+/////////////////////////////////////////////////////////////////////////////
+//
+
+STDMETHODIMP SOActionsApproval::InterfaceSupportsErrorInfo(REFIID riid)
+{
+    static const IID* arr[] =
+    {
+        &IID_ISOActionsApproval,
+    };
+
+    for (int i=0;i<sizeof(arr)/sizeof(arr[0]);i++)
+    {
+#if defined(_MSC_VER) && (_MSC_VER >= 1300)
+        if (InlineIsEqualGUID(*arr[i],riid))
+#else
+        if (::ATL::InlineIsEqualGUID(*arr[i],riid))
+#endif
+            return S_OK;
+    }
+    return S_FALSE;
+}
+
