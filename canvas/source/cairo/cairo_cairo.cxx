f22c4fc218112c01971a2687b6a5631638d973ea
Kurt Zenker <kz@openoffice.org>
2006-02-28 09:32:45 +0000

    INTEGRATION: CWS cairocanvas (1.1.2); FILE ADDED 2006/01/23 17:52:05 radekdoulik 1.1.2.2: Issue number:  #51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik fixes problem with 0x0 sized canvases (https://bugzilla.novell.com/show_bug.cgi?id=114020) 2005/12/19 22:28:28 radekdoulik 1.1.2.1: Issue number:  #51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik added forgotten file

---
+++ canvas/source/cairo/cairo_cairo.cxx
@@ -0,0 +1,77 @@
+#include <X11/extensions/Xrender.h>
+#include <X11/Xlib.h>
+#include "cairo_cairo.hxx"
+#include "cairo_helper.hxx"
+
+namespace cairo
+{
+
+#include <cairo/cairo-xlib.h>
+#include <cairo/cairo-xlib-xrender.h>
+
+    Surface::Surface( const void* pSysData, int x, int y, int width, int height )
+        : mnRefCount( 1 ),
+          mpSysData( pSysData ),
+          mbFreePixmap( false )
+    {
+        mpSurface = (cairo_surface_t*) cairoHelperGetSurface( pSysData, x, y, width, height );
+        mpDisplay = (Display*) cairoHelperGetDisplay( pSysData );
+        mhDrawable = cairoHelperGetWindow( pSysData );
+    }
+
+    Surface::Surface( const void* pSysData, void *pBmpData, int width, int height )
+        : mnRefCount( 1 ),
+          mpSysData( pSysData ),
+          mbFreePixmap( false )
+    {
+        mpSurface = (cairo_surface_t*) cairoHelperGetSurface( pSysData, pBmpData, width, height );
+        mpDisplay = (Display*) cairoHelperGetDisplay( pSysData );
+        mhDrawable = cairoHelperGetWindow( pSysData );
+    }
+
+
+    Surface::~Surface()
+    {
+        if( mpSurface )
+        {
+            cairo_surface_destroy( mpSurface );
+            mpSurface = NULL;
+        }
+        if( mbFreePixmap && mhDrawable )
+            XFreePixmap( (Display*) mpDisplay, mhDrawable );
+    }
+
+    Surface* Surface::getSimilar( Content aContent, int width, int height )
+    {
+        Pixmap hPixmap;
+
+        if( mpSysData && mpDisplay && mhDrawable ) {
+            XRenderPictFormat *pFormat;
+            int nFormat;
+
+            switch (aContent) {
+            case CAIRO_CONTENT_ALPHA:
+                nFormat = PictStandardA8;
+                break;
+            case CAIRO_CONTENT_COLOR:
+                nFormat = PictStandardRGB24;
+                break;
+            case CAIRO_CONTENT_COLOR_ALPHA:
+            default:
+                nFormat = PictStandardARGB32;
+                break;
+            }
+
+            pFormat = XRenderFindStandardFormat( (Display*) mpDisplay, nFormat );
+            hPixmap = XCreatePixmap( (Display*) mpDisplay, cairoHelperGetWindow( mpSysData ),
+                                     width > 0 ? width : 1, height > 0 ? height : 1,
+                                     pFormat->depth );
+
+            return new Surface( mpSysData, mpDisplay, (long) hPixmap, pFormat,
+                                cairo_xlib_surface_create_with_xrender_format( (Display*) mpDisplay, hPixmap,
+                                                                               DefaultScreenOfDisplay( (Display *) mpDisplay ),
+                                                                               pFormat, width, height ) );
+        } else
+            return new Surface( mpSysData, mpDisplay, 0, NULL, cairo_surface_create_similar( mpSurface, aContent, width, height ) );
+    }
+}
