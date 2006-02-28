7c000c531b5e6177e3b4e3366fe29ff1870e83f3
Kurt Zenker <kz@openoffice.org>
2006-02-28 09:35:17 +0000

    INTEGRATION: CWS cairocanvas (1.1.2); FILE ADDED 2005/09/14 10:50:58 radekdoulik 1.1.2.2: Issue number: #51657 Submitted by: radekdoulik Reviewed by: radekdoulik Sync code in ooo-build with cairo canvas cws 2005/07/07 07:57:58 radekdoulik 1.1.2.1: Issue number:  51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik initial import of cairo canvas code

---
+++ canvas/source/cairo/cairo_canvashelper_texturefill.cxx
@@ -0,0 +1,215 @@
+/*************************************************************************
+ *
+ *  $RCSfile: cairo_canvashelper_texturefill.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-02-28 10:35:17 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#include <canvas/debug.hxx>
+
+#ifndef INCLUDED_RTL_MATH_HXX
+#include <rtl/math.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_RENDERING_TEXTDIRECTION_HPP__
+#include <com/sun/star/rendering/TextDirection.hpp>
+#endif
+#ifndef _COM_SUN_STAR_RENDERING_TEXTURINGMODE_HPP_
+#include <com/sun/star/rendering/TexturingMode.hpp>
+#endif
+#ifndef _COM_SUN_STAR_RENDERING_PATHCAPTYPE_HPP_
+#include <com/sun/star/rendering/PathCapType.hpp>
+#endif
+#ifndef _COM_SUN_STAR_RENDERING_PATHJOINTYPE_HPP_
+#include <com/sun/star/rendering/PathJoinType.hpp>
+#endif
+
+#ifndef _TL_POLY_HXX
+#include <tools/poly.hxx>
+#endif
+#ifndef _SV_WINDOW_HXX
+#include <vcl/window.hxx>
+#endif
+#ifndef _SV_BITMAPEX_HXX
+#include <vcl/bitmapex.hxx>
+#endif
+#ifndef _SV_BMPACC_HXX
+#include <vcl/bmpacc.hxx>
+#endif
+#ifndef _VCL_CANVASTOOLS_HXX
+#include <vcl/canvastools.hxx>
+#endif
+
+#ifndef _BGFX_MATRIX_B2DHOMMATRIX_HXX
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#endif
+#ifndef _BGFX_RANGE_B2DRECTANGLE_HXX
+#include <basegfx/range/b2drectangle.hxx>
+#endif
+#ifndef _BGFX_POINT_B2DPOINT_HXX
+#include <basegfx/point/b2dpoint.hxx>
+#endif
+#ifndef _BGFX_VECTOR_B2DSIZE_HXX
+#include <basegfx/vector/b2dsize.hxx>
+#endif
+#ifndef _BGFX_POLYGON_B2DPOLYGON_HXX
+#include <basegfx/polygon/b2dpolygon.hxx>
+#endif
+#ifndef _BGFX_POLYGON_B2DPOLYGONTOOLS_HXX
+#include <basegfx/polygon/b2dpolygontools.hxx>
+#endif
+#ifndef _BGFX_POLYGON_B2DPOLYPOLYGONTOOLS_HXX
+#include <basegfx/polygon/b2dpolypolygontools.hxx>
+#endif
+#ifndef _BGFX_POLYGON_B2DLINEGEOMETRY_HXX
+#include <basegfx/polygon/b2dlinegeometry.hxx>
+#endif
+#ifndef _BGFX_TOOLS_CANVASTOOLS_HXX
+#include <basegfx/tools/canvastools.hxx>
+#endif
+#ifndef _BGFX_NUMERIC_FTOOLS_HXX
+#include <basegfx/numeric/ftools.hxx>
+#endif
+
+#include <utility>
+
+#include <comphelper/sequence.hxx>
+#include <canvas/canvastools.hxx>
+
+#include "cairo_textlayout.hxx"
+#include "cairo_parametricpolypolygon.hxx"
+#include "cairo_canvashelper.hxx"
+#include "cairo_canvasbitmap.hxx"
+#include "cairo_impltools.hxx"
+#include "cairo_canvasfont.hxx"
+
+using namespace ::com::sun::star;
+
+namespace cairocanvas
+{
+    namespace
+    {
+        bool textureFill( OutputDevice&         rOutDev,
+                          GraphicObject&        rGraphic,
+                          const ::Point&        rPosPixel,
+                          const ::Size&         rNextTileX,
+                          const ::Size&         rNextTileY,
+                          sal_Int32             nTilesX,
+                          sal_Int32             nTilesY,
+                          const ::Size&         rTileSize,
+                          const GraphicAttr&    rAttr)
+        {
+            BOOL    bRet( false );
+            Point   aCurrPos;
+            int     nX, nY;
+
+            for( nY=0; nY < nTilesY; ++nY )
+            {
+                aCurrPos.X() = rPosPixel.X() + nY*rNextTileY.Width();
+                aCurrPos.Y() = rPosPixel.Y() + nY*rNextTileY.Height();
+
+                for( nX=0; nX < nTilesX; ++nX )
+                {
+                    // update return value. This method should return true, if
+                    // at least one of the looped Draws succeeded.
+                    bRet |= rGraphic.Draw( &rOutDev,
+                                           aCurrPos,
+                                           rTileSize,
+                                           &rAttr );
+
+                    aCurrPos.X() += rNextTileX.Width();
+                    aCurrPos.Y() += rNextTileX.Height();
+                }
+            }
+
+            return bRet;
+        }
+
+        inline sal_Int32 roundDown( const double& rVal )
+        {
+            return static_cast< sal_Int32 >( floor( rVal ) );
+        }
+
+        inline sal_Int32 roundUp( const double& rVal )
+        {
+            return static_cast< sal_Int32 >( ceil( rVal ) );
+        }
+    }
+
+    uno::Reference< rendering::XCachedPrimitive > CanvasHelper::fillTexturedPolyPolygon( const rendering::XCanvas&                          rCanvas,
+                                                                                         const uno::Reference< rendering::XPolyPolygon2D >& xPolyPolygon,
+                                                                                         const rendering::ViewState&                        viewState,
+                                                                                         const rendering::RenderState&                      renderState,
+                                                                                         const uno::Sequence< rendering::Texture >&         textures )
+    {
+        CHECK_AND_THROW( xPolyPolygon.is(),
+                         "CanvasHelper::fillPolyPolygon(): polygon is NULL");
+        CHECK_AND_THROW( textures.getLength(),
+                         "CanvasHelper::fillTexturedPolyPolygon: empty texture sequence");
+
+    cairo_save( mpCairo );
+
+    useStates( viewState, renderState, true );
+    mpTextures = &textures;
+    drawPolyPolygonPath( xPolyPolygon, Fill );
+    mpTextures = NULL;
+
+    cairo_restore( mpCairo );
+
+        return uno::Reference< rendering::XCachedPrimitive >(NULL);
+    }
+}
