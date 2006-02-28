f9473389dedf31502be16ad93ed21c535e546329
Kurt Zenker <kz@openoffice.org>
2006-02-28 09:48:46 +0000

    INTEGRATION: CWS cairocanvas (1.10.4); FILE MERGED 2005/11/24 17:47:25 thb 1.10.4.1: #i58018# applied pjanik's patch - 0L and sal_Int32 have differing types on x86_64

--- cppcanvas/source/mtfrenderer/textaction.cxx
+++ cppcanvas/source/mtfrenderer/textaction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textaction.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:42:02 $
+ *  last change: $Author: kz $ $Date: 2006-02-28 10:48:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -400,7 +400,7 @@ namespace cppcanvas
                                                 ::std::min(
                                                     rSubset.mnSubsetEnd - rSubset.mnSubsetBegin,
                                                     rOrigContext.Length ),
-                                                0L ) );
+                                                sal_Int32( 0 ) ) );
 
                 const rendering::StringContext aContext( rOrigContext.Text,
                                                          nNewStartPos,
