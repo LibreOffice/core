d9a99ec698e2dbccd5ea30533db8be2217322f20
Kurt Zenker <kz@openoffice.org>
2006-02-28 09:37:44 +0000

    INTEGRATION: CWS cairocanvas (1.1.2); FILE ADDED 2005/11/08 21:23:05 radekdoulik 1.1.2.3: Issue number:  #51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik updated cairo canvas to be usable by canvas02 2005/09/14 10:51:00 radekdoulik 1.1.2.2: Issue number: #51657 Submitted by: radekdoulik Reviewed by: radekdoulik Sync code in ooo-build with cairo canvas cws 2005/07/07 07:58:02 radekdoulik 1.1.2.1: Issue number:  51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik initial import of cairo canvas code

---
+++ canvas/source/cairo/cairo_repainttarget.hxx
@@ -0,0 +1,68 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: cairo_repainttarget.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-02-28 10:37:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CAIROCANVAS_REPAINTTARGET_HXX
+#define _CAIROCANVAS_REPAINTTARGET_HXX
+
+#ifndef _RTL_REF_HXX_
+#include <rtl/ref.hxx>
+#endif
+
+#include "cairo_cairo.hxx"
+
+using namespace ::cairo;
+
+namespace cairocanvas
+{
+    /* Definition of RepaintTarget interface */
+
+    /** Target interface for XCachedPrimitive implementations
+
+        This interface must be implemented on all canvas
+        implementations that hand out XCachedPrimitives
+     */
+    class RepaintTarget
+    {
+    public:
+        virtual ~RepaintTarget() {}
+
+        // call this when a bitmap is repainted
+        virtual bool repaint( ::cairo::Surface* pSurface,
+                  const ::com::sun::star::rendering::ViewState& viewState,
+                  const ::com::sun::star::rendering::RenderState& renderState ) = 0;
+    };
+}
+
+#endif /* _CAIROCANVAS_REPAINTTARGET_HXX */
