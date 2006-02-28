1e200d8219983beaf346322e3021824b5732dc83
Kurt Zenker <kz@openoffice.org>
2006-02-28 09:47:51 +0000

    INTEGRATION: CWS cairocanvas (1.2.112); FILE MERGED 2006/01/27 13:16:41 thb 1.2.112.2: RESYNC: (1.2-1.3); FILE MERGED 2005/11/09 13:00:27 radekdoulik 1.2.112.1: Issue number:  #51657 Submitted by:  radekdoulik Reviewed by:   radekdoulik scp2 changes to include cairo canvas component in serveces.rdb

--- scp2/source/canvas/makefile.mk
+++ scp2/source/canvas/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-12-14 14:43:57 $
+#   last change: $Author: kz $ $Date: 2006-02-28 10:47:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -54,6 +54,10 @@ SCPDEFS+=-DSYSTEM_AGG
 PARFILES= canvascommons.par		\
           directxcanvas.par     \
           vclcanvas.par
+
+.IF "$(ENABLE_CAIRO)" == "TRUE"
+PARFILES+= cairocanvas.par
+.ENDIF
 
 # --- File ---------------------------------------------------------
 
