b1b924f81d570b7f12e3a2c915d1e79309dd7d14
Rüdiger Timm <rt@openoffice.org>
2006-03-06 09:19:39 +0000

    INTEGRATION: CWS dbo510 (1.4.40); FILE MERGED 2005/11/01 22:19:11 dbo 1.4.40.1: adopted to ServiceDecl Issue number: Submitted by: Reviewed by:

--- desktop/source/deployment/gui/makefile.mk
+++ desktop/source/deployment/gui/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 17:17:47 $
+#   last change: $Author: rt $ $Date: 2006-03-06 10:19:39 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,8 +51,7 @@ SLOFILES = \
         $(SLO)$/dp_gui_cmdenv.obj
 
 SHL1OBJS = \
-        $(SLO)$/dp_misc.obj \
-    $(SLO)$/dp_servicefactory.obj
+        $(SLO)$/dp_misc.obj
 
 SHL1TARGET = $(TARGET)$(UPD)$(DLLPOSTFIX).uno
 SHL1VERSIONMAP = ..$/deployment.map
