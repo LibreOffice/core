b767b1b9b7ec127a629322cd56e6279cd8540a0c
Rüdiger Timm <rt@openoffice.org>
2006-03-08 13:00:50 +0000

    INTEGRATION: CWS ause048 (1.14.30); FILE MERGED 2006/02/06 14:38:46 hjs 1.14.30.1: #i61266# DLLSUFFIX is obsolete. use DLLPOSTFIX instead

--- configmgr/source/backend/makefile.mk
+++ configmgr/source/backend/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 03:32:45 $
+#   last change: $Author: rt $ $Date: 2006-03-08 14:00:50 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -81,5 +81,4 @@ SLOFILES=\
 # --- Targets ---
 
 .INCLUDE : target.mk
-.INCLUDE :  $(PRJ)$/util$/target.pmk
 
