cadea1175c712a30ab157ad20280ff9dfe79fa17
Vladimir Glazounov <vg@openoffice.org>
2006-03-14 10:29:57 +0000

    INTEGRATION: CWS pbrwuno (1.3.88); FILE MERGED 2005/11/02 12:17:20 fs 1.3.88.13: #i10000# exception specifications 2005/11/02 11:43:45 fs 1.3.88.12: #i10000# exception specifications 2005/10/31 11:13:08 fs 1.3.88.11: teach the ComposedPropertyUIUpdate to handle missing properties 2005/10/25 07:13:14 fs 1.3.88.10: #i53095# knitting lose ends (amongst others, make the handlers available as service) 2005/10/17 14:09:36 fs 1.3.88.9: #i53095# some cleanup of remaining TODOs 2005/10/13 13:01:09 fs 1.3.88.8: #i53095# introduce an XObjectInspector/Model 2005/10/11 13:29:42 fs 1.3.88.7: #i53095# phase 3: introduced XPropertyHandler and XObjectInspectorUI same open issues as in previous phase (plus probably some more, since not everything is tested, yet :-\) 2005/10/05 07:07:01 fs 1.3.88.6: RESYNC: (1.3-1.4); FILE MERGED 2005/09/05 07:41:53 fs 1.3.88.5: #i53095# phase 3, part 1: introduced XPropertyControl and relatives, describing one control in the ObjectInspector, responsible for one property known issues: - rebuildPropertyUI can cause problems now: If the user clicks into   the control for property A, which causes property B to be committed,   which causes the UI for property A to be rebuilt, then this will   crash currently. Reason: rebuildPropertyUI now synchronously replaces   the VCL-Window of the rebuilt control, which is exactly the one   which is still in some MouseButtonDown-handler.   possible solutions:   - see if rebuiltPropertyUI can be obsoleted - handlers should be able     to just obtain the XPropertyControl from the PropertyUI, and     re-initialize the control. Shouldn't they?`   - make one of the steps in the chain (mouse-click, handler-call,     rebuildPropertyUI-callback) asynchronous. 2005/08/18 12:44:34 fs 1.3.88.4: #i53095#, phase 2
    moved (nearly) all property handling to dedicated handlers, the controller is
    now simply managing a set of handlers
    
    open issues for making the property browser completely generic:
    - target page for a property - at the moment, the pbrw uses form-specific
      knowledge
    - relative position of properties. Again, the pbrw uses the OPropertyInfoService
      which is not generic
    - isComposeable for a given property. Also OPropertyInfoService-dependent ATM
    - help ids of pages and the pbrw as a whole. They're hard-coded at the moment
    
    other open issues:
    everything in the code which is tagged with TOD/UNOize. Those are items which
    do not immediately hinder phase 3 (real UNOization, i.e. definition of new
    UNO interfaces for the handlers, the controller, and so on), but need to be
    addressed in phase 4 (knit lose ends)
    2005/08/12 16:30:14 fs 1.3.88.3: - more fine-grained control in the IPropertyBrowserUI which elements
      to enable or disable
    - moved designing the SQL command into a dedicated handler
    - some more reactions on actuating properties move to dedicated handlers
    - *nearly* completed implementation of the "composed browser UI", which
      collects and combines UI change requests (IPropertyBrowserUI)
      (still missing: proper auto-firing)
    2005/08/10 15:41:47 fs 1.3.88.2: #i53095#
    get rid of nearly all [1] the implementations in OPropertyBrowserController::Clicked,
    and move them to a FormComponentHandler
    
    [1] still to migrate:
    - browsing for events (needs a dedicated event property handler)
    - handling for clicking the button of the Command property - this
      is kind of asynchronous, and IPropertyHandler is not yet prepared for this
    2005/08/09 14:00:05 fs 1.3.88.1: #i53095# phase 1:
    - don't use strings to transver values between controls and introspectee, but Anys
    - first version of a dedicated property handler for form-component-related properties
      (not yet completed)
    known regressions over previous phase:
    - handlers for events not yet implemented, thus some assertions
    - click handlers for form-component-related properties do not yet work,
      thus the browse buttons mostly do not work

--- extensions/source/propctrlr/propertycomposer.hxx
+++ extensions/source/propctrlr/propertycomposer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertycomposer.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:24:04 $
+ *  last change: $Author: vg $ $Date: 2006-03-14 11:29:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,71 +36,144 @@
 #ifndef EXTENSIONS_SOURCE_PROPCTRLR_PROPERTYCOMPOSER_HXX
 #define EXTENSIONS_SOURCE_PROPCTRLR_PROPERTYCOMPOSER_HXX
 
-#ifndef EXTENSIONS_SOURCE_PROPCTRLR_PROPERTYHANDLER_HXX
-#include "propertyhandler.hxx"
+#ifndef _EXTENSIONS_PROPCTRLR_PCRCOMMON_HXX_
+#include "pcrcommon.hxx"
+#endif
+#ifndef _EXTENSIONS_FORMSCTRLR_FORMBROWSERTOOLS_HXX_
+#include "formbrowsertools.hxx"
+#endif
+#ifndef EXTENSIONS_SOURCE_PROPCTRLR_COMPOSEDUIUPDATE_HXX
+#include "composeduiupdate.hxx"
+#endif
+#ifndef _EXTENSIONS_FORMSCTRLR_FORMBROWSERTOOLS_HXX_
+#include "formbrowsertools.hxx"
 #endif
 
 /** === begin UNO includes === **/
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+#ifndef _COM_SUN_STAR_INSPECTION_XPROPERTYHANDLER_HPP_
+#include <com/sun/star/inspection/XPropertyHandler.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
+#include <com/sun/star/lang/DisposedException.hpp>
+#endif
 /** === end UNO includes === **/
 
+#ifndef _CPPUHELPER_COMPBASE2_HXX_
+#include <cppuhelper/compbase2.hxx>
+#endif
+#ifndef _COMPHELPER_BROADCASTHELPER_HXX_
+#include <comphelper/broadcasthelper.hxx>
+#endif
+#ifndef COMPHELPER_INC_COMPHELPER_LISTENERNOTIFICATION_HXX
+#include <comphelper/listenernotification.hxx>
+#endif
+
 #include <vector>
+#include <set>
 
 //........................................................................
 namespace pcr
 {
 //........................................................................
 
-    class OPropertyInfoService;
     //====================================================================
     //= PropertyComposer
     //====================================================================
-    /** implements an <type>IPropertyHandler</type> which composes it's information
+    typedef ::cppu::WeakComponentImplHelper2    <   ::com::sun::star::inspection::XPropertyHandler
+                                                ,   ::com::sun::star::beans::XPropertyChangeListener
+                                                >   PropertyComposer_Base;
+    /** implements an <type>XPropertyHandler</type> which composes it's information
         from a set of other property handlers
     */
-    class PropertyComposer : public IPropertyHandler
+    class PropertyComposer  :public PropertyComposer_Base
+                            ,public ::comphelper::OBaseMutex
+                            ,public IPropertyExistenceCheck
     {
     public:
-        typedef ::std::vector< ::rtl::Reference< IPropertyHandler > >   HandlerArray;
+        typedef ::std::vector< ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyHandler > >
+                                                            HandlerArray;
 
     private:
-        const HandlerArray          m_aSlaveHandlers;
-        ::std::auto_ptr< OPropertyInfoService >
-                                    m_pInfoService;
-        oslInterlockedCount         m_refCount;
+        HandlerArray                                    m_aSlaveHandlers;
+        ::std::auto_ptr< ComposedPropertyUIUpdate >     m_pUIRequestComposer;
+        PropertyChangeListeners                         m_aPropertyListeners;
+        bool                                            m_bSupportedPropertiesAreKnown;
+        PropertyBag                                     m_aSupportedProperties;
 
     public:
-        /** constructs an <type>IPropertyHandler</type> which composes it's information from a set
+        /** constructs an <type>XPropertyHandler</type> which composes it's information from a set
             of other property handlers
 
             @param _rSlaveHandlers
                 the set of slave handlers to invoke. Must not be <NULL/>
         */
-        PropertyComposer( const ::std::vector< ::rtl::Reference< IPropertyHandler > >& _rSlaveHandlers );
+        PropertyComposer( const ::std::vector< ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyHandler > >& _rSlaveHandlers );
 
     public:
-        // IReference overridables
-        oslInterlockedCount SAL_CALL acquire();
-        oslInterlockedCount SAL_CALL release();
-
-        // IPropertyHandler overridables
-        virtual bool SAL_CALL supportsUIDescriptor( PropertyId _nPropId ) const;
-        virtual ::com::sun::star::uno::Any          SAL_CALL getPropertyValue( PropertyId _nPropId, bool _bLazy = true ) const;
-        virtual void                                SAL_CALL setPropertyValue( PropertyId _nPropId, const ::com::sun::star::uno::Any& _rValue );
-        virtual ::com::sun::star::uno::Any          SAL_CALL getPropertyValueFromStringRep( PropertyId _nPropId, const ::rtl::OUString& _rStringRep ) const;
-        virtual ::rtl::OUString                     SAL_CALL getStringRepFromPropertyValue( PropertyId _nPropId, const ::com::sun::star::uno::Any& _rValue ) const;
+        // XPropertyHandler overridables
+        virtual void                                SAL_CALL inspect( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& _rxIntrospectee ) throw (::com::sun::star::lang::NullPointerException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Any          SAL_CALL getPropertyValue( const ::rtl::OUString& _rPropertyName ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+        virtual void                                SAL_CALL setPropertyValue( const ::rtl::OUString& _rPropertyName, const ::com::sun::star::uno::Any& _rValue ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Any SAL_CALL convertToPropertyValue( const ::rtl::OUString& _rPropertyName, const ::com::sun::star::uno::Any& _rControlValue ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Any SAL_CALL convertToControlValue( const ::rtl::OUString& _rPropertyName, const ::com::sun::star::uno::Any& _rPropertyValue, const ::com::sun::star::uno::Type& _rControlValueType ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
         virtual ::com::sun::star::beans::PropertyState
-                                                    SAL_CALL getPropertyState( PropertyId _nPropId ) const;
-        virtual void                                SAL_CALL startAllPropertyChangeListening( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyChangeListener >& _rxListener );
-        virtual void                                SAL_CALL stopAllPropertyChangeListening( );
-        virtual ::std::vector< ::com::sun::star::beans::Property >
-                                                    SAL_CALL getSupportedProperties() const;
-        virtual ::std::vector< ::rtl::OUString >    SAL_CALL getSupersededProperties( ) const;
-        virtual ::std::vector< ::rtl::OUString >    SAL_CALL getActuatingProperties( ) const;
-        virtual void                                SAL_CALL describePropertyUI( PropertyId _nPropId, PropertyUIDescriptor& /* [out] */ _rDescriptor ) const;
-        virtual void                                SAL_CALL initializePropertyUI( PropertyId _nPropId, IPropertyBrowserUI* _pUpdater );
-        virtual bool                                SAL_CALL requestUserInputOnButtonClick( PropertyId _nPropId, bool _bPrimary, ::com::sun::star::uno::Any& _rData );
-        virtual void                                SAL_CALL executeButtonClick( PropertyId _nPropId, bool _bPrimary, const ::com::sun::star::uno::Any& _rData, IPropertyBrowserUI* _pUpdater );
-        virtual void                                SAL_CALL actuatingPropertyChanged( PropertyId _nActuatingPropId, const ::com::sun::star::uno::Any& _rNewValue, const ::com::sun::star::uno::Any& _rOldValue, IPropertyBrowserUI* _pUpdater, bool _bFirstTimeInit );
+                                                    SAL_CALL getPropertyState( const ::rtl::OUString& _rPropertyName ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+        virtual void                                SAL_CALL addPropertyChangeListener( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyChangeListener >& _rxListener ) throw (::com::sun::star::uno::RuntimeException);
+        virtual void                                SAL_CALL removePropertyChangeListener( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyChangeListener >& _rxListener ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::com::sun::star::beans::Property >
+                                                    SAL_CALL getSupportedProperties() throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::rtl::OUString >
+                                                    SAL_CALL getSupersededProperties( ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< ::rtl::OUString >
+                                                    SAL_CALL getActuatingProperties( ) throw (::com::sun::star::uno::RuntimeException);
+        virtual void                                SAL_CALL describePropertyLine( const ::rtl::OUString& _rPropertyName, ::com::sun::star::inspection::LineDescriptor& /* [out] */ _rDescriptor, const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyControlFactory >& _rxControlFactory ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::NullPointerException, ::com::sun::star::uno::RuntimeException);
+        virtual ::sal_Bool                          SAL_CALL isComposable( const ::rtl::OUString& _rPropertyName ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::inspection::InteractiveSelectionResult
+                                                    SAL_CALL onInteractivePropertySelection( const ::rtl::OUString& _rPropertyName, sal_Bool _bPrimary, ::com::sun::star::uno::Any& _rData, const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XObjectInspectorUI >& _rxInspectorUI ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::NullPointerException, ::com::sun::star::uno::RuntimeException);
+        virtual void                                SAL_CALL actuatingPropertyChanged( const ::rtl::OUString& _rActuatingPropertyName, const ::com::sun::star::uno::Any& _rNewValue, const ::com::sun::star::uno::Any& _rOldValue, const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XObjectInspectorUI >& _rxInspectorUI, sal_Bool _bFirstTimeInit ) throw (::com::sun::star::lang::NullPointerException, ::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool                            SAL_CALL suspend( sal_Bool _bSuspend ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XComponent
+        DECLARE_XCOMPONENT();
+        virtual void                                SAL_CALL disposing();
+
+        // XPropertyChangeListener
+        virtual void SAL_CALL propertyChange( const ::com::sun::star::beans::PropertyChangeEvent& evt ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XEventListener
+        virtual void SAL_CALL disposing( const ::com::sun::star::lang::EventObject& Source ) throw (::com::sun::star::uno::RuntimeException);
+
+        // IPropertyExistenceCheck
+        virtual ::sal_Bool SAL_CALL hasPropertyByName( const ::rtl::OUString& _rName ) throw (::com::sun::star::uno::RuntimeException);
+
+    private:
+        /** ensures that m_pUIRequestComposer exists
+        */
+        void    impl_ensureUIRequestComposer( const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XObjectInspectorUI >& _rxInspectorUI );
+
+        /** checks whether a given property exists in <member>m_aSupportedProperties</member>
+        */
+        bool    impl_isSupportedProperty_nothrow( const ::rtl::OUString& _rPropertyName )
+        {
+            ::com::sun::star::beans::Property aDummy; aDummy.Name = _rPropertyName;
+            return m_aSupportedProperties.find( aDummy ) != m_aSupportedProperties.end();
+        }
+
+    private:
+        friend class MethodGuard;
+        class MethodGuard : public ::osl::MutexGuard
+        {
+        public:
+            MethodGuard( PropertyComposer& _rInstance )
+                : ::osl::MutexGuard( _rInstance.m_aMutex )
+            {
+                if ( _rInstance.m_aSlaveHandlers.empty() )
+                    throw ::com::sun::star::lang::DisposedException( ::rtl::OUString(), *(&_rInstance) );
+            }
+        };
     };
 
 //........................................................................
