d8c859ba51c0ae4bf62dd5cf1d49ef88441efdc0
Vladimir Glazounov <vg@openoffice.org>
2006-03-14 10:22:01 +0000

    INTEGRATION: CWS pbrwuno (1.1.2); FILE ADDED 2005/10/13 13:01:01 fs 1.1.2.4: #i53095# introduce an XObjectInspector/Model 2005/10/11 13:29:07 fs 1.1.2.3: #i53095# phase 3: introduced XPropertyHandler and XObjectInspectorUI same open issues as in previous phase (plus probably some more, since not everything is tested, yet :-\) 2005/09/05 07:41:48 fs 1.1.2.2: #i53095# phase 3, part 1: introduced XPropertyControl and relatives, describing one control in the ObjectInspector, responsible for one property known issues: - rebuildPropertyUI can cause problems now: If the user clicks into   the control for property A, which causes property B to be committed,   which causes the UI for property A to be rebuilt, then this will   crash currently. Reason: rebuildPropertyUI now synchronously replaces   the VCL-Window of the rebuilt control, which is exactly the one   which is still in some MouseButtonDown-handler.   possible solutions:   - see if rebuiltPropertyUI can be obsoleted - handlers should be able     to just obtain the XPropertyControl from the PropertyUI, and     re-initialize the control. Shouldn't they?`   - make one of the steps in the chain (mouse-click, handler-call,     rebuildPropertyUI-callback) asynchronous. 2005/08/09 13:59:58 fs 1.1.2.1: #i53095# phase 1: - don't use strings to transver values between controls and introspectee, but Anys - first version of a dedicated property handler for form-component-related properties   (not yet completed) known regressions over previous phase: - handlers for events not yet implemented, thus some assertions - click handlers for form-component-related properties do not yet work,   thus the browse buttons mostly do not work

---
+++ extensions/source/propctrlr/enumrepresentation.hxx
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: enumrepresentation.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2006-03-14 11:22:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef EXTENSIONS_SOURCE_PROPCTRLR_ENUMREPRESENTATION_HXX
+#define EXTENSIONS_SOURCE_PROPCTRLR_ENUMREPRESENTATION_HXX
+
+/** === begin UNO includes === **/
+#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
+#include <com/sun/star/uno/Any.hxx>
+#endif
+/** === end UNO includes === **/
+
+#ifndef _RTL_REF_HXX_
+#include <rtl/ref.hxx>
+#endif
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+#include <vector>
+
+//........................................................................
+namespace pcr
+{
+//........................................................................
+
+    //====================================================================
+    //= IPropertyEnumRepresentation
+    //====================================================================
+    class IPropertyEnumRepresentation : public ::rtl::IReference
+    {
+    public:
+        /** retrieves all descriptions of all possible values of the enumeration property
+        */
+        virtual ::std::vector< ::rtl::OUString > SAL_CALL getDescriptions(
+            ) const = 0;
+
+        /** converts a given description into a property value
+        */
+        virtual void SAL_CALL getValueFromDescription(
+                const ::rtl::OUString& _rDescription,
+                ::com::sun::star::uno::Any& _out_rValue
+            ) const = 0;
+
+        /** converts a given property value into a description
+        */
+        virtual ::rtl::OUString SAL_CALL getDescriptionForValue(
+                const ::com::sun::star::uno::Any& _rEnumValue
+            ) const = 0;
+    };
+
+//........................................................................
+} // namespace pcr
+//........................................................................
+
+#endif // EXTENSIONS_SOURCE_PROPCTRLR_ENUMREPRESENTATION_HXX
+
