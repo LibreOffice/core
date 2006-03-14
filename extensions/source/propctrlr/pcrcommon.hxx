e2d34ad6e928bdb1806dfda0e82d5b744f6d9553
Vladimir Glazounov <vg@openoffice.org>
2006-03-14 10:27:40 +0000

    INTEGRATION: CWS pbrwuno (1.3.158); FILE MERGED 2005/11/02 12:17:18 fs 1.3.158.14: #i10000# exception specifications 2005/10/26 14:03:33 fs 1.3.158.13: some cleanups for finalizing #i53095# 2005/10/25 11:52:43 fs 1.3.158.12: #i53095# some exception specifications, and some conceptual changes for multiple handlers supporting the same property 2005/10/25 07:13:13 fs 1.3.158.11: #i53095# knitting lose ends (amongst others, make the handlers available as service) 2005/10/17 10:28:00 fs 1.3.158.10: #i53095# make numeric field exchange its values as double 2005/10/17 07:17:05 fs 1.3.158.9: replace MeasurementUnit with css.util.MeasureUnit 2005/10/14 12:43:46 fs 1.3.158.8: #i53095# properly care for MAYBEVOID properties and AMBIGUOUS property values 2005/10/14 08:40:43 fs 1.3.158.7: #i53095# let the XObjectInspectorModel provide category meta information part 2005/10/13 13:01:06 fs 1.3.158.6: #i53095# introduce an XObjectInspector/Model 2005/10/11 13:29:28 fs 1.3.158.5: #i53095# phase 3: introduced XPropertyHandler and XObjectInspectorUI same open issues as in previous phase (plus probably some more, since not everything is tested, yet :-\) 2005/10/05 07:03:51 fs 1.3.158.4: RESYNC: (1.3-1.4); FILE MERGED 2005/09/05 07:41:52 fs 1.3.158.3: #i53095# phase 3, part 1: introduced XPropertyControl and relatives, describing one control in the ObjectInspector, responsible for one property known issues: - rebuildPropertyUI can cause problems now: If the user clicks into   the control for property A, which causes property B to be committed,   which causes the UI for property A to be rebuilt, then this will   crash currently. Reason: rebuildPropertyUI now synchronously replaces   the VCL-Window of the rebuilt control, which is exactly the one   which is still in some MouseButtonDown-handler.   possible solutions:   - see if rebuiltPropertyUI can be obsoleted - handlers should be able     to just obtain the XPropertyControl from the PropertyUI, and     re-initialize the control. Shouldn't they?`   - make one of the steps in the chain (mouse-click, handler-call,     rebuildPropertyUI-callback) asynchronous. 2005/08/31 08:28:55 fs 1.3.158.2: #i53095# get rid of Set/GetPropertyName at the IBrowserControl 2005/08/18 12:44:32 fs 1.3.158.1: #i53095#, phase 2
    moved (nearly) all property handling to dedicated handlers, the controller is
    now simply managing a set of handlers
    
    open issues for making the property browser completely generic:
    - target page for a property - at the moment, the pbrw uses form-specific
      knowledge
    - relative position of properties. Again, the pbrw uses the OPropertyInfoService
      which is not generic
    - isComposeable for a given property. Also OPropertyInfoService-dependent ATM
    - help ids of pages and the pbrw as a whole. They're hard-coded at the moment
    
    other open issues:
    everything in the code which is tagged with TOD/UNOize. Those are items which
    do not immediately hinder phase 3 (real UNOization, i.e. definition of new
    UNO interfaces for the handlers, the controller, and so on), but need to be
    addressed in phase 4 (knit lose ends)

--- extensions/source/propctrlr/pcrcommon.hxx
+++ extensions/source/propctrlr/pcrcommon.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pcrcommon.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:20:51 $
+ *  last change: $Author: vg $ $Date: 2006-03-14 11:27:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,27 @@
 
 #define EDITOR_LIST_APPEND              (sal_uInt16)-1
 #define EDITOR_LIST_REPLACE_EXISTING    (sal_uInt16)-1
-#define EDITOR_PAGE_CURRENT             (sal_uInt16)-1
+
+/** === begin UNO includes === **/
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYCHANGELISTENER_HPP_
+#include <com/sun/star/beans/XPropertyChangeListener.hpp>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYCHANGEEVENT_HPP_
+#include <com/sun/star/beans/PropertyChangeEvent.hpp>
+#endif
+/** === end UNO includes === **/
 
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
+#endif
+#ifndef _VCL_FLDUNIT_HXX
+#include <vcl/fldunit.hxx>
+#endif
+#ifndef COMPHELPER_INC_COMPHELPER_LISTENERNOTIFICATION_HXX
+#include <comphelper/listenernotification.hxx>
 #endif
 
 //............................................................................
@@ -50,13 +67,115 @@ namespace pcr
 //............................................................................
 
     #define OWN_PROPERTY_ID_INTROSPECTEDOBJECT      0x0010
-    #define OWN_PROPERTY_ID_INTROSPECTEDCOLLECTION  0x0011
-    #define OWN_PROPERTY_ID_CURRENTPAGE             0x0012
-    #define OWN_PROPERTY_ID_CONTROLCONTEXT          0x0013
-    #define OWN_PROPERTY_ID_TABBINGMODEL            0x0014
-    #define OWN_PROPERTY_ID_CONTEXTDOCUMENT         0x0015
+    #define OWN_PROPERTY_ID_CURRENTPAGE             0x0011
+    #define OWN_PROPERTY_ID_CONTROLCONTEXT          0x0012
+    #define OWN_PROPERTY_ID_TABBINGMODEL            0x0013
 
-    String getStandardString();
+    //========================================================================
+    //= types
+    //========================================================================
+    typedef ::comphelper::OSimpleListenerContainer  <   ::com::sun::star::beans::XPropertyChangeListener
+                                                    ,   ::com::sun::star::beans::PropertyChangeEvent
+                                                    >   PropertyChangeListeners;
+
+    //========================================================================
+    //= helper
+    //========================================================================
+    // small helper to make the "swap" call on an STL container a single-line call, which
+    // in it's canonic form "aFoo.swap( Container() )" doesn't compile with GCC
+    template< class CONTAINER >
+    void clearContainer( CONTAINER& _rContainer )
+    {
+        CONTAINER aEmpty;
+        _rContainer.swap( aEmpty );
+    }
+
+    //========================================================================
+    //= HelpIdUrl
+    //========================================================================
+    /// small helper to translate help ids into help urls
+    class HelpIdUrl
+    {
+    public:
+        static sal_uInt32 getHelpId( const ::rtl::OUString& _rHelpURL );
+        static ::rtl::OUString getHelpURL( sal_uInt32 _nHelpId );
+    };
+
+    //========================================================================
+    //= MeasurementUnitConversion
+    //========================================================================
+    /** small helper to convert between <type>MeasurementUnit</type> and
+        <type>FieldUnit</type>
+    */
+    class MeasurementUnitConversion
+    {
+    public:
+        static sal_Int16 convertToMeasurementUnit( FieldUnit _nFieldUnit, sal_Int16 _rFieldToUNOValueFactor );
+        static FieldUnit convertToFieldUnit( sal_Int16 _nMeasurementUnit, sal_Int16& _rFieldToUNOValueFactor );
+    };
+
+    //====================================================================
+    //= StlSyntaxSequence
+    //====================================================================
+    template< class ELEMENT >
+    class StlSyntaxSequence : public ::com::sun::star::uno::Sequence< ELEMENT >
+    {
+    private:
+        typedef ::com::sun::star::uno::Sequence< ELEMENT >  UnoBase;
+
+    public:
+        inline StlSyntaxSequence() : UnoBase() { }
+        inline StlSyntaxSequence( const UnoBase& rSeq ) : UnoBase( rSeq ) { }
+        inline StlSyntaxSequence( const ELEMENT* pElements, sal_Int32 len ) : UnoBase( pElements, len ) { }
+        inline StlSyntaxSequence( sal_Int32 len ) : UnoBase( len ) { }
+
+        operator const UnoBase&() const { return *this; }
+        operator       UnoBase&()       { return *this; }
+
+        typedef const ELEMENT* const_iterator;
+        typedef       ELEMENT* iterator;
+
+        inline const_iterator begin() const { return UnoBase::getConstArray(); }
+        inline const_iterator end() const { return UnoBase::getConstArray() + UnoBase::getLength(); }
+
+        inline iterator begin() { return UnoBase::getArray(); }
+        inline iterator end() { return UnoBase::getArray() + UnoBase::getLength(); }
+
+        inline sal_Int32 size() const { return UnoBase::getLength(); }
+        inline bool empty() const { return UnoBase::getLength() == 0; }
+
+        inline void resize( size_t _newSize ) { UnoBase::realloc( _newSize ); }
+
+        inline iterator erase( iterator _pos )
+        {
+            iterator loop = end();
+            while ( --loop != _pos )
+                *( loop - 1 ) = *loop;
+            resize( size() - 1 );
+        }
+    };
+
+    //========================================================================
+    //= UNO helpers
+    //========================================================================
+#define DECLARE_XCOMPONENT() \
+    virtual void SAL_CALL dispose(  ) throw (::com::sun::star::uno::RuntimeException); \
+    virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException); \
+    virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+#define IMPLEMENT_FORWARD_XCOMPONENT( classname, baseclass ) \
+    void SAL_CALL classname::dispose(  ) throw (::com::sun::star::uno::RuntimeException) \
+    { \
+    baseclass::WeakComponentImplHelperBase::dispose(); \
+    } \
+    void SAL_CALL classname::addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& _Listener ) throw (::com::sun::star::uno::RuntimeException) \
+    { \
+        baseclass::WeakComponentImplHelperBase::addEventListener( _Listener ); \
+    } \
+    void SAL_CALL classname::removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& _Listener ) throw (::com::sun::star::uno::RuntimeException) \
+    { \
+        baseclass::WeakComponentImplHelperBase::removeEventListener( _Listener ); \
+    } \
 
 //............................................................................
 } // namespace pcr
