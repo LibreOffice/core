de60118da24e02367d0499f181aeab486d4f7d50
Vladimir Glazounov <vg@openoffice.org>
2006-03-15 08:11:55 +0000

    INTEGRATION: CWS jsc3 (1.3.14); FILE MERGED 2006/01/20 13:02:19 jsc 1.3.14.1: #i56247# unify include guards

--- codemaker/source/codemaker/options.cxx
+++ codemaker/source/codemaker/options.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: options.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:10:16 $
+ *  last change: $Author: vg $ $Date: 2006-03-15 09:11:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,7 @@
  *
  ************************************************************************/
 
-#ifndef _CODEMAKER_OPTIONS_HXX_
-#include    <codemaker/options.hxx>
-#endif
+#include "codemaker/options.hxx"
 
 using namespace rtl;
 
