ea6b08200d42fb4ad4813c272989b93f927aab3f
Vladimir Glazounov <vg@openoffice.org>
2006-03-16 10:14:13 +0000

    INTEGRATION: CWS jaxpapi (1.10.56); FILE MERGED 2006/02/04 23:18:01 cmc 1.10.56.1: #i58308# jaxp->xml-apis.jar and parser.jar -> xercesImpl.jar

--- officecfg/org/openoffice/helper/makefile.mk
+++ officecfg/org/openoffice/helper/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: hr $ $Date: 2005-10-25 10:45:39 $
+#   last change: $Author: vg $ $Date: 2006-03-16 11:14:13 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,7 +43,7 @@ PACKAGE=org$/openoffice$/helper
 
 .INCLUDE :  settings.mk
 
-JARFILES = jaxp.jar parser.jar
+JARFILES = xml-apis.jar xercesImpl.jar
 
 JAVACLASSFILES= \
     $(CLASSDIR)$/$(PACKAGE)$/DefaultNamespaceRemover.class \
