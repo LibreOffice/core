cc7210bd1b192cdbd879efd11f181b938cda3bd0
Vladimir Glazounov <vg@openoffice.org>
2006-03-16 11:59:10 +0000

    INTEGRATION: CWS sixtyfour02 (1.3.14); FILE MERGED 2006/02/25 12:52:53 cmc 1.3.14.1: #i62321 streaming ULONG -> sal_uInt32

--- idl/source/prj/database.cxx
+++ idl/source/prj/database.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: database.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:51:58 $
+ *  last change: $Author: vg $ $Date: 2006-03-16 12:59:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
