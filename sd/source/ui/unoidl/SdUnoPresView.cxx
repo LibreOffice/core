3fd7dd9f53783aa00369d015639db2ebcbca62d5
Oliver Bolte <obo@openoffice.org>
2006-03-21 16:34:45 +0000

    INTEGRATION: CWS viewswitch (1.6.74); FILE MERGED 2005/11/29 15:37:07 af 1.6.74.1: #i57552# Replaced base class DrawController to DrawSubController.

--- sd/source/ui/unoidl/SdUnoPresView.cxx
+++ sd/source/ui/unoidl/SdUnoPresView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdUnoPresView.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 17:23:53 $
+ *  last change: $Author: obo $ $Date: 2006-03-21 17:34:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,120 +35,25 @@
 
 #include "SdUnoPresView.hxx"
 
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
-#include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef _VOS_MUTEX_HXX_
-#include <vos/mutex.hxx>
-#endif
-#ifndef _SV_SVAPP_HXX
-#include <vcl/svapp.hxx>
-#endif
-
-using ::rtl::OUString;
-using namespace ::vos;
-using namespace ::cppu;
-using namespace ::com::sun::star;
-using namespace ::com::sun::star::uno;
 
 namespace sd {
 
 
 SdUnoPresView::SdUnoPresView (
-    ViewShellBase& rBase,
-    ViewShell& rViewShell,
+    DrawController& rController,
+    DrawViewShell& rViewShell,
     View& rView) throw()
-    : SdUnoDrawView (rBase, rViewShell, rView)
+    : SdUnoDrawView (rController, rViewShell, rView)
 {
 }
 
 
 
 
-SdUnoPresView::~SdUnoPresView() throw()
+SdUnoPresView::~SdUnoPresView (void) throw()
 {
 }
 
 
-
-
-// XTypeProvider
-
-IMPLEMENT_GET_IMPLEMENTATION_ID(SdUnoPresView);
-
-
-
-
-// XServiceInfo
-
-
-OUString SAL_CALL SdUnoPresView::getImplementationName (void)
-    throw(RuntimeException)
-{
-    return OUString( RTL_CONSTASCII_USTRINGPARAM( "SdUnoPresView" ) );
-}
-
-
-
-
-//----------------------------------------------------------------------
-//------ The Properties of this implementation -------------------------
-//----------------------------------------------------------------------
-
-// Id must be the index into the array
-enum properties
-{
-    PROPERTY_CURRENTPAGE = 0,
-
-    PROPERTY_COUNT
-};
-
-/**
- * All Properties of this implementation. Must be sorted by name.
- */
-static beans::Property * getBasicProps()
-{
-    static beans::Property *pTable = 0;
-
-    if( ! pTable )
-    {
-        ::osl::MutexGuard guard( ::osl::Mutex::getGlobalMutex() );
-        if( ! pTable )
-        {
-
-            static beans::Property aBasicProps[PROPERTY_COUNT] =
-            {
-                beans::Property( OUString( RTL_CONSTASCII_USTRINGPARAM("CurrentPage") ),        PROPERTY_CURRENTPAGE,   ::getCppuType((const Reference< drawing::XDrawPage > *)0), beans::PropertyAttribute::BOUND ),
-            };
-            pTable = aBasicProps;
-        }
-    }
-    return pTable;
-}
-
-//----------------------------------------------------------------------
-//------ XPropertySet & OPropertySetHelper -----------------------------
-//----------------------------------------------------------------------
-
-/**
- * Create a table that map names to index values.
- */
-IPropertyArrayHelper & SdUnoPresView::getInfoHelper()
-{
-    OGuard aGuard( Application::GetSolarMutex() );
-
-    static OPropertyArrayHelper aInfo( getBasicProps(), PROPERTY_COUNT );
-    return aInfo;
-}
-
-//----------------------------------------------------------------------
-
-Reference < beans::XPropertySetInfo >  SdUnoPresView::getPropertySetInfo() throw ( ::com::sun::star::uno::RuntimeException)
-{
-    OGuard aGuard( Application::GetSolarMutex() );
-
-    static Reference < beans::XPropertySetInfo >  xInfo( createPropertySetInfo( getInfoHelper() ) );
-    return xInfo;
-}
 
 } // end of namespace sd
