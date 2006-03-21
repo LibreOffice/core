7105bd2a0ac7f6b64fa2caba9d20fdf7c620fc23
Oliver Bolte <obo@openoffice.org>
2006-03-21 16:34:20 +0000

    INTEGRATION: CWS viewswitch (1.13.4); FILE MERGED 2006/01/20 14:32:10 af 1.13.4.3: #i57552# Fixed resync problems. 2006/01/19 17:50:40 af 1.13.4.2: RESYNC: (1.13-1.15); FILE MERGED 2005/11/29 15:36:30 af 1.13.4.1: #i57552# Replaced base class DrawController to DrawSubController.

--- sd/source/ui/unoidl/SdUnoOutlineView.cxx
+++ sd/source/ui/unoidl/SdUnoOutlineView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdUnoOutlineView.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 17:23:36 $
+ *  last change: $Author: obo $ $Date: 2006-03-21 17:34:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,53 +35,16 @@
 
 #include "SdUnoOutlineView.hxx"
 
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
-#include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-
-#ifndef _SVX_UNOSHAPE_HXX
-#include <svx/unoshape.hxx>
-#endif
-
-#include <svx/svdobj.hxx>
-#include <svx/svdpagv.hxx>
-#include <svx/unoshape.hxx>
-#include <svx/unoshcol.hxx>
-
-#ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
-#include <toolkit/helper/vclunohelper.hxx>
-#endif
-
-#ifndef SD_WINDOW_HXX
-#include "Window.hxx"
-#endif
+#include "DrawController.hxx"
 #ifndef SD_OUTLINE_VIEW_SHELL_HXX
 #include "OutlineViewShell.hxx"
 #endif
-#include "unohelp.hxx"
+#include "sdpage.hxx"
 #include "unopage.hxx"
-#include "unomodel.hxx"
-#ifndef SD_VIEW_HXX
-#include "View.hxx"
-#endif
-#ifndef SD_DRAW_VIEW_SHELL_HXX
-#include "DrawViewShell.hxx"
-#endif
-#include "drawdoc.hxx"
-#ifndef SD_DRAW_DOC_SHELL_HXX
-#include "DrawDocShell.hxx"
-#endif
-#ifndef SD_GRAPHIC_VIEW_SHELL_HXX
-#include "GraphicViewShell.hxx"
-#endif
-#ifndef SD_PRESENTATION_VIEW_SHELL_HXX
-#include "PresentationViewShell.hxx"
-#endif
-#include <cppuhelper/proptypehlp.hxx>
 
-using ::rtl::OUString;
-using namespace ::vos;
-using namespace ::cppu;
+#include <cppuhelper/proptypehlp.hxx>
+#include <svx/unopage.hxx>
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 
@@ -89,97 +52,23 @@ using namespace ::com::sun::star::uno;
 
 namespace sd {
 
-//----------------------------------------------------------------------
-//------ SdUnoOutlineView--------------------------------------------------
-//----------------------------------------------------------------------
-
 SdUnoOutlineView::SdUnoOutlineView(
-    ViewShellBase& rBase,
+    DrawController& rController,
     OutlineViewShell& rViewShell,
     View& rView) throw()
-    :   DrawController (rBase, rViewShell, rView),
-        mxCurrentPage(NULL)
+    :   DrawSubController(),
+        mrController(rController),
+        mrOutlineViewShell(rViewShell),
+        mrView(rView)
 {
 }
 
 
 
 
-SdUnoOutlineView::~SdUnoOutlineView() throw()
+SdUnoOutlineView::~SdUnoOutlineView (void) throw()
 {
-    try
-    {
-        Reference<XComponent> xComponent (mxCurrentPage, UNO_QUERY);
-        if (xComponent.is())
-            xComponent->removeEventListener(this);
-    }
-    catch (Exception aException)
-    {
-    }
 }
-
-
-
-
-OutlineViewShell* SdUnoOutlineView::GetDrawViewShell (void) const
-{
-    return static_cast<OutlineViewShell*>(mpViewShell);
-}
-
-
-
-
-// XComponent
-void SAL_CALL SdUnoOutlineView::dispose (void)
-    throw(::com::sun::star::uno::RuntimeException)
-{
-    Reference<XComponent> xComponent (mxCurrentPage, UNO_QUERY);
-    if (xComponent.is())
-        xComponent->removeEventListener(this);
-    mxCurrentPage = NULL;
-
-    DrawController::dispose();
-}
-
-
-
-
-// XTypeProvider
-
-IMPLEMENT_GET_IMPLEMENTATION_ID(SdUnoOutlineView);
-
-
-
-
-
-// XServiceInfo
-
-
-sal_Char pImplSdUnoOutlineViewService[sizeof("com.sun.star.presentation.OutlineView")] = "com.sun.star.presentation.OutlineView";
-
-OUString SAL_CALL SdUnoOutlineView::getImplementationName(  ) throw(RuntimeException)
-{
-    return OUString( RTL_CONSTASCII_USTRINGPARAM( "SdUnoOutlineView" ) );
-}
-
-
-
-
-sal_Bool SAL_CALL SdUnoOutlineView::supportsService( const OUString& ServiceName ) throw(RuntimeException)
-{
-    return ServiceName.equalsAscii( pImplSdUnoOutlineViewService );
-}
-
-
-
-
-Sequence< OUString > SAL_CALL SdUnoOutlineView::getSupportedServiceNames(  ) throw(RuntimeException)
-{
-    OUString aService( RTL_CONSTASCII_USTRINGPARAM( pImplSdUnoOutlineViewService ) );
-    Sequence< OUString > aSeq( &aService, 1 );
-    return aSeq;
-}
-
 
 
 
@@ -212,19 +101,11 @@ void SAL_CALL SdUnoOutlineView::setCurrentPage (
     const Reference< drawing::XDrawPage >& xPage)
     throw(RuntimeException)
 {
-    OGuard aGuard( Application::GetSolarMutex() );
+    SvxDrawPage* pDrawPage = SvxDrawPage::getImplementation( xPage );
+    SdrPage *pSdrPage = pDrawPage ? pDrawPage->GetSdrPage() : NULL;
 
-    DBG_ASSERT (mpView!=NULL, "View is NULL in SdUnoDrawView::setCurrentPage");
-
-    if (mpViewShell != NULL)
-    {
-        SvxDrawPage* pDrawPage = SvxDrawPage::getImplementation( xPage );
-        SdrPage *pSdrPage = pDrawPage ? pDrawPage->GetSdrPage() : NULL;
-
-        OutlineViewShell* pOutlineViewShell = GetDrawViewShell();
-        if (pSdrPage != NULL && pOutlineViewShell != NULL)
-            pOutlineViewShell->SetCurrentPage(dynamic_cast<SdPage*>(pSdrPage));
-    }
+    if (pSdrPage != NULL)
+        mrOutlineViewShell.SetCurrentPage(dynamic_cast<SdPage*>(pSdrPage));
 }
 
 
@@ -233,38 +114,13 @@ void SAL_CALL SdUnoOutlineView::setCurrentPage (
 Reference< drawing::XDrawPage > SAL_CALL SdUnoOutlineView::getCurrentPage (void)
     throw(RuntimeException)
 {
-    ThrowIfDisposed();
-    OGuard aGuard( Application::GetSolarMutex() );
-
     Reference<drawing::XDrawPage>  xPage;
 
-    if (mpViewShell != NULL)
-    {
-        SdPage* pPage = mpViewShell->getCurrentPage();
-         if (pPage != NULL)
-             xPage = Reference<drawing::XDrawPage>::query(pPage->getUnoPage());
-    }
+    SdPage* pPage = mrOutlineViewShell.getCurrentPage();
+    if (pPage != NULL)
+        xPage = Reference<drawing::XDrawPage>::query(pPage->getUnoPage());
 
     return xPage;
-}
-
-
-
-
-/**
- * All Properties of this implementation. Must be sorted by name.
- */
-void SdUnoOutlineView::FillPropertyTable (
-    ::std::vector<beans::Property>& rProperties)
-{
-    DrawController::FillPropertyTable (rProperties);
-
-    rProperties.push_back (
-        beans::Property( OUString(
-            RTL_CONSTASCII_USTRINGPARAM("CurrentPage") ),
-            PROPERTY_CURRENTPAGE,
-            ::getCppuType((const Reference< drawing::XDrawPage > *)0),
-            beans::PropertyAttribute::BOUND));
 }
 
 
@@ -280,11 +136,9 @@ sal_Bool SdUnoOutlineView::convertFastPropertyValue (
 {
     sal_Bool bResult = sal_False;
 
-    OGuard aGuard( Application::GetSolarMutex() );
-
     switch( nHandle )
     {
-        case PROPERTY_CURRENTPAGE:
+        case DrawController::PROPERTY_CURRENTPAGE:
             {
                 Reference< drawing::XDrawPage > xOldPage( getCurrentPage() );
                 Reference< drawing::XDrawPage > xNewPage;
@@ -299,8 +153,6 @@ sal_Bool SdUnoOutlineView::convertFastPropertyValue (
             break;
 
         default:
-            bResult = DrawController::convertFastPropertyValue
-                (rConvertedValue, rOldValue, nHandle, rValue);
             break;
     }
 
@@ -317,11 +169,9 @@ void SdUnoOutlineView::setFastPropertyValue_NoBroadcast (
     const Any& rValue)
     throw ( com::sun::star::uno::Exception)
 {
-    OGuard aGuard( Application::GetSolarMutex() );
-
     switch( nHandle )
     {
-        case PROPERTY_CURRENTPAGE:
+        case DrawController::PROPERTY_CURRENTPAGE:
         {
             Reference< drawing::XDrawPage > xPage;
             rValue >>= xPage;
@@ -330,7 +180,6 @@ void SdUnoOutlineView::setFastPropertyValue_NoBroadcast (
         break;
 
         default:
-            DrawController::setFastPropertyValue_NoBroadcast (nHandle, rValue);
             break;
     }
 }
@@ -341,8 +190,6 @@ void SdUnoOutlineView::setFastPropertyValue_NoBroadcast (
 void SAL_CALL SdUnoOutlineView::disposing (const ::com::sun::star::lang::EventObject& rEventObject)
     throw (::com::sun::star::uno::RuntimeException)
 {
-    if (rEventObject.Source == mxCurrentPage)
-        FireSwitchCurrentPage(NULL);
 }
 
 
@@ -352,54 +199,18 @@ void SdUnoOutlineView::getFastPropertyValue(
     Any & rRet,
     sal_Int32 nHandle ) const
 {
-    OGuard aGuard( Application::GetSolarMutex() );
-
-    if (mpViewShell != NULL)
+    switch( nHandle )
     {
-        switch( nHandle )
+        case DrawController::PROPERTY_CURRENTPAGE:
         {
-            case PROPERTY_CURRENTPAGE:
-            {
-                SdPage* pPage = const_cast<OutlineViewShell*>(
-                    static_cast<const OutlineViewShell*>(mpViewShell)
-                    )->GetActualPage();
-
-                if (pPage != NULL)
-                    rRet <<= pPage->getUnoPage();
-            }
-            break;
-
-            default:
-                DrawController::getFastPropertyValue (rRet, nHandle);
+            SdPage* pPage = const_cast<OutlineViewShell&>(mrOutlineViewShell).GetActualPage();
+            if (pPage != NULL)
+                rRet <<= pPage->getUnoPage();
         }
-    }
-}
+        break;
 
-
-
-
-void SdUnoOutlineView::FireSwitchCurrentPage (SdPage* pCurrentPage) throw()
-{
-
-    Reference<drawing::XDrawPage> xNewPage;
-    if (pCurrentPage != NULL)
-        xNewPage = Reference<drawing::XDrawPage>(pCurrentPage->getUnoPage(), UNO_QUERY);
-
-    if (xNewPage != mxCurrentPage)
-    {
-        Any aNewValue (makeAny(xNewPage));
-        Any aOldValue (makeAny(mxCurrentPage));
-        FirePropertyChange (PROPERTY_CURRENTPAGE, aNewValue, aOldValue);
-
-        Reference<XComponent> xComponent (mxCurrentPage, UNO_QUERY);
-        if (xComponent.is())
-            xComponent->removeEventListener(this);
-
-        mxCurrentPage = xNewPage;
-
-        xComponent = Reference<XComponent>(mxCurrentPage, UNO_QUERY);
-        if (xComponent.is())
-            xComponent->addEventListener(this);
+        default:
+            break;
     }
 }
 
