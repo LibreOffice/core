fdd350d35efae9e76d4adb6d103dc9dbc27c2a8e
Oliver Bolte <obo@openoffice.org>
2006-03-22 09:22:27 +0000

    INTEGRATION: CWS vcl55 (1.10.192); FILE MERGED 2006/03/14 12:12:23 pl 1.10.192.2: RESYNC: (1.10-1.11); FILE MERGED 2006/03/06 14:09:59 hdu 1.10.192.1: #132892# be more defensive in X11SalVirtualDevice destructor

--- vcl/unx/source/gdi/salvd.cxx
+++ vcl/unx/source/gdi/salvd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salvd.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-28 10:47:23 $
+ *  last change: $Author: obo $ $Date: 2006-03-22 10:22:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -186,6 +186,7 @@ X11SalVirtualDevice::~X11SalVirtualDevice()
 {
     if( pGraphics_ )
         delete pGraphics_;
+    pGraphics_ = NULL;
 
     if( GetDrawable() && !bExternPixmap_ )
         XFreePixmap( GetXDisplay(), GetDrawable() );
