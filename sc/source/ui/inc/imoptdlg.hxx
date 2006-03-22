b665834f4fc9ab3a7568522c2ddc591aa1aa08fc
Oliver Bolte <obo@openoffice.org>
2006-03-22 11:11:02 +0000

    INTEGRATION: CWS dr46 (1.11.106); FILE MERGED 2006/02/16 13:06:20 er 1.11.106.1: #i4925# CSV export with option 'save as shown'

--- sc/source/ui/inc/imoptdlg.hxx
+++ sc/source/ui/inc/imoptdlg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imoptdlg.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:33:06 $
+ *  last change: $Author: obo $ $Date: 2006-03-22 12:11:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,91 +70,55 @@
 
 //===================================================================
 
-//CHINA001 class ScImportOptions;
-//CHINA001 class ScDelimiterTable;
-
-//CHINA001 class ScImportOptionsDlg : public ModalDialog
-//CHINA001 {
-//CHINA001 public:
-//CHINA001 ScImportOptionsDlg( Window*              pParent,
-//CHINA001 BOOL                    bAscii = TRUE,
-//CHINA001 const ScImportOptions*   pOptions = NULL,
-//CHINA001 const String*            pStrTitle = NULL,
-//CHINA001 BOOL                    bMultiByte = FALSE,
-//CHINA001 BOOL                    bOnlyDbtoolsEncodings = FALSE,
-//CHINA001 BOOL                    bImport = TRUE );
-//CHINA001
-//CHINA001 ~ScImportOptionsDlg();
-//CHINA001
-//CHINA001 void GetImportOptions( ScImportOptions& rOptions ) const;
-//CHINA001
-//CHINA001 private:
-//CHINA001 FixedLine           aFlFieldOpt;
-//CHINA001 FixedText            aFtFont;
-//CHINA001 SvxTextEncodingBox   aLbFont;
-//CHINA001 FixedText            aFtFieldSep;
-//CHINA001 ComboBox         aEdFieldSep;
-//CHINA001 FixedText            aFtTextSep;
-//CHINA001 ComboBox         aEdTextSep;
-//CHINA001 CheckBox            aCbFixed;
-//CHINA001 OKButton         aBtnOk;
-//CHINA001 CancelButton     aBtnCancel;
-//CHINA001 HelpButton           aBtnHelp;
-//CHINA001
-//CHINA001 ScDelimiterTable*    pFieldSepTab;
-//CHINA001 ScDelimiterTable*    pTextSepTab;
-//CHINA001
-//CHINA001 private:
-//CHINA001 USHORT GetCodeFromCombo( const ComboBox& rEd ) const;
-//CHINA001
-//CHINA001 DECL_LINK( FixedWidthHdl, CheckBox* );
-//CHINA001 DECL_LINK( DoubleClickHdl, ListBox* );
-//CHINA001 };
-
-//------------------------------------------------------------------------
-
 class SC_DLLPUBLIC ScImportOptions
 {
 public:
         ScImportOptions()
-            : nFieldSepCode(0),nTextSepCode(0),eCharSet(RTL_TEXTENCODING_DONTKNOW),bFixedWidth(FALSE)
+            : nFieldSepCode(0), nTextSepCode(0),
+            eCharSet(RTL_TEXTENCODING_DONTKNOW), bFixedWidth(FALSE),
+            bSaveAsShown(FALSE)
         {}
         ScImportOptions( const String& rStr );
 
         ScImportOptions( sal_Unicode nFieldSep, sal_Unicode nTextSep, const String& rStr )
-            : nFieldSepCode(nFieldSep),nTextSepCode(nTextSep),aStrFont(rStr),bFixedWidth(FALSE)
+            : nFieldSepCode(nFieldSep), nTextSepCode(nTextSep), aStrFont(rStr),
+            bFixedWidth(FALSE), bSaveAsShown(FALSE)
         { eCharSet = ScGlobal::GetCharsetValue(aStrFont); }
 
         ScImportOptions( sal_Unicode nFieldSep, sal_Unicode nTextSep, rtl_TextEncoding nEnc )
-            : nFieldSepCode(nFieldSep),nTextSepCode(nTextSep),bFixedWidth(FALSE)
+            : nFieldSepCode(nFieldSep), nTextSepCode(nTextSep),
+            bFixedWidth(FALSE), bSaveAsShown(FALSE)
         { SetTextEncoding( nEnc ); }
 
         ScImportOptions( const ScImportOptions& rCpy )
-            : nFieldSepCode (rCpy.nFieldSepCode),
-              nTextSepCode  (rCpy.nTextSepCode),
-              aStrFont      (rCpy.aStrFont),
-              eCharSet      (rCpy.eCharSet),
-              bFixedWidth   (rCpy.bFixedWidth)
+            : nFieldSepCode     (rCpy.nFieldSepCode),
+              nTextSepCode      (rCpy.nTextSepCode),
+              aStrFont          (rCpy.aStrFont),
+              eCharSet          (rCpy.eCharSet),
+              bFixedWidth       (rCpy.bFixedWidth),
+              bSaveAsShown      (rCpy.bSaveAsShown)
         {}
 
     ScImportOptions& operator=( const ScImportOptions& rCpy )
                         {
-                            nFieldSepCode = rCpy.nFieldSepCode;
-                            nTextSepCode  = rCpy.nTextSepCode;
-                            aStrFont      = rCpy.aStrFont;
-                            eCharSet      = rCpy.eCharSet;
-                            bFixedWidth   = rCpy.bFixedWidth;
+                            nFieldSepCode   = rCpy.nFieldSepCode;
+                            nTextSepCode    = rCpy.nTextSepCode;
+                            aStrFont        = rCpy.aStrFont;
+                            eCharSet        = rCpy.eCharSet;
+                            bFixedWidth     = rCpy.bFixedWidth;
+                            bSaveAsShown    = rCpy.bSaveAsShown;
                             return *this;
                         }
 
     BOOL             operator==( const ScImportOptions& rCmp )
                         {
                             return
-                                   nFieldSepCode == rCmp.nFieldSepCode
-                                && nTextSepCode  == rCmp.nTextSepCode
-                                && eCharSet      == rCmp.eCharSet
-                                && aStrFont      == rCmp.aStrFont
-                                && bFixedWidth   == rCmp.bFixedWidth;
+                                   nFieldSepCode    == rCmp.nFieldSepCode
+                                && nTextSepCode     == rCmp.nTextSepCode
+                                && eCharSet         == rCmp.eCharSet
+                                && aStrFont         == rCmp.aStrFont
+                                && bFixedWidth      == rCmp.bFixedWidth
+                                && bSaveAsShown     == rCmp.bSaveAsShown;
                         }
     String  BuildString() const;
 
@@ -165,6 +129,7 @@ public:
     String      aStrFont;
     CharSet     eCharSet;
     BOOL        bFixedWidth;
+    BOOL        bSaveAsShown;
 };
 
 
