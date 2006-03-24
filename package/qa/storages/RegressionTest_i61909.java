6c4e8ba46b4c0ec2c07be8bcfe88463fb273091c
Oliver Bolte <obo@openoffice.org>
2006-03-24 12:19:38 +0000

    INTEGRATION: CWS fwk33 (1.1.2); FILE ADDED 2006/03/14 11:17:02 mav 1.1.2.1: #i61909# new regression test

---
+++ package/qa/storages/RegressionTest_i61909.java
@@ -0,0 +1,167 @@
+package complex.storages;
+
+import java.net.URI;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+
+import com.sun.star.uno.XInterface;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.lang.XSingleServiceFactory;
+
+import com.sun.star.bridge.XUnoUrlResolver;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XInterface;
+import com.sun.star.io.XStream;
+import com.sun.star.io.XInputStream;
+
+import com.sun.star.embed.*;
+
+import share.LogWriter;
+import complex.storages.TestHelper;
+import complex.storages.StorageTest;
+
+public class RegressionTest_i61909 implements StorageTest {
+
+    XMultiServiceFactory m_xMSF;
+    XSingleServiceFactory m_xStorageFactory;
+    TestHelper m_aTestHelper;
+
+    public RegressionTest_i61909( XMultiServiceFactory xMSF, XSingleServiceFactory xStorageFactory, LogWriter aLogWriter )
+    {
+        m_xMSF = xMSF;
+        m_xStorageFactory = xStorageFactory;
+        m_aTestHelper = new TestHelper( aLogWriter, "RegressionTest_i61909: " );
+    }
+
+    public boolean test()
+    {
+        try
+        {
+            String sTempFileURL = m_aTestHelper.CreateTempFile( m_xMSF );
+            if ( sTempFileURL == null || sTempFileURL == "" )
+            {
+                m_aTestHelper.Error( "No valid temporary file was created!" );
+                return false;
+            }
+
+            // create storage based on the temporary stream
+            Object pArgs[] = new Object[2];
+            pArgs[0] = (Object) sTempFileURL;
+            pArgs[1] = new Integer( ElementModes.WRITE );
+
+            Object oTempStorage = m_xStorageFactory.createInstanceWithArguments( pArgs );
+            XStorage xTempStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oTempStorage );
+            if ( xTempStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            byte pBytes[] = new byte[36000];
+            for ( int nInd = 0; nInd < 36000; nInd++ )
+                pBytes[nInd] = (byte)( nInd % 128 );
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xTempStorage, "SubStream1", "MediaType1", true, pBytes ) )
+                return false;
+
+            // open a new substorage
+            XStorage xTempSubStorage = m_aTestHelper.openSubStorage( xTempStorage,
+                                                                        "SubStorage1",
+                                                                        ElementModes.WRITE );
+            if ( xTempSubStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create substorage!" );
+                return false;
+            }
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xTempSubStorage, "SubStream2", "MediaType2", true, pBytes ) )
+                return false;
+
+            // set "MediaType" property for storages and check that "IsRoot" and "OpenMode" properties are set correctly
+            if ( !m_aTestHelper.setStorageTypeAndCheckProps( xTempStorage,
+                                                            "MediaType3",
+                                                            true,
+                                                            ElementModes.WRITE ) )
+                return false;
+
+            // set "MediaType" property for storages and check that "IsRoot" and "OpenMode" properties are set correctly
+            if ( !m_aTestHelper.setStorageTypeAndCheckProps( xTempSubStorage,
+                                                            "MediaType4",
+                                                            false,
+                                                            ElementModes.WRITE ) )
+                return false;
+
+            // commit substorage first
+            if ( !m_aTestHelper.commitStorage( xTempSubStorage ) )
+                return false;
+
+            // commit the root storage so the contents must be stored now
+            if ( !m_aTestHelper.commitStorage( xTempStorage ) )
+                return false;
+
+            // dispose used storage to free resources
+            if ( !m_aTestHelper.disposeStorage( xTempStorage ) )
+                return false;
+
+            // ================================================
+            // now reopen the storage, and insert a new stream
+            // ================================================
+
+            Object oStep2TempStorage = m_xStorageFactory.createInstanceWithArguments( pArgs );
+            XStorage xStep2TempStorage = (XStorage) UnoRuntime.queryInterface( XStorage.class, oStep2TempStorage );
+            if ( xStep2TempStorage == null )
+            {
+                m_aTestHelper.Error( "Can't create temporary storage representation!" );
+                return false;
+            }
+
+            // open a new substream, set "MediaType" and "Compressed" properties to it and write some bytes
+            if ( !m_aTestHelper.WriteBytesToSubstream( xStep2TempStorage, "SubStream3", "MediaType5", true, pBytes ) )
+                return false;
+
+            // commit the root storage so the contents must be stored now
+            if ( !m_aTestHelper.commitStorage( xStep2TempStorage ) )
+                return false;
+
+            // dispose used storage to free resources
+            if ( !m_aTestHelper.disposeStorage( xStep2TempStorage ) )
+                return false;
+
+            // ================================================
+            // now access the stream using ZipInputStream
+            // ================================================
+
+            URI aUri = new URI( sTempFileURL );
+            File aFile = new File( aUri );
+            FileInputStream aFileStream = new FileInputStream( aFile );
+            ZipInputStream aZipStream = new ZipInputStream( aFileStream );
+
+            ZipEntry aEntry;
+            int nNumber = 0;
+            m_aTestHelper.Message( "Available entries:" );
+            while ( ( aEntry = aZipStream.getNextEntry() ) != null )
+            {
+                nNumber++;
+                m_aTestHelper.Message( aEntry.getName() );
+            }
+
+            if ( nNumber != 6 )
+            {
+                m_aTestHelper.Error( "Wrong number of entries: " + nNumber + ", Expected: 6" );
+                return false;
+            }
+
+            return true;
+        }
+        catch( Exception e )
+        {
+            m_aTestHelper.Error( "Exception: " + e );
+            return false;
+        }
+    }
+}
+
