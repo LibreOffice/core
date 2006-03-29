361be0e46de22da8af7d3e53a0e14229f55e96ea
Oliver Bolte <obo@openoffice.org>
2006-03-29 11:15:23 +0000

    INTEGRATION: CWS dba203a (1.7.76); FILE MERGED 2006/03/20 13:11:15 fs 1.7.76.1: #i10000#

--- connectivity/source/drivers/evoab/LConnection.cxx
+++ connectivity/source/drivers/evoab/LConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LConnection.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:43:29 $
+ *  last change: $Author: obo $ $Date: 2006-03-29 12:15:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,6 @@
 #endif
 #ifndef _CONNECTIVITY_RESOURCE_HRC_
 #include "Resource.hrc"
-#endif
-#ifndef _CONNECTIVITY_MODULECONTEXT_HXX_
-#include "ModuleContext.hxx"
 #endif
 #ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
