5d6419f8e6d0ba4283ad290d1e21dfd9e41742bf
Oliver Bolte <obo@openoffice.org>
2006-03-29 11:16:30 +0000

    INTEGRATION: CWS dba203a (1.11.74); FILE MERGED 2006/03/17 15:41:10 fs 1.11.74.1: ModuleContext is obsolete (during #i60163#)

--- connectivity/source/drivers/flat/EConnection.cxx
+++ connectivity/source/drivers/flat/EConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EConnection.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:59:50 $
+ *  last change: $Author: obo $ $Date: 2006-03-29 12:16:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,6 @@
 #endif
 #ifndef _CONNECTIVITY_RESOURCE_HRC_
 #include "Resource.hrc"
-#endif
-#ifndef _CONNECTIVITY_MODULECONTEXT_HXX_
-#include "ModuleContext.hxx"
 #endif
 #ifndef _CONNECTIVITY_FLAT_ODRIVER_HXX_
 #include "flat/EDriver.hxx"
