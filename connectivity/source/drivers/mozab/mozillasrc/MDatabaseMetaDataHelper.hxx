e8c9ba38d675b5ab89851089e3dc274a12f8c06f
Oliver Bolte <obo@openoffice.org>
2006-03-29 11:18:37 +0000

    INTEGRATION: CWS dba203a (1.7.76); FILE MERGED 2006/03/17 15:37:57 fs 1.7.76.1: #i60163# localized error messages

--- connectivity/source/drivers/mozab/mozillasrc/MDatabaseMetaDataHelper.hxx
+++ connectivity/source/drivers/mozab/mozillasrc/MDatabaseMetaDataHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MDatabaseMetaDataHelper.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:27:27 $
+ *  last change: $Author: obo $ $Date: 2006-03-29 12:18:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,49 +36,13 @@
 #ifndef _CONNECTIVITY_MAB_DATABASEMETADATAHELPER_HXX_
 #define _CONNECTIVITY_MAB_DATABASEMETADATAHELPER_HXX_
 
- /*
-#ifndef _COM_SUN_STAR_SDBC_XRESULTSET_HPP_
-#include <com/sun/star/sdbc/XResultSet.hpp>
+#ifndef CONNECITIVITY_MOZAB_ERROR_RESOURCE_HXX
+#include "MErrorResource.hxx"
 #endif
-#ifndef _COM_SUN_STAR_SDBC_XROW_HPP_
-#include <com/sun/star/sdbc/XRow.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XRESULTSETMETADATASUPPLIER_HPP_
-#include <com/sun/star/sdbc/XResultSetMetaDataSupplier.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XCLOSEABLE_HPP_
-#include <com/sun/star/sdbc/XCloseable.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XCOLUMNLOCATE_HPP_
-#include <com/sun/star/sdbc/XColumnLocate.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UTIL_XCANCELLABLE_HPP_
-#include <com/sun/star/util/XCancellable.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XWARNINGSSUPPLIER_HPP_
-#include <com/sun/star/sdbc/XWarningsSupplier.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XRESULTSETUPDATE_HPP_
-#include <com/sun/star/sdbc/XResultSetUpdate.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XROWUPDATE_HPP_
-#include <com/sun/star/sdbc/XRowUpdate.hpp>
-#endif
-#ifndef _CPPUHELPER_COMPBASE7_HXX_
-#include <cppuhelper/compbase7.hxx>
-#endif
-*/
+
 #ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
 #include <comphelper/proparrhlp.hxx>
 #endif
- /*
-#ifndef _CONNECTIVITY_FILE_ASTATEMENT_HXX_
-#include "file/FStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_COMMONTOOLS_HXX_
-#include "connectivity/CommonTools.hxx"
-#endif
-*/
 #ifndef _COMPHELPER_PROPERTYCONTAINER_HXX_
 #include <comphelper/propertycontainer.hxx>
 #endif
@@ -97,15 +61,21 @@
 #include <com/sun/star/mozilla/MozillaProductType.hpp>
 #endif
 
-
 namespace connectivity
 {
     namespace mozab
     {
-        class MDatabaseMetaDataHelper
+        class MDatabaseMetaDataHelper : public ErrorResourceAccess
         {
+        private:
+            sal_Bool                                        m_bProfileExists ;
+            ::std::vector< ::rtl::OUString >                m_aTableNames;
+            ::std::vector< ::rtl::OUString >                m_aTableTypes;
+            ::com::sun::star::mozilla::MozillaProductType   m_ProductType;
+            ::rtl::OUString                                 m_ProfileName;
+
         public:
-            MDatabaseMetaDataHelper( );
+            MDatabaseMetaDataHelper();
             ~MDatabaseMetaDataHelper();
 
             //
@@ -118,19 +88,11 @@ namespace connectivity
                                 const ::com::sun::star::uno::Sequence< ::rtl::OUString >& types,
                                 ODatabaseMetaDataResultSet::ORows& _rRows);
 
-            const ::rtl::OUString& getErrorString() { return m_aErrorString; }
-
             sal_Bool   testLDAPConnection( OConnection* _pCon );
             sal_Bool   NewAddressBook( OConnection* _pCon,const ::rtl::OUString & aTableName);
 
         private:
-            sal_Bool                            m_bProfileExists ;
-            ::std::vector< ::rtl::OUString >    m_aTableNames;
-            ::std::vector< ::rtl::OUString >    m_aTableTypes;
-            ::rtl::OUString                     m_aErrorString;
             void setAbSpecificError( OConnection* _pCon, sal_Bool bGivenURI );
-            ::com::sun::star::mozilla::MozillaProductType m_ProductType;
-            ::rtl::OUString                               m_ProfileName;
         };
     }
 
