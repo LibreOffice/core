101dcaf8992880b42d1ef1cb79b4a43424c1294a
Vladimir Glazounov <vg@openoffice.org>
2006-03-31 08:51:43 +0000

    INTEGRATION: CWS swstability01 (1.6.218); FILE MERGED 2006/03/16 10:43:17 ama 1.6.218.3: #i55547# unshared/shared header/footer causes more problems 2006/03/14 08:52:56 ama 1.6.218.2: Fix #i55547#i58359# Undo of page descriptions with header/footer 2006/03/13 15:33:51 ama 1.6.218.1: Fix #i55547#: Duplication of header/footer content causes undo crashes

--- sw/source/core/undo/SwUndoPageDesc.cxx
+++ sw/source/core/undo/SwUndoPageDesc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwUndoPageDesc.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:16:18 $
+ *  last change: $Author: vg $ $Date: 2006-03-31 09:51:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,8 +41,109 @@
 #include <SwRewriter.hxx>
 #include <undobj.hxx>
 #include <comcore.hrc>
-#include <headerfooterhelper.hxx>
+#include <fmtcntnt.hxx>
+#include <fmthdft.hxx>
 
+#ifdef DEBUG
+#include <ndindex.hxx>
+#endif
+
+
+#ifdef DEBUG
+// Pure debug help function to have a quick look at the header/footer attributes.
+void DebugHeaderFooterContent( const SwPageDesc& rPageDesc )
+{
+    ULONG nHeaderMaster = ULONG_MAX;
+    ULONG nHeaderLeft = ULONG_MAX;
+    ULONG nFooterMaster = ULONG_MAX;
+    ULONG nFooterLeft = ULONG_MAX;
+    int nHeaderCount = 0;
+    int nLeftHeaderCount = 0;
+    int nFooterCount = 0;
+    int nLeftFooterCount = 0;
+    bool bSharedHeader = false;
+    bool bSharedFooter = false;
+
+    SwFmtHeader& rHead = (SwFmtHeader&)rPageDesc.GetMaster().GetHeader();
+    SwFmtFooter& rFoot = (SwFmtFooter&)rPageDesc.GetMaster().GetFooter();
+    SwFmtHeader& rLeftHead = (SwFmtHeader&)rPageDesc.GetLeft().GetHeader();
+    SwFmtFooter& rLeftFoot = (SwFmtFooter&)rPageDesc.GetLeft().GetFooter();
+    if( rHead.IsActive() )
+    {
+        SwFrmFmt* pHeaderFmt = rHead.GetHeaderFmt();
+        if( pHeaderFmt )
+        {
+            SwClientIter aIter( *pHeaderFmt );
+            SwClient *pLast = aIter.GoStart();
+            if( pLast )
+                do
+                {
+                    ++nHeaderCount;
+                } while( 0 != ( pLast = aIter++ ));
+            const SwFmtCntnt* pCntnt = &pHeaderFmt->GetCntnt();
+            if( pCntnt->GetCntntIdx() )
+                nHeaderMaster = pCntnt->GetCntntIdx()->GetIndex();
+            else
+                nHeaderMaster = 0;
+        }
+        bSharedHeader = rPageDesc.IsHeaderShared();
+        SwFrmFmt* pLeftHeaderFmt = rLeftHead.GetHeaderFmt();
+        if( pLeftHeaderFmt )
+        {
+            SwClientIter aIter( *pLeftHeaderFmt );
+            SwClient *pLast = aIter.GoStart();
+            if( pLast )
+                do
+                {
+                    ++nLeftHeaderCount;
+                } while( 0 != ( pLast = aIter++ ));
+            const SwFmtCntnt* pLeftCntnt = &pLeftHeaderFmt->GetCntnt();
+            if( pLeftCntnt->GetCntntIdx() )
+                nHeaderLeft = pLeftCntnt->GetCntntIdx()->GetIndex();
+            else
+                nHeaderLeft = 0;
+        }
+    }
+    if( rFoot.IsActive() )
+    {
+        SwFrmFmt* pFooterFmt = rFoot.GetFooterFmt();
+        if( pFooterFmt )
+        {
+            SwClientIter aIter( *pFooterFmt );
+            SwClient *pLast = aIter.GoStart();
+            if( pLast )
+                do
+                {
+                    ++nFooterCount;
+                } while( 0 != ( pLast = aIter++ ));
+            const SwFmtCntnt* pCntnt = &pFooterFmt->GetCntnt();
+            if( pCntnt->GetCntntIdx() )
+                nFooterMaster = pCntnt->GetCntntIdx()->GetIndex();
+            else
+                nFooterMaster = 0;
+        }
+        bSharedFooter = rPageDesc.IsFooterShared();
+        SwFrmFmt* pLeftFooterFmt = rLeftFoot.GetFooterFmt();
+        if( pLeftFooterFmt )
+        {
+            SwClientIter aIter( *pLeftFooterFmt );
+            SwClient *pLast = aIter.GoStart();
+            if( pLast )
+                do
+                {
+                    ++nLeftFooterCount;
+                } while( 0 != ( pLast = aIter++ ));
+            const SwFmtCntnt* pLeftCntnt = &pLeftFooterFmt->GetCntnt();
+            if( pLeftCntnt->GetCntntIdx() )
+                nFooterLeft = pLeftCntnt->GetCntntIdx()->GetIndex();
+            else
+                nFooterLeft = 0;
+        }
+    }
+    int i = 0;
+    ++i; // To set a breakpoint
+}
+#endif
 
 SwUndoPageDesc::SwUndoPageDesc(const SwPageDesc & _aOld,
                                const SwPageDesc & _aNew,
@@ -50,22 +151,168 @@ SwUndoPageDesc::SwUndoPageDesc(const SwPageDesc & _aOld,
     : SwUndo(_aOld.GetName() != _aNew.GetName() ?
              UNDO_RENAME_PAGEDESC
              :UNDO_CHANGE_PAGEDESC),
-      aOld(_aOld, _pDoc), aNew(_aNew, _pDoc), pDoc(_pDoc)
+      aOld(_aOld, _pDoc), aNew(_aNew, _pDoc), pDoc(_pDoc), bExchange( false )
 {
     ASSERT(0 != pDoc, "no document?");
 
-    // The headers/footers from the two SwPageDesc saved by this undo
-    // are still in the main document array. In order to preserve the
-    // absolute indices, we need to move them into the undo nodes
-    // array.
-    // #i46909# no undo if header or footer changed
-    //    SwNodes& rNodes = * const_cast<SwNodes*>( pDoc->GetUndoNds() );
-    //    saveHeaderFooterNodes( (SwPageDesc&)aOld, rNodes );
-    //    saveHeaderFooterNodes( (SwPageDesc&)aNew, rNodes );
+#ifdef DEBUG
+    DebugHeaderFooterContent( (SwPageDesc&)aOld );
+    DebugHeaderFooterContent( (SwPageDesc&)aNew );
+#endif
+
+    /*
+    The page description changes.
+    If there are no header/footer content changes like header on/off or change from shared content
+    to unshared etc., there is no reason to duplicate the content nodes (Crash i55547)
+    But this happens, this Undo Ctor will destroy the unnecessary duplicate and manipulate the
+    content pointer of the both page descriptions.
+    */
+    SwPageDesc &rOldDesc = (SwPageDesc&)aOld;
+    SwPageDesc &rNewDesc = (SwPageDesc&)aNew;
+    const SwFmtHeader& rOldHead = rOldDesc.GetMaster().GetHeader();
+    const SwFmtHeader& rNewHead = rNewDesc.GetMaster().GetHeader();
+    const SwFmtFooter& rOldFoot = rOldDesc.GetMaster().GetFooter();
+    const SwFmtFooter& rNewFoot = rNewDesc.GetMaster().GetFooter();
+    // If header/footer will be activated or deactivated, this undo will not work.
+    bExchange = ( rOldHead.IsActive() == rNewHead.IsActive() ) &&
+                      ( rOldFoot.IsActive() == rNewFoot.IsActive() );
+    if( rOldHead.IsActive() && ( rOldDesc.IsHeaderShared() != rNewDesc.IsHeaderShared() ) )
+        bExchange = false;
+    if( rOldFoot.IsActive() && ( rOldDesc.IsFooterShared() != rNewDesc.IsFooterShared() ) )
+        bExchange = false;
+    if( bExchange )
+    {
+        if( rNewHead.IsActive() )
+        {
+            SwFrmFmt* pFormat = new SwFrmFmt( *rNewHead.GetHeaderFmt() );
+            // The Ctor of this object will remove the duplicate!
+            SwFmtHeader aFmtHeader( pFormat );
+            if( !rNewDesc.IsHeaderShared() )
+            {
+                pFormat = new SwFrmFmt( *rNewDesc.GetLeft().GetHeader().GetHeaderFmt() );
+                // The Ctor of this object will remove the duplicate!
+                SwFmtHeader aFmtHeader( pFormat );
+            }
+        }
+        // Same procedure for footers...
+        if( rNewFoot.IsActive() )
+        {
+            SwFrmFmt* pFormat = new SwFrmFmt( *rNewFoot.GetFooterFmt() );
+            // The Ctor of this object will remove the duplicate!
+            SwFmtFooter aFmtFooter( pFormat );
+            if( !rNewDesc.IsFooterShared() )
+            {
+                pFormat = new SwFrmFmt( *rNewDesc.GetLeft().GetFooter().GetFooterFmt() );
+                // The Ctor of this object will remove the duplicate!
+                SwFmtFooter aFmtFooter( pFormat );
+            }
+        }
+
+        // After this exchange method the old page description will point to zero,
+        // the new one will point to the node position of the original content nodes.
+        ExchangeContentNodes( (SwPageDesc&)aOld, (SwPageDesc&)aNew );
+#ifdef DEBUG
+        DebugHeaderFooterContent( (SwPageDesc&)aOld );
+        DebugHeaderFooterContent( (SwPageDesc&)aNew );
+#endif
+    }
 }
 
 SwUndoPageDesc::~SwUndoPageDesc()
 {
+}
+
+
+void SwUndoPageDesc::ExchangeContentNodes( SwPageDesc& rSource, SwPageDesc &rDest )
+{
+    ASSERT( bExchange, "You shouldn't do that." );
+    const SwFmtHeader& rDestHead = rDest.GetMaster().GetHeader();
+    const SwFmtHeader& rSourceHead = rSource.GetMaster().GetHeader();
+    if( rDestHead.IsActive() )
+    {
+        // Let the destination page descrition point to the source node position,
+        // from now on this descriptor is responsible for the content nodes!
+        const SfxPoolItem* pItem;
+        rDest.GetMaster().GetAttrSet().GetItemState( RES_HEADER, FALSE, &pItem );
+        SfxPoolItem *pNewItem = pItem->Clone();
+        SwFrmFmt* pNewFmt = ((SwFmtHeader*)pNewItem)->GetHeaderFmt();
+#ifdef DEBUG
+        const SwFmtCntnt& rSourceCntnt = rSourceHead.GetHeaderFmt()->GetCntnt();
+        const SwFmtCntnt& rDestCntnt = rDestHead.GetHeaderFmt()->GetCntnt();
+#endif
+        pNewFmt->SetAttr( rSourceHead.GetHeaderFmt()->GetCntnt() );
+        delete pNewItem;
+
+        // Let the source page description point to zero node position,
+        // it loses the responsible and can be destroyed without removing the content nodes.
+        rSource.GetMaster().GetAttrSet().GetItemState( RES_HEADER, FALSE, &pItem );
+        pNewItem = pItem->Clone();
+        pNewFmt = ((SwFmtHeader*)pNewItem)->GetHeaderFmt();
+        pNewFmt->SetAttr( SwFmtCntnt() );
+        delete pNewItem;
+
+        if( !rDest.IsHeaderShared() )
+        {
+            // Same procedure for unshared header..
+            const SwFmtHeader& rSourceLeftHead = rSource.GetLeft().GetHeader();
+            rDest.GetLeft().GetAttrSet().GetItemState( RES_HEADER, FALSE, &pItem );
+            pNewItem = pItem->Clone();
+            pNewFmt = ((SwFmtHeader*)pNewItem)->GetHeaderFmt();
+#ifdef DEBUG
+            const SwFmtCntnt& rSourceCntnt = rSourceLeftHead.GetHeaderFmt()->GetCntnt();
+            const SwFmtCntnt& rDestCntnt = rDest.GetLeft().GetHeader().GetHeaderFmt()->GetCntnt();
+#endif
+            pNewFmt->SetAttr( rSourceLeftHead.GetHeaderFmt()->GetCntnt() );
+            delete pNewItem;
+            rSource.GetLeft().GetAttrSet().GetItemState( RES_HEADER, FALSE, &pItem );
+            pNewItem = pItem->Clone();
+            pNewFmt = ((SwFmtHeader*)pNewItem)->GetHeaderFmt();
+            pNewFmt->SetAttr( SwFmtCntnt() );
+            delete pNewItem;
+        }
+    }
+    // Same procedure for footers...
+    const SwFmtFooter& rDestFoot = rDest.GetMaster().GetFooter();
+    const SwFmtFooter& rSourceFoot = rSource.GetMaster().GetFooter();
+    if( rDestFoot.IsActive() )
+    {
+        const SfxPoolItem* pItem;
+        rDest.GetMaster().GetAttrSet().GetItemState( RES_FOOTER, FALSE, &pItem );
+        SfxPoolItem *pNewItem = pItem->Clone();
+        SwFrmFmt *pNewFmt = ((SwFmtFooter*)pNewItem)->GetFooterFmt();
+        pNewFmt->SetAttr( rSourceFoot.GetFooterFmt()->GetCntnt() );
+        delete pNewItem;
+
+#ifdef DEBUG
+        const SwFmtCntnt& rFooterSourceCntnt = rSourceFoot.GetFooterFmt()->GetCntnt();
+        const SwFmtCntnt& rFooterDestCntnt = rDestFoot.GetFooterFmt()->GetCntnt();
+#endif
+        rSource.GetMaster().GetAttrSet().GetItemState( RES_FOOTER, FALSE, &pItem );
+        pNewItem = pItem->Clone();
+        pNewFmt = ((SwFmtFooter*)pNewItem)->GetFooterFmt();
+        pNewFmt->SetAttr( SwFmtCntnt() );
+        delete pNewItem;
+
+        if( !rDest.IsFooterShared() )
+        {
+            const SwFmtFooter& rSourceLeftFoot = rSource.GetLeft().GetFooter();
+#ifdef DEBUG
+            const SwFmtCntnt& rFooterSourceCntnt = rSourceLeftFoot.GetFooterFmt()->GetCntnt();
+            const SwFmtCntnt& rFooterDestCntnt =
+                rDest.GetLeft().GetFooter().GetFooterFmt()->GetCntnt();
+#endif
+            rDest.GetLeft().GetAttrSet().GetItemState( RES_FOOTER, FALSE, &pItem );
+            pNewItem = pItem->Clone();
+            pNewFmt = ((SwFmtFooter*)pNewItem)->GetFooterFmt();
+            pNewFmt->SetAttr( rSourceLeftFoot.GetFooterFmt()->GetCntnt() );
+            delete pNewItem;
+            rSource.GetLeft().GetAttrSet().GetItemState( RES_FOOTER, FALSE, &pItem );
+            pNewItem = pItem->Clone();
+            pNewFmt = ((SwFmtFooter*)pNewItem)->GetFooterFmt();
+            pNewFmt->SetAttr( SwFmtCntnt() );
+            delete pNewItem;
+        }
+    }
 }
 
 void SwUndoPageDesc::Undo(SwUndoIter & rIt)
@@ -73,6 +320,10 @@ void SwUndoPageDesc::Undo(SwUndoIter & rIt)
     BOOL bUndo = pDoc->DoesUndo();
 
     pDoc->DoUndo(FALSE);
+
+    // Move (header/footer)content node responsibility from new page descriptor to old one again.
+    if( bExchange )
+        ExchangeContentNodes( (SwPageDesc&)aNew, (SwPageDesc&)aOld );
     pDoc->ChgPageDesc(aOld.GetName(), aOld);
     pDoc->DoUndo(bUndo);
 }
@@ -82,13 +333,16 @@ void SwUndoPageDesc::Redo(SwUndoIter & rIt)
     BOOL bUndo = pDoc->DoesUndo();
 
     pDoc->DoUndo(FALSE);
+
+    // Move (header/footer)content node responsibility from old page descriptor to new one again.
+    if( bExchange )
+        ExchangeContentNodes( (SwPageDesc&)aOld, (SwPageDesc&)aNew );
     pDoc->ChgPageDesc(aNew.GetName(), aNew);
     pDoc->DoUndo(bUndo);
 }
 
 void SwUndoPageDesc::Repeat(SwUndoIter & rIt)
 {
-    Redo(rIt);
 }
 
 SwRewriter SwUndoPageDesc::GetRewriter() const
