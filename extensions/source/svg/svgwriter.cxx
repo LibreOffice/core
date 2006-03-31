59ef5473e8d8c31a63d7aa77ca395f91cb70908e
Vladimir Glazounov <vg@openoffice.org>
2006-03-31 08:25:05 +0000

    INTEGRATION: CWS long2int (1.6.36); FILE MERGED 2005/10/26 18:07:12 kendy 1.6.36.1: #i56715# Trivial long/ULONG -> sal_Int32/sal_uInt32 patches extracted from ooo64bit02 CWS.

--- extensions/source/svg/svgwriter.cxx
+++ extensions/source/svg/svgwriter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svgwriter.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:42:10 $
+ *  last change: $Author: vg $ $Date: 2006-03-31 09:25:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,7 +54,7 @@ protected:
     virtual void            _ExportAutoStyles() {}
     virtual void            _ExportContent() {}
     virtual void            _ExportMasterStyles() {}
-    virtual ULONG           exportDoc( enum ::xmloff::token::XMLTokenEnum eClass ) { return 0; }
+    virtual sal_uInt32      exportDoc( enum ::xmloff::token::XMLTokenEnum eClass ) { return 0; }
 
 public:
 
