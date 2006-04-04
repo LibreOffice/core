e45e7ae5f9c0107438e196e600f22a42aa90e4c3
Vladimir Glazounov <vg@openoffice.org>
2006-04-04 08:06:44 +0000

    INTEGRATION: CWS pdfexportimprove (1.11.94); FILE MERGED 2006/03/09 09:50:11 pl 1.11.94.1: #i61139# viewer preferences

--- filter/source/pdf/makefile.mk
+++ filter/source/pdf/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 21:48:41 $
+#   last change: $Author: vg $ $Date: 2006-04-04 09:06:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -68,6 +68,7 @@ SHL1STDLIBS=\
     $(TKLIB)			\
     $(VCLLIB)			\
     $(SVLLIB)			\
+    $(SFX2LIB)			\
     $(UNOTOOLSLIB)		\
     $(TOOLSLIB)			\
     $(COMPHELPERLIB)	\
