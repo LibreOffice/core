19b059abd876d6b7dc2f10c3ef227032e4aedc1b
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 13:17:11 +0000

    INTEGRATION: CWS internatiodel (1.10.30); FILE MERGED 2006/02/10 19:28:37 er 1.10.30.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- scaddins/source/datefunc/datefunc.cxx
+++ scaddins/source/datefunc/datefunc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datefunc.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:23:27 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 14:17:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,9 +54,6 @@
 #endif
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-#ifndef _TOOLS_ISOLANG_HXX
-#include <tools/isolang.hxx>
 #endif
 #ifndef _TOOLS_RESMGR_HXX
 #include <tools/resmgr.hxx>
