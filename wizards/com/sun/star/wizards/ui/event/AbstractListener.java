6dfb08c17bcee785a10a69fa4550eb98e6700f04
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:30:35 +0000

    INTEGRATION: CWS dbwizardpp3 (1.6.44); FILE MERGED 2006/03/27 10:47:53 tv 1.6.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/ui/event/AbstractListener.java
+++ wizards/com/sun/star/wizards/ui/event/AbstractListener.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AbstractListener.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:50:03 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:30:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
