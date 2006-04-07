ead3da7d70b9979349e5ea12535f228bdca4c021
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:23:17 +0000

    INTEGRATION: CWS dbwizardpp3 (1.7.44); FILE MERGED 2006/03/27 10:47:51 tv 1.7.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/ui/FieldSelection.java
+++ wizards/com/sun/star/wizards/ui/FieldSelection.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FieldSelection.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:47:12 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:23:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
