5235cf12857162f63ce3f3c1a53d1bcf9aee118d
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:06:12 +0000

    INTEGRATION: CWS internatiodel (1.9.188); FILE MERGED 2006/02/10 19:30:52 er 1.9.188.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- sw/inc/breakit.hxx
+++ sw/inc/breakit.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: breakit.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:34:20 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:06:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,8 +39,8 @@
 #ifndef _SOLAR_H
 #include <tools/solar.h>
 #endif
-#ifndef _LANG_HXX //autogen
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
