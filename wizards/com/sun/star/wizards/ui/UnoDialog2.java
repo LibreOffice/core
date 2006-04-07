5cb0f77140748cd53a92a4c93781771ac924e757
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:27:13 +0000

    INTEGRATION: CWS dbwizardpp3 (1.9.44); FILE MERGED 2006/03/27 10:47:52 tv 1.9.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/ui/UnoDialog2.java
+++ wizards/com/sun/star/wizards/ui/UnoDialog2.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoDialog2.java,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:49:13 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:27:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
