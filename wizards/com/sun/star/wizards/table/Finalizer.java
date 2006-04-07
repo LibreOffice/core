809f4babf86fef77e3b180712df09572e637b65b
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:54:09 +0000

    INTEGRATION: CWS dbwizardpp3 (1.6.44); FILE MERGED 2006/03/27 10:47:37 tv 1.6.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/table/Finalizer.java
+++ wizards/com/sun/star/wizards/table/Finalizer.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Finalizer.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:40:30 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:54:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
