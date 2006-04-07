bac4e40b16c36ea3ef2a66cc7ebeecaa9a1cac3c
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:31:08 +0000

    INTEGRATION: CWS internatiodel (1.9.66); FILE MERGED 2006/01/19 17:53:13 er 1.9.66.2: RESYNC: (1.9-1.10); FILE MERGED 2005/06/24 13:12:01 er 1.9.66.1: #i50205# get rid of class International

--- vcl/source/control/longcurr.cxx
+++ vcl/source/control/longcurr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: longcurr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:47:53 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:31:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -375,12 +375,6 @@ void LongCurrencyFormatter::ImplLoadRes( const ResId& rResId )
     if ( NUMERICFORMATTER_STRICTFORMAT & nMask )
         SetStrictFormat(  (BOOL)pMgr->ReadShort() );
 
-    if ( NUMERICFORMATTER_I12 & nMask )
-    {
-        DBG_ERROR( "MT: Removed class International - missing ResCTOR!" );
-        International( ResId( (RSHEADER_TYPE *)pMgr->GetClass() ) );
-        pMgr->Increment( pMgr->GetObjSize( (RSHEADER_TYPE *)pMgr->GetClass() ) );
-    }
     if ( NUMERICFORMATTER_DECIMALDIGITS & nMask )
         SetDecimalDigits( pMgr->ReadShort() );
 
