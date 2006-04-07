45ba50c8ff0ef061d40277801fe6796b53b8bfca
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 15:22:35 +0000

    INTEGRATION: CWS internatiodel (1.2.878); FILE MERGED 2006/01/19 23:32:45 er 1.2.878.2: RESYNC: (1.2-1.3); FILE MERGED 2005/06/24 13:13:43 er 1.2.878.1: #i50205# get rid of class International

--- sc/inc/unitconv.hxx
+++ sc/inc/unitconv.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unitconv.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:01:40 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 16:22:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,8 +61,6 @@ public:
 
 };
 
-
-class International;
 
 class ScUnitConverter : public StrCollection
 {
