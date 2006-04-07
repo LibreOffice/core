1bdbddaef344479c7b31721e9413ad6599fd677e
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:09:06 +0000

    INTEGRATION: CWS internatiodel (1.6.188); FILE MERGED 2006/02/10 19:30:54 er 1.6.188.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- sw/source/core/doc/sortopt.cxx
+++ sw/source/core/doc/sortopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sortopt.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:18:48 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:09:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,8 +39,8 @@
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
 #endif
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 
 #ifndef _SORTOPT_HXX
