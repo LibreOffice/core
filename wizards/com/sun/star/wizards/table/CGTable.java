78128d8d123639344b5de0fd1ab619847cf38bda
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:53:34 +0000

    INTEGRATION: CWS dbwizardpp3 (1.5.22); FILE MERGED 2006/03/27 10:47:36 tv 1.5.22.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/table/CGTable.java
+++ wizards/com/sun/star/wizards/table/CGTable.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CGTable.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:22:47 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:53:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
