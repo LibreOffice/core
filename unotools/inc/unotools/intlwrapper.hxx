86b46d2923d5ff2b2fc54a37ec7f845df3ed572e
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 15:28:32 +0000

    INTEGRATION: CWS internatiodel (1.3.32); FILE MERGED 2006/02/10 19:32:55 er 1.3.32.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- unotools/inc/unotools/intlwrapper.hxx
+++ unotools/inc/unotools/intlwrapper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: intlwrapper.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:32:25 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 16:28:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,8 +52,8 @@
 #include <unotools/collatorwrapper.hxx>
 #endif
 
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 
 
