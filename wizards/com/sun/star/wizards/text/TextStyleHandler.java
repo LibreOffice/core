b1fcd38742442bbe7bead87e670741e718bde438
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:57:15 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.44); FILE MERGED 2006/03/27 10:47:38 tv 1.4.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/text/TextStyleHandler.java
+++ wizards/com/sun/star/wizards/text/TextStyleHandler.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TextStyleHandler.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:42:42 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:57:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
--- wizards/com/sun/star/wizards/text/TextTableHandler.java
+++ wizards/com/sun/star/wizards/text/TextTableHandler.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TextTableHandler.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:42:59 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:57:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
