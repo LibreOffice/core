ed41fb0cbfdd4d2a93a2992cfcea4eb068742c07
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:18:49 +0000

    INTEGRATION: CWS internatiodel (1.7.92); FILE MERGED 2006/02/10 19:31:00 er 1.7.92.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- sw/source/ui/inc/fontcfg.hxx
+++ sw/source/ui/inc/fontcfg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fontcfg.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2005-11-16 09:51:48 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:18:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,8 +41,8 @@
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
 #endif
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
