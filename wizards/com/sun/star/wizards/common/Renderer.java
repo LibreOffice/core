2dd1cbe62cf46d89e445a05f14896ec1bd3156dc
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:33:19 +0000

    INTEGRATION: CWS dbwizardpp3 (1.3.44); FILE MERGED 2006/03/27 10:47:24 tv 1.3.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/common/Renderer.java
+++ wizards/com/sun/star/wizards/common/Renderer.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Renderer.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:21:11 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:33:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
