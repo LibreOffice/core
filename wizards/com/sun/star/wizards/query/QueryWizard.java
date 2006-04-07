9539d7e64c7871eaac01473ea566c61f47aca629
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:49:44 +0000

    INTEGRATION: CWS dbwizardpp3 (1.8.14); FILE MERGED 2006/03/27 10:47:34 tv 1.8.14.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/query/QueryWizard.java
+++ wizards/com/sun/star/wizards/query/QueryWizard.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: QueryWizard.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-01-26 17:20:38 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:49:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
