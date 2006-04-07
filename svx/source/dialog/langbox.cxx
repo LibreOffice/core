7c1347db9b9cbd59eb14a9d250b55db4d6f1673f
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:59:09 +0000

    INTEGRATION: CWS internatiodel (1.15.242); FILE MERGED 2006/02/10 19:27:48 er 1.15.242.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- svx/source/dialog/langbox.cxx
+++ svx/source/dialog/langbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: langbox.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:25:16 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:59:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,8 +55,8 @@
 #ifndef _SHL_HXX
 #include <tools/shl.hxx>
 #endif
-#ifndef _ISOLANG_HXX
-#include <tools/isolang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #pragma hdrstop
 
