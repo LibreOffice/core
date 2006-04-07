3589984a74f887e33a19c16676d4178e17932e51
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:11:36 +0000

    INTEGRATION: CWS internatiodel (1.48.52); FILE MERGED 2006/02/10 19:23:40 er 1.48.52.3: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId 2006/01/19 15:24:32 er 1.48.52.2: RESYNC: (1.48-1.51); FILE MERGED 2005/06/24 13:07:47 er 1.48.52.1: #i50205# get rid of class International

--- connectivity/source/drivers/flat/ETable.cxx
+++ connectivity/source/drivers/flat/ETable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ETable.cxx,v $
  *
- *  $Revision: 1.51 $
+ *  $Revision: 1.52 $
  *
- *  last change: $Author: obo $ $Date: 2005-12-21 13:16:52 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:11:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -68,9 +68,6 @@
 #ifndef _COMPHELPER_SEQUENCE_HXX_
 #include <comphelper/sequence.hxx>
 #endif
-#ifndef _INTN_HXX //autogen
-#include <tools/intn.hxx>
-#endif
 #ifndef _ZFORLIST_HXX //autogen
 #include <svtools/zforlist.hxx>
 #endif
@@ -96,8 +93,8 @@
 #ifndef _UTL_CONFIGMGR_HXX_
 #include <unotools/configmgr.hxx>
 #endif
-#ifndef _ISOLANG_HXX
-#include <tools/isolang.hxx>
+#ifndef INCLUDED_I18NPOOL_MSLANGID_HXX
+#include <i18npool/mslangid.hxx>
 #endif
 #ifndef _DBHELPER_DBCONVERSION_HXX_
 #include "connectivity/dbconversion.hxx"
@@ -380,11 +377,9 @@ OFlatTable::OFlatTable(sdbcx::OCollection* _pTables,OFlatConnection* _pConnectio
 void OFlatTable::construct()
 {
     Any aValue = ConfigManager::GetDirectConfigProperty(ConfigManager::LOCALE);
-    LanguageType eLanguage = ConvertIsoStringToLanguage(comphelper::getString(aValue),'-');
+    LanguageType eLanguage = MsLangId::convertIsoStringToLanguage(comphelper::getString(aValue),'-');
 
-    String sLanguage, sCountry;
-    ConvertLanguageToIsoNames(eLanguage, sLanguage, sCountry);
-    ::com::sun::star::lang::Locale aAppLocale(sLanguage,sCountry,rtl::OUString());
+    ::com::sun::star::lang::Locale aAppLocale(MsLangId::convertLanguageToLocale(eLanguage));
     Sequence< ::com::sun::star::uno::Any > aArg(1);
     aArg[0] <<= aAppLocale;
 
