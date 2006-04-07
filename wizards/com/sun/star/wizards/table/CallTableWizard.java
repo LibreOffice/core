6b646b18a6d938ca077c7353abaa632b247030a4
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:53:13 +0000

    INTEGRATION: CWS dbwizardpp3 (1.3.44); FILE MERGED 2006/03/27 10:47:36 tv 1.3.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/table/CallTableWizard.java
+++ wizards/com/sun/star/wizards/table/CallTableWizard.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CallTableWizard.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:39:45 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:53:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
