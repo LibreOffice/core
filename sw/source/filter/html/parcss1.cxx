0f1c1509cb2efd4213697b6bda639969e74fde5e
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:13:11 +0000

    INTEGRATION: CWS internatiodel (1.4.764); FILE MERGED 2006/01/20 10:24:39 er 1.4.764.2: RESYNC: (1.4-1.5); FILE MERGED 2005/06/24 13:11:19 er 1.4.764.1: #i50205# get rid of class International

--- sw/source/filter/html/parcss1.cxx
+++ sw/source/filter/html/parcss1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parcss1.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:46:48 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:13:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,9 +47,6 @@
 
 #ifndef _TOOLS_DEBUG_HXX //autogen
 #include <tools/debug.hxx>
-#endif
-#ifndef _INTN_HXX //autogen
-#include <tools/intn.hxx>
 #endif
 #ifndef _SV_SVAPP_HXX //autogen
 #include <vcl/svapp.hxx>
