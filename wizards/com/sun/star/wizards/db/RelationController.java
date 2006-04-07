8a549a94a85f789d928d06dce97e3beabf57f25e
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:36:44 +0000

    INTEGRATION: CWS dbwizardpp3 (1.2.22); FILE MERGED 2006/03/27 10:47:27 tv 1.2.22.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/db/RelationController.java
+++ wizards/com/sun/star/wizards/db/RelationController.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RelationController.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 08:02:54 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:36:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
