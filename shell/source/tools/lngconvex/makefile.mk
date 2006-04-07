fc11ded3a7476e035638ab3d956feb4fa182af58
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 15:35:04 +0000

    INTEGRATION: CWS internatiodel (1.3.32); FILE MERGED 2006/02/14 11:14:10 er 1.3.32.1: #i52115# libi18nisolang

--- shell/source/tools/lngconvex/makefile.mk
+++ shell/source/tools/lngconvex/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:53:00 $
+#   last change: $Author: vg $ $Date: 2006-04-07 16:35:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ APP1OBJS=$(OBJ)$/$(TARGET).obj\
 
 APP1STDLIBS= $(SALLIB)\
             $(TOOLSLIB)\
+            $(I18NISOLANGLIB)\
             $(SOLARLIBDIR)$/atools.lib\
             msvcprt.lib
 
