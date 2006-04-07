fa816036025087f818a924a784b49fe8efe56438
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:49:31 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.44); FILE MERGED 2006/03/27 10:47:34 tv 1.4.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/query/QuerySummary.java
+++ wizards/com/sun/star/wizards/query/QuerySummary.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: QuerySummary.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:35:26 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:49:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
