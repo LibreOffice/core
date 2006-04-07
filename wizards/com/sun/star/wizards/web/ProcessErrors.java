2dca84d2a9822df78d8fcebacd00227ad990b06b
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:41:39 +0000

    INTEGRATION: CWS dbwizardpp3 (1.3.44); FILE MERGED 2006/03/27 10:47:57 tv 1.3.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/web/ProcessErrors.java
+++ wizards/com/sun/star/wizards/web/ProcessErrors.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ProcessErrors.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:57:15 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:41:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
--- wizards/com/sun/star/wizards/web/ProcessStatusRenderer.java
+++ wizards/com/sun/star/wizards/web/ProcessStatusRenderer.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ProcessStatusRenderer.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:57:31 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:41:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
