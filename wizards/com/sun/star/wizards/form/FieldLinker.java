1a1244d858cb6861dd1e9d7c6afdbe6584dd1432
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:41:41 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.22); FILE MERGED 2006/03/27 10:47:30 tv 1.4.22.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/form/FieldLinker.java
+++ wizards/com/sun/star/wizards/form/FieldLinker.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FieldLinker.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:19:50 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:41:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
