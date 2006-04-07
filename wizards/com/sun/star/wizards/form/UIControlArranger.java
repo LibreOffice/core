39d30893db8a6e12172caba5b94e52214c68bcd5
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:44:35 +0000

    INTEGRATION: CWS dbwizardpp3 (1.8.44); FILE MERGED 2006/03/27 10:47:31 tv 1.8.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/form/UIControlArranger.java
+++ wizards/com/sun/star/wizards/form/UIControlArranger.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UIControlArranger.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:32:32 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:44:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
