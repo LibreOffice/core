3eadd1e13627302a0ff3b2836a48c605d1b7bf98
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:44:51 +0000

    INTEGRATION: CWS dbwizardpp3 (1.3.44); FILE MERGED 2006/03/27 10:47:31 tv 1.3.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/form/XCallFormWizard.java
+++ wizards/com/sun/star/wizards/form/XCallFormWizard.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XCallFormWizard.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:32:49 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:44:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
