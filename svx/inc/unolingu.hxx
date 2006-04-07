9f89b832995292f41fc57f2f1c5d6355e3a56a35
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:58:21 +0000

    INTEGRATION: CWS internatiodel (1.11.242); FILE MERGED 2006/02/10 19:27:46 er 1.11.242.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- svx/inc/unolingu.hxx
+++ svx/inc/unolingu.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unolingu.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:26:24 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:58:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,8 @@
 #ifndef _UNO_LINGU_HXX
 #define _UNO_LINGU_HXX
 
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
