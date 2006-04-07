a495915c257a3b2a721e52195212806e4d6c287e
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:23:29 +0000

    INTEGRATION: CWS internatiodel (1.13.46); FILE MERGED 2006/02/10 19:31:14 er 1.13.46.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- forms/source/component/FormattedField.hxx
+++ forms/source/component/FormattedField.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FormattedField.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:40:49 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:23:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,6 @@
 
 #ifndef _FORMS_EDITBASE_HXX_
 #include "EditBase.hxx"
-#endif
-
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
 #endif
 
 #ifndef _COMPHELPER_PROPERTY_MULTIPLEX_HXX_
