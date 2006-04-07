09648a1430b0b3e5909eb0cd52d3a4f1c5a41e06
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:29:26 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.44); FILE MERGED 2006/03/27 10:47:22 tv 1.4.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/agenda/CGAgenda.java
+++ wizards/com/sun/star/wizards/agenda/CGAgenda.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CGAgenda.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:16:28 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:28:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
--- wizards/com/sun/star/wizards/agenda/TemplateConsts.java
+++ wizards/com/sun/star/wizards/agenda/TemplateConsts.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TemplateConsts.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:17:18 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:29:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
