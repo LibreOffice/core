094cd911846e77ff537a9ec956c781cff19d7f30
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:32:25 +0000

    INTEGRATION: CWS dbwizardpp3 (1.8.22); FILE MERGED 2006/03/27 10:47:24 tv 1.8.22.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/common/JavaTools.java
+++ wizards/com/sun/star/wizards/common/JavaTools.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: JavaTools.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:15:09 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:32:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
