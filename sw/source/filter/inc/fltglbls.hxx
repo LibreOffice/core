41ea82b3c16694b8d7b0721bd01ea96bf922c50a
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:14:39 +0000

    INTEGRATION: CWS internatiodel (1.3.188); FILE MERGED 2006/02/10 19:30:57 er 1.3.188.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- sw/source/filter/inc/fltglbls.hxx
+++ sw/source/filter/inc/fltglbls.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fltglbls.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:49:21 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 15:14:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,8 +38,8 @@
 #ifndef _STRING_HXX //autogen
 #include <tools/string.hxx>
 #endif
-#ifndef _LANG_HXX //autogen
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #ifndef _SVARRAY_HXX //autogen
 #include <svtools/svarray.hxx>
