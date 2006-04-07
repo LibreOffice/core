8efb09539a5baa1ce222d32bdba13f705e11472d
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:41:28 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.44); FILE MERGED 2006/03/27 10:47:30 tv 1.4.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/form/DataEntrySetter.java
+++ wizards/com/sun/star/wizards/form/DataEntrySetter.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataEntrySetter.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:29:03 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:41:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
