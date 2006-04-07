fb0cb9185275f576aba3d39c2e2db6485dc5d0be
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:51:07 +0000

    INTEGRATION: CWS dbwizardpp3 (1.4.44); FILE MERGED 2006/03/27 10:47:35 tv 1.4.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/report/GroupFieldHandler.java
+++ wizards/com/sun/star/wizards/report/GroupFieldHandler.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GroupFieldHandler.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:37:25 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:51:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
