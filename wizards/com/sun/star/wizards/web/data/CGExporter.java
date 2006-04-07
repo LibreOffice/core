20030d458dafba1abe32025fac63d4a22c56094f
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:48:28 +0000

    INTEGRATION: CWS dbwizardpp3 (1.3.44); FILE MERGED 2006/03/27 10:48:00 tv 1.3.44.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/web/data/CGExporter.java
+++ wizards/com/sun/star/wizards/web/data/CGExporter.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CGExporter.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:01:00 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:46:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
--- wizards/com/sun/star/wizards/web/data/CGSession.java
+++ wizards/com/sun/star/wizards/web/data/CGSession.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CGSession.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:02:32 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:48:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
