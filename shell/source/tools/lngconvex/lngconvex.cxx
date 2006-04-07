3a86e728d063cb2bec546211a84fa404bb442924
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 15:34:44 +0000

    INTEGRATION: CWS internatiodel (1.13.32); FILE MERGED 2006/02/10 19:33:19 er 1.13.32.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- shell/source/tools/lngconvex/lngconvex.cxx
+++ shell/source/tools/lngconvex/lngconvex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lngconvex.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:52:45 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 16:34:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 #include "osl/file.hxx"
 
 #include "tools/config.hxx"
-#include "tools/isolang.hxx"
+#include "i18npool/mslangid.hxx"
 
 #include <iostream>
 #include <fstream>
@@ -454,7 +454,7 @@ void start_language_section(
 
     std::string lang_section("LANGUAGE ");
 
-    LanguageType ltype = ConvertIsoNamesToLanguage(iso_lang.language(), iso_lang.country());
+    LanguageType ltype = MsLangId::convertIsoNamesToLanguage(iso_lang.language(), iso_lang.country());
 
     char buff[10];
     int primLangID = PRIMARYLANGID(ltype);
