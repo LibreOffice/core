12c776d47086d2c2fc13b8669dc3540817d9a6df
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 14:41:13 +0000

    INTEGRATION: CWS internatiodel (1.2.34); FILE MERGED 2006/02/10 19:31:45 er 1.2.34.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- chart2/source/view/makefile.mk
+++ chart2/source/view/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 01:35:58 $
+#   last change: $Author: vg $ $Date: 2006-04-07 15:41:13 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -84,6 +84,7 @@ SHL1STDLIBS=	$(CHARTTOOLS)		\
                 $(SVXLIB)			\
                 $(TKLIB)			\
                 $(TOOLSLIB) 		\
+                $(I18NISOLANGLIB)   \
                 $(VCLLIB)           \
                 $(SFXLIB)
 
