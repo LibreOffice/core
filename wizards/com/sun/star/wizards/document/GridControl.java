c0d95f8d3ba49e00912659052a6b6242a214f05c
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 11:38:30 +0000

    INTEGRATION: CWS dbwizardpp3 (1.5.22); FILE MERGED 2006/03/27 10:47:28 tv 1.5.22.1: expanded tabs to spaces

--- wizards/com/sun/star/wizards/document/GridControl.java
+++ wizards/com/sun/star/wizards/document/GridControl.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GridControl.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:19:16 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 12:38:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
