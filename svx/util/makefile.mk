df4448f6602eae8f7da46a2fa71c6de37249e923
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 13:09:23 +0000

    INTEGRATION: CWS internatiodel (1.45.242); FILE MERGED 2006/02/10 19:27:58 er 1.45.242.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- svx/util/makefile.mk
+++ svx/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.45 $
+#   $Revision: 1.46 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:24:13 $
+#   last change: $Author: vg $ $Date: 2006-04-07 14:09:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -114,6 +114,7 @@ SHL1STDLIBS= \
             $(SOTLIB) \
             $(UNOTOOLSLIB) \
             $(TOOLSLIB) \
+            $(I18NISOLANGLIB) \
             $(XMLSCRIPTLIB) \
             $(COMPHELPERLIB) \
             $(UCBHELPERLIB)	\
@@ -157,6 +158,7 @@ SHL2STDLIBS= \
             $(SOTLIB) \
             $(UNOTOOLSLIB) \
             $(TOOLSLIB) \
+            $(I18NISOLANGLIB) \
             $(COMPHELPERLIB) \
             $(UCBHELPERLIB)	\
             $(CPPUHELPERLIB)	\
