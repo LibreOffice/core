1ec5d4f68b7c61b668e31e9795580bf768a682b8
Rüdiger Timm <rt@openoffice.org>
2006-04-11 10:09:44 +0000

    #i64261# Fix by bc: remove dependency on ErrorHandler.

--- wizards/com/sun/star/wizards/web/export/FilterExporter.java
+++ wizards/com/sun/star/wizards/web/export/FilterExporter.java
@@ -1,17 +1,43 @@
-/*
- * Created on 17.10.2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+/*************************************************************************
+*
+*  OpenOffice.org - a multi-platform office productivity suite
+*
+*  $RCSfile: FilterExporter.java,v $
+*
+*  $Revision: 1.4 $
+*
+*  last change: $Author: rt $ $Date: 2006-04-11 11:09:44 $
+*
+*  The Contents of this file are made available subject to
+*  the terms of GNU Lesser General Public License Version 2.1.
+*
+*
+*    GNU Lesser General Public License Version 2.1
+*    =============================================
+*    Copyright 2005 by Sun Microsystems, Inc.
+*    901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License version 2.1, as published by the Free Software Foundation.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*    MA  02111-1307  USA
+*
+************************************************************************/
 package com.sun.star.wizards.web.export;
 
 import com.sun.star.io.IOException;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.wizards.common.Properties;
 import com.sun.star.wizards.ui.event.Task;
-import com.sun.star.wizards.web.ErrorHandler;
-import com.sun.star.wizards.web.ProcessErrors;
 import com.sun.star.wizards.web.data.CGDocument;
 import com.sun.star.wizards.web.data.CGExporter;
 
@@ -21,7 +47,7 @@ import com.sun.star.wizards.web.data.CGExporter;
  * An exporter which is configured with a filter name, and
  * uses the specified filter to export documents.
  */
-public class FilterExporter extends AbstractExporter implements ProcessErrors {
+public class FilterExporter extends AbstractExporter  {
 
     protected String filterName;
     protected Properties props = new Properties();
