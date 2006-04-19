c804aa68497b072220b41deb5725f30cbc2213b7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-04-19 12:44:48 +0000

    INTEGRATION: CWS sb49 (1.4.2); FILE MERGED 2006/03/15 16:21:31 sb 1.4.2.1: #i62098# Added check that no two service ctors have the same signature; along the way, added some helper functions and some missing const qualifiers.

--- idlc/inc/idlc/astsequence.hxx
+++ idlc/inc/idlc/astsequence.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: astsequence.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-03-09 10:48:09 $
+ *  last change: $Author: hr $ $Date: 2006-04-19 13:44:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ public:
             delete m_pRelativName;
     }
 
-    AstType* getMemberType()
+    AstType* getMemberType() const
         { return m_pMemberType; }
 
     virtual bool isUnsigned() const
