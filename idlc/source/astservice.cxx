ab1af32d986fd54c80112855f6eb7e3c6ee5c823
Jens-Heiner Rechtien <hr@openoffice.org>
2006-04-19 12:45:37 +0000

    INTEGRATION: CWS sb49 (1.1.2); FILE ADDED 2006/03/16 10:13:00 sb 1.1.2.2: #i62098# Fixed previous fix (and added a testcase). 2006/03/15 16:21:59 sb 1.1.2.1: #i62098# Added check that no two service ctors have the same signature; along the way, added some helper functions and some missing const qualifiers.

---
+++ idlc/source/astservice.cxx
@@ -0,0 +1,74 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: astservice.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-04-19 13:45:37 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "sal/config.h"
+
+#include "idlc/astmember.hxx"
+#include "idlc/astoperation.hxx"
+#include "idlc/astservice.hxx"
+#include "idlc/asttype.hxx"
+
+bool AstService::checkLastConstructor() const {
+    AstOperation const * last = static_cast< AstOperation const * >(getLast());
+    for (DeclList::const_iterator i(getIteratorBegin()); i != getIteratorEnd();
+         ++i)
+    {
+        if (*i != last && (*i)->getNodeType() == NT_operation) {
+            AstOperation const * ctor = static_cast< AstOperation * >(*i);
+            if (ctor->isVariadic() && last->isVariadic()) {
+                return true;
+            }
+            sal_uInt32 n = ctor->nMembers();
+            if (n == last->nMembers()) {
+                for (DeclList::const_iterator i1(ctor->getIteratorBegin()),
+                         i2(last->getIteratorBegin());
+                     i1 != ctor->getIteratorEnd(); ++i1, ++i2)
+                {
+                    sal_Int32 r1;
+                    AstDeclaration const * t1 = deconstructAndResolveTypedefs(
+                        static_cast< AstMember * >(*i1)->getType(), &r1);
+                    sal_Int32 r2;
+                    AstDeclaration const * t2 = deconstructAndResolveTypedefs(
+                        static_cast< AstMember * >(*i2)->getType(), &r2);
+                    if (r1 == r2 && t1->getScopedName() == t2->getScopedName())
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
