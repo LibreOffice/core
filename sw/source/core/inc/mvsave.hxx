fe3bbc3f9d7f9ffdcb284ee34d1c02f31be77450
Jens-Heiner Rechtien <hr@openoffice.org>
2006-04-19 13:20:27 +0000

    INTEGRATION: CWS redline01 (1.5.128); FILE MERGED 2006/03/22 13:14:20 ama 1.5.128.1: Fix #i59534: Text insertion at the end of a redline

--- sw/source/core/inc/mvsave.hxx
+++ sw/source/core/inc/mvsave.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mvsave.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:51:06 $
+ *  last change: $Author: hr $ $Date: 2006-04-19 14:20:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -217,10 +217,11 @@ class _SaveRedlEndPosForRestore
 {
     SvPtrarr* pSavArr;
     SwNodeIndex* pSavIdx;
+    xub_StrLen nSavCntnt;
 
     void _Restore();
 public:
-    _SaveRedlEndPosForRestore( const SwNodeIndex& rInsIdx );
+    _SaveRedlEndPosForRestore( const SwNodeIndex& rInsIdx, xub_StrLen nCntnt );
     ~_SaveRedlEndPosForRestore();
     void Restore() { if( pSavArr ) _Restore(); }
 };
