c4be88a3267973d9b2602cfe6b8db197cb6a9025
Kurt Zenker <kz@openoffice.org>
2006-04-26 19:45:46 +0000

    INTEGRATION: CWS taskpane (1.3.116); FILE MERGED 2006/04/12 15:18:18 af 1.3.116.3: #i61359# Fixed some compiler problems. 2006/03/29 15:30:09 af 1.3.116.2: RESYNC: (1.3-1.4); FILE MERGED 2006/02/22 09:35:36 af 1.3.116.1: #i61359# Add some more event types.

--- sd/source/ui/inc/EventMultiplexer.hxx
+++ sd/source/ui/inc/EventMultiplexer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EventMultiplexer.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:21:10 $
+ *  last change: $Author: kz $ $Date: 2006-04-26 20:45:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,78 +59,92 @@ public:
         EventMultiplexer.  Removing a listener as response is not necessary,
         though.
     */
-    static const EventId EID_DISPOSING              = 0x0001;
+    static const EventId EID_DISPOSING              = 0x00000001;
 
     /** The selection in the center pane has changed.
     */
-    static const EventId EID_EDIT_VIEW_SELECTION    = 0x0002;
+    static const EventId EID_EDIT_VIEW_SELECTION    = 0x00000002;
 
     /** The selection in the slide sorter has changed, regardless of whether
         the slide sorter is displayed in the left pane or the center pane.
     */
-    static const EventId EID_SLIDE_SORTER_SELECTION = 0x0004;
+    static const EventId EID_SLIDE_SORTER_SELECTION = 0x00000004;
 
     /** The current page has changed.
     */
-    static const EventId EID_CURRENT_PAGE           = 0x0008;
+    static const EventId EID_CURRENT_PAGE           = 0x00000008;
 
     /** The current MainViewShell (the ViewShell displayed in the center
         pane) has been removed.
     */
-    static const EventId EID_MAIN_VIEW_REMOVED      = 0x0010;
+    static const EventId EID_MAIN_VIEW_REMOVED      = 0x00000010;
 
     /** A new ViewShell has been made the MainViewShell.
     */
-    static const EventId EID_MAIN_VIEW_ADDED        = 0x0020;
+    static const EventId EID_MAIN_VIEW_ADDED        = 0x00000020;
 
     /** A ViewShell has been removed from one of the panes.  Note that for
         the ViewShell in the center pane bth this event type and
         EID_MAIN_VIEW_REMOVED is broadcasted.
     */
-    static const EventId EID_VIEW_REMOVED           = 0x0040;
+    static const EventId EID_VIEW_REMOVED           = 0x00000040;
 
     /** A new ViewShell is being displayed in one of the panes.  Note that
         for the ViewShell in the center pane both this event type and
         EID_MAIN_VIEW_ADDED is broadcasted.
     */
-    static const EventId EID_VIEW_ADDED             = 0x0080;
+    static const EventId EID_VIEW_ADDED             = 0x00000080;
 
     /** The PaneManager is being destroyed.
     */
-    static const EventId EID_PANE_MANAGER_DYING     = 0x0100;
+    static const EventId EID_PANE_MANAGER_DYING     = 0x00000100;
 
     /** The edit mode of the ViewShell in the center pane has been modified.
     */
-    static const EventId EID_EDIT_MODE              = 0x0200;
+    static const EventId EID_EDIT_MODE              = 0x00000200;
 
     /** One or more pages have been inserted into or deleted from the model.
     */
-    static const EventId EID_PAGE_ORDER             = 0x0400;
+    static const EventId EID_PAGE_ORDER             = 0x00000400;
 
     /** Text editing in one of the shapes in the MainViewShell has started.
     */
-    static const EventId EID_BEGIN_TEXT_EDIT        = 0x0800;
+    static const EventId EID_BEGIN_TEXT_EDIT        = 0x00000800;
 
     /** Text editing in one of the shapes in the MainViewShell has ended.
     */
-    static const EventId EID_END_TEXT_EDIT          = 0x1000;
+    static const EventId EID_END_TEXT_EDIT          = 0x00001000;
 
     /** A UNO controller has been attached to the UNO frame.
     */
-    static const EventId EID_CONTROLLER_ATTACHED    = 0x2000;
+    static const EventId EID_CONTROLLER_ATTACHED    = 0x00002000;
 
     /** A UNO controller has been detached to the UNO frame.
     */
-    static const EventId EID_CONTROLLER_DETACHED    = 0x4000;
+    static const EventId EID_CONTROLLER_DETACHED    = 0x00004000;
+
+    /** The state of a shape has changed.  The page is available in the user data.
+    */
+    static const EventId EID_SHAPE_CHANGED          = 0x00008000;
+
+    /** A shape has been inserted to a page.  The page is available in the
+        user data.
+    */
+    static const EventId EID_SHAPE_INSERTED         = 0x00010000;
+
+    /** A shape has been removed from a page.  The page is available in the
+        user data.
+    */
+    static const EventId EID_SHAPE_REMOVED          = 0x00020000;
 
     const ViewShellBase& mrBase;
     EventId meEventId;
-    void* mpUserData;
+    const void* mpUserData;
 
     EventMultiplexerEvent (
         const ViewShellBase& rBase,
         EventId eEventId,
-        void* pUserData);
+        const void* pUserData);
 };
 
 
