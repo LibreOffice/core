6fda652837755f005a444d52e8e27bd06c64f5e3
Rüdiger Timm <rt@openoffice.org>
2006-05-02 11:01:08 +0000

    INTEGRATION: CWS mhu12 (1.7.16); FILE MERGED 2006/04/19 12:55:30 sb 1.7.16.1: #i47132# Adapted to new functionality from rtl/alloc.h to obtain executable memory for the raw vtable blocks (which contain code snippets together with the vtables).

--- bridges/source/cpp_uno/gcc3_freebsd_intel/cpp2uno.cxx
+++ bridges/source/cpp_uno/gcc3_freebsd_intel/cpp2uno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cpp2uno.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:26:21 $
+ *  last change: $Author: rt $ $Date: 2006-05-02 12:01:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -415,20 +415,22 @@ unsigned char * codeSnippet(
 
 }
 
-void ** bridges::cpp_uno::shared::VtableFactory::mapBlockToVtable(char * block)
+void ** bridges::cpp_uno::shared::VtableFactory::mapBlockToVtable(void * block)
 {
-    return reinterpret_cast< void ** >(block) + 2;
+    return static_cast< void ** >(block) + 2;
 }
 
-char * bridges::cpp_uno::shared::VtableFactory::createBlock(
-    sal_Int32 slotCount, void *** slots)
+sal_Size bridges::cpp_uno::shared::VtableFactory::getBlockSize(
+    sal_Int32 slotCount)
 {
-    char * block = new char[
-        (slotCount + 2) * sizeof (void *) + slotCount * codeSnippetSize];
-    *slots = mapBlockToVtable(block);
-    (*slots)[-2] = 0;
-    (*slots)[-1] = 0;
-    return block;
+    return (slotCount + 2) * sizeof (void *) + slotCount * codeSnippetSize;
+}
+
+void ** bridges::cpp_uno::shared::VtableFactory::initializeBlock(void * block) {
+    void ** slots = mapBlockToVtable(block);
+    slots[-2] = 0;
+    slots[-1] = 0;
+    return slots;
 }
 
 unsigned char * bridges::cpp_uno::shared::VtableFactory::addLocalFunctions(
