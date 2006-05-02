7a5c8f1cf042a17da08745d346d38b32619e6c9e
Rüdiger Timm <rt@openoffice.org>
2006-05-02 14:39:56 +0000

    INTEGRATION: CWS sfxcleanup (1.4.18); FILE MERGED 2006/02/28 13:59:38 mba 1.4.18.1: #132394#: remove superfluous code

--- unoxml/source/service/services.cxx
+++ unoxml/source/service/services.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: services.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:07:34 $
+ *  last change: $Author: rt $ $Date: 2006-05-02 15:39:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,8 @@
 #include <cppuhelper/interfacecontainer.h>
 #include <com/sun/star/lang/XSingleServiceFactory.hpp>
 #include <osl/mutex.hxx>
-#include <sfx2/sfxuno.hxx>
+#include <com/sun/star/registry/XRegistryKey.hpp>
+#include <cppuhelper/factory.hxx>
 
 #include "../dom/documentbuilder.hxx"
 #include "../dom/saxbuilder.hxx"
