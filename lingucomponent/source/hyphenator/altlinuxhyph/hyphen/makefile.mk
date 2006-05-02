47469117b90f8269e330556a60f898cd79f5e3eb
Rüdiger Timm <rt@openoffice.org>
2006-05-02 14:39:01 +0000

    INTEGRATION: CWS sfxcleanup (1.9.34); FILE MERGED 2006/02/28 08:54:20 mba 1.9.34.1: #132394#: remove superfluous code

--- lingucomponent/source/hyphenator/altlinuxhyph/hyphen/makefile.mk
+++ lingucomponent/source/hyphenator/altlinuxhyph/hyphen/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:40:18 $
+#   last change: $Author: rt $ $Date: 2006-05-02 15:39:01 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -120,7 +120,6 @@ SHL1STDLIBS= \
         $(SVTOOLLIB)	\
         $(SVLLIB)		\
         $(VCLLIB)		\
-        $(SFXLIB)		\
         $(SALLIB)		\
         $(UCBHELPERLIB)	\
         $(UNOTOOLSLIB)	\
