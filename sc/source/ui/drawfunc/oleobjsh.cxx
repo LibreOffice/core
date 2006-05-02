463ce0ac91cf1be18d84dc581010868ac60e7d6b
Rüdiger Timm <rt@openoffice.org>
2006-05-02 14:49:56 +0000

    INTEGRATION: CWS sfxcleanup (1.4.126); FILE MERGED 2006/03/02 10:39:28 mba 1.4.126.2: #132394#: remove superfluous code 2006/03/02 09:26:21 mba 1.4.126.1: #132394#: remove superfluous code

--- sc/source/ui/drawfunc/oleobjsh.cxx
+++ sc/source/ui/drawfunc/oleobjsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oleobjsh.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:00:55 $
+ *  last change: $Author: rt $ $Date: 2006-05-02 15:49:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,6 @@ SFX_IMPL_INTERFACE(ScOleObjectShell, ScDrawShell, ScResId(SCSTR_OLEOBJECTSHELL) 
     SFX_OBJECTBAR_REGISTRATION( SFX_OBJECTBAR_OBJECT|SFX_VISIBILITY_STANDARD|SFX_VISIBILITY_SERVER,
                                 ScResId(RID_DRAW_OBJECTBAR) );
     SFX_POPUPMENU_REGISTRATION( ScResId(RID_POPUP_OLE) );
-    SFX_OBJECTMENU_REGISTRATION( SID_OBJECTMENU0, ScResId(RID_OBJECTMENU_DRAW) );
 }
 
 TYPEINIT1( ScOleObjectShell, ScDrawShell );
