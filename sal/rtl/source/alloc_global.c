fe13f9ea2f4cfe1dd2a9e52ddac6a0c3bdab8e68
Rüdiger Timm <rt@openoffice.org>
2006-05-02 11:12:23 +0000

    INTEGRATION: CWS mhu12 (1.1.2); FILE ADDED 2006/04/20 11:00:11 mhu 1.1.2.2: #i23172# #i57764# #125511# Cleaned up, added documentation. 2006/04/11 16:57:54 mhu 1.1.2.1: #125511# #i23172# #i57754# new global allocator implementation, based on rtl_arena + rtl_cache.

---
+++ sal/rtl/source/alloc_global.c
@@ -0,0 +1,363 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: alloc_global.c,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-02 12:12:23 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "rtl/alloc.h"
+
+#ifndef INCLUDED_STRING_H
+#include <string.h>
+#define INCLUDED_STRING_H
+#endif
+
+#if !defined(FORCE_SYSALLOC)
+
+/* ================================================================= *
+ *
+ * custom allocator includes.
+ *
+ * ================================================================= */
+
+#ifndef INCLUDED_STDIO_H
+#include <stdio.h>
+#define INCLUDED_STDIO_H
+#endif
+
+#ifndef INCLUDED_RTL_ALLOC_IMPL_H
+#include "alloc_impl.h"
+#endif
+
+#ifndef INCLUDED_SAL_INTERNAL_ONCE_H
+#include "internal/once.h"
+#endif
+
+#ifndef _SAL_MACROS_H_
+#include "sal/macros.h"
+#endif
+
+#ifndef _OSL_DIAGNOSE_H_
+#include "osl/diagnose.h"
+#endif
+
+/* ================================================================= *
+ *
+ * custom allocator internals.
+ *
+ * ================================================================= */
+
+static const sal_Size g_alloc_sizes[] =
+{
+    /* powers of 2**(1/4) */
+    4 *    4,           6 *    4,
+    4 *    8, 5 *    8, 6 *    8, 7 *    8,
+    4 *   16, 5 *   16, 6 *   16, 7 *   16,
+    4 *   32, 5 *   32, 6 *   32, 7 *   32,
+    4 *   64, 5 *   64, 6 *   64, 7 *   64,
+    4 *  128, 5 *  128, 6 *  128, 7 *  128,
+    4 *  256, 5 *  256, 6 *  256, 7 *  256,
+    4 *  512, 5 *  512, 6 *  512, 7 *  512,
+    4 * 1024, 5 * 1024, 6 * 1024, 7 * 1024,
+    4 * 2048, 5 * 2048, 6 * 2048, 7 * 2048,
+    4 * 4096
+};
+
+#define RTL_MEMORY_CACHED_LIMIT 4 * 4096
+#define RTL_MEMORY_CACHED_SIZES (sizeof(g_alloc_sizes) / sizeof(g_alloc_sizes[0]))
+
+static rtl_cache_type * g_alloc_caches[RTL_MEMORY_CACHED_SIZES] =
+{
+    0,
+};
+
+#define RTL_MEMALIGN       8
+#define RTL_MEMALIGN_SHIFT 3
+
+static rtl_cache_type * g_alloc_table[RTL_MEMORY_CACHED_LIMIT >> RTL_MEMALIGN_SHIFT] =
+{
+    0,
+};
+
+static rtl_arena_type * gp_alloc_arena = 0;
+
+/* ================================================================= *
+ *
+ * custom allocator initialization / finalization.
+ *
+ * ================================================================= */
+
+static void
+rtl_memory_once_init (void)
+{
+    {
+        /* global memory arena */
+        OSL_ASSERT(gp_alloc_arena == 0);
+
+        gp_alloc_arena = rtl_arena_create (
+            "rtl_alloc_arena",
+            2048,     /* quantum */
+            0,        /* w/o quantum caching */
+            0,        /* default source */
+            rtl_arena_alloc,
+            rtl_arena_free,
+            0         /* flags */
+        );
+        OSL_ASSERT(gp_alloc_arena != 0);
+    }
+    {
+        sal_Size size;
+        int i, n = RTL_MEMORY_CACHED_SIZES;
+
+        for (i = 0; i < n; i++)
+        {
+            char name[RTL_CACHE_NAME_LENGTH + 1];
+            (void) snprintf (name, sizeof(name), "rtl_alloc_%lu", g_alloc_sizes[i]);
+            g_alloc_caches[i] = rtl_cache_create (name, g_alloc_sizes[i], 0, NULL, NULL, NULL, NULL, NULL, 0);
+        }
+
+        size = RTL_MEMALIGN;
+        for (i = 0; i < n; i++)
+        {
+            while (size <= g_alloc_sizes[i])
+            {
+                g_alloc_table[(size - 1) >> RTL_MEMALIGN_SHIFT] = g_alloc_caches[i];
+                size += RTL_MEMALIGN;
+            }
+        }
+    }
+}
+
+static int
+rtl_memory_init (void)
+{
+    static sal_once_type g_once = SAL_ONCE_INIT;
+    SAL_ONCE(&g_once, rtl_memory_once_init);
+    return (gp_alloc_arena != 0);
+}
+
+/* ================================================================= */
+
+#if defined(__GNUC__)
+static void rtl_memory_fini (void) __attribute__((destructor));
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#pragma fini(rtl_memory_fini)
+static void rtl_memory_fini (void);
+#endif /* __GNUC__ || __SUNPRO_C */
+
+void
+rtl_memory_fini (void)
+{
+    int i, n;
+
+    /* clear g_alloc_table */
+    memset (g_alloc_table, 0, sizeof(g_alloc_table));
+
+    /* cleanup g_alloc_caches */
+    for (i = 0, n = RTL_MEMORY_CACHED_SIZES; i < n; i++)
+    {
+        if (g_alloc_caches[i] != 0)
+        {
+            rtl_cache_destroy (g_alloc_caches[i]);
+            g_alloc_caches[i] = 0;
+        }
+    }
+
+    /* cleanup gp_alloc_arena */
+    if (gp_alloc_arena != 0)
+    {
+        rtl_arena_destroy (gp_alloc_arena);
+        gp_alloc_arena = 0;
+    }
+}
+
+/* ================================================================= *
+ *
+ * custom allocator implemenation.
+ *
+ * ================================================================= */
+
+void *
+SAL_CALL rtl_allocateMemory (sal_Size n)
+{
+    void * p = 0;
+    if (n > 0)
+    {
+        char *     addr;
+        sal_Size   size = RTL_MEMORY_ALIGN(n + RTL_MEMALIGN, RTL_MEMALIGN);
+
+        int index = (size - 1) >> RTL_MEMALIGN_SHIFT;
+        OSL_ASSERT(RTL_MEMALIGN >= sizeof(sal_Size));
+
+try_alloc:
+        if (index < RTL_MEMORY_CACHED_LIMIT >> RTL_MEMALIGN_SHIFT)
+            addr = (char*)rtl_cache_alloc(g_alloc_table[index]);
+        else
+            addr = (char*)rtl_arena_alloc (gp_alloc_arena, &size);
+
+        if (addr != 0)
+        {
+            ((sal_Size*)(addr))[0] = size;
+            p = addr + RTL_MEMALIGN;
+        }
+        else if (gp_alloc_arena == 0)
+        {
+            if (rtl_memory_init())
+            {
+                /* try again */
+                goto try_alloc;
+            }
+        }
+    }
+    return (p);
+}
+
+/* ================================================================= */
+
+void SAL_CALL rtl_freeMemory (void * p)
+{
+    if (p != 0)
+    {
+        char *   addr = (char*)(p) - RTL_MEMALIGN;
+        sal_Size size = ((sal_Size*)(addr))[0];
+
+        int index = (size - 1) >> RTL_MEMALIGN_SHIFT;
+        if (index < RTL_MEMORY_CACHED_LIMIT >> RTL_MEMALIGN_SHIFT)
+            rtl_cache_free(g_alloc_table[index], addr);
+        else
+            rtl_arena_free (gp_alloc_arena, addr, size);
+    }
+}
+
+/* ================================================================= */
+
+void * SAL_CALL rtl_reallocateMemory (void * p, sal_Size n)
+{
+    if (n > 0)
+    {
+        if (p != 0)
+        {
+            void *   p_old = p;
+            sal_Size n_old = ((sal_Size*)( (char*)(p) - RTL_MEMALIGN  ))[0];
+
+            p = rtl_allocateMemory (n);
+            if (p != 0)
+            {
+                memcpy (p, p_old, SAL_MIN(n, n_old));
+                rtl_freeMemory (p_old);
+            }
+        }
+        else
+        {
+            p = rtl_allocateMemory (n);
+        }
+    }
+    else if (p != 0)
+    {
+        rtl_freeMemory (p), p = 0;
+    }
+    return (p);
+}
+
+#else  /* FORCE_SYSALLOC */
+
+/* ================================================================= *
+ *
+ * system allocator includes.
+ *
+ * ================================================================= */
+
+#ifndef INCLUDED_STDLIB_H
+#include <stdlib.h>
+#define INCLUDED_STDLIB_H
+#endif
+
+/* ================================================================= *
+ *
+ * system allocator implemenation.
+ *
+ * ================================================================= */
+
+void * SAL_CALL rtl_allocateMemory (sal_Size n)
+{
+    return malloc (n);
+}
+
+/* ================================================================= */
+
+void SAL_CALL rtl_freeMemory (void * p)
+{
+    free (p);
+}
+
+/* ================================================================= */
+
+void * SAL_CALL rtl_reallocateMemory (void * p, sal_Size n)
+{
+    return realloc (p, n);
+}
+
+/* ================================================================= */
+
+void
+rtl_memory_fini (void)
+{
+    /* nothing to do */
+}
+
+#endif /* FORCE_SYSALLOC */
+
+/* ================================================================= *
+ *
+ * rtl_(allocate|free)ZeroMemory() implemenation.
+ *
+ * ================================================================= */
+
+void * SAL_CALL rtl_allocateZeroMemory (sal_Size n)
+{
+    void * p = rtl_allocateMemory (n);
+    if (p != 0)
+        memset (p, 0, n);
+    return (p);
+}
+
+/* ================================================================= */
+
+void SAL_CALL rtl_freeZeroMemory (void * p, sal_Size n)
+{
+    if (p != 0)
+    {
+        memset (p, 0, n);
+        rtl_freeMemory (p);
+    }
+}
+
+/* ================================================================= */
