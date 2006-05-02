d808509113b2f85a489df16e102e46619fd78ae0
Rüdiger Timm <rt@openoffice.org>
2006-05-02 14:07:27 +0000

    INTEGRATION: CWS sfxcleanup (1.5.146); FILE MERGED 2006/02/27 08:26:36 mba 1.5.146.1: #132394#: remove superfluous code

--- sd/source/ui/view/clview.cxx
+++ sd/source/ui/view/clview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: clview.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:05:40 $
+ *  last change: $Author: rt $ $Date: 2006-05-02 15:07:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -114,10 +114,6 @@ void ClientView::InvalidateOneWin(::Window& rWin, const Rectangle& rRect)
 void ClientView::CompleteRedraw(OutputDevice* pOutDev, const Region& rReg)
 {
     DrawView::CompleteRedraw(pOutDev, rReg);
-
-    // Stets Demo-Output auf dem ganzen Fenster ausgeben
-    Rectangle aRect( Point(0, 0), pOutDev->GetOutputSize() );
-    SFX_APP()->SpoilDemoOutput(*pOutDev, aRect );
 }
 
 
