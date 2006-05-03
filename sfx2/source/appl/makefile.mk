f2bedd49e4a9e464b6e6027a4289030b6f755b51
Rüdiger Timm <rt@openoffice.org>
2006-05-03 06:35:51 +0000

    #i10000# Remove conflict markers left over from last resync.

--- sfx2/source/appl/makefile.mk
+++ sfx2/source/appl/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.42 $
+#   $Revision: 1.43 $
 #
-#   last change: $Author: rt $ $Date: 2006-05-02 16:19:50 $
+#   last change: $Author: rt $ $Date: 2006-05-03 07:35:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -125,7 +125,6 @@ EXCEPTIONSFILES=\
 SLOFILES +=\
         $(SLO)$/appmac.obj
 .ENDIF
->>>>>>> 1.41
 
 # --- Targets -------------------------------------------------------
 
