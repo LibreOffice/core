a580761b1f49a522da6d4985e1f4df1b00f97581
Rüdiger Timm <rt@openoffice.org>
2006-05-04 07:40:47 +0000

    INTEGRATION: CWS dba203c (1.5.104); FILE MERGED 2006/04/10 08:36:13 oj 1.5.104.2: RESYNC: (1.5-1.6); FILE MERGED 2006/03/29 11:35:41 fs 1.5.104.1: #133638# renamed ambiguous ::dbui::OLocalResourceAccess to LocalresourceAccess

--- dbaccess/source/ui/dlg/TextConnectionHelper.cxx
+++ dbaccess/source/ui/dlg/TextConnectionHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TextConnectionHelper.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-29 12:37:27 $
+ *  last change: $Author: rt $ $Date: 2006-05-04 08:40:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -302,7 +302,7 @@ DBG_NAME(OTextConnectionHelper)
     // -----------------------------------------------------------------------
     sal_Bool OTextConnectionHelper::checkItems()
     {
-        OLocalResourceAccess aStringResAccess(PAGE_TEXT, RSC_TABPAGE);
+        LocalResourceAccess aStringResAccess(PAGE_TEXT, RSC_TABPAGE);
             // for accessing the strings which are local to our own resource block
         String sExtension = GetExtension();
         String aErrorText;
