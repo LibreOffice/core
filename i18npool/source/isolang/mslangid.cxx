e4ebca6669495fedb7f5df7e4272e5388b90c56e
Rüdiger Timm <rt@openoffice.org>
2006-05-04 08:12:42 +0000

    INTEGRATION: CWS locales203 (1.2.2); FILE MERGED 2006/04/20 16:52:13 er 1.2.2.2: #i61058# new: hasForbiddenCharacters(), getReplacementForObsoleteLanguage() 2006/04/12 17:29:58 er 1.2.2.1: remove SISSL

--- i18npool/source/isolang/mslangid.cxx
+++ i18npool/source/isolang/mslangid.cxx
@@ -1,61 +1,35 @@
 /*************************************************************************
  *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
  *  $RCSfile: mslangid.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $  $Date: 2006-04-07 14:39:23 $
+ *  last change: $Author: rt $ $Date: 2006-05-04 09:12:42 $
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
- *
- *         - GNU Lesser General Public License Version 2.1
- *         - Sun Industry Standards Source License Version 1.1
- *
- *  Sun Microsystems Inc., October, 2000
- *
- *  GNU Lesser General Public License Version 2.1
- *  =============================================
- *  Copyright 2004 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
  *
  *
- *  Sun Industry Standards Source License Version 1.1
- *  =================================================
- *  The contents of this file are subject to the Sun Industry Standards
- *  Source License Version 1.1 (the "License"); You may not use this file
- *  except in compliance with the License. You may obtain a copy of the
- *  License at http://www.openoffice.org/license.html.
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  Software provided under this License is provided on an "AS IS" basis,
- *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
- *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
- *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
- *  See the License for the specific provisions governing your rights and
- *  obligations concerning the Software.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
  *
- *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  *
- *  Copyright: 2004 by Sun Microsystems, Inc.
- *
- *  All Rights Reserved.
- *
- *  Contributor(s): _______________________________________
- *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
  *
  ************************************************************************/
 
@@ -220,6 +194,22 @@ bool MsLangId::isRightToLeft( LanguageType nLang )
 }
 
 
+// static
+bool MsLangId::hasForbiddenCharacters( LanguageType nLang )
+{
+    switch (nLang & LANGUAGE_MASK_PRIMARY)
+    {
+        case LANGUAGE_CHINESE  & LANGUAGE_MASK_PRIMARY:
+        case LANGUAGE_JAPANESE & LANGUAGE_MASK_PRIMARY:
+        case LANGUAGE_KOREAN   & LANGUAGE_MASK_PRIMARY:
+            return true;
+            break;
+        default:
+            break;
+    }
+    return false;
+}
+
 
 // static
 sal_Int16 MsLangId::getScriptType( LanguageType nLang )
@@ -305,4 +295,34 @@ sal_Int16 MsLangId::getScriptType( LanguageType nLang )
         break;
     }
     return nScript;
+}
+
+
+// static
+LanguageType MsLangId::getReplacementForObsoleteLanguage( LanguageType nLang )
+{
+    switch (nLang)
+    {
+        case LANGUAGE_OBSOLETE_USER_LATIN:
+            nLang = LANGUAGE_LATIN;
+        case LANGUAGE_OBSOLETE_USER_MAORI:
+            nLang = LANGUAGE_MAORI_NEW_ZEALAND;
+        case LANGUAGE_OBSOLETE_USER_KINYARWANDA:
+            nLang = LANGUAGE_KINYARWANDA_RWANDA;
+        case LANGUAGE_OBSOLETE_USER_UPPER_SORBIAN:
+            nLang = LANGUAGE_UPPER_SORBIAN_GERMANY;
+        case LANGUAGE_OBSOLETE_USER_LOWER_SORBIAN:
+            nLang = LANGUAGE_LOWER_SORBIAN_GERMANY;
+        case LANGUAGE_OBSOLETE_USER_OCCITAN:
+            nLang = LANGUAGE_OCCITAN_FRANCE;
+        case LANGUAGE_OBSOLETE_USER_BRETON:
+            nLang = LANGUAGE_BRETON_FRANCE;
+        case LANGUAGE_OBSOLETE_USER_KALAALLISUT:
+            nLang = LANGUAGE_KALAALLISUT_GREENLAND;
+        case LANGUAGE_OBSOLETE_USER_LUXEMBOURGISH:
+            nLang = LANGUAGE_LUXEMBOURGISH_LUXEMBOURG;
+        default:
+            ;   // nothing
+    }
+    return nLang;
 }
