62f451cb03d5202e2f15bf440c53a61b2efd99f8
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:54:24 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2006/03/31 12:19:39 obr 1.1.2.12: #i63583# eliminated warnings 2006/03/28 10:49:45 obr 1.1.2.11: #i47890# redid the last patch to make it more clear 2006/03/23 14:45:26 obr 1.1.2.10: fixed endless loop problem with tooltips 2006/02/15 10:59:22 obr 1.1.2.9: #i47890# replaced tabs with spaces 2006/01/05 14:06:31 obr 1.1.2.8: #i47890# override toolkit name and version and reworked bridge initialization 2005/11/16 12:29:16 obr 1.1.2.7: no longer return NULL in factory_create_accessible 2005/11/16 09:09:20 obr 1.1.2.6: gail rev. 1.8.6 fixes the things we needed gsignalhook for 2005/10/20 07:09:09 obr 1.1.2.5: #i47890# made cxx files standalone, avoid queryInterface on each API call and demacrofied try/catch to include appropriate warnings 2005/09/28 07:24:12 obr 1.1.2.4: #i47890# changed ref-counting/life cycle once again, adjust tree and role of dialogs and filter sub-menu windows 2005/09/26 11:01:00 obr 1.1.2.3: #i47890# reworked lifecycle of atk wrapper objects 2005/06/14 13:57:23 obr 1.1.2.2: #i47890# global focus event listening stuff 2005/04/21 14:59:24 mmeeks 1.1.2.1: Issue number: i#47890# Submitted by: mmeeks
    Hacked up prototype of atk bridge

---
+++ vcl/unx/gtk/a11y/atkfactory.cxx
@@ -0,0 +1,174 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: atkfactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 10:54:24 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <plugins/gtk/gtkframe.hxx>
+#include "atkwrapper.hxx"
+#include "atkfactory.hxx"
+
+using namespace ::com::sun::star;
+
+extern "C" {
+
+/*
+ *  Instances of this dummy object class are returned whenever we have to
+ *  create an AtkObject, but can't touch the OOo object anymore since it
+ *  is already disposed.
+ */
+
+static AtkStateSet *
+noop_wrapper_ref_state_set( AtkObject * )
+{
+    AtkStateSet *state_set = atk_state_set_new();
+    atk_state_set_add_state( state_set, ATK_STATE_DEFUNCT );
+    return state_set;
+}
+
+static void
+atk_noop_object_wrapper_class_init(AtkNoOpObjectClass *klass)
+{
+    AtkObjectClass *atk_class = ATK_OBJECT_CLASS( klass );
+    atk_class->ref_state_set = noop_wrapper_ref_state_set;
+}
+
+static GType
+atk_noop_object_wrapper_get_type(void)
+{
+    static GType type = 0;
+
+    if (!type)
+    {
+        static const GTypeInfo typeInfo =
+        {
+            sizeof (AtkNoOpObjectClass),
+            (GBaseInitFunc) NULL,
+            (GBaseFinalizeFunc) NULL,
+            (GClassInitFunc) atk_noop_object_wrapper_class_init,
+            (GClassFinalizeFunc) NULL,
+            NULL,
+            sizeof (AtkObjectWrapper),
+            0,
+            (GInstanceInitFunc) NULL,
+            NULL
+        } ;
+
+        type = g_type_register_static (ATK_TYPE_OBJECT, "OOoAtkNoOpObj", &typeInfo, (GTypeFlags)0) ;
+  }
+  return type;
+}
+
+AtkObject*
+atk_noop_object_wrapper_new()
+{
+  AtkObject *accessible;
+
+  accessible = (AtkObject *) g_object_new (atk_noop_object_wrapper_get_type(), NULL);
+  g_return_val_if_fail (accessible != NULL, NULL);
+
+  accessible->role = ATK_ROLE_INVALID;
+  accessible->layer = ATK_LAYER_INVALID;
+
+  return accessible;
+}
+
+/*
+ * The wrapper factory
+ */
+
+static GType
+wrapper_factory_get_accessible_type(void)
+{
+  return atk_object_wrapper_get_type();
+}
+
+static AtkObject*
+wrapper_factory_create_accessible( GObject *pObj )
+{
+    GtkWidget* parent_widget = gtk_widget_get_parent( GTK_WIDGET( pObj ) );
+
+    // gail_container_real_remove_gtk tries to re-instanciate an accessible
+    // for a widget that is about to vanish ..
+    if( ! parent_widget )
+        return atk_noop_object_wrapper_new();
+
+    GtkSalFrame* pFrame = GtkSalFrame::getFromWindow( GTK_WINDOW( parent_widget ) );
+    g_return_val_if_fail( pFrame != NULL, NULL );
+
+    /* HACK: if the parent gtk window has an accessible already assigned, use
+     * this one to avoid endless recursion (see atkwindow.cxx).
+     */
+    AtkObject* parent_accessible = (AtkObject *) g_object_get_data(G_OBJECT( parent_widget ),
+        "ooo:tooltip-accessible");
+
+    if( ! parent_accessible )
+        parent_accessible = gtk_widget_get_accessible(parent_widget);
+
+    uno::Reference< accessibility::XAccessible > xAccessible(pFrame->getAccessible( true ));
+
+    if( xAccessible.is() )
+        return atk_object_wrapper_new( xAccessible, parent_accessible );
+
+    return NULL;
+}
+
+static void
+wrapper_factory_class_init( AtkObjectFactoryClass *klass )
+{
+  klass->create_accessible   = wrapper_factory_create_accessible;
+  klass->get_accessible_type = wrapper_factory_get_accessible_type;
+}
+
+GType
+wrapper_factory_get_type (void)
+{
+  static GType t = 0;
+
+  if (!t) {
+    static const GTypeInfo tinfo =
+    {
+      sizeof (AtkObjectFactoryClass),
+      NULL, NULL, (GClassInitFunc) wrapper_factory_class_init,
+      NULL, NULL, sizeof (AtkObjectFactory), 0, NULL, NULL
+    };
+
+    t = g_type_register_static (
+        ATK_TYPE_OBJECT_FACTORY, "OOoAtkObjectWrapperFactory",
+        &tinfo, (GTypeFlags) 0);
+  }
+
+  return t;
+}
+
+} // extern C
+
