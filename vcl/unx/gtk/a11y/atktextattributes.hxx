938628100c9b070dd8364029bdf480ee2e081860
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:58:09 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2005/12/22 08:24:30 obr 1.1.2.1: #i47890# Moved the text attribute implementations to a dedicated source file and fixet atk_text_get_text

---
+++ vcl/unx/gtk/a11y/atktextattributes.hxx
@@ -0,0 +1,60 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: atktextattributes.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 10:58:09 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef __ATK_ATKTEXTATTRIBUTES_HXX__
+#define __ATK_ATKTEXTATTRIBUTES_HXX__
+
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+
+#include <atk/atk.h>
+
+AtkAttributeSet*
+attribute_set_new_from_property_values(
+    const com::sun::star::uno::Sequence< com::sun::star::beans::PropertyValue >& rAttributeList,
+    const com::sun::star::beans::PropertyState ePropertyState );
+
+bool
+attribute_set_map_to_property_values(
+    AtkAttributeSet* attribute_set,
+    com::sun::star::uno::Sequence< com::sun::star::beans::PropertyValue >& rValueList );
+
+
+#endif
