927045b27c9857aa49c3eabc74da959e6df10363
Rüdiger Timm <rt@openoffice.org>
2006-05-05 08:56:51 +0000

    INTEGRATION: CWS fwkc03fixes (1.1.2); FILE ADDED 2006/04/28 13:12:09 mav 1.1.2.1: #134455# fix objects crosslinking

---
+++ embedserv/source/inc/embeddocaccess.hxx
@@ -0,0 +1,70 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: embeddocaccess.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 09:56:51 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _EMBEDDOCACCESS_HXX_
+#define _EMBEDDOCACCESS_HXX_
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1300)
+#undef _DEBUG
+#endif
+
+#include <oleidl.h>
+#include <atlcomcli.h>
+
+#include <cppuhelper/weak.hxx>
+
+class EmbedDocument_Impl;
+struct LockedEmbedDocument_Impl
+{
+    EmbedDocument_Impl* m_pEmbedDocument;
+    CComPtr< IPersistStorage > m_pLocker;
+};
+
+class EmbeddedDocumentInstanceAccess_Impl : public ::cppu::OWeakObject
+{
+    ::osl::Mutex m_aMutex;
+    EmbedDocument_Impl* m_pEmbedDocument;
+
+    public:
+    EmbeddedDocumentInstanceAccess_Impl( EmbedDocument_Impl* pDoc )
+    : m_pEmbedDocument( pDoc )
+    {}
+
+    LockedEmbedDocument_Impl GetEmbedDocument();
+    void ClearEmbedDocument();
+};
+
+#endif
+
