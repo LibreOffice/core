a22f7e29b7472941ec8a326723b9bddf0badd1f2
Rüdiger Timm <rt@openoffice.org>
2006-05-05 08:04:30 +0000

    INTEGRATION: CWS vcl57 (1.203.8); FILE MERGED 2006/04/07 12:28:10 pl 1.203.8.1: #i63638# refine frame check for popup closing

--- vcl/unx/source/window/salframe.cxx
+++ vcl/unx/source/window/salframe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salframe.cxx,v $
  *
- *  $Revision: 1.203 $
+ *  $Revision: 1.204 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-29 11:29:28 $
+ *  last change: $Author: rt $ $Date: 2006-05-05 09:04:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2593,7 +2593,13 @@ long X11SalFrame::HandleMouseEvent( XEvent *pEvent )
                                  pFrame->GetStackingWindow() == aChild )
                             )
                         {
-                            bClosePopups = false;
+                            // #i63638# check that pointer is inside window, not
+                            // only inside stacking window
+                            if( root_x >= pFrame->maGeometry.nX && root_x < pFrame->maGeometry.nX+pFrame->maGeometry.nWidth &&
+                                root_y >= pFrame->maGeometry.nY && root_y < pFrame->maGeometry.nX+pFrame->maGeometry.nHeight )
+                            {
+                                bClosePopups = false;
+                            }
                             break;
                         }
                     }
