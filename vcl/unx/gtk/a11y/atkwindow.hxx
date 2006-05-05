151773696d3ffade52e217611c5167923edd0658
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:59:01 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2006/01/05 14:06:34 obr 1.1.2.1: #i47890# override toolkit name and version and reworked bridge initialization

---
+++ vcl/unx/gtk/a11y/atkwindow.hxx
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: atkwindow.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 10:59:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef __ATK_WINDOW_HXX__
+#define __ATK_WINDOW_HXX__
+
+#include <atk/atk.h>
+
+#define OOO_TYPE_WINDOW_WRAPPER ooo_window_wrapper_get_type()
+
+GType ooo_window_wrapper_get_type (void);
+
+#endif
