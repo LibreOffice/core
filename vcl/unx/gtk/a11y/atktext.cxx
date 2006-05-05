5f6305c48800ed5341440b9c5c2ffbe3d49a3564
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:57:47 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2006/04/28 12:35:46 obr 1.1.2.19: further fixed adjust_boundaries and added more tracing facilities 2006/04/28 07:16:37 obr 1.1.2.18: finally got assertion right (hopefully) 2006/04/27 12:18:56 obr 1.1.2.17: #134575# LINE_START/END indices are identical 2006/04/27 08:53:06 obr 1.1.2.16: added untested default attributes implementation 2006/04/27 08:40:54 obr 1.1.2.15: fixed end_offset assertion 2006/04/13 12:48:16 obr 1.1.2.14: check input values for get_text 2006/03/31 12:19:40 obr 1.1.2.13: #i63583# eliminated warnings 2006/02/15 10:59:25 obr 1.1.2.12: #i47890# replaced tabs with spaces 2006/01/30 11:14:06 obr 1.1.2.11: #i47890# reworked text-changed notifications 2006/01/12 13:56:13 obr 1.1.2.10: #i47890# Solaris compile fixes 2006/01/12 07:31:27 obr 1.1.2.9: #i47890# fixed get_text to return full text for end_offset -1 2005/12/22 08:24:12 obr 1.1.2.8: #i47890# Moved the text attribute implementations to a dedicated source file and fixet atk_text_get_text 2005/11/30 10:51:50 obr 1.1.2.7: further text attribute mapping and removal of debugging output 2005/11/16 16:02:45 obr 1.1.2.6: added basic support for text attributes 2005/10/26 10:47:59 obr 1.1.2.5: #i47890# atk_text_get_at/before/after_offset reworked 2005/10/20 11:57:31 obr 1.1.2.4: #i47890# initially set name and description + disabled some tracing 2005/10/20 07:09:12 obr 1.1.2.3: #i47890# made cxx files standalone, avoid queryInterface on each API call and demacrofied try/catch to include appropriate warnings 2005/05/31 14:08:24 mmeeks 1.1.2.2: Issue number: i#47890# Submitted by: mmeeks
    Impl. AtkSelection wrapper, fix misc. AtkText selection bits.
    2005/04/21 14:59:26 mmeeks 1.1.2.1: Issue number: i#47890#
    Submitted by: mmeeks
    
    Hacked up prototype of atk bridge

---
+++ vcl/unx/gtk/a11y/atktext.cxx
@@ -0,0 +1,620 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: atktext.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 10:57:47 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "atkwrapper.hxx"
+#include "atktextattributes.hxx"
+
+#include <com/sun/star/accessibility/AccessibleTextType.hpp>
+#include <com/sun/star/accessibility/TextSegment.hpp>
+#include <com/sun/star/accessibility/XAccessibleText.hpp>
+
+// #define ENABLE_TRACING
+
+#ifdef ENABLE_TRACING
+#include <stdio.h>
+#endif
+
+using namespace ::com::sun::star;
+
+static sal_Int16
+text_type_from_boundary(AtkTextBoundary boundary_type)
+{
+    switch(boundary_type)
+    {
+        case ATK_TEXT_BOUNDARY_CHAR:
+            return accessibility::AccessibleTextType::CHARACTER;
+        case ATK_TEXT_BOUNDARY_WORD_START:
+        case ATK_TEXT_BOUNDARY_WORD_END:
+            return accessibility::AccessibleTextType::WORD;
+        case ATK_TEXT_BOUNDARY_SENTENCE_START:
+        case ATK_TEXT_BOUNDARY_SENTENCE_END:
+            return accessibility::AccessibleTextType::SENTENCE;
+        case ATK_TEXT_BOUNDARY_LINE_START:
+        case ATK_TEXT_BOUNDARY_LINE_END:
+            return accessibility::AccessibleTextType::LINE;
+        default:
+            return -1;
+    }
+}
+
+/*****************************************************************************/
+
+static gchar *
+adjust_boundaries( accessibility::XAccessibleText* pText,
+                   accessibility::TextSegment& rTextSegment,
+                   AtkTextBoundary  boundary_type,
+                   gint * start_offset, gint * end_offset )
+{
+    accessibility::TextSegment aTextSegment;
+    rtl::OUString aString;
+    gint start = 0, end = 0;
+
+    if( rTextSegment.SegmentText.getLength() > 0 )
+    {
+        switch(boundary_type)
+        {
+        case ATK_TEXT_BOUNDARY_CHAR:
+        case ATK_TEXT_BOUNDARY_LINE_START:
+        case ATK_TEXT_BOUNDARY_LINE_END:
+        case ATK_TEXT_BOUNDARY_SENTENCE_START:
+            start = rTextSegment.SegmentStart;
+            end = rTextSegment.SegmentEnd;
+            aString = rTextSegment.SegmentText;
+            break;
+
+        // the OOo break iterator behaves as SENTENCE_START
+        case ATK_TEXT_BOUNDARY_SENTENCE_END:
+            if( rTextSegment.SegmentStart > 0 )
+                start = rTextSegment.SegmentStart - 1;
+            if( rTextSegment.SegmentEnd > 0 )
+                end = rTextSegment.SegmentEnd - 1;
+            aString = pText->getTextRange(start, end);
+            break;
+
+        case ATK_TEXT_BOUNDARY_WORD_START:
+            start = rTextSegment.SegmentStart;
+
+            // Determine the start index of the next segment
+            aTextSegment = pText->getTextBehindIndex(rTextSegment.SegmentEnd,
+                                                     text_type_from_boundary(boundary_type));
+            if( aTextSegment.SegmentText.getLength() > 0 )
+                end = aTextSegment.SegmentStart;
+            else
+                end = pText->getCharacterCount();
+
+            aString = pText->getTextRange(start, end);
+            break;
+
+        case ATK_TEXT_BOUNDARY_WORD_END:
+            end = rTextSegment.SegmentEnd;
+
+            // Determine the end index of the previous segment
+            aTextSegment = pText->getTextBeforeIndex(rTextSegment.SegmentStart,
+                                                     text_type_from_boundary(boundary_type));
+            if( aTextSegment.SegmentText.getLength() > 0 )
+                start = aTextSegment.SegmentEnd;
+            else
+                start = 0;
+
+            aString = pText->getTextRange(start, end);
+            break;
+
+        default:
+            return NULL;
+        }
+    }
+
+    *start_offset = start;
+    *end_offset   = end;
+
+#ifdef ENABLE_TRACING
+    fprintf(stderr, "adjust_boundaries( %d, %d, %d ) returns %d, %d\n",
+        rTextSegment.SegmentStart, rTextSegment.SegmentEnd, boundary_type,
+        start, end);
+#endif
+
+    return OUStringToGChar(aString);
+}
+
+/*****************************************************************************/
+
+static accessibility::XAccessibleText*
+    getText( AtkText *pText ) throw (uno::RuntimeException)
+{
+    AtkObjectWrapper *pWrap = ATK_OBJECT_WRAPPER( pText );
+    if( pWrap )
+    {
+        if( !pWrap->mpText && pWrap->mpContext )
+        {
+            uno::Any any = pWrap->mpContext->queryInterface( accessibility::XAccessibleText::static_type(NULL) );
+            pWrap->mpText = reinterpret_cast< accessibility::XAccessibleText * > (any.pReserved);
+            pWrap->mpText->acquire();
+        }
+
+        return pWrap->mpText;
+    }
+
+    return NULL;
+}
+
+/*****************************************************************************/
+
+extern "C" {
+
+static gchar *
+text_wrapper_get_text (AtkText *text,
+                       gint     start_offset,
+                       gint     end_offset)
+{
+    gchar * ret = NULL;
+
+    g_return_val_if_fail( (end_offset == -1) || (end_offset >= start_offset), NULL );
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            rtl::OString aUtf8 = rtl::OUStringToOString( pText->getText(), RTL_TEXTENCODING_UTF8 );
+            if( -1 == end_offset )
+                ret = g_strdup( aUtf8.getStr() );
+            else
+                ret = g_strndup( aUtf8.getStr() + start_offset, end_offset - start_offset );
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getText()" );
+    }
+
+#ifdef ENABLE_TRACING
+    fprintf(stderr, "text_wrapper_get_text( %d,%d ) returns %s\n", start_offset, end_offset, ret);
+#endif
+    return ret;
+}
+
+static gchar *
+text_wrapper_get_text_after_offset (AtkText          *text,
+                                    gint             offset,
+                                    AtkTextBoundary  boundary_type,
+                                    gint             *start_offset,
+                                    gint             *end_offset)
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            accessibility::TextSegment aTextSegment = pText->getTextBehindIndex(offset, text_type_from_boundary(boundary_type));
+            return adjust_boundaries(pText, aTextSegment, boundary_type, start_offset, end_offset);
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in get_text_after_offset()" );
+    }
+
+    return NULL;
+}
+
+static gchar *
+text_wrapper_get_text_at_offset (AtkText          *text,
+                                 gint             offset,
+                                 AtkTextBoundary  boundary_type,
+                                 gint             *start_offset,
+                                 gint             *end_offset)
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            accessibility::TextSegment aTextSegment = pText->getTextAtIndex(offset, text_type_from_boundary(boundary_type));
+            return adjust_boundaries(pText, aTextSegment, boundary_type, start_offset, end_offset);
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in get_text_at_offset()" );
+    }
+
+    return NULL;
+}
+
+static gunichar
+text_wrapper_get_character_at_offset (AtkText          *text,
+                                      gint             offset)
+{
+    gint start, end;
+    gunichar uc = 0;
+
+    gchar * char_as_string =
+        text_wrapper_get_text_at_offset(text, offset, ATK_TEXT_BOUNDARY_CHAR,
+                                        &start, &end);
+    if( char_as_string )
+    {
+        uc = g_utf8_get_char( char_as_string );
+        g_free( char_as_string );
+    }
+
+    return uc;
+}
+
+static gchar *
+text_wrapper_get_text_before_offset (AtkText          *text,
+                                     gint             offset,
+                                     AtkTextBoundary  boundary_type,
+                                     gint             *start_offset,
+                                     gint             *end_offset)
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            accessibility::TextSegment aTextSegment = pText->getTextBeforeIndex(offset, text_type_from_boundary(boundary_type));
+            return adjust_boundaries(pText, aTextSegment, boundary_type, start_offset, end_offset);
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in text_before_offset()" );
+    }
+
+    return NULL;
+}
+
+static gint
+text_wrapper_get_caret_offset (AtkText          *text)
+{
+    gint offset = -1;
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            offset = pText->getCaretPosition();
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getCaretPosition()" );
+    }
+
+#ifdef ENABLE_TRACING
+    fprintf(stderr, "get_caret_offset(%p) returns %d\n", text, offset);
+#endif
+
+    return offset;
+}
+
+static gboolean
+text_wrapper_set_caret_offset (AtkText *text,
+                               gint     offset)
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            return pText->setCaretPosition( offset );
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in setCaretPosition()" );
+    }
+
+    return FALSE;
+}
+
+static AtkAttributeSet *
+text_wrapper_get_run_attributes( AtkText        *text,
+                                 gint           offset,
+                                 gint           *start_offset,
+                                 gint           *end_offset)
+{
+    AtkAttributeSet *pSet = NULL;
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            uno::Sequence< beans::PropertyValue > aAttributeList =
+                pText->getCharacterAttributes( offset, uno::Sequence< rtl::OUString > () );
+
+            pSet = attribute_set_new_from_property_values( aAttributeList, beans::PropertyState_DIRECT_VALUE );
+
+            accessibility::TextSegment aTextSegment =
+                pText->getTextAtIndex(offset, accessibility::AccessibleTextType::ATTRIBUTE_RUN);
+
+            *start_offset = aTextSegment.SegmentStart;
+            *end_offset = aTextSegment.SegmentEnd + 1; // FIXME: TESTME
+        }
+    }
+    catch(const uno::Exception& e) {
+
+        g_warning( "Exception in get_run_attributes()" );
+
+        if( pSet )
+        {
+            atk_attribute_set_free( pSet );
+            pSet = NULL;
+        }
+    }
+
+    return pSet;
+}
+
+/*****************************************************************************/
+
+static AtkAttributeSet *
+text_wrapper_get_default_attributes( AtkText *text )
+{
+    AtkAttributeSet *pSet = NULL;
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            uno::Sequence< beans::PropertyValue > aAttributeList =
+                pText->getCharacterAttributes( 0, uno::Sequence< rtl::OUString > () );
+
+            pSet = attribute_set_new_from_property_values( aAttributeList, beans::PropertyState_DEFAULT_VALUE );
+        }
+    }
+    catch(const uno::Exception& e) {
+
+        g_warning( "Exception in get_default_attributes()" );
+
+        if( pSet )
+        {
+            atk_attribute_set_free( pSet );
+            pSet = NULL;
+        }
+    }
+
+    return pSet;
+}
+
+/*****************************************************************************/
+
+static void
+text_wrapper_get_character_extents( AtkText          *text,
+                                    gint             offset,
+                                    gint             *x,
+                                    gint             *y,
+                                    gint             *width,
+                                    gint             *height,
+                                    AtkCoordType      coords )
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            *x = *y = *width = *height = 0;
+            awt::Rectangle aRect = pText->getCharacterBounds( offset );
+
+            gint origin_x = 0;
+            gint origin_y = 0;
+
+            if( coords == ATK_XY_SCREEN )
+            {
+                g_return_if_fail( ATK_IS_COMPONENT( text ) );
+                atk_component_get_position( ATK_COMPONENT( text ), &origin_x, &origin_y, coords);
+            }
+
+            *x = aRect.X + origin_x;
+            *y = aRect.Y + origin_y;
+            *width = aRect.Width;
+            *height = aRect.Height;
+
+#ifdef ENABLE_TRACING
+            fprintf(stderr, "get_character_extents(%d, %d) returns: %d,%d,%d,%d ",
+                offset, coords, *x, *y, *width, *height);
+#endif
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getCharacterBounds" );
+    }
+}
+
+static gint
+text_wrapper_get_character_count (AtkText *text)
+{
+    gint rv = 0;
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            rv = pText->getCharacterCount();
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getCharacterCount" );
+    }
+
+#ifdef ENABLE_TRACING
+    fprintf(stderr, "get_character_count(%p) returns: %d\n", text, rv);
+#endif
+
+    return rv;
+}
+
+static gint
+text_wrapper_get_offset_at_point (AtkText     *text,
+                                  gint         x,
+                                  gint         y,
+                                  AtkCoordType coords)
+{
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            gint origin_x = 0;
+            gint origin_y = 0;
+
+            if( coords == ATK_XY_SCREEN )
+            {
+                g_return_val_if_fail( ATK_IS_COMPONENT( text ), -1 );
+                atk_component_get_position( ATK_COMPONENT( text ), &origin_x, &origin_y, coords);
+            }
+
+            return pText->getIndexAtPoint( awt::Point(x - origin_x, y - origin_y) );
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getIndexAtPoint" );
+    }
+
+    return -1;
+}
+
+// FIXME: the whole series of selections API is problematic ...
+
+static gint
+text_wrapper_get_n_selections (AtkText *text)
+{
+    gint rv = 0;
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            rv = ( pText->getSelectionEnd() > pText->getSelectionStart() ) ? 1 : 0;
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getSelectionEnd() or getSelectionStart()" );
+    }
+
+#ifdef ENABLE_TRACING
+    fprintf(stderr, "get_n_selections(%p) returns %d\n", text, rv);
+#endif
+
+    return rv;
+}
+
+static gchar *
+text_wrapper_get_selection (AtkText *text,
+                            gint     selection_num,
+                            gint    *start_offset,
+                            gint    *end_offset)
+{
+    g_return_val_if_fail( selection_num == 0, FALSE );
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+        {
+            *start_offset = pText->getSelectionStart();
+            *end_offset   = pText->getSelectionEnd();
+
+            return OUStringToGChar( pText->getSelectedText() );
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getSelectionEnd(), getSelectionStart() or getSelectedText()" );
+    }
+
+    return NULL;
+}
+
+static gboolean
+text_wrapper_add_selection (AtkText *text,
+                            gint     start_offset,
+                            gint     end_offset)
+{
+    // FIXME: can we try to be more compatible by expanding an
+    //        existing adjacent selection ?
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            return pText->setSelection( start_offset, end_offset ); // ?
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in setSelection()" );
+    }
+
+    return FALSE;
+}
+
+static gboolean
+text_wrapper_remove_selection (AtkText *text,
+                               gint     selection_num)
+{
+    g_return_val_if_fail( selection_num == 0, FALSE );
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            return pText->setSelection( 0, 0 ); // ?
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in setSelection()" );
+    }
+
+    return FALSE;
+}
+
+static gboolean
+text_wrapper_set_selection (AtkText *text,
+                            gint     selection_num,
+                            gint     start_offset,
+                            gint     end_offset)
+{
+    g_return_val_if_fail( selection_num == 0, FALSE );
+
+    try {
+        accessibility::XAccessibleText* pText = getText( text );
+        if( pText )
+            return pText->setSelection( start_offset, end_offset );
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in setSelection()" );
+    }
+
+    return FALSE;
+}
+
+} // extern "C"
+
+void
+textIfaceInit (AtkTextIface *iface)
+{
+  g_return_if_fail (iface != NULL);
+
+  iface->get_text = text_wrapper_get_text;
+  iface->get_character_at_offset = text_wrapper_get_character_at_offset;
+  iface->get_text_before_offset = text_wrapper_get_text_before_offset;
+  iface->get_text_at_offset = text_wrapper_get_text_at_offset;
+  iface->get_text_after_offset = text_wrapper_get_text_after_offset;
+  iface->get_caret_offset = text_wrapper_get_caret_offset;
+  iface->set_caret_offset = text_wrapper_set_caret_offset;
+  iface->get_character_count = text_wrapper_get_character_count;
+  iface->get_n_selections = text_wrapper_get_n_selections;
+  iface->get_selection = text_wrapper_get_selection;
+  iface->add_selection = text_wrapper_add_selection;
+  iface->remove_selection = text_wrapper_remove_selection;
+  iface->set_selection = text_wrapper_set_selection;
+  iface->get_run_attributes = text_wrapper_get_run_attributes;
+  iface->get_default_attributes = text_wrapper_get_default_attributes;
+  iface->get_character_extents = text_wrapper_get_character_extents;
+  iface->get_offset_at_point = text_wrapper_get_offset_at_point;
+}
