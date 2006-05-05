b843ce260c8f97c19e76ce85c2c49872f5af580b
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:59:35 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2006/03/31 14:09:18 obr 1.1.2.13: #i63580# AtkRectangle already in atk 1.7.0, but ATK_TYPE_RECTANGLE in 1.7.2 2006/02/17 06:09:46 obr 1.1.2.12: #i47890# removed useless target.pmk 2006/02/16 11:08:14 obr 1.1.2.11: #i47890# removed build conditionals (EXPERIMENTAL_ATKBRIDGE) 2006/01/12 13:56:16 obr 1.1.2.10: #i47890# Solaris compile fixes 2006/01/05 14:06:35 obr 1.1.2.9: #i47890# override toolkit name and version and reworked bridge initialization 2005/12/22 08:24:32 obr 1.1.2.8: #i47890# Moved the text attribute implementations to a dedicated source file and fixet atk_text_get_text 2005/11/15 11:38:50 mmeeks 1.1.2.7: Issue number:  i#47890# Submitted by:  mmeeks
    Fix experimental / conditional on build.
    2005/11/04 14:56:28 mmeeks 1.1.2.6: i#47890#
    
    Add 'EXPERIMENTAL' conditionals all around the a11y bits.
    2005/10/20 07:09:13 obr 1.1.2.5: #i47890# made cxx files standalone, avoid queryInterface on each API call and demacrofied try/catch to include appropriate warnings
    2005/10/11 09:49:52 obr 1.1.2.4: #i47890# filter sub-menu and combo-box popup windows
    2005/09/26 11:01:02 obr 1.1.2.3: #i47890# reworked lifecycle of atk wrapper objects
    2005/06/14 13:57:25 obr 1.1.2.2: #i47890# global focus event listening stuff
    2005/04/21 14:59:32 mmeeks 1.1.2.1: Issue number: i#47890#
    Submitted by: mmeeks
    
    Hacked up prototype of atk bridge

---
+++ vcl/unx/gtk/a11y/makefile.mk
@@ -0,0 +1,102 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2006-05-05 10:59:35 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PRJNAME=vcl
+TARGET=gtka11y
+ENABLE_EXCEPTIONS=TRUE
+
+# workaround for makedepend hang
+MKDEPENDSOLVER=
+LIBTARGET=NO
+NO_DEFAULT_STL=YES
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+# --- Files --------------------------------------------------------
+
+.IF "$(GUIBASE)"!="unx"
+
+dummy:
+    @echo "Nothing to build for GUIBASE $(GUIBASE)"
+
+.ELSE		# "$(GUIBASE)"!="unx"
+
+.IF "$(ENABLE_GTK)" != ""
+
+PKGCONFIG_MODULES=gtk+-2.0
+.INCLUDE : pkg_config.mk
+
+CFLAGS+=-DVERSION=\"$(UPD)$(LAST_MINOR)\"
+
+ATKVERSION:=$(shell $(PKGCONFIG) --modversion atk | $(AWK) -v num=true -f $(SOLARENV)$/bin$/getcompver.awk)
+
+.IF $(ATKVERSION) >= "000100070000"
+CFLAGS+=-DHAS_ATKRECTANGLE
+.ENDIF
+
+LIB1TARGET=$(SLB)$/$(TARGET).lib
+LIB1OBJFILES=\
+    $(SLO)$/atkaction.obj \
+    $(SLO)$/atkbridge.obj \
+    $(SLO)$/atkcomponent.obj \
+    $(SLO)$/atkeditabletext.obj \
+    $(SLO)$/atkfactory.obj \
+    $(SLO)$/atkhypertext.obj \
+    $(SLO)$/atkimage.obj \
+    $(SLO)$/atklistener.obj \
+    $(SLO)$/atkselection.obj \
+    $(SLO)$/atktable.obj \
+    $(SLO)$/atktext.obj \
+    $(SLO)$/atktextattributes.obj \
+    $(SLO)$/atkutil.obj \
+    $(SLO)$/atkvalue.obj \
+    $(SLO)$/atkwindow.obj \
+    $(SLO)$/atkwrapper.obj
+
+.ELSE # "$(ENABLE_GTK)" != ""
+
+dummy:
+    @echo GTK disabled - nothing to build
+.ENDIF
+.ENDIF		# "$(GUIBASE)"!="unx"
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+
