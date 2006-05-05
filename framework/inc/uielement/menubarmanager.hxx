75d54f646dc0ade540cb32f15f62a181bf7d0dff
Rüdiger Timm <rt@openoffice.org>
2006-05-05 08:53:39 +0000

    INTEGRATION: CWS fwkc03fixes (1.14.82); FILE MERGED 2006/05/02 15:53:52 jl 1.14.82.1: #135271# Crash caused by CLI when exiting office

--- framework/inc/uielement/menubarmanager.hxx
+++ framework/inc/uielement/menubarmanager.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: menubarmanager.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-05 18:10:16 $
+ *  last change: $Author: rt $ $Date: 2006-05-05 09:53:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -296,7 +296,7 @@ class MenuBarManager : public com::sun::star::frame::XStatusListener            
         ::com::sun::star::uno::Reference< ::com::sun::star::ui::XAcceleratorConfiguration >            m_xModuleAcceleratorManager;
         ::com::sun::star::uno::Reference< ::com::sun::star::ui::XAcceleratorConfiguration >            m_xGlobalAcceleratorManager;
         // #110897#
-        const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >&        mxServiceFactory;
+        ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >               mxServiceFactory;
         sal_Int16                                                                                      m_nSymbolsStyle;
 };
 
