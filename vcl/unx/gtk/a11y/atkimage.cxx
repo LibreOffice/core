c05c763ea5657ce3212caa0a3c8370c0ee57001c
Rüdiger Timm <rt@openoffice.org>
2006-05-05 09:55:05 +0000

    INTEGRATION: CWS atkbridge (1.1.2); FILE ADDED 2006/03/31 12:19:39 obr 1.1.2.5: #i63583# eliminated warnings 2006/02/15 10:59:23 obr 1.1.2.4: #i47890# replaced tabs with spaces 2006/01/12 13:56:11 obr 1.1.2.3: #i47890# Solaris compile fixes 2005/10/20 07:09:10 obr 1.1.2.2: #i47890# made cxx files standalone, avoid queryInterface on each API call and demacrofied try/catch to include appropriate warnings 2005/05/31 11:55:06 mmeeks 1.1.2.1: Issue number: i#47890# Submitted by: mmeeks
    	Add AtkHypertext, AtkImage impls.

---
+++ vcl/unx/gtk/a11y/atkimage.cxx
@@ -0,0 +1,143 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: atkimage.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-05-05 10:55:05 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "atkwrapper.hxx"
+
+#include <com/sun/star/accessibility/XAccessibleImage.hpp>
+
+#include <stdio.h>
+
+using namespace ::com::sun::star;
+
+// FIXME
+static G_CONST_RETURN gchar *
+getAsConst( rtl::OUString rString )
+{
+    static const int nMax = 10;
+    static rtl::OString aUgly[nMax];
+    static int nIdx = 0;
+    nIdx = (nIdx + 1) % nMax;
+    aUgly[nIdx] = rtl::OUStringToOString( rString, RTL_TEXTENCODING_UTF8 );
+    return aUgly[ nIdx ];
+}
+
+static accessibility::XAccessibleImage*
+    getImage( AtkImage *pImage ) throw (uno::RuntimeException)
+{
+    AtkObjectWrapper *pWrap = ATK_OBJECT_WRAPPER( pImage );
+    if( pWrap )
+    {
+        if( !pWrap->mpImage && pWrap->mpContext )
+        {
+            uno::Any any = pWrap->mpContext->queryInterface( accessibility::XAccessibleImage::static_type(NULL) );
+            pWrap->mpImage = reinterpret_cast< accessibility::XAccessibleImage * > (any.pReserved);
+            pWrap->mpImage->acquire();
+        }
+
+        return pWrap->mpImage;
+    }
+
+    return NULL;
+}
+
+extern "C" {
+
+static G_CONST_RETURN gchar *
+image_get_image_description( AtkImage *image )
+{
+    try {
+        accessibility::XAccessibleImage* pImage = getImage( image );
+        if( pImage )
+            return getAsConst( pImage->getAccessibleImageDescription() );
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getAccessibleImageDescription()" );
+    }
+
+    return NULL;
+}
+
+static void
+image_get_image_position( AtkImage     *image,
+                          gint         *x,
+                          gint         *y,
+                          AtkCoordType  coord_type )
+{
+    *x = *y = 0;
+    if( ATK_IS_COMPONENT( image ) )
+        atk_component_get_position( ATK_COMPONENT( image ), x, y, coord_type );
+    else
+        g_warning( "FIXME: no image position information" );
+}
+
+static void
+image_get_image_size( AtkImage *image,
+                      gint     *width,
+                      gint     *height )
+{
+    *width = 0;
+    *height = 0;
+    try {
+        accessibility::XAccessibleImage* pImage = getImage( image );
+        if( pImage )
+        {
+            *width = pImage->getAccessibleImageWidth();
+            *height = pImage->getAccessibleImageHeight();
+        }
+    }
+    catch(const uno::Exception& e) {
+        g_warning( "Exception in getAccessibleImageHeight() or Width" );
+    }
+}
+
+static gboolean
+image_set_image_description( AtkImage *, const gchar * )
+{
+    g_warning ("FIXME: no set image description");
+    return FALSE;
+}
+
+} // extern "C"
+
+void
+imageIfaceInit (AtkImageIface *iface)
+{
+  g_return_if_fail (iface != NULL);
+
+  iface->set_image_description = image_set_image_description;
+  iface->get_image_description = image_get_image_description;
+  iface->get_image_position = image_get_image_position;
+  iface->get_image_size = image_get_image_size;
+}
