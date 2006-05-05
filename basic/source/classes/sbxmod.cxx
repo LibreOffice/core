614d9d4bc690a3e528edace78d83e45fc21066de
Rüdiger Timm <rt@openoffice.org>
2006-05-05 07:47:08 +0000

    INTEGRATION: CWS ab26 (1.28.10); FILE MERGED 2006/04/28 06:42:43 ab 1.28.10.1: #i64834# Syntax highlighting: byte as keyword

--- basic/source/classes/sbxmod.cxx
+++ basic/source/classes/sbxmod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbxmod.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 14:01:25 $
+ *  last change: $Author: rt $ $Date: 2006-05-05 08:47:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -109,6 +109,7 @@ static char* strListBasicKeyWords[] = {
     "binary",
     "boolean",
     "byref",
+    "byte",
     "byval",
     "call",
     "case",
