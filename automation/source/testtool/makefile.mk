5c88ccfd5052dc90f71fdd9669828d1f4f09387a
Rüdiger Timm <rt@openoffice.org>
2006-05-05 07:06:10 +0000

    INTEGRATION: CWS gh12a (1.5.34); FILE MERGED 2006/05/03 13:52:22 gh 1.5.34.1: #i63675# add testtool.ini/rc to installation

--- automation/source/testtool/makefile.mk
+++ automation/source/testtool/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:31:53 $
+#   last change: $Author: rt $ $Date: 2006-05-05 08:06:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -100,4 +100,18 @@ $(MISC)$/xfilter.pl : filter.pl
        +$(COPY) filter.pl $(MISC)$/xfilter.pl
        +attrib  -r $(MISC)$/xfilter.pl
 .ENDIF
+
+
+
+.IF "$(GUI)"=="UNX"
+INIFILE=$(BIN)$/.testtoolrc
+.ELIF "$(GUI)"=="WNT"
+INIFILE=$(BIN)$/testtool.ini
+.END
+
+$(INIFILE): testtool.ini
+        +-$(COPY) $< $@
+
+ALLTAR: \
+        $(INIFILE)
 
