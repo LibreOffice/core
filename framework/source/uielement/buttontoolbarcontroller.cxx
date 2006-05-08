f7cbe4078baaf78d921d8e8bd70092e4af9fbc4c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-05-08 14:18:40 +0000

    INTEGRATION: CWS tbcontrols (1.1.2); FILE ADDED 2006/04/27 16:10:28 cd 1.1.2.1: #i64501# Added simple button without status updates (supporting fast startup)

---
+++ framework/source/uielement/buttontoolbarcontroller.cxx
@@ -0,0 +1,356 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: buttontoolbarcontroller.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-05-08 15:18:40 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_UIELEMENT_BUTTONTOOLBARCONTROLLER_HXX
+#include "uielement/buttontoolbarcontroller.hxx"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_TOOLBAR_HXX_
+#include "uielement/toolbar.hxx"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_UTIL_XURLTRANSFORMER_HPP_
+#include <com/sun/star/util/XURLTransformer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCHPROVIDER_HPP_
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
+#include <com/sun/star/lang/DisposedException.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XMACROEXPANDER_HPP_
+#include "com/sun/star/util/XMacroExpander.hpp"
+#endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include "com/sun/star/uno/XComponentContext.hpp"
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include "com/sun/star/beans/XPropertySet.hpp"
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#include <rtl/uri.hxx>
+#include <vos/mutex.hxx>
+#include <comphelper/processfactory.hxx>
+#include <unotools/ucbstreamhelper.hxx>
+#include <tools/urlobj.hxx>
+#include <vcl/svapp.hxx>
+#include <vcl/mnemonic.hxx>
+#include <vcl/window.hxx>
+#include <vcl/graph.hxx>
+#include <vcl/bitmap.hxx>
+#include <svtools/filter.hxx>
+#include <svtools/miscopt.hxx>
+
+using namespace ::rtl;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::frame;
+using namespace ::com::sun::star::util;
+
+namespace framework
+{
+
+ButtonToolbarController::ButtonToolbarController(
+    const uno::Reference< lang::XMultiServiceFactory >& rServiceManager,
+    const uno::Reference< frame::XFrame >&              rFrame,
+    ToolBar*                                            pToolBar,
+    USHORT                                              nID,
+    const rtl::OUString&                                aCommand ) :
+    cppu::OWeakObject(),
+    m_bInitialized( sal_False ),
+    m_bDisposed( sal_False ),
+    m_aCommandURL( aCommand ),
+    m_xServiceManager( rServiceManager ),
+    m_pToolbar( pToolBar )
+{
+}
+
+ButtonToolbarController::~ButtonToolbarController()
+{
+}
+
+        // XInterface
+uno::Any SAL_CALL ButtonToolbarController::queryInterface( const uno::Type& rType )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    Any a = ::cppu::queryInterface(
+                rType ,
+                static_cast< frame::XStatusListener* >( this ),
+                static_cast< frame::XToolbarController* >( this ),
+                static_cast< lang::XInitialization* >( this ),
+                static_cast< lang::XComponent* >( this ),
+                static_cast< util::XUpdatable* >( this ));
+
+    if ( a.hasValue() )
+        return a;
+
+    return cppu::OWeakObject::queryInterface( rType );
+}
+
+void SAL_CALL ButtonToolbarController::acquire() throw ()
+{
+    cppu::OWeakObject::acquire();
+}
+
+void SAL_CALL ButtonToolbarController::release() throw ()
+{
+    cppu::OWeakObject::release();
+}
+
+// XInitialization
+void SAL_CALL ButtonToolbarController::initialize(
+    const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments )
+throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException)
+{
+    const rtl::OUString aFrameName( RTL_CONSTASCII_USTRINGPARAM( "Frame" ));
+    const rtl::OUString aCommandURLName( RTL_CONSTASCII_USTRINGPARAM( "CommandURL" ));
+    const rtl::OUString aServiceManagerName( RTL_CONSTASCII_USTRINGPARAM( "ServiceManager" ));
+
+    bool bInitialized( true );
+
+    {
+        vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+        if ( m_bDisposed )
+            throw DisposedException();
+
+        bInitialized = m_bInitialized;
+    }
+
+    if ( !bInitialized )
+    {
+        vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+        m_bInitialized = sal_True;
+
+        PropertyValue aPropValue;
+        for ( int i = 0; i < aArguments.getLength(); i++ )
+        {
+            if ( aArguments[i] >>= aPropValue )
+            {
+                if ( aPropValue.Name.equalsAscii( "Frame" ))
+                    m_xFrame.set(aPropValue.Value,UNO_QUERY);
+                else if ( aPropValue.Name.equalsAscii( "CommandURL" ))
+                    aPropValue.Value >>= m_aCommandURL;
+                else if ( aPropValue.Name.equalsAscii( "ServiceManager" ))
+                    m_xServiceManager.set(aPropValue.Value,UNO_QUERY);
+            }
+        }
+    }
+}
+
+// XComponent
+void SAL_CALL ButtonToolbarController::dispose() throw (::com::sun::star::uno::RuntimeException)
+{
+    Reference< XComponent > xThis( static_cast< OWeakObject* >(this), UNO_QUERY );
+
+    {
+        vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+        if ( m_bDisposed )
+            throw DisposedException();
+
+        m_xServiceManager.clear();
+        m_xURLTransformer.clear();
+        m_xFrame.clear();
+        m_pToolbar = 0;
+        m_bDisposed = sal_True;
+    }
+}
+
+void SAL_CALL ButtonToolbarController::addEventListener(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    // do nothing
+}
+
+void SAL_CALL ButtonToolbarController::removeEventListener(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    // do nothing
+}
+
+// XUpdatable
+void SAL_CALL ButtonToolbarController::update()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+    if ( m_bDisposed )
+        throw DisposedException();
+}
+
+// XEventListener
+void SAL_CALL ButtonToolbarController::disposing(
+    const com::sun::star::lang::EventObject& Source )
+throw ( ::com::sun::star::uno::RuntimeException )
+{
+    uno::Reference< uno::XInterface > xSource( Source.Source );
+
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+    if ( m_bDisposed )
+        return;
+
+    uno::Reference< uno::XInterface > xIfac( m_xFrame, uno::UNO_QUERY );
+    if ( xIfac == xSource )
+        m_xFrame.clear();
+}
+
+void SAL_CALL ButtonToolbarController::statusChanged( const ::com::sun::star::frame::FeatureStateEvent& Event )
+throw ( ::com::sun::star::uno::RuntimeException )
+{
+    // do nothing
+    if ( m_bDisposed )
+        throw DisposedException();
+}
+
+// XToolbarController
+void SAL_CALL ButtonToolbarController::execute( sal_Int16 KeyModifier )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    uno::Reference< frame::XDispatch >      xDispatch;
+    uno::Reference< frame::XFrame >         xFrame;
+    uno::Reference< util::XURLTransformer > xURLTransformer;
+    rtl::OUString                           aCommandURL;
+    ::com::sun::star::util::URL             aTargetURL;
+
+    {
+        vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+        if ( m_bDisposed )
+            throw DisposedException();
+
+        if ( m_bInitialized &&
+             m_xFrame.is() &&
+             m_xServiceManager.is() &&
+             m_aCommandURL.getLength() )
+        {
+            if ( !m_xURLTransformer.is() )
+            {
+                m_xURLTransformer = uno::Reference< util::XURLTransformer >(
+                    m_xServiceManager->createInstance( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.util.URLTransformer" ))),
+                    uno::UNO_QUERY_THROW );
+            }
+
+            xFrame          = m_xFrame;
+            aCommandURL     = m_aCommandURL;
+            xURLTransformer = m_xURLTransformer;
+        }
+    }
+
+    uno::Reference< frame::XDispatchProvider > xDispatchProvider( xFrame, uno::UNO_QUERY );
+    if ( xDispatchProvider.is() )
+    {
+        aTargetURL.Complete = aCommandURL;
+        xURLTransformer->parseStrict( aTargetURL );
+        xDispatch = xDispatchProvider->queryDispatch( aTargetURL, ::rtl::OUString(), 0 );
+    }
+
+    if ( xDispatch.is() )
+    {
+        try
+        {
+            Sequence<PropertyValue>   aArgs( 1 );
+
+            // Provide key modifier information to dispatch function
+            aArgs[0].Name   = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "KeyModifier" ));
+            aArgs[0].Value  = makeAny( KeyModifier );
+
+            xDispatch->dispatch( aTargetURL, aArgs );
+        }
+        catch ( DisposedException& )
+        {
+        }
+    }
+}
+
+void SAL_CALL ButtonToolbarController::click()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    vos::OGuard aSolarMutexGuard( Application::GetSolarMutex() );
+
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    sal_Int16   nKeyModifier( (sal_Int16)m_pToolbar->GetModifier() );
+    execute( nKeyModifier );
+}
+
+void SAL_CALL ButtonToolbarController::doubleClick()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    // do nothing
+    if ( m_bDisposed )
+        throw DisposedException();
+}
+
+uno::Reference< awt::XWindow > SAL_CALL ButtonToolbarController::createPopupWindow()
+throw (::com::sun::star::uno::RuntimeException)
+{
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    return uno::Reference< awt::XWindow >();
+}
+
+uno::Reference< awt::XWindow > SAL_CALL ButtonToolbarController::createItemWindow(
+    const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindow >& Parent )
+throw (::com::sun::star::uno::RuntimeException)
+{
+    if ( m_bDisposed )
+        throw DisposedException();
+
+    return uno::Reference< awt::XWindow >();
+}
+
+} // namespace
