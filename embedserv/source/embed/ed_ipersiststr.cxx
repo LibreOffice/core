a65619bb72cd8e8c1fa28bf703ae077c318704c3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-05-08 14:01:04 +0000

    INTEGRATION: CWS mbapp3 (1.16.2); FILE MERGED 2006/04/06 11:43:10 mba 1.16.2.1: #134222#: don't use GlobalMutex for acces to RefCount

--- embedserv/source/embed/ed_ipersiststr.cxx
+++ embedserv/source/embed/ed_ipersiststr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ed_ipersiststr.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-05 09:55:46 $
+ *  last change: $Author: hr $ $Date: 2006-05-08 15:01:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -388,10 +388,7 @@ STDMETHODIMP_(ULONG) EmbedDocument_Impl::Release()
 
     sal_Int32 nCount = osl_decrementInterlockedCount( &m_refCount );
     if ( nCount == 0 )
-    {
         delete this;
-    }
-
     return nCount;
 }
 
