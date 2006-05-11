aeb138156a4c1ef6751132fcf3f20297239f906f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-05-11 12:32:48 +0000

    INTEGRATION: CWS atkbridge2 (1.2.4); FILE MERGED 2006/05/08 13:45:16 obr 1.2.4.1: #135353# fixed crashes when closing sub-toolbars and application exit

--- vcl/unx/inc/plugins/gtk/atkbridge.hxx
+++ vcl/unx/inc/plugins/gtk/atkbridge.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atkbridge.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-05 11:01:21 $
+ *  last change: $Author: hr $ $Date: 2006-05-11 13:32:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,6 +40,7 @@
 #include <dllapi.h>
 #endif
 
-void VCL_DLLPUBLIC InitAtkBridge(void);
+bool VCL_DLLPUBLIC InitAtkBridge(void);
+void VCL_DLLPUBLIC DeInitAtkBridge(void);
 
 #endif
