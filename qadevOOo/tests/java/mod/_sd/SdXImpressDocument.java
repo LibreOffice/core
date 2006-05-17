7271bfdbf8beaf6f4a96d77b757fbbdd08386ede
Vladimir Glazounov <vg@openoffice.org>
2006-05-17 12:39:44 +0000

    INTEGRATION: CWS qadev26 (1.7.12); FILE MERGED 2006/02/20 15:00:32 cn 1.7.12.1: #i62338# make added shape transparent to have a regression test for UI which pops up while printing transparent shapes via API

--- qadevOOo/tests/java/mod/_sd/SdXImpressDocument.java
+++ qadevOOo/tests/java/mod/_sd/SdXImpressDocument.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdXImpressDocument.java,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:19:01 $
+ *  last change: $Author: vg $ $Date: 2006-05-17 13:39:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,15 +34,16 @@
  ************************************************************************/
 package mod._sd;
 
-import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertySet;
 import com.sun.star.drawing.XShape;
 import com.sun.star.frame.XController;
 import com.sun.star.frame.XModel;
+import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiServiceFactory;
 import com.sun.star.uno.UnoRuntime;
-import com.sun.star.view.PrintJobEvent;
-import com.sun.star.view.XPrintable;
 import com.sun.star.view.XSelectionSupplier;
 import ifc.view._XPrintJobBroadcaster;
 import java.io.File;
@@ -53,7 +54,6 @@ import lib.StatusException;
 import lib.TestCase;
 import lib.TestEnvironment;
 import lib.TestParameters;
-import share.LogWriter;
 
 import util.DrawTools;
 import util.SOfficeFactory;
@@ -61,57 +61,57 @@ import util.utils;
 
 
 /**
-* Test for object which is represented by service
-* <code>com.sun.star.presentation.PresentationDocument</code>. <p>
-* Object implements the following interfaces :
-* <ul>
-*  <li> <code>com::sun::star::lang::XMultiServiceFactory</code></li>
-*  <li> <code>com::sun::star::drawing::XMasterPagesSupplier</code></li>
-*  <li> <code>com::sun::star::presentation::XCustomPresentationSupplier</code></li>
-*  <li> <code>com::sun::star::document::XLinkTargetSupplier</code></li>
-*  <li> <code>com::sun::star::beans::XPropertySet</code></li>
-*  <li> <code>com::sun::star::drawing::XLayerSupplier</code></li>
-*  <li> <code>com::sun::star::presentation::XPresentationSupplier</code></li>
-*  <li> <code>com::sun::star::style::XStyleFamiliesSupplier</code></li>
-*  <li> <code>com::sun::star::drawing::DrawingDocument</code></li>
-*  <li> <code>com::sun::star::drawing::XDrawPageDuplicator</code></li>
-*  <li> <code>com::sun::star::drawing::XDrawPagesSupplier</code></li>
-* </ul>
-* @see com.sun.star.presentation.PresentationDocument
-* @see com.sun.star.lang.XMultiServiceFactory
-* @see com.sun.star.drawing.XMasterPagesSupplier
-* @see com.sun.star.presentation.XCustomPresentationSupplier
-* @see com.sun.star.document.XLinkTargetSupplier
-* @see com.sun.star.beans.XPropertySet
-* @see com.sun.star.drawing.XLayerSupplier
-* @see com.sun.star.presentation.XPresentationSupplier
-* @see com.sun.star.style.XStyleFamiliesSupplier
-* @see com.sun.star.drawing.DrawingDocument
-* @see com.sun.star.drawing.XDrawPageDuplicator
-* @see com.sun.star.drawing.XDrawPagesSupplier
-* @see ifc.lang._XMultiServiceFactory
-* @see ifc.drawing._XMasterPagesSupplier
-* @see ifc.presentation._XCustomPresentationSupplier
-* @see ifc.document._XLinkTargetSupplier
-* @see ifc.beans._XPropertySet
-* @see ifc.drawing._XLayerSupplier
-* @see ifc.presentation._XPresentationSupplier
-* @see ifc.style._XStyleFamiliesSupplier
-* @see ifc.drawing._DrawingDocument
-* @see ifc.drawing._XDrawPageDuplicator
-* @see ifc.drawing._XDrawPagesSupplier
-*/
+ * Test for object which is represented by service
+ * <code>com.sun.star.presentation.PresentationDocument</code>. <p>
+ * Object implements the following interfaces :
+ * <ul>
+ *  <li> <code>com::sun::star::lang::XMultiServiceFactory</code></li>
+ *  <li> <code>com::sun::star::drawing::XMasterPagesSupplier</code></li>
+ *  <li> <code>com::sun::star::presentation::XCustomPresentationSupplier</code></li>
+ *  <li> <code>com::sun::star::document::XLinkTargetSupplier</code></li>
+ *  <li> <code>com::sun::star::beans::XPropertySet</code></li>
+ *  <li> <code>com::sun::star::drawing::XLayerSupplier</code></li>
+ *  <li> <code>com::sun::star::presentation::XPresentationSupplier</code></li>
+ *  <li> <code>com::sun::star::style::XStyleFamiliesSupplier</code></li>
+ *  <li> <code>com::sun::star::drawing::DrawingDocument</code></li>
+ *  <li> <code>com::sun::star::drawing::XDrawPageDuplicator</code></li>
+ *  <li> <code>com::sun::star::drawing::XDrawPagesSupplier</code></li>
+ * </ul>
+ * @see com.sun.star.presentation.PresentationDocument
+ * @see com.sun.star.lang.XMultiServiceFactory
+ * @see com.sun.star.drawing.XMasterPagesSupplier
+ * @see com.sun.star.presentation.XCustomPresentationSupplier
+ * @see com.sun.star.document.XLinkTargetSupplier
+ * @see com.sun.star.beans.XPropertySet
+ * @see com.sun.star.drawing.XLayerSupplier
+ * @see com.sun.star.presentation.XPresentationSupplier
+ * @see com.sun.star.style.XStyleFamiliesSupplier
+ * @see com.sun.star.drawing.DrawingDocument
+ * @see com.sun.star.drawing.XDrawPageDuplicator
+ * @see com.sun.star.drawing.XDrawPagesSupplier
+ * @see ifc.lang._XMultiServiceFactory
+ * @see ifc.drawing._XMasterPagesSupplier
+ * @see ifc.presentation._XCustomPresentationSupplier
+ * @see ifc.document._XLinkTargetSupplier
+ * @see ifc.beans._XPropertySet
+ * @see ifc.drawing._XLayerSupplier
+ * @see ifc.presentation._XPresentationSupplier
+ * @see ifc.style._XStyleFamiliesSupplier
+ * @see ifc.drawing._DrawingDocument
+ * @see ifc.drawing._XDrawPageDuplicator
+ * @see ifc.drawing._XDrawPagesSupplier
+ */
 public class SdXImpressDocument extends TestCase {
     XComponent xImpressDoc;
     XComponent xImpressDoc2;
 
     /**
-    * Called while disposing a <code>TestEnvironment</code>.
-    * Disposes Impress document.
-    * @param tParam test parameters
-    * @param tEnv the environment to cleanup
-    * @param log writer to log information while testing
-    */
+     * Called while disposing a <code>TestEnvironment</code>.
+     * Disposes Impress document.
+     * @param tParam test parameters
+     * @param tEnv the environment to cleanup
+     * @param log writer to log information while testing
+     */
     protected void cleanup(TestParameters Param, PrintWriter log) {
         log.println("disposing xImpressDoc");
         util.DesktopTools.closeDoc(xImpressDoc);;
@@ -119,33 +119,33 @@ public class SdXImpressDocument extends TestCase {
     }
 
     /**
-    * Creating a Testenvironment for the interfaces to be tested.
-    * Creates new impress document that is the instance of the service
-    * <code>com.sun.star.presentation.PresentationDocument</code>.
-    * @see com.sun.star.presentation.PresentationDocument
-    */
+     * Creating a Testenvironment for the interfaces to be tested.
+     * Creates new impress document that is the instance of the service
+     * <code>com.sun.star.presentation.PresentationDocument</code>.
+     * @see com.sun.star.presentation.PresentationDocument
+     */
     public synchronized TestEnvironment createTestEnvironment(TestParameters Param,
-                                                              PrintWriter log)
+        PrintWriter log)
         throws StatusException {
         log.println("creating a test environment");
 
         // get a soffice factory object
         SOfficeFactory SOF = SOfficeFactory.getFactory(
-                                     (XMultiServiceFactory) Param.getMSF());
+            (XMultiServiceFactory) Param.getMSF());
 
         try {
-            log.println("creating a impress document");
-            xImpressDoc = SOF.createImpressDoc(null);
+            log.println("creating two impress documents");
             xImpressDoc2 = SOF.createImpressDoc(null);
+            xImpressDoc = SOF.createImpressDoc(null);
         } catch (com.sun.star.uno.Exception e) {
             e.printStackTrace(log);
-            throw new StatusException("Couldn't create document", e);
+            throw new StatusException("Couldn't create documents", e);
         }
 
         XModel xModel1 = (XModel) UnoRuntime.queryInterface(XModel.class,
-                                                            xImpressDoc);
+            xImpressDoc);
         XModel xModel2 = (XModel) UnoRuntime.queryInterface(XModel.class,
-                                                            xImpressDoc2);
+            xImpressDoc2);
 
         XController cont1 = xModel1.getCurrentController();
         XController cont2 = xModel2.getCurrentController();
@@ -154,10 +154,30 @@ public class SdXImpressDocument extends TestCase {
         cont2.getFrame().setName("cont2");
 
         XSelectionSupplier sel = (XSelectionSupplier) UnoRuntime.queryInterface(
-                                         XSelectionSupplier.class, cont1);
+            XSelectionSupplier.class, cont1);
 
         XShape aShape = SOF.createShape(xImpressDoc, 5000, 3500, 7500, 5000,
-                                        "Rectangle");
+            "Rectangle");
+
+
+        XPropertySet xShapeProps = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, aShape);
+
+        try {
+            xShapeProps.setPropertyValue("FillStyle", com.sun.star.drawing.FillStyle.SOLID);
+            xShapeProps.setPropertyValue("FillTransparence", new Integer(50));
+        } catch (UnknownPropertyException ex) {
+            ex.printStackTrace(log);
+            throw new StatusException("Couldn't make shape transparent", ex);
+        } catch (PropertyVetoException ex) {
+            ex.printStackTrace(log);
+            throw new StatusException("Couldn't make shape transparent", ex);
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            ex.printStackTrace(log);
+            throw new StatusException("Couldn't make shape transparent", ex);
+        } catch (WrappedTargetException ex) {
+            ex.printStackTrace(log);
+            throw new StatusException("Couldn't make shape transparent", ex);
+        }
 
         DrawTools.getDrawPage(xImpressDoc, 0).add(aShape);
 
