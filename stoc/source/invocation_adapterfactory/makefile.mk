a4d46b8b12d4a6d1e7f40a8aa14cca2e84f00c9c
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:47:57 +0000

    INTEGRATION: CWS ause043 (1.11.10); FILE MERGED 2005/11/09 13:33:18 hjs 1.11.10.1: #i56702# remove unused variable

--- stoc/source/invocation_adapterfactory/makefile.mk
+++ stoc/source/invocation_adapterfactory/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 07:57:19 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:47:57 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@ PRJ=..$/..
 PRJNAME=stoc
 TARGET = invocadapt.uno
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC = TRUE
 COMP1TYPELIST = invadp
 
 # --- Settings -----------------------------------------------------
