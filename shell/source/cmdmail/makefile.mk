e16f4d25f01d057db21347bc69b7c86041bf1645
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 13:04:39 +0000

    INTEGRATION: CWS ause043 (1.5.22); FILE MERGED 2005/10/31 17:17:16 hjs 1.5.22.1: #i56702# remove overrides for default uno header generation

--- shell/source/cmdmail/makefile.mk
+++ shell/source/cmdmail/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:52:25 $
+#   last change: $Author: vg $ $Date: 2006-05-24 14:04:39 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,6 @@ TARGET=cmdmail
 LIBTARGET=NO
 ENABLE_EXCEPTIONS=TRUE
 COMP1TYPELIST=$(TARGET)
-COMPRDB=$(SOLARBINDIR)$/types.rdb
 
 # --- Settings -----------------------------------------------------
 
