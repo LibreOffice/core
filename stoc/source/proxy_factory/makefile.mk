4614db6bd1a53ce590eb67d9cc913833692ea04b
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:49:21 +0000

    INTEGRATION: CWS ause043 (1.9.10); FILE MERGED 2005/11/09 13:33:19 hjs 1.9.10.1: #i56702# remove unused variable

--- stoc/source/proxy_factory/makefile.mk
+++ stoc/source/proxy_factory/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 08:01:57 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:49:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,6 @@ PRJ=..$/..
 PRJNAME=stoc
 TARGET = proxyfac.uno
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC = TRUE
 COMP1TYPELIST = proxyfac
 
 # --- Settings -----------------------------------------------------
