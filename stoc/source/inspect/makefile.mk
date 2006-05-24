47665fde08a3de3d5deaf6526113a7bcf19c7812
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:47:07 +0000

    INTEGRATION: CWS ause043 (1.9.10); FILE MERGED 2005/11/09 13:33:17 hjs 1.9.10.1: #i56702# remove unused variable

--- stoc/source/inspect/makefile.mk
+++ stoc/source/inspect/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 07:56:07 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:47:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@ PRJ=..$/..
 PRJNAME=	stoc
 TARGET = introspection.uno
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC = TRUE
 COMP1TYPELIST = insp
 
 # --- Settings -----------------------------------------------------
