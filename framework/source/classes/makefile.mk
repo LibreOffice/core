2713e3220d44d9b8727428e20aaa250dd41a4f44
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:38:26 +0000

    INTEGRATION: CWS ause043 (1.33.38); FILE MERGED 2005/11/08 11:22:56 hjs 1.33.38.1: #i56702# removed useless BOOTSTRAP_SERVICE=FALSE (it is the default)

--- framework/source/classes/makefile.mk
+++ framework/source/classes/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.33 $
+#   $Revision: 1.34 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:12:27 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:38:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@ PRJ=..$/..
 PRJNAME=			framework
 TARGET=				fwk_classes
 ENABLE_EXCEPTIONS=	TRUE
-BOOTSTRAP_SERVICE=	FALSE
 
 # --- Settings -----------------------------------------------------
 
