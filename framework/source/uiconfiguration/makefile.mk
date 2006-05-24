b8ceb2405e815b029e9d5946b24440efe7e1f40f
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:41:14 +0000

    INTEGRATION: CWS ause043 (1.5.38); FILE MERGED 2005/12/22 12:58:31 hjs 1.5.38.2: RESYNC: (1.5-1.6); FILE MERGED 2005/11/08 11:23:04 hjs 1.5.38.1: #i56702# removed useless BOOTSTRAP_SERVICE=FALSE (it is the default)

--- framework/source/uiconfiguration/makefile.mk
+++ framework/source/uiconfiguration/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-11-11 12:07:27 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:41:14 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,15 +37,11 @@ PRJ=..$/..
 PRJNAME=			framework
 TARGET=				fwk_uiconfiguration
 USE_DEFFILE=		TRUE
-NO_BSYMBOLIC=		TRUE
 ENABLE_EXCEPTIONS=	TRUE
-BOOTSTRAP_SERVICE=	FALSE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  		svpre.mk
 .INCLUDE :  		settings.mk
-.INCLUDE :  		sv.mk
 
 # --- Generate -----------------------------------------------------
 
