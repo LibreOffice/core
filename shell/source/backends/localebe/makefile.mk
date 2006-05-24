3e201448e8f88f966fad5adf5fe0d8de1ad2a3b8
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 13:04:09 +0000

    INTEGRATION: CWS ause043 (1.5.22); FILE MERGED 2005/10/31 17:17:15 hjs 1.5.22.1: #i56702# remove overrides for default uno header generation

--- shell/source/backends/localebe/makefile.mk
+++ shell/source/backends/localebe/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:49:24 $
+#   last change: $Author: vg $ $Date: 2006-05-24 14:04:09 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -42,9 +42,6 @@ LIBTARGET=NO
 ENABLE_EXCEPTIONS=TRUE
 
 COMP1TYPELIST=$(TARGET)
-COMPRDB=$(SOLARBINDIR)$/types.rdb
-UNOUCROUT=$(OUT)$/inc$/$(TARGET)
-INCPRE=$(UNOUCROUT)
 
 # --- Settings ---
 
