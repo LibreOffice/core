94ae180d89912f4192d2e09e2c3e6cd8d354d198
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 13:14:15 +0000

    INTEGRATION: CWS ause043 (1.4.52); FILE MERGED 2005/10/31 11:40:32 hjs 1.4.52.1: #i56702# remove unneeded UNOITYPE definition

--- filter/source/xmlfilterdetect/makefile.mk
+++ filter/source/xmlfilterdetect/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 22:00:02 $
+#   last change: $Author: vg $ $Date: 2006-05-24 14:14:15 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,6 @@ TARGET=				fl_xmlfilterdetect
 
 NO_BSYMBOLIC=		TRUE
 ENABLE_EXCEPTIONS=	TRUE
-BOOTSTRAP_SERVICE=	FALSE
 
 # --- Settings -----------------------------------------------------
 
