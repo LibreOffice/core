f3c4d8c47d838d33b15a9d428f376d84b328656c
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:54:14 +0000

    INTEGRATION: CWS ause043 (1.11.16); FILE MERGED 2005/10/31 12:35:11 hjs 1.11.16.1: #i56702# remove overrides for default uno header generation

--- fpicker/source/win32/filepicker/makefile.mk
+++ fpicker/source/win32/filepicker/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 23:46:59 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:54:14 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,6 @@ TARGET=fps
 #LIBTARGET=NO
 ENABLE_EXCEPTIONS=TRUE
 COMP1TYPELIST=$(TARGET)
-COMPRDB=$(SOLARBINDIR)$/types.rdb
-UNOUCROUT=$(OUT)$/inc$/fpicker
-INCPRE+=$(OUT)$/inc$/fpicker
 
 # --- Settings -----------------------------------------------------
 
