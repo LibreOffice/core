031a04085c970d1904ed7a73b01c14f6bf00e63e
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:46:42 +0000

    INTEGRATION: CWS ause043 (1.11.10); FILE MERGED 2005/11/09 13:33:16 hjs 1.11.10.1: #i56702# remove unused variable

--- stoc/source/corereflection/makefile.mk
+++ stoc/source/corereflection/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 07:53:53 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:46:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@ PRJ=..$/..
 PRJNAME=	stoc
 TARGET = reflection.uno
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC = TRUE
 COMP1TYPELIST = corefl
 
 # --- Settings -----------------------------------------------------
