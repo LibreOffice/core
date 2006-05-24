806b1e1ccfea4d063840ac850f44d26cfa91c431
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:08:20 +0000

    INTEGRATION: CWS vq32 (1.6.96); FILE MERGED 2006/05/20 21:54:01 vq 1.6.96.1: #i65415# Use SDI1xxxx instead of SDIxxxx.

--- starmath/sdi/makefile.mk
+++ starmath/sdi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 15:02:44 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:08:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@ PRJ=..
 
 PRJNAME=starmath
 TARGET=smslots
-SDIEXPORT=smath
+SDI1EXPORT=smath
 SVSDIINC=$(PRJ)$/inc
 
 # --- Settings -----------------------------------------------------
@@ -47,9 +47,9 @@ SVSDIINC=$(PRJ)$/inc
 # --- Files --------------------------------------------------------
 
 
-SDINAME=$(TARGET)
+SDI1NAME=$(TARGET)
 
-SVSDIDEPEND= \
+SVSDI1DEPEND= \
     $(SOLARINCDIR)$/svxslots.ilb \
     smath.sdi \
     smslots.sdi \
