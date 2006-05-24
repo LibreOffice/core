d93c33b16e6b3f76281fcd586b7402265eb66f72
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:51:32 +0000

    INTEGRATION: CWS ause043 (1.9.10); FILE MERGED 2005/11/09 13:33:20 hjs 1.9.10.1: #i56702# remove unused variable

--- stoc/source/servicemanager/makefile.mk
+++ stoc/source/servicemanager/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 08:11:13 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:51:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@ PRJ=..$/..
 PRJNAME=	stoc
 TARGET = servicemgr.uno
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC = TRUE
 BOOTSTRAP_SERVICE=TRUE
 COMP1TYPELIST = smgr
 
