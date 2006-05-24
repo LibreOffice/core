1e0b313dca76187d6af6663ea6ab1553d107d9a4
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 13:34:25 +0000

    INTEGRATION: CWS ause043 (1.12.6); FILE MERGED 2005/10/31 17:18:23 hjs 1.12.6.1: #i56702# remove overrides for default uno header generation

--- dtrans/source/win32/dtobj/makefile.mk
+++ dtrans/source/win32/dtobj/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 18:26:58 $
+#   last change: $Author: vg $ $Date: 2006-05-24 14:34:25 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,12 +46,8 @@ LIBTARGET=NO
 
 # ------------------------------------------------------------------
 
-.INCLUDE : ..$/..$/cppumaker.mk
 #-DUNICODE -D_UNICODE
 CFLAGS+=-GR -Ob0
-UNOTYPES= \
-    com.sun.star.datatransfer.XSystemTransferable
-    
 
 SLOFILES=$(SLO)$/DtObjFactory.obj\
          $(SLO)$/APNDataObject.obj\
