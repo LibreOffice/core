034f06990053d54568026d0953b13d97d2852d94
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:20:30 +0000

    INTEGRATION: CWS vq32 (1.6.428); FILE MERGED 2006/05/20 21:55:29 vq 1.6.428.1: #i65415# Use SDI1xxxx instead of SDIxxxx.

--- svx/sdi/makefile.mk
+++ svx/sdi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 20:11:16 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:20:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,12 +43,12 @@ TARGET=svxslots
 
 .INCLUDE :  settings.mk
 
-SDINAME=$(TARGET)
-SDIEXPORT=svx
+SDI1NAME=$(TARGET)
+SDI1EXPORT=svx
 
 # --- Files --------------------------------------------------------
 
-SVSDIDEPEND= \
+SVSDI1DEPEND= \
         svxitems.sdi \
     fmslots.sdi \
     svx.sdi	\
