95c0e643e1257cec6006196a1db3080864f7d385
Vladimir Glazounov <vg@openoffice.org>
2006-05-24 12:42:19 +0000

    INTEGRATION: CWS ause043 (1.4.38); FILE MERGED 2005/11/08 13:54:19 hjs 1.4.38.1: #i56702# remove local uno header generation

--- framework/util/guiapps/makefile.mk
+++ framework/util/guiapps/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 02:09:28 $
+#   last change: $Author: vg $ $Date: 2006-05-24 13:42:19 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,12 +40,7 @@ TARGETTYPE=GUI
 
 USE_DEFFILE=		TRUE
 ENABLE_EXCEPTIONS=	TRUE
-NO_BSYMBOLIC=		TRUE
 GEN_HID=                TRUE
-
-#COMP3TYPELIST=		fwl
-#COMP4TYPELIST=		fwk
-#COMP5TYPELIST=		lgd
 
 # --- Settings -----------------------------------------------------
 
