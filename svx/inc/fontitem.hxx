55b573add1279b40c6fa37cd95027cdf51dfb344
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:50:21 +0000

    INTEGRATION: CWS vgcleanupmacros (1.8.410); FILE MERGED 2006/05/17 11:20:37 vg 1.8.410.1: #i65486# cleanup:remove obsolete macros

--- svx/inc/fontitem.hxx
+++ svx/inc/fontitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fontitem.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:42:54 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:50:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,14 +52,10 @@
 #endif
 
 class SvXMLUnitConverter;
-#ifdef _USE_NAMESPACE
 namespace rtl
 {
-#endif
     class OUString;
-#ifdef _USE_NAMESPACE
 }
-#endif
 
 // class SvxFontItem -----------------------------------------------------
 
