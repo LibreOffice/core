cf28db13ced40e92cd4c9f51ebdd3794a8f53440
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:45:24 +0000

    INTEGRATION: CWS vgcleanupmacros (1.5.20); FILE MERGED 2006/05/17 11:23:21 vg 1.5.20.1: #i65486# cleanup:remove obsolete macros

--- vos/source/timer.cxx
+++ vos/source/timer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: timer.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:11:02 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:45:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -103,12 +103,7 @@ protected:
 
 };
 
-
-
-#ifdef _USE_NAMESPACE
 using namespace vos;
-#endif
-
 
 /////////////////////////////////////////////////////////////////////////////
 //
