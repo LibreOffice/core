0919a61de755b308795ab8896c6e9adce65273e0
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:37:23 +0000

    INTEGRATION: CWS vgcleanupmacros (1.3.20); FILE MERGED 2006/05/17 11:23:17 vg 1.3.20.1: #i65486# cleanup:remove obsolete macros

--- vos/inc/vos/mutex.hxx
+++ vos/inc/vos/mutex.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mutex.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:00:15 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:37:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,10 +46,8 @@
 #   include <osl/mutex.h>
 #endif
 
-#ifdef _USE_NAMESPACE
 namespace vos
 {
-#endif
 
 
 /** IMutex interface
@@ -191,10 +189,7 @@ protected:
     IMutex* m_pMutex;
 };
 
-
-#ifdef _USE_NAMESPACE
 }
-#endif
 
 
 #endif  //_VOS_MUTEX_HXX_
