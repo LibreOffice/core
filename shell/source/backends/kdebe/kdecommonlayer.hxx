93b7b4588a1b4aeae7bc6418f8d5633b2f6566ff
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:26:08 +0000

    INTEGRATION: CWS kdesettings (1.1.2); FILE ADDED 2006/04/11 12:40:15 ebischoff 1.1.2.1: Issue #63557 Have OOo use KDE system-wide settings

---
+++ shell/source/backends/kdebe/kdecommonlayer.hxx
@@ -0,0 +1,72 @@
+#ifndef KDECOMMONLAYER_HXX_
+#define KDECOMMONLAYER_HXX_
+
+#ifndef KDEBACKEND_HXX_
+#include "kdebackend.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XLAYER_HPP_
+#include <com/sun/star/configuration/backend/XLayer.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_BACKENDACCESSEXCEPTION_HPP_
+#include <com/sun/star/configuration/backend/BackendAccessException.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XLAYERCONTENTDESCIBER_HPP_
+#include <com/sun/star/configuration/backend/XLayerContentDescriber.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UTIL_XTIMESTAMPED_HPP_
+#include <com/sun/star/util/XTimeStamped.hpp>
+#endif
+
+#ifndef _CPPUHELPER_IMPLBASE2_HXX_
+#include <cppuhelper/implbase2.hxx>
+#endif
+
+namespace css = com::sun::star ;
+namespace uno = css::uno ;
+namespace lang = css::lang ;
+namespace backend = css::configuration::backend ;
+namespace util = css::util ;
+
+/**
+  Implementation of the XLayer interface for the KDE values mapped into
+  the org.openoffice.Office.Common configuration component.
+  */
+class KDECommonLayer : public cppu::WeakImplHelper2<backend::XLayer, util::XTimeStamped>
+{
+public :
+    /**
+      Constructor given the component context
+
+      @param xContext       The component context
+    */
+
+    KDECommonLayer(const uno::Reference<uno::XComponentContext>& xContext);
+
+    // XLayer
+    virtual void SAL_CALL readData(
+        const uno::Reference<backend::XLayerHandler>& xHandler)
+        throw ( backend::MalformedDataException,
+            lang::NullPointerException,
+            lang::WrappedTargetException,
+            uno::RuntimeException) ;
+
+    // XTimeStamped
+    virtual rtl::OUString SAL_CALL getTimestamp(void)
+        throw (uno::RuntimeException);
+
+    protected:
+
+    /** Destructor */
+    ~KDECommonLayer(void) {}
+
+    private :
+        uno::Reference<backend::XLayerContentDescriber> m_xLayerContentDescriber ;
+  } ;
+
+#endif // KDECOMMONLAYER
