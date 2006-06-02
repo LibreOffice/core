eaf105f947694c29ef9edd9a5f63c5cccdc247cc
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:42:49 +0000

    INTEGRATION: CWS vgcleanupmacros (1.4.20); FILE MERGED 2006/05/17 11:23:20 vg 1.4.20.1: #i65486# cleanup:remove obsolete macros

--- vos/source/pipe.cxx
+++ vos/source/pipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pipe.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:08:26 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:42:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,10 +37,7 @@
 #include <vos/pipe.hxx>
 #include <vos/diagnose.hxx>
 
-#ifdef _USE_NAMESPACE
 using namespace vos;
-#endif
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Pipe
