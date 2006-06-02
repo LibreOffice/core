022ccda76934149ea35a1a34afe9c252b1f6bf03
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:42:35 +0000

    INTEGRATION: CWS vgcleanupmacros (1.5.10); FILE MERGED 2006/05/17 11:23:20 vg 1.5.10.1: #i65486# cleanup:remove obsolete macros

--- vos/source/object.cxx
+++ vos/source/object.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: object.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:33:52 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:42:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,10 +47,7 @@
 
 #include <vos/object.hxx>
 
-
-#ifdef _USE_NAMESPACE
 using namespace vos;
-#endif
 
 /////////////////////////////////////////////////////////////////////////////
 // Object super class
