172d85ae0dda208b1b65ef66a924170e11df15e5
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:24:22 +0000

    INTEGRATION: CWS kdesettings (1.1.2); FILE ADDED 2006/04/11 12:40:13 ebischoff 1.1.2.1: Issue #63557 Have OOo use KDE system-wide settings

---
+++ shell/source/backends/kdebe/kdebackend.cxx
@@ -0,0 +1,179 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: kdebackend.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:24:08 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "kdebackend.hxx"
+
+#ifndef KDECOMMONLAYER_HXX_
+#include "kdecommonlayer.hxx"
+#endif
+#ifndef KDEINETLAYER_HXX_
+#include "kdeinetlayer.hxx"
+#endif
+#ifndef KDEVCLLAYER_HXX_
+#include "kdevcllayer.hxx"
+#endif
+
+//------------------------------------------------------------------------------
+
+KDEBackend* KDEBackend::mInstance= 0;
+
+KDEBackend* KDEBackend::createInstance(const uno::Reference<uno::XComponentContext>& xContext)
+{
+    if (mInstance == 0)
+    {
+        mInstance = new KDEBackend (xContext);
+    }
+
+    return mInstance;
+}
+
+//------------------------------------------------------------------------------
+
+KDEBackend::KDEBackend(const uno::Reference<uno::XComponentContext>& xContext)
+        throw (backend::BackendAccessException)
+    : BackendBase(mMutex), m_xContext(xContext)
+{
+}
+
+//------------------------------------------------------------------------------
+
+KDEBackend::~KDEBackend(void)
+{
+}
+
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XLayer> SAL_CALL KDEBackend::getLayer(
+    const rtl::OUString& aComponent, const rtl::OUString& aTimestamp)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    uno::Reference<backend::XLayer> xLayer;
+
+    if( aComponent.equalsAscii("org.openoffice.Office.Common" ) )
+    {
+        xLayer = new KDECommonLayer(m_xContext);
+    }
+    else if( aComponent.equalsAscii("org.openoffice.Inet" ) )
+    {
+        xLayer = new KDEInetLayer(m_xContext);
+    }
+    else if( aComponent.equalsAscii("org.openoffice.VCL" ) )
+    {
+        xLayer = new KDEVCLLayer(m_xContext);
+    }
+
+    return xLayer;
+}
+
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XUpdatableLayer> SAL_CALL
+KDEBackend::getUpdatableLayer(const rtl::OUString& aComponent)
+    throw (backend::BackendAccessException,lang::NoSupportException,
+           lang::IllegalArgumentException)
+{
+    throw lang::NoSupportException( rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("KDEBackend: No Update Operation allowed, Read Only access") ),
+        *this) ;
+}
+
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL KDEBackend::getBackendName(void)
+{
+    return rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("com.sun.star.comp.configuration.backend.KDEBackend") );
+}
+
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL KDEBackend::getImplementationName(void)
+    throw (uno::RuntimeException)
+{
+    return getBackendName() ;
+}
+
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL KDEBackend::getBackendServiceNames(void)
+{
+    uno::Sequence<rtl::OUString> aServices(2) ;
+    aServices[0] = rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("com.sun.star.configuration.backend.KDEBackend")) ;
+    aServices[1] = rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("com.sun.star.configuration.backend.PlatformBackend")) ;
+
+    return aServices ;
+}
+
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL KDEBackend::getSupportedComponents(void)
+{
+    uno::Sequence<rtl::OUString> aSupportedComponentsList(3) ;
+    aSupportedComponentsList[0] = rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("org.openoffice.VCL")) ;
+    aSupportedComponentsList[1] = rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Inet")) ;
+    aSupportedComponentsList[2] = rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("org.openoffice.Office.Common")) ;
+
+    return aSupportedComponentsList ;
+}
+
+//------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL KDEBackend::supportsService(const rtl::OUString& aServiceName)
+    throw (uno::RuntimeException)
+{
+    uno::Sequence< rtl::OUString > const svc = getBackendServiceNames();
+
+    for(sal_Int32 i = 0; i < svc.getLength(); ++i )
+        if(svc[i] == aServiceName)
+            return true;
+
+    return false;
+}
+
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString>
+SAL_CALL KDEBackend::getSupportedServiceNames(void)
+    throw (uno::RuntimeException)
+{
+    return getBackendServiceNames() ;
+}
+
+// ---------------------------------------------------------------------------------------
---
+++ shell/source/backends/kdebe/kdebackend.hxx
@@ -0,0 +1,155 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: kdebackend.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:24:22 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef KDEBACKEND_HXX_
+#define KDEBACKEND_HXX_
+
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSCHEMASUPPLIER_HPP_
+#include <com/sun/star/configuration/backend/XSingleLayerStratum.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
+#include <com/sun/star/lang/XInitialization.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CONFIGURATION_INVALIDBOOTSTRAPFILEEXCEPTION_HPP_
+#include <com/sun/star/configuration/InvalidBootstrapFileException.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_CANNOTCONNECTEXCEPTION_HPP_
+#include <com/sun/star/configuration/backend/CannotConnectException.hpp>
+#endif
+
+#ifndef _CPPUHELPER_COMPBASE2_HXX_
+#include <cppuhelper/compbase2.hxx>
+#endif
+
+#ifndef INCLUDED_MAP
+#include <map>
+#define INCLUDED_MAP
+#endif
+
+namespace css = com::sun::star ;
+namespace uno = css::uno ;
+namespace lang = css::lang ;
+namespace backend = css::configuration::backend ;
+
+
+//------------------------------------------------------------------------------
+typedef cppu::WeakComponentImplHelper2<backend::XSingleLayerStratum,
+                       lang::XServiceInfo> BackendBase ;
+
+/**
+  Implements the SingleLayerStratum service for KDE access.
+  */
+class KDEBackend : public BackendBase {
+    public :
+
+    static KDEBackend* createInstance(const uno::Reference<uno::XComponentContext>& xContext);
+
+        // XServiceInfo
+        virtual rtl::OUString SAL_CALL getImplementationName(  )
+            throw (uno::RuntimeException) ;
+
+        virtual sal_Bool SAL_CALL supportsService( const rtl::OUString& aServiceName )
+            throw (uno::RuntimeException) ;
+
+        virtual uno::Sequence<rtl::OUString> SAL_CALL getSupportedServiceNames(  )
+            throw (uno::RuntimeException) ;
+
+        /**
+          Provides the implementation name.
+
+          @return   implementation name
+          */
+        static rtl::OUString SAL_CALL getBackendName(void) ;
+
+        /**
+          Provides the supported services names
+
+          @return   service names
+          */
+        static uno::Sequence<rtl::OUString> SAL_CALL getBackendServiceNames(void) ;
+
+        /**
+          Provides the supported component nodes
+
+          @return supported component nodes
+        */
+        static uno::Sequence<rtl::OUString> SAL_CALL getSupportedComponents(void) ;
+
+        //XSingleLayerStratum
+        virtual uno::Reference<backend::XLayer> SAL_CALL
+            getLayer( const rtl::OUString& aLayerId, const rtl::OUString& aTimestamp )
+                throw (backend::BackendAccessException, lang::IllegalArgumentException) ;
+
+        virtual uno::Reference<backend::XUpdatableLayer> SAL_CALL
+            getUpdatableLayer( const rtl::OUString& aLayerId )
+                throw (backend::BackendAccessException, lang::NoSupportException,
+                       lang::IllegalArgumentException) ;
+
+    protected:
+        /**
+          Service constructor from a service factory.
+
+          @param xContext   component context
+          */
+         KDEBackend(const uno::Reference<uno::XComponentContext>& xContext)
+            throw (backend::BackendAccessException);
+
+        /** Destructor */
+        ~KDEBackend(void) ;
+
+    private:
+
+        /** Build KDE/OO mapping table */
+        void initializeMappingTable ();
+
+
+        /** The component context */
+        uno::Reference<uno::XComponentContext> m_xContext;
+
+        /** Mutex for reOOurces protection */
+        osl::Mutex mMutex ;
+
+        static KDEBackend* mInstance;
+};
+
+#endif // CONFIGMGR_LOCALBE_LOCALSINGLESTRATUM_HXX_
