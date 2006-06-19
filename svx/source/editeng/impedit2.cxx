af66b080c46f26e48d3200f267850e7a521efcef
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:42:04 +0000

    INTEGRATION: CWS warnings01 (1.108.50); FILE MERGED 2006/05/23 18:30:56 sb 1.108.50.2: RESYNC: (1.108-1.111); FILE MERGED 2006/04/20 14:49:50 cl 1.108.50.1: warning free code changes

--- svx/source/editeng/impedit2.cxx
+++ svx/source/editeng/impedit2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impedit2.cxx,v $
  *
- *  $Revision: 1.111 $
+ *  $Revision: 1.112 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-05 08:18:48 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:42:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,8 +34,6 @@
  ************************************************************************/
 
 #include <eeng_pch.hxx>
-
-#pragma hdrstop
 
 #include <lspcitem.hxx>
 #include <flditem.hxx>
