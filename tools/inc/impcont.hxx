e8e31551cf1f33bafba6f62668f708576efb9b38
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:25:53 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/10/27 12:28:51 sb 1.2.8.1: #i53898# Made code warning-free.

--- tools/inc/impcont.hxx
+++ tools/inc/impcont.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impcont.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:42:13 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 13:25:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,10 @@ private:
     USHORT          nCount;             // Anzahl Pointer
     void**          pNodes;             // Pointer auf die Daten
 
+#if defined DBG_UTIL
+    static char const * DbgCheckCBlock(void const *);
+#endif
+
 public:
                     // Fuer List-Container
                     CBlock( USHORT nSize, CBlock* pPrev, CBlock* pNext );
@@ -89,6 +93,8 @@ public:
 
 private:
                     CBlock( const CBlock& r );
+
+    friend class Container;
 };
 
 /*************************************************************************
