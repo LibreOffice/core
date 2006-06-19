04c4ab845f9f34b4af11f881b661c092b450d63c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:10:16 +0000

    INTEGRATION: CWS warnings01 (1.5.150); FILE MERGED 2006/05/12 16:34:21 sb 1.5.150.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- filter/source/config/tools/merge/makefile.mk
+++ filter/source/config/tools/merge/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 21:35:57 $
+#   last change: $Author: hr $ $Date: 2006-06-19 17:10:16 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ     = ..$/..$/..$/..
 TARGET  = FCFGMerge
-PRJNAME = $(TARGET)
+PRJNAME = filter
 PACKAGE = com$/sun$/star$/filter$/config$/tools$/merge
 
 # --- Settings -----------------------------------------------------
