30adc50449294617f76e4a7ce7fe9932dc0499f3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:10:26 +0000

    INTEGRATION: CWS warnings01 (1.3.4); FILE MERGED 2005/10/25 09:12:59 pl 1.3.4.1: #i55991# removed warnings for solaris platform

--- UnoControls/source/inc/progressmonitor.hxx
+++ UnoControls/source/inc/progressmonitor.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: progressmonitor.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:20:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:10:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -780,7 +780,7 @@ protected:
 //____________________________________________________________________________________________________________
 
 private:
-
+    using BaseControl::impl_recalcLayout;
     /**_______________________________________________________________________________________________________
         @short      -
         @descr      -
