da364b4dbda94bc097ad00567760ecac767bd5f5
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:56:34 +0000

    INTEGRATION: CWS warnings01 (1.3.32); FILE MERGED 2005/10/28 14:48:12 cd 1.3.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/jobs/helponstartup.hxx
+++ framework/inc/jobs/helponstartup.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helponstartup.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:22:07 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:56:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -172,8 +172,8 @@ class HelpOnStartup : public css::lang::XTypeProvider
         // css.uno.XInterface
         // css.lang.XTypeProvider
         // css.lang.XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // css.task.XJob
--- framework/inc/jobs/jobdispatch.hxx
+++ framework/inc/jobs/jobdispatch.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: jobdispatch.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:22:51 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:56:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -207,8 +207,8 @@ class JobDispatch : public  css::lang::XTypeProvider
     public:
 
         // XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // Xinitialization
