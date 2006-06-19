e0903f8919a9fbb9b7ca971c3eeab62ae8777cf1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:02:45 +0000

    INTEGRATION: CWS warnings01 (1.2.60); FILE MERGED 2005/11/15 17:54:32 pl 1.2.60.1: #i55991# removed warnings

--- svtools/source/edit/textundo.hxx
+++ svtools/source/edit/textundo.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textundo.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:29:43 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:02:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,9 @@ public:
                     TextUndoManager( TextEngine* pTextEngine );
                     ~TextUndoManager();
 
+    using SfxUndoManager::Undo;
     virtual BOOL    Undo( USHORT nCount=1 );
+    using SfxUndoManager::Redo;
     virtual BOOL    Redo( USHORT nCount=1 );
 
 };
