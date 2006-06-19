2f5fdf985790cd4fb00f6d73cc0bec23c16c671b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 21:56:24 +0000

    INTEGRATION: CWS warnings01 (1.3.26); FILE MERGED 2005/11/14 10:36:05 pl 1.3.26.1: #i55991# removed warnings

--- toolkit/inc/toolkit/controls/roadmapcontrol.hxx
+++ toolkit/inc/toolkit/controls/roadmapcontrol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: roadmapcontrol.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:49:38 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 22:56:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -167,7 +167,10 @@ namespace toolkit{
 
     public:
         UnoControlRoadmapModel();
-        UnoControlRoadmapModel( const UnoControlRoadmapModel& rModel ) : UnoControlModel( rModel ), maContainerListeners( *this ) {;}
+        UnoControlRoadmapModel( const UnoControlRoadmapModel& rModel ) :
+            UnoControlRoadmapModel_Base( rModel ),
+            UnoControlRoadmapModel_IBase( rModel ),
+            maContainerListeners( *this ) {}
         UnoControlModel*    Clone() const { return new UnoControlRoadmapModel( *this ); }
 
 
