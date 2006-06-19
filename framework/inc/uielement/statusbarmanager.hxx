e1b7cae9b83664fc30f6b6f634cd68e9fad7de5c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:06:32 +0000

    INTEGRATION: CWS warnings01 (1.5.32); FILE MERGED 2005/10/28 14:48:22 cd 1.5.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/uielement/statusbarmanager.hxx
+++ framework/inc/uielement/statusbarmanager.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: statusbarmanager.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:46:59 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:05:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,8 +118,8 @@ class StatusBarManager : public ::com::sun::star::frame::XFrameActionListener   
         virtual ~StatusBarManager();
 
         //  XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         StatusBar* GetStatusBar() const;
 
--- framework/inc/uielement/uicommanddescription.hxx
+++ framework/inc/uielement/uicommanddescription.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uicommanddescription.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:48:50 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:06:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,8 +118,8 @@ class UICommandDescription :  public com::sun::star::lang::XTypeProvider    ,
         virtual ~UICommandDescription();
 
         //  XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // XNameAccess
