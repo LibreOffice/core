1843661e6194134af1221f08b98af3f005aabba8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:43:51 +0000

    INTEGRATION: CWS warnings01 (1.6.6); FILE MERGED 2006/03/10 19:08:12 lo 1.6.6.1: warnings01: remove warnings in unoxml

--- unoxml/source/dom/characterdata.hxx
+++ unoxml/source/dom/characterdata.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: characterdata.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:56:22 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:43:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
