d0e7ada82b9176631ec60a242726fe0c59fcf001
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:11:04 +0000

    INTEGRATION: CWS warnings01 (1.10.150); FILE MERGED 2006/05/12 16:34:22 sb 1.10.150.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- filter/source/svg/makefile.mk
+++ filter/source/svg/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 21:54:37 $
+#   last change: $Author: hr $ $Date: 2006-06-19 17:11:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #*************************************************************************
 
 PRJ=..$/..
-PRJNAME=svgfilter
+PRJNAME=filter
 TARGET=svgfilter
 
 ENABLE_EXCEPTIONS=TRUE
