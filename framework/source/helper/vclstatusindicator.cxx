9ea4fb33c2f8b42c893dae98415cb3d2e30b6995
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:20:36 +0000

    INTEGRATION: CWS warnings01 (1.3.32); FILE MERGED 2005/10/28 14:48:34 cd 1.3.32.1: #i55991# Warning free code changes for gcc

--- framework/source/helper/vclstatusindicator.cxx
+++ framework/source/helper/vclstatusindicator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vclstatusindicator.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:27:53 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:20:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
