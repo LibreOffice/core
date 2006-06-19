9ce9b5140ed585821c86adcb1bdb6913cf7420bd
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:01:21 +0000

    INTEGRATION: CWS warnings01 (1.10.112); FILE MERGED 2006/04/07 18:02:46 sb 1.10.112.2: RESYNC: (1.10-1.11); FILE MERGED 2006/04/06 14:39:45 ab 1.10.112.1: #i55991# warning-free code

--- svx/source/dialog/cfg.hxx
+++ svx/source/dialog/cfg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfg.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-07 10:16:40 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:01:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -274,7 +274,7 @@ public:
     // methods inherited from SaveInData
     SvxEntries*         GetEntries();
     void                SetEntries( SvxEntries* );
-    bool                HasURL( const rtl::OUString& URL ) { return FALSE; }
+    bool                HasURL( const rtl::OUString& URL ) { (void)URL; return FALSE; }
     bool                HasSettings() { return m_xMenuSettings.is(); }
     void                Reset();
     bool                Apply();
@@ -325,13 +325,13 @@ public:
         :
             nId( 0 ),
             bPopUp( FALSE ),
+            bStrEdited( FALSE ),
             bIsUserDefined( FALSE ),
             bIsMain( FALSE ),
-            bStrEdited( FALSE ),
+            bIsParentData( FALSE ),
             bIsVisible( TRUE ),
             nStyle( 0 ),
-            pEntries( 0 ),
-            bIsParentData( FALSE )
+            pEntries( 0 )
     {}
 
     ~SvxConfigEntry();
