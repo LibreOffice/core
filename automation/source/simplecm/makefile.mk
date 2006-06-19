ec307ecb0dc22577435cc32bb381ea7c259c7a2d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:25:14 +0000

    INTEGRATION: CWS warnings01 (1.3.12); FILE MERGED 2006/05/22 15:21:20 sb 1.3.12.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- automation/source/simplecm/makefile.mk
+++ automation/source/simplecm/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:28:35 $
+#   last change: $Author: hr $ $Date: 2006-06-20 00:25:14 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..
 
-PRJNAME=AUTOMATION
+PRJNAME=automation
 TARGET=simplecm
 
 # --- Settings -----------------------------------------------------
