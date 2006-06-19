26d675d2c636e3eaec53700a3a6a7750889739f0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 13:02:19 +0000

    INTEGRATION: CWS warnings01 (1.8.16); FILE MERGED 2005/10/27 10:51:09 pl 1.8.16.1: #i55991# removed warnings for solaris platform

--- unotools/inc/unotools/streamwrap.hxx
+++ unotools/inc/unotools/streamwrap.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: streamwrap.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:38:17 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 14:02:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -208,7 +208,7 @@ public:
     virtual void SAL_CALL truncate() throw(::com::sun::star::io::IOException, ::com::sun::star::uno::RuntimeException);
 };
 
-};
+}
 // namespace utl
 
 #endif // _UTL_STREAM_WRAPPER_HXX_
