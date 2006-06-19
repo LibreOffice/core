c091087ced7b762cbec803a4c1b2c11fd52aed90
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:07:57 +0000

    INTEGRATION: CWS warnings01 (1.3.4); FILE MERGED 2005/10/25 09:12:17 pl 1.3.4.1: #i55991# removed warnings for solaris platform

--- UnoControls/inc/basecontainercontrol.hxx
+++ UnoControls/inc/basecontainercontrol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: basecontainercontrol.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:14:01 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:07:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -524,7 +524,7 @@ public:
 //____________________________________________________________________________________________________________
 
 protected:
-
+    using OComponentHelper::disposing;
     /**_______________________________________________________________________________________________________
         @short
         @descr
