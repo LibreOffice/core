d76d634d3476512063492d9477c4daf79e57a4cc
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:01:44 +0000

    INTEGRATION: CWS warnings01 (1.3.34); FILE MERGED 2005/11/16 21:33:59 pl 1.3.34.1: #i55991# removed warnings

--- xmloff/source/chart/SchXMLParagraphContext.cxx
+++ xmloff/source/chart/SchXMLParagraphContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SchXMLParagraphContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:25:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:01:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@ void SchXMLParagraphContext::EndElement()
 SvXMLImportContext* SchXMLParagraphContext::CreateChildContext(
     USHORT nPrefix,
     const OUString& rLocalName,
-    const uno::Reference< xml::sax::XAttributeList >& xAttrList )
+    const uno::Reference< xml::sax::XAttributeList >& )
 {
     if( nPrefix == XML_NAMESPACE_TEXT )
     {
