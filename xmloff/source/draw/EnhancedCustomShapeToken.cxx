98b89fe42ccac83c5a34f7dec1044012623294c7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:07:33 +0000

    INTEGRATION: CWS warnings01 (1.7.34); FILE MERGED 2005/11/16 21:34:07 pl 1.7.34.1: #i55991# removed warnings

--- xmloff/source/draw/EnhancedCustomShapeToken.cxx
+++ xmloff/source/draw/EnhancedCustomShapeToken.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnhancedCustomShapeToken.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:39:36 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:07:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ static ::osl::Mutex& getHashMapMutex()
 
 struct TokenTable
 {
-    char*                               pS;
+    const char*                         pS;
     EnhancedCustomShapeTokenEnum        pE;
 };
 
