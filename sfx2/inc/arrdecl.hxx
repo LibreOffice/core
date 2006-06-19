d63153232bbc0a35a65130685cf36c1297132e7b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:57:15 +0000

    INTEGRATION: CWS warnings01 (1.4.66); FILE MERGED 2005/11/28 16:12:51 cd 1.4.66.1: #i55991# Remove warnings

--- sfx2/inc/arrdecl.hxx
+++ sfx2/inc/arrdecl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: arrdecl.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:44:27 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:57:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,59 +44,59 @@
 
 struct CntUpdateResult;
 
-SV_DECL_PTRARR_DEL(CntUpdateResults_Impl, CntUpdateResult*, 4, 4);
+SV_DECL_PTRARR_DEL(CntUpdateResults_Impl, CntUpdateResult*, 4, 4)
 
 class SfxObjectShell;
-SV_DECL_PTRARR( SfxObjectShellArr_Impl, SfxObjectShell*, 4, 4 );
+SV_DECL_PTRARR( SfxObjectShellArr_Impl, SfxObjectShell*, 4, 4 )
 
 class SfxViewFrame;
-SV_DECL_PTRARR( SfxViewFrameArr_Impl, SfxViewFrame*, 4, 4 );
+SV_DECL_PTRARR( SfxViewFrameArr_Impl, SfxViewFrame*, 4, 4 )
 
 class SfxViewShell;
-SV_DECL_PTRARR( SfxViewShellArr_Impl, SfxViewShell*, 4, 4 );
+SV_DECL_PTRARR( SfxViewShellArr_Impl, SfxViewShell*, 4, 4 )
 
 class SfxObjectFactory;
 typedef SfxObjectFactory* SfxObjectFactoryPtr;
-SV_DECL_PTRARR( SfxObjectFactoryArr_Impl, SfxObjectFactoryPtr, 3, 3 );
+SV_DECL_PTRARR( SfxObjectFactoryArr_Impl, SfxObjectFactoryPtr, 3, 3 )
 
 struct SfxTbxCtrlFactory;
-SV_DECL_PTRARR_DEL( SfxTbxCtrlFactArr_Impl, SfxTbxCtrlFactory*, 8, 4 );
+SV_DECL_PTRARR_DEL( SfxTbxCtrlFactArr_Impl, SfxTbxCtrlFactory*, 8, 4 )
 
 struct SfxStbCtrlFactory;
-SV_DECL_PTRARR_DEL( SfxStbCtrlFactArr_Impl, SfxStbCtrlFactory*, 8, 4 );
+SV_DECL_PTRARR_DEL( SfxStbCtrlFactArr_Impl, SfxStbCtrlFactory*, 8, 4 )
 
 struct SfxMenuCtrlFactory;
-SV_DECL_PTRARR_DEL( SfxMenuCtrlFactArr_Impl, SfxMenuCtrlFactory*, 2, 2 );
+SV_DECL_PTRARR_DEL( SfxMenuCtrlFactArr_Impl, SfxMenuCtrlFactory*, 2, 2 )
 
 struct SfxChildWinFactory;
-SV_DECL_PTRARR_DEL( SfxChildWinFactArr_Impl, SfxChildWinFactory*, 2, 2 );
+SV_DECL_PTRARR_DEL( SfxChildWinFactArr_Impl, SfxChildWinFactory*, 2, 2 )
 
 class SfxModule;
-SV_DECL_PTRARR( SfxModuleArr_Impl, SfxModule*, 2, 2 );
+SV_DECL_PTRARR( SfxModuleArr_Impl, SfxModule*, 2, 2 )
 
 class SfxFilter;
-DECL_PTRARRAY( SfxFilterArr_Impl, SfxFilter*, 4, 4 );
+DECL_PTRARRAY( SfxFilterArr_Impl, SfxFilter*, 4, 4 )
 
 class SfxFrame;
 typedef SfxFrame* SfxFramePtr;
-SV_DECL_PTRARR( SfxFrameArr_Impl, SfxFramePtr, 4, 4 );
+SV_DECL_PTRARR( SfxFrameArr_Impl, SfxFramePtr, 4, 4 )
 
-DECLARE_LIST( SfxFilterList_Impl, SfxFilter* );
+DECLARE_LIST( SfxFilterList_Impl, SfxFilter* )
 
 struct SfxExternalLib_Impl;
 typedef SfxExternalLib_Impl* SfxExternalLibPtr;
-SV_DECL_PTRARR_DEL( SfxExternalLibArr_Impl, SfxExternalLibPtr, 2, 2 );
+SV_DECL_PTRARR_DEL( SfxExternalLibArr_Impl, SfxExternalLibPtr, 2, 2 )
 
 //class XEventListenerRef;
 //typedef XEventListenerRef* XEventListenerPtr;
-//SV_DECL_PTRARR_DEL( XEventListenerArr_Impl, XEventListenerPtr, 4, 4 );
+//SV_DECL_PTRARR_DEL( XEventListenerArr_Impl, XEventListenerPtr, 4, 4 )
 
 //class XFrameRef;
 //typedef XFrameRef* XFramePtr;
-//SV_DECL_PTRARR_DEL( XFrameArr_Impl, XFramePtr, 4, 4 );
+//SV_DECL_PTRARR_DEL( XFrameArr_Impl, XFramePtr, 4, 4 )
 
 class SfxSlot;
 typedef SfxSlot* SfxSlotPtr;
-SV_DECL_PTRARR( SfxSlotArr_Impl, SfxSlotPtr, 20, 20 );
+SV_DECL_PTRARR( SfxSlotArr_Impl, SfxSlotPtr, 20, 20 )
 
 #endif
