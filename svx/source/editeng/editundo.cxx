5ae9d4e3aec991463c06593616062a67594a28cb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:39:50 +0000

    INTEGRATION: CWS warnings01 (1.5.220); FILE MERGED 2006/04/20 14:49:49 cl 1.5.220.1: warning free code changes

--- svx/source/editeng/editundo.cxx
+++ svx/source/editeng/editundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editundo.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:29:18 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:39:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,8 +34,6 @@
  ************************************************************************/
 
 #include <eeng_pch.hxx>
-
-#pragma hdrstop
 
 #include <impedit.hxx>
 #include <editundo.hxx>
