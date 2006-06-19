7d140c6ec4bf008a41cba310680441b14fe59a73
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:23:22 +0000

    INTEGRATION: CWS warnings01 (1.26.4); FILE MERGED 2005/11/01 12:47:19 cd 1.26.4.1: #i53898# Warning free code for sun solaris compiler

--- configmgr/source/inc/bootstrap.hxx
+++ configmgr/source/inc/bootstrap.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bootstrap.hxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:43:04 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:23:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -99,6 +99,9 @@ namespace configmgr
 
         // two-phase construct
         void initialize();
+
+    protected:
+        using ComponentContext::initialize;
 
     public:
          // XServiceInfo
