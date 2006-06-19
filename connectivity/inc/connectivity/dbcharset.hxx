11f1e252a08c6166f2120717c28a71d0ad3aa609
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:59:46 +0000

    INTEGRATION: CWS warnings01 (1.5.30); FILE MERGED 2005/11/07 14:42:53 fs 1.5.30.1: #i57457# warning-free code

--- connectivity/inc/connectivity/dbcharset.hxx
+++ connectivity/inc/connectivity/dbcharset.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbcharset.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:00:40 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:59:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -85,7 +85,7 @@ namespace dbtools
         typedef CharsetIterator const_iterator;
 
         OCharsetMap();
-        ~OCharsetMap();
+        virtual ~OCharsetMap();
 
         struct IANA { };
 
