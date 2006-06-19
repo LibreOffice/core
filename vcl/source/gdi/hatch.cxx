04737cb8f3daaefa1b2c4aa52a4655ed13459239
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 18:23:59 +0000

    INTEGRATION: CWS warnings01 (1.3.70); FILE MERGED 2005/11/04 17:25:00 pl 1.3.70.1: #i55991# removed warnings for linux/solaris

--- vcl/source/gdi/hatch.cxx
+++ vcl/source/gdi/hatch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hatch.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:59:33 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 19:23:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 #include <hatch.hxx>
 #endif
 
-DBG_NAME( Hatch );
+DBG_NAME( Hatch )
 
 // --------------
 // - ImplHatch -
