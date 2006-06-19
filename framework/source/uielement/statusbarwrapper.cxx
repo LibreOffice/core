c084a49825d4b41cc2eb91161af6e5d981ec793e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:41:31 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:46 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/source/uielement/statusbarwrapper.cxx
+++ framework/source/uielement/statusbarwrapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: statusbarwrapper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:58:12 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:41:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,9 +118,8 @@ namespace framework
 StatusBarWrapper::StatusBarWrapper(
     const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& xServiceManager
     )
-:
-    m_xServiceFactory( xServiceManager ),
-    UIConfigElementWrapperBase( UIElementType::STATUSBAR )
+ :  UIConfigElementWrapperBase( UIElementType::STATUSBAR ),
+    m_xServiceFactory( xServiceManager )
 {
 }
 
