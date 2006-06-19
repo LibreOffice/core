c7246526b227a8e1bafc7974d269a60181397613
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:50:07 +0000

    INTEGRATION: CWS warnings01 (1.2.4); FILE MERGED 2005/10/18 08:35:43 np 1.2.4.1: #i53898#

--- autodoc/source/ary/cpp/tplparam.cxx
+++ autodoc/source/ary/cpp/tplparam.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tplparam.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:38:58 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:50:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,7 +98,7 @@ TplParameter_Const::Compare( const TemplateParameter & i_rOther ) const
 
 void
 TplParameter_Const::Get_Text( StreamStr &                   o_rOut,
-                              const ary::cpp::DisplayGate & i_rGate ) const
+                              const ary::cpp::DisplayGate &  ) const
 {
     o_rOut << sConstant;
 }
