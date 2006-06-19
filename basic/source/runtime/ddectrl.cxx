9c76e1419ed31f4ece8e53f1b449cacc4028d2fe
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:44:54 +0000

    INTEGRATION: CWS warnings01 (1.2.30); FILE MERGED 2005/11/07 12:02:48 ab 1.2.30.1: #i53898# Removed warnings

--- basic/source/runtime/ddectrl.cxx
+++ basic/source/runtime/ddectrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ddectrl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:38:05 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:44:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,14 +39,10 @@
 #ifndef _SVDDE_HXX //autogen
 #include <svtools/svdde.hxx>
 #endif
-#pragma hdrstop
 #include "ddectrl.hxx"
 #ifndef _SBERRORS_HXX
 #include <sberrors.hxx>
 #endif
-
-//#include "segmentc.hxx"
-#pragma SW_SEGMENT_CLASS( SBRUNTIME, SBRUNTIME_CODE )
 
 #define DDE_FREECHANNEL ((DdeConnection*)0xffffffff)
 
