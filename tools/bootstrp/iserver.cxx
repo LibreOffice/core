d9a070b54264cb51628f7640bd146f383acf8794
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:20:30 +0000

    INTEGRATION: CWS warnings01 (1.3.8); FILE MERGED 2005/10/14 11:19:22 sb 1.3.8.1: #i53898# Made code warning-free; cleanup.

--- tools/bootstrp/iserver.cxx
+++ tools/bootstrp/iserver.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: iserver.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:29:49 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 13:20:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
  ************************************************************************/
 #include "iparser.hxx"
 #include "geninfo.hxx"
-#include "appdef.hxx"
+#include "bootstrp/appdef.hxx"
 #include <stdio.h>
 
 
