ce8f23e8800682c19182cf0c275f34684f6d27f3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:23:36 +0000

    INTEGRATION: CWS warnings01 (1.2.34); FILE MERGED 2005/11/16 22:47:14 pl 1.2.34.1: #i55991# removed warnings

--- xmloff/source/style/AttributeContainerHandler.cxx
+++ xmloff/source/style/AttributeContainerHandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AttributeContainerHandler.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:21:53 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:23:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,12 +110,12 @@ sal_Bool XMLAttributeContainerHandler::equals(
     return sal_False;
 }
 
-sal_Bool XMLAttributeContainerHandler::importXML( const OUString& rStrImpValue, Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const
+sal_Bool XMLAttributeContainerHandler::importXML( const OUString& /*rStrImpValue*/, Any& /*rValue*/, const SvXMLUnitConverter& /*rUnitConverter*/ ) const
 {
     return sal_True;
 }
 
-sal_Bool XMLAttributeContainerHandler::exportXML( OUString& rStrExpValue, const Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const
+sal_Bool XMLAttributeContainerHandler::exportXML( OUString& /*rStrExpValue*/, const Any& /*rValue*/, const SvXMLUnitConverter& /*rUnitConverter*/ ) const
 {
     return sal_True;
 }
