8f8df504205b1b6cde86d307b67705419df074af
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:18:42 +0000

    INTEGRATION: CWS warnings01 (1.4.34); FILE MERGED 2005/11/03 17:46:59 cl 1.4.34.1: warning free code changes for unxlngi6

--- xmloff/source/forms/formcellbinding.hxx
+++ xmloff/source/forms/formcellbinding.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formcellbinding.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:08:51 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:18:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -300,4 +300,4 @@ namespace xmloff
 }   // namespace xmloff
 //............................................................................
 
-#endif XMLOFF_FORMS_FORMCELLBINDING
+#endif // XMLOFF_FORMS_FORMCELLBINDING
