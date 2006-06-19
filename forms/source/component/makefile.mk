1c95f5cb5920a056dca8a7bff1536e0936839aed
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 11:55:25 +0000

    INTEGRATION: CWS warnings01 (1.11.68); FILE MERGED 2006/05/12 16:35:06 sb 1.11.68.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- forms/source/component/makefile.mk
+++ forms/source/component/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 22:50:52 $
+#   last change: $Author: hr $ $Date: 2006-06-19 12:55:25 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -93,7 +93,8 @@ SLOFILES=	$(SLO)$/Grid.obj \
             $(SLO)$/navigationbar.obj \
             $(SLO)$/formcontrolfont.obj \
             $(SLO)$/refvaluecomponent.obj \
-            $(SLO)$/imgprod.obj
+            $(SLO)$/imgprod.obj \
+            $(SLO)$/findpos.obj
 
 # --- Targets ----------------------------------
 
