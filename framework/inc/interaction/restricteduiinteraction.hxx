68fb3dea8919da4f9976be020128dc908a2bea8f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:55:39 +0000

    INTEGRATION: CWS warnings01 (1.2.32); FILE MERGED 2005/10/28 14:48:11 cd 1.2.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/interaction/restricteduiinteraction.hxx
+++ framework/inc/interaction/restricteduiinteraction.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: restricteduiinteraction.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:21:19 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:55:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -139,8 +139,8 @@ class RestrictedUIInteraction : public  css::lang::XTypeProvider
     public:
 
         // XInterface, XTypeProvider
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         //_________________________________
         /**
