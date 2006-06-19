3b683521c289d421fc1fdc5621a8016b484be38c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 19:41:23 +0000

    INTEGRATION: CWS warnings01 (1.4.62); FILE MERGED 2006/01/25 19:37:51 sb 1.4.62.2: RESYNC: (1.4-1.5); FILE MERGED 2005/10/25 12:43:59 pl 1.4.62.1: #i55991# removed warnings for linux platform

--- svtools/source/config/addxmltostorageoptions.cxx
+++ svtools/source/config/addxmltostorageoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: addxmltostorageoptions.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-11 08:46:14 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 20:41:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -239,7 +239,7 @@ SvtAddXMLToStorageOptions::SvtAddXMLToStorageOptions()
     // ... and initialize ouer data container only if it not already exist!
     if( !m_pDataContainer )
     {
-        RTL_LOGFILE_CONTEXT(aLog, "svtools (???) ::SvtAddXMLToStorageOptions_Impl::ctor()");
+        RTL_LOGFILE_CONTEXT(aLog, "svtools ( ??? ) ::SvtAddXMLToStorageOptions_Impl::ctor()");
         m_pDataContainer = new SvtAddXMLToStorageOptions_Impl;
 
         ItemHolder1::holdConfigItem(E_ADDXMLTOSTORAGEOPTIONS);
