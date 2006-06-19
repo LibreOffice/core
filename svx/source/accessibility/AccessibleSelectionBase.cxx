028d5000cdae519c9c0e67260c1cadae4543b655
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 13:53:49 +0000

    INTEGRATION: CWS warnings01 (1.4.222); FILE MERGED 2006/02/16 12:12:35 fs 1.4.222.1: #i55991# warning-free code (unxsols4)

--- svx/source/accessibility/AccessibleSelectionBase.cxx
+++ svx/source/accessibility/AccessibleSelectionBase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleSelectionBase.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:17:24 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 14:53:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,4 +110,4 @@ namespace accessibility
         ::osl::MutexGuard aGuard( implGetMutex() );
         OCommonAccessibleSelection::deselectAccessibleChild( nSelectedChildIndex );
     }
-};
+}
