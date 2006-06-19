2d3b2f4aa1980c0a6e039888c35abc033753b220
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:20:45 +0000

    INTEGRATION: CWS warnings01 (1.22.34); FILE MERGED 2005/11/03 17:47:00 cl 1.22.34.1: warning free code changes for unxlngi6

--- xmloff/source/forms/propertyexport.hxx
+++ xmloff/source/forms/propertyexport.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertyexport.hxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:15:42 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:20:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
