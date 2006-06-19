831269b2cf4738dd9c419c6bb3f8b13a97669613
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:54:58 +0000

    INTEGRATION: CWS warnings01 (1.5.18); FILE MERGED 2006/02/21 17:42:59 os 1.5.18.1: #i53898# ';' removed

--- lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hprophelp.hxx
+++ lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hprophelp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hprophelp.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:39:05 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:54:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,11 +57,11 @@
 
 namespace com { namespace sun { namespace star { namespace beans {
     class   XPropertySet;
-}}}};
+}}}}
 
 namespace com { namespace sun { namespace star { namespace linguistic2 {
     struct  LinguServiceEvent;
-}}}};
+}}}}
 
 
 using namespace ::rtl;
