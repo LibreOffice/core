e444e583e134a981bf95b72f2fc98273f0dd5fde
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:54:28 +0000

    INTEGRATION: CWS warnings01 (1.2.4); FILE MERGED 2006/02/10 14:25:12 os 1.2.4.2: #i59838# warnings removed 2005/10/04 08:26:30 sb 1.2.4.1: #i53898# Merged in lost 1.1.32.1.

--- hwpfilter/source/htags.cpp
+++ hwpfilter/source/htags.cpp
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htags.cpp,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:38:53 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:54:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,7 @@
  *
  ************************************************************************/
 
-/* NAME $Id: htags.cpp,v 1.2 2005-09-07 16:38:53 rt Exp $
+/* NAME
  * PURPOSE
  *
  * NOTES
@@ -45,7 +45,7 @@
 #include "precompile.h"
 
 #ifdef __GNUG__
-#pragma implementaion "htags.h"
+#pragma implementation "htags.h"
 #endif
 
 #include <string.h>
@@ -153,7 +153,7 @@ bool OlePicture::Read(HWPFile & hwpf)
      char tname[200];
      wchar_t wtname[200];
      tmpnam(tname);
-     if (!(fp = fopen(tname, "wb")))
+     if (0 == (fp = fopen(tname, "wb")))
           return false;
      fwrite(data, size, 1, fp);
      fclose(fp);
