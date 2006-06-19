682289b57f481ed588cc0232851762d53dede5b1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:00:39 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:17 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/services/modulemanager.hxx
+++ framework/inc/services/modulemanager.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: modulemanager.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:32:48 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:00:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -146,8 +146,8 @@ class ModuleManager : public  css::lang::XTypeProvider
         virtual ~ModuleManager(                                                                   );
 
         // XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // XModuleManager
