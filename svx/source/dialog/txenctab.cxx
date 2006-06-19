4e7afce894bd0ecd1f41ea5ab5bbc8767e57814d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:37:17 +0000

    INTEGRATION: CWS warnings01 (1.2.222); FILE MERGED 2006/04/20 14:49:47 cl 1.2.222.1: warning free code changes

--- svx/source/dialog/txenctab.cxx
+++ svx/source/dialog/txenctab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txenctab.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:21:38 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:37:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
-
-// include ---------------------------------------------------------------
 
 #include "dialogs.hrc" // -> RID_SVXSTR_TEXTENCODING_TABLE
 #include "dialmgr.hxx"
