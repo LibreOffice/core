9f05f038491a326f3b06bae66892cb3edd36d305
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:14:21 +0000

    INTEGRATION: CWS warnings01 (1.2.18); FILE MERGED 2006/05/15 12:09:02 sb 1.2.18.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- fpicker/source/win32/filepicker/controlaccess.hxx
+++ fpicker/source/win32/filepicker/controlaccess.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: controlaccess.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:41:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:14:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 #endif
 
 #ifndef _RTL_USTRING_HXX_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #include <windows.h>
