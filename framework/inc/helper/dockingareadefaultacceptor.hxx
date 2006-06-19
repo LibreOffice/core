08dd89ff4bf72740dfcb921578f09b9c3120f987
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:53:04 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:08 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/helper/dockingareadefaultacceptor.hxx
+++ framework/inc/helper/dockingareadefaultacceptor.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dockingareadefaultacceptor.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:15:49 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:52:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,8 +130,8 @@ class DockingAreaDefaultAcceptor    :   public css::lang::XTypeProvider         
         //  XInterface
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         //---------------------------------------------------------------------------------------------------------
         //  XDockingAreaAcceptor
--- framework/inc/helper/ocomponentaccess.hxx
+++ framework/inc/helper/ocomponentaccess.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ocomponentaccess.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:16:50 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:53:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -172,8 +172,8 @@ class OComponentAccess  :   public css::lang::XTypeProvider             ,
         //  XInterface
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         //---------------------------------------------------------------------------------------------------------
         //  XEnumerationAccess
