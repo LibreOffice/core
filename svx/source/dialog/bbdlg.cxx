0187a37f598df5baa3f5c766e8f863f7d2bcc503
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:00:17 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/04/20 14:49:33 cl 1.3.222.1: warning free code changes

--- svx/source/dialog/bbdlg.cxx
+++ svx/source/dialog/bbdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bbdlg.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:35:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:00:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,6 @@
 #ifndef _SHL_HXX //autogen
 #include <tools/shl.hxx>
 #endif
-#pragma hdrstop
 
 #include "dialogs.hrc"
 #include "page.hrc"
