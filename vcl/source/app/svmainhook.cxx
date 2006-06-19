532535b7d49c5fd4f2a688f407f1c8af4159fb8f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 18:14:53 +0000

    INTEGRATION: CWS warnings01 (1.2.14); FILE MERGED 2005/11/04 16:49:33 pl 1.2.14.1: #i55991# removed warnings for linux/solaris

--- vcl/source/app/svmainhook.cxx
+++ vcl/source/app/svmainhook.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svmainhook.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2005-10-13 09:36:48 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 19:14:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 
 #ifndef MACOSX
 
-BOOL ImplSVMainHook( BOOL *pbInit )
+BOOL ImplSVMainHook( BOOL * )
 {
     return FALSE;   // indicate that ImplSVMainHook is not implemented
 }
