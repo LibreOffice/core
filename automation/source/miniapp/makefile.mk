706f0e09f3c7033b195a742b53945114a6f9b6a7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:21:34 +0000

    INTEGRATION: CWS warnings01 (1.4.12); FILE MERGED 2006/05/22 15:21:18 sb 1.4.12.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- automation/source/miniapp/makefile.mk
+++ automation/source/miniapp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 19:19:47 $
+#   last change: $Author: hr $ $Date: 2006-06-20 00:21:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..
 
-PRJNAME=AUTOMATION
+PRJNAME=automation
 TARGET=miniapp
 
 # --- Settings ------------------------------------------------------------
