f8ca6b4a0725892c7a22833b1b9ca67e5adca5e5
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:32:26 +0000

    INTEGRATION: CWS warnings01 (1.2.92); FILE MERGED 2005/09/22 21:56:45 sb 1.2.92.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/02 16:30:31 sb 1.2.92.1: #i53898# Made code warning-free.

--- store/source/makefile.mk
+++ store/source/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 08:42:35 $
+#   last change: $Author: hr $ $Date: 2006-06-20 00:32:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,6 +37,7 @@ PRJ=..
 
 PRJNAME=store
 TARGET=store
+ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings ---
 
