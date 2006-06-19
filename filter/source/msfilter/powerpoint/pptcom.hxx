539884c29092ddcd2b1a2c2725037635798fcf27
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:10:51 +0000

    INTEGRATION: CWS warnings01 (1.2.150); FILE MERGED 2006/05/12 16:34:22 sb 1.2.150.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- filter/source/msfilter/powerpoint/pptcom.hxx
+++ filter/source/msfilter/powerpoint/pptcom.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pptcom.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:45:49 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:10:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,8 @@
 #ifndef _PPTCOM_HXX
 #define _PPTCOM_HXX
 
-#ifndef __RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 #ifndef _DEBUG_HXX
 #include <tools/debug.hxx>
