c957572a2d456b860dffb1a9e38cdcb981af0df7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:40:07 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/10/27 16:03:12 sb 1.2.8.1: #i53898# Made code warning-free.

--- idl/inc/command.hxx
+++ idl/inc/command.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: command.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:45:00 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:40:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -82,7 +82,7 @@ public:
                 ~SvCommand();
 };
 
-void Init( const SvCommand & rCommand );
+void Init();
 class SvIdlWorkingBase;
 BOOL ReadIdl( SvIdlWorkingBase * pDataBase, const SvCommand & rCommand );
 void DeInit();
