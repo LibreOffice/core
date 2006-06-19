fa704236190896bd0cccc2fbad064f6876aa6c81
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:28:18 +0000

    INTEGRATION: CWS warnings01 (1.2.94); FILE MERGED 2006/02/14 10:17:37 cd 1.2.94.5: #i55991# Fix warnings for ms c++ compiler 2005/11/09 18:02:41 pl 1.2.94.4: #i53898# removed warnings 2005/11/01 12:47:28 cd 1.2.94.3: #i53898# Warning free code for sun solaris compiler 2005/09/22 17:26:49 sb 1.2.94.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/06 12:27:44 cd 1.2.94.1: #i53898# Make code warning free

--- configmgr/source/platformbe/componentdefn.cxx
+++ configmgr/source/platformbe/componentdefn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: componentdefn.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:13:55 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:28:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -66,15 +66,18 @@ static const cppu::ImplementationEntry kImplementations_entries[] =
         NULL,
         0
     },
-    { NULL }
+    { NULL, NULL, NULL, NULL, NULL, 0 }
 } ;
 //------------------------------------------------------------------------------
 
 extern "C" void SAL_CALL component_getImplementationEnvironment(
-                                            const sal_Char **aEnvTypeName,
-                                            uno_Environment **aEnvironment) {
-    *aEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME ;
+                const sal_Char  **ppEnvTypeName,
+                uno_Environment ** /* ppEnv */
+            )
+{
+    *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
+
 //------------------------------------------------------------------------------
 
 extern "C" sal_Bool SAL_CALL component_writeInfo(void *aServiceManager,
@@ -94,3 +97,4 @@ extern "C" void *component_getFactory(const sal_Char *aImplementationName,
                                             kImplementations_entries) ;
 }
 //------------------------------------------------------------------------------
+
