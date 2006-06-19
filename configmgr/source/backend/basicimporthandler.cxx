5ab8756a439379f49732cc7a2402eb8dffa36b93
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:18:10 +0000

    INTEGRATION: CWS warnings01 (1.3.78); FILE MERGED 2005/09/22 16:29:23 sb 1.3.78.2: RESYNC: (1.3-1.4); FILE MERGED 2005/09/06 12:27:37 cd 1.3.78.1: #i53898# Make code warning free

--- configmgr/source/backend/basicimporthandler.cxx
+++ configmgr/source/backend/basicimporthandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: basicimporthandler.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:24:41 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:18:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,10 +51,10 @@ namespace configmgr
 
 BasicImportHandler::BasicImportHandler(
     Backend const & xBackend,OUString const & aEntity, const sal_Bool&  bNotify)
-: m_xBackend(xBackend)
+: m_bSendNotification(bNotify)
+, m_xBackend(xBackend)
 , m_aComponentName()
 , m_aEntity(aEntity)
-, m_bSendNotification(bNotify)
 {
     OSL_ENSURE( m_xBackend.is(), "Creating an import handler without a target backend" );
 }
