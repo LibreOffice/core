996362dddb090bae63bc820814759cbebe2949f8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:41:00 +0000

    INTEGRATION: CWS warnings01 (1.2.32); FILE MERGED 2005/11/02 14:57:56 cl 1.2.32.1: warning free code changes

--- xmloff/source/text/XMLIndexSpanEntryContext.cxx
+++ xmloff/source/text/XMLIndexSpanEntryContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexSpanEntryContext.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:08:25 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:41:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 #endif
 
 #ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLINDEXTEMPLATECONTEXT_HXX_
