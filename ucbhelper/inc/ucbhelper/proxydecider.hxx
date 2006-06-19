0d132d49b8ebb8836f5954102e75fa6bcc81750e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 11:11:04 +0000

    INTEGRATION: CWS warnings01 (1.2.12); FILE MERGED 2005/09/22 18:55:46 sb 1.2.12.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/01 12:15:24 sb 1.2.12.1: #i53898# Made code warning-free.

--- ucbhelper/inc/ucbhelper/proxydecider.hxx
+++ ucbhelper/inc/ucbhelper/proxydecider.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: proxydecider.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:30:04 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 12:11:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <rtl/ustring.hxx>
 #endif
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
-#include <com/sun/star/uno/Reference.hXX>
+#include <com/sun/star/uno/Reference.hxx>
 #endif
 #ifndef INCLUDED_UCBHELPERDLLAPI_H
 #include "ucbhelper/ucbhelperdllapi.h"
