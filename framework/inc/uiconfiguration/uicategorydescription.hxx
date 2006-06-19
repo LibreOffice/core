2049a018e57fb24b273308fcf7071860b7b7301e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:03:32 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:20 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/uiconfiguration/uicategorydescription.hxx
+++ framework/inc/uiconfiguration/uicategorydescription.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uicategorydescription.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:40:51 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:03:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,8 +118,8 @@ class UICategoryDescription :  public com::sun::star::lang::XTypeProvider    ,
         virtual ~UICategoryDescription();
 
         //  XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // XNameAccess
