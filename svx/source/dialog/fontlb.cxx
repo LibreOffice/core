c5459d29da0bf5b7da319bf73919408ba08eda6e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:09:40 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/04/20 14:49:36 cl 1.3.222.1: warning free code changes

--- svx/source/dialog/fontlb.cxx
+++ svx/source/dialog/fontlb.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fontlb.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:05:29 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:09:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,8 +40,6 @@
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
 #endif
-
-#pragma hdrstop
 
 // ============================================================================
 
