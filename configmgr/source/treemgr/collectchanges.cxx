c741fe27d42b87766346dc9e7769e8c8e14108d8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:31:55 +0000

    INTEGRATION: CWS warnings01 (1.7.4); FILE MERGED 2005/11/01 12:47:35 cd 1.7.4.1: #i53898# Warning free code for sun solaris compiler

--- configmgr/source/treemgr/collectchanges.cxx
+++ configmgr/source/treemgr/collectchanges.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: collectchanges.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:26:22 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:31:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -86,7 +86,7 @@ bool convertNodeChange(NodeChangeData& aData_, AddNode const& aChange_)
 }
 //-----------------------------------------------------------------------------
 
-bool convertNodeChange(NodeChangeData& aData_, RemoveNode const& aChange_)
+bool convertNodeChange(NodeChangeData& aData_, RemoveNode const& /*aChange_*/)
 {
     aData_.type = NodeChangeData::eRemoveElement;
 
