06c164265b543640d26c3f10b49fd74dee70338b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:50:52 +0000

    INTEGRATION: CWS warnings01 (1.8.36); FILE MERGED 2006/01/31 08:07:50 sb 1.8.36.1: #i53898# Made code warning-free.

--- xmloff/source/text/txtparai.hxx
+++ xmloff/source/text/txtparai.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtparai.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-14 09:10:47 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:50:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -103,9 +103,7 @@ public:
 
     XMLNumberedParaContext( SvXMLImport& rImport,
             sal_uInt16 nPrfx,
-            const ::rtl::OUString& rLName,
-            const ::com::sun::star::uno::Reference<
-                ::com::sun::star::xml::sax::XAttributeList > & xAttrList );
+            const ::rtl::OUString& rLName );
 
     virtual ~XMLNumberedParaContext();
 
