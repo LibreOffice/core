5a24db68db7111f685b8ac271d14d277d3a33628
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:43:36 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/10/14 11:27:24 sb 1.2.8.1: #i53898# Made code warning-free.

--- idl/source/prj/idldll.cxx
+++ idl/source/prj/idldll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idldll.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:52:38 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:43:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
 
 #ifdef WIN
 
