d5a47cd86f57d3c0b480cfcb1f5944004beb1db4
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:46:34 +0000

    INTEGRATION: CWS warnings01 (1.4.140); FILE MERGED 2006/05/12 16:29:54 sb 1.4.140.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- extensions/source/scanner/scanner.hxx
+++ extensions/source/scanner/scanner.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scanner.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:38:44 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:46:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,8 +42,8 @@
 #ifndef _VOS_MUTEX_HXX_
 #include <vos/mutex.hxx>
 #endif
-#ifndef __RTL_USTRING_
-#include <rtl/ustring>
+#ifndef __RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 #ifndef _CPPUHELPER_WEAK_HXX_
 #include <cppuhelper/weak.hxx>
