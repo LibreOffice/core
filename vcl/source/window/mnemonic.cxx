0028785abfa9bf34d94fb7f2c92093869ebe0d8b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 18:39:17 +0000

    INTEGRATION: CWS warnings01 (1.18.70); FILE MERGED 2005/11/10 20:06:34 pl 1.18.70.1: #i55991# removed warnings

--- vcl/source/window/mnemonic.cxx
+++ vcl/source/window/mnemonic.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mnemonic.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:27:44 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 19:39:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -381,9 +381,9 @@ BOOL MnemonicGenerator::CreateMnemonic( XubString& rKey )
 
 uno::Reference< i18n::XCharacterClassification > MnemonicGenerator::GetCharClass()
 {
-    if ( !xCharClass.is() )
-        xCharClass = vcl::unohelper::CreateCharacterClassification();
-    return xCharClass;
+    if ( !mxCharClass.is() )
+        mxCharClass = vcl::unohelper::CreateCharacterClassification();
+    return mxCharClass;
 }
 
 // -----------------------------------------------------------------------
