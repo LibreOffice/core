3a3a7124c414247000ce7e83d3b45e6cc9301199
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:55:51 +0000

    INTEGRATION: CWS warnings01 (1.2.32); FILE MERGED 2005/10/28 14:48:12 cd 1.2.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/interaction/stillinteraction.hxx
+++ framework/inc/interaction/stillinteraction.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stillinteraction.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:21:33 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:55:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,8 +122,8 @@ class StillInteraction : public  css::lang::XTypeProvider
     public:
 
         // XInterface, XTypeProvider
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         //_________________________________
         /**
