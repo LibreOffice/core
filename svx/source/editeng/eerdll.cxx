9ffae9c57d8ef4e3ec3cfa9c7307c538dc31203a
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:41:20 +0000

    INTEGRATION: CWS warnings01 (1.23.222); FILE MERGED 2006/04/20 14:49:50 cl 1.23.222.1: warning free code changes

--- svx/source/editeng/eerdll.cxx
+++ svx/source/editeng/eerdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eerdll.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:31:47 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:41:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,8 +34,6 @@
  ************************************************************************/
 
 #include <eeng_pch.hxx>
-
-#pragma hdrstop
 
 #include <svtools/solar.hrc>
 #include <eerdll.hxx>
