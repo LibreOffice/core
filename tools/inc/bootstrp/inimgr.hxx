1b1c3ad023ef378d63ffaa2b59ca416f2942b0f8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:31:55 +0000

    INTEGRATION: CWS warnings01 (1.1.2); FILE ADDED 2005/10/14 11:23:27 sb 1.1.2.1: #i53898# Moved here from tools/bootstrp/, for better hedabu results.

---
+++ tools/inc/bootstrp/inimgr.hxx
@@ -0,0 +1,70 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: inimgr.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-06-19 13:31:55 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _INIMGR_HXX
+#define _INIMGR_HXX
+
+#include "fsys.hxx"
+#include "string.hxx"
+
+
+/*****************************************************************************
+Purpose: Allows to work on a local set of initialisation files
+If Update is used, the user must ensure that only one set of
+Source and Destination Dir is used. Otherwise ForceUpdate has to be used
+*****************************************************************************/
+
+class IniManager
+{
+private:
+    BOOL bUpdate;
+
+    ByteString sGlobalDir;      /// holds the org. ini dir
+    ByteString sLocalPath;      /// holds path of local ini dir
+
+public:
+    IniManager( ByteString &rDir, ByteString &rLocalDir );
+    IniManager( ByteString &rDir );
+    IniManager();
+
+    ByteString ToLocal( ByteString &rPath );
+    void Update();          /// Call ForceUpdate the First Time called
+    void ForceUpdate();
+
+    static ByteString GetLocalIni();
+    static ByteString GetGlobalIni();
+};
+
+#endif
+
