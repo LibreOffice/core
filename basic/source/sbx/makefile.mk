ae1ce3f8caddf04987ec74e87442a22c804f1c2f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:48:33 +0000

    INTEGRATION: CWS warnings01 (1.3.30); FILE MERGED 2005/12/05 14:17:15 ab 1.3.30.1: #i53898# Removed warnings for solaris

--- basic/source/sbx/makefile.mk
+++ basic/source/sbx/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 21:45:16 $
+#   last change: $Author: hr $ $Date: 2006-06-19 17:48:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -72,8 +72,11 @@ SLOFILES=	\
     $(SLO)$/sbxuint.obj	\
     $(SLO)$/sbxulng.obj	\
     $(SLO)$/sbxform.obj	\
-    $(SLO)$/sbxscan.obj \
+    $(SLO)$/sbxscan.obj 	\
     $(SLO)$/sbxdec.obj
+
+
+EXCEPTIONSFILES=$(SLO)$/sbxarray.obj
 
 # --- Targets -------------------------------------------------------------
 
