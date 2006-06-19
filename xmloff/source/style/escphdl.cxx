349905166d94d82b1f4b58f14d56ffe13c887af6
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:32:26 +0000

    INTEGRATION: CWS warnings01 (1.4.34); FILE MERGED 2005/11/03 17:47:04 cl 1.4.34.1: warning free code changes for unxlngi6

--- xmloff/source/style/escphdl.cxx
+++ xmloff/source/style/escphdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: escphdl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:41:15 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:32:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,7 +79,6 @@ XMLEscapementPropHdl::~XMLEscapementPropHdl()
 
 sal_Bool XMLEscapementPropHdl::importXML( const OUString& rStrImpValue, uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const
 {
-    sal_Bool bRet = sal_False;
     sal_Int16 nVal;
 
     SvXMLTokenEnumerator aTokens( rStrImpValue );
@@ -111,7 +110,6 @@ sal_Bool XMLEscapementPropHdl::importXML( const OUString& rStrImpValue, uno::Any
 
 sal_Bool XMLEscapementPropHdl::exportXML( OUString& rStrExpValue, const uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const
 {
-    sal_Bool bRet = sal_False;
     sal_Int32 nValue;
     OUStringBuffer aOut;
 
