f671721d83b1c3a40ace22acbad4fa3fe15d96b8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:23:35 +0000

    INTEGRATION: CWS warnings01 (1.2.12); FILE MERGED 2006/02/02 09:19:58 gh 1.2.12.1: removed compiler warnings

--- automation/source/server/profiler.hxx
+++ automation/source/server/profiler.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: profiler.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:24:45 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:23:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -100,8 +100,8 @@ private:
     String GetProfileLine( ProfileSnapshot *pStart, ProfileSnapshot *pStop );
 
 
-    ProfileSnapshot *pStart;
-    ProfileSnapshot *pEnd;
+    ProfileSnapshot *mpStart;
+    ProfileSnapshot *mpEnd;
     BOOL bIsProfileIntervalStarted;
 
 
