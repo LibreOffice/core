e826c26cb8d0b626c88816642f7623aaab2da3d1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 11:56:47 +0000

    INTEGRATION: CWS warnings01 (1.6.68); FILE MERGED 2006/03/14 15:20:30 fs 1.6.68.1: #i57457# warning-free code

--- forms/source/component/scrollbar.cxx
+++ forms/source/component/scrollbar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scrollbar.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:52:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 12:56:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -267,7 +267,7 @@ namespace frm
     }
 
     //------------------------------------------------------------------------------
-    sal_Bool OScrollBarModel::commitControlValueToDbColumn( bool _bPostReset )
+    sal_Bool OScrollBarModel::commitControlValueToDbColumn( bool /*_bPostReset*/ )
     {
         OSL_ENSURE( sal_False, "OScrollBarModel::commitControlValueToDbColumn: never to be called (we're not bound)!" );
         return sal_True;
--- forms/source/component/spinbutton.cxx
+++ forms/source/component/spinbutton.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spinbutton.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:52:40 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 12:56:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -215,7 +215,7 @@ namespace frm
     }
 
     //------------------------------------------------------------------------------
-    sal_Bool OSpinButtonModel::commitControlValueToDbColumn( bool _bPostReset )
+    sal_Bool OSpinButtonModel::commitControlValueToDbColumn( bool /*_bPostReset*/ )
     {
         OSL_ENSURE( sal_False, "OSpinButtonModel::commitControlValueToDbColumn: never to be called (we're not bound)!" );
         return sal_True;
