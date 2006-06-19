f5e07d2a818fbd7de0c197da6912c38cbdaffcfe
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:56:33 +0000

    INTEGRATION: CWS warnings01 (1.4.8); FILE MERGED 2006/02/24 14:49:19 sb 1.4.8.3: #i53898# Made code warning-free; removed dead code. 2005/10/14 14:35:41 sb 1.4.8.2: #i53898# Fixed typo. 2005/10/14 11:19:44 sb 1.4.8.1: #i53898# Made code warning-free; cleanup.

--- tools/win/source/dll/makefile.mk
+++ tools/win/source/dll/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 14:46:59 $
+#   last change: $Author: hr $ $Date: 2006-06-19 13:56:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,8 @@ TARGET=dll
 .INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
 .INCLUDE :  sv.mk
+
+.INCLUDE: $(PRJ)$/util$/makefile.pmk
 
 # --- WNT ----------------------------------------------------------
 
