be3decb1f90a956d3cc07d5d0048e112799b21f1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:51:53 +0000

    INTEGRATION: CWS warnings01 (1.3.32); FILE MERGED 2005/11/17 16:33:56 pl 1.3.32.1: #i55991# removed warnings

--- xmloff/source/transform/ControlOOoTContext.cxx
+++ xmloff/source/transform/ControlOOoTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ControlOOoTContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:38:43 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:51:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -83,8 +83,8 @@ void XMLControlOOoTransformerContext::StartElement(
 }
 
 XMLTransformerContext *XMLControlOOoTransformerContext::CreateChildContext(
-        sal_uInt16 nPrefix,
-        const OUString& rLocalName,
+        sal_uInt16 /*nPrefix*/,
+        const OUString& /*rLocalName*/,
         const OUString& rQName,
         const Reference< XAttributeList >& rAttrList )
 {
