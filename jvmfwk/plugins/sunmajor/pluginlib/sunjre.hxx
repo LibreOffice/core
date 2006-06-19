f625aac94858c0b13e394d2120332a4a88ec82fb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:09:38 +0000

    INTEGRATION: CWS warnings01 (1.4.6); FILE MERGED 2005/10/27 15:08:48 pl 1.4.6.1: #i55991# removed warnings for solaris platform

--- jvmfwk/plugins/sunmajor/pluginlib/sunjre.hxx
+++ jvmfwk/plugins/sunmajor/pluginlib/sunjre.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sunjre.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:31:26 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:09:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,6 +49,7 @@ public:
 
     static rtl::Reference<VendorBase> createInstance();
 
+    using VendorBase::getLibraryPaths;
     virtual char const* const* getRuntimePaths(int * size);
     virtual char const* const* getLibraryPaths(int* size);
 
