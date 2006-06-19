5e03746335d285781d294f0f4361ecff43dde819
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:45:08 +0000

    INTEGRATION: CWS warnings01 (1.3.6); FILE MERGED 2006/03/10 19:08:13 lo 1.3.6.1: warnings01: remove warnings in unoxml

--- unoxml/source/dom/documentfragment.cxx
+++ unoxml/source/dom/documentfragment.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentfragment.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:59:04 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:44:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
