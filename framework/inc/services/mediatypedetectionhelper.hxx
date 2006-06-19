5e9b35fa21213645202cbb88dc2e177ad4ce7d7c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:00:27 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:16 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/services/mediatypedetectionhelper.hxx
+++ framework/inc/services/mediatypedetectionhelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mediatypedetectionhelper.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:32:15 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:00:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -150,8 +150,8 @@ class MediaTypeDetectionHelper  :   public css::lang::XTypeProvider
         //  XInterface, XTypeProvider, XServiceInfo
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         //---------------------------------------------------------------------------------------------------------
