dc3e1d048ed2faddd70c1c311ce074adac15b634
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:27:49 +0000

    INTEGRATION: CWS warnings01 (1.12.34); FILE MERGED 2005/11/02 14:57:55 cl 1.12.34.1: warning free code changes

--- xmloff/source/style/TransGradientStyle.cxx
+++ xmloff/source/style/TransGradientStyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TransGradientStyle.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:30:14 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:27:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@
 #endif
 
 #ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _TOOLS_DEBUG_HXX
