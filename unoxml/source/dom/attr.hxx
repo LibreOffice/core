208cb7a476c68f447e14470682b67bef08268117
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:43:12 +0000

    INTEGRATION: CWS warnings01 (1.5.6); FILE MERGED 2006/03/10 19:08:12 lo 1.5.6.1: warnings01: remove warnings in unoxml

--- unoxml/source/dom/attr.hxx
+++ unoxml/source/dom/attr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: attr.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:54:06 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:43:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
