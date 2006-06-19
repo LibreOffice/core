0975cf7e5ecc2723b1514575c7c2f795ff0c689b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:07:09 +0000

    INTEGRATION: CWS warnings01 (1.5.32); FILE MERGED 2005/10/28 14:48:23 cd 1.5.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/uifactory/popupmenucontrollerfactory.hxx
+++ framework/inc/uifactory/popupmenucontrollerfactory.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: popupmenucontrollerfactory.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:49:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:07:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,8 +118,8 @@ class PopupMenuControllerFactory :  public com::sun::star::lang::XTypeProvider  
         virtual ~PopupMenuControllerFactory();
 
         //  XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // XMultiComponentFactory
