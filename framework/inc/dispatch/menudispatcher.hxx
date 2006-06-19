6f735c0474463bb048e2ec9a3922eaaecc2368ba
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:51:25 +0000

    INTEGRATION: CWS warnings01 (1.5.32); FILE MERGED 2005/10/28 14:48:07 cd 1.5.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/dispatch/menudispatcher.hxx
+++ framework/inc/dispatch/menudispatcher.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: menudispatcher.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:13:58 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:51:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -223,8 +223,8 @@ class MenuDispatcher   :   // interfaces
         //  XInterface
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         //---------------------------------------------------------------------------------------------------------
         //  XDispatch
