cee62a2218140319183c41d007a443f2ad41ed6d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:01:37 +0000

    INTEGRATION: CWS warnings01 (1.6.32); FILE MERGED 2005/10/28 14:48:17 cd 1.6.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/services/urltransformer.hxx
+++ framework/inc/services/urltransformer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: urltransformer.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:35:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:01:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -164,8 +164,8 @@ class URLTransformer    :   public css::lang::XTypeProvider     ,
         //  XInterface, XTypeProvider, XServiceInfo
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         //---------------------------------------------------------------------------------------------------------
