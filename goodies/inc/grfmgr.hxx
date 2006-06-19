4c2bed6349d2255fb705397b73c705e13875bdad
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:35:42 +0000

    INTEGRATION: CWS warnings01 (1.18.26); FILE MERGED 2006/04/07 19:19:07 sb 1.18.26.2: RESYNC: (1.18-1.20); FILE MERGED 2005/11/16 16:42:06 pl 1.18.26.1: #i55991# removed warnings

--- goodies/inc/grfmgr.hxx
+++ goodies/inc/grfmgr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: grfmgr.hxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-16 13:09:17 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:35:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -494,8 +494,8 @@ private:
     List            maObjList;
     GraphicCache*   mpCache;
 
-                    GraphicManager( const GraphicManager& rGraphicManager ) {}
-    GraphicManager& operator=( const GraphicManager& rGraphicManager ) { return *this; }
+                    GraphicManager( const GraphicManager& ) {}
+    GraphicManager& operator=( const GraphicManager& ) { return *this; }
 
 #if _SOLAR__PRIVATE
 
