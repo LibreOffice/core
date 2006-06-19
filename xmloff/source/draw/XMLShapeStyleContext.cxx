7a75f5094b3346e1b866384c83c2f8421b808e0f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:08:55 +0000

    INTEGRATION: CWS warnings01 (1.13.32); FILE MERGED 2005/11/16 21:34:08 pl 1.13.32.1: #i55991# removed warnings

--- xmloff/source/draw/XMLShapeStyleContext.cxx
+++ xmloff/source/draw/XMLShapeStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLShapeStyleContext.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:42:27 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:08:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -322,7 +322,7 @@ void XMLShapeStyleContext::FillPropertySet( const Reference< beans::XPropertySet
     }
 }
 
-void XMLShapeStyleContext::Finish( sal_Bool bOverwrite )
+void XMLShapeStyleContext::Finish( sal_Bool /*bOverwrite*/ )
 {
 }
 
