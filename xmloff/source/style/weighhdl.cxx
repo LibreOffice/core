f2368f9cdf42de776f0f3d74d96eb368484a71b2
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:35:18 +0000

    INTEGRATION: CWS warnings01 (1.3.34); FILE MERGED 2005/11/02 14:57:56 cl 1.3.34.1: warning free code changes

--- xmloff/source/style/weighhdl.cxx
+++ xmloff/source/style/weighhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: weighhdl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:50:32 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:35:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@
 #endif
 
 #ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
