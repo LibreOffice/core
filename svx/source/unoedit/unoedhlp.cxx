c9731d1251fddfedc99ce20f2b5607b3f3c90d2a
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:01:10 +0000

    INTEGRATION: CWS warnings01 (1.7.220); FILE MERGED 2006/02/17 16:21:31 cl 1.7.220.1: warning free code changes

--- svx/source/unoedit/unoedhlp.cxx
+++ svx/source/unoedit/unoedhlp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoedhlp.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:10:29 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:01:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
