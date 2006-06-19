42d5b27400765bad5905059df7abdcab0e431edd
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:20:42 +0000

    INTEGRATION: CWS warnings01 (1.8.4); FILE MERGED 2005/11/01 12:47:14 cd 1.8.4.1: #i53898# Warning free code for sun solaris compiler

--- configmgr/source/backend/updatedispatch.cxx
+++ configmgr/source/backend/updatedispatch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: updatedispatch.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:35:18 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:20:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -506,7 +506,7 @@ sal_Int16 UpdateDispatcher::getUpdateAttributes(node::Attributes const & _aAttri
 }
 // -----------------------------------------------------------------------------
 
-sal_Int16 UpdateDispatcher::getUpdateAttributeMask(node::Attributes const & _aAttributes)
+sal_Int16 UpdateDispatcher::getUpdateAttributeMask(node::Attributes const & /*_aAttributes*/)
 {
     // no support for post-creation attribute changes yet
     return 0;
