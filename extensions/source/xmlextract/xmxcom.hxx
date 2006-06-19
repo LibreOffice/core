c2e8dab4a89454e161db4d848a52457bcb6099f6
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:47:02 +0000

    INTEGRATION: CWS warnings01 (1.2.140); FILE MERGED 2006/05/12 16:29:55 sb 1.2.140.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- extensions/source/xmlextract/xmxcom.hxx
+++ extensions/source/xmlextract/xmxcom.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmxcom.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:43:05 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:47:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,8 +39,8 @@
 #ifndef _CPPUHELPER_WEAK_HXX_
 #include <cppuhelper/weak.hxx>
 #endif
-#ifndef __RTL_USTRING_
-#include <rtl/ustring>
+#ifndef __RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 #ifndef _DEBUG_HXX
 #include <tools/debug.hxx>
