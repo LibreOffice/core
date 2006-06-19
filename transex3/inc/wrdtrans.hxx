b85d69245b8eeee2bbbffda28286375e64658c6b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:20:05 +0000

    INTEGRATION: CWS warnings01 (1.2.16); FILE MERGED 2005/11/07 12:47:07 ihi 1.2.16.1: #i57362# Remove warnings

--- transex3/inc/wrdtrans.hxx
+++ transex3/inc/wrdtrans.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrdtrans.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:57:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:20:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+/*
 
+// OBSOLETE //
 
 #ifndef TX3_WRDTRANS_HXX
 #define TX3_WRDTRANS_HXX
@@ -90,4 +92,4 @@ class WordTransformer
 #endif
 
 
-
+*/
