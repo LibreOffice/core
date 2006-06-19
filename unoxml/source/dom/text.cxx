622d616e6aaec43834a950107914a1dc79f2f2e2
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:48:43 +0000

    INTEGRATION: CWS warnings01 (1.4.6); FILE MERGED 2006/03/10 19:08:16 lo 1.4.6.1: warnings01: remove warnings in unoxml

--- unoxml/source/dom/text.cxx
+++ unoxml/source/dom/text.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: text.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:05:57 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:48:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,7 @@ namespace DOM
         init_characterdata(aNodePtr);
     }
 
-    Reference< XText > SAL_CALL CText::splitText(sal_Int32 offset)
+  Reference< XText > SAL_CALL CText::splitText(sal_Int32 /*offset*/)
              throw (RuntimeException)
     {
         return Reference< XText >(this);
