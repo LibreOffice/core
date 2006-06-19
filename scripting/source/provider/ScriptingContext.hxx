275ed76d0a3ac33c4b8a567306deb5ff6e926c9e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:22:05 +0000

    INTEGRATION: CWS warnings01 (1.6.14); FILE MERGED 2005/12/22 14:40:54 ab 1.6.14.1: #i53898# Removed warnings for unxlngi6, unxlngi6.pro

--- scripting/source/provider/ScriptingContext.hxx
+++ scripting/source/provider/ScriptingContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScriptingContext.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:31:29 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:22:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 
 #include <osl/mutex.hxx>
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #include <cppuhelper/implbase1.hxx>
 #include <comphelper/uno3.hxx>
 #include <comphelper/propertycontainer.hxx>
