0770737cadf82f056ba07f2b66e93d3991e3b9c1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:56:19 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:12 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/jobs/job.hxx
+++ framework/inc/jobs/job.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: job.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:22:20 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:56:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -287,8 +287,8 @@ class Job : public  css::lang::XTypeProvider
 
     public:
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
 
         // XJobListener
         virtual void SAL_CALL jobFinished( const css::uno::Reference< css::task::XAsyncJob >& xJob,
