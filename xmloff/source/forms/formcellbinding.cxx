72e7babdf56a6113f9ba896450050e7234881b31
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:18:22 +0000

    INTEGRATION: CWS warnings01 (1.5.32); FILE MERGED 2005/11/16 22:47:10 pl 1.5.32.1: #i55991# removed warnings

--- xmloff/source/forms/formcellbinding.cxx
+++ xmloff/source/forms/formcellbinding.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formcellbinding.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:08:27 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:18:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -195,7 +195,7 @@ namespace xmloff
     }
 
     //------------------------------------------------------------------------
-    bool FormCellBindingHelper::convertStringAddress( const ::rtl::OUString& _rAddressDescription, CellAddress& /* [out] */ _rAddress, sal_Int16 _nAssumeSheet ) const
+    bool FormCellBindingHelper::convertStringAddress( const ::rtl::OUString& _rAddressDescription, CellAddress& /* [out] */ _rAddress, sal_Int16 /*_nAssumeSheet*/ ) const
     {
         Any aAddress;
         return doConvertAddressRepresentations(
