c9de680ddc95077bb67fbfbf3ca1c913ca5d1269
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:26:57 +0000

    INTEGRATION: CWS warnings01 (1.4.10); FILE MERGED 2005/10/28 10:53:43 pl 1.4.10.1: #i55991# removed warnings for solaris platform

--- psprint/source/printergfx/makefile.mk
+++ psprint/source/printergfx/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 12:03:02 $
+#   last change: $Author: hr $ $Date: 2006-06-19 10:26:57 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,6 +39,8 @@ PRJNAME=psprint
 TARGET=gfx
 
 # --- Settings -----------------------------------------------------
+
+ENABLE_EXCEPTIONS=true
 
 .INCLUDE :	svpre.mk
 .INCLUDE :	settings.mk
