8c91ddeaa98e7e93cb626586ff47a72fc367d762
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:13:34 +0000

    INTEGRATION: CWS warnings01 (1.2.60); FILE MERGED 2005/11/15 17:54:42 pl 1.2.60.1: #i55991# removed warnings

--- svtools/source/items/stritem.cxx
+++ svtools/source/items/stritem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stritem.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:00:54 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:13:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,8 +47,8 @@ TYPEINIT1_AUTOFACTORY(SfxStringItem, CntUnencodedStringItem)
 
 //============================================================================
 // virtual
-SfxStringItem::SfxStringItem(USHORT nWhich, SvStream & rStream):
-    CntUnencodedStringItem(nWhich)
+SfxStringItem::SfxStringItem(USHORT which, SvStream & rStream):
+    CntUnencodedStringItem(which)
 {
     UniString aValue;
     readByteString(rStream, aValue);
