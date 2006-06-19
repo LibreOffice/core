05a35a6d89699d7c5dfd3a6ae49c25ee1428bbd8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:48:11 +0000

    INTEGRATION: CWS warnings01 (1.2.30); FILE MERGED 2006/05/19 07:46:21 sb 1.2.30.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- basic/source/sample/makefile.mk
+++ basic/source/sample/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 21:43:56 $
+#   last change: $Author: hr $ $Date: 2006-06-19 17:48:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..
 
-PRJNAME=BASIC
+PRJNAME=basic
 TARGET=sample
 
 # --- Settings ------------------------------------------------------------
