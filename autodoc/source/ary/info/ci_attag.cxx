db1ccf250fe3ff232393dfa68b38fa2b4ebae359
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:54:36 +0000

    INTEGRATION: CWS warnings01 (1.2.4); FILE MERGED 2005/10/18 08:35:53 np 1.2.4.1: #i53898#

--- autodoc/source/ary/info/ci_attag.cxx
+++ autodoc/source/ary/info/ci_attag.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ci_attag.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:08:35 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:54:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -91,7 +91,7 @@ AtTag::Add_Eol()
 }
 
 void
-AtTag::do_StoreAt( DocuDisplay &  o_rDisplay ) const
+AtTag::do_StoreAt( DocuDisplay &  ) const
 {
     // Dummy
 }
