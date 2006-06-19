fed2908e34cda4485fe5e71a83d997dc53afd35d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 11:16:58 +0000

    INTEGRATION: CWS warnings01 (1.13.10); FILE MERGED 2005/11/11 15:50:41 pl 1.13.10.1: #i55991# removed warnings

--- padmin/source/pamain.cxx
+++ padmin/source/pamain.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pamain.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:27:22 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 12:16:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -120,7 +120,6 @@ void MyApp::Main()
     //-------------------------------------------------
     // create the global service-manager
     //-------------------------------------------------
-    Reference< XComponentContext > xCtx;
     Reference< XMultiServiceFactory > xFactory;
     try
     {
