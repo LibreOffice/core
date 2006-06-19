cd1a1942a9e1f345ad57662767910f76c2537868
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:14:42 +0000

    INTEGRATION: CWS warnings01 (1.9.34); FILE MERGED 2005/11/02 14:57:52 cl 1.9.34.1: warning free code changes

--- xmloff/source/draw/ximpnote.cxx
+++ xmloff/source/draw/ximpnote.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximpnote.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:59:17 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:14:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
 
 #ifndef _XIMPNOTES_HXX
 #include "ximpnote.hxx"
