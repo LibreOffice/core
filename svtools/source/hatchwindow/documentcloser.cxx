3d1bdfaabb089729f3a8d8abff228db927cebd17
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:09:43 +0000

    INTEGRATION: CWS warnings01 (1.2.48); FILE MERGED 2006/04/20 14:51:15 sb 1.2.48.1: #i53898# Made code compile and/or warning-free again after resync to SRC680m162.

--- svtools/source/hatchwindow/documentcloser.cxx
+++ svtools/source/hatchwindow/documentcloser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentcloser.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-09 13:58:54 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:09:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -106,6 +106,7 @@ void MainThreadFrameCloserRequest::Start( MainThreadFrameCloserRequest* pMTReque
 // --------------------------------------------------------
 IMPL_STATIC_LINK( MainThreadFrameCloserRequest, worker, MainThreadFrameCloserRequest*, pMTRequest )
 {
+    (void) pThis; // unused
     if ( pMTRequest )
     {
         if ( pMTRequest->m_xFrame.is() )
