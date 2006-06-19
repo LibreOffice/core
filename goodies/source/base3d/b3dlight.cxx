fd933bff8cf1896554008f45ff30ee6eb58e624d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:39:21 +0000

    INTEGRATION: CWS warnings01 (1.3.26); FILE MERGED 2005/11/16 16:42:09 pl 1.3.26.1: #i55991# removed warnings

--- goodies/source/base3d/b3dlight.cxx
+++ goodies/source/base3d/b3dlight.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dlight.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:26:13 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:39:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -104,17 +104,14 @@ const Color B3dLight::GetIntensity(Base3DMaterialValue eVal)
         case Base3DMaterialAmbient:
         {
             return aAmbient;
-            break;
         }
         case Base3DMaterialDiffuse:
         {
             return aDiffuse;
-            break;
         }
         default:
         {
             return aSpecular;
-            break;
         }
     }
 }
