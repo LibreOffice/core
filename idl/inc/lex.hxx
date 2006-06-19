d2348dc9c13bc31ea5c818225763e74941246eee
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:40:30 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/10/27 16:03:13 sb 1.2.8.1: #i53898# Made code warning-free.

--- idl/inc/lex.hxx
+++ idl/inc/lex.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lex.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:46:19 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:40:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -280,7 +280,6 @@ public:
                     {
                         pCurToken = aTokList.Seek( nMaxPos );
                     }
-    void            Replace( const Range & rRange, SvToken * pToken );
 };
 
 
