ac9724549b3ce475507f55568d1450e3fd5432cc
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:39:37 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/04/20 14:49:49 cl 1.3.222.1: warning free code changes

--- svx/source/editeng/editsel.cxx
+++ svx/source/editeng/editsel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editsel.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:28:36 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:39:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,8 +34,6 @@
  ************************************************************************/
 
 #include <eeng_pch.hxx>
-
-#pragma hdrstop
 
 #include <editsel.hxx>
 #include <impedit.hxx>
