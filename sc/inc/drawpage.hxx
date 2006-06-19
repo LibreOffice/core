983ef22c169ca4aff8258fe8f7395d5ffcf780c2
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:31:27 +0000

    INTEGRATION: CWS warnings01 (1.3.182); FILE MERGED 2006/05/12 16:38:14 sb 1.3.182.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162.

--- sc/inc/drawpage.hxx
+++ sc/inc/drawpage.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawpage.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:37:21 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:31:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,6 @@ public:
     ScDrawPage(ScDrawLayer& rNewModel, StarBASIC* pBasic, BOOL bMasterPage=FALSE);
     ~ScDrawPage();
 
-    virtual void    RequestBasic();
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > createUnoPage();
 };
 
