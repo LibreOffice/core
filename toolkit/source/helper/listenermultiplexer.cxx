04c31643c256ee40ecb9a248872f42f64286ac58
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 22:05:34 +0000

    INTEGRATION: CWS warnings01 (1.3.26); FILE MERGED 2005/10/25 15:56:12 mt 1.3.26.1: #i55991# Warning Free Code

--- toolkit/source/helper/listenermultiplexer.cxx
+++ toolkit/source/helper/listenermultiplexer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listenermultiplexer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:22:15 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 23:05:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,6 +44,10 @@
 //  ----------------------------------------------------
 ListenerMultiplexerBase::ListenerMultiplexerBase( ::cppu::OWeakObject& rSource )
     : ::cppu::OInterfaceContainerHelper( GetMutex() ), mrContext( rSource )
+{
+}
+
+ListenerMultiplexerBase::~ListenerMultiplexerBase()
 {
 }
 
