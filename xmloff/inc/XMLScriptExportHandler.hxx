f0043d2dd0255692515d54daf4b794bab50fe829
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:54:16 +0000

    INTEGRATION: CWS warnings01 (1.2.34); FILE MERGED 2005/11/02 14:57:45 cl 1.2.34.1: warning free code changes

--- xmloff/inc/XMLScriptExportHandler.hxx
+++ xmloff/inc/XMLScriptExportHandler.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLScriptExportHandler.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:54:55 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:54:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef _RTL_USTRING_HXX_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLEVENT_HXX
