c78cc009ed37c5519921e9dcf30be80f414266b0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:37:53 +0000

    INTEGRATION: CWS warnings01 (1.11.220); FILE MERGED 2006/04/20 14:49:48 cl 1.11.220.1: warning free code changes

--- svx/source/editeng/editattr.cxx
+++ svx/source/editeng/editattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editattr.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:25:30 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:37:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,8 +34,6 @@
  ************************************************************************/
 
 #include <eeng_pch.hxx>
-
-#pragma hdrstop
 
 #include <svxfont.hxx>
 #include <flditem.hxx>
