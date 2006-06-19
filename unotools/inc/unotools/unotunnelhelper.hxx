b923833cd04d962bafb4d8c18024f7a60fd20d77
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 13:03:46 +0000

    INTEGRATION: CWS warnings01 (1.2.16); FILE MERGED 2005/10/21 09:48:49 dbo 1.2.16.1: #i53898# warning free code

--- unotools/inc/unotools/unotunnelhelper.hxx
+++ unotools/inc/unotools/unotunnelhelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unotunnelhelper.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:40:10 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 14:03:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -183,7 +183,7 @@ protected:
     sal_Int64 getSomething(staruno::Sequence<sal_Int8> const& rTunnelId) throw()
     {
         if (s_aTunnelId.equalTo(rTunnelId))
-            return makeUnoSomething();
+            return this->makeUnoSomething();
         else
             return sal_Int64();
     }
