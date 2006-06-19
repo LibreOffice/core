d3fa762edfdb7150464bfbb556555232835b97eb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 21:55:37 +0000

    INTEGRATION: CWS warnings01 (1.6.26); FILE MERGED 2005/11/14 10:36:04 pl 1.6.26.1: #i55991# removed warnings

--- toolkit/inc/toolkit/controls/accessiblecontrolcontext.hxx
+++ toolkit/inc/toolkit/controls/accessiblecontrolcontext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accessiblecontrolcontext.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:47:47 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 22:55:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -134,6 +134,7 @@ namespace toolkit
         virtual sal_Int32 SAL_CALL getBackground(  ) throw (::com::sun::star::uno::RuntimeException);
 
         // XEventListener
+        using comphelper::OAccessibleContextHelper::disposing;
         virtual void SAL_CALL disposing( const ::com::sun::star::lang::EventObject& Source ) throw (::com::sun::star::uno::RuntimeException);
 
     private:
