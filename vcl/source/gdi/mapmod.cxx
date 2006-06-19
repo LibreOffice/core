29a167351c53c948c1230eb8c365e9a5ebfedf22
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 18:26:32 +0000

    INTEGRATION: CWS warnings01 (1.5.2); FILE MERGED 2005/11/04 17:25:01 pl 1.5.2.1: #i55991# removed warnings for linux/solaris

--- vcl/source/gdi/mapmod.cxx
+++ vcl/source/gdi/mapmod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mapmod.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-01 10:32:20 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 19:26:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,7 @@
 
 // =======================================================================
 
-DBG_NAME( MapMode );
+DBG_NAME( MapMode )
 
 // -----------------------------------------------------------------------
 
