1a447b01958dea7f89fc9a5853b77ea3242e24b8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 13:19:20 +0000

    INTEGRATION: CWS warnings01 (1.3.14); FILE MERGED 2006/03/10 15:10:59 pl 1.3.14.1: #i55991# removed warnings for windows platform

--- shell/source/tools/regsvrex/regsvrex.cxx
+++ shell/source/tools/regsvrex/regsvrex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: regsvrex.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:53:31 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 14:19:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,13 @@
  *
  ************************************************************************/
 
+#if defined _MSC_VER
+#pragma warning(push, 1)
+#endif
 #include <windows.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 typedef HRESULT (__stdcall *lpfnDllRegisterServer)();
 typedef HRESULT (__stdcall *lpfnDllUnregisterServer)();
