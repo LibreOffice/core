97b69b5ff2bc2f9056313a57904c2067d4046feb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:20:18 +0000

    INTEGRATION: CWS warnings01 (1.17.34); FILE MERGED 2005/11/16 22:47:11 pl 1.17.34.1: #i55991# removed warnings

--- xmloff/source/forms/layerimport.hxx
+++ xmloff/source/forms/layerimport.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: layerimport.hxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:13:39 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:20:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,7 +64,7 @@ class XMLPropertyHandlerFactory;
 class SvXMLImportPropertyMapper;
 class XMLPropStyleContext;
 
-SV_DECL_REF( SvXMLStylesContext );
+SV_DECL_REF( SvXMLStylesContext )
     // unfortunately, we can't put this into our namespace, as the macro expands to (amongst others) a forward
     // declaration of the class name, which then would be in the namespace, too
 
