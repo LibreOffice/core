92245878b2712b5d98b6b40edf9ee0007d860075
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:53:33 +0000

    INTEGRATION: CWS warnings01 (1.6.32); FILE MERGED 2005/10/28 14:48:09 cd 1.6.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/helper/oframes.hxx
+++ framework/inc/helper/oframes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oframes.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:17:19 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:53:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -168,8 +168,8 @@ class OFrames   :   public css::lang::XTypeProvider ,
         //  XInterface
         //---------------------------------------------------------------------------------------------------------
 
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+            FWK_DECLARE_XTYPEPROVIDER
 
         //---------------------------------------------------------------------------------------------------------
         //  XFrames
