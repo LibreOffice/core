e835600bddebf64bb92823cdd8bf5f51577ce8be
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 15:21:43 +0000

    INTEGRATION: CWS warnings01 (1.4.220); FILE MERGED 2006/04/20 14:49:58 cl 1.4.220.1: warning free code changes

--- svx/source/options/optitems.cxx
+++ svx/source/options/optitems.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optitems.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:53:09 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 16:21:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
-
-// -----------------------------------------------------------------------
 
 #ifndef _SHL_HXX
 #include <tools/shl.hxx>
