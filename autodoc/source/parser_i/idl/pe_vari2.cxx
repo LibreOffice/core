4c38eb67988c88feb9e71f0aea302f9b03674ffd
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 11:08:26 +0000

    INTEGRATION: CWS warnings01 (1.5.4); FILE MERGED 2005/10/18 08:36:21 np 1.5.4.1: #i53898#

--- autodoc/source/parser_i/idl/pe_vari2.cxx
+++ autodoc/source/parser_i/idl/pe_vari2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_vari2.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:50:54 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 12:08:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -114,7 +114,7 @@ PE_Variable::Process_Identifier( const TokIdentifier & i_rToken )
 }
 
 void
-PE_Variable::Process_Punctuation( const TokPunctuation & i_rToken )
+PE_Variable::Process_Punctuation( const TokPunctuation & )
 {
     if (eState == expect_finish)
     {
