c693fa880d628cda790c528f2205221d7aaffe20
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:53:01 +0000

    INTEGRATION: CWS warnings01 (1.2.4); FILE MERGED 2005/10/18 08:35:49 np 1.2.4.1: #i53898#

--- autodoc/source/ary/idl/it_sequence.cxx
+++ autodoc/source/ary/idl/it_sequence.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: it_sequence.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:55:04 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:53:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ Sequence::~Sequence()
 }
 
 void
-Sequence::do_Visit( Host & io_rHost ) const
+Sequence::do_Visit( Host & ) const
 {
     // yet unused.
 }
