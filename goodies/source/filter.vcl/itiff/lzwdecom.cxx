030f71a34d1294caa99b99b6cab810aa30a779d8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 20:49:17 +0000

    INTEGRATION: CWS warnings01 (1.3.26); FILE MERGED 2005/10/28 12:40:52 sj 1.3.26.1: #i55991# warning free code

--- goodies/source/filter.vcl/itiff/lzwdecom.cxx
+++ goodies/source/filter.vcl/itiff/lzwdecom.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lzwdecom.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:03:07 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 21:49:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,6 @@
  *
  ************************************************************************/
 
-#include <tools/new.hxx>
 #include "lzwdecom.hxx"
 
 LZWDecompressor::LZWDecompressor()
