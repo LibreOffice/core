0da1b1a2756cf58eb21d0e7f788b96b5b9f5d061
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:08:20 +0000

    INTEGRATION: CWS warnings01 (1.2.34); FILE MERGED 2005/11/02 14:57:48 cl 1.2.34.1: warning free code changes

--- xmloff/source/draw/XMLNumberStylesExport.hxx
+++ xmloff/source/draw/XMLNumberStylesExport.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLNumberStylesExport.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:41:19 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:08:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 class SdXMLExport;
