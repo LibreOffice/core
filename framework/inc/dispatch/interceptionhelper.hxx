c3dc14c881dfa671fdd27837473631f8a86c58c4
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:51:07 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/10/28 14:48:07 cd 1.4.32.1: #i55991# Warning free code changes for gcc

--- framework/inc/dispatch/interceptionhelper.hxx
+++ framework/inc/dispatch/interceptionhelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interceptionhelper.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:13:04 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:50:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -282,7 +282,7 @@ class InterceptionHelper : public  css::frame::XDispatchProvider
 
     public:
 
-        DECLARE_XINTERFACE
+        FWK_DECLARE_XINTERFACE
 
         //_________________________________________________
         // XDispatchProvider
--- framework/inc/dispatch/mailtodispatcher.hxx
+++ framework/inc/dispatch/mailtodispatcher.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mailtodispatcher.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:13:44 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:51:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -164,8 +164,8 @@ class MailToDispatcher : // interfaces
         virtual ~MailToDispatcher(                                                                        );
 
         // XInterface, XTypeProvider, XServiceInfo
-        DECLARE_XINTERFACE
-        DECLARE_XTYPEPROVIDER
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
         DECLARE_XSERVICEINFO
 
         // XDispatchProvider
