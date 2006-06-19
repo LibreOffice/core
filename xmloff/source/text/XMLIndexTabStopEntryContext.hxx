be6a47bf3e8fc658afee5d7b40ca4885a5c162e0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:41:49 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/11/02 14:57:57 cl 1.4.32.1: warning free code changes

--- xmloff/source/text/XMLIndexTabStopEntryContext.hxx
+++ xmloff/source/text/XMLIndexTabStopEntryContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexTabStopEntryContext.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:11:09 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:41:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
