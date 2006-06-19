9c5e4fe10dfd8b951fc19579a8ec56a00865331b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 15:15:19 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/04/20 14:49:56 cl 1.3.222.1: warning free code changes

--- svx/source/items/svxempty.cxx
+++ svx/source/items/svxempty.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxempty.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:39:51 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 16:15:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// include ---------------------------------------------------------------
-
-#pragma hdrstop
 
 #include "svxempty.hxx"
 
--- svx/source/items/svxerr.cxx
+++ svx/source/items/svxerr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxerr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:40:05 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 16:15:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,6 @@
 #ifndef _SHL_HXX
 #include <tools/shl.hxx>
 #endif
-#pragma hdrstop
 
 #include "svxerr.hxx"
 #include "dialmgr.hxx"
