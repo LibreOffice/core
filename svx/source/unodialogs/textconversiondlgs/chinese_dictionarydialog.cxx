cafa24bbb95af335342433666c39e0f54012c626
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 15:52:55 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/05/12 16:20:26 sb 1.3.222.1: #i53898# Made code warning-free and/or compile at all after resync to SRC680m162 (and in some cases reverted previous, problematic changes).

--- svx/source/unodialogs/textconversiondlgs/chinese_dictionarydialog.cxx
+++ svx/source/unodialogs/textconversiondlgs/chinese_dictionarydialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chinese_dictionarydialog.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:55:44 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 16:52:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -298,7 +298,7 @@ void DictionaryList::addEntry( const rtl::OUString& rTerm, const rtl::OUString& 
         return;
 
     DictionaryEntry* pEntry = new DictionaryEntry( rTerm, rMapping, nConversionPropertyType, sal_True );
-    SvLBoxEntry* pLBEntry = InsertEntry( makeTabString( *pEntry ), nPos );
+    SvLBoxEntry* pLBEntry = InsertEntryToColumn( makeTabString( *pEntry ), nPos );
     pLBEntry->SetUserData( pEntry );
     SelectRow( GetEntryPos( pLBEntry ) );
 }
