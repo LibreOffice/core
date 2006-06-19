72e48353d181b35ec57797f3fc51eded8429f7c6
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:06:36 +0000

    INTEGRATION: CWS warnings01 (1.2.36); FILE MERGED 2005/09/22 19:59:14 sb 1.2.36.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/19 15:24:03 sb 1.2.36.1: #i53898# Made code warning-free.

--- stoc/source/tdmanager/tdmgr_check.cxx
+++ stoc/source/tdmanager/tdmgr_check.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tdmgr_check.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:13:35 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:06:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -551,6 +551,9 @@ void check( Reference<reflection::XTypeDescription> const & xNewTD,
                             xNewTD, UNO_QUERY_THROW ),
                         Reference<reflection::XSingletonTypeDescription2>(
                             xExistingTD, UNO_QUERY_THROW ) );
+        break;
+
+    default:
         break;
     }
 }
