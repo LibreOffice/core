15a4bb8fb1d7e5634f4d64b8747c31e3ac8b75c7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 13:11:42 +0000

    INTEGRATION: CWS warnings01 (1.1.2); FILE ADDED 2005/10/14 14:41:30 sb 1.1.2.2: #i53898# Fixed typo. 2005/10/14 14:34:30 sb 1.1.2.1: #i53898# Deliver twain.h only on WNT.

---
+++ twain/util/makefile.mk
@@ -0,0 +1,47 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: hr $ $Date: 2006-06-19 14:11:42 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ := ..
+PRJNAME := twain
+TARGET := twain
+
+.INCLUDE: settings.mk
+.INCLUDE: target.mk
+
+ALLTAR: $(OUT)$/inc$/twain.h
+
+$(OUT)$/inc$/%: $(PRJ)$/inc$/%
+    - $(MKDIR) $(@:d)
+    + $(COPY) $< $@
