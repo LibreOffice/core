51f7793ee73244d52fc9f8f068db4bb811e4e2d8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:48:55 +0000

    INTEGRATION: CWS warnings01 (1.2.4); FILE MERGED 2005/10/18 08:35:40 np 1.2.4.1: #i53898#

--- autodoc/inc/ary/idl/ik_ce.hxx
+++ autodoc/inc/ary/idl/ik_ce.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ik_ce.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:14:03 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:48:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,8 +79,8 @@ struct attr
     static E_SightLevel SightLevel(
                             const CodeEntity &  i_ce );
     static bool         Search_Member(
-                            const CodeEntity &  i_ce,
-                            const String &      i_memberName )
+                            const CodeEntity &  ,
+                            const String &      )
                             { return true; }    // KORR_FUTURE
 };
 
