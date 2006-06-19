1eabd17448e5175d3542f4cfbd4dda14525ada1f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 15:22:25 +0000

    INTEGRATION: CWS warnings01 (1.2.222); FILE MERGED 2006/04/20 14:49:59 cl 1.2.222.1: warning free code changes

--- svx/source/outliner/outl_pch.cxx
+++ svx/source/outliner/outl_pch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outl_pch.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:56:31 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 16:22:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,5 +33,4 @@
  *
  ************************************************************************/
 #include <outl_pch.hxx>
-#pragma hdrstop
 
