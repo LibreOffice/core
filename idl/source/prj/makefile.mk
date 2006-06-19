df736f50d7c9980b3af8b54fabd041d0e09c47f8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:43:47 +0000

    INTEGRATION: CWS warnings01 (1.3.6); FILE MERGED 2005/10/27 16:03:19 sb 1.3.6.1: #i53898# Made code warning-free.

--- idl/source/prj/makefile.mk
+++ idl/source/prj/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 17:52:57 $
+#   last change: $Author: hr $ $Date: 2006-06-19 10:43:47 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,6 +41,7 @@ PROJECTPCHSOURCE=$(PRJ)$/util$/idlpch
 
 PRJNAME=SVIDL
 TARGET=prj
+ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------------
 
