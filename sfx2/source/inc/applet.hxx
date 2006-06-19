271c2e943ad8fee8d403ffe052a902662357d8cb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 21:32:18 +0000

    INTEGRATION: CWS warnings01 (1.4.64); FILE MERGED 2005/11/28 16:16:30 cd 1.4.64.1: #i55991# Remove warnings

--- sfx2/source/inc/applet.hxx
+++ sfx2/source/inc/applet.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: applet.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:54:50 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 22:32:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -124,5 +124,5 @@ public:
     SFX_DECL_XSERVICEINFO
 };
 
-};
+}
 #endif
