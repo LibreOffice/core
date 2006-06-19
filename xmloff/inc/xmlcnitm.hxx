5f77dcfa73ffec05ef7f006962694e5539f03fbb
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 16:58:11 +0000

    INTEGRATION: CWS warnings01 (1.3.32); FILE MERGED 2005/11/17 16:41:22 pl 1.3.32.1: #i55991# removed warnings

--- xmloff/inc/xmlcnitm.hxx
+++ xmloff/inc/xmlcnitm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlcnitm.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:13:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 17:58:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ public:
     virtual ~SvXMLAttrContainerItem();
 
     virtual int operator==( const SfxPoolItem& ) const;
+    using SfxPoolItem::Compare;
     virtual int Compare( const SfxPoolItem &rWith ) const;
 
     virtual SfxItemPresentation GetPresentation(
