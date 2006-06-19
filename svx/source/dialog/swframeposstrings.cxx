4c68048e29199d10adcbc1107b73f0208fcc0175
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 14:31:33 +0000

    INTEGRATION: CWS warnings01 (1.3.222); FILE MERGED 2006/04/20 14:49:45 cl 1.3.222.1: warning free code changes

--- svx/source/dialog/swframeposstrings.cxx
+++ svx/source/dialog/swframeposstrings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swframeposstrings.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:09:20 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 15:31:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#pragma hdrstop
 
 #ifndef _SVXSWFRAMEPOSSTRINGS_HXX
 #include <swframeposstrings.hxx>
