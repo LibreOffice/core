6713fadf9313830726caf9bf092d3f2970cfb9e6
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:40:08 +0000

    INTEGRATION: CWS warnings01 (1.3.32); FILE MERGED 2005/11/17 15:19:22 pl 1.3.32.1: #i55991# removed warnings

--- xmloff/source/text/XMLIndexBibliographySourceContext.cxx
+++ xmloff/source/text/XMLIndexBibliographySourceContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexBibliographySourceContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:03:58 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:40:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,8 +122,8 @@ XMLIndexBibliographySourceContext::~XMLIndexBibliographySourceContext()
 }
 
 void XMLIndexBibliographySourceContext::ProcessAttribute(
-    enum IndexSourceParamEnum eParam,
-    const OUString& rValue)
+    enum IndexSourceParamEnum,
+    const OUString&)
 {
     // We have no attributes. Who wants attributes, anyway?
 }
