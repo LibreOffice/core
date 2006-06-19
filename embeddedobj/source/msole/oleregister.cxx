853effe375e90c6ab9dbac0bea54da4548f3ce8d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 23:30:52 +0000

    INTEGRATION: CWS warnings01 (1.3.12); FILE MERGED 2005/11/16 11:40:59 pl 1.3.12.1: #i55991# removed warnings

--- embeddedobj/source/msole/oleregister.cxx
+++ embeddedobj/source/msole/oleregister.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oleregister.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:43:33 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 00:30:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,12 +53,12 @@ using namespace ::com::sun::star;
 
 extern "C" {
 
-void SAL_CALL component_getImplementationEnvironment( const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv )
+void SAL_CALL component_getImplementationEnvironment( const sal_Char ** ppEnvTypeName, uno_Environment ** /*ppEnv*/ )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
 
-void * SAL_CALL component_getFactory( const sal_Char * pImplName, void * pServiceManager, void * pRegistryKey )
+void * SAL_CALL component_getFactory( const sal_Char * pImplName, void * pServiceManager, void * /*pRegistryKey*/ )
 {
     void * pRet = 0;
 
@@ -95,7 +95,7 @@ void * SAL_CALL component_getFactory( const sal_Char * pImplName, void * pServic
     return pRet;
 }
 
-sal_Bool SAL_CALL component_writeInfo( void * pServiceManager, void * pRegistryKey )
+sal_Bool SAL_CALL component_writeInfo( void * /*pServiceManager*/, void * pRegistryKey )
 {
     if (pRegistryKey)
     {
