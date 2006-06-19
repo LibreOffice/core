8bad82fab8ee35fe00775afd74b0debfa2d2efd3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 10:18:11 +0000

    INTEGRATION: CWS warnings01 (1.4.32); FILE MERGED 2005/11/16 13:10:36 pl 1.4.32.1: #i55991# removed warnings

--- framework/source/helper/acceleratorinfo.cxx
+++ framework/source/helper/acceleratorinfo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acceleratorinfo.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:22:19 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:17:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -73,4 +73,4 @@ KeyCode SAL_CALL GetKeyCodeFromCommandURL( ::com::sun::star::uno::Reference< ::c
         return KeyCode();
 }
 
-};
+}
--- framework/source/helper/imageproducer.cxx
+++ framework/source/helper/imageproducer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imageproducer.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:23:24 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 11:18:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,5 +57,5 @@ Image SAL_CALL GetImageFromURL( ::com::sun::star::uno::Reference< ::com::sun::st
         return Image();
 }
 
-};
+}
 
