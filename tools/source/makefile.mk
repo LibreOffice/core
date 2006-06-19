b95cb9c60cca4be76681c9d2ec242e4aab9d4463
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:36:30 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/10/27 12:28:57 sb 1.2.8.1: #i53898# Made code warning-free.

--- tools/source/makefile.mk
+++ tools/source/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 14:09:14 $
+#   last change: $Author: hr $ $Date: 2006-06-19 13:36:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,6 @@ SUBDIRS+= \
     fsys \
     generic \
     intntl \
-    memmgr \
     memtools \
     misc \
     rc \
