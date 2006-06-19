83bef9c9045f85178bb0483c4b6b7854daaf2c8e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 12:07:33 +0000

    INTEGRATION: CWS warnings01 (1.3.14); FILE MERGED 2006/01/27 12:08:05 sb 1.3.14.2: #i53898# Made code warning-free. 2005/11/10 09:50:23 pl 1.3.14.1: #i53898# removed warnings

--- odk/source/unoapploader/unx/unoapploader.c
+++ odk/source/unoapploader/unx/unoapploader.c
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoapploader.c,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:19:58 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 13:07:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,6 +81,9 @@ int main( int argc, char *argv[] )
     char* envstr;
     char* cmdname;
     int size;
+
+    (void) argc; /* avoid warning about unused parameter */
+    (void) argv; /* avoid warning about unused parameter */
 
     /* get the path of the UNO installation */
     path = getPath();
