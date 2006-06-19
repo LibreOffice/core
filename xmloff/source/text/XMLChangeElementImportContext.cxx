e6bc3209a314d7b7a40b84dcba58f83e98afbe90
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 17:38:39 +0000

    INTEGRATION: CWS warnings01 (1.5.32); FILE MERGED 2005/11/17 15:19:21 pl 1.5.32.1: #i55991# removed warnings

--- xmloff/source/text/XMLChangeElementImportContext.cxx
+++ xmloff/source/text/XMLChangeElementImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLChangeElementImportContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:58:59 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 18:38:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -121,7 +121,7 @@ SvXMLImportContext* XMLChangeElementImportContext::CreateChildContext(
 }
 
 // #107848#
-void XMLChangeElementImportContext::StartElement( const Reference< XAttributeList >& xAttrList )
+void XMLChangeElementImportContext::StartElement( const Reference< XAttributeList >& )
 {
     if(bAcceptContent)
     {
