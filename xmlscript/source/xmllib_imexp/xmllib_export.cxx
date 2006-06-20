5c6c872335767fb59f94dc691e0c2c1ce6bbcf0f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:13:21 +0000

    INTEGRATION: CWS warnings01 (1.10.4); FILE MERGED 2005/10/27 15:29:54 dbo 1.10.4.1: #i53898# warning-free for CC

--- xmlscript/source/xmllib_imexp/xmllib_export.cxx
+++ xmlscript/source/xmllib_imexp/xmllib_export.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmllib_export.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:16:31 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:13:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -177,5 +177,5 @@ SAL_CALL exportLibrary(
     xOut->endDocument();
 }
 
-};
+}
 
