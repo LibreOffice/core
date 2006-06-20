f5340c51710b6f8f25fbfd089284975c19e1598b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:01:35 +0000

    INTEGRATION: CWS warnings01 (1.9.4); FILE MERGED 2006/03/09 20:31:45 pl 1.9.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/clipb/WinClipboard.hxx
+++ dtrans/source/win32/clipb/WinClipboard.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WinClipboard.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:15:28 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:01:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,8 +42,8 @@
 // includes
 //------------------------------------------------------------------------
 
-#ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _SAL_TYPES_H_
