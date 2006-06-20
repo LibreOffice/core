cc4988e5509b78e1893ffeac79ce651ac99efd75
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 01:15:04 +0000

    INTEGRATION: CWS warnings01 (1.2.8); FILE MERGED 2005/11/15 16:51:40 thb 1.2.8.1: #i55991# made code compile without warnings for gcc

--- canvas/source/null/null_canvasfont.cxx
+++ canvas/source/null/null_canvasfont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: null_canvasfont.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 12:48:35 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 02:15:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@ using namespace ::com::sun::star;
 namespace nullcanvas
 {
     CanvasFont::CanvasFont( const rendering::FontRequest&                   rFontRequest,
-                            const uno::Sequence< beans::PropertyValue >&    extraFontProperties,
+                            const uno::Sequence< beans::PropertyValue >&    /*extraFontProperties*/,
                             const geometry::Matrix2D&                       fontMatrix ) :
         CanvasFont_Base( m_aMutex ),
         maFontRequest( rFontRequest ),
