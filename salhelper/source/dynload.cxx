74eb8b869da9e8d41dbfba7d9c1b6cf9a5c4697c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 03:11:35 +0000

    INTEGRATION: CWS warnings01 (1.2.134); FILE MERGED 2005/09/23 00:30:01 sb 1.2.134.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/02 15:01:32 sb 1.2.134.1: #i53898# Made code warning-free.

--- salhelper/source/dynload.cxx
+++ salhelper/source/dynload.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dynload.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:01:02 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 04:11:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,8 @@ ORealDynamicLoader* ORealDynamicLoader::newInstance(ORealDynamicLoader ** ppSetT
         return NULL;
     }
 
-    initFunc = (ApiInitFunction)osl_getSymbol(pModule, initFunction.pData);
+    initFunc = (ApiInitFunction)osl_getFunctionSymbol(
+        pModule, initFunction.pData);
 
     if ( !initFunc )
     {
