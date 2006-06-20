c3cce585729ec754353b77d8d52fb62556119524
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:18:46 +0000

    INTEGRATION: CWS warnings01 (1.3.10); FILE MERGED 2006/03/10 15:40:11 sb 1.3.10.2: #i53898# Made code warning-free. 2005/11/09 21:01:46 sb 1.3.10.1: #i53898# Made code warning-free.

--- ucb/source/ucp/expand/ucpexpand.cxx
+++ ucb/source/ucp/expand/ucpexpand.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ucpexpand.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:21:45 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:18:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -249,6 +249,7 @@ sal_Int32 ExpandContentProviderImpl::compareContentIds(
     }
     catch (IllegalIdentifierException & exc)
     {
+        (void) exc; // unused
         OSL_ENSURE(
             0, ::rtl::OUStringToOString(
                 exc.Message, RTL_TEXTENCODING_UTF8 ).getStr() );
@@ -274,7 +275,7 @@ extern "C"
 {
 
 void SAL_CALL component_getImplementationEnvironment(
-    const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv )
+    const sal_Char ** ppEnvTypeName, uno_Environment ** )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
