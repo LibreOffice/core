c32277651d17bed675585965dacc583d288afc5b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:07:41 +0000

    INTEGRATION: CWS warnings01 (1.6.4); FILE MERGED 2006/03/09 20:32:00 pl 1.6.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/misc/ImplHelper.hxx
+++ dtrans/source/win32/misc/ImplHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImplHelper.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:28:23 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:07:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,11 +45,17 @@
 #include <sal/types.h>
 #endif
 
-#ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 
+#if defined _MSC_VER
+#pragma warning(push,1)
+#endif
 #include <windows.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 //------------------------------------------------------------------------
 // deklarations
