2329319d75c01319b2edf53683f48804c500016d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:00:38 +0000

    INTEGRATION: CWS warnings01 (1.3.4); FILE MERGED 2005/10/27 11:44:57 pl 1.3.4.1: #i55991# removed warnings for solaris platform

--- dtrans/source/generic/generic_clipboard.hxx
+++ dtrans/source/generic/generic_clipboard.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: generic_clipboard.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:12:42 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:00:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -141,7 +141,7 @@ namespace dtrans
 
     };
 
-};
+}
 
 // ------------------------------------------------------------------------
 
