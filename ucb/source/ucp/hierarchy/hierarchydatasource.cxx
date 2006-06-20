aad4f8be57d2d4e08d7e48302787c3ef537c2571
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:29:01 +0000

    INTEGRATION: CWS warnings01 (1.7.10); FILE MERGED 2005/11/10 18:10:13 pl 1.7.10.1: #i55991# removed warnings

--- ucb/source/ucp/hierarchy/hierarchydatasource.cxx
+++ ucb/source/ucp/hierarchy/hierarchydatasource.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hierarchydatasource.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:47:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:29:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -234,7 +234,7 @@ public:
         throw ( uno::RuntimeException );
 };
 
-}; // namespace hcp_impl
+} // namespace hcp_impl
 
 using namespace hcp_impl;
 
