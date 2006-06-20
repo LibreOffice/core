f0d2e87057f6d6f0e51e31b59c246cf6d99ad185
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:10:10 +0000

    INTEGRATION: CWS warnings01 (1.35.4); FILE MERGED 2005/11/01 17:10:15 cd 1.35.4.1: #i53898# Warning free code for sun sparc compiler

--- package/inc/ZipPackageFolder.hxx
+++ package/inc/ZipPackageFolder.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ZipPackageFolder.hxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:59:56 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:10:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,7 @@ namespace beans
 {
     struct PropertyValue;
 }
-} } };
+} } }
 class ZipFile;
 class ZipPackage;
 class ZipOutputStream;
