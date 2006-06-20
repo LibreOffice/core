c15df0bb2bb75dddf4e60c03e7c0e490f3cfaa07
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:00:55 +0000

    INTEGRATION: CWS warnings01 (1.22.30); FILE MERGED 2005/11/16 12:58:41 fs 1.22.30.1: #i57457# warning free code

--- connectivity/inc/connectivity/sdbcx/VCollection.hxx
+++ connectivity/inc/connectivity/sdbcx/VCollection.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VCollection.hxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:03:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:00:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
