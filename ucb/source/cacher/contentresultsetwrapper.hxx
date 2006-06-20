335648ea297514c42db56829068bdfa2ce4a44a3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:15:48 +0000

    INTEGRATION: CWS warnings01 (1.5.10); FILE MERGED 2005/11/09 21:01:35 sb 1.5.10.1: #i53898# Made code warning-free.

--- ucb/source/cacher/contentresultsetwrapper.hxx
+++ ucb/source/cacher/contentresultsetwrapper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: contentresultsetwrapper.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:13:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:15:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -134,7 +134,7 @@ protected:
         osl::Mutex* pT;
     public:
 
-        ReacquireableGuard(osl::Mutex * pT) : pT(pT)
+        ReacquireableGuard(osl::Mutex * t) : pT(t)
         {
             pT->acquire();
         }
