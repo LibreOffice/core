ea446ce4a0b9de434bf9b0d60ac1ab6bfec11044
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 02:15:28 +0000

    INTEGRATION: CWS warnings01 (1.19.4); FILE MERGED 2006/05/23 23:55:46 sb 1.19.4.2: RESYNC: (1.19-1.20); FILE MERGED 2006/03/24 15:36:17 fs 1.19.4.1: #i57457# warning-free code (unxlngi6/.pro + unxsoli4.pro)

--- dbaccess/source/ui/inc/WCopyTable.hxx
+++ dbaccess/source/ui/inc/WCopyTable.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WCopyTable.hxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2006-04-19 13:21:37 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 03:15:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -138,8 +138,27 @@ namespace dbaui
         friend class        OWizTypeSelectControl;
         friend class        OCopyTable;
         friend class        OWizNameMatching;
+
     public:
         DECLARE_STL_MAP(::rtl::OUString,::rtl::OUString,::comphelper::UStringMixLess,TNameMapping);
+
+        enum Wizard_Create_Style
+        {
+            WIZARD_APPEND_DATA = 0,
+            WIZARD_DEF_DATA,
+            WIZARD_DEF,
+            WIZARD_DEF_VIEW
+        };
+
+        enum Wizard_Button_Style
+        {
+            WIZARD_NEXT,
+            WIZARD_PREV,
+            WIZARD_FINISH,
+
+            WIZARD_NONE
+        };
+
     private:
         ODatabaseExport::TColumns       m_vDestColumns; // contains the columns
         ODatabaseExport::TColumnVector  m_aDestVec;     // the order to insert the columns
@@ -151,10 +170,42 @@ namespace dbaui
         PushButton              m_pbPrev;
         PushButton              m_pbNext;
         OKButton                m_pbFinish;
+
+        OTypeInfoMap                            m_aTypeInfo;
+        ::std::vector<OTypeInfoMap::iterator>   m_aTypeInfoIndex;
+        OTypeInfoMap                            m_aDestTypeInfo;
+        ::std::vector<OTypeInfoMap::iterator>   m_aDestTypeInfoIndex;
+        TNameMapping                            m_mNameMapping;
+
+        ODatabaseExport::TPositions             m_vColumnPos;
+        ::std::vector<sal_Int32>                m_vColumnTypes;
+
+        ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >       m_xDestObject; // can be a query or a table
+        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >         m_xConnection;  // dest conn
+
+        ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >       m_xSourceObject;
+        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >         m_xSourceConnection;    // source conn
+
+        ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameAccess>     m_xSourceColumns;       // container
+        ::com::sun::star::uno::Reference< ::com::sun::star::util::XNumberFormatter >    m_xFormatter;
+        ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory> m_xFactory;
+
         String                  m_sTypeNames;       // these type names are the ones out of the resource file
         sal_uInt32              m_nPageCount;
         sal_Bool                m_bDeleteSourceColumns;
 
+        ::com::sun::star::lang::Locale  m_aLocale;
+        ::rtl::OUString                 m_sName;    // for a table the name is composed
+        ::rtl::OUString                 m_sSourceName;
+        ::rtl::OUString                 m_aKeyName;
+        TOTypeInfoSP                    m_pTypeInfo; // default type
+        sal_Bool                        m_bAddPKFirstTime;
+        Wizard_Create_Style             m_eCreateStyle;
+        Wizard_Button_Style             m_ePressed;
+        sal_Bool                        m_bCreatePrimaryColumn;
+
+
+    private:
         DECL_LINK( ImplPrevHdl  , PushButton* );
         DECL_LINK( ImplNextHdl  , PushButton* );
         DECL_LINK( ImplOKHdl    , OKButton* );
@@ -170,52 +221,6 @@ namespace dbaui
         void appendKey(::com::sun::star::uno::Reference< ::com::sun::star::sdbcx::XKeysSupplier>& _rxSup,const ODatabaseExport::TColumnVector* _pVec);
         // checks if the type is supported in the destination database
         sal_Bool supportsType(sal_Int32 _nDataType,sal_Int32& _rNewDataType);
-
-    protected:
-        OTypeInfoMap                m_aTypeInfo;
-        ::std::vector<OTypeInfoMap::iterator> m_aTypeInfoIndex;
-        OTypeInfoMap                m_aDestTypeInfo;
-        ::std::vector<OTypeInfoMap::iterator> m_aDestTypeInfoIndex;
-        TNameMapping                m_mNameMapping;
-
-        ODatabaseExport::TPositions m_vColumnPos;
-        ::std::vector<sal_Int32>    m_vColumnTypes;
-
-        ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >       m_xDestObject; // can be a query or a table
-        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >         m_xConnection;  // dest conn
-
-        ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >       m_xSourceObject;
-        ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XConnection >         m_xSourceConnection;    // source conn
-
-        ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameAccess>     m_xSourceColumns;       // container
-        ::com::sun::star::uno::Reference< ::com::sun::star::util::XNumberFormatter >    m_xFormatter;
-        ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory> m_xFactory;
-        ::com::sun::star::lang::Locale  m_aLocale;
-        ::rtl::OUString                 m_sName;    // for a table the name is composed
-        ::rtl::OUString                 m_sSourceName;
-        ::rtl::OUString                 m_aKeyName;
-        TOTypeInfoSP                    m_pTypeInfo; // default type
-        sal_Bool                        m_bAddPKFirstTime;
-    public:
-        enum Wizard_Create_Style
-        {
-            WIZARD_APPEND_DATA = 0,
-            WIZARD_DEF_DATA,
-            WIZARD_DEF,
-            WIZARD_DEF_VIEW
-        };
-
-        enum Wizard_Button_Style
-        {
-            WIZARD_NEXT,
-            WIZARD_PREV,
-            WIZARD_FINISH
-        };
-
-    protected:
-        Wizard_Create_Style m_eCreateStyle;
-        Wizard_Button_Style m_ePressed;
-        sal_Bool            m_bCreatePrimaryColumn;
 
     public:
         // used for copy tables or queries
@@ -239,7 +244,7 @@ namespace dbaui
 
         virtual long        DeactivatePage();
         OKButton&           GetOKButton() { return m_pbFinish; }
-        sal_Bool            WasButtonPressed() const { return m_ePressed; }
+        Wizard_Button_Style GetPressedButton() const { return m_ePressed; }
         void                EnableButton(Wizard_Button_Style eStyle,sal_Bool bEnable);
         void                AddWizardPage(OWizardPage* pPage); // Page wird von OCopyTableWizard gelöscht
         void                RemoveWizardPage(OWizardPage* pPage); // Page goes again to user
@@ -251,7 +256,7 @@ namespace dbaui
         void                loadData();
 
         // returns a vector where the position of a column and if the column is in the selection
-        // when not the value is CONTAINER_ENTRY_NOTFOUND == (sal_uInt32)-1
+        // when not the value is COLUMN_POSITION_NOT_FOUND == (sal_uInt32)-1
         ODatabaseExport::TPositions GetColumnPositions()    const { return m_vColumnPos; }
         ::std::vector<sal_Int32>    GetColumnTypes()        const { return m_vColumnTypes; }
         const TNameMapping*         GetNameMapping()        const { return &m_mNameMapping; }
