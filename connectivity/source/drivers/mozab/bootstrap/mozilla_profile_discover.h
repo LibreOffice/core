e50418af88a7b60e49088cce3f3018192dcf93bf
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:49:49 +0000

    INTEGRATION: CWS warnings01 (1.1.2); FILE ADDED 2005/11/21 10:07:54 fs 1.1.2.2: #i57457# warning-free code on unx* 2005/11/07 14:48:11 fs 1.1.2.1: #i57457# warning-free code

---
+++ connectivity/source/drivers/mozab/bootstrap/mozilla_profile_discover.h
@@ -0,0 +1,54 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: mozilla_profile_discover.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:49:49 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "pre_include_mozilla.h"
+#if defined __GNUC__
+    #pragma GCC system_header
+#endif
+
+#include "nsCOMPtr.h"
+#include "nsISupports.h"
+#include "nsString.h"
+#if defined __SUNPRO_CC
+#pragma disable_warn
+    // somewhere in the files included directly or indirectly in nsString.h, warnings are enabled, again
+#endif
+#include "nsIRegistry.h"
+#include "nsXPIDLString.h"
+#include "nsVoidArray.h"
+#include "nsIFile.h"
+#include "nsILocalFile.h"
+
+#include "post_include_mozilla.h"
