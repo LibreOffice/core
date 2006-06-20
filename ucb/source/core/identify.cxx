1bd667d2458d892db1538ddfb5bafd2ceb450972
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:16:12 +0000

    INTEGRATION: CWS warnings01 (1.3.10); FILE MERGED 2005/11/10 15:32:53 pl 1.3.10.1: #i55991# removed warnings

--- ucb/source/core/identify.cxx
+++ ucb/source/core/identify.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: identify.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:14:12 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:16:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,8 +38,6 @@
  **************************************************************************
 
  *************************************************************************/
-
-#pragma hdrstop
 
 #include "identify.hxx"
 
