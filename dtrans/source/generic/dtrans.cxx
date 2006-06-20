73d85972ed7d30222ebd58b2135f61b128cad55f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:00:14 +0000

    INTEGRATION: CWS warnings01 (1.4.4); FILE MERGED 2005/10/27 11:44:56 pl 1.4.4.1: #i55991# removed warnings for solaris platform

--- dtrans/source/generic/dtrans.cxx
+++ dtrans/source/generic/dtrans.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dtrans.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:12:12 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:00:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,14 +50,14 @@ extern "C"
 //==================================================================================================
 
 void SAL_CALL component_getImplementationEnvironment(const sal_Char ** ppEnvTypeName,
-                                                     uno_Environment ** ppEnv )
+                                                     uno_Environment ** /*ppEnv*/ )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
 
 //==================================================================================================
 
-sal_Bool SAL_CALL component_writeInfo(void * pServiceManager, void * pRegistryKey )
+sal_Bool SAL_CALL component_writeInfo(void * /*pServiceManager*/, void * pRegistryKey )
 {
     if (pRegistryKey)
     {
@@ -97,7 +97,7 @@ sal_Bool SAL_CALL component_writeInfo(void * pServiceManager, void * pRegistryKe
 void * SAL_CALL component_getFactory(
     const sal_Char * pImplName,
     void * pServiceManager,
-    void * pRegistryKey
+    void * /*pRegistryKey*/
 )
 {
     void * pRet = 0;
