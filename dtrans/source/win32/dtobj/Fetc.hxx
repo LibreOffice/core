8b96856585a0978262b90d561e586d90474abbd4
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:04:59 +0000

    INTEGRATION: CWS warnings01 (1.5.4); FILE MERGED 2006/03/09 20:31:53 pl 1.5.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/dtobj/Fetc.hxx
+++ dtrans/source/win32/dtobj/Fetc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Fetc.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:23:54 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:04:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,13 @@
 #include <sal/types.h>
 #endif
 
+#if defined _MSC_VER
+#pragma warning(push,1)
+#endif
 #include <windows.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 /**********************************************************************
  stl container elements must fulfill the following requirements:
