7e829ed355d15e9b22cff0eeaf67eafda463da23
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:49:58 +0000

    INTEGRATION: CWS warnings01 (1.1.2); FILE ADDED 2005/11/21 10:07:54 fs 1.1.2.2: #i57457# warning-free code on unx* 2005/11/07 14:48:28 fs 1.1.2.1: #i57457# warning-free code

---
+++ connectivity/source/drivers/mozab/bootstrap/mozilla_profilemanager.h
@@ -0,0 +1,92 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: mozilla_profilemanager.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:49:58 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "pre_include_mozilla.h"
+#if defined __GNUC__
+    #pragma GCC system_header
+#endif
+
+#include "nsCOMPtr.h"
+#include "nsISupports.h"
+#include "nsString.h"
+#if defined __SUNPRO_CC
+#pragma disable_warn
+    // somewhere in the files included directly or indirectly in nsString.h, warnings are enabled, again
+#endif
+#include "nsIRegistry.h"
+#include "nsXPIDLString.h"
+#include "nsVoidArray.h"
+#include "nsIFile.h"
+#include "nsILocalFile.h"
+
+#include "pratom.h"
+#include "prmem.h"
+#include "plstr.h"
+#include "prenv.h"
+
+#include "nsIEnumerator.h"
+#include "prprf.h"
+#include "nsCOMPtr.h"
+#include "nsIComponentManager.h"
+#include "nsEscape.h"
+#include "nsDirectoryServiceDefs.h"
+#include "nsAppDirectoryServiceDefs.h"
+#include "nsILocalFile.h"
+#include "nsReadableUtils.h"
+
+
+#if defined(XP_MAC) || defined(XP_MACOSX)
+#include <Processes.h>
+#include <CFBundle.h>
+#include "nsILocalFileMac.h"
+#endif
+
+#ifdef XP_UNIX
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include "prnetdb.h"
+#include "prsystem.h"
+#endif
+
+#ifdef VMS
+#include <rmsdef.h>
+#endif
+
+#include "nsIPlatformCharset.h"
+#include <MNSInit.hxx>
+
+#include "post_include_mozilla.h"
