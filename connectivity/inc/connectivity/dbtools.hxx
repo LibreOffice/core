db12fde933867b2410f6b479071d10a5198ba9c8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:00:11 +0000

    INTEGRATION: CWS warnings01 (1.29.18); FILE MERGED 2005/11/16 12:58:40 fs 1.29.18.1: #i57457# warning free code

--- connectivity/inc/connectivity/dbtools.hxx
+++ connectivity/inc/connectivity/dbtools.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbtools.hxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 11:35:36 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:00:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
