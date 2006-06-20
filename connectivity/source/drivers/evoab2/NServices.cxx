3ca586731ebcbe9b8776cbd2f5f1067a5405ce9e
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:24:54 +0000

    INTEGRATION: CWS warnings01 (1.3.30); FILE MERGED 2005/12/22 11:44:42 fs 1.3.30.2: #i57457# warning-free code 2005/11/16 12:59:00 fs 1.3.30.1: #i57457# warning free code

--- connectivity/source/drivers/evoab2/NServices.cxx
+++ connectivity/source/drivers/evoab2/NServices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: NServices.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:53:11 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:24:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -128,7 +128,7 @@ struct ProviderRequest
 
 extern "C" void SAL_CALL component_getImplementationEnvironment(
                 const sal_Char  **ppEnvTypeName,
-                uno_Environment **ppEnv
+                uno_Environment ** /*ppEnv*/
             )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
@@ -136,7 +136,7 @@ extern "C" void SAL_CALL component_getImplementationEnvironment(
 
 //---------------------------------------------------------------------------------------
 extern "C" sal_Bool SAL_CALL component_writeInfo(
-                void* pServiceManager,
+                void* /*pServiceManager*/,
                 void* pRegistryKey
             )
 {
@@ -163,7 +163,7 @@ extern "C" sal_Bool SAL_CALL component_writeInfo(
 extern "C" void* SAL_CALL component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
-                    void* pRegistryKey)
+                    void* /*pRegistryKey*/)
 {
     void* pRet = 0;
     if (pServiceManager)
