27568f5151ba14c39b6ec00706f6b7d8451277d6
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 02:42:38 +0000

    INTEGRATION: CWS warnings01 (1.3.18); FILE MERGED 2006/01/31 13:35:37 sb 1.3.18.1: #i53898# Made code warning-free.

--- setup_native/source/win32/customactions/shellextensions/upgrade.cxx
+++ setup_native/source/win32/customactions/shellextensions/upgrade.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: upgrade.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:41:24 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 03:42:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,9 +36,12 @@
 #undef UNICODE
 #undef _UNICODE
 
+#pragma warning(push, 1) /* disable warnings within system headers */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <msiquery.h>
+#pragma warning(pop)
+
 #include <malloc.h>
 #include <assert.h>
 
