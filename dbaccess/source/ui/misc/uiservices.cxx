9241e666c35d18b90834cb3cd32389df37bae43f
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 02:24:41 +0000

    INTEGRATION: CWS warnings01 (1.19.50); FILE MERGED 2006/03/24 15:36:24 fs 1.19.50.1: #i57457# warning-free code (unxlngi6/.pro + unxsoli4.pro)

--- dbaccess/source/ui/misc/uiservices.cxx
+++ dbaccess/source/ui/misc/uiservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uiservices.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:17:28 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 03:24:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -118,7 +118,7 @@ extern "C" void SAL_CALL createRegistryInfo_DBU()
 
 extern "C" void SAL_CALL component_getImplementationEnvironment(
                 const sal_Char  **ppEnvTypeName,
-                uno_Environment **ppEnv
+                uno_Environment **/*ppEnv*/
             )
 {
     createRegistryInfo_DBU();
@@ -151,7 +151,7 @@ extern "C" sal_Bool SAL_CALL component_writeInfo(
 extern "C" void* SAL_CALL component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
-                    void* pRegistryKey)
+                    void* /*pRegistryKey*/)
 {
     Reference< XInterface > xRet;
     if (pServiceManager && pImplementationName)
