a892d04c04e903ff23c3ce75db18f45113fdae6b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:07:39 +0000

    INTEGRATION: CWS warnings01 (1.1.142); FILE MERGED 2005/09/23 02:12:11 sb 1.1.142.2: RESYNC: (1.1-1.2); FILE MERGED 2005/08/30 08:40:11 sb 1.1.142.1: #i53898# Made code warning-free.

--- soltools/giparser/gi_list.cxx
+++ soltools/giparser/gi_list.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gi_list.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:24:08 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:07:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -165,8 +165,6 @@ List_GenericInfo::InsertInfoByPath( GenericInfo *       let_dpInfo,
          delete let_dpInfo;
         return false;
     }
-
-    return true;
 }
 
 GenericInfo *
