4417add9ae1adaaf9066d0790f5d024891953305
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:24:19 +0000

    INTEGRATION: CWS warnings01 (1.3.30); FILE MERGED 2006/05/23 23:32:47 sb 1.3.30.2: RESYNC: (1.3-1.4); FILE MERGED 2005/11/16 12:58:59 fs 1.3.30.1: #i57457# warning free code

--- connectivity/source/drivers/evoab/makefile.mk
+++ connectivity/source/drivers/evoab/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2006-04-07 13:10:36 $
+#   last change: $Author: hr $ $Date: 2006-06-20 01:24:19 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -67,7 +67,6 @@ EXCEPTIONSFILES=\
 
 SLOFILES=\
     $(EXCEPTIONSFILES)				\
-    $(SLO)$/LNoException.obj		\
     $(SLO)$/LDebug.obj
 
 
