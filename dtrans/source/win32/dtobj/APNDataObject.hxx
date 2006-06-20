1047bb6262d0c1263393dd306671362e0aecc0f0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:03:43 +0000

    INTEGRATION: CWS warnings01 (1.4.4); FILE MERGED 2006/03/09 20:31:50 pl 1.4.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/dtobj/APNDataObject.hxx
+++ dtrans/source/win32/dtobj/APNDataObject.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: APNDataObject.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:21:23 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:03:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,7 +54,7 @@ class CAPNDataObject : public IDataObject
 {
 public:
     CAPNDataObject( IDataObjectPtr rIDataObject );
-    ~CAPNDataObject( );
+    virtual ~CAPNDataObject( );
 
     //-----------------------------------------------------------------
     //IUnknown interface methods
