2275bf96736338362c0c314abf015e693ed849a7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:04:28 +0000

    INTEGRATION: CWS warnings01 (1.3.30); FILE MERGED 2005/12/22 11:44:03 fs 1.3.30.1: #i57457# warning-free code

--- connectivity/source/commontools/TPrivilegesResultSet.cxx
+++ connectivity/source/commontools/TPrivilegesResultSet.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TPrivilegesResultSet.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:12:41 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:04:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -150,7 +150,8 @@ sal_Bool SAL_CALL OResultSetPrivileges::next(  ) throw(SQLException, RuntimeExce
                 return sal_False;
         }
 
-        if ( !(bReturn = ODatabaseMetaDataResultSet::next()) )
+        bReturn = ODatabaseMetaDataResultSet::next();
+        if ( !bReturn )
         {
             m_bBOF = sal_False;
             ODatabaseMetaDataResultSet::next();
