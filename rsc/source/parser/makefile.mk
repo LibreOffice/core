c129751a61ccf29d76ea13241cdd7a553e888566
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:45:36 +0000

    INTEGRATION: CWS warnings01 (1.5.8); FILE MERGED 2005/10/27 13:55:55 pl 1.5.8.1: #i55991# removed warnings for solaris platform

--- rsc/source/parser/makefile.mk
+++ rsc/source/parser/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 13:36:35 $
+#   last change: $Author: hr $ $Date: 2006-06-20 05:45:36 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,9 @@ TARGET=rscpar
 # --- Settings -----------------------------------------------------
 
 #prjpch=T
+
+ENABLE_EXCEPTIONS=true
+
 .INCLUDE :  settings.mk
 
 # --- Files --------------------------------------------------------
