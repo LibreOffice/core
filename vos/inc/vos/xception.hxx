3868b550146a0f7f6759467747c33b8267515d94
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 03:06:05 +0000

    INTEGRATION: CWS warnings01 (1.2.28); FILE MERGED 2005/11/09 12:16:36 pl 1.2.28.3: #i53898# removed warnings 2005/09/23 00:16:40 sb 1.2.28.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/07 16:34:14 sb 1.2.28.1: #i53898# Made code warning-free.

--- vos/inc/vos/xception.hxx
+++ vos/inc/vos/xception.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xception.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2006-06-02 12:41:07 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 04:06:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,9 +77,9 @@ class OException : public OObject
 public:
     virtual ~OException();
     OException() {}
-    OException( const OException &aPattern ) {}
+    OException( const OException & ) : OObject() {}
 
-    OException& SAL_CALL operator=(const OException& excp)
+    OException& SAL_CALL operator=(const OException&)
         { return *this; }
 };
 
