4d4e05b0cc18c18000c356d02c30313512056036
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 02:19:01 +0000

    INTEGRATION: CWS warnings01 (1.3.50); FILE MERGED 2006/03/24 15:36:20 fs 1.3.50.1: #i57457# warning-free code (unxlngi6/.pro + unxsoli4.pro)

--- dbaccess/source/ui/inc/statusbarontroller.hxx
+++ dbaccess/source/ui/inc/statusbarontroller.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: statusbarontroller.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:03:59 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 03:19:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ namespace dbaui
         public:
             OStatusbarController(const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB):m_xORB(_rxORB){}
 
-            DECLARE_XINTERFACE();
+            DECLARE_XINTERFACE()
             // ::com::sun::star::lang::XServiceInfo
             DECLARE_SERVICE_INFO_STATIC();
     };
