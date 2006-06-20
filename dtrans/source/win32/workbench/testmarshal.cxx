eb96a785b6103aa74d35e5b4158ac5b8bb5f6ad3
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:08:38 +0000

    INTEGRATION: CWS warnings01 (1.5.4); FILE MERGED 2006/03/09 20:32:04 pl 1.5.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/workbench/testmarshal.cxx
+++ dtrans/source/win32/workbench/testmarshal.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testmarshal.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:30:51 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:08:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,8 +42,8 @@
 //  other includes
 //_________________________________________________________________________________________________________________________
 
-#ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _SAL_TYPES_H_
@@ -55,8 +55,14 @@
 #endif
 
 #include <stdio.h>
+#if defined _MSC_VER
+#pragma warning(push,1)
+#endif
 #include <windows.h>
 #include <objbase.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 #include <memory>
 
