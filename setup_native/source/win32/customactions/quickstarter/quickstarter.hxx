5c190080d77f5e07c8c349bef8fde78422c61619
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 02:37:15 +0000

    INTEGRATION: CWS warnings01 (1.3.26); FILE MERGED 2006/01/31 13:35:18 sb 1.3.26.1: #i53898# Made code warning-free.

--- setup_native/source/win32/customactions/quickstarter/quickstarter.hxx
+++ setup_native/source/win32/customactions/quickstarter/quickstarter.hxx
@@ -1,8 +1,11 @@
 #pragma once
 
+#pragma warning(push, 1) /* disable warnings within system headers */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <msiquery.h>
+#pragma warning(pop)
+
 #include <string>
 
 std::string GetOfficeInstallationPath(MSIHANDLE handle);
