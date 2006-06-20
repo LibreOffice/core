eae91285d0ce4fdf8011324ee05390f1d8700494
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 01:41:20 +0000

    INTEGRATION: CWS warnings01 (1.9.50); FILE MERGED 2006/05/23 23:43:52 sb 1.9.50.2: RESYNC: (1.9-1.10); FILE MERGED 2006/03/24 15:35:49 fs 1.9.50.1: #i57457# warning-free code (unxlngi6/.pro + unxsoli4.pro)

--- dbaccess/source/core/api/resultset.hxx
+++ dbaccess/source/core/api/resultset.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: resultset.hxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-04 08:36:54 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 02:41:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -277,6 +277,9 @@ namespace dbaccess
     protected:
         void checkReadOnly() const;
         void checkBookmarkable() const;
+
+    private:
+        using ::cppu::OPropertySetHelper::getFastPropertyValue;
     };
 }
 #endif // _DBA_COREAPI_RESULTSET_HXX_
