6dce51fb3ef60e1093881ff4d77dbea6d1254ed0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:59:20 +0000

    INTEGRATION: CWS warnings01 (1.5.4); FILE MERGED 2006/03/09 20:31:41 pl 1.5.4.2: #i55991# removed warnings for windows platform 2005/10/21 14:14:53 pl 1.5.4.1: #i55991# removed warnings for linux platform

--- dtrans/source/cnttype/mcnttype.hxx
+++ dtrans/source/cnttype/mcnttype.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mcnttype.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:10:44 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:59:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,8 +45,8 @@
 #include <cppuhelper/compbase1.hxx>
 #endif
 
-#ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
 #endif
 
 #ifndef _SAL_TYPES_H_
