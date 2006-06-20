2fca6c043e9d211e8b7a570a1e5d81de4d294a3a
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:51:42 +0000

    INTEGRATION: CWS warnings01 (1.2.10); FILE MERGED 2005/10/21 16:24:49 pl 1.2.10.1: #i55991# removed warnings for linux platform

--- sot/inc/agg.hxx
+++ sot/inc/agg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: agg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:33:55 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:51:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,17 +55,17 @@ struct SvAggregate
     BOOL    bMainObj; // TRUE, das Objekt, welches das casting steuert
 
     SvAggregate()
-        : bFactory( FALSE )
-        , bMainObj( FALSE )
-        , pFact( NULL ) {}
+        : pFact( NULL )
+        , bFactory( FALSE )
+        , bMainObj( FALSE ) {}
     SvAggregate( SotObject * pObjP, BOOL bMainP )
-        : bFactory( FALSE )
-        , bMainObj( bMainP )
-        , pObj( pObjP ) {}
+        : pObj( pObjP )
+        , bFactory( FALSE )
+        , bMainObj( bMainP ) {}
     SvAggregate( SotFactory * pFactP )
-        : bFactory( TRUE )
-        , bMainObj( FALSE )
-        , pFact( pFactP ) {}
+        : pFact( pFactP )
+        , bFactory( TRUE )
+        , bMainObj( FALSE ) {}
 };
 
 /************** class SvAggregateMemberList *****************************/
