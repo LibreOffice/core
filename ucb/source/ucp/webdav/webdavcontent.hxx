c8af1a94342e3b809176e34ed45585ffee2cf155
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:38:19 +0000

    INTEGRATION: CWS warnings01 (1.19.10); FILE MERGED 2005/11/10 18:10:31 pl 1.19.10.1: #i55991# removed warnings

--- ucb/source/ucp/webdav/webdavcontent.hxx
+++ ucb/source/ucp/webdav/webdavcontent.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: webdavcontent.hxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:15:42 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:38:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -299,6 +299,6 @@ public:
                           const ::rtl::OUString& rContentId );
 };
 
-};
+}
 
 #endif
