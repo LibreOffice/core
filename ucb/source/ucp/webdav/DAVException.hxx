5db92a9b94bf726cb66b3d4975158618823261a0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:33:18 +0000

    INTEGRATION: CWS warnings01 (1.7.10); FILE MERGED 2005/11/10 18:10:16 pl 1.7.10.1: #i55991# removed warnings

--- ucb/source/ucp/webdav/DAVException.hxx
+++ ucb/source/ucp/webdav/DAVException.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DAVException.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:05:59 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:33:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -151,6 +151,6 @@ class DAVException
     sal_uInt16 getStatus() const { return mStatusCode; }
 };
 
-}; // namespace webdav_ucp
+} // namespace webdav_ucp
 
 #endif // _DAVEXCEPTION_HXX_
