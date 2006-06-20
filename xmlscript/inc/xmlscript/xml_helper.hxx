57ca7e1bf21f5004f6715cdec86f1352c8a9e51a
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:10:32 +0000

    INTEGRATION: CWS warnings01 (1.10.4); FILE MERGED 2005/10/27 15:29:48 dbo 1.10.4.1: #i53898# warning-free for CC

--- xmlscript/inc/xmlscript/xml_helper.hxx
+++ xmlscript/inc/xmlscript/xml_helper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xml_helper.hxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:08:35 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:10:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -162,6 +162,6 @@ SAL_CALL createOutputStream(
     ::rtl::ByteSequence * pOutData )
     SAL_THROW( () );
 
-};
+}
 
 #endif
