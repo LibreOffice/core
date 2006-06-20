35a596b4a7f8d51617bcb98a8af40164f830eec2
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 05:01:46 +0000

    INTEGRATION: CWS warnings01 (1.3.4); FILE MERGED 2006/03/09 20:31:45 pl 1.3.4.1: #i55991# removed warnings for windows platform

--- dtrans/source/win32/clipb/wcbentry.cxx
+++ dtrans/source/win32/clipb/wcbentry.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wcbentry.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:16:06 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 06:01:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -109,7 +109,7 @@ extern "C"
 //----------------------------------------------------------------------
 
 void SAL_CALL component_getImplementationEnvironment(
-    const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv )
+    const sal_Char ** ppEnvTypeName, uno_Environment ** /*ppEnv*/ )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
@@ -121,7 +121,7 @@ void SAL_CALL component_getImplementationEnvironment(
 // (you must use the full services name e.g. com.sun.star.frame.FilePicker
 //-------------------------------------------------------------------------
 
-sal_Bool SAL_CALL component_writeInfo( void* pServiceManager, void* pRegistryKey )
+sal_Bool SAL_CALL component_writeInfo( void* /*pServiceManager*/, void* pRegistryKey )
 {
     sal_Bool bRetVal = sal_False;
 
@@ -148,7 +148,7 @@ sal_Bool SAL_CALL component_writeInfo( void* pServiceManager, void* pRegistryKey
 // returns a factory to create XFilePicker-Services
 //----------------------------------------------------------------------
 
-void* SAL_CALL component_getFactory( const sal_Char* pImplName, uno_Interface* pSrvManager, uno_Interface* pRegistryKey )
+void* SAL_CALL component_getFactory( const sal_Char* pImplName, uno_Interface* pSrvManager, uno_Interface* /*pRegistryKey*/ )
 {
     void* pRet = 0;
 
