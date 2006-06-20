c0a78681b5b0d45b868c7a4cb890405fa96cc410
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 03:20:45 +0000

    INTEGRATION: CWS warnings01 (1.2.442); FILE MERGED 2005/09/23 00:58:57 sb 1.2.442.2: RESYNC: (1.2-1.3); FILE MERGED 2005/09/20 12:57:19 sb 1.2.442.1: #i53898# Globally disable problematic warnings.

--- sal/osl/w32/libutil.c
+++ sal/osl/w32/libutil.c
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: libutil.c,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:06:59 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 04:20:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#pragma warning(push,1) /* disable warnings within system headers */
 #include <windows.h>
+#pragma warning(pop)
 
 static BOOL  g_bInit = FALSE;
 static DWORD g_dwPlatformId = VER_PLATFORM_WIN32_WINDOWS;
