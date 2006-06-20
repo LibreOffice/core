3897248e42a4001594fab1aadf899625aa3f1c18
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:07:51 +0000

    INTEGRATION: CWS warnings01 (1.1.142); FILE MERGED 2005/09/23 02:13:02 sb 1.1.142.2: RESYNC: (1.1-1.2); FILE MERGED 2005/08/29 13:36:17 sb 1.1.142.1: #i53898# Made code warning-free.

--- soltools/inc/gen_info.hxx
+++ soltools/inc/gen_info.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gen_info.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:25:12 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:07:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -75,10 +75,10 @@ class GenericInfo
     const Simstr &      Comment() const         { return sComment; }
     bool                HasSubList() const      { return dpSubList != 0; }
 
-    virtual const List_GenericInfo &
+    const List_GenericInfo &
                         SubList() const         { return HasSubList() ? *dpSubList : CreateMyList(); }
     // ACCESS
-    virtual List_GenericInfo &
+    List_GenericInfo &
                         SubList()               { return HasSubList() ? *dpSubList : CreateMyList(); }
 
   private:
