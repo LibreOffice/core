a61f936265f244d56a53da587cded4f2fefed395
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:03:52 +0000

    INTEGRATION: CWS warnings01 (1.3.30); FILE MERGED 2005/11/16 12:58:44 fs 1.3.30.1: #i57457# warning free code

--- connectivity/source/commontools/TDatabaseMetaDataBase.cxx
+++ connectivity/source/commontools/TDatabaseMetaDataBase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TDatabaseMetaDataBase.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:10:47 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:03:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -67,7 +67,7 @@ ODatabaseMetaDataBase::~ODatabaseMetaDataBase()
 {
 }
 // -----------------------------------------------------------------------------
-void SAL_CALL ODatabaseMetaDataBase::disposing( const EventObject& Source ) throw(RuntimeException)
+void SAL_CALL ODatabaseMetaDataBase::disposing( const EventObject& /*Source*/ ) throw(RuntimeException)
 {
     // cut off all references to the connection
     m_xConnection       = NULL;
