b52591d774f22f4684ef5eaaaf27a461250b733b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:22:48 +0000

    INTEGRATION: CWS warnings01 (1.1.2); FILE ADDED 2006/03/10 15:41:07 sb 1.1.2.1: #i53898# Made code warning-free.

---
+++ ucb/source/ucp/ftp/curl.hxx
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: curl.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:22:48 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_UCB_SOURCE_UCP_FTP_CURL_HXX
+#define INCLUDED_UCB_SOURCE_UCP_FTP_CURL_HXX
+
+#if defined _MSC_VER
+#pragma warning(push, 1)
+#endif
+
+#include "curl/curl.h"
+
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
+
+#endif
