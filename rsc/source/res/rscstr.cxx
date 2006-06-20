6a184c35bb4a24969afcf2e6ab574e1122fc9a62
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 04:48:25 +0000

    INTEGRATION: CWS warnings01 (1.4.8); FILE MERGED 2005/10/27 13:55:59 pl 1.4.8.1: #i55991# removed warnings for solaris platform

--- rsc/source/res/rscstr.cxx
+++ rsc/source/res/rscstr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rscstr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 13:56:52 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 05:48:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -403,7 +403,7 @@ ERRTYPE RscString::WriteRc( const RSCINST & rInst, RscWriteRc & rMem,
 void RscString::WriteRcAccess
 (
     FILE * fOutput,
-    RscTypCont * pTC,
+    RscTypCont * /*pTC*/,
     const char * pName
 )
 {
