077207733b289faf92c5e38e9fa092e3621ab93c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 03:13:29 +0000

    INTEGRATION: CWS warnings01 (1.7.184); FILE MERGED 2005/09/23 00:37:37 sb 1.7.184.2: RESYNC: (1.7-1.8); FILE MERGED 2005/09/14 15:20:28 sb 1.7.184.1: #i53898# Changed OSL_INVALID_PORT into a negative value.

--- sal/inc/osl/socket.h
+++ sal/inc/osl/socket.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: socket.h,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:33:00 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 04:13:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ extern "C" {
 
 /* error returns */
 #define OSL_INADDR_NONE             0xffffffff
-#define OSL_INVALID_PORT            0xffffffff
+#define OSL_INVALID_PORT (-1)
 #define OSL_INVALID_IPX_SOCKET_NO   0xffffffff
 
 /**@HTML
