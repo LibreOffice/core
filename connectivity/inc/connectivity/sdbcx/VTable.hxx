e0de3320d961f6e5ec4bca0d7f1193560bb2d7f2
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-20 00:02:03 +0000

    INTEGRATION: CWS warnings01 (1.12.30); FILE MERGED 2006/06/14 10:54:18 fs 1.12.30.2: #i66367# reverted previous changes related to replacing IdPropertyArrayHelper with PropertyArrayHelper - there's a subtle difference between both ids ... 2005/11/16 12:58:42 fs 1.12.30.1: #i57457# warning free code

--- connectivity/inc/connectivity/sdbcx/VTable.hxx
+++ connectivity/inc/connectivity/sdbcx/VTable.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VTable.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:05:34 $
+ *  last change: $Author: hr $ $Date: 2006-06-20 01:02:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
