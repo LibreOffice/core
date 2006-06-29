b4b24e67c7a7375500e71b007f1b27782035f053
Ivo Hinkelmann <ihi@openoffice.org>
2006-06-29 10:16:10 +0000

    INTEGRATION: CWS ause036 (1.2.82); FILE MERGED 2005/09/13 11:00:41 hjs 1.2.82.2: RESYNC: (1.2-1.3); FILE MERGED 2005/08/18 15:18:50 hjs 1.2.82.1: #i53507# cleanup

--- setup_native/source/ulfconv/makefile.mk
+++ setup_native/source/ulfconv/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 16:30:11 $
+#   last change: $Author: ihi $ $Date: 2006-06-29 11:16:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,6 +46,8 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :  settings.mk
 
 # --- Files --------------------------------------------------------
+
+SLOFILES =	$(SLO)$/$(TARGET).obj
 
 APP1TARGET=$(TARGET)
 APP1OBJS=$(OBJ)$/$(TARGET).obj
