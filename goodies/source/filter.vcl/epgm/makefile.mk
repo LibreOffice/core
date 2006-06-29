5e476135e3beec7b431672cf44d356bf659a52c5
Ivo Hinkelmann <ihi@openoffice.org>
2006-06-29 10:29:43 +0000

    INTEGRATION: CWS ause036 (1.8.34); FILE MERGED 2005/11/15 15:21:01 hjs 1.8.34.1: #i50255# remove obsolete/wronge dependencies

--- goodies/source/filter.vcl/epgm/makefile.mk
+++ goodies/source/filter.vcl/epgm/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 02:38:50 $
+#   last change: $Author: ihi $ $Date: 2006-06-29 11:29:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -67,7 +67,6 @@ SHL1TARGET= 	epg$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB= 	epgm
 SHL1STDLIBS=	$(TOOLSLIB) $(SVLIB) $(CPPULIB) $(SVTOOLLIB) $(SALLIB)
 
-SHL1DEPN=		$(LB)$/epgm.lib
 SHL1LIBS=		$(SLB)$/epgm.lib
 
 .IF "$(GUI)" != "UNX"
