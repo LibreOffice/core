29a86a8a1c0d90aef08c4db72c783d425c79796a
Kurt Zenker <kz@openoffice.org>
2006-07-05 21:00:41 +0000

    INTEGRATION: CWS ause058 (1.9.294); FILE MERGED 2006/06/26 12:29:06 hjs 1.9.294.2: RESYNC: (1.9-1.10); FILE MERGED 2006/06/13 16:21:20 hjs 1.9.294.1: #i55987# seperate INCLUDE from CFLAGS; introduce INCLUDE_C for include path without stlport

--- solenv/inc/unxbsdi2.mk
+++ solenv/inc/unxbsdi2.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxbsdi2.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-19 17:13:23 $
+#   last change: $Author: kz $ $Date: 2006-07-05 22:00:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -66,7 +66,7 @@ CXX*=g++
 # name of C Compiler
 CC*=gcc
 # flags for C and C++ Compiler
-CFLAGS+=-fmessage-length=0 -c $(INCLUDE)
+CFLAGS+=-fmessage-length=0 -c
 
 # flags to enable build with symbols; required for crashdump feature
 .IF "$(ENABLE_SYMBOLS)"=="SMALL"
