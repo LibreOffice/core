a8f3f8b808bfcaff8c8b1a4c34a4b12a85e5bff4
Kurt Zenker <kz@openoffice.org>
2006-07-05 20:57:59 +0000

    INTEGRATION: CWS ause058 (1.20.330); FILE MERGED 2006/06/14 11:15:15 hjs 1.20.330.1: #i58351# applied relevant parts of the patch to support bash

--- solenv/inc/tg_dep.mk
+++ solenv/inc/tg_dep.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: tg_dep.mk,v $
 #
-#   $Revision: 1.20 $
+#   $Revision: 1.21 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:43:19 $
+#   last change: $Author: kz $ $Date: 2006-07-05 21:57:59 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,8 @@
 .IF "$(SLOFILES)$(OBJFILES)$(DEPOBJFILES)$(SRCFILES)$(SRC1FILES)$(SRC2FILES)$(SRC3FILES)$(RCFILES)$(HXX1TARGET)$(HDBDEPNTARGET)$(IDLFILES)$(PARFILES)$(ZIP1TARGET)$(ZIP2TARGET)$(ZIP3TARGET)$(ZIP4TARGET)$(ZIP5TARGET)$(ZIP6TARGET)$(ZIP7TARGET)$(ZIP8TARGET)$(ZIP9TARGET)$(COMP1TYPELIST)$(COMP2TYPELIST)$(COMP3TYPELIST)$(COMP4TYPELIST)$(COMP5TYPELIST)$(COMP6TYPELIST)$(COMP7TYPELIST)$(COMP8TYPELIST)$(COMP9TYPELIST)"!=""
 ALLDEP .PHONY: 
 .IF "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
-    @+if ( -e  "$(SRS)$/$(PWD:f).*.dpr" ) +-$(RM) "$(SRS)$/$(PWD:f).*.dpr" >& $(NULLDEV)
+    @+$(IFEXIST) $(SRS)$/$(PWD:f).*.dpr $(THEN) +-$(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV) $(FI)
+#	@+if ( -e  "$(SRS)$/$(PWD:f).*.dpr" ) +-$(RM) "$(SRS)$/$(PWD:f).*.dpr" >& $(NULLDEV)
 .ELSE
     @+-$(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV)
 .ENDIF
@@ -93,8 +94,8 @@ ALLDPC: \
 .ENDIF
 .ENDIF
 .IF "$(HDBDEPNTARGET)$(OBJFILES)$(SLOFILES)$(DEPOBJFILES)$(RCFILES)$(PARFILES)"!=""
-    @+$(IFEXIST) $(MISC)$/$(TARGET).dpj $(THEN) $(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV)
-    @+$(IFEXIST) $(MISC)$/genjava.mk $(THEN) $(RM) $(MISC)$/genjava.mk >& $(NULLDEV)
+    @+$(IFEXIST) $(MISC)$/$(TARGET).dpj $(THEN) $(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV) $(FI)
+    @+$(IFEXIST) $(MISC)$/genjava.mk $(THEN) $(RM) $(MISC)$/genjava.mk >& $(NULLDEV) $(FI)
     +$(TOUCH) $(MISC)$/$(TARGET).dpc
 .IF "$(SVXLIGHT)"!=""
 #.IF "$(SVXLIGHTSLOFILES)"!=""
