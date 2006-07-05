b9879c71791e68f8ca1cd86bcd7cd7cdb87e941e
Kurt Zenker <kz@openoffice.org>
2006-07-05 21:04:13 +0000

    INTEGRATION: CWS ause058 (1.2.96); FILE MERGED 2006/06/13 16:21:23 hjs 1.2.96.1: #i55987# seperate INCLUDE from CFLAGS; introduce INCLUDE_C for include path without stlport

--- solenv/inc/unxmacx.mk
+++ solenv/inc/unxmacx.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxmacx.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2006-03-08 14:13:02 $
+#   last change: $Author: kz $ $Date: 2006-07-05 22:04:13 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -88,7 +88,7 @@ CXX*=g++
 CC*=gcc
 objc=cc
 
-CFLAGS=-fsigned-char -fmessage-length=0 -malign-natural -c $(INCLUDE)
+CFLAGS=-fsigned-char -fmessage-length=0 -malign-natural -c
 
 # ---------------------------------
 #  Compilation flags
