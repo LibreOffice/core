f5bb89fa1015e309d6df9f823c2326598f5d0456
Kurt Zenker <kz@openoffice.org>
2006-07-06 13:36:34 +0000

    INTEGRATION: CWS warningfixes01 (1.9.4); FILE MERGED 2006/06/27 08:52:59 cd 1.9.4.1: #137321# Reset focus handler to prevent calls while in dtor

--- sfx2/source/dialog/mgetempl.cxx
+++ sfx2/source/dialog/mgetempl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mgetempl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:23:08 $
+ *  last change: $Author: kz $ $Date: 2006-07-06 14:36:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -296,6 +296,8 @@ SfxManageStyleSheetPage::~SfxManageStyleSheetPage()
 */
 
 {
+    aNameEd.SetGetFocusHdl( Link() );
+    aNameEd.SetLoseFocusHdl( Link() );
     delete pFamilies;
     pItem = 0;
     pStyle = 0;
