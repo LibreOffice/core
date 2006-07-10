fb3d6cb2cf7f45709d3d72c3a134701bdc0dbd67
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:17:33 +0000

    INTEGRATION: CWS qiq (1.8.104); FILE MERGED 2006/06/27 13:53:19 fs 1.8.104.2: RESYNC: (1.8-1.9); FILE MERGED 2006/06/16 11:32:28 fs 1.8.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/inc/connectivity/sdbcx/VDescriptor.hxx
+++ connectivity/inc/connectivity/sdbcx/VDescriptor.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VDescriptor.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:01:19 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:17:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
