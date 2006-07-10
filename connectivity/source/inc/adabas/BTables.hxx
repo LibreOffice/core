941c3bd98a3e3ce342203cada6b59b3d784298a6
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:33:09 +0000

    INTEGRATION: CWS qiq (1.9.104); FILE MERGED 2006/06/16 11:32:35 fs 1.9.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/inc/adabas/BTables.hxx
+++ connectivity/source/inc/adabas/BTables.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BTables.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:46:46 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:33:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,8 +52,8 @@ namespace connectivity
         protected:
             virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createDescriptor();
+            virtual sdbcx::ObjectType appendObject( const ::rtl::OUString& _rForName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
 
             void setComments(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor ) throw(::com::sun::star::sdbc::SQLException, ::com::sun::star::uno::RuntimeException);
