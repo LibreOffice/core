b10a8c483d3f116227391532926c3a813ccbcb06
Oliver Bolte <obo@openoffice.org>
2006-07-10 17:51:45 +0000

    INTEGRATION: CWS vq33 (1.1.2); FILE ADDED 2006/07/03 19:16:40 vq 1.1.2.1: #i66706# Move udkversion.mk from sal/inc/ to solenv/inc/.

---
+++ solenv/inc/udkversion.mk
@@ -0,0 +1,41 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: udkversion.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2006-07-10 18:51:45 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+# major 
+UDK_MAJOR=3
+# minor 
+UDK_MINOR=2
+# micro 
+UDK_MICRO=0
