f4547cbb673dd45774fd52fa26546b0a7b3858b3
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:59:08 +0000

    INTEGRATION: CWS qiq (1.1.2); FILE ADDED 2006/06/29 10:49:40 fs 1.1.2.5: added test case for cyclic references in nested sub queries 2006/06/29 10:18:49 fs 1.1.2.4: change HsqlColumDescriptor 2006/06/27 20:37:17 fs 1.1.2.3: #i51143# more tests 2006/06/27 08:06:12 fs 1.1.2.2: some more test cases 2006/06/26 21:11:09 fs 1.1.2.1: #i51143#

---
+++ dbaccess/qa/complex/dbaccess/QueryInQuery.java
@@ -0,0 +1,211 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: QueryInQuery.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:59:08 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+package complex.dbaccess;
+
+import com.sun.star.container.ElementExistException;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sdb.CommandType;
+import com.sun.star.sdb.XSingleSelectQueryComposer;
+import com.sun.star.sdbc.SQLException;
+import com.sun.star.uno.UnoRuntime;
+import connectivity.tools.HsqlColumnDescriptor;
+import connectivity.tools.HsqlTableDescriptor;
+import connectivity.tools.RowSet;
+
+public class QueryInQuery extends complexlib.ComplexTestCase
+{
+    private CRMDatabase                 m_database;
+    private XSingleSelectQueryComposer  m_composer;
+
+    // --------------------------------------------------------------------------------------------------------
+    public String[] getTestMethodNames()
+    {
+        return new String[] {
+            "executeSimpleSelect",
+            "executeAliasedSelect",
+            "checkNameCollisions",
+            "checkCyclicReferences"
+        };
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    private final XMultiServiceFactory getORB()
+    {
+        return (XMultiServiceFactory)param.getMSF();
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    public String getTestObjectName()
+    {
+        return "QueryInQuery";
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    private void createTestCase()
+    {
+        try
+        {
+            m_database = new CRMDatabase( getORB() );
+
+            m_database.getDatabase().getDataSource().createQuery( "query products", "SELECT * FROM \"products\"" );
+
+            XMultiServiceFactory connectionFactory = (XMultiServiceFactory)UnoRuntime.queryInterface(
+                XMultiServiceFactory.class, m_database.getConnection() );
+            m_composer = (XSingleSelectQueryComposer)UnoRuntime.queryInterface(
+                XSingleSelectQueryComposer.class, connectionFactory.createInstance( "com.sun.star.sdb.SingleSelectQueryComposer" ) );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace( System.err );
+            assure( "caught an exception (" + e.getMessage() + ") while creating the test case", false );
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    public void before()
+    {
+        createTestCase();
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    public void after()
+    {
+        if ( m_database != null )
+            m_database.close();
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    private void verifyEqualRowSetContent( int _outerCommandType, String _outerCommand, int _innerCommandType, String _innerCommand ) throws SQLException
+    {
+        RowSet outerRowSet = m_database.getDatabase().createRowSet( _outerCommandType, _outerCommand );
+        outerRowSet.execute();
+
+        RowSet innerRowSet = m_database.getDatabase().createRowSet( _innerCommandType, _innerCommand );
+        innerRowSet.execute();
+
+        outerRowSet.last();
+        innerRowSet.last();
+        assure( "wrong record counts", outerRowSet.getRow() == innerRowSet.getRow() );
+
+        outerRowSet.beforeFirst();
+        innerRowSet.beforeFirst();
+        assure( "wrong column counts", outerRowSet.getColumnCount() == innerRowSet.getColumnCount() );
+
+        while ( outerRowSet.next() && innerRowSet.next() )
+        {
+            for ( int i=1; i <= outerRowSet.getColumnCount(); ++i )
+            {
+                assure( "content of column " + i + " of row " + outerRowSet.getRow() + " not identical",
+                    innerRowSet.getString(i).equals( outerRowSet.getString(i) ) );
+            }
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    /** executes a SQL statement simply selecting all columns from a query
+     */
+    public void executeSimpleSelect() throws SQLException
+    {
+        verifyEqualRowSetContent(
+            CommandType.COMMAND, "SELECT * FROM \"query products\"",
+            CommandType.QUERY, "query products" );
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    /** verifies that aliases for inner queries work as expected
+     */
+    public void executeAliasedSelect() throws SQLException
+    {
+        verifyEqualRowSetContent(
+            CommandType.COMMAND, "SELECT \"PROD\".\"ID\" FROM \"query products\" AS \"PROD\"",
+            CommandType.COMMAND, "SELECT \"ID\" FROM \"products\"" );
+        verifyEqualRowSetContent(
+            CommandType.COMMAND, "SELECT \"PROD\".* FROM \"query products\" AS \"PROD\"",
+            CommandType.QUERY, "query products" );
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    /** verifies that aliases for inner queries work as expected
+     */
+    public void checkNameCollisions()
+    {
+        // create a query with a name which is used by a table
+        boolean caughtExpected = false;
+        try
+        {
+            m_database.getDatabase().getDataSource().createQuery( "products", "SELECT * FROM \"products\"" );
+        }
+        catch ( WrappedTargetException e ) { caughtExpected = true; }
+        catch ( IllegalArgumentException e ) {}
+        catch ( ElementExistException e ) { caughtExpected = true; }
+        assure( "creating queries with the name of an existing table should not be possible",
+            caughtExpected );
+
+        // create a table with a name which is used by a query
+        HsqlTableDescriptor table = new HsqlTableDescriptor( "query products",
+            new HsqlColumnDescriptor[] {
+                new HsqlColumnDescriptor( "ID", "INTEGER" ),
+                new HsqlColumnDescriptor( "Name", "VARCHAR(50)" ) } );
+
+        caughtExpected = false;
+        try
+        {
+            m_database.getDatabase().createTableInSDBCX( table );
+        }
+        catch ( SQLException e ) { caughtExpected = true; }
+        catch ( ElementExistException ex ) { }
+        assure( "creating tables with the name of an existing query should not be possible",
+            caughtExpected );
+    }
+
+    public void checkCyclicReferences() throws ElementExistException, WrappedTargetException, IllegalArgumentException
+    {
+        // some queries which create a cycle in the sub query tree
+        m_database.getDatabase().getDataSource().createQuery( "orders level 1", "SELECT * FROM \"orders level 0\"" );
+        m_database.getDatabase().getDataSource().createQuery( "orders level 2", "SELECT * FROM \"orders level 1\"" );
+        m_database.getDatabase().getDataSource().createQuery( "orders level 3", "SELECT * FROM \"orders level 2\"" );
+        m_database.getDatabase().getDataSource().createQuery( "orders level 0", "SELECT * FROM \"orders level 3\"" );
+
+        RowSet rowSet = m_database.getDatabase().createRowSet( CommandType.QUERY, "orders level 0" );
+
+        boolean caughtExpected = false;
+        try { rowSet.execute(); }
+        catch ( SQLException e ) { caughtExpected = e.SQLState.equals( "OB001" ); }
+
+        assure( "executing a query with cyclic nested sub queries should fail!", caughtExpected );
+    }
+}
