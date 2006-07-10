4da601e50d8d9e5b6fc810320da582ba9390fca4
Oliver Bolte <obo@openoffice.org>
2006-07-10 14:20:34 +0000

    INTEGRATION: CWS qiq (1.1.2); FILE ADDED 2006/05/24 06:55:28 fs 1.1.2.1: new lib sdbt

---
+++ dbaccess/source/sdbtools/inc/module_sdbt.hxx
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: module_sdbt.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-07-10 15:20:34 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef DBACCESS_MODULE_SDBT_HXX
+#define DBACCESS_MODULE_SDBT_HXX
+
+#ifndef UNOTOOLS_INC_UNOTOOLS_COMPONENTRESMODULE_HXX
+#include <unotools/componentresmodule.hxx>
+#endif
+
+//........................................................................
+namespace sdbtools
+{
+//........................................................................
+
+    DEFINE_MODULE( SdbtModule, SdbtClient, SdbtRes )
+
+//........................................................................
+} // namespace sdbtools
+//........................................................................
+
+#endif // DBACCESS_MODULE_SDBT_HXX
+
