93bc01a751d5df335b476f759def638b9cc76662
Oliver Bolte <obo@openoffice.org>
2006-07-10 15:41:27 +0000

    INTEGRATION: CWS qiq (1.7.20); FILE MERGED 2006/07/05 10:01:00 bc 1.7.20.2: #i67000# Aliasnames now only assigned whe needed 2006/07/05 08:59:11 fs 1.7.20.1: merging the changes from CWS dbwizardpp4 into this CWS, to prevent conflicts on integration

--- wizards/com/sun/star/wizards/ui/TitlesComponent.java
+++ wizards/com/sun/star/wizards/ui/TitlesComponent.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TitlesComponent.java,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-06 14:32:44 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 16:41:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
