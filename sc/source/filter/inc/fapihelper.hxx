04ff7f99c039c1f5875923ad43fb61523f8dc257
Oliver Bolte <obo@openoffice.org>
2006-07-10 12:51:44 +0000

    INTEGRATION: CWS dr48 (1.7.176); FILE MERGED 2006/05/11 15:16:40 dr 1.7.176.1: more backporting from CWS chart2mst3

--- sc/source/filter/inc/fapihelper.hxx
+++ sc/source/filter/inc/fapihelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fapihelper.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:18:15 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 13:51:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,65 +63,80 @@
 #include "ftools.hxx"
 #endif
 
-// Get properties =============================================================
-
-/** Puts the value of a property into an Any. The XPropertySet must be valid.
-    @return  sal_True, if the Any could be filled. */
-bool getPropAny(
-        ::com::sun::star::uno::Any& rAny,
-        const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& rxProp,
-        const ::rtl::OUString& rName );
-
-/** Template for getting data from a property. The XPropertySet must be valid.
-    @return  true, if the struct could be filled. */
-template< typename Type >
-inline bool getPropValue(
-        Type& rValue,
-        const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& rxProp,
-        const ::rtl::OUString& rName )
-{
-    ::com::sun::star::uno::Any aAny;
-    return ::getPropAny( aAny, rxProp, rName ) && (aAny >>= rValue);
-}
-
-/** Gets a boolean value from an XPropertySet. The XPropertySet must be valid.
-    @return  true, if the boolean could be filled. */
-inline bool getPropBool(
-        bool& rbValue,
-        const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& rxProp,
-        const ::rtl::OUString& rName )
-{
-    ::com::sun::star::uno::Any aAny;
-    bool bRet = ::getPropAny( aAny, rxProp, rName ) && (aAny.getValueTypeClass() == ::com::sun::star::uno::TypeClass_BOOLEAN);
-    if( bRet )
-        rbValue = (*static_cast< const sal_Bool* >( aAny.getValue() ) == sal_True);
-    return bRet;
-}
-
-/** Gets a boolean value from an XPropertySet.
-    @descr  The XPropertySet must be valid. This is the short version, it returns
-    false on a property with value sal_False and on error.
-    @return  true = if the property contains sal_True;  false = sal_False or error. */
-inline bool getPropBool(
-        const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& rxProp,
-        const ::rtl::OUString& rName )
-{
-    bool bRet;
-    return ::getPropBool( bRet, rxProp, rName ) && bRet;
-}
+namespace com { namespace sun { namespace star {
+    namespace lang { class XMultiServiceFactory; }
+} } }
 
 // Static helper functions ====================================================
 
 class SfxMedium;
+class SfxObjectShell;
 
 /** Static API helper functions. */
 class ScfApiHelper
 {
 public:
+    typedef ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >               XInterfaceRef;
+    typedef ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >    XServiceFactoryRef;
+    typedef ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >                       UnoAnySequence;
+
+public:
+    /** Converts a tools color to a UNO color value. */
+    inline static sal_Int32 ConvertToApiColor( const Color& rColor )
+                            { return static_cast< sal_Int32 >( rColor.GetColor() ); }
+    /** Converts a UNO color value to a tools color. */
+    inline static Color ConvertFromApiColor( sal_Int32 nApiColor )
+                            { return Color( static_cast< ColorData >( nApiColor ) ); }
+
+    /** Converts a non-empty vector into a UNO sequence containing elements of the same type. */
+    template< typename Type >
+    static ::com::sun::star::uno::Sequence< Type >
+                            VectorToSequence( const ::std::vector< Type >& rVector );
+
+    /** Returns the multi service factory from a document shell. */
+    static XServiceFactoryRef GetServiceFactory( SfxObjectShell* pShell );
+
+    /** Creates an instance from the passed service name, using the passed service factory. */
+    static XInterfaceRef CreateInstance(
+                            XServiceFactoryRef xFactory,
+                            const ::rtl::OUString& rServiceName );
+
+    /** Creates an instance from the passed service name, using the service factory of the passed object. */
+    static XInterfaceRef CreateInstance(
+                            SfxObjectShell* pShell,
+                            const ::rtl::OUString& rServiceName );
+
+    /** Creates an instance from the passed service name, using the process service factory. */
+    static XInterfaceRef CreateInstance( const ::rtl::OUString& rServiceName );
+
+    /** Creates an instance from the passed service name, using the passed service factory. */
+    static XInterfaceRef CreateInstanceWithArgs(
+                            XServiceFactoryRef xFactory,
+                            const ::rtl::OUString& rServiceName,
+                            const UnoAnySequence& rArgs );
+
+    /** Creates an instance from the passed service name, using the service factory of the passed object. */
+    static XInterfaceRef CreateInstanceWithArgs(
+                            SfxObjectShell* pShell,
+                            const ::rtl::OUString& rServiceName,
+                            const UnoAnySequence& rArgs );
+
+    /** Creates an instance from the passed service name, using the process service factory. */
+    static XInterfaceRef CreateInstanceWithArgs(
+                            const ::rtl::OUString& rServiceName,
+                            const UnoAnySequence& rArgs );
+
     /** Opens a password dialog and returns the entered password.
         @return  The entered password or an empty string on 'Cancel' or any error. */
     static String       QueryPasswordForMedium( SfxMedium& rMedium );
 };
+
+template< typename Type >
+::com::sun::star::uno::Sequence< Type > ScfApiHelper::VectorToSequence( const ::std::vector< Type >& rVector )
+{
+    DBG_ASSERT( !rVector.empty(), "ScfApiHelper::VectorToSequence - vector is empty" );
+    return ::com::sun::star::uno::Sequence< Type >( &rVector.front(), static_cast< sal_Int32 >( rVector.size() ) );
+}
 
 // Property sets ==============================================================
 
@@ -143,11 +158,13 @@ public:
 class ScfPropertySet
 {
 public:
-    typedef ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >       XPropertySetRef;
-    typedef ::com::sun::star::uno::Reference< ::com::sun::star::beans::XMultiPropertySet >  XMultiPropSetRef;
-    typedef ::com::sun::star::uno::Any                                                      UnoAny;
-    typedef ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >                   UnoAnySequence;
-    typedef ::com::sun::star::uno::Sequence< ::rtl::OUString >                              UnoStringSequence;
+    typedef ::com::sun::star::uno::Reference<
+                ::com::sun::star::beans::XPropertySet >         XPropertySetRef;
+    typedef ::com::sun::star::uno::Reference<
+                ::com::sun::star::beans::XMultiPropertySet >    XMultiPropSetRef;
+    typedef ::com::sun::star::uno::Any                          UnoAny;
+    typedef ::com::sun::star::uno::Sequence< UnoAny >           UnoAnySequence;
+    typedef ::com::sun::star::uno::Sequence< ::rtl::OUString >  OUStringSequence;
 
 public:
     inline explicit     ScfPropertySet() {}
@@ -200,7 +217,7 @@ public:
     /** Gets the specified properties from the property set. Tries to use the XMultiPropertySet interface.
         @param rPropNames  The property names. MUST be ordered alphabetically.
         @param rValues  The related property values. */
-    void                GetProperties( UnoAnySequence& rValues, const UnoStringSequence& rPropNames ) const;
+    void                GetProperties( UnoAnySequence& rValues, const OUStringSequence& rPropNames ) const;
 
     // Set properties ---------------------------------------------------------
 
@@ -222,12 +239,12 @@ public:
 
     /** Puts the passed color into the property set. */
     inline void         SetColorProperty( const ::rtl::OUString& rPropName, const Color& rColor )
-                            { SetProperty( rPropName, static_cast< sal_Int32 >( rColor.GetColor() ) ); }
+                            { SetProperty( rPropName, ScfApiHelper::ConvertToApiColor( rColor ) ); }
 
     /** Puts the passed properties into the property set. Tries to use the XMultiPropertySet interface.
         @param rPropNames  The property names. MUST be ordered alphabetically.
         @param rValues  The related property values. */
-    void                SetProperties( const UnoStringSequence& rPropNames, const UnoAnySequence& rValues );
+    void                SetProperties( const OUStringSequence& rPropNames, const UnoAnySequence& rValues );
 
     // ------------------------------------------------------------------------
 private:
@@ -240,53 +257,79 @@ private:
 /** Generic helper class for reading from and writing to property sets.
 
     Usage:
-    1)  Call the constructor with an array of ASCII strings ordered
-        alphabetically (required by the XMultiPropertySet interface).
-    2a) Get properties from a property set: Call the ReadFromPropertySet()
-        function, then get the read properties with the GetAny() function. The
-        indexes are the same as used for the property names passed to the
-        constructor.
-    2b) Set properties to a property set: Set the values with the GetAny()
-        function. The index is the same as used for the property names passed
-        to the constructor. All values must be set properly. Finally, call the
-        WriteToPropertySet() function.
+    1)  Call the constructor with a null-terminated array of ASCII strings.
+    2a) Read properties from a property set: Call the ReadFromPropertySet()
+        function, then get the properties with the ReadValue() function or the
+        >> stream operator. The properties are returned in order of the array
+        of property names passed in the constructor.
+    2b) Write properties to a property set: Set the values with the
+        WriteValue() function or the << stream operator. The order of the
+        properties is equal to the array of property names passed in the
+        constructor. Finally, call the WriteToPropertySet() function.
  */
 class ScfPropSetHelper
 {
 public:
-    /** @param ppPropNames  An array of ASCII property names.
-        @param nPropCount  Count of property names contained in ppPropNames. */
-    explicit            ScfPropSetHelper( const sal_Char** ppcPropNames, sal_Int32 nPropCount );
+    typedef ::com::sun::star::uno::Any UnoAny;
 
-    /** Returns the property name. */
-    inline const ::rtl::OUString& GetName( sal_Int32 nIdx ) const;
-    /** Returns a reference to the Any containing a property value. */
-    inline ::com::sun::star::uno::Any& GetAny( sal_Int32 nIdx );
+public:
+    /** @param ppPropNames  A null-terminated array of ASCII property names. */
+    explicit            ScfPropSetHelper( const sal_Char* const* ppcPropNames );
+
+    // read properties --------------------------------------------------------
 
     /** Reads all values from the passed property set. */
     void                ReadFromPropertySet( const ScfPropertySet& rPropSet );
+
+    // write properties -------------------------------------------------------
+
+    /** Must be called before reading or storing property values in the helper. */
+    void                InitializeWrite( bool bClearAllAnys = false );
+
+    /** Writes the next value to the value sequence. */
+    template< typename Type >
+    void                WriteValue( const Type& rValue );
+    /** Writes an Any to the value sequence. */
+    void                WriteValue( const UnoAny& rAny );
+    /** Writes a tools string to the value sequence. */
+    inline void         WriteValue( const String& rString )
+                            { WriteValue( ::rtl::OUString( rString ) ); }
+    /** Writes a color value to the value sequence. */
+    inline void         WriteValue( const Color& rColor )
+                            { WriteValue( ScfApiHelper::ConvertToApiColor( rColor ) ); }
+    /** Writes a C++ boolean value to the value sequence. */
+    void                WriteValue( const bool& rbValue );
+
     /** Writes all values to the passed property set. */
     void                WriteToPropertySet( ScfPropertySet& rPropSet ) const;
 
+    // ------------------------------------------------------------------------
 private:
-    typedef ::com::sun::star::uno::Sequence< ::rtl::OUString >              UnoStringSequence;
-    typedef ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >   UnoAnySequence;
+    /** Returns a pointer to the next Any to be written to. */
+    UnoAny*             GetNextAny();
 
-    UnoStringSequence   maNameSeq;          /// Sequence of property names.
+private:
+    typedef ::com::sun::star::uno::Sequence< ::rtl::OUString >  OUStringSequence;
+    typedef ::com::sun::star::uno::Sequence< UnoAny >           UnoAnySequence;
+
+    OUStringSequence    maNameSeq;          /// Sequence of property names.
     UnoAnySequence      maValueSeq;         /// Sequence of property values.
-
+    ScfInt32Vec         maNameOrder;        /// Maps initial order to alphabetical order.
+    size_t              mnNextIdx;          /// Counter for next Any to be processed.
 };
 
-inline const ::rtl::OUString& ScfPropSetHelper::GetName( sal_Int32 nIdx ) const
+template< typename Type >
+void ScfPropSetHelper::WriteValue( const Type& rValue )
 {
-    DBG_ASSERT( (0 <= nIdx) && (nIdx < maNameSeq.getLength()), "ScfPropSetHelper::GetName - invalid index" );
-    return maNameSeq[ nIdx ];
+    if( UnoAny* pAny = GetNextAny() )
+        *pAny <<= rValue;
 }
 
-inline ::com::sun::star::uno::Any& ScfPropSetHelper::GetAny( sal_Int32 nIdx )
+template< typename Type >
+ScfPropSetHelper& operator<<( ScfPropSetHelper& rPropSetHelper, const Type& rValue )
 {
-    DBG_ASSERT( (0 <= nIdx) && (nIdx < maValueSeq.getLength()), "ScfPropSetHelper::GetAny - invalid index" );
-    return maValueSeq[ nIdx ];
+    rPropSetHelper.WriteValue( rValue );
+    return rPropSetHelper;
 }
 
 // ============================================================================
