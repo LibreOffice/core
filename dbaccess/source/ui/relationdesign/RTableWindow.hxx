2f4bc157326b2bebe7d97b298196c2dfdf79f45c
Oliver Bolte <obo@openoffice.org>
2006-07-10 14:45:20 +0000

    INTEGRATION: CWS qiq (1.2.122); FILE MERGED 2006/05/10 11:02:50 fs 1.2.122.1: #i51143# base class (TableWindow) now also allows to hold a query - we ourself don't

--- dbaccess/source/ui/relationdesign/RTableWindow.hxx
+++ dbaccess/source/ui/relationdesign/RTableWindow.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RTableWindow.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:36:00 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 15:45:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,6 +53,8 @@ namespace dbaui
                 The composed name or the window name.
         */
         virtual ::rtl::OUString GetName() const { return GetComposedName(); }
+
+        virtual bool    allowQueries() const;
     };
 }
 #endif //DBAUI_RELTABLEWINDOW_HXX
