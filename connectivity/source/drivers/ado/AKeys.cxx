e94bcbc6af524db490c08d3f6ab6bbfa16974385
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:23:55 +0000

    INTEGRATION: CWS qiq (1.15.104); FILE MERGED 2006/06/27 14:06:46 fs 1.15.104.2: RESYNC: (1.15-1.16); FILE MERGED 2006/06/16 11:32:32 fs 1.15.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/drivers/ado/AKeys.cxx
+++ connectivity/source/drivers/ado/AKeys.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AKeys.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:14:30 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:23:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,6 +63,9 @@
 #ifndef _COMPHELPER_PROPERTY_HXX_
 #include <comphelper/property.hxx>
 #endif
+#ifndef _DBHELPER_DBEXCEPTION_HXX_
+#include <connectivity/dbexception.hxx>
+#endif
 
 using namespace ::comphelper;
 using namespace connectivity;
@@ -84,46 +87,52 @@ void OKeys::impl_refresh() throw(RuntimeException)
     m_aCollection.Refresh();
 }
 // -------------------------------------------------------------------------
-Reference< XPropertySet > OKeys::createEmptyObject()
+Reference< XPropertySet > OKeys::createDescriptor()
 {
     return new OAdoKey(isCaseSensitive(),m_pConnection);
 }
 // -------------------------------------------------------------------------
 // XAppend
-void OKeys::appendObject( const Reference< XPropertySet >& descriptor )
+sdbcx::ObjectType OKeys::appendObject( const ::rtl::OUString& _rForName, const Reference< XPropertySet >& descriptor )
 {
     OAdoKey* pKey = NULL;
-    if(getImplementation(pKey,descriptor) && pKey != NULL)
+    if ( !getImplementation( pKey, descriptor ) || pKey == NULL)
+        ::dbtools::throwGenericSQLException(
+            ::rtl::OUString::createFromAscii( "Could not create key: invalid object descriptor." ),
+            static_cast<XTypeProvider*>(this)
+        );
+
+    // To pass as column parameter to Key's Apppend method
+    OLEVariant vOptional;
+    vOptional.setNoArg();
+
+#if OSL_DEBUG_LEVEL > 0
+    KeyTypeEnum eKey =
+#endif
+        OAdoKey::Map2KeyRule(getINT32(descriptor->getPropertyValue(OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_TYPE))));
+#if OSL_DEBUG_LEVEL > 0
+    (void)eKey;
+#endif
+
+    WpADOKey aKey = pKey->getImpl();
+    ::rtl::OUString sName = aKey.get_Name();
+    if(!sName.getLength())
+        aKey.put_Name(::rtl::OUString::createFromAscii("PrimaryKey") );
+
+    ADOKeys* pKeys = m_aCollection;
+    if ( FAILED(pKeys->Append(OLEVariant((ADOKey*)aKey),
+                            adKeyPrimary, // must be every time adKeyPrimary
+                            vOptional)) )
     {
-        // To pass as column parameter to Key's Apppend method
-        OLEVariant vOptional;
-        vOptional.setNoArg();
-
-#if OSL_DEBUG_LEVEL > 0
-        KeyTypeEnum eKey =
-#endif
-            OAdoKey::Map2KeyRule(getINT32(descriptor->getPropertyValue(OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_TYPE))));
-#if OSL_DEBUG_LEVEL > 0
-        (void)eKey;
-#endif
-
-        WpADOKey aKey = pKey->getImpl();
-        ::rtl::OUString sName = aKey.get_Name();
-        if(!sName.getLength())
-            aKey.put_Name(::rtl::OUString::createFromAscii("PrimaryKey") );
-
-        ADOKeys* pKeys = m_aCollection;
-        if ( FAILED(pKeys->Append(OLEVariant((ADOKey*)aKey),
-                                adKeyPrimary, // must be every time adKeyPrimary
-                                vOptional)) )
-        {
-            ADOS::ThrowException(*m_pConnection->getConnection(),static_cast<XTypeProvider*>(this));
-            // just make sure that an SQLExceptionis thrown here
-            throw SQLException(::rtl::OUString::createFromAscii("Could not append key!"),static_cast<XTypeProvider*>(this),OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_HY0000),1000,Any());
-        }
+        ADOS::ThrowException(*m_pConnection->getConnection(),static_cast<XTypeProvider*>(this));
+        // just make sure that an SQLExceptionis thrown here
+        ::dbtools::throwGenericSQLException(
+            ::rtl::OUString::createFromAscii( "Could not append key." ),
+            static_cast<XTypeProvider*>(this)
+        );
     }
-    else
-        throw SQLException(::rtl::OUString::createFromAscii("Could not append key!"),static_cast<XTypeProvider*>(this),OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_HY0000),1000,Any());
+
+    return new OAdoKey(isCaseSensitive(),m_pConnection,pKey->getImpl());
 }
 // -------------------------------------------------------------------------
 // XDrop
@@ -131,14 +140,6 @@ void OKeys::dropObject(sal_Int32 /*_nPos*/,const ::rtl::OUString _sElementName)
 {
     if(!m_aCollection.Delete(OLEVariant(_sElementName)))
         ADOS::ThrowException(*m_pConnection->getConnection(),static_cast<XTypeProvider*>(this));
-}
-// -----------------------------------------------------------------------------
-sdbcx::ObjectType OKeys::cloneObject(const Reference< XPropertySet >& _xDescriptor)
-{
-    OAdoKey* pKey = NULL;
-    if(getImplementation(pKey,_xDescriptor) && pKey != NULL)
-        return new OAdoKey(isCaseSensitive(),m_pConnection,pKey->getImpl());
-    return sdbcx::ObjectType();
 }
 // -----------------------------------------------------------------------------
 
