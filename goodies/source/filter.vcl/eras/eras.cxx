f81364baec04c1ea73c67051b66b84edff73e6e8
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:19:40 +0000

    INTEGRATION: CWS bitcount01 (1.4.70); FILE MERGED 2006/04/13 23:34:14 thb 1.4.70.2: RESYNC: (1.4-1.5); FILE MERGED 2006/04/11 09:42:08 thb 1.4.70.1: #i63959# Discretizing bitcount for tiff, ras and pict export

--- goodies/source/filter.vcl/eras/eras.cxx
+++ goodies/source/filter.vcl/eras/eras.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eras.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:43:46 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:19:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,6 +122,9 @@ BOOL RASWriter::WriteRAS( const Graphic& rGraphic, SvStream& rRAS,
         aBmp.Convert( BMP_CONVERSION_8BIT_COLORS );
 
     mnDepth = aBmp.GetBitCount();
+
+    // export code below only handles three discrete cases
+    mnDepth = mnDepth <= 1 ? 1 : mnDepth <= 8 ? 8 : 24;
 
     if ( ( mpAcc = aBmp.AcquireReadAccess() ) )
     {
