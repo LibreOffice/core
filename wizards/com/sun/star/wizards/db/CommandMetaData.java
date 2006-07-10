7d12dbb3dbb10ea046819113acd538e6e61ec178
Oliver Bolte <obo@openoffice.org>
2006-07-10 15:38:33 +0000

    INTEGRATION: CWS qiq (1.9.20); FILE MERGED 2006/07/05 08:59:06 fs 1.9.20.2: merging the changes from CWS dbwizardpp4 into this CWS, to prevent conflicts on integration 2006/06/20 14:39:55 bc 1.9.20.1: #i51143# Querywizards made ready for queries in queries

--- wizards/com/sun/star/wizards/db/CommandMetaData.java
+++ wizards/com/sun/star/wizards/db/CommandMetaData.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CommandMetaData.java,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-06 14:14:54 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 16:38:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
