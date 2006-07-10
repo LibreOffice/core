ed047a2608c46e011f485fe57ddc025f202dd067
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:47:33 +0000

    INTEGRATION: CWS qiq (1.3.82); FILE MERGED 2006/05/11 10:38:23 fs 1.3.82.1: during #i51143#: don't use prependContextInfo if we have only one error string

--- forms/source/component/errorbroadcaster.cxx
+++ forms/source/component/errorbroadcaster.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: errorbroadcaster.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:48:38 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:47:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,7 +88,7 @@ namespace frm
     {
         Any aError;
         if ( _rContextDescription.getLength() )
-            aError = makeAny( prependContextInfo( _rException, static_cast< XSQLErrorBroadcaster* >( this ), _rContextDescription ) );
+            aError = makeAny( prependErrorInfo( _rException, static_cast< XSQLErrorBroadcaster* >( this ), _rContextDescription ) );
         else
             aError = makeAny( _rException );
 
