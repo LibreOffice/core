2e55a551271d4a1c91a0b116d9664601a4acb06f
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:15:14 +0000

    INTEGRATION: CWS qiq (1.5.104); FILE MERGED 2006/06/16 11:32:27 fs 1.5.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/inc/connectivity/TColumnsHelper.hxx
+++ connectivity/inc/connectivity/TColumnsHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TColumnsHelper.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:58:27 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:15:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,9 +61,8 @@ namespace connectivity
 
         virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
         virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-        virtual sdbcx::ObjectType cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
-        virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createDescriptor();
+        virtual sdbcx::ObjectType appendObject( const ::rtl::OUString& _rForName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
         virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
     public:
         OColumnsHelper( ::cppu::OWeakObject& _rParent
