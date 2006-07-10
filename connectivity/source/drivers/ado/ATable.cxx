07f31f9aab519a4170bc28e639699f855a9275ce
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:24:06 +0000

    INTEGRATION: CWS qiq (1.27.104); FILE MERGED 2006/06/28 09:21:24 fs 1.27.104.3: #i10000# 2006/06/27 14:10:24 fs 1.27.104.2: RESYNC: (1.27-1.28); FILE MERGED 2006/06/16 11:32:32 fs 1.27.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/drivers/ado/ATable.cxx
+++ connectivity/source/drivers/ado/ATable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ATable.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:15:26 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:24:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -224,7 +224,7 @@ void SAL_CALL OAdoTable::alterColumnByName( const ::rtl::OUString& colName, cons
 
     sal_Bool bError = sal_True;
     OAdoColumn* pColumn = NULL;
-    if(::getImplementation(pColumn,descriptor) && pColumn != NULL)
+    if(::comphelper::getImplementation(pColumn,descriptor) && pColumn != NULL)
     {
         WpADOColumns aColumns = m_aTable.get_Columns();
         bError = !aColumns.Delete(colName);
