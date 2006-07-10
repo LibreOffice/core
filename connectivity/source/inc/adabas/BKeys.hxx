4d81e19fa098624d4daafb76e7320ec3643f8d90
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:32:47 +0000

    INTEGRATION: CWS qiq (1.7.104); FILE MERGED 2006/06/16 11:32:35 fs 1.7.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/inc/adabas/BKeys.hxx
+++ connectivity/source/inc/adabas/BKeys.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BKeys.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:45:17 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:32:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,9 +55,8 @@ namespace connectivity
         protected:
             virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            virtual sdbcx::ObjectType cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
-            virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createDescriptor();
+            virtual sdbcx::ObjectType appendObject( const ::rtl::OUString& _rForName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
         public:
             OKeys(OAdabasTable* _pTable,
