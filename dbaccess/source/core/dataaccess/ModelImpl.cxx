d41d265ff10f0d19b536cc5bb4e3148b15aed313
Oliver Bolte <obo@openoffice.org>
2006-07-10 14:08:28 +0000

    INTEGRATION: CWS qiq (1.13.2); FILE MERGED 2006/06/30 13:27:52 fs 1.13.2.1: warning-free code

--- dbaccess/source/core/dataaccess/ModelImpl.cxx
+++ dbaccess/source/core/dataaccess/ModelImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ModelImpl.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:43:12 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 15:08:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -906,6 +906,11 @@ void ODatabaseModelImpl::commitStorages() SAL_THROW(( IOException, RuntimeExcept
 ModelDependentComponent::ModelDependentComponent( const ::rtl::Reference< ODatabaseModelImpl >& _model )
     :m_pImpl( _model )
     ,m_xMutex( _model->getSharedMutex() )
+{
+}
+
+// -----------------------------------------------------------------------------
+ModelDependentComponent::~ModelDependentComponent()
 {
 }
 
