d7f1e7c38275dbc696d56e505cf08e53eab5f570
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:30:32 +0000

    INTEGRATION: CWS qiq (1.3.104); FILE MERGED 2006/06/27 14:53:21 fs 1.3.104.2: RESYNC: (1.3-1.4); FILE MERGED 2006/06/16 11:32:34 fs 1.3.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/drivers/mysql/YColumns.cxx
+++ connectivity/source/drivers/mysql/YColumns.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: YColumns.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:52:53 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:30:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ OMySQLColumns::OMySQLColumns(   ::cppu::OWeakObject& _rParent
 {
 }
 // -----------------------------------------------------------------------------
-Reference< XPropertySet > OMySQLColumns::createEmptyObject()
+Reference< XPropertySet > OMySQLColumns::createDescriptor()
 {
     return new OMySQLColumn(sal_True);
 }
