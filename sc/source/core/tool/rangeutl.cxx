bb3cb612ce9d34f398b93d6cd6a3e2a55d56ed7c
Oliver Bolte <obo@openoffice.org>
2006-07-10 12:27:33 +0000

    INTEGRATION: CWS dr48 (1.5.174); FILE MERGED 2006/06/01 13:56:58 nn 1.5.174.1: #i63148# IsValidReference for area links etc.

--- sc/source/core/tool/rangeutl.cxx
+++ sc/source/core/tool/rangeutl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rangeutl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:48:58 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 13:27:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -454,7 +454,7 @@ BOOL ScAreaNameIterator::Next( String& rName, ScRange& rRange )
             if ( pRangeName && nPos < pRangeName->GetCount() )
             {
                 ScRangeData* pData = (*pRangeName)[nPos++];
-                if ( pData && pData->IsReference(rRange) )
+                if ( pData && pData->IsValidReference(rRange) )
                 {
                     rName = pData->GetName();
                     return TRUE;                            // gefunden
