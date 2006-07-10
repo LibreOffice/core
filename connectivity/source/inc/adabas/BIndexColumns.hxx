a1457110690faab643b6a163e72a691eeaf35fee
Oliver Bolte <obo@openoffice.org>
2006-07-10 13:32:25 +0000

    INTEGRATION: CWS qiq (1.6.104); FILE MERGED 2006/06/16 11:32:35 fs 1.6.104.1: during #i51143#:
    refactored VCollection:
    - createEmptyObject now named createDescriptor
    - cloneObject removed
    - appendObject now returns the newly created object
      (previously done via a subsequent call to cloneObject)

--- connectivity/source/inc/adabas/BIndexColumns.hxx
+++ connectivity/source/inc/adabas/BIndexColumns.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BIndexColumns.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:44:07 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:32:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,10 +52,9 @@ namespace connectivity
             OAdabasIndex* m_pIndex;
         protected:
             virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createDescriptor();
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
-            virtual sdbcx::ObjectType cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
-            virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
+            virtual sdbcx::ObjectType appendObject( const ::rtl::OUString& _rForName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
         public:
             OIndexColumns(  OAdabasIndex* _pIndex,
                             ::osl::Mutex& _rMutex,
--- connectivity/source/inc/adabas/BIndexes.hxx
+++ connectivity/source/inc/adabas/BIndexes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BIndexes.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:44:21 $
+ *  last change: $Author: obo $ $Date: 2006-07-10 14:32:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,9 +54,8 @@ namespace connectivity
         protected:
             virtual sdbcx::ObjectType createObject(const ::rtl::OUString& _rName);
             virtual void impl_refresh() throw(::com::sun::star::uno::RuntimeException);
-            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createEmptyObject();
-            virtual sdbcx::ObjectType cloneObject(const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& _xDescriptor);
-            virtual void appendObject( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
+            virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > createDescriptor();
+            virtual sdbcx::ObjectType appendObject( const ::rtl::OUString& _rForName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& descriptor );
             virtual void dropObject(sal_Int32 _nPos,const ::rtl::OUString _sElementName);
         public:
             OIndexes(OAdabasTable* _pTable,
