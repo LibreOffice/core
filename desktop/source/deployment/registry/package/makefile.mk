e40960945fba7d1f503108dd63acf809a71f3b0d
Oliver Bolte <obo@openoffice.org>
2006-07-13 16:07:27 +0000

    INTEGRATION: CWS jsc7 (1.4.142); FILE MERGED 2006/06/28 07:21:54 jl 1.4.142.1: #66211# support of license files in extensions

--- desktop/source/deployment/registry/package/makefile.mk
+++ desktop/source/deployment/registry/package/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 17:32:00 $
+#   last change: $Author: obo $ $Date: 2006-07-13 17:07:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,8 @@ SRC1FILES = \
 INCPRE += ..$/..$/inc
 
 SLOFILES = \
-        $(SLO)$/dp_package.obj
+        $(SLO)$/dp_package.obj \
+    $(SLO)$/dp_description.obj
 
 .INCLUDE : ..$/..$/target.pmk
 .INCLUDE : target.mk
