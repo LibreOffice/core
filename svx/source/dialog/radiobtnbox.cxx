2ad625bba8474130250f7f2584a539975334f753
Oliver Bolte <obo@openoffice.org>
2006-07-13 11:01:34 +0000

    INTEGRATION: CWS pathoptions01 (1.1.2); FILE ADDED 2006/07/07 04:35:40 pb 1.1.2.1: fix: #i67100# new class svx::SvxRadioButtonListBox

---
+++ svx/source/dialog/radiobtnbox.cxx
@@ -0,0 +1,126 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: radiobtnbox.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-07-13 12:01:34 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+/*
+#ifdef SVX_DLLIMPLEMENTATION
+#undef SVX_DLLIMPLEMENTATION
+#endif
+*/
+
+// include ---------------------------------------------------------------
+
+#include "radiobtnbox.hxx"
+#include "dialmgr.hxx"
+
+namespace svx {
+
+// class SvxRadioButtonListBox ----------------------------------------------------
+
+SvxRadioButtonListBox::SvxRadioButtonListBox( Window* _pParent, const ResId& _rId ) :
+
+    SvxSimpleTable( _pParent, _rId )
+
+{
+    EnableCheckButton( new SvLBoxButtonData( this, true ) );
+}
+
+SvxRadioButtonListBox::~SvxRadioButtonListBox()
+{
+}
+
+void SvxRadioButtonListBox::SetTabs()
+{
+    SvxSimpleTable::SetTabs();
+/*
+    USHORT nAdjust = SV_LBOXTAB_ADJUST_RIGHT | SV_LBOXTAB_ADJUST_LEFT |
+                     SV_LBOXTAB_ADJUST_CENTER | SV_LBOXTAB_ADJUST_NUMERIC | SV_LBOXTAB_FORCE;
+    if ( aTabs.Count() > 0 )
+    {
+        SvLBoxTab* pTab = (SvLBoxTab*)aTabs.GetObject(0);
+        pTab->nFlags &= ~nAdjust;
+        pTab->nFlags |= SV_LBOXTAB_PUSHABLE | SV_LBOXTAB_ADJUST_CENTER | SV_LBOXTAB_FORCE;
+    }
+*/
+}
+
+void SvxRadioButtonListBox::MouseButtonUp( const MouseEvent& _rMEvt )
+{
+    m_aCurMousePoint = _rMEvt.GetPosPixel();
+    SvxSimpleTable::MouseButtonUp( _rMEvt );
+}
+
+void SvxRadioButtonListBox::KeyInput( const KeyEvent& rKEvt )
+{
+    if ( !rKEvt.GetKeyCode().GetModifier() && KEY_SPACE == rKEvt.GetKeyCode().GetCode() )
+    {
+        SvLBoxEntry* pEntry = FirstSelected();
+        if ( GetCheckButtonState( pEntry ) == SV_BUTTON_UNCHECKED )
+        {
+            SetCheckButtonState( pEntry, SV_BUTTON_CHECKED );
+            GetCheckButtonHdl().Call( NULL );
+            return ;
+        }
+    }
+
+    SvxSimpleTable::KeyInput( rKEvt );
+}
+
+void SvxRadioButtonListBox::HandleEntryChecked( SvLBoxEntry* _pEntry )
+{
+    Select( _pEntry, TRUE );
+    SvButtonState eState = GetCheckButtonState( _pEntry );
+
+    if ( SV_BUTTON_CHECKED == eState )
+    {
+        // we have radio button behavior -> so uncheck the other entries
+        SvLBoxEntry* pEntry = First();
+        while ( pEntry )
+        {
+            if ( pEntry != _pEntry )
+                SetCheckButtonState( pEntry, SV_BUTTON_UNCHECKED );
+            pEntry = Next( pEntry );
+        }
+    }
+    else
+        SetCheckButtonState( _pEntry, SV_BUTTON_CHECKED );
+}
+
+const Point& SvxRadioButtonListBox::GetCurMousePoint() const
+{
+    return m_aCurMousePoint;
+}
+
+} // end of namespace ::svx
+
