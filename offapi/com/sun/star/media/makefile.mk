1f4294f04105f328408048bc9f2e880ca7b9ad3b
Oliver Bolte <obo@openoffice.org>
2006-07-13 11:01:30 +0000

    INTEGRATION: CWS jsc8 (1.4.126); FILE MERGED 2006/06/09 07:05:44 jsc 1.4.126.1: #i65749# remove unncessary service files

--- offapi/com/sun/star/media/makefile.mk
+++ offapi/com/sun/star/media/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 02:21:18 $
+#   last change: $Author: obo $ $Date: 2006-07-13 12:01:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,9 +46,6 @@ PACKAGE=com$/sun$/star$/media
 
 IDLFILES=\
     Manager.idl         \
-    FrameGrabber.idl    \
-    Player.idl          \
-    PlayerWindow.idl    \
     XManager.idl        \
     XFrameGrabber.idl   \
     XPlayer.idl         \
