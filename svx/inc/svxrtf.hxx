b09ca86cd787bf3f52fb1dc955bb3ba39cffd76a
Oliver Bolte <obo@openoffice.org>
2006-07-14 07:43:31 +0000

    INTEGRATION: CWS tl21 (1.18.388); FILE MERGED 2006/06/12 14:29:07 tl 1.18.388.1: #i66167# font size problem when pasting (importing rtf) from sw to sd fixed

--- svx/inc/svxrtf.hxx
+++ svx/inc/svxrtf.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxrtf.hxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:02:46 $
+ *  last change: $Author: obo $ $Date: 2006-07-14 08:43:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
