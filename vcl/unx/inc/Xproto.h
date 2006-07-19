61bdfa4ece1701e08a776b15aab106215d31245e
Kurt Zenker <kz@openoffice.org>
2006-07-19 15:48:44 +0000

    INTEGRATION: CWS warningfixes02 (1.1.2); FILE ADDED 2006/06/30 12:18:46 sb 1.1.2.1: #i66577# Made the code compile (warning-free) on a unxlngi6.pro GCC 4.1.1 Linux box.

---
+++ vcl/unx/inc/Xproto.h
@@ -0,0 +1,59 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: Xproto.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-07-19 16:48:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_VCL_UNX_INC_XPROTO_H
+#define INCLUDED_VCL_UNX_INC_XPROTO_H
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+#include "prex.h"
+
+#if defined __GNUC__
+#pragma GCC system_header
+#elif defined __SUNPRO_CC
+#pragma disable_warn
+#endif
+
+#include <X11/Xproto.h>
+
+#if defined __SUNPRO_CC
+#pragma enable_warn
+#endif
+
+#include "postx.h"
+
+#endif
