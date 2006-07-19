eece83f52f93b43de3991718aee9340a87cc8b39
Kurt Zenker <kz@openoffice.org>
2006-07-19 16:15:33 +0000

    INTEGRATION: CWS warningfixes02 (1.1.2); FILE ADDED 2006/06/30 12:31:43 sb 1.1.2.1: #i66577# Made the code compile (warning-free) on a unxlngi6.pro GCC 4.1.1 Linux box.

---
+++ shell/source/backends/gconfbe/orbit.h
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: orbit.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-07-19 17:15:33 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_SHELL_SOURCE_BACKENDS_GCONFBE_ORBIT_H
+#define INCLUDED_SHELL_SOURCE_BACKENDS_GCONFBE_ORBIT_H
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+#if defined __GNUC__
+#pragma GCC system_header
+#endif
+
+#include <orbit/orbit.h>
+
+#endif
