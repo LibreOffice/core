f8b583d9b7fe47cdd8f6c96dca090fc19408406d
Kurt Zenker <kz@openoffice.org>
2006-07-19 15:32:36 +0000

    INTEGRATION: CWS warningfixes02 (1.12.8); FILE MERGED 2006/06/30 11:58:01 sb 1.12.8.1: #i66577# Made the code compile (warning-free) on a unxlngi6.pro GCC 4.1.1 Linux box.

--- fpicker/source/unx/gnome/SalGtkFilePicker.cxx
+++ fpicker/source/unx/gnome/SalGtkFilePicker.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SalGtkFilePicker.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-06 10:31:59 $
+ *  last change: $Author: kz $ $Date: 2006-07-19 16:32:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1333,7 +1333,7 @@ void SAL_CALL SalGtkFilePicker::setValue( sal_Int16 nControlId, sal_Int16 nContr
 
     else if( tType == GTK_TYPE_TOGGLE_BUTTON )
     {
-        sal_Bool bChecked;
+        sal_Bool bChecked = false;
         rValue >>= bChecked;
         gtk_toggle_button_set_active( GTK_TOGGLE_BUTTON( pWidget ), bChecked );
     }
