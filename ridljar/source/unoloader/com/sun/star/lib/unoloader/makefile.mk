102a5c9447845539a2d916bf2cd2a38d49093656
Kurt Zenker <kz@openoffice.org>
2006-07-20 15:06:08 +0000

    INTEGRATION: CWS sb55 (1.4.20); FILE MERGED 2006/07/10 14:35:32 sb 1.4.20.1: #i66004# More robust now.

--- ridljar/source/unoloader/com/sun/star/lib/unoloader/makefile.mk
+++ ridljar/source/unoloader/com/sun/star/lib/unoloader/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 13:26:32 $
+#   last change: $Author: kz $ $Date: 2006-07-20 16:06:08 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ $(JAVACLASSFILES): $(MISC)$/$(TARGET).mkdir.done
 .ENDIF
 
 $(MISC)$/$(TARGET).mkdir.done .ERRREMOVE:
-    $(MKDIR) $(CLASSDIR)
+    $(MKDIRHIER) $(CLASSDIR)
     $(TOUCH) $@
 
 .ENDIF
