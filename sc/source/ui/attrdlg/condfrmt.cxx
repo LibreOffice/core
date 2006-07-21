5a08a89d34c088b8e260900307268932ae10e44e
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:14:12 +0000

    INTEGRATION: CWS pchfix01 (1.6.216); FILE MERGED 2006/07/12 10:02:23 kaib 1.6.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/attrdlg/condfrmt.cxx
+++ sc/source/ui/attrdlg/condfrmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: condfrmt.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:26:42 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:14:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SFXDISPATCH_HXX //autogen
 #include <sfx2/dispatch.hxx>
