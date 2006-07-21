2ddc33eaed9fbb1491a6ee9815d61d199dfef3a4
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:32:42 +0000

    INTEGRATION: CWS pchfix01 (1.14.84); FILE MERGED 2006/07/12 10:02:03 kaib 1.14.84.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/rtf/eeimpars.cxx
+++ sc/source/filter/rtf/eeimpars.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eeimpars.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 11:51:10 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:32:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 //------------------------------------------------------------------------
 
