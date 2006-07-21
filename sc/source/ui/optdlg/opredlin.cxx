882d6c8c2541670e1d1c59c32696ccab3cfc57d9
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:16:25 +0000

    INTEGRATION: CWS pchfix01 (1.6.10); FILE MERGED 2006/07/12 10:02:48 kaib 1.6.10.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/optdlg/opredlin.cxx
+++ sc/source/ui/optdlg/opredlin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: opredlin.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:34:24 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:16:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 #undef SC_DLLIMPLEMENTATION
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 //------------------------------------------------------------------
 
