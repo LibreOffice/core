702f8451cf118bd370916222d7f694b07f90155f
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:24:30 +0000

    INTEGRATION: CWS pchfix01 (1.40.132); FILE MERGED 2006/07/12 10:03:13 kaib 1.40.132.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/viewfun5.cxx
+++ sc/source/ui/view/viewfun5.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewfun5.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 19:09:26 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:24:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 #ifndef _COM_SUN_STAR_EMBED_XEMBEDOBJECTCLIPBOARDCREATOR_HPP_
 #include <com/sun/star/embed/XEmbedObjectClipboardCreator.hpp>
 #endif
@@ -40,11 +43,7 @@
 #include <com/sun/star/embed/Aspects.hpp>
 #endif
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 #ifndef SVX_UNOMODEL_HXX
 #include <svx/unomodel.hxx>
