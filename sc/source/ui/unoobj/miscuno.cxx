712c2db9df678273f530f9e0ec95b006f9fb976e
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:41:52 +0000

    INTEGRATION: CWS pchfix01 (1.10.216); FILE MERGED 2006/07/12 10:02:57 kaib 1.10.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/unoobj/miscuno.cxx
+++ sc/source/ui/unoobj/miscuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: miscuno.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:48:14 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:41:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include <tools/debug.hxx>
 
