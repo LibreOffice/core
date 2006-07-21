c07e74f097eb9b59fa49296864673d98f04b64ff
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:41:35 +0000

    INTEGRATION: CWS pchfix01 (1.11.216); FILE MERGED 2006/07/12 10:02:06 kaib 1.11.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xml/XMLColumnRowGroupExport.cxx
+++ sc/source/filter/xml/XMLColumnRowGroupExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLColumnRowGroupExport.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:50:29 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:41:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 // INCLUDE ---------------------------------------------------------------
 
