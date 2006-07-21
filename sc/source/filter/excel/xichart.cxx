9ef89a3b6711f2f07f4e662e5ef33211366d7443
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:07:32 +0000

    INTEGRATION: CWS pchfix01 (1.7.10); FILE MERGED 2006/07/12 10:01:54 kaib 1.7.10.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xichart.cxx
+++ sc/source/filter/excel/xichart.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xichart.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:34:46 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:07:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XICHART_HXX
 #include "xichart.hxx"
