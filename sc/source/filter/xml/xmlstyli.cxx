fb5d8b162c14ffae8b6d0c75b06f6eba058138e1
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:57:24 +0000

    INTEGRATION: CWS pchfix01 (1.57.204); FILE MERGED 2006/07/12 10:02:13 kaib 1.57.204.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xml/xmlstyli.cxx
+++ sc/source/filter/xml/xmlstyli.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlstyli.cxx,v $
  *
- *  $Revision: 1.57 $
+ *  $Revision: 1.58 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 12:07:38 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:57:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 #ifndef _XMLSTYLI_HXX
 #include "xmlstyli.hxx"
