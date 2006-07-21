acc4e486cfd99406f17577997e4741f5bb82bf5a
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:37:44 +0000

    INTEGRATION: CWS pchfix01 (1.18.58); FILE MERGED 2006/07/12 10:02:32 kaib 1.18.58.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/docshell/docsh2.cxx
+++ sc/source/ui/docshell/docsh2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docsh2.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-04 15:03:15 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:37:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,16 +33,15 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+
 
 #ifndef _SVDPAGE_HXX //autogen
 #include <svx/svdpage.hxx>
 #endif
 
-#pragma hdrstop
 
 #include <svx/xtable.hxx>
 
