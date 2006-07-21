73ce059849f6dc49f74cc89b52a0fac14f3f01f7
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:25:30 +0000

    INTEGRATION: CWS pchfix01 (1.3.216); FILE MERGED 2006/07/12 10:02:51 kaib 1.3.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/undo/refundo.cxx
+++ sc/source/ui/undo/refundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: refundo.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:37:52 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:25:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 // -----------------------------------------------------------------------
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------------
 
