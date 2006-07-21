a5faa73e4d06d41104e0795c6817f65f05ba3ee5
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:45:45 +0000

    INTEGRATION: CWS pchfix01 (1.6.214); FILE MERGED 2006/07/12 10:02:35 kaib 1.6.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/docshell/servobj.cxx
+++ sc/source/ui/docshell/servobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: servobj.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:07:49 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:45:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,13 +32,12 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 // System - Includes -----------------------------------------------------
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 #include <sot/formats.hxx>
 #include <sfx2/app.hxx>
