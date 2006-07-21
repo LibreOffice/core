1a58a7021dae19c81d0f11f1e83486d10627c60e
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:46:22 +0000

    INTEGRATION: CWS pchfix01 (1.5.206); FILE MERGED 2006/07/12 10:01:47 kaib 1.5.206.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/tool/zforauto.cxx
+++ sc/source/core/tool/zforauto.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: zforauto.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:17:24 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:46:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include <svtools/zforlist.hxx>
 #include <svtools/zformat.hxx>
