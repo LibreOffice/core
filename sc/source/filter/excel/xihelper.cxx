27aad5277bd0555bd23d0cf02747179d65c744f2
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:10:03 +0000

    INTEGRATION: CWS pchfix01 (1.22.152); FILE MERGED 2006/07/12 10:01:54 kaib 1.22.152.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xihelper.cxx
+++ sc/source/filter/excel/xihelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xihelper.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:41:21 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:10:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XIHELPER_HXX
 #include "xihelper.hxx"
