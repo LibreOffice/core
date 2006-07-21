28f15ce8abc9e3ad178753759332d2d561934f61
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:58:50 +0000

    INTEGRATION: CWS pchfix01 (1.7.10); FILE MERGED 2006/07/12 10:02:41 kaib 1.7.10.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/formdlg/structpg.cxx
+++ sc/source/ui/formdlg/structpg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: structpg.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:33:58 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:58:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 //----------------------------------------------------------------------------
 
