da8b4c7e18fdcd04bb4cacf51f7b1e03669c161e
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:03:35 +0000

    INTEGRATION: CWS pchfix01 (1.12.206); FILE MERGED 2006/07/12 10:03:06 kaib 1.12.206.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/olinewin.cxx
+++ sc/source/ui/view/olinewin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: olinewin.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 12:16:40 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:03:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,9 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
