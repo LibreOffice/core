550246c1f8cfe593f67f5f4ab1b59ec4c26c5e9d
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:59:51 +0000

    INTEGRATION: CWS pchfix01 (1.6.214); FILE MERGED 2006/07/12 10:02:15 kaib 1.6.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/Accessibility/AccessibilityHints.cxx
+++ sc/source/ui/Accessibility/AccessibilityHints.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibilityHints.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:15:21 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:59:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include "AccessibilityHints.hxx"
 
