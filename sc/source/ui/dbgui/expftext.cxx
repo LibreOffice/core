86d553d632a3094b286b579215aed288f7552f53
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:22:37 +0000

    INTEGRATION: CWS pchfix01 (1.2.216); FILE MERGED 2006/07/12 10:02:27 kaib 1.2.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/expftext.cxx
+++ sc/source/ui/dbgui/expftext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: expftext.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:35:33 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:22:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include "expftext.hxx"
 #include <vcl/help.hxx>
