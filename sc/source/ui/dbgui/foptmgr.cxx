7fb4504df782a8633d672cdbbc31d494b8a3e822
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:24:31 +0000

    INTEGRATION: CWS pchfix01 (1.4.216); FILE MERGED 2006/07/12 10:02:27 kaib 1.4.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/foptmgr.cxx
+++ sc/source/ui/dbgui/foptmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: foptmgr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:36:28 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:24:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 // System - Includes ---------------------------------------------------------
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
 
 // INCLUDE -------------------------------------------------------------------
