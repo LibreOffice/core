f6fb71f2371313a90f03da373105960c42af80e5
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:18:46 +0000

    INTEGRATION: CWS pchfix01 (1.17.152); FILE MERGED 2006/07/12 10:01:56 kaib 1.17.152.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xlocx.cxx
+++ sc/source/filter/excel/xlocx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xlocx.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:45:56 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:18:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XLOCX_HXX
 #include "xlocx.hxx"
