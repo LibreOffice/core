c6b29344952ae95a80c2465b624200a03c49fabc
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:20:15 +0000

    INTEGRATION: CWS pchfix01 (1.8.216); FILE MERGED 2006/07/12 10:01:56 kaib 1.8.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xlpivot.cxx
+++ sc/source/filter/excel/xlpivot.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xlpivot.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:09:23 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:20:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XLPIVOT_HXX
 #include "xlpivot.hxx"
