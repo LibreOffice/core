e927ab44d84ee5438c686f82a6f1c648e0989fdf
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:24:43 +0000

    INTEGRATION: CWS pchfix01 (1.12.102); FILE MERGED 2006/07/12 10:02:27 kaib 1.12.102.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/imoptdlg.cxx
+++ sc/source/ui/dbgui/imoptdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imoptdlg.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-22 12:09:38 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:24:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include "imoptdlg.hxx"
 #include "scresid.hxx"
