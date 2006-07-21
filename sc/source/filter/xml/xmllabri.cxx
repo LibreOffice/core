df252a3ea29327cb2bbf2400e01b6db3d41c8b79
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:53:39 +0000

    INTEGRATION: CWS pchfix01 (1.10.216); FILE MERGED 2006/07/12 10:02:11 kaib 1.10.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xml/xmllabri.cxx
+++ sc/source/filter/xml/xmllabri.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmllabri.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:09:47 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:53:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 //___________________________________________________________________
 
