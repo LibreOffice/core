c1ba179e34f4389459321f143f784bfb157f22ce
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:12:14 +0000

    INTEGRATION: CWS pchfix01 (1.14.214); FILE MERGED 2006/07/12 10:02:22 kaib 1.14.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/app/typemap.cxx
+++ sc/source/ui/app/typemap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typemap.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:25:37 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:12:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 #include <svx/svxids.hrc>
 #include "sc.hrc"
