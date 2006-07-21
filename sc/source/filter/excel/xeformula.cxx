bc9254de5532978bd519a2d4681a6145024deeba
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:55:19 +0000

    INTEGRATION: CWS pchfix01 (1.10.56); FILE MERGED 2006/07/12 10:01:50 kaib 1.10.56.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xeformula.cxx
+++ sc/source/filter/excel/xeformula.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xeformula.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:31:41 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:55:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XEFORMULA_HXX
 #include "xeformula.hxx"
