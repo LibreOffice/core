358b917d3ded683b75a56ac21ceeed164e8f4447
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:49:43 +0000

    INTEGRATION: CWS pchfix01 (1.6.212); FILE MERGED 2006/07/12 10:02:38 kaib 1.6.212.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/drawfunc/fuconuno.cxx
+++ sc/source/ui/drawfunc/fuconuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuconuno.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:56:08 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:49:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 //------------------------------------------------------------------------
 
