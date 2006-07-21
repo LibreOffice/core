4adaad7446ce73b7f0dbafa5d4b5842be9f1009e
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:53:13 +0000

    INTEGRATION: CWS pchfix01 (1.8.216); FILE MERGED 2006/07/12 10:02:11 kaib 1.8.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xml/xmlfonte.cxx
+++ sc/source/filter/xml/xmlfonte.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlfonte.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:08:45 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:53:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 #ifdef PRECOMPILED
 #include "filt_pch.hxx"
 #endif
 
-#pragma hdrstop
 
 #ifndef SC_ITEMS_HXX
 #include "scitems.hxx"
