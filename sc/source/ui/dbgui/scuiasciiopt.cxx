4e9449c9b0e6e1d9f9a1a46b0c1f87dd2b9e0d58
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:28:15 +0000

    INTEGRATION: CWS pchfix01 (1.8.204); FILE MERGED 2006/07/12 10:02:28 kaib 1.8.204.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/scuiasciiopt.cxx
+++ sc/source/ui/dbgui/scuiasciiopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scuiasciiopt.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 12:08:42 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:28:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #undef SC_DLLIMPLEMENTATION
 
