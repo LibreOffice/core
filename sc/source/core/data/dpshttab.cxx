b696f057b80bdb027854d097261333cf055a6770
Kurt Zenker <kz@openoffice.org>
2006-07-21 09:57:52 +0000

    INTEGRATION: CWS pchfix01 (1.7.168); FILE MERGED 2006/07/12 10:01:33 kaib 1.7.168.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/data/dpshttab.cxx
+++ sc/source/core/data/dpshttab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dpshttab.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 15:04:57 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 10:57:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 // INCLUDE --------------------------------------------------------------
 
