fbc6ec713aee5f741081db1169cd5999e9a60971
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:31:50 +0000

    INTEGRATION: CWS pchfix01 (1.2.140); FILE MERGED 2006/07/12 10:02:02 kaib 1.2.140.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/qpro/qprostyle.cxx
+++ sc/source/filter/qpro/qprostyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: qprostyle.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-01-27 15:51:41 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:31:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 
 #include <sal/config.h>
