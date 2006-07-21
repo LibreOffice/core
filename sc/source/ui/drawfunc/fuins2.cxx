98eae58738bcd389c34b0826c77cda4f8782d16b
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:50:17 +0000

    INTEGRATION: CWS pchfix01 (1.21.132); FILE MERGED 2006/07/12 10:02:38 kaib 1.21.132.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/drawfunc/fuins2.cxx
+++ sc/source/ui/drawfunc/fuins2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuins2.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 19:08:52 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:50:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _COM_SUN_STAR_EMBED_NOVISUALAREASIZEEXCEPTION_HPP_
 #include <com/sun/star/embed/NoVisualAreaSizeException.hpp>
