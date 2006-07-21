076644927dfa3c6efa04b561cad90adb051dea23
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:17:15 +0000

    INTEGRATION: CWS pchfix01 (1.4.56); FILE MERGED 2006/07/12 10:01:56 kaib 1.4.56.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xldumper.cxx
+++ sc/source/filter/excel/xldumper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xldumper.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:45:17 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:17:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XLDUMPER_HXX
 #include "xldumper.hxx"
