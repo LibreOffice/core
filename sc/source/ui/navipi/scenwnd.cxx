89feadf610e36e0da4a6b74f8baea3f26b3c0f73
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:16:08 +0000

    INTEGRATION: CWS pchfix01 (1.5.62); FILE MERGED 2006/07/12 10:02:48 kaib 1.5.62.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/navipi/scenwnd.cxx
+++ sc/source/ui/navipi/scenwnd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scenwnd.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:50:36 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:16:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 //------------------------------------------------------------------
 
