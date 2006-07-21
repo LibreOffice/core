afda45bea3e90cb1825fc935310e6aa6db7f50be
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:40:11 +0000

    INTEGRATION: CWS pchfix01 (1.7.204); FILE MERGED 2006/07/12 10:01:44 kaib 1.7.204.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/tool/rangelst.cxx
+++ sc/source/core/tool/rangelst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rangelst.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 11:39:04 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:40:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 //------------------------------------------------------------------------
 
