ec6aab24c942ff1118ff7edf8153420b751622f0
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:49:56 +0000

    INTEGRATION: CWS pchfix01 (1.6.216); FILE MERGED 2006/07/12 10:01:49 kaib 1.6.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/frmbase.cxx
+++ sc/source/filter/excel/frmbase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmbase.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:30:03 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:49:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 
 #include "formel.hxx"
