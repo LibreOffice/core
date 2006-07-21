6e0017f7a618cb9edd402d4d498f27520ef3efbe
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:02:13 +0000

    INTEGRATION: CWS pchfix01 (1.2.216); FILE MERGED 2006/07/12 10:03:05 kaib 1.2.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/imapwrap.cxx
+++ sc/source/ui/view/imapwrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imapwrap.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:59:59 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:02:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 // INCLUDE ---------------------------------------------------------------
 
