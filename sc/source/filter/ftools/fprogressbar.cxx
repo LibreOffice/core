7cac4c5981fe5d214029fe48c34853698cb529e8
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:25:43 +0000

    INTEGRATION: CWS pchfix01 (1.6.206); FILE MERGED 2006/07/12 10:01:58 kaib 1.6.206.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/ftools/fprogressbar.cxx
+++ sc/source/filter/ftools/fprogressbar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fprogressbar.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:48:24 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:25:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_FPROGRESSBAR_HXX
 #include "fprogressbar.hxx"
