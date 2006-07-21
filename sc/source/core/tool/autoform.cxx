b632bbca35935efffa4e9f593321a7b577c7a647
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:17:19 +0000

    INTEGRATION: CWS pchfix01 (1.18.216); FILE MERGED 2006/07/12 10:01:39 kaib 1.18.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/tool/autoform.cxx
+++ sc/source/core/tool/autoform.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: autoform.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:39:02 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:17:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #define READ_OLDVERS
 
