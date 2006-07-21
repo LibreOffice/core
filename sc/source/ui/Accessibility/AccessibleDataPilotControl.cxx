3a7033588406c4230a804bd79030b3d4b426c05d
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:04:42 +0000

    INTEGRATION: CWS pchfix01 (1.10.216); FILE MERGED 2006/07/12 10:02:16 kaib 1.10.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/Accessibility/AccessibleDataPilotControl.cxx
+++ sc/source/ui/Accessibility/AccessibleDataPilotControl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleDataPilotControl.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:16:56 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:04:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef _SC_ACCESSIBLEDATAPILOTCONTROL_HXX
 #include "AccessibleDataPilotControl.hxx"
