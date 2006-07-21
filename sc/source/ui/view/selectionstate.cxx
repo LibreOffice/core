5e71a99c8e292991a907fdbd1a458ede07fbe55f
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:08:14 +0000

    INTEGRATION: CWS pchfix01 (1.3.216); FILE MERGED 2006/07/12 10:03:09 kaib 1.3.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/selectionstate.cxx
+++ sc/source/ui/view/selectionstate.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: selectionstate.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:05:29 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:08:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_SELECTIONSTATE_HXX
 #include "selectionstate.hxx"
