3e339900f4204c27e42f0e912043ba7da86b5943
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:00:02 +0000

    INTEGRATION: CWS pchfix01 (1.19.58); FILE MERGED 2006/07/12 10:02:42 kaib 1.19.58.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/miscdlgs/acredlin.cxx
+++ sc/source/ui/miscdlgs/acredlin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acredlin.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-05-08 14:53:08 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:00:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 // System - Includes ---------------------------------------------------------
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 #include <svtools/undo.hxx>
 #include <unotools/textsearch.hxx>
