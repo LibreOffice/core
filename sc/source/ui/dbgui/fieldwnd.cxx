55b1f89780a8d626d37d75e2441b42e32b0b3fe6
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:23:25 +0000

    INTEGRATION: CWS pchfix01 (1.17.188); FILE MERGED 2006/07/12 10:02:27 kaib 1.17.188.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/fieldwnd.cxx
+++ sc/source/ui/dbgui/fieldwnd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fieldwnd.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-21 12:02:58 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:23:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include <vcl/virdev.hxx>
 #include <vcl/decoview.hxx>
