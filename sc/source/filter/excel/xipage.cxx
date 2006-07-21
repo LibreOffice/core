b1b02c0dea4bae2352b1f7c5b432679bf6358f0f
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:11:40 +0000

    INTEGRATION: CWS pchfix01 (1.11.214); FILE MERGED 2006/07/12 10:01:55 kaib 1.11.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xipage.cxx
+++ sc/source/filter/excel/xipage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xipage.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:42:03 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:11:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XIPAGE_HXX
 #include "xipage.hxx"
