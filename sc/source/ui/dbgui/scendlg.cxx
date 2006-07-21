7b90c621291dbfbbe31f532338f763bec1cc0111
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:27:48 +0000

    INTEGRATION: CWS pchfix01 (1.12.10); FILE MERGED 2006/07/12 10:02:28 kaib 1.12.10.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/scendlg.cxx
+++ sc/source/ui/dbgui/scendlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scendlg.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:33:33 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:27:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 #undef SC_DLLIMPLEMENTATION
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 #include "scitems.hxx"
 #include <svx/drawitem.hxx>
