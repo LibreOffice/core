8cf9e5d058d05bf0e3caf99f492bb46c91a97c90
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:06:38 +0000

    INTEGRATION: CWS pchfix01 (1.16.214); FILE MERGED 2006/07/12 10:02:17 kaib 1.16.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/Accessibility/AccessiblePageHeaderArea.cxx
+++ sc/source/ui/Accessibility/AccessiblePageHeaderArea.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessiblePageHeaderArea.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:18:40 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:06:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef _SV_GEN_HXX
 #include <tools/gen.hxx>
