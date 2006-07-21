bdf2f79dc2cb8522209f227dd6a4af98c501d664
Kurt Zenker <kz@openoffice.org>
2006-07-21 09:40:36 +0000

    INTEGRATION: CWS pathoptions02 (1.6.8); FILE MERGED 2006/07/19 12:24:42 pb 1.6.8.1: fix: #i67538# radio button column smaller

--- svx/source/dialog/multipat.cxx
+++ svx/source/dialog/multipat.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: multipat.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 12:00:13 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 10:40:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -233,7 +233,7 @@ SvxMultiPathDialog::SvxMultiPathDialog( Window* pParent, BOOL bEmptyAllowed ) :
     pImpl       ( new MultiPath_Impl( bEmptyAllowed ) )
 
 {
-    static long aStaticTabs[]= { 2, 0, 15 };
+    static long aStaticTabs[]= { 2, 0, 12 };
     aRadioLB.SvxSimpleTable::SetTabs( aStaticTabs );
     String sHeader( ResId( STR_HEADER_PATHS ) );
     aRadioLB.SetQuickHelpText( sHeader );
