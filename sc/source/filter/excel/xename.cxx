3af21e31931eeb4c836cd2ba390a0e38d6f750a6
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:58:44 +0000

    INTEGRATION: CWS pchfix01 (1.6.186); FILE MERGED 2006/07/12 10:01:51 kaib 1.6.186.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xename.cxx
+++ sc/source/filter/excel/xename.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xename.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:32:22 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:58:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XENAME_HXX
 #include "xename.hxx"
