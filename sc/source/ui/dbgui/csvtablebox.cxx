30304fc3da6a5e1037a98731e1082b999840e728
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:20:36 +0000

    INTEGRATION: CWS pchfix01 (1.10.72); FILE MERGED 2006/07/12 10:02:26 kaib 1.10.72.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/csvtablebox.cxx
+++ sc/source/ui/dbgui/csvtablebox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: csvtablebox.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-04-19 14:05:08 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:20:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,9 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 // ============================================================================
 
