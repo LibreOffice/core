b0dd090f56e28e10c5f4df7cc1533c6d8212e807
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:19:59 +0000

    INTEGRATION: CWS pchfix01 (1.11.72); FILE MERGED 2006/07/12 10:02:26 kaib 1.11.72.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/csvruler.cxx
+++ sc/source/ui/dbgui/csvruler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: csvruler.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-04-19 14:04:55 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:19:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,9 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 // ============================================================================
 
