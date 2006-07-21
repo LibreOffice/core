6a3361d9b9281b49e182fb948d791ba473df1403
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:53:46 +0000

    INTEGRATION: CWS pchfix01 (1.22.136); FILE MERGED 2006/07/12 10:03:03 kaib 1.22.136.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/drawvie4.cxx
+++ sc/source/ui/view/drawvie4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawvie4.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:38:51 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:53:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,15 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 #ifndef _COM_SUN_STAR_EMBED_NOVISUALAREASIZEEXCEPTION_HPP_
 #include <com/sun/star/embed/NoVisualAreaSizeException.hpp>
 #endif
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------------
 #include <sch/memchrt.hxx>
