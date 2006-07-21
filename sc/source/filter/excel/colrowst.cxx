8720d3194a679ff10f3cce301ad30c431b4f0b64
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:47:20 +0000

    INTEGRATION: CWS pchfix01 (1.29.206); FILE MERGED 2006/07/12 10:01:48 kaib 1.29.206.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/colrowst.cxx
+++ sc/source/filter/excel/colrowst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: colrowst.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 11:40:25 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:47:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,12 +32,11 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 
-#pragma hdrstop
 
 #include "colrowst.hxx"
 
