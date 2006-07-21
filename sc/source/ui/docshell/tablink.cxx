2546d94beb2b60571a2fb9c2b55cb486c411feb7
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:46:08 +0000

    INTEGRATION: CWS pchfix01 (1.22.214); FILE MERGED 2006/07/12 10:02:35 kaib 1.22.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/docshell/tablink.cxx
+++ sc/source/ui/docshell/tablink.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tablink.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:50:48 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:46:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,17 +32,16 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 #ifdef WNT
 #pragma optimize("",off)
 #endif
 
 //------------------------------------------------------------------
 
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------
 
