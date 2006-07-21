4c65db9402856c7bfdc4ff035a0670b3277a22cd
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:08:43 +0000

    INTEGRATION: CWS pchfix01 (1.12.136); FILE MERGED 2006/07/12 10:01:36 kaib 1.12.136.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/data/stlpool.cxx
+++ sc/source/core/data/stlpool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stlpool.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:36:00 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:08:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 //------------------------------------------------------------------------
 
