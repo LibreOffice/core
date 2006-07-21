b27844abe7b948b87a1e46aa8b21d6e8449c37a0
Kurt Zenker <kz@openoffice.org>
2006-07-21 09:51:42 +0000

    INTEGRATION: CWS pchfix01 (1.57.8); FILE MERGED 2006/07/12 10:01:30 kaib 1.57.8.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/data/documen2.cxx
+++ sc/source/core/data/documen2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documen2.cxx,v $
  *
- *  $Revision: 1.57 $
+ *  $Revision: 1.58 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 11:54:20 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 10:51:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #define _BIGINT_HXX
 #define _SFXMULTISEL_HXX
