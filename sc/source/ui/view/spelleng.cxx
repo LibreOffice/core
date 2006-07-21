5383498b2f79d9e23c0b9166a09a612dab0fc319
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:08:40 +0000

    INTEGRATION: CWS pchfix01 (1.13.214); FILE MERGED 2006/07/12 10:03:09 kaib 1.13.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/spelleng.cxx
+++ sc/source/ui/view/spelleng.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spelleng.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-14 08:26:28 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:08:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 #include "spelleng.hxx"
 
 #ifndef _COM_SUN_STAR_I18N_TEXTCONVERSIONOPTION_HPP_
