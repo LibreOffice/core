8dc5331ba6cd4d253e7a2c39043dab45016b7561
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:41:30 +0000

    INTEGRATION: CWS pchfix01 (1.4.216); FILE MERGED 2006/07/12 10:02:57 kaib 1.4.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/unoobj/listenercalls.cxx
+++ sc/source/ui/unoobj/listenercalls.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listenercalls.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:47:44 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:41:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include <com/sun/star/util/XModifyListener.hpp>
 #include <tools/debug.hxx>
