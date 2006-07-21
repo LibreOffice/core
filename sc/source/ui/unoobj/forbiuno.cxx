bfb3247afec572f884400ce59562b28b5ee8ab32
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:39:40 +0000

    INTEGRATION: CWS pchfix01 (1.3.214); FILE MERGED 2006/07/12 10:02:57 kaib 1.3.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/unoobj/forbiuno.cxx
+++ sc/source/ui/unoobj/forbiuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: forbiuno.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:47:03 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:39:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 #include <svx/forbiddencharacterstable.hxx>
 
