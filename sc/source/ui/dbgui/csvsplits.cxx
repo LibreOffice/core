e9008c17329389d032342b3279609a1ae6d5ca73
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:20:11 +0000

    INTEGRATION: CWS pchfix01 (1.3.216); FILE MERGED 2006/07/12 10:02:26 kaib 1.3.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/dbgui/csvsplits.cxx
+++ sc/source/ui/dbgui/csvsplits.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: csvsplits.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:32:30 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:20:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,9 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 // ============================================================================
 
