e27960c5c21bdb040adc7f4654b15f79c51220bc
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:10:54 +0000

    INTEGRATION: CWS pchfix01 (1.18.216); FILE MERGED 2006/07/12 10:01:54 kaib 1.18.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xilink.cxx
+++ sc/source/filter/excel/xilink.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xilink.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:41:33 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:10:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XILINK_HXX
 #include "xilink.hxx"
