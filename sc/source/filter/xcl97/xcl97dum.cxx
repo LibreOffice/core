f451710529b688473a9cb695ec8c9285500fe1b9
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:36:54 +0000

    INTEGRATION: CWS pchfix01 (1.17.206); FILE MERGED 2006/07/12 10:02:04 kaib 1.17.206.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xcl97/xcl97dum.cxx
+++ sc/source/filter/xcl97/xcl97dum.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xcl97dum.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:06:02 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:36:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 
 #include "xcl97dum.hxx"
