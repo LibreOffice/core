6c31971a92ea9e3a7f280a5c60be9bf5f533d4c8
Kurt Zenker <kz@openoffice.org>
2006-07-21 14:01:02 +0000

    INTEGRATION: CWS pchfix01 (1.15.214); FILE MERGED 2006/07/12 10:03:05 kaib 1.15.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/gridwin5.cxx
+++ sc/source/ui/view/gridwin5.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gridwin5.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:59:13 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 15:01:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 // INCLUDE ---------------------------------------------------------------
 
