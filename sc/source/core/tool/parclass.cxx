4fb7ba56075a441f483d149bf9550ea30a196a7a
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:39:26 +0000

    INTEGRATION: CWS pchfix01 (1.4.216); FILE MERGED 2006/07/12 10:01:44 kaib 1.4.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/core/tool/parclass.cxx
+++ sc/source/core/tool/parclass.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parclass.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:47:03 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:38:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 #include "parclass.hxx"
 #include "token.hxx"
