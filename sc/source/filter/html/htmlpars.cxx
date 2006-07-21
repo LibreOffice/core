4b77a8793d6e425088e20710313f4ec04fbc653b
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:26:47 +0000

    INTEGRATION: CWS pchfix01 (1.27.84); FILE MERGED 2006/07/12 10:01:59 kaib 1.27.84.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/html/htmlpars.cxx
+++ sc/source/filter/html/htmlpars.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlpars.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 08:27:34 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:26:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include <boost/shared_ptr.hpp>
 
