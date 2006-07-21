4985c50a9799644cdf00b6ce546b5afef7aed381
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:14:37 +0000

    INTEGRATION: CWS pchfix01 (1.5.102); FILE MERGED 2006/07/12 10:01:55 kaib 1.5.102.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xiview.cxx
+++ sc/source/filter/excel/xiview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xiview.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-22 12:03:30 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:14:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XIVIEW_HXX
 #include "xiview.hxx"
