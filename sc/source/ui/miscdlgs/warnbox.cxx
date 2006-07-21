4c0a121a0c4e127e8fdc222b83c23cd2ded1bb2d
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:11:54 +0000

    INTEGRATION: CWS pchfix01 (1.4.216); FILE MERGED 2006/07/12 10:02:46 kaib 1.4.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/miscdlgs/warnbox.cxx
+++ sc/source/ui/miscdlgs/warnbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: warnbox.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:15:31 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:11:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_WARNBOX_HXX
 #include "warnbox.hxx"
