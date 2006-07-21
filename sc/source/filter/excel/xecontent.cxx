f22d2126f5ee1b0ba8c4fd9be568da1932debc24
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:52:59 +0000

    INTEGRATION: CWS pchfix01 (1.15.216); FILE MERGED 2006/07/12 10:01:50 kaib 1.15.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/excel/xecontent.cxx
+++ sc/source/filter/excel/xecontent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xecontent.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:30:53 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:52:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XECONTENT_HXX
 #include "xecontent.hxx"
