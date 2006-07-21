1374272f10f94b9f4e50efc8c7507c7b73b7cd6e
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:39:56 +0000

    INTEGRATION: CWS pchfix01 (1.46.56); FILE MERGED 2006/07/12 10:02:32 kaib 1.46.56.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/docshell/docsh4.cxx
+++ sc/source/ui/docshell/docsh4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docsh4.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:47:44 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:39:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,15 +33,14 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 #ifndef _COM_SUN_STAR_EMBED_XEMBEDDEDOBJECT_HPP_
 #include <com/sun/star/embed/XEmbeddedObject.hpp>
 #endif
 
-#pragma hdrstop
 
 using namespace ::com::sun::star;
 
