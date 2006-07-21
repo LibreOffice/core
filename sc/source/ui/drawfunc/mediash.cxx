3f02e9ff3d8eafa576b8420325920bdf2e81e75c
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:54:18 +0000

    INTEGRATION: CWS pchfix01 (1.4.62); FILE MERGED 2006/07/12 10:02:40 kaib 1.4.62.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/drawfunc/mediash.cxx
+++ sc/source/ui/drawfunc/mediash.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mediash.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:49:29 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:54:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include <sfx2/app.hxx>
 #include <sfx2/objface.hxx>
