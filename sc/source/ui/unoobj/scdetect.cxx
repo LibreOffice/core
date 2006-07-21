9dba085c25662a2fa53b047a46d40a313f26da7e
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:45:01 +0000

    INTEGRATION: CWS pchfix01 (1.17.138); FILE MERGED 2006/07/12 10:02:58 kaib 1.17.138.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/unoobj/scdetect.cxx
+++ sc/source/ui/unoobj/scdetect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scdetect.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 12:46:18 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:45:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include "scdetect.hxx"
 
