a2fcfba90765d7672ee2c32ad8567d8a95452bc4
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:06:03 +0000

    INTEGRATION: CWS pchfix01 (1.32.80); FILE MERGED 2006/07/12 10:02:17 kaib 1.32.80.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/Accessibility/AccessibleDocumentPagePreview.cxx
+++ sc/source/ui/Accessibility/AccessibleDocumentPagePreview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleDocumentPagePreview.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-31 09:31:27 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:06:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 
 #ifndef _SC_ACCESSIBLEDOCUMENTPAGEPREVIEW_HXX
