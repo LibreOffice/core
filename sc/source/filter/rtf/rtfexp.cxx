c6d1644bfb30cf5d70a45e6b15889bf7f0aef5fa
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:33:14 +0000

    INTEGRATION: CWS pchfix01 (1.5.214); FILE MERGED 2006/07/12 10:02:03 kaib 1.5.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/rtf/rtfexp.cxx
+++ sc/source/filter/rtf/rtfexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rtfexp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:45:04 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:33:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 
 
 #include "scitems.hxx"
