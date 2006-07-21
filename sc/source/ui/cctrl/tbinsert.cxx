f6d9d5dd8538871f5ee46d3a12e1019c34372d8c
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:16:48 +0000

    INTEGRATION: CWS pchfix01 (1.8.214); FILE MERGED 2006/07/12 10:02:25 kaib 1.8.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/cctrl/tbinsert.cxx
+++ sc/source/ui/cctrl/tbinsert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tbinsert.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:29:58 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:16:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,15 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 // System - Includes -----------------------------------------------------
 
 #include <string> // HACK: prevent conflict between STLPORT and Workshop headers
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
 
-#pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------------
 
