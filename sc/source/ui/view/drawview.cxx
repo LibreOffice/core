5292294e8d363639d3db4fff2c3a80e5e67286eb
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:53:58 +0000

    INTEGRATION: CWS pchfix01 (1.39.60); FILE MERGED 2006/07/12 10:03:03 kaib 1.39.60.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/view/drawview.cxx
+++ sc/source/ui/view/drawview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawview.cxx,v $
  *
- *  $Revision: 1.39 $
+ *  $Revision: 1.40 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:52:02 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:53:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 // INCLUDE ---------------------------------------------------------------
 
