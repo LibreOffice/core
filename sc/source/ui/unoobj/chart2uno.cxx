f29ba17d18489ec2571cd5990192a005fbfe82ee
Kurt Zenker <kz@openoffice.org>
2006-07-21 13:29:47 +0000

    INTEGRATION: CWS pchfix01 (1.4.212); FILE MERGED 2006/07/12 10:02:55 kaib 1.4.212.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/unoobj/chart2uno.cxx
+++ sc/source/ui/unoobj/chart2uno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chart2uno.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:43:06 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 14:29:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include "chart2uno.hxx"
 #include "miscuno.hxx"
