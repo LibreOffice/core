c58d8e41a254c19cbb4d9e1e76d5b951199addba
Kurt Zenker <kz@openoffice.org>
2006-07-21 12:06:14 +0000

    INTEGRATION: CWS pchfix01 (1.14.138); FILE MERGED 2006/07/12 10:02:17 kaib 1.14.138.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/ui/Accessibility/AccessibleEditObject.cxx
+++ sc/source/ui/Accessibility/AccessibleEditObject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleEditObject.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 13:03:28 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 13:06:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef _SC_ACCESSIBLEEDITOBJECT_HXX
 #include "AccessibleEditObject.hxx"
