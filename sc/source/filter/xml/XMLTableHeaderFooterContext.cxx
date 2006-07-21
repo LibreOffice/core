a8fe21a02e41b480e7fe700fa7bc2e9291de64fc
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:48:53 +0000

    INTEGRATION: CWS pchfix01 (1.10.214); FILE MERGED 2006/07/12 10:02:08 kaib 1.10.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/xml/XMLTableHeaderFooterContext.cxx
+++ sc/source/filter/xml/XMLTableHeaderFooterContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTableHeaderFooterContext.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:58:00 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:48:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #ifndef _COM_SUN_STAR_TEXT_XTEXT_HPP_
 #include <com/sun/star/text/XText.hpp>
