a7e1aeb15b9682c7075043612b0097f4817319cf
Kurt Zenker <kz@openoffice.org>
2006-07-21 10:46:34 +0000

    INTEGRATION: CWS pchfix01 (1.8.216); FILE MERGED 2006/07/12 10:01:47 kaib 1.8.216.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/dif/difexp.cxx
+++ sc/source/filter/dif/difexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: difexp.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:52:55 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 11:46:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,10 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
-#pragma hdrstop
+
 
 //------------------------------------------------------------------------
 
