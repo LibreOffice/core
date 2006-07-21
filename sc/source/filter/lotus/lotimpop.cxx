6c2cb7e9f9a657a25ad81de1b7d9d04c02495a18
Kurt Zenker <kz@openoffice.org>
2006-07-21 11:28:07 +0000

    INTEGRATION: CWS pchfix01 (1.10.214); FILE MERGED 2006/07/12 10:02:00 kaib 1.10.214.1: #i67080# Converted cxx files in sc, added initial project level pch and stripped old PCH definitions.

--- sc/source/filter/lotus/lotimpop.cxx
+++ sc/source/filter/lotus/lotimpop.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lotimpop.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:41:41 $
+ *  last change: $Author: kz $ $Date: 2006-07-21 12:28:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 #include "lotimpop.hxx"
 
 #ifndef _VOS_MUTEX_HXX_
