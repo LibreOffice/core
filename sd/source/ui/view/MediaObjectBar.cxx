939c3ea178e3175403040c62577c98aacb22a736
Rüdiger Timm <rt@openoffice.org>
2006-07-25 10:46:24 +0000

    INTEGRATION: CWS aw035 (1.5.114); FILE MERGED 2006/07/13 16:05:16 aw 1.5.114.1: #126320# SdrMark::GetObj() -> SdrMark::GetMarkedSdrObj() for isolating selection handling

--- sd/source/ui/view/MediaObjectBar.cxx
+++ sd/source/ui/view/MediaObjectBar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MediaObjectBar.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:38:00 $
+ *  last change: $Author: rt $ $Date: 2006-07-25 11:46:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -116,7 +116,7 @@ void MediaObjectBar::GetState( SfxItemSet& rSet )
 
             if( 1 == pMarkList->GetMarkCount() )
             {
-                SdrObject* pObj =pMarkList->GetMark( 0 )->GetObj();
+                SdrObject* pObj =pMarkList->GetMark( 0 )->GetMarkedSdrObj();
 
                 if( pObj && pObj->ISA( SdrMediaObj ) )
                 {
@@ -158,7 +158,7 @@ void MediaObjectBar::Execute( SfxRequest& rReq )
 
             if( 1 == pMarkList->GetMarkCount() )
             {
-                SdrObject* pObj = pMarkList->GetMark( 0 )->GetObj();
+                SdrObject* pObj = pMarkList->GetMark( 0 )->GetMarkedSdrObj();
 
                 if( pObj && pObj->ISA( SdrMediaObj ) )
                 {
