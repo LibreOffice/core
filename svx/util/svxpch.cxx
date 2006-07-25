a58736d6e514d890480429843ea47b3dec5178d2
Rüdiger Timm <rt@openoffice.org>
2006-07-25 08:30:22 +0000

    INTEGRATION: CWS rt15 (1.3.452); FILE MERGED 2006/06/29 11:25:41 rt 1.3.452.1: #i66824# segmentc.hxx is obsolete.

--- svx/util/svxpch.cxx
+++ svx/util/svxpch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxpch.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:24:41 $
+ *  last change: $Author: rt $ $Date: 2006-07-25 09:30:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -71,6 +71,4 @@
 #include <stdio.h>
 
 #include <sfx.hrc>
-
-#include "segmentc.hxx"
 
