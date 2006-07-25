14aeb2cc24c50580c7ae16f3bc4854786e87fd8b
Rüdiger Timm <rt@openoffice.org>
2006-07-25 08:59:51 +0000

    INTEGRATION: CWS calc37 (1.29.42); FILE MERGED 2006/07/10 14:26:00 dr 1.29.42.1: #i33126# remove cell merge selects merged area, patch by kendy

--- sc/source/ui/view/viewfun2.cxx
+++ sc/source/ui/view/viewfun2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewfun2.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 15:21:51 $
+ *  last change: $Author: rt $ $Date: 2006-07-25 09:59:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -870,10 +870,15 @@ BOOL ScViewFunc::RemoveMerge( BOOL bRecord )
     ScRange aRange;
     if (GetViewData()->GetSimpleArea( aRange ))
     {
+        ScRange aExtended( aRange );
+        GetViewData()->GetDocument()->ExtendMerge( aExtended );
         ScDocShell* pDocSh = GetViewData()->GetDocShell();
+
         HideCursor();
         BOOL bOk = pDocSh->GetDocFunc().UnmergeCells( aRange, bRecord, FALSE );
+        MarkRange( aExtended );
         ShowCursor();
+
         if (bOk)
             pDocSh->UpdateOle(GetViewData());
     }
