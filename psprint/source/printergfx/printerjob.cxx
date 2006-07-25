b399c9621c876122228697badb5e60502d712a0c
Rüdiger Timm <rt@openoffice.org>
2006-07-25 08:36:11 +0000

    INTEGRATION: CWS pj56 (1.37.14); FILE MERGED 2006/07/20 11:10:34 pjanik 1.37.14.1: #i62563#: Do not include deprecated unxmacxp_protos.h.

--- psprint/source/printergfx/printerjob.cxx
+++ psprint/source/printergfx/printerjob.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: printerjob.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:56:44 $
+ *  last change: $Author: rt $ $Date: 2006-07-25 09:36:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,9 +65,6 @@
 #endif
 #ifndef _OSL_THREAD_H_
 #include <osl/thread.h>
-#endif
-#ifdef MACOSX
-#include <unxmacxp_protos.h>
 #endif
 
 #include <stdio.h>
