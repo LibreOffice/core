c40103697665790e129d35f73d71ba04b39b33b5
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:53:54 +0000

    INTEGRATION: CWS dba204b (1.9.160); FILE MERGED 2006/07/07 12:29:56 fs 1.9.160.1: let the button navigation handler supersede ButtonType with ActionButtonType

--- extensions/source/propctrlr/cellbindinghelper.cxx
+++ extensions/source/propctrlr/cellbindinghelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cellbindinghelper.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:06:14 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:53:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -136,7 +136,7 @@ namespace pcr
         {
             sal_Unicode nUpper( _c );
             if ( _c >= 'a' && _c <= 'z' )
-                nUpper += 'A' - 'a';
+                nUpper += sal_Unicode( 'A' - 'a' );
             return nUpper;
         }
     }
