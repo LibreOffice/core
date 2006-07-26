c818d28396f1c4d3249948ea69a4b59bcf3420f5
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:52:03 +0000

    INTEGRATION: CWS dba204b (1.5.104); FILE MERGED 2006/07/11 14:12:42 fs 1.5.104.1: just touch this file, so it gets rebuilt (necessary due to an incompatibility in SolarThreadExecutor introduced with the fix for #i63262#

--- extensions/source/plugin/inc/plugin/win/sysplug.hxx
+++ extensions/source/plugin/inc/plugin/win/sysplug.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sysplug.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-20 12:59:03 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:52:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
