63e5166b4f5883adc2a7b2a7f2a2416ccba9c4e5
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:52:16 +0000

    INTEGRATION: CWS dba204b (1.11.68); FILE MERGED 2006/07/06 14:52:26 fs 1.11.68.1: #i65159# describePropertyLine now returning a LineDescriptor, instead of taking an out parameter / some less warnings

--- extensions/source/propctrlr/browserline.cxx
+++ extensions/source/propctrlr/browserline.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: browserline.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:17:00 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:52:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -467,7 +467,7 @@ namespace pcr
     }
 
     //------------------------------------------------------------------
-    IMPL_LINK( OBrowserLine, OnButtonFocus, PushButton*, pPB )
+    IMPL_LINK( OBrowserLine, OnButtonFocus, PushButton*, /*pPB*/ )
     {
         if ( m_xControl.is() )
         {
