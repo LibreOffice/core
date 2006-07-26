563cb8702b3186c5fe63b2c51fd946c6efe0b956
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:43:27 +0000

    INTEGRATION: CWS dba204b (1.12.94); FILE MERGED 2006/07/17 10:06:59 fs 1.12.94.1: warning-free code: ~OLocalExchangeHelper should be virtual

--- svx/source/inc/fmexch.hxx
+++ svx/source/inc/fmexch.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmexch.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-04 08:34:38 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:43:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -135,7 +135,7 @@ namespace svxform
 
     public:
         OLocalExchangeHelper( Window* _pDragSource );
-        ~OLocalExchangeHelper();
+        virtual ~OLocalExchangeHelper();
 
         void        prepareDrag( );
 
