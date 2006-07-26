8b8cf4f0fa77bc4115727a4fdedd635552a5cfdd
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:25:16 +0000

    INTEGRATION: CWS presfixes10 (1.7.6); FILE MERGED 2006/07/17 16:38:34 dbo 1.7.6.1: #i45197# dont run into busy loop when event queue is forced empty

--- slideshow/source/engine/eventmultiplexer.cxx
+++ slideshow/source/engine/eventmultiplexer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eventmultiplexer.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2005-10-11 08:32:19 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:25:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -255,7 +255,7 @@ void EventMultiplexer::scheduleTick()
 
     // enabled auto mode: simply schedule a timeout event,
     // which will eventually call our tick() method
-    mrEventQueue.addEvent( pEvent );
+    mrEventQueue.addEventForNextRound( pEvent );
 }
 
 void EventMultiplexer::handleTicks()
