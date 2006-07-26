2a616280b3a941e19f92419c3e139ce966a9f342
Rüdiger Timm <rt@openoffice.org>
2006-07-26 06:35:39 +0000

    INTEGRATION: CWS presfixes10 (1.4.8); FILE MERGED 2005/11/07 15:26:54 dbo 1.4.8.1: #i45197# revised code Issue number: Submitted by: Reviewed by:

--- slideshow/source/engine/animationnodes/generateevent.hxx
+++ slideshow/source/engine/animationnodes/generateevent.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: generateevent.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:44:36 $
+ *  last change: $Author: rt $ $Date: 2006-07-26 07:35:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,280 +33,38 @@
  *
  ************************************************************************/
 
-#ifndef _SLIDESHOW_GENERATEEVENT_HXX
-#define _SLIDESHOW_GENERATEEVENT_HXX
+#ifndef INCLUDED_SLIDESHOW_GENERATEEVENT_HXX
+#define INCLUDED_SLIDESHOW_GENERATEEVENT_HXX
 
-// must be first
-#include <canvas/debug.hxx>
-#include <canvas/verbosetrace.hxx>
+#include "slideshowcontext.hxx"
+#include "delayevent.hxx"
+#include "com/sun/star/uno/Any.hxx"
 
-#ifndef _COM_SUN_STAR_DRAWING_XSHAPE_HPP_
-#include <com/sun/star/drawing/XShape.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ANIMATIONS_XANIMATIONNODE_HPP_
-#include <com/sun/star/animations/XAnimationNode.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ANIMATIONS_TIMING_HPP_
-#include <com/sun/star/animations/Timing.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ANIMATIONS_EVENTTRIGGER_HPP_
-#include <com/sun/star/animations/EventTrigger.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ANIMATIONS_EVENT_HPP_
-#include <com/sun/star/animations/Event.hpp>
-#endif
+namespace presentation {
+namespace internal {
 
-#include <shape.hxx>
-#include <slideshowcontext.hxx>
-#include <delayevent.hxx>
+/** Create an event for the given description, calling the given functor.
 
+    @param rEventDescription
+    Directly from API
 
-namespace presentation
-{
-    namespace internal
-    {
-        /** Create an event for the given description, call the
-            given functor.
+    @param rFunctor
+    Functor to call when event fires.
 
-            @param rEventDescription
-            Directly from the API
+    @param rContext
+    Context struct, to provide event queue
 
-            @param rFunctor
-            Functor to call when event fires.
+    @param nAdditionalDelay
+    Additional delay, gets added on top of timeout.
+*/
+EventSharedPtr generateEvent(
+    ::com::sun::star::uno::Any const& rEventDescription,
+    Delay::FunctorT const& rFunctor,
+    SlideShowContext const& rContext,
+    double nAdditionalDelay );
 
-            @param rContext
-            Context struct, to provide event queue
+} // namespace internal
+} // namespace presentation
 
-            @param nAdditionalDelay
-            Additional delay, gets added on top of timeout.
-        */
-        template< typename Functor > void generateEvent( const ::com::sun::star::uno::Any&      rEventDescription,
-                                                         const Functor&                         rFunctor,
-                                                         const SlideShowContext&                rContext,
-                                                         double                                 nAdditionalDelay )
-        {
-            if( !rEventDescription.hasValue() )
-                return;
+#endif /* INCLUDED_SLIDESHOW_GENERATEEVENT_HXX */
 
-            ::com::sun::star::animations::Timing                            eTiming;
-            ::com::sun::star::animations::Event                             aEvent;
-            ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >   aSequence;
-            double                                                          nDelay;
-
-            if( (rEventDescription >>= eTiming) )
-            {
-                switch( eTiming )
-                {
-                    case ::com::sun::star::animations::Timing_INDEFINITE:
-                        return; // don't schedule no event
-
-                    case ::com::sun::star::animations::Timing_MEDIA:
-                        OSL_ENSURE( false,
-                                    "generateEvent(): MEDIA timing not yet implemented" );
-                        return;
-
-                    default:
-                        ENSURE_AND_THROW( false,
-                                          "generateEvent(): unexpected case" );
-                }
-            }
-            else if( (rEventDescription >>= aEvent) )
-            {
-                double nDelay(0.0);
-
-                // try to extract additional event delay
-                if( aEvent.Offset.hasValue() &&
-                    !(aEvent.Offset >>= nDelay) )
-                {
-                    OSL_ENSURE( false,
-                                "generateEvent(): offset values apart from <double> not "
-                                "recognized in animations::Event" );
-                }
-
-                // common vars used inside switch
-                ::com::sun::star::uno::Reference<
-                      ::com::sun::star::animations::XAnimationNode > xNode;
-                ::com::sun::star::uno::Reference<
-                      ::com::sun::star::drawing::XShape >            xShape;
-                ShapeSharedPtr                                       pShape;
-
-                // TODO(F1): Respect aEvent.Repeat value
-
-                switch( aEvent.Trigger )
-                {
-                    default:
-                        ENSURE_AND_THROW( false,
-                                          "generateEvent(): unexpected event trigger" );
-
-                    case ::com::sun::star::animations::EventTrigger::NONE:
-                        // no event at all
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_BEGIN:
-                        OSL_ENSURE( false,
-                                    "generateEvent(): event trigger ON_BEGIN not yet implemented" );
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_END:
-                        OSL_ENSURE( false,
-                                    "generateEvent(): event trigger ON_END not yet implemented" );
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::BEGIN_EVENT:
-                        // try to extract XAnimationNode event source
-                        if( (aEvent.Source >>= xNode) )
-                        {
-                            rContext.mrUserEventQueue.registerAnimationStartEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                xNode );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for BEGIN_EVENT");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::END_EVENT:
-                        // try to extract XAnimationNode event source
-                        if( (aEvent.Source >>= xNode) )
-                        {
-                            rContext.mrUserEventQueue.registerAnimationEndEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                xNode );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for END_EVENT");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_CLICK:
-                        // try to extract XShape event source
-                        if( (aEvent.Source >>= xShape) &&
-                            (pShape=rContext.mpLayerManager->lookupShape(xShape)).get() )
-                        {
-                            rContext.mrUserEventQueue.registerShapeClickEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                pShape );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for ON_CLICK");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_DBL_CLICK:
-                        // try to extract XShape event source
-                        if( (aEvent.Source >>= xShape) &&
-                            (pShape=rContext.mpLayerManager->lookupShape(xShape)).get() )
-                        {
-                            rContext.mrUserEventQueue.registerShapeDoubleClickEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                pShape );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for ON_DBL_CLICK");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_MOUSE_ENTER:
-                        // try to extract XShape event source
-                        if( (aEvent.Source >>= xShape) &&
-                            (pShape=rContext.mpLayerManager->lookupShape(xShape)).get() )
-                        {
-                            rContext.mrUserEventQueue.registerMouseEnterEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                pShape );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for ON_MOUSE_ENTER");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_MOUSE_LEAVE:
-                        // try to extract XShape event source
-                        if( (aEvent.Source >>= xShape) &&
-                            (pShape=rContext.mpLayerManager->lookupShape(xShape)).get() )
-                        {
-                            rContext.mrUserEventQueue.registerMouseLeaveEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                pShape );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for ON_MOUSE_LEAVE");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_PREV:
-                        OSL_ENSURE( false,
-                                    "generateEvent(): event trigger ON_PREV not yet implemented, mapped to ON_NEXT" );
-                        // FALLTHROUGH intended
-                    case ::com::sun::star::animations::EventTrigger::ON_NEXT:
-                        rContext.mrUserEventQueue.registerNextEffectEvent(
-                            makeDelay( rFunctor,
-                                       nDelay + nAdditionalDelay ) );
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::ON_STOP_AUDIO:
-                        // try to extract XAnimationNode event source
-                        if( (aEvent.Source >>= xNode) )
-                        {
-                            rContext.mrUserEventQueue.registerAudioStoppedEvent(
-                                makeDelay( rFunctor,
-                                           nDelay + nAdditionalDelay ),
-                                xNode );
-                        }
-                        else
-                        {
-                            OSL_ENSURE(false,
-                                       "generateEvent(): could not extract source XAnimationNode "
-                                       "for ON_STOP_AUDIO");
-                        }
-                        break;
-
-                    case ::com::sun::star::animations::EventTrigger::REPEAT:
-                        OSL_ENSURE( false,
-                                    "generateEvent(): event trigger REPEAT not yet implemented" );
-                        break;
-                }
-
-                return;
-            }
-            else if( (rEventDescription >>= aSequence) )
-            {
-                OSL_ENSURE( false,
-                            "generateEvent(): sequence of timing primitives not yet implemented" );
-                return;
-            }
-            else if( (rEventDescription >>= nDelay) )
-            {
-                // schedule delay event
-                rContext.mrEventQueue.addEvent( makeDelay( rFunctor,
-                                                           nDelay + nAdditionalDelay ) );
-            }
-        }
-    }
-}
-
-#endif /* _SLIDESHOW_GENERATEEVENT_HXX */
