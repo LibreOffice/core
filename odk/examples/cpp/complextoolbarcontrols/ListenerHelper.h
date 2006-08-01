935278b0eb78980adbe5dd906ffaf1387394c46c
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-01 08:41:53 +0000

    INTEGRATION: CWS odkaddonexample (1.1.2); FILE ADDED 2006/05/05 14:15:41 cd 1.1.2.1: #i65116# New demo add-on using the complex toolbar controls

---
+++ odk/examples/cpp/complextoolbarcontrols/ListenerHelper.h
@@ -0,0 +1,91 @@
+#include <vector>
+#include <hash_map>
+
+#ifndef _COM_SUN_STAR_FRAME_XFRAME_HPP_
+#include <com/sun/star/frame/XFrame.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XSTATUSLISTENER_HPP_
+#include <com/sun/star/frame/XStatusListener.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_FEATURESTATEEVENT_HPP_
+#include <com/sun/star/frame/FeatureStateEvent.hpp>
+#endif
+#ifndef _COM_SUN_STAR_FRAME_XDISPATCH_HPP_
+#include <com/sun/star/frame/XDispatch.hpp>
+#endif
+
+#include <rtl/ustring.hxx>
+#include <cppuhelper/implbase1.hxx>
+
+struct hashObjectName_Impl
+{
+    size_t operator()(const ::rtl::OUString Str) const
+    {
+        return (size_t)Str.hashCode();
+    }
+};
+
+struct eqObjectName_Impl
+{
+    sal_Bool operator()(const ::rtl::OUString Str1, const ::rtl::OUString Str2) const
+    {
+        return ( Str1 == Str2 );
+    }
+};
+
+typedef std::vector < com::sun::star::uno::Reference < com::sun::star::frame::XStatusListener > > StatusListeners;
+
+typedef std::hash_map
+<
+    ::rtl::OUString,
+    StatusListeners,
+    hashObjectName_Impl,
+    eqObjectName_Impl
+>
+ListenerMap;
+
+// For every frame there is *one* Dispatch object for all possible commands
+// this struct contains an array of listeners for every supported command
+// these arrays are accessed by a hash_map (with the command string as index)
+struct ListenerItem
+{
+    ListenerMap aContainer;
+    ::com::sun::star::uno::Reference< com::sun::star::frame::XDispatch > xDispatch;
+    ::com::sun::star::uno::Reference< com::sun::star::frame::XFrame > xFrame;
+};
+
+typedef std::vector < ListenerItem > AllListeners;
+
+class ListenerHelper
+{
+public:
+    void AddListener(
+        const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame,
+        const com::sun::star::uno::Reference < com::sun::star::frame::XStatusListener > xControl,
+        const ::rtl::OUString& aCommand );
+    void RemoveListener(
+        const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame,
+        const com::sun::star::uno::Reference < com::sun::star::frame::XStatusListener > xControl,
+        const ::rtl::OUString& aCommand );
+    void Notify(
+        const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame,
+        const ::rtl::OUString& aCommand,
+        com::sun::star::frame::FeatureStateEvent& rEvent );
+    com::sun::star::uno::Reference < com::sun::star::frame::XDispatch > GetDispatch(
+        const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame,
+        const ::rtl::OUString& aCommand );
+    void AddDispatch(
+        const com::sun::star::uno::Reference < com::sun::star::frame::XDispatch > xDispatch,
+        const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame,
+        const ::rtl::OUString& aCommand );
+};
+
+class ListenerItemEventListener : public cppu::WeakImplHelper1 < ::com::sun::star::lang::XEventListener >
+{
+    ::com::sun::star::uno::Reference< com::sun::star::frame::XFrame > mxFrame;
+public:
+    ListenerItemEventListener( const com::sun::star::uno::Reference < com::sun::star::frame::XFrame >& xFrame)
+        : mxFrame(xFrame)
+    {}
+    virtual void SAL_CALL disposing( const com::sun::star::lang::EventObject& aEvent ) throw (com::sun::star::uno::RuntimeException);
+};
