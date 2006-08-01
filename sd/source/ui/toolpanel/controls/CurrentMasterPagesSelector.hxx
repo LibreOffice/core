6f84c73bae9344d7e4ebaabd519159f4108715fc
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-01 08:22:20 +0000

    INTEGRATION: CWS impress99 (1.5.62); FILE MERGED 2006/07/05 09:28:52 af 1.5.62.1: #137393# Removed unused argument to constructor.

--- sd/source/ui/toolpanel/controls/CurrentMasterPagesSelector.hxx
+++ sd/source/ui/toolpanel/controls/CurrentMasterPagesSelector.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CurrentMasterPagesSelector.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:47:38 $
+ *  last change: $Author: ihi $ $Date: 2006-08-01 09:22:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,6 @@ public:
         TreeNode* pParent,
         SdDrawDocument& rDocument,
         ViewShellBase& rBase,
-        DrawViewShell& rViewShell,
         const ::boost::shared_ptr<MasterPageContainer>& rpContainer);
     virtual ~CurrentMasterPagesSelector (void);
 
