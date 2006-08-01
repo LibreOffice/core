0b4c23f2b5691e211de6d210959b63e7283a0406
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-01 08:35:31 +0000

    INTEGRATION: CWS odkaddonexample (1.2.10); FILE MERGED 2006/06/15 11:41:15 cd 1.2.10.2: #i65428# Fix build problem under Solaris Sparc 2006/05/16 10:50:25 cd 1.2.10.1: #i65116# Added new controls

--- framework/inc/uielement/comboboxtoolbarcontroller.hxx
+++ framework/inc/uielement/comboboxtoolbarcontroller.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: comboboxtoolbarcontroller.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-05-08 15:16:47 $
+ *  last change: $Author: ihi $ $Date: 2006-08-01 09:35:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,9 @@
 //  includes of other projects
 //_________________________________________________________________________________________________________________
 
+//#include <vcl/toolbox.hxx>
+//#include <vcl/combobox.hxx>
 #include <uielement/complextoolbarcontroller.hxx>
-#include <vcl/toolbox.hxx>
-#include <vcl/combobox.hxx>
 
 namespace framework
 {
