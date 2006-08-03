e7d297c874327fc2ad23a256ccc6944b4b021bfd
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-03 12:20:41 +0000

    INTEGRATION: CWS fpicker6 (1.6.74); FILE MERGED 2006/08/01 17:45:26 cmc 1.6.74.1: #i63263# multiselect support

--- fpicker/source/unx/gnome/SalGtkPicker.hxx
+++ fpicker/source/unx/gnome/SalGtkPicker.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SalGtkPicker.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:36:05 $
+ *  last change: $Author: ihi $ $Date: 2006-08-03 13:20:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,6 +46,10 @@
 
 #ifndef _COM_SUN_STAR_UI_DIALOGS_XFILEPICKER_HPP_
 #include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UI_DIALOGS_XFILEPICKERWORKAROUND_HPP_
+#include <com/sun/star/ui/dialogs/XFilePickerWorkaround.hpp>
 #endif
 
 #ifndef _COM_SUN_STAR_UI_XFOLDERPICKER_HPP_
