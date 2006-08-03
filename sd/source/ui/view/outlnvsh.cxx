b916be43c24ebf2d49b8ce1914873434d1877907
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-03 11:35:24 +0000

    INTEGRATION: CWS impress100 (1.80.68); FILE MERGED 2006/07/31 14:23:46 cl 1.80.68.1: #i63682# do not create undo actions for copy operation

--- sd/source/ui/view/outlnvsh.cxx
+++ sd/source/ui/view/outlnvsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outlnvsh.cxx,v $
  *
- *  $Revision: 1.80 $
+ *  $Revision: 1.81 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:08:34 $
+ *  last change: $Author: ihi $ $Date: 2006-08-03 12:35:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -624,7 +624,7 @@ void OutlineViewShell::FuSupport(SfxRequest &rReq)
         (nSlot == SID_TRANSLITERATE_HIRAGANA) ||
         (nSlot == SID_TRANSLITERATE_KATAGANA) ||
         (nSlot == SID_CUT) ||
-        (nSlot == SID_COPY) ||
+//      (nSlot == SID_COPY) ||
         (nSlot == SID_PASTE) ||
         (nSlot == SID_DELETE)))
     {
