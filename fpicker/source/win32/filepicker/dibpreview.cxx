1fbba53b0334e923f8d605e61f29bba8cc6ab000
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-03 11:31:57 +0000

    INTEGRATION: CWS impress100 (1.8.6); FILE MERGED 2006/07/31 13:58:01 cd 1.8.6.1: #i67880# Fix wrong debug only fix from obo

--- fpicker/source/win32/filepicker/dibpreview.cxx
+++ fpicker/source/win32/filepicker/dibpreview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dibpreview.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-11 07:15:28 $
+ *  last change: $Author: ihi $ $Date: 2006-08-03 12:31:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -286,12 +286,12 @@ void SAL_CALL CDIBPreview::onPaint(HWND hWnd, HDC hDC)
             int nX = abs(nWidth - cxDib) / 2;
             int nY = abs(nHeight - cyDib) / 2;
 
-#if OSL_DEBUG_LEVEL > 0
-            int GDIError = StretchDIBits(
+            int GDIError = GDI_ERROR;
+            GDIError = StretchDIBits(
                 hDC, nX, nY, cxDib, cyDib,
                 0, 0, cxDib, cyDib, pBits, pbmi,
                 DIB_RGB_COLORS, SRCCOPY);
-#endif
+
             OSL_ASSERT(GDI_ERROR != GDIError);
 
             // paint the border
