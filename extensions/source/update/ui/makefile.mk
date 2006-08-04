4bbf0b086a49fc28a9e17d23154051abf4ae61d8
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-04 08:57:14 +0000

    INTEGRATION: CWS onlineupdate3 (1.1.2); FILE ADDED 2006/07/26 10:10:07 dv 1.1.2.3: Renamed lib 2006/07/20 15:36:48 dv 1.1.2.2: Load MenuBar image from resource 2006/07/19 07:23:03 dv 1.1.2.1: Initial Version

---
+++ extensions/source/update/ui/makefile.mk
@@ -0,0 +1,86 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2006-08-04 09:57:14 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+PRJ=..$/..$/..
+
+PRJNAME=extensions
+TARGET=updchkui
+
+LIBTARGET=NO
+ENABLE_EXCEPTIONS=TRUE
+
+COMP1TYPELIST=$(TARGET)
+COMPRDB=$(SOLARBINDIR)$/types.rdb
+UNOUCROUT=$(OUT)$/inc$/$(TARGET)
+INCPRE=$(UNOUCROUT)
+
+# --- Settings ---
+
+.INCLUDE : settings.mk
+
+# --- Files ---
+
+SLOFILES=\
+    $(SLO)$/updatecheckui.obj
+
+SRS1NAME=$(TARGET)
+SRC1FILES=\
+    updatecheckui.src
+
+RESLIB1NAME=updchk
+RESLIB1IMAGES=$(PRJ)$/source$/update$/ui
+RESLIB1SRSFILES= $(SRS)$/updchkui.srs
+RESLIB1DEPN= updatecheckui.src updatecheckui.hrc
+
+SHL1NOCHECK=TRUE
+SHL1TARGET=updchk$(UPD)$(DLLPOSTFIX)
+SHL1OBJS=$(SLOFILES)
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+
+SHL1IMPLIB=i$(SHL1TARGET)
+SHL1STDLIBS=    \
+        $(CPPUHELPERLIB) \
+        $(CPPULIB) \
+        $(SALLIB) \
+        $(VCLLIB) \
+        $(TOOLSLIB)
+        
+SHL1VERSIONMAP=..$/exports.map
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+
+# --- Targets ---
+
+.INCLUDE : target.mk
+
