20d70983dfa8cd7459eae5fd32171d94b5eaed45
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-04 12:55:39 +0000

    INTEGRATION: CWS dba204c (1.13.28); FILE MERGED 2006/08/01 13:56:38 fs 1.13.28.2: #i10000# 2006/07/21 11:02:13 fs 1.13.28.1: during #i67635#: don't delete m_pColumns in ::disposing, but in the dtor only

--- dbaccess/source/core/api/preparedstatement.cxx
+++ dbaccess/source/core/api/preparedstatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: preparedstatement.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:39:29 $
+ *  last change: $Author: ihi $ $Date: 2006-08-04 13:55:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -91,13 +91,11 @@ OPreparedStatement::OPreparedStatement(const Reference< XConnection > & _xConn,
 //--------------------------------------------------------------------------
 OPreparedStatement::~OPreparedStatement()
 {
+    m_pColumns->acquire();
+    m_pColumns->disposing();
+    delete m_pColumns;
+
     DBG_DTOR(OPreparedStatement, NULL);
-    if ( m_pColumns )
-    {
-        m_pColumns->acquire();
-        m_pColumns->disposing();
-        delete m_pColumns;
-    }
 }
 
 // com::sun::star::lang::XTypeProvider
