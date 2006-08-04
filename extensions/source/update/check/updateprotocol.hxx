84b8eef53b8bbc0c422af3759b0266265edf5496
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-04 08:56:56 +0000

    INTEGRATION: CWS onlineupdate3 (1.1.2); FILE ADDED 2006/07/25 14:05:56 obr 1.1.2.4: #i66949# manual check implementation 2006/07/21 04:57:46 obr 1.1.2.3: #i66949# added config services to communicate with tools/options 2006/07/19 07:26:27 obr 1.1.2.2: #i66949# functional updates 2006/07/12 09:00:41 obr 1.1.2.1: #i66949# initial commit of update check task

---
+++ extensions/source/update/check/updateprotocol.hxx
@@ -0,0 +1,48 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: updateprotocol.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2006-08-04 09:56:56 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
+#include <com/sun/star/task/XInteractionHandler.hpp>
+#endif
+
+// Returns 'true' if successfully connected to the update server
+bool checkForUpdates(
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > const & rxContext,
+    ::com::sun::star::uno::Reference< ::com::sun::star::task::XInteractionHandler > const & rxInteractionHandler,
+    rtl::OUString& rUpdateURL, rtl::OUString& rVersionFound);
