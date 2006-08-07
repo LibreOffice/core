a878c4f639151b91b5c520f0dbbda324934cb935
Vladimir Glazounov <vg@openoffice.org>
2006-08-07 13:00:34 +0000

    #i10000# Deintegrate cws fpicker6

--- offapi/com/sun/star/ui/dialogs/makefile.mk
+++ offapi/com/sun/star/ui/dialogs/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: ihi $ $Date: 2006-08-03 13:21:25 $
+#   last change: $Author: vg $ $Date: 2006-08-07 13:58:36 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,6 @@ IDLFILES=\
     XFilePickerControlAccess.idl	\
     XFilePickerListener.idl\
     XFilePickerNotifier.idl\
-    XFilePickerWorkaround.idl\
     XFilePreview.idl\
     XFilterGroupManager.idl\
     XFilterManager.idl\
