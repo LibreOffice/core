16d2cb073de65d029dd174c2cf2a5a99aabcb686
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-11 16:45:57 +0000

    INTEGRATION: CWS svp01 (1.13.18); FILE MERGED 2006/06/27 12:49:18 pl 1.13.18.2: RESYNC: (1.13-1.14); FILE MERGED 2006/05/22 16:35:26 pl 1.13.18.1: #135265# free plugin abstraction of dependency on X11 specific SalData

--- vcl/unx/gtk/app/gtkinst.cxx
+++ vcl/unx/gtk/app/gtkinst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gtkinst.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:44:33 $
+ *  last change: $Author: hr $ $Date: 2006-08-11 17:45:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -100,7 +100,7 @@ void GtkHookedYieldMutex::release()
 
 extern "C"
 {
-    #define GET_YIELD_MUTEX() static_cast<GtkHookedYieldMutex*>(GetSalData()->pInstance_->GetYieldMutex())
+    #define GET_YIELD_MUTEX() static_cast<GtkHookedYieldMutex*>(GetSalData()->m_pInstance->GetYieldMutex())
     static void GdkThreadsEnter( void )
     {
         GtkHookedYieldMutex *pYieldMutex = GET_YIELD_MUTEX();
@@ -163,9 +163,9 @@ extern "C"
 #endif
 
         // initialize SalData
-        SalData *pSalData = new GtkData();
+        GtkData *pSalData = new GtkData();
         SetSalData( pSalData );
-        pSalData->pInstance_ = pInstance;
+        pSalData->m_pInstance = pInstance;
         pSalData->Init();
         pSalData->initNWF();
 
