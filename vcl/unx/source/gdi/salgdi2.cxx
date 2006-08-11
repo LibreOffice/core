f64af1a994c8b1a36dc46b030b441eabcc2842f7
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-11 16:51:37 +0000

    INTEGRATION: CWS svp01 (1.30.80); FILE MERGED 2006/06/27 12:59:10 pl 1.30.80.2: RESYNC: (1.30-1.31); FILE MERGED 2006/05/22 16:35:33 pl 1.30.80.1: #135265# free plugin abstraction of dependency on X11 specific SalData

--- vcl/unx/source/gdi/salgdi2.cxx
+++ vcl/unx/source/gdi/salgdi2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salgdi2.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:54:35 $
+ *  last change: $Author: hr $ $Date: 2006-08-11 17:51:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -397,7 +397,7 @@ void X11SalGraphics::YieldGraphicsExpose( Display* pDisplay, SalFrame* pFrame, D
     // get frame if necessary
     if( ! pFrame )
     {
-        const std::list< SalFrame* >& rFrames = GetSalData()->GetDisplay()->getFrames();
+        const std::list< SalFrame* >& rFrames = GetX11SalData()->GetDisplay()->getFrames();
         for( std::list< SalFrame* >::const_iterator it = rFrames.begin(); it != rFrames.end() && ! pFrame; ++it )
         {
             const SystemEnvData* pEnvData = (*it)->GetSystemData();
