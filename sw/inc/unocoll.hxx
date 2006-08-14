da4c83f3ea6df9434c61307faae47d389514a5ca
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:36:28 +0000

    INTEGRATION: CWS writercorehandoff (1.14.464); FILE MERGED 2006/03/17 07:54:48 tra 1.14.464.4: RESYNC: (1.16-1.17); FILE MERGED 2005/10/25 08:23:02 tra 1.14.464.3: RESYNC: (1.15-1.16); FILE MERGED 2005/09/13 12:25:58 tra 1.14.464.2: RESYNC: (1.14-1.15); FILE MERGED 2005/06/07 14:10:30 fme 1.14.464.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/inc/unocoll.hxx
+++ sw/inc/unocoll.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unocoll.hxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:32:43 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:36:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 #ifndef _UNOCOLL_HXX
 #define _UNOCOLL_HXX
-
 #ifndef _SOLAR_H
 #include <tools/solar.h>
 #endif
