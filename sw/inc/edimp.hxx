b6e75453023934d1a7070a879176ba84854403c1
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:21:02 +0000

    INTEGRATION: CWS writercorehandoff (1.1.1.1.1324); FILE MERGED 2005/09/13 11:23:28 tra 1.1.1.1.1324.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/07/13 13:23:42 fme 1.1.1.1.1324.1: #i50348# Make SwDoc accessible via interfaces

--- sw/inc/edimp.hxx
+++ sw/inc/edimp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edimp.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:43:26 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:21:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,10 +35,6 @@
 
 #ifndef _EDIMP_HXX
 #define _EDIMP_HXX
-
-#include "crsrsh.hxx"
-#include "doc.hxx"
-#include "viscrs.hxx"
 
 /*
  * MACROS um ueber alle Bereiche zu iterieren
