879c4fadbd17de3d88f9d55d66916011c7195233
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:42:53 +0000

    INTEGRATION: CWS writercorehandoff (1.4.156); FILE MERGED 2005/09/13 17:33:32 tra 1.4.156.2: RESYNC: (1.4-1.5); FILE MERGED 2005/06/07 14:15:45 fme 1.4.156.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/ui/inc/mailmergechildwindow.hxx
+++ sw/source/ui/inc/mailmergechildwindow.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mailmergechildwindow.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:39:55 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:42:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 #ifndef _SWMAILMERGECHILDWINDOW_HXX
 #define _SWMAILMERGECHILDWINDOW_HXX
-
 #ifndef _SFX_CHILDWIN_HXX
 #include <sfx2/childwin.hxx>
 #endif
@@ -50,8 +49,6 @@
 #ifndef INCLUDED_SWDLLAPI_H
 #include "swdllapi.h"
 #endif
-
-class SwWrtShell;
 
 /*-- 25.05.2004 15:25:56---------------------------------------------------
 
