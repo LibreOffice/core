e53ba8d2fa18d17c943df1c8e2cb4ef76e692865
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:22:33 +0000

    INTEGRATION: CWS writercorehandoff (1.3.1150); FILE MERGED 2005/09/13 11:25:48 tra 1.3.1150.2: RESYNC: (1.3-1.4); FILE MERGED 2005/06/07 14:09:52 fme 1.3.1150.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/inc/flddat.hxx
+++ sw/inc/flddat.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: flddat.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:45:45 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:22:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,8 @@
 #include "fldbas.hxx"
 
 class DateTime;
+class Date;
+class Time;
 
 /*--------------------------------------------------------------------
     Beschreibung: Formate
