df146321f786cef0f08df6c8dd6bd6487d7aec14
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:44:41 +0000

    INTEGRATION: CWS writercorehandoff (1.5.1286); FILE MERGED 2005/09/13 12:57:56 tra 1.5.1286.3: RESYNC: (1.5-1.6); FILE MERGED 2005/06/07 14:13:04 fme 1.5.1286.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:26:43 tra 1.5.1286.1: Unnecessary includes removed #i50348#

--- sw/source/core/access/accheaderfooter.hxx
+++ sw/source/core/access/accheaderfooter.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accheaderfooter.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:50:59 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:44:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,13 +34,8 @@
  ************************************************************************/
 #ifndef _ACCHEADERFOOTER_HXX
 #define _ACCHEADERFOOTER_HXX
-
 #ifndef _ACCCONTEXT_HXX
 #include "acccontext.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_RUNTIMEEXCEPTION_HPP_
-#include <com/sun/star/uno/RuntimeException.hpp>
 #endif
 
 class SwHeaderFrm;
