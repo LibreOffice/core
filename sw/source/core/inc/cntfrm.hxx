9bf42891e0e046983f5e65d8385a5157b49a84bf
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 15:18:20 +0000

    INTEGRATION: CWS writercorehandoff (1.6.744); FILE MERGED 2005/09/13 13:48:44 tra 1.6.744.2: RESYNC: (1.6-1.7); FILE MERGED 2005/06/07 14:13:55 fme 1.6.744.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/core/inc/cntfrm.hxx
+++ sw/source/core/inc/cntfrm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cntfrm.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:43:25 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 16:18:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,15 +34,12 @@
  ************************************************************************/
 #ifndef _CNTFRM_HXX
 #define _CNTFRM_HXX
-
 #include "frame.hxx"
 #include "flowfrm.hxx"
 #include "cshtyp.hxx"
 
 class SwLayoutFrm;
-class SwPageFrm;
 class SwCntntNode;
-struct SwCrsrMoveState;
 class SwBorderAttrs;
 class SwAttrSetChg;
 class SwTxtFrm;
