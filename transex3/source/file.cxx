5ff04fd18b002bc5d82b5b7b4b070f5dba360b91
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:10:01 +0000

    INTEGRATION: CWS sdfcreater (1.1.2); FILE ADDED 2006/07/28 12:23:40 ihi 1.1.2.2: #i61229# Sorted module names and file ; Skiped readlicense module 2006/03/30 13:15:50 ihi 1.1.2.1: #i61229# Use file find to search for l10n files

---
+++ transex3/source/file.cxx
@@ -0,0 +1,25 @@
+#include "file.hxx"
+
+namespace transex
+{
+
+File::File( const rtl::OUString sFile )
+{
+    sFileName = sFile;
+}
+
+File::File( rtl::OUString sFullPath , const rtl::OUString sFile )
+{
+    sFileName = sFile;
+    sFullName = sFullPath;
+}
+
+bool File::lessFile ( const File& rKey1, const File& rKey2 )
+{
+    rtl::OUString sName1( ( static_cast< File >( rKey1 ) ).getFileName() );
+    rtl::OUString sName2( ( static_cast< File >( rKey2 ) ).getFileName() );
+
+    return sName1.compareTo( sName2 ) < 0 ;
+}
+
+}
