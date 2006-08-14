e3a91e748e767c280655905c0ac515db840547cf
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:53:32 +0000

    INTEGRATION: CWS writercorehandoff (1.3.4); FILE MERGED 2006/01/19 08:49:56 fme 1.3.4.1: #i50348# General cleanup - removed unused header files, functions, members, declarations

--- sw/source/core/crsr/trvlreg.cxx
+++ sw/source/core/crsr/trvlreg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: trvlreg.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:07:28 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:53:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,7 @@ FASTBOOL GotoPrevRegion( SwPaM& rCurCrsr, SwPosRegion fnPosRegion,
 
     do {
         while( aIdx.GetIndex() &&
-            0 == ( pNd = aIdx.GetNode().FindStartNode()->GetSectionNode()) )
+            0 == ( pNd = aIdx.GetNode().StartOfSectionNode()->GetSectionNode()) )
             aIdx--;
 
         if( pNd )       // gibt einen weiteren SectionNode ?
