7208ceb5f612fe27ab0c5e9e3ff63b83421594a0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:40:22 +0000

    INTEGRATION: CWS writercorehandoff (1.4.558); FILE MERGED 2005/09/13 17:22:14 tra 1.4.558.2: RESYNC: (1.4-1.5); FILE MERGED 2005/06/07 14:15:42 fme 1.4.558.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/ui/inc/drwbassh.hxx
+++ sw/source/ui/inc/drwbassh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drwbassh.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:11:58 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:40:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,10 +34,8 @@
  ************************************************************************/
 #ifndef _SWDRWBASSH_HXX
 #define _SWDRWBASSH_HXX
-
 #include "basesh.hxx"
 
-class SwWrtShell;
 class SwView;
 class SfxItemSet;
 class SwDrawBase;
