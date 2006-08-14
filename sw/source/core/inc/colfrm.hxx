3605add0deae3569f985cb2c4de7b2bf6abca107
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 15:18:32 +0000

    INTEGRATION: CWS writercorehandoff (1.2.1302); FILE MERGED 2005/09/13 13:48:55 tra 1.2.1302.3: RESYNC: (1.2-1.3); FILE MERGED 2005/06/07 14:13:55 fme 1.2.1302.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:27:40 tra 1.2.1302.1: Unnecessary includes removed #i50348#

--- sw/source/core/inc/colfrm.hxx
+++ sw/source/core/inc/colfrm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: colfrm.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:43:38 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 16:18:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,13 +34,7 @@
  ************************************************************************/
 #ifndef _COLFRM_HXX
 #define _COLFRM_HXX
-
-#ifndef _SVMEMPOOL_HXX //autogen
-#include <tools/mempool.hxx>
-#endif
-
 #include "ftnboss.hxx"
-
 
 class SwColumnFrm: public SwFtnBossFrm
 {
