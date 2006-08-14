9182d2ef2dca646c1109429f8344d5da47f1633d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 15:31:37 +0000

    INTEGRATION: CWS writercorehandoff (1.6.530); FILE MERGED 2005/09/13 14:20:15 tra 1.6.530.3: RESYNC: (1.6-1.7); FILE MERGED 2005/06/07 14:14:11 fme 1.6.530.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:27:50 tra 1.6.530.1: Unnecessary includes removed #i50348#

--- sw/source/core/swg/SwXMLBlockImport.cxx
+++ sw/source/core/swg/SwXMLBlockImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXMLBlockImport.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:42:45 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 16:31:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,16 +41,12 @@
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include <xmloff/xmlnmspe.hxx>
 #endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include <xmloff/xmltoken.hxx>
-#endif
 #ifndef _SW_XMLBLOCKLIST_CONTEXT_HXX
 #include <SwXMLBlockListContext.hxx>
 #endif
 #ifndef _SW_XMLTEXTBLOCKS_HXX
 #include <SwXMLTextBlocks.hxx>
 #endif
-
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
