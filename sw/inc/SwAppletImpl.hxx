5a1e9d6da1135364a22bbf1c985566a6cd5ff86b
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:14:39 +0000

    INTEGRATION: CWS writercorehandoff (1.6.54); FILE MERGED 2005/09/13 11:09:31 tra 1.6.54.2: RESYNC: (1.6-1.7); FILE MERGED 2005/06/07 14:09:35 fme 1.6.54.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/inc/SwAppletImpl.hxx
+++ sw/inc/SwAppletImpl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwAppletImpl.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:28:54 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:14:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
