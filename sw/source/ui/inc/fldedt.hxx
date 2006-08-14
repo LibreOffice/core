75f7a506512cc07d43881bfe1eff07803204fdb0
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:41:13 +0000

    INTEGRATION: CWS writercorehandoff (1.1.1.1.1324); FILE MERGED 2005/09/13 17:24:02 tra 1.1.1.1.1324.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/06/07 14:15:43 fme 1.1.1.1.1324.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/ui/inc/fldedt.hxx
+++ sw/source/ui/inc/fldedt.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fldedt.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:13:24 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:41:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 #ifndef _FLDEDT_HXX
 #define _FLDEDT_HXX
-
 #ifndef _BASEDLGS_HXX //autogen
 #include <sfx2/basedlgs.hxx>
 #endif
@@ -43,8 +42,6 @@
 #include <vcl/imagebtn.hxx>
 #endif
 
-class Window;
-class SfxItemSet;
 class SwView;
 class SwWrtShell;
 
