f6deeff5d6483297b1008491a2478ac33f265c3d
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:15:20 +0000

    INTEGRATION: CWS writercorehandoff (1.2.1280); FILE MERGED 2005/09/13 11:13:09 tra 1.2.1280.3: RESYNC: (1.2-1.3); FILE MERGED 2005/06/07 14:09:37 fme 1.2.1280.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 07:07:20 tra 1.2.1280.1: Unnecessary includes removed #i50348#

--- sw/inc/accessibilityoptions.hxx
+++ sw/inc/accessibilityoptions.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accessibilityoptions.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:31:44 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:15:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,10 +34,6 @@
  ************************************************************************/
 #ifndef _ACCESSIBILITYOPTIONS_HXX
 #define _ACCESSIBILITYOPTIONS_HXX
-
-#ifndef _SOLAR_H
-#include <tools/solar.h>
-#endif
 
 struct SwAccessibilityOptions
 {
