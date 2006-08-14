d561cf0e3b272cc7e8a385be843e8887b2f740ac
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:40:08 +0000

    INTEGRATION: CWS writercorehandoff (1.1.1.1.1324); FILE MERGED 2005/09/13 17:21:53 tra 1.1.1.1.1324.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/06/07 14:15:42 fme 1.1.1.1.1324.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/ui/inc/drformsh.hxx
+++ sw/source/ui/inc/drformsh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drformsh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:11:31 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:40:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,13 +32,11 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 #ifndef _SWDRWFORMSH_HXX
 #define _SWDRWFORMSH_HXX
 
 #include "drwbassh.hxx"
 
-class SdrObject;
 class SwDrawBaseShell;
 
 class SwDrawFormShell: public SwDrawBaseShell
