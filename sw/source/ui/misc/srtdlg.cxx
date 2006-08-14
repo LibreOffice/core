4821199aed811443edf27dd6c38b6c39c2b16993
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:50:53 +0000

    INTEGRATION: CWS writercorehandoff (1.13.462); FILE MERGED 2005/09/13 18:12:47 tra 1.13.462.3: RESYNC: (1.13-1.14); FILE MERGED 2005/06/07 14:15:55 fme 1.13.462.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:29:51 tra 1.13.462.1: Unnecessary includes removed #i50348#

--- sw/source/ui/misc/srtdlg.cxx
+++ sw/source/ui/misc/srtdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srtdlg.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:41:11 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:50:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
@@ -84,9 +83,6 @@
 #endif
 #ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
-#endif
-#ifndef _SORTOPT_HXX
-#include <sortopt.hxx>
 #endif
 #ifndef _MISC_HRC
 #include <misc.hrc>
