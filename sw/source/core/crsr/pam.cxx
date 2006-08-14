8d5ce94d71050b4c9ab4f25497f4d4cbfe149b4c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:52:25 +0000

    INTEGRATION: CWS writercorehandoff (1.16.172); FILE MERGED 2005/09/13 13:12:12 tra 1.16.172.3: RESYNC: (1.16-1.17); FILE MERGED 2005/06/07 14:13:17 fme 1.16.172.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:26:56 tra 1.16.172.1: Unnecessary includes removed #i50348#

--- sw/source/core/crsr/pam.cxx
+++ sw/source/core/crsr/pam.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pam.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:05:49 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:52:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,6 @@
  *
  ************************************************************************/
 
-
 #pragma hdrstop
 
 #ifndef _HINTIDS_HXX
@@ -58,9 +57,6 @@
 #endif
 #ifndef _PAM_HXX
 #include <pam.hxx>
-#endif
-#ifndef _NODE_HXX
-#include <node.hxx>
 #endif
 #ifndef _PAMTYP_HXX
 #include <pamtyp.hxx>
