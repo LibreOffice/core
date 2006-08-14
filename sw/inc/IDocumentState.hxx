0f4e718d2011262e5f6ccbf1b2c4693b74e4ef05
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:13:41 +0000

    INTEGRATION: CWS writercorehandoff (1.1.2); FILE ADDED 2006/04/05 15:02:57 tra 1.1.2.2: IDocumentSettingAccess.hxx 2005/11/10 21:33:40 tra 1.1.2.1: #i50348#

---
+++ sw/inc/IDocumentState.hxx
@@ -0,0 +1,119 @@
+/*************************************************************************
+ *
+ *  $RCSfile: IDocumentState.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:13:41 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+ #ifndef IDOCUMENTSTATE_HXX_INCLUDED
+ #define IDOCUMENTSTATE_HXX_INCLUDED
+
+ /** Get information about the current document state
+ */
+ class IDocumentState
+ {
+ public:
+       /** Bei Formataenderungen muss das zu Fuss gerufen werden!
+    */
+    virtual void SetModified() = 0;
+
+    /**
+    */
+    virtual void ResetModified() = 0;
+
+    /** Dokumentaenderungen?
+    */
+    virtual bool IsModified() const = 0;
+
+    /** Zustaende ueber Ladezustand
+        frueher DocInfo
+    */
+    virtual bool IsLoaded() const = 0;
+
+    /**
+    */
+    virtual bool IsUpdateExpFld() const  = 0;
+
+    /**
+    */
+    virtual bool IsNewDoc() const = 0;
+
+    /**
+    */
+    virtual bool IsPageNums() const = 0;
+
+    /**
+    */
+    virtual void SetPageNums(bool b)    = 0;
+
+    /**
+    */
+    virtual void SetNewDoc(bool b) = 0;
+
+    /**
+    */
+    virtual void SetUpdateExpFldStat(bool b) = 0;
+
+    /**
+    */
+    virtual void SetLoaded(bool b = sal_True) = 0;
+
+ protected:
+    virtual ~IDocumentState() {};
+ };
+
+ #endif // IDOCUMENTSTATE_HXX_INCLUDED
