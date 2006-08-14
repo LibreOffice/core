b933c903cb75a330a4322fafd5f08f26010a15fd
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 17:00:44 +0000

    INTEGRATION: CWS writercorehandoff (1.4.726); FILE MERGED 2005/09/13 18:42:54 tra 1.4.726.3: RESYNC: (1.4-1.5); FILE MERGED 2005/06/07 14:16:06 fme 1.4.726.2: #i50348# General cleanup - removed unused header files, functions, members, declarations etc. 2005/06/06 09:30:10 tra 1.4.726.1: Unnecessary includes removed #i50348#

--- sw/source/ui/uno/unodoc.cxx
+++ sw/source/ui/uno/unodoc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unodoc.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:21:02 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 18:00:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,16 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 // System - Includes -----------------------------------------------------
 
 #include <tools/string.hxx>
 #include <sfx2/docfac.hxx>
-
-#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
-#include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#endif
-
 #include "swdll.hxx"
 #include "docsh.hxx"
 #include "globdoc.hxx"
