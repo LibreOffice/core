046f858bb13b6a5f8a0d7f4a48faf32e228efd89
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:25:15 +0000

    INTEGRATION: CWS writercorehandoff (1.5.326); FILE MERGED 2005/09/13 11:37:55 tra 1.5.326.2: RESYNC: (1.5-1.6); FILE MERGED 2005/06/07 14:10:01 fme 1.5.326.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/inc/hints.hxx
+++ sw/inc/hints.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hints.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:57:34 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 15:25:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -218,10 +218,6 @@ public:
 
     // wo es sich geaendert hat
     const SwAttrSet* GetTheChgdSet() const  { return pTheChgdSet; }
-
-    SfxItemState GetItemState( USHORT nWhich, BOOL bSrchInParent = TRUE,
-                               const SfxPoolItem **ppItem = 0 ) const
-    {   return pChgSet->GetItemState( nWhich, bSrchInParent, ppItem ); }
 
     USHORT Count() const { return pChgSet->Count(); }
     void ClearItem( USHORT nWhich = 0 )
