5aa713c4af31f4f727ef4f517ee3fc2a4cafa02c
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 16:06:50 +0000

    INTEGRATION: CWS writercorehandoff (1.3.252); FILE MERGED 2005/09/13 15:19:17 tra 1.3.252.2: RESYNC: (1.3-1.4); FILE MERGED 2005/06/07 14:14:53 fme 1.3.252.1: #i50348# General cleanup - removed unused header files, functions, members, declarations etc.

--- sw/source/filter/html/svxcss1.hxx
+++ sw/source/filter/html/svxcss1.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxcss1.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:47:28 $
+ *  last change: $Author: hr $ $Date: 2006-08-14 17:06:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 #ifndef _SVXCSS1_HXX
 #define _SVXCSS1_HXX
 
@@ -63,7 +62,6 @@
 class SfxItemPool;
 class SvxBoxItem;
 class FontList;
-class OutputDevice;
 
 /*  */
 
