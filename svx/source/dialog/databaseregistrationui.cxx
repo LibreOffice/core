d61c5bdb5d782d7a9de0398fcccddd06d78f7daa
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-15 09:37:41 +0000

    INTEGRATION: CWS dba30 (1.1.2); FILE ADDED 2006/03/31 13:33:51 fs 1.1.2.1: #i54504# function for administration the database registrations

---
+++ svx/source/dialog/databaseregistrationui.cxx
@@ -0,0 +1,86 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: databaseregistrationui.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2006-08-15 10:37:41 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// === includes (declaration) ============================================
+#ifndef SVX_DATABASE_REGISTRATION_UI_HXX
+#include "databaseregistrationui.hxx"
+#endif
+
+// === includes (UNO) ====================================================
+// === /includes (UNO) ===================================================
+
+// === includes (project) ================================================
+#ifndef _SVX_DIALOG_HXX
+#include "svxdlg.hxx"
+#endif
+#ifndef _SVX_DIALOGS_HRC
+#include "dialogs.hrc"
+#endif
+#ifndef _OFFAPP_CONNPOOLOPTIONS_HXX_
+#include "connpooloptions.hxx"
+#endif
+// === /includes (project) ===============================================
+
+// === includes (global) =================================================
+#ifndef _SFXAPP_HXX
+#include <sfx2/app.hxx>
+#endif
+#ifndef _SFXITEMSET_HXX
+#include <svtools/itemset.hxx>
+#endif
+#ifndef _SV_MSGBOX_HXX
+#include <vcl/msgbox.hxx>
+#endif
+// === /includes (global) ================================================
+
+namespace svx
+{
+    USHORT administrateDatabaseRegistration( Window* _parentWindow )
+    {
+        USHORT nResult = RET_CANCEL;
+
+        SfxItemSet aRegistrationItems( SFX_APP()->GetPool(), SID_SB_DB_REGISTER, SID_SB_DB_REGISTER, 0 );
+
+        SvxAbstractDialogFactory* pDialogFactory = SvxAbstractDialogFactory::Create();
+        ::std::auto_ptr< AbstractSfxSingleTabDialog > pDialog;
+        if ( pDialogFactory )
+            pDialog.reset( pDialogFactory->CreateSfxSingleTabDialog( _parentWindow, aRegistrationItems, NULL, RID_SFXPAGE_DBREGISTER ) );
+        if ( pDialog.get() )
+            nResult = pDialog->Execute();
+
+        return nResult;
+    }
+
+}   // namespace svx
