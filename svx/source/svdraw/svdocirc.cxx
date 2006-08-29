fd9f8e9c5157e9298688468772dff30cc366d91b
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-29 13:40:44 +0000

    INTEGRATION: CWS rt16 (1.25.86); FILE MERGED 2006/08/07 12:14:36 rt 1.25.86.1: #i68214# Remove empty file

--- svx/source/svdraw/svdocirc.cxx
+++ svx/source/svdraw/svdocirc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdocirc.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:41:29 $
+ *  last change: $Author: ihi $ $Date: 2006-08-29 14:40:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,7 +70,6 @@
 #include "svdpool.hxx"
 #include "svdattrx.hxx"
 #include "svdtrans.hxx"
-#include "svdio.hxx"
 #include "svdetc.hxx"
 #include "svddrag.hxx"
 #include "svdmodel.hxx"
