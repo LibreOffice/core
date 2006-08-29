f18c953d4c831fc42e8caca5b7fdd5a2b1b8622a
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-29 13:42:51 +0000

    INTEGRATION: CWS rt16 (1.11.86); FILE MERGED 2006/08/07 12:14:38 rt 1.11.86.1: #i68214# Remove empty file

--- svx/source/svdraw/svdotxln.cxx
+++ svx/source/svdraw/svdotxln.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdotxln.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:45:06 $
+ *  last change: $Author: ihi $ $Date: 2006-08-29 14:42:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,6 @@
 #include "svdotext.hxx"
 #include "svditext.hxx"
 #include "svdmodel.hxx"
-#include "svdio.hxx"
 #include "editdata.hxx"
 
 #ifndef SVX_LIGHT
