f6ced6b8c9c787cdd22d0da41ecd8feac100bec4
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-29 13:42:03 +0000

    INTEGRATION: CWS rt16 (1.19.322); FILE MERGED 2006/08/07 12:14:37 rt 1.19.322.1: #i68214# Remove empty file

--- svx/source/svdraw/svdopage.cxx
+++ svx/source/svdraw/svdopage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdopage.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-19 13:00:19 $
+ *  last change: $Author: ihi $ $Date: 2006-08-29 14:42:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,6 @@
 #include "svdstr.hrc"   // Objektname
 #include "svdtrans.hxx"
 #include "svdetc.hxx"
-#include "svdio.hxx"
 #include "svdxout.hxx"
 #include "svdmodel.hxx"
 #include "svdpage.hxx"
