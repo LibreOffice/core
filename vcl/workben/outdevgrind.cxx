d8c8bcf12ae7fc697ae20f1e0f0a396fe58ba781
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-29 10:07:03 +0000

    INTEGRATION: CWS warningfixes03_SRC680 (1.1.4.1.6); FILE MERGED 2006/08/18 12:51:25 mhu 1.1.4.1.6.1: #i68745# Fixed GCC 4.0.x warning(s).

--- vcl/workben/outdevgrind.cxx
+++ vcl/workben/outdevgrind.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outdevgrind.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:53:25 $
+ *  last change: $Author: ihi $ $Date: 2006-08-29 11:07:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -827,8 +827,8 @@ void grindFunc( OutputDevice&                       rTarget,
 
     fprintf( stdout,
              "Duration: %d ms (%d repetitions)\tOperation: %s\tSetup: %s\n",
-             osl_getGlobalTimer() - nStartTime,
-             nTurns,
+             (int)(osl_getGlobalTimer() - nStartTime),
+             (int)(nTurns),
              iter->first,
              pMsg );
 }
