bdf33f0a2b4f0e260b8fd46eb1c3c7ae1ad01f28
Ivo Hinkelmann <ihi@openoffice.org>
2006-08-29 13:29:55 +0000

    INTEGRATION: CWS rt16 (1.37.88); FILE MERGED 2006/08/08 11:44:39 rt 1.37.88.1: #i68214#

--- svx/source/dialog/srchdlg.cxx
+++ svx/source/dialog/srchdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srchdlg.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:29:17 $
+ *  last change: $Author: ihi $ $Date: 2006-08-29 14:29:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -106,7 +106,7 @@
 #define ITEMID_SEARCH       SID_ATTR_SEARCH
 #define ITEMID_SETITEM      0
 
-#include "srchitem.hxx"
+#include <sfx2/srchitem.hxx>
 #include "pageitem.hxx"
 #include "srchctrl.hxx"
 //CHINA001 #include "srchxtra.hxx"
