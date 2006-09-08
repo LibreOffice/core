0ee49b45e1acd8875d39180901ca9f290feeadeb
Vladimir Glazounov <vg@openoffice.org>
2006-09-08 11:45:08 +0000

    #100000# get rif of solaris warning

--- framework/inc/tabwin/tabwindow.hxx
+++ framework/inc/tabwin/tabwindow.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabwindow.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:31:41 $
+ *  last change: $Author: vg $ $Date: 2006-09-08 12:45:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -140,6 +140,9 @@ class TabWindow :  public ::com::sun::star::lang::XTypeProvider             ,
         FWK_DECLARE_XINTERFACE
         DECLARE_XSERVICEINFO
         FWK_DECLARE_XTYPEPROVIDER
+
+        using ::cppu::OPropertySetHelper::disposing;
+        using ::cppu::OPropertySetHelper::getFastPropertyValue;
 
         //---------------------------------------------------------------------------------------------------------
         //  XInitialization
