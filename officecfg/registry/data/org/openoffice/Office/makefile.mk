4a29d57741c99bc021a0f128bc9401d5dd45e6f3
Oliver Bolte <obo@openoffice.org>
2006-09-15 12:59:26 +0000

    INTEGRATION: CWS ause066 (1.13.30); FILE MERGED 2006/08/30 08:35:49 hjs 1.13.30.1: #i69087# - cleanup

--- officecfg/registry/data/org/openoffice/Office/makefile.mk
+++ officecfg/registry/data/org/openoffice/Office/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: obo $ $Date: 2006-07-13 11:48:15 $
+#   last change: $Author: obo $ $Date: 2006-09-15 13:59:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -36,13 +36,12 @@ PRJ=..$/..$/..$/..$/..
 
 PRJNAME=officecfg
 TARGET=data_ooOffice
+PACKAGE=org.openoffice.Office
 
 .INCLUDE :  settings.mk
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Targets ------------------------------------------------------
-
-PACKAGE=org.openoffice.Office
 
 XCUFILES= \
     Calc.xcu \
