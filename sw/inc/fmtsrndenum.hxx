05cb7f8471b2f247e1ee845ace98c38544602af1
Oliver Bolte <obo@openoffice.org>
2006-09-15 10:40:21 +0000

    INTEGRATION: CWS swqbf81 (1.1.2); FILE ADDED 2006/08/21 06:58:33 od 1.1.2.1: #i68520# - refactoring: new header file for enumeration <SwSurround>

---
+++ sw/inc/fmtsrndenum.hxx
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: fmtsrndenum.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-09-15 11:40:21 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _FMTSRNDENUM_HXX
+#define _FMTSRNDENUM_HXX
+
+enum SwSurround {
+    SURROUND_BEGIN,
+    SURROUND_NONE = SURROUND_BEGIN,
+    SURROUND_THROUGHT,
+    SURROUND_PARALLEL,
+    SURROUND_IDEAL,
+    SURROUND_LEFT,
+    SURROUND_RIGHT,
+    SURROUND_END
+};
+
+#endif
