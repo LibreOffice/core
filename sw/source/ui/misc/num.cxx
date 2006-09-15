fa98794227dbd5b606dd45b799ab9403055ce609
Oliver Bolte <obo@openoffice.org>
2006-09-15 11:56:46 +0000

    INTEGRATION: CWS os86 (1.13.28); FILE MERGED 2006/09/04 12:59:02 os 1.13.28.1: #i68431# missing initialization fixed

--- sw/source/ui/misc/num.cxx
+++ sw/source/ui/misc/num.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: num.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:49:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-15 12:56:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -179,7 +179,8 @@ SwNumPositionTabPage::SwNumPositionTabPage(Window* pParent,
     bInInintControl(FALSE),
     pActNum(0),
     pOutlineDlg(0),
-    pSaveNum(0)
+    pSaveNum(0),
+    bPreset( FALSE )
 {
     FreeResource();
     SetExchangeSupport();
