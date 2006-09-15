ff10e10361cefd6eba90b3281a4fb580e9f8552c
Oliver Bolte <obo@openoffice.org>
2006-09-15 11:05:43 +0000

    INTEGRATION: CWS impress102 (1.9.94); FILE MERGED 2006/08/23 12:04:08 sj 1.9.94.1: #i67457# invalidate itemset only once if setting the whole which range

--- svx/source/sdr/properties/customshapeproperties.cxx
+++ svx/source/sdr/properties/customshapeproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: customshapeproperties.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:29:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-15 12:05:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -123,9 +123,11 @@ namespace sdr
                 sal_uInt16 nWhich2 = aIter.FirstWhich();
                 while( nWhich2 )
                 {
-                    TextProperties::ClearObjectItem( nWhich2 );
+                    TextProperties::ClearObjectItemDirect( nWhich2 );
                     nWhich2 = aIter.NextWhich();
                 }
+                SfxItemSet aSet((SfxItemPool&)(*GetSdrObject().GetObjectItemPool()));
+                ItemSetChanged(aSet);
             }
             else
                 TextProperties::ClearObjectItem( nWhich );
