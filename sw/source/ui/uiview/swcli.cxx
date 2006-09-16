2ace929969f4f7c1927d3a37f02da273c1fe27d1
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:23:17 +0000

    INTEGRATION: CWS pchfix02 (1.9.476); FILE MERGED 2006/09/01 17:53:25 kaib 1.9.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/uiview/swcli.cxx
+++ sw/source/ui/uiview/swcli.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swcli.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:10:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:23:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifndef _COM_SUN_STAR_EMBED_NOVISUALAREASIZEEXCEPTION_HPP_
 #include <com/sun/star/embed/NoVisualAreaSizeException.hpp>
 #endif
 
-#pragma hdrstop
 
 #ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
