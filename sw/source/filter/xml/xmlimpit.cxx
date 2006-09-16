bbb94b609a7468039ccb4bb2edddc23ab5a7c9f4
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:29:35 +0000

    INTEGRATION: CWS pchfix02 (1.16.2); FILE MERGED 2006/09/01 17:52:47 kaib 1.16.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/xmlimpit.cxx
+++ sw/source/filter/xml/xmlimpit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlimpit.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:23:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:29:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "xmlimpit.hxx"
 #include <xmloff/xmluconv.hxx>
 
