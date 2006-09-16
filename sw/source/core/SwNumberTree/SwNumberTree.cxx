932dac77643f991381d278c1ac47b2cfd1c494dd
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:33:41 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:51:10 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- sw/source/core/SwNumberTree/SwNumberTree.cxx
+++ sw/source/core/SwNumberTree/SwNumberTree.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwNumberTree.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:40:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:33:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #include <algorithm>
 #include <functional>
