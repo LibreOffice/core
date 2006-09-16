ca1a5837bf49739ba5a76543aa8330355636a530
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:05:39 +0000

    INTEGRATION: CWS pchfix02 (1.9.26); FILE MERGED 2006/09/01 17:27:47 kaib 1.9.26.1: #i68856# Added header markers and pch files

--- forms/source/xforms/submission.cxx
+++ forms/source/xforms/submission.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: submission.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:05:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:05:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 #include "submission.hxx"
 
 #include "model.hxx"
