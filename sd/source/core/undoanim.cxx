af72368418c1b14864909d987b1da722bf9b6e99
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:17:49 +0000

    INTEGRATION: CWS pchfix02 (1.9.252); FILE MERGED 2006/09/01 17:36:44 kaib 1.9.252.1: #i68856# Added header markers and pch files

--- sd/source/core/undoanim.cxx
+++ sd/source/core/undoanim.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoanim.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-19 11:44:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:17:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_UTIL_XCLONEABLE_HPP_
 #include <com/sun/star/util/XCloneable.hpp>
