6a7e8bb7c0a214e3584f2c9a0f0fa0e1525df5ca
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:35:19 +0000

    INTEGRATION: CWS pchfix02 (1.8.474); FILE MERGED 2006/09/01 17:51:11 kaib 1.8.474.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accframebase.cxx
+++ sw/source/core/access/accframebase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accframebase.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:48:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:35:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLESTATETYPE_HPP_
 #include <com/sun/star/accessibility/AccessibleStateType.hpp>
