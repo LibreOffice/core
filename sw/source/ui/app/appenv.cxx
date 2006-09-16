56f1396f90de33fc7daf7bf4efe5e9a5e6f664c8
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:33:20 +0000

    INTEGRATION: CWS pchfix02 (1.27.2); FILE MERGED 2006/09/01 17:52:49 kaib 1.27.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/app/appenv.cxx
+++ sw/source/ui/app/appenv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: appenv.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:25:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:32:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #if STLPORT_VERSION>=321
 #include <cstdarg>
