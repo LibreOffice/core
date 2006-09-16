1e6c317fca5e547e78755a099b7ae1de686c4c55
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:20:29 +0000

    INTEGRATION: CWS pchfix02 (1.7.104); FILE MERGED 2006/09/01 17:37:26 kaib 1.7.104.1: #i68856# Added header markers and pch files

--- sd/source/ui/toolpanel/controls/RecentMasterPagesSelector.cxx
+++ sd/source/ui/toolpanel/controls/RecentMasterPagesSelector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RecentMasterPagesSelector.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:52:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:20:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "RecentMasterPagesSelector.hxx"
 #include "PreviewValueSet.hxx"
