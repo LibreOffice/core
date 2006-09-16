31b0ef36432115b24a9347fa160d732cf61de3c2
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:54:33 +0000

    INTEGRATION: CWS pchfix02 (1.4.70); FILE MERGED 2006/09/01 17:29:07 kaib 1.4.70.1: #i68856# Added header markers and pch files

--- framework/source/dispatch/interceptionhelper.cxx
+++ framework/source/dispatch/interceptionhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interceptionhelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-05-08 14:43:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:54:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_______________________________________________
 //  my own includes
