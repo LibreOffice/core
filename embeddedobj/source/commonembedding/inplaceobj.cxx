9e44ff2e2551f2dbcfd8282a28f3c91e0a8eba4c
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:40:38 +0000

    INTEGRATION: CWS pchfix02 (1.4.18); FILE MERGED 2006/09/01 17:25:50 kaib 1.4.18.1: #i68856# Added header markers and pch files

--- embeddedobj/source/commonembedding/inplaceobj.cxx
+++ embeddedobj/source/commonembedding/inplaceobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inplaceobj.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:28:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:40:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_EMBEDSTATES_HPP_
 #include <com/sun/star/embed/EmbedStates.hpp>
