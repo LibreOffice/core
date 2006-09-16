a5c00da99f0c5253c478bae2d4f720c9e92a485e
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:38:18 +0000

    INTEGRATION: CWS pchfix02 (1.7.282); FILE MERGED 2006/09/01 17:36:58 kaib 1.7.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/dlgsnap.cxx
+++ sd/source/ui/dlg/dlgsnap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgsnap.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:59:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:38:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #include <svx/svxids.hrc>
 #include <svx/dlgutil.hxx>
