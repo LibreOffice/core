16073dd2c4c7961c75fc099049ea7a3375447113
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:30:04 +0000

    INTEGRATION: CWS pchfix02 (1.7.28); FILE MERGED 2006/09/01 17:37:32 kaib 1.7.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/GraphicObjectBar.cxx
+++ sd/source/ui/view/GraphicObjectBar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GraphicObjectBar.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:46:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:30:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "GraphicObjectBar.hxx"
 
@@ -67,7 +70,6 @@
 #include <svx/grafctrl.hxx>
 #endif
 
-#pragma hdrstop
 
 #include <sfx2/objface.hxx>
 
