10fa07ebf8110a353d4da44b66b92ef384817b70
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:28:10 +0000

    INTEGRATION: CWS pchfix02 (1.82.2); FILE MERGED 2006/09/01 17:52:46 kaib 1.82.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/xmlexp.cxx
+++ sw/source/filter/xml/xmlexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlexp.cxx,v $
  *
- *  $Revision: 1.82 $
+ *  $Revision: 1.83 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:21:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:28:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _COM_SUN_STAR_TEXT_XTEXTDOCUMENT_HPP_
 #include <com/sun/star/text/XTextDocument.hpp>
