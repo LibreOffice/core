1e5344ca607681d7c535b9086d824b477d0df9b7
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:45:44 +0000

    INTEGRATION: CWS pchfix02 (1.84.2); FILE MERGED 2006/09/01 17:52:11 kaib 1.84.2.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/fntcache.cxx
+++ sw/source/core/txtnode/fntcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fntcache.cxx,v $
  *
- *  $Revision: 1.84 $
+ *  $Revision: 1.85 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:46:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:45:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _OUTDEV_HXX //autogen
 #include <vcl/outdev.hxx>
