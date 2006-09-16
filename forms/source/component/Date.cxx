e58b3e0e6bb305cd265b70223641bda5dbb9a39b
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:48:14 +0000

    INTEGRATION: CWS pchfix02 (1.22.18); FILE MERGED 2006/09/01 17:27:34 kaib 1.22.18.1: #i68856# Added header markers and pch files

--- forms/source/component/Date.cxx
+++ forms/source/component/Date.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Date.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 15:18:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:48:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FORMS_DATE_HXX_
 #include "Date.hxx"
