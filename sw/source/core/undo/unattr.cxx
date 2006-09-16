56f0ef596723d8e8aa0bf37bd042847a356137aa
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:50:20 +0000

    INTEGRATION: CWS pchfix02 (1.10.2); FILE MERGED 2006/09/01 17:52:15 kaib 1.10.2.1: #i68856# Added header markers and pch files

--- sw/source/core/undo/unattr.cxx
+++ sw/source/core/undo/unattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unattr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:48:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:50:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define _SVSTDARR_USHORTS
 #define _SVSTDARR_USHORTSSORT
