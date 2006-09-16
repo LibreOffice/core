84862874d79e2914095074d4dc970e0ba50e0d60
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:11:28 +0000

    INTEGRATION: CWS pchfix02 (1.5.2); FILE MERGED 2006/09/01 17:53:17 kaib 1.5.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/dselect.cxx
+++ sw/source/ui/ribbar/dselect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dselect.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:52:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:11:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SFX_BINDINGS_HXX //autogen
 #include <sfx2/bindings.hxx>
