a505e71af6f6c14918e82b45c8e9a0a2379459b7
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:26:44 +0000

    INTEGRATION: CWS pchfix02 (1.9.8); FILE MERGED 2006/09/01 17:35:34 kaib 1.9.8.1: #i68856# Added header markers and pch files

--- scripting/source/dlgprov/dlgevtatt.cxx
+++ scripting/source/dlgprov/dlgevtatt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgevtatt.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-14 07:09:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:26:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_scripting.hxx"
 
 #ifndef SCRIPTING_DLGEVTATT_HXX
 #include "dlgevtatt.hxx"
