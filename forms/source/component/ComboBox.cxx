5bd462a0774d3ac44e444465d7f44892f9594ca8
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:47:34 +0000

    INTEGRATION: CWS pchfix02 (1.33.16); FILE MERGED 2006/09/01 17:27:33 kaib 1.33.16.1: #i68856# Added header markers and pch files

--- forms/source/component/ComboBox.cxx
+++ forms/source/component/ComboBox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ComboBox.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:43:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:47:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FORMS_COMBOBOX_HXX_
 #include "ComboBox.hxx"
