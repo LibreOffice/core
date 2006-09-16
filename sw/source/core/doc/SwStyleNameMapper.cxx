efd2b539afd0cb1ff1f2f5b4c32f97bcc78b553c
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:48:51 +0000

    INTEGRATION: CWS pchfix02 (1.11.476); FILE MERGED 2006/09/01 17:51:21 kaib 1.11.476.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/SwStyleNameMapper.cxx
+++ sw/source/core/doc/SwStyleNameMapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwStyleNameMapper.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:08:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:48:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SWSTYLENAMEMAPPER_HXX
 #include <SwStyleNameMapper.hxx>
