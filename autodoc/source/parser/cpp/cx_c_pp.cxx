8a2155c8898c5a41248a87af391018eaaa244b3b
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:59:10 +0000

    INTEGRATION: CWS pchfix02 (1.5.30); FILE MERGED 2006/09/01 17:15:41 kaib 1.5.30.1: #i68856# Added header markers and pch files

--- autodoc/source/parser/cpp/cx_c_pp.cxx
+++ autodoc/source/parser/cpp/cx_c_pp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cx_c_pp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:17:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:59:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include "cx_c_pp.hxx"
