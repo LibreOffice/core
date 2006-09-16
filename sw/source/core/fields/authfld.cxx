be36b30ecd87e1568495b964e54bbe97c1e59a5d
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:09:39 +0000

    INTEGRATION: CWS pchfix02 (1.27.2); FILE MERGED 2006/09/01 17:51:40 kaib 1.27.2.1: #i68856# Added header markers and pch files

--- sw/source/core/fields/authfld.cxx
+++ sw/source/core/fields/authfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: authfld.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:11:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define _SVSTDARR_STRINGSDTOR
 #define _SVSTDARR_USHORTS
