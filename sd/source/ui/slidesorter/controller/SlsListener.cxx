0d953f8fecf7a6d83eea1793c98f27496895f09a
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:06:15 +0000

    INTEGRATION: CWS pchfix02 (1.12.144); FILE MERGED 2006/09/01 17:37:17 kaib 1.12.144.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/controller/SlsListener.cxx
+++ sd/source/ui/slidesorter/controller/SlsListener.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsListener.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:30:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:06:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 #include "SlsListener.hxx"
 
 #include "SlideSorterViewShell.hxx"
