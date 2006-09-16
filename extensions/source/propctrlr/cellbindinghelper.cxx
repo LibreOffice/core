32f20f45df10b41091442d8da1a7358c6efe9d11
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:14:23 +0000

    INTEGRATION: CWS pchfix02 (1.10.22); FILE MERGED 2006/09/01 17:26:39 kaib 1.10.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/cellbindinghelper.cxx
+++ extensions/source/propctrlr/cellbindinghelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cellbindinghelper.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:53:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:14:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef EXTENSIONS_PROPCTRLR_CELLBINDINGHELPER_HXX
 #include "cellbindinghelper.hxx"
