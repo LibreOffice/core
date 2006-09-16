5557914439ab5f5ff69a8632578542c0cd7c30e2
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:01:52 +0000

    INTEGRATION: CWS pchfix02 (1.21.2); FILE MERGED 2006/09/01 17:51:32 kaib 1.21.2.1: #i68856# Added header markers and pch files

--- sw/source/core/docnode/nodes.cxx
+++ sw/source/core/docnode/nodes.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: nodes.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:05:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:01:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #include <stdlib.h>
 
