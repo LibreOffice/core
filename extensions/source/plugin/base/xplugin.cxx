c1d0c7d318cc7bd227f879028c3009a3033a2c85
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:09:27 +0000

    INTEGRATION: CWS pchfix02 (1.18.92); FILE MERGED 2006/09/01 17:26:35 kaib 1.18.92.1: #i68856# Added header markers and pch files

--- extensions/source/plugin/base/xplugin.cxx
+++ extensions/source/plugin/base/xplugin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xplugin.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-16 13:00:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:09:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
