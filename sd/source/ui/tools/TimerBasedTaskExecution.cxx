c10084ac7b4e04fa9045e68302aa117ffd8625b4
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:22:49 +0000

    INTEGRATION: CWS pchfix02 (1.2.104); FILE MERGED 2006/09/01 17:37:27 kaib 1.2.104.1: #i68856# Added header markers and pch files

--- sd/source/ui/tools/TimerBasedTaskExecution.cxx
+++ sd/source/ui/tools/TimerBasedTaskExecution.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TimerBasedTaskExecution.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:54:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:22:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "tools/TimerBasedTaskExecution.hxx"
 #include "tools/AsynchronousTask.hxx"
