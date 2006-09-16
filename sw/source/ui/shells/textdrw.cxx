634ac3ae7d1320b6f446eb0d3412412e038ce9a9
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:16:53 +0000

    INTEGRATION: CWS pchfix02 (1.10.48); FILE MERGED 2006/09/01 17:53:20 kaib 1.10.48.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/textdrw.cxx
+++ sw/source/ui/shells/textdrw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textdrw.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:44:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:16:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SVDVIEW_HXX //autogen
 #include <svx/svdview.hxx>
