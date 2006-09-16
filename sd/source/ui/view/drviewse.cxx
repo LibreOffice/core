08d1adfda3daeca2c1eb6fa151da3f40170fdf58
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:39:08 +0000

    INTEGRATION: CWS pchfix02 (1.60.24); FILE MERGED 2006/09/01 17:37:38 kaib 1.60.24.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/drviewse.cxx
+++ sd/source/ui/view/drviewse.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drviewse.cxx,v $
  *
- *  $Revision: 1.60 $
+ *  $Revision: 1.61 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:25:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:39:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "DrawViewShell.hxx"
 #include "slideshow.hxx"
@@ -133,7 +136,6 @@
 
 #include <sfx2/ipclient.hxx>
 
-#pragma hdrstop
 
 #include <svtools/urihelper.hxx>
 #include <sfx2/topfrm.hxx>
