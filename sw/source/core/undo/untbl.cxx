78c0c573b89c96f96f2ceb9ad3872c3db54181e1
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:54:08 +0000

    INTEGRATION: CWS pchfix02 (1.23.2); FILE MERGED 2006/09/01 17:52:18 kaib 1.23.2.1: #i68856# Added header markers and pch files

--- sw/source/core/undo/untbl.cxx
+++ sw/source/core/undo/untbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: untbl.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:52:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:54:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
