7ca7342f47ca62702e6832a4a2ef266763cb5f63
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:29:12 +0000

    INTEGRATION: CWS pchfix02 (1.4.6); FILE MERGED 2006/09/01 17:15:23 kaib 1.4.6.1: #i68856# Added header markers and pch files

--- autodoc/source/ary/idl/it_tplparam.cxx
+++ autodoc/source/ary/idl/it_tplparam.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: it_tplparam.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:53:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:29:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
