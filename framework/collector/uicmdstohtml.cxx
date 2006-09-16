42c72128749a2cafee5d5e3318f45f3893632f86
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:39:47 +0000

    INTEGRATION: CWS pchfix02 (1.4.202); FILE MERGED 2006/09/01 17:28:56 kaib 1.4.202.1: #i68856# Added header markers and pch files

--- framework/collector/uicmdstohtml.cxx
+++ framework/collector/uicmdstohtml.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uicmdstohtml.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:55:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:39:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #include <sal/main.h>
 #include <osl/file.hxx>
