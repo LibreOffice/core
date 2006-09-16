96abdcee90617f4aaa804c230fa26bd0ef0d9398
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:32:52 +0000

    INTEGRATION: CWS pchfix02 (1.31.2); FILE MERGED 2006/09/01 17:53:32 kaib 1.31.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/glbltree.cxx
+++ sw/source/ui/utlui/glbltree.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: glbltree.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:03:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:32:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifndef _SVSTDARR_HXX
 #define _SVSTDARR_STRINGSDTOR
 #include <svtools/svstdarr.hxx>
