178f4d8d1871f8687863223a2d1ea200de9487d4
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:18:57 +0000

    INTEGRATION: CWS pchfix02 (1.3.34); FILE MERGED 2006/09/01 17:56:05 kaib 1.3.34.1: #i68856# Added header markers and pch files

--- ucbhelper/source/client/activedatastreamer.cxx
+++ ucbhelper/source/client/activedatastreamer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: activedatastreamer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:33:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:18:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucbhelper.hxx"
 
 /**************************************************************************
                                 TODO
