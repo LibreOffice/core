2087d958951b6ff47127a19de3d8135cf88cdb10
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:44:47 +0000

    INTEGRATION: CWS pchfix02 (1.5.476); FILE MERGED 2006/09/01 17:52:10 kaib 1.5.476.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/atrtox.cxx
+++ sw/source/core/txtnode/atrtox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atrtox.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:11:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:44:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _DOC_HXX
 #include <doc.hxx>
