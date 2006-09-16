5b62848920eb5ff48d9f4bd757107d4c34c3ec76
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:04:46 +0000

    INTEGRATION: CWS pchfix02 (1.40.2); FILE MERGED 2006/09/01 17:51:36 kaib 1.40.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edattr.cxx
+++ sw/source/core/edit/edattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edattr.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:07:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:04:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
