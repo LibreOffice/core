1445b40eb8375af290ebfb2281568adcbcd08862
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:26:22 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:37:30 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/unokywds.cxx
+++ sd/source/ui/unoidl/unokywds.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unokywds.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:50:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:26:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SD_UNOKYWDS_HXX_
 #define SD_DEFINE_KEYWORDS
