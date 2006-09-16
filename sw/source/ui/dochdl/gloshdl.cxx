5ba2538ca24789e536ade653cd60a854665a548f
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:51:35 +0000

    INTEGRATION: CWS pchfix02 (1.23.2); FILE MERGED 2006/09/01 17:53:02 kaib 1.23.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/dochdl/gloshdl.cxx
+++ sw/source/ui/dochdl/gloshdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gloshdl.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:33:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:51:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
 #endif
