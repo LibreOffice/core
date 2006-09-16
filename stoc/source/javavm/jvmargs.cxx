a92cf1e872c2167d87337911c48f103f608b18f9
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:31:17 +0000

    INTEGRATION: CWS pchfix02 (1.16.36); FILE MERGED 2006/09/01 17:41:13 kaib 1.16.36.1: #i68856# Added header markers and pch files

--- stoc/source/javavm/jvmargs.cxx
+++ stoc/source/javavm/jvmargs.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: jvmargs.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:59:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:31:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 #include "jvmargs.hxx"
 #include <rtl/ustring.hxx>
