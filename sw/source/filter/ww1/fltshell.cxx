9291d6dfcb5467235fa0866c2d767c209575cd59
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:18:53 +0000

    INTEGRATION: CWS pchfix02 (1.19.2); FILE MERGED 2006/09/01 17:52:37 kaib 1.19.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/ww1/fltshell.cxx
+++ sw/source/filter/ww1/fltshell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fltshell.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:13:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:18:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #include <ctype.h>
 
