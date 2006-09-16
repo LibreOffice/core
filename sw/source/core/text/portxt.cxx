39e2684e33ba3a63af9d0f18da91f8336498ae69
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:39:33 +0000

    INTEGRATION: CWS pchfix02 (1.43.2); FILE MERGED 2006/09/01 17:52:06 kaib 1.43.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/portxt.cxx
+++ sw/source/core/text/portxt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: portxt.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:42:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:39:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <ctype.h>
 
