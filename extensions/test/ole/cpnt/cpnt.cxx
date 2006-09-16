e164239110a9396bf2ce3ef0dba1d50b96331cca
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:32:29 +0000

    INTEGRATION: CWS pchfix02 (1.15.78); FILE MERGED 2006/09/01 17:26:51 kaib 1.15.78.1: #i68856# Added header markers and pch files

--- extensions/test/ole/cpnt/cpnt.cxx
+++ extensions/test/ole/cpnt/cpnt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cpnt.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 11:59:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:32:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #include <windows.h>
 #include <atlbase.h>
 #include <stdio.h>
