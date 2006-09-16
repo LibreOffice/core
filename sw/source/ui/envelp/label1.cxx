4f9ff041af16a46f773d3f09f6cb450db2f1d2f3
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:54:43 +0000

    INTEGRATION: CWS pchfix02 (1.34.2); FILE MERGED 2006/09/01 17:53:04 kaib 1.34.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/label1.cxx
+++ sw/source/ui/envelp/label1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: label1.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:35:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:54:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,12 +32,14 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #ifndef _SV_WAITOBJ_HXX //autogen
 #include <vcl/waitobj.hxx>
