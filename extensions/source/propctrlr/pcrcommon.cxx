2545a611ef2137f9838ccac872dca20772970f65
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:20:17 +0000

    INTEGRATION: CWS pchfix02 (1.4.94); FILE MERGED 2006/09/01 17:26:42 kaib 1.4.94.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/pcrcommon.cxx
+++ extensions/source/propctrlr/pcrcommon.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pcrcommon.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:27:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:20:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_PROPCTRLR_PCRCOMMON_HXX_
 #include "pcrcommon.hxx"
