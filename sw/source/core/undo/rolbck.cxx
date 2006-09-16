7cc6e8e24776007ebf492644d31232d2c1e26b20
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:50:05 +0000

    INTEGRATION: CWS pchfix02 (1.16.2); FILE MERGED 2006/09/01 17:52:15 kaib 1.16.2.1: #i68856# Added header markers and pch files

--- sw/source/core/undo/rolbck.cxx
+++ sw/source/core/undo/rolbck.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rolbck.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:48:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:50:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX //autogen
 #include <hintids.hxx>
