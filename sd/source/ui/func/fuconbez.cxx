ef48285082993c371472bb10d10d648f24f4828e
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:47:39 +0000

    INTEGRATION: CWS pchfix02 (1.13.142); FILE MERGED 2006/09/01 17:37:05 kaib 1.13.142.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fuconbez.cxx
+++ sd/source/ui/func/fuconbez.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuconbez.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:16:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:47:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "fuconbez.hxx"
 
@@ -60,7 +63,6 @@
 #include <sfx2/viewfrm.hxx>
 #endif
 
-#pragma hdrstop
 
 #include <svx/svxids.hrc>
 
