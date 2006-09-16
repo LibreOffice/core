816e4335f7bede5d508891fdd91b0592e80784ff
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:56:22 +0000

    INTEGRATION: CWS pchfix02 (1.4.476); FILE MERGED 2006/09/01 17:51:27 kaib 1.4.476.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/gctable.cxx
+++ sw/source/core/doc/gctable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gctable.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:16:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:56:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
