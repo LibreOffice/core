42836322ccd31e323f2bd328a1322ff77a9529f7
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:05:14 +0000

    INTEGRATION: CWS pchfix02 (1.9.446); FILE MERGED 2006/09/01 17:53:12 kaib 1.9.446.1: #i68856# Added header markers and pch files

--- sw/source/ui/lingu/hyp.cxx
+++ sw/source/ui/lingu/hyp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hyp.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-06 10:55:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:05:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "initui.hxx"
 #include "view.hxx"
