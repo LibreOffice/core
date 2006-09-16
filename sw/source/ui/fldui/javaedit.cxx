fa96d6bb410683ffd9f75e97f050e4fe05c85255
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:00:08 +0000

    INTEGRATION: CWS pchfix02 (1.11.476); FILE MERGED 2006/09/01 17:53:08 kaib 1.11.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/fldui/javaedit.cxx
+++ sw/source/ui/fldui/javaedit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: javaedit.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:44:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:00:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
@@ -40,7 +43,6 @@
 // include ---------------------------------------------------------------
 
 
-#pragma hdrstop
 
 #define _JAVAEDIT_CXX
 
