b90acc2ec24728d81b30a9e72c2e9331f0ab77c2
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:29:00 +0000

    INTEGRATION: CWS pchfix02 (1.9.168); FILE MERGED 2006/09/01 17:26:48 kaib 1.9.168.1: #i68856# Added header markers and pch files

--- extensions/source/svg/svgaction.cxx
+++ extensions/source/svg/svgaction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svgaction.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-05 13:31:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:29:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #include "svgaction.hxx"
 #include <vcl/virdev.hxx>
