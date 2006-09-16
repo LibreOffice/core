b711377b39660d8dc3cf0b4f329c890b4b88844b
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:43:08 +0000

    INTEGRATION: CWS pchfix02 (1.17.178); FILE MERGED 2006/09/01 17:52:09 kaib 1.17.178.1: #i68856# Added header markers and pch files

--- sw/source/core/tox/tox.cxx
+++ sw/source/core/tox/tox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tox.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-27 09:45:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:43:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 
 #ifndef _TOOLS_RESID_HXX
