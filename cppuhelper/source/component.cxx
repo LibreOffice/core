396d146019a8e4688ffe3ab7bf13f1a7c9d7a226
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:39:35 +0000

    INTEGRATION: CWS pchfix02 (1.13.14); FILE MERGED 2006/09/01 17:23:17 kaib 1.13.14.1: #i68856# Added header markers and pch files

--- cppuhelper/source/component.cxx
+++ cppuhelper/source/component.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: component.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:32:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:39:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppuhelper.hxx"
 
 #ifndef _RTL_STRING_HXX_
 #include <rtl/string.hxx>
