22f2df6e9751151d6fa08e66f0989f2c6c0f03f4
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:46:06 +0000

    INTEGRATION: CWS pchfix02 (1.15.28); FILE MERGED 2006/09/01 17:37:04 kaib 1.15.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/docshell/sdclient.cxx
+++ sd/source/ui/docshell/sdclient.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdclient.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:31:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:46:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #include "Client.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_NOVISUALAREASIZEEXCEPTION_HPP_
@@ -51,7 +54,6 @@
 
 #include <toolkit/helper/vclunohelper.hxx>
 
-#pragma hdrstop
 
 #include "misc.hxx"
 
