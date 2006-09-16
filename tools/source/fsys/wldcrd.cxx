3047631446fe3f463e49b616313359a03650b917
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:54:37 +0000

    INTEGRATION: CWS pchfix02 (1.2.120); FILE MERGED 2006/09/01 17:54:51 kaib 1.2.120.1: #i68856# Added header markers and pch files

--- tools/source/fsys/wldcrd.cxx
+++ tools/source/fsys/wldcrd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wldcrd.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:18:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:54:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #ifndef _WLDCRD_HXX
 #include <wldcrd.hxx>
