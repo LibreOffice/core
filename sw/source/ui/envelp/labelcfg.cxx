768eaa6dec5d99e9d6f75df2aeac4090cb93818d
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:54:57 +0000

    INTEGRATION: CWS pchfix02 (1.11.2); FILE MERGED 2006/09/01 17:53:05 kaib 1.11.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/labelcfg.cxx
+++ sw/source/ui/envelp/labelcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: labelcfg.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:35:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:54:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SWTYPES_HXX
 #include <swtypes.hxx>
