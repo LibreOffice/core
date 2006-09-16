843d2f847075af53dee7a73d977633235015a718
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:02:28 +0000

    INTEGRATION: CWS pchfix02 (1.56.2); FILE MERGED 2006/09/01 17:53:10 kaib 1.56.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/frmdlg/frmpage.cxx
+++ sw/source/ui/frmdlg/frmpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmpage.cxx,v $
  *
- *  $Revision: 1.56 $
+ *  $Revision: 1.57 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:38:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:02:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
