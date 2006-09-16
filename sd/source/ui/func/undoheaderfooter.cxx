6140747fb86158063e0a4fe12f4057d20ac650aa
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:00:34 +0000

    INTEGRATION: CWS pchfix02 (1.4.280); FILE MERGED 2006/09/01 17:37:13 kaib 1.4.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/undoheaderfooter.cxx
+++ sd/source/ui/func/undoheaderfooter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoheaderfooter.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:55:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:59:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _SFXDISPATCH_HXX
 #include <sfx2/dispatch.hxx>
--- sd/source/ui/func/unmovss.cxx
+++ sd/source/ui/func/unmovss.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unmovss.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:56:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:00:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "unmovss.hxx"
 #include "DrawDocShell.hxx"
