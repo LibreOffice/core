1f38481fbb76a72ab311441cab2557fb2d4eda00
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:19:36 +0000

    INTEGRATION: CWS pchfix02 (1.12.280); FILE MERGED 2006/09/01 17:36:45 kaib 1.12.280.1: #i68856# Added header markers and pch files

--- sd/source/filter/sdpptwrp.cxx
+++ sd/source/filter/sdpptwrp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdpptwrp.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:16:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:19:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include <sfx2/docfile.hxx>
 #include <sfx2/docfilt.hxx>
