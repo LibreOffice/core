0335055543eb50ab80e0771a859e7c8afcdf9b88
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:37:02 +0000

    INTEGRATION: CWS pchfix02 (1.38.6); FILE MERGED 2006/09/01 17:32:56 kaib 1.38.6.1: #i68856# Added header markers and pch files

--- psprint/source/printergfx/printerjob.cxx
+++ psprint/source/printergfx/printerjob.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: printerjob.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:36:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:37:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_psprint.hxx"
 
 #ifndef __SGI_STL_VECTOR
 #include <vector>
