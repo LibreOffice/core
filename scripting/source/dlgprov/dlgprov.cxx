b75e8570d1143317d261af413eb19f8d6e12993b
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:26:57 +0000

    INTEGRATION: CWS pchfix02 (1.7.8); FILE MERGED 2006/09/01 17:35:34 kaib 1.7.8.1: #i68856# Added header markers and pch files

--- scripting/source/dlgprov/dlgprov.cxx
+++ scripting/source/dlgprov/dlgprov.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgprov.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-14 07:10:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:26:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_scripting.hxx"
 
 #ifndef SCRIPTING_DLGPROV_HXX
 #include "dlgprov.hxx"
