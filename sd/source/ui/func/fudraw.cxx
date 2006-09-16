8e66c435d4b38f7a10243717bde4fb87fe9bdfa6
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:49:27 +0000

    INTEGRATION: CWS pchfix02 (1.26.24); FILE MERGED 2006/09/01 17:37:06 kaib 1.26.24.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fudraw.cxx
+++ sd/source/ui/func/fudraw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fudraw.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:21:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:49:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #include <sot/object.hxx>
 #ifndef _EEITEM_HXX //autogen
 #include <svx/eeitem.hxx>
