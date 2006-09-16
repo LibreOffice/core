b2365138c18c4da4f6dc2ccf473b9e9735e4db72
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:42:09 +0000

    INTEGRATION: CWS pchfix02 (1.8.8); FILE MERGED 2006/09/01 17:31:23 kaib 1.8.8.1: #i68856# Added header markers and pch files

--- io/source/acceptor/acc_pipe.cxx
+++ io/source/acceptor/acc_pipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acc_pipe.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:15:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:42:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 #include "osl/security.hxx"
 #include "acceptor.hxx"
 #include <com/sun/star/connection/ConnectionSetupException.hpp>
