ce19e7bf5529a937f59b53009c2d4c6de4dfd787
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:41:34 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:25:50 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- embeddedobj/source/commonembedding/specialobject.cxx
+++ embeddedobj/source/commonembedding/specialobject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: specialobject.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:29:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:41:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef  _COM_SUN_STAR_UI_DIALOGS_XEXECUTABLEDIALOG_HPP_
 #include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
