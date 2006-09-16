ce203dd4afc394eb85d8aff8e72fd64e0e963c29
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:38:37 +0000

    INTEGRATION: CWS pchfix02 (1.11.474); FILE MERGED 2006/09/01 17:51:13 kaib 1.11.474.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accselectionhelper.cxx
+++ sw/source/core/access/accselectionhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accselectionhelper.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:55:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:38:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLESELECTION_HPP_
 #include <com/sun/star/accessibility/XAccessibleSelection.hpp>
