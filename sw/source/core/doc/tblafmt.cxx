91371e2d608720e3d052388d06a81c6420b1c69f
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:58:42 +0000

    INTEGRATION: CWS pchfix02 (1.18.104); FILE MERGED 2006/09/01 17:51:29 kaib 1.18.104.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/tblafmt.cxx
+++ sw/source/core/doc/tblafmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tblafmt.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:40:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:58:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 
-#pragma hdrstop
+
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 
