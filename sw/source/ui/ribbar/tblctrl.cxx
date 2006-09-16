dfc52ebd7ae7990fd5fa552a69a7b44d0e98bd9d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:11:55 +0000

    INTEGRATION: CWS pchfix02 (1.7.476); FILE MERGED 2006/09/01 17:53:17 kaib 1.7.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/tblctrl.cxx
+++ sw/source/ui/ribbar/tblctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tblctrl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:45:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:11:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SFXINTITEM_HXX //autogen
 #include <svtools/intitem.hxx>
