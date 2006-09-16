699a5b26f97f46fd145358518a755625e379fff4
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:17:03 +0000

    INTEGRATION: CWS pchfix02 (1.5.94); FILE MERGED 2006/09/01 17:26:40 kaib 1.5.94.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/formbrowsertools.cxx
+++ extensions/source/propctrlr/formbrowsertools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formbrowsertools.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:22:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:17:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_FORMSCTRLR_FORMBROWSERTOOLS_HXX_
 #include "formbrowsertools.hxx"
