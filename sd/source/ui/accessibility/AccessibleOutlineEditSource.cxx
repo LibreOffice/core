a488b6c7bf565131f2ed93352bdce15adc93bb8a
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:24:32 +0000

    INTEGRATION: CWS pchfix02 (1.8.278); FILE MERGED 2006/09/01 17:36:50 kaib 1.8.278.1: #i68856# Added header markers and pch files

--- sd/source/ui/accessibility/AccessibleOutlineEditSource.cxx
+++ sd/source/ui/accessibility/AccessibleOutlineEditSource.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleOutlineEditSource.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:26:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:24:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SVX_UNOEDHLP_HXX
 #include <svx/unoedhlp.hxx>
