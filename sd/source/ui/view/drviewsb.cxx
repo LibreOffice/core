007ea29658be98cc66328d4c89f6c5104ca33196
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:38:28 +0000

    INTEGRATION: CWS pchfix02 (1.24.142); FILE MERGED 2006/09/01 17:37:38 kaib 1.24.142.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/drviewsb.cxx
+++ sd/source/ui/view/drviewsb.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drviewsb.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:44:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:38:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_UI_DIALOGS_XEXECUTABLEDIALOG_HPP_
 #include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
@@ -92,7 +95,6 @@
 //CHINA001 #endif
 #include <svx/svxdlg.hxx> //CHINA001
 #include <svx/dialogs.hrc> //CHINA001
-#pragma hdrstop
 
 #ifndef INCLUDED_SVTOOLS_USEROPTIONS_HXX
 #include <svtools/useroptions.hxx>
