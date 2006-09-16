191b37606ed158e058f9099001ec3f45fadd88b8
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:23:13 +0000

    INTEGRATION: CWS pchfix02 (1.6.94); FILE MERGED 2006/09/01 17:26:44 kaib 1.6.94.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/selectlabeldialog.cxx
+++ extensions/source/propctrlr/selectlabeldialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: selectlabeldialog.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:32:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:23:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #ifndef _EXTENSIONS_PROPCTRLR_SELECTLABELDIALOG_HXX_
 #include "selectlabeldialog.hxx"
 #endif
