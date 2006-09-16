1385dc79861b642a5dbbef1f85fc047d75894e22
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:41:19 +0000

    INTEGRATION: CWS pchfix02 (1.51.2); FILE MERGED 2006/09/01 17:52:55 kaib 1.51.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/optpage.cxx
+++ sw/source/ui/config/optpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optpage.cxx,v $
  *
- *  $Revision: 1.51 $
+ *  $Revision: 1.52 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:29:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:41:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
