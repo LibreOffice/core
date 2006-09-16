a75712808790318a23a18366ccec91c80305dffe
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:05:00 +0000

    INTEGRATION: CWS pchfix02 (1.14.2); FILE MERGED 2006/09/01 17:53:12 kaib 1.14.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/lingu/hhcwrp.cxx
+++ sw/source/ui/lingu/hhcwrp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hhcwrp.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:48:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:05:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef PRECOMPILED
 #include "ui_pch.hxx"
 #endif
 
-#pragma hdrstop
 
 #ifndef _UIPARAM_HXX
 #include <uiparam.hxx>
