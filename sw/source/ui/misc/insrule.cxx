128c07282fc61fd8b939d1e03d14e285bea1ed56
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:07:44 +0000

    INTEGRATION: CWS pchfix02 (1.9.476); FILE MERGED 2006/09/01 17:53:14 kaib 1.9.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/misc/insrule.cxx
+++ sw/source/ui/misc/insrule.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: insrule.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:33:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:07:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #include "uiparam.hxx"
 #include "hintids.hxx"
