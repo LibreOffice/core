608b7e9befcdb4752ce47b561e21f34f3e1d8bfb
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:38:51 +0000

    INTEGRATION: CWS pchfix02 (1.25.2); FILE MERGED 2006/09/01 17:51:14 kaib 1.25.2.1: #i68856# Added header markers and pch files

--- sw/source/core/access/acctable.cxx
+++ sw/source/core/access/acctable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acctable.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:47:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:38:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _VOS_MUTEX_HXX_ //autogen
 #include <vos/mutex.hxx>
