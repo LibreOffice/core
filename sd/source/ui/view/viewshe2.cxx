ecf6a3423736e4430ad9c6617c164f7932dc9e05
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:45:02 +0000

    INTEGRATION: CWS pchfix02 (1.43.8); FILE MERGED 2006/09/01 17:37:42 kaib 1.43.8.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/viewshe2.cxx
+++ sd/source/ui/view/viewshe2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewshe2.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 11:09:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:45:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_NOVISUALAREASIZEEXCEPTION_HPP_
 #include <com/sun/star/embed/NoVisualAreaSizeException.hpp>
