49f8b448e1612df5f2c62ea53cdbf35db04eb586
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:17:17 +0000

    INTEGRATION: CWS pchfix02 (1.8.474); FILE MERGED 2006/09/01 17:51:47 kaib 1.8.474.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/dbg_lay.cxx
+++ sw/source/core/layout/dbg_lay.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbg_lay.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:09:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:17:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 /* -----------------08.01.99 14:55-------------------
  * Und hier die Beschreibung:
@@ -113,7 +116,6 @@
 #endif
 
 
-#pragma hdrstop
 
 #include "dbg_lay.hxx"
 
