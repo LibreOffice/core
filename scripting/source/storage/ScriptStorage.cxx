e94fed408115f0dce67319f2ac08559e106ede79
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:32:15 +0000

    INTEGRATION: CWS pchfix02 (1.30.36); FILE MERGED 2006/09/01 17:35:38 kaib 1.30.36.1: #i68856# Added header markers and pch files

--- scripting/source/storage/ScriptStorage.cxx
+++ scripting/source/storage/ScriptStorage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScriptStorage.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:37:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:32:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
 ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_scripting.hxx"
 #include <osl/file.hxx>
 #include <osl/time.h>
 #include <cppuhelper/implementationentry.hxx>
