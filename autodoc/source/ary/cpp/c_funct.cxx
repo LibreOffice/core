1dfc2ee8968ab77d2769fdd919b9298fed5814ab
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:16:49 +0000

    INTEGRATION: CWS pchfix02 (1.2.30); FILE MERGED 2006/09/01 17:15:15 kaib 1.2.30.1: #i68856# Added header markers and pch files

--- autodoc/source/ary/cpp/c_funct.cxx
+++ autodoc/source/ary/cpp/c_funct.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: c_funct.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:31:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:15:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <ary/cpp/c_funct.hxx>
--- autodoc/source/ary/cpp/c_osigna.cxx
+++ autodoc/source/ary/cpp/c_osigna.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: c_osigna.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:32:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:16:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
