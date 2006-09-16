98ccd46978db63bb5c2a7bebcc25e0d419e1d59d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:43:28 +0000

    INTEGRATION: CWS pchfix02 (1.5.8); FILE MERGED 2006/09/01 17:31:24 kaib 1.5.8.1: #i68856# Added header markers and pch files

--- io/source/connector/ctr_pipe.cxx
+++ io/source/connector/ctr_pipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ctr_pipe.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:17:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:43:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 
 #include "connector.hxx"
 
--- io/source/stm/factreg.cxx
+++ io/source/stm/factreg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: factreg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:17:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:43:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 
 #include <osl/diagnose.h>
 #include <cppuhelper/factory.hxx>
