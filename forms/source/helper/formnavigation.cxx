faed2115fe86142cbc70a3ea4bf17bb70900abee
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:56:20 +0000

    INTEGRATION: CWS pchfix02 (1.6.26); FILE MERGED 2006/09/01 17:27:40 kaib 1.6.26.1: #i68856# Added header markers and pch files

--- forms/source/helper/formnavigation.cxx
+++ forms/source/helper/formnavigation.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formnavigation.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:57:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:56:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef FORMS_FORM_NAVIGATION_HXX
 #include "formnavigation.hxx"
