da49584b85dff3fdbdd06c7c8808759bba463033
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:14:56 +0000

    INTEGRATION: CWS pchfix02 (1.4.38); FILE MERGED 2006/09/01 17:20:37 kaib 1.4.38.1: #i68856# Added header markers and pch files

--- configmgr/source/misc/configinteractionhandler.cxx
+++ configmgr/source/misc/configinteractionhandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: configinteractionhandler.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-01-24 16:43:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:14:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include "sal/config.h"
 
