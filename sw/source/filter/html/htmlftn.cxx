d5101e7646d3aba4e080751384d590a93604e486
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:10:36 +0000

    INTEGRATION: CWS pchfix02 (1.9.2); FILE MERGED 2006/09/01 17:52:30 kaib 1.9.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/html/htmlftn.cxx
+++ sw/source/filter/html/htmlftn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlftn.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:04:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:10:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 
-#pragma hdrstop
+
 
 #ifndef _HTMLOUT_HXX //autogen
 #include <svtools/htmlout.hxx>
