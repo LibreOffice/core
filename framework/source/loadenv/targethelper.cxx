32e0187e7917eba16fcbe1b0452c0c3ff5fb22fa
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:06:10 +0000

    INTEGRATION: CWS pchfix02 (1.4.52); FILE MERGED 2006/09/01 17:29:16 kaib 1.4.52.1: #i68856# Added header markers and pch files

--- framework/source/loadenv/targethelper.cxx
+++ framework/source/loadenv/targethelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: targethelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:24:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:05:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_______________________________________________
 // own includes
