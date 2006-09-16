81f4481f4c28290692d964d61d76f0db99f4b517
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:26:37 +0000

    INTEGRATION: CWS pchfix02 (1.8.2); FILE MERGED 2006/09/01 17:51:55 kaib 1.8.2.1: #i68856# Added header markers and pch files

--- sw/source/core/objectpositioning/tolayoutanchoredobjectposition.cxx
+++ sw/source/core/objectpositioning/tolayoutanchoredobjectposition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tolayoutanchoredobjectposition.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:30:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:26:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _TOLAYOUTANCHOREDOBJECTPOSITION_HXX
 #include <tolayoutanchoredobjectposition.hxx>
 #endif
