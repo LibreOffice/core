b91abe57b850f330efb350507735d9523629cfa9
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:28:38 +0000

    INTEGRATION: CWS pchfix02 (1.31.2); FILE MERGED 2006/09/01 17:52:46 kaib 1.31.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/xmlfmt.cxx
+++ sw/source/filter/xml/xmlfmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlfmt.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:22:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:28:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
