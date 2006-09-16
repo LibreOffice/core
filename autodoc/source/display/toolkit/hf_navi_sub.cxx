22caf65ede7a56279eb18298a529296ee482826e
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:52:39 +0000

    INTEGRATION: CWS pchfix02 (1.4.6); FILE MERGED 2006/09/01 17:15:37 kaib 1.4.6.1: #i68856# Added header markers and pch files

--- autodoc/source/display/toolkit/hf_navi_sub.cxx
+++ autodoc/source/display/toolkit/hf_navi_sub.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hf_navi_sub.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:00:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:52:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
