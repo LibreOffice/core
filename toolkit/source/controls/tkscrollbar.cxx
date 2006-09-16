5f001b9db14304e3f28dd8c946e25cb5d07212c2
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:19:33 +0000

    INTEGRATION: CWS pchfix02 (1.6.136); FILE MERGED 2006/09/01 17:54:23 kaib 1.6.136.1: #i68856# Added header markers and pch files

--- toolkit/source/controls/tkscrollbar.cxx
+++ toolkit/source/controls/tkscrollbar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tkscrollbar.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:18:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:18:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef TOOLKIT_TOOLKIT_CONTROLS_TKSCROLLBAR_HXX
 #include "toolkit/controls/tkscrollbar.hxx"
