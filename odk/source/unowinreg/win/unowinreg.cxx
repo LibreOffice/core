acfcb5f7380a4fd5821f194e9dbb721dc5cfb743
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:17:32 +0000

    INTEGRATION: CWS pchfix02 (1.2.28); FILE MERGED 2006/09/01 17:32:28 kaib 1.2.28.1: #i68856# Added header markers and pch files

--- odk/source/unowinreg/win/unowinreg.cxx
+++ odk/source/unowinreg/win/unowinreg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unowinreg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:08:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:17:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_odk.hxx"
 
 #if defined _MSC_VER
 #pragma warning(push, 1)
