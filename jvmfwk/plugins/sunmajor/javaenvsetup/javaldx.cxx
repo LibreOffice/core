a26a5dd7d849e2e30a4b7e3716f7be5123664948
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:45:14 +0000

    INTEGRATION: CWS pchfix02 (1.12.4); FILE MERGED 2006/09/01 17:31:39 kaib 1.12.4.1: #i68856# Added header markers and pch files

--- jvmfwk/plugins/sunmajor/javaenvsetup/javaldx.cxx
+++ jvmfwk/plugins/sunmajor/javaenvsetup/javaldx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: javaldx.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 16:19:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:45:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_jvmfwk.hxx"
 
 #include <stdio.h>
 #include <stdlib.h>
