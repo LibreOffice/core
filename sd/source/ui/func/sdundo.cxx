8e5479a34c6ce7e3c6e775339da28c14af50b7d6
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:59:08 +0000

    INTEGRATION: CWS pchfix02 (1.3.280); FILE MERGED 2006/09/01 17:37:12 kaib 1.3.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/sdundo.cxx
+++ sd/source/ui/func/sdundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdundo.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:54:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:59:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "sdundo.hxx"
 #include <tools/debug.hxx>
