f07cff38713e2afe463b15a33a98365ea0ec57d1
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:08:14 +0000

    INTEGRATION: CWS pchfix02 (1.4.194); FILE MERGED 2006/09/01 17:37:18 kaib 1.4.194.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/controller/SlsTransferable.cxx
+++ sd/source/ui/slidesorter/controller/SlsTransferable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsTransferable.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-01-24 14:43:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:08:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "SlsTransferable.hxx"
 
