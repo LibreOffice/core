eb84979e8f6d0d2b6c33f882c03639e37396d535
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:45:28 +0000

    INTEGRATION: CWS pchfix02 (1.10.4); FILE MERGED 2006/09/01 17:31:39 kaib 1.10.4.1: #i68856# Added header markers and pch files

--- jvmfwk/plugins/sunmajor/pluginlib/gnujre.cxx
+++ jvmfwk/plugins/sunmajor/pluginlib/gnujre.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gnujre.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 16:19:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:45:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_jvmfwk.hxx"
 
 #include "osl/file.hxx"
 #include "osl/thread.h"
