7bc3bbc6d77d1b7c39d0e54d1ffd4ab551bdc4b8
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:25:56 +0000

    INTEGRATION: CWS pchfix02 (1.12.2); FILE MERGED 2006/09/01 17:52:44 kaib 1.12.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/ww8/dump/dump8a.cxx
+++ sw/source/filter/ww8/dump/dump8a.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dump8a.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:20:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:25:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 
 //#include <stdio.h>        // SEEK_SET
