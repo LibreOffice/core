860721deefd5ee93306642b9ed6efd4cdfa7e836
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:13:39 +0000

    INTEGRATION: CWS pchfix02 (1.5.52); FILE MERGED 2006/09/01 17:29:22 kaib 1.5.52.1: #i68856# Added header markers and pch files

--- framework/source/threadhelp/lockhelper.cxx
+++ framework/source/threadhelp/lockhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lockhelper.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:30:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:13:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
