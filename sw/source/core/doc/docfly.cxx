c189a7171892d2d38070de1b68bbd2ab57af5f9f
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:52:22 +0000

    INTEGRATION: CWS pchfix02 (1.29.2); FILE MERGED 2006/09/01 17:51:24 kaib 1.29.2.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/docfly.cxx
+++ sw/source/core/doc/docfly.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docfly.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:57:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:52:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
