4c5e1eead2ae8b238390d16d058479b57c4add8c
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:41:32 +0000

    INTEGRATION: CWS pchfix02 (1.8.352); FILE MERGED 2006/09/01 17:52:55 kaib 1.8.352.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/prtopt.cxx
+++ sw/source/ui/config/prtopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prtopt.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2005-12-21 15:12:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:41:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _UTL_CONFIGMGR_HXX_
 #include <unotools/configmgr.hxx>
