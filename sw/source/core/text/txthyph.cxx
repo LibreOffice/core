21e1ebdc08d0b622fbc2593676a441871e678dac
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:41:31 +0000

    INTEGRATION: CWS pchfix02 (1.21.2); FILE MERGED 2006/09/01 17:52:07 kaib 1.21.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/txthyph.cxx
+++ sw/source/core/text/txthyph.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txthyph.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:44:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:41:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
