705afec95d3afa388bbb3790822442ab3ebfb075
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:47:32 +0000

    INTEGRATION: CWS pchfix02 (1.8.476); FILE MERGED 2006/09/01 17:52:13 kaib 1.8.476.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/txatbase.cxx
+++ sw/source/core/txtnode/txatbase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txatbase.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:14:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:47:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SFXITEMPOOL_HXX
 #include <svtools/itempool.hxx>
