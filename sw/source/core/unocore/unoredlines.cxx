0235a4cbf336f6579ee49b3e1396d18c988f3ac4
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:00:06 +0000

    INTEGRATION: CWS pchfix02 (1.3.474); FILE MERGED 2006/09/01 17:52:22 kaib 1.3.474.1: #i68856# Added header markers and pch files

--- sw/source/core/unocore/unoredlines.cxx
+++ sw/source/core/unocore/unoredlines.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoredlines.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:29:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:00:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _VOS_MUTEX_HXX_ //autogen
 #include <vos/mutex.hxx>
