0dcc207d615a56efd0b18f09ef7cbfa8f036b236
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:47:35 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:52:59 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/dbui/mmpreparemergepage.cxx
+++ sw/source/ui/dbui/mmpreparemergepage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmpreparemergepage.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:06:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:47:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 #ifndef _MAILMERGEPREPAREMERGEPAGE_HXX
 #include <mmpreparemergepage.hxx>
 #endif
