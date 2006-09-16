e6327c0a845a6be6b8c4e33d8871649d5b022c99
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:34:24 +0000

    INTEGRATION: CWS pchfix02 (1.28.2); FILE MERGED 2006/09/01 17:51:10 kaib 1.28.2.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accdoc.cxx
+++ sw/source/core/access/accdoc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accdoc.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:41:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:34:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _SV_WINDOW_HXX
 #include <vcl/window.hxx>
 #endif
@@ -39,7 +42,6 @@
 #include <rootfrm.hxx>
 #endif
 
-#pragma hdrstop
 
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
