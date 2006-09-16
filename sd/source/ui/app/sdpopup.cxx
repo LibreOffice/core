f914c0afc7df319771ca6197dc262f21acfb54d5
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:32:08 +0000

    INTEGRATION: CWS pchfix02 (1.9.280); FILE MERGED 2006/09/01 17:36:54 kaib 1.9.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/app/sdpopup.cxx
+++ sd/source/ui/app/sdpopup.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdpopup.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:45:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:32:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _EEITEM_HXX //autogen
 #include <svx/eeitem.hxx>
