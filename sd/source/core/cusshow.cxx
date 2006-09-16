968bae9ad85878f1b9a3d395c1d509e9c698db22
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:13:53 +0000

    INTEGRATION: CWS pchfix02 (1.6.280); FILE MERGED 2006/09/01 17:36:41 kaib 1.6.280.1: #i68856# Added header markers and pch files

--- sd/source/core/cusshow.cxx
+++ sd/source/core/cusshow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cusshow.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:10:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:13:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
 #include <com/sun/star/lang/XComponent.hpp>
