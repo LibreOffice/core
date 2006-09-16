6699b926968cc00192ea88cb003fe0bf46a945fe
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:31:43 +0000

    INTEGRATION: CWS pchfix02 (1.4.2); FILE MERGED 2006/09/01 17:53:31 kaib 1.4.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/warnpassword.cxx
+++ sw/source/ui/uno/warnpassword.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: warnpassword.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:02:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:31:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
 + ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 // ============================================================================
 
 #ifndef SW_WARNPASSWORD_HXX
