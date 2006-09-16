b680ddd444d9d4a769b0577c00460290cde11307
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:58:55 +0000

    INTEGRATION: CWS pchfix02 (1.17.186); FILE MERGED 2006/09/01 17:26:29 kaib 1.17.186.1: #i68856# Added header markers and pch files

--- extensions/source/dbpilots/controlwizard.cxx
+++ extensions/source/dbpilots/controlwizard.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: controlwizard.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:26:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:58:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_DBP_CONTROLWIZARD_HXX
 #include "controlwizard.hxx"
