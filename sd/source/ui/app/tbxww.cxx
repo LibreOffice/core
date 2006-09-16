023587251372c783e76c50473a614e385a68b85f
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:32:49 +0000

    INTEGRATION: CWS pchfix02 (1.19.280); FILE MERGED 2006/09/01 17:36:55 kaib 1.19.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/app/tbxww.cxx
+++ sd/source/ui/app/tbxww.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tbxww.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:47:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:32:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include <sfx2/bindings.hxx>
 #include <svx/svxids.hrc>
