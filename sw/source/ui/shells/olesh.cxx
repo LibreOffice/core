ce6ba4633252cc17f57a790e6913810e1791b2d3
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:16:07 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:20 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/olesh.cxx
+++ sw/source/ui/shells/olesh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: olesh.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:52:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:16:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SFXAPP_HXX //autogen
 #include <sfx2/app.hxx>
