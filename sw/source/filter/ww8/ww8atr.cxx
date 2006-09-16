c173791ee8c368f7dbde2c4a043936aa61540d0b
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:22:43 +0000

    INTEGRATION: CWS pchfix02 (1.95.2); FILE MERGED 2006/09/01 17:52:40 kaib 1.95.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/ww8/ww8atr.cxx
+++ sw/source/filter/ww8/ww8atr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ww8atr.cxx,v $
  *
- *  $Revision: 1.96 $
+ *  $Revision: 1.97 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:55:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:22:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
 /*
