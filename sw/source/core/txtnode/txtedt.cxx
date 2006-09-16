d46d587b6d5efaa7485d68373c7a55264a23bb7c
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:48:18 +0000

    INTEGRATION: CWS pchfix02 (1.73.2); FILE MERGED 2006/09/01 17:52:13 kaib 1.73.2.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/txtedt.cxx
+++ sw/source/core/txtnode/txtedt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtedt.cxx,v $
  *
- *  $Revision: 1.73 $
+ *  $Revision: 1.74 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:48:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:48:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 // So kann man die Linguistik-Statistik ( (Tmp-Path)\swlingu.stk ) aktivieren:
 //#define LINGU_STATISTIK
