4659c8c31a97faac30271bd1aa80bf2113a3eb69
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:44:02 +0000

    INTEGRATION: CWS pchfix02 (1.12.6); FILE MERGED 2006/09/01 18:00:54 kaib 1.12.6.1: #i68856# Added header markers and pch files

--- xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.cxx
+++ xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: securityenvironment_mscryptimpl.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 08:09:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:44:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlsecurity.hxx"
 
 #ifndef _SAL_CONFIG_H_
 #include <sal/config.h>
