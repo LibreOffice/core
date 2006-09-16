3321f682b1914ee5e18c42376581fafa6962da2b
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:50:51 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:51:23 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/doccorr.cxx
+++ sw/source/core/doc/doccorr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: doccorr.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:52:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:50:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _DOC_HXX
 #include <doc.hxx>
