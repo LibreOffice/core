31aaeb069b620254a4ab04351c8d378a1c66196d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:44:08 +0000

    INTEGRATION: CWS pchfix02 (1.7.8); FILE MERGED 2006/09/01 17:31:25 kaib 1.7.8.1: #i68856# Added header markers and pch files

--- io/source/stm/opipe.cxx
+++ io/source/stm/opipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: opipe.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:18:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:44:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 
 // streams
 #include <com/sun/star/io/XInputStream.hpp>
