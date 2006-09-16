6cb2f3280aada592602a2e9cc5427f3022f0aaea
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:44:32 +0000

    INTEGRATION: CWS pchfix02 (1.16.278); FILE MERGED 2006/09/01 17:37:42 kaib 1.16.278.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/tabcontr.cxx
+++ sd/source/ui/view/tabcontr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabcontr.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:19:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:44:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #include "TabControl.hxx"
 
 #include <sfx2/viewfrm.hxx>
@@ -47,7 +50,6 @@
 #include <sfx2/dispatch.hxx>
 #endif
 
-#pragma hdrstop
 
 #include "sdattr.hxx"
 #include "app.hxx"
