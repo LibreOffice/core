fd6a5f24f86c273ba0d21f8c04aa2906871cccc6
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:42:14 +0000

    INTEGRATION: CWS pchfix02 (1.3.36); FILE MERGED 2006/09/01 17:41:21 kaib 1.3.36.1: #i68856# Added header markers and pch files

--- stoc/test/testsmgr_cpnt.cxx
+++ stoc/test/testsmgr_cpnt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testsmgr_cpnt.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:24:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:42:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 #ifndef _RTL_ALLOC_H_
 #include <rtl/alloc.h>
 #endif
