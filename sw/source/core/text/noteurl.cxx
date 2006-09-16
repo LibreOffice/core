ced1e230182819cf01d48426d1114daf71787de0
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:36:46 +0000

    INTEGRATION: CWS pchfix02 (1.3.476); FILE MERGED 2006/09/01 17:52:03 kaib 1.3.476.1: #i68856# Added header markers and pch files

--- sw/source/core/text/noteurl.cxx
+++ sw/source/core/text/noteurl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: noteurl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:56:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:36:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "swtypes.hxx"
 
