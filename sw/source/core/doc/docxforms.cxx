24dbaa8007628b371bb348a8319c981fb00ef7eb
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:55:25 +0000

    INTEGRATION: CWS pchfix02 (1.3.476); FILE MERGED 2006/09/01 17:51:27 kaib 1.3.476.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/docxforms.cxx
+++ sw/source/core/doc/docxforms.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docxforms.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:15:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:55:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 
 #ifndef _DOC_HXX
