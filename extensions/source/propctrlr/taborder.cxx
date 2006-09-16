304bb37e4a2a0aa683fd4ecc61ee18e99219df9a
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:24:34 +0000

    INTEGRATION: CWS pchfix02 (1.7.22); FILE MERGED 2006/09/01 17:26:45 kaib 1.7.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/taborder.cxx
+++ extensions/source/propctrlr/taborder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: taborder.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 08:01:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:24:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef EXTENSIONS_SOURCE_PROPCTRLR_TABORDER_HXX
 #include "taborder.hxx"
