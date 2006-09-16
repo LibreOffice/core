d36d7c23540fd89acdb78932d940ad8c2c91bd57
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:27:51 +0000

    INTEGRATION: CWS pchfix02 (1.8.2); FILE MERGED 2006/09/01 17:53:28 kaib 1.8.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/SwXFilterOptions.cxx
+++ sw/source/ui/uno/SwXFilterOptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXFilterOptions.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:59:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:27:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _SWXFILTEROPTIONS_HXX
 #include <SwXFilterOptions.hxx>
