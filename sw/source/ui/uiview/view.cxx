19a33ef64aa913682f54d9a56a46fc67ba34dc8d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:23:50 +0000

    INTEGRATION: CWS pchfix02 (1.96.2); FILE MERGED 2006/09/01 17:53:25 kaib 1.96.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uiview/view.cxx
+++ sw/source/ui/uiview/view.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: view.cxx,v $
  *
- *  $Revision: 1.96 $
+ *  $Revision: 1.97 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:56:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:23:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <string> // HACK: prevent conflict between STLPORT and Workshop headers
 
