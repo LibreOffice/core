c049cff702bb2b66f8712ac6c175a6473b7fa0a1
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:25:50 +0000

    INTEGRATION: CWS pchfix02 (1.16.282); FILE MERGED 2006/09/01 17:36:50 kaib 1.16.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/accessibility/AccessiblePresentationShape.cxx
+++ sd/source/ui/accessibility/AccessiblePresentationShape.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessiblePresentationShape.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:27:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:25:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SD_ACCESSIBILITY_ACCESSIBLE_PRESENTATION_SHAPE_HXX
 #include "AccessiblePresentationShape.hxx"
