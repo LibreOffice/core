9b7f02bceff5ccdf7270b8dc74cb223afba011a8
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:35:33 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:16:05 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- automation/source/server/prof_usl.cxx
+++ automation/source/server/prof_usl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prof_usl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:23:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:35:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 
 
 #include <procfs.h>
