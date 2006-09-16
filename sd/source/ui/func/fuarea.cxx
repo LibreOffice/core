7b0cad5ded18961b9d570f196f688df24c295cc0
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:46:34 +0000

    INTEGRATION: CWS pchfix02 (1.6.214); FILE MERGED 2006/09/01 17:37:04 kaib 1.6.214.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fuarea.cxx
+++ sd/source/ui/func/fuarea.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuarea.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 16:54:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:46:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fuarea.hxx"
 
