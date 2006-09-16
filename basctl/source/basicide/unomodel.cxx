9f1e6793b268a8ecc1c4fbd7dd1a79f2442968b1
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:30:25 +0000

    INTEGRATION: CWS pchfix02 (1.5.102); FILE MERGED 2006/09/01 17:16:22 kaib 1.5.102.1: #i68856# Added header markers and pch files

--- basctl/source/basicide/unomodel.cxx
+++ basctl/source/basicide/unomodel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unomodel.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:12:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:30:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basctl.hxx"
 
 #include "unomodel.hxx"
 
