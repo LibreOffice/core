185583a15e1a4d687ec8a9ac34fc036fb2d91ac9
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:17:35 +0000

    INTEGRATION: CWS pchfix02 (1.10.282); FILE MERGED 2006/09/01 17:36:44 kaib 1.10.282.1: #i68856# Added header markers and pch files

--- sd/source/core/typemap.cxx
+++ sd/source/core/typemap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typemap.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:18:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:17:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #define ITEMID_CASEMAP          0
 #define ITEMID_DBTYPE           0
