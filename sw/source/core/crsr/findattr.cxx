53182fb6ef0185b801b01fba5c337025cea339a6
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:45:50 +0000

    INTEGRATION: CWS pchfix02 (1.12.2); FILE MERGED 2006/09/01 17:51:19 kaib 1.12.2.1: #i68856# Added header markers and pch files

--- sw/source/core/crsr/findattr.cxx
+++ sw/source/core/crsr/findattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: findattr.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:51:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:45:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _COM_SUN_STAR_LANG_LOCALE_HPP_
 #include <com/sun/star/lang/Locale.hpp>
