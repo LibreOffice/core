8e7583e0d703c24633d5d5bbe580f4965535b492
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:00:02 +0000

    INTEGRATION: CWS pchfix02 (1.3.52); FILE MERGED 2006/09/01 17:29:11 kaib 1.3.52.1: #i68856# Added header markers and pch files

--- framework/source/helper/propertysetcontainer.cxx
+++ framework/source/helper/propertysetcontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertysetcontainer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:19:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:00:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_HELPER_PROPERTYSETCONTAINER_HXX_
 #include <helper/propertysetcontainer.hxx>
