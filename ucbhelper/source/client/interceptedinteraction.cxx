c781ed50118c7cec956690bc923c3d64699bf9cf
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:20:08 +0000

    INTEGRATION: CWS pchfix02 (1.4.8); FILE MERGED 2006/09/01 17:56:06 kaib 1.4.8.1: #i68856# Added header markers and pch files

--- ucbhelper/source/client/interceptedinteraction.cxx
+++ ucbhelper/source/client/interceptedinteraction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interceptedinteraction.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:11:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:20:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucbhelper.hxx"
 
 #ifndef _UCBHELPER_INTERCEPTEDINTERACTION_HXX_
 #include <ucbhelper/interceptedinteraction.hxx>
