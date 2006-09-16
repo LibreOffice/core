9f0e3c7cf80acb198772f2801f586c23eaedb6c7
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:08:26 +0000

    INTEGRATION: CWS pchfix02 (1.10.2); FILE MERGED 2006/09/01 17:51:39 kaib 1.10.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edundo.cxx
+++ sw/source/core/edit/edundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edundo.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:10:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:08:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SVDVIEW_HXX //autogen wg. SdrView
 #include <svx/svdview.hxx>
