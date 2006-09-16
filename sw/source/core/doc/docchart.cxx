3559a798c5c0ea76cd34d3e4a8eb3de63ce4f1e5
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:50:22 +0000

    INTEGRATION: CWS pchfix02 (1.9.474); FILE MERGED 2006/09/01 17:51:22 kaib 1.9.474.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/docchart.cxx
+++ sw/source/core/doc/docchart.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docchart.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:10:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:50:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <float.h>
 
