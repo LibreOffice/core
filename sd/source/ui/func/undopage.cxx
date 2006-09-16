d11bb4604231a82df10051b59eb194e261bf46f6
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:00:21 +0000

    INTEGRATION: CWS pchfix02 (1.6.278); FILE MERGED 2006/09/01 17:37:13 kaib 1.6.278.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/undopage.cxx
+++ sd/source/ui/func/undopage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undopage.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:56:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:00:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "undopage.hxx"
 
