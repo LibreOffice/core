a2629875a915baef89097877cb881563d2219107
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:44:36 +0000

    INTEGRATION: CWS pchfix02 (1.9.2); FILE MERGED 2006/09/01 17:51:18 kaib 1.9.2.1: #i68856# Added header markers and pch files

--- sw/source/core/crsr/callnk.cxx
+++ sw/source/core/crsr/callnk.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: callnk.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:50:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:44:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
