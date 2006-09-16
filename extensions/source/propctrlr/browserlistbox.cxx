683d91752f613cc52814939cab0ec9dd48b2f06a
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:13:11 +0000

    INTEGRATION: CWS pchfix02 (1.16.22); FILE MERGED 2006/09/01 17:26:38 kaib 1.16.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/browserlistbox.cxx
+++ extensions/source/propctrlr/browserlistbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: browserlistbox.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:52:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:13:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_PROPCTRLR_BROWSERLISTBOX_HXX_
 #include "browserlistbox.hxx"
