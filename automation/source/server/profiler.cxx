d0ae70008ec5ba8d58f4350b54753207cc38b263
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:35:47 +0000

    INTEGRATION: CWS pchfix02 (1.4.16); FILE MERGED 2006/09/01 17:16:05 kaib 1.4.16.1: #i68856# Added header markers and pch files

--- automation/source/server/profiler.cxx
+++ automation/source/server/profiler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: profiler.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:23:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:35:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 
 
 #ifndef _TIME_HXX //autogen
