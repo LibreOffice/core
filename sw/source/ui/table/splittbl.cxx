70be8e320acfdce6918736bbd2138b74b762a6c5
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:21:04 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:23 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/table/splittbl.cxx
+++ sw/source/ui/table/splittbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: splittbl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:03:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:21:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #ifndef _WRTSH_HXX //autogen
 #include <wrtsh.hxx>
