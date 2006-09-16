ad4734d9c49a23092b3e2c77cb549a25c04e4b3b
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:23:17 +0000

    INTEGRATION: CWS pchfix02 (1.29.28); FILE MERGED 2006/09/01 17:37:28 kaib 1.29.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/SdUnoDrawView.cxx
+++ sd/source/ui/unoidl/SdUnoDrawView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdUnoDrawView.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:44:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:23:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "SdUnoDrawView.hxx"
 
