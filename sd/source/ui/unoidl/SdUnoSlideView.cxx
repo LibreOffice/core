fc476eed25f34c23460ae5aa465c4c6ef2829a2c
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:23:58 +0000

    INTEGRATION: CWS pchfix02 (1.12.122); FILE MERGED 2006/09/01 17:37:28 kaib 1.12.122.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/SdUnoSlideView.cxx
+++ sd/source/ui/unoidl/SdUnoSlideView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdUnoSlideView.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-10 14:58:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:23:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "SdUnoSlideView.hxx"
 
