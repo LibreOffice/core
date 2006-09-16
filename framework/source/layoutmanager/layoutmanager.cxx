4635fbbd3ac2f7de1246358cebd4266430fb3bed
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:04:56 +0000

    INTEGRATION: CWS pchfix02 (1.52.8); FILE MERGED 2006/09/01 17:29:15 kaib 1.52.8.1: #i68856# Added header markers and pch files

--- framework/source/layoutmanager/layoutmanager.cxx
+++ framework/source/layoutmanager/layoutmanager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: layoutmanager.cxx,v $
  *
- *  $Revision: 1.53 $
+ *  $Revision: 1.54 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:09:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:04:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
