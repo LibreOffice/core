aa3679ddea3808fa3003f60bfeb16ae91d623aa9
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:16:34 +0000

    INTEGRATION: CWS pchfix02 (1.9.272); FILE MERGED 2006/09/01 17:37:24 kaib 1.9.272.1: #i68856# Added header markers and pch files

--- sd/source/ui/toolpanel/ToolPanel.cxx
+++ sd/source/ui/toolpanel/ToolPanel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ToolPanel.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 11:32:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:16:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "taskpane/ToolPanel.hxx"
 
