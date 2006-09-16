6a46feb970e97cf9054b12435490a59e94442d83
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:59:14 +0000

    INTEGRATION: CWS pchfix02 (1.4.26); FILE MERGED 2006/09/01 17:27:42 kaib 1.4.26.1: #i68856# Added header markers and pch files

--- forms/source/richtext/attributedispatcher.cxx
+++ forms/source/richtext/attributedispatcher.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: attributedispatcher.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:58:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:59:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef FORMS_SOURCE_RICHTEXT_ATTRIBUTEDISPATCHER_HXX
 #include "attributedispatcher.hxx"
