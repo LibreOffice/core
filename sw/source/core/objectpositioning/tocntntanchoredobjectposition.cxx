2e138e20f8e317c45e937d512c1257f161a52321
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:26:24 +0000

    INTEGRATION: CWS pchfix02 (1.18.2); FILE MERGED 2006/09/01 17:51:55 kaib 1.18.2.1: #i68856# Added header markers and pch files

--- sw/source/core/objectpositioning/tocntntanchoredobjectposition.cxx
+++ sw/source/core/objectpositioning/tocntntanchoredobjectposition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tocntntanchoredobjectposition.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:30:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:26:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _TOCNTNTANCHOREDOBJECTPOSITION_HXX
 #include <tocntntanchoredobjectposition.hxx>
 #endif
