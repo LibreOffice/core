e3baa6f63402b52410b90d31ec602bd54f6c9b2e
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:22:50 +0000

    INTEGRATION: CWS pchfix02 (1.2.60); FILE MERGED 2006/09/01 17:23:04 kaib 1.2.60.1: #i68856# Added header markers and pch files

--- cppu/test/alignment/pass1.cxx
+++ cppu/test/alignment/pass1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pass1.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 09:01:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:22:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppu.hxx"
 
 #include <stdio.h>
 #include <vector>
