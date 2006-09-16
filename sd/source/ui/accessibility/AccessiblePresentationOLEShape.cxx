cdf7d01e4da44f36e892c6001e599f929e564da8
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:25:35 +0000

    INTEGRATION: CWS pchfix02 (1.12.282); FILE MERGED 2006/09/01 17:36:50 kaib 1.12.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/accessibility/AccessiblePresentationOLEShape.cxx
+++ sd/source/ui/accessibility/AccessiblePresentationOLEShape.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessiblePresentationOLEShape.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:27:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:25:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SD_ACCESSIBILITY_ACCESSIBLE_PRESENTATION_OLE_SHAPE_HXX
 #include "AccessiblePresentationOLEShape.hxx"
