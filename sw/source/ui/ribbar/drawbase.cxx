5ff69cd9ad673dce2009c08558a3a95a9205707b
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:11:14 +0000

    INTEGRATION: CWS pchfix02 (1.15.2); FILE MERGED 2006/09/01 17:53:16 kaib 1.15.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/drawbase.cxx
+++ sw/source/ui/ribbar/drawbase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawbase.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:52:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:11:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include <hintids.hxx>
 #ifndef _UIPARAM_HXX
 #include <uiparam.hxx>
