7fcf9139e0f477c0bf48431996d61d5ee45c0a49
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:42:12 +0000

    INTEGRATION: CWS pchfix02 (1.11.280); FILE MERGED 2006/09/01 17:37:01 kaib 1.11.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/prntopts.cxx
+++ sd/source/ui/dlg/prntopts.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prntopts.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:16:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:42:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #include "sdattr.hxx"
 #include "optsitem.hxx"
