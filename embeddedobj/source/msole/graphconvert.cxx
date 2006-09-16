0e977b76c310ae01e1121985803f3c5bb0e9d28a
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:43:49 +0000

    INTEGRATION: CWS pchfix02 (1.7.36); FILE MERGED 2006/09/01 17:25:52 kaib 1.7.36.1: #i68856# Added header markers and pch files

--- embeddedobj/source/msole/graphconvert.cxx
+++ embeddedobj/source/msole/graphconvert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: graphconvert.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 19:05:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:43:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
