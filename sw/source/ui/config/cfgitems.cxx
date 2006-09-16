204dcdc6abe90d1520c5e709d4b10823b50a6cd8
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:39:38 +0000

    INTEGRATION: CWS pchfix02 (1.12.474); FILE MERGED 2006/09/01 17:52:54 kaib 1.12.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/cfgitems.cxx
+++ sw/source/ui/config/cfgitems.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfgitems.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:41:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:39:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef __SBX_SBXVARIABLE_HXX //autogen
 #include <basic/sbxvar.hxx>
