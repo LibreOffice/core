5b76b0e11fa9341ababa1b7aabb5f9a407e797a8
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:26:06 +0000

    INTEGRATION: CWS pchfix02 (1.16.52); FILE MERGED 2006/09/01 17:29:30 kaib 1.16.52.1: #i68856# Added header markers and pch files

--- framework/source/uielement/toolbarsmenucontroller.cxx
+++ framework/source/uielement/toolbarsmenucontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toolbarsmenucontroller.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:42:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:26:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_UIELEMENT_TOOLBARSMENUCONTROLLER_HXX_
 #include <uielement/toolbarsmenucontroller.hxx>
