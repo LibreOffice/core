a6be399952237448de3f7a14cbe10ae8021149a0
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:22:53 +0000

    INTEGRATION: CWS pchfix02 (1.96.2); FILE MERGED 2006/09/01 17:51:51 kaib 1.96.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: paintfrm.cxx,v $
  *
- *  $Revision: 1.97 $
+ *  $Revision: 1.98 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 07:51:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:22:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #include <hintids.hxx>
