c2c827a41719769090f096e7e0ecdeddce012149
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:20:11 +0000

    INTEGRATION: CWS pchfix02 (1.26.2); FILE MERGED 2006/09/01 17:51:49 kaib 1.26.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/laycache.cxx
+++ sw/source/core/layout/laycache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: laycache.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:27:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:20:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <hintids.hxx>
 #ifndef _SVX_BRKITEM_HXX //autogen
