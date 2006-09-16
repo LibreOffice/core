c060b07801579df4b3598b1f610a945ae9cf170a
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:32:47 +0000

    INTEGRATION: CWS pchfix02 (1.9.10); FILE MERGED 2006/09/01 17:41:14 kaib 1.9.10.1: #i68856# Added header markers and pch files

--- stoc/source/registry_tdprovider/rdbtdp_tdenumeration.cxx
+++ stoc/source/registry_tdprovider/rdbtdp_tdenumeration.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rdbtdp_tdenumeration.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:04:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:32:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 //=========================================================================
 // Todo:
