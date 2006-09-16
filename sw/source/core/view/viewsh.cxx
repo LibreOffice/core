8ba4a56d92998e0560f2deea6f555b63b94bd552
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:03:15 +0000

    INTEGRATION: CWS pchfix02 (1.64.2); FILE MERGED 2006/09/01 17:52:25 kaib 1.64.2.1: #i68856# Added header markers and pch files

--- sw/source/core/view/viewsh.cxx
+++ sw/source/core/view/viewsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewsh.cxx,v $
  *
- *  $Revision: 1.64 $
+ *  $Revision: 1.65 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:59:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:03:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define _SVX_PARAITEM_HXX
 #define _SVX_TEXTITEM_HXX
