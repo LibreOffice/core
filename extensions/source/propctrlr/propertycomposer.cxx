7e92bbcb2d9bc50e1e6c6814110ad25ff7d80ac7
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:21:53 +0000

    INTEGRATION: CWS pchfix02 (1.8.22); FILE MERGED 2006/09/01 17:26:43 kaib 1.8.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/propertycomposer.cxx
+++ extensions/source/propctrlr/propertycomposer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertycomposer.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:59:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:21:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #ifndef EXTENSIONS_SOURCE_PROPCTRLR_PROPERTYCOMPOSER_HXX
 #include "propertycomposer.hxx"
 #endif
