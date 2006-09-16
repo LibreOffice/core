7a7787933e1da44d80d5663becc088f692641d75
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:34:27 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:16:04 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- automation/source/miniapp/testapp.cxx
+++ automation/source/miniapp/testapp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testapp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:21:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:34:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 
 #ifndef _SVT_FILEDLG_HXX //autogen
 #include <svtools/filedlg.hxx>
