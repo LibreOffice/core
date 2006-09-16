010d18bdd049ddd35a9462570b239832867bf6de
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:39:41 +0000

    INTEGRATION: CWS pchfix02 (1.28.2); FILE MERGED 2006/09/01 17:53:36 kaib 1.28.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/wrtsh/select.cxx
+++ sw/source/ui/wrtsh/select.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: select.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:04:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:39:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <limits.h>
 
