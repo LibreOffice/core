f114b3547d350373d2e4d240edb3b22017b57592
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:34:52 +0000

    INTEGRATION: CWS pchfix02 (1.26.2); FILE MERGED 2006/09/01 17:52:50 kaib 1.26.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/app/docst.cxx
+++ sw/source/ui/app/docst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docst.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:26:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:34:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef  _COM_SUN_STAR_STYLE_XSTYLEFAMILIESSUPPLIER_HPP_
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #endif
