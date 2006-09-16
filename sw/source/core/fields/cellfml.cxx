3751fefd3d07d2b64557bf2e78aed4d915e22896
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:10:00 +0000

    INTEGRATION: CWS pchfix02 (1.9.2); FILE MERGED 2006/09/01 17:51:40 kaib 1.9.2.1: #i68856# Added header markers and pch files

--- sw/source/core/fields/cellfml.cxx
+++ sw/source/core/fields/cellfml.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cellfml.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:12:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:10:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <float.h>
 #ifndef _HINTIDS_HXX
