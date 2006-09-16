92761611922d8991f9abf77b1718dd6ec3c80a31
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:30:29 +0000

    INTEGRATION: CWS pchfix02 (1.4.202); FILE MERGED 2006/09/01 17:29:33 kaib 1.4.202.1: #i68856# Added header markers and pch files

--- framework/source/xml/menuconfiguration.cxx
+++ framework/source/xml/menuconfiguration.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: menuconfiguration.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:04:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:30:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
