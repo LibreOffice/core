b974e7989d9331c67d3e0fc5844a6fb05e263bb7
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:15:23 +0000

    INTEGRATION: CWS pchfix02 (1.40.2); FILE MERGED 2006/09/01 17:51:44 kaib 1.40.2.1: #i68856# Added header markers and pch files

--- sw/source/core/frmedt/tblsel.cxx
+++ sw/source/core/frmedt/tblsel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tblsel.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:16:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:15:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
