1cccb39b835144631e59951c4cf2ba02bd3df66a
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:44:03 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:51:17 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/core/bastyp/tabcol.cxx
+++ sw/source/core/bastyp/tabcol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabcol.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:02:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:44:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "tabcol.hxx"
 
