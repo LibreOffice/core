e7be41352eac3f3f69f9df9a1d9e2828ecfc05f5
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:30:07 +0000

    INTEGRATION: CWS pchfix02 (1.12.102); FILE MERGED 2006/09/01 17:36:53 kaib 1.12.102.1: #i68856# Added header markers and pch files

--- sd/source/ui/animations/SlideTransitionPane.cxx
+++ sd/source/ui/animations/SlideTransitionPane.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlideTransitionPane.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:44:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:30:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_ANIMATIONS_XANIMATIONNODE_HPP_
 #include <com/sun/star/animations/XAnimationNode.hpp>
