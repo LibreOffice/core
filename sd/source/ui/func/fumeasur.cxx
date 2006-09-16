b7253538d2538f8565664c5806bdd9b095d4663b
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:51:55 +0000

    INTEGRATION: CWS pchfix02 (1.6.214); FILE MERGED 2006/09/01 17:37:07 kaib 1.6.214.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fumeasur.cxx
+++ sd/source/ui/func/fumeasur.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fumeasur.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 17:00:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:51:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fumeasur.hxx"
 
