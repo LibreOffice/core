1e71ddf49d58f2ec301fe2ba3361d9acac02ba2d
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:56:08 +0000

    INTEGRATION: CWS pchfix02 (1.9.10); FILE MERGED 2006/09/01 17:28:04 kaib 1.9.10.1: #i68856# Added header markers and pch files

--- fpicker/source/win32/filepicker/dibpreview.cxx
+++ fpicker/source/win32/filepicker/dibpreview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dibpreview.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-03 12:31:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:56:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_fpicker.hxx"
 
 //------------------------------------------------------------------------
 // includes
