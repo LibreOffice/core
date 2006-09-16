899354f4156da9295bc005c7c71aa8ac87911cdb
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:32:06 +0000

    INTEGRATION: CWS pchfix02 (1.9.36); FILE MERGED 2006/09/01 17:37:33 kaib 1.9.36.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/PrintManager.cxx
+++ sd/source/ui/view/PrintManager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PrintManager.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 10:32:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:32:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "PrintManager.hxx"
 
