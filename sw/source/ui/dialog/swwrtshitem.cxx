06ad62eb8018c4eae71fb58fca7517a22c437812
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:50:52 +0000

    INTEGRATION: CWS pchfix02 (1.5.474); FILE MERGED 2006/09/01 17:53:01 kaib 1.5.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/dialog/swwrtshitem.cxx
+++ sw/source/ui/dialog/swwrtshitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swwrtshitem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:16:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:50:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "swwrtshitem.hxx"
 TYPEINIT1(SwWrtShellItem,SfxPoolItem);
 SwWrtShellItem::SwWrtShellItem( USHORT nWhich, SwWrtShell* pSh )
