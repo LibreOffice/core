cf3b66005dbe3483231a6d2b0f1ac7cbe4e17c22
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:41:48 +0000

    INTEGRATION: CWS pchfix02 (1.7.16); FILE MERGED 2006/09/01 17:25:51 kaib 1.7.16.1: #i68856# Added header markers and pch files

--- embeddedobj/source/commonembedding/visobj.cxx
+++ embeddedobj/source/commonembedding/visobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: visobj.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:29:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:41:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_ASPECTS_HPP_
 #include <com/sun/star/embed/Aspects.hpp>
