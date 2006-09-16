ab8f457b11cebd7e95052b1b800bb96148ce7c7b
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:35:55 +0000

    INTEGRATION: CWS pchfix02 (1.24.198); FILE MERGED 2006/09/01 17:37:36 kaib 1.24.198.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/drtxtob1.cxx
+++ sd/source/ui/view/drtxtob1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drtxtob1.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 14:33:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:35:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "TextObjectBar.hxx"
 
@@ -126,7 +129,6 @@
 #include <svtools/itemiter.hxx>
 #endif
 
-#pragma hdrstop
 
 #include "app.hrc"
 
