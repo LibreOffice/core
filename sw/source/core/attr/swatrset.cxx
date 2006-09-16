4708ed647a1396126779d3f6c82a9a13beabdb4f
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:41:13 +0000

    INTEGRATION: CWS pchfix02 (1.8.2); FILE MERGED 2006/09/01 17:51:15 kaib 1.8.2.1: #i68856# Added header markers and pch files

--- sw/source/core/attr/swatrset.cxx
+++ sw/source/core/attr/swatrset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swatrset.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:49:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:40:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <hintids.hxx>
 
--- sw/source/core/bastyp/bparr.cxx
+++ sw/source/core/bastyp/bparr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bparr.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:49:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:41:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <string.h>
 #include <limits.h>
