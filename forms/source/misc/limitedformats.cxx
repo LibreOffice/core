2a85abb1c1461e596c5b28e19525a485673cdbee
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:57:52 +0000

    INTEGRATION: CWS pchfix02 (1.7.26); FILE MERGED 2006/09/01 17:27:41 kaib 1.7.26.1: #i68856# Added header markers and pch files

--- forms/source/misc/limitedformats.cxx
+++ forms/source/misc/limitedformats.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: limitedformats.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:58:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:57:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FORMS_LIMITED_FORMATS_HXX_
 #include "limitedformats.hxx"
