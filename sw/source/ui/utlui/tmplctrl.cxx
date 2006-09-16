1167c9cccfeebef7474db01cd0e633dcc372d47d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:35:34 +0000

    INTEGRATION: CWS pchfix02 (1.5.474); FILE MERGED 2006/09/01 17:53:33 kaib 1.5.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/tmplctrl.cxx
+++ sw/source/ui/utlui/tmplctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tmplctrl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:31:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:35:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 // include ---------------------------------------------------------------
 
-#pragma hdrstop
 
 #ifndef _SFXSTYLE_HXX //autogen
 #include <svtools/style.hxx>
