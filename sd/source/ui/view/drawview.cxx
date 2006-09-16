5aa746c6932b10437725f746e765701c8f40a7ea
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:35:15 +0000

    INTEGRATION: CWS pchfix02 (1.40.24); FILE MERGED 2006/09/01 17:37:35 kaib 1.40.24.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/drawview.cxx
+++ sd/source/ui/view/drawview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawview.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:24:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:35:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SFXDISPATCH_HXX //autogen
 #include <sfx2/dispatch.hxx>
