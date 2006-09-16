a745cc223c1cedcccfd56c6fef748ea9d6b566db
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:19:48 +0000

    INTEGRATION: CWS pchfix02 (1.14.38); FILE MERGED 2006/09/01 17:54:23 kaib 1.14.38.1: #i68856# Added header markers and pch files

--- toolkit/source/controls/unocontrolcontainer.cxx
+++ toolkit/source/controls/unocontrolcontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unocontrolcontainer.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:04:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:19:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 
 #ifndef _COM_SUN_STAR_AWT_XVCLCONTAINERPEER_HPP_
