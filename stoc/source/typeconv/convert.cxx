9ddf33e08fdc9900abd9896671804c9dedacd821
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:37:55 +0000

    INTEGRATION: CWS pchfix02 (1.15.8); FILE MERGED 2006/09/01 17:41:18 kaib 1.15.8.1: #i68856# Added header markers and pch files

--- stoc/source/typeconv/convert.cxx
+++ stoc/source/typeconv/convert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: convert.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:24:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:37:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 #include <osl/diagnose.h>
 #include <cppuhelper/factory.hxx>
