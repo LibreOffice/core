313e4e6fce92966446f18b51b310a7a4274f3fdc
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:56:26 +0000

    INTEGRATION: CWS pchfix02 (1.2.120); FILE MERGED 2006/09/01 17:54:53 kaib 1.2.120.1: #i68856# Added header markers and pch files

--- tools/source/generic/link.cxx
+++ tools/source/generic/link.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: link.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:21:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:56:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #ifndef _LINK_HXX
 #include <link.hxx>
