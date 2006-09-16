e80ee5766850d7b0a12b4fbb3a5105b0726e508f
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:39:35 +0000

    INTEGRATION: CWS pchfix02 (1.10.10); FILE MERGED 2006/09/01 17:15:29 kaib 1.10.10.1: #i68856# Added header markers and pch files

--- autodoc/source/display/html/cfrstd.cxx
+++ autodoc/source/display/html/cfrstd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfrstd.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-03 16:49:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:39:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
