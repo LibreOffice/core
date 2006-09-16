48aab9bb7ed8ba6d04a569e54a38ed1979e9a643
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:24:28 +0000

    INTEGRATION: CWS pchfix02 (1.91.2); FILE MERGED 2006/09/01 17:51:52 kaib 1.91.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/tabfrm.cxx
+++ sw/source/core/layout/tabfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabfrm.cxx,v $
  *
- *  $Revision: 1.91 $
+ *  $Revision: 1.92 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:29:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:24:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "pagefrm.hxx"
 #include "rootfrm.hxx"
 #include "cntfrm.hxx"
