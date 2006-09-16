cfd19337326ac450920277b16dba177f3fcb93d3
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:46:59 +0000

    INTEGRATION: CWS pchfix02 (1.8.214); FILE MERGED 2006/09/01 17:37:04 kaib 1.8.214.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fubullet.cxx
+++ sd/source/ui/func/fubullet.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fubullet.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 16:54:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:46:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fubullet.hxx"
 
