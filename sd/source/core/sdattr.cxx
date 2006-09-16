54bbaf3b1c50d5031d4dd98125392f13820343a3
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:15:31 +0000

    INTEGRATION: CWS pchfix02 (1.4.280); FILE MERGED 2006/09/01 17:36:42 kaib 1.4.280.1: #i68856# Added header markers and pch files

--- sd/source/core/sdattr.cxx
+++ sd/source/core/sdattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdattr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:13:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:15:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "sdattr.hxx"
 
