5f609d3addc63124f58c9bfca8b7bfb41ea25987
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:37:55 +0000

    INTEGRATION: CWS pchfix02 (1.65.2); FILE MERGED 2006/09/01 17:51:13 kaib 1.65.2.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accpara.cxx
+++ sw/source/core/access/accpara.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accpara.cxx,v $
  *
- *  $Revision: 1.65 $
+ *  $Revision: 1.66 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:46:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:37:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _TXTFRM_HXX
 #include <txtfrm.hxx>
