4de771717d013ea061d0a80b5ea2ac448f11a9a8
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:59:37 +0000

    INTEGRATION: CWS pchfix02 (1.4.280); FILE MERGED 2006/09/01 17:37:12 kaib 1.4.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/undoback.cxx
+++ sd/source/ui/func/undoback.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoback.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:55:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:59:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "undoback.hxx"
 #include "sdpage.hxx"
