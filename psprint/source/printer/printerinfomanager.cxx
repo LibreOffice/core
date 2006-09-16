65bf39225165fba7563c288022772af5e40ef8ba
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:35:59 +0000

    INTEGRATION: CWS pchfix02 (1.36.16); FILE MERGED 2006/09/01 17:32:55 kaib 1.36.16.1: #i68856# Added header markers and pch files

--- psprint/source/printer/printerinfomanager.cxx
+++ psprint/source/printer/printerinfomanager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: printerinfomanager.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 16:30:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:35:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_psprint.hxx"
 
 #include <unistd.h>
 #include <sys/wait.h>
