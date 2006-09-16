76afa21f3ed02d64929ad51f609855fa40a6b969
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:34:37 +0000

    INTEGRATION: CWS pchfix02 (1.3.280); FILE MERGED 2006/09/01 17:36:56 kaib 1.3.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/SpellDialogChildWindow.cxx
+++ sd/source/ui/dlg/SpellDialogChildWindow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SpellDialogChildWindow.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:50:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:34:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "SpellDialogChildWindow.hxx"
 #include <svx/svxids.hrc>
