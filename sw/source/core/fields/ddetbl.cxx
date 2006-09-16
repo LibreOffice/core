cff73231f451a3b3621058af2168dd62e73d16a9
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:10:55 +0000

    INTEGRATION: CWS pchfix02 (1.5.2); FILE MERGED 2006/09/01 17:51:41 kaib 1.5.2.1: #i68856# Added header markers and pch files

--- sw/source/core/fields/ddetbl.cxx
+++ sw/source/core/fields/ddetbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ddetbl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:12:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:10:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _FRMFMT_HXX //autogen
 #include <frmfmt.hxx>
