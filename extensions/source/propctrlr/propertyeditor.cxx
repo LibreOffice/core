1aa8be1d94dc9578acd1cbdac23ce3821fb075c9
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:22:08 +0000

    INTEGRATION: CWS pchfix02 (1.16.94); FILE MERGED 2006/09/01 17:26:43 kaib 1.16.94.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/propertyeditor.cxx
+++ extensions/source/propctrlr/propertyeditor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertyeditor.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:30:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:22:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_PROPCTRLR_PROPERTYEDITOR_HXX_
 #include "propertyeditor.hxx"
