96b35402149caefa0bd6a507da7bd1b1aff83721
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:57:07 +0000

    INTEGRATION: CWS pchfix02 (1.5.60); FILE MERGED 2006/09/01 17:20:23 kaib 1.5.60.1: #i68856# Added header markers and pch files

--- configmgr/source/api2/elementaccess.cxx
+++ configmgr/source/api2/elementaccess.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: elementaccess.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:12:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:57:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include "elementaccess.hxx"
 
