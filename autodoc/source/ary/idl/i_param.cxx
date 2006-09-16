61bd0c90e4466752b7b6ac8baf479f3181961239
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:23:26 +0000

    INTEGRATION: CWS pchfix02 (1.3.6); FILE MERGED 2006/09/01 17:15:19 kaib 1.3.6.1: #i68856# Added header markers and pch files

--- autodoc/source/ary/idl/i_param.cxx
+++ autodoc/source/ary/idl/i_param.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: i_param.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:50:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:23:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <ary/idl/i_param.hxx>
