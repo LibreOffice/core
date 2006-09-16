fe5ea7f4da8fe980663e6ee3943279f7a24e5991
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:28:04 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:29 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/SwXPrintPreviewSettings.cxx
+++ sw/source/ui/uno/SwXPrintPreviewSettings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXPrintPreviewSettings.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:17:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:28:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _SW_XPRINTPREVIEWSETTINGS_HXX_
 #include <SwXPrintPreviewSettings.hxx>
 #endif
