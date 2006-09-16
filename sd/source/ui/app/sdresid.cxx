0adb03317bd718cf69dd3a72e9b0a89dd5bd39cc
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:32:21 +0000

    INTEGRATION: CWS pchfix02 (1.2.282); FILE MERGED 2006/09/01 17:36:54 kaib 1.2.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/app/sdresid.cxx
+++ sd/source/ui/app/sdresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdresid.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:45:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:32:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _SHL_HXX //autogen
 #include <tools/shl.hxx>
