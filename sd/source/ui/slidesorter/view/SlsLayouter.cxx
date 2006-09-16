e682a3ebd9e45dbbbe0536b8283ce6a335d6eecd
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:10:25 +0000

    INTEGRATION: CWS pchfix02 (1.8.248); FILE MERGED 2006/09/01 17:37:20 kaib 1.8.248.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/view/SlsLayouter.cxx
+++ sd/source/ui/slidesorter/view/SlsLayouter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsLayouter.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-24 07:44:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:10:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "view/SlsLayouter.hxx"
 
