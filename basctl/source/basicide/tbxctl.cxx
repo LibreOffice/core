de1f39f97087cb7b5c668e81afc0577b998ce915
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:30:11 +0000

    INTEGRATION: CWS pchfix02 (1.12.100); FILE MERGED 2006/09/01 17:16:22 kaib 1.12.100.1: #i68856# Added header markers and pch files

--- basctl/source/basicide/tbxctl.cxx
+++ basctl/source/basicide/tbxctl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tbxctl.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:11:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:30:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basctl.hxx"
+
 
 #include <ide_pch.hxx>
 
-#pragma hdrstop
 
 #define _BASIDE_POPUPWINDOWTBX
 #include <tbxctl.hxx>
