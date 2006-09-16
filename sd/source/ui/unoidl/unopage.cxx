44d9e477c31a1256a052e338fdbcb4969c942151
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:27:39 +0000

    INTEGRATION: CWS pchfix02 (1.82.28); FILE MERGED 2006/09/01 17:37:30 kaib 1.82.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/unopage.cxx
+++ sd/source/ui/unoidl/unopage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unopage.cxx,v $
  *
- *  $Revision: 1.82 $
+ *  $Revision: 1.83 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:45:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:27:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
