5d21cf18592d87f68577f5c90a9605149734e69a
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:05:27 +0000

    INTEGRATION: CWS pchfix02 (1.25.2); FILE MERGED 2006/09/01 17:53:12 kaib 1.25.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/lingu/olmenu.cxx
+++ sw/source/ui/lingu/olmenu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: olmenu.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:56:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:05:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
