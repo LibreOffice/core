75e1841f7160d5431af564b1789472cbba356d42
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:36:14 +0000

    INTEGRATION: CWS pchfix02 (1.5.30); FILE MERGED 2006/09/01 17:16:05 kaib 1.5.30.1: #i68856# Added header markers and pch files

--- automation/source/server/retstrm.cxx
+++ automation/source/server/retstrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: retstrm.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-04-19 13:58:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:36:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
