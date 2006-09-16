d2416b89fe1f0f00c22c8d6111b1aa415c6d9b8f
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:41:42 +0000

    INTEGRATION: CWS pchfix02 (1.38.2); FILE MERGED 2006/09/01 17:51:16 kaib 1.38.2.1: #i68856# Added header markers and pch files

--- sw/source/core/bastyp/calc.cxx
+++ sw/source/core/bastyp/calc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: calc.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:49:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:41:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 
 #include <cctype>
