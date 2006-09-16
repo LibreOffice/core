3801e195d8dd5e35a98f5ad1c631e8495d850d93
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:34:21 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:33 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/prcntfld.cxx
+++ sw/source/ui/utlui/prcntfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prcntfld.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:30:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:34:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 // include ---------------------------------------------------------------
 
-#pragma hdrstop
 
 #include "prcntfld.hxx"
 
