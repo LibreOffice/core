c9b940ab361bc6aca8633ccc30bdeb25e7f187b1
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:21:42 +0000

    INTEGRATION: CWS pchfix02 (1.17.26); FILE MERGED 2006/09/01 17:36:47 kaib 1.17.26.1: #i68856# Added header markers and pch files

--- sd/source/filter/grf/sdgrffilter.cxx
+++ sd/source/filter/grf/sdgrffilter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdgrffilter.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:27:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:21:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #pragma warning (disable:4190)
 
