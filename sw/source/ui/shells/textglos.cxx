fcde4d6917bbbb547f9af46e177e0442f0302cb1
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:17:20 +0000

    INTEGRATION: CWS pchfix02 (1.8.2); FILE MERGED 2006/09/01 17:53:21 kaib 1.8.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/textglos.cxx
+++ sw/source/ui/shells/textglos.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textglos.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:54:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:17:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SFXREQUEST_HXX //autogen
 #include <sfx2/request.hxx>
