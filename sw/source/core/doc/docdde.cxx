ace2db5fba97cdb75a4b30d3e690c439303c3126
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:51:05 +0000

    INTEGRATION: CWS pchfix02 (1.10.2); FILE MERGED 2006/09/01 17:51:23 kaib 1.10.2.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/docdde.cxx
+++ sw/source/core/doc/docdde.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docdde.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:56:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:51:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <stdlib.h>
 
