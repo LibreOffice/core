89a05e64573fe341a30714a18c236c0a9d074973
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:54:24 +0000

    INTEGRATION: CWS pchfix02 (1.58.20); FILE MERGED 2006/09/01 17:54:51 kaib 1.58.20.1: #i68856# Added header markers and pch files

--- tools/source/fsys/urlobj.cxx
+++ tools/source/fsys/urlobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: urlobj.cxx,v $
  *
- *  $Revision: 1.58 $
+ *  $Revision: 1.59 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-20 16:11:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:54:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #ifndef _URLOBJ_HXX
 #include "urlobj.hxx"
