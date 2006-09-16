18e8488e7d6fb46a8f31606b98853b3536e7ddd8
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:59:16 +0000

    INTEGRATION: CWS pchfix02 (1.3.44); FILE MERGED 2006/09/01 17:54:55 kaib 1.3.44.1: #i68856# Added header markers and pch files

--- tools/source/memtools/table.cxx
+++ tools/source/memtools/table.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: table.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:48:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:59:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #define _TOOLS_TABLE_CXX
 
