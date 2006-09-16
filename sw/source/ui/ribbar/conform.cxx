f6ea81edf7a2d9b726c11e35955a964fd8bd6d2f
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:10:30 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:53:16 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/conform.cxx
+++ sw/source/ui/ribbar/conform.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: conform.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:51:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:10:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 
 #ifndef _SVX_FMGLOB_HXX //autogen
