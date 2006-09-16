86b01d828ad8aa6a118bd1d4894c27f3025b9956
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:14:24 +0000

    INTEGRATION: CWS pchfix02 (1.11.2); FILE MERGED 2006/09/01 17:51:44 kaib 1.11.2.1: #i68856# Added header markers and pch files

--- sw/source/core/frmedt/feflyole.cxx
+++ sw/source/core/frmedt/feflyole.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: feflyole.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:15:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:14:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_EMBEDSTATES_HPP_
 #include <com/sun/star/embed/EmbedStates.hpp>
