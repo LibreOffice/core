6524662eaff94f64fa558491fee50027b71b3606
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:56:55 +0000

    INTEGRATION: CWS pchfix02 (1.22.214); FILE MERGED 2006/09/01 17:37:10 kaib 1.22.214.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/futempl.cxx
+++ sd/source/ui/func/futempl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: futempl.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 17:05:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:56:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
-#pragma hdrstop
+
 #ifndef  _COM_SUN_STAR_STYLE_XSTYLEFAMILIESSUPPLIER_HPP_
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #endif
