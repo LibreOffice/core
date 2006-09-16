d227b6210f44c8ff5e760803f3e6bd2d4e7e6c73
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:13:26 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:36:41 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/core/CustomAnimationPreset.cxx
+++ sd/source/core/CustomAnimationPreset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CustomAnimationPreset.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:09:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:12:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_UTIL_XCLONEABLE_HPP_
 #include <com/sun/star/util/XCloneable.hpp>
