c7e6a81a3253eb101cfb4be02953d6aa5239ac5b
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:55:00 +0000

    INTEGRATION: CWS pchfix02 (1.5.52); FILE MERGED 2006/09/01 17:29:07 kaib 1.5.52.1: #i68856# Added header markers and pch files

--- framework/source/dispatch/loaddispatcher.cxx
+++ framework/source/dispatch/loaddispatcher.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: loaddispatcher.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:16:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:54:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_______________________________________________
 // my own includes
