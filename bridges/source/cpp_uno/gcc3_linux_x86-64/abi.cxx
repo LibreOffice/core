9248b5b616c039d50fd73264ccb7886453c3e463
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:50:11 +0000

    INTEGRATION: CWS pchfix02 (1.2.42); FILE MERGED 2006/09/01 17:17:34 kaib 1.2.42.1: #i68856# Added header markers and pch files

--- bridges/source/cpp_uno/gcc3_linux_x86-64/abi.cxx
+++ bridges/source/cpp_uno/gcc3_linux_x86-64/abi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: abi.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2006-03-08 08:49:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:50:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 
 // This is an implementation of the x86-64 ABI as described in 'System V
 // Application Binary Interface, AMD64 Architecture Processor Supplement'
