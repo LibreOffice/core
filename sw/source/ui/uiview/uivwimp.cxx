dd194d208faf589eee9af14e65cfa0fa8e28b755
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:23:37 +0000

    INTEGRATION: CWS pchfix02 (1.20.458); FILE MERGED 2006/09/01 17:53:25 kaib 1.20.458.1: #i68856# Added header markers and pch files

--- sw/source/ui/uiview/uivwimp.cxx
+++ sw/source/ui/uiview/uivwimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uivwimp.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 15:07:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:23:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 #include <cmdid.h>
 #include "globals.hrc"
 
