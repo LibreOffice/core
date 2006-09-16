32fd758a1d1f4c6b80c16b50c8177b106e77692e
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:44:03 +0000

    INTEGRATION: CWS pchfix02 (1.34.12); FILE MERGED 2006/09/01 17:25:53 kaib 1.34.12.1: #i68856# Added header markers and pch files

--- embeddedobj/source/msole/olecomponent.cxx
+++ embeddedobj/source/msole/olecomponent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: olecomponent.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 12:33:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:44:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_ILLEGALARGUMENTEXCEPTION_HPP_
 #include <com/sun/star/lang/IllegalArgumentException.hpp>
