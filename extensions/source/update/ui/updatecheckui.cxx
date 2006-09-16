e7ef45aeff9ea7381e60998c708cdc9483fc662c
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:30:50 +0000

    INTEGRATION: CWS pchfix02 (1.2.14); FILE MERGED 2006/09/01 17:26:49 kaib 1.2.14.1: #i68856# Added header markers and pch files

--- extensions/source/update/ui/updatecheckui.cxx
+++ extensions/source/update/ui/updatecheckui.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: updatecheckui.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ihi $ $Date: 2006-09-07 14:01:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:30:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #include <list>
 
