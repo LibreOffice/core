efdbab6f4288da6c9a51338c59df055caa247a87
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:43:55 +0000

    INTEGRATION: CWS pchfix02 (1.9.8); FILE MERGED 2006/09/01 17:31:25 kaib 1.9.8.1: #i68856# Added header markers and pch files

--- io/source/stm/omark.cxx
+++ io/source/stm/omark.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: omark.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:17:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:43:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 
 #include <map>
 #include <vector>
