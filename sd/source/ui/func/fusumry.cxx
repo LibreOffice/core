23d1b48ba82c0e6a2c0aa00abc957c7ba9aa4025
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:56:42 +0000

    INTEGRATION: CWS pchfix02 (1.9.200); FILE MERGED 2006/09/01 17:37:10 kaib 1.9.200.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fusumry.cxx
+++ sd/source/ui/func/fusumry.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fusumry.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 14:30:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:56:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fusumry.hxx"
 
