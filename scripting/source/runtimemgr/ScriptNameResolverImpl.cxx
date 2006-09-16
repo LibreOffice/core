7ba304511e086ce04aa4b76644945665a7af6e12
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:29:43 +0000

    INTEGRATION: CWS pchfix02 (1.29.36); FILE MERGED 2006/09/01 17:35:36 kaib 1.29.36.1: #i68856# Added header markers and pch files

--- scripting/source/runtimemgr/ScriptNameResolverImpl.cxx
+++ scripting/source/runtimemgr/ScriptNameResolverImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScriptNameResolverImpl.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:33:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:29:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_scripting.hxx"
 
 #include <vector>
 #include <stdlib.h>
