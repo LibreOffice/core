1dfd5dd24198014459dd83dbc0ea1129c64e599c
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:51:40 +0000

    INTEGRATION: CWS pchfix02 (1.15.24); FILE MERGED 2006/09/01 17:27:36 kaib 1.15.24.1: #i68856# Added header markers and pch files

--- forms/source/component/ImageButton.cxx
+++ forms/source/component/ImageButton.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImageButton.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:51:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:51:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FRM_IMAGE_BUTTON_HXX_
 #include "ImageButton.hxx"
