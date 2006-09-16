9394762b5970a99dabc9b984c89ce57cf92b8cc1
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:02:33 +0000

    INTEGRATION: CWS pchfix02 (1.17.2); FILE MERGED 2006/09/01 17:52:24 kaib 1.17.2.1: #i68856# Added header markers and pch files

--- sw/source/core/view/vdraw.cxx
+++ sw/source/core/view/vdraw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vdraw.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:58:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:02:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SVDMODEL_HXX //autogen
 #include <svx/svdmodel.hxx>
