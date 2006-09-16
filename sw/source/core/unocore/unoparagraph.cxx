b3a23ade9cce01cb325603f827d2b897be4e335e
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:58:49 +0000

    INTEGRATION: CWS pchfix02 (1.34.2); FILE MERGED 2006/09/01 17:52:21 kaib 1.34.2.1: #i68856# Added header markers and pch files

--- sw/source/core/unocore/unoparagraph.cxx
+++ sw/source/core/unocore/unoparagraph.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoparagraph.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:55:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:58:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <cmdid.h>
 #ifndef _UNOMID_H
