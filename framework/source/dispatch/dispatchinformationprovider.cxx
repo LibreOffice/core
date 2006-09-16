70a6ef9e976eab93fdb24a0c5a9bcb2ab01fc5d9
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:54:21 +0000

    INTEGRATION: CWS pchfix02 (1.3.202); FILE MERGED 2006/09/01 17:29:07 kaib 1.3.202.1: #i68856# Added header markers and pch files

--- framework/source/dispatch/dispatchinformationprovider.cxx
+++ framework/source/dispatch/dispatchinformationprovider.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dispatchinformationprovider.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:19:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:53:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
