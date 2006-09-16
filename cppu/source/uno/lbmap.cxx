f11bc0042d5d40eb3029d1d4fe9d5478bb81415f
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:21:32 +0000

    INTEGRATION: CWS pchfix02 (1.25.10); FILE MERGED 2006/09/01 17:23:03 kaib 1.25.10.1: #i68856# Added header markers and pch files

--- cppu/source/uno/lbmap.cxx
+++ cppu/source/uno/lbmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lbmap.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:14:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:21:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppu.hxx"
 
 #include <hash_map>
 #include <set>
