9a4fecf851f6af7cac145c64c4dc5bf805931c97
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:21:40 +0000

    INTEGRATION: CWS pchfix02 (1.32.22); FILE MERGED 2006/09/01 17:26:43 kaib 1.32.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/propcontroller.cxx
+++ extensions/source/propctrlr/propcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propcontroller.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:58:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:21:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #ifndef _EXTENSIONS_PROPCTRLR_PROPCONTROLLER_HXX_
 #include "propcontroller.hxx"
 #endif
