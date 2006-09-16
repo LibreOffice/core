c719e4eabe29b9e3ab380d2883d567e8d97ac3dc
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:13:47 +0000

    INTEGRATION: CWS pchfix02 (1.12.48); FILE MERGED 2006/09/01 17:53:18 kaib 1.12.48.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/drformsh.cxx
+++ sw/source/ui/shells/drformsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drformsh.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:43:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:13:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <hintids.hxx>
 #ifndef _SVX_HLNKITEM_HXX //autogen
