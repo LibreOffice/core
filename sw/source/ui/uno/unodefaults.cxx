6e9203303535682e3fc16afeaf8a73f2780edbfe
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:29:37 +0000

    INTEGRATION: CWS pchfix02 (1.6.454); FILE MERGED 2006/09/01 17:53:29 kaib 1.6.454.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/unodefaults.cxx
+++ sw/source/ui/uno/unodefaults.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unodefaults.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-28 11:31:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:29:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _UNODEFAULTS_HXX
 #include <unodefaults.hxx>
