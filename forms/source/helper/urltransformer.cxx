a4a7a5f97adb52df356bd4307f1f878426e334f0
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:57:25 +0000

    INTEGRATION: CWS pchfix02 (1.3.112); FILE MERGED 2006/09/01 17:27:40 kaib 1.3.112.1: #i68856# Added header markers and pch files

--- forms/source/helper/urltransformer.cxx
+++ forms/source/helper/urltransformer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: urltransformer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:54:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:56:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 #ifndef FORMS_SOURCE_INC_URLTRANSFORMER_HXX
 #include "urltransformer.hxx"
 #endif
