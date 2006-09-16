d73c8e12f77fffaccba9843d491907727ea256f0
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:20:10 +0000

    INTEGRATION: CWS pchfix02 (1.15.10); FILE MERGED 2006/09/01 17:23:01 kaib 1.15.10.1: #i68856# Added header markers and pch files

--- cppu/source/threadpool/threadpool.cxx
+++ cppu/source/threadpool/threadpool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: threadpool.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:12:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:20:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppu.hxx"
 #include <hash_set>
 #include <stdio.h>
 
