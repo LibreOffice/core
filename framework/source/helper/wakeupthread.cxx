b8f9ceba036233f0e5671a9c72a12eca135a095d
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:01:54 +0000

    INTEGRATION: CWS pchfix02 (1.4.202); FILE MERGED 2006/09/01 17:29:12 kaib 1.4.202.1: #i68856# Added header markers and pch files

--- framework/source/helper/wakeupthread.cxx
+++ framework/source/helper/wakeupthread.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wakeupthread.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:28:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:01:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_______________________________________________
 // include files of own module
