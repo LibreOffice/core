44e06c25985ba3addfdfac94339cd7c27c16d91a
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:12:38 +0000

    INTEGRATION: CWS pchfix02 (1.3.260); FILE MERGED 2006/09/01 17:32:23 kaib 1.3.260.1: #i68856# Added header markers and pch files

--- odk/examples/DevelopersGuide/OfficeDev/FilterDevelopment/FlatXmlFilterDetection/fdcomp.cxx
+++ odk/examples/DevelopersGuide/OfficeDev/FilterDevelopment/FlatXmlFilterDetection/fdcomp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fdcomp.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:38:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:12:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -37,6 +37,9 @@
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_odk.hxx"
 
 #include <stdio.h>
 
