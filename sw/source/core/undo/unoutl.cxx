076200a0af497f581529bc5f821191230d8957be
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:52:46 +0000

    INTEGRATION: CWS pchfix02 (1.4.476); FILE MERGED 2006/09/01 17:52:17 kaib 1.4.476.1: #i68856# Added header markers and pch files

--- sw/source/core/undo/unoutl.cxx
+++ sw/source/core/undo/unoutl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoutl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:21:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:52:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "doc.hxx"
 #include "swundo.hxx"           // fuer die UndoIds
