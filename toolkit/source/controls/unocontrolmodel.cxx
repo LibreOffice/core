1f2f3ffa83b804d35b665ea2e1baa9b00f39d256
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:20:19 +0000

    INTEGRATION: CWS pchfix02 (1.48.24); FILE MERGED 2006/09/01 17:54:24 kaib 1.48.24.1: #i68856# Added header markers and pch files

--- toolkit/source/controls/unocontrolmodel.cxx
+++ toolkit/source/controls/unocontrolmodel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unocontrolmodel.cxx,v $
  *
- *  $Revision: 1.48 $
+ *  $Revision: 1.49 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 15:56:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:20:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef _COM_SUN_STAR_BEANS_PROPERTYSTATE_HPP_
 #include <com/sun/star/beans/PropertyState.hpp>
