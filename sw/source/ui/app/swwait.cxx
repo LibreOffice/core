447b3bd78716a7e2b2858083a40f6cbcc6227312
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:36:25 +0000

    INTEGRATION: CWS pchfix02 (1.6.390); FILE MERGED 2006/09/01 17:52:51 kaib 1.6.390.1: #i68856# Added header markers and pch files

--- sw/source/ui/app/swwait.cxx
+++ sw/source/ui/app/swwait.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swwait.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2005-11-16 13:54:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:36:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SFXVIEWFRM_HXX //autogen wg. SfxViewFrame
 #include <sfx2/viewfrm.hxx>
