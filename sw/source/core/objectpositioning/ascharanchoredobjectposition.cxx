f423a4d1d46461f717ce388a9e4bec8ecabb937f
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:25:53 +0000

    INTEGRATION: CWS pchfix02 (1.5.474); FILE MERGED 2006/09/01 17:51:54 kaib 1.5.474.1: #i68856# Added header markers and pch files

--- sw/source/core/objectpositioning/ascharanchoredobjectposition.cxx
+++ sw/source/core/objectpositioning/ascharanchoredobjectposition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ascharanchoredobjectposition.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:34:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:25:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _ASCHARANCHOREDOBJECTPOSITION_HXX
 #include <ascharanchoredobjectposition.hxx>
 #endif
