48676611b30cb6beb832663c8106424116d4e4b2
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:25:11 +0000

    INTEGRATION: CWS pchfix02 (1.10.476); FILE MERGED 2006/09/01 17:51:54 kaib 1.10.476.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/virtoutp.cxx
+++ sw/source/core/layout/virtoutp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: virtoutp.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:32:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:25:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SV_WINDOW_HXX //autogen
 #include <vcl/window.hxx>
