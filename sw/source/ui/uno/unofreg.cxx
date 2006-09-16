d4f1555e0e82727f2a7f6e85238eca354276ace4
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:30:18 +0000

    INTEGRATION: CWS pchfix02 (1.14.2); FILE MERGED 2006/09/01 17:53:30 kaib 1.14.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/unofreg.cxx
+++ sw/source/ui/uno/unofreg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unofreg.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:00:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:30:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "SwXFilterOptions.hxx"
 
 #ifndef _SAL_TYPES_H_
