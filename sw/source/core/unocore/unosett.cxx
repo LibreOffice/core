dbebba8ef2bf8f8387f8eb3505c484d4a48b9af6
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:00:48 +0000

    INTEGRATION: CWS pchfix02 (1.47.2); FILE MERGED 2006/09/01 17:52:23 kaib 1.47.2.1: #i68856# Added header markers and pch files

--- sw/source/core/unocore/unosett.cxx
+++ sw/source/core/unocore/unosett.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unosett.cxx,v $
  *
- *  $Revision: 1.47 $
+ *  $Revision: 1.48 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:57:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:00:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <swtypes.hxx>
 #include <cmdid.h>
