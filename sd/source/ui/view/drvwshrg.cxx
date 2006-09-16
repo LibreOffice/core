167e1231bef6ea9bacfc31890f944abb7623dff1
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:40:31 +0000

    INTEGRATION: CWS pchfix02 (1.17.144); FILE MERGED 2006/09/01 17:37:39 kaib 1.17.144.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/drvwshrg.cxx
+++ sd/source/ui/view/drvwshrg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drvwshrg.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:45:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:40:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "DrawViewShell.hxx"
 
@@ -63,7 +66,6 @@
 #include <avmedia/mediaplayer.hxx>
 #endif
 
-#pragma hdrstop
 
 #include "app.hrc"
 #include "strings.hrc"
