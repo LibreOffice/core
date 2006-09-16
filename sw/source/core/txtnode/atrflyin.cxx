2febd7c56b6cf76a44a200413db143701875d4f4
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:44:03 +0000

    INTEGRATION: CWS pchfix02 (1.6.2); FILE MERGED 2006/09/01 17:52:10 kaib 1.6.2.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/atrflyin.cxx
+++ sw/source/core/txtnode/atrflyin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atrflyin.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:46:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:44:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "hintids.hxx"
 #include "cntfrm.hxx"       // _GetFly
