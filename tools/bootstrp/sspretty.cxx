17ec2b361c24fbd5ce25868042a124f7c5859942
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:49:49 +0000

    INTEGRATION: CWS pchfix02 (1.2.120); FILE MERGED 2006/09/01 17:54:47 kaib 1.2.120.1: #i68856# Added header markers and pch files

--- tools/bootstrp/sspretty.cxx
+++ tools/bootstrp/sspretty.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sspretty.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:32:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:49:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 #include "iparser.hxx"
 #include "geninfo.hxx"
 #include <stdio.h>
