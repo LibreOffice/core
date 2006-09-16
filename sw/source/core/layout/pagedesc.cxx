43b6dc011970f96a1bc82a61abf223d9ed97bae1
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:22:21 +0000

    INTEGRATION: CWS pchfix02 (1.12.474); FILE MERGED 2006/09/01 17:51:51 kaib 1.12.474.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/pagedesc.cxx
+++ sw/source/core/layout/pagedesc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pagedesc.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:25:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:22:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #include <hintids.hxx>
