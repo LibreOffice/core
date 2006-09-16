3dc59d58ad8004dc0e452e81ad5f6ba50b9e30d5
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:01:20 +0000

    INTEGRATION: CWS pchfix02 (1.4.476); FILE MERGED 2006/09/01 17:53:09 kaib 1.4.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/frmdlg/colmgr.cxx
+++ sw/source/ui/frmdlg/colmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: colmgr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:49:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:01:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "hintids.hxx"
 
