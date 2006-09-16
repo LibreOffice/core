5e74e9ec125caff73b123ceafb2811bad693ef5e
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:53:58 +0000

    INTEGRATION: CWS pchfix02 (1.10.238); FILE MERGED 2006/09/01 17:53:04 kaib 1.10.238.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/envimg.cxx
+++ sw/source/ui/envelp/envimg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: envimg.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-16 12:46:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:53:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
