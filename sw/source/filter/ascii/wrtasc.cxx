1a31bddcfe8aad8d874c74a6abeeb4cc9b9f3804
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:04:23 +0000

    INTEGRATION: CWS pchfix02 (1.9.474); FILE MERGED 2006/09/01 17:52:26 kaib 1.9.474.1: #i68856# Added header markers and pch files

--- sw/source/filter/ascii/wrtasc.cxx
+++ sw/source/filter/ascii/wrtasc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrtasc.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:34:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:04:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
