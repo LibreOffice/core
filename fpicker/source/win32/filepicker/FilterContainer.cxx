83e0d423f14f617a2f1c114f71f6a94355ed28b6
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:53:48 +0000

    INTEGRATION: CWS pchfix02 (1.3.104); FILE MERGED 2006/09/01 17:28:03 kaib 1.3.104.1: #i68856# Added header markers and pch files

--- fpicker/source/win32/filepicker/FilterContainer.cxx
+++ fpicker/source/win32/filepicker/FilterContainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FilterContainer.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:38:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:53:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_fpicker.hxx"
 
 
 #include <stdexcept>
