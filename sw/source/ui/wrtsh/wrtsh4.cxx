52f1820fbf0bfdb868995da714790dd23b19e616
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:40:39 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:37 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/wrtsh/wrtsh4.cxx
+++ sw/source/ui/wrtsh/wrtsh4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrtsh4.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:41:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:40:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
