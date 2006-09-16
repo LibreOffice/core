85439fa14cb9bf3d0a5170d4fe138eb2a5d60944
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:12:37 +0000

    INTEGRATION: CWS pchfix02 (1.28.170); FILE MERGED 2006/09/01 17:53:17 kaib 1.28.170.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/workctrl.cxx
+++ sw/source/ui/ribbar/workctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: workctrl.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:22:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:12:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <string> // HACK: prevent conflict between STLPORT and Workshop headers
 
