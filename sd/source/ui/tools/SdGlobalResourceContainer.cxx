fa597ed8f4fe642f4b561a6524ed60d33af7c07a
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:22:21 +0000

    INTEGRATION: CWS pchfix02 (1.4.104); FILE MERGED 2006/09/01 17:37:27 kaib 1.4.104.1: #i68856# Added header markers and pch files

--- sd/source/ui/tools/SdGlobalResourceContainer.cxx
+++ sd/source/ui/tools/SdGlobalResourceContainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SdGlobalResourceContainer.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:53:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:22:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "tools/SdGlobalResourceContainer.hxx"
 
