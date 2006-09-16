e58d07fd29a69aa6619742cb001277588e61f246
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:30:33 +0000

    INTEGRATION: CWS pchfix02 (1.21.2); FILE MERGED 2006/09/01 17:53:30 kaib 1.21.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/unomailmerge.cxx
+++ sw/source/ui/uno/unomailmerge.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unomailmerge.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:01:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:30:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
