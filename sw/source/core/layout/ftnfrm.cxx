9a85f05393688bfe31e067565d245a47488dd69e
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:19:29 +0000

    INTEGRATION: CWS pchfix02 (1.32.2); FILE MERGED 2006/09/01 17:51:49 kaib 1.32.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/ftnfrm.cxx
+++ sw/source/core/layout/ftnfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ftnfrm.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:26:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:19:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _TXTFTN_HXX
 #include <txtftn.hxx>
