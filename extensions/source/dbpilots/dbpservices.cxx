14f0eb85390191faf8180f4c71117627b3133d3b
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:59:15 +0000

    INTEGRATION: CWS pchfix02 (1.5.186); FILE MERGED 2006/09/01 17:26:29 kaib 1.5.186.1: #i68856# Added header markers and pch files

--- extensions/source/dbpilots/dbpservices.cxx
+++ extensions/source/dbpilots/dbpservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbpservices.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:28:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:59:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_COMPONENT_MODULE_HXX_
 #include "componentmodule.hxx"
