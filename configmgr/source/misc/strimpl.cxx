7e60c43c027e4151df9b68a732be68c64e236fd0
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:18:18 +0000

    INTEGRATION: CWS pchfix02 (1.15.60); FILE MERGED 2006/09/01 17:20:40 kaib 1.15.60.1: #i68856# Added header markers and pch files

--- configmgr/source/misc/strimpl.cxx
+++ configmgr/source/misc/strimpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strimpl.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:13:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:18:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #ifndef _CONFIGMGR_STRDECL_HXX_
 #include "strdecl.hxx"
