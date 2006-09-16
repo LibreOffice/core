9eb13da8ca2355ba2760ae68b9f46f681e8736e8
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:38:14 +0000

    INTEGRATION: CWS pchfix02 (1.9.404); FILE MERGED 2006/09/01 17:52:53 kaib 1.9.404.1: #i68856# Added header markers and pch files

--- sw/source/ui/chrdlg/numpara.cxx
+++ sw/source/ui/chrdlg/numpara.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: numpara.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-08 17:31:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:38:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #include "cmdid.h"
 #include "swtypes.hxx"
