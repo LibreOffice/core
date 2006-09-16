899153a35b27e9aafe12c897b331f706ee521e7d
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:29:37 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:37:32 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/unowcntr.cxx
+++ sd/source/ui/unoidl/unowcntr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unowcntr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:56:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:29:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
 #include <com/sun/star/lang/XComponent.hpp>
