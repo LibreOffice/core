53e0ed149c246bb9e766fa23507f98f335922f99
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:03:06 +0000

    INTEGRATION: CWS pchfix02 (1.5.26); FILE MERGED 2006/09/01 17:27:45 kaib 1.5.26.1: #i68856# Added header markers and pch files

--- forms/source/xforms/computedexpression.cxx
+++ forms/source/xforms/computedexpression.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: computedexpression.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:03:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:03:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #include "computedexpression.hxx"
 #include "unohelper.hxx"
