485f4a7067ce1a46345e30f04a3ba24e31b6bf8d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:32:38 +0000

    INTEGRATION: CWS pchfix02 (1.40.2); FILE MERGED 2006/09/01 17:53:32 kaib 1.40.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/content.cxx
+++ sw/source/ui/utlui/content.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: content.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:02:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:32:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _SVSTDARR_HXX
 #define _SVSTDARR_STRINGSDTOR
