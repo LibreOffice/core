cbf9ba5202d162bc9e566ac6bb78385b3b4c778a
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:54:50 +0000

    INTEGRATION: CWS pchfix02 (1.6.42); FILE MERGED 2006/09/01 17:54:52 kaib 1.6.42.1: #i68856# Added header markers and pch files

--- tools/source/fsys/wntmsc.cxx
+++ tools/source/fsys/wntmsc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wntmsc.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:43:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:54:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #pragma warning (push,1)
 #include <stdio.h>
