f32f9715751e4cc74dd551aa7e402e2fc9b43768
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:14:30 +0000

    INTEGRATION: CWS pchfix02 (1.26.2); FILE MERGED 2006/09/01 17:52:33 kaib 1.26.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/rtf/rtffly.cxx
+++ sw/source/filter/rtf/rtffly.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rtffly.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:09:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:14:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
