a624e2ccbbc1a337e2a009b43ef527f1c6d92a84
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:36:17 +0000

    INTEGRATION: CWS pchfix02 (1.10.20); FILE MERGED 2006/09/01 17:32:55 kaib 1.10.20.1: #i68856# Added header markers and pch files

--- psprint/source/printergfx/bitmap_gfx.cxx
+++ psprint/source/printergfx/bitmap_gfx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bitmap_gfx.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:26:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:36:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_psprint.hxx"
 
 #ifndef _PSPRINT_PRINTERGFX_HXX_
 #include <psprint/printergfx.hxx>
