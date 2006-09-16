ad92b214acb123841fca52690d99e060b570cdb6
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:53:42 +0000

    INTEGRATION: CWS pchfix02 (1.8.42); FILE MERGED 2006/09/01 17:54:51 kaib 1.8.42.1: #i68856# Added header markers and pch files

--- tools/source/fsys/tdir.cxx
+++ tools/source/fsys/tdir.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tdir.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:42:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:53:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #define _DIR_CXX
 
