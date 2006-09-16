dafe955e592d6e76ed85c482e3ff4f246e0b4dc1
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:19:21 +0000

    INTEGRATION: CWS pchfix02 (1.11.2); FILE MERGED 2006/09/01 17:52:37 kaib 1.11.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/ww1/w1filter.cxx
+++ sw/source/filter/ww1/w1filter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: w1filter.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:13:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:19:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
