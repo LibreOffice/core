5725d7e6240a2603b8d155baf1b3eaa23b079ff6
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:40:50 +0000

    INTEGRATION: CWS pchfix02 (1.32.196); FILE MERGED 2006/09/01 17:37:00 kaib 1.32.196.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/navigatr.cxx
+++ sd/source/ui/dlg/navigatr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: navigatr.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-20 09:19:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:40:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _URLOBJ_HXX //autogen
 #include <tools/urlobj.hxx>
