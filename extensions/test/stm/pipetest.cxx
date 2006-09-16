d27bb58727ebed1451dcd3d020030c7fa343781d
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:34:29 +0000

    INTEGRATION: CWS pchfix02 (1.4.184); FILE MERGED 2006/09/01 17:26:53 kaib 1.4.184.1: #i68856# Added header markers and pch files

--- extensions/test/stm/pipetest.cxx
+++ extensions/test/stm/pipetest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pipetest.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:00:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:34:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #include <smart/com/sun/star/test/XSimpleTest.hxx>
 #include <smart/com/sun/star/io/XOutputStream.hxx>
 #include <smart/com/sun/star/io/XInputStream.hxx>
