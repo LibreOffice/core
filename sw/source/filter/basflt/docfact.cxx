ee1a8621ec349c83f3b1d7ed09144dc5ff04fa0e
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:04:37 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:52:26 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/basflt/docfact.cxx
+++ sw/source/filter/basflt/docfact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docfact.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:00:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:04:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _REF_HXX
 #include <tools/ref.hxx>
