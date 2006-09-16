ef5219e4222b202b38a2bda92705d982d4b2b26d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:15:52 +0000

    INTEGRATION: CWS pchfix02 (1.7.48); FILE MERGED 2006/09/01 17:53:20 kaib 1.7.48.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/mediash.cxx
+++ sw/source/ui/shells/mediash.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mediash.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:44:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:15:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 
 #ifndef _CMDID_H
