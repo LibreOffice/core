c2bb4245517e6b70b041af8feff1f3501d0fb14e
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:48:36 +0000

    INTEGRATION: CWS pchfix02 (1.4.66); FILE MERGED 2006/09/01 18:00:58 kaib 1.4.66.1: #i68856# Added header markers and pch files

--- xmlsecurity/tools/demo/util2.cxx
+++ xmlsecurity/tools/demo/util2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: util2.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-11 09:21:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:48:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlsecurity.hxx"
 
 #include <rtl/locale.h>
 #include <osl/nlsupport.h>
