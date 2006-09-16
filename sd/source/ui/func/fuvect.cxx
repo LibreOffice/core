8b15e3f164c2d4c6eb5363fd1902241d9a3cf40e
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:58:23 +0000

    INTEGRATION: CWS pchfix02 (1.8.28); FILE MERGED 2006/09/01 17:37:11 kaib 1.8.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fuvect.cxx
+++ sd/source/ui/func/fuvect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuvect.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:43:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:58:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fuvect.hxx"
 
@@ -52,7 +55,6 @@
 #include <svx/svdedtv.hxx>
 #endif
 
-#pragma hdrstop
 
 #ifndef SD_VIEW_HXX
 #include "View.hxx"
