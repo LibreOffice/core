89bdd019349f7b08cfb9e114fca1c38a4184b49c
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:39:00 +0000

    INTEGRATION: CWS pchfix02 (1.6.282); FILE MERGED 2006/09/01 17:36:59 kaib 1.6.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/gluectrl.cxx
+++ sd/source/ui/dlg/gluectrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gluectrl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:01:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:39:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include <string> // HACK: prevent conflict between STLPORT and Workshop headers
 
