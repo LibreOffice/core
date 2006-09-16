b6f3c5c2970222dd2a46fe3b27d5dec3504c2c0d
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:34:51 +0000

    INTEGRATION: CWS pchfix02 (1.9.104); FILE MERGED 2006/09/01 17:36:56 kaib 1.9.104.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/TemplateScanner.cxx
+++ sd/source/ui/dlg/TemplateScanner.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TemplateScanner.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:45:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:34:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "TemplateScanner.hxx"
 
