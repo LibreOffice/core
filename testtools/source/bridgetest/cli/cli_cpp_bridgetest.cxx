e29f11da0e83e81b49a132a27c6a263d0f66a036
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:37:56 +0000

    INTEGRATION: CWS pchfix02 (1.7.10); FILE MERGED 2006/09/01 17:53:59 kaib 1.7.10.1: #i68856# Added header markers and pch files

--- testtools/source/bridgetest/cli/cli_cpp_bridgetest.cxx
+++ testtools/source/bridgetest/cli/cli_cpp_bridgetest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cli_cpp_bridgetest.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:13:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:37:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_testtools.hxx"
 
 #using <mscorlib.dll>
 #using <System.dll>
