3764784eb44901024c7027b0c6178d084383054d
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:35:45 +0000

    INTEGRATION: CWS pchfix02 (1.26.14); FILE MERGED 2006/09/01 18:00:48 kaib 1.26.14.1: #i68856# Added header markers and pch files

--- xmlsecurity/source/dialogs/macrosecurity.cxx
+++ xmlsecurity/source/dialogs/macrosecurity.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: macrosecurity.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:13:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:35:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlsecurity.hxx"
 
 #include <xmlsecurity/macrosecurity.hxx>
 #include <xmlsecurity/certificatechooser.hxx>
