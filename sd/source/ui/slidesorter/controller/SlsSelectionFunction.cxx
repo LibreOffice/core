166c1f9f0d1e71cc12f502ecfd5e0ad4808d1141
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:07:25 +0000

    INTEGRATION: CWS pchfix02 (1.27.100); FILE MERGED 2006/09/01 17:37:18 kaib 1.27.100.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/controller/SlsSelectionFunction.cxx
+++ sd/source/ui/slidesorter/controller/SlsSelectionFunction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsSelectionFunction.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:45:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:07:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "controller/SlsSelectionFunction.hxx"
 
