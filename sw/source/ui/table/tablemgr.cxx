4039a3ada406865045cbf38cb7b2f8399dc5b6ee
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:21:46 +0000

    INTEGRATION: CWS pchfix02 (1.8.474); FILE MERGED 2006/09/01 17:53:24 kaib 1.8.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/table/tablemgr.cxx
+++ sw/source/ui/table/tablemgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tablemgr.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:06:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:21:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <sot/storage.hxx>
 #ifndef _SCH_DLL_HXX
