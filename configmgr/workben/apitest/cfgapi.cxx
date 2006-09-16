e015d03ea3ee49f347132b5de1e99351d2fd550f
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:37:35 +0000

    INTEGRATION: CWS pchfix02 (1.22.60); FILE MERGED 2006/09/01 17:20:55 kaib 1.22.60.1: #i68856# Added header markers and pch files

--- configmgr/workben/apitest/cfgapi.cxx
+++ configmgr/workben/apitest/cfgapi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfgapi.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:45:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:37:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 #define _PRIVATE_TEST_
 
 #include <iostream>
