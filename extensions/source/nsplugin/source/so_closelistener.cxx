5e7d1bd746e82ee8d0de6b65e8b24de490aec789
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:02:37 +0000

    INTEGRATION: CWS pchfix02 (1.2.102); FILE MERGED 2006/09/01 17:26:31 kaib 1.2.102.1: #i68856# Added header markers and pch files

--- extensions/source/nsplugin/source/so_closelistener.cxx
+++ extensions/source/nsplugin/source/so_closelistener.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: so_closelistener.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-09 13:53:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:02:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #include "so_closelistener.hxx"
 
