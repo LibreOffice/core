4743114242ada282aec124eeb2581bd18f43e9b6
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:28:04 +0000

    INTEGRATION: CWS pchfix02 (1.4.122); FILE MERGED 2006/09/01 17:37:31 kaib 1.4.122.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/unopool.cxx
+++ sd/source/ui/unoidl/unopool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unopool.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 15:01:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:28:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef INCLUDED_I18NPOOL_MSLANGID_HXX
 #include <i18npool/mslangid.hxx>
