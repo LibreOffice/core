8229b967ced44b095d3e01dcc17991551f9e1810
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:40:48 +0000

    INTEGRATION: CWS pchfix02 (1.10.2); FILE MERGED 2006/09/01 17:52:54 kaib 1.10.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/optcomp.cxx
+++ sw/source/ui/config/optcomp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optcomp.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:28:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:40:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
