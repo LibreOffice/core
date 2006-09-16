9b5f17a6bf230726e8c1668d0e40cefebb07fd97
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:32:38 +0000

    INTEGRATION: CWS pchfix02 (1.46.2); FILE MERGED 2006/09/01 17:52:49 kaib 1.46.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/xmltexti.cxx
+++ sw/source/filter/xml/xmltexti.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltexti.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:25:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:32:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <comphelper/storagehelper.hxx>
 #include <comphelper/processfactory.hxx>
