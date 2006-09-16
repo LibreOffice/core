8de73c846bcc94d3a1c8f4f80507ec7536c00fe5
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:43:09 +0000

    INTEGRATION: CWS pchfix02 (1.11.36); FILE MERGED 2006/09/01 17:25:52 kaib 1.11.36.1: #i68856# Added header markers and pch files

--- embeddedobj/source/general/xcreator.cxx
+++ embeddedobj/source/general/xcreator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xcreator.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-20 09:51:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:43:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
 #include <com/sun/star/embed/ElementModes.hpp>
