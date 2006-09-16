a8f9d81312e836958798ca7642380027e31fc606
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:42:13 +0000

    INTEGRATION: CWS pchfix02 (1.9.2); FILE MERGED 2006/09/01 17:52:08 kaib 1.9.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/txtpaint.cxx
+++ sw/source/core/text/txtpaint.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtpaint.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:45:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:42:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 
 #include "txtpaint.hxx"
