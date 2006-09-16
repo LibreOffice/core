da4cf4d8ddd9830636bf0736114f998c21350b1f
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:08:51 +0000

    INTEGRATION: CWS pchfix02 (1.8.2); FILE MERGED 2006/09/01 17:51:39 kaib 1.8.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edws.cxx
+++ sw/source/core/edit/edws.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edws.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:11:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:08:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 
 #ifndef _WINDOW_HXX //autogen
