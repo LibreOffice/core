860f269f09f65a2e5dd95062626b5fe82fc69a45
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:03:33 +0000

    INTEGRATION: CWS pchfix02 (1.4.112); FILE MERGED 2006/09/01 17:27:46 kaib 1.4.112.1: #i68856# Added header markers and pch files

--- forms/source/xforms/datatyperepository.cxx
+++ forms/source/xforms/datatyperepository.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datatyperepository.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:15:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:03:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef FORMS_SOURCE_XFORMS_DATATYPEREPOSITORY_HXX
 #include "datatyperepository.hxx"
