48c73c672dbcc1f37352210eab3cb4583352816c
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:32:23 +0000

    INTEGRATION: CWS pchfix02 (1.9.476); FILE MERGED 2006/09/01 17:53:31 kaib 1.9.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/condedit.cxx
+++ sw/source/ui/utlui/condedit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: condedit.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:24:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:32:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 // include ---------------------------------------------------------------
 
-#pragma hdrstop
 
 #ifndef _SOT_FORMATS_HXX //autogen
 #include <sot/formats.hxx>
