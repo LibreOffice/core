653a77931425c258493566173c95b87e76c5f6b6
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:24:39 +0000

    INTEGRATION: CWS pchfix02 (1.9.282); FILE MERGED 2006/09/01 17:37:29 kaib 1.9.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/facreg.cxx
+++ sd/source/ui/unoidl/facreg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: facreg.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:47:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:24:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_REGISTRY_XREGISTRYKEY_HPP_
 #include <com/sun/star/registry/XRegistryKey.hpp>
