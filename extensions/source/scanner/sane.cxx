13b057714f558fa1e4d3ef4d1b5ca3178d83324f
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:27:12 +0000

    INTEGRATION: CWS pchfix02 (1.11.94); FILE MERGED 2006/09/01 17:26:46 kaib 1.11.94.1: #i68856# Added header markers and pch files

--- extensions/source/scanner/sane.cxx
+++ extensions/source/scanner/sane.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sane.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-16 13:00:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:27:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #include <cstdarg>
 #include <math.h>
 #include <tools/stream.hxx>
