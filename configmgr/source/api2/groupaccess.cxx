e0a9c72365d3c226df61c12626b6d9f0dbd4a399
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:57:47 +0000

    INTEGRATION: CWS pchfix02 (1.3.60); FILE MERGED 2006/09/01 17:20:23 kaib 1.3.60.1: #i68856# Added header markers and pch files

--- configmgr/source/api2/groupaccess.cxx
+++ configmgr/source/api2/groupaccess.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: groupaccess.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:13:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:57:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include "groupaccess.hxx"
 #include "accessimpl.hxx"
