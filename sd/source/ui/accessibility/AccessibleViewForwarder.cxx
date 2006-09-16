5852c3bbf70dec52dd9cc5ae094328d49d8dd7d4
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:27:37 +0000

    INTEGRATION: CWS pchfix02 (1.6.280); FILE MERGED 2006/09/01 17:36:51 kaib 1.6.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/accessibility/AccessibleViewForwarder.cxx
+++ sd/source/ui/accessibility/AccessibleViewForwarder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleViewForwarder.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:29:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:27:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SVX_ACCESSIBILITY_ACCESSIBLE_VIEW_FORWARDER_HXX
 #include "AccessibleViewForwarder.hxx"
