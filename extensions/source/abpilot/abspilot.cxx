d92dec6fd483cf29ba8574002f98cc58d716b8d7
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:51:57 +0000

    INTEGRATION: CWS pchfix02 (1.14.96); FILE MERGED 2006/09/01 17:26:24 kaib 1.14.96.1: #i68856# Added header markers and pch files

--- extensions/source/abpilot/abspilot.cxx
+++ extensions/source/abpilot/abspilot.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: abspilot.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-28 10:38:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:51:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef EXTENSIONS_ABSPILOT_HXX
 #include "abspilot.hxx"
