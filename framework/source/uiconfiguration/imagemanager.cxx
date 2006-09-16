0ced1fdeafcb8aae634ccb8765e377643e6f6957
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:16:00 +0000

    INTEGRATION: CWS pchfix02 (1.9.52); FILE MERGED 2006/09/01 17:29:23 kaib 1.9.52.1: #i68856# Added header markers and pch files

--- framework/source/uiconfiguration/imagemanager.cxx
+++ framework/source/uiconfiguration/imagemanager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imagemanager.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:30:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:15:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_UICONFIGURATION_IMAGEMANAGER_HXX_
 #include <uiconfiguration/imagemanager.hxx>
--- framework/source/uiconfiguration/moduleuicfgsupplier.cxx
+++ framework/source/uiconfiguration/moduleuicfgsupplier.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: moduleuicfgsupplier.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:31:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:16:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_UICONFIGURATION_MODULEUICFGSUPPLIER_HXX_
 #include <uiconfiguration/moduleuicfgsupplier.hxx>
