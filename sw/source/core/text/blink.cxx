a3259329791a3348ac124d228d9557d6700cb7f1
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:33:25 +0000

    INTEGRATION: CWS pchfix02 (1.6.2); FILE MERGED 2006/09/01 17:52:01 kaib 1.6.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/blink.cxx
+++ sw/source/core/text/blink.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: blink.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:35:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:33:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _DEBUG_HXX //autogen
 #include <tools/debug.hxx>
