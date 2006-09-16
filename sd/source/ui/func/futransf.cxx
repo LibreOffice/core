b1c55d891cc1a6a4266d0d02415befc8e59ae68b
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:57:49 +0000

    INTEGRATION: CWS pchfix02 (1.8.30); FILE MERGED 2006/09/01 17:37:11 kaib 1.8.30.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/futransf.cxx
+++ sd/source/ui/func/futransf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: futransf.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:43:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:57:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "futransf.hxx"
 
