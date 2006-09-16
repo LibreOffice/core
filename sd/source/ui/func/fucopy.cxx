85fc425556d266219b203bddf634424da8323d09
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:48:59 +0000

    INTEGRATION: CWS pchfix02 (1.10.28); FILE MERGED 2006/09/01 17:37:06 kaib 1.10.28.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fucopy.cxx
+++ sd/source/ui/func/fucopy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fucopy.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 11:32:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:48:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fucopy.hxx"
 
