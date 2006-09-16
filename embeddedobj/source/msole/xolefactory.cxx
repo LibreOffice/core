5998237c206b3ea1633725150abbd1304d6845df
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:46:03 +0000

    INTEGRATION: CWS pchfix02 (1.7.18); FILE MERGED 2006/09/01 17:25:54 kaib 1.7.18.1: #i68856# Added header markers and pch files

--- embeddedobj/source/msole/xolefactory.cxx
+++ embeddedobj/source/msole/xolefactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xolefactory.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:31:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:46:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
 #include <com/sun/star/embed/ElementModes.hpp>
