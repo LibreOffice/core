578b4f88e6d0c4daa79c9a98531f1a0931525e78
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:15:05 +0000

    INTEGRATION: CWS pchfix02 (1.23.476); FILE MERGED 2006/09/01 17:53:19 kaib 1.23.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/grfsh.cxx
+++ sw/source/ui/shells/grfsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: grfsh.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:50:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:15:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 
 #ifndef _CMDID_H
