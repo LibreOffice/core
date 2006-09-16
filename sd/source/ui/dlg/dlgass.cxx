15917c65978b1ad7bc7aa97c975a1d04bdb4e599
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:36:29 +0000

    INTEGRATION: CWS pchfix02 (1.40.94); FILE MERGED 2006/09/01 17:36:57 kaib 1.40.94.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/dlgass.cxx
+++ sd/source/ui/dlg/dlgass.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgass.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:05:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:36:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
