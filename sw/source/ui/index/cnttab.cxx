aa67988f945bef9539010cbcf57b1ed31695ac2e
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:03:52 +0000

    INTEGRATION: CWS pchfix02 (1.68.2); FILE MERGED 2006/09/01 17:53:11 kaib 1.68.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/index/cnttab.cxx
+++ sw/source/ui/index/cnttab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cnttab.cxx,v $
  *
- *  $Revision: 1.68 $
+ *  $Revision: 1.69 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:47:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:03:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #include <rsc/rscsfx.hxx>
 
