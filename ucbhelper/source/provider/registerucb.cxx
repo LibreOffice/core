08c2fd5186eb0d840010dd9ed4219822472b5390
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:24:17 +0000

    INTEGRATION: CWS pchfix02 (1.5.34); FILE MERGED 2006/09/01 17:56:08 kaib 1.5.34.1: #i68856# Added header markers and pch files

--- ucbhelper/source/provider/registerucb.cxx
+++ ucbhelper/source/provider/registerucb.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: registerucb.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:40:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:23:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucbhelper.hxx"
 
 #ifndef _UCBHELPER_REGISTERUCB_HXX_
 #include <registerucb.hxx>
