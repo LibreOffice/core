9944c8074e09a27e619743191f314ea3d60c1d5a
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:38:28 +0000

    INTEGRATION: CWS pchfix02 (1.12.476); FILE MERGED 2006/09/01 17:52:53 kaib 1.12.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/chrdlg/pardlg.cxx
+++ sw/source/ui/chrdlg/pardlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pardlg.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:39:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:38:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #include "hintids.hxx"
 
