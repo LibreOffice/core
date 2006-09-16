ba0bbbcbd83be8fa54a23b67de0043b4c0770d5c
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:14:58 +0000

    INTEGRATION: CWS pchfix02 (1.5.40); FILE MERGED 2006/09/01 17:54:20 kaib 1.5.40.1: #i68856# Added header markers and pch files

--- toolkit/source/awt/vclxspinbutton.cxx
+++ toolkit/source/awt/vclxspinbutton.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vclxspinbutton.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:01:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:14:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef TOOLKIT_AWT_VCLXSPINBUTTON_HXX
 #include "toolkit/awt/vclxspinbutton.hxx"
