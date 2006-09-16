cb7e20045e6dfeb05b2dfa977a588031d6f4f9ff
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:21:17 +0000

    INTEGRATION: CWS pchfix02 (1.2.136); FILE MERGED 2006/09/01 17:54:24 kaib 1.2.136.1: #i68856# Added header markers and pch files

--- toolkit/source/helper/externallock.cxx
+++ toolkit/source/helper/externallock.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: externallock.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:21:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:21:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef _TOOLKIT_HELPER_EXTERNALLOCK_HXX_
 #include <toolkit/helper/externallock.hxx>
