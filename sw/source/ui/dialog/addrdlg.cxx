9a52ad9866f9895cc2b1389ecf10ab034b86780a
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:48:32 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:53:00 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/dialog/addrdlg.cxx
+++ sw/source/ui/dialog/addrdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: addrdlg.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:10:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:48:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
