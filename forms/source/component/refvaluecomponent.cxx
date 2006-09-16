b65bdcd5a10e56b503877db03f7be2ddbc302a21
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:55:25 +0000

    INTEGRATION: CWS pchfix02 (1.6.26); FILE MERGED 2006/09/01 17:27:39 kaib 1.6.26.1: #i68856# Added header markers and pch files

--- forms/source/component/refvaluecomponent.cxx
+++ forms/source/component/refvaluecomponent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: refvaluecomponent.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:56:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:55:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef EFORMS2_FORMS_SOURCE_COMPONENT_REFVALUECOMPONENT_HXX
 #include "refvaluecomponent.hxx"
