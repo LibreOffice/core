1d0e146ab967d3b878143632a300c5a3f9c4f82f
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:58:00 +0000

    INTEGRATION: CWS pchfix02 (1.7.206); FILE MERGED 2006/09/01 17:51:29 kaib 1.7.206.1: #i68856# Added header markers and pch files

--- sw/source/core/doc/sortopt.cxx
+++ sw/source/core/doc/sortopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sortopt.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 15:09:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:58:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
