bdda675cb674226ce5a701ced1c39997056f6101
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:16:11 +0000

    INTEGRATION: CWS pchfix02 (1.2.12); FILE MERGED 2006/09/01 17:32:27 kaib 1.2.12.1: #i68856# Added header markers and pch files

--- odk/examples/cpp/complextoolbarcontrols/exports.cxx
+++ odk/examples/cpp/complextoolbarcontrols/exports.cxx
@@ -1,3 +1,41 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: exports.cxx,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:16:11 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_odk.hxx"
 #include <stdio.h>
 
 #include <osl/mutex.hxx>
