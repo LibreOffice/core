6fd806b302ccdd55045891e78c7fc8096736261f
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:08:28 +0000

    INTEGRATION: CWS pchfix02 (1.6.122); FILE MERGED 2006/09/01 17:37:18 kaib 1.6.122.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/model/SlideSorterModel.cxx
+++ sd/source/ui/slidesorter/model/SlideSorterModel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlideSorterModel.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-06 16:26:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:08:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "model/SlideSorterModel.hxx"
 
