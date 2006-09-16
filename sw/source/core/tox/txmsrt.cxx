722459fa62624e80c0e618c20240cc4bc76fdd7a
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:43:36 +0000

    INTEGRATION: CWS pchfix02 (1.25.2); FILE MERGED 2006/09/01 17:52:09 kaib 1.25.2.1: #i68856# Added header markers and pch files

--- sw/source/core/tox/txmsrt.cxx
+++ sw/source/core/tox/txmsrt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txmsrt.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:46:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:43:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _TOOLS_RESID_HXX
 #include <tools/resid.hxx>
