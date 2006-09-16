e82e0fe8ec659ef86090e96b159ab3e0cb2d3489
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:55:30 +0000

    INTEGRATION: CWS pchfix02 (1.10.2); FILE MERGED 2006/09/01 17:53:05 kaib 1.10.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/labfmt.cxx
+++ sw/source/ui/envelp/labfmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: labfmt.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:36:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:55:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #ifndef _TL_POLY_HXX
 #include <tools/poly.hxx>
