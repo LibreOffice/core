bba60da10934a993b9b74c590556c2872a704980
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:42:56 +0000

    INTEGRATION: CWS pchfix02 (1.7.446); FILE MERGED 2006/09/01 17:52:56 kaib 1.7.446.1: #i68856# Added header markers and pch files

--- sw/source/ui/dbui/createaddresslistdialog.cxx
+++ sw/source/ui/dbui/createaddresslistdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: createaddresslistdialog.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-06 10:53:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:42:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #ifndef _SWTYPES_HXX
 #include <swtypes.hxx>
