f221efbc331c00d37a235822c74eb3fb84b19189
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:49:45 +0000

    INTEGRATION: CWS pchfix02 (1.8.10); FILE MERGED 2006/09/01 17:28:00 kaib 1.8.10.1: #i68856# Added header markers and pch files

--- fpicker/source/office/OfficeFilePicker.cxx
+++ fpicker/source/office/OfficeFilePicker.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OfficeFilePicker.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2006-08-07 14:00:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:49:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_fpicker.hxx"
 
 #include "OfficeFilePicker.hxx"
 
