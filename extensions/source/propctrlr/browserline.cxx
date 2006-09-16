ca803aced8970c2ced61691a869c95bbe2b58a02
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:12:55 +0000

    INTEGRATION: CWS pchfix02 (1.12.22); FILE MERGED 2006/09/01 17:26:38 kaib 1.12.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/browserline.cxx
+++ extensions/source/propctrlr/browserline.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: browserline.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:52:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:12:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_PROPCTRLR_BROWSERLINE_HXX_
 #include "browserline.hxx"
