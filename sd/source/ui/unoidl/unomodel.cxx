a13dff64b274759cdcfed4c3f25fdd3705e965d9
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:26:56 +0000

    INTEGRATION: CWS pchfix02 (1.95.92); FILE MERGED 2006/09/01 17:37:30 kaib 1.95.92.1: #i68856# Added header markers and pch files

--- sd/source/ui/unoidl/unomodel.cxx
+++ sd/source/ui/unoidl/unomodel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unomodel.cxx,v $
  *
- *  $Revision: 1.95 $
+ *  $Revision: 1.96 $
  *
- *  last change: $Author: rt $ $Date: 2006-04-28 14:59:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:26:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
