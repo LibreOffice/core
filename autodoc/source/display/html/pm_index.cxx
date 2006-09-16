d8ecc31b178a09a862700cae851741f36f67f1ff
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:44:14 +0000

    INTEGRATION: CWS pchfix02 (1.5.30); FILE MERGED 2006/09/01 17:15:32 kaib 1.5.30.1: #i68856# Added header markers and pch files

--- autodoc/source/display/html/pm_index.cxx
+++ autodoc/source/display/html/pm_index.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pm_index.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:36:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:44:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
