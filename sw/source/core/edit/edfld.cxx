c536265db1f7834f8b0ed26f98447cb678bcf2c8
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:05:28 +0000

    INTEGRATION: CWS pchfix02 (1.14.2); FILE MERGED 2006/09/01 17:51:36 kaib 1.14.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edfld.cxx
+++ sw/source/core/edit/edfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edfld.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:08:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:05:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _UNOTOOLS_CHARCLASS_HXX
 #include <unotools/charclass.hxx>
