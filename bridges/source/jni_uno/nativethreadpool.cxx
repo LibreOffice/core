bb40752390df18f33c82badaab0c473fad11f097
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:58:42 +0000

    INTEGRATION: CWS pchfix02 (1.4.22); FILE MERGED 2006/09/01 17:17:40 kaib 1.4.22.1: #i68856# Added header markers and pch files

--- bridges/source/jni_uno/nativethreadpool.cxx
+++ bridges/source/jni_uno/nativethreadpool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: nativethreadpool.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:47:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:58:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 
 #include "jvmaccess/virtualmachine.hxx"
 #include "rtl/byteseq.h"
