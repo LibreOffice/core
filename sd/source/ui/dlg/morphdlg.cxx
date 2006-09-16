c8f5dbc0e5b4fba379469c794293f5cc1edff3ca
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:40:36 +0000

    INTEGRATION: CWS pchfix02 (1.10.282); FILE MERGED 2006/09/01 17:37:00 kaib 1.10.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/morphdlg.cxx
+++ sd/source/ui/dlg/morphdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: morphdlg.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:08:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:40:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
