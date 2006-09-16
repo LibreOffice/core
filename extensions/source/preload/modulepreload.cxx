1e261588d3b6a41c93ee62a0a79eeafc7ef07909
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:11:58 +0000

    INTEGRATION: CWS pchfix02 (1.2.186); FILE MERGED 2006/09/01 17:26:37 kaib 1.2.186.1: #i68856# Added header markers and pch files

--- extensions/source/preload/modulepreload.cxx
+++ extensions/source/preload/modulepreload.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: modulepreload.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:59:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:11:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #include "componentmodule.cxx"
 
