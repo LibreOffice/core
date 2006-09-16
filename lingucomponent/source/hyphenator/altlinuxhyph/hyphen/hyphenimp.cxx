dc62809f367d94dfaf485219c62aff8417c1fd27
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:07:26 +0000

    INTEGRATION: CWS pchfix02 (1.17.12); FILE MERGED 2006/09/01 17:31:54 kaib 1.17.12.1: #i68856# Added header markers and pch files

--- lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hyphenimp.cxx
+++ lingucomponent/source/hyphenator/altlinuxhyph/hyphen/hyphenimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hyphenimp.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:55:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:07:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_lingucomponent.hxx"
 
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
