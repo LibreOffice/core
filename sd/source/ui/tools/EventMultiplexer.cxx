59aecd26fa1269c94e7ab1190f03f812d9ea38c6
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:21:28 +0000

    INTEGRATION: CWS pchfix02 (1.11.102); FILE MERGED 2006/09/01 17:37:27 kaib 1.11.102.1: #i68856# Added header markers and pch files

--- sd/source/ui/tools/EventMultiplexer.cxx
+++ sd/source/ui/tools/EventMultiplexer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EventMultiplexer.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-26 20:53:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:21:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "EventMultiplexer.hxx"
 
