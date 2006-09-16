9b38eebf14fd82055893150c6777ce8d7e157044
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:01:17 +0000

    INTEGRATION: CWS pchfix02 (1.6.2); FILE MERGED 2006/09/01 17:27:44 kaib 1.6.2.1: #i68856# Added header markers and pch files

--- forms/source/richtext/richtextvclcontrol.cxx
+++ forms/source/richtext/richtextvclcontrol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: richtextvclcontrol.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:33:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:01:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTVCLCONTROL_HXX
 #include "richtextvclcontrol.hxx"
