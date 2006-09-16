2a8a06e9428d22fd287f8bd4b8289314b5d547ed
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:35:48 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:51:12 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accfrmobjmap.cxx
+++ sw/source/core/access/accfrmobjmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accfrmobjmap.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:49:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:35:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _DOC_HXX
 #include <doc.hxx>
