2e75aeb84da89f46829a4798f1865cbc305cd15d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:45:51 +0000

    INTEGRATION: CWS pchfix02 (1.7.26); FILE MERGED 2006/09/01 17:31:26 kaib 1.7.26.1: #i68856# Added header markers and pch files

--- io/test/stm/pumptest.cxx
+++ io/test/stm/pumptest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pumptest.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:34:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:45:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 #include <stdio.h>
 
 #ifndef _OSL_TIME_H_
