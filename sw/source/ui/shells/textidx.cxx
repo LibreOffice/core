77e6bf728f3b3a7262872492c32af9ec041f8f43
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:17:33 +0000

    INTEGRATION: CWS pchfix02 (1.9.2); FILE MERGED 2006/09/01 17:53:21 kaib 1.9.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/textidx.cxx
+++ sw/source/ui/shells/textidx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textidx.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:55:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:17:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #include <hintids.hxx>
 #include <uiparam.hxx>
 #ifndef _MSGBOX_HXX //autogen
