9b788879482f70892d71dd784ce33dffb9e4da26
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:30:50 +0000

    INTEGRATION: CWS pchfix02 (1.29.18); FILE MERGED 2006/09/01 17:20:50 kaib 1.29.18.1: #i68856# Added header markers and pch files

--- configmgr/source/treemgr/noderef.cxx
+++ configmgr/source/treemgr/noderef.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: noderef.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:33:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:30:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include "anynoderef.hxx"
 #include "valueref.hxx"
