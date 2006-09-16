2ea1c1c100a20fadf690d1c9b685f02e5f9a8c65
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:02:25 +0000

    INTEGRATION: CWS pchfix02 (1.9.26); FILE MERGED 2006/09/01 17:27:45 kaib 1.9.26.1: #i68856# Added header markers and pch files

--- forms/source/solar/control/navtoolbar.cxx
+++ forms/source/solar/control/navtoolbar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: navtoolbar.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:02:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:02:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 #ifndef FORMS_SOLAR_CONTROL_NAVTOOLBAR_HXX
 #include "navtoolbar.hxx"
 #endif
