0842cf2b88c677bb5e611af0d486457b639d3a9e
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:09:44 +0000

    INTEGRATION: CWS pchfix02 (1.6.30); FILE MERGED 2006/09/01 17:15:48 kaib 1.6.30.1: #i68856# Added header markers and pch files

--- autodoc/source/parser_i/idl/pe_file2.cxx
+++ autodoc/source/parser_i/idl/pe_file2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_file2.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:48:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:09:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <s2_luidl/pe_file2.hxx>
