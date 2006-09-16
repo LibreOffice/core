5c57514e21ecf907104995dc64401ac14b7e214e
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:18:50 +0000

    INTEGRATION: CWS pchfix02 (1.4.52); FILE MERGED 2006/09/01 17:29:25 kaib 1.4.52.1: #i68856# Added header markers and pch files

--- framework/source/uielement/controlmenucontroller.cxx
+++ framework/source/uielement/controlmenucontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: controlmenucontroller.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:36:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:18:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_UIELEMENT_CONTROLMENUCONTROLLER_HXX_
 #include <uielement/controlmenucontroller.hxx>
