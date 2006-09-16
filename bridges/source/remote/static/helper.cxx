66a0853443f74fe09698d6d5f1b706c837da5543
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:59:10 +0000

    INTEGRATION: CWS pchfix02 (1.6.22); FILE MERGED 2006/09/01 17:17:40 kaib 1.6.22.1: #i68856# Added header markers and pch files

--- bridges/source/remote/static/helper.cxx
+++ bridges/source/remote/static/helper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helper.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:50:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:59:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 #include <rtl/alloc.h>
 #include <osl/diagnose.h>
 
