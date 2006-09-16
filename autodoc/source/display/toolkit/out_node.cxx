12f501e6343bc31956a1dbef44e65f9fe9ab8d36
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:53:20 +0000

    INTEGRATION: CWS pchfix02 (1.3.30); FILE MERGED 2006/09/01 17:15:38 kaib 1.3.30.1: #i68856# Added header markers and pch files

--- autodoc/source/display/toolkit/out_node.cxx
+++ autodoc/source/display/toolkit/out_node.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: out_node.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:02:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:53:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
