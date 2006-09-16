67efb59e1c0d31cf44c881ee512db31cdeaea31e
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:06:52 +0000

    INTEGRATION: CWS pchfix02 (1.20.2); FILE MERGED 2006/09/01 17:51:37 kaib 1.20.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edlingu.cxx
+++ sw/source/core/edit/edlingu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edlingu.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:53:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:06:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _COMCORE_HRC
 #include <comcore.hrc>
