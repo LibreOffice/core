2919bd63bb1df0571f6ce9e10acadf85bd9d82ea
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:16:49 +0000

    INTEGRATION: CWS pchfix02 (1.62.2); FILE MERGED 2006/09/01 17:51:46 kaib 1.62.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/calcmove.cxx
+++ sw/source/core/layout/calcmove.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: calcmove.cxx,v $
  *
- *  $Revision: 1.62 $
+ *  $Revision: 1.63 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:25:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:16:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "rootfrm.hxx"
 #include "pagefrm.hxx"
 #include "cntfrm.hxx"
