75c8a35fa5ca2322e2895cef53568594b60b4902
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:17:21 +0000

    INTEGRATION: CWS pchfix02 (1.8.24); FILE MERGED 2006/09/01 17:54:22 kaib 1.8.24.1: #i68856# Added header markers and pch files

--- toolkit/source/controls/formattedcontrol.cxx
+++ toolkit/source/controls/formattedcontrol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formattedcontrol.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 15:55:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:17:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef TOOLKIT_FORMATTED_CONTROL_HXX
 #include <toolkit/controls/formattedcontrol.hxx>
