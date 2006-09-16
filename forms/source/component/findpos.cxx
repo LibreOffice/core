d7596b7f2a3f305978f370935eb658988b1632b1
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:54:04 +0000

    INTEGRATION: CWS pchfix02 (1.2.26); FILE MERGED 2006/09/01 17:27:38 kaib 1.2.26.1: #i68856# Added header markers and pch files

--- forms/source/component/findpos.cxx
+++ forms/source/component/findpos.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: findpos.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:54:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:54:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #include "sal/config.h"
 
