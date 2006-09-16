f7d41a9d0f48bf0356511c6203565035c12d4901
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:37:56 +0000

    INTEGRATION: CWS pchfix02 (1.6.474); FILE MERGED 2006/09/01 17:53:35 kaib 1.6.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/web/wformsh.cxx
+++ sw/source/ui/web/wformsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wformsh.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:35:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:37:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "itemdef.hxx"
 
--- sw/source/ui/web/wlistsh.cxx
+++ sw/source/ui/web/wlistsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wlistsh.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:36:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:37:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 #include "itemdef.hxx"
 
 
