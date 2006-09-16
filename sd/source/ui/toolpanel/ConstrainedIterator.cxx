0672bdd7b7cbc7e25220b0b85a915bdb2f5f3222
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:12:46 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:37:21 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/toolpanel/ConstrainedIterator.cxx
+++ sd/source/ui/toolpanel/ConstrainedIterator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ConstrainedIterator.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:28:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:12:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 // This is a definition file of a template class.  It is therefore
 // included by other files and thus has to be guarded against multiple
