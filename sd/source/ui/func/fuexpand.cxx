977e4b37cdd6c7b00ae4d39b1183bf26727dc240
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:50:08 +0000

    INTEGRATION: CWS pchfix02 (1.14.200); FILE MERGED 2006/09/01 17:37:06 kaib 1.14.200.1: #i68856# Added header markers and pch files

--- sd/source/ui/func/fuexpand.cxx
+++ sd/source/ui/func/fuexpand.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuexpand.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 14:29:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:50:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include "fuexpand.hxx"
 
