9147262865b1d43b6930f9b75b9e80124e021a68
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:51:26 +0000

    INTEGRATION: CWS pchfix02 (1.7.112); FILE MERGED 2006/09/01 17:27:36 kaib 1.7.112.1: #i68856# Added header markers and pch files

--- forms/source/component/Hidden.cxx
+++ forms/source/component/Hidden.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Hidden.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:43:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:51:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FORMS_HIDDEN_HXX_
 #include "Hidden.hxx"
