513df9e898851e6e3616a536b0cbe0b49f491b84
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:17:27 +0000

    INTEGRATION: CWS pchfix02 (1.3.30); FILE MERGED 2006/09/01 17:15:53 kaib 1.3.30.1: #i68856# Added header markers and pch files

--- autodoc/source/parser_i/tokens/tkpstam2.cxx
+++ autodoc/source/parser_i/tokens/tkpstam2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tkpstam2.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:12:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:17:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <tokens/tkpstam2.hxx>
