b414527ba4daa746d37dcad5ad24d43b79d4167c
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:40:01 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:52:06 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/core/text/txtcache.cxx
+++ sw/source/core/text/txtcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtcache.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:04:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:40:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "errhdl.hxx"
 
