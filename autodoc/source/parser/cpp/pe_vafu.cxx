2eb7e710ec64c92807c64c8f64f4db853432cd01
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:04:00 +0000

    INTEGRATION: CWS pchfix02 (1.7.6); FILE MERGED 2006/09/01 17:15:44 kaib 1.7.6.1: #i68856# Added header markers and pch files

--- autodoc/source/parser/cpp/pe_vafu.cxx
+++ autodoc/source/parser/cpp/pe_vafu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_vafu.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:05:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:04:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 
 #include <precomp.h>
