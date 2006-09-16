c42d6fc02c4c923f01f57092eefa50e70944d6a2
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:37:10 +0000

    INTEGRATION: CWS pchfix02 (1.27.14); FILE MERGED 2006/09/01 17:16:06 kaib 1.27.14.1: #i68856# Added header markers and pch files

--- automation/source/server/statemnt.cxx
+++ automation/source/server/statemnt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: statemnt.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:24:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:37:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 
 #ifndef _COM_SUN_STAR_FRAME_XFRAMESSUPPLIER_HPP_
 #include <com/sun/star/frame/XFramesSupplier.hpp>
