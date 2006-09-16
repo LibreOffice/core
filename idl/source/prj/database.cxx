f776e4e2102cc2c052b824e5b9d0b66fe28f02ea
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:49:14 +0000

    INTEGRATION: CWS pchfix02 (1.5.4); FILE MERGED 2006/09/01 17:31:05 kaib 1.5.4.1: #i68856# Added header markers and pch files

--- idl/source/prj/database.cxx
+++ idl/source/prj/database.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: database.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:43:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:48:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idl.hxx"
 
 #include <ctype.h>
 #include <stdio.h>
