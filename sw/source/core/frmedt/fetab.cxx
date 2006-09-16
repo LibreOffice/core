734dab7c4ee06470c5cc1f2cd0a1b18cb18bd67a
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:15:08 +0000

    INTEGRATION: CWS pchfix02 (1.38.2); FILE MERGED 2006/09/01 17:51:44 kaib 1.38.2.1: #i68856# Added header markers and pch files

--- sw/source/core/frmedt/fetab.cxx
+++ sw/source/core/frmedt/fetab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fetab.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:16:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:14:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define ITEMID_BOXINFO SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
--- sw/source/core/frmedt/fews.cxx
+++ sw/source/core/frmedt/fews.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fews.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:16:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:15:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _LIST_HXX //autogen
 #include <tools/list.hxx>
