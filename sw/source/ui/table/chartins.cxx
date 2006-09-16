4eda2ff8daeec99a533c5d64329951e3f9392fce
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:19:12 +0000

    INTEGRATION: CWS pchfix02 (1.14.168); FILE MERGED 2006/09/01 17:53:22 kaib 1.14.168.1: #i68856# Added header markers and pch files

--- sw/source/ui/table/chartins.cxx
+++ sw/source/ui/table/chartins.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chartins.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:23:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:19:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #define _CHARTINS_CXX
 
