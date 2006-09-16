9b7414cffc09147ca4a2505236350522e7ef890d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:11:41 +0000

    INTEGRATION: CWS pchfix02 (1.15.2); FILE MERGED 2006/09/01 17:53:17 kaib 1.15.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/ribbar/inputwin.cxx
+++ sw/source/ui/ribbar/inputwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inputwin.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:52:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:11:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _GEN_HXX //autogen
 #include <tools/gen.hxx>
