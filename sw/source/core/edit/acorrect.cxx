ef3d76afb5a03100c01fbac5ed97d2197373c2d8
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:04:02 +0000

    INTEGRATION: CWS pchfix02 (1.13.2); FILE MERGED 2006/09/01 17:51:35 kaib 1.13.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/acorrect.cxx
+++ sw/source/core/edit/acorrect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acorrect.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:53:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:04:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define _STD_VAR_ARRAYS
 
