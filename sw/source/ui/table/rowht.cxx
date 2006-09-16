f954d0092f7db6c15333420962a198bf1cb119a0
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:20:36 +0000

    INTEGRATION: CWS pchfix02 (1.9.474); FILE MERGED 2006/09/01 17:53:23 kaib 1.9.474.1: #i68856# Added header markers and pch files

--- sw/source/ui/table/rowht.cxx
+++ sw/source/ui/table/rowht.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rowht.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:01:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:20:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #ifndef _SFXINTITEM_HXX //autogen
 #include <svtools/intitem.hxx>
