27c09ec46ca399435a2fa975a0461e1e5d596929
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:46:12 +0000

    INTEGRATION: CWS pchfix02 (1.9.180); FILE MERGED 2006/09/01 17:52:58 kaib 1.9.180.1: #i68856# Added header markers and pch files

--- sw/source/ui/dbui/mmgreetingspage.cxx
+++ sw/source/ui/dbui/mmgreetingspage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmgreetingspage.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-04-27 09:46:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:46:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 #ifndef _MAILMERGEGREETINGSPAGE_HXX
 #include <mmgreetingspage.hxx>
 #endif
