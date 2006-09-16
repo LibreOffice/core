9f308064f42ca1d6f82340cc529a6624a1c87dde
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:14:21 +0000

    INTEGRATION: CWS pchfix02 (1.35.86); FILE MERGED 2006/09/01 17:36:42 kaib 1.35.86.1: #i68856# Added header markers and pch files

--- sd/source/core/drawdoc2.cxx
+++ sd/source/core/drawdoc2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawdoc2.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-04 14:54:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:14:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _COM_SUN_STAR_EMBED_XVISUALOBJECT_HPP_
 #include <com/sun/star/embed/XVisualObject.hpp>
