d1230c0b79096e1ff83ff3af03cdbc8203cf90e1
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:45:30 +0000

    INTEGRATION: CWS pchfix02 (1.15.2); FILE MERGED 2006/09/01 17:52:58 kaib 1.15.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/dbui/mmaddressblockpage.cxx
+++ sw/source/ui/dbui/mmaddressblockpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmaddressblockpage.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:31:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:45:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 #ifndef _MAILMERGEADDRESSBLOCKPAGE_HXX
 #include <mmaddressblockpage.hxx>
 #endif
