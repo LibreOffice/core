f9750b65681476577dbf1680f4fce25ff217a414
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:15:53 +0000

    INTEGRATION: CWS pchfix02 (1.64.10); FILE MERGED 2006/09/01 17:54:21 kaib 1.64.10.1: #i68856# Added header markers and pch files

--- toolkit/source/awt/vclxwindow.cxx
+++ toolkit/source/awt/vclxwindow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vclxwindow.cxx,v $
  *
- *  $Revision: 1.65 $
+ *  $Revision: 1.66 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 10:57:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:15:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 #ifndef _COM_SUN_STAR_AWT_WINDOWEVENT_HPP_
 #include <com/sun/star/awt/WindowEvent.hpp>
