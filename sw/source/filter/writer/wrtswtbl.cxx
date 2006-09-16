1054720a0203c89cedc6a7bf3bcf4b9acd15078e
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:18:38 +0000

    INTEGRATION: CWS pchfix02 (1.12.2); FILE MERGED 2006/09/01 17:52:37 kaib 1.12.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/writer/wrtswtbl.cxx
+++ sw/source/filter/writer/wrtswtbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrtswtbl.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:13:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:18:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
 #endif
