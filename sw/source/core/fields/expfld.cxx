6829e5b55b01b2bc4e091438d23cf2e1323d6a30
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:11:24 +0000

    INTEGRATION: CWS pchfix02 (1.24.2); FILE MERGED 2006/09/01 17:51:41 kaib 1.24.2.1: #i68856# Added header markers and pch files

--- sw/source/core/fields/expfld.cxx
+++ sw/source/core/fields/expfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: expfld.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:13:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:11:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
