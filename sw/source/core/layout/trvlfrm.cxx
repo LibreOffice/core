eb496c30d1b53180f7a3d4cb1f48a2d638d36c46
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:24:43 +0000

    INTEGRATION: CWS pchfix02 (1.50.2); FILE MERGED 2006/09/01 17:51:53 kaib 1.50.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/trvlfrm.cxx
+++ sw/source/core/layout/trvlfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: trvlfrm.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:30:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:24:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 
 #ifndef _HINTIDS_HXX
