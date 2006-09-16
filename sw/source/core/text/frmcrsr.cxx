5266c2827472b08b0815399ae3f99931705bf78e
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:33:39 +0000

    INTEGRATION: CWS pchfix02 (1.41.2); FILE MERGED 2006/09/01 17:52:01 kaib 1.41.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/frmcrsr.cxx
+++ sw/source/core/text/frmcrsr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmcrsr.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:36:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:33:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "ndtxt.hxx"        // GetNode()
 #include "pam.hxx"          // SwPosition
