e880cd4c8835fef35951b5dd6e61cb5588757c84
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:32:44 +0000

    INTEGRATION: CWS pchfix02 (1.4.36); FILE MERGED 2006/09/01 17:35:38 kaib 1.4.36.1: #i68856# Added header markers and pch files

--- scripting/source/storage/XMLElement.cxx
+++ scripting/source/storage/XMLElement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLElement.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:39:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:32:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_scripting.hxx"
 #include "XMLElement.hxx"
 #include <osl/diagnose.h>
 
