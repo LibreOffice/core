9889b1197e6acf361cc21bf18f3175d4fcdfaaf6
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:44:33 +0000

    INTEGRATION: CWS pchfix02 (1.4.8); FILE MERGED 2006/09/01 17:31:25 kaib 1.4.8.1: #i68856# Added header markers and pch files

--- io/source/stm/streamhelper.cxx
+++ io/source/stm/streamhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: streamhelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:18:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:44:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_io.hxx"
 #include <rtl/alloc.h>
 
 #include <limits>
