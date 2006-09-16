d260defc0043a7449b75d3b298614a3ee6f8f7e5
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:45:09 +0000

    INTEGRATION: CWS pchfix02 (1.73.88); FILE MERGED 2006/09/01 17:37:03 kaib 1.73.88.1: #i68856# Added header markers and pch files

--- sd/source/ui/docshell/docshel4.cxx
+++ sd/source/ui/docshell/docshel4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docshel4.cxx,v $
  *
- *  $Revision: 1.73 $
+ *  $Revision: 1.74 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-04 14:58:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:45:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "DrawDocShell.hxx"
 
@@ -114,7 +117,6 @@
 #include <com/sun/star/drawing/XDrawView.hpp>
 #endif
 
-#pragma hdrstop
 
 #include "app.hrc"
 #include "glob.hrc"
