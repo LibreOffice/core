68cd37393b2c4474fde59a1f3c3d52335c1faf96
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:54:16 +0000

    INTEGRATION: CWS pchfix02 (1.17.2); FILE MERGED 2006/09/01 17:53:04 kaib 1.17.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/envlop1.cxx
+++ sw/source/ui/envelp/envlop1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: envlop1.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:35:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:54:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,12 +32,14 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #include "dbmgr.hxx"
 #ifndef _SFXAPP_HXX //autogen
