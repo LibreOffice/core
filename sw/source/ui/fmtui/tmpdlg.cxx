eab0ae35fad7a98f6926dfd2b40f4177464daa0d
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:00:36 +0000

    INTEGRATION: CWS pchfix02 (1.21.472); FILE MERGED 2006/09/01 17:53:08 kaib 1.21.472.1: #i68856# Added header markers and pch files

--- sw/source/ui/fmtui/tmpdlg.cxx
+++ sw/source/ui/fmtui/tmpdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tmpdlg.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:47:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:00:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #ifndef _UIPARAM_HXX
 #include <uiparam.hxx>
