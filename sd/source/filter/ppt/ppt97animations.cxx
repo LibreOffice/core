5f6569601e96495e2afa3babb9a78bb25dfa6373
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:23:03 +0000

    INTEGRATION: CWS pchfix02 (1.5.274); FILE MERGED 2006/09/01 17:36:48 kaib 1.5.274.1: #i68856# Added header markers and pch files

--- sd/source/filter/ppt/ppt97animations.cxx
+++ sd/source/filter/ppt/ppt97animations.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ppt97animations.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 10:44:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:23:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "ppt97animations.hxx"
 
