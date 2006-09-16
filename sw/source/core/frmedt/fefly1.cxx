20aff9d5d6b31f4ca3b4858ccb1163bea765216e
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:14:10 +0000

    INTEGRATION: CWS pchfix02 (1.31.2); FILE MERGED 2006/09/01 17:51:43 kaib 1.31.2.1: #i68856# Added header markers and pch files

--- sw/source/core/frmedt/fefly1.cxx
+++ sw/source/core/frmedt/fefly1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fefly1.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 11:40:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:14:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
