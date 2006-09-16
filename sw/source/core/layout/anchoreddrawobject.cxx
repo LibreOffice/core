ebfc6198577b3d2dfb9f811d0eb3b2bde7ab5397
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:16:07 +0000

    INTEGRATION: CWS pchfix02 (1.17.2); FILE MERGED 2006/09/01 17:51:46 kaib 1.17.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/anchoreddrawobject.cxx
+++ sw/source/core/layout/anchoreddrawobject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: anchoreddrawobject.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 11:41:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:16:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _ANCHOREDDRAWOBJECT_HXX
 #include <anchoreddrawobject.hxx>
 #endif
