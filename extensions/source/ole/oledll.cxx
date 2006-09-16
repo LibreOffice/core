f08d98fa33eddb6370bbe0a0c15320936aa5afb5
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:04:07 +0000

    INTEGRATION: CWS pchfix02 (1.5.130); FILE MERGED 2006/09/01 17:26:32 kaib 1.5.130.1: #i68856# Added header markers and pch files

--- extensions/source/ole/oledll.cxx
+++ extensions/source/ole/oledll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oledll.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 15:59:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:04:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #define STRICT
 #define _WIN32_WINNT 0x0400
