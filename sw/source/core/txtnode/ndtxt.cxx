b68b14dd8a69bf47732c1b2026ea5a737a385d24
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:46:28 +0000

    INTEGRATION: CWS pchfix02 (1.64.2); FILE MERGED 2006/09/01 17:52:12 kaib 1.64.2.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ndtxt.cxx,v $
  *
- *  $Revision: 1.64 $
+ *  $Revision: 1.65 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:47:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:46:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include <hintids.hxx>
 
 #ifndef _COM_SUN_STAR_I18N_SCRIPTTYPE_HDL_
