62fc13834389bc109f5e770c89e05a05ca00a912
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:58:05 +0000

    INTEGRATION: CWS pchfix02 (1.4.26); FILE MERGED 2006/09/01 17:27:41 kaib 1.4.26.1: #i68856# Added header markers and pch files

--- forms/source/misc/listenercontainers.cxx
+++ forms/source/misc/listenercontainers.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listenercontainers.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 12:58:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:58:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef FORMS_SOURCE_MISC_LISTENERCONTAINERS_HXX
 #include "listenercontainers.hxx"
