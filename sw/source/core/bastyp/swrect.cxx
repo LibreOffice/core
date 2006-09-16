f5b4c8081f121d57f5e8926f2ef1e05480f04568
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:43:20 +0000

    INTEGRATION: CWS pchfix02 (1.14.2); FILE MERGED 2006/09/01 17:51:17 kaib 1.14.2.1: #i68856# Added header markers and pch files

--- sw/source/core/bastyp/swrect.cxx
+++ sw/source/core/bastyp/swrect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swrect.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:50:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:43:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef PRODUCT
 #ifndef _STREAM_HXX //autogen
