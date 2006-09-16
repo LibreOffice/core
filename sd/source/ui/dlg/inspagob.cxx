4a09347cd3e557a7188b7772e08a434e57782289
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:40:08 +0000

    INTEGRATION: CWS pchfix02 (1.8.282); FILE MERGED 2006/09/01 17:37:00 kaib 1.8.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/inspagob.cxx
+++ sd/source/ui/dlg/inspagob.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inspagob.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:05:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:40:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #include "inspagob.hxx"
 
