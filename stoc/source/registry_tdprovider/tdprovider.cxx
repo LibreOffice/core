88e2a2b893cdf932d9900f12c5ae7e6779cea38f
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:35:08 +0000

    INTEGRATION: CWS pchfix02 (1.20.10); FILE MERGED 2006/09/01 17:41:16 kaib 1.20.10.1: #i68856# Added header markers and pch files

--- stoc/source/registry_tdprovider/tdprovider.cxx
+++ stoc/source/registry_tdprovider/tdprovider.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tdprovider.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:04:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:35:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
