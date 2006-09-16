3f1b13986ef16ab18e20b03ba16cee848f3cf263
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:50:34 +0000

    INTEGRATION: CWS pchfix02 (1.6.2); FILE MERGED 2006/09/01 17:52:15 kaib 1.6.2.1: #i68856# Added header markers and pch files

--- sw/source/core/undo/unbkmk.cxx
+++ sw/source/core/undo/unbkmk.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unbkmk.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:49:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:50:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "doc.hxx"
 #include "docary.hxx"
