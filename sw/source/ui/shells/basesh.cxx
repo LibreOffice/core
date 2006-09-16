aa0bd3d0513a7a7f169d30378b56020c0f650b99
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:12:52 +0000

    INTEGRATION: CWS pchfix02 (1.76.2); FILE MERGED 2006/09/01 17:53:18 kaib 1.76.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/basesh.cxx
+++ sw/source/ui/shells/basesh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: basesh.cxx,v $
  *
- *  $Revision: 1.76 $
+ *  $Revision: 1.77 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:52:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:12:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include <sot/factory.hxx>
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
