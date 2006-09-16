4911234e2ef0e00d4c2b0c998efb0fe5c2d3d8e7
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:01:16 +0000

    INTEGRATION: CWS pchfix02 (1.14.20); FILE MERGED 2006/09/01 17:17:42 kaib 1.14.20.1: #i68856# Added header markers and pch files

--- bridges/source/remote/urp/urp_job.cxx
+++ bridges/source/remote/urp/urp_job.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: urp_job.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:53:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:01:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 #include <string.h>
 #include <stdio.h>
 #include <osl/diagnose.h>
