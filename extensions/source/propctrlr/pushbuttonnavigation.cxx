43f78352a2ac57ee448bd0009638d544528141ff
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:22:59 +0000

    INTEGRATION: CWS pchfix02 (1.5.22); FILE MERGED 2006/09/01 17:26:44 kaib 1.5.22.1: #i68856# Added header markers and pch files

--- extensions/source/propctrlr/pushbuttonnavigation.cxx
+++ extensions/source/propctrlr/pushbuttonnavigation.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pushbuttonnavigation.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 08:00:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:22:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef EXTENSIONS_PROPCTRLR_PUSHBUTTONNAVIGATION_HXX
 #include "pushbuttonnavigation.hxx"
