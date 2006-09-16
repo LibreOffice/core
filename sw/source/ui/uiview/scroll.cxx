9c503e087bb3721e1c200f7370e7d10792510c90
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:22:50 +0000

    INTEGRATION: CWS pchfix02 (1.7.476); FILE MERGED 2006/09/01 17:53:25 kaib 1.7.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/uiview/scroll.cxx
+++ sw/source/ui/uiview/scroll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scroll.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:10:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:22:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "swtypes.hxx"
 #include "swrect.hxx"
