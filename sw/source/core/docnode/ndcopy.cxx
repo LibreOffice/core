23a3b0cb10d5f82f021c43c2313db6650358b763
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:59:40 +0000

    INTEGRATION: CWS pchfix02 (1.22.2); FILE MERGED 2006/09/01 17:51:30 kaib 1.22.2.1: #i68856# Added header markers and pch files

--- sw/source/core/docnode/ndcopy.cxx
+++ sw/source/core/docnode/ndcopy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ndcopy.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:03:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:59:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define _ZFORLIST_DECLARE_TABLE
 #ifndef _HINTIDS_HXX
