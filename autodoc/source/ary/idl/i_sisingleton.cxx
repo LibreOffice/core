71d3ed43e749645ed00b6d17ff6c27e98143cfc4
Oliver Bolte <obo@openoffice.org>
2006-09-16 15:24:48 +0000

    INTEGRATION: CWS pchfix02 (1.3.30); FILE MERGED 2006/09/01 17:15:20 kaib 1.3.30.1: #i68856# Added header markers and pch files

--- autodoc/source/ary/idl/i_sisingleton.cxx
+++ autodoc/source/ary/idl/i_sisingleton.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: i_sisingleton.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 16:46:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 16:24:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <ary/idl/i_sisingleton.hxx>
