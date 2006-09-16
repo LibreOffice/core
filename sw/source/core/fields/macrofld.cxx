129e5d18c2121196feda15ac24bd997c7534cf6c
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:12:34 +0000

    INTEGRATION: CWS pchfix02 (1.9.476); FILE MERGED 2006/09/01 17:51:42 kaib 1.9.476.1: #i68856# Added header markers and pch files

--- sw/source/core/fields/macrofld.cxx
+++ sw/source/core/fields/macrofld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: macrofld.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:35:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:12:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
