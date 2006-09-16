e98b834fc42daa321881ce65e3e42addf04859c5
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:36:27 +0000

    INTEGRATION: CWS pchfix02 (1.5.30); FILE MERGED 2006/09/01 17:16:06 kaib 1.5.30.1: #i68856# Added header markers and pch files

--- automation/source/server/scmdstrm.cxx
+++ automation/source/server/scmdstrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scmdstrm.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-04-19 13:58:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:36:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 #ifndef _SFXINTITEM_HXX //autogen
 #include <svtools/intitem.hxx>
 #endif
