102e433b3bc4c6a3f0d093615004fda267065393
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:31:30 +0000

    INTEGRATION: CWS pchfix02 (1.60.2); FILE MERGED 2006/09/01 17:53:31 kaib 1.60.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/unotxvw.cxx
+++ sw/source/ui/uno/unotxvw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unotxvw.cxx,v $
  *
- *  $Revision: 1.60 $
+ *  $Revision: 1.61 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:02:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:31:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #include "viscrs.hxx"
 #include <sfx2/frame.hxx>
