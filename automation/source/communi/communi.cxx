c38819935bf93989841b464720d369e4fc8fcb3c
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:33:49 +0000

    INTEGRATION: CWS pchfix02 (1.4.14); FILE MERGED 2006/09/01 17:16:04 kaib 1.4.14.1: #i68856# Added header markers and pch files

--- automation/source/communi/communi.cxx
+++ automation/source/communi/communi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: communi.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:20:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:33:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 #include <stdio.h>
 #if OSL_DEBUG_LEVEL > 1
 #define DEBUGPRINTF(x) { printf(x); fflush( stdout ); }
