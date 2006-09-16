d61793de3d8074bc0ba02e692e4ba6ea474441ff
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:47:13 +0000

    INTEGRATION: CWS pchfix02 (1.2.10); FILE MERGED 2006/09/01 17:17:31 kaib 1.2.10.1: #i68856# Added header markers and pch files

--- bridges/source/cpp_uno/gcc3_freebsd_x86-64/cpp2uno.cxx
+++ bridges/source/cpp_uno/gcc3_freebsd_x86-64/cpp2uno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cpp2uno.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 09:29:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:46:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 
 #include <stdio.h>
 #include <stdlib.h>
