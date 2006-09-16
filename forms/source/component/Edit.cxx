fe806aa2886d8a55a4de3ff52eaf98590aa20dee
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:48:28 +0000

    INTEGRATION: CWS pchfix02 (1.33.2); FILE MERGED 2006/09/01 17:27:34 kaib 1.33.2.1: #i68856# Added header markers and pch files

--- forms/source/component/Edit.cxx
+++ forms/source/component/Edit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Edit.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:31:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:48:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FORMS_EDIT_HXX_
 #include "Edit.hxx"
