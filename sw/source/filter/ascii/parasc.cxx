2def5313eb9af93fa1b920304c2f7e3d25b30fd8
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:04:10 +0000

    INTEGRATION: CWS pchfix02 (1.23.2); FILE MERGED 2006/09/01 17:52:26 kaib 1.23.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/ascii/parasc.cxx
+++ sw/source/filter/ascii/parasc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parasc.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:59:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:04:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,11 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
 
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
