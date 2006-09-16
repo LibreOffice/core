09d0bf7f775d2a2e416e032f2b939cf5ee34fd79
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:04:22 +0000

    INTEGRATION: CWS pchfix02 (1.15.40); FILE MERGED 2006/09/01 17:26:32 kaib 1.15.40.1: #i68856# Added header markers and pch files

--- extensions/source/ole/oleobjw.cxx
+++ extensions/source/ole/oleobjw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oleobjw.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:44:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:04:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #include "ole2uno.hxx"
 #include "rtl/ustrbuf.hxx"
 
