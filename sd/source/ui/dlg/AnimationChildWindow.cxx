312858c15790d65ee989e009fb28e05b28a6ea8a
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:33:30 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:36:55 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/AnimationChildWindow.cxx
+++ sd/source/ui/dlg/AnimationChildWindow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AnimationChildWindow.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:48:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:33:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "AnimationChildWindow.hxx"
 
