a067c22c89dc44f1e7f4e53359e2a9a8526b3741
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:30:56 +0000

    INTEGRATION: CWS pchfix02 (1.14.146); FILE MERGED 2006/09/01 17:36:53 kaib 1.14.146.1: #i68856# Added header markers and pch files

--- sd/source/ui/app/sddll1.cxx
+++ sd/source/ui/app/sddll1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sddll1.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:13:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:30:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include <svtools/moduleoptions.hxx>
 #include "sddll.hxx"
