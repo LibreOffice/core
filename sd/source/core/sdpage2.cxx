0fb6e8c69284e4575248ae82465483fbfa7969e7
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:16:27 +0000

    INTEGRATION: CWS pchfix02 (1.30.200); FILE MERGED 2006/09/01 17:36:43 kaib 1.30.200.1: #i68856# Added header markers and pch files

--- sd/source/core/sdpage2.cxx
+++ sd/source/core/sdpage2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdpage2.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-10 14:26:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:16:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _SFXDOCFILE_HXX //autogen
 #include <sfx2/docfile.hxx>
