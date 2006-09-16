7a93336b2ee04d3c7bb72d6b50836db5922a68fe
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:38:43 +0000

    INTEGRATION: CWS pchfix02 (1.4.54); FILE MERGED 2006/09/01 17:16:07 kaib 1.4.54.1: #i68856# Added header markers and pch files

--- automation/source/testtool/cretstrm.cxx
+++ automation/source/testtool/cretstrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cretstrm.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:31:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:38:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_automation.hxx"
 #ifndef _CRetStream_HXX
 #include "cretstrm.hxx"
 #endif
