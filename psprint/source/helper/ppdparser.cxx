a7612ae8e804e2b3f73f22af23610838497a8a0d
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:35:02 +0000

    INTEGRATION: CWS pchfix02 (1.21.20); FILE MERGED 2006/09/01 17:32:54 kaib 1.21.20.1: #i68856# Added header markers and pch files

--- psprint/source/helper/ppdparser.cxx
+++ psprint/source/helper/ppdparser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ppdparser.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:25:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:35:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_psprint.hxx"
 #include <stdlib.h>
 #include <stdio.h>
 
