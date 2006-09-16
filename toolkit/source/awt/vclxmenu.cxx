577db61bc2813fb0335d87f61d1bc242657fb507
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:14:04 +0000

    INTEGRATION: CWS pchfix02 (1.9.40); FILE MERGED 2006/09/01 17:54:20 kaib 1.9.40.1: #i68856# Added header markers and pch files

--- toolkit/source/awt/vclxmenu.cxx
+++ toolkit/source/awt/vclxmenu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vclxmenu.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:01:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:14:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
 
 
 #include <toolkit/awt/vclxmenu.hxx>
