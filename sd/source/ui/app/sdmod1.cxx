f01532a6f5564a6fee789b22ea92c711146a19d1
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:31:41 +0000

    INTEGRATION: CWS pchfix02 (1.40.26); FILE MERGED 2006/09/01 17:36:54 kaib 1.40.26.1: #i68856# Added header markers and pch files

--- sd/source/ui/app/sdmod1.cxx
+++ sd/source/ui/app/sdmod1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdmod1.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:53:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:31:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include <svtools/lckbitem.hxx>
 #ifndef _SFXFRAME_HXX //autogen
