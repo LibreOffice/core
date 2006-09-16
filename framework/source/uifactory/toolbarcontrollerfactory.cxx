31aa7be9c787c4c91ca67fa0dc57e7c46448d913
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:28:09 +0000

    INTEGRATION: CWS pchfix02 (1.5.52); FILE MERGED 2006/09/01 17:29:31 kaib 1.5.52.1: #i68856# Added header markers and pch files

--- framework/source/uifactory/toolbarcontrollerfactory.cxx
+++ framework/source/uifactory/toolbarcontrollerfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toolbarcontrollerfactory.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:43:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:28:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
