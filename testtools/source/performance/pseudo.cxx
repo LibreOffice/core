18b22bc07fffd50fceb0359b37b01c3df59ee37d
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:38:11 +0000

    INTEGRATION: CWS pchfix02 (1.4.8); FILE MERGED 2006/09/01 17:53:59 kaib 1.4.8.1: #i68856# Added header markers and pch files

--- testtools/source/performance/pseudo.cxx
+++ testtools/source/performance/pseudo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pseudo.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:46:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:38:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_testtools.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
