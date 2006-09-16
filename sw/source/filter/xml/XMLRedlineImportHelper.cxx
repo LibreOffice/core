3da2106891f4958ef6633b84c360ac212c25610c
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:27:14 +0000

    INTEGRATION: CWS pchfix02 (1.21.2); FILE MERGED 2006/09/01 17:52:45 kaib 1.21.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/XMLRedlineImportHelper.cxx
+++ sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLRedlineImportHelper.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:20:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:27:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 
 #ifndef _XMLREDLINEIMPORTHELPER_HXX
