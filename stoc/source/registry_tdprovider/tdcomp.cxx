3b9c4131f59c1c33755b002c8648b0c1bc36d457
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:33:29 +0000

    INTEGRATION: CWS pchfix02 (1.10.36); FILE MERGED 2006/09/01 17:41:15 kaib 1.10.36.1: #i68856# Added header markers and pch files

--- stoc/source/registry_tdprovider/tdcomp.cxx
+++ stoc/source/registry_tdprovider/tdcomp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tdcomp.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:05:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:33:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 #ifndef _STOC_RDBTDP_BASE_HXX
 #include "base.hxx"
