984e1fc1e8ab36a6c1ff3e123659e534cfec5b61
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:21:14 +0000

    INTEGRATION: CWS pchfix02 (1.9.152); FILE MERGED 2006/09/01 17:36:47 kaib 1.9.152.1: #i68856# Added header markers and pch files

--- sd/source/filter/eppt/pptexanimations.cxx
+++ sd/source/filter/eppt/pptexanimations.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pptexanimations.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:03:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:21:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #ifndef _COM_SUN_STAR_ANIMATIONS_XANIMATIONNODESUPPLIER_HPP_
 #include <com/sun/star/animations/XAnimationNodeSupplier.hpp>
