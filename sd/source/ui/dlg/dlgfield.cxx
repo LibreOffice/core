aab9f1ef8fdeceaa2f27ee5db6c9d014b8a1aeca
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:37:32 +0000

    INTEGRATION: CWS pchfix02 (1.11.280); FILE MERGED 2006/09/01 17:36:58 kaib 1.11.280.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/dlgfield.cxx
+++ sd/source/ui/dlg/dlgfield.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgfield.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:56:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:37:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 #ifdef SD_DLLIMPLEMENTATION
 #undef SD_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #include <svx/editeng.hxx>
 
