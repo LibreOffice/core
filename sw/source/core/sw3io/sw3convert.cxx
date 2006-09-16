323e87b5320f529936cd0f18878df1b0d6563fe3
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:27:20 +0000

    INTEGRATION: CWS pchfix02 (1.3.474); FILE MERGED 2006/09/01 17:51:56 kaib 1.3.474.1: #i68856# Added header markers and pch files

--- sw/source/core/sw3io/sw3convert.cxx
+++ sw/source/core/sw3io/sw3convert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw3convert.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:41:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:27:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <stdlib.h>
 
