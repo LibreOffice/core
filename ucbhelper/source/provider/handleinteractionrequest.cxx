4df7f26744b070877fe4bfc7a9934f3acfae355d
Oliver Bolte <obo@openoffice.org>
2006-09-16 16:22:13 +0000

    INTEGRATION: CWS pchfix02 (1.2.34); FILE MERGED 2006/09/01 17:56:07 kaib 1.2.34.1: #i68856# Added header markers and pch files

--- ucbhelper/source/provider/handleinteractionrequest.cxx
+++ ucbhelper/source/provider/handleinteractionrequest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: handleinteractionrequest.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:37:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 17:22:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucbhelper.hxx"
 
 #ifndef INCLUDED_UCBHELPER_HANDLEINTERACTIONREQUEST_HXX
 #include "ucbhelper/handleinteractionrequest.hxx"
