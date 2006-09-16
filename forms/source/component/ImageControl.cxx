72d50605c47426ea0ec0e67372a3e1761e203255
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:51:53 +0000

    INTEGRATION: CWS pchfix02 (1.43.16); FILE MERGED 2006/09/01 17:27:36 kaib 1.43.16.1: #i68856# Added header markers and pch files

--- forms/source/component/ImageControl.cxx
+++ forms/source/component/ImageControl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImageControl.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:43:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:51:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #ifndef _FRM_IMAGE_CONTROL_HXX_
 #include "ImageControl.hxx"
