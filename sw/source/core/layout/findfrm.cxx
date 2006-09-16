15336b997afff0bdfc034f9432f0a7f7beb65133
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:17:32 +0000

    INTEGRATION: CWS pchfix02 (1.38.70); FILE MERGED 2006/09/01 17:51:47 kaib 1.38.70.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/findfrm.cxx
+++ sw/source/core/layout/findfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: findfrm.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:16:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:17:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "pagefrm.hxx"
 #include "rootfrm.hxx"
