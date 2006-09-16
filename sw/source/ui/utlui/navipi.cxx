277cfa24fbf6a7104450bacac9c263b8f070ccc4
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:33:54 +0000

    INTEGRATION: CWS pchfix02 (1.42.2); FILE MERGED 2006/09/01 17:53:32 kaib 1.42.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/utlui/navipi.cxx
+++ sw/source/ui/utlui/navipi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: navipi.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:03:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:33:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #define NAVIPI_CXX
 
