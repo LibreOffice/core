519c7a964c878520b8bba2a3b0501a70f87323a5
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:09:39 +0000

    INTEGRATION: CWS pchfix02 (1.3.18); FILE MERGED 2006/09/01 17:20:33 kaib 1.3.18.1: #i68856# Added header markers and pch files

--- configmgr/source/data/anydata.cxx
+++ configmgr/source/data/anydata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: anydata.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:22:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include "anydata.hxx"
 
