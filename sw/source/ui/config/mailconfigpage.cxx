e61d6bd2fc1e9bf8d2ed01184d93323cccf95626
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:40:20 +0000

    INTEGRATION: CWS pchfix02 (1.6.2); FILE MERGED 2006/09/01 17:52:54 kaib 1.6.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/mailconfigpage.cxx
+++ sw/source/ui/config/mailconfigpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mailconfigpage.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:28:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:40:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,11 +32,13 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
-#pragma hdrstop
 
 #ifndef _SWTYPES_HXX
 #include <swtypes.hxx>
