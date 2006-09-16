1823645de44667221518ed77a48385f395a917a9
Oliver Bolte <obo@openoffice.org>
2006-09-16 11:35:16 +0000

    INTEGRATION: CWS pchfix02 (1.12.58); FILE MERGED 2006/09/01 17:32:54 kaib 1.12.58.1: #i68856# Added header markers and pch files

--- psprint/source/helper/strhelper.cxx
+++ psprint/source/helper/strhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strhelper.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-01-16 12:54:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 12:35:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_psprint.hxx"
 #include <psprint/strhelper.hxx>
 #if defined(SOLARIS) || defined(IRIX)
 #include <ieeefp.h> // finite
