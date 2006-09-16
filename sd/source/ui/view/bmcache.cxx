d4660046e4f77a987ec9e6990122c80f892be962
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:34:49 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:37:35 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/bmcache.cxx
+++ sd/source/ui/view/bmcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bmcache.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:05:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:34:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #include <limits.h>     // LONG_MAX
 #include <tools/solar.h>
