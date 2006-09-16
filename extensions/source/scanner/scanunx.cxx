4a76bfeb715c49890a71073b487df092b3c003a3
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:28:01 +0000

    INTEGRATION: CWS pchfix02 (1.7.186); FILE MERGED 2006/09/01 17:26:47 kaib 1.7.186.1: #i68856# Added header markers and pch files

--- extensions/source/scanner/scanunx.cxx
+++ extensions/source/scanner/scanunx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scanunx.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:38:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:28:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 #include <scanner.hxx>
 #include <sanedlg.hxx>
 #include <vos/thread.hxx>
