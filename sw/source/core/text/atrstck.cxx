7c34f9ec8a45256d44c376c835484eae0c896994
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:33:11 +0000

    INTEGRATION: CWS pchfix02 (1.23.2); FILE MERGED 2006/09/01 17:52:00 kaib 1.23.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/atrstck.cxx
+++ sw/source/core/text/atrstck.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atrstck.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:35:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:33:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _ERRHDL_HXX
 #include <errhdl.hxx>   // ASSERT
