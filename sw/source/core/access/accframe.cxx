289c58bde64af33871e0f17d15e5fe784cbedb98
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:35:05 +0000

    INTEGRATION: CWS pchfix02 (1.23.2); FILE MERGED 2006/09/01 17:51:11 kaib 1.23.2.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accframe.cxx
+++ sw/source/core/access/accframe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accframe.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:43:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:35:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 
 #include <hintids.hxx>
