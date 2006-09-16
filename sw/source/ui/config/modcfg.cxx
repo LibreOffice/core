4b894bcf6b9d856a79644aeea5b5ffaeaf5cc257
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:40:35 +0000

    INTEGRATION: CWS pchfix02 (1.25.2); FILE MERGED 2006/09/01 17:52:54 kaib 1.25.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/config/modcfg.cxx
+++ sw/source/ui/config/modcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: modcfg.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:28:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:40:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
