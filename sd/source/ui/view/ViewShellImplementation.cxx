507f2c48948cd5caca7931c5a38736a2f9d1933f
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:33:48 +0000

    INTEGRATION: CWS pchfix02 (1.19.144); FILE MERGED 2006/09/01 17:37:34 kaib 1.19.144.1: #i68856# Added header markers and pch files

--- sd/source/ui/view/ViewShellImplementation.cxx
+++ sd/source/ui/view/ViewShellImplementation.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ViewShellImplementation.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:40:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:33:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 #include "ViewShellImplementation.hxx"
 
 #include "sdpage.hxx"
