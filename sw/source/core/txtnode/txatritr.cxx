29ba41faa57d027f32f0aa5f348214085001158b
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:47:46 +0000

    INTEGRATION: CWS pchfix02 (1.7.444); FILE MERGED 2006/09/01 17:52:13 kaib 1.7.444.1: #i68856# Added header markers and pch files

--- sw/source/core/txtnode/txatritr.cxx
+++ sw/source/core/txtnode/txatritr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txatritr.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-05 13:21:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:47:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
