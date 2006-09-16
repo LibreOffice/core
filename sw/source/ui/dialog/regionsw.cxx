8cbd7b72dfa38af030ff11fc0a66afa299f6a459
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:49:36 +0000

    INTEGRATION: CWS pchfix02 (1.35.2); FILE MERGED 2006/09/01 17:53:01 kaib 1.35.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/dialog/regionsw.cxx
+++ sw/source/ui/dialog/regionsw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: regionsw.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:33:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:49:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
