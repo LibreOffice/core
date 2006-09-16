021758fa34f3eaa0cc38b2f4a26226da7cde8d40
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:30:44 +0000

    INTEGRATION: CWS pchfix02 (1.5.2); FILE MERGED 2006/09/01 17:52:47 kaib 1.5.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/xml/xmlithlp.cxx
+++ sw/source/filter/xml/xmlithlp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlithlp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:23:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:30:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #ifndef _SW_XMLITHLP_HXX
 #include "xmlithlp.hxx"
 #endif
