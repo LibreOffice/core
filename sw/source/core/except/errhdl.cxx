baadccc33910cbb0c695bf3c7a8efb50fef04fa3
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:09:25 +0000

    INTEGRATION: CWS pchfix02 (1.4.2); FILE MERGED 2006/09/01 17:51:39 kaib 1.4.2.1: #i68856# Added header markers and pch files

--- sw/source/core/except/errhdl.cxx
+++ sw/source/core/except/errhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: errhdl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:11:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:09:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,11 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #define _ERRHDL_CXX
 
-#pragma hdrstop
 
 #include "stdlib.h"
 #ifdef WIN
