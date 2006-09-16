549325db0b5661085935c63e83c6bf6d7996d075
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:32:47 +0000

    INTEGRATION: CWS pchfix02 (1.21.14); FILE MERGED 2006/09/01 17:16:24 kaib 1.21.14.1: #i68856# Added header markers and pch files

--- basctl/source/dlged/propbrw.cxx
+++ basctl/source/dlged/propbrw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propbrw.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 13:00:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:32:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basctl.hxx"
 
 
 #ifndef _BASCTL_PROPBRW_HXX
