9bee19a71537cc8ee23c9ef034ab03c3132d7c6a
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:07:38 +0000

    INTEGRATION: CWS pchfix02 (1.6.476); FILE MERGED 2006/09/01 17:52:29 kaib 1.6.476.1: #i68856# Added header markers and pch files

--- sw/source/filter/html/css1kywd.cxx
+++ sw/source/filter/html/css1kywd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: css1kywd.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:40:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:07:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "css1kywd.hxx"
 
