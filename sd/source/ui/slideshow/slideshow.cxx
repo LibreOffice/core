ac20e011957471bddbe92d7535e0f69b9f5c2ebf
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:02:39 +0000

    INTEGRATION: CWS pchfix02 (1.7.122); FILE MERGED 2006/09/01 17:37:15 kaib 1.7.122.1: #i68856# Added header markers and pch files

--- sd/source/ui/slideshow/slideshow.cxx
+++ sd/source/ui/slideshow/slideshow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: slideshow.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-06 13:27:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:02:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 #ifndef _SD_SLIDESHOW_HXX
 #include "slideshow.hxx"
 #endif
