bc56252205bc3a4670b034372b18268a1320bdc4
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:37:28 +0000

    INTEGRATION: CWS pchfix02 (1.34.2); FILE MERGED 2006/09/01 17:52:04 kaib 1.34.2.1: #i68856# Added header markers and pch files

--- sw/source/core/text/porfly.cxx
+++ sw/source/core/text/porfly.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: porfly.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:40:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:37:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "dcontact.hxx" // SwDrawContact
 #include "dflyobj.hxx"  // SwVirtFlyDrawObj
