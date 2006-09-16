63e99e792d3dc3349eabd87e1ce36bfeb975d44a
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:08:56 +0000

    INTEGRATION: CWS pchfix02 (1.15.2); FILE MERGED 2006/09/01 17:52:29 kaib 1.15.2.1: #i68856# Added header markers and pch files

--- sw/source/filter/html/htmldraw.cxx
+++ sw/source/filter/html/htmldraw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmldraw.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:03:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:08:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include "hintids.hxx"
 
