a79ec85b4734d441b34fe4d223a7a079a377660c
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:14:42 +0000

    INTEGRATION: CWS pchfix02 (1.4.18); FILE MERGED 2006/09/01 17:20:37 kaib 1.4.18.1: #i68856# Added header markers and pch files

--- configmgr/source/misc/bufferedfile.cxx
+++ configmgr/source/misc/bufferedfile.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: bufferedfile.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:27:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:14:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -30,6 +30,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include <string.h>
 #include "bufferedfile.hxx"
