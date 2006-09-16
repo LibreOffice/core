fac30e4efd249fdfd2fafe9b1f327d33dcda7365
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:19:56 +0000

    INTEGRATION: CWS pchfix02 (1.61.2); FILE MERGED 2006/09/01 17:51:49 kaib 1.61.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/layact.cxx
+++ sw/source/core/layout/layact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: layact.cxx,v $
  *
- *  $Revision: 1.61 $
+ *  $Revision: 1.62 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:27:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:19:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include <time.h>
 #include "rootfrm.hxx"
