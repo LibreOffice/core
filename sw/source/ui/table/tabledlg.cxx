1602bc82892099bc00deb0770337805d800d18df
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:21:32 +0000

    INTEGRATION: CWS pchfix02 (1.30.446); FILE MERGED 2006/09/01 17:53:24 kaib 1.30.446.1: #i68856# Added header markers and pch files

--- sw/source/ui/table/tabledlg.cxx
+++ sw/source/ui/table/tabledlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabledlg.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: kz $ $Date: 2005-10-06 10:55:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:21:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
