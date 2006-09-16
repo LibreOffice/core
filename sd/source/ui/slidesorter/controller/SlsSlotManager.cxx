3c279c1f1589abbd8b2921fb60599d90efe7c91e
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:07:59 +0000

    INTEGRATION: CWS pchfix02 (1.23.118); FILE MERGED 2006/09/01 17:37:18 kaib 1.23.118.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/controller/SlsSlotManager.cxx
+++ sd/source/ui/slidesorter/controller/SlsSlotManager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsSlotManager.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-06 16:21:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:07:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 #include "SlsSlotManager.hxx"
 
 #include "SlideSorterViewShell.hxx"
