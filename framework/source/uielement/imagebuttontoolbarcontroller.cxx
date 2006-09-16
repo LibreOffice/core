c13e9fa274fdffed11d0c2d97884185998d6594c
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:20:51 +0000

    INTEGRATION: CWS pchfix02 (1.3.16); FILE MERGED 2006/09/01 17:29:26 kaib 1.3.16.1: #i68856# Added header markers and pch files

--- framework/source/uielement/imagebuttontoolbarcontroller.cxx
+++ framework/source/uielement/imagebuttontoolbarcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imagebuttontoolbarcontroller.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:38:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:20:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_UIELEMENT_IMAGEBUTTONTOOLBARCONTROLLER_HXX
 #include "uielement/imagebuttontoolbarcontroller.hxx"
