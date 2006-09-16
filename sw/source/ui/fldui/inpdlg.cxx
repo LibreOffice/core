2e6166bddbe54723d1ffbe8daafdd72f7716fc44
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:00:22 +0000

    INTEGRATION: CWS pchfix02 (1.7.476); FILE MERGED 2006/09/01 17:53:08 kaib 1.7.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/fldui/inpdlg.cxx
+++ sw/source/ui/fldui/inpdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inpdlg.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:44:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:59:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 #ifdef SW_DLLIMPLEMENTATION
 #undef SW_DLLIMPLEMENTATION
 #endif
 
 
-#pragma hdrstop
 
 #define _INPDLG_CXX
 
