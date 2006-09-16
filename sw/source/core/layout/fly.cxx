9e3e8793c2b8d5e0a368b5a4779512cb943e1611
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:18:03 +0000

    INTEGRATION: CWS pchfix02 (1.76.2); FILE MERGED 2006/09/01 17:51:47 kaib 1.76.2.1: #i68856# Added header markers and pch files

--- sw/source/core/layout/fly.cxx
+++ sw/source/core/layout/fly.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fly.cxx,v $
  *
- *  $Revision: 1.77 $
+ *  $Revision: 1.78 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 11:42:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:18:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 #include "hintids.hxx"
 #ifndef _SFXITEMITER_HXX //autogen
 #include <svtools/itemiter.hxx>
