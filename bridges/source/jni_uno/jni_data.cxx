2b517aea38be40a83eec19b5e63ba3bc5d6a319d
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:57:42 +0000

    INTEGRATION: CWS pchfix02 (1.21.22); FILE MERGED 2006/09/01 17:17:39 kaib 1.21.22.1: #i68856# Added header markers and pch files

--- bridges/source/jni_uno/jni_data.cxx
+++ bridges/source/jni_uno/jni_data.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: jni_data.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:46:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:57:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 
 #include "jni_bridge.h"
 
