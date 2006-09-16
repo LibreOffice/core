64f824b72111021b482aa6b31f50cd3dabb649d9
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:27:06 +0000

    INTEGRATION: CWS pchfix02 (1.35.76); FILE MERGED 2006/09/01 17:16:20 kaib 1.35.76.1: #i68856# Added header markers and pch files

--- basctl/source/basicide/basobj3.cxx
+++ basctl/source/basicide/basobj3.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: basobj3.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-03 12:43:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:27:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,11 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basctl.hxx"
+
 #include <ide_pch.hxx>
 
-#pragma hdrstop
 #include <vector>
 #include <algorithm>
 #ifndef _SBXCLASS_HXX //autogen
