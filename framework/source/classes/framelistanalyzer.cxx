0dece58c56df1377c5fbb3c532cb2bc4ef2e44b1
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:46:49 +0000

    INTEGRATION: CWS pchfix02 (1.8.136); FILE MERGED 2006/09/01 17:29:02 kaib 1.8.136.1: #i68856# Added header markers and pch files

--- framework/source/classes/framelistanalyzer.cxx
+++ framework/source/classes/framelistanalyzer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: framelistanalyzer.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-07 10:23:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:46:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #include "classes/framelistanalyzer.hxx"
 
