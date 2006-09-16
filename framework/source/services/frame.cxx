550e58c7015774c733638b58375c42dc823dc693
Oliver Bolte <obo@openoffice.org>
2006-09-16 13:09:49 +0000

    INTEGRATION: CWS pchfix02 (1.94.46); FILE MERGED 2006/09/01 17:29:19 kaib 1.94.46.1: #i68856# Added header markers and pch files

--- framework/source/services/frame.cxx
+++ framework/source/services/frame.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frame.cxx,v $
  *
- *  $Revision: 1.95 $
+ *  $Revision: 1.96 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:33:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 14:09:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
