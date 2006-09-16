46aa1de34da13c240ee9e88fd6d6f69c9d5d048e
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:50:03 +0000

    INTEGRATION: CWS pchfix02 (1.7.14); FILE MERGED 2006/09/01 17:54:47 kaib 1.7.14.1: #i68856# Added header markers and pch files

--- tools/bootstrp/sstring.cxx
+++ tools/bootstrp/sstring.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sstring.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-08-03 13:43:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:50:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 #ifndef _TOOLS_STRINGLIST
 #  define _TOOLS_STRINGLIST
