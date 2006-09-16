4637d05c89cf0759f63fafd98f1500682020e004
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:06:24 +0000

    INTEGRATION: CWS pchfix02 (1.11.2); FILE MERGED 2006/09/01 17:51:37 kaib 1.11.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edglss.cxx
+++ sw/source/core/edit/edglss.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edglss.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:08:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:06:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _OSL_ENDIAN_H_
 #include <osl/endian.h>
