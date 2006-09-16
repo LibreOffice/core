6896a6b0ff49708aeacf60a7ee97431c06d19e7e
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:56:27 +0000

    INTEGRATION: CWS pchfix02 (1.4.476); FILE MERGED 2006/09/01 17:53:05 kaib 1.4.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/envelp/syncbtn.cxx
+++ sw/source/ui/envelp/syncbtn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: syncbtn.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:33:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:56:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "uiparam.hxx"
 
