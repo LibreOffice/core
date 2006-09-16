c34caf5ac18e091d6e7dbb7c4c63090b3727fd8e
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:31:00 +0000

    INTEGRATION: CWS pchfix02 (1.5.2); FILE MERGED 2006/09/01 17:53:30 kaib 1.5.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/uno/unomodule.cxx
+++ sw/source/ui/uno/unomodule.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unomodule.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 18:01:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:31:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 // System - Includes -----------------------------------------------------
 
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
