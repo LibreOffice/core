ddb42a8286225b3d1eab403187d504b32eb6aa31
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:25:01 +0000

    INTEGRATION: CWS pchfix02 (1.5.476); FILE MERGED 2006/09/01 17:53:26 kaib 1.5.476.1: #i68856# Added header markers and pch files

--- sw/source/ui/uiview/viewdlg.cxx
+++ sw/source/ui/uiview/viewdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:13:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:25:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #include "hintids.hxx"
 
