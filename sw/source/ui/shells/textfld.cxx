c3734db0313783a0d0567ce5ed0ffd9ec573c1b4
Oliver Bolte <obo@openoffice.org>
2006-09-16 22:17:06 +0000

    INTEGRATION: CWS pchfix02 (1.30.2); FILE MERGED 2006/09/01 17:53:21 kaib 1.30.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/shells/textfld.cxx
+++ sw/source/ui/shells/textfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textfld.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:54:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 23:17:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>  //_immer_ vor den solar-Items
