6fa64e42311d385dc2cfd8ac3cf1c4b844c73711
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:36:17 +0000

    INTEGRATION: CWS pchfix02 (1.7.476); FILE MERGED 2006/09/01 17:51:12 kaib 1.7.476.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accgraphic.cxx
+++ sw/source/core/access/accgraphic.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accgraphic.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:50:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:36:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
-#pragma hdrstop
+
 
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
