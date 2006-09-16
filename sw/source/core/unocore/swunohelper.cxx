542f7e9df7e9d5cffff598760e9dd5b56b98c961
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:55:04 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:52:18 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- sw/source/core/unocore/swunohelper.cxx
+++ sw/source/core/unocore/swunohelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swunohelper.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:52:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:55:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#pragma hdrstop
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #define _SVSTDARR_STRINGS
 
