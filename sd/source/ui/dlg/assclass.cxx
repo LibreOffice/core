d5e71b4bc79f52303bc24849928a275df19e42cf
Oliver Bolte <obo@openoffice.org>
2006-09-16 17:35:18 +0000

    INTEGRATION: CWS pchfix02 (1.3.282); FILE MERGED 2006/09/01 17:36:56 kaib 1.3.282.1: #i68856# Added header markers and pch files

--- sd/source/ui/dlg/assclass.cxx
+++ sd/source/ui/dlg/assclass.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: assclass.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:51:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 18:35:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
 
 #ifndef _LIST_HXX //autogen
 #include <tools/list.hxx>
