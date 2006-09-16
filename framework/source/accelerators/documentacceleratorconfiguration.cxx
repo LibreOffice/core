daaeeb90d8a05e678aab9341d5c5f5202d320752
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:41:11 +0000

    INTEGRATION: CWS pchfix02 (1.8.176); FILE MERGED 2006/09/01 17:28:57 kaib 1.8.176.1: #i68856# Added header markers and pch files

--- framework/source/accelerators/documentacceleratorconfiguration.cxx
+++ framework/source/accelerators/documentacceleratorconfiguration.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentacceleratorconfiguration.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-10 16:11:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:41:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef __FRAMEWORK_ACCELERATORS_DOCUMENTACCELERATORCONFIGURATION_HXX_
 #include <accelerators/documentacceleratorconfiguration.hxx>
