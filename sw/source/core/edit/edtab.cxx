300bbb61d964d06b1aeb736cb342235e3f25e010
Oliver Bolte <obo@openoffice.org>
2006-09-16 20:07:55 +0000

    INTEGRATION: CWS pchfix02 (1.14.2); FILE MERGED 2006/09/01 17:51:38 kaib 1.14.2.1: #i68856# Added header markers and pch files

--- sw/source/core/edit/edtab.cxx
+++ sw/source/core/edit/edtab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edtab.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 16:10:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 21:07:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
