070dd778443b14fbe99db46bbb8e4d70ab0a9567
Oliver Bolte <obo@openoffice.org>
2006-09-16 14:26:19 +0000

    INTEGRATION: CWS pchfix02 (1.21.18); FILE MERGED 2006/09/01 17:20:46 kaib 1.21.18.1: #i68856# Added header markers and pch files

--- configmgr/source/treecache/disposetimer.cxx
+++ configmgr/source/treecache/disposetimer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: disposetimer.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:31:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 15:26:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_configmgr.hxx"
 
 #include <stdio.h>
 
