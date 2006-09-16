6058ed3e1a4392bab314ceaf6fb999f5cee13cb4
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:21:20 +0000

    INTEGRATION: CWS pchfix02 (1.34.10); FILE MERGED 2006/09/01 17:23:02 kaib 1.34.10.1: #i68856# Added header markers and pch files

--- cppu/source/uno/lbenv.cxx
+++ cppu/source/uno/lbenv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lbenv.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:14:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:21:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppu.hxx"
 
 #include "sal/alloca.h"
 #include "osl/diagnose.h"
