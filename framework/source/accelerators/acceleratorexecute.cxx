a25b349d81442f4836153e0078c3b5b634e2be76
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:41:26 +0000

    INTEGRATION: CWS pchfix02 (1.4.202); FILE MERGED 2006/09/01 17:28:57 kaib 1.4.202.1: #i68856# Added header markers and pch files

--- framework/source/accelerators/acceleratorexecute.cxx
+++ framework/source/accelerators/acceleratorexecute.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acceleratorexecute.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:03:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:40:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_framework.hxx"
 
 #ifndef INCLUDED_SVTOOLS_ACCELERATOREXECUTE_HXX
 #include "acceleratorexecute.hxx"
