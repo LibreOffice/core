61d08d2b23da252524b9ca43a0bede0069701d6b
Oliver Bolte <obo@openoffice.org>
2006-09-16 12:00:10 +0000

    INTEGRATION: CWS pchfix02 (1.17.30); FILE MERGED 2006/09/01 17:26:29 kaib 1.17.30.1: #i68856# Added header markers and pch files

--- extensions/source/dbpilots/listcombowizard.cxx
+++ extensions/source/dbpilots/listcombowizard.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listcombowizard.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:53:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 13:00:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
 
 #ifndef _EXTENSIONS_DBP_LISTCOMBOWIZARD_HXX_
 #include "listcombowizard.hxx"
