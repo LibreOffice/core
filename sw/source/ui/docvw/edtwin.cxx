6215e74c9329bc420dcc5dee32456825ad5cdbd2
Oliver Bolte <obo@openoffice.org>
2006-09-16 21:52:34 +0000

    INTEGRATION: CWS pchfix02 (1.135.2); FILE MERGED 2006/09/01 17:53:03 kaib 1.135.2.1: #i68856# Added header markers and pch files

--- sw/source/ui/docvw/edtwin.cxx
+++ sw/source/ui/docvw/edtwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edtwin.cxx,v $
  *
- *  $Revision: 1.135 $
+ *  $Revision: 1.136 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:34:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 22:52:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
 
 #include <tools/list.hxx>
 
