7804102f435610af19dae1b12645153336918b51
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:03:20 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:27:45 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- forms/source/xforms/convert.cxx
+++ forms/source/xforms/convert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: convert.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:44:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:03:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_forms.hxx"
 
 #include "convert.hxx"
 
