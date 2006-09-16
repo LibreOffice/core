6f7f1020b8714d126564ac0ea4725cc3e6aac70b
Oliver Bolte <obo@openoffice.org>
2006-09-16 18:04:04 +0000

    INTEGRATION: CWS pchfix02 (1.3.232); FILE MERGED 2006/09/01 17:37:16 kaib 1.3.232.1: #i68856# Added header markers and pch files

--- sd/source/ui/slidesorter/cache/SlsCacheConfiguration.cxx
+++ sd/source/ui/slidesorter/cache/SlsCacheConfiguration.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: SlsCacheConfiguration.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-11 10:47:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 19:04:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -30,6 +30,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
 
 #include "SlsCacheConfiguration.hxx"
 #include <vos/mutex.hxx>
