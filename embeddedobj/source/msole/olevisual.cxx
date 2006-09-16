78e5dc6e02c1125782e833e4524b6287973c295c
Oliver Bolte <obo@openoffice.org>
2006-09-16 23:45:09 +0000

    INTEGRATION: CWS pchfix02 (1.15.14); FILE MERGED 2006/09/01 17:25:53 kaib 1.15.14.1: #i68856# Added header markers and pch files

--- embeddedobj/source/msole/olevisual.cxx
+++ embeddedobj/source/msole/olevisual.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: olevisual.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:31:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 00:45:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_embeddedobj.hxx"
 #ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
 #endif
