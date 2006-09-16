93fa68507442b32dd6c18665ab42e93d26117747
Oliver Bolte <obo@openoffice.org>
2006-09-16 19:38:09 +0000

    INTEGRATION: CWS pchfix02 (1.29.476); FILE MERGED 2006/09/01 17:51:13 kaib 1.29.476.1: #i68856# Added header markers and pch files

--- sw/source/core/access/accportions.cxx
+++ sw/source/core/access/accportions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accportions.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:54:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-16 20:38:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
 
 #ifndef _ACCPORTIONS_HXX
 #include "accportions.hxx"
