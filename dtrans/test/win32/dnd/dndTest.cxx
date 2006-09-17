c06755f0decb006413bfeb6def5e9dce6b58d447
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:04:28 +0000

    INTEGRATION: CWS pchfix02 (1.10.16); FILE MERGED 2006/09/01 17:25:41 kaib 1.10.16.1: #i68856# Added header markers and pch files

--- dtrans/test/win32/dnd/dndTest.cxx
+++ dtrans/test/win32/dnd/dndTest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dndTest.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:08:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:04:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #if defined _MSC_VER
 #pragma warning(push,1)
