c810c04233197377ad511a869645a8075404dde8
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:14:30 +0000

    INTEGRATION: CWS pchfix02 (1.6.42); FILE MERGED 2006/09/01 17:57:51 kaib 1.6.42.1: #i68856# Added header markers and pch files

--- vcl/source/helper/threadex.cxx
+++ vcl/source/helper/threadex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: threadex.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:50:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:14:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #define THREADEX_IMPLEMENTATION
 #include <threadex.hxx>
