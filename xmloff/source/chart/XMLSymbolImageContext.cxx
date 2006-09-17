9f31e00543eed47f1bc6aa6ff9cd3ec380949955
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:17:04 +0000

    INTEGRATION: CWS pchfix02 (1.3.158); FILE MERGED 2006/09/01 17:59:24 kaib 1.3.158.1: #i68856# Added header markers and pch files

--- xmloff/source/chart/XMLSymbolImageContext.cxx
+++ xmloff/source/chart/XMLSymbolImageContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLSymbolImageContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:28:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:17:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 #include "XMLSymbolImageContext.hxx"
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
