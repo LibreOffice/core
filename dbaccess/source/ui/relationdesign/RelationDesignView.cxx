ba32bf4c754c6e578ada0675cb5b7f8df972a32b
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:28:19 +0000

    INTEGRATION: CWS pchfix02 (1.13.36); FILE MERGED 2006/09/01 17:24:40 kaib 1.13.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/relationdesign/RelationDesignView.cxx
+++ dbaccess/source/ui/relationdesign/RelationDesignView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RelationDesignView.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:45:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:28:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_RELATIONDESIGNVIEW_HXX
 #include "RelationDesignView.hxx"
 #endif
