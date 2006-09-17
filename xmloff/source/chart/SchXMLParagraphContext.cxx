903c769628ae5cad9562e014951fa9a7a4b5ce23
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:15:39 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 17:59:23 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/chart/SchXMLParagraphContext.cxx
+++ xmloff/source/chart/SchXMLParagraphContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SchXMLParagraphContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:01:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:15:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include "SchXMLImport.hxx"
 #include "SchXMLParagraphContext.hxx"
