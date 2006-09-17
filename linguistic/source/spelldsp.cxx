1366d975b706f122c9529e82ee036409d771d2a1
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:55:21 +0000

    INTEGRATION: CWS pchfix02 (1.18.30); FILE MERGED 2006/09/01 17:32:10 kaib 1.18.30.1: #i68856# Added header markers and pch files

--- linguistic/source/spelldsp.cxx
+++ linguistic/source/spelldsp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spelldsp.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:37:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:55:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
 #include <com/sun/star/uno/Reference.h>
