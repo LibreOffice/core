675707dfd3ad43c4d48b6f6dbfb86f895af79db8
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:03:25 +0000

    INTEGRATION: CWS pchfix02 (1.3.184); FILE MERGED 2006/09/01 17:24:25 kaib 1.3.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/control/undosqledit.cxx
+++ dbaccess/source/ui/control/undosqledit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undosqledit.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:37:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:03:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_GENERALUNDO_HXX
 #include "undosqledit.hxx"
