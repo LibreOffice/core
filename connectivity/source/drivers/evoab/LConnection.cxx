9c715d3877bacaf2e6566ffa0806889fe1795deb
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:25:38 +0000

    INTEGRATION: CWS pchfix02 (1.9.60); FILE MERGED 2006/09/01 17:22:06 kaib 1.9.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/evoab/LConnection.cxx
+++ connectivity/source/drivers/evoab/LConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LConnection.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:22:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:25:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_EVOAB_LCONNECTION_HXX_
 #include "LConnection.hxx"
