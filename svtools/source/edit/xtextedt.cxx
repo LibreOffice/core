8310714b8f80b40f7fa2dcf2e4795d6976e7a4b9
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:49:23 +0000

    INTEGRATION: CWS pchfix02 (1.11.92); FILE MERGED 2006/09/01 17:43:04 kaib 1.11.92.1: #i68856# Added header markers and pch files

--- svtools/source/edit/xtextedt.cxx
+++ svtools/source/edit/xtextedt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xtextedt.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:03:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:49:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include <xtextedt.hxx>
 #include <vcl/svapp.hxx>  // International
