f1bfe0407bd4ca7496158a8bada0af083112b179
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:03:58 +0000

    INTEGRATION: CWS pchfix02 (1.9.44); FILE MERGED 2006/09/01 17:54:58 kaib 1.9.44.1: #i68856# Added header markers and pch files

--- tools/source/string/strucvt.cxx
+++ tools/source/string/strucvt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strucvt.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:53:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:03:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 // =======================================================================
 
