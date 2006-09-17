b0f99a31e925e4ad81c8ded5a52cd41f828afdfe
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:08:17 +0000

    INTEGRATION: CWS pchfix02 (1.6.158); FILE MERGED 2006/09/01 18:00:03 kaib 1.6.158.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLIndexTabStopEntryContext.cxx
+++ xmloff/source/text/XMLIndexTabStopEntryContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexTabStopEntryContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:10:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:08:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 #ifndef _XMLOFF_XMLINDEXTABSTOPENTRYCONTEXT_HXX_
