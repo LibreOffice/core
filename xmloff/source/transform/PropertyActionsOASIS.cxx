0162073e4a7d7652a2550a5886e6121b4363f507
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:27:57 +0000

    INTEGRATION: CWS pchfix02 (1.17.34); FILE MERGED 2006/09/01 18:00:19 kaib 1.17.34.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/PropertyActionsOASIS.cxx
+++ xmloff/source/transform/PropertyActionsOASIS.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PropertyActionsOASIS.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:55:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:27:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
