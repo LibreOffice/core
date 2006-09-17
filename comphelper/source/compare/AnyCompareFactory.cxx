4821b9967794698b3ad32f2661ffb44ede1ab118
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:05:52 +0000

    INTEGRATION: CWS pchfix02 (1.4.100); FILE MERGED 2006/09/01 17:19:37 kaib 1.4.100.1: #i68856# Added header markers and pch files

--- comphelper/source/compare/AnyCompareFactory.cxx
+++ comphelper/source/compare/AnyCompareFactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AnyCompareFactory.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:43:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:05:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 
 #ifndef _COM_SUN_STAR_UCB_XANYCOMPAREFACTORY_HPP_
