b77ec659588205494c10be2de777b6f281cdab0a
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:24:15 +0000

    INTEGRATION: CWS pchfix02 (1.42.32); FILE MERGED 2006/09/01 17:59:30 kaib 1.42.32.1: #i68856# Added header markers and pch files

--- xmloff/source/core/xmluconv.cxx
+++ xmloff/source/core/xmluconv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmluconv.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:07:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:24:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_UTIL_DATETIME_HPP_
 #include <com/sun/star/util/DateTime.hpp>
