54bb2c1419f3a1009e91476c7ac7fd210d6b86d8
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:41:45 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 17:59:45 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/script/xmlbasici.cxx
+++ xmloff/source/script/xmlbasici.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlbasici.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:23:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:41:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLBASICI_HXX
 #include "xmlbasici.hxx"
