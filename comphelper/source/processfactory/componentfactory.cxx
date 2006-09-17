a3442c1fe96405d1d9387b10c05aec42130c78a7
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:15:55 +0000

    INTEGRATION: CWS pchfix02 (1.5.100); FILE MERGED 2006/09/01 17:19:45 kaib 1.5.100.1: #i68856# Added header markers and pch files

--- comphelper/source/processfactory/componentfactory.cxx
+++ comphelper/source/processfactory/componentfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: componentfactory.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:54:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:15:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
+
 #ifndef _COMPHELPER_COMPONENTFACTORY_HXX
 #include <comphelper/componentfactory.hxx>
 #endif
@@ -54,7 +57,6 @@
 
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 
