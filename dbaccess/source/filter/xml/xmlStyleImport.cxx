442c98db86f673e0f6cdfb151364a5466488ab22
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:48:50 +0000

    INTEGRATION: CWS pchfix02 (1.5.52); FILE MERGED 2006/09/01 17:24:15 kaib 1.5.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/filter/xml/xmlStyleImport.cxx
+++ dbaccess/source/filter/xml/xmlStyleImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlStyleImport.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:51:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:48:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBA_XMLSTYLEIMPORT_HXX
 #include "xmlStyleImport.hxx"
