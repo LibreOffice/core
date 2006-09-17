4913cb293a99b4c350ef656fbacd2b64cbf4f275
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:26:35 +0000

    INTEGRATION: CWS pchfix02 (1.20.34); FILE MERGED 2006/09/01 18:00:18 kaib 1.20.34.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/Oasis2OOo.cxx
+++ xmloff/source/transform/Oasis2OOo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Oasis2OOo.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:54:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:26:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSETINFO_HPP_
 #include <com/sun/star/beans/XPropertySetInfo.hpp>
