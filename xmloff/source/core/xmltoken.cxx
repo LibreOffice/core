0385d54dcae2da156d61c5a15332305c95b91355
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:24:00 +0000

    INTEGRATION: CWS pchfix02 (1.97.130); FILE MERGED 2006/09/01 17:59:30 kaib 1.97.130.1: #i68856# Added header markers and pch files

--- xmloff/source/core/xmltoken.cxx
+++ xmloff/source/core/xmltoken.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltoken.cxx,v $
  *
- *  $Revision: 1.97 $
+ *  $Revision: 1.98 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-14 09:12:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:24:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
 #include "xmltoken.hxx"
