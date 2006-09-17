b7549714220050e2c436c2c5aeb4a9ffa4a3e52d
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:26:21 +0000

    INTEGRATION: CWS pchfix02 (1.43.82); FILE MERGED 2006/09/01 17:38:29 kaib 1.43.82.1: #i68856# Added header markers and pch files

--- sfx2/source/control/dispatch.cxx
+++ sfx2/source/control/dispatch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dispatch.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:16:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:26:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include <com/sun/star/beans/XPropertySet.hpp>
@@ -85,7 +88,6 @@
 #endif
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 // wg. nAutoPageID
