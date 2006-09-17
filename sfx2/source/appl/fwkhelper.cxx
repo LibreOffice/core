e0c35c1407aea7a468ed96543258069ab174ba3e
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:17:14 +0000

    INTEGRATION: CWS pchfix02 (1.2.146); FILE MERGED 2006/09/01 17:38:23 kaib 1.2.146.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/fwkhelper.cxx
+++ sfx2/source/appl/fwkhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fwkhelper.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 10:24:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:17:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
 #include <com/sun/star/uno/Reference.hxx>
