b0f8ce319a0de2834b01c6723866f93f911fddf7
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:13:51 +0000

    INTEGRATION: CWS pchfix02 (1.66.88); FILE MERGED 2006/09/01 17:38:20 kaib 1.66.88.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/appcfg.cxx
+++ sfx2/source/appl/appcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: appcfg.cxx,v $
  *
- *  $Revision: 1.66 $
+ *  $Revision: 1.67 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:06:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:13:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
 #include <com/sun/star/uno/Reference.hxx>
@@ -109,7 +112,6 @@
 #endif
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 //#include <svtools/agprop.hxx>
