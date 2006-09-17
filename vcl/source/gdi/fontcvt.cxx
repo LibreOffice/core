f7bb9203bf07b743927081e106af6f371c82b76e
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:00:23 +0000

    INTEGRATION: CWS pchfix02 (1.19.112); FILE MERGED 2006/09/01 17:57:42 kaib 1.19.112.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/fontcvt.cxx
+++ vcl/source/gdi/fontcvt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fontcvt.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:23:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:00:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_FONTCVT_HXX
 #include <fontcvt.hxx>
