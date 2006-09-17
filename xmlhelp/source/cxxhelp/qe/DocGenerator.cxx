7c0c7fd62a75fed98662dbf6054cb2f8554db6a2
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:19:18 +0000

    INTEGRATION: CWS pchfix02 (1.8.4); FILE MERGED 2006/09/01 17:58:34 kaib 1.8.4.1: #i68856# Added header markers and pch files

--- xmlhelp/source/cxxhelp/qe/DocGenerator.cxx
+++ xmlhelp/source/cxxhelp/qe/DocGenerator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DocGenerator.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:13:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:19:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlhelp.hxx"
 #ifndef _XMLSEARCH_QE_DOCGENERATOR_HXX_
 #include <qe/DocGenerator.hxx>
 #endif
