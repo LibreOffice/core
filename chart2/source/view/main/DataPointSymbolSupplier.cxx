06b7ae53c6d5af38291bf3511d62996f41b4b034
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:35:31 +0000

    INTEGRATION: CWS pchfix02 (1.4.82); FILE MERGED 2006/09/01 17:19:00 kaib 1.4.82.1: #i68856# Added header markers and pch files

--- chart2/source/view/main/DataPointSymbolSupplier.cxx
+++ chart2/source/view/main/DataPointSymbolSupplier.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataPointSymbolSupplier.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:47:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:35:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 
 #include "chartview/DataPointSymbolSupplier.hxx"
 #include "ShapeFactory.hxx"
