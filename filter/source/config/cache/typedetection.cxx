f80d19c2ecca751325c0767c85696d112aeab9e3
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:38:38 +0000

    INTEGRATION: CWS pchfix02 (1.20.12); FILE MERGED 2006/09/01 17:27:08 kaib 1.20.12.1: #i68856# Added header markers and pch files

--- filter/source/config/cache/typedetection.cxx
+++ filter/source/config/cache/typedetection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typedetection.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 11:14:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:38:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 #include "typedetection.hxx"
 #include "constant.hxx"
 
