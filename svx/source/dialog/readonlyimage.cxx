db856fc930f7cc2c64d6cb93f79fbde53f0d90dd
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:35:59 +0000

    INTEGRATION: CWS pchfix02 (1.4.536); FILE MERGED 2006/09/01 17:46:21 kaib 1.4.536.1: #i68856# Added header markers and pch files

--- svx/source/dialog/readonlyimage.cxx
+++ svx/source/dialog/readonlyimage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: readonlyimage.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:56:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:35:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
