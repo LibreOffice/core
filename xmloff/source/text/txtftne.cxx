434061b1d965828482c2edce1f2e156618ce39d4
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:18:02 +0000

    INTEGRATION: CWS pchfix02 (1.17.16); FILE MERGED 2006/09/01 18:00:10 kaib 1.17.16.1: #i68856# Added header markers and pch files

--- xmloff/source/text/txtftne.cxx
+++ xmloff/source/text/txtftne.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtftne.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:41:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:18:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 /** @#file
