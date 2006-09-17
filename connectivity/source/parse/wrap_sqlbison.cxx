65405239342d28332d39e06626007f42ee0a32db
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:09:54 +0000

    INTEGRATION: CWS pchfix02 (1.3.60); FILE MERGED 2006/09/01 17:22:35 kaib 1.3.60.1: #i68856# Added header markers and pch files

--- connectivity/source/parse/wrap_sqlbison.cxx
+++ connectivity/source/parse/wrap_sqlbison.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrap_sqlbison.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:09:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:09:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,5 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "sqlbison.cxx"
