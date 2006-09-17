61cc14d362536ebc2f31d13421eb90c62d9a3f43
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:36:15 +0000

    INTEGRATION: CWS pchfix02 (1.4.116); FILE MERGED 2006/09/01 17:46:22 kaib 1.4.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/relfld.cxx
+++ svx/source/dialog/relfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: relfld.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:26:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:36:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // include ---------------------------------------------------------------
 
