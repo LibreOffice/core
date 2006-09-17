4122710b76f05f0a2864294767d2c617667ef4df
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:06:23 +0000

    INTEGRATION: CWS pchfix02 (1.7.116); FILE MERGED 2006/09/01 17:46:49 kaib 1.7.116.1: #i68856# Added header markers and pch files

--- svx/source/form/fmdpage.cxx
+++ svx/source/form/fmdpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmdpage.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:54:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:06:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SVX_FMPAGE_HXX
 #include "fmpage.hxx"
