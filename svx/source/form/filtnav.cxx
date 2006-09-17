ad2682a0343aedcab29826747a669c365e9df7f6
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:04:30 +0000

    INTEGRATION: CWS pchfix02 (1.42.116); FILE MERGED 2006/09/01 17:46:48 kaib 1.42.116.1: #i68856# Added header markers and pch files

--- svx/source/form/filtnav.cxx
+++ svx/source/form/filtnav.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filtnav.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:52:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:04:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef _SVX_FMRESIDS_HRC
 #include "fmresids.hrc"
 #endif
