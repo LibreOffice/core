4ee5b9afa283f7d604b877fbac3379d74a6709ef
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:23:59 +0000

    INTEGRATION: CWS pchfix02 (1.6.8); FILE MERGED 2006/09/01 17:56:21 kaib 1.6.8.1: #i68856# Added header markers and pch files

--- unotools/source/config/configvaluecontainer.cxx
+++ unotools/source/config/configvaluecontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: configvaluecontainer.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:53:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:23:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef UNOTOOLS_CONFIGVALUECONTAINER_HXX
 #include <unotools/configvaluecontainer.hxx>
