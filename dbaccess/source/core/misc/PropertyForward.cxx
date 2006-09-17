41ca4225b9067468f013a26da277f6fb311b643d
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:41:48 +0000

    INTEGRATION: CWS pchfix02 (1.7.36); FILE MERGED 2006/09/01 17:24:10 kaib 1.7.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/misc/PropertyForward.cxx
+++ dbaccess/source/core/misc/PropertyForward.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PropertyForward.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:16:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:41:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBA_PROPERTYSETFORWARD_HXX
 #include "PropertyForward.hxx"
 #endif
