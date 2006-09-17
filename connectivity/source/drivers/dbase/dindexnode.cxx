4e6a3c72c0eda0f6fbeef25958e4dcf1abe7b6aa
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:24:30 +0000

    INTEGRATION: CWS pchfix02 (1.19.34); FILE MERGED 2006/09/01 17:22:04 kaib 1.19.34.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/dbase/dindexnode.cxx
+++ connectivity/source/drivers/dbase/dindexnode.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dindexnode.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 15:52:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:24:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_DBASE_INDEXNODE_HXX_
 #include "dbase/dindexnode.hxx"
