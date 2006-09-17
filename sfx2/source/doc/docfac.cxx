0941033cbb51b288a7f7436a48ef453f22bc5a19
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:39:06 +0000

    INTEGRATION: CWS pchfix02 (1.25.88); FILE MERGED 2006/09/01 17:38:38 kaib 1.25.88.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/docfac.cxx
+++ sfx2/source/doc/docfac.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docfac.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:26:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:39:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_REGISTRY_MERGECONFLICTEXCEPTION_HPP_
 #include <com/sun/star/registry/MergeConflictException.hpp>
