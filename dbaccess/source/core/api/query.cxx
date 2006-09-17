ad5c6c77b920aff6c4a8d35249cbb6324e74ec8c
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:35:02 +0000

    INTEGRATION: CWS pchfix02 (1.31.36); FILE MERGED 2006/09/01 17:24:06 kaib 1.31.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/query.cxx
+++ dbaccess/source/core/api/query.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: query.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:05:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:35:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_COREAPI_QUERY_HXX_
 #include "query.hxx"
