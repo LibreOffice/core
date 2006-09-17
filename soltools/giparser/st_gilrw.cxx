edf5be31ffde422d83beb8767dc341d3fb2cbead
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:33:48 +0000

    INTEGRATION: CWS pchfix02 (1.2.30); FILE MERGED 2006/09/01 17:40:16 kaib 1.2.30.1: #i68856# Added header markers and pch files

--- soltools/giparser/st_gilrw.cxx
+++ soltools/giparser/st_gilrw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: st_gilrw.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:24:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:33:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_soltools.hxx"
 
 
 #include <st_gilrw.hxx>
