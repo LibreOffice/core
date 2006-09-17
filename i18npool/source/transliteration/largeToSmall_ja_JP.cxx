273fa1c3aff3069a1bede4e27731e79a1aa490c7
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:30:59 +0000

    INTEGRATION: CWS pchfix02 (1.8.80); FILE MERGED 2006/09/01 17:30:53 kaib 1.8.80.1: #i68856# Added header markers and pch files

--- i18npool/source/transliteration/largeToSmall_ja_JP.cxx
+++ i18npool/source/transliteration/largeToSmall_ja_JP.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: largeToSmall_ja_JP.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:33:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:30:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 // prevent internal compiler error with MSVC6SP3
 #include <utility>
