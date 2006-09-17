5a64d7cce17d2995159d54ed908fc118a645dbbc
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:39:26 +0000

    INTEGRATION: CWS pchfix02 (1.4.6); FILE MERGED 2006/09/01 18:00:34 kaib 1.4.6.1: #i68856# Added header markers and pch files

--- xmlscript/source/xmlflat_imexp/xmlbas_import.cxx
+++ xmlscript/source/xmlflat_imexp/xmlbas_import.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlbas_import.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:12:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:39:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlscript.hxx"
 
 #ifndef XMLSCRIPT_XMLBAS_IMPORT_HXX
 #include "xmlbas_import.hxx"
