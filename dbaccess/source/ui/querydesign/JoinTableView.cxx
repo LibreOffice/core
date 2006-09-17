07427e498c3ca3a7f4284776f2b1618fd05c02a4
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:20:59 +0000

    INTEGRATION: CWS pchfix02 (1.53.50); FILE MERGED 2006/09/01 17:24:36 kaib 1.53.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/JoinTableView.cxx
+++ dbaccess/source/ui/querydesign/JoinTableView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: JoinTableView.cxx,v $
  *
- *  $Revision: 1.53 $
+ *  $Revision: 1.54 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:26:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:20:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_QUERYTABLEVIEW_HXX
 #include "JoinTableView.hxx"
 #endif
