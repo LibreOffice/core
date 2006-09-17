a297e6090b5a941e6b49726644400508da892f67
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:47:18 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:02 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/edit/svmedit2.cxx
+++ svtools/source/edit/svmedit2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svmedit2.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:01:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:47:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include "svmedit2.hxx"
 #include "xtextedt.hxx"
