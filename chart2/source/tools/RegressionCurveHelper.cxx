258dcb233c97891c87ce2923d0046f592da33c2a
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:28:45 +0000

    INTEGRATION: CWS pchfix02 (1.8.80); FILE MERGED 2006/09/01 17:18:54 kaib 1.8.80.1: #i68856# Added header markers and pch files

--- chart2/source/tools/RegressionCurveHelper.cxx
+++ chart2/source/tools/RegressionCurveHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RegressionCurveHelper.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:33:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:28:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "RegressionCurveHelper.hxx"
 #include "ContextHelper.hxx"
 #include "MeanValueRegressionCurveCalculator.hxx"
