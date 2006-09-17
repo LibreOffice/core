e7ef95d60497a917c4bc797c8cb9e74720cf3174
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:15:51 +0000

    INTEGRATION: CWS pchfix02 (1.12.34); FILE MERGED 2006/09/01 18:00:08 kaib 1.12.34.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLTextShapeImportHelper.cxx
+++ xmloff/source/text/XMLTextShapeImportHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextShapeImportHelper.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:48:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:15:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_TEXT_XTEXTCONTENT_HPP_
 #include <com/sun/star/text/XTextContent.hpp>
