d5548d4fd67f396b20c8955cb880f3ecb7b6e73b
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:55:35 +0000

    INTEGRATION: CWS pchfix02 (1.6.20); FILE MERGED 2006/09/01 17:55:46 kaib 1.6.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/hierarchy/hierarchycontentcaps.cxx
+++ ucb/source/ucp/hierarchy/hierarchycontentcaps.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hierarchycontentcaps.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:28:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:55:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 /**************************************************************************
                                 TODO
