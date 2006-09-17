6213295be53acab2778af0d1eb1a9ad350684f6a
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:22:40 +0000

    INTEGRATION: CWS pchfix02 (1.72.86); FILE MERGED 2006/09/01 17:38:27 kaib 1.72.86.1: #i68856# Added header markers and pch files

--- sfx2/source/bastyp/fltfnc.cxx
+++ sfx2/source/bastyp/fltfnc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fltfnc.cxx,v $
  *
- *  $Revision: 1.72 $
+ *  $Revision: 1.73 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:13:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:22:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include "fltfnc.hxx"
 
