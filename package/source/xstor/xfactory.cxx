299eb6703230c2706c41795a0ef9df1035cc4977
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:25:20 +0000

    INTEGRATION: CWS pchfix02 (1.7.10); FILE MERGED 2006/09/01 17:32:39 kaib 1.7.10.1: #i68856# Added header markers and pch files

--- package/source/xstor/xfactory.cxx
+++ package/source/xstor/xfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xfactory.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:12:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:25:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 
 #ifndef _COM_SUN_STAR_UCB_XSIMPLEFILEACCESS_HPP_
 #include <com/sun/star/ucb/XSimpleFileAccess.hpp>
