b10663d1b031e40ceea1431cc5857dc71d14246a
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:45:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.88); FILE MERGED 2006/09/01 17:38:43 kaib 1.4.88.1: #i68856# Added header markers and pch files

--- sfx2/source/explorer/nochaos.cxx
+++ sfx2/source/explorer/nochaos.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: nochaos.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:31:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:45:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include <svtools/itempool.hxx>
 #include <svtools/poolitem.hxx>
