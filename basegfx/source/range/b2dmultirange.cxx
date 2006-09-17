27d39b26167bff8e20c3ad4e5e8a5e440b76a82c
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:04:43 +0000

    INTEGRATION: CWS pchfix02 (1.5.12); FILE MERGED 2006/09/01 17:16:38 kaib 1.5.12.1: #i68856# Added header markers and pch files

--- basegfx/source/range/b2dmultirange.cxx
+++ basegfx/source/range/b2dmultirange.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dmultirange.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 09:57:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:04:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_RANGE_B2DRANGE_HXX
 #include <basegfx/range/b2drange.hxx>
