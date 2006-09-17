6a6e72174b23e254941e4e30fae3852045613ad7
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:12:48 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:21 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/misc/restrictedpaths.cxx
+++ svtools/source/misc/restrictedpaths.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: restrictedpaths.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:20:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:12:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef SVTOOLS_RESTRICTEDPATHS_HXX
 #include "restrictedpaths.hxx"
