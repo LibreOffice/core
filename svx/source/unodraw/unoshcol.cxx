2f3a439234c038b3d9eb7cf950c24be26871e32a
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:15:37 +0000

    INTEGRATION: CWS pchfix02 (1.7.116); FILE MERGED 2006/09/01 17:47:40 kaib 1.7.116.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/unoshcol.cxx
+++ svx/source/unodraw/unoshcol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoshcol.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:57:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:15:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_DOCUMENT_EVENTOBJECT_HPP_
 #include <com/sun/star/document/EventObject.hpp>
