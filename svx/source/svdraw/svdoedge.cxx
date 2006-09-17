89273301c0a5050bbe46188c0b525391dbfa2312
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:54:40 +0000

    INTEGRATION: CWS pchfix02 (1.35.114); FILE MERGED 2006/09/01 17:47:24 kaib 1.35.114.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdoedge.cxx
+++ svx/source/svdraw/svdoedge.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdoedge.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:40:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:54:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "svdoedge.hxx"
 #include "xpool.hxx"
