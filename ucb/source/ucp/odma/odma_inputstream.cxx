0a7f0475ee41bfc756836f47e40ff3afad01b031
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:57:56 +0000

    INTEGRATION: CWS pchfix02 (1.5.70); FILE MERGED 2006/09/01 17:55:48 kaib 1.5.70.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/odma/odma_inputstream.cxx
+++ ucb/source/ucp/odma/odma_inputstream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: odma_inputstream.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:51:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:57:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 #ifndef ODMA_INPUTSTREAM_HXX
 #include "odma_inputstream.hxx"
