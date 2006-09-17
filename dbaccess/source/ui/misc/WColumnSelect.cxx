ec87e3692c9874fd97aac0284b0ceee01f75d5bd
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:14:34 +0000

    INTEGRATION: CWS pchfix02 (1.20.48); FILE MERGED 2006/09/01 17:24:32 kaib 1.20.48.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/WColumnSelect.cxx
+++ dbaccess/source/ui/misc/WColumnSelect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WColumnSelect.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:21:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:14:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_WIZ_COLUMNSELECT_HXX
 #include "WColumnSelect.hxx"
