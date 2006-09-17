f9a8cb57e406849525953d5e20bf40921af6251a
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:27:41 +0000

    INTEGRATION: CWS pchfix02 (1.16.44); FILE MERGED 2006/09/01 17:43:31 kaib 1.16.44.1: #i68856# Added header markers and pch files

--- svtools/source/svrtf/rtfkeywd.cxx
+++ svtools/source/svrtf/rtfkeywd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rtfkeywd.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:10:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:27:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
