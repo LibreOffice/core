e3e6b7425766c2533e3f6c0b5a81841e5a818602
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:19:29 +0000

    INTEGRATION: CWS pchfix02 (1.5.114); FILE MERGED 2006/09/01 17:46:59 kaib 1.5.114.1: #i68856# Added header markers and pch files

--- svx/source/items/e3ditem.cxx
+++ svx/source/items/e3ditem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: e3ditem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:11:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:19:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_DRAWING_DIRECTION3D_HPP_
 #include <com/sun/star/drawing/Direction3D.hpp>
