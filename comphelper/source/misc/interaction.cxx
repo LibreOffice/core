16335db18b9641999b56fa89eddb1354daf2770b
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:11:42 +0000

    INTEGRATION: CWS pchfix02 (1.5.100); FILE MERGED 2006/09/01 17:19:42 kaib 1.5.100.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/interaction.cxx
+++ comphelper/source/misc/interaction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: interaction.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:50:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:11:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef _COMPHELPER_INTERACTION_HXX_
 #include <comphelper/interaction.hxx>
