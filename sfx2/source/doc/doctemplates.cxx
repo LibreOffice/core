3230d67aff2ea38e4144c647a064b469151e0b73
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:40:33 +0000

    INTEGRATION: CWS pchfix02 (1.35.4); FILE MERGED 2006/09/01 17:38:39 kaib 1.35.4.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/doctemplates.cxx
+++ sfx2/source/doc/doctemplates.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: doctemplates.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 14:34:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:40:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include "doctemplates.hxx"
 
