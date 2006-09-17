aec0baed4003ee81d98dd86c30f6db961c403fe7
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:11:38 +0000

    INTEGRATION: CWS pchfix02 (1.16.60); FILE MERGED 2006/09/01 17:22:37 kaib 1.16.60.1: #i68856# Added header markers and pch files

--- connectivity/source/sdbcx/VIndex.cxx
+++ connectivity/source/sdbcx/VIndex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VIndex.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:10:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:11:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_SDBCX_INDEX_HXX_
 #include "connectivity/sdbcx/VIndex.hxx"
