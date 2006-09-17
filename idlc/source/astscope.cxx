61aacc0b6bacd69559d0491ffc505b653cb4eaf1
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:12:33 +0000

    INTEGRATION: CWS pchfix02 (1.11.8); FILE MERGED 2006/09/01 17:31:13 kaib 1.11.8.1: #i68856# Added header markers and pch files

--- idlc/source/astscope.cxx
+++ idlc/source/astscope.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: astscope.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:47:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:12:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 #ifndef _IDLC_ASTSCOPE_HXX_
 #include <idlc/astscope.hxx>
 #endif
