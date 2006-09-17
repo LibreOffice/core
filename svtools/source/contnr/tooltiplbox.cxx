4988d366d5ca90d2c3a258f82c2f65f821c424b9
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:35:44 +0000

    INTEGRATION: CWS pchfix02 (1.3.34); FILE MERGED 2006/09/01 17:42:54 kaib 1.3.34.1: #i68856# Added header markers and pch files

--- svtools/source/contnr/tooltiplbox.cxx
+++ svtools/source/contnr/tooltiplbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tooltiplbox.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 08:26:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:35:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef SVTOOLS_TOOLTIPLBOX_HXX
 #include "tooltiplbox.hxx"
