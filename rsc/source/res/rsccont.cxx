4c266e78984aa7160fab912b180d85697641955e
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:02:09 +0000

    INTEGRATION: CWS pchfix02 (1.5.46); FILE MERGED 2006/09/01 17:33:23 kaib 1.5.46.1: #i68856# Added header markers and pch files

--- rsc/source/res/rsccont.cxx
+++ rsc/source/res/rsccont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rsccont.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 13:55:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:02:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_rsc.hxx"
 /****************** I N C L U D E S **************************************/
 
 // C and C++ Includes.
