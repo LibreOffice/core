1db0ace7d206d68b20d4865daed3c4216b3cb939
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:46:42 +0000

    INTEGRATION: CWS pchfix02 (1.3.534); FILE MERGED 2006/09/01 17:47:18 kaib 1.3.534.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/clonelist.cxx
+++ svx/source/svdraw/clonelist.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: clonelist.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:20:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:46:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // #i13033#
 // New mechanism to hold a ist of all original and cloned objects for later
