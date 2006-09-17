6fdb24a06a1a2d8180039a99ac4f09810235d894
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:01:34 +0000

    INTEGRATION: CWS pchfix02 (1.4.158); FILE MERGED 2006/09/01 17:59:59 kaib 1.4.158.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLAutoMarkFileContext.cxx
+++ xmloff/source/text/XMLAutoMarkFileContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLAutoMarkFileContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:56:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:01:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLAUTOMARKFILECONTEXT_HXX_
 #include "XMLAutoMarkFileContext.hxx"
