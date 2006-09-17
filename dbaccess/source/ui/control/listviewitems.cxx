9d83c6dc6199d5196d60f42d16e5662b10bedbda
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:01:47 +0000

    INTEGRATION: CWS pchfix02 (1.4.118); FILE MERGED 2006/09/01 17:24:24 kaib 1.4.118.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/control/listviewitems.cxx
+++ dbaccess/source/ui/control/listviewitems.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listviewitems.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:41:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:01:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_LISTVIEWITEMS_HXX_
 #include "listviewitems.hxx"
