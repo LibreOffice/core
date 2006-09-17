885b5afed9a3ef2b21dbb404af1f829923e77476
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:43:48 +0000

    INTEGRATION: CWS pchfix02 (1.5.534); FILE MERGED 2006/09/01 17:47:16 kaib 1.5.534.1: #i68856# Added header markers and pch files

--- svx/source/sdr/properties/measureproperties.cxx
+++ svx/source/sdr/properties/measureproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: measureproperties.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:15:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:43:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_PROPERTIES_MEASUREPROPERTIES_HXX
 #include <svx/sdr/properties/measureproperties.hxx>
