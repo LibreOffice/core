57e40d52dceb2a4fbcf5f14d8542d5877ed6d6b8
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:15:39 +0000

    INTEGRATION: CWS pchfix02 (1.2.8); FILE MERGED 2006/09/01 17:31:15 kaib 1.2.8.1: #i68856# Added header markers and pch files

--- idlc/source/wrap_parser.cxx
+++ idlc/source/wrap_parser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrap_parser.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:50:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:15:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,5 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 
 #include "parser.cxx"
