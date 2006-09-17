c4dd2300e16b6bce035d52857c662a28ea4405a7
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:00:36 +0000

    INTEGRATION: CWS pchfix02 (1.8.184); FILE MERGED 2006/09/01 17:24:23 kaib 1.8.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/control/SqlNameEdit.cxx
+++ dbaccess/source/ui/control/SqlNameEdit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SqlNameEdit.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:32:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:00:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_SQLNAMEEDIT_HXX
 #include "SqlNameEdit.hxx"
 #endif
