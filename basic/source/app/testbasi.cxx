7ddf1cdbe9a916d7c6fa5a7ad664e7ff1c82101b
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:58:16 +0000

    INTEGRATION: CWS pchfix02 (1.2.124); FILE MERGED 2006/09/01 17:16:59 kaib 1.2.124.1: #i68856# Added header markers and pch files

--- basic/source/app/testbasi.cxx
+++ basic/source/app/testbasi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testbasi.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:20:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:58:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,5 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 #define testtool
 #include "mybasic.cxx"
