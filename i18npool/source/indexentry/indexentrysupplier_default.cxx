f2b35367f4a9aeecda3833d28840f89b956a1e19
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:18:56 +0000

    INTEGRATION: CWS pchfix02 (1.11.16); FILE MERGED 2006/09/01 17:30:43 kaib 1.11.16.1: #i68856# Added header markers and pch files

--- i18npool/source/indexentry/indexentrysupplier_default.cxx
+++ i18npool/source/indexentry/indexentrysupplier_default.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: indexentrysupplier_default.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:45:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:18:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 #include <indexentrysupplier_default.hxx>
 #include <localedata.hxx>
 #include <i18nutil/unicode.hxx>
