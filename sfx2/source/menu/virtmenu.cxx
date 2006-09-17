7ad87aa93ceb2f8bf22fab00e977bc4097ebde37
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:47:18 +0000

    INTEGRATION: CWS pchfix02 (1.44.86); FILE MERGED 2006/09/01 17:38:44 kaib 1.44.86.1: #i68856# Added header markers and pch files

--- sfx2/source/menu/virtmenu.cxx
+++ sfx2/source/menu/virtmenu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: virtmenu.cxx,v $
  *
- *  $Revision: 1.44 $
+ *  $Revision: 1.45 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:36:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:47:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include <sot/factory.hxx>
 #include <svtools/menuoptions.hxx>
@@ -59,7 +62,6 @@
 #endif
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "virtmenu.hxx"
