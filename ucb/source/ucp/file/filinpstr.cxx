864973c1b6c01acf7f3672083d26403ec3bad9d2
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:46:57 +0000

    INTEGRATION: CWS pchfix02 (1.10.20); FILE MERGED 2006/09/01 17:55:41 kaib 1.10.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/file/filinpstr.cxx
+++ ucb/source/ucp/file/filinpstr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filinpstr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:20:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:46:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 #ifndef _FILINPSTR_HXX_
 #include "filinpstr.hxx"
 #endif
