57516dcbf4a6b8dcc1208e0c1db6ae87f15b4107
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:39:50 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 17:59:43 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/meta/xmlversion.cxx
+++ xmloff/source/meta/xmlversion.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlversion.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:22:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:39:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
 #include <com/sun/star/embed/ElementModes.hpp>
