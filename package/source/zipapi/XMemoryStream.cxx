8d0a1b1ef9a5d4a1472ff3d9c029d26e61678fd0
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:27:35 +0000

    INTEGRATION: CWS pchfix02 (1.2.44); FILE MERGED 2006/09/01 17:32:41 kaib 1.2.44.1: #i68856# Added header markers and pch files

--- package/source/zipapi/XMemoryStream.cxx
+++ package/source/zipapi/XMemoryStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMemoryStream.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:13:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:27:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 #ifndef _XMEMORY_STREAM_HXX
 #include <XMemoryStream.hxx>
 #endif
