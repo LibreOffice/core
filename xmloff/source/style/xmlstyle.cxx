5bcfeae3051fd40c59c03c7009ad2f59da8dc8c7
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:00:54 +0000

    INTEGRATION: CWS pchfix02 (1.38.32); FILE MERGED 2006/09/01 17:59:58 kaib 1.38.32.1: #i68856# Added header markers and pch files

--- xmloff/source/style/xmlstyle.cxx
+++ xmloff/source/style/xmlstyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlstyle.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:37:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:00:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_XML_SAX_SAXPARSEEXCEPTION_HPP_
 #include <com/sun/star/xml/sax/SAXParseException.hpp>
