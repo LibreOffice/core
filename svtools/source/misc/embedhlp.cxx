e26751ebb76147dfe3b494ad9f7cd23da495f04c
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:09:44 +0000

    INTEGRATION: CWS pchfix02 (1.17.90); FILE MERGED 2006/09/01 17:43:19 kaib 1.17.90.1: #i68856# Added header markers and pch files

--- svtools/source/misc/embedhlp.cxx
+++ svtools/source/misc/embedhlp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: embedhlp.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:19:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:09:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include "embedhlp.hxx"
 
