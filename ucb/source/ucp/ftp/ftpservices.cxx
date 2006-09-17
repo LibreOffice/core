60d47dc6bbb9f3b860ccadc68a75d918637819a4
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:52:43 +0000

    INTEGRATION: CWS pchfix02 (1.4.20); FILE MERGED 2006/09/01 17:55:45 kaib 1.4.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/ftp/ftpservices.cxx
+++ ucb/source/ucp/ftp/ftpservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ftpservices.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:25:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:52:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
