aff270c4a4e2b1b8c427a6b2b332d89f3134d623
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:33:54 +0000

    INTEGRATION: CWS pchfix02 (1.7.84); FILE MERGED 2006/09/01 17:24:44 kaib 1.7.84.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/uno/composerdialogs.cxx
+++ dbaccess/source/ui/uno/composerdialogs.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: composerdialogs.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-31 12:16:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:33:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_SOURCE_UI_UNO_COMPOSERDIALOGS_HXX
 #include "composerdialogs.hxx"
