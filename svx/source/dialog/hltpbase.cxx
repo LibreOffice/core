e5a1f0dca612cf5358a14a22c08e2bf7b4a48d40
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:24:00 +0000

    INTEGRATION: CWS pchfix02 (1.29.114); FILE MERGED 2006/09/01 17:46:14 kaib 1.29.114.1: #i68856# Added header markers and pch files

--- svx/source/dialog/hltpbase.cxx
+++ svx/source/dialog/hltpbase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hltpbase.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:13:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:24:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
