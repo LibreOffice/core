6c6d28dea54b9d201bd8d0fccacee25e3fbab4ce
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:11:10 +0000

    INTEGRATION: CWS pchfix02 (1.91.12); FILE MERGED 2006/09/01 17:40:30 kaib 1.91.12.1: #i68856# Added header markers and pch files

--- sot/source/sdstor/ucbstorage.cxx
+++ sot/source/sdstor/ucbstorage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ucbstorage.cxx,v $
  *
- *  $Revision: 1.92 $
+ *  $Revision: 1.93 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-24 10:46:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:11:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #ifndef _COM_SUN_STAR_IO_NOTCONNECTEDEXCEPTION_HPP_
 #include <com/sun/star/io/NotConnectedException.hpp>
