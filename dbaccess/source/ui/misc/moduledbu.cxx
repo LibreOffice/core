6d03e675776f42fe70358c411f17b037cdda203e
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:17:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.178); FILE MERGED 2006/09/01 17:24:34 kaib 1.4.178.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/moduledbu.cxx
+++ dbaccess/source/ui/misc/moduledbu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: moduledbu.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:41:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:17:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_MODULE_DBU_HXX_
 #include "moduledbu.hxx"
