8568cf98a520bdece0a9a8e76c7999d2e3db4ebf
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:58:08 +0000

    INTEGRATION: CWS pchfix02 (1.10.4); FILE MERGED 2006/09/01 17:57:40 kaib 1.10.4.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/bmpacc.cxx
+++ vcl/source/gdi/bmpacc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bmpacc.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:33:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:58:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_SALBTYPE_HXX
 #include <salbtype.hxx>
