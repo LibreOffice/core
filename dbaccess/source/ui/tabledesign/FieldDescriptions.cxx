9f13e68cc781a33b290f0c78bd673db7d70deaaa
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:29:02 +0000

    INTEGRATION: CWS pchfix02 (1.22.4); FILE MERGED 2006/09/01 17:24:41 kaib 1.22.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/tabledesign/FieldDescriptions.cxx
+++ dbaccess/source/ui/tabledesign/FieldDescriptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FieldDescriptions.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:57:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:29:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_FIELDDESCRIPTIONS_HXX
 #include "FieldDescriptions.hxx"
 #endif
