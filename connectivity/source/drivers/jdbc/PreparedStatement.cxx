0d216ae1d7e1da120b57f8c0334937ed5db3e270
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:47:47 +0000

    INTEGRATION: CWS pchfix02 (1.16.60); FILE MERGED 2006/09/01 17:22:20 kaib 1.16.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/jdbc/PreparedStatement.cxx
+++ connectivity/source/drivers/jdbc/PreparedStatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PreparedStatement.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:35:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:47:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef _CONNECTIVITY_JAVA_SQL_PREPAREDSTATEMENT_HXX_
 #include "java/sql/PreparedStatement.hxx"
 #endif
