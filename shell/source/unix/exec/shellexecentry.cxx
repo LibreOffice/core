ea999f3440f9828148a0eeb564744006b3e54d02
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:42:14 +0000

    INTEGRATION: CWS pchfix02 (1.4.32); FILE MERGED 2006/09/01 17:39:07 kaib 1.4.32.1: #i68856# Added header markers and pch files

--- shell/source/unix/exec/shellexecentry.cxx
+++ shell/source/unix/exec/shellexecentry.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shellexecentry.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:19:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:42:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 //-----------------------------------------------------------------------
 //  includes of other projects
