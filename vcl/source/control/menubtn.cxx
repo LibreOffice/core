c1bd339f2919f02415211f5dd79cba86290356bf
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:54:28 +0000

    INTEGRATION: CWS pchfix02 (1.6.112); FILE MERGED 2006/09/01 17:57:38 kaib 1.6.112.1: #i68856# Added header markers and pch files

--- vcl/source/control/menubtn.cxx
+++ vcl/source/control/menubtn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: menubtn.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:18:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:54:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_RC_H
 #include <tools/rc.h>
