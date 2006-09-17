f1a792ad844531278b818c6920e4f887feb0e2ec
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:38:20 +0000

    INTEGRATION: CWS pchfix02 (1.4.10); FILE MERGED 2006/09/01 17:39:04 kaib 1.4.10.1: #i68856# Added header markers and pch files

--- shell/source/backends/kdebe/kdeinetlayer.cxx
+++ shell/source/backends/kdebe/kdeinetlayer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: kdeinetlayer.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 10:26:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:38:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef KDEINETLAYER_HXX_
 #include "kdeinetlayer.hxx"
