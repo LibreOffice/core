a28f25e64415e48c26878daf0df2fcfbb29a1b65
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:42:39 +0000

    INTEGRATION: CWS pchfix02 (1.5.60); FILE MERGED 2006/09/01 17:22:17 kaib 1.5.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/hsqldb/Hservices.cxx
+++ connectivity/source/drivers/hsqldb/Hservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Hservices.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:31:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:42:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "hsqldb/HDriver.hxx"
 #ifndef _CPPUHELPER_FACTORY_HXX_
