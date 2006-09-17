9a6f109b666bd44e5dfff491a3ac9a94cdd9e732
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:12:43 +0000

    INTEGRATION: CWS pchfix02 (1.3.10); FILE MERGED 2006/09/01 17:19:43 kaib 1.3.10.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/namedvaluecollection.cxx
+++ comphelper/source/misc/namedvaluecollection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: namedvaluecollection.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 13:58:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:12:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef COMPHELPER_NAMEDVALUECOLLECTION_HXX
 #include <comphelper/namedvaluecollection.hxx>
