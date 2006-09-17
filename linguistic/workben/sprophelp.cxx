26b059d30abb712a0504b4570a5da352a9b42a27
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:56:58 +0000

    INTEGRATION: CWS pchfix02 (1.2.46); FILE MERGED 2006/09/01 17:32:11 kaib 1.2.46.1: #i68856# Added header markers and pch files

--- linguistic/workben/sprophelp.cxx
+++ linguistic/workben/sprophelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sprophelp.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:57:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:56:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
 
 #include "misc.hxx"
 
