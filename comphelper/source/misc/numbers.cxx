ec3af338d1eaa5864d6de7252b27862b7336ec0a
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:12:55 +0000

    INTEGRATION: CWS pchfix02 (1.4.100); FILE MERGED 2006/09/01 17:19:43 kaib 1.4.100.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/numbers.cxx
+++ comphelper/source/misc/numbers.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: numbers.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:51:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:12:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef _COMPHELPER_NUMBERS_HXX_
 #include <comphelper/numbers.hxx>
