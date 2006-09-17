28cc885204d43f3a5bc072d5c22abcfcb60da481
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:34:44 +0000

    INTEGRATION: CWS pchfix02 (1.46.4); FILE MERGED 2006/09/01 17:58:05 kaib 1.46.4.1: #i68856# Added header markers and pch files

--- vcl/unx/source/app/saldata.cxx
+++ vcl/unx/source/app/saldata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saldata.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:49:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:34:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifdef USE_XTOOLKIT
 #  define SAL_XT
