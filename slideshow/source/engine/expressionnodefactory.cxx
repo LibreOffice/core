f13541430edcee0ed01a41c40200e44875aec71c
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:25:44 +0000

    INTEGRATION: CWS pchfix02 (1.3.50); FILE MERGED 2006/09/01 17:39:30 kaib 1.3.50.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/expressionnodefactory.cxx
+++ slideshow/source/engine/expressionnodefactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: expressionnodefactory.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:25:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:25:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 // must be first
 #include <canvas/debug.hxx>
