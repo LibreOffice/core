cb43269c26dbc0ab44502a2159e0528ab65d3155
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:16:07 +0000

    INTEGRATION: CWS pchfix02 (1.12.16); FILE MERGED 2006/09/01 17:30:41 kaib 1.12.16.1: #i68856# Added header markers and pch files

--- i18npool/source/characterclassification/cclass_unicode_parser.cxx
+++ i18npool/source/characterclassification/cclass_unicode_parser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cclass_unicode_parser.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:43:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:16:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <cclass_unicode.hxx>
 #include <i18nutil/unicode.hxx>
