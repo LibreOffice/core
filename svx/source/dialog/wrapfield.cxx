82673ee9dbdff37d9ce764659967a5d9bac18c9c
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:46:59 +0000

    INTEGRATION: CWS pchfix02 (1.3.536); FILE MERGED 2006/09/01 17:46:29 kaib 1.3.536.1: #i68856# Added header markers and pch files

--- svx/source/dialog/wrapfield.cxx
+++ svx/source/dialog/wrapfield.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrapfield.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:22:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:46:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_WRAPFIELD_HXX
 #include "wrapfield.hxx"
