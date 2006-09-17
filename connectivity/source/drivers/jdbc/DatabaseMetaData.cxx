eb517aaf699235b880a7af8ab7818687f5218af7
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:45:43 +0000

    INTEGRATION: CWS pchfix02 (1.22.60); FILE MERGED 2006/09/01 17:22:19 kaib 1.22.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/jdbc/DatabaseMetaData.cxx
+++ connectivity/source/drivers/jdbc/DatabaseMetaData.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DatabaseMetaData.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:34:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:45:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef _CONNECTIVITY_JAVA_SQL_DATABASEMETADATA_HXX_
 #include "java/sql/DatabaseMetaData.hxx"
 #endif
