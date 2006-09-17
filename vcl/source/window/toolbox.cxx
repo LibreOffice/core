fca5e90db9286e90771f6396eff5c4642da4ca43
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:23:06 +0000

    INTEGRATION: CWS pchfix02 (1.98.112); FILE MERGED 2006/09/01 17:57:56 kaib 1.98.112.1: #i68856# Added header markers and pch files

--- vcl/source/window/toolbox.cxx
+++ vcl/source/window/toolbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toolbox.cxx,v $
  *
- *  $Revision: 1.98 $
+ *  $Revision: 1.99 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:42:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:23:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #include <string.h>
 #include <vector>
 #include <math.h>
