43b22e8af1f89640d215749cf3983d49c7253833
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:05:36 +0000

    INTEGRATION: CWS pchfix02 (1.6.32); FILE MERGED 2006/09/01 17:16:39 kaib 1.6.32.1: #i68856# Added header markers and pch files

--- basegfx/source/tools/canvastools.cxx
+++ basegfx/source/tools/canvastools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: canvastools.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 14:00:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:05:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _COM_SUN_STAR_GEOMETRY_REALSIZE2D_HPP__
 #include <com/sun/star/geometry/RealSize2D.hpp>
