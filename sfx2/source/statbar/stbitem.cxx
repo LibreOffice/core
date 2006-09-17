00f9ee52abedab1323943b000ca4b16452b3b961
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:47:58 +0000

    INTEGRATION: CWS pchfix02 (1.12.48); FILE MERGED 2006/09/01 17:38:45 kaib 1.12.48.1: #i68856# Added header markers and pch files

--- sfx2/source/statbar/stbitem.cxx
+++ sfx2/source/statbar/stbitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stbitem.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:19:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:47:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,13 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #ifndef _SFXSTRITEM_HXX //autogen
 #include <svtools/stritem.hxx>
 #endif
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _COM_SUN_STAR_UTIL_URL_HPP_
