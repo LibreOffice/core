13d316da072c28eb22ea00c22d6f11632f6233ba
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:26:15 +0000

    INTEGRATION: CWS pchfix02 (1.13.44); FILE MERGED 2006/09/01 17:32:40 kaib 1.13.44.1: #i68856# Added header markers and pch files

--- package/source/zipapi/ByteGrabber.cxx
+++ package/source/zipapi/ByteGrabber.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ByteGrabber.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:11:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:26:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 #ifndef _BYTE_GRABBER_HXX_
 #include <ByteGrabber.hxx>
 #endif
