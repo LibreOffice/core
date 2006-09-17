8c5a974c99c5fd4c1392c01ae5596e2c5f9099b7
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:28:53 +0000

    INTEGRATION: CWS pchfix02 (1.5.74); FILE MERGED 2006/09/01 17:47:06 kaib 1.5.74.1: #i68856# Added header markers and pch files

--- svx/source/options/htmlcfg.cxx
+++ svx/source/options/htmlcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlcfg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 11:56:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:28:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "htmlcfg.hxx"
 
