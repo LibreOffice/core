f0febf4f68429287fabc7db3bff1a05b2e802807
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:21:30 +0000

    INTEGRATION: CWS pchfix02 (1.37.2); FILE MERGED 2006/09/01 17:38:26 kaib 1.37.2.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/shutdowniconw32.cxx
+++ sfx2/source/appl/shutdowniconw32.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shutdowniconw32.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 10:38:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:21:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifdef WNT
 
