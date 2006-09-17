11c646c9de00ba9ee0a78a725ae6fc19559678f0
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:53:22 +0000

    INTEGRATION: CWS pchfix02 (1.8.10); FILE MERGED 2006/09/01 17:25:22 kaib 1.8.10.1: #i68856# Added header markers and pch files

--- desktop/win32/source/officeloader/officeloader.cxx
+++ desktop/win32/source/officeloader/officeloader.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: officeloader.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 11:13:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:53:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 #define UNICODE
 #define _UNICODE
 
