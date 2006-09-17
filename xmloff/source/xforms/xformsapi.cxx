0ab4f17c85cfdf68eaec2489b658529687441e8e
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:31:54 +0000

    INTEGRATION: CWS pchfix02 (1.5.34); FILE MERGED 2006/09/01 18:00:22 kaib 1.5.34.1: #i68856# Added header markers and pch files

--- xmloff/source/xforms/xformsapi.cxx
+++ xmloff/source/xforms/xformsapi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xformsapi.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:58:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:31:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include "xformsapi.hxx"
 
