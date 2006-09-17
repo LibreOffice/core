65696dd44ffa4990d33817c303655303df88890e
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:08:28 +0000

    INTEGRATION: CWS pchfix02 (1.21.16); FILE MERGED 2006/09/01 17:40:28 kaib 1.21.16.1: #i68856# Added header markers and pch files

--- sot/source/base/formats.cxx
+++ sot/source/base/formats.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formats.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:53:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:08:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #define _SOT_FORMATS_INCLUDE_SYSTEMFORMATS
 
