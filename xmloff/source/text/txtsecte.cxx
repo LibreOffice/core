594b723fdbcc67db238d23f4ea2537be6f2ec661
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:19:41 +0000

    INTEGRATION: CWS pchfix02 (1.15.160); FILE MERGED 2006/09/01 18:00:12 kaib 1.15.160.1: #i68856# Added header markers and pch files

--- xmloff/source/text/txtsecte.cxx
+++ xmloff/source/text/txtsecte.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtsecte.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:33:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:19:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_TXTPARAE_HXX
 #include "txtparae.hxx"
