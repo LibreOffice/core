d156980e3796c62fd659ee542d365dc420ec3d36
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:26:05 +0000

    INTEGRATION: CWS pchfix02 (1.14.34); FILE MERGED 2006/09/01 17:59:32 kaib 1.14.34.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/XMLShapeStyleContext.cxx
+++ xmloff/source/draw/XMLShapeStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLShapeStyleContext.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:08:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:26:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include <tools/debug.hxx>
 
