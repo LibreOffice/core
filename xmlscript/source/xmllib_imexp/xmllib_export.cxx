fd5712bcd743374355a526c8d0752989caa6f3f3
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:39:39 +0000

    INTEGRATION: CWS pchfix02 (1.11.6); FILE MERGED 2006/09/01 18:00:34 kaib 1.11.6.1: #i68856# Added header markers and pch files

--- xmlscript/source/xmllib_imexp/xmllib_export.cxx
+++ xmlscript/source/xmllib_imexp/xmllib_export.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmllib_export.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:13:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:39:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlscript.hxx"
 
 #include <xmlscript/xmllib_imexp.hxx>
 #include <xmlscript/xml_helper.hxx>
