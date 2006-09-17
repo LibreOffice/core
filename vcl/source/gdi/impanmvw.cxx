6268382866c1e069990c9e3363f28911db20c5e3
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:02:38 +0000

    INTEGRATION: CWS pchfix02 (1.5.320); FILE MERGED 2006/09/01 17:57:43 kaib 1.5.320.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/impanmvw.cxx
+++ vcl/source/gdi/impanmvw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impanmvw.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-01 10:32:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:02:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include "impanmvw.hxx"
 #include "virdev.hxx"
