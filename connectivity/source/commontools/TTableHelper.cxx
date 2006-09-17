cd740a4a6d1a6b57e78460a34ad6ba77613d645f
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:01:37 +0000

    INTEGRATION: CWS pchfix02 (1.5.42); FILE MERGED 2006/09/01 17:21:47 kaib 1.5.42.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/TTableHelper.cxx
+++ connectivity/source/commontools/TTableHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TTableHelper.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 14:51:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:01:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_TABLEHELPER_HXX
 #include "connectivity/TTableHelper.hxx"
