b949df9bd779c7d3017d39d0cae4e28eaba80a47
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:30:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 18:00:21 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/xforms/XFormsBindContext.cxx
+++ xmloff/source/xforms/XFormsBindContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XFormsBindContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:57:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:30:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include "XFormsBindContext.hxx"
 
