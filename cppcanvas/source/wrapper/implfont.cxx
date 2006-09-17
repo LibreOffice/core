d111651afe7ff7e3c5a492b9c41479e2b6ceb034
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:52:38 +0000

    INTEGRATION: CWS pchfix02 (1.6.28); FILE MERGED 2006/09/01 17:22:51 kaib 1.6.28.1: #i68856# Added header markers and pch files

--- cppcanvas/source/wrapper/implfont.cxx
+++ cppcanvas/source/wrapper/implfont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implfont.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:28:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:52:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #include <implfont.hxx>
 #include <canvas/canvastools.hxx>
