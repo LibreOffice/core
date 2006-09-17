a76f11edeb5bb7b087392dfecf25a8faff2e76f4
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:25:07 +0000

    INTEGRATION: CWS pchfix02 (1.35.16); FILE MERGED 2006/09/01 17:56:22 kaib 1.35.16.1: #i68856# Added header markers and pch files

--- unotools/source/i18n/localedatawrapper.cxx
+++ unotools/source/i18n/localedatawrapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: localedatawrapper.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:06:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:25:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #include <cstring>      // memcpy()
 #include <cstdio>       // fprintf(), stderr
