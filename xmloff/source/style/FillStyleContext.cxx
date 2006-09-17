1cb8dffd2d513a1ffd8b0e96d47521bdee3cb452
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:43:08 +0000

    INTEGRATION: CWS pchfix02 (1.9.158); FILE MERGED 2006/09/01 17:59:46 kaib 1.9.158.1: #i68856# Added header markers and pch files

--- xmloff/source/style/FillStyleContext.cxx
+++ xmloff/source/style/FillStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FillStyleContext.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:23:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:43:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_CONTAINER_XNAMECONTAINER_HPP_
 #include <com/sun/star/container/XNameContainer.hpp>
