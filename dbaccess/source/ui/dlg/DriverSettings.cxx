438453767ffb41f71a01591e5ccaa44d3c1ebe89
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:05:44 +0000

    INTEGRATION: CWS pchfix02 (1.5.138); FILE MERGED 2006/09/01 17:24:26 kaib 1.5.138.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/DriverSettings.cxx
+++ dbaccess/source/ui/dlg/DriverSettings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DriverSettings.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2005-12-19 17:16:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:05:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_DRIVERSETTINGS_HXX
 #include "DriverSettings.hxx"
