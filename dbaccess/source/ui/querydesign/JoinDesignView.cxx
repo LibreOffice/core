546f16d71092d94fda1d7c418c1005096726af5c
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:20:32 +0000

    INTEGRATION: CWS pchfix02 (1.19.36); FILE MERGED 2006/09/01 17:24:36 kaib 1.19.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/JoinDesignView.cxx
+++ dbaccess/source/ui/querydesign/JoinDesignView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: JoinDesignView.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:40:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:20:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_JOINDESIGNVIEW_HXX
 #include "JoinDesignView.hxx"
 #endif
