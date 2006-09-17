8f822b923d76980bb78fb85dd2061f89c9847b69
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:15:37 +0000

    INTEGRATION: CWS pchfix02 (1.24.112); FILE MERGED 2006/09/01 17:57:52 kaib 1.24.112.1: #i68856# Added header markers and pch files

--- vcl/source/window/brdwin.cxx
+++ vcl/source/window/brdwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: brdwin.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:35:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:15:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #ifndef _SV_SVIDS_HRC
 #include <svids.hrc>
 #endif
