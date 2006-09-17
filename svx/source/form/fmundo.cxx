8658c8d42f72c4955e532134c4839de4f96fea4c
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:11:03 +0000

    INTEGRATION: CWS pchfix02 (1.36.2); FILE MERGED 2006/09/01 17:46:53 kaib 1.36.2.1: #i68856# Added header markers and pch files

--- svx/source/form/fmundo.cxx
+++ svx/source/form/fmundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmundo.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:01:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:11:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SVX_FMUNDO_HXX
 #include "fmundo.hxx"
