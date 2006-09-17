88b32a4a0ba8ba1d1f258b102815626c8abf8c89
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:07:41 +0000

    INTEGRATION: CWS pchfix02 (1.50.20); FILE MERGED 2006/09/01 17:55:54 kaib 1.50.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/webdav/webdavcontent.cxx
+++ ucb/source/ucp/webdav/webdavcontent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: webdavcontent.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:38:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:07:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 #define BUG_110335 1
 /**************************************************************************
                                 TODO
