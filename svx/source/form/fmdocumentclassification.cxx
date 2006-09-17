e5af463adf9c2bbf759b4aa98422076aca89450e
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:06:08 +0000

    INTEGRATION: CWS pchfix02 (1.10.536); FILE MERGED 2006/09/01 17:46:49 kaib 1.10.536.1: #i68856# Added header markers and pch files

--- svx/source/form/fmdocumentclassification.cxx
+++ svx/source/form/fmdocumentclassification.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmdocumentclassification.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:52:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:06:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_SOURCE_FORM_FMDOCUMENTCLASSIFICATION_HXX
 #include "fmdocumentclassification.hxx"
