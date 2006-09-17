59a64899faf906dd4aa105f48bcf14b12d48935e
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:29:32 +0000

    INTEGRATION: CWS pchfix02 (1.6.42); FILE MERGED 2006/09/01 17:39:34 kaib 1.6.42.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/tools.cxx
+++ slideshow/source/engine/tools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tools.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 14:04:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:29:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 #ifndef  _USE_MATH_DEFINES
 #define  _USE_MATH_DEFINES  // needed by Visual C++ for math constants
