3128c1ca140f775dabca17c59aefcd08db5f1965
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:06:39 +0000

    INTEGRATION: CWS pchfix02 (1.11.72); FILE MERGED 2006/09/01 17:46:01 kaib 1.11.72.1: #i68856# Added header markers and pch files

--- svx/source/customshapes/EnhancedCustomShapeEngine.cxx
+++ svx/source/customshapes/EnhancedCustomShapeEngine.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnhancedCustomShapeEngine.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 11:26:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:06:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _ENHANCED_CUSTOMSHAPE_ENGINE_HXX
 #include "EnhancedCustomShapeEngine.hxx"
