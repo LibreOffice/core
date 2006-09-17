93f4b6c3520e29df6cb1651ba0e8044b1d111f4b
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:11:47 +0000

    INTEGRATION: CWS pchfix02 (1.11.536); FILE MERGED 2006/09/01 17:47:38 kaib 1.11.536.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/unodtabl.cxx
+++ svx/source/unodraw/unodtabl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unodtabl.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:03:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:11:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_DRAWING_LINEDASH_HPP_
 #include <com/sun/star/drawing/LineDash.hpp>
