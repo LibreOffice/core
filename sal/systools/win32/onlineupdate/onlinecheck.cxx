4376150eb13557d05ea462ad1676c0b6e62a2724
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:07:17 +0000

    INTEGRATION: CWS pchfix02 (1.2.8); FILE MERGED 2006/09/01 17:34:20 kaib 1.2.8.1: #i68856# Added header markers and pch files

--- sal/systools/win32/onlineupdate/onlinecheck.cxx
+++ sal/systools/win32/onlineupdate/onlinecheck.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: onlinecheck.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 10:07:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:07:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 #pragma warning( disable: 4668 )
 
 #include <sal/types.h>
