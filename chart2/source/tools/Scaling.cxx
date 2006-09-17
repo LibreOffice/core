d4db56448b601e7a7ce4fd1acf86aa38661a4494
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:29:41 +0000

    INTEGRATION: CWS pchfix02 (1.5.82); FILE MERGED 2006/09/01 17:18:55 kaib 1.5.82.1: #i68856# Added header markers and pch files

--- chart2/source/tools/Scaling.cxx
+++ chart2/source/tools/Scaling.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Scaling.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:34:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:29:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #ifndef _CHART2_SCALING_HXX
 #include "Scaling.hxx"
 #endif
