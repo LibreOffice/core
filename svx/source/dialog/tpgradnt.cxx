560d9eb71bacffc3537a6fa570f9c31938a64ff6
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:44:25 +0000

    INTEGRATION: CWS pchfix02 (1.17.116); FILE MERGED 2006/09/01 17:46:27 kaib 1.17.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/tpgradnt.cxx
+++ svx/source/dialog/tpgradnt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tpgradnt.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:34:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:44:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
