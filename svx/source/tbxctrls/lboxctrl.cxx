c6771b5134da2225a2d94659294b4816995da400
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:06:09 +0000

    INTEGRATION: CWS pchfix02 (1.23.534); FILE MERGED 2006/09/01 17:47:33 kaib 1.23.534.1: #i68856# Added header markers and pch files

--- svx/source/tbxctrls/lboxctrl.cxx
+++ svx/source/tbxctrls/lboxctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lboxctrl.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:49:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:06:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef   _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
