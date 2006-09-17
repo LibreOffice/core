54b6fd396478c6a10b9b866a7b9cfd61e9f67f75
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:33:06 +0000

    INTEGRATION: CWS pchfix02 (1.9.126); FILE MERGED 2006/09/01 17:22:11 kaib 1.9.126.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/file/FNoException.cxx
+++ connectivity/source/drivers/file/FNoException.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FNoException.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2006-01-12 16:59:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:33:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef _CONNECTIVITY_FILE_CATALOG_HXX_
 #include "file/FCatalog.hxx"
 #endif
