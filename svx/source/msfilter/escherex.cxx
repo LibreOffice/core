1f6925a1ce2b54d2be874a933a0fdb1cd4be26c6
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:25:58 +0000

    INTEGRATION: CWS pchfix02 (1.63.70); FILE MERGED 2006/09/01 17:47:04 kaib 1.63.70.1: #i68856# Added header markers and pch files

--- svx/source/msfilter/escherex.cxx
+++ svx/source/msfilter/escherex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: escherex.cxx,v $
  *
- *  $Revision: 1.63 $
+ *  $Revision: 1.64 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 11:27:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:25:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #include <math.h>
 #ifndef _SVX_IMPGRF_HXX
 #include "impgrf.hxx"
