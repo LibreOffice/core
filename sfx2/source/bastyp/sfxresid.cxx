b7b39d89d6ad0d1b167681759c8b5bb1d96215ab
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:25:22 +0000

    INTEGRATION: CWS pchfix02 (1.4.312); FILE MERGED 2006/09/01 17:38:28 kaib 1.4.312.1: #i68856# Added header markers and pch files

--- sfx2/source/bastyp/sfxresid.cxx
+++ sfx2/source/bastyp/sfxresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxresid.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:00:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:25:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _TOOLS_SIMPLERESMGR_HXX_
