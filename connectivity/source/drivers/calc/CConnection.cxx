f154fd324c06b54b54041dbf3ab66bb18e0cee38
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:18:36 +0000

    INTEGRATION: CWS pchfix02 (1.12.60); FILE MERGED 2006/09/01 17:22:00 kaib 1.12.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/calc/CConnection.cxx
+++ connectivity/source/drivers/calc/CConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CConnection.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:17:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:18:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_CALC_CONNECTION_HXX_
 #include "calc/CConnection.hxx"
