be7261531e279e8a80447316eb28b851eab0ccba
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:43:53 +0000

    INTEGRATION: CWS pchfix02 (1.5.378); FILE MERGED 2006/09/01 17:57:30 kaib 1.5.378.1: #i68856# Added header markers and pch files

--- vcl/aqua/source/gdi/salcolorutils.cxx
+++ vcl/aqua/source/gdi/salcolorutils.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salcolorutils.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:41:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:43:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_SALCOLORUTILS_HXX
     #include <salcolorutils.hxx>
