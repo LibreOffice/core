6dc789fb325c9b3ec5e3700e4d08211612590b53
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:49:40 +0000

    INTEGRATION: CWS pchfix02 (1.9.158); FILE MERGED 2006/09/01 17:27:17 kaib 1.9.158.1: #i68856# Added header markers and pch files

--- filter/source/xsltfilter/XSLTFilter.cxx
+++ filter/source/xsltfilter/XSLTFilter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XSLTFilter.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2005-10-24 15:57:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:49:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #include <stdio.h>
 
