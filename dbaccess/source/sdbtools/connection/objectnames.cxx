e550be6fd5117f76376112544fa6c35872fc0e4d
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:49:59 +0000

    INTEGRATION: CWS pchfix02 (1.3.24); FILE MERGED 2006/09/01 17:24:16 kaib 1.3.24.1: #i68856# Added header markers and pch files

--- dbaccess/source/sdbtools/connection/objectnames.cxx
+++ dbaccess/source/sdbtools/connection/objectnames.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objectnames.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:47:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:49:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_SOURCE_SDBTOOLS_CONNECTION_OBJECTNAMES_HXX
 #include "objectnames.hxx"
