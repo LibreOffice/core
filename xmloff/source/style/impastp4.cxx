e852fa8d18df7ffe30387a9bea4a147c9132b76b
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:55:04 +0000

    INTEGRATION: CWS pchfix02 (1.18.22); FILE MERGED 2006/09/01 17:59:54 kaib 1.18.22.1: #i68856# Added header markers and pch files

--- xmloff/source/style/impastp4.cxx
+++ xmloff/source/style/impastp4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impastp4.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-06 14:23:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:55:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
