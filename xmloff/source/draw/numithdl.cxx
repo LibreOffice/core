f6b2fa49f95299fe42698d8a5407b7690ea99163
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:27:53 +0000

    INTEGRATION: CWS pchfix02 (1.3.34); FILE MERGED 2006/09/01 17:59:33 kaib 1.3.34.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/numithdl.cxx
+++ xmloff/source/draw/numithdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: numithdl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:10:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:27:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PROPERTYHANDLER_NUMRULE_HXX
 #include <numithdl.hxx>
