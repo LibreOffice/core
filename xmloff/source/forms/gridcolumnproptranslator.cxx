1d4666a36ef0bdfc1cc25f46cb717b9110595653
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:36:43 +0000

    INTEGRATION: CWS pchfix02 (1.5.18); FILE MERGED 2006/09/01 17:59:41 kaib 1.5.18.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/gridcolumnproptranslator.cxx
+++ xmloff/source/forms/gridcolumnproptranslator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gridcolumnproptranslator.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:35:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:36:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef XMLOFF_SOURCE_FORMS_GRIDCOLUMNPROPTRANSLATOR_HXX
 #include "gridcolumnproptranslator.hxx"
