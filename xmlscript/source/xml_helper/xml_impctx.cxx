ad20b044b16cc1b190ff02e93b278af75adf6980
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:37:38 +0000

    INTEGRATION: CWS pchfix02 (1.10.6); FILE MERGED 2006/09/01 18:00:32 kaib 1.10.6.1: #i68856# Added header markers and pch files

--- xmlscript/source/xml_helper/xml_impctx.cxx
+++ xmlscript/source/xml_helper/xml_impctx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xml_impctx.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:11:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:37:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlscript.hxx"
 
 #include "osl/diagnose.h"
 #include "osl/mutex.hxx"
