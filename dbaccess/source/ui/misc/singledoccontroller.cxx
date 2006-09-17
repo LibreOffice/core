543c733baf8e295ce269d41ed61be2a87cb6c805
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:18:38 +0000

    INTEGRATION: CWS pchfix02 (1.18.36); FILE MERGED 2006/09/01 17:24:34 kaib 1.18.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/singledoccontroller.cxx
+++ dbaccess/source/ui/misc/singledoccontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: singledoccontroller.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:39:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:18:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_SINGLEDOCCONTROLLER_HXX
 #include "singledoccontroller.hxx"
