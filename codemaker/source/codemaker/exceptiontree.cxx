7f5e8ff5c3b34bfc210389cf4c9d3f601319a421
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:34:34 +0000

    INTEGRATION: CWS pchfix02 (1.6.8); FILE MERGED 2006/09/01 17:19:20 kaib 1.6.8.1: #i68856# Added header markers and pch files

--- codemaker/source/codemaker/exceptiontree.cxx
+++ codemaker/source/codemaker/exceptiontree.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: exceptiontree.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:23:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:34:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_codemaker.hxx"
 
 #include "codemaker/exceptiontree.hxx"
 #include "codemaker/typemanager.hxx"
