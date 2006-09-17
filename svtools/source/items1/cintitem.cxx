49ef3ec6dffb4caba3fd8ead63bdfc73c1e99b26
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:02:59 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:14 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/items1/cintitem.cxx
+++ svtools/source/items1/cintitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cintitem.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:12:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:02:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
