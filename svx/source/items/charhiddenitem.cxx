c08240a437889618f0c2ee4bc1f3db8c9e7ea1a8
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:18:20 +0000

    INTEGRATION: CWS pchfix02 (1.4.116); FILE MERGED 2006/09/01 17:46:58 kaib 1.4.116.1: #i68856# Added header markers and pch files

--- svx/source/items/charhiddenitem.cxx
+++ svx/source/items/charhiddenitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: charhiddenitem.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:10:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:18:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define ITEMID_CHARHIDDEN            0
 
