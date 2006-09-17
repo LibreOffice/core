880ae968f4b18cdf0e02b591f46d6d83706d9911
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:13:00 +0000

    INTEGRATION: CWS pchfix02 (1.3.8); FILE MERGED 2006/09/01 17:31:13 kaib 1.3.8.1: #i68856# Added header markers and pch files

--- idlc/source/aststack.cxx
+++ idlc/source/aststack.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: aststack.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:47:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:13:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 #ifndef _RTL_ALLOC_H_
 #include <rtl/alloc.h>
 #endif
