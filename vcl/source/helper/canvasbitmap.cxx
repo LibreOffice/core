cc412af7109a0e5d0174858f7cbb9df9b4c79828
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:13:34 +0000

    INTEGRATION: CWS pchfix02 (1.6.112); FILE MERGED 2006/09/01 17:57:50 kaib 1.6.112.1: #i68856# Added header markers and pch files

--- vcl/source/helper/canvasbitmap.cxx
+++ vcl/source/helper/canvasbitmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: canvasbitmap.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:33:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:13:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #ifndef _COM_SUN_STAR_RENDERING_ENDIANNESS_HPP_
 #include <com/sun/star/rendering/Endianness.hpp>
 #endif
