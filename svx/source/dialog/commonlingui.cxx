16900dfd316838ebdc3b2518d8689838de5e29d8
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:12:07 +0000

    INTEGRATION: CWS pchfix02 (1.6.116); FILE MERGED 2006/09/01 17:46:05 kaib 1.6.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/commonlingui.cxx
+++ svx/source/dialog/commonlingui.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: commonlingui.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:02:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:12:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef SVX_COMMON_LINGUI_HXX
 #include "commonlingui.hxx"
 #endif
