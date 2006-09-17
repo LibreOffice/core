808acb258b236323dbb65208896e49e050d1d4d5
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:51:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.22); FILE MERGED 2006/09/01 17:22:23 kaib 1.4.22.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/kab/KDEInit.cxx
+++ connectivity/source/drivers/kab/KDEInit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: KDEInit.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 10:23:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:51:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_KAB_KDEINIT_H
 #include "KDEInit.h"
