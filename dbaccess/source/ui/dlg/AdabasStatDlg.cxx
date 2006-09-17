36a66d74d2b048b4cfd48f0a21e425a7c09398eb
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:03:54 +0000

    INTEGRATION: CWS pchfix02 (1.9.88); FILE MERGED 2006/09/01 17:24:25 kaib 1.9.88.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/AdabasStatDlg.cxx
+++ dbaccess/source/ui/dlg/AdabasStatDlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AdabasStatDlg.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-29 12:37:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:03:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_ADABASSTATDLG_HXX
 #include "AdabasStatDlg.hxx"
