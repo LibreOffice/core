eaf8e3b2ab61af168b63c01179a472999f6583bb
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:06:35 +0000

    INTEGRATION: CWS pchfix02 (1.5.70); FILE MERGED 2006/09/01 17:43:16 kaib 1.5.70.1: #i68856# Added header markers and pch files

--- svtools/source/items1/poolitem.cxx
+++ svtools/source/items1/poolitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: poolitem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-05 21:39:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:06:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include "poolitem.hxx"
 
