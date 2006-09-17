a3f128abaa1cf9966f2fd04637377844fa937ab6
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:09:24 +0000

    INTEGRATION: CWS pchfix02 (1.49.16); FILE MERGED 2006/09/01 17:22:35 kaib 1.49.16.1: #i68856# Added header markers and pch files

--- connectivity/source/parse/sqliterator.cxx
+++ connectivity/source/parse/sqliterator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sqliterator.cxx,v $
  *
- *  $Revision: 1.51 $
+ *  $Revision: 1.52 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 14:54:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:09:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_PARSE_SQLITERATOR_HXX_
 #include "connectivity/sqliterator.hxx"
