daeda398099e9247bcf4d1a95bb846d7d897c677
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:17:21 +0000

    INTEGRATION: CWS pchfix02 (1.26.116); FILE MERGED 2006/09/01 17:47:42 kaib 1.26.116.1: #i68856# Added header markers and pch files

--- svx/source/unoedit/unofield.cxx
+++ svx/source/unoedit/unofield.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unofield.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:01:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:17:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef _COM_SUN_STAR_UTIL_DATETIME_HPP_
 #include <com/sun/star/util/DateTime.hpp>
 #endif
