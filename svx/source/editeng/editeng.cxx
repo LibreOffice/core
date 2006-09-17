d6961348f15017e347ba3a5252a7432cfd0da58e
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:49:05 +0000

    INTEGRATION: CWS pchfix02 (1.101.112); FILE MERGED 2006/09/01 17:46:37 kaib 1.101.112.1: #i68856# Added header markers and pch files

--- svx/source/editeng/editeng.cxx
+++ svx/source/editeng/editeng.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editeng.cxx,v $
  *
- *  $Revision: 1.101 $
+ *  $Revision: 1.102 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:39:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:49:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #include <eeng_pch.hxx>
 
 // #define _SOLAR__PRIVATE 1
