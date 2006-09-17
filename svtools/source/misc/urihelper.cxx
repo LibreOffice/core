929093e456fe88d0fbbc83a7380fe7b7e7fb1988
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:14:03 +0000

    INTEGRATION: CWS pchfix02 (1.18.92); FILE MERGED 2006/09/01 17:43:22 kaib 1.18.92.1: #i68856# Added header markers and pch files

--- svtools/source/misc/urihelper.cxx
+++ svtools/source/misc/urihelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: urihelper.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:22:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:14:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef SVTOOLS_URIHELPER_HXX
 #include <urihelper.hxx>
