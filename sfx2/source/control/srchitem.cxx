533ba5c2e025732a00754a28ded4539ba1c57511
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:29:18 +0000

    INTEGRATION: CWS pchfix02 (1.17.48); FILE MERGED 2006/09/01 17:38:31 kaib 1.17.48.1: #i68856# Added header markers and pch files

--- sfx2/source/control/srchitem.cxx
+++ sfx2/source/control/srchitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srchitem.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:17:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:29:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 // include ---------------------------------------------------------------
 
@@ -67,7 +70,6 @@
 #endif
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #define _SVX_SRCHITEM_CXX
