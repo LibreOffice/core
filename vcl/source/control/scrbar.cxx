5733ec0044300814edc1ba15faa82c0936c7d0a8
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:54:58 +0000

    INTEGRATION: CWS pchfix02 (1.18.112); FILE MERGED 2006/09/01 17:57:38 kaib 1.18.112.1: #i68856# Added header markers and pch files

--- vcl/source/control/scrbar.cxx
+++ vcl/source/control/scrbar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scrbar.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:18:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:54:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_EVENT_HXX
 #include <event.hxx>
