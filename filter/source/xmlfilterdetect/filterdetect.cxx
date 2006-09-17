e8e9652b83efd087e23375be8dcdb1641d5771dd
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:47:37 +0000

    INTEGRATION: CWS pchfix02 (1.5.194); FILE MERGED 2006/09/01 17:27:15 kaib 1.5.194.1: #i68856# Added header markers and pch files

--- filter/source/xmlfilterdetect/filterdetect.cxx
+++ filter/source/xmlfilterdetect/filterdetect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filterdetect.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:59:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:46:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 #include <iostream.h>
 #include <stdlib.h>
 #include <ctype.h>
--- filter/source/xsltdialog/typedetectionimport.cxx
+++ filter/source/xsltdialog/typedetectionimport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typedetectionimport.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:15:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:47:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #ifndef _COM_SUN_STAR_XML_SAX_INPUTSOURCE_HPP_
 #include <com/sun/star/xml/sax/InputSource.hpp>
