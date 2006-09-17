3797d2fc1609ee0f8dbe467517d66698fed4a1ce
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:59:44 +0000

    INTEGRATION: CWS pchfix02 (1.5.60); FILE MERGED 2006/09/01 17:22:28 kaib 1.5.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/mozab/bootstrap/MNSProfile.cxx
+++ connectivity/source/drivers/mozab/bootstrap/MNSProfile.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MNSProfile.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 10:57:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:59:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "MNSProfile.hxx"
 #include "MNSProfileDirServiceProvider.hxx"
