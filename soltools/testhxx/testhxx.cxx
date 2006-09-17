9ad2040e972cec58da0fe33d3a7b8fe79766aa94
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:35:56 +0000

    INTEGRATION: CWS pchfix02 (1.2.10); FILE MERGED 2006/09/01 17:40:19 kaib 1.2.10.1: #i68856# Added header markers and pch files

--- soltools/testhxx/testhxx.cxx
+++ soltools/testhxx/testhxx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testhxx.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:10:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:35:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,5 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_soltools.hxx"
 
 // This is just a dummy file; see the makefile.mk for the real work.
