88ac9d854f18906b1b6647b84768371e5b0cd06c
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:00:21 +0000

    INTEGRATION: CWS pchfix02 (1.4.36); FILE MERGED 2006/09/01 17:16:35 kaib 1.4.36.1: #i68856# Added header markers and pch files

--- basegfx/source/point/b3dhompoint.cxx
+++ basegfx/source/point/b3dhompoint.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dhompoint.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:44:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:00:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_POINT_B3DHOMPOINT_HXX
 #include <basegfx/point/b3dhompoint.hxx>
