bc328036f9c918ca3755e01a6cb8bb29994ff9ec
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:04:58 +0000

    INTEGRATION: CWS pchfix02 (1.21.50); FILE MERGED 2006/09/01 17:46:00 kaib 1.21.50.1: #i68856# Added header markers and pch files

--- svx/source/accessibility/GraphCtlAccessibleContext.cxx
+++ svx/source/accessibility/GraphCtlAccessibleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GraphCtlAccessibleContext.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:48:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:04:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
