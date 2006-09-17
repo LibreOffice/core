08b8078cdd2a3f7772ddc067737fcd3b3112506d
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:55:49 +0000

    INTEGRATION: CWS pchfix02 (1.3.46); FILE MERGED 2006/09/01 17:32:10 kaib 1.3.46.1: #i68856# Added header markers and pch files

--- linguistic/source/staticmb.cxx
+++ linguistic/source/staticmb.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: staticmb.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:55:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:55:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,3 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
