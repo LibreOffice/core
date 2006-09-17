7cb7bc6988384be013c373e2e4d48cd019bd7581
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:47:17 +0000

    INTEGRATION: CWS pchfix02 (1.22.42); FILE MERGED 2006/09/01 17:57:33 kaib 1.22.42.1: #i68856# Added header markers and pch files

--- vcl/source/app/dbggui.cxx
+++ vcl/source/app/dbggui.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbggui.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:50:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:47:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 
 
