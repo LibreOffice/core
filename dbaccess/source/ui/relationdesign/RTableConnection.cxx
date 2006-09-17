65808f1e500bbe8650225677d5498c82d4d87984
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:27:38 +0000

    INTEGRATION: CWS pchfix02 (1.5.52); FILE MERGED 2006/09/01 17:24:40 kaib 1.5.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/relationdesign/RTableConnection.cxx
+++ dbaccess/source/ui/relationdesign/RTableConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RTableConnection.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:30:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:27:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_RTABLECONNECTION_HXX
 #include "RTableConnection.hxx"
 #endif
