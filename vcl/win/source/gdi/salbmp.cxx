730cdaaf462f8c4066a08b0b54def6e2c43af8b7
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:44:37 +0000

    INTEGRATION: CWS pchfix02 (1.8.76); FILE MERGED 2006/09/01 17:58:13 kaib 1.8.76.1: #i68856# Added header markers and pch files

--- vcl/win/source/gdi/salbmp.cxx
+++ vcl/win/source/gdi/salbmp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salbmp.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:20:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:44:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SVWIN_H
 #include <tools/svwin.h>
