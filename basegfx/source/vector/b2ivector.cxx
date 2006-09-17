a62b022ad3e2be868f4276d5d146951edd6c9844
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:08:11 +0000

    INTEGRATION: CWS pchfix02 (1.5.36); FILE MERGED 2006/09/01 17:16:40 kaib 1.5.36.1: #i68856# Added header markers and pch files

--- basegfx/source/vector/b2ivector.cxx
+++ basegfx/source/vector/b2ivector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2ivector.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:54:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:08:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_VECTOR_B2IVECTOR_HXX
 #include <basegfx/vector/b2ivector.hxx>
