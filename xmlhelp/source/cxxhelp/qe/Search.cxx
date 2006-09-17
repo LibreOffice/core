4f25ea43d850765e9e35afac2bef3bcd7fa6f3f8
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:20:00 +0000

    INTEGRATION: CWS pchfix02 (1.8.8); FILE MERGED 2006/09/01 17:58:34 kaib 1.8.8.1: #i68856# Added header markers and pch files

--- xmlhelp/source/cxxhelp/qe/Search.cxx
+++ xmlhelp/source/cxxhelp/qe/Search.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Search.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:41:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:20:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlhelp.hxx"
 #ifndef _XMLSEARCH_QE_SEARCH_HXX_
 #include <qe/Search.hxx>
 #endif
