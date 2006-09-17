909fc3f4b4afced413ff9ae4ec039b917909aa87
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:26:20 +0000

    INTEGRATION: CWS pchfix02 (1.7.34); FILE MERGED 2006/09/01 17:59:32 kaib 1.7.34.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/animationexport.cxx
+++ xmloff/source/draw/animationexport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: animationexport.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 10:59:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:26:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_ANIMATIONS_XAnimateColor_HPP_
 #include <com/sun/star/animations/XAnimateColor.hpp>
