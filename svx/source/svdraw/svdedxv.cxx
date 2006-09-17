9c8835f6dbd5851d078593396cc106cc7243012b
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:49:29 +0000

    INTEGRATION: CWS pchfix02 (1.50.114); FILE MERGED 2006/09/01 17:47:20 kaib 1.50.114.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdedxv.cxx
+++ svx/source/svdraw/svdedxv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdedxv.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:36:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:49:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef INCLUDED_SVTOOLS_ACCESSIBILITYOPTIONS_HXX
 #include <svtools/accessibilityoptions.hxx>
 #endif
