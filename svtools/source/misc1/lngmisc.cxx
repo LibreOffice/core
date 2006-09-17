05d2e7fca3ac81e18645abf13dfabc3d20c6a8c9
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:16:38 +0000

    INTEGRATION: CWS pchfix02 (1.4.92); FILE MERGED 2006/09/01 17:43:23 kaib 1.4.92.1: #i68856# Added header markers and pch files

--- svtools/source/misc1/lngmisc.cxx
+++ svtools/source/misc1/lngmisc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lngmisc.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:22:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:16:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _SVTOOLS_LNGMISC_HXX_
 #include <lngmisc.hxx>
