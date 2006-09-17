77b19c0ea24328f08fa23f07e943db71993f2f6f
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:03:26 +0000

    INTEGRATION: CWS pchfix02 (1.4.92); FILE MERGED 2006/09/01 17:43:14 kaib 1.4.92.1: #i68856# Added header markers and pch files

--- svtools/source/items1/cenumitm.cxx
+++ svtools/source/items1/cenumitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cenumitm.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:12:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:02:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
--- svtools/source/items1/cstitem.cxx
+++ svtools/source/items1/cstitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cstitem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:12:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:03:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
