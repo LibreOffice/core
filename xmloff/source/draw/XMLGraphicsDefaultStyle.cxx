423e9000e5d747cd17c24bfe6dc46a5b89e93b5f
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:24:42 +0000

    INTEGRATION: CWS pchfix02 (1.4.158); FILE MERGED 2006/09/01 17:59:31 kaib 1.4.158.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/XMLGraphicsDefaultStyle.cxx
+++ xmloff/source/draw/XMLGraphicsDefaultStyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLGraphicsDefaultStyle.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:39:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:24:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
