fc33605b554fe3fd001c98ddd1420842ca7843a3
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:04:21 +0000

    INTEGRATION: CWS pchfix02 (1.6.36); FILE MERGED 2006/09/01 17:24:25 kaib 1.6.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/CollectionView.cxx
+++ dbaccess/source/ui/dlg/CollectionView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CollectionView.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:25:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:04:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_COLLECTIONVIEW_HXX
 #include "CollectionView.hxx"
 #endif
