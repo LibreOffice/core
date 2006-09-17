38c97d54069c6e89c11b8fc2aabdb25b1e69782d
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:10:11 +0000

    INTEGRATION: CWS pchfix02 (1.48.50); FILE MERGED 2006/09/01 17:24:29 kaib 1.48.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/generalpage.cxx
+++ dbaccess/source/ui/dlg/generalpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: generalpage.cxx,v $
  *
- *  $Revision: 1.48 $
+ *  $Revision: 1.49 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:07:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:10:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_GENERALPAGE_HXX_
 #include "generalpage.hxx"
