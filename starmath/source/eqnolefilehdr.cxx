284ca15ef6dd50e1443a77d3f73cd271cd3c9db4
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:52:51 +0000

    INTEGRATION: CWS pchfix02 (1.3.120); FILE MERGED 2006/09/01 17:40:43 kaib 1.3.120.1: #i68856# Added header markers and pch files

--- starmath/source/eqnolefilehdr.cxx
+++ starmath/source/eqnolefilehdr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eqnolefilehdr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 15:07:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:52:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_starmath.hxx"
 
 #ifndef __EQNOLEFILEHDR_HXX__
 #include "eqnolefilehdr.hxx"
