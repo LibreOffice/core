7e866dbd74b859013ed3a2475565fbd91f5c0153
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:42:02 +0000

    INTEGRATION: CWS pchfix02 (1.2.36); FILE MERGED 2006/09/01 17:24:10 kaib 1.2.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/misc/ContainerListener.cxx
+++ dbaccess/source/core/misc/ContainerListener.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ContainerListener.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:16:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:41:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBA_CONTAINERLISTENER_HXX
 #include "ContainerListener.hxx"
