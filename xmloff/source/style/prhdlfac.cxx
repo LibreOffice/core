a3feec34b13eb6e6eed54c2701af99075a280058
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:56:24 +0000

    INTEGRATION: CWS pchfix02 (1.25.160); FILE MERGED 2006/09/01 17:59:55 kaib 1.25.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/prhdlfac.cxx
+++ xmloff/source/style/prhdlfac.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prhdlfac.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:46:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:56:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_DRAWING_COLORMODE_HPP_
 #include <com/sun/star/drawing/ColorMode.hpp>
