a342b11f9f8a026355fd99910fc6e1b26f09a41a
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:27:21 +0000

    INTEGRATION: CWS pchfix02 (1.7.44); FILE MERGED 2006/09/01 17:32:41 kaib 1.7.44.1: #i68856# Added header markers and pch files

--- package/source/zipapi/XFileStream.cxx
+++ package/source/zipapi/XFileStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XFileStream.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:13:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:27:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 #ifndef _XFILE_STREAM_HXX
 #include <XFileStream.hxx>
 #endif
