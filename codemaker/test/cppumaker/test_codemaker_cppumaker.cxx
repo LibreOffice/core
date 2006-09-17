92df4a0d0c129527c18a013ef673443a7a9a2ee3
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:40:19 +0000

    INTEGRATION: CWS pchfix02 (1.9.6); FILE MERGED 2006/09/01 17:19:25 kaib 1.9.6.1: #i68856# Added header markers and pch files

--- codemaker/test/cppumaker/test_codemaker_cppumaker.cxx
+++ codemaker/test/cppumaker/test_codemaker_cppumaker.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: test_codemaker_cppumaker.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-20 16:07:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:40:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_codemaker.hxx"
 
 #include "FILE.hpp"
 #include "lconv.hpp"
