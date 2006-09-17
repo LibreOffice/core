db38f00db2eeb75c65885ea03fb29b9c90c4108d
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:29:17 +0000

    INTEGRATION: CWS pchfix02 (1.52.16); FILE MERGED 2006/09/01 17:59:35 kaib 1.52.16.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/shapeexport2.cxx
+++ xmloff/source/draw/shapeexport2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shapeexport2.cxx,v $
  *
- *  $Revision: 1.52 $
+ *  $Revision: 1.53 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:35:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:29:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef __COMPHELPER_UNOINTERFACETOUNIQUEIDENTIFIERMAPPER__
 #include "unointerfacetouniqueidentifiermapper.hxx"
