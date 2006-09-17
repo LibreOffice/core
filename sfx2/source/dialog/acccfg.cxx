4e0a8c9fa593e0e6a4d66f8c98d7f00f1dd89539
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:30:18 +0000

    INTEGRATION: CWS pchfix02 (1.32.2); FILE MERGED 2006/09/01 17:38:32 kaib 1.32.2.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/acccfg.cxx
+++ sfx2/source/dialog/acccfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acccfg.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:17:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:30:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 //-----------------------------------------------
 // include own files
