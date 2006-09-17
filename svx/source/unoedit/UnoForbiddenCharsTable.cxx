e742912f719ac9b1ec84eb78d6cc8e62c44c6183
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:16:20 +0000

    INTEGRATION: CWS pchfix02 (1.4.536); FILE MERGED 2006/09/01 17:47:41 kaib 1.4.536.1: #i68856# Added header markers and pch files

--- svx/source/unoedit/UnoForbiddenCharsTable.cxx
+++ svx/source/unoedit/UnoForbiddenCharsTable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UnoForbiddenCharsTable.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:09:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:16:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SVX_UNOFORBIDDENCHARSTABLE_HXX_
 #include "UnoForbiddenCharsTable.hxx"
