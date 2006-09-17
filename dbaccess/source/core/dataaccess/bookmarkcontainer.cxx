f9ac99d3e667254b62fa5f0235b77addd83aedbc
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:38:17 +0000

    INTEGRATION: CWS pchfix02 (1.12.52); FILE MERGED 2006/09/01 17:24:08 kaib 1.12.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/bookmarkcontainer.cxx
+++ dbaccess/source/core/dataaccess/bookmarkcontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bookmarkcontainer.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:43:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:38:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_CORE_BOOKMARKCONTAINER_HXX_
 #include "bookmarkcontainer.hxx"
