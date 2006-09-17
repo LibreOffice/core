f4a24e48264b5d322015be17fab7a755fb443832
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:14:40 +0000

    INTEGRATION: CWS pchfix02 (1.4.40); FILE MERGED 2006/09/01 17:58:30 kaib 1.4.40.1: #i68856# Added header markers and pch files

--- xmlhelp/source/cxxhelp/db/Block.cxx
+++ xmlhelp/source/cxxhelp/db/Block.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Block.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:16:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:14:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlhelp.hxx"
 #ifndef _RTL_MEMORY_H_
 #include <rtl/memory.h>
 #endif
