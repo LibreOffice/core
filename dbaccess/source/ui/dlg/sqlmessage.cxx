1f1ee1d4dac6e6f908099e124fb5f22c4cb260ff
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:11:47 +0000

    INTEGRATION: CWS pchfix02 (1.22.36); FILE MERGED 2006/09/01 17:24:30 kaib 1.22.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/sqlmessage.cxx
+++ dbaccess/source/ui/dlg/sqlmessage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sqlmessage.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:27:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:11:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_SQLMESSAGE_HXX_
 #include "sqlmessage.hxx"
