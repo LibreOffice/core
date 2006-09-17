a150f98e484cff7e1063dbc19512dfd68325c2db
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:56:32 +0000

    INTEGRATION: CWS pchfix02 (1.3.80); FILE MERGED 2006/09/01 17:18:33 kaib 1.3.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/chartapiwrapper/LegendWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/LegendWrapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LegendWrapper.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:01:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:56:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "LegendWrapper.hxx"
 #include "macros.hxx"
 #include "InlineContainer.hxx"
