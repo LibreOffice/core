26c90a905196355c6a230f46db5826168264f875
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:46:30 +0000

    INTEGRATION: CWS pchfix02 (1.15.92); FILE MERGED 2006/09/01 17:43:01 kaib 1.15.92.1: #i68856# Added header markers and pch files

--- svtools/source/dialogs/wizardmachine.cxx
+++ svtools/source/dialogs/wizardmachine.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wizardmachine.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 14:55:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:46:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _SVTOOLS_WIZARDMACHINE_HXX_
 #include "wizardmachine.hxx"
