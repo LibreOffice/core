62e96ad1874ef1c900a4972e57c36eb1cfd815ba
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:32:28 +0000

    INTEGRATION: CWS pchfix02 (1.4.32); FILE MERGED 2006/09/01 17:58:22 kaib 1.4.32.1: #i68856# Added header markers and pch files

--- xmerge/java/org/openoffice/xmerge/xmergebridge/FlatXml/cpp/FlatXml.cxx
+++ xmerge/java/org/openoffice/xmerge/xmergebridge/FlatXml/cpp/FlatXml.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FlatXml.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:46:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:32:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmerge.hxx"
 #include <stdio.h>
 #include <osl/mutex.hxx>
 #include <osl/thread.h>
