d7be5e85d684a3334f1f3be545d10032f7d62db7
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:07:48 +0000

    INTEGRATION: CWS pchfix02 (1.3.80); FILE MERGED 2006/09/01 17:18:41 kaib 1.3.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/main/_serviceregistration_controller.cxx
+++ chart2/source/controller/main/_serviceregistration_controller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _serviceregistration_controller.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:36:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:07:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #ifndef _CPPUHELPER_IMPLEMENATIONENTRY_HXX_
 #include <cppuhelper/implementationentry.hxx>
 #endif
