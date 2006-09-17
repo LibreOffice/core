d3a6f4eace92deb8e339cf7c6927aa98782b1695
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:23:31 +0000

    INTEGRATION: CWS pchfix02 (1.2.160); FILE MERGED 2006/09/01 17:59:29 kaib 1.2.160.1: #i68856# Added header markers and pch files

--- xmloff/source/core/xmlkywd.cxx
+++ xmloff/source/core/xmlkywd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlkywd.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:38:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:23:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLKYWD_HXX
 #define XML_DEFINE_KEYWORDS
