348e309c3385068bd2a1185554154d537167bede
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:10:55 +0000

    INTEGRATION: CWS pchfix02 (1.2.140); FILE MERGED 2006/09/01 17:34:22 kaib 1.2.140.1: #i68856# Added header markers and pch files

--- sal/workben/testpipe.cxx
+++ sal/workben/testpipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testpipe.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:55:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:10:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 
 #include <stdio.h>
