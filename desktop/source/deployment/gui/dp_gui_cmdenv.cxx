311445eda11eefc967feb0f0290992b3a6a165ad
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:38:35 +0000

    INTEGRATION: CWS pchfix02 (1.7.26); FILE MERGED 2006/09/01 17:25:09 kaib 1.7.26.1: #i68856# Added header markers and pch files

--- desktop/source/deployment/gui/dp_gui_cmdenv.cxx
+++ desktop/source/deployment/gui/dp_gui_cmdenv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dp_gui_cmdenv.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 17:02:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:38:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "dp_gui.hrc"
 #include "dp_gui.h"
