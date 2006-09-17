8c1314844ceaf4bc4cb60cf5d67cbd672f9d434c
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:14:32 +0000

    INTEGRATION: CWS pchfix02 (1.10.42); FILE MERGED 2006/09/01 17:22:39 kaib 1.10.42.1: #i68856# Added header markers and pch files

--- connectivity/source/simpledbt/staticdbtools_s.cxx
+++ connectivity/source/simpledbt/staticdbtools_s.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: staticdbtools_s.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:39:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:14:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_VIRTUAL_DBTOOLS_HXX
 #include <connectivity/virtualdbtools.hxx>
