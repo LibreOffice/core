2d89bb1afba2681616234c750ef0e751fc3c0edb
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:37:36 +0000

    INTEGRATION: CWS pchfix02 (1.7.36); FILE MERGED 2006/09/01 17:24:07 kaib 1.7.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/ContentHelper.cxx
+++ dbaccess/source/core/dataaccess/ContentHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ContentHelper.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:08:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:37:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBA_CONTENTHELPER_HXX
 #include "ContentHelper.hxx"
 #endif
