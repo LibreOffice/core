647efc7ee09c809174da46bc4a330ae9ce3fd850
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:03:13 +0000

    INTEGRATION: CWS pchfix02 (1.17.40); FILE MERGED 2006/09/01 17:21:48 kaib 1.17.40.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/dbexception.cxx
+++ connectivity/source/commontools/dbexception.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbexception.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:19:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:02:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
