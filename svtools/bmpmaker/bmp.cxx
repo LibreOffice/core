20233b69c3391ce54e30e07f1329e7800f59912e
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:11:28 +0000

    INTEGRATION: CWS pchfix02 (1.15.326); FILE MERGED 2006/09/01 17:42:37 kaib 1.15.326.1: #i68856# Added header markers and pch files

--- svtools/bmpmaker/bmp.cxx
+++ svtools/bmpmaker/bmp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bmp.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:52:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:11:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include <stdio.h>
 #include <signal.h>
