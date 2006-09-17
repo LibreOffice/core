859b049f0baa904fd55df3f1cb0eeab71e3b8b17
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:20:57 +0000

    INTEGRATION: CWS pchfix02 (1.6.92); FILE MERGED 2006/09/01 17:42:44 kaib 1.6.92.1: #i68856# Added header markers and pch files

--- svtools/source/config/apearcfg.cxx
+++ svtools/source/config/apearcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: apearcfg.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:41:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:20:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifdef  SVL_DLLIMPLEMENTATION
 #undef  SVL_DLLIMPLEMENTATION
