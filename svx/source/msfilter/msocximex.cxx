7c92564fd32dad4afc772507cd620a0095154900
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:27:44 +0000

    INTEGRATION: CWS pchfix02 (1.27.74); FILE MERGED 2006/09/01 17:47:05 kaib 1.27.74.1: #i68856# Added header markers and pch files

--- svx/source/msfilter/msocximex.cxx
+++ svx/source/msfilter/msocximex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: msocximex.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:05:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:27:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
