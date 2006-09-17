e54c0a5efe84361b81e80d320ffb48a8011d961a
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:48:13 +0000

    INTEGRATION: CWS pchfix02 (1.45.46); FILE MERGED 2006/09/01 17:43:03 kaib 1.45.46.1: #i68856# Added header markers and pch files

--- svtools/source/edit/texteng.cxx
+++ svtools/source/edit/texteng.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: texteng.cxx,v $
  *
- *  $Revision: 1.45 $
+ *  $Revision: 1.46 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 09:36:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:48:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 
 #ifndef _STREAM_HXX //autogen
