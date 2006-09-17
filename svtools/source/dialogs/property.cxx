e2275f76d3059c8e1fc00f187f2f3fb22f669ecf
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:46:02 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:01 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/dialogs/property.cxx
+++ svtools/source/dialogs/property.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: property.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:00:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:46:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,11 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "ui_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 //------------------------------------------------------------------
