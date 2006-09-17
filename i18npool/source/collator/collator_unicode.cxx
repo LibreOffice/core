ea1b4b0d4c3651753ee8d9cece16b8bdc210d586
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:17:17 +0000

    INTEGRATION: CWS pchfix02 (1.13.14); FILE MERGED 2006/09/01 17:30:41 kaib 1.13.14.1: #i68856# Added header markers and pch files

--- i18npool/source/collator/collator_unicode.cxx
+++ i18npool/source/collator/collator_unicode.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: collator_unicode.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:44:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:17:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <rtl/ustrbuf.hxx>
 #include <collator_unicode.hxx>
