f4049757fc5f6f8c4e6446ae284e9adcebe09793
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:41:34 +0000

    INTEGRATION: CWS pchfix02 (1.6.36); FILE MERGED 2006/09/01 17:24:10 kaib 1.6.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/misc/ContainerMediator.cxx
+++ dbaccess/source/core/misc/ContainerMediator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ContainerMediator.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:16:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:41:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBA_CONTAINERMEDIATOR_HXX
 #include "ContainerMediator.hxx"
 #endif
