56363d82d66c5aad5144d0d6b84c2775f821bd01
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:35:24 +0000

    INTEGRATION: CWS pchfix02 (1.8.182); FILE MERGED 2006/09/01 17:25:07 kaib 1.8.182.1: #i68856# Added header markers and pch files

--- desktop/source/app/cmdlinehelp.cxx
+++ desktop/source/app/cmdlinehelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cmdlinehelp.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-01 12:45:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:35:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include <stdlib.h>
 #ifdef UNX
