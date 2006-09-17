c2d796bc55f928220e3000c4935c29010d8aff03
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:04:07 +0000

    INTEGRATION: CWS pchfix02 (1.10.40); FILE MERGED 2006/09/01 17:25:41 kaib 1.10.40.1: #i68856# Added header markers and pch files

--- dtrans/test/win32/dnd/atlwindow.cxx
+++ dtrans/test/win32/dnd/atlwindow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atlwindow.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:31:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:04:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/lang/XComponent.hpp>
