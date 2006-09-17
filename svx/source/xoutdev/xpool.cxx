bb6b0f26e6b29e50eaa2e0e11f837353444e6146
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:25:00 +0000

    INTEGRATION: CWS pchfix02 (1.11.114); FILE MERGED 2006/09/01 17:47:47 kaib 1.11.114.1: #i68856# Added header markers and pch files

--- svx/source/xoutdev/xpool.cxx
+++ svx/source/xoutdev/xpool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xpool.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:07:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:25:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "xtable.hxx"
 #include "xattr.hxx"
