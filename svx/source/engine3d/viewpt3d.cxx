45111f06180fa32e877539d64a309bf7bf9ba4f1
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:59:48 +0000

    INTEGRATION: CWS pchfix02 (1.5.114); FILE MERGED 2006/09/01 17:46:44 kaib 1.5.114.1: #i68856# Added header markers and pch files

--- svx/source/engine3d/viewpt3d.cxx
+++ svx/source/engine3d/viewpt3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewpt3d.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:35:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:59:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _VIEWPT3D_HXX
 #include "viewpt3d.hxx"
