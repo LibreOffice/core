b0663f20d51ba75bb3571b411f8af67aa2776c4c
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:04:35 +0000

    INTEGRATION: CWS pchfix02 (1.10.46); FILE MERGED 2006/09/01 17:24:26 kaib 1.10.46.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/ConnectionHelper.cxx
+++ dbaccess/source/ui/dlg/ConnectionHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ConnectionHelper.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:01:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:04:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_CONNECTIONHELPER_HXX
 #include "ConnectionHelper.hxx"
