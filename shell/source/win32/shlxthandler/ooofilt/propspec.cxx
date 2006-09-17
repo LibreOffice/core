f7c37a8fb9b5f5d68a08c32aa0975ec843840cf6
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:45:01 +0000

    INTEGRATION: CWS pchfix02 (1.4.30); FILE MERGED 2006/09/01 17:39:10 kaib 1.4.30.1: #i68856# Added header markers and pch files

--- shell/source/win32/shlxthandler/ooofilt/propspec.cxx
+++ shell/source/win32/shlxthandler/ooofilt/propspec.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propspec.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:21:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:44:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
+
 //+-------------------------------------------------------------------------
 //
 //  File:       propspec.cxx
@@ -49,7 +52,6 @@
 #if defined _MSC_VER
 #pragma warning(pop)
 #endif
-#pragma hdrstop
 
 #include "propspec.hxx"
 
--- shell/source/win32/shlxthandler/ooofilt/proxy/ooofiltproxy.cxx
+++ shell/source/win32/shlxthandler/ooofilt/proxy/ooofiltproxy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ooofiltproxy.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:22:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:45:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #if defined _MSC_VER
 #pragma warning(push, 1)
