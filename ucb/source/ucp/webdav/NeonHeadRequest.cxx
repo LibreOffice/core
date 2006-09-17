46b4ee5f39b22e498f5f31a66e9b004a0d83963b
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:06:12 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:55:54 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/webdav/NeonHeadRequest.cxx
+++ ucb/source/ucp/webdav/NeonHeadRequest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: NeonHeadRequest.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 09:34:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:06:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
