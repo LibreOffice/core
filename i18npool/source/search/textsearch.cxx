8fb3cc450125a146bf101e8c00af46fb09c9b06f
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:23:23 +0000

    INTEGRATION: CWS pchfix02 (1.8.16); FILE MERGED 2006/09/01 17:30:48 kaib 1.8.16.1: #i68856# Added header markers and pch files

--- i18npool/source/search/textsearch.cxx
+++ i18npool/source/search/textsearch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textsearch.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:48:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:23:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include "textsearch.hxx"
 #include "levdis.hxx"
