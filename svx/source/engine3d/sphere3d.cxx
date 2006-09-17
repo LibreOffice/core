b7645773ea2fc31cb4c27cc36eac3819628001f5
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:58:52 +0000

    INTEGRATION: CWS pchfix02 (1.14.114); FILE MERGED 2006/09/01 17:46:44 kaib 1.14.114.1: #i68856# Added header markers and pch files

--- svx/source/engine3d/sphere3d.cxx
+++ svx/source/engine3d/sphere3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sphere3d.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:34:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:58:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "svdstr.hrc"
 #include "svdglob.hxx"
