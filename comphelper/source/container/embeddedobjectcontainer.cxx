9d9019b4c81826bacfe181135324edd0b12a44b1
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:06:48 +0000

    INTEGRATION: CWS pchfix02 (1.16.68); FILE MERGED 2006/09/01 17:19:38 kaib 1.16.68.1: #i68856# Added header markers and pch files

--- comphelper/source/container/embeddedobjectcontainer.cxx
+++ comphelper/source/container/embeddedobjectcontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: embeddedobjectcontainer.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 19:31:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:06:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef _COM_SUN_STAR_CONTAINER_XCHILD_HPP_
 #include <com/sun/star/container/XChild.hpp>
