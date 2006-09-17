a57532fe1294ac201da0ee94c3aa93b81da06788
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:00:04 +0000

    INTEGRATION: CWS pchfix02 (1.6.92); FILE MERGED 2006/09/01 17:43:12 kaib 1.6.92.1: #i68856# Added header markers and pch files

--- svtools/source/items/macitem.cxx
+++ svtools/source/items/macitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: macitem.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:12:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:00:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,12 +33,14 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
 #endif
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 #define ITEMID_MACRO 0
 
