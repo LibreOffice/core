7380c4a713836f9263556fdfc2c7f33027e51df7
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:38:43 +0000

    INTEGRATION: CWS pchfix02 (1.7.160); FILE MERGED 2006/09/01 17:59:42 kaib 1.7.160.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/valueproperties.cxx
+++ xmloff/source/forms/valueproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: valueproperties.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:16:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:38:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_FORMS_VALUEPROPERTIES_HXX_
 #include "valueproperties.hxx"
