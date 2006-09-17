87e976906ef2ff74b91b161a9d8fcc4de626f058
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:43:30 +0000

    INTEGRATION: CWS pchfix02 (1.10.194); FILE MERGED 2006/09/01 17:27:12 kaib 1.10.194.1: #i68856# Added header markers and pch files

--- filter/source/placeware/exporter.cxx
+++ filter/source/placeware/exporter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: exporter.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:51:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:43:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #ifndef _COM_SUN_STAR_CONTAINER_XNAMED_HPP_
 #include <com/sun/star/container/XNamed.hpp>
