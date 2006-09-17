849a733bad3599bde8102df5d0e0d1af21d5912a
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:28:12 +0000

    INTEGRATION: CWS pchfix02 (1.17.88); FILE MERGED 2006/09/01 17:38:30 kaib 1.17.88.1: #i68856# Added header markers and pch files

--- sfx2/source/control/request.cxx
+++ sfx2/source/control/request.cxx
@@ -1,8 +1,46 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: request.cxx,v $
+ *
+ *  $Revision: 1.18 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:28:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 /*==================================================================
 // class SfxRequest
 //
 // (C) 1996 - 2000 StarDivision GmbH, Hamburg, Germany
-// $Author: hr $ $Date: 2006-06-19 22:18:00 $ $Revision: 1.17 $
+// $Author: obo $ $Date: 2006-09-17 16:28:12 $ $Revision: 1.18 $
 // $Logfile:   T:/sfx2/source/control/request.cxv  $ $Workfile:   REQUEST.CXX  $
 //------------------------------------------------------------------*/
 
@@ -49,7 +87,6 @@
 #include <comphelper/processfactory.hxx>
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include <svtools/smplhint.hxx>
