880430b1da2c5cab6b0e46751e49d437a53faff7
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:45:18 +0000

    INTEGRATION: CWS pchfix02 (1.6.194); FILE MERGED 2006/09/01 17:27:13 kaib 1.6.194.1: #i68856# Added header markers and pch files

--- filter/source/svg/svgimport.cxx
+++ filter/source/svg/svgimport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svgimport.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:56:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:45:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #include "svgfilter.hxx"
 #include "rtl/ref.hxx"
