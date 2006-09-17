6639a9a9e329f08caaf84b900a253d755c1856b6
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:01:50 +0000

    INTEGRATION: CWS pchfix02 (1.7.60); FILE MERGED 2006/09/01 17:21:47 kaib 1.7.60.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/TSortIndex.cxx
+++ connectivity/source/commontools/TSortIndex.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TSortIndex.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:04:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:01:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef CONNECTIVITY_TSORTINDEX_HXX
 #include "TSortIndex.hxx"
 #endif
