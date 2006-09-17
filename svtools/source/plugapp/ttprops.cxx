c3b814a5934c902db484b7e041bf344a6da36ea1
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:22:52 +0000

    INTEGRATION: CWS pchfix02 (1.2.324); FILE MERGED 2006/09/01 17:43:28 kaib 1.2.324.1: #i68856# Added header markers and pch files

--- svtools/source/plugapp/ttprops.cxx
+++ svtools/source/plugapp/ttprops.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ttprops.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:40:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:22:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _SVTOOLS_TTPROPS_HXX // handmade
 #include "ttprops.hxx"
