f81c2d230b1860c10f572c4ab85d2b219374635d
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:17:01 +0000

    INTEGRATION: CWS pchfix02 (1.6.36); FILE MERGED 2006/09/01 17:19:46 kaib 1.6.36.1: #i68856# Added header markers and pch files

--- comphelper/source/property/ChainablePropertySetInfo.cxx
+++ comphelper/source/property/ChainablePropertySetInfo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChainablePropertySetInfo.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:50:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:16:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 #ifndef _COMPHELPER_CHAINABLEPROPERTYSETINFO_HXX_
 #include <comphelper/ChainablePropertySetInfo.hxx>
 #endif
