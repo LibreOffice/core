abf70e7e3a3dd89b024a889dd79211a3e8cb985a
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:34:34 +0000

    INTEGRATION: CWS pchfix02 (1.17.178); FILE MERGED 2006/09/01 17:24:44 kaib 1.17.178.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/uno/unoadmin.cxx
+++ dbaccess/source/ui/uno/unoadmin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoadmin.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:47:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:34:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_SHARED_DBUSTRINGS_HRC
 #include "dbustrings.hrc"
