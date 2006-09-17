76d89f74908255f9402af4c51187415901f249ad
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:32:15 +0000

    INTEGRATION: CWS pchfix02 (1.114.32); FILE MERGED 2006/09/01 17:59:37 kaib 1.114.32.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/ximpshap.cxx
+++ xmloff/source/draw/ximpshap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximpshap.cxx,v $
  *
- *  $Revision: 1.114 $
+ *  $Revision: 1.115 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:15:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:32:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 
