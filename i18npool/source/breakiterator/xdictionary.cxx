39b74fd3d5a44973c28aa97130835dced7214da4
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:14:44 +0000

    INTEGRATION: CWS pchfix02 (1.13.12); FILE MERGED 2006/09/01 17:30:39 kaib 1.13.12.1: #i68856# Added header markers and pch files

--- i18npool/source/breakiterator/xdictionary.cxx
+++ i18npool/source/breakiterator/xdictionary.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xdictionary.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-06 10:33:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:14:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 // xdictionary.cpp: implementation of the xdictionary class.
 //
