d489b27324d5563e71c00fd23eb7857a373e1e12
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:40:46 +0000

    INTEGRATION: CWS pchfix02 (1.5.34); FILE MERGED 2006/09/01 17:59:44 kaib 1.5.34.1: #i68856# Added header markers and pch files

--- xmloff/source/script/XMLScriptContextFactory.cxx
+++ xmloff/source/script/XMLScriptContextFactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLScriptContextFactory.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:22:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:40:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLSCRIPTCONTEXTFACTORY_HXX
 #include "XMLScriptContextFactory.hxx"
