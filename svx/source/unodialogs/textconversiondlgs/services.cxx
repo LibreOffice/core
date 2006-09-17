c1ba3af57ec5604d748222b7fe05eb90b256d857
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:09:39 +0000

    INTEGRATION: CWS pchfix02 (1.3.536); FILE MERGED 2006/09/01 17:47:36 kaib 1.3.536.1: #i68856# Added header markers and pch files

--- svx/source/unodialogs/textconversiondlgs/services.cxx
+++ svx/source/unodialogs/textconversiondlgs/services.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: services.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:00:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #include "chinese_translation_unodialog.hxx"
 
 #ifndef _CPPUHELPER_IMPLEMENATIONENTRY_HXX_
