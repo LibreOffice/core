cfd7b012562d4805c6b40c714ffa3210c60fe3de
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:11:37 +0000

    INTEGRATION: CWS pchfix02 (1.3.28); FILE MERGED 2006/09/01 17:41:31 kaib 1.3.28.1: #i68856# Added header markers and pch files

--- store/source/object.cxx
+++ store/source/object.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: object.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:43:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:11:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,10 @@
  *
  ************************************************************************/
 
-#define _STORE_OBJECT_CXX_ "$Revision: 1.3 $"
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_store.hxx"
+
+#define _STORE_OBJECT_CXX_ "$Revision: 1.4 $"
 
 #ifndef _SAL_TYPES_H_
 #include <sal/types.h>
