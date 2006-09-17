a98955eff7fc559199519bf1191415c5839858d5
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:16:35 +0000

    INTEGRATION: CWS pchfix02 (1.8.156); FILE MERGED 2006/09/01 17:59:24 kaib 1.8.156.1: #i68856# Added header markers and pch files

--- xmloff/source/chart/XMLChartStyleContext.cxx
+++ xmloff/source/chart/XMLChartStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLChartStyleContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:27:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:16:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 #include "XMLChartStyleContext.hxx"
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
