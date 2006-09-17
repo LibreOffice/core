aa79e898bde6fdcba365a9d5e72e3e7b499696cc
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:12:20 +0000

    INTEGRATION: CWS pchfix02 (1.6.8); FILE MERGED 2006/09/01 17:31:13 kaib 1.6.8.1: #i68856# Added header markers and pch files

--- idlc/source/astoperation.cxx
+++ idlc/source/astoperation.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: astoperation.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:47:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:12:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 #ifndef _IDLC_ASTOPERATION_HXX_
 #include <idlc/astoperation.hxx>
 #endif
