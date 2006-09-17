9f1d74f5b50373d4a91aecd953ae926b1a21aaac
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:10:39 +0000

    INTEGRATION: CWS pchfix02 (1.40.42); FILE MERGED 2006/09/01 17:22:36 kaib 1.40.42.1: #i68856# Added header markers and pch files

--- connectivity/source/sdbcx/VCollection.cxx
+++ connectivity/source/sdbcx/VCollection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VCollection.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:39:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:10:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #include <stdio.h>
 
 #ifndef _CONNECTIVITY_SDBCX_COLLECTION_HXX_
