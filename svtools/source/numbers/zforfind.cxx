747e63e965b57cf3a92493a0db47a4cbc557acf6
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:21:25 +0000

    INTEGRATION: CWS pchfix02 (1.43.26); FILE MERGED 2006/09/01 17:43:26 kaib 1.43.26.1: #i68856# Added header markers and pch files

--- svtools/source/numbers/zforfind.cxx
+++ svtools/source/numbers/zforfind.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: zforfind.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 15:51:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:21:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include <ctype.h>
 #include <stdlib.h>
