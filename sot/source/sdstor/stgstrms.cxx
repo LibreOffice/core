ec0701063eb6a304147348ec8891e1571b887675
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:10:28 +0000

    INTEGRATION: CWS pchfix02 (1.9.14); FILE MERGED 2006/09/01 17:40:29 kaib 1.9.14.1: #i68856# Added header markers and pch files

--- sot/source/sdstor/stgstrms.cxx
+++ sot/source/sdstor/stgstrms.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stgstrms.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:55:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:10:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #include <string.h>     // memcpy()
 
