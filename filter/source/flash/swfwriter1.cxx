af6ba9247916e1c1010c26d84951ff51e9ef5ab9
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:40:51 +0000

    INTEGRATION: CWS pchfix02 (1.19.6); FILE MERGED 2006/09/01 17:27:10 kaib 1.19.6.1: #i68856# Added header markers and pch files

--- filter/source/flash/swfwriter1.cxx
+++ filter/source/flash/swfwriter1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swfwriter1.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-03 12:32:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:40:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 #ifndef _SWF_WRITER_HXX_
 #include "swfwriter.hxx"
 #endif
