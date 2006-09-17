9053c2ae9667f812c452560bdc147afcd99a1642
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:53:39 +0000

    INTEGRATION: CWS pchfix02 (1.5.42); FILE MERGED 2006/09/01 17:22:24 kaib 1.5.42.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/kab/KStatement.cxx
+++ connectivity/source/drivers/kab/KStatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: KStatement.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 14:54:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:53:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "KStatement.hxx"
 #ifndef _CONNECTIVITY_KAB_CONNECTION_HXX_
