8ea1e7fb66bef356ba2beda6f0f8bdc68535e3f4
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:46:24 +0000

    INTEGRATION: CWS pchfix02 (1.33.48); FILE MERGED 2006/09/01 17:38:44 kaib 1.33.48.1: #i68856# Added header markers and pch files

--- sfx2/source/inet/inettbc.cxx
+++ sfx2/source/inet/inettbc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inettbc.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:19:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:46:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #include "inettbc.hxx"
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_ANY_H_
