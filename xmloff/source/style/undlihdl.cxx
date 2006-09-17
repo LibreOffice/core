3e9a585fe269c91f98b7bcab23a288098b9fdc3d
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:57:43 +0000

    INTEGRATION: CWS pchfix02 (1.6.160); FILE MERGED 2006/09/01 17:59:56 kaib 1.6.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/undlihdl.cxx
+++ xmloff/source/style/undlihdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undlihdl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:02:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:57:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PROPERTYHANDLER_UNDERLINETYPES_HXX
 #include <undlihdl.hxx>
