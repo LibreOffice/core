401d80b047897c3787c7532e7d40d2496ba806cf
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:30:59 +0000

    INTEGRATION: CWS pchfix02 (1.17.4); FILE MERGED 2006/09/01 17:58:03 kaib 1.17.4.1: #i68856# Added header markers and pch files

--- vcl/unx/kde/salnativewidgets-kde.cxx
+++ vcl/unx/kde/salnativewidgets-kde.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salnativewidgets-kde.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:48:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:30:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #define _SV_SALNATIVEWIDGETS_KDE_CXX
 
