0fdcf8fdc8f11c04595a63e14db178b475fa4ef1
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:11:02 +0000

    INTEGRATION: CWS pchfix02 (1.14.28); FILE MERGED 2006/09/01 17:41:31 kaib 1.14.28.1: #i68856# Added header markers and pch files

--- store/source/filelckb.cxx
+++ store/source/filelckb.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filelckb.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-24 10:41:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:11:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_store.hxx"
 
 #include <store/filelckb.hxx>
 
