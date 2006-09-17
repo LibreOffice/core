917892f12e19883df49a141418e7faed813121d1
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:15:07 +0000

    INTEGRATION: CWS pchfix02 (1.2.166); FILE MERGED 2006/09/01 17:22:39 kaib 1.2.166.1: #i68856# Added header markers and pch files

--- connectivity/workben/little/main.cxx
+++ connectivity/workben/little/main.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: main.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:50:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:15:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef _CONNECTIVITY_SQLPARSE_HXX
 #include <connectivity/sqlparse.hxx>
 #endif
