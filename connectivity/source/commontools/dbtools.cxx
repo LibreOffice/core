7056c538262e6bb45ba9a5b8290ece222e223dde
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:02:59 +0000

    INTEGRATION: CWS pchfix02 (1.63.6); FILE MERGED 2006/09/01 17:21:48 kaib 1.63.6.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/dbtools.cxx
+++ connectivity/source/commontools/dbtools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbtools.cxx,v $
  *
- *  $Revision: 1.63 $
+ *  $Revision: 1.64 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:27:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:02:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
