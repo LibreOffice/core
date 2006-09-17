f285b349932da772d45a64fed9bbff00750caa24
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:57:57 +0000

    INTEGRATION: CWS pchfix02 (1.6.16); FILE MERGED 2006/09/01 17:25:36 kaib 1.6.16.1: #i68856# Added header markers and pch files

--- dtrans/source/win32/dnd/dndentry.cxx
+++ dtrans/source/win32/dnd/dndentry.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dndentry.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:01:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:57:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #ifndef _CPPUHELPER_FACTORY_HXX_
 #include <cppuhelper/factory.hxx>
