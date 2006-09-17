dee3c1d36c4c09d06116e2f8fc36b66f576d9264
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:14:48 +0000

    INTEGRATION: CWS pchfix02 (1.48.4); FILE MERGED 2006/09/01 17:24:32 kaib 1.48.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/WCopyTable.cxx
+++ dbaccess/source/ui/misc/WCopyTable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WCopyTable.cxx,v $
  *
- *  $Revision: 1.48 $
+ *  $Revision: 1.49 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:54:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:14:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
