8127b0044b197fa6c04dd5170d5ff00e2bcb8737
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:01:11 +0000

    INTEGRATION: CWS pchfix02 (1.13.60); FILE MERGED 2006/09/01 17:22:29 kaib 1.13.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/mozab/mozillasrc/MDatabaseMetaDataHelper.cxx
+++ connectivity/source/drivers/mozab/mozillasrc/MDatabaseMetaDataHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MDatabaseMetaDataHelper.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:50:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:01:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 // Mozilla includes.
 #include <MNSInclude.hxx>
