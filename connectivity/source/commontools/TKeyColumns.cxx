374350c5cc2450f6381b09e3db69b029ee1d9fef
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:00:28 +0000

    INTEGRATION: CWS pchfix02 (1.4.42); FILE MERGED 2006/09/01 17:21:46 kaib 1.4.42.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/TKeyColumns.cxx
+++ connectivity/source/commontools/TKeyColumns.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TKeyColumns.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:19:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:00:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_TKEYCOLUMNS_HXX
 #include "connectivity/TKeyColumns.hxx"
