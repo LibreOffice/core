e8785bd4f1d24c519aa7777322b477440df6f679
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:30:20 +0000

    INTEGRATION: CWS pchfix02 (1.5.12); FILE MERGED 2006/09/01 17:32:43 kaib 1.5.12.1: #i68856# Added header markers and pch files

--- package/source/zippackage/wrapstreamforshare.cxx
+++ package/source/zippackage/wrapstreamforshare.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrapstreamforshare.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:15:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:30:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
