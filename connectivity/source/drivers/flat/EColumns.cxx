cd696dd199c0223a0cd6f9e02ccce976ecf2a8d4
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:36:59 +0000

    INTEGRATION: CWS pchfix02 (1.5.166); FILE MERGED 2006/09/01 17:22:13 kaib 1.5.166.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/flat/EColumns.cxx
+++ connectivity/source/drivers/flat/EColumns.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EColumns.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:59:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:36:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FLAT_COLUMNS_HXX_
 #include "flat/EColumns.hxx"
