a759804732101a3258724c888574eedcd2fd0907
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:01:50 +0000

    INTEGRATION: CWS pchfix02 (1.4.40); FILE MERGED 2006/09/01 17:25:39 kaib 1.4.40.1: #i68856# Added header markers and pch files

--- dtrans/source/win32/dtobj/XNotifyingDataObject.cxx
+++ dtrans/source/win32/dtobj/XNotifyingDataObject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XNotifyingDataObject.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:25:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:01:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
