c1bf89b9c8213e0de5b4e9eca6d6f01fbf80def1
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:06:25 +0000

    INTEGRATION: CWS pchfix02 (1.9.114); FILE MERGED 2006/09/01 17:46:01 kaib 1.9.114.1: #i68856# Added header markers and pch files

--- svx/source/customshapes/EnhancedCustomShape3d.cxx
+++ svx/source/customshapes/EnhancedCustomShape3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnhancedCustomShape3d.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:56:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:06:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _ENHANCEDCUSTOMSHAPE3D_HXX
 #include "EnhancedCustomShape3d.hxx"
