c379d291f8a1a941d2dc7859e5310fd7e2766dc5
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:07:07 +0000

    INTEGRATION: CWS pchfix02 (1.12.34); FILE MERGED 2006/09/01 18:00:03 kaib 1.12.34.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLIndexSourceBaseContext.cxx
+++ xmloff/source/text/XMLIndexSourceBaseContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexSourceBaseContext.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:40:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:07:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 #ifndef _XMLOFF_XMLINDEXSOURCEBASECONTEXT_HXX_
