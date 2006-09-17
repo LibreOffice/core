08de15d816fb43c8f168992cee35f2cbd38d243e
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:03:42 +0000

    INTEGRATION: CWS pchfix02 (1.9.158); FILE MERGED 2006/09/01 18:00:01 kaib 1.9.158.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLChangedRegionImportContext.cxx
+++ xmloff/source/text/XMLChangedRegionImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLChangedRegionImportContext.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:00:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:03:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLCHANGEDREGIONIMPORTCONTEXT_HXX
 #include "XMLChangedRegionImportContext.hxx"
