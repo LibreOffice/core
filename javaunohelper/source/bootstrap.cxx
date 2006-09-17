39b8cdf100ca57bea86f1f03c8e97bcfeeecd104
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:06:38 +0000

    INTEGRATION: CWS pchfix02 (1.11.4); FILE MERGED 2006/09/01 17:31:31 kaib 1.11.4.1: #i68856# Added header markers and pch files

--- javaunohelper/source/bootstrap.cxx
+++ javaunohelper/source/bootstrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bootstrap.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:54:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:06:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_javaunohelper.hxx"
 
 #include "osl/diagnose.h"
 
