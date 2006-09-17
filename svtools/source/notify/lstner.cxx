d4d787a7412560fae8849cee5aa8dd9013a01828
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:19:43 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:25 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/notify/lstner.cxx
+++ svtools/source/notify/lstner.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lstner.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:23:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:19:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef DEBUG_HXX
