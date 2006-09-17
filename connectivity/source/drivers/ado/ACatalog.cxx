2940e793bedd74ac2969f1815bc78c507094a699
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:10:59 +0000

    INTEGRATION: CWS pchfix02 (1.11.6); FILE MERGED 2006/09/01 17:21:54 kaib 1.11.6.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/ado/ACatalog.cxx
+++ connectivity/source/drivers/ado/ACatalog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ACatalog.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:27:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:10:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_ADO_CATALOG_HXX_
 #include "ado/ACatalog.hxx"
