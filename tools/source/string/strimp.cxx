c8fb4f0e4afd8a81aeb644dc1f1c33189cede036
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:03:23 +0000

    INTEGRATION: CWS pchfix02 (1.8.44); FILE MERGED 2006/09/01 17:54:58 kaib 1.8.44.1: #i68856# Added header markers and pch files

--- tools/source/string/strimp.cxx
+++ tools/source/string/strimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strimp.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:53:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:03:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 // =======================================================================
 
