708cc1351e7ebb7e505981df81aff3a0385ef4a4
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:18:07 +0000

    INTEGRATION: CWS pchfix02 (1.9.116); FILE MERGED 2006/09/01 17:46:58 kaib 1.9.116.1: #i68856# Added header markers and pch files

--- svx/source/items/bulitem.cxx
+++ svx/source/items/bulitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bulitem.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:09:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:18:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // include ---------------------------------------------------------------
 
