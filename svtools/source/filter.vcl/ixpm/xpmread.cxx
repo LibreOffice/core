eeca5ac2686dc027b03038f17c72dd3e0c2e6048
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:54:29 +0000

    INTEGRATION: CWS pchfix02 (1.6.90); FILE MERGED 2006/09/01 17:43:08 kaib 1.6.90.1: #i68856# Added header markers and pch files

--- svtools/source/filter.vcl/ixpm/xpmread.cxx
+++ svtools/source/filter.vcl/ixpm/xpmread.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xpmread.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:07:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:54:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _BMPACC_HXX
 #include <vcl/bmpacc.hxx>
