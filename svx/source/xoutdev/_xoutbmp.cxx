f2d1f4ca2c6db03c3f12891b0142661ea18d9c36
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:22:13 +0000

    INTEGRATION: CWS pchfix02 (1.16.86); FILE MERGED 2006/09/01 17:47:45 kaib 1.16.86.1: #i68856# Added header markers and pch files

--- svx/source/xoutdev/_xoutbmp.cxx
+++ svx/source/xoutdev/_xoutbmp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _xoutbmp.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-05 22:19:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:22:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include <sot/factory.hxx>
 #include <tools/urlobj.hxx>
