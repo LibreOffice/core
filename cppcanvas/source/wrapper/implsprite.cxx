7493246383edab463f0e7d4e3c3a7810314d7c5a
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:53:31 +0000

    INTEGRATION: CWS pchfix02 (1.7.24); FILE MERGED 2006/09/01 17:22:51 kaib 1.7.24.1: #i68856# Added header markers and pch files

--- cppcanvas/source/wrapper/implsprite.cxx
+++ cppcanvas/source/wrapper/implsprite.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implsprite.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:43:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:53:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #ifndef _COM_SUN_STAR_RENDERING_XSPRITE_HPP_
 #include <com/sun/star/rendering/XSprite.hpp>
--- cppcanvas/source/wrapper/implspritecanvas.cxx
+++ cppcanvas/source/wrapper/implspritecanvas.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implspritecanvas.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:44:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:53:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #ifndef _BGFX_MATRIX_B2DHOMMATRIX_HXX
 #include <basegfx/matrix/b2dhommatrix.hxx>
