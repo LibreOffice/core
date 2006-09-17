e3f3a3da2f693a28130f3393bb47e0e159d9bdc8
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:58:24 +0000

    INTEGRATION: CWS pchfix02 (1.10.114); FILE MERGED 2006/09/01 17:46:43 kaib 1.10.114.1: #i68856# Added header markers and pch files

--- svx/source/engine3d/polysc3d.cxx
+++ svx/source/engine3d/polysc3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: polysc3d.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:34:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:58:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef INCLUDED_SVTOOLS_OPTIONS3D_HXX
 #include <svtools/options3d.hxx>
