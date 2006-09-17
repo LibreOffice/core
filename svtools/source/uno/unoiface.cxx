3d6b3981fe505c04961ec1dace59ea2764023442
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:30:50 +0000

    INTEGRATION: CWS pchfix02 (1.21.46); FILE MERGED 2006/09/01 17:43:34 kaib 1.21.46.1: #i68856# Added header markers and pch files

--- svtools/source/uno/unoiface.cxx
+++ svtools/source/uno/unoiface.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoiface.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:13:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:30:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #define _SVT_UNOIFACE_CXX
 
