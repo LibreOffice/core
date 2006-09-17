da3696befb759f21cef083f0fb3b8a4e5923d902
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:04:46 +0000

    INTEGRATION: CWS pchfix02 (1.4.92); FILE MERGED 2006/09/01 17:43:15 kaib 1.4.92.1: #i68856# Added header markers and pch files

--- svtools/source/items1/frqitem.cxx
+++ svtools/source/items1/frqitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frqitem.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:16:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:04:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include <unotools/intlwrapper.hxx>
--- svtools/source/items1/ilstitem.cxx
+++ svtools/source/items1/ilstitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ilstitem.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:16:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:04:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_SCRIPT_XTYPECONVERTER_HPP_
 #include <com/sun/star/script/XTypeConverter.hpp>
