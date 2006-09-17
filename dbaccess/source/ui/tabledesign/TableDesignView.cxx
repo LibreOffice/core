9629e0ef7932a1a2daefab6309d427e45b66b0c2
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:30:12 +0000

    INTEGRATION: CWS pchfix02 (1.29.52); FILE MERGED 2006/09/01 17:24:42 kaib 1.29.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/tabledesign/TableDesignView.cxx
+++ dbaccess/source/ui/tabledesign/TableDesignView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TableDesignView.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:33:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:30:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_TABLEDESIGNVIEW_HXX
 #include "TableDesignView.hxx"
 #endif
