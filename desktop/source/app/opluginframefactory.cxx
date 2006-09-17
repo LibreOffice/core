f8c726ccf760e0a23a96f63196ffe8c2b4a6e30a
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:38:09 +0000

    INTEGRATION: CWS pchfix02 (1.4.222); FILE MERGED 2006/09/01 17:25:08 kaib 1.4.222.1: #i68856# Added header markers and pch files

--- desktop/source/app/opluginframefactory.cxx
+++ desktop/source/app/opluginframefactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: opluginframefactory.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:13:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:38:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 //_________________________________________________________________________________________________________________
 //  my own includes
