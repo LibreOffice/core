545d8705ac7ef3cde36dd7b01ac35e2902c3911d
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:22:45 +0000

    INTEGRATION: CWS pchfix02 (1.15.92); FILE MERGED 2006/09/01 17:42:46 kaib 1.15.92.1: #i68856# Added header markers and pch files

--- svtools/source/config/dynamicmenuoptions.cxx
+++ svtools/source/config/dynamicmenuoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dynamicmenuoptions.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:42:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:22:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 //_________________________________________________________________________________________________________________
