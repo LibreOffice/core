e30f555d4ed52ccb524c0ba8ff19893685aacb55
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:05:56 +0000

    INTEGRATION: CWS pchfix02 (1.6.158); FILE MERGED 2006/09/01 18:00:02 kaib 1.6.158.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLIndexChapterInfoEntryContext.cxx
+++ xmloff/source/text/XMLIndexChapterInfoEntryContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexChapterInfoEntryContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:05:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:05:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 #ifndef _XMLOFF_XMLINDEXCHAPTERINFOENTRYCONTEXT_HXX_
