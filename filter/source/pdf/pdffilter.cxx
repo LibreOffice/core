c3b45d9aeee33cf3672e6f0abd43295abddd9c7d
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:42:44 +0000

    INTEGRATION: CWS pchfix02 (1.9.24); FILE MERGED 2006/09/01 17:27:12 kaib 1.9.24.1: #i68856# Added header markers and pch files

--- filter/source/pdf/pdffilter.cxx
+++ filter/source/pdf/pdffilter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pdffilter.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 11:14:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:42:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #include "pdffilter.hxx"
 #include "pdfexport.hxx"
