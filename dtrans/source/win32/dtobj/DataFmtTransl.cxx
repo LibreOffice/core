cdce95ddb8e2c770016e03d75cdb344664f08fba
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:00:27 +0000

    INTEGRATION: CWS pchfix02 (1.20.14); FILE MERGED 2006/09/01 17:25:38 kaib 1.20.14.1: #i68856# Added header markers and pch files

--- dtrans/source/win32/dtobj/DataFmtTransl.cxx
+++ dtrans/source/win32/dtobj/DataFmtTransl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataFmtTransl.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:04:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:00:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 //------------------------------------------------------------------------
 // includes
