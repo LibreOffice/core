cebe434dd7058195e8f1e03b64eb5615ea81809e
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:27:10 +0000

    INTEGRATION: CWS pchfix02 (1.2.58); FILE MERGED 2006/09/01 17:56:23 kaib 1.2.58.1: #i68856# Added header markers and pch files

--- unotools/source/misc/datetime.cxx
+++ unotools/source/misc/datetime.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datetime.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:46:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:27:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef _UNOTOOLS_DATETIME_HXX_
 #include <unotools/datetime.hxx>
