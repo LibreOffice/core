89a3f68e4443b617ac960518b747335de57a7fba
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:51:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.114); FILE MERGED 2006/09/01 17:30:12 kaib 1.4.114.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/ipbm/ipbm.cxx
+++ goodies/source/filter.vcl/ipbm/ipbm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ipbm.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:58:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:51:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <vcl/graph.hxx>
 #include <vcl/bmpacc.hxx>
@@ -553,7 +556,6 @@ extern "C" BOOL GraphicImport(SvStream & rStream, Graphic & rGraphic,
 
 //================== ein bischen Muell fuer Windows ==========================
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifdef WIN
--- goodies/source/filter.vcl/ipcx/ipcx.cxx
+++ goodies/source/filter.vcl/ipcx/ipcx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ipcx.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:59:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:51:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <vcl/graph.hxx>
 #include <vcl/bmpacc.hxx>
@@ -431,7 +434,6 @@ extern "C" BOOL GraphicImport(SvStream & rStream, Graphic & rGraphic,
 
 //================== ein bischen Muell fuer Windows ==========================
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifdef WIN
