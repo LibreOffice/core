72ddab76325c5fffee962c3d16749201f4309679
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:19:39 +0000

    INTEGRATION: CWS pchfix02 (1.5.6); FILE MERGED 2006/09/01 17:19:48 kaib 1.5.6.1: #i68856# Added header markers and pch files

--- comphelper/source/property/propertystatecontainer.cxx
+++ comphelper/source/property/propertystatecontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertystatecontainer.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 11:04:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:19:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef COMPHELPER_PROPERTYSTATECONTAINER_HXX
 #include "comphelper/propertystatecontainer.hxx"
