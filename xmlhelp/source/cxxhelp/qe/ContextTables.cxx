9530ce460e930da14d3567e6131ccec3321bc113
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:19:04 +0000

    INTEGRATION: CWS pchfix02 (1.7.8); FILE MERGED 2006/09/01 17:58:33 kaib 1.7.8.1: #i68856# Added header markers and pch files

--- xmlhelp/source/cxxhelp/qe/ContextTables.cxx
+++ xmlhelp/source/cxxhelp/qe/ContextTables.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ContextTables.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:40:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:19:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlhelp.hxx"
 
 #ifndef _rtl_MEMORY_H_
 #include <rtl/memory.h>
