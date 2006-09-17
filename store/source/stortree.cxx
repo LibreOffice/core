aa6bbac0656618681957e640a48aa8d6464ee427
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:13:15 +0000

    INTEGRATION: CWS pchfix02 (1.6.6); FILE MERGED 2006/09/01 17:41:32 kaib 1.6.6.1: #i68856# Added header markers and pch files

--- store/source/stortree.cxx
+++ store/source/stortree.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stortree.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:34:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:13:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,10 @@
  *
  ************************************************************************/
 
-#define _STORE_STORTREE_CXX "$Revision: 1.6 $"
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_store.hxx"
+
+#define _STORE_STORTREE_CXX "$Revision: 1.7 $"
 
 #ifndef _SAL_TYPES_H_
 #include <sal/types.h>
