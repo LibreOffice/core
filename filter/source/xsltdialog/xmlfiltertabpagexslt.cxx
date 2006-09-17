59d0204ae9302a65e2bc78ab888c88b7dd389df6
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:49:14 +0000

    INTEGRATION: CWS pchfix02 (1.4.194); FILE MERGED 2006/09/01 17:27:16 kaib 1.4.194.1: #i68856# Added header markers and pch files

--- filter/source/xsltdialog/xmlfiltertabpagexslt.cxx
+++ filter/source/xsltdialog/xmlfiltertabpagexslt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlfiltertabpagexslt.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:22:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:49:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #ifndef _COM_SUN_STAR_FRAME_XCONFIGMANAGER_HPP_
 #include <com/sun/star/frame/XConfigManager.hpp>
