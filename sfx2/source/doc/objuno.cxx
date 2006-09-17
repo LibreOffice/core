7c5e4f1bca167b9330b2fbffe7548305d380603f
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:43:51 +0000

    INTEGRATION: CWS pchfix02 (1.27.4); FILE MERGED 2006/09/01 17:38:42 kaib 1.27.4.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/objuno.cxx
+++ sfx2/source/doc/objuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objuno.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 15:41:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:43:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
