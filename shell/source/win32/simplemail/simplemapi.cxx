0425243354ebf4bfb35a9a4b58788b65c9610ca2
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:48:33 +0000

    INTEGRATION: CWS pchfix02 (1.3.98); FILE MERGED 2006/09/01 17:39:13 kaib 1.3.98.1: #i68856# Added header markers and pch files

--- shell/source/win32/simplemail/simplemapi.cxx
+++ shell/source/win32/simplemail/simplemapi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: simplemapi.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:08:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:47:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef INCLUDED_SIMPLEMAPI_HXX
 #include "simplemapi.hxx"
