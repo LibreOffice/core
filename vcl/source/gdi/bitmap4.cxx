04e0ed76659e0ae0dc637ffd06c249b98c03ce52
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:57:40 +0000

    INTEGRATION: CWS pchfix02 (1.8.110); FILE MERGED 2006/09/01 17:57:40 kaib 1.8.110.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/bitmap4.cxx
+++ vcl/source/gdi/bitmap4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bitmap4.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:20:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:57:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <stdlib.h>
 #include <vos/macros.hxx>
