9cfa11bef1c30e61b1ae4778718b74876c131bd0
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:01:45 +0000

    INTEGRATION: CWS pchfix02 (1.4.28); FILE MERGED 2006/09/01 17:16:36 kaib 1.4.28.1: #i68856# Added header markers and pch files

--- basegfx/source/polygon/b2dpolygoncutandtouch.cxx
+++ basegfx/source/polygon/b2dpolygoncutandtouch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dpolygoncutandtouch.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:57:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:01:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_POLYGON_CUTANDTOUCH_HXX
 #include <basegfx/polygon/b2dpolygoncutandtouch.hxx>
