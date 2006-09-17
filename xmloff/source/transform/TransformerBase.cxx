5267ec933fafc0aaebeb8c8be8a0d8b601b7a22c
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:29:06 +0000

    INTEGRATION: CWS pchfix02 (1.19.34); FILE MERGED 2006/09/01 18:00:20 kaib 1.19.34.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/TransformerBase.cxx
+++ xmloff/source/transform/TransformerBase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TransformerBase.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:56:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:29:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
