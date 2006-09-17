b720904ba6bde7e54e781d47a5f26b2166f44bcb
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:12:04 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 18:00:06 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLStringBufferImportContext.cxx
+++ xmloff/source/text/XMLStringBufferImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLStringBufferImportContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:45:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:12:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLSTRINGBUFFERIMPORTCONTEXT_HXX
 #include "XMLStringBufferImportContext.hxx"
