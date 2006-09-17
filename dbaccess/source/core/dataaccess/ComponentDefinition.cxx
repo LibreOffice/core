4a8153e3f7800c254ae4e6307d860c03a7d0b8ac
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:37:23 +0000

    INTEGRATION: CWS pchfix02 (1.10.12); FILE MERGED 2006/09/01 17:24:07 kaib 1.10.12.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/ComponentDefinition.cxx
+++ dbaccess/source/core/dataaccess/ComponentDefinition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ComponentDefinition.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 13:56:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:37:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBA_COREDATAACESS_COMPONENTDEFINITION_HXX
 #include "ComponentDefinition.hxx"
