a16c1613e6744b3c565ae7c44ea03a879cd65468
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:54:37 +0000

    INTEGRATION: CWS pchfix02 (1.5.40); FILE MERGED 2006/09/01 17:25:34 kaib 1.5.40.1: #i68856# Added header markers and pch files

--- dtrans/source/aqua/aqua_service.cxx
+++ dtrans/source/aqua/aqua_service.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: aqua_service.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:08:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:54:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #ifndef _AQUA_CLIPBOARD_HXX_
 #include "aqua_clipboard.hxx"
