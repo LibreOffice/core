c8e8b8b1062e3bdfc729f5c4b89848e0b31ad995
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:46:29 +0000

    INTEGRATION: CWS pchfix02 (1.6.98); FILE MERGED 2006/09/01 17:39:12 kaib 1.6.98.1: #i68856# Added header markers and pch files

--- shell/source/win32/shlxthandler/util/fileextensions.cxx
+++ shell/source/win32/shlxthandler/util/fileextensions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fileextensions.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:06:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:46:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef FILEEXTENSIONS_HXX_INCLUDED
 #include "internal/fileextensions.hxx"
