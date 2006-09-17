dec1bc4cec487ce8bdc04f28488c5dd618e2456a
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:30:09 +0000

    INTEGRATION: CWS pchfix02 (1.7.20); FILE MERGED 2006/09/01 17:18:08 kaib 1.7.20.1: #i68856# Added header markers and pch files

--- canvas/source/vcl/canvasfont.cxx
+++ canvas/source/vcl/canvasfont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: canvasfont.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:19:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:30:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_canvas.hxx"
 
 #include <canvas/debug.hxx>
 
