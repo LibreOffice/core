e695d5265aa85f2f5d6744a4ca633acec65930e5
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:13:19 +0000

    INTEGRATION: CWS pchfix02 (1.8.34); FILE MERGED 2006/09/01 17:24:31 kaib 1.8.34.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/TableCopyHelper.cxx
+++ dbaccess/source/ui/misc/TableCopyHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TableCopyHelper.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:36:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:13:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBUI_TABLECOPYHELPER_HXX
 #include "TableCopyHelper.hxx"
 #endif
