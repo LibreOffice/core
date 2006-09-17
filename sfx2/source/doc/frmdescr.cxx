37a4b88ba4c871a246d258726926d4dbbeb132b7
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:41:16 +0000

    INTEGRATION: CWS pchfix02 (1.11.88); FILE MERGED 2006/09/01 17:38:40 kaib 1.11.88.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/frmdescr.cxx
+++ sfx2/source/doc/frmdescr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmdescr.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:27:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:41:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #include <sot/object.hxx>
 
 #ifndef _STREAM_HXX //autogen
@@ -45,7 +48,6 @@
 #include <svtools/itemset.hxx>
 #endif
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "frmdescr.hxx"
