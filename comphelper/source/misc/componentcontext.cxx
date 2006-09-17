266e99ee44761b620634c031401062fe87567b53
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:10:11 +0000

    INTEGRATION: CWS pchfix02 (1.2.26); FILE MERGED 2006/09/01 17:19:41 kaib 1.2.26.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/componentcontext.cxx
+++ comphelper/source/misc/componentcontext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: componentcontext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:10:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:10:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef COMPHELPER_COMPONENTCONTEXT_HXX
 #include <comphelper/componentcontext.hxx>
