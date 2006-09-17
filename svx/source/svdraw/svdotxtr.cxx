16ce99842fd3885cad941903de5fb031b4009d41
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:58:25 +0000

    INTEGRATION: CWS pchfix02 (1.11.114); FILE MERGED 2006/09/01 17:47:27 kaib 1.11.114.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdotxtr.cxx
+++ svx/source/svdraw/svdotxtr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdotxtr.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:45:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:58:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "svdotext.hxx"
 #include "xoutx.hxx"     // fuer XOutCreatePolygon
