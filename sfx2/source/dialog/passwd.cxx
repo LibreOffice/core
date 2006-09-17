d2928c7d608fe909e95fdb4204f73585e380e2c5
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:34:40 +0000

    INTEGRATION: CWS pchfix02 (1.4.312); FILE MERGED 2006/09/01 17:38:35 kaib 1.4.312.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/passwd.cxx
+++ sfx2/source/dialog/passwd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: passwd.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:28:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:34:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,15 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 // Include ---------------------------------------------------------------
 
 #ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
 #endif
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "passwd.hxx"
