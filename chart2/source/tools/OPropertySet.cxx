e3ec030c9be18644f6e44dcc354ef898abb5af4b
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:28:59 +0000

    INTEGRATION: CWS pchfix02 (1.3.80); FILE MERGED 2006/09/01 17:18:54 kaib 1.3.80.1: #i68856# Added header markers and pch files

--- chart2/source/tools/OPropertySet.cxx
+++ chart2/source/tools/OPropertySet.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OPropertySet.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:32:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:27:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "OPropertySet.hxx"
 #include "ImplOPropertySet.hxx"
 #include "algohelper.hxx"
