daabf5476dbfc15383b26213c4a4a01d7f69649a
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:47:40 +0000

    INTEGRATION: CWS pchfix02 (1.10.536); FILE MERGED 2006/09/01 17:46:36 kaib 1.10.536.1: #i68856# Added header markers and pch files

--- svx/source/editeng/SvXMLAutoCorrectImport.cxx
+++ svx/source/editeng/SvXMLAutoCorrectImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SvXMLAutoCorrectImport.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:24:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:47:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef _SV_XMLAUTOCORRECTIMPORT_HXX
 #include <SvXMLAutoCorrectImport.hxx>
 #endif
