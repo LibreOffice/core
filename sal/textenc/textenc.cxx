d9034f026093303b91ad47d17b6edad04103349b
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:08:50 +0000

    INTEGRATION: CWS pchfix02 (1.4.140); FILE MERGED 2006/09/01 17:34:21 kaib 1.4.140.1: #i68856# Added header markers and pch files

--- sal/textenc/textenc.cxx
+++ sal/textenc/textenc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textenc.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:46:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:08:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 /* TODO! This file should not be called textenc.c, because it is not the
    implementation of rtl/textenc.h.  Rather, it should be called
