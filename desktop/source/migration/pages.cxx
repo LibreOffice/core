57d30a71111b1f7dc531aa1573daef0ae656428c
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:45:43 +0000

    INTEGRATION: CWS pchfix02 (1.11.132); FILE MERGED 2006/09/01 17:25:16 kaib 1.11.132.1: #i68856# Added header markers and pch files

--- desktop/source/migration/pages.cxx
+++ desktop/source/migration/pages.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pages.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-06 12:36:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:45:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version .1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "pages.hxx"
 #include "wizard.hrc"
