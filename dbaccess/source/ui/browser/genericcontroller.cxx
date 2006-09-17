ae221296d8ec384b4236a9dd269f6d45c8a0f61a
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:58:43 +0000

    INTEGRATION: CWS pchfix02 (1.72.4); FILE MERGED 2006/09/01 17:24:22 kaib 1.72.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/browser/genericcontroller.cxx
+++ dbaccess/source/ui/browser/genericcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: genericcontroller.cxx,v $
  *
- *  $Revision: 1.72 $
+ *  $Revision: 1.73 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:49:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:58:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_GENERICCONTROLLER_HXX
 #include "genericcontroller.hxx"
 #endif
