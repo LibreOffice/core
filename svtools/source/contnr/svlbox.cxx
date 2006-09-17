a1aeaafe7036e4a94c808e6f5c7111e5e4dde5c0
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:34:48 +0000

    INTEGRATION: CWS pchfix02 (1.27.58); FILE MERGED 2006/09/01 17:42:54 kaib 1.27.58.1: #i68856# Added header markers and pch files

--- svtools/source/contnr/svlbox.cxx
+++ svtools/source/contnr/svlbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svlbox.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 12:06:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:34:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 /*
     Todo:
