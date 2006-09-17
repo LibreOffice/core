d90a5ca00e80b32d7d1eff9a067faf1a82f95ffd
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:09:08 +0000

    INTEGRATION: CWS pchfix02 (1.67.50); FILE MERGED 2006/09/01 17:46:51 kaib 1.67.50.1: #i68856# Added header markers and pch files

--- svx/source/form/fmshell.cxx
+++ svx/source/form/fmshell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmshell.cxx,v $
  *
- *  $Revision: 1.67 $
+ *  $Revision: 1.68 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:52:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:09:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SVX_FMVWIMP_HXX
 #include "fmvwimp.hxx"
