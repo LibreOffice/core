d6305f870fc4deb719a85ea41df63802ba584176
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:29:00 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 17:42:50 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- svtools/source/config/regoptions.cxx
+++ svtools/source/config/regoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: regoptions.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:48:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:29:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef SVTOOLS_REGOPTIONS_HXX
 #include "regoptions.hxx"
