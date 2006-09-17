43faec0854045377c0627d98285a1e22e0df8f90
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:53:18 +0000

    INTEGRATION: CWS pchfix02 (1.80.4); FILE MERGED 2006/09/01 17:25:33 kaib 1.80.4.1: #i68856# Added header markers and pch files

--- dtrans/source/X11/X11_selection.cxx
+++ dtrans/source/X11/X11_selection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: X11_selection.cxx,v $
  *
- *  $Revision: 1.80 $
+ *  $Revision: 1.81 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:42:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:53:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #include <unistd.h>
 #include <stdio.h>
