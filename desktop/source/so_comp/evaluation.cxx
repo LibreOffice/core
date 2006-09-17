0faae18393bda9d30976d3668b9ebaca51435fce
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:48:48 +0000

    INTEGRATION: CWS pchfix02 (1.7.222); FILE MERGED 2006/09/01 17:25:18 kaib 1.7.222.1: #i68856# Added header markers and pch files

--- desktop/source/so_comp/evaluation.cxx
+++ desktop/source/so_comp/evaluation.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: evaluation.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:47:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:48:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "evaluation.hxx"
 
