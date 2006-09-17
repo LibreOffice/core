65a108b939aa93b088004bf5be345e285d0e2eb4
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:17:03 +0000

    INTEGRATION: CWS pchfix02 (1.3.160); FILE MERGED 2006/09/01 17:24:33 kaib 1.3.160.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/documentcontroller.cxx
+++ dbaccess/source/ui/misc/documentcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentcontroller.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-19 11:52:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:17:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_SOURCE_UI_INC_DOCUMENTCONTROLLER_HXX
 #include "documentcontroller.hxx"
