6c0bd0206f26f788a618a4c91baf398fb05f1b52
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:32:58 +0000

    INTEGRATION: CWS pchfix02 (1.9.184); FILE MERGED 2006/09/01 17:24:43 kaib 1.9.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/uno/DBTypeWizDlgSetup.cxx
+++ dbaccess/source/ui/uno/DBTypeWizDlgSetup.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DBTypeWizDlgSetup.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:48:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:32:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBU_REGHELPER_HXX_
 #include "dbu_reghelper.hxx"
