a45ed83aaa9ba551280f763e5706c389dbbecdfb
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:44:38 +0000

    INTEGRATION: CWS pchfix02 (1.21.116); FILE MERGED 2006/09/01 17:46:27 kaib 1.21.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/tphatch.cxx
+++ svx/source/dialog/tphatch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tphatch.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:34:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:44:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
