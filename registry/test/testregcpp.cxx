6615855e7bf14b6ceb305a51820c98df407a6e1d
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:54:29 +0000

    INTEGRATION: CWS pchfix02 (1.7.24); FILE MERGED 2006/09/01 17:33:09 kaib 1.7.24.1: #i68856# Added header markers and pch files

--- registry/test/testregcpp.cxx
+++ registry/test/testregcpp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testregcpp.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:17:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:54:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_registry.hxx"
 
 #include <iostream>
 #include <stdio.h>
