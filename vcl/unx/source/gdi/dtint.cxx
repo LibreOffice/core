2936b5a1d6495c89d1cdc58f3112eb73c4b50160
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:37:22 +0000

    INTEGRATION: CWS pchfix02 (1.27.4); FILE MERGED 2006/09/01 17:58:07 kaib 1.27.4.1: #i68856# Added header markers and pch files

--- vcl/unx/source/gdi/dtint.cxx
+++ vcl/unx/source/gdi/dtint.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dtint.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:50:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:37:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <unistd.h>
 #include <sys/types.h>
