5892e4fea7b7cdf5c5a2c6896635f59bc0327d86
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:02:41 +0000

    INTEGRATION: CWS pchfix02 (1.12.10); FILE MERGED 2006/09/01 17:16:37 kaib 1.12.10.1: #i68856# Added header markers and pch files

--- basegfx/source/polygon/b2dpolypolygoncutter.cxx
+++ basegfx/source/polygon/b2dpolypolygoncutter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dpolypolygoncutter.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:44:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:02:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
