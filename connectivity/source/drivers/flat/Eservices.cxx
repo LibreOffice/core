254744c78f8479e3e87d5b27fe309d9a8524e55a
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:39:28 +0000

    INTEGRATION: CWS pchfix02 (1.6.60); FILE MERGED 2006/09/01 17:22:15 kaib 1.6.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/flat/Eservices.cxx
+++ connectivity/source/drivers/flat/Eservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Eservices.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:29:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:39:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FLAT_EDRIVER_HXX_
 #include "flat/EDriver.hxx"
