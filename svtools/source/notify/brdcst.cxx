ae06acba43ecd1b8740f1d8909bc6e5021e6f6a2
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:17:53 +0000

    INTEGRATION: CWS pchfix02 (1.5.92); FILE MERGED 2006/09/01 17:43:24 kaib 1.5.92.1: #i68856# Added header markers and pch files

--- svtools/source/notify/brdcst.cxx
+++ svtools/source/notify/brdcst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: brdcst.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:22:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:17:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _DEBUG_HXX
