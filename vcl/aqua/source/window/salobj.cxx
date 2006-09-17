0a83c36669da5735c71b8a7a1a0cd9a06a86a8c2
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:46:32 +0000

    INTEGRATION: CWS pchfix02 (1.8.378); FILE MERGED 2006/09/01 17:57:32 kaib 1.8.378.1: #i68856# Added header markers and pch files

--- vcl/aqua/source/window/salobj.cxx
+++ vcl/aqua/source/window/salobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salobj.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:45:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:46:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <string.h>
 
