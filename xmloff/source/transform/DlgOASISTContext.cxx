4726978182bf1563f26c8d8962f58f97bc4d2682
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:22:36 +0000

    INTEGRATION: CWS pchfix02 (1.3.158); FILE MERGED 2006/09/01 18:00:14 kaib 1.3.158.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/DlgOASISTContext.cxx
+++ xmloff/source/transform/DlgOASISTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DlgOASISTContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:40:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:22:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_DLGOASISTCONTEXT_HXX
 #include "DlgOASISTContext.hxx"
