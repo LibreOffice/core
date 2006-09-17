b83a3fdba6b7b6b4dcf9f45005278639ed78be2d
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:13:36 +0000

    INTEGRATION: CWS pchfix02 (1.11.16); FILE MERGED 2006/09/01 17:30:38 kaib 1.11.16.1: #i68856# Added header markers and pch files

--- i18npool/source/breakiterator/breakiterator_ctl.cxx
+++ i18npool/source/breakiterator/breakiterator_ctl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: breakiterator_ctl.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:41:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:13:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <com/sun/star/i18n/CharacterIteratorMode.hpp>
 #include <breakiterator_ctl.hxx>
