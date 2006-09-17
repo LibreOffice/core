59d8deec83b5846c8523527df4cbef19fa100555
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:43:44 +0000

    INTEGRATION: CWS pchfix02 (1.3.36); FILE MERGED 2006/09/01 17:24:11 kaib 1.3.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/misc/warnings.cxx
+++ dbaccess/source/core/misc/warnings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: warnings.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:17:32 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:43:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBA_CORE_WARNINGS_HXX
 #include "warnings.hxx"
