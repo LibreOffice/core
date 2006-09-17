6d03037a889163a1f9e1f751051d4654e63639d9
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:37:09 +0000

    INTEGRATION: CWS pchfix02 (1.24.36); FILE MERGED 2006/09/01 17:24:07 kaib 1.24.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/viewcontainer.cxx
+++ dbaccess/source/core/api/viewcontainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontainer.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:07:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:37:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_CORE_VIEWCONTAINER_HXX_
 #include "viewcontainer.hxx"
