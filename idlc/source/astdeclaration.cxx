5c93e9bbd0032b96aa4a9a536e144bd56c435be5
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:11:14 +0000

    INTEGRATION: CWS pchfix02 (1.9.8); FILE MERGED 2006/09/01 17:31:12 kaib 1.9.8.1: #i68856# Added header markers and pch files

--- idlc/source/astdeclaration.cxx
+++ idlc/source/astdeclaration.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: astdeclaration.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:46:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:11:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 #ifndef _IDLC_ASTDECLARATION_HXX_
 #include <idlc/astdeclaration.hxx>
 #endif
