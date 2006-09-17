a5f4bbd1086c93f09fd0c40a294c64415a597ed2
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:52:31 +0000

    INTEGRATION: CWS pchfix02 (1.3.20); FILE MERGED 2006/09/01 17:55:44 kaib 1.3.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/ftp/ftpintreq.cxx
+++ ucb/source/ucp/ftp/ftpintreq.cxx
@@ -1,3 +1,41 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ftpintreq.cxx,v $
+ *
+ *  $Revision: 1.4 $
+ *
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:51:29 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 #include <com/sun/star/ucb/UnsupportedNameClashException.hpp>
 #include <com/sun/star/ucb/NameClash.hpp>
 #include "ftpintreq.hxx"
