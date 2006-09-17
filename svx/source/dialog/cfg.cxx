6f4b075c142f242c939c493d4abf9a7c935b0432
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:11:12 +0000

    INTEGRATION: CWS pchfix02 (1.34.16); FILE MERGED 2006/09/01 17:46:04 kaib 1.34.16.1: #i68856# Added header markers and pch files

--- svx/source/dialog/cfg.cxx
+++ svx/source/dialog/cfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfg.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 11:10:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:11:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _HELP_HXX //autogen
 #include <vcl/help.hxx>
