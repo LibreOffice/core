2fb9c74087b4c5b59727a9500674eed7eb1a2602
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:56:25 +0000

    INTEGRATION: CWS pchfix02 (1.27.16); FILE MERGED 2006/09/01 17:16:58 kaib 1.27.16.1: #i68856# Added header markers and pch files

--- basic/source/app/dialogs.cxx
+++ basic/source/app/dialogs.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dialogs.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:32:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:56:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 #ifndef _SOLAR_H
 #include <tools/solar.h>
