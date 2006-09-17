c1a1cd814b90ee863849878dd0d9929e6a625663
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:21:44 +0000

    INTEGRATION: CWS pchfix02 (1.8.76); FILE MERGED 2006/09/01 17:57:56 kaib 1.8.76.1: #i68856# Added header markers and pch files

--- vcl/source/window/syschild.cxx
+++ vcl/source/window/syschild.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: syschild.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 16:36:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:21:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_SVSYS_HXX
 #include <svsys.h>
