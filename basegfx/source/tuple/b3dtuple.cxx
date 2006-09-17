a539938662cedd5d81409ea2df56fcc162cbe794
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:07:09 +0000

    INTEGRATION: CWS pchfix02 (1.8.26); FILE MERGED 2006/09/01 17:16:40 kaib 1.8.26.1: #i68856# Added header markers and pch files

--- basegfx/source/tuple/b3dtuple.cxx
+++ basegfx/source/tuple/b3dtuple.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dtuple.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 14:00:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:07:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_TUPLE_B3DTUPLE_HXX
 #include <basegfx/tuple/b3dtuple.hxx>
