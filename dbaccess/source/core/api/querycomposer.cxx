49b8eff1b23d3022f4feaeea8b3bc436391ca334
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:35:16 +0000

    INTEGRATION: CWS pchfix02 (1.65.4); FILE MERGED 2006/09/01 17:24:06 kaib 1.65.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/querycomposer.cxx
+++ dbaccess/source/core/api/querycomposer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: querycomposer.cxx,v $
  *
- *  $Revision: 1.65 $
+ *  $Revision: 1.66 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:43:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:35:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _CONNECTIVITY_COMMONTOOLS_HXX_
 #include "querycomposer.hxx"
