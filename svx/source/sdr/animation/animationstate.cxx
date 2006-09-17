5fae2a82bacb84845684f20d8dafdcf5189f38bb
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:33:24 +0000

    INTEGRATION: CWS pchfix02 (1.4.536); FILE MERGED 2006/09/01 17:47:09 kaib 1.4.536.1: #i68856# Added header markers and pch files

--- svx/source/sdr/animation/animationstate.cxx
+++ svx/source/sdr/animation/animationstate.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: animationstate.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:01:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:33:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_ANIMATION_ANIMATIONSTATE_HXX
 #include <svx/sdr/animation/animationstate.hxx>
