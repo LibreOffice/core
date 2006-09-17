1d66e5abcb609363671df0000e79e65d7bedc7f1
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:54:00 +0000

    INTEGRATION: CWS pchfix02 (1.12.112); FILE MERGED 2006/09/01 17:57:38 kaib 1.12.112.1: #i68856# Added header markers and pch files

--- vcl/source/control/longcurr.cxx
+++ vcl/source/control/longcurr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: longcurr.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:17:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:54:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <sot/object.hxx>
 #define _TOOLS_BIGINT
