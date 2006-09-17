2e5c72cb2a9fbd51eb4e561005744b51cb694a37
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:00:34 +0000

    INTEGRATION: CWS pchfix02 (1.41.6); FILE MERGED 2006/09/01 17:17:01 kaib 1.41.6.1: #i68856# Added header markers and pch files

--- basic/source/classes/sbunoobj.cxx
+++ basic/source/classes/sbunoobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbunoobj.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 10:53:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:00:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 //#include <stl_queue.h>
 
 #ifndef _VOS_MUTEX_HXX_ //autogen
