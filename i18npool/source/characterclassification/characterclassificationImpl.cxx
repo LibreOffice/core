2176dc1a7dd9e67ab6212d9ac18fd2f376b60c8c
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:16:21 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:30:41 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- i18npool/source/characterclassification/characterclassificationImpl.cxx
+++ i18npool/source/characterclassification/characterclassificationImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: characterclassificationImpl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:43:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:16:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <characterclassificationImpl.hxx>
 #ifndef _RTL_USTRBUF_HXX_
