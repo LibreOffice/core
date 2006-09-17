65fa970fca32964adb84dfffade5d4c584cef5aa
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:09:16 +0000

    INTEGRATION: CWS pchfix02 (1.12.36); FILE MERGED 2006/09/01 17:19:40 kaib 1.12.36.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/accessiblewrapper.cxx
+++ comphelper/source/misc/accessiblewrapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accessiblewrapper.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:47:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:09:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef COMPHELPER_ACCESSIBLE_WRAPPER_HXX
 #include "comphelper/accessiblewrapper.hxx"
