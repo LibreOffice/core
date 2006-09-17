736efd77b40412d94d1be913f3699d2467995557
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:03:32 +0000

    INTEGRATION: CWS pchfix02 (1.25.116); FILE MERGED 2006/09/01 17:47:31 kaib 1.25.116.1: #i68856# Added header markers and pch files

--- svx/source/svrtf/svxrtf.cxx
+++ svx/source/svrtf/svxrtf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxrtf.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:49:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:03:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
