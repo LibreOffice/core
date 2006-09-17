1fae2198aa9f5fd0dc59aae76b7e3fbe05781cb2
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:59:25 +0000

    INTEGRATION: CWS pchfix02 (1.25.24); FILE MERGED 2006/09/01 17:17:00 kaib 1.25.24.1: #i68856# Added header markers and pch files

--- basic/source/classes/eventatt.cxx
+++ basic/source/classes/eventatt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eventatt.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:39:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:59:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 //#include <stl_queue.h>
 
