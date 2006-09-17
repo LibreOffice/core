a0505feecbe6965fbf026470154058e922aa75f0
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:05:38 +0000

    INTEGRATION: CWS pchfix02 (1.21.6); FILE MERGED 2006/09/01 17:21:50 kaib 1.21.6.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/adabas/BConnection.cxx
+++ connectivity/source/drivers/adabas/BConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BConnection.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:27:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:05:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
