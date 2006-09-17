dfc277380d16d2e0327b5442ff0d1c041e3929a6
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:11:24 +0000

    INTEGRATION: CWS pchfix02 (1.6.16); FILE MERGED 2006/09/01 17:40:30 kaib 1.6.16.1: #i68856# Added header markers and pch files

--- sot/source/sdstor/unostorageholder.cxx
+++ sot/source/sdstor/unostorageholder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unostorageholder.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:56:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:11:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
 #include <com/sun/star/uno/Reference.hxx>
