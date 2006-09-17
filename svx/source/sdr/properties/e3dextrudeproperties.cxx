7bc31567b7dd9e4850eea593385e20eddcd0e064
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:42:07 +0000

    INTEGRATION: CWS pchfix02 (1.4.536); FILE MERGED 2006/09/01 17:47:15 kaib 1.4.536.1: #i68856# Added header markers and pch files

--- svx/source/sdr/properties/e3dextrudeproperties.cxx
+++ svx/source/sdr/properties/e3dextrudeproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: e3dextrudeproperties.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:12:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:41:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_PROPERTIES_E3DEXTRUDEPROPERTIES_HXX
 #include <svx/sdr/properties/e3dextrudeproperties.hxx>
--- svx/source/sdr/properties/e3dproperties.cxx
+++ svx/source/sdr/properties/e3dproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: e3dproperties.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:12:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:42:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_PROPERTIES_E3DPROPERTIES_HXX
 #include <svx/sdr/properties/e3dproperties.hxx>
