97200a4654f81b48688449da29c6931e19d498e1
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:47:00 +0000

    INTEGRATION: CWS pchfix02 (1.3.160); FILE MERGED 2006/09/01 17:59:48 kaib 1.3.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/SinglePropertySetInfoCache.cxx
+++ xmloff/source/style/SinglePropertySetInfoCache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SinglePropertySetInfoCache.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:29:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:46:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
 #include <com/sun/star/lang/XTypeProvider.hpp>
--- xmloff/source/style/StyleMap.cxx
+++ xmloff/source/style/StyleMap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: StyleMap.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:30:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:47:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
