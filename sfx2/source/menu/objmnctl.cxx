c678d05455080fd877bfe7058ad8de0e0911e290
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:47:03 +0000

    INTEGRATION: CWS pchfix02 (1.5.88); FILE MERGED 2006/09/01 17:38:44 kaib 1.5.88.1: #i68856# Added header markers and pch files

--- sfx2/source/menu/objmnctl.cxx
+++ sfx2/source/menu/objmnctl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objmnctl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:36:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:47:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #ifndef _COM_SUN_STAR_EMBED_VERBDESCRIPTOR_HPP_
 #include <com/sun/star/embed/VerbDescriptor.hpp>
 #endif
@@ -48,7 +51,6 @@
 #include <svtools/stritem.hxx>
 #endif
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "sfxsids.hrc"
