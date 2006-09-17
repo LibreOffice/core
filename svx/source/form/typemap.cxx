92b53ee1610a7688885e48b1cb4c4f0700c86e46
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:14:39 +0000

    INTEGRATION: CWS pchfix02 (1.4.536); FILE MERGED 2006/09/01 17:46:55 kaib 1.4.536.1: #i68856# Added header markers and pch files

--- svx/source/form/typemap.cxx
+++ svx/source/form/typemap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typemap.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:04:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:14:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SFX_OBJITEM_HXX //autogen
 #include <sfx2/objitem.hxx>
