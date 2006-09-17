32133ebf3abdc2f30f543ed5e7bdaab3fba3f9e7
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:02:03 +0000

    INTEGRATION: CWS pchfix02 (1.10.60); FILE MERGED 2006/09/01 17:21:47 kaib 1.10.60.1: #i68856# Added header markers and pch files

--- connectivity/source/commontools/dbcharset.cxx
+++ connectivity/source/commontools/dbcharset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbcharset.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:05:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:02:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _DBHELPER_DBCHARSET_HXX_
 #include <connectivity/dbcharset.hxx>
