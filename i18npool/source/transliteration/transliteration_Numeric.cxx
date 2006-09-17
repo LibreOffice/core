e18f0b3ed914426686f776577322f6463572066d
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:32:20 +0000

    INTEGRATION: CWS pchfix02 (1.6.16); FILE MERGED 2006/09/01 17:30:55 kaib 1.6.16.1: #i68856# Added header markers and pch files

--- i18npool/source/transliteration/transliteration_Numeric.cxx
+++ i18npool/source/transliteration/transliteration_Numeric.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: transliteration_Numeric.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:50:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:32:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <transliteration_Numeric.hxx>
 #include <nativenumbersupplier.hxx>
