c4e435b609db9098ee5353376419fb9bba200fc9
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:36:25 +0000

    INTEGRATION: CWS pchfix02 (1.19.190); FILE MERGED 2006/09/01 17:58:06 kaib 1.19.190.1: #i68856# Added header markers and pch files

--- vcl/unx/source/app/soicon.cxx
+++ vcl/unx/source/app/soicon.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: soicon.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-29 11:27:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:36:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <salunx.h>
 
