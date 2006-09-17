c39740e6aea937287a4bc064fa44f29c2a4c178f
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:35:01 +0000

    INTEGRATION: CWS pchfix02 (1.10.114); FILE MERGED 2006/09/01 17:47:11 kaib 1.10.114.1: #i68856# Added header markers and pch files

--- svx/source/sdr/contact/viewcontact.cxx
+++ svx/source/sdr/contact/viewcontact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontact.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:29:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:35:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_CONTACT_VIEWCONTACT_HXX
 #include <svx/sdr/contact/viewcontact.hxx>
