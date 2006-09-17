934ab16f34ffb6075058d27db7599abe4d04a3b8
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:09:56 +0000

    INTEGRATION: CWS pchfix02 (1.7.22); FILE MERGED 2006/09/01 17:19:41 kaib 1.7.22.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/asyncnotification.cxx
+++ comphelper/source/misc/asyncnotification.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: asyncnotification.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 15:24:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:09:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef COMPHELPER_ASYNCNOTIFICATION_HXX
 #include <comphelper/asyncnotification.hxx>
