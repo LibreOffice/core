87bb6e5dbeccb268edb8f535a7cd08ae727812c6
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:36:39 +0000

    INTEGRATION: CWS pchfix02 (1.2.6); FILE MERGED 2006/09/01 17:39:02 kaib 1.2.6.1: #i68856# Added header markers and pch files

--- shell/source/backends/desktopbe/desktopbecdef.cxx
+++ shell/source/backends/desktopbe/desktopbecdef.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: desktopbecdef.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 12:28:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:36:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSCHEMASUPPLIER_HPP_
 #include <com/sun/star/configuration/backend/XSingleLayerStratum.hpp>
