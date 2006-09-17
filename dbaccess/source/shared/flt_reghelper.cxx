27c5148becf75d8d984f7d04843f500319cc9c71
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:53:27 +0000

    INTEGRATION: CWS pchfix02 (1.3.184); FILE MERGED 2006/09/01 17:24:18 kaib 1.3.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/shared/flt_reghelper.cxx
+++ dbaccess/source/shared/flt_reghelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: flt_reghelper.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:16:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:52:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _FLT_REGHELPER_HXX_
 #include "flt_reghelper.hxx"
