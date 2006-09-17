bddc82b08455c19f856c9c54e6fdf979cc5766b3
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:04:29 +0000

    INTEGRATION: CWS pchfix02 (1.7.42); FILE MERGED 2006/09/01 17:22:31 kaib 1.7.42.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/mysql/YUsers.cxx
+++ connectivity/source/drivers/mysql/YUsers.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: YUsers.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:31:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:04:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_MYSQL_USERS_HXX_
 #include "mysql/YUsers.hxx"
