f3e2acadb9d81fd75f813f57e09b0b58614375b5
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:36:42 +0000

    INTEGRATION: CWS pchfix02 (1.13.116); FILE MERGED 2006/09/01 17:47:12 kaib 1.13.116.1: #i68856# Added header markers and pch files

--- svx/source/sdr/contact/viewcontactofsdrobj.cxx
+++ svx/source/sdr/contact/viewcontactofsdrobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontactofsdrobj.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:28:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:36:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_CONTACT_VIEWCONTACTOFSDROBJ_HXX
 #include <svx/sdr/contact/viewcontactofsdrobj.hxx>
