036580d0308c0dff93c7c24bb2697c5eb4f61c05
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:40:21 +0000

    INTEGRATION: CWS pchfix02 (1.5.50); FILE MERGED 2006/09/01 17:39:41 kaib 1.5.50.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/transitions/parametricpolypolygonfactory.cxx
+++ slideshow/source/engine/transitions/parametricpolypolygonfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parametricpolypolygonfactory.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:55:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:40:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 #include <canvas/debug.hxx>
 #include <parametricpolypolygonfactory.hxx>
