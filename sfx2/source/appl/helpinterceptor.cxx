45a4f53f3d2480007ae613c363b31dfe1fb47bb7
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:17:50 +0000

    INTEGRATION: CWS pchfix02 (1.25.88); FILE MERGED 2006/09/01 17:38:23 kaib 1.25.88.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/helpinterceptor.cxx
+++ sfx2/source/appl/helpinterceptor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helpinterceptor.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:09:35 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:17:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include "helpinterceptor.hxx"
 #include "helpdispatch.hxx"
