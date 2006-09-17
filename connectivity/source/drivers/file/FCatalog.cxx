1bc7cee0ba12f6fa48250051bd8b9cf2a3d10ad8
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:31:31 +0000

    INTEGRATION: CWS pchfix02 (1.14.166); FILE MERGED 2006/09/01 17:22:10 kaib 1.14.166.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/file/FCatalog.cxx
+++ connectivity/source/drivers/file/FCatalog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FCatalog.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:54:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:31:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FILE_CATALOG_HXX_
 #include "file/FCatalog.hxx"
