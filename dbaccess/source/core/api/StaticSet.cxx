99cdebdd50e062d694e322deaedf8e39984bfe6e
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:33:12 +0000

    INTEGRATION: CWS pchfix02 (1.17.36); FILE MERGED 2006/09/01 17:24:04 kaib 1.17.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/StaticSet.cxx
+++ dbaccess/source/core/api/StaticSet.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: StaticSet.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:04:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:33:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_CORE_API_STATICSET_HXX
 #include "StaticSet.hxx"
