07abb5efd139709e82fe0b4ab1481bbd13f75979
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:19:00 +0000

    INTEGRATION: CWS pchfix02 (1.55.34); FILE MERGED 2006/09/01 18:00:11 kaib 1.55.34.1: #i68856# Added header markers and pch files

--- xmloff/source/text/txtparai.cxx
+++ xmloff/source/text/txtparai.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtparai.cxx,v $
  *
- *  $Revision: 1.55 $
+ *  $Revision: 1.56 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:50:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:19:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef __COMPHELPER_UNOINTERFACETOUNIQUEIDENTIFIERMAPPER__
 #include "unointerfacetouniqueidentifiermapper.hxx"
