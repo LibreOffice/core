ccb5ce8ad5ee84670f06f642fcb2dda586c12264
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:27:48 +0000

    INTEGRATION: CWS pchfix02 (1.11.12); FILE MERGED 2006/09/01 17:32:42 kaib 1.11.12.1: #i68856# Added header markers and pch files

--- package/source/zipapi/XUnbufferedStream.cxx
+++ package/source/zipapi/XUnbufferedStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XUnbufferedStream.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:13:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:27:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 #ifndef _XUNBUFFERED_STREAM_HXX
 #include <XUnbufferedStream.hxx>
 #endif
