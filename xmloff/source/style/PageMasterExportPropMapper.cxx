2bb0f06f480ceda229dd167961a5e592318518c8
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:44:54 +0000

    INTEGRATION: CWS pchfix02 (1.17.34); FILE MERGED 2006/09/01 17:59:47 kaib 1.17.34.1: #i68856# Added header markers and pch files

--- xmloff/source/style/PageMasterExportPropMapper.cxx
+++ xmloff/source/style/PageMasterExportPropMapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageMasterExportPropMapper.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:00:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:44:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PAGEMASTEREXPORTPROPMAPPER_HXX
 #include "PageMasterExportPropMapper.hxx"
