d16d946f49c0d559b60455b27ddb64d3525c02cd
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:15:48 +0000

    INTEGRATION: CWS pchfix02 (1.51.116); FILE MERGED 2006/09/01 17:46:56 kaib 1.51.116.1: #i68856# Added header markers and pch files

--- svx/source/gallery2/galbrws2.cxx
+++ svx/source/gallery2/galbrws2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: galbrws2.cxx,v $
  *
- *  $Revision: 1.51 $
+ *  $Revision: 1.52 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:01:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:15:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define ITEMID_BRUSH 0
 
