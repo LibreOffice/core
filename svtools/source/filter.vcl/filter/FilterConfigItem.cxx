154df47a5b61e882438bab6e5ce01b260f8efbf3
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:50:37 +0000

    INTEGRATION: CWS pchfix02 (1.13.90); FILE MERGED 2006/09/01 17:43:05 kaib 1.13.90.1: #i68856# Added header markers and pch files

--- svtools/source/filter.vcl/filter/FilterConfigItem.cxx
+++ svtools/source/filter.vcl/filter/FilterConfigItem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FilterConfigItem.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:04:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:50:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _FILTER_CONFIG_ITEM_HXX_
 #include "FilterConfigItem.hxx"
