9d4fd2838a1d0390a34868709393b338eff95dfd
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:47:24 +0000

    INTEGRATION: CWS pchfix02 (1.6.194); FILE MERGED 2006/09/01 17:27:15 kaib 1.6.194.1: #i68856# Added header markers and pch files

--- filter/source/xsltdialog/typedetectionexport.cxx
+++ filter/source/xsltdialog/typedetectionexport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: typedetectionexport.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:15:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:47:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #ifndef _COM_SUN_STAR_XML_SAX_XATTRIBUTELIST_HXX
 #include <com/sun/star/xml/sax/XAttributeList.hpp>
