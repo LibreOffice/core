824697173084deeeecc32641c6d03186b65208e0
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:54:25 +0000

    INTEGRATION: CWS pchfix02 (1.6.34); FILE MERGED 2006/09/01 17:59:53 kaib 1.6.34.1: #i68856# Added header markers and pch files

--- xmloff/source/style/chrhghdl.cxx
+++ xmloff/source/style/chrhghdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chrhghdl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:01:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:52:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 
 #ifndef _XMLOFF_PROPERTYHANDLER_CHARHEIGHTTYPES_HXX
--- xmloff/source/style/csmaphdl.cxx
+++ xmloff/source/style/csmaphdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: csmaphdl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:01:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:53:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PROPERTYHANDLER_CASEMAPTYPES_HXX
 #include <csmaphdl.hxx>
