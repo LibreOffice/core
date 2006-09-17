0eceb3c0bf3acf522e18e84e42883c97bce2d5e8
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:26:55 +0000

    INTEGRATION: CWS pchfix02 (1.105.36); FILE MERGED 2006/09/01 17:24:39 kaib 1.105.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/querycontroller.cxx
+++ dbaccess/source/ui/querydesign/querycontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: querycontroller.cxx,v $
  *
- *  $Revision: 1.105 $
+ *  $Revision: 1.106 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:45:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:26:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_QUERYCONTROLLER_HXX
 #include "querycontroller.hxx"
 #endif
