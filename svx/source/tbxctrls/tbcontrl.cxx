cd4213b23cb7db5a015f10b6f8461717b8f5b1fd
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:06:36 +0000

    INTEGRATION: CWS pchfix02 (1.71.74); FILE MERGED 2006/09/01 17:47:33 kaib 1.71.74.1: #i68856# Added header markers and pch files

--- svx/source/tbxctrls/tbcontrl.cxx
+++ svx/source/tbxctrls/tbcontrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tbcontrl.cxx,v $
  *
- *  $Revision: 1.71 $
+ *  $Revision: 1.72 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 16:03:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:06:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // include ---------------------------------------------------------------
 
