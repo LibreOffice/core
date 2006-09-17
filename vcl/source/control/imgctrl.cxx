daceaf76a9c17cb20b24f100358dde49dc24b88e
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:53:48 +0000

    INTEGRATION: CWS pchfix02 (1.9.378); FILE MERGED 2006/09/01 17:57:37 kaib 1.9.378.1: #i68856# Added header markers and pch files

--- vcl/source/control/imgctrl.cxx
+++ vcl/source/control/imgctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imgctrl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:47:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:53:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _SV_EVENT_HXX
 #include <event.hxx>
