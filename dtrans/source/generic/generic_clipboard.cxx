41dd4f4ac46d9b2271d0f4537f01ccb713274643
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:56:39 +0000

    INTEGRATION: CWS pchfix02 (1.5.16); FILE MERGED 2006/09/01 17:25:35 kaib 1.5.16.1: #i68856# Added header markers and pch files

--- dtrans/source/generic/generic_clipboard.cxx
+++ dtrans/source/generic/generic_clipboard.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: generic_clipboard.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 06:00:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:56:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #include <generic_clipboard.hxx>
 
