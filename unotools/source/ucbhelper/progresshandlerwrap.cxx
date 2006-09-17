ceaf93f502b2609c66c1dbb6b55b93833633aea1
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:29:40 +0000

    INTEGRATION: CWS pchfix02 (1.3.58); FILE MERGED 2006/09/01 17:56:26 kaib 1.3.58.1: #i68856# Added header markers and pch files

--- unotools/source/ucbhelper/progresshandlerwrap.cxx
+++ unotools/source/ucbhelper/progresshandlerwrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: progresshandlerwrap.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:51:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:29:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef _UTL_PROGRESSHANDLERWRAP_HXX_
 #include <unotools/progresshandlerwrap.hxx>
