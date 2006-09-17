47093e704b7d0796459e9be2c3d0eedc2fd5769d
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:54:50 +0000

    INTEGRATION: CWS pchfix02 (1.65.8); FILE MERGED 2006/09/01 17:16:57 kaib 1.65.8.1: #i68856# Added header markers and pch files

--- basic/source/app/app.cxx
+++ basic/source/app/app.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: app.cxx,v $
  *
- *  $Revision: 1.65 $
+ *  $Revision: 1.66 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 10:24:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:54:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
