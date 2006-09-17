f6632cbbca1bb911492353cbc65319dc8cdec22e
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:47:31 +0000

    INTEGRATION: CWS pchfix02 (1.31.88); FILE MERGED 2006/09/01 17:38:45 kaib 1.31.88.1: #i68856# Added header markers and pch files

--- sfx2/source/notify/eventsupplier.cxx
+++ sfx2/source/notify/eventsupplier.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eventsupplier.cxx,v $
  *
- *  $Revision: 1.31 $
+ *  $Revision: 1.32 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:36:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:47:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 //--------------------------------------------------------------------------------------------------------
 
