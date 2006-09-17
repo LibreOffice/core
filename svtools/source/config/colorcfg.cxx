3eb213a2f268e41d4a5afce99b6e4a5ee9b86c73
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:21:53 +0000

    INTEGRATION: CWS pchfix02 (1.12.44); FILE MERGED 2006/09/01 17:42:45 kaib 1.12.44.1: #i68856# Added header markers and pch files

--- svtools/source/config/colorcfg.cxx
+++ svtools/source/config/colorcfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: colorcfg.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:05:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:21:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifdef  SVL_DLLIMPLEMENTATION
 #undef  SVL_DLLIMPLEMENTATION
