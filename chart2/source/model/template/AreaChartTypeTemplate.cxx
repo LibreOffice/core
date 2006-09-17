9cf500dd0a4a2e636abf180e8595594cb8379741
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:16:43 +0000

    INTEGRATION: CWS pchfix02 (1.5.80); FILE MERGED 2006/09/01 17:18:46 kaib 1.5.80.1: #i68856# Added header markers and pch files

--- chart2/source/model/template/AreaChartTypeTemplate.cxx
+++ chart2/source/model/template/AreaChartTypeTemplate.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AreaChartTypeTemplate.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:09:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:16:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "AreaChartTypeTemplate.hxx"
 #include "AreaChartType.hxx"
 #include "macros.hxx"
