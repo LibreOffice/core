aae71d8855c81ca4bfedd7fe6720b75009439455
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:18:58 +0000

    INTEGRATION: CWS pchfix02 (1.6.86); FILE MERGED 2006/09/01 17:38:24 kaib 1.6.86.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/linksrc.cxx
+++ sfx2/source/appl/linksrc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: linksrc.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:10:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:18:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 
 #include "linksrc.hxx"
