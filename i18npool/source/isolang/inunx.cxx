c440c28ba0e368c4720f0179d1783044f85263b1
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:20:37 +0000

    INTEGRATION: CWS pchfix02 (1.3.38); FILE MERGED 2006/09/01 17:30:45 kaib 1.3.38.1: #i68856# Added header markers and pch files

--- i18npool/source/isolang/inunx.cxx
+++ i18npool/source/isolang/inunx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inunx.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-12 10:51:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:20:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <stdlib.h>  // for getenv()
 #include <stdio.h>
