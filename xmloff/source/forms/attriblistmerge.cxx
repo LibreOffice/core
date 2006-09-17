4e99dbd6c0941c436b556e2ec791a966fe4858b2
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:32:55 +0000

    INTEGRATION: CWS pchfix02 (1.4.16); FILE MERGED 2006/09/01 17:59:38 kaib 1.4.16.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/attriblistmerge.cxx
+++ xmloff/source/forms/attriblistmerge.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: attriblistmerge.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:21:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:32:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_FORMS_ATTRIBLISTMERGE_HXX_
 #include "attriblistmerge.hxx"
