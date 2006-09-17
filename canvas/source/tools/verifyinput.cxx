094c3d71501bfd5d9521e20e2db099af58873e5b
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:28:25 +0000

    INTEGRATION: CWS pchfix02 (1.3.20); FILE MERGED 2006/09/01 17:18:07 kaib 1.3.20.1: #i68856# Added header markers and pch files

--- canvas/source/tools/verifyinput.cxx
+++ canvas/source/tools/verifyinput.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: verifyinput.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:19:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:28:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_canvas.hxx"
 
 #include <com/sun/star/geometry/AffineMatrix2D.hpp>
 #include <com/sun/star/geometry/Matrix2D.hpp>
