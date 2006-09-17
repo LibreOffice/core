94ec7504f4d7e0479817331f81b5d3ac7ff16745
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:54:45 +0000

    INTEGRATION: CWS pchfix02 (1.17.50); FILE MERGED 2006/09/01 17:24:19 kaib 1.17.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/app/AppDetailView.cxx
+++ dbaccess/source/ui/app/AppDetailView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AppDetailView.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:06:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:54:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_APPDETAILVIEW_HXX
 #include "AppDetailView.hxx"
 #endif
