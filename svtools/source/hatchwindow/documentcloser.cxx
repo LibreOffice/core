933d39192a172c045d408fabb3c8ec6c323e93b9
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:57:13 +0000

    INTEGRATION: CWS pchfix02 (1.3.92); FILE MERGED 2006/09/01 17:43:11 kaib 1.3.92.1: #i68856# Added header markers and pch files

--- svtools/source/hatchwindow/documentcloser.cxx
+++ svtools/source/hatchwindow/documentcloser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentcloser.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:09:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:57:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_UTIL_XCLOSEBROADCASTER_HPP_
 #include <com/sun/star/util/XCloseBroadcaster.hpp>
