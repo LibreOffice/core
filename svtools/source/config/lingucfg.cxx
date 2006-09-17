960a3c01a8d8be4435411018c6ce74aa7e33106f
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:26:57 +0000

    INTEGRATION: CWS pchfix02 (1.6.92); FILE MERGED 2006/09/01 17:42:48 kaib 1.6.92.1: #i68856# Added header markers and pch files

--- svtools/source/config/lingucfg.cxx
+++ svtools/source/config/lingucfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lingucfg.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:11:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:26:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 
 #ifndef _COM_SUN_STAR_LANG_LOCALE_HPP_
