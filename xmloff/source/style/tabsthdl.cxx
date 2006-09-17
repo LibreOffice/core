9032881e0563826809be4b49df5d3572011520d7
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:57:30 +0000

    INTEGRATION: CWS pchfix02 (1.4.34); FILE MERGED 2006/09/01 17:59:55 kaib 1.4.34.1: #i68856# Added header markers and pch files

--- xmloff/source/style/tabsthdl.cxx
+++ xmloff/source/style/tabsthdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabsthdl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:35:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:57:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PROPERTYHANDLER_TABSTOPTYPES_HXX
 #include <tabsthdl.hxx>
