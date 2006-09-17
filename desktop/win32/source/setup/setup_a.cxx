2797906139ef550744e41570e8ee58b811887f1e
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:54:02 +0000

    INTEGRATION: CWS pchfix02 (1.3.222); FILE MERGED 2006/09/01 17:25:22 kaib 1.3.222.1: #i68856# Added header markers and pch files

--- desktop/win32/source/setup/setup_a.cxx
+++ desktop/win32/source/setup/setup_a.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: setup_a.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:54:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:53:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,5 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "setup.cpp"
