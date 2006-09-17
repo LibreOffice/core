13a0bdd44023751934c62e48a823bf7b7c4815e1
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:12:04 +0000

    INTEGRATION: CWS pchfix02 (1.6.28); FILE MERGED 2006/09/01 17:41:31 kaib 1.6.28.1: #i68856# Added header markers and pch files

--- store/source/storcach.cxx
+++ store/source/storcach.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: storcach.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:44:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:12:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,10 @@
  *
  ************************************************************************/
 
-#define _STORE_STORCACH_CXX "$Revision: 1.6 $"
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_store.hxx"
+
+#define _STORE_STORCACH_CXX "$Revision: 1.7 $"
 
 #ifndef _SAL_TYPES_H_
 #include <sal/types.h>
