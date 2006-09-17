bdf92f6356e38f3a28b9835f7e48528f231da10c
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:29:28 +0000

    INTEGRATION: CWS pchfix02 (1.18.36); FILE MERGED 2006/09/01 17:24:02 kaib 1.18.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/BookmarkSet.cxx
+++ dbaccess/source/core/api/BookmarkSet.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BookmarkSet.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:00:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:29:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBACCESS_CORE_API_BOOKMARKSET_HXX
 #include "BookmarkSet.hxx"
 #endif
