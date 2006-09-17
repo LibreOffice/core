0ca5371d813f8c681ec26c6601053565f8b48c3f
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:33:41 +0000

    INTEGRATION: CWS pchfix02 (1.5.40); FILE MERGED 2006/09/01 17:19:19 kaib 1.5.40.1: #i68856# Added header markers and pch files

--- codemaker/source/bonobowrappermaker/corbatype.cxx
+++ codemaker/source/bonobowrappermaker/corbatype.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: corbatype.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:08:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:33:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_codemaker.hxx"
 
 #include <stdio.h>
 #ifndef _RTL_ALLOC_H_
