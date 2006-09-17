b0ce80edb01c1d74709c56070d13ba93a2c40efc
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:30:50 +0000

    INTEGRATION: CWS pchfix02 (1.33.52); FILE MERGED 2006/09/01 17:24:03 kaib 1.33.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/CRowSetDataColumn.cxx
+++ dbaccess/source/core/api/CRowSetDataColumn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CRowSetDataColumn.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:34:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:30:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBACORE_DATACOLUMN_HXX_
 #include "CRowSetDataColumn.hxx"
