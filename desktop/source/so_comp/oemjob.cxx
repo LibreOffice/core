67aa762cf69204c038fcf7baff063cabcfcbf277
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:49:01 +0000

    INTEGRATION: CWS pchfix02 (1.4.222); FILE MERGED 2006/09/01 17:25:18 kaib 1.4.222.1: #i68856# Added header markers and pch files

--- desktop/source/so_comp/oemjob.cxx
+++ desktop/source/so_comp/oemjob.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oemjob.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:47:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:49:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "oemjob.hxx"
 #ifndef _RTL_BOOTSTRAP_HXX_
