010150b891b2c4db961bae274e809276653eebf2
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:30:15 +0000

    INTEGRATION: CWS pchfix02 (1.4.160); FILE MERGED 2006/09/01 18:00:21 kaib 1.4.160.1: #i68856# Added header markers and pch files

--- xmloff/source/xforms/SchemaRestrictionContext.cxx
+++ xmloff/source/xforms/SchemaRestrictionContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SchemaRestrictionContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:00:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:30:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include "SchemaRestrictionContext.hxx"
 #include "xformsapi.hxx"
