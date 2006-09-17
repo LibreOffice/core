c5b1d12e2fce0d2c617a21fe5ab69d0d315a9807
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:05:11 +0000

    INTEGRATION: CWS pchfix02 (1.3.536); FILE MERGED 2006/09/01 17:47:33 kaib 1.3.536.1: #i68856# Added header markers and pch files

--- svx/source/tbxctrls/formatpaintbrushctrl.cxx
+++ svx/source/tbxctrls/formatpaintbrushctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formatpaintbrushctrl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:48:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:05:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "formatpaintbrushctrl.hxx"
 
