b46a052584c3e8570500c7469954852b7aad0b44
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:33:19 +0000

    INTEGRATION: CWS pchfix02 (1.6.88); FILE MERGED 2006/09/01 17:38:34 kaib 1.6.88.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/itemconnect.cxx
+++ sfx2/source/dialog/itemconnect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: itemconnect.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:22:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:32:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef SFX_ITEMCONNECT_HXX
 #include "itemconnect.hxx"
--- sfx2/source/dialog/mailmodelapi.cxx
+++ sfx2/source/dialog/mailmodelapi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mailmodelapi.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:22:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:33:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 // includes --------------------------------------------------------------
 
 #ifndef  _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
