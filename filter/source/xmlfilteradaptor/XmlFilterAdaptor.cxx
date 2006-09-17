830902c6c203133cbccbd4cdda73131be5e8f5fc
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:45:59 +0000

    INTEGRATION: CWS pchfix02 (1.16.194); FILE MERGED 2006/09/01 17:27:14 kaib 1.16.194.1: #i68856# Added header markers and pch files

--- filter/source/xmlfilteradaptor/XmlFilterAdaptor.cxx
+++ filter/source/xmlfilteradaptor/XmlFilterAdaptor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XmlFilterAdaptor.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:57:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:45:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 #include <iostream.h>
 #include <stdlib.h>
 #include <ctype.h>
