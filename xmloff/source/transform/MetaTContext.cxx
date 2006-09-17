4c6b18e4f7b07c9f2347553ab1314a7f8db0c6ed
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:25:31 +0000

    INTEGRATION: CWS pchfix02 (1.5.34); FILE MERGED 2006/09/01 18:00:17 kaib 1.5.34.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/MetaTContext.cxx
+++ xmloff/source/transform/MetaTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MetaTContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:53:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:25:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_XML_SAX_SAXPARSEEXCEPTION_HPP_
 #include <com/sun/star/xml/sax/SAXParseException.hpp>
