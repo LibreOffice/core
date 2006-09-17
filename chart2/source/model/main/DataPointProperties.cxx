736fdf3167dcfd850e7cefbae4c9754fb4797a55
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:09:43 +0000

    INTEGRATION: CWS pchfix02 (1.10.80); FILE MERGED 2006/09/01 17:18:42 kaib 1.10.80.1: #i68856# Added header markers and pch files

--- chart2/source/model/main/DataPointProperties.cxx
+++ chart2/source/model/main/DataPointProperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataPointProperties.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:55:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:09:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "DataPointProperties.hxx"
 #include "macros.hxx"
 
