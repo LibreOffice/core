fa724dce31e65431171db26c61ab483cb7a455b3
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:58:01 +0000

    INTEGRATION: CWS pchfix02 (1.73.50); FILE MERGED 2006/09/01 17:24:21 kaib 1.73.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/browser/dsbrowserDnD.cxx
+++ dbaccess/source/ui/browser/dsbrowserDnD.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dsbrowserDnD.cxx,v $
  *
- *  $Revision: 1.73 $
+ *  $Revision: 1.74 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:57:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:58:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _SBA_UNODATBR_HXX_
 #include "unodatbr.hxx"
