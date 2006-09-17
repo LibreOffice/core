034eec691d927ef8bcf3f6e244788ee129a54f27
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:34:09 +0000

    INTEGRATION: CWS pchfix02 (1.6.90); FILE MERGED 2006/09/01 17:42:53 kaib 1.6.90.1: #i68856# Added header markers and pch files

--- svtools/source/contnr/svimpicn.cxx
+++ svtools/source/contnr/svimpicn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svimpicn.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:52:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:34:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include <limits.h>
 #ifndef _METRIC_HXX
