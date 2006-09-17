44817b80fba73379aa2260281a76bda850aa4a24
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:08:10 +0000

    INTEGRATION: CWS pchfix02 (1.8.76); FILE MERGED 2006/09/01 17:57:46 kaib 1.8.76.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/pdfextoutdevdata.cxx
+++ vcl/source/gdi/pdfextoutdevdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pdfextoutdevdata.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 17:25:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:08:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _VCL_PDFEXTOUTDEVDATA_HXX
 #include "pdfextoutdevdata.hxx"
