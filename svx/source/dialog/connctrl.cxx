9eeced53094621e37409842cf36b13344a708a1b
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:12:21 +0000

    INTEGRATION: CWS pchfix02 (1.9.50); FILE MERGED 2006/09/01 17:46:05 kaib 1.9.50.1: #i68856# Added header markers and pch files

--- svx/source/dialog/connctrl.cxx
+++ svx/source/dialog/connctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: connctrl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:49:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:12:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // include ---------------------------------------------------------------
 
