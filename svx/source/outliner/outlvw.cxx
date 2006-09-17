81b1de3b839e6d3fa61db55672395bd04cd86414
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:31:43 +0000

    INTEGRATION: CWS pchfix02 (1.27.116); FILE MERGED 2006/09/01 17:47:08 kaib 1.27.116.1: #i68856# Added header markers and pch files

--- svx/source/outliner/outlvw.cxx
+++ svx/source/outliner/outlvw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outlvw.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:23:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:31:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #include <outl_pch.hxx>
 
 #ifndef _SFXSTYLE_HXX //autogen
