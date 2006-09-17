c3c49d043ef7c9e48e3a6bf1e5d02cb52a4489c5
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:28:11 +0000

    INTEGRATION: CWS pchfix02 (1.2.50); FILE MERGED 2006/09/01 17:18:07 kaib 1.2.50.1: #i68856# Added header markers and pch files

--- canvas/source/tools/surfaceproxy.cxx
+++ canvas/source/tools/surfaceproxy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: surfaceproxy.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 12:55:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:27:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_canvas.hxx"
 
 #include <boost/bind.hpp>
 #include <basegfx/polygon/b2dpolygoncutandtouch.hxx>
