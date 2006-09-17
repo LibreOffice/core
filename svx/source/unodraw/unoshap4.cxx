1da72d2b2b154626e63aa61034995fc2154d6d21
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:15:08 +0000

    INTEGRATION: CWS pchfix02 (1.25.116); FILE MERGED 2006/09/01 17:47:40 kaib 1.25.116.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/unoshap4.cxx
+++ svx/source/unodraw/unoshap4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoshap4.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:56:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:15:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_UTIL_XMODIFIABLE_HPP_
 #include <com/sun/star/util/XModifiable.hpp>
