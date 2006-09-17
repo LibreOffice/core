288cb0a312adeb8cdded8a0972454fab04558c19
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:41:51 +0000

    INTEGRATION: CWS pchfix02 (1.7.8); FILE MERGED 2006/09/01 17:53:45 kaib 1.7.8.1: #i68856# Added header markers and pch files

--- testshl2/source/dynamicregister.cxx
+++ testshl2/source/dynamicregister.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dynamicregister.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:27:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:41:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_testshl2.hxx"
 
 #include "dynamicregister.hxx"
 #ifndef _OSL_PROCESS_H_
