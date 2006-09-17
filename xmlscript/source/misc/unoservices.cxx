dc7651fac34ad607d91d8c27ce46cffb74c4f8cf
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:37:10 +0000

    INTEGRATION: CWS pchfix02 (1.4.6); FILE MERGED 2006/09/01 18:00:31 kaib 1.4.6.1: #i68856# Added header markers and pch files

--- xmlscript/source/misc/unoservices.cxx
+++ xmlscript/source/misc/unoservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoservices.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:10:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:36:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlscript.hxx"
 
 #ifndef _CPPUHELPER_IMPLEMENTATIONENTRY_HXX_
 #include <cppuhelper/implementationentry.hxx>
--- xmlscript/source/xml_helper/xml_byteseq.cxx
+++ xmlscript/source/xml_helper/xml_byteseq.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xml_byteseq.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:11:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:37:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlscript.hxx"
 #include <rtl/memory.h>
 
 #include <cppuhelper/implbase1.hxx>
