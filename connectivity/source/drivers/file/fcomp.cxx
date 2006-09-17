4cfe11873a2f8f733d44f38df170d76b444c56a6
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:36:18 +0000

    INTEGRATION: CWS pchfix02 (1.25.42); FILE MERGED 2006/09/01 17:22:12 kaib 1.25.42.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/file/fcomp.cxx
+++ connectivity/source/drivers/file/fcomp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fcomp.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:27:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:36:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FILE_FCOMP_HXX_
 #include "file/fcomp.hxx"
