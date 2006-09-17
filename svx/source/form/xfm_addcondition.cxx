591a1d7ba34b7006ff947f4ca0cbfc4dc81d4e80
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:14:52 +0000

    INTEGRATION: CWS pchfix02 (1.3.536); FILE MERGED 2006/09/01 17:46:55 kaib 1.3.536.1: #i68856# Added header markers and pch files

--- svx/source/form/xfm_addcondition.cxx
+++ svx/source/form/xfm_addcondition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xfm_addcondition.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:04:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:14:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_SOURCE_FORM_XFM_ADDCONDITION_HXX
 #include "xfm_addcondition.hxx"
