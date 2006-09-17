a9177beb9bbe6945e06bba0c8fad304c43c8d19e
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:49:57 +0000

    INTEGRATION: CWS pchfix02 (1.4.326); FILE MERGED 2006/09/01 17:43:04 kaib 1.4.326.1: #i68856# Added header markers and pch files

--- svtools/source/filepicker/pickerhistory.cxx
+++ svtools/source/filepicker/pickerhistory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pickerhistory.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:31:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:49:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef SVTOOLS_PICKERHISTORY_HXX
 #include "pickerhistory.hxx"
