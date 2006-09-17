1a022602103bc56773fb52e58b3e0a195fb06a4c
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:43:40 +0000

    INTEGRATION: CWS pchfix02 (1.6.28); FILE MERGED 2006/09/01 17:30:05 kaib 1.6.28.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/eppm/dlgeppm.cxx
+++ goodies/source/filter.vcl/eppm/dlgeppm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgeppm.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:42:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:43:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 #ifndef GCC
-#pragma hdrstop
 #endif
 #include <tools/ref.hxx>
 #include <svtools/FilterConfigItem.hxx>
