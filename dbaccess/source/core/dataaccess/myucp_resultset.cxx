161170a39fcf03bb292c217ff86fd5c2028cb0fa
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:41:05 +0000

    INTEGRATION: CWS pchfix02 (1.4.52); FILE MERGED 2006/09/01 17:24:10 kaib 1.4.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/myucp_resultset.cxx
+++ dbaccess/source/core/dataaccess/myucp_resultset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: myucp_resultset.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:46:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:41:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 /**************************************************************************
                                 TODO
