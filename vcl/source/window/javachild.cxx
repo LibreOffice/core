f9d8e8eb566417ec27a14d46aba7ffcd0d4fa391
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:19:00 +0000

    INTEGRATION: CWS pchfix02 (1.6.112); FILE MERGED 2006/09/01 17:57:54 kaib 1.6.112.1: #i68856# Added header markers and pch files

--- vcl/source/window/javachild.cxx
+++ vcl/source/window/javachild.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: javachild.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:38:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:19:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 
 #ifdef SOLAR_JAVA
