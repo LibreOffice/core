040ba03b88ff737638e8a2b7a7ca7e1bac7fd9dc
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:00:09 +0000

    INTEGRATION: CWS pchfix02 (1.17.36); FILE MERGED 2006/09/01 17:24:23 kaib 1.17.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/control/RelationControl.cxx
+++ dbaccess/source/ui/control/RelationControl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RelationControl.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:24:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:00:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_RELATIONCONTROL_HXX
 #include "RelationControl.hxx"
 #endif
