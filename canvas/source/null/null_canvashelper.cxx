226a4ce7378c1ed98416aa4f914ac759e78a14d5
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:23:32 +0000

    INTEGRATION: CWS pchfix02 (1.3.20); FILE MERGED 2006/09/01 17:18:03 kaib 1.3.20.1: #i68856# Added header markers and pch files

--- canvas/source/null/null_canvashelper.cxx
+++ canvas/source/null/null_canvashelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: null_canvashelper.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:15:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:23:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_canvas.hxx"
 
 #include <canvas/debug.hxx>
 
