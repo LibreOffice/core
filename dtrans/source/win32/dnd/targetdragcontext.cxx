90d05e0d421dccf580969f0978d93006f55374bc
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:59:31 +0000

    INTEGRATION: CWS pchfix02 (1.6.40); FILE MERGED 2006/09/01 17:25:37 kaib 1.6.40.1: #i68856# Added header markers and pch files

--- dtrans/source/win32/dnd/targetdragcontext.cxx
+++ dtrans/source/win32/dnd/targetdragcontext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: targetdragcontext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:19:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:59:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #ifndef _RTL_UNLOAD_H_
 #include <rtl/unload.h>
