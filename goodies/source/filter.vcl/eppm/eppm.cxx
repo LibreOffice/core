a71dee137bfafa697660e281ffa8cea2da592c58
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:43:54 +0000

    INTEGRATION: CWS pchfix02 (1.6.114); FILE MERGED 2006/09/01 17:30:05 kaib 1.6.114.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/eppm/eppm.cxx
+++ goodies/source/filter.vcl/eppm/eppm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eppm.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:42:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:43:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <vcl/svapp.hxx>
 #include <vcl/graph.hxx>
@@ -287,7 +290,6 @@ extern "C" BOOL __LOADONCALLAPI DoExportDialog( FltCallDialogParameter& rPara )
 }
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 // ------------------------------------------------------------------------
