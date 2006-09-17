9d73380532daca223e744563067b28780bb4e09e
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:50:53 +0000

    INTEGRATION: CWS pchfix02 (1.22.6); FILE MERGED 2006/09/01 17:22:22 kaib 1.22.6.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/jdbc/tools.cxx
+++ connectivity/source/drivers/jdbc/tools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tools.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:28:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:50:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include <cstdarg>
 #ifndef _CONNECTIVITY_JAVA_TOOLS_HXX_
