65677f62dc38a0257baa590b0246c7af47e61df5
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:00:45 +0000

    INTEGRATION: CWS pchfix02 (1.9.114); FILE MERGED 2006/09/01 17:47:29 kaib 1.9.114.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdsnpv.cxx
+++ svx/source/svdraw/svdsnpv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdsnpv.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:44:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:00:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "svdsnpv.hxx"
 #include <math.h>
