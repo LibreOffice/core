d24d94a1ab3736c5259ba66f18f3eefa0f0d63a3
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:03:50 +0000

    INTEGRATION: CWS pchfix02 (1.7.28); FILE MERGED 2006/09/01 17:16:37 kaib 1.7.28.1: #i68856# Added header markers and pch files

--- basegfx/source/polygon/b3dpolygontools.cxx
+++ basegfx/source/polygon/b3dpolygontools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dpolygontools.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:59:10 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:03:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
