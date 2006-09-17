289a395dd4c26704818bcebec3222eac412ad9e4
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:45:02 +0000

    INTEGRATION: CWS pchfix02 (1.10.98); FILE MERGED 2006/09/01 17:25:15 kaib 1.10.98.1: #i68856# Added header markers and pch files

--- desktop/source/deployment/registry/sfwk/dp_sfwk.cxx
+++ desktop/source/deployment/registry/sfwk/dp_sfwk.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dp_sfwk.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-22 11:09:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:45:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 #include "dp_sfwk.hrc"
 #include "dp_backend.h"
 #include "dp_ucb.h"
