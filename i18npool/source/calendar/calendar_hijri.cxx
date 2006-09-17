e9f39ace5d1e53b7686e955a407c05b05f5b405e
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:15:51 +0000

    INTEGRATION: CWS pchfix02 (1.9.16); FILE MERGED 2006/09/01 17:30:40 kaib 1.9.16.1: #i68856# Added header markers and pch files

--- i18npool/source/calendar/calendar_hijri.cxx
+++ i18npool/source/calendar/calendar_hijri.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: calendar_hijri.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:43:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:15:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <stdlib.h>
 #include <math.h>
--- i18npool/source/characterclassification/cclass_unicode.cxx
+++ i18npool/source/characterclassification/cclass_unicode.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cclass_unicode.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:43:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:15:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <cclass_unicode.hxx>
 #include <com/sun/star/i18n/UnicodeScript.hpp>
