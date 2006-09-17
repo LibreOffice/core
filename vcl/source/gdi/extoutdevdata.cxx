983d93ceceba71d5b70d9ed71ab9f40a45b77d1f
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:59:41 +0000

    INTEGRATION: CWS pchfix02 (1.3.376); FILE MERGED 2006/09/01 17:57:41 kaib 1.3.376.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/extoutdevdata.cxx
+++ vcl/source/gdi/extoutdevdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: extoutdevdata.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:56:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:59:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef _VCL_EXTOUTDEVDATA_HXX
 #include "extoutdevdata.hxx"
