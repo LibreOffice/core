6d254721543e4c79207c6057437621d9b64ab60e
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:05:56 +0000

    INTEGRATION: CWS pchfix02 (1.7.30); FILE MERGED 2006/09/01 17:34:18 kaib 1.7.30.1: #i68856# Added header markers and pch files

--- sal/rtl/source/uri.cxx
+++ sal/rtl/source/uri.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uri.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:31:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:05:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 #include "rtl/uri.h"
 
