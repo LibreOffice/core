9448592df27dede1bb0275480f54a89c841156c3
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:08:43 +0000

    INTEGRATION: CWS pchfix02 (1.98.4); FILE MERGED 2006/09/01 17:57:46 kaib 1.98.4.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/pdfwriter_impl.cxx
+++ vcl/source/gdi/pdfwriter_impl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pdfwriter_impl.cxx,v $
  *
- *  $Revision: 1.98 $
+ *  $Revision: 1.99 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:45:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:08:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #define _USE_MATH_DEFINES
 #include <math.h>
