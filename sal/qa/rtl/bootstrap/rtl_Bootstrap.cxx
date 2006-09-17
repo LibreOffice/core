684c6da4415a41aac5c1eec33e0642195ef43192
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:55:06 +0000

    INTEGRATION: CWS pchfix02 (1.4.140); FILE MERGED 2006/09/01 17:34:09 kaib 1.4.140.1: #i68856# Added header markers and pch files

--- sal/qa/rtl/bootstrap/rtl_Bootstrap.cxx
+++ sal/qa/rtl/bootstrap/rtl_Bootstrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rtl_Bootstrap.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:42:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:55:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 // Documentation about bootstraping can be found at:
 // http://udk.openoffice.org/common/man/concept/micro_deployment.html
