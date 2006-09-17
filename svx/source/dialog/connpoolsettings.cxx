870d2961026306230028969323c14619a63d4fa9
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:13:54 +0000

    INTEGRATION: CWS pchfix02 (1.5.116); FILE MERGED 2006/09/01 17:46:06 kaib 1.5.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/connpoolsettings.cxx
+++ svx/source/dialog/connpoolsettings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: connpoolsettings.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:03:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:13:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
