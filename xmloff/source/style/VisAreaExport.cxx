43e4377e5dc3da05ada28e5c26055d99a12a7cc5
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:47:41 +0000

    INTEGRATION: CWS pchfix02 (1.4.160); FILE MERGED 2006/09/01 17:59:49 kaib 1.4.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/VisAreaExport.cxx
+++ xmloff/source/style/VisAreaExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VisAreaExport.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:30:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:47:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_AWT_RECTANGLE_HPP_
 #include <com/sun/star/awt/Rectangle.hpp>
