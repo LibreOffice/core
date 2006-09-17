571cf1ced236c30ff19f20195d796ee8186c1e82
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:35:03 +0000

    INTEGRATION: CWS pchfix02 (1.33.78); FILE MERGED 2006/09/01 17:18:59 kaib 1.33.78.1: #i68856# Added header markers and pch files

--- chart2/source/view/main/ChartView.cxx
+++ chart2/source/view/main/ChartView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChartView.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:46:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:35:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "ChartViewImpl.hxx"
 #include "ViewDefines.hxx"
 #include "VDiagram.hxx"
