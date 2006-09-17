b8a0a46714365888267f08a299150a1648613268
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:09:10 +0000

    INTEGRATION: CWS pchfix02 (1.3.536); FILE MERGED 2006/09/01 17:47:35 kaib 1.3.536.1: #i68856# Added header markers and pch files

--- svx/source/unodialogs/textconversiondlgs/chinese_translation_unodialog.cxx
+++ svx/source/unodialogs/textconversiondlgs/chinese_translation_unodialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chinese_translation_unodialog.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:57:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:08:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "chinese_translation_unodialog.hxx"
 #include "chinese_translationdialog.hxx"
--- svx/source/unodialogs/textconversiondlgs/chinese_translationdialog.cxx
+++ svx/source/unodialogs/textconversiondlgs/chinese_translationdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chinese_translationdialog.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:57:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:09:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "chinese_translationdialog.hxx"
 #include "chinese_translationdialog.hrc"
