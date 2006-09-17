1b905110f0669d60623daf0cd9e12993eb57d43d
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:18:49 +0000

    INTEGRATION: CWS pchfix02 (1.14.60); FILE MERGED 2006/09/01 17:22:00 kaib 1.14.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/calc/CDatabaseMetaData.cxx
+++ connectivity/source/drivers/calc/CDatabaseMetaData.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CDatabaseMetaData.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 10:57:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:18:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 
 #ifndef _CONNECTIVITY_CALC_OCALCDATABASEMETADATA_HXX_
