a3a94479eb532f9ab32fdabe8ba0b0338409ed77
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:51:53 +0000

    INTEGRATION: CWS pchfix02 (1.9.184); FILE MERGED 2006/09/01 17:24:17 kaib 1.9.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/shared/dbastrings.cxx
+++ dbaccess/source/shared/dbastrings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbastrings.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:16:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:51:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBACCESS_SHARED_DBASTRINGS_HRC
 #include "dbastrings.hrc"
 #endif
