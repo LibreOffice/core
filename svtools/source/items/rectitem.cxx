a6a86a8d034fcd1175a3dc278951dc8432e3d84f
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:01:09 +0000

    INTEGRATION: CWS pchfix02 (1.8.92); FILE MERGED 2006/09/01 17:43:13 kaib 1.8.92.1: #i68856# Added header markers and pch files

--- svtools/source/items/rectitem.cxx
+++ svtools/source/items/rectitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rectitem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:13:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:00:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include "rectitem.hxx"
 
--- svtools/source/items/rngitem.cxx
+++ svtools/source/items/rngitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rngitem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:13:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:01:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
