1e3352d2ec81258f728425bdb50264efc03083a5
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:19:37 +0000

    INTEGRATION: CWS pchfix02 (1.7.2); FILE MERGED 2006/09/01 17:55:14 kaib 1.7.2.1: #i68856# Added header markers and pch files

--- transex3/source/srciter.cxx
+++ transex3/source/srciter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srciter.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:11:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:19:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_transex3.hxx"
 
 #include "srciter.hxx"
 #include <stdio.h>
