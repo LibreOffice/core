4779344d6a96773e67b8925c4486e85fc061d45f
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:52:37 +0000

    INTEGRATION: CWS pchfix02 (1.6.326); FILE MERGED 2006/09/01 17:43:06 kaib 1.6.326.1: #i68856# Added header markers and pch files

--- svtools/source/filter.vcl/filter/gradwrap.cxx
+++ svtools/source/filter.vcl/filter/gradwrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gradwrap.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:38:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:52:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include <math.h>
 #include <svgrad.hxx>
