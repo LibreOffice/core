2891011131875d9c8d27830969a39d7181166b60
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:44:21 +0000

    INTEGRATION: CWS pchfix02 (1.5.76); FILE MERGED 2006/09/01 17:33:58 kaib 1.5.76.1: #i68856# Added header markers and pch files

--- sal/cpprt/operators_new_delete.cxx
+++ sal/cpprt/operators_new_delete.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: operators_new_delete.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-28 10:33:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:44:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 #ifdef WNT /* avoid 'std::bad_alloc' unresolved externals */
 #define _CRTIMP
