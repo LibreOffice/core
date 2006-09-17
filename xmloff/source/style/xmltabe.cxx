5e76d7be9a2c7262a71d8259a0b2b31f41dc11ae
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:01:08 +0000

    INTEGRATION: CWS pchfix02 (1.12.160); FILE MERGED 2006/09/01 17:59:58 kaib 1.12.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/xmltabe.cxx
+++ xmloff/source/style/xmltabe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltabe.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:55:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:01:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #include <tools/debug.hxx>
 
