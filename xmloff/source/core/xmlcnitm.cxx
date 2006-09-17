946c4e9f250d37f4c0eee0c50a90748b0fe69786
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:22:03 +0000

    INTEGRATION: CWS pchfix02 (1.8.34); FILE MERGED 2006/09/01 17:59:28 kaib 1.8.34.1: #i68856# Added header markers and pch files

--- xmloff/source/core/xmlcnitm.cxx
+++ xmloff/source/core/xmlcnitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlcnitm.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:06:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:22:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
