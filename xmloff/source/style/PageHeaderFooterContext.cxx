0a4597fbd8864b9b54faf79713e8fee6461a8446
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:44:41 +0000

    INTEGRATION: CWS pchfix02 (1.7.34); FILE MERGED 2006/09/01 17:59:47 kaib 1.7.34.1: #i68856# Added header markers and pch files

--- xmloff/source/style/PageHeaderFooterContext.cxx
+++ xmloff/source/style/PageHeaderFooterContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageHeaderFooterContext.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:25:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:44:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,9 +33,9 @@
  *
  ************************************************************************/
 
-#ifdef PCH
-#include "filt_pch.hxx"
-#endif
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
+
 
 
 
