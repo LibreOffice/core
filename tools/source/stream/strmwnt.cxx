bda99e2fcf04eba733b5c0562279254ffbc96aee
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:02:15 +0000

    INTEGRATION: CWS pchfix02 (1.5.42); FILE MERGED 2006/09/01 17:54:57 kaib 1.5.42.1: #i68856# Added header markers and pch files

--- tools/source/stream/strmwnt.cxx
+++ tools/source/stream/strmwnt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strmwnt.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:52:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:02:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 /*
     Todo: StreamMode <-> AllocateMemory
