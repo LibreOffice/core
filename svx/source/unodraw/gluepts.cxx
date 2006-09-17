ef09740785ce343e3517b1b663c6579c52022c03
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:10:51 +0000

    INTEGRATION: CWS pchfix02 (1.15.116); FILE MERGED 2006/09/01 17:47:37 kaib 1.15.116.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/gluepts.cxx
+++ svx/source/unodraw/gluepts.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gluepts.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:54:07 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:10:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_CONTAINER_XIDENTIFIERCONTAINER_HPP_
 #include <com/sun/star/container/XIdentifierContainer.hpp>
