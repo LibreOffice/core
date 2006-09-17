bed7976a303a42b20668f2886ae59e8ad254150f
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:26:57 +0000

    INTEGRATION: CWS pchfix02 (1.2.28); FILE MERGED 2006/09/01 17:56:23 kaib 1.2.28.1: #i68856# Added header markers and pch files

--- unotools/source/misc/componentresmodule.cxx
+++ unotools/source/misc/componentresmodule.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: componentresmodule.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:38:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:26:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef UNOTOOLS_INC_UNOTOOLS_COMPONENTRESMODULE_HXX
 #include <unotools/componentresmodule.hxx>
