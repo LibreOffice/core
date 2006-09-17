375ac0d96c02364a143d994409dfcf4bbe5377b5
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:56:31 +0000

    INTEGRATION: CWS pchfix02 (1.5.18); FILE MERGED 2006/09/01 17:32:11 kaib 1.5.18.1: #i68856# Added header markers and pch files

--- linguistic/source/thesdta.cxx
+++ linguistic/source/thesdta.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: thesdta.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 13:52:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:56:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX //autogen wg. DBG_ASSERT
 #include <tools/debug.hxx>
