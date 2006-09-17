ea124e5f4d55c3d9e28597699bf92cc309bfbdde
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:43:37 +0000

    INTEGRATION: CWS pchfix02 (1.181.10); FILE MERGED 2006/09/01 17:38:42 kaib 1.181.10.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/objstor.cxx
+++ sfx2/source/doc/objstor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objstor.cxx,v $
  *
- *  $Revision: 1.181 $
+ *  $Revision: 1.182 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 14:23:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:43:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
