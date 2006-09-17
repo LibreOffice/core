d91a8b67a5095b9b20649db530e1d3eae2c5650d
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:14:30 +0000

    INTEGRATION: CWS pchfix02 (1.44.18); FILE MERGED 2006/09/01 17:59:22 kaib 1.44.18.1: #i68856# Added header markers and pch files

--- xmloff/source/chart/PropertyMaps.cxx
+++ xmloff/source/chart/PropertyMaps.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PropertyMaps.cxx,v $
  *
- *  $Revision: 1.44 $
+ *  $Revision: 1.45 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:34:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:14:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 // include PropertyMap.hxx with this define
 // to create the maps
