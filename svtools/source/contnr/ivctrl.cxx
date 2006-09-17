b7db0a113a1f27042a4ad335621d2e65047cfe54
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:33:25 +0000

    INTEGRATION: CWS pchfix02 (1.21.92); FILE MERGED 2006/09/01 17:42:53 kaib 1.21.92.1: #i68856# Added header markers and pch files

--- svtools/source/contnr/ivctrl.cxx
+++ svtools/source/contnr/ivctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ivctrl.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:51:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:33:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "ivctrl.hxx"
