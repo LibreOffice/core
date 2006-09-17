d597dcc500a41a8b1432c5d76124fc8ac48da186
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:40:40 +0000

    INTEGRATION: CWS pchfix02 (1.5.534); FILE MERGED 2006/09/01 17:47:14 kaib 1.5.534.1: #i68856# Added header markers and pch files

--- svx/source/sdr/properties/captionproperties.cxx
+++ svx/source/sdr/properties/captionproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: captionproperties.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:10:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:40:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_PROPERTIES_CAPTIONPROPERTIES_HXX
 #include <svx/sdr/properties/captionproperties.hxx>
