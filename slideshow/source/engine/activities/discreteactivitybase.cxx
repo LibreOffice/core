b56b8c48c77d9039c1e781b781a85fa17c6f163f
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:32:33 +0000

    INTEGRATION: CWS pchfix02 (1.5.48); FILE MERGED 2006/09/01 17:39:36 kaib 1.5.48.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/activities/discreteactivitybase.cxx
+++ slideshow/source/engine/activities/discreteactivitybase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: discreteactivitybase.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2005-10-11 08:40:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:32:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 // must be first
 #include <canvas/debug.hxx>
