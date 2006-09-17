d857081fc4d7c2494c13f7e25ac4403d48570f91
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:42:56 +0000

    INTEGRATION: CWS pchfix02 (1.10.26); FILE MERGED 2006/09/01 17:25:13 kaib 1.10.26.1: #i68856# Added header markers and pch files

--- desktop/source/deployment/registry/dp_backend.cxx
+++ desktop/source/deployment/registry/dp_backend.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dp_backend.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 17:06:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:42:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "dp_backend.h"
 #include "dp_ucb.h"
