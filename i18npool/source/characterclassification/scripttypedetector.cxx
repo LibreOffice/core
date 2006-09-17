5d76ffb4a073b407cef9d39f31f6e7ee5133e928
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:16:36 +0000

    INTEGRATION: CWS pchfix02 (1.8.16); FILE MERGED 2006/09/01 17:30:41 kaib 1.8.16.1: #i68856# Added header markers and pch files

--- i18npool/source/characterclassification/scripttypedetector.cxx
+++ i18npool/source/characterclassification/scripttypedetector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scripttypedetector.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:44:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:16:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <com/sun/star/i18n/CTLScriptType.hpp>
 #include <com/sun/star/i18n/ScriptDirection.hpp>
