fa50d18663c0f5e93fa216032792d853516502c3
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:10:25 +0000

    INTEGRATION: CWS pchfix02 (1.26.4); FILE MERGED 2006/09/01 17:24:29 kaib 1.26.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/indexdialog.cxx
+++ dbaccess/source/ui/dlg/indexdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: indexdialog.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:51:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:10:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_INDEXDIALOG_HXX_
 #include "indexdialog.hxx"
