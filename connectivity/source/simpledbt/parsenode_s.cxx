9d84e413c0ddb4ce1ddbe47e3a57109d94db76da
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:13:44 +0000

    INTEGRATION: CWS pchfix02 (1.4.166); FILE MERGED 2006/09/01 17:22:38 kaib 1.4.166.1: #i68856# Added header markers and pch files

--- connectivity/source/simpledbt/parsenode_s.cxx
+++ connectivity/source/simpledbt/parsenode_s.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parsenode_s.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:47:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:13:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_VIRTUAL_DBTOOLS_HXX
 #include <connectivity/virtualdbtools.hxx>
