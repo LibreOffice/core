1682a3858bb0034ccb61489ea1d554d9acfbe71f
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:13:48 +0000

    INTEGRATION: CWS pchfix02 (1.15.36); FILE MERGED 2006/09/01 17:19:43 kaib 1.15.36.1: #i68856# Added header markers and pch files

--- comphelper/source/misc/regpathhelper.cxx
+++ comphelper/source/misc/regpathhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: regpathhelper.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:49:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:13:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #include <stdio.h>
 
