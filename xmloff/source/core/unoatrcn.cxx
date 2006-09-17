2cdab6e167b70de679501f21d6aef7a48425960d
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:21:27 +0000

    INTEGRATION: CWS pchfix02 (1.5.34); FILE MERGED 2006/09/01 17:59:28 kaib 1.5.34.1: #i68856# Added header markers and pch files

--- xmloff/source/core/unoatrcn.cxx
+++ xmloff/source/core/unoatrcn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoatrcn.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:05:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:21:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
