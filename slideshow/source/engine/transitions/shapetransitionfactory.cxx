4d6a648dd8e92559cc04d23f9f4c068eb7c1dfc2
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:41:10 +0000

    INTEGRATION: CWS pchfix02 (1.5.42); FILE MERGED 2006/09/01 17:39:42 kaib 1.5.42.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/transitions/shapetransitionfactory.cxx
+++ slideshow/source/engine/transitions/shapetransitionfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shapetransitionfactory.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 14:04:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:41:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 #include <basegfx/numeric/ftools.hxx>
 #include <basegfx/matrix/b2dhommatrix.hxx>
