099ff52338b037b1323a50829bc40d241a249a67
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:29:49 +0000

    INTEGRATION: CWS pchfix02 (1.52.48); FILE MERGED 2006/09/01 17:38:32 kaib 1.52.48.1: #i68856# Added header markers and pch files

--- sfx2/source/control/unoctitm.cxx
+++ sfx2/source/control/unoctitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoctitm.cxx,v $
  *
- *  $Revision: 1.53 $
+ *  $Revision: 1.54 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:16:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:29:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include <tools/debug.hxx>
 
