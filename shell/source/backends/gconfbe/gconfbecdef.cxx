ce571362213de49ff180f6afd8f26523397fd5ae
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:37:07 +0000

    INTEGRATION: CWS pchfix02 (1.9.6); FILE MERGED 2006/09/01 17:39:03 kaib 1.9.6.1: #i68856# Added header markers and pch files

--- shell/source/backends/gconfbe/gconfbecdef.cxx
+++ shell/source/backends/gconfbe/gconfbecdef.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gconfbecdef.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 12:29:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:37:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef GCONFBACKEND_HXX_
 #include "gconfbackend.hxx"
