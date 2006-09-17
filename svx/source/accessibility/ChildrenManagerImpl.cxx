66ad48669e884471f29013849b64f13a64f0485e
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:04:15 +0000

    INTEGRATION: CWS pchfix02 (1.35.116); FILE MERGED 2006/09/01 17:45:59 kaib 1.35.116.1: #i68856# Added header markers and pch files

--- svx/source/accessibility/ChildrenManagerImpl.cxx
+++ svx/source/accessibility/ChildrenManagerImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChildrenManagerImpl.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:54:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:04:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "ChildrenManagerImpl.hxx"
 
