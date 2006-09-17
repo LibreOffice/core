bb9f4cd99f08653d8685b054f8c5be4a97a71260
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:46:40 +0000

    INTEGRATION: CWS pchfix02 (1.3.116); FILE MERGED 2006/09/01 17:46:29 kaib 1.3.116.1: #i68856# Added header markers and pch files

--- svx/source/dialog/txenctab.cxx
+++ svx/source/dialog/txenctab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txenctab.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:37:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:46:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include "dialogs.hrc" // -> RID_SVXSTR_TEXTENCODING_TABLE
 #include "dialmgr.hxx"
