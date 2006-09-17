c9cf7360c9aa5adeeca4020f88348e615dc96147
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:47:43 +0000

    INTEGRATION: CWS pchfix02 (1.5.28); FILE MERGED 2006/09/01 17:30:08 kaib 1.5.28.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/icgm/class4.cxx
+++ goodies/source/filter.vcl/icgm/class4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: class4.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:45:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:47:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <math.h>
 #include <main.hxx>
