f362909bc8b5914bfab2b6431d13c591e8d66e92
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:36:28 +0000

    INTEGRATION: CWS pchfix02 (1.5.312); FILE MERGED 2006/09/01 17:38:36 kaib 1.5.312.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/styfitem.cxx
+++ sfx2/source/dialog/styfitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: styfitem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:32:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:36:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 // INCLUDE ---------------------------------------------------------------
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "styfitem.hxx"
