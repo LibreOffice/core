d64cdb2f48734844723c1cab7f293442883624ae
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:46:36 +0000

    INTEGRATION: CWS pchfix02 (1.17.88); FILE MERGED 2006/09/01 17:38:44 kaib 1.17.88.1: #i68856# Added header markers and pch files

--- sfx2/source/menu/mnuitem.cxx
+++ sfx2/source/menu/mnuitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mnuitem.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:35:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:46:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifdef SOLARIS
 // HACK: prevent conflict between STLPORT and Workshop headers on Solaris 8
@@ -85,7 +88,6 @@
 #endif
 #include <framework/menuconfiguration.hxx>
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "app.hxx"
