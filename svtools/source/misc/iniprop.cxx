9c7bc6fde346570a02430703c622ab91f9fd7f4a
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:12:00 +0000

    INTEGRATION: CWS pchfix02 (1.2.324); FILE MERGED 2006/09/01 17:43:21 kaib 1.2.324.1: #i68856# Added header markers and pch files

--- svtools/source/misc/iniprop.cxx
+++ svtools/source/misc/iniprop.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: iniprop.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:20:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:12:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 
 #include "iniprop.hxx"
 
