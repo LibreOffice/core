d7feb06a13d151c1eab9f1533b04a849e559cd26
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:43:35 +0000

    INTEGRATION: CWS pchfix02 (1.7.18); FILE MERGED 2006/09/01 17:55:38 kaib 1.7.18.1: #i68856# Added header markers and pch files

--- ucb/source/core/provprox.cxx
+++ ucb/source/core/provprox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: provprox.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 14:33:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:43:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
