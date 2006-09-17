b206e60beba4163951522c79b58705ac6b3f3ff1
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:51:30 +0000

    INTEGRATION: CWS pchfix02 (1.8.24); FILE MERGED 2006/09/01 17:22:50 kaib 1.8.24.1: #i68856# Added header markers and pch files

--- cppcanvas/source/wrapper/implbitmap.cxx
+++ cppcanvas/source/wrapper/implbitmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implbitmap.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:42:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:51:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #include <implbitmap.hxx>
 #include <implbitmapcanvas.hxx>
