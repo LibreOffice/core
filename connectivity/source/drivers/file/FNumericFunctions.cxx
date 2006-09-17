87ec0dcdfaa4ccc47dc0add0642ff4956a699070
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:33:20 +0000

    INTEGRATION: CWS pchfix02 (1.6.60); FILE MERGED 2006/09/01 17:22:11 kaib 1.6.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/file/FNumericFunctions.cxx
+++ connectivity/source/drivers/file/FNumericFunctions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FNumericFunctions.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:25:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:33:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include <cmath>
 #include "file/FNumericFunctions.hxx"
