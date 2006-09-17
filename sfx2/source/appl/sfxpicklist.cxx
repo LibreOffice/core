f1fe5cde0926b2e09322b0a3361fc05b2b612f2b
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:21:01 +0000

    INTEGRATION: CWS pchfix02 (1.28.88); FILE MERGED 2006/09/01 17:38:25 kaib 1.28.88.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/sfxpicklist.cxx
+++ sfx2/source/appl/sfxpicklist.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxpicklist.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:12:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:21:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef INCLUDED_SVTOOLS_HISTORYOPTIONS_HXX
 #include <svtools/historyoptions.hxx>
