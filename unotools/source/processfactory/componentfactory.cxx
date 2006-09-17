4ba1151261170a8b2f1296009703a9282b3a6f2e
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:28:18 +0000

    INTEGRATION: CWS pchfix02 (1.3.58); FILE MERGED 2006/09/01 17:56:24 kaib 1.3.58.1: #i68856# Added header markers and pch files

--- unotools/source/processfactory/componentfactory.cxx
+++ unotools/source/processfactory/componentfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: componentfactory.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:47:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:28:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
+
 #include <unotools/componentfactory.hxx>
 #include <comphelper/processfactory.hxx>
 
@@ -57,7 +60,6 @@
 #include <osl/module.h>
 
 
-#pragma hdrstop
 
 
 using namespace ::com::sun::star;
