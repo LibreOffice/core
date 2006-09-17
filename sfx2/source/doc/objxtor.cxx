bb6000801488c00d5be80b5cd6eb864bb0a5dd7b
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:44:06 +0000

    INTEGRATION: CWS pchfix02 (1.65.88); FILE MERGED 2006/09/01 17:38:42 kaib 1.65.88.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/objxtor.cxx
+++ sfx2/source/doc/objxtor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objxtor.cxx,v $
  *
- *  $Revision: 1.65 $
+ *  $Revision: 1.66 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:30:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:44:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #include "arrdecl.hxx"
 
@@ -78,7 +81,6 @@
 #include <svtools/stritem.hxx>
 #endif
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _SBXCLASS_HXX //autogen
