5de8eb09de5170d2e0c6debd9a90432dc10015a1
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:50:16 +0000

    INTEGRATION: CWS pchfix02 (1.10.116); FILE MERGED 2006/09/01 17:46:37 kaib 1.10.116.1: #i68856# Added header markers and pch files

--- svx/source/editeng/edtspell.cxx
+++ svx/source/editeng/edtspell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edtspell.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:40:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:50:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include <eeng_pch.hxx>
 
