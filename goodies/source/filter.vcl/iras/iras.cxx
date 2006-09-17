d184eb601bad5ccd3f980e9a4c1bce5a5090f82d
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:52:37 +0000

    INTEGRATION: CWS pchfix02 (1.5.60); FILE MERGED 2006/09/01 17:30:13 kaib 1.5.60.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/iras/iras.cxx
+++ goodies/source/filter.vcl/iras/iras.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: iras.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-16 13:11:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:52:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <vcl/graph.hxx>
 #include <vcl/bmpacc.hxx>
@@ -387,7 +390,6 @@ extern "C" BOOL GraphicImport(SvStream & rStream, Graphic & rGraphic,
 
 //================== ein bischen Muell fuer Windows ==========================
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifdef WIN
