4222b2e03122b48c8426cb4aea81d40f207093e4
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:23:18 +0000

    INTEGRATION: CWS pchfix02 (1.6.34); FILE MERGED 2006/09/01 18:00:15 kaib 1.6.34.1: #i68856# Added header markers and pch files

--- xmloff/source/transform/EventOASISTContext.cxx
+++ xmloff/source/transform/EventOASISTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EventOASISTContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:52:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:23:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_EVENTOASISTCONTEXT_HXX
 #include "EventOASISTContext.hxx"
