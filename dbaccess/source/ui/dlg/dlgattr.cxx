c9abd95fac5f75d1d0cf63273aa5ffd0c5dd0fb6
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:09:05 +0000

    INTEGRATION: CWS pchfix02 (1.7.176); FILE MERGED 2006/09/01 17:24:28 kaib 1.7.176.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/dlgattr.cxx
+++ dbaccess/source/ui/dlg/dlgattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgattr.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:30:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:09:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 
 #ifdef MAC
