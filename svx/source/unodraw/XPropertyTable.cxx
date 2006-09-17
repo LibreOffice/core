6d46ef6e134ef0d2eff7a7b78308b206cec70bd3
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:10:37 +0000

    INTEGRATION: CWS pchfix02 (1.6.114); FILE MERGED 2006/09/01 17:47:37 kaib 1.6.114.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/XPropertyTable.cxx
+++ svx/source/unodraw/XPropertyTable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XPropertyTable.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:53:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:10:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_DRAWING_POLYPOLYGONBEZIERCOORDS_HPP_
 #include <com/sun/star/drawing/PolyPolygonBezierCoords.hpp>
