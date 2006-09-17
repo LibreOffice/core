1fa4fcadb5a6d90686398e81573e458a311231d7
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:53:00 +0000

    INTEGRATION: CWS pchfix02 (1.28.116); FILE MERGED 2006/09/01 17:46:39 kaib 1.28.116.1: #i68856# Added header markers and pch files

--- svx/source/editeng/impedit5.cxx
+++ svx/source/editeng/impedit5.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impedit5.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:42:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:53:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include <eeng_pch.hxx>
 
