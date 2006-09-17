bff5322fa0ebc970ce2f840d5371eb2be66cefa2
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:33:09 +0000

    INTEGRATION: CWS pchfix02 (1.6.18); FILE MERGED 2006/09/01 17:59:38 kaib 1.6.18.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/controlelement.cxx
+++ xmloff/source/forms/controlelement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: controlelement.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 09:22:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:33:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_FORMS_CONTROLELEMENT_HXX_
 #include "controlelement.hxx"
