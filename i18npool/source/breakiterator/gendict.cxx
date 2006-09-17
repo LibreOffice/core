3635e2c94d76672c521bdbe90ab84bb1c661caac
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:14:30 +0000

    INTEGRATION: CWS pchfix02 (1.7.8); FILE MERGED 2006/09/01 17:30:39 kaib 1.7.8.1: #i68856# Added header markers and pch files

--- i18npool/source/breakiterator/gendict.cxx
+++ i18npool/source/breakiterator/gendict.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gendict.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:27:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:14:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <stdio.h>
 #include <string.h>
