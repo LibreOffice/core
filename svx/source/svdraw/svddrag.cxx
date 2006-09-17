356ee509ebdd0d92532c556c22c652b80b56e574
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:48:06 +0000

    INTEGRATION: CWS pchfix02 (1.3.116); FILE MERGED 2006/09/01 17:47:19 kaib 1.3.116.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svddrag.cxx
+++ svx/source/svdraw/svddrag.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svddrag.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:34:37 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:48:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #include "svdview.hxx"
 
 #ifndef _SVDDRAG_HXX //autogen
