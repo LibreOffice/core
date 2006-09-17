491c1755edecc7748105cf2511fda7501324a11a
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:01:17 +0000

    INTEGRATION: CWS pchfix02 (1.52.2); FILE MERGED 2006/09/01 17:46:46 kaib 1.52.2.1: #i68856# Added header markers and pch files

--- svx/source/fmcomp/fmgridif.cxx
+++ svx/source/fmcomp/fmgridif.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmgridif.cxx,v $
  *
- *  $Revision: 1.52 $
+ *  $Revision: 1.53 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:38:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:01:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_SDBC_RESULTSETTYPE_HPP_
 #include <com/sun/star/sdbc/ResultSetType.hpp>
