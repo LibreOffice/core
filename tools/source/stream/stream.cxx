e193ccfef09f54efa3bd0b9d162f2e562f4e5be6
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:01:33 +0000

    INTEGRATION: CWS pchfix02 (1.23.42); FILE MERGED 2006/09/01 17:54:57 kaib 1.23.42.1: #i68856# Added header markers and pch files

--- tools/source/stream/stream.cxx
+++ tools/source/stream/stream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stream.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:51:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:01:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_tools.hxx"
 
 // ToDo:
 //  - Read->RefreshBuffer->Auf Aenderungen von nBufActualLen reagieren
