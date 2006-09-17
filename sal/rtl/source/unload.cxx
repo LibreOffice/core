2610fedfe5b5b731533dfc8961fb6e60f0aa1e6c
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:05:43 +0000

    INTEGRATION: CWS pchfix02 (1.10.10); FILE MERGED 2006/09/01 17:34:18 kaib 1.10.10.1: #i68856# Added header markers and pch files

--- sal/rtl/source/unload.cxx
+++ sal/rtl/source/unload.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unload.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:45:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:05:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 #ifndef _RTL_UNLOAD_H_
 #include <rtl/unload.h>
