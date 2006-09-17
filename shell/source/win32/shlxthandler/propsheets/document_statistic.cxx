45f00ee409fdc282a6c8b0fb74c73af70849798d
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:45:15 +0000

    INTEGRATION: CWS pchfix02 (1.4.98); FILE MERGED 2006/09/01 17:39:11 kaib 1.4.98.1: #i68856# Added header markers and pch files

--- shell/source/win32/shlxthandler/propsheets/document_statistic.cxx
+++ shell/source/win32/shlxthandler/propsheets/document_statistic.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: document_statistic.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:02:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:45:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #pragma warning (disable : 4786 4503)
 
