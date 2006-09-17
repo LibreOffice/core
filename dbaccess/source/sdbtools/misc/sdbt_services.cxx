25f175b7dd2b3db61d8ed400b79a6eb1d96a3011
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:50:41 +0000

    INTEGRATION: CWS pchfix02 (1.2.36); FILE MERGED 2006/09/01 17:24:16 kaib 1.2.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/sdbtools/misc/sdbt_services.cxx
+++ dbaccess/source/sdbtools/misc/sdbt_services.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdbt_services.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:21:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:50:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_MODULE_SDBT_HXX
 #include "module_sdbt.hxx"
