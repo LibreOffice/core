6636314347ed18ddbe60ea7d90cde0dda1cbf0e2
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:02:15 +0000

    INTEGRATION: CWS pchfix02 (1.4.48); FILE MERGED 2006/09/01 17:46:46 kaib 1.4.48.1: #i68856# Added header markers and pch files

--- svx/source/fmcomp/trace.cxx
+++ svx/source/fmcomp/trace.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: trace.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:00:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:02:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #ifndef _TRACE_HXX_
 #include "trace.hxx"
 #endif
