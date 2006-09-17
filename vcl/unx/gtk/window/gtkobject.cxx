36f8aaca812034bc36e8d05143a76d6715ad3d8a
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:30:31 +0000

    INTEGRATION: CWS pchfix02 (1.9.4); FILE MERGED 2006/09/01 17:58:02 kaib 1.9.4.1: #i68856# Added header markers and pch files

--- vcl/unx/gtk/window/gtkobject.cxx
+++ vcl/unx/gtk/window/gtkobject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gtkobject.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:46:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:30:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <plugins/gtk/gtkobject.hxx>
 #include <plugins/gtk/gtkframe.hxx>
