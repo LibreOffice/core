c0de2bc63842f04d26b4ecb51dae72525d89fef8
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:52:16 +0000

    INTEGRATION: CWS pchfix02 (1.5.140); FILE MERGED 2006/09/01 17:34:06 kaib 1.5.140.1: #i68856# Added header markers and pch files

--- sal/qa/osl/semaphore/osl_Semaphore.cxx
+++ sal/qa/osl/semaphore/osl_Semaphore.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: osl_Semaphore.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:37:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:52:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 //------------------------------------------------------------------------
 // include files
