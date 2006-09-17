4e6cab5a61cde5b25b145d1b1c8329621bcf06d5
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:56:45 +0000

    INTEGRATION: CWS pchfix02 (1.7.26); FILE MERGED 2006/09/01 17:43:10 kaib 1.7.26.1: #i68856# Added header markers and pch files

--- svtools/source/fsstor/fsstorage.cxx
+++ svtools/source/fsstor/fsstorage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fsstorage.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 11:15:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:56:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
 #include <com/sun/star/beans/PropertyValue.hpp>
