9064e6447458e45559e49c3c38a1e7842e9f95cc
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:41:18 +0000

    INTEGRATION: CWS pchfix02 (1.6.114); FILE MERGED 2006/09/01 17:30:02 kaib 1.6.114.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/egif/egif.cxx
+++ goodies/source/filter.vcl/egif/egif.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: egif.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:30:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:41:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <vcl/graph.hxx>
 #include <vcl/svapp.hxx>
@@ -598,7 +601,6 @@ extern "C" BOOL __LOADONCALLAPI DoExportDialog( FltCallDialogParameter& rPara )
 
 // ------------------------------------------------------------------------
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifdef WIN
