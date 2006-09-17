49f3c5e2e3015a5181df4715f4c1cee44e2ff0ec
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:29:24 +0000

    INTEGRATION: CWS pchfix02 (1.80.34); FILE MERGED 2006/09/01 17:32:43 kaib 1.80.34.1: #i68856# Added header markers and pch files

--- package/source/zippackage/ZipPackageFolder.cxx
+++ package/source/zippackage/ZipPackageFolder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ZipPackageFolder.cxx,v $
  *
- *  $Revision: 1.80 $
+ *  $Revision: 1.81 $
  *
- *  last change: $Author: rt $ $Date: 2005-11-10 15:51:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:29:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_package.hxx"
 #ifndef _ZIP_PACKAGE_FOLDER_HXX
 #include <ZipPackageFolder.hxx>
 #endif
