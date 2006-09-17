7f8546f9da4f27ee849d5a1cea9978e774028712
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:18:48 +0000

    INTEGRATION: CWS pchfix02 (1.7.536); FILE MERGED 2006/09/01 17:47:43 kaib 1.7.536.1: #i68856# Added header markers and pch files

--- svx/source/unoedit/unoviwed.cxx
+++ svx/source/unoedit/unoviwed.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoviwed.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:12:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:18:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SV_OUTDEV_HXX
 #include <vcl/outdev.hxx>
