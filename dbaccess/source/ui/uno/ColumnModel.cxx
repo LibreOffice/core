2b7826bcd1d347705eb1737a24ba5bed1cbb9d7c
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:32:19 +0000

    INTEGRATION: CWS pchfix02 (1.6.52); FILE MERGED 2006/09/01 17:24:43 kaib 1.6.52.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/uno/ColumnModel.cxx
+++ dbaccess/source/ui/uno/ColumnModel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ColumnModel.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:34:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:32:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_COLUMNMODEL_HXX
 #include "ColumnModel.hxx"
