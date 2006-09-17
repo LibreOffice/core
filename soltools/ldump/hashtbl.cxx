31073cf1c1e928a9b273519a519e03906a8b529f
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:34:24 +0000

    INTEGRATION: CWS pchfix02 (1.3.30); FILE MERGED 2006/09/01 17:40:17 kaib 1.3.30.1: #i68856# Added header markers and pch files

--- soltools/ldump/hashtbl.cxx
+++ soltools/ldump/hashtbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hashtbl.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:27:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:34:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_soltools.hxx"
 
 #include "hashtbl.hxx"
 #include <string.h>
