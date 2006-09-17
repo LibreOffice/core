35d9e0e1f3f8d70249bf2260ea6c8b585b7310af
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:18:52 +0000

    INTEGRATION: CWS pchfix02 (1.19.92); FILE MERGED 2006/09/01 17:42:43 kaib 1.19.92.1: #i68856# Added header markers and pch files

--- svtools/source/brwbox/datwin.cxx
+++ svtools/source/brwbox/datwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datwin.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:40:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:18:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 #include "datwin.hxx"
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifndef _APP_HXX //autogen
