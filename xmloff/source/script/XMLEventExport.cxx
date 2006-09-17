1467a764452cdb68bd413bc0c44ddf029be586b0
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:40:04 +0000

    INTEGRATION: CWS pchfix02 (1.16.34); FILE MERGED 2006/09/01 17:59:43 kaib 1.16.34.1: #i68856# Added header markers and pch files

--- xmloff/source/script/XMLEventExport.cxx
+++ xmloff/source/script/XMLEventExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLEventExport.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:22:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:40:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLEVENTEXPORT_HXX
 #include "XMLEventExport.hxx"
