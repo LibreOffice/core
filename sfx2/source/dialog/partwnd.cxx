1073e2d7a82706844e17960e5344c8fa427d3a74
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:34:26 +0000

    INTEGRATION: CWS pchfix02 (1.15.88); FILE MERGED 2006/09/01 17:38:35 kaib 1.15.88.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/partwnd.cxx
+++ sfx2/source/dialog/partwnd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: partwnd.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:23:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:34:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 // includes ******************************************************************
 
