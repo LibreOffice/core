663610dfaea488486a8c43d24d4e1272ee9aecfb
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:07:57 +0000

    INTEGRATION: CWS pchfix02 (1.28.58); FILE MERGED 2006/09/01 17:22:34 kaib 1.28.58.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/odbc/OTools.cxx
+++ connectivity/source/drivers/odbc/OTools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OTools.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:56:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:07:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_OTOOLS_HXX_
 #include "odbc/OTools.hxx"
