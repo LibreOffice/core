3ec9c50e5853fd232f0250b65c07be10b183cd58
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:58:10 +0000

    INTEGRATION: CWS pchfix02 (1.13.80); FILE MERGED 2006/09/01 17:18:35 kaib 1.13.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/dialogs/dlg_ObjectProperties.cxx
+++ chart2/source/controller/dialogs/dlg_ObjectProperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlg_ObjectProperties.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:10:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:58:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #ifndef _ZFORLIST_HXX
 #ifndef _ZFORLIST_DECLARE_TABLE
 #define _ZFORLIST_DECLARE_TABLE
