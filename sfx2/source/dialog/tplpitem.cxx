99d51f48ebb56412f32f6d6077e357bdac2eabc2
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:37:38 +0000

    INTEGRATION: CWS pchfix02 (1.8.88); FILE MERGED 2006/09/01 17:38:37 kaib 1.8.88.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/tplpitem.cxx
+++ sfx2/source/dialog/tplpitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tplpitem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:25:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:37:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,10 +33,12 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 // INCLUDE ---------------------------------------------------------------
 
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "tplpitem.hxx"
