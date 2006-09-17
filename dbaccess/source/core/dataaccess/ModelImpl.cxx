1d4b323a23adb515503cc1716a9480f37428d4a8
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:37:50 +0000

    INTEGRATION: CWS pchfix02 (1.16.4); FILE MERGED 2006/09/01 17:24:07 kaib 1.16.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/ModelImpl.cxx
+++ dbaccess/source/core/dataaccess/ModelImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ModelImpl.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:44:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:37:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_COREDATAACCESS_MODELIMPL_HXX_
 #include "ModelImpl.hxx"
