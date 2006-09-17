6ed1ca65db44f72f213a5c6201232b28e662464d
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:35:02 +0000

    INTEGRATION: CWS pchfix02 (1.4.32); FILE MERGED 2006/09/01 17:39:01 kaib 1.4.32.1: #i68856# Added header markers and pch files

--- shell/source/all/ooofilereader/autostyletag.cxx
+++ shell/source/all/ooofilereader/autostyletag.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: autostyletag.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:14:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:34:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef AUTOSTYLETAG_HXX_INCLUDED
 #include "autostyletag.hxx"
