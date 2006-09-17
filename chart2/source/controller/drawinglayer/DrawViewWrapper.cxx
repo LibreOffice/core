338b7cdd046d75b54cbbbbc507fbf1fac9384c11
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:00:47 +0000

    INTEGRATION: CWS pchfix02 (1.11.6); FILE MERGED 2006/09/01 17:18:36 kaib 1.11.6.1: #i68856# Added header markers and pch files

--- chart2/source/controller/drawinglayer/DrawViewWrapper.cxx
+++ chart2/source/controller/drawinglayer/DrawViewWrapper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DrawViewWrapper.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 13:00:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:00:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "DrawViewWrapper.hxx"
 #include "DrawModelWrapper.hxx"
 #include "ConfigurationAccess.hxx"
