b00c4c4f53fa5757e416dc0031532819872f636f
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:56:06 +0000

    INTEGRATION: CWS pchfix02 (1.7.22); FILE MERGED 2006/09/01 17:30:17 kaib 1.7.22.1: #i68856# Added header markers and pch files

--- goodies/source/unographic/descriptor.cxx
+++ goodies/source/unographic/descriptor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: descriptor.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-05 21:54:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:56:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include "descriptor.hxx"
 
