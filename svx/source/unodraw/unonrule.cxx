e351e691203416844a0f986b95c8d819c0dc59ea
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:13:40 +0000

    INTEGRATION: CWS pchfix02 (1.19.174); FILE MERGED 2006/09/01 17:47:39 kaib 1.19.174.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/unonrule.cxx
+++ svx/source/unodraw/unonrule.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unonrule.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2006-05-02 15:36:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:13:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define PROPERTY_NONE 0
 #define ITEMID_BRUSH        0
