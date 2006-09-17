2df70e35d272fe60709e372151925a37a0b8a968
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:38:24 +0000

    INTEGRATION: CWS pchfix02 (1.3.160); FILE MERGED 2006/09/01 17:59:42 kaib 1.3.160.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/strings.cxx
+++ xmloff/source/forms/strings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strings.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:16:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:38:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_FORMS_STRINGS_HXX_
 #define XMLFORM_IMPLEMENT_STRINGS
