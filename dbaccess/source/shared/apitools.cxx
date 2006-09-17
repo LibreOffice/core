a49ead2d0ab489863f9dadbe7f9396e4ddbcf129
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:50:56 +0000

    INTEGRATION: CWS pchfix02 (1.9.178); FILE MERGED 2006/09/01 17:24:17 kaib 1.9.178.1: #i68856# Added header markers and pch files

--- dbaccess/source/shared/apitools.cxx
+++ dbaccess/source/shared/apitools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: apitools.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:14:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:50:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBASHARED_APITOOLS_HXX_
 #include "apitools.hxx"
