598b26adbfef18657fc14a5a64a3a17319c0c75d
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:51:16 +0000

    INTEGRATION: CWS pchfix02 (1.3.122); FILE MERGED 2006/09/01 17:40:41 kaib 1.3.122.1: #i68856# Added header markers and pch files

--- starmath/source/action.cxx
+++ starmath/source/action.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: action.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 15:04:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:51:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-#pragma hdrstop
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_starmath.hxx"
+
 
 #include "action.hxx"
 #include "smdll.hxx"
