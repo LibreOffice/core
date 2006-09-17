1f861041e36f79da8be4af2f5e320a9c05cdc91d
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:38:44 +0000

    INTEGRATION: CWS pchfix02 (1.3.166); FILE MERGED 2006/09/01 17:22:14 kaib 1.3.166.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/flat/EPreparedStatement.cxx
+++ connectivity/source/drivers/flat/EPreparedStatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EPreparedStatement.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:00:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:38:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FLAT_EPREPAREDSTATEMENT_HXX_
 #include "flat/EPreparedStatement.hxx"
--- connectivity/source/drivers/flat/EStatement.cxx
+++ connectivity/source/drivers/flat/EStatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EStatement.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:01:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:38:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_FLAT_ESTATEMENT_HXX_
 #include "flat/EStatement.hxx"
