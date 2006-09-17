e3b49d9238c0ac53468a545cb1d20d1bfb14961f
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:46:50 +0000

    INTEGRATION: CWS pchfix02 (1.5.62); FILE MERGED 2006/09/01 17:34:01 kaib 1.5.62.1: #i68856# Added header markers and pch files

--- sal/osl/unx/uunxapi.cxx
+++ sal/osl/unx/uunxapi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uunxapi.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 08:07:15 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:46:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
  #ifndef _OSL_UUNXAPI_H_
  #include "uunxapi.h"
