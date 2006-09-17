ebe785444789a8a981643e1aa7c188d867cff6f5
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:10:14 +0000

    INTEGRATION: CWS pchfix02 (1.8.16); FILE MERGED 2006/09/01 17:40:29 kaib 1.8.16.1: #i68856# Added header markers and pch files

--- sot/source/sdstor/stgelem.cxx
+++ sot/source/sdstor/stgelem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stgelem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:54:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:09:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #include <string.h> // memset(), memcpy()
 #include <rtl/ustring.hxx>
--- sot/source/sdstor/stgole.cxx
+++ sot/source/sdstor/stgole.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stgole.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:55:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:10:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sot.hxx"
 
 #include "rtl/string.h"
 #include "rtl/string.h"
