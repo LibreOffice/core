ceacbe83dde06f51aaf0271be0eb530e3f5b796a
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:48:07 +0000

    INTEGRATION: CWS pchfix02 (1.10.34); FILE MERGED 2006/09/01 17:59:49 kaib 1.10.34.1: #i68856# Added header markers and pch files

--- xmloff/source/style/XMLBackgroundImageExport.cxx
+++ xmloff/source/style/XMLBackgroundImageExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLBackgroundImageExport.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:00:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:48:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _COM_SUN_STAR_STYLE_GRAPHICLOCATION_HPP_
 #include <com/sun/star/style/GraphicLocation.hpp>
