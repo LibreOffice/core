4dc75c9a3ac7ecab17959500075b44c4c1f77af7
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:50:46 +0000

    INTEGRATION: CWS pchfix02 (1.32.60); FILE MERGED 2006/09/01 17:57:35 kaib 1.32.60.1: #i68856# Added header markers and pch files

--- vcl/source/app/unohelp.cxx
+++ vcl/source/app/unohelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unohelp.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:45:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:50:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 
 
