8490dff2f83ebd1a29a995c1f7d874c73aef7e94
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:53:14 +0000

    INTEGRATION: CWS pchfix02 (1.52.114); FILE MERGED 2006/09/01 17:46:39 kaib 1.52.114.1: #i68856# Added header markers and pch files

--- svx/source/editeng/svxacorr.cxx
+++ svx/source/editeng/svxacorr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxacorr.cxx,v $
  *
- *  $Revision: 1.52 $
+ *  $Revision: 1.53 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:43:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:53:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define ITEMID_UNDERLINE    0
 #define ITEMID_WEIGHT       0
