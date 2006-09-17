e52ae2c3ed2a9f5b1870e30b4d30f01958fd3ecc
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:15:56 +0000

    INTEGRATION: CWS pchfix02 (1.2.4); FILE MERGED 2006/09/01 17:46:08 kaib 1.2.4.1: #i68856# Added header markers and pch files

--- svx/source/dialog/databaseregistrationui.cxx
+++ svx/source/dialog/databaseregistrationui.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: databaseregistrationui.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:37:41 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:15:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 // === includes (declaration) ============================================
 #ifndef SVX_DATABASE_REGISTRATION_UI_HXX
