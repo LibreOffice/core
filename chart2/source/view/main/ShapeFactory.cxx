ac794bfefd6e5f303972f032f49ad737903c5757
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:38:06 +0000

    INTEGRATION: CWS pchfix02 (1.19.78); FILE MERGED 2006/09/01 17:19:01 kaib 1.19.78.1: #i68856# Added header markers and pch files

--- chart2/source/view/main/ShapeFactory.cxx
+++ chart2/source/view/main/ShapeFactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ShapeFactory.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:50:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:38:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "ShapeFactory.hxx"
 #include "ViewDefines.hxx"
 #include "Stripe.hxx"
