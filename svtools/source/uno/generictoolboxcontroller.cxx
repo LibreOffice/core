051bb6cc141840c0e5ffd4bae8b077a9601f856d
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:29:17 +0000

    INTEGRATION: CWS pchfix02 (1.4.92); FILE MERGED 2006/09/01 17:43:33 kaib 1.4.92.1: #i68856# Added header markers and pch files

--- svtools/source/uno/generictoolboxcontroller.cxx
+++ svtools/source/uno/generictoolboxcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: generictoolboxcontroller.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:13:20 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:29:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef __SVTOOLS_GENERICTOOLBOXCONTROLLER_HXX_
 #include "generictoolboxcontroller.hxx"
