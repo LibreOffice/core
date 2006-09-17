3075dc460a07cbe7ca2eb09328f16cde01e3d5be
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:14:57 +0000

    INTEGRATION: CWS pchfix02 (1.12.8); FILE MERGED 2006/09/01 17:31:15 kaib 1.12.8.1: #i68856# Added header markers and pch files

--- idlc/source/idlcproduce.cxx
+++ idlc/source/idlcproduce.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idlcproduce.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:49:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:14:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_idlc.hxx"
 #ifndef _IDLC_IDLC_HXX_
 #include <idlc/idlc.hxx>
 #endif
