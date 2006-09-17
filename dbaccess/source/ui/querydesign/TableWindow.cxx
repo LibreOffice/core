d114e459ea594626a7953f4c1c86479be543f564
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:25:29 +0000

    INTEGRATION: CWS pchfix02 (1.32.36); FILE MERGED 2006/09/01 17:24:38 kaib 1.32.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/TableWindow.cxx
+++ dbaccess/source/ui/querydesign/TableWindow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TableWindow.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:44:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:25:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_TABLEWINDOW_HXX
 #include "TableWindow.hxx"
 #endif
