42cf71a8fd3e2d502d2830f1ff238c21913b29f1
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:30:23 +0000

    INTEGRATION: CWS pchfix02 (1.18.92); FILE MERGED 2006/09/01 17:42:51 kaib 1.18.92.1: #i68856# Added header markers and pch files

--- svtools/source/config/syslocaleoptions.cxx
+++ svtools/source/config/syslocaleoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: syslocaleoptions.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 11:11:55 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:30:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifdef  SVL_DLLIMPLEMENTATION
 #undef  SVL_DLLIMPLEMENTATION
