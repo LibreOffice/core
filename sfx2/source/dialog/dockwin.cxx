e9d4abcb786697870c6db290c5f58c1a51acff32
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:31:42 +0000

    INTEGRATION: CWS pchfix02 (1.40.84); FILE MERGED 2006/09/01 17:38:33 kaib 1.40.84.1: #i68856# Added header markers and pch files

--- sfx2/source/dialog/dockwin.cxx
+++ sfx2/source/dialog/dockwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dockwin.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:21:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:31:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _SFXENUMITEM_HXX //autogen
 #include <svtools/eitem.hxx>
