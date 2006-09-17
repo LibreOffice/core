cbd09912ba88426d08e90edca97f4ec54d8e0263
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:52:05 +0000

    INTEGRATION: CWS pchfix02 (1.8.18); FILE MERGED 2006/09/01 17:32:08 kaib 1.8.18.1: #i68856# Added header markers and pch files

--- linguistic/source/convdic.cxx
+++ linguistic/source/convdic.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: convdic.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 13:46:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:52:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
 
 #ifndef INCLUDED_I18NPOOL_LANG_H
 #include <i18npool/lang.h>
