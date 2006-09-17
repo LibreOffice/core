5e7ceab9fbc7f1d4a03ccf709aabbcb576d1d744
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:48:23 +0000

    INTEGRATION: CWS pchfix02 (1.16.116); FILE MERGED 2006/09/01 17:46:36 kaib 1.16.116.1: #i68856# Added header markers and pch files

--- svx/source/editeng/editdbg.cxx
+++ svx/source/editeng/editdbg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editdbg.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:38:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:48:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #include <eeng_pch.hxx>
 
