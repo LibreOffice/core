10469fae774794233af8d682aabcf49b08f253ef
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:01:47 +0000

    INTEGRATION: CWS pchfix02 (1.6.34); FILE MERGED 2006/09/01 17:59:59 kaib 1.6.34.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLAutoTextContainerEventImport.cxx
+++ xmloff/source/text/XMLAutoTextContainerEventImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLAutoTextContainerEventImport.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:37:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:01:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLAUTOTEXTCONTAINEREVENTIMPORT_HXX
 #include "XMLAutoTextContainerEventImport.hxx"
