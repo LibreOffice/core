f2e573ea158c5e715ddc0354ea0b4309be1400b1
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:39:41 +0000

    INTEGRATION: CWS pchfix02 (1.43.4); FILE MERGED 2006/09/01 17:58:09 kaib 1.43.4.1: #i68856# Added header markers and pch files

--- vcl/unx/source/gdi/salprnpsp.cxx
+++ vcl/unx/source/gdi/salprnpsp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salprnpsp.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:52:03 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:39:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 /**
   this file implements the sal printer interface ( SalPrinter, SalInfoPrinter
