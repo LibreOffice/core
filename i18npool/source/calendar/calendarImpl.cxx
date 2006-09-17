a54c2718c62d10d139e5fd7d10183b0dbc907888
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:14:57 +0000

    INTEGRATION: CWS pchfix02 (1.12.16); FILE MERGED 2006/09/01 17:30:39 kaib 1.12.16.1: #i68856# Added header markers and pch files

--- i18npool/source/calendar/calendarImpl.cxx
+++ i18npool/source/calendar/calendarImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: calendarImpl.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:42:51 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:14:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include "calendarImpl.hxx"
 #include "localedata.hxx"
