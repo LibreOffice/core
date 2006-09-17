f5ffe271be8431421cdc2dc2406acec91e3f5cd5
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:49:52 +0000

    INTEGRATION: CWS pchfix02 (1.49.128); FILE MERGED 2006/09/01 17:57:35 kaib 1.49.128.1: #i68856# Added header markers and pch files

--- vcl/source/app/svdata.cxx
+++ vcl/source/app/svdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdata.cxx,v $
  *
- *  $Revision: 1.49 $
+ *  $Revision: 1.50 $
  *
- *  last change: $Author: vg $ $Date: 2006-05-18 10:08:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:49:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #include <string.h>
 
 #ifndef _SV_SVSYS_HXX
