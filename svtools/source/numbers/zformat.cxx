0b5c8abb9655fb19f2cbce05f5025f3a52f1b855
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:21:54 +0000

    INTEGRATION: CWS pchfix02 (1.70.26); FILE MERGED 2006/09/01 17:43:27 kaib 1.70.26.1: #i68856# Added header markers and pch files

--- svtools/source/numbers/zformat.cxx
+++ svtools/source/numbers/zformat.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: zformat.cxx,v $
  *
- *  $Revision: 1.70 $
+ *  $Revision: 1.71 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 15:52:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:21:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #include <stdio.h>
 #include <ctype.h>
 #include <float.h>
