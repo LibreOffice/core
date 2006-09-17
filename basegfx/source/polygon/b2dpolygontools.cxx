fadfa62c7d9584af5e55de98d9db6a5e49c26929
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:02:00 +0000

    INTEGRATION: CWS pchfix02 (1.22.10); FILE MERGED 2006/09/01 17:16:36 kaib 1.22.10.1: #i68856# Added header markers and pch files

--- basegfx/source/polygon/b2dpolygontools.cxx
+++ basegfx/source/polygon/b2dpolygontools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dpolygontools.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:43:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:02:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_NUMERIC_FTOOLS_HXX
 #include <basegfx/numeric/ftools.hxx>
