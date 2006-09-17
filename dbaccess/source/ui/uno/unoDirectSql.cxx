1b1048566135569621b6a6d1bbe9004b7f353d2d
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:34:20 +0000

    INTEGRATION: CWS pchfix02 (1.5.178); FILE MERGED 2006/09/01 17:24:44 kaib 1.5.178.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/uno/unoDirectSql.cxx
+++ dbaccess/source/ui/uno/unoDirectSql.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoDirectSql.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:47:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:34:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_UNODIRECTSQL_HXX
 #include "unoDirectSql.hxx"
