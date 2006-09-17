936d92294ae132e63e5135ea89536dcfc74cb50a
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:28:36 +0000

    INTEGRATION: CWS pchfix02 (1.50.34); FILE MERGED 2006/09/01 17:46:16 kaib 1.50.34.1: #i68856# Added header markers and pch files

--- svx/source/dialog/numpages.cxx
+++ svx/source/dialog/numpages.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: numpages.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 15:53:31 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:28:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
