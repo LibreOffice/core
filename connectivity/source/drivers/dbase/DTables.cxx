9f25d0e322d88d4e123ce491321becb71b9a3e2d
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:24:03 +0000

    INTEGRATION: CWS pchfix02 (1.25.42); FILE MERGED 2006/09/01 17:22:04 kaib 1.25.42.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/dbase/DTables.cxx
+++ connectivity/source/drivers/dbase/DTables.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DTables.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:26:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:24:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_DBASE_TABLES_HXX_
 #include "dbase/DTables.hxx"
