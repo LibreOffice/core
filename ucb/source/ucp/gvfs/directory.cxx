0664f1da5fbaf45244500cba5e57169a09c5acc2
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:54:55 +0000

    INTEGRATION: CWS pchfix02 (1.4.20); FILE MERGED 2006/09/01 17:55:46 kaib 1.4.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/gvfs/directory.cxx
+++ ucb/source/ucp/gvfs/directory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: directory.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:27:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:54:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 /*
  * This file pinched from webdavdatasupplier (etc.)
  * cut & paste + new getData impl. & collate ResultSet code.
