92e0ca97a45db64cbd4aea9241b80fa0bec3eac8
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:30:57 +0000

    INTEGRATION: CWS pchfix02 (1.13.16); FILE MERGED 2006/09/01 17:56:26 kaib 1.13.16.1: #i68856# Added header markers and pch files

--- unotools/source/ucbhelper/xtempfile.cxx
+++ unotools/source/ucbhelper/xtempfile.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xtempfile.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:10:54 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:30:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef _XTEMPFILE_HXX
 #include <XTempFile.hxx>
