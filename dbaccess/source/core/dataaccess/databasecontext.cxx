0c09ad8a7498a5fa2581d22702f34ab106f169f9
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:39:14 +0000

    INTEGRATION: CWS pchfix02 (1.34.32); FILE MERGED 2006/09/01 17:24:08 kaib 1.34.32.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/databasecontext.cxx
+++ dbaccess/source/core/dataaccess/databasecontext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: databasecontext.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 15:21:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:39:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_COREDATAACCESS_DATABASECONTEXT_HXX_
 #include "databasecontext.hxx"
