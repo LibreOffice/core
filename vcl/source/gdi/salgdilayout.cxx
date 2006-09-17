007937f54d4bc138f3717c6f3095605d715ebad1
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:10:36 +0000

    INTEGRATION: CWS pchfix02 (1.24.4); FILE MERGED 2006/09/01 17:57:48 kaib 1.24.4.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/salgdilayout.cxx
+++ vcl/source/gdi/salgdilayout.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salgdilayout.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-11 17:45:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:10:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <tools/ref.hxx>
 #ifndef _SV_SVSYS_HXX
