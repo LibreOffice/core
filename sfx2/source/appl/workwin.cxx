68edcea30402025506b3b1fe85be17a1d308686a
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:21:59 +0000

    INTEGRATION: CWS pchfix02 (1.63.14); FILE MERGED 2006/09/01 17:38:26 kaib 1.63.14.1: #i68856# Added header markers and pch files

--- sfx2/source/appl/workwin.cxx
+++ sfx2/source/appl/workwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: workwin.cxx,v $
  *
- *  $Revision: 1.64 $
+ *  $Revision: 1.65 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-07 16:45:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:21:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
+
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "docfile.hxx"
