7da476cc4c072e86ea756443dbc1d22b7b5fccf2
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:53:18 +0000

    INTEGRATION: CWS pchfix02 (1.10.16); FILE MERGED 2006/09/01 17:30:14 kaib 1.10.16.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/itiff/itiff.cxx
+++ goodies/source/filter.vcl/itiff/itiff.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: itiff.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 17:13:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:53:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #ifndef _SV_GRAPH_HXX
 #include <vcl/graph.hxx>
@@ -1291,7 +1294,6 @@ extern "C" BOOL GraphicImport(SvStream & rStream, Graphic & rGraphic,
 
 //============================= fuer Windows ==================================
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #ifdef WIN
