7dde4602ec2ee9318c9796b6c79c2800d1c73664
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:28:08 +0000

    INTEGRATION: CWS pchfix02 (1.7.64); FILE MERGED 2006/09/01 17:46:16 kaib 1.7.64.1: #i68856# Added header markers and pch files

--- svx/source/dialog/multipat.cxx
+++ svx/source/dialog/multipat.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: multipat.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 10:40:36 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:28:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifdef SVX_DLLIMPLEMENTATION
 #undef SVX_DLLIMPLEMENTATION
