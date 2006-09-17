de4879a3f26f2ecda693fde7dfb0df9833d6fe7d
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:40:54 +0000

    INTEGRATION: CWS pchfix02 (1.9.116); FILE MERGED 2006/09/01 17:47:15 kaib 1.9.116.1: #i68856# Added header markers and pch files

--- svx/source/sdr/properties/customshapeproperties.cxx
+++ svx/source/sdr/properties/customshapeproperties.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: customshapeproperties.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-15 12:05:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:40:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_PROPERTIES_CUSTOMSHAPEPROPERTIES_HXX
 #include <svx/sdr/properties/customshapeproperties.hxx>
