a0944253f01394fce9233ed0a3a005540d3a78cf
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:25:00 +0000

    INTEGRATION: CWS pchfix02 (1.18.92); FILE MERGED 2006/09/01 17:42:47 kaib 1.18.92.1: #i68856# Added header markers and pch files

--- svtools/source/config/historyoptions.cxx
+++ svtools/source/config/historyoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: historyoptions.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:44:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:24:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,8 +32,10 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 //_________________________________________________________________________________________________________________
