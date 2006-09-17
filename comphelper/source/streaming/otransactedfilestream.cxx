33a13c23b57d225a37ba2f9ebe3caad0dcf52f1b
Oliver Bolte <obo@openoffice.org>
2006-09-17 16:20:54 +0000

    INTEGRATION: CWS pchfix02 (1.4.36); FILE MERGED 2006/09/01 17:19:49 kaib 1.4.36.1: #i68856# Added header markers and pch files

--- comphelper/source/streaming/otransactedfilestream.cxx
+++ comphelper/source/streaming/otransactedfilestream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: otransactedfilestream.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 22:53:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 17:20:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
