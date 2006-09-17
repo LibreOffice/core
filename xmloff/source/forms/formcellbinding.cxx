16d025760bc3ecd50fbf49f898cd0ec09a3d0167
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:34:57 +0000

    INTEGRATION: CWS pchfix02 (1.6.34); FILE MERGED 2006/09/01 17:59:39 kaib 1.6.34.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/formcellbinding.cxx
+++ xmloff/source/forms/formcellbinding.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formcellbinding.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:18:22 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:34:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef XMLOFF_FORMS_FORMCELLBINDING
 #include "formcellbinding.hxx"
