3115266d56f6db2524dce36b24105b2424e9b2f2
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:00:34 +0000

    INTEGRATION: CWS pchfix02 (1.5.116); FILE MERGED 2006/09/01 17:45:57 kaib 1.5.116.1: #i68856# Added header markers and pch files

--- svx/source/accessibility/AccessibleFrameSelector.cxx
+++ svx/source/accessibility/AccessibleFrameSelector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleFrameSelector.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:52:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:00:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_ACCESSIBLEFRAMESELECTOR_HXX
 #include "AccessibleFrameSelector.hxx"
