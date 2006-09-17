d29e807025a907126e2ab528e3cc88229d59203d
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:31:02 +0000

    INTEGRATION: CWS pchfix02 (1.2.106); FILE MERGED 2006/09/01 17:22:09 kaib 1.2.106.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/evoab2/NTable.cxx
+++ connectivity/source/drivers/evoab2/NTable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: NTable.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-28 10:34:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:31:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,9 @@
  *
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "NTable.hxx"
 #ifndef _CONNECTIVITY_EVOAB_TABLES_HXX_
