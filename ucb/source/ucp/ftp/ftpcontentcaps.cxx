2a224f134d746ad5adb174a83ff6dc4b8729d8b1
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:49:52 +0000

    INTEGRATION: CWS pchfix02 (1.8.20); FILE MERGED 2006/09/01 17:55:43 kaib 1.8.20.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/ftp/ftpcontentcaps.cxx
+++ ucb/source/ucp/ftp/ftpcontentcaps.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ftpcontentcaps.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:23:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:49:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 #include <com/sun/star/beans/Property.hpp>
 #include <com/sun/star/beans/PropertyAttribute.hpp>
