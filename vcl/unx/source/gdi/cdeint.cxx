a99e03f8c4079005637b4d1443407aa6d15a1fac
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:37:08 +0000

    INTEGRATION: CWS pchfix02 (1.11.320); FILE MERGED 2006/09/01 17:58:07 kaib 1.11.320.1: #i68856# Added header markers and pch files

--- vcl/unx/source/gdi/cdeint.cxx
+++ vcl/unx/source/gdi/cdeint.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cdeint.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-01 10:38:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:37:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
