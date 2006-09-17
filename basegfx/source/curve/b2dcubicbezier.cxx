e9cf81b204443f9dcc1c55a7cf85eb761a03ecfa
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:58:29 +0000

    INTEGRATION: CWS pchfix02 (1.11.28); FILE MERGED 2006/09/01 17:16:34 kaib 1.11.28.1: #i68856# Added header markers and pch files

--- basegfx/source/curve/b2dcubicbezier.cxx
+++ basegfx/source/curve/b2dcubicbezier.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b2dcubicbezier.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:56:14 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:58:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _BGFX_CURVE_B2DCUBICBEZIER_HXX
 #include <basegfx/curve/b2dcubicbezier.hxx>
