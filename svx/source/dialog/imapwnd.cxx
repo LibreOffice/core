c08c3786a5a91f2e8260f1794ed9e833551d4512
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:25:24 +0000

    INTEGRATION: CWS pchfix02 (1.17.48); FILE MERGED 2006/09/01 17:46:14 kaib 1.17.48.1: #i68856# Added header markers and pch files

--- svx/source/dialog/imapwnd.cxx
+++ svx/source/dialog/imapwnd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imapwnd.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:49:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:25:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _URLOBJ_HXX //autogen
 #include <tools/urlobj.hxx>
