d419a44af2d9d81566dbe98375fc38aaf0068d16
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:50:18 +0000

    INTEGRATION: CWS pchfix02 (1.3.118); FILE MERGED 2006/09/01 17:30:10 kaib 1.3.118.1: #i68856# Added header markers and pch files

--- goodies/source/filter.vcl/idxf/dxftblrd.cxx
+++ goodies/source/filter.vcl/idxf/dxftblrd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dxftblrd.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:56:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:50:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #include <string.h>
 #include <dxftblrd.hxx>
