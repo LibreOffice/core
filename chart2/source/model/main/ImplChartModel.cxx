1926d700f737135cb0b3bda8be4b09401d9fa0de
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:12:37 +0000

    INTEGRATION: CWS pchfix02 (1.9.80); FILE MERGED 2006/09/01 17:18:44 kaib 1.9.80.1: #i68856# Added header markers and pch files

--- chart2/source/model/main/ImplChartModel.cxx
+++ chart2/source/model/main/ImplChartModel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImplChartModel.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:00:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:12:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "ImplChartModel.hxx"
 #include "CachedDataSequence.hxx"
 #include "DataSeries.hxx"
