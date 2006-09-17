f2bb9d9efa36adf4bdac015fa964b7a9bcee8696
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:54:55 +0000

    INTEGRATION: CWS pchfix02 (1.71.84); FILE MERGED 2006/09/01 17:47:24 kaib 1.71.84.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdograf.cxx
+++ svx/source/svdraw/svdograf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdograf.cxx,v $
  *
- *  $Revision: 1.73 $
+ *  $Revision: 1.74 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:30:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:54:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define _ANIMATION
 #define ITEMID_GRF_CROP 0
