3df40edf4e94f477875aa9ec6d2cfebacc275707
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:24:29 +0000

    INTEGRATION: CWS pchfix02 (1.8.16); FILE MERGED 2006/09/01 17:30:49 kaib 1.8.16.1: #i68856# Added header markers and pch files

--- i18npool/source/textconversion/textconversion_zh.cxx
+++ i18npool/source/textconversion/textconversion_zh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textconversion_zh.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 04:49:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:24:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_i18npool.hxx"
 
 #include <assert.h>
 #include <textconversion.hxx>
