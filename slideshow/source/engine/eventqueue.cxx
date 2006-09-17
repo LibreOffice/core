df78a515ec6975ef4b752d5d5b78ad21af829359
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:25:30 +0000

    INTEGRATION: CWS pchfix02 (1.10.10); FILE MERGED 2006/09/01 17:39:30 kaib 1.10.10.1: #i68856# Added header markers and pch files

--- slideshow/source/engine/eventqueue.cxx
+++ slideshow/source/engine/eventqueue.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eventqueue.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:25:33 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:25:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
 
 // must be first
 #include <canvas/debug.hxx>
