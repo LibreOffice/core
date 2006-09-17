4e24e127e85179d88d99169799cc71bdf3c28b17
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:59:58 +0000

    INTEGRATION: CWS pchfix02 (1.37.18); FILE MERGED 2006/09/01 17:59:58 kaib 1.37.18.1: #i68856# Added header markers and pch files

--- xmloff/source/style/xmlnumi.cxx
+++ xmloff/source/style/xmlnumi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlnumi.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 16:37:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:59:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
