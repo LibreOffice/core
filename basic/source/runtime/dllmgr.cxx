be4d7610358f5eb2e696972871eaeea4d2ecfbad
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:04:35 +0000

    INTEGRATION: CWS pchfix02 (1.8.24); FILE MERGED 2006/09/01 17:17:04 kaib 1.8.24.1: #i68856# Added header markers and pch files

--- basic/source/runtime/dllmgr.cxx
+++ basic/source/runtime/dllmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dllmgr.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:45:06 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:04:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 #include <stdlib.h>
 
