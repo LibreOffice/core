3654bc2a8ca69b630a620e62bc8f43f3db2a7d24
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:34:34 +0000

    INTEGRATION: CWS pchfix02 (1.23.36); FILE MERGED 2006/09/01 17:24:05 kaib 1.23.36.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/definitioncolumn.cxx
+++ dbaccess/source/core/api/definitioncolumn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: definitioncolumn.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 15:05:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:34:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_COREAPI_DEFINITIONSETTINGS_HXX_
 #include "definitioncolumn.hxx"
