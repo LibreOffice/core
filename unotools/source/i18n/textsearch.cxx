f896c4dc64d8d63500c3726989350d3b3be4960f
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:26:10 +0000

    INTEGRATION: CWS pchfix02 (1.11.8); FILE MERGED 2006/09/01 17:56:23 kaib 1.11.8.1: #i68856# Added header markers and pch files

--- unotools/source/i18n/textsearch.cxx
+++ unotools/source/i18n/textsearch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textsearch.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 11:50:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:26:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef INCLUDED_I18NPOOL_MSLANGID_HXX
 #include <i18npool/mslangid.hxx>
