9977dace7c30cf2d9d7ae1dfd84b6539516dd364
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:32:56 +0000

    INTEGRATION: CWS pchfix02 (1.8.46); FILE MERGED 2006/09/01 17:43:36 kaib 1.8.46.1: #i68856# Added header markers and pch files

--- svtools/workben/svdem.cxx
+++ svtools/workben/svdem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-19 14:53:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:32:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #include <stdio.h>
 #include <cppuhelper/servicefactory.hxx>
 #include <comphelper/processfactory.hxx>
