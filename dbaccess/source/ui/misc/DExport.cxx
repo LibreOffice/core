5e7beeee29b7e97a26aa672093dd1421613a3cb6
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:12:25 +0000

    INTEGRATION: CWS pchfix02 (1.33.34); FILE MERGED 2006/09/01 17:24:31 kaib 1.33.34.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/misc/DExport.cxx
+++ dbaccess/source/ui/misc/DExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DExport.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:07:59 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:12:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_DATABASEEXPORT_HXX
 #include "DExport.hxx"
 #endif
