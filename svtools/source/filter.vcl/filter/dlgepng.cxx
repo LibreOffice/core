5a9aece36572bb1d3a5112e1ac78dff2be405876
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:51:25 +0000

    INTEGRATION: CWS pchfix02 (1.4.92); FILE MERGED 2006/09/01 17:43:05 kaib 1.4.92.1: #i68856# Added header markers and pch files

--- svtools/source/filter.vcl/filter/dlgepng.cxx
+++ svtools/source/filter.vcl/filter/dlgepng.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgepng.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:04:53 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:51:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 #include <tools/ref.hxx>
 #include <vcl/msgbox.hxx>
 #include "dlgepng.hxx"
