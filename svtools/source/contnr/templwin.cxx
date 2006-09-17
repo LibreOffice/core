ab24663f89650cd3dd0206facc792e15456f1fd8
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:35:32 +0000

    INTEGRATION: CWS pchfix02 (1.73.58); FILE MERGED 2006/09/01 17:42:54 kaib 1.73.58.1: #i68856# Added header markers and pch files

--- svtools/source/contnr/templwin.cxx
+++ svtools/source/contnr/templwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: templwin.cxx,v $
  *
- *  $Revision: 1.73 $
+ *  $Revision: 1.74 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 12:06:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:35:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _SVTOOLS_TEMPLWIN_HXX
 #include "templwin.hxx"
