98791c458c5e5ec9112fb1bd0dbfadcce7745378
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:33:39 +0000

    INTEGRATION: CWS pchfix02 (1.6.184); FILE MERGED 2006/09/01 17:24:05 kaib 1.6.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/api/callablestatement.cxx
+++ dbaccess/source/core/api/callablestatement.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: callablestatement.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 10:04:49 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:33:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef _DBA_COREAPI_CALLABLESTATEMENT_HXX_
 #include <callablestatement.hxx>
 #endif
