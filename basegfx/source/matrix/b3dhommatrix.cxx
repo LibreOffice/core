dab0221202d706c55100f6a66699487b831f5f69
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:59:09 +0000

    INTEGRATION: CWS pchfix02 (1.10.10); FILE MERGED 2006/09/01 17:16:34 kaib 1.10.10.1: #i68856# Added header markers and pch files

--- basegfx/source/matrix/b3dhommatrix.cxx
+++ basegfx/source/matrix/b3dhommatrix.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dhommatrix.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 09:56:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:59:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef INCLUDED_RTL_INSTANCE_HXX
 #include <rtl/instance.hxx>
