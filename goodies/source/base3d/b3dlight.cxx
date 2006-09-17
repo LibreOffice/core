7141cbdd453507491dced81a3515026b4b27d7fa
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:38:05 +0000

    INTEGRATION: CWS pchfix02 (1.4.26); FILE MERGED 2006/09/01 17:30:00 kaib 1.4.26.1: #i68856# Added header markers and pch files

--- goodies/source/base3d/b3dlight.cxx
+++ goodies/source/base3d/b3dlight.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: b3dlight.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:39:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:38:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_goodies.hxx"
 
 #ifndef _B3D_B3DLIGHT_HXX
 #include "b3dlight.hxx"
