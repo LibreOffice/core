8bb480624bae84a190aa51b2c6ddaa75f37f6865
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:49:28 +0000

    INTEGRATION: CWS pchfix02 (1.3.10); FILE MERGED 2006/09/01 17:25:19 kaib 1.3.10.1: #i68856# Added header markers and pch files

--- desktop/source/splash/firststart.cxx
+++ desktop/source/splash/firststart.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: firststart.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 11:13:29 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:49:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "firststart.hxx"
 #include "../migration/wizard.hxx"
