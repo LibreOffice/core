683808d29af3eabd25cfddd007f1bb07dee72356
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:33:37 +0000

    INTEGRATION: CWS pchfix02 (1.6.536); FILE MERGED 2006/09/01 17:47:10 kaib 1.6.536.1: #i68856# Added header markers and pch files

--- svx/source/sdr/animation/scheduler.cxx
+++ svx/source/sdr/animation/scheduler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scheduler.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:02:02 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:33:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _SDR_ANIMATION_SCHEDULER_HXX
 #include <svx/sdr/animation/scheduler.hxx>
