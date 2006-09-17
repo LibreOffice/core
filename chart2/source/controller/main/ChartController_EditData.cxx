fb7083351e12e07d914298f497df65d99517d301
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:04:28 +0000

    INTEGRATION: CWS pchfix02 (1.3.80); FILE MERGED 2006/09/01 17:18:39 kaib 1.3.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/main/ChartController_EditData.cxx
+++ chart2/source/controller/main/ChartController_EditData.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChartController_EditData.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:32:01 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:04:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "ChartController.hxx"
 #include "macros.hxx"
 
