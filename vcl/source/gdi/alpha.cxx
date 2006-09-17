f1567450a134674ba6f966c64888017a91377bfe
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:56:24 +0000

    INTEGRATION: CWS pchfix02 (1.8.112); FILE MERGED 2006/09/01 17:57:39 kaib 1.8.112.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/alpha.cxx
+++ vcl/source/gdi/alpha.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: alpha.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 19:19:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:56:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 #ifndef _DEBUG_HXX
 #include <tools/debug.hxx>
 #endif
