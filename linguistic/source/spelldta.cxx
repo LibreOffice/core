cc8ae16f42db42c5f0d38c9160a44575effe03c8
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:55:35 +0000

    INTEGRATION: CWS pchfix02 (1.4.46); FILE MERGED 2006/09/01 17:32:10 kaib 1.4.46.1: #i68856# Added header markers and pch files

--- linguistic/source/spelldta.cxx
+++ linguistic/source/spelldta.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spelldta.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:55:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:55:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_linguistic.hxx"
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
 #include <com/sun/star/uno/Reference.h>
