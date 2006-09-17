b4286ca863a3dfc118e30cbeecda90a25b151f71
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:43:31 +0000

    INTEGRATION: CWS pchfix02 (1.9.60); FILE MERGED 2006/09/01 17:22:17 kaib 1.9.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/hsqldb/StorageNativeOutputStream.cxx
+++ connectivity/source/drivers/hsqldb/StorageNativeOutputStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: StorageNativeOutputStream.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:32:25 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:43:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #if defined(HAVE_CONFIG_H) && HAVE_CONFIG_H
 #include <config.h>
 #endif
