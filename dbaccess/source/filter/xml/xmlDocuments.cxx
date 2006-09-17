9cf3cd9cfe8a28b930d4e05d80aafe06ac3695dc
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:46:48 +0000

    INTEGRATION: CWS pchfix02 (1.4.178); FILE MERGED 2006/09/01 17:24:14 kaib 1.4.178.1: #i68856# Added header markers and pch files

--- dbaccess/source/filter/xml/xmlDocuments.cxx
+++ dbaccess/source/filter/xml/xmlDocuments.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlDocuments.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-23 12:10:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:46:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBA_XMLDOCUMENTS_HXX
 #include "xmlDocuments.hxx"
 #endif
