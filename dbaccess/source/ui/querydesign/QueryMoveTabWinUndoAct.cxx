8d97baff4c7f34d9cb4cc52cf7bb5ba52b2e84d2
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:22:28 +0000

    INTEGRATION: CWS pchfix02 (1.3.184); FILE MERGED 2006/09/01 17:24:37 kaib 1.3.184.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/QueryMoveTabWinUndoAct.cxx
+++ dbaccess/source/ui/querydesign/QueryMoveTabWinUndoAct.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: QueryMoveTabWinUndoAct.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:24:44 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:22:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBAUI_OQUERYMOVETABWINUNDOACT_HXX
 #include "QueryMoveTabWinUndoAct.hxx"
