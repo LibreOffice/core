00cc0658c7bd3fe9284810e08fad40cd0206ab90
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:02:17 +0000

    INTEGRATION: CWS pchfix02 (1.8.80); FILE MERGED 2006/09/01 17:18:37 kaib 1.8.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/itemsetwrapper/ItemConverter.cxx
+++ chart2/source/controller/itemsetwrapper/ItemConverter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ItemConverter.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:29:26 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:02:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "ItemConverter.hxx"
 #include "macros.hxx"
 
