4178f0068bbd833ec2357830c27a7522bf9775f2
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:56:33 +0000

    INTEGRATION: CWS pchfix02 (1.13.70); FILE MERGED 2006/09/01 17:55:47 kaib 1.13.70.1: #i68856# Added header markers and pch files

--- ucb/source/ucp/hierarchy/hierarchyprovider.cxx
+++ ucb/source/ucp/hierarchy/hierarchyprovider.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hierarchyprovider.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:48:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:56:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_ucb.hxx"
 
 /**************************************************************************
                                 TODO
