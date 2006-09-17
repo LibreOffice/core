e0fed3966f4d8cb6aa763f6c4057ebe5aeff6426
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:49:12 +0000

    INTEGRATION: CWS pchfix02 (1.18.4); FILE MERGED 2006/09/01 17:22:48 kaib 1.18.4.1: #i68856# Added header markers and pch files

--- cppcanvas/source/mtfrenderer/implrenderer.cxx
+++ cppcanvas/source/mtfrenderer/implrenderer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: implrenderer.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2006-08-09 07:55:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:49:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #include <canvas/debug.hxx>
 #include <canvas/verbosetrace.hxx>
