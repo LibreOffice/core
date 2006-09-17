b57d1f955bafbd2360c6e1acc9cf895d2559d278
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:57:53 +0000

    INTEGRATION: CWS pchfix02 (1.5.90); FILE MERGED 2006/09/01 17:43:11 kaib 1.5.90.1: #i68856# Added header markers and pch files

--- svtools/source/hatchwindow/ipwin.cxx
+++ svtools/source/hatchwindow/ipwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ipwin.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:09:56 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:57:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
