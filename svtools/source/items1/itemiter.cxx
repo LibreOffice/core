6e5b38d41b8f399c72958b39dd4bea03cea0261e
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:04:59 +0000

    INTEGRATION: CWS pchfix02 (1.3.326); FILE MERGED 2006/09/01 17:43:15 kaib 1.3.326.1: #i68856# Added header markers and pch files

--- svtools/source/items1/itemiter.cxx
+++ svtools/source/items1/itemiter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: itemiter.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:05:34 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:04:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,10 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
+
 #ifndef GCC
-#pragma hdrstop
 #endif
 
 #include "itemiter.hxx"
