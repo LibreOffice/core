2b2990ff754e1ba753d8b887364f539fa287d1bc
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:58:10 +0000

    INTEGRATION: CWS pchfix02 (1.8.40); FILE MERGED 2006/09/01 17:25:36 kaib 1.8.40.1: #i68856# Added header markers and pch files

--- dtrans/source/win32/dnd/globals.cxx
+++ dtrans/source/win32/dnd/globals.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: globals.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:16:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:58:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dtrans.hxx"
 
 #ifndef _COM_SUN_STAR_DATATRANSFER_DND_DNDCONSTANTS_HPP_
 #include <com/sun/star/datatransfer/dnd/DNDConstants.hpp>
