f04765a89414274020cce66d984a0c746af02b3d
Oliver Bolte <obo@openoffice.org>
2006-09-17 15:38:04 +0000

    INTEGRATION: CWS pchfix02 (1.3.312); FILE MERGED 2006/09/01 17:38:38 kaib 1.3.312.1: #i68856# Added header markers and pch files

--- sfx2/source/doc/QuerySaveDocument.cxx
+++ sfx2/source/doc/QuerySaveDocument.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: QuerySaveDocument.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:35:43 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 16:38:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 #include "QuerySaveDocument.hxx"
 
 #include "sfx.hrc"
