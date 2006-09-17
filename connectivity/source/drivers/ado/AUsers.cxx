a68c2c26dcf8711ea273dfb94eeda63bab9f5f15
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:16:28 +0000

    INTEGRATION: CWS pchfix02 (1.10.42); FILE MERGED 2006/09/01 17:21:58 kaib 1.10.42.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/ado/AUsers.cxx
+++ connectivity/source/drivers/ado/AUsers.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AUsers.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:24:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:16:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_ADO_USERS_HXX_
 #include "ado/AUsers.hxx"
