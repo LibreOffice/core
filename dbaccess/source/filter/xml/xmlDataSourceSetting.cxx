7fedd646b63a0ac7ec9830f2b9a965c7a79a1a14
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:46:07 +0000

    INTEGRATION: CWS pchfix02 (1.8.4); FILE MERGED 2006/09/01 17:24:13 kaib 1.8.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/filter/xml/xmlDataSourceSetting.cxx
+++ dbaccess/source/filter/xml/xmlDataSourceSetting.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlDataSourceSetting.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:47:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:46:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBA_XMLDATASOURCESETTING_HXX
 #include "xmlDataSourceSetting.hxx"
 #endif
