4d72029e6ed021f098aea31c9f6ce82bcf670d91
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:09:45 +0000

    INTEGRATION: CWS pchfix02 (1.16.48); FILE MERGED 2006/09/01 17:24:29 kaib 1.16.48.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/dsselect.cxx
+++ dbaccess/source/ui/dlg/dsselect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dsselect.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:07:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:09:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_ODBC_CONFIG_HXX_
 #include "odbcconfig.hxx"
