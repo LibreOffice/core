048cc68808f89f37090ef73c117c0771240f5cac
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:21:48 +0000

    INTEGRATION: CWS pchfix02 (1.9.60); FILE MERGED 2006/09/01 17:22:02 kaib 1.9.60.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/dbase/DDriver.cxx
+++ connectivity/source/drivers/dbase/DDriver.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DDriver.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:19:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:21:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef _CONNECTIVITY_DBASE_DDRIVER_HXX_
 #include "dbase/DDriver.hxx"
 #endif
