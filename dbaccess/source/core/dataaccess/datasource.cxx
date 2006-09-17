c8858eba2f02f986dca919cb487272191c5406a8
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:39:41 +0000

    INTEGRATION: CWS pchfix02 (1.71.4); FILE MERGED 2006/09/01 17:24:09 kaib 1.71.4.1: #i68856# Added header markers and pch files

--- dbaccess/source/core/dataaccess/datasource.cxx
+++ dbaccess/source/core/dataaccess/datasource.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datasource.cxx,v $
  *
- *  $Revision: 1.71 $
+ *  $Revision: 1.72 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-15 10:44:50 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:39:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBA_COREDATAACCESS_DATASOURCE_HXX_
 #include "datasource.hxx"
