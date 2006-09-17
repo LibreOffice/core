7885d1f94b7cca15bb58bca3449dc57723b22ad1
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:03:29 +0000

    INTEGRATION: CWS pchfix02 (1.4.140); FILE MERGED 2006/09/01 17:34:16 kaib 1.4.140.1: #i68856# Added header markers and pch files

--- sal/qa/rtl_strings/rtl_old_teststrbuf.cxx
+++ sal/qa/rtl_strings/rtl_old_teststrbuf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rtl_old_teststrbuf.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:58:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:03:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
 
 // LLA:
 // this file is converted to use with testshl2
