5af19d45c9cad330f4992fd601def80007a93fcb
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:29:13 +0000

    INTEGRATION: CWS pchfix02 (1.6.58); FILE MERGED 2006/09/01 17:56:25 kaib 1.6.58.1: #i68856# Added header markers and pch files

--- unotools/source/streaming/streamwrap.cxx
+++ unotools/source/streaming/streamwrap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: streamwrap.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:49:58 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:29:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_unotools.hxx"
 
 #ifndef _UTL_STREAM_WRAPPER_HXX_
 #include <unotools/streamwrap.hxx>
