8ca65d8c6b479a12a0b1f2f6db7607e9a56b4eea
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:34:57 +0000

    INTEGRATION: CWS pchfix02 (1.3.88); FILE MERGED 2006/09/01 17:25:07 kaib 1.3.88.1: #i68856# Added header markers and pch files

--- desktop/source/app/checkinstall.cxx
+++ desktop/source/app/checkinstall.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: checkinstall.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 14:43:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:34:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #include "checkinstall.hxx"
 
