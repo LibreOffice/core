e01810c68489e94b046a2ff2c82c2938e17e1432
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:33:35 +0000

    INTEGRATION: CWS pchfix02 (1.23.34); FILE MERGED 2006/09/01 17:59:38 kaib 1.23.34.1: #i68856# Added header markers and pch files

--- xmloff/source/forms/controlpropertymap.cxx
+++ xmloff/source/forms/controlpropertymap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: controlpropertymap.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:16:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:33:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
