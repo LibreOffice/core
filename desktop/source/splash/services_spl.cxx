be779325a6c109148f4f9e4b086c95b7c7e03b2f
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:49:42 +0000

    INTEGRATION: CWS pchfix02 (1.2.222); FILE MERGED 2006/09/01 17:25:19 kaib 1.2.222.1: #i68856# Added header markers and pch files

--- desktop/source/splash/services_spl.cxx
+++ desktop/source/splash/services_spl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: services_spl.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:49:40 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:49:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
 
 #ifndef _COM_SUN_STAR_BEANS_NAMEDVALUE_HPP_
 #include <com/sun/star/beans/NamedValue.hpp>
