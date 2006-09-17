0fae11cfe3de43ae965ddef4273e3a7c3c6cf390
Oliver Bolte <obo@openoffice.org>
2006-09-17 03:57:03 +0000

    INTEGRATION: CWS pchfix02 (1.37.114); FILE MERGED 2006/09/01 17:46:42 kaib 1.37.114.1: #i68856# Added header markers and pch files

--- svx/source/engine3d/obj3d.cxx
+++ svx/source/engine3d/obj3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: obj3d.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-29 14:33:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 04:57:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #define ITEMID_COLOR            SID_ATTR_3D_LIGHTCOLOR
 
