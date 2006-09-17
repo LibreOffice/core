6ee4232232efa1a50ec12f7bc5d5ea24688826ef
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:11:04 +0000

    INTEGRATION: CWS pchfix02 (1.5.26); FILE MERGED 2006/09/01 17:17:08 kaib 1.5.26.1: #i68856# Added header markers and pch files

--- basic/source/sbx/sbxobj.cxx
+++ basic/source/sbx/sbxobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbxobj.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:51:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:11:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
