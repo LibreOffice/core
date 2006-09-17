2908e08e17bcbfc0d4f2cc405ea5d916c4b54328
Oliver Bolte <obo@openoffice.org>
2006-09-17 13:55:15 +0000

    INTEGRATION: CWS pchfix02 (1.30.90); FILE MERGED 2006/09/01 17:43:09 kaib 1.30.90.1: #i68856# Added header markers and pch files

--- svtools/source/filter.vcl/wmf/enhwmf.cxx
+++ svtools/source/filter.vcl/wmf/enhwmf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: enhwmf.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 21:08:08 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 14:55:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #include "winmtf.hxx"
 #ifndef _OSL_ENDIAN_H_
