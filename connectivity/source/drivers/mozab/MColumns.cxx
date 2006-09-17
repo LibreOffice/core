2652cec8f4f73a3cf40a4c71acb5a9701df5a749
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:55:37 +0000

    INTEGRATION: CWS pchfix02 (1.4.166); FILE MERGED 2006/09/01 17:22:26 kaib 1.4.166.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/mozab/MColumns.cxx
+++ connectivity/source/drivers/mozab/MColumns.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MColumns.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:16:30 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:55:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef _CONNECTIVITY_MOZAB_COLUMNS_HXX_
 #include "MColumns.hxx"
