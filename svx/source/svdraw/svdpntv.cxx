b65e3d6e34eeb721090020efbbaae78d2ff662e0
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:00:05 +0000

    INTEGRATION: CWS pchfix02 (1.27.70); FILE MERGED 2006/09/01 17:47:28 kaib 1.27.70.1: #i68856# Added header markers and pch files

--- svx/source/svdraw/svdpntv.cxx
+++ svx/source/svdraw/svdpntv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdpntv.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:30:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:00:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_AWT_XWINDOW_HPP_
 #include <com/sun/star/awt/XWindow.hpp>
