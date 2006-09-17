cc168a85faca8391298c7038b768d5e507358cd2
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:08:25 +0000

    INTEGRATION: CWS pchfix02 (1.15.60); FILE MERGED 2006/09/01 17:22:34 kaib 1.15.60.1: #i68856# Added header markers and pch files

--- connectivity/source/manager/mdrivermanager.cxx
+++ connectivity/source/manager/mdrivermanager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mdrivermanager.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:07:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:08:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include <stdio.h>
 
