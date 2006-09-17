7c9906770ad91d0f4e6d72cedc9af2ffe53ae046
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:01:33 +0000

    INTEGRATION: CWS pchfix02 (1.23.24); FILE MERGED 2006/09/01 17:17:02 kaib 1.23.24.1: #i68856# Added header markers and pch files

--- basic/source/comp/dim.cxx
+++ basic/source/comp/dim.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dim.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:41:17 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:01:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 #include <sbx.hxx>
 #include "sbcomp.hxx"
 
