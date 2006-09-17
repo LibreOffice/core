dfcc9da04c39902e9f4603d0506f81d29e1dccbf
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:10:39 +0000

    INTEGRATION: CWS pchfix02 (1.15.50); FILE MERGED 2006/09/01 17:24:29 kaib 1.15.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/dlg/indexfieldscontrol.cxx
+++ dbaccess/source/ui/dlg/indexfieldscontrol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: indexfieldscontrol.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:08:28 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:10:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef _DBAUI_INDEXFIELDSCONTROL_HXX_
 #include "indexfieldscontrol.hxx"
