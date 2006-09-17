1e928fcec2c0ceaf004f03acb404a8df3225c5b5
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:42:02 +0000

    INTEGRATION: CWS pchfix02 (1.17.12); FILE MERGED 2006/09/01 17:27:11 kaib 1.17.12.1: #i68856# Added header markers and pch files

--- filter/source/pdf/impdialog.cxx
+++ filter/source/pdf/impdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impdialog.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 09:28:48 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:42:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #include "impdialog.hxx"
 #include "impdialog.hrc"
