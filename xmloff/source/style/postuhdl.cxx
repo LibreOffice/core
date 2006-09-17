cbc22a2c03cb06f0e22cffbdefdb102b23d7584d
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:56:12 +0000

    INTEGRATION: CWS pchfix02 (1.4.160); FILE MERGED 2006/09/01 17:59:55 kaib 1.4.160.1: #i68856# Added header markers and pch files

--- xmloff/source/style/postuhdl.cxx
+++ xmloff/source/style/postuhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: postuhdl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:46:12 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:56:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_PROPERTYHANDLER_POSTURETYPES_HXX
 #include <postuhdl.hxx>
