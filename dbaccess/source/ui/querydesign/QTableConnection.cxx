7941abee2d1f36536d7a1c047c97bec91ff6c13a
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:21:16 +0000

    INTEGRATION: CWS pchfix02 (1.4.182); FILE MERGED 2006/09/01 17:24:36 kaib 1.4.182.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/querydesign/QTableConnection.cxx
+++ dbaccess/source/ui/querydesign/QTableConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: QTableConnection.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:21:05 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:21:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 #ifndef DBAUI_QUERYTABLECONNECTION_HXX
 #include "QTableConnection.hxx"
 #endif
