8aa5c0779d0d7b9b58dfd8f00299f4bd2317a796
Oliver Bolte <obo@openoffice.org>
2006-09-17 02:14:18 +0000

    INTEGRATION: CWS pchfix02 (1.3.166); FILE MERGED 2006/09/01 17:22:38 kaib 1.3.166.1: #i68856# Added header markers and pch files

--- connectivity/source/simpledbt/charset_s.cxx
+++ connectivity/source/simpledbt/charset_s.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: charset_s.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:45:52 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:13:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_DBTOOLS_CHARSET_S_HXX
 #include "charset_s.hxx"
--- connectivity/source/simpledbt/refbase.cxx
+++ connectivity/source/simpledbt/refbase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: refbase.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:48:18 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 03:14:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #ifndef CONNECTIVITY_DBTOOLS_REFBASE_HXX
 #include "refbase.hxx"
