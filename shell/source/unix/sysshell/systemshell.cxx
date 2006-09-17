641c195cab9781fc6eddd6bc944a72021841d263
Oliver Bolte <obo@openoffice.org>
2006-09-17 00:42:56 +0000

    INTEGRATION: CWS pchfix02 (1.6.32); FILE MERGED 2006/09/01 17:39:08 kaib 1.6.32.1: #i68856# Added header markers and pch files

--- shell/source/unix/sysshell/systemshell.cxx
+++ shell/source/unix/sysshell/systemshell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: systemshell.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 14:20:23 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 01:42:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
 
 #ifndef _SYSTEMSHELL_HXX_
 #include "systemshell.hxx"
