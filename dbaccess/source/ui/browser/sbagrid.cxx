6baaa2b921e54409a618682d2582d5dda47d3f97
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:58:58 +0000

    INTEGRATION: CWS pchfix02 (1.75.50); FILE MERGED 2006/09/01 17:24:22 kaib 1.75.50.1: #i68856# Added header markers and pch files

--- dbaccess/source/ui/browser/sbagrid.cxx
+++ dbaccess/source/ui/browser/sbagrid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbagrid.cxx,v $
  *
- *  $Revision: 1.75 $
+ *  $Revision: 1.76 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:58:21 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:58:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_SBA_GRID_HRC
 #include "sbagrid.hrc"
