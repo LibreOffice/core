784302572eb141511fa903cfb2d1298abdf4fb20
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:04:45 +0000

    INTEGRATION: CWS pchfix02 (1.8.60); FILE MERGED 2006/09/01 17:21:50 kaib 1.8.60.1: #i68856# Added header markers and pch files

--- connectivity/source/cpool/ZPooledConnection.cxx
+++ connectivity/source/cpool/ZPooledConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ZPooledConnection.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 01:07:27 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:04:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 #ifndef CONNECTIVITY_POOLEDCONNECTION_HXX
 #include "ZPooledConnection.hxx"
 #endif
