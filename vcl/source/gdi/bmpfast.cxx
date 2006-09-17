34583f2b1c588c39c8cdbda8961774b1853900e1
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:59:00 +0000

    INTEGRATION: CWS pchfix02 (1.5.110); FILE MERGED 2006/09/01 17:57:41 kaib 1.5.110.1: #i68856# Added header markers and pch files

--- vcl/source/gdi/bmpfast.cxx
+++ vcl/source/gdi/bmpfast.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bmpfast.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-08 08:34:09 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:59:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <bmpfast.hxx>
 
