8e8e22d4cd3de64543ccf00cc4716faf4ee37c6c
Oliver Bolte <obo@openoffice.org>
2006-09-17 06:48:05 +0000

    INTEGRATION: CWS pchfix02 (1.7.108); FILE MERGED 2006/09/01 17:27:16 kaib 1.7.108.1: #i68856# Added header markers and pch files

--- filter/source/xsltdialog/xmlfilterdialogcomponent.cxx
+++ filter/source/xsltdialog/xmlfilterdialogcomponent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlfilterdialogcomponent.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-09 15:05:13 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 07:48:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_filter.hxx"
 
 #ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
