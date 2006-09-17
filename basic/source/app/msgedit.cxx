9b1462bc8a7c14127b7e6c832b1e5c0ad2dac3f7
Oliver Bolte <obo@openoffice.org>
2006-09-17 08:56:59 +0000

    INTEGRATION: CWS pchfix02 (1.23.26); FILE MERGED 2006/09/01 17:16:58 kaib 1.23.26.1: #i68856# Added header markers and pch files

--- basic/source/app/msgedit.cxx
+++ basic/source/app/msgedit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: msgedit.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 17:36:19 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 09:56:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 /*************************************************************************
 File Versions so far:
 No Version          Initial Implementation without Version Information
