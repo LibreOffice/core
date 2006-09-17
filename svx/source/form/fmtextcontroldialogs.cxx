134b8e5620156043dee8dfc311b0e28acb4c7fed
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:10:06 +0000

    INTEGRATION: CWS pchfix02 (1.7.116); FILE MERGED 2006/09/01 17:46:52 kaib 1.7.116.1: #i68856# Added header markers and pch files

--- svx/source/form/fmtextcontroldialogs.cxx
+++ svx/source/form/fmtextcontroldialogs.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmtextcontroldialogs.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 15:58:11 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:10:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_SOURCE_FORM_FMTEXTCONTROLDIALOGS_HXX
 #include "fmtextcontroldialogs.hxx"
