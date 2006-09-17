2a133224b1e956cd043679e4318c3ea6319ac864
Oliver Bolte <obo@openoffice.org>
2006-09-17 04:27:13 +0000

    INTEGRATION: CWS pchfix02 (1.135.112); FILE MERGED 2006/09/01 17:47:05 kaib 1.135.112.1: #i68856# Added header markers and pch files

--- svx/source/msfilter/msdffimp.cxx
+++ svx/source/msfilter/msdffimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: msdffimp.cxx,v $
  *
- *  $Revision: 1.135 $
+ *  $Revision: 1.136 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 16:19:47 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 05:27:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
 
 #ifndef _COM_SUN_STAR_EMBED_ASPECTS_HPP_
