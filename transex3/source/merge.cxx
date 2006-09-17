af5f72d2b9f2c07940e628f57355fbc1850e1af9
Oliver Bolte <obo@openoffice.org>
2006-09-17 07:19:21 +0000

    INTEGRATION: CWS pchfix02 (1.26.2); FILE MERGED 2006/09/01 17:55:14 kaib 1.26.2.1: #i68856# Added header markers and pch files

--- transex3/source/merge.cxx
+++ transex3/source/merge.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: merge.cxx,v $
  *
- *  $Revision: 1.26 $
+ *  $Revision: 1.27 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:23:24 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 08:19:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_transex3.hxx"
 #include <stdio.h>
 #include <tools/fsys.hxx>
 #include "export.hxx"
