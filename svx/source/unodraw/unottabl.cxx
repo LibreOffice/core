6fb92f45a569ee857c2323f575ba14cfae4cd3cc
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:16:05 +0000

    INTEGRATION: CWS pchfix02 (1.8.536); FILE MERGED 2006/09/01 17:47:41 kaib 1.8.536.1: #i68856# Added header markers and pch files

--- svx/source/unodraw/unottabl.cxx
+++ svx/source/unodraw/unottabl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unottabl.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 01:09:16 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:16:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef _COM_SUN_STAR_AWT_GRADIENT_HPP_
 #include <com/sun/star/awt/Gradient.hpp>
