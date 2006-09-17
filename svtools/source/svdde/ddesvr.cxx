a4e9a7c81ba2f8bd4d57bd7dfb320854c7c87faf
Oliver Bolte <obo@openoffice.org>
2006-09-17 14:25:38 +0000

    INTEGRATION: CWS pchfix02 (1.5.324); FILE MERGED 2006/09/01 17:43:30 kaib 1.5.324.1: #i68856# Added header markers and pch files

--- svtools/source/svdde/ddesvr.cxx
+++ svtools/source/svdde/ddesvr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ddesvr.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:46:04 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 15:25:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 #define UNICODE
 #include "ddeimp.hxx"
