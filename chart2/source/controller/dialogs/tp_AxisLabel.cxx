ff68d1cd691d5640b71d7c6aea0359966365773c
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:58:36 +0000

    INTEGRATION: CWS pchfix02 (1.5.80); FILE MERGED 2006/09/01 17:18:35 kaib 1.5.80.1: #i68856# Added header markers and pch files

--- chart2/source/controller/dialogs/tp_AxisLabel.cxx
+++ chart2/source/controller/dialogs/tp_AxisLabel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tp_AxisLabel.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:13:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:58:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "tp_AxisLabel.hxx"
 
 #include "ResId.hxx"
