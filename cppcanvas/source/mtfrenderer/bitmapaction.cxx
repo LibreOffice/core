83c80ad6ff32d8ba1293e7713c915554c13745ab
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:49:25 +0000

    INTEGRATION: CWS pchfix02 (1.8.24); FILE MERGED 2006/09/01 17:22:48 kaib 1.8.24.1: #i68856# Added header markers and pch files

--- cppcanvas/source/mtfrenderer/bitmapaction.cxx
+++ cppcanvas/source/mtfrenderer/bitmapaction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bitmapaction.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 13:39:39 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:48:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_cppcanvas.hxx"
 
 #include <rtl/logfile.hxx>
 #include <com/sun/star/rendering/XBitmap.hpp>
