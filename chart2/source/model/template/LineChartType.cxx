52bfe753a05753b2191899c53a0caba4ca5c30ee
Oliver Bolte <obo@openoffice.org>
2006-09-17 12:18:58 +0000

    INTEGRATION: CWS pchfix02 (1.7.80); FILE MERGED 2006/09/01 17:18:48 kaib 1.7.80.1: #i68856# Added header markers and pch files

--- chart2/source/model/template/LineChartType.cxx
+++ chart2/source/model/template/LineChartType.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LineChartType.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:21:00 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 13:18:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
 #include "LineChartType.hxx"
 #include "PropertyHelper.hxx"
 #include "algohelper.hxx"
