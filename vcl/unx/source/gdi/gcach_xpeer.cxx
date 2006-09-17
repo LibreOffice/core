25fb00d207d858b313a61964fc226a89b2608601
Oliver Bolte <obo@openoffice.org>
2006-09-17 11:37:36 +0000

    INTEGRATION: CWS pchfix02 (1.42.110); FILE MERGED 2006/09/01 17:58:07 kaib 1.42.110.1: #i68856# Added header markers and pch files

--- vcl/unx/source/gdi/gcach_xpeer.cxx
+++ vcl/unx/source/gdi/gcach_xpeer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gcach_xpeer.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 16:20:38 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 12:37:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #include <rtl/ustring.hxx>
 #include <osl/module.h>
