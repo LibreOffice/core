d21a6d50794f50b82a17b58641af607b5badc22b
Oliver Bolte <obo@openoffice.org>
2006-09-17 09:25:23 +0000

    INTEGRATION: CWS pchfix02 (1.10.34); FILE MERGED 2006/09/01 17:59:31 kaib 1.10.34.1: #i68856# Added header markers and pch files

--- xmloff/source/draw/XMLImageMapContext.cxx
+++ xmloff/source/draw/XMLImageMapContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLImageMapContext.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:07:45 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 10:24:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLOFF_XMLIMAGEMAPCONTEXT_HXX_
 #include "XMLImageMapContext.hxx"
