940cacc524891fa57fdc38aeaf0d0894aece3d76
Oliver Bolte <obo@openoffice.org>
2006-09-17 05:05:25 +0000

    INTEGRATION: CWS pchfix02 (1.32.48); FILE MERGED 2006/09/01 17:47:33 kaib 1.32.48.1: #i68856# Added header markers and pch files

--- svx/source/tbxctrls/grafctrl.cxx
+++ svx/source/tbxctrls/grafctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: grafctrl.cxx,v $
  *
- *  $Revision: 1.32 $
+ *  $Revision: 1.33 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 12:57:57 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 06:05:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 #define ITEMID_SIZE 0
 #define ITEMID_BRUSH 0
 #define ITEMID_GRF_CROP SID_ATTR_GRAF_CROP
