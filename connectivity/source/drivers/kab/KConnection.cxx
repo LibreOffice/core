837cd76692c6dfdd9210524f9c8d2cdc080896b2
Oliver Bolte <obo@openoffice.org>
2006-09-17 01:52:11 +0000

    INTEGRATION: CWS pchfix02 (1.6.22); FILE MERGED 2006/09/01 17:22:23 kaib 1.6.22.1: #i68856# Added header markers and pch files

--- connectivity/source/drivers/kab/KConnection.cxx
+++ connectivity/source/drivers/kab/KConnection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: KConnection.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-01 10:23:42 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 02:51:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_connectivity.hxx"
 
 #include "KConnection.hxx"
 #ifndef _CONNECTIVITY_KAB_DATABASEMETADATA_HXX_
