387f682e6430680bc4a35530d95e5abc14e28158
Oliver Bolte <obo@openoffice.org>
2006-09-17 10:16:26 +0000

    INTEGRATION: CWS pchfix02 (1.2.158); FILE MERGED 2006/09/01 18:00:09 kaib 1.2.158.1: #i68856# Added header markers and pch files

--- xmloff/source/text/XMLTextTableContext.cxx
+++ xmloff/source/text/XMLTextTableContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextTableContext.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:26:46 $
+ *  last change: $Author: obo $ $Date: 2006-09-17 11:16:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmloff.hxx"
 
 #ifndef _XMLTEXTTABLECONTEXT_HXX
 #include "XMLTextTableContext.hxx"
