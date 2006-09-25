72971416b2a7708d728404efaa63bbdf87ef4ae4
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 12:29:33 +0000

    INTEGRATION: CWS obo05 (1.23.8); FILE MERGED 2006/08/28 13:47:48 obo 1.23.8.1: #i53611# diable warnings C4917 and 4555

--- embedserv/source/embed/docholder.cxx
+++ embedserv/source/embed/docholder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docholder.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:39:29 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 13:29:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+#if defined(_MSC_VER) && (_MSC_VER > 1310)
+#pragma warning(disable : 4917 4555)
+#endif
 
 
 #include "xwin.hxx"
