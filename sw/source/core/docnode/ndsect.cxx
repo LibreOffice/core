626c0e029bb8e7980c192a41143df202fbe4829f
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 08:26:51 +0000

    INTEGRATION: CWS osxpatchpool (1.27.14); FILE MERGED 2006/08/28 12:33:40 obr 1.27.14.1: #i68810# applied 2nd patch from issue (tra)

--- sw/source/core/docnode/ndsect.cxx
+++ sw/source/core/docnode/ndsect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ndsect.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:00:38 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 09:26:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -272,7 +272,7 @@ SwSection* SwDoc::Insert( const SwPaM& rRange, const SwSection& rNew,
     SwSectionNode* pNewSectNode = 0;
 
     IDocumentRedlineAccess::RedlineMode_t eOld = GetRedlineMode();
-    SetRedlineMode_intern( (eOld & ~IDocumentRedlineAccess::REDLINE_SHOW_MASK) | IDocumentRedlineAccess::REDLINE_IGNORE );
+    SetRedlineMode_intern( (IDocumentRedlineAccess::RedlineMode_t)((eOld & ~IDocumentRedlineAccess::REDLINE_SHOW_MASK) | IDocumentRedlineAccess::REDLINE_IGNORE));
 
     if( rRange.HasMark() )
     {
