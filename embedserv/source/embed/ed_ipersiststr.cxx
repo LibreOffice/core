36e319ce5fffb3d1eb7f999a060f05e49b3d62b3
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 12:30:01 +0000

    INTEGRATION: CWS obo05 (1.19.8); FILE MERGED 2006/08/28 13:47:49 obo 1.19.8.1: #i53611# diable warnings C4917 and 4555

--- embedserv/source/embed/ed_ipersiststr.cxx
+++ embedserv/source/embed/ed_ipersiststr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ed_ipersiststr.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:40:15 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 13:30:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+#if defined(_MSC_VER) && (_MSC_VER > 1310)
+#pragma warning(disable : 4917 4555)
+#endif
 
 #include "embeddoc.hxx"
 
