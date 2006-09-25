af5a14eca29c2aa73049701664193a869178e9c1
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 08:53:26 +0000

    INTEGRATION: CWS dmake46 (1.3.2); FILE MERGED 2006/09/21 20:57:37 vq 1.3.2.2: #i67689# Release dmake 4.6. 2006/07/23 04:27:59 vq 1.3.2.1: #i67689# Bump version to 4.6-cvs.

--- dmake/winnt/microsft/config.h
+++ dmake/winnt/microsft/config.h
@@ -1,4 +1,4 @@
-/* RCS  $Id: config.h,v 1.3 2006-06-29 11:32:54 ihi Exp $
+/* RCS  $Id: config.h,v 1.4 2006-09-25 09:53:26 vg Exp $
 --
 -- SYNOPSIS
 --      Configurarion include file.
@@ -27,7 +27,7 @@
 
 /* Name and version number of this package */
 #define PACKAGE "dmake"
-#define VERSION "4.5"
+#define VERSION "4.6"
 #define BUILDINFO "Windows / MS Visual C++"
 
 #if defined (_MSC_VER)
