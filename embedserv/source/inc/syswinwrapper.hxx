8e7805c4c94c2982ef519cc045edb0394026fc76
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 12:32:29 +0000

    INTEGRATION: CWS obo05 (1.3.34); FILE MERGED 2006/08/28 13:48:35 obo 1.3.34.1: #i53611# diable warnings C4917 and 4555

--- embedserv/source/inc/syswinwrapper.hxx
+++ embedserv/source/inc/syswinwrapper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: syswinwrapper.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:57:00 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 13:32:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -183,4 +183,3 @@ protected:
     };
 
 }
-
