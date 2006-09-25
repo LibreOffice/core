2b868b5d5a412ca107c04a05ff7aa4d70f75347d
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 12:30:46 +0000

    INTEGRATION: CWS obo05 (1.4.8); FILE MERGED 2006/08/28 13:47:49 obo 1.4.8.1: #i53611# diable warnings C4917 and 4555

--- embedserv/source/embed/register.cxx
+++ embedserv/source/embed/register.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: register.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:41:18 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 13:30:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+#if defined(_MSC_VER) && (_MSC_VER > 1310)
+#pragma warning(disable : 4917 4555)
+#endif
 
 #include "servprov.hxx"
 
