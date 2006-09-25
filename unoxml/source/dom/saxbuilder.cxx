6919055a5309c92ce91c174e1d87ee85571eca94
Vladimir Glazounov <vg@openoffice.org>
2006-09-25 12:03:02 +0000

    INTEGRATION: CWS obo05 (1.4.6); FILE MERGED 2006/08/28 13:00:20 obo 1.4.6.1: #53611# warning C4701 disabled

--- unoxml/source/dom/saxbuilder.cxx
+++ unoxml/source/dom/saxbuilder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saxbuilder.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:48:22 $
+ *  last change: $Author: vg $ $Date: 2006-09-25 13:03:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+#if defined(_MSC_VER) && (_MSC_VER > 1310)
+#pragma warning(disable : 4701)
+#endif
 
 #include <stdio.h>
 #include "node.hxx"
