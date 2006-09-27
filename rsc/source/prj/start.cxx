bbce00a743e3a6ae48f723eda06f0ebb59708568
Vladimir Glazounov <vg@openoffice.org>
2006-09-27 08:52:13 +0000

    INTEGRATION: CWS jam01 (1.5.36); FILE MERGED 2006/07/18 16:26:39 kaib 1.5.36.2: #i67514# Fixed mismatched string comparison length 2006/07/18 15:53:15 kaib 1.5.36.1: #i67514# Added command line parameter -rsc2= to rsc

--- rsc/source/prj/start.cxx
+++ rsc/source/prj/start.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: start.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:00:48 $
+ *  last change: $Author: vg $ $Date: 2006-09-27 09:52:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -228,6 +228,7 @@ static BOOL CallRsc2( ByteString aRsc2Name,
             if( !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ),  "-fp=", 4 )
               || !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ), "-fo=", 4 )
               || !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ), "-pp=", 4 )
+              || !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ), "-rsc2=", 6 )
               || !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ), "-presponse", 9 )
               || !rsc_strnicmp( (char *)pCmdLine->GetEntry( i ), "-rc", 3 )
               || !rsc_stricmp( (char *)pCmdLine->GetEntry( i ), "-+" )
@@ -350,6 +351,10 @@ int cdecl main ( int argc, char ** argv)
             else if( !rsc_strnicmp( (*ppStr) + 1, "pp=", 3 ) )
             { // anderer Name fuer den Preprozessor
                 aPrePro = (*ppStr) + 4;
+            }
+            else if( !rsc_strnicmp( (*ppStr) + 1, "rsc2=", 5 ) )
+            { // Accept alternate name for the rsc2 compiler
+                aRsc2Name = (*ppStr) + 6;
             }
             else if( !rsc_strnicmp( (*ppStr) + 1, "fo=", 3 ) )
             { // anderer Name fuer .res-file
