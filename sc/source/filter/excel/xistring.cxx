5a6c35312fc78e7d04335b12bdb9803155593457
Vladimir Glazounov <vg@openoffice.org>
2006-09-27 08:46:25 +0000

    INTEGRATION: CWS jam01 (1.2.6); FILE MERGED 2006/07/25 17:16:04 kaib 1.2.6.1: #i67378# Tweaked Jamfiles and fixed some PCH issues

--- sc/source/filter/excel/xistring.cxx
+++ sc/source/filter/excel/xistring.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xistring.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:42:59 $
+ *  last change: $Author: vg $ $Date: 2006-09-27 09:46:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+#include "precompiled_sc.hxx"
 
 #ifndef SC_XISTRING_HXX
 #include "xistring.hxx"
