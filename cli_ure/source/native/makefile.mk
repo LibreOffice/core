29e4b73ea8f26282b576e2f9cc710c575ccd1b23
Kurt Zenker <kz@openoffice.org>
2006-10-05 10:06:05 +0000

    INTEGRATION: CWS ause068 (1.18.10); FILE MERGED 2006/09/21 17:28:12 hjs 1.18.10.1: #i69536# + no longer required before echo command

--- cli_ure/source/native/makefile.mk
+++ cli_ure/source/native/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.19 $
+#   $Revision: 1.20 $
 #
-#   last change: $Author: vg $ $Date: 2006-09-25 13:05:01 $
+#   last change: $Author: kz $ $Date: 2006-10-05 11:06:05 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -133,7 +133,7 @@ CFLAGSCXX += -clr:oldSyntax
 .ENDIF
 
 $(ASSEMBLY_ATTRIBUTES) : assembly.cxx $(BIN)$/cliuno.snk $(BIN)$/cliureversion.mk
-    @+echo $(ASSEMBLY_KEY_X)
+    @echo $(ASSEMBLY_KEY_X)
     $(GNUCOPY) -p assembly.cxx $@
     +echo $(ECHOQUOTE) \
     [assembly:System::Reflection::AssemblyVersion( "$(CLI_CPPUHELPER_NEW_VERSION)" )]; $(ECHOQUOTE) \
