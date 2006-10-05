e1085ad58a26b4a58cf939e517b42aee7f08d201
Kurt Zenker <kz@openoffice.org>
2006-10-05 15:22:10 +0000

    INTEGRATION: CWS vq37 (1.20.130); FILE MERGED 2006/10/04 19:29:25 vq 1.20.130.1: #i69989# Add missing '-' in tg_config.mk and remove unneeded '+'.

--- solenv/inc/tg_srs.mk
+++ solenv/inc/tg_srs.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: tg_srs.mk,v $
 #
-#   $Revision: 1.20 $
+#   $Revision: 1.21 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-05 21:59:36 $
+#   last change: $Author: kz $ $Date: 2006-10-05 16:22:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ $(MISC)$/$(PWD:f).$(SRS$(TNR)NAME).dprr: $(SRC$(TNR)FILES) $(HIDSRS$(TNR)PARTICL
     +$(RSC) $(SRSDEFAULT) $(RSC_SRS_CHARSET) $(RSCFLAGS) -I$(RSCEXTINC) -I$(INCLOCPRJ)  -I$(INCLOCAL) -I$(INC) -I$(INCCOM) $(RSCDEFS) $(RSCUPDVERDEF) -fp={$(SRS)$/$(SRS$(TNR)NAME).srs} -fo=$@ $(SRC$(TNR)FILES)
 
 $(foreach,i,$(SRC$(TNR)FILES) $(COMMONMISC)$/$(TARGET)$/$i) : $$(@:f) localize.sdf 
-    +-$(MKDIR) $(@:d)
+    -$(MKDIR) $(@:d)
     +-$(RM) $@
     $(WRAPCMD) $(TRANSEX) -p $(PRJNAME) -i $(@:f) -o $(@).$(INPATH) -m localize.sdf -l all
     +$(RENAME) $@.$(INPATH) $@
