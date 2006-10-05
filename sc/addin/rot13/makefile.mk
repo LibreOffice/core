15eb0be82f0c67b5695cbc54e597c334ddb75802
Kurt Zenker <kz@openoffice.org>
2006-10-05 10:02:28 +0000

    INTEGRATION: CWS ause068 (1.8.156); FILE MERGED 2006/09/25 15:26:29 hjs 1.8.156.1: #i69536# + no longer required before echo command

--- sc/addin/rot13/makefile.mk
+++ sc/addin/rot13/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: vg $ $Date: 2006-04-07 16:21:09 $
+#   last change: $Author: kz $ $Date: 2006-10-05 11:02:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -102,8 +102,8 @@ $(MISC)$/rot.lst : \
     ..$/inc$/rot13.hrc \
     ..$/inc$/addin.h
 .IF "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
-    @+echo $< > $@
+    @echo $< > $@
 .ELSE
-    @+echo $(<:+"\n":s/ //) > $@
+    @echo $(<:+"\n":s/ //) > $@
 .ENDIF
 
