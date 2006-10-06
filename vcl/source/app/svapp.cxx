3472654c08100449f710cc376b69158c4534eb0c
Kurt Zenker <kz@openoffice.org>
2006-10-06 08:58:12 +0000

    INTEGRATION: CWS impresshydra (1.66.48); FILE MERGED 2006/09/20 13:10:12 cl 1.66.48.4: RESYNC: (1.69-1.70); FILE MERGED 2006/09/18 09:08:22 pl 1.66.48.3: #i12719# add property default screen number, change naming 2006/08/28 12:58:40 cl 1.66.48.2: RESYNC: (1.66-1.69); FILE MERGED 2006/08/07 14:44:21 pl 1.66.48.1: #i67721# preparations for multi monitor support

--- vcl/source/app/svapp.cxx
+++ vcl/source/app/svapp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svapp.cxx,v $
  *
- *  $Revision: 1.70 $
+ *  $Revision: 1.71 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:49:38 $
+ *  last change: $Author: kz $ $Date: 2006-10-06 09:58:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1354,10 +1354,28 @@ unsigned int Application::GetScreenCount()
     return pSys ? pSys->GetDisplayScreenCount() : 0;
 }
 
+bool Application::IsMultiDisplay()
+{
+    SalSystem* pSys = ImplGetSalSystem();
+    return pSys ? pSys->IsMultiDisplay() : false;
+}
+
+unsigned int Application::GetDefaultDisplayNumber()
+{
+    SalSystem* pSys = ImplGetSalSystem();
+    return pSys ? pSys->GetDefaultDisplayNumber() : 0;
+}
+
 Rectangle Application::GetScreenPosSizePixel( unsigned int nScreen )
 {
     SalSystem* pSys = ImplGetSalSystem();
     return pSys ? pSys->GetDisplayScreenPosSizePixel( nScreen ) : Rectangle();
+}
+
+Rectangle Application::GetWorkAreaPosSizePixel( unsigned int nScreen )
+{
+    SalSystem* pSys = ImplGetSalSystem();
+    return pSys ? pSys->GetDisplayWorkAreaPosSizePixel( nScreen ) : Rectangle();
 }
 
 // -----------------------------------------------------------------------
