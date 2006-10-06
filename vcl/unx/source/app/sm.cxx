ac8c5e538dab00512fb10a951a3fb93b993c1ff0
Kurt Zenker <kz@openoffice.org>
2006-10-06 09:04:48 +0000

    INTEGRATION: CWS impresshydra (1.26.46); FILE MERGED 2006/09/20 13:49:38 cl 1.26.46.4: RESYNC: (1.27-1.28); FILE MERGED 2006/08/29 14:49:28 cl 1.26.46.3: fixed merge conflicts 2006/08/28 13:11:24 cl 1.26.46.2: RESYNC: (1.26-1.27); FILE MERGED 2006/08/09 14:35:07 pl 1.26.46.1: #i67721# on the way to multiscreen support

--- vcl/unx/source/app/sm.cxx
+++ vcl/unx/source/app/sm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sm.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:36:12 $
+ *  last change: $Author: kz $ $Date: 2006-10-06 10:04:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -460,10 +460,10 @@ void SessionManagerClient::open()
         ICEConnectionObserver::unlock();
 
         SalDisplay* pDisp = GetX11SalData()->GetDisplay();
-        if( pDisp->GetDrawable() && aClientID.Len() )
+        if( pDisp->GetDrawable( pDisp->GetDefaultScreenNumber() ) && aClientID.Len() )
         {
             XChangeProperty( pDisp->GetDisplay(),
-                             pDisp->GetDrawable(),
+                             pDisp->GetDrawable( pDisp->GetDefaultScreenNumber() ),
                              XInternAtom( pDisp->GetDisplay(), "SM_CLIENT_ID", False ),
                              XA_STRING,
                              8,
