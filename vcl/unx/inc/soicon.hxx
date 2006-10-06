8b871cb9a776f353278d843895033e16a121bc35
Kurt Zenker <kz@openoffice.org>
2006-10-06 09:02:18 +0000

    INTEGRATION: CWS impresshydra (1.4.322); FILE MERGED 2006/08/09 14:35:05 pl 1.4.322.1: #i67721# on the way to multiscreen support

--- vcl/unx/inc/soicon.hxx
+++ vcl/unx/inc/soicon.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: soicon.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:48:39 $
+ *  last change: $Author: kz $ $Date: 2006-10-06 10:02:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,6 +39,7 @@ class SalDisplay;
 class SalBitmap;
 class Bitmap;
 
-BOOL SelectAppIconPixmap( SalDisplay *pDisplay, USHORT nIcon, USHORT iconSize,
+BOOL SelectAppIconPixmap( SalDisplay *pDisplay, int nScreen,
+                          USHORT nIcon, USHORT iconSize,
                           Pixmap& icon_pixmap, Pixmap& icon_mask );
 #endif
