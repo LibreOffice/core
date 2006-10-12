467a7ada04b502767f625de3630659d1e30c8be8
Oliver Bolte <obo@openoffice.org>
2006-10-12 09:51:16 +0000

    INTEGRATION: CWS sb59 (1.3.100); FILE MERGED 2006/08/10 12:04:50 sb 1.3.100.1: #i67487# Made code warning-free (wntmsci10).

--- fpicker/source/win32/filepicker/customcontrol.hxx
+++ fpicker/source/win32/filepicker/customcontrol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: customcontrol.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:42:40 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 10:51:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,13 @@
 #include <sal/types.h>
 #endif
 
+#if defined _MSC_VER
+#pragma warning(push, 1)
+#endif
 #include <windows.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 //-----------------------------------
 //
