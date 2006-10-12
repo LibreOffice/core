7cf6e17cbd3b2de260aaa46928f757b73da57aa0
Oliver Bolte <obo@openoffice.org>
2006-10-12 10:31:36 +0000

    INTEGRATION: CWS sb59 (1.6.4); FILE MERGED 2006/08/18 15:00:05 sb 1.6.4.2: RESYNC: (1.6-1.7); FILE MERGED 2006/08/11 22:05:05 thb 1.6.4.1: #i68336# Made canvas warning free for wntmsci10

--- canvas/source/tools/image.cxx
+++ canvas/source/tools/image.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: image.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 03:25:49 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 11:31:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -69,6 +69,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 #if defined(WNT)
+# if defined _MSC_VER
+# pragma warning(push,1)
+# endif
+
     namespace  win32
     {
         #undef DECLARE_HANDLE
@@ -79,6 +83,10 @@
         #define NOMINMAX
         #include <windows.h>
     }
+
+# if defined _MSC_VER
+# pragma warning(pop)
+# endif
 #else
     namespace unx
     {
