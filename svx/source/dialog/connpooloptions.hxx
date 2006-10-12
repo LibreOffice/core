c55d4a0186b090843aab035e3e3577f581fcfdf3
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:08:02 +0000

    INTEGRATION: CWS sb59 (1.4.488); FILE MERGED 2006/08/03 13:51:27 cl 1.4.488.1: removed compiler warnings

--- svx/source/dialog/connpooloptions.hxx
+++ svx/source/dialog/connpooloptions.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: connpooloptions.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:46:02 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:08:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,6 +65,8 @@ namespace offapp
     class DriverListControl;
     class ConnectionPoolOptionsPage : public SfxTabPage
     {
+        using TabPage::ActivatePage;
+
     protected:
         FixedLine               m_aFrame;
         CheckBox                m_aEnablePooling;
