d50bb43a6d14564748541fd898c98980143f9458
Oliver Bolte <obo@openoffice.org>
2006-10-12 12:01:22 +0000

    INTEGRATION: CWS sb59 (1.12.62); FILE MERGED 2006/08/03 13:51:53 cl 1.12.62.1: removed compiler warnings

--- svx/source/outliner/outleeng.cxx
+++ svx/source/outliner/outleeng.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outleeng.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:30:25 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 13:01:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ OutlinerEditEng::~OutlinerEditEng()
 {
 }
 
-void OutlinerEditEng::PaintingFirstLine( USHORT nPara, const Point& rStartPos, long nBaseLineY, const Point& rOrigin, short nOrientation, OutputDevice* pOutDev )
+void OutlinerEditEng::PaintingFirstLine( USHORT nPara, const Point& rStartPos, long /*nBaseLineY*/, const Point& rOrigin, short nOrientation, OutputDevice* pOutDev )
 {
     pOwner->PaintBullet( nPara, rStartPos, rOrigin, nOrientation, pOutDev );
 }
