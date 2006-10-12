189d57c8399efba8851f44ea79a7833370f8497d
Oliver Bolte <obo@openoffice.org>
2006-10-12 12:29:45 +0000

    INTEGRATION: CWS sb59 (1.6.62); FILE MERGED 2006/08/28 12:58:11 sb 1.6.62.1: #i67487# Made code warning-free (wntmsci10).

--- svx/source/xml/xmltxtimp.cxx
+++ svx/source/xml/xmltxtimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltxtimp.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 06:20:53 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 13:29:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -298,7 +298,7 @@ void SvxReadXML( EditEngine& rEditEngine, SvStream& rStream, const ESelection& r
         }
         while(0);
     }
-    catch( uno::Exception& e )
+    catch( uno::Exception& )
     {
     }
 }
