ff350bf344ba56f3e5a481f4a2157928844b3449
Oliver Bolte <obo@openoffice.org>
2006-10-12 09:56:24 +0000

    INTEGRATION: CWS sb59 (1.6.30); FILE MERGED 2006/08/10 12:04:54 sb 1.6.30.1: #i67487# Made code warning-free (wntmsci10).

--- fpicker/source/win32/misc/WinImplHelper.hxx
+++ fpicker/source/win32/misc/WinImplHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WinImplHelper.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:14:57 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 10:56:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,13 @@
 #include <rtl/ustring.hxx>
 #endif
 
+#if defined _MSC_VER
+#pragma warning(push, 1)
+#endif
 #include <windows.h>
+#if defined _MSC_VER
+#pragma warning(pop)
+#endif
 
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
