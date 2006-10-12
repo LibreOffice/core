6d78a4f6ff02d165d36729ce246b0e16e7ac7820
Oliver Bolte <obo@openoffice.org>
2006-10-12 14:57:28 +0000

    INTEGRATION: CWS sb59 (1.6.282); FILE MERGED 2006/08/18 19:23:06 sb 1.6.282.2: RESYNC: (1.6-1.7); FILE MERGED 2006/08/16 09:31:24 sb 1.6.282.1: #i67487# Made code warning-free (wntmsci10).

--- sfx2/source/inc/versdlg.hxx
+++ sfx2/source/inc/versdlg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: versdlg.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 11:11:47 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 15:57:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,6 +53,7 @@
 #include "basedlgs.hxx"
 
 class SfxObjectShell;
+class SfxViewFrame;
 struct SfxVersionInfo;
 
 class SfxVersionsTabListBox_Impl  : public SvTabListBox
