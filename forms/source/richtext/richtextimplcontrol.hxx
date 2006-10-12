51ad50bb8af578b286245c25a290c67dfbe4f600
Oliver Bolte <obo@openoffice.org>
2006-10-12 10:14:38 +0000

    INTEGRATION: CWS sb59 (1.5.28); FILE MERGED 2006/08/29 16:11:01 sb 1.5.28.1: #i67487# Made code warning-free (wntmsci10).

--- forms/source/richtext/richtextimplcontrol.hxx
+++ forms/source/richtext/richtextimplcontrol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: richtextimplcontrol.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:00:44 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 11:14:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -102,7 +102,7 @@ namespace frm
     public:
         RichTextControlImpl( Control* _pAntiImpl, RichTextEngine* _pEngine,
             ITextAttributeListener* _pTextAttrListener, ITextSelectionListener* _pSelectionListener );
-        ~RichTextControlImpl();
+        virtual ~RichTextControlImpl();
 
         /** updates the cache with the state of all attribute values from the given set, notifies
             the listener if the state changed
