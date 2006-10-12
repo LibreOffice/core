91772ca3b0169dbb8d8da2f98471e0c942927bfa
Oliver Bolte <obo@openoffice.org>
2006-10-12 10:48:06 +0000

    INTEGRATION: CWS sb59 (1.2.14); FILE MERGED 2006/08/03 13:47:05 cl 1.2.14.1: removed compiler warnings

--- svx/inc/radiobtnbox.hxx
+++ svx/inc/radiobtnbox.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: radiobtnbox.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 11:58:38 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 11:48:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,6 +50,8 @@ namespace svx {
 
 class SVX_DLLPUBLIC SvxRadioButtonListBox : public SvxSimpleTable
 {
+    using SvxSimpleTable::SetTabs;
+
 private:
     Point               m_aCurMousePoint;
 
