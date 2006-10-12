6d075a882ce0f4b0c85098f8b0e09045a529d84d
Oliver Bolte <obo@openoffice.org>
2006-10-12 14:28:51 +0000

    INTEGRATION: CWS sb59 (1.3.300); FILE MERGED 2006/08/02 15:38:13 cl 1.3.300.1: fixed compiler warnings

--- svtools/uno/pathservice.cxx
+++ svtools/uno/pathservice.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pathservice.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:32:03 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 15:28:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -117,7 +117,7 @@ css::uno::Reference< css::uno::XInterface > PathService_CreateInstance (
     const css::uno::Reference< css::lang::XMultiServiceFactory > &)
 {
     return css::uno::Reference< css::uno::XInterface >(
-        reinterpret_cast< cppu::OWeakObject* >(new PathService()));
+        static_cast< cppu::OWeakObject* >(new PathService()));
 }
 
 // -----------------------------------------------------------------------
