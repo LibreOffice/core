2c7a1db3890011d9b91095155beba3e98bc1f3b7
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:51:28 +0000

    INTEGRATION: CWS sb59 (1.4.488); FILE MERGED 2006/08/28 12:57:55 sb 1.4.488.1: #i67487# Made code warning-free (wntmsci10).

--- svx/source/inc/fmtextcontrolshell.hxx
+++ svx/source/inc/fmtextcontrolshell.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmtextcontrolshell.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:20:44 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:51:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -160,7 +160,7 @@ namespace svx
 
     public:
         FmTextControlShell( SfxViewFrame* _pFrame );
-        ~FmTextControlShell();
+        virtual ~FmTextControlShell();
 
         // clean up any resources associated with this instance
         void    dispose();
