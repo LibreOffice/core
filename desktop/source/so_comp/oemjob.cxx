e02b00628734cbc1a41fefda22b2fd2536cae7a6
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:17:28 +0000

    INTEGRATION: CWS sb59 (1.4.202); FILE MERGED 2006/07/20 07:55:36 sb 1.4.202.1: #i67537# Made code warning-free.

--- desktop/source/so_comp/oemjob.cxx
+++ desktop/source/so_comp/oemjob.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oemjob.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 09:49:01 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:17:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -173,7 +173,7 @@ throw ( RuntimeException )
 }
 
 // XJob
-Any SAL_CALL OEMPreloadJob::execute(const Sequence<NamedValue>& args)
+Any SAL_CALL OEMPreloadJob::execute(const Sequence<NamedValue>&)
 throw ( RuntimeException )
 {
     sal_Bool bCont = sal_False;
