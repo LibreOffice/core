b6d8121761352b4100b1d890899ff8fceb021ddd
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:12:40 +0000

    INTEGRATION: CWS sb59 (1.3.202); FILE MERGED 2006/07/20 07:55:31 sb 1.3.202.1: #i67537# Made code warning-free.

--- desktop/source/inc/exithelper.hxx
+++ desktop/source/inc/exithelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: exithelper.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:36:19 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:12:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -75,4 +75,4 @@ class ExitHelper
 
 } // namespace desktop
 
-#endif // #ifndef _DESKTOP_EXITHELPER_HXX_ 
+#endif // #ifndef _DESKTOP_EXITHELPER_HXX_
