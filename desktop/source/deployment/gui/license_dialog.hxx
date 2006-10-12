c0cde60c070ffbfa127336d33d1bb1d095db6b4e
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:08:28 +0000

    INTEGRATION: CWS sb59 (1.2.2); FILE MERGED 2006/07/20 07:55:25 sb 1.2.2.1: #i67537# Made code warning-free.

--- desktop/source/deployment/gui/license_dialog.hxx
+++ desktop/source/deployment/gui/license_dialog.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: license_dialog.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 17:04:37 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:08:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
