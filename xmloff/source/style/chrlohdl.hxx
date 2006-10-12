aac6dd3320e39a0eac988380b81e74ace61c0620
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:48:48 +0000

    INTEGRATION: CWS sb59 (1.2.152); FILE MERGED 2006/08/09 12:53:56 sb 1.2.152.1: #i67487# Made code warning-free (wntmsci10).

--- xmloff/source/style/chrlohdl.hxx
+++ xmloff/source/style/chrlohdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chrlohdl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:39:45 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:48:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,7 @@ class XMLCharLanguageHdl : public XMLPropertyHandler
 public:
     virtual ~XMLCharLanguageHdl();
 
-    virtual sal_Bool equals( const ::com::sun::star::uno::Any& r1, const ::com::sun::star::uno::Any& r2 ) const;
+    virtual bool equals( const ::com::sun::star::uno::Any& r1, const ::com::sun::star::uno::Any& r2 ) const;
 
     virtual sal_Bool importXML( const ::rtl::OUString& rStrImpValue, ::com::sun::star::uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const;
     virtual sal_Bool exportXML( ::rtl::OUString& rStrExpValue, const ::com::sun::star::uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const;
@@ -59,7 +59,7 @@ class XMLCharCountryHdl : public XMLPropertyHandler
 public:
     virtual ~XMLCharCountryHdl();
 
-    virtual sal_Bool equals( const ::com::sun::star::uno::Any& r1, const ::com::sun::star::uno::Any& r2 ) const;
+    virtual bool equals( const ::com::sun::star::uno::Any& r1, const ::com::sun::star::uno::Any& r2 ) const;
 
     virtual sal_Bool importXML( const ::rtl::OUString& rStrImpValue, ::com::sun::star::uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const;
     virtual sal_Bool exportXML( ::rtl::OUString& rStrExpValue, const ::com::sun::star::uno::Any& rValue, const SvXMLUnitConverter& rUnitConverter ) const;
