12530f1614f952d0c6fb18278d8c97706ae6e8bf
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:59:02 +0000

    INTEGRATION: CWS sb59 (1.8.22); FILE MERGED 2006/08/11 15:36:02 thb 1.8.22.1: #i68336# Removed unused params; added a few using declarations for hidden methods; added a few casts; added some default statements to get cppcanvas warning free

--- cppcanvas/source/mtfrenderer/bitmapaction.cxx
+++ cppcanvas/source/mtfrenderer/bitmapaction.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bitmapaction.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:48:44 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:59:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -92,6 +92,7 @@ namespace cppcanvas
                 virtual sal_Int32 getActionCount() const;
 
             private:
+                using Action::render;
                 virtual bool render( uno::Reference< rendering::XCachedPrimitive >& rCachedPrimitive,
                                      const ::basegfx::B2DHomMatrix&                 rTransformation ) const;
 
