e9358bfacb63a5c3d8af6c8a14b2bd286132e3cd
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:48:38 +0000

    INTEGRATION: CWS sb59 (1.8.260); FILE MERGED 2006/08/03 13:51:46 cl 1.8.260.1: removed compiler warnings

--- svx/source/gallery2/galbrws1.hxx
+++ svx/source/gallery2/galbrws1.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: galbrws1.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-01 12:58:15 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:48:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,8 @@ class GalleryBrowser1 : public Control, SfxListener
 {
     friend class GalleryBrowser;
     friend class GalleryThemeListBox;
+    using Control::Notify;
+    using Window::KeyInput;
 
 private:
 
