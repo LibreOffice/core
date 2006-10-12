c5bc0e384a4cdb27435747530e4debb194c811c4
Oliver Bolte <obo@openoffice.org>
2006-10-12 10:59:23 +0000

    INTEGRATION: CWS sb59 (1.5.486); FILE MERGED 2006/08/28 12:56:47 sb 1.5.486.1: #i67487# Made code warning-free (wntmsci10).

--- svx/inc/xexch.hxx
+++ svx/inc/xexch.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xexch.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:36:48 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 11:59:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@ public:
 
                         XFillExchangeData();
                         XFillExchangeData(const XFillAttrSetItem rXFillAttrSetItem);
-                        ~XFillExchangeData();
+                        virtual ~XFillExchangeData();
 
                         // Zuweisungsoperator
                         XFillExchangeData& operator=( const XFillExchangeData& rXFillExchangeData );
