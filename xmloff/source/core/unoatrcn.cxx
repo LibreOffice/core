90bd7cd13afabced449ab0209873d746a933f8ba
Oliver Bolte <obo@openoffice.org>
2006-10-12 13:39:04 +0000

    INTEGRATION: CWS sb59 (1.5.24); FILE MERGED 2006/08/09 12:53:49 sb 1.5.24.1: #i67487# Made code warning-free (wntmsci10).

--- xmloff/source/core/unoatrcn.cxx
+++ xmloff/source/core/unoatrcn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoatrcn.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:21:27 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 14:39:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -96,7 +96,7 @@ uno::Type SAL_CALL SvUnoAttributeContainer::getElementType(void)
 sal_Bool SAL_CALL SvUnoAttributeContainer::hasElements(void)
     throw( uno::RuntimeException )
 {
-    return mpContainer->GetAttrCount();
+    return mpContainer->GetAttrCount() != 0;
 }
 
 sal_uInt16 SvUnoAttributeContainer::getIndexByName(const OUString& aName ) const
