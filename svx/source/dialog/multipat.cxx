a8dd10005c1ec37d335ac1edab75965692e80478
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:19:20 +0000

    INTEGRATION: CWS sb59 (1.7.2); FILE MERGED 2006/08/03 13:51:32 cl 1.7.2.1: removed compiler warnings

--- svx/source/dialog/multipat.cxx
+++ svx/source/dialog/multipat.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: multipat.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 04:28:08 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:19:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -138,7 +138,7 @@ IMPL_LINK( SvxMultiPathDialog, AddHdl_Impl, PushButton *, EMPTYARG )
         if ( pImpl->bIsRadioButtonMode )
         {
             ULONG nPos = aRadioLB.GetEntryPos( sInsPath, 1 );
-            if ( -1 == nPos )
+            if ( (ULONG)-1 == nPos )
             {
                 String sNewEntry( '\t' );
                 sNewEntry += sInsPath;
