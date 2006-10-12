3cd3d27ebf1ad71fd4b632a8b06d7fb2234099a8
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:22:35 +0000

    INTEGRATION: CWS sb59 (1.6.14); FILE MERGED 2006/08/03 13:51:34 cl 1.6.14.1: removed compiler warnings

--- svx/source/dialog/optjava.hxx
+++ svx/source/dialog/optjava.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optjava.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-13 12:00:51 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:22:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -71,6 +71,7 @@ class   SvxJavaClassPathDlg;
 
 class SvxJavaTable : public SvxSimpleTable
 {
+    using SvxSimpleTable::SetTabs;
 private:
     Point               m_aCurMousePoint;
 
