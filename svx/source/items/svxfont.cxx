58139179008f20b5464bdf220cdd825465bdf1f9
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:55:54 +0000

    INTEGRATION: CWS sb59 (1.10.62); FILE MERGED 2006/08/03 13:51:50 cl 1.10.62.1: removed compiler warnings

--- svx/source/items/svxfont.cxx
+++ svx/source/items/svxfont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svxfont.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:23:38 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:55:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -185,7 +185,7 @@ XubString SvxFont::CalcCaseMap( const XubString &rTxt ) const
             // Bug: wenn das Attribut mitten im Wort beginnt.
             BOOL bBlank = TRUE;
 
-            for( UINT32 i = 0; i < aTxt.Len(); ++i )
+            for( USHORT i = 0; i < aTxt.Len(); ++i )
             {
                 if( sal_Unicode(' ') == aTxt.GetChar(i) || sal_Unicode('\t') == aTxt.GetChar(i) )
                     bBlank = TRUE;
@@ -827,7 +827,7 @@ void SvxDoDrawCapital::SetSpace()
 void SvxDoDrawCapital::Do( const XubString &_rTxt, const xub_StrLen _nIdx,
                            const xub_StrLen _nLen, const BOOL bUpper)
 {
-    BYTE nProp;
+    BYTE nProp = 0;
     Size aPartSize;
 
     // Einstellen der gewuenschten Fonts
