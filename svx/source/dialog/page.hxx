efeffc700d5b6b7c896163b4b3e1f2c2a746804e
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:23:55 +0000

    INTEGRATION: CWS sb59 (1.4.14); FILE MERGED 2006/08/18 17:37:38 sb 1.4.14.2: RESYNC: (1.4-1.5); FILE MERGED 2006/08/03 13:51:34 cl 1.4.14.1: removed compiler warnings

--- svx/source/dialog/page.hxx
+++ svx/source/dialog/page.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: page.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 08:28:40 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:23:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -119,6 +119,9 @@ typedef USHORT MarginPosition;
 
 class SvxPageDescPage : public SfxTabPage
 {
+    using TabPage::ActivatePage;
+    using TabPage::DeactivatePage;
+
     // paper format
     FixedLine           aPaperSizeFl;
     FixedText           aPaperFormatText;
