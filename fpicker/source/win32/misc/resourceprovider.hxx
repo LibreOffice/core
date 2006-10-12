82651cd274b890cc15c766a74aeb58c263641a37
Oliver Bolte <obo@openoffice.org>
2006-10-12 09:56:52 +0000

    INTEGRATION: CWS sb59 (1.2.100); FILE MERGED 2006/08/10 12:04:55 sb 1.2.100.1: #i67487# Made code warning-free (wntmsci10).

--- fpicker/source/win32/misc/resourceprovider.hxx
+++ fpicker/source/win32/misc/resourceprovider.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: resourceprovider.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:53:20 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 10:56:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ public:
     CResourceProvider( );
     ~CResourceProvider( );
 
-    rtl::OUString getResString( sal_Int32 aId );
+    rtl::OUString getResString( sal_Int16 aId );
 
 private:
     CResourceProvider_Impl* m_pImpl;
