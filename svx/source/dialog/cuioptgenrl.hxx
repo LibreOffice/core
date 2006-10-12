b754559bf2d2b6a9e60d5fe95605463fbe15e28c
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:10:03 +0000

    INTEGRATION: CWS sb59 (1.3.488); FILE MERGED 2006/08/03 13:51:28 cl 1.3.488.1: removed compiler warnings

--- svx/source/dialog/cuioptgenrl.hxx
+++ svx/source/dialog/cuioptgenrl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cuioptgenrl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:52:56 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:10:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,6 +63,7 @@ struct GeneralTabPage_Impl;
 
 class SvxGeneralTabPage : public SfxTabPage
 {
+    using TabPage::DeactivatePage;
 private:
     FixedText           aCompanyLbl;
     Edit                aCompanyEdit;
