72cd953808d143978ac232bd7e2aca25bd64e89b
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:44:00 +0000

    INTEGRATION: CWS sb59 (1.6.14); FILE MERGED 2006/08/03 13:51:44 cl 1.6.14.1: removed compiler warnings

--- svx/source/form/ParseContext.cxx
+++ svx/source/form/ParseContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ParseContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:02:42 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:44:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ using namespace ::connectivity;
 //==========================================================================
 //= OSystemParseContext
 //==========================================================================
-DBG_NAME(OSystemParseContext);
+DBG_NAME(OSystemParseContext)
 //-----------------------------------------------------------------------------
 OSystemParseContext::OSystemParseContext() : IParseContext()
 {
