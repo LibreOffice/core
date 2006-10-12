2f806c90c7b8d3592c713da5544baf2f0e65884e
Oliver Bolte <obo@openoffice.org>
2006-10-12 11:05:22 +0000

    INTEGRATION: CWS sb59 (1.4.488); FILE MERGED 2006/08/03 13:51:26 cl 1.4.488.1: removed compiler warnings

--- svx/source/dialog/align.hxx
+++ svx/source/dialog/align.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: align.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:32:20 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:04:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,6 +77,8 @@ namespace svx {
 
 class AlignmentTabPage : public SfxTabPage
 {
+    using TabPage::DeactivatePage;
+
 public:
     virtual             ~AlignmentTabPage();
 
--- svx/source/dialog/backgrnd.hxx
+++ svx/source/dialog/backgrnd.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: backgrnd.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:34:58 $
+ *  last change: $Author: obo $ $Date: 2006-10-12 12:05:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -80,6 +80,7 @@ class SvxBrushItem;
 
 class SvxBackgroundTabPage : public SvxTabPage
 {
+    using TabPage::DeactivatePage;
 public:
     static SfxTabPage*  Create( Window* pParent, const SfxItemSet& rAttrSet );
     static USHORT*      GetRanges();
