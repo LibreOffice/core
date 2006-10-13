f93cf7ecbe8d8317a42c9fa040db5229516023af
Oliver Bolte <obo@openoffice.org>
2006-10-13 06:58:10 +0000

    INTEGRATION: CWS sb59 (1.45.64); FILE MERGED 2006/07/25 08:52:52 sb 1.45.64.1: #i67487# Made code warning-free (wntmsci10).

--- svtools/source/contnr/svtreebx.cxx
+++ svtools/source/contnr/svtreebx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svtreebx.cxx,v $
  *
- *  $Revision: 1.47 $
+ *  $Revision: 1.48 $
  *
- *  last change: $Author: kz $ $Date: 2006-10-06 10:35:33 $
+ *  last change: $Author: obo $ $Date: 2006-10-13 07:58:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
