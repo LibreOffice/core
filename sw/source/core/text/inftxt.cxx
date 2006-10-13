817d2dea33a557e49bcbeccd50a6127221fa1e58
Oliver Bolte <obo@openoffice.org>
2006-10-13 07:18:40 +0000

    INTEGRATION: CWS swqbf86 (1.105.34); FILE MERGED 2006/10/06 11:51:32 fme 1.105.34.2: RESYNC: (1.105-1.106); FILE MERGED 2006/09/18 13:04:29 fme 1.105.34.1: #i68435# bArrowDone might be used uninitialized. I'll move this flag from SwTxtSizeInfo to SwTxtFormatInfo.

--- sw/source/core/text/inftxt.cxx
+++ sw/source/core/text/inftxt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inftxt.cxx,v $
  *
- *  $Revision: 1.106 $
+ *  $Revision: 1.107 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:34:55 $
+ *  last change: $Author: obo $ $Date: 2006-10-13 08:18:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1491,6 +1491,7 @@ SwTxtFormatInfo::SwTxtFormatInfo( const SwTxtFormatInfo& rInf,
     bFtnDone = TRUE;
     bErgoDone = TRUE;
     bNumDone = TRUE;
+    bArrowDone = TRUE;
     bStop = FALSE;
     bNewLine = TRUE;
     bShift  = FALSE;
