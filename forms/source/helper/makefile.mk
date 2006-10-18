d0a9a4fc325023718cb328a1b2a37a17b9898cbf
Ivo Hinkelmann <ihi@openoffice.org>
2006-10-18 12:17:45 +0000

    INTEGRATION: CWS dba205b (1.4.104); FILE MERGED 2006/08/03 14:40:32 fs 1.4.104.1: #i64593# +windowstateguard

--- forms/source/helper/makefile.mk
+++ forms/source/helper/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 22:53:59 $
+#   last change: $Author: ihi $ $Date: 2006-10-18 13:17:45 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,8 @@ INCPRE+=$(SOLARINCDIR)$/offuh
 
 SLOFILES=	$(SLO)$/formnavigation.obj \
             $(SLO)$/controlfeatureinterception.obj \
-            $(SLO)$/urltransformer.obj
+            $(SLO)$/urltransformer.obj \
+            $(SLO)$/windowstateguard.obj
 
 # --- Targets ----------------------------------
 
