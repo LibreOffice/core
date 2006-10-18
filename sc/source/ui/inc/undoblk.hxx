0c3179801b43f6e37795138785fa8570ca923726
Ivo Hinkelmann <ihi@openoffice.org>
2006-10-18 11:27:54 +0000

    INTEGRATION: CWS scr1c1 (1.13.244); FILE MERGED 2006/09/07 14:20:56 jodygoldberg 1.13.244.1: Issue number:  20857 Submitted by:  jodygoldberg
    Implements the the core changes to support parsing and generating cell/range
    references in different formats (XL R1C1/A1) along with some tools for using
    the new types.  This adds two new functions
        XL_INDIRECT
        XL_ADDRESS
    but does _not_ connect them in the xls importer.  Nor does the patch make any
    UI changes.  Those will need to be discussed.  The OOo parser/generator should
    not be impacted by the changes.

--- sc/source/ui/inc/undoblk.hxx
+++ sc/source/ui/inc/undoblk.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoblk.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-21 12:05:00 $
+ *  last change: $Author: ihi $ $Date: 2006-10-18 12:27:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -718,6 +718,7 @@ public:
 private:
     ScDocument*     pUndoDoc;
     String          aFormula;
+    ScAddress::Convention eConv;
     ULONG           nStartChangeAction;
     ULONG           nEndChangeAction;
 
