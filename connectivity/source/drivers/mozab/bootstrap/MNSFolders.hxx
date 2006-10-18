5ae2764e28872344f347f6ba1b226864b70cf768
Ivo Hinkelmann <ihi@openoffice.org>
2006-10-18 12:10:22 +0000

    INTEGRATION: CWS dba205b (1.3.146); FILE MERGED 2006/08/04 11:28:23 fs 1.3.146.1: #i62667# ::rtl::OUString instead of ::rtl::OString

--- connectivity/source/drivers/mozab/bootstrap/MNSFolders.hxx
+++ connectivity/source/drivers/mozab/bootstrap/MNSFolders.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MNSFolders.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:23:18 $
+ *  last change: $Author: ihi $ $Date: 2006-10-18 13:10:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,9 +47,8 @@
 
 #include <rtl/ustring.hxx>
 
-::rtl::OString getAppDir();
-::rtl::OString getRegistryDir(::com::sun::star::mozilla::MozillaProductType product);
-::rtl::OString getRegistryFileName(::com::sun::star::mozilla::MozillaProductType product);
+::rtl::OUString getRegistryDir(::com::sun::star::mozilla::MozillaProductType product);
+::rtl::OUString getRegistryFileName(::com::sun::star::mozilla::MozillaProductType product);
 
 #endif
 
