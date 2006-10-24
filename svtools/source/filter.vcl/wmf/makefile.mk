9a9456ff35967b3c0ef5332a73b2225dcc5b9127
Jens-Heiner Rechtien <hr@openoffice.org>
2006-10-24 12:33:05 +0000

    INTEGRATION: CWS impress108 (1.3.358); FILE MERGED 2006/10/06 07:58:24 cl 1.3.358.1: added exceptions since stl is now used

--- svtools/source/filter.vcl/wmf/makefile.mk
+++ svtools/source/filter.vcl/wmf/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 15:45:59 $
+#   last change: $Author: hr $ $Date: 2006-10-24 13:33:05 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,6 +37,7 @@ PRJ=..$/..$/..
 
 PRJNAME=svtools
 TARGET=wmf
+ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
 
