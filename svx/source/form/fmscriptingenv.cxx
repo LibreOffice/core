eee76c0cc54cd22fbf17af5b3d4050e98f34d2e1
Rüdiger Timm <rt@openoffice.org>
2006-10-30 07:47:52 +0000

    INTEGRATION: CWS ause060 (1.2.20); FILE MERGED 2006/10/09 15:37:32 hjs 1.2.20.1: #i70234# add missing include

--- svx/source/form/fmscriptingenv.cxx
+++ svx/source/form/fmscriptingenv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmscriptingenv.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-28 15:01:24 $
+ *  last change: $Author: rt $ $Date: 2006-10-30 08:47:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
 
 #ifndef SVX_FMSCRIPTINGENV_HXX
 #include "fmscriptingenv.hxx"
