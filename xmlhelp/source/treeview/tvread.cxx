bdcc44932b17fc0658831695bcf2984511a39a63
Rüdiger Timm <rt@openoffice.org>
2006-10-30 07:35:42 +0000

    INTEGRATION: CWS ause060 (1.19.2); FILE MERGED 2006/09/26 10:47:26 hjs 1.19.2.1: #i69234# fix automated pch include; add license header

--- xmlhelp/source/treeview/tvread.cxx
+++ xmlhelp/source/treeview/tvread.cxx
@@ -1,3 +1,42 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tvread.cxx,v $
+ *
+ *  $Revision: 1.21 $
+ *
+ *  last change: $Author: rt $ $Date: 2006-10-30 08:35:42 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_xmlhelp.hxx"
+
 #include <string.h>
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
@@ -408,9 +447,6 @@ TVRead::hasByHierarchicalName( const rtl::OUString& aName )
 
 
 /**************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_xmlhelp.hxx"
 /*                                                                        */
 /*                      TVChildTarget                                     */
 /*                                                                        */
