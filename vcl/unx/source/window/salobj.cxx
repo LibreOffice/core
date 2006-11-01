29cf135c55d49c17547d77aae56c1737dc916384
Vladimir Glazounov <vg@openoffice.org>
2006-11-01 14:30:41 +0000

    INTEGRATION: CWS syswinshape (1.15.30); FILE MERGED 2006/10/20 12:55:31 pl 1.15.30.2: RESYNC: (1.15-1.17); FILE MERGED 2006/10/09 17:00:52 pl 1.15.30.1: #i69275# support for shaped system windows

--- vcl/unx/source/window/salobj.cxx
+++ vcl/unx/source/window/salobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salobj.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-10-06 10:07:37 $
+ *  last change: $Author: vg $ $Date: 2006-11-01 15:30:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -225,7 +225,7 @@ SalClipRegion::SalClipRegion()
 SalClipRegion::~SalClipRegion()
 {
     if ( ClipRectangleList )
-        delete ClipRectangleList;
+        delete [] ClipRectangleList;
 }
 
 
@@ -233,7 +233,7 @@ void
 SalClipRegion::BeginSetClipRegion( ULONG nRects )
 {
     if (ClipRectangleList)
-        delete ClipRectangleList;
+        delete [] ClipRectangleList;
 
     ClipRectangleList = new XRectangle[nRects];
     numClipRectangles = 0;
