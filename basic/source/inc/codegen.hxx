a03c2a7181bd9d289ac8e9d53cd9b2c8ac3743b2
Vladimir Glazounov <vg@openoffice.org>
2006-11-01 15:15:00 +0000

    INTEGRATION: CWS npower3 (1.5.70); FILE MERGED 2006/08/18 14:26:58 npower 1.5.70.2: #i64377# check in forgotten local edits <sigh> 2006/04/20 09:41:56 npower 1.5.70.1: #i64377# initial modifications to support large program sizes

--- basic/source/inc/codegen.hxx
+++ basic/source/inc/codegen.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: codegen.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-09-29 16:31:37 $
+ *  last change: $Author: vg $ $Date: 2006-11-01 16:15:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,20 +56,44 @@ class SbiCodeGen {              // Code-Erzeugung:
 public:
     SbiCodeGen( SbModule&, SbiParser*, short );
     SbiParser* GetParser() { return pParser; }
-    USHORT Gen( SbiOpcode );
-    USHORT Gen( SbiOpcode, UINT16 );
-    USHORT Gen( SbiOpcode, UINT16, UINT16 );
-    void Patch( USHORT o, USHORT v ){ aCode.Patch( o, v ); }
-    void BackChain( USHORT off )    { aCode.Chain( off );  }
+    UINT32 Gen( SbiOpcode );
+    UINT32 Gen( SbiOpcode, UINT32 );
+    UINT32 Gen( SbiOpcode, UINT32, UINT32 );
+    void Patch( UINT32 o, UINT32 v ){ aCode.Patch( o, v ); }
+    void BackChain( UINT32 off )    { aCode.Chain( off );  }
     void Statement();
     void GenStmnt();            // evtl. Statement-Opcode erzeugen
-    USHORT GetPC();
-    USHORT GetOffset()              { return GetPC() + 1; }
+    UINT32 GetPC();
+    UINT32 GetOffset()              { return GetPC() + 1; }
     void Save();
 
     // #29955 for-Schleifen-Ebene pflegen
     void IncForLevel( void ) { nForLevel++; }
     void DecForLevel( void ) { nForLevel--; }
+
+    static UINT32 calcNewOffSet( BYTE* pCode, UINT16 nOffset );
+    static UINT16 calcLegacyOffSet( BYTE* pCode, UINT32 nOffset );
+
+};
+
+template < class T, class S >
+class PCodeBuffConvertor
+{
+    T m_nSize; //
+    BYTE* m_pStart;
+    BYTE* m_pCnvtdBuf;
+    S m_nCnvtdSize; //
+
+    //  Disable usual copying symantics and bodgy default ctor
+    PCodeBuffConvertor();
+    PCodeBuffConvertor(const PCodeBuffConvertor& );
+    PCodeBuffConvertor& operator = ( const PCodeBuffConvertor& );
+public:
+    PCodeBuffConvertor( BYTE* pCode, T nSize ): m_nSize( nSize ),  m_pStart( pCode ), m_pCnvtdBuf( NULL ), m_nCnvtdSize( 0 ){ convert(); }
+    S GetSize(){ return m_nCnvtdSize; }
+    void convert();
+    // Caller owns the buffer returned
+    BYTE* GetBuffer() { return m_pCnvtdBuf; }
 };
 
 // #111897 PARAM_INFO flags start at 0x00010000 to not
