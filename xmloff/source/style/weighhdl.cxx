3e802a25c941f8d9c901d0426458f52a8dfff744
Vladimir Glazounov <vg@openoffice.org>
2006-11-01 15:09:25 +0000

    INTEGRATION: CWS kendy15 (1.6.6); FILE MERGED 2006/10/20 09:06:44 kendy 1.6.6.1: #i70594# Fix warning: XYZ is used uninitialized in this function

--- xmloff/source/style/weighhdl.cxx
+++ xmloff/source/style/weighhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: weighhdl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 14:51:57 $
+ *  last change: $Author: vg $ $Date: 2006-11-01 16:09:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -165,7 +165,7 @@ sal_Bool XMLFontWeightPropHdl::exportXML( OUString& rStrExpValue, const Any& rVa
     sal_Bool bRet = sal_False;
     FontWeight eWeight;
 
-    float fValue;
+    float fValue = float();
     if( !( rValue >>= fValue ) )
     {
         sal_Int32 nValue;
