8878bfef9c0f3bbbaee14b08560d9a28318b4a7b
Vladimir Glazounov <vg@openoffice.org>
2006-11-01 14:12:28 +0000

    INTEGRATION: CWS swqbf88 (1.23.26); FILE MERGED 2006/10/05 09:24:39 od 1.23.26.1: #i70122# method <SwAnchoredObject::GetObjRectWithSpaces()> 	 - use bounding rectangle of anchored object.

--- sw/source/core/layout/anchoredobject.cxx
+++ sw/source/core/layout/anchoredobject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: anchoredobject.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:16:21 $
+ *  last change: $Author: vg $ $Date: 2006-11-01 15:12:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -740,7 +740,11 @@ const SwRect& SwAnchoredObject::GetObjRectWithSpaces() const
     }
     if ( !mbObjRectWithSpacesValid )
     {
-        maObjRectWithSpaces = GetObjRect();
+        // --> OD 2006-10-05 #i70122# - correction:
+        // use bounding rectangle of anchored objects.
+//        maObjRectWithSpaces = GetObjRect();
+        maObjRectWithSpaces = GetObjBoundRect();
+        // <--
         const SwFrmFmt& rFmt = GetFrmFmt();
         const SvxULSpaceItem& rUL = rFmt.GetULSpace();
         const SvxLRSpaceItem& rLR = rFmt.GetLRSpace();
