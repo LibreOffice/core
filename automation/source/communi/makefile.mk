4f9cb4487b66a9bd851fb79291e2f6157a91a6c8
Vladimir Glazounov <vg@openoffice.org>
2006-11-02 11:17:11 +0000

    INTEGRATION: CWS bserver22 (1.4.28); FILE MERGED 2006/10/23 17:50:17 kz 1.4.28.1: #141908# syntax

--- automation/source/communi/makefile.mk
+++ automation/source/communi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-20 00:20:53 $
+#   last change: $Author: vg $ $Date: 2006-11-02 12:17:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -47,7 +47,7 @@ TARGET=communi
 # --- Files --------------------------------------------------------
 
 SLOFILES=   \
-    $(SLO)$/communi.obj \
+    $(SLO)$/communi.obj
 
 # --- Targets ------------------------------------------------------
 
