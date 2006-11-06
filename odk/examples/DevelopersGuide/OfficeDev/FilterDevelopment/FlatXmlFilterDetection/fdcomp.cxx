105dd4d77fc9c4d64c876610560ae5443f15d045
Kurt Zenker <kz@openoffice.org>
2006-11-06 14:02:34 +0000

    INTEGRATION: CWS jsc14 (1.4.14); FILE MERGED 2006/10/25 08:52:26 jsc 1.4.14.1: #i70636# remove auto generated precompiled header include

--- odk/examples/DevelopersGuide/OfficeDev/FilterDevelopment/FlatXmlFilterDetection/fdcomp.cxx
+++ odk/examples/DevelopersGuide/OfficeDev/FilterDevelopment/FlatXmlFilterDetection/fdcomp.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fdcomp.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:12:38 $
+ *  last change: $Author: kz $ $Date: 2006-11-06 15:02:34 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -37,9 +37,6 @@
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_odk.hxx"
 
 #include <stdio.h>
 
