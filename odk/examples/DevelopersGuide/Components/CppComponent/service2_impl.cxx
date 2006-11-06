b5d4023e2dda63a4bc9c863f5cb5f04ff3edc632
Kurt Zenker <kz@openoffice.org>
2006-11-06 14:00:19 +0000

    INTEGRATION: CWS jsc14 (1.6.14); FILE MERGED 2006/10/25 08:52:24 jsc 1.6.14.1: #i70636# remove auto generated precompiled header include

--- odk/examples/DevelopersGuide/Components/CppComponent/service2_impl.cxx
+++ odk/examples/DevelopersGuide/Components/CppComponent/service2_impl.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: service2_impl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:10:05 $
+ *  last change: $Author: kz $ $Date: 2006-11-06 15:00:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -37,9 +37,6 @@
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_odk.hxx"
 
 #include <cppuhelper/implbase3.hxx> // "3" implementing three interfaces
 #include <cppuhelper/factory.hxx>
