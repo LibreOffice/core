d872d6bb7bcf7edc8f702c0269ebf4a95c08738e
Kurt Zenker <kz@openoffice.org>
2006-11-07 13:48:52 +0000

    INTEGRATION: CWS vq39 (1.2.26); FILE MERGED 2006/11/03 04:47:48 vq 1.2.26.1: #i71114# Add missing pch includes.

--- dbaccess/source/ui/misc/asyncmodaldialog.cxx
+++ dbaccess/source/ui/misc/asyncmodaldialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: asyncmodaldialog.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-10-05 13:07:26 $
+ *  last change: $Author: kz $ $Date: 2006-11-07 14:48:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
 
 #ifndef DBACCESS_ASYNCMODALDIALOG_HXX
 #include "asyncmodaldialog.hxx"
