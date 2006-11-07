d3fd833be0fbc121c36e7e0b14d6b1ccd238f196
Kurt Zenker <kz@openoffice.org>
2006-11-07 13:49:55 +0000

    INTEGRATION: CWS vq39 (1.2.14); FILE MERGED 2006/11/03 03:05:12 vq 1.2.14.1: #i71114# Add missing pch includes.

--- comphelper/source/xml/attributelist.cxx
+++ comphelper/source/xml/attributelist.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: attributelist.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-13 11:41:10 $
+ *  last change: $Author: kz $ $Date: 2006-11-07 14:49:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_comphelper.hxx"
 
 #ifndef _ATTRIBUTE_LIST_HXX
 #include <comphelper/attributelist.hxx>
