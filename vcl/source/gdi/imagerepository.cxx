f1c1e969477c4b5c5d54ce9cd8f3116b42a2ec4b
Kurt Zenker <kz@openoffice.org>
2006-11-07 13:52:21 +0000

    INTEGRATION: CWS vq39 (1.2.52); FILE MERGED 2006/11/03 03:05:14 vq 1.2.52.1: #i71114# Add missing pch includes.

--- vcl/source/gdi/imagerepository.cxx
+++ vcl/source/gdi/imagerepository.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imagerepository.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-10-05 13:09:59 $
+ *  last change: $Author: kz $ $Date: 2006-11-07 14:52:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
 
 #ifndef VCL_IMAGEREPOSITORY_HXX
 #include "imagerepository.hxx"
