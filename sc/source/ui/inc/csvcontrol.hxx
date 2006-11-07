423f066a178c96b12231f7baa5989cd5b9350f3f
Kurt Zenker <kz@openoffice.org>
2006-11-07 13:50:33 +0000

    INTEGRATION: CWS pure01 (1.13.310); FILE MERGED 2006/11/06 12:21:55 sparcmoz 1.13.310.1: #70684# invalid pure specifier (only = 0 is allowed)

--- sc/source/ui/inc/csvcontrol.hxx
+++ sc/source/ui/inc/csvcontrol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: csvcontrol.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:17:47 $
+ *  last change: $Author: kz $ $Date: 2006-11-07 14:50:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -444,7 +444,7 @@ public:
 
 protected:
     /** Derived classes create a new accessible object here. */
-    virtual ScAccessibleCsvControl* ImplCreateAccessible() = NULL;
+    virtual ScAccessibleCsvControl* ImplCreateAccessible() = 0;
 };
 
 
