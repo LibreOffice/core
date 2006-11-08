f09e6ef3ff3b5b25930d08cb159ebf62135c3eb6
Kurt Zenker <kz@openoffice.org>
2006-11-08 10:54:34 +0000

    INTEGRATION: CWS basmgr01 (1.1.2); FILE ADDED 2006/09/28 20:21:29 fs 1.1.2.1: #i69957# sb library does provide UNO components now ...

---
+++ basic/source/uno/sbmodule.hxx
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: sbmodule.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-11-08 11:54:34 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SBMODULE_HXX
+#define SBMODULE_HXX
+
+#ifndef COMPHELPER_INC_COMPHELPER_COMPONENTMODULE_HXX
+#include <comphelper/componentmodule.hxx>
+#endif
+
+//........................................................................
+namespace basic
+{
+//........................................................................
+
+    DECLARE_COMPONENT_MODULE( BasicModule, BasicModuleClient );
+
+//........................................................................
+} // namespace basic
+//........................................................................
+
+#endif // SBMODULE_HXX
+
