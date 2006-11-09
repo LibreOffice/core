a61ed23fa12c51e3aac8f7d77ffb7d374f8bba15
Kurt Zenker <kz@openoffice.org>
2006-11-09 09:51:38 +0000

    #i10000# avoid warning

--- basic/source/uno/namecont.cxx
+++ basic/source/uno/namecont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: namecont.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 11:54:13 $
+ *  last change: $Author: kz $ $Date: 2006-11-09 10:51:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -692,11 +692,13 @@ sal_Bool SfxLibraryContainer::init(
             }
             catch ( xml::sax::SAXException& e )
             {
+                (void) e; // avoid warning
                 OSL_ENSURE( 0, OUStringToOString( e.Message, RTL_TEXTENCODING_ASCII_US ).getStr() );
                 return sal_False;
             }
             catch ( io::IOException& e )
             {
+                (void) e; // avoid warning
                 OSL_ENSURE( 0, OUStringToOString( e.Message, RTL_TEXTENCODING_ASCII_US ).getStr() );
                 return sal_False;
             }
