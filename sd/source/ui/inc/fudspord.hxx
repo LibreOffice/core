92d5e57a84572c9adae8c6ec21fe56ae0e3d2f6a
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 13:33:45 +0000

    INTEGRATION: CWS aw024 (1.2.308); FILE MERGED 2006/02/01 21:06:06 aw 1.2.308.4: adaption to m154 after resync 2006/01/26 20:02:57 aw 1.2.308.3: RESYNC: (1.3-1.4); FILE MERGED 2005/09/17 12:29:57 aw 1.2.308.2: RESYNC: (1.2-1.3); FILE MERGED 2005/05/19 12:11:30 aw 1.2.308.1: #i39529#

--- sd/source/ui/inc/fudspord.hxx
+++ sd/source/ui/inc/fudspord.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fudspord.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-12-14 17:10:48 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 14:33:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,8 +45,8 @@
 #include <vcl/pointr.hxx>
 #endif
 
+class SdrDropMarkerOverlay;
 class SdrObject;
-class SdrViewUserMarker;
 
 namespace sd {
 
@@ -74,10 +74,11 @@ public:
 
 protected:
     virtual ~FuDisplayOrder (void);
+    void implClearOverlay();
 
     Pointer         aPtr;
-     SdrObject*         pRefObj;
-     SdrViewUserMarker* pUserMarker;
+    SdrObject*      pRefObj;
+    SdrDropMarkerOverlay*                       mpOverlay;
 
 private:
     FuDisplayOrder (
