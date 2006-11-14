08fc53da6fccb51d47d86e57fa1ffd25965c9ca8
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 12:48:19 +0000

    INTEGRATION: CWS aw024 (1.3.506); FILE MERGED 2006/09/21 19:32:43 aw 1.3.506.5: RESYNC: (1.6-1.7); FILE MERGED 2006/07/04 13:19:29 aw 1.3.506.4: RESYNC: (1.5-1.6); FILE MERGED 2006/01/26 22:11:37 aw 1.3.506.3: RESYNC: (1.4-1.5); FILE MERGED 2005/09/18 05:09:08 aw 1.3.506.2: RESYNC: (1.3-1.4); FILE MERGED 2005/05/19 12:31:39 aw 1.3.506.1: #i39529#

--- svx/source/svdraw/svdoutl.cxx
+++ svx/source/svdraw/svdoutl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdoutl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:58:53 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 13:48:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,9 +57,6 @@
 #ifndef _SFXITEMPOOL_HXX //autogen wg. SfxItemPool
 #include <svtools/itempool.hxx>
 #endif
-
-
-//TYPEINIT1( SdrOutliner, Outliner );
 
 /*************************************************************************
 |*
