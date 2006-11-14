fa0f66450493358b5296c54797ca1751e2d44ba0
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 15:14:59 +0000

    INTEGRATION: CWS aw024 (1.3.18); FILE MERGED 2006/09/22 05:58:51 aw 1.3.18.3: RESYNC: (1.4-1.5); FILE MERGED 2005/09/20 04:42:02 aw 1.3.18.2: RESYNC: (1.3-1.4); FILE MERGED 2005/08/04 14:47:08 sj 1.3.18.1: #48467# using GraphicExporter component for exports, graphic filter progress now works now via xStatusIndicater instead of callback mechanism

--- goodies/source/filter.vcl/idxf/idxf.cxx
+++ goodies/source/filter.vcl/idxf/idxf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idxf.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:50:46 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 16:14:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,22 +45,16 @@
 
 //================== GraphicImport - die exportierte Funktion ================
 
-#ifdef WNT
-extern "C" BOOL _cdecl GraphicImport(SvStream & rStream, Graphic & rGraphic,
-                            PFilterCallback pCallback, void * pCallerData,
-                                FilterConfigItem*, BOOL)
-#else
-extern "C" BOOL GraphicImport(SvStream & rStream, Graphic & rGraphic,
-                            PFilterCallback pCallback, void * pCallerData,
-                                FilterConfigItem*, BOOL)
-#endif
+extern "C" BOOL __LOADONCALLAPI GraphicImport(SvStream & rStream, Graphic & rGraphic, FilterConfigItem*, BOOL )
 {
     DXFRepresentation aDXF;
     DXF2GDIMetaFile aConverter;
     GDIMetaFile aMTF;
 
-    if (aDXF.Read(rStream,pCallback,pCallerData,0,60)==FALSE) return FALSE;
-    if (aConverter.Convert(aDXF,aMTF,pCallback,pCallerData,60,100)==FALSE) return FALSE;
+    if ( aDXF.Read( rStream, 0, 60 ) == FALSE )
+        return FALSE;
+    if ( aConverter.Convert( aDXF, aMTF, 60, 100 ) == FALSE )
+        return FALSE;
     rGraphic=Graphic(aMTF);
 
     return TRUE;
