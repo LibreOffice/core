423cd1031eb3605afe9040bbe5580e9e906baffe
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 14:33:09 +0000

    INTEGRATION: CWS aw024 (1.17.72); FILE MERGED 2005/09/19 15:20:59 aw 1.17.72.2: RESYNC: (1.17-1.18); FILE MERGED 2005/04/26 14:50:14 aw 1.17.72.1: #i39528#

--- chart2/source/tools/makefile.mk
+++ chart2/source/tools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 01:35:43 $
+#   last change: $Author: ihi $ $Date: 2006-11-14 15:33:09 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -113,7 +113,8 @@ SHL1STDLIBS=    \
                 $(CPPULIB)			\
                 $(CPPUHELPERLIB)	\
                 $(SALLIB)			\
-                $(TOOLSLIB)
+                $(TOOLSLIB)			\
+                $(BASEGFXLIB)
 
 #				$(COMPHELPERLIB)	\
 #				$(GOODIESLIB)		\
