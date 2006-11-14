03d0d50d2604080cb0ee3dc013f76d2f7ae97461
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 13:01:39 +0000

    INTEGRATION: CWS aw024 (1.10.202); FILE MERGED 2006/05/29 17:04:46 aw 1.10.202.3: RESYNC: (1.11-1.12); FILE MERGED 2005/09/19 16:03:25 aw 1.10.202.2: RESYNC: (1.10-1.11); FILE MERGED 2005/05/17 16:02:01 aw 1.10.202.1: #i39529#

--- filter/source/flash/makefile.mk
+++ filter/source/flash/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 14:13:15 $
+#   last change: $Author: ihi $ $Date: 2006-11-14 14:01:39 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,7 @@ SHL1STDLIBS+=\
 # dynamic libraries
 SHL1STDLIBS+=\
     $(SVTOOLLIB)		\
-    $(GOODIESLIB)		\
+    $(BASEGFXLIB)		\
     $(VCLLIB)			\
     $(UNOTOOLSLIB)		\
     $(TOOLSLIB)			\
