a2db3c550578b696e38918adb9084a48cf376a9f
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 13:37:27 +0000

    INTEGRATION: CWS aw024 (1.2.54); FILE MERGED 2006/09/21 23:35:59 aw 1.2.54.6: RESYNC: (1.7-1.8); FILE MERGED 2006/05/12 20:50:03 aw 1.2.54.5: RESYNC: (1.5-1.7); FILE MERGED 2006/01/26 20:16:00 aw 1.2.54.4: RESYNC: (1.4-1.5); FILE MERGED 2005/11/18 14:44:36 aw 1.2.54.3: RESYNC: (1.3-1.4); FILE MERGED 2005/09/17 13:47:55 aw 1.2.54.2: RESYNC: (1.2-1.3); FILE MERGED 2005/05/19 12:11:35 aw 1.2.54.1: #i39529#

--- sd/source/ui/tools/IdleDetection.cxx
+++ sd/source/ui/tools/IdleDetection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: IdleDetection.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 19:21:53 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 14:37:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,6 +46,10 @@
 #include <sfx2/viewfrm.hxx>
 
 #include <com/sun/star/frame/XFrame.hdl>
+
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
 
 using namespace ::com::sun::star;
 
