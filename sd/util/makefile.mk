0643799debaf32ac4c18729d65dca5050450051a
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 13:48:07 +0000

    INTEGRATION: CWS aw024 (1.38.22); FILE MERGED 2006/08/03 16:42:08 aw 1.38.22.7: RESYNC: (1.44-1.45); FILE MERGED 2006/07/04 14:57:18 aw 1.38.22.6: RESYNC: (1.43-1.44); FILE MERGED 2006/05/12 21:01:23 aw 1.38.22.5: RESYNC: (1.42-1.43); FILE MERGED 2006/01/26 20:26:01 aw 1.38.22.4: RESYNC: (1.40-1.42); FILE MERGED 2005/11/18 14:51:13 aw 1.38.22.3: RESYNC: (1.39-1.40); FILE MERGED 2005/09/17 14:21:35 aw 1.38.22.2: RESYNC: (1.38-1.39); FILE MERGED 2005/05/19 12:11:47 aw 1.38.22.1: #i39529#

--- sd/util/makefile.mk
+++ sd/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.45 $
+#   $Revision: 1.46 $
 #
-#   last change: $Author: obo $ $Date: 2006-07-13 09:54:26 $
+#   last change: $Author: ihi $ $Date: 2006-11-14 14:48:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -80,6 +80,7 @@ SHL1STDLIBS+= \
     $(CPPCANVASLIB) \
     $(BASEGFXLIB) \
     $(GOODIESLIB) \
+    $(BASEGFXLIB) \
     $(SVTOOLLIB) \
     $(TKLIB) \
     $(VCLLIB) \
