45e09645554e41360df9f607fced312b61bc4a6c
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 15:12:46 +0000

    INTEGRATION: CWS aw024 (1.8.36); FILE MERGED 2006/07/07 15:59:41 aw 1.8.36.4: adaptions after resync SRC680m171->SRC680m174 2006/07/04 17:43:55 aw 1.8.36.3: RESYNC: (1.9-1.10); FILE MERGED 2005/09/20 04:36:14 aw 1.8.36.2: RESYNC: (1.8-1.9); FILE MERGED 2005/08/04 14:47:06 sj 1.8.36.1: #48467# using GraphicExporter component for exports, graphic filter progress now works now via xStatusIndicater instead of callback mechanism

--- goodies/source/filter.vcl/etiff/makefile.mk
+++ goodies/source/filter.vcl/etiff/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: ihi $ $Date: 2006-06-29 11:30:59 $
+#   last change: $Author: ihi $ $Date: 2006-11-14 16:12:46 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@ SLOFILES =	$(SLO)$/etiff.obj
 
 SHL1TARGET= 	eti$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB= 	etiff
-SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB)
+SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB) $(SVTOOLLIB)
 SHL1LIBS=		$(SLB)$/etiff.lib
 
 .IF "$(GUI)" != "UNX"
