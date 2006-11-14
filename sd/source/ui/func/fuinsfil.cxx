d37672b2bcc2b3c8f00d8d1916e162839dfc0fa9
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 13:29:27 +0000

    INTEGRATION: CWS aw024 (1.34.92); FILE MERGED 2006/09/21 23:13:15 aw 1.34.92.5: RESYNC: (1.37-1.38); FILE MERGED 2006/01/26 19:53:15 aw 1.34.92.4: RESYNC: (1.35-1.37); FILE MERGED 2005/09/17 11:25:47 aw 1.34.92.3: RESYNC: (1.34-1.35); FILE MERGED 2005/05/26 11:24:33 aw 1.34.92.2: #i39531# 2005/05/19 12:11:26 aw 1.34.92.1: #i39529#

--- sd/source/ui/func/fuinsfil.cxx
+++ sd/source/ui/func/fuinsfil.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuinsfil.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 18:51:00 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 14:29:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -443,7 +443,7 @@ BOOL FuInsertFile::InsSDDinDrMode(SfxMedium* pMedium)
         }
         else
         {
-            pPage = static_cast<SdPage*>(pView->GetPageViewPvNum(0)->GetPage());
+            pPage = static_cast<SdPage*>(pView->GetSdrPageView()->GetPage());
         }
 
         USHORT nPos = 0xFFFF;
