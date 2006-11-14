52c7588f5c4cd26d4381d52de5e9d4e3f36534b4
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 14:09:36 +0000

    INTEGRATION: CWS aw024 (1.26.152); FILE MERGED 2006/09/22 00:55:18 aw 1.26.152.7: RESYNC: (1.31-1.33); FILE MERGED 2006/09/08 19:36:04 aw 1.26.152.6: RESYNC: (1.30-1.31); FILE MERGED 2006/08/03 16:54:50 aw 1.26.152.5: RESYNC: (1.29-1.30); FILE MERGED 2006/05/12 21:24:17 aw 1.26.152.4: RESYNC: (1.28-1.29); FILE MERGED 2006/01/26 20:40:23 aw 1.26.152.3: RESYNC: (1.27-1.28); FILE MERGED 2005/09/17 18:35:06 aw 1.26.152.2: RESYNC: (1.26-1.27); FILE MERGED 2005/05/19 12:15:15 aw 1.26.152.1: #i39529#

--- sw/source/core/frmedt/fefly1.cxx
+++ sw/source/core/frmedt/fefly1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fefly1.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:14:10 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 15:09:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -475,7 +475,7 @@ void SwFEShell::SetFlyPos( const Point& rAbsPos )
     if ( !pFly )
         return;
 
-    SwSaveHdl aSaveX( Imp() );
+    //SwSaveHdl aSaveX( Imp() );
 
     //Bei Absatzgebundenen Flys muss ausgehend von der absoluten
     //Position ein neuer Anker gesetzt werden. Anker und neue RelPos werden
