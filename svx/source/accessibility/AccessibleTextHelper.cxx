093d3b3f662f2441c8761a91c16e634301d985b3
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 12:12:51 +0000

    INTEGRATION: CWS aw024 (1.37.892); FILE MERGED 2006/11/10 03:00:31 aw 1.37.892.8: RESYNC: (1.43-1.44); FILE MERGED 2006/09/21 18:09:01 aw 1.37.892.7: RESYNC: (1.42-1.43); FILE MERGED 2006/08/03 15:09:27 aw 1.37.892.6: RESYNC: (1.41-1.42); FILE MERGED 2006/07/04 12:02:00 aw 1.37.892.5: RESYNC: (1.40-1.41); FILE MERGED 2006/05/12 18:21:36 aw 1.37.892.4: RESYNC: (1.39-1.40); FILE MERGED 2006/01/26 21:36:33 aw 1.37.892.3: RESYNC: (1.38-1.39); FILE MERGED 2005/09/18 01:52:07 aw 1.37.892.2: RESYNC: (1.37-1.38); FILE MERGED 2005/05/19 12:31:06 aw 1.37.892.1: #i39529#

--- svx/source/accessibility/AccessibleTextHelper.cxx
+++ svx/source/accessibility/AccessibleTextHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleTextHelper.cxx,v $
  *
- *  $Revision: 1.44 $
+ *  $Revision: 1.45 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 12:01:00 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 13:12:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -121,6 +121,10 @@
 
 #ifndef _VCL_UNOHELP_HXX
 #include <vcl/unohelp.hxx>
+#endif
+
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
 #endif
 
 //------------------------------------------------------------------------
