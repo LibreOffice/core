15df777e7185dece1bf1d09b591e2f0e5ae91ab0
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 14:39:16 +0000

    INTEGRATION: CWS aw024 (1.1.1.1.1090); FILE MERGED 2005/09/19 18:28:32 aw 1.1.1.1.1090.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/08/04 15:37:39 sj 1.1.1.1.1090.1: #48467# using GraphicExporter component for exports, graphic filter progress now works now via xStatusIndicater instead of callback mechanism

--- svtools/inc/xpmread.hxx
+++ svtools/inc/xpmread.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xpmread.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:19:23 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 15:39:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,7 +122,7 @@ private:
     ULONG               ImplGetULONG( ULONG nPara );
 
 public:
-                        XPMReader( SvStream& rStm, void* pCallData );
+                        XPMReader( SvStream& rStm );
     virtual             ~XPMReader();
 
     ReadState           ReadXPM( Graphic& rGraphic );
@@ -134,6 +134,6 @@ public:
 // - ImportXPM -
 // -------------
 
-BOOL ImportXPM( SvStream& rStream, Graphic& rGraphic, void* pCallerData );
+BOOL ImportXPM( SvStream& rStream, Graphic& rGraphic );
 
 #endif // _XPMREAD_HXX
