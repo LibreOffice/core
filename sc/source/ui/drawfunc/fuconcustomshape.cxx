195638032932b13f5b0ed70f644d48a6bc196eec
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 14:50:24 +0000

    INTEGRATION: CWS aw024 (1.6.94); FILE MERGED 2006/08/03 19:03:43 aw 1.6.94.4: RESYNC: (1.8-1.9); FILE MERGED 2005/09/20 01:42:38 aw 1.6.94.3: RESYNC: (1.7-1.8); FILE MERGED 2005/05/13 17:09:08 aw 1.6.94.2: RESYNC: (1.6-1.7); FILE MERGED 2005/04/26 14:54:36 aw 1.6.94.1: #i39528#

--- sc/source/ui/drawfunc/fuconcustomshape.cxx
+++ sc/source/ui/drawfunc/fuconcustomshape.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fuconcustomshape.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 13:48:56 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 15:50:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,6 +79,8 @@
 #ifndef _SVX_ADJITEM_HXX
 #include <svx/adjitem.hxx>
 #endif
+
+#include <math.h>
 
 //------------------------------------------------------------------------
 
