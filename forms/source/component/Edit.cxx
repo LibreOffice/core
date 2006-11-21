58855f1bf48e0daa9ac09ff625fe62a7a8f243cb
Vladimir Glazounov <vg@openoffice.org>
2006-11-21 16:40:37 +0000

    INTEGRATION: CWS pj65 (1.35.10); FILE MERGED 2006/10/31 13:41:03 pjanik 1.35.10.1: #i71027#: prevent warnings on Mac OS X with gcc 4.0.1.

--- forms/source/component/Edit.cxx
+++ forms/source/component/Edit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Edit.cxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 11:10:05 $
+ *  last change: $Author: vg $ $Date: 2006-11-21 17:40:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -728,7 +728,7 @@ void OEditModel::onConnectedDbColumn( const Reference< XInterface >& _rxForm )
             m_bMaxTextLenModified = getINT16(m_xAggregateSet->getPropertyValue(PROPERTY_MAXTEXTLEN)) != 0;
             if ( !m_bMaxTextLenModified )
             {
-                sal_Int32 nFieldLen;
+                sal_Int32 nFieldLen = 0;
                 xField->getPropertyValue(::rtl::OUString::createFromAscii("Precision")) >>= nFieldLen;
 
                 if (nFieldLen && nFieldLen <= USHRT_MAX)
