3b0ccd85a9ec28f5e46d47370fb5855f96217415
Vladimir Glazounov <vg@openoffice.org>
2006-11-21 16:14:32 +0000

    INTEGRATION: CWS pj65 (1.18.50); FILE MERGED 2006/11/06 11:14:30 pjanik 1.18.50.1: #i71027#: Prevent warnings on Mac OS X.

--- svx/source/xml/xmlxtexp.cxx
+++ svx/source/xml/xmlxtexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlxtexp.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 13:30:00 $
+ *  last change: $Author: vg $ $Date: 2006-11-21 17:14:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -479,7 +479,7 @@ void SvxXMLColorEntryExporter::exportEntry( const OUString& rStrName, const Any&
 {
     mrExport.AddAttribute( XML_NAMESPACE_DRAW, XML_NAME, rStrName );
 
-    sal_Int32 nColor;
+    sal_Int32 nColor = 0;
     rValue >>= nColor;
 
     OUStringBuffer aOut;
