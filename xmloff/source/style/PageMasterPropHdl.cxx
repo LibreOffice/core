664999d0d49af401d81eaeb1ccbc5b0e96d699bd
Vladimir Glazounov <vg@openoffice.org>
2006-11-21 16:36:03 +0000

    INTEGRATION: CWS pj65 (1.13.12); FILE MERGED 2006/10/31 14:03:43 pjanik 1.13.12.1: #i71027#: prevent warnings on Mac OS X with gcc 4.0.1.

--- xmloff/source/style/PageMasterPropHdl.cxx
+++ xmloff/source/style/PageMasterPropHdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageMasterPropHdl.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 14:45:48 $
+ *  last change: $Author: vg $ $Date: 2006-11-21 17:36:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -305,7 +305,7 @@ sal_Bool XMLPMPropHdl_PaperTrayNumber::exportXML(
         const SvXMLUnitConverter& ) const
 {
     sal_Bool    bRet = sal_False;
-    sal_Int32   nPaperTray;
+    sal_Int32   nPaperTray = 0;
 
     if( rValue >>= nPaperTray )
     {
