a7a8f245b25c62eb96214d051b5b5cc13035c778
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:41:53 +0000

    INTEGRATION: CWS hr33 (1.4.138); FILE MERGED 2006/11/02 18:46:07 hr 1.4.138.2: RESYNC: (1.4-1.5); FILE MERGED 2006/05/03 16:02:15 hr 1.4.138.1: #i55967#: remove not needed STL includes

--- framework/inc/uifactory/statusbarcontrollerfactory.hxx
+++ framework/inc/uifactory/statusbarcontrollerfactory.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: statusbarcontrollerfactory.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 11:07:22 $
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:41:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,13 +35,6 @@
 
 #ifndef __FRAMEWORK_UIFACTORY_STATUSBARCONTROLLERFACTORY_HXX_
 #define __FRAMEWORK_UIFACTORY_STATUSBARCONTROLLERFACTORY_HXX_
-
-/** Attention: stl headers must(!) be included at first. Otherwhise it can make trouble
-               with solaris headers ...
-*/
-#include <vector>
-#include <list>
-#include <hash_map>
 
 //_________________________________________________________________________________________________________________
 //  my own includes
