295c02093c140e9e6ee4c334546ccee4ad4427cf
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:43:48 +0000

    INTEGRATION: CWS hr33 (1.13.2); FILE MERGED 2006/11/07 12:50:55 hr 1.13.2.2: RESYNC: (1.13-1.17); FILE MERGED 2006/05/03 15:01:44 hr 1.13.2.1: #i55967#: remove not needed STL includes

--- transex3/source/xmlparse.cxx
+++ transex3/source/xmlparse.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlparse.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-10-24 13:26:54 $
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:43:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,8 +35,6 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_transex3.hxx"
-#include <hash_map> /* std::hashmap*/
-#include <deque>    /* std::deque*/
 #include <iterator> /* std::iterator*/
 
 
