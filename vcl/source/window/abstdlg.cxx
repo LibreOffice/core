55e18e426a75163a09ed0fe6afdeed1df9e569f4
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:39:50 +0000

    INTEGRATION: CWS asyncdialogs (1.2.332); FILE MERGED 2006/09/20 18:42:24 pb 1.2.332.4: RESYNC: (1.4-1.5); FILE MERGED 2006/07/12 18:54:48 pb 1.2.332.3: RESYNC: (1.3-1.4); FILE MERGED 2005/12/08 13:09:30 kso 1.2.332.2: RESYNC: (1.2-1.3); FILE MERGED 2005/11/01 14:28:53 kso 1.2.332.1: #i57125# - dialog adaptions for Threading Framework.

--- vcl/source/window/abstdlg.cxx
+++ vcl/source/window/abstdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: abstdlg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:14:58 $
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:39:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,5 +57,10 @@ VclAbstractDialogFactory* VclAbstractDialogFactory::Create()
 }
 
 VclAbstractDialog::~VclAbstractDialog()
+{
+}
+
+// virtual
+VclAbstractDialog2::~VclAbstractDialog2()
 {
 }
