3da118aa81b68e8004a52b99afe90de94f2085d8
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:38:12 +0000

    INTEGRATION: CWS hr33 (1.9.2); FILE MERGED 2006/11/07 12:48:30 hr 1.9.2.2: RESYNC: (1.9-1.12); FILE MERGED 2006/05/02 16:54:43 hr 1.9.2.1: #i10000#: clean up license header

--- testtools/source/bridgetest/cppobj.cxx
+++ testtools/source/bridgetest/cppobj.cxx
@@ -1,13 +1,37 @@
-/**************************************************************************
-#*
-#*    last change   $Author: vg $ $Date: 2006-09-25 12:47:43 $
-#*    $Revision: 1.12 $
-#*
-#*    $Logfile: $
-#*
-#*    Copyright (c) 1989 - 2000, Star Office GmbH
-#*
-#************************************************************************/
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: cppobj.cxx,v $
+ *
+ *  $Revision: 1.13 $
+ *
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:38:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_testtools.hxx"
