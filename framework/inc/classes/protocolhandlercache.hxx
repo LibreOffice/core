686fe3f8d02b97540719fb8c0da8205bd4d2d21f
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:40:27 +0000

    INTEGRATION: CWS hr33 (1.5.138); FILE MERGED 2006/11/02 18:55:26 hr 1.5.138.2: RESYNC: (1.5-1.6); FILE MERGED 2006/05/03 15:50:21 hr 1.5.138.1: #i55967#: remove not needed STL includes

--- framework/inc/classes/protocolhandlercache.hxx
+++ framework/inc/classes/protocolhandlercache.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: protocolhandlercache.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:49:37 $
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:40:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,12 +35,6 @@
 
 #ifndef __FRAMEWORK_CLASSES_PROTOCOLHANDLERCACHE_HXX_
 #define __FRAMEWORK_CLASSES_PROTOCOLHANDLERCACHE_HXX_
-
-/** Attention: stl headers must(!) be included at first. Otherwhise it can make trouble
-               with solaris headers ...
-*/
-#include <hash_map>
-#include <vector>
 
 //_________________________________________________________________________________________________________________
 //  my own includes
