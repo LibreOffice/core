0edeeeeb2109f28e97a3d1fb28926fbc6df34c54
Vladimir Glazounov <vg@openoffice.org>
2006-11-22 09:41:30 +0000

    INTEGRATION: CWS asyncdialogs (1.1.4); FILE ADDED 2005/11/01 14:27:40 kso 1.1.4.1: #i57125# - dialog adaptions for Threading Framework.

---
+++ svtools/inc/dialogclosedlistener.hxx
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *
+ *  $RCSfile: dialogclosedlistener.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2006-11-22 10:41:30 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SVTOOLS_DIALOGCLOSEDLISTENER_HXX
+#define _SVTOOLS_DIALOGCLOSEDLISTENER_HXX
+
+#ifndef INCLUDED_SVTDLLAPI_H
+#include "svtools/svtdllapi.h"
+#endif
+
+#ifndef  _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+#ifndef  _COM_SUN_STAR_UI_DIALOGS_XDIALOGCLOSEDLISTENER_HPP_
+#include <com/sun/star/ui/dialogs/XDialogClosedListener.hpp>
+#endif
+#ifndef _LINK_HXX
+#include <tools/link.hxx>
+#endif
+
+//.........................................................................
+namespace svt
+{
+//.........................................................................
+
+    //=====================================================================
+    //= ODialogClosedListener
+    //=====================================================================
+    /**
+        C++ class to implement a ::com::sun::star::ui::dialogs::XDialogClosedListener
+    */
+    class SVT_DLLPUBLIC DialogClosedListener :
+        public ::cppu::WeakImplHelper1< ::com::sun::star::ui::dialogs::XDialogClosedListener >
+    {
+    private:
+        /**
+            This link will be called when the dialog was closed.
+
+            The link must have the type:
+            DECL_LINK( DialogClosedHdl, ::com::sun::star::ui::dialogs::DialogClosedEvent* );
+        */
+        Link    m_aDialogClosedLink;
+
+    public:
+        DialogClosedListener();
+        DialogClosedListener( const Link& rLink );
+
+        inline void SetDialogClosedLink( const Link& rLink ) { m_aDialogClosedLink = rLink; }
+
+        // XDialogClosedListener methods
+        virtual void SAL_CALL   dialogClosed( const ::com::sun::star::ui::dialogs::DialogClosedEvent& aEvent ) throw (::com::sun::star::uno::RuntimeException);
+
+        // XEventListener methods
+        virtual void SAL_CALL   disposing( const ::com::sun::star::lang::EventObject& Source ) throw( ::com::sun::star::uno::RuntimeException );
+    };
+
+//.........................................................................
+}   // namespace svt
+//.........................................................................
+
+#endif// COMPHELPER_DIALOGCLOSEDLISTENER_HXX
+
