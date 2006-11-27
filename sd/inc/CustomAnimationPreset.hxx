3481979b263b03160bf13dd307af47b97f030638
Rüdiger Timm <rt@openoffice.org>
2006-11-27 08:39:18 +0000

    #i71977# Missing include added (patch by pjanik).

--- sd/inc/CustomAnimationPreset.hxx
+++ sd/inc/CustomAnimationPreset.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CustomAnimationPreset.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:50:54 $
+ *  last change: $Author: rt $ $Date: 2006-11-27 09:39:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,6 +56,7 @@
 #include <CustomAnimationEffect.hxx>
 #endif
 
+#include <hash_map>
 
 namespace sd {
 
