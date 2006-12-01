e974f1aaf2b37fa1fe920c4637360504bb218e95
Rüdiger Timm <rt@openoffice.org>
2006-12-01 14:44:09 +0000

    INTEGRATION: CWS swautomatic01 (1.37.92); FILE MERGED 2006/10/10 17:34:39 fme 1.37.92.3: RESYNC: (1.38-1.39); FILE MERGED 2006/08/10 16:19:59 fme 1.37.92.2: RESYNC: (1.37-1.38); FILE MERGED 2006/08/01 15:07:47 fme 1.37.92.1: #i64576# Automatic styles

--- sw/source/core/layout/findfrm.cxx
+++ sw/source/core/layout/findfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: findfrm.cxx,v $
  *
- *  $Revision: 1.39 $
+ *  $Revision: 1.40 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:17:32 $
+ *  last change: $Author: rt $ $Date: 2006-12-01 15:44:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -665,14 +665,6 @@ const SwAttrSet* SwFrm::GetAttrSet() const
         return &((const SwCntntFrm*)this)->GetNode()->GetSwAttrSet();
     else
         return &((const SwLayoutFrm*)this)->GetFmt()->GetAttrSet();
-}
-
-SwAttrSet* SwFrm::GetAttrSet()
-{
-    if ( IsCntntFrm() )
-        return &((SwCntntFrm*)this)->GetNode()->GetSwAttrSet();
-    else
-        return (SwAttrSet*)&((SwLayoutFrm*)this)->GetFmt()->GetAttrSet();
 }
 
 /*************************************************************************
