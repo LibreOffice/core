c9a340802a377b8b93205ecd64cc5e794db461ae
Rüdiger Timm <rt@openoffice.org>
2006-12-01 13:37:35 +0000

    INTEGRATION: CWS wfilter (1.24.14); FILE MERGED 2006/11/17 08:48:22 sj 1.24.14.1: #i69283# making filter module warning free

--- solenv/inc/unxsoli4.mk
+++ solenv/inc/unxsoli4.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxsoli4.mk,v $
 #
-#   $Revision: 1.25 $
+#   $Revision: 1.26 $
 #
-#   last change: $Author: vg $ $Date: 2006-11-21 15:11:38 $
+#   last change: $Author: rt $ $Date: 2006-12-01 14:37:35 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -114,7 +114,6 @@ MODULES_WITH_WARNINGS := \
     chart2 \
     devtools \
     extensions \
-    filter \
     lingu \
     r_tools \
     sc \
