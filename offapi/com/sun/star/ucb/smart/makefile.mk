5a3f4d9271156f88ec860590df5643bbf6ee3bc3
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:17:59 +0000

    INTEGRATION: CWS dba22a (1.4.222); FILE MERGED 2006/11/13 10:55:22 fs 1.4.222.1: corrected PRJNAME

--- offapi/com/sun/star/ucb/smart/makefile.mk
+++ offapi/com/sun/star/ucb/smart/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 08:17:05 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:17:59 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssucbs
 PACKAGE=com$/sun$/star$/ucb$/smart
