4d8828bc37201b3b17096d12f0af038ad7660ccc
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:10:08 +0000

    INTEGRATION: CWS dba22a (1.7.222); FILE MERGED 2006/11/13 10:55:12 fs 1.7.222.1: corrected PRJNAME

--- offapi/com/sun/star/mozilla/makefile.mk
+++ offapi/com/sun/star/mozilla/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 02:27:03 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:10:08 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssmozilla
 PACKAGE=com$/sun$/star$/mozilla
