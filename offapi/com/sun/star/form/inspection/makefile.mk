97510ebe0fff347a6e1af6e350d063a6e6f328d6
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:05:32 +0000

    INTEGRATION: CWS dba22a (1.2.162); FILE MERGED 2006/11/13 10:55:07 fs 1.2.162.1: corrected PRJNAME

--- offapi/com/sun/star/form/inspection/makefile.mk
+++ offapi/com/sun/star/form/inspection/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: vg $ $Date: 2006-03-14 11:05:12 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:05:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssfinsp
 PACKAGE=com$/sun$/star$/form$/inspection
