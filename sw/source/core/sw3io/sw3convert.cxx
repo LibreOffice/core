edf82013dff7d03c778e3091066a8b4f391bec80
Rüdiger Timm <rt@openoffice.org>
2006-12-01 14:44:23 +0000

    INTEGRATION: CWS swautomatic01 (1.3.324); FILE MERGED 2006/10/10 17:42:38 fme 1.3.324.2: RESYNC: (1.3-1.4); FILE MERGED 2006/08/09 08:56:32 fme 1.3.324.1: #i64576# Automatic styles

--- sw/source/core/sw3io/sw3convert.cxx
+++ sw/source/core/sw3io/sw3convert.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw3convert.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:27:20 $
+ *  last change: $Author: rt $ $Date: 2006-12-01 15:44:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -526,7 +526,7 @@ void lcl_sw3io__ConvertNumLRSpace( SwTxtNode& rTxtNd, const SwNumRule& rNumRule,
     // man den wieder Erreichen, sonst den der NumRule.
     const SfxPoolItem *pItem;
     const SvxLRSpaceItem *pParaLRSpace = 0;
-    const SwAttrSet *pAttrSet = rTxtNd.GetpSwAttrSet();
+    const SfxItemSet *pAttrSet = rTxtNd.GetpSwAttrSet();
     if( pAttrSet &&
         SFX_ITEM_SET == pAttrSet->GetItemState( RES_LR_SPACE, FALSE, &pItem ) )
         pParaLRSpace = (const SvxLRSpaceItem *)pItem;
