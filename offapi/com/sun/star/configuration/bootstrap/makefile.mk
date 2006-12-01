9378462f3296d228fe0f92effaca3c2fb4029240
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:02:23 +0000

    INTEGRATION: CWS dba22a (1.3.222); FILE MERGED 2006/11/13 10:55:02 fs 1.3.222.1: corrected PRJNAME

--- offapi/com/sun/star/configuration/bootstrap/makefile.mk
+++ offapi/com/sun/star/configuration/bootstrap/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 22:45:27 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:02:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #*************************************************************************
 
 PRJ = ..$/..$/..$/..$/..
-PRJNAME = offapi
+PRJNAME=offapi
 
 TARGET = csscbootstrap
 PACKAGE = com$/sun$/star$/configuration$/bootstrap
