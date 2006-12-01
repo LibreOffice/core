44fee08b2c7e403efac53ca97ef71a80f11a1a58
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:19:21 +0000

    INTEGRATION: CWS dba22a (1.4.222); FILE MERGED 2006/11/13 10:55:24 fs 1.4.222.1: corrected PRJNAME

--- offapi/com/sun/star/xforms/makefile.mk
+++ offapi/com/sun/star/xforms/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:33:41 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:19:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssxforms
 PACKAGE=com$/sun$/star$/xforms
