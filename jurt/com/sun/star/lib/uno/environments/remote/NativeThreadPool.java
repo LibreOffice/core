6fa4daa6417ad1ad20c51ed8454ad4d235fa8ee5
Rüdiger Timm <rt@openoffice.org>
2006-12-01 13:52:02 +0000

    INTEGRATION: CWS sb23 (1.2.66); FILE MERGED 2006/08/18 16:21:34 sb 1.2.66.3: RESYNC: (1.2-1.3); FILE MERGED 2005/03/17 14:42:32 sb 1.2.66.2: #88601# Minor fix. 2005/03/14 10:55:36 sb 1.2.66.1: #i35277# Further cleanup.

--- jurt/com/sun/star/lib/uno/environments/remote/NativeThreadPool.java
+++ jurt/com/sun/star/lib/uno/environments/remote/NativeThreadPool.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: NativeThreadPool.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:01:26 $
+ *  last change: $Author: rt $ $Date: 2006-12-01 14:52:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,8 +74,9 @@ final class NativeThreadPool implements IThreadPool {
     }
 
     public void putJob(Job job) {
-        putJob(pool, job.getThreadId().getBytes(), job,
-               job.getOperation() != null, !job.isSynchron());
+        putJob(
+            pool, job.getThreadId().getBytes(), job, job.isRequest(),
+            job.isRequest() && !job.isSynchronous());
     }
 
     public void dispose(Throwable throwable) {
