74a6fda935eda45ecb69f3a53aef6d93135383a2
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:00:24 +0000

    INTEGRATION: CWS dba22a (1.3.222); FILE MERGED 2006/11/13 10:55:00 fs 1.3.222.1: corrected PRJNAME

--- offapi/com/sun/star/animations/makefile.mk
+++ offapi/com/sun/star/animations/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 20:43:05 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:00:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssanimations
 PACKAGE=com$/sun$/star$/animations
