7de37839f56d5d00eb2081f4a909d5ce17b98751
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:33:12 +0000

    INTEGRATION: CWS dba22a (1.15.6); FILE MERGED 2006/11/13 10:19:46 fs 1.15.6.1: corrected PRJNAME

--- hsqldb/makefile.mk
+++ hsqldb/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: vg $ $Date: 2006-09-25 09:43:59 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:33:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=.
 
-PRJNAME=so_hsqldb
+PRJNAME=hsqldb
 TARGET=so_hsqldb
 
 # --- Settings -----------------------------------------------------
