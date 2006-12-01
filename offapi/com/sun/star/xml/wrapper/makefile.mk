65239144390065cd36ca64690db2b4377762bf50
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:21:54 +0000

    INTEGRATION: CWS dba22a (1.3.222); FILE MERGED 2006/11/13 10:55:27 fs 1.3.222.1: corrected PRJNAME

--- offapi/com/sun/star/xml/wrapper/makefile.mk
+++ offapi/com/sun/star/xml/wrapper/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 10:40:19 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:21:54 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=xmlsecurity
+PRJNAME=offapi
 
 TARGET=xsec-wrapper
 PACKAGE=com$/sun$/star$/xml$/wrapper
