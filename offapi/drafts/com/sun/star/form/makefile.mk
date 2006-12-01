ffb6fd4cb5576bd7f9415bb6836b8110659aab6a
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:22:20 +0000

    INTEGRATION: CWS dba22a (1.5.222); FILE MERGED 2006/11/13 10:55:28 fs 1.5.222.1: corrected PRJNAME

--- offapi/drafts/com/sun/star/form/makefile.mk
+++ offapi/drafts/com/sun/star/form/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 14:20:58 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:22:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=ui
+PRJNAME=offapi
 
 TARGET=dcssform
 PACKAGE=drafts$/com$/sun$/star$/form
