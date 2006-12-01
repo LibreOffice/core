4a4529875c96cabd78d92e5b286b69434d65e029
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:21:28 +0000

    INTEGRATION: CWS dba22a (1.4.222); FILE MERGED 2006/11/13 10:55:26 fs 1.4.222.1: corrected PRJNAME

--- offapi/com/sun/star/xml/input/makefile.mk
+++ offapi/com/sun/star/xml/input/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 10:09:11 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:21:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ = ..$/..$/..$/..$/..
 
-PRJNAME = api
+PRJNAME=offapi
 
 TARGET = cssxmlinput
 PACKAGE = com$/sun$/star$/xml$/input
