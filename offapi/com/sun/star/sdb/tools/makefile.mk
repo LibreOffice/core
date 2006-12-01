5d05308727296a12abfff44e34240c6dba4e6893
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:13:37 +0000

    INTEGRATION: CWS dba22a (1.2.102); FILE MERGED 2006/11/13 10:55:17 fs 1.2.102.1: corrected PRJNAME

--- offapi/com/sun/star/sdb/tools/makefile.mk
+++ offapi/com/sun/star/sdb/tools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2006-07-10 14:52:10 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:13:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=csssdbtools
 PACKAGE=com$/sun$/star$/sdb$/tools
