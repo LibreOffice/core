841c4ce3f9257b0015784f634e74ed318aaa98a4
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:10:21 +0000

    INTEGRATION: CWS dba22a (1.10.222); FILE MERGED 2006/11/13 10:55:12 fs 1.10.222.1: corrected PRJNAME

--- offapi/com/sun/star/packages/makefile.mk
+++ offapi/com/sun/star/packages/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 02:29:32 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:10:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=csspackages
 PACKAGE=com$/sun$/star$/packages
