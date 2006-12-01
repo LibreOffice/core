baf9d0dd66aa846a6b45c8f1c1930454cc7a65c5
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:01:07 +0000

    INTEGRATION: CWS dba22a (1.6.222); FILE MERGED 2006/11/13 10:55:01 fs 1.6.222.1: corrected PRJNAME

--- offapi/com/sun/star/chart/makefile.mk
+++ offapi/com/sun/star/chart/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 22:17:54 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:01:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=csschart
 PACKAGE=com$/sun$/star$/chart
