0e037e26191a0a790541a469098346e937e22f4b
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:03:00 +0000

    INTEGRATION: CWS dba22a (1.7.222); FILE MERGED 2006/11/13 10:55:03 fs 1.7.222.1: corrected PRJNAME

--- offapi/com/sun/star/datatransfer/dnd/makefile.mk
+++ offapi/com/sun/star/datatransfer/dnd/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 22:57:41 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:03:00 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=api
+PRJNAME=offapi
 
 TARGET=cssddnd
 PACKAGE=com$/sun$/star$/datatransfer$/dnd
