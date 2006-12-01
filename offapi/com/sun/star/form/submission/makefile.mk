b76730dea4b4dd774fe3ca31d5ba86e84f87250b
Rüdiger Timm <rt@openoffice.org>
2006-12-01 16:07:00 +0000

    INTEGRATION: CWS dba22a (1.3.220); FILE MERGED 2006/11/13 10:55:07 fs 1.3.220.1: corrected PRJNAME

--- offapi/com/sun/star/form/submission/makefile.mk
+++ offapi/com/sun/star/form/submission/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 00:51:24 $
+#   last change: $Author: rt $ $Date: 2006-12-01 17:06:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..$/..$/..
 
-PRJNAME=ui
+PRJNAME=offapi
 
 TARGET=cssfsubmission
 PACKAGE=com$/sun$/star$/form$/submission
