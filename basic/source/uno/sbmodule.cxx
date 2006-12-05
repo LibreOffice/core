fc42919e3322a8f258689457083ba4d0f2844ad3
Rüdiger Timm <rt@openoffice.org>
2006-12-05 11:05:01 +0000

    #i71457# pch include statement added (patch by vq).

--- basic/source/uno/sbmodule.cxx
+++ basic/source/uno/sbmodule.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbmodule.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 11:54:24 $
+ *  last change: $Author: rt $ $Date: 2006-12-05 12:04:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basic.hxx"
 
 #ifndef SBMODULE_HXX
 #include "sbmodule.hxx"
