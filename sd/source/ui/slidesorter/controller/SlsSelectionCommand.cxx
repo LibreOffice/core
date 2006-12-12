b7a644a638e14663b35f7e2cb19665b7113b47a1
Kurt Zenker <kz@openoffice.org>
2006-12-12 17:29:24 +0000

    INTEGRATION: CWS sdwarningsbegone (1.5.38); FILE MERGED 2006/11/27 15:15:03 cl 1.5.38.1: #i69285# warning free code changes for sd project

--- sd/source/ui/slidesorter/controller/SlsSelectionCommand.cxx
+++ sd/source/ui/slidesorter/controller/SlsSelectionCommand.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsSelectionCommand.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 19:07:12 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 18:29:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@ void SelectionCommand::AddPages (::std::auto_ptr<PageSelector::PageSelection> pS
     PageSelector::PageSelection::iterator iPageIndex = pSelection->begin();
     PageSelector::PageSelection::iterator iEnd = pSelection->end();
     for (; iPageIndex!=iEnd; ++iPageIndex)
-        AddPage(*iPageIndex);
+        AddPage((USHORT)(*iPageIndex));
 }
 
 
