09b61b895be22350a468daca608bb96b0c333d7c
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:52:02 +0000

    INTEGRATION: CWS sdwarningsbegone (1.3.314); FILE MERGED 2006/11/22 12:42:07 cl 1.3.314.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/inc/unoaprms.hxx
+++ sd/source/ui/inc/unoaprms.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoaprms.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:59:00 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:52:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -170,7 +170,6 @@ public:
     virtual ~SdAnimationPrmsUndoAction();
     virtual void Undo();
     virtual void Redo();
-    virtual void Repeat();
 };
 
 #endif      // _SD_UNOAPRMS_HXX
