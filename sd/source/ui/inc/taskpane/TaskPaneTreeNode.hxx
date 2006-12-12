4382a85d5308d6fdcaa59b199a7be90ed3d3a71d
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:55:25 +0000

    INTEGRATION: CWS sdwarningsbegone (1.5.316); FILE MERGED 2006/11/22 12:42:08 cl 1.5.316.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/inc/taskpane/TaskPaneTreeNode.hxx
+++ sd/source/ui/inc/taskpane/TaskPaneTreeNode.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TaskPaneTreeNode.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:02:41 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:55:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,7 +47,7 @@
 
 namespace sd {
 class ObjectBarManager;
-};
+}
 
 namespace sd { namespace toolpanel {
 
