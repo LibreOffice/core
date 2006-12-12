f013e80734bffb22ca8e3ec1ec6a8c8191c314b9
Kurt Zenker <kz@openoffice.org>
2006-12-12 14:57:35 +0000

    INTEGRATION: CWS obo08 (1.55.94); FILE MERGED 2006/12/06 10:07:02 obo 1.55.94.1: #i70239# removed manifest file after binding it into library/executable

--- solenv/inc/_tg_app.mk
+++ solenv/inc/_tg_app.mk
@@ -128,6 +128,7 @@ $(APP1TARGETN): $(APP1OBJS) $(APP1LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP1TARGET).lnk
         +-$(RM) $(MISC)\$(APP1TARGET).lst
@@ -296,6 +297,7 @@ $(APP2TARGETN): $(APP2OBJS) $(APP2LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP2TARGET).lnk
         +-$(RM) $(MISC)\$(APP2TARGET).lst
@@ -464,6 +466,7 @@ $(APP3TARGETN): $(APP3OBJS) $(APP3LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP3TARGET).lnk
         +-$(RM) $(MISC)\$(APP3TARGET).lst
@@ -632,6 +635,7 @@ $(APP4TARGETN): $(APP4OBJS) $(APP4LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP4TARGET).lnk
         +-$(RM) $(MISC)\$(APP4TARGET).lst
@@ -800,6 +804,7 @@ $(APP5TARGETN): $(APP5OBJS) $(APP5LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP5TARGET).lnk
         +-$(RM) $(MISC)\$(APP5TARGET).lst
@@ -968,6 +973,7 @@ $(APP6TARGETN): $(APP6OBJS) $(APP6LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP6TARGET).lnk
         +-$(RM) $(MISC)\$(APP6TARGET).lst
@@ -1136,6 +1142,7 @@ $(APP7TARGETN): $(APP7OBJS) $(APP7LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP7TARGET).lnk
         +-$(RM) $(MISC)\$(APP7TARGET).lst
@@ -1304,6 +1311,7 @@ $(APP8TARGETN): $(APP8OBJS) $(APP8LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP8TARGET).lnk
         +-$(RM) $(MISC)\$(APP8TARGET).lst
@@ -1472,6 +1480,7 @@ $(APP9TARGETN): $(APP9OBJS) $(APP9LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP9TARGET).lnk
         +-$(RM) $(MISC)\$(APP9TARGET).lst
@@ -1640,6 +1649,7 @@ $(APP10TARGETN): $(APP10OBJS) $(APP10LIBS) \
         )
     @-+echo linking $@.manifest ...
     +$(IFEXIST) $@.manifest $(THEN) mt.exe -manifest $@.manifest -outputresource:$@$(EMQ);1 $(FI)
+    +$(IFEXIST) $@.manifest $(THEN) $(RM) $@.manifest $(FI)
 .ELSE
         +-$(RM) $(MISC)\$(APP10TARGET).lnk
         +-$(RM) $(MISC)\$(APP10TARGET).lst
