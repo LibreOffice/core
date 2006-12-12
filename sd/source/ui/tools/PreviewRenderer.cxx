8b7512ca0cf825b5d6a9c4d34c6f43e57303ee4f
Kurt Zenker <kz@openoffice.org>
2006-12-12 17:53:05 +0000

    INTEGRATION: CWS sdwarningsbegone (1.10.36); FILE MERGED 2006/11/22 15:11:08 cl 1.10.36.2: RESYNC: (1.10-1.11); FILE MERGED 2006/11/22 12:42:18 cl 1.10.36.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/tools/PreviewRenderer.cxx
+++ sd/source/ui/tools/PreviewRenderer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PreviewRenderer.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 14:37:41 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 18:53:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -479,7 +479,7 @@ Image PreviewRenderer::ScaleBitmap (
 
 
 
-void PreviewRenderer::SFX_NOTIFY(SfxBroadcaster& rBC,
+void PreviewRenderer::SFX_NOTIFY(SfxBroadcaster&,
     const TypeId& rBCType,
     const SfxHint& rHint,
     const TypeId& rHintType)
