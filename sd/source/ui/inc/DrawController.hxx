d5a05ba65e7b56049cb71342f9aab3b6e368eb33
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:32:29 +0000

    INTEGRATION: CWS sdwarningsbegone (1.11.184); FILE MERGED 2006/11/22 15:07:07 cl 1.11.184.2: RESYNC: (1.11-1.12); FILE MERGED 2006/11/22 12:42:00 cl 1.11.184.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/inc/DrawController.hxx
+++ sd/source/ui/inc/DrawController.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DrawController.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-01 14:16:26 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:32:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -265,6 +265,9 @@ protected:
     */
     void ThrowIfDisposed (void) const
         throw (::com::sun::star::lang::DisposedException);
+
+    using cppu::OPropertySetHelper::disposing;
+    using cppu::OPropertySetHelper::getFastPropertyValue;
 
 private:
     ViewShellBase& mrBase;
