3c8ef6caba413679a46752831d6ad35730af53a6
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:37:39 +0000

    INTEGRATION: CWS sdwarningsbegone (1.9.182); FILE MERGED 2006/11/22 12:42:02 cl 1.9.182.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/inc/SlideSorterViewShell.hxx
+++ sd/source/ui/inc/SlideSorterViewShell.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlideSorterViewShell.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:27:10 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:37:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -73,7 +73,7 @@ class SlideSorterViewShell
     friend class controller::SlotManager;
 public:
     TYPEINFO();
-    SFX_DECL_INTERFACE(SD_IF_SDSLIDESORTERVIEWSHELL);
+    SFX_DECL_INTERFACE(SD_IF_SDSLIDESORTERVIEWSHELL)
 
     enum TabBarEntry
     {
