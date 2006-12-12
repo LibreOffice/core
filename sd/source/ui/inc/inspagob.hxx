728fa5298b663b058b5aaedb9bf0019e2db78c23
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:44:08 +0000

    INTEGRATION: CWS sdwarningsbegone (1.3.316); FILE MERGED 2006/11/22 12:42:04 cl 1.3.316.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/inc/inspagob.hxx
+++ sd/source/ui/inc/inspagob.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inspagob.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:44:50 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:44:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ private:
     HelpButton              aBtnHelp;
 
     SfxMedium*              pMedium;
-    const SdDrawDocument*   pDoc;
+    const SdDrawDocument*   mpDoc;
     const String&           rName;
 
     void                    Reset();
