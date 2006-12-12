9375a182811e5906d81b9c3e59b8e48fdbe320d2
Kurt Zenker <kz@openoffice.org>
2006-12-12 16:27:25 +0000

    INTEGRATION: CWS sdwarningsbegone (1.13.38); FILE MERGED 2006/11/27 13:48:05 cl 1.13.38.2: #i69285# warning free code changes for sd project 2006/11/22 12:41:58 cl 1.13.38.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/func/outlinfo.cxx
+++ sd/source/ui/func/outlinfo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outlinfo.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 18:58:50 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 17:27:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -94,13 +94,13 @@ OutlinerInfo::~OutlinerInfo()
 
 // -----------------------------------------------------------------------------
 
-void OutlinerInfo::SetTextObj( SdDrawDocument* pDoc, SdrRectObj* pObj, OutputDevice* _pOut )
+void OutlinerInfo::SetTextObj( SdDrawDocument* pDoc, SdrRectObj* pObj, OutputDevice* pOut )
 {
     Clear();
 
     SdrOutliner& rOutliner = pDoc->GetDrawOutliner();
 
-    pOut = _pOut;
+    mpOut = pOut;
     rOutliner.SetText( *pObj->GetOutlinerParaObject() );
 
     aObjBound = pObj->GetCurrentBoundRect();
@@ -197,133 +197,13 @@ BOOL OutlinerInfo::GetParaCharCount( const ULONG nPara ) const
 
 // -----------------------------------------------------------------------------
 
-/*
 IMPL_LINK(OutlinerInfo, DrawPortionHdl, DrawPortionInfo*, pInfo)
 {
     // #101500#
     Point aStart;
     sal_Bool bIsVertical(IsVertical());
-    sal_Bool bUseBreakIterator(sal_False);
-
-    pOut->SetFont((const Font&)pInfo->rFont);
-
-    FontMetric aFontMetric(pOut->GetFontMetric());
-
-    // initialize BreakIterator
-    Reference < com::sun::star::i18n::XBreakIterator > xBreak;
-    Reference < XMultiServiceFactory > xMSF = ::comphelper::getProcessServiceFactory();
-    Reference < XInterface > xInterface = xMSF->createInstance(::rtl::OUString::createFromAscii("com.sun.star.i18n.BreakIterator"));
-    ::com::sun::star::lang::Locale aFontLocale = SvxCreateLocale(pInfo->rFont.GetLanguage());
-
-    if(xInterface.is())
-    {
-        Any x = xInterface->queryInterface(::getCppuType((const Reference< XBreakIterator >*)0));
-        x >>= xBreak;
-    }
-
-    if(xBreak.is())
-    {
-        bUseBreakIterator = sal_True;
-    }
-
-    if(bIsVertical)
-    {
-        aStart.X() = pInfo->rStartPos.X() + aTextOffset.X() - aFontMetric.GetDescent();
-        aStart.Y() = pInfo->rStartPos.Y() + aTextOffset.Y();
-
-        const Point aTopLeft(aStart.X(), aParaBound.Top());
-        const Point aBottomRight(aStart.X() + aFontMetric.GetLineHeight(), aParaBound.Bottom());
-        const Rectangle aCurRect(aTopLeft, aBottomRight);
-
-        if(pInfo->nPara != nCurPara)
-        {
-            nCurPara = pInfo->nPara;
-            pParagraphs[nCurPara].aRect = aCurRect;
-        }
-        else
-        {
-            pParagraphs[nCurPara].aRect.Union(aCurRect);
-        }
-    }
-    else
-    {
-        aStart.X() = pInfo->rStartPos.X() + aTextOffset.X();
-        aStart.Y() = pInfo->rStartPos.Y() + aTextOffset.Y() - aFontMetric.GetAscent();
-
-        const Point aTopLeft(aParaBound.Left(), aStart.Y());
-        const Point aBottomRight(aParaBound.Right(), aStart.Y() + aFontMetric.GetLineHeight());
-        const Rectangle aCurRect(aTopLeft, aBottomRight);
-
-        if(pInfo->nPara != nCurPara)
-        {
-            nCurPara = pInfo->nPara;
-            pParagraphs[nCurPara].aRect = aCurRect;
-        }
-        else
-        {
-            pParagraphs[nCurPara].aRect.Union(aCurRect);
-        }
-    }
-
-    if(pInfo->nTextLen && (0xFFFF != pInfo->nIndex))
-    {
-        pParagraphs[nCurPara].nCharCount += pInfo->nTextLen;
-
-        for(sal_uInt16 nCharIndex(0); nCharIndex < pInfo->nTextLen;)
-        {
-            xub_StrLen nNextGlyphLen(1);
-            sal_Bool bIsSingleSpace(sal_False);
-
-            if(bUseBreakIterator)
-            {
-                sal_Int32 nDone(0L);
-                nNextGlyphLen = (xub_StrLen)xBreak->nextCharacters( pInfo->rText, pInfo->nTextStart, aFontLocale,
-                    CharacterIteratorMode::SKIPCELL, 1, nDone) - (pInfo->nTextStart);
-            }
-
-            if(!bIsSingleSpace)
-            {
-                Rectangle aTextRect;
-                sal_Bool bDidWork = pOut->GetTextBoundRect(aTextRect, pInfo->rText,
-                    pInfo->nTextStart, pInfo->nTextStart + nCharIndex, nNextGlyphLen);
-
-                if(bDidWork)
-                {
-//                  if(bIsVertical)
-//                      aTextRect.SetPos(aStart + Point(-aFontMetric.GetDescent(), 0));
-//                  else
-//                      aTextRect.SetPos(aStart + Point(0, aFontMetric.GetAscent()));
-                    aTextRect.Move(0, aFontMetric.GetAscent());
-                    aTextRect.Move(aStart.X(), aStart.Y());
-
-                    aCharacterList.Insert(
-                        new OutlinerCharacter(
-                            aTextRect,
-                            pInfo->nPara,
-                            pInfo->rFont.GetColor()
-                            // #101500# CharCode is no longer used.
-                            //,pInfo->rText.GetChar(nCharIndex + pInfo->nTextStart)
-                            ),
-                        LIST_APPEND);
-                }
-            }
-
-            // use glyph lengh for loop
-            nCharIndex += nNextGlyphLen;
-        }
-    }
-
-    return 0L;
-}
-*/
-
-IMPL_LINK(OutlinerInfo, DrawPortionHdl, DrawPortionInfo*, pInfo)
-{
-    // #101500#
-    Point aStart;
-    sal_Bool bIsVertical(IsVertical());
-    pOut->SetFont((const Font&)pInfo->rFont);
-    FontMetric aFontMetric(pOut->GetFontMetric());
+    mpOut->SetFont((const Font&)pInfo->rFont);
+    FontMetric aFontMetric(mpOut->GetFontMetric());
     sal_Bool bUseBreakIterator(sal_False);
 
     // initialize BreakIterator
@@ -385,7 +265,7 @@ IMPL_LINK(OutlinerInfo, DrawPortionHdl, DrawPortionInfo*, pInfo)
     if(pInfo->nTextLen && (0xFFFF != pInfo->nIndex))
     {
         pParagraphs[nCurPara].nCharCount += pInfo->nTextLen;
-        sal_uInt16 nInsertIndex((sal_uInt16)LIST_APPEND);
+        sal_uInt16 nInsertIndex(0xffff);
 
         if(pInfo->IsRTL())
             nInsertIndex = (sal_uInt16)aCharacterList.Count();
@@ -404,7 +284,7 @@ IMPL_LINK(OutlinerInfo, DrawPortionHdl, DrawPortionInfo*, pInfo)
 
             if(!bIsSingleSpace)
             {
-                Size aGlyphSize(pInfo->rFont.GetPhysTxtSize(pOut,
+                Size aGlyphSize(pInfo->rFont.GetPhysTxtSize(mpOut,
                     pInfo->rText, nCharIndex + pInfo->nTextStart, nNextGlyphLen));
 
                 if(bIsVertical)
@@ -430,7 +310,7 @@ IMPL_LINK(OutlinerInfo, DrawPortionHdl, DrawPortionInfo*, pInfo)
                     aStart.X() = pInfo->rStartPos.X() + aTextOffset.X() + dx;
             }
 
-            nCharIndex += nNextGlyphLen;
+            nCharIndex = nCharIndex + nNextGlyphLen;
         }
     }
 
