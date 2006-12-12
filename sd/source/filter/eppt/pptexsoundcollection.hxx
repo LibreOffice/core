7f4330c5e4b2581c6dcdb482c723f69627a26411
Kurt Zenker <kz@openoffice.org>
2006-12-12 15:41:24 +0000

    INTEGRATION: CWS sdwarningsbegone (1.2.190); FILE MERGED 2006/11/22 12:41:37 cl 1.2.190.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/filter/eppt/pptexsoundcollection.hxx
+++ sd/source/filter/eppt/pptexsoundcollection.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pptexsoundcollection.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2006-03-06 09:04:26 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 16:41:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,6 @@ class ExSoundCollection : private List
         void                    Write( SvStream& rSt );
 };
 
-}; // namespace ppt
+} // namespace ppt
 
 #endif
