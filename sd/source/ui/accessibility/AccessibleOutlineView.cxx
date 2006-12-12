eb04a481654186bec9c4da63238c61a84ec8e257
Kurt Zenker <kz@openoffice.org>
2006-12-12 15:48:28 +0000

    INTEGRATION: CWS sdwarningsbegone (1.13.36); FILE MERGED 2006/11/22 14:59:55 cl 1.13.36.2: RESYNC: (1.13-1.14); FILE MERGED 2006/11/22 12:41:40 cl 1.13.36.1: #i69285# warning free code changes for unxlngi6.pro

--- sd/source/ui/accessibility/AccessibleOutlineView.cxx
+++ sd/source/ui/accessibility/AccessibleOutlineView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleOutlineView.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 14:23:41 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 16:48:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -188,8 +188,6 @@ sal_Int32 SAL_CALL
 {
     // forward
     return maTextHelper.GetChildCount();
-
-    return 0;
 }
 
 
