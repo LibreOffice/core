8308dbe29d3bb36edae1b90135b338d41e56ae51
Kurt Zenker <kz@openoffice.org>
2006-12-12 17:52:20 +0000

    INTEGRATION: CWS sdwarningsbegone (1.5.316); FILE MERGED 2006/11/27 13:48:14 cl 1.5.316.1: #i69285# warning free code changes for sd project

--- sd/source/ui/toolpanel/controls/SlideTransitionPanel.hxx
+++ sd/source/ui/toolpanel/controls/SlideTransitionPanel.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlideTransitionPanel.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:43:46 $
+ *  last change: $Author: kz $ $Date: 2006-12-12 18:52:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,6 +70,8 @@ public:
         ::com::sun::star::accessibility::XAccessible > CreateAccessibleObject (
             const ::com::sun::star::uno::Reference<
             ::com::sun::star::accessibility::XAccessible>& rxParent);
+
+    using Window::GetWindow;
 
 private:
     Size maPreferredSize;
