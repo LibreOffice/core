07b07d4966cc5d0cdc4e2a941f2b8ee308e35af9
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:31:01 +0000

    INTEGRATION: CWS presfixes09 (1.4.18); FILE MERGED 2006/09/15 22:14:37 thb 1.4.18.2: RESYNC: (1.4-1.5); FILE MERGED 2006/03/24 18:23:17 thb 1.4.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/animationnodes/animationsetnode.hxx
+++ slideshow/source/engine/animationnodes/animationsetnode.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: animationsetnode.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-26 07:32:46 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:31:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 
 #include "animationbasenode.hxx"
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 class AnimationSetNode : public AnimationBaseNode
@@ -60,7 +60,7 @@ private:
 };
 
 } // namespace internal
-} // namespace presentation
+} // namespace slideshow
 
 #endif /* INCLUDED_SLIDESHOW_ANIMATIONSETNODE_HXX */
 
