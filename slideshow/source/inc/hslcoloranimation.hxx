15f5b6a3024f363ca7ed5926012296d7bea520d7
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:58:03 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/03/24 18:23:34 thb 1.3.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/inc/hslcoloranimation.hxx
+++ slideshow/source/inc/hslcoloranimation.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hslcoloranimation.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:13:21 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:58:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 /* Definition of HSLColorAnimation interface */
 
-namespace presentation
+namespace slideshow
 {
     namespace internal
     {
