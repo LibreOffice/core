de67303486f5c1d747176b9f884b7d3f83b29516
Kurt Zenker <kz@openoffice.org>
2006-12-13 13:46:28 +0000

    INTEGRATION: CWS presfixes09 (1.7.32); FILE MERGED 2006/09/18 12:51:09 thb 1.7.32.3: #i10000# Made code compile warning-free 2006/09/15 15:42:13 thb 1.7.32.2: RESYNC: (1.7-1.8); FILE MERGED 2006/03/15 13:45:57 thb 1.7.32.1: #i49357# Added VCL window coordinate absolutization method

--- canvas/source/tools/makefile.mk
+++ canvas/source/tools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: obo $ $Date: 2006-10-12 11:31:58 $
+#   last change: $Author: kz $ $Date: 2006-12-13 14:46:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -75,7 +75,7 @@ SLOFILES =	\
 
 SHL1TARGET= 	$(TARGET)$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB= 	i$(TARGET)
-SHL1STDLIBS=	$(SALLIB) $(CPPULIB) $(BASEGFXLIB) $(CPPUHELPERLIB) $(COMPHELPERLIB) $(VCLLIB) $(TOOLSLIB)
+SHL1STDLIBS=	$(SALLIB) $(CPPULIB) $(BASEGFXLIB) $(CPPUHELPERLIB) $(COMPHELPERLIB) $(VCLLIB) $(TKLIB)
 
 .IF "$(ENABLE_AGG)"=="YES"
     SLOFILES += $(SLO)$/bitmap.obj \
