9594686d950467ede6d656f8c7f854805fe0a5f5
Kurt Zenker <kz@openoffice.org>
2006-12-13 11:00:44 +0000

    INTEGRATION: CWS oihelp (1.5.42); FILE MERGED 2006/11/13 12:48:37 fs 1.5.42.1: #i71485# changes in the css.inspection interfaces, make this here compile only for the moment

--- extensions/source/propctrlr/newdatatype.cxx
+++ extensions/source/propctrlr/newdatatype.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: newdatatype.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 13:19:45 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 12:00:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -103,7 +103,7 @@ namespace pcr
     }
 
     //--------------------------------------------------------------------
-    IMPL_LINK( NewDataTypeDialog, OnNameModified, void*, _pNotInterestedIn )
+    IMPL_LINK( NewDataTypeDialog, OnNameModified, void*, /*_pNotInterestedIn*/ )
     {
         String sCurrentName = GetName();
         bool bNameIsOK = ( sCurrentName.Len() > 0 )
