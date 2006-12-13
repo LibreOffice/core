b94961d19727747d0e3e20a9366906e4b643bccb
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:42:04 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/10/18 19:58:28 thb 1.3.18.3: RESYNC: (1.3-1.4); FILE MERGED 2006/04/24 13:25:32 thb 1.3.18.2: #i53194# Unified include statements (local headers always have double quotes; external headers angle brackets); reverted EventMultiplexer pause events to shared_ptr; removed EventMultiplexer::removeViewHandler(), since the handler is held weakly, anyway. 2006/03/24 18:23:24 thb 1.3.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/transitions/fourboxwipe.cxx
+++ slideshow/source/engine/transitions/fourboxwipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fourboxwipe.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 08:39:52 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:42:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,14 +36,14 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_slideshow.hxx"
 
-#include "canvas/debug.hxx"
+#include <canvas/debug.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/polygon/b2dpolypolygon.hxx>
+#include <basegfx/numeric/ftools.hxx>
 #include "fourboxwipe.hxx"
-#include "basegfx/matrix/b2dhommatrix.hxx"
-#include "basegfx/polygon/b2dpolypolygon.hxx"
-#include "basegfx/numeric/ftools.hxx"
 
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 ::basegfx::B2DPolyPolygon FourBoxWipe::operator () ( double t )
