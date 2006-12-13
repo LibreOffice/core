9ccb27fd89acf8d0a456a5a44669f7abeb41a4ca
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:54:30 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/03/24 18:23:32 thb 1.3.18.3: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow 2006/03/15 15:22:22 thb 1.3.18.2: #i49357# Removed external include guards from all non-export headers (and from the cxx files, anyway) 2006/03/06 22:14:34 thb 1.3.18.1: #i53194# #i55294# #i59324# Overhauled IntrinsicAnimationActivity; fixes GIF animation import; corrected rehearse timings sprite size; several cosmetic changes (removed external header guards); prepared scene for sprite prio

--- slideshow/source/inc/disposable.hxx
+++ slideshow/source/inc/disposable.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: disposable.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:08:56 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:54:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,18 +36,14 @@
 #ifndef _SLIDESHOW_DISPOSABLE_HXX
 #define _SLIDESHOW_DISPOSABLE_HXX
 
-#ifndef _SAL_TYPES_H_
 #include <sal/types.h>
-#endif
 
-#ifndef BOOST_SHARED_PTR_HPP_INCLUDED
 #include <boost/shared_ptr.hpp>
-#endif
 
 
 /* Definition of Disposable interface */
 
-namespace presentation
+namespace slideshow
 {
     namespace internal
     {
