c25ee6b5e21a7c196ac5be7f6e831497d02b89c8
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:56:15 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/03/24 18:23:33 thb 1.3.18.2: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow 2006/03/06 22:14:34 thb 1.3.18.1: #i53194# #i55294# #i59324# Overhauled IntrinsicAnimationActivity; fixes GIF animation import; corrected rehearse timings sprite size; several cosmetic changes (removed external header guards); prepared scene for sprite prio

--- slideshow/source/inc/eventhandler.hxx
+++ slideshow/source/inc/eventhandler.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: eventhandler.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:11:09 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:56:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,16 +36,14 @@
 #ifndef _SLIDESHOW_EVENTHANDLER_HXX
 #define _SLIDESHOW_EVENTHANDLER_HXX
 
-#ifndef BOOST_SHARED_PTR_HPP_INCLUDED
 #include <boost/shared_ptr.hpp>
-#endif
 
 #include <disposable.hxx>
 
 
 /* Definition of EventHandler interface */
 
-namespace presentation
+namespace slideshow
 {
     namespace internal
     {
