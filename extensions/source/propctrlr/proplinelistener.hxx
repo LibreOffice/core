22feb5e1e2b6e3019169991803232315c23bd26f
Kurt Zenker <kz@openoffice.org>
2006-12-13 11:03:09 +0000

    INTEGRATION: CWS oihelp (1.5.154); FILE MERGED 2006/11/13 14:34:00 fs 1.5.154.1: #i71485# PropertyControlObserver notifications

--- extensions/source/propctrlr/proplinelistener.hxx
+++ extensions/source/propctrlr/proplinelistener.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: proplinelistener.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-03-14 11:31:45 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 12:03:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
