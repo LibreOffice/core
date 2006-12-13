1fc2f2275a0a97ed8812d7075e8b76376c25e2d1
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:01:44 +0000

    INTEGRATION: CWS presfixes09 (1.8.60); FILE MERGED 2006/11/15 12:43:47 thb 1.8.60.1: #140456# en passant, removed obsolete RMBitmap member at various places

--- vcl/source/gdi/impbmp.cxx
+++ vcl/source/gdi/impbmp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impbmp.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:02:51 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:01:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,6 @@
 ImpBitmap::ImpBitmap() :
             mnRefCount  ( 1UL ),
             mnChecksum  ( 0UL ),
-            mpRMBitmap  ( NULL ),
             mpSalBitmap ( ImplGetSVData()->mpDefInst->CreateSalBitmap() ),
             maSourceSize( 0, 0 )
 {
