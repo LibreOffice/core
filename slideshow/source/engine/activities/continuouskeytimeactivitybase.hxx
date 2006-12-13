175870b531ff6d1c60627cfac74ea83152377c84
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:26:18 +0000

    INTEGRATION: CWS presfixes09 (1.4.18); FILE MERGED 2006/10/18 19:53:22 thb 1.4.18.2: RESYNC: (1.4-1.5); FILE MERGED 2006/03/24 18:23:13 thb 1.4.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/activities/continuouskeytimeactivitybase.hxx
+++ slideshow/source/engine/activities/continuouskeytimeactivitybase.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: continuouskeytimeactivitybase.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 13:57:45 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:26:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #include <vector>
 
 
-namespace presentation
+namespace slideshow
 {
     namespace internal
     {
