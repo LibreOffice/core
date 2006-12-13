2a906d8542d9cbac78d6df8e25107c9e93e2521b
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:18:18 +0000

    INTEGRATION: CWS updatefeed (1.1.2); FILE ADDED 2006/12/06 11:20:44 obr 1.1.2.3: #i71853# do not generate headers 2006/12/06 10:44:30 obr 1.1.2.2: #i71853# set http request header fields 2006/11/23 14:56:14 obr 1.1.2.1: #i71853# update information provider service

---
+++ extensions/source/update/feed/makefile.mk
@@ -0,0 +1,73 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2006-12-13 15:18:18 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+PRJ=..$/..$/..
+
+PRJNAME=extensions
+TARGET=updatefeed
+
+LIBTARGET=NO
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings ---
+
+.INCLUDE : settings.mk
+
+# no "lib" prefix
+DLLPRE =
+
+# --- Files ---
+
+
+SLOFILES=\
+    $(SLO)$/updatefeed.obj \
+        
+SHL1NOCHECK=TRUE
+SHL1TARGET=$(TARGET).uno   
+SHL1OBJS=$(SLOFILES)
+
+SHL1IMPLIB=i$(SHL1TARGET)
+SHL1STDLIBS=    \
+        $(CPPUHELPERLIB) \
+        $(CPPULIB) \
+        $(SALLIB)
+        
+SHL1VERSIONMAP=..$/exports.map
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+
+# --- Targets ---
+
+.INCLUDE : target.mk
+
