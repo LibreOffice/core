2225948615c1532c46baa8122fbc80838452784b
Kurt Zenker <kz@openoffice.org>
2006-12-13 15:04:45 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/03/24 18:23:39 thb 1.3.18.2: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow 2006/03/15 15:22:25 thb 1.3.18.1: #i49357# Removed external include guards from all non-export headers (and from the cxx files, anyway)

--- slideshow/source/inc/stringanimation.hxx
+++ slideshow/source/inc/stringanimation.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stringanimation.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:22:05 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 16:04:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,14 +38,12 @@
 
 #include <animation.hxx>
 
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
 
 
 /* Definition of StringAnimation interface */
 
-namespace presentation
+namespace slideshow
 {
     namespace internal
     {
