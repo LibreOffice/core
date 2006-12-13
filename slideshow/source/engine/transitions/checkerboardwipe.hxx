79a1c62bcd1d858e1413a7354483a9a6c82f2f58
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:38:15 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/04/24 13:25:30 thb 1.3.18.2: #i53194# Unified include statements (local headers always have double quotes; external headers angle brackets); reverted EventMultiplexer pause events to shared_ptr; removed EventMultiplexer::removeViewHandler(), since the handler is held weakly, anyway. 2006/03/24 18:23:21 thb 1.3.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/transitions/checkerboardwipe.hxx
+++ slideshow/source/engine/transitions/checkerboardwipe.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: checkerboardwipe.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:49:24 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:38:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,12 +36,13 @@
 #if ! defined INCLUDED_SLIDESHOW_CHECKERBOARDWIPE_HXX
 #define INCLUDED_SLIDESHOW_CHECKERBOARDWIPE_HXX
 
-#include "osl/diagnose.h"
+#include <osl/diagnose.h>
+
 #include "transitiontools.hxx"
 #include "parametricpolypolygon.hxx"
 
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 /// Generate a check board wipe (across)
