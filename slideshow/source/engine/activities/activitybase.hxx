cf95c57b7968c20397ab87f97da793649055fd9b
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:25:03 +0000

    INTEGRATION: CWS presfixes09 (1.5.16); FILE MERGED 2006/03/24 18:23:13 thb 1.5.16.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/activities/activitybase.hxx
+++ slideshow/source/engine/activities/activitybase.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: activitybase.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2005-10-11 08:39:34 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:25:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #include "animatableshape.hxx"
 #include "shapeattributelayer.hxx"
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 /** Base class for animation activities.
