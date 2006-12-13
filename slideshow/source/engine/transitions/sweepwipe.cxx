e2d1fefd87282980b4fed53d746fe3be9f1cf3b9
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:47:12 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/10/18 19:59:48 thb 1.3.18.4: RESYNC: (1.3-1.4); FILE MERGED 2006/10/18 10:45:56 thb 1.3.18.3: #142144# Moving to native M_PI_* constants 2006/04/24 13:25:34 thb 1.3.18.2: #i53194# Unified include statements (local headers always have double quotes; external headers angle brackets); reverted EventMultiplexer pause events to shared_ptr; removed EventMultiplexer::removeViewHandler(), since the handler is held weakly, anyway. 2006/03/24 18:23:27 thb 1.3.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/transitions/sweepwipe.cxx
+++ slideshow/source/engine/transitions/sweepwipe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sweepwipe.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 08:42:26 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:47:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,15 +36,15 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_slideshow.hxx"
 
-#include "canvas/debug.hxx"
+#include <canvas/debug.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/numeric/ftools.hxx>
 #include "clockwipe.hxx"
 #include "sweepwipe.hxx"
 #include "transitiontools.hxx"
-#include "basegfx/matrix/b2dhommatrix.hxx"
-#include "basegfx/numeric/ftools.hxx"
 
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 ::basegfx::B2DPolyPolygon SweepWipe::operator () ( double t )
@@ -73,7 +73,7 @@ namespace internal {
         }
         else {
             aTransform.translate( -0.5, -0.5 );
-            aTransform.rotate( F_PI );
+            aTransform.rotate( M_PI );
             aTransform.translate( 0.5, 0.5 );
             poly.transform( aTransform );
         }
