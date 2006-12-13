61704eff4c5df5efedfae01492ea8849f20a3f36
Kurt Zenker <kz@openoffice.org>
2006-12-13 14:41:42 +0000

    INTEGRATION: CWS presfixes09 (1.3.18); FILE MERGED 2006/03/24 18:23:23 thb 1.3.18.1: #i37778# Moved whole slideshow engine from namespace presentation (which conflicts with one of the UNO subnamespaces) to slideshow

--- slideshow/source/engine/transitions/figurewipe.hxx
+++ slideshow/source/engine/transitions/figurewipe.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: figurewipe.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:53:20 $
+ *  last change: $Author: kz $ $Date: 2006-12-13 15:41:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 #include "parametricpolypolygon.hxx"
 
 
-namespace presentation {
+namespace slideshow {
 namespace internal {
 
 class FigureWipe : public ParametricPolyPolygon
