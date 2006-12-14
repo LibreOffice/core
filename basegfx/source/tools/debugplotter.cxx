0f2bd8c3357f80356eeddef9ca671bc1f23ba536
Kurt Zenker <kz@openoffice.org>
2006-12-14 23:31:23 +0000

    #i10000# add precompiled header declaration

--- basegfx/source/tools/debugplotter.cxx
+++ basegfx/source/tools/debugplotter.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: debugplotter.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 15:08:35 $
+ *  last change: $Author: kz $ $Date: 2006-12-15 00:31:23 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -58,6 +58,9 @@
  *
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_basegfx.hxx"
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
