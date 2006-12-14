3fc5c6a1a2f50a3440229f3499ab33ae7c39fff8
Vladimir Glazounov <vg@openoffice.org>
2006-12-14 16:59:28 +0000

    #i10000# tools needed

--- canvas/source/tools/makefile.mk
+++ canvas/source/tools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: kz $ $Date: 2006-12-13 14:46:28 $
+#   last change: $Author: vg $ $Date: 2006-12-14 17:59:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -75,7 +75,7 @@ SLOFILES =	\
 
 SHL1TARGET= 	$(TARGET)$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB= 	i$(TARGET)
-SHL1STDLIBS=	$(SALLIB) $(CPPULIB) $(BASEGFXLIB) $(CPPUHELPERLIB) $(COMPHELPERLIB) $(VCLLIB) $(TKLIB)
+SHL1STDLIBS=	$(SALLIB) $(CPPULIB) $(BASEGFXLIB) $(CPPUHELPERLIB) $(COMPHELPERLIB) $(VCLLIB) $(TKLIB) $(TOOLSLIB)
 
 .IF "$(ENABLE_AGG)"=="YES"
     SLOFILES += $(SLO)$/bitmap.obj \
