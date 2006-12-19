730f83c3ea4a5102a3cba324bc38566289d654bb
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 15:33:05 +0000

    INTEGRATION: CWS aw038 (1.1.2); FILE ADDED 2006/12/07 17:23:29 cl 1.1.2.1: #i68101# use elements for title and description, not attributes

---
+++ xmloff/source/draw/descriptionimp.hxx
@@ -0,0 +1,73 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: descriptionimp.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2006-12-19 16:33:05 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _XMLOFF_DESCRIPTIONIMP_HXX
+#define _XMLOFF_DESCRIPTIONIMP_HXX
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include "xmlictxt.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_DRAWING_XSHAPE_HPP_
+#include <com/sun/star/drawing/XShape.hpp>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// office:events inside a shape
+
+class SdXMLDescriptionContext : public SvXMLImportContext
+{
+private:
+    com::sun::star::uno::Reference< com::sun::star::drawing::XShape > mxShape;
+    ::rtl::OUString msText;
+public:
+    TYPEINFO();
+
+    SdXMLDescriptionContext( SvXMLImport& rImport,
+        sal_uInt16 nPrfx,
+        const rtl::OUString& rLocalName,
+        const com::sun::star::uno::Reference< com::sun::star::xml::sax::XAttributeList>& xAttrList,
+        const com::sun::star::uno::Reference< com::sun::star::drawing::XShape >& rxShape );
+    virtual ~SdXMLDescriptionContext();
+
+    virtual void EndElement();
+
+    // This method is called for all characters that are contained in the
+    // current element. The default is to ignore them.
+    virtual void Characters( const ::rtl::OUString& rChars );
+};
+
+#endif  //  _XMLOFF_EVENTIMP_HXX
+
