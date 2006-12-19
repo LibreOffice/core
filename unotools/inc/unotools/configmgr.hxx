c9e28e9c9712038c9bf46bae13116c9eaedcb754
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 17:34:03 +0000

    INTEGRATION: CWS native72 (1.20.50); FILE MERGED 2006/12/05 08:52:24 pb 1.20.50.1: fix: #i70859# ABOUTBOXPRODUCTVERSION added

--- unotools/inc/unotools/configmgr.hxx
+++ unotools/inc/unotools/configmgr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: configmgr.hxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: kz $ $Date: 2006-01-05 18:18:48 $
+ *  last change: $Author: ihi $ $Date: 2006-12-19 18:34:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -112,7 +112,8 @@ namespace utl
                 PRODUCTXMLFILEFORMATNAME,
                 PRODUCTXMLFILEFORMATVERSION,
                 WRITERCOMPATIBILITYVERSIONOOO11,
-                OPENSOURCECONTEXT
+                OPENSOURCECONTEXT,
+                ABOUTBOXPRODUCTVERSION
             };
             //direct readonly access to some special configuration elements
             static com::sun::star::uno::Any GetDirectConfigProperty(ConfigProperty eProp);
