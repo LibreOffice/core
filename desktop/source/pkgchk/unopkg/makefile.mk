5d67f49b21e24a06a1d1ba730b5158604f439add
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 10:45:40 +0000

    INTEGRATION: CWS jl46 (1.1.2); FILE ADDED 2006/12/06 09:28:31 jl 1.1.2.4: #i69173# bin/so directory was not created properly 2006/10/30 14:17:33 jl 1.1.2.3: #i71001# Extension Manager in unopkg has now the correct icon 2006/10/30 13:17:00 jl 1.1.2.2: #i71001# Extension Manager in unopkg has now the correct icon 2006/10/26 15:10:09 jl 1.1.2.1: #i69173# build unopkg separately because it is now a pure gui application

---
+++ desktop/source/pkgchk/unopkg/makefile.mk
@@ -0,0 +1,117 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2006-12-19 11:45:40 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..
+
+PRJNAME = desktop
+TARGET = unopkg
+TARGETTYPE = GUI
+ENABLE_EXCEPTIONS = TRUE
+NO_BSYMBOLIC = TRUE
+LIBTARGET=NO
+
+PRJINC += ..$/..$/deployment
+.INCLUDE : settings.mk
+
+.IF "$(SYSTEM_DB)" == "YES"
+CFLAGS+=-DSYSTEM_DB -I$(DB_INCLUDES)
+.ENDIF
+
+
+SHARED_OBJS = \
+        $(OBJ)$/unopkg_misc.obj \
+        $(OBJ)$/unopkg_cmdenv.obj
+
+#################################################################
+
+MYAPP1OBJS = \
+        $(SHARED_OBJS) \
+        $(OBJ)$/unopkg_app.obj
+APP1NOSAL = TRUE        
+APP1OBJS = \
+        $(MYAPP1OBJS) \
+        $(OBJ)$/lockfile.obj \
+        $(OBJ)$/dp_version.obj \
+        $(SLO)$/dp_misc.obj
+
+APP1STDLIBS = \
+        $(SALLIB) \
+        $(CPPULIB) \
+        $(CPPUHELPERLIB) \
+        $(COMPHELPERLIB) \
+        $(UCBHELPERLIB) \
+        $(UNOTOOLSLIB) \
+        $(TOOLSLIB) \
+        $(VCLLIB)
+
+APP1TARGET = so$/unopkg
+
+.IF "$(GUI)" == "WNT"
+APP1ICON=$(SOLARRESDIR)$/icons/so8-main-app.ico
+APP1LINKRES=$(MISC)$/$(TARGET)1.res
+.ENDIF # WNT
+#################################################################
+APP2NOSAL = TRUE     
+APP2OBJS = $(APP1OBJS)
+APP2STDLIBS = $(APP1STDLIBS)
+APP2TARGET = unopkg
+
+
+.IF "$(GUI)" == "WNT"
+APP2ICON=$(SOLARRESDIR)$/icons/ooo-main-app.ico
+APP2LINKRES=$(MISC)$/$(TARGET)1.res
+.ENDIF # WNT
+
+DEPOBJFILES = $(MYAPP1OBJS) \
+        $(OBJ)$/lockfile.obj \
+        $(OBJ)$/dp_version.obj \
+        $(SLO)$/dp_misc.obj
+
+.IF "$(debug)" != ""
+
+# disable MSVC inlining for debugging
+.IF "$(COM)" == "MSC"
+CFLAGS += -Ob0
+.ENDIF
+
+.ENDIF
+
+.INCLUDE : target.mk
+
+$(APP1TARGETN) : $(PRJ)$/$(BIN)$/so
+
+$(PRJ)$/$(BIN)$/so:
+    @+-$(MKDIRHIER) $(BIN)$/so >& $(NULLDEV)
+
