fbfaba071916dc9cafe9d403a96a46219d69b9ad
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 12:16:11 +0000

    INTEGRATION: CWS dr51 (1.4.302); FILE MERGED 2006/11/14 20:12:18 er 1.4.302.1: correct PRJNAME

--- sc/qa/complex/dataPilot/interfaceTests/container/makefile.mk
+++ sc/qa/complex/dataPilot/interfaceTests/container/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 18:06:59 $
+#   last change: $Author: ihi $ $Date: 2006-12-19 13:16:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ = ..$/..$/..$/..$/..
 TARGET  = DataPilotInterfaceContainer
-PRJNAME = $(TARGET)
+PRJNAME = sc
 PACKAGE = complex$/dataPilot$/interfaceTests$/container
 
 # --- Settings -----------------------------------------------------
