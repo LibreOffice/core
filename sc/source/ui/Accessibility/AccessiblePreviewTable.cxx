4631b3127df27e70fe2be5ff0d25836f2a03cc57
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 12:26:34 +0000

    INTEGRATION: CWS dr51 (1.25.86); FILE MERGED 2006/10/30 10:05:42 dr 1.25.86.1: #i68213# set correct event source

--- sc/source/ui/Accessibility/AccessiblePreviewTable.cxx
+++ sc/source/ui/Accessibility/AccessiblePreviewTable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessiblePreviewTable.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 13:07:19 $
+ *  last change: $Author: ihi $ $Date: 2006-12-19 13:26:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -129,7 +129,7 @@ void ScAccessiblePreviewTable::Notify( SfxBroadcaster& rBC, const SfxHint& rHint
         {
             AccessibleEventObject aEvent;
             aEvent.EventId = AccessibleEventId::VISIBLE_DATA_CHANGED;
-            aEvent.Source = uno::Reference< XAccessible >(this);
+            aEvent.Source = uno::Reference< XAccessibleContext >(this);
             CommitChange(aEvent);
         }
     }
