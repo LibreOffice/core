d49a2e3612282759534d398e4de6f23df8b5c031
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 17:33:12 +0000

    INTEGRATION: CWS native72 (1.5.34); FILE MERGED 2006/11/14 12:22:12 obr 1.5.34.1: #i67228# added support for relative install directories

--- setup_native/scripts/uninstall_linux.sh
+++ setup_native/scripts/uninstall_linux.sh
@@ -13,8 +13,19 @@ then
   exit 2
 fi
 
-#RPM_DB_PATH=$HOME/.RPM_DATABASE
-RPM_DB_PATH=$1/.RPM_DATABASE
+INSTALLDIR=$1
+
+# Check for old style .RPM_OFFICEDATABASE first
+if [ -d ${INSTALLDIR}/.RPM_OFFICEDATABASE ]; then
+  RPM_DB_PATH=${INSTALLDIR}/.RPM_OFFICEDATABASE
+else
+  RPM_DB_PATH=${INSTALLDIR}/.RPM_DATABASE
+fi
+
+# the RPM_DB_PATH must be absolute
+if [ ! "${RPM_DB_PATH:0:1}" = "/" ]; then
+  RPM_DB_PATH=`cd ${RPM_DB_PATH}; pwd`
+fi
 
 RPMLIST=`rpm --dbpath $RPM_DB_PATH --query --all`
 
@@ -35,10 +46,10 @@ mv -f $1/program/bootstraprc.orig $1/program/bootstraprc
 
 rpm --dbpath $RPM_DB_PATH --erase $RPMLIST || exit 2
 
-echo "Deleting directories"
+echo "Removing RPM database ..."
 rm -rf $RPM_DB_PATH
 
 echo
-echo "Deinstallation done ..."
+echo "Deinstallation done."
 
 exit 0
