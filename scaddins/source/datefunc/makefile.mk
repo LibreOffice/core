c3d824a30215250f4193e885498ea9a420cc8274
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-19 12:13:28 +0000

    INTEGRATION: CWS dr51 (1.12.26); FILE MERGED 2006/11/14 20:10:45 er 1.12.26.1: correct PRJNAME

--- scaddins/source/datefunc/makefile.mk
+++ scaddins/source/datefunc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 13:36:18 $
+#   last change: $Author: ihi $ $Date: 2006-12-19 13:13:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #**************************************************************************
 
 PRJ=..$/..
-PRJNAME=addin
+PRJNAME=scaddins
 
 TARGET=date
 
