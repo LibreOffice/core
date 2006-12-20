dd3f32fb9fd8a324f07e0738ae821cda7498eae2
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-20 13:24:31 +0000

    INTEGRATION: CWS jl49 (1.1.2); FILE ADDED 2006/11/20 12:41:04 sb 1.1.2.4: #i70481# Next step of dp_gui_updatedialog; various related improvements in other code. 2006/11/17 16:52:41 sb 1.1.2.3: #i70481# Next step of dp_gui_updatedialog; various related improvements in other code. 2006/11/17 12:59:44 sb 1.1.2.2: #i70481# Next step of dp_gui_updatedialog; various related improvements in other code. 2006/11/16 16:04:31 jl 1.1.2.1: #i70481# new files for download and installation dialog

---
+++ desktop/source/deployment/gui/dp_gui_updatedata.hxx
@@ -0,0 +1,75 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: dp_gui_updatedata.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2006-12-20 14:24:31 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#if ! defined INCLUDED_DP_GUI_UPDATEDATA_HXX
+#define INCLUDED_DP_GUI_UPDATEDATA_HXX
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+#ifndef _RTL_USTRING_HXX_
+#include "rtl/ustring.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
+#include "com/sun/star/uno/Reference.hxx"
+#endif
+
+
+namespace com { namespace sun { namespace star { namespace deployment {
+        class XPackageManager;
+        class XPackage;
+}}}}
+namespace com { namespace sun { namespace star { namespace xml { namespace dom {
+        class XNode;
+}}}}}
+
+
+namespace dp_gui {
+
+struct UpdateData
+{
+    ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > aInstalledPackage;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > aPackageManager;
+    // The content of the update information
+    ::com::sun::star::uno::Reference< ::com::sun::star::xml::dom::XNode > aUpdateInfo;
+    //The URL of the locally downloaded extension. It will only be set if there were no errors
+    //during the download
+    ::rtl::OUString sLocalURL;
+};
+}
+
+#endif
