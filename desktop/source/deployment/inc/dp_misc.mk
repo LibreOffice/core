b1684cbe489dedb5292ad0505c77798d50c83c7c
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-20 13:26:58 +0000

    INTEGRATION: CWS jl49 (1.1.2); FILE ADDED 2006/11/23 15:16:25 sb 1.1.2.2: #i70481# Introduced additional deploymentmisc shared library: fixes for wntmsci10. 2006/11/22 17:40:35 sb 1.1.2.1: #i70481# Introduced additional deploymentmisc shared library.

---
+++ desktop/source/deployment/inc/dp_misc.mk
@@ -0,0 +1,44 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: dp_misc.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2006-12-20 14:26:58 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2006 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+# To be included after settings.mk
+
+# Although the deployment shared library is a UNO component, it also exports
+# some C++ functionality:
+.IF "$(OS)" == "WNT"
+DEPLOYMENTMISCLIB = ideploymentmisc$(UPD)$(DLLPOSTFIX).lib
+.ELSE
+DEPLOYMENTMISCLIB = -ldeploymentmisc$(UPD)$(DLLPOSTFIX)
+.ENDIF
