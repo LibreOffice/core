ea983d5505605cbd055d85f557593daff3d0b291
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-20 13:25:16 +0000

    INTEGRATION: CWS jl49 (1.1.2); FILE ADDED 2006/12/07 11:04:30 jl 1.1.2.11: #70481# online update enhancements 2006/12/05 11:30:53 jl 1.1.2.10: #70481# 2006/11/30 12:50:00 jl 1.1.2.9: #i70481# dp_gui_updateinstalldialog.cxx dp_gui_updateinstalldialog.hxx dp_gui_updateinstalldialog.src 2006/11/29 15:39:54 jl 1.1.2.8: #i70481# enhanced download and installation dialog 2006/11/27 14:23:44 jl 1.1.2.7: #i70481# extended download and installation dialg 2006/11/22 10:33:51 jl 1.1.2.6: #i70481# 2006/11/21 12:18:11 jl 1.1.2.5: #i70481# 2006/11/17 16:52:42 sb 1.1.2.4: #i70481# Next step of dp_gui_updatedialog; various related improvements in other code. 2006/11/17 12:59:44 sb 1.1.2.3: #i70481# Next step of dp_gui_updatedialog; various related improvements in other code. 2006/11/17 10:47:29 jl 1.1.2.2: #i70481# using temp folder for downloads 2006/11/16 16:04:50 jl 1.1.2.1: #i70481# new files for download and installation dialog

---
+++ desktop/source/deployment/gui/dp_gui_updateinstalldialog.hxx
@@ -0,0 +1,162 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: dp_gui_updateinstalldialog.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2006-12-20 14:25:16 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_DESKTOP_SOURCE_DEPLOYMENT_GUI_DP_GUI_INSTALLDIALOG_HXX
+#define INCLUDED_DESKTOP_SOURCE_DEPLOYMENT_GUI_DP_GUI_INSTALLDIALOG_HXX
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+#ifndef _SV_BUTTON_HXX
+#include "vcl/button.hxx"
+#endif
+#ifndef _SV_FIXED_HXX
+#include "vcl/fixed.hxx"
+#endif
+#ifndef _SV_DIALOG_HXX
+#include "vcl/dialog.hxx"
+#endif
+#ifndef _PRGSBAR_HXX
+#include "svtools/prgsbar.hxx"
+#endif
+#ifndef _RTL_REF_HXX_
+#include "rtl/ref.hxx"
+#endif
+#include <vector>
+
+#include "dp_gui_autoscrolledit.hxx"
+/// @HTML
+
+namespace com { namespace sun { namespace star { namespace uno {
+    class XComponentContext;
+}}}}
+namespace com { namespace sun { namespace star { namespace xml { namespace dom {
+    class XNode;
+}}}}}
+namespace com { namespace sun { namespace star { namespace xml { namespace xpath {
+    class XXPathAPI;
+}}}}}
+
+class Window;
+class ::osl::Condition;
+
+namespace dp_gui {
+
+    struct UpdateData;
+    class UpdateCommandEnv;
+
+
+/**
+   The modal &ldquo;Download and Installation&rdquo; dialog.
+*/
+class UpdateInstallDialog: public ModalDialog {
+public:
+    /**
+       Create an instance.
+
+       @param parent
+       the parent window, may be null
+
+       @param selected
+       if true, only check for updates of extensions selected in
+       <code>dp_gui::DialogImpl</code>; if false, check for updates of all
+       deployed extensions
+    */
+    UpdateInstallDialog(Window * parent, std::vector<UpdateData> & aVecUpdateData,
+        ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > const & xCtx);
+
+    ~UpdateInstallDialog();
+
+    BOOL Close();
+    virtual short Execute();
+
+private:
+    UpdateInstallDialog(UpdateInstallDialog &); // not defined
+    void operator =(UpdateInstallDialog &); // not defined
+
+    class Thread;
+    friend class Thread;
+    friend class UpdateCommandEnv;
+
+    DECL_LINK(cancelHandler, void *);
+
+ //   DECL_LINK( startThread,  ::osl::Condition *  );
+
+    //signals in the dialog that we have finished.
+    void updateDone();
+    //Writes a particular error into the info listbox.
+    enum INSTALL_ERROR
+    {
+        ERROR_DOWNLOAD,
+        ERROR_INSTALLATION,
+        ERROR_LICENSE_DECLINED
+    };
+    void setError(INSTALL_ERROR err, ::rtl::OUString const & sExtension, ::rtl::OUString const & exceptionMessage);
+    void setError(::rtl::OUString const & exceptionMessage);
+
+    rtl::Reference< Thread > m_thread;
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > m_xComponentContext;
+    //Signals that an error occurred during download and installation
+    bool m_bError;
+    bool m_bNoEntry;
+    bool m_bActivated;
+
+    ::rtl::OUString m_sInstalling;
+    ::rtl::OUString m_sFinished;
+    ::rtl::OUString m_sNoErrors;
+    ::rtl::OUString m_sErrorDownload;
+    ::rtl::OUString m_sErrorInstallation;
+    ::rtl::OUString m_sErrorLicenseDeclined;
+    ::rtl::OUString m_sNoInstall;
+    ::rtl::OUString m_sThisErrorOccurred;
+    ::rtl::OUString m_sNoTemp;
+
+    FixedText m_ft_action;
+    ProgressBar m_statusbar;
+    FixedText m_ft_extension_name;
+    FixedText m_ft_results;
+    AutoScrollEdit m_mle_info;
+    FixedLine m_line;
+    HelpButton m_help;
+    OKButton m_ok;
+    CancelButton m_cancel;
+};
+
+
+
+
+}
+
+#endif
