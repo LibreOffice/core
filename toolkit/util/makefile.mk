374c9a1e601b4facbc92a6bf8901ac46947d3b8d
Ivo Hinkelmann <ihi@openoffice.org>
2006-12-20 12:55:07 +0000

    INTEGRATION: CWS jl49 (1.18.170); FILE MERGED 2006/12/19 09:51:26 jl 1.18.170.1: #i70481# changes for throbber

--- toolkit/util/makefile.mk
+++ toolkit/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 13:25:15 $
+#   last change: $Author: ihi $ $Date: 2006-12-20 13:55:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -94,6 +94,7 @@ SHL1STDLIBS +=\
 .ENDIF
 .ENDIF
 
+RESLIB1IMAGES=$(PRJ)$/source$/awt
 RES1FILELIST=$(SRS)$/awt.srs
 RESLIB1NAME=$(TARGET)
 RESLIB1SRSFILES=$(RES1FILELIST)
