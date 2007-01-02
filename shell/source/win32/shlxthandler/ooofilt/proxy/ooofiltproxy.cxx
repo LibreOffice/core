bf018aef38a33f977ffd1097c540ae6cb612a712
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 15:12:50 +0000

    INTEGRATION: CWS vistaready01 (1.5.22); FILE MERGED 2006/12/08 12:45:40 lla 1.5.22.1: #i72391# version check wrong

--- shell/source/win32/shlxthandler/ooofilt/proxy/ooofiltproxy.cxx
+++ shell/source/win32/shlxthandler/ooofilt/proxy/ooofiltproxy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ooofiltproxy.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 01:45:01 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 16:12:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
