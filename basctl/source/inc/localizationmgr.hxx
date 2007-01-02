974a1dfe24bafade98745145bb0fb401a147b877
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 14:52:27 +0000

    INTEGRATION: CWS ab31 (1.1.2); FILE ADDED 2006/12/18 15:40:20 ab 1.1.2.5: #i72282# Save and reset current locale before/after Basic run 2006/12/15 14:01:59 ab 1.1.2.4: #i72282# New method LocalizationMgr::handleTranslationbar() 2006/12/13 18:38:48 pb 1.1.2.3: fix: #i72282# forward SfxObjectShell 2006/12/13 11:17:17 ab 1.1.2.2: #i72282# Delete resources for deleted controls 2006/12/13 08:50:17 ab 1.1.2.1: #i72282# class LocalizationMgr

---
+++ basctl/source/inc/localizationmgr.hxx
@@ -0,0 +1,128 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: localizationmgr.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-01-02 15:52:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _LOCALIZATIONMGR_HXX
+#define _LOCALIZATIONMGR_HXX
+
+#ifndef _COM_SUN_STAR_RESOURCE_XSTRINGRESOURCEMANAGER_HPP_
+#include <com/sun/star/resource/XStringResourceManager.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMECONTAINER_HPP_
+#include <com/sun/star/container/XNameContainer.hpp>
+#endif
+
+class BasicIDEShell;
+class SfxObjectShell;
+class DlgEditor;
+
+class LocalizationMgr
+{
+    ::com::sun::star::uno::Reference
+        < ::com::sun::star::resource::XStringResourceManager >  m_xStringResourceManager;
+
+    BasicIDEShell*                                              m_pIDEShell;
+
+    SfxObjectShell*                                             m_pShell;
+    String                                                      m_aLibName;
+
+    ::com::sun::star::lang::Locale                              m_aLocaleBeforeBasicStart;
+
+    enum HandleResourceMode
+    {
+        SET_IDS,
+        RESET_IDS,
+        REMOVE_IDS_FROM_RESOURCE
+    };
+    static sal_Int32 implHandleControlResourceProperties( ::com::sun::star::uno::Any aControlAny,
+            const ::rtl::OUString& aDialogName, const ::rtl::OUString& aCtrlName,
+            ::com::sun::star::uno::Reference< ::com::sun::star::resource::XStringResourceManager >
+            xStringResourceManager, HandleResourceMode eMode );
+
+    void LocalizationMgr::enableResourceForAllLibraryDialogs( void )
+    {
+        implEnableDisableResourceForAllLibraryDialogs( SET_IDS );
+    }
+    void LocalizationMgr::disableResourceForAllLibraryDialogs( void )
+    {
+        implEnableDisableResourceForAllLibraryDialogs( RESET_IDS );
+    }
+    void implEnableDisableResourceForAllLibraryDialogs( HandleResourceMode eMode );
+
+public:
+    LocalizationMgr( BasicIDEShell* pIDEShell, SfxObjectShell* pShell, String aLibName,
+        const ::com::sun::star::uno::Reference
+            < ::com::sun::star::resource::XStringResourceManager >& xStringResourceManager );
+    ::com::sun::star::uno::Reference
+        < ::com::sun::star::resource::XStringResourceManager >getStringResourceManager( void )
+    {
+        return m_xStringResourceManager;
+    }
+
+    bool isLibraryLocalized( void );
+
+    void handleTranslationbar( void );
+
+    void handleAddLocales( ::com::sun::star::uno::Sequence
+        < ::com::sun::star::lang::Locale > aLocaleSeq );
+
+    void handleRemoveLocales( ::com::sun::star::uno::Sequence
+        < ::com::sun::star::lang::Locale > aLocaleSeq );
+
+    void handleSetDefaultLocale( ::com::sun::star::lang::Locale aLocale );
+
+    void handleSetCurrentLocale( ::com::sun::star::lang::Locale aLocale );
+
+    void handleBasicStarted( void );
+
+    void handleBasicStopped( void );
+
+    static void setControlResourceIDsForNewEditorObject( DlgEditor* pEditor,
+        ::com::sun::star::uno::Any aControlAny, const ::rtl::OUString& aCtrlName );
+
+    static void deleteControlResourceIDsForDeletedEditorObject( DlgEditor* pEditor,
+        ::com::sun::star::uno::Any aControlAny, const ::rtl::OUString& aCtrlName );
+
+    static void setStringResourceAtDialog( SfxObjectShell* pShell, const String& aLibName, const String& aDlgName,
+        ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameContainer > xDialogModel );
+
+    static void removeResourceForDialog( SfxObjectShell* pShell, const String& aLibName, const String& aDlgName,
+        ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameContainer > xDialogModel );
+
+    static ::com::sun::star::uno::Reference< ::com::sun::star::resource::XStringResourceManager >
+        getStringResourceFromDialogLibrary( ::com::sun::star::uno::Reference
+            < ::com::sun::star::container::XNameContainer > xDialogLib );
+};
+
+#endif
