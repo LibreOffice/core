7a698b2958fb7bf6dae51be7a4ab4c41dd12cebd
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 15:49:39 +0000

    INTEGRATION: CWS smarttags (1.75.82); FILE MERGED 2006/12/21 10:43:05 jakob_lechner 1.75.82.1: #i72705#: support for smarttags

--- sw/source/core/layout/wsfrm.cxx
+++ sw/source/core/layout/wsfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wsfrm.cxx,v $
  *
- *  $Revision: 1.75 $
+ *  $Revision: 1.76 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-13 12:20:12 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 16:49:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -940,6 +940,7 @@ void SwCntntFrm::Paste( SwFrm* pParent, SwFrm* pSibling)
     if( pPage )
     {
         pPage->InvalidateSpelling();
+        pPage->InvalidateSmartTags();   // SMARTTAGS
         pPage->InvalidateAutoCompleteWords();
         pPage->InvalidateWordCount();
     }
