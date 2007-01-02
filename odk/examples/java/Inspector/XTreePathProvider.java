2bcf2ac1a0830629b4caca118d5ee304c4ee2fe7
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 14:04:35 +0000

    INTEGRATION: CWS sdkinspector (1.1.2); FILE ADDED 2006/12/21 10:16:27 bc 1.1.2.1: #i69688# adding interfaces

---
+++ odk/examples/java/Inspector/XTreePathProvider.java
@@ -0,0 +1,26 @@
+/*
+ * xTreePathProvider.java
+ *
+ * Created on 12. Dezember 2006, 13:30
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bc93774
+ */
+public interface XTreePathProvider {
+
+    public UnoNode getLastPathComponent();
+
+    public UnoNode getPathComponent(int i);
+
+    public int getPathCount();
+
+    public XTreePathProvider getParentPath();
+
+    public XTreePathProvider pathByAddingChild(UnoNode _oUnoNode);
+
+}
