640116f7d5c9314c23f962cacae91d54df1a617e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 15:52:29 +0000

    INTEGRATION: CWS smarttags (1.9.514); FILE MERGED 2006/12/21 10:43:09 jakob_lechner 1.9.514.1: #i72705#: support for smarttags

--- sw/source/ui/makefile.mk
+++ sw/source/ui/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: obo $ $Date: 2005-11-16 13:54:09 $
+#   last change: $Author: hr $ $Date: 2007-01-02 16:52:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -78,7 +78,8 @@ SUBLIBS2= \
         $(SLB)$/misc.lib     \
         $(SLB)$/ribbar.lib   \
         $(SLB)$/shells.lib   \
-        $(SLB)$/table.lib    \
+                $(SLB)$/smartmenu.lib \
+                $(SLB)$/table.lib    \
         $(SLB)$/unoidl.lib   \
         $(SLB)$/utlui.lib    \
         $(SLB)$/web.lib   \
