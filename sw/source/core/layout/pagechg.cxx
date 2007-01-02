c1df0ef304f1d6e952128434bc0a3bd7ad2aefa8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 15:49:26 +0000

    INTEGRATION: CWS smarttags (1.42.110); FILE MERGED 2006/12/21 10:43:05 jakob_lechner 1.42.110.1: #i72705#: support for smarttags

--- sw/source/core/layout/pagechg.cxx
+++ sw/source/core/layout/pagechg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pagechg.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:22:08 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 16:49:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -243,7 +243,7 @@ SwPageFrm::SwPageFrm( SwFrmFmt *pFmt, SwPageDesc *pPgDsc ) :
     SetMaxFtnHeight( pPgDsc->GetFtnInfo().GetHeight() ?
                      pPgDsc->GetFtnInfo().GetHeight() : LONG_MAX ),
     nType = FRMC_PAGE;
-    bInvalidLayout = bInvalidCntnt = bInvalidSpelling = bInvalidAutoCmplWrds = bInvalidWordCount = TRUE;
+    bInvalidLayout = bInvalidCntnt = bInvalidSpelling = bInvalidSmartTags = bInvalidAutoCmplWrds = bInvalidWordCount = TRUE;
     bInvalidFlyLayout = bInvalidFlyCntnt = bInvalidFlyInCnt = bFtnPage = bEndNotePage = FALSE;
 
     const bool bBrowseMode = pFmt->getIDocumentSettingAccess()->get(IDocumentSettingAccess::BROWSE_MODE);
