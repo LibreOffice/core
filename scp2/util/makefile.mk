100d63096cd6477ca9b550417af97d9dbd92e338
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 14:32:01 +0000

    INTEGRATION: CWS native50 (1.32.108); FILE MERGED 2006/11/02 12:14:23 is 1.32.108.6: RESYNC: (1.34-1.35); FILE MERGED 2006/08/10 13:39:27 is 1.32.108.5: RESYNC: (1.33-1.34); FILE MERGED 2006/08/10 10:34:47 is 1.32.108.4: #i65425# new java installer 2006/08/07 10:11:10 is 1.32.108.3: #i65425# new java installer 2006/07/21 11:27:39 is 1.32.108.2: RESYNC: (1.32-1.33); FILE MERGED 2006/06/13 09:54:40 is 1.32.108.1: #i65425# shifting file info from solenv to scp

--- scp2/util/makefile.mk
+++ scp2/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.35 $
+#   $Revision: 1.36 $
 #
-#   last change: $Author: hr $ $Date: 2006-10-24 15:35:28 $
+#   last change: $Author: hr $ $Date: 2007-01-02 15:32:01 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,6 +62,8 @@ SCP1FILES  = installation_ooo.par          \
              shortcut_ooo.par              \
              module_ooo.par                \
              module_hidden_ooo.par         \
+             module_langpack.par           \
+             module_java.par               \
              profile_ooo.par               \
              profileitem_ooo.par           \
              module_writer.par             \
@@ -77,6 +79,7 @@ SCP1FILES  = installation_ooo.par          \
              canvascommons.par             \
              module_math.par               \
              file_math.par                 \
+             module_systemint.par          \
              module_graphicfilter.par      \
              file_graphicfilter.par        \
              module_testtool.par           \
@@ -185,6 +188,8 @@ SCP2FILES  = installation_ooo.par          \
              shortcut_ooo.par              \
              module_ooo.par                \
              module_hidden_ooo.par         \
+             module_langpack.par           \
+             module_java.par               \
              profile_ooo.par               \
              profileitem_ooo.par           \
              module_writer.par             \
@@ -200,6 +205,7 @@ SCP2FILES  = installation_ooo.par          \
              canvascommons.par             \
              module_math.par               \
              file_math.par                 \
+             module_systemint.par          \
              module_graphicfilter.par      \
              file_graphicfilter.par        \
              module_testtool.par           \
