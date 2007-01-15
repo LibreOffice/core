8fcf2d9dcbb6b4a5373e8a5ec136966a5cae6690
Vladimir Glazounov <vg@openoffice.org>
2007-01-15 13:31:26 +0000

    INTEGRATION: CWS dba22b (1.19.56); FILE MERGED 2006/12/13 10:05:18 fs 1.19.56.1: #i72527# proper base class initialization

--- dbaccess/source/core/dataaccess/commanddefinition.cxx
+++ dbaccess/source/core/dataaccess/commanddefinition.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: commanddefinition.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 06:38:46 $
+ *  last change: $Author: vg $ $Date: 2007-01-15 14:31:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,7 +130,6 @@ OCommandDefinition::OCommandDefinition( const Reference< XInterface >& _rxContai
     :OComponentDefinition(_rxContainer,_rElementName,_xORB,_pImpl,sal_False)
 {
     DBG_CTOR(OCommandDefinition, NULL);
-
     registerProperties();
 }
 
