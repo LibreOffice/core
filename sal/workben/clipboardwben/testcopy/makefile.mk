7e35ac0df74e37b97a28a9235249562b9ec171f4
Vladimir Glazounov <vg@openoffice.org>
2007-01-16 15:06:56 +0000

    INTEGRATION: CWS lfs (1.2.184); FILE MERGED 2006/11/30 16:32:00 rene 1.2.184.1: add LFS_CFLAGS only for sal

--- sal/workben/clipboardwben/testcopy/makefile.mk
+++ sal/workben/clipboardwben/testcopy/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 16:56:27 $
+#   last change: $Author: vg $ $Date: 2007-01-16 16:06:16 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,6 +44,9 @@ LIBTARGET=NO
 .INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
 .INCLUDE :  sv.mk
+
+CFLAGS+= $(LFS_CFLAGS)
+CXXFLAGS+= $(LFS_CFLAGS)
 
 # --- Files --------------------------------------------------------
 
