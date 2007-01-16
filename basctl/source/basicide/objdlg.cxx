7ac4dffa7cb101b06bafc76ca3f91e1542941a7d
Vladimir Glazounov <vg@openoffice.org>
2007-01-16 15:33:22 +0000

    INTEGRATION: CWS ab33 (1.17.42); FILE MERGED 2007/01/10 16:34:48 ab 1.17.42.1: #i69280# Removed warnings

--- basctl/source/basicide/objdlg.cxx
+++ basctl/source/basicide/objdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objdlg.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:29:44 $
+ *  last change: $Author: vg $ $Date: 2007-01-16 16:33:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -73,7 +73,7 @@ ObjectTreeListBox::~ObjectTreeListBox()
 {
 }
 
-void ObjectTreeListBox::Command( const CommandEvent& rCEvt )
+void ObjectTreeListBox::Command( const CommandEvent& )
 {
 }
 
