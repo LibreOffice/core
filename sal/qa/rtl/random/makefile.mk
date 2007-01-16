4befbf4a5548e9dfd86ed33dd04eb015661d275e
Vladimir Glazounov <vg@openoffice.org>
2007-01-16 15:01:34 +0000

    INTEGRATION: CWS lfs (1.3.184); FILE MERGED 2006/11/30 16:31:54 rene 1.3.184.1: add LFS_CFLAGS only for sal

--- sal/qa/rtl/random/makefile.mk
+++ sal/qa/rtl/random/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 15:49:44 $
+#   last change: $Author: vg $ $Date: 2007-01-16 16:01:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,6 +44,9 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+
+CFLAGS+= $(LFS_CFLAGS)
+CXXFLAGS+= $(LFS_CFLAGS)
 
 # BEGIN ----------------------------------------------------------------
 # auto generated Target:job by codegen.pl 
