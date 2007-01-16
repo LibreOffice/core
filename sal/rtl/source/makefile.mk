17321a8ddc59274f360a7cfb580d6a3c534f2e3e
Vladimir Glazounov <vg@openoffice.org>
2007-01-16 15:03:17 +0000

    INTEGRATION: CWS lfs (1.25.32); FILE MERGED 2006/11/30 16:31:56 rene 1.25.32.1: add LFS_CFLAGS only for sal

--- sal/rtl/source/makefile.mk
+++ sal/rtl/source/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.26 $
+#   $Revision: 1.27 $
 #
-#   last change: $Author: kz $ $Date: 2006-10-05 10:45:36 $
+#   last change: $Author: vg $ $Date: 2007-01-16 16:03:17 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,6 +51,9 @@ PROJECTPCHSOURCE=cont_pch
 .IF "$(ALLOC)" == "SYS_ALLOC" || "$(ALLOC)" == "TCMALLOC"
 CDEFS+= -DFORCE_SYSALLOC
 .ENDIF
+
+CFLAGS+= $(LFS_CFLAGS)
+CXXFLAGS+= $(LFS_CFLAGS)
 
 # --- Files --------------------------------------------------------
 
