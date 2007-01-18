ff0f36676a88d7a5483f1d8ec8344d4402bdb494
Vladimir Glazounov <vg@openoffice.org>
2007-01-18 13:44:21 +0000

    INTEGRATION: CWS sb66 (1.2.6); FILE MERGED 2007/01/05 10:28:53 obr 1.2.6.1: #i73160# enable background color property

--- toolkit/source/controls/tkthrobber.cxx
+++ toolkit/source/controls/tkthrobber.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tkthrobber.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-12-20 13:53:37 $
+ *  last change: $Author: vg $ $Date: 2007-01-18 14:44:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -71,6 +71,7 @@ namespace toolkit
     UnoThrobberControlModel::UnoThrobberControlModel()
     {
         ImplRegisterProperty( BASEPROPERTY_DEFAULTCONTROL );
+        ImplRegisterProperty( BASEPROPERTY_BACKGROUNDCOLOR );
     }
 
     //--------------------------------------------------------------------
