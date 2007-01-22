5c6815d398fa9dd591c51b5b6261970cff7c90fe
Oliver Bolte <obo@openoffice.org>
2007-01-22 12:19:34 +0000

    INTEGRATION: CWS dr52 (1.8.132); FILE MERGED 2007/01/05 16:06:26 dr 1.8.132.1: #i51348# load drawing object names

--- sc/source/filter/inc/fapihelper.hxx
+++ sc/source/filter/inc/fapihelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fapihelper.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:51:44 $
+ *  last change: $Author: obo $ $Date: 2007-01-22 13:19:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -183,6 +183,8 @@ public:
 
     /** Returns true, if the contained XPropertySet interface is valid. */
     inline bool         Is() const { return mxPropSet.is(); }
+    /** Returns the contained XPropertySet interface. */
+    inline XPropertySetRef GetApiPropertySet() const { return mxPropSet; }
 
     // Get properties ---------------------------------------------------------
 
