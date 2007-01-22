190358860b8350cc230f3ac6092bc9f7ccc7b4a3
Oliver Bolte <obo@openoffice.org>
2007-01-22 12:19:21 +0000

    INTEGRATION: CWS dr52 (1.5.132); FILE MERGED 2006/12/21 12:45:01 dr 1.5.132.1: merge dumper changes from CWS chart2mst3

--- sc/source/filter/ftools/makefile.mk
+++ sc/source/filter/ftools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: obo $ $Date: 2006-07-10 13:48:36 $
+#   last change: $Author: obo $ $Date: 2007-01-22 13:19:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,6 +56,7 @@ PROJECTPCHSOURCE=..\pch\filt_pch
 SLOFILES =	\
         $(SLO)$/fapihelper.obj				\
         $(SLO)$/fdumper.obj					\
+        $(SLO)$/fdumperdff.obj				\
         $(SLO)$/fdumperole.obj				\
         $(SLO)$/fprogressbar.obj			\
         $(SLO)$/ftools.obj
