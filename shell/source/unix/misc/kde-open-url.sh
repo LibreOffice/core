8abbbc9e39aeb21eccbf8b0336994c1b845906fa
Oliver Bolte <obo@openoffice.org>
2007-01-23 11:29:06 +0000

    INTEGRATION: CWS obr04 (1.2.254); FILE MERGED 2006/12/20 15:03:25 obr 1.2.254.1: #i72544# patch applied

--- shell/source/unix/misc/kde-open-url.sh
+++ shell/source/unix/misc/kde-open-url.sh
@@ -51,9 +51,9 @@ esac
 
 # special handling for mailto: uris
 if echo $1 | grep '^mailto:' > /dev/null; then
-  kmailservice $1 &
+  kmailservice "$1" &
 else
-  kfmclient openURL $1 &
+  kfmclient openURL "$1" &
 fi
 
 exit 0
