9f9edc638352713a18b34067cbdf97ea8a3dad28
Oliver Bolte <obo@openoffice.org>
2007-01-23 11:12:45 +0000

    INTEGRATION: CWS fs08 (1.2.38); FILE MERGED 2006/12/18 09:27:17 fridrich_strba 1.2.38.1: convert writerperfect into a framework where converter libraries based on libwpd's api can simply plug themselves

--- writerperfect/source/filter/DocumentHandler.hxx
+++ writerperfect/source/filter/DocumentHandler.hxx
@@ -1,42 +1,27 @@
-/*
- * Copyright (C) 2004 William Lachance (wlach@interlog.com)
- * Copyright (C) 2004 Net Integration Technologies (http://www.net-itech.com)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- *
- *  Contributor(s): Martin Gallwey (gallwey@sun.com)
- *
- */
+#ifndef _OODOCUMENTHANDLER_HXX_
+#define _OODOCUMENTHANDLER_HXX_
 
-/* "This product is not manufactured, approved, or supported by
- * Corel Corporation or Corel Corporation Limited."
- */
-#ifndef _DOCUMENTHANDLER_H
-#define _DOCUMENTHANDLER_H
+#ifndef _COM_SUN_STAR_XML_SAX_XDOCUMENTHANDLER_HPP_
+#include <com/sun/star/xml/sax/XDocumentHandler.hpp>
+#endif
+
 #include <libwpd/libwpd.h>
-#include <libwpd/WPXProperty.h>
-#include <libwpd/WPXString.h>
+
+using com::sun::star::uno::Reference;
+using com::sun::star::xml::sax::XDocumentHandler;
 
 class DocumentHandler
 {
 public:
-        virtual void startDocument() = 0;
-        virtual void endDocument() = 0;
-        virtual void startElement(const char *psName, const WPXPropertyList &xPropList) = 0;
-        virtual void endElement(const char *psName) = 0;
-        virtual void characters(const WPXString &sCharacters) = 0;
+        DocumentHandler(Reference < XDocumentHandler > &xHandler);
+        void startDocument();
+        void endDocument();
+        void startElement(const char *psName, const WPXPropertyList &xPropList);
+        void endElement(const char *psName);
+        void characters(const WPXString &sCharacters);
+
+private:
+        Reference < XDocumentHandler > mxHandler;
 };
-#endif
+
+#endif // _OODOCUMENTHANDLER_HXX_
