153d4a912bb97f8032a9ff9a2b5ea04ecf10820e
Oliver Bolte <obo@openoffice.org>
2007-01-23 06:08:05 +0000

    INTEGRATION: CWS fwk58 (1.17.510); FILE MERGED 2007/01/15 16:05:08 pl 1.17.510.1: #i72812# lazy deletion for Menu and Window

--- vcl/source/helper/makefile.mk
+++ vcl/source/helper/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 12:16:23 $
+#   last change: $Author: obo $ $Date: 2007-01-23 07:08:05 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,8 @@ SLOFILES=\
     $(SLO)$/canvastools.obj		\
     $(SLO)$/xconnection.obj		\
     $(SLO)$/threadex.obj		\
-    $(SLO)$/smartid.obj
+    $(SLO)$/smartid.obj         \
+    $(SLO)$/lazydelete.obj
 
 # --- Targets ------------------------------------------------------
 
