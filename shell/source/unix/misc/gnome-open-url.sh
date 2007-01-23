e70332e4f60e1498d5fe8dca7a8a4a40b3ecf9bb
Oliver Bolte <obo@openoffice.org>
2007-01-23 11:28:56 +0000

    INTEGRATION: CWS obr04 (1.3.166); FILE MERGED 2006/12/20 15:03:25 obr 1.3.166.1: #i72544# patch applied

--- shell/source/unix/misc/gnome-open-url.sh
+++ shell/source/unix/misc/gnome-open-url.sh
@@ -50,6 +50,6 @@ case `uname -s` in
 esac
 
 # use gnome-open utility coming with libgnome if available
-gnome-open $1 2>/dev/null || "$0.bin" $1
+gnome-open "$1" 2>/dev/null || "$0.bin" $1
 
 exit 0
