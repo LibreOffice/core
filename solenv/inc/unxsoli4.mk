25c70c3aedb8373095f192c366b42df1504c9f15
Oliver Bolte <obo@openoffice.org>
2007-01-23 11:09:24 +0000

    INTEGRATION: CWS fs08 (1.26.18); FILE MERGED 2007/01/03 18:13:29 fridrich_strba 1.26.18.2: RESYNC: (1.26-1.27); FILE MERGED 2006/12/19 12:12:29 fridrich_strba 1.26.18.1: remove writerperfect from the modules with warnings since this CWS tries to remove all warnings :-)

--- solenv/inc/unxsoli4.mk
+++ solenv/inc/unxsoli4.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxsoli4.mk,v $
 #
-#   $Revision: 1.28 $
+#   $Revision: 1.29 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 16:25:42 $
+#   last change: $Author: obo $ $Date: 2007-01-23 12:09:24 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -119,7 +119,6 @@ MODULES_WITH_WARNINGS := \
     soldep \
     starmath \
     sw \
-    writerperfect \
     xmlsecurity
 
 STDOBJVCL=$(L)$/salmain.o
