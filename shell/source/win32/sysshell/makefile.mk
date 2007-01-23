75a792411d98d63268b4d93781b7b72edbb15666
Oliver Bolte <obo@openoffice.org>
2007-01-23 11:29:40 +0000

    INTEGRATION: CWS obr04 (1.2.126); FILE MERGED 2006/12/22 07:37:34 obr 1.2.126.1: PRJNAME fixed

--- shell/source/win32/sysshell/makefile.mk
+++ shell/source/win32/sysshell/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 20:10:52 $
+#   last change: $Author: obo $ $Date: 2007-01-23 12:29:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ=..$/..$/..
 
-PRJNAME=SHELL
+PRJNAME=shell
 TARGET=sysshell
 
 # --- Settings -----------------------------------------------------
