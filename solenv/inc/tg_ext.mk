a9780c7513efe7c03d5da9e06b6c4e54293dcfc9
Oliver Bolte <obo@openoffice.org>
2007-01-25 11:54:34 +0000

    INTEGRATION: CWS ause069 (1.72.2); FILE MERGED 2007/01/24 12:40:51 hjs 1.72.2.4: RESYNC: (1.72-1.73); FILE MERGED 2006/12/01 17:42:39 hjs 1.72.2.3: #i70359# remove forced shell calls 2006/10/18 17:13:00 hjs 1.72.2.2: #i70359# remove forced shell calls 2006/10/11 12:36:25 hjs 1.72.2.1: #i70359#: removing + sign in targets; adding to tool macro if required

--- solenv/inc/tg_ext.mk
+++ solenv/inc/tg_ext.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: tg_ext.mk,v $
 #
-#   $Revision: 1.73 $
+#   $Revision: 1.74 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-23 06:34:24 $
+#   last change: $Author: obo $ $Date: 2007-01-25 12:54:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -38,9 +38,9 @@
 # setup INCLUDE variable for use by VC++
 .IF "$(GUI)$(COM)"=="WNTMSC"
 .IF "$(EXT_USE_STLPORT)"==""
-INCLUDE!:=$(shell +$(WRAPCMD) echo $(subst,$/stl, $(SOLARINC)))
+INCLUDE!:=$(shell $(4nt_force_shell)$(WRAPCMD) echo $(subst,$/stl, $(SOLARINC)))
 .ELSE
-INCLUDE!:=$(shell +$(WRAPCMD) echo $(SOLARINC))
+INCLUDE!:=$(shell $(4nt_force_shell)$(WRAPCMD) echo $(SOLARINC))
 .ENDIF			# "$(EXT_USE_STLPORT)"==""
 INCLUDE!:=$(INCLUDE:s/ -I/;/)
 .EXPORT : INCLUDE
@@ -61,9 +61,9 @@ BACK_PATH=..$/..$/..$/
 # Remove entire package from output directory, for example, if new patches are
 # to be applied.
 .IF "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
-    REMOVE_PACKAGE_COMMAND=+-$(RM) -r $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) >& $(NULLDEV)
+    REMOVE_PACKAGE_COMMAND=-$(RM) -r $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) >& $(NULLDEV)
 .ELSE			# "$(GUI)"=="WNT"
-    REMOVE_PACKAGE_COMMAND=+-$(RM) /s $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) >& $(NULLDEV)
+    REMOVE_PACKAGE_COMMAND=-$(RM) /s $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) >& $(NULLDEV)
 .ENDIF			# "$(GUI)"=="WNT"
 
 P_CONFIGURE_DIR=$(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$(CONFIGURE_DIR)
@@ -105,76 +105,76 @@ ALLTAR : \
     $(PACKAGE_DIR)$/$(PREDELIVER_FLAG_FILE)
 
 clean:
-    +cd $(P_BUILD_DIR) && $(BUILD_ACTION) $(BUILD_FLAGS) clean
-    +$(RM) $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
+    cd $(P_BUILD_DIR) && $(BUILD_ACTION) $(BUILD_FLAGS) clean
+    $(RM) $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar.Z
-    @+-$(RM) $@
+    @-$(RM) $@
 .IF "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := sh -c "uncompress -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.Z | tar $(TAR_EXCLUDE_SWITCH) -xvf - ")
 .ELSE			# "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := uncompress -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.Z | tar $(TAR_EXCLUDE_SWITCH) -xvf - )
 .ENDIF			# "$(GUI)"=="UNX"
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar.gz
-    @+-$(RM) $@
+    @-$(RM) $@
 .IF "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := sh -c "gunzip -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.gz $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - ")
 .ELSE			# "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := gunzip -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.gz $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - )
 .ENDIF			# "$(GUI)"=="UNX"
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar.bz2
-    @+-$(RM) $@
+    @-$(RM) $@
 .IF "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := sh -c "bunzip2 -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.bz2 $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - ")
 .ELSE			# "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := bunzip2 -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.bz2 $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - )
 .ENDIF			# "$(GUI)"=="UNX"
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.tgz
-    @+-$(RM) $@
+    @-$(RM) $@
 .IF "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := sh -c "gunzip -c $(BACK_PATH)download$/$(TARFILE_NAME).tgz $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - ")
 .ELSE			# "$(GUI)"=="UNX"
     @noop $(assign UNPACKCMD := gunzip -c $(BACK_PATH)download$/$(TARFILE_NAME).tgz $(TARFILE_FILTER) | tar $(TAR_EXCLUDE_SWITCH) -xvf - )
 .ENDIF			# "$(GUI)"=="UNX"
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar
-    @+-$(RM) $@
+    @-$(RM) $@
     noop $(assign UNPACKCMD := tar $(TAR_EXCLUDE_SWITCH) -xvf $(BACK_PATH)download$/$(TARFILE_NAME).tar)
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 $(MISC)$/%.unpack : $(PRJ)$/download$/%.zip
-    @+-$(RM) $@
+    @-$(RM) $@
     noop $(assign UNPACKCMD := unzip $(BACK_PATH)download$/$(TARFILE_NAME).zip)
-    @+$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
-    @+$(RENAME) $@.$(INPATH) $@
+    @$(TYPE) $(mktmp $(UNPACKCMD)) > $@.$(INPATH)
+    @$(RENAME) $@.$(INPATH) $@
 
 #do unpack
 $(PACKAGE_DIR)$/$(UNTAR_FLAG_FILE) : $(PRJ)$/$(ROUT)$/misc$/$(TARFILE_NAME).unpack $(PATCH_FILE_DEP)
-    +$(IFEXIST) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) $(THEN) $(RENAME) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)_removeme $(FI)
-    +-rm -rf $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)_removeme
+    $(IFEXIST) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) $(THEN) $(RENAME:s/+//) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)_removeme $(FI)
+    -rm -rf $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)_removeme
     @-$(MKDIR) $(PACKAGE_DIR:d)
     @-$(MKDIR) $(PACKAGE_DIR)
-    +cd $(PACKAGE_DIR) && ( $(shell +$(TYPE) $(PRJ)$/$(ROUT)$/misc$/$(TARFILE_NAME).unpack)) && $(TOUCH) $(UNTAR_FLAG_FILE)
+    cd $(PACKAGE_DIR) && ( $(shell $(TYPE) $(PRJ)$/$(ROUT)$/misc$/$(TARFILE_NAME).unpack)) && $(TOUCH) $(UNTAR_FLAG_FILE)
     @echo make writeable...
 .IF "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
-    @+cd $(PACKAGE_DIR) && chmod -R +rw $(TARFILE_ROOTDIR) && $(TOUCH) $(UNTAR_FLAG_FILE)
-    @+cd $(PACKAGE_DIR) && find $(TARFILE_ROOTDIR) -type d -exec chmod a+x {{}} \;
+    @cd $(PACKAGE_DIR) && chmod -R +rw $(TARFILE_ROOTDIR) && $(TOUCH) $(UNTAR_FLAG_FILE)
+    @cd $(PACKAGE_DIR) && find $(TARFILE_ROOTDIR) -type d -exec chmod a+x {{}} \;
 .ELSE			# "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
 # Native W32 tools generate only filedates with even seconds, cygwin also with odd seconds
-    +$(DELAY) 2
-    @+cd $(PACKAGE_DIR) && attrib /s -r $(TARFILE_ROOTDIR) >& $(NULLDEV) && $(TOUCH) $(UNTAR_FLAG_FILE)
+    $(DELAY) 2
+    @cd $(PACKAGE_DIR) && attrib /s -r $(TARFILE_ROOTDIR) >& $(NULLDEV) && $(TOUCH) $(UNTAR_FLAG_FILE)
 .ENDIF			# "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
 
 #add new files to patch
@@ -188,41 +188,41 @@ $(PACKAGE_DIR)$/$(ADD_FILES_FLAG_FILE) : $(PACKAGE_DIR)$/$(UNTAR_FLAG_FILE) $(T_
 #patch
 $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE) : $(PACKAGE_DIR)$/$(ADD_FILES_FLAG_FILE)
 .IF "$(PATCH_FILE_NAME)"=="none" ||	"$(PATCH_FILE_NAME)"==""
-    +cd $(PACKAGE_DIR) && echo no patch needed...
-    +$(TOUCH) $@
+    echo no patch needed...
+    $(TOUCH) $@
 .ELSE			# "$(PATCH_FILE_NAME)"=="none" ||	"$(PATCH_FILE_NAME)"==""
 .IF "$(GUI)"=="WNT"
 # hack to make 4nt version 4,01 work and still get propper
 # errorcodes for versions < 3,00
 .IF "$(my4ver:s/.//:s/,//)" >= "300"
-    +cd $(PACKAGE_DIR) && ( $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | tr -d "\015" | patch $(PATCHFLAGS) -p2 ) && $(TOUCH) $(PATCH_FLAG_FILE)
+    cd $(PACKAGE_DIR) && ( $(TYPE:s/+//) $(BACK_PATH)$(PATCH_FILE_NAME) | tr -d "\015" | patch $(PATCHFLAGS) -p2 ) && $(TOUCH) $(PATCH_FLAG_FILE)
 .ELSE			# "$(my4ver:s/.//:s/,//)" >= "300"
-    +cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | tr -d "\015" | patch $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
+    cd $(PACKAGE_DIR) && $(TYPE:s/+//) $(BACK_PATH)$(PATCH_FILE_NAME) | tr -d "\015" | patch $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
 .ENDIF			# "$(my4ver:s/.//:s/,//)" >= "300"
 .ELSE           # "$(GUI)"=="WNT"
 .IF "$(BSCLIENT)"=="TRUE"
-    +cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | $(GNUPATCH) -f $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
+    cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | $(GNUPATCH) -f $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
 .ELSE           # "$(BSCLIENT)"!=""
-    +cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | $(GNUPATCH) $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
+    cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | $(GNUPATCH) $(PATCHFLAGS) -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
 .ENDIF          # "$(BSCLIENT)"!=""
 .ENDIF          # "$(GUI)"=="WNT"
 .ENDIF			# "$(PATCH_FILE_NAME)"=="none" ||	"$(PATCH_FILE_NAME)"==""
 .IF "$(T_ADDITIONAL_FILES)"!=""
 .IF "$(GUI)"=="WNT"
 # Native W32 tools generate only filedates with even seconds, cygwin also with odd seconds
-    +$(DELAY) 2
+    $(DELAY) 2
 .ENDIF # "$(GUI)"=="WNT"
-    +$(TOUCH) $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE)
+    $(TOUCH) $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE)
 .ENDIF          # "$(T_ADDITIONAL_FILES)"!=""
 
 .IF "$(CONVERTFILES)"!=""
 $(MISC)$/convert_unx_flag :  $(PACKAGE_DIR)$/$(UNTAR_FLAG_FILE)
-    +$(CONVERT) unix $(foreach,i,$(CONVERTFILES) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) && $(TOUCH) $(MISC)$/convert_unx_flag
+    $(CONVERT) unix $(foreach,i,$(CONVERTFILES) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) && $(TOUCH) $(MISC)$/convert_unx_flag
 
 $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE) : $(MISC)$/convert_unx_flag
 
 $(MISC)$/convert_dos_flag : $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE)
-    +$(CONVERT) dos  $(foreach,i,$(CONVERTFILES) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) && $(TOUCH) $(MISC)$/convert_dos_flag
+    $(CONVERT) dos  $(foreach,i,$(CONVERTFILES) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) && $(TOUCH) $(MISC)$/convert_dos_flag
 
 $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE) : $(MISC)$/convert_dos_flag
 
@@ -231,95 +231,95 @@ patch : $(MISC)$/convert_dos_flag
 .ENDIF          # "$(CONVERTFILES)"!=""
 
 $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE) : $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE)
-    @+-$(RM) $@ >& $(NULLDEV)
+    @-$(RM) $@ >& $(NULLDEV)
 .IF "$(CONFIGURE_ACTION)" == "none" || "$(CONFIGURE_ACTION)"==""
-    +$(TOUCH) $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE)
+    $(TOUCH) $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE)
 .ELSE			# "$(CONFIGURE_ACTION)"=="none" || "$(CONFIGURE_ACTION)"==""
     -$(MKDIR) $(P_CONFIGURE_DIR)
-    +cd $(P_CONFIGURE_DIR) && $(CONFIGURE_ACTION) $(CONFIGURE_FLAGS) && $(TOUCH) $(CONFIGURE_FLAG_FILE)
-    +mv $(P_CONFIGURE_DIR)$/$(CONFIGURE_FLAG_FILE) $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE)
+    cd $(P_CONFIGURE_DIR) && $(CONFIGURE_ACTION) $(CONFIGURE_FLAGS) && $(TOUCH) $(CONFIGURE_FLAG_FILE)
+    mv $(P_CONFIGURE_DIR)$/$(CONFIGURE_FLAG_FILE) $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE)
 .ENDIF			# "$(CONFIGURE_ACTION)"=="none" ||	"$(CONFIGURE_ACTION)"==""
 
 
 $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE) : $(PACKAGE_DIR)$/$(CONFIGURE_FLAG_FILE)
-    @+-$(RM) $@ >& $(NULLDEV)
+    @-$(RM) $@ >& $(NULLDEV)
 .IF "$(eq,x$(BUILD_ACTION:s/none//)x,xx true false)"=="true"
-    +$(TOUCH) $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
+    $(TOUCH) $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
 .ELSE			# "$(eq,x$(BUILD_ACTION:s/none//)x,xx true false)"=="true"
     -$(MKDIR) $(P_BUILD_DIR)
-    +cd $(P_BUILD_DIR) && $(BUILD_ACTION) $(BUILD_FLAGS) && $(TOUCH) $(ABS_PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
+    cd $(P_BUILD_DIR) && $(BUILD_ACTION) $(BUILD_FLAGS) && $(TOUCH) $(ABS_PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
 .ENDIF			# "$(eq,x$(BUILD_ACTION:s/none//)x,xx true false)"=="true"
 
 $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE) : $(PACKAGE_DIR)$/$(BUILD_FLAG_FILE)
-    @+-$(RM) $@ >& $(NULLDEV)
+    @-$(RM) $@ >& $(NULLDEV)
 .IF "$(INSTALL_ACTION)"=="none" ||	"$(INSTALL_ACTION)"==""
-    +$(TOUCH) $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE)
+    $(TOUCH) $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE)
 .ELSE			# "$(INSTALL_ACTION)"=="none" ||	"$(INSTALL_ACTION)"==""
     -$(MKDIR) $(P_INSTALL_DIR)
     -$(MKDIR) $(P_INSTALL_TARGET_DIR)
-    +cd $(P_INSTALL_DIR) && $(INSTALL_ACTION) $(INSTALL_FLAGS) && $(TOUCH) $(INSTALL_FLAG_FILE)
-    +mv $(P_INSTALL_DIR)$/$(INSTALL_FLAG_FILE) $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE)
+    cd $(P_INSTALL_DIR) && $(INSTALL_ACTION) $(INSTALL_FLAGS) && $(TOUCH) $(INSTALL_FLAG_FILE)
+    mv $(P_INSTALL_DIR)$/$(INSTALL_FLAG_FILE) $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE)
 .ENDIF			# "$(INSTALL_ACTION)"=="none" ||	"$(INSTALL_ACTION)"==""
 
 $(PACKAGE_DIR)$/$(PREDELIVER_FLAG_FILE) : $(PACKAGE_DIR)$/$(INSTALL_FLAG_FILE)
 .IF "$(OUT2LIB)"!=""
-    +$(COPY) $(foreach,i,$(OUT2LIB) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(LB)
+    $(COPY) $(foreach,i,$(OUT2LIB) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(LB)
 .ENDIF			# "$(OUT2LIB)"!=""
 .IF "$(OUT2INC)"!=""
 .IF "$(OUT2INC_SUBDIR)"!=""
     -$(MKDIR) $(INCCOM)$/$(OUT2INC_SUBDIR)
-    +$(COPY) $(foreach,i,$(OUT2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)$/$(OUT2INC_SUBDIR)
+    $(COPY) $(foreach,i,$(OUT2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)$/$(OUT2INC_SUBDIR)
 .ELSE          # "$(OUT2INC_SUBDIR)"!=""
-    +$(COPY) $(foreach,i,$(OUT2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)
+    $(COPY) $(foreach,i,$(OUT2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)
 .ENDIF          # "$(OUT2INC_SUBDIR)"!=""
 .ENDIF			# "$(OUT2INC)"!=""
 .IF "$(OUTDIR2INC)"!=""
 .IF "$(USE_SHELL)"=="4nt"
     @-$(MKDIR) $(foreach,i,$(OUTDIR2INC) $(INCCOM)$/$(i:b))
-    @echo copied $(foreach,i,$(OUTDIR2INC) $(shell +$(COPY) $(COPYRECURSE) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i$/* $(INCCOM)$/$(i:b) >& $(NULLDEV) && echo $i))
+    @echo copied $(foreach,i,$(OUTDIR2INC) $(shell $(COPY) $(COPYRECURSE) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i$/* $(INCCOM)$/$(i:b) >& $(NULLDEV) && echo $i))
 .ELSE			# "$(USE_SHELL)"=="4nt"
-    +$(COPY) $(COPYRECURSE) $(foreach,i,$(OUTDIR2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)
+    $(COPY) $(COPYRECURSE) $(foreach,i,$(OUTDIR2INC) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(INCCOM)
 .ENDIF			# "$(USE_SHELL)"=="4nt"
 .ENDIF			# "$(OUTDIR2INC)"!=""
 .IF "$(OUT2BIN)"!=""
-    +$(COPY) $(foreach,i,$(OUT2BIN) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(BIN)
+    $(COPY) $(foreach,i,$(OUT2BIN) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(BIN)
 .ENDIF			# "$(OUT2BIN)"!=""
 .IF "$(OUT2CLASS)"!=""
-    +$(COPY) $(foreach,i,$(OUT2CLASS) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(CLASSDIR)
+    $(COPY) $(foreach,i,$(OUT2CLASS) $(PACKAGE_DIR)$/$(TARFILE_ROOTDIR)$/$i) $(CLASSDIR)
 .ENDIF			# "$(OUT2BIN)"!=""
-    +$(TOUCH) $(PACKAGE_DIR)$/$(PREDELIVER_FLAG_FILE)
+    $(TOUCH) $(PACKAGE_DIR)$/$(PREDELIVER_FLAG_FILE)
 
 $(MISC)$/$(TARFILE_ROOTDIR) : $(MISC)$/$(TARFILE_NAME).unpack
-    @+-mv $(MISC)$/$(TARFILE_ROOTDIR) $(MISC)$/$(TARFILE_ROOTDIR).old
-    @+-rm -rf $(MISC)$/$(TARFILE_ROOTDIR).old
-    +cd $(MISC) && $(subst,$(BACK_PATH),..$/..$/ $(shell +$(TYPE) $(PRJ)$/$(ROUT)$/misc$/$(TARFILE_NAME).unpack))
+    @-mv $(MISC)$/$(TARFILE_ROOTDIR) $(MISC)$/$(TARFILE_ROOTDIR).old
+    @-rm -rf $(MISC)$/$(TARFILE_ROOTDIR).old
+    cd $(MISC) && $(subst,$(BACK_PATH),..$/..$/ $(shell $(TYPE) $(PRJ)$/$(ROUT)$/misc$/$(TARFILE_NAME).unpack))
 .IF "$(GUI)"=="UNX"	
-    +$(TOUCH) $@
+    $(TOUCH) $@
 .ENDIF			# "$(GUI)"=="UNX"	
 
 
 .IF "$(P_ADDITIONAL_FILES)"!=""
 $(P_ADDITIONAL_FILES) :
-    +-echo dummy > $@
-    +-$(TOUCH) $@
+    -echo dummy > $@
+    -$(TOUCH) $@
 .ENDIF			 "$(P_ADDITIONAL_FILES)"!=""
 
 .IF "$(T_ADDITIONAL_FILES)"!=""
 $(T_ADDITIONAL_FILES:+".dummy") : $(PACKAGE_DIR)$/$(UNTAR_FLAG_FILE)
-    +-echo dummy > $@
-    +-$(TOUCH) $@
-    +-echo dummy > $(@:d)$(@:b)
-    +-$(TOUCH) $(@:d)$(@:b)
+    -echo dummy > $@
+    -$(TOUCH) $@
+    -echo dummy > $(@:d)$(@:b)
+    -$(TOUCH) $(@:d)$(@:b)
 .ENDIF			 "$(T_ADDITIONAL_FILES)"!=""
 
 create_patch : $(MISC)$/$(TARFILE_ROOTDIR) $(P_ADDITIONAL_FILES) $(PACKAGE_DIR)$/$(PATCH_FLAG_FILE)
-    @+-$(RM) $(MISC)$/$(NEW_PATCH_FILE_NAME).tmp >& $(NULLDEV)
-    @+-$(RM) $(NEW_PATCH_FILE_NAME).bak >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(NEW_PATCH_FILE_NAME).tmp >& $(NULLDEV)
+    @-$(RM) $(NEW_PATCH_FILE_NAME).bak >& $(NULLDEV)
 #ignore returncode of 1 (indicates differences...)	
 # hard coded again to get the same directory level as before. quite ugly...
-    +-cd $(PRJ)$/$(ROUT) && diff -rc misc$/$(TARFILE_ROOTDIR) misc$/build$/$(TARFILE_ROOTDIR) | $(PERL) $(SOLARENV)$/bin$/cleandiff.pl | tr -d "\015" > misc$/$(NEW_PATCH_FILE_NAME).tmp
-    @+-mv $(NEW_PATCH_FILE_NAME) $(NEW_PATCH_FILE_NAME).bak >& $(NULLDEV)
-    @+-mv $(MISC)$/$(NEW_PATCH_FILE_NAME).tmp $(PRJ)$/$(NEW_PATCH_FILE_NAME) >& $(NULLDEV)
+    -cd $(PRJ)$/$(ROUT) && diff -rc misc$/$(TARFILE_ROOTDIR) misc$/build$/$(TARFILE_ROOTDIR) | $(PERL) $(SOLARENV)$/bin$/cleandiff.pl | tr -d "\015" > misc$/$(NEW_PATCH_FILE_NAME).tmp
+    @-mv $(NEW_PATCH_FILE_NAME) $(NEW_PATCH_FILE_NAME).bak >& $(NULLDEV)
+    @-mv $(MISC)$/$(NEW_PATCH_FILE_NAME).tmp $(PRJ)$/$(NEW_PATCH_FILE_NAME) >& $(NULLDEV)
     @echo still some problems with win32 generated patches...
 
 create_clean : $(PACKAGE_DIR)$/$(UNTAR_FLAG_FILE)
