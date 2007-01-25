5864d206ae2878b6bf8ca908f0efde464452bba8
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:38:22 +0000

    INTEGRATION: CWS ause069 (1.2.18); FILE MERGED 2006/11/28 15:36:11 hjs 1.2.18.1: #i70359# remove forced shell calls

--- embeddedobj/qa/embedding/makefile.mk
+++ embeddedobj/qa/embedding/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2006-10-13 11:29:32 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:38:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -87,6 +87,6 @@ CT_APP      = org.openoffice.Runner
 RUN: run
 
 run:
-    +java -cp $(CLASSPATH) $(CT_APP) $(CT_TESTBASE) $(CT_APPEXECCOMMAND) $(CT_TEST)
+    java -cp $(CLASSPATH) $(CT_APP) $(CT_TESTBASE) $(CT_APPEXECCOMMAND) $(CT_TEST)
 
 
