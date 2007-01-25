e884bf6b16199764e2b9b04ab7cdacfb9f656b81
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:30:13 +0000

    INTEGRATION: CWS ause069 (1.6.34); FILE MERGED 2006/11/28 11:48:41 hjs 1.6.34.1: #i70359# remove forced shell calls; a bit cleanup

--- bridges/source/remote/static/makefile.mk
+++ bridges/source/remote/static/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-19 23:50:35 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:30:13 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,13 +39,10 @@ PRJNAME=bridges
 TARGET=bridges_remote_static
 ENABLE_EXCEPTIONS=TRUE
 USE_DEFFILE=TRUE
-NO_BSYMBOLIC=TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # ------------------------------------------------------------------
 
