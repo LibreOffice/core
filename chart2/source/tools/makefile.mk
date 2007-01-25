a3c435b47c5a50a5d9671afe477d90935c3c1e6d
Oliver Bolte <obo@openoffice.org>
2007-01-25 14:38:45 +0000

    INTEGRATION: CWS ause069 (1.18.90); FILE MERGED 2006/12/04 22:50:54 hjs 1.18.90.2: RESYNC: (1.18-1.19); FILE MERGED 2006/11/28 12:22:01 hjs 1.18.90.1: #i70359# remove forced shell calls

--- chart2/source/tools/makefile.mk
+++ chart2/source/tools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.19 $
+#   $Revision: 1.20 $
 #
-#   last change: $Author: ihi $ $Date: 2006-11-14 15:33:09 $
+#   last change: $Author: obo $ $Date: 2007-01-25 15:38:45 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -156,5 +156,5 @@ DEF1DES=		Chart Tools
 
 $(MISC)$/$(SHL1TARGET).flt: makefile.mk \
                             exports.flt
-    +$(TYPE) exports.flt > $@
+    $(TYPE) exports.flt > $@
 
