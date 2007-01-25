f77616e6811478709a06f1a6a3554fe0190f19aa
Oliver Bolte <obo@openoffice.org>
2007-01-25 11:44:55 +0000

    INTEGRATION: CWS ause069 (1.10.590); FILE MERGED 2006/10/11 18:23:19 hjs 1.10.590.1: #i69872# cleanup

--- svx/source/xml/makefile.mk
+++ svx/source/xml/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:15:16 $
+#   last change: $Author: obo $ $Date: 2007-01-25 12:44:55 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,12 +53,6 @@ SLOFILES = \
         $(SLO)$/xmlxtexp.obj		\
         $(SLO)$/xmlgrhlp.obj		\
         $(SLO)$/xmleohlp.obj		
-
-SVXLIGHTOBJFILES=\
-        $(OBJ)$/xmlxtimp.obj		\
-        $(OBJ)$/xmlxtexp.obj		\
-        $(OBJ)$/xmlgrhlp.obj		\
-        $(OBJ)$/xmleohlp.obj		
 
 # --- Targets --------------------------------------------------------------
 
