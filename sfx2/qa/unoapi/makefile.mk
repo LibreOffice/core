350735ae01de61092e64b26de2012783e383434a
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:12:58 +0000

    INTEGRATION: CWS ause069 (1.3.350); FILE MERGED 2006/11/28 12:07:21 hjs 1.3.350.1: #i70359# remove forced shell calls

--- sfx2/qa/unoapi/makefile.mk
+++ sfx2/qa/unoapi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 17:27:22 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:12:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,4 +39,4 @@ PRJNAME=sfx2
 TARGET=qa_unoapi
 
 ALLTAR:
-    +$(SOLARENV)$/bin$/checkapi -sce sfx.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
+    $(SOLARENV)$/bin$/checkapi -sce sfx.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
