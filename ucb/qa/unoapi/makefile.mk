96335a8b1232ba290c2a2c37c6d65d85b651ae41
Oliver Bolte <obo@openoffice.org>
2007-01-25 14:40:31 +0000

    INTEGRATION: CWS ause069 (1.3.78); FILE MERGED 2006/12/01 17:25:10 hjs 1.3.78.1: #i70359# remove forced shell calls

--- ucb/qa/unoapi/makefile.mk
+++ ucb/qa/unoapi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 15:10:12 $
+#   last change: $Author: obo $ $Date: 2007-01-25 15:40:31 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,4 +39,4 @@ PRJNAME=ucb
 TARGET=qa_unoapi
 
 ALLTAR:
-    +$(SOLARENV)$/bin$/checkapi -sce ucb.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
+    $(SOLARENV)$/bin$/checkapi -sce ucb.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
