ad5bdc0228269c874f1a676f4ef60e03c18145e9
Oliver Bolte <obo@openoffice.org>
2007-01-25 11:54:21 +0000

    INTEGRATION: CWS ause069 (1.23.2); FILE MERGED 2006/11/07 04:33:55 hjs 1.23.2.2: RESYNC: (1.23-1.24); FILE MERGED 2006/10/11 12:36:25 hjs 1.23.2.1: #i70359#: removing + sign in targets; adding to tool macro if required

--- solenv/inc/tg_dep.mk
+++ solenv/inc/tg_dep.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: tg_dep.mk,v $
 #
-#   $Revision: 1.24 $
+#   $Revision: 1.25 $
 #
-#   last change: $Author: rt $ $Date: 2006-10-30 08:54:25 $
+#   last change: $Author: obo $ $Date: 2007-01-25 12:54:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -36,44 +36,44 @@
 .IF "$(SLOFILES)$(OBJFILES)$(DEPOBJFILES)$(SRCFILES)$(SRC1FILES)$(SRC2FILES)$(SRC3FILES)$(RCFILES)$(HXX1TARGET)$(HDBDEPNTARGET)$(IDLFILES)$(PARFILES)$(ZIP1TARGET)$(ZIP2TARGET)$(ZIP3TARGET)$(ZIP4TARGET)$(ZIP5TARGET)$(ZIP6TARGET)$(ZIP7TARGET)$(ZIP8TARGET)$(ZIP9TARGET)$(COMP1TYPELIST)$(COMP2TYPELIST)$(COMP3TYPELIST)$(COMP4TYPELIST)$(COMP5TYPELIST)$(COMP6TYPELIST)$(COMP7TYPELIST)$(COMP8TYPELIST)$(COMP9TYPELIST)"!=""
 ALLDEP .PHONY: 
 .IF "$(GUI)"=="UNX" || "$(USE_SHELL)"!="4nt"
-    @+-$(IFEXIST) $(SRS)$/$(PWD:f).*.dpr $(THEN) $(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV) $(FI)
+    @-$(IFEXIST) $(SRS)$/$(PWD:f).*.dpr $(THEN) $(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV) $(FI)
 .ELSE
-    @+-$(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV)
+    @-$(RM) $(SRS)$/$(PWD:f).*.dpr >& $(NULLDEV)
 .ENDIF
-    @+-$(RM) $(MISC)$/$(TARGET).dpr >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(PWD:f).*.dprr >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(TARGET).dpz >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP1TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP2TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP3TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP4TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP5TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP6TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP7TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP8TYPELIST).mk >& $(NULLDEV)
-    @+-$(RM) $(MISC)$/$(COMP9TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(TARGET).dpr >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(PWD:f).*.dprr >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(TARGET).dpz >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP1TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP2TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP3TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP4TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP5TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP6TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP7TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP8TYPELIST).mk >& $(NULLDEV)
+    @-$(RM) $(MISC)$/$(COMP9TYPELIST).mk >& $(NULLDEV)
 .IF "$(DEPFILE_SLO)"!=""	
-    @+-$(RM) $(DEPFILE_SLO) >& $(NULLDEV)
+    @-$(RM) $(DEPFILE_SLO) >& $(NULLDEV)
 .ENDIF			# "$(DEPFILE_SLO)"!=""	
 .IF "$(DEPFILE_OBJ)"!=""	
-    @+-$(RM) $(DEPFILE_OBJ) >& $(NULLDEV)
+    @-$(RM) $(DEPFILE_OBJ) >& $(NULLDEV)
 .ENDIF			# "$(DEPFILE_OBJ)"!=""	
 .IF "$(DEPFILES)" != ""
 #to keep win9x happy
 .IF "$(GROUPSHELL:b:l)"=="4dos"
-    @+-echo $(foreach,i,$(DEPFILES) $(shell +-del $i >& $(NULLDEV))) >& $(NULLDEV)
+    @-echo $(foreach,i,$(DEPFILES) $(shell $(4nt_force_shell)-del $i >& $(NULLDEV))) >& $(NULLDEV)
 .ELSE			# "$(GROUPSHELL:b)"=="4dos"
 .IF "$(USE_SHELL)"=="4nt"
-    @+-echo $(foreach,i,$(DEPFILES) $(shell +-del $i >& $(NULLDEV))) >& $(NULLDEV)
+    @-echo $(foreach,i,$(DEPFILES) $(shell $(shell $(4nt_force_shell)-del $i >& $(NULLDEV))) >& $(NULLDEV)
 .ELSE			# "$(USE_SHELL)"=="4nt"
-    @+-$(RM) $(DEPFILES) >& $(NULLDEV)
+    @-$(RM) $(DEPFILES) >& $(NULLDEV)
 .ENDIF			# "$(USE_SHELL)"=="4nt"
 .ENDIF			# "$(GROUPSHELL:b)"=="4dos"
 .ENDIF			# "$(DEPFILES)" != ""
-    +@echo ---
-    +@echo      Old dependency files removed
-    +@echo ---
+    @echo ---
+    @echo      Old dependency files removed
+    @echo ---
 
 
 ALLDPC: \
@@ -98,14 +98,14 @@ ALLDPC: \
 .ENDIF
 .ENDIF
 .IF "$(HDBDEPNTARGET)$(OBJFILES)$(SLOFILES)$(DEPOBJFILES)$(RCFILES)$(PARFILES)"!=""
-    @+$(IFEXIST) $(MISC)$/$(TARGET).dpj $(THEN) $(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV) $(FI)
-    @+$(IFEXIST) $(MISC)$/genjava.mk $(THEN) $(RM) $(MISC)$/genjava.mk >& $(NULLDEV) $(FI)
-    +$(TOUCH) $(MISC)$/$(TARGET).dpc
+    @$(IFEXIST) $(MISC)$/$(TARGET).dpj $(THEN) $(RM) $(MISC)$/$(TARGET).dpj >& $(NULLDEV) $(FI)
+    @$(IFEXIST) $(MISC)$/genjava.mk $(THEN) $(RM) $(MISC)$/genjava.mk >& $(NULLDEV) $(FI)
+    $(TOUCH) $(MISC)$/$(TARGET).dpc
 .IF "$($(SECOND_BUILD)_SLOFILES)"!=""
-    @+$(TYPE) $(mktmp $(foreach,i,$($(SECOND_BUILD)_SLOFILES) $(i:d:^"\n")$(SECOND_BUILD)_$(i:f) : $i )) >> $(MISC)$/$(TARGET).dpc
+    @$(TYPE) $(mktmp $(foreach,i,$($(SECOND_BUILD)_SLOFILES) $(i:d:^"\n")$(SECOND_BUILD)_$(i:f) : $i )) >> $(MISC)$/$(TARGET).dpc
 .ENDIF
 .IF "$($(SECOND_BUILD)_OBJFILES)"!=""
-    @+$(TYPE) $(mktmp $(foreach,i,$($(SECOND_BUILD)_OBJFILES) $(i:d:^"\n")$(SECOND_BUILD)_$(i:f) : $(i:d:s/obj/slo/)$(i:b).obj )) >> $(MISC)$/$(TARGET).dpc
+    @$(TYPE) $(mktmp $(foreach,i,$($(SECOND_BUILD)_OBJFILES) $(i:d:^"\n")$(SECOND_BUILD)_$(i:f) : $(i:d:s/obj/slo/)$(i:b).obj )) >> $(MISC)$/$(TARGET).dpc
 .ENDIF
 .ELSE			# "$(HDBDEPNTARGET)$(OBJFILES)$(SLOFILES)$(DEPOBJFILES)$(RCFILES)"!=""
     @echo ttt: ppp > $(MISC)$/$(TARGET).dpc
