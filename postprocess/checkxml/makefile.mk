518a70fc505b4e55a3940af4781f2ccbe18169ec
Oliver Bolte <obo@openoffice.org>
2007-01-25 13:11:45 +0000

    INTEGRATION: CWS ause069 (1.2.50); FILE MERGED 2006/11/24 14:50:54 hjs 1.2.50.1: #i70359# remove forced shell calls

--- postprocess/checkxml/makefile.mk
+++ postprocess/checkxml/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 16:31:47 $
+#   last change: $Author: obo $ $Date: 2007-01-25 14:11:45 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,5 +46,5 @@ TARGET=checkxml
 ALLTAR : $(MISC)$/checkxml.done
 
 $(MISC)$/checkxml.done .PHONY:
-    +$(PERL) checkxml.pl
+    $(PERL) checkxml.pl
 
