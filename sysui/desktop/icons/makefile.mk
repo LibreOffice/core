bafa1f7352e92442306689bf8bc9959fd274ca64
Oliver Bolte <obo@openoffice.org>
2007-01-25 14:33:47 +0000

    INTEGRATION: CWS ause069 (1.4.112); FILE MERGED 2007/01/11 00:25:31 hjs 1.4.112.2: RESYNC: (1.4-1.5); FILE MERGED 2006/11/24 14:40:37 hjs 1.4.112.1: #i70359# remove forced shell calls

--- sysui/desktop/icons/makefile.mk
+++ sysui/desktop/icons/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: ihi $ $Date: 2006-12-18 16:37:59 $
+#   last change: $Author: obo $ $Date: 2007-01-25 15:33:47 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -126,7 +126,7 @@ all: \
     $(MISC)$/oxt-extension.ico
 
 $(MISC)$/%.ico: %.ico
-    +$(COPY) $< $@
+    $(COPY) $< $@
 
 .INCLUDE :  target.mk
 
