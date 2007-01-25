c33c2c53a93ce5bdef6092dc6ba53f056cce08ac
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:32:07 +0000

    INTEGRATION: CWS ause069 (1.2.82); FILE MERGED 2006/11/28 11:48:45 hjs 1.2.82.1: #i70359# remove forced shell calls; a bit cleanup

--- bridges/test/performance/makefile.mk
+++ bridges/test/performance/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 23:02:39 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:32:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,13 +40,10 @@ TARGET=test
 LIBTARGET=NO
 TARGETTYPE=CUI
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC=TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- Files --------------------------------------------------------
 
