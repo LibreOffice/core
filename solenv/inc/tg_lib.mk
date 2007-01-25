b04cb3da4548d13cbea92fed5b8ba881eb20c3ff
Oliver Bolte <obo@openoffice.org>
2007-01-25 11:55:40 +0000

    INTEGRATION: CWS ause069 (1.19.2); FILE MERGED 2006/12/01 17:42:39 hjs 1.19.2.2: #i70359# remove forced shell calls 2006/10/11 12:36:25 hjs 1.19.2.1: #i70359#: removing + sign in targets; adding to tool macro if required

--- solenv/inc/tg_lib.mk
+++ solenv/inc/tg_lib.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: tg_lib.mk,v $
 #
-#   $Revision: 1.19 $
+#   $Revision: 1.20 $
 #
-#   last change: $Author: kz $ $Date: 2006-10-05 10:40:49 $
+#   last change: $Author: obo $ $Date: 2007-01-25 12:55:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -42,14 +42,14 @@
 
 $(LIB$(TNR)ARCHIV) :	$(LIB$(TNR)TARGET)
     @echo Making: $@
-    @-+$(RM) $@ >& $(NULLDEV)
+    @-$(RM) $@ >& $(NULLDEV)
 .IF "$(GUI)"=="UNX"
-    @+-$(RM) $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
+    @-$(RM) $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
 .IF "$(OS)" =="HPUX_FRAG_HR"
-    @+-$(RM) $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
+    @-$(RM) $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
     @echo $(LINK) +inst_close -c `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
     @cat $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
-    @source $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
+    @+source $(MISC)$/$(LIB$(TNR)ARCHIV:b)_closetempl.cmd
 .ENDIF
     @echo $(LIBMGR) $(LIB$(TNR)FLAGS) $(LIBFLAGS) $(LIB$(TNR)ARCHIV) `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
 .IF "$(OS)$(COM)"=="NETBSDGCC"
@@ -59,7 +59,7 @@ $(LIB$(TNR)ARCHIV) :	$(LIB$(TNR)TARGET)
      @echo  ranlib $(LIB$(TNR)ARCHIV) >> $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
 .ENDIF
     @cat $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
-    @source $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
+    @+source $(MISC)$/$(LIB$(TNR)ARCHIV:b).cmd
 .ELSE			# "$(GUI)"=="UNX"
     @echo just a dummy > $@
 .ENDIF			# "$(GUI)"=="UNX"
@@ -73,29 +73,29 @@ $(LIB$(TNR)TARGET) :	$(LIB$(TNR)FILES) \
     @echo using: $(LIB$(TNR)TARGET)
     @echo ------------------------------
     @echo Making: $@
-    @+-$(RM) $@ >& $(NULLDEV)
+    @-$(RM) $@ >& $(NULLDEV)
 .IF "$(GUI)"=="UNX"
     @echo $(LIB$(TNR)OBJFILES:s/.obj/.o/) | sed "s#$(PRJ:s/./\./)$/$(ROUT)#$(ROUT)#g" | xargs -n 1 > $@
-    @+cat /dev/null $(LIB$(TNR)FILES:s/.obj/.o/) | xargs -n 1 >> $@
-    @+$(RM) $(@:d)$(@:b).dump
+    @cat /dev/null $(LIB$(TNR)FILES:s/.obj/.o/) | xargs -n 1 >> $@
+    @$(RM) $(@:d)$(@:b).dump
 .IF "$(OS)"=="MACOSX"
-    @-+nm `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(@:d)$(@:b).dump
+    @-nm `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(@:d)$(@:b).dump
 .ELSE
-    @+nm `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(@:d)$(@:b).dump
+    @nm `cat $(LIB$(TNR)TARGET) | sed s\#'^'$(ROUT)\#$(PRJ)$/$(ROUT)\#g` > $(@:d)$(@:b).dump
 .ENDIF
 .ELSE			# "$(GUI)"=="UNX"
 .IF "$(GUI)"=="WNT"
     $(LIBMGR) $(LIBFLAGS) /OUT:$@ @$(mktmp $(LIB$(TNR)FILES) $(LIB$(TNR)OBJFILES))
-    @+-$(RM) $(@:s/.lib/.lin/)
+    @-$(RM) $(@:s/.lib/.lin/)
 .IF "$(LIB$(TNR)OBJFILES)"!=""    
-    @+$(TYPE) $(mktmp $(LIB$(TNR)OBJFILES)) > $(null,$(LIB$(TNR)OBJFILES) $(NULLDEV) $(@:s/.lib/.lin/))
+    @$(TYPE) $(mktmp $(LIB$(TNR)OBJFILES)) > $(null,$(LIB$(TNR)OBJFILES) $(NULLDEV) $(@:s/.lib/.lin/))
 .ENDIF          # "$(LIB$(TNR)OBJFILES)"!=""    
 .IF "$(LIB$(TNR)FILES)"!=""    
     @-$(TYPE) $(foreach,i,$(LIB$(TNR)FILES) $(i:s/.lib/.lin/)) >> $(@:s/.lib/.lin/)
 .ENDIF          # "$(LIB$(TNR)FILES)"!=""    
-    @+$(ECHONL)
+    @$(ECHONL)
 .ELSE			# "$(GUI)"=="WNT"
-    @+-$(RM) $@
+    @-$(RM) $@
     echo $(LIBMGR) r $@ $(LIB$(TNR)OBJFILES)
     $(LIBMGR) r $@ $(LIB$(TNR)OBJFILES) $(LIB$(TNR)FILES) bla.lib
 .ENDIF          # "$(GUI)"=="WNT"
