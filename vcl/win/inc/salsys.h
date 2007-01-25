7c3937f29a2f4bfd2d96464ef95af299c7a0a689
Oliver Bolte <obo@openoffice.org>
2007-01-25 10:26:05 +0000

    INTEGRATION: CWS vcl71 (1.2.2); FILE ADDED 2006/12/22 16:03:33 pl 1.2.2.1: #i72830# #i72472# #i72484# react on display settings changes, provide screen names

---
+++ vcl/win/inc/salsys.h
@@ -0,0 +1,96 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: salsys.h,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-01-25 11:26:05 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SV_SALSYS_H
+#define _SV_SALSYS_H
+
+#include <salsys.hxx>
+
+#include <vector>
+#include <map>
+
+class WinSalSystem : public SalSystem
+{
+    struct DisplayMonitor
+    {
+        rtl::OUString        m_aName;
+        rtl::OUString        m_aDeviceName;
+        Rectangle            m_aArea;
+        Rectangle            m_aWorkArea;
+        sal_Int32            m_nStateFlags;
+
+        DisplayMonitor() : m_nStateFlags( 0 ) {}
+        DisplayMonitor( const rtl::OUString& rName,
+                        const rtl::OUString& rDevName,
+                        const Rectangle& rArea,
+                        const Rectangle& rWorkArea,
+                        DWORD nStateFlags )
+                        : m_aName( rName ),
+                          m_aDeviceName( rDevName ),
+                          m_aArea( rArea ),
+                          m_aWorkArea( rWorkArea ),
+                          m_nStateFlags( nStateFlags )
+        {
+        }
+        ~DisplayMonitor() {}
+    };
+    std::vector<DisplayMonitor>             m_aMonitors;
+    std::map<rtl::OUString, unsigned int>   m_aDeviceNameToMonitor;
+    unsigned int                            m_nPrimary;
+public:
+    WinSalSystem() : m_nPrimary( 0 ) {}
+    virtual ~WinSalSystem();
+
+    virtual unsigned int GetDisplayScreenCount();
+    virtual bool IsMultiDisplay();
+    virtual unsigned int GetDefaultDisplayNumber();
+    virtual Rectangle GetDisplayScreenPosSizePixel( unsigned int nScreen );
+    virtual Rectangle GetDisplayWorkAreaPosSizePixel( unsigned int nScreen );
+    virtual rtl::OUString GetScreenName( unsigned int nScreen );
+    virtual int ShowNativeMessageBox( const String& rTitle,
+                                      const String& rMessage,
+                                      int nButtonCombination,
+                                      int nDefaultButton);
+    bool initMonitors();
+    // discards monitorinfo; used by WM_DISPLAYCHANGED handler
+    void clearMonitors();
+
+    BOOL handleMonitorCallback( sal_IntPtr /*HMONITOR*/,
+                                sal_IntPtr /*HDC*/,
+                                sal_IntPtr /*LPRECT*/ );
+};
+
+#endif // _SV_SALSYS_H
+
