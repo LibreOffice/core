0f79c7169209b190cb15d4472ca451b5c0f2e129
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:27:35 +0000

    INTEGRATION: CWS ause069 (1.2.24); FILE MERGED 2006/11/28 11:48:37 hjs 1.2.24.1: #i70359# remove forced shell calls; a bit cleanup

--- bridges/source/cpp_uno/gcc3_freebsd_x86-64/makefile.mk
+++ bridges/source/cpp_uno/gcc3_freebsd_x86-64/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 09:30:13 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:27:35 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,13 +39,10 @@ PRJNAME=bridges
 TARGET=gcc3_uno
 LIBTARGET=no
 ENABLE_EXCEPTIONS=TRUE
-NO_BSYMBOLIC=TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- Files --------------------------------------------------------
 
