c473956f9573c174c03985ba081e6c49fe8b07c4
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:46:54 +0000

    INTEGRATION: CWS ause069 (1.6.16); FILE MERGED 2006/12/19 14:43:11 hjs 1.6.16.1: #i58881# just a comment...

--- connectivity/source/drivers/evoab2/EApi.cxx
+++ connectivity/source/drivers/evoab2/EApi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EApi.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 02:28:19 $
+ *  last change: $Author: obo $ $Date: 2007-01-25 13:46:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -124,7 +124,8 @@ bool EApiInit()
 }
 
 #if 0
-
+// hjs: SOLARDEF does no longer exist please lookup the required
+// defines in a regular compile line
 /*
  * Test code - enable &
  *
