6f81e901ba889f5f9678ad7d3170c36faf4eb990
Oliver Bolte <obo@openoffice.org>
2007-01-25 10:27:30 +0000

    INTEGRATION: CWS vcl71 (1.15.96); FILE MERGED 2007/01/11 14:07:20 hdu 1.15.96.1: add UNO tools lib

--- vcl/workben/makefile.mk
+++ vcl/workben/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: kz $ $Date: 2006-10-05 10:57:11 $
+#   last change: $Author: obo $ $Date: 2007-01-25 11:27:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,6 +58,7 @@ APP1OBJS= \
     $(OBJ)$/svdem.obj
 
 APP1STDLIBS=	$(CPPULIB)			\
+                $(UNOTOOLSLIB)	\
                 $(CPPUHELPERLIB)	\
                 $(COMPHELPERLIB)	\
                 $(VCLLIB)			\
