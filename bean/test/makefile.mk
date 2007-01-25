e05d7af1015b48589fa25589ba858b3f114a8090
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:05:34 +0000

    INTEGRATION: CWS ause069 (1.3.24); FILE MERGED 2006/12/01 15:06:11 hjs 1.3.24.1: #i70359# remove forced shell calls

--- bean/test/makefile.mk
+++ bean/test/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-10-19 11:55:02 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:05:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -84,11 +84,11 @@ ALLTAR : RUNINSTRUCTIONS
 #The OOoBean uses the classpath to find the office installation.
 #Therefore we must use the jar files from the office.
 RUN:
-    +java -cp $(OFFICE_CLASSPATH) $(OOOBEAN_OPTIONS) $(PACKAGE).Test
+    java -cp $(OFFICE_CLASSPATH) $(OOOBEAN_OPTIONS) $(PACKAGE).Test
 run: RUN
 
 rund:
-    +java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8100 -cp $(OFFICE_CLASSPATH) $(OOOBEAN_OPTIONS) $(PACKAGE).Test
+    java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8100 -cp $(OFFICE_CLASSPATH) $(OOOBEAN_OPTIONS) $(PACKAGE).Test
 
 
 
