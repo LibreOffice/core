e782aa8084143bdc101b69b9545b6cd3fb071b31
Rüdiger Timm <rt@openoffice.org>
2007-01-29 16:03:06 +0000

    INTEGRATION: CWS smarttags2 (1.56.142); FILE MERGED 2007/01/22 15:48:56 fme 1.56.142.1: #i73744# Scan field text for smart tags

--- sw/source/core/text/porfld.cxx
+++ sw/source/core/text/porfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: porfld.cxx,v $
  *
- *  $Revision: 1.56 $
+ *  $Revision: 1.57 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:37:14 $
+ *  last change: $Author: rt $ $Date: 2007-01-29 17:03:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -775,7 +775,7 @@ static sal_Char __READONLY_DATA sDoubleSpace[] = "  ";
 
                 pThis->Width( nOldWidth - nSpaceOffs + 12 );
                 {
-                    SwTxtSlotLen aDiffTxt( &aInf, this, sDoubleSpace );
+                    SwTxtSlot aDiffTxt( &aInf, this, true, false, sDoubleSpace );
                     aInf.DrawText( *this, aInf.GetLen(), sal_True );
                 }
             }
