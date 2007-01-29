c2d0fedfa7785c54e70dfa9e958088e6a730145e
Rüdiger Timm <rt@openoffice.org>
2007-01-29 13:23:57 +0000

    INTEGRATION: CWS atkbridge5 (1.4.136); FILE MERGED 2007/01/22 08:44:18 obr 1.4.136.3: #i72116# reverted previous changes as they come too late during the office launch, also removed set_prgname again 2007/01/22 08:15:36 obr 1.4.136.2: #i72116# restore gail_util vtable before closing 2007/01/21 23:02:43 obr 1.4.136.1: #72116# set accessible name of root accessible to Application::GetAppName if set

--- vcl/unx/gtk/a11y/atkutil.cxx
+++ vcl/unx/gtk/a11y/atkutil.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atkutil.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:28:17 $
+ *  last change: $Author: rt $ $Date: 2007-01-29 14:23:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -705,4 +705,5 @@ ooo_atk_util_get_type (void)
 
   return type;
 }
+
 
