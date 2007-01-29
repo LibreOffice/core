00d5aa1ee2342fa42d417928e85624062a8d0d0f
Rüdiger Timm <rt@openoffice.org>
2007-01-29 14:41:20 +0000

    INTEGRATION: CWS cppuhelpshrink (1.41.72); FILE MERGED 2007/01/24 09:50:56 sb 1.41.72.1: #i72766# Missing include.

--- connectivity/source/sdbcx/VCollection.cxx
+++ connectivity/source/sdbcx/VCollection.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VCollection.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 03:10:39 $
+ *  last change: $Author: rt $ $Date: 2007-01-29 15:41:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,8 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
+
+#include <algorithm>
 #include <stdio.h>
 
 #ifndef _CONNECTIVITY_SDBCX_COLLECTION_HXX_
