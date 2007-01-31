fce52a50b0d1f6f0423e412199b3cfe23db38468
Rüdiger Timm <rt@openoffice.org>
2007-01-31 07:39:43 +0000

    INTEGRATION: CWS macosxmapfiles (1.10.94); FILE MERGED 2007/01/11 14:11:41 tra 1.10.94.1: #69351# Map files are now supported on Mac OS X rudimentary

--- ucb/source/sorter/makefile.mk
+++ ucb/source/sorter/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 15:19:43 $
+#   last change: $Author: rt $ $Date: 2007-01-31 08:39:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -70,10 +70,7 @@ SHL1STDLIBS+=$(TOOLSLIB)
 SHL1LIBS=$(LIB1TARGET)
 SHL1IMPLIB=i$(TARGET)
 
-.IF "$(OS)"=="MACOSX"
-.ELSE
 SHL1VERSIONMAP=exports.map
-.ENDIF
 
 DEF1NAME=$(SHL1TARGET)
 DEF1EXPORTFILE=exports.dxp
