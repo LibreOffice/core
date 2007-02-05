fcefd22b160ec44a589a93cf1dc565af96322133
Vladimir Glazounov <vg@openoffice.org>
2007-02-05 09:54:41 +0000

    INTEGRATION: CWS swqbf95_SRC680 (1.25.172); FILE MERGED 2007/01/25 08:25:30 od 1.25.172.1: #i73790# method <SwFmt::ResetAllAttr()> renamed to <SwFmt::ResetAllFmtAttr()>

--- sw/source/ui/app/docstyle.cxx
+++ sw/source/ui/app/docstyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docstyle.cxx,v $
  *
- *  $Revision: 1.25 $
+ *  $Revision: 1.26 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:35:06 $
+ *  last change: $Author: vg $ $Date: 2007-02-05 10:54:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2176,7 +2176,11 @@ void  SwDocStyleSheetPool::Replace( SfxStyleSheetBase& rSource,
             else if( USHRT_MAX != nPgDscPos )
                 pTargetFmt->ResetAttr( RES_PAGEDESC, RES_FRMATR_END-1 );
             else
-                pTargetFmt->ResetAllAttr();
+            {
+                // --> OD 2007-01-25 #i73790# - method renamed
+                pTargetFmt->ResetAllFmtAttr();
+                // <--
+            }
 
             if( USHRT_MAX != nPgDscPos )
                 rDoc.ChgPageDesc( nPgDscPos,
