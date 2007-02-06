9f713c2c80c310351247ca77758c41759b33fa34
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:07:07 +0000

    INTEGRATION: CWS ause074 (1.2.54); FILE MERGED 2007/01/30 12:09:35 hjs 1.2.54.1: #i73801# remove remaining usages of sv[pre].mk

--- ucbhelper/workben/myucp/makefile.mk
+++ ucbhelper/workben/myucp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 16:43:42 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:07:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,9 +53,7 @@ NO_BSYMBOLIC=TRUE
 
 # --- Settings ---------------------------------------------------------
 
-.INCLUDE: svpre.mk
 .INCLUDE: settings.mk
-.INCLUDE: sv.mk
 
 # --- General -----------------------------------------------------
 
