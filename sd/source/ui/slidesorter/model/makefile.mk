de9bd0669357057057fbcf71e79010006417f65c
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:14:42 +0000

    INTEGRATION: CWS ause074 (1.3.376); FILE MERGED 2007/02/01 11:41:11 hjs 1.3.376.1: #i74024#,#i73801# remove obsolete include

--- sd/source/ui/slidesorter/model/makefile.mk
+++ sd/source/ui/slidesorter/model/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 06:24:30 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:14:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -45,9 +45,7 @@ PRJINC=..$/..
 
 # --- Settings -----------------------------------------------------
 
-#.INCLUDE :  sdpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
