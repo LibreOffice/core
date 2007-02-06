1e5afdb3af4c2bd0858841a51fc82a57666549cc
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:19:58 +0000

    INTEGRATION: CWS ause074 (1.10.40); FILE MERGED 2007/01/30 12:05:16 hjs 1.10.40.1: #i73801# remove remaining usages of sv[pre].mk

--- io/source/stm/makefile.mk
+++ io/source/stm/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 18:30:31 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:19:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -42,9 +42,7 @@ COMP1TYPELIST = stm
 COMPRDB=$(SOLARBINDIR)$/udkapi.rdb
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 DLLPRE =
 # --- Files --------------------------------------------------------
 UNOUCRDEP=$(SOLARBINDIR)$/udkapi.rdb
