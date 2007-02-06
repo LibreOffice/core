097ecf5b3adfc24aa494800eae2e5e8661538559
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:38:32 +0000

    INTEGRATION: CWS ause074 (1.4.58); FILE MERGED 2007/01/30 12:03:34 hjs 1.4.58.1: #i73801# remove remaining usages of sv[pre].mk

--- codemaker/source/idlmaker/makefile.mk
+++ codemaker/source/idlmaker/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 02:16:44 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:38:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,9 +43,7 @@ LIBTARGET=NO
 ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- Files --------------------------------------------------------
 
