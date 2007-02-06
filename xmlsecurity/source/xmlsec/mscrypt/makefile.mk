7a2b0f882835ca9ba54f90aa5637013d99f9be81
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:03:56 +0000

    INTEGRATION: CWS ause074 (1.2.116); FILE MERGED 2007/01/30 12:11:19 hjs 1.2.116.1: #i73801# remove remaining usages of sv[pre].mk

--- xmlsecurity/source/xmlsec/mscrypt/makefile.mk
+++ xmlsecurity/source/xmlsec/mscrypt/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 17:29:13 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:03:56 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -42,9 +42,7 @@ ENABLE_EXCEPTIONS = TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 .INCLUDE :	$(PRJ)$/util$/target.pmk
 
 .IF "$(CRYPTO_ENGINE)" != "mscrypto"
