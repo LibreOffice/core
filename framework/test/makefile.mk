ff21d4956b91e025923ae48f6730bbb8c11279d7
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:02:09 +0000

    INTEGRATION: CWS ause074 (1.11.278); FILE MERGED 2007/01/30 12:04:50 hjs 1.11.278.1: #i73801# remove remaining usages of sv[pre].mk

--- framework/test/makefile.mk
+++ framework/test/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 02:06:05 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:01:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,9 +43,7 @@ NO_BSYMBOLIC=		TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 .IF "$(COM)"=="ICC"
 LINKFLAGS+=/SEGMENTS:1024 /PACKD:32768
