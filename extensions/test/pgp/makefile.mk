e94a4830a0d02afcec6c48079d7c87d42bf890b7
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:00:04 +0000

    INTEGRATION: CWS ause074 (1.3.4); FILE MERGED 2007/01/30 12:04:13 hjs 1.3.4.1: #i73801# remove remaining usages of sv[pre].mk

--- extensions/test/pgp/makefile.mk
+++ extensions/test/pgp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:14:40 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:00:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,7 @@ TARGET  := test_com_sun_star_pgp
 
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 
 # Files --------------------------------------------------------
