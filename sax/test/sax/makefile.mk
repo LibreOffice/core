5045252cc25113fcc2aa2e20be714155b97d0acb
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 11:57:23 +0000

    INTEGRATION: CWS ause074 (1.4.32); FILE MERGED 2007/01/30 12:06:12 hjs 1.4.32.1: #i73801# remove remaining usages of sv[pre].mk

--- sax/test/sax/makefile.mk
+++ sax/test/sax/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 12:06:51 $
+#   last change: $Author: vg $ $Date: 2007-02-06 12:57:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,9 +39,7 @@ TARGET=testsax
 USE_DEFFILE=TRUE
 ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- Files --------------------------------------------------------
 
--- sax/util/makefile.mk
+++ sax/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 12:07:50 $
+#   last change: $Author: vg $ $Date: 2007-02-06 12:57:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,9 +39,7 @@ TARGET = sax.uno
 NO_BSYMBOLIC=TRUE
 ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 DLLPRE =
 #-----------------------------------------------------------
 
