dd1b1e11b629afe44cd2adc1ad353763ae34d973
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 13:02:56 +0000

    INTEGRATION: CWS ause074 (1.2.30); FILE MERGED 2007/01/30 12:05:37 hjs 1.2.30.1: #i73801# remove remaining usages of sv[pre].mk

--- rdbmaker/source/codemaker/makefile.mk
+++ rdbmaker/source/codemaker/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 05:04:12 $
+#   last change: $Author: vg $ $Date: 2007-02-06 14:02:56 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,9 +41,7 @@ TARGET=$(PRJNAME)
 ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # ------------------------------------------------------------------
 
