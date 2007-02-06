cc93b266f701398f6b7265b4db36f03983b66c3c
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:28:02 +0000

    INTEGRATION: CWS ause074 (1.4.172); FILE MERGED 2007/01/30 12:09:13 hjs 1.4.172.1: #i73801# remove remaining usages of sv[pre].mk

--- tools/source/inet/makefile.mk
+++ tools/source/inet/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 14:23:52 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:28:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,9 +37,7 @@ PRJ = ..$/..
 PRJNAME = tools
 TARGET = inet
 
-.INCLUDE: svpre.mk
 .INCLUDE: settings.mk
-.INCLUDE: sv.mk
 .INCLUDE :      $(PRJ)$/util$/makefile.pmk
 
 SLOFILES=\
