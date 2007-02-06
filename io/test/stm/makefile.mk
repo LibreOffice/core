360b4e5d0f5fe342f51fbad4ac30a9eec757559f
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:20:20 +0000

    INTEGRATION: CWS ause074 (1.4.40); FILE MERGED 2007/01/30 12:05:17 hjs 1.4.40.1: #i73801# remove remaining usages of sv[pre].mk

--- io/test/stm/makefile.mk
+++ io/test/stm/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 18:33:31 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:20:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,7 @@ NO_BSYMBOLIC=TRUE
 ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 # --- Files --------------------------------------------------------
 UNOUCRDEP=$(SOLARBINDIR)$/udkapi.rdb
 UNOUCRRDB=$(SOLARBINDIR)$/udkapi.rdb
