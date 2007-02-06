5e947bf75221eb49409fbdd8554567680f467c1c
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:31:06 +0000

    INTEGRATION: CWS ause074 (1.2.172); FILE MERGED 2007/01/30 12:09:17 hjs 1.2.172.1: #i73801# remove remaining usages of sv[pre].mk

--- tools/win/source/fastfsys/makefile.mk
+++ tools/win/source/fastfsys/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 14:49:50 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:31:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,7 @@ TARGET=fastfsys
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 
 # --- WNT ----------------------------------------------------------
 
