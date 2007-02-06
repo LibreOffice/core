f32e5827b88b63f173b58b9e5e70566793c50d13
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 11:56:22 +0000

    INTEGRATION: CWS ause074 (1.3.26); FILE MERGED 2007/01/30 12:02:31 hjs 1.3.26.1: #i73801# remove remaining usages of sv[pre].mk

--- animations/source/animcore/makefile.mk
+++ animations/source/animcore/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 15:47:01 $
+#   last change: $Author: vg $ $Date: 2007-02-06 12:56:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -42,9 +42,7 @@ NO_BSYMBOLIC=TRUE
 #COMPRDB=$(SOLARBINDIR)$/offapi.rdb
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 # --- Files --------------------------------------------------------
 #UNOUCRDEP=$(SOLARBINDIR)$/offapi.rdb
 #UNOUCRRDB=$(SOLARBINDIR)$/offapi.rdb
