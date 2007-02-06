eae758d28c65d25913828c264290c891241418fb
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:27:49 +0000

    INTEGRATION: CWS ause074 (1.13.78); FILE MERGED 2007/01/30 12:09:13 hjs 1.13.78.1: #i73801# remove remaining usages of sv[pre].mk

--- tools/source/generic/makefile.mk
+++ tools/source/generic/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: rt $ $Date: 2006-07-26 08:19:09 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:27:49 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,7 @@ TARGET=gen
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE : svpre.mk
 .INCLUDE : settings.mk
-.INCLUDE : sv.mk
 .INCLUDE :      $(PRJ)$/util$/makefile.pmk
 
 .IF "$(WITH_GPC)"!="NO"
