1cd8329575f2cf1f243773b11d7aa36fe88e7e27
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 13:23:03 +0000

    INTEGRATION: CWS ause074 (1.29.48); FILE MERGED 2007/01/30 12:09:28 hjs 1.29.48.1: #i73801# remove remaining usages of sv[pre].mk

--- ucb/source/ucp/webdav/makefile.mk
+++ ucb/source/ucp/webdav/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.30 $
+#   $Revision: 1.31 $
 #
-#   last change: $Author: rt $ $Date: 2007-01-31 08:41:01 $
+#   last change: $Author: vg $ $Date: 2007-02-06 14:23:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,9 +53,7 @@ NO_BSYMBOLIC=TRUE
 
 # --- Settings ---------------------------------------------------------
 
-.INCLUDE: svpre.mk
 .INCLUDE: settings.mk
-.INCLUDE: sv.mk
 
 CFLAGS+= -DNEON_VERSION=$(NEON_VERSION)
 .IF "$(SYSTEM_NEON)" == "YES"
