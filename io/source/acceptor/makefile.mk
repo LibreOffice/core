e71d63fee92d93ac65f477d0ed882bd8671fe13f
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:19:34 +0000

    INTEGRATION: CWS ause074 (1.13.40); FILE MERGED 2007/01/30 12:05:15 hjs 1.13.40.1: #i73801# remove remaining usages of sv[pre].mk

--- io/source/acceptor/makefile.mk
+++ io/source/acceptor/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 18:28:27 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:19:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,9 +43,7 @@ COMP1TYPELIST = acceptor
 COMPRDB=$(SOLARBINDIR)$/udkapi.rdb
 
 # --- Settings -----------------------------------------------------
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 DLLPRE =
 # ------------------------------------------------------------------
 
