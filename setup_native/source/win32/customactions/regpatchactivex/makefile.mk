92b1f2a17dd6efd770a90d7a1509ef4cfead923e
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:53:11 +0000

    INTEGRATION: CWS ause074 (1.3.64); FILE MERGED 2007/01/30 12:07:46 hjs 1.3.64.1: #i73801# remove remaining usages of sv[pre].mk

--- setup_native/source/win32/customactions/regpatchactivex/makefile.mk
+++ setup_native/source/win32/customactions/regpatchactivex/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-20 03:40:53 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:53:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -45,9 +45,7 @@ USE_DEFFILE=TRUE
 # NO_DEFAULT_STL=TRUE
 ENABLE_EXCEPTIONS=TRUE
 
-#.INCLUDE : svpre.mk
 .INCLUDE : settings.mk
-#.INCLUDE : sv.mk
 
 STDSHL=
 # SOLARINC!:=$(SOLARINC:s/stl//)
