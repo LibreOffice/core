578bf532804b0a7ef43ab78a4bc318d3275bd47b
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 13:14:22 +0000

    INTEGRATION: CWS ause074 (1.10.228); FILE MERGED 2007/01/30 12:10:39 hjs 1.10.228.1: #i73801# remove remaining usages of sv[pre].mk

--- xmloff/source/chart/makefile.mk
+++ xmloff/source/chart/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 13:30:57 $
+#   last change: $Author: vg $ $Date: 2007-02-06 14:14:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,9 +41,7 @@ ENABLE_EXCEPTIONS	= TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE : svpre.mk
 .INCLUDE : settings.mk
-.INCLUDE : sv.mk
 .INCLUDE: $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
