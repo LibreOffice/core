f191746d7012fdfb779ba3bbb1959190c6775bf1
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 12:07:40 +0000

    INTEGRATION: CWS ause074 (1.6.94); FILE MERGED 2007/01/30 12:09:53 hjs 1.6.94.1: #i73801# remove remaining usages of sv[pre].mk

--- uui/util/makefile.mk
+++ uui/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 10:28:36 $
+#   last change: $Author: vg $ $Date: 2007-02-06 13:07:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,9 +41,7 @@ RESTARGET = $(TARGET)
 GEN_HID = true
 NO_BSYMBOLIC = true
 
-.INCLUDE: svpre.mk
 .INCLUDE: settings.mk
-.INCLUDE: sv.mk
 
 SHL1TARGET = $(TARGET)$(UPD)$(DLLPOSTFIX)
 SHL1IMPLIB = i$(TARGET)
