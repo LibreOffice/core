c62915f0e928dfede32d77a96188f39699e6ca6d
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 10:48:58 +0000

    INTEGRATION: CWS calcwarnings (1.14.32); FILE MERGED 2007/02/19 15:38:56 nn 1.14.32.3: RESYNC: (1.16-1.17); FILE MERGED 2006/12/18 09:30:57 nn 1.14.32.2: RESYNC: (1.14-1.16); FILE MERGED 2006/12/15 14:08:35 nn 1.14.32.1: #i69284# warning-free: sc

--- solenv/inc/wntmsci10.mk
+++ solenv/inc/wntmsci10.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: wntmsci10.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: rt $ $Date: 2007-02-21 10:50:48 $
+#   last change: $Author: vg $ $Date: 2007-02-27 11:48:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -221,7 +221,6 @@ MODULES_WITH_WARNINGS := \
     lingu \
     lingucomponent \
     r_tools \
-    sc \
     soldep \
     starmath \
     sw \
