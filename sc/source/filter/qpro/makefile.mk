04acc5f81cf8bc063d7d15e681eb39d7208e983a
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 11:40:18 +0000

    INTEGRATION: CWS calcwarnings (1.2.248); FILE MERGED 2006/12/14 14:01:22 dr 1.2.248.1: #i69284# remove compiler warnings for unxsols4

--- sc/source/filter/qpro/makefile.mk
+++ sc/source/filter/qpro/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2006-01-27 15:51:00 $
+#   last change: $Author: vg $ $Date: 2007-02-27 12:40:18 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -54,17 +54,11 @@ AUTOSEG=true
 
 # --- Files --------------------------------------------------------
 
-CXXFILES = \
-            qpro.cxx        \
-            qproform.cxx \
-            qprostyle.cxx \
-            biff.cxx
-
-SLOFILES = \
-            $(SLO)$/qpro.obj        \
-            $(SLO)$/qproform.obj    \
-            $(SLO)$/qprostyle.obj \
-            $(SLO)$/biff.obj 
+SLOFILES =						\
+        $(SLO)$/qpro.obj		\
+        $(SLO)$/qproform.obj    \
+        $(SLO)$/qprostyle.obj	\
+        $(SLO)$/biff.obj
 
 # --- Tagets -------------------------------------------------------
 
