d901eb0427d901100a0e42fd9652f3849c861032
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 12:19:56 +0000

    INTEGRATION: CWS calcwarnings (1.2.322); FILE MERGED 2006/12/14 17:57:10 nn 1.2.322.1: #i69284# warning-free: ui, unxsols4

--- sc/source/ui/inc/auditsh.hxx
+++ sc/source/ui/inc/auditsh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: auditsh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:13:11 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 13:19:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@ private:
 
 public:
     TYPEINFO();
-    SFX_DECL_INTERFACE(SCID_AUDITING_SHELL);
+    SFX_DECL_INTERFACE(SCID_AUDITING_SHELL)
 
                     ScAuditingShell(ScViewData* pData);
                     ~ScAuditingShell();
