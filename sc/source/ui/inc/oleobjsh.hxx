6cb8283be0eb5b92e7ea8867909da2aad108677c
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 12:24:42 +0000

    INTEGRATION: CWS calcwarnings (1.2.322); FILE MERGED 2006/12/14 17:57:12 nn 1.2.322.1: #i69284# warning-free: ui, unxsols4

--- sc/source/ui/inc/oleobjsh.hxx
+++ sc/source/ui/inc/oleobjsh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: oleobjsh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:40:33 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 13:24:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,7 @@ class ScOleObjectShell: public ScDrawShell
 public:
 
     TYPEINFO();
-    SFX_DECL_INTERFACE(SCID_OLEOBJECT_SHELL);
+    SFX_DECL_INTERFACE(SCID_OLEOBJECT_SHELL)
 
                 ScOleObjectShell(ScViewData* pData);
     virtual     ~ScOleObjectShell();
