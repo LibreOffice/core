01ca162fdd7170feb7e9e67f6b948560b2f4266e
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 11:37:18 +0000

    INTEGRATION: CWS calcwarnings (1.11.118); FILE MERGED 2006/12/12 16:42:50 dr 1.11.118.1: #i69284# remove compiler warnings for unxlngi6

--- sc/source/filter/inc/xistream.hxx
+++ sc/source/filter/inc/xistream.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xistream.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 14:00:59 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 12:37:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@ class XclImpDecrypter;
 typedef ScfRef< XclImpDecrypter > XclImpDecrypterRef;
 
 /** Base class for BIFF stream decryption. */
-class XclImpDecrypter : ScfNoCopy
+class XclImpDecrypter
 {
 public:
     explicit            XclImpDecrypter();
