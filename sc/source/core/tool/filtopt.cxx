4eaca8952350720c7cac531e91208fc894eb52e8
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 11:15:43 +0000

    INTEGRATION: CWS calcwarnings (1.4.110); FILE MERGED 2006/11/28 13:24:47 nn 1.4.110.1: #i69284# warning-free: core, wntmsci10

--- sc/source/core/tool/filtopt.cxx
+++ sc/source/core/tool/filtopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filtopt.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 11:29:34 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 12:15:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -120,7 +120,7 @@ void ScFilterOptions::Commit()
     DBG_ERROR("trying to commit changed ScFilterOptions?");
 }
 
-void ScFilterOptions::Notify( const Sequence<rtl::OUString>& aPropertyNames )
+void ScFilterOptions::Notify( const Sequence<rtl::OUString>& /* aPropertyNames */ )
 {
     DBG_ERROR("properties have been changed")
 }
