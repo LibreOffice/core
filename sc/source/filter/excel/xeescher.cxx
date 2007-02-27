392f16c3dd8a4963f12e51601e7a5ec25f7efe15
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 11:24:19 +0000

    INTEGRATION: CWS calcwarnings (1.16.108); FILE MERGED 2007/02/19 15:54:09 nn 1.16.108.2: RESYNC: (1.16-1.18); FILE MERGED 2006/12/01 14:42:25 dr 1.16.108.1: #i69284# remove compiler warnings for wntmsci10

--- sc/source/filter/excel/xeescher.cxx
+++ sc/source/filter/excel/xeescher.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xeescher.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-22 13:15:43 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 12:24:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -866,7 +866,7 @@ void XclExpNote::Save( XclExpStream& rStrm )
                 rStrm.EndRecord();
 
                 pcBuffer += nWriteChars;
-                nCharsLeft -= nWriteChars;
+                nCharsLeft = nCharsLeft - nWriteChars;
             }
         }
         break;
