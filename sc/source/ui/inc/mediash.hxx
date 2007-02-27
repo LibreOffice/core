fd28119d52b4fe3778209305d27b42df73efe7ab
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 12:24:19 +0000

    INTEGRATION: CWS calcwarnings (1.3.322); FILE MERGED 2006/12/14 17:57:12 nn 1.3.322.1: #i69284# warning-free: ui, unxsols4

--- sc/source/ui/inc/mediash.hxx
+++ sc/source/ui/inc/mediash.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mediash.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:36:56 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 13:24:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,7 @@ class ScMediaShell: public ScDrawShell
 public:
 
     TYPEINFO();
-    SFX_DECL_INTERFACE(SCID_MEDIA_SHELL);
+    SFX_DECL_INTERFACE(SCID_MEDIA_SHELL)
 
                 ScMediaShell(ScViewData* pData);
     virtual     ~ScMediaShell();
