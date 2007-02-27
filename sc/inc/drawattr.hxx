6ade818fa74e65bac25d199919477235679c0669
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 10:56:03 +0000

    INTEGRATION: CWS calcwarnings (1.2.324); FILE MERGED 2006/12/12 17:03:03 nn 1.2.324.1: #i69284# warning-free: ui, unxlngi6

--- sc/inc/drawattr.hxx
+++ sc/inc/drawattr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawattr.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 17:37:06 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 11:56:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ public:
         SvxDrawToolItem( const SvxDrawToolItem& rDrawToolItem ) :
                                 SfxEnumItem( rDrawToolItem ){}
 
-        SvxDrawToolItem(USHORT nWhich ) : SfxEnumItem(nWhich){}
+        SvxDrawToolItem(USHORT nWhichP) : SfxEnumItem(nWhichP){}
 
 
     virtual String              GetValueText() const;
