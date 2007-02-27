8b8d786ab71e5ac39f8c9db75cb354ce07c9bae8
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 12:34:51 +0000

    INTEGRATION: CWS calcwarnings (1.4.110); FILE MERGED 2006/12/12 17:03:19 nn 1.4.110.2: #i69284# warning-free: ui, unxlngi6 2006/12/01 08:53:36 nn 1.4.110.1: #i69284# warning-free: ui, wntmsci10

--- sc/source/ui/navipi/navcitem.cxx
+++ sc/source/ui/navipi/navcitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: navcitem.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: kz $ $Date: 2006-07-21 14:13:59 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 13:34:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,17 +53,17 @@
 
 //------------------------------------------------------------------------
 
-ScNavigatorControllerItem::ScNavigatorControllerItem( USHORT          nId,
+ScNavigatorControllerItem::ScNavigatorControllerItem( USHORT          nIdP,
                                                       ScNavigatorDlg& rDlg,
                                                       SfxBindings&    rBindings )
-    :   SfxControllerItem   ( nId, rBindings ),
+    :   SfxControllerItem   ( nIdP, rBindings ),
         rNavigatorDlg       ( rDlg )
 {
 }
 
 //------------------------------------------------------------------------
 
-void __EXPORT ScNavigatorControllerItem::StateChanged( USHORT nSID, SfxItemState eState,
+void __EXPORT ScNavigatorControllerItem::StateChanged( USHORT /* nSID */, SfxItemState /* eState */,
                                                           const SfxPoolItem* pItem )
 {
     switch( GetId() )
