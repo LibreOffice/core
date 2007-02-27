e41fea0f9ee4bdd71dc4e9445074d7b6400287da
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 10:52:00 +0000

    INTEGRATION: CWS calcwarnings (1.2.88); FILE MERGED 2006/12/13 19:18:18 nn 1.2.88.1: #i69284# warning-free: core, unxsols4

--- sc/inc/addincfg.hxx
+++ sc/inc/addincfg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: addincfg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 12:10:41 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 11:52:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,8 +54,10 @@ public:
             GetProperties(const com::sun::star::uno::Sequence< rtl::OUString >& rNames)
                             { return ConfigItem::GetProperties( rNames ); }
 
-    com::sun::star::uno::Sequence< rtl::OUString > GetNodeNames(rtl::OUString& rNode)
-                            { return ConfigItem::GetNodeNames( rNode ); }
+    using ConfigItem::GetNodeNames;
+
+//  com::sun::star::uno::Sequence< rtl::OUString > GetNodeNames(rtl::OUString& rNode)
+//                          { return ConfigItem::GetNodeNames( rNode ); }
 };
 
 
