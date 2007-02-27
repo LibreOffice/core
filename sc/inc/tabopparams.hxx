cd2be8db292e6a5321c57a6de61094d35efe0650
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 10:58:34 +0000

    INTEGRATION: CWS calcwarnings (1.6.324); FILE MERGED 2006/12/13 19:18:20 nn 1.6.324.1: #i69284# warning-free: core, unxsols4

--- sc/inc/tabopparams.hxx
+++ sc/inc/tabopparams.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabopparams.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 18:00:18 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 11:58:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,7 +110,7 @@ struct ScInterpreterTableOpParams
     }
 };
 
-DECLARE_LIST( ScTabOpList, ScInterpreterTableOpParams* );
+DECLARE_LIST( ScTabOpList, ScInterpreterTableOpParams* )
 
 #endif // SC_TABOPPARAMS_HXX
 
