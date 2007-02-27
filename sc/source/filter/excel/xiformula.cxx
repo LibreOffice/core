fb0c07f01ed1212bd496566094f7e97504528947
Vladimir Glazounov <vg@openoffice.org>
2007-02-27 11:26:44 +0000

    INTEGRATION: CWS calcwarnings (1.3.58); FILE MERGED 2006/12/01 14:42:27 dr 1.3.58.1: #i69284# remove compiler warnings for wntmsci10

--- sc/source/filter/excel/xiformula.cxx
+++ sc/source/filter/excel/xiformula.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xiformula.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-27 09:46:14 $
+ *  last change: $Author: vg $ $Date: 2007-02-27 12:26:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -78,8 +78,8 @@ XclImpFmlaCompImpl::XclImpFmlaCompImpl( const XclImpRoot& rRoot ) :
 }
 
 void XclImpFmlaCompImpl::CreateRangeList(
-        ScRangeList& rScRanges, XclFormulaType eType,
-        const XclTokenArray& rXclTokArr, XclImpStream& rStrm )
+        ScRangeList& rScRanges, XclFormulaType /*eType*/,
+        const XclTokenArray& rXclTokArr, XclImpStream& /*rStrm*/ )
 {
     rScRanges.RemoveAll();
 
