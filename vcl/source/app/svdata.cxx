2e960dc9541f0c364d1a11f5e7dab365312470aa
Oliver Bolte <obo@openoffice.org>
2007-03-05 14:23:54 +0000

    INTEGRATION: CWS vcl74 (1.50.154); FILE MERGED 2007/02/09 13:51:18 pl 1.50.154.1: #i73965# clearer assertion, take into account def window, title for def window

--- vcl/source/app/svdata.cxx
+++ vcl/source/app/svdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdata.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:49:52 $
+ *  last change: $Author: obo $ $Date: 2007-03-05 15:23:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -243,6 +243,7 @@ Window* ImplGetDefaultWindow()
         {
             DBG_WARNING( "ImplGetDefaultWindow(): No AppWindow" );
             pSVData->mpDefaultWin = new WorkWindow( 0, WB_DEFAULTWIN );
+            pSVData->mpDefaultWin->SetText( OUString( RTL_CONSTASCII_USTRINGPARAM( "VCL ImplGetDefaultWindow" ) ) );
         }
         Application::GetSolarMutex().release();
     }
