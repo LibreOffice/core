4883da64509a8b6ca42acaa1511db6b8cd758219
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:50:17 +0000

    INTEGRATION: CWS ause076 (1.8.4); FILE MERGED 2007/02/28 11:08:02 hjs 1.8.4.1: #i74812# cleanup *LIB and more

--- lingucomponent/source/thesaurus/libnth/makefile.mk
+++ lingucomponent/source/thesaurus/libnth/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2007-01-31 08:44:05 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:50:17 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -87,8 +87,6 @@ CFLAGSCXX += -I..$/mythes -I..$/..$/lingutil
 CFLAGSCC += -I..$/mythes -I..$/..$/lingutil
 .ENDIF
 
-.IF "$(header)" == ""
-
 EXCEPTIONSFILES=	\
         $(SLO)$/ntprophelp.obj\
         $(SLO)$/nthesimp.obj
@@ -105,7 +103,6 @@ SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
 SHL1STDLIBS= \
         $(CPPULIB) 	 \
         $(CPPUHELPERLIB) 	 \
-        $(UNOLIB)		\
         $(VOSLIB)		\
         $(TOOLSLIB)		\
         $(SVTOOLLIB)	\
@@ -132,12 +129,7 @@ SHL1VERSIONMAP= $(TARGET).map
 
 # build DEF file
 DEF1NAME	 =$(SHL1TARGET)
-#DEF1DEPN    =$(MISC)$/$(SHL1TARGET).flt
-#DEFLIB1NAME =$(TARGET)
-#DEF1DES     =Linguistic2 main DLL
 DEF1EXPORTFILE=	exports.dxp
-
-.ENDIF
 
 # --- Targets ------------------------------------------------------
 
