c1a43f7dc313041a22c57df8517e1f403ed3a239
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:59:04 +0000

    INTEGRATION: CWS ause076 (1.18.8); FILE MERGED 2007/02/28 11:06:16 hjs 1.18.8.1: #i74812# cleanup *LIB and more

--- connectivity/source/drivers/jdbc/makefile.mk
+++ connectivity/source/drivers/jdbc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: rt $ $Date: 2007-01-31 08:35:16 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:59:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -93,7 +93,6 @@ SHL1STDLIBS=\
     $(CPPULIB)					\
     $(CPPUHELPERLIB)			\
     $(VOSLIB)					\
-    $(OSLLIB)					\
     $(SALLIB)					\
     $(JVMACCESSLIB)				\
     $(DBTOOLSLIB)				\
