adedb0d18faaa7a483e1f0e11fb3f6b87bcc5cb7
Oliver Bolte <obo@openoffice.org>
2007-03-09 08:03:34 +0000

    INTEGRATION: CWS ause076 (1.3.734); FILE MERGED 2007/02/28 10:54:07 hjs 1.3.734.1: #i74812# cleanup *LIB and more

--- svx/workben/msview/makefile.mk
+++ svx/workben/msview/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-10-19 11:49:34 $
+#   last change: $Author: obo $ $Date: 2007-03-09 09:03:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -68,7 +68,7 @@ APP1STDLIBS=	$(CPPULIB)			\
                 $(SALLIB)			\
                 $(VOSLIB)			\
                 $(SOTLIB)			\
-                $(SVLIB)			\
+                $(VCLLIB)			\
                 $(SVXLIB)			\
                 $(SFXLIB)
 
@@ -76,6 +76,5 @@ APP1STDLIBS=	$(CPPULIB)			\
 
 # --- Targets ------------------------------------------------------
 
-ALL :   ALLTAR 
-
 .INCLUDE :	target.mk
+
