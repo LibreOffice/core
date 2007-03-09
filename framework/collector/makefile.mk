c107ec8aac280c75c1499bdb7d2340d0b566fd3b
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:43:21 +0000

    INTEGRATION: CWS ause076 (1.3.294); FILE MERGED 2007/02/28 10:44:53 hjs 1.3.294.1: #i74812# cleanup *LIB and more

--- framework/collector/makefile.mk
+++ framework/collector/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 23:54:59 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:43:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,6 @@ TARGET=				framework_collector
 TARGETTYPE=GUI
 
 ENABLE_EXCEPTIONS=	TRUE
-NO_BSYMBOLIC=		TRUE
 
 # --- Settings -----------------------------------------------------
 
@@ -51,7 +50,7 @@ APP1TARGET= 	uicmdxml
 
 APP1OBJS=		$(SLO)$/cmduicollector.obj
 
-APP1STDLIBS=	$(OSLLIB)							\
+APP1STDLIBS=	\
                 $(SALLIB)							\
                 $(VOSLIB)							\
                 $(TOOLSLIB)                         \
