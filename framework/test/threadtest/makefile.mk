c86fc149b331784f5810794ee3ee6c7c2e719eac
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:43:57 +0000

    INTEGRATION: CWS ause076 (1.7.10); FILE MERGED 2007/02/28 10:44:53 hjs 1.7.10.1: #i74812# cleanup *LIB and more

--- framework/test/threadtest/makefile.mk
+++ framework/test/threadtest/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 13:01:57 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:43:57 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,15 +39,10 @@ TARGET=				threadtest
 LIBTARGET=			NO
 ENABLE_EXCEPTIONS=	TRUE
 USE_DEFFILE=		TRUE
-NO_BSYMBOLIC=		TRUE
 
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-
-.IF "$(COM)"=="ICC"
-LINKFLAGS+=/SEGMENTS:1024 /PACKD:32768
-.ENDIF
 
 # --- application: "threadtest" --------------------------------------------------
 
@@ -56,14 +51,15 @@ APP1TARGET= 	threadtest
 APP1OBJS=		$(SLO)$/threadtest.obj							\
                 $(SLO)$/lockhelper.obj
 
+DEPOBJFILES=$(APP1OBJS)
+
 # [ed] 6/16/02 Add the transaction manager library on OS X
 
 APP1STDLIBS=	$(CPPULIB)										\
                 $(CPPUHELPERLIB)								\
-                $(OSLLIB)										\
                 $(SALLIB)										\
                 $(VOSLIB)										\
-                $(SVLIB)
+                $(VCLLIB)
 
 APP1DEPN=		$(INC)$/threadhelp$/threadhelpbase.hxx			\
                 $(INC)$/threadhelp$/transactionbase.hxx			\
@@ -73,10 +69,7 @@ APP1DEPN=		$(INC)$/threadhelp$/threadhelpbase.hxx			\
                 $(INC)$/threadhelp$/readguard.hxx				\
                 $(INC)$/threadhelp$/writeguard.hxx
 
-.IF "$(GUI)"=="WIN" || "$(GUI)"=="OS2"
-APP1DEF=		$(MISC)$/threadtest.def
-.ENDIF
-
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :	target.mk
+
