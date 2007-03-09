e98ce1645e46e24ff2940194b3f7dd54f3801035
Oliver Bolte <obo@openoffice.org>
2007-03-09 08:22:11 +0000

    INTEGRATION: CWS ause076 (1.17.28); FILE MERGED 2007/02/28 10:59:26 hjs 1.17.28.1: #i74812# cleanup *LIB and more

--- vcl/workben/makefile.mk
+++ vcl/workben/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 13:04:59 $
+#   last change: $Author: obo $ $Date: 2007-03-09 09:22:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -61,12 +61,11 @@ APP1STDLIBS=	$(CPPULIB)			\
                 $(UNOTOOLSLIB)	\
                 $(CPPUHELPERLIB)	\
                 $(COMPHELPERLIB)	\
-                $(VCLLIB)			\
                 $(TOOLSLIB) 		\
                 $(SALLIB)			\
                 $(VOSLIB)			\
                 $(SOTLIB)			\
-                $(SVLIB)
+                $(VCLLIB)
 
 # --- Targets ------------------------------------------------------
 
