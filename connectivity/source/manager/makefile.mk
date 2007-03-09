5a9991153c689fa899b5c52d220b2a5cef9a1360
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:59:48 +0000

    INTEGRATION: CWS ause076 (1.10.8); FILE MERGED 2007/02/28 11:06:17 hjs 1.10.8.1: #i74812# cleanup *LIB and more

--- connectivity/source/manager/makefile.mk
+++ connectivity/source/manager/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: rt $ $Date: 2007-01-31 08:35:38 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:59:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -66,7 +66,6 @@ SHL1STDLIBS=\
     $(CPPULIB)					\
     $(CPPUHELPERLIB)			\
     $(VOSLIB)					\
-    $(OSLLIB)					\
     $(SALLIB)
 
 SHL1DEPN=
