aba919db4544ce7746a5854a302009ed80c24cc3
Oliver Bolte <obo@openoffice.org>
2007-03-09 08:22:53 +0000

    INTEGRATION: CWS ause076 (1.8.20); FILE MERGED 2007/02/28 10:42:11 hjs 1.8.20.1: #i74812# cleanup *LIB and more

--- chart2/source/controller/makefile.mk
+++ chart2/source/controller/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: ihi $ $Date: 2006-11-14 15:30:23 $
+#   last change: $Author: obo $ $Date: 2007-03-09 09:22:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -88,7 +88,7 @@ SHL1STDLIBS=	$(CHARTTOOLS)		\
                 $(BASEGFXLIB)		\
                 $(GOODIESLIB)		\
                 $(SALLIB)			\
-                $(SVLIB)			\
+                $(VCLLIB)			\
                 $(SVLLIB)			\
                 $(SVTOOLLIB)		\
                 $(SVXLIB)			\
