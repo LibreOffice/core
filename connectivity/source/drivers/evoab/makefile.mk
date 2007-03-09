04ffd693da755129a7c9a261d61cae675efa90a7
Oliver Bolte <obo@openoffice.org>
2007-03-09 07:58:06 +0000

    INTEGRATION: CWS ause076 (1.5.140); FILE MERGED 2007/02/28 11:06:15 hjs 1.5.140.1: #i74812# cleanup *LIB and more

--- connectivity/source/drivers/evoab/makefile.mk
+++ connectivity/source/drivers/evoab/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-20 01:24:19 $
+#   last change: $Author: obo $ $Date: 2007-03-09 08:58:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -81,13 +81,11 @@ SHL1STDLIBS=\
     $(CPPULIB)					\
     $(CPPUHELPERLIB)			\
     $(VOSLIB)					\
-    $(OSLLIB)					\
     $(SVLLIB)					\
-    $(SVLIB)					\
+    $(VCLLIB)					\
     $(TOOLSLIB)					\
     $(I18NISOLANGLIB)			\
     $(SVTOOLLIB)				\
-    $(SVTLIB)					\
     $(UNOTOOLSLIB)				\
     $(UCBHELPERLIB)				\
     $(SALLIB)					\
