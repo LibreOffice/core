ec1f6bfb2618d6ca20153ab9638d0bae3b250047
Oliver Bolte <obo@openoffice.org>
2007-03-09 08:00:00 +0000

    INTEGRATION: CWS ause076 (1.4.8); FILE MERGED 2007/02/23 11:16:02 hjs 1.4.8.1: #i74721# cleanup obsolete library variables

--- connectivity/workben/iniParser/makefile.mk
+++ connectivity/workben/iniParser/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 13:37:25 $
+#   last change: $Author: obo $ $Date: 2007-03-09 09:00:00 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -63,12 +63,6 @@ APPSTDLIBS=$(SALLIB) \
 APP1TARGET= $(TARGET)
 APP1OBJS=       $(OBJFILES)
 APP1STDLIBS = $(APPSTDLIBS)
-
-
-    
-.IF "$(GUI)"=="WNT"
-APP1STDLIBS+=$(LIBCIMT)
-.ENDIF
 
 # --- Targets ------------------------------------------------------
     
