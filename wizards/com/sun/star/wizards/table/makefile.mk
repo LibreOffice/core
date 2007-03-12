dd2cf1b5603caa772dbd41de6c3a187bd23d2cc1
Oliver Bolte <obo@openoffice.org>
2007-03-12 09:53:35 +0000

    INTEGRATION: CWS sb36 (1.3.60); FILE MERGED 2005/09/22 04:45:32 sb 1.3.60.2: RESYNC: (1.3-1.4); FILE MERGED 2005/07/11 13:29:42 sb 1.3.60.1: #i51803# Added UNO-Type-Path and Class-Path lines to UNO component MANIFEST.MF.

--- wizards/com/sun/star/wizards/table/makefile.mk
+++ wizards/com/sun/star/wizards/table/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 09:41:38 $
+#   last change: $Author: obo $ $Date: 2007-03-12 10:53:35 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -47,6 +47,7 @@ CUSTOMMANIFESTFILE= MANIFEST.MF
 
 JARCLASSDIRS	= com$/sun$/star$/wizards$/table
 JARTARGET		= $(TARGET).jar
+JARCLASSPATH = commonwizards.jar
 
 # --- Files --------------------------------------------------------
 #SRS2NAME =              dbwizres
