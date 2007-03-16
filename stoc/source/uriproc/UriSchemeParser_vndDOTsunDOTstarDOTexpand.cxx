c3187ef1eadba1c36ee1d6b4a8d1fd8221d1f3d9
Oliver Bolte <obo@openoffice.org>
2007-03-16 08:37:12 +0000

    #i10000# precompiled header missing

--- stoc/source/uriproc/UriSchemeParser_vndDOTsunDOTstarDOTexpand.cxx
+++ stoc/source/uriproc/UriSchemeParser_vndDOTsunDOTstarDOTexpand.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: UriSchemeParser_vndDOTsunDOTstarDOTexpand.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-12 10:55:24 $
+ *  last change: $Author: obo $ $Date: 2007-03-16 09:37:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_stoc.hxx"
 
 #include "sal/config.h"
 
