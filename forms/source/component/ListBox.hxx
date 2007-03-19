d60d6478947dbc0d1bbf5d8c031ef50964a5ff27
Oliver Bolte <obo@openoffice.org>
2007-03-19 11:27:25 +0000

    #i10000#  using OBoundControlModel::setPropertyValues added (warning hidden method, solaris)

--- forms/source/component/ListBox.hxx
+++ forms/source/component/ListBox.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ListBox.hxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-09 13:29:39 $
+ *  last change: $Author: obo $ $Date: 2007-03-19 12:27:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -186,6 +186,7 @@ protected:
 
     // prevent method hiding
     using OBoundControlModel::getFastPropertyValue;
+    using OBoundControlModel::setPropertyValues;
 
 protected:
     // OBoundControlModel overridables
