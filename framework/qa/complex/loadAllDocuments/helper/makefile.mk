5b280b9db0641257c4b0deec32baa6b8085cfb55
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:20:53 +0000

    INTEGRATION: CWS fwkmake (1.4.284); FILE MERGED 2007/03/19 10:12:22 cd 1.4.284.1: #i75516# Change PRJNAME to CVS project name to support warning free code

--- framework/qa/complex/loadAllDocuments/helper/makefile.mk
+++ framework/qa/complex/loadAllDocuments/helper/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:02:05 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:20:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #*************************************************************************
 PRJ = ..$/..$/..$/..
 TARGET  = CheckXComponentLoader
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = complex$/loadAllDocuments$/helper
 
 # --- Settings -----------------------------------------------------
