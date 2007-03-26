edfca0a104cc3e36a31346d9c26b70e99e355f53
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:44:05 +0000

    INTEGRATION: CWS mingwport03 (1.3.36); FILE MERGED 2007/03/19 19:19:58 vg 1.3.36.3: RESYNC: (1.4-1.5); FILE MERGED 2007/01/09 00:43:01 vg 1.3.36.2: RESYNC: (1.3-1.4); FILE MERGED 2006/09/13 12:39:39 vg 1.3.36.1: #i53572# MinGW port

--- chart2/source/view/makefile.mk
+++ chart2/source/view/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 09:23:05 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:44:05 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -94,7 +94,9 @@ SHL1STDLIBS=	$(CHARTTOOLS)		\
 SHL1DEF=		$(MISC)$/$(SHL1TARGET).def
 
 #Specifies the library name to parse for symbols. For Win32 only.
-#DEFLIB1NAME=	_$(TARGET)
+.IF "$(GUI)$(COM)"=="WNTGCC"
+DEFLIB1NAME=	$(TARGET)
+.ENDIF
 
 #A file of symbols to export.
 DEF1EXPORTFILE=	$(PRJ)$/source$/inc$/exports.dxp
