e46d4c0053e0d78359d152c3e7a9a018bc2c59ab
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:20:22 +0000

    INTEGRATION: CWS fwkmake (1.3.284); FILE MERGED 2007/03/19 10:12:21 cd 1.3.284.1: #i75516# Change PRJNAME to CVS project name to support warning free code

--- framework/qa/complex/imageManager/interfaces/makefile.mk
+++ framework/qa/complex/imageManager/interfaces/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 01:00:34 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:20:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ = ..$/..$/..$/..
 TARGET  = ImageManager
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = imageManager$/interfaces
 
 # --- Settings -----------------------------------------------------
