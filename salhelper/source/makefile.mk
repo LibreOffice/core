4a8b906ead788181027bed8acdba5cd8385f6c98
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 14:39:51 +0000

    INTEGRATION: CWS mingwport03 (1.24.4); FILE MERGED 2007/03/20 10:48:10 vg 1.24.4.2: RESYNC MERGED 1.24 1.25 Everything below this line will be added to the revision comment. 2006/09/07 10:45:35 vg 1.24.4.1: #i53572# MinGW port

--- salhelper/source/makefile.mk
+++ salhelper/source/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.25 $
+#   $Revision: 1.26 $
 #
-#   last change: $Author: rt $ $Date: 2007-01-31 08:45:24 $
+#   last change: $Author: vg $ $Date: 2007-03-26 15:39:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -87,6 +87,8 @@ SHL1VERSIONMAP=gcc3_linux_intel.map
 .ELIF "$(OS)$(CPU)$(COMNAME)"=="LINUXSgcc3"
 SHL1VERSIONMAP=gcc3_linux_intel.map
 .ELIF "$(OS)$(CPU)$(COMNAME)"=="MACOSXIgcc3"
+SHL1VERSIONMAP=gcc3_linux_intel.map
+.ELIF "$(GUI)$(COM)"=="WNTGCC"
 SHL1VERSIONMAP=gcc3_linux_intel.map
 .ENDIF
 
