b8248948f0a79c06cd98cf7c7bafddd084a615d2
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 12:25:19 +0000

    INTEGRATION: CWS mingwport03 (1.31.46); FILE MERGED 2007/01/08 19:23:26 vg 1.31.46.3: RESYNC: (1.33-1.34); FILE MERGED 2006/11/08 17:14:11 vg 1.31.46.2: RESYNC: (1.31-1.33); FILE MERGED 2006/09/06 15:32:05 vg 1.31.46.1: #i53572# MinGW port

--- sw/source/filter/html/htmlatr.cxx
+++ sw/source/filter/html/htmlatr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlatr.cxx,v $
  *
- *  $Revision: 1.34 $
+ *  $Revision: 1.35 $
  *
- *  last change: $Author: rt $ $Date: 2006-12-01 15:53:25 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 13:25:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -222,7 +222,7 @@
  * diese Section und die dazugeherigen Tabellen muessen in folgenden Files
  * gepflegt werden: rtf\rtfatr.cxx, sw6\sw6atr.cxx, w4w\w4watr.cxx
  */
-#if !defined(UNX) && !defined(MSC) && !defined(PPC) && !defined(CSET) && !defined(__MWERKS__) && !defined(WTC)
+#if !defined(UNX) && !defined(MSC) && !defined(PPC) && !defined(CSET) && !defined(__MWERKS__) && !defined(WTC) && !defined(__MINGW32__)
 
 #define ATTRFNTAB_SIZE 130
 #if ATTRFNTAB_SIZE != POOLATTR_END - POOLATTR_BEGIN
