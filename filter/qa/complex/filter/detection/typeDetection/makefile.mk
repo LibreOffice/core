6b07569367df93e8203fe53a359327441ed1ca4c
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:17:18 +0000

    INTEGRATION: CWS fwkmake (1.7.10); FILE MERGED 2007/03/19 09:49:26 as 1.7.10.1: #i75515# set right PRJNAME in makefiles

--- filter/qa/complex/filter/detection/typeDetection/makefile.mk
+++ filter/qa/complex/filter/detection/typeDetection/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:45:55 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:17:18 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,8 @@
 #
 #*************************************************************************
 PRJ = ..$/..$/..$/..$/..
+PRJNAME = filter
 TARGET  = TypeDetection
-PRJNAME = $(TARGET)
 PACKAGE = complex$/filter$/detection$/typeDetection
 
 # --- Settings -----------------------------------------------------
