1c0a716ea8421a32036b9840f7dfdf16359c20f7
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:57:07 +0000

    INTEGRATION: CWS mingwport03 (1.10.24); FILE MERGED 2007/01/09 01:37:05 vg 1.10.24.2: RESYNC: (1.10-1.11); FILE MERGED 2006/09/13 14:16:36 vg 1.10.24.1: #i53572# MinGW port

--- goodies/source/filter.vcl/etiff/makefile.mk
+++ goodies/source/filter.vcl/etiff/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: ihi $ $Date: 2006-11-14 16:12:46 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:57:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,9 @@ SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB) $(SVTOOLLIB)
 SHL1LIBS=		$(SLB)$/etiff.lib
 
 .IF "$(GUI)" != "UNX"
+.IF "$(COM)" != "GCC"
 SHL1OBJS=		$(SLO)$/etiff.obj
+.ENDIF
 .ENDIF
 
 SHL1VERSIONMAP=exports.map
