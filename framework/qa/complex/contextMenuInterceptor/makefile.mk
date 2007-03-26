2dcd50469ecce20b8aa9a6a5075213678290defa
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:18:40 +0000

    INTEGRATION: CWS fwkmake (1.3.12); FILE MERGED 2007/03/19 10:12:18 cd 1.3.12.1: #i75516# Change PRJNAME to CVS project name to support warning free code

--- framework/qa/complex/contextMenuInterceptor/makefile.mk
+++ framework/qa/complex/contextMenuInterceptor/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:16:20 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:18:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ = ..$/..$/..
 TARGET  = ContextMenuInterceptor
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = contextMenuInterceptor
 
 # --- Settings -----------------------------------------------------
