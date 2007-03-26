0ef4226f027ec60eaf6b9fe12891503e8e725950
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 14:08:51 +0000

    INTEGRATION: CWS mingwport03 (1.2.42); FILE MERGED 2006/09/21 16:12:51 vg 1.2.42.1: #i53572# MinGW port

--- dtrans/source/win32/mtaole/makefile.mk
+++ dtrans/source/win32/mtaole/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 18:29:25 $
+#   last change: $Author: vg $ $Date: 2007-03-26 15:08:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,6 +37,7 @@ PRJ=..$/..$/..
 PRJNAME=dtrans
 TARGET=mtaolecb
 LIBTARGET=NO
+ENABLE_EXCEPTIONS=YES
 
 # --- Settings ----------------------------------
 
