5ff1de6e316989a78cfcaaf6900d420e29712277
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:17:02 +0000

    INTEGRATION: CWS mingwport03 (1.179.10); FILE MERGED 2007/03/19 17:24:47 vg 1.179.10.4: RESYNC: (1.186-1.191); FILE MERGED 2007/01/08 23:38:53 vg 1.179.10.3: RESYNC: (1.185-1.186); FILE MERGED 2006/11/07 16:55:31 vg 1.179.10.2: RESYNC: (1.179-1.185); FILE MERGED 2006/10/24 15:26:44 vg 1.179.10.1: #i53572# MinGW port

--- solenv/inc/target.mk
+++ solenv/inc/target.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: target.mk,v $
 #
-#   $Revision: 1.192 $
+#   $Revision: 1.193 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 09:05:39 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:17:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -812,6 +812,7 @@ LIB9TARGETN=$(LIB9TARGET)
 .ENDIF
 
 .IF "$(GUI)"=="WNT"
+.IF "$(COM)"!="GCC"
 LIB1ARCHIV=
 LIB2ARCHIV=
 LIB3ARCHIV=
@@ -821,6 +822,7 @@ LIB6ARCHIV=
 LIB7ARCHIV=
 LIB8ARCHIV=
 LIB9ARCHIV=
+.ENDIF
 .ENDIF			# "$(GUI)"=="WNT"
 
 .IF "$(RESLIB1NAME)" != ""
