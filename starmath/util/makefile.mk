8ce25f3e2eecf391d35cb6515b0f5563db1f91f7
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:13:27 +0000

    INTEGRATION: CWS mingwport03 (1.17.124); FILE MERGED 2006/09/07 10:38:51 vg 1.17.124.1: #i53572# MinGW port

--- starmath/util/makefile.mk
+++ starmath/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 15:16:11 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:13:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -104,7 +104,9 @@ SHL1DEPN=	makefile.mk
 SHL1LIBS=   $(SLB)$/starmath.lib
 
 .IF "$(GUI)" != "UNX"
+.IF "$(GUI)$(COM)" != "WNTGCC"
 SHL1OBJS=   $(SLO)$/smdll.obj
+.ENDIF
 .ENDIF # ! UNX
 
 .IF "$(GUI)" == "WNT"
