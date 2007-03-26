b07990907e455850d51ee07aa9426a929d496bc5
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 14:06:18 +0000

    INTEGRATION: CWS mingwport03 (1.6.16); FILE MERGED 2006/11/08 09:42:40 vg 1.6.16.2: RESYNC: (1.6-1.7); FILE MERGED 2006/09/13 14:37:46 vg 1.6.16.1: #i53572# MinGW port

--- dtrans/source/win32/dtobj/APNDataObject.cxx
+++ dtrans/source/win32/dtobj/APNDataObject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: APNDataObject.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:59:45 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 15:06:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,6 +49,9 @@
 #endif
 
 #include <systools/win32/comtools.hxx>
+#ifdef __MINGW32__
+#define __uuidof(I) IID_##I
+#endif
 
 //------------------------------------------------------------------------
 // defines
