88fc4413af8d4ff0009e3401634d3d68db368221
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 14:08:25 +0000

    INTEGRATION: CWS mingwport03 (1.2.42); FILE MERGED 2006/09/21 16:12:06 vg 1.2.42.1: #i53572# MinGW port

--- dtrans/source/win32/misc/makefile.mk
+++ dtrans/source/win32/misc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 18:28:52 $
+#   last change: $Author: vg $ $Date: 2007-03-26 15:08:25 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,6 +39,7 @@ PRJNAME=dtrans
 TARGET=dtutils
 LIBTARGET=NO
 USE_BOUNDCHK=
+ENABLE_EXCEPTIONS=YES
 
 .IF "$(USE_BOUNDCHK)"=="TR"
 bndchk=tr
