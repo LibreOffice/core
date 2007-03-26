56dec3c607876e209167a43d94ccf2a941b85766
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:19:03 +0000

    INTEGRATION: CWS fwkmake (1.6.12); FILE MERGED 2007/03/19 10:12:19 cd 1.6.12.1: #i75516# Change PRJNAME to CVS project name to support warning free code

--- framework/qa/complex/dispatches/makefile.mk
+++ framework/qa/complex/dispatches/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:16:34 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:19:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #*************************************************************************
 PRJ = ..$/..$/..
 TARGET  = checkdispatchapi
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = complex$/dispatches
 
 # --- Settings -----------------------------------------------------
