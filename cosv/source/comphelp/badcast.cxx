de26ce00319461089cb0d7fbd498bf89d2bcb71c
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 12:54:56 +0000

    INTEGRATION: CWS mingwport03 (1.2.22); FILE MERGED 2006/09/07 09:39:09 vg 1.2.22.1: #i53572# MinGW port

--- cosv/source/comphelp/badcast.cxx
+++ cosv/source/comphelp/badcast.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: badcast.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:02:36 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 13:54:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
    due to problems with WNT-STL-headers.
 */
 
-#ifdef WNT
+#if defined(WNT) && !defined(__MINGW32__)
 
 #define _NTSDK
 #include<typeinfo>
