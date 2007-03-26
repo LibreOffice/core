b8064dee1335642f80f899fef7b6b7903d4f89f1
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:25:25 +0000

    INTEGRATION: CWS mingwport03 (1.3.142); FILE MERGED 2007/03/19 17:38:52 vg 1.3.142.2: RESYNC: (1.3-1.4); FILE MERGED 2006/09/07 11:06:32 vg 1.3.142.1: #i53572# MinGW port

--- sal/qa/rtl/bootstrap/makefile.mk
+++ sal/qa/rtl/bootstrap/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 15:58:30 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:25:25 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -84,7 +84,7 @@ APP2OBJS=$(OBJ2FILES)
 # APP2STDLIBS=$(LB)$/libsal.so
 # .ENDIF
 # .IF "$(GUI)" == "WNT"
-# APP2STDLIBS=kernel32.lib $(LB)$/isal.lib
+# APP2STDLIBS=$(KERNEL32LIB) $(LB)$/isal.lib
 # .ENDIF
 APP2STDLIBS=$(SALLIB)
 #------------------------------- All object files -------------------------------
