200853abb68a203550a72eb4f923e5a31eb9225d
Ivo Hinkelmann <ihi@openoffice.org>
2007-03-26 11:19:43 +0000

    INTEGRATION: CWS fwkmake (1.4.12); FILE MERGED 2007/03/19 10:12:20 cd 1.4.12.1: #i75516# Change PRJNAME to CVS project name to support warning free code

--- framework/qa/complex/framework/autosave/makefile.mk
+++ framework/qa/complex/framework/autosave/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:17:01 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:19:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
 #*************************************************************************
 PRJ = ..$/..$/..$/..
 TARGET  = AutoSave
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = complex$/framework$/autosave
 
 # --- Settings -----------------------------------------------------
--- framework/qa/complex/framework/recovery/makefile.mk
+++ framework/qa/complex/framework/recovery/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:17:14 $
+#   last change: $Author: ihi $ $Date: 2007-03-26 12:19:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 PRJ = ..$/..$/..$/..
 TARGET  = RecoveryTest
-PRJNAME = $(TARGET)
+PRJNAME = framework
 PACKAGE = complex$/framework$/recovery
 
 # --- Settings -----------------------------------------------------
