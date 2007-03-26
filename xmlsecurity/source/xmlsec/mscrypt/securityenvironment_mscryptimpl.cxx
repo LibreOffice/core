86754f8fde0c58b1932e7b5e19be2a0302feb44f
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 12:13:33 +0000

    INTEGRATION: CWS mingwport03 (1.12.8); FILE MERGED 2006/11/08 10:58:59 vg 1.12.8.2: RESYNC: (1.12-1.13); FILE MERGED 2006/09/07 08:44:58 vg 1.12.8.1: #i53572# MinGW port

--- xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.cxx
+++ xmlsecurity/source/xmlsec/mscrypt/securityenvironment_mscryptimpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: securityenvironment_mscryptimpl.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 14:44:02 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 13:13:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -431,7 +431,7 @@ void SecurityEnvironment_MSCryptImpl :: rejectPriKey( HCRYPTKEY aPriKey ) throw(
     }
 }
 
-HCRYPTKEY SecurityEnvironment_MSCryptImpl :: getPriKey( unsigned int position ) throw( SecurityException , RuntimeException ) {
+HCRYPTKEY SecurityEnvironment_MSCryptImpl :: getPriKey( unsigned int position ) throw( Exception , RuntimeException ) {
     HCRYPTKEY prikey ;
     std::list< HCRYPTKEY >::iterator keyIt ;
     unsigned int pos ;
