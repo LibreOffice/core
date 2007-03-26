c2b38053cad5c7d0f8ffda3d65f52f60166fe0e4
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:12:03 +0000

    INTEGRATION: CWS mingwport03 (1.5.22); FILE MERGED 2007/03/19 17:03:29 vg 1.5.22.2: RESYNC: (1.5-1.6); FILE MERGED 2006/09/18 15:19:10 vg 1.5.22.1: #i53572# MinGW port

--- setup_native/source/win32/customactions/shellextensions/startmenuicon.cxx
+++ setup_native/source/win32/customactions/shellextensions/startmenuicon.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: startmenuicon.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-25 11:56:23 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 14:12:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 #include <tchar.h>
 #include <string>
 
-#ifdef _WIN32_WINNT
+#if defined(_WIN32_WINNT) && !defined(__MINGW32__)
 #error YES
 #endif
 
