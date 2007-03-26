6f198137611f0aa3f5216bed7d1507b3aedc44d4
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:35:42 +0000

    INTEGRATION: CWS mingwport03 (1.5.164); FILE MERGED 2007/03/19 19:01:04 vg 1.5.164.3: RESYNC: (1.6-1.7); FILE MERGED 2006/11/08 12:24:30 vg 1.5.164.2: RESYNC: (1.5-1.6); FILE MERGED 2006/09/07 15:07:18 vg 1.5.164.1: #i53572# MinGW port

--- xmloff/source/transform/makefile.mk
+++ xmloff/source/transform/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 09:19:23 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:35:42 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -90,7 +90,7 @@ SHL1STDLIBS = \
     $(SALHELPERLIB) \
     $(SALLIB) \
     $(TOOLSLIB)
-.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC" || "$(COM)"=="GCC"
     SHL1STDLIBS += -lxo$(OFFICEUPD)$(DLLPOSTFIX)
 .ELSE
     SHL1STDLIBS += ixo.lib
