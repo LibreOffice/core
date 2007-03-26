a89c7bdf0d60d8dadcd27d037a709efb4a8fe3a7
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 13:00:19 +0000

    INTEGRATION: CWS mingwport03 (1.7.36); FILE MERGED 2007/01/08 20:20:46 vg 1.7.36.3: RESYNC: (1.8-1.9); FILE MERGED 2006/11/07 13:11:13 vg 1.7.36.2: RESYNC: (1.7-1.8); FILE MERGED 2006/09/07 10:22:07 vg 1.7.36.1: #i53572# MinGW port

--- connectivity/source/drivers/mozab/bootstrap/makefile.mk
+++ connectivity/source/drivers/mozab/bootstrap/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: vg $ $Date: 2006-11-21 17:00:42 $
+#   last change: $Author: vg $ $Date: 2007-03-26 14:00:19 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -96,6 +96,21 @@ MOZINC = . -I.. -I..$/mozillasrc -I$(MOZ_INC)  -I$(MOZ_INC)$/nspr -I$(MOZ_INC)$/
         -I$(MOZ_INC)$/xpcom_obsolete -I$(MOZ_INC)$/content
         
 .IF "$(GUI)" == "WNT"
+.IF "$(COM)" == "GCC"
+INCPOST += $(MOZINC)
+CDEFS +=    -DWINVER=0x400 -DMOZILLA_CLIENT \
+        -DNS_NET_FILE -DCookieManagement -DSingleSignon -DClientWallet \
+            -DTRACING -DXP_PC -DXP_WIN -DXP_WIN32 -DHW_THREADS \
+            -DNS_MT_SUPPORTED -DNETLIB_THREAD \
+            -DOJI -DWIN32 -D_X86_ -D_WINDOWS \
+        -DMOZ_XUL -DMOZ_REFLOW_PERF -DMOZ_REFLOW_PERF_DSP \
+        -DNSPR20 -DOS_HAS_DLL -DNO_JNI_STUBS \
+        -DNETSCAPE -DMOZILLA_CLIENT -DJS_THREADSAFE -DNECKO -DINCLUDE_XUL
+CFLAGSCXX += \
+            -fno-rtti -Wall -Wconversion -Wpointer-arith \
+            -Wcast-align -Woverloaded-virtual -Wsynth \
+            -Wno-long-long
+.ELSE
 .IF "$(DBG_LEVEL)" == "0"
 INCPRE += $(MOZINC)
 CDEFS +=    -DWINVER=0x400 -DMOZILLA_CLIENT \
@@ -120,6 +135,7 @@ CDEFS +=    -DWINVER=0x400 -DMOZILLA_CLIENT \
         -DNETSCAPE -DMOZILLA_CLIENT -DJS_THREADSAFE -DNECKO -DINCLUDE_XUL	\
         -UDEBUG
 CFLAGS +=   -Zi -GR- -W3 -Gy -MDd -UDEBUG
+.ENDIF
 .ENDIF
 .ENDIF
 .IF "$(GUI)" == "UNX"
