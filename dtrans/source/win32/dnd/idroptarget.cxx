26beaf08f507b5492e93c98ba12b1357431a2699
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 14:05:16 +0000

    INTEGRATION: CWS mingwport03 (1.4.16); FILE MERGED 2006/11/08 09:41:07 vg 1.4.16.2: RESYNC: (1.4-1.5); FILE MERGED 2006/09/13 14:37:46 vg 1.4.16.1: #i53572# MinGW port

--- dtrans/source/win32/dnd/idroptarget.cxx
+++ dtrans/source/win32/dnd/idroptarget.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idroptarget.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:58:22 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 15:05:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,6 +37,11 @@
 #include "precompiled_dtrans.hxx"
 #include "idroptarget.hxx"
 #include <rtl/unload.h>
+
+#ifdef __MINGW32__
+#define __uuidof(I) IID_##I
+#endif
+
 extern rtl_StandardModuleCount g_moduleCount;
 
 IDropTargetImpl::IDropTargetImpl( DropTarget& pTarget): m_nRefCount( 0),
