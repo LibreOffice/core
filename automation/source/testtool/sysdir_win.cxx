72079efbf7b3ff1a38344fa934611d8421d74f2c
Vladimir Glazounov <vg@openoffice.org>
2007-03-26 12:42:22 +0000

    INTEGRATION: CWS mingwport03 (1.4.58); FILE MERGED 2006/11/07 11:28:41 vg 1.4.58.2: RESYNC: (1.4-1.6); FILE MERGED 2006/09/07 09:06:24 vg 1.4.58.1: #i53572# MinGW port

--- automation/source/testtool/sysdir_win.cxx
+++ automation/source/testtool/sysdir_win.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sysdir_win.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 11:19:24 $
+ *  last change: $Author: vg $ $Date: 2007-03-26 13:42:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -92,7 +92,7 @@ UniString _SHGetSpecialFolder( int nFolderID )
         lpFolderW = ALLOC( WCHAR, 16000 );
 
         SHGetPathFromIDListW( pidl, lpFolderW );
-        aFolder = UniString( lpFolderW );
+        aFolder = UniString( reinterpret_cast<const sal_Unicode*>(lpFolderW) );
 
         FREE( lpFolderW );
         _SHFree( pidl );
