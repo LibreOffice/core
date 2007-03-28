c9b970fdb79c6adf514036e9c597e59c6b4dd94c
Rüdiger Timm <rt@openoffice.org>
2007-03-28 06:33:54 +0000

    #i10000# header markers and pch files

--- sfx2/source/doc/ownsubfilterservice.cxx
+++ sfx2/source/doc/ownsubfilterservice.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ownsubfilterservice.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2007-03-26 11:18:50 $
+ *  last change: $Author: rt $ $Date: 2007-03-28 07:33:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sfx2.hxx"
 
 #ifndef _COM_SUN_STAR_FRAME_DOUBLEINITIALIZATIONEXCEPTION_HPP_
 #include <com/sun/star/frame/DoubleInitializationException.hpp>
