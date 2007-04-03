4b7cb37d5e805f0b6dcc2b6ba2320569e4bb5652
Rüdiger Timm <rt@openoffice.org>
2007-04-03 14:47:04 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2007/01/29 15:27:00 af 1.1.2.5: #68075# Resolved resync problems. 2007/01/22 14:10:03 af 1.1.2.4: #i68075 Introduction of XResourceId interface. 2006/09/07 14:21:10 af 1.1.2.3: #i68075# Transition from URL to ResourceId. 2006/08/28 14:32:28 af 1.1.2.2: #i68075# Fixed compiler problems. 2006/08/16 07:44:25 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/framework/configuration/GenericConfigurationChangeRequest.cxx
@@ -0,0 +1,112 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: GenericConfigurationChangeRequest.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 15:47:04 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "GenericConfigurationChangeRequest.hxx"
+
+#include "framework/FrameworkHelper.hxx"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+
+namespace sd { namespace framework {
+
+GenericConfigurationChangeRequest::GenericConfigurationChangeRequest (
+    const Reference<XResourceId>& rxResourceId,
+    Mode eMode) throw(::com::sun::star::lang::IllegalArgumentException)
+    : GenericConfigurationChangeRequestInterfaceBase(MutexOwner::maMutex),
+      mxResourceId(rxResourceId),
+      meMode(eMode)
+{
+    if ( ! rxResourceId.is() || rxResourceId->getResourceURL().getLength()==0)
+        throw ::com::sun::star::lang::IllegalArgumentException();
+}
+
+
+
+
+GenericConfigurationChangeRequest::~GenericConfigurationChangeRequest (void) throw()
+{
+}
+
+
+
+
+void SAL_CALL GenericConfigurationChangeRequest::execute (
+    const Reference<XConfiguration>& rxConfiguration)
+    throw (RuntimeException)
+{
+    if (rxConfiguration.is())
+    {
+        switch (meMode)
+        {
+            case Activation:
+                rxConfiguration->addResource(mxResourceId);
+                break;
+
+            case Deactivation:
+                rxConfiguration->removeResource(mxResourceId);
+                break;
+        }
+    }
+}
+
+
+
+
+OUString SAL_CALL GenericConfigurationChangeRequest::getName (void)
+    throw (RuntimeException)
+{
+    return OUString::createFromAscii("GenericConfigurationChangeRequest ")
+        + OUString::createFromAscii(meMode==Activation ? "activate " : "deactivate ")
+        + FrameworkHelper::ResourceIdToString(mxResourceId);
+}
+
+
+
+
+void SAL_CALL GenericConfigurationChangeRequest::setName (const OUString& rsName)
+    throw (RuntimeException)
+{
+    (void)rsName;
+    // Ignored.
+}
+
+} } // end of namespace sd::framework
+
