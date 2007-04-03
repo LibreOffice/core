ef02c1b508216a5451ff79a9d35aa0093185f584
Rüdiger Timm <rt@openoffice.org>
2007-04-03 15:03:19 +0000

    INTEGRATION: CWS components1 (1.6.96); FILE MERGED 2006/09/07 14:51:50 af 1.6.96.2: #i68075# Transition from URL to ResourceId. 2006/08/22 11:12:29 af 1.6.96.1: #i68075# Transition to new drawing framework.

--- sd/source/ui/inc/GraphicViewShellBase.hxx
+++ sd/source/ui/inc/GraphicViewShellBase.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GraphicViewShellBase.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-03-21 17:22:55 $
+ *  last change: $Author: rt $ $Date: 2007-04-03 16:03:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,10 +62,7 @@ public:
     virtual void Execute (SfxRequest& rRequest);
 
 protected:
-    /** The ViewTabBar is not supported so this factory method always
-        returns <NULL/>.
-    */
-    virtual ViewTabBar* CreateViewTabBar (void);
+    virtual void InitializeFramework (void);
 };
 
 } // end of namespace sd
