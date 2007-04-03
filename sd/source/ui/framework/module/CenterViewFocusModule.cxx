0afc9e9816795838f087d201ed4c76fd304a41b5
Rüdiger Timm <rt@openoffice.org>
2007-04-03 14:50:35 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2007/03/07 15:44:03 af 1.1.2.9: #i75051# Using getFirstViewForAnchor() instead of getView(). 2007/01/29 15:27:01 af 1.1.2.8: #68075# Resolved resync problems. 2007/01/22 15:12:57 af 1.1.2.7: #i68075 Introduction of XResourceId interface. 2006/11/01 13:15:16 af 1.1.2.6: #i70929# Adaption to changes in ViewShellBase. 2006/10/09 13:07:16 af 1.1.2.5: #i68075# Listening for ResourceActivationEvent. 2006/09/07 16:26:11 af 1.1.2.4: #i68075# Transition from URL to ResourceId. 2006/09/07 14:46:31 af 1.1.2.3: #i68075# Transition from URL to ResourceId. 2006/08/28 14:32:28 af 1.1.2.2: #i68075# Fixed compiler problems. 2006/08/18 14:06:59 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/framework/module/CenterViewFocusModule.cxx
@@ -0,0 +1,206 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: CenterViewFocusModule.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 15:50:35 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "CenterViewFocusModule.hxx"
+
+#include "framework/ConfigurationController.hxx"
+#include "framework/FrameworkHelper.hxx"
+#include "framework/ViewShellWrapper.hxx"
+
+#include "DrawController.hxx"
+#include "ViewShellBase.hxx"
+#include "ViewShellManager.hxx"
+#include "strings.hrc"
+#include "sdresid.hxx"
+
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XCONTROLLERMANAGER_HPP_
+#include <com/sun/star/drawing/framework/XControllerManager.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XCONFIGURATIONCONTROLLER_HPP_
+#include <com/sun/star/drawing/framework/XConfigurationController.hpp>
+#endif
+
+#include <toolkit/awt/vclxdevice.hxx>
+#include <sfx2/viewfrm.hxx>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+using ::sd::framework::FrameworkHelper;
+
+
+namespace sd { namespace framework {
+
+//===== CenterViewFocusModule ====================================================
+
+CenterViewFocusModule::CenterViewFocusModule (Reference<frame::XController>& rxController)
+    : CenterViewFocusModuleInterfaceBase(MutexOwner::maMutex),
+      mbValid(false),
+      mxViewController(),
+      mxConfigurationController(),
+      mpBase(NULL),
+      mbNewViewCreated(false)
+{
+    Reference<XControllerManager> xControllerManager (rxController, UNO_QUERY);
+    if (xControllerManager.is())
+    {
+        mxViewController = xControllerManager->getViewController();
+        mxConfigurationController = xControllerManager->getConfigurationController();
+
+        // Tunnel through the controller to obtain a ViewShellBase.
+        Reference<lang::XUnoTunnel> xTunnel (rxController, UNO_QUERY);
+        if (xTunnel.is())
+        {
+            ::sd::DrawController* pController = reinterpret_cast<sd::DrawController*>(
+                xTunnel->getSomething(sd::DrawController::getUnoTunnelId()));
+            if (pController != NULL)
+                mpBase = pController->GetViewShellBase();
+        }
+
+        // Check, if all required objects do exist.
+        if (mxViewController.is()
+            && mxConfigurationController.is()
+            && mpBase!=NULL)
+        {
+            mbValid = true;
+        }
+    }
+
+    if (mbValid)
+    {
+        mxConfigurationController->addConfigurationChangeListener(
+            this,
+            FrameworkHelper::msConfigurationUpdateEndEvent,
+            Any());
+        mxConfigurationController->addConfigurationChangeListener(
+            this,
+            FrameworkHelper::msResourceActivationEvent,
+            Any());
+    }
+}
+
+
+
+
+CenterViewFocusModule::~CenterViewFocusModule (void)
+{
+}
+
+
+
+
+void SAL_CALL CenterViewFocusModule::disposing (void)
+{
+    if (mxConfigurationController.is())
+        mxConfigurationController->removeConfigurationChangeListener(this);
+
+    mbValid = false;
+    mxViewController = NULL;
+    mxConfigurationController = NULL;
+    mpBase = NULL;
+}
+
+
+
+
+void SAL_CALL CenterViewFocusModule::notifyConfigurationChange (
+    const ConfigurationChangeEvent& rEvent)
+    throw (RuntimeException)
+{
+    if (mbValid)
+    {
+        if (rEvent.Type.equals(FrameworkHelper::msConfigurationUpdateEndEvent))
+        {
+            ConfigurationUpdateEnd();
+        }
+        else if (rEvent.Type.equals(FrameworkHelper::msResourceActivationEvent))
+        {
+            if (rEvent.ResourceId->getResourceURL().match(FrameworkHelper::msViewURLPrefix))
+                mbNewViewCreated = true;
+        }
+    }
+}
+
+
+
+
+void CenterViewFocusModule::ConfigurationUpdateEnd (void)
+{
+    if (mbNewViewCreated)
+    {
+        mbNewViewCreated = false;
+        // Make the center pane the active one.  Tunnel through the
+        // controller to obtain a ViewShell pointer.
+        Reference<XView> xView (mxViewController->getFirstViewForAnchor(
+            FrameworkHelper::CreateResourceId(FrameworkHelper::msCenterPaneURL)));
+        Reference<lang::XUnoTunnel> xTunnel (xView, UNO_QUERY);
+        if (xTunnel.is() && mpBase!=NULL)
+        {
+            ViewShellWrapper* pViewShellWrapper = reinterpret_cast<ViewShellWrapper*>(
+                xTunnel->getSomething(ViewShellWrapper::getUnoTunnelId()));
+            if (pViewShellWrapper != NULL)
+            {
+                ::boost::shared_ptr<ViewShell> pViewShell = pViewShellWrapper->GetViewShell();
+                if (pViewShell.get() != NULL)
+                    mpBase->GetViewShellManager().MoveToTop(*pViewShell);
+            }
+        }
+    }
+}
+
+
+
+
+void SAL_CALL CenterViewFocusModule::disposing (
+    const lang::EventObject& rEvent)
+    throw (RuntimeException)
+{
+    if (mxConfigurationController.is())
+        if (rEvent.Source == mxConfigurationController)
+        {
+            mbValid = false;
+            mxConfigurationController = NULL;
+            mxViewController = NULL;
+            mpBase = NULL;
+        }
+}
+
+
+
+} } // end of namespace sd::framework
