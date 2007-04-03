485f66a623367b6b5117aa8d90c0ec8ac3e64bec
Rüdiger Timm <rt@openoffice.org>
2007-04-03 15:03:34 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2006/08/18 14:56:46 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/inc/ImpressViewShellBase.hxx
@@ -0,0 +1,70 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ImpressViewShellBase.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 16:03:34 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SD_IMPRESS_VIEW_SHELL_BASE_HXX
+#define SD_IMPRESS_VIEW_SHELL_BASE_HXX
+
+#include "ViewShellBase.hxx"
+
+
+namespace sd {
+
+/** This class implements a few features that exist only for the Impress
+    application.
+*/
+class ImpressViewShellBase
+    : public ViewShellBase
+{
+public:
+    TYPEINFO();
+    SFX_DECL_VIEWFACTORY(ImpressViewShellBase);
+
+    /** This constructor is used by the view factory of the SFX
+        macros.
+    */
+    ImpressViewShellBase (SfxViewFrame *pFrame, SfxViewShell* pOldShell);
+    virtual ~ImpressViewShellBase (void);
+
+    /** Callback function for general slot calls.
+    */
+    virtual void Execute (SfxRequest& rRequest);
+
+protected:
+    virtual void InitializeFramework (void);
+};
+
+} // end of namespace sd
+
+#endif
