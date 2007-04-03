3d477753a94bf034e5d145a9adb2634e7177db75
Rüdiger Timm <rt@openoffice.org>
2007-04-03 14:54:53 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2007/03/07 16:02:55 af 1.1.2.8: #i68075# Cleanup. 2007/01/29 15:27:02 af 1.1.2.7: #68075# Resolved resync problems. 2007/01/24 17:25:01 af 1.1.2.6: #i68075# Introduction of XResourceId interface. 2007/01/22 15:28:56 af 1.1.2.5: #i68075 Introduction of XResourceId interface. 2006/09/07 16:26:11 af 1.1.2.4: #i68075# Transition from URL to ResourceId. 2006/09/07 14:51:47 af 1.1.2.3: #i68075# Transition from URL to ResourceId. 2006/08/28 14:37:00 af 1.1.2.2: #i68075# Improved the handling of where and when to show the slide sorter. 2006/08/18 14:10:27 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/framework/module/SlideSorterModule.cxx
@@ -0,0 +1,165 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: SlideSorterModule.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 15:54:53 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "SlideSorterModule.hxx"
+
+#include "framework/FrameworkHelper.hxx"
+
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XTABBAR_HPP_
+#include <com/sun/star/drawing/framework/XTabBar.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_TABBARBUTTON_HPP_
+#include <com/sun/star/drawing/framework/TabBarButton.hpp>
+#endif
+
+#include "strings.hrc"
+#include "sdresid.hxx"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+using ::sd::framework::FrameworkHelper;
+
+
+namespace sd { namespace framework {
+
+
+//===== SlideSorterModule ==================================================
+
+SlideSorterModule::SlideSorterModule (
+    const Reference<frame::XController>& rxController,
+    const OUString& rsLeftPaneURL)
+    : ResourceManager(rxController,
+        FrameworkHelper::CreateResourceId(FrameworkHelper::msSlideSorterURL, rsLeftPaneURL)),
+      mxViewTabBarId(FrameworkHelper::CreateResourceId(
+          FrameworkHelper::msViewTabBarURL,
+          FrameworkHelper::msCenterPaneURL)),
+      mxControllerManager(rxController,UNO_QUERY)
+{
+    if (mxConfigurationController.is())
+    {
+        UpdateViewTabBar();
+
+        AddActiveMainView(FrameworkHelper::msImpressViewURL);
+        AddActiveMainView(FrameworkHelper::msOutlineViewURL);
+        AddActiveMainView(FrameworkHelper::msNotesViewURL);
+
+        AddActiveMainView(FrameworkHelper::msDrawViewURL);
+
+        mxConfigurationController->addConfigurationChangeListener(
+            this,
+            FrameworkHelper::msResourceActivationEvent,
+            Any());
+    }
+}
+
+
+
+
+SlideSorterModule::~SlideSorterModule (void)
+{
+}
+
+
+
+
+void SAL_CALL SlideSorterModule::notifyConfigurationChange (
+    const ConfigurationChangeEvent& rEvent)
+    throw (RuntimeException)
+{
+    if (rEvent.Type.equals(FrameworkHelper::msResourceActivationEvent))
+    {
+        if (rEvent.ResourceId->compareTo(mxViewTabBarId) == 0)
+        {
+            // Update the view tab bar because the view tab bar has just
+            // become active.
+            UpdateViewTabBar();
+        }
+        else if (rEvent.ResourceId->getResourceTypePrefix().equals(
+            FrameworkHelper::msViewURLPrefix)
+            && rEvent.ResourceId->isBoundTo(
+                FrameworkHelper::CreateResourceId(FrameworkHelper::msCenterPaneURL),
+                AnchorBindingMode_DIRECT))
+        {
+            // Update the view tab bar because the view in the center pane
+            // has changed.
+            UpdateViewTabBar();
+        }
+    }
+    else
+    {
+        ResourceManager::notifyConfigurationChange(rEvent);
+    }
+}
+
+
+
+
+void SlideSorterModule::UpdateViewTabBar (void)
+{
+    Reference<XToolBarController> xToolBarController;
+    if (mxControllerManager.is())
+        xToolBarController = mxControllerManager->getToolBarController();
+    if (xToolBarController.is())
+    {
+        Reference<XTabBar> xBar (
+            xToolBarController->getToolBar(mxViewTabBarId),
+            UNO_QUERY);
+        if (xBar.is())
+        {
+            TabBarButton aButtonA;
+            aButtonA.ResourceId = FrameworkHelper::CreateResourceId(
+                FrameworkHelper::msSlideSorterURL,
+                FrameworkHelper::msCenterPaneURL);
+            aButtonA.ButtonLabel = String(SdResId(STR_SLIDE_MODE));
+
+            TabBarButton aButtonB;
+            aButtonB.ResourceId = FrameworkHelper::CreateResourceId(
+                FrameworkHelper::msHandoutViewURL,
+                FrameworkHelper::msCenterPaneURL);
+
+            if ( ! xBar->hasTabBarButton(aButtonA))
+                xBar->addTabBarButtonAfter(aButtonA, aButtonB);
+        }
+    }
+}
+
+
+
+} } // end of namespace sd::framework
