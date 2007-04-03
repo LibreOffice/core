3b69d6af20b1dc525e2daa23e8775b53a478210c
Rüdiger Timm <rt@openoffice.org>
2007-04-03 15:12:32 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2006/08/28 14:32:31 af 1.1.2.2: #i68075# Fixed compiler problems. 2006/08/18 14:00:51 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/inc/framework/PresentationModule.hxx
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: PresentationModule.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 16:12:32 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SD_FRAMEWORK_PRESENTATION_MODULE_HXX
+#define SD_FRAMEWORK_PRESENTATION_MODULE_HXX
+
+#ifndef _COM_SUN_STAR_FRAME_XCONTROLLER_HPP_
+#include <com/sun/star/frame/XController.hpp>
+#endif
+
+namespace sd { namespace framework {
+
+/** The task of this module is to instantiate all modules that belong to the
+    fullscreen presentation.
+*/
+class PresentationModule
+{
+public:
+    static void Initialize (
+        ::com::sun::star::uno::Reference<com::sun::star::frame::XController>& rxController);
+};
+
+} } // end of namespace sd::framework
+
+#endif
