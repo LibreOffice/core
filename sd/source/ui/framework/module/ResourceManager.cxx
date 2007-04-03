64572e5db7d280b757ad5954ad24a60ddee601e9
Rüdiger Timm <rt@openoffice.org>
2007-04-03 14:54:17 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2007/03/15 09:33:32 af 1.1.2.8: #i74543# Calling dispose() instead of disposing() when the configuration controller is disposed. 2007/03/07 10:49:53 af 1.1.2.7: #i75056# Added Enable() and Disable() methods. 2007/02/16 14:02:06 af 1.1.2.6: #i68075# Added some comments. 2007/01/29 15:27:02 af 1.1.2.5: #68075# Resolved resync problems. 2007/01/24 17:24:36 af 1.1.2.4: #i68075# Introduction of XResourceId interface. 2007/01/22 15:25:46 af 1.1.2.3: #i68075 Introduction of XResourceId interface. 2006/09/07 16:26:11 af 1.1.2.2: #i68075# Transition from URL to ResourceId. 2006/09/07 14:05:38 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/framework/module/ResourceManager.cxx
@@ -0,0 +1,315 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ResourceManager.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 15:54:17 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "ResourceManager.hxx"
+
+#include "framework/FrameworkHelper.hxx"
+#include "framework/ConfigurationController.hxx"
+
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XCONTROLLERMANAGER_HPP_
+#include <com/sun/star/drawing/framework/XControllerManager.hpp>
+#endif
+
+#include <comphelper/stl_types.hxx>
+#include <set>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+using ::sd::framework::FrameworkHelper;
+
+namespace {
+    static const sal_Int32 ResourceActivationRequestEvent = 0;
+    static const sal_Int32 ResourceDeactivationRequestEvent = 1;
+}
+
+
+
+
+namespace sd { namespace framework {
+
+class ResourceManager::MainViewContainer
+    : public ::std::set<OUString, ::comphelper::UStringLess>
+{
+public:
+    MainViewContainer (void) {}
+};
+
+
+
+
+//===== ResourceManager =======================================================
+
+ResourceManager::ResourceManager (
+    const Reference<frame::XController>& rxController,
+    const Reference<XResourceId>& rxResourceId)
+    : ResourceManagerInterfaceBase(MutexOwner::maMutex),
+      mxConfigurationController(),
+      mpActiveMainViewContainer(new MainViewContainer()),
+      mxResourceId(rxResourceId),
+      msCurrentMainViewURL(),
+      mbIsEnabled(true)
+{
+    Reference<XControllerManager> xControllerManager (rxController, UNO_QUERY);
+    if (xControllerManager.is())
+    {
+        mxConfigurationController = xControllerManager->getConfigurationController();
+
+        if (mxConfigurationController.is())
+        {
+            mxConfigurationController->addConfigurationChangeListener(
+                this,
+                FrameworkHelper::msResourceActivationRequestEvent,
+                makeAny(ResourceActivationRequestEvent));
+            mxConfigurationController->addConfigurationChangeListener(
+                this,
+                FrameworkHelper::msResourceDeactivationRequestEvent,
+                makeAny(ResourceDeactivationRequestEvent));
+        }
+    }
+}
+
+
+
+
+ResourceManager::~ResourceManager (void)
+{
+}
+
+
+
+
+void ResourceManager::AddActiveMainView (
+    const OUString& rsMainViewURL)
+{
+    mpActiveMainViewContainer->insert(rsMainViewURL);
+}
+
+
+
+
+void SAL_CALL ResourceManager::disposing (void)
+{
+    if (mxConfigurationController.is())
+    {
+        mxConfigurationController->removeConfigurationChangeListener(this);
+        mxConfigurationController = NULL;
+    }
+}
+
+
+
+
+void ResourceManager::Enable (void)
+{
+    mbIsEnabled = true;
+    UpdateForMainViewShell();
+}
+
+
+
+
+void ResourceManager::Disable (void)
+{
+    mbIsEnabled = false;
+    UpdateForMainViewShell();
+}
+
+
+
+
+void SAL_CALL ResourceManager::notifyConfigurationChange (
+    const ConfigurationChangeEvent& rEvent)
+    throw (RuntimeException)
+{
+    OSL_ASSERT(rEvent.ResourceId.is());
+
+    sal_Int32 nEventType;
+    rEvent.UserData >>= nEventType;
+    switch (nEventType)
+    {
+        case ResourceActivationRequestEvent:
+            if (rEvent.ResourceId->isBoundToURL(
+                FrameworkHelper::msCenterPaneURL,
+                AnchorBindingMode_DIRECT))
+            {
+                // A resource directly bound to the center pane has been
+                // requested.
+                if (rEvent.ResourceId->getResourceTypePrefix().equals(
+                    FrameworkHelper::msViewURLPrefix))
+                {
+                    // The requested resource is a view.  Show or hide the
+                    // resource managed by this ResourceManager accordingly.
+                    HandleMainViewSwitch(
+                        rEvent.ResourceId->getResourceURL(),
+                        rEvent.Configuration);
+                }
+            }
+            else if (rEvent.ResourceId->compareTo(mxResourceId) == 0)
+            {
+                // The resource managed by this ResourceManager has been
+                // explicitly been requested (maybe by us).  Remember this
+                // setting.
+                HandleResourceRequest(true, rEvent.Configuration);
+            }
+            break;
+
+        case ResourceDeactivationRequestEvent:
+            if ( ! rEvent.ResourceId->isBoundToURL(
+                FrameworkHelper::msCenterPaneURL,
+                    AnchorBindingMode_DIRECT)
+                && rEvent.ResourceId->compareTo(mxResourceId) == 0)
+            {
+                // The resource managed by this ResourceManager has been
+                // explicitly been requested to be hidden (maybe by us).
+                // Remember this setting.
+                HandleResourceRequest(false, rEvent.Configuration);
+            }
+            break;
+    }
+}
+
+
+
+
+void ResourceManager::UpdateForMainViewShell (void)
+{
+    if (mxConfigurationController.is())
+    {
+        ConfigurationController::Lock aLock (mxConfigurationController);
+
+        if (mbIsEnabled
+            && mpActiveMainViewContainer->find(msCurrentMainViewURL)
+               != mpActiveMainViewContainer->end())
+        {
+            // Activate resource.
+            mxConfigurationController->requestResourceActivation(
+                mxResourceId->getAnchor(),
+                ResourceActivationMode_ADD);
+            mxConfigurationController->requestResourceActivation(
+                mxResourceId,
+                ResourceActivationMode_REPLACE);
+        }
+        else
+        {
+            mxConfigurationController->requestResourceDeactivation(mxResourceId);
+        }
+    }
+}
+
+
+
+
+void ResourceManager::HandleMainViewSwitch (
+    const OUString& rsViewURL,
+    const Reference<XConfiguration>& rxConfiguration)
+{
+    (void)rxConfiguration;
+    msCurrentMainViewURL = rsViewURL;
+    UpdateForMainViewShell();
+}
+
+
+
+
+void ResourceManager::HandleResourceRequest(
+    bool bActivation,
+    const Reference<XConfiguration>& rxConfiguration)
+{
+    if (mbIsEnabled)
+    {
+        Sequence<Reference<XResourceId> > aCenterViews = rxConfiguration->getResources(
+            FrameworkHelper::CreateResourceId(FrameworkHelper::msCenterPaneURL),
+            FrameworkHelper::msViewURLPrefix,
+            AnchorBindingMode_DIRECT);
+        if (aCenterViews.getLength() == 1)
+        {
+            if (bActivation)
+            {
+                mpActiveMainViewContainer->insert(aCenterViews[0]->getResourceURL());
+            }
+            else
+            {
+                MainViewContainer::iterator iElement (
+                    mpActiveMainViewContainer->find(aCenterViews[0]->getResourceURL()));
+                if (iElement != mpActiveMainViewContainer->end())
+                    mpActiveMainViewContainer->erase(iElement);
+            }
+        }
+    }
+}
+
+
+
+
+void SAL_CALL ResourceManager::disposing (
+    const lang::EventObject& rEvent)
+    throw (RuntimeException)
+{
+    if (mxConfigurationController.is()
+        && rEvent.Source == mxConfigurationController)
+    {
+        // Without the configuration controller this class can do nothing.
+        mxConfigurationController = NULL;
+        dispose();
+    }
+}
+
+
+
+
+void ResourceManager::Trace (void) const
+{
+    OSL_TRACE("main views with resource %s:",
+        ::rtl::OUStringToOString(
+            FrameworkHelper::ResourceIdToString(mxResourceId), RTL_TEXTENCODING_UTF8).getStr());
+
+    MainViewContainer::const_iterator iDescriptor;
+    for (iDescriptor=mpActiveMainViewContainer->begin();
+         iDescriptor!=mpActiveMainViewContainer->end();
+         ++iDescriptor)
+    {
+        OSL_TRACE("    %s",
+            ::rtl::OUStringToOString(*iDescriptor, RTL_TEXTENCODING_UTF8).getStr());
+    }
+}
+
+
+
+} } // end of namespace sd::framework
