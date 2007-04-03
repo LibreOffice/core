ea8691b2d2cbaa8cc76de81817e129014cfc8d09
Rüdiger Timm <rt@openoffice.org>
2007-04-03 14:51:50 +0000

    INTEGRATION: CWS components1 (1.1.2); FILE ADDED 2007/01/29 15:27:01 af 1.1.2.3: #68075# Resolved resync problems. 2007/01/22 15:17:45 af 1.1.2.2: #i68075 Introduction of XResourceId interface. 2006/08/18 13:57:41 af 1.1.2.1: #i68075# Initial revision.

---
+++ sd/source/ui/framework/module/DrawModule.cxx
@@ -0,0 +1,62 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: DrawModule.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-03 15:51:50 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "framework/DrawModule.hxx"
+
+#include "framework/FrameworkHelper.hxx"
+#include "CenterViewFocusModule.hxx"
+#include "SlideSorterModule.hxx"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+
+
+
+namespace sd { namespace framework {
+
+
+
+void DrawModule::Initialize (Reference<frame::XController>& rxController)
+{
+    new sd::framework::CenterViewFocusModule(rxController);
+    new sd::framework::SlideSorterModule(
+        rxController,
+        FrameworkHelper::msLeftDrawPaneURL);
+}
+
+
+} } // end of namespace sd::framework
