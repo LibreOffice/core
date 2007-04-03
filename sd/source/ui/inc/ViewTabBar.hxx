e56cd82e85093b6d79dddb2145b2187676b5aeb9
Rüdiger Timm <rt@openoffice.org>
2007-04-03 15:09:10 +0000

    INTEGRATION: CWS components1 (1.4.234); FILE MERGED 2007/01/29 15:27:05 af 1.4.234.4: #68075# Resolved resync problems. 2007/01/22 15:59:46 af 1.4.234.3: #i68075 Introduction of XResourceId interface. 2006/09/07 14:51:50 af 1.4.234.2: #i68075# Transition from URL to ResourceId. 2006/08/22 11:33:27 af 1.4.234.1: #i68075# Transition to new drawing framework.

--- sd/source/ui/inc/ViewTabBar.hxx
+++ sd/source/ui/inc/ViewTabBar.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ViewTabBar.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:20:40 $
+ *  last change: $Author: rt $ $Date: 2007-04-03 16:09:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,23 +36,140 @@
 #ifndef SD_VIEW_TAB_BAR_HXX
 #define SD_VIEW_TAB_BAR_HXX
 
+#ifndef _COM_SUN_STAR_FRAME_XCONTROLLER_HPP_
+#include <com/sun/star/frame/XController.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XPANE_HPP_
+#include <com/sun/star/drawing/framework/XPane.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_TABBARBUTTON_HPP_
+#include <com/sun/star/drawing/framework/TabBarButton.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XTABBAR_HPP_
+#include <com/sun/star/drawing/framework/XTabBar.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XTOOLBAR_HPP_
+#include <com/sun/star/drawing/framework/XToolBar.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XCONFIGURATIONCONTROLLER_HPP_
+#include <com/sun/star/drawing/framework/XConfigurationController.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FRAMEWORK_XCONFIGURATIONCHANGELISTENER_HPP_
+#include <com/sun/star/drawing/framework/XConfigurationChangeListener.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XUNOTUNNEL_HPP_
+#include <com/sun/star/lang/XUnoTunnel.hpp>
+#endif
+#ifndef _VCL_TABCTRL_HXX_
 #include <vcl/tabctrl.hxx>
+#endif
+#ifndef _CPPUHELPER_COMPBASE4_HXX_
+#include <cppuhelper/compbase4.hxx>
+#endif
+#include "MutexOwner.hxx"
+
+#include <vector>
+#include <boost/scoped_ptr.hpp>
+
+namespace sd { namespace tools {
+    class EventMultiplexerEvent;
+} }
 
 namespace sd {
+    class ViewShellBase;
+    class PaneManagerEvent;
+}
 
-class ViewShellBase;
-class PaneManagerEvent;
+namespace {
+    typedef ::cppu::WeakComponentImplHelper4 <
+        ::com::sun::star::drawing::framework::XToolBar,
+        ::com::sun::star::drawing::framework::XTabBar,
+        ::com::sun::star::drawing::framework::XConfigurationChangeListener,
+        ::com::sun::star::lang::XUnoTunnel
+        > ViewTabBarInterfaceBase;
+}
+
+
+
+
+namespace sd {
 
 /** Tab control for switching between views in the center pane.
 */
 class ViewTabBar
-    : public TabControl
+    : private sd::MutexOwner,
+      public ViewTabBarInterfaceBase,
+      public TabControl
 {
 public:
-    ViewTabBar (ViewShellBase& rViewShellBase, ::Window* pParent);
+    ViewTabBar (
+        const ::com::sun::star::uno::Reference<
+            com::sun::star::drawing::framework::XResourceId>& rxViewTabBarId,
+        const ::com::sun::star::uno::Reference<
+        ::com::sun::star::frame::XController>& rxController);
     virtual ~ViewTabBar (void);
 
-    /** The returned value is the calculated as the difference between the
+    virtual void SAL_CALL disposing (void);
+
+
+    //----- drawing::framework::XConfigurationChangeListener ------------------
+
+    virtual void SAL_CALL
+        notifyConfigurationChange (
+            const ::com::sun::star::drawing::framework::ConfigurationChangeEvent& rEvent)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    //----- XEventListener ----------------------------------------------------
+
+    virtual void SAL_CALL disposing(
+        const com::sun::star::lang::EventObject& rEvent)
+        throw (com::sun::star::uno::RuntimeException);
+
+
+    //----- XTabBar -----------------------------------------------------------
+
+    virtual void
+        SAL_CALL addTabBarButtonAfter (
+            const ::com::sun::star::drawing::framework::TabBarButton& rButton,
+            const ::com::sun::star::drawing::framework::TabBarButton& rAnchor)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual void
+        SAL_CALL appendTabBarButton (
+            const ::com::sun::star::drawing::framework::TabBarButton& rButton)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual void
+        SAL_CALL removeTabBarButton (
+            const ::com::sun::star::drawing::framework::TabBarButton& rButton)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual sal_Bool
+        SAL_CALL hasTabBarButton (
+            const ::com::sun::star::drawing::framework::TabBarButton& rButton)
+        throw (::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Sequence<com::sun::star::drawing::framework::TabBarButton>
+        SAL_CALL getTabBarButtons (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    //----- XResource ---------------------------------------------------------
+
+    virtual ::com::sun::star::uno::Reference<
+        ::com::sun::star::drawing::framework::XResourceId> SAL_CALL getResourceId (void)
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    //----- XUnoTunnel --------------------------------------------------------
+
+    virtual sal_Int64 SAL_CALL getSomething (const com::sun::star::uno::Sequence<sal_Int8>& rId)
+        throw (com::sun::star::uno::RuntimeException);
+
+    // ------------------------------------------------------------------------
+
+    /** The returned value is calculated as the difference between the
         total height of the control and the heigh of its first tab page.
         This can be considered a hack.
         This procedure works only when the control is visible.  Calling this
@@ -63,14 +180,50 @@ public:
     */
     int GetHeight (void);
 
-protected:
-    virtual void ActivatePage (void);
-    virtual void Paint (const Rectangle& rPaintArea);
+    virtual void Paint (const Rectangle& rRect);
+
+    void AddTabBarButton (
+        const ::com::sun::star::drawing::framework::TabBarButton& rButton,
+        const ::com::sun::star::drawing::framework::TabBarButton& rAnchor);
+    void AddTabBarButton (
+        const ::com::sun::star::drawing::framework::TabBarButton& rButton);
+    void RemoveTabBarButton (
+        const ::com::sun::star::drawing::framework::TabBarButton& rButton);
+    bool HasTabBarButton (
+        const ::com::sun::star::drawing::framework::TabBarButton& rButton);
+    ::com::sun::star::uno::Sequence<com::sun::star::drawing::framework::TabBarButton>
+        GetTabBarButtons (void);
 
 private:
-    ViewShellBase& mrViewShellBase;
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::frame::XController> mxController;
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::drawing::framework::XConfigurationController> mxConfigurationController;
+    typedef ::std::vector<com::sun::star::drawing::framework::TabBarButton> TabBarButtonList;
+    TabBarButtonList maTabBarButtons;
+    ::boost::scoped_ptr<TabPage> mpTabPage;
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::drawing::framework::XResourceId> mxViewTabBarId;
+    ViewShellBase* mpViewShellBase;
 
-       DECL_LINK(ViewShellBaseEventHandler, PaneManagerEvent*);
+    virtual void ActivatePage (void);
+    void UpdateActiveButton (void);
+    void AddTabBarButton (
+        const ::com::sun::star::drawing::framework::TabBarButton& rButton,
+        sal_Int32 nPosition);
+    void UpdateTabBarButtons (void);
+
+    /** This method is called from the constructor to get the window for an
+        anchor ResourceId and pass it to our base class.  It has to be
+        static because it must not access any of the, not yet initialized
+        members.
+    */
+    static ::Window* GetAnchorWindow(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::drawing::framework::XResourceId>& rxViewTabBarId,
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::frame::XController>& rxController);
+    const ::com::sun::star::uno::Sequence<sal_Int8>& getUnoTunnelId (void);
 };
 
 } // end of namespace sd
