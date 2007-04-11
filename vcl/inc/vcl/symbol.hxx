679713a6633c94bb950c9f7dea280d60639bde17
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 17:13:33 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/02/09 16:11:14 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to vcl/inc/vcl and correspondent necessary changes

---
+++ vcl/inc/vcl/symbol.hxx
@@ -0,0 +1,91 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: symbol.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 18:13:07 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SV_SYMBOL_HXX
+#define _SV_SYMBOL_HXX
+
+#ifndef _SV_SV_H
+#include <vcl/sv.h>
+#endif
+
+// ----------------
+// - Symbol-Types -
+// ----------------
+
+// By changes you must also change: rsc/vclrsc.hxx
+typedef USHORT SymbolType;
+#define SYMBOL_DONTKNOW         ((SymbolType)0)
+#define SYMBOL_IMAGE            ((SymbolType)1)
+#define SYMBOL_ARROW_UP         ((SymbolType)2)
+#define SYMBOL_ARROW_DOWN       ((SymbolType)3)
+#define SYMBOL_ARROW_LEFT       ((SymbolType)4)
+#define SYMBOL_ARROW_RIGHT      ((SymbolType)5)
+#define SYMBOL_SPIN_UP          ((SymbolType)6)
+#define SYMBOL_SPIN_DOWN        ((SymbolType)7)
+#define SYMBOL_SPIN_LEFT        ((SymbolType)8)
+#define SYMBOL_SPIN_RIGHT       ((SymbolType)9)
+#define SYMBOL_FIRST            ((SymbolType)10)
+#define SYMBOL_LAST             ((SymbolType)11)
+#define SYMBOL_PREV             ((SymbolType)12)
+#define SYMBOL_NEXT             ((SymbolType)13)
+#define SYMBOL_PAGEUP           ((SymbolType)14)
+#define SYMBOL_PAGEDOWN         ((SymbolType)15)
+#define SYMBOL_PLAY             ((SymbolType)16)
+#define SYMBOL_REVERSEPLAY      ((SymbolType)17)
+#define SYMBOL_RECORD           ((SymbolType)18)
+#define SYMBOL_STOP             ((SymbolType)19)
+#define SYMBOL_PAUSE            ((SymbolType)20)
+#define SYMBOL_WINDSTART        ((SymbolType)21)
+#define SYMBOL_WINDEND          ((SymbolType)22)
+#define SYMBOL_WINDBACKWARD     ((SymbolType)23)
+#define SYMBOL_WINDFORWARD      ((SymbolType)24)
+#define SYMBOL_CLOSE            ((SymbolType)25)
+#define SYMBOL_ROLLUP           ((SymbolType)26)
+#define SYMBOL_ROLLDOWN         ((SymbolType)27)
+#define SYMBOL_CHECKMARK        ((SymbolType)28)
+#define SYMBOL_RADIOCHECKMARK   ((SymbolType)29)
+#define SYMBOL_SPIN_UPDOWN      ((SymbolType)30)
+#define SYMBOL_FLOAT            ((SymbolType)31)
+#define SYMBOL_DOCK             ((SymbolType)32)
+#define SYMBOL_HIDE             ((SymbolType)33)
+#define SYMBOL_HELP             ((SymbolType)34)
+#define SYMBOL_OS2CLOSE         ((SymbolType)35)
+#define SYMBOL_OS2FLOAT         ((SymbolType)36)
+#define SYMBOL_OS2HIDE          ((SymbolType)37)
+#define SYMBOL_MENU             SYMBOL_SPIN_DOWN
+
+#define SYMBOL_NOSYMBOL         (SYMBOL_DONTKNOW)
+
+#endif // _SV_SYMBOL_HXX
---
+++ vcl/inc/vcl/syschild.hxx
@@ -0,0 +1,82 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: syschild.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 18:13:33 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SV_SYSCHILD_HXX
+#define _SV_SYSCHILD_HXX
+
+#ifndef _SV_SV_H
+#include <vcl/sv.h>
+#endif
+#ifndef _VCL_DLLAPI_H
+#include <vcl/dllapi.h>
+#endif
+
+#ifndef _SV_WINDOW_HXX
+#include <vcl/window.hxx>
+#endif
+
+struct SystemEnvData;
+struct SystemWindowData;
+
+// ---------------------
+// - SystemChildWindow -
+// ---------------------
+
+class VCL_DLLPUBLIC SystemChildWindow : public Window
+{
+private:
+    using Window::ImplInit;
+    SAL_DLLPRIVATE void     ImplInitSysChild( Window* pParent, WinBits nStyle, SystemWindowData *pData );
+
+    // Copy assignment is forbidden and not implemented.
+    SAL_DLLPRIVATE          SystemChildWindow (const SystemChildWindow &);
+    SAL_DLLPRIVATE          SystemChildWindow & operator= (const SystemChildWindow &);
+
+public:
+                            SystemChildWindow( Window* pParent, WinBits nStyle = 0 );
+                            // create a SystemChildWindow using the given SystemWindowData
+                            SystemChildWindow( Window* pParent, WinBits nStyle, SystemWindowData *pData );
+                            SystemChildWindow( Window* pParent, const ResId& rResId );
+                            ~SystemChildWindow();
+
+    const SystemEnvData*    GetSystemData() const;
+
+    //  per default systemchildwindows erase their background for better plugin support
+    //  however, this might not always be required
+    void                    EnableEraseBackground( BOOL bEnable = TRUE );
+    BOOL                    IsEraseBackgroundEnabled();
+};
+
+#endif // _SV_SYSCHILD_HXX
