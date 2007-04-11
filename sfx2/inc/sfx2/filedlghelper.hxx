b25774db4315fbbbb86cc7f66f369c922b053055
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 20:20:42 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/02/09 16:14:44 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to sfx2/inc/sfx2 and correspondent necessary changes

---
+++ sfx2/inc/sfx2/filedlghelper.hxx
@@ -0,0 +1,341 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: filedlghelper.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 21:20:23 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _FILEDLGHELPER_HXX
+#define _FILEDLGHELPER_HXX
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+#ifndef INCLUDED_SFX2_DLLAPI_H
+#include "sfx2/dllapi.h"
+#endif
+
+#ifndef _SAL_TYPES_H_
+#include "sal/types.h"
+#endif
+#ifndef  _COM_SUN_STAR_UNO_REFERENCE_HXX_
+#include <com/sun/star/uno/Reference.hxx>
+#endif
+#ifndef  _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+#ifndef  _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+#ifndef _SOLAR_H
+#include <tools/solar.h>
+#endif
+#ifndef _STRING_HXX
+#include <tools/string.hxx>
+#endif
+#ifndef _ERRCODE_HXX
+#include <tools/errcode.hxx>
+#endif
+
+#ifndef _SV_DIALOG_HXX
+#include <vcl/dialog.hxx>
+#endif
+#ifndef _SV_EDIT_HXX
+#include <vcl/edit.hxx>
+#endif
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+#ifndef _SV_GRAPH_HXX
+#include <vcl/graph.hxx>
+#endif
+
+#ifndef  _SFXDEFS_HXX
+#include <sfx2/sfxdefs.hxx>
+#endif
+#ifndef  _SFX_SFXUNO_HXX
+#include <sfx2/sfxuno.hxx>
+#endif
+
+//-----------------------------------------------------------------------------
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+            namespace ui
+            {
+                namespace dialogs
+                {
+                    class XFilePicker;
+                    class XFilePickerListener;
+                    struct FilePickerEvent;
+                    struct DialogClosedEvent;
+                }
+            }
+        }
+    }
+}
+
+class SfxItemSet;
+class SvStringsDtor;
+class Window;
+
+//-----------------------------------------------------------------------------
+/*
+#define WB_PATH                 0x00100000L
+#define WB_OPEN                 0x00200000L
+#define WB_SAVEAS               0x00400000L
+#define WB_PASSWORD             0x01000000L
+#define WB_READONLY             0x02000000L
+*/
+
+#define SFXWB_INSERT            ( 0x04000000L | WB_OPEN )   // ((WinBits)0x00200000)
+#define SFXWB_PASSWORD          WB_PASSWORD                 // ((WinBits)0x01000000)
+#define SFXWB_READONLY          WB_READONLY                 // ((WinBits)0x02000000)
+#define SFXWB_PATHDIALOG        WB_PATH                     // ((WinBits)0x00100000)
+#define SFXWB_CLASSPATH         ( 0x08000000L | SFXWB_PATHDIALOG )
+#define SFXWB_SHOWALLFOLDER     0x10000000L     // alle Ordner auch Mail/News/...
+#define SFXWB_MULTISELECTION    0x20000000L     // Multiselection an
+#define SFXWB_NOREMOTE          0x40000000L
+#define SFXWB_SHOWVERSIONS      0x80000000L     // Versionsauswahl anzeigen
+
+#define SFXWB_GRAPHIC           0x00800000L     // FileOpen with link and preview box
+#define SFXWB_SHOWSTYLES        0x01000000L     // FileOpen with link and preview box and styles
+
+#define SFXWB_EXPORT            ( 0x040000000L | WB_SAVEAS )    // Export dialog
+
+#define FILEDIALOG_FILTER_ALL   "*.*"
+
+#define FILE_OPEN_SERVICE_NAME      "com.sun.star.ui.dialogs.FilePicker"
+#define FOLDER_PICKER_SERVICE_NAME  "com.sun.star.ui.dialogs.FolderPicker"
+
+//*****************************************************************************
+// SfxUrlDialog
+//*****************************************************************************
+
+//#if 0 // _SOLAR__PRIVATE
+#ifndef DONT_USE_FILE_DIALOG_SERVICE
+
+class SfxUrlDialog : public ModalDialog
+{
+private:
+    Edit            aEdit;
+    OKButton        aOk;
+    CancelButton    aCancel;
+
+public:
+                    SfxUrlDialog( Window* pParent );
+
+    String          GetUrl() const { return aEdit.GetText(); }
+    void            SetUrl( const String& rUrl ) { aEdit.SetText( rUrl ); }
+};
+
+#endif
+//#endif
+
+//-----------------------------------------------------------------------------
+
+namespace sfx2 {
+
+class FileDialogHelper_Impl;
+
+class SFX2_DLLPUBLIC FileDialogHelper
+{
+public:
+    enum Context                        // context where the FileDialogHelper is used
+    {
+        UNKNOWN_CONTEXT,                // unknown context
+        SW_INSERT_GRAPHIC,              // insert graphic in writer
+        SW_INSERT_SOUND,                // insert sound in writer
+        SW_INSERT_VIDEO,                // insert video in writer
+        SC_INSERT_GRAPHIC,              // insert graphic in calc
+        SC_INSERT_SOUND,                // insert sound in calc
+        SC_INSERT_VIDEO,                // insert video in calc
+        SD_INSERT_GRAPHIC,              // insert graphic in draw
+        SD_INSERT_SOUND,                // insert sound in draw
+        SD_INSERT_VIDEO,                // insert video in draw
+        SD_EXPORT,                      // export in draw
+        SI_EXPORT                       // export in impress
+    };
+
+private:
+    Link    m_aDialogClosedLink;
+    ErrCode m_nError;
+
+    ::com::sun::star::uno::Reference < ::com::sun::star::ui::dialogs::XFilePickerListener > mxImp;
+    FileDialogHelper_Impl   *mpImp;
+
+//#if 0 // _SOLAR__PRIVATE
+
+    SAL_DLLPRIVATE sal_Int16 getDialogType( sal_Int64 nFlags ) const;
+
+//#endif
+
+public:
+                            FileDialogHelper( sal_Int64 nFlags,
+                                              const String& rFactory,
+                                              SfxFilterFlags nMust = 0,
+                                              SfxFilterFlags nDont = 0 );
+
+                            FileDialogHelper( sal_Int16 nDialogType,
+                                              sal_Int64 nFlags,
+                                              const String& rFactory,
+                                              SfxFilterFlags nMust = 0,
+                                              SfxFilterFlags nDont = 0 );
+
+                            FileDialogHelper( sal_Int64 nFlags );
+
+                            FileDialogHelper( sal_Int16 nDialogType,
+                                              sal_Int64 nFlags,
+                                              Window* _pPreferredParent = NULL );
+
+    virtual                 ~FileDialogHelper();
+
+    ErrCode                 Execute();
+    void                    StartExecuteModal( const Link& rEndDialogHdl );
+    inline ErrCode          GetError() const { return m_nError; }
+    sal_Int16               GetDialogType() const;
+    sal_Bool                IsPasswordEnabled() const;
+    String                  GetRealFilter() const;
+
+    void                    SetTitle( const String&  rNewTitle );
+    String                  GetPath() const;
+
+    ::com::sun::star::uno::Sequence< ::rtl::OUString > GetMPath() const;
+
+    void                     AddFilter( const String& rFilterName, const String& rExtension );
+    void                     SetCurrentFilter( const String& rFilter );
+
+    /** sets an initial display directory/file name
+
+        @deprecated
+            don't use this method. It contains a lot of magic in determining whether the
+            last segment of the given path/URL denotes a file name or a folder, and by
+            definition, it cannot succeed with this magic *all* the time - there will
+            always be scenarios where it fails.
+
+            Use SetDisplayFolder and SetFileName.
+    */
+    void                     SetDisplayDirectory( const String& rPath );
+
+    /** sets a new folder whose content is to be displayed in the file picker
+
+        @param _rURL
+            specifies the URL of the folder whose content is to be displayed.<br/>
+            If the URL doesn't denote a valid (existent and accessible) folder, the
+            request is silently dropped.
+        @throws ::com::sun::star::uno::RuntimeException
+            if the invocation of any of the file picker or UCB methods throws a RuntimeException.
+    */
+    void                     SetDisplayFolder( const String& _rURL );
+
+    /** sets an initial file name to display
+
+        This method is usually used in "save-as" contexts, where the application should
+        suggest an initial name for the file to save.
+
+        Calling this method is nearly equivalent to calling <code>GetFilePicker().setDefaultName( _rFileName )</code>,
+        with the following differences:
+        <ul><li>The FileDialogHelper remembers the given file name, and upon execution,
+                strips its extension if the dialog is set up for "automatic file name extension".</li>
+            <li>Exceptions thrown from the <code>XFilePicker</code> are caught and silenced.</li>
+        </ul>
+    */
+    void                     SetFileName( const String& _rFileName );
+
+    String                   GetCurrentFilter() const;
+    String                   GetDisplayDirectory() const;
+    ErrCode                  GetGraphic( Graphic& rGraphic ) const;
+
+    ::com::sun::star::uno::Reference < ::com::sun::star::ui::dialogs::XFilePicker > GetFilePicker() const;
+
+    // XFilePickerListener methods
+    virtual void SAL_CALL   FileSelectionChanged( const ::com::sun::star::ui::dialogs::FilePickerEvent& aEvent );
+    virtual void SAL_CALL   DirectoryChanged( const ::com::sun::star::ui::dialogs::FilePickerEvent& aEvent );
+    virtual void SAL_CALL   ControlStateChanged( const ::com::sun::star::ui::dialogs::FilePickerEvent& aEvent );
+    virtual void SAL_CALL   DialogSizeChanged();
+    virtual ::rtl::OUString SAL_CALL    HelpRequested( const ::com::sun::star::ui::dialogs::FilePickerEvent& aEvent );
+
+    // XDialogClosedListener methods
+    virtual void SAL_CALL   DialogClosed( const ::com::sun::star::ui::dialogs::DialogClosedEvent& _rEvent );
+
+    // retrieves the top-most file picker - i.e. the instance which is currently beeing executed
+    static ::com::sun::star::uno::Reference< ::com::sun::star::ui::dialogs::XFilePicker >
+                            GetTopMostFilePicker( );
+
+    /** sets help ids for the controls in the dialog
+        @param _pControlId
+            Pointer to a 0-terminated array of control ids. They must be recruited from the
+            CommonFilePickerElementIds and ExtendedFilePickerElementIds values.
+        @param _pHelpId
+            Pointer to an array of help ids. For each element in _pControlId, there must be
+            a corresponding element herein.
+    */
+    void                    SetControlHelpIds( const sal_Int16* _pControlId, const sal_Int32* _pHelpId );
+    void                    SetDialogHelpId( const sal_Int32 _nHelpId );
+    void                    CreateMatcher( const String& rName );
+
+    /** sets the context of the dialog and trigger necessary actions e.g. loading config, setting help id
+        @param _eNewContext
+            New context for the dialog.
+    */
+    void                    SetContext( Context _eNewContext );
+
+   DECL_LINK( ExecuteSystemFilePicker, void* );
+
+//#if 0 // _SOLAR__PRIVATE
+    ErrCode                  Execute( const String&   rPath,
+                                      SvStringsDtor*& rpURLList,
+                                      SfxItemSet *&   rpSet,
+                                      String&         rFilter );
+    ErrCode                  Execute( SfxItemSet *&   rpSet,
+                                      String&         rFilter );
+//#endif
+};
+
+//#if 0 // _SOLAR__PRIVATE
+ErrCode FileOpenDialog_Impl( sal_Int64 nFlags,
+                             const String& rFact,
+                             SvStringsDtor *& rpURLList,
+                             String& rFilter,
+                             SfxItemSet *& rpSet,
+                             const String* pPath = NULL );
+//#endif
+}
+
+//-----------------------------------------------------------------------------
+
+#endif
+
---
+++ sfx2/inc/sfx2/frmdescr.hxx
@@ -0,0 +1,318 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: frmdescr.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 21:20:42 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _SFX_FRMDESCRHXX
+#define _SFX_FRMDESCRHXX
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+#ifndef INCLUDED_SFX2_DLLAPI_H
+#include "sfx2/dllapi.h"
+#endif
+
+#include <sfx2/sfxsids.hrc>
+
+#ifndef _SFXPOOLITEM_HXX //autogen
+#include <svtools/poolitem.hxx>
+#endif
+#ifndef _URLOBJ_HXX //autogen
+#include <tools/urlobj.hxx>
+#endif
+#ifndef _STRING_HXX //autogen
+#include <tools/string.hxx>
+#endif
+#ifndef _SVARRAY_HXX //autogen
+#include <svtools/svarray.hxx>
+#endif
+#ifndef _SV_GEN_HXX
+#include <tools/gen.hxx>
+#endif
+
+class SvStream;
+struct SfxFrameDescriptor_Impl;
+class SvStrings;
+class SfxFrameDescriptor;
+class Wallpaper;
+typedef SfxFrameDescriptor* SfxFrameDescriptorPtr;
+SV_DECL_PTRARR(SfxFramesArr, SfxFrameDescriptorPtr, 4, 2)
+
+//===========================================================================
+// Die SfxFrame...descriptoren bauen eine rekursive Struktur auf, die
+// s"amtliche zur Anzeige des Frame-Dokuments erforderlichen Daten umfa\st.
+// "Uber einen SfxFrameSetDescriptor hat man Zugriff auf die gesamte darunter
+// liegende Struktur.
+// Aufgrund der besonderen Eigenschaften des SfxFrames-Dokuments ist ein
+// SfxFramesSetDescriptor nicht nur der Inhalt dieses Dokuments, sondern
+// beschreibt auch die View darauf.
+// Das FrameSet wird aus Zeilen aufgebaut, die wiederum die eigentlichen
+// Fenster enthalten. Eine Zeile kann horizontal oder vertikal ausgerichtet
+// sein, woraus sich auch das Alignment des FrameSets ergibt.
+//===========================================================================
+
+enum ScrollingMode
+{
+    ScrollingYes,
+    ScrollingNo,
+    ScrollingAuto
+};
+
+enum SizeSelector
+{
+    SIZE_ABS,
+    SIZE_PERCENT,
+    SIZE_REL
+};
+
+#define BORDER_SET          2
+#define BORDER_YES          1
+#define BORDER_NO           0
+#define SPACING_NOT_SET     -1L
+#define SIZE_NOT_SET        -1L
+
+class SfxItemSet;
+struct SfxFrameProperties;
+
+class SFX2_DLLPUBLIC SfxFrameDescriptor
+{
+    INetURLObject           aURL;
+    INetURLObject           aActualURL;
+    String                  aName;
+    Size                    aMargin;
+    long                    nWidth;
+    ScrollingMode           eScroll;
+    SizeSelector            eSizeSelector;
+    USHORT                  nHasBorder;
+    USHORT                  nItemId;
+    BOOL                    bResizeHorizontal;
+    BOOL                    bResizeVertical;
+    BOOL                    bHasUI;
+    BOOL                    bReadOnly;
+    SfxFrameDescriptor_Impl* pImp;
+    SvStrings*              pScripts;
+    SvStrings*              pComments;
+
+public:
+                            SfxFrameDescriptor();
+                            ~SfxFrameDescriptor();
+
+                            // Eigenschaften
+    void                    TakeProperties( const SfxFrameProperties& rProp );
+
+                            // FileName/URL
+    SfxItemSet*             GetArgs();
+    const INetURLObject&    GetURL() const
+                            { return aURL; }
+    void                    SetURL( const INetURLObject& rURL );
+    void                    SetURL( const String& rURL );
+    const INetURLObject&    GetActualURL() const
+                            { return aActualURL; }
+    void                    SetActualURL( const INetURLObject& rURL );
+    void                    SetActualURL( const String& rURL );
+    BOOL                    CheckContent() const;
+    BOOL                    CompareOriginal( SfxFrameDescriptor& rSet ) const;
+    void                    UnifyContent( BOOL );
+    void                    SetReadOnly( BOOL bSet ) { bReadOnly = bSet;}
+    BOOL                    IsReadOnly(  ) const { return bReadOnly;}
+    void                    SetEditable( BOOL bSet );
+    BOOL                    IsEditable() const;
+
+                            // Size
+    void                    SetWidth( long n )
+                            { nWidth = n; }
+    void                    SetWidthPercent( long n )
+                            { nWidth = n; eSizeSelector = SIZE_PERCENT; }
+    void                    SetWidthRel( long n )
+                            { nWidth = n; eSizeSelector = SIZE_REL; }
+    void                    SetWidthAbs( long n )
+                            { nWidth = n; eSizeSelector = SIZE_ABS; }
+    long                    GetWidth() const
+                            { return nWidth; }
+    SizeSelector            GetSizeSelector() const
+                            { return eSizeSelector; }
+    BOOL                    IsResizable() const
+                            { return bResizeHorizontal && bResizeVertical; }
+    void                    SetResizable( BOOL bRes )
+                            { bResizeHorizontal = bResizeVertical = bRes; }
+
+                            // FrameName
+    const String&           GetName() const
+                            { return aName; }
+    void                    SetName( const String& rName )
+                            { aName = rName; }
+
+                            // Margin, Scrolling
+    const Size&             GetMargin() const
+                            { return aMargin; }
+    void                    SetMargin( const Size& rMargin )
+                            { aMargin = rMargin; }
+    ScrollingMode           GetScrollingMode() const
+                            { return eScroll; }
+    void                    SetScrollingMode( ScrollingMode eMode )
+                            { eScroll = eMode; }
+
+                            // FrameBorder
+    void                    SetWallpaper( const Wallpaper& rWallpaper );
+    const Wallpaper*        GetWallpaper() const;
+    BOOL                    HasFrameBorder() const;
+
+    BOOL                    IsFrameBorderOn() const
+                            { return ( nHasBorder & BORDER_YES ) != 0; }
+
+    void                    SetFrameBorder( BOOL bBorder )
+                            {
+                                nHasBorder = bBorder ?
+                                            BORDER_YES | BORDER_SET :
+                                            BORDER_NO | BORDER_SET;
+                            }
+    BOOL                    IsFrameBorderSet() const
+                            { return (nHasBorder & BORDER_SET) != 0; }
+    void                    ResetBorder()
+                            { nHasBorder = 0; }
+
+    BOOL                    HasUI() const
+                            { return bHasUI; }
+    void                    SetHasUI( BOOL bOn )
+                            { bHasUI = bOn; }
+
+                            // Attribute f"ur das Splitwindow
+    USHORT                  GetItemId() const
+                            { return nItemId; }
+    void                    SetItemId( USHORT nId )
+                            { nItemId = nId; }
+    USHORT                  GetWinBits() const;
+    long                    GetSize() const;
+    USHORT                  GetItemPos() const;
+
+                            // Kopie z.B. f"ur die Views
+    SfxFrameDescriptor*     Clone( BOOL bWithIds = TRUE ) const;
+};
+
+// Kein Bock, einen operator= zu implementieren...
+struct SfxFrameProperties
+{
+    String                              aURL;
+    String                              aName;
+    long                                lMarginWidth;
+    long                                lMarginHeight;
+    long                                lSize;
+    long                                lSetSize;
+    long                                lFrameSpacing;
+    long                                lInheritedFrameSpacing;
+    ScrollingMode                       eScroll;
+    SizeSelector                        eSizeSelector;
+    SizeSelector                        eSetSizeSelector;
+    BOOL                                bHasBorder;
+    BOOL                                bBorderSet;
+    BOOL                                bResizable;
+    BOOL                                bSetResizable;
+    BOOL                                bIsRootSet;
+    BOOL                                bIsInColSet;
+    BOOL                                bHasBorderInherited;
+    SfxFrameDescriptor*                 pFrame;
+
+private:
+    SfxFrameProperties( SfxFrameProperties& ) {}
+public:
+                                        SfxFrameProperties()
+                                            : lMarginWidth( SIZE_NOT_SET ),
+                                              lMarginHeight( SIZE_NOT_SET ),
+                                              lSize( 1L ),
+                                              lSetSize( 1L ),
+                                              lFrameSpacing( SPACING_NOT_SET ),
+                                              lInheritedFrameSpacing( SPACING_NOT_SET ),
+                                              eScroll( ScrollingAuto ),
+                                              eSizeSelector( SIZE_REL ),
+                                              eSetSizeSelector( SIZE_REL ),
+                                              bHasBorder( TRUE ),
+                                              bBorderSet( TRUE ),
+                                              bResizable( TRUE ),
+                                              bSetResizable( TRUE ),
+                                              bIsRootSet( FALSE ),
+                                              bIsInColSet( FALSE ),
+                                              bHasBorderInherited( TRUE ),
+                                              pFrame( 0 ) {}
+
+                                        SfxFrameProperties( const SfxFrameDescriptor *pD );
+                                        ~SfxFrameProperties() { delete pFrame; }
+
+    int                                 operator ==( const SfxFrameProperties& ) const;
+    SfxFrameProperties&                 operator =( const SfxFrameProperties &rProp );
+};
+
+class SfxFrameDescriptorItem : public SfxPoolItem
+{
+    SfxFrameProperties                  aProperties;
+public:
+                                        TYPEINFO();
+
+                                        SfxFrameDescriptorItem ( const SfxFrameDescriptor *pD, const USHORT nId = SID_FRAMEDESCRIPTOR )
+                                            : SfxPoolItem( nId )
+                                            , aProperties( pD )
+                                        {}
+
+                                        SfxFrameDescriptorItem ( const USHORT nId = SID_FRAMEDESCRIPTOR )
+                                            : SfxPoolItem( nId )
+                                        {}
+
+                                        SfxFrameDescriptorItem( const SfxFrameDescriptorItem& rCpy )
+                                            : SfxPoolItem( rCpy )
+                                        {
+                                            aProperties = rCpy.aProperties;
+                                        }
+
+    virtual                             ~SfxFrameDescriptorItem();
+
+    virtual int                         operator ==( const SfxPoolItem& ) const;
+    SfxFrameDescriptorItem&             operator =( const SfxFrameDescriptorItem & );
+
+    virtual SfxItemPresentation         GetPresentation( SfxItemPresentation ePres,
+                                            SfxMapUnit eCoreMetric,
+                                            SfxMapUnit ePresMetric,
+                                            UniString &rText, const IntlWrapper * = 0 ) const;
+
+    virtual SfxPoolItem*                Clone( SfxItemPool *pPool = 0 ) const;
+    //virtual SfxPoolItem*              Create(SvStream &, USHORT) const;
+    //virtual SvStream&                 Store(SvStream &, USHORT nItemVersion ) const;
+    //virtual USHORT                        GetVersion( USHORT nFileFormatVersion ) const;
+
+    const SfxFrameProperties&           GetProperties() const
+                                        { return aProperties; }
+    void                                SetProperties( const SfxFrameProperties& rProp )
+                                        { aProperties = rProp; }
+};
+
+#endif // #ifndef _SFX_FRMDESCRHXX
+
