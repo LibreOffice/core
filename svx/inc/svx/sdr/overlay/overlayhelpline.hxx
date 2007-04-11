994d4ebf6caf5eb229ea699f1b18db074f190072
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 16:02:59 +0000

    INTEGRATION: CWS hedaburemove01 (1.2.72); FILE MERGED 2007/02/09 16:10:09 vg 1.2.72.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

--- svx/inc/svx/sdr/overlay/overlayhelpline.hxx
+++ svx/inc/svx/sdr/overlay/overlayhelpline.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: overlayhelpline.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 13:07:13 $
+ *  last change: $Author: vg $ $Date: 2007-04-11 17:02:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _SVDHLPLN_HXX
-#include <svdhlpln.hxx>
+#include <svx/svdhlpln.hxx>
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
