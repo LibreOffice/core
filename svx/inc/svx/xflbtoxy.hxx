5a873741a7eed05b4fcfb9893f3ead0bada0c3b9
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 15:52:24 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/02/09 16:54:25 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

---
+++ svx/inc/svx/xflbtoxy.hxx
@@ -0,0 +1,88 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: xflbtoxy.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:52:14 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVX_XFLBTOXY_HXX
+#define _SVX_XFLBTOXY_HXX
+
+#ifndef _SFXINTITEM_HXX //autogen
+#include <svtools/intitem.hxx>
+#endif
+
+/*************************************************************************
+|*
+|*
+|*
+\************************************************************************/
+
+class XFillBmpTileOffsetXItem : public SfxUInt16Item
+{
+public:
+                            TYPEINFO();
+                            XFillBmpTileOffsetXItem( USHORT nOffX = 0 );
+                            XFillBmpTileOffsetXItem( SvStream& rIn );
+
+    virtual SfxPoolItem*    Clone( SfxItemPool* pPool = 0 ) const;
+    virtual SfxPoolItem*    Create( SvStream& rIn, USHORT nVer ) const;
+
+    virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
+                                    SfxMapUnit eCoreMetric,
+                                    SfxMapUnit ePresMetric,
+                                    String &rText, const IntlWrapper * = 0 ) const;
+};
+
+
+/*************************************************************************
+|*
+|*
+|*
+\************************************************************************/
+
+class XFillBmpTileOffsetYItem : public SfxUInt16Item
+{
+public:
+                            TYPEINFO();
+                            XFillBmpTileOffsetYItem( USHORT nOffX = 0 );
+                            XFillBmpTileOffsetYItem( SvStream& rIn );
+
+    virtual SfxPoolItem*    Clone( SfxItemPool* pPool = 0 ) const;
+    virtual SfxPoolItem*    Create( SvStream& rIn, USHORT nVer ) const;
+
+    virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
+                                    SfxMapUnit eCoreMetric,
+                                    SfxMapUnit ePresMetric,
+                                    String &rText, const IntlWrapper * = 0 ) const;
+};
+
+#endif
