057843577ecba370ba8684655f98bc9f4f87f787
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 19:06:49 +0000

    INTEGRATION: CWS hedaburemove01 (1.2.94); FILE MERGED 2006/12/12 16:28:10 vg 1.2.94.1: #i72503# get rid of hedabu

--- tools/inc/bootstrp/command.hxx
+++ tools/inc/bootstrp/command.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: command.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:31:45 $
+ *  last change: $Author: vg $ $Date: 2007-04-11 20:06:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 #include <iostream.h>
 
-#include "stream.hxx"
+#include <tools/stream.hxx>
 #define STRLEN 100
 #ifndef UNX
 #define TMPNAME "\\command.tmp"
--- tools/inc/bootstrp/inimgr.hxx
+++ tools/inc/bootstrp/inimgr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inimgr.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:31:55 $
+ *  last change: $Author: vg $ $Date: 2007-04-11 20:06:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,9 +35,8 @@
 #ifndef _INIMGR_HXX
 #define _INIMGR_HXX
 
-#include "fsys.hxx"
-#include "string.hxx"
-
+#include <tools/fsys.hxx>
+#include <tools/string.hxx>
 
 /*****************************************************************************
 Purpose: Allows to work on a local set of initialisation files
