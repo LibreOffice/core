79b0f9b806a75e567496cbda1edab572107005aa
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 16:02:45 +0000

    INTEGRATION: CWS hedaburemove01 (1.8.706); FILE MERGED 2007/02/09 16:10:08 vg 1.8.706.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

--- svx/inc/svx/sdr/contact/viewcontactofsdrobj.hxx
+++ svx/inc/svx/sdr/contact/viewcontactofsdrobj.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontactofsdrobj.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:59:54 $
+ *  last change: $Author: vg $ $Date: 2007-04-11 17:02:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef SDTAKITM_HXX
-#include <sdtakitm.hxx>
+#include <svx/sdtakitm.hxx>
 #endif
 
 #ifndef INCLUDED_SVXDLLAPI_H
