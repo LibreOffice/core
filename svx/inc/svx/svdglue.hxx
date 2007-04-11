f18dfd11149c266e053bcdc63e257f1fad547cab
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 15:21:10 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/02/09 16:53:48 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

---
+++ svx/inc/svx/svdglue.hxx
@@ -0,0 +1,166 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svdglue.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:20:46 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVDGLUE_HXX
+#define _SVDGLUE_HXX
+
+class Window;
+class OutputDevice;
+class SvStream;
+class SdrObject;
+
+#ifndef _CONTNR_HXX //autogen
+#include <tools/contnr.hxx>
+#endif
+#ifndef _GEN_HXX //autogen
+#include <tools/gen.hxx>
+#endif
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define SDRESC_SMART  0x0000
+#define SDRESC_LEFT   0x0001
+#define SDRESC_RIGHT  0x0002
+#define SDRESC_TOP    0x0004
+#define SDRESC_BOTTOM 0x0008
+#define SDRESC_LO    0x0010 /* ni */
+#define SDRESC_LU    0x0020 /* ni */
+#define SDRESC_RO    0x0040 /* ni */
+#define SDRESC_RU    0x0080 /* ni */
+#define SDRESC_HORZ  (SDRESC_LEFT|SDRESC_RIGHT)
+#define SDRESC_VERT  (SDRESC_TOP|SDRESC_BOTTOM)
+#define SDRESC_ALL   0x00FF
+
+#define SDRHORZALIGN_CENTER   0x0000
+#define SDRHORZALIGN_LEFT     0x0001
+#define SDRHORZALIGN_RIGHT    0x0002
+#define SDRHORZALIGN_DONTCARE 0x0010
+#define SDRVERTALIGN_CENTER   0x0000
+#define SDRVERTALIGN_TOP      0x0100
+#define SDRVERTALIGN_BOTTOM   0x0200
+#define SDRVERTALIGN_DONTCARE 0x1000
+
+class SVX_DLLPUBLIC SdrGluePoint {
+    // Bezugspunkt ist SdrObject::GetSnapRect().Center()
+    // bNoPercent=FALSE: Position ist -5000..5000 (1/100)% bzw. 0..10000 (je nach Align)
+    // bNoPercent=TRUE : Position ist in log Einh, rel zum Bezugspunkt
+    Point    aPos;
+    USHORT   nEscDir;
+    USHORT   nId;
+    USHORT   nAlign;
+    FASTBOOL bNoPercent:1;
+    FASTBOOL bReallyAbsolute:1; // Temporaer zu setzen fuer Transformationen am Bezugsobjekt
+    FASTBOOL bUserDefined:1; // #i38892#
+public:
+    SdrGluePoint(): nEscDir(SDRESC_SMART),nId(0),nAlign(0) { bNoPercent=FALSE; bReallyAbsolute=FALSE; bUserDefined=TRUE; }
+    SdrGluePoint(const Point& rNewPos, FASTBOOL bNewPercent=TRUE, USHORT nNewAlign=0): aPos(rNewPos),nEscDir(SDRESC_SMART),nId(0),nAlign(nNewAlign) { bNoPercent=!bNewPercent; bReallyAbsolute=FALSE; bUserDefined=TRUE; }
+    bool operator==(const SdrGluePoint& rCmpGP) const   { return aPos==rCmpGP.aPos && nEscDir==rCmpGP.nEscDir && nId==rCmpGP.nId && nAlign==rCmpGP.nAlign && bNoPercent==rCmpGP.bNoPercent && bReallyAbsolute==rCmpGP.bReallyAbsolute && bUserDefined==rCmpGP.bUserDefined; }
+    bool operator!=(const SdrGluePoint& rCmpGP) const   { return !operator==(rCmpGP); }
+    const Point& GetPos() const                             { return aPos; }
+    void         SetPos(const Point& rNewPos)               { aPos=rNewPos; }
+    USHORT       GetEscDir() const                          { return nEscDir; }
+    void         SetEscDir(USHORT nNewEsc)                  { nEscDir=nNewEsc; }
+    USHORT       GetId() const                              { return nId; }
+    void         SetId(USHORT nNewId)                       { nId=nNewId; }
+    bool         IsPercent() const                          { return !bNoPercent; }
+    void         SetPercent(FASTBOOL bOn)                   { bNoPercent=!bOn; }
+    // Temporaer zu setzen fuer Transformationen am Bezugsobjekt
+    FASTBOOL     IsReallyAbsolute() const                   { return bReallyAbsolute; }
+    void         SetReallyAbsolute(FASTBOOL bOn, const SdrObject& rObj);
+
+    // #i38892#
+    FASTBOOL     IsUserDefined() const                   { return bUserDefined; }
+    void         SetUserDefined(FASTBOOL bNew)           { bUserDefined = bNew; }
+
+    USHORT       GetAlign() const                           { return nAlign; }
+    void         SetAlign(USHORT nAlg)                      { nAlign=nAlg; }
+    USHORT       GetHorzAlign() const                       { return nAlign&0x00FF; }
+    void         SetHorzAlign(USHORT nAlg)                  { nAlign=(nAlign&0xFF00)|(nAlg&0x00FF); }
+    USHORT       GetVertAlign() const                       { return nAlign&0xFF00; }
+    void         SetVertAlign(USHORT nAlg)                  { nAlign=(nAlign&0x00FF)|(nAlg&0xFF00); }
+    void         Draw(OutputDevice& rOut, const SdrObject* pObj) const;
+    FASTBOOL     IsHit(const Point& rPnt, const OutputDevice& rOut, const SdrObject* pObj) const;
+    void         Invalidate(Window& rWin, const SdrObject* pObj) const;
+    Point        GetAbsolutePos(const SdrObject& rObj) const;
+    void         SetAbsolutePos(const Point& rNewPos, const SdrObject& rObj);
+    long         GetAlignAngle() const;
+    void         SetAlignAngle(long nWink);
+    long         EscDirToAngle(USHORT nEsc) const;
+    USHORT       EscAngleToDir(long nWink) const;
+    void         Rotate(const Point& rRef, long nWink, double sn, double cs, const SdrObject* pObj);
+    void         Mirror(const Point& rRef1, const Point& rRef2, const SdrObject* pObj);
+    void         Mirror(const Point& rRef1, const Point& rRef2, long nWink, const SdrObject* pObj);
+    void         Shear (const Point& rRef, long nWink, double tn, FASTBOOL bVShear, const SdrObject* pObj);
+};
+
+#define SDRGLUEPOINT_NOTFOUND 0xFFFF
+
+class SVX_DLLPUBLIC SdrGluePointList {
+    Container aList;
+protected:
+    SdrGluePoint* GetObject(USHORT i) const { return (SdrGluePoint*)(aList.GetObject(i)); }
+public:
+    SdrGluePointList(): aList(1024,4,4) {}
+    SdrGluePointList(const SdrGluePointList& rSrcList): aList(1024,4,4)     { *this=rSrcList; }
+    ~SdrGluePointList()                                                     { Clear(); }
+    void                Clear();
+    void                operator=(const SdrGluePointList& rSrcList);
+    USHORT              GetCount() const                                    { return USHORT(aList.Count()); }
+    // Beim Insert wird dem Objekt (also dem GluePoint) automatisch eine Id zugewiesen.
+    // ReturnCode ist der Index des neuen GluePoints in der Liste
+    USHORT              Insert(const SdrGluePoint& rGP);
+    void                Delete(USHORT nPos)                                 { delete (SdrGluePoint*)aList.Remove(nPos); }
+    SdrGluePoint&       operator[](USHORT nPos)                             { return *GetObject(nPos); }
+    const SdrGluePoint& operator[](USHORT nPos) const                       { return *GetObject(nPos); }
+    USHORT              FindGluePoint(USHORT nId) const;
+    USHORT              HitTest(const Point& rPnt, const OutputDevice& rOut, const SdrObject* pObj, FASTBOOL bBack=FALSE, FASTBOOL bNext=FALSE, USHORT nId0=0) const;
+    void                DrawAll(OutputDevice& rOut, const SdrObject* pObj) const;
+    void                Invalidate(Window& rWin, const SdrObject* pObj) const;
+    // Temporaer zu setzen fuer Transformationen am Bezugsobjekt
+    void                SetReallyAbsolute(FASTBOOL bOn, const SdrObject& rObj);
+    void                Rotate(const Point& rRef, long nWink, double sn, double cs, const SdrObject* pObj);
+    void                Mirror(const Point& rRef1, const Point& rRef2, const SdrObject* pObj);
+    void                Mirror(const Point& rRef1, const Point& rRef2, long nWink, const SdrObject* pObj);
+    void                Shear (const Point& rRef, long nWink, double tn, FASTBOOL bVShear, const SdrObject* pObj);
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif //_SVDGLUE_HXX
+
---
+++ svx/inc/svx/svdhdl.hxx
@@ -0,0 +1,512 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svdhdl.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:21:10 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVDHDL_HXX
+#define _SVDHDL_HXX
+
+#ifndef _GEN_HXX //autogen
+#include <tools/gen.hxx>
+#endif
+
+#ifndef _POINTR_HXX //autogen
+#include <vcl/pointr.hxx>
+#endif
+
+#ifndef _CONTNR_HXX //autogen
+#include <tools/contnr.hxx>
+#endif
+
+#ifndef _SOLAR_HRC
+#include <svtools/solar.hrc>
+#endif
+
+#include <svx/xpoly.hxx>
+#include <svx/svdoedge.hxx>
+
+#ifndef _SDR_OVERLAY_OVERLAYOBJECTLIST_HXX
+#include <svx/sdr/overlay/overlayobjectlist.hxx>
+#endif
+
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
+#include <svx/xpoly.hxx>
+#include <svx/svdoedge.hxx>
+
+class VirtualDevice;
+class OutputDevice;
+class Region;
+class Window;
+class SdrHdlList;
+class SdrMarkView;
+class SdrObject;
+class SdrPageView;
+class SdrHdlBitmapSet;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Jedes Objekt muss in der Lage seine Handles zu erzeugen. Diese werden dann
+// bei einer Selektion abgeholt, bei der View angemeldet und sichtbar gemacht.
+// Wird ein Handle von der Maus beruehrt (IsHit()), so wird von der View der
+// entsprechende Mauszeiger vom Handle abgeholt und der App auf Anfrage zum
+// reinschalten uebergeben.
+// Handles wie z.B. der Rotationsmittelpunkt oder die Spiegelachse werden von
+// der View generiert, wenn vom Controller der entsprechende Transformations-
+// Modus selektiert wird.
+// HDL_MOVE...HDL_LWRGT muessen im enum immer zusammen stehen bleiben!
+
+enum SdrHdlKind
+{
+    HDL_MOVE,       // Handle zum Verschieben des Objekts
+    HDL_UPLFT,      // Oben links
+    HDL_UPPER,      // Oben
+    HDL_UPRGT,      // Oben rechts
+    HDL_LEFT,       // Links
+    HDL_RIGHT,      // Rechts
+    HDL_LWLFT,      // Unten links
+    HDL_LOWER,      // Unten
+    HDL_LWRGT,      // Unten rechts
+    HDL_POLY,       // Punktselektion an Polygon oder Bezierkurve
+    HDL_BWGT,       // Gewicht an einer Bezierkurve
+    HDL_CIRC,       // Winkel an Kreissegmenten, Eckenradius am Rect
+    HDL_REF1,       // Referenzpunkt 1, z.B. Rotationsmitte
+    HDL_REF2,       // Referenzpunkt 2, z.B. Endpunkt der Spiegelachse
+    HDL_MIRX,       // Die Spiegelachse selbst
+    HDL_GLUE,       // GluePoint
+    HDL_ANCHOR,     // anchor symbol (SD, SW)
+    HDL_TRNS,       // interactive transparence
+    HDL_GRAD,       // interactive gradient
+    HDL_COLR,       // interactive color
+    HDL_USER,
+    HDL_ANCHOR_TR,  // #101688# Anchor handle with (0,0) at top right for SW
+
+    // for SJ and the CustomShapeHandles:
+    HDL_CUSTOMSHAPE1
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum BitmapColorIndex
+{
+    LightGreen,
+    Cyan,
+    LightCyan,
+    Red,
+    LightRed,
+    Yellow
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum BitmapMarkerKind
+{
+    Rect_7x7,
+    Rect_9x9,
+    Rect_11x11,
+    Rect_13x13,
+    Circ_7x7,
+    Circ_9x9,
+    Circ_11x11,
+    Elli_7x9,
+    Elli_9x11,
+    Elli_9x7,
+    Elli_11x9,
+    RectPlus_7x7,
+    RectPlus_9x9,
+    RectPlus_11x11,
+    Crosshair,
+    Glue,
+    Anchor,
+
+    // #98388# add AnchorPressed to be able to aninate anchor control, too.
+    AnchorPressed,
+
+    // #101688# AnchorTR for SW
+    AnchorTR,
+    AnchorPressedTR,
+
+    // for SJ and the CustomShapeHandles:
+    Customshape1
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class SVX_DLLPUBLIC SdrHdl
+{
+    friend class                SdrMarkView; // fuer den Zugriff auf nObjHdlNum
+    friend class                SdrHdlList;
+
+    static SdrHdlBitmapSet*     pSimpleSet;
+    static SdrHdlBitmapSet*     pModernSet;
+
+    // #101928#
+    static SdrHdlBitmapSet*     pHighContrastSet;
+
+    // #101928#
+    BitmapEx ImpGetBitmapEx(BitmapMarkerKind eKindOfMarker, sal_uInt16 nInd, sal_Bool bFine, sal_Bool bIsHighContrast);
+
+protected:
+    SdrObject*                  pObj;      // Gehoert das Handle zu einem Objekt?
+    SdrPageView*                pPV;       // Gehoert das Handle zu einem Objekt in einer bestimmten PageView?
+    SdrHdlList*                 pHdlList;  // Zum Feststelen der Handlegroesse
+
+    // OVERLAYMANAGER
+    ::sdr::overlay::OverlayObjectList           maOverlayGroup;
+
+    Point                       aPos;
+
+    SdrHdlKind                  eKind;
+
+    long                        nDrehWink; // Handles bzw. Mauszeiger drehen
+    sal_uInt32                  nObjHdlNum; // wird von der MarkView benoetigt
+    sal_uInt32                  nPolyNum;  // Polygonpunktes
+    sal_uInt32                  nPPntNum;  // Punktnummer des Polygons
+    sal_uInt32                  nSourceHdlNum; // ist noch vollstaendig zu implementieren
+
+    unsigned                    bSelect : 1;   // Ein selektierter Polygonpunkt?
+    unsigned                    b1PixMore : 1; // True=Handle wird 1 Pixel groesser dargestellt
+    unsigned                    bPlusHdl : 1;  // u.a. fuer Hld-Paint Optimierung bei MarkPoint/UnmarkPoint, ...
+
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+    // cleanup marker if one exists
+    void GetRidOfIAObject();
+
+protected:
+    ::sdr::overlay::OverlayObject* CreateOverlayObject(
+        const basegfx::B2DPoint& rPos,
+        BitmapColorIndex eColIndex, BitmapMarkerKind eKindOfMarker, Point aMoveOutsideOffset = Point());
+    BitmapMarkerKind GetNextBigger(BitmapMarkerKind eKnd) const;
+
+public:
+    SdrHdl();
+    SdrHdl(const Point& rPnt, SdrHdlKind eNewKind=HDL_MOVE);
+    virtual ~SdrHdl();
+
+    const ::sdr::overlay::OverlayObjectList& getOverlayObjectList() const { return maOverlayGroup; }
+
+    void SetHdlList(SdrHdlList* pList);
+    SdrHdlKind GetKind() const { return eKind; }
+    void Touch();
+
+    const Point& GetPos() const { return aPos; }
+    void SetPos(const Point& rPnt);
+
+    SdrPageView* GetPageView() const { return pPV; }
+    void SetPageView(SdrPageView* pNewPV) { pPV=pNewPV; }
+
+    SdrObject* GetObj() const { return pObj;  }
+    void SetObj(SdrObject* pNewObj);
+
+    BOOL IsSelected() const { return bSelect; }
+    void SetSelected(BOOL bJa=TRUE);
+
+    void Set1PixMore(BOOL bJa=TRUE);
+    void SetDrehWink(long n);
+
+    BOOL IsCornerHdl() const { return eKind==HDL_UPLFT || eKind==HDL_UPRGT || eKind==HDL_LWLFT || eKind==HDL_LWRGT; }
+    BOOL IsVertexHdl() const { return eKind==HDL_UPPER || eKind==HDL_LOWER || eKind==HDL_LEFT  || eKind==HDL_RIGHT; }
+
+    void SetObjHdlNum(sal_uInt32 nNum) { nObjHdlNum=nNum; }
+    sal_uInt32 GetObjHdlNum() const { return nObjHdlNum; }
+
+    void SetPolyNum(sal_uInt32 nNum) { nPolyNum=nNum; }
+    sal_uInt32 GetPolyNum() const { return nPolyNum; }
+
+    void SetPointNum(sal_uInt32 nNum) { nPPntNum=nNum; }
+    sal_uInt32 GetPointNum() const { return nPPntNum; }
+
+    void SetPlusHdl(BOOL bOn) { bPlusHdl=bOn; }
+    BOOL IsPlusHdl() const { return bPlusHdl; }
+
+    void SetSourceHdlNum(sal_uInt32 nNum) { nSourceHdlNum=nNum; }
+    sal_uInt32 GetSourceHdlNum() const { return nSourceHdlNum; }
+
+    virtual Pointer GetPointer() const;
+    virtual bool IsHdlHit(const Point& rPnt) const;
+
+    // #97016# II
+    virtual BOOL IsFocusHdl() const;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define SDR_HANDLE_COLOR_SIZE_NORMAL            Size(13, 13)
+#define SDR_HANDLE_COLOR_SIZE_SELECTED          Size(17, 17)
+
+class SVX_DLLPUBLIC SdrHdlColor : public SdrHdl
+{
+private:
+    // size of colr markers
+    Size                        aMarkerSize;
+
+    // color
+    Color                       aMarkerColor;
+
+    // callback link when value changed
+    Link                        aColorChangeHdl;
+
+    // use luminance values only
+    unsigned                    bUseLuminance : 1;
+
+    // create marker for this kind
+    SVX_DLLPRIVATE virtual void CreateB2dIAObject();
+
+    // help functions
+    SVX_DLLPRIVATE Bitmap CreateColorDropper(Color aCol);
+    SVX_DLLPRIVATE Color GetLuminance(const Color& rCol);
+    SVX_DLLPRIVATE void CallColorChangeLink();
+
+public:
+    SdrHdlColor(const Point& rRef, Color aCol = Color(COL_BLACK), const Size& rSize = Size(11, 11), BOOL bLum = FALSE);
+    virtual ~SdrHdlColor();
+
+    BOOL IsUseLuminance() const { return bUseLuminance; }
+
+    Color GetColor() const { return aMarkerColor; }
+    void SetColor(Color aNew, BOOL bCallLink = FALSE);
+
+    const Size& GetSize() const { return aMarkerSize; }
+    void SetSize(const Size& rNew);
+
+    void SetColorChangeHdl(const Link& rLink) { aColorChangeHdl = rLink; }
+    const Link& GetColorChangeHdl() const { return aColorChangeHdl; }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class SdrHdlGradient : public SdrHdl
+{
+private:
+    // pointer to used color handles
+    SdrHdlColor*                pColHdl1;
+    SdrHdlColor*                pColHdl2;
+
+    // 2nd position
+    Point                       a2ndPos;
+
+    // is this a gradient or a transparence
+    unsigned                    bGradient : 1;
+
+    // select which handle to move
+    unsigned                    bMoveSingleHandle : 1;
+    unsigned                    bMoveFirstHandle : 1;
+
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+public:
+    SdrHdlGradient(const Point& rRef1, const Point& rRef2, BOOL bGrad = TRUE);
+    virtual ~SdrHdlGradient();
+
+    BOOL IsGradient() const { return bGradient; }
+
+    // set the associated color handles
+    void SetColorHandles(SdrHdlColor* pL1, SdrHdlColor* pL2) { pColHdl1 = pL1; pColHdl2 = pL2; }
+    SdrHdlColor* GetColorHdl1() const { return pColHdl1; }
+    SdrHdlColor* GetColorHdl2() const { return pColHdl2; }
+
+    const Point& Get2ndPos() const { return a2ndPos; }
+    void Set2ndPos(const Point& rPnt);
+
+    // the link called by the color handles
+    DECL_LINK(ColorChangeHdl, SdrHdl*);
+
+    // transformation call, create gradient from this handle
+    void FromIAOToItem(SdrObject* pObj, BOOL bSetItemOnObject, BOOL bUndo);
+
+    // selection flags for interaction
+    BOOL IsMoveSingleHandle() const { return bMoveSingleHandle; }
+    void SetMoveSingleHandle(BOOL bNew) { bMoveSingleHandle = bNew; }
+    BOOL IsMoveFirstHandle() const { return bMoveFirstHandle; }
+    void SetMoveFirstHandle(BOOL bNew) { bMoveFirstHandle = bNew; }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Spiegelachse
+class SdrHdlLine: public SdrHdl
+{
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+protected:
+    SdrHdl*                     pHdl1;
+    SdrHdl*                     pHdl2;
+
+public:
+    SdrHdlLine(SdrHdl& rHdl1, SdrHdl& rHdl2, SdrHdlKind eNewKind=HDL_MIRX) { eKind=eNewKind; pHdl1=&rHdl1; pHdl2=&rHdl2; }
+    virtual ~SdrHdlLine();
+
+    virtual Pointer GetPointer() const;
+    virtual bool IsHdlHit(const Point& rPnt) const;
+};
+
+// Ein SdrHdlBezWgt hat Kenntnis von seinem "BasisHandle". Seine Draw-Methode
+// zeichnet zusaetzlich eine Linie von seiner Position zur Position dieses
+// BasisHandles.
+class SdrHdlBezWgt: public SdrHdl
+{
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+protected:
+    const SdrHdl* pHdl1;
+
+public:
+    // das ist kein Copy-Ctor!!!
+    SdrHdlBezWgt(const SdrHdl* pRefHdl1, SdrHdlKind eNewKind=HDL_BWGT) { eKind=eNewKind; pHdl1=pRefHdl1; }
+    virtual ~SdrHdlBezWgt();
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class E3dVolumeMarker : public SdrHdl
+{
+    basegfx::B2DPolyPolygon             aWireframePoly;
+
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+public:
+    E3dVolumeMarker(const basegfx::B2DPolyPolygon& rWireframePoly);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ImpEdgeHdl: public SdrHdl
+{
+    SdrEdgeLineCode eLineCode;
+
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+public:
+    ImpEdgeHdl(const Point& rPnt, SdrHdlKind eNewKind): SdrHdl(rPnt,eNewKind),eLineCode(MIDDLELINE) {}
+    virtual ~ImpEdgeHdl();
+
+    void SetLineCode(SdrEdgeLineCode eCode);
+    SdrEdgeLineCode GetLineCode() const     { return eLineCode; }
+    BOOL IsHorzDrag() const;
+    virtual Pointer GetPointer() const;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ImpMeasureHdl: public SdrHdl
+{
+    // create marker for this kind
+    virtual void CreateB2dIAObject();
+
+public:
+    ImpMeasureHdl(const Point& rPnt, SdrHdlKind eNewKind): SdrHdl(rPnt,eNewKind) {}
+    virtual ~ImpMeasureHdl();
+
+    virtual Pointer GetPointer() const;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// #97016# II
+class ImplHdlListData;
+
+class SVX_DLLPUBLIC SdrHdlList
+{
+protected:
+    sal_uInt32                  mnFocusIndex;
+    SdrMarkView*                pView;
+    Container                   aList;
+    USHORT                      nHdlSize;
+
+    unsigned                    bRotateShear : 1;
+    unsigned                    bDistortShear : 1;
+    unsigned                    bMoveOutside : 1;      // Handles nach aussen ruecken (fuer TextEdit)
+    unsigned                    bFineHandles : 1;
+
+private:
+    SVX_DLLPRIVATE SdrHdlList(const SdrHdlList&): aList(1024,64,64)  {}
+    SVX_DLLPRIVATE void operator=(const SdrHdlList&)                 {}
+    SVX_DLLPRIVATE BOOL operator==(const SdrHdlList&) const      { return FALSE; }
+    SVX_DLLPRIVATE BOOL operator!=(const SdrHdlList&) const      { return FALSE; }
+
+public:
+    SdrHdlList(SdrMarkView* pV);
+    ~SdrHdlList();
+    void Clear();
+
+    // #97016# II
+    void TravelFocusHdl(sal_Bool bForward);
+    SdrHdl* GetFocusHdl() const;
+    void SetFocusHdl(SdrHdl* pNew);
+    void ResetFocusHdl();
+
+    // Access to View
+    SdrMarkView* GetView() const;
+
+    // Sortierung: 1.Level Erst Refpunkt-Handles, dann normale Handles, dann Glue, dann User, dann Plushandles
+    //             2.Level PageView (Pointer)
+    //             3.Level Position (x+y)
+    void     Sort();
+    ULONG    GetHdlCount() const                       { return aList.Count(); }
+    SdrHdl*  GetHdl(ULONG nNum) const                  { return (SdrHdl*)(aList.GetObject(nNum)); }
+    ULONG    GetHdlNum(const SdrHdl* pHdl) const;
+    void     SetHdlSize(USHORT nSiz);
+    USHORT   GetHdlSize() const                        { return nHdlSize; }
+    void     SetMoveOutside(BOOL bOn);
+    BOOL IsMoveOutside() const                     { return bMoveOutside; }
+    void     SetRotateShear(BOOL bOn);
+    BOOL IsRotateShear() const                     { return bRotateShear; }
+    void     SetDistortShear(BOOL bOn);
+    BOOL IsDistortShear() const                    { return bDistortShear; }
+    void     SetFineHdl(BOOL bOn);
+    BOOL IsFineHdl() const                        { return bFineHandles; }
+
+    // AddHdl uebernimmt das Handle in sein Eigentum. Es muss
+    // also auf dem Heap stehen, da Clear() ein delete macht.
+    void    AddHdl(SdrHdl* pHdl, BOOL bAtBegin=FALSE);
+    SdrHdl* RemoveHdl(ULONG nNum);
+
+    // Zuletzt eingefuegte Handles werden am ehesten getroffen
+    // (wenn Handles uebereinander liegen).
+    SdrHdl* IsHdlListHit(const Point& rPnt, BOOL bBack=FALSE, BOOL bNext=FALSE, SdrHdl* pHdl0=NULL) const;
+    SdrHdl* GetHdl(SdrHdlKind eKind1) const;
+};
+
+#endif //_SVDHDL_HXX
+
