f16e0c84e959977f7ebfd18ebd64f737954b5d72
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 15:20:01 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/04/04 15:11:12 vg 1.1.2.2: resync to SRC680_m207 2007/02/09 16:53:47 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

---
+++ svx/inc/svx/svdedxv.hxx
@@ -0,0 +1,284 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svdedxv.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:19:47 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVDEDXV_HXX
+#define _SVDEDXV_HXX
+
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
+#ifndef _SVDGLEV_HXX
+#include <svx/svdglev.hxx>
+#endif
+
+//************************************************************
+//   Vorausdeklarationen
+//************************************************************
+
+class SdrOutliner;
+class OutlinerView;
+class EditStatus;
+class EditFieldInfo;
+class ImpSdrEditPara;
+
+namespace com { namespace sun { namespace star { namespace uno {
+    class Any;
+} } } }
+
+//************************************************************
+//   Defines
+//************************************************************
+
+enum SdrEndTextEditKind {SDRENDTEXTEDIT_UNCHANGED, // Textobjekt unveraendert
+                         SDRENDTEXTEDIT_CHANGED,   // Textobjekt wurde geaendert
+                         SDRENDTEXTEDIT_DELETED,   // Textobjekt implizit geloescht
+                         SDRENDTEXTEDIT_SHOULDBEDELETED}; // Fuer Writer: Textobjekt sollte geloescht werden
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   @@@@  @@@@@  @@@@@@  @@@@@ @@@@@  @@ @@@@@@  @@ @@ @@ @@@@@ @@   @@
+//  @@  @@ @@  @@     @@  @@    @@  @@ @@   @@    @@ @@ @@ @@    @@   @@
+//  @@  @@ @@  @@     @@  @@    @@  @@ @@   @@    @@ @@ @@ @@    @@ @ @@
+//  @@  @@ @@@@@      @@  @@@@  @@  @@ @@   @@    @@@@@ @@ @@@@  @@@@@@@
+//  @@  @@ @@  @@     @@  @@    @@  @@ @@   @@     @@@  @@ @@    @@@@@@@
+//  @@  @@ @@  @@ @@  @@  @@    @@  @@ @@   @@     @@@  @@ @@    @@@ @@@
+//   @@@@  @@@@@   @@@@   @@@@@ @@@@@  @@   @@      @   @@ @@@@@ @@   @@
+//
+// - Allgemeines Edit fuer objektspeziefische Eigenschaften
+// - Textedit fuer alle vom SdrTextObj abgeleiteten Zeichenobjekte
+// - Macromodus
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class SVX_DLLPUBLIC SdrObjEditView: public SdrGlueEditView
+{
+    friend class                SdrPageView;
+    friend class                ImpSdrEditPara;
+
+protected:
+    // TextEdit
+    SdrObjectWeakRef            mxTextEditObj;          // Aktuell im TextEdit befindliches Obj
+    SdrPageView*                pTextEditPV;
+    SdrOutliner*                pTextEditOutliner;     // Na eben der Outliner fuers TextEdit
+    OutlinerView*               pTextEditOutlinerView; // die aktuelle View des Outliners
+    Window*                     pTextEditWin;          // passendes Win zu pTextEditOutlinerView
+    Cursor*                     pTextEditCursorMerker; // Zum Restaurieren des Cursors am jeweiligen Win
+    ImpSdrEditPara*             pEditPara; // Da hau' ich erstmal alles rein um kompatibel zu bleiben...
+    SdrObject*                  pMacroObj;
+    SdrPageView*                pMacroPV;
+    Window*                     pMacroWin;
+
+    Rectangle                   aTextEditArea;
+    Rectangle                   aMinTextEditArea;
+    Link                        aOldCalcFieldValueLink; // Zum rufen des alten Handlers
+    Point                       aMacroDownPos;
+
+    USHORT                      nMacroTol;
+
+    unsigned                    bTextEditDontDelete : 1;   // Outliner und View bei SdrEndTextEdit nicht deleten (f. Rechtschreibpruefung)
+    unsigned                    bTextEditOnlyOneView : 1;  // Nur eine OutlinerView (f. Rechtschreibpruefung)
+    unsigned                    bTextEditNewObj : 1;       // Aktuell editiertes Objekt wurde gerade neu erzeugt
+    unsigned                    bQuickTextEditMode : 1;    // persistent(->CrtV). Default=TRUE
+    unsigned                    bMacroMode : 1;            // persistent(->CrtV). Default=TRUE
+    unsigned                    bMacroDown : 1;
+
+private:
+    SVX_DLLPRIVATE void ImpClearVars();
+
+protected:
+    OutlinerView* ImpFindOutlinerView(Window* pWin) const;
+
+    // Eine neue OutlinerView auf dem Heap anlegen und alle erforderlichen Parameter setzen.
+    // pTextEditObj, pTextEditPV und pTextEditOutliner muessen initiallisiert sein.
+    OutlinerView* ImpMakeOutlinerView(Window* pWin, BOOL bNoPaint, OutlinerView* pGivenView) const;
+    void ImpPaintOutlinerView(OutlinerView& rOutlView, const Rectangle& rRect) const;
+    void ImpInvalidateOutlinerView(OutlinerView& rOutlView) const;
+
+    // Hintergrundfarbe fuer die Outlinerviews bestimmen
+    Color ImpGetTextEditBackgroundColor() const;
+
+    // Feststellen, ob der gesamte Text markiert ist. Liefert auch TRUE wenn
+    // kein Text vorhanden ist.
+    BOOL ImpIsTextEditAllSelected() const;
+    void ImpMakeTextCursorAreaVisible();
+
+    // Handler fuer AutoGrowing Text bei aktivem Outliner
+    DECL_LINK(ImpOutlinerStatusEventHdl,EditStatus*);
+    DECL_LINK(ImpOutlinerCalcFieldValueHdl,EditFieldInfo*);
+
+    void ImpMacroUp(const Point& rUpPos);
+    void ImpMacroDown(const Point& rDownPos);
+
+protected:
+    // #i71538# make constructors of SdrView sub-components protected to avoid incomplete incarnations which may get casted to SdrView
+    SdrObjEditView(SdrModel* pModel1, OutputDevice* pOut = 0L);
+    virtual ~SdrObjEditView();
+
+public:
+    // Actionhandling fuer Macromodus
+    virtual BOOL IsAction() const;
+    virtual void MovAction(const Point& rPnt);
+    virtual void EndAction();
+    virtual void BrkAction();
+    virtual void BckAction();
+    virtual void TakeActionRect(Rectangle& rRect) const;
+
+    virtual void SFX_NOTIFY(SfxBroadcaster& rBC, const TypeId& rBCType, const SfxHint& rHint, const TypeId& rHintType);
+    virtual void ModelHasChanged();
+
+    //************************************************************************
+    // TextEdit ueber einen Outliner
+    //************************************************************************
+    // QuickTextEditMode bedeutet, dass Objekte mit Text sofort beim Anklicken
+    // editiert werden sollen. Default=TRUE. Persistent.
+    void SetQuickTextEditMode(BOOL bOn) { bQuickTextEditMode=bOn; }
+    BOOL IsQuickTextEditMode() const { return bQuickTextEditMode; }
+
+    // Starten des TextEditMode. Ist pWin==NULL, wird das erste an der View
+    // angemeldete Win verwendet.
+    // Der Cursor des Fensters an dem Editiert wird wird bei
+    // SdrBeginTextEdit() gemerkt und bei SdrEndTextEdit() wieder restauriert.
+    // Die App muss sicherstellen, das die zum Zeitpunkt des BegEdit am
+    // Windows angemeldete Cursorinstanz beim SdrEndTextEdit noch gueltig ist.
+    // Ueber den Parameter pEditOutliner kann die Applikation einen eigenen
+    // Outliner vorgeben, der zum Editieren verwendet wird. Dieser gehoert
+    // nach Aufruf von SdrBeginTextEdit der SdrObjEditView und wird von dieser
+    // spaeter via delete zerstoert (falls bDontDeleteOutliner=FALSE). Die
+    // SdrObjEditView setzt dann das Modusflag (EditEngine/Outliner) an
+    // dieser Instanz und ausserdem auch den StatusEventHdl.
+    // Ebenso kann eine spezifische OutlinerView vorgegeben werden.
+    sal_Bool SdrBeginTextEdit(SdrObject* pObj, SdrPageView* pPV = 0L, Window* pWin = 0L,
+        SdrOutliner* pGivenOutliner = 0L, OutlinerView* pGivenOutlinerView = 0L,
+        sal_Bool bDontDeleteOutliner = sal_False, sal_Bool bOnlyOneView = sal_False);
+    sal_Bool SdrBeginTextEdit(SdrObject* pObj, SdrPageView* pPV = 0L, Window* pWin = 0L, sal_Bool bIsNewObj = sal_False,
+        SdrOutliner* pGivenOutliner = 0L, OutlinerView* pGivenOutlinerView = 0L,
+        sal_Bool bDontDeleteOutliner = sal_False, sal_Bool bOnlyOneView = sal_False, sal_Bool bGrabFocus = sal_True);
+    // bDontDeleteReally ist ein Spezialparameter fuer den Writer.
+    // Ist dieses Flag gesetzt, dann wird ein evtl. leeres Textobjekt
+    // nicht geloescht. Stattdessen gibt es dann einen Returncode
+    // SDRENDTEXTEDIT_SHOULDBEDELETED (anstelle von SDRENDTEXTEDIT_BEDELETED)
+    // der besagt, dass das Objekt geloescht werden sollte.
+    SdrEndTextEditKind SdrEndTextEdit(sal_Bool bDontDeleteReally = sal_False);
+    virtual bool IsTextEdit() const;
+
+    // TRUE=Es wird ein Textrahmen (OBJ_TEXT,OBJ_OUTLINETEXT,...) editiert
+    // ansonsten handelt es sich um ein beschriftetes Zeichenobjekt, an dem
+    // der Text ja bekanntlich hor. und vert. zentriert wird.
+    BOOL IsTextEditFrame() const;
+
+    // Diese Methode liefert TRUE, wenn der Punkt rHit innerhalb der
+    // des Objektbereichs oder der OutlinerView liegt.
+    BOOL IsTextEditHit(const Point& rHit, short nTol) const;
+
+    // Diese Methode liefert TRUE, wenn der Punkt rHit innerhalb des
+    // Handle-dicken Rahmens liegt, der die OutlinerView bei TextFrames
+    // umschliesst.
+    BOOL IsTextEditFrameHit(const Point& rHit) const;
+
+    // Bei aktiver Selektion, also zwischen MouseButtonDown und
+    // MouseButtonUp liefert diese Methode immer TRUE.
+    BOOL IsTextEditInSelectionMode() const;
+
+    // Folgende Methode addiert einen passenden Offset zum MouseEvent
+    // um diesen an den Outliner weiterzureichen.
+    void AddTextEditOfs(MouseEvent& rMEvt) const;
+
+    // Wer das z.Zt. im TextEdit befindliche Objekt braucht:
+    SdrObject* GetTextEditObject() const { return mxTextEditObj.get(); }
+
+    // info about TextEditPageView. Default is 0L.
+    virtual SdrPageView* GetTextEditPageView() const;
+
+    // Das aktuelle Win des Outliners
+    Window* GetTextEditWin() const { return pTextEditWin; }
+    void SetTextEditWin(Window* pWin);
+
+    // An den hier abgeholten Outliner kann man schliesslich
+    // Events versenden, Attribute setzen, Cut/Copy/Paste rufen,
+    // Undo/Redo rufen, etc.
+    const SdrOutliner* GetTextEditOutliner() const { return pTextEditOutliner; }
+    SdrOutliner* GetTextEditOutliner() { return pTextEditOutliner; }
+    const OutlinerView* GetTextEditOutlinerView() const { return pTextEditOutlinerView; }
+    OutlinerView* GetTextEditOutlinerView() { return pTextEditOutlinerView; }
+
+    BOOL KeyInput(const KeyEvent& rKEvt, Window* pWin);
+    virtual BOOL MouseButtonDown(const MouseEvent& rMEvt, Window* pWin);
+    virtual BOOL MouseButtonUp(const MouseEvent& rMEvt, Window* pWin);
+    virtual BOOL MouseMove(const MouseEvent& rMEvt, Window* pWin);
+    virtual BOOL Command(const CommandEvent& rCEvt, Window* pWin);
+    BOOL Cut(ULONG nFormat=SDR_ANYFORMAT);
+    BOOL Yank(ULONG nFormat=SDR_ANYFORMAT);
+    BOOL Paste(Window* pWin=NULL, ULONG nFormat=SDR_ANYFORMAT);
+
+    // #97766# make virtual to change implementation e.g. for SdOutlineView
+    virtual sal_uInt16 GetScriptType() const;
+
+    /* new interface src537 */
+    BOOL GetAttributes(SfxItemSet& rTargetSet, BOOL bOnlyHardAttr=FALSE) const;
+
+    BOOL SetAttributes(const SfxItemSet& rSet, BOOL bReplaceAll);
+    SfxStyleSheet* GetStyleSheet() const; // SfxStyleSheet* GetStyleSheet(BOOL& rOk) const;
+    BOOL SetStyleSheet(SfxStyleSheet* pStyleSheet, BOOL bDontRemoveHardAttr);
+
+    // Intern: Beim Splitteraufziehen neue OutlinerView...
+    virtual void AddWindowToPaintView(OutputDevice* pNewWin);
+    virtual void DeleteWindowFromPaintView(OutputDevice* pOldWin);
+
+    //************************************************************************
+    // Object-MacroModus (z.B. Rect als Button oder sowas):
+    //************************************************************************
+    // Persistent. Default TRUE. SvDraw wertet das Flag u.a. bei
+    // SdrView::GetPreferedPointer() aus. Hat nur Wirkung, wenn das Dokument
+    // Draw-Objekte mit Macrofunktionalitaet hat (SdrObject::HasMacro()==TRUE).
+    void SetMacroMode(BOOL bOn) { bMacroMode=bOn; }
+    BOOL IsMacroMode() const { return bMacroMode; }
+    BOOL BegMacroObj(const Point& rPnt, short nTol, SdrObject* pObj, SdrPageView* pPV, Window* pWin);
+    BOOL BegMacroObj(const Point& rPnt, SdrObject* pObj, SdrPageView* pPV, Window* pWin) { return BegMacroObj(rPnt,-2,pObj,pPV,pWin); }
+    void MovMacroObj(const Point& rPnt);
+    void BrkMacroObj();
+    BOOL EndMacroObj();
+    BOOL IsMacroObj() const { return pMacroObj!=NULL; }
+    BOOL IsMacroObjDown() const { return bMacroDown; }
+
+    /** fills the given any with a XTextCursor for the current text selection.
+        Leaves the any untouched if there currently is no text selected */
+    void getTextSelection( ::com::sun::star::uno::Any& rSelection );
+};
+
+#endif //_SVDEDXV_HXX
+
---
+++ svx/inc/svx/svdetc.hxx
@@ -0,0 +1,362 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svdetc.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:20:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVDETC_HXX
+#define _SVDETC_HXX
+
+#ifndef _STRING_HXX //autogen
+#include <tools/string.hxx>
+#endif
+
+#ifndef _CONTNR_HXX //autogen
+#include <tools/contnr.hxx>
+#endif
+
+#ifndef _OUTDEV_HXX //autogen
+#include <vcl/outdev.hxx>
+#endif
+
+#ifndef _SHL_HXX //autogen
+#include <tools/shl.hxx>
+#endif
+
+#ifndef _OUTLINER_HXX //autogen
+#include <svx/outliner.hxx>
+#endif
+
+#ifndef INCLUDED_SVXDLLAPI_H
+#include "svx/svxdllapi.h"
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ExchangeFormat-Id der DrawingEngine holen. Daten koennen dann per
+//   static BOOL CopyData(pData,nLen,nFormat);
+// bereitgestellt werden, wobei pData/nLen einen SvMemoryStream beschreiben in
+// dem ein SdrModel gestreamt wird an dem fuer die Zeitdauer des Streamens das
+// Flag SdrModel::SetStreamingSdrModel(TRUE) gesetzt wird.
+// ULONG SdrGetExchangeFormat(); -- JP 18.01.99 - dafuer gibt es ein define
+
+class SdrOutliner;
+class SdrModel;
+class SvtSysLocale;
+class CharClass;
+class LocaleDataWrapper;
+
+namespace com { namespace sun { namespace star { namespace lang {
+    struct Locale;
+}}}}
+
+// Einen Outliner mit den engineglobalen
+// Defaulteinstellungen auf dem Heap erzeugen.
+// Ist pMod<>NULL, dann wird der MapMode des uebergebenen
+// Models verwendet. Die resultierende Default-Fonthoehe bleibt
+// jedoch dieselbe (die logische Fonthoehe wird umgerechnet).
+SVX_DLLPUBLIC SdrOutliner* SdrMakeOutliner( USHORT nOutlinerMode, SdrModel* pMod );
+
+// Globale Defaulteinstellungen fuer die DrawingEngine.
+// Diese Einstellungen sollte man direkt beim Applikationsstart
+// vornehmen, noch bevor andere Methoden der Engine gerufen werden.
+class SVX_DLLPUBLIC SdrEngineDefaults
+{
+friend class SdrAttrObj;
+    String     aFontName;
+    FontFamily eFontFamily;
+    Color      aFontColor;
+    ULONG      nFontHeight;
+    MapUnit    eMapUnit;
+    Fraction   aMapFraction;
+
+private:
+    SVX_DLLPRIVATE static SdrEngineDefaults& GetDefaults();
+
+public:
+    SdrEngineDefaults();
+    // Default Fontname ist "Times New Roman"
+    static void       SetFontName(const String& rFontName) { GetDefaults().aFontName=rFontName; }
+    static String     GetFontName()                        { return GetDefaults().aFontName; }
+    // Default FontFamily ist FAMILY_ROMAN
+    static void       SetFontFamily(FontFamily eFam)       { GetDefaults().eFontFamily=eFam; }
+    static FontFamily GetFontFamily()                      { return GetDefaults().eFontFamily; }
+    // Default FontColor ist COL_BLACK
+    static void       SetFontColor(const Color& rColor)    { GetDefaults().aFontColor=rColor; }
+    static Color      GetFontColor()                       { return GetDefaults().aFontColor; }
+    // Default FontHeight ist 847. Die Fonthoehe wird in logischen Einheiten
+    // (MapUnit/MapFraction (siehe unten)) angegeben. Die Defaulteinstellung
+    // 847/100mm entspricht also ca. 24 Point. Verwendet man stattdessen
+    // beispielsweise Twips (SetMapUnit(MAP_TWIP)) (20 Twip = 1 Point) muss
+    // man als Fonthoehe 480 angeben um 24 Point als default zu erhalten.
+    static void       SetFontHeight(ULONG nHeight)         { GetDefaults().nFontHeight=nHeight; }
+    static ULONG      GetFontHeight()                      { return GetDefaults().nFontHeight; }
+    // Der MapMode wird fuer den globalen Outliner benoetigt.
+    // Gleichzeitig bekommt auch jedes neu instanziierte SdrModel
+    // diesen MapMode default zugewiesen.
+    // Default MapUnit ist MAP_100TH_MM
+    static void       SetMapUnit(MapUnit eMap)             { GetDefaults().eMapUnit=eMap; }
+    static MapUnit    GetMapUnit()                         { return GetDefaults().eMapUnit; }
+    // Default MapFraction ist 1/1.
+    static void       SetMapFraction(const Fraction& rMap) { GetDefaults().aMapFraction=rMap; }
+    static Fraction   GetMapFraction()                     { return GetDefaults().aMapFraction; }
+    // Der Aufruf der folgenden Methode veranlasst die Engine
+    // ihre sprachabhaengigen Resourcen neu zu initiallisieren.
+    // Bereits bestehende Undotexte, etc. bleiben jedoch in der
+    // sprache erhalten, in der sie erzeugt wurden.
+    static void       LanguageHasChanged();
+
+
+    // Einen Outliner mit den engineglobalen
+    // Defaulteinstellungen auf dem Heap erzeugen.
+    // Ist pMod<>NULL, dann wird der MapMode des uebergebenen
+    // Models verwendet. Die resultierende Default-Fonthoehe bleibt
+    // jedoch dieselbe (die logische Fonthoehe wird umgerechnet).
+    friend SVX_DLLPUBLIC SdrOutliner* SdrMakeOutliner( USHORT nOutlinerMode, SdrModel* pMod );
+};
+
+class SfxItemSet;
+// Liefert eine Ersatzdarstellung fuer einen XFillStyle
+// Bei XFILL_NONE gibt's FALSE und rCol bleibt unveraendert.
+SVX_DLLPUBLIC FASTBOOL GetDraftFillColor(const SfxItemSet& rSet, Color& rCol);
+
+// Ein Container fuer USHORTs (im Prinzip ein dynamisches Array)
+class UShortCont {
+    Container aArr;
+public:
+    UShortCont(USHORT nBlock, USHORT nInit, USHORT nResize): aArr(nBlock,nInit,nResize) {}
+    void   Clear()                                           { aArr.Clear(); }
+    void   Insert(USHORT nElem, ULONG nPos=CONTAINER_APPEND) { aArr.Insert((void*)ULONG(nElem),nPos); }
+    void   Remove(ULONG nPos)                                { aArr.Remove(nPos); }
+    void   Replace(USHORT nElem, ULONG nPos)                 { aArr.Replace((void*)ULONG(nElem),nPos); }
+    USHORT GetObject(ULONG nPos)                       const { return USHORT(ULONG(aArr.GetObject(nPos))); }
+    ULONG  GetPos(USHORT nElem)                        const { return aArr.GetPos((void*)(ULONG)nElem); }
+    ULONG  GetCount()                                  const { return aArr.Count(); }
+    void   Sort();
+};
+
+class ContainerSorter {
+protected:
+    Container& rCont;
+private:
+//#if 0 // _SOLAR__PRIVATE
+    void ImpSubSort(long nL, long nR) const;
+//#endif // __PRIVATE
+public:
+    ContainerSorter(Container& rNewCont): rCont(rNewCont) {}
+    void DoSort(ULONG a=0, ULONG b=0xFFFFFFFF) const;
+#ifdef This_Is_Just_For_A_Comment
+    Compare() muss returnieren:
+      -1 falls *pElem1<*pElem2
+       0 falls *pElem1=*pElem2
+      +1 falls *pElem1>*pElem2
+#endif
+    virtual int Compare(const void* pElem1, const void* pElem2) const=0;
+private: // damit keiner vergessen wird
+virtual
+        void
+                 Is1stLessThan2nd(const void* pElem1, const void* pElem2) const;
+//  virtual FASTBOOL Is1stLessThan2nd(const void* pElem1, const void* pElem2) const=NULL;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//#if 0 // _SOLAR__PRIVATE
+#define SDRHDC_SAVEPEN                1 /* Save Linecolor                     */
+#define SDRHDC_SAVEBRUSH              2 /* Save Fillcolorn                    */
+#define SDRHDC_SAVEFONT               4 /* Save Font                          */
+#define SDRHDC_SAVEPENANDBRUSH        3 /* Save Line- and FillColor           */
+#define SDRHDC_SAVEPENANDBRUSHANDFONT 7 /* Save Font, Line- and fillcolor     */
+#define SDRHDC_SAVECLIPPING           8 /* Save Clipping                      */
+#define SDRHDC_SAVEALL               15 /* Save Clipping, Font, fill- and linecolor */
+
+class ImpClipMerk;
+class ImpColorMerk;
+class ImpSdrHdcMerk
+{
+    ImpColorMerk* pFarbMerk;
+    ImpClipMerk*  pClipMerk;
+    Color*        pLineColorMerk;
+    USHORT        nMode;
+public:
+    ImpSdrHdcMerk(const OutputDevice& rOut, USHORT nNewMode=SDRHDC_SAVEALL, FASTBOOL bAutoMerk=TRUE);
+    ~ImpSdrHdcMerk();
+    void Save(const OutputDevice& rOut);
+    FASTBOOL IsSaved() const                 { return pFarbMerk!=NULL || pClipMerk!=NULL || pLineColorMerk!=NULL; }
+    void Restore(OutputDevice& rOut, USHORT nMask=SDRHDC_SAVEALL) const;
+};
+//#endif // __PRIVATE
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Ein ItemSet auf Outliner- bzw. EditEngine-Items durchsuchen
+// Liefert TRUE, wenn der Set solchen Items enthaelt.
+BOOL SearchOutlinerItems(const SfxItemSet& rSet, BOOL bInklDefaults, BOOL* pbOnlyEE=NULL);
+
+// zurueck erhaelt man einen neuen WhichTable den
+// man dann irgendwann mit delete platthauen muss.
+USHORT* RemoveWhichRange(const USHORT* pOldWhichTable, USHORT nRangeBeg, USHORT nRangeEnd);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Link;
+
+// Hilfsklasse zur kommunikation zwischen dem Dialog
+// zum aufbrechen von Metafiles (sd/source/ui/dlg/brkdlg.cxx),
+// SdrEditView::DoImportMarkedMtf() und
+// ImpSdrGDIMetaFileImport::DoImport()
+class SVX_DLLPUBLIC SvdProgressInfo
+{
+private:
+    ULONG nSumActionCount;  // Summe aller Actions
+    ULONG nSumCurAction;    // Summe aller bearbeiteten Actions
+
+    ULONG nActionCount;     // Anzahl der Actions im akt. Obj.
+    ULONG nCurAction;       // Anzahl bearbeiteter Act. im akt. Obj.
+
+    ULONG nInsertCount;     // Anzahl einzufuegender Act. im akt. Obj.
+    ULONG nCurInsert;       // Anzahl bereits eingefuegter Actions
+
+    ULONG nObjCount;        // Anzahl der selektierten Objekte
+    ULONG nCurObj;          // Aktuelles Objekt
+
+    Link *pLink;
+
+public:
+    SvdProgressInfo( Link *_pLink );
+
+    void Init( ULONG _nSumActionCount, ULONG _nObjCount );
+
+    BOOL SetNextObject();
+
+    void SetActionCount( ULONG _nActionCount );
+    void SetInsertCount( ULONG _nInsertCount );
+
+    BOOL ReportActions( ULONG nAnzActions );
+    BOOL ReportInserts( ULONG nAnzInserts );
+
+    ULONG GetSumActionCount() const { return nSumActionCount; };
+    ULONG GetSumCurAction() const { return nSumCurAction; };
+    ULONG GetObjCount() const { return nObjCount; };
+    ULONG GetCurObj() const { return nCurObj; };
+
+    ULONG GetActionCount() const { return nActionCount; };
+    ULONG GetCurAction() const { return nCurAction; };
+
+    ULONG GetInsertCount() const { return nInsertCount; };
+    ULONG GetCurInsert() const { return nCurInsert; };
+
+    void ReportError();
+    BOOL ReportRescales( ULONG nAnzRescales );
+};
+
+
+
+class SdrLinkList
+{
+    Container aList;
+protected:
+    unsigned FindEntry(const Link& rLink) const;
+public:
+    SdrLinkList(): aList(1024,4,4)        {}
+    ~SdrLinkList()                        { Clear(); }
+    void Clear();
+    unsigned GetLinkCount() const            { return (unsigned)aList.Count(); }
+    Link& GetLink(unsigned nNum)             { return *((Link*)(aList.GetObject(nNum))); }
+    const Link& GetLink(unsigned nNum) const { return *((Link*)(aList.GetObject(nNum))); }
+    void InsertLink(const Link& rLink, unsigned nPos=0xFFFF);
+    void RemoveLink(const Link& rLink);
+    FASTBOOL HasLink(const Link& rLink) const { return FindEntry(rLink)!=0xFFFF; }
+};
+
+// Fuer die Factory in SvdObj.CXX
+SdrLinkList& ImpGetUserMakeObjHdl();
+SdrLinkList& ImpGetUserMakeObjUserDataHdl();
+
+class SdrOle2Obj;
+class AutoTimer;
+
+class OLEObjCache : public Container
+{
+    ULONG               nSize;
+    AutoTimer*          pTimer;
+
+    BOOL UnloadObj( SdrOle2Obj* pObj );
+    DECL_LINK( UnloadCheckHdl, AutoTimer* );
+
+public:
+    OLEObjCache();
+    ~OLEObjCache();
+
+    void SetSize(ULONG nNewSize);
+    void InsertObj(SdrOle2Obj* pObj);
+    void RemoveObj(SdrOle2Obj* pObj);
+};
+
+
+class SVX_DLLPUBLIC SdrGlobalData
+{
+public:
+    SdrLinkList         aUserMakeObjHdl;
+    SdrLinkList         aUserMakeObjUserDataHdl;
+    SdrOutliner*        pOutliner;
+    SdrEngineDefaults*  pDefaults;
+    ResMgr*             pResMgr;
+    ULONG               nExchangeFormat;
+    OLEObjCache         aOLEObjCache;
+    const SvtSysLocale*         pSysLocale;     // follows always locale settings
+    const CharClass*            pCharClass;     // follows always SysLocale
+    const LocaleDataWrapper*    pLocaleData;    // follows always SysLocale
+
+public:
+    SdrGlobalData();
+    ~SdrGlobalData();
+
+    OLEObjCache&        GetOLEObjCache() { return aOLEObjCache; }
+};
+
+inline SdrGlobalData& GetSdrGlobalData()
+{
+    void** ppAppData=GetAppData(SHL_SVD);
+    if (*ppAppData==NULL) {
+        *ppAppData=new SdrGlobalData;
+    }
+    return *((SdrGlobalData*)*ppAppData);
+}
+
+
+/////////////////////////////////////////////////////////////////////
+
+#endif //_SVDETC_HXX
+
