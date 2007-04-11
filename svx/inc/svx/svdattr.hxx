a641c6239a291844e2dffe8ce7099ad7a1a7a28a
Vladimir Glazounov <vg@openoffice.org>
2007-04-11 15:18:35 +0000

    INTEGRATION: CWS hedaburemove01 (1.1.2); FILE ADDED 2007/02/09 16:53:45 vg 1.1.2.1: #72503# get rid of hedabu procedure: Moving headers to svx/inc/svx and correspondent necessary changes

---
+++ svx/inc/svx/svdattr.hxx
@@ -0,0 +1,138 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svdattr.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-04-11 16:17:46 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVDATTR_HXX
+#define _SVDATTR_HXX
+
+#ifndef _SOLAR_HRC
+#include <svtools/solar.hrc>
+#endif
+
+#ifndef _SOLAR_H
+#include <tools/solar.h>
+#endif
+
+#ifndef _SDANGITM_HXX
+#include <svx/sdangitm.hxx>
+#endif
+#ifndef _SDERITM_HXX
+#include <svx/sderitm.hxx>
+#endif
+#ifndef _SDMETITM_HXX
+#include <svx/sdmetitm.hxx>
+#endif
+#ifndef _SDOOITM_HXX
+#include <svx/sdooitm.hxx>
+#endif
+#ifndef _SDPRCITM_HXX
+#include <svx/sdprcitm.hxx>
+#endif
+#ifndef _SDSHCITM_HXX
+#include <svx/sdshcitm.hxx>
+#endif
+#ifndef _SDSHITM_HXX
+#include <svx/sdshitm.hxx>
+#endif
+#ifndef _SDSHTITM_HXX
+#include <svx/sdshtitm.hxx>
+#endif
+#ifndef _SDSXYITM_HXX
+#include <svx/sdsxyitm.hxx>
+#endif
+#ifndef _SDTAAITM_HXX
+#include <svx/sdtaaitm.hxx>
+#endif
+#ifndef _SDTACITM_HXX
+#include <svx/sdtacitm.hxx>
+#endif
+#ifndef _SDTACITM_HXX
+#include <svx/sdtaditm.hxx>
+#endif
+#ifndef _SDTAGITM_HXX
+#include <svx/sdtagitm.hxx>
+#endif
+#ifndef _SDTAIITM_HXX
+#include <svx/sdtaiitm.hxx>
+#endif
+#ifndef _SDTAITM_HXX
+#include <svx/sdtaitm.hxx>
+#endif
+#ifndef _SDTAKITM_HXX
+#include <svx/sdtakitm.hxx>
+#endif
+#ifndef _SDTAYITM_HXX
+#include <svx/sdtayitm.hxx>
+#endif
+#ifndef SDTCFITM_HXX
+#include <svx/sdtcfitm.hxx>
+#endif
+#ifndef _SDTDITM_HXX
+#include <svx/sdtditm.hxx>
+#endif
+#ifndef _SDTFSITM_HXX
+#include <svx/sdtfsitm.hxx>
+#endif
+#ifndef _SDTMFITM_HXX
+#include <svx/sdtmfitm.hxx>
+#endif
+#ifndef _SDYNITM_HXX
+#include <svx/sdynitm.hxx>
+#endif
+#ifndef _SDGLUITM_HXX
+#include <svx/sdgluitm.hxx>
+#endif
+#ifndef _SDGINITM_HXX
+#include <svx/sdginitm.hxx>
+#endif
+#ifndef _SDGTRITM_HXX
+#include <svx/sdgtritm.hxx>
+#endif
+#ifndef _SDGCOITM_HXX
+#include <svx/sdgcoitm.hxx>
+#endif
+#ifndef _SDGGAITM_HXX
+#include <svx/sdggaitm.hxx>
+#endif
+#ifndef _SDASITM_HXX
+#include <svx/sdasitm.hxx>
+#endif
+#ifndef _SDGMOITM_HXX
+#include <svx/sdgmoitm.hxx>
+#endif
+#ifndef _SDASAITM_HXX
+#include <svx/sdasaitm.hxx>
+#endif
+#endif
+
