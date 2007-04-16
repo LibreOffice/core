2bcc07fbcebf2a4336369fa8c375284d528e9032
Ivo Hinkelmann <ihi@openoffice.org>
2007-04-16 14:36:50 +0000

    INTEGRATION: CWS fwkdbdesign01 (1.11.106); FILE MERGED 2007/03/06 08:43:02 fs 1.11.106.3: #i75146# XFormsWindowState 2007/03/05 08:49:33 fs 1.11.106.2: #i74951# WriterReportWindowState 2007/03/03 15:13:42 fs 1.11.106.1: #i74951# +WriterFormWindowState

--- officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: obo $ $Date: 2006-09-15 14:00:54 $
+#   last change: $Author: ihi $ $Date: 2007-04-16 15:36:50 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,6 +53,8 @@ XCSFILES= \
     ChartCommands.xcs \
     DbuCommands.xcs \
     BaseWindowState.xcs \
+    WriterFormWindowState.xcs \
+    WriterReportWindowState.xcs \
     DbBrowserWindowState.xcs \
     DrawImpressCommands.xcs \
     GenericCommands.xcs \
@@ -72,6 +74,7 @@ XCSFILES= \
     MathWindowState.xcs \
     StartModuleWindowState.xcs \
     WriterWindowState.xcs \
+    XFormsWindowState.xcs \
     WriterGlobalWindowState.xcs \
     WriterWebWindowState.xcs \
     Effects.xcs \
