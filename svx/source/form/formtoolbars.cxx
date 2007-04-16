281acd3586e06335f3c393ae17d912c6975a2c5a
Ivo Hinkelmann <ihi@openoffice.org>
2007-04-16 15:21:44 +0000

    INTEGRATION: CWS fwkdbdesign01 (1.8.280); FILE MERGED 2007/03/03 15:19:16 fs 1.8.280.1: #i74951# removed unused ctor argument

--- svx/source/form/formtoolbars.cxx
+++ svx/source/form/formtoolbars.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formtoolbars.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:12:29 $
+ *  last change: $Author: ihi $ $Date: 2007-04-16 16:21:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,20 +64,8 @@ namespace svxform
     //= FormToolboxes
     //====================================================================
     //--------------------------------------------------------------------
-    FormToolboxes::FormToolboxes( const Reference< XFrame >& _rxFrame, DocumentType _eDocType )
-        :m_eDocumentType( _eDocType )
+    FormToolboxes::FormToolboxes( const Reference< XFrame >& _rxFrame )
     {
-        // the document type
-        if ( eUnknownDocumentType == m_eDocumentType )
-        {
-            Reference< XController > xController;
-            if ( _rxFrame.is() )
-                xController = _rxFrame->getController();
-            OSL_ENSURE( xController.is() && xController->getModel().is(), "FormToolboxes::FormToolboxes: can't determine the document type!" );
-            if ( xController.is() )
-                m_eDocumentType = DocumentClassification::classifyDocument( xController->getModel() );
-        }
-
         // the layout manager
         Reference< XPropertySet > xFrameProps( _rxFrame, UNO_QUERY );
         if ( xFrameProps.is() )
