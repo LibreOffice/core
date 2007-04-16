381bca16985315ffacbda95255a0b19e679d42c5
Ivo Hinkelmann <ihi@openoffice.org>
2007-04-16 10:52:18 +0000

    INTEGRATION: CWS pj74 (1.1.2); FILE ADDED 2007/03/04 07:37:34 pjanik 1.1.2.2: #i74947#: Remove debugging statements. 2007/03/01 09:57:44 pjanik 1.1.2.1: #i74947#: Implement Mac OS X specific system proxy configuration backend..

---
+++ shell/source/backends/macbe/macbackend.cxx
@@ -0,0 +1,158 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: macbackend.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-04-16 11:52:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_shell.hxx"
+
+#include "macbackend.hxx"
+#include "macbelayer.hxx"
+
+#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_COMPONENTCHANGEEVENT_HPP_
+#include <com/sun/star/configuration/backend/ComponentChangeEvent.hpp>
+#endif
+
+#ifndef _UNO_CURRENT_CONTEXT_HXX_
+#include <uno/current_context.hxx>
+#endif
+
+MacOSXBackend::MacOSXBackend(const uno::Reference<uno::XComponentContext>& xContext)
+        throw (backend::BackendAccessException) :
+        ::cppu::WeakImplHelper2 < backend::XSingleLayerStratum, lang::XServiceInfo > (),
+        m_xContext(xContext)
+{
+}
+
+//------------------------------------------------------------------------------
+
+MacOSXBackend::~MacOSXBackend(void)
+{
+}
+
+//------------------------------------------------------------------------------
+
+MacOSXBackend* MacOSXBackend::createInstance(const uno::Reference<uno::XComponentContext>& xContext)
+{
+    return new MacOSXBackend(xContext);
+}
+
+// ---------------------------------------------------------------------------------------
+
+uno::Reference<backend::XLayer> SAL_CALL MacOSXBackend::getLayer(const rtl::OUString& aComponent, const rtl::OUString& /*aTimestamp*/)
+    throw (backend::BackendAccessException, lang::IllegalArgumentException)
+{
+    if( aComponent.equals( getSupportedComponents()[0]) )
+    {
+        if( ! m_xSystemLayer.is() )
+            m_xSystemLayer = new MacOSXLayer(m_xContext);
+
+        return m_xSystemLayer;
+    }
+
+    return uno::Reference<backend::XLayer>();
+}
+
+//------------------------------------------------------------------------------
+
+uno::Reference<backend::XUpdatableLayer> SAL_CALL
+    MacOSXBackend::getUpdatableLayer(const rtl::OUString& /*aComponent*/)
+    throw (backend::BackendAccessException,lang::NoSupportException,
+           lang::IllegalArgumentException)
+{
+    throw lang::NoSupportException(
+        rtl::OUString( RTL_CONSTASCII_USTRINGPARAM(
+                           "MacOSXBackend: No Update Operation allowed, Read Only access") ),
+        *this);
+
+    return NULL;
+}
+
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL MacOSXBackend::getBackendName(void)
+{
+    return rtl::OUString::createFromAscii("com.sun.star.comp.configuration.backend.MacOSXBackend");
+}
+
+//------------------------------------------------------------------------------
+
+rtl::OUString SAL_CALL MacOSXBackend::getImplementationName(void)
+    throw (uno::RuntimeException)
+{
+    return getBackendName();
+}
+
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL MacOSXBackend::getBackendServiceNames(void)
+{
+    uno::Sequence<rtl::OUString> aServiceNameList(2);
+    aServiceNameList[0] = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.configuration.backend.MacOSXBackend"));
+    aServiceNameList[1] = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.configuration.backend.PlatformBackend"));
+
+    return aServiceNameList;
+}
+
+//------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL MacOSXBackend::supportsService(const rtl::OUString& aServiceName)
+    throw (uno::RuntimeException)
+{
+    uno::Sequence< rtl::OUString > const svc = getBackendServiceNames();
+
+    for(sal_Int32 i = 0; i < svc.getLength(); ++i )
+        if(svc[i] == aServiceName)
+            return true;
+
+    return false;
+}
+
+//------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL MacOSXBackend::getSupportedServiceNames(void)
+    throw (uno::RuntimeException)
+{
+    return getBackendServiceNames();
+}
+
+// ---------------------------------------------------------------------------------------
+
+uno::Sequence<rtl::OUString> SAL_CALL MacOSXBackend::getSupportedComponents(void)
+{
+    uno::Sequence<rtl::OUString> aSupportedComponentList(1);
+    aSupportedComponentList[0] = rtl::OUString(RTL_CONSTASCII_USTRINGPARAM( "org.openoffice.Inet" ));
+
+    return aSupportedComponentList;
+}
+
