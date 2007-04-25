fd5f55364bba717e89b3ffce70aa831e45ab2625
Rüdiger Timm <rt@openoffice.org>
2007-04-25 14:03:19 +0000

    INTEGRATION: CWS sixtyfour11 (1.19.136); FILE MERGED 2007/02/20 10:31:08 kendy 1.19.136.1: #i74692# Let the form control properties dialog show even on 64bit.

--- sfx2/source/control/shell.cxx
+++ sfx2/source/control/shell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shell.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 15:51:54 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 15:03:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1293,9 +1293,9 @@ SfxObjectShell* SfxShell::GetObjectShell()
 
 //--------------------------------------------------------------------
 
-BOOL SfxShell::HasUIFeature( ULONG )
+sal_Bool SfxShell::HasUIFeature( sal_uInt32 )
 {
-    return FALSE;
+    return sal_False;
 }
 
 long DispatcherUpdate_Impl( void*, void* pArg )
