fbebdc8205c025fa075f20394585a057367515ca
Rüdiger Timm <rt@openoffice.org>
2007-04-25 13:40:03 +0000

    INTEGRATION: CWS sixtyfour11 (1.11.406); FILE MERGED 2007/03/12 16:39:22 kendy 1.11.406.1: #i75331# Don't introduce new virtual methods where they should be overridden.

--- sd/source/ui/inc/AccessibleDocumentViewBase.hxx
+++ sd/source/ui/inc/AccessibleDocumentViewBase.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleDocumentViewBase.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 04:57:44 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 14:40:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -189,7 +189,7 @@ public:
     */
     virtual ::com::sun::star::uno::Reference<
         ::com::sun::star::accessibility::XAccessible> SAL_CALL
-        getAccessibleChild (long nIndex)
+        getAccessibleChild (sal_Int32 nIndex)
         throw (::com::sun::star::uno::RuntimeException);
 
 
