964a5c5f7b04be765c6acf5b56571dafdbde5bdb
Rüdiger Timm <rt@openoffice.org>
2007-04-25 15:08:55 +0000

    INTEGRATION: CWS npower6 (1.1.2); FILE ADDED 2007/02/21 17:29:21 npower 1.1.2.2: #i74096 add license 2007/02/07 21:17:58 npower 1.1.2.1: #i74096# add compatibility api implementation

---
+++ sc/source/ui/vba/vbapalette.hxx
@@ -0,0 +1,54 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: vbapalette.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-25 16:08:55 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef SC_VBAPALETTE_HXX
+#define SC_VBAPALETTE_HXX
+
+#include "vbahelper.hxx"
+#include <document.hxx>
+#include <com/sun/star/container/XIndexAccess.hpp>
+
+class ScVbaPalette
+{
+private:
+    SfxObjectShell* m_pShell;
+public:
+    ScVbaPalette(  SfxObjectShell* pShell ) : m_pShell( pShell ){}
+    // if no palette available e.g. because the document doesn't have a
+    // palette defined then a default palette will be returned.
+    css::uno::Reference< css::container::XIndexAccess > getPalette();
+};
+
+#endif //SC_VBAPALETTE_HXX
+
