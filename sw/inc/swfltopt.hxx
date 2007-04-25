4a7034056182638e94f51741a967dc500a6c9526
Rüdiger Timm <rt@openoffice.org>
2007-04-25 07:56:53 +0000

    INTEGRATION: CWS os94 (1.3.734); FILE MERGED 2007/03/12 08:07:32 os 1.3.734.1: #i75235# unused methods removed

--- sw/inc/swfltopt.hxx
+++ sw/inc/swfltopt.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swfltopt.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:14:12 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 08:56:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,12 +42,8 @@
 class SwFilterOptions : public utl::ConfigItem
 {
 public:
-    SwFilterOptions();
     SwFilterOptions( sal_uInt16 nCnt, const sal_Char** ppNames,
                       sal_uInt32* pValues );
-
-    sal_Bool CheckNodeContentExist( const sal_Char* pNode,
-                                  const sal_Char* pCntnt );
 
     void GetValues( sal_uInt16 nCnt, const sal_Char** ppNames,
                      sal_uInt32* pValues );
