0a2bdf1f981a5f53d77c0a98a72eda09ae1e0b0b
Rüdiger Timm <rt@openoffice.org>
2007-04-25 07:54:08 +0000

    INTEGRATION: CWS os94 (1.14.266); FILE MERGED 2007/03/12 08:07:30 os 1.14.266.1: #i75235# unused methods removed

--- sw/inc/fldbas.hxx
+++ sw/inc/fldbas.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fldbas.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:22:19 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 08:54:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -309,8 +309,6 @@ public:
             USHORT          Which() const { return nWhich; }
 
     inline  void            UpdateFlds() const;
-
-    static SvStringsDtor* GetFldNames();
 };
 
 inline void SwFieldType::UpdateFlds() const
