521b35acd95d4e04336c7257e3cf16efb5ff4e8d
Rüdiger Timm <rt@openoffice.org>
2007-04-25 08:08:17 +0000

    INTEGRATION: CWS os94 (1.11.246); FILE MERGED 2007/03/12 08:10:43 os 1.11.246.1: #i75235# unused methods removed

--- sw/source/core/layout/objectformatter.cxx
+++ sw/source/core/layout/objectformatter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objectformatter.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:21:09 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 09:08:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -259,31 +259,6 @@ bool SwObjectFormatter::FormatObjsAtFrm( SwFrm& _rAnchorFrm,
     return bSuccess;
 }
 
-/** method to format all as-character anchored floating screen objects
-    at the given anchor frame
-
-    @author OD
-*/
-bool SwObjectFormatter::FormatAsCharAnchoredObjsAtFrm( SwFrm& _rAnchorFrm,
-                                                       const SwPageFrm& _rPageFrm,
-                                                       SwLayAction* _pLayAction )
-{
-    bool bSuccess( true );
-
-    // create corresponding object formatter
-    SwObjectFormatter* pObjFormatter =
-        SwObjectFormatter::CreateObjFormatter( _rAnchorFrm, _rPageFrm, _pLayAction );
-
-    if ( pObjFormatter )
-    {
-        // format as-character anchored floating screen objects
-        pObjFormatter->SetFormatOnlyAsCharAnchored();
-        bSuccess = pObjFormatter->DoFormatObjs();
-    }
-    delete pObjFormatter;
-
-    return bSuccess;
-}
 /** method to format a given floating screen object
 
     @author OD
