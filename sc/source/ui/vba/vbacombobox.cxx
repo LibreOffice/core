352be4c8e8ff715520be41ba512175335b32fe13
Rüdiger Timm <rt@openoffice.org>
2007-04-25 15:04:14 +0000

    INTEGRATION: CWS npower6 (1.1.2); FILE ADDED 2007/03/05 13:52:57 npower 1.1.2.3: #i74096# remove 'old' default method handling foo 2007/02/21 17:29:18 npower 1.1.2.2: #i74096 add license 2007/02/07 21:17:51 npower 1.1.2.1: #i74096# add compatibility api implementation

---
+++ sc/source/ui/vba/vbacombobox.cxx
@@ -0,0 +1,141 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: vbacombobox.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-25 16:04:02 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#include "vbacombobox.hxx"
+#include <vector>
+
+using namespace com::sun::star;
+using namespace org::openoffice;
+
+
+const static rtl::OUString CONTROLSOURCEPROP( RTL_CONSTASCII_USTRINGPARAM("DataFieldProperty") );
+const static rtl::OUString ITEMS( RTL_CONSTASCII_USTRINGPARAM("StringItemList") );
+
+ScVbaComboBox::ScVbaComboBox( const uno::Reference< uno::XComponentContext >& xContext, const uno::Reference< beans::XPropertySet >& xProps ) : m_xContext(xContext), m_xProps( xProps )
+{
+    // grab the default value property name
+    m_xProps->getPropertyValue( CONTROLSOURCEPROP ) >>= sSourceName;
+}
+
+
+// Attributes
+uno::Any SAL_CALL
+ScVbaComboBox::getValue() throw (uno::RuntimeException)
+{
+    return m_xProps->getPropertyValue( sSourceName );
+}
+
+void SAL_CALL
+ScVbaComboBox::setValue( const uno::Any& _value ) throw (uno::RuntimeException)
+{
+    m_xProps->setPropertyValue( sSourceName, _value );
+}
+
+::rtl::OUString SAL_CALL
+ScVbaComboBox::getText() throw (uno::RuntimeException)
+{
+    rtl::OUString result;
+    getValue() >>= result;
+    return result;
+}
+
+void SAL_CALL
+ScVbaComboBox::setText( const ::rtl::OUString& _text ) throw (uno::RuntimeException)
+{
+    setValue( uno::makeAny( _text ) ); // seems the same
+}
+
+// Methods
+void SAL_CALL
+ScVbaComboBox::AddItem( const uno::Any& pvargItem, const uno::Any& pvargIndex ) throw (uno::RuntimeException)
+{
+    if ( pvargItem.hasValue()  )
+    {
+        uno::Sequence< rtl::OUString > sList;
+        m_xProps->getPropertyValue( ITEMS ) >>= sList;
+
+        sal_Int32 nIndex = sList.getLength();
+
+        if ( pvargIndex.hasValue() )
+            pvargIndex >>= nIndex;
+
+        rtl::OUString sString;
+        pvargItem >>= sString;
+
+        // if no index specified or item is to be appended to end of
+        // list just realloc the array and set the last item
+        if ( nIndex  == sList.getLength() )
+        {
+            sal_Int32 nOldSize = sList.getLength();
+            sList.realloc( nOldSize + 1 );
+            sList[ nOldSize ] = sString;
+        }
+        else
+        {
+            // just copy those elements above the one to be inserted
+            std::vector< rtl::OUString > sVec;
+            // reserve just the amount we need to copy
+            sVec.reserve( sList.getLength() - nIndex );
+
+            // point at first element to copy
+            rtl::OUString* pString = sList.getArray() + nIndex;
+            const rtl::OUString* pEndString = sList.getArray() + sList.getLength();
+            // insert the new element
+            sVec.push_back( sString );
+            // copy elements
+            for ( ; pString != pEndString; ++pString )
+                sVec.push_back( *pString );
+
+            sList.realloc(  sList.getLength() + 1 );
+
+            // point at first element to be overwritten
+            pString = sList.getArray() + nIndex;
+            pEndString = sList.getArray() + sList.getLength();
+            std::vector< rtl::OUString >::iterator it = sVec.begin();
+            for ( ; pString != pEndString; ++pString, ++it)
+                *pString = *it;
+            //
+        }
+
+        m_xProps->setPropertyValue( ITEMS, uno::makeAny( sList ) );
+
+    }
+}
+
+void SAL_CALL
+ScVbaComboBox::Clear(  ) throw (uno::RuntimeException)
+{
+        setValue( uno::makeAny( rtl::OUString() ) );
+        m_xProps->setPropertyValue( ITEMS, uno::makeAny( uno::Sequence< rtl::OUString >() ) );
+}
---
+++ sc/source/ui/vba/vbacombobox.hxx
@@ -0,0 +1,75 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: vbacombobox.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-04-25 16:04:14 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef SC_VBA_COMBOBOX_HXX
+#define SC_VBA_COMBOBOX_HXX
+#include <cppuhelper/implbase2.hxx>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/script/XDefaultProperty.hpp>
+#include <org/openoffice/msforms/XComboBox.hpp>
+#include <comphelper/proparrhlp.hxx>
+#include <comphelper/propertycontainer.hxx>
+#include <com/sun/star/beans/PropertyAttribute.hpp>
+
+#include "vbahelper.hxx"
+
+typedef ::cppu::WeakImplHelper2<oo::msforms::XComboBox, css::script::XDefaultProperty > ComboBoxImpl_BASE;
+class ScVbaComboBox : public ComboBoxImpl_BASE
+{
+
+    css::uno::Reference< css::uno::XComponentContext > m_xContext;
+    css::uno::Reference< css::beans::XPropertySet > m_xProps;
+    rtl::OUString sSourceName;
+    rtl::OUString msDftPropName;
+
+
+public:
+    ScVbaComboBox( const css::uno::Reference< css::uno::XComponentContext >& xContext, const css::uno::Reference< css::beans::XPropertySet >& xProps );
+
+    // Attributes
+    virtual css::uno::Any SAL_CALL getValue() throw (css::uno::RuntimeException);
+    virtual void SAL_CALL setValue( const css::uno::Any& _value ) throw (css::uno::RuntimeException);
+    virtual ::rtl::OUString SAL_CALL getText() throw (css::uno::RuntimeException);
+    virtual void SAL_CALL setText( const ::rtl::OUString& _text ) throw (css::uno::RuntimeException);
+
+    // Methods
+    virtual void SAL_CALL AddItem( const css::uno::Any& pvargItem, const css::uno::Any& pvargIndex ) throw (css::uno::RuntimeException);
+    virtual void SAL_CALL Clear(  ) throw (css::uno::RuntimeException);
+
+    // XDefaultProperty
+        ::rtl::OUString SAL_CALL getDefaultPropertyName(  ) throw (css::uno::RuntimeException) { return ::rtl::OUString::createFromAscii("Value"); }
+};
+
+#endif //
