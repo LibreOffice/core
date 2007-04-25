cb3e539d34eb8896782998cfb98d99baa990097b
Rüdiger Timm <rt@openoffice.org>
2007-04-25 08:13:23 +0000

    INTEGRATION: CWS os94 (1.17.266); FILE MERGED 2007/03/12 08:10:48 os 1.17.266.1: #i75235# unused methods removed

--- sw/source/filter/ww8/writerhelper.hxx
+++ sw/source/filter/ww8/writerhelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: writerhelper.hxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 17:14:54 $
+ *  last change: $Author: rt $ $Date: 2007-04-25 09:13:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -738,7 +738,6 @@ namespace sw
         const SwNumFmt* GetNumFmtFromTxtNode(const SwTxtNode &rTxtNode);
 
         const SwNumRule* GetNumRuleFromTxtNode(const SwTxtNode &rTxtNd);
-        //const SwNumRule* GetOutlineNumRuleFromTxtNode(const SwTxtNode &rTxtNd);
         const SwNumRule* GetNormalNumRuleFromTxtNode(const SwTxtNode &rTxtNd);
 
 
