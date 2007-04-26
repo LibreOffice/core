292014574784e581db8c3941e2f0594832ce97f5
Rüdiger Timm <rt@openoffice.org>
2007-04-26 09:05:45 +0000

    INTEGRATION: CWS residcleanup (1.2.26); FILE MERGED 2007/04/20 19:12:46 pl 1.2.26.1: fix cwsanalyze alerts

--- sfx2/inc/sfx2/sfxdlg.hxx
+++ sfx2/inc/sfx2/sfxdlg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxdlg.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 21:28:43 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 10:05:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -138,15 +138,15 @@ class SFX2_DLLPUBLIC SfxAbstractDialogFactory : public VclAbstractDialogFactory
 {
 public:
     static SfxAbstractDialogFactory*    Create();
-    virtual VclAbstractDialog*          CreateSfxDialog( Window* pParent, const SfxBindings& rBindings, const ResId& rResId ) = 0;
-    virtual VclAbstractDialog*          CreateFrameDialog( Window* pParent, const com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame, const ResId& rResId ) = 0;
-    virtual SfxAbstractTabDialog*       CreateTabDialog( const ResId& rResId,
+    virtual VclAbstractDialog*          CreateSfxDialog( Window* pParent, const SfxBindings& rBindings, sal_uInt32 nResId ) = 0;
+    virtual VclAbstractDialog*          CreateFrameDialog( Window* pParent, const com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& rFrame, sal_uInt32 nResId ) = 0;
+    virtual SfxAbstractTabDialog*       CreateTabDialog( sal_uInt32 nResId,
                                             Window* pParent,
                                             const SfxItemSet* pAttrSet,
                                             SfxViewFrame* pViewFrame,
                                             bool bEditFmt=false,
                                             const String *pUserButtonText=0 ) = 0;
-    virtual SfxAbstractTabDialog*       CreateTabDialog( const ResId& rResId,
+    virtual SfxAbstractTabDialog*       CreateTabDialog( sal_uInt32 nResId,
                                             Window* pParent,
                                             const SfxItemSet* pAttrSet,
                                             const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >& xViewFrame,
