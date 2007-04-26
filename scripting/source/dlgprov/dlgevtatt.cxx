493e687171147b080b70e4a724ce8444a2a52606
Rüdiger Timm <rt@openoffice.org>
2007-04-26 06:21:04 +0000

    INTEGRATION: CWS residcleanup (1.11.20); FILE MERGED 2007/03/04 09:27:30 pl 1.11.20.1: #i74635# ResId cleanup

--- scripting/source/dlgprov/dlgevtatt.cxx
+++ scripting/source/dlgprov/dlgevtatt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgevtatt.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 10:34:41 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 07:21:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -501,7 +501,7 @@ namespace dlgprov
             ResMgr* pResMgr = SFX_APP()->GetSfxResManager();
             if( pResMgr )
             {
-                String aRes( ResId(STR_ERRUNOEVENTBINDUNG, pResMgr) );
+                String aRes( ResId(STR_ERRUNOEVENTBINDUNG, *pResMgr) );
                 ::rtl::OUString aQuoteChar( RTL_CONSTASCII_USTRINGPARAM( "\"" ) );
 
                 ::rtl::OUString aOURes = aRes;
