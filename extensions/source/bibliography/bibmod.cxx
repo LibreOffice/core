7054cec67bf93f9676ccb918561f200e26c39896
Rüdiger Timm <rt@openoffice.org>
2007-04-26 07:05:22 +0000

    INTEGRATION: CWS residcleanup (1.9.112); FILE MERGED 2007/02/28 20:46:09 pl 1.9.112.1: #i74635# no more ResMgr fallback

--- extensions/source/bibliography/bibmod.cxx
+++ extensions/source/bibliography/bibmod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bibmod.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 12:55:17 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 08:05:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -101,7 +101,7 @@ void CloseBibModul(HdlBibModul ppBibModul)
 }
 
 BibResId::BibResId( sal_uInt16 nId ) :
-    ResId( nId, pBibModul->GetResMgr() )
+    ResId( nId, *pBibModul->GetResMgr() )
 {
 }
 BibConfig* BibModul::pBibConfig = 0;
