0077a335817886d81ea6f98e6243f028df7f6de5
Rüdiger Timm <rt@openoffice.org>
2007-04-26 08:45:33 +0000

    INTEGRATION: CWS residcleanup (1.43.62); FILE MERGED 2007/02/18 19:21:42 pl 1.43.62.1: #i74635# get rid of implicit global ResMgr

--- svtools/source/misc/imagemgr.cxx
+++ svtools/source/misc/imagemgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imagemgr.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: ihi $ $Date: 2006-12-19 11:34:50 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 09:45:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -628,7 +628,7 @@ static ImageList* CreateImageList_Impl( USHORT nResId )
     ImageList* pList = NULL;
     ResMgr* pResMgr = GetIsoResMgr_Impl();
     DBG_ASSERT( pResMgr, "SvFileInformationManager::CreateImageList_Impl(): no resmgr" );
-    ResId aResId( nResId, pResMgr );
+    ResId aResId( nResId, *pResMgr );
     aResId.SetRT( RSC_IMAGELIST );
     if ( pResMgr->IsAvailable( aResId ) )
         pList = new ImageList( aResId );
