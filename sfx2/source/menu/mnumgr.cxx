dbafd1413971fe7f6c4de7ebcc6afe173dc0b25b
Rüdiger Timm <rt@openoffice.org>
2007-04-26 09:15:09 +0000

    INTEGRATION: CWS residcleanup (1.38.140); FILE MERGED 2007/02/20 16:35:32 pl 1.38.140.1: #i74635# get rid of global ResMgr

--- sfx2/source/menu/mnumgr.cxx
+++ sfx2/source/menu/mnumgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mnumgr.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 15:57:54 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 10:15:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -194,9 +194,8 @@ void SfxMenuManager::UseDefault()
 
     SfxVirtualMenu *pVMenu = 0;
     {
-        ResId aResId(GetType());
+        ResId aResId(GetType(),*pResMgr);
         aResId.SetRT(RSC_MENU);
-        aResId.SetResMgr(pResMgr);
         Menu *pSVMenu = new PopupMenu( aResId );
         //SfxMenuManager::EraseItemCmds( pSVMenu ); // Remove .uno cmds to be compatible with 6.0/src641
 
@@ -348,9 +347,8 @@ USHORT SfxPopupMenuManager::Execute( const Point& rPoint, Window* pWindow, const
 
 void SfxPopupMenuManager::StartInsert()
 {
-    ResId aResId(GetType());
+    ResId aResId(GetType(),*pResMgr);
     aResId.SetRT(RSC_MENU);
-    aResId.SetResMgr(pResMgr);
     pSVMenu = new PopupMenu( aResId );
     TryToHideDisabledEntries_Impl( pSVMenu );
 }
