3e6cbc85344de6f4ee23089c4acb91090849ab83
Rüdiger Timm <rt@openoffice.org>
2007-04-26 07:22:34 +0000

    INTEGRATION: CWS residcleanup (1.4.130); FILE MERGED 2007/02/24 19:59:49 pl 1.4.130.1: #i74635# residcleanup

--- desktop/source/app/desktopresid.cxx
+++ desktop/source/app/desktopresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: desktopresid.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 09:36:04 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 08:22:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@ namespace desktop
 {
 
 DesktopResId::DesktopResId( USHORT nId ) :
-    ResId( nId, Desktop::GetDesktopResManager() )
+    ResId( nId, *Desktop::GetDesktopResManager() )
 {
 }
 
