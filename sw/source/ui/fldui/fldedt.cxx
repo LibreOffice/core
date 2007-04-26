e5f0160ab7f044c0c65435036a3ce010993843a3
Rüdiger Timm <rt@openoffice.org>
2007-04-26 08:10:00 +0000

    INTEGRATION: CWS residcleanup (1.15.240); FILE MERGED 2007/03/04 17:03:02 pl 1.15.240.1: #i73635# ResId cleanup

--- sw/source/ui/fldui/fldedt.cxx
+++ sw/source/ui/fldui/fldedt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fldedt.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:57:53 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 09:10:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -420,7 +420,7 @@ IMPL_LINK( SwFldEditDlg, AddressHdl, PushButton *, pButton )
     SwAbstractDialogFactory* pFact = swui::GetFactory();//CHINA001
     DBG_ASSERT(pFact, "SwAbstractDialogFactory fail!");//CHINA001
 
-    AbstractSfxSingleTabDialog* pDlg = pFact->CreateSfxSingleTabDialog( this, aSet,ResId( RC_DLG_ADDR ));
+    AbstractSfxSingleTabDialog* pDlg = pFact->CreateSfxSingleTabDialog( this, aSet, RC_DLG_ADDR );
     DBG_ASSERT(pDlg, "Dialogdiet fail!");//CHINA001
     if(RET_OK == pDlg->Execute())
     {
