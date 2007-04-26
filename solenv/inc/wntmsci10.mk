d779846c1b7cbb1b674e68972287f580a012b701
Rüdiger Timm <rt@openoffice.org>
2007-04-26 12:57:46 +0000

    INTEGRATION: CWS ause077 (1.19.8); FILE MERGED 2007/04/17 21:07:25 hjs 1.19.8.2: RESYNC: (1.19-1.22); FILE MERGED 2007/03/30 15:51:55 hjs 1.19.8.1: #i75811# add default for CC

--- solenv/inc/wntmsci10.mk
+++ solenv/inc/wntmsci10.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: wntmsci10.mk,v $
 #
-#   $Revision: 1.24 $
+#   $Revision: 1.25 $
 #
-#   last change: $Author: obo $ $Date: 2007-04-19 16:00:16 $
+#   last change: $Author: rt $ $Date: 2007-04-26 13:57:46 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,6 +48,7 @@ AFLAGS=/c /Cp /coff
 # exporting the variable ARCH_FLAGS="..." in the shell, which is used to start build
 ARCH_FLAGS*=
 
+CC*:=$(WRAPCMD) cl
 .IF "$(bndchk)" != ""
 CXX*=nmcl
 .ELSE
