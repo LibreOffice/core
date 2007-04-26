126b6fb829f5f03795b7401db340a818ff0fd19d
Rüdiger Timm <rt@openoffice.org>
2007-04-26 09:08:44 +0000

    INTEGRATION: CWS residcleanup (1.5.156); FILE MERGED 2007/02/20 16:12:51 pl 1.5.156.1: #i74635# get rid of global ResMgr

--- sfx2/source/bastyp/sfxresid.cxx
+++ sfx2/source/bastyp/sfxresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxresid.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:25:22 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 10:08:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,7 @@ static ResMgr* pMgr=NULL;
 
 SfxResId::SfxResId( USHORT nId ) :
 
-    ResId( nId, GetResMgr() )
+    ResId( nId, *GetResMgr() )
 {
 }
 
