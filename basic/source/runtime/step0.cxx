3e2360e0fa81609665e8e396e4fdf67e4420c64e
Rüdiger Timm <rt@openoffice.org>
2007-04-26 07:33:37 +0000

    INTEGRATION: CWS residcleanup (1.27.44); FILE MERGED 2007/03/12 12:22:30 pl 1.27.44.2: #i75311# separate stt and sb resources 2007/02/20 11:08:05 pl 1.27.44.1: #i74635# all basic resources from stt resource file henceforth

--- basic/source/runtime/step0.cxx
+++ basic/source/runtime/step0.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: step0.cxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: obo $ $Date: 2006-11-03 07:03:10 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 08:33:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -836,7 +836,7 @@ void SbiRuntime::StepINPUT()
     {
         if( pRestart && !pIosys->GetChannel() )
         {
-            BasicResId aId( IDS_SBERR_START + 4 );
+            BasResId aId( IDS_SBERR_START + 4 );
             String aMsg( aId );
 
             //****** DONT CHECK IN, TEST ONLY *******
