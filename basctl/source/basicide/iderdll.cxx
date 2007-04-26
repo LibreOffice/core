bb16881a148efc55a32f0998322f7cf4f67e9985
Rüdiger Timm <rt@openoffice.org>
2007-04-26 08:33:47 +0000

    INTEGRATION: CWS residcleanup (1.21.12); FILE MERGED 2007/03/20 15:55:57 pl 1.21.12.2: RESYNC: (1.21-1.22); FILE MERGED 2007/02/24 20:44:45 pl 1.21.12.1: #i74635# residcleanup

--- basctl/source/basicide/iderdll.cxx
+++ basctl/source/basicide/iderdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: iderdll.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:56:08 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 09:33:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,7 @@ BasicIDEDLL* BasicIDEDLL::GetDLL()
 }
 
 IDEResId::IDEResId( USHORT nId ):
-    ResId( nId, (*(BasicIDEModule**)GetAppData(SHL_IDE))->GetResMgr() )
+    ResId( nId, *(*(BasicIDEModule**)GetAppData(SHL_IDE))->GetResMgr() )
 {
 }
 
