0c3c8c77523f3c915b093fc4880f503a5a8fb38c
Rüdiger Timm <rt@openoffice.org>
2007-04-26 06:24:17 +0000

    INTEGRATION: CWS residcleanup (1.6.262); FILE MERGED 2007/02/20 19:38:26 pl 1.6.262.1: #i74635# get rid of global ResMgr

--- svx/source/accessibility/AccessibleFrameSelector.cxx
+++ svx/source/accessibility/AccessibleFrameSelector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleFrameSelector.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 04:00:34 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 07:24:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,8 +130,8 @@ AccFrameSelector::AccFrameSelector( FrameSelector& rFrameSel, FrameBorderType eB
     meBorder( eBorder ),
     maFocusListeners( maFocusMutex ),
     maPropertyListeners( maPropertyMutex ),
-    maNames( ResId( ARR_TEXTS ) ),
-    maDescriptions( ResId(ARR_DESCRIPTIONS ) )
+    maNames( SVX_RES( ARR_TEXTS ) ),
+    maDescriptions( SVX_RES(ARR_DESCRIPTIONS ) )
 {
     FreeResource();
 }
