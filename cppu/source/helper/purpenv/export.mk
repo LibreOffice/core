0489b690aa262bfe923ebc4f0394d5edd468c5f9
Kurt Zenker <kz@openoffice.org>
2007-05-09 12:36:58 +0000

    INTEGRATION: CWS bunoexttm (1.1.2.2.2); FILE ADDED 2007/02/14 17:09:45 kr 1.1.2.2.2.2: changed: renamed windows purpenvhelper similar to cppuhelper 2007/02/02 15:32:57 kr 1.1.2.2.2.1: fixed: windows name

---
+++ cppu/source/helper/purpenv/export.mk
@@ -0,0 +1,12 @@
+
+.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+NAMEpurpenv_helper := uno_purpenvhelper$(COMID)
+
+purpenv_helper_LIB := -l$(NAMEpurpenv_helper)
+
+.ELSE
+NAMEpurpenv_helper := purpenvhelper$(UDK_MAJOR)$(COMID)
+
+purpenv_helper_LIB := $(LIBPRE) i$(NAMEpurpenv_helper).lib
+
+.ENDIF
