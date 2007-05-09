46d8a0dc6cb0ea0370ddc9f6711a5e3ca5e94481
Kurt Zenker <kz@openoffice.org>
2007-05-09 12:29:55 +0000

    INTEGRATION: CWS aw048 (1.2.20); FILE MERGED 2007/04/18 13:50:39 aw 1.2.20.1: #i76339#

--- svx/inc/svx/svdpoev.hxx
+++ svx/inc/svx/svdpoev.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdpoev.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 16:27:24 $
+ *  last change: $Author: kz $ $Date: 2007-05-09 13:29:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -124,13 +124,13 @@ public:
 
     // Hierbei entstehen eventuell beliebig viele neue Objekte:
     void RipUpAtMarkedPoints();
-    BOOL IsRipUpAtMarkedPointsPossible() const;
+    bool IsRipUpAtMarkedPointsPossible() const;
 
     // Alle markierten Polylines werden zu Polygonen, alle offenen
     // Bezierkurven zu geschlossenen.
     void ShutMarkedObjects();
     void CloseMarkedObjects(BOOL bToggle=FALSE, BOOL bOpen=FALSE); // , long nOpenDistance=0);
-    BOOL IsOpenCloseMarkedObjectsPossible() const;
+    bool IsOpenCloseMarkedObjectsPossible() const;
     SdrObjClosedKind GetMarkedObjectsClosedState() const;
 };
 
