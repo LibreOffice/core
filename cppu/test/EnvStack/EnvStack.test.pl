614c02aa47907857c0da5ba0f43b7351d2e38276
Kurt Zenker <kz@openoffice.org>
2007-05-09 12:41:39 +0000

    INTEGRATION: CWS bunoexttm (1.1.2.2.2); FILE ADDED 2007/01/25 14:37:09 kr 1.1.2.2.2.1: fixed: license

---
+++ cppu/test/EnvStack/EnvStack.test.pl
@@ -0,0 +1,97 @@
+:
+eval 'exec perl -wS $0 ${1+"$@"}'
+    if 0;
+
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: EnvStack.test.pl,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2007-05-09 13:41:39 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+my $rc      = 0;
+my $comment = "";
+
+
+@tests = (
+  "EnvStack.tester \"\"           \"\" \"\"",
+  "EnvStack.tester \":A:a\"       \":A:b\"       \"-enter:A-enter:A:a-out:A:a-into:A:b-leave:A:a-leave:A\"",
+  "EnvStack.tester \":A:B:a\"     \":A:B:b\"     \"-enter:A-enter:A:B-enter:A:B:a-out:A:B:a-into:A:B:b-leave:A:B:a-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C:a\"   \":A:B:C:b\"   \"-enter:A-enter:A:B-enter:A:B:C-enter:A:B:C:a-out:A:B:C:a-into:A:B:C:b-leave:A:B:C:a-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C:D:a\" \":A:B:C:D:b\" \"-enter:A-enter:A:B-enter:A:B:C-enter:A:B:C:D-enter:A:B:C:D:a-out:A:B:C:D:a-into:A:B:C:D:b-leave:A:B:C:D:a-leave:A:B:C:D-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:a:b\"     \":A:c:d\"     \"-enter:A-enter:A:a-enter:A:a:b-out:A:a:b-out:A:a-into:A:c-into:A:c:d-leave:A:a:b-leave:A:a-leave:A\"",
+  "EnvStack.tester \":A:B:a:b\"   \":A:B:c:d\"   \"-enter:A-enter:A:B-enter:A:B:a-enter:A:B:a:b-out:A:B:a:b-out:A:B:a-into:A:B:c-into:A:B:c:d-leave:A:B:a:b-leave:A:B:a-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A\"         \"\"           \"-enter:A-out:A-leave:A\"",
+  "EnvStack.tester \":A:B\"       \"\"           \"-enter:A-enter:A:B-out:A:B-out:A-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C\"     \"\"           \"-enter:A-enter:A:B-enter:A:B:C-out:A:B:C-out:A:B-out:A-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C:D\"   \"\"           \"-enter:A-enter:A:B-enter:A:B:C-enter:A:B:C:D-out:A:B:C:D-out:A:B:C-out:A:B-out:A-leave:A:B:C:D-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \"\"           \":a\"         \"-into:a\"",
+  "EnvStack.tester \"\"           \":a:b\"       \"-into:a-into:a:b\"",
+  "EnvStack.tester \"\"           \":a:b:c\"     \"-into:a-into:a:b-into:a:b:c\"",
+  "EnvStack.tester \"\"           \":a:b:c:d\"   \"-into:a-into:a:b-into:a:b:c-into:a:b:c:d\"",
+  "EnvStack.tester \":A\"         \":a\"         \"-enter:A-out:A-into:a-leave:A\"",
+  "EnvStack.tester \":A:B\"       \":a:b\"       \"-enter:A-enter:A:B-out:A:B-out:A-into:a-into:a:b-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C\"     \":a:b:c\"     \"-enter:A-enter:A:B-enter:A:B:C-out:A:B:C-out:A:B-out:A-into:a-into:a:b-into:a:b:c-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C:D\"   \":a:b:c:d\"   \"-enter:A-enter:A:B-enter:A:B:C-enter:A:B:C:D-out:A:B:C:D-out:A:B:C-out:A:B-out:A-into:a-into:a:b-into:a:b:c-into:a:b:c:d-leave:A:B:C:D-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A\"         \":A\"         \"-enter:A-leave:A\"",
+  "EnvStack.tester \":A:B\"       \":A:B\"       \"-enter:A-enter:A:B-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C\"     \":A:B:C\"     \"-enter:A-enter:A:B-enter:A:B:C-leave:A:B:C-leave:A:B-leave:A\"",
+  "EnvStack.tester \":A:B:C:D\"   \":A:B:C:D\"   \"-enter:A-enter:A:B-enter:A:B:C-enter:A:B:C:D-leave:A:B:C:D-leave:A:B:C-leave:A:B-leave:A\""
+);
+
+foreach $test (@tests) {
+    $output = "";
+
+    $cmd = $test;
+    open TESTER, $cmd . "|";
+    while (<TESTER>) {
+        chomp;
+
+        $output = $output . "\t" . $_ . "\n";
+    }
+    close TESTER ;
+
+    if ($? != 0) {
+        $comment = $comment . "TEST FAILED: " . $cmd . "\n";
+        $comment = $comment . $output;
+    }
+    $rc = $rc + $?;
+}
+
+
+print $comment;
+
+if ($rc == 0) {
+    print "*********** SUCCESS\n";
+}
+else {
+    print "*********** FAILURE\n";
+}
