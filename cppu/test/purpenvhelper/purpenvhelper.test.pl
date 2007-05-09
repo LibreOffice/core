d1da3a2e2350a39f17327fe8775e8ef0d7588666
Kurt Zenker <kz@openoffice.org>
2007-05-09 12:50:01 +0000

    INTEGRATION: CWS bunoexttm (1.1.2); FILE ADDED 2007/02/28 17:03:42 kr 1.1.2.1: simplified: moved class to impl., renamed header

---
+++ cppu/test/purpenvhelper/purpenvhelper.test.pl
@@ -0,0 +1,75 @@
+:
+eval 'exec perl -wS $0 ${1+"$@"}'
+    if 0;
+
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: purpenvhelper.test.pl,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2007-05-09 13:50:01 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+my $rc      = 0;
+my $comment = "";
+
+
+@tests=(
+  "env.tester.bin uno:TestEnv"
+);
+
+foreach $test (@tests) {
+    $output = "";
+
+    $cmd = $test;
+    open TESTER, $cmd . "|";
+    while (<TESTER>) {
+        chomp;
+
+        $output = $output . "\t" . $_ . "\n";
+    }
+    close TESTER ;
+
+    if ($? != 0) {
+        $comment = $comment . "TEST FAILED: " . $cmd . "\n";
+        $comment = $comment . $output;
+    }
+    $rc = $rc + $?;
+}
+
+
+print $comment;
+
+if ($rc == 0) {
+    print "*********** SUCCESS\n";
+}
+else {
+    print "*********** FAILURE\n";
+}
