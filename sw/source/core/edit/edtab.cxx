133a63db9e0e41a8b293f8a1f3253ffe49a06d77
Kurt Zenker <kz@openoffice.org>
2007-05-10 14:58:47 +0000

    INTEGRATION: CWS pchfix04 (1.15.100); FILE MERGED 2007/02/05 08:27:22 os 1.15.100.1: #i73604# usage of ITEMID_* removed

--- sw/source/core/edit/edtab.cxx
+++ sw/source/core/edit/edtab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: edtab.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:07:55 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 15:58:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@
 #include "precompiled_sw.hxx"
 
 
-#define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
 #endif
