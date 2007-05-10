541637412a375da41c85d8854cde47f95e2e8800
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:53:34 +0000

    INTEGRATION: CWS pchfix04 (1.36.46); FILE MERGED 2007/04/26 04:45:16 hjs 1.36.46.3: RESYNC: (1.37-1.38); FILE MERGED 2007/02/13 16:27:00 hjs 1.36.46.2: RESYNC: (1.36-1.37); FILE MERGED 2007/02/05 08:35:27 os 1.36.46.1: #i73604# usage of ITEMID_* removed

--- sc/source/ui/Accessibility/AccessibleText.cxx
+++ sc/source/ui/Accessibility/AccessibleText.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleText.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:56:59 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:53:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
-#define ITEMID_FIELD EE_FEATURE_FIELD
+
 
 #include <memory>
 
