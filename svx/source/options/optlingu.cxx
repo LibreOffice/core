4296ebe82a9bf3fb8a7f8be685e4ba06828d1e18
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:55:32 +0000

    INTEGRATION: CWS pchfix04 (1.60.2); FILE MERGED 2007/02/05 12:14:07 os 1.60.2.1: #i73604# usage of ITEMID_* removed

--- svx/source/options/optlingu.cxx
+++ svx/source/options/optlingu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optlingu.cxx,v $
  *
- *  $Revision: 1.61 $
+ *  $Revision: 1.62 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:50:55 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 14:55:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -145,9 +145,6 @@
 #define _SVX_OPTLINGU_CXX
 
 #include "optlingu.hrc"
-
-#define ITEMID_SPELLCHECK   0
-#define ITEMID_HYPHENREGION 0
 
 //CHINA001 #include "optdict.hxx"
 #include "svxdlg.hxx" //CHINA001
