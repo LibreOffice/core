79f8c271468e6bfb651c47a69c970bdc6404e4c4
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:37:15 +0000

    INTEGRATION: CWS pchfix04 (1.9.70); FILE MERGED 2007/03/02 15:58:00 hjs 1.9.70.3: #i71519# re-insert comment 2006/12/12 17:21:06 kaib 1.9.70.2: #i71519# fixing pch in svtools for msc 2006/11/17 11:45:06 mkretzschmar 1.9.70.1: #i71619# Don't include pch in cxx files that are included in other cxx files

--- svtools/source/items/rngitem.cxx
+++ svtools/source/items/rngitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rngitem.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:01:09 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:37:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,239 +40,26 @@
 #include <tools/stream.hxx>
 #endif
 
-
 #ifndef NUMTYPE
-
-#if SUPD<355
-DBG_NAME(SfxRangeItem);
-#endif
-
 
 #define NUMTYPE USHORT
 #define SfxXRangeItem SfxRangeItem
 #define SfxXRangesItem SfxUShortRangesItem
 #include "rngitem.hxx"
-#include "rngitem.cxx"
+#include "rngitem_inc.cxx"
 
 #define NUMTYPE sal_uInt32
 #define SfxXRangeItem SfxULongRangeItem
 #define SfxXRangesItem SfxULongRangesItem
 #include "rngitem.hxx"
-#include "rngitem.cxx"
+#include "rngitem_inc.cxx"
 
 #else
 
-static inline NUMTYPE Count_Impl(const NUMTYPE * pRanges)
-{
-    NUMTYPE nCount = 0;
-    for (; *pRanges; pRanges += 2) nCount += 2;
-    return nCount;
-}
+// We leave this condition just in case NUMTYPE has been defined externally to this
+// file and we are supposed to define the SfxXRangeItem based on that.
 
-// -----------------------------------------------------------------------
-
-TYPEINIT1_AUTOFACTORY(SfxXRangeItem, SfxPoolItem);
-TYPEINIT1_AUTOFACTORY(SfxXRangesItem, SfxPoolItem);
-
-NUMTYPE Count_Impl( const NUMTYPE *pRanges );
-
-// -----------------------------------------------------------------------
-
-SfxXRangeItem::SfxXRangeItem()
-{
-    nFrom = 0;
-    nTo = 0;
-}
-
-// -----------------------------------------------------------------------
-
-SfxXRangeItem::SfxXRangeItem( USHORT which, NUMTYPE from, NUMTYPE to ):
-    SfxPoolItem( which ),
-    nFrom( from ),
-    nTo( to )
-{
-}
-
-
-// -----------------------------------------------------------------------
-
-SfxXRangeItem::SfxXRangeItem( USHORT nW, SvStream &rStream ) :
-    SfxPoolItem( nW )
-{
-    rStream >> nFrom;
-    rStream >> nTo;
-}
-
-// -----------------------------------------------------------------------
-
-SfxXRangeItem::SfxXRangeItem( const SfxXRangeItem& rItem ) :
-    SfxPoolItem( rItem )
-{
-    nFrom = rItem.nFrom;
-    nTo = rItem.nTo;
-}
-
-// -----------------------------------------------------------------------
-
-SfxItemPresentation SfxXRangeItem::GetPresentation
-(
-    SfxItemPresentation     /*ePresentation*/,
-    SfxMapUnit              /*eCoreMetric*/,
-    SfxMapUnit              /*ePresentationMetric*/,
-    XubString&              rText,
-    const IntlWrapper *
-)   const
-{
-    rText = UniString::CreateFromInt64(nFrom);
-    rText += ':';
-    rText += UniString::CreateFromInt64(nTo);
-    return SFX_ITEM_PRESENTATION_NAMELESS;
-}
-
-// -----------------------------------------------------------------------
-
-int SfxXRangeItem::operator==( const SfxPoolItem& rItem ) const
-{
-    DBG_ASSERT( SfxPoolItem::operator==( rItem ), "unequal type" );
-    SfxXRangeItem* pT = (SfxXRangeItem*)&rItem;
-    if( nFrom==pT->nFrom && nTo==pT->nTo )
-        return 1;
-    return 0;
-}
-
-// -----------------------------------------------------------------------
-
-SfxPoolItem* SfxXRangeItem::Clone(SfxItemPool *) const
-{
-    return new SfxXRangeItem( Which(), nFrom, nTo );
-}
-
-// -----------------------------------------------------------------------
-
-SfxPoolItem* SfxXRangeItem::Create(SvStream &rStream, USHORT) const
-{
-    NUMTYPE     nVon, nBis;
-    rStream >> nVon;
-    rStream >> nBis;
-    return new SfxXRangeItem( Which(), nVon, nBis );
-}
-
-// -----------------------------------------------------------------------
-
-SvStream& SfxXRangeItem::Store(SvStream &rStream, USHORT) const
-{
-    rStream << nFrom;
-    rStream << nTo;
-    return rStream;
-}
-
-//=========================================================================
-
-SfxXRangesItem::SfxXRangesItem()
-:   _pRanges(0)
-{
-}
-
-//-------------------------------------------------------------------------
-
-SfxXRangesItem::SfxXRangesItem( USHORT nWID, const NUMTYPE *pRanges )
-:   SfxPoolItem( nWID )
-{
-    NUMTYPE nCount = Count_Impl(pRanges) + 1;
-    _pRanges = new NUMTYPE[nCount];
-    memcpy( _pRanges, pRanges, sizeof(NUMTYPE) * nCount );
-}
-
-//-------------------------------------------------------------------------
-
-SfxXRangesItem::SfxXRangesItem( USHORT nWID, SvStream &rStream )
-:   SfxPoolItem( nWID )
-{
-    NUMTYPE nCount;
-    rStream >> nCount;
-    _pRanges = new NUMTYPE[nCount + 1];
-    for ( NUMTYPE n = 0; n < nCount; ++n )
-        rStream >> _pRanges[n];
-    _pRanges[nCount] = 0;
-}
-
-//-------------------------------------------------------------------------
-
-SfxXRangesItem::SfxXRangesItem( const SfxXRangesItem& rItem )
-:   SfxPoolItem( rItem )
-{
-    NUMTYPE nCount = Count_Impl(rItem._pRanges) + 1;
-    _pRanges = new NUMTYPE[nCount];
-    memcpy( _pRanges, rItem._pRanges, sizeof(NUMTYPE) * nCount );
-}
-
-//-------------------------------------------------------------------------
-
-SfxXRangesItem::~SfxXRangesItem()
-{
-    delete _pRanges;
-}
-
-//-------------------------------------------------------------------------
-
-int SfxXRangesItem::operator==( const SfxPoolItem &rItem ) const
-{
-    const SfxXRangesItem &rOther = (const SfxXRangesItem&) rItem;
-    if ( !_pRanges && !rOther._pRanges )
-        return TRUE;
-    if ( _pRanges || rOther._pRanges )
-        return FALSE;
-
-    NUMTYPE n;
-    for ( n = 0; _pRanges[n] && rOther._pRanges[n]; ++n )
-        if ( *_pRanges != rOther._pRanges[n] )
-            return 0;
-
-    return !_pRanges[n] && !rOther._pRanges[n];
-}
-
-//-------------------------------------------------------------------------
-
-SfxItemPresentation SfxXRangesItem::GetPresentation( SfxItemPresentation /*ePres*/,
-                                    SfxMapUnit /*eCoreMetric*/,
-                                    SfxMapUnit /*ePresMetric*/,
-                                    XubString &/*rText*/,
-                                    const IntlWrapper * ) const
-{
-    HACK(n. i.)
-    return SFX_ITEM_PRESENTATION_NONE;
-}
-
-//-------------------------------------------------------------------------
-
-SfxPoolItem* SfxXRangesItem::Clone( SfxItemPool * ) const
-{
-    return new SfxXRangesItem( *this );
-}
-
-//-------------------------------------------------------------------------
-
-SfxPoolItem* SfxXRangesItem::Create( SvStream &rStream, USHORT ) const
-{
-    return new SfxXRangesItem( Which(), rStream );
-}
-
-//-------------------------------------------------------------------------
-
-SvStream& SfxXRangesItem::Store( SvStream &rStream, USHORT ) const
-{
-    NUMTYPE nCount = Count_Impl( _pRanges );
-    rStream >> nCount;
-    for ( NUMTYPE n = 0; _pRanges[n]; ++n )
-        rStream >> _pRanges[n];
-    return rStream;
-}
-
-
-#undef NUMTYPE
-#undef SfxXRangeItem
-#undef SfxXRangesItem
-
+#include "rngitem_inc.cxx"
 
 #endif
 
