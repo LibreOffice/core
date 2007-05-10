d7bc3399a8b551bfdef18624bf1fa6ead1227b0f
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:36:59 +0000

    INTEGRATION: CWS pchfix04 (1.35.74); FILE MERGED 2007/05/08 17:00:47 hjs 1.35.74.2: RESYNC: (1.35-1.36); FILE MERGED 2007/02/05 12:13:52 os 1.35.74.1: #i73604# usage of ITEMID_* removed

--- svx/source/dialog/dlgfact.cxx
+++ svx/source/dialog/dlgfact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgfact.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:29:14 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 14:36:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -101,7 +101,7 @@
 #include "linkdlg.hxx"
 #include "SpellDialog.hxx"
 #include "cfg.hxx"    //add for SvxConfigDialog
-#define ITEMID_TABSTOP  0 //add for #include "tabstpge.hxx"
+
 #include "numpages.hxx" // add for
 #include "paragrph.hxx" //add for
 #include "tabstpge.hxx" // add  for
