d5e3181facf74e57369a31197624eb5697cd2e2b
Kurt Zenker <kz@openoffice.org>
2007-05-10 16:01:07 +0000

    INTEGRATION: CWS pchfix04 (1.15.22); FILE MERGED 2007/04/26 04:30:07 hjs 1.15.22.2: RESYNC: (1.15-1.16); FILE MERGED 2007/02/05 08:35:36 os 1.15.22.1: #i73604# usage of ITEMID_* removed

--- sc/source/ui/view/gridwin3.cxx
+++ sc/source/ui/view/gridwin3.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gridwin3.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 13:52:18 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 17:01:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
-#define ITEMID_FIELD EE_FEATURE_FIELD
+
 
 #include <svx/svdoutl.hxx>
 #include <svx/svdotext.hxx>
