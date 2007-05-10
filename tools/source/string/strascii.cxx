5ad6980ba20997ab10b831c4b1c39772026d2bc8
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:39:01 +0000

    INTEGRATION: CWS pchfix04 (1.7.44); FILE MERGED 2006/11/16 15:23:02 mkretzschmar 1.7.44.1: #i71619# Don't include pch files in cxx files that will be included

--- tools/source/string/strascii.cxx
+++ tools/source/string/strascii.cxx
@@ -1,12 +1,12 @@
-/*************************************************************************
+#/*************************************************************************
  *
  *  OpenOffice.org - a multi-platform office productivity suite
  *
  *  $RCSfile: strascii.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 01:02:56 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:39:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,7 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_tools.hxx"
+// no include "precompiled_tools.hxx" because this is included in other cxx files.
 
 // =======================================================================
 
