8823fa4ac95ecfb6ed77038b7fe32649db289a12
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:25:45 +0000

    INTEGRATION: CWS pchfix04 (1.49.2); FILE MERGED 2007/02/05 08:27:54 os 1.49.2.1: #i73604# usage of ITEMID_* removed

--- sw/source/ui/uiview/srcview.cxx
+++ sw/source/ui/uiview/srcview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srcview.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 09:21:28 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:25:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,9 +39,6 @@
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
-#endif
-#ifndef _UIPARAM_HXX
-#include <uiparam.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_UTIL_SEARCHOPTIONS_HPP_
