a5249fee7e982b366b3dbb80b0d12901c5faa100
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:11:33 +0000

    INTEGRATION: CWS pchfix04 (1.11.14); FILE MERGED 2007/05/03 15:45:14 hjs 1.11.14.1: #i73078# - fix after resync

--- connectivity/source/manager/makefile.mk
+++ connectivity/source/manager/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 08:59:48 $
+#   last change: $Author: kz $ $Date: 2007-05-10 14:11:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -65,6 +65,7 @@ SHL1OBJS=$(SLOFILES)
 SHL1STDLIBS=\
     $(CPPULIB)					\
     $(CPPUHELPERLIB)			\
+    $(COMPHELPERLIB)			\
     $(VOSLIB)					\
     $(SALLIB)
 
