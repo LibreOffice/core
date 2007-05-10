cad797b95d76c9146eff62caaf98bb5ff1d0bad4
Kurt Zenker <kz@openoffice.org>
2007-05-10 09:35:44 +0000

    INTEGRATION: CWS dba23a (1.11.74); FILE MERGED 2007/02/26 11:48:17 fs 1.11.74.1: remove unused code Issue number:  #i74804# Submitted by:  jnavrati@openoffice.org Reviewed by:   frank.schoenheit@sun.com

--- dbaccess/source/ui/misc/charsets.cxx
+++ dbaccess/source/ui/misc/charsets.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: charsets.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 08:02:21 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 10:35:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -145,11 +145,6 @@ namespace dbaui
         ,m_sDisplayName(_rDisplayName)
     {
         DBG_ASSERT( m_sDisplayName.getLength(), "CharsetDisplayDerefHelper::CharsetDisplayDerefHelper: invalid display name!" );
-    }
-
-    //-------------------------------------------------------------------------
-    CharsetDisplayDerefHelper::CharsetDisplayDerefHelper()
-    {
     }
 
     //=========================================================================
