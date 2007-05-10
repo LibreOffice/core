31ec616fcefb4dd9bfe0ea3c93028784aef66c3e
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:38:26 +0000

    INTEGRATION: CWS pchfix04 (1.2.44); FILE MERGED 2006/12/13 16:07:09 hjs 1.2.44.5: #i71519# removed local headers 2006/11/28 10:42:51 mkretzschmar 1.2.44.4: #i71519# fix clean build: do not include headers from your own project 2006/11/27 18:58:01 mkretzschmar 1.2.44.3: #i71519# fix system-zlib case 2006/11/27 17:36:18 kaib 1.2.44.2: #i71519# tools pch working on msc 2006/11/27 12:20:21 mkretzschmar 1.2.44.1: #i71519# fill in tools pch file. Builds fine on Linux.

--- tools/inc/pch/precompiled_tools.hxx
+++ tools/inc/pch/precompiled_tools.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: precompiled_tools.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:50:53 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:38:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,5 +36,65 @@
 // MARKER(update_precomp.py): Generated on 2006-09-01 17:50:15.029610
 
 #ifdef PRECOMPILED_HEADERS
-#endif
+//---MARKER---
+#include "sal/config.h"
+#include "sal/types.h"
 
+#include "basegfx/point/b2dpoint.hxx"
+#include "basegfx/polygon/b2dpolygon.hxx"
+#include "basegfx/polygon/b2dpolygontools.hxx"
+#include "basegfx/polygon/b2dpolypolygon.hxx"
+#include "basegfx/polygon/b2dpolypolygontools.hxx"
+#include "basegfx/vector/b2dvector.hxx"
+
+#include "boost/static_assert.hpp"
+
+#include "com/sun/star/lang/Locale.hpp"
+#include "com/sun/star/uno/Reference.hxx"
+#include "com/sun/star/uno/Sequence.hxx"
+#include "com/sun/star/util/XStringWidth.hpp"
+
+#include "comphelper/fileformat.h"
+
+#include "cppuhelper/implbase1.hxx"
+
+#include "i18npool/lang.h"
+#include "i18npool/mslangid.hxx"
+
+#include "osl/diagnose.h"
+#include "osl/endian.h"
+#include "osl/file.hxx"
+#include "osl/interlck.h"
+#include "osl/module.h"
+#include "osl/module.hxx"
+#include "osl/mutex.hxx"
+#include "osl/process.h"
+#include "osl/security.h"
+#include "osl/thread.h"
+
+#include "rtl/alloc.h"
+#include "rtl/crc.h"
+#include "rtl/digest.h"
+#include "rtl/instance.hxx"
+#include "rtl/logfile.hxx"
+#include "rtl/math.hxx"
+#include "rtl/memory.h"
+#include "rtl/string.h"
+#include "rtl/string.hxx"
+#include "rtl/tencinfo.h"
+#include "rtl/textcvt.h"
+#include "rtl/textenc.h"
+#include "rtl/ustrbuf.hxx"
+#include "rtl/ustring.h"
+#include "rtl/ustring.hxx"
+
+#include "sys/stat.h"
+#include "sys/types.h"
+
+#include "vos/macros.hxx"
+#include "vos/mutex.hxx"
+#include "vos/process.hxx"
+#include "vos/signal.hxx"
+#include "vos/timer.hxx"
+//---MARKER---
+#endif
