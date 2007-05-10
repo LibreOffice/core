2fdb48c01463cc9bcefca7c24bee9ce9e18b3dab
Kurt Zenker <kz@openoffice.org>
2007-05-10 16:02:20 +0000

    INTEGRATION: CWS pchfix04 (1.48.2); FILE MERGED 2007/04/26 04:25:15 hjs 1.48.2.2: RESYNC: (1.48-1.49); FILE MERGED 2007/02/05 08:35:37 os 1.48.2.1: #i73604# usage of ITEMID_* removed

--- sc/source/ui/view/printfun.cxx
+++ sc/source/ui/view/printfun.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: printfun.cxx,v $
  *
- *  $Revision: 1.49 $
+ *  $Revision: 1.50 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 13:55:28 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 17:02:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
-#define ITEMID_FIELD EE_FEATURE_FIELD
+
 
 #include "printfun.hxx"
 
