eae81d5cf9f4da009cfe71fdddbdab61acc5cb04
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:44:58 +0000

    INTEGRATION: CWS pchfix04 (1.14.86); FILE MERGED 2007/02/05 12:13:58 os 1.14.86.1: #i73604# usage of ITEMID_* removed

--- svx/source/dialog/tpshadow.cxx
+++ svx/source/dialog/tpshadow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tpshadow.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:47:23 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 14:44:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@
 #include "dialogs.hrc"
 #include "tabarea.hrc"
 
-#define ITEMID_COLOR_TABLE      SID_COLOR_TABLE
+
 
 #include "svdattr.hxx"
 #include "xoutx.hxx"
