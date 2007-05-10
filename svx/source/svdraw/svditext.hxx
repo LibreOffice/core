1f0b690273581a22f280d4c1dd1d7252d36b44fb
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:57:46 +0000

    INTEGRATION: CWS pchfix04 (1.3.654); FILE MERGED 2007/02/05 12:14:10 os 1.3.654.1: #i73604# usage of ITEMID_* removed

--- svx/source/svdraw/svditext.hxx
+++ svx/source/svdraw/svditext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svditext.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 00:30:01 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 14:57:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,24 +42,6 @@
 #include <editdata.hxx>
 #endif
 
-//      textitem.hxx        editdata.hxx
-#define ITEMID_COLOR        EE_CHAR_COLOR
-#define ITEMID_FONT         EE_CHAR_FONTINFO
-#define ITEMID_FONTHEIGHT   EE_CHAR_FONTHEIGHT
-#define ITEMID_CHARSCALE_W  EE_CHAR_FONTWIDTH
-#define ITEMID_WEIGHT       EE_CHAR_WEIGHT
-#define ITEMID_UNDERLINE    EE_CHAR_UNDERLINE
-#define ITEMID_CROSSEDOUT   EE_CHAR_STRIKEOUT
-#define ITEMID_POSTURE      EE_CHAR_ITALIC
-#define ITEMID_CONTOUR      EE_CHAR_OUTLINE
-#define ITEMID_SHADOWED     EE_CHAR_SHADOW
-#define ITEMID_ESCAPEMENT   EE_CHAR_ESCAPEMENT
-#define ITEMID_AUTOKERN     EE_CHAR_PAIRKERNING
-#define ITEMID_WORDLINEMODE EE_CHAR_WLM
-
-//      paraitem.hxx       editdata.hxx
-#define ITEMID_ADJUST      EE_PARA_JUST
-#define ITEMID_FIELD       EE_FEATURE_FIELD
 
 #endif  // _SVDITEXT_HXX
 
