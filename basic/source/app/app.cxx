eb388fa60af32247dd894f9de272e89a69ffa009
Kurt Zenker <kz@openoffice.org>
2007-05-10 14:18:00 +0000

    INTEGRATION: CWS pchfix04 (1.67.18); FILE MERGED 2007/01/16 11:25:39 hjs 1.67.18.1: #i73422# changed define name

--- basic/source/app/app.cxx
+++ basic/source/app/app.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: app.cxx,v $
  *
- *  $Revision: 1.68 $
+ *  $Revision: 1.69 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 08:30:56 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 15:18:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1655,10 +1655,10 @@ long BasicFrame::Command( short nID, BOOL bChecked )
             aBasicApp.pHelp->Start( CUniString( "Keyboard" ) );
             break;
         case RID_HELPINDEX:
-            aBasicApp.pHelp->Start( HELP_INDEX );
+            aBasicApp.pHelp->Start( OOO_HELP_INDEX );
             break;
         case RID_HELPINTRO:
-            aBasicApp.pHelp->Start( HELP_HELPONHELP );
+            aBasicApp.pHelp->Start( OOO_HELP_HELPONHELP );
             break;
 */      case RID_HELPABOUT:
             {
