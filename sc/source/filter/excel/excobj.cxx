0a80a3f73849eac53abcefc2aa1320f8fbe14c0d
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:47:09 +0000

    INTEGRATION: CWS pchfix04 (1.42.20); FILE MERGED 2007/04/26 05:41:16 hjs 1.42.20.2: RESYNC: (1.42-1.43); FILE MERGED 2007/02/05 08:35:20 os 1.42.20.1: #i73604# usage of ITEMID_* removed

--- sc/source/filter/excel/excobj.cxx
+++ sc/source/filter/excel/excobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: excobj.cxx,v $
  *
- *  $Revision: 1.43 $
+ *  $Revision: 1.44 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:21:58 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:47:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,6 @@
 
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
-#define ITEMID_FIELD EE_FEATURE_FIELD
 
 #include <svx/colritem.hxx>
 #include <svx/svddef.hxx>
