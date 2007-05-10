381bda9b48198fea879241dbc0de29b1b44a29c7
Kurt Zenker <kz@openoffice.org>
2007-05-10 09:33:51 +0000

    INTEGRATION: CWS dba23a (1.10.254); FILE MERGED 2007/03/13 08:42:17 fs 1.10.254.1: some slight re-factoring (class/method renaming), plus some rudimentary fix for #b6532894#

--- dbaccess/source/ui/inc/unoadmin.hxx
+++ dbaccess/source/ui/inc/unoadmin.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoadmin.hxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 16:05:47 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 10:33:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ namespace dbaui
 {
 //.........................................................................
 
-class IAdminHelper;
+class IDatabaseSettingsDialog;
 
 //=========================================================================
 //= ODatabaseAdministrationDialog
