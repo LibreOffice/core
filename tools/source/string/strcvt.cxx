ff8bde40404c771ba4eb8df29e67153d6bda540c
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:39:12 +0000

    INTEGRATION: CWS pchfix04 (1.10.44); FILE MERGED 2006/11/16 15:23:02 mkretzschmar 1.10.44.1: #i71619# Don't include pch files in cxx files that will be included

--- tools/source/string/strcvt.cxx
+++ tools/source/string/strcvt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: strcvt.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 01:03:09 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:39:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,7 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_tools.hxx"
+// no include "precompiled_tools.hxx" because this is included in other cxx files.
 
 // -----------------------------------------------------------------------
 
