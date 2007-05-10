d5c251af8800c2ecc5a396baeab636d50f5860a3
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:00:00 +0000

    INTEGRATION: CWS pchfix04 (1.94.72); FILE MERGED 2007/04/26 00:15:10 hjs 1.94.72.2: RESYNC: (1.94-1.95); FILE MERGED 2007/02/05 08:27:24 os 1.94.72.1: #i73604# usage of ITEMID_* removed

--- sw/source/core/layout/frmtool.cxx
+++ sw/source/core/layout/frmtool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmtool.cxx,v $
  *
- *  $Revision: 1.95 $
+ *  $Revision: 1.96 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-28 15:47:55 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:00:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,8 +35,6 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
-#define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
-#define ITEMID_SIZE         SID_ATTR_PAGE_SIZE
 #include <hintids.hxx>
 
 #ifndef _BIGINT_HXX //autogen
