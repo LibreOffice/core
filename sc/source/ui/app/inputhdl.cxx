a3274c5526291d3f7829229a40e0235f4c912605
Kurt Zenker <kz@openoffice.org>
2007-05-10 15:53:46 +0000

    INTEGRATION: CWS pchfix04 (1.67.38); FILE MERGED 2007/04/26 04:03:44 hjs 1.67.38.2: RESYNC: (1.67-1.68); FILE MERGED 2007/02/05 08:35:27 os 1.67.38.1: #i73604# usage of ITEMID_* removed

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inputhdl.cxx,v $
  *
- *  $Revision: 1.68 $
+ *  $Revision: 1.69 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:57:49 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 16:53:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
-#define ITEMID_FIELD EE_FEATURE_FIELD
+
 #include <sfx2/app.hxx>
 #include <svx/acorrcfg.hxx>
 #include <svx/algitem.hxx>
