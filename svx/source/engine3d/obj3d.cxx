47e75b333bc6c2ec1747390d89274c1081b2f509
Kurt Zenker <kz@openoffice.org>
2007-05-10 13:47:07 +0000

    INTEGRATION: CWS pchfix04 (1.40.38); FILE MERGED 2007/04/25 18:32:26 hjs 1.40.38.2: RESYNC: (1.40-1.41); FILE MERGED 2007/02/05 12:14:00 os 1.40.38.1: #i73604# usage of ITEMID_* removed

--- svx/source/engine3d/obj3d.cxx
+++ svx/source/engine3d/obj3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: obj3d.cxx,v $
  *
- *  $Revision: 1.42 $
+ *  $Revision: 1.43 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-09 13:30:41 $
+ *  last change: $Author: kz $ $Date: 2007-05-10 14:47:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,6 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
 
-#define ITEMID_COLOR            SID_ATTR_3D_LIGHTCOLOR
 
 #include "svdstr.hrc"
 #include "svdglob.hxx"
