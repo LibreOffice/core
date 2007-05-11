e9667a4784b88e5a1ff6b2546c00549db943b694
Sven Jacobi <sj@openoffice.org>
2007-05-11 13:49:48 +0000

    initial version

--- sdext/source/minimizer/makefile.mk
+++ sdext/source/minimizer/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
-#   last change: $Author: sj $ $Date: 2007-05-11 13:56:50 $
+#   last change: $Author: sj $ $Date: 2007-05-11 14:49:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,8 @@
 #
 #*************************************************************************
 
-PRJ=..$/..$/..
-PRJNAME=customres
+PRJ=..$/..
+PRJNAME=sdext
 TARGET=SunPresentationMinimizer
 GEN_HID=FALSE
 
