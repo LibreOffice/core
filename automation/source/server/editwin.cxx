648a785744260215bf1871666096698bf5dfd588
Kurt Zenker <kz@openoffice.org>
2007-05-11 07:54:05 +0000

    INTEGRATION: CWS gh13 (1.5.24); FILE MERGED 2006/11/17 12:05:39 gh 1.5.24.1: remove warning for debug code

--- automation/source/server/editwin.cxx
+++ automation/source/server/editwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editwin.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:35:06 $
+ *  last change: $Author: kz $ $Date: 2007-05-11 08:54:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -165,7 +165,7 @@ void EditWindow::AddText( const String &rNew )
 
         pImpWorkWindow->m_aInhalt.SetSelection( Selection( SELECTION_MAX, SELECTION_MAX ) );
         pImpWorkWindow->m_aInhalt.ReplaceSelected( aText );
-        nTextLen += aText.Len();
+        nTextLen = nTextLen + aText.Len();
         pImpWorkWindow->m_aInhalt.SetSelection( Selection( SELECTION_MAX, SELECTION_MAX ) );
     }
     else
