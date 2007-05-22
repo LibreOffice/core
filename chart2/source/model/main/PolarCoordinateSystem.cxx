d04c9f50f1238b1c363f07b4dd9fa4ceb6b28937
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:40:20 +0000

    INTEGRATION: CWS chart2mst3 (1.3.4); FILE MERGED 2006/10/18 17:11:38 bm 1.3.4.6: RESYNC: (1.4-1.5); FILE MERGED 2006/04/11 15:33:13 bm 1.3.4.5: make creation of coordinate systems available via factory 2005/10/13 17:38:58 iha 1.3.4.4: renamed BoundedCoordinateSystem to CoordinateSystem 2005/10/07 11:57:07 bm 1.3.4.3: RESYNC: (1.3-1.4); FILE MERGED 2004/09/15 17:32:02 bm 1.3.4.2: API simplification 2004/04/01 10:53:06 bm 1.3.4.1: XChartType: may return a coordinate system now

--- chart2/source/model/main/PolarCoordinateSystem.cxx
+++ chart2/source/model/main/PolarCoordinateSystem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PolarCoordinateSystem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:13:46 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:40:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,70 +36,128 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "PolarCoordinateSystem.hxx"
-#include "PolarTransformation.hxx"
 #include "macros.hxx"
 #include "servicenames_coosystems.hxx"
 
+using namespace ::com::sun::star;
+
 using ::com::sun::star::uno::RuntimeException;
+using ::com::sun::star::uno::Reference;
 using ::com::sun::star::uno::Sequence;
 using ::rtl::OUString;
+
+namespace
+{
+
+static const ::rtl::OUString lcl_aServiceNamePolar2d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.PolarCoordinateSystem2d" ));
+static const ::rtl::OUString lcl_aServiceNamePolar3d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.PolarCoordinateSystem3d" ));
+
+static const ::rtl::OUString lcl_aImplementationNamePolar2d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart2.PolarCoordinateSystem2d" ));
+static const ::rtl::OUString lcl_aImplementationNamePolar3d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart2.PolarCoordinateSystem3d" ));
+}
 
 namespace chart
 {
 
-PolarCoordinateSystem::PolarCoordinateSystem( sal_Int32 nDim ) :
-        m_aTransformationToCartesian(
-            new PolarTransformation( PolarTransformation::TRANSFORM_FROM, nDim ) ),
-        m_aTransformationFromCartesian(
-            new PolarTransformation( PolarTransformation::TRANSFORM_TO, nDim ) ),
-        m_nDim( nDim )
-{
-}
+// explicit
+PolarCoordinateSystem::PolarCoordinateSystem(
+    const uno::Reference< uno::XComponentContext > & xContext,
+    sal_Int32 nDimensionCount /* = 2 */,
+    sal_Bool bSwapXAndYAxis /* = sal_False */ ) :
+        BaseCoordinateSystem( xContext, nDimensionCount, bSwapXAndYAxis )
+{}
+
+PolarCoordinateSystem::PolarCoordinateSystem(
+    const PolarCoordinateSystem & rSource ) :
+        BaseCoordinateSystem( rSource )
+{}
 
 PolarCoordinateSystem::~PolarCoordinateSystem()
 {}
 
 // ____ XCoordinateSystem ____
-::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation > SAL_CALL
-    PolarCoordinateSystem::getTransformationToCartesian()
+::rtl::OUString SAL_CALL PolarCoordinateSystem::getCoordinateSystemType()
     throw (RuntimeException)
-{
-    return m_aTransformationToCartesian;
-}
-
-::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation > SAL_CALL
-    PolarCoordinateSystem::getTransformationFromCartesian()
-    throw (RuntimeException)
-{
-    return m_aTransformationFromCartesian;
-}
-
-sal_Int32 SAL_CALL PolarCoordinateSystem::getDimension() throw (RuntimeException)
-{
-    return m_nDim;
-}
-
-::rtl::OUString SAL_CALL PolarCoordinateSystem::getCoordinateSystemType() throw (RuntimeException)
 {
     return CHART2_COOSYSTEM_POLAR_SERVICE_NAME;
 }
 
-::rtl::OUString SAL_CALL PolarCoordinateSystem::getViewServiceName() throw (RuntimeException)
+::rtl::OUString SAL_CALL PolarCoordinateSystem::getViewServiceName()
+    throw (RuntimeException)
 {
     return CHART2_COOSYSTEM_POLAR_VIEW_SERVICE_NAME;
 }
 
-// ____ XServiceInfo ____
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL PolarCoordinateSystem::createClone()
+    throw (RuntimeException)
+{
+    return Reference< util::XCloneable >( new PolarCoordinateSystem( *this ));
+}
 
+// ____ XServiceInfo ____
 Sequence< OUString > PolarCoordinateSystem::getSupportedServiceNames_Static()
 {
-    Sequence< OUString > aServices;
-//     Sequence< OUString > aServices( 1 );
-//     aServices[ 0 ] = C2U( "com.sun.star.chart2.CoordinateSystem" );
+    Sequence< OUString > aServices( 1 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_POLAR_SERVICE_NAME;
     return aServices;
 }
 
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
 APPHELPER_XSERVICEINFO_IMPL( PolarCoordinateSystem,
-                             C2U( "com.sun.star.comp.chart.PolarCoordinateSystem" ));
+                             C2U( "com.sun.star.comp.chart.PolarCoordinateSystem" ))
+
+
+// =================================
+// ==== PolarCoordinateSystem2d ====
+// =================================
+
+PolarCoordinateSystem2d::PolarCoordinateSystem2d(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        PolarCoordinateSystem( xContext, 2, sal_False )
+{}
+
+PolarCoordinateSystem2d::~PolarCoordinateSystem2d()
+{}
+
+// ____ XServiceInfo ____
+Sequence< OUString > PolarCoordinateSystem2d::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_POLAR_SERVICE_NAME;
+    aServices[ 1 ] = lcl_aServiceNamePolar2d;
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( PolarCoordinateSystem2d, lcl_aImplementationNamePolar2d )
+
+// =================================
+// ==== PolarCoordinateSystem3d ====
+// =================================
+
+PolarCoordinateSystem3d::PolarCoordinateSystem3d(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        PolarCoordinateSystem( xContext, 3, sal_False )
+{}
+
+PolarCoordinateSystem3d::~PolarCoordinateSystem3d()
+{}
+
+// ____ XServiceInfo ____
+Sequence< OUString > PolarCoordinateSystem3d::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_POLAR_SERVICE_NAME;
+    aServices[ 1 ] = lcl_aServiceNamePolar3d;
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( PolarCoordinateSystem3d, lcl_aImplementationNamePolar3d )
 
 }  // namespace chart
