6c38d9398ae00d43349b6303f8094beaf82b9f0e
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 15:04:31 +0000

    INTEGRATION: CWS chart2mst3 (1.2.14); FILE MERGED 2007/04/25 08:35:00 bm 1.2.14.1: saved changes done in file on branch before move here from ..

--- xmloff/inc/xmloff/xmltoken.hxx
+++ xmloff/inc/xmloff/xmltoken.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltoken.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 13:36:29 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 16:04:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2871,6 +2871,9 @@ namespace xmloff { namespace token {
         // --> FLR #i52127#
         XML_NUMBERED_PARAGRAPH,
         // <--
+
+        XML_SORT_BY_X_VALUES,
+
         XML_TOKEN_END
     };
 
