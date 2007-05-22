b23c4617e9d5e1910735fd3391962899cb8d19e0
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:06:12 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:45 iha 1.1.2.4: resync m195 -> m202 2006/11/10 15:06:12 bm 1.1.2.3: #i67875# add undo for own data 2005/11/16 10:53:07 iha 1.1.2.2: don't call cancelAction twice 2005/11/15 18:07:34 iha 1.1.2.1: UndoGuard

---
+++ chart2/source/tools/UndoGuard.cxx
@@ -0,0 +1,119 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: UndoGuard.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:06:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "UndoGuard.hxx"
+
+using namespace ::com::sun::star;
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Sequence;
+using ::rtl::OUString;
+
+namespace chart
+{
+
+UndoGuard_Base::UndoGuard_Base( const rtl::OUString& rUndoString
+        , UndoManager& rUndoManager
+        , const uno::Reference< frame::XModel > & xModel )
+        : m_xModel( xModel )
+        , m_rUndoManager( rUndoManager )
+        , m_aUndoString( rUndoString )
+        , m_bActionPosted( false )
+{
+}
+
+UndoGuard_Base::~UndoGuard_Base()
+{
+}
+
+void UndoGuard_Base::commitAction()
+{
+    if( !m_bActionPosted )
+        m_rUndoManager.postAction( m_aUndoString );
+    m_bActionPosted = true;
+}
+
+//-----------------------------------------------------------------------------
+
+UndoGuard::UndoGuard( const rtl::OUString& rUndoString
+        , UndoManager& rUndoManager
+        , const uno::Reference< frame::XModel > & xModel )
+        : UndoGuard_Base( rUndoString, rUndoManager, xModel )
+{
+    m_rUndoManager.preAction( m_xModel );
+}
+
+UndoGuard::~UndoGuard()
+{
+    if( !m_bActionPosted )
+        m_rUndoManager.cancelAction();
+}
+
+//-----------------------------------------------------------------------------
+
+UndoLiveUpdateGuard::UndoLiveUpdateGuard( const rtl::OUString& rUndoString
+        , UndoManager& rUndoManager
+        , const uno::Reference< frame::XModel > & xModel )
+        : UndoGuard_Base( rUndoString, rUndoManager, xModel )
+{
+    m_rUndoManager.preAction( m_xModel );
+}
+
+UndoLiveUpdateGuard::~UndoLiveUpdateGuard()
+{
+    if( !m_bActionPosted )
+        m_rUndoManager.cancelActionUndo( m_xModel );
+}
+
+//-----------------------------------------------------------------------------
+
+UndoLiveUpdateGuardWithData::UndoLiveUpdateGuardWithData( const rtl::OUString& rUndoString
+        , UndoManager& rUndoManager
+        , const uno::Reference< frame::XModel > & xModel )
+        : UndoGuard_Base( rUndoString, rUndoManager, xModel )
+{
+    m_rUndoManager.preActionWithData( m_xModel );
+}
+
+UndoLiveUpdateGuardWithData::~UndoLiveUpdateGuardWithData()
+{
+    if( !m_bActionPosted )
+        m_rUndoManager.cancelActionUndo( m_xModel );
+}
+
+} //  namespace chart
