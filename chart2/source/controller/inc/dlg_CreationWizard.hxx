e4942786dab938184fac43ae2ef50f384917c7fa
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:58:03 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/03/07 16:52:05 bm 1.1.2.16: #i75207# use an overloaded leaveState() instead of disabling the roadmap entries. Disadvantage: the entries are shown as disabled, advantage: no crash 2007/02/28 16:07:50 bm 1.1.2.15: removed CHART2_ROADMAP_WIZARD define and else branch as we will not switch back from using the roadmap 2007/02/27 14:01:50 bm 1.1.2.14: Disable controls and tabs when invalid ranges were entered 2006/11/10 21:03:21 iha 1.1.2.13: prevent immidiate redraw in wizard and according dialogs (important for charts with many data) 2006/03/10 10:20:41 iha 1.1.2.12: don't use leading underscore 2005/10/11 09:20:24 bm 1.1.2.11: license header change 2005/04/15 14:54:40 bm 1.1.2.10: data source and range choose dialog rework 2004/06/29 12:29:28 bm 1.1.2.9: class for sharing data and functionality between the range chooser and the data source (data series) tab pages. 2004/06/11 15:44:27 iha 1.1.2.8: activate roadmap again 2004/06/09 21:58:21 iha 1.1.2.7: disable roadmap again 2004/06/09 17:49:47 iha 1.1.2.6: enable roadmap 2004/06/08 19:30:34 iha 1.1.2.5: added stops at first and last page; added roadmap (if 0) 2004/06/03 10:03:29 iha 1.1.2.4: changed signiture 'createPage' 2004/05/27 15:58:11 iha 1.1.2.3: #i20344# provide charttype template 2004/04/21 16:55:41 iha 1.1.2.2: #i20344# replace old type dialog with new type page 2004/04/07 21:30:53 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/inc/dlg_CreationWizard.hxx
@@ -0,0 +1,119 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: dlg_CreationWizard.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:58:03 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_CREATION_WIZARD_HXX
+#define _CHART2_CREATION_WIZARD_HXX
+
+#include "ServiceMacros.hxx"
+#include "TimerTriggeredControllerLock.hxx"
+#include "TabPageNotifiable.hxx"
+
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+#ifndef SVTOOLS_INC_ROADMAPWIZARD_HXX
+#include <svtools/roadmapwizard.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+// for auto_ptr
+#include <memory>
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+class RangeChooserTabPage;
+class DataSourceTabPage;
+class ChartTypeTemplateProvider;
+class DialogModel;
+
+class CreationWizard : public
+    svt::RoadmapWizard
+    , public TabPageNotifiable
+{
+public:
+    CreationWizard( Window* pParent,
+        const ::com::sun::star::uno::Reference<
+        ::com::sun::star::frame::XModel >& xChartModel
+        , const ::com::sun::star::uno::Reference<
+        ::com::sun::star::uno::XComponentContext >& xContext
+        , sal_Int32 nOnePageOnlyIndex=-1 );//if nOnePageOnlyIndex is an index of an exsisting  page starting with 0, then only this page is displayed without next/previous and roadmap
+    virtual ~CreationWizard();
+
+    bool isClosable();
+
+    // TabPageNotifiable
+    virtual void setInvalidPage( TabPage * pTabPage );
+    virtual void setValidPage( TabPage * pTabPage );
+
+protected:
+    virtual sal_Bool        leaveState( WizardState _nState );
+    virtual WizardState     determineNextState(WizardState nCurrentState);
+    virtual void            enterState(WizardState nState);
+
+    virtual String  getStateDisplayName( WizardState nState );
+
+private:
+    //no default constructor
+    CreationWizard();
+
+    virtual svt::OWizardPage* createPage(WizardState nState);
+
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::chart2::XChartDocument >   m_xChartModel;
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::uno::XComponentContext>    m_xCC;
+    bool m_bIsClosable;
+    sal_Int32 m_nOnePageOnlyIndex;//if nOnePageOnlyIndex is an index of an exsisting page starting with 0, then only this page is displayed without next/previous and roadmap
+    ChartTypeTemplateProvider*   m_pTemplateProvider;
+    ::std::auto_ptr< DialogModel > m_apDialogModel;
+
+    WizardState m_nFirstState;
+    WizardState m_nLastState;
+
+    TimerTriggeredControllerLock   m_aTimerTriggeredControllerLock;
+
+//     RangeChooserTabPage * m_pRangeChooserTabePage;
+//     DataSourceTabPage *   m_pDataSourceTabPage;
+    bool                  m_bCanTravel;
+};
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
