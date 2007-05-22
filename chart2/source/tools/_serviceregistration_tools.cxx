3cad1d2627063a524711db5e5aefb46bee563fad
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:07:43 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:47 iha 1.1.2.7: resync m195 -> m202 2006/06/26 12:31:41 bm 1.1.2.6: #i66692# offer regression curves and error bars via service manager 2006/04/05 11:18:08 bm 1.1.2.5: make Scalings available via factory 2005/10/11 09:20:57 bm 1.1.2.4: license header change 2005/07/28 09:34:52 bm 1.1.2.3: usage of color schemes and the VaryColorsByPoint property to have correct pie colors and legend entries 2005/06/20 14:35:28 bm 1.1.2.2: register DataSource 2005/05/27 12:19:14 bm 1.1.2.1: offer LabeledDataSequence as service

---
+++ chart2/source/tools/_serviceregistration_tools.cxx
@@ -0,0 +1,183 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: _serviceregistration_tools.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:07:43 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#ifndef _CPPUHELPER_IMPLEMENATIONENTRY_HXX_
+#include <cppuhelper/implementationentry.hxx>
+#endif
+#include "LabeledDataSequence.hxx"
+#include "DataSource.hxx"
+#include "ConfigColorScheme.hxx"
+#include "Scaling.hxx"
+#include "ErrorBar.hxx"
+#include "RegressionCurveModel.hxx"
+
+static struct ::cppu::ImplementationEntry g_entries_chart2_tools[] =
+{
+    {
+          ::chart::LabeledDataSequence::create
+        , ::chart::LabeledDataSequence::getImplementationName_Static
+        , ::chart::LabeledDataSequence::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::DataSource::create
+        , ::chart::DataSource::getImplementationName_Static
+        , ::chart::DataSource::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::ConfigColorScheme::create
+        , ::chart::ConfigColorScheme::getImplementationName_Static
+        , ::chart::ConfigColorScheme::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+
+    ,{
+          ::chart::LogarithmicScaling::create
+        , ::chart::LogarithmicScaling::getImplementationName_Static
+        , ::chart::LogarithmicScaling::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::ExponentialScaling::create
+        , ::chart::ExponentialScaling::getImplementationName_Static
+        , ::chart::ExponentialScaling::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::LinearScaling::create
+        , ::chart::LinearScaling::getImplementationName_Static
+        , ::chart::LinearScaling::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::PowerScaling::create
+        , ::chart::PowerScaling::getImplementationName_Static
+        , ::chart::PowerScaling::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::ErrorBar::create
+        , ::chart::ErrorBar::getImplementationName_Static
+        , ::chart::ErrorBar::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::MeanValueRegressionCurve::create
+        , ::chart::MeanValueRegressionCurve::getImplementationName_Static
+        , ::chart::MeanValueRegressionCurve::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::LinearRegressionCurve::create
+        , ::chart::LinearRegressionCurve::getImplementationName_Static
+        , ::chart::LinearRegressionCurve::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::LogarithmicRegressionCurve::create
+        , ::chart::LogarithmicRegressionCurve::getImplementationName_Static
+        , ::chart::LogarithmicRegressionCurve::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::ExponentialRegressionCurve::create
+        , ::chart::ExponentialRegressionCurve::getImplementationName_Static
+        , ::chart::ExponentialRegressionCurve::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{
+          ::chart::PotentialRegressionCurve::create
+        , ::chart::PotentialRegressionCurve::getImplementationName_Static
+        , ::chart::PotentialRegressionCurve::getSupportedServiceNames_Static
+        , ::cppu::createSingleComponentFactory
+        , 0
+        , 0
+    }
+   ,{ 0, 0, 0, 0, 0, 0 }
+};
+
+// component exports
+extern "C"
+{
+//==================================================================================================
+void SAL_CALL component_getImplementationEnvironment(
+    const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv )
+{
+    *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
+}
+//==================================================================================================
+sal_Bool SAL_CALL component_writeInfo(
+    void * pServiceManager, void * pRegistryKey )
+{
+    return ::cppu::component_writeInfoHelper(
+                pServiceManager, pRegistryKey, g_entries_chart2_tools );
+}
+//==================================================================================================
+void * SAL_CALL component_getFactory(
+    const sal_Char * pImplName, void * pServiceManager, void * pRegistryKey )
+{
+    return ::cppu::component_getFactoryHelper(
+        pImplName, pServiceManager, pRegistryKey , g_entries_chart2_tools );
+}
+}
+//=========================================================================
