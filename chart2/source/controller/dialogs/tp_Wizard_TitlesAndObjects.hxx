c6738e2ed0025196feba693818b2c63ef0ccbdb7
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:50:36 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/02 23:04:57 iha 1.1.2.11: some resource changes according to wizard spec 2006/11/10 21:03:07 iha 1.1.2.10: prevent immidiate redraw in wizard and according dialogs (important for charts with many data) 2006/03/10 10:20:41 iha 1.1.2.9: don't use leading underscore 2006/01/23 15:55:34 iha 1.1.2.8: #i61054# disable next button at wizard correctly 2006/01/06 20:27:28 iha 1.1.2.7: added legendposition to wizard and restructured legend postiion control classes 2005/11/14 18:16:25 iha 1.1.2.6: don't change model during initialization of controls 2005/10/11 09:20:19 bm 1.1.2.5: license header change 2004/06/09 17:45:49 iha 1.1.2.4: live update 2004/04/27 15:00:39 iha 1.1.2.3: warnings removed 2004/04/22 15:00:46 iha 1.1.2.2: #i20344# chabged legend, grid and title resources 2004/04/07 21:27:45 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/dialogs/tp_Wizard_TitlesAndObjects.hxx
@@ -0,0 +1,112 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_Wizard_TitlesAndObjects.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:50:36 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_TP_WIZARD_TITLESANDOBJECTS_HXX
+#define _CHART2_TP_WIZARD_TITLESANDOBJECTS_HXX
+
+#include "TimerTriggeredControllerLock.hxx"
+
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+
+#ifndef _SV_EDIT_HXX
+#include <vcl/edit.hxx>
+#endif
+#ifndef _SV_FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
+//for auto_ptr
+#include <memory>
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+class TitleResources;
+class LegendPositionResources;
+class TitlesAndObjectsTabPage : public svt::OWizardPage
+{
+public:
+    TitlesAndObjectsTabPage( svt::OWizardMachine* pParent
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::chart2::XChartDocument >& xChartModel
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::uno::XComponentContext >& xContext );
+    virtual ~TitlesAndObjectsTabPage();
+
+    virtual void        initializePage();
+    virtual sal_Bool    commitPage( COMMIT_REASON eReason );
+    virtual sal_Bool    determineNextButtonState();
+
+protected:
+    void commitToModel();
+    DECL_LINK( ChangeHdl, void* );
+
+protected:
+    FixedText           m_aFT_TitleDescription;
+    FixedLine           m_aFL_Vertical;
+
+    ::std::auto_ptr< TitleResources >            m_apTitleResources;
+    ::std::auto_ptr< LegendPositionResources >   m_apLegendPositionResources;
+
+    FixedLine           m_aFL_Grids;
+    CheckBox            m_aCB_Grid_X;
+    CheckBox            m_aCB_Grid_Y;
+    CheckBox            m_aCB_Grid_Z;
+
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::chart2::XChartDocument >   m_xChartModel;
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::uno::XComponentContext>    m_xCC;
+
+    bool    m_bCommitToModel;
+    TimerTriggeredControllerLock   m_aTimerTriggeredControllerLock;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
