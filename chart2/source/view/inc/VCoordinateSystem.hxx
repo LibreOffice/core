2de1836a889ad58c437567ef0e21a5591b53a496
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:20:58 +0000

    INTEGRATION: CWS chart2mst3 (1.3.4); FILE MERGED 2006/11/09 15:29:02 iha 1.3.4.17: #i16280# better performance for charts with many data (bar,column,line,area,xy,net charts) 2006/09/24 09:12:57 iha 1.3.4.16: #i61970# correct implementation of axis at 2006/08/25 17:15:10 iha 1.3.4.15: text- and diagram-size dependent auto main step width 2006/08/23 17:07:27 iha 1.3.4.14: enable axis to estimate the maximum automatic MainIncrementCount, reduce count to 2 for net charts 2006/08/22 19:06:55 iha 1.3.4.13: correct y axis for net charts: show labels + take space for labels into account for diagram size 2006/08/10 15:55:51 iha 1.3.4.12: Axis Label Layout - Font&Diagram Size 2006/04/10 12:25:19 iha 1.3.4.11: api restructure axis, grids, scales and increments 2006/03/27 11:49:50 bm 1.3.4.10: implementationreference.hxx is written in lowercase 2006/03/23 15:32:15 iha 1.3.4.9: cleanup category handling 2005/12/21 21:29:39 iha 1.3.4.8: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/25 16:24:32 iha 1.3.4.7: correct labels at z axis for deep stacking 2005/10/24 11:07:12 iha 1.3.4.6: coordinate system restructure 2005/10/13 17:39:13 iha 1.3.4.5: renamed BoundedCoordinateSystem to CoordinateSystem 2005/10/07 12:22:00 bm 1.3.4.4: RESYNC: (1.3-1.4); FILE MERGED 2005/06/07 16:02:05 iha 1.3.4.3: use numberformatter from model 2004/03/22 19:32:46 iha 1.3.4.2: added method getPropertySwapXAndYAxis() 2004/02/26 12:18:09 iha 1.3.4.1: share coordinate systems and scales for combi charts

--- chart2/source/view/inc/VCoordinateSystem.hxx
+++ chart2/source/view/inc/VCoordinateSystem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VCoordinateSystem.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:45:07 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:20:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,19 +35,29 @@
 #ifndef _CHART2_VCOORDINATESYSTEM_HXX
 #define _CHART2_VCOORDINATESYSTEM_HXX
 
+#include "MinimumAndMaximumSupplier.hxx"
+#include "ScaleAutomatism.hxx"
+
 #ifndef _COM_SUN_STAR_CHART2_EXPLICITINCREMENTDATA_HPP_
 #include <com/sun/star/chart2/ExplicitIncrementData.hpp>
 #endif
 #ifndef _COM_SUN_STAR_CHART2_EXPLICITSCALEDATA_HPP_
 #include <com/sun/star/chart2/ExplicitScaleData.hpp>
 #endif
-#ifndef _COM_SUN_STAR_CHART2_XAXIS_HPP_
-#include <com/sun/star/chart2/XAxis.hpp>
+#ifndef _COM_SUN_STAR_CHART2_XCOORDINATESYSTEM_HPP_
+#include <com/sun/star/chart2/XCoordinateSystem.hpp>
 #endif
-#ifndef _COM_SUN_STAR_CHART2_XGRID_HPP_
-#include <com/sun/star/chart2/XGrid.hpp>
+#ifndef _COM_SUN_STAR_CHART2_DATA_XTEXTUALDATASEQUENCE_HPP_
+#include <com/sun/star/chart2/data/XTextualDataSequence.hpp>
 #endif
 
+#ifndef _COMPHELPER_IMPLEMENTATIONREFERENCE_HXX
+#include "comphelper/implementationreference.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_AWT_RECTANGLE_HPP_
+#include <com/sun/star/awt/Rectangle.hpp>
+#endif
 #ifndef _COM_SUN_STAR_DRAWING_HOMOGENMATRIX_HPP_
 #include <com/sun/star/drawing/HomogenMatrix.hpp>
 #endif
@@ -57,28 +67,32 @@
 #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #endif
-// #ifndef _COM_SUN_STAR_CHART2_XBOUNDEDCOORDINATESYSTEMCONTAINER_HPP_
-// #include <com/sun/star/chart2/XBoundedCoordinateSystemContainer.hpp>
-// #endif
+#ifndef _COM_SUN_STAR_UTIL_XNUMBERFORMATSSUPPLIER_HPP_
+#include <com/sun/star/util/XNumberFormatsSupplier.hpp>
+#endif
+
+#include<map>
+#include <vector>
+#include <boost/shared_ptr.hpp>
 
 //.............................................................................
 namespace chart
 {
 //.............................................................................
 
+class ExplicitCategoriesProvider;
+
 //-----------------------------------------------------------------------------
 /**
 */
-class MinimumAndMaximumSupplier;
-class NumberFormatterWrapper;
-
+class VAxisBase;
 class VCoordinateSystem
 {
 public:
     virtual ~VCoordinateSystem();
 
     static VCoordinateSystem* createCoordinateSystem( const ::com::sun::star::uno::Reference<
-                                ::com::sun::star::chart2::XBoundedCoordinateSystem >& xCooSysModel );
+                                ::com::sun::star::chart2::XCoordinateSystem >& xCooSysModel );
 
     virtual void SAL_CALL initPlottingTargets(
                   const ::com::sun::star::uno::Reference<
@@ -88,55 +102,99 @@ public:
                 , const ::com::sun::star::uno::Reference<
                         ::com::sun::star::lang::XMultiServiceFactory >& xFactory )
                         throw (::com::sun::star::uno::RuntimeException);
-    void setOrigin( double* fCoordinateOrigin );
-    void addAxis( const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis >& xAxis );
-    void addGrid( const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XGrid >& xGrid );
-    void doAutoScale( MinimumAndMaximumSupplier* pMinMaxSupplier );
 
-    void setTransformationSceneToScreen( const ::com::sun::star::drawing::HomogenMatrix& rMatrix );
+    void setParticle( const rtl::OUString& rCooSysParticle );
 
-    const ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitScaleData >& getExplicitScales() const {return m_aExplicitScales;}
-    const ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitIncrementData >& getExplicitIncrements() const {return m_aExplicitIncrements;}
-    double getOriginByDimension( sal_Int32 nDim ) const { return m_fCoordinateOrigin[nDim]; }
+    virtual void setTransformationSceneToScreen( const ::com::sun::star::drawing::HomogenMatrix& rMatrix );
+    ::com::sun::star::drawing::HomogenMatrix getTransformationSceneToScreen();
+
+    //better performance for big data
+    virtual ::com::sun::star::uno::Sequence< sal_Int32 > getCoordinateSystemResolution( const ::com::sun::star::awt::Size& rPageSize
+                                    , const ::com::sun::star::awt::Size& rPageResolution );
+
+    ::com::sun::star::chart2::ExplicitScaleData getExplicitScale( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex ) const;
+    ::com::sun::star::chart2::ExplicitIncrementData getExplicitIncrement( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex ) const;
+    ::com::sun::star::uno::Reference< ::com::sun::star::chart2::data::XTextualDataSequence > getExplicitCategoriesProvider();
+
+    // returns a coplete scale set for a given dimension and index; for example if nDimensionIndex==1 and nAxisIndex==2 you get returned the secondary x axis, main y axis and main z axis
+    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitScaleData > getExplicitScales( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex ) const;
+    // returns a coplete increment set for a given dimension and index; for example if nDimensionIndex==1 and nAxisIndex==2 you get returned the secondary x axis, main y axis and main z axis
+    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitIncrementData > getExplicitIncrements( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex ) const;
+
+    void addMinimumAndMaximumSupplier( MinimumAndMaximumSupplier* pMinimumAndMaximumSupplier );
+    bool hasMinimumAndMaximumSupplier( MinimumAndMaximumSupplier* pMinimumAndMaximumSupplier );
+    void clearMinimumAndMaximumSupplierList();
+
+    void prepareScaleAutomatismForDimensionAndIndex( ScaleAutomatism& rScaleAutomatism, sal_Int32 nDimIndex, sal_Int32 nAxisIndex );
+
+    void setExplicitScaleAndIncrement( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex
+        , const ::com::sun::star::chart2::ExplicitScaleData& rExplicitScale
+        , const ::com::sun::star::chart2::ExplicitIncrementData& rExplicitIncrement );
 
     ::com::sun::star::uno::Reference<
-        ::com::sun::star::chart2::XBoundedCoordinateSystem >
+        ::com::sun::star::chart2::XCoordinateSystem >
         getModel() const;
 
+    virtual void createVAxisList(
+            const ::com::sun::star::uno::Reference< ::com::sun::star::util::XNumberFormatsSupplier > & xNumberFormatsSupplier
+            , const ::com::sun::star::awt::Size& rFontReferenceSize
+            , const ::com::sun::star::awt::Rectangle& rMaximumSpaceForLabels );
+
+    virtual void initVAxisInList();
+    virtual void updateScalesAndIncrementsOnAxes();
+
+    virtual void createMaximumAxesLabels();
+    virtual void createAxesLabels();
+    virtual void updatePositions();
+    virtual void createAxesShapes();
+
     virtual void createGridShapes();
-    virtual void createAxesShapes( const ::com::sun::star::awt::Size& rReferenceSize, NumberFormatterWrapper* pNumberFormatterWrapper );
+
+    virtual bool getPropertySwapXAndYAxis() const;
+
+    sal_Int32 getMaximumAxisIndexByDimension( sal_Int32 nDimensionIndex ) const;
+
+    virtual bool needSeriesNamesForAxis() const;
+    void setSeriesNamesForAxis( const ::com::sun::star::uno::Sequence< rtl::OUString >& rSeriesNames );
 
 protected: //methods
     VCoordinateSystem( const ::com::sun::star::uno::Reference<
-        ::com::sun::star::chart2::XBoundedCoordinateSystem >& xCooSys );
+        ::com::sun::star::chart2::XCoordinateSystem >& xCooSys );
 
-    ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XGrid > >&
-        getGridListByDimension( sal_Int32 nDim );
     ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis >
-        getAxisByDimension( sal_Int32 nDim  ) const;
+        getAxisByDimension( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex  ) const;
+    ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > >
+        getGridListFromAxis( const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis >& xAxis );
+
+    VAxisBase* getVAxis( sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex );
+
+    void prepareScaleAutomatism( ScaleAutomatism& rScaleAutomatism, double fMin, double fMax, sal_Int32 nDimIndex, sal_Int32 nAxisIndex );
+
+    sal_Int32 getMaximumIncrementIndexByDimension( sal_Int32 nDimensionIndex ) const;
+
+    rtl::OUString createCIDForAxis( const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::chart2::XAxis >& xAxis
+                    , sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex );
+    rtl::OUString createCIDForGrid( const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::chart2::XAxis >& xAxis
+                    , sal_Int32 nDimensionIndex, sal_Int32 nAxisIndex );
+
+    sal_Int32 getNumberFormatKeyForAxis( const ::com::sun::star::uno::Reference<
+                     ::com::sun::star::chart2::XAxis >& xAxis
+                     , const ::com::sun::star::uno::Reference<
+                     ::com::sun::star::util::XNumberFormatsSupplier >& xNumberFormatsSupplier );
+
+private: //methods
+    void impl_adjustDimension( sal_Int32& rDimensionIndex ) const;
+    void impl_adjustDimensionAndIndex( sal_Int32& rDimensionIndex, sal_Int32& rAxisIndex ) const;
 
 protected: //member
     ::com::sun::star::uno::Reference<
-        ::com::sun::star::chart2::XBoundedCoordinateSystem > m_xCooSysModel;
+        ::com::sun::star::chart2::XCoordinateSystem > m_xCooSysModel;
 
-    ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis > m_xAxis0;
-    ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis > m_xAxis1;
-    ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XAxis > m_xAxis2;
+    rtl::OUString m_aCooSysParticle;
 
-    ::com::sun::star::uno::Sequence<
-        ::com::sun::star::uno::Reference<
-            ::com::sun::star::chart2::XGrid > > m_xGridList0;
-    ::com::sun::star::uno::Sequence<
-        ::com::sun::star::uno::Reference<
-            ::com::sun::star::chart2::XGrid > > m_xGridList1;
-    ::com::sun::star::uno::Sequence<
-        ::com::sun::star::uno::Reference<
-            ::com::sun::star::chart2::XGrid > > m_xGridList2;
-
-    double m_fCoordinateOrigin[3];
-
-    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitScaleData >     m_aExplicitScales;
-    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitIncrementData > m_aExplicitIncrements;
+    typedef std::pair< sal_Int32, sal_Int32 > tFullAxisIndex; //first index is the dimension, second index is the axis index that indicates wether this is a main or secondary axis
 
     //
     ::com::sun::star::uno::Reference<
@@ -148,6 +206,28 @@ protected: //member
     ::com::sun::star::uno::Reference<
                     ::com::sun::star::lang::XMultiServiceFactory>       m_xShapeFactory;
     ::com::sun::star::drawing::HomogenMatrix                            m_aMatrixSceneToScreen;
+
+    //
+    MergedMinimumAndMaximumSupplier m_aMergedMinimumAndMaximumSupplier; //this is used only for autoscaling purpose
+
+    ::com::sun::star::uno::Sequence< rtl::OUString > m_aSeriesNamesForZAxis;
+
+    typedef std::map< tFullAxisIndex, ::boost::shared_ptr< VAxisBase > > tVAxisMap;
+
+    tVAxisMap m_aAxisMap;
+
+private:
+    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitScaleData >     m_aExplicitScales;
+    ::com::sun::star::uno::Sequence< ::com::sun::star::chart2::ExplicitIncrementData > m_aExplicitIncrements;
+
+    typedef std::map< tFullAxisIndex, ::com::sun::star::chart2::ExplicitScaleData > tFullExplicitScaleMap;
+    typedef std::map< tFullAxisIndex, ::com::sun::star::chart2::ExplicitIncrementData > tFullExplicitIncrementMap;
+
+    tFullExplicitScaleMap       m_aSecondaryExplicitScales;
+    tFullExplicitIncrementMap   m_aSecondaryExplicitIncrements;
+
+    comphelper::ImplementationReference< ExplicitCategoriesProvider, ::com::sun::star::chart2::data::XTextualDataSequence >
+                                m_aExplicitCategoriesProvider;
 };
 
 //.............................................................................
