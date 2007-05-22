01ebcc747c26b2153d5a01b692af417dc48cf813
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:33:59 +0000

    INTEGRATION: CWS chart2mst3 (1.4.4); FILE MERGED 2006/10/20 20:29:07 iha 1.4.4.10: implement gradient step count 2005/11/03 11:59:15 bm 1.4.4.9: fire modify events on property change 2005/10/07 11:52:50 bm 1.4.4.8: RESYNC: (1.4-1.5); FILE MERGED 2005/07/22 12:54:32 bm 1.4.4.7: #124485# property FillBackground added 2005/07/14 13:00:22 iha 1.4.4.6: remove unused parameter 'bIncludeStyleProperties' from series and point properties 2005/07/14 11:35:08 iha 1.4.4.5: use identical fast-property-IDs for lineproperties everywhere 2005/07/12 12:57:08 bm 1.4.4.4: use named properties for gradients etc. in chart model 2004/06/08 14:31:05 iha 1.4.4.3: added property RoundedEdges 2004/03/08 18:58:45 iha 1.4.4.2: added Geometry3D to DataPointPoperties 2004/02/13 16:51:28 bm 1.4.4.1: join from changes on branch bm_post_chart01

--- chart2/source/model/main/DataPointProperties.hxx
+++ chart2/source/model/main/DataPointProperties.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataPointProperties.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:55:45 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:33:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,33 +55,30 @@ public:
     enum
     {
         // common
-        PROP_DATAPOINT_STYLE = FAST_PROPERTY_ID_START_DATA_POINT,
-        PROP_DATAPOINT_COLOR,
+        PROP_DATAPOINT_COLOR = FAST_PROPERTY_ID_START_DATA_POINT,
         PROP_DATAPOINT_TRANSPARENCY,
 
         // fill
         PROP_DATAPOINT_FILL_STYLE,
-        PROP_DATAPOINT_TRANSPARENCY_STYLE,
-        PROP_DATAPOINT_TRANSPARENCY_GRADIENT,
-        PROP_DATAPOINT_GRADIENT,
-        PROP_DATAPOINT_HATCH,
-        PROP_DATAPOINT_FILL_BITMAP,
+//         PROP_DATAPOINT_TRANSPARENCY_STYLE,
+        PROP_DATAPOINT_TRANSPARENCY_GRADIENT_NAME,
+        PROP_DATAPOINT_GRADIENT_NAME,
+        PROP_DATAPOINT_GRADIENT_STEPCOUNT,
+        PROP_DATAPOINT_HATCH_NAME,
+        PROP_DATAPOINT_FILL_BITMAP_NAME,
+        PROP_DATAPOINT_FILL_BACKGROUND,
 
         // border (of filled objects)
         PROP_DATAPOINT_BORDER_COLOR,
         PROP_DATAPOINT_BORDER_STYLE,
         PROP_DATAPOINT_BORDER_WIDTH,
-        PROP_DATAPOINT_BORDER_DASH,
+        PROP_DATAPOINT_BORDER_DASH_NAME,
         PROP_DATAPOINT_BORDER_TRANSPARENCY,
-
-        // line
-        PROP_DATAPOINT_LINE_STYLE,
-        PROP_DATAPOINT_LINE_WIDTH,
-        PROP_DATAPOINT_LINE_DASH,
 
         // others
         PROP_DATAPOINT_SYMBOL_PROP,
         PROP_DATAPOINT_OFFSET,
+        PROP_DATAPOINT_GEOMETRY3D,
         PROP_DATAPOINT_LABEL,
         PROP_DATAPOINT_NUMBER_FORMAT,
         PROP_DATAPOINT_REFERENCE_DIAGRAM_SIZE,
@@ -89,14 +86,16 @@ public:
         // statistics
         PROP_DATAPOINT_ERROR_BAR_X,
         PROP_DATAPOINT_ERROR_BAR_Y,
-        PROP_DATAPOINT_SHOW_ERROR_BOX
+        PROP_DATAPOINT_SHOW_ERROR_BOX,
+        PROP_DATAPOINT_PERCENT_DIAGONAL
+
+        // additionally some properites from ::chart::LineProperties
     };
 
     static void AddPropertiesToVector(
-        ::std::vector< ::com::sun::star::beans::Property > & rOutProperties,
-        bool bIncludeStyleProperties = false );
+        ::std::vector< ::com::sun::star::beans::Property > & rOutProperties );
 
-    static void AddDefaultsToMap( ::chart::helper::tPropertyValueMap & rOutMap, bool bIncludeStyleProperties = false );
+    static void AddDefaultsToMap( ::chart::tPropertyValueMap & rOutMap );
 
 private:
     // not implemented
