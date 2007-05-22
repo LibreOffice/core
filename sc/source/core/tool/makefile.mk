cb093068f178ad7f2b8d03e80a4aa5c3d7bf1b5f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:43:27 +0000

    INTEGRATION: CWS chart2mst3 (1.11.36); FILE MERGED 2007/04/25 02:45:33 bm 1.11.36.7: RESYNC: (1.17-1.18); FILE MERGED 2006/10/18 23:39:23 bm 1.11.36.6: RESYNC: (1.15-1.17); FILE MERGED 2005/10/08 07:36:24 bm 1.11.36.5: RESYNC: (1.14-1.15); FILE MERGED 2005/03/17 13:05:30 bm 1.11.36.4: RESYNC: (1.13-1.14); FILE MERGED 2004/08/30 17:44:00 bm 1.11.36.3: RESYNC: (1.12-1.13); FILE MERGED 2004/06/15 18:02:17 bm 1.11.36.2: RESYNC: (1.11-1.12); FILE MERGED 2004/04/29 13:29:08 sab 1.11.36.1: #i17416#; seperate ChartPositionMap and ChartArray

--- sc/source/core/tool/makefile.mk
+++ sc/source/core/tool/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-27 12:17:15 $
+#   last change: $Author: vg $ $Date: 2007-05-22 19:43:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -68,6 +68,7 @@ SLOFILES =  \
         $(SLO)$/callform.obj \
         $(SLO)$/cellform.obj \
         $(SLO)$/chartarr.obj \
+        $(SLO)$/chartpos.obj \
         $(SLO)$/chartlis.obj \
         $(SLO)$/chgtrack.obj \
         $(SLO)$/chgviset.obj \
