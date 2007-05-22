2302bb2ded04227b11895ca1b42d69552d329317
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:39:50 +0000

    INTEGRATION: CWS chart2mst3 (1.3.4); FILE MERGED 2006/10/18 17:11:30 bm 1.3.4.10: RESYNC: (1.4-1.5); FILE MERGED 2005/11/07 17:12:44 bm 1.3.4.9: some missing notifications added 2005/10/07 11:56:47 bm 1.3.4.8: RESYNC: (1.3-1.4); FILE MERGED 2005/09/23 15:25:03 iha 1.3.4.7: #i55000# crash caused by dead reference to mutex 2005/08/03 16:36:20 bm 1.3.4.6: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/07/14 15:32:56 iha 1.3.4.5: cleanup WrappedIgnoreProperty 2005/07/14 12:32:55 iha 1.3.4.4: remove unused parameter 'bIncludeStyleProperties' 2005/03/30 16:31:09 bm 1.3.4.3: make model cloneable (+first undo implementation) 2004/03/02 09:46:54 bm 1.3.4.2: queryInterface to XPropertySet didn't work default fill-style 'solid' default fill-color 'white' 2004/02/13 16:51:34 bm 1.3.4.1: join from changes on branch bm_post_chart01

--- chart2/source/model/main/PageBackground.cxx
+++ chart2/source/model/main/PageBackground.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageBackground.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:13:32 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:39:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,17 +37,13 @@
 #include "precompiled_chart2.hxx"
 #include "PageBackground.hxx"
 #include "macros.hxx"
-#include "algohelper.hxx"
 #include "LineProperties.hxx"
 #include "FillProperties.hxx"
 #include "UserDefinedProperties.hxx"
+#include "ContainerHelper.hxx"
 
 #ifndef CHART_PROPERTYHELPER_HXX
 #include "PropertyHelper.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_DRAWING_FILLSTYLE_HPP_
-#include <com/sun/star/drawing/FillStyle.hpp>
 #endif
 
 #ifndef _RTL_UUID_H_
@@ -72,10 +68,10 @@ static const ::rtl::OUString lcl_aServiceName(
     RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart2.PageBackground" ));
 
 void lcl_AddDefaultsToMap(
-    ::chart::helper::tPropertyValueMap & rOutMap )
+    ::chart::tPropertyValueMap & rOutMap )
 {
-    rOutMap[ ::chart::FillProperties::PROP_FILL_STYLE ] =
-        uno::makeAny( drawing::FillStyle_NONE );
+    rOutMap[ ::chart::FillProperties::PROP_FILL_COLOR ] =
+        uno::makeAny( sal_Int32( 0xffffff ));
 }
 
 const uno::Sequence< Property > & lcl_GetPropertySequence()
@@ -88,18 +84,16 @@ const uno::Sequence< Property > & lcl_GetPropertySequence()
     {
         // get properties
         ::std::vector< ::com::sun::star::beans::Property > aProperties;
-         ::chart::LineProperties::AddPropertiesToVector(
-             aProperties, /* bIncludeStyleProperties = */ true );
-        ::chart::FillProperties::AddPropertiesToVector(
-            aProperties, /* bIncludeStyleProperties = */ true );
+         ::chart::LineProperties::AddPropertiesToVector( aProperties );
+        ::chart::FillProperties::AddPropertiesToVector( aProperties );
         ::chart::UserDefinedProperties::AddPropertiesToVector( aProperties );
 
         // and sort them for access via bsearch
         ::std::sort( aProperties.begin(), aProperties.end(),
-                     ::chart::helper::PropertyNameLess() );
+                     ::chart::PropertyNameLess() );
 
         // transfer result to static Sequence
-        aPropSeq = ::chart::helper::VectorToSequence( aProperties );
+        aPropSeq = ::chart::ContainerHelper::ContainerToSequence( aProperties );
     }
 
     return aPropSeq;
@@ -123,11 +117,25 @@ namespace chart
 
 PageBackground::PageBackground( const uno::Reference< uno::XComponentContext > & xContext ) :
         ::property::OPropertySet( m_aMutex ),
-    m_xContext( xContext )
+    m_xContext( xContext ),
+    m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex ))
+{}
+
+PageBackground::PageBackground( const PageBackground & rOther ) :
+        ::property::OPropertySet( rOther, m_aMutex ),
+    m_xContext( rOther.m_xContext ),
+    m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex ))
 {}
 
 PageBackground::~PageBackground()
 {}
+
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL PageBackground::createClone()
+    throw (uno::RuntimeException)
+{
+    return uno::Reference< util::XCloneable >( new PageBackground( *this ));
+}
 
 // ================================================================================
 
@@ -135,24 +143,20 @@ PageBackground::~PageBackground()
 uno::Any PageBackground::GetDefaultValue( sal_Int32 nHandle ) const
     throw(beans::UnknownPropertyException)
 {
-    static helper::tPropertyValueMap aStaticDefaults;
+    static tPropertyValueMap aStaticDefaults;
 
     // /--
     ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
     if( 0 == aStaticDefaults.size() )
     {
         // initialize defaults
-        LineProperties::AddDefaultsToMap(
-             aStaticDefaults,
-             /* bIncludeStyleProperties = */ true );
-        FillProperties::AddDefaultsToMap(
-            aStaticDefaults,
-            /* bIncludeStyleProperties = */ true );
+        LineProperties::AddDefaultsToMap( aStaticDefaults );
+        FillProperties::AddDefaultsToMap( aStaticDefaults );
         // overrides defaults of FillProperties
         lcl_AddDefaultsToMap( aStaticDefaults );
     }
 
-    helper::tPropertyValueMap::const_iterator aFound(
+    tPropertyValueMap::const_iterator aFound(
         aStaticDefaults.find( nHandle ));
 
     if( aFound == aStaticDefaults.end())
@@ -187,21 +191,59 @@ uno::Reference< beans::XPropertySetInfo > SAL_CALL
     // \--
 }
 
-// ____ XInterface ____
-uno::Any SAL_CALL PageBackground::queryInterface( const uno::Type& aType )
+
+// ____ XModifyBroadcaster ____
+void SAL_CALL PageBackground::addModifyListener( const uno::Reference< util::XModifyListener >& aListener )
     throw (uno::RuntimeException)
 {
-    return ::cppu::OWeakObject::queryInterface( aType );
+    try
+    {
+        uno::Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->addModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
 }
 
-void SAL_CALL PageBackground::acquire() throw ()
+void SAL_CALL PageBackground::removeModifyListener( const uno::Reference< util::XModifyListener >& aListener )
+    throw (uno::RuntimeException)
 {
-    ::cppu::OWeakObject::acquire();
+    try
+    {
+        uno::Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->removeModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
 }
 
-void SAL_CALL PageBackground::release() throw ()
+// ____ XModifyListener ____
+void SAL_CALL PageBackground::modified( const lang::EventObject& aEvent )
+    throw (uno::RuntimeException)
 {
-    ::cppu::OWeakObject::release();
+    m_xModifyEventForwarder->modified( aEvent );
+}
+
+// ____ XEventListener (base of XModifyListener) ____
+void SAL_CALL PageBackground::disposing( const lang::EventObject& Source )
+    throw (uno::RuntimeException)
+{
+    // nothing
+}
+
+// ____ OPropertySet ____
+void PageBackground::firePropertyChangeEvent()
+{
+    fireModifyEvent();
+}
+
+void PageBackground::fireModifyEvent()
+{
+    m_xModifyEventForwarder->modified( lang::EventObject( static_cast< uno::XWeak* >( this )));
 }
 
 // ================================================================================
@@ -216,5 +258,9 @@ uno::Sequence< ::rtl::OUString > PageBackground::getSupportedServiceNames_Static
 
 // implement XServiceInfo methods basing upon getSupportedServiceNames_Static
 APPHELPER_XSERVICEINFO_IMPL( PageBackground, lcl_aServiceName );
+
+using impl::PageBackground_Base;
+
+IMPLEMENT_FORWARD_XINTERFACE2( PageBackground, PageBackground_Base, ::property::OPropertySet )
 
 } //  namespace chart
