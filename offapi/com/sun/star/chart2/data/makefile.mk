d3b71c26a080ddda87cfd1368ac8f8728d26faf5
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 14:09:49 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/04/05 13:23:13 bm 1.1.2.6: change of chart data is notified via XModifyBroadcaster 2005/12/02 14:01:23 bm 1.1.2.5: interfaces and structures for range hightlighting 2005/10/11 12:07:58 bm 1.1.2.4: license header change 2005/09/08 11:33:22 sab 1.1.2.3: #i17416#; add generateLabel 2005/06/17 12:43:21 bm 1.1.2.2: +DataProvder, +XRangeXMLConversion 2005/05/09 09:24:35 bm 1.1.2.1: moved here from namespace css.chart2

---
+++ offapi/com/sun/star/chart2/data/makefile.mk
@@ -0,0 +1,67 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: vg $ $Date: 2007-05-22 15:09:49 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..$/..$/..
+
+TARGET=csschart2data
+PACKAGE=com$/sun$/star$/chart2$/data
+
+PRJNAME=chart2
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+#-------------------------------------------------------------------
+
+IDLFILES= \
+    DataSequenceRole.idl \
+    HighlightedRange.idl \
+    LabelOrigin.idl \
+    XDataProvider.idl \
+    XDataReceiver.idl \
+    XDataSequence.idl \
+    XDataSink.idl \
+    XDataSource.idl \
+    XLabeledDataSequence.idl \
+    XNumericalDataSequence.idl \
+    XRangeHighlighter.idl \
+    XRangeXMLConversion.idl \
+    XTextualDataSequence.idl
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+.INCLUDE :  $(PRJ)$/util$/target.pmk
