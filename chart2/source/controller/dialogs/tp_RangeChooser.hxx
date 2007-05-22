a36f80860959b26bbb6f9877b4891b52af5902df
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:48:01 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/27 13:45:32 bm 1.1.2.15: Disable controls and tabs when invalid ranges were entered 2007/02/09 12:31:18 bm 1.1.2.14: implement adaptions for different look in wizard and stand-alone source data dialog, according to spec FlexibleSourceRangeSelection 2006/10/24 11:55:16 bm 1.1.2.13: enable range chooser in Calc 2005/11/14 17:55:21 iha 1.1.2.12: don't update model again at end of execute as this was done already - undo is done outside of the dialog 2005/10/11 09:20:18 bm 1.1.2.11: license header change 2005/05/09 09:50:57 bm 1.1.2.10: moved parts of API to data namespace 2005/04/19 14:34:41 bm 1.1.2.9: fixing the data source/range chooser dialogs 2005/04/15 14:48:09 bm 1.1.2.8: data source and range choose dialog rework 2004/06/29 12:21:55 bm 1.1.2.7: shared data for range chooser and data source tab pages 2004/06/17 10:03:25 bm 1.1.2.6: data source dialog is a tab dialog now using the same tabpages as the wizard 2004/05/27 15:54:52 iha 1.1.2.5: #i20344# provide charttype template; added parameter bDoLiveUpdate 2004/05/26 13:18:41 bm 1.1.2.4: dialog behaviour improvements 2004/05/07 16:37:04 iha 1.1.2.3: #i20344# init controls from model and apply changes from controls to model 2004/05/05 20:57:02 iha 1.1.2.2: #i20344# start getting to work; added FixedLine; added title 2004/04/07 21:26:29 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/dialogs/tp_RangeChooser.hxx
@@ -0,0 +1,142 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_RangeChooser.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:48:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_TP_RANGECHOOSER_HXX
+#define _CHART2_TP_RANGECHOOSER_HXX
+
+#include "RangeSelectionListener.hxx"
+#include "RangeSelectionButton.hxx"
+#include "TabPageNotifiable.hxx"
+
+#include <com/sun/star/chart2/XChartDocument.hpp>
+#include <com/sun/star/chart2/data/XDataProvider.hpp>
+
+#ifndef _COM_SUN_STAR_CHART2_XCHARTTYPETEMPLATE_HPP_
+#include <com/sun/star/chart2/XChartTypeTemplate.hpp>
+#endif
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+
+#ifndef _SV_EDIT_HXX
+#include <vcl/edit.hxx>
+#endif
+#ifndef _SV_FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_SHEET_XRANGESELECTION_HPP_
+#include <com/sun/star/sheet/XRangeSelection.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+class ChartTypeTemplateProvider;
+class DialogModel;
+
+class RangeChooserTabPage : public svt::OWizardPage, public RangeSelectionListenerParent
+{
+public:
+
+    RangeChooserTabPage( Window* pParent
+                , DialogModel & rDialogModel
+                , ChartTypeTemplateProvider* pTemplateProvider
+                , Dialog * pParentDialog
+                , bool bHideDescription = false );
+virtual ~RangeChooserTabPage();
+
+    //RangeSelectionListenerParent
+    virtual void listeningFinished( const ::rtl::OUString & rNewRange );
+    virtual void disposingRangeSelection();
+
+protected: //methods
+
+    //OWizardPage
+    virtual void ActivatePage();
+
+    void initControlsFromModel();
+    void changeDialogModelAccordingToControls();
+    void changeModelAccordingToControls();
+    bool isValid();
+    void setDirty();
+    bool isDirty() const;
+
+    DECL_LINK( ChooseRangeHdl, void* );
+    DECL_LINK( EditRangeHdl, void* );
+    DECL_LINK( ToogleRowsColumnsHdl, void* );
+    DECL_LINK( ToogleLabelHdl, CheckBox* );
+
+protected: //member
+
+    FixedText       m_aFT_Caption;
+    FixedText       m_aFT_Range;
+    Edit            m_aED_Range;
+    RangeSelectionButton     m_aIB_Range;
+
+    RadioButton     m_aRB_Rows;
+    RadioButton     m_aRB_Columns;
+
+    CheckBox        m_aCB_FirstRowAsLabel;
+    CheckBox        m_aCB_FirstColumnAsLabel;
+
+    sal_Int32       m_nChangingControlCalls;
+    bool            m_bIsDirty;
+
+    ::com::sun::star::uno::Reference<
+         ::com::sun::star::chart2::XChartDocument >         m_xChartDocument;
+    ::com::sun::star::uno::Reference<
+         ::com::sun::star::chart2::data::XDataProvider >    m_xDataProvider;
+
+    rtl::OUString                                           m_aLastValidRangeString;
+    ::com::sun::star::uno::Reference<
+         ::com::sun::star::chart2::XChartTypeTemplate >     m_xCurrentChartTypeTemplate;
+    ChartTypeTemplateProvider*                              m_pTemplateProvider;
+
+    DialogModel &                                           m_rDialogModel;
+    Dialog *                                                m_pParentDialog;
+    TabPageNotifiable *                                     m_pTabPageNotifiable;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
