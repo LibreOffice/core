a2f977208f00156ffc330343ca9ebfebbe73a15f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:34:26 +0000

    INTEGRATION: CWS chart2mst3 (1.3.4); FILE MERGED 2007/02/14 12:05:05 bm 1.3.4.28: #i74133# clone: do late initialization after holding a reference to avoid ref-count problems in copy-CTOR 2007/02/13 16:45:59 bm 1.3.4.27: #i74133# clone: do late initialization after holding a reference to avoid ref-count problems in copy-CTOR 2007/02/12 17:19:09 bm 1.3.4.26: add as event listener to labeled data sequences to release them when they get disposed 2006/11/10 15:06:12 bm 1.3.4.25: #i67875# add undo for own data 2006/10/18 17:10:22 bm 1.3.4.24: RESYNC: (1.4-1.5); FILE MERGED 2006/04/04 15:57:24 bm 1.3.4.23: listen at data sequences for modification events. 2006/04/04 13:38:59 bm 1.3.4.22: listen at data sequences for modification events. 2005/12/21 21:29:17 iha 1.3.4.21: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/29 09:53:35 bm 1.3.4.20: #i58587# correct adding and removing modification listeners at XPropertySet properties 2005/11/07 17:12:43 bm 1.3.4.19: some missing notifications added 2005/11/03 11:59:16 bm 1.3.4.18: fire modify events on property change 2005/10/14 16:58:43 bm 1.3.4.17: model listener mechanism 2005/10/11 13:00:08 bm 1.3.4.16: #i27351# use own mutex in DataPoint to allow cloning 2005/10/07 11:53:21 bm 1.3.4.15: RESYNC: (1.3-1.4); FILE MERGED 2005/09/23 15:24:58 iha 1.3.4.14: #i55000# crash caused by dead reference to mutex 2005/08/03 16:36:18 bm 1.3.4.13: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/08/02 13:49:06 bm 1.3.4.12: data_type is not standard. use mapped_type instead 2005/07/14 13:00:23 iha 1.3.4.11: remove unused parameter 'bIncludeStyleProperties' from series and point properties 2005/07/14 12:45:42 iha 1.3.4.10: remove unused parameter 'bIncludeStyleProperties' from characterproperties 2005/05/09 09:51:14 bm 1.3.4.9: moved parts of API to data namespace 2005/04/01 16:25:39 bm 1.3.4.8: shared data for different chart models 2005/03/30 16:31:07 bm 1.3.4.7: make model cloneable (+first undo implementation) 2004/09/15 17:32:01 bm 1.3.4.6: API simplification 2004/05/26 16:40:55 bm 1.3.4.5: default font sizes adapted 2004/05/07 13:46:42 bm 1.3.4.4: only CTORS with component-context 2004/04/27 13:39:40 bm 1.3.4.3: +XInitialization for transfering Id over factory 2004/03/19 14:32:54 bm 1.3.4.2: XDataSource now contains XLabeledDataSources 2004/02/13 16:51:29 bm 1.3.4.1: join from changes on branch bm_post_chart01

--- chart2/source/model/main/DataSeries.cxx
+++ chart2/source/model/main/DataSeries.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataSeries.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:10:18 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:34:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,15 +37,16 @@
 #include "precompiled_chart2.hxx"
 #include "DataSeries.hxx"
 #include "DataSeriesProperties.hxx"
+#include "DataPointProperties.hxx"
 #include "CharacterProperties.hxx"
 #include "UserDefinedProperties.hxx"
 #include "DataPoint.hxx"
 #include "macros.hxx"
-#include "algohelper.hxx"
-
-#ifndef _COM_SUN_STAR_CHART2_XIDENTIFIABLE_HPP_
-#include <com/sun/star/chart2/XIdentifiable.hpp>
-#endif
+#include "DataSeriesHelper.hxx"
+#include "ContainerHelper.hxx"
+#include "CloneHelper.hxx"
+#include "ModifyListenerHelper.hxx"
+#include "EventListenerHelper.hxx"
 
 #include <algorithm>
 
@@ -72,22 +73,58 @@ const uno::Sequence< Property > & lcl_GetPropertySequence()
     {
         // get properties
         ::std::vector< ::com::sun::star::beans::Property > aProperties;
-        ::chart::DataSeriesProperties::AddPropertiesToVector(
-            aProperties, /* bIncludeStyleProperties = */ true );
-        ::chart::CharacterProperties::AddPropertiesToVector(
-            aProperties, /* bIncludeStyleProperties = */ true );
+        ::chart::DataSeriesProperties::AddPropertiesToVector( aProperties );
+        ::chart::CharacterProperties::AddPropertiesToVector( aProperties );
         ::chart::UserDefinedProperties::AddPropertiesToVector( aProperties );
 
         // and sort them for access via bsearch
         ::std::sort( aProperties.begin(), aProperties.end(),
-                     ::chart::helper::PropertyNameLess() );
+                     ::chart::PropertyNameLess() );
 
         // transfer result to static Sequence
-        aPropSeq = ::chart::helper::VectorToSequence( aProperties );
+        aPropSeq = ::chart::ContainerHelper::ContainerToSequence( aProperties );
     }
 
     return aPropSeq;
 }
+
+typedef ::std::map< sal_Int32, ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > >
+    lcl_tDataPointMap;
+
+void lcl_CloneAttributedDataPoints(
+    const lcl_tDataPointMap & rSource, lcl_tDataPointMap & rDestination,
+    const uno::Reference< uno::XInterface > & xSeries )
+{
+    for( lcl_tDataPointMap::const_iterator aIt( rSource.begin());
+         aIt != rSource.end(); ++aIt )
+    {
+        Reference< beans::XPropertySet > xPoint( (*aIt).second );
+        if( xPoint.is())
+        {
+            Reference< util::XCloneable > xCloneable( xPoint, uno::UNO_QUERY );
+            if( xCloneable.is())
+            {
+                xPoint.set( xCloneable->createClone(), uno::UNO_QUERY );
+                if( xPoint.is())
+                {
+                    Reference< container::XChild > xChild( xPoint, uno::UNO_QUERY );
+                    if( xChild.is())
+                        xChild->setParent( xSeries );
+
+                    rDestination.insert( lcl_tDataPointMap::value_type( (*aIt).first, xPoint ));
+                }
+            }
+        }
+    }
+}
+
+bool lcl_isInternalData( const Reference< chart2::data::XLabeledDataSequence > & xLSeq )
+{
+    Reference< lang::XServiceInfo > xServiceInfo( xLSeq, uno::UNO_QUERY );
+    return ( xServiceInfo.is() && xServiceInfo->getImplementationName().equalsAsciiL(
+                 RTL_CONSTASCII_STRINGPARAM("com.sun.star.comp.chart2.LabeledDataSequence")));
+}
+
 } // anonymous namespace
 
 // ----------------------------------------
@@ -95,13 +132,101 @@ const uno::Sequence< Property > & lcl_GetPropertySequence()
 namespace chart
 {
 
-DataSeries::DataSeries( const ::rtl::OUString & rIdentifier ) :
+DataSeries::DataSeries( const uno::Reference< uno::XComponentContext > & xContext ) :
         ::property::OPropertySet( m_aMutex ),
-    m_aIdentifier( rIdentifier )
-{}
+        m_xContext( xContext ),
+        m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex ))
+{
+}
+
+DataSeries::DataSeries( const DataSeries & rOther ) :
+        ::property::OPropertySet( rOther, m_aMutex ),
+    m_xContext( rOther.m_xContext ),
+    m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex ))
+{
+    if( ! rOther.m_aDataSequences.empty())
+    {
+        if( lcl_isInternalData( rOther.m_aDataSequences.front()))
+            CloneHelper::CloneRefVector< tDataSequenceContainer::value_type >(
+                rOther.m_aDataSequences, m_aDataSequences );
+        else
+            m_aDataSequences = rOther.m_aDataSequences;
+        ModifyListenerHelper::addListenerToAllElements( m_aDataSequences, m_xModifyEventForwarder );
+    }
+
+    CloneHelper::CloneRefVector< Reference< chart2::XRegressionCurve > >( rOther.m_aRegressionCurves, m_aRegressionCurves );
+    ModifyListenerHelper::addListenerToAllElements( m_aRegressionCurves, m_xModifyEventForwarder );
+
+    // add as listener to XPropertySet properties
+    Reference< beans::XPropertySet > xPropertySet;
+    uno::Any aValue;
+
+    getFastPropertyValue( aValue, DataPointProperties::PROP_DATAPOINT_ERROR_BAR_X );
+    if( ( aValue >>= xPropertySet )
+        && xPropertySet.is())
+        ModifyListenerHelper::addListener( xPropertySet, m_xModifyEventForwarder );
+
+    getFastPropertyValue( aValue, DataPointProperties::PROP_DATAPOINT_ERROR_BAR_Y );
+    if( ( aValue >>= xPropertySet )
+        && xPropertySet.is())
+        ModifyListenerHelper::addListener( xPropertySet, m_xModifyEventForwarder );
+}
+
+// late initialization to call after copy-constructing
+void DataSeries::Init( const DataSeries & rOther )
+{
+    if( ! rOther.m_aDataSequences.empty())
+        EventListenerHelper::addListenerToAllElements( m_aDataSequences, this );
+
+    if( ! rOther.m_aAttributedDataPoints.empty())
+    {
+        lcl_CloneAttributedDataPoints(
+            rOther.m_aAttributedDataPoints, m_aAttributedDataPoints,
+            Reference< uno::XInterface >( static_cast< ::cppu::OWeakObject * >( this )));
+        ModifyListenerHelper::addListenerToAllMapElements( m_aAttributedDataPoints, m_xModifyEventForwarder );
+    }
+}
 
 DataSeries::~DataSeries()
-{}
+{
+    try
+    {
+        ModifyListenerHelper::removeListenerFromAllMapElements( m_aAttributedDataPoints, m_xModifyEventForwarder );
+        ModifyListenerHelper::removeListenerFromAllElements( m_aRegressionCurves, m_xModifyEventForwarder );
+        ModifyListenerHelper::removeListenerFromAllElements( m_aDataSequences, m_xModifyEventForwarder );
+
+        // remove listener from XPropertySet properties
+        Reference< beans::XPropertySet > xPropertySet;
+        uno::Any aValue;
+
+        getFastPropertyValue( aValue, DataPointProperties::PROP_DATAPOINT_ERROR_BAR_X );
+        if( ( aValue >>= xPropertySet )
+            && xPropertySet.is())
+            ModifyListenerHelper::removeListener( xPropertySet, m_xModifyEventForwarder );
+
+        getFastPropertyValue( aValue, DataPointProperties::PROP_DATAPOINT_ERROR_BAR_Y );
+        if( ( aValue >>= xPropertySet )
+            && xPropertySet.is())
+            ModifyListenerHelper::removeListener( xPropertySet, m_xModifyEventForwarder );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL DataSeries::createClone()
+    throw (uno::RuntimeException)
+{
+    DataSeries * pNewSeries( new DataSeries( *this ));
+    // hold a reference to the clone
+    uno::Reference< util::XCloneable > xResult( pNewSeries );
+    // do initialization that uses uno references to the clone
+    pNewSeries->Init( *this );
+
+    return xResult;
+}
 
 Sequence< OUString > DataSeries::getSupportedServiceNames_Static()
 {
@@ -112,38 +237,34 @@ Sequence< OUString > DataSeries::getSupportedServiceNames_Static()
     return aServices;
 }
 
-
 // ____ OPropertySet ____
 uno::Any DataSeries::GetDefaultValue( sal_Int32 nHandle ) const
     throw(beans::UnknownPropertyException)
 {
-    static helper::tPropertyValueMap aStaticDefaults;
+    static tPropertyValueMap aStaticDefaults;
 
     // /--
     ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
     if( 0 == aStaticDefaults.size() )
     {
         // initialize defaults
-        DataSeriesProperties::AddDefaultsToMap(
-            aStaticDefaults,
-            /* bIncludeStyleProperties = */ true );
-        CharacterProperties::AddDefaultsToMap(
-            aStaticDefaults,
-            /* bIncludeStyleProperties = */ true );
+        DataSeriesProperties::AddDefaultsToMap( aStaticDefaults );
+        CharacterProperties::AddDefaultsToMap( aStaticDefaults );
+
+        float fDefaultCharHeight = 6.0;
+        aStaticDefaults[ ::chart::CharacterProperties::PROP_CHAR_CHAR_HEIGHT ] =
+            uno::makeAny( fDefaultCharHeight );
+        aStaticDefaults[ ::chart::CharacterProperties::PROP_CHAR_ASIAN_CHAR_HEIGHT ] =
+            uno::makeAny( fDefaultCharHeight );
+        aStaticDefaults[ ::chart::CharacterProperties::PROP_CHAR_COMPLEX_CHAR_HEIGHT ] =
+            uno::makeAny( fDefaultCharHeight );
     }
 
-    helper::tPropertyValueMap::const_iterator aFound(
+    tPropertyValueMap::const_iterator aFound(
         aStaticDefaults.find( nHandle ));
 
     if( aFound == aStaticDefaults.end())
-    {
-        if( nHandle == DataSeriesProperties::PROP_DATASERIES_IDENTIFIER )
-        {
-            return uno::makeAny( m_aIdentifier );
-        }
-        else
-            throw beans::UnknownPropertyException();
-    }
+        throw beans::UnknownPropertyException();
 
     return (*aFound).second;
     // \--
@@ -204,6 +325,35 @@ void SAL_CALL DataSeries::getFastPropertyValue
         OPropertySet::getFastPropertyValue( rValue, nHandle );
 }
 
+void SAL_CALL DataSeries::setFastPropertyValue_NoBroadcast(
+    sal_Int32 nHandle, const uno::Any& rValue )
+    throw (uno::Exception)
+{
+    if(    nHandle == DataPointProperties::PROP_DATAPOINT_ERROR_BAR_Y
+        || nHandle == DataPointProperties::PROP_DATAPOINT_ERROR_BAR_X )
+    {
+        uno::Any aOldValue;
+        Reference< util::XModifyBroadcaster > xBroadcaster;
+        this->getFastPropertyValue( aOldValue, nHandle );
+        if( aOldValue.hasValue() &&
+            (aOldValue >>= xBroadcaster) &&
+            xBroadcaster.is())
+        {
+            ModifyListenerHelper::removeListener( xBroadcaster, m_xModifyEventForwarder );
+        }
+
+        OSL_ASSERT( rValue.getValueType().getTypeClass() == uno::TypeClass_INTERFACE );
+        if( rValue.hasValue() &&
+            (rValue >>= xBroadcaster) &&
+            xBroadcaster.is())
+        {
+            ModifyListenerHelper::addListener( xBroadcaster, m_xModifyEventForwarder );
+        }
+    }
+
+    ::property::OPropertySet::setFastPropertyValue_NoBroadcast( nHandle, rValue );
+}
+
 Reference< beans::XPropertySet >
     SAL_CALL DataSeries::getDataPointByIndex( sal_Int32 nIndex )
     throw (lang::IndexOutOfBoundsException,
@@ -214,23 +364,31 @@ Reference< beans::XPropertySet >
     // /--
     MutexGuard aGuard( GetMutex() );
 
-    if( m_aDataSequences.begin() != m_aDataSequences.end())
+    if( ! m_aDataSequences.empty() )
     {
-        if( 0 <= nIndex && nIndex <
-            (*(m_aDataSequences.begin()))->getData().getLength() )
+        ::std::vector< Reference< chart2::data::XLabeledDataSequence > > aValuesSeries(
+            DataSeriesHelper::getAllDataSequencesByRole(
+                ContainerHelper::ContainerToSequence( m_aDataSequences ),
+                C2U("values"), true ));
+        if( ! aValuesSeries.empty())
         {
-            tDataPointAttributeContainer::iterator aIt(
-                m_aAttributedDataPoints.find( nIndex ));
+            Reference< chart2::data::XDataSequence > xSeq( aValuesSeries.front()->getValues());
+            if( 0 <= nIndex && nIndex < xSeq->getData().getLength())
+            {
+                tDataPointAttributeContainer::iterator aIt(
+                    m_aAttributedDataPoints.find( nIndex ));
 
-            if( aIt == m_aAttributedDataPoints.end())
-            {
-                // create a new XPropertySet for this data point
-                aResult.set( new DataPoint( GetMutex(), this ));
-                m_aAttributedDataPoints[ nIndex ] = aResult;
-            }
-            else
-            {
-                aResult = (*aIt).second;
+                if( aIt == m_aAttributedDataPoints.end())
+                {
+                    // create a new XPropertySet for this data point
+                    aResult.set( new DataPoint( this ));
+                    ModifyListenerHelper::addListener( aResult, m_xModifyEventForwarder );
+                    m_aAttributedDataPoints[ nIndex ] = aResult;
+                }
+                else
+                {
+                    aResult = (*aIt).second;
+                }
             }
         }
         else
@@ -248,73 +406,141 @@ Reference< beans::XPropertySet >
 }
 
 // ____ XDataSink ____
-void SAL_CALL DataSeries::setData( const uno::Sequence< Reference< chart2::XDataSequence > >& aData )
+void SAL_CALL DataSeries::setData( const uno::Sequence< Reference< chart2::data::XLabeledDataSequence > >& aData )
     throw (uno::RuntimeException)
 {
     // /--
     MutexGuard aGuard( GetMutex() );
-    m_aDataSequences = helper::SequenceToVector( aData );
+    ModifyListenerHelper::removeListenerFromAllElements( m_aDataSequences, m_xModifyEventForwarder );
+    EventListenerHelper::removeListenerFromAllElements( m_aDataSequences, this );
+    m_aDataSequences = ContainerHelper::SequenceToVector( aData );
+    EventListenerHelper::addListenerToAllElements( m_aDataSequences, this );
+    ModifyListenerHelper::addListenerToAllElements( m_aDataSequences, m_xModifyEventForwarder );
+    fireModifyEvent();
     // \--
 }
 
 // ____ XDataSource ____
-Sequence< Reference< chart2::XDataSequence > > SAL_CALL DataSeries::getDataSequences()
+Sequence< Reference< chart2::data::XLabeledDataSequence > > SAL_CALL DataSeries::getDataSequences()
     throw (uno::RuntimeException)
 {
     // /--
     MutexGuard aGuard( GetMutex() );
-    return helper::VectorToSequence( m_aDataSequences );
+    return ContainerHelper::ContainerToSequence( m_aDataSequences );
     // \--
 }
 
+
 // ____ XRegressionCurveContainer ____
 void SAL_CALL DataSeries::addRegressionCurve(
-    const uno::Reference< chart2::XRegressionCurve >& aRegressionCurve )
+    const uno::Reference< chart2::XRegressionCurve >& xRegressionCurve )
     throw (lang::IllegalArgumentException,
            uno::RuntimeException)
 {
-    uno::Reference< chart2::XIdentifiable > xIdent( aRegressionCurve, uno::UNO_QUERY );
-    if( ! xIdent.is() ||
-        m_aRegressionCurves.find( xIdent->getIdentifier() ) != m_aRegressionCurves.end() )
+    if( ::std::find( m_aRegressionCurves.begin(), m_aRegressionCurves.end(), xRegressionCurve )
+        != m_aRegressionCurves.end())
         throw lang::IllegalArgumentException();
 
-    m_aRegressionCurves.insert(
-        tRegressionCurveContainerType::value_type( xIdent->getIdentifier(), aRegressionCurve ));
+    m_aRegressionCurves.push_back( xRegressionCurve );
+    ModifyListenerHelper::addListener( xRegressionCurve, m_xModifyEventForwarder );
+    fireModifyEvent();
 }
 
 void SAL_CALL DataSeries::removeRegressionCurve(
-    const uno::Reference< chart2::XRegressionCurve >& aRegressionCurve )
+    const uno::Reference< chart2::XRegressionCurve >& xRegressionCurve )
     throw (container::NoSuchElementException,
            uno::RuntimeException)
 {
-    if( ! aRegressionCurve.is())
+    if( ! xRegressionCurve.is())
         throw container::NoSuchElementException();
 
-    uno::Reference< chart2::XIdentifiable > xIdent( aRegressionCurve, uno::UNO_QUERY );
-    if( xIdent.is())
-    {
-        tRegressionCurveContainerType::iterator aIt( m_aRegressionCurves.find( xIdent->getIdentifier() ));
+    tRegressionCurveContainerType::iterator aIt(
+            ::std::find( m_aRegressionCurves.begin(), m_aRegressionCurves.end(), xRegressionCurve ) );
 
-        if( aIt == m_aRegressionCurves.end())
-            throw container::NoSuchElementException();
+    if( aIt == m_aRegressionCurves.end())
+        throw container::NoSuchElementException(
+            C2U( "The given regression curve is no element of this series" ),
+            static_cast< uno::XWeak * >( this ));
 
-        m_aRegressionCurves.erase( aIt );
-    }
-    else
-        throw container::NoSuchElementException();
+    ModifyListenerHelper::removeListener( xRegressionCurve, m_xModifyEventForwarder );
+    m_aRegressionCurves.erase( aIt );
+    fireModifyEvent();
 }
 
 uno::Sequence< uno::Reference< chart2::XRegressionCurve > > SAL_CALL DataSeries::getRegressionCurves()
     throw (uno::RuntimeException)
 {
-    uno::Sequence< uno::Reference< chart2::XRegressionCurve > > aResult( m_aRegressionCurves.size());
-
-    ::std::transform( m_aRegressionCurves.begin(), m_aRegressionCurves.end(),
-                      aResult.getArray(),
-                      ::std::select2nd< tRegressionCurveContainerType::value_type >() );
-
-    return aResult;
+    return ContainerHelper::ContainerToSequence( m_aRegressionCurves );
 }
+
+void SAL_CALL DataSeries::setRegressionCurves(
+    const Sequence< Reference< chart2::XRegressionCurve > >& aRegressionCurves )
+    throw (uno::RuntimeException)
+{
+    ModifyListenerHelper::removeListenerFromAllElements( m_aRegressionCurves, m_xModifyEventForwarder );
+    m_aRegressionCurves.clear();
+    for( sal_Int32 i=0; i<aRegressionCurves.getLength(); ++i )
+        addRegressionCurve( aRegressionCurves[i] );
+}
+
+// ____ XModifyBroadcaster ____
+void SAL_CALL DataSeries::addModifyListener( const Reference< util::XModifyListener >& aListener )
+    throw (uno::RuntimeException)
+{
+    try
+    {
+        Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->addModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+void SAL_CALL DataSeries::removeModifyListener( const Reference< util::XModifyListener >& aListener )
+    throw (uno::RuntimeException)
+{
+    try
+    {
+        Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->removeModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+// ____ XModifyListener ____
+void SAL_CALL DataSeries::modified( const lang::EventObject& aEvent )
+    throw (uno::RuntimeException)
+{
+    m_xModifyEventForwarder->modified( aEvent );
+}
+
+// ____ XEventListener (base of XModifyListener) ____
+void SAL_CALL DataSeries::disposing( const lang::EventObject& rEventObject )
+    throw (uno::RuntimeException)
+{
+    // forget disposed data sequences
+    tDataSequenceContainer::iterator aIt(
+        ::std::find( m_aDataSequences.begin(), m_aDataSequences.end(), rEventObject.Source ));
+    if( aIt != m_aDataSequences.end())
+        m_aDataSequences.erase( aIt );
+}
+
+// ____ OPropertySet ____
+void DataSeries::firePropertyChangeEvent()
+{
+    fireModifyEvent();
+}
+
+void DataSeries::fireModifyEvent()
+{
+    m_xModifyEventForwarder->modified( lang::EventObject( static_cast< uno::XWeak* >( this )));
+}
+
 
 // ================================================================================
 
