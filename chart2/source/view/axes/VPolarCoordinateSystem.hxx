558fc60d275a199f3ed966d210df8c83c4862af0
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:13:16 +0000

    INTEGRATION: CWS chart2mst3 (1.2.4); FILE MERGED 2006/11/09 15:32:56 iha 1.2.4.7: #i16280# better performance for charts with many data (bar,column,line,area,xy,net charts) 2006/08/25 17:16:48 iha 1.2.4.6: text- and diagram-size dependent auto main step width 2006/08/23 17:07:27 iha 1.2.4.5: enable axis to estimate the maximum automatic MainIncrementCount, reduce count to 2 for net charts 2006/08/10 16:02:24 iha 1.2.4.4: Axis Label Layout - Font&Diagram Size 2005/10/13 17:39:12 iha 1.2.4.3: renamed BoundedCoordinateSystem to CoordinateSystem 2005/10/07 12:19:24 bm 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2005/06/07 16:03:28 iha 1.2.4.1: use numberformatter from model

--- chart2/source/view/axes/VPolarCoordinateSystem.hxx
+++ chart2/source/view/axes/VPolarCoordinateSystem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VPolarCoordinateSystem.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:40:50 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:13:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,11 +49,22 @@ class VPolarCoordinateSystem : public VCoordinateSystem
 {
 public:
     VPolarCoordinateSystem( const ::com::sun::star::uno::Reference<
-        ::com::sun::star::chart2::XBoundedCoordinateSystem >& xCooSys );
+        ::com::sun::star::chart2::XCoordinateSystem >& xCooSys );
     virtual ~VPolarCoordinateSystem();
 
+    //better performance for big data
+    virtual ::com::sun::star::uno::Sequence< sal_Int32 > getCoordinateSystemResolution( const ::com::sun::star::awt::Size& rPageSize
+                                    , const ::com::sun::star::awt::Size& rPageResolution );
+
+    virtual void createVAxisList(
+            const ::com::sun::star::uno::Reference< ::com::sun::star::util::XNumberFormatsSupplier > & xNumberFormatsSupplier
+            , const ::com::sun::star::awt::Size& rFontReferenceSize
+            , const ::com::sun::star::awt::Rectangle& rMaximumSpaceForLabels );
+
+    virtual void initVAxisInList();
+    virtual void updateScalesAndIncrementsOnAxes();
+
     virtual void createGridShapes();
-    virtual void createAxesShapes( const ::com::sun::star::awt::Size& rReferenceSize, NumberFormatterWrapper* pNumberFormatterWrapper );
 
 private:
     VPolarCoordinateSystem();
