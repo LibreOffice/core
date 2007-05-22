e624908e3d02ad2d289891aa92b0d536655ab752
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 15:07:11 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/04/19 09:51:10 bm 1.1.2.16: #i75530# import explicit chart types at data series in correct order 2006/12/06 18:50:46 bm 1.1.2.15: #i64497# allow import and export to deal with flexible source ranges also with own data. This is needed for the clipboard between Calc and Impress 2006/11/14 19:25:38 iha 1.1.2.14: #i71330# xy 'symbols only' get lines when loading older versions 2006/10/20 09:50:40 bm 1.1.2.13: warnings removed 2006/08/02 12:55:15 iha 1.1.2.12: removed unused DomainOffset 2006/08/02 12:26:41 iha 1.1.2.11: read wrongly created files properly while having addin with basediagram xy 2006/06/07 14:59:13 bm 1.1.2.10: #124497# set styles of series before the styles of statistics properties, because the existence of statistics objects is determined by proeprties of the series 2006/03/09 13:51:23 iha 1.1.2.9: wrong include path 2006/02/03 19:52:44 iha 1.1.2.8: remove series index dependency 2005/12/21 21:34:26 iha 1.1.2.7: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/01 12:52:16 iha 1.1.2.6: proper use of default domain (first x sequence) for load and save of scatter charts 2005/10/11 11:52:30 bm 1.1.2.5: license header change 2005/07/15 21:00:17 iha 1.1.2.4: change from series index to series identifier 2005/06/24 09:07:30 bm 1.1.2.3: stock chart 2005/06/20 12:35:31 bm 1.1.2.2: scatter chart 2005/06/17 17:03:18 bm 1.1.2.1: context for series using new API

---
+++ xmloff/source/chart/SchXMLSeries2Context.hxx
@@ -0,0 +1,149 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: SchXMLSeries2Context.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 16:07:11 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef XMLOFF_SCHXMLSERIES2CONTEXT_HXX
+#define XMLOFF_SCHXMLSERIES2CONTEXT_HXX
+
+#include "transporttypes.hxx"
+#include "SchXMLChartContext.hxx"
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include "xmlictxt.hxx"
+#endif
+#ifndef _XMLOFF_SCH_XMLIMPORTHELPER_HXX_
+#include "SchXMLImportHelper.hxx"
+#endif
+// header for class SvXMLStyleContext
+#ifndef _XMLOFF_XMLSTYLE_HXX
+#include "xmlstyle.hxx"
+#endif
+
+#include <vector>
+#include <list>
+
+namespace com { namespace sun { namespace star {
+    namespace chart2 {
+        class XChartDocument;
+        class XDataSeries;
+    }
+}}}
+
+// class for child contexts: series, data point and statistics objects
+class SchXMLSeries2Context : public SvXMLImportContext
+{
+private:
+    SchXMLImportHelper& mrImportHelper;
+    ::com::sun::star::uno::Reference<
+            ::com::sun::star::chart2::XChartDocument > mxNewDoc;
+    ::std::vector< SchXMLAxis >& mrAxes;
+    ::std::list< DataRowPointStyle >& mrStyleList;
+    ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XDataSeries > m_xSeries;
+    sal_Int32 mnSeriesIndex;
+    sal_Int32 mnDataPointIndex;
+    sal_Int32& mrMaxSeriesLength;
+    sal_Int32& mrNumOfLines;
+    sal_Bool& mrStockHasVolume;
+    ::rtl::OUString& mrFirstFirstDomainAddress;
+    sal_Int32& mrFirstFirstDomainIndex;
+    sal_Bool& mrAllRangeAddressesAvailable;
+    SchXMLAxis* mpAttachedAxis;
+    sal_Int32 mnAttachedAxis;
+    ::rtl::OUString msAutoStyleName;
+    ::std::vector< ::rtl::OUString > maDomainAddresses;
+    ::rtl::OUString maGlobalChartTypeName;
+    ::rtl::OUString maSeriesChartTypeName;
+    ::rtl::OUString m_aSeriesRange;
+    ::rtl::OUString m_aSeriesLabelRange;
+    bool            m_bHasDomainContext;
+    tSchXMLLSequencesPerIndex & mrLSequencesPerIndex;
+    sal_Int32& mrCurrentDataIndex;
+    tSchXMLLSequencesPerIndex maPostponedSequences;
+    bool& mrGlobalChartTypeUsedBySeries;
+
+public:
+    SchXMLSeries2Context( SchXMLImportHelper& rImpHelper,
+                          SvXMLImport& rImport, const rtl::OUString& rLocalName,
+                          const ::com::sun::star::uno::Reference<
+                              ::com::sun::star::chart2::XChartDocument > & xNewDoc,
+                          std::vector< SchXMLAxis >& rAxes,
+                          ::std::list< DataRowPointStyle >& rStyleList,
+                          sal_Int32 nSeriesIndex,
+                          sal_Int32& rMaxSeriesLength,
+                          sal_Int32& rNumOfLines,
+                          sal_Bool&  rStockHasVolume,
+                          ::rtl::OUString& rFirstFirstDomainAddress,
+                          sal_Int32& rFirstFirstDomainIndex,
+                          sal_Bool&  rAllRangeAddressesAvailable,
+                          const ::rtl::OUString & aGlobalChartTypeName,
+                          tSchXMLLSequencesPerIndex & rLSequencesPerIndex,
+                          sal_Int32& rCurrentDataIndex,
+                          bool& rGlobalChartTypeUsedBySeries );
+    virtual ~SchXMLSeries2Context();
+
+    virtual void StartElement( const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
+    virtual SvXMLImportContext* CreateChildContext(
+        USHORT nPrefix,
+        const rtl::OUString& rLocalName,
+        const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
+    virtual void EndElement();
+
+    static void initSeriesPropertySets( SeriesDefaultsAndStyles& rSeriesDefaultsAndStyles
+        , const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel );
+
+    static void setDefaultsToSeries( SeriesDefaultsAndStyles& rSeriesDefaultsAndStyles );
+
+    static void setStylesToSeries( SeriesDefaultsAndStyles& rSeriesDefaultsAndStyles
+        , const SvXMLStylesContext* pStylesCtxt
+        , const SvXMLStyleContext*& rpStyle
+        , ::rtl::OUString& rCurrStyleName
+        , SchXMLImportHelper& rImportHelper
+        , bool bIsStockChart );
+
+    static void setStylesToStatisticsObjects( SeriesDefaultsAndStyles& rSeriesDefaultsAndStyles
+        , const SvXMLStylesContext* pStylesCtxt
+        , const SvXMLStyleContext*& rpStyle
+        , ::rtl::OUString& rCurrStyleName );
+
+    static void setStylesToDataPoints( SeriesDefaultsAndStyles& rSeriesDefaultsAndStyles
+        , const SvXMLStylesContext* pStylesCtxt
+        , const SvXMLStyleContext*& rpStyle
+        , ::rtl::OUString& rCurrStyleName
+        , SchXMLImportHelper& rImportHelper
+        , bool bIsStockChart, bool bIsDonutChart );
+
+    static void switchSeriesLinesOff( ::std::list< DataRowPointStyle >& rSeriesStyleList );
+};
+
+// XMLOFF_SCHXMLSERIES2CONTEXT_HXX
+#endif
