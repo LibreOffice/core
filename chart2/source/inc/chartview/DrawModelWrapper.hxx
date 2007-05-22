8712fd9002eab844d790846888682c5547f3e2c2
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:26:23 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/10/20 21:28:24 iha 1.1.2.11: implement selection of additional shapes 2006/03/12 01:26:21 iha 1.1.2.10: changed communication between model, view and controller; implemented old AddIn Interface; changed owneship and lifetime of view 2006/02/20 14:33:21 iha 1.1.2.9: move SchItemPool from controller to view lib -> ChartItemPool 2005/10/11 09:20:38 bm 1.1.2.8: license header change 2005/08/30 14:42:46 bm 1.1.2.7: +attachParentReferenceDevice, +getReferenceDevice 2005/07/13 15:21:55 iha 1.1.2.6: update draw model tables before use in view and dialogs 2005/07/08 12:52:14 iha 1.1.2.5: provide and keep view as service only 2005/07/05 15:32:11 iha 1.1.2.4: redefine diagram size 2005/06/10 18:18:23 iha 1.1.2.3: removed comment 2005/06/01 16:50:23 iha 1.1.2.2: export metafile 2005/05/31 19:10:59 iha 1.1.2.1: create view without controller

---
+++ chart2/source/inc/chartview/DrawModelWrapper.hxx
@@ -0,0 +1,133 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: DrawModelWrapper.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:26:23 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _CHART2_DRAWMODELWRAPPER_HXX
+#define _CHART2_DRAWMODELWRAPPER_HXX
+
+//----
+#ifndef _SVDMODEL_HXX
+#include <svx/svdmodel.hxx>
+#endif
+// header for class SdrObject
+#ifndef _SVDOBJ_HXX
+#include <svx/svdobj.hxx>
+#endif
+
+//----
+#ifndef _COM_SUN_STAR_FRAME_XMODEL_HPP_
+#include <com/sun/star/frame/XModel.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_XDRAWPAGE_HPP_
+#include <com/sun/star/drawing/XDrawPage.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+class DrawModelWrapper : private SdrModel
+{
+private:
+    ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiComponentFactory > m_xMCF;
+    SfxItemPool* m_pChartItemPool;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage > m_xMainDrawPage;
+    ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage > m_xHiddenDrawPage;
+
+    //no default constructor
+    DrawModelWrapper();
+
+public:
+    DrawModelWrapper(::com::sun::star::uno::Reference<
+               ::com::sun::star::uno::XComponentContext > const & xContext );
+    virtual ~DrawModelWrapper();
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > getShapeFactory();
+
+    // the main page will contain the normal view objects
+    ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage > getMainDrawPage();
+    void clearMainDrawPage();
+
+    // the extra page is not visible, but contains some extras like the symbols for data points
+    ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XDrawPage > getHiddenDrawPage();
+
+    static ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >
+         getChartRootShape( const ::com::sun::star::uno::Reference<
+            ::com::sun::star::drawing::XDrawPage>& xPage );
+
+
+    void lockControllers();
+    void unlockControllers();
+
+    /// tries to get an OutputDevice from the XParent of the model to use as reference device
+    void attachParentReferenceDevice(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel > & xChartModel );
+
+    OutputDevice* getReferenceDevice() const;
+
+    SfxItemPool&            GetItemPool();
+    const SfxItemPool&      GetItemPool() const;
+
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >
+        createUnoModel();
+    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >
+        getUnoModel();
+    SdrModel& getSdrModel();
+
+    void updateTablesFromChartModel( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel );
+
+    XColorTable*    GetColorTable() const;
+    XDashList*      GetDashList() const;
+    XLineEndList*   GetLineEndList() const;
+    XGradientList*  GetGradientList() const;
+    XHatchList*     GetHatchList() const;
+    XBitmapList*    GetBitmapList() const;
+
+    SdrObject* getNamedSdrObject( const rtl::OUString& rName );
+    static SdrObject* getNamedSdrObject( const String& rName, SdrObjList* pObjList );
+
+    static bool removeShape( const ::com::sun::star::uno::Reference<
+                    ::com::sun::star::drawing::XShape >& xShape );
+};
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
