6c916a7135d1741c9d973c84ac51805d493c9e57
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:21:52 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/11/22 17:31:20 iha 1.1.2.3: resync 2005/10/11 09:21:01 bm 1.1.2.2: license header change 2004/05/08 17:51:08 iha 1.1.2.1: polartransformation fixes and pie offset implementation

---
+++ chart2/source/view/inc/VPolarTransformation.hxx
@@ -0,0 +1,81 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: VPolarTransformation.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:21:52 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _CHART_VPOLARTRANSFORMATION_HXX
+#define _CHART_VPOLARTRANSFORMATION_HXX
+
+#include "PlottingPositionHelper.hxx"
+
+#ifndef _CPPUHELPER_IMPLBASE1_HXX_
+#include <cppuhelper/implbase1.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_CHART2_XTRANSFORMATION_HPP_
+#include <com/sun/star/chart2/XTransformation.hpp>
+#endif
+
+namespace chart
+{
+
+class VPolarTransformation : public ::cppu::WeakImplHelper1<
+    ::com::sun::star::chart2::XTransformation
+    >
+{
+public:
+    VPolarTransformation( const PolarPlottingPositionHelper& rPositionHelper );
+    virtual ~VPolarTransformation();
+
+    // ____ XTransformation ____
+    /// @see ::com::sun::star::chart2::XTransformation
+    virtual ::com::sun::star::uno::Sequence< double > SAL_CALL transform(
+        const ::com::sun::star::uno::Sequence< double >& rSourceValues )
+        throw (::com::sun::star::lang::IllegalArgumentException,
+               ::com::sun::star::uno::RuntimeException);
+    /// @see ::com::sun::star::chart2::XTransformation
+    virtual sal_Int32 SAL_CALL getSourceDimension()
+        throw (::com::sun::star::uno::RuntimeException);
+    /// @see ::com::sun::star::chart2::XTransformation
+    virtual sal_Int32 SAL_CALL getTargetDimension()
+        throw (::com::sun::star::uno::RuntimeException);
+
+private:
+    PolarPlottingPositionHelper     m_aPositionHelper;
+    ::basegfx::B3DHomMatrix         m_aUnitCartesianToScene;
+};
+
+}  // namespace chart
+
+// _CHART_VPOLARTRANSFORMATION_HXX
+#endif
+
