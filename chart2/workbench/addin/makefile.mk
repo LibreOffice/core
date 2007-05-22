ff599eb488ed8cb508f0a46af51514d24acc6677
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:29:37 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/03/12 01:37:57 iha 1.1.2.1: old AddIn example

---
+++ chart2/workbench/addin/makefile.mk
@@ -0,0 +1,94 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: vg $ $Date: 2007-05-22 19:29:37 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+PRJNAME=chartaddin
+
+TARGET=chartsampleaddin
+
+ENABLE_EXCEPTIONS=TRUE
+USE_DEFFILE=TRUE
+LIBTARGET=NO
+
+# --- Settings ----------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Types -------------------------------------
+
+UNOUCROUT=$(OUT)$/inc$/$(PRJNAME)$/$(TARGET)
+INCPRE+=$(UNOUCROUT)
+
+# --- Types -------------------------------------
+
+# comprehensive type info, so rdb needn't be installed
+# CPPUMAKERFLAGS*=-C
+
+# UNOTYPES=\
+# 	com.sun.star.lang.XInitialization \
+# 	com.sun.star.lang.XServiceName \
+# 	com.sun.star.lang.XServiceInfo \
+# 	com.sun.star.util.XRefreshable \
+# 	com.sun.star.lang.XLocalizable  \
+# 	com.sun.star.chart.XDiagram \
+# 	com.sun.star.chart.XChartDocument \
+# 	com.sun.star.chart.XAxisXSupplier \
+# 	com.sun.star.chart.XAxisYSupplier \
+# 	com.sun.star.chart.XStatisticDisplay \
+# 	com.sun.star.lang.XMultiServiceFactory
+
+# --- Files -------------------------------------
+
+SLOFILES=\
+    $(SLO)$/sampleaddin.obj
+
+# --- Library -----------------------------------
+
+SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1OBJS=$(SLOFILES)
+SHL1STDLIBS= \
+        $(CPPUHELPERLIB)	\
+        $(CPPULIB)			\
+        $(VOSLIB)			\
+        $(SALLIB) 
+
+SHL1DEPN=makefile.mk
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+DEF1EXPORTFILE=exports.dxp
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
