4226dd4192d4a7a8bda481fe29630c8492b0ac41
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:12:40 +0000

    INTEGRATION: CWS chart2mst3 (1.5.4); FILE MERGED 2006/10/18 17:21:12 bm 1.5.4.20: RESYNC: (1.6-1.7); FILE MERGED 2006/08/22 19:46:37 iha 1.5.4.19: some axes cleanup 2006/08/22 15:15:37 iha 1.5.4.18: separate implementation of polar angle axis and polar radius axis 2006/08/16 16:29:11 iha 1.5.4.17: correct distance between diagram and category labels for net chart 2006/08/10 16:05:47 iha 1.5.4.16: Axis Label Layout - Font&Diagram Size 2006/04/10 15:03:37 iha 1.5.4.15: api restructure axis, grids, scales and increments 2006/04/10 12:25:18 iha 1.5.4.14: api restructure axis, grids, scales and increments 2006/03/23 15:31:52 iha 1.5.4.13: cleanup category handling 2006/02/21 19:41:58 iha 1.5.4.12: added property show to axis to make axis title possible with invisible axis and enlarge lifetime for old api 2005/12/21 21:29:33 iha 1.5.4.11: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/10/07 12:18:42 bm 1.5.4.10: RESYNC: (1.5-1.6); FILE MERGED 2005/09/16 16:50:19 iha 1.5.4.9: no creation of dummy categories in model - instead trigger generation of missing labels and categories in view 2005/08/22 17:15:44 iha 1.5.4.8: use percent numberformat in case of percent stacking and use of source format 2005/06/08 11:33:59 iha 1.5.4.7: use number format key at API instead of string 2005/06/07 16:03:26 iha 1.5.4.6: use numberformatter from model 2004/05/08 17:54:55 iha 1.5.4.5: polartransformation fixes and pie offset implementation 2004/03/24 10:39:07 iha 1.5.4.4: change signature of ShapeFactory::createLine2D 2004/03/22 19:41:03 iha 1.5.4.3: transport information bSwapXAndYAxis over method setScales not constructor 2004/03/08 16:40:13 iha 1.5.4.2: no 3D 2004/02/20 17:56:41 iha 1.5.4.1: integrate categories at axis in view

--- chart2/source/view/axes/VPolarAxis.cxx
+++ chart2/source/view/axes/VPolarAxis.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VPolarAxis.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:32:26 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:12:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,43 +36,12 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "VPolarAxis.hxx"
-#include "VPolarGrid.hxx"
-#include "VCartesianAxis.hxx"
-#include "PlottingPositionHelper.hxx"
-#include "ShapeFactory.hxx"
-#include "CommonConverters.hxx"
+#include "VPolarAngleAxis.hxx"
+#include "VPolarRadiusAxis.hxx"
 #include "macros.hxx"
-#include "ViewDefines.hxx"
 #include "TickmarkHelper.hxx"
-#include "PropertyMapper.hxx"
-#include "chartview/NumberFormatterWrapper.hxx"
-#include "chartview/ObjectIdentifier.hxx"
-#include "PolarLabelPositionHelper.hxx"
+#include "ShapeFactory.hxx"
 
-#ifndef INCLUDED_RTL_MATH_HXX
-#include <rtl/math.hxx>
-#endif
-#ifndef _TOOLS_COLOR_HXX
-#include <tools/color.hxx>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
-#include <tools/debug.hxx>
-#endif
-#ifndef _COM_SUN_STAR_STYLE_PARAGRAPHADJUST_HPP_
-#include <com/sun/star/style/ParagraphAdjust.hpp>
-#endif
-#ifndef _COM_SUN_STAR_TEXT_XTEXT_HPP_
-#include <com/sun/star/text/XText.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_XIDENTIFIABLE_HPP_
-#include <com/sun/star/chart2/XIdentifiable.hpp>
-#endif
-
-#ifndef _SVX_UNOPRNMS_HXX
-#include <svx/unoprnms.hxx>
-#endif
-
-#include <algorithm>
 #include <memory>
 
 //.............................................................................
@@ -81,16 +50,21 @@ namespace chart
 //.............................................................................
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::chart2;
-using namespace ::rtl::math;
+
+VPolarAxis* VPolarAxis::createAxis( const AxisProperties& rAxisProperties
+           , const uno::Reference< util::XNumberFormatsSupplier >& xNumberFormatsSupplier
+           , sal_Int32 nDimensionIndex, sal_Int32 nDimensionCount )
+{
+    if( 0==nDimensionIndex )
+        return new VPolarAngleAxis( rAxisProperties, xNumberFormatsSupplier, nDimensionCount );
+    return new VPolarRadiusAxis( rAxisProperties, xNumberFormatsSupplier, nDimensionCount );
+}
 
 VPolarAxis::VPolarAxis( const AxisProperties& rAxisProperties
-            , NumberFormatterWrapper* pNumberFormatterWrapper
-            , sal_Int32 nDimensionCount )
-            : VMeterBase( uno::Reference<XMeter>::query(rAxisProperties.m_xAxisModel)
-                , nDimensionCount )
-            , m_aAxisProperties( rAxisProperties )
-            , m_pNumberFormatterWrapper( pNumberFormatterWrapper )
-            , m_pPosHelper( new PolarPlottingPositionHelper(false) )
+            , const uno::Reference< util::XNumberFormatsSupplier >& xNumberFormatsSupplier
+            , sal_Int32 nDimensionIndex, sal_Int32 nDimensionCount )
+            : VAxisBase( nDimensionIndex, nDimensionCount, rAxisProperties, xNumberFormatsSupplier )
+            , m_pPosHelper( new PolarPlottingPositionHelper() )
             , m_aIncrements()
 {
     PlotterBase::m_pPosHelper = m_pPosHelper;
@@ -107,197 +81,9 @@ void VPolarAxis::setIncrements( const uno::Sequence< ExplicitIncrementData >& rI
     m_aIncrements = rIncrements;
 }
 
-bool createTextShapes_ForAngleAxis( const uno::Reference< lang::XMultiServiceFactory >& xShapeFactory
-                     , const uno::Reference< drawing::XShapes >& xTarget
-                     , ::std::vector< ::std::vector< TickInfo > >& rAllTickInfos
-                     , const ExplicitIncrementData& rIncrement
-                     , AxisLabelProperties& rAxisLabelProperties
-                     , const AxisProperties& rAxisProperties
-                     , PolarPlottingPositionHelper* pPosHelper
-                     , double fLogicRadius
-                     , double fLogicZ
-                     , const FixedNumberFormatter& rFixedNumberFormatter )
+sal_Bool SAL_CALL VPolarAxis::isAnythingToDraw()
 {
-    sal_Int32 nDimensionCount = 2;
-    ShapeFactory aShapeFactory(xShapeFactory);
-
-    //------------------------------------------------
-    //prepare text properties for multipropertyset-interface of shape
-    tNameSequence aPropNames;
-    tAnySequence aPropValues;
-
-    uno::Reference< beans::XPropertySet > xProps( rAxisProperties.m_xAxisModel, uno::UNO_QUERY );
-    PropertyMapper::getTextLabelMultiPropertyLists( xProps, aPropNames, aPropValues, false );
-    LabelPositionHelper::doDynamicFontResize( aPropValues, aPropNames, xProps
-        , rAxisProperties.m_aReferenceSize );
-
-    uno::Any* pColorAny = PropertyMapper::getValuePointer(aPropValues,aPropNames,C2U("CharColor"));
-    sal_Int32 nColor = Color( COL_AUTO ).GetColor();
-    if(pColorAny)
-        *pColorAny >>= nColor;
-    //------------------------------------------------
-
-    //TickInfo* pLastVisibleNeighbourTickInfo = NULL;
-    sal_Int32 nTick = 0;
-    TickIter aIter( rAllTickInfos, rIncrement, 0, 0 );
-    for( TickInfo* pTickInfo = aIter.firstInfo()
-        ; pTickInfo
-        ; pTickInfo = aIter.nextInfo(), nTick++ )
-    {
-        //don't create labels which does not fit into the rythm
-        if( nTick%rAxisLabelProperties.nRhythm != 0)
-            continue;
-
-        //don't create labels for invisible ticks
-        if( !pTickInfo->bPaintIt )
-            continue;
-
-        //if NO OVERLAP -> don't create labels where the
-        //anchor position is the same as for the last label
-        //@todo
-
-        if(!pTickInfo->xTextShape.is())
-        {
-            //create single label
-            bool bHasExtraColor=false;
-            sal_Int32 nExtraColor=0;
-            rtl::OUString aLabel = rFixedNumberFormatter.getFormattedString( pTickInfo->fUnscaledTickValue, nExtraColor, bHasExtraColor );
-            if(pColorAny)
-                *pColorAny = uno::makeAny(bHasExtraColor?nExtraColor:nColor);
-
-            double fLogicAngle = pTickInfo->fUnscaledTickValue;
-
-            LabelAlignment eLabelAlignment(LABEL_ALIGN_CENTER);
-            PolarLabelPositionHelper aPolarLabelPositionHelper(pPosHelper,nDimensionCount,xTarget,&aShapeFactory);
-            awt::Point aAnchorScreenPosition2D( aPolarLabelPositionHelper.getLabelScreenPositionAndAlignment(
-                    eLabelAlignment, true, fLogicAngle, fLogicAngle, fLogicRadius, fLogicRadius, fLogicZ ));
-            LabelPositionHelper::changeTextAdjustment( aPropValues, aPropNames, eLabelAlignment );
-
-            double fRotationAnglePi = rAxisLabelProperties.fRotationAngleDegree*F_PI/180.0;
-            uno::Any aATransformation = ShapeFactory::makeTransformation( aAnchorScreenPosition2D, fRotationAnglePi );
-            rtl::OUString aStackedLabel = ShapeFactory::getStackedString( aLabel, rAxisLabelProperties.bStackCharacters );
-
-            pTickInfo->xTextShape = aShapeFactory.createText( xTarget, aStackedLabel, aPropNames, aPropValues, aATransformation );
-        }
-
-        //if NO OVERLAP -> remove overlapping shapes
-        //@todo
-    }
-    return true;
-}
-
-void VPolarAxis::create2DAngleAxis( const uno::Reference< drawing::XShapes >& xTarget, ::std::vector< ::std::vector< TickInfo > >& rAllTickInfos )
-{
-    double fLogicRadius = m_pPosHelper->getOuterLogicRadius();
-    double fLogicZ      = -0.5;//as defined
-
-    //-----------------------------------------
-    //create axis main lines
-    drawing::PointSequenceSequence aPoints(1);
-    VPolarGrid::createLinePointSequence_ForAngleAxis( aPoints, rAllTickInfos, m_aIncrement, m_aScale, m_pPosHelper, fLogicRadius, fLogicZ );
-    uno::Reference< drawing::XShape > xShape = m_pShapeFactory->createLine2D(
-            xTarget, aPoints, m_aAxisProperties.m_aLineProperties );
-    //because of this name this line will be used for marking the axis
-    m_pShapeFactory->setShapeName( xShape, C2U("MarkHandles") );
-
-    //-----------------------------------------
-    //create labels
-    if( m_aAxisProperties.m_bDisplayLabels )
-    {
-        AxisLabelProperties aAxisLabelProperties;
-        aAxisLabelProperties.init(m_aAxisProperties.m_xAxisModel);
-
-        FixedNumberFormatter aFixedNumberFormatter(
-                m_pNumberFormatterWrapper, aAxisLabelProperties.aNumberFormat );
-
-        while( !createTextShapes_ForAngleAxis( m_xShapeFactory, xTarget, rAllTickInfos
-                        , m_aIncrement, aAxisLabelProperties, m_aAxisProperties, m_pPosHelper
-                        , fLogicRadius, fLogicZ
-                        , aFixedNumberFormatter
-                        ) )
-        {
-        };
-
-        //no staggering for polar angle axis
-    }
-}
-
-void VPolarAxis::create2DRadiusAxis( const uno::Reference< drawing::XShapes >& xTarget, ::std::vector< ::std::vector< TickInfo > >& rAllTickInfos )
-{
-    const ExplicitScaleData& rAngleScale         = m_pPosHelper->getScales()[0];
-    const ExplicitIncrementData& rAngleIncrement = m_aIncrements[0];
-
-    ::std::vector< ::std::vector< TickInfo > > aAngleTickInfos;
-    TickmarkHelper aAngleTickmarkHelper( rAngleScale, rAngleIncrement );
-    aAngleTickmarkHelper.getAllTicks( aAngleTickInfos );
-
-    uno::Reference< XScaling > xInverseScaling( NULL );
-    if( rAngleScale.Scaling.is() )
-        xInverseScaling = rAngleScale.Scaling->getInverseScaling();
-
-    AxisProperties aAxisProperties(m_aAxisProperties);
-    aAxisProperties.m_fInnerDirectionSign=0.0;
-    aAxisProperties.m_bLabelsOutside=true;
-    aAxisProperties.m_bIsMainAxis=false;
-    aAxisProperties.m_aLabelAlignment=LABEL_ALIGN_RIGHT;
-    aAxisProperties.init();
-
-    sal_Int32 nTick = 0;
-    TickIter aIter( aAngleTickInfos, rAngleIncrement, 0, 0 );
-    for( TickInfo* pTickInfo = aIter.firstInfo()
-        ; pTickInfo; pTickInfo = aIter.nextInfo(), nTick++ )
-    {
-        pTickInfo->updateUnscaledValue( xInverseScaling );
-        aAxisProperties.m_pfMainLinePositionAtOtherAxis = new double( pTickInfo->fUnscaledTickValue );
-        if(nTick)
-            aAxisProperties.m_bDisplayLabels=false;
-
-        //-------------------
-        VCartesianAxis aAxis(aAxisProperties,m_pNumberFormatterWrapper
-            ,2,new PolarPlottingPositionHelper(false));
-        aAxis.setMeterData( m_aScale, m_aIncrement );
-        aAxis.init(m_xLogicTarget,m_xFinalTarget,m_xShapeFactory);
-        aAxis.setTransformationSceneToScreen( Matrix4DToHomogenMatrix( m_aMatrixScreenToScene ) );
-        aAxis.setScales( m_pPosHelper->getScales() );
-        aAxis.createShapes();
-    }
-}
-
-void SAL_CALL VPolarAxis::createShapes()
-{
-    DBG_ASSERT(m_pShapeFactory&&m_xLogicTarget.is()&&m_xFinalTarget.is(),"Axis is not proper initialized");
-    if(!(m_pShapeFactory&&m_xLogicTarget.is()&&m_xFinalTarget.is()))
-        return;
-
-    //-----------------------------------------
-    //create named group shape
-    uno::Reference< XIdentifiable > xIdent( m_aAxisProperties.m_xAxisModel, uno::UNO_QUERY );
-    DBG_ASSERT( xIdent.is(), "Axis should support XIdentifiable" );
-    if( ! xIdent.is())
-        return;
-    uno::Reference< drawing::XShapes > xGroupShape_Shapes(
-        m_pShapeFactory->createGroup2D( m_xLogicTarget
-        , ObjectIdentifier::createClassifiedIdentifier(
-                OBJECTTYPE_AXIS, xIdent->getIdentifier() )
-        ) );
-
-    //-----------------------------------------
-    //create all scaled tickmark values
-    std::auto_ptr< TickmarkHelper > apTickmarkHelper( this->createTickmarkHelper() );
-    ::std::vector< ::std::vector< TickInfo > > aAllTickInfos;
-    apTickmarkHelper->getAllTicks( aAllTickInfos );
-
-    //-----------------------------------------
-    //create different axes
-    if(2==m_nDimension)
-    {
-        sal_Int32 nDimensionIndex = m_xMeter->getRepresentedDimension();
-        if(nDimensionIndex==0)
-            this->create2DAngleAxis( xGroupShape_Shapes, aAllTickInfos );
-        else if(nDimensionIndex==1)
-            this->create2DRadiusAxis( xGroupShape_Shapes, aAllTickInfos );
-
-    }
+    return ( 2==m_nDimension && VAxisBase::isAnythingToDraw() );
 }
 
 //.............................................................................
