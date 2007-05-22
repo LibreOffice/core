449b4bc6e4ea05b8325d37b49b9bd8b0004e1db7
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:09:53 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:48 iha 1.1.2.4: resync m195 -> m202 2006/11/22 17:31:19 iha 1.1.2.3: resync 2006/08/22 19:09:04 iha 1.1.2.2: correct y axis for net charts: show labels + take space for labels into account for diagram size 2006/04/10 15:02:33 iha 1.1.2.1: api restructure axis, grids, scales and increments

---
+++ chart2/source/view/axes/VAxisOrGridBase.cxx
@@ -0,0 +1,98 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: VAxisOrGridBase.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:09:53 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "VAxisOrGridBase.hxx"
+#include "ShapeFactory.hxx"
+#include "CommonConverters.hxx"
+#include "TickmarkHelper.hxx"
+
+// header for define DBG_ASSERT
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+using ::com::sun::star::uno::Reference;
+
+VAxisOrGridBase::VAxisOrGridBase( sal_Int32 nDimensionIndex, sal_Int32 nDimensionCount )
+            : PlotterBase( nDimensionCount )
+            , m_nDimensionIndex( nDimensionIndex )
+{
+}
+
+VAxisOrGridBase::~VAxisOrGridBase()
+{
+}
+
+void SAL_CALL VAxisOrGridBase::setExplicitScaleAndIncrement(
+              const ExplicitScaleData& rScale
+            , const ExplicitIncrementData& rIncrement )
+            throw (uno::RuntimeException)
+{
+    m_aScale = rScale;
+    m_aIncrement = rIncrement;
+}
+
+void VAxisOrGridBase::setTransformationSceneToScreen( const drawing::HomogenMatrix& rMatrix )
+{
+    m_aMatrixScreenToScene = HomogenMatrixToB3DHomMatrix(rMatrix);
+    PlotterBase::setTransformationSceneToScreen( rMatrix);
+}
+
+TickmarkHelper* VAxisOrGridBase::createTickmarkHelper()
+{
+    TickmarkHelper* pRet=NULL;
+    if( 2==m_nDimension )
+    {
+        pRet = new TickmarkHelper( m_aScale, m_aIncrement );
+    }
+    else
+    {
+        pRet = new TickmarkHelper_3D( m_aScale, m_aIncrement );
+    }
+    return pRet;
+}
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
