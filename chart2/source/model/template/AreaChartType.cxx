73df81d84a40dc1c7cdeddfc9910586e7e1e529f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:43:44 +0000

    INTEGRATION: CWS chart2mst3 (1.2.4); FILE MERGED 2006/10/18 17:12:40 bm 1.2.4.7: RESYNC: (1.3-1.4); FILE MERGED 2005/10/07 12:00:18 bm 1.2.4.6: RESYNC: (1.2-1.3); FILE MERGED 2005/04/11 10:03:12 iha 1.2.4.5: defines for servicenames for charttypes (fixed apply to 2nd axis again) 2005/03/30 16:31:10 bm 1.2.4.4: make model cloneable (+first undo implementation) 2004/09/16 14:42:44 bm 1.2.4.3: API simplification 2004/09/16 12:27:24 bm 1.2.4.2: API simplification 2004/04/01 10:53:06 bm 1.2.4.1: XChartType: may return a coordinate system now

--- chart2/source/model/template/AreaChartType.cxx
+++ chart2/source/model/template/AreaChartType.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AreaChartType.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:16:29 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:43:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,24 +36,50 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "AreaChartType.hxx"
+#include "macros.hxx"
+#include "servicenames_charttypes.hxx"
 
 using namespace ::com::sun::star;
 
 namespace chart
 {
 
-AreaChartType::AreaChartType( sal_Int32 nDim /* = 2 */ ) :
-        ChartType( nDim )
+AreaChartType::AreaChartType(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        ChartType( xContext )
+{}
+
+AreaChartType::AreaChartType( const AreaChartType & rOther ) :
+        ChartType( rOther )
 {}
 
 AreaChartType::~AreaChartType()
 {}
 
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL AreaChartType::createClone()
+    throw (uno::RuntimeException)
+{
+    return uno::Reference< util::XCloneable >( new AreaChartType( *this ));
+}
+
 // ____ XChartType ____
 ::rtl::OUString SAL_CALL AreaChartType::getChartType()
     throw (uno::RuntimeException)
 {
-    return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.AreaChart" ));
+    return CHART2_SERVICE_NAME_CHARTTYPE_AREA;
 }
+
+uno::Sequence< ::rtl::OUString > AreaChartType::getSupportedServiceNames_Static()
+{
+    uno::Sequence< ::rtl::OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_SERVICE_NAME_CHARTTYPE_AREA;
+    aServices[ 1 ] = C2U( "com.sun.star.chart2.ChartType" );
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( AreaChartType,
+                             C2U( "com.sun.star.comp.chart.AreaChartType" ));
 
 } //  namespace chart
