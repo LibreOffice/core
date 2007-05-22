0a4bb4a608f2bf3127c88af8de48702d30372cdc
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:15:54 +0000

    INTEGRATION: CWS chart2mst3 (1.1.1.1.4); FILE MERGED 2006/03/09 17:41:17 iha 1.1.1.1.4.3: added header 2005/11/22 14:09:27 iha 1.1.1.1.4.2: support BarOverlap and GapWidth 2005/10/24 11:07:07 iha 1.1.1.1.4.1: coordinate system restructure

--- chart2/source/view/charttypes/CategoryPositionHelper.hxx
+++ chart2/source/view/charttypes/CategoryPositionHelper.hxx
@@ -1,3 +1,38 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: CategoryPositionHelper.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:15:54 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
 #ifndef _CHART2_CATEGORYPOSITIONHELPER_HXX
 #define _CHART2_CATEGORYPOSITIONHELPER_HXX
 
@@ -14,11 +49,18 @@ class CategoryPositionHelper
 {
 public:
     CategoryPositionHelper( double fSeriesCount, double CategoryWidth = 1.0);
+    CategoryPositionHelper( const CategoryPositionHelper& rSource );
     CategoryPositionHelper();
     virtual ~CategoryPositionHelper();
 
     double getSlotWidth() const;
     double getSlotPos( double fCategoryX, double fSeriesNumber ) const;
+
+    //Distance between two neighboring bars in same category, seen relative to width of the bar
+    void setInnerDistance( double fInnerDistance );
+
+    //Distance between two neighboring bars in different category, seen relative to width of the bar:
+    void setOuterDistance( double fOuterDistance );
 
 protected:
     double m_fSeriesCount;
