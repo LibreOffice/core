23e8690e630346522a22be96277bddfc696625fb
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:46:49 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/01/23 15:54:46 iha 1.1.2.3: #i61054# disable next button at wizard correctly 2005/10/11 09:20:17 bm 1.1.2.2: license header change 2004/05/05 20:40:10 iha 1.1.2.1: #i20344# location tab page for chart wizard

---
+++ chart2/source/controller/dialogs/tp_Location.hxx
@@ -0,0 +1,93 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_Location.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:46:49 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_TP_LOCATION_HXX
+#define _CHART2_TP_LOCATION_HXX
+
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+
+// header for class FixedText, FixedLine
+#ifndef _SV_FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
+// header for class ListBox
+#ifndef _SV_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+
+class LocationTabPage : public svt::OWizardPage
+{
+public:
+    LocationTabPage( svt::OWizardMachine* pParent
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::chart2::XChartDocument >& xChartModel
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::uno::XComponentContext >& xContext );
+    virtual ~LocationTabPage();
+
+    virtual sal_Bool    determineNextButtonState();
+
+protected:
+    FixedLine     m_aFL_Table;
+    FixedText     m_aFT_Table;
+    ListBox       m_aLB_Table;
+
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::chart2::XChartDocument >   m_xChartModel;
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::uno::XComponentContext>    m_xCC;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
