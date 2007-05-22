9c1d037db96ccf71906a3b81952577b8b6f17952
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:31:30 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:33 iha 1.1.2.7: resync m195 -> m202 2006/04/10 15:03:31 iha 1.1.2.6: api restructure axis, grids, scales and increments 2006/01/24 10:27:43 bm 1.1.2.5: pass ReferenceSizeProvider to allow setting correct Auto-Resize values for new objects 2005/12/01 18:54:47 iha 1.1.2.4: create axis title with correct rotation default + don't mix x and y in barcharts 2005/10/11 09:20:08 bm 1.1.2.3: license header change 2004/04/26 20:06:05 iha 1.1.2.2: #i28172# already exsisiting titles are deleted and created again loosing their properties 2004/04/07 21:33:00 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/dialogs/TitleDialogData.cxx
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: TitleDialogData.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:31:30 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "TitleDialogData.hxx"
+#include "TitleHelper.hxx"
+#include "ChartModelHelper.hxx"
+#include "AxisHelper.hxx"
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+
+TitleDialogData::TitleDialogData( ::std::auto_ptr< ReferenceSizeProvider > apRefSizeProvider )
+        : aPossibilityList(5)
+        , aExistenceList(5)
+        , aTextList(5)
+        , apReferenceSizeProvider( apRefSizeProvider )
+{
+    sal_Int32 nN = 0;
+    for(nN=5;nN--;)
+        aPossibilityList[nN]=sal_True;
+    for(nN=5;nN--;)
+        aExistenceList[nN]=sal_False;
+}
+
+void TitleDialogData::readFromModel( const uno::Reference< frame::XModel>& xChartModel )
+{
+    uno::Reference< XDiagram > xDiagram = ChartModelHelper::findDiagram(xChartModel);
+
+    //get possibilities
+    uno::Sequence< sal_Bool > aAxisPossibilityList;
+    AxisHelper::getAxisOrGridPossibilities( aAxisPossibilityList, xDiagram );
+    this->aPossibilityList[2]=aAxisPossibilityList[0];//x axis title
+    this->aPossibilityList[3]=aAxisPossibilityList[1];//y axis title
+    this->aPossibilityList[4]=aAxisPossibilityList[2];//z axis title
+
+    //find out which title exsist and get their text
+    //main title:
+    for( sal_Int32 nTitleIndex = static_cast< sal_Int32 >( TitleHelper::TITLE_BEGIN);
+         nTitleIndex < static_cast< sal_Int32 >( TitleHelper::NORMAL_TITLE_END );
+         nTitleIndex++)
+    {
+        uno::Reference< XTitle > xTitle =  TitleHelper::getTitle(
+            static_cast< TitleHelper::eTitleType >( nTitleIndex ), xChartModel );
+        this->aExistenceList[nTitleIndex] = xTitle.is();
+        this->aTextList[nTitleIndex]=TitleHelper::getCompleteString( xTitle );
+    }
+}
+
+bool TitleDialogData::writeDifferenceToModel(
+                          const uno::Reference< frame::XModel >& xChartModel
+                        , const uno::Reference< uno::XComponentContext >& xContext
+                        , TitleDialogData* pOldState )
+{
+    bool bChanged = false;
+    for( sal_Int32 nN = static_cast< sal_Int32 >( TitleHelper::TITLE_BEGIN );
+         nN < static_cast< sal_Int32 >( TitleHelper::NORMAL_TITLE_END );
+         nN++)
+    {
+        if( !pOldState || ( pOldState->aExistenceList[nN] != this->aExistenceList[nN] ) )
+        {
+            if(this->aExistenceList[nN])
+            {
+                TitleHelper::createTitle(
+                    static_cast< TitleHelper::eTitleType >( nN ), this->aTextList[nN], xChartModel, xContext,
+                    apReferenceSizeProvider.get() );
+                bChanged = true;
+            }
+            else
+            {
+                TitleHelper::removeTitle( static_cast< TitleHelper::eTitleType >( nN ), xChartModel );
+                bChanged = true;
+            }
+        }
+        else if( !pOldState || ( pOldState->aTextList[nN] != this->aTextList[nN] ) )
+        {
+            //change content
+            uno::Reference< XTitle > xTitle(
+                TitleHelper::getTitle( static_cast< TitleHelper::eTitleType >( nN ), xChartModel ) );
+            if(xTitle.is())
+            {
+                TitleHelper::setCompleteString( this->aTextList[nN], xTitle, xContext );
+                bChanged = true;
+            }
+        }
+    }
+    return bChanged;
+}
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
