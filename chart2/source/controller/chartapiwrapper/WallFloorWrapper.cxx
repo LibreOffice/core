a342022b95571a07ee4d19330e267a98fd22f97d
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:20:05 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:28 iha 1.1.2.17: resync m195 -> m202 2006/10/25 21:51:55 iha 1.1.2.16: cope with context dependent defaults from old chart 2006/10/25 17:53:26 iha 1.1.2.15: cope with context dependent defaults from old chart 2006/09/20 16:27:09 bm 1.1.2.14: #i69680# As the floor is consistent and the default is the same in the old and new API, only set no-default for the wall 2006/09/20 15:58:18 bm 1.1.2.13: #i69680# Wall and floor have no defaults. They are always exported in the old and the new chart. (The wall defaults in the old chart were chart type dependent). 2006/03/12 00:55:15 iha 1.1.2.12: merged ChartAPIValueProvider with Chart2ModelContact 2006/02/17 20:57:44 iha 1.1.2.11: enlarge validity of api wrapper objects to lifetime of new chartmodel 2005/11/15 15:30:15 bm 1.1.2.10: garbage collection, disposing, reference release issues 2005/10/11 09:19:55 bm 1.1.2.9: license header change 2005/08/03 16:36:14 bm 1.1.2.8: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/07/14 12:32:48 iha 1.1.2.7: remove unused parameter 'bIncludeStyleProperties' 2005/07/12 12:56:56 bm 1.1.2.6: use named properties for gradients etc. in chart model 2005/07/07 12:09:22 bm 1.1.2.5: some defaults are different in old chart 2005/05/31 19:07:18 iha 1.1.2.4: create old api without controller 2005/04/19 17:28:40 iha 1.1.2.3: improved old api support (introduced wrappedpropertyset ...) 2004/09/27 17:15:52 iha 1.1.2.2: replaced getInfoHelper().fillPropertyMembersByHandle by NamedFillProperties::GetPropertyNameForHandle 2004/06/04 07:52:03 bm 1.1.2.1: handling named properties like FillGradientName (coming from XML)

---
+++ chart2/source/controller/chartapiwrapper/WallFloorWrapper.cxx
@@ -0,0 +1,215 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WallFloorWrapper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:20:05 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "WallFloorWrapper.hxx"
+#include "macros.hxx"
+#include "Scaling.hxx"
+#include "Chart2ModelContact.hxx"
+#include "ContainerHelper.hxx"
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
+#include <com/sun/star/beans/PropertyAttribute.hpp>
+#endif
+#ifndef _COM_SUN_STAR_DRAWING_FILLSTYLE_HPP_
+#include <com/sun/star/drawing/FillStyle.hpp>
+#endif
+
+// #include "NamedProperties.hxx"
+#include "FillProperties.hxx"
+#include "LineProperties.hxx"
+#include "UserDefinedProperties.hxx"
+// #include "WrappedNamedProperty.hxx"
+#include "WrappedDirectStateProperty.hxx"
+
+#include <algorithm>
+
+#ifndef _RTL_USTRBUF_HXX_
+#include <rtl/ustrbuf.hxx>
+#endif
+#ifndef INCLUDED_RTL_MATH_HXX
+#include <rtl/math.hxx>
+#endif
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+
+using ::com::sun::star::beans::Property;
+using ::osl::MutexGuard;
+using ::com::sun::star::uno::Any;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Sequence;
+using ::rtl::OUString;
+
+namespace
+{
+static const OUString lcl_aServiceName(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart.WallOrFloor" ));
+
+const Sequence< Property > & lcl_GetPropertySequence()
+{
+    static Sequence< Property > aPropSeq;
+
+    // /--
+    MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+    if( 0 == aPropSeq.getLength() )
+    {
+        // get properties
+        ::std::vector< ::com::sun::star::beans::Property > aProperties;
+        ::chart::FillProperties::AddPropertiesToVector( aProperties );
+        ::chart::LineProperties::AddPropertiesToVector( aProperties );
+//         ::chart::NamedProperties::AddPropertiesToVector( aProperties );
+        ::chart::UserDefinedProperties::AddPropertiesToVector( aProperties );
+
+        // and sort them for access via bsearch
+        ::std::sort( aProperties.begin(), aProperties.end(),
+                     ::chart::PropertyNameLess() );
+
+        // transfer result to static Sequence
+        aPropSeq = ::chart::ContainerHelper::ContainerToSequence( aProperties );
+    }
+
+    return aPropSeq;
+}
+
+} // anonymous namespace
+
+// --------------------------------------------------------------------------------
+
+namespace chart
+{
+namespace wrapper
+{
+
+WallFloorWrapper::WallFloorWrapper( bool bWall,
+    ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact ) :
+        m_spChart2ModelContact( spChart2ModelContact ),
+        m_aEventListenerContainer( m_aMutex ),
+        m_bWall( bWall )
+
+{
+}
+
+WallFloorWrapper::~WallFloorWrapper()
+{
+}
+
+// ____ XComponent ____
+void SAL_CALL WallFloorWrapper::dispose()
+    throw (uno::RuntimeException)
+{
+    Reference< uno::XInterface > xSource( static_cast< ::cppu::OWeakObject* >( this ) );
+    m_aEventListenerContainer.disposeAndClear( lang::EventObject( xSource ) );
+
+    // /--
+    MutexGuard aGuard( GetMutex());
+    clearWrappedPropertySet();
+    // \--
+}
+
+void SAL_CALL WallFloorWrapper::addEventListener(
+    const Reference< lang::XEventListener >& xListener )
+    throw (uno::RuntimeException)
+{
+    m_aEventListenerContainer.addInterface( xListener );
+}
+
+void SAL_CALL WallFloorWrapper::removeEventListener(
+    const Reference< lang::XEventListener >& aListener )
+    throw (uno::RuntimeException)
+{
+    m_aEventListenerContainer.removeInterface( aListener );
+}
+
+// ================================================================================
+
+// WrappedPropertySet
+Reference< beans::XPropertySet > WallFloorWrapper::getInnerPropertySet()
+{
+    Reference< beans::XPropertySet > xRet;
+
+    Reference< chart2::XDiagram > xDiagram( m_spChart2ModelContact->getChart2Diagram() );
+    if( xDiagram.is() )
+    {
+        if( m_bWall )
+            xRet.set( xDiagram->getWall() );
+        else
+            xRet.set( xDiagram->getFloor() );
+    }
+
+    return xRet;
+}
+
+const Sequence< beans::Property >& WallFloorWrapper::getPropertySequence()
+{
+    return lcl_GetPropertySequence();
+}
+
+const std::vector< WrappedProperty* > WallFloorWrapper::createWrappedProperties()
+{
+    ::std::vector< ::chart::WrappedProperty* > aWrappedProperties;
+
+//     WrappedNamedProperty::addWrappedProperties( aWrappedProperties, m_spChart2ModelContact );
+
+    // use direct state always, so that in XML the value is always
+    // exported. Because in the old chart the defaults is as follows:
+    // Floor: SOLID (new and old model default), Wall: NONE, except for some chart types (line, scatter)
+    if( m_bWall )
+        aWrappedProperties.push_back( new WrappedDirectStateProperty( C2U("FillStyle"), C2U("FillStyle") ));
+    aWrappedProperties.push_back( new WrappedDirectStateProperty( C2U("FillColor"), C2U("FillColor") ));
+
+    return aWrappedProperties;
+}
+
+// ================================================================================
+
+Sequence< OUString > WallFloorWrapper::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 4 );
+    aServices[ 0 ] = C2U( "com.sun.star.xml.UserDefinedAttributeSupplier" );
+    aServices[ 1 ] = C2U( "com.sun.star.drawing.FillProperties" );
+    aServices[ 2 ] = C2U( "com.sun.star.drawing.LineProperties" );
+    aServices[ 3 ] = C2U( "com.sun.star.beans.PropertySet" );
+
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( WallFloorWrapper, lcl_aServiceName );
+
+} //  namespace wrapper
+} //  namespace chart
