23985dc6b257863ffc061aff099ee9448e873a7a
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:24:45 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/03/12 01:00:14 iha 1.1.2.4: enlarge validity of area or line properties of data series to lifetime of new chartmodel 2005/10/11 09:20:36 bm 1.1.2.3: license header change 2005/07/14 15:32:56 iha 1.1.2.2: cleanup WrappedIgnoreProperty 2005/04/19 17:24:55 iha 1.1.2.1: improved old api support (introduced wrappedpropertyset ...)

---
+++ chart2/source/inc/WrappedProperty.hxx
@@ -0,0 +1,98 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedProperty.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:24:45 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_WRAPPEDPROPERTY_HXX
+#define CHART_WRAPPEDPROPERTY_HXX
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSTATE_HPP_
+#include <com/sun/star/beans/XPropertyState.hpp>
+#endif
+
+#include <map>
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+class WrappedProperty
+{
+    /** The property visible to the outer PropertySet in the PropertySetWrapper may have a different name, type and value than
+    a corresponding property of the inner PropertySet. Use this class to do the conversion between the two.
+    */
+public:
+    WrappedProperty( const ::rtl::OUString& rOuterName, const ::rtl::OUString& rInnerName );
+    virtual ~WrappedProperty();
+
+    const ::rtl::OUString& getOuterName() const;
+    virtual ::rtl::OUString getInnerName() const;
+
+    virtual void setPropertyValue( const ::com::sun::star::uno::Any& rOuterValue, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Any getPropertyValue( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual void setPropertyToDefault( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Any getPropertyDefault( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::beans::PropertyState getPropertyState( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+
+protected:
+    virtual ::com::sun::star::uno::Any convertInnerToOuterValue( const ::com::sun::star::uno::Any& rInnerValue ) const;
+    virtual ::com::sun::star::uno::Any convertOuterToInnerValue( const ::com::sun::star::uno::Any& rOuterValue ) const;
+
+protected:
+    ::rtl::OUString             m_aOuterName;
+    ::rtl::OUString             m_aInnerName;
+};
+
+//.............................................................................
+
+typedef ::std::map< sal_Int32, const WrappedProperty* > tWrappedPropertyMap;
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+
+// CHART_WRAPPEDPROPERTY_HXX
+#endif
