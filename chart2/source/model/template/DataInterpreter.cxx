6e3208c6c9ac3113c74910560e2f658082b3b6b0
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:48:18 +0000

    INTEGRATION: CWS chart2mst3 (1.1.4); FILE ADDED 2007/02/08 21:25:40 iha 1.1.4.17: resync m195 -> m202 2006/08/10 17:10:41 bm 1.1.4.16: second axis for stock charts fixed 2006/04/28 12:44:52 bm 1.1.4.15: #i64618# lcl_LabeledSequenceEquals: allow data sequences to be empty on equality check. 2005/12/21 21:29:23 iha 1.1.4.14: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/02 14:54:17 iha 1.1.4.13: make more failsafe 2005/10/11 09:20:45 bm 1.1.4.12: license header change 2005/08/03 16:36:23 bm 1.1.4.11: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/07/29 13:30:15 bm 1.1.4.10: keep unused data at the diagram, resetStyles, and other small improvements of template handling 2005/05/09 09:51:25 bm 1.1.4.9: moved parts of API to data namespace 2005/05/03 16:52:14 bm 1.1.4.8: XDataProvider, XDataSequence: RangeIdentifiers removed 2005/04/06 13:17:11 iha 1.1.4.7: make more failsave 2004/07/05 11:33:13 iha 1.1.4.6: identify duplicate ranges correctly to prevent duplication of series 2004/05/07 13:40:24 bm 1.1.4.5: context added in CTOR 2004/03/29 13:29:25 bm 1.1.4.4: XDataSequence: getSourceRangeRepresentation -> getSourceIdentifier 2004/03/24 09:44:21 bm 1.1.4.3: +debug function lcl_ShowDataSource for tracing the content of the data source that is parsed some if checks for invalid objects 2004/03/19 14:32:57 bm 1.1.4.2: XDataSource now contains XLabeledDataSources 2004/02/13 16:51:42 bm 1.1.4.1: join from changes on branch bm_post_chart01

---
+++ chart2/source/model/template/DataInterpreter.cxx
@@ -0,0 +1,503 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: DataInterpreter.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:48:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "DataInterpreter.hxx"
+#include "DataSeries.hxx"
+#include "DataSource.hxx"
+#include "DataSeriesHelper.hxx"
+#include "macros.hxx"
+#include "CommonConverters.hxx"
+#include "ContainerHelper.hxx"
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CHART2_DATA_XDATASINK_HPP_
+#include <com/sun/star/chart2/data/XDataSink.hpp>
+#endif
+
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+using namespace ::std;
+using namespace ::chart::ContainerHelper;
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Sequence;
+using ::rtl::OUString;
+
+#if OSL_DEBUG_LEVEL > 1
+namespace
+{
+void lcl_ShowDataSource( const Reference< data::XDataSource > & xSource );
+}
+#endif
+
+namespace chart
+{
+
+DataInterpreter::DataInterpreter(
+    const Reference< uno::XComponentContext > & xContext ) :
+        m_xContext( xContext )
+{}
+
+DataInterpreter::~DataInterpreter()
+{}
+
+Reference< uno::XComponentContext > DataInterpreter::GetComponentContext() const
+{
+    return m_xContext;
+}
+
+// ____ XDataInterpreter ____
+InterpretedData SAL_CALL DataInterpreter::interpretDataSource(
+    const Reference< data::XDataSource >& xSource,
+    const Sequence< beans::PropertyValue >& aArguments,
+    const Sequence< Reference< XDataSeries > >& aSeriesToReUse )
+    throw (uno::RuntimeException)
+{
+    if( ! xSource.is())
+        return InterpretedData();
+
+#if OSL_DEBUG_LEVEL > 2
+    lcl_ShowDataSource( xSource );
+#endif
+
+    Sequence< Reference< data::XLabeledDataSequence > > aData( xSource->getDataSequences() );
+
+    Reference< data::XLabeledDataSequence > xCategories;
+    vector< Reference< data::XLabeledDataSequence > > aSequencesVec;
+
+    // check if we should use categories
+
+    bool bHasCategories( HasCategories( aArguments, aData ));
+
+    // parse data
+    bool bCategoriesUsed = false;
+    for( sal_Int32 i=0; i < aData.getLength(); ++i )
+    {
+        try
+        {
+            if( bHasCategories && ! bCategoriesUsed )
+            {
+                xCategories.set( aData[i] );
+                if( xCategories.is())
+                    SetRole( xCategories->getValues(), C2U("categories"));
+                bCategoriesUsed = true;
+            }
+            else
+            {
+                aSequencesVec.push_back( aData[i] );
+                if( aData[i].is())
+                    SetRole( aData[i]->getValues(), C2U("values-y"));
+            }
+        }
+        catch( uno::Exception & ex )
+        {
+            ASSERT_EXCEPTION( ex );
+        }
+    }
+
+    // create DataSeries
+    vector< Reference< data::XLabeledDataSequence > >::const_iterator
+          aSequencesVecIt = aSequencesVec.begin();
+
+    sal_Int32 nSeriesIndex = 0;
+    vector< Reference< XDataSeries > > aSeriesVec;
+    aSeriesVec.reserve( aSequencesVec.size());
+
+    for( ;aSequencesVecIt != aSequencesVec.end(); ++aSequencesVecIt, ++nSeriesIndex )
+    {
+        Sequence< Reference< data::XLabeledDataSequence > > aNewData( & (*aSequencesVecIt), 1 );
+        Reference< XDataSeries > xSeries;
+        if( nSeriesIndex < aSeriesToReUse.getLength())
+            xSeries.set( aSeriesToReUse[nSeriesIndex] );
+        else
+            xSeries.set( new DataSeries( GetComponentContext() ));
+        OSL_ASSERT( xSeries.is() );
+        Reference< data::XDataSink > xSink( xSeries, uno::UNO_QUERY );
+        OSL_ASSERT( xSink.is() );
+        xSink->setData( aNewData );
+
+        aSeriesVec.push_back( xSeries );
+    }
+
+    Sequence< Sequence< Reference< XDataSeries > > > aSeries(1);
+    aSeries[0] = ContainerToSequence( aSeriesVec );
+    return InterpretedData( aSeries, xCategories, Sequence< Reference< data::XLabeledDataSequence > >() );
+}
+
+InterpretedData SAL_CALL DataInterpreter::reinterpretDataSeries(
+    const InterpretedData& aInterpretedData )
+    throw (uno::RuntimeException)
+{
+    InterpretedData aResult( aInterpretedData );
+    vector< Reference< data::XLabeledDataSequence > > aUnused(
+        SequenceToVector( aInterpretedData.UnusedData ));
+
+    sal_Int32 i=0;
+    Sequence< Reference< XDataSeries > > aSeries( FlattenSequence( aInterpretedData.Series ));
+    const sal_Int32 nCount = aSeries.getLength();
+    for( ; i<nCount; ++i )
+    {
+        try
+        {
+            Reference< data::XDataSource > xSeriesSource( aSeries[i], uno::UNO_QUERY_THROW );
+            Sequence< Reference< data::XLabeledDataSequence > > aNewSequences;
+
+            // values-y
+            Reference< data::XLabeledDataSequence > xValuesY(
+                DataSeriesHelper::getDataSequenceByRole( xSeriesSource, C2U("values-y"), false ));
+            // re-use values-... as values-y
+            if( ! xValuesY.is())
+            {
+                xValuesY.set(
+                    DataSeriesHelper::getDataSequenceByRole( xSeriesSource, C2U("values"), true ));
+                if( xValuesY.is())
+                    SetRole( xValuesY->getValues(), C2U("values-y"));
+            }
+            if( xValuesY.is())
+            {
+                aNewSequences.realloc(1);
+                aNewSequences[0] = xValuesY;
+            }
+
+            Sequence< Reference< data::XLabeledDataSequence > > aSeqs( xSeriesSource->getDataSequences());
+            if( aSeqs.getLength() != aNewSequences.getLength() )
+            {
+                sal_Int32 j=0;
+                for( ; j<aSeqs.getLength(); ++j )
+                {
+                    if( aSeqs[j] != xValuesY )
+                        aUnused.push_back( aSeqs[j] );
+                }
+                Reference< data::XDataSink > xSink( xSeriesSource, uno::UNO_QUERY_THROW );
+
+                xSink->setData( aNewSequences );
+            }
+        }
+        catch( uno::Exception & ex )
+        {
+            ASSERT_EXCEPTION( ex );
+        }
+    }
+
+    aResult.UnusedData = ContainerToSequence( aUnused );
+
+    return aResult;
+}
+
+// criterion: all series must have exactly one data::XLabeledDataSequence
+sal_Bool SAL_CALL DataInterpreter::isDataCompatible(
+    const chart2::InterpretedData& aInterpretedData )
+    throw (uno::RuntimeException)
+{
+    Sequence< Reference< XDataSeries > > aSeries( FlattenSequence( aInterpretedData.Series ));
+    for( sal_Int32 i=0; i<aSeries.getLength(); ++i )
+    {
+        try
+        {
+            Reference< data::XDataSource > xSrc( aSeries[i], uno::UNO_QUERY_THROW );
+            Sequence< Reference< data::XLabeledDataSequence > > aSeq( xSrc->getDataSequences());
+            if( aSeq.getLength() != 1 )
+                return sal_False;
+        }
+        catch( uno::Exception & ex )
+        {
+            ASSERT_EXCEPTION( ex );
+        }
+    }
+
+    return sal_True;
+}
+
+namespace
+{
+
+struct lcl_LabeledSequenceEquals : public unary_function< Reference< data::XLabeledDataSequence >, bool >
+{
+    lcl_LabeledSequenceEquals( const Reference< data::XLabeledDataSequence > & xLSeqToCmp ) :
+            m_bHasLabels ( false ),
+            m_bHasValues ( false )
+    {
+        if( xLSeqToCmp.is())
+        {
+            Reference< data::XDataSequence > xSeq( xLSeqToCmp->getValues());
+            if( xSeq.is())
+            {
+                m_bHasValues = true;
+                m_aValuesRangeRep = xSeq->getSourceRangeRepresentation();
+            }
+
+            xSeq.set( xLSeqToCmp->getLabel());
+            if( xSeq.is())
+            {
+                m_bHasLabels = true;
+                m_aLabelRangeRep = xSeq->getSourceRangeRepresentation();
+            }
+        }
+    }
+
+    bool operator() ( const Reference< data::XLabeledDataSequence > & xSeq )
+    {
+        if( ! xSeq.is())
+            return false;
+
+        Reference< data::XDataSequence > xSeqValues( xSeq->getValues() );
+        Reference< data::XDataSequence > xSeqLabels( xSeq->getLabel() );
+        bool bHasValues = xSeqValues.is();
+        bool bHasLabels = xSeqLabels.is();
+
+        return ( ( (m_bHasValues == bHasValues) &&
+                   (!bHasValues || m_aValuesRangeRep.equals( xSeqValues->getSourceRangeRepresentation())) ) &&
+                 ( (m_bHasLabels == bHasLabels) &&
+                   (!bHasLabels || m_aLabelRangeRep.equals( xSeqLabels->getSourceRangeRepresentation())) )
+            );
+    }
+
+private:
+    bool m_bHasLabels;
+    bool m_bHasValues;
+    OUString m_aValuesRangeRep;
+    OUString m_aLabelRangeRep;
+};
+
+} // anonymous namespace
+
+Reference< data::XDataSource > SAL_CALL DataInterpreter::mergeInterpretedData(
+    const InterpretedData& aInterpretedData )
+    throw (uno::RuntimeException)
+{
+    vector< Reference< data::XLabeledDataSequence > > aResultVec;
+    vector< Reference< data::XLabeledDataSequence > > aUnusedDataVec;
+    aResultVec.reserve( aInterpretedData.Series.getLength() +
+                        aInterpretedData.UnusedData.getLength() +
+                        1 // categories
+        );
+
+    if( aInterpretedData.Categories.is())
+        aResultVec.push_back( aInterpretedData.Categories );
+
+    // add unused data that has the Role categories at front
+    if( aInterpretedData.UnusedData.getLength())
+    {
+        sal_Int32 nSize = aInterpretedData.UnusedData.getLength();
+        for( sal_Int32 i=0; i<nSize; ++i )
+        {
+            Reference< data::XLabeledDataSequence > xPotentialCategories( aInterpretedData.UnusedData[i]  );
+            if( xPotentialCategories.is() )
+            {
+                if( GetRole( xPotentialCategories->getValues()).equalsAsciiL( RTL_CONSTASCII_STRINGPARAM("categories")))
+                    aResultVec.push_back( xPotentialCategories );
+                else
+                    aUnusedDataVec.push_back( xPotentialCategories );
+            }
+        }
+    }
+    else
+        aUnusedDataVec = SequenceToVector( aInterpretedData.UnusedData );
+
+    Sequence< Reference< XDataSeries > > aSeries( FlattenSequence( aInterpretedData.Series ));
+    for( sal_Int32 nSeriesIdx=0; nSeriesIdx<aSeries.getLength(); ++nSeriesIdx )
+    {
+        try
+        {
+            Reference< data::XDataSource > xSrc( aSeries[nSeriesIdx], uno::UNO_QUERY_THROW );
+            Sequence< Reference< data::XLabeledDataSequence > > aSeq( xSrc->getDataSequences());
+
+            // add all sequences of data series
+            for( sal_Int32 nSeqIdx=0; nSeqIdx<aSeq.getLength(); ++nSeqIdx )
+            {
+                Reference< data::XLabeledDataSequence > xAdd( aSeq[nSeqIdx] );
+
+                // only add if sequence is not yet in the result
+                if( find_if( aResultVec.begin(), aResultVec.end(),
+                             lcl_LabeledSequenceEquals( xAdd )) == aResultVec.end())
+                {
+                    aResultVec.push_back( xAdd );
+                }
+            }
+        }
+        catch( uno::Exception & ex )
+        {
+            ASSERT_EXCEPTION( ex );
+        }
+    }
+
+    // add unused data at end
+    copy( aUnusedDataVec.begin(), aUnusedDataVec.end(), back_inserter( aResultVec ));
+
+    return Reference< data::XDataSource >( new DataSource( ContainerToSequence( aResultVec )));
+}
+
+// convenience methods
+
+// static
+OUString DataInterpreter::GetRole( const Reference< data::XDataSequence > & xSeq )
+{
+    OUString aResult;
+    if( ! xSeq.is())
+        return aResult;
+
+    try
+    {
+        Reference< beans::XPropertySet > xProp( xSeq, uno::UNO_QUERY_THROW );
+        xProp->getPropertyValue( C2U("Role")) >>= aResult;
+    }
+    catch( uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+    return aResult;
+}
+
+// static
+void DataInterpreter::SetRole( const Reference< data::XDataSequence > & xSeq, const OUString & rRole )
+{
+    if( ! xSeq.is())
+        return;
+    try
+    {
+        Reference< beans::XPropertySet > xProp( xSeq, uno::UNO_QUERY_THROW );
+        xProp->setPropertyValue( C2U("Role"), uno::makeAny( rRole ));
+    }
+    catch( uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+// static
+uno::Any DataInterpreter::GetProperty(
+    const Sequence< beans::PropertyValue > & aArguments,
+    const OUString & rName )
+{
+    for( sal_Int32 i=aArguments.getLength(); i--; )
+    {
+        if( aArguments[i].Name.equals( rName ))
+            return aArguments[i].Value;
+    }
+    return uno::Any();
+}
+
+// static
+bool DataInterpreter::HasCategories(
+    const Sequence< beans::PropertyValue > & rArguments,
+    const Sequence< Reference< data::XLabeledDataSequence > > & rData )
+{
+    bool bHasCategories = false;
+
+    if( rArguments.getLength() > 0 )
+        GetProperty( rArguments, C2U(("HasCategories"))) >>= bHasCategories;
+
+    for( sal_Int32 nLSeqIdx=0; ! bHasCategories && nLSeqIdx<rData.getLength(); ++nLSeqIdx )
+        bHasCategories = ( rData[nLSeqIdx].is() &&
+                           GetRole( rData[nLSeqIdx]->getValues()).equalsAsciiL(RTL_CONSTASCII_STRINGPARAM("categories")));
+
+    return bHasCategories;
+}
+
+// ------------------------------------------------------------
+
+Sequence< OUString > DataInterpreter::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 1 );
+    aServices[0] = C2U( "com.sun.star.chart2.DataInterpreter" );
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( DataInterpreter, C2U("com.sun.star.comp.chart2.DataInterpreter"));
+
+} // namespace chart
+
+#if OSL_DEBUG_LEVEL > 1
+namespace
+{
+
+void lcl_ShowDataSource( const Reference< data::XDataSource > & xSource )
+{
+    if( ! xSource.is())
+        return;
+
+    OSL_TRACE( "DataSource in DataInterpreter:" );
+    Sequence< Reference< data::XLabeledDataSequence > > aSequences( xSource->getDataSequences());
+    Reference< beans::XPropertySet > xProp;
+    OUString aId;
+    const sal_Int32 nMax = aSequences.getLength();
+    for( sal_Int32 k = 0; k < nMax; ++k )
+    {
+        if( aSequences[k].is())
+        {
+            OUString aSourceRepr(C2U("<none>"));
+            if( aSequences[k]->getValues().is())
+                aSourceRepr = aSequences[k]->getValues()->getSourceRangeRepresentation();
+            xProp.set( aSequences[k]->getValues(), uno::UNO_QUERY );
+            if( xProp.is() &&
+                ( xProp->getPropertyValue( C2U( "Role" )) >>= aId ))
+            {
+                OSL_TRACE( "  <data sequence %d> Role: %s, Source: %s", k, U2C( aId ), U2C( aSourceRepr ));
+            }
+            else
+            {
+                OSL_TRACE( "  <data sequence %d> unknown Role, Source: %s", k, U2C( aSourceRepr ) );
+            }
+
+            aSourceRepr = C2U("<none>");
+            if( aSequences[k]->getLabel().is())
+                aSourceRepr = OUString( aSequences[k]->getLabel()->getSourceRangeRepresentation());
+            xProp.set( aSequences[k]->getLabel(), uno::UNO_QUERY );
+            if( xProp.is() &&
+                ( xProp->getPropertyValue( C2U( "Role" )) >>= aId ))
+            {
+                OSL_TRACE( "  <data sequence label %d> Role: %s, Source: %s", k, U2C( aId ), U2C( aSourceRepr ));
+            }
+            else
+            {
+                OSL_TRACE( "  <data sequence label %d> unknown Role, Source: %s", k, U2C( aSourceRepr ) );
+            }
+        }
+    }
+}
+
+}
+#endif
