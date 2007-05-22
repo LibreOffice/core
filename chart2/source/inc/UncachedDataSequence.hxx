7352cc9fbc36346611aae7eb36237224456e16d6
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:23:37 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/07/25 12:38:58 bm 1.1.2.11: #i64824# notify change in sequence length from internal data provider to uncached data sequence 2006/07/19 15:55:25 bm 1.1.2.10: #i64824# improvement of the data editor for internal data. Especially the handling of flexible data and series with more than one role 2006/04/05 11:43:20 bm 1.1.2.9: include XModifyBroadcaster (not only implement ...) 2006/04/04 15:58:50 bm 1.1.2.8: implement XModifyBroadcaster 2006/04/04 13:45:51 bm 1.1.2.7: add XModifyBroadcaster to DataSequence 2006/01/23 16:47:08 bm 1.1.2.6: member initialization order 2005/10/11 09:20:35 bm 1.1.2.5: license header change 2005/09/08 12:43:29 bm 1.1.2.4: +generateLabel 2005/08/18 14:07:28 bm 1.1.2.3: provide a CTOR with the Role 2005/08/15 09:30:03 bm 1.1.2.2: add an XDataProvider reference to ensure a long-enough life-time for the data sequences to work properly 2005/08/11 16:02:09 bm 1.1.2.1: classes for internal data

---
+++ chart2/source/inc/UncachedDataSequence.hxx
@@ -0,0 +1,249 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: UncachedDataSequence.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:23:37 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _CHART_UNCACHEDDATASEQUENCE_HXX
+#define _CHART_UNCACHEDDATASEQUENCE_HXX
+
+// helper classes
+#ifndef _CPPUHELPER_COMPBASE8_HXX_
+#include <cppuhelper/compbase8.hxx>
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+#ifndef _COMPHELPER_BROADCASTHELPER_HXX_
+#include <comphelper/broadcasthelper.hxx>
+#endif
+#ifndef _COMPHELPER_PROPERTYCONTAINER_HXX_
+#include <comphelper/propertycontainer.hxx>
+#endif
+#ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
+#include <comphelper/proparrhlp.hxx>
+#endif
+#include "ServiceMacros.hxx"
+
+// interfaces and types
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CHART2_XINTERNALDATAPROVIDER_HPP_
+#include <com/sun/star/chart2/XInternalDataProvider.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CHART2_DATA_XDATASEQUENCE_HPP_
+#include <com/sun/star/chart2/data/XDataSequence.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CHART2_DATA_XNUMERICALDATASEQUENCE_HPP_
+#include <com/sun/star/chart2/data/XNumericalDataSequence.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CHART2_DATA_XTEXTUALDATASEQUENCE_HPP_
+#include <com/sun/star/chart2/data/XTextualDataSequence.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XINDEXREPLACE_HPP_
+#include <com/sun/star/container/XIndexReplace.hpp>
+#endif
+#ifndef _COM_SUN_STAR_CONTAINER_XNAMED_HPP_
+#include <com/sun/star/container/XNamed.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XCLONEABLE_HPP_
+#include <com/sun/star/util/XCloneable.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XMODIFYBROADCASTER_HPP_
+#include <com/sun/star/util/XModifyBroadcaster.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XMODIFIABLE_HPP_
+#include <com/sun/star/util/XModifiable.hpp>
+#endif
+
+#include <vector>
+
+// ____________________
+namespace chart
+{
+
+namespace impl
+{
+typedef ::cppu::WeakComponentImplHelper8<
+    ::com::sun::star::chart2::data::XDataSequence,
+    ::com::sun::star::chart2::data::XNumericalDataSequence,
+    ::com::sun::star::chart2::data::XTextualDataSequence,
+    ::com::sun::star::util::XCloneable,
+    ::com::sun::star::util::XModifiable, // contains util::XModifyBroadcaster
+    ::com::sun::star::container::XIndexReplace,
+    ::com::sun::star::container::XNamed, // for setting a new range representation
+    ::com::sun::star::lang::XServiceInfo >
+    UncachedDataSequence_Base;
+}
+
+class UncachedDataSequence :
+        public ::comphelper::OMutexAndBroadcastHelper,
+        public ::comphelper::OPropertyContainer,
+        public ::comphelper::OPropertyArrayUsageHelper< UncachedDataSequence >,
+        public impl::UncachedDataSequence_Base
+{
+public:
+    /** The referring data provider is held as uno reference to ensure its
+        lifetime is at least as long as the one of this object.
+     */
+    UncachedDataSequence(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::chart2::XInternalDataProvider > & xIntDataProv,
+        const ::rtl::OUString & rRangeRepresentation );
+    UncachedDataSequence(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::chart2::XInternalDataProvider > & xIntDataProv,
+        const ::rtl::OUString & rRangeRepresentation,
+        const ::rtl::OUString & rRole );
+    UncachedDataSequence( const UncachedDataSequence & rSource );
+    virtual ~UncachedDataSequence();
+
+    /// declare XServiceInfo methods
+    APPHELPER_XSERVICEINFO_DECL()
+
+    /// merge XInterface implementations
+    DECLARE_XINTERFACE()
+    /// merge XTypeProvider implementations
+    DECLARE_XTYPEPROVIDER()
+
+protected:
+    // ____ XPropertySet ____
+    /// @see ::com::sun::star::beans::XPropertySet
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo()
+        throw (::com::sun::star::uno::RuntimeException);
+    /// @see ::comphelper::OPropertySetHelper
+    virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
+    /// @see ::comphelper::OPropertyArrayUsageHelper
+    virtual ::cppu::IPropertyArrayHelper* createArrayHelper() const;
+
+    // ____ XDataSequence ____
+    virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any > SAL_CALL getData()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::rtl::OUString SAL_CALL getSourceRangeRepresentation()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL generateLabel(
+        ::com::sun::star::chart2::data::LabelOrigin nLabelOrigin )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::sal_Int32 SAL_CALL getNumberFormatKeyByIndex( ::sal_Int32 nIndex )
+        throw (::com::sun::star::lang::IndexOutOfBoundsException,
+               ::com::sun::star::uno::RuntimeException);
+
+    // ____ XNumericalDataSequence ____
+    /// @see ::com::sun::star::chart::data::XNumericalDataSequence
+    virtual ::com::sun::star::uno::Sequence< double > SAL_CALL getNumericalData() throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XTextualDataSequence ____
+    /// @see ::com::sun::star::chart::data::XTextualDataSequence
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getTextualData() throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XIndexReplace ____
+    virtual void SAL_CALL replaceByIndex( ::sal_Int32 Index, const ::com::sun::star::uno::Any& Element )
+        throw (::com::sun::star::lang::IllegalArgumentException,
+               ::com::sun::star::lang::IndexOutOfBoundsException,
+               ::com::sun::star::lang::WrappedTargetException,
+               ::com::sun::star::uno::RuntimeException);
+
+    // ____ XIndexAccess (base of XIndexReplace) ____
+    virtual ::sal_Int32 SAL_CALL getCount()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Any SAL_CALL getByIndex( ::sal_Int32 Index )
+        throw (::com::sun::star::lang::IndexOutOfBoundsException,
+               ::com::sun::star::lang::WrappedTargetException,
+               ::com::sun::star::uno::RuntimeException);
+
+    // ____ XElementAccess (base of XIndexAccess) ____
+    virtual ::com::sun::star::uno::Type SAL_CALL getElementType()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::sal_Bool SAL_CALL hasElements()
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XNamed (for setting a new range representation) ____
+    virtual ::rtl::OUString SAL_CALL getName()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setName( const ::rtl::OUString& aName )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XCloneable ____
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::util::XCloneable > SAL_CALL createClone()
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XModifiable ____
+    virtual ::sal_Bool SAL_CALL isModified()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setModified( ::sal_Bool bModified )
+        throw (::com::sun::star::beans::PropertyVetoException,
+               ::com::sun::star::uno::RuntimeException);
+
+    // ____ XModifyBroadcaster (base of XModifiable) ____
+    virtual void SAL_CALL addModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    void fireModifyEvent();
+
+    mutable ::osl::Mutex                  m_aMutex;
+
+    // <properties>
+    sal_Int32                                       m_nNumberFormatKey;
+    ::rtl::OUString                                 m_sRole;
+    sal_Bool                                        m_bIsHidden;
+    ::com::sun::star::uno::Sequence< sal_Int32 >    m_aHiddenValues;
+    // </properties>
+
+    /** This method registers all properties.  It should be called by all
+        constructors.
+     */
+    void registerProperties();
+
+private:
+    ::com::sun::star::uno::Reference<
+            ::com::sun::star::chart2::XInternalDataProvider > m_xDataProvider;
+    ::rtl::OUString                 m_aSourceRepresentation;
+    ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >
+        m_xModifyEventForwarder;
+};
+
+}  // namespace chart
+
+
+// _CHART_UNCACHEDDATASEQUENCE_HXX
+#endif
