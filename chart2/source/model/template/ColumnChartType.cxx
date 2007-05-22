e6aab91f8dad2aace265caa19e813274ceb5b4f2
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:46:59 +0000

    INTEGRATION: CWS chart2mst3 (1.2.4); FILE MERGED 2006/10/18 17:13:44 bm 1.2.4.8: RESYNC: (1.3-1.4); FILE MERGED 2005/11/22 14:06:35 iha 1.2.4.7: support BarOverlap and GapWidth 2005/10/07 12:02:35 bm 1.2.4.6: RESYNC: (1.2-1.3); FILE MERGED 2005/04/11 10:00:52 iha 1.2.4.5: defines for servicenames for charttypes (fixed apply to 2nd axis again) 2005/03/30 16:31:12 bm 1.2.4.4: make model cloneable (+first undo implementation) 2004/09/16 14:42:46 bm 1.2.4.3: API simplification 2004/09/16 12:27:27 bm 1.2.4.2: API simplification 2004/04/01 10:53:09 bm 1.2.4.1: XChartType: may return a coordinate system now

--- chart2/source/model/template/ColumnChartType.cxx
+++ chart2/source/model/template/ColumnChartType.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ColumnChartType.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:18:07 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:46:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,24 +36,182 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "ColumnChartType.hxx"
+#include "macros.hxx"
+#include "servicenames_charttypes.hxx"
+#include "PropertyHelper.hxx"
+#include "ContainerHelper.hxx"
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
+#include <com/sun/star/beans/PropertyAttribute.hpp>
+#endif
 
 using namespace ::com::sun::star;
+using ::com::sun::star::uno::Sequence;
+using ::com::sun::star::beans::Property;
+
+//-----------------------------------------------------------------------------
+
+namespace
+{
+
+enum
+{
+    PROP_BARCHARTTYPE_OVERLAP_SEQUENCE,
+    PROP_BARCHARTTYPE_GAPWIDTH_SEQUENCE
+};
+
+void lcl_AddPropertiesToVector(
+    ::std::vector< Property > & rOutProperties )
+{
+    rOutProperties.push_back(
+        Property( C2U( "OverlapSequence" ),
+                  PROP_BARCHARTTYPE_OVERLAP_SEQUENCE,
+                  ::getCppuType( reinterpret_cast< const Sequence< sal_Int32 > * >(0)),
+                  beans::PropertyAttribute::BOUND
+                  | beans::PropertyAttribute::MAYBEDEFAULT ));
+
+    rOutProperties.push_back(
+        Property( C2U( "GapwidthSequence" ),
+                  PROP_BARCHARTTYPE_GAPWIDTH_SEQUENCE,
+                  ::getCppuType( reinterpret_cast< const Sequence< sal_Int32 > * >(0)),
+                  beans::PropertyAttribute::BOUND
+                  | beans::PropertyAttribute::MAYBEDEFAULT ));
+}
+
+void lcl_AddDefaultsToMap(
+    ::chart::tPropertyValueMap & rOutMap )
+{
+    Sequence< sal_Int32 > aSeq(2);
+
+    aSeq[0]=aSeq[1]=0;
+    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_BARCHARTTYPE_OVERLAP_SEQUENCE ));
+    rOutMap[ PROP_BARCHARTTYPE_OVERLAP_SEQUENCE ] =
+        uno::makeAny( aSeq );
+
+    aSeq[0]=aSeq[1]=100;
+    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_BARCHARTTYPE_GAPWIDTH_SEQUENCE ));
+    rOutMap[ PROP_BARCHARTTYPE_GAPWIDTH_SEQUENCE ] =
+        uno::makeAny( aSeq );
+}
+
+const Sequence< Property > & lcl_GetPropertySequence()
+{
+    static Sequence< Property > aPropSeq;
+
+    // /--
+    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+    if( 0 == aPropSeq.getLength() )
+    {
+        // get properties
+        ::std::vector< ::com::sun::star::beans::Property > aProperties;
+        lcl_AddPropertiesToVector( aProperties );
+
+        // and sort them for access via bsearch
+        ::std::sort( aProperties.begin(), aProperties.end(),
+                     ::chart::PropertyNameLess() );
+
+        // transfer result to static Sequence
+        aPropSeq = ::chart::ContainerHelper::ContainerToSequence( aProperties );
+    }
+
+    return aPropSeq;
+}
+
+} // anonymous namespace
 
 namespace chart
 {
 
-ColumnChartType::ColumnChartType( sal_Int32 nDim /* = 2 */ ) :
-        ChartType( nDim )
+ColumnChartType::ColumnChartType(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        ChartType( xContext )
 {}
+
+ColumnChartType::ColumnChartType( const ColumnChartType & rOther ) :
+        ChartType( rOther )
+{
+}
 
 ColumnChartType::~ColumnChartType()
 {}
+
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL ColumnChartType::createClone()
+    throw (uno::RuntimeException)
+{
+    return uno::Reference< util::XCloneable >( new ColumnChartType( *this ));
+}
 
 // ____ XChartType ____
 ::rtl::OUString SAL_CALL ColumnChartType::getChartType()
     throw (uno::RuntimeException)
 {
-    return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.ColumnChart" ));
+    return CHART2_SERVICE_NAME_CHARTTYPE_COLUMN;
 }
+
+// ____ OPropertySet ____
+uno::Any ColumnChartType::GetDefaultValue( sal_Int32 nHandle ) const
+    throw(beans::UnknownPropertyException)
+{
+    static tPropertyValueMap aStaticDefaults;
+
+    // /--
+    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+    if( 0 == aStaticDefaults.size() )
+    {
+        // initialize defaults
+        lcl_AddDefaultsToMap( aStaticDefaults );
+    }
+
+    tPropertyValueMap::const_iterator aFound(
+        aStaticDefaults.find( nHandle ));
+
+    if( aFound == aStaticDefaults.end())
+        return uno::Any();
+
+    return (*aFound).second;
+    // \--
+}
+
+::cppu::IPropertyArrayHelper & SAL_CALL ColumnChartType::getInfoHelper()
+{
+    static ::cppu::OPropertyArrayHelper aArrayHelper( lcl_GetPropertySequence(),
+                                                      /* bSorted = */ sal_True );
+    return aArrayHelper;
+}
+
+
+// ____ XPropertySet ____
+uno::Reference< beans::XPropertySetInfo > SAL_CALL
+    ColumnChartType::getPropertySetInfo()
+    throw (uno::RuntimeException)
+{
+    static uno::Reference< beans::XPropertySetInfo > xInfo;
+
+    // /--
+    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+    if( !xInfo.is())
+    {
+        xInfo = ::cppu::OPropertySetHelper::createPropertySetInfo(
+            getInfoHelper());
+    }
+
+    return xInfo;
+    // \--
+}
+
+//
+
+uno::Sequence< ::rtl::OUString > ColumnChartType::getSupportedServiceNames_Static()
+{
+    uno::Sequence< ::rtl::OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_SERVICE_NAME_CHARTTYPE_COLUMN;
+    aServices[ 1 ] = C2U( "com.sun.star.chart2.ChartType" );
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( ColumnChartType,
+                             C2U( "com.sun.star.comp.chart.ColumnChartType" ));
 
 } //  namespace chart
