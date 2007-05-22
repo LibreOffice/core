456bdb2da5dff08da01413c16d2c5a37ff35f290
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:45:19 +0000

    INTEGRATION: CWS chart2mst3 (1.38.24); FILE MERGED 2007/05/14 20:18:22 bm 1.38.24.9: RESYNC: (1.43-1.44); FILE MERGED 2006/11/27 14:41:11 dr 1.38.24.8: #i10000# merge error 2006/11/24 18:19:50 bm 1.38.24.7: RESYNC: (1.41-1.42); FILE MERGED 2006/10/13 13:32:30 dr 1.38.24.6: RESYNC: (1.40-1.41); FILE MERGED 2006/07/27 13:37:56 dr 1.38.24.5: RESYNC: (1.39-1.40); FILE MERGED 2006/07/11 14:08:29 dr 1.38.24.4: #export of chart line and area formats 2006/06/22 10:45:36 dr 1.38.24.3: set BIFF5 chart anchor, support BIFF5 chart sheets 2006/06/16 08:14:03 dr 1.38.24.2: load BIFF5 charts 2005/11/16 17:00:29 dr 1.38.24.1: #i3997# remove non-working BIFF5 chart import

--- sc/source/filter/excel/excobj.cxx
+++ sc/source/filter/excel/excobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: excobj.cxx,v $
  *
- *  $Revision: 1.44 $
+ *  $Revision: 1.45 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 16:47:09 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:45:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,26 +36,6 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sc.hxx"
 
-#ifndef _COM_SUN_STAR_EMBED_ASPECTS_HPP_
-#include <com/sun/star/embed/Aspects.hpp>
-#endif
-#ifndef _COM_SUN_STAR_FORM_XFORMCOMPONENT_HPP_
-#include <com/sun/star/form/XFormComponent.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_XCONTROLSHAPE_HPP_
-#include <com/sun/star/drawing/XControlShape.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_XSHAPES_HPP_
-#include <com/sun/star/drawing/XShapes.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_XINDEXCONTAINER_HPP_
-#include <com/sun/star/container/XIndexContainer.hpp>
-#endif
-
-#include <toolkit/helper/vclunohelper.hxx>
-
-#include <stdio.h>
-
 #include "scitems.hxx"
 #include <svx/eeitem.hxx>
 
@@ -76,10 +56,7 @@
 #include <svx/outliner.hxx>
 #include <sfx2/objsh.hxx>
 #include <sot/storage.hxx>
-#include <sch/schdll.hxx>
-#include <sch/memchrt.hxx>
 #include <svtools/itemset.hxx>
-#include <svtools/moduleoptions.hxx>
 #include <sfx2/app.hxx>
 #include <svx/editdata.hxx>
 #include <svx/editeng.hxx>
@@ -92,17 +69,16 @@
 
 #include "document.hxx"
 #include "drwlayer.hxx"
-#include "chartarr.hxx"
 #include "editutil.hxx"
 
-#ifndef SC_XLESCHER_HXX
-#include "xlescher.hxx"
-#endif
 #ifndef SC_XLTRACER_HXX
 #include "xltracer.hxx"
 #endif
 #ifndef SC_XILINK_HXX
 #include "xilink.hxx"
+#endif
+#ifndef SC_XIESCHER_HXX
+#include "xiescher.hxx"
 #endif
 
 #include "imp_op.hxx"
@@ -114,20 +90,6 @@
 #ifndef _BGFX_POLYGON_B2DPOLYGON_HXX
 #include <basegfx/polygon/b2dpolygon.hxx>
 #endif
-
-using namespace com::sun::star;
-
-// Object Flags
-
-#define OBJF_SELECTED   0x0001          // ---
-#define OBJF_AUTOSIZE   0x0002          // zellgebunden
-#define OBJF_MOVE       0x0004          // ---
-#define OBJF_LOCKED     0x0010          // ---
-#define OBJF_GROUPED    0x0080          // ---
-#define OBJF_HIDDEN     0x0100          // ---
-#define OBJF_VISIBLE    0x0200          // ---
-#define OBJF_PRINTABLE  0x0400          // ---
-
 
 void ImportExcel::Obj()
 {
@@ -143,60 +105,70 @@ void ImportExcel::Obj()
     bool bBiff5 = GetBiff() == EXC_BIFF5;
     aIn.Ignore( bBiff5 ? 6 : 2 );
 
-    Rectangle aRect( aAnchor.GetRect( rDoc, MAP_100TH_MM ) );
-    Point aTL( aRect.TopLeft() );
-    Point aBR( aRect.BottomRight() );
-
-    SfxItemSet aSet( rDoc.GetDrawLayer()->GetItemPool(), SDRATTR_START, SDRATTR_END );
-    SdrObject* pObj = NULL;
-    switch( nType )
+    if( bBiff5 && (nType == EXC_OBJ_CMO_CHART) )
     {
-        case 0x01:  pObj = LineObj( aSet, aTL, aBR ); break;
-        case 0x02:  pObj = RectObj( aSet, aTL, aBR ); break;
-        case 0x05:  pObj = BeginChartObj( aSet, aTL, aBR ); break;
-        default: pExcRoot->pIR->GetTracer().TraceUnsupportedObjects(); break;
+        maStrm.Seek( EXC_REC_SEEK_TO_BEGIN );
+        GetObjectManager().ReadObj5( maStrm );
     }
-
-    if( pObj )
+    else
     {
-        pObj->ClearMergedItem();
-        pObj->SetMergedItemSetAndBroadcast(aSet);
+        Rectangle aRect( aAnchor.GetRect( rDoc, MAP_100TH_MM ) );
+        Point aTL( aRect.TopLeft() );
+        Point aBR( aRect.BottomRight() );
 
-        pObj->NbcSetLogicRect( aRect );
-        pObj->SetLayer( SC_LAYER_FRONT );
-        rDoc.GetDrawLayer()->GetPage( static_cast<sal_uInt16>(nScTab) )->InsertObject( pObj );
-        if( bBiff5 && aIn.GetRecLeft() )
+        SfxItemSet aSet( rDoc.GetDrawLayer()->GetItemPool(), SDRATTR_START, SDRATTR_END );
+        SdrObject* pObj = NULL;
+        switch( nType )
         {
-            sal_uInt8 nNameLen;
-            aIn >> nNameLen;
-            aIn.Ignore( nNameLen + nMacroLen );
+            case EXC_OBJ_CMO_LINE:      pObj = LineObj( aSet, aTL, aBR );   break;
+            case EXC_OBJ_CMO_RECTANGLE: pObj = RectObj( aSet, aTL, aBR );   break;
+            default: pExcRoot->pIR->GetTracer().TraceUnsupportedObjects();  break;
+        }
+
+        if( pObj )
+        {
+            pObj->NbcSetSnapRect( aRect );
+            pObj->NbcSetLayer( SC_LAYER_FRONT );
+
+            pObj->ClearMergedItem();
+            pObj->SetMergedItemSetAndBroadcast( aSet );
+
+            if( SdrPage* pSdrPage = GetSdrPage( nScTab ) )
+                pSdrPage->InsertObject( pObj );
+            if( bBiff5 && aIn.GetRecLeft() )
+            {
+                sal_uInt8 nNameLen;
+                aIn >> nNameLen;
+                aIn.Ignore( nNameLen + nMacroLen );
+            }
         }
     }
 }
 
 
-void ImportExcel::SetLineStyle( SfxItemSet& rSet, short nColor, short nStyle, short nWidth )
+void ImportExcel::SetLineStyle( SfxItemSet& rSet, sal_uInt16 nColor, sal_uInt16 nStyle, sal_uInt16 nWidth )
 {
-    if( nColor >= 0 )
-        rSet.Put( XLineColorItem( String(), GetPalette().GetColor( nColor ) ) );
-    if( nStyle >= 0 )
-    {
-        XLineStyle eStyle = ( nStyle == 1 || nStyle == 2 )
-                          ? XLINE_DASH : XLINE_SOLID;
-        rSet.Put( XLineStyleItem( eStyle ) );
-    }
-    if( nWidth >= 0 )
-        rSet.Put( XLineWidthItem( nWidth * 40 ) );
+    rSet.Put( XLineColorItem( String(), GetPalette().GetColor( nColor ) ) );
+    XLineStyle eStyle = (nStyle == 255) ? XLINE_NONE : (((nStyle == 1) || (nStyle == 2)) ? XLINE_DASH : XLINE_SOLID);
+    rSet.Put( XLineStyleItem( eStyle ) );
+    rSet.Put( XLineWidthItem( nWidth * 40 ) );
 }
 
 
-void ImportExcel::SetFillStyle( SfxItemSet& rSet, short nBg, short nFg, short nStyle )
+void ImportExcel::SetFillStyle( SfxItemSet& rSet, sal_uInt16 nBackIdx, sal_uInt16 nPattIdx, sal_uInt16 nPattern )
 {
-    if( nBg >= 0 )
-        rSet.Put( XFillColorItem( String(), GetPalette().GetColor( nFg ) ) );
-    if( nStyle >= 0 )
-        // Entweder mit Farbe oder leer, keine Patterns!
-        rSet.Put( XFillStyleItem( nStyle ? XFILL_SOLID : XFILL_NONE ) );
+    if( nPattern == EXC_PATT_NONE )
+    {
+        rSet.Put( XFillStyleItem( XFILL_NONE ) );
+    }
+    else
+    {
+        rSet.Put( XFillStyleItem( XFILL_SOLID ) );
+        Color aPattColor = GetPalette().GetColor( nPattIdx );
+        Color aBackColor = GetPalette().GetColor( nBackIdx );
+        Color aColor = XclTools::GetPatternColor( aPattColor, aBackColor, nPattern );
+        rSet.Put( XFillColorItem( String(), aColor ) );
+    }
 }
 
 
@@ -230,286 +202,3 @@ SdrObject* ImportExcel::RectObj( SfxItemSet& rSet, const Point& rUL, const Point
     return pObj;
 }
 
-
-SdrObject* ImportExcel::BeginChartObj( SfxItemSet&, const Point& rUL, const Point& rLR )
-{
-    BYTE        nBg, nFg, nPat, fAuto1, nLc, nLStyle, nLWeight, fAuto2;
-    UINT16      nFRS;
-    aIn >> nBg >> nFg >> nPat >> fAuto1 >> nLc >> nLStyle
-        >> nLWeight >> fAuto2 >> nFRS;
-    aIn.Ignore( 18 );
-
-    ExcelChartData* pData = new ExcelChartData( pD, rUL, rLR, GetCurrScTab() );
-    SetLineStyle( *pData->pAttrs, nLc, nLStyle, nLWeight );
-    SetFillStyle( *pData->pAttrs, nBg, nFg, nPat );
-    pData->pNext = pChart;
-    pChart = pData;
-    pExcRoot->pIR->GetTracer().TraceUnsupportedObjects();
-    return NULL;
-}
-
-
-void ImportExcel::EndChartObj()
-{
-    DBG_ASSERT( pChart, "Kein Chart definiert!" );
-
-    ExcelChartData*         pData = pChart;
-    if( !pData )
-        {
-            pExcRoot->pIR->GetTracer().TraceUnsupportedObjects();
-        return;
-        }
-
-    pChart = pData->pNext;
-
-    if( pUsedChartFirst )
-        pUsedChartLast->pNext = pData;
-    else
-        pUsedChartFirst = pData;
-
-    pUsedChartLast = pData;
-
-    pData->pNext = NULL;
-}
-
-
-ExcelChartData::ExcelChartData( ScDocument* p, const Point& rUL, const Point& rLR, const SCTAB nBT ) :
-    aRect( rUL, rLR ),
-    pNext( NULL ),
-    nCol1( SCCOL_MAX ),
-    nCol2( 0 ),
-    nRow1( SCROW_MAX ),
-    nRow2( 0 ),
-    nTab1( SCTAB_MAX ),
-    nTab2( 0 ),
-    nBaseTab( nBT ),
-    nObjNum( 0xFFFFFFFF )
-{
-    pAttrs = new SfxItemSet( p->GetDrawLayer()->GetItemPool(), SDRATTR_START, SDRATTR_END );
-}
-
-
-ExcelChartData::~ExcelChartData()
-{
-    delete pAttrs;
-}
-
-
-void ImportExcel::ChartSelection( void )
-{
-    UINT8       nId, nRef;
-    UINT16      nFlags, nFmt, nSize;
-    aIn >> nId >> nRef >> nFlags >> nFmt >> nSize;
-    // Nur Links To Worksheet auswerten
-    if( nRef == 2 && nSize )
-    {
-        // Der AI-Record enthaelt eine 3D Cell Reference (Opcode 0x3A)
-        // oder eine 3D Area Reference (Opcode 0x3B) (oder auch nicht...)
-        UINT8       nOp;
-        UINT16      nLink;
-        UINT16      nTab1 = 0, nTab2 = 0, nRow1 = 0, nRow2 = 0;
-        UINT16      nCol1 = 0, nCol2 = 0;
-        BOOL        bValues = FALSE;
-
-        aIn >> nOp;
-
-        if( GetBiff() == EXC_BIFF5 )
-        {
-            aIn >> nLink;
-
-            if( nLink == 0xFFFF )
-            {
-                aIn.Ignore( 8 );
-                switch( nOp )
-                {
-                    case 0x3A:
-                    {
-                        UINT8   nC;
-                        aIn >> nTab1 >> nTab2 >> nRow1 >> nC;
-                        nRow2 = nRow1;
-                        nCol2 = nCol1 = nC;
-                        bValues = TRUE;
-                    }
-                        break;
-                    case 0x3B:
-                    {
-                        UINT8   nC1, nC2;
-                        aIn >> nTab1 >> nTab2 >> nRow1 >> nRow2 >> nC1 >> nC2;
-                        nCol1 = nC1;
-                        nCol2 = nC2;
-                        bValues = TRUE;
-                    }
-                        break;
-                }
-
-                nRow1 &= 0x3FFF;
-                nRow2 &= 0x3FFF;
-            }
-        }
-        else                                                    // Biff8 (?)
-        {
-            UINT16              nIxti;
-
-            DBG_ASSERT( GetBiff() == EXC_BIFF8,
-                "*ImportExcel::ChartSelection(): Den kenn' ich nicht!" );
-
-            if( nOp == 0x3A || nOp == 0x3B )
-            {
-                if( nOp == 0x3A )
-                {
-                    aIn >> nIxti >> nRow1 >> nCol1;
-                    nCol1 &= 0x3FFF;
-                    nCol2 = nCol1;
-                    nRow2 = nRow1;
-                }
-                else
-                {
-                    aIn >> nIxti >> nRow1 >> nRow2 >> nCol1 >> nCol2;
-                    nCol1 &= 0x3FFF;
-                    nCol2 &= 0x3FFF;
-                }
-
-                SCTAB nScTab1 = 0;
-                SCTAB nScTab2 = 0;
-                if( pExcRoot->pIR->GetLinkManager().GetScTabRange( nScTab1, nScTab2, nIxti ) )
-                    bValues = TRUE;
-                nTab1 = static_cast<UINT16>(nScTab1);
-                nTab2 = static_cast<UINT16>(nScTab2);
-            }
-        }   // Ende Biff8
-
-        if( bValues )
-        {
-            DBG_ASSERT( pChart, "Keine Chartdaten!" );
-            if( pChart )
-            {
-                if( pChart->nRow1 > static_cast<SCROW>(nRow1) )
-                    pChart->nRow1 = static_cast<SCROW>(nRow1);
-                if( pChart->nCol1 > static_cast<SCCOL>(nCol1) )
-                    pChart->nCol1 = static_cast<SCCOL>(nCol1);
-                if( pChart->nTab1 > static_cast<SCTAB>(nTab1) )
-                    pChart->nTab1 = static_cast<SCTAB>(nTab1);
-                if( pChart->nRow2 < static_cast<SCROW>(nRow2) )
-                    pChart->nRow2 = static_cast<SCROW>(nRow2);
-                if( pChart->nCol2 < static_cast<SCCOL>(nCol2) )
-                    pChart->nCol2 = static_cast<SCCOL>(nCol2);
-                if( pChart->nTab2 > static_cast<SCTAB>(nTab2) )
-                    pChart->nTab2 = static_cast<SCTAB>(nTab2);
-            }
-        }
-    }
-}
-
-
-void ImportExcel::ChartSeriesText()
-{
-    UINT16  nId;
-    aIn >> nId;
-    if( pChart )
-        pChart->aLastLabel = aIn.ReadByteString( false );
-}
-
-
-void ImportExcel::ChartObjectLink()
-{
-    UINT16 nType, nVar1, nVar2;
-    aIn >> nType >> nVar1 >> nVar2;
-    if( pChart && pChart->aLastLabel.Len() )
-    {
-        switch( nType )
-        {
-            case 1: pChart->aTitle = pChart->aLastLabel; break;
-            case 2: pChart->aYTitle = pChart->aLastLabel; break;
-            case 3: pChart->aXTitle = pChart->aLastLabel; break;
-            case 7: pChart->aZTitle = pChart->aLastLabel; break;
-        }
-        pChart->aLastLabel.Erase();
-    }
-}
-
-
-#ifdef WNT
-#pragma optimize("",off)
-#endif
-
-void ImportExcel::EndAllChartObjects( void )
-{
-    SfxObjectShell* pSh = pD->GetDocumentShell();
-    if( !pSh ) return;
-
-    ExcelChartData*             p = pUsedChartFirst;
-    ExcelChartData*             pDelete;
-
-    while( p )
-    {
-        if( p->nRow1 <= p->nRow2 && p->nCol1 <= p->nCol2 )
-        {
-            //  wenn Chart nicht installiert ist, darf nicht auf SCH_MOD zugegriffen werden!
-            //! Warnung am Storage setzen?
-            if ( SvtModuleOptions().IsChart() )
-            {
-                sal_Int64 nAspect = embed::Aspects::MSOLE_CONTENT;
-                ::rtl::OUString aName;
-                uno::Reference < embed::XEmbeddedObject > xObj = pSh->GetEmbeddedObjectContainer().CreateEmbeddedObject( SvGlobalName( SO3_SCH_CLASSID ).GetByteSequence(), aName );
-
-                awt::Size aSz = xObj->getVisualAreaSize( nAspect );
-                Size aSize( aSz.Width, aSz.Height );
-                if( aSize.Height() == 0 || aSize.Width() == 0 )
-                {
-                    MapUnit aUnit = VCLUnoHelper::UnoEmbed2VCLMapUnit( xObj->getMapUnit( nAspect ) );
-                    aSize.Width() = 5000;
-                    aSize.Height() = 5000;
-                    aSize = Window::LogicToLogic
-                                ( aSize, MapMode( MAP_100TH_MM ), MapMode( aUnit ) );
-                    aSz.Width = aSize.Width();
-                    aSz.Height = aSize.Height();
-                    xObj->setVisualAreaSize( nAspect, aSz );
-                }
-
-                pD->LimitChartArea( p->nTab1, p->nCol1, p->nRow1, p->nCol2, p->nRow2 );
-
-                SdrOle2Obj* pSdrObj = new SdrOle2Obj( svt::EmbeddedObjectRef( xObj, nAspect ), aName, p->aRect );
-
-                pSdrObj->ClearMergedItem();
-                pSdrObj->SetMergedItemSetAndBroadcast(*p->pAttrs);
-
-                pSdrObj->SetLayer( SC_LAYER_FRONT );
-
-                pD->GetDrawLayer()->GetPage( static_cast<sal_uInt16>(p->nBaseTab) )->InsertObject( pSdrObj );
-
-                pSdrObj->NbcSetLogicRect( p->aRect );
-
-                ScChartArray    aChartObj
-                                ( pD, p->nTab1, p->nCol1, p->nRow1, p->nCol2, p->nRow2, aName );
-
-                SchMemChart*    pMemChart = aChartObj.CreateMemChart();
-                SchDLL::Update( xObj, pMemChart );
-                pSdrObj->GetNewReplacement();
-                delete pMemChart;
-            }
-        }
-
-        pDelete = p;
-        p = p->pNext;
-
-        delete pDelete;
-    }
-
-    pUsedChartFirst = pUsedChartLast = NULL;
-
-    p = pChart;
-    while( p )
-    {
-        pDelete = p;
-        p = p->pNext;
-        delete pDelete;
-    }
-
-    GetDoc().UpdateAllCharts();
-}
-
-
-
-#ifdef WNT
-#pragma optimize("",on)
-#endif
