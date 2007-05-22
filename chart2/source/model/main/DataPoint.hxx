48be255798ac1ebd4f1af510710ffb58ccb11675
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:33:34 +0000

    INTEGRATION: CWS chart2mst3 (1.1.1.1.4); FILE MERGED 2006/11/10 10:25:54 bm 1.1.1.1.4.7: warning removed (wrong GetMutex() function defined) 2005/12/02 15:24:37 bm 1.1.1.1.4.6: implement XServiceInfo 2005/11/07 17:12:43 bm 1.1.1.1.4.5: some missing notifications added 2005/10/11 12:58:55 bm 1.1.1.1.4.4: #i27351# use own mutex to allow cloning 2005/10/07 11:52:19 bm 1.1.1.1.4.3: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/09/26 16:43:29 iha 1.1.1.1.4.2: #i55110# crash clone of datapoints, caused by invalid mutex 2005/03/30 16:31:07 bm 1.1.1.1.4.1: make model cloneable (+first undo implementation)

--- chart2/source/model/main/DataPoint.hxx
+++ chart2/source/model/main/DataPoint.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataPoint.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:55:15 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:33:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,58 +35,126 @@
 #ifndef CHART_DATAPOINT_HXX
 #define CHART_DATAPOINT_HXX
 
-#ifndef _CPPUHELPER_WEAK_HXX_
-#include <cppuhelper/weak.hxx>
-#endif
 #ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
 #endif
+#ifndef _CPPUHELPER_IMPLBASE5_HXX_
+#include <cppuhelper/implbase5.hxx>
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_CONTAINER_XCHILD_HPP_
+#include <com/sun/star/container/XChild.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UTIL_XCLONEABLE_HPP_
+#include <com/sun/star/util/XCloneable.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
 
 #include "OPropertySet.hxx"
+#include "MutexContainer.hxx"
+#include "ModifyListenerHelper.hxx"
+#include "ServiceMacros.hxx"
+
+//for auto_ptr
+#include <memory>
 
 namespace chart
 {
 
+namespace impl
+{
+typedef ::cppu::WeakImplHelper5<
+        ::com::sun::star::container::XChild,
+        ::com::sun::star::util::XCloneable,
+        ::com::sun::star::util::XModifyBroadcaster,
+        ::com::sun::star::util::XModifyListener,
+        ::com::sun::star::lang::XServiceInfo >
+    DataPoint_Base;
+}
+
 class DataPoint :
-        public ::cppu::OWeakObject,
+        public MutexContainer,
+        public impl::DataPoint_Base,
         public ::property::OPropertySet
 {
 public:
-    explicit DataPoint( ::osl::Mutex & _rMutex );
-    DataPoint( ::osl::Mutex & _rMutex,
-               const ::com::sun::star::uno::Reference<
-               ::com::sun::star::beans::XPropertySet > & rParentProperties );
+    DataPoint();
+    DataPoint( const ::com::sun::star::uno::Reference<
+                   ::com::sun::star::beans::XPropertySet > & rParentProperties );
     virtual ~DataPoint();
 
-    // ____ XInterface ____
-    virtual ::com::sun::star::uno::Any SAL_CALL queryInterface(
-        const ::com::sun::star::uno::Type & rType )
-        throw (::com::sun::star::uno::RuntimeException);
-    virtual void SAL_CALL acquire() throw ();
-    virtual void SAL_CALL release() throw ();
+    /// merge XInterface implementations
+     DECLARE_XINTERFACE()
+    /// XServiceInfo declarations
+    APPHELPER_XSERVICEINFO_DECL()
 
 protected:
+    explicit DataPoint( const DataPoint & rOther );
+
     // ____ OPropertySet ____
     virtual ::com::sun::star::uno::Any GetDefaultValue( sal_Int32 nHandle ) const
         throw(::com::sun::star::beans::UnknownPropertyException);
-
     virtual ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelper();
+    virtual void SAL_CALL setFastPropertyValue_NoBroadcast
+        ( sal_Int32 nHandle,
+          const ::com::sun::star::uno::Any& rValue )
+        throw (::com::sun::star::uno::Exception);
 
     // ____ XPropertySet ____
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL
         getPropertySetInfo()
         throw (::com::sun::star::uno::RuntimeException);
 
-    ::osl::Mutex & GetMutex();
+    // ____ XCloneable ____
+    // Note: m_xParentProperties are not cloned!
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::util::XCloneable > SAL_CALL createClone()
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XChild ____
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL getParent()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setParent(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& Parent )
+        throw (::com::sun::star::lang::NoSupportException,
+               ::com::sun::star::uno::RuntimeException);
 
     /** const variant of getInfoHelper()
      */
     ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelperConst() const;
 
+    // ____ XModifyBroadcaster ____
+    virtual void SAL_CALL addModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XModifyListener ____
+    virtual void SAL_CALL modified(
+        const ::com::sun::star::lang::EventObject& aEvent )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XEventListener (base of XModifyListener) ____
+    virtual void SAL_CALL disposing(
+        const ::com::sun::star::lang::EventObject& Source )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ OPropertySet ____
+    virtual void firePropertyChangeEvent();
+
+    void fireModifyEvent();
+
 private:
-    ::osl::Mutex & m_rMutex;
     ::com::sun::star::uno::Reference<
         ::com::sun::star::beans::XPropertySet >   m_xParentProperties;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener > m_xModifyEventForwarder;
 };
 
 } //  namespace chart
