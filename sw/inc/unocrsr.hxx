9c3bb623db12a7c0dcf472251a08ea0dbae7ca63
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 15:21:39 +0000

    INTEGRATION: CWS chart2mst3 (1.3.714); FILE MERGED 2006/10/19 06:43:24 bm 1.3.714.4: RESYNC: (1.5-1.6); FILE MERGED 2005/10/09 01:22:02 bm 1.3.714.3: RESYNC: (1.4-1.5); FILE MERGED 2004/08/30 18:54:21 bm 1.3.714.2: RESYNC: (1.3-1.4); FILE MERGED 2004/06/10 11:56:29 tl 1.3.714.1: #i30052# chart2 support

--- sw/inc/unocrsr.hxx
+++ sw/inc/unocrsr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unocrsr.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:36:41 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 16:21:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -82,6 +82,9 @@ public:
                                     { return bSkipOverHiddenSections; }
     void SetSkipOverHiddenSections( BOOL bFlag )
                                     { bSkipOverHiddenSections = bFlag; }
+
+    // make copy of cursor
+    SwUnoCrsr * Clone() const;
 
     DECL_FIXEDMEMPOOL_NEWDEL( SwUnoCrsr )
 };
