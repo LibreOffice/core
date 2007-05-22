dcf3c5e4e23716611287c215571d6bd60e732df9
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:21:08 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/14 13:44:36 iha 1.1.2.9: #i71411# explicitly save font size as mixed defaults are in use 2007/02/08 21:25:29 iha 1.1.2.8: resync m195 -> m202 2006/03/14 19:05:38 iha 1.1.2.7: #i63008# crash leaving inplace mode while having subtitlesx 2006/03/12 00:55:42 iha 1.1.2.6: merged ChartAPIValueProvider with Chart2ModelContact 2006/02/17 20:57:45 iha 1.1.2.5: enlarge validity of api wrapper objects to lifetime of new chartmodel 2005/10/11 09:19:56 bm 1.1.2.4: license header change 2005/06/09 15:48:08 iha 1.1.2.3: includes changed 2005/05/31 19:07:19 iha 1.1.2.2: create old api without controller 2005/04/19 17:29:13 iha 1.1.2.1: improved old api support (introduced wrappedpropertyset ...)

---
+++ chart2/source/controller/chartapiwrapper/WrappedCharacterHeightProperty.cxx
@@ -0,0 +1,182 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedCharacterHeightProperty.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:21:08 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "WrappedCharacterHeightProperty.hxx"
+#include "macros.hxx"
+#include "RelativeSizeHelper.hxx"
+#include "ReferenceSizePropertyProvider.hxx"
+
+// header for define DBG_ASSERT
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+
+using namespace ::com::sun::star;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Any;
+using ::rtl::OUString;
+
+//.............................................................................
+//.............................................................................
+
+//.............................................................................
+//.............................................................................
+
+namespace chart
+{
+namespace wrapper
+{
+WrappedCharacterHeightProperty_Base::WrappedCharacterHeightProperty_Base(
+                            const OUString& rOuterEqualsInnerName
+                            , ReferenceSizePropertyProvider* pRefSizePropProvider )
+                            : WrappedProperty( rOuterEqualsInnerName, rOuterEqualsInnerName )
+                           , m_pRefSizePropProvider( pRefSizePropProvider )
+{
+}
+WrappedCharacterHeightProperty_Base::~WrappedCharacterHeightProperty_Base()
+{
+}
+
+//static
+void WrappedCharacterHeightProperty::addWrappedProperties( std::vector< WrappedProperty* >& rList
+            , ReferenceSizePropertyProvider* pRefSizePropProvider  )
+{
+    rList.push_back( new WrappedCharacterHeightProperty( pRefSizePropProvider ) );
+    rList.push_back( new WrappedAsianCharacterHeightProperty( pRefSizePropProvider ) );
+    rList.push_back( new WrappedComplexCharacterHeightProperty( pRefSizePropProvider ) );
+}
+
+void WrappedCharacterHeightProperty_Base::setPropertyValue( const Any& rOuterValue, const Reference< beans::XPropertySet >& xInnerPropertySet ) const
+                throw (beans::UnknownPropertyException, beans::PropertyVetoException, lang::IllegalArgumentException, lang::WrappedTargetException, uno::RuntimeException)
+{
+    if(xInnerPropertySet.is())
+    {
+        if( m_pRefSizePropProvider )
+            m_pRefSizePropProvider->setCurrentSizeAsReference();
+        xInnerPropertySet->setPropertyValue( m_aInnerName, rOuterValue );
+    }
+}
+
+Any WrappedCharacterHeightProperty_Base::getPropertyValue( const Reference< beans::XPropertySet >& xInnerPropertySet ) const
+                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException)
+{
+    Any aRet;
+    if( xInnerPropertySet.is() )
+    {
+        aRet = xInnerPropertySet->getPropertyValue( m_aInnerName );
+        float fHeight;
+        if( aRet >>= fHeight )
+        {
+            if( m_pRefSizePropProvider )
+            {
+                awt::Size aReferenceSize;
+                if( m_pRefSizePropProvider->getReferenceSize() >>= aReferenceSize )
+                {
+                    awt::Size aCurrentSize = m_pRefSizePropProvider->getCurrentSizeForReference();
+                    aRet <<= static_cast< float >(
+                            RelativeSizeHelper::calculate( fHeight, aReferenceSize, aCurrentSize ));
+                }
+            }
+        }
+    }
+    return aRet;
+}
+
+Any WrappedCharacterHeightProperty_Base::getPropertyDefault( const Reference< beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException)
+{
+    Any aRet;
+    if( xInnerPropertyState.is() )
+    {
+        aRet = xInnerPropertyState->getPropertyDefault( m_aInnerName );
+    }
+    return aRet;
+}
+
+beans::PropertyState WrappedCharacterHeightProperty_Base::getPropertyState( const Reference< beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (beans::UnknownPropertyException, uno::RuntimeException)
+{
+    return beans::PropertyState_DIRECT_VALUE;
+}
+
+Any WrappedCharacterHeightProperty_Base::convertInnerToOuterValue( const Any& rInnerValue ) const
+{
+    OSL_ASSERT("should not be used: WrappedCharacterHeightProperty_Base::convertInnerToOuterValue - check if you miss data");
+    return rInnerValue;
+}
+Any WrappedCharacterHeightProperty_Base::convertOuterToInnerValue( const Any& rOuterValue ) const
+{
+    OSL_ASSERT("should not be used: WrappedCharacterHeightProperty_Base::convertOuterToInnerValue - check if you miss data");
+    return rOuterValue;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+WrappedCharacterHeightProperty::WrappedCharacterHeightProperty( ReferenceSizePropertyProvider* pRefSizePropProvider )
+        : WrappedCharacterHeightProperty_Base( C2U( "CharHeight" ), pRefSizePropProvider )
+{
+}
+WrappedCharacterHeightProperty::~WrappedCharacterHeightProperty()
+{
+}
+
+//-----------------------------------------------------------------------------
+
+WrappedAsianCharacterHeightProperty::WrappedAsianCharacterHeightProperty( ReferenceSizePropertyProvider* pRefSizePropProvider )
+        : WrappedCharacterHeightProperty_Base( C2U( "CharHeightAsian" ), pRefSizePropProvider )
+{
+}
+WrappedAsianCharacterHeightProperty::~WrappedAsianCharacterHeightProperty()
+{
+}
+
+//-----------------------------------------------------------------------------
+
+WrappedComplexCharacterHeightProperty::WrappedComplexCharacterHeightProperty( ReferenceSizePropertyProvider* pRefSizePropProvider )
+        : WrappedCharacterHeightProperty_Base( C2U( "CharHeightComplex" ), pRefSizePropProvider )
+{
+}
+WrappedComplexCharacterHeightProperty::~WrappedComplexCharacterHeightProperty()
+{
+}
+
+} //namespace wrapper
+} //namespace chart
+//.............................................................................
