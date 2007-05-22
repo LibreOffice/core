92e6ed4f54c4ef62a48caca0789cc2b9c47bd30b
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:24:18 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/02/17 20:57:52 iha 1.1.2.4: enlarge validity of api wrapper objects to lifetime of new chartmodel 2005/10/11 09:20:01 bm 1.1.2.3: license header change 2005/05/09 16:27:46 iha 1.1.2.2: changed contructor parameter 2005/04/22 18:51:17 iha 1.1.2.1: old api support - statistic properties

---
+++ chart2/source/controller/chartapiwrapper/WrappedStatisticProperties.hxx
@@ -0,0 +1,64 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedStatisticProperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:24:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_WRAPPED_STATISTIC_PROPERTIES_HXX
+#define CHART_WRAPPED_STATISTIC_PROPERTIES_HXX
+
+#include "WrappedProperty.hxx"
+#include "Chart2ModelContact.hxx"
+
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+//.............................................................................
+namespace chart
+{
+namespace wrapper
+{
+
+class WrappedStatisticProperties
+{
+public:
+    static void addProperties( ::std::vector< ::com::sun::star::beans::Property > & rOutProperties );
+    static void addWrappedPropertiesForSeries( std::vector< WrappedProperty* >& rList );
+    static void addWrappedPropertiesForDiagram( std::vector< WrappedProperty* >& rList
+                    , ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact );
+};
+
+} //namespace wrapper
+} //namespace chart
+//.............................................................................
+
+// CHART_WRAPPED_STATISTIC_PROPERTIES_HXX
+#endif
