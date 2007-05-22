d4d6aa1cb87598494bd1a2a5cdd8c029720cec51
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:55:25 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2005/10/11 09:20:23 bm 1.1.2.2: license header change 2004/05/07 16:20:17 iha 1.1.2.1: #i20344# moved here from dlg_DataSource

---
+++ chart2/source/controller/inc/RangeSelectionButton.hxx
@@ -0,0 +1,63 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: RangeSelectionButton.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:55:25 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_RANGESELECTIONBUTTON_HXX
+#define _CHART2_RANGESELECTIONBUTTON_HXX
+
+// header for Class ImageButton
+#ifndef _SV_BUTTON_HXX
+#include <vcl/button.hxx>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+
+class RangeSelectionButton : public ImageButton
+{
+public:
+    RangeSelectionButton( Window* pParent, const ResId& rResId );
+    virtual ~RangeSelectionButton();
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
