7bef16295c41bd3064613b658761bc6695c34ac1
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:45:53 +0000

    INTEGRATION: CWS chart2mst3 (1.2.4); FILE MERGED 2006/10/18 17:13:18 bm 1.2.4.31: RESYNC: (1.3-1.4); FILE MERGED 2006/04/10 12:25:12 iha 1.2.4.30: api restructure axis, grids, scales and increments 2005/12/21 21:29:22 iha 1.2.4.29: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/29 09:52:50 bm 1.2.4.28: #i58588# make m_xModifyEventForwarder protected for derived classes to be able to listen to children 2005/11/25 16:21:03 iha 1.2.4.27: correct labels at z axis for deep stacking 2005/11/15 18:32:19 bm 1.2.4.26: copy CTOR: add as listener to data series 2005/11/14 13:46:58 iha 1.2.4.25: fixed category axis for created cooredinate system 2005/11/07 20:54:28 iha 1.2.4.24: fixed crash during switching of charttype - remove modify listeners from removed series 2005/11/07 17:12:44 bm 1.2.4.23: some missing notifications added 2005/10/24 11:06:48 iha 1.2.4.22: coordinate system restructure 2005/10/13 17:38:59 iha 1.2.4.21: renamed BoundedCoordinateSystem to CoordinateSystem 2005/10/07 12:01:46 bm 1.2.4.20: RESYNC: (1.2-1.3); FILE MERGED 2005/09/23 15:25:06 iha 1.2.4.19: #i55000# crash caused by dead reference to mutex 2005/09/16 16:50:11 iha 1.2.4.18: no creation of dummy categories in model - instead trigger generation of missing labels and categories in view 2005/08/03 16:36:22 bm 1.2.4.17: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/04/15 14:50:56 bm 1.2.4.16: use vector instead of map to avoid sorting of data series 2005/03/30 16:31:11 bm 1.2.4.15: make model cloneable (+first undo implementation) 2004/09/17 11:12:07 bm 1.2.4.14: setDataSeries: clear vector before adding content 2004/09/17 09:00:04 bm 1.2.4.13: DataSeries is no XIdentifiable, but has a property "Identifier" 2004/09/16 17:02:21 bm 1.2.4.12: API simplification 2004/09/16 16:53:43 bm 1.2.4.11: API simplification 2004/09/16 16:51:14 bm 1.2.4.10: API simplification 2004/09/16 14:42:45 bm 1.2.4.9: API simplification 2004/09/16 13:21:37 bm 1.2.4.8: API simplification 2004/09/16 12:27:26 bm 1.2.4.7: API simplification 2004/09/15 17:32:04 bm 1.2.4.6: API simplification 2004/06/29 12:26:33 bm 1.2.4.5: XChartTypeTemplate changes 2004/05/27 17:41:38 bm 1.2.4.4: error-bars not supported yet 2004/04/01 10:53:08 bm 1.2.4.3: XChartType: may return a coordinate system now 2004/03/02 09:49:18 bm 1.2.4.2: XChartTypeInterface changed 2004/02/13 16:51:39 bm 1.2.4.1: join from changes on branch bm_post_chart01

--- chart2/source/model/template/ChartType.cxx
+++ chart2/source/model/template/ChartType.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChartType.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:17:25 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:45:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,8 +37,17 @@
 #include "precompiled_chart2.hxx"
 #include "ChartType.hxx"
 #include "PropertyHelper.hxx"
-#include "algohelper.hxx"
+#include "CommonFunctors.hxx"
 #include "macros.hxx"
+#include "CartesianCoordinateSystem.hxx"
+#include "Scaling.hxx"
+#include "CloneHelper.hxx"
+#include "AxisIndexDefines.hxx"
+#include "ContainerHelper.hxx"
+
+#ifndef _COM_SUN_STAR_CHART2_AXISTYPE_HPP_
+#include <com/sun/star/chart2/AxisType.hpp>
+#endif
 
 #ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
@@ -53,110 +62,191 @@ using ::com::sun::star::uno::Reference;
 using ::com::sun::star::uno::Any;
 using ::osl::MutexGuard;
 
-namespace
-{
-
-enum
-{
-    PROP_CHARTTYPE_DIMENSION
-};
-
-void lcl_AddPropertiesToVector(
-    ::std::vector< Property > & rOutProperties )
-{
-    rOutProperties.push_back(
-        Property( C2U( "Dimension" ),
-                  PROP_CHARTTYPE_DIMENSION,
-                  ::getCppuType( reinterpret_cast< const sal_Int32 * >(0)),
-                  beans::PropertyAttribute::BOUND
-                  | beans::PropertyAttribute::MAYBEDEFAULT ));
-}
-
-void lcl_AddDefaultsToMap(
-    ::chart::helper::tPropertyValueMap & rOutMap )
-{
-    // must match default in CTOR!
-    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_CHARTTYPE_DIMENSION ));
-    rOutMap[ PROP_CHARTTYPE_DIMENSION ] =
-        uno::makeAny( sal_Int32( 2 ) );
-}
-
-const Sequence< Property > & lcl_GetPropertySequence()
-{
-    static Sequence< Property > aPropSeq;
-
-    // /--
-    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
-    if( 0 == aPropSeq.getLength() )
-    {
-        // get properties
-        ::std::vector< ::com::sun::star::beans::Property > aProperties;
-        lcl_AddPropertiesToVector( aProperties );
-
-        // and sort them for access via bsearch
-        ::std::sort( aProperties.begin(), aProperties.end(),
-                     ::chart::helper::PropertyNameLess() );
-
-        // transfer result to static Sequence
-        aPropSeq = ::chart::helper::VectorToSequence( aProperties );
-    }
-
-    return aPropSeq;
-}
-
-} // anonymous namespace
-
 namespace chart
 {
 
-ChartType::ChartType( sal_Int32 nDim /* = 2 */ ) :
-        ::property::OPropertySet( m_aMutex )
+ChartType::ChartType(
+    const Reference< uno::XComponentContext > & xContext ) :
+        ::property::OPropertySet( m_aMutex ),
+        m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex )),
+        m_xContext( xContext ),
+        m_bNotifyChanges( true )
+{}
+
+ChartType::ChartType( const ChartType & rOther ) :
+        ::property::OPropertySet( rOther, m_aMutex ),
+    m_xModifyEventForwarder( new ModifyListenerHelper::ModifyEventForwarder( m_aMutex )),
+    m_xContext( rOther.m_xContext ),
+    m_bNotifyChanges( true )
 {
-    if( nDim != 2 )
-        setFastPropertyValue_NoBroadcast( PROP_CHARTTYPE_DIMENSION, uno::makeAny( nDim ));
+    CloneHelper::CloneRefVector< Reference< chart2::XDataSeries > >( rOther.m_aDataSeries, m_aDataSeries );
+    ModifyListenerHelper::addListenerToAllElements( m_aDataSeries, m_xModifyEventForwarder );
 }
 
 ChartType::~ChartType()
-{}
-
-Sequence< OUString > ChartType::getSupportedServiceNames_Static()
 {
-    Sequence< OUString > aServices( 2 );
+    ModifyListenerHelper::removeListenerFromAllElements( m_aDataSeries, m_xModifyEventForwarder );
+    m_aDataSeries.clear();
+}
 
-    aServices[ 0 ] = C2U( "com.sun.star.chart2.ChartType" );
-    aServices[ 1 ] = C2U( "com.sun.star.beans.PropertySet" );
-    return aServices;
+Reference< uno::XComponentContext > ChartType::GetComponentContext() const
+{
+    return m_xContext;
+}
+
+// ____ XChartType ____
+Reference< chart2::XCoordinateSystem > SAL_CALL
+    ChartType::createCoordinateSystem( ::sal_Int32 DimensionCount )
+    throw (lang::IllegalArgumentException,
+           uno::RuntimeException)
+{
+    Reference< chart2::XCoordinateSystem > xResult(
+        new CartesianCoordinateSystem(
+            GetComponentContext(), DimensionCount, /* bSwapXAndYAxis */ sal_False ));
+
+    for( sal_Int32 i=0; i<DimensionCount; ++i )
+    {
+        Reference< chart2::XAxis > xAxis( xResult->getAxisByDimension( i, MAIN_AXIS_INDEX ) );
+        if( !xAxis.is() )
+        {
+            OSL_ENSURE(false,"a created coordinate system should have an axis for each dimension");
+            continue;
+        }
+
+        chart2::ScaleData aScaleData = xAxis->getScaleData();
+        aScaleData.Orientation = chart2::AxisOrientation_MATHEMATICAL;
+        aScaleData.Scaling = new LinearScaling( 1.0, 0.0 );
+
+        switch( i )
+        {
+            case 0: aScaleData.AxisType = chart2::AxisType::CATEGORY; break;
+            case 2: aScaleData.AxisType = chart2::AxisType::SERIES; break;
+            default: aScaleData.AxisType = chart2::AxisType::REALNUMBER; break;
+        }
+
+        xAxis->setScaleData( aScaleData );
+    }
+
+    return xResult;
+}
+
+Sequence< OUString > SAL_CALL ChartType::getSupportedMandatoryRoles()
+    throw (uno::RuntimeException)
+{
+    static Sequence< OUString > aDefaultSeq;
+
+    if( aDefaultSeq.getLength() == 0 )
+    {
+        aDefaultSeq.realloc( 2 );
+        aDefaultSeq[0] = C2U( "label" );
+        aDefaultSeq[1] = C2U( "values-y" );
+    }
+
+    return aDefaultSeq;
+}
+
+Sequence< OUString > SAL_CALL ChartType::getSupportedOptionalRoles()
+    throw (uno::RuntimeException)
+{
+    static Sequence< OUString > aDefaultOptRolesSeq;
+
+//     if( aDefaultOptRolesSeq.getLength() == 0 )
+//     {
+//         aDefaultOptRolesSeq.realloc( 1 );
+//         aDefaultOptRolesSeq[0] = C2U( "error-bars-y" );
+//     }
+
+    return aDefaultOptRolesSeq;
+}
+
+OUString SAL_CALL ChartType::getRoleOfSequenceForSeriesLabel()
+    throw (uno::RuntimeException)
+{
+    return C2U( "values-y" );
+}
+
+void ChartType::impl_addDataSeriesWithoutNotification(
+        const Reference< chart2::XDataSeries >& xDataSeries )
+{
+    if( ::std::find( m_aDataSeries.begin(), m_aDataSeries.end(), xDataSeries )
+        != m_aDataSeries.end())
+        throw lang::IllegalArgumentException();
+
+    m_aDataSeries.push_back( xDataSeries );
+    ModifyListenerHelper::addListener( xDataSeries, m_xModifyEventForwarder );
+}
+
+// ____ XDataSeriesContainer ____
+void SAL_CALL ChartType::addDataSeries( const Reference< chart2::XDataSeries >& xDataSeries )
+    throw (lang::IllegalArgumentException,
+           uno::RuntimeException)
+{
+    impl_addDataSeriesWithoutNotification( xDataSeries );
+    fireModifyEvent();
+}
+
+void SAL_CALL ChartType::removeDataSeries( const Reference< chart2::XDataSeries >& xDataSeries )
+    throw (container::NoSuchElementException,
+           uno::RuntimeException)
+{
+    if( !xDataSeries.is())
+        throw container::NoSuchElementException();
+
+    tDataSeriesContainerType::iterator aIt(
+            ::std::find( m_aDataSeries.begin(), m_aDataSeries.end(), xDataSeries ) );
+
+    if( aIt == m_aDataSeries.end())
+        throw container::NoSuchElementException(
+            C2U( "The given series is no element of this charttype" ),
+            static_cast< uno::XWeak * >( this ));
+
+    ModifyListenerHelper::removeListener( xDataSeries, m_xModifyEventForwarder );
+    m_aDataSeries.erase( aIt );
+    fireModifyEvent();
+}
+
+Sequence< Reference< chart2::XDataSeries > > SAL_CALL ChartType::getDataSeries()
+    throw (uno::RuntimeException)
+{
+    return ContainerHelper::ContainerToSequence( m_aDataSeries );
+}
+
+void SAL_CALL ChartType::setDataSeries( const Sequence< Reference< chart2::XDataSeries > >& aDataSeries )
+    throw (lang::IllegalArgumentException,
+           uno::RuntimeException)
+{
+    m_bNotifyChanges = false;
+    try
+    {
+        Sequence< Reference< chart2::XDataSeries > > aOldSeries( this->getDataSeries() );
+        for( sal_Int32 nN=0; nN<aOldSeries.getLength(); ++nN )
+            ModifyListenerHelper::removeListener( aOldSeries[nN], m_xModifyEventForwarder );
+        m_aDataSeries.clear();
+
+        for( sal_Int32 i=0; i<aDataSeries.getLength(); ++i )
+            impl_addDataSeriesWithoutNotification( aDataSeries[i] );
+    }
+    catch( ... )
+    {
+        m_bNotifyChanges = true;
+        throw;
+    }
+    m_bNotifyChanges = true;
+    fireModifyEvent();
 }
 
 // ____ OPropertySet ____
 uno::Any ChartType::GetDefaultValue( sal_Int32 nHandle ) const
     throw(beans::UnknownPropertyException)
 {
-    static helper::tPropertyValueMap aStaticDefaults;
-
-    // /--
-    ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
-    if( 0 == aStaticDefaults.size() )
-    {
-        // initialize defaults
-        lcl_AddDefaultsToMap( aStaticDefaults );
-    }
-
-    helper::tPropertyValueMap::const_iterator aFound(
-        aStaticDefaults.find( nHandle ));
-
-    if( aFound == aStaticDefaults.end())
-        return uno::Any();
-
-    return (*aFound).second;
-    // \--
+    return uno::Any();
 }
 
 // ____ OPropertySet ____
 ::cppu::IPropertyArrayHelper & SAL_CALL ChartType::getInfoHelper()
 {
-    static ::cppu::OPropertyArrayHelper aArrayHelper( lcl_GetPropertySequence(),
-                                                      /* bSorted = */ sal_True );
+    static ::cppu::OPropertyArrayHelper aArrayHelper(
+        Sequence< beans::Property >(), /* bSorted */ sal_True );
 
     return aArrayHelper;
 }
@@ -181,17 +271,66 @@ uno::Reference< beans::XPropertySetInfo > SAL_CALL
     // \--
 }
 
+// ____ XModifyBroadcaster ____
+void SAL_CALL ChartType::addModifyListener( const uno::Reference< util::XModifyListener >& aListener )
+    throw (uno::RuntimeException)
+{
+    try
+    {
+        uno::Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->addModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+void SAL_CALL ChartType::removeModifyListener( const uno::Reference< util::XModifyListener >& aListener )
+    throw (uno::RuntimeException)
+{
+    try
+    {
+        uno::Reference< util::XModifyBroadcaster > xBroadcaster( m_xModifyEventForwarder, uno::UNO_QUERY_THROW );
+        xBroadcaster->removeModifyListener( aListener );
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
+}
+
+// ____ XModifyListener ____
+void SAL_CALL ChartType::modified( const lang::EventObject& aEvent )
+    throw (uno::RuntimeException)
+{
+    m_xModifyEventForwarder->modified( aEvent );
+}
+
+// ____ XEventListener (base of XModifyListener) ____
+void SAL_CALL ChartType::disposing( const lang::EventObject& Source )
+    throw (uno::RuntimeException)
+{
+    // nothing
+}
+
+// ____ OPropertySet ____
+void ChartType::firePropertyChangeEvent()
+{
+    fireModifyEvent();
+}
+
+void ChartType::fireModifyEvent()
+{
+    if( m_bNotifyChanges )
+        m_xModifyEventForwarder->modified( lang::EventObject( static_cast< uno::XWeak* >( this )));
+}
+
 // ================================================================================
 
 using impl::ChartType_Base;
 
 IMPLEMENT_FORWARD_XINTERFACE2( ChartType, ChartType_Base, ::property::OPropertySet )
 IMPLEMENT_FORWARD_XTYPEPROVIDER2( ChartType, ChartType_Base, ::property::OPropertySet )
-
-// do this in derived classes!
-
-// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
-APPHELPER_XSERVICEINFO_IMPL( ChartType,
-                             C2U( "com.sun.star.comp.chart.ChartType" ));
 
 } //  namespace chart
