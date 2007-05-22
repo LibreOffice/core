ad74d0be5959b3dbd114e4c4806b05899b101b52
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:44:24 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/02 22:18:23 iha 1.1.2.10: some resource changes according to wizard spec 2006/11/10 21:03:06 iha 1.1.2.9: prevent immidiate redraw in wizard and according dialogs (important for charts with many data) 2006/08/21 17:03:45 iha 1.1.2.8: #i46521# replace modal x value sorting dialog by a checkbox in the chartwizard; perform sorting in view only and not in the cached chart data (as there is no cached data in the model anymore) 2006/03/10 10:20:41 iha 1.1.2.7: don't use leading underscore 2005/11/14 12:07:59 iha 1.1.2.6: keep parameter while switching between charttypes in charttype page 2005/10/11 09:20:15 bm 1.1.2.5: license header change 2004/05/27 15:52:19 iha 1.1.2.4: #i20344# provide charttype template; added parameter bDoLiveUpdate 2004/05/05 20:50:46 iha 1.1.2.3: #i20344# FixedLine on top 2004/04/19 15:17:40 iha 1.1.2.2: #i20344# enable extra controls; removed line count here 2004/04/07 21:29:32 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/dialogs/tp_ChartType.hxx
@@ -0,0 +1,148 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_ChartType.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:44:24 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_TP_CHARTTYPE_HXX
+#define _CHART2_TP_CHARTTYPE_HXX
+
+#include <vector>
+
+#include "ChartTypeDialogController.hxx"
+#include "ChartTypeTemplateProvider.hxx"
+#include "TimerTriggeredControllerLock.hxx"
+
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+#ifndef _SVTOOLS_WIZARDMACHINE_HXX_
+#include <svtools/wizardmachine.hxx>
+#endif
+
+#ifndef _VALUESET_HXX
+#include <svtools/valueset.hxx>
+#endif
+#ifndef _SV_FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
+// header for class MetricField
+#ifndef _SV_FIELD_HXX
+#include <vcl/field.hxx>
+#endif
+// header for class ListBox
+#ifndef _SV_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+class ChartTypeResourceGroup;
+class AxisTypeResourceGroup;
+class Dim3DLookResourceGroup;
+class StackingResourceGroup;
+class SplineResourceGroup;
+class GeometryResourceGroup;
+class ChartTypeParameter;
+class SortByXValuesResourceGroup;
+
+class ChartTypeTabPage : public ResourceChangeListener, public svt::OWizardPage, public ChartTypeTemplateProvider
+{
+public:
+    ChartTypeTabPage( Window* pParent
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::chart2::XChartDocument >& xChartModel
+                , const ::com::sun::star::uno::Reference<
+                ::com::sun::star::uno::XComponentContext >& xContext
+                , bool bDoLiveUpdate, bool bHideDescription = false );
+    virtual ~ChartTypeTabPage();
+
+    virtual void        initializePage();
+    virtual sal_Bool    commitPage( COMMIT_REASON eReason );
+
+    virtual ::com::sun::star::uno::Reference<
+                ::com::sun::star::chart2::XChartTypeTemplate > getCurrentTemplate() const;
+
+protected:
+    ChartTypeDialogController* getSelectedMainType();
+    void showAllControls( ChartTypeDialogController& rTypeController );
+    void fillAllControls( const ChartTypeParameter& rParameter, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xTemplateProps=::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >() );
+    ChartTypeParameter getCurrentParamter() const;
+
+    virtual void stateChanged( ChangingResource* pResource );
+
+    void commitToModel( const ChartTypeParameter& rParameter );
+
+    DECL_LINK( SelectMainTypeHdl, void* );
+    DECL_LINK( SelectSubTypeHdl, void* );
+
+protected:
+    FixedText   m_aFT_ChooseType;
+    ListBox     m_aMainTypeList;
+    ValueSet    m_aSubTypeList;
+
+    AxisTypeResourceGroup*      m_pAxisTypeResourceGroup;
+    Dim3DLookResourceGroup*     m_pDim3DLookResourceGroup;
+    StackingResourceGroup*      m_pStackingResourceGroup;
+    SplineResourceGroup*        m_pSplineResourceGroup;
+    GeometryResourceGroup*      m_pGeometryResourceGroup;
+    SortByXValuesResourceGroup* m_pSortByXValuesResourceGroup;
+
+    long    m_nYTopPos;//top y position of the different ResourceGroups (AxisTypeResourceGroup, etc)
+
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::chart2::XChartDocument >   m_xChartModel;
+    ::com::sun::star::uno::Reference<
+                       ::com::sun::star::uno::XComponentContext >    m_xCC;
+
+    ::std::vector< ChartTypeDialogController* > m_aChartTypeDialogControllerList;
+    ChartTypeDialogController*                  m_pCurrentMainType;
+
+    sal_Int32 m_nChangingCalls;
+    bool      m_bDoLiveUpdate;
+
+    TimerTriggeredControllerLock   m_aTimerTriggeredControllerLock;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
