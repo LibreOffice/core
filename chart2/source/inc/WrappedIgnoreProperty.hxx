5c317157c887b7d702dfcd5daebc11983b5884f2
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:24:36 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2005/10/11 09:20:36 bm 1.1.2.2: license header change 2005/07/14 15:27:29 iha 1.1.2.1: cleanup WrappedIgnoreProperty Issue number: Submitted by: Reviewed by:

---
+++ chart2/source/inc/WrappedIgnoreProperty.hxx
@@ -0,0 +1,87 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedIgnoreProperty.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:24:36 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_WRAPPED_IGNORE_PROPERTY_HXX
+#define CHART_WRAPPED_IGNORE_PROPERTY_HXX
+
+#include "WrappedProperty.hxx"
+
+#include <vector>
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+class WrappedIgnoreProperty : public WrappedProperty
+{
+public:
+    WrappedIgnoreProperty( const ::rtl::OUString& rOuterName, const ::com::sun::star::uno::Any& rDefaultValue );
+    virtual ~WrappedIgnoreProperty();
+
+    virtual void setPropertyValue( const ::com::sun::star::uno::Any& rOuterValue, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Any getPropertyValue( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual void setPropertyToDefault( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Any getPropertyDefault( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::beans::PropertyState getPropertyState( const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyState >& xInnerPropertyState ) const
+                        throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+
+protected:
+    ::com::sun::star::uno::Any     m_aDefaultValue;
+};
+
+class WrappedIgnoreProperties
+{
+public:
+    static void addIgnoreLineProperties( std::vector< WrappedProperty* >& rList );
+
+    static void addIgnoreFillProperties( std::vector< WrappedProperty* >& rList );
+    static void addIgnoreFillProperties_without_BitmapProperties( std::vector< WrappedProperty* >& rList );
+    static void addIgnoreFillProperties_only_BitmapProperties( std::vector< WrappedProperty* >& rList );
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+
+// CHART_WRAPPED_IGNORE_PROPERTY_HXX
+#endif
