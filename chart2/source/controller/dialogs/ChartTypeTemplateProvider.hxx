aa35ab64f2b0ec34aa99c5d5cebe117cc7a22def
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:27:11 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2005/10/11 09:20:04 bm 1.1.2.2: license header change 2004/05/27 15:50:59 iha 1.1.2.1: #i20344# provide charttype template - needed in case no data is selected at all or in case the dialog should not be in liveupdate mode

---
+++ chart2/source/controller/dialogs/ChartTypeTemplateProvider.hxx
@@ -0,0 +1,60 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ChartTypeTemplateProvider.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:27:11 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_DIALOGS_TEMPLATEPROVIDER_HXX
+#define _CHART2_DIALOGS_TEMPLATEPROVIDER_HXX
+
+#include <com/sun/star/chart2/XChartTypeTemplate.hpp>
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+
+class ChartTypeTemplateProvider
+{
+public:
+    virtual ::com::sun::star::uno::Reference<
+                ::com::sun::star::chart2::XChartTypeTemplate > getCurrentTemplate() const =0;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
