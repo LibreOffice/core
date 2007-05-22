0d5ae721309494fb01f409c71a574b9ab316f6c8
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:13:34 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2005/10/11 09:20:32 bm 1.1.2.3: license header change 2005/08/03 16:36:16 bm 1.1.2.2: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2004/07/01 13:35:52 iha 1.1.2.1: helper for pie chart

---
+++ chart2/source/inc/ColorPerPointHelper.hxx
@@ -0,0 +1,74 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ColorPerPointHelper.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:13:34 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _CHART2_COLORPERPOINTHELPER_HXX
+#define _CHART2_COLORPERPOINTHELPER_HXX
+
+#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
+#include <com/sun/star/beans/XPropertySet.hpp>
+#endif
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+//-----------------------------------------------------------------------------
+/**
+*/
+
+class ColorPerPointHelper
+{
+public:
+    static bool hasPointOwnColor(
+        const ::com::sun::star::uno::Reference<
+              ::com::sun::star::beans::XPropertySet >& xDataSeriesProperties
+        , sal_Int32 nPointIndex
+        , const ::com::sun::star::uno::Reference<
+              ::com::sun::star::beans::XPropertySet >& xDataPointProperties //may be NULL this is just for performance
+         );
+
+    // returns true if AttributedDataPoints contains nPointIndex and the
+    // property Color is DEFAULT
+    static bool hasPointOwnProperties(
+        const ::com::sun::star::uno::Reference<
+              ::com::sun::star::beans::XPropertySet >& xSeriesProperties
+        , sal_Int32 nPointIndex );
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+#endif
