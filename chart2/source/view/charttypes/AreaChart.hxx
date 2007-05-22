9d4911b472716e86388716d0bef4c12940aa4cdc
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:14:31 +0000

    INTEGRATION: CWS chart2mst3 (1.12.4); FILE MERGED 2006/10/23 19:14:45 iha 1.12.4.17: #i65580# Legend shows no symbols for series with automatic symbols 2006/09/29 14:59:20 iha 1.12.4.16: corrected creation of 3D pies and donuts (extrude instead of lathe objects- for corect texture, get rid of problem with extended scene due to rotating bounding box, fixed #i67743# and no 90degree rotation difference between old and new API anymore) 2006/09/24 09:12:57 iha 1.12.4.15: #i61970# correct implementation of axis at 2006/07/01 20:59:29 iha 1.12.4.14: define and respect aspect ratio of diagram 2006/03/29 22:24:46 iha 1.12.4.13: restructure legend entry creation + legend entries in data labels 2006/03/09 17:41:16 iha 1.12.4.12: added header 2006/02/23 13:29:46 iha 1.12.4.11: enable gaps for NAN values in line charts 2005/10/24 11:07:01 iha 1.12.4.10: coordinate system restructure 2005/08/18 11:42:03 iha 1.12.4.9: moved colorscheme transport 2005/07/28 09:34:53 bm 1.12.4.8: usage of color schemes and the VaryColorsByPoint property to have correct pie colors and legend entries 2005/05/09 08:57:39 dr 1.12.4.7: #i30462# scaling for stacked charts 2005/04/28 15:49:13 dr 1.12.4.6: #i30426# enhanced linear scaling 2004/09/17 11:24:20 iha 1.12.4.5: implement api redesign - dimension property 2004/04/13 16:34:22 iha 1.12.4.4: corrected stacking 2004/03/01 17:22:58 iha 1.12.4.3: different x autoscaling for net chart 2004/02/28 08:36:19 iha 1.12.4.2: special autoscaling for Y (because of area grounding line) 2004/02/18 20:34:27 iha 1.12.4.1: add parameter 'ConnectLastToFirstPoint' for net chart

--- chart2/source/view/charttypes/AreaChart.hxx
+++ chart2/source/view/charttypes/AreaChart.hxx
@@ -1,3 +1,38 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: AreaChart.hxx,v $
+ *
+ *  $Revision: 1.13 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:14:31 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
 #ifndef _CHART2_AREACHART_HXX
 #define _CHART2_AREACHART_HXX
 
@@ -21,8 +56,14 @@ class AreaChart : public VSeriesPlotter
 public:
     AreaChart( const ::com::sun::star::uno::Reference<
              ::com::sun::star::chart2::XChartType >& xChartTypeModel
+             , sal_Int32 nDimensionCount
              , bool bCategoryXAxis, bool bNoArea=false
              , PlottingPositionHelper* pPlottingPositionHelper=NULL //takes owner ship
+             , bool bConnectLastToFirstPoint=false
+             , bool bAddOneToXMax=false
+             , bool bExpandIfValuesCloseToBorder=true
+             , sal_Int32 nKeepAspectRatio=-1 //0->no 1->yes other value->automatic
+             , const ::com::sun::star::drawing::Direction3D& rAspectRatio=::com::sun::star::drawing::Direction3D(1,1,1)//only taken into account if nKeepAspectRatio==1
              );
     virtual ~AreaChart();
 
@@ -37,12 +78,23 @@ public:
     virtual void SAL_CALL setTransformation( const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation >& xTransformationToLogicTarget, const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation >& xTransformationToFinalPage ) throw (::com::sun::star::uno::RuntimeException);
     */
 
-    virtual void addSeries( VDataSeries* pSeries, sal_Int32 xSlot = -1,sal_Int32 ySlot = -1 );
+    virtual void addSeries( VDataSeries* pSeries, sal_Int32 zSlot = -1, sal_Int32 xSlot = -1,sal_Int32 ySlot = -1 );
+
+    //-------------------
+    virtual ::com::sun::star::drawing::Direction3D  getPreferredDiagramAspectRatio() const;
+    virtual bool keepAspectRatio() const;
 
     //-------------------------------------------------------------------------
     // MinimumAndMaximumSupplier
     //-------------------------------------------------------------------------
-    virtual double getMaximumZ();
+    virtual double getMaximumX();
+    virtual bool isExpandIfValuesCloseToBorder( sal_Int32 nDimensionIndex );
+    virtual bool isSeperateStackingForDifferentSigns( sal_Int32 nDimensionIndex );
+
+    //-------------------------------------------------------------------------
+
+    virtual ::com::sun::star::chart2::LegendSymbolStyle getLegendSymbolStyle();
+    virtual ::com::sun::star::uno::Any getExplicitSymbol( const VDataSeries& rSeries, sal_Int32 nPointIndex=-1/*-1 for series symbol*/ );
 
     //-------------------------------------------------------------------------
     //-------------------------------------------------------------------------
@@ -54,17 +106,33 @@ private: //methods
     void impl_createSeriesShapes();
     bool impl_createArea( VDataSeries* pSeries
                 , ::com::sun::star::drawing::PolyPolygonShape3D* pSeriesPoly
-                , ::com::sun::star::drawing::PolyPolygonShape3D* pPreviousSeriesPoly );
+                , ::com::sun::star::drawing::PolyPolygonShape3D* pPreviousSeriesPoly
+                , PlottingPositionHelper* pPosHelper );
     bool impl_createLine( VDataSeries* pSeries
-                , ::com::sun::star::drawing::PolyPolygonShape3D* pSeriesPoly );
+                , ::com::sun::star::drawing::PolyPolygonShape3D* pSeriesPoly
+                , PlottingPositionHelper* pPosHelper );
 
-    double  getTransformedDepth() const;
-    double  getLogicGrounding() const;
+    void    impl_maybeReplaceNanWithZero( double& rfValue );
 
 private: //member
+    PlottingPositionHelper*             m_pMainPosHelper;
+
     bool                                m_bArea;//false -> line or symbol only
     bool                                m_bLine;
     bool                                m_bSymbol;
+    bool                                m_bConnectLastToFirstPoint;//used e.g. for net chart
+    bool                                m_bAddOneToXMax;//used e.g. for net chart (the angle axis needs a different autoscaling)
+    bool                                m_bExpandIfValuesCloseToBorder; // e.g. false for net charts
+    sal_Int32                           m_nKeepAspectRatio; //0->no 1->yes other value->automatic
+    ::com::sun::star::drawing::Direction3D m_aGivenAspectRatio; //only used if nKeepAspectRatio==1
+
+    enum tNanHandling
+    {
+        NAN_AS_ZERO,
+        NAN_AS_GAP,
+        NAN_AS_INTERPOLATED
+    };
+    tNanHandling    m_eNanHandling;
 
     //Properties for splines:
     ::com::sun::star::chart2::CurveStyle    m_eCurveStyle;
