b693ba21212024182a19d7ae5182d273266e635a
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:20:59 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/04/10 15:01:35 iha 1.1.2.1: api restructure axis, grids, scales and increments

---
+++ chart2/source/controller/chartapiwrapper/WrappedAxisAndGridExistenceProperties.hxx
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedAxisAndGridExistenceProperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:20:59 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_WRAPPED_AXISANDGRIDEXISTENCE_PROPERTIES_HXX
+#define CHART_WRAPPED_AXISANDGRIDEXISTENCE_PROPERTIES_HXX
+
+#include "WrappedProperty.hxx"
+#include "Chart2ModelContact.hxx"
+
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+//.............................................................................
+namespace chart
+{
+namespace wrapper
+{
+
+class WrappedAxisAndGridExistenceProperties
+{
+public:
+    static void addWrappedProperties( std::vector< WrappedProperty* >& rList
+        , ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact );
+};
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+class WrappedAxisTitleExistenceProperties
+{
+public:
+    static void addWrappedProperties( std::vector< WrappedProperty* >& rList
+        , ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact );
+};
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+class WrappedAxisLabelExistenceProperties
+{
+public:
+   static void addWrappedProperties( std::vector< WrappedProperty* >& rList
+       , ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact );
+};
+
+} //namespace wrapper
+} //namespace chart
+//.............................................................................
+
+// CHART_WRAPPED_SCALE_PROPERTY_HXX
+#endif
