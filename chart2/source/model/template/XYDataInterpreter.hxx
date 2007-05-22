856355daa6b5ea9586b4e4207f24a4431bb9a529
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:53:21 +0000

    INTEGRATION: CWS chart2mst3 (1.1.4); FILE ADDED 2005/10/11 09:20:49 bm 1.1.4.5: license header change 2005/05/09 09:51:31 bm 1.1.4.4: moved parts of API to data namespace 2004/05/07 13:40:25 bm 1.1.4.3: context added in CTOR 2004/03/19 14:33:00 bm 1.1.4.2: XDataSource now contains XLabeledDataSources 2004/02/13 16:51:47 bm 1.1.4.1: join from changes on branch bm_post_chart01

---
+++ chart2/source/model/template/XYDataInterpreter.hxx
@@ -0,0 +1,71 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: XYDataInterpreter.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:53:21 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_XYDATAINTERPRETER_HXX
+#define CHART_XYDATAINTERPRETER_HXX
+
+#ifndef CHART_DATAINTERPRETER_HXX
+#include "DataInterpreter.hxx"
+#endif
+
+namespace chart
+{
+
+class XYDataInterpreter : public DataInterpreter
+{
+public:
+    explicit XYDataInterpreter(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
+    virtual ~XYDataInterpreter();
+
+protected:
+    // ____ XDataInterpreter ____
+    virtual ::com::sun::star::chart2::InterpretedData SAL_CALL interpretDataSource(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::data::XDataSource >& xSource,
+        const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aArguments,
+        const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XDataSeries > >& aSeriesToReUse )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::chart2::InterpretedData SAL_CALL reinterpretDataSeries(
+        const ::com::sun::star::chart2::InterpretedData& aInterpretedData )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual sal_Bool SAL_CALL isDataCompatible(
+        const ::com::sun::star::chart2::InterpretedData& aInterpretedData )
+        throw (::com::sun::star::uno::RuntimeException);
+};
+
+} // namespace chart
+
+// CHART_XYDATAINTERPRETER_HXX
+#endif
