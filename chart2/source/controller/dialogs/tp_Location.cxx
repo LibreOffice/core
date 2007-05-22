0e2d0a6f11c157d1021e54bf54c63dd744cd019f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:46:30 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/05/18 16:12:16 bm 1.1.2.7: #i74635# ResMgr cleanup ResId -> SchResId 2007/02/08 21:25:35 iha 1.1.2.6: resync m195 -> m202 2006/10/27 14:59:20 iha 1.1.2.5: name consolidation 2006/01/23 15:54:28 iha 1.1.2.4: #i61054# disable next button at wizard correctly 2005/10/11 09:20:17 bm 1.1.2.3: license header change 2004/05/17 17:33:52 iha 1.1.2.2: provide object naming; resource cleanup 2004/05/05 20:40:36 iha 1.1.2.1: #i20344# location tab page for chart wizard

---
+++ chart2/source/controller/dialogs/tp_Location.cxx
@@ -0,0 +1,78 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_Location.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:46:30 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "tp_Location.hxx"
+#include "tp_Location.hrc"
+#include "ResId.hxx"
+#include "Strings.hrc"
+#include "NoWarningThisInCTOR.hxx"
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+
+
+LocationTabPage::LocationTabPage( svt::OWizardMachine* pParent
+        , const uno::Reference< XChartDocument >& xChartModel
+        , const uno::Reference< uno::XComponentContext >& xContext )
+        : OWizardPage( pParent, SchResId(TP_LOCATION) )
+        , m_aFL_Table( this, SchResId( FL_TABLE ) )
+        , m_aFT_Table( this, SchResId( FT_TABLE ) )
+        , m_aLB_Table( this, SchResId( LB_TABLE ) )
+        , m_xChartModel( xChartModel )
+        , m_xCC( xContext )
+{
+    FreeResource();
+    this->SetText( String( SchResId( STR_PAGE_CHART_LOCATION ) ) );
+}
+
+LocationTabPage::~LocationTabPage()
+{
+}
+
+sal_Bool LocationTabPage::determineNextButtonState()
+{
+    return sal_False;
+}
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
