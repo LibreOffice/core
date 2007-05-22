c8058930456ef48f2e297fbc24a1e9cf27d9c419
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:15:53 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/07/28 18:17:48 iha 1.1.2.4: support XShape interface at chart area for old API misuse... 2006/02/17 20:57:37 iha 1.1.2.3: enlarge validity of api wrapper objects to lifetime of new chartmodel 2005/10/11 09:19:53 bm 1.1.2.2: license header change 2005/07/07 16:24:15 bm 1.1.2.1: use wrapper for area to get correct named properties (hatches, gradients etc.)

---
+++ chart2/source/controller/chartapiwrapper/AreaWrapper.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: AreaWrapper.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:15:53 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART_AREAWRAPPER_HXX
+#define CHART_AREAWRAPPER_HXX
+
+#include "WrappedPropertySet.hxx"
+#include "ServiceMacros.hxx"
+
+#ifndef _CPPUHELPER_IMPLBASE3_HXX_
+#include <cppuhelper/implbase3.hxx>
+#endif
+#ifndef _COMPHELPER_UNO3_HXX_
+#include <comphelper/uno3.hxx>
+#endif
+#ifndef _CPPUHELPER_INTERFACECONTAINER_HXX_
+#include <cppuhelper/interfacecontainer.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_DRAWING_XSHAPE_HPP_
+#include <com/sun/star/drawing/XShape.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
+#include <com/sun/star/lang/XComponent.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_CHART2_XCHARTDOCUMENT_HPP_
+#include <com/sun/star/chart2/XChartDocument.hpp>
+#endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
+
+#include <boost/shared_ptr.hpp>
+
+namespace chart
+{
+
+namespace wrapper
+{
+
+class Chart2ModelContact;
+
+class AreaWrapper : public ::cppu::ImplInheritanceHelper3<
+                      WrappedPropertySet
+                    , com::sun::star::drawing::XShape
+                    , com::sun::star::lang::XComponent
+                    , com::sun::star::lang::XServiceInfo
+                    >
+{
+public:
+    AreaWrapper( ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact );
+    virtual ~AreaWrapper();
+
+    /// XServiceInfo declarations
+    APPHELPER_XSERVICEINFO_DECL()
+
+    // ____ XShape ____
+    virtual ::com::sun::star::awt::Point SAL_CALL getPosition()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setPosition( const ::com::sun::star::awt::Point& aPosition )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::awt::Size SAL_CALL getSize()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setSize( const ::com::sun::star::awt::Size& aSize )
+        throw (::com::sun::star::beans::PropertyVetoException,
+               ::com::sun::star::uno::RuntimeException);
+
+    // ____ XShapeDescriptor (base of XShape) ____
+    virtual ::rtl::OUString SAL_CALL getShapeType()
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XComponent ____
+    virtual void SAL_CALL dispose()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference<
+                                            ::com::sun::star::lang::XEventListener >& xListener )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference<
+                                               ::com::sun::star::lang::XEventListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+
+protected:
+    // ____ WrappedPropertySet ____
+    virtual const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::Property >& getPropertySequence();
+    virtual const std::vector< WrappedProperty* > createWrappedProperties();
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > getInnerPropertySet();
+
+private:
+    ::boost::shared_ptr< Chart2ModelContact >   m_spChart2ModelContact;
+    ::cppu::OInterfaceContainerHelper           m_aEventListenerContainer;
+};
+
+} //  namespace wrapper
+} //  namespace chart
+
+// CHART_AREAWRAPPER_HXX
+#endif
