c4f3615c2c04ddb2a4a55a4fca92453b98c12ddd
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:14:54 +0000

    INTEGRATION: CWS chart2mst3 (1.5.4); FILE MERGED 2006/07/01 21:00:52 iha 1.5.4.17: define and respect aspect ratio of diagram 2006/03/09 17:41:16 iha 1.5.4.16: added header 2005/11/22 14:10:54 iha 1.5.4.15: support BarOverlap and GapWidth 2005/10/24 11:07:03 iha 1.5.4.14: coordinate system restructure 2005/08/18 11:42:03 iha 1.5.4.13: moved colorscheme transport 2005/07/28 09:34:54 bm 1.5.4.12: usage of color schemes and the VaryColorsByPoint property to have correct pie colors and legend entries 2005/04/28 15:49:13 dr 1.5.4.11: #i30426# enhanced linear scaling 2004/09/17 11:24:20 iha 1.5.4.10: implement api redesign - dimension property 2004/04/29 09:28:04 iha 1.5.4.9: correct 3d bar object position and size 2004/04/28 20:16:51 iha 1.5.4.8: removed /cvs/graphics/chart2/source/inc/RegressionCurveHelper.hxx,v: needs patch 2004/03/24 17:26:36 iha 1.5.4.7: changed method getLabelScreenPositionAndAlignment 2004/03/23 14:55:03 iha 1.5.4.6: removed method createDataPoint2D_Bar 2004/03/11 12:27:28 iha 1.5.4.5: added meethod getPreferredDiagramAspectRatio() for charttype dependent 3D scene aspect ratio 2004/03/09 09:03:11 iha 1.5.4.4: crash with negative pyramid; do not change base width of cuboids and cylinders with height 2004/03/08 19:00:51 iha 1.5.4.3: read DataPointGeometry from model 2004/02/28 08:47:14 iha 1.5.4.2: special autoscaling for Y (because of grounding line) 2004/02/23 18:55:02 iha 1.5.4.1: auto scaling: make automatic border charttype dependent

--- chart2/source/view/charttypes/BarChart.hxx
+++ chart2/source/view/charttypes/BarChart.hxx
@@ -1,20 +1,48 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: BarChart.hxx,v $
+ *
+ *  $Revision: 1.6 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:14:54 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
 #ifndef _CHART2_BARCHART_HXX
 #define _CHART2_BARCHART_HXX
 
 #include "VSeriesPlotter.hxx"
-#include "DatapointGeometry.hxx"
 
 //.............................................................................
 namespace chart
 {
 //.............................................................................
 class BarPositionHelper;
-
-enum Geometry3D { GEOMETRY_UNKNOWN
-                , GEOMETRY_CUBOID
-                , GEOMETRY_CYLINDER
-                , GEOMETRY_CONE
-                , GEOMETRY_PYRAMID };
 
 class BarChart : public VSeriesPlotter
 {
@@ -23,7 +51,8 @@ class BarChart : public VSeriesPlotter
     //-------------------------------------------------------------------------
 public:
     BarChart( const ::com::sun::star::uno::Reference<
-            ::com::sun::star::chart2::XChartType >& xChartTypeModel );
+            ::com::sun::star::chart2::XChartType >& xChartTypeModel
+            , sal_Int32 nDimensionCount );
     virtual ~BarChart();
 
     //-------------------------------------------------------------------------
@@ -37,6 +66,18 @@ public:
     virtual void SAL_CALL setTransformation( const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation >& xTransformationToLogicTarget, const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation >& xTransformationToFinalPage ) throw (::com::sun::star::uno::RuntimeException);
     */
 
+    virtual void addSeries( VDataSeries* pSeries, sal_Int32 zSlot = -1, sal_Int32 xSlot = -1,sal_Int32 ySlot = -1 );
+
+    //-------------------
+    virtual ::com::sun::star::drawing::Direction3D  getPreferredDiagramAspectRatio() const;
+    virtual bool keepAspectRatio() const;
+
+    //-------------------------------------------------------------------------
+    // MinimumAndMaximumSupplier
+    //-------------------------------------------------------------------------
+    virtual double getMinimumX();
+    virtual double getMaximumX();
+
     //-------------------------------------------------------------------------
     //-------------------------------------------------------------------------
     //-------------------------------------------------------------------------
@@ -49,24 +90,25 @@ private: //methods
         createDataPoint3D_Bar(
                           const ::com::sun::star::uno::Reference<
                                 ::com::sun::star::drawing::XShapes >& xTarget
-                        , const DataPointGeometry& rGeometry
+                        , const ::com::sun::star::drawing::Position3D& rPosition
+                        , const ::com::sun::star::drawing::Direction3D& rSize
+                        , double fTopHeight, sal_Int32 nRotateZAngleHundredthDegree
                         , const ::com::sun::star::uno::Reference<
                                 ::com::sun::star::beans::XPropertySet >& xObjectProperties
-                        , Geometry3D eGeometry );
-    ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShape >
-        createDataPoint2D_Bar(
-                          const ::com::sun::star::uno::Reference<
-                                ::com::sun::star::drawing::XShapes >& xTarget
-                        , const DataPointGeometry& rGeometry
-                        , const ::com::sun::star::uno::Reference<
-                                ::com::sun::star::beans::XPropertySet >& xObjectProperties );
+                        , sal_Int32 nGeometry3D );
 
     ::com::sun::star::awt::Point getLabelScreenPositionAndAlignment(
                         LabelAlignment& rAlignment, bool bMiddlePosition
-                        , const DataPointGeometry& rTransformedGeom ) const;
+                        , double fScaledX, double fScaledLowerYValue, double fScaledUpperYValue, double fScaledZ
+                        , double fScaledLowerBarDepth, double fScaledUpperBarDepth
+                        , BarPositionHelper* pPosHelper ) const;
+
+    virtual PlottingPositionHelper& getPlottingPositionHelper( sal_Int32 nAxisIndex ) const;//nAxisIndex indicates wether the position belongs to the main axis ( nAxisIndex==0 ) or secondary axis ( nAxisIndex==1 )
 
 private: //member
-    BarPositionHelper*                   m_pPosHelper;
+    BarPositionHelper*                   m_pMainPosHelper;
+    ::com::sun::star::uno::Sequence< sal_Int32 > m_aOverlapSequence;
+    ::com::sun::star::uno::Sequence< sal_Int32 > m_aGapwidthSequence;
 };
 //.............................................................................
 } //namespace chart
