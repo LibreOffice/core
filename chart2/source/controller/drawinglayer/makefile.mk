bbc0f14ccfd6d671b9a8963b21448bf3c2c9f576
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:51:31 +0000

    INTEGRATION: CWS chart2mst3 (1.1.1.1.4); FILE MERGED 2005/10/07 11:23:39 bm 1.1.1.1.4.2: RESYNC: (1.1.1.1-1.2); FILE MERGED 2005/05/31 19:08:40 iha 1.1.1.1.4.1: create view without controller

--- chart2/source/controller/drawinglayer/makefile.mk
+++ chart2/source/controller/drawinglayer/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 00:20:40 $
+#   last change: $Author: vg $ $Date: 2007-05-22 17:51:31 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -47,8 +47,7 @@ ENABLE_EXCEPTIONS=	TRUE
 # --- export library -------------------------------------------------
 
 #object files to build and link together to lib $(SLB)$/$(TARGET).lib
-SLOFILES=   	$(SLO)$/DrawModelWrapper.obj \
-                $(SLO)$/DrawViewWrapper.obj \
+SLOFILES=   	$(SLO)$/DrawViewWrapper.obj \
                 $(SLO)$/ViewElementListProvider.obj
 
 # --- Targets -----------------------------------------------------------------
