132e728b30433b4a258119c55999c08ac58a1f0f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:25:08 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:31 iha 1.1.2.3: resync m195 -> m202 2005/10/11 09:20:02 bm 1.1.2.2: license header change 2005/04/19 17:32:10 iha 1.1.2.1: improved old api support (introduced wrappedpropertyset ...)

---
+++ chart2/source/controller/chartapiwrapper/WrappedTextRotationProperty.cxx
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: WrappedTextRotationProperty.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:25:08 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "WrappedTextRotationProperty.hxx"
+#include "macros.hxx"
+
+using ::com::sun::star::uno::Any;
+using ::rtl::OUString;
+
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+WrappedTextRotationProperty::WrappedTextRotationProperty()
+    : ::chart::WrappedProperty( C2U( "TextRotation" ), C2U( "TextRotation" ) )
+{
+}
+WrappedTextRotationProperty::~WrappedTextRotationProperty()
+{
+}
+Any WrappedTextRotationProperty::convertInnerToOuterValue( const Any& rInnerValue ) const
+{
+    Any aRet;
+    double fVal;
+    if( rInnerValue >>= fVal )
+    {
+        sal_Int32 n100thDegrees = static_cast< sal_Int32 >( fVal * 100.0 );
+        aRet <<= n100thDegrees;
+    }
+    return aRet;
+}
+Any WrappedTextRotationProperty::convertOuterToInnerValue( const Any& rOuterValue ) const
+{
+    Any aRet;
+    sal_Int32 nVal;
+    if( rOuterValue >>= nVal )
+    {
+        double fDoubleDegrees = ( static_cast< double >( nVal ) / 100.0 );
+        aRet <<= fDoubleDegrees;
+    }
+    return aRet;
+}
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
