d6cd3b671e75df3943575528d66964acede0e37e
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:22:38 +0000

    INTEGRATION: CWS chart2mst3 (1.3.12); FILE MERGED 2006/04/05 11:13:12 bm 1.3.12.1: make Scalings available via factory

--- chart2/source/inc/Scaling.hxx
+++ chart2/source/inc/Scaling.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Scaling.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 00:46:15 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:22:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,16 +34,23 @@
  ************************************************************************/
 #ifndef _CHART2_SCALING_HXX
 #define _CHART2_SCALING_HXX
+#include "ServiceMacros.hxx"
 
 #ifndef _COM_SUN_STAR_CHART2_XSCALING_HPP_
 #include <com/sun/star/chart2/XScaling.hpp>
 #endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
 #ifndef _COM_SUN_STAR_LANG_XSERVICENAME_HPP_
 #include <com/sun/star/lang/XServiceName.hpp>
 #endif
+#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
+#include <com/sun/star/uno/XComponentContext.hpp>
+#endif
 
-#ifndef _CPPUHELPER_IMPLBASE2_HXX_
-#include <cppuhelper/implbase2.hxx>
+#ifndef _CPPUHELPER_IMPLBASE3_HXX_
+#include <cppuhelper/implbase3.hxx>
 #endif
 
 //.............................................................................
@@ -55,15 +62,24 @@ namespace chart
 /**
 */
 
-class LogarithmicScaling : public ::cppu::WeakImplHelper2 <
+class LogarithmicScaling : public ::cppu::WeakImplHelper3 <
         ::com::sun::star::chart2::XScaling,
-        ::com::sun::star::lang::XServiceName
+        ::com::sun::star::lang::XServiceName,
+        ::com::sun::star::lang::XServiceInfo
         >
 {
 public:
-    LogarithmicScaling();
-    explicit LogarithmicScaling( double fBase );
+    /// base is 10.0
+    explicit LogarithmicScaling(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
+    LogarithmicScaling( double fBase = 10.0 );
     virtual ~LogarithmicScaling();
+
+    /// establish methods for factory instatiation
+    APPHELPER_SERVICE_FACTORY_HELPER( LogarithmicScaling )
+    /// declare XServiceInfo methods
+    APPHELPER_XSERVICEINFO_DECL()
 
     // ____ XScaling ____
         virtual double SAL_CALL
@@ -81,19 +97,30 @@ public:
 private:
     const double m_fBase;
     const double m_fLogOfBase;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >      m_xContext;
 };
 
 // ----------------------------------------
 
-class ExponentialScaling : public ::cppu::WeakImplHelper2 <
+class ExponentialScaling : public ::cppu::WeakImplHelper3 <
         ::com::sun::star::chart2::XScaling,
-        ::com::sun::star::lang::XServiceName
+        ::com::sun::star::lang::XServiceName,
+        ::com::sun::star::lang::XServiceInfo
         >
 {
 public:
-    ExponentialScaling();
-    explicit ExponentialScaling( double fBase );
+    /// base is 10.0
+    explicit ExponentialScaling(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
+    explicit ExponentialScaling( double fBase = 10.0 );
     virtual ~ExponentialScaling();
+
+    /// establish methods for factory instatiation
+    APPHELPER_SERVICE_FACTORY_HELPER( ExponentialScaling )
+    /// declare XServiceInfo methods
+    APPHELPER_XSERVICEINFO_DECL()
 
     // ____ XScaling ____
         virtual double SAL_CALL
@@ -110,19 +137,31 @@ public:
 
 private:
     const double m_fBase;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >      m_xContext;
 };
 
 // ----------------------------------------
 
-class LinearScaling : public ::cppu::WeakImplHelper2 <
+class LinearScaling : public ::cppu::WeakImplHelper3 <
         ::com::sun::star::chart2::XScaling,
-        ::com::sun::star::lang::XServiceName
+        ::com::sun::star::lang::XServiceName,
+        ::com::sun::star::lang::XServiceInfo
         >
 {
 public:
+    /// y(x) = x
+    explicit LinearScaling(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
     /// y(x) = fSlope * x + fOffset
-    LinearScaling( double fSlope, double fOffset );
+    LinearScaling( double fSlope = 1.0, double fOffset = 0.0 );
     virtual ~LinearScaling();
+
+    /// establish methods for factory instatiation
+    APPHELPER_SERVICE_FACTORY_HELPER( LinearScaling )
+    /// declare XServiceInfo methods
+    APPHELPER_XSERVICEINFO_DECL()
 
     // ____ XScaling ____
         virtual double SAL_CALL
@@ -140,18 +179,30 @@ public:
 private:
     const double m_fSlope;
     const double m_fOffset;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >      m_xContext;
 };
 
 // ----------------------------------------
 
-class PowerScaling : public ::cppu::WeakImplHelper2 <
+class PowerScaling : public ::cppu::WeakImplHelper3 <
         ::com::sun::star::chart2::XScaling,
-        ::com::sun::star::lang::XServiceName
+        ::com::sun::star::lang::XServiceName,
+        ::com::sun::star::lang::XServiceInfo
         >
 {
 public:
-    PowerScaling( double fExponent );
+    /// exponent 10.0
+    explicit PowerScaling(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
+    explicit PowerScaling( double fExponent = 10.0 );
     virtual ~PowerScaling();
+
+    /// establish methods for factory instatiation
+    APPHELPER_SERVICE_FACTORY_HELPER( PowerScaling )
+    /// declare XServiceInfo methods
+    APPHELPER_XSERVICEINFO_DECL()
 
     // ____ XScaling ____
         virtual double SAL_CALL
@@ -168,6 +219,8 @@ public:
 
 private:
     const double m_fExponent;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >      m_xContext;
 };
 
 //.............................................................................
