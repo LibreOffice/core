c2be7fbdbc9ee8cd5aef88385d53bc737182bb89
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 15:26:06 +0000

    INTEGRATION: CWS chart2mst3 (1.4.54); FILE MERGED 2006/12/12 16:43:12 tl 1.4.54.1: #i71244# update charts in writer

--- sw/source/core/docnode/makefile.mk
+++ sw/source/core/docnode/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 03:20:33 $
+#   last change: $Author: vg $ $Date: 2007-05-22 16:26:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -45,6 +45,9 @@ TARGET=docnode
 .INCLUDE :	$(PRJ)$/inc$/sw.mk
 
 # --- Files --------------------------------------------------------
+
+EXCEPTIONSFILES=    \
+    $(SLO)$/ndtbl.obj
 
 SLOFILES =	\
         $(SLO)$/ndindex.obj \
