1278192f1ce1a8c4ce98b7a0bfa1c0026ceab8d7
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:30:44 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2006/03/10 10:55:22 iha 1.1.2.4: don't use leading underscore 2005/10/11 09:20:41 bm 1.1.2.3: license header change 2004/09/16 16:10:02 bm 1.1.2.2: API simplification 2004/09/16 16:01:37 bm 1.1.2.1: API simplification

---
+++ chart2/source/model/inc/_serviceregistration_charttypes.hxx
@@ -0,0 +1,50 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: _serviceregistration_charttypes.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:30:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART2__SERVICEREGISTRATION_CHARTTYPES_HXX
+#define CHART2__SERVICEREGISTRATION_CHARTTYPES_HXX
+
+#ifndef _CPPUHELPER_IMPLEMENATIONENTRY_HXX_
+#include <cppuhelper/implementationentry.hxx>
+#endif
+
+class ChartTypeEntriesForServiceRegistration
+{
+public:
+    static struct ::cppu::ImplementationEntry*
+        getImplementationEntries();
+};
+
+// CHART2__SERVICEREGISTRATION_CHARTTYPES_HXX
+#endif
