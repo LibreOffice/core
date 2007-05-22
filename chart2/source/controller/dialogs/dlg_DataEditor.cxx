7fcffec46bdd80080e55f96c51493dad5a8b87a5
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:32:56 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/05/18 16:12:16 bm 1.1.2.11: #i74635# ResMgr cleanup ResId -> SchResId 2007/02/08 21:25:33 iha 1.1.2.10: resync m195 -> m202 2006/12/13 15:36:15 bm 1.1.2.9: apply pending changes on window close 2006/07/20 09:53:52 bm 1.1.2.8: #i64824# show shared sequences correctly. Missing: insert new series 2006/07/19 15:55:24 bm 1.1.2.7: #i64824# improvement of the data editor for internal data. Especially the handling of flexible data and series with more than one role 2006/03/14 16:50:13 bm 1.1.2.6: #129537# remove as listener from misc options in DTOR 2006/03/08 17:53:15 bm 1.1.2.5: new UI design for data dialog 2006/03/08 10:38:52 bm 1.1.2.4: new UI for DataEditor. Define TEST_NEW_UI in dlg_DataEditor.hrc to activate 2005/10/11 09:20:08 bm 1.1.2.3: license header change 2005/08/19 14:29:10 bm 1.1.2.2: enable icons in the toolbox 2005/08/17 16:53:06 bm 1.1.2.1: data editor for standalone data (first approach)

---
+++ chart2/source/controller/dialogs/dlg_DataEditor.cxx
@@ -0,0 +1,399 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: dlg_DataEditor.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:32:56 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "dlg_DataEditor.hxx"
+#include "dlg_DataEditor.hrc"
+#include "Strings.hrc"
+#include "DataBrowser.hxx"
+
+#include "ResId.hxx"
+#include "Strings.hrc"
+#include "SchSlotIds.hxx"
+
+#ifndef _SFXDISPATCH_HXX
+#include <sfx2/dispatch.hxx>
+#endif
+#ifndef _SV_MSGBOX_HXX
+#include <vcl/msgbox.hxx>
+#endif
+#ifndef _SV_TASKPANELIST_HXX
+#include <vcl/taskpanelist.hxx>
+#endif
+#ifndef INCLUDED_SVTOOLS_MISCOPT_HXX
+#include <svtools/miscopt.hxx>
+#endif
+#ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
+#include <svtools/pathoptions.hxx>
+#endif
+
+// for SfxBoolItem
+#ifndef _SFXENUMITEM_HXX
+#include <svtools/eitem.hxx>
+#endif
+
+#include <vcl/edit.hxx>
+
+#include <com/sun/star/frame/XStorable.hpp>
+#include <com/sun/star/chart2/XChartDocument.hpp>
+
+// for storing/reading the position and size of the dialog
+// #include <svtools/viewoptions.hxx>
+
+using namespace ::com::sun::star;
+using ::com::sun::star::uno::Reference;
+using ::rtl::OUString;
+
+
+namespace chart
+{
+
+DataEditor::DataEditor(
+    Window* pParent,
+    const Reference< chart2::XChartDocument > & xChartDoc,
+    const Reference< uno::XComponentContext > & xContext ) :
+        ModalDialog( pParent, SchResId( DLG_DIAGRAM_DATA )),
+        m_bReadOnly( false ),
+        m_apBrwData( new DataBrowser( this, SchResId( CTL_DATA ), true /* bLiveUpdate */)),
+        m_aTbxData( this, SchResId( TBX_DATA )),
+        m_xChartDoc( xChartDoc ),
+        m_xContext( xContext ),
+        m_aToolboxImageList( SchResId( IL_DIAGRAM_DATA )),
+        m_aToolboxImageListHighContrast( SchResId( IL_HC_DIAGRAM_DATA ))
+{
+    FreeResource();
+
+    // set min size to current size
+    SetMinOutputSizePixel( GetOutputSizePixel() );
+
+    ApplyImageList();
+
+    m_aTbxData.SetSizePixel( m_aTbxData.CalcWindowSizePixel() );
+    m_aTbxData.SetSelectHdl( LINK( this, DataEditor, ToolboxHdl ));
+
+    m_apBrwData->SetCursorMovedHdl( LINK( this, DataEditor,   BrowserCursorMovedHdl ));
+    m_apBrwData->SetCellModifiedHdl( LINK( this, DataEditor,  CellModified ));
+
+    UpdateData();
+    GrabFocus();
+    m_apBrwData->GrabFocus();
+
+    bool bReadOnly = true;
+    Reference< frame::XStorable > xStor( m_xChartDoc, uno::UNO_QUERY );
+    if( xStor.is())
+        bReadOnly = xStor->isReadonly();
+    SetReadOnly( bReadOnly );
+
+    // #101228# change buttons to flat-look if set so by user
+    SvtMiscOptions aMiscOptions;
+    const sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
+    // react on changes
+    aMiscOptions.AddListener( LINK( this, DataEditor, MiscHdl ) );
+    m_aTbxData.SetOutStyle( nStyle );
+
+    // set good window width
+    Size aWinSize( GetOutputSizePixel());
+    Size aWinSizeWithBorder( GetSizePixel());
+    Point aWinPos( OutputToAbsoluteScreenPixel( GetPosPixel()));
+    sal_Int32 nMinWidth = aWinSize.getWidth();
+    sal_Int32 nMaxWidth = GetDesktopRectPixel().getWidth() -
+        (aWinSizeWithBorder.getWidth() - aWinSize.getWidth() + aWinPos.getX()) - 10; // leave some space
+    sal_Int32 nBrowserWidth = m_apBrwData->GetTotalWidth() + 12 + 16; // plus padding + 16?
+    sal_Int32 nWindowWidth = ::std::max( nMinWidth, nBrowserWidth );
+    nWindowWidth = ::std::min( nMaxWidth, nBrowserWidth );
+    aWinSize.setWidth( nWindowWidth );
+    SetOutputSizePixel( aWinSize );
+    AdaptBrowseBoxSize();
+
+//     ImplAdjustHeaderControls( false /* bRefreshFromModel */ );
+
+    // allow travelling to toolbar with F6
+    notifySystemWindow( this, & m_aTbxData, ::comphelper::mem_fun( & TaskPaneList::AddWindow ));
+}
+
+DataEditor::~DataEditor()
+{
+    notifySystemWindow( this, & m_aTbxData, ::comphelper::mem_fun( & TaskPaneList::RemoveWindow ));
+
+    SvtMiscOptions aMiscOptions;
+    aMiscOptions.RemoveListener( LINK( this, DataEditor, MiscHdl ) );
+
+    OSL_TRACE( "DataEditor: DTOR" );
+}
+
+// react on click (or keypress) on toolbar icon
+IMPL_LINK( DataEditor, ToolboxHdl, void *, EMPTYARG )
+{
+    switch( m_aTbxData.GetCurItemId() )
+    {
+        case TBI_DATA_INSERT_ROW:
+            m_apBrwData->InsertRow();
+            break;
+        case TBI_DATA_INSERT_COL:
+            m_apBrwData->InsertColumn();
+//             ImplAdjustHeaderControls( true /* bRefreshFromModel */ );
+            break;
+        case TBI_DATA_DELETE_ROW:
+            m_apBrwData->RemoveRow();
+            break;
+        case TBI_DATA_DELETE_COL:
+            m_apBrwData->RemoveColumn();
+//             ImplAdjustHeaderControls( true /* bRefreshFromModel */ );
+            break;
+        case TBI_DATA_SWAP_COL :
+            m_apBrwData->SwapColumn ();
+//             ImplAdjustHeaderControls( true /* bRefreshFromModel */ );
+            break;
+        case TBI_DATA_SWAP_ROW :
+            m_apBrwData->SwapRow ();
+            break;
+//         case TBI_DATA_SORT_COL :
+//             m_apBrwData->QuickSortCol();
+//             break;
+//         case TBI_DATA_SORT_ROW :
+//             m_apBrwData->QuickSortRow();
+//             break;
+//         case TBI_DATA_SORT_TABLE_COL :
+//             m_apBrwData->QuickSortTableCols ();
+//             break;
+//         case TBI_DATA_SORT_TABLE_ROW :
+//             m_apBrwData->QuickSortTableRows ();
+//             break;
+    }
+
+    return 0;
+}
+
+// refresh toolbar icons according to currently selected cell in brwose box
+IMPL_LINK( DataEditor, BrowserCursorMovedHdl, void *, EMPTYARG )
+{
+    if( m_bReadOnly )
+        return 0;
+
+    m_aTbxData.EnableItem( TBI_DATA_INSERT_ROW, m_apBrwData->MayInsertRow() );
+    m_aTbxData.EnableItem( TBI_DATA_INSERT_COL, m_apBrwData->MayInsertColumn() );
+    m_aTbxData.EnableItem( TBI_DATA_DELETE_ROW, m_apBrwData->MayDeleteRow() );
+    m_aTbxData.EnableItem( TBI_DATA_DELETE_COL, m_apBrwData->MayDeleteColumn() );
+
+    m_aTbxData.EnableItem( TBI_DATA_SWAP_COL,   m_apBrwData->MaySwapColumns() );
+    m_aTbxData.EnableItem( TBI_DATA_SWAP_ROW,   m_apBrwData->MaySwapRows() );
+
+//     m_aTbxData.EnableItem( TBI_DATA_SORT_COL,       m_apBrwData->MaySortColumn() );
+//     m_aTbxData.EnableItem( TBI_DATA_SORT_ROW,       m_apBrwData->MaySortRow() );
+//     m_aTbxData.EnableItem( TBI_DATA_SORT_TABLE_COL, m_apBrwData->MaySortColumn() );
+//     m_aTbxData.EnableItem( TBI_DATA_SORT_TABLE_ROW, m_apBrwData->MaySortRow() );
+
+    return 0;
+}
+
+// disable all modifying controls
+void DataEditor::SetReadOnly( bool bReadOnly )
+{
+    m_bReadOnly = bReadOnly;
+    if( m_bReadOnly )
+    {
+        m_aTbxData.EnableItem( TBI_DATA_INSERT_ROW, FALSE );
+        m_aTbxData.EnableItem( TBI_DATA_INSERT_COL, FALSE );
+        m_aTbxData.EnableItem( TBI_DATA_DELETE_ROW, FALSE );
+        m_aTbxData.EnableItem( TBI_DATA_DELETE_COL, FALSE );
+        m_aTbxData.EnableItem( TBI_DATA_SWAP_COL, FALSE );
+        m_aTbxData.EnableItem( TBI_DATA_SWAP_ROW, FALSE );
+//         m_aTbxData.EnableItem( TBI_DATA_SORT_COL, FALSE );
+//         m_aTbxData.EnableItem( TBI_DATA_SORT_ROW, FALSE );
+//         m_aTbxData.EnableItem( TBI_DATA_SORT_TABLE_ROW, FALSE );
+//         m_aTbxData.EnableItem( TBI_DATA_SORT_TABLE_COL, FALSE );
+    }
+
+    m_apBrwData->SetReadOnly( m_bReadOnly );
+}
+
+IMPL_LINK( DataEditor, MiscHdl, void*, EMPTYARG )
+{
+    SvtMiscOptions aMiscOptions;
+    sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
+
+    m_aTbxData.SetOutStyle( nStyle );
+
+    return 0L;
+}
+
+IMPL_LINK( DataEditor, CellModified, void*, EMPTYARG )
+{
+    return 0;
+}
+
+// IMPL_LINK( DataEditor, BrowserColumnResized, void*, EMPTYARG )
+// {
+//     ImplAdjustHeaderControls( false /* bRefreshFromModel */ );
+//     return 0;
+// }
+
+// IMPL_LINK( DataEditor, BrowserContentScrolled, void*, EMPTYARG )
+// {
+//     ImplAdjustHeaderControls( false /* bRefreshFromModel */ );
+//     return 0;
+// }
+
+void DataEditor::UpdateData()
+{
+    m_apBrwData->SetDataFromModel( m_xChartDoc, m_xContext );
+}
+
+// react on the change of the underlying document by displaying the new data
+// void DataEditor::SFX_NOTIFY(
+//     SfxBroadcaster& rBC,
+//     const TypeId& rBCType,
+//     const SfxHint& rHint,
+//     const TypeId& rHintType )
+// {
+//     if( rHint.Type() == TYPE(SfxSimpleHint) )
+//     {
+//         // note: if dynamic_cast works this should be changed
+//         switch( static_cast< const SfxSimpleHint & >( rHint ).GetId())
+//         {
+//             case SFX_HINT_DOCCHANGED:
+//                 UpdateData();
+//                 break;
+
+//             case SFX_HINT_DYING:
+//                 break;
+//         }
+//     }
+// }
+
+// {
+//     BOOL bRet = TRUE;
+
+//     // confirm changes currently made and not saved
+//     m_apBrwData->EndEditing();
+
+//     if( m_apBrwData->IsDirty() )
+//     {
+//         QueryBox aSafetyQuery( this, WB_YES_NO_CANCEL | WB_DEF_YES,
+//                                String( SchResId( STR_DIAGRAM_DATA_SAFETY_QUERY )));
+//         long nQueryResult = aSafetyQuery.Execute();
+
+//         bRet = ( nQueryResult != RET_CANCEL );
+
+//         if( nQueryResult == RET_YES )
+//         {
+//             // save changes
+//             ApplyChangesToModel();
+//         }
+//     }
+
+//     if( bRet )
+//     {
+//         // close child window
+//         SfxBoolItem aItem( SID_DIAGRAM_DATA, FALSE );
+//         if( m_pBindings )
+//         {
+//             SfxDispatcher* pDisp = m_pBindings->GetDispatcher();
+//             if( pDisp )
+//                 pDisp->Execute( SID_DIAGRAM_DATA, SFX_CALLMODE_ASYNCHRON, &aItem, 0L);
+//             else
+//                 DBG_ERROR( "Couldn't dispatch command" );
+//         }
+//     }
+
+//     return ( bRet? SfxFloatingWindow::Close(): FALSE );
+// }
+
+void DataEditor::AdaptBrowseBoxSize()
+{
+    Size aSize( PixelToLogic( GetResizeOutputSizePixel(), MAP_APPFONT ));
+    Size aDataSize;
+
+    aDataSize.setWidth( aSize.getWidth() - 12 );
+    aDataSize.setHeight( aSize.getHeight() - 31 -24 );
+
+    m_apBrwData->SetSizePixel( LogicToPixel( aDataSize, MAP_APPFONT ));
+}
+
+void DataEditor::Resize()
+{
+    Dialog::Resize();
+    AdaptBrowseBoxSize();
+//     ImplAdjustHeaderControls( false /* bRefreshFromModel */ );
+}
+
+BOOL DataEditor::Close()
+{
+    ApplyChangesToModel();
+    return ModalDialog::Close();
+}
+
+void DataEditor::ApplyChangesToModel()
+{
+    m_apBrwData->EndEditing();
+}
+
+// sets the correct toolbar icons depending on the current mode (e.g. high contrast)
+void DataEditor::ApplyImageList()
+{
+    bool bIsHighContrast = ( true && GetDisplayBackground().GetColor().IsDark() );
+
+    ImageList& rImgLst = bIsHighContrast
+        ? m_aToolboxImageListHighContrast
+        : m_aToolboxImageList;
+
+    m_aTbxData.SetImageList( rImgLst );
+}
+
+// add/remove a window (the toolbar) to/from the global list, so that F6
+// travels/no longer travels over this window.  _rMemFunc may be
+// TaskPaneList::AddWindow or TaskPaneList::RemoveWindow
+void DataEditor::notifySystemWindow(
+    Window* pWindow, Window* pToRegister,
+    ::comphelper::mem_fun1_t< TaskPaneList, Window* > rMemFunc )
+{
+    OSL_ENSURE( pWindow, "Window must not be null!" );
+    if( !pWindow )
+        return;
+    Window* pParent = pWindow->GetParent();
+    while( pParent && ! pParent->IsSystemWindow() )
+    {
+        pParent = pParent->GetParent();
+    }
+    if ( pParent && pParent->IsSystemWindow())
+    {
+        SystemWindow* pSystemWindow = static_cast< SystemWindow* >( pParent );
+        rMemFunc( pSystemWindow->GetTaskPaneList(),( pToRegister ));
+    }
+}
+
+} // namespace chart
