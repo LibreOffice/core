93623f6cf0a88953394aaf70eb8f7dde5d414a3f
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:56:57 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/02/08 21:25:42 iha 1.1.2.5: resync m195 -> m202 2005/10/11 09:20:51 bm 1.1.2.4: license header change 2005/08/12 13:00:41 bm 1.1.2.3: return type forgotten 2005/08/03 16:36:26 bm 1.1.2.2: algohelper.hxx split up into CommonFunctors.hxx ContainerHelper.hxx CloneHelper.hxx 2005/07/28 09:36:11 bm 1.1.2.1: color scheme that reads the default colors from the configuration

---
+++ chart2/source/tools/ConfigColorScheme.cxx
@@ -0,0 +1,207 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ConfigColorScheme.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:56:57 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "ConfigColorScheme.hxx"
+#include "ContainerHelper.hxx"
+#include "macros.hxx"
+
+#include <unotools/configitem.hxx>
+
+#include <set>
+
+using namespace ::com::sun::star;
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Sequence;
+using ::rtl::OUString;
+
+namespace
+{
+
+static const OUString aSeriesPropName( RTL_CONSTASCII_USTRINGPARAM("Series"));
+
+} // anonymous namespace
+
+// --------------------------------------------------------------------------------
+
+namespace chart
+{
+namespace impl
+{
+class ChartConfigItem : public ::utl::ConfigItem
+{
+public:
+    explicit ChartConfigItem( ConfigItemListener & rListener );
+    virtual ~ChartConfigItem();
+
+    void addPropertyNotification( const OUString & rPropertyName );
+    void removePropertyNotification( const OUString & rPropertyName );
+
+    uno::Any getProperty( const OUString & aPropertyName );
+
+protected:
+    // ____ ::utl::ConfigItem ____
+    virtual void Notify( const Sequence< OUString > & aPropertyNames );
+
+private:
+    ConfigItemListener & m_rListener;
+    ::std::set< OUString >        m_aPropertiesToNotify;
+};
+
+ChartConfigItem::ChartConfigItem( ConfigItemListener & rListener ) :
+        ::utl::ConfigItem( C2U("Office.Chart/DefaultColor")),
+    m_rListener( rListener )
+{}
+
+ChartConfigItem::~ChartConfigItem()
+{}
+
+void ChartConfigItem::Notify( const Sequence< OUString > & aPropertyNames )
+{
+    for( sal_Int32 nIdx=0; nIdx<aPropertyNames.getLength(); ++nIdx )
+    {
+        if( m_aPropertiesToNotify.find( aPropertyNames[nIdx] ) != m_aPropertiesToNotify.end())
+            m_rListener.notify( aPropertyNames[nIdx] );
+    }
+}
+
+void ChartConfigItem::addPropertyNotification( const OUString & rPropertyName )
+{
+    m_aPropertiesToNotify.insert( rPropertyName );
+    EnableNotification( ContainerHelper::ContainerToSequence( m_aPropertiesToNotify ));
+}
+
+void ChartConfigItem::removePropertyNotification( const OUString & rPropertyName )
+{
+    if( m_aPropertiesToNotify.erase( rPropertyName ))
+        EnableNotification( ContainerHelper::ContainerToSequence( m_aPropertiesToNotify ));
+}
+
+uno::Any ChartConfigItem::getProperty( const OUString & aPropertyName )
+{
+    Sequence< uno::Any > aValues(
+        GetProperties( Sequence< OUString >( &aPropertyName, 1 )));
+    if( ! aValues.getLength())
+        return uno::Any();
+    return aValues[0];
+}
+
+} // namespace impl
+
+// --------------------------------------------------------------------------------
+
+// explicit
+ConfigColorScheme::ConfigColorScheme(
+    const Reference< uno::XComponentContext > & xContext ) :
+        m_xContext( xContext  ),
+        m_nNumberOfColors( 0 ),
+        m_bNeedsUpdate( true )
+{
+}
+
+ConfigColorScheme::~ConfigColorScheme()
+{}
+
+void ConfigColorScheme::retrieveConfigColors()
+{
+    if( ! m_xContext.is())
+        return;
+
+    // create config item if necessary
+    if( ! m_apChartConfigItem.get())
+    {
+        m_apChartConfigItem.reset(
+            new impl::ChartConfigItem( *this ));
+        m_apChartConfigItem->addPropertyNotification( aSeriesPropName );
+    }
+    OSL_ASSERT( m_apChartConfigItem.get());
+    if( ! m_apChartConfigItem.get())
+        return;
+
+    // retrieve colors
+    uno::Any aValue(
+        m_apChartConfigItem->getProperty( aSeriesPropName ));
+    if( aValue >>= m_aColorSequence )
+        m_nNumberOfColors = m_aColorSequence.getLength();
+    m_bNeedsUpdate = false;
+}
+
+// ____ XColorScheme ____
+::sal_Int32 SAL_CALL ConfigColorScheme::getColorByIndex( ::sal_Int32 nIndex )
+    throw (uno::RuntimeException)
+{
+    if( m_bNeedsUpdate )
+        retrieveConfigColors();
+
+    if( m_nNumberOfColors > 0 )
+        return static_cast< sal_Int32 >( m_aColorSequence[ nIndex % m_nNumberOfColors ] );
+
+    // fall-back: hard-coded standard colors
+    static sal_Int32 nDefaultColors[] =  {
+        0x9999ff, 0x993366, 0xffffcc,
+        0xccffff, 0x660066, 0xff8080,
+        0x0066cc, 0xccccff, 0x000080,
+        0xff00ff, 0x00ffff, 0xffff00
+    };
+
+    static const sal_Int32 nMaxDefaultColors = sizeof( nDefaultColors ) / sizeof( sal_Int32 );
+    return nDefaultColors[ nIndex % nMaxDefaultColors ];
+}
+
+void ConfigColorScheme::notify( const OUString & rPropertyName )
+{
+    if( rPropertyName.equals( aSeriesPropName ))
+        m_bNeedsUpdate = true;
+}
+
+// ================================================================================
+
+Sequence< OUString > ConfigColorScheme::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 1 );
+    aServices[ 0 ] = C2U( "com.sun.star.chart2.ColorScheme" );
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( ConfigColorScheme,
+                             C2U( "com.sun.star.comp.chart2.ConfigDefaultColorScheme" ))
+
+// ================================================================================
+
+} //  namespace chart
