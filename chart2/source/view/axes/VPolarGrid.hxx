a1957f394019914d2f5eef234e3faf8a3c02cd92
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 18:13:38 +0000

    INTEGRATION: CWS chart2mst3 (1.3.12); FILE MERGED 2006/04/10 15:03:38 iha 1.3.12.4: api restructure axis, grids, scales and increments 2006/04/10 12:25:18 iha 1.3.12.3: api restructure axis, grids, scales and increments 2005/12/21 21:29:34 iha 1.3.12.2: remove identifiers from model objects and create an index based CID protocol instead for selection purposes 2005/11/18 20:50:41 iha 1.3.12.1: enable selection of subgrids

--- chart2/source/view/axes/VPolarGrid.hxx
+++ chart2/source/view/axes/VPolarGrid.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VPolarGrid.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:41:17 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 19:13:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,13 +35,9 @@
 #ifndef _CHART2_VPOLARGRID_HXX
 #define _CHART2_VPOLARGRID_HXX
 
-#include "VMeterBase.hxx"
+#include "VAxisOrGridBase.hxx"
 #include "TickmarkHelper.hxx"
 #include "VLineProperties.hxx"
-
-#ifndef _COM_SUN_STAR_CHART2_XGRID_HPP_
-#include <com/sun/star/chart2/XGrid.hpp>
-#endif
 
 #ifndef _COM_SUN_STAR_DRAWING_POINTSEQUENCESEQUENCE_HPP_
 #include <com/sun/star/drawing/PointSequenceSequence.hpp>
@@ -57,15 +53,17 @@ namespace chart
 */
 class PolarPlottingPositionHelper;
 
-class VPolarGrid : public VMeterBase
+class VPolarGrid : public VAxisOrGridBase
 {
 //-------------------------------------------------------------------------
 // public methods
 //-------------------------------------------------------------------------
 public:
-    VPolarGrid( const ::com::sun::star::uno::Reference<
-         ::com::sun::star::chart2::XGrid >& xGrid
-         , sal_Int32 nDimensionCount );
+    VPolarGrid( sal_Int32 nDimensionIndex, sal_Int32 nDimensionCount
+        , const ::com::sun::star::uno::Sequence<
+            ::com::sun::star::uno::Reference<
+                ::com::sun::star::beans::XPropertySet > > & rGridPropertiesList //main grid, subgrid, subsubgrid etc
+        );
     virtual ~VPolarGrid();
 
     virtual void SAL_CALL createShapes();
@@ -82,17 +80,20 @@ public:
                     , double fLogicRadius, double fLogicZ );
 
 private: //member
+    ::com::sun::star::uno::Sequence<
+        ::com::sun::star::uno::Reference<
+            ::com::sun::star::beans::XPropertySet > > m_aGridPropertiesList;//main grid, subgrid, subsubgrid etc
     PolarPlottingPositionHelper* m_pPosHelper;
     ::com::sun::star::uno::Sequence<
             ::com::sun::star::chart2::ExplicitIncrementData >   m_aIncrements;
 
     void    getAllTickInfos( sal_Int32 nDimensionIndex, ::std::vector< ::std::vector< TickInfo > >& rAllTickInfos ) const;
 
-    void    create2DRadiusGrid( const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >& xTarget
+    void    create2DRadiusGrid( const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >& xLogicTarget
                     , ::std::vector< ::std::vector< TickInfo > >& rRadiusTickInfos
                     , ::std::vector< ::std::vector< TickInfo > >& rAngleTickInfos
                     , const ::std::vector<VLineProperties>& rLinePropertiesList );
-    void    create2DAngleGrid( const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >& xTarget
+    void    create2DAngleGrid( const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >& xLogicTarget
                     , ::std::vector< ::std::vector< TickInfo > >& rRadiusTickInfos
                     , ::std::vector< ::std::vector< TickInfo > >& rAngleTickInfos
                     , const ::std::vector<VLineProperties>& rLinePropertiesList );
