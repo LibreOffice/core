dff74ff249d85aeb11d422b887c1107ee7be1439
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 17:31:53 +0000

    INTEGRATION: CWS chart2mst3 (1.3.4); FILE MERGED 2006/10/18 17:09:41 bm 1.3.4.6: RESYNC: (1.4-1.5); FILE MERGED 2006/04/11 15:41:32 bm 1.3.4.5: make creation of coordinate systems available via factory 2005/10/13 17:38:56 iha 1.3.4.4: renamed BoundedCoordinateSystem to CoordinateSystem 2005/10/07 11:51:29 bm 1.3.4.3: RESYNC: (1.3-1.4); FILE MERGED 2004/09/15 17:32:01 bm 1.3.4.2: API simplification 2004/04/01 10:53:06 bm 1.3.4.1: XChartType: may return a coordinate system now

--- chart2/source/model/main/CartesianCoordinateSystem.cxx
+++ chart2/source/model/main/CartesianCoordinateSystem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CartesianCoordinateSystem.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:08:43 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 18:31:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,68 +36,128 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "CartesianCoordinateSystem.hxx"
-#include "IdentityTransformation.hxx"
 #include "macros.hxx"
 #include "servicenames_coosystems.hxx"
 
+using namespace ::com::sun::star;
+
 using ::com::sun::star::uno::RuntimeException;
+using ::com::sun::star::uno::Reference;
 using ::com::sun::star::uno::Sequence;
 using ::rtl::OUString;
+
+namespace
+{
+
+static const ::rtl::OUString lcl_aServiceNameCartesian2d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.CartesianCoordinateSystem2d" ));
+static const ::rtl::OUString lcl_aServiceNameCartesian3d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.CartesianCoordinateSystem3d" ));
+
+static const ::rtl::OUString lcl_aImplementationNameCartesian2d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart2.CartesianCoordinateSystem2d" ));
+static const ::rtl::OUString lcl_aImplementationNameCartesian3d(
+    RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.chart2.CartesianCoordinateSystem3d" ));
+}
 
 namespace chart
 {
 
-CartesianCoordinateSystem::CartesianCoordinateSystem( sal_Int32 nDim /* = 2 */ ) :
-        m_aTransformationToCartesian( new IdentityTransformation( nDim ) ),
-        m_aTransformationFromCartesian( new IdentityTransformation( nDim ) ),
-        m_nDim( nDim )
-{
-}
+// explicit
+CartesianCoordinateSystem::CartesianCoordinateSystem(
+    const uno::Reference< uno::XComponentContext > & xContext,
+    sal_Int32 nDimensionCount /* = 2 */,
+    sal_Bool bSwapXAndYAxis /* = sal_False */ ) :
+        BaseCoordinateSystem( xContext, nDimensionCount, bSwapXAndYAxis )
+{}
+
+CartesianCoordinateSystem::CartesianCoordinateSystem(
+    const CartesianCoordinateSystem & rSource ) :
+        BaseCoordinateSystem( rSource )
+{}
 
 CartesianCoordinateSystem::~CartesianCoordinateSystem()
 {}
 
 // ____ XCoordinateSystem ____
-::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation > SAL_CALL
-    CartesianCoordinateSystem::getTransformationToCartesian()
+::rtl::OUString SAL_CALL CartesianCoordinateSystem::getCoordinateSystemType()
     throw (RuntimeException)
-{
-    return m_aTransformationToCartesian;
-}
-
-::com::sun::star::uno::Reference< ::com::sun::star::chart2::XTransformation > SAL_CALL
-    CartesianCoordinateSystem::getTransformationFromCartesian()
-    throw (RuntimeException)
-{
-    return m_aTransformationFromCartesian;
-}
-
-sal_Int32 SAL_CALL CartesianCoordinateSystem::getDimension() throw (RuntimeException)
-{
-    return m_nDim;
-}
-
-::rtl::OUString SAL_CALL CartesianCoordinateSystem::getCoordinateSystemType() throw (RuntimeException)
 {
     return CHART2_COOSYSTEM_CARTESIAN_SERVICE_NAME;
 }
 
-::rtl::OUString SAL_CALL CartesianCoordinateSystem::getViewServiceName() throw (RuntimeException)
+::rtl::OUString SAL_CALL CartesianCoordinateSystem::getViewServiceName()
+    throw (RuntimeException)
 {
     return CHART2_COOSYSTEM_CARTESIAN_VIEW_SERVICE_NAME;
 }
 
-// ____ XServiceInfo ____
+// ____ XCloneable ____
+uno::Reference< util::XCloneable > SAL_CALL CartesianCoordinateSystem::createClone()
+    throw (RuntimeException)
+{
+    return Reference< util::XCloneable >( new CartesianCoordinateSystem( *this ));
+}
 
+// ____ XServiceInfo ____
 Sequence< OUString > CartesianCoordinateSystem::getSupportedServiceNames_Static()
 {
-    Sequence< OUString > aServices;
-//     Sequence< OUString > aServices( 1 );
-//     aServices[ 0 ] = OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.chart2.CoordinateSystem" ));
+    Sequence< OUString > aServices( 1 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_CARTESIAN_SERVICE_NAME;
     return aServices;
 }
 
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
 APPHELPER_XSERVICEINFO_IMPL( CartesianCoordinateSystem,
                              C2U( "com.sun.star.comp.chart.CartesianCoordinateSystem" ))
+
+
+// =================================
+// ==== CartesianCoordinateSystem2d ====
+// =================================
+
+CartesianCoordinateSystem2d::CartesianCoordinateSystem2d(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        CartesianCoordinateSystem( xContext, 2, sal_False )
+{}
+
+CartesianCoordinateSystem2d::~CartesianCoordinateSystem2d()
+{}
+
+// ____ XServiceInfo ____
+Sequence< OUString > CartesianCoordinateSystem2d::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_CARTESIAN_SERVICE_NAME;
+    aServices[ 1 ] = lcl_aServiceNameCartesian2d;
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( CartesianCoordinateSystem2d, lcl_aImplementationNameCartesian2d )
+
+// =================================
+// ==== CartesianCoordinateSystem3d ====
+// =================================
+
+CartesianCoordinateSystem3d::CartesianCoordinateSystem3d(
+    const uno::Reference< uno::XComponentContext > & xContext ) :
+        CartesianCoordinateSystem( xContext, 3, sal_False )
+{}
+
+CartesianCoordinateSystem3d::~CartesianCoordinateSystem3d()
+{}
+
+// ____ XServiceInfo ____
+Sequence< OUString > CartesianCoordinateSystem3d::getSupportedServiceNames_Static()
+{
+    Sequence< OUString > aServices( 2 );
+    aServices[ 0 ] = CHART2_COOSYSTEM_CARTESIAN_SERVICE_NAME;
+    aServices[ 1 ] = lcl_aServiceNameCartesian3d;
+    return aServices;
+}
+
+// implement XServiceInfo methods basing upon getSupportedServiceNames_Static
+APPHELPER_XSERVICEINFO_IMPL( CartesianCoordinateSystem3d, lcl_aImplementationNameCartesian3d )
 
 }  // namespace chart
