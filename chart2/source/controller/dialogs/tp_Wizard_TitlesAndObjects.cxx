c887faceaca3439647bf8264f7213b77f756ee74
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 16:50:14 +0000

    INTEGRATION: CWS chart2mst3 (1.1.2); FILE ADDED 2007/05/18 16:12:17 bm 1.1.2.26: #i74635# ResMgr cleanup ResId -> SchResId 2007/02/27 16:46:42 iha 1.1.2.25: change string according to wizard spec 2007/02/08 21:25:35 iha 1.1.2.24: resync m195 -> m202 2007/02/02 23:03:06 iha 1.1.2.23: some resource changes according to wizard spec 2006/11/10 21:03:07 iha 1.1.2.22: prevent immidiate redraw in wizard and according dialogs (important for charts with many data) 2006/10/27 14:59:20 iha 1.1.2.21: name consolidation 2006/10/21 11:02:40 iha 1.1.2.20: removed string STR_GRIDLINES use TR_OBJECT_GRIDS instead 2006/04/10 15:03:32 iha 1.1.2.19: api restructure axis, grids, scales and increments 2006/03/10 10:20:41 iha 1.1.2.18: don't use leading underscore 2006/01/24 10:35:46 bm 1.1.2.17: ChartModelHelper::findDiagram now also works with chart2::XChartDocument 2006/01/23 15:55:08 iha 1.1.2.16: #i61054# disable next button at wizard correctly 2006/01/12 17:54:54 iha 1.1.2.15: change name of wizard step 4 according to spec 2006/01/06 20:27:27 iha 1.1.2.14: added legendposition to wizard and restructured legend postiion control classes 2005/12/06 19:00:31 iha 1.1.2.13: #i58065# use grid HelpIds for Grid ChaeckBoxes 2005/11/18 18:19:53 iha 1.1.2.12: restructuring for proper subgrid support 2005/11/15 10:55:34 iha 1.1.2.11: only change model if controls have changed 2005/11/14 18:16:49 iha 1.1.2.10: don't change model during initialization of controls 2005/11/14 15:59:30 iha 1.1.2.9: remove direct view update call as automatic updates are now available and guard against to many view updates 2005/10/11 09:20:18 bm 1.1.2.8: license header change 2005/06/16 12:55:35 iha 1.1.2.7: create legend on demand 2004/06/09 17:46:10 iha 1.1.2.6: live update 2004/05/17 17:33:53 iha 1.1.2.5: provide object naming; resource cleanup 2004/05/05 20:52:27 iha 1.1.2.4: #i20344# added page title 2004/04/27 13:32:41 iha 1.1.2.3: warnings removed 2004/04/22 15:00:45 iha 1.1.2.2: #i20344# chabged legend, grid and title resources 2004/04/07 21:27:08 iha 1.1.2.1: #i20344# new

---
+++ chart2/source/controller/dialogs/tp_Wizard_TitlesAndObjects.cxx
@@ -0,0 +1,195 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_Wizard_TitlesAndObjects.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-05-22 17:50:14 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_chart2.hxx"
+
+#include "tp_Wizard_TitlesAndObjects.hxx"
+#include "tp_Wizard_TitlesAndObjects.hrc"
+#include "Strings.hrc"
+#include "res_Titles.hxx"
+#include "res_LegendPosition.hxx"
+#include "ResId.hxx"
+#include "HelpIds.hrc"
+#include "macros.hxx"
+#include "ChartModelHelper.hxx"
+#include "AxisHelper.hxx"
+#include "LegendHelper.hxx"
+#include "NoWarningThisInCTOR.hxx"
+#include "ControllerLockGuard.hxx"
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::chart2;
+
+
+TitlesAndObjectsTabPage::TitlesAndObjectsTabPage( svt::OWizardMachine* pParent
+        , const uno::Reference< XChartDocument >& xChartModel
+        , const uno::Reference< uno::XComponentContext >& xContext )
+        : OWizardPage( pParent, SchResId(TP_WIZARD_TITLEANDOBJECTS) )
+        , m_aFT_TitleDescription( this, SchResId( FT_TITLEDESCRIPTION ) )
+        , m_aFL_Vertical( this, SchResId( FL_VERTICAL ) )
+        , m_apTitleResources( new TitleResources(this) )
+        , m_apLegendPositionResources( new LegendPositionResources(this,xContext) )
+        , m_aFL_Grids( this, SchResId( FL_GRIDS ) )
+        , m_aCB_Grid_X( this, SchResId( CB_X_SECONDARY ) )
+        , m_aCB_Grid_Y( this, SchResId( CB_Y_SECONDARY ) )
+        , m_aCB_Grid_Z( this, SchResId( CB_Z_SECONDARY ) )
+        , m_xChartModel( xChartModel )
+        , m_xCC( xContext )
+        , m_bCommitToModel( true )
+        , m_aTimerTriggeredControllerLock( uno::Reference< frame::XModel >( m_xChartModel, uno::UNO_QUERY ) )
+{
+    FreeResource();
+
+    this->SetText( String( SchResId( STR_PAGE_CHART_ELEMENTS ) ) );
+
+    Font aFont( m_aFT_TitleDescription.GetControlFont() );
+    aFont.SetWeight( WEIGHT_BOLD );
+    m_aFT_TitleDescription.SetControlFont( aFont );
+
+    m_aCB_Grid_X.SetHelpId( HID_SCH_CB_XGRID );
+    m_aCB_Grid_Y.SetHelpId( HID_SCH_CB_YGRID );
+    m_aCB_Grid_Z.SetHelpId( HID_SCH_CB_ZGRID );
+
+    m_apTitleResources->SetUpdateDataHdl( LINK( this, TitlesAndObjectsTabPage, ChangeHdl ));
+    m_apLegendPositionResources->SetChangeHdl( LINK( this, TitlesAndObjectsTabPage, ChangeHdl ));
+
+    m_aCB_Grid_X.SetToggleHdl( LINK( this, TitlesAndObjectsTabPage, ChangeHdl ));
+    m_aCB_Grid_Y.SetToggleHdl( LINK( this, TitlesAndObjectsTabPage, ChangeHdl ));
+    m_aCB_Grid_Z.SetToggleHdl( LINK( this, TitlesAndObjectsTabPage, ChangeHdl ));
+}
+
+TitlesAndObjectsTabPage::~TitlesAndObjectsTabPage()
+{
+}
+
+void TitlesAndObjectsTabPage::initializePage()
+{
+    m_bCommitToModel = false;
+
+    //init titles
+    {
+        TitleDialogData aTitleInput;
+        aTitleInput.readFromModel( uno::Reference< frame::XModel >( m_xChartModel, uno::UNO_QUERY) );
+        m_apTitleResources->writeToResources( aTitleInput );
+    }
+
+    //init legend
+    {
+        m_apLegendPositionResources->writeToResources( uno::Reference< frame::XModel >( m_xChartModel, uno::UNO_QUERY) );
+    }
+
+    //init grid checkboxes
+    {
+        uno::Reference< XDiagram > xDiagram = ChartModelHelper::findDiagram( m_xChartModel );
+        uno::Sequence< sal_Bool > aPossibilityList;
+        uno::Sequence< sal_Bool > aExistenceList;
+        AxisHelper::getAxisOrGridPossibilities( aPossibilityList, xDiagram, sal_False );
+        AxisHelper::getAxisOrGridExcistence( aExistenceList, xDiagram, sal_False );
+        m_aCB_Grid_X.Enable( aPossibilityList[0] );
+        m_aCB_Grid_Y.Enable( aPossibilityList[1] );
+        m_aCB_Grid_Z.Enable( aPossibilityList[2] );
+        m_aCB_Grid_X.Check( aExistenceList[0] );
+        m_aCB_Grid_Y.Check( aExistenceList[1] );
+        m_aCB_Grid_Z.Check( aExistenceList[2] );
+    }
+
+    m_bCommitToModel = true;
+}
+
+sal_Bool TitlesAndObjectsTabPage::commitPage( COMMIT_REASON eReason )
+{
+    if( m_apTitleResources->IsModified() ) //titles may have changed in the meanwhile
+        commitToModel();
+    return sal_True;//return false if this page should not be left
+}
+
+void TitlesAndObjectsTabPage::commitToModel()
+{
+    bool bChanged = false;
+
+    m_aTimerTriggeredControllerLock.startTimer();
+    uno::Reference< frame::XModel >  xModel( m_xChartModel, uno::UNO_QUERY);
+
+    ControllerLockGuard aLockedControllers( xModel );
+
+    //commit title changes to model
+    {
+        TitleDialogData aTitleOutput;
+        m_apTitleResources->readFromResources( aTitleOutput );
+        bChanged = bChanged || aTitleOutput.writeDifferenceToModel( xModel, m_xCC );
+        m_apTitleResources->ClearModifyFlag();
+    }
+
+    //commit legend changes to model
+    {
+        bChanged = true;
+        m_apLegendPositionResources->writeToModel( xModel );
+    }
+
+    //commit grid changes to model
+    {
+        uno::Reference< XDiagram > xDiagram = ChartModelHelper::findDiagram( xModel );
+        uno::Sequence< sal_Bool > aOldExistenceList;
+        AxisHelper::getAxisOrGridExcistence( aOldExistenceList, xDiagram, sal_False );
+        uno::Sequence< sal_Bool > aNewExistenceList(aOldExistenceList);
+        aNewExistenceList[0]=m_aCB_Grid_X.IsChecked();
+        aNewExistenceList[1]=m_aCB_Grid_Y.IsChecked();
+        aNewExistenceList[2]=m_aCB_Grid_Z.IsChecked();
+        AxisHelper::changeVisibilityOfGrids( xDiagram
+                , aOldExistenceList, aNewExistenceList, m_xCC );
+    }
+}
+
+IMPL_LINK( TitlesAndObjectsTabPage, ChangeHdl, void *, EMPTYARG )
+{
+    if( m_bCommitToModel )
+        commitToModel();
+    return 0;
+}
+
+sal_Bool TitlesAndObjectsTabPage::determineNextButtonState()
+{
+    return sal_False;
+}
+
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
