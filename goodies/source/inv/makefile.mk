8f19d3813b0e41f916fc1723ddfcec17175f5bfc
Vladimir Glazounov <vg@openoffice.org>
2007-05-25 10:13:02 +0000

    INTEGRATION: CWS mingwport04 (1.13.12); FILE MERGED 2007/04/17 15:00:04 vg 1.13.12.1: #i75844# MinGW port efforts part 2

--- goodies/source/inv/makefile.mk
+++ goodies/source/inv/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 15:00:04 $
+#   last change: $Author: vg $ $Date: 2007-05-25 11:13:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -129,8 +129,10 @@ $(MISC)$/$(SHL1TARGET).def: makefile.mk
     @echo -------------------------------------------
     @echo DEF-File erstellen $@
     @echo LIBRARY     $(DLLNAME)                    >$@
+.IF "$(COM)"!="GCC"
     @echo DESCRIPTION 'Star Cube DLL'               >>$@
     @echo DATA                READ WRITE NONSHARED  >>$@
+.ENDIF
     @echo EXPORTS                                   >>$@
     @echo     StartInvader    @22                    >>$@
 
