aadc91a565b72a20e7a3789bb4473c7715e5fca1
Vladimir Glazounov <vg@openoffice.org>
2007-05-25 09:53:46 +0000

    INTEGRATION: CWS mingwport04 (1.2.4); FILE MERGED 2007/04/17 13:11:48 vg 1.2.4.1: #i75844# MinGW port efforts part 2

--- bridges/source/cpp_uno/mingw_intel/cpp2uno.cxx
+++ bridges/source/cpp_uno/mingw_intel/cpp2uno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cpp2uno.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-03-26 13:14:31 $
+ *  last change: $Author: vg $ $Date: 2007-05-25 10:53:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_bridges.hxx"
 
 #include <com/sun/star/uno/genfunc.hxx>
 #include "com/sun/star/uno/RuntimeException.hpp"
