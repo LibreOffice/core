7e47093865d5232f67538e94ae76bccc2b2a7769
Vladimir Glazounov <vg@openoffice.org>
2007-05-25 10:05:34 +0000

    INTEGRATION: CWS mingwport04 (1.2.6); FILE MERGED 2007/04/18 13:23:35 vg 1.2.6.1: #i75844# MinGW port efforts part II

--- external/mingwheaders/makefile.mk
+++ external/mingwheaders/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2007-05-10 13:19:03 $
+#   last change: $Author: vg $ $Date: 2007-05-25 11:05:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -52,63 +52,89 @@ MINGW_W32API_INCLUDE_DIR=$(COMPATH)$/include/
 MINGW_W32API_LIB_DIR=$(COMPATH)$/lib/
 .ENDIF
 
+SYS_INCLUDE_DIR=$(MINGW_INCLUDE_DIR)$/sys/
+
 PSDK_INCLUDE_DIR=$(PSDK_HOME)$/include$/
 
 ATL_INCLUDE_DIR=$(PSDK_HOME)$/include$/atl$/
 
+DIRECTXSDK_INCLUDE_DIR=$(DIRECTXSDK_HOME)$/include$/
+
 FILES_TO_COPY_FROM_MINGW=excpt.h \
              tchar.h
 
-FILES_TO_COPY_FROM_W32API=basetyps.h \
+FILES_TO_COPY_FROM_W32API=amvideo.h \
+              basetyps.h \
+              bdatypes.h \
+              oaidl.h \
               objfwd.h \
-              rpcndr.h \
+              objidl.h \
+              uxtheme.h \
               winbase.h \
               wininet.h \
               winnt.h \
+              winuser.h \
               winver.h \
               wtypes.h
 
-FILES_TO_COPY_FROM_PSDK=adoint.h \
+FILES_TO_COPY_FROM_SYS=stat.h
+
+FILES_TO_COPY_FROM_PSDK=adoctint.h \
+            adodef.h \
+            adoguids.h \
+            adoint.h \
             commctrl.h \
-            exdispid.h \
+            control.h \
+            filter.h \
+            gdiplusbitmap.h \
+            gdiplusbrush.h \
             gdiplusenums.h \
+            gdiplusfont.h \
             gdiplusheaders.h \
+            gdiplusimageattributes.h \
             gdiplusimaging.h \
+            gdiplusmatrix.h \
+            gdipluspath.h \
+            gdipluspen.h \
+            gdiplusregion.h \
             imagehlp.h \
+            mapinls.h \
             mapiwin.h \
-            oaidl.h \
-            objidl.h \
+                msdasc.h \
+            multimon.h \
             ocidl.h \
-            oleauto.h \
-            olectl.h \
             oledb.h \
+            oleidl.h \
             propidl.h \
             qedit.h \
             shlobj.h \
+            shobjidl.h \
             shtypes.h \
             specstrings.h \
             strmif.h \
+            strsafe.h \
+            urlmon.h \
             wincrypt.h \
-            winerror.h \
             wingdi.h \
             winsock2.h \
             ws2tcpip.h \
             wspiapi.h \
+            adoid.h \
             amstream.h \
             austream.h \
             ddstream.h \
             dispex.h \
-            filter.h \
+            dshow.h \
+            exdispid.h \
             gdiplus.h \
             mmstream.h \
-                msdasc.h \
             msxml.h \
-            oleidl.h \
+            oleauto.h \
+            olectl.h \
             qos.h \
-            shobjidl.h \
             shlguid.h \
             transact.h \
-            urlmon.h
+            winerror.h
 
 FILES_TO_COPY_FROM_ATL=atlbase.h \
                atlcom.h \
@@ -117,22 +143,25 @@ FILES_TO_COPY_FROM_ATL=atlbase.h \
                atlwin.h \
                statreg.h
 
+FILES_TO_COPY_FROM_DIRECTXSDK=dxtrans.h
+
 $(MISC)$/mingwheader_patch_all : $(MISC)$/mingwheader_patch $(LB)$/libmsvcrt.a
 
 $(MISC)$/mingwheader_patch : $(MISC)$/mingwheader_copy
-    -patch -bd $(MISC)$/mingw -p0 -N -f -Z <mingw_headers.patch
-    $(COPY) $(MISC)$/mingw$/include/adoctint.h ../ado
+    patch -bd $(MISC)$/mingw -p0 -N -f -Z <mingw_headers.patch
     $(TOUCH) $(MISC)$/mingwheader_add
     $(TOUCH) $(MISC)$/mingwheader_patch
 
 $(MISC)$/mingwheader_copy:
-    $(MKDIR) $(MISC)$/mingw$/include
-    $(MKDIR) $(MISC)$/mingw$/include/atl
+    $(MKDIRHIER) $(MISC)$/mingw$/include
+    $(MKDIRHIER) $(MISC)$/mingw$/include/atl
+    $(MKDIRHIER) $(MISC)$/mingw$/include/sys
     $(COPY) $(FILES_TO_COPY_FROM_MINGW:^$(MINGW_INCLUDE_DIR)) $(MISC)$/mingw$/include
     $(COPY) $(FILES_TO_COPY_FROM_W32API:^$(MINGW_W32API_INCLUDE_DIR)) $(MISC)$/mingw$/include
+    $(COPY) $(FILES_TO_COPY_FROM_SYS:^$(SYS_INCLUDE_DIR)) $(MISC)$/mingw$/include$/sys
     $(COPY) $(FILES_TO_COPY_FROM_PSDK:^$(PSDK_INCLUDE_DIR)) $(MISC)$/mingw$/include
     $(COPY) $(FILES_TO_COPY_FROM_ATL:^$(ATL_INCLUDE_DIR)) $(MISC)$/mingw$/include$/atl
-    $(COPY) ../ado/adoctint.h $(MISC)$/mingw$/include
+    $(COPY) $(FILES_TO_COPY_FROM_DIRECTXSDK:^$(DIRECTXSDK_INCLUDE_DIR)) $(MISC)$/mingw$/include
     $(TOUCH) $(MISC)$/mingwheader_copy
 
 $(LB)$/libmsvcrt.a:
