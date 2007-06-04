e1d7d160640f34ece5b4822fdf84f17a42ca4fc4
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-04 12:32:27 +0000

    INTEGRATION: CWS qadev29 (1.1.2); FILE ADDED 2007/03/14 12:36:35 cn 1.1.2.2: #i75120# final version of interface test 2007/03/05 20:35:32 cn 1.1.2.1: #i75120: new initial version of this interface test

---
+++ qadevOOo/tests/java/ifc/beans/_XPropertyContainer.java
@@ -0,0 +1,320 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: _XPropertyContainer.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-06-04 13:32:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+package ifc.beans;
+
+import com.sun.star.beans.IllegalTypeException;
+import com.sun.star.beans.NotRemoveableException;
+import com.sun.star.beans.PropertyExistException;
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.uno.Any;
+import lib.MultiMethodTest;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertyContainer;
+import lib.Status;
+import lib.StatusException;
+
+/**
+ * Testing <code>com.sun.star.beans.XPropertyAccess</code>
+ * interface methods :
+ * <ul>
+ *  <li><code>addProperty()</code></li>
+ *  <li><code>removeProperty()</code></li>
+ * </ul>
+ * @see com.sun.star.beans.XPropertyAccess
+ */
+public class _XPropertyContainer extends MultiMethodTest {
+
+    /**
+     * oObj filled by MultiMethodTest
+     */
+    public XPropertyContainer oObj = null;// oObj filled by MultiMethodTest
+
+    /**
+     * object relation X<CODE>PropertyAccess.propertyNotRemovable</CODE><br>
+     * This relation must be filled from the module. It contains a property which must
+     * be a property of the implementaion object.
+     */
+    private String propertyNotRemovable = null;
+
+    /**
+     * checks if the object relation <CODE>XPropertyAccess.propertyNotRemovable</CODE>
+     * is available
+     */
+    public void before() {
+        propertyNotRemovable = (String) tEnv.getObjRelation("XPropertyContainer.propertyNotRemovable");
+        if (propertyNotRemovable == null) {
+            throw new StatusException(Status.failed("Object raltion 'XPropertyAccess.propertyNotRemovable' is null"));
+        }
+    }
+
+
+
+    /**
+     * Test calls the method and checks if the returned sequence contanis a propterty which is named
+     * in the object relation <code>XPropertyAccess.propertyNotRemovable</code>.
+     */
+    public void _addProperty() {
+
+        boolean ok = true;
+        boolean test = true;
+        boolean exp = false;
+
+        try {
+            log.println("try to add following property:\n" +
+                    "\t('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) ...");
+            oObj.addProperty("myXPropertContainerProperty" , com.sun.star.beans.PropertyAttribute.MAYBEVOID, null);
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("ERROR: Exception was thrown while adding following property: " +
+                    "('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) : " +
+                    ex.toString());
+            log.println("... FAILED");
+            test = false;
+        } catch (PropertyExistException ex) {
+            log.println("ERROR: Exception was thrown while adding following property: " +
+                    "('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) : " +
+                    ex.toString());
+            log.println("... FAILED");
+            test = false;
+        } catch (IllegalTypeException ex) {
+            log.println("ERROR: Exception was thrown while adding following property: " +
+                    "('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) : " +
+                    ex.toString());
+           log.println("... FAILED");
+           test = false;
+        }
+
+        if ( test){
+            log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+        try {
+            log.println("add following property second time:\n" +
+                    "\t('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) ...");
+            oObj.addProperty("myXPropertContainerProperty" , com.sun.star.beans.PropertyAttribute.MAYBEVOID, null);
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("ERROR: wrong Exception was thrown while adding following property: " +
+                    "('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) : " +
+                    "Expected: 'PropertyExistException'\n" +
+                    "Got:      'IllegalArgumentException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        } catch (PropertyExistException ex) {
+            log.println("OK: expected Exception was thrown." + ex.toString());
+            test = true;
+            exp = true;
+        } catch (IllegalTypeException ex) {
+            log.println("ERROR: wrong Exception was thrown while adding following property: " +
+                    "('myXPropertContainerProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null) : " +
+                    "Expected: 'PropertyExistException'\n" +
+                    "Got:      'IllegalTypeException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        }
+        if (! exp){
+            log.println("FAILED: expected exception 'UnknownPropertyException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+        try {
+            log.println("try to add following property:\n" +
+                    "\t('IllegalTypeProperty', com.sun.star.beans.PropertyAttribute.MAYBEVOID, Any.VOID) ...");
+            oObj.addProperty("IllegalTypeProperty", com.sun.star.beans.PropertyAttribute.MAYBEVOID, Any.VOID);
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("ERROR: wrong Exception was thrown while adding following property: " +
+                    "'', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null\n" +
+                    "Expected: 'IllegalTypeException'\n" +
+                    "Got:      'IllegalArgumentException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        } catch (PropertyExistException ex) {
+            log.println("ERROR: wrong Exception was thrown while adding following property: " +
+                    "'', com.sun.star.beans.PropertyAttribute.MAYBEVOID, null\n" +
+                    "Expected: 'IllegalArgumentException'\n" +
+                    "Got:      'PropertyExistException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        } catch (IllegalTypeException ex) {
+            log.println("OK: expected Exception was thrown." + ex.toString());
+            test = true;
+            exp = true;
+        }
+
+        if (! exp){
+            log.println("FAILED: expected exception 'IllegalTypeException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+        try {
+            short attrib = -1;
+            log.println("add following property:\n" +
+                    "'', -1, new Object");
+            oObj.addProperty("", attrib, null);
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("OK: expected Exception was thrown." + ex.toString());
+            test = true;
+            exp = true;
+        } catch (PropertyExistException ex) {
+            log.println("ERROR: wrong Exception was thrown while adding following property: " +
+                    "'invalidPropertyAttribute', -1, null\n" +
+                    "Expected: 'IllegalTypeException'\n" +
+                    "Got:      'PropertyExistException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        } catch (IllegalTypeException ex) {
+            log.println("ERROR: unexpected Exception was thrown while adding following property: " +
+                    "'', -1, null\n" +
+                    "Expected: 'IllegalArgumentException'\n" +
+                    "Got:      'IllegalTypeException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        }
+
+        if (! exp){
+            log.println("FAILED: expected exception 'IllegalArgumentException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        ok &= test;
+
+        tRes.tested("addProperty()", ok );
+        return;
+    }
+
+    /**
+     * Test calls the method and checks if the returned sequence contanis a propterty which is named
+     * in the object relation <code>XPropertyAccess.propertyNotRemovable</code>.
+     */
+    public void _removeProperty() {
+
+        requiredMethod("addProperty()");
+
+        boolean ok = true;
+        boolean test = true;
+        boolean exp = false;
+
+        try {
+            log.println("remove 'myXPropertContainerProperty'");
+            oObj.removeProperty("myXPropertContainerProperty");
+        } catch (NotRemoveableException ex) {
+            log.println("ERROR: could not remote property 'myXPropertContainerProperty'" + ex.toString());
+            log.println("... FAILED");
+            test = false;
+        } catch (UnknownPropertyException ex) {
+            log.println("ERROR: could not remote property 'myXPropertContainerProperty'" + ex.toString());
+            log.println("... FAILED");
+            test = false;
+        }
+        if ( test){
+            log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp=false;
+
+        try {
+            log.println("remove not removeable property '" + propertyNotRemovable + "'") ;
+            oObj.removeProperty(propertyNotRemovable);
+        } catch (NotRemoveableException ex) {
+            log.println("OK: expected Exception was thrown: " + ex.toString());
+            test = true;
+            exp = true;
+        } catch (UnknownPropertyException ex) {
+            log.println("ERROR: wrong exception was thrown.\n" +
+                    "Expected: 'NotRemoveableException'\n" +
+                    "Got:      'UnknownPropertyException'\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        }
+
+        if (! exp){
+            log.println("FAILED: expected exception 'NotRemoveableException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+
+        try {
+            log.println("remove property 'thisPropertyDoesNotExist'");
+            oObj.removeProperty("thisPropertyDoesNotExist");
+        } catch (UnknownPropertyException ex) {
+            log.println("OK: expected Exception was thrown: " + ex.toString());
+            test = true;
+            exp = true;
+        } catch (NotRemoveableException ex) {
+            log.println("ERROR: wrong exception was thrown.\n" +
+                    "Expected: 'UnknownPropertyException'\n" +
+                    "Got:      'NotRemoveableException\n" +
+                    ex.toString());
+            log.println("... FAILED");
+            exp = true;
+        }
+        if (! exp){
+            log.println("FAILED: expected exception 'UnknownPropertyException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        tRes.tested("removeProperty()", ok );
+        return;
+    }
+
+
+}    /// finish class XPropertyContainer
