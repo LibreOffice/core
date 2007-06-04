0f1b3130c2f30fa8a3e22bbc175fa7c492b8f93e
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-04 12:35:59 +0000

    INTEGRATION: CWS qadev29 (1.5.36); FILE MERGED 2007/03/07 19:52:10 cn 1.5.36.1: #i75120# update comments for new interfaces XPropertyAccess and XPropertyContainer. Test not ready yet.

--- qadevOOo/tests/java/mod/_forms/OHiddenModel.java
+++ qadevOOo/tests/java/mod/_forms/OHiddenModel.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OHiddenModel.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:12:08 $
+ *  last change: $Author: ihi $ $Date: 2007-06-04 13:35:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,6 +34,7 @@
  ************************************************************************/
 package mod._forms;
 
+import com.sun.star.beans.PropertyValue;
 import java.io.PrintWriter;
 
 import lib.StatusException;
@@ -56,53 +57,57 @@ import com.sun.star.util.XCloseable;
 
 
 /**
-* Test for object which is represented by service
-* <code>com.sun.star.form.component.HiddenControl</code>. <p>
-* Object implements the following interfaces :
-* <ul>
-*  <li> <code>com::sun::star::io::XPersistObject</code></li>
-*  <li> <code>com::sun::star::container::XChild</code></li>
-*  <li> <code>com::sun::star::form::FormControlModel</code></li>
-*  <li> <code>com::sun::star::form::component::HiddenControl</code></li>
-*  <li> <code>com::sun::star::form::FormComponent</code></li>
+ * Test for object which is represented by service
+ * <code>com.sun.star.form.component.HiddenControl</code>. <p>
+ * Object implements the following interfaces :
+ * <ul>
+ *  <li> <code>com::sun::star::io::XPersistObject</code></li>
+ *  <li> <code>com::sun::star::container::XChild</code></li>
+ *  <li> <code>com::sun::star::form::FormControlModel</code></li>
+ *  <li> <code>com::sun::star::form::component::HiddenControl</code></li>
+ *  <li> <code>com::sun::star::form::FormComponent</code></li>
+ *  <li> <code>com::sun::star::beans::XPropertyAccess</code></li>
+*  <li> <code>com::sun::star::beans::XPropertyContainer</code></li>
 *  <li> <code>com::sun::star::beans::XPropertySet</code></li>
-*  <li> <code>com::sun::star::beans::XFastPropertySet</code></li>
-*  <li> <code>com::sun::star::beans::XPropertyState</code></li>
-*  <li> <code>com::sun::star::container::XNamed</code></li>
-*  <li> <code>com::sun::star::beans::XMultiPropertySet</code></li>
-*  <li> <code>com::sun::star::lang::XComponent</code></li>
-* </ul> <p>
-* This object test <b> is NOT </b> designed to be run in several
-* threads concurently.
-* @see com.sun.star.io.XPersistObject
-* @see com.sun.star.container.XChild
-* @see com.sun.star.form.FormControlModel
-* @see com.sun.star.form.component.HiddenControl
-* @see com.sun.star.form.FormComponent
+ *  <li> <code>com::sun::star::beans::XFastPropertySet</code></li>
+ *  <li> <code>com::sun::star::beans::XPropertyState</code></li>
+ *  <li> <code>com::sun::star::container::XNamed</code></li>
+ *  <li> <code>com::sun::star::beans::XMultiPropertySet</code></li>
+ *  <li> <code>com::sun::star::lang::XComponent</code></li>
+ * </ul> <p>
+ * This object test <b> is NOT </b> designed to be run in several
+ * threads concurently.
+ * @see com.sun.star.io.XPersistObject
+ * @see com.sun.star.container.XChild
+ * @see com.sun.star.form.FormControlModel
+ * @see com.sun.star.form.component.HiddenControl
+ * @see com.sun.star.form.FormComponent
+ * @see com.sun.star.beans.XPropertyAccess
+* @see com.sun.star.beans.XPropertyContainer
 * @see com.sun.star.beans.XPropertySet
-* @see com.sun.star.beans.XFastPropertySet
-* @see com.sun.star.beans.XPropertyState
-* @see com.sun.star.container.XNamed
-* @see com.sun.star.beans.XMultiPropertySet
-* @see com.sun.star.lang.XComponent
-* @see ifc.io._XPersistObject
-* @see ifc.container._XChild
-* @see ifc.form._FormControlModel
-* @see ifc.form.component._HiddenControl
-* @see ifc.form._FormComponent
-* @see ifc.beans._XPropertySet
-* @see ifc.beans._XFastPropertySet
-* @see ifc.beans._XPropertyState
-* @see ifc.container._XNamed
-* @see ifc.beans._XMultiPropertySet
-* @see ifc.lang._XComponent
-*/
+ * @see com.sun.star.beans.XFastPropertySet
+ * @see com.sun.star.beans.XPropertyState
+ * @see com.sun.star.container.XNamed
+ * @see com.sun.star.beans.XMultiPropertySet
+ * @see com.sun.star.lang.XComponent
+ * @see ifc.io._XPersistObject
+ * @see ifc.container._XChild
+ * @see ifc.form._FormControlModel
+ * @see ifc.form.component._HiddenControl
+ * @see ifc.form._FormComponent
+ * @see ifc.beans._XPropertySet
+ * @see ifc.beans._XFastPropertySet
+ * @see ifc.beans._XPropertyState
+ * @see ifc.container._XNamed
+ * @see ifc.beans._XMultiPropertySet
+ * @see ifc.lang._XComponent
+ */
 public class OHiddenModel extends TestCase {
     XComponent xDrawDoc;
 
     /**
-    * Creates Drawing document.
-    */
+     * Creates Drawing document.
+     */
     protected void initialize(TestParameters tParam, PrintWriter log) {
         SOfficeFactory SOF = SOfficeFactory.getFactory(((XMultiServiceFactory) tParam.getMSF()));
 
@@ -117,14 +122,14 @@ public class OHiddenModel extends TestCase {
     }
 
     /**
-    * Disposes drawing document.
-    */
+     * Disposes drawing document.
+     */
     protected void cleanup(TestParameters tParam, PrintWriter log) {
         log.println("    disposing xDrawDoc ");
 
         try {
             XCloseable closer = (XCloseable) UnoRuntime.queryInterface(
-                                        XCloseable.class, xDrawDoc);
+                    XCloseable.class, xDrawDoc);
             closer.close(true);
         } catch (com.sun.star.util.CloseVetoException e) {
             log.println("couldn't close document");
@@ -134,17 +139,17 @@ public class OHiddenModel extends TestCase {
     }
 
     /**
-    * Creates hidden component, then adds Form into draw page,
-    * and inserts the component into Form components' collection.
-    *     Object relations created :
-    * <ul>
-    *  <li> <code>'OBJNAME'</code> for
-    *      {@link ifc.io._XPersistObject} : name of service which is
-    *    represented by this object. </li>
-    * </ul>
-    */
+     * Creates hidden component, then adds Form into draw page,
+     * and inserts the component into Form components' collection.
+     *     Object relations created :
+     * <ul>
+     *  <li> <code>'OBJNAME'</code> for
+     *      {@link ifc.io._XPersistObject} : name of service which is
+     *    represented by this object. </li>
+     * </ul>
+     */
     protected synchronized TestEnvironment createTestEnvironment(TestParameters Param,
-                                                                 PrintWriter log) {
+            PrintWriter log) {
         XInterface oObj = null;
 
 
@@ -166,12 +171,12 @@ public class OHiddenModel extends TestCase {
             Object frm = nc.getByName("OHiddenModelForm");
 
             XNameContainer frmNC = (XNameContainer) UnoRuntime.queryInterface(
-                                           XNameContainer.class, frm);
+                    XNameContainer.class, frm);
 
             frmNC.insertByName("OHiddenModel", ctrl);
             oObj = (XInterface) AnyConverter.toObject(
-                           new Type(XInterface.class),
-                           frmNC.getByName("OHiddenModel"));
+                    new Type(XInterface.class),
+                    frmNC.getByName("OHiddenModel"));
         } catch (com.sun.star.lang.WrappedTargetException e) {
             e.printStackTrace(log);
             throw new StatusException("Can't create and add control", e);
@@ -194,6 +199,12 @@ public class OHiddenModel extends TestCase {
 
         log.println("adding DrawDocument as obj relation to environment");
         tEnv.addObjRelation("OBJNAME", "stardiv.one.form.component.Hidden");
+
+        PropertyValue prop = new PropertyValue();
+        prop.Name = "Name";
+        prop.Value = "new Text since XPropertyAccess";
+        tEnv.addObjRelation("XPropertyAccess.propertyToChange", prop);
+        tEnv.addObjRelation("XPropertyContainer.propertyNotRemovable", "Name");
 
         return tEnv;
     } // finish method getTestEnvironment
