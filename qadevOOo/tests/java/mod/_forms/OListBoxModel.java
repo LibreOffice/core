31de66f0606e1e6ae6783daeec6568a2618540cf
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-04 12:36:42 +0000

    INTEGRATION: CWS qadev29 (1.5.36); FILE MERGED 2007/03/07 19:53:46 cn 1.5.36.1: #i75120# bugifx: initialize the super class first, then fill member variables with content

--- qadevOOo/tests/java/mod/_forms/OListBoxModel.java
+++ qadevOOo/tests/java/mod/_forms/OListBoxModel.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OListBoxModel.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:13:35 $
+ *  last change: $Author: ihi $ $Date: 2007-06-04 13:36:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,6 +72,8 @@ import util.DBTools;
 *  <li> <code>com::sun::star::container::XNamed</code></li>
 *  <li> <code>com::sun::star::lang::XComponent</code></li>
 *  <li> <code>com::sun::star::lang::XEventListener</code></li>
+*  <li> <code>com::sun::star::beans::XPropertyAccess</code></li>
+*  <li> <code>com::sun::star::beans::XPropertyContainer</code></li>
 *  <li> <code>com::sun::star::beans::XPropertySet</code></li>
 *  <li> <code>com::sun::star::form::XLoadListener</code></li>
 *  <li> <code>com::sun::star::form::component::DatabaseListBox</code></li>
@@ -94,6 +96,8 @@ import util.DBTools;
 * @see com.sun.star.container.XNamed
 * @see com.sun.star.lang.XComponent
 * @see com.sun.star.lang.XEventListener
+* @see com.sun.star.beans.XPropertyAccess
+* @see com.sun.star.beans.XPropertyContainer
 * @see com.sun.star.beans.XPropertySet
 * @see com.sun.star.form.XLoadListener
 * @see com.sun.star.form.component.DatabaseListBox
@@ -143,6 +147,8 @@ public class OListBoxModel extends GenericModelTest {
      */
     protected void initialize(TestParameters tParam, PrintWriter log) {
 
+        super.initialize(tParam, log);
+
         super.m_ChangePropertyName = "SelectedItems";
 
         super.m_kindOfControl="ListBox";
@@ -162,10 +168,7 @@ public class OListBoxModel extends GenericModelTest {
 
         super.m_LCShape_Type = "FixedText";
 
-        super.initialize(tParam, log);
-
     }
-
     /**
      * calls <CODE>cleanup()</CODE> from it's super class
      * @param tParam the test parameter
