1fe52117847f8a6d847276f147620ba08ab5e713
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-04 12:36:27 +0000

    INTEGRATION: CWS qadev29 (1.5.36); FILE MERGED 2007/03/07 19:53:16 cn 1.5.36.1: #i75120# bugifx: initialize the super class first, then fill member variables with content

--- qadevOOo/tests/java/mod/_forms/OImageControlModel.java
+++ qadevOOo/tests/java/mod/_forms/OImageControlModel.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OImageControlModel.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:13:07 $
+ *  last change: $Author: ihi $ $Date: 2007-06-04 13:36:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -78,6 +78,8 @@ import com.sun.star.util.XCloseable;
 *  <li> <code>com::sun::star::container::XNamed</code></li>
 *  <li> <code>com::sun::star::lang::XComponent</code></li>
 *  <li> <code>com::sun::star::lang::XEventListener</code></li>
+*  <li> <code>com::sun::star::beans::XPropertyAccess</code></li>
+*  <li> <code>com::sun::star::beans::XPropertyContainer</code></li>
 *  <li> <code>com::sun::star::beans::XPropertySet</code></li>
 *  <li> <code>com::sun::star::form::XLoadListener</code></li>
 *  <li> <code>com::sun::star::container::XChild</code></li>
@@ -101,6 +103,8 @@ import com.sun.star.util.XCloseable;
 * @see com.sun.star.container.XNamed
 * @see com.sun.star.lang.XComponent
 * @see com.sun.star.lang.XEventListener
+* @see com.sun.star.beans.XPropertyAccess
+* @see com.sun.star.beans.XPropertyContainer
 * @see com.sun.star.beans.XPropertySet
 * @see com.sun.star.form.XLoadListener
 * @see com.sun.star.container.XChild
@@ -143,7 +147,9 @@ public class OImageControlModel extends GenericModelTest {
      */
     protected void initialize(TestParameters tParam, PrintWriter log) {
 
-        super.m_kindOfControl="DatabaseImageControl";
+        super.initialize(tParam, log);
+
+        super.m_kindOfControl=  "DatabaseImageControl";
 
         super.m_ObjectName = "stardiv.one.form.component.ImageControl";
 
@@ -154,10 +160,7 @@ public class OImageControlModel extends GenericModelTest {
 
         super.m_LCShape_Type = "FixedText";
 
-        super.initialize(tParam, log);
-
-    }
-    /**
+    }    /**
      * calls <CODE>cleanup()</CODE> from it's super class
      * @param tParam the test parameter
      * @param log the log writer
