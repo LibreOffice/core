8aae6f1469cc8122dab9d390e1380143ce4f7fd0
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-04 12:32:06 +0000

    INTEGRATION: CWS qadev29 (1.1.2); FILE ADDED 2007/04/18 13:06:47 cn 1.1.2.3: #i75120#: update comments 2007/03/06 15:16:11 cn 1.1.2.2: #i75120# final version of interface test 2007/03/05 19:22:02 cn 1.1.2.1: #i75120: new initial version of this interface test

---
+++ qadevOOo/tests/java/ifc/beans/_XPropertyAccess.java
@@ -0,0 +1,244 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: _XPropertyAccess.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-06-04 13:32:06 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+package ifc.beans;
+
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.lang.WrappedTargetException;
+import lib.MultiMethodTest;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertyAccess;
+import lib.Status;
+import lib.StatusException;
+
+/**
+ * Testing <code>com.sun.star.beans.XPropertyAccess</code>
+ * interface methods :
+ * <ul>
+ *  <li><code>getPropertyValues()</code></li>
+ *  <li><code>setPropertyValues()</code></li>
+ * </ul>
+ * @see com.sun.star.beans.XPropertyAccess
+ */
+public class _XPropertyAccess extends MultiMethodTest {
+
+    /**
+     * oObj filled by MultiMethodTest
+     */
+    public XPropertyAccess oObj = null;// oObj filled by MultiMethodTest
+
+    /**
+     * object relation X<CODE>PropertyAccess.propertyToChange</CODE><br>
+     * This relation must be filled from the module. It contains a property which must
+     * be kind of String property, available at <CODE>getPropertyValues()</CODE> and changeable by
+     * <CODE>setPropertyValues()</CODE>
+     */
+    public PropertyValue propertyToChange = null;
+
+    /**
+     * checks if the object relation <CODE>XPropertyAccess.propertyToChange</CODE>
+     * is available
+     */
+    public void before() {
+        propertyToChange = (PropertyValue) tEnv.getObjRelation("XPropertyAccess.propertyToChange");
+        if (propertyToChange == null) {
+            throw new StatusException(Status.failed("Object raltion 'XPropertyAccess.propertyToChange' is null"));
+        }
+    }
+
+    /**
+     * Test calls the method and checks if the returned sequenze contanis a propterty which is named
+     * in the object relation <code>XPropertyAccess.propertyToChange</code>.
+     */
+    public void _getPropertyValues() {
+        PropertyValue[] properties = oObj.getPropertyValues();
+
+        boolean ok = true;
+
+        if (properties != null){
+
+            boolean found = false;
+            for (int i=0; i < properties.length; i++){
+                if (properties[i].Name.equals(propertyToChange.Name)) found = true;
+            }
+            if (! found){
+                log.println("ERROR: could not find desired property '"+ propertyToChange.Name+"'");
+                ok=false;
+            }
+
+        } else {
+            log.println("ERROR: the method returned NULL");
+            ok =false;
+        }
+
+        tRes.tested("getPropertyValues()", ok );
+        return;
+    }
+
+    /**
+     * Test calls the method and checks if:
+     * <ul>
+     *    <li>the property given by the object relation
+     *    <CODE>XPropertyAccess.propertyToChange</CODE> has changed</LI>
+     *    <li><CODE>com.sun.star.lang.IllegalArgumentException</CODE> was thrown if a <CODE>Integer</CODE>
+     *    value was set to a <CODE>String</CODE> property</LI>
+     *    <li><CODE>com.sun.star.beans.UnknownPropertyException</CODE> was throen if an invalid property
+     *    was set</LI>
+     * </ul>
+     */
+    public void _setPropertyValues(){
+
+        boolean ok = true;
+        boolean test = true;
+        boolean exp = false;
+
+        try {
+            PropertyValue[] newProps = new PropertyValue[1];
+            newProps[0] = propertyToChange;
+
+            log.println("try to set property vlaues given by object relation 'XPropertyAccess.propertyToChange'...");
+            oObj.setPropertyValues(newProps);
+
+        } catch (UnknownPropertyException ex) {
+            log.println("ERROR: Exception was thrown while trying to set property value: " +
+                ex.toString());
+            test = false;
+        } catch (PropertyVetoException ex) {
+            log.println("ERROR: Exception was thrown while trying to set property value: " +
+                ex.toString());
+            test = false;
+        } catch (WrappedTargetException ex) {
+            log.println("ERROR: Exception was thrown while trying to set property value: " +
+                ex.toString());
+            test = false;
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("ERROR: Exception was thrown while trying to set property value: " +
+                ex.toString());
+            test = false;
+        }
+
+        if ( test){
+            log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+        try {
+            log.println("try to set integer value to string property, " +
+                "expect 'com.sun.star.lang.IllegalArgumentException'...");
+            PropertyValue[] newProps = new PropertyValue[1];
+            PropertyValue failedProp = new PropertyValue();
+            failedProp.Name = propertyToChange.Name;
+            failedProp.Value = new Integer(10);
+            newProps[0] = failedProp;
+            oObj.setPropertyValues(newProps);
+        } catch (PropertyVetoException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set null value: " +
+                ex.toString());
+            exp = true;
+        } catch (WrappedTargetException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set null value: " +
+                ex.toString());
+            exp = true;
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("OK: exptected exception was thrown while trying to set null value: " +
+                ex.toString());
+            test = true;
+            exp = true;
+        } catch (UnknownPropertyException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set null value: " +
+                ex.toString());
+            exp = true;
+        }
+
+        if (! exp){
+            log.println("FAILED: expected exception 'UnknownPropertyException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        ok &= test;
+        test = false;
+        exp = false;
+        try {
+
+            log.println("try to set values with invalid property name. " +
+                "Expect 'com.sun.star.beans.UnknownPropertyException'...");
+
+            PropertyValue[] newProps = new PropertyValue[1];
+            PropertyValue newProp = new PropertyValue();
+            newProp.Name = "XPropertyAccess.InvalidPropertyName";
+            newProp.Value = "invalid property";
+            newProps[0] = newProp;
+
+            oObj.setPropertyValues(newProps);
+
+        } catch (WrappedTargetException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set invalid value: " +
+                ex.toString());
+            exp = true;
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set invalid value: " +
+                ex.toString());
+            exp = true;
+        } catch (PropertyVetoException ex) {
+            log.println("ERROR: unexptected exception was thrown while trying to set invalid value: " +
+                ex.toString());
+            exp = true;
+        } catch (UnknownPropertyException ex) {
+            log.println("OK: Exptected exception was thrown while trying to set invalid value: " +
+                ex.toString());
+            exp = true;
+            test = true;
+        }
+
+        ok &= test;
+
+        if (! exp){
+            log.println("FAILED: expected exception 'UnknownPropertyException' was not thrown");
+        } else {
+            if (test) log.println("... OK");
+        }
+
+        tRes.tested("setPropertyValues()", ok);
+        return;
+
+    }
+
+}    /// finish class XPropertyAccess
+
+
