2b4d9cc868f058527194ce106682e52b851654cd
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 16:51:22 +0000

    INTEGRATION: CWS bgdlremove (1.11.12); FILE MERGED 2007/05/11 09:37:43 kso 1.11.12.1: export.dxp no longer needed.

--- ucb/source/cacher/makefile.mk
+++ ucb/source/cacher/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 14:21:20 $
+#   last change: $Author: ihi $ $Date: 2007-06-05 17:51:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -69,8 +69,6 @@ SHL1IMPLIB=i$(TARGET)
 SHL1VERSIONMAP=exports.map
 
 DEF1NAME=$(SHL1TARGET)
-DEF1EXPORTFILE=exports.dxp
-DEF1DES=Cached Dynamic Resultset
 
 .INCLUDE: target.mk
 
