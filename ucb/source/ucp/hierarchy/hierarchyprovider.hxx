b10b7ed1c16ea7dcdc473fdc413a32435b8a28bb
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 17:07:11 +0000

    INTEGRATION: CWS bgdlremove (1.7.108); FILE MERGED 2007/05/18 11:37:17 kso 1.7.108.1: #i77419# - cleanup of ucbhelper namespaces.

--- ucb/source/ucp/hierarchy/hierarchyprovider.hxx
+++ ucb/source/ucp/hierarchy/hierarchyprovider.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hierarchyprovider.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:48:30 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 18:07:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -112,7 +112,7 @@ ConfigProviderMap;
 
 //=========================================================================
 
-class HierarchyContentProvider : public ::ucb::ContentProviderImplHelper,
+class HierarchyContentProvider : public ::ucbhelper::ContentProviderImplHelper,
                                  public com::sun::star::lang::XInitialization
 {
     ConfigProviderMap   m_aConfigProviderMap;
