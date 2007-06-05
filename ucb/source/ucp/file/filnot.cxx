fafc15835110fb1e3629af6ad30c58ee31cc46da
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 16:55:25 +0000

    INTEGRATION: CWS bgdlremove (1.4.40); FILE MERGED 2007/05/11 10:07:44 kso 1.4.40.1: #i76911# - no longer uses VOS.

--- ucb/source/ucp/file/filnot.cxx
+++ ucb/source/ucp/file/filnot.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filnot.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:47:24 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 17:55:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -137,7 +137,7 @@ void ContentEventNotifier::notifyRemoved( const rtl::OUString& aChildName )
 
     BaseContent* pp = new BaseContent( m_pMyShell,xChildId,aChildName );
     {
-        vos::OGuard aGuard( pp->m_aMutex );
+        osl::MutexGuard aGuard( pp->m_aMutex );
         pp->m_nState |= BaseContent::Deleted;
     }
 
