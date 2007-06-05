6c7bbd6b4cb6fb6853d87d6d5f54ac0ea2814d8e
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 16:30:20 +0000

    INTEGRATION: CWS bgdlremove (1.9.310); FILE MERGED 2007/05/11 09:14:29 kso 1.9.310.1: #i76911# - ucbhelper lib no longer uses VOS. (vos::ORef => rtl::Reference, vos::OMutex => osl::Mutex, ...)

--- sw/source/core/swg/SwXMLBlockImport.cxx
+++ sw/source/core/swg/SwXMLBlockImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXMLBlockImport.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:28:09 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 17:30:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,7 +79,7 @@ SwXMLBlockListImport::~SwXMLBlockListImport ( void )
 SvXMLImportContext *SwXMLBlockListImport::CreateContext(
         sal_uInt16 nPrefix,
         const OUString& rLocalName,
-        const Reference< xml::sax::XAttributeList > & xAttrList )
+        const uno::Reference< xml::sax::XAttributeList > & xAttrList )
 {
     SvXMLImportContext *pContext = 0;
     if ( XML_NAMESPACE_BLOCKLIST == nPrefix &&
@@ -118,7 +118,7 @@ SwXMLTextBlockImport::~SwXMLTextBlockImport ( void )
 SvXMLImportContext *SwXMLTextBlockImport::CreateContext(
         sal_uInt16 nPrefix,
         const OUString& rLocalName,
-        const Reference< xml::sax::XAttributeList > & xAttrList )
+        const uno::Reference< xml::sax::XAttributeList > & xAttrList )
 {
     SvXMLImportContext *pContext = 0;
     if( XML_NAMESPACE_OFFICE == nPrefix &&
