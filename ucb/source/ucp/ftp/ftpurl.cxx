9e492131d1f3e17e8525983d92e8d7bf10807148
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 17:01:46 +0000

    INTEGRATION: CWS bgdlremove (1.23.30); FILE MERGED 2007/05/25 10:49:37 kso 1.23.30.1: #i76910# - Remove obsolete background download stuff from UCB.

--- ucb/source/ucp/ftp/ftpurl.cxx
+++ ucb/source/ucp/ftp/ftpurl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ftpurl.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-01 10:12:33 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 18:01:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,6 @@
 #include "ftphandleprovider.hxx"
 #include "ftpinpstr.hxx"
 #include "ftpcfunc.hxx"
-#include "debughelper.hxx"
 #include "ftpcontainer.hxx"
 
 using namespace ftp;
