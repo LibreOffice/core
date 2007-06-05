dcae1c4c7b6bdbb74bb9f301a9d7263b6a4ad180
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 17:13:40 +0000

    INTEGRATION: CWS bgdlremove (1.5.62); FILE MERGED 2007/05/18 11:37:17 kso 1.5.62.2: #i77419# - cleanup of ucbhelper namespaces. 2007/05/11 12:59:03 kso 1.5.62.1: #i76911# - ucbhelper no longer uses VOS

--- ucb/source/ucp/package/pkgdatasupplier.hxx
+++ ucb/source/ucp/package/pkgdatasupplier.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pkgdatasupplier.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 05:30:41 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 18:13:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@ namespace package_ucp {
 struct DataSupplier_Impl;
 class Content;
 
-class DataSupplier : public ucb::ResultSetDataSupplier
+class DataSupplier : public ::ucbhelper::ResultSetDataSupplier
 {
     DataSupplier_Impl* m_pImpl;
 
@@ -60,22 +60,22 @@ public:
                   sal_Int32 nOpenMode );
     virtual ~DataSupplier();
 
-    virtual rtl::OUString queryContentIdentifierString( sal_Int32 nIndex );
+    virtual rtl::OUString queryContentIdentifierString( sal_uInt32 nIndex );
     virtual com::sun::star::uno::Reference<
                 com::sun::star::ucb::XContentIdentifier >
-    queryContentIdentifier( sal_Int32 nIndex );
+    queryContentIdentifier( sal_uInt32 nIndex );
     virtual com::sun::star::uno::Reference< com::sun::star::ucb::XContent >
-    queryContent( sal_Int32 nIndex );
+    queryContent( sal_uInt32 nIndex );
 
-    virtual sal_Bool getResult( sal_Int32 nIndex );
+    virtual sal_Bool getResult( sal_uInt32 nIndex );
 
-    virtual sal_Int32 totalCount();
-    virtual sal_Int32 currentCount();
+    virtual sal_uInt32 totalCount();
+    virtual sal_uInt32 currentCount();
     virtual sal_Bool isCountFinal();
 
     virtual com::sun::star::uno::Reference< com::sun::star::sdbc::XRow >
-    queryPropertyValues( sal_Int32 nIndex  );
-    virtual void releasePropertyValues( sal_Int32 nIndex );
+    queryPropertyValues( sal_uInt32 nIndex  );
+    virtual void releasePropertyValues( sal_uInt32 nIndex );
 
     virtual void close();
 
