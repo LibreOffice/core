26af5e903c0a1036b1292a17adf639e402cdd773
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 13:53:24 +0000

    INTEGRATION: CWS bgdlremove (1.4.28); FILE MERGED 2007/05/18 11:38:08 kso 1.4.28.1: #i77419# - cleanup of ucbhelper namespaces.

--- ucbhelper/source/provider/commandenvironmentproxy.cxx
+++ ucbhelper/source/provider/commandenvironmentproxy.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: commandenvironmentproxy.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:21:03 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 14:53:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,7 @@ using namespace com::sun::star::ucb;
 using namespace com::sun::star::uno;
 using namespace rtl;
 
-namespace ucb
+namespace ucbhelper
 {
 
 //=========================================================================
@@ -188,5 +188,5 @@ CommandEnvironmentProxy::getProgressHandler()
     return m_pImpl->m_xProgressHandler;
 }
 
-} /* namespace ucb */
+} /* namespace ucbhelper */
 
