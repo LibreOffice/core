c045dde562abb0bd6fd387dead3bd9d806ac2b23
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 17:28:52 +0000

    INTEGRATION: CWS bgdlremove (1.21.26); FILE MERGED 2007/05/18 11:40:45 kso 1.21.26.2: #i77419# - cleanup of ucbhelper namespaces. 2007/05/11 09:17:38 kso 1.21.26.1: 76911# - ucbhelper lib no longer uses VOS. (vos::ORef => rtl::Reference, vos::OMutex => osl::Mutex, ...)

--- xmlhelp/source/cxxhelp/provider/provider.cxx
+++ xmlhelp/source/cxxhelp/provider/provider.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: provider.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 01:17:13 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 18:28:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -86,14 +86,7 @@
 #include <com/sun/star/container/XNameReplace.hpp>
 #endif
 
-using namespace com::sun::star::frame;
-using namespace com::sun::star::beans;
-using namespace com::sun::star::container;
-using namespace com::sun::star::lang;
-using namespace com::sun::star::ucb;
-using namespace com::sun::star::uno;
-using namespace rtl;
-
+using namespace com::sun::star;
 using namespace chelp;
 
 //=========================================================================
@@ -104,10 +97,11 @@ using namespace chelp;
 //=========================================================================
 //=========================================================================
 
-ContentProvider::ContentProvider( const Reference< XMultiServiceFactory >& rSMgr )
-    : ::ucb::ContentProviderImplHelper( rSMgr ),
+ContentProvider::ContentProvider(
+    const uno::Reference< lang::XMultiServiceFactory >& rSMgr )
+    : ::ucbhelper::ContentProviderImplHelper( rSMgr ),
         isInitialized( false ),
-        m_aScheme( OUString::createFromAscii( MYUCP_URL_SCHEME ) ),
+        m_aScheme( rtl::OUString::createFromAscii( MYUCP_URL_SCHEME ) ),
         m_pDatabases( 0 )
 {
 }
@@ -126,12 +120,12 @@ ContentProvider::~ContentProvider()
 //=========================================================================
 
 XINTERFACE_IMPL_6( ContentProvider,
-                   XTypeProvider,
-                   XServiceInfo,
-                   XContentProvider,
-                   XComponent,
-                   XEventListener, /* base of XContainerListener */
-                   XContainerListener);
+                   lang::XTypeProvider,
+                   lang::XServiceInfo,
+                   ucb::XContentProvider,
+                   lang::XComponent,
+                   lang::XEventListener, /* base of XContainerListener */
+                   container::XContainerListener);
 
 //=========================================================================
 //
@@ -140,11 +134,11 @@ XINTERFACE_IMPL_6( ContentProvider,
 //=========================================================================
 
 XTYPEPROVIDER_IMPL_5( ContentProvider,
-                         XTypeProvider,
-                         XServiceInfo,
-                         XContentProvider,
-                      XComponent,
-                      XContainerListener);
+                         lang::XTypeProvider,
+                         lang::XServiceInfo,
+                         ucb::XContentProvider,
+                      lang::XComponent,
+                      container::XContainerListener);
 
 //=========================================================================
 //
@@ -153,24 +147,21 @@ XTYPEPROVIDER_IMPL_5( ContentProvider,
 //=========================================================================
 
 rtl::OUString SAL_CALL ContentProvider::getImplementationName()
-    throw( com::sun::star::uno::RuntimeException )
+    throw( uno::RuntimeException )
 {
     return getImplementationName_Static();
 }
 
-
 rtl::OUString ContentProvider::getImplementationName_Static()
 {
-    return OUString::createFromAscii("CHelpContentProvider" );
+    return rtl::OUString::createFromAscii("CHelpContentProvider" );
 }
-
 
 sal_Bool SAL_CALL
 ContentProvider::supportsService(const rtl::OUString& ServiceName )
-    throw( com::sun::star::uno::RuntimeException )
+    throw( uno::RuntimeException )
 {
-    com::sun::star::uno::Sequence< rtl::OUString > aSNL =
-        getSupportedServiceNames();
+    uno::Sequence< rtl::OUString > aSNL = getSupportedServiceNames();
     const rtl::OUString* pArray = aSNL.getArray();
     for( sal_Int32 i = 0; i < aSNL.getLength(); i++ )
     {
@@ -181,44 +172,36 @@ ContentProvider::supportsService(const rtl::OUString& ServiceName )
     return sal_False;
 }
 
-com::sun::star::uno::Sequence< rtl::OUString > SAL_CALL
+uno::Sequence< rtl::OUString > SAL_CALL
 ContentProvider::getSupportedServiceNames()
-    throw( com::sun::star::uno::RuntimeException )
+    throw( uno::RuntimeException )
 {
     return getSupportedServiceNames_Static();
 }
 
-static com::sun::star::uno::Reference<
-com::sun::star::uno::XInterface > SAL_CALL
+static uno::Reference< uno::XInterface > SAL_CALL
 ContentProvider_CreateInstance(
-    const com::sun::star::uno::Reference<
-    com::sun::star::lang::XMultiServiceFactory> & rSMgr )
-    throw( com::sun::star::uno::Exception )
+         const uno::Reference< lang::XMultiServiceFactory> & rSMgr )
+    throw( uno::Exception )
 {
-    com::sun::star::lang::XServiceInfo* pX =
-        (com::sun::star::lang::XServiceInfo*)new ContentProvider( rSMgr );
-    return com::sun::star::uno::Reference<
-        com::sun::star::uno::XInterface >::query( pX );
+    lang::XServiceInfo * pX = static_cast< lang::XServiceInfo * >(
+        new ContentProvider( rSMgr ) );
+    return uno::Reference< uno::XInterface >::query( pX );
 }
 
-
-com::sun::star::uno::Sequence< rtl::OUString >
+uno::Sequence< rtl::OUString >
 ContentProvider::getSupportedServiceNames_Static()
 {
-    com::sun::star::uno::Sequence< rtl::OUString > aSNS( 2 );
+    uno::Sequence< rtl::OUString > aSNS( 2 );
     aSNS.getArray()[ 0 ] =
-        OUString::createFromAscii(
+        rtl::OUString::createFromAscii(
             MYUCP_CONTENT_PROVIDER_SERVICE_NAME1 );
     aSNS.getArray()[ 1 ] =
-        OUString::createFromAscii(
+        rtl::OUString::createFromAscii(
             MYUCP_CONTENT_PROVIDER_SERVICE_NAME2 );
 
     return aSNS;
 }
-
-
-
-
 
 //=========================================================================
 //
@@ -235,22 +218,25 @@ ONE_INSTANCE_SERVICE_FACTORY_IMPL( ContentProvider );
 //=========================================================================
 
 // virtual
-Reference< XContent > SAL_CALL ContentProvider::queryContent( const Reference< XContentIdentifier >& xCanonicId )
-    throw( IllegalIdentifierException, RuntimeException )
+uno::Reference< ucb::XContent > SAL_CALL
+ContentProvider::queryContent(
+        const uno::Reference< ucb::XContentIdentifier >& xCanonicId )
+    throw( ucb::IllegalIdentifierException, uno::RuntimeException )
 {
-    if ( ! xCanonicId->getContentProviderScheme().equalsIgnoreAsciiCase( m_aScheme ) )
+    if ( !xCanonicId->getContentProviderScheme()
+             .equalsIgnoreAsciiCase( m_aScheme ) )
     {   // Wrong URL-scheme
-        throw IllegalIdentifierException();
+        throw ucb::IllegalIdentifierException();
     }
 
     {
         osl::MutexGuard aGuard( m_aMutex );
-        if( ! isInitialized )
+        if( !isInitialized )
             init();
     }
 
-    if( ! m_pDatabases )
-        throw RuntimeException();
+    if( !m_pDatabases )
+        throw uno::RuntimeException();
 
     rtl::OUString aOUString( m_pDatabases->getInstallPathAsURL() );
     rtl::OString aOString( aOUString.getStr(),
@@ -258,29 +244,29 @@ Reference< XContent > SAL_CALL ContentProvider::queryContent( const Reference< X
                            RTL_TEXTENCODING_UTF8 );
 
     // Check, if a content with given id already exists...
-    Reference< XContent > xContent
-        = queryExistingContent( xCanonicId ).getBodyPtr();
+    uno::Reference< ucb::XContent > xContent
+        = queryExistingContent( xCanonicId ).get();
     if ( xContent.is() )
         return xContent;
 
-    xContent = new Content( m_xSMgr,this,xCanonicId,m_pDatabases );
+    xContent = new Content( m_xSMgr, this, xCanonicId, m_pDatabases );
 
     // Further checks
 
     if ( !xContent->getIdentifier().is() )
-        throw IllegalIdentifierException();
+        throw ucb::IllegalIdentifierException();
 
     return xContent;
 }
 
 void SAL_CALL
 ContentProvider::dispose()
-    throw ( com::sun::star::uno::RuntimeException)
+    throw ( uno::RuntimeException)
 {
     if(m_xContainer.is())
     {
         m_xContainer->removeContainerListener(this);
-        m_xContainer = Reference<XContainer>(0);
+        m_xContainer.clear();
     }
 }
 
@@ -288,8 +274,8 @@ ContentProvider::dispose()
 #include <provider/debughelper.hxx>
 
 void SAL_CALL
-ContentProvider::elementReplaced(const ContainerEvent& Event)
-    throw (::com::sun::star::uno::RuntimeException)
+ContentProvider::elementReplaced(const container::ContainerEvent& Event)
+    throw (uno::RuntimeException)
 {
     if(!m_pDatabases)
         return;
@@ -309,15 +295,16 @@ ContentProvider::elementReplaced(const ContainerEvent& Event)
     m_pDatabases->changeCSS(element);
 }
 
-
 void ContentProvider::init()
 {
     osl::MutexGuard aGuard( m_aMutex );
 
     isInitialized = true;
-    Reference< XMultiServiceFactory >  sProvider( getConfiguration() );
-    Reference< XHierarchicalNameAccess > xHierAccess( getHierAccess( sProvider,
-                                                                     "org.openoffice.Office.Common" ) );
+    uno::Reference< lang::XMultiServiceFactory > sProvider(
+        getConfiguration() );
+    uno::Reference< container::XHierarchicalNameAccess > xHierAccess(
+        getHierAccess( sProvider,
+                       "org.openoffice.Office.Common" ) );
 
     rtl::OUString instPath( getKey( xHierAccess,"Path/Current/Help" ) );
     if( ! instPath.getLength() )
@@ -326,19 +313,23 @@ void ContentProvider::init()
     // replace anything like $(instpath);
     subst( instPath );
 
-
-    rtl::OUString stylesheet(getKey(xHierAccess,"Help/HelpStyleSheet"));
-    try {
+    rtl::OUString stylesheet( getKey( xHierAccess,"Help/HelpStyleSheet" ) );
+    try
+    {
         // now adding as configuration change listener for the stylesheet
-        Reference<XNameAccess> xAccess(xHierAccess,UNO_QUERY);
-        if(xAccess.is()) {
-            Any aAny =
-                xAccess->getByName(rtl::OUString::createFromAscii("Help"));
+        uno::Reference< container::XNameAccess> xAccess(
+            xHierAccess, uno::UNO_QUERY );
+        if( xAccess.is() )
+        {
+            uno::Any aAny =
+                xAccess->getByName( rtl::OUString::createFromAscii( "Help" ) );
             aAny >>= m_xContainer;
-            if(m_xContainer.is())
-                m_xContainer->addContainerListener(this);
+            if( m_xContainer.is() )
+                m_xContainer->addContainerListener( this );
         }
-    } catch(const com::sun::star::uno::Exception&) {
+    }
+    catch( uno::Exception const & )
+    {
     }
 
     /**
@@ -350,32 +341,35 @@ void ContentProvider::init()
      *  vendorshort
      */
 
-    xHierAccess = getHierAccess( sProvider,
-                                 "org.openoffice.Setup" );
-    rtl::OUString productname( getKey(  xHierAccess,"Product/ooName" ) );
+    xHierAccess = getHierAccess( sProvider, "org.openoffice.Setup" );
+    rtl::OUString productname(
+        getKey( xHierAccess,"Product/ooName" ) );
 
-    rtl::OUString setupversion( getKey(  xHierAccess,"Product/ooSetupVersion" ) );
-    rtl::OUString setupextension( getKey(  xHierAccess,"Product/ooSetupExtension") );
-    rtl::OUString productversion( setupversion +
-                                  rtl::OUString::createFromAscii( " " ) +
-                                  setupextension );
+    rtl::OUString setupversion(
+        getKey( xHierAccess,"Product/ooSetupVersion" ) );
+    rtl::OUString setupextension(
+        getKey( xHierAccess,"Product/ooSetupExtension") );
+    rtl::OUString productversion(
+        setupversion +
+        rtl::OUString::createFromAscii( " " ) +
+        setupextension );
 
+    xHierAccess = getHierAccess( sProvider, "org.openoffice.Webtop.Common" );
+    rtl::OUString vendorname(
+        getKey( xHierAccess,"Product/ooName" ) );
 
-    xHierAccess = getHierAccess( sProvider,
-                                 "org.openoffice.Webtop.Common" );
-    rtl::OUString vendorname( getKey(  xHierAccess,"Product/ooName" ) );
-
-    setupversion = rtl::OUString( getKey(  xHierAccess,"Product/ooSetupVersion" ) );
-    setupextension = rtl::OUString ( getKey(  xHierAccess,"Product/ooSetupExtension") );
-    rtl::OUString vendorversion( setupversion +
-                                 rtl::OUString::createFromAscii( " " ) +
-                                 setupextension );
+    setupversion = rtl::OUString(
+        getKey( xHierAccess,"Product/ooSetupVersion" ) );
+    setupextension = rtl::OUString(
+        getKey(  xHierAccess,"Product/ooSetupExtension") );
+    rtl::OUString vendorversion(
+        setupversion +
+        rtl::OUString::createFromAscii( " " ) +
+        setupextension );
     rtl::OUString vendorshort = vendorname;
 
-
-    Sequence< rtl::OUString > aImagesZipPaths( 2 );
-    xHierAccess = getHierAccess( sProvider,
-                                 "org.openoffice.Office.Common" );
+    uno::Sequence< rtl::OUString > aImagesZipPaths( 2 );
+    xHierAccess = getHierAccess( sProvider,  "org.openoffice.Office.Common" );
 
     rtl::OUString aPath( getKey( xHierAccess, "Path/Current/UserConfig" ) );
     subst( aPath );
@@ -396,25 +390,23 @@ void ContentProvider::init()
                                   vendorshort,
                                   stylesheet,
                                   m_xSMgr );
-
 }
 
-
-
-
-Reference< XMultiServiceFactory > ContentProvider::getConfiguration() const
+uno::Reference< lang::XMultiServiceFactory >
+ContentProvider::getConfiguration() const
 {
-    Reference< XMultiServiceFactory > sProvider;
+    uno::Reference< lang::XMultiServiceFactory > sProvider;
     if( m_xSMgr.is() )
     {
-        Any aAny;
+        uno::Any aAny;
         aAny <<= rtl::OUString::createFromAscii( "plugin" );
-        PropertyValue aProp( rtl::OUString::createFromAscii( "servertype" ),
-                             -1,
-                             aAny,
-                             PropertyState_DIRECT_VALUE );
+        beans::PropertyValue aProp(
+            rtl::OUString::createFromAscii( "servertype" ),
+            -1,
+            aAny,
+            beans::PropertyState_DIRECT_VALUE );
 
-        Sequence< Any > seq(1);
+        uno::Sequence< uno::Any > seq(1);
         seq[0] <<= aProp;
 
         try
@@ -423,69 +415,69 @@ Reference< XMultiServiceFactory > ContentProvider::getConfiguration() const
                 rtl::OUString::createFromAscii(
                     "com.sun.star.configuration.ConfigurationProvider" );
             sProvider =
-                Reference< XMultiServiceFactory >(
+                uno::Reference< lang::XMultiServiceFactory >(
                     m_xSMgr->createInstanceWithArguments(
                         sProviderService,seq ),
-                    UNO_QUERY );
+                    uno::UNO_QUERY );
         }
-        catch( const com::sun::star::uno::Exception& )
+        catch( const uno::Exception& )
         {
-          OSL_ENSURE( sProvider.is(),"cant instantiate configuration" );
+            OSL_ENSURE( sProvider.is(), "cant instantiate configuration" );
         }
     }
 
     return sProvider;
 }
 
-
-
-Reference< XHierarchicalNameAccess >
-ContentProvider::getHierAccess( const Reference< XMultiServiceFactory >& sProvider,
-                                const char* file ) const
+uno::Reference< container::XHierarchicalNameAccess >
+ContentProvider::getHierAccess(
+    const uno::Reference< lang::XMultiServiceFactory >& sProvider,
+    const char* file ) const
 {
-    Reference< XHierarchicalNameAccess > xHierAccess;
+    uno::Reference< container::XHierarchicalNameAccess > xHierAccess;
 
     if( sProvider.is() )
     {
-        Sequence< Any > seq(1);
-        rtl::OUString sReaderService =
-            rtl::OUString::createFromAscii( "com.sun.star.configuration.ConfigurationAccess" );
+        uno::Sequence< uno::Any > seq( 1 );
+        rtl::OUString sReaderService(
+            rtl::OUString::createFromAscii(
+                "com.sun.star.configuration.ConfigurationAccess" ) );
 
-        seq[0] <<= rtl::OUString::createFromAscii( file );
+        seq[ 0 ] <<= rtl::OUString::createFromAscii( file );
 
         try
         {
             xHierAccess =
-                Reference< XHierarchicalNameAccess >
-                ( sProvider->createInstanceWithArguments( sReaderService,seq ),
-                  UNO_QUERY );
+                uno::Reference< container::XHierarchicalNameAccess >(
+                    sProvider->createInstanceWithArguments(
+                        sReaderService, seq ),
+                    uno::UNO_QUERY );
         }
-        catch( const com::sun::star::uno::Exception& )
+        catch( const uno::Exception& )
         {
         }
     }
-
     return xHierAccess;
 }
 
 
 
 rtl::OUString
-ContentProvider::getKey(const Reference<
-                        XHierarchicalNameAccess >& xHierAccess,
-                        const char* key) const
+ContentProvider::getKey(
+    const uno::Reference< container::XHierarchicalNameAccess >& xHierAccess,
+    const char* key ) const
 {
     rtl::OUString instPath;
     if( xHierAccess.is() )
     {
-        Any aAny;
+        uno::Any aAny;
         try
         {
             aAny =
                 xHierAccess->getByHierarchicalName(
-                    rtl::OUString::createFromAscii(key));
+                    rtl::OUString::createFromAscii( key ) );
         }
-        catch( const com::sun::star::container::NoSuchElementException& )
+        catch( const container::NoSuchElementException& )
         {
         }
         aAny >>= instPath;
@@ -493,47 +485,47 @@ ContentProvider::getKey(const Reference<
     return instPath;
 }
 
-
 sal_Bool
-ContentProvider::getBooleanKey(const Reference<
-                        XHierarchicalNameAccess >& xHierAccess,
-                        const char* key) const
+ContentProvider::getBooleanKey(
+    const uno::Reference< container::XHierarchicalNameAccess >& xHierAccess,
+    const char* key ) const
 {
   sal_Bool ret = sal_False;
   if( xHierAccess.is() )
-    {
-      Any aAny;
+  {
+      uno::Any aAny;
       try
-        {
+      {
           aAny =
             xHierAccess->getByHierarchicalName(
-                                               rtl::OUString::createFromAscii(key));
-        }
-      catch( const com::sun::star::container::NoSuchElementException& )
-        {
-        }
+                rtl::OUString::createFromAscii( key ) );
+      }
+      catch( const container::NoSuchElementException& )
+      {
+      }
       aAny >>= ret;
-    }
+  }
   return ret;
 }
 
-
-
 void ContentProvider::subst( rtl::OUString& instpath ) const
 {
-    Reference< XConfigManager >  xCfgMgr;
+    uno::Reference< frame::XConfigManager > xCfgMgr;
     if( m_xSMgr.is() )
     {
         try
         {
             xCfgMgr =
-                Reference< XConfigManager >(
-                    m_xSMgr->createInstance( rtl::OUString::createFromAscii( "com.sun.star.config.SpecialConfigManager" ) ),
-                    UNO_QUERY );
+                uno::Reference< frame::XConfigManager >(
+                    m_xSMgr->createInstance(
+                        rtl::OUString::createFromAscii(
+                            "com.sun.star.config.SpecialConfigManager" ) ),
+                    uno::UNO_QUERY );
         }
-        catch( const com::sun::star::uno::Exception&)
+        catch( const uno::Exception&)
         {
-            OSL_ENSURE( xCfgMgr.is()," cant instantiate the special config manager " );
+            OSL_ENSURE( xCfgMgr.is(),
+                        "cant instantiate the special config manager " );
         }
     }
 
