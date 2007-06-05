cbc1df7aaf0a998c3dde381786d6d3ddc5106a9a
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 17:05:21 +0000

    INTEGRATION: CWS bgdlremove (1.3.108); FILE MERGED 2007/05/18 11:37:17 kso 1.3.108.1: #i77419# - cleanup of ucbhelper namespaces.

--- ucb/source/ucp/hierarchy/dynamicresultset.hxx
+++ ucb/source/ucp/hierarchy/dynamicresultset.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dynamicresultset.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:45:31 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 18:05:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@
 
 namespace hierarchy_ucp {
 
-class DynamicResultSet : public ::ucb::ResultSetImplHelper
+class DynamicResultSet : public ::ucbhelper::ResultSetImplHelper
 {
     rtl::Reference< HierarchyContent > m_xContent;
 
