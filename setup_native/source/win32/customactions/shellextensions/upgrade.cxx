7284f508a1858f34a1cc46039fa46e61f2f4ad6e
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 09:52:56 +0000

    INTEGRATION: CWS obo12 (1.5.8); FILE MERGED 2007/05/10 13:21:55 obo 1.5.8.2: #i76713# _WIN32_WINDOWS = 0x0410 for Win98 2007/05/10 10:39:28 obo 1.5.8.1: #i76713# #error YES removed (Vista PSDK)

--- setup_native/source/win32/customactions/shellextensions/upgrade.cxx
+++ setup_native/source/win32/customactions/shellextensions/upgrade.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: upgrade.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2007-03-26 14:12:16 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 10:52:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,6 +36,8 @@
 #undef UNICODE
 #undef _UNICODE
 
+#define _WIN32_WINDOWS 0x0410
+
 #pragma warning(push, 1) /* disable warnings within system headers */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -47,10 +49,6 @@
 
 #include <tchar.h>
 #include <string>
-
-#if defined(_WIN32_WINNT) && !defined(__MINGW32__)
-#error YES
-#endif
 
 using namespace std;
 
