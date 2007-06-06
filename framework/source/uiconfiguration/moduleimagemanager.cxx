c6704c2fa506b78c3515188da7ba0716d119805d
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-06 13:02:02 +0000

    INTEGRATION: CWS ka009 (1.13.22); FILE MERGED 2007/02/12 15:45:26 ka 1.13.22.5: RESYNC: (1.17-1.18); FILE MERGED 2007/01/04 14:48:25 ka 1.13.22.4: RESYNC: (1.15-1.17); FILE MERGED 2006/10/27 14:10:00 ka 1.13.22.3: RESYNC: (1.14-1.15); FILE MERGED 2006/10/13 10:34:39 ka 1.13.22.2: RESYNC: (1.13-1.14); FILE MERGED 2006/07/12 21:29:06 ka 1.13.22.1: #i66680#: added patch for optimized ImageList handling

--- framework/source/uiconfiguration/moduleimagemanager.cxx
+++ framework/source/uiconfiguration/moduleimagemanager.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: moduleimagemanager.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-23 07:31:37 $
+ *  last change: $Author: ihi $ $Date: 2007-06-06 14:02:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -355,8 +355,7 @@ ImageList* CmdImageList::impl_getImageList( sal_Int16 nImageType )
     if ( !m_pImageList[nImageType] )
     {
         m_pImageList[nImageType] = new ImageList( m_aImageNameVector,
-                                                  OUString::createFromAscii( ImageType_Prefixes[nImageType] ),
-                                                  NULL );
+                                                  OUString::createFromAscii( ImageType_Prefixes[nImageType] ) );
     }
 
     return m_pImageList[nImageType];
@@ -641,7 +640,9 @@ sal_Bool ModuleImageManager::implts_loadUserImages(
 
                     // Delete old image list and create a new one from the read bitmap
                     delete m_pUserImageList[nImageType];
-                    m_pUserImageList[nImageType] = new ImageList( aUserBitmap, aUserImagesVector );
+                    m_pUserImageList[nImageType] = new ImageList();
+                    m_pUserImageList[nImageType]->InsertFromHorizontalStrip
+                        ( aUserBitmap, aUserImagesVector );
                     return sal_True;
                 }
             }
@@ -713,7 +714,7 @@ sal_Bool ModuleImageManager::implts_storeUserImages(
                 if ( xBitmapStream.is() )
                 {
                     SvStream* pSvStream = utl::UcbStreamHelper::CreateStream( xBitmapStream );
-                    vcl::PNGWriter aPngWriter( pImageList->GetBitmapEx() );
+                    vcl::PNGWriter aPngWriter( pImageList->GetAsHorizontalStrip() );
                     aPngWriter.Write( *pSvStream );
                     delete pSvStream;
 
