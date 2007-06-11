d2461503af403b4745a7da853b502d3f69775911
Oliver Bolte <obo@openoffice.org>
2007-06-11 13:58:40 +0000

    INTEGRATION: CWS chart05 (1.23.2); FILE MERGED 2007/05/31 12:39:17 bm 1.23.2.1: check m_pDrawViewWrapper before accessing

--- chart2/source/controller/main/ChartController_Window.cxx
+++ chart2/source/controller/main/ChartController_Window.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ChartController_Window.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 18:04:30 $
+ *  last change: $Author: obo $ $Date: 2007-06-11 14:58:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1151,9 +1151,12 @@ bool ChartController::requestQuickHelp(
     awt::Rectangle & rOutEqualRect )
 {
     // help text
-    ::rtl::OUString aCID(
-        SelectionHelper::getHitObjectCID(
-            aAtLogicPosition, *m_pDrawViewWrapper ));
+    ::rtl::OUString aCID;
+    if( m_pDrawViewWrapper )
+    {
+        aCID = SelectionHelper::getHitObjectCID(
+            aAtLogicPosition, *m_pDrawViewWrapper );
+    }
     bool bResult( aCID.getLength());
 
     if( bResult )
