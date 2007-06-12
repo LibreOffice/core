ac7642f3b58d9cc568af9250e1e38cdb5912e67b
Oliver Bolte <obo@openoffice.org>
2007-06-12 05:09:32 +0000

    INTEGRATION: CWS dmake48 (1.5.2); FILE MERGED 2007/04/24 17:42:30 vq 1.5.2.2: #i73700# Release dmake 4.8. 2007/01/21 03:00:39 vq 1.5.2.1: #i73700# Bump version to 4.8-cvs.

--- dmake/winnt/microsft/config.h
+++ dmake/winnt/microsft/config.h
@@ -1,4 +1,4 @@
-/* RCS  $Id: config.h,v 1.5 2007-01-18 09:51:47 vg Exp $
+/* RCS  $Id: config.h,v 1.6 2007-06-12 06:09:32 obo Exp $
 --
 -- SYNOPSIS
 --      Configurarion include file.
@@ -27,7 +27,7 @@
 
 /* Name and version number of this package */
 #define PACKAGE "dmake"
-#define VERSION "4.7"
+#define VERSION "4.8"
 #define BUILDINFO "Windows / MS Visual C++"
 
 #if defined (_MSC_VER)
