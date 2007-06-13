bae2876fd3296fd0f40b56c1896bc6b481b2f35c
Sven Jacobi <sj@openoffice.org>
2007-06-13 08:07:51 +0000

    #i78171# fixed crash when clicking next after canceling the file open dialog

--- sdext/source/minimizer/optimizerdialog.cxx
+++ sdext/source/minimizer/optimizerdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optimizerdialog.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: sj $ $Date: 2007-05-24 10:08:36 $
+ *  last change: $Author: sj $ $Date: 2007-06-13 09:07:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -751,7 +751,7 @@ void ActionListener::actionPerformed( const ActionEvent& rEvent )
             {
                 mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavHelp ), TKGet( TK_Enabled ), Any( sal_True ) );
                 mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavBack ), TKGet( TK_Enabled ), Any( sal_True ) );
-                mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavNext ), TKGet( TK_Enabled ), Any( sal_True ) );
+                mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavNext ), TKGet( TK_Enabled ), Any( sal_False ) );
                 mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavFinish ), TKGet( TK_Enabled ), Any( sal_True ) );
                 mrOptimizerDialog.setControlProperty( TKGet( TK_btnNavCancel ), TKGet( TK_Enabled ), Any( sal_True ) );
                 mrOptimizerDialog.EnablePage( ITEM_ID_SUMMARY );
