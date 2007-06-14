b65d77079cb70218774e8e693a2d0bba846498c0
Dirk Völzke <dv@openoffice.org>
2007-06-14 06:16:44 +0000

    #i78425# fixed usage of wrong namespace

--- desktop/source/deployment/manager/dp_informationprovider.cxx
+++ desktop/source/deployment/manager/dp_informationprovider.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dp_informationprovider.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2007-06-12 06:19:11 $
+ *  last change: $Author: dv $ $Date: 2007-06-14 07:16:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -193,7 +193,7 @@ PackageInformationProvider::getPackageLocation( const rtl::OUString& _sExtension
 
     if ( aLocationURL.getLength() )
     {
-        ::ucb::Content aContent( aLocationURL, NULL );
+        ::ucbhelper::Content aContent( aLocationURL, NULL );
         aLocationURL = aContent.getURL();
     }
 
