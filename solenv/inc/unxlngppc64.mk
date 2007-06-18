5c153fb90c34d20cb415864f2d1f28d00052ad04
Kurt Zenker <kz@openoffice.org>
2007-06-18 15:34:03 +0000

    INTEGRATION: CWS ppc64one (1.1.2); FILE ADDED 2007/06/01 12:05:04 cmc 1.1.2.2: #i77898# add the toc stuff to the other flags 2007/05/29 14:18:59 cmc 1.1.2.1: #i77898# add ppc64

---
+++ solenv/inc/unxlngppc64.mk
@@ -0,0 +1,40 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: unxlngppc64.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2007-06-18 16:34:03 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+# mk file for linux ppc64, inherit from ppc and add some flags
+
+.INCLUDE : unxlngppc4.mk
+
+CDEFS+=-DPOWERPC64
+CFLAGSCXX+=-mminimal-toc
