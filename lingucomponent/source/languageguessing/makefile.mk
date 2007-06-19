4020743a66e0d439bc4bfcc9d870a1bbbc6719f5
Kurt Zenker <kz@openoffice.org>
2007-06-19 15:01:34 +0000

    INTEGRATION: CWS languageguessing (1.1.2); FILE ADDED 2007/01/12 14:22:37 tl 1.1.2.3: #i73173# integrate Google SoC language-guessing 2007/01/12 14:04:59 tl 1.1.2.2: #i73173# integrate Google SoC language-guessing 2007/01/12 11:06:52 tl 1.1.2.1: #i73173# integrate Google SoC language-guessing

---
+++ lingucomponent/source/languageguessing/makefile.mk
@@ -0,0 +1,91 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2007-06-19 16:01:34 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+PRJNAME=lingucomponent
+
+TARGET=guesslang
+
+#EXTERNAL_WARNINGS_NOT_ERRORS=TRUE
+
+ENABLE_EXCEPTIONS=TRUE
+
+#----- Settings ---------------------------------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files --------------------------------------------------------
+
+.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+LIBTEXTCATLIB=-ltextcat
+.ELSE               # "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+LIBTEXTCATLIB=ilibtextcat.lib
+.ENDIF  # "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+
+SLOFILES = \
+        $(SLO)$/altstrfunc.obj \
+        $(SLO)$/guess.obj \
+        $(SLO)$/guesslang.obj \
+        $(SLO)$/simpleguesser.obj
+
+
+SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+
+SHL1STDLIBS= \
+        $(CPPUHELPERLIB)        \
+        $(CPPULIB)              \
+        $(LIBTEXTCATLIB)        \
+        $(SALLIB)               \
+        $(SVLLIB)               \
+        $(SVTOOLLIB)            \
+        $(TOOLSLIB)             \
+        $(UNOTOOLSLIB)
+
+# build DLL
+SHL1LIBS=       $(SLB)$/$(TARGET).lib
+SHL1IMPLIB=     i$(TARGET)
+SHL1DEPN=       $(SHL1LIBS)
+SHL1DEF=        $(MISC)$/$(SHL1TARGET).def
+.IF "$(OS)"!="MACOSX"
+SHL1VERSIONMAP= $(TARGET).map
+.ENDIF
+
+# build DEF file
+DEF1NAME    =$(SHL1TARGET)
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : target.mk
+
