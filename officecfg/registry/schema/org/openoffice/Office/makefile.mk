4dd47bca01728052c032e4486dd51f8273cbbd7d
Kurt Zenker <kz@openoffice.org>
2007-06-20 13:00:28 +0000

    INTEGRATION: CWS jl65 (1.14.156); FILE MERGED 2007/06/12 08:09:33 jl 1.14.156.1: #i78209# Download extension link in Extension Manager

--- officecfg/registry/schema/org/openoffice/Office/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: obo $ $Date: 2006-09-15 14:00:37 $
+#   last change: $Author: kz $ $Date: 2007-06-20 14:00:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,6 +55,7 @@ XCSFILES= \
     Draw.xcs \
     Events.xcs \
     Embedding.xcs \
+    ExtensionManager.xcs \
     Impress.xcs \
     Java.xcs \
     Jobs.xcs \
