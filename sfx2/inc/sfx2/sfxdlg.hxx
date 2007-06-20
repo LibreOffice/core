1b6d1b00e1d4fa140493b23244dd4dfb07e2c88d
Kurt Zenker <kz@openoffice.org>
2007-06-20 09:44:22 +0000

    INTEGRATION: CWS jl61 (1.2.38); FILE MERGED 2007/06/15 14:06:01 jl 1.2.38.2: RESYNC: (1.2-1.3); FILE MERGED 2007/05/07 10:39:39 pb 1.2.38.1: fix: #i75869# CreateOptionsDialog() added

--- sfx2/inc/sfx2/sfxdlg.hxx
+++ sfx2/inc/sfx2/sfxdlg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxdlg.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 10:05:45 $
+ *  last change: $Author: kz $ $Date: 2007-06-20 10:44:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -172,6 +172,9 @@ public:
 
     static SfxTabPage *CreateSfxAcceleratorConfigPage(
         Window *pParent, const SfxItemSet& rSet );
+
+    virtual VclAbstractDialog*  CreateOptionsDialog(
+        Window* pParent, const rtl::OUString& rExtensionId, const rtl::OUString& rApplicationContext ) = 0;
 };
 
 #endif
