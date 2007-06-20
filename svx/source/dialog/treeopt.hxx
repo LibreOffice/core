efb117d87e0084345635384e33fe6c490312f1b2
Kurt Zenker <kz@openoffice.org>
2007-06-20 09:37:10 +0000

    INTEGRATION: CWS jl61 (1.8.272); FILE MERGED 2007/06/15 05:09:16 pb 1.8.272.8: fix: #i75869# XDialog hacks removed 2007/06/14 08:42:59 pb 1.8.272.7: fix: use new ContainerWindowProvider 2007/06/04 06:27:08 pb 1.8.272.6: fix: #i75869# last page saving 2007/06/01 13:22:40 pb 1.8.272.5: fix: #i75869# some more enhancements 2007/05/28 11:52:22 pb 1.8.272.4: fix: #i75869# more improvements 2007/05/14 09:34:27 pb 1.8.272.3: fix: #i75869# more enhancements 2007/05/11 07:25:48 pb 1.8.272.2: fix: #i75869# more extensions options implemented 2007/05/07 10:47:22 pb 1.8.272.1: fix: #i75869# LoadFromConfig(), GetGroupId(), Ctor() added

--- svx/source/dialog/treeopt.hxx
+++ svx/source/dialog/treeopt.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: treeopt.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 12:32:38 $
+ *  last change: $Author: kz $ $Date: 2007-06-20 10:37:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,8 +55,20 @@
 #ifndef _SV_FIXBRD_HXX //autogen
 #include <vcl/fixbrd.hxx>
 #endif
+#ifndef _SV_FIXED_HXX
+#include <vcl/fixed.hxx>
+#endif
 
-#define NUMBER_OF_OPTION_PAGES  12
+#include <vector>
+
+// static ----------------------------------------------------------------
+
+BOOL            EnableSSO();
+CreateTabPage   GetSSOCreator( void );
+
+// class OfaOptionsTreeListBox -------------------------------------------
+
+//!#define NUMBER_OF_OPTION_PAGES  12
 class SfxModule;
 class SfxShell;
 class SfxItemSet;
@@ -76,13 +88,115 @@ public:
     BOOL            IsInCollapse()const {return bInCollapse;}
 };
 
-BOOL            EnableSSO();
-CreateTabPage   GetSSOCreator( void );
+// struct OrderedEntry ---------------------------------------------------
 
-/* -----------------11.02.99 07:51-------------------
- *
- * --------------------------------------------------*/
+struct OrderedEntry
+{
+    sal_Int32       m_nIndex;
+    rtl::OUString   m_sId;
+
+    OrderedEntry( sal_Int32 nIndex, const rtl::OUString& rId ) :
+        m_nIndex( nIndex ), m_sId( rId ) {}
+};
+
+typedef std::vector< OrderedEntry* > VectorOfOrderedEntries;
+
+// struct Module ---------------------------------------------------------
+
+struct Module
+{
+    rtl::OUString           m_sName;
+    bool                    m_bActive;
+    VectorOfOrderedEntries  m_aNodeList;
+
+    Module( const rtl::OUString& rName ) : m_sName( rName ), m_bActive( false ) {}
+};
+
+// struct OptionsLeaf ----------------------------------------------------
+
+struct OptionsLeaf
+{
+    rtl::OUString   m_sId;
+    rtl::OUString   m_sLabel;
+    rtl::OUString   m_sPageURL;
+    rtl::OUString   m_sEventHdl;
+    rtl::OUString   m_sGroupId;
+    sal_Int32       m_nGroupIndex;
+
+    OptionsLeaf(    const rtl::OUString& rId,
+                    const rtl::OUString& rLabel,
+                    const rtl::OUString& rPageURL,
+                    const rtl::OUString& rEventHdl,
+                    const rtl::OUString& rGroupId,
+                    sal_Int32 nGroupIndex ) :
+        m_sId( rId ),
+        m_sLabel( rLabel ),
+        m_sPageURL( rPageURL ),
+        m_sEventHdl( rEventHdl ),
+        m_sGroupId( rGroupId ),
+        m_nGroupIndex( nGroupIndex ) {}
+};
+
+typedef ::std::vector< OptionsLeaf* > VectorOfLeaves;
+typedef ::std::vector< VectorOfLeaves > VectorOfGroupedLeaves;
+
+// struct OptionsNode ----------------------------------------------------
+
+struct OptionsNode
+{
+    rtl::OUString           m_sId;
+    rtl::OUString           m_sLabel;
+    rtl::OUString           m_sPageURL;
+    bool                    m_bAllModules;
+    rtl::OUString           m_sGroupId;
+    sal_Int32               m_nGroupIndex;
+    VectorOfLeaves          m_aLeaves;
+    VectorOfGroupedLeaves   m_aGroupedLeaves;
+
+    OptionsNode(    const rtl::OUString& rId,
+                    const rtl::OUString& rLabel,
+                    const rtl::OUString& rPageURL,
+                    bool bAllModules,
+                    const rtl::OUString& rGroupId,
+                    sal_Int32 nGroupIndex ) :
+        m_sId( rId ),
+        m_sLabel( rLabel ),
+        m_sPageURL( rPageURL ),
+        m_bAllModules( bAllModules ),
+        m_sGroupId( rGroupId ),
+        m_nGroupIndex( nGroupIndex ) {}
+
+    ~OptionsNode()
+    {
+        for ( sal_uInt32 i = 0; i < m_aLeaves.size(); ++i )
+            delete m_aLeaves[i];
+        m_aLeaves.clear();
+        m_aGroupedLeaves.clear();
+    }
+};
+
+typedef ::std::vector< OptionsNode* > VectorOfNodes;
+
+struct LastPageSaver
+{
+    USHORT          m_nLastPageId;
+    rtl::OUString   m_sLastPageURL_Tools;
+    rtl::OUString   m_sLastPageURL_ExtMgr;
+
+    LastPageSaver() : m_nLastPageId( USHRT_MAX ) {}
+};
+
+// class OfaTreeOptionsDialog --------------------------------------------
+
 namespace com { namespace sun { namespace star { namespace frame { class XFrame; } } } }
+namespace com { namespace sun { namespace star { namespace container { class XNameAccess; } } } }
+namespace com { namespace sun { namespace star { namespace lang { class XMultiServiceFactory; } } } }
+namespace com { namespace sun { namespace star { namespace awt { class XContainerWindowProvider; } } } }
+
+struct OptionsPageInfo;
+struct Module;
+class ExtensionsTabPage;
+typedef std::vector< ExtensionsTabPage* > VectorOfPages;
 
 class OfaTreeOptionsDialog : public SfxModalDialog
 {
@@ -108,7 +222,7 @@ private:
     String          sTitle;
     String          sNotLoadedError;
 
-    SvLBoxEntry*            pCurrentPageEntry;
+    SvLBoxEntry*    pCurrentPageEntry;
 
     // for the ColorTabPage
     SfxItemSet*     pColorPageItemSet;
@@ -122,40 +236,61 @@ private:
     BOOL            bExternBrowserActive;
     BOOL            bImageResized;
     bool            bInSelectHdl_Impl;
+    bool            bIsFromExtensionManager;
 
     Timer           aSelectTimer;
 
-    static USHORT   nLastDialogPageId;
+    com::sun::star::uno::Reference < com::sun::star::awt::XContainerWindowProvider >
+                    m_xContainerWinProvider;
+
+    static LastPageSaver*   pLastPageSaver;
 
     SfxItemSet*     CreateItemSet( USHORT nId );
     void            ApplyItemSet( USHORT nId, const SfxItemSet& rSet );
+    void            InitTreeAndHandler();
     void            Initialize( const com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& _xFrame );
     void            ResizeTreeLB( void );   // resizes dialog so that treelistbox has no horizontal scroll bar
 
-protected:
-        DECL_LINK(ExpandedHdl_Impl, SvTreeListBox* );
-        DECL_LINK(ShowPageHdl_Impl, SvTreeListBox* );
-        DECL_LINK(BackHdl_Impl, PushButton* );
-        DECL_LINK( OKHdl_Impl, Button * );
-        DECL_LINK( HintHdl_Impl, Timer * );
-        DECL_LINK( SelectHdl_Impl, Timer * );
+    void            LoadExtensionOptions( const rtl::OUString& rExtensionId );
+    rtl::OUString   GetModuleIdentifier( const com::sun::star::uno::Reference<
+                                            com::sun::star::lang::XMultiServiceFactory >& xMFac,
+                                         const com::sun::star::uno::Reference<
+                                            com::sun::star::frame::XFrame >& xFrame );
+    Module*         LoadModule( const rtl::OUString& rModuleIdentifier,
+                                const com::sun::star::uno::Reference<
+                                    com::sun::star::container::XNameAccess >& xRoot );
+    void            LoadNodes( const com::sun::star::uno::Reference<
+                                    com::sun::star::container::XNameAccess >& xRoot,
+                               Module* pModule,
+                               const rtl::OUString& rExtensionId,
+                               VectorOfNodes& rOutNodeList );
+    void            InsertNodes( const VectorOfNodes& rNodeList );
 
-        virtual long    Notify( NotifyEvent& rNEvt );
-        virtual void    DataChanged( const DataChangedEvent& rDCEvt );
-        virtual short   Execute();
+protected:
+    DECL_LINK(ExpandedHdl_Impl, SvTreeListBox* );
+    DECL_LINK(ShowPageHdl_Impl, SvTreeListBox* );
+    DECL_LINK(BackHdl_Impl, PushButton* );
+    DECL_LINK( OKHdl_Impl, Button * );
+    DECL_LINK( HintHdl_Impl, Timer * );
+    DECL_LINK( SelectHdl_Impl, Timer * );
+
+    virtual long    Notify( NotifyEvent& rNEvt );
+    virtual void    DataChanged( const DataChangedEvent& rDCEvt );
+    virtual short   Execute();
 
 public:
-        OfaTreeOptionsDialog( Window* pParent,
-            const com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& _xFrame );
-        ~OfaTreeOptionsDialog();
+    OfaTreeOptionsDialog( Window* pParent,
+        const com::sun::star::uno::Reference< com::sun::star::frame::XFrame >& _xFrame );
+    OfaTreeOptionsDialog( Window* pParent, const rtl::OUString& rExtensionId );
+    ~OfaTreeOptionsDialog();
 
-        void    AddTabPage( USHORT nId, const String& rPageName, USHORT nGroup);
-        USHORT  AddGroup(const String& rGroupName,  SfxShell* pCreateShell,
-                                    SfxModule* pCreateModule, USHORT nDialogId);
-        void    ActivateLastSelection();
-        void    ActivatePage(USHORT nResId);
-        void    ApplyItemSets();
+    OptionsPageInfo*    AddTabPage( USHORT nId, const String& rPageName, USHORT nGroup );
+    USHORT              AddGroup(   const String& rGroupName,  SfxShell* pCreateShell,
+                                    SfxModule* pCreateModule, USHORT nDialogId );
 
+    void    ActivateLastSelection();
+    void    ActivatePage(USHORT nResId);
+    void    ApplyItemSets();
 
     USHORT                  GetColorChanged() const { return nChangeType; }
     XColorTable*            GetColorTable() { return pColorTab; }
@@ -163,9 +298,9 @@ public:
     // helper functions to call the language settings TabPage from the SpellDialog
     static  void ApplyLanguageOptions(const SfxItemSet& rSet);
 };
-/* -----------------11.02.99 15:49-------------------
- *
- * --------------------------------------------------*/
+
+// class OfaPageResource -------------------------------------------------
+
 class OfaPageResource : public Resource
 {
     ResStringArray      aGeneralDlgAry;
@@ -196,5 +331,49 @@ public:
     ResStringArray& GetChartArray()         {return aChartDlgAry;}
     ResStringArray& GetFilterArray()        {return aFilterDlgAry;}
     ResStringArray& GetDatasourcesArray()   {return aDatasourcesDlgAry;}
+};
+
+// class ExtensionsTabPage -----------------------------------------------
+
+namespace com { namespace sun { namespace star { namespace awt { class XWindow; } } } }
+namespace com { namespace sun { namespace star { namespace awt { class XContainerWindowEventHandler; } } } }
+
+class ExtensionsTabPage : public TabPage
+{
+private:
+    rtl::OUString       m_sPageURL;
+    com::sun::star::uno::Reference< com::sun::star::awt::XWindow >
+                        m_xPage;
+    rtl::OUString       m_sEventHdl;
+    com::sun::star::uno::Reference< com::sun::star::awt::XContainerWindowEventHandler >
+                        m_xEventHdl;
+    com::sun::star::uno::Reference< com::sun::star::awt::XContainerWindowProvider >
+                        m_xWinProvider;
+    bool                m_bIsWindowHidden;
+
+    void                CreateDialogWithHandler();
+    sal_Bool            DispatchAction( const rtl::OUString& rAction );
+
+public:
+    ExtensionsTabPage(
+        Window* pParent, const ResId&,
+        const rtl::OUString& rPageURL, const rtl::OUString& rEvtHdl,
+        const com::sun::star::uno::Reference<
+            com::sun::star::awt::XContainerWindowProvider >& rProvider );
+
+    ExtensionsTabPage(
+        Window* pParent, WinBits nStyle,
+        const rtl::OUString& rPageURL, const rtl::OUString& rEvtHdl,
+        const com::sun::star::uno::Reference<
+            com::sun::star::awt::XContainerWindowProvider >& rProvider );
+
+    virtual ~ExtensionsTabPage();
+
+    virtual void    ActivatePage();
+    virtual void    DeactivatePage();
+
+    void            ResetPage();
+    void            SavePage();
+    void            HideWindow();
 };
 
