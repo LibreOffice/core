be91d19c08a07bd706ad723a11d3c909671413c9
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-26 15:05:41 +0000

    INTEGRATION: CWS mav21 (1.8.88); FILE MERGED 2007/03/08 12:38:02 mav 1.8.88.1: #i75233# the implementation for unknown objects

--- embeddedobj/util/makefile.mk
+++ embeddedobj/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 18:50:48 $
+#   last change: $Author: hr $ $Date: 2007-06-26 16:05:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -54,6 +54,7 @@ LIB1OBJFILES= \
         $(SLO)$/embedobj.obj\
         $(SLO)$/inplaceobj.obj\
         $(SLO)$/visobj.obj\
+        $(SLO)$/dummyobject.obj\
         $(SLO)$/xcreator.obj\
         $(SLO)$/xfactory.obj\
         $(SLO)$/register.obj\
