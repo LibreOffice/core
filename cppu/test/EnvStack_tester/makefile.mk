4478b0d04c3c12d1f3c4bf1c751ad3be7c7456e8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-26 16:20:26 +0000

    INTEGRATION: CWS ause081 (1.2.8); FILE MERGED 2007/06/22 12:25:51 hjs 1.2.8.1: #i78393# remove now obsolete setting to avoid description

--- cppu/test/EnvStack_tester/makefile.mk
+++ cppu/test/EnvStack_tester/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2007-05-09 13:42:46 $
+#   last change: $Author: hr $ $Date: 2007-06-26 17:20:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,6 @@ TARGET  := EnvStack.tester
 
 ENABLE_EXCEPTIONS  := TRUE
 NO_BSYMBOLIC       := TRUE
-NO_SHL_DESCRIPTION := TRUE
 USE_DEFFILE        := TRUE
 
 
