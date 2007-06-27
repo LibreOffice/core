bfae32aa4d946e884965a491804d9c07e632cb31
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:12:13 +0000

    INTEGRATION: CWS vgbugs07 (1.2.274); FILE MERGED 2007/06/04 13:23:26 vg 1.2.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/draw/ximpshow.hxx
+++ xmloff/source/draw/ximpshow.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximpshow.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:01:37 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:12:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_XIMPSHOW_HXX
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 #ifndef _SDXMLIMP_IMPL_HXX
