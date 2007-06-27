5bb0699c8bc6a44bda6b6e635c653b49e8ce1c02
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:36:28 +0000

    INTEGRATION: CWS vgbugs07 (1.19.160); FILE MERGED 2007/06/04 13:31:37 vg 1.19.160.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/filter.vcl/jpeg/jpeg.cxx
+++ svtools/source/filter.vcl/jpeg/jpeg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: jpeg.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 15:41:27 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:36:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,8 +57,8 @@ extern "C"
 #define _JPEGPRIVATE
 #include <vcl/bmpacc.hxx>
 #include "jpeg.hxx"
-#include "FilterConfigItem.hxx"
-#include "filter.hxx"
+#include <svtools/FilterConfigItem.hxx>
+#include <svtools/filter.hxx>
 
 // -----------
 // - Defines -
