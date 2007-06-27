3c63ae6ded52f763725bb3b51b76243403a90273
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:05:56 +0000

    INTEGRATION: CWS vgbugs07 (1.4.526); FILE MERGED 2007/06/04 13:31:24 vg 1.4.526.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/inc/xmlaccelcfg.hxx
+++ svtools/inc/xmlaccelcfg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlaccelcfg.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 14:18:27 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:05:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #include <cppuhelper/weak.hxx>
 #endif
 
-#include <accelcfg.hxx>
+#include <svtools/accelcfg.hxx>
 
 class OReadAccelatorDocumentHandler : public ::com::sun::star::xml::sax::XDocumentHandler,
                                       public ::cppu::OWeakObject
