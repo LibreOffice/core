32c19b7392b69ec8cda00694a29eb2da7e29795b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:17:01 +0000

    INTEGRATION: CWS vgbugs07 (1.6.200); FILE MERGED 2007/06/04 13:31:27 vg 1.6.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/config/options3d.cxx
+++ svtools/source/config/options3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: options3d.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 14:27:52 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:15:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 //  includes
 //_________________________________________________________________________________________________________________
 
-#include "options3d.hxx"
+#include <svtools/options3d.hxx>
 
 #ifndef _UTL_CONFIGMGR_HXX_
 #include <unotools/configmgr.hxx>
--- svtools/source/config/sourceviewconfig.cxx
+++ svtools/source/config/sourceviewconfig.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sourceviewconfig.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 14:29:56 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:17:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 #ifndef _SVTOOLS_SOURCEVIEWCONFIG_HXX
-#include <sourceviewconfig.hxx>
+#include <svtools/sourceviewconfig.hxx>
 #endif
 #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
@@ -54,7 +54,7 @@
 #include <rtl/instance.hxx>
 #endif
 #ifndef _SFXSMPLHINT_HXX
-#include "smplhint.hxx"
+#include <svtools/smplhint.hxx>
 #endif
 
 #include <itemholder1.hxx>
