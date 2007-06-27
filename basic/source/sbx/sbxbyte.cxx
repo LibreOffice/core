20830b41a90c37bd98baec0b3815ab4c7f6f5921
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:28:21 +0000

    INTEGRATION: CWS vgbugs07 (1.6.84); FILE MERGED 2007/06/04 13:22:18 vg 1.6.84.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- basic/source/sbx/sbxbyte.cxx
+++ basic/source/sbx/sbxbyte.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbxbyte.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:08:23 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:28:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 #ifndef _ERRCODE_HXX //autogen
 #include <tools/errcode.hxx>
 #endif
-#include "sbx.hxx"
+#include <basic/sbx.hxx>
 #include "sbxconv.hxx"
 
 BYTE ImpGetByte( const SbxValues* p )
