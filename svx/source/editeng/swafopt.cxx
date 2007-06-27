dcc9673707e0bdd2e8d7b37af726d93dc689dd8f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:00:35 +0000

    INTEGRATION: CWS vgbugs07 (1.6.352); FILE MERGED 2007/06/04 13:26:50 vg 1.6.352.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/editeng/swafopt.cxx
+++ svx/source/editeng/swafopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swafopt.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 13:40:32 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:00:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,7 +43,7 @@
 #include <tools/string.hxx>
 #endif
 
-#include "swafopt.hxx"
+#include <svx/swafopt.hxx>
 
 /*------------------------------------------------------------------------
  Beschreibung:
