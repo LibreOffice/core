ddf11c973cf7c43e540174f45f4b0d4d5f01b53e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:10:22 +0000

    INTEGRATION: CWS vgbugs07 (1.2.274); FILE MERGED 2007/06/04 13:23:39 vg 1.2.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLTextListItemContext.hxx
+++ xmloff/source/text/XMLTextListItemContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextListItemContext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:23:08 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:10:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLTEXTLISTITEMCONTEXT_HXX
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 class XMLTextImportHelper;
