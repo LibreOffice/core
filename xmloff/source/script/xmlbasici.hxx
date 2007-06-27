86d7fc6dcc77384938d0449ee3e3bda1e7445999
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:22:17 +0000

    INTEGRATION: CWS vgbugs07 (1.3.276); FILE MERGED 2007/06/04 13:23:28 vg 1.3.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/script/xmlbasici.hxx
+++ xmloff/source/script/xmlbasici.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlbasici.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:21:16 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:22:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 
