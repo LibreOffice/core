10ba0cd0fbe7787a44eb4b927670064ceeba6668
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:25:20 +0000

    INTEGRATION: CWS vgbugs07 (1.2.200); FILE MERGED 2007/06/04 13:31:29 vg 1.2.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/control/indexentryres.cxx
+++ svtools/source/control/indexentryres.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: indexentryres.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 14:39:04 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:25:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,8 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
-#include <svtdata.hxx>
-#include <svtools.hrc>
+#include <svtools/svtdata.hxx>
+#include <svtools/svtools.hrc>
 
 
 #ifndef SVTOOLS_INDEXENTRYRESSOURCE_HXX
