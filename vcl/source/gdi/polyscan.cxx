068fc38032e76d2d6a76dac9c5ae80be9d8184b0
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:22:07 +0000

    INTEGRATION: CWS vgbugs07 (1.4.240); FILE MERGED 2007/06/04 13:29:38 vg 1.4.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/gdi/polyscan.cxx
+++ vcl/source/gdi/polyscan.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: polyscan.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:09:22 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:22:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 #include <string.h>
 #include <tools/new.hxx>
-#include "salbtype.hxx"
+#include <vcl/salbtype.hxx>
 #include "polyscan.hxx"
 
 #ifndef _TL_POLY_HXX
