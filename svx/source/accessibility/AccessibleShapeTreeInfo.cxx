c024bc7d953d4e7df549352f9033e2f60e4273a8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:39:22 +0000

    INTEGRATION: CWS vgbugs07 (1.11.354); FILE MERGED 2007/06/04 13:26:08 vg 1.11.354.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/accessibility/AccessibleShapeTreeInfo.cxx
+++ svx/source/accessibility/AccessibleShapeTreeInfo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleShapeTreeInfo.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 04:02:40 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:39:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
 
-#include "AccessibleShapeTreeInfo.hxx"
+#include <svx/AccessibleShapeTreeInfo.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
