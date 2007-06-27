213cc725b482e97c0cff6b70302e966cef19bded
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:06:29 +0000

    INTEGRATION: CWS vgbugs07 (1.8.124); FILE MERGED 2007/06/04 13:23:38 vg 1.8.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLSectionFootnoteConfigImport.cxx
+++ xmloff/source/text/XMLSectionFootnoteConfigImport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLSectionFootnoteConfigImport.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:11:08 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:05:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,19 +56,19 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 #ifndef _XMLOFF_PROPERTYSETMAPPER_HXX
-#include "xmlprmap.hxx"
+#include <xmloff/xmlprmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -76,19 +76,19 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_PROPMAPPINGTYPES_HXX
-#include "maptype.hxx"
+#include <xmloff/maptype.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNUMI_HXX
-#include "xmlnumi.hxx"
+#include <xmloff/xmlnumi.hxx>
 #endif
 
 #ifndef _XMLOFF_TEXTPRMAP_HXX_
-#include "txtprmap.hxx"
+#include <xmloff/txtprmap.hxx>
 #endif
 
 #ifndef _TOOLS_DEBUG_HXX
--- xmloff/source/text/XMLSectionSourceImportContext.cxx
+++ xmloff/source/text/XMLSectionSourceImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLSectionSourceImportContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:11:50 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:06:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,19 +49,19 @@
 #endif
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _XMLOFF_TEXTIMP_HXX_
-#include "txtimp.hxx"
+#include <xmloff/txtimp.hxx>
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -69,7 +69,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_H_
