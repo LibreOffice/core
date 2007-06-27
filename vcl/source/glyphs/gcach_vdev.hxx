070f61d21aa5d4ed126d238ae473d87f24ec4b1c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:25:38 +0000

    INTEGRATION: CWS vgbugs07 (1.9.606); FILE MERGED 2007/06/04 13:29:45 vg 1.9.606.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/glyphs/gcach_vdev.hxx
+++ vcl/source/glyphs/gcach_vdev.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gcach_vdev.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:14:36 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:25:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,7 @@
  *
  ************************************************************************/
 
-#include <glyphcache.hxx>
+#include <vcl/glyphcache.hxx>
 
 // -----------------------------------------------------------------------
 
