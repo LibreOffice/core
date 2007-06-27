0e020270ded4d3067c5533f466d3a91e16582887
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:13:14 +0000

    INTEGRATION: CWS vgbugs07 (1.12.8); FILE MERGED 2007/06/04 13:22:14 vg 1.12.8.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- basic/source/app/appwin.cxx
+++ basic/source/app/appwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: appwin.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 15:18:15 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:13:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@
 
 #include "basic.hrc"
 #include "app.hxx"
-#include "mybasic.hxx"
+#include <basic/mybasic.hxx>
 #include "status.hxx"
 #include "appwin.hxx"
 #include "dataedit.hxx"
