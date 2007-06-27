57235a1c525ff0a72f41108dc387f17ad2d5ef19
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:54:51 +0000

    INTEGRATION: CWS vgbugs07 (1.27.320); FILE MERGED 2007/06/04 13:26:48 vg 1.27.320.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/editeng/editdoc.hxx
+++ svx/source/editeng/editdoc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editdoc.hxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 12:35:25 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:54:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 #include <editattr.hxx>
 #include <edtspell.hxx>
-#include <svxfont.hxx>
+#include <svx/svxfont.hxx>
 
 #ifndef _SFXITEMSET_HXX //autogen
 #include <svtools/itemset.hxx>
