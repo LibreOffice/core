86538aa2963a0cef3be2230c9cb439c1aa7cd25f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:36:03 +0000

    INTEGRATION: CWS vgbugs07 (1.9.90); FILE MERGED 2007/06/04 13:23:31 vg 1.9.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/breakhdl.cxx
+++ xmloff/source/style/breakhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: breakhdl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-21 17:37:12 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:36:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,11 +41,11 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 #ifndef _RTL_USTRBUF_HXX_
@@ -61,7 +61,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLEMENT_HXX
-#include "xmlelement.hxx"
+#include <xmloff/xmlelement.hxx>
 #endif
 
 using namespace ::rtl;
