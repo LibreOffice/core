102591986b5ade82a6aa31f79de0cdce9ebc3cc3
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 22:38:25 +0000

    INTEGRATION: CWS vgbugs07 (1.5.22); FILE MERGED 2007/06/04 13:35:21 vg 1.5.22.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- soldep/source/depper.cxx
+++ soldep/source/depper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: depper.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-25 16:28:10 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 23:38:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,8 +40,8 @@
 #include <vcl/msgbox.hxx>
 #include <svtools/prgsbar.hxx>
 
-#include "depper.hxx"
-#include "objwin.hxx"
+#include <soldep/depper.hxx>
+#include <soldep/objwin.hxx>
 
 #include "math.h"
 #include "time.h"
