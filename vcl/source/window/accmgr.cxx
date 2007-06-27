b143a502f9a963ded81bb31dde517dc36c2ba8d9
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:27:58 +0000

    INTEGRATION: CWS vgbugs07 (1.6.240); FILE MERGED 2007/06/04 13:29:46 vg 1.6.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/window/accmgr.cxx
+++ vcl/source/window/accmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accmgr.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:15:24 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:27:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,7 +47,7 @@
 #include <accel.h>
 #endif
 #ifndef _SV_ACCEL_HXX
-#include <accel.hxx>
+#include <vcl/accel.hxx>
 #endif
 #ifndef _SV_ACCMGR_HXX
 #include <accmgr.hxx>
