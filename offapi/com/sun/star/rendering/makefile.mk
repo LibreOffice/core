899f624b8f1f1b361e9977e656b339bbd21f2b3c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 11:29:38 +0000

    INTEGRATION: CWS awttree01 (1.5.70); FILE MERGED 2007/03/11 22:03:17 thb 1.5.70.1: #i75289# Initial version of a simplified XCanvas interface

--- offapi/com/sun/star/rendering/makefile.mk
+++ offapi/com/sun/star/rendering/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2006-12-01 17:11:37 $
+#   last change: $Author: hr $ $Date: 2007-06-27 12:29:38 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -106,6 +106,7 @@ IDLFILES=\
     XCachedPrimitive.idl \
     XGraphicDevice.idl \
     XCanvas.idl \
+    XSimpleCanvas.idl \
     XCustomSprite.idl \
     XCanvasFont.idl \
     XHalfFloatBitmap.idl \
