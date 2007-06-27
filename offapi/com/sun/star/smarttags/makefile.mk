ef1a0e325d9f5bd2fbf25cd1c470e201e32d9de0
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 12:33:52 +0000

    INTEGRATION: CWS smarttags3 (1.2.30); FILE MERGED 2007/04/16 12:35:15 fme 1.2.30.1: #i75130# New Smart Tag API

--- offapi/com/sun/star/smarttags/makefile.mk
+++ offapi/com/sun/star/smarttags/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2007-01-02 16:56:43 $
+#   last change: $Author: hr $ $Date: 2007-06-27 13:33:52 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,6 +46,9 @@ PACKAGE=com$/sun$/star$/smarttags
 # ------------------------------------------------------------------------
 
 IDLFILES=\
+        SmartTagAction.idl\
+        SmartTagRecognizer.idl\
+        SmartTagRecognizerMode.idl\
         XSmartTagAction.idl\
         XSmartTagRecognizer.idl
 
