1fe83bfbb5b31c9bd8502b2151e2fb11deaf7785
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:42:26 +0000

    INTEGRATION: CWS vgbugs07 (1.4.352); FILE MERGED 2007/06/04 13:26:28 vg 1.4.352.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/stddlg.cxx
+++ svx/source/dialog/stddlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stddlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 04:39:55 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:41:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 #include <vcl/msgbox.hxx>
 #endif
 
-#include "stddlg.hxx"
+#include <svx/stddlg.hxx>
 
 // STATIC DATA -----------------------------------------------------------
 
