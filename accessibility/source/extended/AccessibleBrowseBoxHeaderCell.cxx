9fdb3c7ed44c97f625cdc2ab31af886c7d8eed7a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:31:12 +0000

    INTEGRATION: CWS a11ysep (1.1.2); FILE ADDED 2007/02/28 07:26:16 fs 1.1.2.4: #i10000# 2006/10/04 19:58:02 fs 1.1.2.3: MANUAL RESYNC m130->m185: file was changed in MWS, but has been moved herein in CWS 2005/09/28 11:36:27 fs 1.1.2.2: manual resync (files have been moved herein from another location): licence change 2005/03/07 08:29:15 fs 1.1.2.1: #i44293# moved implementations herein from svtools module

---
+++ accessibility/source/extended/AccessibleBrowseBoxHeaderCell.cxx
@@ -0,0 +1,186 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: AccessibleBrowseBoxHeaderCell.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:31:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_accessibility.hxx"
+
+#include "accessibility/extended/AccessibleBrowseBoxHeaderCell.hxx"
+
+#ifndef _SVTOOLS_ACCESSIBLETABLEPROVIDER_HXX
+#include <svtools/accessibletableprovider.hxx>
+#endif
+#ifndef ACCESSIBILITY_EXT_ACCESSIBLEBROWSEBOX_HXX
+#include "accessibility/extended/AccessibleBrowseBox.hxx"
+#endif
+
+namespace accessibility
+{
+    using namespace ::com::sun::star::accessibility;
+    using namespace ::com::sun::star::lang;
+    using namespace ::com::sun::star::uno;
+    using namespace ::svt;
+
+AccessibleBrowseBoxHeaderCell::AccessibleBrowseBoxHeaderCell(sal_Int32 _nColumnRowId,
+                                  const Reference< XAccessible >& rxParent,
+                                  IAccessibleTableProvider&                  rBrowseBox,
+                                  const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindow >& _xFocusWindow,
+                                  AccessibleBrowseBoxObjType  eObjType)
+: BrowseBoxAccessibleElement(rxParent,
+                          rBrowseBox,
+                          _xFocusWindow,
+                          eObjType,
+                          rBrowseBox.GetAccessibleObjectName( eObjType ,_nColumnRowId),
+                          rBrowseBox.GetAccessibleObjectDescription( eObjType ,_nColumnRowId))
+, m_nColumnRowId(_nColumnRowId)
+{
+}
+/** Creates a new AccessibleStateSetHelper and fills it with states of the
+    current object.
+    @return
+        A filled AccessibleStateSetHelper.
+*/
+::utl::AccessibleStateSetHelper* AccessibleBrowseBoxHeaderCell::implCreateStateSetHelper()
+{
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ::utl::AccessibleStateSetHelper*
+        pStateSetHelper = new ::utl::AccessibleStateSetHelper;
+
+    if( isAlive() )
+    {
+        // SHOWING done with mxParent
+        if( implIsShowing() )
+            pStateSetHelper->AddState( AccessibleStateType::SHOWING );
+
+        BBSolarGuard aSolarGuard;
+        pStateSetHelper->AddState( AccessibleStateType::VISIBLE );
+        pStateSetHelper->AddState( AccessibleStateType::FOCUSABLE );
+        pStateSetHelper->AddState( AccessibleStateType::TRANSIENT );
+        pStateSetHelper->AddState( AccessibleStateType::SELECTABLE );
+
+        sal_Bool bSelected = isRowBarCell() ? mpBrowseBox->IsRowSelected(m_nColumnRowId) : mpBrowseBox->IsColumnSelected(m_nColumnRowId);
+        if ( bSelected )
+            pStateSetHelper->AddState( AccessibleStateType::SELECTED );
+    }
+    else
+        pStateSetHelper->AddState( AccessibleStateType::DEFUNC );
+
+    return pStateSetHelper;
+}
+// -----------------------------------------------------------------------------
+/** @return
+        The count of visible children.
+*/
+sal_Int32 SAL_CALL AccessibleBrowseBoxHeaderCell::getAccessibleChildCount()
+    throw ( RuntimeException )
+{
+    return 0;
+}
+// -----------------------------------------------------------------------------
+
+/** @return
+        The XAccessible interface of the specified child.
+*/
+Reference<XAccessible > SAL_CALL AccessibleBrowseBoxHeaderCell::getAccessibleChild( sal_Int32 )
+    throw ( IndexOutOfBoundsException,RuntimeException )
+{
+    throw IndexOutOfBoundsException();
+}
+// -----------------------------------------------------------------------------
+
+/** Grabs the focus to the column header. */
+void SAL_CALL AccessibleBrowseBoxHeaderCell::grabFocus()
+    throw ( ::com::sun::star::uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    if ( isRowBarCell() )
+        mpBrowseBox->SelectRow(m_nColumnRowId);
+    else
+        mpBrowseBox->SelectColumn(static_cast<sal_uInt16>(m_nColumnRowId)); //!!!
+}
+// -----------------------------------------------------------------------------
+/** @return
+        The name of this class.
+*/
+::rtl::OUString SAL_CALL AccessibleBrowseBoxHeaderCell::getImplementationName()
+    throw ( ::com::sun::star::uno::RuntimeException )
+{
+    return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.svtools.AccessibleBrowseBoxHeaderCell" ) );
+}
+// -----------------------------------------------------------------------------
+namespace
+{
+    Rectangle getRectangle(IAccessibleTableProvider* _pBrowseBox,sal_Int32 _nRowColIndex, BOOL _bOnScreen,BOOL _bRowBar)
+    {
+        sal_Int32 nRow  = 0;
+        sal_uInt16 nCol =  (sal_uInt16)_nRowColIndex;
+        if ( _bRowBar )
+        {
+            nRow = _nRowColIndex + 1;
+            nCol = 0;
+        }
+
+        Rectangle aRet(_pBrowseBox->GetFieldRectPixelAbs( nRow , nCol, TRUE, _bOnScreen));
+        return Rectangle(aRet.TopLeft() - Point(0,aRet.GetHeight()),aRet.GetSize());
+    }
+}
+
+Rectangle AccessibleBrowseBoxHeaderCell::implGetBoundingBox()
+{
+    return getRectangle(mpBrowseBox,m_nColumnRowId,FALSE,isRowBarCell());
+}
+// -----------------------------------------------------------------------------
+
+Rectangle AccessibleBrowseBoxHeaderCell::implGetBoundingBoxOnScreen()
+{
+    return getRectangle(mpBrowseBox,m_nColumnRowId,TRUE,isRowBarCell());
+}
+// -----------------------------------------------------------------------------
+sal_Int32 SAL_CALL AccessibleBrowseBoxHeaderCell::getAccessibleIndexInParent()
+    throw ( RuntimeException )
+{
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    sal_Int32 nIndex = m_nColumnRowId;
+    if ( mpBrowseBox->HasRowHeader() )
+        --nIndex;
+    return nIndex;
+}
+// -----------------------------------------------------------------------------
+} // namespace accessibility
+// -----------------------------------------------------------------------------
+
+
