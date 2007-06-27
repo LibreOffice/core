463177fbd534f2e99112c0656f3bd028683d4bfd
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:47:32 +0000

    INTEGRATION: CWS vgbugs07 (1.28.114); FILE MERGED 2007/06/04 13:23:32 vg 1.28.114.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/xmlnume.cxx
+++ xmloff/source/style/xmlnume.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlnume.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-13 12:16:05 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:47:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,16 +45,16 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_PROPERTYHANDLER_FONTTYPES_HXX
 #include "fonthdl.hxx"
@@ -105,10 +105,10 @@
 #include "XMLTextListAutoStylePool.hxx"
 #endif
 #ifndef _XMLOFF_XMLNUME_HXX
-#include "xmlnume.hxx"
+#include <xmloff/xmlnume.hxx>
 #endif
 #ifndef _XMLOFF_XMLEXP_HXX
-#include "xmlexp.hxx"
+#include <xmloff/xmlexp.hxx>
 #endif
 
 #ifndef _VCL_VCLENUM_HXX
