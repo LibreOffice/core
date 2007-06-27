ddbc7ac120db630508d602b8d38808337a5b7800
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:25:50 +0000

    INTEGRATION: CWS vgbugs07 (1.6.124); FILE MERGED 2007/06/04 13:23:46 vg 1.6.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/transform/TransformerContext.cxx
+++ xmloff/source/transform/TransformerContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TransformerContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:29:20 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:25:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@
 #include <com/sun/star/xml/sax/XAttributeList.hpp>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_TRANSFOERMERBASE_HXX
