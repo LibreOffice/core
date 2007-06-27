85027382ccdcdf7344cfbb779fd5ecf4ef8a1922
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:42:10 +0000

    INTEGRATION: CWS vgbugs07 (1.6.320); FILE MERGED 2007/06/04 13:27:14 vg 1.6.320.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/outliner/outlundo.cxx
+++ svx/source/outliner/outlundo.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outlundo.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 13:02:41 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:42:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 #include <outl_pch.hxx>
 
 #define _OUTLINER_CXX
-#include <outliner.hxx>
+#include <svx/outliner.hxx>
 #include <outlundo.hxx>
 
 
