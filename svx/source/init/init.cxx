b3923ec9f46b17c57fcc327063b1df27ab4aee12
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:25:23 +0000

    INTEGRATION: CWS vgbugs07 (1.9.50); FILE MERGED 2007/06/04 13:27:03 vg 1.9.50.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/init/init.cxx
+++ svx/source/init/init.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: init.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:50:03 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:25:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,8 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
 
-#include "svxdlg.hxx"
-#include "dialogs.hrc"
+#include <svx/svxdlg.hxx>
+#include <svx/dialogs.hrc>
 
 #include "vos/mutex.hxx"
 #include <vcl/edit.hxx>
