04c8305424eb89a1e506937deb1d3e5d256671fc
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:42:13 +0000

    INTEGRATION: CWS vgbugs07 (1.2.276); FILE MERGED 2007/06/04 13:23:31 vg 1.2.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/escphdl.hxx
+++ xmloff/source/style/escphdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: escphdl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:41:33 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:39:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_ESCAPETYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
--- xmloff/source/style/opaquhdl.hxx
+++ xmloff/source/style/opaquhdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: opaquhdl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:45:55 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:42:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_OPAQUETYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
