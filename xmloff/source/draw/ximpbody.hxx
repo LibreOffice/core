cd6fa9f39cc204f5e51aabaa517fe0b67c541a61
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:10:34 +0000

    INTEGRATION: CWS vgbugs07 (1.8.276); FILE MERGED 2007/06/04 13:23:25 vg 1.8.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/draw/ximpbody.hxx
+++ xmloff/source/draw/ximpbody.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximpbody.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:57:07 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:09:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XIMPBODY_HXX
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 #ifndef _SDXMLIMP_IMPL_HXX
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XIMPPAGE_HXX
