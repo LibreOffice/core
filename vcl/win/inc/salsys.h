292f1a6974932c9e30b331eaaa083ab46f845d03
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:55:36 +0000

    INTEGRATION: CWS vgbugs07 (1.3.104); FILE MERGED 2007/06/04 13:30:02 vg 1.3.104.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/win/inc/salsys.h
+++ vcl/win/inc/salsys.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salsys.h,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-25 11:26:05 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:55:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 #ifndef _SV_SALSYS_H
 #define _SV_SALSYS_H
 
-#include <salsys.hxx>
+#include <vcl/salsys.hxx>
 
 #include <vector>
 #include <map>
