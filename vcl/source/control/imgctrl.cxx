1b6afd06c18d8e38801a7a5164f3cadbced9eb9a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:06:43 +0000

    INTEGRATION: CWS vgbugs07 (1.10.240); FILE MERGED 2007/06/04 13:29:33 vg 1.10.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/control/imgctrl.cxx
+++ vcl/source/control/imgctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: imgctrl.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:53:48 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:06:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,9 +37,9 @@
 #include "precompiled_vcl.hxx"
 
 #ifndef _SV_EVENT_HXX
-#include <event.hxx>
+#include <vcl/event.hxx>
 #endif
-#include <imgctrl.hxx>
+#include <vcl/imgctrl.hxx>
 
 // -----------------------------------------------------------------------
 
