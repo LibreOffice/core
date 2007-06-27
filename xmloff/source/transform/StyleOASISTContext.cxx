5b960b390a6a04b99dfb3bc4ec6c06de4c80d4e4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:24:12 +0000

    INTEGRATION: CWS vgbugs07 (1.15.86); FILE MERGED 2007/06/04 13:23:45 vg 1.15.86.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/transform/StyleOASISTContext.cxx
+++ xmloff/source/transform/StyleOASISTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: StyleOASISTContext.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2006-12-04 08:12:15 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:24:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,10 +52,10 @@
 #include <rtl/ustrbuf.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
@@ -91,7 +91,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
