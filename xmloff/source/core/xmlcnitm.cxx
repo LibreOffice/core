88e5f15fd4d39b108d3e564e4a8d42b26d657a0d
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:56:54 +0000

    INTEGRATION: CWS vgbugs07 (1.9.124); FILE MERGED 2007/06/04 13:23:21 vg 1.9.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/core/xmlcnitm.cxx
+++ xmloff/source/core/xmlcnitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlcnitm.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:22:03 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:56:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 #include <com/sun/star/lang/XUnoTunnel.hpp>
 #endif
 
-#include "xmlcnimp.hxx"
+#include <xmloff/xmlcnimp.hxx>
 #include "unoatrcn.hxx"
 
 using namespace rtl;
