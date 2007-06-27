9fdc7db141a30a059ba43bc3b96e4eb0489aab2d
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:58:01 +0000

    INTEGRATION: CWS sdblogging (1.1.2); FILE ADDED 2007/05/20 13:33:17 fs 1.1.2.2: #i10000# 2007/04/05 07:34:20 fs 1.1.2.1: #i76119# initial implementation of the new logging API

---
+++ extensions/source/logging/log_module.cxx
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: log_module.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:58:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_extensions.hxx"
+
+#ifndef EXTENSIONS_LOG_MODULE_HXX
+#include "log_module.hxx"
+#endif
+
+#ifndef INCLUDED_RTL_INSTANCE_HXX
+#include <rtl/instance.hxx>
+#endif
+
+//........................................................................
+namespace logging
+{
+//........................................................................
+
+    IMPLEMENT_COMPONENT_MODULE( LogModule );
+
+//........................................................................
+} // namespace logging
+//........................................................................
+
