e6517c02c6da998e04491993835dab2e6635a698
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:09:31 +0000

    INTEGRATION: CWS vgbugs07 (1.14.92); FILE MERGED 2007/06/04 13:23:25 vg 1.14.92.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/draw/ximpcustomshape.cxx
+++ xmloff/source/draw/ximpcustomshape.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximpcustomshape.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 14:16:16 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:09:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -77,7 +77,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _ENHANCED_CUSTOMSHAPE_TOKEN_HXX
@@ -85,11 +85,11 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLTKMAP_HXX
-#include "xmltkmap.hxx"
+#include <xmloff/xmltkmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -97,11 +97,11 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 #ifndef _XEXPTRANSFORM_HXX
