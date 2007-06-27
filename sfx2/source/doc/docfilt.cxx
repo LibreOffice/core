93e5e47b6f28bb64a54daedaf0eaae2d4e300feb
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 22:19:42 +0000

    INTEGRATION: CWS vgbugs07 (1.21.218); FILE MERGED 2007/06/04 13:34:51 vg 1.21.218.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- sfx2/source/doc/docfilt.cxx
+++ sfx2/source/doc/docfilt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docfilt.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:39:34 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 23:19:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,11 +58,11 @@
 #include <com/sun/star/container/XNameAccess.hpp>
 #endif
 
-#include "docfac.hxx"
-#include "docfilt.hxx"
+#include <sfx2/docfac.hxx>
+#include <sfx2/docfilt.hxx>
 #include "fltfnc.hxx"
-#include "sfxuno.hxx"
-#include "objsh.hxx"
+#include <sfx2/sfxuno.hxx>
+#include <sfx2/objsh.hxx>
 
 using namespace ::com::sun::star;
 
