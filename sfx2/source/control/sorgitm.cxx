f49d44d3561206273291f3af3252ad80575a1b2c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 22:07:56 +0000

    INTEGRATION: CWS vgbugs07 (1.6.218); FILE MERGED 2007/06/04 13:34:45 vg 1.6.218.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- sfx2/source/control/sorgitm.cxx
+++ sfx2/source/control/sorgitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sorgitm.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:29:05 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 23:07:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sfx2.hxx"
 
-#include "sfxsids.hrc"
+#include <sfx2/sfxsids.hrc>
 #include "sorgitm.hxx"
 // STATIC DATA -----------------------------------------------------------
 
