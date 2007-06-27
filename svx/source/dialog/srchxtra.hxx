1c468c84da2037b92fdc53a52f2e426a3b37e8c2
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:40:52 +0000

    INTEGRATION: CWS vgbugs07 (1.3.876); FILE MERGED 2007/06/04 13:26:28 vg 1.3.876.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/srchxtra.hxx
+++ svx/source/dialog/srchxtra.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srchxtra.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:05:25 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:40:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,7 +47,7 @@
 #include <sfx2/tabdlg.hxx>
 #endif
 
-#include "checklbx.hxx"
+#include <svx/checklbx.hxx>
 #include "srchdlg.hxx"
 
 // class SvxSearchFormatDialog -------------------------------------------
