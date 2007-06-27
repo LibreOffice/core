ee415d126308626f5f858b23ffb89ee6ce79a857
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:43:18 +0000

    INTEGRATION: CWS vgbugs07 (1.3.606); FILE MERGED 2007/06/04 13:29:57 vg 1.3.606.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/unx/inc/saltimer.h
+++ vcl/unx/inc/saltimer.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saltimer.h,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:47:35 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:43:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _SV_SALTIMER_H
 
 #ifndef _SV_SALTIMER_HXX
-#include <saltimer.hxx>
+#include <vcl/saltimer.hxx>
 #endif
 
 class X11SalTimer : public SalTimer
