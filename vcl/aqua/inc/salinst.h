5e09fe170d41276b7a8549431bdb5192eff4ab0a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 18:49:15 +0000

    INTEGRATION: CWS vgbugs07 (1.10.604); FILE MERGED 2007/06/04 13:29:25 vg 1.10.604.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/aqua/inc/salinst.h
+++ vcl/aqua/inc/salinst.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salinst.h,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:34:41 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 19:49:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _SV_SALINST_H
 
 #ifndef _SV_SV_H
-#include <sv.h>
+#include <vcl/sv.h>
 #endif
 #ifndef _VOS_MUTEX_HXX
 #include <vos/mutex.hxx>
