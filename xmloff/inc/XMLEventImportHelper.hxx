4bba9629e492df8b11083d9fda016538843b12c1
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:39:33 +0000

    INTEGRATION: CWS vgbugs07 (1.4.276); FILE MERGED 2007/06/04 13:23:17 vg 1.4.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/inc/XMLEventImportHelper.hxx
+++ xmloff/inc/XMLEventImportHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLEventImportHelper.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:48:48 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:39:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLEVENT_HXX
-#include "xmlevent.hxx"
+#include <xmloff/xmlevent.hxx>
 #endif
 
 #include <map>
