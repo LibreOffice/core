8fbedf875b989433dbc7bdbe02bd0e1e0f76bc4a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:49:31 +0000

    INTEGRATION: CWS vgbugs07 (1.13.124); FILE MERGED 2007/06/04 13:23:33 vg 1.13.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/xmltabe.cxx
+++ xmloff/source/style/xmltabe.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltabe.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:01:08 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:49:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,19 +50,19 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_XMLEXP_HXX
-#include "xmlexp.hxx"
+#include <xmloff/xmlexp.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLTABE_HXX
