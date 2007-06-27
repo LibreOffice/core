86703c00e91f5e55c9f2a6ad07477052b3798084
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:44:12 +0000

    INTEGRATION: CWS sdblogging (1.14.114); FILE MERGED 2007/04/05 07:14:47 fs 1.14.114.1: #i76119# Logging.xcs

--- officecfg/registry/schema/org/openoffice/Office/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: kz $ $Date: 2007-06-20 14:00:28 $
+#   last change: $Author: hr $ $Date: 2007-06-27 14:44:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -61,6 +61,7 @@ XCSFILES= \
     Jobs.xcs \
     Labels.xcs \
     Linguistic.xcs \
+    Logging.xcs \
     Math.xcs \
     OptionsDialog.xcs \
     ProtocolHandler.xcs \
