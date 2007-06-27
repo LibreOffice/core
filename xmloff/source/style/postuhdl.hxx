9e62fcbb97c1234c3c5e5a30b14a9de183f3c697
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:46:02 +0000

    INTEGRATION: CWS vgbugs07 (1.2.276); FILE MERGED 2007/06/04 13:23:32 vg 1.2.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/postuhdl.hxx
+++ xmloff/source/style/postuhdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: postuhdl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:46:37 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:42:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_POSTURETYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
--- xmloff/source/style/weighhdl.hxx
+++ xmloff/source/style/weighhdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: weighhdl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:50:50 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:46:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_FONTWEIGHTTYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
