0a4b627ad6dc834491f18d2b742a97927ccb5337
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:35:44 +0000

    INTEGRATION: CWS vgbugs07 (1.19.218); FILE MERGED 2007/06/04 13:27:09 vg 1.19.218.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/msfilter/msoleexp.cxx
+++ svx/source/msfilter/msoleexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: msoleexp.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: ihi $ $Date: 2006-12-19 13:59:44 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:35:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -103,7 +103,7 @@
 #include <svtools/embedhlp.hxx>
 
 #ifndef _MSDFFIMP_HXX
-#include "msdffimp.hxx" // extern sichtbare Header-Datei
+#include <svx/msdffimp.hxx> // extern sichtbare Header-Datei
 #endif
 
 #include "msoleexp.hxx"
