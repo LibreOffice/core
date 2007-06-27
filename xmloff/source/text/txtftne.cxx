4afb6d0ed53d5c624fb28b4ad6bc3f64408b7104
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:15:16 +0000

    INTEGRATION: CWS vgbugs07 (1.20.86); FILE MERGED 2007/06/06 11:42:07 vg 1.20.86.2: RESYNC: (1.20-1.21); FILE MERGED 2007/06/04 13:23:41 vg 1.20.86.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/txtftne.cxx
+++ xmloff/source/text/txtftne.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtftne.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-25 13:19:57 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:15:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -97,37 +97,37 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLNUMFE_HXX
-#include "xmlnumfe.hxx"
+#include <xmloff/xmlnumfe.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLEXP_HXX
-#include "xmlexp.hxx"
+#include <xmloff/xmlexp.hxx>
 #endif
 #ifndef _XMLOFF_FAMILIES_HXX_
-#include "families.hxx"
+#include <xmloff/families.hxx>
 #endif
 #ifndef _XMLOFF_XMLNUME_HXX
-#include "xmlnume.hxx"
+#include <xmloff/xmlnume.hxx>
 #endif
 #ifndef _XMLOFF_XMLTEXTCHARSTYLENAMESELEMENTEXPORT_HXX
 #include "XMLTextCharStyleNamesElementExport.hxx"
 #endif
 #ifndef _XMLOFF_XMLEVENTEXPORT_HXX
-#include "XMLEventExport.hxx"
+#include <xmloff/XMLEventExport.hxx>
 #endif
 #ifndef _XMLOFF_TXTPARAE_HXX
-#include "txtparae.hxx"
+#include <xmloff/txtparae.hxx>
 #endif
 
 using namespace ::rtl;
