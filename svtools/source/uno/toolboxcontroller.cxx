9a7509a6b817b7e5b267d1d02e63bf1da620b31a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:02:33 +0000

    INTEGRATION: CWS vgbugs07 (1.11.200); FILE MERGED 2007/06/04 13:31:47 vg 1.11.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/uno/toolboxcontroller.cxx
+++ svtools/source/uno/toolboxcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toolboxcontroller.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:30:13 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:02:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_svtools.hxx"
 
 #ifndef _SVTOOLS_TOOLBOXCONTROLLER_HXX
-#include <toolboxcontroller.hxx>
+#include <svtools/toolboxcontroller.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
@@ -73,7 +73,7 @@
 #endif
 
 #ifndef INCLUDED_SVTOOLS_MISCOPT_HXX
-#include <miscopt.hxx>
+#include <svtools/miscopt.hxx>
 #endif
 
 #ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
