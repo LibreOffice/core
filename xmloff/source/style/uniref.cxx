49fecbdee907297751b0b7c34a482d1d0ec0ae7b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:45:35 +0000

    INTEGRATION: CWS vgbugs07 (1.3.124); FILE MERGED 2007/06/04 13:23:32 vg 1.3.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/uniref.cxx
+++ xmloff/source/style/uniref.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uniref.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:57:57 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:45:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_xmloff.hxx"
 
 #ifndef _UNIVERSALL_REFERENCE_HXX
-#include "uniref.hxx"
+#include <xmloff/uniref.hxx>
 #endif
 
 UniRefBase::~UniRefBase()
