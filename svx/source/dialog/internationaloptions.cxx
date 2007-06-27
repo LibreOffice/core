33d72e2a164b7982364e708c1868ab4abdf2e4ad
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:17:17 +0000

    INTEGRATION: CWS vgbugs07 (1.6.50); FILE MERGED 2007/06/04 13:26:20 vg 1.6.50.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/internationaloptions.cxx
+++ svx/source/dialog/internationaloptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: internationaloptions.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:35:16 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:17:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,9 +51,9 @@
 #include <svtools/eitem.hxx>
 #endif
 
-#include "dialogs.hrc"
+#include <svx/dialogs.hrc>
 #include "helpid.hrc"
-#include "dialmgr.hxx"
+#include <svx/dialmgr.hxx>
 
 namespace offapp
 {
