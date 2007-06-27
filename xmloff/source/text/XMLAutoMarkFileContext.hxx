d1500b053be65cb63dba14d5539ccbb015135471
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:55:01 +0000

    INTEGRATION: CWS vgbugs07 (1.2.274); FILE MERGED 2007/06/04 13:23:35 vg 1.2.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLAutoMarkFileContext.hxx
+++ xmloff/source/text/XMLAutoMarkFileContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLAutoMarkFileContext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:56:23 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:50:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_XMLAUTOMARKFILECONTEXT_HXX_
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 
