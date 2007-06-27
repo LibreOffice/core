1b5a0feeff570167b0e1173b82f7d624788541c7
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:47:23 +0000

    INTEGRATION: CWS vgbugs07 (1.4.350); FILE MERGED 2007/06/04 13:27:16 vg 1.4.350.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/sdr/contact/viewcontactofsdrobjcustomshape.cxx
+++ svx/source/sdr/contact/viewcontactofsdrobjcustomshape.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontactofsdrobjcustomshape.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:36:56 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:46:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _SVDOASHP_HXX
-#include <svdoashp.hxx>
+#include <svx/svdoashp.hxx>
 #endif
 
 #ifndef _SDR_CONTACT_DISPLAYINFO_HXX
--- svx/source/sdr/contact/viewcontactofvirtobj.cxx
+++ svx/source/sdr/contact/viewcontactofvirtobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontactofvirtobj.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:37:38 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:47:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _SVDOVIRT_HXX
-#include <svdovirt.hxx>
+#include <svx/svdovirt.hxx>
 #endif
 
 #ifndef _SDR_CONTACT_DISPLAYINFO_HXX
