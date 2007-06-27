acffefd25ed944827bec2b0a379d39ef407753c9
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:07:41 +0000

    INTEGRATION: CWS vgbugs07 (1.3.90); FILE MERGED 2007/06/04 13:32:23 vg 1.3.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- tools/bootstrp/sspretty.cxx
+++ tools/bootstrp/sspretty.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sspretty.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:49:49 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:07:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,8 +35,8 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_tools.hxx"
-#include "iparser.hxx"
-#include "geninfo.hxx"
+#include <tools/iparser.hxx>
+#include <tools/geninfo.hxx>
 #include <stdio.h>
 
 
