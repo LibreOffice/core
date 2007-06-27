1099383dfa14ebd03b6693ea3b30cf131f992a66
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:23:52 +0000

    INTEGRATION: CWS vgbugs07 (1.5.240); FILE MERGED 2007/06/04 13:29:38 vg 1.5.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/gdi/salnativewidgets-none.cxx
+++ vcl/source/gdi/salnativewidgets-none.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salnativewidgets-none.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:11:27 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:23:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_vcl.hxx"
 
-#include <salgdi.hxx>
+#include <vcl/salgdi.hxx>
 
 using namespace rtl;
 
