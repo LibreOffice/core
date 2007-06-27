6999f004d72fda111644ac5b8efaaebafabb1f91
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:25:42 +0000

    INTEGRATION: CWS vgbugs07 (1.2.206); FILE MERGED 2007/06/04 13:22:17 vg 1.2.206.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- basic/source/runtime/ddectrl.hxx
+++ basic/source/runtime/ddectrl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ddectrl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:38:20 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:25:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <tools/link.hxx>
 #endif
 #ifndef _SBERRORS_HXX
-#include "sberrors.hxx"
+#include <basic/sberrors.hxx>
 #endif
 #ifndef _STRING_HXX //autogen
 #include <tools/string.hxx>
