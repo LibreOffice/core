feda085508b3cdca6d80bd48b7c2a5e355323be8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 12:22:03 +0000

    INTEGRATION: CWS smarttags3 (1.17.676); FILE MERGED 2007/05/25 11:15:56 fme 1.17.676.2: RESYNC: (1.17-1.18); FILE MERGED 2007/03/05 13:56:12 fme 1.17.676.1: #i75130# New API and implementation of smart tags

--- sw/source/core/unocore/makefile.mk
+++ sw/source/core/unocore/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.18 $
+#   $Revision: 1.19 $
 #
-#   last change: $Author: vg $ $Date: 2007-05-22 16:33:36 $
+#   last change: $Author: hr $ $Date: 2007-06-27 13:22:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -72,6 +72,7 @@ EXCEPTIONSFILES=	\
     $(SLO)$/unosrch.obj\
     $(SLO)$/unostyle.obj\
     $(SLO)$/unotbl.obj \
+    $(SLO)$/unotextmarkup.obj\
     $(SLO)$/TextCursorHelper.obj \
     $(SLO)$/unotext.obj
 
@@ -110,10 +111,11 @@ SLOFILES =	\
     $(SLO)$/unoport.obj\
     $(SLO)$/unoredline.obj\
     $(SLO)$/unoredlines.obj\
-    $(SLO)$/unosrch.obj\
+        $(SLO)$/unosrch.obj\
     $(SLO)$/unostyle.obj\
     $(SLO)$/unotbl.obj \
-    $(SLO)$/TextCursorHelper.obj \
+        $(SLO)$/unotextmarkup.obj\
+        $(SLO)$/TextCursorHelper.obj \
     $(SLO)$/unotext.obj\
     $(SLO)$/unoclbck.obj\
     $(SLO)$/unomap.obj\
