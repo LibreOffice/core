fea8490b4e96a2679454c5f754ab3036ea3869ce
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:21:12 +0000

    INTEGRATION: CWS vgbugs07 (1.6.124); FILE MERGED 2007/06/04 13:23:44 vg 1.6.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/transform/MetaTContext.cxx
+++ xmloff/source/transform/MetaTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MetaTContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:25:31 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:21:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,10 +49,10 @@
 #include <com/sun/star/xml/sax/XAttributeList.hpp>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
