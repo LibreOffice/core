778e628cf1d8aa12b27a2dce02be92ed0ca6f088
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:01:16 +0000

    INTEGRATION: CWS vgbugs07 (1.5.240); FILE MERGED 2007/06/04 13:29:30 vg 1.5.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/app/idlemgr.cxx
+++ vcl/source/app/idlemgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idlemgr.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:48:16 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:01:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #include <idlemgr.hxx>
 #endif
 #ifndef _SV_SVAPP_HXX
-#include <svapp.hxx>
+#include <vcl/svapp.hxx>
 #endif
 
 // =======================================================================
