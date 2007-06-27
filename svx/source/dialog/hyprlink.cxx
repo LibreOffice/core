06d1b51b473eda5ca53da26c2766fd6a235260d5
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:13:11 +0000

    INTEGRATION: CWS vgbugs07 (1.12.50); FILE MERGED 2007/06/04 13:26:19 vg 1.12.50.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/hyprlink.cxx
+++ svx/source/dialog/hyprlink.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hyprlink.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:34:29 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:13:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,9 +79,9 @@
 #endif
 
 #include "hlnkitem.hxx"
-#include "dialogs.hrc"
+#include <svx/dialogs.hrc>
 #include "hyprlink.hrc"
-#include "dialmgr.hxx"
+#include <svx/dialmgr.hxx>
 #include "hyprlink.hxx"
 #include "hyprdlg.hxx"
 
