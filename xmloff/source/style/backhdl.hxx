2f322d06fe65f2e1b4fa7549ed81e6959a67eea6
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:35:18 +0000

    INTEGRATION: CWS vgbugs07 (1.3.156); FILE MERGED 2007/06/04 13:23:30 vg 1.3.156.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/backhdl.hxx
+++ xmloff/source/style/backhdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: backhdl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:30:37 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:35:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_BRUSHTYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_STYLE_GRAPHICLOCATION_HPP_
