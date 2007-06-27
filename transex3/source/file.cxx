68dda5d195a7e187ce865fdb7375cf4c8d1a8f01
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:29:25 +0000

    INTEGRATION: CWS vgbugs07 (1.4.40); FILE MERGED 2007/06/04 13:33:11 vg 1.4.40.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- transex3/source/file.cxx
+++ transex3/source/file.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: file.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 08:17:16 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:29:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_transex3.hxx"
-#include "file.hxx"
+#include <transex3/file.hxx>
 
 namespace transex
 {
