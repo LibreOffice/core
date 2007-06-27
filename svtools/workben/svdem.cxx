dcfc71e322a9cd4c46b3fa72f662e8d2f1fc303f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:04:16 +0000

    INTEGRATION: CWS vgbugs07 (1.10.68); FILE MERGED 2007/06/04 13:31:48 vg 1.10.68.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/workben/svdem.cxx
+++ svtools/workben/svdem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdem.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-07 13:08:11 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:04:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,12 +59,12 @@
 #include <ctrltool.hxx>
 #include <ctrlbox.hxx>
 #include <tabbar.hxx>
-#include <valueset.hxx>
-#include <headbar.hxx>
+#include <svtools/valueset.hxx>
+#include <svtools/headbar.hxx>
 #include <prgsbar.hxx>
 #include <calendar.hxx>
-#include <prnsetup.hxx>
-#include <printdlg.hxx>
+#include <svtools/prnsetup.hxx>
+#include <svtools/printdlg.hxx>
 
 using namespace ::com::sun::star;
 
