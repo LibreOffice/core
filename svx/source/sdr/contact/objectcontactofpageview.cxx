44168441105e5278b99c6412b7120ffefebf5712
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:44:27 +0000

    INTEGRATION: CWS vgbugs07 (1.16.254); FILE MERGED 2007/06/04 13:27:15 vg 1.16.254.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/sdr/contact/objectcontactofpageview.cxx
+++ svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: objectcontactofpageview.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-26 12:05:22 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:44:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,11 +44,11 @@
 #endif
 
 #ifndef _SVDPAGV_HXX
-#include <svdpagv.hxx>
+#include <svx/svdpagv.hxx>
 #endif
 
 #ifndef _SVDPAGE_HXX
-#include <svdpage.hxx>
+#include <svx/svdpage.hxx>
 #endif
 
 #ifndef _SDR_CONTACT_DISPLAYINFO_HXX
@@ -60,7 +60,7 @@
 #endif
 
 #ifndef _SVDVIEW_HXX
-#include <svdview.hxx>
+#include <svx/svdview.hxx>
 #endif
 
 #ifndef _SDR_CONTACT_VIEWCONTACT_HXX
@@ -76,7 +76,7 @@
 #endif
 
 #ifndef _SDRPAGEWINDOW_HXX
-#include <sdrpagewindow.hxx>
+#include <svx/sdrpagewindow.hxx>
 #endif
 
 #ifndef _SDRPAINTWINDOW_HXX
