ae3b75781d89dd0989662c0ef6483d0a182d5599
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:37:09 +0000

    INTEGRATION: CWS vgbugs07 (1.3.878); FILE MERGED 2007/06/04 13:26:07 vg 1.3.878.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/accessibility/AccessibleEmptyEditSource.hxx
+++ svx/source/accessibility/AccessibleEmptyEditSource.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccessibleEmptyEditSource.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:14:53 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:37:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #endif
 
 #include <memory>
-#include "unoedsrc.hxx"
+#include <svx/unoedsrc.hxx>
 
 class SdrObject;
 class SdrView;
