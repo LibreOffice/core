3414a736fd14ef6cb8baf841172c6f93e286a110
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:32:32 +0000

    INTEGRATION: CWS impress122 (1.52.56); FILE MERGED 2007/06/14 09:57:18 cl 1.52.56.1: #i24969# moved slide transition and sound to smil timing root node

--- xmloff/source/draw/sdxmlimp.cxx
+++ xmloff/source/draw/sdxmlimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdxmlimp.cxx,v $
  *
- *  $Revision: 1.53 $
+ *  $Revision: 1.54 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:05:34 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:32:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -203,6 +203,7 @@ static __FAR_DATA SvXMLTokenMapEntry aDrawPageAttrTokenMap[] =
     { XML_NAMESPACE_DRAW,           XML_MASTER_PAGE_NAME,               XML_TOK_DRAWPAGE_MASTER_PAGE_NAME   },
     { XML_NAMESPACE_PRESENTATION,   XML_PRESENTATION_PAGE_LAYOUT_NAME,  XML_TOK_DRAWPAGE_PAGE_LAYOUT_NAME   },
     { XML_NAMESPACE_DRAW,           XML_ID,                             XML_TOK_DRAWPAGE_ID                 },
+    { XML_NAMESPACE_XML,            XML_ID,                             XML_TOK_DRAWPAGE_ID                 },
     { XML_NAMESPACE_XLINK,          XML_HREF,                           XML_TOK_DRAWPAGE_HREF               },
     { XML_NAMESPACE_PRESENTATION,   XML_USE_HEADER_NAME,                XML_TOK_DRAWPAGE_USE_HEADER_NAME    },
     { XML_NAMESPACE_PRESENTATION,   XML_USE_FOOTER_NAME,                XML_TOK_DRAWPAGE_USE_FOOTER_NAME    },
