bce1ef94136103926bb5033037eb6a75d89c1fa1
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:34:16 +0000

    INTEGRATION: CWS vgbugs07 (1.8.50); FILE MERGED 2007/06/04 13:26:26 vg 1.8.50.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/pastedlg.cxx
+++ svx/source/dialog/pastedlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pastedlg.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:41:29 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:34:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@
 #include <svtools/sores.hxx>
 #include <vcl/svapp.hxx>
 
-#include "dialmgr.hxx"
+#include <svx/dialmgr.hxx>
 
 SvPasteObjectDialog::SvPasteObjectDialog( Window* pParent )
 
