f35605f4f1f2d60dbd00746a1fbe65961fc2293d
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:17:34 +0000

    INTEGRATION: CWS vgbugs07 (1.5.274); FILE MERGED 2007/06/04 13:23:27 vg 1.5.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/forms/officeforms.hxx
+++ xmloff/source/forms/officeforms.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: officeforms.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:14:59 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:17:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include "formattributes.hxx"
 #endif
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 #ifndef XMLOFF_FORMS_LOGGING_HXX
 #include "logging.hxx"
