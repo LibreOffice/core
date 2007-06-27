ab6b78dfe7c7aa2174ba8fccc8935a24dcfb1b47
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:12:35 +0000

    INTEGRATION: CWS vgbugs07 (1.20.92); FILE MERGED 2007/06/04 13:31:26 vg 1.20.92.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/config/historyoptions.cxx
+++ svtools/source/config/historyoptions.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: historyoptions.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-23 07:12:21 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:12:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 //  includes
 //_________________________________________________________________________________________________________________
 
-#include "historyoptions.hxx"
+#include <svtools/historyoptions.hxx>
 
 #ifndef _UTL_CONFIGMGR_HXX_
 #include <unotools/configmgr.hxx>
