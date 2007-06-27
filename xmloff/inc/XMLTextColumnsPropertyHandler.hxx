821552a3fe10c5db4ec1c07001653f809b707178
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:43:55 +0000

    INTEGRATION: CWS vgbugs07 (1.3.114); FILE MERGED 2007/06/04 13:23:18 vg 1.3.114.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/inc/XMLTextColumnsPropertyHandler.hxx
+++ xmloff/inc/XMLTextColumnsPropertyHandler.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextColumnsPropertyHandler.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 14:37:01 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:43:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_XMLTEXTCOLUMNSPROPERTYHANDLER_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
