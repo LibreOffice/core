670e2417cf5645973759821955e4c293c686b178
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:59:58 +0000

    INTEGRATION: CWS sdblogging (1.1.2); FILE ADDED 2007/04/05 07:35:08 fs 1.1.2.1: #i76119# initial implementation of the new logging API

---
+++ extensions/source/logging/makefile.mk
@@ -0,0 +1,83 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: hr $ $Date: 2007-06-27 14:59:58 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+PRJNAME=extensions
+TARGET=log
+
+# --- Settings ----------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files -------------------------------------
+
+# ... object files ............................
+EXCEPTIONSFILES= \
+        $(SLO)$/log_services.obj        \
+        $(SLO)$/logger.obj              \
+        $(SLO)$/filehandler.obj         \
+        $(SLO)$/plaintextformatter.obj  \
+        $(SLO)$/consolehandler.obj      \
+        $(SLO)$/loghandler.obj          \
+        $(SLO)$/loggerconfig.obj        \
+
+SLOFILES= \
+        $(EXCEPTIONSFILES) \
+        $(SLO)$/log_module.obj          \
+        $(SLO)$/logrecord.obj           \
+
+# --- library -----------------------------------
+
+LIB1TARGET=$(SLB)$/$(TARGET)_t.lib
+LIB1FILES=\
+        $(SLB)$/$(TARGET).lib
+
+SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+
+SHL1STDLIBS= \
+        $(COMPHELPERLIB) \
+        $(CPPUHELPERLIB) \
+        $(CPPULIB) \
+        $(SALLIB)
+
+SHL1LIBS=$(LIB1TARGET)
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+SHL1VERSIONMAP=$(TARGET).map
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
+
