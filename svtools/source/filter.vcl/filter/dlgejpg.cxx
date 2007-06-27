fc53667215e30aa7cb1e1a3a469773060612530b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:33:36 +0000

    INTEGRATION: CWS vgbugs07 (1.8.36); FILE MERGED 2007/06/04 13:31:36 vg 1.8.36.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/filter.vcl/filter/dlgejpg.cxx
+++ svtools/source/filter.vcl/filter/dlgejpg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgejpg.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 09:44:13 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:33:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 #include "dlgejpg.hxx"
 #include "dlgejpg.hrc"
 #include "strings.hrc"
-#include <FilterConfigItem.hxx>
+#include <svtools/FilterConfigItem.hxx>
 
 #define KEY_QUALITY     "Quality"
 #define KEY_GRAYSCALES  "ColorMode"
