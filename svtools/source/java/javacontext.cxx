04a8ea91d907a8f8f84b325d9fe5f9157199b33a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:47:11 +0000

    INTEGRATION: CWS vgbugs07 (1.6.144); FILE MERGED 2007/06/04 13:31:42 vg 1.6.144.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/java/javacontext.cxx
+++ svtools/source/java/javacontext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: javacontext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: jl $ $Date: 2006-12-06 14:40:23 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:47:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,13 +43,13 @@
 #include "com/sun/star/uno/Type.hxx"
 #endif
 #ifndef _SVTOOLS_SVTDATA_HXX
-#include "svtdata.hxx"
+#include <svtools/svtdata.hxx>
 #endif
 #ifndef _SVTOOLS_JAVACONTEXT_HXX_
-#include "javacontext.hxx"
+#include <svtools/javacontext.hxx>
 #endif
 #ifndef _SVTOOLS_JAVAINTERACTION_HXX_
-#include "javainteractionhandler.hxx"
+#include <svtools/javainteractionhandler.hxx>
 #endif
 
 
