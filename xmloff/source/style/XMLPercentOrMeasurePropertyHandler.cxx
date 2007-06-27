e6e2c712c88f6e618eba087a31edabff2eaa981b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:33:52 +0000

    INTEGRATION: CWS vgbugs07 (1.4.90); FILE MERGED 2007/06/04 13:23:30 vg 1.4.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/XMLPercentOrMeasurePropertyHandler.cxx
+++ xmloff/source/style/XMLPercentOrMeasurePropertyHandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLPercentOrMeasurePropertyHandler.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-21 17:36:48 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:33:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 using namespace ::com::sun::star;
