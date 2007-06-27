27332dffabd0ce433785104b04b1686cf5bef1ca
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 18:01:13 +0000

    INTEGRATION: CWS vgbugs07 (1.7.132); FILE MERGED 2007/06/04 13:27:23 vg 1.7.132.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/svdraw/svdfmtf.hxx
+++ svx/source/svdraw/svdfmtf.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdfmtf.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2007-01-25 11:07:11 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 19:01:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef _SVDOBJ_HXX
-#include "svdobj.hxx"
+#include <svx/svdobj.hxx>
 #endif
 
 //************************************************************
