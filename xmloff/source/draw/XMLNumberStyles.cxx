f5dafadf04d9a26dce1c5a23936b5af1cde02506
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:59:42 +0000

    INTEGRATION: CWS vgbugs07 (1.11.124); FILE MERGED 2007/06/04 13:23:23 vg 1.11.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/draw/XMLNumberStyles.cxx
+++ xmloff/source/draw/XMLNumberStyles.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLNumberStyles.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:25:23 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:59:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,15 +53,15 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #include "sdxmlexp_impl.hxx"
