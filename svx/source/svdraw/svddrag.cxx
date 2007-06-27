cc3d4771d32d94b82d2cf96592b2548d522d7115
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:58:48 +0000

    INTEGRATION: CWS vgbugs07 (1.4.352); FILE MERGED 2007/06/04 13:27:22 vg 1.4.352.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/svdraw/svddrag.cxx
+++ svx/source/svdraw/svddrag.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svddrag.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 05:48:06 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:58:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,10 +35,10 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-#include "svdview.hxx"
+#include <svx/svdview.hxx>
 
 #ifndef _SVDDRAG_HXX //autogen
-#include "svddrag.hxx"
+#include <svx/svddrag.hxx>
 #endif
 
 void SdrDragStat::Clear(FASTBOOL bLeaveOne)
