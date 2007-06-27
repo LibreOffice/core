4d95d5c0ec8369419c1b9b1e5807b35c9bc03bb1
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:22:44 +0000

    INTEGRATION: CWS vgbugs07 (1.5.274); FILE MERGED 2007/06/04 13:23:45 vg 1.5.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/transform/PersAttrListTContext.hxx
+++ xmloff/source/transform/PersAttrListTContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PersAttrListTContext.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:51:14 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:22:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PERSATTRLISTTCONTEXT_HXX
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_TRANSFORMERCONTEXT_HXX
 #include "TransformerContext.hxx"
