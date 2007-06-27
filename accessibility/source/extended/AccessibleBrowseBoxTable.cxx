af2da58960a6df404697addc646b9637fae370d4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:31:24 +0000

    INTEGRATION: CWS a11ysep (1.1.2); FILE ADDED 2007/02/28 07:26:16 fs 1.1.2.4: #i10000# 2006/10/06 11:53:48 fs 1.1.2.3: #i10000# 2005/09/28 11:36:28 fs 1.1.2.2: manual resync (files have been moved herein from another location): licence change 2005/03/07 08:29:16 fs 1.1.2.1: #i44293# moved implementations herein from svtools module

---
+++ accessibility/source/extended/AccessibleBrowseBoxTable.cxx
@@ -0,0 +1,290 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: AccessibleBrowseBoxTable.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:31:24 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_accessibility.hxx"
+
+
+#ifndef ACCESSIBILITY_EXT_ACCESSIBLEBROWSEBOXTABLE_HXX
+#include "accessibility/extended/AccessibleBrowseBoxTable.hxx"
+#endif
+#ifndef _SVTOOLS_ACCESSIBLETABLEPROVIDER_HXX
+#include <svtools/accessibletableprovider.hxx>
+#endif
+
+// ============================================================================
+
+using ::rtl::OUString;
+
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Sequence;
+using ::com::sun::star::uno::Any;
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::accessibility;
+using namespace ::svt;
+
+// ============================================================================
+
+namespace accessibility {
+
+// ============================================================================
+
+// Ctor/Dtor/disposing --------------------------------------------------------
+
+DBG_NAME( AccessibleBrowseBoxTable )
+
+AccessibleBrowseBoxTable::AccessibleBrowseBoxTable(
+        const Reference< XAccessible >& rxParent,
+        IAccessibleTableProvider&                      rBrowseBox ) :
+    AccessibleBrowseBoxTableBase( rxParent, rBrowseBox, BBTYPE_TABLE )
+{
+    DBG_CTOR( AccessibleBrowseBoxTable, NULL );
+}
+
+AccessibleBrowseBoxTable::~AccessibleBrowseBoxTable()
+{
+    DBG_DTOR( AccessibleBrowseBoxTable, NULL );
+}
+
+// XAccessibleContext ---------------------------------------------------------
+
+Reference< XAccessible > SAL_CALL
+AccessibleBrowseBoxTable::getAccessibleChild( sal_Int32 nChildIndex )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidIndex( nChildIndex );
+    return mpBrowseBox->CreateAccessibleCell(
+        implGetRow( nChildIndex ), (sal_Int16)implGetColumn( nChildIndex ) );
+}
+
+sal_Int32 SAL_CALL AccessibleBrowseBoxTable::getAccessibleIndexInParent()
+    throw ( uno::RuntimeException )
+{
+    ensureIsAlive();
+    return BBINDEX_TABLE;
+}
+
+// XAccessibleComponent -------------------------------------------------------
+
+Reference< XAccessible > SAL_CALL
+AccessibleBrowseBoxTable::getAccessibleAtPoint( const awt::Point& rPoint )
+    throw ( uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+
+    Reference< XAccessible > xChild;
+    sal_Int32 nRow = 0;
+    sal_uInt16 nColumnPos = 0;
+    if( mpBrowseBox->ConvertPointToCellAddress( nRow, nColumnPos, VCLPoint( rPoint ) ) )
+        xChild = mpBrowseBox->CreateAccessibleCell( nRow, nColumnPos );
+
+    return xChild;
+}
+
+void SAL_CALL AccessibleBrowseBoxTable::grabFocus()
+    throw ( uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    mpBrowseBox->GrabTableFocus();
+}
+
+Any SAL_CALL AccessibleBrowseBoxTable::getAccessibleKeyBinding()
+    throw ( uno::RuntimeException )
+{
+    ensureIsAlive();
+    return Any();   // no special key bindings for data table
+}
+
+// XAccessibleTable -----------------------------------------------------------
+
+OUString SAL_CALL AccessibleBrowseBoxTable::getAccessibleRowDescription( sal_Int32 nRow )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidRow( nRow );
+    return mpBrowseBox->GetRowDescription( nRow );
+}
+
+OUString SAL_CALL AccessibleBrowseBoxTable::getAccessibleColumnDescription( sal_Int32 nColumn )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidColumn( nColumn );
+    return mpBrowseBox->GetColumnDescription( (sal_uInt16)nColumn );
+}
+
+Reference< XAccessibleTable > SAL_CALL AccessibleBrowseBoxTable::getAccessibleRowHeaders()
+    throw ( uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    return implGetHeaderBar( BBINDEX_ROWHEADERBAR );
+}
+
+Reference< XAccessibleTable > SAL_CALL AccessibleBrowseBoxTable::getAccessibleColumnHeaders()
+    throw ( uno::RuntimeException )
+{
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    return implGetHeaderBar( BBINDEX_COLUMNHEADERBAR );
+}
+
+Sequence< sal_Int32 > SAL_CALL AccessibleBrowseBoxTable::getSelectedAccessibleRows()
+    throw ( uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+
+    Sequence< sal_Int32 > aSelSeq;
+    implGetSelectedRows( aSelSeq );
+    return aSelSeq;
+}
+
+Sequence< sal_Int32 > SAL_CALL AccessibleBrowseBoxTable::getSelectedAccessibleColumns()
+    throw ( uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+
+    Sequence< sal_Int32 > aSelSeq;
+    implGetSelectedColumns( aSelSeq );
+    return aSelSeq;
+}
+
+sal_Bool SAL_CALL AccessibleBrowseBoxTable::isAccessibleRowSelected( sal_Int32 nRow )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidRow( nRow );
+    return implIsRowSelected( nRow );
+}
+
+sal_Bool SAL_CALL AccessibleBrowseBoxTable::isAccessibleColumnSelected( sal_Int32 nColumn )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidColumn( nColumn );
+    return implIsColumnSelected( nColumn );
+}
+
+Reference< XAccessible > SAL_CALL AccessibleBrowseBoxTable::getAccessibleCellAt(
+        sal_Int32 nRow, sal_Int32 nColumn )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidAddress( nRow, nColumn );
+    return mpBrowseBox->CreateAccessibleCell( nRow, (sal_Int16)nColumn );
+}
+
+sal_Bool SAL_CALL AccessibleBrowseBoxTable::isAccessibleSelected(
+        sal_Int32 nRow, sal_Int32 nColumn )
+    throw ( lang::IndexOutOfBoundsException, uno::RuntimeException )
+{
+    BBSolarGuard aSolarGuard;
+    ::osl::MutexGuard aGuard( getOslMutex() );
+    ensureIsAlive();
+    ensureIsValidAddress( nRow, nColumn );
+    return implIsRowSelected( nRow ) || implIsColumnSelected( nColumn );
+}
+
+// XServiceInfo ---------------------------------------------------------------
+
+OUString SAL_CALL AccessibleBrowseBoxTable::getImplementationName()
+    throw ( uno::RuntimeException )
+{
+    return OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.comp.svtools.AccessibleBrowseBoxTable" ) );
+}
+
+// internal virtual methods ---------------------------------------------------
+
+Rectangle AccessibleBrowseBoxTable::implGetBoundingBox()
+{
+    return mpBrowseBox->calcTableRect(FALSE);
+}
+
+Rectangle AccessibleBrowseBoxTable::implGetBoundingBoxOnScreen()
+{
+    return mpBrowseBox->calcTableRect();
+}
+
+// internal helper methods ----------------------------------------------------
+
+Reference< XAccessibleTable > AccessibleBrowseBoxTable::implGetHeaderBar(
+        sal_Int32 nChildIndex )
+    throw ( uno::RuntimeException )
+{
+    Reference< XAccessible > xRet;
+    Reference< XAccessibleContext > xContext( mxParent, uno::UNO_QUERY );
+    if( xContext.is() )
+    {
+        try
+        {
+            xRet = xContext->getAccessibleChild( nChildIndex );
+        }
+        catch( lang::IndexOutOfBoundsException& )
+        {
+            DBG_ERROR( "implGetHeaderBar - wrong child index" );
+        }
+        // RuntimeException goes to caller
+    }
+    return Reference< XAccessibleTable >( xRet, uno::UNO_QUERY );
+}
+
+// ============================================================================
+
+} // namespace accessibility
+
+// ============================================================================
+
