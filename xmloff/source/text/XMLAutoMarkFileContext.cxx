495d6c23a71abf80603cf98266b9638eadef1624
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:52:12 +0000

    INTEGRATION: CWS vgbugs07 (1.5.124); FILE MERGED 2007/06/04 13:23:35 vg 1.5.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLAutoMarkFileContext.cxx
+++ xmloff/source/text/XMLAutoMarkFileContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLAutoMarkFileContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:01:34 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:50:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _RTL_USTRING_HXX_
@@ -49,7 +49,7 @@
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -57,7 +57,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
--- xmloff/source/text/XMLCalculationSettingsContext.cxx
+++ xmloff/source/text/XMLCalculationSettingsContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLCalculationSettingsContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:02:38 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:52:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,19 +46,19 @@
 #include <com/sun/star/text/XTextDocument.hpp>
 #endif
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 
