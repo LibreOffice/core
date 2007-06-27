fe3bdf30d37c31e4bcccbf82c9e94fb958eb823c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:13:49 +0000

    INTEGRATION: CWS vgbugs07 (1.5.124); FILE MERGED 2007/06/04 13:23:40 vg 1.5.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/txtdropi.cxx
+++ xmloff/source/text/txtdropi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtdropi.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:17:05 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:13:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,22 +43,22 @@
 #include "txtdropi.hxx"
 #endif
 #ifndef _XMLOFF_XMLTKMAP_HXX
-#include "xmltkmap.hxx"
+#include <xmloff/xmltkmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 using namespace ::rtl;
