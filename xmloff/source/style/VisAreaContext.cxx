f3a532de3a97b738474d11d3f34ff23c4b972736
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:30:02 +0000

    INTEGRATION: CWS vgbugs07 (1.7.124); FILE MERGED 2007/06/04 13:23:30 vg 1.7.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/VisAreaContext.cxx
+++ xmloff/source/style/VisAreaContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VisAreaContext.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:47:27 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:30:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,19 +46,19 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _SV_GEN_HXX
