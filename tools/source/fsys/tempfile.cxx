68f9257baffdd2fdaa38f0d26a6c6c6511a7a9d7
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:11:24 +0000

    INTEGRATION: CWS vgbugs07 (1.10.90); FILE MERGED 2007/06/04 13:32:24 vg 1.10.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- tools/source/fsys/tempfile.cxx
+++ tools/source/fsys/tempfile.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tempfile.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:53:56 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:11:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_tools.hxx"
 
-#include "tempfile.hxx"
+#include <tools/tempfile.hxx>
 #include "comdep.hxx"
 
 #include <rtl/ustring.hxx>
@@ -47,9 +47,9 @@
 #include <rtl/instance.hxx>
 #endif
 #ifndef _TOOLS_TIME_HXX
-#include <time.hxx>
+#include <tools/time.hxx>
 #endif
-#include <debug.hxx>
+#include <tools/debug.hxx>
 #include <stdio.h>
 
 #ifdef UNX
