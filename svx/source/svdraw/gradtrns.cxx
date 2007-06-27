3d7607bde818a0986d24c0d45680af423ef948f7
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 17:56:31 +0000

    INTEGRATION: CWS vgbugs07 (1.5.256); FILE MERGED 2007/06/04 13:27:21 vg 1.5.256.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/svdraw/gradtrns.cxx
+++ svx/source/svdraw/gradtrns.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gradtrns.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 13:37:46 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 18:56:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_svx.hxx"
 
 #include "gradtrns.hxx"
-#include "svdobj.hxx"
+#include <svx/svdobj.hxx>
 
 #ifndef _B3D_B3DCOLOR_HXX
 #include <goodies/b3dcolor.hxx>
