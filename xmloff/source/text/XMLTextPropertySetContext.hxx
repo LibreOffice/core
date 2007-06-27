20262aac973978106241dc042aed73a9f3913017
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:12:06 +0000

    INTEGRATION: CWS vgbugs07 (1.4.156); FILE MERGED 2007/06/04 13:23:40 vg 1.4.156.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLTextPropertySetContext.hxx
+++ xmloff/source/text/XMLTextPropertySetContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextPropertySetContext.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:47:24 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:12:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_XMLTEXTPROPERTYSETCONTEXT_HXX
 
 #ifndef _XMLOFF_XMLPROPERTYSETCONTEXT_HXX
-#include "xmlprcon.hxx"
+#include <xmloff/xmlprcon.hxx>
 #endif
 
 
