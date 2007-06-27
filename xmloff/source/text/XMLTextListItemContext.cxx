281e406e0ed5a9372f3bb3213d7d5e489dfe6936
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:10:08 +0000

    INTEGRATION: CWS vgbugs07 (1.7.124); FILE MERGED 2007/06/04 13:23:39 vg 1.7.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLTextListItemContext.cxx
+++ xmloff/source/text/XMLTextListItemContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextListItemContext.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:14:08 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:10:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,16 +41,16 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 #ifndef _XMLOFF_TXTPARAI_HXX
 #include "txtparai.hxx"
@@ -59,7 +59,7 @@
 #include "XMLTextListBlockContext.hxx"
 #endif
 #ifndef _XMLOFF_TXTIMP_HXX
-#include "txtimp.hxx"
+#include <xmloff/txtimp.hxx>
 #endif
 
 
