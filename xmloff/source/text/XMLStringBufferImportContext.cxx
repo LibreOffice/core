41a56c3048e98db7da0ab30addd7128d9f7b9a31
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:07:01 +0000

    INTEGRATION: CWS vgbugs07 (1.5.124); FILE MERGED 2007/06/04 13:23:38 vg 1.5.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLStringBufferImportContext.cxx
+++ xmloff/source/text/XMLStringBufferImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLStringBufferImportContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:12:04 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:07:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
