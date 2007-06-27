268c1b2a550d7229efddff5e9724c8781f1fdebe
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:47:05 +0000

    INTEGRATION: CWS vgbugs07 (1.4.876); FILE MERGED 2007/06/04 13:26:30 vg 1.4.876.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/textattr.hxx
+++ svx/source/dialog/textattr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textattr.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 22:15:41 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:47:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 // include ---------------------------------------------------------------
 
 #ifndef _SVX_DLG_CTRL_HXX
-#include "dlgctrl.hxx"
+#include <svx/dlgctrl.hxx>
 #endif
 
 #ifndef _GROUP_HXX //autogen
