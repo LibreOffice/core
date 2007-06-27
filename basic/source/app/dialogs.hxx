e75e10a08d442babdf9cb05aae0b8ce0cecb93a4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:14:28 +0000

    INTEGRATION: CWS vgbugs07 (1.13.8); FILE MERGED 2007/06/04 13:22:14 vg 1.13.8.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- basic/source/app/dialogs.hxx
+++ basic/source/app/dialogs.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dialogs.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-11 09:05:36 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:14:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -85,7 +85,7 @@
 class SbxVariable;
 
 #ifndef _BASIC_TTRESHLP_HXX
-#include "ttstrhlp.hxx"
+#include <basic/ttstrhlp.hxx>
 #endif
 
 class AboutDialog : public ModalDialog {
