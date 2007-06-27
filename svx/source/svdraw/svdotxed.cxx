f01c40afd74a4b35288ef2b8468c66ee8391e5bc
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 18:09:02 +0000

    INTEGRATION: CWS vgbugs07 (1.18.256); FILE MERGED 2007/06/04 13:27:28 vg 1.18.256.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/svdraw/svdotxed.cxx
+++ svx/source/svdraw/svdotxed.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdotxed.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 13:47:32 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 19:09:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,13 +36,13 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
 
-#include "svdotext.hxx"
+#include <svx/svdotext.hxx>
 #include "svditext.hxx"
-#include "svdmodel.hxx" // fuer GetMaxObjSize
-#include "svdoutl.hxx"
+#include <svx/svdmodel.hxx> // fuer GetMaxObjSize
+#include <svx/svdoutl.hxx>
 
 #ifndef _OUTLINER_HXX //autogen
-#include "outliner.hxx"
+#include <svx/outliner.hxx>
 #endif
 
 #ifndef _EDITSTAT_HXX //autogen
@@ -54,7 +54,7 @@
 #endif
 
 #ifndef _EEITEM_HXX
-#include "eeitem.hxx"
+#include <svx/eeitem.hxx>
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
