14d1f4b76168dcf33be0d40e0f2c77868b921088
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:03:55 +0000

    INTEGRATION: CWS vgbugs07 (1.17.124); FILE MERGED 2007/06/04 13:23:38 vg 1.17.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLRedlineExport.cxx
+++ xmloff/source/text/XMLRedlineExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLRedlineExport.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:10:26 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:03:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -97,11 +97,11 @@
 #endif
 
 #ifndef _XMLOFF_XMLEXP_HXX
-#include "xmlexp.hxx"
+#include <xmloff/xmlexp.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 
