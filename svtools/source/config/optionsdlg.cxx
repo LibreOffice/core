dc15b46c019df823ad4bb0dbab686f2de2519a51
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:15:32 +0000

    INTEGRATION: CWS vgbugs07 (1.4.200); FILE MERGED 2007/06/04 13:31:27 vg 1.4.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/config/optionsdlg.cxx
+++ svtools/source/config/optionsdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: optionsdlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 14:28:05 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:15:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 
-#include "optionsdlg.hxx"
+#include <svtools/optionsdlg.hxx>
 
 #ifndef _UTL_CONFIGMGR_HXX_
 #include <unotools/configmgr.hxx>
