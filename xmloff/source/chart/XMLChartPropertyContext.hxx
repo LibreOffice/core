97a69f23cbacbc03c40e99b71bbdc62c45a5a1ca
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:50:56 +0000

    INTEGRATION: CWS vgbugs07 (1.4.156); FILE MERGED 2007/06/04 13:23:20 vg 1.4.156.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/chart/XMLChartPropertyContext.hxx
+++ xmloff/source/chart/XMLChartPropertyContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLChartPropertyContext.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 18:02:31 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:50:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 #define _XMLOFF_CHARTPROPERTYCONTEXT_HXX_
 
 #ifndef _XMLOFF_XMLPROPERTYSETCONTEXT_HXX
-#include "xmlprcon.hxx"
+#include <xmloff/xmlprcon.hxx>
 #endif
 
 class XMLChartPropertyContext : public SvXMLPropertySetContext
