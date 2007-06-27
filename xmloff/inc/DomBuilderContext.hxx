8f412e1a629f6b02a15c03ef9be8f9e4df8c0ab4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:36:06 +0000

    INTEGRATION: CWS vgbugs07 (1.3.276); FILE MERGED 2007/06/04 13:23:16 vg 1.3.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/inc/DomBuilderContext.hxx
+++ xmloff/inc/DomBuilderContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DomBuilderContext.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:35:15 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:36:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,11 +42,11 @@
 //
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
-#include "Reference.hxx"
+#include <xmloff/Reference.hxx>
 #endif
 
 
