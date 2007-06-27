bde9c42f628c5f1fbae05f82f00e806761560cff
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:44:12 +0000

    INTEGRATION: CWS vgbugs07 (1.6.878); FILE MERGED 2007/06/04 13:26:09 vg 1.6.878.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/customshapes/EnhancedCustomShapeGeometry.hxx
+++ svx/source/customshapes/EnhancedCustomShapeGeometry.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnhancedCustomShapeGeometry.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 20:27:22 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:44:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <sal/types.h>
 #endif
 #ifndef _MSDFFDEF_HXX
-#include "msdffdef.hxx"
+#include <svx/msdffdef.hxx>
 #endif
 
 struct SvxMSDffVertPair
