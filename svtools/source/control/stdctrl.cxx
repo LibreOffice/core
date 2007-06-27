bd69c96663dde87b70363d320df5e9fbe564592b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:25:46 +0000

    INTEGRATION: CWS vgbugs07 (1.4.36); FILE MERGED 2007/06/04 13:31:29 vg 1.4.36.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/control/stdctrl.cxx
+++ svtools/source/control/stdctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stdctrl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 10:31:43 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:25:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 
-#include <stdctrl.hxx>
+#include <svtools/stdctrl.hxx>
 
 // =======================================================================
 
