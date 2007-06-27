2d366b05165772d086f67867bfc125e8334ff2dd
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:03:36 +0000

    INTEGRATION: CWS vgbugs07 (1.5.182); FILE MERGED 2007/06/04 13:31:47 vg 1.5.182.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/uno/pathservice.cxx
+++ svtools/uno/pathservice.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pathservice.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 15:28:51 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:03:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 
-#include "pathoptions.hxx"
+#include <svtools/pathoptions.hxx>
 
 #ifndef _SAL_TYPES_H_
 #include "sal/types.h"
