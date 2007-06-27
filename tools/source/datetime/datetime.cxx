aa5491213626cde05ae1b6393542c8c9c0276ebb
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:09:05 +0000

    INTEGRATION: CWS vgbugs07 (1.8.90); FILE MERGED 2007/06/04 13:32:24 vg 1.8.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- tools/source/datetime/datetime.cxx
+++ tools/source/datetime/datetime.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datetime.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:51:33 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:09:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_tools.hxx"
 
-#include <datetime.hxx>
+#include <tools/datetime.hxx>
 #include <rtl/math.hxx>
 
 /*************************************************************************
