3088452d06e12c77c08f19d441cb19bb58594703
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 19:13:46 +0000

    INTEGRATION: CWS vgbugs07 (1.3.240); FILE MERGED 2007/06/04 13:29:35 vg 1.3.240.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- vcl/source/gdi/graphictools.cxx
+++ vcl/source/gdi/graphictools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: graphictools.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 12:01:34 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 20:13:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <tools/vcompat.hxx>
 #endif
 
-#include "graphictools.hxx"
+#include <vcl/graphictools.hxx>
 
 static ::rtl::OString polyToString( const Polygon& rPoly )
 {
