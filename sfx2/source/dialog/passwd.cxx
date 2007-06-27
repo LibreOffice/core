2986b9a6bb4d8a15d305e1ae2143840ad705028b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 22:16:02 +0000

    INTEGRATION: CWS vgbugs07 (1.6.32); FILE MERGED 2007/06/04 13:34:49 vg 1.6.32.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- sfx2/source/dialog/passwd.cxx
+++ sfx2/source/dialog/passwd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: passwd.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 10:11:43 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 23:14:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #ifndef GCC
 #endif
 
-#include "passwd.hxx"
+#include <sfx2/passwd.hxx>
 #include "sfxtypes.hxx"
 #include "sfxresid.hxx"
 
--- sfx2/source/dialog/sfxurl.cxx
+++ sfx2/source/dialog/sfxurl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxurl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 10:12:09 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 23:16:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sfx2.hxx"
-#include <filedlghelper.hxx>
+#include <sfx2/filedlghelper.hxx>
 #include <sfxresid.hxx>
 #include <dialog.hrc>
 
