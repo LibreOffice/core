90e2eade755c320e2deae34b312056197d5c5129
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:57:59 +0000

    INTEGRATION: CWS vgbugs07 (1.12.124); FILE MERGED 2007/06/04 13:23:36 vg 1.12.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLFootnoteImportContext.cxx
+++ xmloff/source/text/XMLFootnoteImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLFootnoteImportContext.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:04:30 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:55:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,15 +49,15 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 
 #ifndef _XMLOFF_TEXTIMP_HXX_
-#include "txtimp.hxx"
+#include <xmloff/txtimp.hxx>
 #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -65,7 +65,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLFOOTNOTEBODYIMPORTCONTEXT_HXX
--- xmloff/source/text/XMLIndexMarkExport.cxx
+++ xmloff/source/text/XMLIndexMarkExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLIndexMarkExport.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:06:26 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:57:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@
 #endif
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
@@ -69,11 +69,11 @@
 #endif
 
 #ifndef _XMLOFF_XMLEXP_HXX
-#include "xmlexp.hxx"
+#include <xmloff/xmlexp.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 
