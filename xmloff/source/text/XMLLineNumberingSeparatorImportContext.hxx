82559910bef4bcb2532a92f3f8c72251629d8edf
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:06:45 +0000

    INTEGRATION: CWS vgbugs07 (1.2.274); FILE MERGED 2007/06/04 13:23:38 vg 1.2.274.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLLineNumberingSeparatorImportContext.hxx
+++ xmloff/source/text/XMLLineNumberingSeparatorImportContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLLineNumberingSeparatorImportContext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:14:35 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:03:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_XMLLINENUMBERINGSEPARATORIMPORTCONTEXT_HXX_
 
 #ifndef _XMLOFF_XMLICTXT_HXX
-#include "xmlictxt.hxx"
+#include <xmloff/xmlictxt.hxx>
 #endif
 
 #ifndef _RTL_USTRING_HXX_
