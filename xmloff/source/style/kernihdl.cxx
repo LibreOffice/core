23d246c3a518d22a108a9f09a5d1e7bfdfafc54e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:40:42 +0000

    INTEGRATION: CWS vgbugs07 (1.7.124); FILE MERGED 2007/06/04 13:23:31 vg 1.7.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/kernihdl.cxx
+++ xmloff/source/style/kernihdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: kernihdl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:55:18 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:40:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,11 +51,11 @@
 // --
 
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include <xmltoken.hxx>
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 
 using namespace ::rtl;
