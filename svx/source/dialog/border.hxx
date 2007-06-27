68a20df5c9872b1d45ead0e082d9d617f6fda71a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:49:40 +0000

    INTEGRATION: CWS vgbugs07 (1.6.322); FILE MERGED 2007/06/04 13:26:12 vg 1.6.322.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/border.hxx
+++ svx/source/dialog/border.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: border.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 12:05:46 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:49:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,7 @@
 #include <sfx2/tabdlg.hxx>
 #endif
 #ifndef SVX_FRMSEL_HXX
-#include "frmsel.hxx"
+#include <svx/frmsel.hxx>
 #endif
 // forward ---------------------------------------------------------------
 
