61d25c0dd48524bef71b67d63db3745fc02b41eb
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:07:27 +0000

    INTEGRATION: CWS vgbugs07 (1.6.354); FILE MERGED 2007/06/04 13:26:17 vg 1.6.354.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svx/source/dialog/frmdirlbox.cxx
+++ svx/source/dialog/frmdirlbox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmdirlbox.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 04:20:56 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 17:07:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_svx.hxx"
 
 #ifndef _SVX_FRMDIRLBOX_HXX
-#include "frmdirlbox.hxx"
+#include <svx/frmdirlbox.hxx>
 #endif
 
 namespace svx {
