98a2207b038cbcc462c4dda01113332edcda104e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 14:37:15 +0000

    INTEGRATION: CWS vgbugs07 (1.3.276); FILE MERGED 2007/06/04 13:23:31 vg 1.3.276.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/style/chrhghdl.hxx
+++ xmloff/source/style/chrhghdl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chrhghdl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:39:08 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 15:37:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _XMLOFF_PROPERTYHANDLER_CHARHEIGHTTYPES_HXX
 
 #ifndef _XMLOFF_PROPERTYHANDLERBASE_HXX
-#include <xmlprhdl.hxx>
+#include <xmloff/xmlprhdl.hxx>
 #endif
 
 /**
