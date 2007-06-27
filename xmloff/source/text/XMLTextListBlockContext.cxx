8f27856cdce44466153473ddb2020aa51bea0e86
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 15:09:39 +0000

    INTEGRATION: CWS vgbugs07 (1.8.114); FILE MERGED 2007/06/04 13:23:39 vg 1.8.114.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/text/XMLTextListBlockContext.cxx
+++ xmloff/source/text/XMLTextListBlockContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextListBlockContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 14:53:54 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 16:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,19 +54,19 @@
 #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
-#include "xmlimp.hxx"
+#include <xmloff/xmlimp.hxx>
 #endif
 #ifndef _XMLOFF_XMLNUMI_HXX
-#include "xmlnumi.hxx"
+#include <xmloff/xmlnumi.hxx>
 #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
-#include "nmspmap.hxx"
+#include <xmloff/nmspmap.hxx>
 #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 #ifndef _XMLTEXTLISTITEMCONTEXT_HXX
