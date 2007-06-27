4d0911e1a8c1fa27c6302a639263939a02e390a5
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 13:52:46 +0000

    INTEGRATION: CWS vgbugs07 (1.5.124); FILE MERGED 2007/06/04 13:23:20 vg 1.5.124.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- xmloff/source/chart/XMLSymbolTypePropertyHdl.cxx
+++ xmloff/source/chart/XMLSymbolTypePropertyHdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLSymbolTypePropertyHdl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:17:18 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:52:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 #include "XMLSymbolTypePropertyHdl.hxx"
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _COM_SUN_STAR_CHART_CHARTERRORINDICATORTYPE_HPP_
 #include <com/sun/star/chart/ChartErrorIndicatorType.hpp>
--- xmloff/source/chart/XMLTextOrientationHdl.cxx
+++ xmloff/source/chart/XMLTextOrientationHdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextOrientationHdl.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:17:32 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 14:52:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,10 @@
 #include "XMLTextOrientationHdl.hxx"
 
 #ifndef _XMLOFF_XMLUCONV_HXX
-#include "xmluconv.hxx"
+#include <xmloff/xmluconv.hxx>
 #endif
 #ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
+#include <xmloff/xmltoken.hxx>
 #endif
 
 using namespace ::com::sun::star;
