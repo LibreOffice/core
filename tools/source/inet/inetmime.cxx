db6ec82fdbcb070b9062522863b33510ea2c1447
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:15:07 +0000

    INTEGRATION: CWS vgbugs07 (1.12.90); FILE MERGED 2007/06/04 13:32:26 vg 1.12.90.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- tools/source/inet/inetmime.cxx
+++ tools/source/inet/inetmime.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: inetmime.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 00:57:35 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:15:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,10 +42,10 @@
 #include "rtl/tencinfo.h"
 
 #ifndef _DATETIME_HXX
-#include <datetime.hxx>
+#include <tools/datetime.hxx>
 #endif
 #ifndef TOOLS_INETMIME_HXX
-#include <inetmime.hxx>
+#include <tools/inetmime.hxx>
 #endif
 
 namespace unnamed_tools_inetmime {} using namespace unnamed_tools_inetmime;
