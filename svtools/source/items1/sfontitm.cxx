4a99c06a33fdf4651d2f0984180b9ee7d0697f8c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 20:46:35 +0000

    INTEGRATION: CWS vgbugs07 (1.4.200); FILE MERGED 2007/06/04 13:31:40 vg 1.4.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/source/items1/sfontitm.cxx
+++ svtools/source/items1/sfontitm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfontitm.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:06:47 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 21:45:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,7 @@
 #endif
 
 #ifndef _SFONTITM_HXX
-#include <sfontitm.hxx>
+#include <svtools/sfontitm.hxx>
 #endif
 
 //============================================================================
