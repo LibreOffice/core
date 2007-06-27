f98067d530e0ffdd74fe271e5f4d7e52fd79a9fd
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 21:03:49 +0000

    INTEGRATION: CWS vgbugs07 (1.5.200); FILE MERGED 2007/06/04 13:31:48 vg 1.5.200.1: #i76605# Remove -I .../inc/module hack introduced by hedaburemove01

--- svtools/workben/browser.cxx
+++ svtools/workben/browser.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: browser.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 15:32:30 $
+ *  last change: $Author: hr $ $Date: 2007-06-27 22:03:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@
 #endif
 
 #ifndef _SVX_BRWBOX_HXX //autogen
-#include <brwbox.hxx>
+#include <svtools/brwbox.hxx>
 #endif
 
 #ifndef _SV_GROUP_HXX //autogen
