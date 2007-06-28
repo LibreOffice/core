ce23ae0de7206521392d44b3dac2add1e2db71ce
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-28 15:31:27 +0000

    #i10000#: fix includes

--- accessibility/inc/accessibility/extended/textwindowaccessibility.hxx
+++ accessibility/inc/accessibility/extended/textwindowaccessibility.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textwindowaccessibility.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:21:55 $
+ *  last change: $Author: hr $ $Date: 2007-06-28 16:31:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -121,6 +121,7 @@
 
 #include <memory>
 #include <queue>
+#include <hash_map>
 
 class TextEngine;
 class TextView;
