8258a4d5abdc4166714091fb6fad5805d8773bfc
Rüdiger Timm <rt@openoffice.org>
2007-07-03 10:48:43 +0000

    INTEGRATION: CWS native87 (1.1.10); FILE MERGED 2007/06/28 15:01:30 is 1.1.10.2: #i78444# earlier check for uid file to avoid race condition 2007/04/24 10:09:18 is 1.1.10.1: #i65425# adding header

--- javainstaller2/src/JavaSetup/org/openoffice/setup/InstallData.java
+++ javainstaller2/src/JavaSetup/org/openoffice/setup/InstallData.java
@@ -1,6 +1,42 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: InstallData.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-07-03 11:48:43 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
 package org.openoffice.setup;
 
 import org.openoffice.setup.SetupData.PackageDescription;
+import org.openoffice.setup.Util.Controller;
 import org.openoffice.setup.Util.SystemManager;
 import java.io.File;
 import java.util.HashMap;
@@ -126,6 +162,9 @@ public class InstallData
     private void setSolarisUserInstall() {
         if (( isUserInstallation ) && (osType.equalsIgnoreCase("SunOS"))) {
             isSolarisUserInstallation = true;
+            if ( isInstallationMode ) {
+                Controller.checkForUidFile(this);
+            }
         }
     }
 
