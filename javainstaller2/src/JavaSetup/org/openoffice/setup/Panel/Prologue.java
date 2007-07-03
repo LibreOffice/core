17cbd06d3ccfe8acaf45104195aebe70b9729384
Rüdiger Timm <rt@openoffice.org>
2007-07-03 10:56:54 +0000

    INTEGRATION: CWS native87 (1.2.2); FILE MERGED 2007/06/28 15:02:26 is 1.2.2.2: #i65425# user info string on welcome page 2007/04/24 10:09:25 is 1.2.2.1: #i65425# adding header

--- javainstaller2/src/JavaSetup/org/openoffice/setup/Panel/Prologue.java
+++ javainstaller2/src/JavaSetup/org/openoffice/setup/Panel/Prologue.java
@@ -1,5 +1,41 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: Prologue.java,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-07-03 11:56:54 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
 package org.openoffice.setup.Panel;
 
+import org.openoffice.setup.InstallData;
 import org.openoffice.setup.PanelHelper.PanelLabel;
 import org.openoffice.setup.PanelHelper.PanelTitle;
 import org.openoffice.setup.ResourceManager;
@@ -29,6 +65,14 @@ public class Prologue extends JPanel {
 
         contentPanel.add(label1, BorderLayout.NORTH);
         contentPanel.add(label2, BorderLayout.CENTER);
+
+        InstallData data = InstallData.getInstance();
+
+        if ( data.isUserInstallation() ) {
+            String text3 = ResourceManager.getString("String_Prologue4");
+            PanelLabel label3 = new PanelLabel(text3, true);
+            contentPanel.add(label3, BorderLayout.SOUTH);
+        }
 
         add(contentPanel, BorderLayout.CENTER);
     }
