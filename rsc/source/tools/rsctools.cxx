93bf30c071164e5a57336136a011ea494d67abe0
Rüdiger Timm <rt@openoffice.org>
2007-07-03 13:01:24 +0000

    INTEGRATION: CWS vcl79 (1.15.26); FILE MERGED 2007/06/18 13:13:51 pl 1.15.26.1: #i78468# remove old MAC

--- rsc/source/tools/rsctools.cxx
+++ rsc/source/tools/rsctools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rsctools.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 16:04:21 $
+ *  last change: $Author: rt $ $Date: 2007-07-03 14:01:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@
 #include <rtl/alloc.h>
 #include <rtl/memory.h>
 
-#if defined (WIN) || defined (MAC)
+#if defined (WIN)
 #define ONLY_NEW
 #endif
 
