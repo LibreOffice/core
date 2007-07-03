cea7d51e310c7b4098de27144e4a7c12db5bbe31
Rüdiger Timm <rt@openoffice.org>
2007-07-03 13:03:43 +0000

    INTEGRATION: CWS locales23 (1.3.54); FILE MERGED 2007/06/20 11:12:13 er 1.3.54.1: #i78464# remove MacOS9 specifics

--- i18npool/source/isolang/insys.cxx
+++ i18npool/source/isolang/insys.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: insys.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 09:20:22 $
+ *  last change: $Author: rt $ $Date: 2007-07-03 14:03:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,10 +39,6 @@
 #if defined( WNT )
 
 #include "inwnt.cxx"
-
-#elif defined( MAC )
-
-#include "inmac.cxx"
 
 #elif defined( UNX )
 
