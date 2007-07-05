8f6f79a32bcb8d795a0c545bf03e573dc3d3ca35
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:14:25 +0000

    INTEGRATION: CWS aquavcl01 (1.1.2); FILE ADDED 2006/07/19 10:19:48 pjanik 1.1.2.3: #i10000#: Change the prototype of GetOGLFnc to reflect the changes done in  SRC680_m173. 2006/02/27 21:33:53 fheckl 1.1.2.2: Replacing VCLVIEW with CarbonViewRef 2005/11/27 09:48:17 fheckl 1.1.2.1: Integrating aqua changes for OOo 2.0

---
+++ vcl/aqua/inc/salogl.h
@@ -0,0 +1,118 @@
+/*************************************************************************
+*
+*  OpenOffice.org - a multi-platform office productivity suite
+*
+*  $RCSfile: salogl.h,v $
+*
+*  $Revision: 1.2 $
+*
+*  last change: $Author: rt $ $Date: 2007-07-05 08:14:25 $
+*
+*  The Contents of this file are made available subject to
+*  the terms of GNU Lesser General Public License Version 2.1.
+*
+*
+*    GNU Lesser General Public License Version 2.1
+*    =============================================
+*    Copyright 2005 by Sun Microsystems, Inc.
+*    901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License version 2.1, as published by the Free Software Foundation.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*    MA  02111-1307  USA
+*
+************************************************************************/
+
+#ifndef _SV_SALOGL_H
+#define _SV_SALOGL_H
+
+//#if defined EXPLICITLY_INCLUDE_GLX_H
+//#include <prex.h>
+//#define GL_H
+//#include <GL/glx.h>
+//#include <postx.h>
+//#else
+//typedef void* GLXContext;
+//typedef XID   GLXDrawable;
+//// needed GLX_ constants
+//#define GLX_USE_GL            1
+//#define GLX_DOUBLEBUFFER  5
+//#endif
+
+#ifndef _SV_SALOGL_HXX
+#include <salogl.hxx>
+#endif
+
+#ifndef _OSL_MODULE_H
+#include <osl/module.h>
+#endif
+
+// -------------
+// - SalOpenGL -
+// -------------
+
+class SalGraphics;
+
+class AquaSalOpenGL : public SalOpenGL
+{
+private:
+    static CarbonViewRef        mhOGLLastDC;
+    static ULONG        mnOGLState;
+
+    //  static oslModule        mpGLLib;
+//  static GLXContext       maGLXContext;
+//  static ULONG            mnOGLState;
+//  static Display*     mpDisplay;
+//  static XVisualInfo* mpVisualInfo;
+//  static BOOL         mbHaveGLVisual;
+//
+//  static BOOL         ImplInit();
+//
+//  static GLXContext (*pCreateContext)( Display *, XVisualInfo *, GLXContext, Bool );
+//  static void       (*pDestroyContext)( Display *, GLXContext );
+//  static GLXContext (*pGetCurrentContext)( );
+//  static Bool       (*pMakeCurrent)( Display *, GLXDrawable, GLXContext );
+//  static void     (*pSwapBuffers)( Display*, GLXDrawable );
+//  static int      (*pGetConfig)( Display*, XVisualInfo*, int, int* );
+//  static void     (*pFlush)();
+//
+//  Drawable            maDrawable;
+//
+//  static void* resolveSymbol( const char* pSym );
+public:
+
+        AquaSalOpenGL( SalGraphics* pGraphics );
+    ~AquaSalOpenGL();
+
+//  static void     Release();
+//    static void     ReleaseLib();
+//  static void     MakeVisualWeights(
+//                                      Display *pDisplay,
+//                                      XVisualInfo* pInfos,
+//                                      int* pWeights,
+//                                      int nVisuals
+//                                      );
+
+    // overload all pure virtual methods
+    virtual bool        IsValid();
+    virtual oglFunction GetOGLFnc( const char * );
+    virtual void        OGLEntry( SalGraphics* pGraphics );
+    virtual void        OGLExit( SalGraphics* pGraphics );
+    virtual void        StartScene( SalGraphics* pGraphics );
+    virtual void        StopScene();
+    virtual BOOL Create();
+    virtual void Release();
+
+};
+
+#endif // _SV_SALOGL_H
