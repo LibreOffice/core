104230fa2e849433d9f03e4689a760799ddff51b
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:12:19 +0000

    INTEGRATION: CWS aquavcl01 (1.15.112); FILE MERGED 2007/05/31 07:45:19 pl 1.15.112.23: implement CaptureMouse using overlay window 2007/05/29 15:56:38 pl 1.15.112.22: simplify AquaSalGraphics, paint to window from backbuffer 2007/05/23 14:25:33 pl 1.15.112.21: normal centering on parent 2007/05/14 11:38:48 pl 1.15.112.20: protect against dispatching to dead or dying AquaSalFrames 2007/05/10 17:55:35 pl 1.15.112.19: #i73689# fix writer initial draw problem 2007/03/09 22:49:22 pjanik 1.15.112.18: Extend AquaSalFrame for TSM events. 2006/12/30 19:28:58 pjanik 1.15.112.17: #i72385#: Add Quartz/AQUA specific parts. 2006/12/30 14:15:24 pjanik 1.15.112.16: Remove redundant callback procedure. 2006/11/08 06:53:04 pjanik 1.15.112.15: Add empty methods for shaped system windows. 2006/10/14 13:27:19 pjanik 1.15.112.14: Finish resync. 2006/08/07 18:31:55 pjanik 1.15.112.13: Add support for native menus. 2006/07/31 18:42:51 fheckl 1.15.112.12: Basic fullscreen method (working on main monitor) implemented 2006/04/27 20:47:01 tra 1.15.112.11: *** empty log message *** 2006/03/19 11:53:17 ssa 1.15.112.10: remove old QuickDraw implementation, AquaSalGraphics members now protected, use RGB color space, general code cleanup 2006/03/09 23:55:16 ssa 1.15.112.9: code cleanup 2006/03/09 08:09:06 ssa 1.15.112.8: post timer events to main thread, improve live resizing, use solar mutex 2006/03/08 21:33:46 tra 1.15.112.7: cleanup minor improvements regarding window handling 2006/02/27 21:33:52 fheckl 1.15.112.6: Replacing VCLVIEW with CarbonViewRef 2006/02/19 21:02:32 tra 1.15.112.5: some cleanup 2006/02/18 14:12:07 tra 1.15.112.4: Carbon changes 2006/02/16 21:36:31 fheckl 1.15.112.3: Removing Cocoa code and replacing most of it with Carbon calls 2006/01/31 01:14:15 fheckl 1.15.112.2: *** empty log message *** 2005/11/27 09:48:15 fheckl 1.15.112.1: Integrating aqua changes for OOo 2.0

--- vcl/aqua/inc/salframe.h
+++ vcl/aqua/inc/salframe.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salframe.h,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:48:48 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:12:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,32 +36,198 @@
 #ifndef _SV_SALFRAME_H
 #define _SV_SALFRAME_H
 
+#include <salframe.hxx>
+
+#include <salmenu.h>
+
 #ifndef _SV_SV_H
 #include <vcl/sv.h>
 #endif
 
-#ifndef _SV_VCLWINDOW_H
-#include <VCLWindow.h>
-#endif
 #ifndef _SV_SYSDATA_HXX
 #include <sysdata.hxx>
 #endif
 
+#include <aquavcltypes.h>
+#include <vector>
+#include <utility>
+#include <stdexcept>
+
+class AquaSalGraphics;
+class AquaSalFrame;
+class AquaSalTimer;
+class AquaSalInstance;
+
+typedef struct SalFrame::SalPointerState SalPointerState;
+
 // ----------------
-// - SalFrameData -
+// - AquaSalFrame -
 // ----------------
 
-struct SalFrameData
+class AquaSalFrame : public SalFrame
 {
-    VCLWINDOW               mhWnd;                  // Window handle
-    SalGraphics*            mpGraphics;             // current frame graphics
-    SalFrame*               mpNextFrame;            // pointer to next frame
-    SalFrame*               mpParent;               // pointer to parent frame
+public:
+    CarbonWindowRef         mrWindow;               // Window handle
+    AquaSalGraphics*        mpGraphics;             // current frame graphics
+    AquaSalFrame*           mpParent;               // pointer to parent frame
     void*                   mpInst;                 // instance handle for callback
-    SALFRAMEPROC            mpProc;                 // callback proc
+     SystemEnvData          maSysData;              // system data
     long                    mnWidth;                // client width in pixels
     long                    mnHeight;               // client height in pixels
+    int                     mnMinWidth;             // min. client width in pixels
+    int                     mnMinHeight;            // min. client height in pixels
+    int                     mnMaxWidth;             // max. client width in pixels
+    int                     mnMaxHeight;            // max. client height in pixels
+    Rect                    maFullScreenRect;       // old window size when in FullScreen
+    WindowAttributes        maFullScreenAttr;       // old window attributes when in FullScreen
     BOOL                    mbGraphics;             // is Graphics used?
+    BOOL                    mbFullScreen;           // is Window in FullScreen?
+    AquaSalInstance*        mpSalInstance;
+    bool                    mbShown;
+    bool                    mbInitShow;
+    bool                    mbPositioned;
+    bool                    mbSized;
+
+    ULONG                   mnStyle;
+
+    TSMDocumentID       maTsmDocumentId;
+    SalExtTextInputEvent    maInputEvent;       // preedit text
+public:
+    /** Constructor
+
+        Creates a system window and connects this frame with it.
+
+        @throws std::runtime_error in case window creation fails
+    */
+    AquaSalFrame(SalFrame* pParent, ULONG salFrameStyle, AquaSalInstance* pSalInstance);
+
+    virtual ~AquaSalFrame();
+
+    virtual SalGraphics*        GetGraphics();
+    virtual void                ReleaseGraphics( SalGraphics* pGraphics );
+    virtual BOOL                PostEvent( void* pData );
+    BOOL                        PostTimerEvent( AquaSalTimer *pTimer );
+    virtual void                SetTitle( const XubString& rTitle );
+    virtual void                SetIcon( USHORT nIcon );
+    virtual void                SetMenu( SalMenu* pSalMenu );
+    virtual void                DrawMenuBar();
+    virtual void                Show( BOOL bVisible, BOOL bNoActivate = FALSE );
+    virtual void                Enable( BOOL bEnable );
+    virtual void                SetMinClientSize( long nWidth, long nHeight );
+    virtual void                SetMaxClientSize( long nWidth, long nHeight );
+    virtual void                SetPosSize( long nX, long nY, long nWidth, long nHeight, USHORT nFlags );
+    virtual void                GetClientSize( long& rWidth, long& rHeight );
+    virtual void                GetWorkArea( Rectangle& rRect );
+    virtual SalFrame*           GetParent() const;
+    virtual void                SetWindowState( const SalFrameState* pState );
+    virtual BOOL                GetWindowState( SalFrameState* pState );
+    virtual void                ShowFullScreen( BOOL bFullScreen, sal_Int32 nDisplay );
+    virtual void                StartPresentation( BOOL bStart );
+    virtual void                SetAlwaysOnTop( BOOL bOnTop );
+    virtual void                ToTop( USHORT nFlags );
+    virtual void                SetPointer( PointerStyle ePointerStyle );
+    virtual void                CaptureMouse( BOOL bMouse );
+    virtual void                SetPointerPos( long nX, long nY );
+    virtual void                Flush();
+    virtual void                Sync();
+    virtual void                SetInputContext( SalInputContext* pContext );
+    virtual void                EndExtTextInput( USHORT nFlags );
+    virtual String              GetKeyName( USHORT nKeyCode );
+    virtual String              GetSymbolKeyName( const XubString& rFontName, USHORT nKeyCode );
+    virtual BOOL                MapUnicodeToKeyCode( sal_Unicode aUnicode, LanguageType aLangType, KeyCode& rKeyCode );
+    virtual LanguageType        GetInputLanguage();
+    virtual SalBitmap*          SnapShot();
+    virtual void                UpdateSettings( AllSettings& rSettings );
+    virtual void                Beep( SoundType eSoundType );
+    virtual const SystemEnvData*    GetSystemData() const;
+    virtual SalPointerState     GetPointerState();
+    virtual void                SetParent( SalFrame* pNewParent );
+    virtual bool                SetPluginParent( SystemParentData* pNewParent );
+    virtual void                SetExtendedFrameStyle( SalExtStyle );
+    virtual void                SetBackgroundBitmap( SalBitmap* );
+
+    // shaped system windows
+    // set clip region to none (-> rectangular windows, normal state)
+    virtual void ResetClipRegion();
+    // start setting the clipregion consisting of nRects rectangles
+    virtual void BeginSetClipRegion( ULONG nRects );
+    // add a rectangle to the clip region
+    virtual void UnionClipRegion( long nX, long nY, long nWidth, long nHeight );
+    // done setting up the clipregion
+    virtual void EndSetClipRegion();
+
+    virtual void SetClientSize( long nWidth, long nHeight );
+
+    void UpdateFrameGeometry();
+
+    // trigger painting of the window
+    void SendPaintEvent();
+
+    void ActivateTSM();
+    void DeactivateTSM();
+
+    static bool isAlive( const AquaSalFrame* pFrame )
+    { return GetSalData()->maFrameCheck.find( pFrame ) != GetSalData()->maFrameCheck.end(); }
+
+    static AquaSalFrame* GetCaptureFrame() { return s_pCaptureFrame; }
+
+ private: // methods
+
+    /** Create a new system window.
+        The newly create window will be associated whith this frame.
+
+        @param pParent
+        the parent of the window may be NULL
+
+        @param nSalFrameStyle
+        the style of the new window
+
+        @throws std::runtime_error in case window creation fails
+     */
+    void CreateNewSystemWindow(CarbonWindowRef pParent, ULONG nSalFrameStyle);
+
+    BOOL ImplPostUserEvent( UInt32 eventKind, void *pData );
+
+    /** Install a window event handler
+
+        The window event handler and the corresponding Universal Procedure Pointer (UPP) pointer
+        need to be save during destruction of the frame instance we have to unregister all installed
+        event handlers and dispose the UPP.
+
+        @param upp
+        a universal procedure pointer
+
+        @param nEvents
+        number of events to register
+
+        @param eventSpec
+        the event specification
+
+        @return the status of the registration see Carbon Event Manager reference for details
+     */
+    OSStatus InstallAndRegisterEventHandler(EventHandlerUPP upp, size_t nEvents, const EventTypeSpec* eventSpec);
+    void DeinstallAndUnregisterAllEventHandler();
+
+    /** do things on initial show (like centering on parent or on screen)
+    */
+    void initShow();
+
+ private: // data
+
+    typedef std::pair<EventHandlerUPP, EventHandlerRef> SysWindowEventHandlerData_t;
+    typedef std::vector<SysWindowEventHandlerData_t> SysWindowEventHandlerDataContainer_t;
+    SysWindowEventHandlerDataContainer_t mSysWindowEventHandlerDataContainer;
+
+    // Menu associated with this SalFrame
+    SalMenu *mpMenu;
+
+    static SysWindowEventHandlerData_t     s_aOverlayEvtHandler;
+    static AquaSalFrame*                   s_pCaptureFrame;
+    static CarbonWindowRef                 s_rOverlay; // window handle for overlay (needed in CaptureMouse)
+
+    // make AquaSalFrame non copyable
+    AquaSalFrame( const AquaSalFrame& );
+    AquaSalFrame& operator=(const AquaSalFrame&);
 };
 
 #endif // _SV_SALFRAME_H
