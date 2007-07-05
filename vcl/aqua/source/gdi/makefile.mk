fdc249a0e83dc41831a9c5d88b8d464dc9b28620
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:24:00 +0000

    INTEGRATION: CWS aquavcl01 (1.10.112); FILE MERGED 2007/06/07 14:48:35 hdu 1.10.112.9: remove salfontutils.cxx that has been obsoleted by salatsfontutils.cxx 2007/05/06 18:36:19 tra 1.10.112.8: Rework of parts of the atsui font implementation 2007/04/11 07:51:48 pjanik 1.10.112.7: RESYNC: (1.10-1.11); FILE MERGED 2007/03/08 20:46:44 pjanik 1.10.112.6: Remove unused and old (QuickDraw) salrectangleutils. 2006/08/21 15:21:41 pjanik 1.10.112.5: Rename salgdinative.cxx file to salnativewidgets.cxx, allow enabling of native controls at runtime. 2006/08/18 15:34:38 cl 1.10.112.4: more aqua bitmap rework 2006/08/16 21:27:00 cl 1.10.112.3: fixes some crashes 2006/07/28 18:49:05 ssa 1.10.112.2: initial font support (ATSUI) 2006/02/16 21:36:35 fheckl 1.10.112.1: Removing Cocoa code and replacing most of it with Carbon calls

--- vcl/aqua/source/gdi/makefile.mk
+++ vcl/aqua/source/gdi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 14:17:49 $
+#   last change: $Author: rt $ $Date: 2007-07-05 08:24:00 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,6 +37,8 @@ PRJ=..$/..$/..
 
 PRJNAME=vcl
 TARGET=salgdi
+ENABLE_EXCEPTIONS=TRUE
+
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Settings -----------------------------------------------------
@@ -54,14 +56,13 @@ dummy:
 .ELSE		# "$(GUIBASE)"!="aqua"
 
 SLOFILES=   $(SLO)$/salmathutils.obj      \
-            $(SLO)$/salrectangleutils.obj \
             $(SLO)$/salcolorutils.obj     \
             $(SLO)$/salpixmaputils.obj    \
             $(SLO)$/salgdiutils.obj       \
+            $(SLO)$/salnativewidgets.obj  \
             $(SLO)$/salatsuifontutils.obj \
-            $(SLO)$/salfontutils.obj      \
+            $(SLO)$/salatslayout.obj      \
             $(SLO)$/salgdi.obj            \
-            $(SLO)$/VCLGraphics.obj       \
             $(SLO)$/salvd.obj             \
             $(SLO)$/salprn.obj            \
             $(SLO)$/salbmp.obj            \
