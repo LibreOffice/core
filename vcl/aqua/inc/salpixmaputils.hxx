22697b664f43f86dc49338fe47f1a1750c5a3b0d
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:15:24 +0000

    INTEGRATION: CWS aquavcl01 (1.5.112); FILE MERGED 2007/01/12 21:01:27 mox 1.5.112.1: aqua vcl cleanup. Get rid of obsolete QuickDraw functions, that are not used by the current Quartz 2D code. When Quartz functions are used this kind of color device management code is not needed at all (automatically handled by Quartz).

--- vcl/aqua/inc/salpixmaputils.hxx
+++ vcl/aqua/inc/salpixmaputils.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salpixmaputils.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:49:56 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:15:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,19 +62,7 @@
 
 // ------------------------------------------------------------------
 
-
-PixMapHandle GetNewPixMap ( const Size           &rPixMapSize,
-                            const USHORT          nPixMapBits,
-                            const BitmapPalette  &rBitmapPalette
-                          );
-
-PixMapHandle CopyPixMap ( PixMapHandle  hPixMap );
-
-PixMapHandle GetCGrafPortPixMap ( const Size           &rPixMapSize,
-                                  const USHORT          nPixMapBits,
-                                  const BitmapPalette  &rBitmapPalette,
-                                  const CGrafPtr        pCGraf
-                                );
+// Empty. Do we need this?
 
 // ------------------------------------------------------------------
 
