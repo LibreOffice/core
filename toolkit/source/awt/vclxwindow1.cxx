aaed0385dbbac3b3a73e541d2d28d3f08908c466
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:05:14 +0000

    INTEGRATION: CWS aquavcl01 (1.5.46); FILE MERGED 2006/12/30 19:38:32 pjanik 1.5.46.1: #i72385#: Add Quartz/AQUA specific part.

--- toolkit/source/awt/vclxwindow1.cxx
+++ toolkit/source/awt/vclxwindow1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vclxwindow1.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 12:16:09 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:05:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,6 +72,8 @@ void VCLXWindow::SetSystemParent_Impl( sal_Int64 nHandle )
     aSysParentData.hWnd = (HWND) nHandle;
 #elif defined( OS2 )
     aSysParentData.hWnd = (HWND) nHandle;
+#elif defined( QUARTZ )
+    aSysParentData.rWindow = (WindowRef) nHandle;
 #elif defined( UNX )
     aSysParentData.aWindow = (long)nHandle;
 #endif
