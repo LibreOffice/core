0a0f73512e282f175b9ed0e5c6082010159f9bfe
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:09:39 +0000

    INTEGRATION: CWS aquavcl01 (1.1.2); FILE ADDED 2007/06/26 06:31:46 pjanik 1.1.2.11: #i78712#: Add events for entering and leaving. 2007/06/21 01:37:30 ekato 1.1.2.10: Handle window activate and deactivate events properly. 2007/03/09 22:55:23 pjanik 1.1.2.9: Add TextInput events for TSM. 2007/02/18 21:19:26 pjanik 1.1.2.8: #i73199#: Add Focus events. 2007/01/07 20:02:37 pjanik 1.1.2.7: Add MouseDragged events. 2006/12/13 09:39:28 pjanik 1.1.2.6: Add SALEVENT_MOUSEMOVE event. 2006/09/04 08:31:58 pjanik 1.1.2.5: Add mouse wheel event. 2006/09/04 08:20:36 pjanik 1.1.2.4: Add RawKey event constants. 2006/08/07 18:09:46 pjanik 1.1.2.3: Add native menu events. 2006/08/07 18:04:06 pjanik 1.1.2.2: fix typo 2006/04/27 20:48:10 tra 1.1.2.1: definition of OOo internal events and event type specifiers

---
+++ vcl/aqua/inc/aquavclevents.hxx
@@ -0,0 +1,66 @@
+#ifndef INCLUDED_AQUAVCLEVENTS_HXX
+#define INCLUDED_AQUAVCLEVENTS_HXX
+
+#include <premac.h>
+#include <Carbon/Carbon.h>
+#include <postmac.h>
+
+/* Definition of custom OpenOffice.org events.
+
+   Avoid conflict with Apple defined event class and type
+   definitions by using uppercase letters. Lowercase
+   letter definitions are reserved for Apple!
+ */
+enum {
+  cOOoSalUserEventClass = 'OOUE'
+};
+
+enum {
+  cOOoSalEventUser = 'UEVT',
+  cOOoSalEventTimer = 'EVTT',
+  cOOoSalEventData = 'EVTD',
+  cOOoSalEventParamTypePtr = 'EPPT'
+};
+
+/* Definition of all necessary EventTypeSpec's */
+
+const EventTypeSpec cWindowBoundsChangedEvent = { kEventClassWindow, kEventWindowBoundsChanged };
+const EventTypeSpec cWindowCloseEvent = { kEventClassWindow, kEventWindowClose };
+const EventTypeSpec cOOoSalUserEvent = { cOOoSalUserEventClass, cOOoSalEventUser };
+const EventTypeSpec cOOoSalTimerEvent = { cOOoSalUserEventClass, cOOoSalEventTimer };
+const EventTypeSpec cWindowActivatedEvent[] = { { kEventClassWindow, kEventWindowActivated },
+                                                { kEventClassWindow, kEventWindowDeactivated } };
+const EventTypeSpec cWindowPaintEvent = { kEventClassWindow, kEventWindowPaint };
+const EventTypeSpec cWindowDrawContentEvent = { kEventClassWindow, kEventWindowDrawContent };
+
+const EventTypeSpec cWindowFocusEvent[] = { { kEventClassWindow, kEventWindowFocusAcquired },
+                                            { kEventClassWindow, kEventWindowFocusRelinquish } };
+
+const EventTypeSpec cMouseEnterExitEvent[] = { { kEventClassControl, kEventControlTrackingAreaEntered },
+                                               { kEventClassControl, kEventControlTrackingAreaExited } };
+
+const EventTypeSpec cMouseEvent[] = { { kEventClassMouse, kEventMouseDown },
+                                      { kEventClassMouse, kEventMouseUp },
+                                      { kEventClassMouse, kEventMouseMoved },
+                                      { kEventClassMouse, kEventMouseDragged } };
+const EventTypeSpec cMouseWheelMovedEvent = { kEventClassMouse, kEventMouseWheelMoved };
+const EventTypeSpec cWindowResizeStarted = { kEventClassWindow, kEventWindowResizeStarted };
+const EventTypeSpec cWindowResizeCompleted = { kEventClassWindow, kEventWindowResizeCompleted };
+
+/* Events for native menus */
+const EventTypeSpec cCommandProcessEvent = { kEventClassCommand, kEventCommandProcess };
+const EventTypeSpec cMenuPopulateEvent = { kEventClassMenu, kEventMenuPopulate };
+const EventTypeSpec cMenuClosedEvent = { kEventClassMenu, kEventMenuClosed };
+const EventTypeSpec cMenuTargetItemEvent = { kEventClassMenu, kEventMenuTargetItem };
+
+/* Events for keyboard */
+const EventTypeSpec cKeyboardRawKeyEvents[] = { { kEventClassKeyboard, kEventRawKeyDown},
+                        { kEventClassKeyboard, kEventRawKeyUp},
+                        { kEventClassKeyboard, kEventRawKeyRepeat},
+                        { kEventClassKeyboard, kEventRawKeyModifiersChanged} };
+
+const EventTypeSpec cTextInputEvents[] = { { kEventClassTextInput, kEventTextInputUpdateActiveInputArea},
+                                           { kEventClassTextInput, kEventTextInputUnicodeForKeyEvent},
+                                           { kEventClassTextInput, kEventTextInputOffsetToPos} };
+
+#endif // INCLUDED_AQUAVCLEVENTS_HXX
