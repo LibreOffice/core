bbc90d7cfac9f102035685a501c113fb3e41b69b
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:14:02 +0000

    INTEGRATION: CWS aquavcl01 (1.5.112); FILE MERGED 2005/11/27 09:48:16 fheckl 1.5.112.1: Integrating aqua changes for OOo 2.0

--- vcl/aqua/inc/salobj.h
+++ vcl/aqua/inc/salobj.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salobj.h,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:49:28 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:14:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,16 +44,57 @@
 #include <sysdata.hxx>
 #endif
 
+#include <salobj.hxx>
+
+class AquaSalFrame;
+class AquaSalObject;
+
+
 // -----------------
 // - SalObjectData -
 // -----------------
 
 struct SalObjectData
 {
-    SalFrame*               mpFrame;                // parent frame
-    SalObject*              mpNextObject;           // pointer to next object
+};
+
+class AquaSalObject : public SalObject
+{
+public:
+    AquaSalFrame*               mpFrame;                // parent frame
+    AquaSalObject*              mpNextObject;           // pointer to next object
     void*                   mpInst;                 // instance handle for callback
     SALOBJECTPROC           mpProc;                 // callback proc
+
+
+//  HWND                    mhWnd;                  // Window handle
+//  HWND                    mhWndChild;             // Child Window handle
+//  HWND                    mhLastFocusWnd;         // Child-Window, welches als letztes den Focus hatte
+//  SystemChildData         maSysData;              // SystemEnvData
+//  RGNDATA*                mpClipRgnData;          // ClipRegion-Data
+//  RGNDATA*                mpStdClipRgnData;       // Cache Standard-ClipRegion-Data
+//  RECT*                   mpNextClipRect;         // Naechstes ClipRegion-Rect
+//  BOOL                    mbFirstClipRect;        // Flag for first cliprect to insert
+//  WinSalObject*               mpNextObject;           // pointer to next object
+
+
+    AquaSalObject();
+    virtual ~AquaSalObject();
+
+    virtual void                    ResetClipRegion();
+    virtual USHORT                  GetClipRegionType();
+    virtual void                    BeginSetClipRegion( ULONG nRects );
+    virtual void                    UnionClipRegion( long nX, long nY, long nWidth, long nHeight );
+    virtual void                    EndSetClipRegion();
+    virtual void                    SetPosSize( long nX, long nY, long nWidth, long nHeight );
+    virtual void                    Show( BOOL bVisible );
+    virtual void                    Enable( BOOL nEnable );
+    virtual void                    GrabFocus();
+    virtual void                    SetBackground();
+    virtual void                    SetBackground( SalColor nSalColor );
+    virtual const SystemEnvData*    GetSystemData() const;
+
+    virtual void SetCallback( void* pInst, SALOBJECTPROC pProc );
 };
 
 #endif // _SV_SALOBJ_H
