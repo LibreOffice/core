38c8d19334efca627a9110ca3fa9d9c2f780edca
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:13:44 +0000

    INTEGRATION: CWS aquavcl01 (1.1.2); FILE ADDED 2007/02/17 09:44:40 pjanik 1.1.2.4: minor cleanup 2007/02/17 09:14:12 ericb 1.1.2.3: add metrics to separate text and controls 2007/02/04 19:27:29 ericb 1.1.2.2: misc code changes 2007/02/04 17:19:51 ericb 1.1.2.1: create header for native widgets framework in aqua tree

---
+++ vcl/aqua/inc/salnativewidgets.h
@@ -0,0 +1,48 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: salnativewidgets.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:13:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SV_NATIVEWIDGETS_H
+#define _SV_NATIVEWIDGETS_H
+
+// since 10.4 ... no metrics are returned, and we have to fix the values
+#define BUTTON_WIDTH 16
+#define BUTTON_HEIGHT 17
+
+// for some controls, like spinbuttons + spinboxes, or listboxes
+// we need it to adjust text position beside radio and check buttons
+
+#define TEXT_SEPARATOR 3
+
+#endif  // _SV_NATIVEWIDGETS_H
