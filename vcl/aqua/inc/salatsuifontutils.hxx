53e46d04a1407f974a34b1c30d653ec59ef37400
Rüdiger Timm <rt@openoffice.org>
2007-07-05 07:10:19 +0000

    INTEGRATION: CWS aquavcl01 (1.5.112); FILE MERGED 2007/06/11 14:02:37 hdu 1.5.112.6: simplify SystemFontList header even more 2007/06/11 13:52:08 hdu 1.5.112.5: simplify SystemFontList header 2007/05/15 10:25:07 hdu 1.5.112.4: simplify a little by removing dead code 2007/05/14 11:33:58 hdu 1.5.112.3: unify debug/non-debug of ImplFontDataPtr_t 2007/05/08 08:15:33 hdu 1.5.112.2: fix stl iterator abuse in SystemFontList destructor 2007/05/06 18:36:19 tra 1.5.112.1: Rework of parts of the atsui font implementation

--- vcl/aqua/inc/salatsuifontutils.hxx
+++ vcl/aqua/inc/salatsuifontutils.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salatsuifontutils.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:32:08 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 08:10:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,204 +33,31 @@
  *
  ************************************************************************/
 
-// =======================================================================
-
-// =======================================================================
-
 #ifndef _SV_SALATSUIFONTUTILS_HXX
 #define _SV_SALATSUIFONTUTILS_HXX
 
-#ifndef _LIMITS_H
-    #include <limits.h>
-#endif
+class ImplFontData;
+class ImplDevFontList;
 
-#ifndef _STDDEF_H
-    #include <stddef.h>
-#endif
+#include <vector>
 
-#ifndef _STDIO_H
-    #include <stdio.h>
-#endif
-
-#ifndef _STDLIB_H
-    #include <stdlib.h>
-#endif
-
-#ifndef _STRING_H
-    #include <string.h>
-#endif
-
-#include <premac.h>
-    #include <Carbon/Carbon.h>
-    #include <ApplicationServices/ApplicationServices.h>
-#include <postmac.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// =======================================================================
-
-// =======================================================================
-
-static const short kMacOSCharSize = sizeof(char);
-
-static const short kFontFamilyNameLength = 32;
-static const short kFontStyleNameLength  = 32;
-
-static const long  kFontFamilyNameMemSize  = kFontFamilyNameLength * kMacOSCharSize;
-static const long  kFontStyleNameMemSize   = kFontStyleNameLength  * kMacOSCharSize;
-
-// -----------------------------------------------------------------------
-
-// For simplicity, only this number of font axis variations are considered.
-
-static const short kATSUIMaxVariations  = 32;
-
-// -----------------------------------------------------------------------
-
-// To filter out invisible fonts
-
-static const char kInvalidFontNamePrefixPeriodSign  = '.';
-static const char kInvalidFontNamePrefixPercentSign = '%';
-
-// =======================================================================
-
-// =======================================================================
-
-enum
+/* This class has the responsibility of assembling a list
+   of atsui compatible fonts available on the system and
+   enabling access to that list.
+ */
+class SystemFontList
 {
-    kATSUIDefaultInstance = SHRT_MAX,
-    kATSUINotAnInstance   = -1
+public:
+
+    SystemFontList();
+    ~SystemFontList();
+
+    void    AnnounceFonts( ImplDevFontList& ) const;
+
+private:
+  typedef std::vector<ImplFontData*> ImplFontDataContainer_t;
+  ImplFontDataContainer_t mFontContainer;
 };
-
-// =======================================================================
-
-// =======================================================================
-
-typedef short ATSUIInstanceIndex;
-
-// -----------------------------------------------------------------------
-
-struct FontNameEncodingRecord
-{
-    FontPlatformCode  mnFontPlatformCode;
-    FontScriptCode    mnFontScriptCode;
-    FontLanguageCode  mnFontLanguageCode;
-    ItemCount         mnFontItemCount;
-};
-
-typedef struct FontNameEncodingRecord   FontNameEncodingRecord;
-typedef FontNameEncodingRecord         *FontNameEncodingPtr;
-typedef FontNameEncodingPtr            *FontNameEncodingMatrix;
-
-// -----------------------------------------------------------------------
-
-struct FontNamesRecord
-{
-    ATSUFontID              mnFontID;
-    FontNameEncodingRecord  maFontFamilyNameEncoding;
-    FontNameEncodingRecord  maFontStyleNameEncoding;
-    ATSUIInstanceIndex      mnFontInstanceIndex;
-    ByteOffset              mnFontStyleByteOffset;
-    char                    mpFontName[ 1 ];
-};
-
-typedef struct FontNamesRecord   FontNamesRecord;
-typedef FontNamesRecord         *FontNamesPtr;
-typedef FontNamesPtr            *FontNamesMatrix;
-
-// =======================================================================
-
-// =======================================================================
-
-// Note that the macro that follows evaluates pref twice.
-
-#define ElementMatches( elem, pref )  ((pref) == -1 || (elem) == (pref))
-
-#define EncodingMatchesPrefs( enc, prefs )                                       \
-    ( ElementMatches((enc).mnFontPlatformCode, (prefs).mnFontPlatformCode)   \
-    && ElementMatches((enc).mnFontScriptCode, (prefs).mnFontScriptCode)      \
-    && ElementMatches((enc).mnFontLanguageCode, (prefs).mnFontLanguageCode) )
-
-// -----------------------------------------------------------------------
-
-#define ATSUISetFont( aFontStyle, nFontID )  \
-    ATSUISetIntAttribute( aFontStyle, nFontID, kATSUFontTag )
-
-#define ATSUIGetFont( aFontStyle, rFontID )  \
-    ATSUIGetOneAttribute( aFontStyle, kATSUFontTag, sizeof(ATSUFontID), rFontID )
-
-// -----------------------------------------------------------------------
-
-#define ATSUICountStyleFontVariations( aFontStyle, oActualVariationCount ) \
-    ATSUGetAllFontVariations( aFontStyle, 0, NULL, NULL, oActualVariationCount )
-
-// =======================================================================
-
-// =======================================================================
-
-OSStatus ATSUIDisposeFontNames( const ItemCount  nFontListLength,
-                                FontNamesMatrix  hFontNames
-                              );
-
-OSStatus ATSUIFindBestFontName( ATSUFontID         nFontID,
-                                FontNameCode       nFontNameCode,
-                                FontPlatformCode  *rFontPlatformCode,
-                                FontScriptCode    *rFontScriptCode,
-                                FontLanguageCode  *rFontLanguageCode,
-                                ByteCount          nFontMaxNameLength,
-                                MacOSPtr           rFontName,
-                                ByteCount         *rFontNameLength,
-                                ItemCount         *rFontNameIndex
-                              );
-
-OSStatus ATSUIFONDtoFontID( short            nFONDNumber,
-                            StyleParameter   nFONDStyle,
-                            ATSUFontID      *rFontID,
-                            StyleParameter  *rIntrinsicStyleParameter
-                          );
-
-void ATSUIFPrintFontList( const char       *pFileName,
-                          const char       *pFilePermission,
-                          const ItemCount   nFontItemsCount,
-                          FontNamesMatrix   hFontList
-                        );
-
-FontNamesMatrix ATSUIGetFontNames( ItemCount *rSortedFontListLength );
-
-OSStatus ATSUIGetOneAttribute( ATSUStyle              aFontStyle,
-                               ATSUAttributeTag       nFontAttributeTag,
-                               ByteCount              nFontExpValueSize,
-                               ATSUAttributeValuePtr  rFontAttributeValue
-                             );
-
-OSStatus ATSUIGetStyleFontInstance( ATSUStyle            aFontStyle,
-                                    ATSUFontID          *rFontID,
-                                    ATSUIInstanceIndex  *rFontInstanceIndex
-                                  );
-
-OSStatus ATSUISetIntAttribute( ATSUStyle         aFontStyle,
-                               SInt32            nFontAttributeValue,
-                               ATSUAttributeTag  nFontAttributeTag
-                             );
-
-OSStatus ATSUISetStyleFontInstance( ATSUStyle           aFontStyle,
-                                    ATSUFontID          nFontID,
-                                    ATSUIInstanceIndex  nFontInstanceIndex
-                                  );
-
-void ATSUIPrintFontList( const ItemCount  nFontItemsCount,
-                         FontNamesMatrix  hFontList
-                       );
-
-// =======================================================================
-
-// =======================================================================
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif  // _SV_SALATSUIFONTUTILS_HXX
 
