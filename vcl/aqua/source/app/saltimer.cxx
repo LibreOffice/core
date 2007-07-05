281e94301ed319f7bb1c6f22fe1544b44f61c20b
Rüdiger Timm <rt@openoffice.org>
2007-07-05 09:06:19 +0000

    INTEGRATION: CWS aquavcl01 (1.10.112); FILE MERGED 2007/06/15 17:48:15 pl 1.10.112.10: #i75228# replace timer implementation 2007/05/23 13:21:42 pl 1.10.112.9: more convenient logging 2007/05/14 11:39:42 pl 1.10.112.8: protect against dispatching to dead or dying AquaSalFrames 2006/10/14 13:28:53 pjanik 1.10.112.7: Fix resync problems. 2006/10/13 21:54:43 tra 1.10.112.6: RESYNC: (1.10-1.11); FILE MERGED 2006/04/27 20:47:02 tra 1.10.112.5: *** empty log message *** 2006/03/09 08:03:23 ssa 1.10.112.4: post timer events to main thread, improve live resizing, use solar mutex 2006/03/02 00:31:03 ssa 1.10.112.3: provide AquaSalTimer and solar mutex helper 2006/02/16 21:36:35 fheckl 1.10.112.2: Removing Cocoa code and replacing most of it with Carbon calls 2005/11/27 09:48:34 fheckl 1.10.112.1: Integrating aqua changes for OOo 2.0

--- vcl/aqua/source/app/saltimer.cxx
+++ vcl/aqua/source/app/saltimer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saltimer.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:52:21 $
+ *  last change: $Author: rt $ $Date: 2007-07-05 10:06:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,34 +36,133 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_vcl.hxx"
 
-#ifndef _SV_SALTIMER_HXX
-#include <vcl/saltimer.hxx>
+#ifndef _SV_SALTIMER_H
+#include <vcl/saltimer.h>
 #endif
 #ifndef _SV_SALDATA_HXX
 #include <saldata.hxx>
 #endif
-#ifndef _SV_VCLEVENT_H
-#include <VCLEvent.h>
+#ifndef _SV_SALFRAME_H
+#include <salframe.h>
 #endif
 
 // =======================================================================
 
-void SalTimer::Start( ULONG nMS )
+
+#define USEMAINTHREAD 1
+
+void ImplSalStartTimer ( ULONG nMS, BOOL bMutex)
 {
-    VCLEvent_StartPeriodicEvents( nMS );
-}
+    AquaLog( "ImplSalStartTimer\n");
 
-// -----------------------------------------------------------------------
-
-void SalTimer::Stop()
-{
-    VCLEvent_StopPeriodicEvents();
-}
-
-// -----------------------------------------------------------------------
-
-void SalTimer::SetCallback( SALTIMERPROC pProc )
-{
     SalData* pSalData = GetSalData();
-    pSalData->mpTimerProc = pProc;
+
+    // Store the new timeout
+    pSalData->mnTimerMS = nMS;
+
+    if ( !bMutex )
+        pSalData->mnTimerOrgMS = nMS;
+
+    // Cancel current timer
+    if( pSalData->mbTimerInstalled )
+        RemoveEventLoopTimer( pSalData->mrTimerRef );
+
+    // Install the timer task
+    if( InstallEventLoopTimer( GetMainEventLoop(),
+                               kEventDurationMillisecond * nMS,
+                               kEventDurationForever,
+                               pSalData->mrTimerUPP,
+                               NULL,
+                               &pSalData->mrTimerRef) == noErr )
+    {
+        pSalData->mbTimerInstalled = TRUE;
+    }
+    else
+    {
+        AquaLog( "Could not install timer task!\n");
+        pSalData->mbTimerInstalled = FALSE;
+    }
 }
+
+void AquaSalTimerProc ( EventLoopTimerRef theTimer, void * /* userData */)
+{
+    AquaLog( "...AquaSalTimerProc...\n");
+
+    SalData* pSalData = GetSalData();
+     ImplSVData* pSVData = ImplGetSVData();
+    AquaSalTimer *pSalTimer = (AquaSalTimer*) pSVData->mpSalTimer;
+
+    if( pSalTimer && !pSalData->mbInTimerProc )
+     {
+        #ifdef USEMAINTHREAD
+        // Send event to the main thread
+        if( ! pSalData->maFrames.empty() )
+            pSalData->maFrames.front()->PostTimerEvent( pSalTimer );
+
+        // FIXME?
+        // fire again using current timeout as this is a single shot timer
+        ImplSalStartTimer( pSalData->mnTimerOrgMS, FALSE );
+        #else
+        // call back directly from timer thread
+         if( ImplSalYieldMutexTryToAcquire() )
+         {
+            pSalData->mbInTimerProc = TRUE;
+             pSalTimer->CallCallback();
+            pSalData->mbInTimerProc = FALSE;
+             ImplSalYieldMutexRelease();
+
+             // fire again using current timeout as this is a single shot timer
+            ImplSalStartTimer( pSalData->mnTimerOrgMS, FALSE );
+         }
+         else
+         {
+             // could not acquire solar mutex, so
+             // fire again with a short delay (10ms)
+            AquaLog( "SHOULD NOT HAPPEN! TIMER: solar mutex not free\n");
+            ImplSalStartTimer( 10, TRUE );
+         }
+        #endif
+    }
+}
+
+
+AquaSalTimer::AquaSalTimer( )
+{
+    AquaLog( "AquaSalTimer::AquaSalTimer\n");
+    SalData* pSalData = GetSalData();
+
+    pSalData->mbTimerInstalled = FALSE;
+    pSalData->mnTimerMS = 0;
+    pSalData->mnTimerOrgMS = 0;
+    pSalData->mrTimerUPP = NewEventLoopTimerUPP( AquaSalTimerProc );
+}
+
+AquaSalTimer::~AquaSalTimer()
+{
+    AquaLog( "AquaSalTimer::~AquaSalTimer\n");
+
+    SalData* pSalData = GetSalData();
+    if( pSalData->mbTimerInstalled )
+        RemoveEventLoopTimer( pSalData->mrTimerRef );
+
+    DisposeEventLoopTimerUPP( pSalData->mrTimerUPP );
+}
+
+void AquaSalTimer::Start( ULONG nMS )
+{
+    ImplSalStartTimer(nMS, FALSE);
+}
+
+void AquaSalTimer::Stop()
+{
+    AquaLog( "AquaSalTimer::Stop\n");
+
+    SalData* pSalData = GetSalData();
+    if( pSalData->mbTimerInstalled )
+     {
+        RemoveEventLoopTimer( pSalData->mrTimerRef );
+        pSalData->mbTimerInstalled = FALSE;
+     }
+}
+
+
