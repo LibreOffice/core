c74c8fd43d03f344e15c1d43d21f464effe72082
Rüdiger Timm <rt@openoffice.org>
2007-07-06 06:53:54 +0000

    INTEGRATION: CWS oj14 (1.8.8); FILE MERGED 2007/06/04 17:51:58 oj 1.8.8.3: RESYNC: (1.10-1.11); FILE MERGED 2006/11/07 08:49:53 oj 1.8.8.2: RESYNC: (1.8-1.10); FILE MERGED 2006/01/03 08:07:48 oj 1.8.8.1: export controller class

--- dbaccess/source/core/dataaccess/documentcontainer.hxx
+++ dbaccess/source/core/dataaccess/documentcontainer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: documentcontainer.hxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-09 13:24:05 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 07:53:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,9 @@
 #endif
 #ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
 #include <comphelper/proparrhlp.hxx>
+#endif
+#ifndef _DBASHARED_APITOOLS_HXX_
+#include "apitools.hxx"
 #endif
 
 //........................................................................
