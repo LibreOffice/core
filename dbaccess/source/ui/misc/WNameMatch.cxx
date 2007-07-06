6e772fabc58dc4b07c20858386038b25448e6773
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:35:30 +0000

    INTEGRATION: CWS oj14 (1.16.8); FILE MERGED 2007/06/04 18:46:48 oj 1.16.8.7: RESYNC: (1.21-1.22); FILE MERGED 2007/02/20 15:50:52 oj 1.16.8.6: RESYNC: (1.20-1.21); FILE MERGED 2006/11/07 09:35:30 oj 1.16.8.5: RESYNC: (1.17-1.20); FILE MERGED 2006/07/04 08:13:53 oj 1.16.8.4: RESYNC: (1.16-1.17); FILE MERGED 2006/04/25 13:03:18 oj 1.16.8.3: new include 2006/03/20 07:48:46 oj 1.16.8.2: use of module client helper 2006/01/03 07:49:20 oj 1.16.8.1: changed module client

--- dbaccess/source/ui/misc/WNameMatch.cxx
+++ dbaccess/source/ui/misc/WNameMatch.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: WNameMatch.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 10:35:19 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:35:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,6 @@
 #endif
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef DBAUI_FIELDDESCRIPTIONS_HXX
 #include "FieldDescriptions.hxx"
