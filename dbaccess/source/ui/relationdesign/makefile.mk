6fae0b7061e939f9c15ce3afaccd6875a3afb1f2
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:42:03 +0000

    INTEGRATION: CWS oj14 (1.4.8); FILE MERGED 2006/01/03 07:49:26 oj 1.4.8.1: changed module client

--- dbaccess/source/ui/relationdesign/makefile.mk
+++ dbaccess/source/ui/relationdesign/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 16:37:46 $
+#   last change: $Author: rt $ $Date: 2007-07-06 08:42:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,7 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
