c1c0281cee912c756bfab588ba31c7db64d8a899
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:37:35 +0000

    INTEGRATION: CWS oj14 (1.25.4); FILE MERGED 2007/06/04 18:49:51 oj 1.25.4.6: RESYNC: (1.28-1.29); FILE MERGED 2006/11/08 11:15:28 oj 1.25.4.5: merge conflicts resolved 2006/11/07 09:38:47 oj 1.25.4.4: RESYNC: (1.26-1.28); FILE MERGED 2006/07/04 08:15:32 oj 1.25.4.3: RESYNC: (1.25-1.26); FILE MERGED 2006/03/20 07:48:49 oj 1.25.4.2: use of module client helper 2006/01/03 07:49:21 oj 1.25.4.1: changed module client

--- dbaccess/source/ui/misc/makefile.mk
+++ dbaccess/source/ui/misc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.29 $
+#   $Revision: 1.30 $
 #
-#   last change: $Author: kz $ $Date: 2007-05-10 10:36:40 $
+#   last change: $Author: rt $ $Date: 2007-07-06 08:37:35 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,7 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings ----------------------------------
 
 .INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
 
 # --- Files -------------------------------------
 
