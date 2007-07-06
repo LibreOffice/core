fe579b3124533d821477a4dc2e0bb7cd8ffcde35
Rüdiger Timm <rt@openoffice.org>
2007-07-06 05:47:42 +0000

    INTEGRATION: CWS oj14 (1.29.86); FILE MERGED 2007/06/20 14:32:03 fs 1.29.86.2: #i78685# +statementcomposer 2007/06/04 20:29:29 fs 1.29.86.1: #i78077# +paramwrapper

--- connectivity/source/commontools/makefile.mk
+++ connectivity/source/commontools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.29 $
+#   $Revision: 1.30 $
 #
-#   last change: $Author: obo $ $Date: 2006-07-10 14:20:56 $
+#   last change: $Author: rt $ $Date: 2007-07-06 06:47:42 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,9 @@ EXCEPTIONSFILES=\
         $(SLO)$/TSortIndex.obj                          \
         $(SLO)$/dbcharset.obj                           \
         $(SLO)$/propertyids.obj                         \
-        $(SLO)$/FValue.obj
+        $(SLO)$/FValue.obj                              \
+        $(SLO)$/paramwrapper.obj                        \
+        $(SLO)$/statementcomposer.obj
 
 SLOFILES=\
         $(EXCEPTIONSFILES)								\
