d3c804a3dd81c93116d24e052980297f3786771f
Rüdiger Timm <rt@openoffice.org>
2007-07-06 12:17:36 +0000

    INTEGRATION: CWS pathfinder01 (1.2.120); FILE MERGED 2007/07/01 16:38:50 cl 1.2.120.1: #i41800# added function to give access to svx string resources to other dll

--- svx/inc/svx/svdetc.hxx
+++ svx/inc/svx/svdetc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdetc.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 16:20:01 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 13:17:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -353,6 +353,13 @@ inline SdrGlobalData& GetSdrGlobalData()
         *ppAppData=new SdrGlobalData;
     }
     return *((SdrGlobalData*)*ppAppData);
+}
+
+namespace sdr
+{
+
+SVX_DLLPUBLIC String GetResourceString(sal_uInt16 nResID);
+
 }
 
 
