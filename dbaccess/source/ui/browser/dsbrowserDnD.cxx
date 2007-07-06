f990473e9bbdb4821cd65f4edac95f3d9f04f3e9
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:04:01 +0000

    INTEGRATION: CWS oj14 (1.71.4); FILE MERGED 2007/06/04 18:05:19 oj 1.71.4.7: RESYNC: (1.75-1.76); FILE MERGED 2006/11/07 09:13:02 oj 1.71.4.6: RESYNC: (1.73-1.75); FILE MERGED 2006/07/04 07:48:23 oj 1.71.4.5: RESYNC: (1.72-1.73); FILE MERGED 2006/04/25 13:01:29 oj 1.71.4.4: new include 2006/03/20 07:48:10 oj 1.71.4.3: use of module client helper 2006/01/03 13:19:11 oj 1.71.4.2: RESYNC: (1.71-1.72); FILE MERGED 2006/01/03 07:49:00 oj 1.71.4.1: changed module client

--- dbaccess/source/ui/browser/dsbrowserDnD.cxx
+++ dbaccess/source/ui/browser/dsbrowserDnD.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dsbrowserDnD.cxx,v $
  *
- *  $Revision: 1.76 $
+ *  $Revision: 1.77 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 10:19:13 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:04:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,9 +57,6 @@
 #endif
 #ifndef _DBU_BRW_HRC_
 #include "dbu_brw.hrc"
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
