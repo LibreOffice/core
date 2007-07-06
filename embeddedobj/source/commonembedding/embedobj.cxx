333b7c8b93f1748705a89666a74e26fe7b2ab970
Rüdiger Timm <rt@openoffice.org>
2007-07-06 09:06:37 +0000

    INTEGRATION: CWS oj14 (1.22.2); FILE MERGED 2007/06/04 19:05:07 oj 1.22.2.2: RESYNC: (1.22-1.23); FILE MERGED 2007/03/14 15:48:04 mav 1.22.2.1: #i75401# no need to use implementation service names any more

--- embeddedobj/source/commonembedding/embedobj.cxx
+++ embeddedobj/source/commonembedding/embedobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: embedobj.cxx,v $
  *
- *  $Revision: 1.23 $
+ *  $Revision: 1.24 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 19:35:00 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 10:06:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,7 +98,6 @@
 #include <targetstatecontrol.hxx>
 
 #include "commonembobj.hxx"
-#include "convert.hxx"
 #include "intercept.hxx"
 
 
