95ba13f6b7a9a378628f043049f4e275b3f07f93
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:13:14 +0000

    INTEGRATION: CWS oj14 (1.41.4); FILE MERGED 2007/06/04 18:19:06 oj 1.41.4.7: RESYNC: (1.45-1.47); FILE MERGED 2007/02/20 15:48:18 oj 1.41.4.6: RESYNC: (1.44-1.45); FILE MERGED 2006/11/07 09:20:31 oj 1.41.4.5: RESYNC: (1.43-1.44); FILE MERGED 2006/07/04 07:55:46 oj 1.41.4.4: RESYNC: (1.41-1.43); FILE MERGED 2006/04/25 13:02:53 oj 1.41.4.3: new include 2006/03/20 07:48:25 oj 1.41.4.2: use of module client helper 2006/01/03 07:49:07 oj 1.41.4.1: changed module client

--- dbaccess/source/ui/dlg/adminpages.cxx
+++ dbaccess/source/ui/dlg/adminpages.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: adminpages.cxx,v $
  *
- *  $Revision: 1.47 $
+ *  $Revision: 1.48 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 10:24:13 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:13:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,9 +41,6 @@
 #endif
 #ifndef _DBAUI_DBADMIN_HRC_
 #include "dbadmin.hrc"
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef _DBU_DLG_HRC_
 #include "dbu_dlg.hrc"
