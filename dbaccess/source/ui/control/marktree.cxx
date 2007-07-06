1aadfd261e1d1c550ca6380487a1957d89c5919e
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:07:36 +0000

    INTEGRATION: CWS oj14 (1.14.4); FILE MERGED 2006/11/07 09:16:43 oj 1.14.4.5: RESYNC: (1.15-1.17); FILE MERGED 2006/07/04 07:51:21 oj 1.14.4.4: RESYNC: (1.14-1.15); FILE MERGED 2006/04/25 12:51:37 oj 1.14.4.3: new include 2006/03/20 07:48:15 oj 1.14.4.2: use of module client helper 2006/01/03 07:49:03 oj 1.14.4.1: changed module client

--- dbaccess/source/ui/control/marktree.cxx
+++ dbaccess/source/ui/control/marktree.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: marktree.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: kz $ $Date: 2006-10-05 13:03:15 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:07:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,9 +38,6 @@
 
 #ifndef _DBAUI_MARKTREE_HXX_
 #include "marktree.hxx"
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef _DBU_CONTROL_HRC_
 #include "dbu_control.hrc"
