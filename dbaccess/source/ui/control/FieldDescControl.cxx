0f3667c81a752f700d6473fc481d2e5477d5e149
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:06:00 +0000

    INTEGRATION: CWS oj14 (1.41.4); FILE MERGED 2007/06/04 18:07:38 oj 1.41.4.7: RESYNC: (1.45-1.46); FILE MERGED 2007/02/20 15:47:35 oj 1.41.4.6: RESYNC: (1.44-1.45); FILE MERGED 2006/11/07 09:15:00 oj 1.41.4.5: RESYNC: (1.42-1.44); FILE MERGED 2006/07/04 07:50:10 oj 1.41.4.4: RESYNC: (1.41-1.42); FILE MERGED 2006/04/25 12:51:36 oj 1.41.4.3: new include 2006/03/20 07:48:13 oj 1.41.4.2: use of module client helper 2006/01/03 07:49:02 oj 1.41.4.1: changed module client

--- dbaccess/source/ui/control/FieldDescControl.cxx
+++ dbaccess/source/ui/control/FieldDescControl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FieldDescControl.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 10:20:14 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:06:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -66,9 +66,6 @@
 #endif
 #ifndef _SVX_NUMFMTSH_HXX
 #include <svx/numfmtsh.hxx>
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef _DBU_CONTROL_HRC_
 #include "dbu_control.hrc"
