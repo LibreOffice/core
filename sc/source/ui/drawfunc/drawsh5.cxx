a2e232c6e124c22b3e8847b57d056ac488aa6259
Rüdiger Timm <rt@openoffice.org>
2007-07-06 11:27:50 +0000

    INTEGRATION: CWS c09tosrc (1.33.58); FILE MERGED 2007/06/20 11:29:16 cd 1.33.58.1: #i78694# Calc must support XTransferableSupplier interface

--- sc/source/ui/drawfunc/drawsh5.cxx
+++ sc/source/ui/drawfunc/drawsh5.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawsh5.cxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 09:52:18 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 12:27:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -777,7 +777,10 @@ void ScDrawShell::StateFormatPaintbrush( SfxItemSet& rSet )
         rSet.Put( SfxBoolItem( SID_FORMATPAINTBRUSH, bHasPaintBrush ) );
 }
 
-
+ScDrawView* ScDrawShell::GetDrawView()
+{
+    return pViewData->GetView()->GetScDrawView();
+}
 
 
 
