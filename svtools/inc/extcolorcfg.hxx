5362b310db0613eb33608b56fc2b2865730b6063
Rüdiger Timm <rt@openoffice.org>
2007-07-06 09:01:10 +0000

    INTEGRATION: CWS oj14 (1.1.2); FILE ADDED 2006/11/08 07:24:04 oj 1.1.2.2: warnings removed 2006/11/07 14:26:29 oj 1.1.2.1: new ext color scheme

---
+++ svtools/inc/extcolorcfg.hxx
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: extcolorcfg.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-07-06 10:01:10 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef INCLUDED_SVTOOLS_EXTCOLORCFG_HXX
+#define INCLUDED_SVTOOLS_EXTCOLORCFG_HXX
+
+#ifndef INCLUDED_SVTDLLAPI_H
+#include "svtools/svtdllapi.h"
+#endif
+
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+#ifndef _TOOLS_COLOR_HXX
+#include <tools/color.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_H_
+#include <com/sun/star/uno/Sequence.h>
+#endif
+#ifndef _SFXBRDCST_HXX
+#include <brdcst.hxx>
+#endif
+#ifndef _SFXLSTNER_HXX
+#include <lstner.hxx>
+#endif
+
+//-----------------------------------------------------------------------------
+namespace svtools{
+/* -----------------------------22.03.2002 15:36------------------------------
+
+ ---------------------------------------------------------------------------*/
+class ExtendedColorConfig_Impl;
+struct ExtendedColorConfigValue
+{
+    ::rtl::OUString m_sName;
+    ::rtl::OUString m_sDisplayName;
+    sal_Int32       nColor;
+    ExtendedColorConfigValue() : nColor(0) {}
+    sal_Bool operator !=(const ExtendedColorConfigValue& rCmp) const
+        { return nColor != rCmp.nColor;}
+};
+/* -----------------------------22.03.2002 15:36------------------------------
+
+ ---------------------------------------------------------------------------*/
+class SVT_DLLPUBLIC ExtendedColorConfig : public SfxBroadcaster, public SfxListener
+{
+    friend class ExtendedColorConfig_Impl;
+private:
+    static ExtendedColorConfig_Impl* m_pImpl;
+public:
+    ExtendedColorConfig();
+    ~ExtendedColorConfig();
+
+    virtual void        Notify( SfxBroadcaster& rBC, const SfxHint& rHint );
+
+    // get the configured value
+    ExtendedColorConfigValue        GetColorValue(const ::rtl::OUString& _sComponentName,const ::rtl::OUString& _sName)const;
+    sal_Int32                       GetComponentCount() const;
+    ::rtl::OUString                 GetComponentName(sal_uInt32 _nPos) const;
+    ::rtl::OUString                 GetComponentDisplayName(const ::rtl::OUString& _sComponentName) const;
+    sal_Int32                       GetComponentColorCount(const ::rtl::OUString& _sName) const;
+    ExtendedColorConfigValue        GetComponentColorConfigValue(const ::rtl::OUString& _sComponentName,sal_uInt32 _nPos) const;
+};
+/* -----------------------------22.03.2002 15:31------------------------------
+
+ ---------------------------------------------------------------------------*/
+class SVT_DLLPUBLIC EditableExtendedColorConfig
+{
+    ExtendedColorConfig_Impl*   m_pImpl;
+    sal_Bool            m_bModified;
+public:
+    EditableExtendedColorConfig();
+    ~EditableExtendedColorConfig();
+
+    ::com::sun::star::uno::Sequence< ::rtl::OUString >  GetSchemeNames() const;
+    void                                                DeleteScheme(const ::rtl::OUString& rScheme );
+    void                                                AddScheme(const ::rtl::OUString& rScheme );
+    sal_Bool                                            LoadScheme(const ::rtl::OUString& rScheme );
+    const ::rtl::OUString&                              GetCurrentSchemeName()const;
+    void                        SetCurrentSchemeName(const ::rtl::OUString& rScheme);
+
+    ExtendedColorConfigValue    GetColorValue(const ::rtl::OUString& _sComponentName,const ::rtl::OUString& _sName)const;
+    sal_Int32                   GetComponentCount() const;
+    ::rtl::OUString             GetComponentName(sal_uInt32 _nPos) const;
+    ::rtl::OUString             GetComponentDisplayName(const ::rtl::OUString& _sComponentName) const;
+    sal_Int32                   GetComponentColorCount(const ::rtl::OUString& _sName) const;
+    ExtendedColorConfigValue    GetComponentColorConfigValue(const ::rtl::OUString& _sName,sal_uInt32 _nPos) const;
+    void                        SetColorValue(const ::rtl::OUString& _sComponentName, const ExtendedColorConfigValue& rValue);
+    void                        SetModified();
+    void                        ClearModified(){m_bModified = sal_False;}
+    sal_Bool                    IsModified()const{return m_bModified;}
+    void                        Commit();
+
+    void                        DisableBroadcast();
+    void                        EnableBroadcast();
+};
+}//namespace svtools
+#endif
+
