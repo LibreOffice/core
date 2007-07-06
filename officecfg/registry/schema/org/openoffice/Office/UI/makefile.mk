311c5e5d01e56fdcbc2c54997fcce8bf417d82f5
Rüdiger Timm <rt@openoffice.org>
2007-07-06 05:52:44 +0000

    INTEGRATION: CWS oj14 (1.11.50); FILE MERGED 2007/06/04 19:09:55 oj 1.11.50.2: RESYNC: (1.11-1.12); FILE MERGED 2007/05/03 06:19:31 oj 1.11.50.1: #i76758# new config for report

--- officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: ihi $ $Date: 2007-04-16 15:36:50 $
+#   last change: $Author: rt $ $Date: 2007-07-06 06:52:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -80,7 +80,9 @@ XCSFILES= \
     Effects.xcs \
     Category.xcs \
     GenericCategories.xcs \
-    GlobalSettings.xcs
+    GlobalSettings.xcs \
+    DbReportWindowState.xcs \
+    ReportCommands.xcs
 
 .INCLUDE :  target.mk
 
