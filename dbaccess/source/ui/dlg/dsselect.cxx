41a495cb70b6e10b3c69c3aa1a950ea72e9ca556
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:16:45 +0000

    INTEGRATION: CWS oj14 (1.14.8); FILE MERGED 2007/06/04 18:25:23 oj 1.14.8.6: RESYNC: (1.18-1.19); FILE MERGED 2006/11/07 09:23:12 oj 1.14.8.5: RESYNC: (1.16-1.18); FILE MERGED 2006/07/04 07:58:38 oj 1.14.8.4: RESYNC: (1.14-1.16); FILE MERGED 2006/04/25 13:02:59 oj 1.14.8.3: new include 2006/03/20 07:48:31 oj 1.14.8.2: use of module client helper 2006/01/03 07:49:09 oj 1.14.8.1: changed module client

--- dbaccess/source/ui/dlg/dsselect.cxx
+++ dbaccess/source/ui/dlg/dsselect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dsselect.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 08:00:01 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:16:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,9 +45,6 @@
 #endif
 #ifndef _DBAUI_DSSELECT_HRC_
 #include "dsselect.hrc"
-#endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
-#include "moduledbu.hxx"
 #endif
 #ifndef _DBU_DLG_HRC_
 #include "dbu_dlg.hrc"
