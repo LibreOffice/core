2fe39ebd5f82908025dade308bcebb7e3491fa5e
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:36:26 +0000

    INTEGRATION: CWS oj14 (1.7.8); FILE MERGED 2006/11/08 11:15:28 oj 1.7.8.6: merge conflicts resolved 2006/11/07 09:36:55 oj 1.7.8.5: RESYNC: (1.8-1.11); FILE MERGED 2006/07/04 08:14:57 oj 1.7.8.4: RESYNC: (1.7-1.8); FILE MERGED 2006/04/25 13:03:19 oj 1.7.8.3: new include 2006/03/20 07:48:48 oj 1.7.8.2: use of module client helper 2006/01/03 07:49:20 oj 1.7.8.1: changed module client

--- dbaccess/source/ui/misc/datasourceconnector.cxx
+++ dbaccess/source/ui/misc/datasourceconnector.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datasourceconnector.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 07:16:21 $
+ *  last change: $Author: rt $ $Date: 2007-07-06 08:36:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -93,9 +93,7 @@
 #ifndef _DBU_MISC_HRC_
 #include "dbu_misc.hrc"
 #endif
-#ifndef _DBAUI_MODULE_DBU_HXX_
 #include "moduledbu.hxx"
-#endif
 
 //.........................................................................
 namespace dbaui
