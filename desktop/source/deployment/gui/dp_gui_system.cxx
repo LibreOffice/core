5cc939597bc02388a440c60c68dfc4ae3167f244
Rüdiger Timm <rt@openoffice.org>
2007-07-06 11:34:46 +0000

    INTEGRATION: CWS c09tosrc (1.1.2); FILE ADDED 2007/06/29 10:26:44 rt 1.1.2.2: PCH-include added. 2007/06/29 08:56:37 jl 1.1.2.1: #i79001# Error string for update extension on Windows Vista

---
+++ desktop/source/deployment/gui/dp_gui_system.cxx
@@ -0,0 +1,63 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: dp_gui_system.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-07-06 12:34:46 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_desktop.hxx"
+
+#include "dp_gui_system.hxx"
+#ifdef WNT
+#define WIN32_LEAN_AND_MEAN
+#pragma warning(push,1) /* disable warnings within system headers */
+#include <windows.h>
+#pragma warning(pop)
+#endif
+
+namespace dp_gui {
+
+//We cannot distinguish Vista and 2008 Server
+bool isVista()
+{
+#ifdef WNT
+    OSVERSIONINFO osvi;
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    GetVersionEx(&osvi);
+    return osvi.dwMajorVersion >= 6;
+#else
+    return false;
+#endif
+}
+
+} //namespace dp_gui
