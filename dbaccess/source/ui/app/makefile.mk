580eaea47a3a3010622e17935df9dc068a2f47d1
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:02:23 +0000

    INTEGRATION: CWS oj14 (1.3.8); FILE MERGED 2006/01/03 07:48:59 oj 1.3.8.1: changed module client

--- dbaccess/source/ui/app/makefile.mk
+++ dbaccess/source/ui/app/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 14:24:44 $
+#   last change: $Author: rt $ $Date: 2007-07-06 08:02:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,7 @@ TARGET=app
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
 # IMGLST_SRS=$(SRS)$/$(TARGET).srs
 # --- Files --------------------------------------------------------
 
