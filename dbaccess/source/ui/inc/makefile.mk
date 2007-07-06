3773819e159f62a4c151e981c1b7c090a328f940
Rüdiger Timm <rt@openoffice.org>
2007-07-06 07:31:06 +0000

    INTEGRATION: CWS oj14 (1.3.8); FILE MERGED 2006/01/03 07:49:16 oj 1.3.8.1: changed module client

--- dbaccess/source/ui/inc/makefile.mk
+++ dbaccess/source/ui/inc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 15:58:35 $
+#   last change: $Author: rt $ $Date: 2007-07-06 08:31:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,6 +41,7 @@ TARGET=dba_uiinc
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 
