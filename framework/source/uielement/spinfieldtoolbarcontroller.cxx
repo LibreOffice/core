62109ea57677f4fe03fd70a467b836ca59adb66f
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-10 14:13:01 +0000

    INTEGRATION: CWS fwk66 (1.4.8); FILE MERGED 2007/06/04 11:22:21 cd 1.4.8.1: #i78020# Support merging to menu and toolbar for add-ons

--- framework/source/uielement/spinfieldtoolbarcontroller.cxx
+++ framework/source/uielement/spinfieldtoolbarcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spinfieldtoolbarcontroller.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2007-03-26 13:12:47 $
+ *  last change: $Author: ihi $ $Date: 2007-07-10 15:13:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -238,7 +238,7 @@ long SpinfieldControl::PreNotify( NotifyEvent& rNEvt )
 SpinfieldToolbarController::SpinfieldToolbarController(
     const Reference< XMultiServiceFactory >& rServiceManager,
     const Reference< XFrame >&               rFrame,
-    ToolBar*                                 pToolbar,
+    ToolBox*                                 pToolbar,
     USHORT                                   nID,
     sal_Int32                                nWidth,
     const OUString&                          aCommand ) :
