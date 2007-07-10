ed8f818f831ee54cf2e985b917a3eb30d728e651
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-10 14:10:38 +0000

    INTEGRATION: CWS fwk66 (1.4.64); FILE MERGED 2007/06/04 11:22:20 cd 1.4.64.1: #i78020# Support merging to menu and toolbar for add-ons

--- framework/source/uielement/dropdownboxtoolbarcontroller.cxx
+++ framework/source/uielement/dropdownboxtoolbarcontroller.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dropdownboxtoolbarcontroller.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-21 17:21:39 $
+ *  last change: $Author: ihi $ $Date: 2007-07-10 15:10:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -185,7 +185,7 @@ long ListBoxControl::PreNotify( NotifyEvent& rNEvt )
 DropdownToolbarController::DropdownToolbarController(
     const Reference< XMultiServiceFactory >& rServiceManager,
     const Reference< XFrame >&               rFrame,
-    ToolBar*                                 pToolbar,
+    ToolBox*                                 pToolbar,
     USHORT                                   nID,
     sal_Int32                                nWidth,
     const OUString&                          aCommand ) :
