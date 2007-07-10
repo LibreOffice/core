a803572c74cc585428e003c30c4b5d8f95b45607
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-10 14:17:25 +0000

    INTEGRATION: CWS fwk66 (1.7.60); FILE MERGED 2007/06/06 08:41:13 pb 1.7.60.1: fix: syntax error after resync

--- svtools/source/filter.vcl/filter/dlgejpg.cxx
+++ svtools/source/filter.vcl/filter/dlgejpg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgejpg.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 21:33:36 $
+ *  last change: $Author: ihi $ $Date: 2007-07-10 15:17:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
