96128dd1a78d353bf1d6c07d557cb1fed9e00832
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-10 14:19:33 +0000

    INTEGRATION: CWS fwk66 (1.5.60); FILE MERGED 2007/06/06 09:14:43 pb 1.5.60.1: fix: syntax error after resync

--- svtools/source/productregistration/productregistration.cxx
+++ svtools/source/productregistration/productregistration.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: productregistration.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 21:58:23 $
+ *  last change: $Author: ihi $ $Date: 2007-07-10 15:19:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
