3e9e8723f6f775f0d6ed9869c2e2cc65e313c1c2
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-11 12:04:00 +0000

    INTEGRATION: CWS mba23issues03 (1.9.50); FILE MERGED 2007/06/14 18:56:30 mba 1.9.50.1: #i72366#: remove old MacOS9 code

--- xmloff/source/transform/makefile.mk
+++ xmloff/source/transform/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 14:35:42 $
+#   last change: $Author: ihi $ $Date: 2007-07-11 13:04:00 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -90,7 +90,7 @@ SHL1STDLIBS = \
     $(SALHELPERLIB) \
     $(SALLIB) \
     $(TOOLSLIB)
-.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC" || "$(COM)"=="GCC"
+.IF "$(GUI)"=="UNX" || "$(COM)"=="GCC"
     SHL1STDLIBS += -lxo$(OFFICEUPD)$(DLLPOSTFIX)
 .ELSE
     SHL1STDLIBS += ixo.lib
