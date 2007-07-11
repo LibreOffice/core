4b05afd675bbd70abb54d3dc45ed7ec6d60642d9
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-11 10:00:44 +0000

    INTEGRATION: CWS aquafilepicker01 (1.1.2); FILE ADDED 2007/06/15 21:25:25 fheckl 1.1.2.5: cleanup changes 2007/05/09 06:12:05 fheckl 1.1.2.4: some cleanup and last changes 2007/04/13 18:26:04 fheckl 1.1.2.3: Adding new files and general update 2007/02/20 23:57:42 fheckl 1.1.2.2: Next phase of aqua file picker after some refactoring 2006/10/06 21:23:11 fheckl 1.1.2.1: Initial version of MacOSX native file picker

---
+++ fpicker/source/aqua/makefile.mk
@@ -0,0 +1,90 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2007-07-11 11:00:44 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+
+PRJNAME=fpicker
+TARGET=fps_aqua.uno
+TARGETTYPE=GUI
+LIBTARGET=NO
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+#CFLAGS += -Wall
+DLLPRE=
+
+# ------------------------------------------------------------------
+
+.IF "$(GUIBASE)" != "aqua"
+
+dummy:
+    @echo "Nothing to build. GUIBASE == $(GUIBASE)"
+
+.ELSE # we build for aqua
+
+
+# --- Files --------------------------------------------------------
+
+SLOFILES =\
+        $(SLO)$/resourceprovider.obj    \
+        $(SLO)$/FPentry.obj             \
+        $(SLO)$/SalAquaPicker.obj       \
+        $(SLO)$/SalAquaFilePicker.obj   \
+        $(SLO)$/SalAquaFolderPicker.obj \
+        $(SLO)$/CFStringUtilities.obj   \
+        $(SLO)$/FilterHelper.obj        \
+        $(SLO)$/ControlHelper.obj
+
+SHL1NOCHECK=TRUE
+SHL1TARGET= $(TARGET)
+SHL1OBJS=   $(SLOFILES)
+SHL1STDLIBS=\
+    $(VCLLIB) \
+    $(TOOLSLIB) \
+    $(CPPUHELPERLIB) \
+    $(CPPULIB) \
+    $(SALLIB)
+
+SHL1VERSIONMAP=exports.map
+DEF1NAME=$(SHL1TARGET)
+
+.ENDIF # "$(GUIBASE)" != "aqua"
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
