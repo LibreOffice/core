032cd16fcafab3fe49f8886bdcd1a9c26677d019
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-11 09:59:33 +0000

    INTEGRATION: CWS aquafilepicker01 (1.1.2); FILE ADDED 2007/06/15 21:25:25 fheckl 1.1.2.5: cleanup changes 2007/05/09 06:12:05 fheckl 1.1.2.4: some cleanup and last changes 2007/04/13 18:26:03 fheckl 1.1.2.3: Adding new files and general update 2007/02/20 23:57:41 fheckl 1.1.2.2: Next phase of aqua file picker after some refactoring 2006/10/06 21:23:09 fheckl 1.1.2.1: Initial version of MacOSX native file picker

---
+++ fpicker/source/aqua/SalAquaFolderPicker.hxx
@@ -0,0 +1,138 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: SalAquaFolderPicker.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-07-11 10:59:33 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SALAQUAFOLDERPICKER_HXX_
+#define _SALAQUAFOLDERPICKER_HXX_
+
+//_______________________________________________________________________________________________________________________
+//  includes of other projects
+//_______________________________________________________________________________________________________________________
+
+#ifndef _CPPUHELPER_COMPBASE3_HXX_
+#include <cppuhelper/implbase3.hxx>
+#endif
+
+#ifndef _SALAQUAPICKER_HXX_
+#include "SalAquaPicker.hxx"
+#endif
+
+#include <memory>
+
+#ifndef _RTL_USTRING_H_
+#include <rtl/ustring.hxx>
+#endif
+
+#include <list>
+
+//----------------------------------------------------------
+// class declaration
+//----------------------------------------------------------
+
+class SalAquaFolderPicker :
+        public SalAquaPicker,
+    public cppu::WeakImplHelper3<
+    ::com::sun::star::ui::dialogs::XFolderPicker,
+    ::com::sun::star::lang::XServiceInfo,
+        ::com::sun::star::util::XCancellable >
+{
+public:
+
+    // constructor
+    SalAquaFolderPicker( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& xServiceMgr );
+
+    //------------------------------------------------------------------------------------
+    // XExecutableDialog functions
+    //------------------------------------------------------------------------------------
+
+    virtual void SAL_CALL setTitle( const ::rtl::OUString& aTitle )
+        throw( ::com::sun::star::uno::RuntimeException );
+
+    virtual sal_Int16 SAL_CALL execute(  )
+        throw( ::com::sun::star::uno::RuntimeException );
+
+    //------------------------------------------------------------------------------------
+    // XFolderPicker functions
+    //------------------------------------------------------------------------------------
+
+    virtual void SAL_CALL setDisplayDirectory( const rtl::OUString& rDirectory )
+        throw( com::sun::star::lang::IllegalArgumentException, com::sun::star::uno::RuntimeException );
+
+    virtual rtl::OUString SAL_CALL getDisplayDirectory(  )
+        throw( com::sun::star::uno::RuntimeException );
+
+    virtual rtl::OUString SAL_CALL getDirectory( )
+        throw( com::sun::star::uno::RuntimeException );
+
+    virtual void SAL_CALL setDescription( const rtl::OUString& rDescription )
+        throw( com::sun::star::uno::RuntimeException );
+
+    //------------------------------------------------
+    // XServiceInfo
+    //------------------------------------------------
+
+    virtual ::rtl::OUString SAL_CALL getImplementationName(  )
+        throw(::com::sun::star::uno::RuntimeException);
+
+    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName )
+        throw(::com::sun::star::uno::RuntimeException);
+
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames(  )
+        throw(::com::sun::star::uno::RuntimeException);
+
+    //------------------------------------------------
+    // XCancellable
+    //------------------------------------------------
+
+    virtual void SAL_CALL cancel( )
+        throw( ::com::sun::star::uno::RuntimeException );
+
+    //------------------------------------------------
+    // XEventListener
+    //------------------------------------------------
+
+    virtual void SAL_CALL disposing( const ::com::sun::star::lang::EventObject& aEvent )
+        throw(::com::sun::star::uno::RuntimeException);
+
+private:
+    // prevent copy and assignment
+    SalAquaFolderPicker( const SalAquaFolderPicker& );
+    SalAquaFolderPicker& operator=( const SalAquaFolderPicker& );
+private:
+    // to instantiate own services
+    ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > m_xServiceMgr;
+
+};
+
+#endif // _SALAQUAFOLDERPICKER_HXX_
