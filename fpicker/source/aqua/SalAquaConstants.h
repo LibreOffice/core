bb406e255967ea2d003ad6416c5654d31bdb9817
Ivo Hinkelmann <ihi@openoffice.org>
2007-07-11 09:58:52 +0000

    INTEGRATION: CWS aquafilepicker01 (1.1.2); FILE ADDED 2007/06/16 11:39:34 pjanik 1.1.2.2: #i74798#: Add license header. 2007/04/13 18:26:01 fheckl 1.1.2.1: Adding new files and general update

---
+++ fpicker/source/aqua/SalAquaConstants.h
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: SalAquaConstants.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-07-11 10:58:52 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SALAQUACONSTANTS_H_
+#define _SALAQUACONSTANTS_H_
+
+#define kAppFourCharCode 'OOo2'
+#define kControlPropertyTracking 'Trck'
+#define kControlPropertyLastPartCode 'LsPc'
+#define kControlPropertySubType 'SuTy'
+#define kPopupControlPropertyTitleWidth 'PoTW'
+
+#define kAquaSpaceBetweenControls 8
+#define kAquaSpaceInsideGroup 16
+
+#define kAquaNavigationServicesMinWidth 400
+#define kAquaNavigationServicesPopupControlWidth 250
+
+#endif //_SALAQUACONSTANTS_H_
+
