29af95711d5bc12eb3c05c43efdac4a7ff8fb16b
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:23:20 +0000

    INTEGRATION: CWS presfixes12 (1.4.30); FILE MERGED 2007/02/20 22:23:08 thb 1.4.30.1: #i37778# Added XCanvas::clear() method throughout all implementations

--- canvas/source/null/null_spritehelper.cxx
+++ canvas/source/null/null_spritehelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: null_spritehelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 03:24:12 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:23:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -93,11 +93,6 @@ namespace nullcanvas
     }
 
     void SpriteHelper::redraw( bool& /*io_bSurfaceDirty*/ ) const
-    {
-        // TODO
-    }
-
-    void SpriteHelper::clearSurface()
     {
         // TODO
     }
