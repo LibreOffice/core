27f580e7b3fd9f8ee4ced05d6d27ca63a989b87f
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:56:45 +0000

    INTEGRATION: CWS presfixes12 (1.1.2); FILE ADDED 2007/05/10 20:32:40 thb 1.1.2.10: #i37778# Reworked LayerManager::updateShapeLayers - now much better aligned with general shape update, and avoids superfluous rendering; made unit tests work again; passing down slide background repaint status to LayerManager; relaxed preconditions for Shape::getUpdateArea() - no longer requires views to work; now catching singular view matrix and using sensible default at API border 2007/03/06 21:54:17 thb 1.1.2.9: #i37778# Added bool property  to disable z order correct animations 2007/03/06 18:05:49 thb 1.1.2.8: #i37778# Fixed a few minor issues found during code review - thx to fs 2007/03/05 13:47:12 thb 1.1.2.7: #i37778# Removed Activity::needsScreenUpdate() method - this is now handled by notifying ScreenUpdater explicitely; reorg of SlideShowImpl members, to align lifetime with mutual references; fixed (hopefully) last repaint issues with new LayerManager; added more tests 2007/02/25 01:10:27 thb 1.1.2.6: #i37778# Cleared up error handling a lot: no longer quenching RuntimeExceptions; reporting assertions in the debug case; ViewLayer  now reports resized sprite (which needs re-render from all shapes); fixed missing subset area reduction for glyph-level animations; added return of resize state from  Layer::commitLayerBounds(); adapted unit tests to corrected behaviour 2007/02/13 09:48:46 thb 1.1.2.5: #i37778# Removed fast update code path again, this would need BSP or somesuch to implement efficiently for large number of shapes 2007/02/10 23:23:52 thb 1.1.2.4: #i37778# Renamed setMouseCursor() to setCursorShape() at View interface (name clash with API method otherwise); added LayerManager/Layer unit tests; fixed a bunch of bugs/glitches found during unit testing 2007/02/06 17:18:13 thb 1.1.2.3: #i37778# Moved clear() method from View to ViewLayer (also sprites need to be cleared); fixed a few more cases of local code style violations; removed redundant inline keywords; finished Layer/LayerManager rework (Layer now represents ViewLayers, shapes and rendering are fully under LayerManager control); made shape comparator reusable 2007/01/30 16:43:50 thb 1.1.2.2: #i37778# Made view update/repaint/resize work again; swapped BackgroundShape parameters for correct mtf import 2007/01/29 14:02:13 thb 1.1.2.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

---
+++ slideshow/source/engine/slide/layermanager.hxx
@@ -0,0 +1,408 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: layermanager.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:56:45 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_SLIDESHOW_LAYERMANAGER_HXX
+#define INCLUDED_SLIDESHOW_LAYERMANAGER_HXX
+
+#include <boost/shared_ptr.hpp>
+#include <boost/noncopyable.hpp>
+
+#include <cppcanvas/spritecanvas.hxx>
+
+#include "unoview.hxx"
+#include "unoviewcontainer.hxx"
+#include "attributableshape.hxx"
+#include "layer.hxx"
+#include "tools.hxx"
+
+#include <vector>
+#include <map>
+#include <hash_map>
+#include <algorithm>
+#include <functional>
+
+namespace basegfx {
+    class B2DRange;
+}
+
+namespace slideshow
+{
+    namespace internal
+    {
+        /* Definition of Layermanager class */
+
+        /** This class manages all of a slide's layers (and shapes)
+
+            Since layer content changes when animations start or end,
+            the layer manager keeps track of this and also handles
+            starting/stopping of Shape animations. Note that none of
+            the methods actually perform a screen update, this is
+            always delayed until the ActivitiesQueue explicitely
+            performs it.
+
+            @see Layer
+            @see Shape
+         */
+        class LayerManager : private boost::noncopyable
+        {
+        public:
+            /** Create a new layer manager for the given page bounds
+
+                @param rViews
+                Views currently registered
+
+                @param rPageBounds
+                Overall page bounds, in user space coordinates
+
+                @param bDisableAnimationZOrder
+                When true, all sprite animations run in the
+                foreground.  That is, no extra layers are created, and
+                the slideshow runs potentially faster.
+             */
+            LayerManager( const UnoViewContainer&    rViews,
+                          const ::basegfx::B2DRange& rPageBounds,
+                          bool                       bDisableAnimationZOrder );
+
+            /** Activate the LayerManager
+
+                This method activates the LayerManager. Prior to
+                activation, this instance will be passive, i.e. won't
+                render anything to any view.
+
+                @param bSlideBackgoundPainted
+                When true, the initial slide content on the background
+                layer is already rendered (e.g. from a previous slide
+                transition). When false, LayerManager also renders
+                initial content of background layer on next update()
+                call.
+             */
+            void activate( bool bSlideBackgoundPainted );
+
+            /** Deactivate the LayerManager
+
+                This method deactivates the LayerManager. After
+                deactivation, this instance will be passive,
+                i.e. don't render anything to any view.  Furthermore,
+                if there's currently more than one Layer active, this
+                method also removes all but one.
+             */
+            void deactivate();
+
+            // From ViewEventHandler, forwarded by SlideImpl
+            /// Notify new view added to UnoViewContainer
+            void viewAdded( const UnoViewSharedPtr& rView );
+            /// Notify view removed from UnoViewContainer
+            void viewRemoved( const UnoViewSharedPtr& rView );
+            void viewChanged( const UnoViewSharedPtr& rView );
+            void viewsChanged();
+
+            /** Add the shape to this object
+
+                This method adds a shape to the page.
+             */
+            void addShape( const ShapeSharedPtr& rShape );
+
+            /** Remove shape from this object
+
+                This method removes a shape from the shape.
+             */
+            bool removeShape( const ShapeSharedPtr& rShape );
+
+            /** Lookup a Shape from an XShape model object
+
+                This method looks up the internal shape map for one
+                representing the given XShape.
+
+                @param xShape
+                The XShape object, for which the representing Shape
+                should be looked up.
+             */
+            ShapeSharedPtr lookupShape( const ::com::sun::star::uno::Reference<
+                                           ::com::sun::star::drawing::XShape >& xShape ) const;
+
+            /** Query a subset of the given original shape
+
+                This method queries a new (but not necessarily unique)
+                shape, which displays only the given subset of the
+                original one.
+             */
+            AttributableShapeSharedPtr getSubsetShape( const AttributableShapeSharedPtr&    rOrigShape,
+                                                       const DocTreeNode&                   rTreeNode );
+
+            /** Revoke a previously queried subset shape.
+
+                With this method, a previously requested subset shape
+                is revoked again. If the last client revokes a given
+                subset, it will cease to be displayed, and the
+                original shape will again show the subset data.
+
+                @param rOrigShape
+                The shape the subset was created from
+
+                @param rSubsetShape
+                The subset created from rOrigShape
+             */
+            void revokeSubset( const AttributableShapeSharedPtr& rOrigShape,
+                               const AttributableShapeSharedPtr& rSubsetShape );
+
+            /** Notify the LayerManager that the given Shape starts an
+                animation now.
+
+                This method enters animation mode for the Shape on all
+                registered views.
+             */
+            void enterAnimationMode( const AnimatableShapeSharedPtr& rShape );
+
+            /** Notify the LayerManager that the given Shape is no
+                longer animated.
+
+                This methods ends animation mode for the given Shape
+                on all registered views.
+             */
+            void leaveAnimationMode( const AnimatableShapeSharedPtr& rShape );
+
+            /** Notify that a shape needs an update
+
+                This method notifies the layer manager that a shape
+                update is necessary. This is useful if, during
+                animation playback, changes occur to shapes which make
+                an update necessary on an update() call. Otherwise,
+                update() will not render anything, which is not
+                triggered by calling one of the other LayerManager
+                methods.
+
+                @param rShape
+                Shape which needs an update
+             */
+            void notifyShapeUpdate( const ShapeSharedPtr& rShape);
+
+            /** Check whether any update operations  are pending.
+
+                @return true, if this LayerManager has any updates
+                pending, i.e. needs to repaint something for the next
+                frame.
+             */
+            bool isUpdatePending() const;
+
+            /** Update the content
+
+                This method updates the content on all layers on all
+                registered views. It does not issues a
+                View::updateScreen() call on registered views. Please
+                note that this method only takes into account changes
+                to shapes induced directly by calling methods of the
+                LayerManager. If a shape needs an update, because of
+                some external event unknown to the LayerManager (most
+                notably running animations), you have to notify the
+                LayerManager via notifyShapeUpdate().
+
+                @see LayerManager::updateScreen()
+
+                @return whether the update finished successfully.
+            */
+            bool update();
+
+            /** Render the content to given canvas
+
+                This is a one-shot operation, which simply draws all
+                shapes onto the given canvas, without any caching or
+                other fuzz. Don't use that for repeated output onto
+                the same canvas, the View concept is more optimal
+                then.
+
+                @param rTargetCanvas
+                Target canvas to output onto.
+             */
+            bool renderTo( const ::cppcanvas::CanvasSharedPtr& rTargetCanvas ) const;
+
+        private:
+            /** A hash map which maps the XShape to the corresponding Shape object.
+
+                Provides quicker lookup than ShapeSet for simple mappings
+             */
+            typedef ::std::hash_map<
+                ::com::sun::star::uno::Reference<
+                    ::com::sun::star::drawing::XShape >,
+                ShapeSharedPtr,
+                hash< ::com::sun::star::uno::Reference<
+                      ::com::sun::star::drawing::XShape > > > XShapeHash;
+
+            /** Element of all-shapes set
+             */
+            struct ShapeEntry
+            {
+                /// Shape this entry stands for
+                ShapeSharedPtr    mpShape;
+
+                /// Layer this shape is currently displayed on
+                LayerWeakPtr      mpLayer;
+
+                explicit ShapeEntry( ShapeSharedPtr const& rShape ) :
+                    mpShape(rShape),
+                    mpLayer()
+                {}
+
+                ShapeSharedPtr const& getShape() const { return mpShape; }
+
+                bool operator<( const ShapeEntry& rRHS ) const
+                {
+                    return Shape::lessThanShape::compare(mpShape.get(),
+                                                         rRHS.mpShape.get());
+                }
+            };
+
+            /** Set of all shapes
+             */
+            typedef ::std::set< ShapeEntry > LayerShapeSet;
+            typedef ::std::set< ShapeSharedPtr > ShapeUpdateSet;
+
+
+            ////////////////////////////////////////////////////////////////////////
+
+
+            /// Adds shape area to containing layer's damage area
+            void addUpdateArea( ShapeSharedPtr const& rShape );
+
+            LayerSharedPtr createForegroundLayer() const;
+
+            /** Push changes from updateShapeLayerAssociations() to current layer
+
+                Factored-out method that resizes layer, if necessary,
+                assigns correct layer priority, and repaints contained shapes.
+
+                @param nCurrLayerIndex
+                Index of current layer in maLayers
+
+                @param aFirstLayerShape
+                Valid iterator out of maAllShapes, denoting the first
+                shape from nCurrLayerIndex
+
+                @param aEndLayerShapes
+                Valid iterator or end iterator out of maAllShapes,
+                denoting one-behind-the-last shape of nCurrLayerIndex
+             */
+            void           commitLayerChanges( std::size_t                    nCurrLayerIndex,
+                                               LayerShapeSet::const_iterator  aFirstLayerShape,
+                                               LayerShapeSet::const_iterator  aEndLayerShapes );
+
+            /** Init Shape layers with background layer.
+             */
+            void          putShape2BackgroundLayer( const ShapeEntry& rShapeEntry );
+
+            /** Commits any pending layer reorg, due to shapes either
+                entering or leaving animation mode
+
+                @param bBackgroundLayerPainted
+                When true, LayerManager does not render anything on
+                the background layer. Use this, if background has been
+                updated by other means (e.g. slide transition)
+            */
+            void          updateShapeLayers( bool bBackgroundLayerPainted );
+
+            /** Common stuff when adding a shape
+             */
+            void          implAddShape( const ShapeSharedPtr& rShape );
+
+            /** Common stuff when removing a shape
+             */
+            void          implRemoveShape( const ShapeSharedPtr& rShape );
+
+            /** Add or remove views
+
+                Sharing duplicate code from viewAdded and viewRemoved
+                method. The only point of variation at those places
+                are removal vs. adding.
+             */
+            template<typename LayerFunc,
+                     typename ShapeFunc> void manageViews( LayerFunc layerFunc,
+                                                           ShapeFunc shapeFunc );
+
+            bool updateSprites();
+
+            /// Registered views
+            const UnoViewContainer&  mrViews;
+
+            /// All layers of this object. Vector owns the layers
+            LayerVector              maLayers;
+
+            /** Contains all shapes with their XShape reference as the key
+             */
+            XShapeHash               maXShapeHash;
+
+            /** Set of shapes this LayerManager own
+
+                Contains the same set of shapes as XShapeHash, but is
+                sorted in z order, for painting and layer
+                association. Set entries are enriched with two flags
+                for buffering animation enable/disable changes, and
+                shape update requests.
+            */
+            LayerShapeSet            maAllShapes;
+
+            /** Set of shapes that have requested an update
+
+                When a shape is member of this set, its maShapes entry
+                has bNeedsUpdate set to true. We maintain this
+                redundant information for faster update processing.
+             */
+            ShapeUpdateSet           maUpdateShapes;
+
+            /** Overall slide bounds (in user coordinate
+                system). shapes that exceed this boundary are clipped,
+                thus, layers only need to be of this size.
+             */
+            const basegfx::B2DRange  maPageBounds;
+
+            /// Number of shape sprites currenly active on this LayerManager
+            sal_Int32                mnActiveSprites;
+
+            /// TRUE, if shapes might need to move to different layer
+            bool                     mbLayerAssociationDirty;
+
+            /// FALSE when deactivated
+            bool                     mbActive;
+
+            /** When true, all sprite animations run in the foreground.  That
+                is, no extra layers are created, and the slideshow runs
+                potentially faster.
+             */
+            bool                     mbDisableAnimationZOrder;
+        };
+
+        typedef ::boost::shared_ptr< LayerManager > LayerManagerSharedPtr;
+    }
+}
+
+#endif /* INCLUDED_SLIDESHOW_LAYERMANAGER_HXX */
