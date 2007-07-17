fe7cbec4a04f450af78441129d457fe13470daec
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:54:45 +0000

    INTEGRATION: CWS presfixes12 (1.1.2); FILE ADDED 2007/05/02 21:37:23 thb 1.1.2.4: #i37778# Changed calls to cppcanvas::Canvas::getTransformation() to ViewLayer::getTransformation() where appropriate (because that's more direct); added initial Slide rendering back in (this time optional) 2007/02/25 01:10:26 thb 1.1.2.3: #i37778# Cleared up error handling a lot: no longer quenching RuntimeExceptions; reporting assertions in the debug case; ViewLayer  now reports resized sprite (which needs re-render from all shapes); fixed missing subset area reduction for glyph-level animations; added return of resize state from  Layer::commitLayerBounds(); adapted unit tests to corrected behaviour 2007/01/29 14:53:39 thb 1.1.2.2: #i37778# Fixed gcc3.4 build breakages; merged in a few fixes from HEAD; added PCH stuff to new files 2007/01/29 14:02:09 thb 1.1.2.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

---
+++ slideshow/source/engine/shapes/viewappletshape.cxx
@@ -0,0 +1,307 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: viewappletshape.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:54:45 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
+
+// must be first
+#include <canvas/verbosetrace.hxx>
+#include <canvas/debug.hxx>
+
+#include <comphelper/anytostring.hxx>
+#include <cppuhelper/exc_hlp.hxx>
+
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/range/b2irange.hxx>
+#include <basegfx/tools/canvastools.hxx>
+
+#include <cppcanvas/spritecanvas.hxx>
+#include <canvas/canvastools.hxx>
+
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/lang/XMultiComponentFactory.hpp>
+#include <com/sun/star/rendering/XCanvas.hpp>
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/beans/PropertyValue.hpp>
+#include <com/sun/star/util/XCloseable.hpp>
+#include <com/sun/star/awt/WindowDescriptor.hpp>
+#include <com/sun/star/awt/XToolkit.hpp>
+#include <com/sun/star/awt/XWindow2.hpp>
+#include <com/sun/star/awt/XWindowPeer.hpp>
+#include <com/sun/star/awt/WindowAttribute.hpp>
+#include <com/sun/star/awt/VclWindowPeerAttribute.hpp>
+#include <com/sun/star/awt/PosSize.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/frame/XSynchronousFrameLoader.hpp>
+
+#include "viewappletshape.hxx"
+#include "tools.hxx"
+
+
+using namespace ::com::sun::star;
+
+namespace slideshow
+{
+    namespace internal
+    {
+        ViewAppletShape::ViewAppletShape( const ViewLayerSharedPtr&                       rViewLayer,
+                                          const uno::Reference< drawing::XShape >&        rxShape,
+                                          const ::rtl::OUString&                          rServiceName,
+                                          const char**                                    pPropCopyTable,
+                                          sal_Size                                        nNumPropEntries,
+                                          const uno::Reference< uno::XComponentContext >& rxContext ) :
+            mpViewLayer( rViewLayer ),
+            mxViewer(),
+            mxFrame(),
+            mxComponentContext( rxContext )
+        {
+            ENSURE_AND_THROW( rxShape.is(), "ViewAppletShape::ViewAppletShape(): Invalid Shape" );
+            ENSURE_AND_THROW( mpViewLayer, "ViewAppletShape::ViewAppletShape(): Invalid View" );
+            ENSURE_AND_THROW( mpViewLayer->getCanvas(), "ViewAppletShape::ViewAppletShape(): Invalid ViewLayer canvas" );
+            ENSURE_AND_THROW( mxComponentContext.is(), "ViewAppletShape::ViewAppletShape(): Invalid component context" );
+
+            uno::Reference<lang::XMultiComponentFactory> xFactory(
+                mxComponentContext->getServiceManager(),
+                uno::UNO_QUERY_THROW );
+
+            mxViewer.set( xFactory->createInstanceWithContext( rServiceName,
+                                                               mxComponentContext),
+                          uno::UNO_QUERY_THROW );
+
+            uno::Reference< beans::XPropertySet > xShapePropSet( rxShape,
+                                                                 uno::UNO_QUERY_THROW );
+            uno::Reference< beans::XPropertySet > mxViewerPropSet( mxViewer,
+                                                                   uno::UNO_QUERY_THROW );
+
+            // copy shape properties to applet viewer
+            ::rtl::OUString aPropName;
+            for( sal_Size i=0; i<nNumPropEntries; ++i )
+            {
+                aPropName = ::rtl::OUString::createFromAscii( pPropCopyTable[i] );
+                mxViewerPropSet->setPropertyValue( aPropName,
+                                                   xShapePropSet->getPropertyValue(
+                                                       aPropName ));
+            }
+        }
+
+        // ---------------------------------------------------------------------
+
+        ViewAppletShape::~ViewAppletShape()
+        {
+            try
+            {
+                endApplet();
+            }
+            catch (uno::Exception &)
+            {
+                OSL_ENSURE( false, rtl::OUStringToOString(
+                                comphelper::anyToString(
+                                    cppu::getCaughtException() ),
+                                RTL_TEXTENCODING_UTF8 ).getStr() );
+            }
+        }
+
+        // ---------------------------------------------------------------------
+
+        ViewLayerSharedPtr ViewAppletShape::getViewLayer() const
+        {
+            return mpViewLayer;
+        }
+
+        // ---------------------------------------------------------------------
+
+        bool ViewAppletShape::startApplet( const ::basegfx::B2DRectangle& rBounds )
+        {
+            ENSURE_AND_RETURN( mpViewLayer && mpViewLayer->getCanvas() && mpViewLayer->getCanvas()->getUNOCanvas().is(),
+                               "ViewAppletShape::startApplet(): Invalid or disposed view" );
+            try
+            {
+                ::cppcanvas::CanvasSharedPtr pCanvas = mpViewLayer->getCanvas();
+
+                uno::Reference< beans::XPropertySet > xPropSet( pCanvas->getUNOCanvas()->getDevice(),
+                                                                uno::UNO_QUERY_THROW );
+
+                uno::Reference< awt::XWindow2 > xParentWindow(
+                    xPropSet->getPropertyValue(
+                        ::rtl::OUString::createFromAscii( "Window" )),
+                    uno::UNO_QUERY_THROW );
+
+                uno::Reference<lang::XMultiComponentFactory> xFactory(
+                    mxComponentContext->getServiceManager() );
+
+                if( xFactory.is() )
+                {
+                    // create an awt window to contain the applet
+                    // ==========================================
+
+                    uno::Reference< awt::XToolkit > xToolkit(
+                        xFactory->createInstanceWithContext(
+                            ::rtl::OUString::createFromAscii( "com.sun.star.awt.Toolkit" ),
+                            mxComponentContext ),
+                        uno::UNO_QUERY_THROW );
+
+                    awt::WindowDescriptor aOwnWinDescriptor( awt::WindowClass_SIMPLE,
+                                                             ::rtl::OUString(),
+                                                             uno::Reference< awt::XWindowPeer >(xParentWindow,
+                                                                                                uno::UNO_QUERY_THROW),
+                                                             0,
+                                                             awt::Rectangle(),
+                                                             awt::WindowAttribute::SHOW
+                                                             | awt::VclWindowPeerAttribute::CLIPCHILDREN );
+
+                    uno::Reference< awt::XWindowPeer > xNewWinPeer(
+                        xToolkit->createWindow( aOwnWinDescriptor ));
+                    uno::Reference< awt::XWindow > xOwnWindow( xNewWinPeer,
+                                                               uno::UNO_QUERY_THROW );
+
+
+                    // create a frame, and load the applet into it
+                    // ===========================================
+
+                    mxFrame.set(
+                        xFactory->createInstanceWithContext(
+                            ::rtl::OUString::createFromAscii( "com.sun.star.frame.Frame" ),
+                            mxComponentContext ),
+                        uno::UNO_QUERY_THROW );
+
+                    mxFrame->initialize( xOwnWindow );
+
+                    uno::Reference < frame::XSynchronousFrameLoader > xLoader( mxViewer,
+                                                                               uno::UNO_QUERY_THROW );
+                    xLoader->load( uno::Sequence < beans::PropertyValue >(),
+                                   mxFrame );
+
+
+                    // resize surrounding window and applet to current shape size
+                    // ==========================================================
+
+                    ::basegfx::B2DRange aTmpRange;
+                    ::canvas::tools::calcTransformedRectBounds( aTmpRange,
+                                                                rBounds,
+                                                                mpViewLayer->getTransformation() );
+                    const ::basegfx::B2IRange& rPixelBounds(
+                        ::basegfx::unotools::b2ISurroundingRangeFromB2DRange( aTmpRange ));
+
+                    uno::Reference< awt::XWindow > xSurroundingWindow( mxFrame->getContainerWindow() );
+                    if( xSurroundingWindow.is() )
+                        xSurroundingWindow->setPosSize( static_cast<sal_Int32>(rPixelBounds.getMinX()),
+                                                        static_cast<sal_Int32>(rPixelBounds.getMinY()),
+                                                        static_cast<sal_Int32>(rPixelBounds.getWidth()),
+                                                        static_cast<sal_Int32>(rPixelBounds.getHeight()),
+                                                        awt::PosSize::POSSIZE );
+
+                    uno::Reference< awt::XWindow > xAppletWindow( mxFrame->getComponentWindow() );
+                    if( xAppletWindow.is() )
+                        xAppletWindow->setPosSize( 0, 0,
+                                                   static_cast<sal_Int32>(rPixelBounds.getWidth()),
+                                                   static_cast<sal_Int32>(rPixelBounds.getHeight()),
+                                                   awt::PosSize::POSSIZE );
+                }
+            }
+            catch (uno::Exception &)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // ---------------------------------------------------------------------
+
+        void ViewAppletShape::endApplet()
+        {
+            uno::Reference<util::XCloseable> xCloseable(
+                mxFrame,
+                uno::UNO_QUERY );
+
+            if( xCloseable.is() )
+            {
+                xCloseable->close( sal_True );
+                mxFrame.clear();
+            }
+        }
+
+        // ---------------------------------------------------------------------
+
+        bool ViewAppletShape::render( const ::basegfx::B2DRectangle& rBounds ) const
+        {
+            ::cppcanvas::CanvasSharedPtr pCanvas = mpViewLayer->getCanvas();
+
+            if( !pCanvas )
+                return false;
+
+            if( !mxFrame.is() )
+            {
+                // fill the shape background with black
+                fillRect( pCanvas,
+                          rBounds,
+                          0xFFFFFFFFU );
+            }
+
+            return true;
+        }
+
+        bool ViewAppletShape::resize( const ::basegfx::B2DRectangle& rBounds ) const
+        {
+            if( !mxFrame.is() )
+                return false;
+
+            ::basegfx::B2DRange aTmpRange;
+            ::canvas::tools::calcTransformedRectBounds( aTmpRange,
+                                                        rBounds,
+                                                        mpViewLayer->getTransformation() );
+            const ::basegfx::B2IRange& rPixelBounds(
+                ::basegfx::unotools::b2ISurroundingRangeFromB2DRange( aTmpRange ));
+
+            uno::Reference< awt::XWindow > xFrameWindow( mxFrame->getContainerWindow() );
+            if( xFrameWindow.is() )
+                xFrameWindow->setPosSize( static_cast<sal_Int32>(rPixelBounds.getMinX()),
+                                          static_cast<sal_Int32>(rPixelBounds.getMinY()),
+                                          static_cast<sal_Int32>(rPixelBounds.getWidth()),
+                                          static_cast<sal_Int32>(rPixelBounds.getHeight()),
+                                          awt::PosSize::POSSIZE );
+
+            uno::Reference< awt::XWindow > xAppletWindow( mxFrame->getComponentWindow() );
+            if( xAppletWindow.is() )
+                xAppletWindow->setPosSize( 0, 0,
+                                           static_cast<sal_Int32>(rPixelBounds.getWidth()),
+                                           static_cast<sal_Int32>(rPixelBounds.getHeight()),
+                                           awt::PosSize::POSSIZE );
+
+            return true;
+        }
+    }
+}
