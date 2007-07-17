6125a3fe8c722681716c525a6b26556754b936db
Oliver Bolte <obo@openoffice.org>
2007-07-17 14:20:53 +0000

    INTEGRATION: CWS presfixes12 (1.7.12); FILE MERGED 2007/01/29 14:02:33 thb 1.7.12.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

--- slideshow/source/inc/wakeupevent.hxx
+++ slideshow/source/inc/wakeupevent.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wakeupevent.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 16:08:36 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 15:20:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,7 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-#if ! defined(INCLUDED_SLIDESHOW_WAKEUPEVENT_HXX)
+#ifndef INCLUDED_SLIDESHOW_WAKEUPEVENT_HXX
 #define INCLUDED_SLIDESHOW_WAKEUPEVENT_HXX
 
 #include <canvas/elapsedtime.hxx>
