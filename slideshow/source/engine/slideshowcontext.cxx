e1fe9d8fce786fd9379b4684b2bff162f6e60685
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:40:38 +0000

    INTEGRATION: CWS presfixes12 (1.1.2); FILE ADDED 2007/03/11 00:17:56 thb 1.1.2.2: #i37778# Necessary adaptions for cppcanvas clip behaviour (extra setClip()  methods to clear clip, instead of empty polygon, which denotes everything clipped on canvas); removed cyclic references for intrinsically animated shapes (GIF and drawing layer scroll text); fixed a few coding style inconsistencies 2007/01/29 14:01:56 thb 1.1.2.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

---
+++ slideshow/source/engine/slideshowcontext.cxx
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: slideshowcontext.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:40:38 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uno/XComponentContext.hpp>
+
+#include "slideshowcontext.hxx"
+#include "subsettableshapemanager.hxx"
+#include "screenupdater.hxx"
+#include "eventqueue.hxx"
+#include "activitiesqueue.hxx"
+#include "usereventqueue.hxx"
+#include "eventmultiplexer.hxx"
+#include "unoviewcontainer.hxx"
+
+
+using namespace ::com::sun::star;
+
+namespace slideshow
+{
+namespace internal
+{
+
+SlideShowContext::SlideShowContext( SubsettableShapeManagerSharedPtr& rSubsettableShapeManager,
+                                    EventQueue&                       rEventQueue,
+                                    EventMultiplexer&                 rEventMultiplexer,
+                                    ScreenUpdater&                    rScreenUpdater,
+                                    ActivitiesQueue&                  rActivitiesQueue,
+                                    UserEventQueue&                   rUserEventQueue,
+                                    CursorManager&                    rCursorManager,
+                                    const UnoViewContainer&           rViewContainer,
+                                    const uno::Reference<
+                                          uno::XComponentContext>&    rComponentContext ) :
+        mpSubsettableShapeManager( rSubsettableShapeManager ),
+        mrEventQueue( rEventQueue ),
+        mrEventMultiplexer( rEventMultiplexer ),
+        mrScreenUpdater( rScreenUpdater ),
+        mrActivitiesQueue( rActivitiesQueue ),
+        mrUserEventQueue( rUserEventQueue ),
+        mrCursorManager( rCursorManager ),
+        mrViewContainer( rViewContainer ),
+        mxComponentContext( rComponentContext )
+    {}
+
+void SlideShowContext::dispose()
+{
+    mxComponentContext.clear();
+}
+
+} // namespace internal
+} // namespace slideshow
