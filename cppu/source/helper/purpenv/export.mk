c4402d2822bb8ff65a304d4a12ba1dc37436ccb7
Oliver Bolte <obo@openoffice.org>
2007-07-17 06:26:35 +0000

    INTEGRATION: CWS mingwport05 (1.2.6); FILE MERGED 2007/07/02 15:19:45 vg 1.2.6.1: #i78268# MinGW port efforts

--- cppu/source/helper/purpenv/export.mk
+++ cppu/source/helper/purpenv/export.mk
@@ -6,7 +6,10 @@ purpenv_helper_LIB := -l$(NAMEpurpenv_helper)
 
 .ELSE
 NAMEpurpenv_helper := purpenvhelper$(UDK_MAJOR)$(COMID)
-
+.IF "$(COM)"=="GCC"
+purpenv_helper_LIB := -l$(NAMEpurpenv_helper)
+.ELSE
 purpenv_helper_LIB := $(LIBPRE) i$(NAMEpurpenv_helper).lib
+.ENDIF
 
 .ENDIF
