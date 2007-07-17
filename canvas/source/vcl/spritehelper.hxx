d601d16d2ddcb62488c719f6c3b801d44ed4b1a3
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:26:50 +0000

    INTEGRATION: CWS presfixes12 (1.3.48); FILE MERGED 2007/02/20 22:23:09 thb 1.3.48.1: #i37778# Added XCanvas::clear() method throughout all implementations

--- canvas/source/vcl/spritehelper.hxx
+++ canvas/source/vcl/spritehelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: spritehelper.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:21:33 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:26:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -109,7 +109,6 @@ namespace vclcanvas
                      bool                       bBufferedUpdate ) const;
 
     private:
-        virtual void clearSurface();
         virtual ::basegfx::B2DPolyPolygon polyPolygonFromXPolyPolygon2D(
             ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XPolyPolygon2D >& xPoly ) const;
 
