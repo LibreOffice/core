11dee9e81b963bf95829f1f48e039594136171bf
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:22:06 +0000

    INTEGRATION: CWS presfixes12 (1.2.58); FILE MERGED 2007/02/20 22:23:07 thb 1.2.58.1: #i37778# Added XCanvas::clear() method throughout all implementations

--- canvas/source/cairo/cairo_spritehelper.hxx
+++ canvas/source/cairo/cairo_spritehelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cairo_spritehelper.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-02-28 10:39:02 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:22:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,7 +110,6 @@ namespace cairocanvas
                      bool                       bBufferedUpdate ) const;
 
     private:
-        virtual void clearSurface();
         virtual ::basegfx::B2DPolyPolygon polyPolygonFromXPolyPolygon2D(
             ::com::sun::star::uno::Reference< ::com::sun::star::rendering::XPolyPolygon2D >& xPoly ) const;
 
