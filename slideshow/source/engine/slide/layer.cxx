866ecda93005a6aed9813fb83a71623ba590db50
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:56:15 +0000

    INTEGRATION: CWS presfixes12 (1.1.2); FILE ADDED 2007/05/10 20:32:40 thb 1.1.2.7: #i37778# Reworked LayerManager::updateShapeLayers - now much better aligned with general shape update, and avoids superfluous rendering; made unit tests work again; passing down slide background repaint status to LayerManager; relaxed preconditions for Shape::getUpdateArea() - no longer requires views to work; now catching singular view matrix and using sensible default at API border 2007/03/05 13:47:11 thb 1.1.2.6: #i37778# Removed Activity::needsScreenUpdate() method - this is now handled by notifying ScreenUpdater explicitely; reorg of SlideShowImpl members, to align lifetime with mutual references; fixed (hopefully) last repaint issues with new LayerManager; added more tests 2007/02/25 01:10:27 thb 1.1.2.5: #i37778# Cleared up error handling a lot: no longer quenching RuntimeExceptions; reporting assertions in the debug case; ViewLayer  now reports resized sprite (which needs re-render from all shapes); fixed missing subset area reduction for glyph-level animations; added return of resize state from  Layer::commitLayerBounds(); adapted unit tests to corrected behaviour 2007/02/10 23:23:52 thb 1.1.2.4: #i37778# Renamed setMouseCursor() to setCursorShape() at View interface (name clash with API method otherwise); added LayerManager/Layer unit tests; fixed a bunch of bugs/glitches found during unit testing 2007/02/06 17:18:11 thb 1.1.2.3: #i37778# Moved clear() method from View to ViewLayer (also sprites need to be cleared); fixed a few more cases of local code style violations; removed redundant inline keywords; finished Layer/LayerManager rework (Layer now represents ViewLayers, shapes and rendering are fully under LayerManager control); made shape comparator reusable 2007/01/29 15:36:02 thb 1.1.2.2: #i37778# Fixed more Forte8 compiler breakages 2007/01/29 14:02:12 thb 1.1.2.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

---
+++ slideshow/source/engine/slide/layer.cxx
@@ -0,0 +1,359 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: layer.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:56:15 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_slideshow.hxx"
+
+// must be first
+#include <canvas/debug.hxx>
+
+#include <basegfx/range/b2drange.hxx>
+#include <basegfx/range/b1drange.hxx>
+#include <basegfx/range/b2dmultirange.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <basegfx/polygon/b2dpolypolygon.hxx>
+
+#include "layer.hxx"
+
+#include <boost/bind.hpp>
+
+
+using namespace ::com::sun::star;
+
+namespace slideshow
+{
+    namespace internal
+    {
+        Layer::Layer( const basegfx::B2DRange& rMaxLayerBounds,
+                      Dummy                     ) :
+            maViewEntries(),
+            maBounds(),
+            maNewBounds(),
+            maMaxBounds( rMaxLayerBounds ),
+            mbBoundsDirty(false),
+            mbBackgroundLayer(true),
+            mbClipSet(false)
+        {
+        }
+
+        Layer::Layer( const basegfx::B2DRange& rMaxLayerBounds ) :
+            maViewEntries(),
+            maBounds(),
+            maNewBounds(),
+            maMaxBounds( rMaxLayerBounds ),
+            mbBoundsDirty(false),
+            mbBackgroundLayer(false),
+            mbClipSet(false)
+        {
+        }
+
+        ViewLayerSharedPtr Layer::addView( const ViewSharedPtr& rNewView )
+        {
+            OSL_ASSERT( rNewView );
+
+            ViewEntryVector::iterator aIter;
+            const ViewEntryVector::iterator aEnd( maViewEntries.end() );
+            if( (aIter=std::find_if( maViewEntries.begin(),
+                                     aEnd,
+                                     boost::bind<bool>(
+                                         std::equal_to< ViewSharedPtr >(),
+                                         boost::bind( &ViewEntry::getView, _1 ),
+                                         boost::cref( rNewView )))) != aEnd )
+            {
+                // already added - just return existing layer
+                return aIter->mpViewLayer;
+
+            }
+
+            // not yet added - create new view layer
+            ViewLayerSharedPtr pNewLayer;
+            if( mbBackgroundLayer )
+                pNewLayer = rNewView;
+            else
+                pNewLayer = rNewView->createViewLayer(maBounds);
+
+            // add to local list
+            maViewEntries.push_back(
+                ViewEntry( rNewView,
+                           pNewLayer ));
+
+            return maViewEntries.back().mpViewLayer;
+        }
+
+        ViewLayerSharedPtr Layer::removeView( const ViewSharedPtr& rView )
+        {
+            OSL_ASSERT( rView );
+
+            ViewEntryVector::iterator       aIter;
+            const ViewEntryVector::iterator aEnd( maViewEntries.end() );
+            if( (aIter=std::remove_if( maViewEntries.begin(),
+                                       aEnd,
+                                       boost::bind<bool>(
+                                           std::equal_to< ViewSharedPtr >(),
+                                           boost::bind( &ViewEntry::getView, _1 ),
+                                           boost::cref( rView )))) == aEnd )
+            {
+                // View was not added/is already removed
+                return ViewLayerSharedPtr();
+            }
+
+            ViewLayerSharedPtr pRet( aIter->mpViewLayer );
+            maViewEntries.erase(aIter,aEnd);
+
+            return pRet;
+        }
+
+        void Layer::viewChanged( const ViewSharedPtr& rChangedView )
+        {
+            ViewEntryVector::iterator aIter;
+            const ViewEntryVector::iterator aEnd( maViewEntries.end() );
+            if( (aIter=std::find_if( maViewEntries.begin(),
+                                     aEnd,
+                                     boost::bind<bool>(
+                                         std::equal_to< ViewSharedPtr >(),
+                                         boost::bind( &ViewEntry::getView, _1 ),
+                                         boost::cref( rChangedView )))) !=
+                aEnd )
+            {
+                // adapt size of given ViewLayer - background layer
+                // resizes with view.
+                if( !mbBackgroundLayer )
+                    aIter->mpViewLayer->resize(maBounds);
+            }
+        }
+
+        void Layer::viewsChanged()
+        {
+            // adapt size of given ViewLayer - background layer
+            // resizes with view.
+            if( !mbBackgroundLayer )
+            {
+                std::for_each( maViewEntries.begin(),
+                               maViewEntries.end(),
+                               boost::bind( &ViewLayer::resize,
+                                            boost::bind( &ViewEntry::getViewLayer,
+                                                         _1 ),
+                                            boost::cref(maBounds)));
+            }
+        }
+
+        void Layer::setShapeViews( ShapeSharedPtr const& rShape ) const
+        {
+            rShape->clearAllViewLayers();
+
+            std::for_each( maViewEntries.begin(),
+                           maViewEntries.end(),
+                           boost::bind(&Shape::addViewLayer,
+                                       boost::cref(rShape),
+                                       boost::bind(&ViewEntry::getViewLayer,
+                                                   _1),
+                                       false ));
+        }
+
+        void Layer::setPriority( const ::basegfx::B1DRange& rPrioRange )
+        {
+            if( !mbBackgroundLayer )
+            {
+                std::for_each( maViewEntries.begin(),
+                               maViewEntries.end(),
+                               boost::bind( &ViewLayer::setPriority,
+                                            boost::bind( &ViewEntry::getViewLayer,
+                                                         _1 ),
+                                            boost::cref(rPrioRange)));
+            }
+        }
+
+        void Layer::addUpdateRange( ::basegfx::B2DRange const& rUpdateRange )
+        {
+            // TODO(Q1): move this to B2DMultiRange
+            if( !rUpdateRange.isEmpty() )
+                maUpdateAreas.addRange( rUpdateRange );
+        }
+
+        void Layer::updateBounds( ShapeSharedPtr const& rShape )
+        {
+            if( !mbBackgroundLayer )
+            {
+                if( !mbBoundsDirty )
+                    maNewBounds.reset();
+
+                maNewBounds.expand( rShape->getUpdateArea() );
+            }
+
+            mbBoundsDirty = true;
+        }
+
+        bool Layer::commitBounds()
+        {
+            mbBoundsDirty = false;
+
+            if( mbBackgroundLayer )
+                return false;
+
+            if( maNewBounds == maBounds )
+                return false;
+
+            maBounds = maNewBounds;
+            if( std::count_if( maViewEntries.begin(),
+                               maViewEntries.end(),
+                               boost::bind( &ViewLayer::resize,
+                                            boost::bind( &ViewEntry::getViewLayer,
+                                                         _1 ),
+                                            boost::cref(maBounds)) ) == 0 )
+            {
+                return false;
+            }
+
+            // layer content invalid, update areas have wrong
+            // coordinates/not sensible anymore.
+            clearUpdateRanges();
+
+            return true;
+        }
+
+        void Layer::clearUpdateRanges()
+        {
+            maUpdateAreas.reset();
+        }
+
+        void Layer::clearContent()
+        {
+            // clear content on all view layers
+            std::for_each( maViewEntries.begin(),
+                           maViewEntries.end(),
+                           boost::bind(
+                               &ViewLayer::clear,
+                               boost::bind(
+                                   &ViewEntry::getViewLayer,
+                                   _1)));
+
+            // layer content cleared, update areas are not sensible
+            // anymore.
+            clearUpdateRanges();
+        }
+
+        class LayerEndUpdate : private boost::noncopyable
+        {
+        public:
+            LayerEndUpdate( LayerSharedPtr const& rLayer ) :
+                mpLayer( rLayer )
+            {}
+
+            ~LayerEndUpdate() { if(mpLayer) mpLayer->endUpdate(); }
+
+            void dismiss() { mpLayer.reset(); }
+
+        private:
+            LayerSharedPtr mpLayer;
+        };
+
+        Layer::EndUpdater Layer::beginUpdate()
+        {
+            if( !maUpdateAreas.isEmpty() )
+            {
+                // perform proper layer update. That means, setup proper
+                // clipping, and render each shape that intersects with
+                // the calculated update area
+                ::basegfx::B2DPolyPolygon aClip( maUpdateAreas.getPolyPolygon() );
+
+                // actually, if there happen to be shapes with zero
+                // update area in the maUpdateAreas vector, the
+                // resulting clip polygon will be empty.
+                if( aClip.count() )
+                {
+                    // set clip to all view layers
+                    std::for_each( maViewEntries.begin(),
+                                   maViewEntries.end(),
+                                   boost::bind(
+                                       &ViewLayer::setClip,
+                                       boost::bind(
+                                           &ViewEntry::getViewLayer,
+                                           _1),
+                                       boost::cref(aClip)));
+
+                    // clear update area on all view layers
+                    std::for_each( maViewEntries.begin(),
+                                   maViewEntries.end(),
+                                   boost::bind(
+                                       &ViewLayer::clear,
+                                       boost::bind(
+                                           &ViewEntry::getViewLayer,
+                                           _1)));
+
+                    mbClipSet = true;
+                }
+            }
+
+            return EndUpdater(new LayerEndUpdate(shared_from_this()));
+        }
+
+        void Layer::endUpdate()
+        {
+            if( mbClipSet )
+            {
+                mbClipSet = false;
+
+                basegfx::B2DPolyPolygon aEmptyClip;
+                std::for_each( maViewEntries.begin(),
+                               maViewEntries.end(),
+                               boost::bind(
+                                   &ViewLayer::setClip,
+                                   boost::bind(
+                                       &ViewEntry::getViewLayer,
+                                       _1),
+                                   boost::cref(aEmptyClip)));
+            }
+
+            clearUpdateRanges();
+        }
+
+        bool Layer::isInsideUpdateArea( ShapeSharedPtr const& rShape ) const
+        {
+            return maUpdateAreas.overlaps( rShape->getUpdateArea() );
+        }
+
+        LayerSharedPtr Layer::createBackgroundLayer( const basegfx::B2DRange& rMaxLayerBounds )
+        {
+            return LayerSharedPtr(new Layer( rMaxLayerBounds,
+                                             BackgroundLayer ));
+        }
+
+        LayerSharedPtr Layer::createLayer( const basegfx::B2DRange& rMaxLayerBounds )
+        {
+            return LayerSharedPtr( new Layer( rMaxLayerBounds ) );
+        }
+
+    }
+}
