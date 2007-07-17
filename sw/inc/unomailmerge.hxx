9f0bd57880a587c55f142464dc805c517c967617
Oliver Bolte <obo@openoffice.org>
2007-07-17 12:05:22 +0000

    INTEGRATION: CWS basemodelrefactoring (1.6.714); FILE MERGED 2007/05/10 07:00:21 os 1.6.714.1: include functional added

--- sw/inc/unomailmerge.hxx
+++ sw/inc/unomailmerge.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unomailmerge.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:26:34 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 13:05:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,7 @@
 #ifndef _SFX_OBJSH_HXX
 #include <sfx2/objsh.hxx>   // SfxObjectShellRef
 #endif
-
+#include <functional>
 namespace com { namespace sun { namespace star {
 
     namespace sdbc {
