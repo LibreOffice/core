1c6e7bea32e6c3c2061dbeff05aa465c666ff815
Oliver Bolte <obo@openoffice.org>
2007-07-17 13:38:48 +0000

    INTEGRATION: CWS presfixes12 (1.1.2); FILE ADDED 2007/03/11 01:01:09 thb 1.1.2.3: #i37778# Made wait shape to update screen immediately, as it's otherwise invisible because of not staying past main animation loop commitScreenUpdates() 2007/01/30 16:43:49 thb 1.1.2.2: #i37778# Made view update/repaint/resize work again; swapped BackgroundShape parameters for correct mtf import 2007/01/29 14:01:55 thb 1.1.2.1: Issue number: #i37778#
    Larger slideshow refactoring. Wrote design and coding style manifest,
    and adapted the code to actually conform to this. In detail:
     - cleaned up ownership/disposable/weak_ptr story. removed hacks and
       explicit Disposable implementations, where workaround were available
     - removed object mutices, where superfluous
     - reworked EventMultiplexer (using templatized listener class now), added
       more events. EventMultiplexer now serves as a true blackboard
     - reworked directory structure: disjunct parts are now physically separated
       into directories, instantiation happens via factories & abstract interfaces
     - added CursorManager, to make setting mouse cursor less hackish
     - reworked DrawShape, to implement SeparateListener pattern
     - reworked IntrinsicAnimationActivity, to avoid cyclic references
     - modified hyperlink & shape cursor handling to communicate via
       EventMultiplexer
     - renamed & cleaned up files (presentation.cxx now named slideshowimpl.cxx,
       etc.)
     - added first version of the z-order fix to layer/layermanager
     - cleaned up include guards and include syntax

---
+++ slideshow/source/engine/screenupdater.cxx
@@ -0,0 +1,194 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: screenupdater.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-17 14:38:48 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "screenupdater.hxx"
+#include "listenercontainer.hxx"
+
+#include <boost/bind.hpp>
+#include <vector>
+#include <algorithm>
+
+namespace slideshow
+{
+namespace internal
+{
+    typedef std::vector<
+        std::pair<UnoViewSharedPtr,bool> > UpdateRequestVector;
+
+    struct ScreenUpdater::ImplScreenUpdater
+    {
+        /** List of registered ViewUpdaters, to consult for necessary
+            updates
+        */
+        ThreadUnsafeListenerContainer<
+            ViewUpdateSharedPtr,
+            std::vector<ViewUpdateSharedPtr> > maUpdaters;
+
+        /// Views that have been notified for update
+        UpdateRequestVector                    maViewUpdateRequests;
+
+        /// List of View. Used to issue screen updates on.
+        UnoViewContainer const&                mrViewContainer;
+
+        /// True, if a notifyUpdate() for all views has been issued.
+        bool                                   mbUpdateAllRequest;
+
+        /// True, if at least one notifyUpdate() call had bViewClobbered set
+        bool                                   mbViewClobbered;
+
+        explicit ImplScreenUpdater( UnoViewContainer const& rViewContainer ) :
+            maUpdaters(),
+            maViewUpdateRequests(),
+            mrViewContainer(rViewContainer),
+            mbUpdateAllRequest(false),
+            mbViewClobbered(false)
+        {}
+    };
+
+    ScreenUpdater::ScreenUpdater( UnoViewContainer const& rViewContainer ) :
+        mpImpl(new ImplScreenUpdater(rViewContainer) )
+    {
+    }
+
+    ScreenUpdater::~ScreenUpdater()
+    {
+        // outline because of pimpl
+    }
+
+    void ScreenUpdater::notifyUpdate()
+    {
+        mpImpl->mbUpdateAllRequest = true;
+    }
+
+    void ScreenUpdater::notifyUpdate( const UnoViewSharedPtr& rView,
+                                      bool                    bViewClobbered )
+    {
+        mpImpl->maViewUpdateRequests.push_back(
+            std::make_pair(rView, bViewClobbered) );
+
+        if( bViewClobbered )
+            mpImpl->mbViewClobbered = true;
+    }
+
+    void ScreenUpdater::commitUpdates()
+    {
+        // cases:
+        //
+        // (a) no update necessary at all
+        //
+        // (b) no ViewUpdate-generated update
+        //     I. update all views requested -> for_each( mrViewContainer )
+        //    II. update some views requested -> for_each( maViewUpdateRequests )
+        //
+        // (c) ViewUpdate-triggered update - update all views
+        //
+
+        // any ViewUpdate-triggered updates?
+        const bool bViewUpdatesNeeded(
+            mpImpl->maUpdaters.apply(
+                boost::mem_fn(&ViewUpdate::needsUpdate)) );
+
+        if( bViewUpdatesNeeded )
+        {
+            mpImpl->maUpdaters.applyAll(
+                boost::mem_fn((bool (ViewUpdate::*)())&ViewUpdate::update) );
+        }
+
+        if( bViewUpdatesNeeded ||
+            mpImpl->mbUpdateAllRequest )
+        {
+            // unconditionally update all views
+            std::for_each( mpImpl->mrViewContainer.begin(),
+                           mpImpl->mrViewContainer.end(),
+                           mpImpl->mbViewClobbered ?
+                           boost::mem_fn(&View::paintScreen) :
+                           boost::mem_fn(&View::updateScreen) );
+        }
+        else if( !mpImpl->maViewUpdateRequests.empty() )
+        {
+            // update notified views only
+            UpdateRequestVector::const_iterator aIter(
+                mpImpl->maViewUpdateRequests.begin() );
+            const UpdateRequestVector::const_iterator aEnd(
+                mpImpl->maViewUpdateRequests.end() );
+            while( aIter != aEnd )
+            {
+                // TODO(P1): this is O(n^2) in the number of views, if
+                // lots of views notify updates.
+                const UnoViewVector::const_iterator aEndOfViews(
+                    mpImpl->mrViewContainer.end() );
+                UnoViewVector::const_iterator aFoundView;
+                if( (aFoundView=std::find(mpImpl->mrViewContainer.begin(),
+                                          aEndOfViews,
+                                          aIter->first)) != aEndOfViews )
+                {
+                    if( aIter->second )
+                        (*aFoundView)->paintScreen(); // force-paint
+                    else
+                        (*aFoundView)->updateScreen(); // update changes only
+                }
+
+                ++aIter;
+            }
+        }
+
+        // done - clear requests
+        mpImpl->mbViewClobbered = false;
+        mpImpl->mbUpdateAllRequest = false;
+        UpdateRequestVector().swap( mpImpl->maViewUpdateRequests );
+    }
+
+    void ScreenUpdater::addViewUpdate( ViewUpdateSharedPtr const& rViewUpdate )
+    {
+        mpImpl->maUpdaters.add( rViewUpdate );
+    }
+
+    void ScreenUpdater::removeViewUpdate( ViewUpdateSharedPtr const& rViewUpdate )
+    {
+        mpImpl->maUpdaters.remove( rViewUpdate );
+    }
+
+    void ScreenUpdater::requestImmediateUpdate()
+    {
+        // TODO(F2): This will interfere with other updates, since it
+        // happens out-of-sync with main animation loop. Might cause
+        // artifacts.
+        std::for_each( mpImpl->mrViewContainer.begin(),
+                       mpImpl->mrViewContainer.end(),
+                       boost::mem_fn(&View::updateScreen) );
+    }
+
+} // namespace internal
+} // namespace slideshow
