34479b52941e3313bd27f18f0a48a26d1879710e
Oliver Bolte <obo@openoffice.org>
2007-07-17 12:46:33 +0000

    INTEGRATION: CWS basemodelrefactoring (1.70.104); FILE MERGED 2007/07/16 15:04:08 mba 1.70.104.3: RESYNC: (1.71-1.74); FILE MERGED 2007/05/09 00:48:29 mba 1.70.104.2: RESYNC: (1.70-1.71); FILE MERGED 2007/03/24 12:47:16 mba 1.70.104.1: #i75677#: separation of DocumentInfo and DocumentInfoObject

--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewsh.cxx,v $
  *
- *  $Revision: 1.74 $
+ *  $Revision: 1.75 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 12:31:49 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 13:46:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -113,7 +113,6 @@
 #include "arrdecl.hxx"
 #include <sfx2/docfac.hxx>
 #include "view.hrc"
-#include <sfx2/objuno.hxx>
 #include "sfxlocal.hrc"
 #include <sfx2/sfxbasecontroller.hxx>
 #include <sfx2/topfrm.hxx>
