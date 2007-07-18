0f4970cb52775c8dac499f42b4593b6ab59b4dbe
Oliver Bolte <obo@openoffice.org>
2007-07-18 12:24:58 +0000

    INTEGRATION: CWS swqbf91 (1.6.320); FILE MERGED 2007/06/21 12:53:26 as 1.6.320.1: #i73788# handle sfx terminate listener exlicit

--- framework/inc/general.h
+++ framework/inc/general.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: general.h,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 23:58:36 $
+ *  last change: $Author: obo $ $Date: 2007-07-18 13:24:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,37 +36,55 @@
 #ifndef __FRAMEWORK_GENERAL_H_
 #define __FRAMEWORK_GENERAL_H_
 
-//_________________________________________________________________________________________________________________
-//  includes
-//_________________________________________________________________________________________________________________
+//=============================================================================
+// includes
 
-#ifndef __FRAMEWORK_MACROS_GENERIC_HXX_
 #include <macros/generic.hxx>
-#endif
 
-/* include it to be able to define namespace alias :-) */
-#ifndef _COM_SUN_STAR_UNO_XINTERFACE_HPP_
+/* Normaly XInterface isnt used here ...
+   But we need it to be able to define namespace alias css to anything related to ::com::sun::star
+   :-) */
 #include <com/sun/star/uno/XInterface.hpp>
-#endif
 
-//_________________________________________________________________________________________________________________
-//  namespace
-//_________________________________________________________________________________________________________________
+#include <rtl/ustring.hxx>
 
-namespace framework{
+//=============================================================================
+// namespace
 
-//_________________________________________________________________________________________________________________
-//  some general defines for whole project
-//_________________________________________________________________________________________________________________
+//-----------------------------------------------------------------------------
+/** will make our code more readable if we can use such short name css instead
+    of typing ::com::sun::star everytimes.
 
-#define FEATUREDESCRIPTOR_LOADSTATE                         DECLARE_ASCII("loadFinishedOrCancelled")    /// status event mapped from load event of frame loader
-
-//_________________________________________________________________________________________________________________
-//  some general types for whole project
-//_________________________________________________________________________________________________________________
-
+    On the other side we had so many problems with "using namespace" so we dont use
+    it here any longer.
+ */
 namespace css = ::com::sun::star;
 
-}       //  namespace framework
+namespace framework {
 
-#endif  //  #ifndef __FRAMEWORK_GENERAL_H_
+//-----------------------------------------------------------------------------
+/** status event mapped from load event of frame loader
+
+    @todo think about me
+          should be moved to another more specific place.
+ */
+static const ::rtl::OUString FEATUREDESCRIPTOR_LOADSTATE = DECLARE_ASCII("loadFinishedOrCancelled");
+
+//-----------------------------------------------------------------------------
+/** Those macro is used to make it more clear where a synchronized block will start.
+    Because normal documentation code wont be recognized by some developers to be real
+    I need something where they are thinking about.
+
+    At least this macro will do nothing ... it's empty.
+    But it should make the code more clear .-))
+*/
+#define SYNCHRONIZED_START
+
+//-----------------------------------------------------------------------------
+/** Same then SYNCHRONIZED_START ... but instead it mark the end of such code block.
+*/
+#define SYNCHRONIZED_END
+
+} // namespace framework
+
+#endif // #ifndef __FRAMEWORK_GENERAL_H_
