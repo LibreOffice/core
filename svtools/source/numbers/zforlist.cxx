b16d6e6a64d36379fa92225f7bdae97ce1134934
Oliver Bolte <obo@openoffice.org>
2007-07-18 07:56:35 +0000

    INTEGRATION: CWS fwk67 (1.66.200); FILE MERGED 2007/06/22 11:08:45 cd 1.66.200.1: #i78471# Cleanup - Remove dead code: Mac OS System 9 and earlier

--- svtools/source/numbers/zforlist.cxx
+++ svtools/source/numbers/zforlist.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: zforlist.cxx,v $
  *
- *  $Revision: 1.68 $
+ *  $Revision: 1.69 $
  *
- *  last change: $Author: ihi $ $Date: 2007-07-10 15:19:20 $
+ *  last change: $Author: obo $ $Date: 2007-07-18 08:56:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -4352,8 +4352,6 @@ sal_Char NfCurrencyEntry::GetEuroSymbol( rtl_TextEncoding eTextEncoding )
 #elif UNX
 //          return '\xA4';      // #56121# 0xA4 waere korrekt fuer iso-8859-15
             return '\x80';      // aber Windoze-Code fuer die konvertierten TrueType-Fonts
-#elif MAC
-            return '\xDB';
 #else
 #error EuroSymbol is what?
             return '\x80';
