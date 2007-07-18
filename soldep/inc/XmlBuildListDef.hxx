ed6ffd596f4f273807ba2ac4c6452efd70f4ac5e
Oliver Bolte <obo@openoffice.org>
2007-07-18 06:32:42 +0000

    INTEGRATION: CWS bserver29 (1.1.2); FILE ADDED 2007/06/15 13:54:17 kz 1.1.2.1: #140478# waringfree unix

---
+++ soldep/inc/XmlBuildListDef.hxx
@@ -0,0 +1,20 @@
+#ifndef _XMLBUILDLISTDEF_HXX
+#define _XMLBUILDLISTDEF_HXX
+
+enum DepTypes
+{
+    mdsimple,
+    mdalways,
+    mdforce
+};
+
+#define DEP_MD_SIMPLE   1;
+#define DEP_MD_ALWAYS   2;
+#define DEP_MD_FORCE    4;
+
+static const char * DEP_MD_SIMPLE_STR = "md-simple";
+static const char * DEP_MD_ALWAYS_STR = "md-always";
+static const char * DEP_MD_FORCE_STR  = "md-force";
+static const char * XML_ALL  =  "all";
+
+#endif
