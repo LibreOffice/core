caf75522b5367b6d8f0319078a27b8ec4da5c150
Oliver Bolte <obo@openoffice.org>
2007-07-18 12:03:22 +0000

    INTEGRATION: CWS os100 (1.42.14); FILE MERGED 2007/07/10 13:42:55 os 1.42.14.1: #i65257# text append interfaces

--- offapi/com/sun/star/text/makefile.mk
+++ offapi/com/sun/star/text/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.42 $
+#   $Revision: 1.43 $
 #
-#   last change: $Author: hr $ $Date: 2007-06-27 13:34:30 $
+#   last change: $Author: obo $ $Date: 2007-07-18 13:03:22 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -260,7 +260,14 @@ IDLFILES=\
       XTextTablesSupplier.idl\
       XTextViewCursor.idl\
       XTextViewCursorSupplier.idl\
-      XWordCursor.idl
+    XWordCursor.idl \
+    XParagraphAppend.idl \
+    XTextPortionAppend.idl \
+    XTextContentAppend.idl \
+    XTextConvert.idl \
+    XTextAppendAndConvert.idl \
+    XTextAppend.idl 
+    
 
 # ------------------------------------------------------------------
 
