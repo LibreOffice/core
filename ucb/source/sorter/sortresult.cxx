af6042f90803b22dc3a61c5eeeadbae20df15e1b
Oliver Bolte <obo@openoffice.org>
2007-07-18 06:55:27 +0000

    INTEGRATION: CWS ucbfixes03 (1.16.42); FILE MERGED 2007/07/02 15:37:25 kso 1.16.42.1: #i79072# - Fixed memory leak in SortedResultSet::Move()

--- ucb/source/sorter/sortresult.cxx
+++ ucb/source/sorter/sortresult.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sortresult.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-21 17:43:51 $
+ *  last change: $Author: obo $ $Date: 2007-07-18 07:55:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1704,6 +1704,8 @@ void SortedResultSet::Move( long nPos, long nCount, long nOffset )
     {
         maO2S.Replace( (void*)pTmpArr[ i ], (sal_uInt32)( nTo+i ) );
     }
+
+    delete [] pTmpArr;
 }
 
 //--------------------------------------------------------------------------
