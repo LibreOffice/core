1a4df639e9b8213ae7a7a0f8e4f28d96ccec362d
Oliver Bolte <obo@openoffice.org>
2007-07-18 12:32:03 +0000

    INTEGRATION: CWS swqbf91 (1.1.2); FILE ADDED 2007/06/22 10:45:42 od 1.1.2.6: #i73788# release static reference of FinalThreadManager service hold 	 by <SwThreadJoiner> on office termination. 2007/06/22 08:37:25 od 1.1.2.5: #i73788# no static mutex on stack 2007/06/07 19:48:29 od 1.1.2.4: #i73788## further steps to the final implementation 2007/05/03 15:33:00 od 1.1.2.3: #i73788# further refactoring of non-blocking load of linked graphics 2007/04/13 08:53:44 od 1.1.2.2: #i73788# - adjusting documentation 2007/04/02 10:57:19 od 1.1.2.1: #i73788# - further redesign of non-blocking load of linked graphics

---
+++ sw/source/core/docnode/swthreadjoiner.cxx
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: swthreadjoiner.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2007-07-18 13:32:03 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#include "precompiled_sw.hxx"
+
+#ifndef _SWTHREADJOINER_HXX
+#include <swthreadjoiner.hxx>
+#endif
+
+#ifndef _FINALTHREADMANAGER_HXX
+#include <finalthreadmanager.hxx>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
+#include <comphelper/processfactory.hxx>
+#endif
+
+/** Testing
+
+    @author OD
+*/
+using namespace ::com::sun::star;
+
+uno::Reference< util::XJobManager > SwThreadJoiner::mpThreadJoiner;
+osl::Mutex* SwThreadJoiner::mpGetJoinerMutex = new osl::Mutex();
+
+uno::Reference< util::XJobManager >& SwThreadJoiner::GetThreadJoiner()
+{
+    osl::MutexGuard aGuard(*mpGetJoinerMutex);
+
+    if ( !mpThreadJoiner.is() )
+    {
+        mpThreadJoiner =
+            uno::Reference< util::XJobManager >(
+                ::comphelper::getProcessServiceFactory()->createInstance(
+                    ::rtl::OUString::createFromAscii("com.sun.star.util.JobManager" ) ),
+                uno::UNO_QUERY );
+    }
+
+    return mpThreadJoiner;
+}
+
+bool SwThreadJoiner::ExistsThreadJoiner()
+{
+    return (mpThreadJoiner.is());
+}
+
+void SwThreadJoiner::ReleaseThreadJoiner()
+{
+    mpThreadJoiner.clear();
+}
