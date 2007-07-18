3137d570a2ecd92d128264b055c0706a0a9f088f
Oliver Bolte <obo@openoffice.org>
2007-07-18 07:51:55 +0000

    INTEGRATION: CWS fwk67 (1.2.84); FILE MERGED 2007/06/22 11:08:42 cd 1.2.84.1: #i78471# Cleanup - Remove dead code: Mac OS System 9 and earlier

--- svtools/inc/svtools/nranges.hxx
+++ svtools/inc/svtools/nranges.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: nranges.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 19:29:19 $
+ *  last change: $Author: obo $ $Date: 2007-07-18 08:51:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,7 +64,7 @@
 
 #if SUPD<=383
 
-#if defined MAC || ( defined SOLARIS && defined X86 ) || ( defined SINIX && defined GCC ) || defined SCO
+#if ( defined SOLARIS && defined X86 ) || ( defined SINIX && defined GCC ) || defined SCO
 #define NUMTYPE_ARG int
 #else
 #define NUMTYPE_ARG NUMTYPE
