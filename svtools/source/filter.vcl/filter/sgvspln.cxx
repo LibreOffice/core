761d0d8f3396b83dc2609d2c4ca604f1556efaee
Oliver Bolte <obo@openoffice.org>
2007-07-18 07:55:09 +0000

    INTEGRATION: CWS fwk67 (1.11.218); FILE MERGED 2007/06/22 11:08:44 cd 1.11.218.1: #i78471# Cleanup - Remove dead code: Mac OS System 9 and earlier

--- svtools/source/filter.vcl/filter/sgvspln.cxx
+++ svtools/source/filter.vcl/filter/sgvspln.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sgvspln.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 14:53:19 $
+ *  last change: $Author: obo $ $Date: 2007-07-18 08:55:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,13 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 
-#ifdef MAC
-#include <mac_start.h>
 #include <math.h>
-#include <mac_end.h>
-#else
-#include <math.h>
-#endif
 
 
 #ifndef _TL_POLY_HXX
