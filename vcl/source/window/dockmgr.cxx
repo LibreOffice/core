f351a9c1a07ad7b7847c9c45054e18b57a4e6676
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:19:42 +0000

    INTEGRATION: CWS mergesvp (1.22.2); FILE MERGED 2007/07/04 15:32:39 pl 1.22.2.1: #i79023# header moving

--- vcl/source/window/dockmgr.cxx
+++ vcl/source/window/dockmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dockmgr.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:30:40 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:19:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@
 #include <vcl/event.hxx>
 #endif
 #ifndef _SV_BRDWIN_HXX
-#include <brdwin.hxx>
+#include <vcl/brdwin.hxx>
 #endif
 #ifndef _SV_FLOATWIN_HXX
 #include <vcl/floatwin.hxx>
@@ -73,7 +73,7 @@
 #include <vcl/lineinfo.hxx>
 #endif
 #ifndef _SV_WINDOW_H
-#include <window.h>
+#include <vcl/window.h>
 #endif
 #include <vcl/unowrap.hxx>
 #include <vcl/salframe.hxx>
