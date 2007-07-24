877d77abf7bd085ceb65b67702b0d4afde4586eb
Rüdiger Timm <rt@openoffice.org>
2007-07-24 11:08:19 +0000

    INTEGRATION: CWS dba23b (1.6.282); FILE MERGED 2007/07/04 11:48:28 fs 1.6.282.2: #i10000# 2007/07/04 10:34:29 fs 1.6.282.1: #i78733# managing data sources now done by an external process

--- dbaccess/source/ui/dlg/dsselect.hxx
+++ dbaccess/source/ui/dlg/dsselect.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dsselect.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:03:18 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 12:08:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,30 +36,18 @@
 #ifndef _DBAUI_DSSELECT_HXX_
 #define _DBAUI_DSSELECT_HXX_
 
-#ifndef _DBAUI_DSNTYPES_HXX_
 #include "dsntypes.hxx"
-#endif
-#ifndef _SV_DIALOG_HXX
-#include <vcl/dialog.hxx>
-#endif
-#ifndef _SV_LSTBOX_HXX
-#include <vcl/lstbox.hxx>
-#endif
-#ifndef _SV_BUTTON_HXX
-#include <vcl/button.hxx>
-#endif
-#ifndef _SV_GROUP_HXX
-#include <vcl/group.hxx>
-#endif
-#ifndef _SV_FIXED_HXX
-#include <vcl/fixed.hxx>
-#endif
-#ifndef _RTL_USTRING_HXX_
-#include <rtl/ustring.hxx>
-#endif
-#ifndef _DBAUI_COMMON_TYPES_HXX_
+#include "odbcconfig.hxx"
 #include "commontypes.hxx"
-#endif
+
+#include <vcl/dialog.hxx>
+#include <vcl/lstbox.hxx>
+#include <vcl/button.hxx>
+#include <vcl/group.hxx>
+#include <vcl/fixed.hxx>
+#include <rtl/ustring.hxx>
+
+#include <memory>
 
 class SfxItemSet;
 //.........................................................................
@@ -78,19 +66,31 @@ protected:
     OKButton        m_aOk;
     CancelButton    m_aCancel;
     HelpButton      m_aHelp;
+#ifdef HAVE_ODBC_ADMINISTRATION
     PushButton      m_aManageDatasources;
+#endif
     PushButton      m_aCreateAdabasDB;
     SfxItemSet*     m_pOutputSet;
+#ifdef HAVE_ODBC_ADMINISTRATION
+    ::std::auto_ptr< OOdbcManagement >
+                    m_pODBCManagement;
+#endif
 
 public:
-    ODatasourceSelectDialog(Window* _pParent, const StringBag& _rDatasources, DATASOURCE_TYPE _eType,SfxItemSet* _pOutputSet = NULL);
+    ODatasourceSelectDialog( Window* _pParent, const StringBag& _rDatasources, DATASOURCE_TYPE _eType,SfxItemSet* _pOutputSet = NULL );
+    ~ODatasourceSelectDialog();
 
     inline String   GetSelected() const { return m_aDatasource.GetSelectEntry();}
     void            Select( const String& _rEntry ) { m_aDatasource.SelectEntry(_rEntry); }
 
+    virtual BOOL    Close();
+
 protected:
     DECL_LINK( ListDblClickHdl, ListBox * );
+#ifdef HAVE_ODBC_ADMINISTRATION
     DECL_LINK( ManageClickHdl, PushButton * );
+    DECL_LINK( ManageProcessFinished, void* );
+#endif
     DECL_LINK( CreateDBClickHdl, PushButton * );
     void fillListBox(const StringBag& _rDatasources);
 };
