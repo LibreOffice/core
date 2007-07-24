423fb12030184c0c199b2565b1ba0415a419d7a0
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:15:36 +0000

    INTEGRATION: CWS mergesvp (1.8.2); FILE MERGED 2007/07/04 15:32:38 pl 1.8.2.1: #i79023# header moving

--- vcl/source/helper/canvasbitmap.cxx
+++ vcl/source/helper/canvasbitmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: canvasbitmap.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:26:04 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:15:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 #include <vos/mutex.hxx>
 #endif
 
-#include <canvasbitmap.hxx>
+#include <vcl/canvasbitmap.hxx>
 #include <vcl/canvastools.hxx>
 #include <vcl/bmpacc.hxx>
 #include <vcl/svapp.hxx>
