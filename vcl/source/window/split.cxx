3d3818700bda86e9cbb865a868bece583c1226f4
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:21:35 +0000

    INTEGRATION: CWS mergesvp (1.19.2); FILE MERGED 2007/07/04 15:32:40 pl 1.19.2.1: #i79023# header moving

--- vcl/source/window/split.cxx
+++ vcl/source/window/split.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: split.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 09:58:41 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:21:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -67,7 +67,7 @@
 #include <rtl/instance.hxx>
 #endif
 #ifndef _SV_WINDOW_H
-#include <window.h>
+#include <vcl/window.h>
 #endif
 
 namespace
