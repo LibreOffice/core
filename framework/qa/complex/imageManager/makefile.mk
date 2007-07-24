f326838c76785e1a1bc04aee16f7682268c31b1f
Rüdiger Timm <rt@openoffice.org>
2007-07-24 12:27:28 +0000

    INTEGRATION: CWS complextest1 (1.6.48); FILE MERGED 2007/07/16 13:20:03 sg 1.6.48.1: #i78662#fixed makefile bug

--- framework/qa/complex/imageManager/makefile.mk
+++ framework/qa/complex/imageManager/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: ihi $ $Date: 2007-03-26 12:19:56 $
+#   last change: $Author: rt $ $Date: 2007-07-24 13:27:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -82,6 +82,6 @@ CT_APP      = org.openoffice.Runner
 RUN: run
 
 run:
-    java -cp $(CLASSPATH) $(CT_APP) $(CT_TESTBASE) $(CT_APPEXECCOMMAND) $(CT_PACKAGE).CheckImageManager
+    +java -cp $(CLASSPATH) $(CT_APP) $(CT_TESTBASE) $(CT_APPEXECCOMMAND) $(CT_PACKAGE).CheckImageManager
 
 
