a1c6c755f8025267ed7565217eeeb827d8ca755d
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:33:22 +0000

    INTEGRATION: CWS mergesvp (1.18.2); FILE MERGED 2007/07/04 15:32:41 pl 1.18.2.1: #i79023# header moving

--- vcl/win/inc/salframe.h
+++ vcl/win/inc/salframe.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salframe.h,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:53:15 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:33:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #endif
 
 #ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
+#include <vcl/sysdata.hxx>
 #endif
 
 #ifndef _SV_SALFRAME_HXX
