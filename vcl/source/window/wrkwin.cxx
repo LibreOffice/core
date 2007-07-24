c951285c0f048164f70f168c930087e353bf2e67
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:23:59 +0000

    INTEGRATION: CWS mergesvp (1.19.2); FILE MERGED 2007/07/04 15:32:41 pl 1.19.2.1: #i79023# header moving

--- vcl/source/window/wrkwin.cxx
+++ vcl/source/window/wrkwin.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrkwin.cxx,v $
  *
- *  $Revision: 1.19 $
+ *  $Revision: 1.20 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:36:45 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:23:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,10 +57,10 @@
 #include <vcl/svapp.hxx>
 #endif
 #ifndef _SV_BRDWIN_HXX
-#include <brdwin.hxx>
+#include <vcl/brdwin.hxx>
 #endif
 #ifndef _SV_WINDOW_H
-#include <window.h>
+#include <vcl/window.h>
 #endif
 #ifndef _SV_WRKWIN_HXX
 #include <vcl/wrkwin.hxx>
@@ -69,7 +69,7 @@
 #include <vcl/opengl.hxx>
 #endif
 #ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
+#include <vcl/sysdata.hxx>
 #endif
 
 // =======================================================================
