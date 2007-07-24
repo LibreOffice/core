c624ad4033828e5c659e99d406dcd946212a9fac
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:34:15 +0000

    INTEGRATION: CWS mergesvp (1.12.2); FILE MERGED 2007/07/04 15:32:42 pl 1.12.2.1: #i79023# header moving

--- vcl/win/source/window/salmenu.cxx
+++ vcl/win/source/window/salmenu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salmenu.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:58:28 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:34:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,13 +59,13 @@
 #include <vcl/menu.hxx>
 #endif
 #ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
+#include <vcl/sysdata.hxx>
 #endif
 #ifndef _SV_SALFRAME_H
 #include <salframe.h>
 #endif
 #ifndef _SV_IMPBMP_HXX
-#include <impbmp.hxx>
+#include <vcl/impbmp.hxx>
 #endif
 
 // uncomment the following line to have ownerdrawn menues, ie, with bitmaps
