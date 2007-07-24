44cbad96db0f2f5b458acf6e8256f2cf30d79682
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:30:26 +0000

    INTEGRATION: CWS mergesvp (1.27.2); FILE MERGED 2007/07/04 15:32:43 pl 1.27.2.1: #i79023# header moving

--- vcl/unx/inc/plugins/gtk/gtkframe.hxx
+++ vcl/unx/inc/plugins/gtk/gtkframe.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gtkframe.hxx,v $
  *
- *  $Revision: 1.27 $
+ *  $Revision: 1.28 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:44:49 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:30:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 #include <vcl/salframe.hxx>
 #endif
 #ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
+#include <vcl/sysdata.hxx>
 #endif
 
 #include <list>
