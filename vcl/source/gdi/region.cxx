8721c3ac571562274a108d617638c68489f20cbc
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:14:26 +0000

    INTEGRATION: CWS mergesvp (1.13.2); FILE MERGED 2007/07/04 15:32:38 pl 1.13.2.1: #i79023# header moving

--- vcl/source/gdi/region.cxx
+++ vcl/source/gdi/region.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: region.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:23:01 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:14:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@
 #include <tools/debug.hxx>
 #endif
 #ifndef _REGION_H
-#include <region.h>
+#include <vcl/region.h>
 #endif
 #ifndef _REGION_HXX
 #include <vcl/region.hxx>
