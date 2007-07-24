d8446ff2641862e441fa632dd5e1b4ba42db7b89
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:11:14 +0000

    INTEGRATION: CWS mergesvp (1.13.2); FILE MERGED 2007/07/04 15:32:36 pl 1.13.2.1: #i79023# header moving

--- vcl/source/gdi/graph.cxx
+++ vcl/source/gdi/graph.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: graph.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:13:35 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:09:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_vcl.hxx"
 
 #ifndef _SV_IMPGRAPH_HXX
-#include <impgraph.hxx>
+#include <vcl/impgraph.hxx>
 #endif
 #ifndef _SV_OUTDEV_HXX
 #include <vcl/outdev.hxx>
--- vcl/source/gdi/impprn.cxx
+++ vcl/source/gdi/impprn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impprn.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:16:36 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:11:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@
 #include <vcl/timer.hxx>
 #endif
 #ifndef _SV_IMPPRN_HXX
-#include <impprn.hxx>
+#include <vcl/impprn.hxx>
 #endif
 
 // -----------
