db150fc7bb8f728d95856c436bc1a50af4f93c16
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:29:59 +0000

    INTEGRATION: CWS mergesvp (1.6.2); FILE MERGED 2007/07/04 15:32:42 pl 1.6.2.1: #i79023# header moving

--- vcl/unx/inc/salobj.h
+++ vcl/unx/inc/salobj.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salobj.h,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:42:00 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:29:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <vcl/sv.h>
 #endif
 #ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
+#include <vcl/sysdata.hxx>
 #endif
 #ifndef _SV_SALOBJ_HXX
 #include <vcl/salobj.hxx>
