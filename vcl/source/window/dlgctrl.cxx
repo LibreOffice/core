2e96cfefa5c5c65eeeb86f84d49ec40e7fa56ae3
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:19:14 +0000

    INTEGRATION: CWS mergesvp (1.24.2); FILE MERGED 2007/07/04 15:32:39 pl 1.24.2.1: #i79023# header moving

--- vcl/source/window/dlgctrl.cxx
+++ vcl/source/window/dlgctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dlgctrl.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:29:34 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:19:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@
 #include <vcl/button.hxx>
 #endif
 #ifndef _SV_WINDOW_H
-#include <window.h>
+#include <vcl/window.h>
 #endif
 
 #include <vcl/unohelp.hxx>
