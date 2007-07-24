ffe97af7e344ca67c386e59549146e05e41df064
Rüdiger Timm <rt@openoffice.org>
2007-07-24 09:04:56 +0000

    INTEGRATION: CWS mergesvp (1.8.2); FILE MERGED 2007/07/04 15:32:35 pl 1.8.2.1: #i79023# header moving

--- vcl/source/app/stdtext.cxx
+++ vcl/source/app/stdtext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stdtext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 20:02:22 $
+ *  last change: $Author: rt $ $Date: 2007-07-24 10:04:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include "precompiled_vcl.hxx"
 
 #ifndef _SV_SVIDS_HRC
-#include <svids.hrc>
+#include <vcl/svids.hrc>
 #endif
 #ifndef _SV_SVDATA_HXX
 #include <vcl/svdata.hxx>
