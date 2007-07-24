c3416f88350d2e437c1eb017a232d9605844e0ef
Rüdiger Timm <rt@openoffice.org>
2007-07-24 12:12:11 +0000

    INTEGRATION: CWS complextest1 (1.5.160); FILE MERGED 2007/06/28 16:12:57 cn 1.5.160.1: #i78349# fix bug in makefile

--- sw/qa/unoapi/makefile.mk
+++ sw/qa/unoapi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:25:01 $
+#   last change: $Author: rt $ $Date: 2007-07-24 13:12:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,5 +37,11 @@ PRJ=..$/..
 PRJNAME=sw
 TARGET=qa_unoapi
 
-ALLTAR:
-    $(SOLARENV)$/bin$/checkapi -sce sw.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
+.INCLUDE: settings.mk
+
+.INCLUDE: target.mk
+
+ALLTAR : UNOAPI_TEST
+
+UNOAPI_TEST:
+    +$(SOLARENV)$/bin$/checkapi -sce sw.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
