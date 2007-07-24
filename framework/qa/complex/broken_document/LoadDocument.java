a30b950075b7d5c7843b1e84636c23a09d277096
Rüdiger Timm <rt@openoffice.org>
2007-07-24 12:25:20 +0000

    INTEGRATION: CWS complextest1 (1.1.2); FILE ADDED 2007/06/22 14:23:25 sg 1.1.2.1: #i78662# distributed complex tests for framework

---
+++ framework/qa/complex/broken_document/LoadDocument.java
@@ -0,0 +1,137 @@
+/*************************************************************************
+ *
+ *  $RCSfile: LoadDocument.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Date: 2007-07-24 13:25:20 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+package complex.broken_document;
+
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.frame.FrameSearchFlag;
+import com.sun.star.frame.XComponentLoader;
+import com.sun.star.frame.XFrame;
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.uno.UnoRuntime;
+import complexlib.ComplexTestCase;
+
+/**
+ * Check, if message boxes appear when the Office is in "headless" mode. Tests
+ * bug i15809. This test uses the broken document dbf.dbf.emf.
+ */
+public class LoadDocument extends ComplexTestCase {
+
+    /** defect file to load **/
+    private final String mFileName = "dbf.dbf.emf";
+
+    /**
+     * Get all test methods.
+     * @return The test methods.
+     */
+    public String[] getTestMethodNames() {
+        return new String[]{"checkHeadlessState"};
+    }
+
+    /**
+     * Start Office with "-headless" parameter, then
+     * load the broken document "dbf.dbf.emf", that brings a message box up in
+     * the ui, see if the headless mode of SOffice changes.
+     */
+    public void checkHeadlessState() {
+        XMultiServiceFactory xMSF = (XMultiServiceFactory)param.getMSF();
+        XFrame xDesktop = null;
+
+        try {
+            xDesktop = (XFrame)UnoRuntime.queryInterface(XFrame.class,
+                        xMSF.createInstance("com.sun.star.frame.Desktop"));
+        }
+        catch(com.sun.star.uno.Exception e) {
+            failed("Could not create a desktop instance.");
+        }
+
+        XComponentLoader xDesktopLoader = (XComponentLoader)
+                UnoRuntime.queryInterface(XComponentLoader.class, xDesktop);
+        System.out.println("xDektopLoader is null: " + (xDesktopLoader == null));
+        PropertyValue[] val = new PropertyValue[0];
+
+        String workingDir = (String)param.get("WorkingDir") + System.getProperty("file.separator") + mFileName;
+        System.out.println("Working dir: " + workingDir);
+        String fileUrl = util.utils.getFullURL(workingDir);
+        System.out.println("File Url: " + fileUrl);
+
+        try {
+            xDesktopLoader.loadComponentFromURL(fileUrl, "_blank", 0, val);
+        }
+        catch(com.sun.star.io.IOException e) {
+            failed("Could not load document");
+        }
+        catch(com.sun.star.lang.IllegalArgumentException e) {
+            failed("Could not load document");
+        }
+
+        // try again: headless mode defect now?
+        try {
+            xDesktopLoader.loadComponentFromURL(fileUrl, "_blank", 0, val);
+        }
+        catch(com.sun.star.io.IOException e) {
+            failed("Could not load document");
+        }
+        catch(com.sun.star.lang.IllegalArgumentException e) {
+            failed("Could not load document");
+        }
+
+    }
+}
