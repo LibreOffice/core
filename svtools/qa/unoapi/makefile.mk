8782c4f5f0ed59b984926966dcc30e6afc67435f
Rüdiger Timm <rt@openoffice.org>
2007-07-24 12:30:05 +0000

    INTEGRATION: CWS complextest1 (1.4.116); FILE MERGED 2007/06/28 16:07:39 cn 1.4.116.1: #i78349# fix bug in makefile

--- svtools/qa/unoapi/makefile.mk
+++ svtools/qa/unoapi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 13:06:58 $
+#   last change: $Author: rt $ $Date: 2007-07-24 13:30:05 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -38,5 +38,11 @@ PRJ=..$/..
 PRJNAME=svtools
 TARGET=qa_unoapi
 
-ALLTAR:
-    $(SOLARENV)$/bin$/checkapi -sce svtools.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
+.INCLUDE: settings.mk
+
+.INCLUDE: target.mk
+
+ALLTAR : UNOAPI_TEST
+
+UNOAPI_TEST:
+    +$(SOLARENV)$/bin$/checkapi -sce svtools.sce -xcl knownissues.xcl -tdoc $(PWD)$/testdocuments
