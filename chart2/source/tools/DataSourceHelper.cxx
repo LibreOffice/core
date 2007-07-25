d388baf7840801eb52305f91af3a2c62c6a2ada3
Rüdiger Timm <rt@openoffice.org>
2007-07-25 07:56:38 +0000

    INTEGRATION: CWS chart07 (1.2.12); FILE MERGED 2007/07/10 12:56:07 bm 1.2.12.1: #i69281# warnings removed

--- chart2/source/tools/DataSourceHelper.cxx
+++ chart2/source/tools/DataSourceHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataSourceHelper.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 18:58:02 $
+ *  last change: $Author: rt $ $Date: 2007-07-25 08:56:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -338,6 +338,7 @@ bool DataSourceHelper::detectRangeSegmentation(
         return bSomethingDetected;
 
     OSL_ASSERT( !bIncludeUnusedData ); //bIncludeUnusedData is not supported currently
+    (void)(bIncludeUnusedData); // avoid warning in non-debug build
 
     try
     {
