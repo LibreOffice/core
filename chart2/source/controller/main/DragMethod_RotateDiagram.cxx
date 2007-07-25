ec8238c6ea3a6bbdabccb232a200326d5f50fd25
Rüdiger Timm <rt@openoffice.org>
2007-07-25 07:45:29 +0000

    INTEGRATION: CWS chart07 (1.3.4); FILE MERGED 2007/07/10 13:38:14 bm 1.3.4.1: #i69281# warnings removed

--- chart2/source/controller/main/DragMethod_RotateDiagram.cxx
+++ chart2/source/controller/main/DragMethod_RotateDiagram.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DragMethod_RotateDiagram.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-03 13:40:41 $
+ *  last change: $Author: rt $ $Date: 2007-07-25 08:45:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -215,7 +215,7 @@ void DragMethod_RotateDiagram::CreateOverlayGeometry(::sdr::overlay::OverlayMana
     }
 }
 
-FASTBOOL DragMethod_RotateDiagram::End(FASTBOOL bCopy)
+FASTBOOL DragMethod_RotateDiagram::End(FASTBOOL /* bCopy */)
 {
     Hide();
 
