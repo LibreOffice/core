6234589d26b8560d7b00c040470d3cab2913146e
Rüdiger Timm <rt@openoffice.org>
2007-07-25 07:36:32 +0000

    INTEGRATION: CWS chart07 (1.7.12); FILE MERGED 2007/07/13 11:17:12 iha 1.7.12.1: cleanup data label controls (one implementation only)

--- chart2/source/controller/dialogs/tp_DataLabel.cxx
+++ chart2/source/controller/dialogs/tp_DataLabel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tp_DataLabel.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 17:44:44 $
+ *  last change: $Author: rt $ $Date: 2007-07-25 08:36:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,168 +36,45 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "tp_DataLabel.hxx"
+
 #include "TabPages.hrc"
 #include "ResId.hxx"
-#include "chartview/ChartSfxItemIds.hxx"
-#include "NoWarningThisInCTOR.hxx"
-
-#ifndef _SFXENUMITEM_HXX
-#include <svtools/eitem.hxx>
-#endif
-#ifndef _SVX_CHRTITEM_HXX
-#include <svx/chrtitem.hxx>
-#endif
 
 //.............................................................................
 namespace chart
 {
 //.............................................................................
 
-SchDataDescrTabPage::SchDataDescrTabPage(Window* pWindow,
-                                         const SfxItemSet& rInAttrs) :
+DataLabelsTabPage::DataLabelsTabPage(Window* pWindow, const SfxItemSet& rInAttrs) :
         SfxTabPage(pWindow, SchResId(TP_DATA_DESCR), rInAttrs),
-        aCbValue(this, SchResId(CB_VALUE)),
-        aRbNumber(this, SchResId(RB_NUMBER)),
-        aRbPercent(this, SchResId(RB_PERCENT)),
-        aCbText(this, SchResId(CB_TEXT)),
-        aCbSymbol(this, SchResId(CB_SYMBOL))
+        m_aDataLabelResources(this, rInAttrs)
 {
     FreeResource();
-
-    aCbValue.SetClickHdl( LINK( this, SchDataDescrTabPage, EnableHdl ));
-    aCbText.SetClickHdl(  LINK( this, SchDataDescrTabPage, EnableHdl ));
 }
 
-/*************************************************************************
-|*
-|* Dtor
-|*
-\************************************************************************/
-
-SchDataDescrTabPage::~SchDataDescrTabPage()
+DataLabelsTabPage::~DataLabelsTabPage()
 {
 }
 
-/*************************************************************************
-|*
-|*  Handler fuer ShowSym-Button enablen/disablen
-|*
-\*************************************************************************/
-
-IMPL_LINK( SchDataDescrTabPage, EnableHdl, CheckBox *, pControl )
-{
-    aCbSymbol.Enable( aCbValue.IsChecked() || aCbText.IsChecked() );
-    if( pControl == &aCbValue )
-    {
-        aRbPercent.Enable( aCbValue.IsChecked() );
-        aRbNumber.Enable( aCbValue.IsChecked() );
-    }
-
-    return 0;
-}
-
-/*************************************************************************
-|*
-|* Erzeugung
-|*
-\*************************************************************************/
-
-SfxTabPage* SchDataDescrTabPage::Create(Window* pWindow,
+SfxTabPage* DataLabelsTabPage::Create(Window* pWindow,
                                         const SfxItemSet& rOutAttrs)
 {
-    return new SchDataDescrTabPage(pWindow, rOutAttrs);
+    return new DataLabelsTabPage(pWindow, rOutAttrs);
 }
 
-/*************************************************************************
-|*
-|* Fuellt uebergebenen Item-Set mit Dialogbox-Attributen
-|*
-\*************************************************************************/
-
-BOOL SchDataDescrTabPage::FillItemSet(SfxItemSet& rOutAttrs)
+BOOL DataLabelsTabPage::FillItemSet(SfxItemSet& rOutAttrs)
 {
-    BOOL bText = aCbText.IsChecked();
-    SvxChartDataDescr eDescr;
-
-    if( aCbValue.IsChecked() )
-    {
-        if( aRbNumber.IsChecked() )
-            eDescr = (bText? CHDESCR_TEXTANDVALUE : CHDESCR_VALUE);
-        else
-            eDescr = (bText? CHDESCR_TEXTANDPERCENT : CHDESCR_PERCENT);
-    }
-    else if( aCbText.IsChecked() )
-        eDescr = CHDESCR_TEXT;
-    else
-        eDescr = CHDESCR_NONE;
-
-    rOutAttrs.Put(SvxChartDataDescrItem(eDescr, SCHATTR_DATADESCR_DESCR));
-    rOutAttrs.Put(SfxBoolItem( SCHATTR_DATADESCR_SHOW_SYM,
-                               aCbSymbol.IsChecked()) );
-    return TRUE;
+    return m_aDataLabelResources.FillItemSet(rOutAttrs);
 }
 
-/*************************************************************************
-|*
-|* Initialisierung
-|*
-\*************************************************************************/
-
-void SchDataDescrTabPage::Reset(const SfxItemSet& rInAttrs)
+void DataLabelsTabPage::Reset(const SfxItemSet& rInAttrs)
 {
-    const SfxPoolItem *pPoolItem = NULL;
+    m_aDataLabelResources.Reset(rInAttrs);
+}
 
-    // default state
-    aRbNumber.Enable( FALSE );
-    aRbPercent.Enable( FALSE );
-    aCbSymbol.Enable( FALSE );
-
-    if( rInAttrs.GetItemState(SCHATTR_DATADESCR_SHOW_SYM,
-                              TRUE, &pPoolItem) == SFX_ITEM_SET )
-        aCbSymbol.Check( ((const SfxBoolItem*)pPoolItem)->GetValue() );
-
-    if( rInAttrs.GetItemState(SCHATTR_DATADESCR_DESCR,
-                              TRUE, &pPoolItem) == SFX_ITEM_SET )
-    {
-        switch( ((const SvxChartDataDescrItem*)pPoolItem)->GetValue() )
-        {
-            case CHDESCR_VALUE:
-                aCbValue.Check();
-                aRbNumber.Check();
-                EnableHdl( &aCbValue );
-                break;
-
-            case CHDESCR_PERCENT:
-                aCbValue.Check();
-                aRbPercent.Check();
-                EnableHdl( &aCbValue );
-                break;
-
-            case CHDESCR_TEXT:
-                aCbText.Check();
-                EnableHdl( &aCbText );
-                break;
-
-            case CHDESCR_TEXTANDPERCENT:
-                aCbText.Check();
-                aCbValue.Check();
-                aRbPercent.Check();
-                EnableHdl( &aCbValue );
-                break;
-
-            case CHDESCR_TEXTANDVALUE:
-                aCbText.Check();
-                aCbValue.Check();
-                aRbNumber.Check();
-                EnableHdl( &aCbValue );
-                break;
-
-            default:
-                break;
-        }
-    }
-    if( !aRbNumber.IsChecked() && !aRbPercent.IsChecked() )
-        aRbNumber.Check();
+void DataLabelsTabPage::SetNumberFormatter( SvNumberFormatter* pFormatter )
+{
+    m_aDataLabelResources.SetNumberFormatter( pFormatter );
 }
 
 //.............................................................................
