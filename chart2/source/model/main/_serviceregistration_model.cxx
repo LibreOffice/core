f6326715ecdbbb3582b8546ca5c04ed187aeeb0e
Rüdiger Timm <rt@openoffice.org>
2007-07-25 07:52:12 +0000

    INTEGRATION: CWS chart07 (1.5.12); FILE MERGED 2007/06/25 12:52:12 bm 1.5.12.1: #i74653# store undo manager at model to be able to do an undo even after inplace re-activation

--- chart2/source/model/main/_serviceregistration_model.cxx
+++ chart2/source/model/main/_serviceregistration_model.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _serviceregistration_model.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 18:43:16 $
+ *  last change: $Author: rt $ $Date: 2007-07-25 08:52:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -190,7 +190,7 @@ extern "C"
 {
 //==================================================================================================
 void SAL_CALL component_getImplementationEnvironment(
-    const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv )
+    const sal_Char ** ppEnvTypeName, uno_Environment ** /* ppEnv */ )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
