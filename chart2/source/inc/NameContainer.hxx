12e7c0f959b875bf68ae48149f5d175629056fc1
Rüdiger Timm <rt@openoffice.org>
2007-07-25 07:46:57 +0000

    INTEGRATION: CWS chart07 (1.2.12); FILE MERGED 2007/07/10 14:42:15 bm 1.2.12.1: #i69281# warnings removed

--- chart2/source/inc/NameContainer.hxx
+++ chart2/source/inc/NameContainer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: NameContainer.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 18:19:47 $
+ *  last change: $Author: rt $ $Date: 2007-07-25 08:46:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,11 +60,16 @@ namespace chart
 {
 //.............................................................................
 
-class NameContainer : public ::cppu::WeakImplHelper3<
-          ::com::sun::star::container::XNameContainer
-        , ::com::sun::star::lang::XServiceInfo
-        , ::com::sun::star::util::XCloneable
-        >
+namespace impl
+{
+typedef ::cppu::WeakImplHelper3<
+        ::com::sun::star::container::XNameContainer,
+        ::com::sun::star::lang::XServiceInfo,
+        ::com::sun::star::util::XCloneable >
+    NameContainer_Base;
+}
+
+class NameContainer : public impl::NameContainer_Base
 {
 public:
     NameContainer( const ::com::sun::star::uno::Type& rType, const rtl::OUString& rServicename, const rtl::OUString& rImplementationName );
