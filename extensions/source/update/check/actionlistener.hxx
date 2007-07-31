aff0962505a5025e3aa15d292e6cb58704bc4396
Jens-Heiner Rechtien <hr@openoffice.org>
2007-07-31 14:55:39 +0000

    INTEGRATION: CWS updchk07 (1.2.2); FILE MERGED 2007/07/13 12:46:38 dv 1.2.2.1: #i79162# Set last state after checking failed

--- extensions/source/update/check/actionlistener.hxx
+++ extensions/source/update/check/actionlistener.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: actionlistener.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 14:35:45 $
+ *  last change: $Author: hr $ $Date: 2007-07-31 15:55:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,6 +49,7 @@ class IActionListener : public rtl::IReference
     virtual void            install() = 0;
     virtual void            pause() = 0;
     virtual void            resume() = 0;
+    virtual void            closeAfterFailure() = 0;
 };
 
 #endif //INCLUDED_ACTION_LISTENER_HXX
