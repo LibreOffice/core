272fc96721a5f3b624280307194ebf2618316df5
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-02 13:39:26 +0000

    INTEGRATION: CWS rpt23fix01 (1.2.2); FILE MERGED 2007/07/16 19:35:23 fs 1.2.2.1: #i77800# reportformula moved to ../../core/misc

--- reportdesign/source/ui/misc/makefile.mk
+++ reportdesign/source/ui/misc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-09 11:56:32 $
+#   last change: $Author: hr $ $Date: 2007-08-02 14:39:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,8 +53,7 @@ EXCEPTIONSFILES=	\
         $(SLO)$/ColorListener.obj			\
         $(SLO)$/UITools.obj					\
         $(SLO)$/rptuiservices.obj				\
-        $(SLO)$/toolboxcontroller.obj       \
-        $(SLO)$/reportformula.obj
+        $(SLO)$/toolboxcontroller.obj
 
 
 SLOFILES=	\
