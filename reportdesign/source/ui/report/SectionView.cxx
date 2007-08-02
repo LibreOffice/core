a8bb6b8ee372c2c76b16d55920d8d1af117f7ec2
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-02 13:41:03 +0000

    INTEGRATION: CWS rpt23fix01 (1.2.2); FILE MERGED 2007/07/11 09:03:20 oj 1.2.2.1: #i77271# disable editing

--- reportdesign/source/ui/report/SectionView.cxx
+++ reportdesign/source/ui/report/SectionView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SectionView.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:33 $
+ *  last change: $Author: hr $ $Date: 2007-08-02 14:41:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -78,6 +78,7 @@ OSectionView::OSectionView( SdrModel* pModel, OReportSection* _pSectionWindow, O
     SetBufferedOverlayAllowed(true);
     SetPageBorderVisible(false);
     SetBordVisible();
+    SetQuickTextEditMode(FALSE);
 }
 
 //----------------------------------------------------------------------------
