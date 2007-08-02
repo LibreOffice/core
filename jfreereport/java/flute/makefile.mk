30a225683f68e4e2643519358c581ad933fdd7a0
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-02 13:12:23 +0000

    INTEGRATION: CWS rpt23fix01 (1.2.2); FILE MERGED 2007/07/19 07:32:32 oj 1.2.2.4: #i79528# remove patch for this cws 2007/07/13 06:01:28 oj 1.2.2.3: #i79528# create new patch, the old one contains rev flags which were correct via cvs 2007/07/13 05:39:23 oj 1.2.2.2: #i79528# iml patch for flute 2007/07/12 14:00:10 oj 1.2.2.1: #i79528# iml patch for flute

--- jfreereport/java/flute/makefile.mk
+++ jfreereport/java/flute/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-06 16:02:21 $
+#   last change: $Author: hr $ $Date: 2007-08-02 14:12:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -54,7 +54,7 @@ TARFILE_NAME=$(TARGET)
 
 TARFILE_ROOTDIR=$(TARGET)
 
-CONVERTFILES=ant$/build.xml\
+CONVERTFILES=ant$/build.xml
 
 .IF "$(JAVACISGCJ)"=="yes"
 JAVA_HOME=
