c36741581ae6a4832fb6c0af5441553c9e2c9bd8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-03 12:07:42 +0000

    INTEGRATION: CWS chart10 (1.1.2); FILE ADDED 2007/07/27 12:59:08 iha 1.1.2.2: #i79849# prevent to many view updates of charts - also lock newly loaded charts 2007/07/26 14:29:26 iha 1.1.2.1: #i79849# prevent to many view updates of charts

---
+++ sc/source/core/tool/chartlock.cxx
@@ -0,0 +1,204 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: chartlock.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-08-03 13:07:42 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
+
+
+#include <vcl/svapp.hxx>
+
+#include "chartlock.hxx"
+#include "document.hxx"
+#include "drwlayer.hxx"
+#include <svx/svditer.hxx>
+#include <svx/svdoole2.hxx>
+
+using namespace com::sun::star;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::WeakReference;
+
+#define SC_CHARTLOCKTIMEOUT 660
+
+// ====================================================================
+
+namespace
+{
+
+std::vector< WeakReference< frame::XModel > > lcl_getAllLivingCharts( ScDocument* pDoc )
+{
+    std::vector< WeakReference< frame::XModel > > aRet;
+    if( !pDoc )
+        return aRet;
+    ScDrawLayer* pDrawLayer = pDoc->GetDrawLayer();
+    if (!pDrawLayer)
+        return aRet;
+
+    for (SCTAB nTab=0; nTab<=pDoc->GetMaxTableNumber(); nTab++)
+    {
+        if (pDoc->HasTable(nTab))
+        {
+            SdrPage* pPage = pDrawLayer->GetPage(static_cast<sal_uInt16>(nTab));
+            DBG_ASSERT(pPage,"Page ?");
+
+            SdrObjListIter aIter( *pPage, IM_DEEPNOGROUPS );
+            SdrObject* pObject = aIter.Next();
+            while (pObject)
+            {
+                if( pDoc->IsChart( pObject ) )
+                {
+                    uno::Reference< embed::XEmbeddedObject > xIPObj = ((SdrOle2Obj*)pObject)->GetObjRef();
+                    uno::Reference< embed::XComponentSupplier > xCompSupp( xIPObj, uno::UNO_QUERY );
+                    if( xCompSupp.is())
+                    {
+                        Reference< frame::XModel > xModel( xCompSupp->getComponent(), uno::UNO_QUERY );
+                        if( xModel.is() )
+                            aRet.push_back( xModel );
+                    }
+                }
+                pObject = aIter.Next();
+            }
+        }
+    }
+    return aRet;
+}
+
+}//end anonymous namespace
+
+// === ScChartLockGuard ======================================
+
+ScChartLockGuard::ScChartLockGuard( ScDocument* pDoc ) :
+    maChartModels( lcl_getAllLivingCharts( pDoc ) )
+{
+    std::vector< WeakReference< frame::XModel > >::const_iterator aIter = maChartModels.begin();
+    const std::vector< WeakReference< frame::XModel > >::const_iterator aEnd = maChartModels.end();
+    for( ; aIter != aEnd; ++aIter )
+    {
+        try
+        {
+            Reference< frame::XModel > xModel( *aIter );
+            if( xModel.is())
+                xModel->lockControllers();
+        }
+        catch ( uno::Exception& )
+        {
+            DBG_ERROR("Unexpected exception in ScChartLockGuard");
+        }
+    }
+}
+
+ScChartLockGuard::~ScChartLockGuard()
+{
+    std::vector< WeakReference< frame::XModel > >::const_iterator aIter = maChartModels.begin();
+    const std::vector< WeakReference< frame::XModel > >::const_iterator aEnd = maChartModels.end();
+    for( ; aIter != aEnd; ++aIter )
+    {
+        try
+        {
+            Reference< frame::XModel > xModel( *aIter );
+            if( xModel.is())
+                xModel->unlockControllers();
+        }
+        catch ( uno::Exception& )
+        {
+            DBG_ERROR("Unexpected exception in ScChartLockGuard");
+        }
+    }
+}
+
+void ScChartLockGuard::AlsoLockThisChart( const Reference< frame::XModel >& xModel )
+{
+    if(!xModel.is())
+        return;
+
+    WeakReference< frame::XModel > xWeakModel(xModel);
+
+    std::vector< WeakReference< frame::XModel > >::iterator aFindIter(
+            ::std::find( maChartModels.begin(), maChartModels.end(), xWeakModel ) );
+
+    if( aFindIter == maChartModels.end() )
+    {
+        try
+        {
+            xModel->lockControllers();
+            maChartModels.push_back( xModel );
+        }
+        catch ( uno::Exception& )
+        {
+            DBG_ERROR("Unexpected exception in ScChartLockGuard");
+        }
+    }
+}
+
+// === ScTemporaryChartLock ======================================
+
+ScTemporaryChartLock::ScTemporaryChartLock( ScDocument* pDocP ) :
+    mpDoc( pDocP )
+{
+    maTimer.SetTimeout( SC_CHARTLOCKTIMEOUT );
+    maTimer.SetTimeoutHdl( LINK( this, ScTemporaryChartLock, TimeoutHdl ) );
+}
+
+
+ScTemporaryChartLock::~ScTemporaryChartLock()
+{
+    mpDoc = 0;
+    StopLocking();
+}
+
+void ScTemporaryChartLock::StartOrContinueLocking()
+{
+    if(!mapScChartLockGuard.get())
+        mapScChartLockGuard = std::auto_ptr< ScChartLockGuard >( new ScChartLockGuard(mpDoc) );
+    maTimer.Start();
+}
+
+void ScTemporaryChartLock::StopLocking()
+{
+    maTimer.Stop();
+    mapScChartLockGuard.reset();
+}
+
+void ScTemporaryChartLock::AlsoLockThisChart( const Reference< frame::XModel >& xModel )
+{
+    if(mapScChartLockGuard.get())
+        mapScChartLockGuard->AlsoLockThisChart( xModel );
+}
+
+IMPL_LINK( ScTemporaryChartLock, TimeoutHdl, Timer*, EMPTYARG )
+{
+    mapScChartLockGuard.reset();
+    return 0;
+}
