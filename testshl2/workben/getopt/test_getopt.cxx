c26ef2c99af951d81b46f95bce7f19561caf9904
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-03 09:17:25 +0000

    INTEGRATION: CWS rpt23fix02 (1.3.24); FILE MERGED 2007/07/23 11:15:59 lla 1.3.24.1: #i79902# make demos buildable

--- testshl2/workben/getopt/test_getopt.cxx
+++ testshl2/workben/getopt/test_getopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: test_getopt.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 03:50:49 $
+ *  last change: $Author: hr $ $Date: 2007-08-03 10:17:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -90,7 +90,8 @@ int main( int argc, char* argv[] )
 int _cdecl main( int argc, char* argv[] )
 #endif
 {
-    static char* optionSet[] = {
+    (void) argc;
+    static char const* optionSet[] = {
         "-boom,     stop near error position, exception only",
         "-mode=s,   the output mode, emacs, xml, old. Default is -mode old",
         "-logPath=s, destination path for logging",
