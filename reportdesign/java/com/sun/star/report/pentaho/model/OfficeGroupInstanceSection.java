025c4a4e951fdf9a024c5bda75f561ba56d9f14d
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-03 08:50:30 +0000

    INTEGRATION: CWS rpt23fix02 (1.1.2); FILE ADDED 2007/07/25 15:05:46 tmorgner 1.1.2.1: Issue number:  78128
    This commit fixes the messed up repeating-header system. The whole thing has
    been redesigned to create a clean state-flow that can be debugged and traced
    without any weird mind-bending activities. The report-targets now have a
    new state (IN_GROUP_INSTANCE) to keep track of the group's processing and
    to separate repeating headers and footers from ordinary headers and footers.
    
    This patch also enforces a clean state-flow in the OfficeReportOutputTarget
    and explicitly checks for all allowed band-types and throws exceptions on
    every illegal state.

---
+++ reportdesign/java/com/sun/star/report/pentaho/model/OfficeGroupInstanceSection.java
@@ -0,0 +1,15 @@
+package com.sun.star.report.pentaho.model;
+
+import org.jfree.report.structure.Group;
+
+/**
+ * Creation-Date: 25.07.2007, 14:41:54
+ *
+ * @author Thomas Morgner
+ */
+public class OfficeGroupInstanceSection extends Group
+{
+  public OfficeGroupInstanceSection()
+  {
+  }
+}
