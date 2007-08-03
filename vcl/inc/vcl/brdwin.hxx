46a2286b949262ba2b700a335aa5a6344e5a5b31
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-03 13:04:29 +0000

    INTEGRATION: CWS aquavcl02 (1.2.2); FILE MERGED 2007/07/31 09:48:10 pjanik 1.2.2.1: Bring back changes from removed inc/brdwin.hxx.

--- vcl/inc/vcl/brdwin.hxx
+++ vcl/inc/vcl/brdwin.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: brdwin.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-24 09:58:00 $
+ *  last change: $Author: hr $ $Date: 2007-08-03 14:04:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -317,6 +317,7 @@ class ImplSmallBorderWindowView : public ImplBorderWindowView
     sal_Int32               mnTopBorder;
     sal_Int32               mnRightBorder;
     sal_Int32               mnBottomBorder;
+    bool                    mbNWFBorder;
 
 public:
                             ImplSmallBorderWindowView( ImplBorderWindow* pBorderWindow );
