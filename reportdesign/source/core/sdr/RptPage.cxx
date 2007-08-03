adff16aa7e431e1c28a5ee44aeb7f96e7ef92ac8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-08-03 08:55:28 +0000

    INTEGRATION: CWS rpt23fix02 (1.2.4); FILE MERGED 2007/07/26 08:15:44 oj 1.2.4.1: #i77907# remove shapes when section is in disposing and dispose the shape afterwards

--- reportdesign/source/core/sdr/RptPage.cxx
+++ reportdesign/source/core/sdr/RptPage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RptPage.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:16 $
+ *  last change: $Author: hr $ $Date: 2007-08-03 09:55:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -86,7 +86,6 @@ OReportPage::OReportPage( const OReportPage& rPage )
 OReportPage::~OReportPage()
 {
     DBG_DTOR( rpt_OReportPage,NULL);
-    Clear();
 }
 
 //----------------------------------------------------------------------------
