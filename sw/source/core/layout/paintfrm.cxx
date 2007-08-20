f90fe9716a235a3934ff9334e0c0c7bc8e9f1019
Ivo Hinkelmann <ihi@openoffice.org>
2007-08-20 12:42:36 +0000

    INTEGRATION: CWS swqbf106_SRC680 (1.105.16); FILE MERGED 2007/08/15 12:07:28 od 1.105.16.1: #i80691# method <SwRootFrm::Paint(..) 	 - include page border and shadow area into paint area

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: paintfrm.cxx,v $
  *
- *  $Revision: 1.105 $
+ *  $Revision: 1.106 $
  *
- *  last change: $Author: hr $ $Date: 2007-08-03 13:40:17 $
+ *  last change: $Author: ihi $ $Date: 2007-08-20 13:42:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -2879,7 +2879,12 @@ void SwRootFrm::Paint( const SwRect& rRect ) const
                 pSpecSubsLines = new SwSubsRects;
             }
 
-            SwRect aPaintRect( pPage->Frm() );
+            // --> OD 2007-08-15 #i80691#
+            // include border and shadow area into paint area.
+//            SwRect aPaintRect( pPage->Frm() );
+            SwRect aPaintRect;
+            pPage->GetBorderAndShadowBoundRect( pPage->Frm(), pSh, aPaintRect );
+            // <--
             aPaintRect._Intersection( aRect );
 
             if ( bExtraData )
