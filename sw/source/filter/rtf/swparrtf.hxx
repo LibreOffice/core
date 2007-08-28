db973e6d1bd0056be0d0071f982fda8daa19fb9a
Vladimir Glazounov <vg@openoffice.org>
2007-08-28 09:06:18 +0000

    INTEGRATION: CWS swqbf108_SRC680 (1.30.20); FILE MERGED 2007/08/20 07:08:01 fme 1.30.20.1: #i80767# According to tono, we should completely remove XlateTxtColName

--- sw/source/filter/rtf/swparrtf.hxx
+++ sw/source/filter/rtf/swparrtf.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swparrtf.hxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: hr $ $Date: 2007-08-03 11:00:25 $
+ *  last change: $Author: vg $ $Date: 2007-08-28 10:06:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -415,7 +415,6 @@ class SwRTFParser : public SvxRTFParser
     void SetStyleAttr( SfxItemSet& rCollSet,
                         const SfxItemSet& rStyleSet,
                         const SfxItemSet& rDerivedSet );
-    String XlateFmtColName( const String &rName );  //Takashi Ono for CJK
     SwTxtFmtColl* MakeStyle( USHORT nNo, const SvxRTFStyleType& rStyle );
     SwCharFmt* MakeCharStyle( USHORT nNo, const SvxRTFStyleType& rStyle );
     void MakeStyleTab();
