225610e980abc3b5772b854ee97c8e47a404d1fa
Vladimir Glazounov <vg@openoffice.org>
2007-08-28 12:39:19 +0000

    INTEGRATION: CWS navorder (1.16.130); FILE MERGED 2007/06/21 13:34:40 cl 1.16.130.1: #i68097# added api for shape navigation order

--- sd/source/ui/unoidl/unopage.hxx
+++ sd/source/ui/unoidl/unopage.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unopage.hxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 14:39:09 $
+ *  last change: $Author: vg $ $Date: 2007-08-28 13:39:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,6 +122,9 @@ protected:
     bool     mbIsImpressDocument;
 
     virtual void disposing() throw();
+
+    ::com::sun::star::uno::Any getNavigationOrder();
+    void setNavigationOrder( const ::com::sun::star::uno::Any& rValue );
 
     void throwIfDisposed() const throw (::com::sun::star::uno::RuntimeException );
 
