4a0522e25f46f46a02aeec76c9f80b79cba4594f
Kurt Zenker <kz@openoffice.org>
2007-09-05 16:45:48 +0000

    INTEGRATION: CWS ause083 (1.4.940); FILE MERGED 2007/07/17 10:30:32 hjs 1.4.940.1: #i79470# cleanup to survive pch

--- svx/source/stbctrls/makefile.mk
+++ svx/source/stbctrls/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 00:18:33 $
+#   last change: $Author: kz $ $Date: 2007-09-05 17:45:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,8 +46,6 @@ AUTOSEG=true
 
 .INCLUDE :  settings.mk
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
-
-CFLAGS+=-DDG_DLL
 
 # --- Files --------------------------------------------------------
 
