0852dbca9ac310b3a3e898a411d7dff04d352ddc
Kurt Zenker <kz@openoffice.org>
2007-09-05 16:38:34 +0000

    INTEGRATION: CWS ause083 (1.3.20); FILE MERGED 2007/07/23 11:36:57 hjs 1.3.20.2: RESYNC: (1.3-1.5); FILE MERGED 2007/07/17 22:15:20 mba 1.3.20.1: #i79470#: missing pch include

--- svtools/source/edit/textwindowpeer.cxx
+++ svtools/source/edit/textwindowpeer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textwindowpeer.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-18 10:04:13 $
+ *  last change: $Author: kz $ $Date: 2007-09-05 17:38:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,8 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svtools.hxx"
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
