c2b72e47132e1daeee8f0863fe1c581b5a164c30
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:49:21 +0000

    INTEGRATION: CWS mingwport06 (1.3.144); FILE MERGED 2007/08/24 13:06:54 vg 1.3.144.1: #i75499# pragma is for MSVC

--- sal/systools/win32/onlineupdate/onlinecheck.cxx
+++ sal/systools/win32/onlineupdate/onlinecheck.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: onlinecheck.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 09:07:17 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:49:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,9 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sal.hxx"
+#ifdef _MSC_VER
 #pragma warning( disable: 4668 )
+#endif
 
 #include <sal/types.h>
 
