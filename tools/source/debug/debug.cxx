4e1ca5a2ad0e7431270751ff0cfa8722257be56e
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:14:46 +0000

    INTEGRATION: CWS mingwport06 (1.13.24); FILE MERGED 2007/08/27 14:34:49 vg 1.13.24.1: #i75499# pragma for MSVC

--- tools/source/debug/debug.cxx
+++ tools/source/debug/debug.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: debug.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 22:09:41 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:14:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,9 +51,13 @@
 #include <stdio.h>
 
 #if defined ( WNT )
+#ifdef _MSC_VER
 #pragma warning (push,1)
+#endif
 #include <tools/svwin.h>
+#ifdef _MSC_VER
 #pragma warning (pop)
+#endif
 #endif
 
 #include <tools/debug.hxx>
