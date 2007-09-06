a3a635282c50e107d928cdb238cafe64c31998aa
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:18:36 +0000

    INTEGRATION: CWS mingwport06 (1.11.122); FILE MERGED 2007/08/24 13:08:29 vg 1.11.122.1: #i75499# pragma is for MSVC

--- sc/source/core/data/pivot2.cxx
+++ sc/source/core/data/pivot2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pivot2.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 16:45:17 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:18:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 
 
-#ifdef WNT
+#ifdef _MSC_VER
 #pragma optimize("",off)
 #pragma optimize("q",off) // p-code off
 #endif
