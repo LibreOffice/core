3332e96588dfdf645533f103a0a479084c870882
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:24:49 +0000

    INTEGRATION: CWS mingwport06 (1.11.6); FILE MERGED 2007/08/24 13:15:16 vg 1.11.6.1: #i75499# pragma is for MSVC

--- sot/source/sdstor/stgcache.cxx
+++ sot/source/sdstor/stgcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stgcache.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 12:49:50 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:24:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,7 +70,7 @@ typedef std::hash_map
     std::hash< INT32 >,
     NAMESPACE_STD(equal_to)< INT32 >
 > UsrStgPagePtr_Impl;
-#ifdef WNT
+#ifdef _MSC_VER
 #pragma warning( disable: 4786 )
 #endif
 
