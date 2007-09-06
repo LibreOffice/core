adb3747664af065cd37bd6d02eb3c38ee0faed61
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:13:16 +0000

    INTEGRATION: CWS mingwport06 (1.5.76); FILE MERGED 2007/08/24 13:18:27 vg 1.5.76.1: #i75499# pragma is for MSVC

--- svtools/source/items1/stylepool.cxx
+++ svtools/source/items1/stylepool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stylepool.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 21:46:10 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:13:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,7 @@
  *
  ************************************************************************/
 
-#ifndef GCC
+#ifdef _MSC_VER
 #pragma hdrstop
 #endif
 
