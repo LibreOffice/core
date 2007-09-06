9baa4c51752d5e424f2e06752da1c3c330dbf9af
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:24:13 +0000

    INTEGRATION: CWS mingwport06 (1.7.56); FILE MERGED 2007/08/24 13:14:41 vg 1.7.56.1: #i75499# pragma is for MSVC

--- shell/source/win32/shlxthandler/propsheets/listviewbuilder.cxx
+++ shell/source/win32/shlxthandler/propsheets/listviewbuilder.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: listviewbuilder.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 16:13:04 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:24:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_shell.hxx"
 
+#ifdef _MSC_VER
 #pragma warning (disable : 4786 4503)
+#endif
 
 //------------------------------------
 // include
