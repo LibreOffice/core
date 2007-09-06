7dd35bdd7cdfca2a78ce6654f8c4afff5d2e29e0
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:43:01 +0000

    INTEGRATION: CWS mingwport06 (1.7.58); FILE MERGED 2007/08/24 12:55:23 vg 1.7.58.1: #i75499# pragma is for MSVC

--- bridges/source/cpp_uno/shared/vtablefactory.cxx
+++ bridges/source/cpp_uno/shared/vtablefactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vtablefactory.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:56:54 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:43:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,9 +58,13 @@
 #include <sys/mman.h>
 #elif defined SAL_W32
 #define WIN32_LEAN_AND_MEAN
+#ifdef _MSC_VER
 #pragma warning(push,1) // disable warnings within system headers
+#endif
 #include <windows.h>
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 #else
 #error Unsupported platform
 #endif
