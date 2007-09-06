e36ff63fc1040c5a01a5e969d3e71be841e69087
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:05:50 +0000

    INTEGRATION: CWS mingwport06 (1.11.166); FILE MERGED 2007/08/27 14:33:42 vg 1.11.166.1: #i75499# pragma for MSVC

--- sw/source/ui/dbui/mmgreetingspage.cxx
+++ sw/source/ui/dbui/mmgreetingspage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mmgreetingspage.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 09:03:30 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:05:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -238,7 +238,9 @@ void    SwGreetingsHandler::Contains(sal_Bool bContainsGreeting)
   -----------------------------------------------------------------------*/
 SwMailMergeGreetingsPage::SwMailMergeGreetingsPage( SwMailMergeWizard* _pParent) :
     svt::OWizardPage(_pParent, SW_RES(DLG_MM_GREETINGS_PAGE)),
+#ifdef _MSC_VER
 #pragma warning (disable : 4355)
+#endif
     m_aHeaderFI(this, SW_RES(          FI_HEADER     ) ),
     m_aGreetingLineCB(this, SW_RES(    CB_GREETINGLINE ) ),
     m_aPersonalizedCB(this, SW_RES(      CB_PERSONALIZED ) ),
@@ -262,7 +264,9 @@ SwMailMergeGreetingsPage::SwMailMergeGreetingsPage( SwMailMergeWizard* _pParent)
     m_aPrevSetIB(       this, SW_RES( IB_PREVSET     ) ),
     m_aNextSetIB(       this, SW_RES( IB_NEXTSET     ) ),
     m_sDocument(        SW_RES(       STR_DOCUMENT  ) )
+#ifdef _MSC_VER
 #pragma warning (default : 4355)
+#endif
 {
     m_pWizard = _pParent;
     m_pGreetingLineCB = &m_aGreetingLineCB;
@@ -450,7 +454,9 @@ IMPL_LINK(SwMailMergeGreetingsPage, InsertDataHdl_Impl, ImageButton*, pButton)
   -----------------------------------------------------------------------*/
 SwMailBodyDialog::SwMailBodyDialog(Window* pParent, SwMailMergeWizard* _pWizard) :
     SfxModalDialog(pParent, SW_RES(DLG_MM_MAILBODY)),
+#ifdef _MSC_VER
 #pragma warning (disable : 4355)
+#endif
     m_aGreetingLineCB(this, SW_RES(    CB_GREETINGLINE ) ),
     m_aPersonalizedCB(this, SW_RES(      CB_PERSONALIZED ) ),
     m_aFemaleFT(this, SW_RES(            FT_FEMALE   ) ),
@@ -472,7 +478,9 @@ SwMailBodyDialog::SwMailBodyDialog(Window* pParent, SwMailMergeWizard* _pWizard)
     m_aOK(   this, SW_RES(             PB_OK           ) ),
     m_aCancel(   this, SW_RES(         PB_CANCEL       ) ),
     m_aHelp(   this, SW_RES(           PB_HELP         ) )
+#ifdef _MSC_VER
 #pragma warning (default : 4355)
+#endif
 {
     m_pWizard = _pWizard;
     m_pGreetingLineCB = &m_aGreetingLineCB;
