aab9cf7c2e70ff0d4f8bec600f5ca315801acdf9
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:44:53 +0000

    INTEGRATION: CWS mingwport06 (1.6.66); FILE MERGED 2007/08/24 13:05:14 vg 1.6.66.1: #i75499# pragma is for MSVC

--- idlc/inc/idlc/idlctypes.hxx
+++ idlc/inc/idlc/idlctypes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: idlctypes.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:04:36 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:44:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -92,7 +92,7 @@ class AstScope;
 AstDeclaration* SAL_CALL scopeAsDecl(AstScope* pScope);
 AstScope* SAL_CALL declAsScope(AstDeclaration* pDecl);
 
-#ifdef WIN32
+#ifdef _MSC_VER
 #pragma warning( disable : 4541 )
 #endif
 
