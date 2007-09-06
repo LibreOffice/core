a34a8630fdb9ee3fcd926947709bd5489f2d309d
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:33:57 +0000

    INTEGRATION: CWS mingwport06 (1.2.22); FILE MERGED 2007/08/31 13:19:55 vg 1.2.22.1: #i80359# MinGW port efforts

--- setup_native/source/win32/customactions/relnotes/makefile.mk
+++ setup_native/source/win32/customactions/relnotes/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-06 14:32:53 $
+#   last change: $Author: kz $ $Date: 2007-09-06 13:33:57 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,8 +55,6 @@ UWINAPILIB=
 .IF "$(GUI)"=="WNT"
 
 SLOFILES =	$(SLO)$/relnotes.obj
-
-SHL1OBJS = $(SLOFILES)
 
 STDSHL+=	\
     $(ADVAPI32LIB)\
