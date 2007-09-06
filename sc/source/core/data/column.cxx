29c5a4e4c2c5f219f3023425cb892cf006f44f9a
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:17:53 +0000

    INTEGRATION: CWS mingwport06 (1.22.154); FILE MERGED 2007/08/24 13:08:29 vg 1.22.154.1: #i75499# pragma is for MSVC

--- sc/source/core/data/column.cxx
+++ sc/source/core/data/column.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: column.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:00:13 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:17:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -688,7 +688,7 @@ void ScColumn::ApplyAttr( SCROW nRow, const SfxPoolItem& rAttr )
 #endif
 }
 
-#ifdef WNT
+#ifdef _MSC_VER
 #pragma optimize ( "", off )
 #endif
 
@@ -781,7 +781,7 @@ BOOL ScColumn::Search( SCROW nRow, SCSIZE& nIndex ) const
     return bFound;
 }
 
-#ifdef WNT
+#ifdef _MSC_VER
 #pragma optimize ( "", on )
 #endif
 
