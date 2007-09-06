0532ff18c85e03c69a10e532b2ea092b93745e63
Rüdiger Timm <rt@openoffice.org>
2007-09-06 14:22:16 +0000

    #i10000# Include fixed.

--- svx/source/sdr/contact/viewcontactofsdrobj.cxx
+++ svx/source/sdr/contact/viewcontactofsdrobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: viewcontactofsdrobj.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-05 17:40:16 $
+ *  last change: $Author: rt $ $Date: 2007-09-06 15:22:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -85,7 +85,7 @@
 #endif
 
 #ifndef _SDRPAINTWINDOW_HXX
-#include <svx/sdrpaintwindow.hxx>
+#include <sdrpaintwindow.hxx>
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
