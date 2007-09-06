0854b00b55722f3ace332a7fadf1eafc8f09c084
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:55:02 +0000

    INTEGRATION: CWS qadev30 (1.6.42); FILE MERGED 2007/07/05 07:46:31 cn 1.6.42.1: #i79247# close documents via util.DesktopTools.close() instead of dispose

--- qadevOOo/tests/java/mod/_sch/AccWall.java
+++ qadevOOo/tests/java/mod/_sch/AccWall.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AccWall.java,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:11:23 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:55:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@ public class AccWall extends TestCase {
     protected TestEnvironment createTestEnvironment(
         TestParameters Param, PrintWriter log) {
 
-        if (xChartDoc != null) xChartDoc.dispose();
+        if (xChartDoc != null) cleanup(Param, log);
         log.println( "creating a chart document" );
         SOfficeFactory SOF = SOfficeFactory.getFactory( (XMultiServiceFactory)Param.getMSF());
         try {
@@ -142,8 +142,9 @@ public class AccWall extends TestCase {
     * @param log writer to log information while testing
     */
     protected void cleanup( TestParameters Param, PrintWriter log) {
-        log.println("dispose chart document");
-        xChartDoc.dispose();
+        log.println( "    closing xChartDoc " );
+        util.DesktopTools.closeDoc(xChartDoc);
+
     }
 
 }
