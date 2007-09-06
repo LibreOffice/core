32bacbb904f2153138a6d2aa5414ccf704d16ff4
Kurt Zenker <kz@openoffice.org>
2007-09-06 13:10:03 +0000

    INTEGRATION: CWS mingwport06 (1.4.30); FILE MERGED 2007/08/24 13:27:49 vg 1.4.30.1: #i75499# pragma is for MSVC

--- udm/source/inc/precomp.h
+++ udm/source/inc/precomp.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: precomp.h,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:34:37 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 14:10:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,9 @@
 #endif
 
 #ifdef NP_LOCALBUILD
+#ifdef _MSC_VER
 #pragma warning( disable : 4786 )
+#endif
 #endif
 
 #include <cosv/csv_precomp.h>
