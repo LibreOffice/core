ba541dab4c98d5b2659e83e7ff5700a7799d9a8b
Ivo Hinkelmann <ihi@openoffice.org>
2007-09-13 17:05:44 +0000

    INTEGRATION: CWS macosxspellchecker (1.1.2); FILE ADDED 2007/09/13 02:37:03 msicotte 1.1.2.5: remove reference to exports.dxp - only needed for windows 2007/09/12 22:23:14 msicotte 1.1.2.4: updated echo when nothing to build 2007/09/12 21:11:58 msicotte 1.1.2.3: #i77592#: change to build libMacOSXSpell only for MAC OS X AQUA 2007/08/29 13:57:36 msicotte 1.1.2.2: renaming macosxspell.map->MacOSXSpell.map 2007/08/29 01:52:00 msicotte 1.1.2.1: #i77592#: new files to implement libmacosxspell680mxi.dylib

---
+++ lingucomponent/source/spellcheck/macosxspell/makefile.mk
@@ -0,0 +1,97 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2007-09-13 18:05:44 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..
+
+PRJNAME	= lingucomponent
+TARGET	= MacOSXSpell
+ENABLE_EXCEPTIONS=TRUE
+USE_DEFFILE=TRUE
+
+.IF "$(GUIBASE)"=="aqua"
+
+#----- Settings ---------------------------------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files --------------------------------------------------------
+
+CFLAGSCXX += -I..$/..$/lingutil
+CFLAGSCXX += -x objective-c++ -fobjc-exceptions
+
+EXCEPTIONSFILES=	\
+        $(SLO)$/sprophelp.obj\
+        $(SLO)$/macspellimp.obj
+
+
+SLOFILES=	\
+        $(SLO)$/sprophelp.obj\
+        $(SLO)$/macreg.obj\
+        $(SLO)$/macspellimp.obj
+
+
+SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+
+SHL1STDLIBS= \
+        $(CPPULIB) 	 \
+        $(CPPUHELPERLIB) 	 \
+        $(VOSLIB)		\
+        $(TOOLSLIB)		\
+        $(SVTOOLLIB)	\
+        $(SVLLIB)		\
+        $(VCLLIB)		\
+        $(SALLIB)		\
+        $(UCBHELPERLIB)	\
+        $(UNOTOOLSLIB)	\
+        $(LNGLIB)
+
+SHL1STDLIBS+= -framework Cocoa
+
+# build DLL
+SHL1LIBS=		$(SLB)$/$(TARGET).lib
+SHL1IMPLIB=		i$(TARGET)
+SHL1DEPN=		$(SHL1LIBS)
+SHL1DEF=		$(MISC)$/$(SHL1TARGET).def
+
+SHL1VERSIONMAP= $(TARGET).map
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : target.mk
+
+.ELSE
+dummy:
+    @echo " Nothing to build for GUIBASE=$(GUIBASE)"
+.ENDIF
