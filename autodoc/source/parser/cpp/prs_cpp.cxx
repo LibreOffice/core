f2b649d5bfc78c2e223dbfe797e951b13e5ca665
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:16:23 +0000

    INTEGRATION: CWS adc17 (1.4.18); FILE MERGED 2007/08/31 14:45:35 np 1.4.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser/cpp/prs_cpp.cxx
+++ autodoc/source/parser/cpp/prs_cpp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: prs_cpp.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:04:41 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:15:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <cpp/prs_cpp.hxx>
