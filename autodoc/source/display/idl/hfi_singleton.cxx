63dd20fffae7170da8eccb91b05c6b88cf689029
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:58:38 +0000

    INTEGRATION: CWS adc17 (1.6.18); FILE MERGED 2007/08/31 14:45:31 np 1.6.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/idl/hfi_singleton.cxx
+++ autodoc/source/display/idl/hfi_singleton.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hfi_singleton.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:48:17 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:58:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "hfi_singleton.hxx"
