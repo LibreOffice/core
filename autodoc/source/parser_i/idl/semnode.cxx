7e0dc36d3b51170a1584c8201669f065d2278f5f
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:25:39 +0000

    INTEGRATION: CWS adc17 (1.6.18); FILE MERGED 2007/08/31 14:45:37 np 1.6.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser_i/idl/semnode.cxx
+++ autodoc/source/parser_i/idl/semnode.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: semnode.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:12:29 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:23:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <s2_luidl/semnode.hxx>
