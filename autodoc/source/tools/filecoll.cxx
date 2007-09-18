8a3e0093af5265e972a23b4112966cb35ba8e633
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:28:48 +0000

    INTEGRATION: CWS adc17 (1.9.18); FILE MERGED 2007/08/31 14:45:38 np 1.9.18.1: #i80569# remove wrong pchs.

--- autodoc/source/tools/filecoll.cxx
+++ autodoc/source/tools/filecoll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filecoll.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:17:41 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:28:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <tools/filecoll.hxx>
