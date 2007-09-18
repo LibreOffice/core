c817f0e5a706bb2d17deb91db94c22e820472fa9
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:03:23 +0000

    INTEGRATION: CWS adc17 (1.5.18); FILE MERGED 2007/08/31 14:45:32 np 1.5.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/idl/hi_linkhelper.cxx
+++ autodoc/source/display/idl/hi_linkhelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hi_linkhelper.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:51:01 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:01:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "hi_linkhelper.hxx"
--- autodoc/source/display/kernel/displfct.cxx
+++ autodoc/source/display/kernel/displfct.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: displfct.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:51:31 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:02:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include "displfct.hxx"
