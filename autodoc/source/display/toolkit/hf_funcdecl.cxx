cdc9449746ead0c47f264ed466b806138d19a203
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:04:18 +0000

    INTEGRATION: CWS adc17 (1.6.18); FILE MERGED 2007/08/31 14:45:32 np 1.6.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/toolkit/hf_funcdecl.cxx
+++ autodoc/source/display/toolkit/hf_funcdecl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hf_funcdecl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:51:58 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:02:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <toolkit/hf_funcdecl.hxx>
