aa83fee99e0d18d10852f724f71eb2bbfbdf24ff
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:19:04 +0000

    INTEGRATION: CWS adc17 (1.11.18); FILE MERGED 2007/08/31 14:45:36 np 1.11.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser_i/idl/parsenv2.cxx
+++ autodoc/source/parser_i/idl/parsenv2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: parsenv2.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:08:15 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:19:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <s2_luidl/parsenv2.hxx>
