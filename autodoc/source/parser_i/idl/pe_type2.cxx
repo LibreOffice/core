e6099e7e6aa3bf14552432b05108560f2eb99355
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:24:42 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:37 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser_i/idl/pe_type2.cxx
+++ autodoc/source/parser_i/idl/pe_type2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_type2.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:11:48 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:22:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <s2_luidl/pe_type2.hxx>
