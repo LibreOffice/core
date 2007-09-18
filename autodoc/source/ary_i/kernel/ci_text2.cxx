1e54bc127229bf7851e5bbb8fae0f0826c3f945d
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:52:40 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:30 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/ary_i/kernel/ci_text2.cxx
+++ autodoc/source/ary_i/kernel/ci_text2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ci_text2.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:38:39 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:49:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <ary_i/ci_text2.hxx>
