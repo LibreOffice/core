d912d380733aded60e9c222c47b65cb55d169511
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:29:01 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:38 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/tools/tkpchars.cxx
+++ autodoc/source/tools/tkpchars.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tkpchars.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:17:55 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:29:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <tools/tkpchars.hxx>
