81d6faf46d185f29c3ec132e7db665f3a1a4362e
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:14:43 +0000

    INTEGRATION: CWS adc17 (1.8.18); FILE MERGED 2007/08/31 14:45:34 np 1.8.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser/cpp/pe_file.cxx
+++ autodoc/source/parser/cpp/pe_file.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_file.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:01:59 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:12:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include "pe_file.hxx"
--- autodoc/source/parser/cpp/pe_vafu.cxx
+++ autodoc/source/parser/cpp/pe_vafu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_vafu.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:04:00 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:14:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "pe_vafu.hxx"
