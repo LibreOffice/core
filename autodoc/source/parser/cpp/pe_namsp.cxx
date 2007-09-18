56f73b79f8e216f76311cbf05b0ba2de3275a0c8
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:14:02 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:34 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser/cpp/pe_namsp.cxx
+++ autodoc/source/parser/cpp/pe_namsp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_namsp.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:02:47 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:13:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <pe_namsp.hxx>
