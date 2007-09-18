cfc4f0c4693822c02210b883912d1bb4e092e049
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:18:24 +0000

    INTEGRATION: CWS adc17 (1.9.18); FILE MERGED 2007/08/31 14:45:36 np 1.9.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser_i/idl/cx_idlco.cxx
+++ autodoc/source/parser_i/idl/cx_idlco.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cx_idlco.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:07:34 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:18:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <s2_luidl/cx_idlco.hxx>
