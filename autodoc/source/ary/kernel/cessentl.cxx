165fdc599818d9f926c90c7f5dd208dca20e3a90
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:42:02 +0000

    INTEGRATION: CWS adc17 (1.6.18); FILE MERGED 2007/08/31 14:45:28 np 1.6.18.1: #i80569# remove wrong pchs.

--- autodoc/source/ary/kernel/cessentl.cxx
+++ autodoc/source/ary/kernel/cessentl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cessentl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:31:28 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:42:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <ary/cessentl.hxx>
