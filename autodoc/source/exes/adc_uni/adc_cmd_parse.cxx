38006986fdb07f5b0156ba5d8acd1bf02886262d
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:07:19 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:33 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/exes/adc_uni/adc_cmd_parse.cxx
+++ autodoc/source/exes/adc_uni/adc_cmd_parse.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: adc_cmd_parse.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:54:35 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:05:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "adc_cmd_parse.hxx"
--- autodoc/source/parser/adoc/cx_a_std.cxx
+++ autodoc/source/parser/adoc/cx_a_std.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cx_a_std.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:56:31 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:07:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <adoc/cx_a_std.hxx>
