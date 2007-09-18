aaba5de9a8a1d51bdc6b9db1e20a947c09efea7d
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:59:06 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:31 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/idl/hfi_interface.cxx
+++ autodoc/source/display/idl/hfi_interface.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hfi_interface.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:46:36 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:56:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "hfi_interface.hxx"
