4b819fa075006d6d1d0dda47acc46b83ba0a676b
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:50:14 +0000

    INTEGRATION: CWS adc17 (1.11.18); FILE MERGED 2007/08/31 14:45:30 np 1.11.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/html/cfrstd.cxx
+++ autodoc/source/display/html/cfrstd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfrstd.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:39:35 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:50:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <cfrstd.hxx>
