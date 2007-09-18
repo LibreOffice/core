8c969ff7ca20ae888a7b2bd48ed902091e09baa6
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:08:16 +0000

    INTEGRATION: CWS adc17 (1.5.18); FILE MERGED 2007/08/31 14:45:33 np 1.5.18.1: #i80569# remove wrong pchs.

--- autodoc/source/exes/adc_uni/adc_msg.cxx
+++ autodoc/source/exes/adc_uni/adc_msg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: adc_msg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:55:02 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:05:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <adc_msg.hxx>
