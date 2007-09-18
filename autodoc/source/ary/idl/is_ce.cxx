95fc37cbb81a6bf924feb1c773830f01dd134a65
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:37:46 +0000

    INTEGRATION: CWS adc17 (1.4.18); FILE MERGED 2007/08/31 14:45:27 np 1.4.18.1: #i80569# remove wrong pchs.

--- autodoc/source/ary/idl/is_ce.cxx
+++ autodoc/source/ary/idl/is_ce.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: is_ce.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:27:15 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:37:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "is_ce.hxx"
