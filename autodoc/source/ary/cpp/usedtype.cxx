68a71a0ccb9cbc0b776604a4d3fa5333d35f7d9e
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 12:30:16 +0000

    INTEGRATION: CWS adc17 (1.5.18); FILE MERGED 2007/08/31 14:45:26 np 1.5.18.1: #i80569# remove wrong pchs.

--- autodoc/source/ary/cpp/usedtype.cxx
+++ autodoc/source/ary/cpp/usedtype.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: usedtype.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:20:08 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 13:30:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <ary/cpp/usedtype.hxx>
