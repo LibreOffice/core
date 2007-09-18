d45a1c39ec570d9bb2854234e32c0c75913b11b4
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:03:50 +0000

    INTEGRATION: CWS adc17 (1.7.18); FILE MERGED 2007/08/31 14:45:32 np 1.7.18.1: #i80569# remove wrong pchs.

--- autodoc/source/display/toolkit/hf_title.cxx
+++ autodoc/source/display/toolkit/hf_title.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hf_title.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:52:54 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:03:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include <toolkit/hf_title.hxx>
