fd0ef8e6be661f8695fe742f768f2cabe16b3e05
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:22:15 +0000

    INTEGRATION: CWS adc17 (1.6.18); FILE MERGED 2007/08/31 14:45:36 np 1.6.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser_i/idl/pe_evalu.cxx
+++ autodoc/source/parser_i/idl/pe_evalu.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_evalu.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:09:15 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:19:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,9 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
 
 #include <precomp.h>
 #include <s2_luidl/pe_evalu.hxx>
