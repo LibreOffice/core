8941d9482af050aedac86c6263d6625e17675381
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 14:06:53 +0000

    INTEGRATION: CWS chart11 (1.10.2); FILE MERGED 2007/07/31 12:56:46 bm 1.10.2.1: #i80084# avoid usage of map operator[] with enums as keys, simplify initialization of default property values

--- chart2/source/model/template/ScatterChartTypeTemplate.cxx
+++ chart2/source/model/template/ScatterChartTypeTemplate.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ScatterChartTypeTemplate.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-25 08:54:42 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 15:06:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -114,19 +114,12 @@ void lcl_AddPropertiesToVector(
 void lcl_AddDefaultsToMap(
     ::chart::tPropertyValueMap & rOutMap )
 {
-    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_STYLE ));
-    rOutMap[ PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_STYLE ] =
-        uno::makeAny( chart2::CurveStyle_LINES );
-
-    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_RESOLUTION ));
-    rOutMap[ PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_RESOLUTION ] =
-        uno::makeAny( sal_Int32( 20 ) );
+    ::chart::PropertyHelper::setPropertyValueDefault( rOutMap, PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_STYLE, chart2::CurveStyle_LINES );
+    ::chart::PropertyHelper::setPropertyValueDefault< sal_Int32 >( rOutMap, PROP_SCATTERCHARTTYPE_TEMPLATE_CURVE_RESOLUTION, 20 );
 
     // todo: check whether order 3 means polygons of order 3 or 2. (see
     // http://www.people.nnov.ru/fractal/Splines/Basis.htm )
-    OSL_ASSERT( rOutMap.end() == rOutMap.find( PROP_SCATTERCHARTTYPE_TEMPLATE_SPLINE_ORDER ));
-    rOutMap[ PROP_SCATTERCHARTTYPE_TEMPLATE_SPLINE_ORDER ] =
-        uno::makeAny( sal_Int32( 3 ) );
+    ::chart::PropertyHelper::setPropertyValueDefault< sal_Int32 >( rOutMap, PROP_SCATTERCHARTTYPE_TEMPLATE_SPLINE_ORDER, 3 );
 }
 
 const Sequence< Property > & lcl_GetPropertySequence()
