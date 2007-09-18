1040af40584f89d038dae1cfddee1ec64cce23f1
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:59:23 +0000

    INTEGRATION: CWS chart11 (1.6.38); FILE MERGED 2007/08/23 12:36:29 bm 1.6.38.2: RESYNC: (1.6-1.7); FILE MERGED 2007/07/31 22:19:25 bm 1.6.38.1: #i79522# warnings on Solaris removed

--- chart2/source/model/main/Axis.hxx
+++ chart2/source/model/main/Axis.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: Axis.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2007-08-17 12:14:21 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:59:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -186,6 +186,7 @@ protected:
 
     // ____ OPropertySet ____
     virtual void firePropertyChangeEvent();
+    using OPropertySet::disposing;
 
     void fireModifyEvent();
 
