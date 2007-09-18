ff1ff964cc1b764dc2b6e166191374b8eee28c28
Vladimir Glazounov <vg@openoffice.org>
2007-09-18 13:13:09 +0000

    INTEGRATION: CWS adc17 (1.9.18); FILE MERGED 2007/08/31 14:45:34 np 1.9.18.1: #i80569# remove wrong pchs.

--- autodoc/source/parser/cpp/pe_funct.cxx
+++ autodoc/source/parser/cpp/pe_funct.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pe_funct.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 17:02:13 $
+ *  last change: $Author: vg $ $Date: 2007-09-18 14:13:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,10 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_autodoc.hxx"
-
 
 #include <precomp.h>
 #include "pe_funct.hxx"
