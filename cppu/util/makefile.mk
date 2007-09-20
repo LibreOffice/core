bf349f38d8f650307a40cc019cff210d18fcde7b
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 13:44:33 +0000

    INTEGRATION: CWS os2port01 (1.8.40); FILE MERGED 2007/09/05 07:05:08 obr 1.8.40.3: RESYNC: (1.9-1.11); FILE MERGED 2007/08/06 10:47:25 obr 1.8.40.2: RESYNC: (1.8-1.9); FILE MERGED 2006/12/28 14:53:44 ydario 1.8.40.1: OS/2 initial import.

--- cppu/util/makefile.mk
+++ cppu/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: obo $ $Date: 2007-07-18 12:34:25 $
+#   last change: $Author: vg $ $Date: 2007-09-20 14:44:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ SHL1LIBS= \
     $(SLB)$/cppu_threadpool.lib	\
     $(SLB)$/cppu_cppu.lib
 
-.IF "$(GUI)" == "WNT"
+.IF "$(GUI)" == "WNT" || "$(GUI)"=="OS2"
 SHL1TARGET=$(TARGET)$(UDK_MAJOR)
 .ELSE
 SHL1TARGET= uno_$(TARGET)
