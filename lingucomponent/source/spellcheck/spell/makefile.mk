b6923cb3f86bbd2649ea7283452529a270662195
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 13:31:44 +0000

    INTEGRATION: CWS os2port01 (1.13.30); FILE MERGED 2007/09/05 08:34:19 obr 1.13.30.3: RESYNC: (1.15-1.16); FILE MERGED 2007/07/17 08:51:25 obr 1.13.30.2: RESYNC: (1.13-1.15); FILE MERGED 2006/12/28 15:01:25 ydario 1.13.30.1: OS/2 initial import.

--- lingucomponent/source/spellcheck/spell/makefile.mk
+++ lingucomponent/source/spellcheck/spell/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.16 $
+#   $Revision: 1.17 $
 #
-#   last change: $Author: hr $ $Date: 2007-07-31 13:08:22 $
+#   last change: $Author: vg $ $Date: 2007-09-20 14:31:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,6 +48,9 @@ HUNSPELLLIB=$(HUNSPELL_LIBS)
 .IF "$(GUI)"=="UNX"
 ULINGULIB=-lulingu
 .ENDIF # unx
+.IF  "$(GUI)"=="OS2"
+ULINGULIB=$(SLB)\libulingu.lib
+.ENDIF
 .IF "$(GUI)"=="WNT"
 ULINGULIB=libulingu.lib
 .ENDIF # wnt
