2ae525ae38b20bb8381b1184d76b35008582cde9
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 14:10:39 +0000

    INTEGRATION: CWS os2port01 (1.1.2); FILE ADDED 2006/11/29 14:26:35 ydario 1.1.2.1: Initial OS/2 import.

---
+++ sal/osl/os2/file_path_helper.cxx
@@ -0,0 +1,411 @@
+/*************************************************************************
+ *
+ *  $RCSfile: file_path_helper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-09-20 15:10:29 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+ /*******************************************
+     Includes
+  ******************************************/
+
+ #ifndef _OSL_THREAD_H_
+ #include "osl/thread.h"
+ #endif
+
+ #ifndef _OSL_FILE_PATH_HELPER_H_
+ #include "file_path_helper.h"
+ #endif
+
+ #ifndef _OSL_FILE_PATH_HELPER_HXX_
+ #include "file_path_helper.hxx"
+ #endif
+
+ #ifndef _OSL_UUNXAPI_HXX_
+ #include "uunxapi.hxx"
+ #endif
+
+ #ifndef _OSL_DIAGNOSE_H_
+ #include <osl/diagnose.h>
+ #endif
+
+ #ifndef _RTL_USTRING_HXX_
+ #include <rtl/ustring.hxx>
+ #endif
+
+ /*******************************************
+     Constants
+  ******************************************/
+
+  const sal_Unicode FPH_CHAR_PATH_SEPARATOR = (sal_Unicode)'\\';
+  const sal_Unicode FPH_CHAR_DOT            = (sal_Unicode)'.';
+  const sal_Unicode FPH_CHAR_COLON          = (sal_Unicode)':';
+
+  inline const rtl::OUString FPH_PATH_SEPARATOR()
+      { return rtl::OUString::createFromAscii("\\"); }
+  inline const rtl::OUString FPH_LOCAL_DIR_ENTRY()
+      { return rtl::OUString::createFromAscii("."); }
+  inline const rtl::OUString FPH_PARENT_DIR_ENTRY()
+      { return rtl::OUString::createFromAscii(".."); }
+
+ /*******************************************
+  *  osl_systemPathRemoveSeparator
+  ******************************************/
+
+ void SAL_CALL osl_systemPathRemoveSeparator(rtl_uString* pustrPath)
+ {
+     OSL_PRECOND(pustrPath, "osl_systemPathRemoveSeparator: Invalid parameter");
+
+    // maybe there are more than one separator at end
+    // so we run in a loop
+    while ((pustrPath->length > 1) && (FPH_CHAR_PATH_SEPARATOR == pustrPath->buffer[pustrPath->length - 1]))
+    {
+        pustrPath->length--;
+        pustrPath->buffer[pustrPath->length] = (sal_Unicode)'\0';
+    }
+
+    OSL_POSTCOND((0 == pustrPath->length) || (1 == pustrPath->length) || \
+                 (pustrPath->length > 1 && pustrPath->buffer[pustrPath->length - 1] != FPH_CHAR_PATH_SEPARATOR), \
+                 "osl_systemPathRemoveSeparator: Post condition failed");
+ }
+
+ /*******************************************
+    osl_systemPathEnsureSeparator
+  ******************************************/
+
+ void SAL_CALL osl_systemPathEnsureSeparator(rtl_uString** ppustrPath)
+ {
+     OSL_PRECOND(ppustrPath && (NULL != *ppustrPath), \
+                "osl_systemPathEnsureSeparator: Invalid parameter");
+
+     rtl::OUString path(*ppustrPath);
+    sal_Int32     lp = path.getLength();
+    sal_Int32     i  = path.lastIndexOf(FPH_CHAR_PATH_SEPARATOR);
+
+    if ((lp > 1 && i != (lp - 1)) || ((lp < 2) && i < 0))
+    {
+        path += FPH_PATH_SEPARATOR();
+        rtl_uString_assign(ppustrPath, path.pData);
+    }
+
+    OSL_POSTCOND(path.lastIndexOf(FPH_CHAR_PATH_SEPARATOR) == (path.getLength() - 1), \
+                 "osl_systemPathEnsureSeparator: Post condition failed");
+ }
+
+ /*******************************************
+  *  osl_systemPathIsRelativePath
+  ******************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsRelativePath(const rtl_uString* pustrPath)
+ {
+     OSL_PRECOND(pustrPath, "osl_systemPathIsRelativePath: Invalid parameter");
+     return (!osl_systemPathIsAbsolutePath(pustrPath));
+ }
+
+ /******************************************
+  *  osl_systemPathIsAbsolutePath
+  *****************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsAbsolutePath(const rtl_uString* pustrPath)
+ {
+     OSL_PRECOND(pustrPath, "osl_systemPathIsAbsolutePath: Invalid parameter");
+    if (pustrPath->length == 0)
+        return sal_False;
+    if (pustrPath->buffer[0] == FPH_CHAR_PATH_SEPARATOR)
+        return sal_True;
+    if (pustrPath->buffer[1] == FPH_CHAR_COLON
+            && pustrPath->buffer[2] == FPH_CHAR_PATH_SEPARATOR)
+        return sal_True;
+    return sal_False;
+ }
+
+ /******************************************
+    osl_systemPathMakeAbsolutePath
+  *****************************************/
+
+ void SAL_CALL osl_systemPathMakeAbsolutePath(
+     const rtl_uString* pustrBasePath,
+    const rtl_uString* pustrRelPath,
+    rtl_uString**      ppustrAbsolutePath)
+{
+    rtl::OUString base(rtl_uString_getStr(const_cast<rtl_uString*>(pustrBasePath)));
+    rtl::OUString rel(const_cast<rtl_uString*>(pustrRelPath));
+
+    if (base.getLength() > 0)
+        osl_systemPathEnsureSeparator(&base.pData);
+
+    base += rel;
+
+    rtl_uString_acquire(base.pData);
+    *ppustrAbsolutePath = base.pData;
+}
+
+
+ /*****************************************
+    osl_systemPathGetParent
+  ****************************************/
+
+ sal_Int32 SAL_CALL osl_systemPathGetParent(rtl_uString* pustrPath)
+ {
+     return 0;
+ }
+
+ /*******************************************
+     osl_systemPathGetFileOrLastDirectoryPart
+  ******************************************/
+
+ void SAL_CALL osl_systemPathGetFileNameOrLastDirectoryPart(
+     const rtl_uString*     pustrPath,
+    rtl_uString**       ppustrFileNameOrLastDirPart)
+{
+    OSL_PRECOND(pustrPath && ppustrFileNameOrLastDirPart, \
+                "osl_systemPathGetFileNameOrLastDirectoryPart: Invalid parameter");
+
+    rtl::OUString path(const_cast<rtl_uString*>(pustrPath));
+
+    osl_systemPathRemoveSeparator(path.pData);
+
+    rtl::OUString last_part;
+
+    if (path.getLength() > 1 || (1 == path.getLength() && *path.getStr() != FPH_CHAR_PATH_SEPARATOR))
+    {
+        sal_Int32 idx_ps = path.lastIndexOf(FPH_CHAR_PATH_SEPARATOR);
+        idx_ps++; // always right to increment by one even if idx_ps == -1!
+        last_part = rtl::OUString(path.getStr() + idx_ps);
+    }
+    rtl_uString_assign(ppustrFileNameOrLastDirPart, last_part.pData);
+}
+
+
+ /********************************************
+     osl_systemPathIsHiddenFileOrDirectoryEntry
+ *********************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsHiddenFileOrDirectoryEntry(
+     const rtl_uString* pustrPath)
+{
+    OSL_PRECOND(pustrPath, "osl_systemPathIsHiddenFileOrDirectoryEntry: Invalid parameter");
+
+    sal_Bool is_hidden = sal_False;
+
+    if (pustrPath->length > 0)
+    {
+        rtl::OUString fdp;
+
+        osl_systemPathGetFileNameOrLastDirectoryPart(pustrPath, &fdp.pData);
+
+        is_hidden = ((fdp.pData->length > 0) && (fdp.pData->buffer[0] == FPH_CHAR_DOT) &&
+                     !osl_systemPathIsLocalOrParentDirectoryEntry(fdp.pData));
+     }
+
+    return is_hidden;
+}
+
+
+ /************************************************
+     osl_systemPathIsLocalOrParentDirectoryEntry
+ ************************************************/
+
+sal_Bool SAL_CALL osl_systemPathIsLocalOrParentDirectoryEntry(
+    const rtl_uString* pustrPath)
+{
+    OSL_PRECOND(pustrPath, "osl_systemPathIsLocalOrParentDirectoryEntry: Invalid parameter");
+
+    rtl::OUString dirent;
+
+    osl_systemPathGetFileNameOrLastDirectoryPart(pustrPath, &dirent.pData);
+
+    return (
+            (dirent == FPH_LOCAL_DIR_ENTRY()) ||
+            (dirent == FPH_PARENT_DIR_ENTRY())
+           );
+}
+
+/***********************************************
+ Simple iterator for a path list separated by
+ the specified character
+ **********************************************/
+
+class path_list_iterator
+{
+public:
+
+    /******************************************
+      constructor
+
+     after construction get_current_item
+     returns the first path in list, no need
+     to call reset first
+     *****************************************/
+    path_list_iterator(const rtl::OUString& path_list, sal_Unicode list_separator = FPH_CHAR_COLON) :
+        m_path_list(path_list),
+        m_end(m_path_list.getStr() + m_path_list.getLength() + 1),
+        m_separator(list_separator)
+    {
+        reset();
+    }
+
+    /******************************************
+     reset the iterator
+     *****************************************/
+    void reset()
+    {
+        m_path_segment_begin = m_path_segment_end = m_path_list.getStr();
+        advance();
+    }
+
+    /******************************************
+     move the iterator to the next position
+     *****************************************/
+    void next()
+    {
+        OSL_PRECOND(!done(), "path_list_iterator: Already done!");
+
+        m_path_segment_begin = ++m_path_segment_end;
+        advance();
+    }
+
+    /******************************************
+     check if done
+     *****************************************/
+    bool done() const
+    {
+        return (m_path_segment_end >= m_end);
+    }
+
+    /******************************************
+     return the current item
+     *****************************************/
+    rtl::OUString get_current_item() const
+    {
+        return rtl::OUString(
+            m_path_segment_begin,
+            (m_path_segment_end - m_path_segment_begin));
+    }
+
+private:
+
+    /******************************************
+     move m_path_end to the next separator or
+     to the edn of the string
+     *****************************************/
+    void advance()
+    {
+        while (!done() && *m_path_segment_end && (*m_path_segment_end != m_separator))
+            ++m_path_segment_end;
+
+        OSL_ASSERT(m_path_segment_end <= m_end);
+    }
+
+private:
+    rtl::OUString       m_path_list;
+    const sal_Unicode*  m_end;
+    const sal_Unicode   m_separator;
+    const sal_Unicode*  m_path_segment_begin;
+    const sal_Unicode*  m_path_segment_end;
+
+// prevent copy and assignment
+private:
+    /******************************************
+     copy constructor
+     remember: do not simply copy m_path_begin
+     and m_path_end because they point to
+     the memory of other.m_path_list!
+     *****************************************/
+    path_list_iterator(const path_list_iterator& other);
+
+    /******************************************
+     assignment operator
+      remember: do not simply copy m_path_begin
+     and m_path_end because they point to
+     the memory of other.m_path_list!
+     *****************************************/
+    path_list_iterator& operator=(const path_list_iterator& other);
+};
+
+ /************************************************
+      osl_searchPath
+  ***********************************************/
+
+sal_Bool SAL_CALL osl_searchPath(
+     const rtl_uString* pustrFilePath,
+    const rtl_uString* pustrSearchPathList,
+    rtl_uString**      ppustrPathFound)
+{
+    OSL_PRECOND(pustrFilePath && pustrSearchPathList && ppustrPathFound, "osl_searchPath: Invalid parameter");
+
+    bool               bfound = false;
+    rtl::OUString      fp(const_cast<rtl_uString*>(pustrFilePath));
+    rtl::OUString      pl = rtl::OUString(const_cast<rtl_uString*>(pustrSearchPathList));
+    path_list_iterator pli(pl);
+
+    while (!pli.done())
+    {
+        rtl::OUString p = pli.get_current_item();
+        osl::systemPathEnsureSeparator(p);
+        p += fp;
+
+        if (osl::access(p, F_OK) > -1)
+        {
+            bfound = true;
+            rtl_uString_assign(ppustrPathFound, p.pData);
+            break;
+        }
+        pli.next();
+    }
+    return bfound;
+}
---
+++ sal/osl/os2/file_path_helper.h
@@ -0,0 +1,326 @@
+/*************************************************************************
+ *
+ *  $RCSfile: file_path_helper.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-09-20 15:10:39 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+ #ifndef _OSL_FILE_PATH_HELPER_H_
+ #define _OSL_FILE_PATH_HELPER_H_
+
+
+ #ifndef _SAL_TYPES_H_
+ #include <sal/types.h>
+ #endif
+
+ #ifndef _RTL_USTRING_H_
+ #include <rtl/ustring.h>
+ #endif
+
+
+ #ifdef __cplusplus
+ extern "C"
+ {
+ #endif
+
+
+ /*******************************************
+    osl_systemPathRemoveSeparator
+    Removes the last separator from the
+    given system path if any and if the path
+    is not the root path '/'
+
+    @param  ppustrPath [inout] a system path
+            if the path is not the root path
+            and the last character is a
+            path separator it will be cut off
+               ppustrPath must not be NULL and
+            must point to a valid rtl_uString
+
+    @returns nothing
+
+  ******************************************/
+
+ void SAL_CALL osl_systemPathRemoveSeparator(
+     /*inout*/ rtl_uString* pustrPath);
+
+ /*******************************************
+    osl_systemPathEnsureSeparator
+    Adds a trailing path separator to the
+    given system path if not already there
+    and if the path is not the root path '/'
+
+      @param    pustrPath [inout] a system path
+            if the path is not the root path
+            '/' and has no trailing separator
+            a separator will be added
+            ppustrPath must not be NULL and
+            must point to a valid rtl_uString
+
+    @returns nothing
+
+  ******************************************/
+
+ void SAL_CALL osl_systemPathEnsureSeparator(
+     /*inout*/ rtl_uString** ppustrPath);
+
+ /*******************************************
+    osl_systemPathIsRelativePath
+    Returns true if the given path is a
+    relative path and so starts not with '/'
+
+    @param  pustrPath [in] a system path
+            pustrPath must not be NULL
+
+    @returns sal_True if the given path
+             doesn't start with a separator
+             else sal_False will be returned
+
+  ******************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsRelativePath(
+     const rtl_uString* pustrPath);
+
+ /******************************************
+    osl_systemPathIsAbsolutePath
+    Returns true if the given path is an
+    absolute path and so starts with a '/'
+
+    @param pustrPath [in] a system path
+           pustrPath must not be NULL
+
+    @returns sal_True if the given path
+             start's with a separator else
+             sal_False will be returned
+
+  *****************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsAbsolutePath(
+     const rtl_uString* pustrPath);
+
+ /******************************************
+    osl_systemPathMakeAbsolutePath
+    Append a relative path to a base path
+
+    @param  pustrBasePath [in] a system
+            path that will be considered as
+            base path
+            pustrBasePath must not be NULL
+
+    @param  pustrRelPath [in] a system path
+            that will be considered as
+            relative path
+            pustrBasePath must not be NULL
+
+    @param  ppustrAbsolutePath [out] the
+            resulting path which is a
+            concatination of the base and
+            the relative path
+            if base path is empty the
+            resulting absolute path is the
+            relative path
+            if relative path is empty the
+            resulting absolute path is the
+            base path
+            if base and relative path are
+            empty the resulting absolute
+            path is also empty
+            ppustrAbsolutePath must not be
+            NULL and *ppustrAbsolutePath
+            must be 0 or point to a valid
+            rtl_uString
+
+  *****************************************/
+
+ void SAL_CALL osl_systemPathMakeAbsolutePath(
+     const rtl_uString* pustrBasePath,
+    const rtl_uString* pustrRelPath,
+    rtl_uString**      ppustrAbsolutePath);
+
+ /*****************************************
+    osl_systemPathGetParent
+    Replaces the last occurrance of a path
+    separator with '\0' and returns the
+    position where the '/' was replaced
+
+    @param  pustrPath [inout] a system
+            path, the last separator of
+            this path will be replaced by
+            a '\0'
+            if the path is the root path
+            '/' or the path is considered
+            as to have no parent, e.g.
+            '/NoParent' or 'NoParent' or
+            the path is empty no
+            replacement will be made
+            pustrPath must not be NULL
+
+    @returns the position of the last path
+             separator that was replaced
+             or 0 if no replacement took
+             place
+
+  ****************************************/
+
+ sal_Int32 SAL_CALL osl_systemPathGetParent(
+    /*inout*/ rtl_uString* pustrPath);
+
+ /*****************************************
+     osl_systemPathGetFileOrLastDirectoryPart
+    Returns the file or the directory part
+    of the given path
+
+    @param pustrPath [in] a system path,
+           must not be NULL
+
+    @param ppustrFileOrDirPart [out] on
+           return receives the last part
+           of the given directory or the
+           file name
+           if pustrPath is the root path
+           '/' an empty string will be
+           returned
+           if pustrPath has a trailing
+           '/' the last part before the
+           '/' will be returned else
+           the part after the last '/'
+           will be returned
+
+    @returns nothing
+
+  ****************************************/
+ void SAL_CALL osl_systemPathGetFileNameOrLastDirectoryPart(
+     const rtl_uString*     pustrPath,
+    rtl_uString**       ppustrFileNameOrLastDirPart);
+
+
+ /********************************************
+     osl_systemPathIsHiddenFileOrDirectoryEntry
+    Returns sal_True if the last part of
+    given system path is not '.' or '..'
+    alone and starts with a '.'
+
+    @param pustrPath [in] a system path,
+           must not be NULL
+
+    @returns sal_True if the last part of
+             the given system path starts
+             with '.' or sal_False the last
+             part is '.' or '..' alone or
+             doesn't start with a dot
+
+ *********************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsHiddenFileOrDirectoryEntry(
+     const rtl_uString* pustrPath);
+
+
+ /************************************************
+     osl_systemPathIsLocalOrParentDirectoryEntry
+    Returns sal_True if the last part of the given
+    system path is the local directory entry '.'
+    or the parent directory entry '..'
+
+    @param pustrPath [in] a system path,
+           must not be NULL
+
+    @returns sal_True if the last part of the
+             given system path is '.' or '..'
+             else sal_False
+
+ ************************************************/
+
+ sal_Bool SAL_CALL osl_systemPathIsLocalOrParentDirectoryEntry(
+     const rtl_uString* pustrPath);
+
+
+ /************************************************
+      osl_searchPath
+    Searches for a file name or path name in all
+    directories specified by a given path list.
+    Symbolic links in the resulting path will not be
+    resolved, it's up to the caller to do this.
+
+    @param pustrFilePath [in] a file name or
+    directory name to search for, the name must
+    be provided as system path not as a file URL
+
+    @param pustrSearchPathList [in] a ':'
+    separated list of paths in which to search for
+    the file or directory name
+
+    @ppustrPathFound [out] on success receives the
+    complete path of the file or directory found
+    as a system path
+
+    @returns sal_True if the specified file or
+    directory was found else sal_False
+  ***********************************************/
+
+ sal_Bool SAL_CALL osl_searchPath(
+     const rtl_uString* pustrFilePath,
+    const rtl_uString* pustrSearchPathList,
+    rtl_uString**      ppustrPathFound);
+
+
+ #ifdef __cplusplus
+ }
+ #endif
+
+
+ #endif /* #ifndef _OSL_PATH_HELPER_H_ */
+
