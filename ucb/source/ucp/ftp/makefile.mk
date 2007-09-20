da7cecb8c4400b544bdecce14718f9a42d07af43
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 15:27:58 +0000

    INTEGRATION: CWS os2port01 (1.14.90); FILE MERGED 2007/08/11 20:49:44 obr 1.14.90.2: RESYNC: (1.14-1.16); FILE MERGED 2006/12/28 15:08:03 ydario 1.14.90.1: OS/2 initial import.

--- ucb/source/ucp/ftp/makefile.mk
+++ ucb/source/ucp/ftp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.16 $
+#   $Revision: 1.17 $
 #
-#   last change: $Author: ihi $ $Date: 2007-06-05 18:02:00 $
+#   last change: $Author: vg $ $Date: 2007-09-20 16:27:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,10 @@ SHL1STDLIBS=\
     $(SALLIB)  \
     $(UCBHELPERLIB) \
     $(CURLLIB)
+
+.IF "$(GUI)" == "OS2"
+SHL1STDLIBS+=ssl.lib crypto.lib libz.lib
+.ENDIF
 
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
 SHL1LIBS= \
