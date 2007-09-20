0973258923d8a3be3196b963e5e7186e843f20d5
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 15:01:47 +0000

    INTEGRATION: CWS os2port01 (1.2.2); FILE ADDED 2006/11/29 14:33:54 ydario 1.2.2.1: Initial OS/2 import.

---
+++ vcl/os2/inc/salframe.h
@@ -0,0 +1,203 @@
+/*************************************************************************
+ *
+ *  $RCSfile: salframe.h,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-09-20 16:01:47 $
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU Lesser General Public License Version 2.1
+ *         - Sun Industry Standards Source License Version 1.1
+ *
+ *  Sun Microsystems Inc., October, 2000
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2000 by Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *
+ *  Sun Industry Standards Source License Version 1.1
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.1 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://www.openoffice.org/license.html.
+ *
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ *
+ *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *
+ *  Copyright: 2000 by Sun Microsystems, Inc.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor(s): _______________________________________
+ *
+ *
+ ************************************************************************/
+
+#ifndef _SV_SALFRAME_H
+#define _SV_SALFRAME_H
+
+#ifndef _SV_SV_H
+#include <sv.h>
+#endif
+
+#ifndef _SV_SYSDATA_HXX
+#include <sysdata.hxx>
+#endif
+
+#ifndef _SV_SALFRAME_HXX
+#include <salframe.hxx>
+#endif
+
+
+#ifndef PM_BIDI_INCLUDED
+#include <pmbidi.h>
+#endif
+
+#ifndef __UCONV_H__
+#include <uconv.h>
+#endif
+
+#ifndef __UNIDEF_H__
+#include <unidef.h>
+#endif
+
+#ifndef __UNIKBD_H__
+#include <unikbd.h>
+#endif
+
+//class SalFrame;
+class Os2SalGraphics;
+
+// ----------------
+// - SalFrameData -
+// ----------------
+
+//class SalFrameData
+class Os2SalFrame : public SalFrame
+{
+public:
+    HWND                    mhWndFrame;             // HWND-Frame
+    HWND                    mhWndClient;            // HWND-Client
+    HAB                     mhAB;                   // HAB
+    HPOINTER                mhPointer;              // Current MousePointer
+    void*                   mpInst;                 // VCL-Instance
+    //SALFRAMEPROC          mpProc;                 // VCL-Proc
+    Os2SalGraphics*         mpGraphics;             // current frame graphics
+    Os2SalFrame*                mpNextFrame;            // pointer to next frame
+    SystemEnvData           maSysData;              // system data
+    SalFrameState           maState;                // frame state
+    int                     mnShowState;            // show state
+    ULONG                   mnStyle;                // SalFrameStyle
+    long                    mnWidth;                // Window-Witdth
+    long                    mnHeight;               // Window-Height
+    SWP                     maFullScreenRect;       // WindowRect befor FullScreenMode
+    BOOL                    mbGraphics;             // is Graphics used
+    BOOL                    mbAllwayOnTop;          // Allways on top modus
+    BOOL                    mbVisible;              // Visible Show/Hide-Status
+    BOOL                    mbMinHide;              // hide called from OS2
+    BOOL                    mbHandleIME;            // TRUE: Wir handeln die IME-Messages
+    BOOL                    mbConversionMode;       // TRUE: Wir befinden uns im Conversion-Modus
+    BOOL                    mbCandidateMode;        // TRUE: Wir befinden uns im Candidate-Modus
+    BOOL                    mbCaption;              // has window a caption
+    BOOL                    mbBorder;               // has window a border
+    BOOL                    mbFixBorder;            // has window a fixed border
+    BOOL                    mbSizeBorder;           // has window a sizeable border
+    BOOL                    mbNoIcon;               // is an window without an icon
+    BOOL                    mbFloatWin;             // is a FloatingWindow
+    BOOL                    mbFullScreen;           // TRUE: in full screen mode
+    BOOL                    mbPresentation;         // TRUE: Presentation Mode running
+    BOOL                    mbInShow;               // innerhalb eines Show-Aufrufs
+    BOOL                    mbRestoreMaximize;      // Restore-Maximize
+    BOOL                    mbInMoveMsg;            // Move-Message wird verarbeitet
+    BOOL                    mbInSizeMsg;            // Size-Message wird verarbeitet
+    BOOL                    mbFullScreenToolWin;    // WS_EX_TOOLWINDOW reset in FullScreenMode
+    BOOL                    mbDefPos;               // default-position
+    BOOL                    mbOverwriteState;       // TRUE: WindowState darf umgesetzt werden
+
+    int                     mnMinWidth;             // min. client width in pixeln
+    int                     mnMinHeight;            // min. client height in pixeln
+    int                     mnMaxWidth;             // max. client width in pixeln
+    int                     mnMaxHeight;            // max. client height in pixeln
+    static ULONG            mnInputLang;            // current Input Language
+    KHAND                   mnKeyboardHandle;       // current unicode keyboard
+    static BOOL             mbInReparent;           // TRUE: ignore focus lost and gain due to reparenting
+
+private:
+    Os2SalFrame*            mpParentFrame;          // parent frame pointer
+
+public:
+    Os2SalFrame();
+    virtual ~Os2SalFrame();
+
+    virtual SalGraphics*        GetGraphics();
+    virtual void                ReleaseGraphics( SalGraphics* pGraphics );
+    virtual BOOL                PostEvent( void* pData );
+    virtual void                SetTitle( const XubString& rTitle );
+    virtual void                SetIcon( USHORT nIcon );
+    virtual void                                SetMenu( SalMenu* pSalMenu );
+    virtual void                                DrawMenuBar();
+    virtual void                SetExtendedFrameStyle( SalExtStyle nExtStyle );
+    virtual void                Show( BOOL bVisible, BOOL bNoActivate = FALSE );
+    virtual void                Enable( BOOL bEnable );
+    virtual void                SetMinClientSize( long nWidth, long nHeight );
+    virtual void                SetMaxClientSize( long nWidth, long nHeight );
+    virtual void                SetPosSize( long nX, long nY, long nWidth, long nHeight, USHORT nFlags );
+    virtual void                GetClientSize( long& rWidth, long& rHeight );
+    virtual void                GetWorkArea( RECTL& rRect );
+    virtual void                GetWorkArea( Rectangle& rRect );
+    virtual SalFrame*           GetParent() const;
+    virtual void                SetWindowState( const SalFrameState* pState );
+    virtual BOOL                GetWindowState( SalFrameState* pState );
+    virtual void                ShowFullScreen( BOOL bFullScreen );
+    virtual void                StartPresentation( BOOL bStart );
+    virtual void                SetAlwaysOnTop( BOOL bOnTop );
+    virtual void                ToTop( USHORT nFlags );
+    virtual void                SetPointer( PointerStyle ePointerStyle );
+    virtual void                CaptureMouse( BOOL bMouse );
+    virtual void                SetPointerPos( long nX, long nY );
+    virtual void                Flush();
+    virtual void                Sync();
+    virtual void                SetInputContext( SalInputContext* pContext );
+    virtual void                EndExtTextInput( USHORT nFlags );
+    virtual String              GetKeyName( USHORT nKeyCode );
+    virtual String              GetSymbolKeyName( const XubString& rFontName, USHORT nKeyCode );
+    virtual BOOL                MapUnicodeToKeyCode( sal_Unicode aUnicode, LanguageType aLangType, KeyCode& rKeyCode );
+    virtual LanguageType        GetInputLanguage();
+    virtual SalBitmap*          SnapShot();
+    virtual void                UpdateSettings( AllSettings& rSettings );
+    virtual void                Beep( SoundType eSoundType );
+    virtual const SystemEnvData*    GetSystemData() const;
+    virtual SalPointerState     GetPointerState();
+    virtual void                SetParent( SalFrame* pNewParent );
+    virtual bool                SetPluginParent( SystemParentData* pNewParent );
+    virtual void                SetBackgroundBitmap( SalBitmap* );
+
+};
+
+#endif // _SV_SALFRAME_H
