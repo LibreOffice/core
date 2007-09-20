21deca86dd142f8788623eda8d22442e172531dd
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 14:58:48 +0000

    INTEGRATION: CWS os2port01 (1.9.48); FILE MERGED 2007/08/13 11:17:09 obr 1.9.48.2: RESYNC: (1.9-1.10); FILE MERGED 2006/12/28 15:05:48 ydario 1.9.48.1: OS/2 initial import.

--- soltools/cpp/makefile.mk
+++ soltools/cpp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 09:20:47 $
+#   last change: $Author: vg $ $Date: 2007-09-20 15:58:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,9 @@ OBJFILES= \
     $(OBJ)$/_unix.obj
 
 APP1TARGET	=	$(TARGET)
+.IF "$(GUI)" != "OS2"
 APP1STACK	=	1000000
+.ENDIF
 APP1LIBS	=	$(LB)$/$(TARGET).lib
 APP1DEPN	=   $(LB)$/$(TARGET).lib
 
