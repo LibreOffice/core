1ab720ee1878b24384462e2e625b719073138aef
Vladimir Glazounov <vg@openoffice.org>
2007-09-20 13:43:54 +0000

    INTEGRATION: CWS os2port01 (1.10.64); FILE MERGED 2007/09/05 07:00:12 obr 1.10.64.3: RESYNC: (1.11-1.12); FILE MERGED 2007/08/06 10:29:17 obr 1.10.64.2: RESYNC: (1.10-1.11); FILE MERGED 2006/12/28 14:53:43 ydario 1.10.64.1: OS/2 initial import.

--- cppu/inc/uno/environment.h
+++ cppu/inc/uno/environment.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: environment.h,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-18 12:20:15 $
+ *  last change: $Author: vg $ $Date: 2007-09-20 14:43:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,10 +52,10 @@ extern "C"
 struct _uno_ExtEnvironment;
 struct _typelib_InterfaceTypeDescription;
 
-#ifdef SAL_W32
+#if defined( SAL_W32)
 #pragma pack(push, 8)
 #elif defined(SAL_OS2)
-#pragma pack(8)
+#pragma pack(push, 8)
 #endif
 
 /** The binary specification of an UNO environment.
@@ -267,10 +267,8 @@ typedef struct _uno_ExtEnvironment
 
 } uno_ExtEnvironment;
 
-#ifdef SAL_W32
+#if defined( SAL_W32) ||  defined(SAL_OS2)
 #pragma pack(pop)
-#elif defined(SAL_OS2)
-#pragma pack()
 #endif
 
 /** Function exported by some bridge library providing acquireInterface(), releaseInterface();
