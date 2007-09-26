e89a46fdab3e8a4819ef57a3ca2ccd39eee00844
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-26 13:46:02 +0000

    INTEGRATION: CWS dba24a (1.6.108); FILE MERGED 2007/07/30 11:27:11 fs 1.6.108.1: #i80175# no 'using namespace ::rtl'

--- dbaccess/source/filter/xml/xmlservices.cxx
+++ dbaccess/source/filter/xml/xmlservices.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlservices.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 13:33:48 $
+ *  last change: $Author: hr $ $Date: 2007-09-26 14:46:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,6 @@
 
 /********************************************************************************************/
 
-using namespace ::rtl;
 using namespace ::dbaxml;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
