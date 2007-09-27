7dce2735635128d669491659722c7ead38b5538f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:15:18 +0000

    INTEGRATION: CWS swwarnings (1.2.710); FILE MERGED 2007/03/05 12:45:51 tl 1.2.710.1: #i69287# warning-free code

--- sw/source/ui/inc/wolesh.hxx
+++ sw/source/ui/inc/wolesh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wolesh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:18:23 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 12:15:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 class SwWebOleShell: public SwOleShell
 {
 public:
-    SFX_DECL_INTERFACE(SW_WEBOLESHELL);
+    SFX_DECL_INTERFACE(SW_WEBOLESHELL)
 
     virtual     ~SwWebOleShell();
                 SwWebOleShell(SwView &rView);
