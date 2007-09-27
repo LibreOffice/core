65b53f221b0b64fa764c53f5dcf1103f82119be6
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:10:25 +0000

    INTEGRATION: CWS swwarnings (1.3.710); FILE MERGED 2007/02/22 15:06:48 tl 1.3.710.1: #i69287# warning-free code

--- sw/source/ui/inc/swcont.hxx
+++ sw/source/ui/inc/swcont.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swcont.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:02:53 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 12:10:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -101,7 +101,7 @@ public:
     virtual BOOL            IsProtect() const;
     const SwContentType*    GetParent() const {return pParent;}
     const String&           GetName()   const {return sContentName;}
-    int                     operator==(const SwContent& rCont) const
+    int                     operator==(const SwContent& /*rCont*/) const
                                 {
                                     //gleich sind sie nie, sonst fallen sie aus dem Array
                                     return FALSE;
