ed328a8c329e4a28666308c73718acd95c903bcc
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:18:28 +0000

    INTEGRATION: CWS swwarnings (1.3.632); FILE MERGED 2007/04/03 12:59:19 tl 1.3.632.1: #i69287# warning-free code

--- sw/source/core/SwNumberTree/makefile.mk
+++ sw/source/core/SwNumberTree/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-11-11 10:52:04 $
+#   last change: $Author: hr $ $Date: 2007-09-27 08:18:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -46,6 +46,9 @@ TARGET=SwNumberTree
 SLOFILES =	\
         $(SLO)$/SwNumberTree.obj \
         $(SLO)$/SwNodeNum.obj
+
+EXCEPTIONSFILES = \
+        $(SLO)$/SwNumberTree.obj 
 
 # --- Tagets -------------------------------------------------------
 
