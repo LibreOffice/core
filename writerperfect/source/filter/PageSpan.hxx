cdbabb0f175de3ee10fcc3a96366cd8902f68045
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:55:52 +0000

    INTEGRATION: CWS swwarnings (1.3.6); FILE MERGED 2007/04/13 11:16:01 os 1.3.6.1: warning removed

--- writerperfect/source/filter/PageSpan.hxx
+++ writerperfect/source/filter/PageSpan.hxx
@@ -26,7 +26,13 @@
  */
 #ifndef _PAGESPAN_H
 #define _PAGESPAN_H
+#if defined _MSC_VER
+#pragma warning( push, 1 )
+#endif
 #include <libwpd/libwpd.h>
+#if defined _MSC_VER
+#pragma warning( pop )
+#endif
 #include <vector>
 
 class DocumentElement;
