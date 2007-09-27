ab38191fe21d1b4a5e5518ee1536b057f60353b5
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:40:36 +0000

    INTEGRATION: CWS swwarnings (1.7.222); FILE MERGED 2007/03/08 17:29:48 ama 1.7.222.1: #i69287#: warning free code

--- sw/source/core/doc/visiturl.cxx
+++ sw/source/core/doc/visiturl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: visiturl.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 20:59:25 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 08:40:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,7 +81,7 @@ SwURLStateChanged::~SwURLStateChanged()
     EndListening( *INetURLHistory::GetOrCreate() );
 }
 
-void SwURLStateChanged::Notify( SfxBroadcaster& rBC, const SfxHint& rHint )
+void SwURLStateChanged::Notify( SfxBroadcaster& , const SfxHint& rHint )
 {
     if( rHint.ISA( INetURLHistoryHint ) && pDoc->GetRootFrm() )
     {
