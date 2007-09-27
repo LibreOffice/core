e4a762ef767526d28476d7bdee62db6d7a97b700
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 08:04:40 +0000

    INTEGRATION: CWS swwarnings (1.8.222); FILE MERGED 2007/03/08 08:23:53 fme 1.8.222.1: #i69287# Warning free code

--- sw/source/core/layout/movedfwdfrmsbyobjpos.cxx
+++ sw/source/core/layout/movedfwdfrmsbyobjpos.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: movedfwdfrmsbyobjpos.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:20:39 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 09:04:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -104,10 +104,10 @@ bool SwMovedFwdFrmsByObjPos::DoesRowContainMovedFwdFrm( const SwRowFrm& _rRowFrm
         const NodeMapEntry& rEntry = *(aIter);
         if ( rEntry.second >= nPageNumOfRow )
         {
-            SwClientIter aIter( *const_cast<SwTxtNode*>( rEntry.first ) );
-            for( SwTxtFrm* pTxtFrm = (SwTxtFrm*)aIter.First( TYPE(SwTxtFrm) );
+            SwClientIter aFrmIter( *const_cast<SwTxtNode*>( rEntry.first ) );
+            for( SwTxtFrm* pTxtFrm = (SwTxtFrm*)aFrmIter.First( TYPE(SwTxtFrm) );
                  pTxtFrm;
-                 pTxtFrm = (SwTxtFrm*)aIter.Next() )
+                 pTxtFrm = (SwTxtFrm*)aFrmIter.Next() )
             {
                 // --> OD 2004-12-03 #115759# - assure that found text frame
                 // is the first one.
