542e316f18c9e26bad450871e1b78c9aaa552ad2
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 08:58:23 +0000

    INTEGRATION: CWS swwarnings (1.7.222); FILE MERGED 2007/03/15 15:52:15 tl 1.7.222.1: #i69287# warning-free code

--- sw/source/filter/ww1/w1par.cxx
+++ sw/source/filter/ww1/w1par.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: w1par.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:19:35 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 09:58:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -96,7 +96,7 @@ static ULONG WW1_Read_FieldIniFlags()
 // Diese werden dann einfach per Pipe 'uebertragen'.
 //
 
-ULONG WW1Reader::Read(SwDoc& rDoc, const String& rBaseURL, SwPaM& rPam, const String& cName)
+ULONG WW1Reader::Read(SwDoc& rDoc, const String& rBaseURL, SwPaM& rPam, const String& /*cName*/)
 {
     ULONG nRet = ERR_SWG_READ_ERROR;
     ASSERT(pStrm!=NULL, "W1-Read ohne Stream");
