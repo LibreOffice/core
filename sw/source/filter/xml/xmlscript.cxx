6cf4bc31c56386e752b9a59ae8a406e9816f564b
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 09:13:21 +0000

    INTEGRATION: CWS swwarnings (1.6.222); FILE MERGED 2007/04/11 07:03:11 tl 1.6.222.1: #i69287# warning-free code

--- sw/source/filter/xml/xmlscript.cxx
+++ sw/source/filter/xml/xmlscript.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlscript.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:31:28 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 10:13:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
