0b52e955bc6f53cbf910af9054dc0c6d378f4f55
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 08:48:04 +0000

    INTEGRATION: CWS swwarnings (1.6.222); FILE MERGED 2007/04/03 13:00:41 tl 1.6.222.1: #i69287# warning-free code

--- sw/source/filter/html/htmlflyt.cxx
+++ sw/source/filter/html/htmlflyt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: htmlflyt.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:09:52 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 09:48:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,9 +36,8 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
 
-
-
 #include "htmlfly.hxx"
+
 
 #define TE(t,p,c) (BYTE)( HTML_OUT_##t | HTML_POS_##p | HTML_CNTNR_##c )
 
