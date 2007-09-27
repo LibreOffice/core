5379038bfc7c1fbafb55ec4163256b6b91fbbbef
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:43:46 +0000

    INTEGRATION: CWS swwarnings (1.4.710); FILE MERGED 2007/04/03 12:59:46 tl 1.4.710.1: #i69287# warning-free code

--- sw/source/core/draw/makefile.mk
+++ sw/source/core/draw/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 03:25:12 $
+#   last change: $Author: hr $ $Date: 2007-09-27 08:43:46 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,6 +58,8 @@ SLOFILES =  \
         $(SLO)$/dpage.obj
 
 EXCEPTIONSFILES= \
+                $(SLO)$/dview.obj	\
+                $(SLO)$/dcontact.obj	\
                 $(SLO)$/drawdoc.obj
 
 # --- Tagets -------------------------------------------------------
