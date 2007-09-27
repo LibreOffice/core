142ce137f9fc67784fcb716ab21bfe069571ee68
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 10:54:02 +0000

    INTEGRATION: CWS swwarnings (1.2.708); FILE MERGED 2007/03/05 12:45:44 tl 1.2.708.1: #i69287# warning-free code

--- sw/source/ui/inc/beziersh.hxx
+++ sw/source/ui/inc/beziersh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: beziersh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:00:14 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 11:54:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 class SwBezierShell: public SwBaseShell
 {
 public:
-    SFX_DECL_INTERFACE(SW_BEZIERSHELL);
+    SFX_DECL_INTERFACE(SW_BEZIERSHELL)
     TYPEINFO();
 
             SwBezierShell(SwView &rView);
