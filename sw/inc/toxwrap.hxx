2fdcacabc273d0b8669b02a01c743cb86fd6b93e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:13:59 +0000

    INTEGRATION: CWS swwarnings (1.5.710); FILE MERGED 2007/03/05 12:43:18 tl 1.5.710.1: #i69287# warning-free code

--- sw/inc/toxwrap.hxx
+++ sw/inc/toxwrap.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: toxwrap.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:21:07 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 08:13:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ namespace i18n {
 namespace lang {
     class XMultiServiceFactory;
 }
-}}};
+}}}
 
 
 class String;
