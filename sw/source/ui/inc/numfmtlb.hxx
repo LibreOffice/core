99ccc41c25afd51c1d146d50c711dfe037cee3e1
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:05:52 +0000

    INTEGRATION: CWS swwarnings (1.6.710); FILE MERGED 2007/03/26 12:09:08 tl 1.6.710.1: #i69287# warning-free code

--- sw/source/ui/inc/numfmtlb.hxx
+++ sw/source/ui/inc/numfmtlb.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: numfmtlb.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:53:24 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 12:05:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
