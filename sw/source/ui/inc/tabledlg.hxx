6f165a1af05f9dbe1195a4b9c69c9db1dc3f8e0c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:11:43 +0000

    INTEGRATION: CWS swwarnings (1.4.710); FILE MERGED 2007/04/13 12:18:05 tl 1.4.710.2: #i69287# binfilter related comments removed 2007/02/28 12:23:29 os 1.4.710.1: #i69287# warnings removed

--- sw/source/ui/inc/tabledlg.hxx
+++ sw/source/ui/inc/tabledlg.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tabledlg.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:07:26 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 12:11:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 
 
 #include "swtypes.hxx"
-#include "swtablerep.hxx" //CHINA001
+#include "swtablerep.hxx"
 
 class SwWrtShell;
 class SwTabCols;
@@ -61,61 +61,5 @@ public:
                     const SfxItemSet* pItemSet, SwWrtShell* pSh );
 };
 
-//CHINA001 class SwTableRep
-//CHINA001 {
-//CHINA001 TColumn*     pTColumns;
-//CHINA001
-//CHINA001 SwTwips      nTblWidth;
-//CHINA001 SwTwips      nSpace;
-//CHINA001 SwTwips      nLeftSpace;
-//CHINA001 SwTwips  nRightSpace;
-//CHINA001 USHORT       nAlign;
-//CHINA001 USHORT       nColCount;
-//CHINA001 USHORT       nAllCols;
-//CHINA001 USHORT       nWidthPercent;
-//CHINA001 BOOL         bComplex : 1;
-//CHINA001 BOOL         bLineSelected : 1;
-//CHINA001 BOOL     bWidthChanged : 1;
-//CHINA001 BOOL     bColsChanged : 1;
-//CHINA001
-//CHINA001 public:
-//CHINA001 SwTableRep( const SwTabCols& rTabCol, BOOL bComplex );
-//CHINA001 ~SwTableRep();
-//CHINA001
-//CHINA001 BOOL     FillTabCols( SwTabCols& rTabCol ) const;
-//CHINA001
-//CHINA001 SwTwips  GetLeftSpace() const            {return nLeftSpace;}
-//CHINA001 void         SetLeftSpace(SwTwips nSet)      {nLeftSpace = nSet;}
-//CHINA001
-//CHINA001 SwTwips      GetRightSpace() const           {return nRightSpace;}
-//CHINA001 void         SetRightSpace(SwTwips nSet)     {nRightSpace = nSet;}
-//CHINA001
-//CHINA001 SwTwips      GetWidth() const                {return nTblWidth;}
-//CHINA001 void         SetWidth(SwTwips nSet)          {nTblWidth = nSet;}
-//CHINA001
-//CHINA001 USHORT       GetWidthPercent() const         {return nWidthPercent;}
-//CHINA001 void         SetWidthPercent(USHORT nSet)    {nWidthPercent = nSet;}
-//CHINA001
-//CHINA001 USHORT       GetAlign() const                {return nAlign;}
-//CHINA001 void         SetAlign(USHORT nSet)           {nAlign = nSet;}
-//CHINA001
-//CHINA001 BOOL     IsComplex() const               {return bComplex;}
-//CHINA001 USHORT       GetColCount() const             {return nColCount;}
-//CHINA001 USHORT       GetAllColCount() const          {return nAllCols;}
-//CHINA001
-//CHINA001 BOOL     HasColsChanged() const          {return bColsChanged;}
-//CHINA001 void     SetColsChanged()                {bColsChanged = TRUE;}
-//CHINA001
-//CHINA001 BOOL     HasWidthChanged() const         {return bWidthChanged;}
-//CHINA001 void         SetWidthChanged()               {bWidthChanged  = TRUE;}
-//CHINA001
-//CHINA001 BOOL         IsLineSelected() const          {return bLineSelected;}
-//CHINA001 void     SetLineSelected(BOOL bSet)      {bLineSelected = bSet;}
-//CHINA001
-//CHINA001 SwTwips      GetSpace() const                { return nSpace;}
-//CHINA001 void         SetSpace(SwTwips nSet)          {nSpace = nSet;}
-//CHINA001
-//CHINA001 TColumn*     GetColumns() const              {return pTColumns;}
-//CHINA001 };
 
 #endif
