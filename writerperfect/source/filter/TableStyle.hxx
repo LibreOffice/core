f1960d7b7b8146f8fb1dec3f23737e2b99cec344
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:56:35 +0000

    INTEGRATION: CWS swwarnings (1.3.6); FILE MERGED 2007/04/13 11:16:03 os 1.3.6.1: warning removed

--- writerperfect/source/filter/TableStyle.hxx
+++ writerperfect/source/filter/TableStyle.hxx
@@ -27,7 +27,13 @@
  */
 #ifndef _TABLESTYLE_H
 #define _TABLESTYLE_H
+#if defined _MSC_VER
+#pragma warning( push, 1 )
+#endif
 #include <libwpd/libwpd.h>
+#if defined _MSC_VER
+#pragma warning( pop )
+#endif
 #include <vector>
 
 #include "Style.hxx"
