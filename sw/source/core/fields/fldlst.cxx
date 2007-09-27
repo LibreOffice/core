90efcc9170b999f8ca30fc70f0e8a41d4b1e54de
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:49:36 +0000

    INTEGRATION: CWS swwarnings (1.6.222); FILE MERGED 2007/02/22 15:06:29 tl 1.6.222.1: #i69287# warning-free code

--- sw/source/core/fields/fldlst.cxx
+++ sw/source/core/fields/fldlst.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fldlst.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 21:12:20 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 08:49:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@
     Beschreibung: Sortieren der Input-Eintraege
  --------------------------------------------------------------------*/
 
-SwInputFieldList::SwInputFieldList( SwEditShell* pShell, FASTBOOL bBuildTmpLst )
+SwInputFieldList::SwInputFieldList( SwEditShell* pShell, BOOL bBuildTmpLst )
     : pSh(pShell)
 {
     // Hier die Liste aller Eingabefelder sortiert erstellen
