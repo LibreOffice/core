f8e2cec3fe8edd6e75a9a01bb7922905a9493e30
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 09:12:36 +0000

    INTEGRATION: CWS swwarnings (1.16.62); FILE MERGED 2007/08/20 15:50:08 tl 1.16.62.2: RESYNC: (1.16-1.17); FILE MERGED 2007/05/30 13:31:43 os 1.16.62.1: warnings removed

--- sw/source/filter/xml/xmlitemm.cxx
+++ sw/source/filter/xml/xmlitemm.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlitemm.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2007-08-03 12:58:58 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 10:12:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@
 
 using namespace ::xmloff::token;
 
-#define M_E( p, l, w, m ) \
+#define MAP_ENTRY( p, l, w, m ) \
     { XML_NAMESPACE_##p, XML_##l, w, m }
 #define M_E_SI( p, l, w, m ) \
     { XML_NAMESPACE_##p, XML_##l, w, MID_SW_FLAG_SPECIAL_ITEM_IMPORT|m }
@@ -82,13 +82,13 @@ SvXMLItemMapEntry aXMLTableItemMap[] =
     M_E_SE( FO, MARGIN_LEFT,        RES_LR_SPACE,   MID_L_MARGIN ),
     M_E_SE( FO, MARGIN_RIGHT,       RES_LR_SPACE,   MID_R_MARGIN ),
     // RES_UL_SPACE
-    M_E( FO,    MARGIN_TOP,         RES_UL_SPACE,   MID_UP_MARGIN ),
-    M_E( FO,    MARGIN_BOTTOM,      RES_UL_SPACE,   MID_LO_MARGIN ),
+    MAP_ENTRY( FO,   MARGIN_TOP,         RES_UL_SPACE,   MID_UP_MARGIN ),
+    MAP_ENTRY( FO,   MARGIN_BOTTOM,      RES_UL_SPACE,   MID_LO_MARGIN ),
     // RES_PAGEDESC
-    M_E( STYLE, PAGE_NUMBER,        RES_PAGEDESC,   MID_PAGEDESC_PAGENUMOFFSET),
+    MAP_ENTRY( STYLE,    PAGE_NUMBER,        RES_PAGEDESC,   MID_PAGEDESC_PAGENUMOFFSET),
     // RES_BREAK
-    M_E( FO,    BREAK_BEFORE,       RES_BREAK,  MID_BREAK_BEFORE ),
-    M_E( FO,    BREAK_AFTER,        RES_BREAK,  MID_BREAK_AFTER ),
+    MAP_ENTRY( FO,   BREAK_BEFORE,       RES_BREAK,  MID_BREAK_BEFORE ),
+    MAP_ENTRY( FO,   BREAK_AFTER,        RES_BREAK,  MID_BREAK_AFTER ),
     // RES_CNTNT
     // not required
     // RES_HEADER
@@ -106,28 +106,28 @@ SvXMLItemMapEntry aXMLTableItemMap[] =
     // RES_VERT_ORIENT
     // not required
     // RES_HORI_ORIENT
-    M_E( TABLE, ALIGN,                  RES_HORI_ORIENT, 0 ),
+    MAP_ENTRY( TABLE,    ALIGN,                  RES_HORI_ORIENT, 0 ),
     // RES_ANCHOR
     // not required
     // RES_BACKGROUND
-    M_E( FO,    BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
-    M_E( STYLE, BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
+    MAP_ENTRY( FO,   BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
+    MAP_ENTRY( STYLE,    BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
     // RES_BOX
     // not required
     // RES_SHADOW
-    M_E( STYLE, SHADOW,             RES_SHADOW, 0 ),
+    MAP_ENTRY( STYLE,    SHADOW,             RES_SHADOW, 0 ),
     // RES_FRMMACRO
     // not required
     // RES_COL
     // not required
     // RES_KEEP
-    M_E( FO, KEEP_WITH_NEXT,        RES_KEEP, 0 ),
+    MAP_ENTRY( FO, KEEP_WITH_NEXT,       RES_KEEP, 0 ),
     // RES_URL
     // not required
     // RES_EDIT_IN_READONLY
     // not required
     // RES_LAYOUT_SPLIT
-    M_E( STYLE, MAY_BREAK_BETWEEN_ROWS, RES_LAYOUT_SPLIT, 0 ),
+    MAP_ENTRY( STYLE,    MAY_BREAK_BETWEEN_ROWS, RES_LAYOUT_SPLIT, 0 ),
     // RES_CHAIN
     // not required
     // RES_LINENUMBER
@@ -140,10 +140,10 @@ SvXMLItemMapEntry aXMLTableItemMap[] =
     M_E_SE( TEXT, XMLNS, RES_UNKNOWNATR_CONTAINER, 0 ),
 
     // RES_FRAMEDIR
-    M_E( STYLE, WRITING_MODE, RES_FRAMEDIR, 0 ),
+    MAP_ENTRY( STYLE, WRITING_MODE, RES_FRAMEDIR, 0 ),
 
     // RES_COLLAPSING_BORDERS
-    M_E( TABLE, BORDER_MODEL, RES_COLLAPSING_BORDERS, 0 ),
+    MAP_ENTRY( TABLE, BORDER_MODEL, RES_COLLAPSING_BORDERS, 0 ),
 
     M_END
 };
@@ -151,7 +151,7 @@ SvXMLItemMapEntry aXMLTableItemMap[] =
 SvXMLItemMapEntry aXMLTableColItemMap[] =
 {
     M_E_SI( STYLE,  COLUMN_WIDTH,     RES_FRM_SIZE, MID_FRMSIZE_COL_WIDTH ),
-    M_E( STYLE,     REL_COLUMN_WIDTH, RES_FRM_SIZE, MID_FRMSIZE_REL_COL_WIDTH ),
+    MAP_ENTRY( STYLE,        REL_COLUMN_WIDTH, RES_FRM_SIZE, MID_FRMSIZE_REL_COL_WIDTH ),
     M_END
 };
 
@@ -160,8 +160,8 @@ SvXMLItemMapEntry aXMLTableRowItemMap[] =
     // RES_FILL_ORDER
     // not required
     // RES_FRM_SIZE
-    M_E( STYLE, ROW_HEIGHT,         RES_FRM_SIZE,   MID_FRMSIZE_FIX_HEIGHT ),
-    M_E( STYLE, MIN_ROW_HEIGHT,     RES_FRM_SIZE,   MID_FRMSIZE_MIN_HEIGHT ),
+    MAP_ENTRY( STYLE,    ROW_HEIGHT,         RES_FRM_SIZE,   MID_FRMSIZE_FIX_HEIGHT ),
+    MAP_ENTRY( STYLE,    MIN_ROW_HEIGHT,     RES_FRM_SIZE,   MID_FRMSIZE_MIN_HEIGHT ),
     // RES_PAPER_BIN
     // not required
     // RES_LR_SPACE
@@ -193,8 +193,8 @@ SvXMLItemMapEntry aXMLTableRowItemMap[] =
     // RES_ANCHOR
     // not required
     // RES_BACKGROUND
-    M_E( FO,    BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
-    M_E( STYLE, BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
+    MAP_ENTRY( FO,   BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
+    MAP_ENTRY( STYLE,    BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
     // RES_BOX
     // not required
     // RES_ANCHOR
@@ -258,30 +258,30 @@ SvXMLItemMapEntry aXMLTableCellItemMap[] =
     // RES_SURROUND
     // not required
     // RES_VERT_ORIENT
-    M_E( STYLE, VERTICAL_ALIGN,                 RES_VERT_ORIENT, 0 ),
+    MAP_ENTRY( STYLE,    VERTICAL_ALIGN,                 RES_VERT_ORIENT, 0 ),
     // RES_HORI_ORIENT
     // not required
     // RES_ANCHOR
     // not required
     // RES_BACKGROUND
-    M_E( FO,    BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
-    M_E( STYLE, BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
+    MAP_ENTRY( FO,   BACKGROUND_COLOR,       RES_BACKGROUND, MID_BACK_COLOR ),
+    MAP_ENTRY( STYLE,    BACKGROUND_IMAGE,       RES_BACKGROUND, MID_SW_FLAG_ELEMENT_ITEM ),
     // RES_BOX
-    M_E( STYLE, BORDER_LINE_WIDTH,        RES_BOX, ALL_BORDER_LINE_WIDTH ),
-    M_E( STYLE, BORDER_LINE_WIDTH_LEFT,   RES_BOX, LEFT_BORDER_LINE_WIDTH ),
-    M_E( STYLE, BORDER_LINE_WIDTH_RIGHT,  RES_BOX, RIGHT_BORDER_LINE_WIDTH ),
-    M_E( STYLE, BORDER_LINE_WIDTH_TOP,    RES_BOX, TOP_BORDER_LINE_WIDTH ),
-    M_E( STYLE, BORDER_LINE_WIDTH_BOTTOM, RES_BOX, BOTTOM_BORDER_LINE_WIDTH ),
-    M_E( FO,    PADDING,                  RES_BOX, ALL_BORDER_PADDING ),
-    M_E( FO,    PADDING_LEFT,             RES_BOX, LEFT_BORDER_PADDING ),
-    M_E( FO,    PADDING_RIGHT,            RES_BOX, RIGHT_BORDER_PADDING ),
-    M_E( FO,    PADDING_TOP,              RES_BOX, TOP_BORDER_PADDING ),
-    M_E( FO,    PADDING_BOTTOM,           RES_BOX, BOTTOM_BORDER_PADDING ),
-    M_E( FO,    BORDER,                   RES_BOX, ALL_BORDER ),
-    M_E( FO,    BORDER_LEFT,              RES_BOX, LEFT_BORDER ),
-    M_E( FO,    BORDER_RIGHT,             RES_BOX, RIGHT_BORDER ),
-    M_E( FO,    BORDER_TOP,               RES_BOX, TOP_BORDER ),
-    M_E( FO,    BORDER_BOTTOM,            RES_BOX, BOTTOM_BORDER ),
+    MAP_ENTRY( STYLE,    BORDER_LINE_WIDTH,        RES_BOX, ALL_BORDER_LINE_WIDTH ),
+    MAP_ENTRY( STYLE,    BORDER_LINE_WIDTH_LEFT,   RES_BOX, LEFT_BORDER_LINE_WIDTH ),
+    MAP_ENTRY( STYLE,    BORDER_LINE_WIDTH_RIGHT,  RES_BOX, RIGHT_BORDER_LINE_WIDTH ),
+    MAP_ENTRY( STYLE,    BORDER_LINE_WIDTH_TOP,    RES_BOX, TOP_BORDER_LINE_WIDTH ),
+    MAP_ENTRY( STYLE,    BORDER_LINE_WIDTH_BOTTOM, RES_BOX, BOTTOM_BORDER_LINE_WIDTH ),
+    MAP_ENTRY( FO,   PADDING,                  RES_BOX, ALL_BORDER_PADDING ),
+    MAP_ENTRY( FO,   PADDING_LEFT,             RES_BOX, LEFT_BORDER_PADDING ),
+    MAP_ENTRY( FO,   PADDING_RIGHT,            RES_BOX, RIGHT_BORDER_PADDING ),
+    MAP_ENTRY( FO,   PADDING_TOP,              RES_BOX, TOP_BORDER_PADDING ),
+    MAP_ENTRY( FO,   PADDING_BOTTOM,           RES_BOX, BOTTOM_BORDER_PADDING ),
+    MAP_ENTRY( FO,   BORDER,                   RES_BOX, ALL_BORDER ),
+    MAP_ENTRY( FO,   BORDER_LEFT,              RES_BOX, LEFT_BORDER ),
+    MAP_ENTRY( FO,   BORDER_RIGHT,             RES_BOX, RIGHT_BORDER ),
+    MAP_ENTRY( FO,   BORDER_TOP,               RES_BOX, TOP_BORDER ),
+    MAP_ENTRY( FO,   BORDER_BOTTOM,            RES_BOX, BOTTOM_BORDER ),
     // RES_SHADOW
     // not required
     // RES_FRMMACRO
@@ -308,7 +308,7 @@ SvXMLItemMapEntry aXMLTableCellItemMap[] =
     M_E_SE( TEXT, XMLNS, RES_UNKNOWNATR_CONTAINER, 0 ),
 
     // RES_FRAMEDIR
-    M_E( STYLE, WRITING_MODE, RES_FRAMEDIR, 0 ),
+    MAP_ENTRY( STYLE, WRITING_MODE, RES_FRAMEDIR, 0 ),
 
     M_END
 };
