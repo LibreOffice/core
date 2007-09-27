51888c67498061e4aa3f5db700b043c37f092a21
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 07:52:02 +0000

    INTEGRATION: CWS swwarnings (1.3.710); FILE MERGED 2007/04/03 12:59:53 tl 1.3.710.1: #i69287# warning-free code

--- sw/source/core/frmedt/makefile.mk
+++ sw/source/core/frmedt/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 03:38:46 $
+#   last change: $Author: hr $ $Date: 2007-09-27 08:52:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -74,6 +74,11 @@ SLOFILES =  \
         $(SLO)$/fetab.obj \
         $(SLO)$/fews.obj \
         $(SLO)$/tblsel.obj
+        
+EXCEPTIONSFILES=\
+        $(SLO)$/fefly1.obj \
+        $(SLO)$/fews.obj
+        
 
 # --- Tagets -------------------------------------------------------
 
