0b9e6ab2fab270b9baddcdbd33375273d8a0cda8
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:07:34 +0000

    INTEGRATION: CWS swwarnings (1.6.710); FILE MERGED 2007/02/22 15:06:47 tl 1.6.710.1: #i69287# warning-free code

--- sw/source/ui/inc/pggrid.hxx
+++ sw/source/ui/inc/pggrid.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pggrid.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:57:18 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 12:07:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -109,7 +109,10 @@ public:
 
     virtual BOOL    FillItemSet(SfxItemSet &rSet);
     virtual void    Reset(const SfxItemSet &rSet);
+
+    using TabPage::ActivatePage;
     virtual void    ActivatePage( const SfxItemSet& rSet );
+    using TabPage::DeactivatePage;
     virtual int     DeactivatePage( SfxItemSet* pSet = 0 );
 };
 
