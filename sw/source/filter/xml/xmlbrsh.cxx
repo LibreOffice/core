f66d7559a4e4bcbb3a320b2c151c7847134588a5
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 09:09:34 +0000

    INTEGRATION: CWS swwarnings (1.13.222); FILE MERGED 2007/04/11 07:03:10 tl 1.13.222.1: #i69287# warning-free code

--- sw/source/filter/xml/xmlbrsh.cxx
+++ sw/source/filter/xml/xmlbrsh.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlbrsh.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 22:27:55 $
+ *  last change: $Author: hr $ $Date: 2007-09-27 10:09:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
