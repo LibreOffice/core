d631907e44696bfc34cbc518e76b6a8c64d695c4
Kurt Zenker <kz@openoffice.org>
2007-10-02 14:20:30 +0000

    INTEGRATION: CWS native111 (1.54.18); FILE MERGED 2007/09/25 09:37:17 is 1.54.18.1: #150535# fixing multiple shellnew files in installation set

--- solenv/bin/modules/installer/worker.pm
+++ solenv/bin/modules/installer/worker.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: worker.pm,v $
 #
-#   $Revision: 1.54 $
+#   $Revision: 1.55 $
 #
-#   last change: $Author: kz $ $Date: 2007-09-06 09:54:05 $
+#   last change: $Author: kz $ $Date: 2007-10-02 15:20:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -931,12 +931,14 @@ sub write_content_into_inf_file
             if ( $directory =~ /\bPREDEFINED_OSSHELLNEWDIR\b/ )
             {
                 $shellnewstring = $shellnewstring . $onefile->{'Name'} . "\n";
-                if ( $firstlanguage ) { add_shellnewfile_into_filesarray($filesref, $onefile, $inffile); }
+                if (( $firstlanguage ) && ( ! $installer::globals::shellnewfilesadded )) { add_shellnewfile_into_filesarray($filesref, $onefile, $inffile); }
             }
         }
 
         $shellnewstring =~ s/\s*$//;
         replace_in_template_file($templatefile, "SHELLNEWFILESPLACEHOLDER", $shellnewstring);
+
+        $installer::globals::shellnewfilesadded = 1;
     }
 
     # Second part: Start menu entries
