5ffd1971c3ba3a6e619485c57d512b10df2112d6
Kurt Zenker <kz@openoffice.org>
2007-10-09 14:26:12 +0000

    INTEGRATION: CWS basmgr03 (1.8.16); FILE MERGED 2007/09/03 12:45:14 fs 1.8.16.1: #i76129# link with COMPHELPERLIB

--- ucb/source/ucp/tdoc/makefile.mk
+++ ucb/source/ucp/tdoc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: ihi $ $Date: 2007-06-05 18:15:40 $
+#   last change: $Author: kz $ $Date: 2007-10-09 15:26:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -76,6 +76,7 @@ SHL1IMPLIB=i$(TARGET)
 SHL1VERSIONMAP=	exports.map
 
 SHL1STDLIBS=\
+    $(COMPHELPERLIB) \
     $(CPPUHELPERLIB) \
     $(CPPULIB) \
     $(SALLIB)  \
