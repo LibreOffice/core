d57125226f47d5fd951835577fbb896d2375e5a7
Kurt Zenker <kz@openoffice.org>
2007-10-09 14:24:04 +0000

    INTEGRATION: CWS basmgr03 (1.25.12); FILE MERGED 2007/08/23 20:09:06 fs 1.25.12.3: #i73331# documentenumeration moved from module unotools to module basctl (no other clients so far) 2007/07/31 12:33:07 fs 1.25.12.2: #i73331# +docsignature 2007/07/30 13:40:18 fs 1.25.12.1: #i73331# +doceventlistener

--- basctl/source/basicide/makefile.mk
+++ basctl/source/basicide/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.25 $
+#   $Revision: 1.26 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-15 15:57:46 $
+#   last change: $Author: kz $ $Date: 2007-10-09 15:24:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,10 @@ EXCEPTIONSFILES=$(SLO)$/scriptdocument.obj  \
                 $(SLO)$/register.obj	\
                 $(SLO)$/tbxctl.obj		\
                 $(SLO)$/basidectrlr.obj	\
-                $(SLO)$/localizationmgr.obj
+                $(SLO)$/localizationmgr.obj \
+                $(SLO)$/doceventnotifier.obj \
+                $(SLO)$/docsignature.obj \
+                $(SLO)$/documentenumeration.obj
 
 SLOFILES =  $(EXCEPTIONSFILES) \
             $(SLO)$/basdoc.obj	\
