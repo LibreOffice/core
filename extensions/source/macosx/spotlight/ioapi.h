5bae216281b5b86e2b53f342b4c946964de4ca49
Kurt Zenker <kz@openoffice.org>
2007-10-09 14:06:53 +0000

    INTEGRATION: CWS spotlightplugin01 (1.1.2); FILE ADDED 2007/09/14 21:06:34 fheckl 1.1.2.1: #72355 adding new files for macosx spotlight plugin

---
+++ extensions/source/macosx/spotlight/ioapi.h
@@ -0,0 +1,106 @@
+/*************************************************************************
+*
+*   OpenOffice.org - a multi-platform office productivity suite
+*
+*   $RCSfile: ioapi.h,v $
+*
+*   $Revision: 1.2 $
+*
+*   last change: $Author: kz $ $Date: 2007-10-09 15:05:39 $
+*
+*   The Contents of this file are made available subject to
+*   the terms of GNU Lesser General Public License Version 2.1.
+*
+*
+*     GNU Lesser General Public License Version 2.1
+*     =============================================
+*     Copyright 2005 by Sun Microsystems, Inc.
+*     901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*     This library is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU Lesser General Public
+*     License version 2.1, as published by the Free Software Foundation.
+*
+*     This library is distributed in the hope that it will be useful,
+*     but WITHOUT ANY WARRANTY; without even the implied warranty of
+*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*     Lesser General Public License for more details.
+*
+*     You should have received a copy of the GNU Lesser General Public
+*     License along with this library; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA  02111-1307  USA
+*
+*************************************************************************/
+
+/* ioapi.h -- IO base function header for compress/uncompress .zip
+   files using zlib + zip or unzip API
+
+   Version 1.01e, February 12th, 2005
+
+   Copyright (C) 1998-2005 Gilles Vollant
+*/
+
+#ifndef _ZLIBIOAPI_H
+#define _ZLIBIOAPI_H
+
+#include <zlib.h>
+
+#define ZLIB_FILEFUNC_SEEK_CUR (1)
+#define ZLIB_FILEFUNC_SEEK_END (2)
+#define ZLIB_FILEFUNC_SEEK_SET (0)
+
+#define ZLIB_FILEFUNC_MODE_READ      (1)
+#define ZLIB_FILEFUNC_MODE_WRITE     (2)
+#define ZLIB_FILEFUNC_MODE_READWRITEFILTER (3)
+
+#define ZLIB_FILEFUNC_MODE_EXISTING (4)
+#define ZLIB_FILEFUNC_MODE_CREATE   (8)
+
+
+#ifndef ZCALLBACK
+#define ZCALLBACK
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef voidpf (ZCALLBACK *open_file_func) OF((voidpf opaque, const char* filename, int mode));
+typedef uLong  (ZCALLBACK *read_file_func) OF((voidpf opaque, voidpf stream, void* buf, uLong size));
+typedef uLong  (ZCALLBACK *write_file_func) OF((voidpf opaque, voidpf stream, const void* buf, uLong size));
+typedef long   (ZCALLBACK *tell_file_func) OF((voidpf opaque, voidpf stream));
+typedef long   (ZCALLBACK *seek_file_func) OF((voidpf opaque, voidpf stream, uLong offset, int origin));
+typedef int    (ZCALLBACK *close_file_func) OF((voidpf opaque, voidpf stream));
+typedef int    (ZCALLBACK *testerror_file_func) OF((voidpf opaque, voidpf stream));
+
+typedef struct zlib_filefunc_def_s
+{
+    open_file_func      zopen_file;
+    read_file_func      zread_file;
+    write_file_func     zwrite_file;
+    tell_file_func      ztell_file;
+    seek_file_func      zseek_file;
+    close_file_func     zclose_file;
+    testerror_file_func zerror_file;
+    voidpf              opaque;
+} zlib_filefunc_def;
+
+
+
+void fill_fopen_filefunc OF((zlib_filefunc_def* pzlib_filefunc_def));
+
+#define ZREAD(filefunc,filestream,buf,size) ((*((filefunc).zread_file))((filefunc).opaque,filestream,buf,size))
+#define ZWRITE(filefunc,filestream,buf,size) ((*((filefunc).zwrite_file))((filefunc).opaque,filestream,buf,size))
+#define ZTELL(filefunc,filestream) ((*((filefunc).ztell_file))((filefunc).opaque,filestream))
+#define ZSEEK(filefunc,filestream,pos,mode) ((*((filefunc).zseek_file))((filefunc).opaque,filestream,pos,mode))
+#define ZCLOSE(filefunc,filestream) ((*((filefunc).zclose_file))((filefunc).opaque,filestream))
+#define ZERROR(filefunc,filestream) ((*((filefunc).zerror_file))((filefunc).opaque,filestream))
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
