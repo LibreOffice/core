7b6a7f4b2001b6f0a5c07d2c599aaa4b6560681e
Kurt Zenker <kz@openoffice.org>
2007-10-09 14:29:10 +0000

    INTEGRATION: CWS basmgr03 (1.41.76); FILE MERGED 2007/09/06 06:25:12 fs 1.41.76.2: RESYNC: (1.41-1.42); FILE MERGED 2007/08/01 09:47:02 fs 1.41.76.1: #i73331# vbauno lib needs to be linked against toolkit now

--- sc/util/makefile.mk
+++ sc/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.43 $
+#   $Revision: 1.44 $
 #
-#   last change: $Author: hjs $ $Date: 2007-09-07 10:34:18 $
+#   last change: $Author: kz $ $Date: 2007-10-09 15:29:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -278,6 +278,8 @@ SHL9STDLIBS= \
         $(SVTOOLLIB)    \
         $(SVLLIB) \
         $(ISCLIB) \
+        $(VCLLIB) \
+        $(TKLIB) \
 
 
 SHL9DEPN=$(SHL1TARGETN) $(SHL8TARGETN)
