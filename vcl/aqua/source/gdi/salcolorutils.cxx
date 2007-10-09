098fc27f5ac2e0178871d192cbc56a9305c14cc8
Kurt Zenker <kz@openoffice.org>
2007-10-09 14:14:47 +0000

    INTEGRATION: CWS aquavcl03 (1.8.4); FILE MERGED 2007/09/14 15:44:39 pl 1.8.4.1: header cleanup

--- vcl/aqua/source/gdi/salcolorutils.cxx
+++ vcl/aqua/source/gdi/salcolorutils.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salcolorutils.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-05 15:59:28 $
+ *  last change: $Author: kz $ $Date: 2007-10-09 15:14:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,10 +36,8 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_vcl.hxx"
 
-#ifndef _SV_SALCOLORUTILS_HXX
-    #include <salcolorutils.hxx>
-#endif
-#include <vcl/salbtype.hxx>
+#include "salcolorutils.hxx"
+#include "vcl/salbtype.hxx"
 
 // =======================================================================
 
