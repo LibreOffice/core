e976a609c92ac953bb2d0902afa658bc1697d137
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:20:33 +0000

    INTEGRATION: CWS sb71 (1.5.98); FILE MERGED 2007/06/22 13:27:17 sb 1.5.98.1: Made code warning-free on unxmacxi.pro.

--- cppu/test/alignment/diagnose.h
+++ cppu/test/alignment/diagnose.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: diagnose.h,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 09:00:29 $
+ *  last change: $Author: vg $ $Date: 2007-10-15 12:20:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,10 +52,10 @@
 #if OSL_DEBUG_LEVEL > 1
 
 #define BINTEST_VERIFYOFFSET( s, m, n ) \
-    fprintf( stderr, "> OFFSET_OF(" #s ", " #m ") = %d\n", OFFSET_OF(s, m) ); \
+    fprintf( stderr, "> OFFSET_OF(" #s ", " #m ") = %lu\n", static_cast< unsigned long >(OFFSET_OF(s, m)) ); \
     if (OFFSET_OF(s, m) != n) { fprintf( stderr, "### OFFSET_OF(" #s ", " #m ") = %d instead of expected %d!!!\n", OFFSET_OF(s, m), n ); abort(); }
 #define BINTEST_VERIFYSIZE( s, n ) \
-    fprintf( stderr, "> sizeof (" #s ") = %d\n", sizeof(s) ); \
+    fprintf( stderr, "> sizeof (" #s ") = %lu\n", static_cast< unsigned long >(sizeof(s)) ); \
     if (sizeof(s) != n) { fprintf( stderr, "### sizeof(" #s ") = %d instead of expected %d!!!\n", sizeof(s), n ); abort(); }
 #define BINTEST_VERIFYALIGNMENT( s, n ) \
     fprintf( stderr, "> alignment of " #s " = %d\n", ALIGNMENT(s, n) ); \
@@ -64,9 +64,9 @@
 #else
 
 #define BINTEST_VERIFYOFFSET( s, m, n ) \
-    if (OFFSET_OF(s, m) != n) { fprintf( stderr, "### OFFSET_OF(" #s ", " #m ") = %d instead of expected %d!!!\n", OFFSET_OF(s, m), n ); abort(); }
+    if (OFFSET_OF(s, m) != n) { fprintf( stderr, "### OFFSET_OF(" #s ", " #m ") = %lu instead of expected %lu!!!\n", static_cast< unsigned long >(OFFSET_OF(s, m)), static_cast< unsigned long >(n) ); abort(); }
 #define BINTEST_VERIFYSIZE( s, n ) \
-    if (sizeof(s) != n) { fprintf( stderr, "### sizeof(" #s ") = %d instead of expected %d!!!\n", sizeof(s), n ); abort(); }
+    if (sizeof(s) != n) { fprintf( stderr, "### sizeof(" #s ") = %lu instead of expected %lu!!!\n", static_cast< unsigned long >(sizeof(s)), static_cast< unsigned long >(n) ); abort(); }
 #define BINTEST_VERIFYALIGNMENT( s, n ) \
     if (ALIGNMENT(s, n) != n) { fprintf( stderr, "### alignment of " #s " = %d instead of expected %d!!!\n", ALIGNMENT(s, n), n ); abort(); }
 
