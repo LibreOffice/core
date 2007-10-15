4c11350a16c2bfcfcb8dff65ee10f90c4e0ac54a
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:00:43 +0000

    INTEGRATION: CWS sb71 (1.10.44); FILE MERGED 2007/06/14 15:07:07 sb 1.10.44.1: #i75466# Adapted to new APP|SHLnRPATH.

--- stoc/source/inspect/makefile.mk
+++ stoc/source/inspect/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 13:47:07 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:00:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ SHL1STDLIBS= \
         $(SALLIB)
 
 SHL1VERSIONMAP = introspection.map
+SHL1RPATH=URELIB
 
 SHL1DEPN=
 SHL1IMPLIB=	i$(TARGET)
