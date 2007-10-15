57fad19c2ecae8b461edf18f5e8545f553e02f9d
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:23:44 +0000

    INTEGRATION: CWS sb71 (1.7.8); FILE MERGED 2007/08/14 07:18:31 sb 1.7.8.3: #i77184# Dropped libxml2wrapper again (too much trouble to /DELAYLOAD libxml2). 2007/08/13 14:35:45 sb 1.7.8.2: #i77184# Both XML2LIB and LIBXML2LIB were used; drop XML2LIB in favor of LIBXML2LIB. 2007/07/23 13:41:49 sb 1.7.8.1: #i77184# Introduced libxml2wrapper.

--- unoxml/source/service/makefile.mk
+++ unoxml/source/service/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: ihi $ $Date: 2007-06-05 13:54:28 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:23:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,7 @@ SHL1LIBS= \
     $(SLB)$/fastsax.lib
 
 SHL1STDLIBS= \
-    $(XML2LIB) \
+    $(LIBXML2LIB) \
     $(TOOLSLIB)	\
     $(COMPHELPERLIB)	\
     $(CPPUHELPERLIB)	\
