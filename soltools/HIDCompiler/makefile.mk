2be4797342cc940a76e8afdfc6276284e4056802
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 12:04:44 +0000

    INTEGRATION: CWS sb71 (1.7.24); FILE MERGED 2007/08/15 07:34:39 sb 1.7.24.4: #i77184# Windows /DELAYLOAD depends on objects delivered from sal, but is not needed for this internal tool, anyway. 2007/08/13 13:07:14 sb 1.7.24.3: #i77184# Dropped NO_WINDOWS_DELAYLOAD again (probably not needed at the moment after all). 2007/07/10 14:22:21 sb 1.7.24.2: #i77184# Improved previous change. 2007/07/10 11:10:42 sb 1.7.24.1: #i77184# soltools does not use the Windows delayload mechanism (it is below sal where the delayload.obj is built).

--- soltools/HIDCompiler/makefile.mk
+++ soltools/HIDCompiler/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 15:36:17 $
+#   last change: $Author: vg $ $Date: 2007-10-15 13:04:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,6 +41,10 @@ TARGETTYPE=CUI
 LIBTARGET=no
 NO_DEFAULT_STL=TRUE
 INCPRE=$(MISC)
+
+# Windows /DELAYLOAD depends on objects delivered from sal, but is not needed
+# for this internal tool, anyway:
+DELAYLOADOBJ=
 
 # --- Settings -----------------------------------------------------
 
