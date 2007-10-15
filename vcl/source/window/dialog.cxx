46fd8006f6134c759623a0a7e5b127a79435999d
Ivo Hinkelmann <ihi@openoffice.org>
2007-10-15 15:27:13 +0000

    INTEGRATION: CWS vcl23101_SRC680 (1.40.48); FILE MERGED 2007/09/11 15:18:33 pl 1.40.48.1: #i80446# avoid side effects of SetModalInputMode if dialog is already not modal

--- vcl/source/window/dialog.cxx
+++ vcl/source/window/dialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dialog.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-24 10:18:51 $
+ *  last change: $Author: ihi $ $Date: 2007-10-15 16:27:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -508,7 +508,7 @@ long Dialog::Notify( NotifyEvent& rNEvt )
             // make sure the dialog is still modal
             // changing focus between application frames may
             // have re-enabled input for our parent
-            if( mbInExecute )
+            if( mbInExecute && mbModalMode )
             {
                 // do not change modal counter (pSVData->maAppData.mnModalDialog)
                 SetModalInputMode( FALSE );
