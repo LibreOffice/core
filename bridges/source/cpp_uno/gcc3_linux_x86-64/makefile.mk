22ee5d1d464683b9466b2cb92580c64201ab55ad
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 10:57:28 +0000

    INTEGRATION: CWS sb71 (1.7.8); FILE MERGED 2007/06/14 15:04:34 sb 1.7.8.1: #i75466# Adapted to new APP|SHLnRPATH.

--- bridges/source/cpp_uno/gcc3_linux_x86-64/makefile.mk
+++ bridges/source/cpp_uno/gcc3_linux_x86-64/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2007-04-25 14:57:20 $
+#   last change: $Author: vg $ $Date: 2007-10-15 11:57:28 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -68,6 +68,7 @@ SHL1TARGET= $(TARGET)
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
 SHL1IMPLIB=i$(TARGET)
 SHL1VERSIONMAP=..$/..$/bridge_exports.map
+SHL1RPATH=URELIB
 
 SHL1OBJS = $(SLOFILES)
 SHL1LIBS = $(SLB)$/cpp_uno_shared.lib
