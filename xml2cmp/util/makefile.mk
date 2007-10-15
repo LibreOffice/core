2635ce681f5f956d96d923f6c56ef2908c0c8b84
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 12:08:20 +0000

    INTEGRATION: CWS sb71 (1.9.8); FILE MERGED 2007/08/15 07:34:13 sb 1.9.8.4: #i77184# Windows /DELAYLOAD depends on objects delivered from sal, but is not needed for this internal tool, anyway. 2007/08/13 13:08:36 sb 1.9.8.3: #i77184# Dropped NO_WINDOWS_DELAYLOAD again (probably not needed at the moment after all). 2007/07/10 13:55:17 sb 1.9.8.2: #i77184# Improved naming.' 2007/07/10 13:50:13 sb 1.9.8.1: #i77184# xml2cmp does not use the Windows delayload mechanism (it is below sal where the delayload.obj is built).

--- xml2cmp/util/makefile.mk
+++ xml2cmp/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: vg $ $Date: 2006-09-25 13:27:44 $
+#   last change: $Author: vg $ $Date: 2007-10-15 13:08:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,6 +39,9 @@ PRJNAME=xml2cmp
 TARGET=xml2cmp
 TARGETTYPE=CUI
 
+# Windows /DELAYLOAD depends on objects delivered from sal, but is not needed
+# for this internal tool, anyway:
+DELAYLOADOBJ=
 
 # --- Settings -----------------------------------------------------
 ENABLE_EXCEPTIONS=TRUE
