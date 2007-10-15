965124b310756436c68e2a2350a773091041e937
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:52:33 +0000

    INTEGRATION: CWS sb71 (1.13.16); FILE MERGED 2007/08/23 17:55:29 sb 1.13.16.2: RESYNC: (1.13-1.14); FILE MERGED 2007/07/25 12:02:12 sb 1.13.16.1: #i77184# Drop MSVCRTLIB again and use already existing LIBCMT instead (which for wntmsci10 and wntmsci11 at least correctly differentiates between using msvcrt.lib and msvcrtd.lib; this probably needs to be added for wntgcci6, too).

--- sal/systools/win32/uwinapi/makefile.mk
+++ sal/systools/win32/uwinapi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: ihi $ $Date: 2007-08-20 13:36:11 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:52:33 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -153,7 +153,7 @@ SHL1STDLIBS+=\
         $(USER32LIB)\
         $(ADVAPI32LIB)\
         $(VERSIONLIB)\
-        $(MSVCRTLIB)\
+        $(LIBCMT)\
         $(SHLWAPILIB)
 
         
