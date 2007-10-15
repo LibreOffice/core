30a0e8b1db94efffaf98dc17f5bb7bd0d329267c
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:02:23 +0000

    INTEGRATION: CWS sb71 (1.10.44); FILE MERGED 2007/06/14 15:07:09 sb 1.10.44.1: #i75466# Adapted to new APP|SHLnRPATH.

--- stoc/source/proxy_factory/makefile.mk
+++ stoc/source/proxy_factory/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 13:49:21 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:02:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -64,6 +64,7 @@ SHL1DEPN=
 SHL1IMPLIB=i$(TARGET)
 SHL1LIBS=$(SLB)$/$(TARGET).lib
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+SHL1RPATH=URELIB
 
 DEF1NAME=$(SHL1TARGET)
 
