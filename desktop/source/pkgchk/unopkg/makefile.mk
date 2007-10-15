2b4554602b8f9b0dee45f5055ff98f39dcff500f
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 12:00:45 +0000

    INTEGRATION: CWS sb71 (1.7.2); FILE MERGED 2007/08/23 18:15:18 sb 1.7.2.2: RESYNC: (1.7-1.8); FILE MERGED 2007/08/21 14:58:47 sb 1.7.2.1: #i75466# Have loaders around OOo executables to set PATH and URE_BOOTSTRAP environment variables appropriately.

--- desktop/source/pkgchk/unopkg/makefile.mk
+++ desktop/source/pkgchk/unopkg/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: obo $ $Date: 2007-07-18 08:03:27 $
+#   last change: $Author: vg $ $Date: 2007-10-15 13:00:45 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -114,3 +114,12 @@ $(APP1TARGETN) : $(MISC)$/binso_created.flg
 $(MISC)$/binso_created.flg:
     @@-$(MKDIRHIER) $(BIN)$/so && $(TOUCH) $@
 
+.IF "$(GUI)" == "WNT"
+ALLTAR: $(BIN)$/unopkg.bin $(BIN)$/so$/unopkg.bin
+
+$(BIN)$/unopkg.bin: $(BIN)$/unopkg$(EXECPOST)
+    $(COPY) $< $@
+
+$(BIN)$/so$/unopkg.bin: $(BIN)$/so$/unopkg$(EXECPOST)
+    $(COPY) $< $@
+.ENDIF
