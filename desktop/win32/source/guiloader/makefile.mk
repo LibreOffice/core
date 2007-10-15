21c754db0b515a3ebffa60e007c1e2cc9b19640b
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 12:02:32 +0000

    INTEGRATION: CWS sb71 (1.1.2); FILE ADDED 2007/10/12 07:20:41 sb 1.1.2.3: #i75466# Fix build on wntmsci11 (will probably be reverted once OpenOffice_woURE is working on wntmsci11). 2007/08/21 14:55:50 sb 1.1.2.2: #i75466# Have loaders around OOo executables to set PATH and URE_BOOTSTRAP environment variables appropriately. 2007/08/20 14:15:57 hro 1.1.2.1: #i77184# New GUI and CUI loader to set PATH variable for DLL searching

---
+++ desktop/win32/source/guiloader/makefile.mk
@@ -0,0 +1,71 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: vg $ $Date: 2007-10-15 13:02:32 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PRJNAME=desktop
+TARGET=guiloader
+LIBTARGET=NO
+TARGETTYPE=GUI
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+# --- Files --------------------------------------------------------
+
+APP1TARGET=guiloader
+APP1NOSAL=TRUE
+APP1ICON=$(SOLARRESDIR)$/icons/ooo-main-app.ico
+APP1OBJS=$(OBJ)$/genericloader.obj $(OBJ)$/extendloaderenvironment.obj
+STDLIB1=$(SHLWAPILIB)
+
+APP2TARGET=so$/guiloader
+APP2NOSAL=TRUE
+APP2ICON=$(SOLARRESDIR)$/icons/so8-main-app.ico
+APP2OBJS=$(OBJ)$/genericloader.obj $(OBJ)$/extendloaderenvironment.obj
+STDLIB2=$(SHLWAPILIB)
+
+# Until every DLL is linked against $(DELAYLOADOBJ) just as on wntmsci10:
+.IF "$(OS)$(COM)$(CPU)$(COMEX)" == "WNTMSCI11"
+APP1OBJS+=$(L)$/delayload.obj
+APP2OBJS+=$(L)$/delayload.obj
+.ENDIF
+
+# --- Targets ------------------------------------------------------
+
+
+.INCLUDE :  target.mk
+
