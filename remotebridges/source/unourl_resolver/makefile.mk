44feef9fc6e22cc6e37e366ccc4b31f7d1688474
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:22:32 +0000

    INTEGRATION: CWS sb71 (1.10.16); FILE MERGED 2007/06/14 15:06:38 sb 1.10.16.1: #i75466# Adapted to new APP|SHLnRPATH.

--- remotebridges/source/unourl_resolver/makefile.mk
+++ remotebridges/source/unourl_resolver/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 13:59:13 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:22:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -61,6 +61,7 @@ SHL1DEPN=
 SHL1IMPLIB=	i$(TARGET)
 SHL1LIBS=	$(SLB)$/$(TARGET).lib
 SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+SHL1RPATH=  URELIB
 
 DEF1NAME=	$(SHL1TARGET)
 
