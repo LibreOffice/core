e5dfcd372c5cf848ec8dcca3a84bc13f80d63f66
Vladimir Glazounov <vg@openoffice.org>
2007-10-15 11:17:31 +0000

    INTEGRATION: CWS sb71 (1.16.34); FILE MERGED 2007/10/02 09:04:34 sb 1.16.34.2: RESYNC: (1.16-1.17); FILE MERGED 2007/08/13 14:35:28 sb 1.16.34.1: #i77184# Both XML2LIB and LIBXML2LIB were used; drop XML2LIB in favor of LIBXML2LIB.

--- forms/util/makefile.mk
+++ forms/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: vg $ $Date: 2007-09-20 14:42:36 $
+#   last change: $Author: vg $ $Date: 2007-10-15 12:17:31 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -78,7 +78,7 @@ SHL1STDLIBS= \
         $(TKLIB) \
         $(SVXLIB) \
         $(UCBHELPERLIB) \
-        $(XML2LIB) \
+        $(LIBXML2LIB) \
         $(ICUUCLIB) \
         $(ICUINLIB)
 
