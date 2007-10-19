5b40ce920ae539733540b3c4a50cfcb6e5defd0f
Tobias Krause <tkr@openoffice.org>
2007-10-19 11:52:28 +0000

    #31053# WebDAV Https Support

--- openssl/makefile.mk
+++ openssl/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: tkr $ $Date: 2007-10-17 16:03:02 $
+#   last change: $Author: tkr $ $Date: 2007-10-19 12:52:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -60,49 +60,42 @@ TARFILE_NAME=$(OPENSSL_NAME)
 
 CONFIGURE_DIR=.
 CONFIGURE_ACTION=config
-CONFIGURE_FLAGS=shared -I$(SYSBASE)$/usr$/include -L$(SYSBASE)$/usr$/lib
+CONFIGURE_FLAGS=-I$(SYSBASE)$/usr$/include -L$(SYSBASE)$/usr$/lib shared 
 
 BUILD_DIR=.
 BUILD_ACTION=make
 
+OUT2LIB = libssl.*
+OUT2LIB += libcrypto.*
+OUT2INC += include/openssl/*
+
 .IF "$(OS)" == "LINUX"
-
 PATCH_FILE_NAME=openssllnx.patch
-
-BUILD_ACTION=make 'SHARED_LDFLAGS=-Wl,--version-script=./lib$$(SHLIBDIRS)_OOo_0_9_8e.map'
+# if you build openssl as shared library you have to patch the Makefile.Shared "LD_LIBRARY_PATH=$$LD_LIBRARY_PATH \"
+#BUILD_ACTION=make 'SHARED_LDFLAGS=-Wl,--version-script=./lib$$(SHLIBDIRS)_OOo_0_9_8e.map'
 .ENDIF
 
 .IF "$(OS)" == "SOLARIS"
-OPENSSL_NAME=openssl-0.9.8a
-CONFIGURE_DIR=. 
-CONFIGURE_ACTION=Configure solaris-sparcv9-gcc  
-.IF "$(PROCTYPE)" == "x86"
-CONFIGURE_ACTION=Configure solaris-x86-gcc  
+PATCH_FILE_NAME=opensslsol.patch
+#BUILD_ACTION=make 'SHARED_LDFLAGS=-G -dy -z text -M./lib$$$$$$$$(SHLIBDIRS)_OOo_0_9_8e.map'
 .ENDIF
-.IF "$(PROCTYPE)" == "x86_64"
-CONFIGURE_ACTION=Configure solaris-x86_64-gcc  
-.ENDIF
-
-CONFIGURE_FLAGS=shared
-
-.ENDIF
-
-
 
 .IF "$(OS)" == "WNT"
-
 PATCH_FILE_NAME=openssl.patch
-
 CONFIGURE_DIR=. 
 CONFIGURE_ACTION=$(PERL) configure
 CONFIGURE_FLAGS=VC-WIN32
 
-#.IMPORT .IGNORE : SOLARINC
-#INCLUDE := $(SOLARINC:s/-I/;)
-#.EXPORT : INCLUDE
-
 BUILD_DIR=.
 BUILD_ACTION=cmd /c "ms\do_ms $(PERL)" && nmake -f ms/ntdll.mak
+
+OUT2LIB = out32dll/ssleay32.lib
+OUT2LIB += out32dll/libeay32.lib
+OUT2LIB += out32dll/ssleay32.lib
+OUT2LIB += out32dll/libeay32.lib
+OUT2BIN = out32dll/ssleay32.dll
+OUT2BIN += out32dll/libeay32.dll
+OUT2INC = inc32/openssl/*
 .ENDIF
 
 #set INCLUDE=D:\sol_temp\n\msvc7net3\PlatformSDK\include;D:\sol_temp\n\msvc7net3\include\ && set path=%path%;D:\sol_temp\r\btw\SRC680\perl\bin &&
