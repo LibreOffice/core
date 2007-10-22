d5daeb123646ae2aa569e7f21b5675bb0b44d7fa
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 15:33:21 +0000

    INTEGRATION: CWS chart14 (1.4.14); FILE MERGED 2007/09/28 10:40:57 iha 1.4.14.1: #i82051# Allow to display each part of a data label in a separate line

--- xmloff/source/chart/SchXMLTools.hxx
+++ xmloff/source/chart/SchXMLTools.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SchXMLTools.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: ihi $ $Date: 2007-08-17 12:06:19 $
+ *  last change: $Author: vg $ $Date: 2007-10-22 16:33:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,6 +57,7 @@ namespace com { namespace sun { namespace star {
 
 class XMLPropStyleContext;
 class SvXMLStylesContext;
+class SvXMLExport;
 
 namespace SchXMLTools
 {
@@ -97,6 +98,8 @@ namespace SchXMLTools
         tSchXMLLSequencesPerIndex * pLSequencesPerIndex = 0 );
 
     ::com::sun::star::uno::Any getPropertyFromContext( const ::rtl::OUString& rPropertyName, const XMLPropStyleContext * pPropStyleContext, const SvXMLStylesContext* pStylesCtxt );
+
+    void exportText( SvXMLExport& rExport, const ::rtl::OUString& rText, bool bConvertTabsLFs );
 }
 
 #endif  // SCH_XML_TOOLS_HXX_
