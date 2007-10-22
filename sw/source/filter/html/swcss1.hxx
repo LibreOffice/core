5d2cff883ee1becd7f48b50abbb30f917c60e2c4
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 14:12:11 +0000

    INTEGRATION: CWS swusing (1.6.38); FILE MERGED 2007/10/11 10:19:51 tl 1.6.38.2: #i82476# make 'using' declarations private 2007/10/10 14:15:08 tl 1.6.38.1: #i82476# make newly added 'using' declarations private

--- sw/source/filter/html/swcss1.hxx
+++ sw/source/filter/html/swcss1.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swcss1.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:51:37 $
+ *  last change: $Author: vg $ $Date: 2007-10-22 15:12:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
