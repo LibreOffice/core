5d4c5920089659dee9202c62933cc420a014cdee
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 14:24:56 +0000

    INTEGRATION: CWS swusing (1.55.36); FILE MERGED 2007/10/10 14:15:12 tl 1.55.36.1: #i82476# make newly added 'using' declarations private

--- sw/source/ui/inc/view.hxx
+++ sw/source/ui/inc/view.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: view.hxx,v $
  *
- *  $Revision: 1.55 $
+ *  $Revision: 1.56 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 12:14:01 $
+ *  last change: $Author: vg $ $Date: 2007-10-22 15:24:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -424,13 +424,14 @@ protected:
     // form letter execution
     void    GenerateFormLetter(BOOL bUseCurrentDocument);
 
+    using SfxShell::GetDispatcher;
+
 public:
 
     SFX_DECL_VIEWFACTORY(SwView);
     SFX_DECL_INTERFACE(SW_VIEWSHELL)
     TYPEINFO();
 
-    using SfxShell::GetDispatcher;
     SfxDispatcher   &GetDispatcher();
 
     void                    GotFocus() const;
