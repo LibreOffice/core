fe536e89957697582071faa6abc336c345c3a1a2
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 15:46:39 +0000

    INTEGRATION: CWS chart14 (1.2.12); FILE MERGED 2007/10/11 13:30:35 iha 1.2.12.5: #i82383#, #i82424# number format for data labels - preselect automatic number format - handle mixed states 2007/09/28 10:32:13 iha 1.2.12.4: #i82051# Allow to display each part of a data label in a separate line 2007/09/20 07:27:28 iha 1.2.12.3: #i43489# allow to display value and percentage at the same time 2007/09/20 07:26:18 iha 1.2.12.2: #i43489# allow to display value and percentage at the same time 2007/08/10 22:10:30 iha 1.2.12.1: #i37792# number format for data labels

--- chart2/source/controller/dialogs/res_DataLabel.hxx
+++ chart2/source/controller/dialogs/res_DataLabel.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: res_DataLabel.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-25 08:34:15 $
+ *  last change: $Author: vg $ $Date: 2007-10-22 16:46:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,7 @@
 #ifndef CHART2_RES_DATALABEL_HXX
 #define CHART2_RES_DATALABEL_HXX
 
+#include "res_TextSeparator.hxx"
 // header for class CheckBox
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
@@ -63,24 +64,30 @@ public:
     void SetNumberFormatter( SvNumberFormatter* pFormatter );
 
 private:
-    CheckBox            m_aCbValue;
-    RadioButton         m_aRbNumber;
-    RadioButton         m_aRbPercent;
-//DLNF    PushButton          m_aPB_NumberFormatForValue;
-//DLNF    PushButton          m_aPB_NumberFormatForPercent;
-    CheckBox            m_aCbText;
-    CheckBox            m_aCbSymbol;
+    CheckBox            m_aCBNumber;
+    PushButton          m_aPB_NumberFormatForValue;
+    CheckBox            m_aCBPercent;
+    PushButton          m_aPB_NumberFormatForPercent;
+    CheckBox            m_aCBCategory;
+    CheckBox            m_aCBSymbol;
+
+    TextSeparatorResources  m_aSeparatorResources;
 
     SvNumberFormatter*  m_pNumberFormatter;
+    bool                m_bNumberFormatMixedState;
+    bool                m_bPercentFormatMixedState;
     ULONG               m_nNumberFormatForValue;
     ULONG               m_nNumberFormatForPercent;
+
+    bool                m_bSourceFormatMixedState;
+    bool                m_bPercentSourceMixedState;
     bool                m_bSourceFormatForValue;
     bool                m_bSourceFormatForPercent;
 
     Window*             m_pWindow;
     SfxItemPool*        m_pPool;
 
-//DLNF    DECL_LINK(NumberFormatDialogHdl, PushButton * );
+    DECL_LINK(NumberFormatDialogHdl, PushButton * );
     DECL_LINK(CheckHdl, CheckBox* );
     void EnableControls();
 };
