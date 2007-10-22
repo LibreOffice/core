081b583a9dffbab99cae30288dae42999150ed59
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 14:13:44 +0000

    INTEGRATION: CWS swusing (1.13.34); FILE MERGED 2007/10/10 14:15:09 tl 1.13.34.1: #i82476# make newly added 'using' declarations private

--- sw/source/ui/dbui/createaddresslistdialog.cxx
+++ sw/source/ui/dbui/createaddresslistdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: createaddresslistdialog.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 11:30:06 $
+ *  last change: $Author: vg $ $Date: 2007-10-22 15:13:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -128,11 +128,13 @@ class SwAddressControl_Impl : public Control
 
     virtual long        PreNotify( NotifyEvent& rNEvt );
     virtual void        Command( const CommandEvent& rCEvt );
+
+    using Window::SetData;
+
 public:
     SwAddressControl_Impl(Window* pParent, const ResId& rResId );
     ~SwAddressControl_Impl();
 
-    using Window::SetData;
     void        SetData(SwCSVData& rDBData);
 
     void        SetCurrentDataSet(sal_uInt32 nSet);
