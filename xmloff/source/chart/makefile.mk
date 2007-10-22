36fa0dc9ca00245d46d82b6efdef430be0a9cc73
Vladimir Glazounov <vg@openoffice.org>
2007-10-22 15:34:38 +0000

    INTEGRATION: CWS chart14 (1.12.90); FILE MERGED 2007/09/28 10:39:17 iha 1.12.90.1: #i82051# Allow to display each part of a data label in a separate line

--- xmloff/source/chart/makefile.mk
+++ xmloff/source/chart/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: vg $ $Date: 2007-05-22 16:08:44 $
+#   last change: $Author: vg $ $Date: 2007-10-22 16:34:38 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -63,6 +63,7 @@ SLOFILES =	$(SLO)$/ColorPropertySet.obj \
             $(SLO)$/SchXMLAutoStylePoolP.obj \
             $(SLO)$/XMLChartPropertyContext.obj \
             $(SLO)$/XMLSymbolImageContext.obj \
+            $(SLO)$/XMLLabelSeparatorContext.obj \
             $(SLO)$/XMLTextOrientationHdl.obj \
             $(SLO)$/XMLSymbolTypePropertyHdl.obj \
             $(SLO)$/transporttypes.obj
