d2328283068cf1af600e15b848eaaa8cd490eefb
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:45:55 +0000

    INTEGRATION: CWS so3deadcorpses (1.5.244); FILE MERGED 2007/09/09 18:47:58 fs 1.5.244.1: #i81309# so3 is dead

--- sc/source/ui/inc/linkarea.hxx
+++ sc/source/ui/inc/linkarea.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: linkarea.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-22 10:47:58 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:45:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,10 +62,6 @@
 #ifndef _SVTOOLS_INETTBC_HXX
 #include <svtools/inettbc.hxx>
 #endif
-
-//REMOVE    #ifndef _EMBOBJ_HXX //autogen
-//REMOVE    #include <so3/embobj.hxx>
-//REMOVE    #endif
 
 //REMOVE    #ifndef SO2_DECL_SVEMBEDDEDOBJECT_DEFINED
 //REMOVE    #define SO2_DECL_SVEMBEDDEDOBJECT_DEFINED
