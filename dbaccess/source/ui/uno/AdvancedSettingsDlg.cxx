f8fb7ab3976e37dabc31259c1089f05cfe57a29e
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-01 14:38:20 +0000

    INTEGRATION: CWS dba24b (1.4.130); FILE MERGED 2007/08/27 10:42:05 fs 1.4.130.1: some re-factoring in preparation of #i80930#: moved declaration from .hxx to .cxx

--- dbaccess/source/ui/uno/AdvancedSettingsDlg.cxx
+++ dbaccess/source/ui/uno/AdvancedSettingsDlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AdvancedSettingsDlg.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 07:31:51 $
+ *  last change: $Author: hr $ $Date: 2007-11-01 15:38:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,23 +36,9 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_dbaccess.hxx"
 
-#ifndef _DBU_REGHELPER_HXX_
+#include "unoadmin.hxx"
 #include "dbu_reghelper.hxx"
-#endif
-#ifndef _DBAUI_ADVANCEDSETTINGSDLG_HXX
-#include "AdvancedSettingsDlg.hxx"
-#endif
-#ifndef DBAUI_ADVANCEDPAGEDLG_HXX
-#include "AdvancedPageDlg.hxx"
-#endif
-
-
-using namespace dbaui;
-
-extern "C" void SAL_CALL createRegistryInfo_OAdvancedSettingsDialog()
-{
-    static OMultiInstanceAutoRegistration< OAdvancedSettingsDialog > aAutoRegistration;
-}
+#include "advancedsettingsdlg.hxx"
 
 //.........................................................................
 namespace dbaui
@@ -63,79 +49,119 @@ namespace dbaui
     using namespace ::com::sun::star::lang;
     using namespace ::com::sun::star::beans;
 
-//=========================================================================
-//-------------------------------------------------------------------------
-OAdvancedSettingsDialog::OAdvancedSettingsDialog(const Reference< XMultiServiceFactory >& _rxORB)
-    :ODatabaseAdministrationDialog(_rxORB)
-{
-}
-//-------------------------------------------------------------------------
-Sequence<sal_Int8> SAL_CALL OAdvancedSettingsDialog::getImplementationId(  ) throw(RuntimeException)
-{
-    static ::cppu::OImplementationId aId;
-    return aId.getImplementationId();
-}
+    //=========================================================================
+    //= OAdvancedSettingsDialog
+    //=========================================================================
+    class OAdvancedSettingsDialog
+            :public ODatabaseAdministrationDialog
+            ,public ::comphelper::OPropertyArrayUsageHelper< OAdvancedSettingsDialog >
+    {
 
-//-------------------------------------------------------------------------
-Reference< XInterface > SAL_CALL OAdvancedSettingsDialog::Create(const Reference< XMultiServiceFactory >& _rxFactory)
-{
-    return *(new OAdvancedSettingsDialog(_rxFactory));
-}
+    protected:
+        OAdvancedSettingsDialog(const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB);
 
-//-------------------------------------------------------------------------
-::rtl::OUString SAL_CALL OAdvancedSettingsDialog::getImplementationName() throw(RuntimeException)
-{
-    return getImplementationName_Static();
-}
+    public:
+        // XTypeProvider
+        virtual ::com::sun::star::uno::Sequence<sal_Int8> SAL_CALL getImplementationId(  ) throw(::com::sun::star::uno::RuntimeException);
 
-//-------------------------------------------------------------------------
-::rtl::OUString OAdvancedSettingsDialog::getImplementationName_Static() throw(RuntimeException)
-{
-    return ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.comp.dbu.OAdvancedSettingsDialog"));
-}
+        // XServiceInfo
+        virtual ::rtl::OUString SAL_CALL getImplementationName() throw(::com::sun::star::uno::RuntimeException);
+        virtual ::comphelper::StringSequence SAL_CALL getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException);
 
-//-------------------------------------------------------------------------
-::comphelper::StringSequence SAL_CALL OAdvancedSettingsDialog::getSupportedServiceNames() throw(RuntimeException)
-{
-    return getSupportedServiceNames_Static();
-}
+        // XServiceInfo - static methods
+        static ::com::sun::star::uno::Sequence< ::rtl::OUString > getSupportedServiceNames_Static(void) throw( ::com::sun::star::uno::RuntimeException );
+        static ::rtl::OUString getImplementationName_Static(void) throw( ::com::sun::star::uno::RuntimeException );
+        static ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >
+                SAL_CALL Create(const ::com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >&);
 
-//-------------------------------------------------------------------------
-::comphelper::StringSequence OAdvancedSettingsDialog::getSupportedServiceNames_Static() throw(RuntimeException)
-{
-    ::comphelper::StringSequence aSupported(1);
-    aSupported.getArray()[0] = ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.sdb.AdvancedDatabaseSettingsDialog"));
-    return aSupported;
-}
+        // XPropertySet
+        virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo>  SAL_CALL getPropertySetInfo() throw(::com::sun::star::uno::RuntimeException);
+        virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper();
 
-//-------------------------------------------------------------------------
-Reference<XPropertySetInfo>  SAL_CALL OAdvancedSettingsDialog::getPropertySetInfo() throw(RuntimeException)
-{
-    Reference<XPropertySetInfo>  xInfo( createPropertySetInfo( getInfoHelper() ) );
-    return xInfo;
-}
+        // OPropertyArrayUsageHelper
+        virtual ::cppu::IPropertyArrayHelper* createArrayHelper( ) const;
+    protected:
+    // OGenericUnoDialog overridables
+        virtual Dialog* createDialog(Window* _pParent);
+    };
 
-//-------------------------------------------------------------------------
-::cppu::IPropertyArrayHelper& OAdvancedSettingsDialog::getInfoHelper()
-{
-    return *const_cast<OAdvancedSettingsDialog*>(this)->getArrayHelper();
-}
+    //=========================================================================
+    //-------------------------------------------------------------------------
+    OAdvancedSettingsDialog::OAdvancedSettingsDialog(const Reference< XMultiServiceFactory >& _rxORB)
+        :ODatabaseAdministrationDialog(_rxORB)
+    {
+    }
+    //-------------------------------------------------------------------------
+    Sequence<sal_Int8> SAL_CALL OAdvancedSettingsDialog::getImplementationId(  ) throw(RuntimeException)
+    {
+        static ::cppu::OImplementationId aId;
+        return aId.getImplementationId();
+    }
 
-//------------------------------------------------------------------------------
-::cppu::IPropertyArrayHelper* OAdvancedSettingsDialog::createArrayHelper( ) const
-{
-    Sequence< Property > aProps;
-    describeProperties(aProps);
-    return new ::cppu::OPropertyArrayHelper(aProps);
-}
-//------------------------------------------------------------------------------
-Dialog* OAdvancedSettingsDialog::createDialog(Window* _pParent)
-{
-    OAdvancedTabPageDlg* pDlg = new OAdvancedTabPageDlg(_pParent, m_pDatasourceItems, m_xORB,m_aInitialSelection);
-    return pDlg;
-}
+    //-------------------------------------------------------------------------
+    Reference< XInterface > SAL_CALL OAdvancedSettingsDialog::Create(const Reference< XMultiServiceFactory >& _rxFactory)
+    {
+        return *(new OAdvancedSettingsDialog(_rxFactory));
+    }
+
+    //-------------------------------------------------------------------------
+    ::rtl::OUString SAL_CALL OAdvancedSettingsDialog::getImplementationName() throw(RuntimeException)
+    {
+        return getImplementationName_Static();
+    }
+
+    //-------------------------------------------------------------------------
+    ::rtl::OUString OAdvancedSettingsDialog::getImplementationName_Static() throw(RuntimeException)
+    {
+        return ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("org.openoffice.comp.dbu.OAdvancedSettingsDialog"));
+    }
+
+    //-------------------------------------------------------------------------
+    ::comphelper::StringSequence SAL_CALL OAdvancedSettingsDialog::getSupportedServiceNames() throw(RuntimeException)
+    {
+        return getSupportedServiceNames_Static();
+    }
+
+    //-------------------------------------------------------------------------
+    ::comphelper::StringSequence OAdvancedSettingsDialog::getSupportedServiceNames_Static() throw(RuntimeException)
+    {
+        ::comphelper::StringSequence aSupported(1);
+        aSupported.getArray()[0] = ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.sdb.AdvancedDatabaseSettingsDialog"));
+        return aSupported;
+    }
+
+    //-------------------------------------------------------------------------
+    Reference<XPropertySetInfo>  SAL_CALL OAdvancedSettingsDialog::getPropertySetInfo() throw(RuntimeException)
+    {
+        Reference<XPropertySetInfo>  xInfo( createPropertySetInfo( getInfoHelper() ) );
+        return xInfo;
+    }
+
+    //-------------------------------------------------------------------------
+    ::cppu::IPropertyArrayHelper& OAdvancedSettingsDialog::getInfoHelper()
+    {
+        return *const_cast<OAdvancedSettingsDialog*>(this)->getArrayHelper();
+    }
+
+    //------------------------------------------------------------------------------
+    ::cppu::IPropertyArrayHelper* OAdvancedSettingsDialog::createArrayHelper( ) const
+    {
+        Sequence< Property > aProps;
+        describeProperties(aProps);
+        return new ::cppu::OPropertyArrayHelper(aProps);
+    }
+    //------------------------------------------------------------------------------
+    Dialog* OAdvancedSettingsDialog::createDialog(Window* _pParent)
+    {
+        AdvancedSettingsDialog* pDlg = new AdvancedSettingsDialog(_pParent, m_pDatasourceItems, m_xORB,m_aInitialSelection);
+        return pDlg;
+    }
 
 //.........................................................................
 }   // namespace dbaui
 //.........................................................................
 
+extern "C" void SAL_CALL createRegistryInfo_OAdvancedSettingsDialog()
+{
+    static ::dbaui::OMultiInstanceAutoRegistration< ::dbaui::OAdvancedSettingsDialog > aAutoRegistration;
+}
