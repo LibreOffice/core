b89d5a2e9b9a82d26d35546256da8086c1f31472
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-01 14:17:18 +0000

    INTEGRATION: CWS dba24b (1.3.180); FILE MERGED 2007/08/16 06:14:43 oj 1.3.180.1: #i56898# clean up of the class structure

--- dbaccess/source/ui/inc/RelControliFace.hxx
+++ dbaccess/source/ui/inc/RelControliFace.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: RelControliFace.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 03:12:55 $
+ *  last change: $Author: hr $ $Date: 2007-11-01 15:17:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,10 @@
 #ifndef _COM_SUN_STAR_SDBC_XCONNECTION_HPP_
 #include <com/sun/star/sdbc/XConnection.hpp>
 #endif
+#include "TableConnectionData.hxx"
 
 namespace dbaui
 {
-    class OTableConnectionData;
     class IRelationControlInterface
     {
     public:
@@ -50,7 +50,7 @@ namespace dbaui
         /** getConnectionData returns the current connection data
             @return the current connectiondata
         */
-        virtual OTableConnectionData* getConnectionData() const = 0;
+        virtual TTableConnectionData::value_type getConnectionData() const = 0;
 
         /** setValid set the valid inside, can be used for OK buttons
             @param  _bValid true when the using control allows an update
