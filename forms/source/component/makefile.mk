0c0f9313f5a1bd4da17da8eca0572cc97415e133
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-01 13:56:41 +0000

    INTEGRATION: CWS dba24b (1.13.34); FILE MERGED 2007/09/03 09:23:16 fs 1.13.34.1: #i76024# +cachedrowset

--- forms/source/component/makefile.mk
+++ forms/source/component/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: kz $ $Date: 2007-05-10 09:57:12 $
+#   last change: $Author: hr $ $Date: 2007-11-01 14:56:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -96,6 +96,7 @@ SLOFILES=	$(SLO)$/Grid.obj \
             $(SLO)$/imgprod.obj \
             $(SLO)$/findpos.obj \
             $(SLO)$/propertybaghelper.obj \
+            $(SLO)$/cachedrowset.obj \
 
 # --- Targets ----------------------------------
 
