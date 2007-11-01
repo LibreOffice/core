ef5e07997b3e829db783f578fa093b3513525644
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-01 17:05:02 +0000

    INTEGRATION: CWS dxliberate01 (1.39.100); FILE MERGED 2007/10/01 10:06:54 thb 1.39.100.2: RESYNC: (1.39-1.40); FILE MERGED 2007/08/04 20:32:39 thb 1.39.100.1: #i48454# Made dxcanvas actually appear on OOo builds

--- scp2/util/makefile.mk
+++ scp2/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.41 $
+#   $Revision: 1.42 $
 #
-#   last change: $Author: vg $ $Date: 2007-10-15 12:57:36 $
+#   last change: $Author: hr $ $Date: 2007-11-01 18:05:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -126,6 +126,10 @@ SCP1FILES +=                           \
              module_quickstart.par     \
              file_quickstart.par       \
              registryitem_quickstart.par
+
+.IF "$(ENABLE_DIRECTX)"!=""
+SCP1FILES += directxcanvas.par
+.ENDIF
 
 .IF "$(DISABLE_ACTIVEX)"==""
 SCP1FILES +=                           \
