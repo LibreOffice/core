13502bb51e910ed28bf5ce06d7d2d11c525f209c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-01 14:12:00 +0000

    INTEGRATION: CWS dba24b (1.15.26); FILE MERGED 2007/09/28 11:54:37 oj 1.15.26.3: RESYNC: (1.15-1.16); FILE MERGED 2007/08/29 13:21:52 fs 1.15.26.2: #i10000# 2007/08/27 10:48:42 fs 1.15.26.1: removed unneeded include

--- dbaccess/source/ui/dlg/dbwiz.cxx
+++ dbaccess/source/ui/dlg/dbwiz.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dbwiz.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: ihi $ $Date: 2007-09-13 17:58:43 $
+ *  last change: $Author: hr $ $Date: 2007-11-01 15:12:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -83,9 +83,6 @@
 #endif
 #ifndef DBAUI_CONNECTIONPAGE_HXX
 #include "ConnectionPage.hxx"
-#endif
-#ifndef DBAUI_ADVANCEDPAGEDLG_HXX
-#include "AdvancedPageDlg.hxx"
 #endif
 #ifndef DBAUI_DRIVERSETTINGS_HXX
 #include "DriverSettings.hxx"
@@ -281,7 +278,7 @@ SfxItemSet* ODbTypeWizDialog::getWriteOutputSet()
     return m_pImpl->createConnection();
 }
 // -----------------------------------------------------------------------------
-Reference< XMultiServiceFactory > ODbTypeWizDialog::getORB()
+Reference< XMultiServiceFactory > ODbTypeWizDialog::getORB() const
 {
     return m_pImpl->getORB();
 }
