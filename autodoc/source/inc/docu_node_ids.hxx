fdd6a2b2ebb441e35bc6d9aa57208e72ff493830
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 15:45:26 +0000

    INTEGRATION: CWS adc18 (1.1.2); FILE ADDED 2007/10/18 15:21:39 np 1.1.2.1: #i81775#

---
+++ autodoc/source/inc/docu_node_ids.hxx
@@ -0,0 +1,72 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: docu_node_ids.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-11-02 16:45:26 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef ADC_DOCU_NODE_IDS_HXX
+#define ADC_DOCU_NODE_IDS_HXX
+
+
+
+namespace ary
+{
+namespace doc
+{
+namespace nodetype
+{
+enum E_Ids
+{
+
+    nt_none,
+    nt_OldCppDocu,
+    nt_OldIdlDocu
+
+
+
+
+
+
+
+
+
+};
+}   // namespace nodetype
+}   // namespace doc
+}   // namespace ary
+
+namespace docnt = ::ary::doc::nodetype;
+
+
+
+
+#endif
