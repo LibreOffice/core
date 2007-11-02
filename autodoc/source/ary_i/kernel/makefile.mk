2e75dda180767175c14bf437ff730f5e7591c943
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 15:23:10 +0000

    INTEGRATION: CWS adc18 (1.3.56); FILE MERGED 2007/10/18 14:24:49 np 1.3.56.1: #i81775#

--- autodoc/source/ary_i/kernel/makefile.mk
+++ autodoc/source/ary_i/kernel/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 17:23:33 $
+#   last change: $Author: hr $ $Date: 2007-11-02 16:23:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,8 +56,7 @@ PRJINC=$(PRJ)$/source
 OBJFILES= \
     $(OBJ)$/ci_atag2.obj	\
     $(OBJ)$/ci_text2.obj	\
-    $(OBJ)$/codeinf2.obj	\
-    $(OBJ)$/d_token.obj	
+    $(OBJ)$/d_token.obj
 
 
 
