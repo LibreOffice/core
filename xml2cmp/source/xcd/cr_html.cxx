26b3fca1908ff7f64972a4d1131dd13c76b310a4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 11:56:02 +0000

    INTEGRATION: CWS os2port02 (1.9.20); FILE MERGED 2007/10/04 19:45:26 ydario 1.9.20.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- xml2cmp/source/xcd/cr_html.cxx
+++ xml2cmp/source/xcd/cr_html.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cr_html.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: vg $ $Date: 2006-09-25 13:26:08 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 12:56:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ HtmlCreator::HtmlCreator( const char *              i_pOutputFileName,
                           const XmlElement &        i_rDocument,
                           const Simstr &            i_sIDL_BaseDirectory )
     :   aFile(i_pOutputFileName, std::ios::out
-#ifdef WNT
+#if defined(WNT) || defined(OS2)
                                                | std::ios::binary
 #endif
                                                              ),
