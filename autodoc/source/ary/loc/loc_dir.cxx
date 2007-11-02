f0469ee4b26648492a4b045f8477114e8e2a3b4a
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 15:16:02 +0000

    INTEGRATION: CWS adc18 (1.4.2); FILE MERGED 2007/10/19 13:08:58 np 1.4.2.2: #i81775# 2007/10/18 14:19:06 np 1.4.2.1: #i81775#

--- autodoc/source/ary/loc/loc_dir.cxx
+++ autodoc/source/ary/loc/loc_dir.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: loc_dir.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2007-09-18 13:45:33 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 16:16:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,6 +38,9 @@
 
 
 // NOT FULLY DEFINED SERVICES
+#include <ary/loc/loc_file.hxx>
+#include <sortedids.hxx>
+#include "locs_le.hxx"
 
 
 namespace ary
@@ -45,39 +48,105 @@ namespace ary
 namespace loc
 {
 
-
-void
-Directory::Add_ChildDir( const udmstri &     i_sName,
-                         Lid                 i_nId )
+struct Directory::Container
 {
-    aChildDirs[i_sName] = i_nId;
+    typedef SortedIds<Le_Compare>           SortedChildList;
+
+    SortedChildList     aSubDirectories;
+    SortedChildList     aFiles;
+
+                        Container()
+                            :   aSubDirectories(),
+                                aFiles()
+                            {}
+};
+
+
+
+
+Directory::Directory(Le_id i_assignedRoot)
+    :   sLocalName(),
+        nParentDirectory(0),
+        nAssignedRoot(i_assignedRoot),
+        aAssignedNode(),
+        pChildren(new Container)
+{
+    aAssignedNode.Assign_Entity(*this);
+}
+
+Directory::Directory( const String  &   i_localName,
+                      Le_id             i_parentDirectory )
+    :   sLocalName(i_localName),
+        nParentDirectory(i_parentDirectory),
+        nAssignedRoot(0),
+        aAssignedNode(),
+        pChildren(new Container)
+{
+    aAssignedNode.Assign_Entity(*this);
+}
+
+Directory::~Directory()
+{
 }
 
 void
-Directory::Add_File( const udmstri &     i_sName,
-                     Lid                 i_nId )
+Directory::Add_Dir(const Directory  &  i_dir)
 {
-    aFiles[i_sName] = i_nId;
+    pChildren->aSubDirectories.Add(i_dir.LeId());
 }
 
-Directory::Directory( Lid i_nId )
-    :   // aChildDirs,
-        // aFiles,
-        nId( i_nId )
+void
+Directory::Add_File(const File & i_file)
 {
+    pChildren->aFiles.Add(i_file.LeId());
 }
 
-SubDirectory::SubDirectory( Lid                 i_nId,
-                            const udmstri &     i_sName,
-                            Lid                 i_nParentDirectory )
-    :   Directory(i_nId),
-        sName(i_sName),
-        nParentDirectory(i_nParentDirectory)
+Le_id
+Directory::Search_Child(const String & i_name) const
 {
+    Le_id
+        ret = Search_Dir(i_name);
+    if (ret.IsValid())
+        return ret;
+    return Search_File(i_name);
 }
 
+Le_id
+Directory::Search_Dir(const String & i_name) const
+{
+    return pChildren->aSubDirectories.Search(i_name);
+}
+
+Le_id
+Directory::Search_File(const String & i_name) const
+{
+    return pChildren->aFiles.Search(i_name);
+}
+
+void
+Directory::do_Accept(csv::ProcessorIfc & io_processor) const
+{
+    csv::CheckedCall(io_processor,*this);
+}
+
+ClassId
+Directory::get_AryClass() const
+{
+    return class_id;
+}
+
+const String &
+Directory::inq_LocalName() const
+{
+    return sLocalName;
+}
+
+Le_id
+Directory::inq_ParentDirectory() const
+{
+    return nParentDirectory;
+}
 
 
 } // namespace loc
 } // namespace ary
-
