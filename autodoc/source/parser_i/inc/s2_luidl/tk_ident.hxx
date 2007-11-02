d6ad98a966b0691424d96524e574140872c48d9c
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 16:19:05 +0000

    INTEGRATION: CWS adc18 (1.2.56); FILE MERGED 2007/10/19 10:37:32 np 1.2.56.1: #i81775#

--- autodoc/source/parser_i/inc/s2_luidl/tk_ident.hxx
+++ autodoc/source/parser_i/inc/s2_luidl/tk_ident.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tk_ident.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:05:45 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 17:19:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ class TokIdentifier : public Token
                         Text() const;
   private:
     // DATA
-    udmstri             sText;
+    String              sText;
 };
 
 class TokNameSeparator : public Token
