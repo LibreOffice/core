9888dc04208b7cbe3ad9705d2d42c981f2745a09
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 11:19:47 +0000

    INTEGRATION: CWS os2port02 (1.22.134); FILE MERGED 2007/10/04 19:44:53 ydario 1.22.134.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- svx/source/svxlink/fileobj.cxx
+++ svx/source/svxlink/fileobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fileobj.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:14:20 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 12:19:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,10 +35,6 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifdef OS2
-#include <vcl/sysdep.hxx>
-#endif
 
 #ifndef _SV_WRKWIN_HXX
 #include <vcl/wrkwin.hxx>
