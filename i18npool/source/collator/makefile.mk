0d2e3812f3ae4685abe00c9610f6cca3d0c115fc
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 11:37:29 +0000

    INTEGRATION: CWS os2port02 (1.14.2); FILE MERGED 2007/09/29 16:40:30 ydario 1.14.2.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- i18npool/source/collator/makefile.mk
+++ i18npool/source/collator/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: vg $ $Date: 2007-09-20 15:33:25 $
+#   last change: $Author: hr $ $Date: 2007-11-02 12:37:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ rules_dependencies:=$(foreach,i,$(tempvar) data$/$i)
 .IF "$(GUI)"=="WNT"
 CFLAGSCXX+=-DLOCAL_RULE_LANGS="\"$(LOCAL_RULE_LANGS)\""
 .ELIF "$(GUI)"=="OS2"
-CFLAGSCXX+=-DLOCAL_RULE_LANGS="$(LOCAL_RULE_LANGS)"
+#CFLAGSCXX+=-DLOCAL_RULE_LANGS="$(LOCAL_RULE_LANGS)"
 .ELSE
 CFLAGSCXX+=-DLOCAL_RULE_LANGS='"$(LOCAL_RULE_LANGS)"'
 .ENDIF
