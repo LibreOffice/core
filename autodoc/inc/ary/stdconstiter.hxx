b23f7a46e4bd2ecfad69b325fbf91662ed2d8fd7
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 13:43:44 +0000

    INTEGRATION: CWS adc18 (1.3.56); FILE MERGED 2007/10/18 13:06:35 np 1.3.56.1: #i81775#

--- autodoc/inc/ary/stdconstiter.hxx
+++ autodoc/inc/ary/stdconstiter.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stdconstiter.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 15:55:48 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 14:43:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,7 @@
 
 #ifndef ARY_STDCONSTITER_HXX
 #define ARY_STDCONSTITER_HXX
+//  KORR_DEPRECATED_3.0
 
 
 // USED SERVICES
@@ -98,7 +99,8 @@ class Dyn_StdConstIterator
 };
 
 
+
+
+
 }   // namespace ary
-
-
 #endif
