0385cac00b789aa1b167db587258f7b8cca40683
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 13:54:13 +0000

    INTEGRATION: CWS adc18 (1.1.2); FILE ADDED 2007/10/18 14:50:40 np 1.1.2.1: #i81775#

---
+++ autodoc/inc/ary/cpp/cp_def.hxx
@@ -0,0 +1,107 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: cp_def.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-11-02 14:54:13 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef ARY_CPP_CP_DEF_HXX
+#define ARY_CPP_CP_DEF_HXX
+
+
+// USED SERVICES
+    // BASE CLASSES
+    // OTHER
+#include <ary/cpp/c_types4cpp.hxx>
+
+namespace ary
+{
+namespace cpp
+{
+    class Define;
+    class InputContext;
+    class Macro;
+}
+}
+
+
+
+
+namespace ary
+{
+namespace cpp
+{
+
+
+
+/** Acess to C++ defines and macros in the repository.
+*/
+class DefPilot
+{
+  public:
+    typedef DefsList::const_iterator                DefsIterator;
+
+
+
+    // LIFECYCLE
+    virtual             ~DefPilot() {}
+
+
+    // OPERATIONS
+    virtual Define &    Store_Define(
+                            const InputContext& i_rContext,
+                            const String  &     i_sName,
+                            const StringVector &
+                                                i_rDefinition ) = 0;
+    virtual Macro &     Store_Macro(
+                            const InputContext& i_rContext,
+                            const String  &     i_sName,
+                            const StringVector &
+                                                i_rParams,
+                            const StringVector &
+                                                i_rDefinition ) = 0;
+    // INQUIRY
+    virtual const DefineEntity &
+                        Find_Def(
+                            De_id               i_id ) const = 0;
+    virtual DefsResultList
+                        AllDefines() const = 0;
+    virtual DefsResultList
+                        AllMacros() const = 0;
+};
+
+
+
+
+
+}   // namespace cpp
+}   // namespace ary
+#endif
