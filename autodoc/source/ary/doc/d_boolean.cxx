de8ca9f4fde908de7b7b0fc04cf6e17481355808
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 14:36:51 +0000

    INTEGRATION: CWS adc18 (1.1.2); FILE ADDED 2007/10/22 12:56:31 np 1.1.2.2: #i81775# 2007/10/18 15:03:53 np 1.1.2.1: #i81775#

---
+++ autodoc/source/ary/doc/d_boolean.cxx
@@ -0,0 +1,63 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: d_boolean.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-11-02 15:36:51 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <precomp.h>
+#include <ary/doc/d_boolean.hxx>
+
+
+// NOT FULLY DEFINED SERVICES
+
+
+
+namespace ary
+{
+namespace doc
+{
+
+Boolean::~Boolean()
+{
+}
+
+void
+Boolean::do_Accept(csv::ProcessorIfc & io_processor ) const
+{
+    csv::CheckedCall(io_processor,*this);
+}
+
+
+
+
+}   // namespace doc
+}   // namespace ary
