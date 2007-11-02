5a44d6a2cbd1e2564036a871c585c8a8052d8d6f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 11:24:34 +0000

    INTEGRATION: CWS os2port02 (1.9.20); FILE MERGED 2007/10/05 13:00:35 ydario 1.9.20.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- configmgr/workben/local_io/makefile.mk
+++ configmgr/workben/local_io/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 08:47:58 $
+#   last change: $Author: hr $ $Date: 2007-11-02 12:24:34 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,7 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :  settings.mk
 
 # --- Files --------------------------------------------------------
-CDEFS += -DDLL_VERSION=\"$(UPD)$(DLLPOSTFIX)\"
+CDEFS += -DDLL_VERSION=$(EMQ)"$(UPD)$(DLLPOSTFIX)$(EMQ)"
 
 # ... common for all test executables ..............................
 APPSTDLIBS=\
