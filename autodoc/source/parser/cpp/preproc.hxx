7cf35c1ad39d70a178b8284d37845d4989d5e12f
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 16:00:26 +0000

    INTEGRATION: CWS adc18 (1.2.56); FILE MERGED 2007/10/19 10:37:30 np 1.2.56.1: #i81775#

--- autodoc/source/parser/cpp/preproc.hxx
+++ autodoc/source/parser/cpp/preproc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: preproc.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:32:07 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 17:00:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ class DefineDescription;
 class PreProcessor
 {
   public:
-    typedef std::map< udmstri, DefineDescription* > MacroMap;
+    typedef std::map< String, DefineDescription* > MacroMap;
 
     // LIFECYCLE
                         PreProcessor();
