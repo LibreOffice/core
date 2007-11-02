d2fbaa671a5392ee9fb65bc950837c4d72e8de22
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 14:17:38 +0000

    INTEGRATION: CWS adc18 (1.1.2); FILE ADDED 2007/10/18 14:58:27 np 1.1.2.1: #i81775#

---
+++ autodoc/inc/ary/loc/loc_filebase.hxx
@@ -0,0 +1,82 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: loc_filebase.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-11-02 15:17:38 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef ARY_LOC_FILEBASE_HXX
+#define ARY_LOC_FILEBASE_HXX
+
+// USED SERVICES
+    // BASE CLASSES
+#include <ary/loc/loc_le.hxx>
+    // COMPONENTS
+    // PARAMETERS
+
+
+namespace ary
+{
+namespace loc
+{
+
+
+
+/** Base class for classes representing source code files.
+*/
+class FileBase : public LocationEntity
+{
+  public:
+    virtual             ~FileBase() {}
+
+    String              Extension() const;
+
+  protected:
+                        FileBase(
+                            const String  &     i_localName ,
+                            Le_id               i_parentDirectory );
+  private:
+    // Interface LocationEntity:
+    virtual const String &
+                        inq_LocalName() const;
+    virtual Le_id       inq_ParentDirectory() const;
+
+    // DATA
+    String              sLocalName;
+    Le_id               nParentDirectory;
+};
+
+
+
+
+} // namespace loc
+} // namespace ary
+#endif
