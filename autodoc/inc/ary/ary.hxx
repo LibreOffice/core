f1c69f94fdd11d6c2d57cf3acaa948771c3ef277
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 13:36:51 +0000

    INTEGRATION: CWS adc18 (1.4.56); FILE MERGED 2007/10/18 13:06:34 np 1.4.56.1: #i81775#

--- autodoc/inc/ary/ary.hxx
+++ autodoc/inc/ary/ary.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ary.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 15:48:56 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 14:36:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,41 +36,36 @@
 #ifndef ARY_ARY_HXX
 #define ARY_ARY_HXX
 
-//  VERSION:            Autodoc 2.2
-
 
 // USED SERVICES
     // BASE CLASSES
-    // COMPONENTS
-    // PARAMETERS
+    // OTHER
 
 namespace ary
 {
-    class Command;
+namespace idl
+{
+    class Gate;
+}
 
-    namespace idl
-    {
-        class Gate;
-    }
+namespace cpp
+{
+    class Gate;
+}
+}
 
-    namespace cpp
-    {
-        class RwGate;
-        class DisplayGate;
-    }
 
-namespace n22
+
+namespace ary
 {
 
 /** Starting point for all work with the
     Autodoc Sourcecode Repository.
 
-    @resp
     Create and destroy the repository and
     give access to the "Gates" for different tasks.
 
-    @collab ::ary::cpp::DisplayGate
-    @collab ::ary::cpp::RwGate
+    @collab ::ary::cpp::Gate
     @collab ::ary::idl::Gate
 */
 
@@ -78,148 +73,26 @@ class Repository
 {
   public:
     //  LIFECYCLE
-    virtual             ~Repository() { }
-    static Repository & Create_();
-    static Repository & The_();
-    static void         Destroy_();      /// Destroys the Repository.
-
-    // OPERATIONS
-    void                Perform(
-                            ::ary::Command &    io_rCommand );
-
+    virtual             ~Repository() {}
+    static DYN Repository &
+                        Create_();
     // INQUIRY
-    const String &      Name() const;
-    bool                HasIdl() const;
-    bool                HasCpp() const;
-    const ::ary::idl::Gate &
-                        Gate_Idl() const;
-    const ary::cpp::DisplayGate &
-                        Gate_Cpp() const;
-
+    virtual const String &
+                        Title() const = 0;
+    virtual const ::ary::cpp::Gate &
+                        Gate_Cpp() const = 0;
+    virtual const ::ary::idl::Gate &
+                        Gate_Idl() const = 0;
     // ACCESS
-    ::ary::idl::Gate &  Gate_Idl();
-    ::ary::cpp::RwGate &
-                        Gate_Cpp();
-    void                Set_Name(
-                            const String &      i_sName );
-  private:
-    // Locals
-    virtual void                        do_Perform(::ary::Command & io_rCommand) = 0;
-    virtual const String &              inq_Name() const = 0;
-    virtual bool                        inq_HasIdl() const = 0;
-    virtual bool                        inq_HasCpp() const = 0;
-    virtual const ::ary::idl::Gate &    inq_Gate_Idl() const = 0;
-    virtual const ::ary::cpp::DisplayGate &
-                                        inq_Gate_Cpp() const = 0;
-    virtual ::ary::idl::Gate &          access_Gate_Idl() = 0;
-    virtual ::ary::cpp::RwGate &        access_Gate_Cpp() = 0;
-    virtual void                        do_Set_Name(const String & i_sName) = 0;
+    virtual ::ary::cpp::Gate &
+                        Gate_Cpp() = 0;
+    virtual ::ary::idl::Gate &
+                        Gate_Idl() = 0;
+    virtual void        Set_Title(
+                            const String &      i_sName ) = 0;
 };
 
-
-
-
-// IMPLEMENTATION
-inline void
-Repository::Perform( ::ary::Command & io_rCommand )
-    { do_Perform(io_rCommand); }
-
-inline const String &
-Repository::Name() const
-    { return inq_Name(); }
-
-inline bool
-Repository::HasIdl() const
-    { return inq_HasIdl(); }
-
-inline bool
-Repository::HasCpp() const
-    { return inq_HasCpp(); }
-
-inline const ::ary::idl::Gate &
-Repository::Gate_Idl() const
-    { return inq_Gate_Idl(); }
-
-inline const cpp::DisplayGate &
-Repository::Gate_Cpp() const
-    { return inq_Gate_Cpp(); }
-
-inline ::ary::idl::Gate &
-Repository::Gate_Idl()
-    { return access_Gate_Idl(); }
-
-inline cpp::RwGate &
-Repository::Gate_Cpp()
-    { return access_Gate_Cpp(); }
-inline void
-Repository::Set_Name( const String & i_sName )
-    { do_Set_Name(i_sName); }
-
-} // namespace n22
-
-
-
-    class IdGenerator;
-
-/** Starting point for work with the repository.
-
-    @short
-    From here you get handles for all work on the
-    repository.
-
-    @descr
-    In the whole repository interface the abbreviation
-    "Ce" means "CodeEntity". A CodeEntity is
-    one of the items that can be found in the repository
-    and that is able to have a documentation.
-**/
-
-class Repository
-{
-  public:
-    //  LIFECYCLE
-    virtual             ~Repository() { }
-    static Repository & Create_(
-                            DYN IdGenerator *   let_dpIds );    /// May be 0. Then a default is used.
-    static Repository & The_();
-    static void         Destroy_();      /// Destroys the Repository.
-
-    // INQUIRY
-    const cpp::DisplayGate &
-                        DisplayGate_Cpp() const;
-    const udmstri &     Name() const;
-
-    // ACCESS
-    cpp::RwGate &       RwGate_Cpp();
-
-
-  private:
-    virtual const cpp::DisplayGate &
-                        inq_DisplayGate_Cpp() const = 0;
-    virtual const udmstri &
-                        inq_Name() const = 0;
-    virtual cpp::RwGate &
-                        access_RwGate_Cpp() = 0;
-};
-
-
-
-// IMPLEMENTATION
-inline const cpp::DisplayGate &
-Repository::DisplayGate_Cpp() const
-    { return inq_DisplayGate_Cpp(); }
-inline const udmstri &
-Repository::Name() const
-    { return inq_Name(); }
-inline cpp::RwGate &
-Repository::RwGate_Cpp()
-    { return access_RwGate_Cpp(); }
 
 
 } // namespace ary
-
-
 #endif
-
-
-
