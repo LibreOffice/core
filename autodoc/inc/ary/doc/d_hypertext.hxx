ca6a1ca2e1d2e6f3453e8f1dcb587d75487eaeb4
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 13:58:37 +0000

    INTEGRATION: CWS adc18 (1.1.2); FILE ADDED 2007/10/18 14:54:13 np 1.1.2.1: #i81775#

---
+++ autodoc/inc/ary/doc/d_hypertext.hxx
@@ -0,0 +1,69 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: d_hypertext.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: hr $ $Date: 2007-11-02 14:58:37 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef ARY_DOC_D_HYPERTEXT_HXX
+#define ARY_DOC_D_HYPERTEXT_HXX
+
+// USED SERVICES
+#include <ary/doc/ht/dht_component.hxx>
+
+namespace csv
+{
+    class ProcessorIfc;
+}
+
+
+
+
+namespace ary
+{
+namespace doc
+{
+
+
+typedef std::vector<ht::Component>  HyperText;
+
+void                Accept(
+                        const HyperText &   i_text,
+                        csv::ProcessorIfc & io_processor );
+
+const HyperText &   NullText();
+
+
+
+
+}   // namspace doc
+}   // namspace ary
+#endif
