b36354f0e74a3d437bdc7cdbc632513e233d9d12
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 12:12:11 +0000

    INTEGRATION: CWS os2port02 (1.3.36); FILE MERGED 2007/10/05 13:01:02 ydario 1.3.36.2: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration. 2007/09/29 16:27:07 ydario 1.3.36.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- cpputools/source/regcomplazy/makefile.mk
+++ cpputools/source/regcomplazy/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:36:42 $
+#   last change: $Author: hr $ $Date: 2007-11-02 13:12:11 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -47,7 +47,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Files --------------------------------------------------------
 
-CDEFS += -DDLL_VERSION=\"$(UPD)$(DLLPOSTFIX)\"
+CDEFS += -DDLL_VERSION=$(EMQ)"$(UPD)$(DLLPOSTFIX)$(EMQ)"
 
 DEPOBJFILES= \
     $(OBJ)$/regcomplazy.obj 
