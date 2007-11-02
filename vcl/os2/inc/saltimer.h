f86bf189d8555183a282af2a897c20095d4abcb9
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 11:47:17 +0000

    INTEGRATION: CWS os2port02 (1.2.2); FILE MERGED 2007/10/08 14:28:29 obr 1.2.2.2: RESYNC: (1.2-1.3); FILE MERGED 2007/09/30 13:04:32 ydario 1.2.2.1: Issue number: i82034 Submitted by: ydario Reviewed by:  ydario Commit of changes for OS/2 CWS source code integration.

--- vcl/os2/inc/saltimer.h
+++ vcl/os2/inc/saltimer.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saltimer.h,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2007-09-25 10:06:29 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 12:47:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _SV_SALTIMER_H
 
 #ifndef _SV_SALTIMER_HXX
-#include <saltimer.hxx>
+#include <vcl/saltimer.hxx>
 #endif
 
 class Os2SalTimer : public SalTimer
