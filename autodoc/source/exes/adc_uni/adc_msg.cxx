75fd5f299ba324f173a73c5bbdb846be8850b764
Jens-Heiner Rechtien <hr@openoffice.org>
2007-11-02 15:43:47 +0000

    INTEGRATION: CWS adc18 (1.6.2); FILE MERGED 2007/10/18 15:23:15 np 1.6.2.1: #i81775#

--- autodoc/source/exes/adc_uni/adc_msg.cxx
+++ autodoc/source/exes/adc_uni/adc_msg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: adc_msg.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2007-09-18 14:05:39 $
+ *  last change: $Author: hr $ $Date: 2007-11-02 16:43:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 
 // NOT FULLY DEFINED SERVICES
 #include <cosv/file.hxx>
-#include <cosv/template/tpltools.hxx>
+#include <cosv/tpl/tpltools.hxx>
 
 
 namespace autodoc
