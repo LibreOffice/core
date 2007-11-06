c4dd0a64fc43dd20a85e30e81680a0e3cb4a936d
Rüdiger Timm <rt@openoffice.org>
2007-11-06 14:42:06 +0000

    INTEGRATION: CWS execshellmacrogone (1.7.102); FILE MERGED 2007/10/24 06:35:39 rene 1.7.102.1: #i79760# krs patches for supressing "Executing shell macro..." output

--- extensions/source/activex/msidl/makefile.mk
+++ extensions/source/activex/msidl/makefile.mk
@@ -1,12 +1,12 @@
 #**************************************************************************
 #
-#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.7 2007-03-26 13:05:46 vg Exp $
+#     $Header: /zpool/svn/migration/cvs_rep_09_09_08/code/extensions/source/activex/msidl/makefile.mk,v 1.8 2007-11-06 15:42:06 rt Exp $
 #
 # =========================================================================
 #
-#     $Date: 2007-03-26 13:05:46 $
-#     $Author: vg $
-#     $Revision: 1.7 $
+#     $Date: 2007-11-06 15:42:06 $
+#     $Author: rt $
+#     $Revision: 1.8 $
 #
 # =========================================================================
 #
@@ -39,10 +39,10 @@ SOURCE=so_activex.idl
 MTL_SWITCHES=-tlb $(MISC)$/so_activex.tlb -h $(MISC)$/so_activex.h -iid $(MISC)$/so_activex_i.c -dlldata $(MISC)$/so_activex_dll.c -proxy $(MISC)$/so_activex_p.c -Oicf
 
 .IF "$(COM)"=="GCC"
-MTL_SWITCHES+=-cpp_cmd gcc -cpp_opt "-x c -E -D__stdcall= -D_stdcall= -nostdinc -I $(shell cygpath -w $(PSDK_HOME)/include)"
+MTL_SWITCHES+=-cpp_cmd gcc -cpp_opt "-x c -E -D__stdcall= -D_stdcall= -nostdinc -I $(shell @cygpath -w $(PSDK_HOME)/include)"
 
 $(MISC)$/activex_component : $(SOURCE) 
-    $(MTL) $(MTL_SWITCHES) -I $(shell cygpath -w $(PSDK_HOME)/include|sed 's#\\#\\\\#g') $(SOURCE) 
+    $(MTL) $(MTL_SWITCHES) -I $(shell @cygpath -w $(PSDK_HOME)/include|sed 's#\\#\\\\#g') $(SOURCE) 
 .ELSE
 $(MISC)$/activex_component : $(SOURCE) 
     $(WRAPCMD) $(MTL) $(MTL_SWITCHES) $(INCLUDE) $(SOURCE) && $(TOUCH) $@
