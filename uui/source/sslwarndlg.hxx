6670e4f2d3d4f605d456d04f2f0d4cb5861da36e
Rüdiger Timm <rt@openoffice.org>
2007-11-07 09:11:19 +0000

    INTEGRATION: CWS tkr05_SRC680 (1.1.2); FILE ADDED 2007/10/09 10:08:51 tkr 1.1.2.2: #31053# https webdav support 2007/10/01 07:55:22 tkr 1.1.2.1: #31053#: HTTPS Support

---
+++ uui/source/sslwarndlg.hxx
@@ -0,0 +1,99 @@
+/*************************************************************************
+*
+*  OpenOffice.org - a multi-platform office productivity suite
+*
+*  $RCSfile: sslwarndlg.hxx,v $
+*
+*  $Revision: 1.2 $
+*
+*  last change: $Author: rt $ $Date: 2007-11-07 10:11:19 $
+*
+*  The Contents of this file are made available subject to
+*  the terms of GNU Lesser General Public License Version 2.1.
+*
+*
+*    GNU Lesser General Public License Version 2.1
+*    =============================================
+*    Copyright 2005 by Sun Microsystems, Inc.
+*    901 San Antonio Road, Palo Alto, CA 94303, USA
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License version 2.1, as published by the Free Software Foundation.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*    MA  02111-1307  USA
+*
+************************************************************************/
+#ifndef UUI_SSLWARN_HXX
+#define UUI_SSLWARN_HXX
+
+
+#include <vcl/dialog.hxx>
+
+#include <vcl/fixed.hxx>
+#include <vcl/button.hxx>
+
+#ifndef _COM_SUN_STAR_SECURITY_XCERTIFICATE_HPP_
+#include <com/sun/star/security/XCertificate.hpp>
+#endif
+#ifndef _COM_SUN_STAR_XML_CRYPTO_XXSECURITYENVIRONMENT_HPP_
+#include <com/sun/star/xml/crypto/XSecurityEnvironment.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+namespace cssu = com::sun::star::uno;
+namespace dcss = ::com::sun::star;
+
+
+using namespace com::sun::star;
+
+//=====================================================================
+//= Https_WarnDialog
+//=====================================================================
+class SSLWarnDialog : public ModalDialog
+{
+private:
+    FixedText       m_aLabel1;
+    PushButton      m_aOkButton;
+    CancelButton    m_aCancelButton;
+    PushButton      m_aCommandButtonViewCert;
+    FixedLine       m_aLine;
+    FixedImage      m_aWarnImage;
+
+
+    const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& m_xServiceFactory;
+    const cssu::Reference< dcss::security::XCertificate >& m_rXCert;
+    Window* m_pParent;
+    ResMgr*     pResourceMgr;
+
+    DECL_LINK( OKHdl_Impl, PushButton * );
+    DECL_LINK( ViewCertHdl_Impl, PushButton * );
+
+    public:
+    SSLWarnDialog( Window* pParent,
+                       const cssu::Reference< dcss::security::XCertificate >& rXCert,
+                       const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory >& xServiceFactory,
+                       ResMgr * pResMgr );
+
+    const com::sun::star::uno::Reference< com::sun::star::lang::XMultiServiceFactory > getServiceFactory() { return m_xServiceFactory; };
+
+    cssu::Reference< dcss::security::XCertificate > getCert() { return m_rXCert; };
+
+    Window* getParent() { return m_pParent; };
+
+    void setDescription1Text( const rtl::OUString &aText ) { m_aLabel1.SetText( aText ); };
+};
+
+#endif // UUI_SSLWARN_HXX
+
