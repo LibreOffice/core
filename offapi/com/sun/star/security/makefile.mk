d7c94e8a6f97c9a69a95c8dae1e66f41e505d308
Rüdiger Timm <rt@openoffice.org>
2007-11-07 08:58:00 +0000

    INTEGRATION: CWS tkr05_SRC680 (1.6.172); FILE MERGED 2007/10/11 14:26:49 tkr 1.6.172.2: #31053# WebDAV Https Support 2007/09/20 10:59:22 tkr 1.6.172.1: #i31053#: WebDAV HTTPS Support

--- offapi/com/sun/star/security/makefile.mk
+++ offapi/com/sun/star/security/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2006-12-01 17:14:18 $
+#   last change: $Author: rt $ $Date: 2007-11-07 09:58:00 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,10 @@ IDLFILES=\
     CertificateValidity.idl             \
     XCertificateExtension.idl           \
     XDocumentDigitalSignatures.idl	\
-    KeyUsage.idl
+    KeyUsage.idl			\
+    XCertificateContainer.idl		\
+    CertificateContainer.idl \
+    CertificateContainerStatus.idl
 
 # ------------------------------------------------------------------
 
