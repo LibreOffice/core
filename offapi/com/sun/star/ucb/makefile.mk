105703ce9e65ce776b167d331fb291fa77b85405
Rüdiger Timm <rt@openoffice.org>
2007-11-07 08:58:38 +0000

    INTEGRATION: CWS tkr05_SRC680 (1.46.168); FILE MERGED 2007/09/20 10:59:22 tkr 1.46.168.1: #i31053#: WebDAV HTTPS Support

--- offapi/com/sun/star/ucb/makefile.mk
+++ offapi/com/sun/star/ucb/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.46 $
+#   $Revision: 1.47 $
 #
-#   last change: $Author: kz $ $Date: 2006-12-13 15:15:02 $
+#   last change: $Author: rt $ $Date: 2007-11-07 09:58:38 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -265,7 +265,8 @@ IDLFILES=\
      XSimpleFileAccess3.idl\
      XSortedDynamicResultSetFactory.idl\
     XSourceInitialization.idl\
-    XWebDAVCommandEnvironment.idl
+    XWebDAVCommandEnvironment.idl \
+    CertificateValidationRequest.idl
 
 # ------------------------------------------------------------------
 
