8ec830dada4d0b03f140429fbd91f11b4f42e58b
Rüdiger Timm <rt@openoffice.org>
2007-11-09 07:17:27 +0000

    INTEGRATION: CWS dbodf12import (1.1.2); FILE ADDED 2007/11/01 09:15:36 oj 1.1.2.1: #i83190# new elements

---
+++ dbaccess/source/filter/xml/xmlJavaClassPath.cxx
@@ -0,0 +1,118 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: xmlJavaClassPath.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-11-09 08:17:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_dbaccess.hxx"
+#ifndef DBA_XMLJAVACLASSPATH_HXX_INCLUDED
+#include "xmlJavaClassPath.hxx"
+#endif
+#ifndef DBA_XMLFILTER_HXX
+#include "xmlfilter.hxx"
+#endif
+#ifndef _XMLOFF_XMLTOKEN_HXX
+#include <xmloff/xmltoken.hxx>
+#endif
+#ifndef _XMLOFF_XMLNMSPE_HXX
+#include <xmloff/xmlnmspe.hxx>
+#endif
+#ifndef _XMLOFF_NMSPMAP_HXX
+#include <xmloff/nmspmap.hxx>
+#endif
+#ifndef DBA_XMLENUMS_HXX
+#include "xmlEnums.hxx"
+#endif
+#ifndef DBACCESS_SHARED_XMLSTRINGS_HRC
+#include "xmlstrings.hrc"
+#endif
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#ifndef TOOLS_DIAGNOSE_EX_H
+#include <tools/diagnose_ex.h>
+#endif
+
+namespace dbaxml
+{
+    using namespace ::com::sun::star::uno;
+    using namespace ::com::sun::star::xml::sax;
+DBG_NAME(OXMLJavaClassPath)
+
+OXMLJavaClassPath::OXMLJavaClassPath( ODBFilter& rImport,
+                sal_uInt16 nPrfx, const ::rtl::OUString& _sLocalName,
+                const Reference< XAttributeList > & _xAttrList ) :
+    SvXMLImportContext( rImport, nPrfx, _sLocalName )
+{
+    DBG_CTOR(OXMLJavaClassPath,NULL);
+
+    OSL_ENSURE(_xAttrList.is(),"Attribute list is NULL!");
+    const SvXMLNamespaceMap& rMap = rImport.GetNamespaceMap();
+    const SvXMLTokenMap& rTokenMap = rImport.GetComponentElemTokenMap();
+
+    ::rtl::OUString sJavaClassPath;
+    const sal_Int16 nLength = (_xAttrList.is()) ? _xAttrList->getLength() : 0;
+    for(sal_Int16 i = 0; i < nLength; ++i)
+    {
+        ::rtl::OUString sLocalName;
+        const rtl::OUString sAttrName = _xAttrList->getNameByIndex( i );
+        const sal_uInt16 nPrefix = rMap.GetKeyByAttrName( sAttrName,&sLocalName );
+        const rtl::OUString sValue = _xAttrList->getValueByIndex( i );
+        switch( rTokenMap.Get( nPrefix, sLocalName ) )
+        {
+            case XML_TOK_HREF:
+                sJavaClassPath += sValue;
+                break;
+        }
+        if ( sJavaClassPath.getLength() )
+            sJavaClassPath += ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(" "));
+    }
+    if ( sJavaClassPath.getLength() )
+    {
+        PropertyValue aProperty;
+        aProperty.Name = ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("JavaDriverClassPath"));
+        aProperty.Value <<= sJavaClassPath;
+        rImport.addInfo(aProperty);
+    }
+}
+// -----------------------------------------------------------------------------
+
+OXMLJavaClassPath::~OXMLJavaClassPath()
+{
+    DBG_DTOR(OXMLJavaClassPath,NULL);
+}
+// -----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+} // namespace dbaxml
+// -----------------------------------------------------------------------------
