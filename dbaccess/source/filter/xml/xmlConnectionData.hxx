80e48d06533b8c961898cdf2c501816b04d306b2
Rüdiger Timm <rt@openoffice.org>
2007-11-09 07:13:16 +0000

    INTEGRATION: CWS dbodf12import (1.1.2); FILE ADDED 2007/11/01 09:14:34 oj 1.1.2.1: #i83190# new elements

---
+++ dbaccess/source/filter/xml/xmlConnectionData.hxx
@@ -0,0 +1,68 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: xmlConnectionData.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2007-11-09 08:13:16 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef DBA_XMLCONNECTIONDATA_HXX_INCLUDED
+#define DBA_XMLCONNECTIONDATA_HXX_INCLUDED
+
+#ifndef _XMLOFF_XMLICTXT_HXX
+#include <xmloff/xmlictxt.hxx>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#include <vector>
+
+namespace dbaxml
+{
+    class ODBFilter;
+    class OXMLConnectionData : public SvXMLImportContext
+    {
+        bool m_bFoundOne;
+
+        ODBFilter& GetOwnImport();
+    public:
+
+        OXMLConnectionData( ODBFilter& rImport, sal_uInt16 nPrfx,
+                    const ::rtl::OUString& rLName);
+        virtual ~OXMLConnectionData();
+
+        virtual SvXMLImportContext *CreateChildContext( sal_uInt16 nPrefix,
+                    const ::rtl::OUString& rLocalName,
+                    const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XAttributeList > & xAttrList );
+    };
+// -----------------------------------------------------------------------------
+} // namespace dbaxml
+// -----------------------------------------------------------------------------
+
+#endif // DBA_XMLCONNECTIONDATA_HXX_INCLUDED
