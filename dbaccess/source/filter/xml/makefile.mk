9bb400a9a4974cc6a7b5e0957b0de7b1ef628f6d
Rüdiger Timm <rt@openoffice.org>
2007-11-09 07:12:37 +0000

    INTEGRATION: CWS dbodf12import (1.6.224); FILE MERGED 2007/11/01 09:13:31 oj 1.6.224.1: #i83190# implement odf12 importer

--- dbaccess/source/filter/xml/makefile.mk
+++ dbaccess/source/filter/xml/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 14:12:04 $
+#   last change: $Author: rt $ $Date: 2007-11-09 08:12:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -66,6 +66,12 @@ SLOFILES=	$(SLO)$/xmlfilter.obj				\
             $(SLO)$/xmlStyleImport.obj			\
             $(SLO)$/xmlHelper.obj				\
             $(SLO)$/xmlAutoStyle.obj			\
+            $(SLO)$/xmlConnectionData.obj		\
+            $(SLO)$/xmlDatabaseDescription.obj	\
+            $(SLO)$/xmlFileBasedDatabase.obj	\
+            $(SLO)$/xmlServerDatabase.obj		\
+            $(SLO)$/xmlConnectionResource.obj	\
+            $(SLO)$/xmlJavaClassPath.obj		\
             $(SLO)$/xmlservices.obj
 
 # --- Library -----------------------------------
