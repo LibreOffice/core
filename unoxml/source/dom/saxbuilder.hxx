267d0921043f5e159f1654ba528bfc0dcd68ae29
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-19 15:45:36 +0000

    INTEGRATION: CWS updchk10 (1.4.38); FILE MERGED 2007/09/14 16:19:52 lo 1.4.38.1: #i81589# xml serialization

--- unoxml/source/dom/saxbuilder.hxx
+++ unoxml/source/dom/saxbuilder.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: saxbuilder.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 00:48:32 $
+ *  last change: $Author: ihi $ $Date: 2007-11-19 16:45:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,9 +61,12 @@
 
 using namespace rtl;
 using namespace com::sun::star::uno;
-using namespace com::sun::star::lang;
 using namespace com::sun::star::xml::dom;
 using namespace com::sun::star::xml::sax;
+
+using com::sun::star::lang::XServiceInfo;
+using com::sun::star::lang::XSingleServiceFactory;
+using com::sun::star::lang::XMultiServiceFactory;
 
 namespace DOM
 {
