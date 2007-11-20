96a87a6ee0ff5b600d97c6fc29f72e9cea84dc8b
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:26:53 +0000

    INTEGRATION: CWS reportdesign01 (1.11.98); FILE MERGED 2007/10/04 11:36:12 lla 1.11.98.1: #i67655# cleanups

--- sal/qa/OStringBuffer/makefile.mk
+++ sal/qa/OStringBuffer/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 15:55:50 $
+#   last change: $Author: ihi $ $Date: 2007-11-20 19:26:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,7 @@ SHL1IMPLIB= i$(SHL1TARGET)
 
 DEF1NAME    =$(SHL1TARGET)
 # DEF1EXPORTFILE= export.exp
-SHL1VERSIONMAP = export.map
+SHL1VERSIONMAP = $(PRJ)$/qa$/export.map
 
 
 #------------------------------- All object files -------------------------------
