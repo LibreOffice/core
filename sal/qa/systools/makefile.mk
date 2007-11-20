4f9246c4c86f96316ff6dab26868dd6da1978dbf
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:51:18 +0000

    INTEGRATION: CWS reportdesign01 (1.4.98); FILE MERGED 2007/10/04 11:36:19 lla 1.4.98.1: #i67655# cleanups

--- sal/qa/systools/makefile.mk
+++ sal/qa/systools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 16:02:50 $
+#   last change: $Author: ihi $ $Date: 2007-11-20 19:50:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -66,7 +66,7 @@ SHL1IMPLIB= i$(SHL1TARGET)
 
 DEF1NAME    =$(SHL1TARGET)
 # DEF1EXPORTFILE= export.exp
-SHL1VERSIONMAP= export.map
+SHL1VERSIONMAP= $(PRJ)$/qa$/export.map
 # auto generated Target:joblist
 # END ------------------------------------------------------------------
 
--- sal/qa/testHelperFunctions/makefile.mk
+++ sal/qa/testHelperFunctions/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 16:03:03 $
+#   last change: $Author: ihi $ $Date: 2007-11-20 19:51:18 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ SHL1STDLIBS=\
 
 SHL1IMPLIB= i$(SHL1TARGET)
 DEF1NAME    =$(SHL1TARGET)
-SHL1VERSIONMAP = export.map
+SHL1VERSIONMAP = $(PRJ)$/qa$/export.map
 
 # END --------------------------------------------------------------
 
