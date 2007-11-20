c07f66946b1e2213e104913a58084514981af1d7
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:00:20 +0000

    INTEGRATION: CWS reportdesign01 (1.2.28); FILE MERGED 2007/10/16 06:08:57 oj 1.2.28.1: #i82574# import print condition for section

--- reportdesign/source/filter/xml/xmlComponent.cxx
+++ reportdesign/source/filter/xml/xmlComponent.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlComponent.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:17 $
+ *  last change: $Author: ihi $ $Date: 2007-11-20 19:00:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -124,7 +124,7 @@ OXMLComponent::OXMLComponent( ORptFilter& _rImport
             ::rtl::OUString sLocalName;
             const ::rtl::OUString sAttrName = _xAttrList->getNameByIndex( i );
             const sal_uInt16 nPrefix = rMap.GetKeyByAttrName( sAttrName,&sLocalName );
-            ::rtl::OUString sValue = _xAttrList->getValueByIndex( i );
+            const ::rtl::OUString sValue = _xAttrList->getValueByIndex( i );
 
             switch( rTokenMap.Get( nPrefix, sLocalName ) )
             {
