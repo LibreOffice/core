e332a19e0e1c77a660b30061ecd918b3c318af7e
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:48:05 +0000

    INTEGRATION: CWS reportdesign01 (1.5.98); FILE MERGED 2007/10/04 09:03:58 lla 1.5.98.1: #i67655# only updates on some tests to build warning free

--- sal/qa/rtl/uuid/rtl_Uuid.cxx
+++ sal/qa/rtl/uuid/rtl_Uuid.cxx
@@ -1,3 +1,41 @@
+ /*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: rtl_Uuid.cxx,v $
+ *
+ *  $Revision: 1.6 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-11-20 19:48:05 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sal.hxx"
+
 #include <math.h>
 #include <stdio.h>
 
@@ -35,8 +73,6 @@ inline void printUString( const ::rtl::OUString & str, const sal_Char * msg = ""
  * For diagnostics( from sal/test/testuuid.cxx )
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_sal.hxx"
 void printUuid( sal_uInt8 *pNode )
 {
     for( sal_Int32 i1 = 0 ; i1 < 4 ; i1++ )
