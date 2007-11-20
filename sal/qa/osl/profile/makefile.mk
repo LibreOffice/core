43caea55a7121113a624b46b9c9358faab2ed50c
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:33:07 +0000

    INTEGRATION: CWS reportdesign01 (1.4.98); FILE MERGED 2007/10/04 11:36:14 lla 1.4.98.1: #i67655# cleanups

--- sal/qa/osl/profile/makefile.mk
+++ sal/qa/osl/profile/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: vg $ $Date: 2007-01-16 15:57:25 $
+#   last change: $Author: ihi $ $Date: 2007-11-20 19:33:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ SHL1STDLIBS=\
 
 SHL1IMPLIB= i$(SHL1TARGET)
 DEF1NAME    =$(SHL1TARGET)
-SHL1VERSIONMAP = export.map
+SHL1VERSIONMAP = $(PRJ)$/qa$/export.map
 # END ------------------------------------------------------------------
 
 
