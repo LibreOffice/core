61523caa0a76672095e126ecd9820db36614f578
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-20 18:03:47 +0000

    INTEGRATION: CWS reportdesign01 (1.3.6); FILE MERGED 2007/10/16 06:08:59 oj 1.3.6.1: #i82574# import print condition for section

--- reportdesign/source/filter/xml/xmlRow.cxx
+++ reportdesign/source/filter/xml/xmlRow.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlRow.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-26 14:23:46 $
+ *  last change: $Author: ihi $ $Date: 2007-11-20 19:03:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -104,7 +104,7 @@ OXMLRow::OXMLRow( ORptFilter& rImport
      ::rtl::OUString sLocalName;
         const rtl::OUString sAttrName = _xAttrList->getNameByIndex( i );
         const sal_uInt16 nPrefix = rMap.GetKeyByAttrName( sAttrName,&sLocalName );
-        rtl::OUString sValue = _xAttrList->getValueByIndex( i );
+        const rtl::OUString sValue = _xAttrList->getValueByIndex( i );
 
         switch( rTokenMap.Get( nPrefix, sLocalName ) )
         {
