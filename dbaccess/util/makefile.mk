cc3d441dd45abe5a11e14cec0243cf4f8f84c31e
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-21 15:17:26 +0000

    INTEGRATION: CWS dba24c (1.31.40); FILE MERGED 2007/10/22 08:28:27 fs 1.31.40.1: #i52527#/#i73705#: DBA needs to link with SFX now (shouldn't the DocumentMacroMode be moved to module framework instead?)

--- dbaccess/util/makefile.mk
+++ dbaccess/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.31 $
+#   $Revision: 1.32 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-06 08:46:44 $
+#   last change: $Author: ihi $ $Date: 2007-11-21 16:17:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -74,6 +74,7 @@ SHL1STDLIBS= \
         $(CPPULIB) \
         $(VOSLIB) \
         $(SALLIB) \
+        $(SFXLIB) \
         $(SALHELPERLIB)
 
 SHL1LIBS=$(LIB1TARGET)
