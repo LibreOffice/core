0029015ea2b57dc29ba243213341a459e8565102
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-21 14:10:44 +0000

    INTEGRATION: CWS dba24c (1.16.132); FILE MERGED 2007/09/13 11:43:22 fs 1.16.132.1: removed unnecessary forward

--- connectivity/source/sdbcx/VView.cxx
+++ connectivity/source/sdbcx/VView.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: VView.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 03:13:01 $
+ *  last change: $Author: ihi $ $Date: 2007-11-21 15:10:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,6 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-#include <stdio.h>
 
 #ifndef _CONNECTIVITY_SDBCX_VIEW_HXX_
 #include "connectivity/sdbcx/VView.hxx"
