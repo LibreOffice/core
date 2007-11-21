2bc6487ebdfd2e35e5d4a047648b946831de6927
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-21 15:25:10 +0000

    INTEGRATION: CWS dba24c (1.7.30); FILE MERGED 2007/10/10 13:13:52 fs 1.7.30.1: in preparation of #i73705#/#i52527#: Macro Warning dialog is now available at the default interaction handler: +SVLLIB

--- uui/util/makefile.mk
+++ uui/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 13:07:40 $
+#   last change: $Author: ihi $ $Date: 2007-11-21 16:25:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,6 +55,7 @@ SHL1LIBS = \
 
 SHL1STDLIBS = \
     $(SVTOOLLIB) \
+    $(SVLLIB) \
     $(TKLIB) \
     $(VCLLIB) \
     $(UNOTOOLSLIB) \
