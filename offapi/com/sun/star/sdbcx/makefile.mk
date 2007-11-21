f3d6833e138f5d52a0f61e2f65edaea178b4605a
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-21 14:17:37 +0000

    INTEGRATION: CWS dba24c (1.7.180); FILE MERGED 2007/09/13 11:34:47 fs 1.7.180.1: #i49183# +XAlterView

--- offapi/com/sun/star/sdbcx/makefile.mk
+++ offapi/com/sun/star/sdbcx/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2006-12-01 17:14:04 $
+#   last change: $Author: ihi $ $Date: 2007-11-21 15:17:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -78,6 +78,7 @@ IDLFILES=\
     View.idl \
     ViewDescriptor.idl \
     XAlterTable.idl \
+    XAlterView.idl \
     XAppend.idl \
     XAuthorizable.idl \
     XColumnsSupplier.idl \
