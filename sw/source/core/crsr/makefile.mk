032b6a8b435331738f295e4e5667cf5ee6e44f0b
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-22 14:30:47 +0000

    INTEGRATION: CWS swcolsel (1.3.708); FILE MERGED 2007/10/05 15:53:24 ama 1.3.708.3: RESYNC: (1.4-1.5); FILE MERGED Merge conflicts solved 2007/08/21 14:31:23 ama 1.3.708.2: RESYNC: (1.3-1.4); FILE MERGED 2007/05/09 07:54:17 ama 1.3.708.1: #i1596#: Block selection

--- sw/source/core/crsr/makefile.mk
+++ sw/source/core/crsr/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hr $ $Date: 2007-09-27 08:30:07 $
+#   last change: $Author: ihi $ $Date: 2007-11-22 15:30:47 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -57,6 +57,7 @@ EXCEPTIONSFILES=    \
     $(SLO)$/viscrs.obj
 
 SLOFILES =	\
+    $(SLO)$/BlockCursor.obj \
     $(SLO)$/bookmrk.obj \
     $(SLO)$/callnk.obj \
     $(SLO)$/crbm.obj \
