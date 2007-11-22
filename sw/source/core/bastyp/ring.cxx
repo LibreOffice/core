cf9c6a8648ff07c61203a35f591dfd1330ced514
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-22 14:29:19 +0000

    INTEGRATION: CWS swcolsel (1.4.220); FILE MERGED 2007/04/13 13:06:34 ama 1.4.220.1: Fix #1596#: Rectangular selection

--- sw/source/core/bastyp/ring.cxx
+++ sw/source/core/bastyp/ring.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ring.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 20:42:47 $
+ *  last change: $Author: ihi $ $Date: 2007-11-22 15:29:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -116,6 +116,18 @@ void Ring::MoveRingTo(Ring *pDestRing)
     pDestPrev->pNext = this;
     pDestRing->pPrev = pMyPrev;
     pPrev = pDestPrev;
+}
+
+sal_uInt32 Ring::numberOf() const
+{
+    sal_uInt32 nRet = 1;
+    const Ring* pNxt = pNext;
+    while( pNxt != this )
+    {
+        ++nRet;
+        pNxt = pNxt->GetNext();
+    }
+    return nRet;
 }
 
 
