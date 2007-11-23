59fbda5214161d957cce92060955967018857cd7
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 13:35:48 +0000

    INTEGRATION: CWS configrefactor01 (1.7.84); FILE MERGED 2007/01/16 12:18:23 mmeeks 1.7.84.4: Submitted by: mmeeks Kill 'memory::Segment' - no longer needed. Bin some other (empty / redundant) headers. 2007/01/12 11:48:33 mmeeks 1.7.84.3: Remove a load of unneeded Accessor fluff Fix SimpleReferenceObject - it has to have 1 virtual method. 2007/01/11 10:35:36 mmeeks 1.7.84.2: Submitted by: mmeeks
    Large scale re-factoring, remove fine-grained locking in favor of a simple,
    single global lock (still in progress).
    Identify and fix various hot-spots.
    Remove otherwise empty / non-compiled files.
    Kill UpdateAccessor
    2007/01/08 20:49:02 mmeeks 1.7.84.1: Issue number:
    Submitted by: mmeeks
    Substantial configmgr re-factoring #1 ...
    	+ remove endless typedef chains
    	+ remove custom allocator & associated complexity
    	+ remove Pointer, and 'Address' classes

--- configmgr/source/treecache/cachedata.hxx
+++ configmgr/source/treecache/cachedata.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cachedata.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:22:46 $
+ *  last change: $Author: ihi $ $Date: 2007-11-23 14:35:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,74 +70,61 @@ namespace configmgr
     class CacheData
     {
     public:
-        typedef CacheLine        Module;
-        typedef CacheLineRef     ModuleRef;
-        typedef CacheLine::Path     Path;
-        typedef CacheLine::Name     ModuleName;
-    public:
-        CacheData(memory::HeapManager & _rHeapManager);
+        CacheData();
         virtual ~CacheData();
 
         /// retrieve the module tree name for the given path
-        static ModuleName extractModuleName(Path const& _aPath);
+        static CacheLine::Name extractModuleName(CacheLine::Path const& _aPath);
 
+        // attach a module with a given name
+        void attachModule(data::TreeAddress _aLocation, CacheLine::Name const & _aModule);
         /// check if the given module exists already (and is not empty)
-        bool hasModule(ModuleName const & _aModule) const;
+        bool hasModule(CacheLine::Name const & _aModule) const;
         /// checks if the given module exists and has defaults available
-        bool hasModuleDefaults(memory::Accessor const & _aAccessor, ModuleName const & _aModule) const;
+        bool hasModuleDefaults(CacheLine::Name const & _aModule) const;
 
-        /// creates a new data segment reference for the given path if exists
-        memory::Segment * attachDataSegment(memory::SegmentAddress const & _aLocation, ModuleName const & _aModule);
-        /// gets a data segment reference for the given path if it exists
-        memory::Segment * getDataSegment(ModuleName const & _aModule);
-        /// gets a data segment address for the given module if it exists
-        memory::SegmentAddress getDataSegmentAddress(ModuleName const & _aModule) const;
+        /// gets a tree address for the given module if it exists
+        data::TreeAddress getTreeAddress(CacheLine::Name const & _aModule) const;
 
         /// checks whether a certain node exists in the tree
-        bool  hasNode(memory::Accessor const & _aAccessor, Path const & _aLocation) const;
+        bool  hasNode(CacheLine::Path const & _aLocation) const;
 
         /// retrieve the given node without changing its ref count
-        data::NodeAddress   getNode(memory::Accessor const & _aAccessor, Path const & _rPath);
+        data::NodeAddress   getNode(CacheLine::Path const & _rPath);
         /// retrieve the given template tree without changing its ref count
-        data::TreeAddress   getTemplateTree(memory::Accessor const & _aAccessor, Path const & aTemplateName ) const;
+        data::TreeAddress   getTemplateTree( CacheLine::Path const & aTemplateName ) const;
 
         /// retrieve the subtree at _aPath and clientAcquire() it
-        data::NodeAddress acquireNode(memory::Accessor const & _aAccessor, Path const & _aPath );
-        /// retrieve the subtree at _aPath and clientAcquire() it
-        data::TreeAddress acquireModule( ModuleName const & _aModule );
+        data::NodeAddress acquireNode(CacheLine::Path const & _aPath );
+        /// retrieve the subtree at _aPath and clientAcquire() it, return true on success
+        bool acquireModule( CacheLine::Name const & _aModule );
         /// clientRelease() the tree at aComponentName, and return the resulting reference count
-        CacheLine::RefCount releaseModule( ModuleName const & _aModule, bool _bKeepDeadModule = false );
+        oslInterlockedCount releaseModule( CacheLine::Name const & _aModule, bool _bKeepDeadModule = false );
 
-        bool insertDefaults( memory::UpdateAccessor& _aAccessToken,
-                             backend::NodeInstance const & _aDefaultInstance
+        bool insertDefaults( backend::NodeInstance const & _aDefaultInstance
                            ) CFG_UNO_THROW_RTE();
 
         /// merge the given changes into this tree - reflects old values to _anUpdate
-        void applyUpdate(   memory::UpdateAccessor& _aUpdateToken,
-                            backend::UpdateInstance & _anUpdate) CFG_UNO_THROW_RTE(  );
+        void applyUpdate( backend::UpdateInstance & _anUpdate) CFG_UNO_THROW_RTE(  );
 
         // low-level interface for cache management
-        typedef std::map<ModuleName, ModuleRef> ModuleList;
+        typedef std::map<CacheLine::Name, CacheLineRef> ModuleList;
         ModuleList& accessModuleList() { return m_aModules; }
 
-        memory::HeapManager & getHeapManager() const { return m_rHeapManager; }
     protected:
-        virtual ModuleRef doCreateAttachedModule(memory::HeapManager & _rHeapManager, const memory::SegmentAddress & _aLocation, const ModuleName& _aName) CFG_UNO_THROW_RTE(  );
+        virtual CacheLineRef doCreateAttachedModule(data::TreeAddress _aLocation, const CacheLine::Name& _aName) CFG_UNO_THROW_RTE(  );
 
-        data::TreeAddress internalGetPartialTree(memory::Accessor const & _aAccessor, Path const & _aPath ) const;
-        data::NodeAddress internalGetNode(memory::Accessor const & _aAccessor, const Path& _rPath) const;
+        data::TreeAddress internalGetPartialTree(CacheLine::Path const & _aPath ) const;
+        data::NodeAddress internalGetNode(const CacheLine::Path& _rPath) const;
 
-        ModuleRef internalAttachModule(const memory::SegmentAddress & _aLocation, const ModuleName& _aName) CFG_UNO_THROW_RTE(  );
-        void internalAddModule(ModuleName const & _aName, ModuleRef const & _aModule);
+        CacheLineRef internalAttachModule(data::TreeAddress _aLocation, const CacheLine::Name& _aName) CFG_UNO_THROW_RTE(  );
+        void internalAddModule(CacheLine::Name const & _aName, CacheLineRef const & _aModule);
 
-        ModuleRef internalGetModule(const ModuleName& _aName) const;
-        ModuleRef internalGetModule(const Path& _aLocation) const;
+        CacheLineRef internalGetModule(const CacheLine::Name& _aName) const;
+        CacheLineRef internalGetModule(const CacheLine::Path& _aLocation) const;
 
-        memory::HeapManager & internalHeapManager() { return m_rHeapManager; }
     private:
         ModuleList m_aModules;
-
-        memory::HeapManager & m_rHeapManager;
     };
 ////////////////////////////////////////////////////////////////////////////////
     /** A collection of CacheLines for templates
@@ -146,25 +133,20 @@ namespace configmgr
     class TemplateCacheData : public CacheData
     {
     public:
-        TemplateCacheData(memory::HeapManager & _rHeapManager)
-        : CacheData(_rHeapManager)
+        TemplateCacheData() : CacheData()
         {
         }
-
-        /// gets a data segment reference for the given path - creates if necessary
-        memory::Segment * createDataSegment(ModuleName const & _aModule);
 
         /** add the given template tree at the given location,
             return the tree that is now pertinent and clientAcquire() it once
         */
-        data::TreeAddress addTemplates( memory::UpdateAccessor& _aAccessToken,
-                                        backend::ComponentData const & _aComponentInstance
+        data::TreeAddress addTemplates( backend::ComponentData const & _aComponentInstance
                                        ) CFG_UNO_THROW_RTE();
 
+        // create a new module with the given name
+        void createModule(CacheLine::Name const & _aModule) CFG_UNO_THROW_RTE();
     private:
-        virtual ModuleRef doCreateAttachedModule(memory::HeapManager & _rHeapManager, const memory::SegmentAddress & _aLocation, const ModuleName& _aName) CFG_UNO_THROW_RTE(  );
-
-        CacheLineRef implNewCacheLine(ModuleName const & _aModule) CFG_UNO_THROW_RTE(  );
+        virtual CacheLineRef doCreateAttachedModule(data::TreeAddress _aLocation, const CacheLine::Name& _aName) CFG_UNO_THROW_RTE(  );
     };
 //-----------------------------------------------------------------------------
     /** A collection of CacheLines
@@ -173,38 +155,35 @@ namespace configmgr
     class ExtendedCacheData : public CacheData
     {
     public:
-        ExtendedCacheData(memory::HeapManager & _rHeapManager)
-        : CacheData(_rHeapManager)
+        ExtendedCacheData() : CacheData()
         {
         }
-
-        /// gets a data segment reference for the given path - creates if necessary
-        memory::Segment * createDataSegment(ModuleName const & _aModule);
 
         /** add the given subtree at the given location,
             return the tree that is now pertinent and clientAcquire() it once
         */
-        data::TreeAddress addComponentData( memory::UpdateAccessor& _aAccessToken,
-                                            backend::ComponentInstance const & _aComponentInstance,
+        data::TreeAddress addComponentData( backend::ComponentInstance const & _aComponentInstance,
                                             bool _bWithDefaults
                                            ) CFG_UNO_THROW_RTE();
 
-        typedef std::vector< ModuleName >  PendingModuleList;
+        typedef std::vector< CacheLine::Name >  PendingModuleList;
+
         /// find the modules having pending changes
-        bool hasPending(ModuleName const & _aModule);
+        bool hasPending(CacheLine::Name const & _aModule);
         /// find the modules having pending changes
         void findPendingModules( PendingModuleList & _rPendingList );
 
         /// add or merge the given subtreechange at the given location
         void addPending(backend::ConstUpdateInstance const & _anUpdate) CFG_UNO_THROW_RTE(  );
         /// remove and return pending changes for the given component
-        std::auto_ptr<SubtreeChange> releasePending(ModuleName const & _aModule) CFG_UNO_THROW_RTE(  );
+        std::auto_ptr<SubtreeChange> releasePending(CacheLine::Name const & _aModule) CFG_UNO_THROW_RTE(  );
+
+        // create a new module with the given name
+        void createModule(CacheLine::Name const & _aModule) CFG_UNO_THROW_RTE();
     private:
-        virtual ModuleRef doCreateAttachedModule(memory::HeapManager & _rHeapManager, const memory::SegmentAddress & _aLocation, const ModuleName& _aName) CFG_UNO_THROW_RTE(  );
+        virtual CacheLineRef doCreateAttachedModule(data::TreeAddress _aLocation, const CacheLine::Name& _aName) CFG_UNO_THROW_RTE(  );
 
-        ExtendedCacheLineRef implNewCacheLine(ModuleName const & _aModule) CFG_UNO_THROW_RTE(  );
-
-        ExtendedCacheLineRef implExtended(ModuleRef const & _aSimpleRef) const;
+        ExtendedCacheLineRef implExtended(CacheLineRef const & _aSimpleRef) const;
     };
 //-----------------------------------------------------------------------------
 
