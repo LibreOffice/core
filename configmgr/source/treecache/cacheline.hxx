eeeca63be04fba0ac524dc2ed1f2e148de3e9699
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 13:36:53 +0000

    INTEGRATION: CWS configrefactor01 (1.5.84); FILE MERGED 2007/01/16 12:18:24 mmeeks 1.5.84.5: Submitted by: mmeeks Kill 'memory::Segment' - no longer needed. Bin some other (empty / redundant) headers. 2007/01/12 11:48:33 mmeeks 1.5.84.4: Remove a load of unneeded Accessor fluff Fix SimpleReferenceObject - it has to have 1 virtual method. 2007/01/11 20:16:04 mmeeks 1.5.84.3: Submitted by: mmeeks More re-factoring, lots of locking rationalized, drastically reduced the mutex count, also removed ~300k interlocked increments with a non-interlocking SimpleReferencedObject base 2007/01/11 10:35:36 mmeeks 1.5.84.2: Submitted by: mmeeks
    Large scale re-factoring, remove fine-grained locking in favor of a simple,
    single global lock (still in progress).
    Identify and fix various hot-spots.
    Remove otherwise empty / non-compiled files.
    Kill UpdateAccessor
    2007/01/08 20:49:02 mmeeks 1.5.84.1: Issue number:
    Submitted by: mmeeks
    Substantial configmgr re-factoring #1 ...
    	+ remove endless typedef chains
    	+ remove custom allocator & associated complexity
    	+ remove Pointer, and 'Address' classes

--- configmgr/source/treecache/cacheline.hxx
+++ configmgr/source/treecache/cacheline.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cacheline.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:23:30 $
+ *  last change: $Author: ihi $ $Date: 2007-11-23 14:36:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,12 +36,6 @@
 #ifndef CONFIGMGR_CACHELINE_HXX
 #define CONFIGMGR_CACHELINE_HXX
 
-#ifndef CONFIGMGR_TREEADDRESS_HXX
-#include "treeaddress.hxx"
-#endif
-#ifndef CONFIGMGR_SEGMENT_HXX
-#include "segment.hxx"
-#endif
 #ifndef CONFIGMGR_CONFIGPATH_HXX_
 #include "configpath.hxx"
 #endif
@@ -51,12 +45,12 @@
 #ifndef CONFIGMGR_UTILITY_HXX_
 #include "utility.hxx"
 #endif
+#ifndef INCLUDED_SHARABLE_TREEFRAGMENT_HXX
+#include "treefragment.hxx"
+#endif
 
 #ifndef _OSL_INTERLOCK_H_
 #include <osl/interlck.h>
-#endif
-#ifndef _SALHELPER_SIMPLEREFERENCEOBJECT_HXX_
-#include <salhelper/simplereferenceobject.hxx>
 #endif
 #ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
@@ -67,76 +61,65 @@ namespace configmgr
 ////////////////////////////////////////////////////////////////////////////////
     using ::rtl::OUString;
 
-    namespace data { class NodeAddress; class TreeAddress; class NodeAccess; }
+    namespace data {
+        /* class NodeAddress; */ class NodeAccess;
+    }
 ////////////////////////////////////////////////////////////////////////////////
     /** This object represents a cache line for a single configuration tree
     */
-    class CacheLine : public salhelper::SimpleReferenceObject, Noncopyable
+    class CacheLine : public configmgr::SimpleReferenceObject, Noncopyable
     {
     public:
-        typedef oslInterlockedCount RefCount;
-
         typedef configuration::Name         Name;
         typedef configuration::AbsolutePath Path;
 
         // create a new CacheLine for the given component name
         static rtl::Reference<CacheLine>
-            createNew( Name const & _aModuleName, memory::HeapManager & _rHeapImpl );
+            createNew( Name const & _aModuleName );
 
         // create a new CacheLine attached to the given memory location
         static rtl::Reference<CacheLine>
-            createAttached( Name const & _aModuleName, memory::HeapManager & _rHeapImpl,
-                            memory::SegmentAddress const & _aLocation );
+            createAttached( Name const & _aModuleName, data::TreeAddress _aLocation );
 
-        memory::Segment * getDataSegment();
+        bool hasDefaults() const;
 
-        memory::SegmentAddress getDataSegmentAddress() const;
+        bool isEmpty() const { return m_base == NULL; }
+        data::TreeAddress   getTreeAddress() const { return m_base; }
+        data::TreeAddress   getPartialTree(Path const & _aTemplatePath ) const;
+        data::NodeAddress   getNode(Path const & _aPath) const;
 
-        bool isEmpty() const { return m_base.isNull(); }
-
-        bool hasDefaults(memory::Accessor const & _aAccessor) const;
-
-        data::TreeAddress getModuleRootAddress( ) const { return m_base; }
-
-        data::TreeAddress   getPartialTree(memory::Accessor const & _aAccessor, Path const & _aTemplatePath ) const;
-        data::NodeAddress   getNode(memory::Accessor const & _aAccessor, Path const & _aPath) const;
-
-        data::TreeAddress   setComponentData( memory::UpdateAccessor& _aAccessToken,
-                                              backend::ComponentData const & _aComponentInstance,
+        data::TreeAddress   setComponentData( backend::ComponentData const & _aComponentInstance,
                                               bool _bWithDefaults
                                            ) CFG_UNO_THROW_RTE();
 
-        data::TreeAddress   insertDefaults(   memory::UpdateAccessor& _aAccessToken,
-                                              backend::NodeInstance const & _aDefaultInstance
+        data::TreeAddress   insertDefaults(   backend::NodeInstance const & _aDefaultInstance
                                            ) CFG_UNO_THROW_RTE();
 
         // get the module name for this component
         Name getModuleName() const;
 
         /// add a client for this module's data
-        RefCount clientReferences() const { return m_nDataRefs; }
+        oslInterlockedCount clientReferences() const { return m_nDataRefs; }
         /// add a client for this modules data
-        RefCount clientAcquire() { return osl_incrementInterlockedCount(&m_nDataRefs); }
+        oslInterlockedCount clientAcquire() { return osl_incrementInterlockedCount(&m_nDataRefs); }
         /// subtract a client for this modules data
-        RefCount clientRelease() { return osl_decrementInterlockedCount(&m_nDataRefs); }
+        oslInterlockedCount clientRelease() { return osl_decrementInterlockedCount(&m_nDataRefs); }
 
     protected:
         // create a new CacheLine attached to the given memory location
         explicit
-        CacheLine( Name const & _aModuleName, memory::HeapManager & _rHeapImpl,
-                    memory::SegmentAddress const & _aLocation );
+        CacheLine( Name const & _aModuleName, data::TreeAddress _pLocation );
 
         // create a new empty CacheLine for the given component name
         explicit
-        CacheLine( Name const & _aModuleName, memory::HeapManager & _rHeapImpl );
+        CacheLine( Name const & _aModuleName );
 
-        data::NodeAccess internalGetNode(memory::Accessor const & _anAccessor, Path const & _rPath) const;
+        data::NodeAccess internalGetNode(Path const & _rPath) const;
 
         data::TreeAddress base() const { return m_base; }
         void setBase(data::TreeAddress _base);
 
     private:
-        memory::Segment     m_storage;
         data::TreeAddress   m_base;
         Name                m_name;
 
@@ -152,12 +135,12 @@ namespace configmgr
     public:
         // create a new CacheLine for the given component name
         static rtl::Reference<ExtendedCacheLine>
-            createNew( Name const & _aModuleName, memory::HeapManager & _rHeapImpl );
+            createNew( Name const & _aModuleName );
 
         // create a new CacheLine attached to the given memory location
         static rtl::Reference<ExtendedCacheLine>
-            createAttached( Name const & _aModuleName, memory::HeapManager & _rHeapImpl,
-                            memory::SegmentAddress const & _aLocation );
+            createAttached( Name const & _aModuleName,
+                            data::TreeAddress _aLocation );
 
     // management of pending changes
         bool hasPending() const {return m_pPending.get() != NULL;}
@@ -168,24 +151,17 @@ namespace configmgr
     private:
         // create a new empty CacheLine for the given component name
         explicit
-        ExtendedCacheLine( Name const & _aModuleName, memory::HeapManager & _rHeapImpl );
+        ExtendedCacheLine( Name const & _aModuleName );
 
         // create a new CacheLine attached to the given memory location
         explicit
-        ExtendedCacheLine( Name const & _aModuleName, memory::HeapManager & _rHeapImpl,
-                            memory::SegmentAddress const & _aLocation );
+        ExtendedCacheLine( Name const & _aModuleName,
+                           data::TreeAddress _aLocation );
 
     private:
         std::auto_ptr<SubtreeChange> m_pPending;
     };
     typedef rtl::Reference<ExtendedCacheLine> ExtendedCacheLineRef;
-////////////////////////////////////////////////////////////////////////////////
-
-    inline
-    memory::Segment * CacheLine::getDataSegment()
-    { return &m_storage; }
-
-////////////////////////////////////////////////////////////////////////////////
 
 } // namespace configmgr
 
