f61847b67b398292da3a229dc3af7619607c8d0c
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 15:25:37 +0000

    INTEGRATION: CWS langstatusbar (1.8.84); FILE MERGED 2007/10/12 08:45:02 tl 1.8.84.1: #i77208# new language status-abr control

--- sw/source/ui/lingu/makefile.mk
+++ sw/source/ui/lingu/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2007-04-25 09:16:44 $
+#   last change: $Author: ihi $ $Date: 2007-11-23 16:25:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,6 +44,9 @@ TARGET=lingu
 .INCLUDE :  $(PRJ)$/inc$/sw.mk
 
 # --- Files --------------------------------------------------------
+
+EXCEPTIONSFILES= \
+        $(SLO)$/olmenu.obj \
 
 SLOFILES =  \
         $(SLO)$/hyp.obj \
