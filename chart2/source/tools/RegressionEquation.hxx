6f2db986171a19e561079beb74f15987186ae289
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 11:08:37 +0000

    INTEGRATION: CWS chart17 (1.1.2); FILE ADDED 2007/10/25 12:54:52 bm 1.1.2.2: #10000# using ::cppu::OPropertySetHelper::disposing; needed for Solaris compiler 2007/10/12 12:32:44 bm 1.1.2.1: #i7998# equations for regression curves

---
+++ chart2/source/tools/RegressionEquation.hxx
@@ -0,0 +1,151 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: RegressionEquation.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2007-11-23 12:08:37 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef CHART2_REGRESSIONEQUATION_HXX
+#define CHART2_REGRESSIONEQUATION_HXX
+
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/util/XCloneable.hpp>
+#include <com/sun/star/util/XModifyBroadcaster.hpp>
+#include <com/sun/star/util/XModifyListener.hpp>
+#include <com/sun/star/chart2/XTitle.hpp>
+
+#include "MutexContainer.hxx"
+#include "OPropertySet.hxx"
+#include "ServiceMacros.hxx"
+#include "ModifyListenerHelper.hxx"
+
+#include <cppuhelper/implbase4.hxx>
+#include <comphelper/uno3.hxx>
+
+namespace chart
+{
+
+namespace impl
+{
+typedef ::cppu::WeakImplHelper4<
+        ::com::sun::star::util::XCloneable,
+        ::com::sun::star::util::XModifyBroadcaster,
+        ::com::sun::star::util::XModifyListener,
+        ::com::sun::star::chart2::XTitle >
+    RegressionEquation_Base;
+}
+
+class RegressionEquation :
+        public MutexContainer,
+        public impl::RegressionEquation_Base,
+        public ::property::OPropertySet
+{
+public:
+    explicit RegressionEquation(
+        const ::com::sun::star::uno::Reference<
+            ::com::sun::star::uno::XComponentContext > & xContext );
+    virtual ~RegressionEquation();
+
+    /// XServiceInfo declarations
+    APPHELPER_XSERVICEINFO_DECL()
+    /// merge XInterface implementations
+     DECLARE_XINTERFACE()
+    /// establish methods for factory instatiation
+    APPHELPER_SERVICE_FACTORY_HELPER( RegressionEquation )
+
+protected:
+    explicit RegressionEquation( const RegressionEquation & rOther );
+
+    // ____ OPropertySet ____
+    virtual ::com::sun::star::uno::Any GetDefaultValue( sal_Int32 nHandle ) const
+        throw(::com::sun::star::beans::UnknownPropertyException);
+
+    virtual ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelper();
+
+    // ____ XPropertySet ____
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL
+        getPropertySetInfo()
+        throw (::com::sun::star::uno::RuntimeException);
+
+
+    // ____ XCloneable ____
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::util::XCloneable > SAL_CALL createClone()
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XModifyBroadcaster ____
+    virtual void SAL_CALL addModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeModifyListener(
+        const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XModifyListener ____
+    virtual void SAL_CALL modified(
+        const ::com::sun::star::lang::EventObject& aEvent )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XEventListener (base of XModifyListener) ____
+    virtual void SAL_CALL disposing(
+        const ::com::sun::star::lang::EventObject& Source )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // ____ XTitle ____
+    virtual ::com::sun::star::uno::Sequence<
+        ::com::sun::star::uno::Reference<
+        ::com::sun::star::chart2::XFormattedString > > SAL_CALL getText()
+        throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setText( const ::com::sun::star::uno::Sequence<
+                                   ::com::sun::star::uno::Reference<
+                                   ::com::sun::star::chart2::XFormattedString > >& Strings )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    using ::cppu::OPropertySetHelper::disposing;
+
+    // ____ OPropertySet ____
+    virtual void firePropertyChangeEvent();
+
+    void fireModifyEvent();
+
+private:
+    ::com::sun::star::uno::Sequence<
+        ::com::sun::star::uno::Reference<
+            ::com::sun::star::chart2::XFormattedString > > m_aStrings;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener > m_xModifyEventForwarder;
+    ::com::sun::star::uno::Reference<
+        ::com::sun::star::uno::XComponentContext >
+                        m_xContext;
+};
+
+} //  namespace chart
+
+// CHART2_REGRESSIONEQUATION_HXX
+#endif
