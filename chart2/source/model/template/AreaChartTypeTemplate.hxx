cf7f69c9e5df526fb1629cc860c62b3cdc1b6e26
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 10:59:44 +0000

    INTEGRATION: CWS chart17 (1.6.66); FILE MERGED 2007/11/05 14:02:15 iha 1.6.66.1: #i63857#, #i4039# more flexible placement of data point labels, best fit for pie labels

--- chart2/source/model/template/AreaChartTypeTemplate.hxx
+++ chart2/source/model/template/AreaChartTypeTemplate.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AreaChartTypeTemplate.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 18:44:25 $
+ *  last change: $Author: ihi $ $Date: 2007-11-23 11:59:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,6 +98,8 @@ protected:
         throw (::com::sun::star::uno::RuntimeException);
 
     // ____ ChartTypeTemplate ____
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XChartType >
+                getChartTypeForIndex( sal_Int32 nChartTypeIndex );
     virtual sal_Int32 getDimension() const;
     virtual StackMode getStackMode( sal_Int32 nChartTypeIndex ) const;
 
