094ce29ac136b1d7f04c1e3d347a39cf1bd8e333
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 13:12:30 +0000

    INTEGRATION: CWS configrefactor01 (1.4.84); FILE MERGED 2007/01/11 10:35:28 mmeeks 1.4.84.1: Submitted by: mmeeks
    Large scale re-factoring, remove fine-grained locking in favor of a simple,
    single global lock (still in progress).
    Identify and fix various hot-spots.
    Remove otherwise empty / non-compiled files.
    Kill UpdateAccessor

--- configmgr/source/data/makefile.mk
+++ configmgr/source/data/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 03:39:27 $
+#   last change: $Author: ihi $ $Date: 2007-11-23 14:12:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,14 +48,8 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Files -------------------------------------
 
 SLOFILES=	\
-        $(SLO)$/simpleheap.obj	\
-        $(SLO)$/heap.obj	\
-        $(SLO)$/segmentheap.obj	\
         $(SLO)$/anydata.obj	\
-        $(SLO)$/sequence.obj	\
-        $(SLO)$/types.obj	\
-        $(SLO)$/accessor.obj		\
-        $(SLO)$/updateaccessor.obj		\
+        $(SLO)$/sequence.obj
 
 # --- Targets ----------------------------------
 
