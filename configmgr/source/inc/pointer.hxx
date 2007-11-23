8f9aa0a5f8f659c4d22c58df1fee9bc0f1ecdcd4
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-23 13:22:47 +0000

    INTEGRATION: CWS configrefactor01 (1.5.68); FILE MERGED 2007/01/08 20:48:56 mmeeks 1.5.68.1: Issue number: Submitted by: mmeeks Substantial configmgr re-factoring #1 ... 	+ remove endless typedef chains 	+ remove custom allocator & associated complexity 	+ remove Pointer, and 'Address' classes

--- configmgr/source/inc/pointer.hxx
+++ configmgr/source/inc/pointer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pointer.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2005-12-28 17:30:54 $
+ *  last change: $Author: ihi $ $Date: 2007-11-23 14:22:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,59 +32,3 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#ifndef CONFIGMGR_POINTER_HXX
-#define CONFIGMGR_POINTER_HXX
-
-#ifndef CONFIGMGR_MEMORYMODEL_HXX
-#include "memorymodel.hxx"
-#endif
-
-namespace configmgr
-{
-// -----------------------------------------------------------------------------
-    namespace memory
-    {
-    // -------------------------------------------------------------------------
-        class Accessor;
-        class UpdateAccessor;
-    // -------------------------------------------------------------------------
-        /// class mediating read-only access to a memory::Segment
-        class Pointer
-        {
-            friend class Accessor;
-            friend class UpdateAccessor;
-
-            typedef memory::Address AddressType;
-
-            AddressType m_value;
-
-            struct Opaque_;
-        public:
-            typedef AddressType RawAddress;
-
-            Pointer() : m_value(0) {}
-            explicit Pointer(AddressType p) : m_value(p) {}
-
-            RawAddress value() const { return m_value; }
-
-            bool isNull()   const { return m_value == 0; }
-            bool is()       const { return m_value != 0; }
-
-            operator Opaque_ const * () const { return reinterpret_cast<Opaque_ const *>(m_value); }
-
-            friend bool operator == (Pointer lhs, Pointer rhs)
-            { return lhs.value() == rhs.value(); }
-
-            friend bool operator != (Pointer lhs, Pointer rhs)
-            { return lhs.value() != rhs.value(); }
-        };
-
-    // -------------------------------------------------------------------------
-    }
-// -----------------------------------------------------------------------------
-} // namespace configmgr
-// -----------------------------------------------------------------------------
-
-#endif // CONFIGMGR_POINTER_HXX
-
