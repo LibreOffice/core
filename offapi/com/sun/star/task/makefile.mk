52b5c28c0fee6a2b208155d64efc724b96f8bce2
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-26 15:34:29 +0000

    INTEGRATION: CWS tkr06 (1.20.212); FILE MERGED 2007/11/05 09:19:11 mav 1.20.212.1: #i82892# allow better control over password container

--- offapi/com/sun/star/task/makefile.mk
+++ offapi/com/sun/star/task/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.21 $
+#   $Revision: 1.22 $
 #
-#   last change: $Author: ihi $ $Date: 2007-11-21 15:18:25 $
+#   last change: $Author: ihi $ $Date: 2007-11-26 16:34:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -71,6 +71,7 @@ IDLFILES=\
     XJob.idl\
     XJobExecutor.idl\
     XJobListener.idl\
+    XMasterPasswordHandling.idl\
     XPasswordContainer.idl\
     XStatusIndicator.idl\
     XStatusIndicatorFactory.idl\
