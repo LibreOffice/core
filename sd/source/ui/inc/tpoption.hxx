89d8701c09dc1d983c15f23e7ca64453a16a94e7
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-26 13:36:41 +0000

    INTEGRATION: CWS aw053 (1.10.164); FILE MERGED 2007/09/14 10:58:23 aw 1.10.164.1: #i80528# Removal of draft paint modes

--- sd/source/ui/inc/tpoption.hxx
+++ sd/source/ui/inc/tpoption.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tpoption.hxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 17:49:34 $
+ *  last change: $Author: ihi $ $Date: 2007-11-26 14:36:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,12 +88,6 @@ public:
 class SdTpOptionsContents : public SfxTabPage
 {
 private:
-    FixedLine aGrpViewSubstitute;
-    CheckBox aCbxExternGraphic;
-    CheckBox aCbxOutlineMode;
-    CheckBox aCbxNoText;
-    CheckBox aCbxHairlineMode;
-
     FixedLine    aGrpDisplay;
     CheckBox    aCbxRuler;
     CheckBox    aCbxDragStripes;
