253856c195463a91a2f00fb989da0413e6762c98
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-26 14:12:37 +0000

    INTEGRATION: CWS vcl83 (1.3.90); FILE MERGED 2007/11/12 16:28:18 hdu 1.3.90.3: RESYNC: (1.3-1.4); FILE MERGED 2007/10/17 14:00:13 pl 1.3.90.2: fix a conversion warning 2007/09/28 14:48:53 hdu 1.3.90.1: #i82069# PDF export: implement symbol aliasing for glyph widths

--- vcl/source/gdi/pdffontcache.cxx
+++ vcl/source/gdi/pdffontcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pdffontcache.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2007-11-01 11:01:51 $
+ *  last change: $Author: ihi $ $Date: 2007-11-26 15:12:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
