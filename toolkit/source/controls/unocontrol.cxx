19fc0d7f261b067dc4bd8589726aaa2641ffdfc3
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-26 15:27:38 +0000

    INTEGRATION: CWS tkr06 (1.48.46); FILE MERGED 2007/11/08 10:18:58 cd 1.48.46.2: #i83238# Use PrepareWindowDescriptor to set dependent properties before the properties change loop copy the data to the peer 2007/11/08 08:56:26 cd 1.48.46.1: #i83238# New virtual method to set dependent properties before the properties change loop copy the data to the peer

--- toolkit/source/controls/unocontrol.cxx
+++ toolkit/source/controls/unocontrol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unocontrol.cxx,v $
  *
- *  $Revision: 1.50 $
+ *  $Revision: 1.51 $
  *
- *  last change: $Author: ihi $ $Date: 2007-11-22 11:34:16 $
+ *  last change: $Author: ihi $ $Date: 2007-11-26 16:27:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
