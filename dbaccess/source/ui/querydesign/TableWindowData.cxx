db294ef8e0c396315e5baa30c13c877a708ca642
Ivo Hinkelmann <ihi@openoffice.org>
2007-11-29 13:27:29 +0000

    #i10000# build fix

--- dbaccess/source/ui/querydesign/TableWindowData.cxx
+++ dbaccess/source/ui/querydesign/TableWindowData.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TableWindowData.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2007-11-28 16:27:00 $
+ *  last change: $Author: ihi $ $Date: 2007-11-29 14:27:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ using namespace ::com::sun::star::container;
 //==================================================================
 // class OTableWindowData
 //==================================================================
-//DBG_NAME(OTableWindowData);
+DBG_NAME(OTableWindowData)
 //------------------------------------------------------------------------------
 OTableWindowData::OTableWindowData( const Reference< XPropertySet>& _xTable
                                    ,const ::rtl::OUString& _rComposedName
