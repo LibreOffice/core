b57353c665a91ce11f03bbb0a6d53432d1eb2c79
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 10:20:07 +0000

    INTEGRATION: CWS npower8 (1.1.2); FILE ADDED 2007/11/02 17:10:40 rene 1.1.2.6: $(shell ls -> $(shell @ls to suppress "Executing shell macro" message of dmake 2007/07/10 09:36:04 npower 1.1.2.5: use temp file for command on regmerge to avoid command line lenght issues 2007/07/09 11:14:53 npower 1.1.2.4: bah duplicating stuff in target.mk but... I can't get it to work without that 2007/07/09 10:40:42 npower 1.1.2.3: cleanup makefile, make it work as standard IDLFILES target 2007/07/09 08:30:39 npower 1.1.2.2: temporary hack to get the urd's to build 2007/07/05 11:04:07 npower 1.1.2.1: constants builds the generated constants from the generated idl provided by oovbaapi/genconstidl

---
+++ oovbaapi/org/openoffice/constants/makefile.mk
@@ -0,0 +1,71 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: vg $ $Date: 2007-12-07 11:20:07 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+PRJ=..$/..$/..
+
+PRJNAME=oovbapi
+
+TARGET=constants
+PACKAGE=org$/openoffice$/constants
+
+# --- Settings -----------------------------------------------------
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+.IF "$(ENABLE_VBA)"!="YES"
+dummy:
+        @echo "not building vba..."
+.ENDIF
+
+# ------------------------------------------------------------------------
+
+# I tried to use the IDLFILES target but it failed dismally
+MY_GEN_IDL_PATH=$(MISC)$/idl
+MY_GEN_UCR_PATH=$(OUT)$/ucr$/$(PACKAGE)
+MYTMPIDLFILES=$(shell @ls $(MY_GEN_IDL_PATH))
+#IDLFILES=$(foreach,i,$(MYTMPIDLFILES) $(MY_GEN_IDL_PATH)$/$(i))
+MYIDLFILES=$(foreach,i,$(MYTMPIDLFILES) $(MY_GEN_IDL_PATH)$/$(i))
+MYURDFILES=$(foreach,i,$(MYIDLFILES) $(MY_GEN_UCR_PATH)$/$(i:b).urd)
+MYDBTARGET=$(OUT)$/ucr/constants.db
+
+ALLTAR: $(MYDBTARGET)
+
+$(MY_GEN_UCR_PATH)$/%.urd: $(MY_GEN_IDL_PATH)$/%.idl
+    $(IDLC) -C -O $(MY_GEN_UCR_PATH) -I $(MY_GEN_IDL_PATH)$(PATH_SEPERATOR)$(SOLARIDLDIR) $?
+
+$(MYDBTARGET) : $(MYURDFILES)  $(MYIDLFILES)
+    $(REGMERGE) $(OUT)$/ucr/constants.db /UCR @$(mktmp $(MYURDFILES))
+
+# ------------------------------------------------------------------
+
+.INCLUDE :  target.mk
