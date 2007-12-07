a4dbc8b46a6611637143223fa4a0c999cd984648
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 10:06:05 +0000

    INTEGRATION: CWS npower8 (1.2.4); FILE MERGED 2007/10/17 17:52:47 npower 1.2.4.5: #i77189# 2007/10/16 09:36:59 npower 1.2.4.4: #i77189# 2007/09/03 12:19:42 npower 1.2.4.3: #i77189# sync with oobuild 2007/07/18 13:46:48 npower 1.2.4.2: #i77189# sync ooo-build and this module 2007/05/10 11:23:11 npower 1.2.4.1: -m#i77189#

--- sc/source/ui/vba/vbaworkbook.cxx
+++ sc/source/ui/vba/vbaworkbook.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vbaworkbook.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-25 16:12:49 $
+ *  last change: $Author: vg $ $Date: 2007-12-07 11:06:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+#include "helperdecl.hxx"
 #include <tools/urlobj.hxx>
+#include <comphelper/unwrapargs.hxx>
 
 #include <com/sun/star/util/XModifiable.hpp>
 #include <com/sun/star/util/XProtectable.hpp>
@@ -41,14 +43,20 @@
 #include <com/sun/star/frame/XStorable.hpp>
 #include <com/sun/star/frame/XFrame.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
+#include <org/openoffice/excel/XlFileFormat.hpp>
 
+#include "scextopt.hxx"
 #include "vbaworksheet.hxx"
 #include "vbaworksheets.hxx"
 #include "vbaworkbook.hxx"
 #include "vbawindows.hxx"
+#include "vbastyles.hxx"
 #include "vbahelper.hxx"
-
+#include "vbapalette.hxx"
+#include <osl/file.hxx>
 #include <stdio.h>
+#include "vbanames.hxx"  // Amelia Wang
+#include "nameuno.hxx"
 
 // Much of the impl. for the equivalend UNO module is
 // sc/source/ui/unoobj/docuno.cxx, viewuno.cxx
@@ -77,10 +85,140 @@ protected:
         return xSheet;
     }
 public:
-    ActiveSheet( uno::Reference< uno::XComponentContext >& xContext ) : ScVbaWorksheet( xContext ) {}
+    ActiveSheet( const uno::Reference< vba::XHelperInterface >& xParent, const uno::Reference< uno::XComponentContext >& xContext ) : ScVbaWorksheet( xParent, xContext ) {}
 
 };
 
+uno::Sequence< sal_Int32 > ScVbaWorkbook::ColorData;
+
+void ScVbaWorkbook::initColorData( const uno::Sequence< sal_Int32 >& sColors )
+{
+        const sal_Int32* pSource = sColors.getConstArray();
+        sal_Int32* pDest = ColorData.getArray();
+        const sal_Int32* pEnd = pSource + sColors.getLength();
+        for ( ; pSource != pEnd; ++pSource, ++pDest )
+            *pDest = *pSource;
+}
+
+
+void SAL_CALL
+ScVbaWorkbook::ResetColors(  ) throw (::script::BasicErrorException, ::uno::RuntimeException)
+{
+        uno::Reference< container::XIndexAccess > xIndexAccess( ScVbaPalette::getDefaultPalette(), uno::UNO_QUERY_THROW );
+        sal_Int32 nLen = xIndexAccess->getCount();
+        ColorData.realloc( nLen );
+
+        uno::Sequence< sal_Int32 > dDefaultColors( nLen );
+        sal_Int32* pDest = dDefaultColors.getArray();
+        for ( sal_Int32 index=0; index < nLen; ++pDest, ++index )
+            xIndexAccess->getByIndex( index )  >>= (*pDest);
+        initColorData( dDefaultColors );
+}
+
+::uno::Any SAL_CALL
+ScVbaWorkbook::Colors( const ::uno::Any& Index ) throw (::script::BasicErrorException, ::uno::RuntimeException)
+{
+    uno::Any aRet;
+    if ( Index.getValue() )
+    {
+        sal_Int32 nIndex = 0;
+        Index >>= nIndex;
+        aRet = uno::makeAny( XLRGBToOORGB( ColorData[ --nIndex ] ) );
+    }
+    else
+        aRet = uno::makeAny( ColorData );
+    return aRet;
+}
+
+::sal_Int32 SAL_CALL
+ScVbaWorkbook::FileFormat(  ) throw (::script::BasicErrorException, ::uno::RuntimeException)
+{
+        sal_Int32 aFileFormat = 0;
+        rtl::OUString aFilterName;
+        uno::Sequence< beans::PropertyValue > aArgs = getModel()->getArgs();
+
+        // #FIXME - seems suspect should we not walk through the properties
+        // to find the FilterName
+        if (aArgs[0].Name.equalsAscii( "FilterName")) {
+            aArgs[0].Value >>= aFilterName;
+        } else {
+           aArgs[1].Value >>= aFilterName;
+        }
+
+        if (aFilterName.equalsAscii("Text - txt - csv (StarCalc)")) {
+            aFileFormat = excel::XlFileFormat::xlCSV; //xlFileFormat.
+        }
+
+        if (aFilterName.equalsAscii("DBF")) {
+            aFileFormat = excel::XlFileFormat::xlDBF4;
+        }
+
+        if (aFilterName.equalsAscii("DIF")) {
+            aFileFormat = excel::XlFileFormat::xlDIF;
+        }
+
+        if (aFilterName.equalsAscii("Lotus")) {
+            aFileFormat = excel::XlFileFormat::xlWK3;
+        }
+
+        if (aFilterName.equalsAscii("MS Excel 4.0")) {
+            aFileFormat = excel::XlFileFormat::xlExcel4Workbook;
+        }
+
+        if (aFilterName.equalsAscii("MS Excel 5.0/95")) {
+            aFileFormat = excel::XlFileFormat::xlExcel5;
+        }
+
+        if (aFilterName.equalsAscii("MS Excel 97")) {
+            aFileFormat = excel::XlFileFormat::xlExcel9795;
+        }
+
+        if (aFilterName.equalsAscii("HTML (StarCalc)")) {
+            aFileFormat = excel::XlFileFormat::xlHtml;
+        }
+
+        if (aFilterName.equalsAscii("calc_StarOffice_XML_Calc_Template")) {
+            aFileFormat = excel::XlFileFormat::xlTemplate;
+        }
+
+        if (aFilterName.equalsAscii("StarOffice XML (Calc)")) {
+            aFileFormat = excel::XlFileFormat::xlWorkbookNormal;
+        }
+        if (aFilterName.equalsAscii("calc8")) {
+            aFileFormat = excel::XlFileFormat::xlWorkbookNormal;
+        }
+
+        return aFileFormat;
+}
+
+void
+ScVbaWorkbook::init()
+{
+    if ( !ColorData.getLength() )
+        ResetColors();
+}
+ScVbaWorkbook::ScVbaWorkbook(   const css::uno::Reference< oo::vba::XHelperInterface >& xParent, const css::uno::Reference< css::uno::XComponentContext >& xContext) :ScVbaWorkbook_BASE( xParent, xContext ), mxModel(NULL)
+{
+    //#FIXME this persists the color data per office instance and
+    // not per workbook instance, need to hook the data into XModel
+    // ( e.g. we already store the imported palette in there )
+    // so we should,
+    // a) make the class that does that a service
+    // b) make that service implement XIndexContainer
+    init();
+}
+
+ScVbaWorkbook::ScVbaWorkbook(   const css::uno::Reference< oo::vba::XHelperInterface >& xParent, const css::uno::Reference< css::uno::XComponentContext >& xContext, css::uno::Reference< css::frame::XModel > xModel ) : ScVbaWorkbook_BASE( xParent, xContext ),  mxModel( xModel )
+{
+    init();
+}
+
+ScVbaWorkbook::ScVbaWorkbook( uno::Sequence< uno::Any> const & args,
+    uno::Reference< uno::XComponentContext> const & xContext ) : ScVbaWorkbook_BASE( getXSomethingFromArgs< vba::XHelperInterface >( args, 0 ), xContext ),  mxModel( getXSomethingFromArgs< frame::XModel >( args, 1 ) )
+
+{
+    init();
+}
 
 ::rtl::OUString
 ScVbaWorkbook::getName() throw (uno::RuntimeException)
@@ -90,7 +228,7 @@ ScVbaWorkbook::getName() throw (uno::RuntimeException)
     {
 
         INetURLObject aURL( getModel()->getURL() );
-        sName = aURL.GetLastName();
+        ::osl::File::getSystemPathFromFileURL( aURL.GetLastName(), sName );
     }
     else
     {
@@ -121,33 +259,37 @@ ScVbaWorkbook::getFullName() throw (uno::RuntimeException)
 uno::Reference< excel::XWorksheet >
 ScVbaWorkbook::getActiveSheet() throw (uno::RuntimeException)
 {
-    return new ActiveSheet( m_xContext );
+    return new ActiveSheet( this, mxContext );
 }
 uno::Any SAL_CALL
 ScVbaWorkbook::Sheets( const uno::Any& aIndex ) throw (uno::RuntimeException)
 {
     return Worksheets( aIndex );
 }
+
+
+
 uno::Any SAL_CALL
 ScVbaWorkbook::Worksheets( const uno::Any& aIndex ) throw (uno::RuntimeException)
 {
     uno::Reference< frame::XModel > xModel( getModel() );
     uno::Reference <sheet::XSpreadsheetDocument> xSpreadDoc( xModel, uno::UNO_QUERY_THROW );
-    uno::Reference<sheet::XSpreadsheets > xSheets( xSpreadDoc->getSheets(), uno::UNO_QUERY_THROW );
-    uno::Reference< vba::XCollection > xWorkSheets( uno::Reference< excel::XWorksheets >( new ScVbaWorksheets(m_xContext, xSheets, xModel ) ), uno::UNO_QUERY_THROW );
+    uno::Reference<container::XIndexAccess > xSheets( xSpreadDoc->getSheets(), uno::UNO_QUERY_THROW );
+    uno::Reference< vba::XCollection > xWorkSheets(  new ScVbaWorksheets( this, mxContext, xSheets, xModel ) );
     if (  aIndex.getValueTypeClass() == uno::TypeClass_VOID )
     {
         return uno::Any( xWorkSheets );
     }
-    return uno::Any( xWorkSheets->Item( aIndex ) );
+    // pass on to collection
+    return uno::Any( xWorkSheets->Item( aIndex, uno::Any() ) );
 }
 uno::Any SAL_CALL
 ScVbaWorkbook::Windows( const uno::Any& aIndex ) throw (uno::RuntimeException)
 {
-    uno::Reference< vba::XCollection >  xWindows = ScVbaWindows::Windows( m_xContext );
+    uno::Reference< vba::XCollection >  xWindows = ScVbaWindows::Windows( mxContext );
     if ( aIndex.getValueTypeClass() == uno::TypeClass_VOID )
         return uno::Any( xWindows );
-    return uno::Any( xWindows->Item( aIndex ) );
+    return uno::Any( xWindows->Item( aIndex, uno::Any() ) );
 }
 void
 ScVbaWorkbook::Close( const uno::Any &rSaveArg, const uno::Any &rFileArg,
@@ -259,9 +401,108 @@ ScVbaWorkbook::Save() throw (uno::RuntimeException)
 }
 
 void
+ScVbaWorkbook::SaveCopyAs( const rtl::OUString& sFileName ) throw ( uno::RuntimeException)
+{
+    rtl::OUString aURL;
+    osl::FileBase::getFileURLFromSystemPath( sFileName, aURL );
+    uno::Reference< frame::XStorable > xStor( getModel(), uno::UNO_QUERY_THROW );
+    uno::Sequence<  beans::PropertyValue > storeProps(1);
+    storeProps[0].Name = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "FilterName" ) );
+    storeProps[0].Value <<= rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "MS Excel 97" ) );
+    xStor->storeToURL( aURL, storeProps );
+}
+
+void
 ScVbaWorkbook::Activate() throw (uno::RuntimeException)
 {
     uno::Reference< frame::XFrame > xFrame( getModel()->getCurrentController()->getFrame(), uno::UNO_QUERY_THROW );
     xFrame->activate();
 }
 
+css::uno::Any SAL_CALL
+ScVbaWorkbook::Styles( const::uno::Any& Item ) throw (uno::RuntimeException)
+{
+    // quick look and Styles object doesn't seem to have a valid parent
+    // or a least the object browser just shows an object that has no
+    // variables ( therefore... leave as NULL for now )
+    uno::Reference< vba::XCollection > dStyles = new ScVbaStyles( uno::Reference< vba::XHelperInterface >(), mxContext, getModel() );
+    if ( Item.hasValue() )
+        return dStyles->Item( Item, uno::Any() );
+    return uno::makeAny( dStyles );
+}
+
+// Amelia Wang
+uno::Any SAL_CALL
+ScVbaWorkbook::Names( ) throw (uno::RuntimeException)
+{
+    uno::Reference< frame::XModel > xModel( getModel() );
+    ScDocShell * pDocShell = ( ScDocShell* )SfxObjectShell::GetWorkingDocument();
+    if ( !pDocShell )
+        throw uno::RuntimeException(::rtl::OUString(
+                                RTL_CONSTASCII_USTRINGPARAM( "Cann't recognise the 'Names' interface. ") ),
+                                uno::Reference< XInterface >() );
+    uno::Reference< beans::XPropertySet > xProps( xModel, uno::UNO_QUERY_THROW );
+    uno::Reference< sheet::XNamedRanges > xNamedRanges(  xProps->getPropertyValue( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("NamedRanges") ) ), uno::UNO_QUERY_THROW );
+    uno::Reference< vba::XCollection > xNames( new ScVbaNames( this , mxContext , xNamedRanges , xModel ));
+    return uno::Any( xNames );
+}
+
+rtl::OUString&
+ScVbaWorkbook::getServiceImplName()
+{
+    static rtl::OUString sImplName( RTL_CONSTASCII_USTRINGPARAM("ScVbaWorkbook") );
+    return sImplName;
+}
+
+uno::Sequence< rtl::OUString >
+ScVbaWorkbook::getServiceNames()
+{
+    static uno::Sequence< rtl::OUString > aServiceNames;
+    if ( aServiceNames.getLength() == 0 )
+    {
+        aServiceNames.realloc( 1 );
+        aServiceNames[ 0 ] = rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("org.openoffice.excel.Workbook" ) );
+    }
+    return aServiceNames;
+}
+
+::rtl::OUString SAL_CALL
+ScVbaWorkbook::getCodeName() throw (css::uno::RuntimeException)
+{
+#ifdef VBA_OOBUILD_HACK
+    uno::Reference< frame::XModel > xModel( getModel(), uno::UNO_QUERY_THROW );
+    ScDocument* pDoc = getDocShell( xModel )->GetDocument();
+    ScExtDocOptions* pExtOptions = pDoc->GetExtDocOptions();
+    ScExtDocSettings pExtSettings = pExtOptions->GetDocSettings();
+    ::rtl::OUString sGlobCodeName = pExtSettings.maGlobCodeName;
+    return sGlobCodeName;
+#else
+    throw uno::RuntimeException( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("Not implemented") ), uno::Reference< uno::XInterface >() );
+#endif
+}
+#ifdef VBA_OOBUILD_HACK
+void SAL_CALL
+ScVbaWorkbook::setCodeName( const ::rtl::OUString& sGlobCodeName ) throw (css::uno::RuntimeException)
+{
+    uno::Reference< frame::XModel > xModel( getModel(), uno::UNO_QUERY_THROW );
+    ScDocument* pDoc = getDocShell( xModel )->GetDocument();
+    ScExtDocOptions* pExtOptions = pDoc->GetExtDocOptions();
+    ScExtDocSettings pExtSettings = pExtOptions->GetDocSettings();
+    pExtSettings.maGlobCodeName = sGlobCodeName;
+#else
+void SAL_CALL
+ScVbaWorkbook::setCodeName( const ::rtl::OUString& ) throw (css::uno::RuntimeException)
+{
+    throw uno::RuntimeException( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("Not implemented") ), uno::Reference< uno::XInterface >() );
+#endif
+}
+
+namespace workbook
+{
+namespace sdecl = comphelper::service_decl;
+sdecl::vba_service_class_<ScVbaWorkbook, sdecl::with_args<true> > serviceImpl;
+extern sdecl::ServiceDecl const serviceDecl(
+    serviceImpl,
+    "ScVbaWorkbook",
+    "org.openoffice.excel.Workbook" );
+}
