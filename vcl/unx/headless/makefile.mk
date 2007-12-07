322092139d95f940b199f1e3eb14e4ca967a7e8d
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 10:51:03 +0000

    INTEGRATION: CWS macosxquicktime01 (1.3.82); FILE MERGED 2007/10/15 11:12:17 pl 1.3.82.1: #i82621# do nto build svp on MacOSX aqua

--- vcl/unx/headless/makefile.mk
+++ vcl/unx/headless/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-24 10:25:44 $
+#   last change: $Author: vg $ $Date: 2007-12-07 11:51:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ CDEFS+= -DWITH_SVP_LISTENING
 
 # --- Files --------------------------------------------------------
 
-.IF "$(GUI)" == "UNX"
+.IF "$(GUIBASE)" == "unx"
 
 SLOFILES=\
         $(SLO)$/svpinst.obj\
