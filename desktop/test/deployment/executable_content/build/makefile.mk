01c99b83c3010a322b636ea9eafcc538e9b8f01a
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 09:24:40 +0000

    INTEGRATION: CWS jl83 (1.1.2); FILE ADDED 2007/11/30 15:08:43 jl 1.1.2.4: #i84019# 2007/11/30 14:02:43 jl 1.1.2.3: #i84019# 2007/11/30 13:56:24 jl 1.1.2.2: #i84019# 2007/11/30 12:58:47 jl 1.1.2.1: #i84019# test executable which will be delivered by the hello.oxt extension

---
+++ desktop/test/deployment/executable_content/build/makefile.mk
@@ -0,0 +1,59 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: vg $ $Date: 2007-12-07 10:24:40 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..$/..
+
+PRJNAME = desktop
+TARGET = hello
+LIBTARGET=NO
+NO_DEFAULT_STL=TRUE
+LIBSALCPPRT=
+
+.INCLUDE : settings.mk
+
+
+APP1NOSAL = TRUE        
+APP1OBJS = \
+          $(OBJ)$/hello.obj
+
+
+APP1TARGET = $(TARGET)
+
+DEPOBJFILES = \
+        $(OBJ)$/hello.obj
+
+
+.INCLUDE : target.mk
+
