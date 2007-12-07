4561737a78237392029fe8312192340e6353751d
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 10:42:12 +0000

    INTEGRATION: CWS macosxquicktime01 (1.1.2); FILE ADDED 2007/11/02 14:06:10 msicotte 1.1.2.6: #i82621# cleanup framegrabber and player 2007/10/28 19:58:27 msicotte 1.1.2.5: #i82621# initial framegrabber implementation and remove unneeded Windos class stuff 2007/10/25 18:11:45 msicotte 1.1.2.4: #82234# implement getPreferredPlayerWindowSize and use it when the window is created - also hide the QTcontrollerbar 2007/10/25 16:17:50 msicotte 1.1.2.3: #i82234# code to make the player window visible and link it to the QTMovie 2007/10/25 03:59:27 msicotte 1.1.2.2: add part implementation to window.cxx 2007/10/05 03:13:48 msicotte 1.1.2.1: #i82234# inital commit of new files and changes for Mac Os X QuickTime integration

---
+++ avmedia/source/quicktime/player.cxx
@@ -0,0 +1,484 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: player.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2007-12-07 11:42:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <math.h>
+
+#include "player.hxx"
+#include "framegrabber.hxx"
+#include "window.hxx"
+
+// Quicktime 7+ in Mac OS X 10.4
+#define QT701 0x07010000
+
+// Quicktime 6.4+ in Mac OS X 10.3
+#define QT64 0x06400000
+
+using namespace ::com::sun::star;
+
+namespace avmedia { namespace quicktime {
+
+// ----------------
+// - Player -
+// ----------------
+
+Player::Player( const uno::Reference< lang::XMultiServiceFactory >& rxMgr ) :
+    mxMgr( rxMgr ),
+    mpMovieView( nil ),
+    mpMovie( nil ),
+    /* GST
+    mbFakeVideo (sal_False ),
+    */
+    mnUnmutedVolume( 0 ),
+    mnStopTime( DBL_MAX ),  //max double
+    mbMuted( false ),
+    mbLooping( false ),
+    mbInitialized( false ),
+    mnWindowID( 0 ),
+    mnDuration( 0 ),
+    mnWidth( 0 ),
+    mnHeight( 0 ),
+    mnVersion( 0 ),
+    maSizeCondition( osl_createCondition() )
+{
+    OSErr result;
+
+    NSApplicationLoad();
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    // check the version of QuickTime installed
+    result = Gestalt(gestaltQuickTime,&mnVersion);
+    if ((result == noErr) && (mnVersion >= QT701))
+    {
+      // we have version 7.01 or later, initialize
+      mpMovie = [QTMovie movie];
+      [mpMovie retain];
+      mbInitialized = true;
+    }
+    [pool release];
+}
+
+// ------------------------------------------------------------------------------
+
+Player::~Player()
+{
+    if( mbInitialized )
+    {
+        if( mpMovieView )
+        {
+            [mpMovieView setMovie:nil];
+            mpMovieView = nil;
+        }
+
+        if( mpMovie )
+        {
+            [mpMovie release];
+            mpMovie = nil;
+        }
+
+    }
+}
+
+
+// ------------------------------------------------------------------------------
+
+bool Player::create( const ::rtl::OUString& rURL )
+{
+    bool    bRet = false;
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    NSURL* aURL = [NSURL URLWithString:[[NSString alloc] initWithCharacters: rURL.getStr() length: rURL.getLength()] ];
+
+    // create the Movie
+
+    if( mbInitialized )
+    {
+
+        mpMovie = [mpMovie initWithURL:aURL error:nil];
+        if(mpMovie)
+        {
+            [mpMovie retain];
+            maURL = rURL;
+            bRet = true;
+        }
+    }
+
+    [pool release];
+
+    return bRet;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::start(  )
+    throw (uno::RuntimeException)
+{
+  OSL_TRACE ("Player::start");
+
+  if ( mbInitialized && mpMovie )
+    {
+      [mpMovie play];
+    }
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::stop(  )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::stop");
+    if ( mpMovie )
+    {
+      [mpMovie stop];
+    }
+
+}
+
+// ------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL Player::isPlaying()
+    throw (uno::RuntimeException)
+{
+    bool bRet = false;
+
+    if ( mbInitialized )
+    {
+        if ([mpMovie rate] != 0)
+        {
+            bRet = true;
+        }
+    }
+
+    return bRet;
+}
+
+// ------------------------------------------------------------------------------
+
+double SAL_CALL Player::getDuration(  )
+    throw (uno::RuntimeException)
+{
+    // slideshow checks for non-zero duration, so cheat here
+    double duration = 0.01;
+
+    if ( mpMovie ) // && mnDuration > 0 ) {
+    {
+        QTTime structDuration =  [mpMovie duration] ;
+        duration = (double)structDuration.timeValue / (double)structDuration.timeScale;
+    }
+
+    return duration;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setMediaTime( double fTime )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::setMediaTime");
+
+  if ( mpMovie )
+  {
+      [mpMovie setCurrentTime: QTMakeTimeWithTimeInterval(fTime)];
+  }
+}
+
+// ------------------------------------------------------------------------------
+
+double SAL_CALL Player::getMediaTime(  )
+    throw (uno::RuntimeException)
+{
+  double position = 0.0;
+
+  if ( mpMovie )
+  {
+      QTTime structDuration =  [mpMovie currentTime] ;
+      position = (double)structDuration.timeValue / (double)structDuration.timeScale;
+  }
+
+  if(isPlaying() && position>mnStopTime)
+  {
+      stop();
+  }
+
+
+  return position;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setStopTime( double fTime )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::setStopTime %f", fTime);
+
+    mnStopTime = fTime;
+
+}
+
+// ------------------------------------------------------------------------------
+
+double SAL_CALL Player::getStopTime(  )
+    throw (uno::RuntimeException)
+{
+    double fRet = 0.0;
+
+    fRet = mnStopTime;
+
+    return fRet;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setRate( double fRate )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::setRate");
+
+    // Quicktime: 0 = stop, 1 = normal speed, 2 = double speed, -1 = normal speed backwards
+    if ( mpMovie )
+    {
+        [mpMovie setRate: fRate];
+    }
+}
+
+// ------------------------------------------------------------------------------
+
+double SAL_CALL Player::getRate(  )
+    throw (uno::RuntimeException)
+{
+    // Quicktime: 0 = stop, 1 = normal speed, 2 = double speed, -1 = normal speed backwards
+    double rate = 1.0;
+
+    OSL_TRACE ("Player::getRate");
+
+    if ( mpMovie )
+    {
+        rate = (double) [mpMovie rate];
+    }
+
+    return rate;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setPlaybackLoop( sal_Bool bSet )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::setPlaybackLoop? %s", bSet?"True":"False" );
+
+    if(bSet)
+    {
+        [mpMovie setAttribute:[NSNumber numberWithBool:YES] forKey: QTMovieLoopsAttribute]  ;
+    }
+    else
+    {
+         [mpMovie setAttribute:[NSNumber numberWithBool:NO] forKey: QTMovieLoopsAttribute]  ;
+    }
+}
+
+// ------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL Player::isPlaybackLoop(  )
+    throw (uno::RuntimeException)
+{
+    bool bRet = [[mpMovie attributeForKey:QTMovieLoopsAttribute] boolValue];
+
+    OSL_TRACE ("Player::isPlaybackLoop ? %s", bRet?"True":"False" );
+
+    return bRet;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setMute( sal_Bool bSet )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE( "set mute: %d muted: %d unmuted volume: %lf", bSet, mbMuted, mnUnmutedVolume );
+
+    // change the volume to 0 or the unmuted volume
+    if(  mpMovie && mbMuted != bSet )
+    {
+        [mpMovie setMuted: bSet ];
+        mbMuted = bSet;
+    }
+
+}
+
+// ------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL Player::isMute(  )
+    throw (uno::RuntimeException)
+{
+    OSL_TRACE ("Player::isMuted");
+
+    return mbMuted;
+}
+
+// ------------------------------------------------------------------------------
+
+void SAL_CALL Player::setVolumeDB( sal_Int16 nVolumeDB )
+    throw (uno::RuntimeException)
+{
+    // OOo db volume -40 = QTVolume 0
+    // OOo db volume 0   = QTvolume 1
+    if(nVolumeDB==-40)
+    {
+        mnUnmutedVolume = 0;
+    }
+    else
+    {
+        mnUnmutedVolume = pow( 10.0, nVolumeDB / 20.0 );
+    }
+
+    OSL_TRACE( "set volume: %d gst volume: %f", nVolumeDB, mnUnmutedVolume );
+
+    // change volume
+    if( !mbMuted && mpMovie )
+    {
+        [mpMovie setVolume: mnUnmutedVolume ];
+    }
+}
+
+// ------------------------------------------------------------------------------
+
+sal_Int16 SAL_CALL Player::getVolumeDB(  )
+    throw (uno::RuntimeException)
+{
+    sal_Int16 nVolumeDB = 0.0;
+
+    if( mpMovie )
+      {
+          float volume = 0.0;
+
+          volume = [mpMovie volume];
+          if(volume>0)            //protect from log10(0)
+          {
+              nVolumeDB = (sal_Int16) ( 20.0*log10 ( volume ) );
+          }
+          else
+          {
+              nVolumeDB = -40 ;  // QT zero volume is no volume, -40db
+          }
+      }
+
+    return nVolumeDB;
+}
+
+// ------------------------------------------------------------------------------
+
+awt::Size SAL_CALL Player::getPreferredPlayerWindowSize(  )
+    throw (uno::RuntimeException)
+{
+    NSSize  nsSize = [[mpMovie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue];
+    awt::Size aSize( nsSize.width, nsSize.height );
+    return aSize;
+}
+
+
+// ------------------------------------------------------------------------------
+
+uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( const uno::Sequence< uno::Any >& aArguments )
+    throw (uno::RuntimeException)
+{
+    uno::Reference< ::media::XPlayerWindow >    xRet;
+    awt::Size                                   aSize( getPreferredPlayerWindowSize() );
+    NSSize                                      nsSize( NSMakeSize(aSize.Width, aSize.Height) );
+
+    OSL_TRACE( "Player::createPlayerWindow %d %d length: %d", aSize.Width, aSize.Height, aArguments.getLength() );
+
+    if( aSize.Width > 0 && aSize.Height > 0 )
+    {
+        ::avmedia::quicktime::Window* pWindow = new ::avmedia::quicktime::Window( mxMgr, *this );
+        xRet = pWindow;
+
+        sal_IntPtr nPtr; aArguments[0] >>= nPtr;
+        mpMovieView = reinterpret_cast< QTMovieView * >(nPtr);
+        [mpMovieView setMovie: mpMovie];
+        [mpMovieView setControllerVisible: NO];
+        [mpMovieView setFrameSize: nsSize ];
+    }
+
+    return xRet;
+}
+
+// ------------------------------------------------------------------------------
+
+uno::Reference< media::XFrameGrabber > SAL_CALL Player::createFrameGrabber(  )
+    throw (::com::sun::star::uno::RuntimeException)
+{
+  uno::Reference< media::XFrameGrabber > xRet;
+  OSL_TRACE ("Player::createFrameGrabber");
+
+  if( maURL.getLength() > 0 )
+  {
+      FrameGrabber* pGrabber = new FrameGrabber( mxMgr );
+
+      xRet = pGrabber;
+
+      if( !pGrabber->create( maURL ) )
+      {
+          xRet.clear();
+      }
+  }
+
+  return xRet;
+}
+
+// ------------------------------------------------------------------------------
+
+::rtl::OUString SAL_CALL Player::getImplementationName(  )
+    throw (uno::RuntimeException)
+{
+    return ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( AVMEDIA_QUICKTIME_PLAYER_IMPLEMENTATIONNAME ) );
+}
+
+// ------------------------------------------------------------------------------
+
+sal_Bool SAL_CALL Player::supportsService( const ::rtl::OUString& ServiceName )
+    throw (uno::RuntimeException)
+{
+    return ServiceName.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM ( AVMEDIA_QUICKTIME_PLAYER_SERVICENAME ) );
+}
+
+// ------------------------------------------------------------------------------
+
+uno::Sequence< ::rtl::OUString > SAL_CALL Player::getSupportedServiceNames(  )
+    throw (uno::RuntimeException)
+{
+    uno::Sequence< ::rtl::OUString > aRet(1);
+    aRet[0] = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( AVMEDIA_QUICKTIME_PLAYER_SERVICENAME ) );
+
+    return aRet;
+}
+
+} // namespace quicktime
+} // namespace avmedia
