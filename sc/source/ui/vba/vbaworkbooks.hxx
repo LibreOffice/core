dc8338994ab9bf5c93f9d4036ee26bc1ebd7fcc4
Vladimir Glazounov <vg@openoffice.org>
2007-12-07 10:06:42 +0000

    INTEGRATION: CWS npower8 (1.2.4); FILE MERGED 2007/09/03 12:19:42 npower 1.2.4.3: #i77189# sync with oobuild 2007/07/18 13:46:48 npower 1.2.4.2: #i77189# sync ooo-build and this module 2007/05/10 11:23:11 npower 1.2.4.1: -m#i77189#

--- sc/source/ui/vba/vbaworkbooks.hxx
+++ sc/source/ui/vba/vbaworkbooks.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vbaworkbooks.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-25 16:13:24 $
+ *  last change: $Author: vg $ $Date: 2007-12-07 11:06:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,29 +45,34 @@
 
 class ScModelObj;
 
-typedef ::cppu::ImplInheritanceHelper1< ScVbaCollectionBaseImpl, oo::excel::XWorkbooks > ScVbaWorkbooks_BASE;
+typedef CollTestImplHelper< oo::excel::XWorkbooks > ScVbaWorkbooks_BASE;
 
 class ScVbaWorkbooks : public ScVbaWorkbooks_BASE
 {
 private:
     css::uno::Reference< oo::vba::XGlobals > getGlobals() throw (css::uno::RuntimeException);
+    rtl::OUString   getFileFilterType( const rtl::OUString& rString );
     bool    isTextFile( const rtl::OUString& rString );
+    bool    isSpreadSheetFile( const rtl::OUString& rString );
     static sal_Int16& getCurrentDelim(){ static sal_Int16 nDelim = 44; return nDelim; }
 public:
-    ScVbaWorkbooks( const css::uno::Reference< css::uno::XComponentContext >& xContext );
+    ScVbaWorkbooks( const css::uno::Reference< oo::vba::XHelperInterface >& xParent, const css::uno::Reference< css::uno::XComponentContext >& xContext );
     virtual ~ScVbaWorkbooks() {}
 
     // XEnumerationAccess
     virtual css::uno::Type SAL_CALL getElementType() throw (css::uno::RuntimeException);
     virtual css::uno::Reference< css::container::XEnumeration > SAL_CALL createEnumeration() throw (css::uno::RuntimeException);
 
-    // ScVbaCollectionBaseImpl
+    // ScVbaWorkbooks_BASE
     virtual css::uno::Any createCollectionObject( const css::uno::Any& aSource );
+    virtual rtl::OUString& getServiceImplName();
+    virtual css::uno::Sequence<rtl::OUString> getServiceNames();
 
     // XWorkbooks
     virtual css::uno::Any SAL_CALL Add() throw (css::uno::RuntimeException);
     virtual void SAL_CALL Close(  ) throw (css::uno::RuntimeException);
     virtual css::uno::Any SAL_CALL Open( const ::rtl::OUString& Filename, const css::uno::Any& UpdateLinks, const css::uno::Any& ReadOnly, const css::uno::Any& Format, const css::uno::Any& Password, const css::uno::Any& WriteResPassword, const css::uno::Any& IgnoreReadOnlyRecommended, const css::uno::Any& Origin, const css::uno::Any& Delimiter, const css::uno::Any& Editable, const css::uno::Any& Notify, const css::uno::Any& Converter, const css::uno::Any& AddToMru ) throw (css::uno::RuntimeException);
+
 };
 
 #endif /* SC_VBA_WORKBOOKS_HXX */
