3ccc2d7af59943ac11e44ae04f7a0926c36ee9da
Kurt Zenker <kz@openoffice.org>
2007-12-12 14:32:46 +0000

    INTEGRATION: CWS pmladek08 (1.13.34); FILE MERGED 2007/12/10 14:15:41 pmladek 1.13.34.1: #i82569# add back support for build with neon-0.24.X

--- ucb/source/ucp/webdav/LinkSequence.cxx
+++ ucb/source/ucp/webdav/LinkSequence.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LinkSequence.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-03 12:12:27 $
+ *  last change: $Author: kz $ $Date: 2007-12-12 15:32:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -180,7 +180,11 @@ bool LinkSequence::createFromXML( const rtl::OString & rInData,
                       rInData.getStr() + nStart,
                       nEnd - nStart + TOKEN_LENGTH );
 
+#if NEON_VERSION >= 0250
         success = !ne_xml_failed( parser );
+#else
+        success = !!ne_xml_valid( parser );
+#endif
 
         ne_xml_destroy( parser );
 
