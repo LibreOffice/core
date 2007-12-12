d85fd0a85cf4c2b182696a3294e78a6bab6a31ff
Kurt Zenker <kz@openoffice.org>
2007-12-12 14:33:03 +0000

    INTEGRATION: CWS pmladek08 (1.14.34); FILE MERGED 2007/12/10 14:15:41 pmladek 1.14.34.1: #i82569# add back support for build with neon-0.24.X

--- ucb/source/ucp/webdav/LockSequence.cxx
+++ ucb/source/ucp/webdav/LockSequence.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: LockSequence.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-03 12:12:52 $
+ *  last change: $Author: kz $ $Date: 2007-12-12 15:33:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -338,7 +338,11 @@ bool LockSequence::createFromXML( const rtl::OString & rInData,
                       rInData.getStr() + nStart,
                       nEnd - nStart + TOKEN_LENGTH );
 
+#if NEON_VERSION >= 0250
         success = !ne_xml_failed( parser );
+#else
+        success = !!ne_xml_valid( parser );
+#endif
 
         ne_xml_destroy( parser );
 
