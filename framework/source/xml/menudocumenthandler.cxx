28aca134ab345e4cef425c63bcfa35c0949e1b4b
Oliver Bolte <obo@openoffice.org>
2008-01-04 15:23:56 +0000

    INTEGRATION: CWS fwk77 (1.11.92); FILE MERGED 2007/10/30 07:45:11 cd 1.11.92.1: #i83012# Fix warnings for 64bit

--- framework/source/xml/menudocumenthandler.cxx
+++ framework/source/xml/menudocumenthandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: menudocumenthandler.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-03 13:50:50 $
+ *  last change: $Author: obo $ $Date: 2008-01-04 16:23:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -241,7 +241,7 @@ throw(  SAXException, RuntimeException )
 
     if ( m_xLocator.is() )
     {
-        snprintf( buffer, sizeof(buffer), "Line: %ld - ", m_xLocator->getLineNumber() );
+        snprintf( buffer, sizeof(buffer), "Line: %ld - ", static_cast<long>( m_xLocator->getLineNumber() ));
         return OUString::createFromAscii( buffer );
     }
     else
