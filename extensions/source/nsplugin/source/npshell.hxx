379aac7131c466b4344f19a3005d721405d41cc5
Jens-Heiner Rechtien <hr@openoffice.org>
2008-01-04 12:09:31 +0000

    INTEGRATION: CWS gcc430 (1.5.428); FILE MERGED 2007/12/13 10:15:27 cmc 1.5.428.1: #i84518# define only if not already defined

--- extensions/source/nsplugin/source/npshell.hxx
+++ extensions/source/nsplugin/source/npshell.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: npshell.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:39:08 $
+ *  last change: $Author: hr $ $Date: 2008-01-04 13:09:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,9 @@
 
 #ifdef UNIX
 
-#define MOZ_X11
+#ifndef MOZ_X11
+#  define MOZ_X11
+#endif
 
 #include <gtk/gtk.h>
 #include <X11/Xlib.h>
