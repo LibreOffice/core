04858e0a6d78726046ec75896082c4e30a4e463d
Oliver Bolte <obo@openoffice.org>
2008-01-04 16:11:50 +0000

    INTEGRATION: CWS native110 (1.1.2); FILE ADDED 2007/11/12 16:03:54 is 1.1.2.1: #i82248# module.pl into source directory

---
+++ scp2/source/templates/makefile.mk
@@ -0,0 +1,59 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2008-01-04 17:11:50 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+
+PRJNAME=scp2
+TARGET=templates
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+.INCLUDE :  target.mk
+
+# force expanding
+COMPLETELANGISO_VAR:=$(uniq $(completelangiso) $(alllangiso))
+.EXPORT : COMPLETELANGISO_VAR
+
+# used here to force expanding before export
+CUSTOM_IMAGE_SETS!:=$(CUSTOM_IMAGE_SETS)
+.EXPORT : CUSTOM_IMAGE_SETS
+
+ALLTAR : $(INCCOM)$/alllangmodules.inc
+
+.PHONY $(INCCOM)$/alllangmodules.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
