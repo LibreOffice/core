ea1585b3c640c75ccb2b455e195d0308c2eb073e
Oliver Bolte <obo@openoffice.org>
2008-01-04 13:14:14 +0000

    INTEGRATION: CWS ause088 (1.22.4); FILE MERGED 2007/12/13 12:04:46 hjs 1.22.4.1: #i84299# remove packtools.mk

--- sysui/desktop/share/makefile.mk
+++ sysui/desktop/share/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.22 $
+#   $Revision: 1.23 $
 #
-#   last change: $Author: rt $ $Date: 2007-11-06 15:57:36 $
+#   last change: $Author: obo $ $Date: 2008-01-04 14:14:14 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,6 @@ TARGET=desktopshare
 # --- Product Version Information ----------------------------------
 
 .INCLUDE :  ../productversion.mk
-.INCLUDE :  packtools.mk
 
 # --- Files --------------------------------------------------------
 
