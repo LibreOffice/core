371ea4320c8289cc246538101ca1e71fc6bdd46f
Oliver Bolte <obo@openoffice.org>
2008-01-04 15:38:49 +0000

    INTEGRATION: CWS fwk77 (1.9.116); FILE MERGED 2007/10/18 12:48:29 mav 1.9.116.1: #i81435# introduce SeekableOutputStream implementation; use new registration

--- comphelper/source/streaming/seqstream.cxx
+++ comphelper/source/streaming/seqstream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: seqstream.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 17:21:22 $
+ *  last change: $Author: obo $ $Date: 2008-01-04 16:38:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -234,7 +234,8 @@ void SAL_CALL OSequenceOutputStream::flush(  ) throw(NotConnectedException, Buff
     if (!m_bConnected)
         throw NotConnectedException();
 
-    // nothing to do here
+    // cut the sequence to the real size
+    m_rSequence.realloc(m_nSize);
 }
 
 //--------------------------------------------------------------------------
