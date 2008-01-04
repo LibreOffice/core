d6a537f9144affb3045b06de743a07e204cfc9e0
Oliver Bolte <obo@openoffice.org>
2008-01-04 13:13:49 +0000

    INTEGRATION: CWS ause088 (1.14.52); FILE MERGED 2007/12/13 12:04:46 hjs 1.14.52.1: #i84299# remove packtools.mk

--- sysui/desktop/mandriva/makefile.mk
+++ sysui/desktop/mandriva/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: kz $ $Date: 2007-05-10 15:24:51 $
+#   last change: $Author: obo $ $Date: 2008-01-04 14:13:49 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,6 @@ TARGET=mandriva
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-.INCLUDE :  packtools.mk
 
 # --- Product Version Information ----------------------------------
 
