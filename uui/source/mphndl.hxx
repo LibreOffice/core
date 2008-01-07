3ee0f56ee6427f1b0432a01a7c73356b1d99cc75
Oliver Bolte <obo@openoffice.org>
2008-01-07 07:43:03 +0000

    INTEGRATION: CWS supdremove (1.2.136); FILE MERGED 2007/11/16 10:23:42 vg 1.2.136.1: #i83674# cleanup: remove obsolete SUPD macro use

--- uui/source/mphndl.hxx
+++ uui/source/mphndl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mphndl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:25:12 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 08:43:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -107,9 +107,7 @@ public:
     getSupportedServiceNames_static();
 
     static com::sun::star::uno::Reference< com::sun::star::uno::XInterface >
-#if SUPD >= 590
     SAL_CALL
-#endif // SUPD 590
     createInstance(com::sun::star::uno::Reference<
                        com::sun::star::lang::XMultiServiceFactory > const &);
 };
