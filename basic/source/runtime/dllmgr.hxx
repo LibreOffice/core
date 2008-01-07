543281dd50f6f5116025491a0dfe734f68941955
Oliver Bolte <obo@openoffice.org>
2008-01-07 07:44:47 +0000

    INTEGRATION: CWS supdremove (1.3.42); FILE MERGED 2007/11/16 10:24:02 vg 1.3.42.1: #i83674# cleanup: remove obsolete SUPD macro use

--- basic/source/runtime/dllmgr.hxx
+++ basic/source/runtime/dllmgr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dllmgr.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 14:25:42 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 08:44:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,14 +52,10 @@
 //#ifndef _TOOLS_HXX
 //#include <tools.hxx>
 //#endif
-#if SUPD > 340
 #define _SVSTDARR_STRINGS
 //#ifndef _SVSTDARR_HXX
 //#include <svstdarr.hxx>
 //#endif
-#else
-//#include <svmem.hxx>
-#endif
 #ifndef _SBERRORS_HXX
 #include <basic/sberrors.hxx>
 #endif
