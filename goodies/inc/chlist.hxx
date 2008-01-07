d1051fb8319c631904d018c4b5d8e200141b8323
Oliver Bolte <obo@openoffice.org>
2008-01-07 07:59:19 +0000

    INTEGRATION: CWS supdremove (1.2.242); FILE MERGED 2007/11/16 10:25:01 vg 1.2.242.1: #i83674# cleanup: remove obsolete SUPD macro use

--- goodies/inc/chlist.hxx
+++ goodies/inc/chlist.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: chlist.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 02:15:55 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 08:59:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -84,13 +84,9 @@ DECLARE_LIST(NotifyList, NotifyItem*)
 
 #ifndef STRING_LIST
 #define STRING_LIST
-#if SUPD >= 380
 #define _TOOLS_STRINGLIST
 #ifndef _SSTRING_HXX
 #include <tools/sstring.hxx>
-#endif
-#else
-DECLARE_LIST(StringList, String*)
 #endif
 #endif
 
