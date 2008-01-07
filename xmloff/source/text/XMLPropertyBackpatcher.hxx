6d83a37feb1c853d33486e2115a81d1c1b21c813
Oliver Bolte <obo@openoffice.org>
2008-01-07 07:47:53 +0000

    INTEGRATION: CWS supdremove (1.5.372); FILE MERGED 2007/11/16 10:24:11 vg 1.5.372.1: #i83674# cleanup: remove obsolete SUPD macro use

--- xmloff/source/text/XMLPropertyBackpatcher.hxx
+++ xmloff/source/text/XMLPropertyBackpatcher.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLPropertyBackpatcher.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:15:05 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 08:47:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,14 +45,8 @@
 #include <vector>
 #endif
 
-#if SUPD > 632 || DVO_TEST
 #ifndef _COMPHELPER_STLTYPES_HXX_
 #include <comphelper/stl_types.hxx>
-#endif
-#else
-#ifndef _XMLOFF_FUNCTIONAL_HXX
-#include "functional.hxx"
-#endif
 #endif
 
 namespace rtl { class OUString; }
@@ -116,18 +110,10 @@ class XMLPropertyBackpatcher
 
     /// backpatch list for unresolved IDs
     //::std::map<const ::rtl::OUString, BackpatchListType*> aBackpatchListMap;
-#if SUPD > 632 || DVO_TEST
     ::std::map<const ::rtl::OUString, void*, ::comphelper::UStringLess> aBackpatchListMap;
-#else
-    ::std::map<const ::rtl::OUString, void*, less_functor> aBackpatchListMap;
-#endif
 
     /// mapping of names -> IDs
-#if SUPD > 632 || DVO_TEST
     ::std::map<const ::rtl::OUString, A, ::comphelper::UStringLess> aIDMap;
-#else
-    ::std::map<const ::rtl::OUString, A, less_functor> aIDMap;
-#endif
 
 public:
 
