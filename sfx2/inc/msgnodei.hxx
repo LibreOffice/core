74a2dcd21c41feb93a16cb5c090c99058b79e94c
Oliver Bolte <obo@openoffice.org>
2008-01-07 08:01:54 +0000

    INTEGRATION: CWS supdremove (1.2.624); FILE MERGED 2007/11/16 10:25:19 vg 1.2.624.1: #i83674# cleanup: remove obsolete SUPD macro use

--- sfx2/inc/msgnodei.hxx
+++ sfx2/inc/msgnodei.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: msgnodei.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:06:28 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 09:01:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -126,10 +126,6 @@ public:
                             SfxMsgReceiverListItem( const SfxMsgReceiverListItem& rItem );
                             ~SfxMsgReceiverListItem();
 
-#if SUPD<355
-    virtual int             IsPoolable() const;
-#endif
-
     virtual int             operator==( const SfxPoolItem& ) const;
 
     virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
@@ -182,10 +178,6 @@ public:
                             SfxMsgAttachFileListItem( USHORT nWhich, SvStream& rStream );
                             SfxMsgAttachFileListItem( const SfxMsgAttachFileListItem& rItem );
                             ~SfxMsgAttachFileListItem();
-
-#if SUPD<355
-    virtual int             IsPoolable() const;
-#endif
 
     virtual int             operator==( const SfxPoolItem& ) const;
 
