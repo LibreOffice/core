ce440983fb610a0ddd9a16d14914a94f03c6b3a3
Oliver Bolte <obo@openoffice.org>
2008-01-07 07:38:36 +0000

    INTEGRATION: CWS supdremove (1.18.90); FILE MERGED 2007/11/16 10:23:11 vg 1.18.90.1: #i83674# cleanup: remove obsolete SUPD macro use

--- svx/source/dialog/thesdlg.cxx
+++ svx/source/dialog/thesdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: thesdlg.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-05 17:45:10 $
+ *  last change: $Author: obo $ $Date: 2008-01-07 08:38:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -151,11 +151,6 @@ private:
     OKButton        aOKBtn;
     CancelButton    aCancelBtn;
     HelpButton      aHelpBtn;
-
-#if SUPD <= 626
-    Reference< XThesaurus >         xThesaurus;
-    uno::Sequence< lang::Locale >   aSuppLang;      // supported languages
-#endif
 
     DECL_LINK( DoubleClickHdl_Impl, ListBox * );
 
