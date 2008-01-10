406c7d29f401d55b4e13199649f5868488d8e24f
Oliver Bolte <obo@openoffice.org>
2008-01-10 10:56:25 +0000

    INTEGRATION: CWS xmlfilter02 (1.1.6); FILE ADDED 2007/11/14 12:58:58 hbrinkm 1.1.6.3: namespace clean up 2007/10/01 10:10:57 os 1.1.6.2: integration of cws writerfilter3 into cws xmlfilter02 2007/07/25 08:01:19 os 1.1.6.1: file OOXMLBinaryObjectReference.cxx was added on branch cws_src680_xmlfilter02 on 2007-10-01 10:10:57 +0000

---
+++ writerfilter/source/ooxml/OOXMLBinaryObjectReference.cxx
@@ -0,0 +1,92 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: OOXMLBinaryObjectReference.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2008-01-10 11:56:25 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#include "OOXMLBinaryObjectReference.hxx"
+
+namespace writerfilter {
+namespace ooxml
+{
+
+using namespace ::com::sun::star;
+
+OOXMLBinaryObjectReference::OOXMLBinaryObjectReference
+(OOXMLStream::Pointer_t pStream)
+: mpStream(pStream), mbRead(false)
+{
+}
+
+OOXMLBinaryObjectReference::~OOXMLBinaryObjectReference()
+{
+}
+
+void OOXMLBinaryObjectReference::read()
+{
+    sal_uInt32 nMaxReadBytes = 1024*1024;
+    uno::Sequence<sal_Int8> aSeq(nMaxReadBytes);
+    uno::Reference<io::XInputStream> xInputStream = mpStream->getInputStream();
+
+    sal_uInt32 nSize = 0;
+    sal_uInt32 nOldSize = 0;
+    sal_uInt32 nBytesRead = 0;
+
+    while ((nBytesRead = xInputStream->readSomeBytes(aSeq, nMaxReadBytes)) > 0)
+    {
+        nOldSize = nSize;
+        nSize += nBytesRead;
+        mSequence.realloc(nSize);
+
+        memcpy(&mSequence[nOldSize], aSeq.getArray(), nBytesRead);
+    }
+
+    mbRead = true;
+}
+
+void OOXMLBinaryObjectReference::resolve(BinaryObj & rHandler)
+{
+    if (! mbRead)
+        read();
+
+    writerfilter::Reference<Properties>::Pointer_t pRef =
+        writerfilter::Reference<Properties>::Pointer_t();
+
+    rHandler.data(reinterpret_cast<sal_uInt8 *>(&mSequence[0]),
+                  mSequence.getLength(), pRef);
+}
+
+string OOXMLBinaryObjectReference::getType() const
+{
+    return "OOXMLBinaryObjectReference";
+}
+
+}}
