66f37f42ba7f1a85ba3b8a2f01d1809e6907e43d
Oliver Bolte <obo@openoffice.org>
2008-01-10 10:32:22 +0000

    INTEGRATION: CWS xmlfilter02 (1.9.12); FILE MERGED 2007/11/29 15:37:56 hbrinkm 1.9.12.5: save XModel and XShapes in document 2007/11/21 11:40:00 hbrinkm 1.9.12.4: getFastTokenHandler 2007/11/14 12:50:47 hbrinkm 1.9.12.3: namespace clean up 2007/10/18 08:26:41 hbrinkm 1.9.12.2: WW8ResourceModel.hxx moved to inc/resourcemodel, ooxmlidsToXML 2007/10/01 10:16:57 os 1.9.12.1: integration of cws writerfilter3 into cws xmlfilter02

--- writerfilter/inc/ooxml/OOXMLDocument.hxx
+++ writerfilter/inc/ooxml/OOXMLDocument.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OOXMLDocument.hxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: vg $ $Date: 2007-10-29 15:29:43 $
+ *  last change: $Author: obo $ $Date: 2008-01-10 11:32:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,33 +48,99 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #endif
 #ifndef INCLUDED_WW8_RESOURCE_MODEL_HXX
-#include <doctok/WW8ResourceModel.hxx>
+#include <resourcemodel/WW8ResourceModel.hxx>
 #endif
 #ifndef _COM_SUN_STAR_XML_SAX_XPARSER_HOO_
 #include <com/sun/star/xml/sax/XParser.hpp>
 #endif
+#include <com/sun/star/xml/sax/XFastParser.hpp>
+#include <com/sun/star/xml/sax/XFastTokenHandler.hpp>
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/drawing/XShapes.hpp>
 
+/**
+   @file OOXMLDocument.hxx
 
+   <h1>Import of OOXML WordprocessingML Documents</h1>
+
+   The following picture shows the classes involved in importing OOXML
+   WordprocessingML documents.
+
+   @image html ooxmlimportchain.png
+
+   The DOCX consists of parts. Each part is an XML document.  The
+   OOXMLDocument opens the DOCX and creates a SAX parser for the part
+   containing the main document content. The OOXMLDocument creates a
+   SAX handler, too. This handler is set as the handler for the events
+   created by the parser. Finally the OOXMLDocument initiates the
+   parsing process.
+
+   The SAX handler hosts a stack of contexts. Each context is an
+   instance of a class derived from OOXMLContext. There is a context
+   class for each <define> in the model.xml.
+
+   For a detailed information about how the contexts are handled see
+   the documentation for OOXMLContext.
+
+   The contexts know how to convert an element in OOXML to the
+   intermediate format that the domain mapper understands. They
+   enumerate the according entity in OOXML by sending the according
+   events to the domain mapper.
+
+   The domain mapper knows how to convert the intermediate format to
+   API calls. It takes the events sent by the contexts and uses the
+   core API to insert the according elements to the core.
+ */
+
+namespace writerfilter {
 namespace ooxml
 {
 
-using namespace doctok;
 using namespace com::sun::star;
 
 class WRITERFILTER_DLLPUBLIC OOXMLStream
 {
 public:
-    enum StreamType_t { UNKNOWN, DOCUMENT, STYLES, FONTTABLE, NUMBERING, FOOTNOTES, ENDNOTES, COMMENTS };
+    enum StreamType_t { UNKNOWN, DOCUMENT, STYLES, FONTTABLE, NUMBERING, FOOTNOTES, ENDNOTES, COMMENTS, THEME };
     typedef boost::shared_ptr<OOXMLStream> Pointer_t;
 
     virtual ~OOXMLStream() {}
 
+    /**
+       Returns parser for this stream.
+     */
     virtual uno::Reference<xml::sax::XParser> getParser() = 0;
+
+    /**
+       Returns fast parser for this stream.
+     */
+    virtual uno::Reference<xml::sax::XFastParser> getFastParser() = 0;
+
+    /**
+       Returns input stream for this stream.
+     */
     virtual uno::Reference<io::XInputStream> getInputStream() = 0;
+
+    /**
+       Returns component context for this stream.
+     */
     virtual uno::Reference<uno::XComponentContext> getContext() = 0;
+
+    /**
+       Returns target URL from relationships for a given id.
+
+       @param rId           the id to look for
+
+       @return the URL found or an empty string
+     */
+    virtual ::rtl::OUString getTargetForId(const ::rtl::OUString & rId) = 0;
+
+    virtual uno::Reference<xml::sax::XFastTokenHandler>
+    getFastTokenHandler(uno::Reference<uno::XComponentContext> rContext) = 0;
+
 };
 
-class WRITERFILTER_DLLPUBLIC OOXMLDocument : public doctok::Reference<Stream>
+class WRITERFILTER_DLLPUBLIC OOXMLDocument : public writerfilter::Reference<Stream>
 {
 public:
     /**
@@ -84,22 +150,99 @@ public:
 
     virtual ~OOXMLDocument() {}
 
-    virtual void resolve(Stream &) = 0;
+    /**
+       Resolves this document to a stream handler.
 
+       @param rStream     stream handler to resolve this document to
+     */
+    virtual void resolve(Stream & rStream) = 0;
+
+    /**
+       Returns string representation of the type of this reference.
+
+       DEBUGGING PURPOSE ONLY.
+     */
     virtual string getType() const = 0;
 
+    /**
+       Resolves a footnote to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param rNoteId       id of the footnote to resolve
+     */
     virtual void resolveFootnote(Stream & rStream,
                                  const rtl::OUString & rNoteId) = 0;
+
+    /**
+       Resolves an endnote to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param rNoteId       id of the endnote to resolve
+     */
     virtual void resolveEndnote(Stream & rStream,
                                 const rtl::OUString & rNoteId) = 0;
+
+    /**
+       Resolves a comment to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param rComment      id of the comment to resolve
+     */
     virtual void resolveComment(Stream & rStream,
-                                const rtl::OUString & rNoteId) = 0;
+                                const rtl::OUString & rCommentId) = 0;
+
+    /**
+       Resolves a picture to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param rPictureId    id of the picture to resolve
+     */
+    virtual void resolvePicture(Stream & rStream,
+                                const rtl::OUString & rPictureId) = 0;
+
+    /**
+       Resolves a header to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param type          type of header to resolve:
+                            NS_ooxml::LN_Value_ST_HrdFtr_even     header on even page
+                            NS_ooxml::LN_Value_ST_HrdFtr_default  header on right page
+                            NS_ooxml::LN_Value_ST_HrdFtr_first    header on first page
+
+       @param rId           id of the header
+     */
     virtual void resolveHeader(Stream & rStream,
                                const sal_Int32 type,
                                const rtl::OUString & rId) = 0;
+
+    /**
+       Resolves a footer to a stream handler.
+
+       @param rStream       stream handler to resolve to
+       @param type          type of footer to resolve:
+                            NS_ooxml::LN_Value_ST_HrdFtr_even     header on even page
+                            NS_ooxml::LN_Value_ST_HrdFtr_default  header on right page
+                            NS_ooxml::LN_Value_ST_HrdFtr_first    header on first page
+
+       @param rId           id of the header
+    */
     virtual void resolveFooter(Stream & rStream,
                                const sal_Int32 type,
                                const rtl::OUString & rId) = 0;
+
+    /**
+       Returns target URL from relationships for a given id.
+
+       @param rId           the id to look for
+
+       @return the URL found or an empty string
+     */
+    virtual ::rtl::OUString getTargetForId(const ::rtl::OUString & rId) = 0;
+
+    virtual void setModel(uno::Reference<frame::XModel> xModel) = 0;
+    virtual uno::Reference<frame::XModel> getModel() = 0;
+    virtual void setShapes(uno::Reference<drawing::XShapes> xShapes) = 0;
+    virtual uno::Reference<drawing::XShapes> getShapes() = 0;
 };
 
 
@@ -120,6 +263,10 @@ public:
 
     static OOXMLDocument *
     createDocument(OOXMLStream::Pointer_t pStream);
+
 };
-}
+
+void ooxmlidsToXML(::std::iostream & out);
+
+}}
 #endif // INCLUDED_OOXML_DOCUMENT_HXX
