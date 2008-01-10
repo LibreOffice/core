06a7c5051a58f44e2057c51bffeeb0b05417cc39
Oliver Bolte <obo@openoffice.org>
2008-01-10 10:42:40 +0000

    INTEGRATION: CWS xmlfilter02 (1.1.6); FILE ADDED 2007/11/14 12:57:07 hbrinkm 1.1.6.4: namespace clean up 2007/10/18 09:35:12 hbrinkm 1.1.6.3: WW8ResourceModel.hxx moved to inc/resourcemodel 2007/10/01 10:10:55 os 1.1.6.2: integration of cws writerfilter3 into cws xmlfilter02 2007/08/03 15:14:10 os 1.1.6.1: file ThemeTable.hxx was added on branch cws_src680_xmlfilter02 on 2007-10-01 10:10:55 +0000

---
+++ writerfilter/source/dmapper/ThemeTable.hxx
@@ -0,0 +1,73 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ThemeTable.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: obo $ $Date: 2008-01-10 11:42:40 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_THEMETABLE_HXX
+#define INCLUDED_THEMETABLE_HXX
+
+#ifndef INCLUDED_WRITERFILTERDLLAPI_H
+#include <WriterFilterDllApi.hxx>
+#endif
+#include <resourcemodel/WW8ResourceModel.hxx>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <map>
+
+namespace writerfilter {
+namespace dmapper
+{
+using namespace std;
+
+struct ThemeTable_Impl;
+
+class WRITERFILTER_DLLPRIVATE ThemeTable : public Properties, public Table
+{
+    ThemeTable_Impl *m_pImpl;
+
+public:
+    ThemeTable();
+    virtual ~ThemeTable();
+
+    // Properties
+    virtual void attribute(Id Name, Value & val);
+    virtual void sprm(Sprm & sprm);
+
+    // Table
+    virtual void entry(int pos, writerfilter::Reference<Properties>::Pointer_t ref);
+
+    const ::rtl::OUString getFontNameForTheme(const Id id) const;
+};
+typedef boost::shared_ptr< ThemeTable >          ThemeTablePtr;
+}}
+
+#endif //
