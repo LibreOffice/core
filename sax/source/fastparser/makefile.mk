f290458774d7680f3324604dc8d2fa03e165e229
Oliver Bolte <obo@openoffice.org>
2008-01-10 11:52:41 +0000

    INTEGRATION: CWS xmlfilter02 (1.1.2); FILE ADDED 2007/03/02 11:24:30 dr 1.1.2.3: using system expat, patch by Hubert Figuiere 2007/02/20 14:13:38 dr 1.1.2.2: remove lib prefix 2007/01/24 14:32:39 cl 1.1.2.1: initial filter framework for office open xml import

---
+++ sax/source/fastparser/makefile.mk
@@ -0,0 +1,81 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2008-01-10 12:52:41 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+PRJ=..$/..
+
+PRJNAME=sax
+TARGET=fastsax.uno
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+DLLPRE =
+
+.IF "$(SYSTEM_ZLIB)" == "YES"
+CFLAGS+=-DSYSTEM_ZLIB
+.ENDIF
+
+.IF "$(SYSTEM_EXPAT)" == "YES"
+CFLAGS+=-DSYSTEM_EXPAT
+.ENDIF
+
+#-----------------------------------------------------------
+
+SLOFILES =\
+        $(SLO)$/fastparser.obj\
+        $(SLO)$/fastattribs.obj\
+        $(SLO)$/xml2utf.obj
+
+SHL1TARGET= $(TARGET)
+SHL1IMPLIB= i$(TARGET)
+
+SHL1STDLIBS= \
+        $(SALLIB)  \
+        $(CPPULIB) \
+        $(CPPUHELPERLIB)\
+        $(EXPATASCII3RDLIB)
+
+SHL1DEPN=
+SHL1VERSIONMAP=	$(SOLARENV)$/src$/component.map
+SHL1LIBS=		$(SLB)$/$(TARGET).lib
+SHL1DEF=		$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=		$(SHL1TARGET)
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :	target.mk
+
+
+
