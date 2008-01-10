b90dc97fc3bb694da5e16a536f83047d19cea1f9
Oliver Bolte <obo@openoffice.org>
2008-01-10 11:46:18 +0000

    INTEGRATION: CWS xmlfilter02 (1.6.40); FILE MERGED 2007/11/21 12:11:21 hbrinkm 1.6.40.4: new service: FastTokenHandler 2007/11/14 13:59:12 hbrinkm 1.6.40.3: FastContextHandler for shapes 2007/03/01 17:33:03 cl 1.6.40.2: moved Attribute.idl to namespace xml 2007/01/24 15:12:32 cl 1.6.40.1: added fast sax api

--- offapi/com/sun/star/xml/sax/makefile.mk
+++ offapi/com/sun/star/xml/sax/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2006-12-01 17:21:41 $
+#   last change: $Author: obo $ $Date: 2008-01-10 12:46:18 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,6 +58,15 @@ IDLFILES=\
     XExtendedDocumentHandler.idl\
     XLocator.idl\
     XParser.idl\
+    XFastParser.idl\
+    XFastDocumentHandler.idl\
+    XFastContextHandler.idl\
+    XFastShapeContextHandler.idl\
+    XFastTokenHandler.idl\
+    XFastAttributeList.idl\
+    FastToken.idl \
+    FastShapeContextHandler.idl \
+    FastTokenHandler.idl
 
 # ------------------------------------------------------------------
 
