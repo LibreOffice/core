247cdcd092956e64a8b9d885aba9e38de3091ce1
Oliver Bolte <obo@openoffice.org>
2008-01-10 11:00:03 +0000

    INTEGRATION: CWS xmlfilter02 (1.8.12); FILE MERGED 2007/12/03 09:39:06 hbrinkm 1.8.12.5: use logger for debug output 2007/11/29 15:45:30 hbrinkm 1.8.12.4: make token handler member of OOXMLStream 2007/11/21 11:50:35 hbrinkm 1.8.12.3: getFastTokenHandler 2007/11/14 12:58:59 hbrinkm 1.8.12.2: namespace clean up 2007/10/01 10:10:58 os 1.8.12.1: integration of cws writerfilter3 into cws xmlfilter02

--- writerfilter/source/ooxml/OOXMLStreamImpl.cxx
+++ writerfilter/source/ooxml/OOXMLStreamImpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OOXMLStreamImpl.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hbrinkm $ $Date: 2007-06-04 08:46:56 $
+ *  last change: $Author: obo $ $Date: 2008-01-10 12:00:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,12 +34,16 @@
  ************************************************************************/
 
 #include "OOXMLStreamImpl.hxx"
+#include "OOXMLFastTokenHandler.hxx"
 #include <iostream>
 
 #ifndef _COM_SUN_STAR_CONTAINER_XHIERARCHICALSTORAGEACCESS_HPP_
 #include <com/sun/star/embed/XHierarchicalStorageAccess.hpp>
 #endif
 
+//#define DEBUG_STREAM
+
+namespace writerfilter {
 namespace ooxml
 {
 
@@ -50,8 +54,7 @@ OOXMLStreamImpl::OOXMLStreamImpl
  uno::Reference<embed::XStorage> xStorage, StreamType_t nType)
 : mxContext(xContext), mxStorage(xStorage), mnStreamType(nType)
 {
-    mxRelationshipAccess = uno::Reference<embed::XRelationshipAccess>
-        (mxStorage, uno::UNO_QUERY);
+    mxRelationshipAccess.set(mxStorage, uno::UNO_QUERY_THROW);
 
     init();
 }
@@ -63,8 +66,7 @@ OOXMLStreamImpl::OOXMLStreamImpl
   mnStreamType(nStreamType),
   msPath(rOOXMLStream.msPath)
 {
-    mxRelationshipAccess = uno::Reference<embed::XRelationshipAccess>
-        (rOOXMLStream.mxDocumentStream, uno::UNO_QUERY);
+    mxRelationshipAccess.set(rOOXMLStream.mxDocumentStream, uno::UNO_QUERY_THROW);
 
     init();
 }
@@ -77,8 +79,7 @@ OOXMLStreamImpl::OOXMLStreamImpl
   mnStreamType(UNKNOWN),
   msId(rId)
 {
-    mxRelationshipAccess = uno::Reference<embed::XRelationshipAccess>
-        (mxStorage, uno::UNO_QUERY);
+    mxRelationshipAccess.set(mxStorage, uno::UNO_QUERY_THROW);
 
     init();
 }
@@ -91,18 +92,26 @@ OOXMLStreamImpl::OOXMLStreamImpl
   msId(rId),
   msPath(rOOXMLStream.msPath)
 {
-    mxRelationshipAccess = uno::Reference<embed::XRelationshipAccess>
-        (rOOXMLStream.mxDocumentStream, uno::UNO_QUERY);
+    mxRelationshipAccess.set(rOOXMLStream.mxDocumentStream, uno::UNO_QUERY_THROW);
 
     init();
 }
 
 OOXMLStreamImpl::~OOXMLStreamImpl()
 {
+#ifdef DEBUG_STREAM
+    logger("DEBUG", "</stream>");
+#endif
 }
 
-void OOXMLStreamImpl::init()
+bool OOXMLStreamImpl::getTarget(uno::Reference<embed::XRelationshipAccess>
+                                xRelationshipAccess,
+                                StreamType_t nStreamType,
+                                const ::rtl::OUString & rId,
+                                ::rtl::OUString & rDocumentTarget)
 {
+    bool bFound = false;
+
     static rtl::OUString sType(RTL_CONSTASCII_USTRINGPARAM("Type"));
     static rtl::OUString sId(RTL_CONSTASCII_USTRINGPARAM("Id"));
     static rtl::OUString sDocumentType(RTL_CONSTASCII_USTRINGPARAM("http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"));
@@ -112,12 +121,18 @@ void OOXMLStreamImpl::init()
     static rtl::OUString sFootnotesType(RTL_CONSTASCII_USTRINGPARAM("http://schemas.openxmlformats.org/officeDocument/2006/relationships/footnotes"));
     static rtl::OUString sEndnotesType(RTL_CONSTASCII_USTRINGPARAM("http://schemas.openxmlformats.org/officeDocument/2006/relationships/endnotes"));
     static rtl::OUString sCommentsType(RTL_CONSTASCII_USTRINGPARAM("http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments"));
+    static rtl::OUString sThemeType(RTL_CONSTASCII_USTRINGPARAM("http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme"));
     static rtl::OUString sTarget(RTL_CONSTASCII_USTRINGPARAM("Target"));
+    static rtl::OUString sTargetMode(RTL_CONSTASCII_USTRINGPARAM("TargetMode"));
+    static rtl::OUString sExternal(RTL_CONSTASCII_USTRINGPARAM("External"));
 
-    rtl::OUString sStreamType(sDocumentType);
+    rtl::OUString sStreamType;
 
-    switch (mnStreamType)
+    switch (nStreamType)
     {
+    case DOCUMENT:
+        sStreamType = sDocumentType;
+        break;
     case STYLES:
         sStreamType = sStylesType;
         break;
@@ -136,23 +151,24 @@ void OOXMLStreamImpl::init()
     case COMMENTS:
         sStreamType = sCommentsType;
         break;
+    case THEME:
+        sStreamType = sThemeType;
+    break;
     default:
         break;
     }
 
-    if (mxRelationshipAccess.is())
+    if (xRelationshipAccess.is())
     {
         uno::Sequence< uno::Sequence< beans::StringPair > >aSeqs =
-            mxRelationshipAccess->getAllRelationships();
-
-        bool bFound = false;
-
-        rtl::OUString sDocumentTarget;
+            xRelationshipAccess->getAllRelationships();
 
         for (sal_Int32 j = 0; j < aSeqs.getLength(); j++)
         {
             uno::Sequence< beans::StringPair > aSeq = aSeqs[j];
 
+            bool bExternalTarget = false;
+            ::rtl::OUString sMyTarget;
             for (sal_Int32 i = 0; i < aSeq.getLength(); i++)
             {
                 beans::StringPair aPair = aSeq[i];
@@ -161,46 +177,74 @@ void OOXMLStreamImpl::init()
                     aPair.Second.compareTo(sStreamType) == 0)
                     bFound = true;
                 else if (aPair.First.compareTo(sId) == 0 &&
-                         aPair.Second.compareTo(msId) == 0)
+                         aPair.Second.compareTo(rId) == 0)
                     bFound = true;
                 else if (aPair.First.compareTo(sTarget) == 0)
-                {
-                    sDocumentTarget = msPath;
-                    sDocumentTarget += aPair.Second;
-                }
+                    sMyTarget = aPair.Second;
+                else if (aPair.First.compareTo(sTargetMode) == 0 &&
+                         aPair.Second.compareTo(sExternal) == 0)
+                    bExternalTarget = true;
 
-                clog << "\""
-                     << OUStringToOString(aPair.First,
-                                          RTL_TEXTENCODING_ASCII_US).getStr()
-                     << ","
-                     << OUStringToOString(aPair.Second,
-                                          RTL_TEXTENCODING_ASCII_US).getStr()
-                     << "\"" << endl;
             }
 
             if (bFound)
-                break;
-        }
-
-        clog << OUStringToOString(sDocumentTarget,
-                                  RTL_TEXTENCODING_ASCII_US).getStr() << endl;
-
-        if (bFound)
-        {
-            sal_Int32 nLastIndex = sDocumentTarget.lastIndexOf('/');
-            if (nLastIndex >= 0)
-                msPath = sDocumentTarget.copy(0, nLastIndex + 1);
-
-            uno::Reference<embed::XHierarchicalStorageAccess>
-                xHierarchicalStorageAccess(mxStorage, uno::UNO_QUERY);
-
-            if (xHierarchicalStorageAccess.is())
             {
-                uno::Any aAny(xHierarchicalStorageAccess->
-                              openStreamElementByHierarchicalName
-                              (sDocumentTarget, embed::ElementModes::READ));
-                aAny >>= mxDocumentStream;
+                if (bExternalTarget)
+                    rDocumentTarget = sMyTarget;
+                else
+                {
+                    rDocumentTarget = msPath;
+                    rDocumentTarget += sMyTarget;
+                }
+
+                break;
             }
+        }
+    }
+
+    return bFound;
+}
+
+::rtl::OUString OOXMLStreamImpl::getTargetForId(const ::rtl::OUString & rId)
+{
+    ::rtl::OUString sTarget;
+
+    uno::Reference<embed::XRelationshipAccess> xRelationshipAccess
+        (mxDocumentStream, uno::UNO_QUERY_THROW);
+
+    if (getTarget(xRelationshipAccess, UNKNOWN, rId, sTarget))
+        return sTarget;
+
+    return ::rtl::OUString();
+}
+
+void OOXMLStreamImpl::init()
+{
+    ::rtl::OUString sDocumentTarget;
+
+    bool bFound = getTarget(mxRelationshipAccess,
+                            mnStreamType, msId, sDocumentTarget);
+#ifdef DEBUG_STREAM
+    logger("DEBUG", string("<stream>")
+           + OUStringToOString(sDocumentTarget,
+                               RTL_TEXTENCODING_ASCII_US).getStr());
+#endif
+
+    if (bFound)
+    {
+        sal_Int32 nLastIndex = sDocumentTarget.lastIndexOf('/');
+        if (nLastIndex >= 0)
+            msPath = sDocumentTarget.copy(0, nLastIndex + 1);
+
+        uno::Reference<embed::XHierarchicalStorageAccess>
+            xHierarchicalStorageAccess(mxStorage, uno::UNO_QUERY);
+
+        if (xHierarchicalStorageAccess.is())
+        {
+            uno::Any aAny(xHierarchicalStorageAccess->
+                          openStreamElementByHierarchicalName
+                          (sDocumentTarget, embed::ElementModes::READ));
+            aAny >>= mxDocumentStream;
         }
     }
 }
@@ -235,6 +279,16 @@ uno::Reference<uno::XComponentContext> OOXMLStreamImpl::getContext()
     return mxContext;
 }
 
+uno::Reference <xml::sax::XFastTokenHandler>
+OOXMLStreamImpl::getFastTokenHandler
+(uno::Reference<uno::XComponentContext> xContext)
+{
+    if (! mxFastTokenHandler.is())
+        mxFastTokenHandler.set(new OOXMLFastTokenHandler(xContext));
+
+    return mxFastTokenHandler;
+}
+
 OOXMLStream::Pointer_t
 OOXMLDocumentFactory::createStream
 (uno::Reference<uno::XComponentContext> xContext,
@@ -267,4 +321,4 @@ OOXMLDocumentFactory::createStream
                              rId));
 }
 
-}
+}}
