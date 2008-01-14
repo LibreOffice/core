a6a86b3915883e82b35e43886f57cb3a39b4b079
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 13:33:51 +0000

    INTEGRATION: CWS wae4extensions (1.6.192); FILE MERGED 2007/09/27 07:18:22 fs 1.6.192.1: #i81612# warning-free code

--- extensions/source/abpilot/fieldmappingimpl.cxx
+++ extensions/source/abpilot/fieldmappingimpl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fieldmappingimpl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 12:52:53 $
+ *  last change: $Author: ihi $ $Date: 2008-01-14 14:33:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -304,7 +304,7 @@ namespace abp
             const ::rtl::OUString sProgrammaticNodeName = ::rtl::OUString::createFromAscii( "ProgrammaticFieldName" );
             const ::rtl::OUString sAssignedNodeName = ::rtl::OUString::createFromAscii( "AssignedFieldName" );
 
-            for (pExistentFields; pExistentFields != pExistentFieldsEnd; ++pExistentFields)
+            for ( ; pExistentFields != pExistentFieldsEnd; ++pExistentFields )
             {
 #ifdef DBG_UTIL
                 ::rtl::OUString sRedundantProgrammaticName;
