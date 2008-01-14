1b7af926776765a67f7bb34c95a8ea645dcff231
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 13:41:23 +0000

    INTEGRATION: CWS wae4extensions (1.5.388); FILE MERGED 2007/09/27 10:20:00 fs 1.5.388.1: #i81612# warning-free code

--- extensions/source/config/ldap/ldapuserprof.hxx
+++ extensions/source/config/ldap/ldapuserprof.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ldapuserprof.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:23:22 $
+ *  last change: $Author: ihi $ $Date: 2008-01-14 14:41:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,14 +36,7 @@
 #ifndef EXTENSIONS_CONFIG_LDAP_LDAPUSERPROF_HXX_
 #define EXTENSIONS_CONFIG_LDAP_LDAPUSERPROF_HXX_
 
-#ifdef WITH_OPENLDAP
-#include <ldap.h>
-#else
-#ifndef LDAP_INCLUDED
-#define LDAP_INCLUDED
-#include <ldap/ldap.h>
-#endif // LDAP_INCLUDED
-#endif
+#include "wrapldapinclude.hxx"
 
 #ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
