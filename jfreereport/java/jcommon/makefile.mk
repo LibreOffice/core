ae7e5e50f410ad4b65aa528420d8df898d41cbfc
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 14:28:37 +0000

    INTEGRATION: CWS pentaho1 (1.1.2); FILE ADDED 2007/12/03 07:36:50 oj 1.1.2.3: #i80723# enable patch file 2007/10/23 05:45:48 oj 1.1.2.2: #i80723# #i81510# makefile changes 2007/10/22 11:45:58 oj 1.1.2.1: #i80723# #i81510# switch to new branch from pentaho

---
+++ jfreereport/java/jcommon/makefile.mk
@@ -0,0 +1,82 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2008-01-14 15:28:37 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+
+PRJNAME=jfreereport
+TARGET=jcommon-1.0.10
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# override buildfile
+ANT_BUILDFILE=ant$/build.xml
+
+.INCLUDE : antsettings.mk
+
+.IF "$(SOLAR_JAVA)" != ""
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=$(TARGET)
+
+TARFILE_ROOTDIR=$(TARGET)
+
+PATCH_FILE_NAME=patches$/jcommon.patch
+
+CONVERTFILES=ant$/build.xml\
+                ant$/build.properties
+                
+OUT2CLASS=$(TARGET).jar
+
+.IF "$(JAVACISGCJ)"=="yes"
+JAVA_HOME=
+.EXPORT : JAVA_HOME
+BUILD_ACTION=$(ANT) -Dbuild.label="build-$(RSCREVISION)" -Dbuild.compiler=gcj -f $(ANT_BUILDFILE) compile
+.ELSE
+BUILD_ACTION=$(ANT) -Dbuild.label="build-$(RSCREVISION)" -f $(ANT_BUILDFILE) compile
+.ENDIF
+
+.ENDIF # $(SOLAR_JAVA)!= ""
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+
+.IF "$(SOLAR_JAVA)" != ""
+.INCLUDE : tg_ext.mk
+.ENDIF
+
