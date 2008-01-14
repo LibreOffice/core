d014739a22990989241a4b2e09dcfe5dbf640df4
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 15:22:11 +0000

    INTEGRATION: CWS aquavcl04 (1.15.2); FILE MERGED 2007/10/15 15:18:32 pl 1.15.2.1: avoid assertion

--- vcl/source/gdi/impprn.cxx
+++ vcl/source/gdi/impprn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: impprn.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: kz $ $Date: 2007-10-09 15:19:55 $
+ *  last change: $Author: ihi $ $Date: 2008-01-14 16:22:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -489,6 +489,7 @@ void ImplQPrinter::EndQueuePrint()
                                  maJobSetup.ImplGetConstData(),
                                  this );
             mpPrinter->EndJob();
+            mbJobActive = mbPrinting = FALSE;
             mpParent->ImplEndPrint();
         }
     }
