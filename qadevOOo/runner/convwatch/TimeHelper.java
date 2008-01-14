3997789e9b73971924fdd46a1926ae6e8ebf9c84
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 12:19:21 +0000

    INTEGRATION: CWS qadev31 (1.1.2); FILE ADDED 2007/10/30 14:27:38 lla 1.1.2.1: #144950# cleanup

---
+++ qadevOOo/runner/convwatch/TimeHelper.java
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: TimeHelper.java,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2008-01-14 13:19:21 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+package convwatch;
+
+public class TimeHelper
+{
+    /*
+      wait a second the caller don't need to handle the interruptexception
+      @param _nSeconds how long should we wait
+      @param _sReason  give a good reason, why we have to wait
+     */
+    static void waitInSeconds(int _nSeconds, String _sReason)
+        {
+            GlobalLogWriter.get().println("Wait " + String.valueOf(_nSeconds) + " sec. Reason: " + _sReason);
+            try {
+                java.lang.Thread.sleep(_nSeconds * 1000);
+            } catch (java.lang.InterruptedException e2) {}
+        }
+}
