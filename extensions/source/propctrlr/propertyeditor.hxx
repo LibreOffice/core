40165c0d9f305f4e5c3e75d9b6dde5ddfa471c7c
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 14:00:20 +0000

    INTEGRATION: CWS wae4extensions (1.14.44); FILE MERGED 2007/09/27 07:18:25 fs 1.14.44.1: #i81612# warning-free code

--- extensions/source/propctrlr/propertyeditor.hxx
+++ extensions/source/propctrlr/propertyeditor.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: propertyeditor.hxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 08:53:10 $
+ *  last change: $Author: ihi $ $Date: 2008-01-14 15:00:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -145,6 +145,10 @@ namespace pcr
         sal_Int32 getMinimumHeight();
 
         void                        CommitModified();
+
+    protected:
+        using Window::SetHelpText;
+        using Window::Update;
 
     private:
         OBrowserPage* getPage( sal_uInt16& _rPageId );
