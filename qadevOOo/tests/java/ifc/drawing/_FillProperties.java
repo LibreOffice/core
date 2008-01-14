3639dd5bf4ce9bf57394aa80a934e57e27208e94
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-14 12:22:43 +0000

    INTEGRATION: CWS qadev31 (1.5.2); FILE MERGED 2007/12/17 18:39:21 cn 1.5.2.1: #ii84664# bugfix in FillBitmap: If property is not avaiable the test results in NULL

--- qadevOOo/tests/java/ifc/drawing/_FillProperties.java
+++ qadevOOo/tests/java/ifc/drawing/_FillProperties.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: _FillProperties.java,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:51:52 $
+ *  last change: $Author: ihi $ $Date: 2008-01-14 13:22:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 package ifc.drawing;
 
 import com.sun.star.beans.XPropertySetInfo;
@@ -43,84 +42,89 @@ import com.sun.star.uno.AnyConverter;
 import com.sun.star.uno.Type;
 
 /**
-* Testing <code>com.sun.star.drawing.FillProperties</code>
-* service properties :
-* <ul>
-*  <li><code> FillStyle</code></li>
-*  <li><code> FillColor</code></li>
-*  <li><code> FillTransparence</code></li>
-*  <li><code> FillTransparenceGradientName</code></li>
-*  <li><code> FillTransparenceGradient</code></li>
-*  <li><code> FillGradientName</code></li>
-*  <li><code> FillGradient</code></li>
-*  <li><code> FillHatchName</code></li>
-*  <li><code> FillHatch</code></li>
-*  <li><code> FillBitmapName</code></li>
-*  <li><code> FillBitmap</code></li>
-*  <li><code> FillBitmapURL</code></li>
-*  <li><code> FillBitmapOffsetX</code></li>
-*  <li><code> FillBitmapOffsetY</code></li>
-*  <li><code> FillBitmapPositionOffsetX</code></li>
-*  <li><code> FillBitmapPositionOffsetY</code></li>
-*  <li><code> FillBitmapRectanglePoint</code></li>
-*  <li><code> FillBitmapLogicalSize</code></li>
-*  <li><code> FillBitmapSizeX</code></li>
-*  <li><code> FillBitmapSizeY</code></li>
-*  <li><code> FillBitmapMode</code></li>
-*  <li><code> FillBackground</code></li>
-* </ul> <p>
-* Properties testing is automated by <code>lib.MultiPropertyTest</code>.
-* @see com.sun.star.drawing.FillProperties
-*/
+ * Testing <code>com.sun.star.drawing.FillProperties</code>
+ * service properties :
+ * <ul>
+ *  <li><code> FillStyle</code></li>
+ *  <li><code> FillColor</code></li>
+ *  <li><code> FillTransparence</code></li>
+ *  <li><code> FillTransparenceGradientName</code></li>
+ *  <li><code> FillTransparenceGradient</code></li>
+ *  <li><code> FillGradientName</code></li>
+ *  <li><code> FillGradient</code></li>
+ *  <li><code> FillHatchName</code></li>
+ *  <li><code> FillHatch</code></li>
+ *  <li><code> FillBitmapName</code></li>
+ *  <li><code> FillBitmap</code></li>
+ *  <li><code> FillBitmapURL</code></li>
+ *  <li><code> FillBitmapOffsetX</code></li>
+ *  <li><code> FillBitmapOffsetY</code></li>
+ *  <li><code> FillBitmapPositionOffsetX</code></li>
+ *  <li><code> FillBitmapPositionOffsetY</code></li>
+ *  <li><code> FillBitmapRectanglePoint</code></li>
+ *  <li><code> FillBitmapLogicalSize</code></li>
+ *  <li><code> FillBitmapSizeX</code></li>
+ *  <li><code> FillBitmapSizeY</code></li>
+ *  <li><code> FillBitmapMode</code></li>
+ *  <li><code> FillBackground</code></li>
+ * </ul> <p>
+ * Properties testing is automated by <code>lib.MultiPropertyTest</code>.
+ * @see com.sun.star.drawing.FillProperties
+ */
 public class _FillProperties extends MultiPropertyTest {
 
     XBitmap the_bitmap = null;
     XBitmap the_secondBitmap = null;
-
     /**
      * Property tester which changes URL.
      */
     protected PropertyTester URLTester = new PropertyTester() {
+
         protected Object getNewValue(String propName, Object oldValue)
                 throws java.lang.IllegalArgumentException {
-            if (oldValue.equals(util.utils.getFullTestURL("space-metal.jpg")))
-                return util.utils.getFullTestURL("crazy-blue.jpg"); else
+            if (oldValue.equals(util.utils.getFullTestURL("space-metal.jpg"))) {
+                return util.utils.getFullTestURL("crazy-blue.jpg");
+            } else {
                 return util.utils.getFullTestURL("space-metal.jpg");
+            }
         }
-    } ;
-
+    };
     /**
      * Property tester which switches two strings.
      */
     protected PropertyTester StringTester = new PropertyTester() {
+
         protected Object getNewValue(String propName, Object oldValue)
                 throws java.lang.IllegalArgumentException {
-            if (oldValue.equals(str1))
-                return str2; else
+            if (oldValue.equals(str1)) {
+                return str2;
+            } else {
                 return str1;
+            }
         }
-    } ;
-
+    };
     /**
      * Property tester which switches two XBitmap objects.
      */
     protected PropertyTester BitmapTester = new PropertyTester() {
+
         protected Object getNewValue(String propName, Object oldValue)
                 throws java.lang.IllegalArgumentException {
-            if (util.ValueComparer.equalValue(oldValue,the_bitmap))
-                return the_secondBitmap; else
+            if (util.ValueComparer.equalValue(oldValue, the_bitmap)) {
+                return the_secondBitmap;
+            } else {
                 return the_bitmap;
+            }
         }
-    } ;
+    };
 
     /**
      * This property must have an URL format
      */
     public void _FillBitmapURL() {
-        log.println("Testing with custom Property tester") ;
-        testProperty("FillBitmapURL", URLTester) ;
+        log.println("Testing with custom Property tester");
+        testProperty("FillBitmapURL", URLTester);
     }
-
     public String str1 = "";
     public String str2 = "";
 
@@ -128,40 +132,40 @@ public class _FillProperties extends MultiPropertyTest {
      * This property must have predefined values
      */
     public void _FillGradientName() {
-        log.println("Testing with custom Property tester") ;
+        log.println("Testing with custom Property tester");
         str1 = "Gradient 1";
         str2 = "Gradient 3";
-        testProperty("FillGradientName", StringTester) ;
+        testProperty("FillGradientName", StringTester);
     }
 
     /**
      * This property must have predefined values
      */
     public void _FillBitmapName() {
-        log.println("Testing with custom Property tester") ;
+        log.println("Testing with custom Property tester");
         str1 = "Sky";
         str2 = "Blank";
-        testProperty("FillBitmapName", StringTester) ;
+        testProperty("FillBitmapName", StringTester);
     }
 
     /**
      * This property must have predefined values
      */
     public void _FillTransparenceGradientName() {
-        log.println("Testing with custom Property tester") ;
+        log.println("Testing with custom Property tester");
         str1 = "Transparency 2";
         str2 = "Transparency 1";
-        testProperty("FillTransparenceGradientName", StringTester) ;
+        testProperty("FillTransparenceGradientName", StringTester);
     }
 
     /**
      * This property must have predefined values
      */
     public void _FillHatchName() {
-        log.println("Testing with custom Property tester") ;
+        log.println("Testing with custom Property tester");
         str1 = "Black 0 degrees";
         str2 = "Black 45 degrees";
-        testProperty("FillHatchName", StringTester) ;
+        testProperty("FillHatchName", StringTester);
     }
 
     /**
@@ -178,49 +182,46 @@ public class _FillProperties extends MultiPropertyTest {
         if (!info.hasPropertyByName(propName)) {
             if (isOptional(propName)) {
                 // skipping optional property test
-                log.println("Property '" + propName
-                        + "' is optional and not supported");
-                tRes.tested(propName,true);
+                log.println("Property '" + propName + "' is optional and not supported");
+                tRes.tested(propName, true);
                 return;
-            } else {
-
-                try {
-
-                    oObj.setPropertyValue("FillBitmapURL", util.utils.getFullTestURL("crazy-blue.jpg"));
-
-                    the_bitmap = (XBitmap) AnyConverter.toObject(
-                                        new Type(XBitmap.class), oObj.getPropertyValue("FillBitmap"));
-
-                    oObj.setPropertyValue("FillBitmapURL", util.utils.getFullTestURL("space-metal.jpg"));
-
-                    the_secondBitmap = (XBitmap) AnyConverter.toObject(
-                                        new Type(XBitmap.class),oObj.getPropertyValue("FillBitmap"));
-
-                    testProperty("FillBitmap", BitmapTester) ;
-
-                } catch (com.sun.star.beans.PropertyVetoException e) {
-                    log.println("Couldn't change Bitmap");
-                    e.printStackTrace(log);
-                    tRes.tested("FillBitmap",false);
-
-                } catch (com.sun.star.beans.UnknownPropertyException e) {
-                    log.println("Couldn't change Bitmap");
-                    e.printStackTrace(log);
-                    tRes.tested("FillBitmap",false);
-
-                } catch (com.sun.star.lang.WrappedTargetException e) {
-                    log.println("Couldn't change Bitmap");
-                    e.printStackTrace(log);
-                    tRes.tested("FillBitmap",false);
-
-                } catch (com.sun.star.lang.IllegalArgumentException e) {
-                    log.println("Couldn't change Bitmap");
-                    e.printStackTrace(log);
-                    tRes.tested("FillBitmap",false);
-                }
             }
+        } else {
 
+            try {
+
+                oObj.setPropertyValue("FillBitmapURL", util.utils.getFullTestURL("crazy-blue.jpg"));
+
+                the_bitmap = (XBitmap) AnyConverter.toObject(
+                        new Type(XBitmap.class), oObj.getPropertyValue("FillBitmap"));
+
+                oObj.setPropertyValue("FillBitmapURL", util.utils.getFullTestURL("space-metal.jpg"));
+
+                the_secondBitmap = (XBitmap) AnyConverter.toObject(
+                        new Type(XBitmap.class), oObj.getPropertyValue("FillBitmap"));
+
+                testProperty("FillBitmap", BitmapTester);
+
+            } catch (com.sun.star.beans.PropertyVetoException e) {
+                log.println("Couldn't change Bitmap");
+                e.printStackTrace(log);
+                tRes.tested("FillBitmap", false);
+
+            } catch (com.sun.star.beans.UnknownPropertyException e) {
+                log.println("Couldn't change Bitmap");
+                e.printStackTrace(log);
+                tRes.tested("FillBitmap", false);
+
+            } catch (com.sun.star.lang.WrappedTargetException e) {
+                log.println("Couldn't change Bitmap");
+                e.printStackTrace(log);
+                tRes.tested("FillBitmap", false);
+
+            } catch (com.sun.star.lang.IllegalArgumentException e) {
+                log.println("Couldn't change Bitmap");
+                e.printStackTrace(log);
+                tRes.tested("FillBitmap", false);
+            }
         }
     }
-}
-
+    }
