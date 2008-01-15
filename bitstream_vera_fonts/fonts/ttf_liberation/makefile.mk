5551221c575d45db43dedba18dcbe61f08f06ecc
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-15 11:14:01 +0000

    INTEGRATION: CWS symex (1.1.2); FILE ADDED 2007/11/30 14:15:07 hdu 1.1.2.2: #i59853# simplify makefiles and build.lst (thanks Ause!) 2007/11/29 10:30:31 hdu 1.1.2.1: #151427# add font family Liberation

---
+++ bitstream_vera_fonts/fonts/ttf_liberation/makefile.mk
@@ -0,0 +1,70 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: ihi $ $Date: 2008-01-15 12:14:01 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=../..
+
+PRJNAME=bitstream_vera_fonts
+TARGET=ttf_liberation
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=liberation-fonts
+TARFILE_ROOTDIR=liberation-fonts
+
+PATCH_FILE_NAME=
+
+
+CONFIGURE_DIR=
+CONFIGURE_ACTION=
+
+BUILD_DIR=
+BUILD_ACTION=
+BUILD_FLAGS=
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+
+.IF "$(WITH_FONTS)"!="NO"
+.IF "$(GUI)" != "WNT"
+.INCLUDE : tg_ext.mk
+.ENDIF
+.ENDIF
+
