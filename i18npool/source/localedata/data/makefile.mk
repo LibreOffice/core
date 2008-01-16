e9d6f1da5b41b68c6819d053376434ce58c03dc2
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-16 13:33:21 +0000

    INTEGRATION: CWS ause085 (1.43.2); FILE MERGED 2007/11/26 15:00:05 hjs 1.43.2.1: #i83981# makefile fixes

--- i18npool/source/localedata/data/makefile.mk
+++ i18npool/source/localedata/data/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.43 $
+#   $Revision: 1.44 $
 #
-#   last change: $Author: rt $ $Date: 2007-11-13 14:35:25 $
+#   last change: $Author: ihi $ $Date: 2008-01-16 14:33:21 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -473,6 +473,12 @@ SHL4OBJS= \
 
 LIB4TARGET=	$(SLB)$/$(SHL4TARGET).lib
 LIB4OBJFILES=$(SHL4OBJS)
+
+DEPOBJFILES= \
+    $(SHL1OBJS) \
+    $(SHL2OBJS) \
+    $(SHL3OBJS) \
+    $(SHL4OBJS)
 
 # --- Targets ------------------------------------------------------
 
