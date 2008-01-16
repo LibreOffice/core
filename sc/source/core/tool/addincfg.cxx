09b1a83ae224522db26384ccd0e32d82d4f2ea29
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-16 13:29:39 +0000

    INTEGRATION: CWS ause085 (1.2.370); FILE MERGED 2007/12/05 13:21:00 hjs 1.2.370.1: #i84262# move define from commandline into generated header; add missing pch includes

--- sc/source/core/tool/addincfg.cxx
+++ sc/source/core/tool/addincfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: addincfg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 12:11:47 $
+ *  last change: $Author: ihi $ $Date: 2008-01-16 14:29:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
-//------------------------------------------------------------------
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include <tools/debug.hxx>
 
