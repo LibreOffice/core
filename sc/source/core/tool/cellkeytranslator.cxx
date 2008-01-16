6a5e7daf883bc93f93aea1e9218c489b05ce10d6
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-16 13:29:51 +0000

    INTEGRATION: CWS ause085 (1.3.16); FILE MERGED 2007/12/05 13:21:01 hjs 1.3.16.1: #i84262# move define from commandline into generated header; add missing pch includes

--- sc/source/core/tool/cellkeytranslator.cxx
+++ sc/source/core/tool/cellkeytranslator.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cellkeytranslator.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2007-11-01 14:21:26 $
+ *  last change: $Author: ihi $ $Date: 2008-01-16 14:29:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include "cellkeytranslator.hxx"
 #include "comphelper/processfactory.hxx"
