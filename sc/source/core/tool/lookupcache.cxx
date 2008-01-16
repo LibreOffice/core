fe3d7c8313a745113f84b8be32daa4d40c2e12d2
Ivo Hinkelmann <ihi@openoffice.org>
2008-01-16 13:30:33 +0000

    INTEGRATION: CWS ause085 (1.2.80); FILE MERGED 2007/12/05 13:21:01 hjs 1.2.80.1: #i84262# move define from commandline into generated header; add missing pch includes

--- sc/source/core/tool/lookupcache.cxx
+++ sc/source/core/tool/lookupcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: lookupcache.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 13:53:55 $
+ *  last change: $Author: ihi $ $Date: 2008-01-16 14:30:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include "lookupcache.hxx"
 #include "document.hxx"
