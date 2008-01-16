8cb7503f341f7688326861599ad0979194ea509c
Rüdiger Timm <rt@openoffice.org>
2008-01-16 07:45:48 +0000

    INTEGRATION: CWS rt25_SRC680 (1.6.940); FILE MERGED 2008/01/14 14:39:58 rt 1.6.940.1: #i85224# Set 'to' header entry.

--- solenv/bin/modules/installer/mail.pm
+++ solenv/bin/modules/installer/mail.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: mail.pm,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 09:05:38 $
+#   last change: $Author: rt $ $Date: 2008-01-16 08:45:48 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -71,6 +71,8 @@ sub send_mail
 
     my @message = ();
 
+    my $recipient_string = join ',', @listener;
+    push(@message, "To: $recipient_string\n");
     push(@message, "Subject: $subject");
     push(@message, "\n");
     push(@message, "Located at $destdir");
