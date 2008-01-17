de18cd64a039ae13fd2a97a5904e45756d813ae3
Vladimir Glazounov <vg@openoffice.org>
2008-01-17 14:37:58 +0000

    #i85315# fix

--- writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
+++ writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OOXMLFastContextHandler.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2008-01-17 15:01:15 $
+ *  last change: $Author: vg $ $Date: 2008-01-17 15:37:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -171,7 +171,12 @@ void OOXMLFastContextHandler::dumpOpenContexts()
     }
 
     char buffer[256];
+#ifdef MACOSX
     snprintf(buffer, sizeof(buffer), "%ld", aSetContexts.size());
+#else
+    snprintf(buffer, sizeof(buffer), "%d", aSetContexts.size());
+#endif
+
     logger("DEBUG", string("<count>") + buffer + "</count>");
     logger("DEBUG", "</open-contexts>");
 }
