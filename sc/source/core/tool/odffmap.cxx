da0379b9c78179f6c54004ec7c1e8c79203bb77c
Oliver Bolte <obo@openoffice.org>
2008-01-28 09:19:56 +0000

    #i85602# precompiled header include missing

--- sc/source/core/tool/odffmap.cxx
+++ sc/source/core/tool/odffmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: odffmap.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-10 13:14:02 $
+ *  last change: $Author: obo $ $Date: 2008-01-28 10:19:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,6 +32,9 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
 
 #include "compiler.hxx"
 
