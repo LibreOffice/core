4dc2795bced12daba2d714214833e81f5e36591b
Vladimir Glazounov <vg@openoffice.org>
2008-01-28 15:30:09 +0000

    INTEGRATION: CWS jsc20 (1.3.362); FILE MERGED 2008/01/03 12:07:46 jsc 1.3.362.1: #i83166# correct typo for finalizer method

--- odk/examples/DevelopersGuide/ProfUNO/Lifetime/MyUnoObject.java
+++ odk/examples/DevelopersGuide/ProfUNO/Lifetime/MyUnoObject.java
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MyUnoObject.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2003-06-30 15:44:50 $
+ *  last change: $Author: vg $ $Date: 2008-01-28 16:30:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  the BSD license.
@@ -41,7 +41,11 @@
 class MyUnoObject implements com.sun.star.uno.XInterface
 {
     public MyUnoObject() {}
-    void finialize() { System.out.println( "finalizer called" ); }
+
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println( "finalizer called" );
+    }
 
     public static void main( String args[] ) throws java.lang.InterruptedException
     {
