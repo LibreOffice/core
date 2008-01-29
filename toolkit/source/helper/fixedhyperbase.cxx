aee3d3cf7c5d7373f803996ca55be889b20c564f
Rüdiger Timm <rt@openoffice.org>
2008-01-29 14:06:50 +0000

    INTEGRATION: CWS fwk80_SRC680 (1.1.2); FILE ADDED 2008/01/11 14:54:26 pb 1.1.2.1: fix: #i83756# class FixedHyperlinkBase

---
+++ toolkit/source/helper/fixedhyperbase.cxx
@@ -0,0 +1,80 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: fixedhyperbase.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-01-29 15:06:50 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_toolkit.hxx"
+
+#include <toolkit/helper/fixedhyperbase.hxx>
+
+//........................................................................
+namespace toolkit
+{
+//........................................................................
+
+//  ----------------------------------------------------
+//  class FixedHyperlinkBase
+//  ----------------------------------------------------
+
+FixedHyperlinkBase::FixedHyperlinkBase( Window* pParent, const ResId& rId ) :
+    FixedText( pParent, rId )
+{
+}
+
+FixedHyperlinkBase::FixedHyperlinkBase( Window* pParent, WinBits nWinStyle ) :
+    FixedText( pParent, nWinStyle )
+{
+}
+
+FixedHyperlinkBase::~FixedHyperlinkBase()
+{
+}
+
+void FixedHyperlinkBase::SetURL( const String& )
+{
+}
+
+String FixedHyperlinkBase::GetURL() const
+{
+    return String::EmptyString();
+}
+
+void FixedHyperlinkBase::SetDescription( const String& )
+{
+}
+
+//........................................................................
+} // namespace toolkit
+//........................................................................
+
