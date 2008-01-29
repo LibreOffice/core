41610dd92c95c53fca24f4b31dd34b4853a6e46d
Rüdiger Timm <rt@openoffice.org>
2008-01-29 14:27:12 +0000

    INTEGRATION: CWS fwk80_SRC680 (1.2.36); FILE MERGED 2008/01/21 08:30:22 pb 1.2.36.1: fix: #i83756# vclxaccessiblefixedhyperlink.obj

--- accessibility/source/standard/makefile.mk
+++ accessibility/source/standard/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2007-06-27 15:36:43 $
+#   last change: $Author: rt $ $Date: 2008-01-29 15:27:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,8 @@ SLOFILES=   \
             $(SLO)$/vclxaccessibledropdowncombobox.obj	\
             $(SLO)$/vclxaccessibledropdownlistbox.obj	\
             $(SLO)$/vclxaccessibleedit.obj				\
-            $(SLO)$/vclxaccessiblefixedtext.obj			\
+            $(SLO)$/vclxaccessiblefixedhyperlink.obj    \
+            $(SLO)$/vclxaccessiblefixedtext.obj         \
             $(SLO)$/vclxaccessiblelist.obj				\
             $(SLO)$/vclxaccessiblelistbox.obj			\
             $(SLO)$/vclxaccessiblelistitem.obj			\
