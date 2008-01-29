b8c2adff4a849ab882682cef52104ceb4b7d5a73
Rüdiger Timm <rt@openoffice.org>
2008-01-29 15:07:22 +0000

    INTEGRATION: CWS vcl85_SRC680 (1.48.36); FILE MERGED 2007/12/21 13:03:17 pl 1.48.36.2: RESYNC: (1.48-1.49); FILE MERGED 2007/12/07 10:10:00 pl 1.48.36.1: #i84337# fix a typo, avoid using NULL ResMgr ptr

--- tools/source/rc/resmgr.cxx
+++ tools/source/rc/resmgr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: resmgr.cxx,v $
  *
- *  $Revision: 1.49 $
+ *  $Revision: 1.50 $
  *
- *  last change: $Author: kz $ $Date: 2007-12-12 13:14:45 $
+ *  last change: $Author: rt $ $Date: 2008-01-29 16:07:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1515,7 +1515,7 @@ ResMgr* ResMgr::CreateFallbackResMgr( const ResId& rId, const Resource* pResourc
             }
             if( bHaveStack )
             {
-                ResId aId( rId.GetId(), *pFallbackResMgr );
+                ResId aId( rId.GetId(), *pFallback );
                 aId.SetRT( rId.GetRT() );
                 if( !pFallback->GetResource( aId, pResource ) )
                     bHaveStack = false;
