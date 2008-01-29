4e7d61a52c96b43f7720539508f56c90572ba6eb
Rüdiger Timm <rt@openoffice.org>
2008-01-29 08:55:31 +0000

    INTEGRATION: CWS openssl02 (1.60.12); FILE MERGED 2008/01/08 07:33:52 tkr 1.60.12.1: #i84181# correct Windows packaging issue

--- scp2/source/ooo/makefile.mk
+++ scp2/source/ooo/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.61 $
+#   $Revision: 1.62 $
 #
-#   last change: $Author: obo $ $Date: 2008-01-04 17:09:37 $
+#   last change: $Author: rt $ $Date: 2008-01-29 09:55:31 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -208,7 +208,7 @@ SCPDEFS+=-DDISABLE_NEON
 .ENDIF
 
 # if yes or unset (neon not used) -> do not install openssl library!
-.IF $(SYSTEM_OPENSSL) == "NO"
+.IF $(SYSTEM_OPENSSL) != "YES"
 SCPDEFS+=-DOPENSSL
 .ENDIF
 
