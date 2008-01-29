367e1aff2ab13b3c97c64b22cf318bc95d2655e8
Rüdiger Timm <rt@openoffice.org>
2008-01-29 14:43:36 +0000

    INTEGRATION: CWS dr58_SRC680 (1.4.188); FILE MERGED 2007/12/21 16:25:41 er 1.4.188.1: remove superfluous SC_MAXFIELDS

--- sc/source/ui/inc/tpusrlst.hxx
+++ sc/source/ui/inc/tpusrlst.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tpusrlst.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 13:28:03 $
+ *  last change: $Author: rt $ $Date: 2008-01-29 15:43:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,8 +51,6 @@
 #ifndef _SVEDIT_HXX //autogen
 #include <svtools/svmedit.hxx>
 #endif
-
-#define SC_MAXFIELDS 200
 
 //========================================================================
 // Benutzerdefinierte Listen:
