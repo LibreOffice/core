8c51fd7d01985d1b1b0ebd5a172306218ceedb53
Rüdiger Timm <rt@openoffice.org>
2008-01-29 13:39:50 +0000

    INTEGRATION: CWS dba24f_SRC680 (1.20.20); FILE MERGED 2008/01/14 10:31:33 fs 1.20.20.1: #i85215# don't reset m_aValueSeq prematurely

--- forms/source/component/ListBox.hxx
+++ forms/source/component/ListBox.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ListBox.hxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: hr $ $Date: 2007-11-01 14:55:26 $
+ *  last change: $Author: rt $ $Date: 2008-01-29 14:39:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -201,6 +201,9 @@ private:
     @precond we don't actually have an external list source
     */
     void        implRefreshListFromDbBinding( );
+
+    StringSequence
+                impl_getValues() const { return m_aValueSeq.getLength() ? m_aValueSeq : getStringItemList(); }
 };
 
 //==================================================================
