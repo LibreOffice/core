65d6a0f34518353d085bbf4e36de1e016e969790
Rüdiger Timm <rt@openoffice.org>
2008-01-29 15:24:08 +0000

    INTEGRATION: CWS vcl85_SRC680 (1.2.136); FILE MERGED 2008/01/09 08:53:13 sb 1.2.136.1: #i85099# Fix silent merge conflict from CWS sb71 and CWS mergesvp.

--- vcl/util/linksvp/makefile.mk
+++ vcl/util/linksvp/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-24 10:33:09 $
+#   last change: $Author: rt $ $Date: 2008-01-29 16:24:08 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,13 +39,13 @@ PRJNAME=vcl
 TARGET=svp
 TARGETTYPE=CUI
 
-.IF "$(OS)" == "SOLARIS"
-LINKFLAGSRUNPATH=-R/usr/sfw/lib -R\''$$ORIGIN'\'
-.ENDIF
-
 # --- Settings -----------------------------------------------------------
 
 .INCLUDE :  settings.mk
+
+.IF "$(OS)" == "SOLARIS"
+LINKFLAGSRUNPATH_OOO := -R/usr/sfw/lib $(LINKFLAGSRUNPATH_OOO)
+.ENDIF
 
 .IF "$(GUIBASE)" == "unx"
 # headless plugin
