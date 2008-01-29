9f5d5351a91364aa8ebf7d450acbba915fb126e6
Rüdiger Timm <rt@openoffice.org>
2008-01-29 14:29:58 +0000

    INTEGRATION: CWS dr58_SRC680 (1.13.188); FILE MERGED 2008/01/14 15:14:08 dr 1.13.188.1: #i84878# shared formulas still broken sometimes

--- sc/source/filter/inc/namebuff.hxx
+++ sc/source/filter/inc/namebuff.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: namebuff.hxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:34:59 $
+ *  last change: $Author: rt $ $Date: 2008-01-29 15:29:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -204,6 +204,7 @@ class ShrfmlaBuffer : public ExcRoot
 public:
                             ShrfmlaBuffer( RootData* pRD );
     virtual                 ~ShrfmlaBuffer();
+    void                    Clear();
     void                    Store( const ScRange& rRange, const ScTokenArray& );
     USHORT                  Find (const ScAddress & rAddress ) const;
 
