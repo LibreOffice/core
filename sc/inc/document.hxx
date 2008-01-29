fbb698889843230aea808ac3ff53d46d58e6681b
Rüdiger Timm <rt@openoffice.org>
2008-01-29 14:15:18 +0000

    INTEGRATION: CWS dr58_SRC680 (1.105.30); FILE MERGED 2008/01/11 15:10:07 dr 1.105.30.1: #i10000# missing include

--- sc/inc/document.hxx
+++ sc/inc/document.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: document.hxx,v $
  *
- *  $Revision: 1.106 $
+ *  $Revision: 1.107 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-10 13:08:25 $
+ *  last change: $Author: rt $ $Date: 2008-01-29 15:15:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,6 +74,7 @@
 #endif
 
 #include <memory>
+#include <map>
 
 class KeyEvent;
 class OutputDevice;
