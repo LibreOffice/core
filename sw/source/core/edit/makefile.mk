7425ed0885a5a0f44b1d60471e01f16ea7442304
Vladimir Glazounov <vg@openoffice.org>
2008-01-29 07:38:18 +0000

    INTEGRATION: CWS sw8u10bf04 (1.10.122); FILE MERGED 2007/12/20 13:51:54 ama 1.10.122.3: Merge problem solved 2007/12/20 12:29:47 ama 1.10.122.2: RESYNC: (1.10-1.11); FILE MERGED 2007/12/13 09:00:19 ama 1.10.122.1: Fix #i84507#: boost needs exception handling

--- sw/source/core/edit/makefile.mk
+++ sw/source/core/edit/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: kz $ $Date: 2007-12-13 11:38:50 $
+#   last change: $Author: vg $ $Date: 2008-01-29 08:38:18 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -49,9 +49,9 @@ AUTOSEG=true
 # --- Files --------------------------------------------------------
 
 EXCEPTIONSFILES=\
+        $(SLO)$/eddel.obj \
         $(SLO)$/edlingu.obj \
         $(SLO)$/edfldexp.obj \
-        $(SLO)$/eddel.obj \
         $(SLO)$/edtab.obj
 
 SLOFILES =	\
