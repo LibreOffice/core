460d6787feff5dba163149a25ea24b9ca020d32a
Rüdiger Timm <rt@openoffice.org>
2008-01-29 13:41:40 +0000

    INTEGRATION: CWS pj89 (1.5.40); FILE MERGED 2007/12/23 09:46:54 pjanik 1.5.40.1: #i84856#: bridges is completely WaE clean now.

--- bridges/test/inter_libs_exc/makefile.mk
+++ bridges/test/inter_libs_exc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 08:49:22 $
+#   last change: $Author: rt $ $Date: 2008-01-29 14:41:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,6 @@ TARGETTYPE=CUI
 ENABLE_EXCEPTIONS=TRUE
 USE_DEFFILE=TRUE
 
-EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
