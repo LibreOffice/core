c09219d5ed9ff8b25b7c0cecb490d098ab42941c
René Engelhard <rene@openoffice.org>
2008-02-01 09:21:44 +0000

    #i85747# --enable-mediawiki and --with-system-apache-commons

--- swext/mediawiki/makefile.mk
+++ swext/mediawiki/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: rene $ $Date: 2008-01-30 21:14:31 $
+#   last change: $Author: rene $ $Date: 2008-02-01 10:21:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,5 +37,25 @@ PRJ=..
 PRJNAME=swext
 TARGET=mediawiki
 
+.IF "$(ENABLE_MEDIAWIKI)" == "YES"
 .INCLUDE : ant.mk
 ALLTAR: ANTBUILD
+
+.IF "$(SYSTEM_APACHE_COMMONS)" != "YES"
+COMMONS_CODEC_JAR=jars/commons-codec-1.3.jar 
+COMMONS_LANG_JAR=jars/commons-lang-2.3.jar
+COMMONS_HTTPCLIENT_JAR=jars/commons-httpclient-3.0.1.jar
+COMMONS_LOGGING_JAR=jars/commons-logging-1.1.jar
+.ENDIF
+
+.IF "$(SYSTEM_XML_APIS)" != "YES"
+XML_APIS_JAR = $(SOLARVER)$/$(INPATH)$/lib/xml-apis.jar
+.ENDIF
+
+ANT_FLAGS+=-Dcommons-codec-jar=$(COMMONS_CODEC_JAR) -Dcommons-lang-jar=$(COMMONS_LANG_JAR) -Dcommons-httpclient-jar=$(COMMONS_HTTPCLIENT_JAR) -Dcommons-logging-jar=$(COMMONS_LOGGING_JAR) -Dxml-apis-jar=$(XML_APIS_JAR)
+
+.ELSE
+@all:
+    @echo "MediaWiki Editor extension disabled."
+.ENDIF
+
