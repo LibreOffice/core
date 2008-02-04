d66a6fb67c53404aec3b9fc8de933b4764ff0fff
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:59:57 +0000

    INTEGRATION: CWS gcc430two (1.12.54); FILE MERGED 2008/01/28 09:53:09 rene 1.12.54.1: more gcc 4.3.0 things

--- package/source/zipapi/XUnbufferedStream.cxx
+++ package/source/zipapi/XUnbufferedStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XUnbufferedStream.cxx,v $
  *
- *  $Revision: 1.12 $
+ *  $Revision: 1.13 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 17:27:48 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:59:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,6 +60,7 @@
 #include <EncryptedDataHeader.hxx>
 #endif
 #include <algorithm>
+#include <string.h>
 
 #if 0
 // for debugging purposes here
