a1aec13bb12ef368d988090b51be1de37fad2976
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:57:39 +0000

    INTEGRATION: CWS gcc430two (1.13.6); FILE MERGED 2008/01/28 09:53:12 rene 1.13.6.1: more gcc 4.3.0 things

--- unoxml/source/dom/node.cxx
+++ unoxml/source/dom/node.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: node.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: vg $ $Date: 2007-12-06 11:18:20 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:57:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,6 +34,7 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 #include "node.hxx"
 #include "element.hxx"
 #include "text.hxx"
