fa5263d3600f015d9492c2e12ee2a8865fbdb29c
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:49:45 +0000

    INTEGRATION: CWS gcc430two (1.2.10); FILE MERGED 2008/01/28 09:53:13 rene 1.2.10.1: more gcc 4.3.0 things

--- writerfilter/source/ooxml/OOXMLBinaryObjectReference.cxx
+++ writerfilter/source/ooxml/OOXMLBinaryObjectReference.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OOXMLBinaryObjectReference.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-10 11:56:25 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:49:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,6 +33,7 @@
  *
  ************************************************************************/
 #include "OOXMLBinaryObjectReference.hxx"
+#include <string.h>
 
 namespace writerfilter {
 namespace ooxml
