f474db505aa93cce1401c3f6ada651b6c73c39be
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:29:43 +0000

    INTEGRATION: CWS gcc430two (1.11.202); FILE MERGED 2008/01/28 09:53:04 rene 1.11.202.1: more gcc 4.3.0 things

--- connectivity/source/drivers/jdbc/InputStream.cxx
+++ connectivity/source/drivers/jdbc/InputStream.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: InputStream.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 02:47:00 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:29:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,6 +41,8 @@
 #ifndef _CONNECTIVITY_JAVA_TOOLS_HXX_
 #include "java/tools.hxx"
 #endif
+
+#include <string.h>
 
 using namespace connectivity;
 //**************************************************************
