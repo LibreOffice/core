3dd8d24e23394614fdfab05d044bfd34914a0890
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:41:35 +0000

    INTEGRATION: CWS gcc430two (1.18.194); FILE MERGED 2008/01/28 09:53:05 rene 1.18.194.1: more gcc 4.3.0 things

--- filter/source/xmlfilteradaptor/XmlFilterAdaptor.cxx
+++ filter/source/xmlfilteradaptor/XmlFilterAdaptor.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XmlFilterAdaptor.cxx,v $
  *
- *  $Revision: 1.18 $
+ *  $Revision: 1.19 $
  *
- *  last change: $Author: rt $ $Date: 2006-12-01 14:32:11 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:41:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_filter.hxx"
-#include <iostream.h>
+#include <iostream>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
