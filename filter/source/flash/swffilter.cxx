4ff6ecf6e843af07a531e1dec0c77384182e6d1b
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:41:22 +0000

    INTEGRATION: CWS gcc430two (1.6.166); FILE MERGED 2008/01/28 09:53:05 rene 1.6.166.1: more gcc 4.3.0 things

--- filter/source/flash/swffilter.cxx
+++ filter/source/flash/swffilter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swffilter.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2007-01-18 14:09:46 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:41:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -88,6 +88,7 @@
 
 //#include <stdlib.h>
 //#include <windows.h>
+#include <string.h>
 
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::frame;
