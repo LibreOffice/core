2049d810d76f9e3251ddcb1b8be5945e44c4506f
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:43:38 +0000

    INTEGRATION: CWS gcc430two (1.9.86); FILE MERGED 2008/01/28 09:53:02 rene 1.9.86.1: more gcc 4.3.0 things

--- bridges/source/cpp_uno/gcc3_linux_powerpc/cpp2uno.cxx
+++ bridges/source/cpp_uno/gcc3_linux_powerpc/cpp2uno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cpp2uno.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:48:08 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:43:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,8 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_bridges.hxx"
+
+#include <string.h>
 
 #include <com/sun/star/uno/genfunc.hxx>
 #include <uno/data.h>
