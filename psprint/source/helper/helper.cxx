fb0ea3786c0df92ab9467683dbc356bbf8aa8d7a
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:35:18 +0000

    INTEGRATION: CWS gcc430two (1.29.98); FILE MERGED 2008/01/28 09:53:09 rene 1.29.98.1: more gcc 4.3.0 things

--- psprint/source/helper/helper.cxx
+++ psprint/source/helper/helper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helper.cxx,v $
  *
- *  $Revision: 1.29 $
+ *  $Revision: 1.30 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 12:34:47 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:35:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,6 +39,7 @@
 #include <cstring>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include <psprint/helper.hxx>
 #include <tools/string.hxx>
