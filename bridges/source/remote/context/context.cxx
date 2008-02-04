5ebdae45f4f1ce7a749dff5e22c80ff5e08e936f
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:44:03 +0000

    INTEGRATION: CWS gcc430two (1.10.86); FILE MERGED 2008/01/28 09:53:02 rene 1.10.86.1: more gcc 4.3.0 things

--- bridges/source/remote/context/context.cxx
+++ bridges/source/remote/context/context.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: context.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:58:56 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:44:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,6 +36,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_bridges.hxx"
 #include <stdio.h>
+#include <string.h>
 #include <list>
 #include <hash_map>
 #include <utility>
