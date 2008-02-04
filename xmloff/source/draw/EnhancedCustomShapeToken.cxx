ab35e2dded810707461f8a8f2ad8a09db803a067
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:51:45 +0000

    INTEGRATION: CWS gcc430two (1.9.268); FILE MERGED 2008/01/28 09:53:15 rene 1.9.268.1: more gcc 4.3.0 things

--- xmloff/source/draw/EnhancedCustomShapeToken.cxx
+++ xmloff/source/draw/EnhancedCustomShapeToken.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnhancedCustomShapeToken.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 10:24:28 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:51:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,7 @@
 #include <osl/mutex.hxx>
 #endif
 #include <hash_map>
+#include <string.h>
 
 namespace xmloff { namespace EnhancedCustomShapeToken {
 
