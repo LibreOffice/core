a652fd1337a2892928d984844e79ab26c6350b65
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:47:57 +0000

    INTEGRATION: CWS gcc430two (1.7.24); FILE MERGED 2008/01/28 09:53:10 rene 1.7.24.1: more gcc 4.3.0 things

--- sdext/source/minimizer/pppoptimizertoken.cxx
+++ sdext/source/minimizer/pppoptimizertoken.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pppoptimizertoken.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: sj $ $Date: 2007-08-17 09:38:07 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:47:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,6 +40,7 @@
 #include <osl/mutex.hxx>
 #endif
 #include <hash_map>
+#include <string.h>
 
 struct TCheck
 {
