979cfc03fb3d3a2ffb780fdc61decba08bec10cb
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-05 14:45:15 +0000

    INTEGRATION: CWS calcsolver (1.8.350); FILE MERGED 2007/12/20 09:42:54 nn 1.8.350.1: exceptions

--- sc/source/ui/docshell/makefile.mk
+++ sc/source/ui/docshell/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: rt $ $Date: 2006-07-25 09:50:14 $
+#   last change: $Author: ihi $ $Date: 2008-02-05 15:45:15 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -105,6 +105,7 @@ SLOFILES =  \
 
 
 EXCEPTIONSFILES= \
+        $(SLO)$/docsh.obj \
         $(SLO)$/docsh8.obj \
         $(SLO)$/dbdocimp.obj
 
