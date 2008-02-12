01e449f33542c5c07bab27d7443bdc1430560f8a
Vladimir Glazounov <vg@openoffice.org>
2008-02-12 12:54:40 +0000

    INTEGRATION: CWS obr07 (1.1.2); FILE ADDED 2008/01/24 13:57:26 obr 1.1.2.1: moved MacOS X specific resources (cursor/nib) to vcl/aqua

---
+++ vcl/aqua/source/res/makefile.mk
@@ -0,0 +1,58 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+TARGET=aquares
+
+# --- Settings -------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files ----------------------------------------------------
+
+.IF "$(OS)"!="MACOSX"
+
+dummy:
+        @echo "Nothing to build for OS $(OS)"
+
+.ELSE           # "$(OS)"!="MACOSX"
+
+ZIPFLAGS = -r
+ZIP1TARGET = osxres
+#ZIP1DIR = 
+ZIP1LIST = MainMenu.nib cursors
+
+# --- Targets --------------------------------------------------
+
+.INCLUDE : target.mk
+
+.ENDIF		# "$(OS)"!="MACOSX"	
+
