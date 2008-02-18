45b604989c1be8578469596a93d12d5019086a49
Rüdiger Timm <rt@openoffice.org>
2008-02-18 14:50:12 +0000

    INTEGRATION: CWS chart19 (1.1.2); FILE ADDED 2008/01/10 10:06:12 iha 1.1.2.3: #i16776# starting angle for pie charts - use dial control 2008/01/08 12:24:32 iha 1.1.2.2: #i37823# clockwise pie charts 2007/12/15 12:55:27 iha 1.1.2.1: #i16776# starting angle for pie charts

---
+++ chart2/source/controller/dialogs/tp_PolarOptions.hxx
@@ -0,0 +1,72 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tp_PolarOptions.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-02-18 15:50:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _CHART2_TP_POLAROPTIONS_HXX
+#define _CHART2_TP_POLAROPTIONS_HXX
+
+#include <sfx2/tabdlg.hxx>
+#include <vcl/fixed.hxx>
+#include <vcl/button.hxx>
+#include <vcl/field.hxx>
+#include <svx/dialcontrol.hxx>
+
+//.............................................................................
+namespace chart
+{
+//.............................................................................
+
+class PolarOptionsTabPage : public SfxTabPage
+{
+
+public:
+    PolarOptionsTabPage(Window* pParent, const SfxItemSet& rInAttrs );
+    virtual ~PolarOptionsTabPage();
+
+    static SfxTabPage* Create(Window* pParent, const SfxItemSet& rInAttrs);
+    virtual BOOL FillItemSet(SfxItemSet& rOutAttrs);
+    virtual void Reset(const SfxItemSet& rInAttrs);
+
+private:
+    CheckBox         m_aCB_Clockwise;
+    FixedLine        m_aFL_StartingAngle;
+    svx::DialControl m_aAngleDial;
+    FixedText        m_aFT_Degrees;
+    NumericField     m_aNF_StartingAngle;
+};
+
+//.............................................................................
+} //namespace chart
+//.............................................................................
+
+#endif
