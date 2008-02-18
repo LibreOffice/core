4c6fcb70225820f97fb1d04330baad1cd1ffe038
Rüdiger Timm <rt@openoffice.org>
2008-02-18 13:50:30 +0000

    INTEGRATION: CWS macosxdnd (1.11.4); FILE MERGED 2007/12/04 21:22:50 tra 1.11.4.2: Clipboard transition to Cocoa 2007/11/13 10:11:19 tra 1.11.4.1: #i79620# experimental Carbon based D&D implementation, no need to optimize - will be replaced by Cocoa version

--- dtrans/source/aqua/makefile.mk
+++ dtrans/source/aqua/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-05 09:13:35 $
+#   last change: $Author: rt $ $Date: 2008-02-18 14:50:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,6 @@ LIBTARGET=NO
 COMP1TYPELIST=$(TARGET)
 .ENDIF # "$(GUIBASE)"=="aqua"
 .ENDIF # "$(OS)"=="MACOSX" 
-USE_BOUNDCHK=
 
 # --- Settings -----------------------------------------------------
 
@@ -52,27 +51,28 @@ USE_BOUNDCHK=
 
 # ------------------------------------------------------------------
 
-CFLAGS+=-fconstant-cfstrings
-
 .IF "$(OS)"!="MACOSX"
-
 dummy:
     @echo "Nothing to build for this platform"
- 
-.ELSE		# "$(OS)"!="MACOSX"
-
+.ELSE # "$(OS)"!="MACOSX"
 .IF "$(GUIBASE)"!="aqua"
 dummy:
     @echo "Nothing to build for GUIBASE $(GUIBASE)"
 .ELSE
 
+CFLAGSCXX+=-fconstant-cfstrings -x objective-c++ -fobjc-exceptions
+
 SLOFILES= \
-        $(SLO)$/aqua_service.obj	\
         $(SLO)$/aqua_clipboard.obj \
         $(SLO)$/DataFlavorMapping.obj \
         $(SLO)$/OSXTransferable.obj \
         $(SLO)$/HtmlFmtFlt.obj \
-        $(SLO)$/PictToBmpFlt.obj
+        $(SLO)$/PictToBmpFlt.obj \
+        $(SLO)$/DropTarget.obj \
+        $(SLO)$/DragSource.obj \
+        $(SLO)$/service_entry.obj \
+        $(SLO)$/DragSourceContext.obj \
+        $(SLO)$/DragActionConversion.obj
 
 SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
 
@@ -81,21 +81,22 @@ SHL1STDLIBS= \
         $(CPPULIB) 	\
         $(CPPUHELPERLIB) \
         -framework Carbon \
-        -framework QuickTime
+        -framework QuickTime \
+        -framework Cocoa
 
 SHL1DEPN=
 SHL1IMPLIB=	i$(SHL1TARGET)
 
 SHL1OBJS=	$(SLOFILES)
 
-APP1NOSAL=TRUE
-APP1TARGET=test_aquacb
-APP1OBJS=$(SLO)$/test_aquacb.obj
-APP1STDLIBS= \
-        $(SALLIB)	\
-        $(CPPULIB) 	\
-        $(CPPUHELPERLIB) \
-        -framework Cocoa
+#APP1NOSAL=TRUE
+#APP1TARGET=test_aquacb
+#APP1OBJS=$(SLO)$/test_aquacb.obj
+#APP1STDLIBS= 
+#$(SALLIB)
+#$(CPPULIB)
+#$(CPPUHELPERLIB)
+#-framework Cocoa
 
 # --- Targets ------------------------------------------------------
 
