c7eef20eccc5d47a5744fb5e358604cca42ac7c4
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:29:42 +0000

    INTEGRATION: CWS supdremove02 (1.2.10); FILE MERGED 2008/01/28 14:38:00 rt 1.2.10.1: #i85482# Remove UPD from library name

--- oox/util/makefile.mk
+++ oox/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2008-01-17 08:06:10 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:29:42 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@ LIB1FILES=	\
 
 # --- Shared-Library -----------------------------------------------
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 SHL1IMPLIB= i$(SHL1TARGET)
 SHL1USE_EXPORTS=ordinal
 
