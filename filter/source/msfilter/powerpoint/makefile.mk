390658b771633a40c70300038cd2eb490c96d9a0
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:29:41 +0000

    INTEGRATION: CWS supdremove02 (1.5.276); FILE MERGED 2008/01/25 16:13:32 rt 1.5.276.1: #i85482# Remove UPD from library names.

--- filter/source/msfilter/powerpoint/makefile.mk
+++ filter/source/msfilter/powerpoint/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 14:13:29 $
+#   last change: $Author: obo $ $Date: 2008-02-25 16:29:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ SLOFILES=	$(SLO)$/pptimporter.obj						\
 
 # --- Library -----------------------------------
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 SHL1STDLIBS=\
             $(SOTLIB)			\
             $(TOOLSLIB)			\
