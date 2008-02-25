2b3d36ae64124f3cdb154a64cc57d7983f1f8abe
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:49:04 +0000

    INTEGRATION: CWS supdremove02 (1.5.474); FILE MERGED 2008/01/29 15:45:20 rt 1.5.474.1: #i85484# RSCUPDVER not needed here.

--- desktop/source/migration/makefile.mk
+++ desktop/source/migration/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: hr $ $Date: 2006-01-27 16:20:47 $
+#   last change: $Author: obo $ $Date: 2008-02-25 16:49:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,6 @@ ENABLE_EXCEPTIONS=TRUE
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-RSCUPDVER=$(RSCREVISION)(SV$(UPD)$(UPDMINOR))
 
 # --- Files --------------------------------------------------------
 
