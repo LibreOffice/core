ae5b7923ebaa5207fab6ab8e660c6a8e59484360
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:31:21 +0000

    INTEGRATION: CWS supdremove02 (1.11.146); FILE MERGED 2008/01/31 13:49:28 rt 1.11.146.1: #i85482# Remove UPD from resource name.

--- filter/source/pdf/pdfdialog.cxx
+++ filter/source/pdf/pdfdialog.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pdfdialog.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: ihi $ $Date: 2007-03-26 11:14:16 $
+ *  last change: $Author: obo $ $Date: 2008-02-25 16:31:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -110,7 +110,6 @@ PDFDialog::PDFDialog( const Reference< XMultiServiceFactory > &rxMSF ) :
 {
     ByteString aResMgrName( "pdffilter" );
 
-    aResMgrName.Append( ByteString::CreateFromInt32( SOLARUPD ) );
     mpResMgr = ResMgr::CreateResMgr( aResMgrName.GetBuffer(), Application::GetSettings().GetUILocale() );
 }
 
