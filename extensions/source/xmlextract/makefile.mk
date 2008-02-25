aeefec8ca7d2250ae2b6b212dfc10578ab96615e
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:36:06 +0000

    INTEGRATION: CWS supdremove02 (1.7.450); FILE MERGED 2008/01/25 16:00:12 rt 1.7.450.1: #i85482# Remove UPD from library names.

--- extensions/source/xmlextract/makefile.mk
+++ extensions/source/xmlextract/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 20:42:42 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:36:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ SLOFILES=	$(SLO)$/xmxuno.obj									\
 
 # --- Library -----------------------------------
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 SHL1IMPLIB=	i$(SHL1TARGET)
 
 SHL1VERSIONMAP=exports.map
