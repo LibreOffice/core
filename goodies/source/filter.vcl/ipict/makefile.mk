9db87ada5bf8c4a83ff34e9949e24dc7151eae68
Oliver Bolte <obo@openoffice.org>
2008-02-25 16:22:59 +0000

    INTEGRATION: CWS supdremove02 (1.11.84); FILE MERGED 2008/01/28 15:42:37 rt 1.11.84.1: #i85482# Remove UPD from library name

--- goodies/source/filter.vcl/ipict/makefile.mk
+++ goodies/source/filter.vcl/ipict/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 14:58:54 $
+#   last change: $Author: obo $ $Date: 2008-02-25 17:22:59 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ SLOFILES =  $(SLO)$/ipict.obj
 
 # ==========================================================================
 
-SHL1TARGET=     ipt$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=     ipt$(DLLPOSTFIX)
 SHL1IMPLIB=     ipict
 SHL1STDLIBS=    $(VCLLIB) $(TOOLSLIB) $(SALLIB)
 SHL1LIBS=       $(SLB)$/ipict.lib
