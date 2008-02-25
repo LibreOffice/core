6e5714c7232afd66d02968b18211857260bac491
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:30:46 +0000

    INTEGRATION: CWS supdremove02 (1.5.40); FILE MERGED 2008/01/28 13:12:31 rt 1.5.40.1: #i85482# Remove UPD from library name

--- UnoControls/source/base/makefile.mk
+++ UnoControls/source/base/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 09:16:22 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:30:46 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -54,7 +54,7 @@ SLOFILES=		$(SLO)$/multiplexer.obj				\
 #LIB1TARGET= 	$(SLB)$/$(TARGET).lib
 #LIB1OBJFILES=	$(SLOFILES)
 
-#SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+#SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 
 #SHL1STDLIBS=\
 #		$(ONELIB)	 \
