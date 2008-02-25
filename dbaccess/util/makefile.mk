31ff2ba14be6f5ade3bb718871a5f187aa4489d3
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:24:53 +0000

    INTEGRATION: CWS supdremove02 (1.32.50); FILE MERGED 2008/01/28 08:04:42 rt 1.32.50.1: #i85482# Remove UPD from library name

--- dbaccess/util/makefile.mk
+++ dbaccess/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.32 $
+#   $Revision: 1.33 $
 #
-#   last change: $Author: ihi $ $Date: 2007-11-21 16:17:26 $
+#   last change: $Author: obo $ $Date: 2008-02-25 16:24:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,7 @@ LIB1FILES=\
         $(SLB)$/core_resource.lib	\
         $(SLB)$/dbashared.lib	
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 
 SHL1STDLIBS= \
         $(SVTOOLLIB) \
@@ -106,7 +106,7 @@ LIB2FILES=\
         $(SLB)$/app.lib				\
         $(SLB)$/uicontrols.lib
 
-SHL2TARGET=$(TARGET2)$(UPD)$(DLLPOSTFIX)
+SHL2TARGET=$(TARGET2)$(DLLPOSTFIX)
 
 SHL2STDLIBS= \
         $(SVXLIB)				\
@@ -177,7 +177,7 @@ LIB3FILES=\
         $(SLB)$/sdbtmisc.lib \
         $(SLB)$/sdbtshared.lib
 
-SHL3TARGET=$(TARGET3)$(UPD)$(DLLPOSTFIX)
+SHL3TARGET=$(TARGET3)$(DLLPOSTFIX)
 
 SHL3STDLIBS= \
         $(CPPULIB) \
