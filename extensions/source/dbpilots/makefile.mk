bff5e419eec9be0aa41641dc5a95b40d3b2c0d2d
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:32:47 +0000

    INTEGRATION: CWS supdremove02 (1.8.310); FILE MERGED 2008/01/25 15:58:27 rt 1.8.310.1: #i85482# Remove UPD from library names.

--- extensions/source/dbpilots/makefile.mk
+++ extensions/source/dbpilots/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: hr $ $Date: 2006-06-19 10:44:37 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:32:47 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -82,7 +82,7 @@ SRC1FILES=			gridpages.src	\
 RESLIB1NAME=dbp
 RESLIB1SRSFILES= $(SRS)$/dbp.srs
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 SHL1VERSIONMAP= $(TARGET).map
 
 SHL1STDLIBS= \
