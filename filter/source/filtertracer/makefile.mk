cc8a34c95dcd9b7d9b1563543165640b8c3f361d
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:28:59 +0000

    INTEGRATION: CWS supdremove02 (1.5.276); FILE MERGED 2008/01/25 16:13:31 rt 1.5.276.1: #i85482# Remove UPD from library names.

--- filter/source/filtertracer/makefile.mk
+++ filter/source/filtertracer/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: vg $ $Date: 2006-05-24 14:13:00 $
+#   last change: $Author: obo $ $Date: 2008-02-25 16:28:59 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -51,7 +51,7 @@ SLOFILES=	$(SLO)$/filtertracer.obj					\
 
 # --- Library -----------------------------------
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 SHL1STDLIBS=$(CPPULIB)			\
             $(CPPUHELPERLIB)	\
             $(SALLIB)			\
