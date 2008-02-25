2bbdc7764f3ee4b353d7bbc33b8b2763365595b4
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:33:40 +0000

    INTEGRATION: CWS supdremove02 (1.6.450); FILE MERGED 2008/01/25 15:58:28 rt 1.6.450.1: #i85482# Remove UPD from library names.

--- extensions/source/preload/makefile.mk
+++ extensions/source/preload/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 19:59:15 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:33:40 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,7 @@ RESLIB1NAME=preload
 RESLIB1IMAGES=$(PRJ)$/res
 RESLIB1SRSFILES= $(SRS)$/preload.srs
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 SHL1VERSIONMAP= $(TARGET).map
 
 SHL1STDLIBS= \
