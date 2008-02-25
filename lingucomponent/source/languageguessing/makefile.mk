7c99d7b84e0f57f9f569f9879dcf82c291ee5627
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:44:03 +0000

    INTEGRATION: CWS supdremove02 (1.3.36); FILE MERGED 2008/01/31 16:01:50 rt 1.3.36.2: RESYNC: (1.3-1.4); FILE MERGED 2008/01/28 16:15:24 rt 1.3.36.1: #i85482# Remove UPD from library name

--- lingucomponent/source/languageguessing/makefile.mk
+++ lingucomponent/source/languageguessing/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2008-01-29 14:42:04 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:44:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ SLOFILES = \
         $(SLO)$/simpleguesser.obj
 
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 
 SHL1STDLIBS= \
         $(CPPUHELPERLIB)        \
