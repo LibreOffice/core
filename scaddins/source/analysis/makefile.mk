a8f0b97ed73402d4db5906eec69704a5dd61c9ca
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:37:49 +0000

    INTEGRATION: CWS supdremove02 (1.17.18); FILE MERGED 2008/01/28 10:40:57 rt 1.17.18.1: #i85482# Remove UPD from library name

--- scaddins/source/analysis/makefile.mk
+++ scaddins/source/analysis/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: vg $ $Date: 2007-09-20 14:28:20 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:37:49 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -107,7 +107,7 @@ SRC1FILES =  \
 
 # --- Library -----------------------------------
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 SHL1OBJS=$(SLOFILES)
 SHL1STDLIBS= \
         $(VCLLIB)			\
