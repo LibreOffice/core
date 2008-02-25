db4ec3c7bde382c3ceea00b25eb1e4a48ebcebdc
Oliver Bolte <obo@openoffice.org>
2008-02-25 15:21:15 +0000

    INTEGRATION: CWS supdremove02 (1.9.106); FILE MERGED 2008/01/28 13:58:45 rt 1.9.106.1: #i85482# Remove UPD from library name

--- svx/source/unodialogs/textconversiondlgs/makefile.mk
+++ svx/source/unodialogs/textconversiondlgs/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: ihi $ $Date: 2007-11-21 18:51:50 $
+#   last change: $Author: obo $ $Date: 2008-02-25 16:21:15 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@ RESLIB1SRSFILES= $(SRS)$/$(TARGET).srs
 .IF "$(GUI)" == "OS2"
 SHL1TARGET= txcnvdlg
 .ELSE
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 .ENDIF
 SHL1VERSIONMAP= export.map
 
