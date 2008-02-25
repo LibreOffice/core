10999d6d2fbca7dd98fa4d05f286d8e930105b97
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:44:30 +0000

    INTEGRATION: CWS supdremove02 (1.17.32); FILE MERGED 2008/01/28 16:15:24 rt 1.17.32.1: #i85482# Remove UPD from library name

--- lingucomponent/source/spellcheck/spell/makefile.mk
+++ lingucomponent/source/spellcheck/spell/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: vg $ $Date: 2007-09-20 14:31:44 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:44:30 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -85,7 +85,7 @@ SLOFILES=	\
         $(SLO)$/sspellimp.obj
 
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 
 SHL1STDLIBS= \
         $(CPPULIB) 	 \
