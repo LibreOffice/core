fef4cb484d3f55298a2c774683c4fa9fc89b5c8c
Oliver Bolte <obo@openoffice.org>
2008-02-25 16:23:41 +0000

    INTEGRATION: CWS supdremove02 (1.10.84); FILE MERGED 2008/01/28 15:42:38 rt 1.10.84.1: #i85482# Remove UPD from library name

--- goodies/source/filter.vcl/iras/makefile.mk
+++ goodies/source/filter.vcl/iras/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 14:59:17 $
+#   last change: $Author: obo $ $Date: 2008-02-25 17:23:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ SLOFILES =	$(SLO)$/iras.obj
 
 # ==========================================================================
 
-SHL1TARGET= 	ira$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= 	ira$(DLLPOSTFIX)
 SHL1IMPLIB= 	iras
 SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB)
 SHL1LIBS=		$(SLB)$/iras.lib
--- goodies/source/filter.vcl/itga/makefile.mk
+++ goodies/source/filter.vcl/itga/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 14:59:41 $
+#   last change: $Author: obo $ $Date: 2008-02-25 17:23:41 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ SLOFILES =	$(SLO)$/itga.obj
 
 # ==========================================================================
 
-SHL1TARGET= 	itg$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= 	itg$(DLLPOSTFIX)
 SHL1IMPLIB= 	itga
 SHL1STDLIBS=	$(VCLLIB) $(TOOLSLIB) $(SALLIB)
 SHL1LIBS=		$(SLB)$/itga.lib
