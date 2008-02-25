a123a27239557b3ab8b857c91ecd9a2bd46ed8d9
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:43:51 +0000

    INTEGRATION: CWS supdremove02 (1.16.14); FILE MERGED 2008/01/28 16:15:23 rt 1.16.14.1: #i85482# Remove UPD from library name

--- lingucomponent/source/hyphenator/altlinuxhyph/hyphen/makefile.mk
+++ lingucomponent/source/hyphenator/altlinuxhyph/hyphen/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.16 $
+#   $Revision: 1.17 $
 #
-#   last change: $Author: kz $ $Date: 2007-12-12 13:23:48 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:43:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,7 @@ SLOFILES=	\
         $(SLO)$/hyphenimp.obj
 
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 
 SHL1STDLIBS= \
         $(CPPULIB) 	 \
