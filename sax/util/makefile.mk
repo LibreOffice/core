5ea67ae8d13011ee3f58696072c6af530ae0def3
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:30:25 +0000

    INTEGRATION: CWS supdremove02 (1.6.6); FILE MERGED 2008/01/28 14:28:47 rt 1.6.6.1: #i85482# Remove UPD from library name

--- sax/util/makefile.mk
+++ sax/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: obo $ $Date: 2008-01-10 12:53:16 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:30:25 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,7 @@ LIB1FILES=\
 
 # sax
 
-SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= $(TARGET)$(DLLPOSTFIX)
 SHL1IMPLIB= i$(TARGET)
 SHL1USE_EXPORTS=ordinal
 
