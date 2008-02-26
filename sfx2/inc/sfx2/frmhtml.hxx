bbed55bc17bcd02a502bbb141058b7a986dc2fac
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:57:46 +0000

    INTEGRATION: CWS custommeta (1.2.142); FILE MERGED 2008/01/16 17:04:21 mst 1.2.142.1: interface change: Out_DocInfo and ParseMetaOptions - sfx2/inc/sfx2/sfxhtml.hxx, source/bastyp/sfxhtml.cxx:   + SfxHTMLParser::ParseMetaOptions takes a XDocumentProperties parameter     instead of SfxDocumentInfo; also remove the now obsolete counter parameter - sfx2/inc/sfx2/{frmhtml.hxx,frmhtmlw.hxx}, source/bastyp/frmhtmlw.cxx:   + SfxFrameHTMLWriter::Out_DocInfo takes a XDocumentProperties parameter     instead of SfxDocumentInfo

--- sfx2/inc/sfx2/frmhtml.hxx
+++ sfx2/inc/sfx2/frmhtml.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmhtml.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 21:21:06 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 14:57:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,7 +48,6 @@
 #include <sfx2/sfxhtml.hxx>
 
 class SfxFrameHTMLParser;
-class SfxDocumentInfo;
 class _SfxFrameHTMLContexts;
 class SfxFrameSetObjectShell;
 class SvULongs;
