9c90b1fceef85a760d71875da2f504a280560bb5
Oliver Bolte <obo@openoffice.org>
2008-02-26 09:41:49 +0000

    INTEGRATION: CWS swcrossref01_DEV300 (1.7.444); FILE MERGED 2007/10/01 13:41:36 od 1.7.444.2: RESYNC: (1.7-1.8); FILE MERGED merge conflict resolved. 2007/09/28 10:24:01 od 1.7.444.1: #i81002# adjustments due to refactoring of class <SwBookmark>

--- sw/source/core/undo/unbkmk.cxx
+++ sw/source/core/undo/unbkmk.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unbkmk.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:30:16 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 10:41:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@ SwUndoBookmark::SwUndoBookmark( SwUndoId nUndoId, const SwBookmark& rBkmk )
     : SwUndo( nUndoId )
 {
     BYTE nType = SwHstryBookmark::BKMK_POS;
-    if( rBkmk.GetOtherPos() )
+    if( rBkmk.GetOtherBookmarkPos() )
         nType |= SwHstryBookmark::BKMK_OTHERPOS;
     pHBookmark = new SwHstryBookmark( rBkmk, nType );
 }
