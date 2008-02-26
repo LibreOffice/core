cd6f21fa5d8f89d8355da23db53178b99fe5116d
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:47:29 +0000

    INTEGRATION: CWS custommeta (1.4.62); FILE MERGED 2008/02/01 10:36:11 mst 1.4.62.3: RESYNC: (1.5-1.6); FILE MERGED 2007/12/10 10:28:05 mst 1.4.62.2: RESYNC: (1.4-1.5); FILE MERGED 2007/12/07 14:05:56 mst 1.4.62.1: - unoxml/source/dom/document.{hxx,cxx}:   implement interface css::xml::sax::XSAXSerializable - unoxml/source/dom/*.{hxx,cxx}:   add virtual method saxify() on all subclasses of CNode   add missing implementations of getLocalName()

--- unoxml/source/dom/cdatasection.hxx
+++ unoxml/source/dom/cdatasection.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cdatasection.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2007-12-06 10:58:28 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 14:47:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -55,6 +55,10 @@ namespace DOM
         CCDATASection(const xmlNodePtr aNodePtr);
 
     public:
+
+        virtual void SAL_CALL saxify(
+            const Reference< XDocumentHandler >& i_xHandler);
+
         virtual Reference< XText > SAL_CALL splitText(sal_Int32 offset)
              throw (RuntimeException)
         {
