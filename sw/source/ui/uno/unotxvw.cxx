88082aa57ac6740816c74c7664f3c8a3916877fd
Oliver Bolte <obo@openoffice.org>
2008-02-26 09:49:19 +0000

    INTEGRATION: CWS swcrossref01_DEV300 (1.64.162); FILE MERGED 2007/12/03 09:24:41 od 1.64.162.3: RESYNC: (1.65-1.66); FILE MERGED 2007/10/01 15:34:07 od 1.64.162.2: RESYNC: (1.64-1.65); FILE MERGED 2007/09/28 10:24:04 od 1.64.162.1: #i81002# adjustments due to refactoring of class <SwBookmark>

--- sw/source/ui/uno/unotxvw.cxx
+++ sw/source/ui/uno/unotxvw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unotxvw.cxx,v $
  *
- *  $Revision: 1.66 $
+ *  $Revision: 1.67 $
  *
- *  last change: $Author: rt $ $Date: 2007-11-12 16:33:29 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 10:49:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1519,7 +1519,7 @@ void SwXTextViewCursor::gotoRange(
         else if(pRange && pRange->GetBookmark())
         {
             SwBookmark* pBkm = pRange->GetBookmark();
-            pSrcNode = &pBkm->GetPos().nNode.GetNode();
+            pSrcNode = &pBkm->GetBookmarkPos().nNode.GetNode();
         }
         else if (pPara && pPara->GetCrsr())
         {
