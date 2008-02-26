1fc4c2fe0c4ce49a57b07f4e29076f308244a6b2
Oliver Bolte <obo@openoffice.org>
2008-02-26 06:33:14 +0000

    INTEGRATION: CWS cropmaster2000_DEV300 (1.2.318); FILE MERGED 2007/11/24 21:15:46 cl 1.2.318.1: #i83933# added interactive graphic cropping

--- svx/inc/svx/svdtypes.hxx
+++ svx/inc/svx/svdtypes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdtypes.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 16:28:33 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 07:33:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,10 @@ enum SdrDragMode
 
     // new modes for interactive transparence and gradient tools
     SDRDRAG_TRANSPARENCE,
-    SDRDRAG_GRADIENT
+    SDRDRAG_GRADIENT,
+
+    // new mode for interactive cropping of graphics
+    SDRDRAG_CROP
 };
 
 
