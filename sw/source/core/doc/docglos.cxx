ad0eb35e54c2c606d9afc8f207df962d5a28dd4c
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:06:28 +0000

    INTEGRATION: CWS custommeta (1.8.78); FILE MERGED 2008/01/17 16:06:05 mst 1.8.78.1: - sw/source/core/doc/{doccomp.cxx,docglbl.cxx,docglos.cxx},   sw/source/core/graphic/ndgrf.cxx:   + adapt to SwDoc interface change   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/core/doc/makefile.mk:   + enable exceptions for dbgoutsw.cxx, doccomp.cxx, docglbl.cxx, docglos.cxx

--- sw/source/core/doc/docglos.cxx
+++ sw/source/core/doc/docglos.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docglos.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:35:50 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 14:06:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,6 +58,74 @@
 #ifndef _CRSRSH_HXX
 #include <crsrsh.hxx>
 #endif
+#include <docsh.hxx>
+
+#include <com/sun/star/document/XDocumentPropertiesSupplier.hpp>
+#include <com/sun/star/document/XDocumentProperties.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/beans/XPropertySetInfo.hpp>
+
+
+using namespace ::com::sun::star;
+
+
+/// copy document properties via public interface
+static void lcl_copyDocumentProperties(
+        uno::Reference<document::XDocumentProperties> i_xSource,
+        uno::Reference<document::XDocumentProperties> i_xTarget) {
+    DBG_ASSERT(i_xSource.is(), "null reference");
+    DBG_ASSERT(i_xTarget.is(), "null reference");
+
+    i_xTarget->setAuthor(i_xSource->getAuthor());
+    i_xTarget->setGenerator(i_xSource->getGenerator());
+    i_xTarget->setCreationDate(i_xSource->getCreationDate());
+    i_xTarget->setTitle(i_xSource->getTitle());
+    i_xTarget->setSubject(i_xSource->getSubject());
+    i_xTarget->setDescription(i_xSource->getDescription());
+    i_xTarget->setKeywords(i_xSource->getKeywords());
+    i_xTarget->setLanguage(i_xSource->getLanguage());
+    i_xTarget->setModifiedBy(i_xSource->getModifiedBy());
+    i_xTarget->setModificationDate(i_xSource->getModificationDate());
+    i_xTarget->setPrintedBy(i_xSource->getPrintedBy());
+    i_xTarget->setPrintDate(i_xSource->getPrintDate());
+    i_xTarget->setTemplateName(i_xSource->getTemplateName());
+    i_xTarget->setTemplateURL(i_xSource->getTemplateURL());
+    i_xTarget->setTemplateDate(i_xSource->getTemplateDate());
+    i_xTarget->setAutoloadURL(i_xSource->getAutoloadURL());
+    i_xTarget->setAutoloadSecs(i_xSource->getAutoloadSecs());
+    i_xTarget->setDefaultTarget(i_xSource->getDefaultTarget());
+    i_xTarget->setDocumentStatistics(i_xSource->getDocumentStatistics());
+    i_xTarget->setEditingCycles(i_xSource->getEditingCycles());
+    i_xTarget->setEditingDuration(i_xSource->getEditingDuration());
+
+    uno::Reference<beans::XPropertySet> xSourceUDSet(
+        i_xSource->getUserDefinedProperties(), uno::UNO_QUERY_THROW);
+    uno::Reference<beans::XPropertyContainer> xTargetUD(
+        i_xTarget->getUserDefinedProperties());
+    uno::Reference<beans::XPropertySet> xTargetUDSet(xTargetUD,
+        uno::UNO_QUERY_THROW);
+    uno::Sequence<beans::Property> tgtprops
+        = xTargetUDSet->getPropertySetInfo()->getProperties();
+    for (sal_Int32 i = 0; i < tgtprops.getLength(); ++i) {
+        try {
+            xTargetUD->removeProperty(tgtprops [i].Name);
+        } catch (uno::Exception &) {
+            // ignore
+        }
+    }
+    try {
+        uno::Reference<beans::XPropertySetInfo> xSetInfo
+            = xSourceUDSet->getPropertySetInfo();
+        uno::Sequence<beans::Property> srcprops = xSetInfo->getProperties();
+        for (sal_Int32 i = 0; i < srcprops.getLength(); ++i) {
+            ::rtl::OUString name = srcprops[i].Name;
+            xTargetUD->addProperty(name, srcprops[i].Attributes,
+                xSourceUDSet->getPropertyValue(name));
+        }
+    } catch (uno::Exception &) {
+        // ignore
+    }
+}
 
 /* -----------------22.07.99 11:47-------------------
     Description: inserts an AutoText block
@@ -80,7 +148,22 @@ BOOL SwDoc::InsertGlossary( SwTextBlocks& rBlock, const String& rEntry,
 
             // alle FixFelder aktualisieren. Dann aber auch mit der
             // richtigen DocInfo!
-            pGDoc->SetDocumentInfo( *GetDocumentInfo() );
+            // FIXME: UGLY: Because we cannot limit the range in which to do
+            // field updates, we must update the fixed fields at the glossary
+            // entry document.
+            // To be able to do this, we copy the document properties of the
+            // target document to the glossary document
+            DBG_ASSERT(GetDocShell(), "no SwDocShell");
+            uno::Reference<document::XDocumentPropertiesSupplier> xDPS(
+                GetDocShell()->GetModel(), uno::UNO_QUERY_THROW);
+            uno::Reference<document::XDocumentProperties> xDocProps
+                = xDPS->getDocumentProperties();
+            DBG_ASSERT(pGDoc->GetDocShell(), "no SwDocShell at glossary");
+            uno::Reference<document::XDocumentPropertiesSupplier> xGlosDPS(
+                pGDoc->GetDocShell()->GetModel(), uno::UNO_QUERY_THROW);
+            uno::Reference<document::XDocumentProperties> xGlosDocProps
+                = xGlosDPS->getDocumentProperties();
+            lcl_copyDocumentProperties(xDocProps, xGlosDocProps);
             pGDoc->SetFixFields(false, NULL);
 
             //StartAllAction();
