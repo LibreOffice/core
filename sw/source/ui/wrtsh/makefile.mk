5843eeff80dc110a48ded5d064da6e6c7f07e6d4
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:27:32 +0000

    INTEGRATION: CWS custommeta (1.6.1032); FILE MERGED 2008/01/17 17:35:20 mst 1.6.1032.1: - sw/source/ui/app/{docsh.cxx,docsh2.cxx,docshini.cxx}:   + adapt to SwDoc interface change   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/uiview/{srcview.cxx,view.cxx}:   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/uiview/makefile.mk:   + enable exceptions for view.cxx - sw/source/ui/wrtsh/wrtsh2.cxx:   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/wrtsh/makefile.mk:   + enable exceptions for wrtsh2.cxx

--- sw/source/ui/wrtsh/makefile.mk
+++ sw/source/ui/wrtsh/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 11:39:20 $
+#   last change: $Author: obo $ $Date: 2008-02-26 14:27:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,8 @@ SLOFILES =  \
         $(SLO)$/wrtundo.obj
 
 EXCEPTIONSFILES = \
-                $(SLO)$/wrtsh1.obj
+        $(SLO)$/wrtsh1.obj \
+        $(SLO)$/wrtsh2.obj
 
 # --- Tagets -------------------------------------------------------
 
