44c7a53d5ccf5e46825eab31661640e26e43d5ea
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:35:28 +0000

    INTEGRATION: CWS custommeta (1.21.112); FILE MERGED 2008/01/21 11:56:27 mst 1.21.112.1: - svx/source/dialog/{scriptdlg.cxx,selector.cxx}:   + remove unneeded includes

--- svx/source/dialog/scriptdlg.cxx
+++ svx/source/dialog/scriptdlg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: scriptdlg.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: kz $ $Date: 2007-10-09 15:18:03 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 14:35:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,6 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/frame/XDesktop.hpp>
 #include <com/sun/star/script/XInvocation.hpp>
-#include <com/sun/star/document/XDocumentInfoSupplier.hpp>
 #include <com/sun/star/script/provider/XScriptProviderSupplier.hpp>
 #include <com/sun/star/script/provider/XScriptProvider.hpp>
 #include <com/sun/star/script/browse/BrowseNodeTypes.hpp>
