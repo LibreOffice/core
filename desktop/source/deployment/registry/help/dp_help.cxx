15f6b8a5e71d06e5dbbd5d11ce1d9bfa093f5b15
Oliver Bolte <obo@openoffice.org>
2008-02-26 06:47:39 +0000

    INTEGRATION: CWS ab45_DEV300 (1.2.76); FILE MERGED 2008/02/18 08:27:11 ab 1.2.76.1: #i83624# Adapted error message for tree file errors

--- desktop/source/deployment/registry/help/dp_help.cxx
+++ desktop/source/deployment/registry/help/dp_help.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dp_help.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2007-11-19 13:09:06 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 07:47:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -368,8 +368,11 @@ void BackendImpl::implProcessHelp
                             rtl::OUString aDecodedFile = rtl::Uri::decode( aErrorInfo.m_aXMLParsingFile,
                                 rtl_UriDecodeWithCharset, RTL_TEXTENCODING_UTF8 );
                             aErrStr += aDecodedFile;
-                            aErrStr += rtl::OUString::createFromAscii( ", line " );
-                            aErrStr += ::rtl::OUString::valueOf( aErrorInfo.m_nXMLParsingLine );
+                            if( aErrorInfo.m_nXMLParsingLine != -1 )
+                            {
+                                aErrStr += rtl::OUString::createFromAscii( ", line " );
+                                aErrStr += ::rtl::OUString::valueOf( aErrorInfo.m_nXMLParsingLine );
+                            }
                         }
                     }
 
