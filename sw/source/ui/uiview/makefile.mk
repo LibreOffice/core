7689c2ffca6a13f0f862eda952f743e9600a96b5
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:25:52 +0000

    INTEGRATION: CWS custommeta (1.10.78); FILE MERGED 2008/01/17 17:35:20 mst 1.10.78.1: - sw/source/ui/app/{docsh.cxx,docsh2.cxx,docshini.cxx}:   + adapt to SwDoc interface change   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/uiview/{srcview.cxx,view.cxx}:   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/uiview/makefile.mk:   + enable exceptions for view.cxx - sw/source/ui/wrtsh/wrtsh2.cxx:   + use XDocumentProperties instead of SfxDocumentInfo - sw/source/ui/wrtsh/makefile.mk:   + enable exceptions for wrtsh2.cxx

--- sw/source/ui/uiview/makefile.mk
+++ sw/source/ui/uiview/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.10 $
+#   $Revision: 1.11 $
 #
-#   last change: $Author: hr $ $Date: 2007-09-27 12:35:12 $
+#   last change: $Author: obo $ $Date: 2008-02-26 14:25:52 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,6 +56,7 @@ EXCEPTIONSFILES= \
         $(SLO)$/srcview.obj \
         $(SLO)$/swcli.obj  \
         $(SLO)$/uivwimp.obj \
+        $(SLO)$/view.obj \
         $(SLO)$/view2.obj \
         $(SLO)$/viewling.obj
 
