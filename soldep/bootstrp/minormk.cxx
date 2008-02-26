3f23f0d4eb15d205dacb47a43fb68d91148d22f0
Oliver Bolte <obo@openoffice.org>
2008-02-26 07:09:29 +0000

    INTEGRATION: CWS rt26 (1.2.36); FILE MERGED 2008/02/13 16:01:31 rt 1.2.36.1: #i86065# Take own header appdef.hxx.

--- soldep/bootstrp/minormk.cxx
+++ soldep/bootstrp/minormk.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: minormk.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 16:32:13 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 08:09:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
  ************************************************************************/
 
 #include "minormk.hxx"
-#include <bootstrp/appdef.hxx>
+#include <appdef.hxx>
 #include <tools/fsys.hxx>
 #include <tools/geninfo.hxx>
 #include "prodmap.hxx"
