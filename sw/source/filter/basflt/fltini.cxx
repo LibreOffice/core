02128b8c6ae21becb5723472391ae4e7ba13fd08
Oliver Bolte <obo@openoffice.org>
2008-02-26 13:16:39 +0000

    INTEGRATION: CWS custommeta (1.53.76); FILE MERGED 2008/02/01 10:30:10 mst 1.53.76.2: RESYNC: (1.53-1.54); FILE MERGED 2008/01/18 11:02:13 mst 1.53.76.1: - sw/inc/swgpar.hxx, sw/source/filter/basflt/fltini.cxx:   + remove unused file swgpar.hxx

--- sw/source/filter/basflt/fltini.cxx
+++ sw/source/filter/basflt/fltini.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fltini.cxx,v $
  *
- *  $Revision: 1.54 $
+ *  $Revision: 1.55 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 09:24:46 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 14:16:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -100,9 +100,6 @@
 #endif
 #ifndef _FLTINI_HXX
 #include <fltini.hxx>
-#endif
-#ifndef _SWGPAR_HXX
-#include <swgpar.hxx>           // fuer den SW/G Parser
 #endif
 #ifndef _HINTS_HXX //autogen
 #include <hints.hxx>
