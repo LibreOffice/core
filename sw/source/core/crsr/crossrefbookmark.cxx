d27351da817207c331b7758c810ce207b3cfe136
Oliver Bolte <obo@openoffice.org>
2008-02-28 10:15:30 +0000

    #i10000# problem with unix platforms

--- sw/source/core/crsr/crossrefbookmark.cxx
+++ sw/source/core/crsr/crossrefbookmark.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: crossrefbookmark.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 10:34:06 $
+ *  last change: $Author: obo $ $Date: 2008-02-28 11:15:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,7 +59,7 @@ SwCrossRefBookmark::SwCrossRefBookmark( const SwPosition& aPos,
             "<SwCrossRefBookmark::SwCrossRefBookmark(..)> - cross-reference bookmark doesn't mark text node." )
     ASSERT( GetBookmarkPos().nContent.GetIndex() == 0,
             "<SwCrossRefBookmark::SwCrossRefBookmark(..)> - cross-reference bookmark doesn't mark start of text node." )
-    ASSERT( mnSubType == IDocumentBookmarkAccess::CrossReferenceBookmarkSubType::HEADING ||
+    ASSERT( mnSubType == IDocumentBookmarkAccess::HEADING ||
             bookmarkfunc::isNumItemCrossRefBookmarkName( rName ),
             "<SwCrossRefBookmark::SwCrossRefBookmark(..)> - name doesn't fit. Serious issue, please inform OD!" );
 }
