8bf100e62aaf8d3fb5e396df278f8f5f5bc36311
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:34:16 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.2.70); FILE MERGED 2008/02/21 09:57:24 oj 1.2.70.1: #i85225# fixes found with PMD

--- reportdesign/java/com/sun/star/report/pentaho/model/PageLayout.java
+++ reportdesign/java/com/sun/star/report/pentaho/model/PageLayout.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageLayout.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:07 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:34:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,6 @@
 package com.sun.star.report.pentaho.model;
 
 import org.jfree.report.structure.Section;
-import org.jfree.report.structure.Element;
 import com.sun.star.report.pentaho.OfficeNamespaces;
 
 /**
