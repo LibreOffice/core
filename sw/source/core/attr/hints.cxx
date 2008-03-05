ea4638fc7ef0eccd34efbb29a04452d64797f3fe
Kurt Zenker <kz@openoffice.org>
2008-03-05 15:52:01 +0000

    INTEGRATION: CWS swnewlistlevelattrs_DEV300 (1.13.142); FILE MERGED 2008/01/31 12:39:23 od 1.13.142.1: #i85348# refactoring: adjust includes

--- sw/source/core/attr/hints.cxx
+++ sw/source/core/attr/hints.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hints.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:25:47 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 16:52:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,6 +36,9 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
 
+#ifndef _HINTS_HXX
+#include <hints.hxx>
+#endif
 
 #ifndef _COM_SUN_STAR_I18N_SCRIPTTYPE_HDL_
 #include <com/sun/star/i18n/ScriptType.hdl>
