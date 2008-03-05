014ccaa4e73878f2b6f953685faf1e317bb57aa7
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:27:50 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.3.22); FILE MERGED 2008/02/21 09:57:23 oj 1.3.22.3: #i85225# fixes found with PMD 2008/02/20 10:52:55 oj 1.3.22.2: format changes 2008/02/13 07:04:21 oj 1.3.22.1: #i85225# impl chart readhandler and oleproducer

--- reportdesign/java/com/sun/star/report/SOImageService.java
+++ reportdesign/java/com/sun/star/report/SOImageService.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SOImageService.java,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 13:43:19 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:27:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 package com.sun.star.report;
 
 import java.awt.Dimension;
@@ -58,143 +57,132 @@ import com.sun.star.uno.XComponentContext;
  */
 public class SOImageService implements ImageService
 {
-  private XGraphicProvider m_xGraphicProvider;
 
-  /**
-   * Creates a new instance of SOImageService
-   */
-  public SOImageService(final XComponentContext xCompContext)
-      throws ReportExecutionException, com.sun.star.uno.Exception
-  {
-    if (xCompContext == null)
+    private XGraphicProvider m_xGraphicProvider;
+
+    /**
+     * Creates a new instance of SOImageService
+     */
+    public SOImageService(final XComponentContext xCompContext)
+            throws ReportExecutionException, com.sun.star.uno.Exception
     {
-      throw new ReportExecutionException();
-    }
-
-
-    final XMultiComponentFactory m_xMCF = xCompContext.getServiceManager();
-    m_xGraphicProvider = (XGraphicProvider) UnoRuntime.queryInterface(XGraphicProvider.class,
-        m_xMCF.createInstanceWithContext("com.sun.star.graphic.GraphicProvider", xCompContext));
-
-    if (m_xGraphicProvider == null)
-    {
-      throw new ReportExecutionException("There is no graphic-provider available.");
-    }
-  }
-
-  public Dimension getImageSize(final InputStream image) throws ReportExecutionException
-  {
-    return getImageSize(new InputStreamToXInputStreamAdapter(image));
-  }
-
-  private Dimension getImageSize(final XInputStream image) throws ReportExecutionException
-  {
-    final Dimension dim = new Dimension();
-    try
-    {
-      final PropertyValue[] value = new PropertyValue[1];
-      value[0] = new PropertyValue();
-      value[0].Name = "InputStream";
-      value[0].Value = image;
-
-      final XPropertySet xImage = (XPropertySet)UnoRuntime.queryInterface( XPropertySet.class,
-        m_xGraphicProvider.queryGraphic( value ) );
-
-      if (xImage != null)
-      {
-        final XPropertySetInfo xInfo = xImage.getPropertySetInfo();
-        if (xInfo.hasPropertyByName("Size100thMM"))
+        if (xCompContext == null)
         {
-          Size imageSize = (Size) xImage.getPropertyValue("Size100thMM");
-          dim.setSize(imageSize.Width, imageSize.Height);
-          if (dim.height == 0 && dim.width == 0)
-          {
-            imageSize = (Size) xImage.getPropertyValue("SizePixel");
-            final int dpi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
-            final double fac = 2540 / dpi;
-            dim.setSize(imageSize.Width * fac, imageSize.Height * fac);
-          }
+            throw new ReportExecutionException();
         }
-        else if (xInfo.hasPropertyByName("SizePixel"))
+
+
+        final XMultiComponentFactory m_xMCF = xCompContext.getServiceManager();
+        m_xGraphicProvider = (XGraphicProvider) UnoRuntime.queryInterface(XGraphicProvider.class,
+                m_xMCF.createInstanceWithContext("com.sun.star.graphic.GraphicProvider", xCompContext));
+
+        if (m_xGraphicProvider == null)
         {
-          final Size imageSize = (Size) xImage.getPropertyValue("SizePixel");
-          final int dpi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
-          final double fac = 2540 / dpi;
-          dim.setSize(imageSize.Width * fac, imageSize.Height * fac);
+            throw new ReportExecutionException("There is no graphic-provider available.");
         }
-      }
     }
-    catch (UnknownPropertyException ex)
-    {
-      throw new ReportExecutionException("Failed to query Image-Size", ex);
-    }
-    catch (WrappedTargetException ex)
-    {
-      throw new ReportExecutionException("Failed to query Image-Size", ex);
-    }
-    catch (com.sun.star.lang.IllegalArgumentException ex)
-    {
-      throw new ReportExecutionException("Failed to query Image-Size", ex);
-    }
-    catch (IOException ex)
-    {
-      throw new ReportExecutionException("Failed to query Image-Size", ex);
-    }
-    return dim;
-  }
 
-  public Dimension getImageSize(final byte[] image) throws ReportExecutionException
-  {
-    return getImageSize(new ByteArrayToXInputStreamAdapter(image));
-  }
-
-  private String getMimeType(final XInputStream image) throws ReportExecutionException
-  {
-    String mimeType = null;
-    try
+    public Dimension getImageSize(final InputStream image) throws ReportExecutionException
     {
-      final PropertyValue[] value = new PropertyValue[] { new PropertyValue() };
-      value[0].Name = "InputStream";
-      value[0].Value = image;
+        return getImageSize(new InputStreamToXInputStreamAdapter(image));
+    }
 
-      final XPropertySet xImage = (XPropertySet)UnoRuntime.queryInterface(XPropertySet.class,
-          m_xGraphicProvider.queryGraphic(value));
-
-      if (xImage != null)
-      {
-        final XPropertySetInfo xInfo = xImage.getPropertySetInfo();
-        if (xInfo.hasPropertyByName("MimeType"))
+    private Dimension getImageSize(final XInputStream image) throws ReportExecutionException
+    {
+        final Dimension dim = new Dimension();
+        try
         {
-          mimeType = (String) xImage.getPropertyValue("MimeType");
+            final PropertyValue[] value = new PropertyValue[1];
+            value[0] = new PropertyValue();
+            value[0].Name = "InputStream";
+            value[0].Value = image;
+
+            final XPropertySet xImage = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class,
+                    m_xGraphicProvider.queryGraphic(value));
+
+            if (xImage != null)
+            {
+                final XPropertySetInfo xInfo = xImage.getPropertySetInfo();
+                if (xInfo.hasPropertyByName("Size100thMM"))
+                {
+                    Size imageSize = (Size) xImage.getPropertyValue("Size100thMM");
+                    dim.setSize(imageSize.Width, imageSize.Height);
+                    if (dim.height == 0 && dim.width == 0)
+                    {
+                        imageSize = (Size) xImage.getPropertyValue("SizePixel");
+                        final int dpi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
+                        final double fac = 2540 / dpi;
+                        dim.setSize(imageSize.Width * fac, imageSize.Height * fac);
+                    }
+                }
+                else if (xInfo.hasPropertyByName("SizePixel"))
+                {
+                    final Size imageSize = (Size) xImage.getPropertyValue("SizePixel");
+                    final int dpi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
+                    final double fac = 2540 / dpi;
+                    dim.setSize(imageSize.Width * fac, imageSize.Height * fac);
+                }
+            }
         }
-      }
+        catch (Exception ex)
+        {
+            throw new ReportExecutionException("Failed to query Image-Size", ex);
+        }
+        return dim;
     }
-    catch (UnknownPropertyException ex)
-    {
-      throw new ReportExecutionException();
-    }
-    catch (WrappedTargetException ex)
-    {
-      throw new ReportExecutionException();
-    }
-    catch (com.sun.star.lang.IllegalArgumentException ex)
-    {
-      throw new ReportExecutionException();
-    }
-    catch (IOException ex)
-    {
-      throw new ReportExecutionException();
-    }
-    return mimeType;
-  }
 
-  public String getMimeType(final InputStream image) throws ReportExecutionException
-  {
-    return getMimeType(new InputStreamToXInputStreamAdapter(image));
-  }
+    public Dimension getImageSize(final byte[] image) throws ReportExecutionException
+    {
+        return getImageSize(new ByteArrayToXInputStreamAdapter(image));
+    }
 
-  public String getMimeType(final byte[] image) throws ReportExecutionException
-  {
-    return getMimeType(new ByteArrayToXInputStreamAdapter(image));
-  }
+    private String getMimeType(final XInputStream image) throws ReportExecutionException
+    {
+        String mimeType = null;
+        try
+        {
+            final PropertyValue[] value = new PropertyValue[]{new PropertyValue()};
+            value[0].Name = "InputStream";
+            value[0].Value = image;
+
+            final XPropertySet xImage = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class,
+                    m_xGraphicProvider.queryGraphic(value));
+
+            if (xImage != null)
+            {
+                final XPropertySetInfo xInfo = xImage.getPropertySetInfo();
+                if (xInfo.hasPropertyByName("MimeType"))
+                {
+                    mimeType = (String) xImage.getPropertyValue("MimeType");
+                }
+            }
+        }
+        catch (UnknownPropertyException ex)
+        {
+            throw new ReportExecutionException();
+        }
+        catch (WrappedTargetException ex)
+        {
+            throw new ReportExecutionException();
+        }
+        catch (com.sun.star.lang.IllegalArgumentException ex)
+        {
+            throw new ReportExecutionException();
+        }
+        catch (IOException ex)
+        {
+            throw new ReportExecutionException();
+        }
+        return mimeType;
+    }
+
+    public String getMimeType(final InputStream image) throws ReportExecutionException
+    {
+        return getMimeType(new InputStreamToXInputStreamAdapter(image));
+    }
+
+    public String getMimeType(final byte[] image) throws ReportExecutionException
+    {
+        return getMimeType(new ByteArrayToXInputStreamAdapter(image));
+    }
 }
