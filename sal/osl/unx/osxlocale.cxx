6ccec360dda099a6da2ec9360353a6f928ad63bd
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:44:52 +0000

    INTEGRATION: CWS aquavcl05_DEV300 (1.2.94); FILE MERGED 2008/01/29 09:08:08 ericb 1.2.94.1: #i81024# fix leak

--- sal/osl/unx/osxlocale.cxx
+++ sal/osl/unx/osxlocale.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: osxlocale.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-03 12:49:13 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:44:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -128,6 +128,9 @@ extern "C" int macxp_OSXConvertCFEncodingToIANACharSetName( char *buffer, unsign
 
     sCFEncodingName = CFStringConvertEncodingToIANACharSetName( cfEncoding );
     CFStringGetCString( sCFEncodingName, buffer, bufferLen, cfEncoding );
+
+    if ( sCFEncodingName )
+        CFRelease( sCFEncodingName );
 
     return( noErr );
 }
