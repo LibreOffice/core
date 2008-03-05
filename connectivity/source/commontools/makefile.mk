d3c35773f6edece383a1e503e4eec63143fbf62e
Kurt Zenker <kz@openoffice.org>
2008-03-05 15:42:24 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.32.32); FILE MERGED 2008/02/18 13:01:33 oj 1.32.32.2: RESYNC: (1.32-1.33); FILE MERGED 2008/01/24 14:47:19 oj 1.32.32.1: #i85225# mved fromforms

--- connectivity/source/commontools/makefile.mk
+++ connectivity/source/commontools/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.33 $
+#   $Revision: 1.34 $
 #
-#   last change: $Author: rt $ $Date: 2008-01-30 07:48:18 $
+#   last change: $Author: kz $ $Date: 2008-03-05 16:42:24 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -85,6 +85,8 @@ EXCEPTIONSFILES=\
         $(SLO)$/statementcomposer.obj                   \
         $(SLO)$/RowFunctionParser.obj                   \
         $(SLO)$/sqlerror.obj                            \
+        $(SLO)$/filtermanager.obj                       \
+        $(SLO)$/parameters.obj							\
         $(SLO)$/formattedcolumnvalue.obj
 
 SLOFILES=\
