c7ec57b80901065d86a052cc42ee559e4f1c42bb
Kurt Zenker <kz@openoffice.org>
2008-03-05 17:08:05 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.4.20); FILE MERGED 2008/02/19 06:42:51 oj 1.4.20.2: #i85225# ongoing work 2008/01/24 12:39:32 oj 1.4.20.1: #i85225# changes for chart

--- reportdesign/source/shared/uistrings.cxx
+++ reportdesign/source/shared/uistrings.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: uistrings.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: ihi $ $Date: 2007-11-20 19:05:50 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 18:08:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,5 +61,7 @@ namespace rptui
     IMPLEMENT_CONSTASCII_USTRING(DBOVERLAPPEDCONTROL            , "OverlappedControl");
     IMPLEMENT_CONSTASCII_USTRING(PROPERTY_FORMULALIST           , "FormulaList");
     IMPLEMENT_CONSTASCII_USTRING(PROPERTY_SCOPE                 , "Scope");
+    IMPLEMENT_CONSTASCII_USTRING(PROPERTY_CHARTTYPE             , "ChartType");
+    IMPLEMENT_CONSTASCII_USTRING(PROPERTY_PREVIEW_COUNT         , "RowLimit");
 }
 
