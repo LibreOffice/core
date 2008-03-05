bbdec9a0bbccdf3a16d736fa6b6cc1b77b764a68
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:40:34 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.2.70); FILE MERGED 2008/02/20 10:52:56 oj 1.2.70.2: format changes 2008/02/13 07:04:23 oj 1.2.70.1: #i85225# impl chart readhandler and oleproducer

--- reportdesign/java/com/sun/star/report/pentaho/parser/data/DataStyleReadHandler.java
+++ reportdesign/java/com/sun/star/report/pentaho/parser/data/DataStyleReadHandler.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DataStyleReadHandler.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: vg $ $Date: 2008-02-12 13:10:40 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:40:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-
 package com.sun.star.report.pentaho.parser.data;
 
 import java.util.ArrayList;
@@ -50,87 +48,87 @@ import org.xml.sax.SAXException;
 
 public class DataStyleReadHandler extends ElementReadHandler
 {
-  private DataStyle dataStyle;
-  private ArrayList children;
-  private boolean hasCData;
 
-  public DataStyleReadHandler(final boolean hasCData)
-  {
-    this.hasCData = hasCData;
-    this.dataStyle = new DataStyle();
-    this.children = new ArrayList();
-  }
+    private DataStyle dataStyle;
+    private ArrayList children;
+    private boolean hasCData;
 
-  /**
-   * Returns the handler for a child element.
-   *
-   * @param tagName the tag name.
-   * @param atts    the attributes.
-   * @return the handler or null, if the tagname is invalid.
-   * @throws org.xml.sax.SAXException if there is a parsing error.
-   */
-  protected XmlReadHandler getHandlerForChild(final String uri,
-                                              final String tagName,
-                                              final Attributes atts)
-      throws SAXException
-  {
-    if (OfficeNamespaces.DATASTYLE_NS.equals(uri) || OfficeNamespaces.STYLE_NS.equals(uri) )
+    public DataStyleReadHandler(final boolean hasCData)
     {
-      final DataStyleReadHandler xrh = new DataStyleReadHandler("text".equals(tagName) || "currency-symbol".equals(tagName));
-      children.add(xrh);
-      return xrh;
+        this.hasCData = hasCData;
+        this.dataStyle = new DataStyle();
+        this.children = new ArrayList();
     }
 
-    return null;
-  }
-
-  /**
-   * This method is called to process the character data between element tags.
-   *
-   * @param ch     the character buffer.
-   * @param start  the start index.
-   * @param length the length.
-   * @throws org.xml.sax.SAXException if there is a parsing error.
-   */
-  public void characters(final char[] ch, final int start, final int length)
-      throws SAXException
-  {
-    if (hasCData)
+    /**
+     * Returns the handler for a child element.
+     *
+     * @param tagName the tag name.
+     * @param atts    the attributes.
+     * @return the handler or null, if the tagname is invalid.
+     * @throws org.xml.sax.SAXException if there is a parsing error.
+     */
+    protected XmlReadHandler getHandlerForChild(final String uri,
+            final String tagName,
+            final Attributes atts)
+            throws SAXException
     {
-      children.add(new StaticText(new String(ch, start, length)));
-    }
-  }
+        if (OfficeNamespaces.DATASTYLE_NS.equals(uri) || OfficeNamespaces.STYLE_NS.equals(uri))
+        {
+            final DataStyleReadHandler xrh = new DataStyleReadHandler("text".equals(tagName) || "currency-symbol".equals(tagName));
+            children.add(xrh);
+            return xrh;
+        }
 
-  /**
-   * Done parsing.
-   *
-   * @throws org.xml.sax.SAXException if there is a parsing error.
-   */
-  protected void doneParsing() throws SAXException
-  {
-    for (int i = 0; i < children.size(); i++)
+        return null;
+    }
+
+    /**
+     * This method is called to process the character data between element tags.
+     *
+     * @param ch     the character buffer.
+     * @param start  the start index.
+     * @param length the length.
+     * @throws org.xml.sax.SAXException if there is a parsing error.
+     */
+    public void characters(final char[] ch, final int start, final int length)
+            throws SAXException
     {
-      final Object o = children.get(i);
-      if (o instanceof ElementReadHandler)
-      {
-        final ElementReadHandler handler = (ElementReadHandler) o;
-        dataStyle.addNode(handler.getElement());
-      }
-      else if (o instanceof StaticText)
-      {
-        dataStyle.addNode((StaticText) o);
-      }
+        if (hasCData)
+        {
+            children.add(new StaticText(new String(ch, start, length)));
+        }
     }
-  }
 
+    /**
+     * Done parsing.
+     *
+     * @throws org.xml.sax.SAXException if there is a parsing error.
+     */
+    protected void doneParsing() throws SAXException
+    {
+        for (int i = 0; i < children.size(); i++)
+        {
+            final Object o = children.get(i);
+            if (o instanceof ElementReadHandler)
+            {
+                final ElementReadHandler handler = (ElementReadHandler) o;
+                dataStyle.addNode(handler.getElement());
+            }
+            else if (o instanceof StaticText)
+            {
+                dataStyle.addNode((StaticText) o);
+            }
+        }
+    }
 
-  public DataStyle getDataStyle()
-  {
-    return dataStyle;
-  }
+    public DataStyle getDataStyle()
+    {
+        return dataStyle;
+    }
 
-  public Element getElement()
-  {
-    return dataStyle;
-  }
+    public Element getElement()
+    {
+        return dataStyle;
+    }
 }
