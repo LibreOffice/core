172b0d90b4150c003109ddb0fdeda54de2056635
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:10:45 +0000

    INTEGRATION: CWS swnewlistlevelattrs_DEV300 (1.8.142); FILE MERGED 2008/02/13 11:18:32 od 1.8.142.1: #i85348# adjustments due to changed method name at class <SwDoc>

--- sw/source/core/undo/unspnd.cxx
+++ sw/source/core/undo/unspnd.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unspnd.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:33:07 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:10:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -167,7 +167,7 @@ void SwUndoSplitNode::Undo( SwUndoIter& rUndoIter )
                 rPam.SetMark();
                 rPam.GetPoint()->nContent = pTNd->GetTxt().Len();
 
-                pDoc->RstTxtAttr( rPam, TRUE );
+                pDoc->RstTxtAttrs( rPam, TRUE );
                 pHistory->TmpRollback( pDoc, 0, FALSE );
             }
         }
