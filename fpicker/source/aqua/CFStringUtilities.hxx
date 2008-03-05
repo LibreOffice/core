f371c0dc538504c94c5b6114209576b40e43deb8
Kurt Zenker <kz@openoffice.org>
2008-03-05 15:34:31 +0000

    INTEGRATION: CWS aquafilepicker02_DEV300 (1.2.40); FILE MERGED 2008/01/14 08:23:23 fheckl 1.2.40.3: Code cleanup and some implementation details 2008/01/02 18:25:58 fheckl 1.2.40.2: issue #80399 more Cocoa 2007/11/18 19:36:36 fheckl 1.2.40.1: initial support for Cocoa version

--- fpicker/source/aqua/CFStringUtilities.hxx
+++ fpicker/source/aqua/CFStringUtilities.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: CFStringUtilities.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2007-07-11 10:57:30 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 16:34:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,16 @@
 
 #include <premac.h>
 #include <Carbon/Carbon.h>
+#include <Cocoa/Cocoa.h>
 #include <postmac.h>
 
+#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
+#endif
+
+#ifndef _SAL_TYPES_H_
 #include <sal/types.h>
+#endif
 
 #ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
@@ -55,6 +61,7 @@ enum InfoType {
 
 rtl::OUString CFStringToOUString(const CFStringRef sOrig);
 rtl::OUString FSRefToOUString(FSRef fsRef, InfoType info = FULLPATH);
+rtl::OUString CFURLRefToOUString(CFURLRef aUrlRef, InfoType info);
 CFStringRef CFStringCreateWithOUString(const rtl::OUString& aString);
 
 //debug utils
@@ -83,6 +90,13 @@ inline void DBG_PRINT_ENTRY(const char * classname, const char * methodname, con
 #endif
 }
 
+inline void DBG_PRINT_ENTRY(const char * classname, const char * methodname, const char* param1, const NSString* value1) {
+    OSL_TRACE(">>> %s::%s%s%s =", classname, methodname, PARAMFILLER, param1);
+#if OSL_DEBUG_LEVEL > 1
+    NSLog(value1);
+#endif
+}
+
 inline void DBG_PRINT_ENTRY(const char * classname, const char * methodname, const char* param1, const int value1) {
     OSL_TRACE(">>> %s::%s%s%s = %d", classname, methodname, PARAMFILLER, param1, value1);
 }
@@ -107,6 +121,13 @@ inline void DBG_PRINT_ENTRY(const char * classname, const char * methodname, con
     OSL_TRACE(">>> %s::%s%s%s = %d%s%s =", classname, methodname, PARAMFILLER, param1, value1, PARAMFILLER, param2, value2);
 #if OSL_DEBUG_LEVEL > 1
     CFShow(value2);
+#endif
+}
+
+inline void DBG_PRINT_ENTRY(const char * classname, const char * methodname, const char* param1, const int value1, const char* param2, const NSString* value2) {
+    OSL_TRACE(">>> %s::%s%s%s = %d%s%s =", classname, methodname, PARAMFILLER, param1, value1, PARAMFILLER, param2, value2);
+#if OSL_DEBUG_LEVEL > 1
+    NSLog(value2);
 #endif
 }
 
@@ -140,6 +161,13 @@ inline void DBG_PRINT_EXIT(const char * classname, const char * methodname, cons
     OSL_TRACE("<<< %s::%s%sreturnValue = ", classname, methodname, PARAMFILLER);
 #if OSL_DEBUG_LEVEL > 1
     CFShow(retVal);
+#endif
+}
+
+inline void DBG_PRINT_EXIT(const char * classname, const char * methodname, const NSString* retVal) {
+    OSL_TRACE("<<< %s::%s%sreturnValue = ", classname, methodname, PARAMFILLER);
+#if OSL_DEBUG_LEVEL > 1
+    NSLog(retVal);
 #endif
 }
 
