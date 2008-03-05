3b915d420eaaa8edeac6d70ca16d48a629d9d4e4
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:28:39 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.2.70); FILE MERGED 2008/02/21 09:57:23 oj 1.2.70.2: #i85225# fixes found with PMD 2008/02/13 07:04:22 oj 1.2.70.1: #i85225# impl chart readhandler and oleproducer

--- reportdesign/java/com/sun/star/report/pentaho/DefaultNameGenerator.java
+++ reportdesign/java/com/sun/star/report/pentaho/DefaultNameGenerator.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DefaultNameGenerator.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:04 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:28:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,8 +33,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-
 package com.sun.star.report.pentaho;
 
 import java.io.IOException;
@@ -43,76 +41,115 @@ import com.sun.star.report.OutputRepository;
 
 public class DefaultNameGenerator
 {
-  private OutputRepository outputRepository;
 
-  public DefaultNameGenerator(final OutputRepository outputRepository)
-  {
-    if (outputRepository == null)
-    {
-      throw new NullPointerException();
-    }
-    this.outputRepository = outputRepository;
-  }
+    private OutputRepository outputRepository;
 
-  /**
-   * Generates a new, unique name for storing resources in the output repository. Assuming that proper synchronization
-   * has been applied, the generated name will be unique within that repository.
-   *
-   * @param namePrefix a user defined name for that resource.
-   * @param mimeType   the mime type of the resource to be stored in the repository.
-   * @return the generated, fully qualified name.
-   */
-  public String generateName(final String namePrefix, final String mimeType)
-      throws IOException
-  {
-    final String name;
-    if (namePrefix != null)
+    public DefaultNameGenerator(final OutputRepository outputRepository)
     {
-      name = namePrefix;
-    }
-    else
-    {
-      name = "file";
+        if (outputRepository == null)
+        {
+            throw new NullPointerException();
+        }
+        this.outputRepository = outputRepository;
     }
 
-    final String suffix = getSuffixForType(mimeType);
-    final String firstFileName = name + "." + suffix;
-    if (outputRepository.exists(firstFileName) == false)
+    public String generateName(final String namePrefix, final String mimeType)
+            throws IOException
     {
-      return firstFileName;
-    }
-    int counter = 0;
-    while (true)
-    {
-      if (counter < 0) // wraparound should not happen..
-      {
-        throw new IOException();
-      }
-      final String filename = name + counter + "." + suffix;
-      if (outputRepository.exists(filename) == false)
-      {
-        return filename;
-      }
-      counter += 1;
-    }
-  }
-
-  protected String getSuffixForType(final String mimeType)
-  {
-    if ("image/png".equals(mimeType))
-    {
-      return "png";
-    }
-    if ("image/jpeg".equals(mimeType))
-    {
-      return "jpg";
-    }
-    if ("image/gif".equals(mimeType))
-    {
-      return "gif";
+        return generateName(namePrefix, mimeType, true);
     }
 
-    // todo ... use a flexible mapping ...
-    return "dat";
-  }
+    public String generateStorageName(final String namePrefix, final String mimeType)
+            throws IOException
+    {
+        return generateName(namePrefix, mimeType, false);
+    }
+
+    /**
+     * Generates a new, unique name for storing resources in the output repository. Assuming that proper synchronization
+     * has been applied, the generated name will be unique within that repository.
+     *
+     * @param namePrefix a user defined name for that resource.
+     * @param mimeType   the mime type of the resource to be stored in the repository.
+     * @return the generated, fully qualified name.
+     */
+    private String generateName(final String namePrefix, final String mimeType, boolean isStream)
+            throws IOException
+    {
+        final String name;
+        if (namePrefix != null)
+        {
+            name = namePrefix;
+        }
+        else
+        {
+            name = "file";
+        }
+
+        String firstFileName = name;
+        String suffix = null;
+        if (mimeType != null)
+        {
+            suffix = getSuffixForType(mimeType);
+            firstFileName += "." + suffix;
+        }
+        boolean exists = false;
+        if (isStream)
+        {
+            exists = outputRepository.exists(firstFileName);
+        }
+        else
+        {
+            exists = outputRepository.existsStorage(firstFileName);
+        }
+        if (exists == false)
+        {
+            return firstFileName;
+        }
+        int counter = 0;
+        while (true)
+        {
+            if (counter < 0) // wraparound should not happen..
+            {
+                throw new IOException();
+            }
+            String filename = name + counter;
+            if (suffix != null)
+            {
+                filename += "." + suffix;
+            }
+            if (isStream)
+            {
+                exists = outputRepository.exists(filename);
+            }
+            else
+            {
+                exists = outputRepository.existsStorage(filename);
+            }
+            if (exists == false)
+            {
+                return filename;
+            }
+            counter += 1;
+        }
+    }
+
+    protected String getSuffixForType(final String mimeType)
+    {
+        if ("image/png".equals(mimeType))
+        {
+            return "png";
+        }
+        if ("image/jpeg".equals(mimeType))
+        {
+            return "jpg";
+        }
+        if ("image/gif".equals(mimeType))
+        {
+            return "gif";
+        }
+
+        // todo ... use a flexible mapping ...
+        return "dat";
+    }
 }
