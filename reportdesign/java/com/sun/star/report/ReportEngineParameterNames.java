87f48ffc754a5c9ae6cf46bdfa731eda00ad05d6
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:25:18 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.2.70); FILE MERGED 2008/02/19 06:42:48 oj 1.2.70.2: #i85225# ongoing work 2008/02/13 07:04:21 oj 1.2.70.1: #i85225# impl chart readhandler and oleproducer

--- reportdesign/java/com/sun/star/report/ReportEngineParameterNames.java
+++ reportdesign/java/com/sun/star/report/ReportEngineParameterNames.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ReportEngineParameterNames.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:03 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:25:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,6 +50,10 @@ public class ReportEngineParameterNames
   public static final String OUTPUT_REPOSITORY = "output.repository";
   public static final String INPUT_DATASOURCE_FACTORY = "input.datasource-factory";
   public static final String IMAGE_SERVICE = "ImageService";
+  public static final String INPUT_REPORTJOB_FACTORY = "input.reportjob-factory";
+  public static final String INPUT_MASTER_COLUMNS = "input.master-columns";
+  public static final String INPUT_MASTER_VALUES = "input.master-values";
+  public static final String INPUT_DETAIL_COLUMNS = "input.detail-columns";
   public static final String MIMETYPE = "output.mimetype";
 
   private ReportEngineParameterNames ()
