2b2014f114c877aadbc5d9dc86176cf37201c99c
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:28:52 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.3.62); FILE MERGED 2008/02/21 09:57:23 oj 1.3.62.2: #i85225# fixes found with PMD 2008/02/20 10:52:55 oj 1.3.62.1: format changes

--- reportdesign/java/com/sun/star/report/pentaho/OfficeNamespaces.java
+++ reportdesign/java/com/sun/star/report/pentaho/OfficeNamespaces.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: OfficeNamespaces.java,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2007-08-03 09:48:43 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:28:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,11 +33,7 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-
 package com.sun.star.report.pentaho;
-
-import java.io.File;
 
 /**
  * Creation-Date: Feb 22, 2007, 1:53:29 PM
@@ -47,38 +43,38 @@ import java.io.File;
 public class OfficeNamespaces
 {
 
-  public static final String OFFICE_NS = "urn:oasis:names:tc:opendocument:xmlns:office:1.0";
-  public static final String TABLE_NS = "urn:oasis:names:tc:opendocument:xmlns:table:1.0";
-  public static final String TEXT_NS = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
-  public static final String STYLE_NS = "urn:oasis:names:tc:opendocument:xmlns:style:1.0";
-  public static final String DRAWING_NS = "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0";
-  public static final String FO_NS = "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0";
-  public static final String XLINK_NS = "http://www.w3.org/1999/xlink";
-  public static final String PURL_NS = "http://purl.org/dc/elements/1.1/";
-  public static final String META_NS = "urn:oasis:names:tc:opendocument:xmlns:meta:1.0";
-  public static final String DATASTYLE_NS = "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0";
-  public static final String SVG_NS = "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0";
-  public static final String CHART_NS = "urn:oasis:names:tc:opendocument:xmlns:chart:1.0";
-  public static final String DR3D_NS = "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0";
-  public static final String MATHML_NS = "http://www.w3.org/1998/Math/MathML";
-  public static final String FORM_NS = "urn:oasis:names:tc:opendocument:xmlns:form:1.0";
-  public static final String SCRIPT_NS = "urn:oasis:names:tc:opendocument:xmlns:script:1.0";
-  public static final String OO2004_NS = "http://openoffice.org/2004/office";
-  public static final String OOW2004_NS = "http://openoffice.org/2004/writer";
-  public static final String OOC2004_NS = "http://openoffice.org/2004/calc";
-  public static final String XML_EVENT_NS = "http://www.w3.org/2001/xml-events";
-  public static final String XFORMS_NS = "http://www.w3.org/2002/xforms";
-  public static final String XSD_NS = "http://www.w3.org/2001/XMLSchema";
-  public static final String XSI_NS = "http://www.w3.org/2001/XMLSchema-instance";
-  public static final String OOREPORT_NS = "http://openoffice.org/2005/report";
-  public static final String CONFIG = "urn:oasis:names:tc:opendocument:xmlns:config:1.0";
-  public static final String INTERNAL_NS = "http://reporting.pentaho.org/namespaces/engine";
-  /**
-   * @deprecated
-   */
-  public static final String NUMBER_NS = DATASTYLE_NS;
+    public static final String OFFICE_NS = "urn:oasis:names:tc:opendocument:xmlns:office:1.0";
+    public static final String TABLE_NS = "urn:oasis:names:tc:opendocument:xmlns:table:1.0";
+    public static final String TEXT_NS = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
+    public static final String STYLE_NS = "urn:oasis:names:tc:opendocument:xmlns:style:1.0";
+    public static final String DRAWING_NS = "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0";
+    public static final String FO_NS = "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0";
+    public static final String XLINK_NS = "http://www.w3.org/1999/xlink";
+    public static final String PURL_NS = "http://purl.org/dc/elements/1.1/";
+    public static final String META_NS = "urn:oasis:names:tc:opendocument:xmlns:meta:1.0";
+    public static final String DATASTYLE_NS = "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0";
+    public static final String SVG_NS = "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0";
+    public static final String CHART_NS = "urn:oasis:names:tc:opendocument:xmlns:chart:1.0";
+    public static final String DR3D_NS = "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0";
+    public static final String MATHML_NS = "http://www.w3.org/1998/Math/MathML";
+    public static final String FORM_NS = "urn:oasis:names:tc:opendocument:xmlns:form:1.0";
+    public static final String SCRIPT_NS = "urn:oasis:names:tc:opendocument:xmlns:script:1.0";
+    public static final String OO2004_NS = "http://openoffice.org/2004/office";
+    public static final String OOW2004_NS = "http://openoffice.org/2004/writer";
+    public static final String OOC2004_NS = "http://openoffice.org/2004/calc";
+    public static final String XML_EVENT_NS = "http://www.w3.org/2001/xml-events";
+    public static final String XFORMS_NS = "http://www.w3.org/2002/xforms";
+    public static final String XSD_NS = "http://www.w3.org/2001/XMLSchema";
+    public static final String XSI_NS = "http://www.w3.org/2001/XMLSchema-instance";
+    public static final String OOREPORT_NS = "http://openoffice.org/2005/report";
+    public static final String CONFIG = "urn:oasis:names:tc:opendocument:xmlns:config:1.0";
+    public static final String INTERNAL_NS = "http://reporting.pentaho.org/namespaces/engine";
+    /**
+     * @deprecated
+     */
+    public static final String NUMBER_NS = DATASTYLE_NS;
 
-  private OfficeNamespaces()
-  {
-  }
+    private OfficeNamespaces()
+    {
+    }
 }
