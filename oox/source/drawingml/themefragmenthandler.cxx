5e1786865f7eb59294d5c2404720b2ff8128b6c9
Kurt Zenker <kz@openoffice.org>
2008-03-05 17:32:38 +0000

    INTEGRATION: CWS xmlfilter03_DEV300 (1.2.4); FILE MERGED 2008/02/06 10:11:26 dr 1.2.4.2: implement ThemeFragmentHandler using FragmentHandler2 2008/02/04 13:32:36 dr 1.2.4.1: rework of fragment handler/context handler base classes

--- oox/source/drawingml/themefragmenthandler.cxx
+++ oox/source/drawingml/themefragmenthandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: themefragmenthandler.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-17 08:05:52 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 18:32:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,76 +33,57 @@
  *
  ************************************************************************/
 
-#include "comphelper/anytostring.hxx"
-#include "cppuhelper/exc_hlp.hxx"
-
-#include <com/sun/star/beans/XMultiPropertySet.hpp>
-#include <com/sun/star/container/XNamed.hpp>
-
 #include "oox/drawingml/themefragmenthandler.hxx"
 #include "oox/drawingml/objectdefaultcontext.hxx"
 #include "oox/drawingml/themeelementscontext.hxx"
-#include "oox/core/namespaces.hxx"
-#include "tokens.hxx"
 
 using ::rtl::OUString;
-using namespace ::com::sun::star;
 using namespace ::oox::core;
-using namespace ::oox::drawingml;
-using namespace ::com::sun::star::uno;
-using namespace ::com::sun::star::xml::sax;
-using namespace ::com::sun::star::container;
 
-namespace oox { namespace drawingml {
+namespace oox {
+namespace drawingml {
 
-ThemeFragmentHandler::ThemeFragmentHandler( const XmlFilterRef& xFilter, const OUString& rFragmentPath, Theme& rTheme )
-    throw()
-: FragmentHandler( xFilter, rFragmentPath )
-, mrTheme( rTheme )
+// ============================================================================
+
+ThemeFragmentHandler::ThemeFragmentHandler( XmlFilterBase& rFilter, const OUString& rFragmentPath, Theme& rTheme ) :
+    FragmentHandler2( rFilter, rFragmentPath ),
+    mrTheme( rTheme )
 {
 }
+
 ThemeFragmentHandler::~ThemeFragmentHandler()
-    throw()
 {
-
 }
-Reference< XFastContextHandler > ThemeFragmentHandler::createFastChildContext( sal_Int32 aElementToken, const Reference< XFastAttributeList >& /*xAttribs */ )
-    throw (SAXException, RuntimeException)
+
+ContextWrapper ThemeFragmentHandler::onCreateContext( sal_Int32 nElement, const AttributeList& )
 {
     // CT_OfficeStyleSheet
-    Reference< XFastContextHandler > xRet;
-
-    switch( aElementToken )
+    switch( getCurrentElement() )
     {
-        case NMSP_DRAWINGML|XML_themeElements:              // CT_BaseStyles
-        {
-            xRet.set( new themeElementsContext( this, mrTheme ) );
-            break;
-        }
-        case NMSP_DRAWINGML|XML_objectDefaults:             // CT_ObjectStyleDefaults
-        {
-            xRet.set( new objectDefaultContext( this, mrTheme ) );
-            break;
-        }
-        case NMSP_DRAWINGML|XML_extraClrSchemeLst:          // CT_ColorSchemeList
-        break;
-        case NMSP_DRAWINGML|XML_custClrLst:                 // CustomColorList
-        break;
-        case NMSP_DRAWINGML|XML_ext:                        // CT_OfficeArtExtension
+        case XML_ROOT_CONTEXT:
+            return nElement == (NMSP_DRAWINGML|XML_theme);
+
+        case NMSP_DRAWINGML|XML_theme:
+            switch( nElement )
+            {
+                case NMSP_DRAWINGML|XML_themeElements:              // CT_BaseStyles
+                    return new themeElementsContext( *this, mrTheme );
+                case NMSP_DRAWINGML|XML_objectDefaults:             // CT_ObjectStyleDefaults
+                    return new objectDefaultContext( *this, mrTheme );
+                case NMSP_DRAWINGML|XML_extraClrSchemeLst:          // CT_ColorSchemeList
+                    return false;
+                case NMSP_DRAWINGML|XML_custClrLst:                 // CustomColorList
+                    return false;
+                case NMSP_DRAWINGML|XML_ext:                        // CT_OfficeArtExtension
+                    return false;
+            }
         break;
     }
-    if( !xRet.is() )
-        xRet.set( this );
-    return xRet;
-}
-void SAL_CALL ThemeFragmentHandler::endDocument()
-    throw (::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeException)
-{
+    return false;
 }
 
-//--------------------------------------------------------------------------------------------------------------
+// ============================================================================
 
-
-
-} }
+} // namespace drawingml
+} // namespace oox
 
