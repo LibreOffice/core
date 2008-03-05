8aa76b47ecc16686dc6199d1bd55f4d728b2d3ec
Kurt Zenker <kz@openoffice.org>
2008-03-05 17:13:10 +0000

    INTEGRATION: CWS xmlfilter03_DEV300 (1.2.4); FILE MERGED 2008/01/28 14:33:05 dr 1.2.4.1: load macro enabled spreadsheets, and xm:macrosheet sheets

--- oox/source/core/filterdetect.cxx
+++ oox/source/core/filterdetect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: filterdetect.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-17 08:05:50 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 18:13:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -241,7 +241,8 @@ OUString FilterDetectDocHandler::getFilterNameFromContentType( const OUString& r
 {
     if( rContentType.equalsAscii( "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml" ) )
         return maWordFilterName;
-    if( rContentType.equalsAscii( "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml" ) )
+    if( rContentType.equalsAscii( "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml" ) ||
+        rContentType.equalsAscii( "application/vnd.ms-excel.sheet.macroEnabled.main+xml" ) )
         return maExcelFilterName;
     if( rContentType.equalsAscii( "application/vnd.ms-excel.sheet.binary.macroEnabled.main" ) )
         return maExcelBinFilterName;
