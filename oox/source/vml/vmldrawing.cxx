0059e3ee8749099a6a8e80038e83437ef655c646
Kurt Zenker <kz@openoffice.org>
2008-03-05 17:55:04 +0000

    INTEGRATION: CWS xmlfilter03_DEV300 (1.2.4); FILE MERGED 2008/02/05 14:10:57 dr 1.2.4.1: oox::core::ContextHandler2 and oox::core::FragmentHandler2 for convenience

--- oox/source/vml/vmldrawing.cxx
+++ oox/source/vml/vmldrawing.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: vmldrawing.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-17 08:06:07 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 18:55:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,6 @@
 #include "oox/vml/drawing.hxx"
 #include "oox/core/namespaces.hxx"
 #include "tokens.hxx"
-
-using namespace ::oox::core;
 
 namespace oox { namespace vml {
 
