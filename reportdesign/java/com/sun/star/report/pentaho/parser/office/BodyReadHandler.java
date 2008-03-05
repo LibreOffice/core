c186493c25a90f736cecb22ede861e1f97801e9e
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:41:05 +0000

    INTEGRATION: CWS rptchart01_DEV300 (1.2.70); FILE MERGED 2008/02/19 06:42:49 oj 1.2.70.1: #i85225# ongoing work

--- reportdesign/java/com/sun/star/report/pentaho/parser/office/BodyReadHandler.java
+++ reportdesign/java/com/sun/star/report/pentaho/parser/office/BodyReadHandler.java
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: BodyReadHandler.java,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-09 11:56:09 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:41:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -78,6 +78,7 @@ public class BodyReadHandler extends ElementReadHandler
       reportReadHandler = new ReportReadHandler();
       return reportReadHandler;
     }
+
     return null;
   }
 
