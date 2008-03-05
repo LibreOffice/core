f16daff033a4ebe5e9acddcc532fd16af85671c3
Kurt Zenker <kz@openoffice.org>
2008-03-05 15:39:26 +0000

    INTEGRATION: CWS dba30a (1.13.178); FILE MERGED 2008/02/05 21:23:36 fs 1.13.178.1: #i85879# cleanup the toolbar mess in the data source browser

--- officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: rt $ $Date: 2007-07-06 06:52:44 $
+#   last change: $Author: kz $ $Date: 2008-03-05 16:39:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,6 +56,7 @@ XCSFILES= \
     WriterFormWindowState.xcs \
     WriterReportWindowState.xcs \
     DbBrowserWindowState.xcs \
+    DbTableDataWindowState.xcs \
     DrawImpressCommands.xcs \
     GenericCommands.xcs \
     MathCommands.xcs \
