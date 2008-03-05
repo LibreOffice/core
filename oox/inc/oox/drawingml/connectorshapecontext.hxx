26669a820faddd667ee5e0903ed204c460fc1dce
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:38:40 +0000

    INTEGRATION: CWS xmlfilter03_DEV300 (1.2.4); FILE MERGED 2008/02/06 10:11:10 dr 1.2.4.2: implement ThemeFragmentHandler using FragmentHandler2 2008/02/04 13:31:33 dr 1.2.4.1: rework of fragment handler/context handler base classes

--- oox/inc/oox/drawingml/connectorshapecontext.hxx
+++ oox/inc/oox/drawingml/connectorshapecontext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: connectorshapecontext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-17 08:05:45 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:38:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,8 +44,7 @@ namespace oox { namespace drawingml {
 class ConnectorShapeContext : public ShapeContext
 {
 public:
-    ConnectorShapeContext( const ::oox::core::FragmentHandlerRef& xHandler, sal_Int32 aElementToken,
-        oox::drawingml::ShapePtr pMasterShapePtr, oox::drawingml::ShapePtr pGroupShapePtr );
+    ConnectorShapeContext( ::oox::core::ContextHandler& rParent, ShapePtr pMasterShapePtr, ShapePtr pGroupShapePtr );
     virtual ~ConnectorShapeContext();
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext( ::sal_Int32 Element, const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastAttributeList >& Attribs ) throw (::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeException);
 };
