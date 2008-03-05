8686618c9a6109b6002565a8170ca0fe529b360a
Kurt Zenker <kz@openoffice.org>
2008-03-05 16:05:27 +0000

    INTEGRATION: CWS aquavcl05_DEV300 (1.7.6); FILE MERGED 2008/02/13 10:58:24 pl 1.7.6.1: #i85963# support bevel buton hint

--- vcl/inc/vcl/salnativewidgets.hxx
+++ vcl/inc/vcl/salnativewidgets.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: salnativewidgets.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 16:20:20 $
+ *  last change: $Author: kz $ $Date: 2008-03-05 17:05:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -381,6 +381,17 @@ class MenubarValue
 public:
     MenubarValue() { maTopDockingAreaHeight=0; }
     int             maTopDockingAreaHeight;
+};
+
+/*  PushButtonValue:
+ *
+ *  Value container for pushbuttons specifying additional drawing hints
+ */
+class PushButtonValue
+{
+public:
+PushButtonValue() : mbBevelButton( false ) {}
+    bool            mbBevelButton:1;
 };
 
 /* ImplControlValue:
