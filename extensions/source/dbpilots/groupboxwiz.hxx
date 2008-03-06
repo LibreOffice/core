d9618ff4379f12ac23a655cd19f6101433d6c735
Kurt Zenker <kz@openoffice.org>
2008-03-06 17:41:35 +0000

    INTEGRATION: CWS odbmacros2 (1.8.424); FILE MERGED 2008/02/11 11:14:56 fs 1.8.424.3: IWizardPage is COMMIT_REASON is deprecated - replace usages with CommitPageReason, while I have an svtools-incompatible CWS 2008/01/30 13:19:50 fs 1.8.424.2: canAdvance made const 2008/01/15 09:52:42 fs 1.8.424.1: some re-factoring of OWizardMachine, RoadmapWizard and derived classes, to prepare the migration UI for #i49133#

--- extensions/source/dbpilots/groupboxwiz.hxx
+++ extensions/source/dbpilots/groupboxwiz.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: groupboxwiz.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 19:30:50 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 18:41:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -83,7 +83,7 @@ namespace dbp
     protected:
         // OWizardMachine overridables
         virtual ::svt::OWizardPage* createPage( WizardState _nState );
-        virtual WizardState         determineNextState( WizardState _nCurrentState );
+        virtual WizardState         determineNextState( WizardState _nCurrentState ) const;
         virtual void                enterState( WizardState _nState );
 
         virtual sal_Bool onFinish(sal_Int32 _nResult);
@@ -129,8 +129,8 @@ namespace dbp
 
         // OWizardPage overridables
         virtual void        initializePage();
-        virtual sal_Bool    commitPage(COMMIT_REASON _eReason);
-        virtual sal_Bool    determineNextButtonState();
+        virtual sal_Bool    commitPage( CommitPageReason _eReason );
+        virtual bool        canAdvance() const;
 
         DECL_LINK( OnMoveEntry, PushButton* );
         DECL_LINK( OnEntrySelected, ListBox* );
@@ -157,7 +157,7 @@ namespace dbp
     protected:
         // OWizardPage overridables
         virtual void        initializePage();
-        virtual sal_Bool    commitPage(COMMIT_REASON _eReason);
+        virtual sal_Bool    commitPage( CommitPageReason _eReason );
 
         OOptionGroupSettings& getSettings() { return static_cast<OGroupBoxWizard*>(getDialog())->getSettings(); }
     };
@@ -187,7 +187,7 @@ namespace dbp
 
         // OWizardPage overridables
         virtual void        initializePage();
-        virtual sal_Bool    commitPage(COMMIT_REASON _eReason);
+        virtual sal_Bool    commitPage( CommitPageReason _eReason );
 
         void implTraveledOptions();
 
@@ -229,8 +229,8 @@ namespace dbp
 
         // OWizardPage overridables
         virtual void        initializePage();
-        virtual sal_Bool    commitPage(COMMIT_REASON _eReason);
-        virtual sal_Bool    determineNextButtonState();
+        virtual sal_Bool    commitPage( CommitPageReason _eReason );
+        virtual bool        canAdvance() const;
     };
 
 //.........................................................................
