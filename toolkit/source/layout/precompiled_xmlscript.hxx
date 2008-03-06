c4a9531d2dfaaca9a7b664b2a75233738808c45b
Kurt Zenker <kz@openoffice.org>
2008-03-06 10:47:03 +0000

    INTEGRATION: CWS layout_DEV300 (1.1.2); FILE ADDED 2008/02/15 09:56:41 jcn 1.1.2.1: Remove byteseq.cxx c&p from xml_byteseq.cxx.

---
+++ toolkit/source/layout/precompiled_xmlscript.hxx
@@ -0,0 +1,8 @@
+/*
+  xmlscript/source/xml_helper/xml_byteseq.cxx compile helper.
+
+  Avoid introducing a toolkit dependency on xmlscript.
+
+  It would be nice to modify xml_byteseq.cxx making it friendlier
+  to include.
+*/
