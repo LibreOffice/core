3ad12e3d38d9cbf213767bad681efa672744bfad
Kurt Zenker <kz@openoffice.org>
2008-03-06 17:23:59 +0000

    INTEGRATION: CWS odbmacros2 (1.7.376); FILE MERGED 2007/12/13 11:22:37 fs 1.7.376.1: #i49133# some refactoring, mostly related to the m_xCurrentFrame->m_aCurrentFrame change

--- dbaccess/source/ui/inc/callbacks.hxx
+++ dbaccess/source/ui/inc/callbacks.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: callbacks.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 15:44:58 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 18:23:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,6 +49,7 @@
 class CommandEvent;
 class SvLBoxEntry;
 class String;
+class Point;
 struct AcceptDropEvent;
 struct ExecuteDropEvent;
 
