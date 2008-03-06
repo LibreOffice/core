ed4f069f76282c44576b4ba0436059b96ba7965c
Kurt Zenker <kz@openoffice.org>
2008-03-06 10:40:28 +0000

    INTEGRATION: CWS layout_DEV300 (1.1.2); FILE ADDED 2008/02/19 13:36:27 thb 1.1.2.2: Corrected typo (appears only in dbgutil build) 2008/02/08 18:44:42 jcn 1.1.2.1: Initial toolkit import from ee9a2fcc29d7e2f01cc80ef7c13bf7bc7d55ae7e.
        layout/source/awt -> toolkit/source/awt
        layout/source/core -> toolkit/source/layout
        layout/source/wrapper -> toolkit/source/vclcompat
        layout/inc/layout -> toolkit/inc/layout
        layout/source/inc -> toolkit/inc/layout
        layout/workben -> toolkit/workben/layout
    
    That's ooo-build trunk r11539 @ ooh680-m5/src680-m245.

---
+++ toolkit/source/awt/vclxfixedline.cxx
@@ -0,0 +1,122 @@
+#include "vclxfixedline.hxx"
+#ifndef _TOOLKIT_HELPER_PROPERTY_HXX_
+#include "toolkit/helper/property.hxx"
+#endif
+
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+#include <vcl/fixed.hxx>
+
+#ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
+#include <toolkit/helper/vclunohelper.hxx>
+#endif
+
+#include <com/sun/star/awt/PosSize.hpp>
+
+using namespace toolkit;
+//........................................................................
+namespace layoutimpl
+{
+//........................................................................
+
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star;
+
+//====================================================================
+//= VCLXFixedLine
+//====================================================================
+DBG_NAME( VCLXFixedLine )
+//--------------------------------------------------------------------
+VCLXFixedLine::VCLXFixedLine()
+: VCLXWindow()
+{
+    DBG_CTOR( VCLXFixedLine, NULL );
+}
+
+//--------------------------------------------------------------------
+VCLXFixedLine::~VCLXFixedLine()
+{
+    DBG_DTOR( VCLXFixedLine, NULL );
+}
+
+//--------------------------------------------------------------------
+//    IMPLEMENT_FORWARD_XINTERFACE1( VCLXFixedLine, VCLXWindow )
+
+//--------------------------------------------------------------------
+IMPLEMENT_FORWARD_XTYPEPROVIDER1( VCLXFixedLine, VCLXWindow )
+
+//--------------------------------------------------------------------
+void SAL_CALL VCLXFixedLine::dispose( ) throw(RuntimeException)
+{
+    {
+        ::vos::OGuard aGuard( GetMutex() );
+
+        EventObject aDisposeEvent;
+        aDisposeEvent.Source = *this;
+    }
+
+    VCLXWindow::dispose();
+}
+
+//--------------------------------------------------------------------
+::com::sun::star::awt::Size SAL_CALL VCLXFixedLine::getMinimumSize()
+    throw(::com::sun::star::uno::RuntimeException)
+{
+    return awt::Size( 8, 8 );
+}
+
+//--------------------------------------------------------------------
+void VCLXFixedLine::ProcessWindowEvent( const VclWindowEvent& _rVclWindowEvent )
+{
+    ::vos::OClearableGuard aGuard( GetMutex() );
+
+    switch ( _rVclWindowEvent.GetId() )
+    {
+        default:
+            aGuard.clear();
+            VCLXWindow::ProcessWindowEvent( _rVclWindowEvent );
+            break;
+    }
+}
+
+//--------------------------------------------------------------------
+void SAL_CALL VCLXFixedLine::setProperty( const ::rtl::OUString& PropertyName, const Any &Value ) throw(RuntimeException)
+{
+    ::vos::OGuard aGuard( GetMutex() );
+
+    if ( GetWindow() )
+    {
+        sal_uInt16 nPropertyId = GetPropertyId( PropertyName );
+        switch ( nPropertyId )
+        {
+            default:
+                VCLXWindow::setProperty( PropertyName, Value );
+        }
+    }
+}
+
+//--------------------------------------------------------------------
+Any SAL_CALL VCLXFixedLine::getProperty( const ::rtl::OUString& PropertyName ) throw(RuntimeException)
+{
+    ::vos::OGuard aGuard( GetMutex() );
+
+    Any aReturn;
+    if ( GetWindow() )
+    {
+        sal_uInt16 nPropertyId = GetPropertyId( PropertyName );
+        switch ( nPropertyId )
+        {
+            default:
+                aReturn = VCLXWindow::getProperty( PropertyName );
+        }
+    }
+    return aReturn;
+}
+
+//........................................................................
+} // namespace toolkit
+//........................................................................
