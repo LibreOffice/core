66ce40d3c0af4dab8602a62a5231933e5f85deb6
Kurt Zenker <kz@openoffice.org>
2008-03-06 18:29:39 +0000

    INTEGRATION: CWS odbmacros2 (1.1.2); FILE ADDED 2008/01/14 12:02:42 fs 1.1.2.1: sample how to use the OGenericUnoDialog class

---
+++ svtools/workben/unodialog/udlg_services.cxx
@@ -0,0 +1,66 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: udlg_services.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-03-06 19:29:39 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+/** === begin UNO includes === **/
+/** === end UNO includes === **/
+
+#include "udlg_module.hxx"
+
+//........................................................................
+namespace udlg
+{
+//........................................................................
+
+    //--------------------------------------------------------------------
+    extern void createRegistryInfo_UnoDialogSkeleton();
+
+    static void initializeModule()
+    {
+        static bool bInitialized( false );
+        if ( !bInitialized )
+        {
+            ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+            if ( !bInitialized )
+            {
+                createRegistryInfo_UnoDialogSkeleton();
+            }
+        }
+    }
+
+//........................................................................
+} // namespace udlg
+//........................................................................
+
+IMPLEMENT_COMPONENT_LIBRARY_API( ::udlg::UdlgModule, ::udlg::initializeModule )
