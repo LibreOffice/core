8dadfe6139f2dff51012f0bb1d553d8abd7e13ed
Kurt Zenker <kz@openoffice.org>
2008-03-06 11:33:14 +0000

    INTEGRATION: CWS layout_DEV300 (1.3.1148); FILE MERGED 2008/02/12 12:42:47 jcn 1.3.1148.1: Initial import of layout-dialogs-svx-sw.diff.

--- sw/source/ui/inc/wordcountdialog.hxx
+++ sw/source/ui/inc/wordcountdialog.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wordcountdialog.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:18:42 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 12:33:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,8 @@
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
+#include <layout/layout.hxx>
+#include <layout/layout-pre.hxx>
 struct SwDocStat;
 class SwWordCountDialog : public SfxModalDialog
 {
@@ -69,5 +71,7 @@ public:
 
     void    SetValues(const SwDocStat& rCurrent, const SwDocStat& rDoc);
 };
+
+#include <layout/layout-post.hxx>
 
 #endif
