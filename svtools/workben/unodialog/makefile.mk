fbc6671941bed69f4268bc4c54df87a9a555e978
Kurt Zenker <kz@openoffice.org>
2008-03-06 18:27:08 +0000

    INTEGRATION: CWS odbmacros2 (1.1.2); FILE ADDED 2008/01/21 12:27:52 fs 1.1.2.3: #i10000 2008/01/15 14:04:23 fs 1.1.2.2: extended the example with a complete roadmap wizard skeleton 2008/01/14 12:02:01 fs 1.1.2.1: sample how to use the OGenericUnoDialog class

---
+++ svtools/workben/unodialog/makefile.mk
@@ -0,0 +1,97 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2008-03-06 19:27:08 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..
+PRJINC=$(PRJ)/inc
+PRJNAME=svtools
+TARGET=udlg
+USE_DEFFILE=TRUE
+
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings ----------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files -------------------------------------
+
+# ... resource files ............................
+
+SRS1NAME=$(TARGET)
+SRC1FILES =	\
+        roadmapskeleton.src
+
+# ... object files ............................
+SLOFILES=   $(SLO)$/unodialogsample.obj \
+            $(SLO)$/roadmapskeleton.obj \
+            $(SLO)$/roadmapskeletonpages.obj \
+            $(SLO)$/udlg_module.obj \
+            $(SLO)$/udlg_services.obj \
+
+# --- library -----------------------------------
+
+SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1VERSIONMAP=$(TARGET).map
+
+SHL1STDLIBS= \
+        $(CPPULIB)          \
+        $(CPPUHELPERLIB)    \
+        $(COMPHELPERLIB)    \
+        $(UNOTOOLSLIB)      \
+        $(TOOLSLIB)         \
+        $(SALLIB)           \
+        $(SVTOOLLIB)        \
+        $(VCLLIB)
+
+SHL1LIBS=       $(SLB)$/$(TARGET).lib
+SHL1IMPLIB=     i$(TARGET)
+SHL1DEPN=       $(SHL1LIBS)
+SHL1DEF=        $(MISC)$/$(SHL1TARGET).def
+
+DEF1NAME=       $(SHL1TARGET)
+
+# --- .res files -------------------------------
+
+RES1FILELIST=\
+    $(SRS)$/$(TARGET).srs
+
+RESLIB1NAME=$(TARGET)
+RESLIB1IMAGES=$(PRJ)$/res
+RESLIB1SRSFILES=$(RES1FILELIST)
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
+
