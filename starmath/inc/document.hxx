362996a225ea77ab4fef50ea08a8ba81702309d2
Kurt Zenker <kz@openoffice.org>
2008-03-06 17:44:52 +0000

    INTEGRATION: CWS odbmacros2 (1.32.48); FILE MERGED 2008/03/04 12:01:39 fs 1.32.48.2: RESYNC: (1.32-1.33); FILE MERGED 2007/12/12 11:22:14 fs 1.32.48.1: #i49133# ctor now takes new (defaulted) argument which allows to switch off scripting support (by calling SfxObjectShell::SetHasNoBasic)

--- starmath/inc/document.hxx
+++ starmath/inc/document.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: document.hxx,v $
  *
- *  $Revision: 1.33 $
+ *  $Revision: 1.34 $
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 14:43:27 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 18:44:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -191,7 +191,7 @@ public:
     using SotObject::GetInterface;
     SFX_DECL_OBJECTFACTORY();
 
-                SmDocShell(SfxObjectCreateMode eMode = SFX_CREATE_MODE_EMBEDDED);
+                SmDocShell(SfxObjectCreateMode eMode = SFX_CREATE_MODE_EMBEDDED, const sal_Bool _bScriptSupport = sal_True);
     virtual     ~SmDocShell();
 
     void        LoadSymbols();
