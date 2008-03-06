0cf34de25e2c76e31930f81aff250b640e94ad96
Kurt Zenker <kz@openoffice.org>
2008-03-06 17:51:27 +0000

    INTEGRATION: CWS odbmacros2 (1.2.68); FILE MERGED 2007/12/19 14:49:52 fs 1.2.68.1: #i49133# export sbxToUnoValue (was declared extern in various other modules, anyway)

--- basic/inc/basic/sbuno.hxx
+++ basic/inc/basic/sbuno.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sbuno.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 12:53:10 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 18:51:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,6 +48,8 @@ SbxObjectRef GetSbUnoObject( const String& aName, const com::sun::star::uno::Any
 
 // Force creation of all properties for debugging
 void createAllObjectProperties( SbxObject* pObj );
+
+::com::sun::star::uno::Any sbxToUnoValue( SbxVariable* pVar );
 
 #endif
 
