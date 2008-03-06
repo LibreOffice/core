21951ebac533414fd0d8de023051f0c143ab7720
Kurt Zenker <kz@openoffice.org>
2008-03-06 16:30:04 +0000

    INTEGRATION: CWS chart20_DEV300 (1.15.28); FILE MERGED 2008/02/21 15:59:28 bm 1.15.28.1: #i366# error bars from ranges

--- chart2/source/model/main/ImplChartModel.cxx
+++ chart2/source/model/main/ImplChartModel.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImplChartModel.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:01:28 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 17:30:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,6 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "ImplChartModel.hxx"
-#include "CachedDataSequence.hxx"
 #include "DataSeries.hxx"
 #include "StyleFamilies.hxx"
 #include "StyleFamily.hxx"
