01e73e8f19f090697645af8be36d87ea5f1a4677
Kurt Zenker <kz@openoffice.org>
2008-03-06 11:34:29 +0000

    INTEGRATION: CWS layout_DEV300 (1.1.2); FILE ADDED 2008/02/12 09:58:28 jcn 1.1.2.1: Move vos-based Application out to vosapp.hxx.

---
+++ transex3/inc/transex3/vosapp.hxx
@@ -0,0 +1,33 @@
+#ifndef VOSAPP_HXX
+#define VOSAPP_HXX
+
+#include <sal/main.h>
+#include <tools/solar.h>
+#include <tools/string.hxx>
+#include <vos/process.hxx>
+
+// Mininmal vcl/svapp compatibility without vcl dependence
+class Application
+{
+public:
+    USHORT GetCommandLineParamCount();
+    XubString GetCommandLineParam( USHORT nParam );
+    virtual void Main() = 0;
+};
+
+// Urg: Cut & Paste from svapp.cxx: we don't want to depend on vcl
+USHORT Application::GetCommandLineParamCount()
+{
+    vos::OStartupInfo aStartInfo;
+    return (USHORT)aStartInfo.getCommandArgCount();
+}
+
+XubString Application::GetCommandLineParam( USHORT nParam )
+{
+    vos::OStartupInfo aStartInfo;
+    rtl::OUString aParam;
+    aStartInfo.getCommandArg( nParam, aParam );
+    return XubString( aParam );
+}
+
+#endif /* VOSAPP_HXX */
