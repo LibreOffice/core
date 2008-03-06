b8d3d08e2dfeeee0eb6f34526411d34f0ea72487
Kurt Zenker <kz@openoffice.org>
2008-03-06 14:24:50 +0000

    INTEGRATION: CWS ause084_DEV300 (1.5.614); FILE MERGED 2007/12/18 10:09:33 hjs 1.5.614.1: #i78233# remove PDBTARGET and friends

--- sc/source/ui/styleui/makefile.mk
+++ sc/source/ui/styleui/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 22:36:26 $
+#   last change: $Author: kz $ $Date: 2008-03-06 15:24:50 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,13 +37,6 @@ PRJ=..$/..$/..
 
 PRJNAME=sc
 TARGET=styleui
-
-PROJECTPCH4DLL=TRUE
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\pch\ui_pch
-
-AUTOSEG=true
 
 # --- Settings -----------------------------------------------------
 
