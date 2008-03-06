f1cd874e2c74d7fa511d2ddeef9e58239434181d
Kurt Zenker <kz@openoffice.org>
2008-03-06 17:37:32 +0000

    INTEGRATION: CWS odbmacros2 (1.12.44); FILE MERGED 2008/02/04 13:08:56 fs 1.12.44.2: RESYNC: (1.12-1.13); FILE MERGED 2008/01/15 07:18:39 fs 1.12.44.1: #i67987# use a roadmap in this wizard

--- extensions/source/abpilot/datasourcehandling.cxx
+++ extensions/source/abpilot/datasourcehandling.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: datasourcehandling.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:33:39 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 18:37:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -552,6 +552,16 @@ namespace abp
         if ( !isValid() )
             return ::rtl::OUString();
         return m_pImpl->sName;
+    }
+
+    //---------------------------------------------------------------------
+    bool ODataSource::hasTable( const ::rtl::OUString& _rTableName ) const
+    {
+        if ( !isConnected() )
+            return false;
+
+        const StringBag& aTables( getTableNames() );
+        return aTables.find( _rTableName ) != aTables.end();
     }
 
     //---------------------------------------------------------------------
