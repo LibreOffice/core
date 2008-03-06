ed46e87cce967f865f628c96257c2e086db8337d
Kurt Zenker <kz@openoffice.org>
2008-03-06 14:23:49 +0000

    INTEGRATION: CWS ause084_DEV300 (1.4.614); FILE MERGED 2007/12/18 10:09:32 hjs 1.4.614.1: #i78233# remove PDBTARGET and friends

--- sc/source/ui/navipi/makefile.mk
+++ sc/source/ui/navipi/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 22:16:46 $
+#   last change: $Author: kz $ $Date: 2008-03-06 15:23:49 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -37,13 +37,6 @@ PRJ=..$/..$/..
 
 PRJNAME=sc
 TARGET=navipi
-
-PROJECTPCH4DLL=TRUE
-PROJECTPCH=ui_pch
-PDBTARGET=ui_pch
-PROJECTPCHSOURCE=..\pch\ui_pch
-
-AUTOSEG=true
 
 # --- Settings -----------------------------------------------------
 
