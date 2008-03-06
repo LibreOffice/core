619a755d7b410d82171b17481cfd0e99efbfba0a
Kurt Zenker <kz@openoffice.org>
2008-03-06 18:32:27 +0000

    INTEGRATION: CWS odbmacros2 (1.2.88); FILE MERGED 2008/01/23 14:29:42 fs 1.2.88.1: support support for a window's BorderStyle

--- tools/inc/tools/rc.h
+++ tools/inc/tools/rc.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rc.h,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 20:15:55 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 19:32:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -59,6 +59,7 @@ typedef short RSWND_STYLE;
 #define WINDOW_QUICKTEXT                0x0200
 #define WINDOW_EXTRALONG                0x0800
 #define WINDOW_UNIQUEID                 0x1000
+#define WINDOW_BORDER_STYLE             0x2000
 
 // Definition der Struktur die alle "WorkWindow"-Resourcen haben
 #define WORKWIN_SHOWNORMAL              0
