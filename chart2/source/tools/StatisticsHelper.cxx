8cd82d4fed977cd233adf27e60309315d427d8f8
Kurt Zenker <kz@openoffice.org>
2008-03-06 16:50:58 +0000

    INTEGRATION: CWS chart20_DEV300 (1.4.152); FILE MERGED 2008/02/22 17:13:24 bm 1.4.152.3: #i366# unused variable 2008/02/22 16:51:39 bm 1.4.152.2: #i366# +removeErrorBars() 2008/02/21 15:59:28 bm 1.4.152.1: #i366# error bars from ranges

--- chart2/source/tools/StatisticsHelper.cxx
+++ chart2/source/tools/StatisticsHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: StatisticsHelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:29:54 $
+ *  last change: $Author: kz $ $Date: 2008-03-06 17:50:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,12 +36,25 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_chart2.hxx"
 #include "StatisticsHelper.hxx"
+#include "DataSeriesHelper.hxx"
+#include "ErrorBar.hxx"
+#include "macros.hxx"
 
-#ifndef INCLUDED_RTL_MATH_HXX
 #include <rtl/math.hxx>
-#endif
+#include <rtl/ustrbuf.hxx>
+#include <comphelper/processfactory.hxx>
+
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <com/sun/star/chart2/data/XLabeledDataSequence.hpp>
+#include <com/sun/star/chart2/data/XNumericalDataSequence.hpp>
+#include <com/sun/star/chart2/data/XDataSink.hpp>
+#include <com/sun/star/chart/ErrorBarStyle.hpp>
 
 using ::com::sun::star::uno::Sequence;
+using ::com::sun::star::uno::Reference;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+using namespace ::com::sun::star;
 
 namespace
 {
@@ -80,6 +93,95 @@ double lcl_getVariance( const Sequence< double > & rData, sal_Int32 & rOutValidC
     }
 
     return fResult;
+}
+
+Reference< chart2::data::XLabeledDataSequence > lcl_getErrorBarLabeledSequence(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    bool bPositiveValue, bool bYError,
+    OUString & rOutRoleNameUsed )
+{
+    OUStringBuffer aRole( C2U("error-bars-"));
+    if( bYError )
+        aRole.append( sal_Unicode( 'y' ));
+    else
+        aRole.append( sal_Unicode( 'x' ));
+
+    OUString aPlainRole = aRole.makeStringAndClear();
+    aRole.append( aPlainRole );
+    aRole.append( sal_Unicode( '-' ));
+
+    if( bPositiveValue )
+        aRole = aRole.appendAscii( "positive" );
+    else
+        aRole = aRole.appendAscii( "negative" );
+
+    OUString aLongRole = aRole.makeStringAndClear();
+    Reference< chart2::data::XLabeledDataSequence > xLSeq(
+        ::chart::DataSeriesHelper::getDataSequenceByRole( xDataSource, aLongRole ));
+    // try role without "-negative" or "-positive" postfix
+    if( xLSeq.is())
+        rOutRoleNameUsed = aLongRole;
+    else
+    {
+        xLSeq.set( ::chart::DataSeriesHelper::getDataSequenceByRole( xDataSource, aPlainRole ));
+        if( xLSeq.is())
+            rOutRoleNameUsed = aPlainRole;
+        else
+            rOutRoleNameUsed = aLongRole;
+    }
+
+    return xLSeq;
+}
+
+void lcl_setRole(
+    const Reference< chart2::data::XDataSequence > & xNewSequence,
+    const OUString & rRole )
+{
+    Reference< beans::XPropertySet > xSeqProp( xNewSequence, uno::UNO_QUERY );
+    if( xSeqProp.is())
+        xSeqProp->setPropertyValue( C2U("Role"), uno::makeAny( rRole ));
+}
+
+void lcl_addSequenceToDataSource(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    const Reference< chart2::data::XDataSequence > & xNewSequence,
+    const OUString & rRole )
+{
+    Reference< chart2::data::XDataSink > xSink( xDataSource, uno::UNO_QUERY );
+    Reference< lang::XMultiServiceFactory > xFact( comphelper::getProcessServiceFactory(), uno::UNO_QUERY_THROW );
+    if( ! ( xFact.is() && xSink.is() ))
+        return;
+
+    Reference< chart2::data::XLabeledDataSequence > xLSeq(
+        xFact->createInstance( C2U("com.sun.star.chart2.data.LabeledDataSequence")), uno::UNO_QUERY );
+    if( xLSeq.is())
+    {
+        lcl_setRole( xNewSequence, rRole );
+        xLSeq->setValues( xNewSequence );
+        Sequence< Reference< chart2::data::XLabeledDataSequence > > aSequences(
+            xDataSource->getDataSequences());
+        aSequences.realloc( aSequences.getLength() + 1 );
+        aSequences[ aSequences.getLength() - 1 ] = xLSeq;
+        xSink->setData( aSequences );
+    }
+}
+
+void lcl_setXMLRangePropertyAtDataSequence(
+    const Reference< chart2::data::XDataSequence > & xDataSequence,
+    const OUString & rXMLRange )
+{
+    try
+    {
+        const OUString aXMLRangePropName( C2U( "CachedXMLRange" ));
+        Reference< beans::XPropertySet > xProp( xDataSequence, uno::UNO_QUERY_THROW );
+        Reference< beans::XPropertySetInfo > xInfo( xProp->getPropertySetInfo());
+        if( xInfo.is() && xInfo->hasPropertyByName( aXMLRangePropName ))
+            xProp->setPropertyValue( aXMLRangePropName, uno::makeAny( rXMLRange ));
+    }
+    catch( const uno::Exception & ex )
+    {
+        ASSERT_EXCEPTION( ex );
+    }
 }
 
 } // anonymous namespace
@@ -127,5 +229,183 @@ double StatisticsHelper::getStandardError( const Sequence< double > & rData )
     return fResult;
 }
 
+// static
+Reference< chart2::data::XLabeledDataSequence > StatisticsHelper::getErrorLabeledDataSequenceFromDataSource(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    bool bPositiveValue,
+    bool bYError /* = true */ )
+{
+    Reference< chart2::data::XLabeledDataSequence > xResult;
+    if( !xDataSource.is())
+        return xResult;
+
+    OUString aRole;
+    Reference< chart2::data::XLabeledDataSequence > xLSeq(
+        lcl_getErrorBarLabeledSequence( xDataSource, bPositiveValue, bYError, aRole ));
+    if( xLSeq.is())
+        xResult.set( xLSeq );
+
+    return xResult;
+}
+
+// static
+Reference< chart2::data::XDataSequence > StatisticsHelper::getErrorDataSequenceFromDataSource(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    bool bPositiveValue,
+    bool bYError /* = true */ )
+{
+    Reference< chart2::data::XLabeledDataSequence > xLSeq(
+        StatisticsHelper::getErrorLabeledDataSequenceFromDataSource(
+            xDataSource, bPositiveValue,
+            bYError ));
+    if( !xLSeq.is())
+        return Reference< chart2::data::XDataSequence >();
+
+    return xLSeq->getValues();
+}
+
+// static
+double StatisticsHelper::getErrorFromDataSource(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    sal_Int32 nIndex,
+    bool bPositiveValue,
+    bool bYError /* = true */ )
+{
+    double fResult = 0.0;
+    ::rtl::math::setNan( & fResult );
+
+    Reference< chart2::data::XDataSequence > xValues(
+        StatisticsHelper::getErrorDataSequenceFromDataSource( xDataSource, bPositiveValue, bYError ));
+
+    Reference< chart2::data::XNumericalDataSequence > xNumValues( xValues, uno::UNO_QUERY );
+    if( xNumValues.is())
+    {
+        Sequence< double > aData( xNumValues->getNumericalData());
+        if( nIndex < aData.getLength())
+            fResult = aData[nIndex];
+    }
+    else if( xValues.is())
+    {
+        Sequence< uno::Any > aData( xValues->getData());
+        if( nIndex < aData.getLength())
+            aData[nIndex] >>= fResult;
+    }
+
+    return fResult;
+}
+
+// static
+void StatisticsHelper::setErrorDataSequence(
+    const Reference< chart2::data::XDataSource > & xDataSource,
+    const Reference< chart2::data::XDataProvider > & xDataProvider,
+    const OUString & rNewRange,
+    bool bPositiveValue,
+    bool bYError /* = true */,
+    OUString * pXMLRange /* = 0 */ )
+{
+    Reference< chart2::data::XDataSink > xDataSink( xDataSource, uno::UNO_QUERY );
+    if( ! ( xDataSink.is() && xDataProvider.is()))
+        return;
+
+    OUString aRole;
+    Reference< chart2::data::XLabeledDataSequence > xLSeq(
+        lcl_getErrorBarLabeledSequence( xDataSource, bPositiveValue, bYError, aRole ));
+    Reference< chart2::data::XDataSequence > xNewSequence(
+        xDataProvider->createDataSequenceByRangeRepresentation( rNewRange ));
+    if( xNewSequence.is())
+    {
+        if( pXMLRange )
+            lcl_setXMLRangePropertyAtDataSequence( xNewSequence, *pXMLRange );
+        if( xLSeq.is())
+        {
+            lcl_setRole( xNewSequence, aRole );
+            xLSeq->setValues( xNewSequence );
+        }
+        else
+            lcl_addSequenceToDataSource( xDataSource, xNewSequence, aRole );
+    }
+}
+
+// static
+Reference< beans::XPropertySet > StatisticsHelper::addErrorBars(
+    const Reference< chart2::XDataSeries > & xDataSeries,
+    const Reference< uno::XComponentContext > & xContext,
+    sal_Int32 nStyle,
+    bool bYError /* = true */ )
+{
+    Reference< beans::XPropertySet > xErrorBar;
+    Reference< beans::XPropertySet > xSeriesProp( xDataSeries, uno::UNO_QUERY );
+    if( !xSeriesProp.is())
+        return xErrorBar;
+
+    const OUString aPropName( bYError ? C2U("ErrorBarY") : C2U("ErrorBarX"));
+    if( !( xSeriesProp->getPropertyValue( aPropName ) >>= xErrorBar ) ||
+        !xErrorBar.is())
+    {
+        xErrorBar.set( new ErrorBar( xContext ));
+    }
+
+    OSL_ASSERT( xErrorBar.is());
+    if( xErrorBar.is())
+    {
+        xErrorBar->setPropertyValue( C2U("ErrorBarStyle"), uno::makeAny( nStyle ));
+    }
+
+    xSeriesProp->setPropertyValue( aPropName, uno::makeAny( xErrorBar ));
+
+    return xErrorBar;
+}
+
+// static
+Reference< beans::XPropertySet > StatisticsHelper::getErrorBars(
+    const Reference< chart2::XDataSeries > & xDataSeries,
+    bool bYError /* = true */ )
+{
+    Reference< beans::XPropertySet > xSeriesProp( xDataSeries, uno::UNO_QUERY );
+    Reference< beans::XPropertySet > xErrorBar;
+    const OUString aPropName( bYError ? C2U("ErrorBarY") : C2U("ErrorBarX"));
+
+    if ( xSeriesProp.is())
+        xSeriesProp->getPropertyValue( aPropName ) >>= xErrorBar;
+
+    return xErrorBar;
+}
+
+// static
+bool StatisticsHelper::hasErrorBars(
+    const Reference< chart2::XDataSeries > & xDataSeries,
+    bool bYError /* = true */ )
+{
+    Reference< beans::XPropertySet > xErrorBar( getErrorBars( xDataSeries, bYError ));
+    sal_Int32 nStyle = ::com::sun::star::chart::ErrorBarStyle::NONE;
+
+    return ( xErrorBar.is() &&
+             ( xErrorBar->getPropertyValue( C2U("ErrorBarStyle")) >>= nStyle ) &&
+             nStyle != ::com::sun::star::chart::ErrorBarStyle::NONE );
+}
+
+// static
+void StatisticsHelper::removeErrorBars(
+    const Reference< chart2::XDataSeries > & xDataSeries,
+    bool bYError /* = true  */ )
+{
+    Reference< beans::XPropertySet > xErrorBar( getErrorBars( xDataSeries, bYError ));
+    if ( xErrorBar.is())
+        xErrorBar->setPropertyValue( C2U("ErrorBarStyle"), uno::makeAny(
+                                         ::com::sun::star::chart::ErrorBarStyle::NONE ));
+}
+
+// static
+bool StatisticsHelper::usesErrorBarRanges(
+    const Reference< chart2::XDataSeries > & xDataSeries,
+    bool bYError /* = true */ )
+{
+    Reference< beans::XPropertySet > xErrorBar( getErrorBars( xDataSeries, bYError ));
+    sal_Int32 nStyle = ::com::sun::star::chart::ErrorBarStyle::NONE;
+
+    return ( xErrorBar.is() &&
+             ( xErrorBar->getPropertyValue( C2U("ErrorBarStyle")) >>= nStyle ) &&
+             nStyle == ::com::sun::star::chart::ErrorBarStyle::FROM_DATA );
+}
 
 } //  namespace chart
