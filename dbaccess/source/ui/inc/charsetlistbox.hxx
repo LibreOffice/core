ddd90c0578c2e37f6e406e467827300ebdb86dd6
Kurt Zenker <kz@openoffice.org>
2008-03-07 10:24:13 +0000

    INTEGRATION: CWS textconnectionsettings (1.1.2); FILE ADDED 2008/03/05 12:51:52 fs 1.1.2.1: #i86720# dedicated class for displaying a list of Base-supported character sets

---
+++ dbaccess/source/ui/inc/charsetlistbox.hxx
@@ -0,0 +1,73 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: charsetlistbox.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-03-07 11:24:13 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef CHARSETLISTBOX_HXX
+#define CHARSETLISTBOX_HXX
+
+#include "charsets.hxx"
+
+/** === begin UNO includes === **/
+/** === end UNO includes === **/
+
+#include <vcl/lstbox.hxx>
+
+class SfxItemSet;
+
+//........................................................................
+namespace dbaui
+{
+//........................................................................
+
+    //====================================================================
+    //= CharSetListBox
+    //====================================================================
+    class CharSetListBox : public ListBox
+    {
+    public:
+        CharSetListBox( Window* _pParent, const ResId& _rResId );
+        ~CharSetListBox();
+
+        void    SelectEntryByIanaName( const String& _rIanaName );
+        bool    StoreSelectedCharSet( SfxItemSet& _rSet, const USHORT _nItemId );
+
+    private:
+        OCharsetDisplay     m_aCharSets;
+    };
+
+//........................................................................
+} // namespace dbaui
+//........................................................................
+
+#endif // CHARSETLISTBOX_HXX
