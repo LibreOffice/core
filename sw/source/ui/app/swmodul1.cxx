dfc7332d271dbe0771e5537435e998e993a9537b
Kurt Zenker <kz@openoffice.org>
2008-03-07 15:32:15 +0000

    INTEGRATION: CWS cjksp1_DEV300 (1.36.286); FILE MERGED 2007/10/23 06:03:48 pflin 1.36.286.5: RESYNC: (1.39-1.40); FILE MERGED 2007/09/27 04:29:40 pflin 1.36.286.4: RESYNC: (1.38-1.39); FILE MERGED 2007/07/03 04:50:19 pflin 1.36.286.3: RESYNC: (1.37-1.38); FILE MERGED 2007/05/08 10:21:04 pflin 1.36.286.2: RESYNC: (1.36-1.37); FILE MERGED 2007/04/23 08:01:37 pflin 1.36.286.1: Text grid enhancement: grid layout refactor

--- sw/source/ui/app/swmodul1.cxx
+++ sw/source/ui/app/swmodul1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swmodul1.cxx,v $
  *
- *  $Revision: 1.41 $
+ *  $Revision: 1.42 $
  *
- *  last change: $Author: rt $ $Date: 2008-02-19 13:53:14 $
+ *  last change: $Author: kz $ $Date: 2008-03-07 16:32:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -704,4 +704,11 @@ void SwModule::CheckSpellChanges( sal_Bool bOnlineSpelling,
 //      pSpell->SetSpellWrongAgain( sal_False );
 //      pSpell->SetSpellAllAgain( sal_False );
     }
+}
+
+void SwModule::ApplyDefaultPageMode(sal_Bool bIsSquaredPageMode)
+{
+    if(!pUsrPref)
+        GetUsrPref(sal_False);
+    pUsrPref->SetDefaultPageMode(bIsSquaredPageMode);
 }
