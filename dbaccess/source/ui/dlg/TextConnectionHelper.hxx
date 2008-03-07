bf258165fda87dd638c7275cd762f4be26f80fa2
Kurt Zenker <kz@openoffice.org>
2008-03-07 10:21:43 +0000

    INTEGRATION: CWS textconnectionsettings (1.5.166); FILE MERGED 2008/03/05 12:55:50 fs 1.5.166.1: #i86720# some re-factoring, owed to the fact that we now have a stand-alone dialog for entering text/csv settings, which re-uses existing functionality

--- dbaccess/source/ui/dlg/TextConnectionHelper.hxx
+++ dbaccess/source/ui/dlg/TextConnectionHelper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TextConnectionHelper.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 10:23:22 $
+ *  last change: $Author: kz $ $Date: 2008-03-07 11:21:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,6 +42,9 @@
 #ifndef _DBAUI_ADMINPAGES_HXX_
 #include "adminpages.hxx"
 #endif
+#ifndef CHARSETLISTBOX_HXX
+#include "charsetlistbox.hxx"
+#endif
 #ifndef _UCBHELPER_CONTENT_HXX
 #include <ucbhelper/content.hxx>
 #endif
@@ -57,7 +60,9 @@
 #ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
 #endif
-
+#ifndef _SV_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
 
 
 //.........................................................................
@@ -66,25 +71,32 @@ namespace dbaui
 {
 //.........................................................................
 
+    #define TC_EXTENSION    ((short)0x01)   // a section specifying the extension of the files to connect to
+    #define TC_SEPARATORS   ((short)0x02)   // a section specifying the various separators
+    #define TC_HEADER       ((short)0x04)   // a section containing the "Text contains header" check box only
+    #define TC_CHARSET      ((short)0x08)   // not yet implemented
 
     //========================================================================
     //= OTextConnectionPage
     //========================================================================
     class OTextConnectionHelper : public Control
     {
-    OTextConnectionHelper();
-    public:
-        OTextConnectionHelper( Window* pParent, sal_Bool _bWizardMode = sal_False);
+        OTextConnectionHelper();
+
         Link        m_aModifiedHandler;     /// to be called if something on the page has been modified
-          CheckBox  m_aHeader;
-        FixedLine   m_aLineFormat;
+
+    public:
+        OTextConnectionHelper( Window* pParent, const short _nAvailableSections );
+        virtual ~OTextConnectionHelper();
+
+    private:
         FixedText   m_aFTExtensionHeader;
         RadioButton m_aRBAccessTextFiles;
         RadioButton m_aRBAccessCSVFiles;
         RadioButton m_aRBAccessOtherFiles;
         Edit        m_aETOwnExtension;
         FixedText   m_aFTExtensionExample;
-
+        FixedLine   m_aLineFormat;
         FixedText   m_aFieldSeparatorLabel;
         ComboBox    m_aFieldSeparator;
         FixedText   m_aTextSeparatorLabel;
@@ -93,15 +105,18 @@ namespace dbaui
         ComboBox    m_aDecimalSeparator;
         FixedText   m_aThousandsSeparatorLabel;
         ComboBox    m_aThousandsSeparator;
+          CheckBox  m_aRowHeader;
+        FixedLine   m_aCharSetHeader;
+        FixedText   m_aCharSetLabel;
+        CharSetListBox  m_aCharSet;
         String      m_aFieldSeparatorList;
         String      m_aTextSeparatorList;
         String      m_aTextNone;
-        sal_Bool    m_bWizardMode;
         String      m_aOldExtension;
         Link        m_aGetExtensionHandler; /// to be called if a new type is selected
 
+        short       m_nAvailableSections;
 
-        virtual ~OTextConnectionHelper();
     protected:
         void callModifiedHdl() const { if (m_aModifiedHandler.IsSet()) m_aModifiedHandler.Call((void*)this); }
         Link getControlModifiedLink() { return LINK(this, OTextConnectionHelper, OnControlModified); }
@@ -116,7 +131,7 @@ namespace dbaui
 
 
     public:
-        void        implInitControls(const SfxItemSet& _rSet, sal_Bool _bSaveValue, sal_Bool _bValid);
+        void        implInitControls(const SfxItemSet& _rSet, sal_Bool _bValid);
         void        fillControls(::std::vector< ISaveValueWrapper* >& _rControlList);
         void        fillWindows(::std::vector< ISaveValueWrapper* >& _rControlList);
         void        SetClickHandler(const Link& _rHandler) { m_aGetExtensionHandler = _rHandler; }
