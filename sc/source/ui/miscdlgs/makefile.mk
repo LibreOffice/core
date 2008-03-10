b2c73a1d9f136fcacced7571f61f0563d26e0a86
Oliver Bolte <obo@openoffice.org>
2008-03-10 13:05:29 +0000

    #i86824#

--- sc/source/ui/miscdlgs/makefile.mk
+++ sc/source/ui/miscdlgs/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: kz $ $Date: 2008-03-07 12:22:58 $
+#   last change: $Author: obo $ $Date: 2008-03-10 14:05:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -87,9 +87,7 @@ SLOFILES =  \
 EXCEPTIONSFILES = \
     $(SLO)$/acredlin.obj        \
     $(SLO)$/conflictsdlg.obj    \
-    $(SLO)$/sharedocdlg.obj
-
-EXCEPTIONSFILES= \
+    $(SLO)$/sharedocdlg.obj		\
     $(SLO)$/optsolver.obj       \
     $(SLO)$/solveroptions.obj   \
     $(SLO)$/solverutil.obj
