ab5abed12d35e7a7d7fcd8058f320bdc08e15b9a
Oliver Bolte <obo@openoffice.org>
2008-03-10 07:32:14 +0000

    #i10000# return impl_createInstance( 0 ) removed

--- sfx2/source/doc/sfxmodelfactory.cxx
+++ sfx2/source/doc/sfxmodelfactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfxmodelfactory.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2008-03-06 19:56:18 $
+ *  last change: $Author: obo $ $Date: 2008-03-10 08:32:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -149,7 +149,6 @@ namespace sfx2
     //--------------------------------------------------------------------
     Reference< XInterface > SAL_CALL SfxModelFactory::createInstance(  ) throw (Exception, RuntimeException)
     {
-        return impl_createInstance( 0 );
         return createInstanceWithArguments( Sequence< Any >() );
     }
 
