d3aef04968b53259757a67552a61b56ee5cbb367
Oliver Bolte <obo@openoffice.org>
2008-03-10 06:20:54 +0000

    #i10000# colon removed

--- svtools/workben/unodialog/udlg_module.hxx
+++ svtools/workben/unodialog/udlg_module.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: udlg_module.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: kz $ $Date: 2008-03-06 19:29:26 $
+ *  last change: $Author: obo $ $Date: 2008-03-10 07:20:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,7 +43,7 @@ namespace udlg
 {
 //........................................................................
 
-    DEFINE_MODULE( UdlgModule, UdlgClient, UdlgResId );
+    DEFINE_MODULE( UdlgModule, UdlgClient, UdlgResId )
 
 //........................................................................
 } // namespace udlg
