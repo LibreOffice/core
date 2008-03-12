2e2c129ab0bd498d54b5517d0b2d4044461e3343
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:41:50 +0000

    INTEGRATION: CWS impresstables2 (1.7.46); FILE MERGED 2007/08/01 14:22:58 cl 1.7.46.2: RESYNC: (1.7-1.8); FILE MERGED 2007/07/27 09:09:53 cl 1.7.46.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/EnumPropertyHdl.cxx
+++ xmloff/source/style/EnumPropertyHdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: EnumPropertyHdl.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:23:33 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:41:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -83,7 +83,9 @@
 #include <com/sun/star/uno/Any.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::uno;
 
 ///////////////////////////////////////////////////////////////////////////////
