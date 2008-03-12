4449034c2521912418a782cfa2cd889f66613aa5
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:05:38 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/27 11:57:22 cl 1.1.2.8: #i68103# fixed clipboard 2008/02/15 14:22:23 cl 1.1.2.7: #i68103# 2008/01/24 17:07:52 cl 1.1.2.6: #i68103# reworked table layouter 2008/01/13 19:57:16 cl 1.1.2.5: #i68103# working on i18n 2008/01/01 18:37:42 cl 1.1.2.4: #i68103# working on tables 2007/10/11 15:18:52 cl 1.1.2.3: #i68103# added undo for tables 2007/03/20 12:54:51 cl 1.1.2.2: fixed unix compile errors 2007/03/15 17:16:20 cl 1.1.2.1: #i68103# adding a shape for tables

---
+++ svx/source/table/tablerow.cxx
@@ -0,0 +1,409 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tablerow.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:05:38 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <com/sun/star/lang/DisposedException.hpp>
+
+#include "cell.hxx"
+#include "tablerow.hxx"
+#include "tableundo.hxx"
+#include "svx/svdmodel.hxx"
+#include "svx/svdotable.hxx"
+
+// -----------------------------------------------------------------------------
+
+using ::rtl::OUString;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::table;
+using namespace ::com::sun::star::beans;
+
+// -----------------------------------------------------------------------------
+
+namespace sdr { namespace table {
+
+const sal_Int32 Property_Height = 0;
+const sal_Int32 Property_OptimalHeight = 1;
+const sal_Int32 Property_IsVisible = 2;
+const sal_Int32 Property_IsStartOfNewPage = 3;
+
+// -----------------------------------------------------------------------------
+// TableRow
+// -----------------------------------------------------------------------------
+
+TableRow::TableRow( const TableModelRef& xTableModel, sal_Int32 nRow, sal_Int32 nColumns )
+: TableRowBase( getStaticPropertySetInfo() )
+, mxTableModel( xTableModel )
+, mnRow( nRow )
+, mnHeight( 0 )
+, mbOptimalHeight( sal_True )
+, mbIsVisible( sal_True )
+, mbIsStartOfNewPage( sal_False )
+{
+    if( nColumns < 20 )
+        maCells.reserve( 20 );
+
+    if( nColumns )
+    {
+        maCells.resize( nColumns );
+        while( nColumns-- )
+            maCells[ nColumns ] = mxTableModel->createCell();
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+TableRow::~TableRow()
+{
+}
+
+// -----------------------------------------------------------------------------
+
+void TableRow::dispose()
+{
+    mxTableModel.clear();
+    if( !maCells.empty() )
+    {
+        CellVector::iterator aIter( maCells.begin() );
+        while( aIter != maCells.end() )
+            (*aIter++)->dispose();
+        CellVector().swap(maCells);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+void TableRow::throwIfDisposed() const throw (::com::sun::star::uno::RuntimeException)
+{
+    if( !mxTableModel.is() )
+        throw DisposedException();
+}
+
+// -----------------------------------------------------------------------------
+
+TableRow& TableRow::operator=( const TableRow& r )
+{
+    mnHeight = r.mnHeight;
+    mbOptimalHeight = r.mbOptimalHeight;
+    mbIsVisible = r.mbIsVisible;
+    mbIsStartOfNewPage = r.mbIsStartOfNewPage;
+    maName = r.maName;
+
+    return *this;
+}
+
+// -----------------------------------------------------------------------------
+
+void TableRow::insertColumns( sal_Int32 nIndex, sal_Int32 nCount, CellVector::iterator* pIter /* = 0 */  )
+{
+    throwIfDisposed();
+    if( nCount )
+    {
+        if( nIndex < static_cast< sal_Int32 >( maCells.size() ) )
+        {
+            CellVector::iterator aIter( maCells.begin() );
+            while( nIndex-- )
+                aIter++;
+            while( nCount-- )
+            {
+                CellRef xCell;
+                if( pIter )
+                    xCell = (*(*pIter)++);
+                else
+                    xCell = mxTableModel->createCell();
+
+                maCells.insert( aIter, xCell );
+            }
+        }
+        else
+        {
+            nIndex = static_cast< sal_Int32 >( maCells.size() );
+            maCells.resize( nIndex + nCount );
+            while(nCount--)
+            {
+                CellRef xCell;
+                if( pIter )
+                    xCell = (*(*pIter)++);
+                else
+                    xCell = mxTableModel->createCell();
+
+                maCells[nIndex++] = xCell;
+            }
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+void TableRow::removeColumns( sal_Int32 nIndex, sal_Int32 nCount )
+{
+    throwIfDisposed();
+    if( (nCount >= 0) && ( nIndex >= 0) )
+    {
+        if( (nIndex + nCount) < static_cast< sal_Int32 >( maCells.size() ) )
+        {
+            CellVector::iterator aBegin( maCells.begin() );
+            while( nIndex-- && (aBegin != maCells.end()) )
+                aBegin++;
+
+            if( nCount > 1 )
+            {
+                CellVector::iterator aEnd( aBegin );
+                while( nCount-- && (aEnd != maCells.end()) )
+                    aEnd++;
+                maCells.erase( aBegin, aEnd );
+            }
+            else
+            {
+                maCells.erase( aBegin );
+            }
+        }
+        else
+        {
+            maCells.resize( nIndex );
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// XCellRange
+// -----------------------------------------------------------------------------
+
+Reference< XCell > SAL_CALL TableRow::getCellByPosition( sal_Int32 nColumn, sal_Int32 nRow ) throw (IndexOutOfBoundsException, RuntimeException)
+{
+    throwIfDisposed();
+    if( nRow != 0 )
+        throw IndexOutOfBoundsException();
+
+    return mxTableModel->getCellByPosition( nColumn, mnRow );
+}
+
+// -----------------------------------------------------------------------------
+
+Reference< XCellRange > SAL_CALL TableRow::getCellRangeByPosition( sal_Int32 nLeft, sal_Int32 nTop, sal_Int32 nRight, sal_Int32 nBottom ) throw (IndexOutOfBoundsException, RuntimeException)
+{
+    throwIfDisposed();
+    if( (nLeft >= 0 ) && (nTop == 0) && (nRight >= nLeft) && (nBottom == 0)  )
+    {
+        return mxTableModel->getCellRangeByPosition( nLeft, mnRow, nRight, mnRow );
+    }
+    throw IndexOutOfBoundsException();
+}
+
+// -----------------------------------------------------------------------------
+
+Reference< XCellRange > SAL_CALL TableRow::getCellRangeByName( const OUString& /*aRange*/ ) throw (RuntimeException)
+{
+    throwIfDisposed();
+    return Reference< XCellRange >();
+}
+
+// -----------------------------------------------------------------------------
+// XNamed
+// -----------------------------------------------------------------------------
+
+OUString SAL_CALL TableRow::getName() throw (RuntimeException)
+{
+    return maName;
+}
+
+// -----------------------------------------------------------------------------
+
+void SAL_CALL TableRow::setName( const OUString& aName ) throw (RuntimeException)
+{
+    maName = aName;
+}
+
+// -----------------------------------------------------------------------------
+// XFastPropertySet
+// -----------------------------------------------------------------------------
+
+void SAL_CALL TableRow::setFastPropertyValue( sal_Int32 nHandle, const Any& aValue ) throw (UnknownPropertyException, PropertyVetoException, IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, RuntimeException)
+{
+    bool bOk = false;
+    bool bChange = false;
+
+    TableRowUndo* pUndo = 0;
+    if( mxTableModel.is() && mxTableModel->getSdrTableObj() && mxTableModel->getSdrTableObj()->IsInserted() )
+    {
+        TableRowRef xThis( this );
+        pUndo = new TableRowUndo( xThis );
+    }
+
+    switch( nHandle )
+    {
+    case Property_Height:
+        {
+            sal_Int32 nHeight = mnHeight;
+            bOk = aValue >>= nHeight;
+            if( bOk && (mnHeight != nHeight) )
+            {
+                mnHeight = nHeight;
+                mbOptimalHeight = mnHeight == 0;
+                bChange = true;
+            }
+            break;
+        }
+
+    case Property_OptimalHeight:
+        {
+            sal_Bool bOptimalHeight = mbOptimalHeight;
+            bOk = aValue >>= bOptimalHeight;
+            if( bOk && (mbOptimalHeight != bOptimalHeight) )
+            {
+                mbOptimalHeight = bOptimalHeight;
+                if( bOptimalHeight )
+                    mnHeight = 0;
+                bChange = true;
+            }
+            break;
+        }
+    case Property_IsVisible:
+        {
+            sal_Bool bIsVisible = mbIsVisible;
+            bOk = aValue >>= bIsVisible;
+            if( bOk && (mbIsVisible != bIsVisible) )
+            {
+                mbIsVisible = bIsVisible;
+                bChange = true;
+            }
+            break;
+        }
+
+    case Property_IsStartOfNewPage:
+        {
+            sal_Bool bIsStartOfNewPage = mbIsStartOfNewPage;
+            bOk = aValue >>= bIsStartOfNewPage;
+            if( bOk && (mbIsStartOfNewPage != bIsStartOfNewPage) )
+            {
+                mbIsStartOfNewPage = bIsStartOfNewPage;
+                bChange = true;
+            }
+            break;
+        }
+    default:
+        throw UnknownPropertyException();
+    }
+    if( !bOk )
+        throw IllegalArgumentException();
+
+    if( bChange )
+    {
+        if( pUndo )
+        {
+            SdrModel* pModel = mxTableModel->getSdrTableObj()->GetModel();
+            if( pModel )
+            {
+                pModel->AddUndo( pUndo );
+                pUndo = 0;
+            }
+        }
+        mxTableModel->setModified(sal_True);
+    }
+
+    if( pUndo )
+        delete pUndo;
+}
+
+// -----------------------------------------------------------------------------
+
+Any SAL_CALL TableRow::getFastPropertyValue( sal_Int32 nHandle ) throw (UnknownPropertyException, WrappedTargetException, RuntimeException)
+{
+    switch( nHandle )
+    {
+    case Property_Height:           return Any( mnHeight );
+    case Property_OptimalHeight:    return Any( mbOptimalHeight );
+    case Property_IsVisible:        return Any( mbIsVisible );
+    case Property_IsStartOfNewPage: return Any( mbIsStartOfNewPage );
+    default:                        throw UnknownPropertyException();
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+rtl::Reference< ::comphelper::FastPropertySetInfo > TableRow::getStaticPropertySetInfo()
+{
+    static rtl::Reference< ::comphelper::FastPropertySetInfo > xInfo;
+    if( !xInfo.is() )
+    {
+        ::osl::MutexGuard aGuard( ::osl::Mutex::getGlobalMutex() );
+        if( !xInfo.is() )
+        {
+            comphelper::PropertyVector aProperties(6);
+
+            aProperties[0].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "Height" ) );
+            aProperties[0].Handle = Property_Height;
+            aProperties[0].Type = ::getCppuType((const sal_Int32*)0);
+            aProperties[0].Attributes = 0;
+
+            aProperties[1].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "OptimalHeight" ) );
+            aProperties[1].Handle = Property_OptimalHeight;
+            aProperties[1].Type = ::getBooleanCppuType();
+            aProperties[1].Attributes = 0;
+
+            aProperties[2].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "IsVisible" ) );
+            aProperties[2].Handle = Property_IsVisible;
+            aProperties[2].Type = ::getBooleanCppuType();
+            aProperties[2].Attributes = 0;
+
+            aProperties[3].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "IsStartOfNewPage" ) );
+            aProperties[3].Handle = Property_IsStartOfNewPage;
+            aProperties[3].Type = ::getBooleanCppuType();
+            aProperties[3].Attributes = 0;
+
+            aProperties[4].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "Size" ) );
+            aProperties[4].Handle = Property_Height;
+            aProperties[4].Type = ::getCppuType((const sal_Int32*)0);
+            aProperties[4].Attributes = 0;
+
+            aProperties[5].Name = OUString( RTL_CONSTASCII_USTRINGPARAM( "OptimalSize" ) );
+            aProperties[5].Handle = Property_OptimalHeight;
+            aProperties[5].Type = ::getBooleanCppuType();
+            aProperties[5].Attributes = 0;
+
+            xInfo.set( new ::comphelper::FastPropertySetInfo(aProperties) );
+        }
+    }
+
+    return xInfo;
+}
+
+// -----------------------------------------------------------------------------
+
+
+} }
