7cb0ed1b9c4345265c68ee23f40aac0b897fbb0f
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:09:28 +0000

    INTEGRATION: CWS impresstables2 (1.21.34); FILE MERGED 2007/08/01 14:52:49 cl 1.21.34.2: RESYNC: (1.21-1.22); FILE MERGED 2007/07/27 09:09:56 cl 1.21.34.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/txtftne.cxx
+++ xmloff/source/text/txtftne.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtftne.cxx,v $
  *
- *  $Revision: 1.22 $
+ *  $Revision: 1.23 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:15:16 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:09:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,7 +130,9 @@
 #include <xmloff/txtparae.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
