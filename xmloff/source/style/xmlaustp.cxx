e3b63fbd2156aee7e8682046172ce0c72b68bc48
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:55:09 +0000

    INTEGRATION: CWS impresstables2 (1.23.36); FILE MERGED 2007/08/01 14:35:30 cl 1.23.36.2: RESYNC: (1.23-1.24); FILE MERGED 2007/07/27 09:09:54 cl 1.23.36.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/xmlaustp.cxx
+++ xmloff/source/style/xmlaustp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlaustp.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:46:17 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:55:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,7 +79,9 @@
 
 
 using namespace ::std;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
