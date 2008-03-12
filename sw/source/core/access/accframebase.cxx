cddc5d566939f05ed7cffb8ab9d0c4b66bcd655a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:13:59 +0000

    INTEGRATION: CWS impresstables2 (1.10.82); FILE MERGED 2007/10/11 22:11:41 cl 1.10.82.2: RESYNC: (1.10-1.11); FILE MERGED 2007/10/11 13:46:24 cl 1.10.82.1: #i68103# fixed namespace issues

--- sw/source/core/access/accframebase.cxx
+++ sw/source/core/access/accframebase.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accframebase.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:20:45 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:13:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -93,7 +93,7 @@
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 sal_Bool SwAccessibleFrameBase::IsSelected()
 {
