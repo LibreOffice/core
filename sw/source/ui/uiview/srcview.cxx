c77bbeaba4b78226af0ffe109deb334a32f86221
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:56:45 +0000

    INTEGRATION: CWS impresstables2 (1.53.20); FILE MERGED 2008/03/11 18:50:21 cl 1.53.20.3: RESYNC: (1.54-1.56); FILE MERGED 2007/10/12 00:30:47 cl 1.53.20.2: RESYNC: (1.53-1.54); FILE MERGED 2007/10/11 14:21:51 cl 1.53.20.1: #i68103# fixed namespace issues

--- sw/source/ui/uiview/srcview.cxx
+++ sw/source/ui/uiview/srcview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: srcview.cxx,v $
  *
- *  $Revision: 1.56 $
+ *  $Revision: 1.57 $
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 16:48:23 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:56:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -222,7 +222,7 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::ui::dialogs;
 using namespace ::com::sun::star::i18n;
 using namespace ::com::sun::star::lang;
-using namespace ::rtl;
+using ::rtl::OUString;
 using namespace ::sfx2;
 
 
