388753a47cfb469ba48355d7102f6c45d387c066
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:31:12 +0000

    INTEGRATION: CWS impresstables2 (1.16.30); FILE MERGED 2007/08/01 14:09:39 cl 1.16.30.2: RESYNC: (1.16-1.17); FILE MERGED 2007/07/27 09:09:51 cl 1.16.30.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/draw/XMLShapeStyleContext.cxx
+++ xmloff/source/draw/XMLShapeStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLShapeStyleContext.cxx,v $
  *
- *  $Revision: 1.17 $
+ *  $Revision: 1.18 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:00:31 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:31:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -84,7 +84,9 @@
 
 #include "sdpropls.hxx"
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
