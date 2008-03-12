28c83844c86b8e66f140b5c0b588dc09ac6f25d7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:39:19 +0000

    INTEGRATION: CWS impresstables2 (1.36.38); FILE MERGED 2007/10/11 23:30:59 cl 1.36.38.2: RESYNC: (1.36-1.38); FILE MERGED 2007/10/11 14:06:10 cl 1.36.38.1: #i68103# fixed namespace issues

--- sw/source/filter/xml/xmlfmt.cxx
+++ sw/source/filter/xml/xmlfmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlfmt.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 10:10:29 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:39:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -141,7 +141,8 @@
 #include <docsh.hxx>
 
 using namespace ::com::sun::star;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
 using namespace ::xmloff::token;
 
 class SwXMLConditionParser_Impl
