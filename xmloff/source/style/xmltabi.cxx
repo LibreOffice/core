f7e990c5b0b874a3755a529117670b2f1abeaff3
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:59:05 +0000

    INTEGRATION: CWS impresstables2 (1.14.80); FILE MERGED 2007/08/01 14:37:22 cl 1.14.80.2: RESYNC: (1.14-1.15); FILE MERGED 2007/07/27 09:09:55 cl 1.14.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/xmltabi.cxx
+++ xmloff/source/style/xmltabi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltabi.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:49:46 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:59:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -76,7 +76,9 @@
 #define _SVSTDARR_USHORTS
 #include <svtools/svstdarr.hxx>
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
