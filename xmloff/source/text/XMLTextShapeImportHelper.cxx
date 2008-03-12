a34e3d388c0f6b411e8c6c8608bbf36311923f92
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:06:33 +0000

    INTEGRATION: CWS impresstables2 (1.14.70); FILE MERGED 2007/08/01 14:51:34 cl 1.14.70.2: RESYNC: (1.14-1.15); FILE MERGED 2007/07/27 09:09:56 cl 1.14.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/XMLTextShapeImportHelper.cxx
+++ xmloff/source/text/XMLTextShapeImportHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextShapeImportHelper.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:12:21 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:06:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -69,7 +69,9 @@
 #include "XMLTextShapeImportHelper.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::frame;
 using namespace ::com::sun::star::drawing;
