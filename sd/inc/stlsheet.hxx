92e33606e48917573ab27958462244995bbe1db4
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:22:55 +0000

    INTEGRATION: CWS impresstables2 (1.3.378); FILE MERGED 2008/02/07 20:07:59 cl 1.3.378.3: #i68103# add XModifyBroadcaster to SdStyleSheet 2008/01/29 18:02:17 cl 1.3.378.2: #i68103# added SfxUnoStyleSheet to fix a dynamic_cast issue under linux 2007/07/26 17:47:06 cl 1.3.378.1: #i68103# added cell styles

--- sd/inc/stlsheet.hxx
+++ sd/inc/stlsheet.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: stlsheet.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:03:23 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:22:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,26 +36,39 @@
 #ifndef _SD_STLSHEET_HXX
 #define _SD_STLSHEET_HXX
 
+#include <rtl/ref.hxx>
 
-#ifndef _SFXSTYLE_HXX //autogen
+#include <com/sun/star/style/XStyle.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/beans/XPropertyState.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/util/XModifyBroadcaster.hpp>
+
+#include <cppuhelper/interfacecontainer.h>
+#include <cppuhelper/implbase5.hxx>
+#include <cppuhelper/basemutex.hxx>
+
 #include <svtools/style.hxx>
-#endif
-class SdStyleSheet : public SfxStyleSheet
+
+#include <svx/unoipset.hxx>
+
+#include <boost/scoped_ptr.hpp>
+
+class ModifyListenerForewarder;
+
+typedef cppu::ImplInheritanceHelper5< SfxUnoStyleSheet,
+                                    ::com::sun::star::beans::XPropertySet,
+                                    ::com::sun::star::lang::XServiceInfo,
+                                    ::com::sun::star::beans::XPropertyState,
+                                    ::com::sun::star::util::XModifyBroadcaster,
+                                    ::com::sun::star::lang::XComponent > SdStyleSheetBase ;
+
+class SdStyleSheet : public SdStyleSheetBase, private ::cppu::BaseMutex
 {
-protected:
-    virtual void Load (SvStream& rIn, USHORT nVersion);
-    virtual void Store(SvStream& rOut);
-
-    virtual void SFX_NOTIFY(SfxBroadcaster& rBC, const TypeId& rBCType,
-                            const SfxHint& rHint, const TypeId& rHintType);
-
 public:
-                        TYPEINFO();
-                        SdStyleSheet(const String& rName,
-                                     SfxStyleSheetBasePool& rPool,
-                                     SfxStyleFamily eFamily,
-                                     USHORT nMask);
-    virtual             ~SdStyleSheet();
+    SdStyleSheet( const rtl::OUString& rDisplayName, SfxStyleSheetBasePool& rPool, SfxStyleFamily eFamily, USHORT nMask );
+    SdStyleSheet( const SdStyleSheet& );
 
     virtual BOOL        SetParent (const String& rParentName);
     virtual SfxItemSet& GetItemSet();
@@ -63,15 +76,92 @@ public:
     virtual BOOL        HasFollowSupport() const;
     virtual BOOL        HasParentSupport() const;
     virtual BOOL        HasClearParentSupport() const;
+    virtual BOOL        SetName( const UniString& );
+    virtual void        SetHelpId( const String& r, ULONG nId );
 
-            List*       CreateChildList() const;
-
-            void        AdjustToFontHeight(SfxItemSet& rSet,
-                                           BOOL bOnlyMissingItems = TRUE);
+    void        AdjustToFontHeight(SfxItemSet& rSet, BOOL bOnlyMissingItems = TRUE);
 
     SdStyleSheet* GetRealStyleSheet() const;
     SdStyleSheet* GetPseudoStyleSheet() const;
+
+    void SetApiName( const ::rtl::OUString& rApiName );
+    rtl::OUString GetApiName() const { return msApiName; }
+
+    static rtl::OUString GetFamilyString( SfxStyleFamily eFamily );
+
+    static SdStyleSheet* CreateEmptyUserStyle( SfxStyleSheetBasePool& rPool, SfxStyleFamily eFamily );
+
+    // XInterface
+    virtual void SAL_CALL release(  ) throw ();
+
+    // XServiceInfo
+    virtual ::rtl::OUString SAL_CALL getImplementationName() throw(::com::sun::star::uno::RuntimeException);
+    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) throw(::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames() throw(::com::sun::star::uno::RuntimeException);
+
+    // XNamed
+    virtual ::rtl::OUString SAL_CALL getName(  ) throw(::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setName( const ::rtl::OUString& aName ) throw(::com::sun::star::uno::RuntimeException);
+
+    // XStyle
+    virtual sal_Bool SAL_CALL isUserDefined(  ) throw(::com::sun::star::uno::RuntimeException);
+    virtual sal_Bool SAL_CALL isInUse(  ) throw(::com::sun::star::uno::RuntimeException);
+    virtual ::rtl::OUString SAL_CALL getParentStyle(  ) throw(::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setParentStyle( const ::rtl::OUString& aParentStyle ) throw(::com::sun::star::container::NoSuchElementException, ::com::sun::star::uno::RuntimeException);
+
+    // XPropertySet
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo() throw(::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setPropertyValue( const ::rtl::OUString& aPropertyName, const ::com::sun::star::uno::Any& aValue ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Any SAL_CALL getPropertyValue( const ::rtl::OUString& PropertyName ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL addPropertyChangeListener( const ::rtl::OUString& aPropertyName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyChangeListener >& xListener ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removePropertyChangeListener( const ::rtl::OUString& aPropertyName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertyChangeListener >& aListener ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL addVetoableChangeListener( const ::rtl::OUString& PropertyName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XVetoableChangeListener >& aListener ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeVetoableChangeListener( const ::rtl::OUString& PropertyName, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XVetoableChangeListener >& aListener ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    // XPropertyState
+    virtual ::com::sun::star::beans::PropertyState SAL_CALL getPropertyState( const ::rtl::OUString& PropertyName ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyState > SAL_CALL getPropertyStates( const ::com::sun::star::uno::Sequence< ::rtl::OUString >& aPropertyName ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setPropertyToDefault( const ::rtl::OUString& PropertyName ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Any SAL_CALL getPropertyDefault( const ::rtl::OUString& aPropertyName ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+    // XModifyBroadcaster
+    virtual void SAL_CALL addModifyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeModifyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::util::XModifyListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+    // XComponent
+    virtual void SAL_CALL dispose(  ) throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+    void notifyModifyListener();
+
+protected:
+    const SfxItemPropertyMap* getPropertyMapEntry( const ::rtl::OUString& rPropertyName ) const throw();
+
+    virtual void Load (SvStream& rIn, USHORT nVersion);
+    virtual void Store(SvStream& rOut);
+
+    virtual void SFX_NOTIFY(SfxBroadcaster& rBC, const TypeId& rBCType, const SfxHint& rHint, const TypeId& rHintType);
+    virtual             ~SdStyleSheet();
+
+    void throwIfDisposed() throw (::com::sun::star::uno::RuntimeException);
+
+    virtual void disposing();
+
+    rtl::OUString   msApiName;
+    rtl::Reference< SfxStyleSheetBasePool > mxPool;
+
+    /** boradcast helper for events */
+    ::cppu::OBroadcastHelper mrBHelper;
+
+    boost::scoped_ptr< ModifyListenerForewarder > mpModifyListenerForewarder;
+
+private:
+    SdStyleSheet& operator=( const SdStyleSheet& ); // not implemented
 };
+
+typedef rtl::Reference< SdStyleSheet > SdStyleSheetRef;
+typedef std::vector< SdStyleSheetRef > SdStyleSheetVector;
 
 #endif     // _SD_STLSHEET_HXX
 
