e019a10e7af435c73848fac4b4473206f54971e6
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:16:44 +0000

    INTEGRATION: CWS impresstables2 (1.6.80); FILE MERGED 2007/08/01 14:59:13 cl 1.6.80.2: RESYNC: (1.6-1.7); FILE MERGED 2007/07/27 09:10:00 cl 1.6.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/TransformerContext.cxx
+++ xmloff/source/transform/TransformerContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: TransformerContext.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:25:50 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:16:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,8 @@
 #include "TransformerContext.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
 
