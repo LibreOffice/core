3f50e676906f8b0be355af66fd7b0b7c58d10a33
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:16:12 +0000

    INTEGRATION: CWS impresstables2 (1.28.82); FILE MERGED 2007/10/11 22:14:21 cl 1.28.82.2: RESYNC: (1.28-1.30); FILE MERGED 2007/10/11 13:46:25 cl 1.28.82.1: #i68103# fixed namespace issues

--- sw/source/core/access/acctable.cxx
+++ sw/source/core/access/acctable.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acctable.cxx,v $
  *
- *  $Revision: 1.30 $
+ *  $Revision: 1.31 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:24:14 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:16:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -115,7 +115,8 @@
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
 
 const sal_Char sServiceName[] = "com.sun.star.table.AccessibleTableView";
 const sal_Char sImplementationName[] = "com.sun.star.comp.Writer.SwAccessibleTableView";
