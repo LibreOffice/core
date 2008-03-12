417c4ef4045a89cee4b36346d329563673b0cadd
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:48:08 +0000

    INTEGRATION: CWS impresstables2 (1.4.46); FILE MERGED 2007/08/01 14:28:53 cl 1.4.46.2: RESYNC: (1.4-1.5); FILE MERGED 2007/07/27 09:09:54 cl 1.4.46.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/XMLPercentOrMeasurePropertyHandler.cxx
+++ xmloff/source/style/XMLPercentOrMeasurePropertyHandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLPercentOrMeasurePropertyHandler.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:33:52 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:48:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,9 @@
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 
 XMLPercentOrMeasurePropertyHandler::XMLPercentOrMeasurePropertyHandler( sal_Bool bPercent )
 : mbPercent( bPercent )
