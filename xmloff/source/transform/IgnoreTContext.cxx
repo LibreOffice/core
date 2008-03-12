3479212ccd9b7cc1992d76ffb7a295bc0e360528
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:14:53 +0000

    INTEGRATION: CWS impresstables2 (1.6.80); FILE MERGED 2007/07/27 09:10:00 cl 1.6.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/IgnoreTContext.cxx
+++ xmloff/source/transform/IgnoreTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: IgnoreTContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:25:04 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:14:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,7 +43,8 @@
 #include "TransformerBase.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
 
