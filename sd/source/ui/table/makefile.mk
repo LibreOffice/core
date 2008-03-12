2b0ef21a33a8657d7ee079f046041b592a591713
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:47:25 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2007/12/01 20:32:13 cl 1.1.2.4: fixing unix compile problem 2007/10/11 15:48:57 cl 1.1.2.3: #i68103# initial table style implemenation 2007/05/03 09:26:27 cl 1.1.2.2: new table toolbar 2007/04/13 09:47:52 cl 1.1.2.1: #i68103# draft table user interface

---
+++ sd/source/ui/table/makefile.mk
@@ -0,0 +1,64 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: rt $ $Date: 2008-03-12 11:47:25 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PROJECTPCH=sd
+PROJECTPCHSOURCE=$(PRJ)$/util$/sd
+PRJNAME=sd
+TARGET=uitable
+ENABLE_EXCEPTIONS=TRUE
+AUTOSEG=true
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+# --- Files --------------------------------------------------------
+
+SRS1NAME=$(TARGET)
+SRC1FILES =\
+    TableDesignPane.src\
+
+SLOFILES = \
+        $(SLO)$/tablefunction.obj\
+        $(SLO)$/tableobjectbar.obj\
+        $(SLO)$/TableDesignPane.obj
+
+
+# --- Tagets -------------------------------------------------------
+
+.INCLUDE :  target.mk
