dd1bf62149a9739c429bb957d4494cb583a66915
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:43:26 +0000

    INTEGRATION: CWS impresstables2 (1.44.60); FILE MERGED 2008/02/20 17:47:36 cl 1.44.60.4: RESYNC: (1.46-1.47); FILE MERGED 2007/12/01 19:22:26 cl 1.44.60.3: RESYNC: (1.45-1.46); FILE MERGED 2007/10/11 23:33:04 cl 1.44.60.2: RESYNC: (1.44-1.45); FILE MERGED 2007/10/11 14:08:35 cl 1.44.60.1: #i68103# fixed namespace issues

--- sw/source/filter/xml/xmltexte.cxx
+++ sw/source/filter/xml/xmltexte.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltexte.cxx,v $
  *
- *  $Revision: 1.47 $
+ *  $Revision: 1.48 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 09:25:16 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:43:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -122,7 +122,8 @@
 #include <SwStyleNameMapper.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::style;
