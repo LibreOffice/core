a0958cbd9c1709184de728a0712626a8267997bf
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:54:45 +0000

    INTEGRATION: CWS impresstables2 (1.11.400); FILE MERGED 2008/03/11 18:50:16 cl 1.11.400.3: RESYNC: (1.12-1.13); FILE MERGED 2007/10/12 00:25:38 cl 1.11.400.2: RESYNC: (1.11-1.12); FILE MERGED 2007/10/11 14:18:40 cl 1.11.400.1: #i68103# fixed namespace issues

--- sw/source/ui/shells/textdrw.cxx
+++ sw/source/ui/shells/textdrw.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textdrw.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 16:31:49 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:54:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -92,7 +92,7 @@
 
 
 using namespace ::com::sun::star;
-using namespace ::rtl;
+using ::rtl::OUString;
 /*---------------------------------------------------------------------------
     Beschreibung:
  ----------------------------------------------------------------------------*/
