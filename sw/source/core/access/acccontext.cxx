441e44b33f4f75eb65a94c78cc4767efe75e69a4
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:12:52 +0000

    INTEGRATION: CWS impresstables2 (1.55.82); FILE MERGED 2007/10/11 22:10:14 cl 1.55.82.2: RESYNC: (1.55-1.56); FILE MERGED 2007/10/11 13:43:27 cl 1.55.82.1: #i68103# fixed namespace issues

--- sw/source/core/access/acccontext.cxx
+++ sw/source/core/access/acccontext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: acccontext.cxx,v $
  *
- *  $Revision: 1.56 $
+ *  $Revision: 1.57 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:19:02 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:12:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -141,7 +141,7 @@ void lcl_SwAccessibleContext_DbgMsg( SwAccessibleContext *pThisAcc,
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 void SwAccessibleContext::InitStates()
 {
