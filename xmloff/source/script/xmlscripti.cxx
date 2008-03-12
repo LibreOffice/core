55f3a5e8fbb4b2aadb5f366bc2efd23816c8f846
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:40:45 +0000

    INTEGRATION: CWS impresstables2 (1.14.40); FILE MERGED 2007/12/02 01:05:00 cl 1.14.40.1: fixing namespace problems

--- xmloff/source/script/xmlscripti.cxx
+++ xmloff/source/script/xmlscripti.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlscripti.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:22:32 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:40:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 
 #include <com/sun/star/document/XEventsSupplier.hpp>
 
-using namespace rtl;
+using ::rtl::OUString;
 using namespace com::sun::star;
 using namespace com::sun::star::uno;
 using namespace com::sun::star::lang;
