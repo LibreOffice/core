d4cc55f73fbf7ce4de2f12afccad3248eac45367
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:00:29 +0000

    INTEGRATION: CWS impresstables2 (1.5.80); FILE MERGED 2007/08/01 14:39:16 cl 1.5.80.2: RESYNC: (1.5-1.6); FILE MERGED 2007/07/27 09:09:55 cl 1.5.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/XMLCalculationSettingsContext.cxx
+++ xmloff/source/text/XMLCalculationSettingsContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLCalculationSettingsContext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:52:12 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:00:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,9 @@
 #endif
 
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
 using namespace ::com::sun::star::text;
