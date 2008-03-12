cff3986a8bfeefe3e6569b84f9f692094e0c9922
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:31:41 +0000

    INTEGRATION: CWS hro34 (1.5.144); FILE MERGED 2008/03/07 10:38:14 hro 1.5.144.1: #i78851# #i72556# Implemented Vista style File Dialog

--- fpicker/source/win32/filepicker/FilterContainer.cxx
+++ fpicker/source/win32/filepicker/FilterContainer.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FilterContainer.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: obo $ $Date: 2006-10-12 10:48:49 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:31:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -243,6 +243,17 @@ sal_Bool SAL_CALL CFilterContainer::getNextFilter( FILTER_ENTRY_T& nextFilterEnt
     return bRet;
 }
 
+//-----------------------------------------------------------------------------------------
+void SAL_CALL CFilterContainer::setCurrentFilter( const ::rtl::OUString& aName )
+{
+    m_sCurrentFilter = aName;
+}
+
+//-----------------------------------------------------------------------------------------
+::rtl::OUString SAL_CALL CFilterContainer::getCurrentFilter() const
+{
+    return m_sCurrentFilter;
+}
 
 //###################################################################
 
