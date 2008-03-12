c784a653c0e4d7b6c16d36621afc5c8d0742763f
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:53:03 +0000

    INTEGRATION: CWS impresstables2 (1.7.46); FILE MERGED 2007/08/01 14:33:18 cl 1.7.46.2: RESYNC: (1.7-1.8); FILE MERGED 2007/07/27 09:09:54 cl 1.7.46.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/postuhdl.cxx
+++ xmloff/source/style/postuhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: postuhdl.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:42:29 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:53:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -68,7 +68,9 @@
 #include <xmloff/xmlelement.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
