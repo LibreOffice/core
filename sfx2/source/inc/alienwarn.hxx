e7a3e7ecf131dc697ddfb517b28a077366a05a73
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:34:46 +0000

    INTEGRATION: CWS odfversion12_DEV300 (1.5.182); FILE MERGED 2008/03/03 16:27:24 pb 1.5.182.1: fix: #i86649# controls renamed

--- sfx2/source/inc/alienwarn.hxx
+++ sfx2/source/inc/alienwarn.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: alienwarn.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 23:25:56 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:34:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,11 +49,11 @@ class SfxAlienWarningDialog : public SfxModalDialog
 private:
     FixedImage              m_aQueryImage;
     FixedText               m_aInfoText;
-    OKButton                m_aYesBtn;
-    CancelButton            m_aNoBtn;
+    OKButton                m_aKeepCurrentBtn;
+    CancelButton            m_aSaveODFBtn;
     HelpButton              m_aMoreInfoBtn;
     FixedLine               m_aOptionLine;
-    CheckBox                m_aWarningOffBox;
+    CheckBox                m_aWarningOnBox;
 
     void                    InitSize();
 
