e710f1a68b878e9b27a2fb58613e00dcad87cc67
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:15:58 +0000

    INTEGRATION: CWS impresstables2 (1.4.80); FILE MERGED 2007/08/01 14:57:18 cl 1.4.80.2: RESYNC: (1.4-1.5); FILE MERGED 2007/07/27 09:10:00 cl 1.4.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/ProcAttrTContext.cxx
+++ xmloff/source/transform/ProcAttrTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ProcAttrTContext.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:23:14 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:15:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,8 @@
 #include <xmloff/nmspmap.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
 using namespace ::xmloff::token;
