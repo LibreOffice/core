1fa250154673f7dc5ecaed02d51a148f59089cf3
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:54:11 +0000

    INTEGRATION: CWS impresstables2 (1.19.12); FILE MERGED 2007/08/01 14:34:27 cl 1.19.12.2: RESYNC: (1.19-1.21); FILE MERGED 2007/07/27 09:09:54 cl 1.19.12.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/styleexp.cxx
+++ xmloff/source/style/styleexp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: styleexp.cxx,v $
  *
- *  $Revision: 1.21 $
+ *  $Revision: 1.22 $
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 09:44:50 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:54:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -114,7 +114,9 @@
 #include <xmloff/XMLEventExport.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::style;
