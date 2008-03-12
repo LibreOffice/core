33a4d83a1aa1439c77c8df24b1a5b0a1c4e073c7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:45:53 +0000

    INTEGRATION: CWS impresstables2 (1.5.70); FILE MERGED 2007/08/01 14:27:45 cl 1.5.70.2: RESYNC: (1.5-1.6); FILE MERGED 2007/07/27 09:09:53 cl 1.5.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/XMLClipPropertyHandler.cxx
+++ xmloff/source/style/XMLClipPropertyHandler.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLClipPropertyHandler.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:31:38 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:45:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -63,7 +63,9 @@
 #include <xmloff/xmltoken.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::text;
