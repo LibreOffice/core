5e76807d0d4edde209a1c65877e31140175a999a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:15:09 +0000

    INTEGRATION: CWS impresstables2 (1.7.80); FILE MERGED 2007/08/01 14:56:21 cl 1.7.80.2: RESYNC: (1.7-1.8); FILE MERGED 2007/07/27 09:10:00 cl 1.7.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/MutableAttrList.cxx
+++ xmloff/source/transform/MutableAttrList.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: MutableAttrList.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:21:27 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:15:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,8 @@
 #include "MutableAttrList.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::osl;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
