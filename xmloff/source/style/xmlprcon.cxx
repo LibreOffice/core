6a15949aeec369b650cae91cd5b19dab2ef524c6
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:57:47 +0000

    INTEGRATION: CWS impresstables2 (1.7.80); FILE MERGED 2007/08/01 14:36:40 cl 1.7.80.2: RESYNC: (1.7-1.8); FILE MERGED 2007/07/27 09:09:55 cl 1.7.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/xmlprcon.cxx
+++ xmloff/source/style/xmlprcon.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlprcon.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:48:31 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:57:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,9 @@
 #include <xmloff/xmlprcon.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::std;
 
