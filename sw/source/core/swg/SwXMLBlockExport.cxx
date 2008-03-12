2aa92f20fc75d9085a70d88159edcf50b73fedfc
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:22:50 +0000

    INTEGRATION: CWS impresstables2 (1.12.400); FILE MERGED 2007/10/11 22:51:20 cl 1.12.400.2: RESYNC: (1.12-1.13); FILE MERGED 2007/10/11 13:54:05 cl 1.12.400.1: #i68103# fixed namespace issues

--- sw/source/core/swg/SwXMLBlockExport.cxx
+++ sw/source/core/swg/SwXMLBlockExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SwXMLBlockExport.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:09:20 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:22:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 // #110680#
 SwXMLBlockListExport::SwXMLBlockListExport(
