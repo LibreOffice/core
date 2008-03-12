8befb399158db675b4ae7a117e6c2e5f92e69236
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:57:14 +0000

    INTEGRATION: CWS impresstables2 (1.45.70); FILE MERGED 2007/08/01 14:36:25 cl 1.45.70.2: RESYNC: (1.45-1.46); FILE MERGED 2007/07/27 09:09:55 cl 1.45.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/xmlnumfi.cxx
+++ xmloff/source/style/xmlnumfi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlnumfi.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:48:03 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:57:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,7 +64,9 @@
 #include <xmloff/xmltoken.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
