137a2a8455bcc7040845e32013ecb0fc2bc0067e
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:03:57 +0000

    INTEGRATION: CWS impresstables2 (1.9.80); FILE MERGED 2007/10/11 21:33:02 cl 1.9.80.3: RESYNC: (1.10-1.11); FILE MERGED 2007/08/01 14:50:43 cl 1.9.80.2: RESYNC: (1.9-1.10); FILE MERGED 2007/07/27 09:09:56 cl 1.9.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/XMLTextMarkImportContext.cxx
+++ xmloff/source/text/XMLTextMarkImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextMarkImportContext.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2007-07-31 17:35:27 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:03:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,9 @@
 #endif
 
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::text;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
