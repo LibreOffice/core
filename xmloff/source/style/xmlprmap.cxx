9e863671ade41b8bcd70e99d82706f07b9ddd654
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:58:02 +0000

    INTEGRATION: CWS impresstables2 (1.8.80); FILE MERGED 2007/08/01 14:36:59 cl 1.8.80.2: RESYNC: (1.8-1.9); FILE MERGED 2007/07/27 09:09:55 cl 1.8.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/xmlprmap.cxx
+++ xmloff/source/style/xmlprmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlprmap.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:49:00 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:58:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,9 @@
 
 
 using namespace ::std;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
 using ::xmloff::token::GetXMLToken;
