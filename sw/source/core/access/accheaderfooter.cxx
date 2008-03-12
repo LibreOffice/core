7a4cd47585bd5fcae5a45dfdae1621ce94a95805
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:14:32 +0000

    INTEGRATION: CWS impresstables2 (1.13.400); FILE MERGED 2007/10/11 22:12:26 cl 1.13.400.2: RESYNC: (1.13-1.14); FILE MERGED 2007/10/11 13:46:24 cl 1.13.400.1: #i68103# fixed namespace issues

--- sw/source/core/access/accheaderfooter.cxx
+++ sw/source/core/access/accheaderfooter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: accheaderfooter.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:21:44 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:14:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,7 @@ using namespace ::com::sun::star;
 using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::accessibility;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 const sal_Char sServiceNameHeader[] = "com.sun.star.text.AccessibleHeaderView";
 const sal_Char sServiceNameFooter[] = "com.sun.star.text.AccessibleFooterView";
