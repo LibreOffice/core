45a5425401b8cae2f5e512a42d0e4b9bcdf34e20
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:13:29 +0000

    INTEGRATION: CWS impresstables2 (1.4.80); FILE MERGED 2007/07/27 09:09:59 cl 1.4.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/DlgOASISTContext.cxx
+++ xmloff/source/transform/DlgOASISTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DlgOASISTContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 11:22:36 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:13:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,8 @@
 #include "TransformerBase.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
 using namespace ::xmloff::token;
