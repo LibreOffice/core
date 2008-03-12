5aec5ed309d510de46490d3cff902a5038424cbc
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:09:09 +0000

    INTEGRATION: CWS impresstables2 (1.63.50); FILE MERGED 2008/03/11 18:22:08 cl 1.63.50.6: RESYNC: (1.67-1.69); FILE MERGED 2008/02/24 00:21:45 cl 1.63.50.5: RESYNC: (1.66-1.67); FILE MERGED 2007/12/01 19:18:52 cl 1.63.50.4: RESYNC: (1.65-1.66); FILE MERGED 2007/10/11 21:33:15 cl 1.63.50.3: RESYNC: (1.64-1.65); FILE MERGED 2007/08/01 14:52:43 cl 1.63.50.2: RESYNC: (1.63-1.64); FILE MERGED 2007/07/27 09:09:56 cl 1.63.50.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/txtfldi.cxx
+++ xmloff/source/text/txtfldi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtfldi.cxx,v $
  *
- *  $Revision: 1.69 $
+ *  $Revision: 1.70 $
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 13:38:55 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:09:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -195,7 +195,9 @@
 #endif
 
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::text;
