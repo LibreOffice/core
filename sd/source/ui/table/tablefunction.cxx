48e237d98cc371e468b8d8d63dba71df75d63452
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:47:37 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/03/04 18:30:04 cl 1.1.2.14: #i68103# adding table design dialog for draw 2008/02/22 23:45:23 cl 1.1.2.13: fixed rtf clipboard pasting 2008/01/28 08:17:45 cl 1.1.2.12: #i68103# create new table if table style is selected without selected table 2008/01/21 15:53:31 cl 1.1.2.11: #i68103# added dialog for 'Insert Table' in impress/draw 2008/01/02 17:04:13 cl 1.1.2.10: #i68103# fixed linux compile warning 2007/12/14 14:21:24 cl 1.1.2.9: #i68103# working on tables 2007/12/01 15:36:44 cl 1.1.2.8: #i68103# working on table designs 2007/11/29 19:07:18 cl 1.1.2.7: #i68103# working on table templates 2007/10/11 15:48:57 cl 1.1.2.6: #i68103# initial table style implemenation 2007/08/06 19:41:40 cl 1.1.2.5: #i68103# worked on cell merging for tables in impress&draw 2007/07/31 11:21:40 cl 1.1.2.4: fixing linux build problems 2007/07/26 18:17:39 cl 1.1.2.3: #i68103# added cell styles 2007/04/18 18:47:45 cl 1.1.2.2: added missing precompiled header directive 2007/04/13 09:48:05 cl 1.1.2.1: #i68103# draft table user interface

---
+++ sd/source/ui/table/tablefunction.cxx
@@ -0,0 +1,250 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tablefunction.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:47:37 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sd.hxx"
+
+#include <sal/config.h>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/drawing/XSelectionFunction.hpp>
+#include <com/sun/star/awt/KeyModifier.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
+
+#include <cppuhelper/compbase2.hxx>
+#include <cppuhelper/basemutex.hxx>
+
+#include <vcl/svapp.hxx>
+
+#include <svx/svdotable.hxx>
+#include <svx/sdr/overlay/overlayobjectcell.hxx>
+#include <svx/sdr/overlay/overlaymanager.hxx>
+#include <svx/svxids.hrc>
+#include <svx/outlobj.hxx>
+#include <svx/svdoutl.hxx>
+#include <svx/svdpagv.hxx>
+#include <svx/svdetc.hxx>
+#include <svx/editstat.hxx>
+#include <svx/unolingu.hxx>
+#include <svx/sdrpagewindow.hxx>
+#include <svx/sdr/table/tabledesign.hxx>
+#include <svx/svxdlg.hxx>
+#include <vcl/msgbox.hxx>
+
+#include <svtools/itempool.hxx>
+#include <sfx2/viewfrm.hxx>
+#include <sfx2/dispatch.hxx>
+#include <sfx2/bindings.hxx>
+#include <sfx2/request.hxx>
+#include <svtools/style.hxx>
+
+#include "framework/FrameworkHelper.hxx"
+#include "app.hrc"
+#include "glob.hrc"
+#include "DrawViewShell.hxx"
+#include "drawdoc.hxx"
+#include "DrawDocShell.hxx"
+#include "Window.hxx"
+#include "drawview.hxx"
+#include "sdresid.hxx"
+
+using ::rtl::OUString;
+using namespace ::sd;
+using namespace ::sdr::table;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::beans;
+using namespace ::com::sun::star::util;
+using namespace ::com::sun::star::frame;
+using namespace ::com::sun::star::container;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::drawing;
+using namespace ::com::sun::star::linguistic2;
+
+namespace css = ::com::sun::star;
+
+namespace sd
+{
+extern void showTableDesignDialog( ::Window*, ViewShellBase& );
+
+static void apply_table_style( SdrTableObj* pObj, SdrModel* pModel, const OUString& sTableStyle )
+{
+    if( pModel && pObj )
+    {
+        Reference< XNameAccess > xPool( dynamic_cast< XNameAccess* >( pModel->GetStyleSheetPool() ) );
+        if( xPool.is() ) try
+        {
+            const OUString sFamilyName( RTL_CONSTASCII_USTRINGPARAM( "table" ) );
+            Reference< XNameContainer > xTableFamily( xPool->getByName( sFamilyName ), UNO_QUERY_THROW );
+            OUString aStdName( RTL_CONSTASCII_USTRINGPARAM("default") );
+            if( sTableStyle.getLength() )
+                aStdName = sTableStyle;
+            Reference< XIndexAccess > xStyle( xTableFamily->getByName( aStdName ), UNO_QUERY_THROW );
+            pObj->setTableStyle( xStyle );
+        }
+        catch( Exception& )
+        {
+            DBG_ERROR("sd::apply_default_table_style(), exception caught!");
+        }
+    }
+}
+
+void DrawViewShell::FuTable(SfxRequest& rReq)
+{
+    switch( rReq.GetSlot() )
+    {
+    case SID_INSERT_TABLE:
+    {
+        sal_Int32 nColumns = 0;
+        sal_Int32 nRows = 0;
+        OUString sTableStyle;
+
+        SFX_REQUEST_ARG( rReq, pCols, SfxUInt16Item, SID_ATTR_TABLE_COLUMN, sal_False );
+        SFX_REQUEST_ARG( rReq, pRows, SfxUInt16Item, SID_ATTR_TABLE_ROW, sal_False );
+        SFX_REQUEST_ARG( rReq, pStyle, SfxStringItem, SID_TABLE_STYLE, sal_False );
+
+        if( pCols )
+            nColumns = pCols->GetValue();
+
+        if( pRows )
+            nRows = pRows->GetValue();
+
+        if( pStyle )
+            sTableStyle = pStyle->GetValue();
+
+        if( (nColumns == 0) || (nRows == 0) )
+        {
+            SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
+            ::std::auto_ptr<SvxAbstractNewTableDialog> pDlg( pFact ? pFact->CreateSvxNewTableDialog( NULL ) : 0);
+
+            if( !pDlg.get() || (pDlg->Execute() != RET_OK) )
+                break;
+
+            nColumns = pDlg->getColumns();
+            nRows = pDlg->getRows();
+        }
+
+        Size aSize( 14100, 200 );
+
+        Point aPos;
+        Rectangle aWinRect(aPos, GetActiveWindow()->GetOutputSizePixel() );
+        aPos = aWinRect.Center();
+        aPos = GetActiveWindow()->PixelToLogic(aPos);
+        aPos.X() -= aSize.Width() / 2;
+        aPos.Y() -= aSize.Height() / 2;
+        Rectangle aRect (aPos, aSize);
+
+        ::sdr::table::SdrTableObj* pObj = new ::sdr::table::SdrTableObj( GetDoc(), aRect, nColumns, nRows );
+        pObj->NbcSetStyleSheet( GetDoc()->GetDefaultStyleSheet(), sal_True );
+        apply_table_style( pObj, GetDoc(), sTableStyle );
+        SdrPageView* pPV = mpView->GetSdrPageView();
+        mpView->InsertObjectAtView(pObj, *pPV, SDRINSERT_SETDEFLAYER);
+        Invalidate(SID_DRAWTBX_INSERT);
+        rReq.Ignore();
+        break;
+    }
+    case SID_TABLEDESIGN:
+    {
+        if( GetDoc() && (GetDoc()->GetDocumentType() == DOCUMENT_TYPE_DRAW) )
+        {
+            // in draw open a modal dialog since we have no tool pane yet
+            showTableDesignDialog( GetActiveWindow(), GetViewShellBase() );
+        }
+        else
+        {
+            // Make the slide transition panel visible (expand it) in the
+            // tool pane.
+            framework::FrameworkHelper::Instance(GetViewShellBase())->RequestTaskPanel(
+                framework::FrameworkHelper::msTableDesignPanelURL);
+        }
+
+        Cancel();
+        rReq.Done ();
+    }
+    default:
+        break;
+    }
+}
+
+// --------------------------------------------------------------------
+
+void DrawViewShell::GetTableMenuState( SfxItemSet &rSet )
+{
+    bool bIsUIActive = GetDocSh()->IsUIActive();
+    if( bIsUIActive )
+    {
+        rSet.DisableItem( SID_INSERT_TABLE );
+    }
+    else
+    {
+        String aActiveLayer = mpDrawView->GetActiveLayer();
+        SdrPageView* pPV = mpDrawView->GetSdrPageView();
+
+        if( bIsUIActive ||
+            ( aActiveLayer.Len() != 0 && pPV && ( pPV->IsLayerLocked(aActiveLayer) ||
+            !pPV->IsLayerVisible(aActiveLayer) ) ) ||
+            SD_MOD()->GetWaterCan() )
+        {
+            rSet.DisableItem( SID_INSERT_TABLE );
+        }
+    }
+}
+
+// --------------------------------------------------------------------
+
+void CreateTableFromRTF( SvStream& rStream, SdDrawDocument* pModel )
+{
+    rStream.Seek( 0 );
+
+    if( pModel )
+    {
+        SdrPage* pPage = pModel->GetPage(0);
+        if( pPage )
+        {
+            Size aSize( 200, 200 );
+            Point aPos;
+            Rectangle aRect (aPos, aSize);
+            ::sdr::table::SdrTableObj* pObj = new ::sdr::table::SdrTableObj( pModel, aRect, 1, 1 );
+            pObj->NbcSetStyleSheet( pModel->GetDefaultStyleSheet(), sal_True );
+            OUString sTableStyle;
+            apply_table_style( pObj, pModel, sTableStyle );
+
+            pPage->NbcInsertObject( pObj );
+
+            sdr::table::SdrTableObj::ImportAsRTF( rStream, *pObj );
+        }
+    }
+}
+
+}
