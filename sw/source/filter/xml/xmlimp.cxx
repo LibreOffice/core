9113718284eadf835f8713e883f380b0c5185cbb
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:39:53 +0000

    INTEGRATION: CWS impresstables2 (1.101.70); FILE MERGED 2008/03/06 18:14:07 cl 1.101.70.3: fixing merge errors 2007/10/11 23:31:22 cl 1.101.70.2: RESYNC: (1.101-1.103); FILE MERGED 2007/10/11 14:08:34 cl 1.101.70.1: #i68103# fixed namespace issues

--- sw/source/filter/xml/xmlimp.cxx
+++ sw/source/filter/xml/xmlimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlimp.cxx,v $
  *
- *  $Revision: 1.104 $
+ *  $Revision: 1.105 $
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 14:21:30 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:39:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -160,7 +160,8 @@
 
 #include <xmloff/xmlmetai.hxx>
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::text;
