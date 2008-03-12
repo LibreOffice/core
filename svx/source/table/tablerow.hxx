2af4f789d38bcb32d11becaa31c6d30e72266137
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:05:51 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/27 11:57:22 cl 1.1.2.4: #i68103# fixed clipboard 2007/10/11 15:18:52 cl 1.1.2.3: #i68103# added undo for tables 2007/03/20 12:54:52 cl 1.1.2.2: fixed unix compile errors 2007/03/15 17:16:34 cl 1.1.2.1: #i68103# adding a shape for tables

---
+++ svx/source/table/tablerow.hxx
@@ -0,0 +1,100 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tablerow.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:05:51 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVX_TABLEROW_HXX_
+#define _SVX_TABLEROW_HXX_
+
+#include <com/sun/star/table/XCellRange.hpp>
+#include <com/sun/star/container/XNamed.hpp>
+#include <cppuhelper/implbase2.hxx>
+
+#include "propertyset.hxx"
+#include "tablemodel.hxx"
+
+// -----------------------------------------------------------------------------
+
+namespace sdr { namespace table {
+
+// -----------------------------------------------------------------------------
+// TableRow
+// -----------------------------------------------------------------------------
+
+typedef ::cppu::ImplInheritanceHelper2< ::comphelper::FastPropertySet, ::com::sun::star::table::XCellRange, ::com::sun::star::container::XNamed > TableRowBase;
+
+class TableRow : public TableRowBase
+{
+    friend class TableModel;
+    friend class TableRowUndo;
+public:
+    TableRow( const TableModelRef& xTableModel, sal_Int32 nRow, sal_Int32 nColumns );
+    virtual ~TableRow();
+
+    void dispose();
+    void throwIfDisposed() const throw (::com::sun::star::uno::RuntimeException);
+
+    TableRow& operator=( const TableRow& );
+
+    void insertColumns( sal_Int32 nIndex, sal_Int32 nCount, CellVector::iterator* pIter = 0 );
+    void removeColumns( sal_Int32 nIndex, sal_Int32 nCount );
+
+    // XCellRange
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::table::XCell > SAL_CALL getCellByPosition( sal_Int32 nColumn, sal_Int32 nRow ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::table::XCellRange > SAL_CALL getCellRangeByPosition( sal_Int32 nLeft, sal_Int32 nTop, sal_Int32 nRight, sal_Int32 nBottom ) throw (::com::sun::star::lang::IndexOutOfBoundsException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::table::XCellRange > SAL_CALL getCellRangeByName( const ::rtl::OUString& aRange ) throw (::com::sun::star::uno::RuntimeException);
+
+    // XNamed
+    virtual ::rtl::OUString SAL_CALL getName() throw (::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL setName( const ::rtl::OUString& aName ) throw (::com::sun::star::uno::RuntimeException);
+
+    // XFastPropertySet
+    virtual void SAL_CALL setFastPropertyValue( ::sal_Int32 nHandle, const ::com::sun::star::uno::Any& aValue ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+    virtual ::com::sun::star::uno::Any SAL_CALL getFastPropertyValue( ::sal_Int32 nHandle ) throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException);
+
+private:
+    static rtl::Reference< ::comphelper::FastPropertySetInfo > getStaticPropertySetInfo();
+
+    TableModelRef mxTableModel;
+    CellVector  maCells;
+    sal_Int32   mnRow;
+    sal_Int32   mnHeight;
+    sal_Bool    mbOptimalHeight;
+    sal_Bool    mbIsVisible;
+    sal_Bool    mbIsStartOfNewPage;
+    ::rtl::OUString maName;
+};
+
+} }
+
+#endif
