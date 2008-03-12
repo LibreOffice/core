17df219b4c468696784f067a68e8045d4e693554
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:49:28 +0000

    INTEGRATION: CWS impresstables2 (1.8.70); FILE MERGED 2007/08/01 14:29:26 cl 1.8.70.2: RESYNC: (1.8-1.9); FILE MERGED 2007/07/27 09:09:54 cl 1.8.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/bordrhdl.cxx
+++ xmloff/source/style/bordrhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bordrhdl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:35:33 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:49:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,9 @@
 #include <com/sun/star/table/BorderLine.hpp>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
