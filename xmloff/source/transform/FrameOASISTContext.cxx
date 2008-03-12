354cd11ff527c1ca8aca07b95e7f7e9b499ee57f
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:14:38 +0000

    INTEGRATION: CWS impresstables2 (1.8.12); FILE MERGED 2007/07/27 09:10:00 cl 1.8.12.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/transform/FrameOASISTContext.cxx
+++ xmloff/source/transform/FrameOASISTContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FrameOASISTContext.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: ihi $ $Date: 2007-04-16 13:13:58 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:14:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,8 @@
 #include "TransformerBase.hxx"
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
 using namespace ::xmloff::token;
