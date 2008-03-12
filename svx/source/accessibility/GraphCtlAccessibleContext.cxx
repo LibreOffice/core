0d3eb7b917a646b90a3c27483ba1971bf7a7f30b
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:36:35 +0000

    INTEGRATION: CWS impresstables2 (1.23.136); FILE MERGED 2007/08/01 17:03:37 cl 1.23.136.2: RESYNC: (1.23-1.24); FILE MERGED 2007/05/31 11:13:58 cl 1.23.136.1: #i68103# merging changes for tables

--- svx/source/accessibility/GraphCtlAccessibleContext.cxx
+++ svx/source/accessibility/GraphCtlAccessibleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GraphCtlAccessibleContext.cxx,v $
  *
- *  $Revision: 1.24 $
+ *  $Revision: 1.25 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:41:37 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:36:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,7 +130,7 @@
 using namespace ::vos;
 using namespace ::cppu;
 using namespace ::osl;
-using namespace ::rtl;
+using ::rtl::OUString;
 using namespace ::accessibility;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
