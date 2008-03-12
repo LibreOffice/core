6d83a522dc21e1b574141ee36dac12bb02d40607
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:42:50 +0000

    INTEGRATION: CWS impresstables2 (1.12.80); FILE MERGED 2007/08/01 14:23:26 cl 1.12.80.2: RESYNC: (1.12-1.13); FILE MERGED 2007/07/27 09:09:53 cl 1.12.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/ImageStyle.cxx
+++ xmloff/source/style/ImageStyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ImageStyle.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:24:59 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:42:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -89,7 +89,9 @@
 #endif
 
 using namespace ::com::sun::star;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::xmloff::token;
 
 enum SvXMLTokenMapAttrs
