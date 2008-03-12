cf2a41a69a3ce36b2e1e7a61f6a031d05fd6b23c
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:11:38 +0000

    INTEGRATION: CWS impresstables2 (1.36.70); FILE MERGED 2008/01/30 13:22:21 cl 1.36.70.3: RESYNC: (1.37-1.38); FILE MERGED 2007/08/01 14:53:51 cl 1.36.70.2: RESYNC: (1.36-1.37); FILE MERGED 2007/07/27 09:09:57 cl 1.36.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/txtprhdl.cxx
+++ xmloff/source/text/txtprhdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtprhdl.cxx,v $
  *
- *  $Revision: 1.38 $
+ *  $Revision: 1.39 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-07 08:48:33 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:11:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -116,7 +116,9 @@
 #endif
 
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 //using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::style;
