5eb507c6fe9fb3dbac9eb3512798dfa0dd478d39
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:37:40 +0000

    INTEGRATION: CWS hro34 (1.4.120); FILE MERGED 2008/03/07 10:36:23 hro 1.4.120.1: #i72556# Added Auto COM PTR and Vista APIs to uwinapi

--- sal/systools/win32/uwinapi/FindFirstVolumeMountPointA.cpp
+++ sal/systools/win32/uwinapi/FindFirstVolumeMountPointA.cpp
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FindFirstVolumeMountPointA.cpp,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: ihi $ $Date: 2007-06-05 11:01:55 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:37:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,7 +32,24 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
 #include "macros.h"
-
 DEFINE_DEFAULT_THUNK( kernel32, TRYLOAD, HANDLE, WINAPI, FindFirstVolumeMountPointA, (LPCSTR lpszRootPathName, LPSTR lpszVolumeMountPoint, DWORD cchBufferLength) )
+/*
+extern "C" _declspec( dllexport ) FARPROC kernel32_FindFirstVolumeMountPointA_Ptr;
+static HANDLE __stdcall FindFirstVolumeMountPointA_Failure (LPSTR lpszRootPathName, LPSTR lpszVolumeMountPoint, DWORD cchBufferLength);
+static _declspec ( naked ) void FindFirstVolumeMountPointA_Thunk()
+{
+     ResolveThunk_TRYLOAD( &kernel32_FindFirstVolumeMountPointA_Ptr, "kernel32" ".dll", "FindFirstVolumeMountPointA", 0, (FARPROC)FindFirstVolumeMountPointA_Failure );
+     _asm jmp [kernel32_FindFirstVolumeMountPointA_Ptr] }
+
+//extern "C" _declspec( naked ) HANDLE __stdcall
+//extern "C" HANDLE WINAPI
+extern "C" _declspec( naked ) HANDLE __stdcall FindFirstVolumeMountPointA (LPCSTR lpszRootPathName, LPSTR lpszVolumeMountPoint, DWORD cchBufferLength)
+{
+    _asm jmp [kernel32_FindFirstVolumeMountPointA_Ptr]
+}
+
+extern "C" _declspec( dllexport ) FARPROC kernel32_FindFirstVolumeMountPointA_Ptr = (FARPROC)FindFirstVolumeMountPointA_Thunk;
+static HANDLE __stdcall FindFirstVolumeMountPointA_Failure (LPSTR lpszRootPathName, LPSTR lpszVolumeMountPoint, DWORD cchBufferLength)
+{ SetLastError( 120L ); return (HANDLE)0; }
+*/
