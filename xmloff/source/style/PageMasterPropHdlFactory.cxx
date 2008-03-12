0ac9ec4b08d56d7f8404c9af60daee8c61affe48
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:43:57 +0000

    INTEGRATION: CWS impresstables2 (1.9.80); FILE MERGED 2007/08/01 14:25:28 cl 1.9.80.2: RESYNC: (1.9-1.10); FILE MERGED 2007/07/27 09:09:53 cl 1.9.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/PageMasterPropHdlFactory.cxx
+++ xmloff/source/style/PageMasterPropHdlFactory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: PageMasterPropHdlFactory.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:27:50 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:43:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -71,7 +71,9 @@
 #endif
 
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::xmloff::token;
 using namespace ::com::sun::star;
 
