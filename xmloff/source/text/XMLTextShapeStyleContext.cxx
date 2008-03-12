d65459b780116c29c82d54d7fd0e5ada8d1dac0a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:06:49 +0000

    INTEGRATION: CWS impresstables2 (1.10.80); FILE MERGED 2007/08/01 14:51:40 cl 1.10.80.2: RESYNC: (1.10-1.11); FILE MERGED 2007/07/27 09:09:56 cl 1.10.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/XMLTextShapeStyleContext.cxx
+++ xmloff/source/text/XMLTextShapeStyleContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextShapeStyleContext.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:12:35 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:06:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,9 @@
 #include <xmloff/XMLTextShapeStyleContext.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star::document;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::xml::sax;
