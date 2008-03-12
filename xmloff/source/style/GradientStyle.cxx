486fd26b288496317c02795b0c61caacdb36e189
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:42:21 +0000

    INTEGRATION: CWS impresstables2 (1.15.70); FILE MERGED 2007/08/01 14:23:20 cl 1.15.70.2: RESYNC: (1.15-1.16); FILE MERGED 2007/07/27 09:09:53 cl 1.15.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/GradientStyle.cxx
+++ xmloff/source/style/GradientStyle.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GradientStyle.cxx,v $
  *
- *  $Revision: 1.16 $
+ *  $Revision: 1.17 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:24:29 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:42:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -91,7 +91,9 @@
 
 
 using namespace ::com::sun::star;
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::xmloff::token;
 
 enum SvXMLTokenMapAttrs
