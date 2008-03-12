3a1f83479aa86a271b2677342b4f14267afa76a2
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:07:59 +0000

    INTEGRATION: CWS impresstables2 (1.37.8); FILE MERGED 2007/08/01 18:52:08 cl 1.37.8.3: RESYNC: (1.38-1.39); FILE MERGED 2007/06/27 18:06:29 cl 1.37.8.2: RESYNC: (1.37-1.38); FILE MERGED 2007/05/31 11:46:43 cl 1.37.8.1: #i68103# fixed a namespace problem

--- svx/source/tbxctrls/grafctrl.cxx
+++ svx/source/tbxctrls/grafctrl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: grafctrl.cxx,v $
  *
- *  $Revision: 1.39 $
+ *  $Revision: 1.40 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:16:53 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:07:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,7 +98,7 @@
 #include "tbxcolor.hxx"
 
 // namespaces
-using namespace ::rtl;
+using ::rtl::OUString;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::frame;
 using namespace ::com::sun::star::util;
