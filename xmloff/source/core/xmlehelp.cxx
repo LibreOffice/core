10089a547807c9abcdc74bf0de3c37652103b6d4
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:29:15 +0000

    INTEGRATION: CWS impresstables2 (1.14.70); FILE MERGED 2007/08/01 14:07:34 cl 1.14.70.2: RESYNC: (1.14-1.15); FILE MERGED 2007/07/27 09:09:50 cl 1.14.70.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/core/xmlehelp.cxx
+++ xmloff/source/core/xmlehelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlehelp.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 14:57:09 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:29:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,9 @@
 #include <xmloff/xmltoken.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::xmloff::token;
 
 void SvXMLExportHelper::AddLength( sal_Int32 nValue, MapUnit eValueUnit,
