690b8012afbaae6e74af7d7b67d9472e7e037676
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:07:08 +0000

    INTEGRATION: CWS impresstables2 (1.3.80); FILE MERGED 2007/08/01 14:50:21 cl 1.3.80.2: RESYNC: (1.3-1.4); FILE MERGED 2007/07/27 09:09:56 cl 1.3.80.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/text/XMLTextTableContext.cxx
+++ xmloff/source/text/XMLTextTableContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLTextTableContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:12:50 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:07:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,8 @@
 #include <xmloff/XMLTextTableContext.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::text;
 
