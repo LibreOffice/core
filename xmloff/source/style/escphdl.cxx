8afa9709d1f4e2ab829128847a20eb9bc43dbc67
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:51:18 +0000

    INTEGRATION: CWS impresstables2 (1.8.46); FILE MERGED 2007/08/01 14:31:18 cl 1.8.46.2: RESYNC: (1.8-1.9); FILE MERGED 2007/07/27 09:09:54 cl 1.8.46.1: fixed build issues due to pch and namespace ::rtl

--- xmloff/source/style/escphdl.cxx
+++ xmloff/source/style/escphdl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: escphdl.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:38:57 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:51:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,9 @@
 #include <xmloff/xmlelement.hxx>
 #endif
 
-using namespace ::rtl;
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
 
