05d3e7a08a5585d586302024f0036ae693fde58b
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:32:26 +0000

    INTEGRATION: CWS impresstables2 (1.39.38); FILE MERGED 2007/10/11 23:12:02 cl 1.39.38.2: RESYNC: (1.39-1.40); FILE MERGED 2007/10/11 14:01:40 cl 1.39.38.1: #i68103# fixed namespace issues

--- sw/source/core/unocore/unoport.cxx
+++ sw/source/core/unocore/unoport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoport.cxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:38:37 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:32:26 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,7 +98,7 @@
 //#endif
 
 using namespace ::com::sun::star;
-using namespace ::rtl;
+using ::rtl::OUString;
 
 
 /******************************************************************
