54dea27b94bc813a7c97f0cf860a10a94e82c004
Rüdiger Timm <rt@openoffice.org>
2008-03-13 13:11:23 +0000

    #i10000# No UPD in library names any more.

--- svtools/workben/unodialog/makefile.mk
+++ svtools/workben/unodialog/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: kz $ $Date: 2008-03-06 19:27:08 $
+#   last change: $Author: rt $ $Date: 2008-03-13 14:11:23 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@ SLOFILES=   $(SLO)$/unodialogsample.obj \
 
 # --- library -----------------------------------
 
-SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
 SHL1VERSIONMAP=$(TARGET).map
 
 SHL1STDLIBS= \
