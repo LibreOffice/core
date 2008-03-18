fe95014929c2d4e1bab6023f29c1d0b8f43a227c
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 14:58:02 +0000

    INTEGRATION: CWS sw8u10bf05 (1.2.236); FILE MERGED 2008/01/25 10:38:42 ama 1.2.236.1: Fix #85182#: Remove unused functions

--- sw/source/core/inc/swthreadjoiner.hxx
+++ sw/source/core/inc/swthreadjoiner.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swthreadjoiner.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-18 13:34:42 $
+ *  last change: $Author: vg $ $Date: 2008-03-18 15:58:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,8 +50,6 @@ class SwThreadJoiner
     public:
 
         static ::com::sun::star::uno::Reference< ::com::sun::star::util::XJobManager >& GetThreadJoiner();
-
-        static bool ExistsThreadJoiner();
 
         static void ReleaseThreadJoiner();
 
