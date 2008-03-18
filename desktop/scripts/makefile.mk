9697be1528fc35b3aca0ce602519ff197381ae09
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:44:17 +0000

    INTEGRATION: CWS sb83 (1.11.184); FILE MERGED 2008/02/12 12:44:26 sb 1.11.184.4: #i86041# do not add Mozilla library directory to LD_LIBRARY_PATH in soffice start script 2008/01/31 17:00:15 is 1.11.184.3: #i85112# new basis  link 2008/01/11 14:22:02 sb 1.11.184.2: #i84200# build basis-link only on Windows (some Solaris sed complains about missing line end, and this basis-link is not needed there, anyway) 2007/12/27 12:26:15 sb 1.11.184.1: #i84200# The version number in basis-link needs to be abstracted over.  TODO: The for-now explicit 2.4 needs to come from somewhere else!

--- desktop/scripts/makefile.mk
+++ desktop/scripts/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.11 $
+#   $Revision: 1.12 $
 #
-#   last change: $Author: vg $ $Date: 2007-02-06 14:07:54 $
+#   last change: $Author: vg $ $Date: 2008-03-18 13:44:17 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -58,10 +58,3 @@ UNIXTEXT= \
     $(MISC)$/unopkg.sh
 
 .INCLUDE :  target.mk
-
-ALLTAR: $(MISC)$/so$/soffice.sh
-
-$(MISC)$/so$/soffice.sh: soffice_lean.sh
-    @@-$(MKDIR) $(MISC)$/so
-    $(TYPE) soffice_lean.sh | tr -d "\015" > $@
-
