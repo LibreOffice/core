11d8dc5510af8ba3451f1700f1f7c850f75a0bdf
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:01:03 +0000

    INTEGRATION: CWS sb83 (1.12.4); FILE MERGED 2008/01/29 15:12:27 sb 1.12.4.1: #i85112# merged in cws_src680_native121

--- solenv/bin/modules/installer/scpzipfiles.pm
+++ solenv/bin/modules/installer/scpzipfiles.pm
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: scpzipfiles.pm,v $
 #
-#   $Revision: 1.12 $
+#   $Revision: 1.13 $
 #
-#   last change: $Author: obo $ $Date: 2008-01-04 16:58:39 $
+#   last change: $Author: vg $ $Date: 2008-03-18 13:01:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -180,6 +180,9 @@ sub resolving_scpzip_replace_flag
                     replace_all_ziplistvariables_in_file($onefileref, $variableshashref);
                     installer::files::save_file($destinationpath ,$onefileref);
                 }
+
+                # Saving the original source, where the file was found
+                $onefile->{'originalsourcepath'} = $onefile->{'sourcepath'};
 
                 # Saving the original source, where the file was found
                 $onefile->{'originalsourcepath'} = $onefile->{'sourcepath'};
