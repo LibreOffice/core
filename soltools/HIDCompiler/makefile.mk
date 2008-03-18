d6488be5199b424ed517d52e8b4ec85639254f08
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 13:06:58 +0000

    INTEGRATION: CWS sb83 (1.8.10); FILE MERGED 2007/12/21 14:51:30 sb 1.8.10.1: #i84200# Delayloading of uwinapi.dll is only needed in a few specific executables (loaders in module desktop) (but needed on all Windows platforms).

--- soltools/HIDCompiler/makefile.mk
+++ soltools/HIDCompiler/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: vg $ $Date: 2007-10-15 13:04:44 $
+#   last change: $Author: vg $ $Date: 2008-03-18 14:06:58 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,10 +41,6 @@ TARGETTYPE=CUI
 LIBTARGET=no
 NO_DEFAULT_STL=TRUE
 INCPRE=$(MISC)
-
-# Windows /DELAYLOAD depends on objects delivered from sal, but is not needed
-# for this internal tool, anyway:
-DELAYLOADOBJ=
 
 # --- Settings -----------------------------------------------------
 
