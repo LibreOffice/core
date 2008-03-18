feecbd0fb4dc1192fa206d871ea91440eac7fea0
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:38:09 +0000

    INTEGRATION: CWS sb83 (1.2.2); FILE MERGED 2008/02/06 14:07:23 sb 1.2.2.4: #i84200# streamlined image set processing 2008/01/31 09:41:41 is 1.2.2.3: #i85112# language modules with names and descriptions 2008/01/30 09:29:31 sb 1.2.2.2: #i85112# merged in cws_src680_native121 2008/01/29 16:00:26 sb 1.2.2.1: #i85112# merged in cws_src680_native121

--- scp2/source/templates/makefile.mk
+++ scp2/source/templates/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: obo $ $Date: 2008-01-04 17:11:50 $
+#   last change: $Author: vg $ $Date: 2008-03-18 13:38:09 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -48,12 +48,40 @@ TARGET=templates
 COMPLETELANGISO_VAR:=$(uniq $(completelangiso) $(alllangiso))
 .EXPORT : COMPLETELANGISO_VAR
 
-# used here to force expanding before export
-CUSTOM_IMAGE_SETS!:=$(CUSTOM_IMAGE_SETS)
-.EXPORT : CUSTOM_IMAGE_SETS
-
-ALLTAR : $(INCCOM)$/alllangmodules.inc
+ALLTAR : $(INCCOM)$/alllangmodules.inc $(INCCOM)$/alllangmodules_root.inc $(INCCOM)$/alllangmodules_base.inc $(INCCOM)$/alllangmodules_calc.inc $(INCCOM)$/alllangmodules_draw.inc $(INCCOM)$/alllangmodules_impress.inc $(INCCOM)$/alllangmodules_math.inc $(INCCOM)$/alllangmodules_writer.inc $(INCCOM)$/alllangmodules_onlineupdate.inc
 
 .PHONY $(INCCOM)$/alllangmodules.inc:
     @@-$(RENAME) $@ $@.tmp
     $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_root.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_root.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_base.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_base.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_calc.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_calc.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_draw.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_draw.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_impress.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_impress.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_math.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_math.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_writer.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_writer.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
+
+.PHONY $(INCCOM)$/alllangmodules_onlineupdate.inc:
+    @@-$(RENAME) $@ $@.tmp
+    $(PERL) -w modules.pl -i $(PRJ)$/source$/templates$/module_langpack_onlineupdate.sct -o $@.tmp && $(RENAME:s/+//) $@.tmp $@
