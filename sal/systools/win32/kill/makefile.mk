fff79f1b3c23d06125636f661397c4c0737bdda4
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:21:06 +0000

    INTEGRATION: CWS sb83 (1.6.26); FILE MERGED 2007/12/21 14:49:24 sb 1.6.26.1: #i84200# Delayloading of uwinapi.dll is only needed in a few specific executables (loaders in module desktop; moved everything relevant from module sal there) (but needed on all Windows platforms).

--- sal/systools/win32/kill/makefile.mk
+++ sal/systools/win32/kill/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: vg $ $Date: 2007-10-15 12:52:07 $
+#   last change: $Author: vg $ $Date: 2008-03-18 13:21:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,6 @@ CXXFLAGS+= $(LFS_CFLAGS)
 # --- Files --------------------------------------------------------
 
 UWINAPILIB=
-DELAYLOADOBJ=
 
 OBJFILES=\
     $(OBJ)$/kill.obj
