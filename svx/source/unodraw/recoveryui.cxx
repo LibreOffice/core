0f1dd0dc5718f26deb5fb92dcb3a7722b7a72b0e
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 11:45:28 +0000

    INTEGRATION: CWS sb83 (1.10.114); FILE MERGED 2008/02/01 13:25:37 sb 1.10.114.2: RESYNC: (1.10-1.11); FILE MERGED 2008/01/23 15:31:12 sb 1.10.114.1: #i84200# access files in various layers via explicit (OOO|BRAND)_BASE_DIR expansions

--- svx/source/unodraw/recoveryui.cxx
+++ svx/source/unodraw/recoveryui.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: recoveryui.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2008-01-24 18:20:21 $
+ *  last change: $Author: vg $ $Date: 2008-03-18 12:45:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -222,7 +222,7 @@ static OUString GetCrashConfigDir()
 {
 
 #if defined(WNT) || defined(OS2)
-    OUString    ustrValue = OUString::createFromAscii("${$SYSBINDIR/bootstrap.ini:UserInstallation}");
+    OUString    ustrValue = OUString::createFromAscii("${$BRAND_BASE_DIR/program/bootstrap.ini:UserInstallation}");
 #else
     OUString    ustrValue = OUString::createFromAscii("$SYSUSERCONFIG");
 #endif
