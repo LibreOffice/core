3af1671e6917ec3eef1db381a0a78c1281c8d3d7
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:11:01 +0000

    INTEGRATION: CWS sb83 (1.12.18); FILE MERGED 2008/02/01 11:02:16 sb 1.12.18.4: #i84200# added SHLnRPATH=OXT for libraries within extensions (that only link against URE libs) 2008/01/09 10:51:41 sb 1.12.18.3: RESYNC: (1.12-1.13); FILE MERGED 2007/12/19 10:36:17 sb 1.12.18.2: #i84200# typo 2007/12/03 13:42:36 sb 1.12.18.1: #i84200# Added LINKFLAGSRUNPATH_BRAND.

--- solenv/inc/unxlngx6.mk
+++ solenv/inc/unxlngx6.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: unxlngx6.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: kz $ $Date: 2007-12-12 13:21:19 $
+#   last change: $Author: vg $ $Date: 2008-03-18 13:11:01 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -163,6 +163,8 @@ LINKFLAGSRUNPATH_URELIB=-Wl,-rpath,\''$$ORIGIN'\'
 LINKFLAGSRUNPATH_UREBIN=-Wl,-rpath,\''$$ORIGIN/../lib:$$ORIGIN'\'
     #TODO: drop $ORIGIN once no URE executable is also shipped in OOo
 LINKFLAGSRUNPATH_OOO=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../ure-link/lib'\'
+LINKFLAGSRUNPATH_BRAND=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../basis-link/program:$$ORIGIN/../basis-link/ure-link/lib'\'
+LINKFLAGSRUNPATH_OXT=
 LINKFLAGS=-Wl,-z,combreloc $(LINKFLAGSDEFS)
 
 # linker flags for linking applications
