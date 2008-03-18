eaf5648be922217834da6c0bf5c3854bb6ab708c
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 12:52:10 +0000

    INTEGRATION: CWS sb83 (1.2.34); FILE MERGED 2008/02/08 09:17:00 sb 1.2.34.5: #i84200# extract all the meat from (soffice|unkpkg).bin into (soffice|unkpkg).dll, where the .dll are in the basis layer and the .bin, on all plattforms, are in the brand layer, where they can include brand-specific icons on Windows (read by vcl; potentially visible in Windows Explorer according to hro) 2008/01/29 14:43:46 sb 1.2.34.4: #i84200# cuiloader was not used at all, cuiloader/genericloader.cxx has been moved to guiloader/genericloader.cxx' cuiloader" 2007/12/21 16:21:29 sb 1.2.34.3: #i84200# fail with message 2007/12/21 14:55:38 sb 1.2.34.2: #i84200# Extend PATH with both basis-link and ure-link. 2007/11/22 09:45:46 sb 1.2.34.1: Merged in CWS sb80.

--- desktop/win32/source/extendloaderenvironment.cxx
+++ desktop/win32/source/extendloaderenvironment.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: extendloaderenvironment.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-07 09:57:15 $
+ *  last change: $Author: vg $ $Date: 2008-03-18 13:52:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,6 @@
 #include "sal/config.h"
 
 #include <cstddef>
-#include <stdlib.h> // problems with cstdlib std::abort
 
 #define WIN32_LEAN_AND_MEAN
 #if defined _MSC_VER
@@ -49,71 +48,296 @@
 #pragma warning(pop)
 #endif
 
-#include "rtl/string.h"
-#include "sal/delayload.hxx"
+#include "sal/types.h"
 
 #include "extendloaderenvironment.hxx"
 
+namespace {
+
+WCHAR * filename(WCHAR * path) {
+    WCHAR * f = path;
+    for (WCHAR * p = path;;) {
+        switch (*p++) {
+        case L'\0':
+            return f;
+        case L'\\':
+            f = p;
+            break;
+        }
+    }
+}
+
+// Concatenate two strings.
+//
+// @param path
+// An output paremeter taking the resulting path; must point at a valid range of
+// memory of size at least MAX_PATH.  If NULL is returned, the content is
+// unspecified.
+//
+// @param frontBegin, frontEnd
+// Forms a valid range [frontBegin .. frontEnd) of less than MAX_PATH size.
+//
+// @param backBegin, backLength
+// Forms a valid range [backBeghin .. backBegin + backLength) of less than
+// MAX_PATH size.
+//
+// @return
+// A pointer to the terminating null character of the concatenation, or NULL if
+// a failure occurred.
+WCHAR * buildPath(
+    WCHAR * path, WCHAR const * frontBegin, WCHAR const * frontEnd,
+    WCHAR const * backBegin, std::size_t backLength)
+{
+    if (backLength <
+        static_cast< std::size_t >(MAX_PATH - (frontEnd - frontBegin)))
+        // hopefully std::size_t is large enough
+    {
+        WCHAR * p;
+        if (frontBegin == path) {
+            p = const_cast< WCHAR * >(frontEnd);
+        } else {
+            p = path;
+            while (frontBegin != frontEnd) {
+                *p++ = *frontBegin++;
+            }
+        }
+        for (; backLength > 0; --backLength) {
+            *p++ = *backBegin++;
+        }
+        *p = L'\0';
+        return p;
+    } else {
+        SetLastError(ERROR_FILENAME_EXCED_RANGE);
+        return NULL;
+    }
+}
+
+// Resolve a link file.
+//
+// @param path
+// An input/output parameter taking the path; must point at a valid range of
+// memory of size at least MAX_PATH.  On input, contains the null-terminated
+// full path of the link file.  On output, contains the null-terminated full
+// path of the resolved link; if NULL is returned, the content is unspecified.
+//
+// @return
+// A pointer to the terminating null character of path, or NULL if a failure
+// occurred.
+WCHAR * resolveLink(WCHAR * path) {
+    HANDLE h = CreateFileW(
+        path, FILE_READ_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+    if (h == INVALID_HANDLE_VALUE) {
+        return NULL;
+    }
+    char p1[MAX_PATH];
+    DWORD n;
+    BOOL ok = ReadFile(h, p1, MAX_PATH, &n, NULL);
+    CloseHandle(h);
+    if (!ok) {
+        return NULL;
+    }
+    WCHAR p2[MAX_PATH];
+    std::size_t n2 = 0;
+    bool colon = false;
+    for (DWORD i = 0; i < n;) {
+        unsigned char c = static_cast< unsigned char >(p1[i++]);
+        switch (c) {
+        case '\0':
+            SetLastError(ERROR_BAD_PATHNAME);
+            return NULL;
+        case '\x0A':
+        case '\x0D':
+            if (n2 == MAX_PATH) {
+                SetLastError(ERROR_FILENAME_EXCED_RANGE);
+                return NULL;
+            }
+            p2[n2] = L'\0';
+            break;
+        case ':':
+            colon = true;
+            // fall through
+        default:
+            // Convert from UTF-8 to UTF-16:
+            if (c <= 0x7F) {
+                p2[n2++] = c;
+            } else if (c >= 0xC2 && c <= 0xDF && i < n &&
+                       static_cast< unsigned char >(p1[i]) >= 0x80 &&
+                       static_cast< unsigned char >(p1[i]) <= 0xBF)
+            {
+                p2[n2++] = ((c & 0x1F) << 6) |
+                    (static_cast< unsigned char >(p1[i++]) & 0x3F);
+            } else if (n - i > 1 &&
+                       ((c == 0xE0 &&
+                         static_cast< unsigned char >(p1[i]) >= 0xA0 &&
+                         static_cast< unsigned char >(p1[i]) <= 0xBF) ||
+                        ((c >= 0xE1 && c <= 0xEC || c >= 0xEE && c <= 0xEF) &&
+                         static_cast< unsigned char >(p1[i]) >= 0x80 &&
+                         static_cast< unsigned char >(p1[i]) <= 0xBF) ||
+                        (c == 0xED &&
+                         static_cast< unsigned char >(p1[i]) >= 0x80 &&
+                         static_cast< unsigned char >(p1[i]) <= 0x9F)) &&
+                       static_cast< unsigned char >(p1[i + 1]) >= 0x80 &&
+                       static_cast< unsigned char >(p1[i + 1]) <= 0xBF)
+            {
+                p2[n2++] = ((c & 0x0F) << 12) |
+                    ((static_cast< unsigned char >(p1[i]) & 0x3F) << 6) |
+                    (static_cast< unsigned char >(p1[i + 1]) & 0x3F);
+                i += 2;
+            } else if (n - 2 > 1 &&
+                       ((c == 0xF0 &&
+                         static_cast< unsigned char >(p1[i]) >= 0x90 &&
+                         static_cast< unsigned char >(p1[i]) <= 0xBF) ||
+                        (c >= 0xF1 && c <= 0xF3 &&
+                         static_cast< unsigned char >(p1[i]) >= 0x80 &&
+                         static_cast< unsigned char >(p1[i]) <= 0xBF) ||
+                        (c == 0xF4 &&
+                         static_cast< unsigned char >(p1[i]) >= 0x80 &&
+                         static_cast< unsigned char >(p1[i]) <= 0x8F)) &&
+                       static_cast< unsigned char >(p1[i + 1]) >= 0x80 &&
+                       static_cast< unsigned char >(p1[i + 1]) <= 0xBF &&
+                       static_cast< unsigned char >(p1[i + 2]) >= 0x80 &&
+                       static_cast< unsigned char >(p1[i + 2]) <= 0xBF)
+            {
+                sal_Int32 u = ((c & 0x07) << 18) |
+                    ((static_cast< unsigned char >(p1[i]) & 0x3F) << 12) |
+                    ((static_cast< unsigned char >(p1[i + 1]) & 0x3F) << 6) |
+                    (static_cast< unsigned char >(p1[i + 2]) & 0x3F);
+                i += 3;
+                p2[n2++] = static_cast< WCHAR >(((u - 0x10000) >> 10) | 0xD800);
+                p2[n2++] = static_cast< WCHAR >(
+                    ((u - 0x10000) & 0x3FF) | 0xDC00);
+            } else {
+                SetLastError(ERROR_BAD_PATHNAME);
+                return NULL;
+            }
+            break;
+        }
+    }
+    WCHAR * end;
+    if (colon || p2[0] == L'\\') {
+        // Interpret p2 as an absolute path:
+        end = path;
+    } else {
+        // Interpret p2 as a relative path:
+        end = filename(path);
+    }
+    return buildPath(path, path, end, p2, n2);
+}
+
+void fail() {
+    LPWSTR buf = NULL;
+    FormatMessageW(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+        GetLastError(), 0, reinterpret_cast< LPWSTR >(&buf), 0, NULL);
+    MessageBoxW(NULL, buf, NULL, MB_OK | MB_ICONERROR);
+    LocalFree(buf);
+    TerminateProcess(GetCurrentProcess(), 255);
+}
+
+bool contains(WCHAR const * paths, WCHAR const * path, WCHAR const * pathEnd) {
+    WCHAR const * q = path;
+    for (WCHAR const * p = paths;; ++p) {
+        WCHAR c = *p;
+        switch (c) {
+        case L'\0':
+            return q == pathEnd;
+        case L';':
+            if (q == pathEnd) {
+                return true;
+            }
+            q = path;
+            break;
+        default:
+            if (q != NULL) {
+                if (q != pathEnd && *q == c) {
+                    ++q;
+                } else {
+                    q = NULL;
+                }
+            }
+            break;
+        }
+    }
+}
+
+}
+
 namespace desktop_win32 {
 
-void extendLoaderEnvironment() {
-    char programPath[MAX_PATH];
-    char * programPathEnd = delayload::getModulePath(programPath, NULL);
-    if (programPathEnd == NULL) {
-        abort();
+void extendLoaderEnvironment(WCHAR * binPath, WCHAR * iniDirectory) {
+    if (!GetModuleFileNameW(NULL, iniDirectory, MAX_PATH)) {
+        fail();
     }
-    *programPathEnd = '\0';
-    std::size_t const envSepLen = RTL_CONSTASCII_LENGTH(";");
-    std::size_t const maxEnv = 32767;
-    char pad[MAX_PATH - 1 + envSepLen + maxEnv];
+    WCHAR * pathEnd = filename(iniDirectory);
+    WCHAR name[MAX_PATH + MY_LENGTH(L".bin")];
         // hopefully std::size_t is large enough to not overflow
-    char * ureBinPathEnd = delayload::getUreBinPath(
-        pad, programPath, programPathEnd);
-    //TODO  As long as there is an old style OOo that has no ure-link, do
-    // nothing if ureBinPath cannot be determined; once there is only a new
-    // style OOo that always has to have a ure-link, fail if ureBinPath cannot
-    // be determined:
-    if (ureBinPathEnd != NULL) {
-        DWORD n = GetEnvironmentVariableA(
-            "PATH", ureBinPathEnd + envSepLen, maxEnv);
-        if (n >= maxEnv) {
-            abort();
+    WCHAR * nameEnd = name;
+    for (WCHAR * p = pathEnd; *p != L'\0'; ++p) {
+        *nameEnd++ = *p;
+    }
+    if (!(nameEnd - name >= 4 && nameEnd[-4] == L'.' &&
+          (nameEnd[-3] == L'E' || nameEnd[-3] == L'e') &&
+          (nameEnd[-2] == L'X' || nameEnd[-2] == L'x') &&
+          (nameEnd[-1] == L'E' || nameEnd[-1] == L'e')))
+    {
+        *nameEnd = L'.';
+        nameEnd += 4;
+    }
+    nameEnd[-3] = 'b';
+    nameEnd[-2] = 'i';
+    nameEnd[-1] = 'n';
+    buildPath(binPath, iniDirectory, pathEnd, name, nameEnd - name);
+    WCHAR path[MAX_PATH];
+    pathEnd = buildPath(
+        path, iniDirectory, pathEnd, MY_STRING(L"..\\basis-link"));
+    if (pathEnd == NULL) {
+        fail();
+    }
+    pathEnd = resolveLink(path);
+    if (pathEnd == NULL) {
+        fail();
+    }
+    std::size_t const maxEnv = 32767;
+    WCHAR pad[2 * MAX_PATH + maxEnv];
+        // hopefully std::size_t is large enough to not overflow
+    WCHAR * padEnd = buildPath(pad, path, pathEnd, MY_STRING(L"\\program"));
+    if (padEnd == NULL) {
+        fail();
+    }
+    WCHAR env[maxEnv];
+    DWORD n = GetEnvironmentVariableW(L"PATH", env, maxEnv);
+    if (n >= maxEnv || n == 0 && GetLastError() != ERROR_ENVVAR_NOT_FOUND) {
+        fail();
+    }
+    env[n] = L'\0';
+    bool exclude1 = contains(env, pad, padEnd);
+    WCHAR * pad2 = exclude1 ? pad : padEnd + 1;
+    pathEnd = buildPath(path, path, pathEnd, MY_STRING(L"\\ure-link"));
+    if (pathEnd == NULL) {
+        fail();
+    }
+    pathEnd = resolveLink(path);
+    if (pathEnd == NULL) {
+        fail();
+    }
+    padEnd = buildPath(pad2, path, pathEnd, MY_STRING(L"\\bin"));
+    if (padEnd == NULL) {
+        fail();
+    }
+    bool exclude2 = contains(env, pad2, padEnd);
+    if (!(exclude1 && exclude2)) {
+        if (!(exclude1 || exclude2)) {
+            pad2[-1] = L';';
         }
-        bool set = true;
-        if (n == 0) {
-            if (GetLastError() != ERROR_ENVVAR_NOT_FOUND) {
-                abort();
-            }
-        } else {
-            // Do not add ureBinPath to the PATH if it is already there; this
-            // check fails if a differently spelled equivalent of ureBinPath is
-            // already there, but the assumption is that if multiple application
-            // loaders recursively execute this code the spelling of ureBinPath
-            // will be equivalent, so PATH cannot grow unbounded:
-            std::ptrdiff_t ureBinPathLen = ureBinPathEnd - pad;
-            for (char const * p = ureBinPathEnd + envSepLen;;) {
-                char const * q = p + 1;
-                while (*q != '\0' && *q != ';') {
-                    ++q;
-                }
-                if (q - p == ureBinPathLen &&
-                    strncmp(p, pad, ureBinPathLen) == 0)
-                {
-                    set = false;
-                    break;
-                }
-                p = q;
-                if (*p == '\0') {
-                    break;
-                }
-                ++p;
-            }
-            if (set) {
-                *ureBinPathEnd = ';';
-            }
+        WCHAR * p = exclude2 ? pad2 - 1 : padEnd;
+        if (n != 0) {
+            *p++ = L';';
         }
-        if (set && !SetEnvironmentVariableA("PATH", pad)) {
-            abort();
+        for (DWORD i = 0; i <= n; ++i) {
+            *p++ = env[i];
+        }
+        if (!SetEnvironmentVariableW(L"PATH", pad)) {
+            fail();
         }
     }
 }
