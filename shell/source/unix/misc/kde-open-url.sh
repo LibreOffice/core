226a2259a779ce93ce73089366e77671154f99b2
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 11:34:05 +0000

    INTEGRATION: CWS sb83 (1.3.64); FILE MERGED 2008/02/12 12:45:17 sb 1.3.64.1: #i86041# do not add Mozilla library directory to LD_LIBRARY_PATH in soffice start script

--- shell/source/unix/misc/kde-open-url.sh
+++ shell/source/unix/misc/kde-open-url.sh
@@ -1,54 +1,5 @@
 #!/bin/sh
 
-# restore search path for dynamic loader to system defaults to
-# avoid version clashes of mozilla libraries shipped with OOo
-case `uname -s` in
-  AIX)
-    LIBPATH=$SYSTEM_LIBPATH
-    if [ -z "$LIBPATH" ]; then
-        unset LIBPATH SYSTEM_LIBPATH
-    else
-        export LIBPATH; unset SYSTEM_LIBPATH
-    fi
-    ;;
-
-  Darwin)
-    DYLD_LIBRARY_PATH=$SYSTEM_DYLD_LIBRARY_PATH
-    if [ -z "$DYLD_LIBRARY_PATH" ]; then
-        unset DYLD_LIBRARY_PATH SYSTEM_DYLD_LIBRARY_PATH
-    else
-        export DYLD_LIBRARY_PATH; unset SYSTEM_DYLD_LIBRARY_PATH
-    fi
-    ;;
-
-  HP-UX)
-    SHLIB_PATH=$SYSTEM_SHLIB_PATH
-    if [ -z "$SHLIB_PATH" ]; then
-        unset SHLIB_PATH SYSTEM_SHLIB_PATH
-    else
-        export SHLIB_PATH; unset SYSTEM_SHLIB_PATH
-    fi
-    ;;
-
-  IRIX*)
-    LD_LIBRARYN32_PATH=$SYSTEM_LD_LIBRARYN32_PATH
-    if [ -z "$LD_LIBRARYN32_PATH" ]; then
-        unset LD_LIBRARYN32_PATH SYSTEM_LD_LIBRARYN32_PATH
-    else
-        export LD_LIBRARYN32_PATH; unset $SYSTEM_LD_LIBRARYN32_PATH
-    fi
-    ;;
-
-  *)
-    LD_LIBRARY_PATH=$SYSTEM_LD_LIBRARY_PATH
-    if [ -z "$LD_LIBRARY_PATH" ]; then
-        unset LD_LIBRARY_PATH SYSTEM_LD_LIBRARY_PATH
-    else
-        export LD_LIBRARY_PATH; unset SYSTEM_LD_LIBRARY_PATH
-    fi
-    ;;
-esac
-
 # special handling for mailto: uris
 if echo $1 | grep '^mailto:' > /dev/null; then
   kmailservice "$1" &
