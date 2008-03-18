6428e734ce9e1c551e12507321fd532a86b2bce1
Vladimir Glazounov <vg@openoffice.org>
2008-03-18 13:44:44 +0000

    INTEGRATION: CWS mingwport12 (1.45.8); FILE MERGED 2008/03/17 10:43:23 vg 1.45.8.1: #i86795# fix for mingw

--- berkeleydb/makefile.mk
+++ berkeleydb/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.45 $
+#   $Revision: 1.46 $
 #
-#   last change: $Author: obo $ $Date: 2008-01-04 16:17:49 $
+#   last change: $Author: vg $ $Date: 2008-03-18 14:44:44 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -148,6 +148,9 @@ OUT2BIN=$(BUILD_DIR)$/.libs$/libdb*42.dll
 
 OUT2INC= \
     $(BUILD_DIR)$/db.h
+.IF "$(GUI)$(COM)"=="WNTGCC"
+.EXPORT : PWD
+.ENDIF
 
 .ELSE
 # make use of stlport headerfiles
