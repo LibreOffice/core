76f5e37fe834ca98219975aa93e6c4d39477b82b
Rüdiger Timm <rt@openoffice.org>
2008-03-26 06:04:41 +0000

    One license header should be sufficient

--- bridges/test/performance/testperformance.cxx
+++ bridges/test/performance/testperformance.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: testperformance.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:06:06 $
+ *  last change: $Author: rt $ $Date: 2008-03-26 07:04:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,40 +36,6 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_bridges.hxx"
-/*#*************************************************************************
-#
- *  OpenOffice.org - a multi-platform office productivity suite
- *
- *  $RCSfile: testperformance.cxx,v $
- *
- *  $Revision: 1.3 $
- *
- *  last change: $Author: obo $ $Date: 2006-09-16 16:06:06 $
- *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
- *
- *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
- *
-#************************************************************************* */
 
 #include <stdio.h>
 #include <math.h>
