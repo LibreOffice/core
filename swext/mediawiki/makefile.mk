1070e7af9b01574c13e9bee5a202d8e7ed39536a
Mikhail Voitenko <mav@openoffice.org>
2008-03-26 15:30:08 +0000

    allow easy name change

--- swext/mediawiki/makefile.mk
+++ swext/mediawiki/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: mav $ $Date: 2008-02-13 12:07:47 $
+#   last change: $Author: mav $ $Date: 2008-03-26 16:30:07 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,6 +53,6 @@ ALLTAR: ANTBUILD
 
 .ELSE
 @all:
-    @echo "MediaWiki Editor extension disabled."
+    @echo "MediaWiki Publisher extension disabled."
 .ENDIF
 
