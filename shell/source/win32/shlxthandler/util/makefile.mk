793284ab1919899468f8d57290e08e188c978237
Kurt Zenker <kz@openoffice.org>
2008-04-02 08:45:27 +0000

    INTEGRATION: CWS obo24 (1.4.214); FILE MERGED 2008/03/13 15:26:36 obo 1.4.214.1: #i75046# additional build for 64 bit shell extension (Windows Vista)

--- shell/source/win32/shlxthandler/util/makefile.mk
+++ shell/source/win32/shlxthandler/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-07 20:06:40 $
+#   last change: $Author: kz $ $Date: 2008-04-02 09:45:27 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -47,6 +47,10 @@ CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -
 CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
 CDEFS+=-D_WIN32_IE=0x501
 
+CFLAGS_X64+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
+CFLAGS_X64+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
+CDEFS_X64+=-D_WIN32_IE=0x501
+
 # --- Files --------------------------------------------------------
 
 SLOFILES=$(SLO)$/dbgmacros.obj\
@@ -55,6 +59,15 @@ SLOFILES=$(SLO)$/dbgmacros.obj\
     $(SLO)$/utilities.obj\
     $(SLO)$/iso8601_converter.obj
 
+SLOFILES_X64=$(SLO_X64)$/dbgmacros.obj\
+    $(SLO_X64)$/fileextensions.obj\
+    $(SLO_X64)$/registry.obj\
+    $(SLO_X64)$/utilities.obj\
+    $(SLO_X64)$/iso8601_converter.obj
+
 # --- Targets ------------------------------------------------------
 
-.INCLUDE :	target.mk 
+.INCLUDE :	set_wntx64.mk
+.INCLUDE :	target.mk
+.INCLUDE :	tg_wntx64.mk
+
