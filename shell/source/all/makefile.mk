86afc1bd073f1a02ad7825c8a66bc9aa9ff2929d
Kurt Zenker <kz@openoffice.org>
2008-04-02 08:42:56 +0000

    INTEGRATION: CWS obo24 (1.6.38); FILE MERGED 2008/03/13 15:26:34 obo 1.6.38.1: #i75046# additional build for 64 bit shell extension (Windows Vista)

--- shell/source/all/makefile.mk
+++ shell/source/all/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: hr $ $Date: 2007-11-02 13:10:42 $
+#   last change: $Author: kz $ $Date: 2008-04-02 09:42:56 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,15 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :  settings.mk
 
 .IF "$(GUI)" == "WNT"
-CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501 
+CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
 CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
 CDEFS+=-D_WIN32_IE=0x501
+
+.IF "$(BUILD_X64)"!=""
+CFLAGS_X64+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
+CFLAGS_X64+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
+CDEFS_X64+=-D_WIN32_IE=0x501
+.ENDIF # "$(BUILD_X64)"!=""
 .ENDIF
 
 .IF "$(GUI)" == "OS2"
@@ -60,5 +66,11 @@ CFLAGS+=-DSYSTEM_EXPAT
 .ENDIF
 
 SLOFILES=$(SLO)$/xml_parser.obj
+.IF "$(BUILD_X64)"!=""
+SLOFILES_X64=$(SLO_X64)$/xml_parser.obj
+.ENDIF # "$(BUILD_X64)"!=""
 
+.INCLUDE :  set_wntx64.mk
 .INCLUDE :  target.mk
+.INCLUDE :  tg_wntx64.mk
+
