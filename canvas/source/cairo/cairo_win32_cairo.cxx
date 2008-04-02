4eb43e86607768ceb4df4f218f0dfc3d8640370c
Kurt Zenker <kz@openoffice.org>
2008-04-02 08:44:48 +0000

    INTEGRATION: CWS cairoquartz01 (1.1.2); FILE ADDED 2008/03/27 22:58:56 thb 1.1.2.10: Nevermind, PEBKAC - fixed the last win32 build breakers now 2008/01/18 19:02:39 mox 1.1.2.9: Fix namespace of headers on other platforms - win32 & Xlib. 2008/01/12 23:52:06 mox 1.1.2.8: This time cairo actually WORKS ON MACOSX! 2007/07/29 08:19:49 mox 1.1.2.7: Rework Quartz window drawing to align with the implementation in VCL 2007/07/20 10:11:48 mox 1.1.2.6: Cleanup includes and revise code comments 2007/07/19 17:25:50 mox 1.1.2.5: Cleanup - We don't need to store mpSysData inside the Surface (in 1.2 API). 2007/07/19 16:19:02 mox 1.1.2.4: Further cairo win32 backend cleanup. 2007/07/19 07:26:35 mox 1.1.2.3: Legacy cairo API support is not needed on Win32 and Mac OS X Quartz. 2007/07/18 21:32:40 radekdoulik 1.1.2.2: Issue number:  69066 Submitted by:  radekdoulik Reviewed by:   radekdoulik
    Removed platform dependend code from cairo_canvashelper_text.cxx
    2007/07/17 22:34:42 mox 1.1.2.1: Rework cross-platform files to be separate for each platform.
    The <1.2 API support is now mostly in platform specific files.

---
+++ canvas/source/cairo/cairo_win32_cairo.cxx
@@ -0,0 +1,203 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: cairo_win32_cairo.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-02 09:44:48 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_canvas.hxx"
+
+
+#ifdef WNT
+/************************************************************************
+ * Win32 surface backend for OpenOffice.org Cairo Canvas                *
+ ************************************************************************/
+
+#include <tools/prewin.h>
+#include <windows.h>
+#include <tools/postwin.h>
+
+#include <osl/diagnose.h>
+
+#include "cairo_cairo.hxx"
+
+#ifdef CAIRO_HAS_WIN32_SURFACE
+
+#include <cairo-win32.h>
+
+namespace cairo
+{
+  /**
+   * Surface::Surface:     Create generic Canvas surface using given Cairo Surface
+   *
+   * @param pSurface Cairo Surface
+   *
+   * This constructor only stores data, it does no processing.
+   * It is used with e.g. cairo_image_surface_create_for_data()
+   * and Surface::getSimilar()
+   *
+   * Set the mpSurface to the new surface or NULL
+   **/
+    Surface::Surface( cairo_surface_t* pSurface )
+            : mnRefCount( 1 ),
+              mpSurface( pSurface )
+    {
+    }
+
+
+  /**
+   * Surface::Surface:   Create Canvas surface from Window reference.
+   * @param pSysData Platform native system environment data (struct SystemEnvData in vcl/inc/sysdata.hxx)
+   * @param x horizontal location of the new surface
+   * @param y vertical location of the new surface
+   * @param width width of the new surface
+   * @param height height of the new surface
+   *
+   * pSysData contains the platform native Window reference.
+   * pSysData is used to create a surface on the Window
+   *
+   * Set the mpSurface to the new surface or NULL
+   **/
+    Surface::Surface( const SystemEnvData* pSysData, int x, int y, int /* width */, int /* height */ )
+        : mnRefCount( 1 ),
+          mpSurface( NULL )
+    {
+        HDC hDC;
+
+        hDC = GetDC( pSysData->hWnd );
+        mpSurface = cairo_win32_surface_create( hDC );
+        cairo_surface_set_device_offset( mpSurface, x, y );
+    }
+
+
+  /**
+   * Surface::Surface:   Create platfrom native Canvas surface from BitmapSystemData
+   * @param pSysData (not used)
+   * @param pBmpData Platform native image data (struct BitmapSystemData in vcl/inc/bitmap.hxx)
+   * @param width width of the new surface
+   * @param height height of the new surface
+   *
+   * Create a surface based on image data on pBmpData
+   *
+   * Set the mpSurface to the new surface or NULL
+   **/
+    Surface::Surface( const SystemEnvData* /* pSysData */, const BitmapSystemData* pBmpData, int width, int height )
+        : mnRefCount( 1 ),
+          mpSurface( NULL )
+    {
+        OSL_ASSERT (pBmpData->mnWidth == width && pBmpData->mnHeight == height);
+
+        // NOTE: width and height are not used in win32
+        OSL_ASSERT (pBmpData->pDIB == NULL);
+
+        // FIXME: could we use
+        // cairo_win32_surface_create_with_ddb (HDC hdc, cairo_format_t format, int width, int height);
+        // and
+        // cairo_win32_surface_create_with_dib (cairo_format_t format, int width, int height);
+        // instead?
+        //
+
+        if (pBmpData->pDIB != NULL) {
+#if 0
+            // This code will not work anyway, as most (?) DIBs that
+            // come here will be in bottom-down order (upside-down)
+            // and in any case have a different order of colour
+            // channels compared to what cairo expects.
+            PBITMAPINFOHEADER pBIH = (PBITMAPINFOHEADER) GlobalLock ((HANDLE) p_BmpData->pDIB);
+            cairo_format_t fmt;
+            OSL_ASSERT (pBIH->biBitCount == 24 && pBIH->biCompression == BI_RGB);
+            mhDIB = p_BmpData->pDIB;
+            if (pBIH->biBitCount == 24 && pBIH->biCompression == BI_RGB)
+            {
+                mpSurface = cairo_image_surface_create_for_data (((unsigned char *) pBIH) + pBIH->biSize,
+                                                                 CAIRO_FORMAT_RGB24,
+                                                                 pBIH->biWidth, pBIH->biHeight,
+                                                                 4*((3*pBIH->biWidth-1)/4+1));
+            }
+#else
+            // So just leave mpSurface to NULL, little else we can do at
+            // this stage. Hopefully the Win32 patch to
+            // cairocanvas::DeviceHelper::getSurface(BitmapSystemData&,
+            // const Size&) will catch the cases where this
+            // constructor would be called with a DIB bitmap, and we
+            // will never get here. At least it worked for Ballmer.ppt.
+#endif
+        } else {
+            HDC hDC;
+            void* hOrigBitmap;
+            hDC = CreateCompatibleDC( NULL );
+            OSL_TRACE ("::cairo::cairo::Surface::Surface(): Selecting bitmap %p into DC %p", pBmpData->pDDB, hDC);
+            hOrigBitmap = SelectObject( hDC, (HANDLE) pBmpData->pDDB );
+            if (hOrigBitmap == NULL)
+                OSL_TRACE ("SelectObject failed: %d", GetLastError ());
+            mpSurface = cairo_win32_surface_create( hDC );
+        }
+
+    }
+
+
+  /**
+   * Surface::getDepth:  Get the color depth of the Canvas surface.
+   *
+   * @return color depth
+   **/
+    int
+    Surface::getDepth()
+    {
+        if (mpSurface) {
+            switch (cairo_surface_get_content (mpSurface)) {
+                case CAIRO_CONTENT_ALPHA:       return 8;  break;
+                case CAIRO_CONTENT_COLOR:       return 24; break;
+                case CAIRO_CONTENT_COLOR_ALPHA: return 32; break;
+            }
+        }
+        OSL_TRACE("Canvas::cairo::Surface::getDepth(): ERROR - depth unspecified!");
+        return -1;
+    }
+
+
+  /**
+   * Surface::fillSystemGraphicsData:   Fill SystemGraphicsData with native surface data
+   * @param aSystemGraphicsData Platform native system graphics data (struct SystemGraphicsData in vcl/inc/sysdata.hxx)
+   *
+   */
+    void Surface::fillSystemGraphicsData( SystemGraphicsData& aSystemGraphicsData)
+    {
+        aSystemGraphicsData.hDC = cairo_win32_surface_get_dc( mpSurface );
+    }
+
+}  // namespace cairo
+
+#endif   // CAIRO_HAS_WIN32_SURFACE
+
+#endif   // WNT
