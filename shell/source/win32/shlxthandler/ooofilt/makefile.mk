c47d4af5dec94901cc4c6e1a6df05afd7ed41685
Kurt Zenker <kz@openoffice.org>
2008-04-02 08:44:16 +0000

    INTEGRATION: CWS obo24 (1.6.80); FILE MERGED 2008/03/13 15:26:36 obo 1.6.80.1: #i75046# additional build for 64 bit shell extension (Windows Vista)

--- shell/source/win32/shlxthandler/ooofilt/makefile.mk
+++ shell/source/win32/shlxthandler/ooofilt/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 13:55:46 $
+#   last change: $Author: kz $ $Date: 2008-04-02 09:44:16 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,8 +44,8 @@ USE_DEFFILE=TRUE
 
 .INCLUDE :  settings.mk
 
-CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501 
-CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820 
+CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
+CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
 CDEFS+=-D_WIN32_IE=0x501
 
 # --- Files --------------------------------------------------------
@@ -75,18 +75,54 @@ SHL1STDLIBS+=$(OLE32LIB)\
      $(SHELL32LIB)\
      $(KERNEL32LIB)\
      $(OLDNAMESLIB)
-     
+
 SHL1LIBS+=$(SLB)$/util.lib\
     $(SLB)$/ooofilereader.lib
 
 SHL1DEPN=
-SHL1OBJS=$(SLOFILES) 
+SHL1OBJS=$(SLOFILES)
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
 DEF1NAME=$(SHL1TARGET)
 DEF1EXPORTFILE=exports.dxp
 
+
+.IF "$(BUILD_X64)"!=""
+#---------------------------- x64 -------------------------
+CFLAGS_X64+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
+CFLAGS_X64+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
+CDEFS_X64+=-D_WIN32_IE=0x501
+USE_DEFFILE_X64=TRUE
+
+SLOFILES_X64=$(SLO_X64)$/ooofilt.obj\
+        $(SLO_X64)$/propspec.obj
+
+SHL1TARGET_X64=$(TARGET)
+
+SHL1LIBS_X64=$(SOLARLIBDIR_X64)$/zlib.lib\
+    $(SOLARLIBDIR_X64)$/expat_xmlparse.lib\
+    $(SOLARLIBDIR_X64)$/expat_xmltok.lib
+
+SHL1STDLIBS_X64+=$(OLE32LIB_X64)\
+     $(ADVAPI32LIB_X64)\
+     $(COMCTL32LIB_X64)\
+     $(UUIDLIB_X64)\
+     $(USER32LIB_X64) \
+     $(SHELL32LIB_X64)\
+     $(KERNEL32LIB_X64)\
+     $(MSVCPRT_X64) \
+     $(MSVCRT_X64) \
+     $(OLDNAMESLIB_X64)
+
+SHL1LIBS_X64+=$(SLB_X64)$/util.lib\
+    $(SLB_X64)$/ooofilereader.lib
+SHL1OBJS_X64=$(SLOFILES_X64)
+SHL1DEF_X64=$(MISC_X64)$/$(SHL1TARGET_X64).def
+DEF1NAME_X64=$(SHL1TARGET_X64)
+DEF1EXPORTFILE_X64=exports.dxp
+.ENDIF # "$(BUILD_X64)"!=""
+
 # --- Targets ------------------------------------------------------
 
+.INCLUDE :	set_wntx64.mk
 .INCLUDE :	target.mk
-
-
+.INCLUDE :	tg_wntx64.mk
