b552633b17f821621cf60e5c6ead5bf49c8f4f44
Kurt Zenker <kz@openoffice.org>
2008-04-02 08:43:10 +0000

    INTEGRATION: CWS obo24 (1.4.38); FILE MERGED 2008/03/13 15:26:34 obo 1.4.38.1: #i75046# additional build for 64 bit shell extension (Windows Vista)

--- shell/source/all/ooofilereader/makefile.mk
+++ shell/source/all/ooofilereader/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: hr $ $Date: 2007-11-02 13:11:14 $
+#   last change: $Author: kz $ $Date: 2008-04-02 09:43:10 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,12 @@ ENABLE_EXCEPTIONS=TRUE
 .INCLUDE :  settings.mk
 
 .IF "$(GUI)" != "OS2"
-CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501 
+CFLAGS+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
 CFLAGS+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
 CDEFS+=-D_WIN32_IE=0x501
+CFLAGS_X64+=-DISOLATION_AWARE_ENABLED -DWIN32_LEAN_AND_MEAN -DXML_UNICODE -D_NTSDK -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0501
+CFLAGS_X64+=-wd4710 -wd4711 -wd4514 -wd4619 -wd4217 -wd4820
+CDEFS_X64+=-D_WIN32_IE=0x501
 .ENDIF
 .IF "$(GUI)" == "OS2"
 CFLAGS+=-DISOLATION_AWARE_ENABLED -DXML_UNICODE -DXML_UNICODE_WCHAR_T -DUNICODE -D_UNICODE
@@ -61,12 +64,28 @@ SLOFILES=$(SLO)$/basereader.obj\
          $(SLO)$/simpletag.obj\
          $(SLO)$/keywordstag.obj\
          $(SLO)$/autostyletag.obj
-    
+
 LIB1TARGET=$(SLB)$/$(TARGET).lib
 LIB1OBJFILES=$(SLOFILES)
 LIB1FILES=$(SLB)$/zipfile.lib\
           $(SLB)$/xmlparser.lib
-    
+
+.IF "$(BUILD_X64)"!=""
+SLOFILES_X64=$(SLO_X64)$/basereader.obj\
+         $(SLO_X64)$/metainforeader.obj\
+         $(SLO_X64)$/contentreader.obj\
+         $(SLO_X64)$/simpletag.obj\
+         $(SLO_X64)$/keywordstag.obj\
+         $(SLO_X64)$/autostyletag.obj
+
+LIB1TARGET_X64=$(SLB_X64)$/$(TARGET).lib
+LIB1OBJFILES_X64=$(SLOFILES_X64)
+LIB1FILES_X64=$(SLB_X64)$/zipfile.lib\
+          $(SLB_X64)$/xmlparser.lib
+.ENDIF # "$(BUILD_X64)"!=""
 # --- Targets ------------------------------------------------------
 
+.INCLUDE :	set_wntx64.mk
 .INCLUDE :	target.mk
+.INCLUDE :	tg_wntx64.mk
+
