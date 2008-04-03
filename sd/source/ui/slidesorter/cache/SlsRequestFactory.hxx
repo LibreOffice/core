250d985771e8e61fa7923fd1d3b4d835b22945e0
Kurt Zenker <kz@openoffice.org>
2008-04-03 13:21:07 +0000

    INTEGRATION: CWS presenterview (1.6.74); FILE MERGED 2008/02/26 12:25:31 af 1.6.74.2: #i18486# Dropped the use of templates for implementing cache classes. 2007/04/26 15:41:42 af 1.6.74.1: #i75317# Separation of SlideSorter from ViewShell.

--- sd/source/ui/slidesorter/cache/SlsRequestFactory.hxx
+++ sd/source/ui/slidesorter/cache/SlsRequestFactory.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SlsRequestFactory.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 14:35:18 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 14:21:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,91 +36,21 @@
 #ifndef SD_SLIDESORTER_REQUEST_FACTORY_HXX
 #define SD_SLIDESORTER_REQUEST_FACTORY_HXX
 
-#include "model/SlideSorterModel.hxx"
-#include "model/SlsPageDescriptor.hxx"
-#include "view/SlideSorterView.hxx"
-#include <svx/svdpagv.hxx>
-
-#ifndef _SDRPAGEWINDOW_HXX
-#include <svx/sdrpagewindow.hxx>
-#endif
-
-#include <svx/sdr/contact/viewcontact.hxx>
-
-namespace sd { namespace slidesorter { namespace view {
-class PageObjectViewObjectContact;
-} } }
+#include "cache/SlsCacheContext.hxx"
 
 namespace sd { namespace slidesorter { namespace cache {
 
-template <class Queue, bool UseAheadOfTimeRequests>
+class RequestQueue;
+
 class RequestFactory
 {
 public:
     void operator() (
-        const model::SlideSorterModel& rModel,
-        const view::SlideSorterView& rView,
-        Queue& mrQueue);
+        RequestQueue& rRequestQueue,
+        const SharedCacheContext& rpCacheContext);
 };
 
 
-
-
-//=============================================================================
-// Implementation
-
-//=====  RequestFactory  ======================================================
-
-template <class Queue,bool UseAheadOfTimeRequests>
-    void RequestFactory<Queue,UseAheadOfTimeRequests>::operator() (
-        const model::SlideSorterModel& rModel,
-        const view::SlideSorterView& rView,
-        Queue& rQueue)
-{
-    // Fill the queues with the new content.  Visible page objects go into
-    // the regular queue, non-visible page objects go into the ahead-of-time
-    // queue.
-    SdrPageView* pPageView = rView.GetSdrPageView();
-    SdrPageWindow* pPageWindow = NULL;
-    if (pPageView != NULL)
-        pPageWindow = pPageView->GetPageWindow(0);
-    if (pPageWindow != NULL)
-    {
-        ::sdr::contact::ObjectContact& rObjectContact (
-            pPageWindow->GetObjectContact());
-
-        // Add the requests for the visible pages.
-        model::SlideSorterModel::Enumeration aPageEnumeration (
-            rModel.GetVisiblePagesEnumeration());
-        while (aPageEnumeration.HasMoreElements())
-        {
-            model::SharedPageDescriptor pDescriptor (aPageEnumeration.GetNextElement());
-            view::PageObjectViewObjectContact* pRequest (pDescriptor->GetViewObjectContact());
-            if (pRequest != NULL)
-                rQueue.AddRequest(*pRequest, VISIBLE_NO_PREVIEW);
-        }
-
-        if (UseAheadOfTimeRequests)
-        {
-            // Add the requests for the non-visible pages.
-            aPageEnumeration = rModel.GetAllPagesEnumeration();
-            while (aPageEnumeration.HasMoreElements())
-            {
-                model::SharedPageDescriptor pDescriptor (aPageEnumeration.GetNextElement());
-                view::PageObjectViewObjectContact* pRequest =
-                    static_cast<view::PageObjectViewObjectContact*>(
-                        &pDescriptor->GetPageObject()->GetViewContact()
-                        .GetViewObjectContact(rObjectContact));
-                if ( ! pDescriptor->IsVisible())
-                    rQueue.AddRequest(*pRequest, NOT_VISIBLE);
-            }
-        }
-    }
-}
-
-
-
 } } } // end of namespace ::sd::slidesorter::cache
-
 
 #endif
