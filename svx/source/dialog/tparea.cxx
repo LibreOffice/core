251a51e756b4c480d5ddac10bebc6b5ac06b058d
Kurt Zenker <kz@openoffice.org>
2008-04-03 15:58:37 +0000

    INTEGRATION: CWS vcl87 (1.28.322); FILE MERGED 2008/03/03 14:43:49 pl 1.28.322.1: #i76307# take RTL UI into account

--- svx/source/dialog/tparea.cxx
+++ svx/source/dialog/tparea.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tparea.cxx,v $
  *
- *  $Revision: 1.28 $
+ *  $Revision: 1.29 $
  *
- *  last change: $Author: ihi $ $Date: 2007-07-12 10:56:04 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 16:58:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -800,6 +800,8 @@ SvxAreaTabPage::SvxAreaTabPage( Window* pParent, const SfxItemSet& rInAttrs ) :
 
     aTypeLB.SetSelectHdl( LINK( this, SvxAreaTabPage, SelectDialogTypeHdl_Impl ) );
 
+    // #i76307# always paint the preview in LTR, because this is what the document does
+    aCtlXRectPreview.EnableRTL(FALSE);
 }
 
 // -----------------------------------------------------------------------
