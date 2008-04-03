f58b46e6f9325f8bd1f5f9e093cfa85b2fe348fd
Kurt Zenker <kz@openoffice.org>
2008-04-03 13:35:08 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2007/09/11 11:59:52 af 1.1.2.3: #i18486# Added properties that control showing the focus and modifiability. 2007/08/13 14:03:45 af 1.1.2.2: #i18486# Added color properties. 2007/04/25 15:45:38 af 1.1.2.1: #i75317# Initial revision.

---
+++ sd/source/ui/slidesorter/inc/controller/SlsProperties.hxx
@@ -0,0 +1,136 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: SlsProperties.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 14:35:08 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SD_SLIDESORTER_PROPERTIES_HEADER
+#define SD_SLIDESORTER_PROPERTIES_HEADER
+
+#include <tools/color.hxx>
+
+namespace sd { namespace slidesorter { namespace controller {
+
+/** An extensible set of properties used throughout the slide sorter.
+*/
+class Properties
+{
+public:
+    Properties (void);
+    ~Properties (void);
+
+    /** When this method returns <TRUE/> then the current slide is
+        highlighted in the view.  The default value is <FALSE/>.
+    */
+    bool IsHighlightCurrentSlide (void) const;
+    void SetHighlightCurrentSlide (const bool bIsHighlightCurrentSlide);
+
+    /** When this method returns <TRUE/> then the selection is indicated in
+        the view (typically by drawing rectangles around the selected
+        slides.)  The default value is <TRUE/>.
+    */
+    bool IsShowSelection (void) const;
+    void SetShowSelection (const bool bIsShowSelection);
+
+    /** When this method returns <TRUE/> then the focusdselection is indicated in
+        the view (typically by drawing dotted rectangles around the selected
+        slides.)  The default value is <TRUE/>.
+    */
+    bool IsShowFocus (void) const;
+    void SetShowFocus (const bool bIsShowFocus);
+
+    /** When this method returns <TRUE/> then on a selection change the
+        visible area is adapted so that the selected slides are shown
+        centered in the view.  This can be used to center the current slide
+        by selecting only the current slide.  The default value is <FALSE/>.
+    */
+    bool IsCenterSelection (void) const;
+    void SetCenterSelection (const bool bIsCenterSelection);
+
+    /** When this mehod returns <TRUE/> then the view may try to change the
+        visible area by scrolling it smoothly on the screen.  Experimental.
+        Default value is <FALSE/>.
+    */
+    bool IsSmoothSelectionScrolling (void) const;
+    void SetSmoothSelectionScrolling (const bool bIsSmoothSelectionScrolling);
+
+    /** When this method returns <TRUE/> then during a full screen
+        presentation the previews in a slide sorter are not updated.
+        Default value is <TRUE/>.
+    */
+    bool IsSuspendPreviewUpdatesDuringFullScreenPresentation (void) const;
+    void SetSuspendPreviewUpdatesDuringFullScreenPresentation (const bool bFlag);
+
+    /** Return the background color.
+    */
+    Color GetBackgroundColor (void) const;
+    void SetBackgroundColor (const Color& rColor);
+
+    /** Return the text color.
+    */
+    Color GetTextColor (void) const;
+    void SetTextColor (const Color& rColor);
+
+    /** Return the color in which selections are to be painted.
+    */
+    Color GetSelectionColor (void) const;
+    void SetSelectionColor (const Color& rColor);
+
+    /** Return the color used for highlighting e.g. the current slide.
+    */
+    Color GetHighlightColor (void) const;
+    void SetHighlightColor (const Color& rColor);
+
+    /** The UI can be set to be read only indepently from the model status.
+        Used for instance in the presenter view.
+    */
+    bool IsUIReadOnly (void) const;
+    void SetUIReadOnly (const bool bIsUIReadOnly);
+
+private:
+    bool mbIsHighlightCurrentSlide;
+    bool mbIsShowSelection;
+    bool mbIsShowFocus;
+    bool mbIsCenterSelection;
+    bool mbIsSmoothSelectionScrolling;
+    bool mbIsSuspendPreviewUpdatesDuringFullScreenPresentation;
+    Color maBackgroundColor;
+    Color maTextColor;
+    Color maSelectionColor;
+    Color maHighlightColor;
+    bool mbIsUIReadOnly;
+};
+
+} } } // end of namespace ::sd::slidesorter::controller
+
+#endif
+
