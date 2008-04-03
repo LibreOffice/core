15684671238f0be95b8ceb3707ed534bf33cb93d
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:39:17 +0000

    INTEGRATION: CWS presenterview (1.2.22); FILE MERGED 2007/10/16 15:28:38 af 1.2.22.3: #i18486# Put code in ProcessStartupService in try/catch block. 2007/06/19 14:37:08 af 1.2.22.2: #i18486# Code cleanup. 2007/06/19 08:05:58 af 1.2.22.1: #i18486# Added code for instantiating services at framework startup.

--- sd/source/ui/framework/module/ModuleController.cxx
+++ sd/source/ui/framework/module/ModuleController.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ModuleController.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-03 15:53:05 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:39:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,9 +47,18 @@ using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::drawing::framework;
 using ::rtl::OUString;
+using ::sd::tools::ConfigurationAccess;
 
+#undef VERBOSE
+//#define VERBOSE 2
 
 namespace sd { namespace framework {
+
+static const sal_Int32 snFactoryPropertyCount (2);
+static const sal_Int32 snStartupPropertyCount (1);
+
+
+
 
 class ModuleController::ResourceToFactoryMap
     : public ::std::hash_map<
@@ -125,7 +134,7 @@ ModuleController::ModuleController (const Reference<XComponentContext>& rxContex
       mpLoadedFactories(new LoadedFactoryContainer())
 {
     (void)rxContext;
-    LoadFactories();
+    LoadFactories(rxContext);
 }
 
 
@@ -144,29 +153,30 @@ void SAL_CALL ModuleController::disposing (void)
 
 
 
-void ModuleController::LoadFactories (void)
+
+void ModuleController::LoadFactories (const Reference<XComponentContext>& rxContext)
 {
     try
     {
-        tools::ConfigurationAccess aConfiguration (
+        ConfigurationAccess aConfiguration (
+            rxContext,
             OUString::createFromAscii("/org.openoffice.Office.Impress/"),
-            tools::ConfigurationAccess::READ_ONLY);
+            ConfigurationAccess::READ_ONLY);
         Reference<container::XNameAccess> xFactories (
             aConfiguration.GetConfigurationNode(
                 OUString::createFromAscii("MultiPaneGUI/Framework/ResourceFactories")),
             UNO_QUERY);
-        ::std::vector<rtl::OUString> aProperties (2);
+        ::std::vector<rtl::OUString> aProperties (snFactoryPropertyCount);
         aProperties[0] = OUString::createFromAscii("ServiceName");
         aProperties[1] = OUString::createFromAscii("ResourceList");
-        tools::ConfigurationAccess::ForAll(
+        ConfigurationAccess::ForAll(
             xFactories,
             aProperties,
-            ::boost::bind(&ModuleController::ProcessFactory, this, _1));
+            ::boost::bind(&ModuleController::ProcessFactory, this, _2));
     }
     catch (Exception&)
     {
-        OSL_TRACE("ERROR in ModuleController::LoadDependencies");
-        // Ignore exception.
+        OSL_TRACE("ERROR in ModuleController::LoadFactories");
     }
 }
 
@@ -175,24 +185,100 @@ void ModuleController::LoadFactories (void)
 
 void ModuleController::ProcessFactory (const ::std::vector<Any>& rValues)
 {
-    if (rValues.size() == 2)
+    OSL_ASSERT(rValues.size() == snFactoryPropertyCount);
+
+    // Get the service name of the factory.
+    rtl::OUString sServiceName;
+    rValues[0] >>= sServiceName;
+
+    // Get all resource URLs that are created by the factory.
+    Reference<container::XNameAccess> xResources (rValues[1], UNO_QUERY);
+    ::std::vector<rtl::OUString> aURLs;
+    tools::ConfigurationAccess::FillList(
+        xResources,
+        OUString::createFromAscii("URL"),
+        aURLs);
+
+#if defined VERBOSE && VERBOSE>0
+    OSL_TRACE("ModuleController::adding factory %s",
+        OUStringToOString(sServiceName, RTL_TEXTENCODING_UTF8).getStr());
+#endif
+
+    // Add the resource URLs to the map.
+    ::std::vector<rtl::OUString>::const_iterator iResource;
+    for (iResource=aURLs.begin(); iResource!=aURLs.end(); ++iResource)
     {
-        // Get the service name of the factory.
+        (*mpResourceToFactoryMap)[*iResource] = sServiceName;
+#if defined VERBOSE && VERBOSE>1
+        OSL_TRACE("    %s",
+            OUStringToOString(*iResource, RTL_TEXTENCODING_UTF8).getStr());
+#endif
+    }
+}
+
+
+
+
+void ModuleController::InstantiateStartupServices (void)
+{
+    try
+    {
+        tools::ConfigurationAccess aConfiguration (
+            OUString::createFromAscii("/org.openoffice.Office.Impress/"),
+            tools::ConfigurationAccess::READ_ONLY);
+        Reference<container::XNameAccess> xFactories (
+            aConfiguration.GetConfigurationNode(
+                OUString::createFromAscii("MultiPaneGUI/Framework/StartupServices")),
+            UNO_QUERY);
+        ::std::vector<rtl::OUString> aProperties (snStartupPropertyCount);
+        aProperties[0] = OUString::createFromAscii("ServiceName");
+        tools::ConfigurationAccess::ForAll(
+            xFactories,
+            aProperties,
+            ::boost::bind(&ModuleController::ProcessStartupService, this, _2));
+    }
+    catch (Exception&)
+    {
+        OSL_TRACE("ERROR in ModuleController::InstantiateStartupServices");
+    }
+}
+
+
+
+
+void ModuleController::ProcessStartupService (const ::std::vector<Any>& rValues)
+{
+    OSL_ASSERT(rValues.size() == snStartupPropertyCount);
+
+    try
+    {
+        // Get the service name of the startup service.
         rtl::OUString sServiceName;
         rValues[0] >>= sServiceName;
 
-        // Get all resource URLs that are created by the factory.
-        Reference<container::XNameAccess> xResources (rValues[1], UNO_QUERY);
-        ::std::vector<rtl::OUString> aURLs;
-        tools::ConfigurationAccess::FillList(
-            xResources,
-            OUString::createFromAscii("URL"),
-            aURLs);
+        // Instantiate service.
+        Reference<lang::XMultiServiceFactory> xGlobalFactory (
+            ::comphelper::getProcessServiceFactory(), UNO_QUERY);
+        if (xGlobalFactory.is())
+        {
+            // Create the startup service.
+            Sequence<Any> aArguments(1);
+            aArguments[0] <<= mxController;
+            // Note that when the new object will be destroyed at the end of
+            // this scope when it does not register itself anywhere.
+            // Typically it will add itself as ConfigurationChangeListener
+            // at the configuration controller.
+            xGlobalFactory->createInstanceWithArguments(sServiceName, aArguments);
 
-        // Add the resource URLs to the map.
-        ::std::vector<rtl::OUString>::const_iterator iResource;
-        for (iResource=aURLs.begin(); iResource!=aURLs.end(); ++iResource)
-            (*mpResourceToFactoryMap)[*iResource] = sServiceName;
+#if defined VERBOSE && VERBOSE>0
+            OSL_TRACE("ModuleController::created startup service %s",
+                OUStringToOString(sServiceName, RTL_TEXTENCODING_UTF8).getStr());
+#endif
+        }
+    }
+    catch (Exception&)
+    {
+        OSL_TRACE("ERROR in ModuleController::ProcessStartupServices");
     }
 }
 
@@ -224,9 +310,9 @@ void SAL_CALL ModuleController::requestResource (const OUString& rsResourceURL)
                 // Create the factory service.
                 Sequence<Any> aArguments(1);
                 aArguments[0] <<= mxController;
-                xFactory = Reference<XInterface>(xGlobalFactory->createInstanceWithArguments(
+                xFactory = xGlobalFactory->createInstanceWithArguments(
                     iFactory->second,
-                    aArguments));
+                    aArguments);
 
                 // Remember that this factory has been instanced.
                 (*mpLoadedFactories)[iFactory->second] = xFactory;
@@ -248,7 +334,9 @@ void SAL_CALL ModuleController::initialize (const Sequence<Any>& aArguments)
         try
         {
             // Get the XController from the first argument.
-            mxController = Reference<frame::XController>(aArguments[0], UNO_QUERY);
+            mxController = Reference<frame::XController>(aArguments[0], UNO_QUERY_THROW);
+
+            InstantiateStartupServices();
         }
         catch (RuntimeException&)
         {}
