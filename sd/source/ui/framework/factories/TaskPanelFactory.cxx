0c5ca28461b582b283cb155c342abbdb0251cb2c
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:34:56 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2008/03/19 10:34:29 af 1.1.2.4: #i18486# Show AND expand activated resources. 2007/11/15 12:49:35 af 1.1.2.3: #i18486# Added isAnchorOnly() method. 2007/08/03 08:27:28 af 1.1.2.2: #i18486# Panels are collapsed not hidden when deactivated. 2007/07/10 15:05:27 af 1.1.2.1: #i18486# Moved here from ..\module

---
+++ sd/source/ui/framework/factories/TaskPanelFactory.cxx
@@ -0,0 +1,336 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: TaskPanelFactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:34:56 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+#include "TaskPanelFactory.hxx"
+#include "TaskPaneViewShell.hxx"
+#include "DrawController.hxx"
+#include "framework/FrameworkHelper.hxx"
+#include <cppuhelper/compbase1.hxx>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+using ::sd::framework::FrameworkHelper;
+using ::sd::toolpanel::TaskPaneViewShell;
+
+namespace sd { namespace framework {
+
+Reference<XInterface> SAL_CALL TaskPanelFactory_createInstance (
+    const Reference<XComponentContext>& rxContext)
+{
+    return Reference<XInterface>(static_cast<XWeak*>(new TaskPanelFactory(rxContext)));
+}
+
+
+
+
+::rtl::OUString TaskPanelFactory_getImplementationName (void) throw(RuntimeException)
+{
+    return ::rtl::OUString(
+        RTL_CONSTASCII_USTRINGPARAM("com.sun.star.comp.Draw.framework.TaskPanelFactory"));
+}
+
+
+
+
+Sequence<rtl::OUString> SAL_CALL TaskPanelFactory_getSupportedServiceNames (void)
+    throw (RuntimeException)
+{
+    static const OUString sServiceName(
+        OUString::createFromAscii("com.sun.star.drawing.framework.TaskPanelFactory"));
+    return Sequence<rtl::OUString>(&sServiceName, 1);
+}
+
+
+
+
+//===== ToolPanelResource =====================================================
+
+namespace {
+
+typedef ::cppu::WeakComponentImplHelper1 <
+    css::drawing::framework::XResource
+    > TaskPanelResourceInterfaceBase;
+
+class TaskPanelResource
+    : private ::cppu::BaseMutex,
+      public TaskPanelResourceInterfaceBase
+{
+public:
+    TaskPanelResource (
+        const Reference<XResourceId>& rxResourceId,
+        const TaskPaneViewShell::PanelId ePaneId);
+    virtual ~TaskPanelResource (void);
+
+    virtual void SAL_CALL disposing (void);
+
+    const TaskPaneViewShell::PanelId GetPaneId (void) const;
+
+    // XResource
+
+    virtual Reference<XResourceId> SAL_CALL getResourceId (void)
+        throw (css::uno::RuntimeException);
+
+    virtual sal_Bool SAL_CALL isAnchorOnly (void) throw (RuntimeException)
+    { return false; }
+
+private:
+    const Reference<XResourceId> mxResourceId;
+    const TaskPaneViewShell::PanelId mePaneId;
+};
+
+} // end of anonymous namespace.
+
+
+
+
+//===== TaskPanelFactory =======================================================
+
+TaskPanelFactory::TaskPanelFactory (
+    const ::com::sun::star::uno::Reference<com::sun::star::uno::XComponentContext>& rxContext)
+    : TaskPanelFactoryInterfaceBase(m_aMutex),
+      mpViewShellBase(NULL)
+{
+    (void)rxContext;
+}
+
+
+
+
+TaskPanelFactory::~TaskPanelFactory (void)
+{
+}
+
+
+
+
+void SAL_CALL TaskPanelFactory::disposing (void)
+{
+}
+
+
+
+
+//===== XInitialization =======================================================
+
+void SAL_CALL TaskPanelFactory::initialize(
+    const ::com::sun::star::uno::Sequence<com::sun::star::uno::Any>& aArguments)
+    throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException)
+{
+    if (aArguments.getLength() > 0)
+    {
+        try
+        {
+            // Get the XController from the first argument.
+            Reference<frame::XController> xController (aArguments[0], UNO_QUERY_THROW);
+
+            // Tunnel through the controller to obtain access to the ViewShellBase.
+            try
+            {
+                Reference<lang::XUnoTunnel> xTunnel (xController, UNO_QUERY_THROW);
+                DrawController* pController
+                    = reinterpret_cast<DrawController*>(
+                        sal::static_int_cast<sal_uIntPtr>(
+                            xTunnel->getSomething(DrawController::getUnoTunnelId())));
+                if (pController != NULL)
+                    mpViewShellBase = pController->GetViewShellBase();
+
+            }
+            catch(RuntimeException&)
+            {}
+
+
+            Reference<XControllerManager> xCM (xController, UNO_QUERY_THROW);
+            Reference<XConfigurationController> xCC (
+                xCM->getConfigurationController(), UNO_QUERY_THROW);
+            xCC->addResourceFactory(FrameworkHelper::msMasterPagesTaskPanelURL, this);
+            xCC->addResourceFactory(FrameworkHelper::msLayoutTaskPanelURL, this);
+            xCC->addResourceFactory(FrameworkHelper::msCustomAnimationTaskPanelURL, this);
+            xCC->addResourceFactory(FrameworkHelper::msSlideTransitionTaskPanelURL, this);
+        }
+        catch (RuntimeException&)
+        {
+        }
+    }
+}
+
+
+//===== XResourceController ===================================================
+
+Reference<XResource> SAL_CALL TaskPanelFactory::createResource (
+    const Reference<XResourceId>& rxResourceId)
+    throw (RuntimeException)
+{
+    Reference<XResource> xResource;
+
+    if ( ! rxResourceId.is())
+        return NULL;
+
+    OUString sResourceURL (rxResourceId->getResourceURL());
+
+    if (sResourceURL.match(FrameworkHelper::msTaskPanelURLPrefix))
+    {
+        TaskPaneViewShell::PanelId ePaneId (TaskPaneViewShell::PID_UNKNOWN);
+
+        if (sResourceURL.equals(FrameworkHelper::msMasterPagesTaskPanelURL))
+        {
+            ePaneId = TaskPaneViewShell::PID_MASTER_PAGES;
+        }
+        else if (sResourceURL.equals(FrameworkHelper::msLayoutTaskPanelURL))
+        {
+            ePaneId = TaskPaneViewShell::PID_LAYOUT;
+        }
+        else if (sResourceURL.equals(FrameworkHelper::msCustomAnimationTaskPanelURL))
+        {
+            ePaneId = TaskPaneViewShell::PID_CUSTOM_ANIMATION;
+        }
+        else if (sResourceURL.equals(FrameworkHelper::msSlideTransitionTaskPanelURL))
+        {
+            ePaneId = TaskPaneViewShell::PID_SLIDE_TRANSITION;
+        }
+
+        if (ePaneId!=TaskPaneViewShell::PID_UNKNOWN && mpViewShellBase!=NULL)
+        {
+            toolpanel::TaskPaneViewShell* pTaskPane
+                = dynamic_cast<toolpanel::TaskPaneViewShell*>(
+                    FrameworkHelper::Instance(*mpViewShellBase)
+                        ->GetViewShell(FrameworkHelper::msRightPaneURL).get());
+            if (pTaskPane != NULL)
+            {
+                xResource = new TaskPanelResource(
+                    rxResourceId,
+                    ePaneId);
+                pTaskPane->ShowPanel(ePaneId);
+                pTaskPane->ExpandPanel(ePaneId);
+            }
+        }
+    }
+
+    return xResource;
+}
+
+
+
+
+void SAL_CALL TaskPanelFactory::releaseResource (
+    const Reference<XResource>& rxResource)
+    throw (RuntimeException)
+{
+    toolpanel::TaskPaneViewShell* pTaskPane
+        = dynamic_cast<toolpanel::TaskPaneViewShell*>(
+            FrameworkHelper::Instance(*mpViewShellBase)
+                ->GetViewShell(FrameworkHelper::msRightPaneURL).get());
+
+    rtl::Reference<TaskPanelResource> pResource = dynamic_cast<TaskPanelResource*>(
+        rxResource.get());
+
+    if (pTaskPane != NULL && pResource.is())
+        pTaskPane->CollapsePanel(pResource->GetPaneId());
+
+    Reference<XComponent> xComponent (rxResource, UNO_QUERY);
+    if (xComponent.is())
+        xComponent->dispose();
+}
+
+
+
+
+//=============================================================================
+
+void TaskPanelFactory::ThrowIfDisposed (void) const
+    throw (lang::DisposedException)
+{
+    if (rBHelper.bDisposed || rBHelper.bInDispose)
+    {
+        throw lang::DisposedException (
+            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                "TaskPanelFactory object has already been disposed")),
+            const_cast<uno::XWeak*>(static_cast<const uno::XWeak*>(this)));
+    }
+}
+
+
+
+
+//===== ToolPanelResource =====================================================
+
+namespace {
+
+TaskPanelResource::TaskPanelResource (
+    const Reference<XResourceId>& rxResourceId,
+    const TaskPaneViewShell::PanelId ePaneId)
+    : TaskPanelResourceInterfaceBase(m_aMutex),
+      mxResourceId(rxResourceId),
+      mePaneId(ePaneId)
+{
+}
+
+
+
+
+TaskPanelResource::~TaskPanelResource (void)
+{
+}
+
+
+
+
+void SAL_CALL TaskPanelResource::disposing (void)
+{
+}
+
+
+
+
+const TaskPaneViewShell::PanelId TaskPanelResource::GetPaneId (void) const
+{
+    return mePaneId;
+}
+
+
+
+
+Reference<XResourceId> SAL_CALL TaskPanelResource::getResourceId (void)
+    throw (css::uno::RuntimeException)
+{
+    return mxResourceId;
+}
+
+} // end of anonymous namespace
+
+} } // end of namespace sd::framework
