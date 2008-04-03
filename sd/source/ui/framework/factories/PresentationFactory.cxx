d8b1c1ca589c141cfc983d96cf88eb3b3ce9378e
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:34:39 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2008/01/09 18:28:23 cl 1.1.2.6: #i15900# slideshow api consolidation 2007/11/15 12:48:00 af 1.1.2.5: #i18486# Added isAnchorOnly() method. 2007/10/26 12:23:07 af 1.1.2.4: #i18486# Removed dependency from one static OUString to another in another module. 2007/09/11 11:07:09 af 1.1.2.3: #i18486# Deactivation of the presentation marker stops the slide show. 2007/07/25 12:14:55 af 1.1.2.2: #i18486# Added documentation. 2007/07/10 15:05:27 af 1.1.2.1: #i18486# Moved here from ..\module

---
+++ sd/source/ui/framework/factories/PresentationFactory.cxx
@@ -0,0 +1,329 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: PresentationFactory.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:34:39 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "framework/PresentationFactory.hxx"
+
+#include "framework/FrameworkHelper.hxx"
+#include "DrawController.hxx"
+#include "ViewShellBase.hxx"
+#include <com/sun/star/drawing/framework/XControllerManager.hpp>
+#include <cppuhelper/compbase1.hxx>
+#include "slideshow.hxx"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+using ::sd::framework::FrameworkHelper;
+
+
+namespace sd { namespace framework {
+
+namespace {
+
+typedef ::cppu::WeakComponentImplHelper1 <lang::XInitialization> PresentationFactoryProviderInterfaceBase;
+
+class PresentationFactoryProvider
+    : protected MutexOwner,
+      public PresentationFactoryProviderInterfaceBase
+{
+public:
+    PresentationFactoryProvider (const Reference<XComponentContext>& rxContext);
+    virtual ~PresentationFactoryProvider (void);
+
+    virtual void SAL_CALL disposing (void);
+
+    // XInitialization
+
+    virtual void SAL_CALL initialize(
+        const ::com::sun::star::uno::Sequence<com::sun::star::uno::Any>& aArguments)
+        throw (::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+};
+
+
+
+
+typedef ::cppu::WeakComponentImplHelper1 <XView> PresentationViewInterfaceBase;
+
+/** The PresentationView is not an actual view, it is a marker whose
+    existence in a configuration indicates that a slideshow is running
+    (in another application window).
+*/
+class PresentationView
+    : protected MutexOwner,
+      public PresentationViewInterfaceBase
+{
+public:
+    PresentationView (const Reference<XResourceId>& rxViewId)
+        : PresentationViewInterfaceBase(maMutex),mxResourceId(rxViewId) {};
+    virtual ~PresentationView (void) {};
+
+    // XView
+
+    virtual Reference<XResourceId> SAL_CALL getResourceId (void) throw (RuntimeException)
+    { return mxResourceId; };
+
+    virtual sal_Bool SAL_CALL isAnchorOnly (void) throw (RuntimeException)
+    { return false; }
+
+
+private:
+    Reference<XResourceId> mxResourceId;
+};
+
+} // end of anonymous namespace.
+
+
+
+
+//===== PresentationFactoryProvider service ===================================
+
+Reference<XInterface> SAL_CALL PresentationFactoryProvider_createInstance (
+    const Reference<XComponentContext>& rxContext)
+{
+    return Reference<XInterface>(static_cast<XWeak*>(new PresentationFactoryProvider(rxContext)));
+}
+
+
+
+
+::rtl::OUString PresentationFactoryProvider_getImplementationName (void) throw(RuntimeException)
+{
+    return ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+        "com.sun.star.comp.Draw.framework.PresentationFactoryProvider"));
+}
+
+
+
+
+Sequence<rtl::OUString> SAL_CALL PresentationFactoryProvider_getSupportedServiceNames (void)
+    throw (RuntimeException)
+{
+    static const ::rtl::OUString sServiceName(RTL_CONSTASCII_USTRINGPARAM(
+        "com.sun.star.drawing.framework.PresentationFactoryProvider"));
+    return Sequence<rtl::OUString>(&sServiceName, 1);
+}
+
+
+
+
+//===== PresentationFactory ===================================================
+
+const ::rtl::OUString PresentationFactory::msPresentationViewURL(
+    OUString::createFromAscii("private:resource/view/Presentation"));
+
+
+PresentationFactory::PresentationFactory (
+    const Reference<frame::XController>& rxController)
+    : PresentationFactoryInterfaceBase(MutexOwner::maMutex),
+      mxConfigurationController(),
+      mxController(rxController)
+{
+    try
+    {
+        // Get the XController from the first argument.
+        Reference<XControllerManager> xControllerManager(rxController, UNO_QUERY_THROW);
+        mxConfigurationController = xControllerManager->getConfigurationController();
+    }
+    catch (RuntimeException&)
+    {
+        OSL_ASSERT(false);
+    }
+}
+
+
+
+
+
+PresentationFactory::~PresentationFactory (void)
+{
+}
+
+
+
+
+void SAL_CALL PresentationFactory::disposing (void)
+{
+}
+
+
+
+
+//----- XViewFactory ----------------------------------------------------------
+
+Reference<XResource> SAL_CALL PresentationFactory::createResource (
+    const Reference<XResourceId>& rxViewId)
+    throw (RuntimeException)
+{
+    ThrowIfDisposed();
+
+    if (rxViewId.is())
+        if ( ! rxViewId->hasAnchor() && rxViewId->getResourceURL().equals(msPresentationViewURL))
+            return new PresentationView(rxViewId);
+
+    return Reference<XResource>();
+}
+
+
+
+
+void SAL_CALL PresentationFactory::releaseResource (
+    const Reference<XResource>& rxView)
+    throw (RuntimeException)
+{
+    ThrowIfDisposed();
+    (void)rxView;
+
+    Reference<lang::XUnoTunnel> xTunnel (mxController, UNO_QUERY);
+    if (xTunnel.is())
+    {
+        ::sd::DrawController* pController = reinterpret_cast<sd::DrawController*>(
+            xTunnel->getSomething(sd::DrawController::getUnoTunnelId()));
+        if (pController != NULL)
+        {
+            ViewShellBase* pBase = pController->GetViewShellBase();
+            if (pBase != NULL)
+                SlideShow::Stop( *pBase );
+        }
+    }
+}
+
+
+
+
+//===== XConfigurationChangeListener ==========================================
+
+void SAL_CALL PresentationFactory::notifyConfigurationChange (
+    const ConfigurationChangeEvent& rEvent)
+    throw (RuntimeException)
+{
+    (void)rEvent;
+}
+
+
+
+
+//===== lang::XEventListener ==================================================
+
+void SAL_CALL PresentationFactory::disposing (
+    const lang::EventObject& rEventObject)
+    throw (RuntimeException)
+{
+    (void)rEventObject;
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------
+
+void PresentationFactory::ThrowIfDisposed (void) const
+    throw (lang::DisposedException)
+{
+    if (rBHelper.bDisposed || rBHelper.bInDispose)
+    {
+        throw lang::DisposedException (
+            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                "PresentationFactory object has already been disposed")),
+            const_cast<uno::XWeak*>(static_cast<const uno::XWeak*>(this)));
+    }
+}
+
+
+
+namespace {
+
+//===== PresentationFactoryProvider ===========================================
+
+PresentationFactoryProvider::PresentationFactoryProvider (
+    const Reference<XComponentContext>& rxContext)
+    : PresentationFactoryProviderInterfaceBase(maMutex)
+{
+    (void)rxContext;
+}
+
+
+
+
+PresentationFactoryProvider::~PresentationFactoryProvider (void)
+{
+}
+
+
+
+
+void PresentationFactoryProvider::disposing (void)
+{
+}
+
+
+
+
+// XInitialization
+
+void SAL_CALL PresentationFactoryProvider::initialize(
+    const Sequence<Any>& aArguments)
+    throw (Exception, RuntimeException)
+{
+    if (aArguments.getLength() > 0)
+    {
+        try
+        {
+            // Get the XController from the first argument.
+            Reference<frame::XController> xController (aArguments[0], UNO_QUERY_THROW);
+            Reference<XControllerManager> xCM (xController, UNO_QUERY_THROW);
+            Reference<XConfigurationController> xCC (xCM->getConfigurationController());
+            if (xCC.is())
+                xCC->addResourceFactory(
+                    PresentationFactory::msPresentationViewURL,
+                    new PresentationFactory(xController));
+        }
+        catch (RuntimeException&)
+        {
+            OSL_ASSERT(false);
+        }
+    }
+}
+
+
+
+} // end of anonymous namespace.
+
+
+} } // end of namespace sd::framework
