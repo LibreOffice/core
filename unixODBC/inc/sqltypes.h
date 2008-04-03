efc83dfc17f5559e59e02f291f2778b62942599e
Kurt Zenker <kz@openoffice.org>
2008-04-03 15:44:11 +0000

    INTEGRATION: CWS s390xport01 (1.2.46); FILE MERGED 2008/03/26 12:42:03 cmc 1.2.46.1: #i87464# add support for s390x

--- unixODBC/inc/sqltypes.h
+++ unixODBC/inc/sqltypes.h
@@ -31,7 +31,7 @@ extern "C" {
 #endif
 
 #ifndef SIZEOF_LONG
-# if defined(__alpha__) || defined(__x86_64__) || defined(__ia64__)
+# if defined(__alpha__) || defined(__x86_64__) || defined(__ia64__) || defined(__s390x__)
 # define SIZEOF_LONG        8
 #else
 # define SIZEOF_LONG        4
