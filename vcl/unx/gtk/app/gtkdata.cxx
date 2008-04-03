a8b450ce9773e604c77eadde5fc7b6089eb089d1
Kurt Zenker <kz@openoffice.org>
2008-04-03 16:06:22 +0000

    INTEGRATION: CWS vcl87 (1.36.16); FILE MERGED 2008/03/07 16:59:00 pl 1.36.16.1: #i76650# RandR support for screen size changes

--- vcl/unx/gtk/app/gtkdata.cxx
+++ vcl/unx/gtk/app/gtkdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: gtkdata.cxx,v $
  *
- *  $Revision: 1.36 $
+ *  $Revision: 1.37 $
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 16:20:56 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 17:06:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -191,6 +191,7 @@ GdkFilterReturn GtkSalDisplay::filterGdkEvent( GdkXEvent* sys_event,
             }
         }
         X11SalObject::Dispatch( pEvent );
+        pDisplay->processRandREvent( pEvent );
     }
 
     return aFilterReturn;
