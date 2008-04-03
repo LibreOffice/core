55e28e8d42c648f9670ce3b58f9e71f1be58c03c
Kurt Zenker <kz@openoffice.org>
2008-04-03 15:46:27 +0000

    INTEGRATION: CWS s390xport01 (1.1.2); FILE ADDED 2008/03/26 12:56:28 cmc 1.1.2.1: #i87464# add support for s390x

---
+++ solenv/inc/unxlngs390x.mk
@@ -0,0 +1,39 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: unxlngs390x.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2008-04-03 16:46:27 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+# mk file for linux s390x, inherit from s390 and add some flags
+
+.INCLUDE : unxlngs3904.mk
+
+CDEFS+=-DS390X
