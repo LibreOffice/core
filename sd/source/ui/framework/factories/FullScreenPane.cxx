9f789cbf7aefb29fac2c0f1507abd6195f487afe
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:33:44 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2008/02/28 16:26:09 af 1.1.2.10: #i18486# FullScreenPane creates a new top-level window. 2008/02/26 10:46:17 af 1.1.2.9: #i18486# Creating canvas directly without CanvasFactory. 2008/01/29 14:33:15 af 1.1.2.8: #i18486# Be content with bitmap canvas instead of sprite canvas. 2008/01/21 10:50:18 af 1.1.2.7: #i18486# Added support for arguments of the resource URL. 2007/12/12 13:03:07 af 1.1.2.6: #i18486# Added support for shared canvas via the PresenterCanvasFactory. 2007/11/15 12:51:49 af 1.1.2.5: #i18486# Added mnSavedBorderStyle member. 2007/09/25 07:40:04 af 1.1.2.4: #i18486# Added disposing(). 2007/07/20 07:36:22 af 1.1.2.3: #i18486# Added GetWorkWindow() method. 2007/07/18 09:35:34 af 1.1.2.2: #i18486# Hide layout manager. 2007/07/10 15:05:27 af 1.1.2.1: #i18486# Moved here from ..\module

---
+++ sd/source/ui/framework/factories/FullScreenPane.cxx
@@ -0,0 +1,188 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: FullScreenPane.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:33:44 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "FullScreenPane.hxx"
+#include "ViewShellBase.hxx"
+#include <cppcanvas/vclfactory.hxx>
+#include <sfx2/dispatch.hxx>
+#include <sfx2/topfrm.hxx>
+#include <toolkit/helper/vclunohelper.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/frame/XLayoutManager.hpp>
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+#include <com/sun/star/util/URL.hpp>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+using ::rtl::OUString;
+
+namespace sd { namespace framework {
+
+FullScreenPane::FullScreenPane (
+    const Reference<XComponentContext>& rxComponentContext,
+    const Reference<XResourceId>& rxPaneId,
+    const ::Window* pViewShellWindow)
+    : FrameWindowPane(rxPaneId,NULL),
+      mxComponentContext(rxComponentContext),
+      mpWorkWindow(new WorkWindow(NULL, 0))
+{
+    if ( ! rxPaneId.is())
+        throw lang::IllegalArgumentException();
+
+    sal_Int32 nScreenNumber = 1;
+    ExtractArguments(rxPaneId, nScreenNumber);
+
+    if (mpWorkWindow.get() == NULL)
+        return;
+
+    // Create a new top-leve window that is displayed full screen.
+    mpWorkWindow->ShowFullScreenMode(TRUE, nScreenNumber);
+    mpWorkWindow->SetMenuBarMode(MENUBAR_MODE_HIDE);
+    mpWorkWindow->SetBorderStyle(WINDOW_BORDER_REMOVEBORDER);
+    mpWorkWindow->SetBackground(Wallpaper());
+    mpWorkWindow->Show();
+
+    // Set title and icon of the new window to those of the current window
+    // of the view shell.
+    if (pViewShellWindow != NULL)
+    {
+        const SystemWindow* pSystemWindow = pViewShellWindow->GetSystemWindow();
+        mpWorkWindow->SetText(pSystemWindow->GetText());
+        mpWorkWindow->SetIcon(pSystemWindow->GetIcon());
+    }
+
+    // For some reason the VCL canvas can not paint into a WorkWindow.
+    // Therefore a child window is created that covers the WorkWindow
+    // completely.
+    mpWindow = new ::Window(mpWorkWindow.get());
+    mpWindow->SetPosSizePixel(Point(0,0), mpWorkWindow->GetSizePixel());
+    mpWindow->Show();
+    mpWindow->SetBackground(Wallpaper());
+    mxWindow = VCLUnoHelper::GetInterface(mpWindow);
+
+    // Create the canvas.
+    mxCanvas = CreateCanvas();
+
+    mpWindow->GrabFocus();
+}
+
+
+
+
+FullScreenPane::~FullScreenPane (void) throw()
+{
+}
+
+
+
+
+void SAL_CALL FullScreenPane::disposing (void)
+{
+    // We have created the window pointed to by mpWindow, we delete it.
+    if (mpWindow != NULL)
+    {
+        delete mpWindow;
+    }
+
+    mpWorkWindow.reset();
+
+    FrameWindowPane::disposing();
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+
+Reference<rendering::XCanvas> FullScreenPane::CreateCanvas (void)
+    throw (RuntimeException)
+{
+    ::Window* pWindow = VCLUnoHelper::GetWindow(mxWindow);
+    if (pWindow != NULL)
+    {
+        Sequence<Any> aArg (5);
+
+        // common: first any is VCL pointer to window (for VCL canvas)
+        aArg[0] = makeAny(reinterpret_cast<sal_Int64>(pWindow));
+        aArg[1] = Any();
+        aArg[2] = makeAny(::com::sun::star::awt::Rectangle());
+        aArg[3] = makeAny(sal_False);
+        aArg[4] = makeAny(mxWindow);
+
+        Reference<lang::XMultiServiceFactory> xFactory (
+            mxComponentContext->getServiceManager(), UNO_QUERY_THROW);
+        return Reference<rendering::XCanvas>(
+            xFactory->createInstanceWithArguments(
+                OUString::createFromAscii("com.sun.star.rendering.VCLCanvas"),
+                aArg),
+            UNO_QUERY);
+    }
+    else
+        throw RuntimeException();
+}
+
+
+
+
+void FullScreenPane::ExtractArguments (
+    const Reference<XResourceId>& rxPaneId,
+    sal_Int32& rnScreenNumberReturnValue)
+{
+    // Extract arguments from the resource URL.
+    const util::URL aURL = rxPaneId->getFullResourceURL();
+    sal_Int32 nIndex = 0;
+    while (nIndex >= 0)
+    {
+        const OUString aToken = aURL.Arguments.getToken(0, '&', nIndex);
+        if (aToken.getLength() > 0)
+        {
+            // Split at the first '='.
+            const sal_Int32 nAssign = aToken.indexOf('=');
+            const OUString sKey = aToken.copy(0, nAssign);
+            const OUString sValue = aToken.copy(nAssign+1);
+
+            if (sKey.compareToAscii("ScreenNumber") == 0)
+            {
+                rnScreenNumberReturnValue = sValue.toInt32();
+            }
+        }
+    }
+}
+
+
+} } // end of namespace sd::framework
