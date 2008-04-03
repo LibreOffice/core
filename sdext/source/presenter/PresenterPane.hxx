0a7be25f092df96c48304b1fc37eeb48d47e7a81
Kurt Zenker <kz@openoffice.org>
2008-04-03 15:00:18 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2008/02/26 15:09:16 af 1.1.2.2: #i18486# Factored out base class PresenterPaneBase. 2008/01/21 15:43:34 af 1.1.2.1: #i18486# Initial revision.

---
+++ sdext/source/presenter/PresenterPane.hxx
@@ -0,0 +1,115 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: PresenterPane.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 16:00:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SD_PRESENTER_PRESENTER_PANE_HXX
+#define SD_PRESENTER_PRESENTER_PANE_HXX
+
+#include "PresenterPaneBase.hxx"
+#include <com/sun/star/awt/XMouseListener.hpp>
+#include <com/sun/star/awt/XMouseMotionListener.hpp>
+#include <com/sun/star/awt/XWindowListener.hpp>
+#include <com/sun/star/container/XChild.hpp>
+#include <com/sun/star/drawing/XPresenterHelper.hpp>
+#include <com/sun/star/drawing/framework/XPane.hpp>
+#include <com/sun/star/drawing/framework/XPaneBorderPainter.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/rendering/XCanvas.hpp>
+#include <rtl/ref.hxx>
+#include <boost/noncopyable.hpp>
+
+namespace css = ::com::sun::star;
+
+
+namespace sdext { namespace presenter {
+
+/** Pane used by the presenter screen.  Pane objects are stored in the
+    PresenterPaneContainer.  Sizes and positions are controlled
+    by the PresenterWindowManager.  Interactive positioning and resizing is
+    managed by the PresenterPaneBorderManager.  Borders around panes are
+    painted by the PresenterPaneBorderPainter.
+*/
+class PresenterPane : public PresenterPaneBase
+{
+public:
+    PresenterPane (const css::uno::Reference<css::uno::XComponentContext>& rxContext);
+    virtual ~PresenterPane (void);
+
+    static ::rtl::OUString getImplementationName_static (void);
+    static css::uno::Sequence< ::rtl::OUString > getSupportedServiceNames_static (void);
+    static css::uno::Reference<css::uno::XInterface> Create(
+        const css::uno::Reference<css::uno::XComponentContext>& rxContext)
+        SAL_THROW((css::uno::Exception));
+
+
+    // XPane
+
+    css::uno::Reference<css::awt::XWindow> SAL_CALL getWindow (void)
+        throw (css::uno::RuntimeException);
+
+    css::uno::Reference<css::rendering::XCanvas> SAL_CALL getCanvas (void)
+        throw (css::uno::RuntimeException);
+
+
+    // XWindowListener
+
+    virtual void SAL_CALL windowResized (const css::awt::WindowEvent& rEvent)
+        throw (css::uno::RuntimeException);
+
+    virtual void SAL_CALL windowMoved (const css::awt::WindowEvent& rEvent)
+        throw (css::uno::RuntimeException);
+
+    virtual void SAL_CALL windowShown (const css::lang::EventObject& rEvent)
+        throw (css::uno::RuntimeException);
+
+    virtual void SAL_CALL windowHidden (const css::lang::EventObject& rEvent)
+        throw (css::uno::RuntimeException);
+
+
+    // XPaintListener
+
+    virtual void SAL_CALL windowPaint (const css::awt::PaintEvent& rEvent)
+        throw (css::uno::RuntimeException);
+
+
+private:
+    virtual void CreateCanvases (
+        const css::uno::Reference<css::awt::XWindow>& rxParentWindow,
+        const css::uno::Reference<css::rendering::XSpriteCanvas>& rxParentCanvas);
+};
+
+} } // end of namespace ::sd::presenter
+
+#endif
