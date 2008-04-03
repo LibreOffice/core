178b0f79a4fdb982e4e9310cce0c1c4bf7b9ea7b
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:25:05 +0000

    INTEGRATION: CWS presenterview (1.3.388); FILE MERGED 2007/06/11 12:12:13 af 1.3.388.1: #i18486# Fixed handling of view shell changes.

--- sd/source/ui/animations/AnimationSchemesPane.hxx
+++ sd/source/ui/animations/AnimationSchemesPane.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: AnimationSchemesPane.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:31:42 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:25:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -127,6 +127,7 @@ private:
 
     bool         mbHasSelection;
     bool         mbUpdatingControls;
+    bool         mbIsMainViewChangePending;
 };
 
 } //  namespace sd
