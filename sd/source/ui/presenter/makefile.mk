7c03751838df193ccc8d53ab23329beb19d5b06c
Kurt Zenker <kz@openoffice.org>
2008-04-03 13:09:39 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2008/02/26 12:55:24 af 1.1.2.6: #i18486# Removed PresenterCanvasFactory.  Added CanvasUpdateRequester and PresenterPreviewCache. 2008/01/21 13:33:55 af 1.1.2.5: #i18486# Moved PresenterCanvas, PresenterTheme, PresenterPaneDecoration, SlideShowView to sdext.  Created PresenterHelper and integrated PresenterWorkaroundService into that. 2007/12/12 13:33:30 af 1.1.2.4: #i18486# Added PresenterCanvas, PresenterCanvasFactory, PresenterTextView. 2007/11/15 13:20:53 af 1.1.2.3: #i18486# Added PresenterWorkaroundService. 2007/09/11 11:56:02 af 1.1.2.2: #i18486# Added PresenterTheme. 2007/07/12 12:51:41 af 1.1.2.1: #i18486# Initial revision.

---
+++ sd/source/ui/presenter/makefile.mk
@@ -0,0 +1,64 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: kz $ $Date: 2008-04-03 14:09:39 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PROJECTPCH=sd
+PROJECTPCHSOURCE=$(PRJ)$/util$/sd
+PRJNAME=sd
+TARGET=presenter
+ENABLE_EXCEPTIONS=TRUE
+AUTOSEG=true
+PRJINC=.. ..$/slidesorter
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+# --- Files --------------------------------------------------------
+
+SLOFILES =      							\
+    $(SLO)$/CanvasUpdateRequester.obj		\
+    $(SLO)$/PresenterCanvas.obj				\
+    $(SLO)$/PresenterHelper.obj				\
+    $(SLO)$/PresenterPreviewCache.obj		\
+    $(SLO)$/PresenterTextView.obj			\
+    $(SLO)$/SlideRenderer.obj
+
+# --- Tagets -------------------------------------------------------
+
+.INCLUDE :  target.mk
+
