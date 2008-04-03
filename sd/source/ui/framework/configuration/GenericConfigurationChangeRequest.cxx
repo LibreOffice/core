f91d218475314b4013b15cdfe0b4008938c9f4ef
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:30:25 +0000

    INTEGRATION: CWS presenterview (1.2.22); FILE MERGED 2007/07/24 10:58:28 af 1.2.22.1: #i18486# Fixed resync problems.

--- sd/source/ui/framework/configuration/GenericConfigurationChangeRequest.cxx
+++ sd/source/ui/framework/configuration/GenericConfigurationChangeRequest.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: GenericConfigurationChangeRequest.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-03 15:47:04 $
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:30:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -49,7 +49,7 @@ namespace sd { namespace framework {
 
 GenericConfigurationChangeRequest::GenericConfigurationChangeRequest (
     const Reference<XResourceId>& rxResourceId,
-    Mode eMode) throw(::com::sun::star::lang::IllegalArgumentException)
+    const Mode eMode) throw(::com::sun::star::lang::IllegalArgumentException)
     : GenericConfigurationChangeRequestInterfaceBase(MutexOwner::maMutex),
       mxResourceId(rxResourceId),
       meMode(eMode)
