a573f260457354abe75642eac61407d8fbd68c2b
Kurt Zenker <kz@openoffice.org>
2008-04-03 12:35:27 +0000

    INTEGRATION: CWS presenterview (1.1.2); FILE ADDED 2007/11/15 12:50:25 af 1.1.2.3: #i18486# Added isAnchorOnly() method. 2007/09/25 07:41:27 af 1.1.2.2: #i18486# Using VCLUnoHelper instead of tunnel to obtain Window pointer for XWindow reference. 2007/07/10 15:05:27 af 1.1.2.1: #i18486# Moved here from ..\module

---
+++ sd/source/ui/framework/factories/ViewShellWrapper.cxx
@@ -0,0 +1,273 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ViewShellWrapper.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: kz $ $Date: 2008-04-03 13:35:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include "precompiled_sd.hxx"
+
+#include "framework/ViewShellWrapper.hxx"
+#include "framework/Pane.hxx"
+#include "ViewShell.hxx"
+#include "Window.hxx"
+
+#include <com/sun/star/drawing/framework/XPane.hpp>
+
+#include <rtl/uuid.h>
+#include <toolkit/helper/vclunohelper.hxx>
+#include <vcl/svapp.hxx>
+#include <vos/mutex.hxx>
+
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::drawing::framework;
+
+using ::rtl::OUString;
+
+namespace sd { namespace framework {
+
+ViewShellWrapper::ViewShellWrapper (
+    ::boost::shared_ptr<ViewShell> pViewShell,
+    const Reference<XResourceId>& rxViewId,
+    const Reference<awt::XWindow>& rxWindow)
+    : ViewShellWrapperInterfaceBase(MutexOwner::maMutex),
+      mpViewShell(pViewShell),
+      mxViewId(rxViewId),
+      mxWindow(rxWindow)
+{
+    if (rxWindow.is())
+    {
+        rxWindow->addWindowListener(this);
+        if (pViewShell != NULL)
+        {
+            pViewShell->Resize();
+        }
+    }
+}
+
+
+
+
+ViewShellWrapper::~ViewShellWrapper (void)
+{
+}
+
+
+
+
+void SAL_CALL ViewShellWrapper::disposing (void)
+{
+    OSL_TRACE("disposing ViewShellWrapper %x", this);
+    Reference<awt::XWindow> xWindow (mxWindow);
+    if (xWindow.is())
+    {
+        OSL_TRACE("removing ViewShellWrapper %x from window listener at %x", this, mxWindow.get());
+        xWindow->removeWindowListener(this);
+    }
+
+    mpViewShell.reset();
+}
+
+
+
+
+::boost::shared_ptr<ViewShell> ViewShellWrapper::GetViewShell (void)
+{
+    return mpViewShell;
+}
+
+
+
+
+bool ViewShellWrapper::IsUnique (void)
+{
+    return m_refCount==1;
+}
+
+
+
+
+//----- XResource -------------------------------------------------------------
+
+Reference<XResourceId> SAL_CALL ViewShellWrapper::getResourceId (void)
+    throw (RuntimeException)
+{
+    return mxViewId;
+}
+
+
+
+
+sal_Bool SAL_CALL ViewShellWrapper::isAnchorOnly (void)
+    throw (RuntimeException)
+{
+    return false;
+}
+
+
+
+
+//----- XRelocatableResource --------------------------------------------------
+
+sal_Bool SAL_CALL ViewShellWrapper::relocateToAnchor (
+    const Reference<XResource>& xResource)
+    throw (RuntimeException)
+{
+    sal_Bool bResult (false);
+
+    Reference<XPane> xPane (xResource, UNO_QUERY);
+    if (xPane.is())
+    {
+        // Detach from the window of the old pane.
+        Reference<awt::XWindow> xWindow (mxWindow);
+        if (xWindow.is())
+            xWindow->removeWindowListener(this);
+        mxWindow = NULL;
+
+        if (mpViewShell.get() != NULL)
+        {
+            ::Window* pWindow = VCLUnoHelper::GetWindow(xPane->getWindow());
+            if (pWindow != NULL && mpViewShell->RelocateToParentWindow(pWindow))
+            {
+                bResult = sal_True;
+
+                // Attach to the window of the new pane.
+                xWindow = Reference<awt::XWindow>(xPane->getWindow(), UNO_QUERY);
+                if (xWindow.is())
+                {
+                    xWindow->addWindowListener(this);
+                    mpViewShell->Resize();
+                }
+            }
+        }
+    }
+
+    return bResult;
+}
+
+
+
+
+//----- XUnoTunnel ------------------------------------------------------------
+
+const Sequence<sal_Int8>& ViewShellWrapper::getUnoTunnelId (void)
+{
+    static Sequence<sal_Int8>* pSequence = NULL;
+    if (pSequence == NULL)
+    {
+        const ::vos::OGuard aSolarGuard (Application::GetSolarMutex());
+        if (pSequence == NULL)
+        {
+            static ::com::sun::star::uno::Sequence<sal_Int8> aSequence (16);
+            rtl_createUuid((sal_uInt8*)aSequence.getArray(), 0, sal_True);
+            pSequence = &aSequence;
+        }
+    }
+    return *pSequence;
+}
+
+
+
+
+sal_Int64 SAL_CALL ViewShellWrapper::getSomething (const Sequence<sal_Int8>& rId)
+    throw (RuntimeException)
+{
+    sal_Int64 nResult = 0;
+
+    if (rId.getLength() == 16
+        && rtl_compareMemory(getUnoTunnelId().getConstArray(), rId.getConstArray(), 16) == 0)
+    {
+        nResult = reinterpret_cast<sal_Int64>(this);
+    }
+
+    return nResult;
+}
+
+
+
+
+//===== awt::XWindowListener ==================================================
+
+void SAL_CALL ViewShellWrapper::windowResized (const awt::WindowEvent& rEvent)
+    throw (RuntimeException)
+{
+    (void)rEvent;
+    ViewShell* pViewShell (mpViewShell.get());
+    if (pViewShell != NULL)
+        pViewShell->Resize();
+}
+
+
+
+
+void SAL_CALL ViewShellWrapper::windowMoved (const awt::WindowEvent& rEvent)
+    throw (RuntimeException)
+{
+    (void)rEvent;
+}
+
+
+
+
+void SAL_CALL ViewShellWrapper::windowShown (const lang::EventObject& rEvent)
+    throw (RuntimeException)
+{
+    (void)rEvent;
+    ViewShell* pViewShell (mpViewShell.get());
+    if (pViewShell != NULL)
+        pViewShell->Resize();
+}
+
+
+
+
+void SAL_CALL ViewShellWrapper::windowHidden (const lang::EventObject& rEvent)
+    throw (RuntimeException)
+{
+    (void)rEvent;
+}
+
+
+
+
+//===== XEventListener ========================================================
+
+void SAL_CALL ViewShellWrapper::disposing (const lang::EventObject& rEvent)
+    throw (RuntimeException)
+{
+    if (rEvent.Source == mxWindow)
+        mxWindow = NULL;
+}
+
+
+} } // end of namespace sd::framework
