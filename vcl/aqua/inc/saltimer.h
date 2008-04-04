8dd92b3eb5151c99fb0e864961552a97d0c535b1
Kurt Zenker <kz@openoffice.org>
2008-04-04 09:57:17 +0000

    INTEGRATION: CWS aquavcl06 (1.6.64); FILE MERGED 2008/03/18 18:25:41 pl 1.6.64.1: #i86581# timer recursion protection is not necessary on MacOS since timers get called on Yield only anyway. Moreover it was harmful since it didn not take yield levels into account

--- vcl/aqua/inc/saltimer.h
+++ vcl/aqua/inc/saltimer.h
@@ -4,9 +4,9 @@
 *
 *  $RCSfile: saltimer.h,v $
 *
-*  $Revision: 1.6 $
+*  $Revision: 1.7 $
 *
-*  last change: $Author: ihi $ $Date: 2008-01-14 16:14:42 $
+*  last change: $Author: kz $ $Date: 2008-04-04 10:57:17 $
 *
 *  The Contents of this file are made available subject to
 *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,6 @@ class AquaSalTimer : public SalTimer
 
     static NSTimer* pRunningTimer;
     static bool bDispatchTimer;
-    static bool bTimerInDispatch;
 };
 
 #endif
