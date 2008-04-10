76abfad05d6cfe381e01edfdf7a4b2037adf6131
Rüdiger Timm <rt@openoffice.org>
2008-04-10 12:31:01 +0000

    INTEGRATION: CWS changefileheader (1.2.342); FILE MERGED 2008/04/01 12:54:00 thb 1.2.342.2: #i85898# Stripping all external header guards 2008/03/31 16:53:49 rt 1.2.342.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/docnode/finalthreadmanager.cxx
+++ sw/source/core/docnode/finalthreadmanager.cxx
@@ -1,57 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: finalthreadmanager.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2007-07-18 13:30:42 $
+ * $RCSfile: finalthreadmanager.cxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_sw.hxx"
-
-#ifndef _FINALTHREADMANAGER_HXX
 #include <finalthreadmanager.hxx>
-#endif
 
 #ifndef _OSL_THREAD_HXX_
 #include <osl/thread.hxx>
 #endif
-#ifndef _ERRHDL_HXX
 #include <errhdl.hxx>
-#endif
-
-#ifndef _PAUSETHREADSTARTING_HXX
 #include <pausethreadstarting.hxx>
-#endif
-
-#ifndef _SWTHREADJOINER_HXX
 #include <swthreadjoiner.hxx>
-#endif
 
 #include <com/sun/star/frame/XDesktop.hpp>
 #include <rtl/ustring.hxx>
