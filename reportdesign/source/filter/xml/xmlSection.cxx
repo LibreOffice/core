0a5b2265d8e8c70310a400764eaeb4b3cb6f566c
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:44:29 +0000

    INTEGRATION: CWS changefileheader (1.5.56); FILE MERGED 2008/04/01 15:23:39 thb 1.5.56.3: #i85898# Stripping all external header guards 2008/04/01 12:33:11 thb 1.5.56.2: #i85898# Stripping all external header guards 2008/03/31 13:32:11 rt 1.5.56.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/filter/xml/xmlSection.cxx
+++ reportdesign/source/filter/xml/xmlSection.cxx
@@ -1,87 +1,55 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlSection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-20 19:04:03 $
+ * $RCSfile: xmlSection.cxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPT_XMLSECTION_HXX
 #include "xmlSection.hxx"
-#endif
-#ifndef RPT_XMLFILTER_HXX
 #include "xmlfilter.hxx"
-#endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
 #include <xmloff/xmltoken.hxx>
-#endif
-#ifndef _XMLOFF_XMLNMSPE_HXX
 #include <xmloff/xmlnmspe.hxx>
-#endif
-#ifndef _XMLOFF_NMSPMAP_HXX
 #include <xmloff/nmspmap.hxx>
-#endif
-#ifndef _XMLOFF_XMLUCONV_HXX
 #include <xmloff/xmluconv.hxx>
-#endif
-#ifndef RPT_XMLHELPER_HXX
 #include "xmlHelper.hxx"
-#endif
-#ifndef RPT_XMLENUMS_HXX
 #include "xmlEnums.hxx"
-#endif
 #ifndef RPT_XMLCOLUMN_HXX
 #include "xmlColumn.hxx"
 #endif
 #ifndef RPT_XMLCondPrtExpr_HXX
 #include "xmlCondPrtExpr.hxx"
 #endif
-#ifndef RPT_XMLSTYLEIMPORT_HXX
 #include "xmlStyleImport.hxx"
-#endif
-#ifndef _CONNECTIVITY_DBTOOLS_HXX_
 #include <connectivity/dbtools.hxx>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef _COM_SUN_STAR_REPORT_REPORTPRINTOPTION_HPP_
 #include <com/sun/star/report/ReportPrintOption.hpp>
-#endif
 #ifndef REPORTDESIGN_SHARED_XMLSTRINGS_HRC
 #include "xmlstrings.hrc"
 #endif
-#ifndef RPT_XMLTABLE_HXX
 #include "xmlTable.hxx"
-#endif
 
 
 namespace rptxml
