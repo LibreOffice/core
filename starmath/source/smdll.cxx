4c50d15430295a091daf86d59179a41e74cd77e7
Rüdiger Timm <rt@openoffice.org>
2008-04-10 11:49:24 +0000

    INTEGRATION: CWS changefileheader (1.12.164); FILE MERGED 2008/04/01 15:41:59 thb 1.12.164.3: #i85898# Stripping all external header guards 2008/04/01 12:41:40 thb 1.12.164.2: #i85898# Stripping all external header guards 2008/03/31 16:29:46 rt 1.12.164.1: #i87441# Change license header to LPGL v3.

--- starmath/source/smdll.cxx
+++ starmath/source/smdll.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: smdll.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 07:55:08 $
+ * $RCSfile: smdll.cxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,46 +32,24 @@
 #include "precompiled_starmath.hxx"
 
 
-#ifndef _SOT_FACTORY_HXX //autogen
 #include <sot/factory.hxx>
-#endif
 #ifndef _SVX_SVXIDS_HRC //autogen
 #include <svx/svxids.hrc>
 #endif
-#ifndef _SVX_MODCTRL_HXX //autogen
 #include <svx/modctrl.hxx>
-#endif
-#ifndef _SVX_ZOOMCTRL_HXX //autogen
 #include <svx/zoomctrl.hxx>
-#endif
-#ifndef _SFX_OBJFAC_HXX
 #include <sfx2/docfac.hxx>
-#endif
-#ifndef _SVX_LBOXCTRL_HXX_
 #include <svx/lboxctrl.hxx>
-#endif
-#ifndef _SFXDOCFILE_HXX //autogen
 #include <sfx2/docfile.hxx>
-#endif
-#ifndef _SFX_DOCFILT_HACK_HXX //autogen
 #include <sfx2/docfilt.hxx>
-#endif
-#ifndef _SFXAPP_HXX //autogen
 #include <sfx2/app.hxx>
-#endif
 
 #ifndef _SMDLL_HXX
 #include <smdll.hxx>
 #endif
-#ifndef DOCUMENT_HXX
 #include <document.hxx>
-#endif
-#ifndef TOOLBOX_HXX
 #include <toolbox.hxx>
-#endif
-#ifndef VIEW_HXX
 #include <view.hxx>
-#endif
 
 #ifndef _STARMATH_HRC
 #include <starmath.hrc>
