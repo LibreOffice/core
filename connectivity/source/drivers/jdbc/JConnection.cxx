e69398d949cc195547939b77e3f4a087041108bc
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:26:06 +0000

    INTEGRATION: CWS changefileheader (1.12.22); FILE MERGED 2008/04/01 10:53:04 thb 1.12.22.2: #i85898# Stripping all external header guards 2008/03/28 15:23:42 rt 1.12.22.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/jdbc/JConnection.cxx
+++ connectivity/source/drivers/jdbc/JConnection.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: JConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-30 07:54:23 $
+ * $RCSfile: JConnection.cxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,43 +35,18 @@
 #include "java/lang/Class.hxx"
 #include "java/tools.hxx"
 #include "java/ContextClassLoader.hxx"
-
-#ifndef _CONNECTIVITY_JAVA_SQL_DATABASEMETADATA_HXX_
 #include "java/sql/DatabaseMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_STATEMENT_HXX_
 #include "java/sql/JStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_DRIVER_HXX_
 #include "java/sql/Driver.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_PREPAREDSTATEMENT_HXX_
 #include "java/sql/PreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_CALLABLESTATEMENT_HXX_
 #include "java/sql/CallableStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_SQLWARNING_HXX_
 #include "java/sql/SQLWarning.hxx"
-#endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_SQLWARNING_HPP_
 #include <com/sun/star/sdbc/SQLWarning.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_SQLWARNING_HPP_
 #include <com/sun/star/sdbc/SQLWarning.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_NAMEDVALUE_HPP_
 #include <com/sun/star/beans/NamedValue.hpp>
-#endif
-#ifndef _CONNECTIVITY_SQLPARSE_HXX
 #include "connectivity/sqlparse.hxx"
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include "connectivity/dbexception.hxx"
-#endif
 #include "java/util/Property.hxx"
 #include "java/LocalRef.hxx"
 #include "resource/jdbc_log.hrc"
