258e2ce560cd73529225a47ef1d561ea48b7a744
Rüdiger Timm <rt@openoffice.org>
2008-04-10 16:22:10 +0000

    INTEGRATION: CWS changefileheader (1.75.38); FILE MERGED 2008/04/01 15:57:56 thb 1.75.38.3: #i85898# Stripping all external header guards 2008/04/01 12:54:49 thb 1.75.38.2: #i85898# Stripping all external header guards 2008/03/31 16:55:50 rt 1.75.38.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/ww8/wrtww8.hxx
+++ sw/source/filter/ww8/wrtww8.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: wrtww8.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.75 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 16:30:22 $
+ * $RCSfile: wrtww8.hxx,v $
+ * $Revision: 1.76 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
@@ -37,12 +32,8 @@
 #ifndef _WRTWW8_HXX
 #define _WRTWW8_HXX
 
-#ifndef _SOLAR_H
 #include <tools/solar.h>        // UINTXX
-#endif
-#ifndef _SV_GEN_HXX //autogen
 #include <tools/gen.hxx>
-#endif
 #ifndef _SVSTDARR_HXX
 #define _SVSTDARR_ULONGS
 #include <svtools/svstdarr.hxx>
@@ -61,34 +52,17 @@
 #ifndef WRT_FN_HXX
 #include <wrt_fn.hxx>
 #endif
-#ifndef _MSOCXIMEX_HXX
 #include <svx/msocximex.hxx>
-#endif
 
 #ifndef WW8STRUC_HXX
 #include "ww8struc.hxx"
 #endif
-#ifndef _WW8SCAN_HXX
 #include "ww8scan.hxx"
-#endif
-#ifndef WW_FIELDS_HXX
 #include "fields.hxx"
-#endif
-
-#ifndef WW_TYPES
 #include "types.hxx"
-#endif
-#ifndef SW_WRITERHELPER
 #include "writerhelper.hxx"
-#endif
-
-#ifndef SW_MS_MSFILTER_HXX
 #include "../inc/msfilter.hxx"
-#endif
-
-#ifndef _EXPFLD_HXX
 #include <expfld.hxx>
-#endif
 
 // einige Forward Deklarationen
 class BitmapPalette;
