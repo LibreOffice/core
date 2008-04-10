a03388ad9fc5bc1cef87886fb7caf65e0f0175e2
Rüdiger Timm <rt@openoffice.org>
2008-04-10 10:04:53 +0000

    INTEGRATION: CWS changefileheader (1.62.38); FILE MERGED 2008/04/01 15:56:10 thb 1.62.38.3: #i85898# Stripping all external header guards 2008/04/01 12:53:28 thb 1.62.38.2: #i85898# Stripping all external header guards 2008/03/31 16:52:36 rt 1.62.38.1: #i87441# Change license header to LPGL v3.

--- sw/inc/fesh.hxx
+++ sw/inc/fesh.hxx
@@ -1,62 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: fesh.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.62 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 14:48:59 $
+ * $RCSfile: fesh.hxx,v $
+ * $Revision: 1.63 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _FESH_HXX
 #define _FESH_HXX
 
 #include <com/sun/star/text/RelOrientation.hpp>
-
-#ifndef _COM_SUN_STAR_EMBED_XCLASSIFIEDOBJECT_HPP_
 #include <com/sun/star/embed/XClassifiedObject.hpp>
-#endif
 
 #include <svx/svdobj.hxx>
-
-#ifndef INCLUDED_SWDLLAPI_H
 #include "swdllapi.h"
-#endif
-#ifndef _EDITSH_HXX
 #include <editsh.hxx>
-#endif
-#ifndef _FLYENUM_HXX
 #include <flyenum.hxx>
-#endif
 
 // OD 25.06.2003 #108784#
-#ifndef _SVDTYPES_HXX
 #include <svx/svdtypes.hxx>
-#endif
 
 #include <svtools/embedhlp.hxx>
 
