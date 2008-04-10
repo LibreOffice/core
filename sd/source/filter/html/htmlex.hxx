a362baf49f10a218ff6f42585dce2ee8f15504aa
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:54:36 +0000

    INTEGRATION: CWS changefileheader (1.13.188); FILE MERGED 2008/04/01 15:34:02 thb 1.13.188.3: #i85898# Stripping all external header guards 2008/04/01 12:38:31 thb 1.13.188.2: #i85898# Stripping all external header guards 2008/03/31 13:56:52 rt 1.13.188.1: #i87441# Change license header to LPGL v3.

--- sd/source/filter/html/htmlex.hxx
+++ sd/source/filter/html/htmlex.hxx
@@ -1,89 +1,54 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: htmlex.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:39:23 $
+ * $RCSfile: htmlex.hxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SD_HTMLEX_HXX
 #define _SD_HTMLEX_HXX
 
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
 #include <com/sun/star/beans/PropertyValue.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_XSIMPLEFILEACCESS_HPP_
 #include <com/sun/star/ucb/XSimpleFileAccess.hpp>
-#endif
-
-#ifndef _SV_GDIMTF_HXX //autogen
 #include <vcl/gdimtf.hxx>
-#endif
-#ifndef _SFXITEMSET_HXX //autogen
 #include <svtools/itemset.hxx>
-#endif
-#ifndef _SD_RESLTN_HXX
 #include "resltn.hxx"       // enum PublishingResolution
-#endif
-#ifndef _SV_COLRDLG_HXX
 #include <svtools/colrdlg.hxx>
-#endif
-#ifndef _EHDL_HXX //autogen
 #include <svtools/ehdl.hxx>
-#endif
 
 #include "strings.hrc"
-
-#ifndef SD_DRAW_DOC_SHELL_HXX
 #include "DrawDocShell.hxx"
-#endif
-
-#ifndef SD_WINDOW_HXX
 #include "Window.hxx"
-#endif
-
-#ifndef SD_VIEW_SHELL_HXX
 #include "ViewShell.hxx"
-#endif
-
-#ifndef INC_ASSCLASS
 #include "assclass.hxx"
-#endif
 
 #ifndef _SD_RESID_HXX
 #include "sdresid.hxx"
 #endif
-
-#ifndef _SD_PUBDLG_HXX
 #include "pubdlg.hxx"
-#endif
 
 #include <boost/scoped_ptr.hpp>
 
