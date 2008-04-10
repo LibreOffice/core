f755ac7c7fd73d5156d01b2e73ce0fa436e4641a
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:15:54 +0000

    INTEGRATION: CWS changefileheader (1.6.72); FILE MERGED 2008/04/01 15:19:31 thb 1.6.72.3: #i85898# Stripping all external header guards 2008/04/01 12:31:03 thb 1.6.72.2: #i85898# Stripping all external header guards 2008/03/31 13:39:31 rt 1.6.72.1: #i87441# Change license header to LPGL v3.

--- goodies/source/base3d/b3ddeflt.hxx
+++ goodies/source/base3d/b3ddeflt.hxx
@@ -1,60 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: b3ddeflt.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 22:26:48 $
+ * $RCSfile: b3ddeflt.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _B3D_B3DDEFLT_HXX
 #define _B3D_B3DDEFLT_HXX
 
-#ifndef _B3D_B3DCOMMN_HXX
 #include "b3dcommn.hxx"
-#endif
-
-#ifndef _SV_BITMAP_HXX
 #include <vcl/bitmap.hxx>
-#endif
-
-#ifndef _SV_SALBTYPE_HXX
 #include <vcl/salbtype.hxx>
-#endif
-
-#ifndef _SV_ALPHA_HXX
 #include <vcl/alpha.hxx>
-#endif
-
-#ifndef _BXD_INTERPOLATOR_HXX
 #include <goodies/bxdintpo.hxx>
-#endif
 
 /*************************************************************************
 |*
