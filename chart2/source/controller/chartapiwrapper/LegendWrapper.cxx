74f7c8f029ea42868caf60578c18a60dfb89f0a2
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:26:11 +0000

    INTEGRATION: CWS changefileheader (1.7.94); FILE MERGED 2008/04/01 15:03:58 thb 1.7.94.3: #i85898# Stripping all external header guards 2008/04/01 10:50:16 thb 1.7.94.2: #i85898# Stripping all external header guards 2008/03/28 16:43:14 rt 1.7.94.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/chartapiwrapper/LegendWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/LegendWrapper.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: LegendWrapper.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-25 08:27:11 $
+ * $RCSfile: LegendWrapper.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,32 +35,14 @@
 #include "Chart2ModelContact.hxx"
 #include "LegendHelper.hxx"
 #include "ContainerHelper.hxx"
-
-#ifndef INCLUDED_COMPHELPER_INLINE_CONTAINER_HXX
 #include <comphelper/InlineContainer.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_XTITLED_HPP_
 #include <com/sun/star/chart2/XTitled.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART_CHARTLEGENDPOSITION_HPP_
 #include <com/sun/star/chart/ChartLegendPosition.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_LEGENDPOSITION_HPP_
 #include <com/sun/star/chart2/LegendPosition.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_LEGENDEXPANSION_HPP_
 #include <com/sun/star/chart2/LegendExpansion.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_RELATIVEPOSITION_HPP_
 #include <com/sun/star/chart2/RelativePosition.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_FILLSTYLE_HPP_
 #include <com/sun/star/drawing/FillStyle.hpp>
-#endif
 
 #include "CharacterProperties.hxx"
 #include "LineProperties.hxx"
@@ -78,10 +55,7 @@
 #include "WrappedDirectStateProperty.hxx"
 
 #include <algorithm>
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
 
 using namespace ::com::sun::star;
 using ::com::sun::star::beans::Property;
