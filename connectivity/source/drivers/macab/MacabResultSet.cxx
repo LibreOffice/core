eeee621f282f26c511fb157f8e6a12c278ba652c
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:54:48 +0000

    INTEGRATION: CWS changefileheader (1.2.86); FILE MERGED 2008/04/01 10:53:09 thb 1.2.86.2: #i85898# Stripping all external header guards 2008/03/28 15:23:47 rt 1.2.86.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/macab/MacabResultSet.cxx
+++ connectivity/source/drivers/macab/MacabResultSet.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MacabResultSet.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-09-13 17:53:58 $
+ * $RCSfile: MacabResultSet.cxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,40 +32,17 @@
 #include "precompiled_connectivity.hxx"
 
 #include "MacabResultSet.hxx"
-#ifndef _CONNECTIVITY_MACAB_ADDRESSBOOK_HXX_
 #include "MacabAddressBook.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_RECORDS_HXX_
 #include "MacabRecords.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_UTILITIES_HXX_
 #include "macabutilities.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_RESULTSETMETADATA_HXX_
 #include "MacabResultSetMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_CONNECTION_HXX_
 #include "MacabConnection.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_CONDITION_HXX_
 #include "macabcondition.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_ORDER_HXX_
 #include "macaborder.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBCX_COMPAREBOOKMARK_HPP_
 #include <com/sun/star/sdbcx/CompareBookmark.hpp>
-#endif
-#ifndef CONNECTIVITY_CONNECTION_HXX
 #include "TConnection.hxx"
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
-#endif
 
 using namespace connectivity::macab;
 using namespace cppu;
