f9291febfb9fd3c781c3840b52c1dc671a4cf041
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:14:53 +0000

    INTEGRATION: CWS changefileheader (1.8.8); FILE MERGED 2008/04/01 15:23:46 thb 1.8.8.3: #i85898# Stripping all external header guards 2008/04/01 12:33:16 thb 1.8.8.2: #i85898# Stripping all external header guards 2008/03/31 13:32:22 rt 1.8.8.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/report/ReportSection.cxx
+++ reportdesign/source/ui/report/ReportSection.cxx
@@ -1,117 +1,71 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ReportSection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 18:13:26 $
+ * $RCSfile: ReportSection.cxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef REPORT_REPORTSECTION_HXX
 #include "ReportSection.hxx"
-#endif
-#ifndef RPTUI_REPORT_WINDOW_HXX
 #include "ReportWindow.hxx"
-#endif
-#ifndef RPTUI_DESIGNVIEW_HXX
 #include "DesignView.hxx"
-#endif
 #ifndef REPORTDESIGN_SHARED_UISTRINGS_HRC
 #include "uistrings.hrc"
 #endif
-#ifndef _REPORT_RPTUIOBJ_HXX
 #include "RptObject.hxx"
-#endif
-#ifndef REPORT_RPTMODEL_HXX
 #include "RptModel.hxx"
-#endif
-#ifndef _REPORT_SECTIONVIEW_HXX
 #include "SectionView.hxx"
-#endif
-#ifndef _REPORT_RPTUIPAGE_HXX
 #include "RptPage.hxx"
-#endif
-#ifndef RPTUI_REPORTCONTROLLER_HXX
 #include "ReportController.hxx"
-#endif
 #include "UITools.hxx"
-#ifndef _SVDPAGV_HXX
 #include <svx/svdpagv.hxx>
-#endif
-#ifndef _EEITEMID_HXX
 #include <svx/eeitemid.hxx>
-#endif
-#ifndef _SVX_ADJITEM_HXX
 #include <svx/adjitem.hxx>
-#endif
 #include <svx/sdrpaintwindow.hxx>
-#ifndef _COM_SUN_STAR_DATATRANSFER_CLIPBOARD_XCLIPBOARD_HPP_
 #include <com/sun/star/datatransfer/clipboard/XClipboard.hpp>
-#endif
 #include <toolkit/helper/convert.hxx>
-#ifndef _REPORT_RPTUIDEF_HXX
 #include "RptDef.hxx"
-#endif
-#ifndef RPTUI_VIEWSWINDOW_HXX
 #include "ViewsWindow.hxx"
-#endif
 #ifndef RTPUI_REPORTDESIGN_HELPID_HRC
 #include "helpids.hrc"
 #endif
 #ifndef _RPTUI_DLGRESID_HRC
 #include "RptResId.hrc"
 #endif
-#ifndef _REPORT_RPTUICLIP_HXX
 #include "dlgedclip.hxx"
-#endif
-#ifndef RPTUI_UNDOACTIONS_HXX
 #include "UndoActions.hxx"
-#endif
 #ifndef _RPTUI_SLOTID_HRC_
 #include "rptui_slotid.hrc"
 #endif
 #ifndef _SBASLTID_HRC
 #include <svx/svxids.hrc>
 #endif
-#ifndef _SVX_DBAEXCHANGE_HXX_
 #include <svx/dbaexchange.hxx>
-#endif
-#ifndef _CONNECTIVITY_DBTOOLS_HXX_
 #include <connectivity/dbtools.hxx>
-#endif
-#ifndef _GALLERY_HXX_
 #include <svx/gallery.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX
 #include <svtools/itempool.hxx>
-#endif
 #include <svx/unoshape.hxx>
 #include <unotools/confignode.hxx>
 #include <svtools/extcolorcfg.hxx>
