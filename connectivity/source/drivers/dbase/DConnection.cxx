eb706e267b4fecaaf653253ce39fb9b65e652247
Rüdiger Timm <rt@openoffice.org>
2008-04-10 07:32:49 +0000

    INTEGRATION: CWS changefileheader (1.19.182); FILE MERGED 2008/04/01 15:08:40 thb 1.19.182.3: #i85898# Stripping all external header guards 2008/04/01 10:52:52 thb 1.19.182.2: #i85898# Stripping all external header guards 2008/03/28 15:23:28 rt 1.19.182.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/dbase/DConnection.cxx
+++ connectivity/source/drivers/dbase/DConnection.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: DConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 16:16:06 $
+ * $RCSfile: DConnection.cxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,33 +34,17 @@
 #ifndef _CONNECTIVITY_DBASE_OCONNECTION_HXX_
 #include "dbase/DConnection.hxx"
 #endif
-#ifndef _CONNECTIVITY_DBASE_ODATABASEMETADATA_HXX_
 #include "dbase/DDatabaseMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_DBASE_CATALOG_HXX_
 #include "dbase/DCatalog.hxx"
-#endif
 #ifndef _CONNECTIVITY_DBASE_ODRIVER_HXX_
 #include "dbase/DDriver.hxx"
 #endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _URLOBJ_HXX //autogen wg. INetURLObject
 #include <tools/urlobj.hxx>
-#endif
-#ifndef _CONNECTIVITY_DBASE_DPREPAREDSTATEMENT_HXX_
 #include "dbase/DPreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_DBASE_DSTATEMENT_HXX_
 #include "dbase/DStatement.hxx"
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
-#endif
 
 using namespace connectivity::dbase;
 using namespace connectivity::file;
