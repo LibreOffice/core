11baba722ecb30bba9cadb35ea6abb6275151949
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:58:41 +0000

    INTEGRATION: CWS changefileheader (1.6.242); FILE MERGED 2008/04/01 15:56:09 thb 1.6.242.3: #i85898# Stripping all external header guards 2008/04/01 12:53:28 thb 1.6.242.2: #i85898# Stripping all external header guards 2008/03/31 16:52:35 rt 1.6.242.1: #i87441# Change license header to LPGL v3.

--- sw/inc/docary.hxx
+++ sw/inc/docary.hxx
@@ -1,43 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: docary.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 07:59:22 $
+ * $RCSfile: docary.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _DOCARY_HXX
 #define _DOCARY_HXX
 
-#ifndef _COM_SUN_STAR_I18N_FORBIDDENCHARACTERS_HPP_
 #include <com/sun/star/i18n/ForbiddenCharacters.hpp>
-#endif
 
 class SwFieldType;
 class SwFrmFmt;
@@ -58,14 +51,8 @@ namespace com { namespace sun { namespace star { namespace i18n {
 #ifndef _TABLE_HXX //autogen
 #include <tools/table.hxx>
 #endif
-
-#ifndef _SWTYPES_HXX //autogen
 #include <swtypes.hxx>
-#endif
-
-#ifndef _SVARRAY_HXX //autogen
 #include <svtools/svarray.hxx>
-#endif
 
 typedef SwFieldType* SwFldTypePtr;
 #define GROW_FLDTYPES   16
