3026fdbe403be8ae05d575e5fb2ca6d603a71a96
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:03:10 +0000

    INTEGRATION: CWS changefileheader (1.12.588); FILE MERGED 2008/04/01 15:30:55 thb 1.12.588.3: #i85898# Stripping all external header guards 2008/04/01 12:36:46 thb 1.12.588.2: #i85898# Stripping all external header guards 2008/03/31 17:15:42 rt 1.12.588.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/inc/imoptdlg.hxx
+++ sc/source/ui/inc/imoptdlg.hxx
@@ -1,72 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: imoptdlg.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-03-22 12:11:02 $
+ * $RCSfile: imoptdlg.hxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_IMOPTDLG_HXX
 #define SC_IMOPTDLG_HXX
 
-#ifndef _SV_DIALOG_HXX //autogen
 #include <vcl/dialog.hxx>
-#endif
-
-#ifndef _SV_FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
-#endif
-
-#ifndef _SV_COMBOBOX_HXX //autogen
 #include <vcl/combobox.hxx>
-#endif
-
-#ifndef _SV_LSTBOX_HXX //autogen
 #include <vcl/lstbox.hxx>
-#endif
-
-#ifndef _SV_BUTTON_HXX //autogen
 #include <vcl/imagebtn.hxx>
-#endif
-
-#ifndef _SVX_TXENCBOX_HXX
 #include <svx/txencbox.hxx>
-#endif
-
-#ifndef INCLUDED_SCDLLAPI_H
 #include "scdllapi.h"
-#endif
-
-#ifndef SC_SCGLOB_HXX
 #include "global.hxx"
-#endif
 
 //===================================================================
 
