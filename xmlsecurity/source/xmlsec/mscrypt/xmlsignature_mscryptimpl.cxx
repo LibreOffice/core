4d95a08d8c4ea76dcefaa7ff2d9cc35bd49f5548
Rüdiger Timm <rt@openoffice.org>
2008-04-10 23:36:03 +0000

    INTEGRATION: CWS changefileheader (1.8.66); FILE MERGED 2008/04/01 16:11:06 thb 1.8.66.3: #i85898# Stripping all external header guards 2008/04/01 13:06:26 thb 1.8.66.2: #i85898# Stripping all external header guards 2008/03/31 16:31:04 rt 1.8.66.1: #i87441# Change license header to LPGL v3.

--- xmlsecurity/source/xmlsec/mscrypt/xmlsignature_mscryptimpl.cxx
+++ xmlsecurity/source/xmlsec/mscrypt/xmlsignature_mscryptimpl.cxx
@@ -1,54 +1,40 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlsignature_mscryptimpl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-04-17 10:26:29 $
+ * $RCSfile: xmlsignature_mscryptimpl.cxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_xmlsecurity.hxx"
-
-#ifndef _SAL_CONFIG_H_
 #include <sal/config.h>
-#endif
-
-#ifndef _RTL_UUID_H_
 #include <rtl/uuid.h>
-#endif
 
 #include "com/sun/star/xml/crypto/SecurityOperationStatus.hdl"
-
-#ifndef _XMLSIGNATURE_MSCRYPTIMPL_HXX_
 #include "xmlsignature_mscryptimpl.hxx"
-#endif
 
 #ifndef _XMLDOCUMENTWRAPPER_XMLSECIMPL_HXX_
 #include "xmldocumentwrapper_xmlsecimpl.hxx"
@@ -61,14 +47,8 @@
 #ifndef _SECURITYENVIRONMENT_MSCRYPTIMPL_HXX_
 #include "securityenvironment_mscryptimpl.hxx"
 #endif
-
-#ifndef _XMLSTREAMIO_XMLSECIMPL_HXX_
 #include "xmlstreamio.hxx"
-#endif
-
-#ifndef _ERRORCALLBACK_XMLSECIMPL_HXX_
 #include "errorcallback.hxx"
-#endif
 
 #include "xmlsec/xmlsec.h"
 #include "xmlsec/xmldsig.h"
