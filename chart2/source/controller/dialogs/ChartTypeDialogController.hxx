dab5f8d94d864b61016fe4baa98c7273a4dfa211
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:41:59 +0000

    INTEGRATION: CWS changefileheader (1.3.106); FILE MERGED 2008/04/01 15:03:59 thb 1.3.106.3: #i85898# Stripping all external header guards 2008/04/01 10:50:18 thb 1.3.106.2: #i85898# Stripping all external header guards 2008/03/28 16:43:20 rt 1.3.106.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/dialogs/ChartTypeDialogController.hxx
+++ chart2/source/controller/dialogs/ChartTypeDialogController.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ChartTypeDialogController.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-03 13:37:08 $
+ * $RCSfile: ChartTypeDialogController.hxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,34 +33,21 @@
 
 #include "ChangingResource.hxx"
 #include "ThreeDHelper.hxx"
-
-#ifndef INCLUDED_COMPHELPER_INLINE_CONTAINER_HXX
 #include <comphelper/InlineContainer.hxx>
-#endif
 
 #include <com/sun/star/chart2/CurveStyle.hpp>
 #include <com/sun/star/chart2/XChartDocument.hpp>
 #include <com/sun/star/chart2/XChartTypeTemplate.hpp>
-
-#ifndef _VALUESET_HXX
 #include <svtools/valueset.hxx>
-#endif
 // header for class CheckBox
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
 // header for class FixedText
-#ifndef _SV_FIXED_HXX
 #include <vcl/fixed.hxx>
-#endif
 // header for class MetricField
-#ifndef _SV_FIELD_HXX
 #include <vcl/field.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#endif
 
 //.............................................................................
 namespace chart
