e562ee998ff46302f2185b48154442fe7602bbb6
Rüdiger Timm <rt@openoffice.org>
2008-04-10 12:19:13 +0000

    INTEGRATION: CWS changefileheader (1.8.20); FILE MERGED 2008/04/01 13:02:29 thb 1.8.20.2: #i85898# Stripping all external header guards 2008/03/28 15:53:01 rt 1.8.20.1: #i87441# Change license header to LPGL v3.

--- writerfilter/source/doctok/WW8DocumentImpl.hxx
+++ writerfilter/source/doctok/WW8DocumentImpl.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: WW8DocumentImpl.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-01-10 11:46:14 $
+ * $RCSfile: WW8DocumentImpl.hxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,27 +32,15 @@
 #define INCLUDED_WW8_DOCUMENT_IMPL_HXX
 
 #include <set>
-#ifndef INCLUDED_WW8_PIECE_TABLE_HXX
 #include <WW8PieceTable.hxx>
-#endif
-#ifndef INCLUDED_WW8_BIN_TABLE_HXX
 #include <WW8BinTable.hxx>
-#endif
-#ifndef INCLUDED_RESOURCES_HXX
 #include <resources.hxx>
-#endif
 #ifndef INCLUDED_WW8_RESOURCE_MODEL_HXX
 #include <resourcemodel/WW8ResourceModel.hxx>
 #endif
-#ifndef INCLUDED_PLCF_HXX
 #include <PLCF.hxx>
-#endif
-#ifndef INCLUDED_WW8_STTBF_HXX
 #include <WW8Sttbf.hxx>
-#endif
-#ifndef INCLUDED_X_NOTE_HELPER_HXX
 #include <XNoteHelper.hxx>
-#endif
 
 namespace writerfilter {
 namespace doctok
