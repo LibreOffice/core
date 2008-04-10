f975794a597a597aaa9c926d4b5ebea55b9fcba8
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:22:18 +0000

    INTEGRATION: CWS changefileheader (1.19.354); FILE MERGED 2008/04/01 15:50:18 thb 1.19.354.3: #i85898# Stripping all external header guards 2008/04/01 12:48:10 thb 1.19.354.2: #i85898# Stripping all external header guards 2008/03/31 14:19:23 rt 1.19.354.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/dlgctl3d.cxx
+++ svx/source/dialog/dlgctl3d.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dlgctl3d.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 07:33:15 $
+ * $RCSfile: dlgctl3d.cxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,46 +32,19 @@
 #include "precompiled_svx.hxx"
 
 #include <svx/dialogs.hrc>
-
-#ifndef _SVX_XFLCLIT_HXX //autogen
 #include <svx/xflclit.hxx>
-#endif
-#ifndef _SVX_FMMODEL_HXX
 #include <svx/fmmodel.hxx>
-#endif
-#ifndef _SVX_FMPAGE_HXX
 #include <svx/fmpage.hxx>
-#endif
-#ifndef _E3D_VIEW3D_HXX //autogen
 #include <svx/view3d.hxx>
-#endif
-#ifndef _E3D_POLYSC3D_HXX //autogen
 #include <svx/polysc3d.hxx>
-#endif
-#ifndef _E3D_OBJ3D_HXX //autogen
 #include <svx/obj3d.hxx>
-#endif
-#ifndef _CAMERA3D_HXX //autogen
 #include <svx/camera3d.hxx>
-#endif
-#ifndef _VOLUME3D_HXX //autogen
 #include <svx/volume3d.hxx>
-#endif
-#ifndef _E3D_SPHERE3D_HXX //autogen
 #include <svx/sphere3d.hxx>
-#endif
-#ifndef _E3D_CUBE3D_HXX //autogen
 #include <svx/cube3d.hxx>
-#endif
-#ifndef _SV_EVENT_HXX //autogen
 #include <vcl/event.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX
 #include <svtools/itempool.hxx>
-#endif
-#ifndef _SFXSTYLE_HXX
 #include <svtools/style.hxx>
-#endif
 
 #include <svx/dlgctl3d.hxx>
 #include <goodies/base3d.hxx>
