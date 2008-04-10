47ddb0bfb66184bdeac2a59fd52b70926b310b44
Rüdiger Timm <rt@openoffice.org>
2008-04-10 13:34:32 +0000

    INTEGRATION: CWS changefileheader (1.6.118); FILE MERGED 2008/04/01 16:02:11 thb 1.6.118.3: #i85898# Stripping all external header guards 2008/04/01 12:57:58 thb 1.6.118.2: #i85898# Stripping all external header guards 2008/03/31 15:30:13 rt 1.6.118.1: #i87441# Change license header to LPGL v3.

--- ucb/source/cacher/cacheddynamicresultsetstub.cxx
+++ ucb/source/cacher/cacheddynamicresultsetstub.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: cacheddynamicresultsetstub.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 13:42:21 $
+ * $RCSfile: cacheddynamicresultsetstub.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,26 +32,11 @@
 #include "precompiled_ucb.hxx"
 
 #include <cacheddynamicresultsetstub.hxx>
-
-#ifndef _COM_SUN_STAR_SDBC_XRESULTSET_HPP_
 #include <com/sun/star/sdbc/XResultSet.hpp>
-#endif
-
-#ifndef _CACHED_CONTENT_RESULTSET_STUB_HXX
 #include <cachedcontentresultsetstub.hxx>
-#endif
-
-#ifndef  _COM_SUN_STAR_UCB_CONTENTRESULTSETCAPABILITY_HPP_
 #include <com/sun/star/ucb/ContentResultSetCapability.hpp>
-#endif
-
-#ifndef  _COM_SUN_STAR_UCB_XSORTEDDYNAMICRESULTSETFACTORY_HPP_
 #include <com/sun/star/ucb/XSortedDynamicResultSetFactory.hpp>
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
 
 using namespace com::sun::star::lang;
 using namespace com::sun::star::sdbc;
