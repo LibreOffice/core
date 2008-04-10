a5485173b762e1d1c44656d6c14865fe42f8ae26
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:43:02 +0000

    INTEGRATION: CWS changefileheader (1.13.498); FILE MERGED 2008/04/01 12:36:21 thb 1.13.498.2: #i85898# Stripping all external header guards 2008/03/31 17:14:45 rt 1.13.498.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/inc/xecontent.hxx
+++ sc/source/filter/inc/xecontent.hxx
@@ -1,63 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xecontent.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-07-10 13:53:28 $
+ * $RCSfile: xecontent.hxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_XECONTENT_HXX
 #define SC_XECONTENT_HXX
 
-#ifndef SC_RANGELST_HXX
 #include "rangelst.hxx"
-#endif
-
-#ifndef SC_XLCONTENT_HXX
 #include "xlcontent.hxx"
-#endif
-#ifndef SC_XLADDRESS_HXX
 #include "xladdress.hxx"
-#endif
-#ifndef SC_XEROOT_HXX
 #include "xeroot.hxx"
-#endif
-#ifndef SC_XERECORD_HXX
 #include "xerecord.hxx"
-#endif
-#ifndef SC_XESTRING_HXX
 #include "xestring.hxx"
-#endif
-#ifndef SC_XEFORMULA_HXX
 #include "xeformula.hxx"
-#endif
 
 /* ============================================================================
 Classes to export the big Excel document contents (related to several cells or
