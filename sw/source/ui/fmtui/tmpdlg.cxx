d90b81069cd06c8b6608a67c00e7af5c85c79345
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:30:51 +0000

    INTEGRATION: CWS changefileheader (1.24.240); FILE MERGED 2008/04/01 15:59:05 thb 1.24.240.3: #i85898# Stripping all external header guards 2008/04/01 12:55:20 thb 1.24.240.2: #i85898# Stripping all external header guards 2008/03/31 16:58:12 rt 1.24.240.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/fmtui/tmpdlg.cxx
+++ sw/source/ui/fmtui/tmpdlg.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: tmpdlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.24 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 11:50:26 $
+ * $RCSfile: tmpdlg.cxx,v $
+ * $Revision: 1.25 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,103 +36,49 @@
 #endif
 
 
-#ifndef _HINTIDS_HXX
 #include <hintids.hxx>
-#endif
 
 #ifndef _SVSTDARR_STRINGSSORTDTOR
 #define _SVSTDARR_STRINGSSORTDTOR
 #include <svtools/svstdarr.hxx>
 #endif
-#ifndef _SV_MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SVX_HDFT_HXX //autogen
 #include <svx/hdft2.hxx>
-#endif
-#ifndef _SVX_FLSTITEM_HXX //autogen
 #include <svx/flstitem.hxx>
-#endif
-#ifndef _SVX_HTMLMODE_HXX //autogen
 #include <svx/htmlmode.hxx>
-#endif
 #include <svx/htmlcfg.hxx>
-#ifndef _SVTOOLS_CJKOPTIONS_HXX
 #include <svtools/cjkoptions.hxx>
-#endif
-#ifndef _NUMPARA_HXX
 #include <numpara.hxx>
-#endif
-#ifndef _FMTCLDS_HXX //autogen
 #include <fmtclds.hxx>
-#endif
-#ifndef _SWTYPES_HXX
 #include <swtypes.hxx>
-#endif
-#ifndef _SWMODULE_HXX
 #include <swmodule.hxx>
-#endif
-#ifndef _ERRHDL_HXX
 #include <errhdl.hxx>       // fuer Create-Methoden
-#endif
-#ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
-#endif
 #ifndef _VIEW_HXX
 #include <view.hxx>
 #endif
-#ifndef _SWWDOCSH_HXX //autogen
 #include <wdocsh.hxx>
-#endif
-#ifndef _VIEWOPT_HXX
 #include <viewopt.hxx>
-#endif
-#ifndef _PGFNOTE_HXX
 #include <pgfnote.hxx>
-#endif
 #ifndef _PGFGRID_HXX
 #include <pggrid.hxx>
 #endif
 #ifndef _TMPDLG_HXX
 #include <tmpdlg.hxx>       // der Dialog
 #endif
-#ifndef _COLUMN_HXX
 #include <column.hxx>       // Spalten
-#endif
-#ifndef _DRPCPS_HXX
 #include <drpcps.hxx>       // Initialen
-#endif
-#ifndef _FRMPAGE_HXX
 #include <frmpage.hxx>      // Rahmen
-#endif
-#ifndef _WRAP_HXX
 #include <wrap.hxx>         // Rahmen
-#endif
-#ifndef _FRMMGR_HXX
 #include <frmmgr.hxx>       // Rahmen
-#endif
-#ifndef _CCOLL_HXX
 #include <ccoll.hxx>        // CondColl
-#endif
 #include <swuiccoll.hxx>
-#ifndef _DOCSTYLE_HXX
 #include <docstyle.hxx>     //
-#endif
-#ifndef _FMTCOL_HXX
 #include <fmtcol.hxx>       //
-#endif
-#ifndef _MACASSGN_HXX
 #include <macassgn.hxx>     //
-#endif
-#ifndef _POOLFMT_HXX
 #include <poolfmt.hxx>
-#endif
-#ifndef _UITOOL_HXX
 #include <uitool.hxx>
-#endif
-#ifndef _SHELLRES_HXX //autogen
 #include <shellres.hxx>
-#endif
 
 #ifndef _CMDID_H
 #include <cmdid.h>
@@ -151,9 +92,7 @@
 #ifndef _FMTUI_HRC
 #include <fmtui.hrc>
 #endif
-#ifndef _SWSTYLENAMEMAPPER_HXX
 #include <SwStyleNameMapper.hxx>
-#endif
 #include <svx/svxids.hrc>
 #include <svtools/stritem.hxx>
 #include <svtools/aeitem.hxx>
