131e63fc5f3bf822ca218df4e1f511323a5e1f3d
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:47:08 +0000

    INTEGRATION: CWS changefileheader (1.76.32); FILE MERGED 2008/04/01 15:59:30 thb 1.76.32.3: #i85898# Stripping all external header guards 2008/04/01 12:55:40 thb 1.76.32.2: #i85898# Stripping all external header guards 2008/03/31 16:58:56 rt 1.76.32.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/index/cnttab.cxx
+++ sw/source/ui/index/cnttab.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: cnttab.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.76 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 12:51:02 $
+ * $RCSfile: cnttab.cxx,v $
+ * $Revision: 1.77 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,124 +36,53 @@
 
 
 #include <rsc/rscsfx.hxx>
-
-#ifndef _SV_MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SV_HELP_HXX
 #include <vcl/help.hxx>
-#endif
-#ifndef _SFXSTRITEM_HXX //autogen
 #include <svtools/stritem.hxx>
-#endif
-#ifndef SVTOOLS_URIHELPER_HXX
 #include <svtools/urihelper.hxx>
-#endif
-#ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
 #include <svtools/pathoptions.hxx>
-#endif
-#ifndef _SFXREQUEST_HXX //autogen
 #include <sfx2/request.hxx>
-#endif
-#ifndef _SFXVIEWFRM_HXX //autogen
 #include <sfx2/viewfrm.hxx>
-#endif
-#ifndef _SFXDISPATCH_HXX //autogen
 #include <sfx2/dispatch.hxx>
-#endif
-#ifndef _SFXDOCFILE_HXX
 #include <sfx2/docfile.hxx>
-#endif
 #include <svx/dialogs.hrc>
 #include <svx/svxdlg.hxx>
 #include <svx/flagsdef.hxx>
-#ifndef _SVX_SIMPTABL_HXX //autogen wg. SvxSimpleTable
 #include <svx/simptabl.hxx>
-#endif
-#ifndef  _COM_SUN_STAR_UI_DIALOGS_TEMPLATEDESCRIPTION_HPP_
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UI_DIALOGS_XFILEPICKER_HPP_
 #include <com/sun/star/ui/dialogs/XFilePicker.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UI_DIALOGS_XFILTERMANAGER_HPP_
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
-#endif
-#ifndef SVTOOLS_INDEXENTRYRESSOURCE_HXX
 #include <svtools/indexentryres.hxx>
-#endif
-#ifndef _UNO_LINGU_HXX
 #include <svx/unolingu.hxx>
-#endif
-#ifndef _COLUMN_HXX //autogen
 #include <column.hxx>
-#endif
-#ifndef _FMTFSIZE_HXX //autogen
 #include <fmtfsize.hxx>
-#endif
-#ifndef _SHELLIO_HXX
 #include <shellio.hxx>
-#endif
-#ifndef _AUTHFLD_HXX
 #include <authfld.hxx>
-#endif
-#ifndef _SWTYPES_HXX
 #include <swtypes.hxx>
-#endif
-#ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
-#endif
 #ifndef _VIEW_HXX
 #include <view.hxx>
 #endif
 #ifndef _BASESH_HXX
 #include <basesh.hxx>
 #endif
-#ifndef _OUTLINE_HXX
 #include <outline.hxx>
-#endif
-#ifndef _CNTTAB_HXX
 #include <cnttab.hxx>
-#endif
-#ifndef _SWUI_CNTTAB_HXX
 #include <swuicnttab.hxx>
-#endif
-#ifndef _FORMEDT_HXX
 #include <formedt.hxx>
-#endif
-#ifndef _POOLFMT_HXX
 #include <poolfmt.hxx>
-#endif
 #ifndef _POOLFMT_HRC
 #include <poolfmt.hrc>
 #endif
-#ifndef _UITOOL_HXX
 #include <uitool.hxx>
-#endif
-#ifndef _FMTCOL_HXX //autogen
 #include <fmtcol.hxx>
-#endif
-#ifndef _FLDBAS_HXX //autogen wg. SwFieldType
 #include <fldbas.hxx>
-#endif
-#ifndef _EXPFLD_HXX //autogen wg. SwSetExpFieldType
 #include <expfld.hxx>
-#endif
-#ifndef _UNOTOOLS_HXX
 #include <unotools.hxx>
-#endif
-#ifndef _UNOTXDOC_HXX
 #include <unotxdoc.hxx>
-#endif
-#ifndef _SWDOCSH_HXX
 #include <docsh.hxx>
-#endif
-#ifndef _SWMODULE_HXX
 #include <swmodule.hxx>
-#endif
-#ifndef _MODOPT_HXX
 #include <modcfg.hxx>
-#endif
 
 #ifndef _CMDID_H
 #include <cmdid.h>
@@ -178,18 +102,12 @@
 #ifndef _GLOBALS_HRC
 #include <globals.hrc>
 #endif
-#ifndef _SWSTYLENAMEMAPPER_HXX
 #include <SwStyleNameMapper.hxx>
-#endif
-#ifndef _FILEDLGHELPER_HXX
 #include <sfx2/filedlghelper.hxx>
-#endif
 #ifndef _TOXHLP_HXX
 #include <toxwrap.hxx>
 #endif
-#ifndef _CHPFLD_HXX
 #include <chpfld.hxx>
-#endif
 
 #ifndef _UTLUI_HRC
 #include "utlui.hrc"
@@ -204,10 +122,7 @@ using namespace ::com::sun::star::uno;
 using namespace com::sun::star::ui::dialogs;
 using ::rtl::OUString;
 using namespace ::sfx2;
-
-#ifndef _SVTOOLS_EDITBROWSEBOX_HXX_
 #include <svtools/editbrowsebox.hxx>
-#endif
 
 static const sal_Unicode aDeliStart = '['; //fuer die form
 static const sal_Unicode aDeliEnd    = ']'; //fuer die form
