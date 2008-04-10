200c040035b881d45839506242da5f322a1acc8a
Rüdiger Timm <rt@openoffice.org>
2008-04-10 07:08:07 +0000

    INTEGRATION: CWS changefileheader (1.25.22); FILE MERGED 2008/04/01 15:08:33 thb 1.25.22.3: #i85898# Stripping all external header guards 2008/04/01 10:52:47 thb 1.25.22.2: #i85898# Stripping all external header guards 2008/03/28 15:23:23 rt 1.25.22.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/adabas/BConnection.cxx
+++ connectivity/source/drivers/adabas/BConnection.cxx
@@ -1,80 +1,50 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: BConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.25 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-02-04 13:28:37 $
+ * $RCSfile: BConnection.cxx,v $
+ * $Revision: 1.26 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
-#ifndef _CONNECTIVITY_ADABAS_BCONNECTION_HXX_
 #include "adabas/BConnection.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADABAS_BDRIVER_HXX_
 #include "adabas/BDriver.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADABAS_CATALOG_HXX_
 #include "adabas/BCatalog.hxx"
-#endif
-#ifndef _CONNECTIVITY_ODBC_OFUNCTIONS_HXX_
 #include "odbc/OFunctions.hxx"
-#endif
-#ifndef _CONNECTIVITY_OTOOLS_HXX_
 #include "odbc/OTools.hxx"
-#endif
 #ifndef _CONNECTIVITY_ODBC_ODATABASEMETADATA_HXX_
 #include "adabas/BDatabaseMetaData.hxx"
 #endif
-#ifndef CONNECTIVITY_ADABAS_STATEMENT_HXX
 #include "adabas/BStatement.hxx"
-#endif
-#ifndef CONNECTIVITY_ADABAS_PREPAREDSTATEMENT_HXX
 #include "adabas/BPreparedStatement.hxx"
-#endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _DBHELPER_DBCHARSET_HXX_
 #include <connectivity/dbcharset.hxx>
-#endif
-#ifndef _CONNECTIVITY_PARSE_SQLITERATOR_HXX_
 #include "connectivity/sqliterator.hxx"
-#endif
-#ifndef _CONNECTIVITY_SQLPARSE_HXX
 #include <connectivity/sqlparse.hxx>
-#endif
 
 #include <string.h>
 
