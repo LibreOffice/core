bf6b55bfe4ace31133fe22011f507a2b03d5a02a
Rüdiger Timm <rt@openoffice.org>
2008-04-10 07:17:56 +0000

    INTEGRATION: CWS changefileheader (1.29.22); FILE MERGED 2008/04/01 15:08:35 thb 1.29.22.3: #i85898# Stripping all external header guards 2008/04/01 10:52:48 thb 1.29.22.2: #i85898# Stripping all external header guards 2008/03/28 15:23:25 rt 1.29.22.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/ado/AConnection.cxx
+++ connectivity/source/drivers/ado/AConnection.cxx
@@ -1,84 +1,49 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: AConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.29 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-30 07:49:05 $
+ * $RCSfile: AConnection.cxx,v $
+ * $Revision: 1.30 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-
-#ifndef _CONNECTIVITY_ADO_ACONNECTION_HXX_
 #include "ado/AConnection.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_ADATABASEMETADATA_HXX_
 #include "ado/ADatabaseMetaData.hxx"
-#endif
-
-#ifndef _CONNECTIVITY_ADO_ADRIVER_HXX_
 #include "ado/ADriver.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_ASTATEMENT_HXX_
 #include "ado/AStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_ACALLABLESTATEMENT_HXX_
 #include "ado/ACallableStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_APREPAREDSTATEMENT_HXX_
 #include "ado/APreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_CATALOG_HXX_
 #include "ado/ACatalog.hxx"
-#endif
-#ifndef _COM_SUN_STAR_SDBC_COLUMNVALUE_HPP_
 #include <com/sun/star/sdbc/ColumnValue.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_TRANSACTIONISOLATION_HPP_
 #include <com/sun/star/sdbc/TransactionIsolation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XROW_HPP_
 #include <com/sun/star/sdbc/XRow.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include "connectivity/dbexception.hxx"
-#endif
-#ifndef _OSL_FILE_HXX_
 #include <osl/file.hxx>
-#endif
 
 
 using namespace dbtools;
