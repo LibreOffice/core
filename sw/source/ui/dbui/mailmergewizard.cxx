f8c4cc9b709c13271eb4221dde96a1cf5237b9fc
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:28:17 +0000

    INTEGRATION: CWS changefileheader (1.15.38); FILE MERGED 2008/04/01 15:58:37 thb 1.15.38.3: #i85898# Stripping all external header guards 2008/04/01 12:55:07 thb 1.15.38.2: #i85898# Stripping all external header guards 2008/03/31 16:57:03 rt 1.15.38.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/dbui/mailmergewizard.cxx
+++ sw/source/ui/dbui/mailmergewizard.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: mailmergewizard.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.15 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-06 19:03:45 $
+ * $RCSfile: mailmergewizard.cxx,v $
+ * $Revision: 1.16 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,60 +35,28 @@
 #endif
 
 
-#ifndef _MAILMERGEWIZARD_HXX
 #include <mailmergewizard.hxx>
-#endif
-#ifndef _MAILMERGEDOCSELECTPAGE_HXX
 #include <mmdocselectpage.hxx>
-#endif
-#ifndef _MAILMERGEOUTPUTTYPEPAGE_HXX
 #include <mmoutputtypepage.hxx>
-#endif
-#ifndef _MAILMERGEADDRESSBLOCKPAGE_HXX
 #include <mmaddressblockpage.hxx>
-#endif
-#ifndef _MAILMERGEPREPAREMERGEPAGE_HXX
 #include <mmpreparemergepage.hxx>
-#endif
-#ifndef _MAILMERGEMERGEPAGE_HXX
 #include <mmmergepage.hxx>
-#endif
-#ifndef _MAILMERGEGREETINGSPAGE_HXX
 #include <mmgreetingspage.hxx>
-#endif
-#ifndef _MAILMERGEOUTPUTPAGE_HXX
 #include <mmoutputpage.hxx>
-#endif
-#ifndef _MAILMERGELAYOUTPAGE_HXX
 #include <mmlayoutpage.hxx>
-#endif
-#ifndef _MMCONFIGITEM_HXX
 #include <mmconfigitem.hxx>
-#endif
-#ifndef _SW_ABSTDLG_HXX
 #include <swabstdlg.hxx>
-#endif
 #ifndef _DBUI_HRC
 #include <dbui.hrc>
 #endif
-#ifndef _SWVIEW_HXX
 #include <view.hxx>
-#endif
 #ifndef _DBMGR_HXX
 #include <dbmgr.hxx>
 #endif
-#ifndef _SFXVIEWFRM_HXX
 #include <sfx2/viewfrm.hxx>
-#endif
-#ifndef _WRTSH_HXX
 #include <wrtsh.hxx>
-#endif
-#ifndef _SFXVIEWFRM_HXX
 #include <sfx2/viewfrm.hxx>
-#endif
-#ifndef _SV_MSGBOX_HXX
 #include "vcl/msgbox.hxx" // RET_CANCEL
-#endif
 
 #include <helpid.h>
 #include <dbui.hrc>
