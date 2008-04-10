a9a52d2b56804be5aee0295ade1c4fe737aae197
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:11:17 +0000

    INTEGRATION: CWS changefileheader (1.2.38); FILE MERGED 2008/04/01 15:59:51 thb 1.2.38.3: #i85898# Stripping all external header guards 2008/04/01 12:56:05 thb 1.2.38.2: #i85898# Stripping all external header guards 2008/03/31 16:59:55 rt 1.2.38.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/utlui/viewlayoutctrl.cxx
+++ sw/source/ui/utlui/viewlayoutctrl.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: viewlayoutctrl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 15:09:49 $
+ * $RCSfile: viewlayoutctrl.cxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,29 +32,18 @@
 #include "precompiled_sw.hxx"
 
 // include ---------------------------------------------------------------
-
-#ifndef _VIEWLAYOUTCTRL_HXX
 #include <viewlayoutctrl.hxx>
-#endif
 
 #ifndef _STATUS_HXX //autogen
 #include <vcl/status.hxx>
 #endif
-#ifndef _SV_IMAGE_HXX
 #include <vcl/image.hxx>
-#endif
-#ifndef _SFXENUMITEM_HXX
 #include <svtools/eitem.hxx>
-#endif
-#ifndef _SVX_VIEWLAYOUTITEM_HXX
 #include <svx/viewlayoutitem.hxx>
-#endif
 #ifndef _UTLUI_HRC
 #include <utlui.hrc>
 #endif
-#ifndef _SWTYPES_HXX
 #include <swtypes.hxx>  // fuer Pathfinder
-#endif
 
 // STATIC DATA -----------------------------------------------------------
 
