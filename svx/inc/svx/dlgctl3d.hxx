ebef6b3edcf286035aea1af76596805136b2a9fc
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:30:56 +0000

    INTEGRATION: CWS changefileheader (1.3.440); FILE MERGED 2008/04/01 15:49:31 thb 1.3.440.2: #i85898# Stripping all external header guards 2008/03/31 14:18:11 rt 1.3.440.1: #i87441# Change license header to LPGL v3.

--- svx/inc/svx/dlgctl3d.hxx
+++ svx/inc/svx/dlgctl3d.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dlgctl3d.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 15:15:26 $
+ * $RCSfile: dlgctl3d.hxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,41 +33,18 @@
 
 // includes --------------------------------------------------------------
 
-#ifndef _TL_POLY_HXX
 #include <tools/poly.hxx>
-#endif
-
-#ifndef _SV_CTRL_HXX
 #include <vcl/ctrl.hxx>
-#endif
-
-#ifndef _B3D_B3DGEOM_HXX
 #include <goodies/b3dgeom.hxx>
-#endif
-
-#ifndef _B3D_B3DTRANS_HXX
 #include <goodies/b3dtrans.hxx>
-#endif
-
-#ifndef _B3D_MATRIL3D_HXX
 #include <goodies/matril3d.hxx>
-#endif
-
-#ifndef _B3D_B3DLIGHT_HXX
 #include <goodies/b3dlight.hxx>
-#endif
-
-#ifndef _SV_SCRBAR_HXX
 #include <vcl/scrbar.hxx>
-#endif
 
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
-
-#ifndef _SFXITEMSET_HXX //autogen
 #include <svtools/itemset.hxx>
-#endif
 
 class FmFormModel;
 class FmFormPage;
