ac38f5a0847645c85af4132f02744ef8093c8948
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:18:51 +0000

    INTEGRATION: CWS changefileheader (1.9.182); FILE MERGED 2008/04/01 15:09:01 thb 1.9.182.3: #i85898# Stripping all external header guards 2008/04/01 10:53:13 thb 1.9.182.2: #i85898# Stripping all external header guards 2008/03/28 15:23:58 rt 1.9.182.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/mozab/mozillasrc/MNSMozabProxy.cxx
+++ connectivity/source/drivers/mozab/mozillasrc/MNSMozabProxy.cxx
@@ -1,59 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MNSMozabProxy.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 16:20:47 $
+ * $RCSfile: MNSMozabProxy.cxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-#ifndef _CONNECTIVITY_MAB_MOZABHELPER_HXX_
 #include "MNSMozabProxy.hxx"
-#endif
 
 #include "resource/mozab_res.hrc"
-
-#ifndef _CONNECTIVITY_MAB_DATABASEMETADATAHELPER_HXX_
 #include "MDatabaseMetaDataHelper.hxx"
-#endif
-#ifndef _CONNECTIVITY_MAB_QUERY_HXX_
 #include "MQuery.hxx"
-#endif
-
-#ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
-#endif
-#ifndef _OSL_CONDITN_HXX_
 #include <osl/conditn.hxx>
-#endif
 
 #include "pre_include_mozilla.h"
 #include <nsIProxyObjectManager.h>
@@ -66,18 +49,11 @@
 #include "nsILDAPConnection.h"
 #include "nsILDAPOperation.h"
 #include "post_include_mozilla.h"
-
-#ifndef _CONNECTIVITY_MAB_QUERY_HXX_
 #include "MQuery.hxx"
-#endif
-#ifndef _CONNECTIVITY_MAB_QUERYHELPER_HXX_
 #include <MQueryHelper.hxx>
-#endif
 #include <com/sun/star/uno/Reference.hxx>
 #include <unotools/processfactory.hxx>
-#ifndef _COM_SUN_STAR_MOZILLA_XPROXYRUNNER_HPP_
 #include "com/sun/star/mozilla/XProxyRunner.hpp"
-#endif
 
 using namespace com::sun::star::uno;
 using namespace com::sun::star::lang;
