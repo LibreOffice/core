c58a32d937712c86ab40c99e9f553c01cfd38bf9
Rüdiger Timm <rt@openoffice.org>
2008-04-10 14:41:36 +0000

    INTEGRATION: CWS changefileheader (1.53.16); FILE MERGED 2008/04/01 16:02:27 thb 1.53.16.3: #i85898# Stripping all external header guards 2008/04/01 12:58:27 thb 1.53.16.2: #i85898# Stripping all external header guards 2008/03/31 15:30:31 rt 1.53.16.1: #i87441# Change license header to LPGL v3.

--- ucb/source/ucp/webdav/NeonSession.cxx
+++ ucb/source/ucp/webdav/NeonSession.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: NeonSession.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.53 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-02-19 12:35:08 $
+ * $RCSfile: NeonSession.cxx,v $
+ * $Revision: 1.54 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,85 +33,33 @@
 
 #include <hash_map>
 #include <string.h>
-#ifndef _RTL_STRING_H_
 #include <rtl/string.h>
-#endif
-#ifndef NE_SOCKET_H
 #include <ne_socket.h>
-#endif
-#ifndef NE_AUTH_H
 #include <ne_auth.h>
-#endif
-#ifndef NE_REDIRECT_H
 #include <ne_redirect.h>
-#endif
-#ifndef NE_LOCKS_H
 #include <ne_locks.h>
-#endif
-
-#ifndef NE_SSL_H
 #include <ne_ssl.h>
-#endif
-
-#ifndef __XML_PARSER_H__
 #include "libxml/parser.h"
-#endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-
-#ifndef _DAVAUTHLISTENER_HXX_
 #include "DAVAuthListener.hxx"
-#endif
-#ifndef _NEONTYPES_HXX_
 #include "NeonTypes.hxx"
-#endif
-#ifndef _NEONSESSION_HXX_
 #include "NeonSession.hxx"
-#endif
-#ifndef _NEONINPUTSTREAM_HXX_
 #include "NeonInputStream.hxx"
-#endif
-#ifndef _NEONPROPFINDREQUEST_HXX_
 #include "NeonPropFindRequest.hxx"
-#endif
-#ifndef _NEONHEADREQUEST_HXX_
 #include "NeonHeadRequest.hxx"
-#endif
-#ifndef _NEONURI_HXX_
 #include "NeonUri.hxx"
-#endif
 #ifndef _LINKSEQUENCE_HXX_
 #include "LinkSequence.hxx"
 #endif
 
 #include <com/sun/star/xml/crypto/XSEInitializer.hpp>
-
-#ifndef _UCBDEADPROPERTYVALUE_HXX_
 #include "UCBDeadPropertyValue.hxx"
-#endif
-#ifndef _COM_SUN_STAR_XML_CRYPTO_XSECURITYENVIRONMENT_HPP_
 #include <com/sun/star/xml/crypto/XSecurityEnvironment.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SECURITY_XCERTIFICATE_HPP_
 #include <com/sun/star/security/XCertificate.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SECURITY_CERTIFICATEVALIDITY_HPP_
 #include <com/sun/star/security/CertificateValidity.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_SECURITY_CERTIFICATECONTAINERSTATUS_HPP_
 #include <com/sun/star/security/CertificateContainerStatus.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_SECURITY_CERTIFICATECONTAINER_HPP_
 #include <com/sun/star/security/CertificateContainer.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_SECURITY_XCERTIFICATECONTAINER_HPP_
 #include <com/sun/star/security/XCertificateContainer.hpp>
-#endif
 
 
 #ifndef _SIMPLECERTIFICATIONVALIDATIONREQUEST_HXX_
