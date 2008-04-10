906f496512ea55b8a1a70d06d6530b1b88b15ec0
Rüdiger Timm <rt@openoffice.org>
2008-04-10 11:44:19 +0000

    INTEGRATION: CWS changefileheader (1.13.242); FILE MERGED 2008/04/01 15:56:43 thb 1.13.242.3: #i85898# Stripping all external header guards 2008/04/01 12:53:46 thb 1.13.242.2: #i85898# Stripping all external header guards 2008/03/31 16:53:39 rt 1.13.242.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/access/accselectionhelper.cxx
+++ sw/source/core/access/accselectionhelper.cxx
@@ -1,35 +1,30 @@
  /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: accselectionhelper.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 08:24:02 $
+ * $RCSfile: accselectionhelper.cxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,36 +32,18 @@
 #include "precompiled_sw.hxx"
 
 
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLESELECTION_HPP_
 #include <com/sun/star/accessibility/XAccessibleSelection.hpp>
-#endif
-#ifndef _ACCSELECTIONHELPER_HXX_
 #include <accselectionhelper.hxx>
-#endif
 
 #ifndef _ACCCONTEXT_HXX
 #include <acccontext.hxx>
 #endif
-#ifndef _ACCMAP_HXX
 #include <accmap.hxx>
-#endif
-#ifndef _SVX_ACCESSIBILITY_ACCESSIBLE_SHAPE_HXX
 #include <svx/AccessibleShape.hxx>
-#endif
-
-#ifndef _VIEWSH_HXX
 #include <viewsh.hxx>
-#endif
-#ifndef _FESH_HXX
 #include "fesh.hxx"
-#endif
-
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>        // for SolarMutex
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 
 
 using namespace ::com::sun::star;
