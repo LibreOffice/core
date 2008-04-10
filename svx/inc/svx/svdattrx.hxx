09a3242c2623d25d07bea53ed202fc3ee7aa8c52
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:13:44 +0000

    INTEGRATION: CWS changefileheader (1.2.466); FILE MERGED 2008/04/01 12:46:52 thb 1.2.466.2: #i85898# Stripping all external header guards 2008/03/31 14:18:16 rt 1.2.466.1: #i87441# Change license header to LPGL v3.

--- svx/inc/svx/svdattrx.hxx
+++ svx/inc/svx/svdattrx.hxx
@@ -1,88 +1,51 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdattrx.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 16:17:58 $
+ * $RCSfile: svdattrx.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SVDATTRX_HXX
 #define _SVDATTRX_HXX
 
-#ifndef _SXMLHITM_HXX
 #include <svx/sxmlhitm.hxx>
-#endif
-
-#ifndef _SXALLITM_HXX
 #include <svx/sxallitm.hxx>
-#endif
-
-#ifndef _SXCAITM_HXX
 #include <svx/sxcaitm.hxx>
-#endif
-
-#ifndef _SXCECITM_HXX
 #include <svx/sxcecitm.hxx>
-#endif
 
 #ifndef _SXCGIITM_HXX
 #include <svx/sxcgitm.hxx>
 #endif
-
-#ifndef _SXCIAITM_HXX
 #include <svx/sxciaitm.hxx>
-#endif
-
-#ifndef _SXCIKITM_HXX
 #include <svx/sxcikitm.hxx>
-#endif
-
-#ifndef _SXCLLITM_HXX
 #include <svx/sxcllitm.hxx>
-#endif
-
-#ifndef _SXCTITM_HXX
 #include <svx/sxctitm.hxx>
-#endif
-
-#ifndef _SXEKITM_HXX
 #include <svx/sxekitm.hxx>
-#endif
-
-#ifndef _SXELDITM_HXX
 #include <svx/sxelditm.hxx>
-#endif
-
-#ifndef _SXENDITM_HXX
 #include <svx/sxenditm.hxx>
-#endif
 
 #ifndef _SXFIITM_HXX
 #include <svx/sxfiitm.hxx>
@@ -91,113 +54,35 @@
 #ifndef _SXLAYITM_HXX
 #include <svx/sxlayitm.hxx>
 #endif
-
-#ifndef _SXLOGITM_HXX
 #include <svx/sxlogitm.hxx>
-#endif
-
-#ifndef _SXMBRITM_HXX
 #include <svx/sxmbritm.hxx>
-#endif
-
-#ifndef _SXMFSITM_HXX
 #include <svx/sxmfsitm.hxx>
-#endif
-
-#ifndef _SXMKITM_HXX
 #include <svx/sxmkitm.hxx>
-#endif
-
-#ifndef _SXMOITM_HXX
 #include <svx/sxmoitm.hxx>
-#endif
-
-#ifndef _SXMOVITM_HXX
 #include <svx/sxmovitm.hxx>
-#endif
 
 #ifndef _SXMSIITM_HXX
 #include <svx/sxmsitm.hxx>
 #endif
-
-#ifndef _SXMSPITM_HXX
 #include <svx/sxmspitm.hxx>
-#endif
-
-#ifndef _SXMSUITM_HXX
 #include <svx/sxmsuitm.hxx>
-#endif
-
-#ifndef _SXMTAITM_HXX
 #include <svx/sxmtaitm.hxx>
-#endif
-
-#ifndef _SXMTFITM_HXX
 #include <svx/sxmtfitm.hxx>
-#endif
-
-#ifndef _SXMTPITM_HXX
 #include <svx/sxmtpitm.hxx>
-#endif
-
-#ifndef _SXMTRITM_HXX
 #include <svx/sxmtritm.hxx>
-#endif
-
-#ifndef _SXMUITM_HXX
 #include <svx/sxmuitm.hxx>
-#endif
-
-#ifndef _SXONEITM_HXX
 #include <svx/sxoneitm.hxx>
-#endif
-
-#ifndef _SXONITM_HXX
 #include <svx/sxonitm.hxx>
-#endif
-
-#ifndef _SXOPITM_HXX
 #include <svx/sxopitm.hxx>
-#endif
-
-#ifndef _SXRAITM_HXX
 #include <svx/sxraitm.hxx>
-#endif
-
-#ifndef _SXREAITM_HXX
 #include <svx/sxreaitm.hxx>
-#endif
-
-#ifndef _SXREOITM_HXX
 #include <svx/sxreoitm.hxx>
-#endif
-
-#ifndef _SXROAITM_HXX
 #include <svx/sxroaitm.hxx>
-#endif
-
-#ifndef _SXROOITM_HXX
 #include <svx/sxrooitm.hxx>
-#endif
-
-#ifndef _SXSAITM_HXX
 #include <svx/sxsaitm.hxx>
-#endif
-
-#ifndef _SXSALITM_HXX
 #include <svx/sxsalitm.hxx>
-#endif
-
-#ifndef _SXSIITM_HXX
 #include <svx/sxsiitm.hxx>
-#endif
-
-#ifndef _SXSOITM_HXX
 #include <svx/sxsoitm.hxx>
-#endif
-
-#ifndef _SXTRAITM_HXX
 #include <svx/sxtraitm.hxx>
-#endif
 
 #endif  //_SVDATTRX_HXX
