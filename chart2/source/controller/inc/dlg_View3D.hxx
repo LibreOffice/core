2243721f7fba580e36afff2741773d832c98428b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:42:39 +0000

    INTEGRATION: CWS changefileheader (1.2.126); FILE MERGED 2008/04/01 15:04:08 thb 1.2.126.3: #i85898# Stripping all external header guards 2008/04/01 10:50:24 thb 1.2.126.2: #i85898# Stripping all external header guards 2008/03/28 16:43:46 rt 1.2.126.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/inc/dlg_View3D.hxx
+++ chart2/source/controller/inc/dlg_View3D.hxx
@@ -1,63 +1,48 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dlg_View3D.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-05-22 17:59:53 $
+ * $RCSfile: dlg_View3D.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _CHART2_DLG_VIEW3D_DIAGRAM_HXX
 #define _CHART2_DLG_VIEW3D_DIAGRAM_HXX
 
-#ifndef _COM_SUN_STAR_DRAWING_SHADEMODE_HPP_
 #include <com/sun/star/drawing/ShadeMode.hpp>
-#endif
-#ifndef _COM_SUN_STAR_FRAME_XMODEL_HPP_
 #include <com/sun/star/frame/XModel.hpp>
-#endif
 
 // header for class TabDialog
-#ifndef _SV_TABDLG_HXX
 #include <vcl/tabdlg.hxx>
-#endif
 // header for class TabControl
-#ifndef _SV_TABCTRL_HXX
 #include <vcl/tabctrl.hxx>
-#endif
 // header for class OKButton
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
 // header for class XColorTable
-#ifndef _XTABLE_HXX
 #include <svx/xtable.hxx>
-#endif
 
 #include "ControllerLockGuard.hxx"
 
