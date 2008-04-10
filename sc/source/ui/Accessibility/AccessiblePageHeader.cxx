eefe2015f2dda1798ef7e1d61961f652eb9266db
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:42:21 +0000

    INTEGRATION: CWS changefileheader (1.21.330); FILE MERGED 2008/04/01 15:30:35 thb 1.21.330.3: #i85898# Stripping all external header guards 2008/04/01 12:36:37 thb 1.21.330.2: #i85898# Stripping all external header guards 2008/03/31 17:15:06 rt 1.21.330.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/Accessibility/AccessiblePageHeader.cxx
+++ sc/source/ui/Accessibility/AccessiblePageHeader.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: AccessiblePageHeader.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.21 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:55:47 $
+ * $RCSfile: AccessiblePageHeader.cxx,v $
+ * $Revision: 1.22 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,56 +32,32 @@
 #include "precompiled_sc.hxx"
 
 #include "AccessiblePageHeader.hxx"
-#ifndef _SC_ACCESSIBLEPAGEHEADERAREA_HXX
 #include "AccessiblePageHeaderArea.hxx"
-#endif
-#ifndef SC_ACCESSIBILITYHINTS_HXX
 #include "AccessibilityHints.hxx"
-#endif
 #include "prevwsh.hxx"
 #include "unoguard.hxx"
 #include "miscuno.hxx"
 #include "prevloc.hxx"
-#ifndef SC_DOCUMENT_HXX
 #include "document.hxx"
-#endif
-#ifndef SC_STLPOOL_HXX
 #include "stlpool.hxx"
-#endif
-#ifndef SC_ITEMS_HXX
 #include "scitems.hxx"
-#endif
-#ifndef SC_SCATTR_HXX
 #include "attrib.hxx"
-#endif
-#ifndef SC_SCRESID_HXX
 #include "scresid.hxx"
-#endif
 #ifndef SC_SC_HRC
 #include "sc.hrc"
 #endif
 
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
 #include <com/sun/star/accessibility/AccessibleStateType.hpp>
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEEVENTID_HPP_
 #include <com/sun/star/accessibility/AccessibleEventId.hpp>
-#endif
 
 #include <vcl/window.hxx>
 #include <svtools/smplhint.hxx>
 #include <unotools/accessiblestatesethelper.hxx>
-#ifndef _SFXSTYLE_HXX
 #include <svtools/style.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX
 #include <svtools/itempool.hxx>
-#endif
-#ifndef _EDITOBJ_HXX
 #include <svx/editobj.hxx>
-#endif
-#ifndef _TOOLKIT_HELPER_CONVERT_HXX_
 #include <toolkit/helper/convert.hxx>
-#endif
 
 #include <algorithm>
 
