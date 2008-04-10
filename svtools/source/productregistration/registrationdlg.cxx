28ef27f8151dc54165633b25d07ecebc88861108
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:56:50 +0000

    INTEGRATION: CWS changefileheader (1.7.246); FILE MERGED 2008/04/01 15:45:27 thb 1.7.246.3: #i85898# Stripping all external header guards 2008/04/01 12:43:52 thb 1.7.246.2: #i85898# Stripping all external header guards 2008/03/31 13:02:29 rt 1.7.246.1: #i87441# Change license header to LPGL v3.

--- svtools/source/productregistration/registrationdlg.cxx
+++ svtools/source/productregistration/registrationdlg.cxx
@@ -1,49 +1,39 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: registrationdlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 21:58:34 $
+ * $RCSfile: registrationdlg.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
-
-#ifndef SVTOOLS_REGISTRATIONDLG_HXX
 #include "registrationdlg.hxx"
-#endif
 
 #if 0 /* @@@ */
-#ifndef _SVTOOLS_SVTDATA_HXX
 #include <svtools/svtdata.hxx>
-#endif
 #ifndef _SVTOOLS_HRC
 #include <svtools/svtools.hrc>
 #endif
@@ -52,13 +42,8 @@
 #ifndef SVTOOLS_REGISTRATIONDLG_HRC
 #include "registrationdlg.hrc"
 #endif
-
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 
 //........................................................................
 namespace svt
