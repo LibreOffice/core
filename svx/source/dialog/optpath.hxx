1d556b5f72d2054259910e9cbb0660e4e24db76b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:37:08 +0000

    INTEGRATION: CWS changefileheader (1.8.622); FILE MERGED 2008/04/01 15:50:28 thb 1.8.622.3: #i85898# Stripping all external header guards 2008/04/01 12:48:19 thb 1.8.622.2: #i85898# Stripping all external header guards 2008/03/31 14:19:59 rt 1.8.622.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/optpath.hxx
+++ svx/source/dialog/optpath.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: optpath.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2006-11-22 10:36:38 $
+ * $RCSfile: optpath.hxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _SVX_OPTPATH_HXX
@@ -37,9 +32,7 @@
 
 // include ---------------------------------------------------------------
 
-#ifndef _SFXTABDLG_HXX //autogen
 #include <sfx2/tabdlg.hxx>
-#endif
 #ifndef _FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
 #endif
@@ -48,22 +41,16 @@
 #endif
 
 #ifdef _SVX_OPTPATH_CXX
-#ifndef _HEADBAR_HXX //autogen
 #include <svtools/headbar.hxx>
-#endif
 #else
 class HeaderBar;
 #endif
-#ifndef SVX_CONTROLL_FUCUS_HELPER_HXX
 #include "ControlFocusHelper.hxx"
-#endif
 
 #ifndef _COM_SUN_STAR_UI_XFOLDERPICKER_HPP_
 #include <com/sun/star/ui/dialogs/XFolderPicker.hpp>
 #endif
-#ifndef _SVTOOLS_DIALOGCLOSEDLISTENER_HXX
 #include <svtools/dialogclosedlistener.hxx>
-#endif
 
 // forward ---------------------------------------------------------------
 
