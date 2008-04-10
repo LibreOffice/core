97b07f871e9d30d699d0557dc63ac5cbe4901ba4
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:43:19 +0000

    INTEGRATION: CWS changefileheader (1.3.70); FILE MERGED 2008/04/01 12:33:11 thb 1.3.70.2: #i85898# Stripping all external header guards 2008/03/31 13:32:10 rt 1.3.70.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/filter/xml/xmlReportElementBase.cxx
+++ reportdesign/source/filter/xml/xmlReportElementBase.cxx
@@ -1,57 +1,39 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlReportElementBase.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-26 14:23:33 $
+ * $RCSfile: xmlReportElementBase.cxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPT_XMLREPORTELEMENTBASE_HXX
 #include "xmlReportElementBase.hxx"
-#endif
-#ifndef RPT_XMLFILTER_HXX
 #include "xmlfilter.hxx"
-#endif
-#ifndef RPT_XMLCONTROLPROPERTY_HXX
 #include "xmlControlProperty.hxx"
-#endif
-#ifndef RPT_XMLREPORTELEMENT_HXX
 #include "xmlReportElement.hxx"
-#endif
-#ifndef RPT_XMLENUMS_HXX
 #include "xmlEnums.hxx"
-#endif
-#ifndef RPT_XMLTABLE_HXX
 #include "xmlTable.hxx"
-#endif
 
 namespace rptxml
 {
