c1db319ad77c45dfa801a895d0119be97a62dc5a
Rüdiger Timm <rt@openoffice.org>
2008-04-10 16:19:46 +0000

    INTEGRATION: CWS changefileheader (1.20.114); FILE MERGED 2008/04/01 15:57:53 thb 1.20.114.3: #i85898# Stripping all external header guards 2008/04/01 12:54:47 thb 1.20.114.2: #i85898# Stripping all external header guards 2008/03/31 16:55:49 rt 1.20.114.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/ww8/writerhelper.hxx
+++ sw/source/filter/ww8/writerhelper.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: writerhelper.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.20 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-01-10 12:31:55 $
+ * $RCSfile: writerhelper.hxx,v $
+ * $Revision: 1.21 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
@@ -41,35 +36,16 @@
 #include <typeinfo>
 #include <vector>
 #include <map>
-
-#ifndef _COM_SUN_STAR_EMBED_XEMBEDDEDOBJECT_HPP_
 #include <com/sun/star/embed/XEmbeddedObject.hpp>
-#endif
 
 #include <sfx2/objsh.hxx>
-
-#ifndef WW_TYPES
 #   include "types.hxx"
-#endif
-
-#ifndef _SFXITEMPOOL_HXX
 #   include <svtools/itempool.hxx>     //SfxItemPool
-#endif
-#ifndef _SFXITEMSET_HXX
 #   include <svtools/itemset.hxx>      //SfxItemSet
-#endif
-#ifndef _FORMAT_HXX
 #   include <format.hxx>               //SwFmt
-#endif
-#ifndef _NODE_HXX
 #   include <node.hxx>                 //SwCntntNode
-#endif
-#ifndef _PAM_HXX
 #   include <pam.hxx>                  //SwPaM
-#endif
-#ifndef _TL_POLY_HXX
 #   include <tools/poly.hxx>           //Polygon, PolyPolygon
-#endif
 
 //Uncomment to dump debugging streams of graphics
 #if OSL_DEBUG_LEVEL > 1
