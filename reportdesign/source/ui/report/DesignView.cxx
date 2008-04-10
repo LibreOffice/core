b5f6b1308a6f5c93efda88899c52cc7c3613fe59
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:13:30 +0000

    INTEGRATION: CWS changefileheader (1.4.26); FILE MERGED 2008/04/01 15:23:45 thb 1.4.26.3: #i85898# Stripping all external header guards 2008/04/01 12:33:16 thb 1.4.26.2: #i85898# Stripping all external header guards 2008/03/31 13:32:22 rt 1.4.26.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/report/DesignView.cxx
+++ reportdesign/source/ui/report/DesignView.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: DesignView.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 13:50:32 $
+ * $RCSfile: DesignView.cxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
@@ -37,53 +32,31 @@
 #ifndef RPT_DESIGNVIEW_HXX
 #include "DesignView.hxx"
 #endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 #ifndef DBUI_TABLECONTROLLER_HXX
 #include "ReportController.hxx"
 #endif
-#ifndef _COMPHELPER_TYPES_HXX_
 #include <comphelper/types.hxx>
-#endif
-#ifndef INCLUDED_SVTOOLS_SYSLOCALE_HXX
 #include <svtools/syslocale.hxx>
-#endif
-#ifndef _REPORT_RPTUIDEF_HXX
 #include "RptDef.hxx"
-#endif
 #include "UITools.hxx"
 #include "RptObject.hxx"
-#ifndef _REPORT_PROPBRW_HXX
 #include "propbrw.hxx"
-#endif
-#ifndef _TOOLKIT_HELPER_CONVERT_HXX_
 #include <toolkit/helper/convert.hxx>
-#endif
 #ifndef RTPUI_REPORTDESIGN_HELPID_HRC
 #include "helpids.hrc"
 #endif
-#ifndef _REPORT_SECTIONVIEW_HXX
 #include "SectionView.hxx"
-#endif
-#ifndef REPORT_REPORTSECTION_HXX
 #include "ReportSection.hxx"
-#endif
 #ifndef _RPTUI_SLOTID_HRC_
 #include "rptui_slotid.hrc"
 #endif
 #ifndef _SBASLTID_HRC
 #include <svx/svxids.hrc>
 #endif
-#ifndef RPTUI_ADDFIELDWINDOW_HXX
 #include "AddField.hxx"
-#endif
-#ifndef RPTUI_SCROLLHELPER_HXX
 #include "ScrollHelper.hxx"
-#endif
-#ifndef INCLUDED_RPTUI_NAVIGATOR_HXX
 #include "Navigator.hxx"
-#endif
 #include <vcl/svapp.hxx>
 
 namespace rptui
