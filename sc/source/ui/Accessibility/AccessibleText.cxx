6f48de2b6a5ac15c99929053b4b63ef5f37a0892
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:44:09 +0000

    INTEGRATION: CWS changefileheader (1.41.88); FILE MERGED 2008/04/01 15:30:36 thb 1.41.88.3: #i85898# Stripping all external header guards 2008/04/01 12:36:38 thb 1.41.88.2: #i85898# Stripping all external header guards 2008/03/31 17:15:06 rt 1.41.88.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/Accessibility/AccessibleText.cxx
+++ sc/source/ui/Accessibility/AccessibleText.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: AccessibleText.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.41 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 15:39:33 $
+ * $RCSfile: AccessibleText.cxx,v $
+ * $Revision: 1.42 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,61 +36,24 @@
 
 
 #include <memory>
-
-#ifndef _SC_ACCESSIBLETEXT_HXX
 #include "AccessibleText.hxx"
-#endif
-
-#ifndef SC_TABVWSH_HXX
 #include "tabvwsh.hxx"
-#endif
-#ifndef SC_EDITUTIL_HXX
 #include "editutil.hxx"
-#endif
-#ifndef SC_DOCUMENT_HXX
 #include "document.hxx"
-#endif
-#ifndef SC_SCMOD_HXX
 #include "scmod.hxx"
-#endif
-#ifndef SC_PREVWSH_HXX
 #include "prevwsh.hxx"
-#endif
-#ifndef SC_DOCSHELL_HXX
 #include "docsh.hxx"
-#endif
-#ifndef SC_PREVLOC_HXX
 #include "prevloc.hxx"
-#endif
-#ifndef SC_UNOGUARD_HXX
 #include "unoguard.hxx"
-#endif
 #include "patattr.hxx"
-#ifndef SC_INPUTWIN_HXX
 #include "inputwin.hxx"
-#endif
-
-#ifndef _SVX_UNOFORED_HXX
 #include <svx/unofored.hxx>
-#endif
-#ifndef _MyEDITVIEW_HXX
 #include <svx/editview.hxx>
-#endif
-#ifndef _SVX_UNOEDHLP_HXX
 #include <svx/unoedhlp.hxx>
-#endif
-#ifndef _SV_VIRDEV_HXX
 #include <vcl/virdev.hxx>
-#endif
-#ifndef _EDITOBJ_HXX
 #include <svx/editobj.hxx>
-#endif
-#ifndef _SVX_ADJITEM_HXX
 #include <svx/adjitem.hxx>
-#endif
-#ifndef _SVDMODEL_HXX
 #include <svx/svdmodel.hxx>
-#endif
 
 
 // ============================================================================
