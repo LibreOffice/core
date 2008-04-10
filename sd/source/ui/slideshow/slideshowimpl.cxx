487dfc4953640a537269fe8172822c9c7701930d
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:55:50 +0000

    INTEGRATION: CWS changefileheader (1.52.14); FILE MERGED 2008/04/01 15:35:50 thb 1.52.14.3: #i85898# Stripping all external header guards 2008/04/01 12:39:14 thb 1.52.14.2: #i85898# Stripping all external header guards 2008/03/31 13:58:43 rt 1.52.14.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/slideshow/slideshowimpl.cxx
+++ sd/source/ui/slideshow/slideshowimpl.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: slideshowimpl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.53 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 14:13:26 $
+ * $RCSfile: slideshowimpl.cxx,v $
+ * $Revision: 1.54 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -48,34 +43,17 @@
 #include <com/sun/star/awt/SystemPointer.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
 #include <com/sun/star/frame/XDispatch.hpp>
-
-#ifndef _VOS_PROCESS_HXX_
 #include <vos/process.hxx>
-#endif
-
-#ifndef _AEITEM_HXX
 #include <svtools/aeitem.hxx>
-#endif
-
-#ifndef SVTOOLS_URIHELPER_HXX
 #include <svtools/urihelper.hxx>
-#endif
 
 #include <toolkit/unohlp.hxx>
 
 #include <sfx2/imagemgr.hxx>
-
-#ifndef _SFXREQUEST_HXX
 #include <sfx2/request.hxx>
-#endif
 #include "sfx2/docfile.hxx"
-
-#ifndef _SVX_UNOAPI_HXX_
 #include <svx/unoapi.hxx>
-#endif
-#ifndef _SVDOOLE2_HXX
 #include <svx/svdoole2.hxx>
-#endif
 
 // for child window ids
 #include <sfx2/templdlg.hxx>
@@ -88,23 +66,11 @@
 #include <svx/hyprlink.hxx>
 #include <svx/hyperdlg.hxx>
 #include <svx/galbrws.hxx>
-
-#ifndef SD_NAVIGATOR_CHILD_WINDOW_HXX
 #include "NavigatorChildWindow.hxx"
-#endif
-#ifndef SD_ANIMATION_CHILD_WINDOW_HXX
 #include "AnimationChildWindow.hxx"
-#endif
-
-#ifndef _SD_SLIDESHOWIMPL_HXX_
 #include <slideshowimpl.hxx>
-#endif
-#ifndef _SD_SLIDESHOWVIEWIMPL_HXX_
 #include <slideshowviewimpl.hxx>
-#endif
-#ifndef _SD_PGJUMP_HXX
 #include <pgjump.hxx>
-#endif
 #include "PaneHider.hxx"
 
 #include "glob.hrc"
