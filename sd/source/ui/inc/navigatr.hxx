8ba8ec5cbe952edb9a8437dc8ad6c42725c847b6
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:26:29 +0000

    INTEGRATION: CWS changefileheader (1.14.134); FILE MERGED 2008/04/01 15:35:28 thb 1.14.134.3: #i85898# Stripping all external header guards 2008/04/01 12:39:08 thb 1.14.134.2: #i85898# Stripping all external header guards 2008/03/31 13:58:16 rt 1.14.134.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/inc/navigatr.hxx
+++ sd/source/ui/inc/navigatr.hxx
@@ -1,59 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: navigatr.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.14 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-08-28 13:38:13 $
+ * $RCSfile: navigatr.hxx,v $
+ * $Revision: 1.15 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SD_NAVIGATOR_HXX
 #define SD_NAVIGATOR_HXX
 
-#ifndef _SV_WINDOW_HXX
 #include <vcl/window.hxx>
-#endif
-#ifndef _SV_LSTBOX_HXX //autogen
 #include <vcl/lstbox.hxx>
-#endif
 #ifndef _TOOLBOX_HXX //autogen
 #include <vcl/toolbox.hxx>
 #endif
-#ifndef _SFXCTRLITEM_HXX //autogen
 #include <sfx2/ctrlitem.hxx>
-#endif
-#ifndef _SDTREELB_HXX
 #include "sdtreelb.hxx"
-#endif
-#ifndef _PRESENTATION_HXX
 #include "pres.hxx"
-#endif
 
 #define NAVSTATE_NONE           0x00000000
 
