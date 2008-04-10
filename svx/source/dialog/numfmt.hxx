d882a779f89e834e384deabd1dba8bf22602aa0b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:24:50 +0000

    INTEGRATION: CWS changefileheader (1.5.368); FILE MERGED 2008/04/01 15:50:24 thb 1.5.368.3: #i85898# Stripping all external header guards 2008/04/01 12:48:15 thb 1.5.368.2: #i85898# Stripping all external header guards 2008/03/31 14:19:56 rt 1.5.368.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/numfmt.hxx
+++ svx/source/dialog/numfmt.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: numfmt.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 17:22:35 $
+ * $RCSfile: numfmt.hxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _SVX_NUMFMT_HXX
@@ -37,21 +32,10 @@
 
 //------------------------------------------------------------------------
 
-#ifndef _WINDOW_HXX //autogen
 #include <vcl/window.hxx>
-#endif
-
-#ifndef _COLOR_HXX //autogen
 #include <tools/color.hxx>
-#endif
-
-#ifndef _STRING_HXX //autogen
 #include <tools/string.hxx>
-#endif
-
-#ifndef _SFXTABDLG_HXX //autogen
 #include <sfx2/tabdlg.hxx>
-#endif
 
 #ifndef _FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
@@ -72,14 +56,8 @@
 #ifndef _FIELD_HXX //autogen
 #include <vcl/field.hxx>
 #endif
-
-#ifndef _SVX_LANGBOX_HXX //autogen
 #include <svx/langbox.hxx>
-#endif
-
-#ifndef SVX_FONTLB_HXX //autogen
 #include "fontlb.hxx"
-#endif
 
 //CHINA001 #define SVX_NUMVAL_STANDARD      -1234.12345678901234
 //CHINA001 #define SVX_NUMVAL_CURRENCY      -1234
