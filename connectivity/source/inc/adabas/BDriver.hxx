aae698d45296f981e81ab7c0efd8776b6f824136
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:43:47 +0000

    INTEGRATION: CWS changefileheader (1.12.12); FILE MERGED 2008/04/01 15:09:08 thb 1.12.12.3: #i85898# Stripping all external header guards 2008/04/01 10:53:18 thb 1.12.12.2: #i85898# Stripping all external header guards 2008/03/28 15:24:07 rt 1.12.12.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/inc/adabas/BDriver.hxx
+++ connectivity/source/inc/adabas/BDriver.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: BDriver.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 15:26:28 $
+ * $RCSfile: BDriver.hxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,15 +40,9 @@
 #include <com/sun/star/lang/XEventListener.hpp>
 
 //#include <unotools/tempfile.hxx>
-#ifndef _CPPUHELPER_IMPLBASE4_HXX_
 #include <cppuhelper/implbase4.hxx>
-#endif
-#ifndef _CONNECTIVITY_OFUNCTIONDEFS_HXX_
 #include "odbc/OFunctiondefs.hxx"
-#endif
-#ifndef _CONNECTIVITY_ODBC_ODRIVER_HXX_
 #include "odbc/ODriver.hxx"
-#endif
 
 namespace connectivity
 {
