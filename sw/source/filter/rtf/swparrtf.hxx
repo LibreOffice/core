a1b2b76e31fa14ca0a84dd0a03d1670a4d9334bb
Rüdiger Timm <rt@openoffice.org>
2008-04-10 16:01:48 +0000

    INTEGRATION: CWS changefileheader (1.33.58); FILE MERGED 2008/04/01 15:57:50 thb 1.33.58.3: #i85898# Stripping all external header guards 2008/04/01 12:54:44 thb 1.33.58.2: #i85898# Stripping all external header guards 2008/03/31 16:55:46 rt 1.33.58.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/rtf/swparrtf.hxx
+++ sw/source/filter/rtf/swparrtf.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: swparrtf.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.33 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 14:19:17 $
+ * $RCSfile: swparrtf.hxx,v $
+ * $Revision: 1.34 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
@@ -51,37 +46,17 @@
 #define _SVSTDARR_BOOLS
 #include <svtools/svstdarr.hxx>
 #endif
-#ifndef _SVXRTF_HXX //autogen
 #include <svx/svxrtf.hxx>
-#endif
-#ifndef _SVX_NUMITEM_HXX
 #include <svx/numitem.hxx>
-#endif
-#ifndef _SVX_BOXITEM_HXX //autogen
 #include <svx/boxitem.hxx>
-#endif
-#ifndef _REDLINE_HXX
 #include <redline.hxx>
-#endif
 #ifndef __SGI_STL_ALGORITHM
 #include <algorithm>
 #endif
-
-#ifndef _FLTSHELL_HXX
 #include <fltshell.hxx>         // fuer den Attribut Stack
-#endif
-
-#ifndef _NDINDEX_HXX
 #include <ndindex.hxx>
-#endif
-
-#ifndef SW_MS_MSFILTER_HXX
 #include "../inc/msfilter.hxx"
-#endif
-
-#ifndef _SVDOBJ_HXX
 #include <svx/svdobj.hxx>
-#endif
 
 
 extern void GetLineIndex(SvxBoxItem &rBox, short nLineThickness, short nSpace, BYTE nCol, short nIdx,
