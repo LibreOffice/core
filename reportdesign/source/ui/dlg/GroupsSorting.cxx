0dba39644f883c701ef4af4cb0ca23fcf62dffe0
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:53:57 +0000

    INTEGRATION: CWS changefileheader (1.7.26); FILE MERGED 2008/04/01 15:23:40 thb 1.7.26.3: #i85898# Stripping all external header guards 2008/04/01 12:33:12 thb 1.7.26.2: #i85898# Stripping all external header guards 2008/03/31 13:32:15 rt 1.7.26.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/dlg/GroupsSorting.cxx
+++ reportdesign/source/ui/dlg/GroupsSorting.cxx
@@ -1,60 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: GroupsSorting.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 13:47:22 $
+ * $RCSfile: GroupsSorting.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPTUI_GROUPS_SORTING_HXX
 #include "GroupsSorting.hxx"
-#endif
 #ifndef RPTUI_GROUPSSORTING_HRC
 #include "GroupsSorting.hrc"
 #endif
-#ifndef _CONNECTIVITY_DBTOOLS_HXX_
 #include <connectivity/dbtools.hxx>
-#endif
-#ifndef _SVTOOLS_EDITBROWSEBOX_HXX_
 #include <svtools/editbrowsebox.hxx>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include <com/sun/star/beans/XPropertySet.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_DATATYPE_HPP_
 #include <com/sun/star/sdbc/DataType.hpp>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 #ifndef _RPTUI_DLGRESID_HRC
 #include "RptResId.hrc"
 #endif
@@ -67,21 +49,15 @@
 #ifndef RTPUI_REPORTDESIGN_HELPID_HRC
 #include "helpids.hrc"
 #endif
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
 #ifndef _GLOBLMN_HRC
 #include <svx/globlmn.hrc>
 #endif
 #ifndef _SBASLTID_HRC
 #include <svx/svxids.hrc>
 #endif
-#ifndef _COM_SUN_STAR_REPORT_GROUPON_HPP_
 #include <com/sun/star/report/GroupOn.hpp>
-#endif
-#ifndef RPTUI_GROUP_EXCHANGE_HXX
 #include "GroupExchange.hxx"
-#endif
 #ifndef RPTUI_TOOLS_HXX
 #include "UITools.hxx"
 #endif
@@ -89,21 +65,11 @@
 #ifndef REPORTDESIGN_SHARED_UISTRINGS_HRC
 #include "uistrings.hrc"
 #endif
-#ifndef RPTUI_REPORTCONTROLLER_HXX
 #include "ReportController.hxx"
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_XCONTAINERLISTENER_HPP_
 #include <com/sun/star/container/XContainerListener.hpp>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE1_HXX_
 #include <cppuhelper/implbase1.hxx>
-#endif
-#ifndef _COMPHELPER_PROPERTY_HXX_
 #include <comphelper/property.hxx>
-#endif
-#ifndef _SV_MNEMONIC_HXX
 #include <vcl/mnemonic.hxx>
-#endif
 #include <algorithm>
 #include <boost/bind.hpp>
 
