e908d406110a6a028b58a3ae7d2e5558000bc337
Rüdiger Timm <rt@openoffice.org>
2008-04-10 23:05:35 +0000

    INTEGRATION: CWS changefileheader (1.16.260); FILE MERGED 2008/04/01 15:50:33 thb 1.16.260.3: #i85898# Stripping all external header guards 2008/04/01 12:48:22 thb 1.16.260.2: #i85898# Stripping all external header guards 2008/03/31 14:20:05 rt 1.16.260.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/swpossizetabpage.cxx
+++ svx/source/dialog/swpossizetabpage.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: swpossizetabpage.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:25:10 $
+ * $RCSfile: swpossizetabpage.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,53 +36,23 @@
 #endif
 
 #include <cstddef>
-
-#ifndef _SVX_SWPOSSIZETABPAGE_HXX
 #include <swpossizetabpage.hxx>
-#endif
-#ifndef _SVX_DIALMGR_HXX
 #include <svx/dialmgr.hxx>
-#endif
-#ifndef _SVX_DLGUTIL_HXX
 #include <dlgutil.hxx>
-#endif
-#ifndef _SVX_ANCHORID_HXX
 #include <anchorid.hxx>
-#endif
-#ifndef _AEITEM_HXX
 #include <svtools/aeitem.hxx>
-#endif
-#ifndef _SVXSWFRAMEVALIDATION_HXX
 #include <swframevalidation.hxx>
-#endif
 #include <tools/debug.hxx>
 #include <svx/dialogs.hrc>
 #include <swpossizetabpage.hrc>
-#ifndef _SVX_HTMLMODE_HXX //autogen
 #include <htmlmode.hxx>
-#endif
-#ifndef _SVDVIEW_HXX //autogen
 #include <svx/svdview.hxx>
-#endif
-#ifndef _SVDPAGV_HXX
 #include <svx/svdpagv.hxx>
-#endif
-#ifndef _SVX_RECTENUM_HXX
 #include <svx/rectenum.hxx>
-#endif
-#ifndef _COM_SUN_STAR_TEXT_TEXTCONTENTANCHORTYPE_HPP_
 #include <com/sun/star/text/TextContentAnchorType.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_TEXT_HORIORIENTATION_HPP_
 #include <com/sun/star/text/HoriOrientation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_TEXT_VERTORIENTATION_HPP_
 #include <com/sun/star/text/VertOrientation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_TEXT_RELORIENTATION_HPP_
 #include <com/sun/star/text/RelOrientation.hpp>
-#endif
 
 using namespace ::com::sun::star::text;
 #define SwFPos SvxSwFramePosString
