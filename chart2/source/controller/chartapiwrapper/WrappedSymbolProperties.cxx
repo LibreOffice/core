5818f86a2da0f5940a00dac1512b23333efcfe13
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:38:12 +0000

    INTEGRATION: CWS changefileheader (1.6.20); FILE MERGED 2008/04/01 15:03:59 thb 1.6.20.3: #i85898# Stripping all external header guards 2008/04/01 10:50:18 thb 1.6.20.2: #i85898# Stripping all external header guards 2008/03/28 16:43:18 rt 1.6.20.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/chartapiwrapper/WrappedSymbolProperties.cxx
+++ chart2/source/controller/chartapiwrapper/WrappedSymbolProperties.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: WrappedSymbolProperties.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-06 16:20:21 $
+ * $RCSfile: WrappedSymbolProperties.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,26 +36,12 @@
 #include "macros.hxx"
 #include "FastPropertyIdRanges.hxx"
 #include "ChartTypeHelper.hxx"
-
-#ifndef _COM_SUN_STAR_CHART2_SYMBOL_HPP_
 #include <com/sun/star/chart2/Symbol.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART2_SYMBOLSTYLE_HPP_
 #include <com/sun/star/chart2/SymbolStyle.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_SIZE_HPP_
 #include <com/sun/star/awt/Size.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CHART_CHARTSYMBOLTYPE_HPP_
 #include <com/sun/star/chart/ChartSymbolType.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_LINESTYLE_HPP_
 #include <com/sun/star/drawing/LineStyle.hpp>
-#endif
 
 #include <com/sun/star/graphic/XGraphicProvider.hpp>
 
@@ -68,17 +49,10 @@
 #include <svx/unoprnms.hxx>
 
 // for Graphic
-#ifndef _SV_GRAPH_HXX
 #include <vcl/graph.hxx>
-#endif
 // for GraphicObject
-#ifndef _GRFMGR_HXX
 #include <goodies/grfmgr.hxx>
-#endif
-
-#ifndef _SV_OUTDEV_HXX
 #include <vcl/outdev.hxx>
-#endif
 
 #include <comphelper/processfactory.hxx>
 
