40c578f897306408986a928679d1f6d529fb5fb9
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:14:18 +0000

    INTEGRATION: CWS changefileheader (1.4.34); FILE MERGED 2008/04/01 15:30:58 thb 1.4.34.3: #i85898# Stripping all external header guards 2008/04/01 12:36:48 thb 1.4.34.2: #i85898# Stripping all external header guards 2008/03/31 17:15:46 rt 1.4.34.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/inc/parawin.hxx
+++ sc/source/ui/inc/parawin.hxx
@@ -1,67 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: parawin.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 11:21:01 $
+ * $RCSfile: parawin.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_PARAWIN_HXX
 #define SC_PARAWIN_HXX
 
-#ifndef SC_FUNCUTL_HXX
 #include "funcutl.hxx"
-#endif
-
-#ifndef SC_SCGLOB_HXX
 #include "global.hxx"       // ScAddress
-#endif
-
-#ifndef _STDCTRL_HXX //autogen
 #include <svtools/stdctrl.hxx>
-#endif
 #ifndef _LSTBOX_HXX //autogen
 #include <vcl/lstbox.hxx>
 #endif
-
-#ifndef _SV_GROUP_HXX //autogen
 #include <vcl/group.hxx>
-#endif
-
-#ifndef _SVEDIT_HXX //autogen
 #include <svtools/svmedit.hxx>
-#endif
-
-#ifndef _SV_TABPAGE_HXX //autogen
 #include <vcl/tabpage.hxx>
-#endif
 
 #ifndef _SVSTDARR_STRINGS
 
@@ -69,10 +47,7 @@
 #include <svtools/svstdarr.hxx>
 
 #endif
-
-#ifndef _SV_TABCTRL_HXX //autogen
 #include <vcl/tabctrl.hxx>
-#endif
 
 #include <vector>
 
