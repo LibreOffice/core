84a5d05e712d1c7d585a63b6138bd8b9312bae4b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 15:26:19 +0000

    INTEGRATION: CWS changefileheader (1.7.60); FILE MERGED 2008/04/01 16:03:50 thb 1.7.60.3: #i85898# Stripping all external header guards 2008/04/01 13:00:08 thb 1.7.60.2: #i85898# Stripping all external header guards 2008/03/31 15:33:12 rt 1.7.60.1: #i87441# Change license header to LPGL v3.

--- uui/source/logindlg.cxx
+++ uui/source/logindlg.cxx
@@ -1,48 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: logindlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 08:19:15 $
+ * $RCSfile: logindlg.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
-#ifndef _SVT_FILEDLG_HXX
 #include <svtools/filedlg.hxx>
-#endif
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
-
-#ifndef UUI_LOGINDLG_HXX
 #include "logindlg.hxx"
-#endif
 
 #ifndef UUI_LOGINDLG_HRC
 #include "logindlg.hrc"
@@ -50,10 +38,7 @@
 #ifndef UUI_IDS_HRC
 #include "ids.hrc"
 #endif
-
-#ifndef _TOOLS_RESID_HXX
 #include <tools/resid.hxx>
-#endif
 
 #ifdef UNX
 #include <limits.h>
