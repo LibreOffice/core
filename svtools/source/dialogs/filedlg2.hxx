de47bb13e650c42e4ef97f4b458474caaf51b66f
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:32:56 +0000

    INTEGRATION: CWS changefileheader (1.4.546); FILE MERGED 2008/04/01 15:45:00 thb 1.4.546.2: #i85898# Stripping all external header guards 2008/03/31 13:01:40 rt 1.4.546.1: #i87441# Change license header to LPGL v3.

--- svtools/source/dialogs/filedlg2.hxx
+++ svtools/source/dialogs/filedlg2.hxx
@@ -1,53 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: filedlg2.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 20:59:47 $
+ * $RCSfile: filedlg2.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _FILEDLG2_HXX
 #define _FILEDLG2_HXX
 
-#ifndef _TOOLS_DEBUG_HXX //autogen
 #include <tools/debug.hxx>
-#endif
-#ifndef _FSYS_HXX //autogen
 #include <tools/fsys.hxx>
-#endif
 #ifndef _SV_BUTTON_HXX //autogen wg. PushButton
 #include <vcl/button.hxx>
 #endif
-#ifndef _VCL_UNOHELP_HXX
 #include <vcl/unohelp.hxx>
-#endif
 
 class FixedText;
 class Edit;
@@ -72,10 +61,7 @@ struct ImpFilterItem
 };
 
 DECLARE_LIST( ImpFilterList, ImpFilterItem* )
-
-#ifndef _SV_LSTBOX_HXX
 #include <vcl/lstbox.hxx>
-#endif // _SV_LSTBOX_HXX
 
 class KbdListBox : public ListBox
 {
