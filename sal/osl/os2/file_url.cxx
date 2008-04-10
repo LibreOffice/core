19ebe1d9812d4f47cb526848f63b424109cfbe15
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:55:16 +0000

    INTEGRATION: CWS changefileheader (1.3.90); FILE MERGED 2008/04/01 12:34:07 thb 1.3.90.2: #i85898# Stripping all external header guards 2008/03/31 13:23:41 rt 1.3.90.1: #i87441# Change license header to LPGL v3.

--- sal/osl/os2/file_url.cxx
+++ sal/osl/os2/file_url.cxx
@@ -1,43 +1,35 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: file_url.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-09-25 09:48:07 $
+ * $RCSfile: file_url.cxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #include <ctype.h>
-
-#ifndef __OSL_SYSTEM_H__
 #include "system.h"
-#endif
 
 #ifndef _LIMITS_H
 #include <limits.h>
@@ -58,58 +50,25 @@
 #ifndef _UNISTD_H
 #include <unistd.h>
 #endif
-
-#ifndef _OSL_FILE_H_
 #include <osl/file.h>
-#endif
-
-#ifndef _OSL_SECURITY_H_
 #include <osl/security.h>
-#endif
-
-#ifndef _RTL_URI_H_
 #include <rtl/uri.h>
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
-
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
-
-#ifndef _RTL_USTRBUF_H_
 #include <rtl/ustrbuf.h>
-#endif
 
 #ifndef _OSL_TREAD_H_
 #include <osl/thread.h>
 #endif
-
-#ifndef _OSL_FILE_HXX_
 #include <osl/file.hxx>
-#endif
-
-#ifndef _OSL_MUTEX_H_
 #include <osl/mutex.h>
-#endif
-
-#ifndef _OSL_PROCESS_H_
 #include <osl/process.h>
-#endif
-
-#ifndef _FILE_ERROR_TRANSL_H_
 #include "file_error_transl.h"
-#endif
 
 #ifndef _FILE_URL_H_
 #include "file_url.h"
 #endif
-
-#ifndef _OSL_FILE_PATH_HELPER_HXX_
 #include "file_path_helper.hxx"
-#endif
 
 #ifndef _OSL_UUNXAPI_HXX_
 #include "uunxapi.hxx"
