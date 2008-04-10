693374b682f593dbb8276d9cbb4d8434be987eab
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:07:52 +0000

    INTEGRATION: CWS changefileheader (1.16.182); FILE MERGED 2008/04/01 15:08:47 thb 1.16.182.3: #i85898# Stripping all external header guards 2008/04/01 10:53:00 thb 1.16.182.2: #i85898# Stripping all external header guards 2008/03/28 15:23:35 rt 1.16.182.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/flat/EConnection.cxx
+++ connectivity/source/drivers/flat/EConnection.cxx
@@ -1,71 +1,49 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: EConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 16:17:49 $
+ * $RCSfile: EConnection.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-
-#ifndef _CONNECTIVITY_FLAT_ECONNECTION_HXX_
 #include "flat/EConnection.hxx"
-#endif
-#ifndef _CONNECTIVITY_FLAT_EDATABASEMETADATA_HXX_
 #include "flat/EDatabaseMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_FLAT_CATALOG_HXX_
 #include "flat/ECatalog.hxx"
-#endif
 #ifndef _CONNECTIVITY_FLAT_ODRIVER_HXX_
 #include "flat/EDriver.hxx"
 #endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _URLOBJ_HXX //autogen wg. INetURLObject
 #include <tools/urlobj.hxx>
-#endif
-#ifndef _CONNECTIVITY_FLAT_DPREPAREDSTATEMENT_HXX_
 #include "flat/EPreparedStatement.hxx"
-#endif
 #ifndef _CONNECTIVITY_FLAT_DSTATEMENT_HXX_
 #include "flat/EStatement.hxx"
 #endif
-#ifndef _COMPHELPER_EXTRACT_HXX_
 #include <comphelper/extract.hxx>
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
-#endif
 
 using namespace connectivity::flat;
 using namespace connectivity::file;
