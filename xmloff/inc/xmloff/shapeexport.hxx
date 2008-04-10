65c825a81cfcf8d00710939fdfc13c88a40a22dd
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:06:18 +0000

    INTEGRATION: CWS changefileheader (1.4.18); FILE MERGED 2008/04/01 16:09:22 thb 1.4.18.3: #i85898# Stripping all external header guards 2008/04/01 13:04:27 thb 1.4.18.2: #i85898# Stripping all external header guards 2008/03/31 16:27:57 rt 1.4.18.1: #i87441# Change license header to LPGL v3.

--- xmloff/inc/xmloff/shapeexport.hxx
+++ xmloff/inc/xmloff/shapeexport.hxx
@@ -1,59 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: shapeexport.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 10:22:47 $
+ * $RCSfile: shapeexport.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _XMLOFF_SHAPEEXPORT_HXX_
 #define _XMLOFF_SHAPEEXPORT_HXX_
 
-#ifndef _SAL_CONFIG_H_
 #include "sal/config.h"
-#endif
-
-#ifndef INCLUDED_XMLOFF_DLLAPI_H
 #include "xmloff/dllapi.h"
-#endif
-
-#ifndef _SAL_TYPES_H_
 #include "sal/types.h"
-#endif
 
 #include <rtl/ref.hxx>
 #include <rtl/ustring.hxx>
 #include <rtl/ustrbuf.hxx>
-
-#ifndef _UNIVERSALL_REFERENCE_HXX
 #include <xmloff/uniref.hxx>
-#endif
 
 #include <com/sun/star/drawing/XShape.hpp>
 #include <com/sun/star/drawing/XShapes.hpp>
@@ -61,22 +45,10 @@
 #include <com/sun/star/beans/PropertyValue.hpp>
 
 #include <map>
-
-#ifndef _XMLOFF_PROPERTYSETMAPPER_HXX
 #include <xmloff/xmlprmap.hxx>
-#endif
-
-#ifndef _XMLOFF_XMLEXPPR_HXX
 #include <xmloff/xmlexppr.hxx>
-#endif
-
-#ifndef _XMLOFF_ANIMEXP_HXX
 #include <xmloff/animexp.hxx>
-#endif
-
-#ifndef _XMLOFF_FAMILIES_HXX_
 #include <xmloff/families.hxx>
-#endif
 
 #include "xmloff/table/XMLTableExport.hxx"
 
