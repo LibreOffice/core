b450c6e59fcb6ea20572f620aebc89ed7a76603f
Rüdiger Timm <rt@openoffice.org>
2008-04-10 15:31:38 +0000

    INTEGRATION: CWS changefileheader (1.16.46); FILE MERGED 2008/04/01 15:22:21 thb 1.16.46.3: #i85898# Stripping all external header guards 2008/04/01 12:32:11 thb 1.16.46.2: #i85898# Stripping all external header guards 2008/03/31 16:19:06 rt 1.16.46.1: #i87441# Change license header to LPGL v3.

--- package/source/manifest/ManifestExport.cxx
+++ package/source/manifest/ManifestExport.cxx
@@ -1,59 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ManifestExport.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2006-11-21 17:51:57 $
+ * $RCSfile: ManifestExport.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_package.hxx"
-
-#ifndef _MANIFEST_EXPORT_HXX
 #include <ManifestExport.hxx>
-#endif
-#ifndef _MANIFEST_DEFINES_HXX
 #include <ManifestDefines.hxx>
-#endif
 #ifndef _COM_SUN_STAR_XML_SAX_XATTRIBUTELIST_HXX
 #include <com/sun/star/xml/sax/XAttributeList.hpp>
 #endif
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
 #ifndef _BASE64_CODEC_HXX_
 #include <Base64Codec.hxx>
 #endif
-#ifndef _COM_SUN_STAR_XML_SAX_XEXTENDEDDOCUMENTHANDLER_HPP_
 #include <com/sun/star/xml/sax/XExtendedDocumentHandler.hpp>
-#endif
 #ifndef _COM_SUN_STAR_XML_SAX_XDOCUMENTHANDLER_HXX
 #include <com/sun/star/xml/sax/XDocumentHandler.hpp>
 #endif
