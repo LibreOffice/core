a742fd71cf9c79dca24b9dfaeacf6b9716096f3f
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:44:08 +0000

    INTEGRATION: CWS changefileheader (1.21.246); FILE MERGED 2008/04/01 15:44:24 thb 1.21.246.3: #i85898# Stripping all external header guards 2008/04/01 12:43:09 thb 1.21.246.2: #i85898# Stripping all external header guards 2008/03/31 13:00:56 rt 1.21.246.1: #i87441# Change license header to LPGL v3.

--- svtools/inc/svimpbox.hxx
+++ svtools/inc/svimpbox.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svimpbox.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.21 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 14:38:25 $
+ * $RCSfile: svimpbox.hxx,v $
+ * $Revision: 1.22 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -42,19 +37,12 @@
 #ifndef _SCRBAR_HXX
 #include <vcl/scrbar.hxx>
 #endif
-#ifndef _VCL_VCLEVENT_HXX
 #include <vcl/vclevent.hxx>
-#endif
 // #102891# ----------------
-#ifndef _UNOTOOLS_INTLWRAPPER_HXX
 #include <unotools/intlwrapper.hxx>
-#endif
 // #97680# -----------------
 #include <vector>
-
-#ifndef SVTOOLS_ACCESSIBLE_FACTORY_ACCESS_HXX
 #include "svtaccessiblefactory.hxx"
-#endif
 
 class SvTreeListBox;
 class Point;
