27471fced819f2bf710dba4d4cbeda891218d884
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:47:12 +0000

    INTEGRATION: CWS changefileheader (1.23.122); FILE MERGED 2008/04/01 16:10:17 thb 1.23.122.3: #i85898# Stripping all external header guards 2008/04/01 13:05:47 thb 1.23.122.2: #i85898# Stripping all external header guards 2008/03/31 16:28:51 rt 1.23.122.1: #i87441# Change license header to LPGL v3.

--- xmloff/source/transform/Oasis2OOo.cxx
+++ xmloff/source/transform/Oasis2OOo.cxx
@@ -1,116 +1,64 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: Oasis2OOo.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.23 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-08-20 16:32:44 $
+ * $RCSfile: Oasis2OOo.cxx,v $
+ * $Revision: 1.24 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_xmloff.hxx"
-
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSETINFO_HPP_
 #include <com/sun/star/beans/XPropertySetInfo.hpp>
-#endif
-#ifndef _RTL_UUID_H_
 #include <rtl/uuid.h>
-#endif
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-#ifndef _XMLOFF_NMSPMAP_HXX
 #include <xmloff/nmspmap.hxx>
-#endif
-#ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
-#endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
 #include <xmloff/xmltoken.hxx>
-#endif
-#ifndef _XMLOFF_DEEPTCONTEXT_HXX
 #include "DeepTContext.hxx"
-#endif
-#ifndef _XMLOFF_METATCONTEXT_HXX
 #include "MetaTContext.hxx"
-#endif
-#ifndef _XMLOFF_DOCUMENTTCONTEXT_HXX
 #include "DocumentTContext.hxx"
-#endif
 #ifndef _XMLOFF_NOTESCONTEXT_HXX
 #include "NotesTContext.hxx"
 #endif
-#ifndef _XMLOFF_STYLEOASISTCONTEXT_HXX
 #include "StyleOASISTContext.hxx"
-#endif
-#ifndef _XMLOFF_FRAMEOASISTCONTEXT_HXX
 #include "FrameOASISTContext.hxx"
-#endif
-#ifndef _XMLOFF_EVENTOASISTCONTEXT_HXX
 #include "EventOASISTContext.hxx"
-#endif
-#ifndef _XMLOFF_DLGOASISTCONTEXT_HXX
 #include "DlgOASISTContext.hxx"
-#endif
 #ifndef _XMLOFF_CONTROLOASISTCONTEXT_HXX
 #include "ControlOASISTContext.hxx"
 #endif
-#ifndef _XMLOFF_FORMPROPOASISTCONTEXT_HXX
 #include "FormPropOASISTContext.hxx"
-#endif
-#ifndef _XMLOFF_CHARTOASISTCONTEXT_HXX
 #include "ChartOASISTContext.hxx"
-#endif
-#ifndef XMLOFF_XMLCHARTPLOTAREAOASISTCONTEXT_HXX
 #include "ChartPlotAreaOASISTContext.hxx"
-#endif
-#ifndef _XMLOFF_MUTABLEATTRLIST_HXX
 #include "MutableAttrList.hxx"
-#endif
-#ifndef _XMLOFF_ELEMTRANSFORMERACTION_HXX
 #include "ElemTransformerAction.hxx"
-#endif
-#ifndef _XMLOFF_ATTRTRANSFORMERACTION_HXX
 #include "AttrTransformerAction.hxx"
-#endif
-#ifndef _XMLOFF_TRANSFORMERACTIONS_HXX
 #include "TransformerActions.hxx"
-#endif
-#ifndef _XMLOFF_FAMILYTYPE_HXX
 #include "FamilyType.hxx"
-#endif
-#ifndef _XMLOFF_XMLUCONV_HXX
 #include <xmloff/xmluconv.hxx>
-#endif
-#ifndef _XMLOFF_OASIS2OOO_HXX
 #include "Oasis2OOo.hxx"
-#endif
 
 using ::rtl::OUString;
 using ::rtl::OUStringBuffer;
