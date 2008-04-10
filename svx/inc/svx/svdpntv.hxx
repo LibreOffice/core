e2c061bb1c15f3aacfa116a342fd551e6e9e3b45
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:24:49 +0000

    INTEGRATION: CWS changefileheader (1.6.118); FILE MERGED 2008/04/01 15:49:39 thb 1.6.118.3: #i85898# Stripping all external header guards 2008/04/01 12:46:56 thb 1.6.118.2: #i85898# Stripping all external header guards 2008/03/31 14:18:18 rt 1.6.118.1: #i87441# Change license header to LPGL v3.

--- svx/inc/svx/svdpntv.hxx
+++ svx/inc/svx/svdpntv.hxx
@@ -1,92 +1,49 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdpntv.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 14:04:28 $
+ * $RCSfile: svdpntv.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SVDPNTV_HXX
 #define _SVDPNTV_HXX
 
-#ifndef _SFXBRDCST_HXX
 #include <svtools/brdcst.hxx>
-#endif
-
-#ifndef _SFXLSTNER_HXX //autogen
 #include <svtools/lstner.hxx>
-#endif
-
-#ifndef _SFXSMPLHINT_HXX
 #include <svtools/smplhint.hxx>
-#endif
-
-#ifndef _UNDO_HXX //autogen
 #include <svtools/undo.hxx>
-#endif
-
-#ifndef _SVDDRAG_HXX
 #include <svx/svddrag.hxx>
-#endif
-
-#ifndef _SVDLAYER_HXX
 #include <svx/svdlayer.hxx>  // fuer SetOfByte
-#endif
-
-#ifndef _WINDOW_HXX //autogen
 #include <vcl/window.hxx>
-#endif
-
-#ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_XCONTROLCONTAINER_HPP_
 #include <com/sun/star/awt/XControlContainer.hpp>
-#endif
-
-#ifndef _SFXITEMSET_HXX
 #include <svtools/itemset.hxx>
-#endif
-
-#ifndef _TIMER_HXX //autogen
 #include <vcl/timer.hxx>
-#endif
-
-#ifndef INCLUDED_SVXDLLAPI_H
 #include "svx/svxdllapi.h"
-#endif
-
-#ifndef INCLUDED_SVTOOLS_OPTIONSDRAWINGLAYER_HXX
 #include <svtools/optionsdrawinglayer.hxx>
-#endif
 
 //************************************************************
 //   Pre-Defines
