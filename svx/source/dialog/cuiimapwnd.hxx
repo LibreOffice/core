cde8c13396d1024a49302b4096c0f4051cef6296
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:16:59 +0000

    INTEGRATION: CWS changefileheader (1.4.368); FILE MERGED 2008/04/01 15:50:17 thb 1.4.368.1: #i85898# Stripping all external header guards

--- svx/source/dialog/cuiimapwnd.hxx
+++ svx/source/dialog/cuiimapwnd.hxx
@@ -1,33 +1,17 @@
 #ifndef _CUI_IMAPWND_HXX
 #define _CUI_IMAPWND_HXX
 
-#ifndef _SV_DIALOG_HXX //autogen
 #include <vcl/dialog.hxx>
-#endif
-#ifndef _SV_FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
-#endif
 #ifndef _SV_BUTTON_HXX //autogen
 #include <vcl/button.hxx>
 #endif
-#ifndef _SV_MENU_HXX //autogen
 #include <vcl/menu.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX //autogen
 #include <svtools/itempool.hxx>
-#endif
-#ifndef _GOODIES_IMAPOBJ_HXX //autogen
 #include <svtools/imapobj.hxx>
-#endif
-#ifndef _TRANSFER_HXX //autogen
 #include <svtools/transfer.hxx>
-#endif
-#ifndef _IMAP_HXX //autogen
 #include <svtools/imap.hxx>
-#endif
-#ifndef _SFXFRAME_HXX
 #include <sfx2/frame.hxx>
-#endif
 #include <svtools/svmedit.hxx>
 
 
