b726e1930d49030f6ec8eedcb83ad877f978beec
Rüdiger Timm <rt@openoffice.org>
2008-04-10 14:42:08 +0000

    INTEGRATION: CWS changefileheader (1.12.152); FILE MERGED 2008/04/01 16:02:27 thb 1.12.152.2: #i85898# Stripping all external header guards 2008/03/31 15:30:31 rt 1.12.152.1: #i87441# Change license header to LPGL v3.

--- ucb/source/ucp/webdav/NeonTypes.hxx
+++ ucb/source/ucp/webdav/NeonTypes.hxx
@@ -1,53 +1,40 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: NeonTypes.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-01-31 18:17:30 $
+ * $RCSfile: NeonTypes.hxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _NEONTYPES_HXX_
 #define _NEONTYPES_HXX_
 
-#ifndef NE_SESSION_H
 #include <ne_session.h>
-#endif
-#ifndef NE_UTILS_H
 #include <ne_utils.h>
-#endif
-#ifndef NE_BASIC_H
 #include <ne_basic.h>
-#endif
-#ifndef NE_PROPS_H
 #include <ne_props.h>
-#endif
 
 typedef ne_session                  HttpSession;
 typedef ne_status                   HttpStatus;
