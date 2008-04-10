b83eb091a25ac9b8221f89fa578bf010753c6178
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:21:04 +0000

    INTEGRATION: CWS changefileheader (1.16.182); FILE MERGED 2008/04/01 10:53:14 thb 1.16.182.2: #i85898# Stripping all external header guards 2008/03/28 15:23:59 rt 1.16.182.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/mozab/mozillasrc/MQueryHelper.cxx
+++ connectivity/source/drivers/mozab/mozillasrc/MQueryHelper.cxx
@@ -1,58 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MQueryHelper.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 16:21:11 $
+ * $RCSfile: MQueryHelper.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-
-#ifndef _CONNECTIVITY_MAB_QUERYHELPER_HXX_
 #include <MQueryHelper.hxx>
-#endif
 
 #include "MTypeConverter.hxx"
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
-#endif
 #ifndef _CONNECTIVITY_MOZAB_BCONNECTION_HXX_
 #include <MConnection.hxx>
 #endif
 
 #include "resource/mozab_res.hrc"
-
-#ifndef _CONNECTIVITY_MAB_NS_DECLARES_HXX_
 #include "MNSDeclares.hxx"
-#endif
 
 using namespace connectivity::mozab;
 
