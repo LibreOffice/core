a687867599cb20b3aa4a99ea4848c3cc9a6d8bcd
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:41:17 +0000

    INTEGRATION: CWS changefileheader (1.10.700); FILE MERGED 2008/04/01 15:30:49 thb 1.10.700.3: #i85898# Stripping all external header guards 2008/04/01 12:36:44 thb 1.10.700.2: #i85898# Stripping all external header guards 2008/03/31 17:15:39 rt 1.10.700.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/inc/asciiopt.hxx
+++ sc/source/ui/inc/asciiopt.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: asciiopt.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.10 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 21:12:21 $
+ * $RCSfile: asciiopt.hxx,v $
+ * $Revision: 1.11 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,9 +33,7 @@
 #ifndef SC_ASCIIOPT_HXX
 #define SC_ASCIIOPT_HXX
 
-#ifndef _STRING_HXX //autogen
 #include <tools/string.hxx>
-#endif
 #ifndef _DIALOG_HXX //autogen
 #include <vcl/dialog.hxx>
 #endif
@@ -59,16 +52,9 @@
 #ifndef _FIELD_HXX //autogen
 #include <vcl/field.hxx>
 #endif
-#ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
-#endif
-#ifndef _SVX_TXENCBOX_HXX
 #include <svx/txencbox.hxx>
-#endif
-
-#ifndef _SC_CSVTABLEBOX_HXX
 #include "csvtablebox.hxx"
-#endif
 
 
 // ============================================================================
