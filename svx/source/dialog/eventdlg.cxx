3e260c7764903a0730abd985fb575cb54779c8ca
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:28:32 +0000

    INTEGRATION: CWS changefileheader (1.15.236); FILE MERGED 2008/04/01 15:50:19 thb 1.15.236.3: #i85898# Stripping all external header guards 2008/04/01 12:48:11 thb 1.15.236.2: #i85898# Stripping all external header guards 2008/03/31 14:19:24 rt 1.15.236.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/eventdlg.cxx
+++ svx/source/dialog/eventdlg.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: eventdlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.15 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2007-10-09 15:17:38 $
+ * $RCSfile: eventdlg.cxx,v $
+ * $Revision: 1.16 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,20 +32,10 @@
 #include "precompiled_svx.hxx"
 
 // include ***************************************************************
-
-#ifndef _SVEDIT_HXX //autogen
 #include <svtools/svmedit.hxx>
-#endif
-#ifndef TOOLS_DIAGNOSE_EX_H
 #include <tools/diagnose_ex.h>
-#endif
-
-#ifndef  _COM_SUN_STAR_DOCUMENT_XEVENTSSUPPLIER_HPP_
 #include <com/sun/star/document/XEventsSupplier.hpp>
-#endif
-#ifndef  _COM_SUN_STAR_FRAME_XMODULEMANAGER_HPP_
 #include <com/sun/star/frame/XModuleManager.hpp>
-#endif
 
 #include <comphelper/processfactory.hxx>
 #include <comphelper/documentinfo.hxx>
@@ -69,12 +54,8 @@
 #include <sfx2/fcontnr.hxx>
 #include <svtools/eventcfg.hxx>
 
-#ifndef _HEADERTABLISTBOX_HXX
 #include "headertablistbox.hxx"
-#endif
-#ifndef _MACROPG_IMPL_HXX
 #include "macropg_impl.hxx"
-#endif
 
 #include <svx/dialmgr.hxx>
 #include <svx/dialogs.hrc>
