7e35ae247cb0dc6bf407004772c9449603b03c5c
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:04:34 +0000

    INTEGRATION: CWS changefileheader (1.90.146); FILE MERGED 2008/04/01 15:30:04 thb 1.90.146.3: #i85898# Stripping all external header guards 2008/04/01 12:36:08 thb 1.90.146.2: #i85898# Stripping all external header guards 2008/03/31 17:14:28 rt 1.90.146.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/excel/biffdump.cxx
+++ sc/source/filter/excel/biffdump.cxx
@@ -1,86 +1,54 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: biffdump.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.90 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-11-01 16:23:57 $
+ * $RCSfile: biffdump.cxx,v $
+ * $Revision: 1.91 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sc.hxx"
-
-#ifndef SC_BIFFDUMP_HXX
 #include "biffdump.hxx"
-#endif
 
 #if EXC_INCL_DUMPER
-
-#ifndef _STREAM_HXX
 #include <tools/stream.hxx>
-#endif
-#ifndef _SFXDOCFILE_HXX
 #include <sfx2/docfile.hxx>
-#endif
-#ifndef _SFX_OBJSH_HXX
 #include <sfx2/objsh.hxx>
-#endif
-#ifndef _SOT_STORINFO_HXX
 #include <sot/storinfo.hxx>
-#endif
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <rtl/math.hxx>
-
-#ifndef SC_DOCUMENT_HXX
 #include "document.hxx"
-#endif
-#ifndef SC_SCGLOB_HXX
 #include "global.hxx"
-#endif
-
-#ifndef SC_FPROGRESSBAR_HXX
 #include "fprogressbar.hxx"
-#endif
-#ifndef SC_XLPIVOT_HXX
 #include "xlpivot.hxx"
-#endif
-#ifndef SC_XICONTENT_HXX
 #include "xicontent.hxx"
-#endif
-
-#ifndef _IMP_OP_HXX
 #include "imp_op.hxx"
-#endif
 
 #define GETSTR(s)       ByteString( s, RTL_TEXTENCODING_MS_1252 )
 
