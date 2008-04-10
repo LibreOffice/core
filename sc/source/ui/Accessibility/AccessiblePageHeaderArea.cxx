87d02a2579e2fa3fd963e881964abc4c253a65e5
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:42:36 +0000

    INTEGRATION: CWS changefileheader (1.18.388); FILE MERGED 2008/04/01 15:30:35 thb 1.18.388.3: #i85898# Stripping all external header guards 2008/04/01 12:36:37 thb 1.18.388.2: #i85898# Stripping all external header guards 2008/03/31 17:15:06 rt 1.18.388.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/Accessibility/AccessiblePageHeaderArea.cxx
+++ sc/source/ui/Accessibility/AccessiblePageHeaderArea.cxx
@@ -1,65 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: AccessiblePageHeaderArea.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.18 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2006-12-19 13:26:22 $
+ * $RCSfile: AccessiblePageHeaderArea.cxx,v $
+ * $Revision: 1.19 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sc.hxx"
-
-#ifndef _SV_GEN_HXX
 #include <tools/gen.hxx>
-#endif
-
-#ifndef _SC_ACCESSIBLEPAGEHEADERAREA_HXX
 #include "AccessiblePageHeaderArea.hxx"
-#endif
-#ifndef _SC_ACCESSIBLETEXT_HXX
 #include "AccessibleText.hxx"
-#endif
-#ifndef SC_ACCESSIBILITYHINTS_HXX
 #include "AccessibilityHints.hxx"
-#endif
-#ifndef SC_UNOGUARD_HXX
 #include "unoguard.hxx"
-#endif
-#ifndef SC_EDITSRC_HXX
 #include "editsrc.hxx"
-#endif
 #include "prevwsh.hxx"
 #include "prevloc.hxx"
-#ifndef SC_SCRESID_HXX
 #include "scresid.hxx"
-#endif
 #ifndef SC_SC_HRC
 #include "sc.hrc"
 #endif
@@ -70,28 +49,13 @@
 #ifndef _COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLESTATETYPE_HPP_
 #include <com/sun/star/accessibility/AccessibleStateType.hpp>
 #endif
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEEVENTID_HPP_
 #include <com/sun/star/accessibility/AccessibleEventId.hpp>
-#endif
-
-#ifndef _EDITOBJ_HXX
 #include <svx/editobj.hxx>
-#endif
-#ifndef _SVX_ACCESSILE_TEXT_HELPER_HXX_
 #include <svx/AccessibleTextHelper.hxx>
-#endif
-#ifndef _RTL_UUID_H_
 #include <rtl/uuid.h>
-#endif
-#ifndef _UTL_ACCESSIBLESTATESETHELPER_HXX_
 #include <unotools/accessiblestatesethelper.hxx>
-#endif
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-#ifndef _TOOLKIT_HELPER_CONVERT_HXX_
 #include <toolkit/helper/convert.hxx>
-#endif
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
