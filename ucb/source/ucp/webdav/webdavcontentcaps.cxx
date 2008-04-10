5b37a3e53f2ba5de3ce6dda4f9c5763e3f610e35
Rüdiger Timm <rt@openoffice.org>
2008-04-10 14:44:39 +0000

    INTEGRATION: CWS changefileheader (1.20.72); FILE MERGED 2008/04/01 16:02:28 thb 1.20.72.3: #i85898# Stripping all external header guards 2008/04/01 12:58:28 thb 1.20.72.2: #i85898# Stripping all external header guards 2008/03/31 15:30:32 rt 1.20.72.1: #i87441# Change license header to LPGL v3.

--- ucb/source/ucp/webdav/webdavcontentcaps.cxx
+++ ucb/source/ucp/webdav/webdavcontentcaps.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: webdavcontentcaps.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.20 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-06-05 18:21:21 $
+ * $RCSfile: webdavcontentcaps.cxx,v $
+ * $Revision: 1.21 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -42,59 +37,29 @@
  *************************************************************************/
 
 #include <set>
-
-#ifndef _COM_SUN_STAR_BEANS_PROPERTY_HPP_
 #include <com/sun/star/beans/Property.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
 #include <com/sun/star/beans/PropertyValue.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_COMMANDINFO_HPP_
 #include <com/sun/star/ucb/CommandInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_OPENCOMMANDARGUMENT2_HPP_
 #include <com/sun/star/ucb/OpenCommandArgument2.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_INSERTCOMMANDARGUMENT_HPP_
 #include <com/sun/star/ucb/InsertCommandArgument.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_POSTCOMMANDARGUMENT2_HPP_
 #include <com/sun/star/ucb/PostCommandArgument2.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_TRANSFERINFO_HPP_
 #include <com/sun/star/ucb/TransferInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
-#endif
 #ifndef _COM_SUN_STAR_UTIL_DATETIME_HXX_
 #include <com/sun/star/util/DateTime.hpp>
 #endif
-#ifndef _COM_SUN_STAR_UCB_LINK_HPP_
 #include <com/sun/star/ucb/Link.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_LOCK_HPP_
 #include <com/sun/star/ucb/Lock.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UCB_LOCKENTRY_HPP_
 #include <com/sun/star/ucb/LockEntry.hpp>
-#endif
-
-#ifndef _WEBDAV_UCP_CONTENT_HXX
 #include "webdavcontent.hxx"
-#endif
 #ifndef _WEBDAV_UCP_PROVIDFER_HXX
 #include "webdavprovider.hxx"
 #endif
 #ifndef _WEBDAV_SESSION_HXX
 #include "DAVSession.hxx"
 #endif
-#ifndef _WEBDAV_UCP_CONTENTPROPERTIES_HXX
 #include "ContentProperties.hxx"
-#endif
 
 using namespace com::sun::star;
 using namespace webdav_ucp;
