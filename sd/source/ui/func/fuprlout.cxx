2015f3ab29660df48249d6cb416ded65dab643b8
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:24:19 +0000

    INTEGRATION: CWS changefileheader (1.15.14); FILE MERGED 2008/04/01 15:34:42 thb 1.15.14.3: #i85898# Stripping all external header guards 2008/04/01 12:38:55 thb 1.15.14.2: #i85898# Stripping all external header guards 2008/03/31 13:58:06 rt 1.15.14.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/func/fuprlout.cxx
+++ sd/source/ui/func/fuprlout.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: fuprlout.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-02 09:47:35 $
+ * $RCSfile: fuprlout.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,26 +33,13 @@
 
 
 #include "fuprlout.hxx"
-
-#ifndef _SV_WRKWIN_HXX
 #include <vcl/wrkwin.hxx>
-#endif
-#ifndef _SFXDISPATCH_HXX //autogen
 #include <sfx2/dispatch.hxx>
-#endif
-#ifndef _SFXSMPLHINT_HXX //autogen
 #include <svtools/smplhint.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX //autogen
 #include <svtools/itempool.hxx>
-#endif
 #include <sot/storage.hxx>
-#ifndef _SV_MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SVDUNDO_HXX //autogen
 #include <svx/svdundo.hxx>
-#endif
 
 #include <sfx2/viewfrm.hxx>
 #include <sfx2/request.hxx>
@@ -65,16 +47,12 @@
 #include "drawdoc.hxx"
 #include "sdpage.hxx"
 #include "pres.hxx"
-#ifndef SD_DRAW_VIEW_SHELL_HXX
 #include "DrawViewShell.hxx"
-#endif
 #ifndef SD_FRAMW_VIEW_HXX
 #include "FrameView.hxx"
 #endif
 #include "stlpool.hxx"
-#ifndef SD_VIEW_HXX
 #include "View.hxx"
-#endif
 #include "glob.hrc"
 #include "glob.hxx"
 #include "strings.hrc"
@@ -86,9 +64,7 @@
 #include "unmovss.hxx"
 #include "sdattr.hxx"
 #include "sdresid.hxx"
-#ifndef SD_DRAW_VIEW_HXX
 #include "drawview.hxx"
-#endif
 #include "eetext.hxx"
 #include <svx/editdata.hxx>
 #include "sdabstdlg.hxx"
