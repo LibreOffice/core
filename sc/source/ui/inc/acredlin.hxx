e5679b0dcdd8b033cac602878c75850d714b5251
Rüdiger Timm <rt@openoffice.org>
2008-04-10 21:40:16 +0000

    INTEGRATION: CWS changefileheader (1.7.618); FILE MERGED 2008/04/01 15:30:49 thb 1.7.618.3: #i85898# Stripping all external header guards 2008/04/01 12:36:43 thb 1.7.618.2: #i85898# Stripping all external header guards 2008/03/31 17:15:38 rt 1.7.618.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/inc/acredlin.hxx
+++ sc/source/ui/inc/acredlin.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: acredlin.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-02-06 13:05:09 $
+ * $RCSfile: acredlin.hxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,52 +40,26 @@
 #ifndef _GROUP_HXX //autogen
 #include <vcl/group.hxx>
 #endif
-
-#ifndef _HEADBAR_HXX //autogen
 #include <svtools/headbar.hxx>
-#endif
-
-#ifndef _SVTABBX_HXX //autogen
 #include <svtools/svtabbx.hxx>
-#endif
 
 
-#ifndef SC_RANGENAM_HXX
 #include "rangenam.hxx"
-#endif
-
-#ifndef SC_ANYREFDG_HXX
 #include "anyrefdg.hxx"
-#endif
-
-#ifndef _SV_LSTBOX_HXX //autogen
 #include <vcl/lstbox.hxx>
-#endif
 
 #ifndef _SVX_ACREDLIN_HXX
 #include <svx/ctredlin.hxx>
 #endif
-
-#ifndef _SVX_SIMPTABL_HXX
 #include <svx/simptabl.hxx>
-#endif
 
 #ifndef _SVARRAY_HXX
 #define _SVARRAY_HXX
 #include <svtools/svarray.hxx>
 #endif
-
-#ifndef SC_CHGTRACK_HXX
 #include "chgtrack.hxx"
-#endif
-
-#ifndef SC_CHGVISET_HXX
 #include "chgviset.hxx"
-#endif
-
-#ifndef _SV_TIMER_HXX //autogen
 #include <vcl/timer.hxx>
-#endif
 
 class ScViewData;
 class ScDocument;
