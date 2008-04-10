07212dc1a1d4803fca4acdf21e79b8cbbd7ae27d
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:54:09 +0000

    INTEGRATION: CWS changefileheader (1.11.224); FILE MERGED 2008/04/01 15:30:20 thb 1.11.224.3: #i85898# Stripping all external header guards 2008/04/01 12:36:24 thb 1.11.224.2: #i85898# Stripping all external header guards 2008/03/31 17:14:48 rt 1.11.224.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/inc/xlpivot.hxx
+++ sc/source/filter/inc/xlpivot.hxx
@@ -1,73 +1,46 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xlpivot.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.11 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-08-02 13:31:51 $
+ * $RCSfile: xlpivot.hxx,v $
+ * $Revision: 1.12 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_XLPIVOT_HXX
 #define SC_XLPIVOT_HXX
 
-#ifndef _COM_SUN_STAR_SHEET_GENERALFUNCTION_HPP_
 #include <com/sun/star/sheet/GeneralFunction.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDORIENTATION_HPP_
 #include <com/sun/star/sheet/DataPilotFieldOrientation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDSORTMODE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldSortMode.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDSHOWITEMSMODE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldShowItemsMode.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDLAYOUTMODE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldLayoutMode.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDREFERENCETYPE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldReferenceType.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDREFERENCEITEMTYPE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldReferenceItemType.hpp>
-#endif
-
-#ifndef _DATETIME_HXX
 #include <tools/datetime.hxx>
-#endif
-
-#ifndef SC_FTOOLS_HXX
 #include "ftools.hxx"
-#endif
-#ifndef SC_XLADDRESS_HXX
 #include "xladdress.hxx"
-#endif
 
 class XclImpStream;
 class XclExpStream;
