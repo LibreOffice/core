4304e1c52a329494be13f8d744342aa3e8e28071
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:55:43 +0000

    INTEGRATION: CWS changefileheader (1.27.300); FILE MERGED 2008/04/01 15:30:39 thb 1.27.300.3: #i85898# Stripping all external header guards 2008/04/01 12:36:40 thb 1.27.300.2: #i85898# Stripping all external header guards 2008/03/31 17:15:13 rt 1.27.300.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/dbgui/csvgrid.cxx
+++ sc/source/ui/dbgui/csvgrid.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: csvgrid.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.27 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2007-05-10 16:55:02 $
+ * $RCSfile: csvgrid.cxx,v $
+ * $Revision: 1.28 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,36 +33,16 @@
 
 
 // ============================================================================
-
-#ifndef _SC_CSVGRID_HXX
 #include "csvgrid.hxx"
-#endif
 
 #include <algorithm>
-
-#ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
-#endif
-#ifndef _SFXSMPLHINT_HXX
 #include <svtools/smplhint.hxx>
-#endif
-#ifndef _TL_POLY_HXX
 #include <tools/poly.hxx>
-#endif
-
-#ifndef SC_SCMOD_HXX
 #include "scmod.hxx"
-#endif
-#ifndef SC_ASCIIOPT_HXX
 #include "asciiopt.hxx"
-#endif
-#ifndef SC_IMPEX_HXX
 #include "impex.hxx"
-#endif
-
-#ifndef _SC_ACCESSIBLECSVCONTROL_HXX
 #include "AccessibleCsvControl.hxx"
-#endif
 
 // *** edit engine ***
 #include "scitems.hxx"
@@ -78,10 +53,7 @@
 #include <svx/fhgtitem.hxx>
 #include <svx/fontitem.hxx>
 #include <svtools/itemset.hxx>
-
-#ifndef SC_EDITUTIL_HXX
 #include "editutil.hxx"
-#endif
 // *** edit engine ***
 
 
