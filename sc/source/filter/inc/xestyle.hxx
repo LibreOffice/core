b258a3f2de07432529d49ae9a4df2d29da5d173e
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:46:20 +0000

    INTEGRATION: CWS changefileheader (1.19.88); FILE MERGED 2008/04/01 15:30:18 thb 1.19.88.3: #i85898# Stripping all external header guards 2008/04/01 12:36:22 thb 1.19.88.2: #i85898# Stripping all external header guards 2008/03/31 17:14:46 rt 1.19.88.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/inc/xestyle.hxx
+++ sc/source/filter/inc/xestyle.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xestyle.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 15:30:43 $
+ * $RCSfile: xestyle.hxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,32 +32,14 @@
 #define SC_XESTYLE_HXX
 
 #include <map>
-
-#ifndef _SVMEMPOOL_HXX
 #include <tools/mempool.hxx>
-#endif
-#ifndef _STRING_HXX
 #include <tools/string.hxx>
-#endif
-#ifndef _ZFORLIST_HXX
 #include <svtools/zforlist.hxx>
-#endif
-#ifndef INCLUDED_SVTOOLS_NFKEYTAB_HXX
 #include <svtools/nfkeytab.hxx>
-#endif
-#ifndef _SVX_SVXFONT_HXX
 #include <svx/svxfont.hxx>
-#endif
-
-#ifndef SC_XLSTYLE_HXX
 #include "xlstyle.hxx"
-#endif
-#ifndef SC_XEROOT_HXX
 #include "xeroot.hxx"
-#endif
-#ifndef SC_XERECORD_HXX
 #include "xerecord.hxx"
-#endif
 
 /* ============================================================================
 - Buffers for style records (PALETTE, FONT, FORMAT, XF, STYLE).
