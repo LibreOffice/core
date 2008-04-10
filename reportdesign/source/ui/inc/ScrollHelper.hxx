42e814e1ba82f967cd51753e5ab2bffa6522cc24
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:04:48 +0000

    INTEGRATION: CWS changefileheader (1.4.26); FILE MERGED 2008/04/01 15:23:42 thb 1.4.26.3: #i85898# Stripping all external header guards 2008/04/01 12:33:14 thb 1.4.26.2: #i85898# Stripping all external header guards 2008/03/31 13:32:17 rt 1.4.26.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/inc/ScrollHelper.hxx
+++ reportdesign/source/ui/inc/ScrollHelper.hxx
@@ -1,64 +1,43 @@
-#ifndef RPTUI_SCROLLHELPER_HXX
-#define RPTUI_SCROLLHELPER_HXX
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ScrollHelper.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 13:48:43 $
+ * $RCSfile: ScrollHelper.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
+#ifndef RPTUI_SCROLLHELPER_HXX
+#define RPTUI_SCROLLHELPER_HXX
 
-#ifndef _SV_SCRBAR_HXX
 #include <vcl/scrbar.hxx>
-#endif
-#ifndef _COM_SUN_STAR_REPORT_XSECTION_HPP_
 #include <com/sun/star/report/XSection.hpp>
-#endif
-#ifndef _COMPHELPER_PROPERTY_MULTIPLEX_HXX_
 #include <comphelper/propmultiplex.hxx>
-#endif
-#ifndef RPTUI_REPORT_DEFINES_HXX
 #include "ReportDefines.hxx"
-#endif
-#ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
-#endif
-#ifndef _SVDEDTV_HXX
 #include <svx/svdedtv.hxx>
-#endif
-#ifndef _CPPUHELPER_BASEMUTEX_HXX_
 #include "cppuhelper/basemutex.hxx"
-#endif
-#ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
-#endif
 #include <boost/shared_ptr.hpp>
 #include <vcl/dockwin.hxx>
 #include <MarkedSection.hxx>
