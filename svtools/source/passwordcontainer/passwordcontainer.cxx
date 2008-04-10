c41deefd1c0e40f91efcb06f70bdc10360f2e200
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:53:14 +0000

    INTEGRATION: CWS changefileheader (1.16.76); FILE MERGED 2008/04/01 15:45:26 thb 1.16.76.3: #i85898# Stripping all external header guards 2008/04/01 12:43:51 thb 1.16.76.2: #i85898# Stripping all external header guards 2008/03/31 13:02:27 rt 1.16.76.1: #i87441# Change license header to LPGL v3.

--- svtools/source/passwordcontainer/passwordcontainer.cxx
+++ svtools/source/passwordcontainer/passwordcontainer.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: passwordcontainer.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 15:25:26 $
+ * $RCSfile: passwordcontainer.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,26 +34,11 @@
 #include "passwordcontainer.hxx"
 
 #include <svtools/pathoptions.hxx>
-
-#ifndef _CPPUHELPER_FACTORY_HXX_
 #include "cppuhelper/factory.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_REGISTRY_XSIMPLEREGISTRY_HPP_
 #include <com/sun/star/registry/XSimpleRegistry.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
 #include <com/sun/star/beans/PropertyValue.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_TASK_MASTERPASSWORDREQUEST_HPP_
 #include <com/sun/star/task/MasterPasswordRequest.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_TASK_NOMASTEREXCEPTION_HPP_
 #include <com/sun/star/task/NoMasterException.hpp>
-#endif
 
 #include <rtl/cipher.h>
 #include <rtl/digest.h>
