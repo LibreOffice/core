f67660832cef7ccb9cd14bf7d9970d9e7d2d5875
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:54:02 +0000

    INTEGRATION: CWS changefileheader (1.6.4); FILE MERGED 2008/04/01 15:21:12 thb 1.6.4.2: #i85898# Stripping all external header guards 2008/03/31 16:25:03 rt 1.6.4.1: #i87441# Change license header to LPGL v3.

--- lingucomponent/source/languageguessing/guesslang.cxx
+++ lingucomponent/source/languageguessing/guesslang.cxx
@@ -1,36 +1,30 @@
 /*************************************************************************
  *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $RCSfile: guesslang.cxx,v $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  $Revision: 1.6 $
+ * $RCSfile: guesslang.cxx,v $
+ * $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2008-03-26 08:58:51 $
+ * This file is part of OpenOffice.org.
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -56,16 +50,9 @@
 #include <com/sun/star/registry/XRegistryKey.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/linguistic2/XLanguageGuessing.hpp>
-
-#ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
 #include <svtools/pathoptions.hxx>
-#endif
-#ifndef _UNOTOOLS_LOCALFILEHELPER_HXX
 #include <unotools/localfilehelper.hxx>
-#endif
-#ifndef _OSL_THREAD_H_
 #include <osl/thread.h>
-#endif
 
 using namespace ::rtl;
 using namespace ::osl;
