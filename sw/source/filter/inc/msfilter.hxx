891c12c31928464eb61cae7ca8192097985967af
Rüdiger Timm <rt@openoffice.org>
2008-04-10 15:51:20 +0000

    INTEGRATION: CWS changefileheader (1.16.240); FILE MERGED 2008/04/01 15:57:47 thb 1.16.240.3: #i85898# Stripping all external header guards 2008/04/01 12:54:42 thb 1.16.240.2: #i85898# Stripping all external header guards 2008/03/31 16:55:45 rt 1.16.240.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/inc/msfilter.hxx
+++ sw/source/filter/inc/msfilter.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: msfilter.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 09:52:54 $
+ * $RCSfile: msfilter.hxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
@@ -41,37 +36,16 @@
 #include <set>
 #include <map>
 #include <vector>
-
-#ifndef _SWTYPES_HXX
 #   include <swtypes.hxx>       //SwTwips
-#endif
-#ifndef _STRING_HXX
 #   include <tools/string.hxx>  //String
-#endif
-#ifndef WW_WWSTYLES_HXX
 #   include "wwstyles.hxx"      //ww::sti
-#endif
-#ifndef _RTL_TEXTENC_H
 #   include <rtl/textenc.h>     //rtl_TextEncoding
-#endif
-#ifndef _SV_GEN_HXX
 #   include <tools/gen.hxx>     //Size
-#endif
-#ifndef _DATETIME_HXX
 #include <tools/datetime.hxx>
-#endif
-#ifndef _FLTSHELL_HXX
 #include <fltshell.hxx>         // fuer den Attribut Stack
-#endif
-#ifndef _REDLINE_HXX
 #include <redline.hxx>
-#endif
-#ifndef _SHELLIO_HXX
 #include <shellio.hxx>
-#endif
-#ifndef _ZFORLIST_HXX
 #include <svtools/zforlist.hxx>
-#endif
 
 #define CREATE_CONST_ASC(s) String::CreateFromAscii( RTL_CONSTASCII_STRINGPARAM(s))
 
