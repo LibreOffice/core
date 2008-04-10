1004b1c49b93f10c714b62e8aebb36a722195a53
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:51:06 +0000

    INTEGRATION: CWS changefileheader (1.8.298); FILE MERGED 2008/04/01 15:33:59 thb 1.8.298.3: #i85898# Stripping all external header guards 2008/04/01 12:38:30 thb 1.8.298.2: #i85898# Stripping all external header guards 2008/03/31 13:56:48 rt 1.8.298.1: #i87441# Change license header to LPGL v3.

--- sd/source/filter/eppt/pptexanimations.hxx
+++ sd/source/filter/eppt/pptexanimations.hxx
@@ -1,59 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: pptexanimations.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 16:40:42 $
+ * $RCSfile: pptexanimations.hxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SD_PPT_EXANIMATIONS_HXX
 #define _SD_PPT_EXANIMATIONS_HXX
 
-#ifndef _COM_SUN_STAR_ANIMATIONS_XTIMECONTAINER_HPP_
 #include <com/sun/star/animations/XTimeContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_XDRAWPAGE_HPP_
 #include <com/sun/star/drawing/XDrawPage.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ANIMATIONS_XANIMATE_HPP_
 #include <com/sun/star/animations/XAnimate.hpp>
-#endif
 #ifndef _SD_PPTANIMATIONS_HXX
 #include "../ppt/pptanimations.hxx"
 #endif
-#ifndef _SD_PPT_EXSOUNDCOLLECTION_HXX
 #include <pptexsoundcollection.hxx>
-#endif
-#ifndef _SVX_ESCHEREX_HXX
 #include <svx/escherex.hxx>
-#endif
 
 #ifdef DBG_ANIM_LOG
 #include <stdio.h>
