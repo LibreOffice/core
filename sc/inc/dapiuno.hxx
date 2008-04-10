cbfa7f2da9fc8b4b528376f736890c3f3e9647e0
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:23:10 +0000

    INTEGRATION: CWS changefileheader (1.9.638); FILE MERGED 2008/04/01 15:29:23 thb 1.9.638.3: #i85898# Stripping all external header guards 2008/04/01 12:35:53 thb 1.9.638.2: #i85898# Stripping all external header guards 2008/03/31 17:13:27 rt 1.9.638.1: #i87441# Change license header to LPGL v3.

--- sc/inc/dapiuno.hxx
+++ sc/inc/dapiuno.hxx
@@ -1,125 +1,62 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dapiuno.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2006-01-13 16:51:24 $
+ * $RCSfile: dapiuno.hxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_DAPIUNO_HXX
 #define SC_DAPIUNO_HXX
 
-#ifndef SC_SCGLOB_HXX
 #include "global.hxx"
-#endif
-
-#ifndef SC_DPOBJECT_HXX
 #include "dpobject.hxx"
-#endif
-
-#ifndef SC_RANGEUTL_HXX
 #include "rangeutl.hxx"     // ScArea
-#endif
-
-#ifndef _SFXLSTNER_HXX //autogen
 #include <svtools/lstner.hxx>
-#endif
-
-#ifndef _SFX_ITEMPROP_HXX
 #include <svtools/itemprop.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_SHEET_XDATAPILOTTABLE_HPP_
 #include <com/sun/star/sheet/XDataPilotTable.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_XDATAPILOTTABLES_HPP_
 #include <com/sun/star/sheet/XDataPilotTables.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_XDATAPILOTDESCRIPTOR_HPP_
 #include <com/sun/star/sheet/XDataPilotDescriptor.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_XENUMERATIONACCESS_HPP_
 #include <com/sun/star/container/XEnumerationAccess.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_XNAMECONTAINER_HPP_
 #include <com/sun/star/container/XNameContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_GENERALFUNCTION_HPP_
 #include <com/sun/star/sheet/GeneralFunction.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDORIENTATION_HPP_
 #include <com/sun/star/sheet/DataPilotFieldOrientation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_XDATAPILOTFIELD_HPP_
 #include <com/sun/star/sheet/XDataPilotField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_XDATAPILOTFIELDGROUPING_HPP_
 #include <com/sun/star/sheet/XDataPilotFieldGrouping.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XUNOTUNNEL_HPP_
 #include <com/sun/star/lang/XUnoTunnel.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include <com/sun/star/beans/XPropertySet.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDAUTOSHOWINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldAutoShowInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDLAYOUTINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldLayoutInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDREFERENCE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldReference.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDSORTINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldSortInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDGROUPINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldGroupInfo.hpp>
-#endif
-
-#ifndef _CPPUHELPER_IMPLBASE2_HXX_
 #include <cppuhelper/implbase2.hxx>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE3_HXX_
 #include <cppuhelper/implbase3.hxx>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE4_HXX_
 #include <cppuhelper/implbase4.hxx>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE5_HXX_
 #include <cppuhelper/implbase5.hxx>
-#endif
 
 #include <memory>
 #include <vector>
