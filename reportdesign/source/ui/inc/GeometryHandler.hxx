be1fbc37530ebaf4d87ed5c3cc9fee316859c180
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:01:39 +0000

    INTEGRATION: CWS changefileheader (1.4.56); FILE MERGED 2008/04/01 15:23:41 thb 1.4.56.3: #i85898# Stripping all external header guards 2008/04/01 12:33:13 thb 1.4.56.2: #i85898# Stripping all external header guards 2008/03/31 13:32:16 rt 1.4.56.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/inc/GeometryHandler.hxx
+++ reportdesign/source/ui/inc/GeometryHandler.hxx
@@ -1,77 +1,52 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: GeometryHandler.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-20 19:10:00 $
+ * $RCSfile: GeometryHandler.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef RPT_GEOMETRYHANDLER_HXX
 #define RPT_GEOMETRYHANDLER_HXX
 
-#ifndef _SAL_CONFIG_H_
 #include "sal/config.h"
-#endif
-#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
 #include "com/sun/star/uno/XComponentContext.hpp"
-#endif
-#ifndef _CPPUHELPER_COMPBASE3_HXX_
 #include "cppuhelper/compbase3.hxx"
-#endif
-#ifndef _CPPUHELPER_BASEMUTEX_HXX_
 #include "cppuhelper/basemutex.hxx"
-#endif
-#ifndef _COM_SUN_STAR_INSPECTION_XPROPERTYHANDLER_HPP_
 #include "com/sun/star/inspection/XPropertyHandler.hpp"
-#endif
-#ifndef _COM_SUN_STAR_SCRIPT_XTYPECONVERTER_HPP_
 #include "com/sun/star/script/XTypeConverter.hpp"
-#endif
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include "com/sun/star/beans/XPropertySet.hpp"
-#endif
-#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include "com/sun/star/lang/XServiceInfo.hpp"
-#endif
 #include "com/sun/star/report/XReportComponent.hpp"
 #include "com/sun/star/report/XFunction.hpp"
 #include <com/sun/star/beans/XPropertyChangeListener.hpp>
-#ifndef _COM_SUN_STAR_SDBC_XROWSET_HPP_
 #include <com/sun/star/sdbc/XRowSet.hpp>
-#endif
 #include <memory>
 #include <comphelper/stl_types.hxx>
 #include <comphelper/listenernotification.hxx>
-#ifndef RPTUI_METADATA_HXX_
 #include "metadata.hxx"
-#endif
 
 //........................................................................
 namespace rptui
