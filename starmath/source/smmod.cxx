67b2bc3136c2313a5974e34b5f7dc2aa659fdcfc
Rüdiger Timm <rt@openoffice.org>
2008-04-10 11:49:41 +0000

    INTEGRATION: CWS changefileheader (1.21.96); FILE MERGED 2008/04/01 15:41:59 thb 1.21.96.3: #i85898# Stripping all external header guards 2008/04/01 12:41:40 thb 1.21.96.2: #i85898# Stripping all external header guards 2008/03/31 16:29:46 rt 1.21.96.1: #i87441# Change license header to LPGL v3.

--- starmath/source/smmod.cxx
+++ starmath/source/smmod.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: smmod.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.21 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-05-25 12:15:14 $
+ * $RCSfile: smmod.cxx,v $
+ * $Revision: 1.22 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,71 +32,31 @@
 #include "precompiled_starmath.hxx"
 
 
-
-#ifndef _GLOBNAME_HXX //autogen
 #include <tools/globname.hxx>
-#endif
-#ifndef _SV_STATUS_HXX //autogen
 #include <vcl/status.hxx>
-#endif
-#ifndef _SFXMSG_HXX //autogen
 #include <sfx2/msg.hxx>
-#endif
-#ifndef _SFXAPP_HXX //autogen
 #include <sfx2/app.hxx>
-#endif
-#ifndef _SFXOBJFACE_HXX //autogen
 #include <sfx2/objface.hxx>
-#endif
-#ifndef _SFX_WHITER_HXX //autogen
 #include <svtools/whiter.hxx>
-#endif
-#ifndef _SFXREQUEST_HXX //autogen
 #include <sfx2/request.hxx>
-#endif
-#ifndef _SFX_HRC //autogen
 #include <sfx2/sfx.hrc>
-#endif
-#ifndef _SFXVIEWSH_HXX
 #include <sfx2/viewsh.hxx>
-#endif
-#ifndef _SV_WRKWIN_HXX //autogen
 #include <vcl/wrkwin.hxx>
-#endif
 #ifndef _SVX_SVXIDS_HRC //autogen
 #include <svx/svxids.hrc>
 #endif
-#ifndef _SV_MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SV_VIRDEV_HXX
 #include <vcl/virdev.hxx>
-#endif
-#ifndef INCLUDED_SVTOOLS_SYSLOCALE_HXX
 #include <svtools/syslocale.hxx>
-#endif
-#ifndef _RTTI_HXX
 #include <tools/rtti.hxx>
-#endif
-
-#ifndef _SMMOD_HXX
 #include "smmod.hxx"
-#endif
-#ifndef SYMBOL_HXX
 #include "symbol.hxx"
-#endif
-#ifndef _CONFIG_HXX
 #include "config.hxx"
-#endif
 #ifndef _DIALOG_HXX
 #include "dialog.hxx"
 #endif
-#ifndef EDIT_HXX
 #include "edit.hxx"
-#endif
-#ifndef VIEW_HXX
 #include "view.hxx"
-#endif
 #include "starmath.hrc"
 
 TYPEINIT1( SmModule, SfxModule );
