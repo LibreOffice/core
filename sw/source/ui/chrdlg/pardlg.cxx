cb2db6ce68ea26fbc9bc73ec8232bd791cdd497b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:04:17 +0000

    INTEGRATION: CWS changefileheader (1.14.240); FILE MERGED 2008/04/01 15:58:20 thb 1.14.240.3: #i85898# Stripping all external header guards 2008/04/01 12:55:03 thb 1.14.240.2: #i85898# Stripping all external header guards 2008/03/31 16:56:31 rt 1.14.240.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/chrdlg/pardlg.cxx
+++ sw/source/ui/chrdlg/pardlg.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: pardlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.14 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 10:20:15 $
+ * $RCSfile: pardlg.cxx,v $
+ * $Revision: 1.15 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -42,13 +37,8 @@
 
 
 #include "hintids.hxx"
-
-#ifndef _SVX_HTMLMODE_HXX
 #include <svx/htmlmode.hxx>
-#endif
-#ifndef _SFXSTYLE_HXX //autogen
 #include <svtools/style.hxx>
-#endif
 
 #include <svx/htmlcfg.hxx>
 
@@ -56,9 +46,7 @@
 #define _SVSTDARR_STRINGSSORTDTOR
 #include <svtools/svstdarr.hxx>
 #endif
-#ifndef _SVTOOLS_CJKOPTIONS_HXX
 #include <svtools/cjkoptions.hxx>
-#endif
 #include "docsh.hxx"
 #include "wrtsh.hxx"
 #include "frmatr.hxx"
@@ -70,10 +58,7 @@
 #include "drpcps.hxx"
 #include "uitool.hxx"
 #include "viewopt.hxx"
-
-#ifndef _NUMPARA_HXX
 #include <numpara.hxx>
-#endif
 #include "chrdlg.hrc"
 #include "poolfmt.hrc"
 #include <svx/svxids.hrc>
