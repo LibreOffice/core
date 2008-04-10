465efaf86e83bfa841f096c7988296bdacd6ca7d
Rüdiger Timm <rt@openoffice.org>
2008-04-10 18:16:33 +0000

    INTEGRATION: CWS changefileheader (1.7.26); FILE MERGED 2008/04/01 15:23:46 thb 1.7.26.3: #i85898# Stripping all external header guards 2008/04/01 12:33:17 thb 1.7.26.2: #i85898# Stripping all external header guards 2008/03/31 13:32:22 rt 1.7.26.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/ui/report/ViewsWindow.cxx
+++ reportdesign/source/ui/report/ViewsWindow.cxx
@@ -1,94 +1,62 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ViewsWindow.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 13:52:21 $
+ * $RCSfile: ViewsWindow.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPTUI_VIEWSWINDOW_HXX
 #include "ViewsWindow.hxx"
-#endif
 #include "ScrollHelper.hxx"
 #include "UndoActions.hxx"
-#ifndef RPTUI_REPORT_WINDOW_HXX
 #include "ReportWindow.hxx"
-#endif
-#ifndef RPTUI_DESIGNVIEW_HXX
 #include "DesignView.hxx"
-#endif
-#ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
-#endif
-#ifndef RPTUI_REPORTCONTROLLER_HXX
 #include "ReportController.hxx"
-#endif
 #ifndef RPTUI_TOOLS_HXX
 #include "UITools.hxx"
 #endif
-#ifndef RPTUI_ENDMARKER_HXX
 #include "EndMarker.hxx"
-#endif
-#ifndef _REPORT_RPTUIDEF_HXX
 #include "RptDef.hxx"
-#endif
 #ifndef _RPTUI_DLGRESID_HRC
 #include "RptResId.hrc"
 #endif
-#ifndef _REPORT_SECTIONVIEW_HXX
 #include "SectionView.hxx"
-#endif
-#ifndef REPORT_REPORTSECTION_HXX
 #include "ReportSection.hxx"
-#endif
 #ifndef REPORTDESIGN_SHARED_UISTRINGS_HRC
 #include "uistrings.hrc"
 #endif
 #ifndef _RPTUI_SLOTID_HRC_
 #include "rptui_slotid.hrc"
 #endif
-#ifndef _REPORT_RPTUICLIP_HXX
 #include "dlgedclip.hxx"
-#endif
 #include "RptObject.hxx"
 #include "RptObject.hxx"
-#ifndef _RPTUI_MODULE_HELPER_RPT_HXX_
 #include "ModuleHelper.hxx"
-#endif
-#ifndef _SVDPAGV_HXX
 #include <svx/svdpagv.hxx>
-#endif
-#ifndef _SVX_UNOSHAPE_HXX
 #include <svx/unoshape.hxx>
-#endif
 #include <vcl/svapp.hxx>
 #include <boost/bind.hpp>
 
