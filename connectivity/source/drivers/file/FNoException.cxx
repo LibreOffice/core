d7687ee3958e533d23fe7d58cf2843495a3f1ca5
Rüdiger Timm <rt@openoffice.org>
2008-04-10 07:59:37 +0000

    INTEGRATION: CWS changefileheader (1.11.122); FILE MERGED 2008/04/01 15:08:46 thb 1.11.122.3: #i85898# Stripping all external header guards 2008/04/01 10:52:59 thb 1.11.122.2: #i85898# Stripping all external header guards 2008/03/28 15:23:34 rt 1.11.122.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/file/FNoException.cxx
+++ connectivity/source/drivers/file/FNoException.cxx
@@ -1,64 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: FNoException.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.11 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 14:06:26 $
+ * $RCSfile: FNoException.cxx,v $
+ * $Revision: 1.12 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-#ifndef _CONNECTIVITY_FILE_CATALOG_HXX_
 #include "file/FCatalog.hxx"
-#endif
-#ifndef _CONNECTIVITY_FILE_FCOMP_HXX_
 #include "file/fcomp.hxx"
-#endif
-#ifndef _CONNECTIVITY_FILE_FANALYZER_HXX_
 #include "file/fanalyzer.hxx"
-#endif
-#ifndef _CONNECTIVITY_FILE_FRESULTSET_HXX_
 #include "file/FResultSet.hxx"
-#endif
-#ifndef _CONNECTIVITY_FILE_OPREPAREDSTATEMENT_HXX_
 #include "file/FPreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_FILE_VALUE_HXX_
 #include <connectivity/FValue.hxx>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef CONNECTIVITY_TKEYVALUE_HXX
 #include "TKeyValue.hxx"
-#endif
 
 using namespace connectivity;
 using namespace connectivity::file;
