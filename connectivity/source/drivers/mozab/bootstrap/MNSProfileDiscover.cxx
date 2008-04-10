d3a4c9a73fe0ce31ece7f4dd13832b13ba96a58a
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:12:37 +0000

    INTEGRATION: CWS changefileheader (1.6.202); FILE MERGED 2008/04/01 10:53:13 thb 1.6.202.2: #i85898# Stripping all external header guards 2008/03/28 15:23:55 rt 1.6.202.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/mozab/bootstrap/MNSProfileDiscover.cxx
+++ connectivity/source/drivers/mozab/bootstrap/MNSProfileDiscover.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MNSProfileDiscover.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-10-24 15:05:50 $
+ * $RCSfile: MNSProfileDiscover.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -103,9 +98,7 @@ static ::rtl::OUString szDirectoryString =::rtl::OUString::createFromAscii("dire
 #ifndef MAXPATHLEN
 #define MAXPATHLEN 1024
 #endif
-#ifndef _MNSFOLDERS_HXX_
 #include <MNSFolders.hxx>
-#endif
 #include <MNSINIParser.hxx>
 
 namespace connectivity
