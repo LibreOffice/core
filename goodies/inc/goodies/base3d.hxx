713fe81cfc6437527ce85a7f1e1b8aceff3f56f8
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:09:56 +0000

    INTEGRATION: CWS changefileheader (1.4.28); FILE MERGED 2008/04/01 15:19:27 thb 1.4.28.3: #i85898# Stripping all external header guards 2008/04/01 12:31:03 thb 1.4.28.2: #i85898# Stripping all external header guards 2008/03/31 13:39:30 rt 1.4.28.1: #i87441# Change license header to LPGL v3.

--- goodies/inc/goodies/base3d.hxx
+++ goodies/inc/goodies/base3d.hxx
@@ -1,77 +1,53 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: base3d.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-01-07 08:59:31 $
+ * $RCSfile: base3d.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _B3D_BASE3D_HXX
 #define _B3D_BASE3D_HXX
 
-#ifndef _B3D_B3DLIGHT_HXX
 #include <goodies/b3dlight.hxx>
-#endif
 
 //#ifndef _B3D_HMATRIX_HXX
 //#include "hmatrix.hxx"
 //#endif
-
-#ifndef _B3D_B3DENTITY_HXX
 #include <goodies/b3dentty.hxx>
-#endif
-
-#ifndef _B3D_B3DCOMPO_HXX
 #include <goodies/b3dcompo.hxx>
-#endif
 
 #ifndef _INC_FLOAT
 #include <float.h>
 #endif
 
 // MUSS-Aenderung
-#ifndef _SV_OUTDEV3D_HXX
 #include <vcl/outdev3d.hxx>
-#endif
-
-#ifndef _SV_TIMER_HXX
 #include <vcl/timer.hxx>
-#endif
-
-#ifndef _VOS_MUTEX_HXX_
 #include <vos/mutex.hxx>
-#endif
-
-#ifndef _SV_GEN_HXX
 #include <tools/gen.hxx>
-#endif
 
 // Vorausdeklarationen
 class B3dTexture;
