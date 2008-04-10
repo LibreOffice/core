6fe2b1ef6189e872c2e6eb72dd03c5240b36f181
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:47:26 +0000

    INTEGRATION: CWS changefileheader (1.5.372); FILE MERGED 2008/04/01 15:09:08 thb 1.5.372.3: #i85898# Stripping all external header guards 2008/04/01 10:53:18 thb 1.5.372.2: #i85898# Stripping all external header guards 2008/03/28 15:24:08 rt 1.5.372.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/inc/adabas/BStatement.hxx
+++ connectivity/source/inc/adabas/BStatement.hxx
@@ -1,52 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: BStatement.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 06:46:19 $
+ * $RCSfile: BStatement.hxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef CONNECTIVITY_ADABAS_STATEMENT_HXX
 #define CONNECTIVITY_ADABAS_STATEMENT_HXX
 
-#ifndef _CONNECTIVITY_ODBC_OSTATEMENT_HXX_
 #include "odbc/OStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADABAS_BCONNECTION_HXX_
 #include "adabas/BConnection.hxx"
-#endif
 #ifndef _CONNECTIVITY_COMMONTOOLS_HXX_
 #include "connectivity/commontools.hxx"
 #endif
-#ifndef _VOS_REF_HXX_
 #include <vos/ref.hxx>
-#endif
 
 namespace connectivity
 {
