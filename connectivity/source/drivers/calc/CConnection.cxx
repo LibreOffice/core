25629144669e94df89cd536218cf6fa9eaf268ad
Rüdiger Timm <rt@openoffice.org>
2008-04-10 07:28:50 +0000

    INTEGRATION: CWS changefileheader (1.16.8); FILE MERGED 2008/04/01 15:08:39 thb 1.16.8.3: #i85898# Stripping all external header guards 2008/04/01 10:52:51 thb 1.16.8.2: #i85898# Stripping all external header guards 2008/03/28 15:23:27 rt 1.16.8.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/calc/CConnection.cxx
+++ connectivity/source/drivers/calc/CConnection.cxx
@@ -1,86 +1,54 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: CConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 16:26:31 $
+ * $RCSfile: CConnection.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-
-#ifndef _CONNECTIVITY_CALC_CONNECTION_HXX_
 #include "calc/CConnection.hxx"
-#endif
-#ifndef _CONNECTIVITY_CALC_DATABASEMETADATA_HXX_
 #include "calc/CDatabaseMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_CALC_CATALOG_HXX_
 #include "calc/CCatalog.hxx"
-#endif
 #ifndef _CONNECTIVITY_CALC_ODRIVER_HXX_
 #include "calc/CDriver.hxx"
 #endif
 #ifndef CONNECTIVITY_RESOURCE_CALC_HRC
 #include "resource/calc_res.hrc"
 #endif
-#ifndef CONNECTIVITY_SHAREDRESOURCES_HXX
 #include "resource/sharedresources.hxx"
-#endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_FRAME_XCOMPONENTLOADER_HPP_
 #include <com/sun/star/frame/XComponentLoader.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_XSPREADSHEETDOCUMENT_HPP_
 #include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
-#endif
-#ifndef _URLOBJ_HXX //autogen wg. INetURLObject
 #include <tools/urlobj.hxx>
-#endif
-#ifndef _CONNECTIVITY_CALC_PREPAREDSTATEMENT_HXX_
 #include "calc/CPreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_CALC_STATEMENT_HXX_
 #include "calc/CStatement.hxx"
-#endif
-#ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
 #include <svtools/pathoptions.hxx>
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include <connectivity/dbexception.hxx>
-#endif
-#ifndef _CPPUHELPER_EXC_HLP_HXX_
 #include <cppuhelper/exc_hlp.hxx>
-#endif
 
 using namespace connectivity::calc;
 using namespace connectivity::file;
