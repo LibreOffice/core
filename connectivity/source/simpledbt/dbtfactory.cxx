c3ba358bde08d8aea20031a14eec4d2169c6b390
Rüdiger Timm <rt@openoffice.org>
2008-04-10 11:04:01 +0000

    INTEGRATION: CWS changefileheader (1.7.74); FILE MERGED 2008/04/01 10:53:48 thb 1.7.74.2: #i85898# Stripping all external header guards 2008/03/28 15:24:41 rt 1.7.74.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/simpledbt/dbtfactory.cxx
+++ connectivity/source/simpledbt/dbtfactory.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dbtfactory.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-11-01 14:52:52 $
+ * $RCSfile: dbtfactory.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,19 +33,10 @@
 
 #include <connectivity/virtualdbtools.hxx>
 #include <connectivity/formattedcolumnvalue.hxx>
-
-#ifndef CONNECTIVITY_DBTOOLS_DBTFACTORY_HXX
 #include "dbtfactory.hxx"
-#endif
-#ifndef CONNECTIVITY_DBTOOLS_PARSER_SIMPLE_HXX
 #include "parser_s.hxx"
-#endif
-#ifndef CONNECTIVITY_STATIC_DBTOOLS_SIMPLE_HXX
 #include "staticdbtools_s.hxx"
-#endif
-#ifndef CONNECTIVITY_DBTOOLS_CHARSET_S_HXX
 #include "charset_s.hxx"
-#endif
 
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
