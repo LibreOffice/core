4edfa3ff872b4304413fd4f465efb9ef2002bf58
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:39:41 +0000

    INTEGRATION: CWS changefileheader (1.24.14); FILE MERGED 2008/04/01 15:35:08 thb 1.24.14.3: #i85898# Stripping all external header guards 2008/04/01 12:39:00 thb 1.24.14.2: #i85898# Stripping all external header guards 2008/03/31 13:58:09 rt 1.24.14.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/inc/DrawViewShell.hxx
+++ sd/source/ui/inc/DrawViewShell.hxx
@@ -1,68 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: DrawViewShell.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.25 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 13:53:47 $
+ * $RCSfile: DrawViewShell.hxx,v $
+ * $Revision: 1.26 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SD_DRAW_VIEW_SHELL_HXX
 #define SD_DRAW_VIEW_SHELL_HXX
 
-#ifndef SD_VIEW_SHELL_HXX
 #include "ViewShell.hxx"
-#endif
-#ifndef SD_ASYNCHRONOUS_CALL_HXX
 #include "tools/AsynchronousCall.hxx"
-#endif
-
-#ifndef _VIEWFAC_HXX //autogen
 #include <sfx2/viewfac.hxx>
-#endif
-#ifndef _SFXVIEWSH_HXX //autogen
 #include <sfx2/viewsh.hxx>
-#endif
-
-#ifndef SD_TAB_CONTROL_HXX
 #include "TabControl.hxx"
-#endif
-#ifndef _PRESENTATION_HXX
 #include "pres.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_LANG_XEVENTLISTENER_HPP_
 #include <com/sun/star/lang/XEventListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SCANNER_XSCANNERMANAGER_HPP_
 #include <com/sun/star/scanner/XScannerManager.hpp>
-#endif
 
 class SdPage;
 class DrawDocShell;
