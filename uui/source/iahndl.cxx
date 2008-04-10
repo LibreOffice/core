dec28fb6fd1bf0efa85080ba5c14419303560d67
Rüdiger Timm <rt@openoffice.org>
2008-04-10 15:22:47 +0000

    INTEGRATION: CWS changefileheader (1.66.2); FILE MERGED 2008/04/01 16:03:50 thb 1.66.2.3: #i85898# Stripping all external header guards 2008/04/01 13:00:07 thb 1.66.2.2: #i85898# Stripping all external header guards 2008/03/31 15:33:10 rt 1.66.2.1: #i87441# Change license header to LPGL v3.

--- uui/source/iahndl.cxx
+++ uui/source/iahndl.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: iahndl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.66 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 12:25:38 $
+ * $RCSfile: iahndl.cxx,v $
+ * $Revision: 1.67 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -98,10 +93,7 @@
 #include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
 #include "com/sun/star/uno/RuntimeException.hpp"
 #include "com/sun/star/xforms/InvalidDataOnSubmitException.hpp"
-
-#ifndef _COM_SUN_STAR_SECURITY_CERTIFICATEVALIDITY_HPP_
 #include <com/sun/star/security/CertificateValidity.hpp>
-#endif
 
 #include "vos/mutex.hxx"
 #include "tools/rcid.h"
@@ -125,13 +117,8 @@
 #include "unknownauthdlg.hxx"
 #include "sslwarndlg.hxx"
 #include "openlocked.hxx"
-
-#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
 #include <comphelper/processfactory.hxx>
-#endif
-#ifndef _ZFORLIST_HXX
 #include <svtools/zforlist.hxx>
-#endif
 using namespace com::sun;
 
 namespace csss = ::com::sun::star::security;
