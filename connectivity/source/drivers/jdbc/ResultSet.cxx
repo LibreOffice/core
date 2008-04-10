164f62973b0c9a6435de79e758e0fdc6e4451acf
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:27:59 +0000

    INTEGRATION: CWS changefileheader (1.34.22); FILE MERGED 2008/04/01 15:08:51 thb 1.34.22.3: #i85898# Stripping all external header guards 2008/04/01 10:53:05 thb 1.34.22.2: #i85898# Stripping all external header guards 2008/03/28 15:23:42 rt 1.34.22.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/jdbc/ResultSet.cxx
+++ connectivity/source/drivers/jdbc/ResultSet.cxx
@@ -1,101 +1,56 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ResultSet.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.34 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-02-04 13:30:36 $
+ * $RCSfile: ResultSet.cxx,v $
+ * $Revision: 1.35 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_connectivity.hxx"
-#ifndef _CONNECTIVITY_JAVA_SQL_RESULTSET_HXX_
 #include "java/sql/ResultSet.hxx"
-#endif
 #include "java/math/BigDecimal.hxx"
-#ifndef _CONNECTIVITY_JAVA_SQL_STATEMENT_HXX_
 #include "java/sql/JStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_SQLWARNING_HXX_
 #include "java/sql/SQLWarning.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_ARRAY_HXX_
 #include "java/sql/Array.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_REF_HXX_
 #include "java/sql/Ref.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_CLOB_HXX_
 #include "java/sql/Clob.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_TIMESTAMP_HXX_
 #include "java/sql/Timestamp.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_BLOB_HXX_
 #include "java/sql/Blob.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_SQL_RESULTSETMETADATA_HXX_
 #include "java/sql/ResultSetMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_IO_INPUTSTREAM_HXX_
 #include "java/io/InputStream.hxx"
-#endif
-#ifndef _CONNECTIVITY_JAVA_TOOLS_HXX_
 #include "java/tools.hxx"
-#endif
-#ifndef _COMPHELPER_PROPERTY_HXX_
 #include <comphelper/property.hxx>
-#endif
-#ifndef _CONNECTIVITY_COMMONTOOLS_HXX_
 #include "connectivity/CommonTools.hxx"
-#endif
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
-#ifndef _COMPHELPER_SEQUENCE_HXX_
 #include <comphelper/sequence.hxx>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-#ifndef CONNECTIVITY_CONNECTION_HXX
 #include "TConnection.hxx"
-#endif
-#ifndef _COMPHELPER_TYPES_HXX_
 #include <comphelper/types.hxx>
-#endif
-#ifndef _CONNECTIVITY_DBTOOLS_HXX_
 #include "connectivity/dbtools.hxx"
-#endif
-#ifndef _DBHELPER_DBEXCEPTION_HXX_
 #include "connectivity/dbexception.hxx"
-#endif
 
 #include <string.h>
 
