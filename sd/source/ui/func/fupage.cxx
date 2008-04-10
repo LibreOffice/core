93cabacd931f643e8147df1a3884220d8bbe59a5
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:23:27 +0000

    INTEGRATION: CWS changefileheader (1.29.100); FILE MERGED 2008/04/01 15:34:40 thb 1.29.100.3: #i85898# Stripping all external header guards 2008/04/01 12:38:55 thb 1.29.100.2: #i85898# Stripping all external header guards 2008/03/31 13:58:06 rt 1.29.100.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/func/fupage.cxx
+++ sd/source/ui/func/fupage.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: fupage.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.30 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-04 12:45:30 $
+ * $RCSfile: fupage.cxx,v $
+ * $Revision: 1.31 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,40 +40,23 @@
 
 #include <svx/svxids.hrc>
 #include <svx/dialogs.hrc>
-
-#ifndef _SFXITEMPOOL_HXX //autogen
 #include <svtools/itempool.hxx>
-#endif
 #ifndef _MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
 #endif
-#ifndef _SFXREQUEST_HXX //autogen
 #include <sfx2/request.hxx>
-#endif
-#ifndef _SFXSTRITEM_HXX //autogen
 #include <svtools/stritem.hxx>
-#endif
-#ifndef _SV_PRNTYPES_HXX //autogen
 #include <vcl/prntypes.hxx>
-#endif
-#ifndef _SFXSTYLE_HXX
 #include <svtools/style.hxx>
-#endif
-#ifndef _SD_STLSHEET_HXX
 #include <stlsheet.hxx>
-#endif
 #ifndef _SVX_SVDORECT_HXX
 #include <svx/svdorect.hxx>
 #endif
 #ifndef _SVX_SVDUNDO_HXX
 #include <svx/svdundo.hxx>
 #endif
-#ifndef _EEITEM_HXX
 #include <svx/eeitem.hxx>
-#endif
-#ifndef _SVX_FRMDIRITEM_HXX
 #include <svx/frmdiritem.hxx>
-#endif
 #include <svx/xbtmpit.hxx>
 #include <svx/xsetit.hxx>
 #include <svtools/itempool.hxx>
@@ -97,21 +75,13 @@
 
 #include "strings.hrc"
 #include "sdpage.hxx"
-#ifndef SD_VIEW_HXX
 #include "View.hxx"
-#endif
-#ifndef SD_WINDOW_HXX
 #include "Window.hxx"
-#endif
 #include "pres.hxx"
 #include "drawdoc.hxx"
 #include "DrawDocShell.hxx"
-#ifndef SD_VIEW_SHELL_HXX
 #include "ViewShell.hxx"
-#endif
-#ifndef SD_DRAW_VIEW_SHELL_HXX
 #include "DrawViewShell.hxx"
-#endif
 #include "app.hrc"
 #include "unchss.hxx"
 #include "undoback.hxx"
