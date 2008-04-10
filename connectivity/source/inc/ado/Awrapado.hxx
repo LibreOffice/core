b9fa09514b74c957b2792dd8b3b028de9bdbd5c5
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:59:10 +0000

    INTEGRATION: CWS changefileheader (1.15.276); FILE MERGED 2008/04/01 10:53:21 thb 1.15.276.2: #i85898# Stripping all external header guards 2008/03/28 15:24:13 rt 1.15.276.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/inc/ado/Awrapado.hxx
+++ connectivity/source/inc/ado/Awrapado.hxx
@@ -1,43 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: Awrapado.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.15 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-20 02:00:24 $
+ * $RCSfile: Awrapado.hxx,v $
+ * $Revision: 1.16 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _CONNECTIVITY_ADO_AWRAPADO_HXX_
 #define _CONNECTIVITY_ADO_AWRAPADO_HXX_
 
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
 
 // Includes fuer ADO
 #include "ado_pre_sys_include.h"
@@ -47,15 +40,9 @@
 #include "ado_post_sys_include.h"
 
 
-#ifndef _CONNECTIVITY_ADO_ADOIMP_HXX_
 #include "ado/adoimp.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_AOLEWRAP_HXX_
 #include "ado/Aolewrap.hxx"
-#endif
-#ifndef _CONNECTIVITY_ADO_AOLEVARIANT_HXX_
 #include "ado/Aolevariant.hxx"
-#endif
 
 namespace connectivity
 {
