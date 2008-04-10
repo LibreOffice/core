afb3c9a048525af3a78459a2f3236057650a8085
Rüdiger Timm <rt@openoffice.org>
2008-04-10 12:08:09 +0000

    INTEGRATION: CWS changefileheader (1.13.58); FILE MERGED 2008/04/01 15:56:55 thb 1.13.58.3: #i85898# Stripping all external header guards 2008/04/01 12:53:55 thb 1.13.58.2: #i85898# Stripping all external header guards 2008/03/31 16:53:46 rt 1.13.58.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/doc/docdde.cxx
+++ sw/source/core/doc/docdde.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: docdde.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 10:36:07 $
+ * $RCSfile: docdde.cxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,50 +38,22 @@
 #ifndef _APP_HXX
 #include <vcl/svapp.hxx>
 #endif
-#ifndef _URLOBJ_HXX
 #include <tools/urlobj.hxx>
-#endif
 
 #define _SVSTDARR_STRINGS
 #include <svtools/svstdarr.hxx>
-
-#ifndef _SVXLINKMGR_HXX
 #include <svx/linkmgr.hxx>          // LinkManager
-#endif
-#ifndef _UNOTOOLS_CHARCLASS_HXX
 #include <unotools/charclass.hxx>
-#endif
-
-#ifndef _FMTCNTNT_HXX //autogen
 #include <fmtcntnt.hxx>
-#endif
-#ifndef _DOC_HXX
 #include <doc.hxx>
-#endif
-#ifndef _SWSERV_HXX
 #include <swserv.hxx>           // fuer Server-Funktionalitaet
-#endif
-#ifndef _BOOKMRK_HXX
 #include <bookmrk.hxx>          // fuer die Bookmarks
-#endif
-#ifndef _SECTION_HXX
 #include <section.hxx>          // fuer SwSectionFmt
-#endif
-#ifndef _SWTABLE_HXX
 #include <swtable.hxx>          // fuer SwTable
-#endif
-#ifndef _NODE_HXX
 #include <node.hxx>
-#endif
-#ifndef _NDTXT_HXX
 #include <ndtxt.hxx>
-#endif
-#ifndef _PAM_HXX
 #include <pam.hxx>
-#endif
-#ifndef _DOCARY_HXX
 #include <docary.hxx>
-#endif
 
 using namespace ::com::sun::star;
 
