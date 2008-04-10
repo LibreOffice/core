7cc6a784553e51078274a61a4ba72c82aa2efe47
Rüdiger Timm <rt@openoffice.org>
2008-04-10 08:47:43 +0000

    INTEGRATION: CWS changefileheader (1.3.22); FILE MERGED 2008/04/01 10:53:08 thb 1.3.22.2: #i85898# Stripping all external header guards 2008/03/28 15:23:46 rt 1.3.22.1: #i87441# Change license header to LPGL v3.

--- connectivity/source/drivers/macab/MacabConnection.cxx
+++ connectivity/source/drivers/macab/MacabConnection.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MacabConnection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-30 07:56:15 $
+ * $RCSfile: MacabConnection.cxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,31 +32,14 @@
 #include "precompiled_connectivity.hxx"
 
 #include "MacabConnection.hxx"
-#ifndef _CONNECTIVITY_MACAB_ADDRESSBOOK_HXX_
 #include "MacabAddressBook.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_DATABASEMETADATA_HXX_
 #include "MacabDatabaseMetaData.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_STATEMENT_HXX_
 #include "MacabStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_PREPAREDSTATEMENT_HXX_
 #include "MacabPreparedStatement.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_DRIVER_HXX_
 #include "MacabDriver.hxx"
-#endif
-#ifndef _CONNECTIVITY_MACAB_CATALOG_HXX_
 #include "MacabCatalog.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_SDBC_COLUMNVALUE_HPP_
 #include <com/sun/star/sdbc/ColumnValue.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_TRANSACTIONISOLATION_HPP_
 #include <com/sun/star/sdbc/TransactionIsolation.hpp>
-#endif
 
 using namespace connectivity::macab;
 using namespace com::sun::star::uno;
--- connectivity/source/drivers/macab/MacabDatabaseMetaData.cxx
+++ connectivity/source/drivers/macab/MacabDatabaseMetaData.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: MacabDatabaseMetaData.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-30 07:56:42 $
+ * $RCSfile: MacabDatabaseMetaData.cxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,43 +32,19 @@
 #include "precompiled_connectivity.hxx"
 
 #include "MacabDatabaseMetaData.hxx"
-
-#ifndef _CONNECTIVITY_MACAB_ADDRESSBOOK_HXX_
 #include "MacabAddressBook.hxx"
-#endif
-
-#ifndef _CONNECTIVITY_MACAB_HEADER_HXX_
 #include "MacabHeader.hxx"
-#endif
-
-#ifndef _CONNECTIVITY_MACAB_GROUP_HXX_
 #include "MacabGroup.hxx"
-#endif
-
-#ifndef _CONNECTIVITY_MACAB_UTILITIES_HXX_
 #include "macabutilities.hxx"
-#endif
 
 #ifndef CONNECTIVITY_MACAB_DRIVER_HXX_
 #include "MacabDriver.hxx"
 #endif
-
-#ifndef _CONNECTIVITY_FDATABASEMETADATARESULTSET_HXX_
 #include "FDatabaseMetaDataResultSet.hxx"
-#endif
-#ifndef _CONNECTIVITY_OTYPEINFO_HXX_
 #include "OTypeInfo.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_SDBC_COLUMNVALUE_HPP_
 #include <com/sun/star/sdbc/ColumnValue.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_RESULTSETTYPE_HPP_
 #include <com/sun/star/sdbc/ResultSetType.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_TRANSACTIONISOLATION_HPP_
 #include <com/sun/star/sdbc/TransactionIsolation.hpp>
-#endif
 
 #include <vector>
 
