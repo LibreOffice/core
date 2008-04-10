83cbb2c2f6551fa36982b8ec891e50890de21f0b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:38:05 +0000

    INTEGRATION: CWS changefileheader (1.3.96); FILE MERGED 2008/04/01 15:23:38 thb 1.3.96.3: #i85898# Stripping all external header guards 2008/04/01 12:33:10 thb 1.3.96.2: #i85898# Stripping all external header guards 2008/03/31 13:32:08 rt 1.3.96.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/filter/xml/xmlGroup.cxx
+++ reportdesign/source/filter/xml/xmlGroup.cxx
@@ -1,84 +1,48 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlGroup.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-08-02 14:33:34 $
+ * $RCSfile: xmlGroup.cxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPT_XMLGROUP_HXX
 #include "xmlGroup.hxx"
-#endif
-#ifndef RPT_XMLSECTION_HXX
 #include "xmlSection.hxx"
-#endif
-#ifndef RPT_XMLFUNCTION_HXX
 #include "xmlFunction.hxx"
-#endif
-#ifndef RPT_XMLFILTER_HXX
 #include "xmlfilter.hxx"
-#endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
 #include <xmloff/xmltoken.hxx>
-#endif
-#ifndef _XMLOFF_XMLNMSPE_HXX
 #include <xmloff/xmlnmspe.hxx>
-#endif
-#ifndef _XMLOFF_NMSPMAP_HXX
 #include <xmloff/nmspmap.hxx>
-#endif
-#ifndef _XMLOFF_XMLUCONV_HXX
 #include <xmloff/xmluconv.hxx>
-#endif
-#ifndef RPT_XMLHELPER_HXX
 #include "xmlHelper.hxx"
-#endif
-#ifndef RPT_XMLENUMS_HXX
 #include "xmlEnums.hxx"
-#endif
-#ifndef _UCBHELPER_CONTENT_HXX
 #include <ucbhelper/content.hxx>
-#endif
-#ifndef _COMPHELPER_NAMECONTAINER_HXX_
 #include <comphelper/namecontainer.hxx>
-#endif
-#ifndef _COM_SUN_STAR_REPORT_GROUPON_HPP_
 #include <com/sun/star/report/GroupOn.hpp>
-#endif
-#ifndef _COM_SUN_STAR_REPORT_KEEPTOGETHER_HPP_
 #include <com/sun/star/report/KeepTogether.hpp>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 
 namespace rptxml
 {
