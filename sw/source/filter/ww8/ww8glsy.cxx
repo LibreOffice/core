d2bad61bb4e10855ba257c593918399042f4ce39
Rüdiger Timm <rt@openoffice.org>
2008-04-10 16:23:05 +0000

    INTEGRATION: CWS changefileheader (1.29.242); FILE MERGED 2008/04/01 15:57:57 thb 1.29.242.3: #i85898# Stripping all external header guards 2008/04/01 12:54:49 thb 1.29.242.2: #i85898# Stripping all external header guards 2008/03/31 16:55:50 rt 1.29.242.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/ww8/ww8glsy.cxx
+++ sw/source/filter/ww8/ww8glsy.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ww8glsy.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.29 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 10:03:32 $
+ * $RCSfile: ww8glsy.cxx,v $
+ * $Revision: 1.30 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,51 +32,24 @@
 #include "precompiled_sw.hxx"
 
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
-
-#ifndef _URLOBJ_HXX
 #include <tools/urlobj.hxx>
-#endif
-
-#ifndef SVTOOLS_URIHELPER_HXX
 #include <svtools/urihelper.hxx>
-#endif
-
-#ifndef _RTL_TENCINFO_H
 #include <rtl/tencinfo.h>
-#endif
-
-#ifndef _SWSWERROR_H
 #include <swerror.h>
-#endif
 #ifndef _NDTXT
 #include <ndtxt.hxx>
 #endif
 #include <pam.hxx>
-#ifndef _SHELLIO_HXX
 #include <shellio.hxx>
-#endif
 #ifndef _DOCSH_HXX
 #include <docsh.hxx>
 #endif
-#ifndef _FMTANCHR_HXX //autogen wg. SwFmtAnchor
 #include <fmtanchr.hxx>
-#endif
-#ifndef _FRMFMT_HXX //autogen wg. SwFrmFmt
 #include <frmfmt.hxx>
-#endif
-#ifndef _DOC_HXX //autogen wg. SwDoc
 #include <doc.hxx>
-#endif
-#ifndef _DOCARY_HXX //autogen wg. SwDoc
 #include <docary.hxx>
-#endif
-
-#ifndef _WW8GLSY_HXX
 #include "ww8glsy.hxx"
-#endif
-#ifndef _WW8PAR_HXX
 #include "ww8par.hxx"
-#endif
 
 WW8Glossary::WW8Glossary(SvStorageStreamRef &refStrm, BYTE nVersion,
     SvStorage *pStg)
