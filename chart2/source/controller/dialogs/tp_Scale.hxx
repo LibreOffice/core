ba857ba9aa6dc29659980b2f4cdb2dde1d95f76d
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:24:33 +0000

    INTEGRATION: CWS changefileheader (1.6.66); FILE MERGED 2008/04/01 15:04:04 thb 1.6.66.2: #i85898# Stripping all external header guards 2008/03/28 16:43:32 rt 1.6.66.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/dialogs/tp_Scale.hxx
+++ chart2/source/controller/dialogs/tp_Scale.hxx
@@ -1,60 +1,47 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: tp_Scale.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-10-22 16:48:24 $
+ * $RCSfile: tp_Scale.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _CHART2_TP_SCALES_HXX
 #define _CHART2_TP_SCALES_HXX
 
 // header for SfxTabPage
-#ifndef _SFXTABDLG_HXX
 #include <sfx2/tabdlg.hxx>
-#endif
 // header for class FormattedField
-#ifndef _FMTFIELD_HXX_
 #include <svtools/fmtfield.hxx>
-#endif
 // header for FixedText
-#ifndef _SV_FIXED_HXX
 #include <vcl/fixed.hxx>
-#endif
 // header for CheckBox
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
 // header for MetricField
-#ifndef _SV_FIELD_HXX
 #include <vcl/field.hxx>
-#endif
 //class SvNumberFormatter;
 
 //.............................................................................
