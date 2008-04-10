2e1add23c32015382232fef1d1844edf26691be7
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:52:17 +0000

    INTEGRATION: CWS changefileheader (1.10.328); FILE MERGED 2008/04/01 12:36:24 thb 1.10.328.2: #i85898# Stripping all external header guards 2008/03/31 17:14:47 rt 1.10.328.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/inc/xldumper.hxx
+++ sc/source/filter/inc/xldumper.hxx
@@ -1,64 +1,46 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xldumper.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.10 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 12:37:29 $
+ * $RCSfile: xldumper.hxx,v $
+ * $Revision: 1.11 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SC_XLDUMPER_HXX
 #define SC_XLDUMPER_HXX
 
-#ifndef SC_FDUMPER_HXX
 #include "fdumper.hxx"
-#endif
 
 #if SCF_INCL_DUMPER
 
 #include <stack>
-
-#ifndef SC_FDUMPERDFF_HXX
 #include "fdumperdff.hxx"
-#endif
-#ifndef SC_FDUMPEROLE_HXX
 #include "fdumperole.hxx"
-#endif
-#ifndef SC_XLCONST_HXX
 #include "xlconst.hxx"
-#endif
-#ifndef SC_XLSTRING_HXX
 #include "xlstring.hxx"
-#endif
-#ifndef SC_XLSTYLE_HXX
 #include "xlstyle.hxx"
-#endif
 
 class SfxMedium;
 class SfxObjectShell;
