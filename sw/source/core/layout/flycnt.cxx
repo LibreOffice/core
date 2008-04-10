f0c24924ee4359d43515e4948b7689c42cf7ef03
Rüdiger Timm <rt@openoffice.org>
2008-04-10 13:43:54 +0000

    INTEGRATION: CWS changefileheader (1.64.118); FILE MERGED 2008/04/01 15:57:15 thb 1.64.118.3: #i85898# Stripping all external header guards 2008/04/01 12:54:13 thb 1.64.118.2: #i85898# Stripping all external header guards 2008/03/31 16:54:21 rt 1.64.118.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/layout/flycnt.cxx
+++ sw/source/core/layout/flycnt.cxx
@@ -1,44 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: flycnt.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.64 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2008-01-04 13:21:42 $
+ * $RCSfile: flycnt.cxx,v $
+ * $Revision: 1.65 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
-
-#ifndef _BIGINT_HXX //autogen
 #include <tools/bigint.hxx>
-#endif
 #include "pagefrm.hxx"
 #include "rootfrm.hxx"
 #include "cntfrm.hxx"
@@ -55,26 +47,12 @@
 #include "ndtxt.hxx"
 #include "swundo.hxx"
 #include "errhdl.hxx"
-
-#ifndef _SVX_ULSPITEM_HXX //autogen
 #include <svx/ulspitem.hxx>
-#endif
-#ifndef _SVX_LRSPITEM_HXX //autogen
 #include <svx/lrspitem.hxx>
-#endif
-
-#ifndef _FMTANCHR_HXX //autogen
 #include <fmtanchr.hxx>
-#endif
-#ifndef _FMTORNT_HXX //autogen
 #include <fmtornt.hxx>
-#endif
-#ifndef _FMTFSIZE_HXX //autogen
 #include <fmtfsize.hxx>
-#endif
-#ifndef _FMTSRND_HXX //autogen
 #include <fmtsrnd.hxx>
-#endif
 
 #include "tabfrm.hxx"
 #include "flyfrms.hxx"
@@ -82,30 +60,18 @@
 #include "sectfrm.hxx"
 
 // OD 29.10.2003 #113049#
-#ifndef _TOCNTNTANCHOREDOBJECTPOSITION_HXX
 #include <tocntntanchoredobjectposition.hxx>
-#endif
 // OD 2004-05-24 #i28701#
-#ifndef _DCONTACT_HXX
 #include <dcontact.hxx>
-#endif
-#ifndef _SORTEDOBJS_HXX
 #include <sortedobjs.hxx>
-#endif
 // --> OD 2005-09-29 #125370#,#125957#
-#ifndef _LAYOUTER_HXX
 #include <layouter.hxx>
-#endif
 // <--
 // --> OD 2005-11-17 #i56300#
-#ifndef _OBJECTFORMATTERTXTFRM_HXX
 #include <objectformattertxtfrm.hxx>
-#endif
 // <--
 // --> OD 2006-03-06 #125892#
-#ifndef _HANDLEANCHORNODECHG_HXX
 #include <HandleAnchorNodeChg.hxx>
-#endif
 // <--
 
 using namespace ::com::sun::star;
