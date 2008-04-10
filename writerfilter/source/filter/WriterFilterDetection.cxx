12db6538efe0a31581b9eed68246b0443e6293a8
Rüdiger Timm <rt@openoffice.org>
2008-04-10 12:34:50 +0000

    INTEGRATION: CWS changefileheader (1.2.36); FILE MERGED 2008/04/01 20:10:06 thb 1.2.36.4: #i85898# Stripping all external header guards, now manually fixing misspelled ones and other compile-time breakage 2008/04/01 16:06:45 thb 1.2.36.3: #i85898# Stripping all external header guards 2008/04/01 13:02:31 thb 1.2.36.2: #i85898# Stripping all external header guards 2008/03/28 15:53:02 rt 1.2.36.1: #i87441# Change license header to LPGL v3.

--- writerfilter/source/filter/WriterFilterDetection.cxx
+++ writerfilter/source/filter/WriterFilterDetection.cxx
@@ -1,63 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: WriterFilterDetection.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: os $ $Date: 2007-03-08 09:18:02 $
+ * $RCSfile: WriterFilterDetection.cxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
-#ifndef _CPPUHELPER_IMPLEMENTATIONENTRY_
 #include <cppuhelper/implementationentry.hxx>
-#endif
-
-#ifndef _WriterFilterDetection_DETECTION_HXX
 #include <WriterFilterDetection.hxx>
-#endif
-#ifndef _COMPHELPER_STORAGEHELPER_HXX
 #include <comphelper/storagehelper.hxx>
-#endif
-#ifndef _COM_SUN_STAR_EMBED_ELEMENTMODES_HPP_
-#include <com/sun/star/embed/ElementModes.HPP>
-#endif
-#ifndef _COM_SUN_STAR_IO_XINPUTSTREAM_HPP_
 #include <com/sun/star/io/XInputStream.hpp>
-#endif
-#ifndef _SOT_STORAGE_HXX
 #include <sot/storage.hxx>
-#endif
 //#ifndef _SFXDOCFILE_HXX //todo: remove sfx2!
 //#include <sfx2/docfile.hxx>
 //#endif
-#ifndef _UNOTOOLS_STREAMHELPER_HXX_
 #include <unotools/ucbstreamhelper.hxx>
-#endif
+
 using namespace ::rtl;
 using namespace ::cppu;
 using namespace ::com::sun::star;
