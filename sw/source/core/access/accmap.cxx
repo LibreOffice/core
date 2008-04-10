d7b01358d0e3d33e98e418abbc2689a951e14916
Rüdiger Timm <rt@openoffice.org>
2008-04-10 11:40:53 +0000

    INTEGRATION: CWS changefileheader (1.58.34); FILE MERGED 2008/04/01 15:56:42 thb 1.58.34.3: #i85898# Stripping all external header guards 2008/04/01 12:53:45 thb 1.58.34.2: #i85898# Stripping all external header guards 2008/03/31 16:53:39 rt 1.58.34.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/access/accmap.cxx
+++ sw/source/core/access/accmap.cxx
@@ -1,35 +1,30 @@
  /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: accmap.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.58 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 12:15:14 $
+ * $RCSfile: accmap.cxx,v $
+ * $Revision: 1.59 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,125 +32,53 @@
 #include "precompiled_sw.hxx"
 
 
-#ifndef _VOS_REF_HXX_
 #include <vos/ref.hxx>
-#endif
-#ifndef _CPPUHELPER_WEAKREF_HXX_
 #include <cppuhelper/weakref.hxx>
-#endif
-#ifndef _SV_WINDOW_HXX
 #include <vcl/window.hxx>
-#endif
-#ifndef _SVDMODEL_HXX
 #include <svx/svdmodel.hxx>
-#endif
-#ifndef SVX_UNOMOD_HXX
 #include <svx/unomod.hxx>
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 
 #include <map>
 #include <list>
-#ifndef _ACCMAP_HXX
 #include <accmap.hxx>
-#endif
 #ifndef _ACCCONTEXT_HXX
 #include <acccontext.hxx>
 #endif
-#ifndef _ACCDOC_HXX
 #include <accdoc.hxx>
-#endif
-#ifndef _ACCPREVIEW_HXX
 #include <accpreview.hxx>
-#endif
-#ifndef _ACCPAGE_HXX
 #include <accpage.hxx>
-#endif
-#ifndef _ACCPARA_HXX
 #include <accpara.hxx>
-#endif
-#ifndef _ACCHEADERFOOTER_HXX
 #include <accheaderfooter.hxx>
-#endif
-#ifndef _ACCFOOTNOTE_HXX
 #include <accfootnote.hxx>
-#endif
-#ifndef _ACCTEXTFRAME_HXX
 #include <acctextframe.hxx>
-#endif
-#ifndef _ACCGRAPHIC_HXX
 #include <accgraphic.hxx>
-#endif
-#ifndef _ACCEMBEDDED_HXX
 #include <accembedded.hxx>
-#endif
-#ifndef _ACCCELL_HXX
 #include <acccell.hxx>
-#endif
-#ifndef _ACCTABLE_HXX
 #include <acctable.hxx>
-#endif
-#ifndef _FESH_HXX
 #include "fesh.hxx"
-#endif
-#ifndef _ROOTFRM_HXX
 #include <rootfrm.hxx>
-#endif
-#ifndef _TXTFRM_HXX
 #include <txtfrm.hxx>
-#endif
-#ifndef _HFFRM_HXX
 #include <hffrm.hxx>
-#endif
-#ifndef _FTNFRM_HXX
 #include <ftnfrm.hxx>
-#endif
-#ifndef _CELLFRM_HXX
 #include <cellfrm.hxx>
-#endif
-#ifndef _TABFRM_HXX
 #include <tabfrm.hxx>
-#endif
-#ifndef _PAGEFRM_HXX
 #include <pagefrm.hxx>
-#endif
-#ifndef _NDTYP_HXX
 #include <ndtyp.hxx>
-#endif
 #ifndef IDOCUMENTDRAWMODELACCESS_HXX_INCLUDED
 #include <IDocumentDrawModelAccess.hxx>
 #endif
-#ifndef _SVX_ACCESSIBILITY_SHAPE_TYPE_HANDLER_HXX
 #include <svx/ShapeTypeHandler.hxx>
-#endif
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLERELATIONTYPE_HPP_
 #include <com/sun/star/accessibility/AccessibleRelationType.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEEVENTID_HPP_
 #include <com/sun/star/accessibility/AccessibleEventId.hpp>
-#endif
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLESTATETYPE_HPP_
 #include <com/sun/star/accessibility/AccessibleStateType.hpp>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE1_HXX_
 #include <cppuhelper/implbase1.hxx>
-#endif
 // OD 15.01.2003 #103492#
-#ifndef _PAGEPREVIEWLAYOUT_HXX
 #include <pagepreviewlayout.hxx>
-#endif
 // --> OD 2005-12-13 #i27301#
-#ifndef _PAM_HXX
 #include <pam.hxx>
-#endif
-#ifndef _NDTXT_HXX
 #include <ndtxt.hxx>
-#endif
 // <--
 
 using namespace ::com::sun::star;
