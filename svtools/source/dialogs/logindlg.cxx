d86b6fb5b5d9c1fbb4cd3fa6139a0acc229256b5
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:35:06 +0000

    INTEGRATION: CWS changefileheader (1.9.228); FILE MERGED 2008/04/01 15:45:00 thb 1.9.228.3: #i85898# Stripping all external header guards 2008/04/01 12:43:30 thb 1.9.228.2: #i85898# Stripping all external header guards 2008/03/31 13:01:43 rt 1.9.228.1: #i87441# Change license header to LPGL v3.

--- svtools/source/dialogs/logindlg.cxx
+++ svtools/source/dialogs/logindlg.cxx
@@ -1,51 +1,38 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: logindlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-07-10 15:16:36 $
+ * $RCSfile: logindlg.cxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
-
-#ifndef _SVT_FILEDLG_HXX
 #include <filedlg.hxx>
-#endif
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
-
-#ifndef _SVTOOLS_LOGINDLG_HXX_
 #include <svtools/logindlg.hxx>
-#endif
 
 #ifndef _SVTOOLS_LOGINDLG_HRC_
 #include "logindlg.hrc"
@@ -53,10 +40,7 @@
 #ifndef _SVTOOLS_HRC
 #include <svtools/svtools.hrc>
 #endif
-
-#ifndef _SVTOOLS_SVTDATA_HXX
 #include <svtools/svtdata.hxx>
-#endif
 
 #ifdef UNX
 #include <limits.h>
