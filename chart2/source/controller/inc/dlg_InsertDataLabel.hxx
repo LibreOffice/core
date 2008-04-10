b51214ce23993a14351b3eb4808a81b4dabf9aa7
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:40:59 +0000

    INTEGRATION: CWS changefileheader (1.5.66); FILE MERGED 2008/04/01 15:04:08 thb 1.5.66.2: #i85898# Stripping all external header guards 2008/03/28 16:43:46 rt 1.5.66.1: #i87441# Change license header to LPGL v3.

--- chart2/source/controller/inc/dlg_InsertDataLabel.hxx
+++ chart2/source/controller/inc/dlg_InsertDataLabel.hxx
@@ -1,52 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dlg_InsertDataLabel.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-10-22 16:50:23 $
+ * $RCSfile: dlg_InsertDataLabel.hxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef CHART2_DLG_INSERT_DATALABELS_GRID_HXX
 #define CHART2_DLG_INSERT_DATALABELS_GRID_HXX
 
 // header for class ModalDialog
-#ifndef _SV_DIALOG_HXX
 #include <vcl/dialog.hxx>
-#endif
 // header for class CheckBox
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
 // header for class SfxItemSet
-#ifndef _SFXITEMSET_HXX
 #include <svtools/itemset.hxx>
-#endif
 //for auto_ptr
 #include <memory>
 
