f6468a896e1d13b6838f5495be1bd45cc04be166
Rüdiger Timm <rt@openoffice.org>
2008-04-10 09:12:18 +0000

    INTEGRATION: CWS changefileheader (1.6.62); FILE MERGED 2008/04/01 12:39:57 thb 1.6.62.2: #i85898# Stripping all external header guards 2008/03/31 16:07:59 rt 1.6.62.1: #i87441# Change license header to LPGL v3.

--- setup_native/source/win32/customactions/reg4msdoc/register.cxx
+++ setup_native/source/win32/customactions/reg4msdoc/register.cxx
@@ -1,65 +1,40 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: register.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:29:29 $
+ * $RCSfile: register.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
-#ifndef _REGISTER_HXX_
 #include "register.hxx"
-#endif
-
-#ifndef _REGISTRYEXCEPTION_HXX_
 #include "registryexception.hxx"
-#endif
-
-#ifndef _REGISTRATIONCONTEXTINFORMATION_HXX_
 #include "registrationcontextinformation.hxx"
-#endif
-
-#ifndef _USERREGISTRAR_HXX_
 #include "userregistrar.hxx"
-#endif
-
-#ifndef _WINDOWSREGISTRY_HXX_
 #include "windowsregistry.hxx"
-#endif
-
-#ifndef _STRINGCONVERTER_HXX_
 #include "stringconverter.hxx"
-#endif
-
-#ifndef INCLUDED_MSIHELPER_HXX
 #include "msihelper.hxx"
-#endif
 
 #ifdef _MSC_VER
 #pragma warning(push, 1) /* disable warnings within system headers */
