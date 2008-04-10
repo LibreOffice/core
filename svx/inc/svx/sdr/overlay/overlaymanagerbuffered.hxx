f7de39432ba33944c80ebf95b5f30216b48746f3
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:22:37 +0000

    INTEGRATION: CWS changefileheader (1.4.368); FILE MERGED 2008/04/01 15:50:05 thb 1.4.368.3: #i85898# Stripping all external header guards 2008/04/01 12:47:53 thb 1.4.368.2: #i85898# Stripping all external header guards 2008/03/31 14:19:02 rt 1.4.368.1: #i87441# Change license header to LPGL v3.

--- svx/inc/svx/sdr/overlay/overlaymanagerbuffered.hxx
+++ svx/inc/svx/sdr/overlay/overlaymanagerbuffered.hxx
@@ -1,52 +1,39 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: overlaymanagerbuffered.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-26 12:03:42 $
+ * $RCSfile: overlaymanagerbuffered.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SDR_OVERLAY_OVERLAYMANAGERBUFFERED_HXX
 #define _SDR_OVERLAY_OVERLAYMANAGERBUFFERED_HXX
 
-#ifndef _SDR_OVERLAY_OVERLAYMANAGER_HXX
 #include <svx/sdr/overlay/overlaymanager.hxx>
-#endif
-
-#ifndef _BGFX_RANGE_B2IRANGE_HXX
 #include <basegfx/range/b2irange.hxx>
-#endif
-
-#ifndef _SV_VIRDEV_HXX
 #include <vcl/virdev.hxx>
-#endif
 
 //////////////////////////////////////////////////////////////////////////////
 // predeclarations
