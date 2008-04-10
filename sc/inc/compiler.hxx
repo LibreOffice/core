b7d142bc0ff85a534f19b1ea519adb12ca09f8b0
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:21:04 +0000

    INTEGRATION: CWS changefileheader (1.35.34); FILE MERGED 2008/04/01 15:29:22 thb 1.35.34.3: #i85898# Stripping all external header guards 2008/04/01 12:35:52 thb 1.35.34.2: #i85898# Stripping all external header guards 2008/03/31 17:13:27 rt 1.35.34.1: #i87441# Change license header to LPGL v3.

--- sc/inc/compiler.hxx
+++ sc/inc/compiler.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: compiler.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.35 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-06 15:15:10 $
+ * $RCSfile: compiler.hxx,v $
+ * $Revision: 1.36 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,43 +35,16 @@
 #include <string.h>
 #define INCLUDED_STRING_H
 #endif
-
-#ifndef _SVMEMPOOL_HXX
 #include <tools/mempool.hxx>
-#endif
-
-#ifndef INCLUDED_SCDLLAPI_H
 #include "scdllapi.h"
-#endif
-
-#ifndef SC_SCGLOB_HXX
 #include "global.hxx"
-#endif
-
-#ifndef SC_TOKENARRAY_HXX
 #include "tokenarray.hxx"
-#endif
-#ifndef SC_TOKEN_HXX
 #include "token.hxx"
-#endif
-#ifndef SC_INTRUREF_HXX
 #include "intruref.hxx"
-#endif
-#ifndef SC_GRAMMAR_HXX
 #include "grammar.hxx"
-#endif
-
-#ifndef _UNOTOOLS_CHARCLASS_HXX
 #include <unotools/charclass.hxx>
-#endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
-#endif
 
 namespace com { namespace sun { namespace star {
     namespace sheet {
