817e2a7b6dbec4af7b05fbe09ed33823cc77d00b
Rüdiger Timm <rt@openoffice.org>
2008-04-10 16:42:58 +0000

    INTEGRATION: CWS changefileheader (1.9.34); FILE MERGED 2008/04/01 15:58:09 thb 1.9.34.3: #i85898# Stripping all external header guards 2008/04/01 12:54:56 thb 1.9.34.2: #i85898# Stripping all external header guards 2008/03/31 16:56:11 rt 1.9.34.1: #i87441# Change license header to LPGL v3.

--- sw/source/filter/xml/xmlithlp.cxx
+++ sw/source/filter/xml/xmlithlp.cxx
@@ -1,103 +1,54 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlithlp.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 12:41:26 $
+ * $RCSfile: xmlithlp.cxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
-#ifndef _SW_XMLITHLP_HXX
 #include "xmlithlp.hxx"
-#endif
-
-#ifndef _HINTIDS_HXX
 #include "hintids.hxx"
-#endif
-
-#ifndef SW_UNOMID_HXX
 #include "unomid.h"
-#endif
-
-#ifndef _SVX_UNOMID_HXX
 #include <svx/unomid.hxx>
-#endif
-
-#ifndef _SVX_LRSPITEM_HXX
 #include <svx/lrspitem.hxx>
-#endif
-
-#ifndef _SVX_ULSPITEM_HXX
 #include <svx/ulspitem.hxx>
-#endif
-
-#ifndef _SVX_SHADITEM_HXX
 #include <svx/shaditem.hxx>
-#endif
-
-#ifndef _SVX_BOXITEM_HXX
 #include <svx/boxitem.hxx>
-#endif
-
-#ifndef _SVX_BRKITEM_HXX
 #include <svx/brkitem.hxx>
-#endif
-
-#ifndef _SVX_KEEPITEM_HXX
 #include <svx/keepitem.hxx>
-#endif
-
-#ifndef _SVX_BRSHITEM_HXX
 #include <svx/brshitem.hxx>
-#endif
-
-#ifndef _FMTPDSC_HXX
 #include "fmtpdsc.hxx"
-#endif
-
-#ifndef _FMTORNT_HXX
 #include "fmtornt.hxx"
-#endif
-
-#ifndef _FMTFSIZE_HXX
 #include "fmtfsize.hxx"
-#endif
 
 #ifndef _FMTLSPLT_HXX
 #include "fmtlsplt.hxx"
 #endif
-
-#ifndef _XMLOFF_XMLUCONV_HXX
 #include <xmloff/xmluconv.hxx>
-#endif
 
 using ::rtl::OUString;
 using namespace ::xmloff::token;
