1eb88bb6701e04d6fc4367f94456502debcba8be
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:13:05 +0000

    INTEGRATION: CWS changefileheader (1.19.222); FILE MERGED 2008/04/01 15:30:08 thb 1.19.222.3: #i85898# Stripping all external header guards 2008/04/01 12:36:11 thb 1.19.222.2: #i85898# Stripping all external header guards 2008/03/31 17:14:31 rt 1.19.222.1: #i87441# Change license header to LPGL v3.

--- sc/source/filter/excel/xepivot.cxx
+++ sc/source/filter/excel/xepivot.cxx
@@ -1,106 +1,59 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xepivot.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-08-02 13:30:55 $
+ * $RCSfile: xepivot.cxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sc.hxx"
-
-#ifndef SC_XEPIVOT_HXX
 #include "xepivot.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDSORTINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldSortInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDAUTOSHOWINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldAutoShowInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDLAYOUTINFO_HPP_
 #include <com/sun/star/sheet/DataPilotFieldLayoutInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDREFERENCE_HPP_
 #include <com/sun/star/sheet/DataPilotFieldReference.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SHEET_DATAPILOTFIELDGROUPBY_HPP_
 #include <com/sun/star/sheet/DataPilotFieldGroupBy.hpp>
-#endif
 
 #include <algorithm>
 #include <math.h>
-
-#ifndef INCLUDED_RTL_MATH_HXX
 #include <rtl/math.hxx>
-#endif
-#ifndef _DATE_HXX
 #include <tools/date.hxx>
-#endif
-#ifndef _ZFORMAT_HXX
 #include <svtools/zformat.hxx>
-#endif
-
-#ifndef _SOT_STORAGE_HXX
 #include <sot/storage.hxx>
-#endif
-
-#ifndef SC_DOCUMENT_HXX
 #include "document.hxx"
-#endif
-#ifndef SC_DPOBJECT_HXX
 #include "dpobject.hxx"
-#endif
-#ifndef SC_DPSAVE_HXX
 #include "dpsave.hxx"
-#endif
-#ifndef SC_DPDIMSAVE_HXX
 #include "dpdimsave.hxx"
-#endif
-#ifndef SC_DPSHTTAB_HXX
 #include "dpshttab.hxx"
-#endif
 #ifndef __GLOBSTR_HRC_
 #include "globstr.hrc"
 #endif
-
-#ifndef SC_FAPIHELPER_HXX
 #include "fapihelper.hxx"
-#endif
-#ifndef SC_XESTRING_HXX
 #include "xestring.hxx"
-#endif
-#ifndef SC_XELINK_HXX
 #include "xelink.hxx"
-#endif
 
 using ::com::sun::star::sheet::DataPilotFieldOrientation;
 using ::com::sun::star::sheet::DataPilotFieldOrientation_HIDDEN;
