7ae6faf795d24681bc216ab3536ee6f6182c3455
Rüdiger Timm <rt@openoffice.org>
2008-04-10 23:15:01 +0000

    INTEGRATION: CWS changefileheader (1.31.340); FILE MERGED 2008/04/01 15:50:35 thb 1.31.340.3: #i85898# Stripping all external header guards 2008/04/01 12:48:24 thb 1.31.340.2: #i85898# Stripping all external header guards 2008/03/31 14:20:08 rt 1.31.340.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/transfrm.cxx
+++ svx/source/dialog/transfrm.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: transfrm.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.32 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-04 10:54:40 $
+ * $RCSfile: transfrm.cxx,v $
+ * $Revision: 1.33 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,30 +38,13 @@
 // include ---------------------------------------------------------------
 
 
-
-#ifndef _SHL_HXX //autogen
 #include <tools/shl.hxx>
-#endif
-
-#ifndef _SFXAPP_HXX //autogen
 #include <sfx2/app.hxx>
-#endif
-
-#ifndef _SVDVIEW_HXX //autogen
 #include <svx/svdview.hxx>
-#endif
-#ifndef _SVDOBJ_HXX //autogen
 #include <svx/svdobj.hxx>
-#endif
-#ifndef _SVDPAGV_HXX //autogen
 #include <svx/svdpagv.hxx>
-#endif
-#ifndef _SVDOTEXT_HXX //autogen
 #include <svx/svdotext.hxx>
-#endif
-#ifndef _SDERITM_HXX //autogen
 #include <svx/sderitm.hxx>
-#endif
 
 #include <svx/svxids.hrc>
 #include <svx/dialogs.hrc>
@@ -78,21 +56,10 @@
 #include "dlgutil.hxx"
 #include <svx/svxenum.hxx>
 #include "anchorid.hxx"
-
-#ifndef _SFXMODULE_HXX
 #include <sfx2/module.hxx>
-#endif
-
-#ifndef _SFXRECTITEM_HXX //autogen
 #include <svtools/rectitem.hxx>
-#endif
-
-#ifndef _AEITEM_HXX //autogen
 #include <svtools/aeitem.hxx>
-#endif
-#ifndef _SVX_SWPOSSIZETABPAGE_HXX
 #include <swpossizetabpage.hxx>
-#endif
 
 // Toleranz fuer WorkingArea
 #define DIFF 1000
