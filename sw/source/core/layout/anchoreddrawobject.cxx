0944cc87c0ece007622874129d7fc1bd9576495f
Rüdiger Timm <rt@openoffice.org>
2008-04-10 13:37:47 +0000

    INTEGRATION: CWS changefileheader (1.24.118); FILE MERGED 2008/04/01 15:57:14 thb 1.24.118.3: #i85898# Stripping all external header guards 2008/04/01 12:54:12 thb 1.24.118.2: #i85898# Stripping all external header guards 2008/03/31 16:54:20 rt 1.24.118.1: #i87441# Change license header to LPGL v3.

--- sw/source/core/layout/anchoreddrawobject.cxx
+++ sw/source/core/layout/anchoreddrawobject.cxx
@@ -1,77 +1,52 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: anchoreddrawobject.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.24 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2008-01-04 13:21:14 $
+ * $RCSfile: anchoreddrawobject.cxx,v $
+ * $Revision: 1.25 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sw.hxx"
-#ifndef _ANCHOREDDRAWOBJECT_HXX
 #include <anchoreddrawobject.hxx>
-#endif
-#ifndef _SVDOBJ_HXX
 #include <svx/svdobj.hxx>
-#endif
-#ifndef _DCONTACT_HXX
 #include <dcontact.hxx>
-#endif
-#ifndef _ROOTFRM_HXX
 #include <rootfrm.hxx>
-#endif
-#ifndef _PAGEFRM_HXX
 #include <pagefrm.hxx>
-#endif
-#ifndef _CNTFRM_HXX
 #include <cntfrm.hxx>
-#endif
-#ifndef _DOC_HXX
 #include <doc.hxx>
-#endif
 #ifndef _TOCNTNTANCHOROBJECTPOSITION_HXX
 #include <tocntntanchoredobjectposition.hxx>
 #endif
 #ifndef _TOLAYOUTANCHOROBJECTPOSITION_HXX
 #include <tolayoutanchoredobjectposition.hxx>
 #endif
-#ifndef _FRMTOOL_HXX
 #include <frmtool.hxx>
-#endif
-#ifndef _FMTORNT_HXX
 #include <fmtornt.hxx>
-#endif
 // --> OD 2004-08-12 #i32795#
-#ifndef _TXTFRM_HXX
 #include <txtfrm.hxx>
-#endif
 // <--
 // --> OD 2004-08-12 #i32795#
 // template class <std::vector>
@@ -79,14 +54,10 @@
 // <--
 
 // --> OD 2004-08-10 #i28749#
-#ifndef _COM_SUN_STAR_TEXT_POSITIONLAYOUTDIR_HPP_
 #include <com/sun/star/text/PositionLayoutDir.hpp>
-#endif
 // <--
 // --> OD 2005-03-09 #i44559#
-#ifndef _NDTXT_HXX
 #include <ndtxt.hxx>
-#endif
 // <--
 
 using namespace ::com::sun::star;
