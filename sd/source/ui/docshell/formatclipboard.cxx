c95a2a50fd4a7e7837dc5e0a46536421265fc439
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:00:30 +0000

    INTEGRATION: CWS changefileheader (1.8.298); FILE MERGED 2008/04/01 15:34:25 thb 1.8.298.2: #i85898# Stripping all external header guards 2008/03/31 13:57:59 rt 1.8.298.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/docshell/formatclipboard.cxx
+++ sd/source/ui/docshell/formatclipboard.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: formatclipboard.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 17:12:52 $
+ * $RCSfile: formatclipboard.cxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,20 +32,13 @@
 #include "precompiled_sd.hxx"
 
 #include "formatclipboard.hxx"
-
-#ifndef _E3D_GLOBL3D_HXX
 #include <svx/globl3d.hxx>
-#endif
 
 // header for class SfxItemIter
-#ifndef _SFXITEMITER_HXX
 #include <svtools/itemiter.hxx>
-#endif
 
 // header for class SfxStyleSheet
-#ifndef _SFXSTYLE_HXX
 #include <svtools/style.hxx>
-#endif
 
 /*--------------------------------------------------------------------
  --------------------------------------------------------------------*/
