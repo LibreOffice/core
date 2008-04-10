79fc00f3fd0bf2f28e048d4f1e24d6b582a877bf
Rüdiger Timm <rt@openoffice.org>
2008-04-10 22:09:19 +0000

    INTEGRATION: CWS changefileheader (1.20.100); FILE MERGED 2008/04/01 15:36:29 thb 1.20.100.3: #i85898# Stripping all external header guards 2008/04/01 12:39:37 thb 1.20.100.2: #i85898# Stripping all external header guards 2008/03/31 13:59:12 rt 1.20.100.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/view/drviews8.cxx
+++ sd/source/ui/view/drviews8.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: drviews8.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.20 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-26 17:03:54 $
+ * $RCSfile: drviews8.cxx,v $
+ * $Revision: 1.21 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,131 +38,58 @@
 #ifndef _TOOLKIT_HELPER_VCLUNOHELPER_HXX_
 #include <toolkit/unohlp.hxx>
 #endif
-#ifndef _SFXDISPATCH_HXX
 #include <sfx2/dispatch.hxx>
-#endif
 #ifndef _SVXIDS_HXX
 #include <svx/svxids.hrc>
 #endif
-#ifndef _SV_MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SVDDEF_HXX //autogen
 #include <svx/svddef.hxx>
-#endif
-#ifndef _SFX_BINDINGS_HXX //autogen
 #include <sfx2/bindings.hxx>
-#endif
-#ifndef _SFXREQUEST_HXX //autogen
 #include <sfx2/request.hxx>
-#endif
-#ifndef _SFXVIEWFRM_HXX
 #include <sfx2/viewfrm.hxx>
-#endif
-#ifndef _SVDOGRAF_HXX //autogen
 #include <svx/svdograf.hxx>
-#endif
-#ifndef _SVDPAGV_HXX //autogen
 #include <svx/svdpagv.hxx>
-#endif
 
 #include "app.hrc"
 #include "strings.hrc"
 
 #include "misc.hxx"
 #include "fuzoom.hxx"
-#ifndef SD_FU_DISPLAY_ORDER_HXX
 #include "fudspord.hxx"
-#endif
-#ifndef SD_FU_TRANSFORM_HXX
 #include "futransf.hxx"
-#endif
-#ifndef SD_FU_LINE_HXX
 #include "fuline.hxx"
-#endif
-#ifndef SD_FU_AREA_HXX
 #include "fuarea.hxx"
-#endif
-#ifndef SD_FU_CHAR_HXX
 #include "fuchar.hxx"
-#endif
-#ifndef SD_FU_PARAGRAPH_HXX
 #include "fuparagr.hxx"
-#endif
-#ifndef SD_FU_BULLET_HXX
 #include "fubullet.hxx"
-#endif
-#ifndef SD_FU_TEMPLATE_HXX
 #include "futempl.hxx"
-#endif
-#ifndef SD_FU_INSERT_HXX
 #include "fuinsert.hxx"
-#endif
-#ifndef SD_FU_PRESENTATION_LAYOUT_HXX
 #include "fuprlout.hxx"
-#endif
-#ifndef SD_FU_PAGE_HXX
 #include "fupage.hxx"
-#endif
 #ifndef SD_FU_OBJECT_ANIMATION_PARAMETERS_HXX
 #include "fuoaprms.hxx"
 #endif
-#ifndef SD_FU_COPY_HXX
 #include "fucopy.hxx"
-#endif
 #ifndef SD_FU_LINE_END_HXX
 #include "fulinend.hxx"
 #endif
-#ifndef SD_FU_SNAP_LINE_HXX
 #include "fusnapln.hxx"
-#endif
-#ifndef SD_FU_OUTLINE_BULLET_HXX
 #include "fuolbull.hxx"
-#endif
-#ifndef SD_FU_INSERT_FILE_HXX
 #include "fuinsfil.hxx"
-#endif
-#ifndef SD_FU_LINK_HXX
 #include "fulink.hxx"
-#endif
-#ifndef SD_FU_THESAURUS_HXX
 #include "futhes.hxx"
-#endif
-#ifndef SD_FU_TEXT_ATTR_DLG_HXX
 #include "futxtatt.hxx"
-#endif
-#ifndef SD_FU_MEASURE_DLG_HXX
 #include "fumeasur.hxx"
-#endif
-#ifndef SD_FU_CONNECTION_DLG_HXX
 #include "fuconnct.hxx"
-#endif
-#ifndef SD_FU_MORPH_HXX
 #include "fumorph.hxx"
-#endif
-#ifndef SD_FU_VECTORIZE_HXX
 #include "fuvect.hxx"
-#endif
 #include "sdresid.hxx"
-#ifndef SD_WINDOW_HXX
 #include "Window.hxx"
-#endif
-#ifndef SD_DRAW_VIEW_HXX
 #include "drawview.hxx"
-#endif
 #include "zoomlist.hxx"
-
-#ifndef _VOS_MUTEX_HXX_
 #include <vos/mutex.hxx>
-#endif
-
-#ifndef _SV_SALBTYPE_HXX
 #include <vcl/salbtype.hxx>     // FRound
-#endif
-
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
 
 namespace sd {
 
