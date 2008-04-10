32a0fd3604fc54f76275d2fcf6ebcfbf33603235
Rüdiger Timm <rt@openoffice.org>
2008-04-10 20:56:35 +0000

    INTEGRATION: CWS changefileheader (1.6.368); FILE MERGED 2008/04/01 15:50:12 thb 1.6.368.3: #i85898# Stripping all external header guards 2008/04/01 12:48:04 thb 1.6.368.2: #i85898# Stripping all external header guards 2008/03/31 14:19:18 rt 1.6.368.1: #i87441# Change license header to LPGL v3.

--- svx/source/dialog/align.hxx
+++ svx/source/dialog/align.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: align.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 16:46:32 $
+ * $RCSfile: align.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _SVX_ALIGN_HXX
@@ -50,26 +45,12 @@
 #ifndef _LSTBOX_HXX //autogen
 #include <vcl/lstbox.hxx>
 #endif
-#ifndef _SFXTABDLG_HXX //autogen
 #include <sfx2/tabdlg.hxx>
-#endif
-
-#ifndef _VALUESET_HXX
 #include <svtools/valueset.hxx>
-#endif
-
-#ifndef SVX_DIALCONTROL_HXX
 #include "dialcontrol.hxx"
-#endif
-#ifndef SVX_WRAPFIELD_HXX
 #include "wrapfield.hxx"
-#endif
-#ifndef SVX_ORIENTHELPER_HXX
 #include "orienthelper.hxx"
-#endif
-#ifndef _SVX_FRMDIRLBOX_HXX
 #include <svx/frmdirlbox.hxx>
-#endif
 
 namespace svx {
 
