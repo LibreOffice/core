b0b9f5d84b33879acdf90a02335fc11f3634b137
Rüdiger Timm <rt@openoffice.org>
2008-04-10 17:30:28 +0000

    INTEGRATION: CWS changefileheader (1.6.8); FILE MERGED 2008/04/01 15:23:36 thb 1.6.8.3: #i85898# Stripping all external header guards 2008/04/01 12:33:08 thb 1.6.8.2: #i85898# Stripping all external header guards 2008/03/31 13:32:06 rt 1.6.8.1: #i87441# Change license header to LPGL v3.

--- reportdesign/source/filter/xml/xmlCell.cxx
+++ reportdesign/source/filter/xml/xmlCell.cxx
@@ -1,75 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xmlCell.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 18:00:56 $
+ * $RCSfile: xmlCell.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include "precompiled_reportdesign.hxx"
-
-#ifndef RPT_XMLCELL_HXX
 #include "xmlCell.hxx"
-#endif
-#ifndef RPT_XMLHELPER_HXX
 #include "xmlHelper.hxx"
-#endif
-#ifndef _XMLOFF_XMLUCONV_HXX
 #include <xmloff/xmluconv.hxx>
-#endif
-#ifndef RPT_XMLFILTER_HXX
 #include "xmlfilter.hxx"
-#endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
 #include <xmloff/xmltoken.hxx>
-#endif
-#ifndef _XMLOFF_XMLNMSPE_HXX
 #include <xmloff/xmlnmspe.hxx>
-#endif
-#ifndef _XMLOFF_NMSPMAP_HXX
 #include <xmloff/nmspmap.hxx>
-#endif
-#ifndef RPT_XMLENUMS_HXX
 #include "xmlEnums.hxx"
-#endif
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef RPT_XMLSTYLEIMPORT_HXX
 #include "xmlStyleImport.hxx"
-#endif
-#ifndef _COMPHELPER_NAMECONTAINER_HXX_
 #include <comphelper/namecontainer.hxx>
-#endif
-#ifndef _COMPHELPER_GENERICPROPERTYSET_HXX_
 #include <comphelper/genericpropertyset.hxx>
-#endif
 #ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HXX_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
 #endif
@@ -79,21 +49,11 @@
 #ifndef RPT_SHARED_XMLSTRINGS_HRC
 #include "xmlstrings.hrc"
 #endif
-#ifndef RPT_XMLTABLE_HXX
 #include "xmlTable.hxx"
-#endif
-#ifndef RPT_XMLFIXEDTEXT_HXX
 #include "xmlFixedText.hxx"
-#endif
-#ifndef RPT_XMLFORMATTEDFIELD_HXX
 #include "xmlFormattedField.hxx"
-#endif
-#ifndef RPT_XMLIMAGE_HXX
 #include "xmlImage.hxx"
-#endif
-#ifndef RPT_XMLFIXEDCONTENT_HXX
 #include "xmlFixedContent.hxx"
-#endif
 #include "xmlSubDocument.hxx"
 
 namespace rptxml
