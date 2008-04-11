bbfc76f690c43fe5348163638d4b23bfa784bdc2
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:08:35 +0000

    INTEGRATION: CWS changefileheader (1.13.20); FILE MERGED 2008/04/01 15:04:47 thb 1.13.20.3: #i85898# Stripping all external header guards 2008/04/01 12:26:07 thb 1.13.20.2: #i85898# Stripping all external header guards 2008/03/31 07:22:53 rt 1.13.20.1: #i87441# Change license header to LPGL v3.

--- codemaker/source/cppumaker/cpputype.hxx
+++ codemaker/source/cppumaker/cpputype.hxx
@@ -1,62 +1,45 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: cpputype.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-07-31 13:48:15 $
+ * $RCSfile: cpputype.hxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef INCLUDED_CODEMAKER_SOURCE_CPPUMAKER_CPPUTYPE_HXX
 #define INCLUDED_CODEMAKER_SOURCE_CPPUMAKER_CPPUTYPE_HXX
 
-#ifndef INCLUDED_CODEMAKER_DEPENDENCIES_HXX
 #include "codemaker/dependencies.hxx"
-#endif
-#ifndef INCLUDED_CODEMAKER_OPTIONS_HXX
 #include "codemaker/options.hxx"
-#endif
-#ifndef INCLUDED_CODEMAKER_TYPEMANAGER_HXX
 #include "codemaker/typemanager.hxx"
-#endif
-#ifndef INCLUDED_CODEMAKER_COMMONCPP_HXX
 #include "codemaker/commoncpp.hxx"
-#endif
 #ifndef INCLUDED_regisry_reader_hxx
 #include "registry/reader.hxx"
 #endif
-#ifndef INCLUDED_registry_types_h
 #include "registry/types.h"
-#endif
-#ifndef _RTL_STRING_HXX_
 #include "rtl/string.hxx"
-#endif
 
 namespace rtl { class OUString; }
 namespace codemaker {
