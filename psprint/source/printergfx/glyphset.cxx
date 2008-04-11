c06f626809a1b40b1795d0d464228de85c757e2e
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:23:19 +0000

    INTEGRATION: CWS changefileheader (1.23.64); FILE MERGED 2008/04/01 15:22:46 thb 1.23.64.3: #i85898# Stripping all external header guards 2008/04/01 11:00:36 thb 1.23.64.2: #i85898# Stripping all external header guards 2008/03/28 15:38:32 rt 1.23.64.1: #i87441# Change license header to LPGL v3.

--- psprint/source/printergfx/glyphset.cxx
+++ psprint/source/printergfx/glyphset.cxx
@@ -1,69 +1,47 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: glyphset.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.23 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-03-26 11:16:38 $
+ * $RCSfile: glyphset.cxx,v $
+ * $Revision: 1.24 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_psprint.hxx"
-
-#ifndef _OSL_THREAD_H_
 #include <osl/thread.h>
-#endif
-#ifndef _PSPRINT_GLYPHSET_HXX_
 #include <glyphset.hxx>
-#endif
-#ifndef _PSPRINT_PRINTERUTIL_HXX_
 #include <psputil.hxx>
-#endif
-#ifndef _PSPRINT_PRINTERGFX_HXX_
 #include <psprint/printergfx.hxx>
-#endif
-#ifndef _PSPRINT_FONTMANAGER_HXX_
 #include <psprint/fontmanager.hxx>
-#endif
 #ifndef __SGI_STL_SET
 #include <set>
 #endif
 #include <map>
 #include <sal/alloca.h>
-
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
-
-#ifndef _RTL_STRBUF_HXX_
 #include <rtl/strbuf.hxx>
-#endif
 
 #ifndef __SUBFONT_H
 #define NO_LIST
