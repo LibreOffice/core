81cf41b4cd35e9555330b076d5fa53a138be6cfa
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:20:14 +0000

    INTEGRATION: CWS changefileheader (1.22.44); FILE MERGED 2008/04/01 15:38:55 thb 1.22.44.2: #i85898# Stripping all external header guards 2008/03/31 13:38:10 rt 1.22.44.1: #i87441# Change license header to LPGL v3.

--- sfx2/source/bastyp/sfxhtml.cxx
+++ sfx2/source/bastyp/sfxhtml.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: sfxhtml.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.22 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-02-26 15:06:53 $
+ * $RCSfile: sfxhtml.cxx,v $
+ * $Revision: 1.23 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,45 +38,21 @@
 #include "openflag.hxx"
 
 #include <comphelper/string.hxx>
-
-#ifndef _HTMLKYWD_HXX
 #include <svtools/htmlkywd.hxx>
-#endif
-#ifndef _HTMLTOKN_H
 #include <svtools/htmltokn.h>
-#endif
-#ifndef _IMAP_HXX
 #include <svtools/imap.hxx>
-#endif
-#ifndef _GOODIES_IMAPCIRC_HXX
 #include <svtools/imapcirc.hxx>
-#endif
-#ifndef _GOODIES_IMAPOBJ_HXX
 #include <svtools/imapobj.hxx>
-#endif
-#ifndef _GOODIES_IMAPPOLY_HXX
 #include <svtools/imappoly.hxx>
-#endif
-#ifndef _GOODIES_IMAPRECT_HXX
 #include <svtools/imaprect.hxx>
-#endif
 #ifndef _SVSTDARR_ULONGS_DECL
 #define _SVSTDARR_ULONGS
 #include <svtools/svstdarr.hxx>
 #endif
-#ifndef _ZFORLIST_HXX
 #include <svtools/zforlist.hxx>
-#endif
-#ifndef _INETTYPE_HXX
 #include <svtools/inettype.hxx>
-#endif
-
-#ifndef _RTL_TENCINFO_H
 #include <rtl/tencinfo.h>
-#endif
-#ifndef _TOOLS_TENCCVT_HXX
 #include <tools/tenccvt.hxx>
-#endif
 
 #include <sfx2/sfxhtml.hxx>
 
