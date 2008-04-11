4596ded997cfb2b0527c87501d74f92796fb69ea
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:51:25 +0000

    INTEGRATION: CWS changefileheader (1.4.738); FILE MERGED 2008/04/01 15:18:32 thb 1.4.738.2: #i85898# Stripping all external header guards 2008/04/01 10:58:00 thb 1.4.738.1: #i85898# Stripping all external header guards

--- framework/inc/xml/toolboxconfiguration.hxx
+++ framework/inc/xml/toolboxconfiguration.hxx
@@ -1,33 +1,18 @@
 #ifndef __FRAMEWORK_XML_TOOLBOXCONFIGURATION_HXX_
 #define __FRAMEWORK_XML_TOOLBOXCONFIGURATION_HXX_
 
-#ifndef _SVARRAY_HXX
 #include <svtools/svarray.hxx>
-#endif
-#ifndef _SV_BITMAP_HXX
 #include <vcl/bitmap.hxx>
-#endif
-#ifndef _STRING_HXX
 #include <tools/string.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_IO_XINPUTSTREAM_HPP_
 #include <com/sun/star/io/XInputStream.hpp>
-#endif
 #ifndef _COM_SUN_STAR_IO_XOUPUTSTREAM_HPP_
 #include <com/sun/star/io/XOutputStream.hpp>
 #endif
-#ifndef _COM_SUN_STAR_CONTAINER_XINDEXCONTAINER_HPP_
 #include <com/sun/star/container/XIndexContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_XINDEXACCESS_HPP_
 #include <com/sun/star/container/XIndexAccess.hpp>
-#endif
 
 // #110897#
-#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#endif
 
 namespace framework
 {
