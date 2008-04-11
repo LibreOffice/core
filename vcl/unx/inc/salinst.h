ed26f2d8a312c8f95613a88a0f866938149c418e
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:37:40 +0000

    INTEGRATION: CWS changefileheader (1.16.22); FILE MERGED 2008/04/01 13:01:54 thb 1.16.22.2: #i85898# Stripping all external header guards 2008/03/28 15:45:16 rt 1.16.22.1: #i87441# Change license header to LPGL v3.

--- vcl/unx/inc/salinst.h
+++ vcl/unx/inc/salinst.h
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: salinst.h,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 16:43:49 $
+ * $RCSfile: salinst.h,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,22 +32,15 @@
 #ifndef _SV_SALINST_H
 #define _SV_SALINST_H
 
-#ifndef _SV_SV_H
 #include <vcl/sv.h>
-#endif
 #ifndef _VOS_MUTEX_HXX
 #include <vos/mutex.hxx>
 #endif
 #ifndef _VOS_THREAD_HXX
 #include <vos/thread.hxx>
 #endif
-#ifndef _VCL_DLLAPI_H
 #include <vcl/dllapi.h>
-#endif
-
-#ifndef _SV_SALINST_HXX
 #include <vcl/salinst.hxx>
-#endif
 
 class VCL_DLLPUBLIC SalYieldMutex : public NAMESPACE_VOS(OMutex)
 {
