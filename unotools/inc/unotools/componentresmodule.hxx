bf85103e0d8894131a4dac42717549b36806be77
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:06:52 +0000

    INTEGRATION: CWS changefileheader (1.4.20); FILE MERGED 2008/04/01 16:03:25 thb 1.4.20.3: #i85898# Stripping all external header guards 2008/04/01 12:59:33 thb 1.4.20.2: #i85898# Stripping all external header guards 2008/03/31 13:03:34 rt 1.4.20.1: #i87441# Change license header to LPGL v3.

--- unotools/inc/unotools/componentresmodule.hxx
+++ unotools/inc/unotools/componentresmodule.hxx
@@ -1,53 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: componentresmodule.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-30 09:32:14 $
+ * $RCSfile: componentresmodule.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef UNOTOOLS_INC_UNOTOOLS_COMPONENTRESMODULE_HXX
 #define UNOTOOLS_INC_UNOTOOLS_COMPONENTRESMODULE_HXX
 
-#ifndef COMPHELPER_INC_COMPHELPER_COMPONENTMODULE_HXX
 #include <comphelper/componentmodule.hxx>
-#endif
-#ifndef INCLUDED_UNOTOOLSDLLAPI_H
 #include <unotools/unotoolsdllapi.h>
-#endif
 
 /** === begin UNO includes === **/
 /** === end UNO includes === **/
-
-#ifndef _TOOLS_RESID_HXX
 #include <tools/resid.hxx>
-#endif
 
 #include <memory>
 
