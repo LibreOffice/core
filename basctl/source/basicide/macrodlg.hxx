2e262607451b37fa2cde1ddb96df6191a0b852ad
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:46:15 +0000

    INTEGRATION: CWS changefileheader (1.9.86); FILE MERGED 2008/04/01 15:00:37 thb 1.9.86.3: #i85898# Stripping all external header guards 2008/04/01 10:47:46 thb 1.9.86.2: #i85898# Stripping all external header guards 2008/03/28 16:04:48 rt 1.9.86.1: #i87441# Change license header to LPGL v3.

--- basctl/source/basicide/macrodlg.hxx
+++ basctl/source/basicide/macrodlg.hxx
@@ -1,55 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: macrodlg.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:57:16 $
+ * $RCSfile: macrodlg.hxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 //
 #ifndef _MACRODLG_HXX
 #define _MACRODLG_HXX
 
-#ifndef _SVHEADER_HXX
 #include <svheader.hxx>
-#endif
 
 #include <bastype2.hxx>
 #include <bastype3.hxx>
-
-#ifndef _BASEDLGS_HXX //autogen
 #include <sfx2/basedlgs.hxx>
-#endif
-
-#ifndef _SV_FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
-#endif
 
 #ifndef _SV_BUTTON_HXX //autogen
 #include <vcl/button.hxx>
