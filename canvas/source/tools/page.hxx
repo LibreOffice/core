8850d3fab047c3ecf23c60e7f8e4461ae765ee15
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:38:36 +0000

    INTEGRATION: CWS changefileheader (1.2.136); FILE MERGED 2008/04/01 15:03:07 thb 1.2.136.3: #i85898# Stripping all external header guards 2008/04/01 10:49:27 thb 1.2.136.2: #i85898# Stripping all external header guards 2008/03/28 16:35:12 rt 1.2.136.1: #i87441# Change license header to LPGL v3.

--- canvas/source/tools/page.hxx
+++ canvas/source/tools/page.hxx
@@ -1,56 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: page.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2005-11-02 12:54:04 $
+ * $RCSfile: page.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef INCLUDED_CANVAS_PAGE_HXX
 #define INCLUDED_CANVAS_PAGE_HXX
 
-#ifndef _BGFX_VECTOR_B2ISIZE_HXX
 #include <basegfx/vector/b2isize.hxx>
-#endif
-#ifndef _BGFX_RANGE_B2IRECTANGLE_HXX
 #include <basegfx/range/b2irectangle.hxx>
-#endif
-#ifndef INCLUDED_CANVAS_ICOLORBUFFER_HXX
 #include <canvas/rendering/icolorbuffer.hxx>
-#endif
-#ifndef INCLUDED_CANVAS_IRENDERMODULE_HXX
 #include <canvas/rendering/irendermodule.hxx>
-#endif
-#ifndef INCLUDED_CANVAS_ISURFACE_HXX
 #include <canvas/rendering/isurface.hxx>
-#endif
 
 #include <list>
 #include <vector>
