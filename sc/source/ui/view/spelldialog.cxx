097875a5f36740b3df82c13a691f8d3c5ce8ce0a
Rüdiger Timm <rt@openoffice.org>
2008-04-11 00:38:28 +0000

    INTEGRATION: CWS changefileheader (1.6.330); FILE MERGED 2008/04/01 12:37:00 thb 1.6.330.2: #i85898# Stripping all external header guards 2008/03/31 17:20:01 rt 1.6.330.1: #i87441# Change license header to LPGL v3.

--- sc/source/ui/view/spelldialog.cxx
+++ sc/source/ui/view/spelldialog.cxx
@@ -1,43 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: spelldialog.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-02-27 13:56:35 $
+ * $RCSfile: spelldialog.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sc.hxx"
-#ifndef SC_SPELLDIALOG_HXX
 #include "spelldialog.hxx"
-#endif
 
 #include <sfx2/app.hxx>
 #include <sfx2/bindings.hxx>
@@ -46,10 +39,7 @@
 #include <svx/editstat.hxx>
 #include <svx/editview.hxx>
 #include <svx/unolingu.hxx>
-
-#ifndef SC_SELECTIONSTATE_HXX
 #include "selectionstate.hxx"
-#endif
 
 #include "spelleng.hxx"
 #include "tabvwsh.hxx"
