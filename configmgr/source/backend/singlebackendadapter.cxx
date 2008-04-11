7f8c4892370976368fe5aac6c6696a1cb634a03e
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:32:47 +0000

    INTEGRATION: CWS changefileheader (1.12.72); FILE MERGED 2008/04/01 12:27:19 thb 1.12.72.2: #i85898# Stripping all external header guards 2008/03/31 12:22:41 rt 1.12.72.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/backend/singlebackendadapter.cxx
+++ configmgr/source/backend/singlebackendadapter.cxx
@@ -1,62 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: singlebackendadapter.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:06:59 $
+ * $RCSfile: singlebackendadapter.cxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_configmgr.hxx"
-
-#ifndef CONFIGMGR_BACKEND_SINGLEBACKENDADAPTER_HXX_
 #include "singlebackendadapter.hxx"
-#endif // CONFIGMGR_BACKEND_SINGLEBACKENDADAPTER_HXX_
 
 #ifndef CONFIGMGR_API_FACTORY_HXX_
 #include "confapifactory.hxx"
 #endif // CONFIGMGR_API_FACTORY_HXX_
-
-#ifndef CONFIGMGR_SERVICEINFOHELPER_HXX_
 #include "serviceinfohelper.hxx"
-#endif // CONFIGMGR_SERVICEINFOHELPER_HXX_
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XMULTILAYERSTRATUM_HPP_
 #include <com/sun/star/configuration/backend/XMultiLayerStratum.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONFIGURATION_CANNOTLOADCONFIGURATIONEXCEPTION_HPP_
 #include <com/sun/star/configuration/CannotLoadConfigurationException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
 namespace configmgr { namespace backend {
 
 //==============================================================================
