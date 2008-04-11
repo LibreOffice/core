d42b272aec234a564d33b002e6ad2a0f6592ed29
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:04:08 +0000

    INTEGRATION: CWS changefileheader (1.2.44); FILE MERGED 2008/04/01 14:58:55 thb 1.2.44.3: #i85898# Stripping all external header guards 2008/04/01 10:45:51 thb 1.2.44.2: #i85898# Stripping all external header guards 2008/03/28 15:57:50 rt 1.2.44.1: #i87441# Change license header to LPGL v3.

--- accessibility/source/extended/accessibletabbarbase.cxx
+++ accessibility/source/extended/accessibletabbarbase.cxx
@@ -1,53 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: accessibletabbarbase.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:33:22 $
+ * $RCSfile: accessibletabbarbase.cxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_accessibility.hxx"
-
-#ifndef ACCESSIBILITY_EXT_ACCESSIBLETABBARBASE_HXX
 #include "accessibility/extended/accessibletabbarbase.hxx"
-#endif
 #ifndef ACCESSIBILITY_EXT_ACCESSIBLETABBARPAGELIST
 #include "accessibility/extended/accessibletabbarpagelist.hxx"
 #endif
-#ifndef _TOOLKIT_HELPER_EXTERNALLOCK_HXX_
 #include <toolkit/helper/externallock.hxx>
-#endif
-#ifndef _TABBAR_HXX
 #include <svtools/tabbar.hxx>
-#endif
 
 //.........................................................................
 namespace accessibility
