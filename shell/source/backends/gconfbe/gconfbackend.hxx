76c0716351ba487c1e18c36aba3f6c16fcf22eb1
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:54:56 +0000

    INTEGRATION: CWS changefileheader (1.7.130); FILE MERGED 2008/04/01 15:40:07 thb 1.7.130.3: #i85898# Stripping all external header guards 2008/04/01 12:41:09 thb 1.7.130.2: #i85898# Stripping all external header guards 2008/03/31 13:17:07 rt 1.7.130.1: #i87441# Change license header to LPGL v3.

--- shell/source/backends/gconfbe/gconfbackend.hxx
+++ shell/source/backends/gconfbe/gconfbackend.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: gconfbackend.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2006-08-04 12:29:03 $
+ * $RCSfile: gconfbackend.hxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,36 +34,17 @@
 #ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSCHEMASUPPLIER_HPP_
 #include <com/sun/star/configuration/backend/XSingleLayerStratum.hpp>
 #endif
-
-#ifndef _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
 #include <com/sun/star/lang/XInitialization.hpp>
-#endif // _COM_SUN_STAR_LANG_XINITIALIZATION_HPP_
-#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
 #include <com/sun/star/uno/XComponentContext.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#endif // _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 
 //#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
 //#include <com/sun/star/lang/XMultiServiceFactory.hpp>
 //#endif // _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_INVALIDBOOTSTRAPFILEEXCEPTION_HPP_
 #include <com/sun/star/configuration/InvalidBootstrapFileException.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_CANNOTCONNECTEXCEPTION_HPP_
 #include <com/sun/star/configuration/backend/CannotConnectException.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XBACKENDCHANGESNOTIFIER_HPP_
 #include <com/sun/star/configuration/backend/XBackendChangesNotifier.hpp>
-#endif
-
-#ifndef _CPPUHELPER_COMPBASE3_HXX_
 #include <cppuhelper/compbase3.hxx>
-#endif // _CPPUHELPER_COMPBASE3_HXX_
 
 #ifndef INCLUDED_MAP
 #include <map>
