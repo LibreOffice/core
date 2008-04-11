cb76e6a204a9ef103b32f067e33cd3f8bf998d82
Rüdiger Timm <rt@openoffice.org>
2008-04-11 10:35:05 +0000

    INTEGRATION: CWS changefileheader (1.35.24); FILE MERGED 2008/04/01 15:01:48 thb 1.35.24.3: #i85898# Stripping all external header guards 2008/04/01 10:48:39 thb 1.35.24.2: #i85898# Stripping all external header guards 2008/03/28 16:07:01 rt 1.35.24.1: #i87441# Change license header to LPGL v3.

--- basic/source/app/dialogs.cxx
+++ basic/source/app/dialogs.cxx
@@ -1,54 +1,40 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: dialogs.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.35 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2008-01-28 13:59:21 $
+ * $RCSfile: dialogs.cxx,v $
+ * $Revision: 1.36 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_basic.hxx"
-
-#ifndef _SOLAR_H
 #include <tools/solar.h>
-#endif
 
 #include <algorithm>
-
-#ifndef _TOOLS_RC_HXX //autogen
 #include <tools/rc.hxx>
-#endif
-
-#ifndef _SV_METRIC_HXX
 #include <vcl/metric.hxx>
-#endif
 #ifndef _DIALOG_HXX //autogen
 #include <vcl/dialog.hxx>
 #endif
@@ -61,45 +47,29 @@
 #ifndef _EDIT_HXX //autogen
 #include <vcl/edit.hxx>
 #endif
-#ifndef _CONFIG_HXX
 #include <tools/config.hxx>
-#endif
 #ifndef _MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
 #endif
-#ifndef _TOOLS_DEBUG_HXX //autogen
 #include <tools/debug.hxx>
-#endif
 #ifndef _SV_FILEDLG_HXX //autogen
 #include <svtools/filedlg.hxx>
 #endif
-#ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
-#endif
-#ifndef _FSYS_HXX //autogen
 #include <tools/fsys.hxx>
-#endif
-#ifndef _SVTOOLS_STRINGTRANSFER_HXX_
 #include <svtools/stringtransfer.hxx>
-#endif
-#ifndef _SV_SPLITWIN_HXX //autogen
 #include <vcl/splitwin.hxx>
-#endif
 #ifndef _ZFORLIST_HXX //autogen
 #include <svtools/zformat.hxx>
 #endif
-#ifndef _CTRLTOOL_HXX
 #include <svtools/ctrltool.hxx>
-#endif
 
 // Ohne Includeschutz
 #include <svtools/svtdata.hxx>
 #include <svtools/solar.hrc>
 
 
-#ifndef _BASIC_DISPDEFS_HXX
 #include <basic/dispdefs.hxx>
-#endif
 #include <basic/testtool.hxx>
 #include "dialogs.hxx"
 #include "resids.hrc"
