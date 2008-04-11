f266790b477456c1e3fec365308dae25a7261b46
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:46:10 +0000

    INTEGRATION: CWS changefileheader (1.38.58); FILE MERGED 2008/04/01 16:05:48 thb 1.38.58.3: #i85898# Stripping all external header guards 2008/04/01 13:01:58 thb 1.38.58.2: #i85898# Stripping all external header guards 2008/03/28 15:45:24 rt 1.38.58.1: #i87441# Change license header to LPGL v3.

--- vcl/unx/source/app/i18n_im.cxx
+++ vcl/unx/source/app/i18n_im.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: i18n_im.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.38 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-02-04 13:53:57 $
+ * $RCSfile: i18n_im.cxx,v $
+ * $Revision: 1.39 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -57,29 +52,17 @@
 
 #include <salunx.h>
 #include <saldisp.hxx>
-
-#ifndef _SAL_I18N_INPUTMETHOD_HXX
 #include "i18n_im.hxx"
-#endif
-
-#ifndef _SAL_I18N_STATUS_HXX
 #include <i18n_status.hxx>
-#endif
 
 #ifdef MACOSX
 #include <osl/process.h>
 #include <tools/string.hxx>
 #endif
-
-#ifndef _OSL_THREAD_H_
 #include <osl/thread.h>
-#endif
 
 using namespace vcl;
-
-#ifndef _SAL_I18N_CALLBACK_HXX
 #include "i18n_cb.hxx"
-#endif
 #if defined(SOLARIS) ||  defined(LINUX) || defined(IRIX)
 extern "C" char * XSetIMValues(XIM im, ...);
 #endif
