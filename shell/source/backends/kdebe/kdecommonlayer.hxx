203faadd0d14a97308b5088075ad2c61c3c76c4f
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:58:41 +0000

    INTEGRATION: CWS changefileheader (1.2.160); FILE MERGED 2008/04/01 15:40:10 thb 1.2.160.2: #i85898# Stripping all external header guards 2008/04/01 12:41:10 thb 1.2.160.1: #i85898# Stripping all external header guards

--- shell/source/backends/kdebe/kdecommonlayer.hxx
+++ shell/source/backends/kdebe/kdecommonlayer.hxx
@@ -1,31 +1,15 @@
 #ifndef KDECOMMONLAYER_HXX_
 #define KDECOMMONLAYER_HXX_
 
-#ifndef KDEBACKEND_HXX_
 #include "kdebackend.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_XCOMPONENTCONTEXT_HPP_
 #include <com/sun/star/uno/XComponentContext.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XLAYER_HPP_
 #include <com/sun/star/configuration/backend/XLayer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_BACKENDACCESSEXCEPTION_HPP_
 #include <com/sun/star/configuration/backend/BackendAccessException.hpp>
-#endif
 #ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XLAYERCONTENTDESCIBER_HPP_
 #include <com/sun/star/configuration/backend/XLayerContentDescriber.hpp>
 #endif
-
-#ifndef _COM_SUN_STAR_UTIL_XTIMESTAMPED_HPP_
 #include <com/sun/star/util/XTimeStamped.hpp>
-#endif
-
-#ifndef _CPPUHELPER_IMPLBASE2_HXX_
 #include <cppuhelper/implbase2.hxx>
-#endif
 
 namespace css = com::sun::star ;
 namespace uno = css::uno ;
