0c8721defb5f35391945bd1244b110cbfe3e0f88
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:10:40 +0000

    INTEGRATION: CWS changefileheader (1.30.8); FILE MERGED 2008/04/01 16:03:26 thb 1.30.8.3: #i85898# Stripping all external header guards 2008/04/01 12:59:38 thb 1.30.8.2: #i85898# Stripping all external header guards 2008/03/31 13:03:35 rt 1.30.8.1: #i87441# Change license header to LPGL v3.

--- unotools/inc/unotools/localedatawrapper.hxx
+++ unotools/inc/unotools/localedatawrapper.hxx
@@ -1,61 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: localedatawrapper.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.30 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 18:41:05 $
+ * $RCSfile: localedatawrapper.hxx,v $
+ * $Revision: 1.31 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _UNOTOOLS_LOCALEDATAWRAPPER_HXX
 #define _UNOTOOLS_LOCALEDATAWRAPPER_HXX
 
-#ifndef _STRING_HXX
 #include <tools/string.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_I18N_XLOCALEDATA2_HPP_
 #include <com/sun/star/i18n/XLocaleData2.hpp>
-#endif
-#ifndef _COM_SUN_STAR_I18N_LOCALEITEM_HPP_
 #include <com/sun/star/i18n/LocaleItem.hpp>
-#endif
-#ifndef _COM_SUN_STAR_I18N_RESERVEDWORDS_HPP_
 #include <com/sun/star/i18n/reservedWords.hpp>
-#endif
-
-#ifndef INCLUDED_UNOTOOLS_READWRITEMUTEXGUARD_HXX
 #include <unotools/readwritemutexguard.hxx>
-#endif
-#ifndef INCLUDED_UNOTOOLSDLLAPI_H
 #include "unotools/unotoolsdllapi.h"
-#endif
 
 #ifndef BOOST_SHARED_PTR_HPP_INCLUDED
 #include <boost/shared_ptr.hpp>
