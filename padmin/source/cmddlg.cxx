f2f3e17471129c9aae1042088e0a619707b8ad4c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:58:54 +0000

    INTEGRATION: CWS changefileheader (1.14.6); FILE MERGED 2008/04/01 15:22:32 thb 1.14.6.3: #i85898# Stripping all external header guards 2008/04/01 12:32:21 thb 1.14.6.2: #i85898# Stripping all external header guards 2008/03/31 13:15:08 rt 1.14.6.1: #i87441# Change license header to LPGL v3.

--- padmin/source/cmddlg.cxx
+++ padmin/source/cmddlg.cxx
@@ -1,62 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: cmddlg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.14 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 16:52:20 $
+ * $RCSfile: cmddlg.cxx,v $
+ * $Revision: 1.15 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #include <stdio.h>
-
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
 #ifndef _PAD_RTSETUP_HRC_
 #include <rtsetup.hrc>
 #endif
-#ifndef _PAD_COMMANDDLG_HXX_
 #include <cmddlg.hxx>
-#endif
-#ifndef _PAD_PADIALOG_HXX_
 #include <padialog.hxx>
-#endif
-#ifndef _PAD_HELPER_HXX_
 #include <helper.hxx>
-#endif
-
-#ifndef _PAD_PRTSETUP_HXX_
 #include <prtsetup.hxx>
-#endif
 
 using namespace psp;
 using namespace rtl;
