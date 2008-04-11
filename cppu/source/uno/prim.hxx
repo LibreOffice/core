8ffdc35dee3cbda62c83dc1b2ced1d8740c739a3
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:42:31 +0000

    INTEGRATION: CWS changefileheader (1.17.84); FILE MERGED 2008/04/01 15:10:35 thb 1.17.84.3: #i85898# Stripping all external header guards 2008/04/01 12:28:03 thb 1.17.84.2: #i85898# Stripping all external header guards 2008/03/31 07:23:22 rt 1.17.84.1: #i87441# Change license header to LPGL v3.

--- cppu/source/uno/prim.hxx
+++ cppu/source/uno/prim.hxx
@@ -1,74 +1,53 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: prim.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.17 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 13:15:15 $
+ * $RCSfile: prim.hxx,v $
+ * $Revision: 1.18 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef PRIM_HXX
 #define PRIM_HXX
 
-#ifndef _TYPELIB_TYPEDESCRIPTION_H_
 #include "typelib/typedescription.h"
-#endif
 #ifndef _typelib_TypeClass_H_
 #include "typelib/typeclass.h"
 #endif
-#ifndef _UNO_SEQUENCE2_H_
 #include "uno/sequence2.h"
-#endif
-#ifndef _UNO_ANY2_H_
 #include "uno/any2.h"
-#endif
-#ifndef _UNO_DATA_H_
 #include "uno/data.h"
-#endif
-#ifndef _UNO_MAPPING_H_
 #include "uno/mapping.h"
-#endif
-#ifndef _UNO_DISPATCHER_H_
 #include "uno/dispatcher.h"
-#endif
 
 #ifndef _OSL_INTERLCK_H
 #include "osl/interlck.h"
 #endif
-#ifndef _OSL_DIAGNOSE_H_
 #include "osl/diagnose.h"
-#endif
 #ifndef _RTL_USTRING_HXX
 #include "rtl/ustring.hxx"
 #endif
-#ifndef _RTL_ALLOC_H_
 #include "rtl/alloc.h"
-#endif
 
 #if OSL_DEBUG_LEVEL > 1
 #include "rtl/ustrbuf.hxx"
