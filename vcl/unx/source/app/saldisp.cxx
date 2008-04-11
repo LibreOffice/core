bc8f30999374e28ac8fe6c2dee071c29d78acaf5
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:48:34 +0000

    INTEGRATION: CWS changefileheader (1.93.58); FILE MERGED 2008/04/01 16:05:49 thb 1.93.58.3: #i85898# Stripping all external header guards 2008/04/01 13:01:58 thb 1.93.58.2: #i85898# Stripping all external header guards 2008/03/28 15:45:25 rt 1.93.58.1: #i87441# Change license header to LPGL v3.

--- vcl/unx/source/app/saldisp.cxx
+++ vcl/unx/source/app/saldisp.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: saldisp.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.94 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 17:07:56 $
+ * $RCSfile: saldisp.cxx,v $
+ * $Revision: 1.95 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -89,59 +84,25 @@ Status XineramaGetInfo(Display*, int, XRectangle*, unsigned char*, int*);
 #include <postx.h>
 
 #include <salunx.h>
-#ifndef _SAL_TYPES_H_
 #include <sal/types.h>
-#endif
-
-#ifndef _SAL_I18N_INPUTMETHOD_HXX
 #include "i18n_im.hxx"
-#endif
-#ifndef _SAL_I18N_XKBDEXTENSION_HXX
 #include "i18n_xkb.hxx"
-#endif
-
-#ifndef _SV_SALDISP_HXX
 #include <saldisp.hxx>
-#endif
-#ifndef _SV_SALDATA_HXX
 #include <saldata.hxx>
-#endif
-#ifndef _SV_SALINST_HXX
 #include <vcl/salinst.hxx>
-#endif
-#ifndef _SV_SALGDI_H
 #include <salgdi.h>
-#endif
-#ifndef _SV_SALFRAME_H
 #include <salframe.h>
-#endif
-#ifndef _SV_KEYCODES_HXX
 #include <vcl/keycodes.hxx>
-#endif
-#ifndef _SV_SALBTYPE_HXX
 #include <vcl/salbtype.hxx>
-#endif
-#ifndef _SV_SALBMP_H
 #include <salbmp.h>
-#endif
-#ifndef _SV_SALOGL_H
 #include <salogl.h>
-#endif
 #ifndef _OSL_THREADMUTEX_H_
 #include <osl/mutex.h>
 #endif
-#ifndef _SV_SALOBJ_H
 #include <salobj.h>
-#endif
-#ifndef _VCL_SM_HXX
 #include <sm.hxx>
-#endif
-#ifndef _VCL_WMADAPTOR_HXX_
 #include <wmadaptor.hxx>
-#endif
-#ifndef _SV_DTINT_HXX
 #include <dtint.hxx>
-#endif
 
 #include <osl/socket.h>
 
