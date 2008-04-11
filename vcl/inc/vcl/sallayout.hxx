3d21d22a5d91e2377b23d6c58d99d65c1914074c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:55:24 +0000

    INTEGRATION: CWS changefileheader (1.4.120); FILE MERGED 2008/04/01 16:05:21 thb 1.4.120.3: #i85898# Stripping all external header guards 2008/04/01 13:01:15 thb 1.4.120.2: #i85898# Stripping all external header guards 2008/03/28 15:44:16 rt 1.4.120.1: #i87441# Change license header to LPGL v3.

--- vcl/inc/vcl/sallayout.hxx
+++ vcl/inc/vcl/sallayout.hxx
@@ -1,44 +1,37 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: sallayout.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 17:03:13 $
+ * $RCSfile: sallayout.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SV_SALLAYOUT_HXX
 #define _SV_SALLAYOUT_HXX
 
-#ifndef _SV_GEN_HXX
 #include <tools/gen.hxx>
-#endif
 
 #include <vector>
 namespace basegfx {
@@ -52,10 +45,7 @@ typedef unsigned short LanguageType;
 
 #include <vector>
 #include <list>
-
-#ifndef _VCL_DLLAPI_H
 #include <vcl/dllapi.h>
-#endif
 
 // for typedef sal_UCS4
 #include <vcl/vclenum.hxx>
