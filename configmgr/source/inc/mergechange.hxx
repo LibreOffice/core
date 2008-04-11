2f25daa456b062d7cc6fa866833954c4d0241067
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:50:52 +0000

    INTEGRATION: CWS changefileheader (1.9.16); FILE MERGED 2008/04/01 12:27:24 thb 1.9.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:44 rt 1.9.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/inc/mergechange.hxx
+++ configmgr/source/inc/mergechange.hxx
@@ -1,51 +1,39 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: mergechange.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:20:18 $
+ * $RCSfile: mergechange.hxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef INCLUDED_CONFIGMGR_MERGECHANGE_HXX
 #define INCLUDED_CONFIGMGR_MERGECHANGE_HXX
 
-#ifndef INCLUDED_CONFIGMGR_NAMECREATOR_HXX
 #include "namecreator.hxx"
-#endif
-
-#ifndef CONFIGMGR_CHANGE_HXX
 #include "change.hxx"
-#endif
-#ifndef CONFIGMGR_TREECHANGELIST_HXX
 #include "treechangelist.hxx"
-#endif
 
 namespace configmgr
 {
