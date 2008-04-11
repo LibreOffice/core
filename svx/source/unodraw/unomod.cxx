34eb4b0e1a71237cc09e4e20aed0dd468e6cc469
Rüdiger Timm <rt@openoffice.org>
2008-04-11 02:57:43 +0000

    INTEGRATION: CWS changefileheader (1.22.36); FILE MERGED 2008/04/01 15:51:53 thb 1.22.36.3: #i85898# Stripping all external header guards 2008/04/01 12:50:16 thb 1.22.36.2: #i85898# Stripping all external header guards 2008/03/31 14:24:14 rt 1.22.36.1: #i87441# Change license header to LPGL v3.

--- svx/source/unodraw/unomod.cxx
+++ svx/source/unodraw/unomod.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: unomod.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.22 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 10:10:49 $
+ * $RCSfile: unomod.cxx,v $
+ * $Revision: 1.23 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,73 +32,25 @@
 #include "precompiled_svx.hxx"
 
 #define _SVX_USE_UNOGLOBALS_
-
-#ifndef _COM_SUN_STAR_LANG_SERVICENOTREGISTEREDEXCEPTION_HPP_
 #include <com/sun/star/lang/ServiceNotRegisteredException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_NOSUPPORTEXCEPTION_HPP_
 #include <com/sun/star/lang/NoSupportException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_DRAWING_XSHAPE_HPP_
 #include <com/sun/star/drawing/XShape.hpp>
-#endif
-
-#ifndef _VOS_MUTEX_HXX_
 #include <vos/mutex.hxx>
-#endif
-
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
-
-#ifndef _LIST_HXX
 #include <tools/list.hxx>
-#endif
-
-#ifndef _SFX_ITEMPROP_HXX
 #include <svtools/itemprop.hxx>
-#endif
-
-#ifndef _SVTOOLS_UNOEVENT_HXX_
 #include <svtools/unoevent.hxx>
-#endif
-
-#ifndef _COMPHELPER_SEQUENCE_HXX_
 #include <comphelper/sequence.hxx>
-#endif
 
 #include <cppuhelper/implbase2.hxx>
-
-#ifndef _SVX_UNOFILL_HXX_
 #include <unofill.hxx>
-#endif
-
-#ifndef _SVX_UNONRULE_HXX
 #include <unonrule.hxx>
-#endif
-
-#ifndef _SVTOOLS_UNOIMAP_HXX
 #include <svtools/unoimap.hxx>
-#endif
-
-#ifndef _SVX_FMDPAGE_HXX
 #include <svx/fmdpage.hxx>
-#endif
-#ifndef _SVX_FMMODEL_HXX
 #include <svx/fmmodel.hxx>
-#endif
-
-#ifndef _SVX_FMPAGE_HXX
 #include <svx/fmpage.hxx>
-#endif
-
-#ifndef _SFX_HRC
 #include <sfx2/sfx.hrc>
-#endif
-
-#ifndef _SVX_UNOAPI_HXX_
 #include <unoapi.hxx>
-#endif
 
 #include <svx/svdmodel.hxx>
 #include "globl3d.hxx"
