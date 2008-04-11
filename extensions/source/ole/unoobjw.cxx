f9bda5ebc05f313acb0ad521a31fc74371d3a84d
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:18:39 +0000

    INTEGRATION: CWS changefileheader (1.23.48); FILE MERGED 2008/04/02 06:19:35 thb 1.23.48.4: #i85898# Corrected misspelled include statement 2008/04/01 15:15:09 thb 1.23.48.3: #i85898# Stripping all external header guards 2008/04/01 12:29:46 thb 1.23.48.2: #i85898# Stripping all external header guards 2008/03/31 12:31:34 rt 1.23.48.1: #i87441# Change license header to LPGL v3.

--- extensions/source/ole/unoobjw.cxx
+++ extensions/source/ole/unoobjw.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: unoobjw.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.23 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:48:30 $
+ * $RCSfile: unoobjw.cxx,v $
+ * $Revision: 1.24 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -50,48 +45,21 @@
 #include <vos/diagnose.hxx>
 #include <vos/refernce.hxx>
 #include <tools/debug.hxx>
-
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_METHODCONCEPT_HPP_
 #include <com/sun/star/beans/MethodConcept.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYCONCEPT_HPP_
 #include <com/sun/star/beans/PropertyConcept.hpp>
-#endif
-#ifndef _COM_SUN_STAR_SCRIPT_FAILREASON_HPP_
 #include <com/sun/star/script/FailReason.hpp>
-#endif
-#ifndef _COM_SUN_STAR_REFLECTION_PARAMINFO_HPP_
 #include <com/sun/star/reflection/ParamInfo.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_XEXACTNAME_HPP_
 #include <com/sun/star/beans/XExactName.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_NOSUCHELEMENTEXCEPTION_HPP_
 #include <com/sun/star/container/NoSuchElementException.hpp>
-#endif
 
 #include <com/sun/star/beans/XMaterialHolder.hpp>
 #include <com/sun/star/script/XInvocation2.hpp>
 #include <com/sun/star/script/MemberType.hpp>
 #include <com/sun/star/reflection/XIdlReflection.hpp>
-
-#ifndef _TYPELIB_TYPEDESCRIPTION_H_
-#include <uno/typedescription.h>
-#endif
-#ifndef _OSL_INTERLOCK_H_
 #include <osl/interlck.h>
-#endif
-#ifndef _COM_SUN_STAR_UNO_GENFUNC_H_
 #include <com/sun/star/uno/genfunc.h>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE1_HXX_
 #include <cppuhelper/implbase1.hxx>
-#endif
 
 #include "comifaces.hxx"
 #include "jscriptclasses.hxx"
