0d41821cfbefc47d9cf062399f35578c27f96925
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:13:08 +0000

    INTEGRATION: CWS changefileheader (1.10.16); FILE MERGED 2008/04/01 15:40:22 thb 1.10.16.3: #i85898# Stripping all external header guards 2008/04/01 12:41:13 thb 1.10.16.2: #i85898# Stripping all external header guards 2008/03/31 13:17:11 rt 1.10.16.1: #i87441# Change license header to LPGL v3.

--- shell/source/unix/sysshell/recently_used_file_handler.cxx
+++ shell/source/unix/sysshell/recently_used_file_handler.cxx
@@ -1,66 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: recently_used_file_handler.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.10 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-02-04 13:58:48 $
+ * $RCSfile: recently_used_file_handler.cxx,v $
+ * $Revision: 1.11 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_shell.hxx"
 
-#ifndef _SYSTEMSHELL_HXX_
 #include "systemshell.hxx"
-#endif
-
-#ifndef _OSL_PROCESS_H_
 #include "osl/process.h"
-#endif
-
-#ifndef _RTL_USTRING_HXX_
 #include "rtl/ustring.hxx"
-#endif
-
-#ifndef _RTL_STRING_HXX_
 #include "rtl/string.hxx"
-#endif
-
-#ifndef _RTL_STRBUF_HXX_
 #include "rtl/strbuf.hxx"
-#endif
 
 #include "osl/thread.h"
-
-#ifndef INCLUDED_RECENTLY_USED_FILE_HXX
 #include "recently_used_file.hxx"
-#endif
 
 #include "internal/xml_parser.hxx"
 #include "internal/i_xml_parser_event_handler.hxx"
