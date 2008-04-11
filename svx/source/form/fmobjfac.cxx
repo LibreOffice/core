b5d86420a5a72a4601b76e7334c1ec723341cefc
Rüdiger Timm <rt@openoffice.org>
2008-04-11 00:02:11 +0000

    INTEGRATION: CWS changefileheader (1.17.368); FILE MERGED 2008/04/01 15:51:01 thb 1.17.368.3: #i85898# Stripping all external header guards 2008/04/01 12:48:51 thb 1.17.368.2: #i85898# Stripping all external header guards 2008/03/31 14:21:54 rt 1.17.368.1: #i87441# Change license header to LPGL v3.

--- svx/source/form/fmobjfac.cxx
+++ svx/source/form/fmobjfac.cxx
@@ -1,56 +1,39 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: fmobjfac.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.17 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 18:13:34 $
+ * $RCSfile: fmobjfac.cxx,v $
+ * $Revision: 1.18 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifndef _COMPHELPER_STLTYPES_HXX_
 #include <comphelper/stl_types.hxx>
-#endif
-
-#ifndef _SVDOBJ_HXX
 #include <svx/svdobj.hxx>
-#endif
-
-#ifndef _SVX_FMTOOLS_HXX
 #include "fmtools.hxx"
-#endif
-
-#ifndef _SVX_FMSERVS_HXX
 #include "fmservs.hxx"
-#endif
 
 #ifndef _FM_FMOBJFAC_HXX
 #include "fmobjfac.hxx"
@@ -63,10 +46,7 @@
 #ifndef _FM_FMOBJ_HXX
 #include "fmobj.hxx"
 #endif
-
-#ifndef _SVX_FMSHIMP_HXX
 #include "fmshimp.hxx"
-#endif
 
 #ifndef _FM_FMSHELL_HXX
 #include <svx/fmshell.hxx>
@@ -75,53 +55,24 @@
 #ifndef _SVX_SVXIDS_HRC
 #include <svx/svxids.hrc>
 #endif
-
-#ifndef _SVX_TBXFORM_HXX
 #include "tbxform.hxx"
-#endif
-
-#ifndef _TOOLS_RESID_HXX //autogen
 #include <tools/resid.hxx>
-#endif
 
 #ifndef _SVX_FMRESIDS_HRC
 #include "fmresids.hrc"
 #endif
-
-#ifndef _SHL_HXX
 #include <tools/shl.hxx>
-#endif
-
-#ifndef _SVX_DIALMGR_HXX
 #include <svx/dialmgr.hxx>
-#endif
-
-#ifndef _SVX_FMSERVS_HXX
 #include "fmservs.hxx"
-#endif
-
-#ifndef _SVX_TABWIN_HXX
 #include "tabwin.hxx"
-#endif
-
-#ifndef _SVX_FMEXPL_HXX
 #include "fmexpl.hxx"
-#endif
-
-#ifndef _SVX_FILTNAV_HXX
 #include "filtnav.hxx"
-#endif
 
 #ifndef _SVX_FMPROP_HRC
 #include "fmprop.hrc"
 #endif
-#ifndef SVX_FMPROPBRW_HXX
 #include "fmPropBrw.hxx"
-#endif
-
-#ifndef _SVX_DATANAVI_HXX
 #include "datanavi.hxx"
-#endif
 
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
