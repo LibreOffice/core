97626374867b0bedf39f01b4ca9f46764a465071
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:41:13 +0000

    INTEGRATION: CWS changefileheader (1.22.16); FILE MERGED 2008/04/01 15:06:55 thb 1.22.16.3: #i85898# Stripping all external header guards 2008/04/01 12:27:35 thb 1.22.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:54 rt 1.22.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/treecache/invalidatetree.cxx
+++ configmgr/source/treecache/invalidatetree.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: invalidatetree.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.22 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:38:34 $
+ * $RCSfile: invalidatetree.cxx,v $
+ * $Revision: 1.23 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,39 +33,15 @@
 
 
 #include "cachecontroller.hxx"
-
-#ifndef CONFIGMGR_CHANGE_HXX
 #include "change.hxx"
-#endif
-#ifndef _CONFIGMGR_TREE_VALUENODE_HXX
 #include "valuenode.hxx"
-#endif
-
-#ifndef CONFIGMGR_UPDATEHELPER_HXX
 #include "updatehelper.hxx"
-#endif
-#ifndef _CONFIGMGR_TREEACTIONS_HXX_
 #include "treeactions.hxx"
-#endif
-
-#ifndef CONFIGMGR_TREEACCESSOR_HXX
 #include "treeaccessor.hxx"
-#endif
-
-#ifndef _CONFIGMGR_TRACER_HXX_
 #include "tracer.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
-#endif
-#ifndef _COM_SUN_STAR_CONTAINER_NOSUCHELEMENTEXCEPTION_HPP_
 #include <com/sun/star/container/NoSuchElementException.hpp>
-#endif
-
-#ifndef _VOS_THREAD_HXX_
 #include <vos/thread.hxx>
-#endif
 
 #ifndef INCLUDED_ALGORITHM
 #include <algorithm>
