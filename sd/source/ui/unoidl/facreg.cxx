bee0c8b86059dfa7345635347cddaca24fb5e0f5
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:59:07 +0000

    INTEGRATION: CWS changefileheader (1.13.22); FILE MERGED 2008/04/01 15:36:12 thb 1.13.22.3: #i85898# Stripping all external header guards 2008/04/01 12:39:25 thb 1.13.22.2: #i85898# Stripping all external header guards 2008/03/31 13:59:08 rt 1.13.22.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/unoidl/facreg.cxx
+++ sd/source/ui/unoidl/facreg.cxx
@@ -1,64 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: facreg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.14 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 14:56:39 $
+ * $RCSfile: facreg.cxx,v $
+ * $Revision: 1.15 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sd.hxx"
-
-#ifndef _COM_SUN_STAR_REGISTRY_XREGISTRYKEY_HPP_
 #include <com/sun/star/registry/XRegistryKey.hpp>
-#endif
 
 #include "sddll.hxx"
 
-#ifndef _CPPUHELPER_FACTORY_HXX_
 #include <cppuhelper/factory.hxx>
-#endif
-#ifndef _UNO_LBNAMES_H_
 #include <uno/lbnames.h>
-#endif
-
-#ifndef SFX2_SFXMODELFACTORY_HXX
 #include <sfx2/sfxmodelfactory.hxx>
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include "osl/diagnose.h"
-#endif
-#ifndef _SAL_TYPES_H_
 #include "sal/types.h"
-#endif
 
 #ifndef INCLUDED_STRING_H
 #include <string.h>
