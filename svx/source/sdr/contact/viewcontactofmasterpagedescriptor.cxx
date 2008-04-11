cf84ce19fde5781e707e9cd6c24420b00ea405f5
Rüdiger Timm <rt@openoffice.org>
2008-04-11 01:26:03 +0000

    INTEGRATION: CWS changefileheader (1.13.118); FILE MERGED 2008/04/01 15:51:26 thb 1.13.118.3: #i85898# Stripping all external header guards 2008/04/01 12:49:30 thb 1.13.118.2: #i85898# Stripping all external header guards 2008/03/31 14:23:11 rt 1.13.118.1: #i87441# Change license header to LPGL v3.

--- svx/source/sdr/contact/viewcontactofmasterpagedescriptor.cxx
+++ svx/source/sdr/contact/viewcontactofmasterpagedescriptor.cxx
@@ -1,92 +1,48 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: viewcontactofmasterpagedescriptor.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-01-29 14:04:52 $
+ * $RCSfile: viewcontactofmasterpagedescriptor.cxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifndef _SDR_CONTACT_VIEWCONTACTOFMASTERPAGEDESCRIPTOR_HXX
 #include <svx/sdr/contact/viewcontactofmasterpagedescriptor.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VIEWOBJECTCONTACT_HXX
 #include <svx/sdr/contact/viewobjectcontact.hxx>
-#endif
-
-#ifndef _SVDPAGE_HXX
 #include <svx/svdpage.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_DISPLAYINFO_HXX
 #include <svx/sdr/contact/displayinfo.hxx>
-#endif
-
-#ifndef _SVDOBJ_HXX
 #include <svx/svdobj.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_OBJECTCONTACTOFOBJLISTPAINTER_HXX
 #include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VIEWCONTACTPAINTHELPER_HXX
 #include <svx/sdr/contact/viewcontactpainthelper.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VOCBITMAPBUFFER_HXX
 #include <svx/sdr/contact/vocbitmapbuffer.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VOCOFMASTERPAGEDESCRIPTOR_HXX
 #include <svx/sdr/contact/vocofmasterpagedescriptor.hxx>
-#endif
-
-#ifndef _SV_TIMER_HXX
 #include <vcl/timer.hxx>
-#endif
-
-#ifndef _SVDPAGV_HXX
 #include <svx/svdpagv.hxx>
-#endif
-
-#ifndef _SVDVIEW_HXX
 #include <svx/svdview.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VIEWCONTACTOFSDRPAGE_HXX
 #include <svx/sdr/contact/viewcontactofsdrpage.hxx>
-#endif
 
 //////////////////////////////////////////////////////////////////////////////
 
