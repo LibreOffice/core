f7f3a6886e0c9655d8f39838cfb1187b9e41cdb4
Rüdiger Timm <rt@openoffice.org>
2008-04-11 01:54:23 +0000

    INTEGRATION: CWS changefileheader (1.27.320); FILE MERGED 2008/04/01 12:49:50 thb 1.27.320.2: #i85898# Stripping all external header guards 2008/03/31 14:23:28 rt 1.27.320.1: #i87441# Change license header to LPGL v3.

--- svx/source/svdraw/svdcrtv.cxx
+++ svx/source/svdraw/svdcrtv.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdcrtv.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.27 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-24 12:02:15 $
+ * $RCSfile: svdcrtv.cxx,v $
+ * $Revision: 1.28 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,62 +40,30 @@
 #include <svx/svdpagv.hxx>
 #include <svx/svdpage.hxx>
 #include <svx/svdetc.hxx>
-
-#ifndef _E3D_SCENE3D_HXX
 #include <svx/scene3d.hxx>
-#endif
-
-#ifndef _E3D_VIEW3D_HXX
 #include <svx/view3d.hxx>
-#endif
 
 // #116425#
-#ifndef _SDR_CONTACT_OBJECTCONTACTOFOBJLISTPAINTER_HXX
 #include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
-#endif
 
 // #116425#
-#ifndef _SDR_CONTACT_DISPLAYINFO_HXX
 #include <svx/sdr/contact/displayinfo.hxx>
-#endif
-
-#ifndef _SVDOUNO_HXX
 #include <svx/svdouno.hxx>
-#endif
 
 #define XOR_CREATE_PEN          PEN_SOLID
-
-#ifndef _SVDOPATH_HXX
 #include <svx/svdopath.hxx>
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#ifndef _SDR_OVERLAY_OVERLAYPOLYPOLYGON_HXX
 #include <svx/sdr/overlay/overlaypolypolygon.hxx>
-#endif
-
-#ifndef _SDR_OVERLAY_OVERLAYMANAGER_HXX
 #include <svx/sdr/overlay/overlaymanager.hxx>
-#endif
-
-#ifndef _SDR_OVERLAY_OVERLAYSDROBJECT_HXX
 #include <svx/sdr/overlay/overlaysdrobject.hxx>
-#endif
-
-#ifndef _SDRPAINTWINDOW_HXX
 #include <sdrpaintwindow.hxx>
-#endif
 
 // #i72535#
-#ifndef _SVX_FMOBJ_HXX
 #include "fmobj.hxx"
-#endif
 
 // #i68562#
-#ifndef _SVDOCIRC_HXX
 #include <svx/svdocirc.hxx>
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
