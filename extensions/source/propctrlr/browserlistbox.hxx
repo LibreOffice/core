072a18b26f5fa32949711d298a529fa28689ba88
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:39:03 +0000

    INTEGRATION: CWS changefileheader (1.12.48); FILE MERGED 2008/04/01 15:15:11 thb 1.12.48.3: #i85898# Stripping all external header guards 2008/04/01 12:29:47 thb 1.12.48.2: #i85898# Stripping all external header guards 2008/03/31 12:31:39 rt 1.12.48.1: #i87441# Change license header to LPGL v3.

--- extensions/source/propctrlr/browserlistbox.hxx
+++ extensions/source/propctrlr/browserlistbox.hxx
@@ -1,75 +1,53 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: browserlistbox.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:56:11 $
+ * $RCSfile: browserlistbox.hxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _EXTENSIONS_PROPCTRLR_BROWSERLISTBOX_HXX_
 #define _EXTENSIONS_PROPCTRLR_BROWSERLISTBOX_HXX_
 
-#ifndef _EXTENSIONS_PROPCTRLR_BROWSERLINE_HXX_
 #include "browserline.hxx"
-#endif
 #ifndef _EXTENSIONS_PROPCTRLR_MODULEPRC_HXX_
 #include "modulepcr.hxx"
 #endif
-#ifndef _EXTENSIONS_PROPCTRLR_PCRCOMMON_HXX_
 #include "pcrcommon.hxx"
-#endif
 
 /** === begin UNO includes === **/
-#ifndef _COM_SUN_STAR_INSPECTION_XPROPERTYCONTROL_HPP_
 #include <com/sun/star/inspection/XPropertyControl.hpp>
-#endif
-#ifndef _COM_SUN_STAR_INSPECTION_XPROPERTYHANDLER_HPP_
 #include <com/sun/star/inspection/XPropertyHandler.hpp>
-#endif
 /** === end UNO includes === **/
-
-#ifndef _SV_SCRBAR_HXX
 #include <vcl/scrbar.hxx>
-#endif
-#ifndef _SV_LSTBOX_HXX
 #include <vcl/lstbox.hxx>
-#endif
 #ifndef _SV_BUTTON_HXX
 #include <vcl/button.hxx>
 #endif
-#ifndef _LINK_HXX
 #include <tools/link.hxx>
-#endif
-#ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
-#endif
 
 #include <set>
 #include <vector>
