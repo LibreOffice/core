ce7fe83400c578dfbb58054d394ae33e8ad9ddb7
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:12:55 +0000

    INTEGRATION: CWS changefileheader (1.29.12); FILE MERGED 2008/04/01 15:22:45 thb 1.29.12.3: #i85898# Stripping all external header guards 2008/04/01 11:00:35 thb 1.29.12.2: #i85898# Stripping all external header guards 2008/03/28 15:38:24 rt 1.29.12.1: #i87441# Change license header to LPGL v3.

--- psprint/source/fontmanager/fontconfig.cxx
+++ psprint/source/fontmanager/fontconfig.cxx
@@ -1,44 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: fontconfig.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.29 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2008-02-12 13:09:57 $
+ * $RCSfile: fontconfig.cxx,v $
+ * $Revision: 1.30 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_psprint.hxx"
-
-#ifndef _PSPRINT_FONTMANAGER_HXX_
 #include <psprint/fontmanager.hxx>
-#endif
 #ifndef _PSPRINT_FONTCACHE_HXX_
 #include <psprint/fontcache.hxx>
 #endif
@@ -76,9 +68,7 @@ typedef int FcSetName;
 
 #include <cstdio>
 #include <cstdarg>
-#ifndef _UTL_ATOM_HXX_
 #include <unotools/atom.hxx>
-#endif
 #ifndef _OSL_MODULE_H
 #include <osl/module.h>
 #endif
@@ -88,12 +78,8 @@ typedef int FcSetName;
 #ifndef _RTL_USTRBUF_HXX
 #include <rtl/ustrbuf.hxx>
 #endif
-#ifndef _OSL_PROCESS_H_
 #include <osl/process.h>
-#endif
-#ifndef _RTL_LOCALE_HXX_
 #include <rtl/locale.hxx>
-#endif
 #include <sal/alloca.h>
 
 #include <utility>
