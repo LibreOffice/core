43725d83da7cfbc6746ee9952cf9c0ae4e482c0f
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:36:22 +0000

    INTEGRATION: CWS changefileheader (1.9.50); FILE MERGED 2008/04/01 14:58:45 thb 1.9.50.3: #i85898# Stripping all external header guards 2008/04/01 10:45:10 thb 1.9.50.2: #i85898# Stripping all external header guards 2008/03/28 15:57:39 rt 1.9.50.1: #i87441# Change license header to LPGL v3.

--- accessibility/bridge/source/java/WindowsAccessBridgeAdapter.cxx
+++ accessibility/bridge/source/java/WindowsAccessBridgeAdapter.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: WindowsAccessBridgeAdapter.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-03-26 13:47:45 $
+ * $RCSfile: WindowsAccessBridgeAdapter.cxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,46 +40,19 @@
 #include <tools/prewin.h>
 #include <wtypes.h>
 #include <tools/postwin.h>
-
-#ifndef _RTL_PROCESS_H_
 #include <rtl/process.h>
-#endif
-
-#ifndef _LINK_HXX
 #include <tools/link.hxx>
-#endif
 
 #ifndef _SVAPP_HXX
 #include <vcl/svapp.hxx>
 #endif
-
-#ifndef _SV_WINDOW_HXX
 #include <vcl/window.hxx>
-#endif
-
-#ifndef _SV_SYSDATA_HXX
 #include <vcl/sysdata.hxx>
-#endif
-
-#ifndef _UNO_CURRENT_CONTEXT_HXX_
 #include <uno/current_context.hxx>
-#endif
-
-#ifndef _UNO_ENVIRONMENT_H_
 #include <uno/environment.h>
-#endif
-
-#ifndef _UNO_MAPPING_HXX_
 #include <uno/mapping.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_ACCESSIBLEROLE_HPP_
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLE_HPP_
 #include <com/sun/star/accessibility/XAccessible.hpp>
-#endif
 
 #ifndef _JVMACCESS_UNOVIRTUALMACHINE_HXX_
 #include "jvmaccess/unovirtualmachine.hxx"
