3ccdb76031dca5e1498e438d8d091c17555251b4
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:24:10 +0000

    INTEGRATION: CWS changefileheader (1.46.6); FILE MERGED 2008/04/01 15:22:46 thb 1.46.6.3: #i85898# Stripping all external header guards 2008/04/01 11:00:36 thb 1.46.6.2: #i85898# Stripping all external header guards 2008/03/28 15:38:32 rt 1.46.6.1: #i87441# Change license header to LPGL v3.

--- psprint/source/printergfx/printerjob.cxx
+++ psprint/source/printergfx/printerjob.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: printerjob.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.46 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 16:48:42 $
+ * $RCSfile: printerjob.cxx,v $
+ * $Revision: 1.47 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,36 +34,16 @@
 #ifndef __SGI_STL_VECTOR
 #include <vector>
 #endif
-#ifndef _PSPRINT_PRINTERJOB_HXX_
 #include <psprint/printerjob.hxx>
-#endif
-#ifndef _PSPRINT_PPDPARSER_HXX_
 #include <psprint/ppdparser.hxx>
-#endif
-#ifndef _PSPRINT_STRHELPER_HXX_
 #include <psprint/strhelper.hxx>
-#endif
-#ifndef _PSPRINT_PRINTERINFOMANAGER_HXX_
 #include <psprint/printerinfomanager.hxx>
-#endif
-#ifndef _PSPRINT_PRINTERGFX_HXX_
 #include <psprint/printergfx.hxx>
-#endif
-#ifndef _PSPRINT_PRINTERUTIL_HXX_
 #include <psputil.hxx>
-#endif
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
-#ifndef _RTL_STRBUF_HXX_
 #include <rtl/strbuf.hxx>
-#endif
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-#ifndef _OSL_THREAD_H_
 #include <osl/thread.h>
-#endif
 
 #include <stdio.h>
 #include <sys/types.h>
