061acf214956e6114c93ea21c38ca5bbdd59c1b2
Rüdiger Timm <rt@openoffice.org>
2008-04-11 10:29:01 +0000

    INTEGRATION: CWS changefileheader (1.77.26); FILE MERGED 2008/04/01 15:01:47 thb 1.77.26.3: #i85898# Stripping all external header guards 2008/04/01 10:48:39 thb 1.77.26.2: #i85898# Stripping all external header guards 2008/03/28 16:06:59 rt 1.77.26.1: #i87441# Change license header to LPGL v3.

--- basic/source/app/app.cxx
+++ basic/source/app/app.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: app.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.77 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 15:49:40 $
+ * $RCSfile: app.cxx,v $
+ * $Revision: 1.78 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,25 +34,18 @@
 #ifndef _MSGBOX_HXX //autogen
 #include <vcl/msgbox.hxx>
 #endif
-#ifndef _FSYS_HXX //autogen
 #include <tools/fsys.hxx>
-#endif
 #ifndef _SV_FILEDLG_HXX //autogen
 #include <svtools/filedlg.hxx>
 #endif
-#ifndef _CONFIG_HXX
 #include <tools/config.hxx>
-#endif
 
 #include <vcl/font.hxx>
 
 #ifndef _BASIC_TTRESHLP_HXX
 #include <basic/ttstrhlp.hxx>
 #endif
-
-#ifndef _SBXCLASS_HXX //autogen
 #include <basic/sbx.hxx>
-#endif
 #include <svtools/filedlg.hxx>
 
 #include <osl/module.h>
@@ -78,24 +66,14 @@
 #ifndef _RUNTIME_HXX
 #include "runtime.hxx"
 #endif
-
-#ifndef _SB_INTERN_HXX
 #include "sbintern.hxx"
-#endif
 
 #ifdef _USE_UNO
 #include <ucbhelper/contentbroker.hxx>
 #include <ucbhelper/configurationkeys.hxx>
-#ifndef _COMPHELPER_REGPATHHELPER_HXX_
 #include <comphelper/regpathhelper.hxx>
-#endif
-#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
 #include <comphelper/processfactory.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
 #include <com/sun/star/beans/PropertyValue.hpp>
-#endif
 #ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
 #include <com/sun/star/beans/XComponent.hpp>
 #endif
@@ -104,10 +82,7 @@
 #include <com/sun/star/ucb/XContentProviderManager.hpp>
 
 #include <ucbhelper/content.hxx>
-
-#ifndef INCLUDED_SVTOOLS_SYSLOCALE_HXX
 #include <svtools/syslocale.hxx>
-#endif
 
 using namespace comphelper;
 using namespace cppu;
