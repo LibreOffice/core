83ba14d70b2e6f0fb2b234134bfd291a80d0a271
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:19:32 +0000

    INTEGRATION: CWS changefileheader (1.4.8); FILE MERGED 2008/04/01 12:33:01 thb 1.4.8.2: #i85898# Stripping all external header guards 2008/03/31 13:31:47 rt 1.4.8.1: #i87441# Change license header to LPGL v3.

--- reportdesign/inc/ReportDefinition.hxx
+++ reportdesign/inc/ReportDefinition.hxx
@@ -1,72 +1,50 @@
-#ifndef REPORTDESIGN_API_REPORTDEFINITION_HXX
-#define REPORTDESIGN_API_REPORTDEFINITION_HXX
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: ReportDefinition.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-08 12:46:07 $
+ * $RCSfile: ReportDefinition.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
-#ifndef _COM_SUN_STAR_REPORT_XREPORTDEFINITION_HPP_
+#ifndef REPORTDESIGN_API_REPORTDEFINITION_HXX
+#define REPORTDESIGN_API_REPORTDEFINITION_HXX
+
 #include <com/sun/star/report/XReportDefinition.hpp>
-#endif
-#ifndef _CPPUHELPER_COMPBASE10_HXX_
 #include <cppuhelper/compbase10.hxx>
-#endif
-#ifndef _CPPUHELPER_BASEMUTEX_HXX_
 #include <cppuhelper/basemutex.hxx>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XUNOTUNNEL_HPP_
 #include <com/sun/star/lang/XUnoTunnel.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
 #include <com/sun/star/lang/XTypeProvider.hpp>
-#endif
 #include <com/sun/star/frame/XTitle.hpp>
 #include <com/sun/star/frame/XTitleChangeBroadcaster.hpp>
 #include <com/sun/star/frame/XUntitledNumbers.hpp>
-#ifndef INCLUDED_CPPUHELPER_PROPERTYSETMIXIN_HXX
 #include <cppuhelper/propertysetmixin.hxx>
-#endif
 #include <comphelper/uno3.hxx>
 #include <comphelper/embeddedobjectcontainer.hxx>
-#ifndef SVX_UNOMOD_HXX
 #include <svx/unomod.hxx>
-#endif
-#ifndef _COM_SUN_STAR_UTIL_XNUMBERFORMATSSUPPLIER_HPP_
 #include <com/sun/star/util/XNumberFormatsSupplier.hpp>
-#endif
 #include <com/sun/star/frame/XModule.hpp>
 #include "ReportHelperDefines.hxx"
 #include <boost/shared_ptr.hpp>
