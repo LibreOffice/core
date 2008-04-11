bcec1baf5d6f1857bfc084633d31b8dde841a59f
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:16:55 +0000

    INTEGRATION: CWS changefileheader (1.4.100); FILE MERGED 2008/04/01 12:33:27 thb 1.4.100.2: #i85898# Stripping all external header guards 2008/03/31 13:15:57 rt 1.4.100.1: #i87441# Change license header to LPGL v3.

--- rsc/inc/rscmgr.hxx
+++ rsc/inc/rscmgr.hxx
@@ -1,55 +1,40 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: rscmgr.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 13:32:53 $
+ * $RCSfile: rscmgr.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _RSCMGR_HXX
 #define _RSCMGR_HXX
 
-#ifndef _RSCALL_H
 #include <rscall.h>
-#endif
-#ifndef _RSCERROR_H
 #include <rscerror.h>
-#endif
-#ifndef _RSCHASH_HXX
 #include <rschash.hxx>
-#endif
-#ifndef _RSCTOP_HXX
 #include <rsctop.hxx>
-#endif
-#ifndef _RSCCLASS_HXX
 #include <rscclass.hxx>
-#endif
 
 /******************* R s c M g r *****************************************/
 class RscMgr : public RscClass {
