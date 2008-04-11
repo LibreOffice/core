4cd3c94eaf95a1b0ea98228b545aa6e59015b21c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 10:22:42 +0000

    INTEGRATION: CWS changefileheader (1.9.132); FILE MERGED 2008/04/01 15:15:21 thb 1.9.132.3: #i85898# Stripping all external header guards 2008/04/01 12:29:54 thb 1.9.132.2: #i85898# Stripping all external header guards 2008/03/31 12:31:55 rt 1.9.132.1: #i87441# Change license header to LPGL v3.

--- extensions/source/propctrlr/standardcontrol.hxx
+++ extensions/source/propctrlr/standardcontrol.hxx
@@ -1,84 +1,52 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: standardcontrol.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 08:54:41 $
+ * $RCSfile: standardcontrol.hxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _EXTENSIONS_PROPCTRLR_STANDARDCONTROL_HXX_
 #define _EXTENSIONS_PROPCTRLR_STANDARDCONTROL_HXX_
 
-#ifndef _EXTENSIONS_PROPCTRLR_COMMONCONTROL_HXX_
 #include "commoncontrol.hxx"
-#endif
-#ifndef _EXTENSIONS_PROPCTRLR_PCRCOMMON_HXX_
 #include "pcrcommon.hxx"
-#endif
 
 /** === begin UNO includes === **/
-#ifndef _COM_SUN_STAR_INSPECTION_XNUMERICCONTROL_HPP_
 #include <com/sun/star/inspection/XNumericControl.hpp>
-#endif
-#ifndef _COM_SUN_STAR_INSPECTION_XSTRINGLISTCONTROL_HPP_
 #include <com/sun/star/inspection/XStringListControl.hpp>
-#endif
-#ifndef _COM_SUN_STAR_INSPECTION_XHYPERLINKCONTROL_HPP_
 #include <com/sun/star/inspection/XHyperlinkControl.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
-#endif
 /** === end UNO includes === **/
-
-#ifndef _SV_FIELD_HXX
 #include <vcl/field.hxx>
-#endif
-#ifndef _LONGCURR_HXX
 #include <vcl/longcurr.hxx>
-#endif
-#ifndef _CTRLBOX_HXX
 #include <svtools/ctrlbox.hxx>
-#endif
-#ifndef _SV_LSTBOX_HXX
 #include <vcl/lstbox.hxx>
-#endif
-#ifndef _SV_COMBOBOX_HXX
 #include <vcl/combobox.hxx>
-#endif
-#ifndef _CALENDAR_HXX
 #include <svtools/calendar.hxx>
-#endif
-#ifndef _FMTFIELD_HXX_
 #include <svtools/fmtfield.hxx>
-#endif
 
 #include <set>
 
