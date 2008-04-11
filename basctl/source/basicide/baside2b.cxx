01ad0e91921aa7008aa5d18bf66c2f734a8001f6
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:35:26 +0000

    INTEGRATION: CWS changefileheader (1.58.60); FILE MERGED 2008/04/01 15:00:34 thb 1.58.60.2: #i85898# Stripping all external header guards 2008/03/28 16:04:40 rt 1.58.60.1: #i87441# Change license header to LPGL v3.

--- basctl/source/basicide/baside2b.cxx
+++ basctl/source/basicide/baside2b.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: baside2b.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.58 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 15:43:02 $
+ * $RCSfile: baside2b.cxx,v $
+ * $Revision: 1.59 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,19 +38,10 @@
 #include <ide_pch.hxx>
 
 
-#ifndef _URLOBJ_HXX
 #include <tools/urlobj.hxx>
-#endif
-#ifndef _UNOTOOLS_CHARCLASS_HXX
 #include <unotools/charclass.hxx>
-#endif
-#ifndef SVTOOLS_URIHELPER_HXX
 #include <svtools/urihelper.hxx>
-#endif
-
-#ifndef _SBXCLASS_HXX //autogen
 #include <basic/sbx.hxx>
-#endif
 #include <vcl/sound.hxx>
 #include <svtools/xtextedt.hxx>
 #include <svtools/txtattr.hxx>
@@ -70,29 +56,18 @@
 #include <basobj.hxx>
 #include <iderdll.hxx>
 #include <iderdll2.hxx>
-
-#ifndef _SV_TASKPANELIST_HXX
 #include <vcl/taskpanelist.hxx>
-#endif
-
-#ifndef _SV_HELP_HXX //autogen
 #include <vcl/help.hxx>
-#endif
 
 //#ifndef _SFX_HELP_HXX //autogen
 //#include <sfx2/sfxhelp.hxx>
 //#endif
-
-#ifndef _SVTOOLS_SOURCEVIEWCONFIG_HXX
 #include <svtools/sourceviewconfig.hxx>
-#endif
 
 #ifndef _COM_SUN_STAR_SCRIPT_XLIBRYARYCONTAINER2_HPP_
 #include <com/sun/star/script/XLibraryContainer2.hpp>
 #endif
-#ifndef _COMPHELPER_PROCESSFACTORY_HXX_
 #include <comphelper/processfactory.hxx>
-#endif
 
 
 using namespace ::com::sun::star;
