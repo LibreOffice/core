3887d6ca288988e32b79936409724b8167a5da43
Rüdiger Timm <rt@openoffice.org>
2008-04-11 02:01:52 +0000

    INTEGRATION: CWS changefileheader (1.95.34); FILE MERGED 2008/04/01 15:51:40 thb 1.95.34.3: #i85898# Stripping all external header guards 2008/04/01 12:49:58 thb 1.95.34.2: #i85898# Stripping all external header guards 2008/03/31 14:23:31 rt 1.95.34.1: #i87441# Change license header to LPGL v3.

--- svx/source/svdraw/svdobj.cxx
+++ svx/source/svdraw/svdobj.cxx
@@ -1,44 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdobj.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.95 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 09:54:34 $
+ * $RCSfile: svdobj.cxx,v $
+ * $Revision: 1.96 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifndef _COM_SUN_STAR_LANG_XCOMPONENT_HPP_
 #include <com/sun/star/lang/XComponent.hpp>
-#endif
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -103,95 +95,32 @@
 #include <svx/xhatch.hxx>
 #include <svx/xflhtit.hxx>
 #include <svx/xbtmpit.hxx>
-
-#ifndef _SVDPOOL_HXX
 #include <svx/svdpool.hxx>
-#endif
-
-#ifndef _MyEDITENG_HXX
 #include <svx/editeng.hxx>
-#endif
-
-#ifndef _SV_SALBTYPE_HXX
 #include <vcl/salbtype.hxx>     // FRound
-#endif
-
-#ifndef _SFX_WHITER_HXX
 #include <svtools/whiter.hxx>
-#endif
 
 // #97849#
-#ifndef _SVX_FMMODEL_HXX
 #include <svx/fmmodel.hxx>
-#endif
-
-#ifndef _SFX_OBJSH_HXX
 #include <sfx2/objsh.hxx>
-#endif
-
-#ifndef _SFXOBJFACE_HXX
 #include <sfx2/objface.hxx>
-#endif
-
-#ifndef _SVX_SVDOIMP_HXX
 #include "svdoimp.hxx"
-#endif
-
-#ifndef _VCL_GRAPHICTOOLS_HXX_
 #include <vcl/graphictools.hxx>
-#endif
-
-#ifndef INCLUDED_SVTOOLS_COLORCFG_HXX
 #include <svtools/colorcfg.hxx>
-#endif
-
-#ifndef _SDR_PROPERTIES_EMPTYPROPERTIES_HXX
 #include <svx/sdr/properties/emptyproperties.hxx>
-#endif
 
 // #110094#
-#ifndef _SDR_CONTACT_VIEWCONTACTOFSDROBJ_HXX
 #include <svx/sdr/contact/viewcontactofsdrobj.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VIEWCONTACTOFGRAPHIC_HXX
 #include <svx/sdr/contact/viewcontactofgraphic.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_OBJECTCONTACTOFOBJLISTPAINTER_HXX
 #include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_DISPLAYINFO_HXX
 #include <svx/sdr/contact/displayinfo.hxx>
-#endif
-
-#ifndef _BGFX_POLYGON_B2DPOLYGON_HXX
 #include <basegfx/polygon/b2dpolygon.hxx>
-#endif
-
-#ifndef _BGFX_POLYGON_B2DPOLYGONTOOLS_HXX
 #include <basegfx/polygon/b2dpolygontools.hxx>
-#endif
-
-#ifndef _BGFX_MATRIX_B2DHOMMATRIX_HXX
 #include <basegfx/matrix/b2dhommatrix.hxx>
-#endif
-
-#ifndef _BGFX_POLYPOLYGON_B2DPOLYGONTOOLS_HXX
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
-#endif
-
-#ifndef _BGFX_RANGE_B2DRANGE_HXX
 #include <basegfx/range/b2drange.hxx>
-#endif
-#ifndef _SVX_UNOSHAPE_HXX
 #include <svx/unoshape.hxx>
-#endif
-
-#ifndef _SV_VIRDEV_HXX
 #include <vcl/virdev.hxx>
-#endif
 
 #include "svx/svdotable.hxx"
 
