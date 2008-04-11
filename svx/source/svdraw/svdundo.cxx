a88d8ed04fef8a532a05d53adc13664ab88b11ec
Rüdiger Timm <rt@openoffice.org>
2008-04-11 02:15:39 +0000

    INTEGRATION: CWS changefileheader (1.30.36); FILE MERGED 2008/04/01 15:51:45 thb 1.30.36.3: #i85898# Stripping all external header guards 2008/04/01 12:50:05 thb 1.30.36.2: #i85898# Stripping all external header guards 2008/03/31 14:23:36 rt 1.30.36.1: #i87441# Change license header to LPGL v3.

--- svx/source/svdraw/svdundo.cxx
+++ svx/source/svdraw/svdundo.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdundo.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.30 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 09:57:13 $
+ * $RCSfile: svdundo.cxx,v $
+ * $Revision: 1.31 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -47,33 +42,18 @@
 #include "svdstr.hrc"   // Namen aus der Resource
 #include "svdglob.hxx"  // StringCache
 #include <svx/scene3d.hxx>
-
-#ifndef _OUTLOBJ_HXX //autogen
 #include <svx/outlobj.hxx>
-#endif
 
 // #i11426#
-#ifndef _SVDOGRP_HXX
 #include <svx/svdogrp.hxx>
-#endif
-
-#ifndef _SDR_PROPERTIES_ITEMSETTOOLS_HXX
 #include <svx/sdr/properties/itemsettools.hxx>
-#endif
-
-#ifndef _SDR_PROPERTIES_PROPERTIES_HXX
 #include <svx/sdr/properties/properties.hxx>
-#endif
 
 // #109587#
-#ifndef _SVDCAPT_HXX
 #include <svx/svdocapt.hxx>
-#endif
 
 // #109587#
-#ifndef _SFX_WHITER_HXX
 #include <svtools/whiter.hxx>
-#endif
 
 #include "svdviter.hxx"
 
