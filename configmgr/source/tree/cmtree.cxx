5fde4c30c310372e214eb9384b209d5a693fa4c0
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:29:44 +0000

    INTEGRATION: CWS changefileheader (1.39.16); FILE MERGED 2008/04/01 15:06:53 thb 1.39.16.3: #i85898# Stripping all external header guards 2008/04/01 12:27:32 thb 1.39.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:52 rt 1.39.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/tree/cmtree.cxx
+++ configmgr/source/tree/cmtree.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: cmtree.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.39 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:31:21 $
+ * $RCSfile: cmtree.cxx,v $
+ * $Revision: 1.40 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,29 +34,14 @@
 #include <stdio.h>
 
 #include "subtree.hxx"
-#ifndef CONFIGMGR_CHANGE_HXX
 #include "change.hxx"
-#endif
-#ifndef CONFIGMGR_TREECHANGELIST_HXX
 #include "treechangelist.hxx"
-#endif
-
-#ifndef CONFIGMGR_TREEPROVIDER_HXX
 #include "treeprovider.hxx"
-#endif
 
 //#include "treeactions.hxx"
-
-#ifndef _RTL_STRING_HXX_
 #include <rtl/string.hxx>
-#endif
-#ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring.hxx>
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
 
 #ifndef INCLUDED_DEQUE
 #include <deque>
