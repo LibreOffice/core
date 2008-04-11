42bdb1d4be3e3065acab165a41b3ba158fc84ca9
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:12:56 +0000

    INTEGRATION: CWS changefileheader (1.7.26); FILE MERGED 2008/04/01 16:03:01 thb 1.7.26.3: #i85898# Stripping all external header guards 2008/04/01 12:59:00 thb 1.7.26.2: #i85898# Stripping all external header guards 2008/03/31 13:07:32 rt 1.7.26.1: #i87441# Change license header to LPGL v3.

--- UnoControls/source/base/multiplexer.cxx
+++ UnoControls/source/base/multiplexer.cxx
@@ -1,52 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: multiplexer.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.7 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:08:53 $
+ * $RCSfile: multiplexer.cxx,v $
+ * $Revision: 1.8 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 //____________________________________________________________________________________________________________
 //  my own include
 //____________________________________________________________________________________________________________
 
-#ifndef _UNOCONTROLS_MULTIPLEXER_HXX
 #include "multiplexer.hxx"
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of other projects
 //____________________________________________________________________________________________________________
-
-#ifndef _VOS_DIAGNOSE_H_
 #include <vos/diagnose.hxx>
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of my own project
