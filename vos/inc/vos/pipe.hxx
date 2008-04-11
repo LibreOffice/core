15412e80dea66c96268851b75fe4c245d2480520
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:51:15 +0000

    INTEGRATION: CWS changefileheader (1.8.38); FILE MERGED 2008/04/01 16:06:07 thb 1.8.38.2: #i85898# Stripping all external header guards 2008/03/31 07:26:55 rt 1.8.38.1: #i87441# Change license header to LPGL v3.

--- vos/inc/vos/pipe.hxx
+++ vos/inc/vos/pipe.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: pipe.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2006-06-02 12:37:52 $
+ * $RCSfile: pipe.hxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,30 +32,16 @@
 #ifndef _VOS_PIPE_HXX_
 #define _VOS_PIPE_HXX_
 
-#ifndef _OSL_PIPE_H_
 #   include <osl/pipe.h>
-#endif
-#ifndef _VOS_TYPES_HXX_
 #   include <vos/types.hxx>
-#endif
-#ifndef _VOS_OBJECT_HXX_
 #   include <vos/object.hxx>
-#endif
-#ifndef _VOS_ISTREAM_HXX_
 #   include <vos/istream.hxx>
-#endif
 #ifndef _VOS_REFERMCE_HXX_
 #   include <vos/refernce.hxx>
 #endif
-#ifndef _VOS_REFOBJ_HXX_
 #   include <vos/refobj.hxx>
-#endif
-#ifndef _VOS_SECURITY_HXX_
 #   include <vos/security.hxx>
-#endif
-#ifndef _RTL_USTRING_HXX_
 #   include <rtl/ustring.hxx>
-#endif
 
 
 namespace vos
