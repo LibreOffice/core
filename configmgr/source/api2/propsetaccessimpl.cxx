c9a95a3ad4b3e95bc50b18895c2f2a8831bfda96
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:06:32 +0000

    INTEGRATION: CWS changefileheader (1.22.16); FILE MERGED 2008/04/01 15:06:32 thb 1.22.16.3: #i85898# Stripping all external header guards 2008/04/01 12:27:13 thb 1.22.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:37 rt 1.22.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/api2/propsetaccessimpl.cxx
+++ configmgr/source/api2/propsetaccessimpl.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: propsetaccessimpl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.22 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:08:00 $
+ * $RCSfile: propsetaccessimpl.cxx,v $
+ * $Revision: 1.23 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,60 +32,23 @@
 #include "precompiled_configmgr.hxx"
 
 #include "propsetaccessimpl.hxx"
-
-#ifndef CONFIGMGR_API_PROPERTYINFOIMPL_HXX_
 #include "propertyinfohelper.hxx"
-#endif
-#ifndef CONFIGMGR_API_NODEACCESS_HXX_
 #include "apinodeaccess.hxx"
-#endif
-#ifndef CONFIGMGR_API_NODEUPDATE_HXX_
 #include "apinodeupdate.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGNODE_HXX_
 #include "noderef.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGVALUEREF_HXX_
 #include "valueref.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGANYNODE_HXX_
 #include "anynoderef.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGCHANGE_HXX_
 #include "nodechange.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGGROUP_HXX_
 #include "configgroup.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGNOTIFIER_HXX_
 #include "confignotifier.hxx"
-#endif
-#ifndef CONFIGMGR_API_BROADCASTER_HXX_
 #include "broadcaster.hxx"
-#endif
-#ifndef CONFIGMGR_API_APITYPES_HXX_
 #include "apitypes.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGURATION_ATTRIBUTES_HXX_
 #include "attributes.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSETINFO_HPP_
 #include <com/sun/star/beans/XPropertySetInfo.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE1_HXX_
 #include <cppuhelper/implbase1.hxx>
-#endif
 
 #ifndef INCLUDED_ALGORITHM
 #include <algorithm>
