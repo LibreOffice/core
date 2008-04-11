7fb15cd394495f791397b49fc4167bb165151161
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:48:17 +0000

    INTEGRATION: CWS changefileheader (1.4.60); FILE MERGED 2008/04/01 16:05:20 thb 1.4.60.3: #i85898# Stripping all external header guards 2008/04/01 13:01:13 thb 1.4.60.2: #i85898# Stripping all external header guards 2008/03/28 15:44:15 rt 1.4.60.1: #i87441# Change license header to LPGL v3.

--- vcl/inc/vcl/pdfextoutdevdata.hxx
+++ vcl/inc/vcl/pdfextoutdevdata.hxx
@@ -1,52 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: pdfextoutdevdata.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2008-01-29 08:21:59 $
+ * $RCSfile: pdfextoutdevdata.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _VCL_PDFEXTOUTDEVDATA_HXX
 #define _VCL_PDFEXTOUTDEVDATA_HXX
 
-#ifndef _VCL_DLLAPI_H
 #include <vcl/dllapi.h>
-#endif
 
 #include <vcl/pdfwriter.hxx>
 #include <vcl/extoutdevdata.hxx>
 #include <vcl/gdimtf.hxx>
 #include <vcl/mapmod.hxx>
-#ifndef _RTTI_HXX
 #include <tools/rtti.hxx>
-#endif
 #include <vector>
 #include <deque>
 
