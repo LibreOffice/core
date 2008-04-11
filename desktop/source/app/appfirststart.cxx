85044840842ccc302b1ada5d6ce7c38d840d22e2
Rüdiger Timm <rt@openoffice.org>
2008-04-11 02:35:08 +0000

    INTEGRATION: CWS changefileheader (1.4.14); FILE MERGED 2008/04/03 14:05:55 pb 1.4.14.3: fix: #i22905# open source needs no license 2008/04/01 10:54:55 thb 1.4.14.2: #i85898# Stripping all external header guards 2008/03/28 15:26:28 rt 1.4.14.1: #i87441# Change license header to LPGL v3.

--- desktop/source/app/appfirststart.cxx
+++ desktop/source/app/appfirststart.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: appfirststart.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2008-03-18 13:45:27 $
+ * $RCSfile: appfirststart.cxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,15 +32,11 @@
 #include <rtl/bootstrap.hxx>
 #include <rtl/ustring.hxx>
 #include <tools/datetime.hxx>
+#include <unotools/configmgr.hxx>
 
 #include <comphelper/processfactory.hxx>
-
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include <com/sun/star/beans/XPropertySet.hpp>
-#endif
-#ifndef _COM_SUN_STAR_BEANS_NAMEDVALUE_HPP_
 #include <com/sun/star/beans/NamedValue.hpp>
-#endif
 
 #include "app.hxx"
 
@@ -118,12 +109,19 @@ OUString Desktop::GetLicensePath()
 /* Check if we need to accept license. */
 sal_Bool Desktop::LicenseNeedsAcceptance()
 {
-    static const ::rtl::OUString BOOTPARAM_SHOWLICENSE = ::rtl::OUString::createFromAscii("HideEula");
-    ::rtl::OUString sValue;
-    sal_Bool        bParamExists = ::rtl::Bootstrap::get(BOOTPARAM_SHOWLICENSE, sValue);
-    sal_Bool        bShowLicense = sal_True;
-    if (bParamExists)
-        bShowLicense = ! sValue.toBoolean();
+    sal_Bool bShowLicense = sal_True;
+    sal_Int32 nOpenSourceContext = 0;
+    try
+    {
+        ::utl::ConfigManager::GetDirectConfigProperty(
+            ::utl::ConfigManager::OPENSOURCECONTEXT ) >>= nOpenSourceContext;
+    }
+    catch( const ::com::sun::star::uno::Exception& ) {}
+
+    // open source needs no license
+    if ( nOpenSourceContext > 0 )
+        bShowLicense = sal_False;
+
     return bShowLicense;
 }
 
