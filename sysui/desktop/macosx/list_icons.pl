f77b10da0c5e8869fd6fa4fc2dc2f37a67ba467f
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:36:11 +0000

    INTEGRATION: CWS changefileheader (1.2.18); FILE MERGED 2008/03/28 15:39:07 rt 1.2.18.1: #i87441# Change license header to LPGL v3.

--- sysui/desktop/macosx/list_icons.pl
+++ sysui/desktop/macosx/list_icons.pl
@@ -4,32 +4,32 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 
 #*************************************************************************
 #
-#   OpenOffice.org - a multi-platform office productivity suite
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
-#   $RCSfile: list_icons.pl,v $
+# Copyright 2008 by Sun Microsystems, Inc.
 #
-#   The Contents of this file are made available subject to
-#   the terms of GNU Lesser General Public License Version 2.1.
+# OpenOffice.org - a multi-platform office productivity suite
 #
+# $RCSfile: list_icons.pl,v $
 #
-#     GNU Lesser General Public License Version 2.1
-#     =============================================
-#     Copyright 2005 by Sun Microsystems, Inc.
-#     901 San Antonio Road, Palo Alto, CA 94303, USA
+# $Revision: 1.3 $
 #
-#     This library is free software; you can redistribute it and/or
-#     modify it under the terms of the GNU Lesser General Public
-#     License version 2.1, as published by the Free Software Foundation.
+# This file is part of OpenOffice.org.
 #
-#     This library is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#     Lesser General Public License for more details.
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
 #
-#     You should have received a copy of the GNU Lesser General Public
-#     License along with this library; if not, write to the Free Software
-#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-#     MA  02111-1307  USA
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
 #
 #*************************************************************************
 
