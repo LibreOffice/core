85e6fec9a891366eb257395a6f9fb529d57c9ece
Rüdiger Timm <rt@openoffice.org>
2008-04-11 10:18:05 +0000

    INTEGRATION: CWS changefileheader (1.6.84); FILE MERGED 2008/04/01 15:02:36 thb 1.6.84.3: #i85898# Stripping all external header guards 2008/04/01 10:49:07 thb 1.6.84.2: #i85898# Stripping all external header guards 2008/03/28 16:31:01 rt 1.6.84.1: #i87441# Change license header to LPGL v3.

--- bridges/source/remote/urp/urp_propertyobject.hxx
+++ bridges/source/remote/urp/urp_propertyobject.hxx
@@ -1,66 +1,46 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: urp_propertyobject.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2006-12-01 14:49:05 $
+ * $RCSfile: urp_propertyobject.hxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #include <stdio.h>
-
-#ifndef _OSL_INTERLCK_H_
 #include <osl/interlck.h>
-#endif
-
-#ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
-#endif
 
 #ifndef _OSL_CONDITN_H_
 #include <osl/conditn.h>
 #endif
-
-#ifndef _RTL_STRING_HXX_
 #include <rtl/string.hxx>
-#endif
-
-#ifndef _UNO_SEQUENCE2_H_
 #include <uno/sequence2.h>
-#endif
 
 #ifndef _BRIDGES_REMOTE_REMOTE_H_
 #include <bridges/remote/remote.h>
 #endif
-
-#ifndef _URP_PROPERTY_HXX_
 #include "urp_property.hxx"
-#endif
 
 namespace bridges_urp {
 
