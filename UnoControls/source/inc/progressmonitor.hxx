8af67bffcd779cd58d8da66dd89274ff3749a134
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:17:57 +0000

    INTEGRATION: CWS changefileheader (1.4.26); FILE MERGED 2008/04/01 12:59:06 thb 1.4.26.2: #i85898# Stripping all external header guards 2008/03/31 13:07:32 rt 1.4.26.1: #i87441# Change license header to LPGL v3.

--- UnoControls/source/inc/progressmonitor.hxx
+++ UnoControls/source/inc/progressmonitor.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: progressmonitor.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:10:26 $
+ * $RCSfile: progressmonitor.hxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,128 +35,49 @@
 //  includes of other projects
 //____________________________________________________________________________________________________________
 
-#ifndef _COM_SUN_STAR_AWT_XFILEDIALOG_HPP_
 #include <com/sun/star/awt/XFileDialog.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTEXTCOMPONENT_HPP_
 #include <com/sun/star/awt/XTextComponent.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XLISTBOX_HPP_
 #include <com/sun/star/awt/XListBox.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XPROGRESSMONITOR_HPP_
 #include <com/sun/star/awt/XProgressMonitor.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_TEXTALIGN_HPP_
 #include <com/sun/star/awt/TextAlign.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XSCROLLBAR_HPP_
 #include <com/sun/star/awt/XScrollBar.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XVCLCONTAINERPEER_HPP_
 #include <com/sun/star/awt/XVclContainerPeer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTABCONTROLLERMODEL_HPP_
 #include <com/sun/star/awt/XTabControllerModel.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XMESSAGEBOX_HPP_
 #include <com/sun/star/awt/XMessageBox.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTEXTEDITFIELD_HPP_
 #include <com/sun/star/awt/XTextEditField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_STYLE_HPP_
 #include <com/sun/star/awt/Style.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTIMEFIELD_HPP_
 #include <com/sun/star/awt/XTimeField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XVCLWINDOWPEER_HPP_
 #include <com/sun/star/awt/XVclWindowPeer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCONTROLMODEL_HPP_
 #include <com/sun/star/awt/XControlModel.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XSPINFIELD_HPP_
 #include <com/sun/star/awt/XSpinField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XUNOCONTROLCONTAINER_HPP_
 #include <com/sun/star/awt/XUnoControlContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTEXTLAYOUTCONSTRAINS_HPP_
 #include <com/sun/star/awt/XTextLayoutConstrains.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XNUMERICFIELD_HPP_
 #include <com/sun/star/awt/XNumericField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XBUTTON_HPP_
 #include <com/sun/star/awt/XButton.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTEXTAREA_HPP_
 #include <com/sun/star/awt/XTextArea.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XIMAGEBUTTON_HPP_
 #include <com/sun/star/awt/XImageButton.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XFIXEDTEXT_HPP_
 #include <com/sun/star/awt/XFixedText.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCONTROLCONTAINER_HPP_
 #include <com/sun/star/awt/XControlContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XDIALOG_HPP_
 #include <com/sun/star/awt/XDialog.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_SCROLLBARORIENTATION_HPP_
 #include <com/sun/star/awt/ScrollBarOrientation.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XRADIOBUTTON_HPP_
 #include <com/sun/star/awt/XRadioButton.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCURRENCYFIELD_HPP_
 #include <com/sun/star/awt/XCurrencyField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XPATTERNFIELD_HPP_
 #include <com/sun/star/awt/XPatternField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_VCLWINDOWPEERATTRIBUTE_HPP_
 #include <com/sun/star/awt/VclWindowPeerAttribute.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTABCONTROLLER_HPP_
 #include <com/sun/star/awt/XTabController.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XVCLCONTAINER_HPP_
 #include <com/sun/star/awt/XVclContainer.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XDATEFIELD_HPP_
 #include <com/sun/star/awt/XDateField.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCOMBOBOX_HPP_
 #include <com/sun/star/awt/XComboBox.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCONTROL_HPP_
 #include <com/sun/star/awt/XControl.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XCHECKBOX_HPP_
 #include <com/sun/star/awt/XCheckBox.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_MESSAGEBOXCOMMAND_HPP_
 #include <com/sun/star/awt/MessageBoxCommand.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XLAYOUTCONSTRAINS_HPP_
 #include <com/sun/star/awt/XLayoutConstrains.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XPROGRESSBAR_HPP_
 #include <com/sun/star/awt/XProgressBar.hpp>
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of my own project
 //____________________________________________________________________________________________________________
-
-#ifndef _UNOCONTROLS_BASECONTAINERCONTROL_CTRL_HXX
 #include "basecontainercontrol.hxx"
-#endif
 
 //____________________________________________________________________________________________________________
 //  namespace
