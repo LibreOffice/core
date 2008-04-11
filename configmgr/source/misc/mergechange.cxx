369fd1883c7a4fa006f2f619bbe06e1234fa85cf
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:21:49 +0000

    INTEGRATION: CWS changefileheader (1.25.16); FILE MERGED 2008/04/01 12:27:29 thb 1.25.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:50 rt 1.25.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/misc/mergechange.cxx
+++ configmgr/source/misc/mergechange.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: mergechange.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.25 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:29:17 $
+ * $RCSfile: mergechange.cxx,v $
+ * $Revision: 1.26 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,39 +34,17 @@
 #include <stdio.h>
 
 #include "mergechange.hxx"
-
-#ifndef CONFIGMGR_MERGEHELPER_HXX
 #include "mergehelper.hxx"
-#endif
-#ifndef CONFIGMGR_UPDATEHELPER_HXX
 #include "updatehelper.hxx"
-#endif
-#ifndef _CONFIGMGR_TREEACTIONS_HXX_
 #include "treeactions.hxx"
-#endif
-
-#ifndef CONFIGMGR_CHANGE_HXX
 #include "change.hxx"
-#endif
-#ifndef CONFIGMGR_TREE_CHANGEFACTORY_HXX
 #include "treechangefactory.hxx"
-#endif
-#ifndef CONFIGMGR_TREECHANGELIST_HXX
 #include "treechangelist.hxx"
-#endif
-
-#ifndef CONFIGMGR_CONFIGEXCEPT_HXX_
 #include "configexcept.hxx"
-#endif
-
-#ifndef CONFIGMGR_TREEACCESSOR_HXX
 #include "treeaccessor.hxx"
-#endif
 
 
-#ifndef _CONFIGMGR_TRACER_HXX_
 #include "tracer.hxx"
-#endif
 
 #define ASCII(x) rtl::OUString::createFromAscii(x)
 
