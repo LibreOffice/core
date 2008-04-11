fd3bc3013d72e956b2afacd4778d3629fe915bdf
Rüdiger Timm <rt@openoffice.org>
2008-04-11 01:55:14 +0000

    INTEGRATION: CWS changefileheader (1.19.36); FILE MERGED 2008/04/01 15:51:36 thb 1.19.36.3: #i85898# Stripping all external header guards 2008/04/01 12:49:50 thb 1.19.36.2: #i85898# Stripping all external header guards 2008/03/31 14:23:28 rt 1.19.36.1: #i87441# Change license header to LPGL v3.

--- svx/source/svdraw/svddrgmt.cxx
+++ svx/source/svdraw/svddrgmt.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svddrgmt.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 09:49:08 $
+ * $RCSfile: svddrgmt.cxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -42,10 +37,7 @@
 #ifndef _MATH_H
 #define _MATH_H
 #endif
-
-#ifndef _BIGINT_HXX //autogen
 #include <tools/bigint.hxx>
-#endif
 #include <vcl/svapp.hxx>
 
 #include "xattr.hxx"
@@ -65,42 +57,15 @@
 #include <svx/dialogs.hrc>
 #include <svx/dialmgr.hxx>
 #include <svx/sdgcpitm.hxx>
-
-#ifndef _BGFX_POLYGON_B2DPOLYGON_HXX
 #include <basegfx/polygon/b2dpolygon.hxx>
-#endif
-
-#ifndef _BGFX_POLYGON_B2DPOLYGONTOOLS_HXX
 #include <basegfx/polygon/b2dpolygontools.hxx>
-#endif
-
-#ifndef _SDR_OVERLAY_OVERLAYPOLYPOLYGON_HXX
 #include <svx/sdr/overlay/overlaypolypolygon.hxx>
-#endif
-
-#ifndef _SDR_OVERLAY_OVERLAYMANAGER_HXX
 #include <svx/sdr/overlay/overlaymanager.hxx>
-#endif
-
-#ifndef _SDR_OVERLAY_OVERLAYROOLINGRECTANGLE_HXX
 #include <svx/sdr/overlay/overlayrollingrectangle.hxx>
-#endif
-
-#ifndef _SDRPAGEWINDOW_HXX
 #include <svx/sdrpagewindow.hxx>
-#endif
-
-#ifndef _SDRPAINTWINDOW_HXX
 #include <sdrpaintwindow.hxx>
-#endif
-
-#ifndef _BGFX_MATRIX_B2DHOMMATRIX_HXX
 #include <basegfx/matrix/b2dhommatrix.hxx>
-#endif
-
-#ifndef _BGFX_POLYPOLYGON_B2DPOLYGONTOOLS_HXX
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
