fcde53fc529c17e87448c9972c745fa5ee35f908
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:31:32 +0000

    INTEGRATION: CWS changefileheader (1.10.12); FILE MERGED 2008/04/01 15:17:03 thb 1.10.12.3: #i85898# Stripping all external header guards 2008/04/01 12:30:50 thb 1.10.12.2: #i85898# Stripping all external header guards 2008/03/31 13:13:16 rt 1.10.12.1: #i87441# Change license header to LPGL v3.

--- fpicker/source/win32/filepicker/FileOpenDlg.hxx
+++ fpicker/source/win32/filepicker/FileOpenDlg.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: FileOpenDlg.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.10 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 07:31:25 $
+ * $RCSfile: FileOpenDlg.hxx,v $
+ * $Revision: 1.11 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,23 +35,15 @@
 // includes
 //------------------------------------------------------------------------
 
-#ifndef _SAL_TYPES_H_
 #include <sal/types.h>
-#endif
 
 #ifndef _RTL_USTRING_HXX_
 #include <rtl/ustring>
 #endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
 
 #include "platform_xp.h"
-
-#ifndef _GETFILENAMEWRAPPER_HXX_
 #include "getfilenamewrapper.hxx"
-#endif
 
 // because we don't want to import the new W2k platform skd
 // into our build environment if have stolen the definition
