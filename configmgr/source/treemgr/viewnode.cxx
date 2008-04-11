450ef38177839ab942e8052c0b0ac2b0e4700050
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:59:19 +0000

    INTEGRATION: CWS changefileheader (1.5.16); FILE MERGED 2008/04/01 12:27:39 thb 1.5.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:57 rt 1.5.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/treemgr/viewnode.cxx
+++ configmgr/source/treemgr/viewnode.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: viewnode.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:49:36 $
+ * $RCSfile: viewnode.cxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,33 +32,14 @@
 #include "precompiled_configmgr.hxx"
 
 #include "viewnode.hxx"
-
-#ifndef CONFIGMGR_VALUENODEACCESS_HXX
 #include "valuenodeaccess.hxx"
-#endif
-#ifndef CONFIGMGR_GROUPNODEACCESS_HXX
 #include "groupnodeaccess.hxx"
-#endif
-#ifndef CONFIGMGR_SETNODEACCESS_HXX
 #include "setnodeaccess.hxx"
-#endif
-
-#ifndef CONFIGMGR_SETNODEBEHAVIOR_HXX_
 #include "setnodeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_GROUPNODEBEHAVIOR_HXX_
 #include "groupnodeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_VALUENODEBEHAVIOR_HXX_
 #include "valuenodeimpl.hxx"
-#endif
-
-#ifndef CONFIGMGR_CONFIGNODEIMPL_HXX_
 #include "treeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_VIEWBEHAVIOR_HXX_
 #include "viewstrategy.hxx"
-#endif
 
 //-----------------------------------------------------------------------------
 namespace configmgr
