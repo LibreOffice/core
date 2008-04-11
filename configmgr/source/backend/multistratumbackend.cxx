83d8fdc8813f07e8973338079e8da087f3d2e6e1
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:31:25 +0000

    INTEGRATION: CWS changefileheader (1.12.16); FILE MERGED 2008/04/01 15:06:38 thb 1.12.16.3: #i85898# Stripping all external header guards 2008/04/01 12:27:18 thb 1.12.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:40 rt 1.12.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/backend/multistratumbackend.cxx
+++ configmgr/source/backend/multistratumbackend.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: multistratumbackend.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.12 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:10:34 $
+ * $RCSfile: multistratumbackend.cxx,v $
+ * $Revision: 1.13 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,50 +36,21 @@
 #ifndef CONFIGMGR_API_FACTORY_HXX_
 #include "confapifactory.hxx"
 #endif // CONFIGMGR_API_FACTORY_HXX_
-
-#ifndef CONFIGMGR_SERVICEINFOHELPER_HXX_
 #include "serviceinfohelper.hxx"
-#endif // CONFIGMGR_SERVICEINFOHELPER_HXX_
-
-#ifndef CONFIGMGR_BACKEND_BACKENDSTRATALISTENER_HXX
 #include "backendstratalistener.hxx"
-#endif
 
 #ifndef _CONFIGMGR_BOOTSTRAP_HXX
 #include "bootstrap.hxx"
 #endif
-
-#ifndef CONFIGMGR_SIMPLEINTERACTIONREQUEST_HXX
 #include "simpleinteractionrequest.hxx"
-#endif
-
-#ifndef CONFIGMGR_CONFIGINTERACTIONHANDLER_HXX
 #include "configinteractionhandler.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XMULTILAYERSTRATUM_HPP_
 #include <com/sun/star/configuration/backend/XMultiLayerStratum.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XSINGLELAYERSTRATUM_HPP_
 #include <com/sun/star/configuration/backend/XSingleLayerStratum.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_STRATUMCREATIONEXCEPTION_HPP_
 #include <com/sun/star/configuration/backend/StratumCreationException.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
 #include <com/sun/star/task/XInteractionHandler.hpp>
-#endif // _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
-#ifndef _COM_SUN_STAR_LANG_DISPOSEDEXCEPTION_HPP_
 #include <com/sun/star/lang/DisposedException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_WRAPPEDTARGETRUNTIMEEXCEPTION_HPP_
 #include <com/sun/star/lang/WrappedTargetRuntimeException.hpp>
-#endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif // _RTL_USTRBUF_HXX_
 
 #include <cppuhelper/exc_hlp.hxx>
 
