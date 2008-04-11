6b8a273c17cdcd246f7ffdb21ffe7aeb3a14a252
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:39:30 +0000

    INTEGRATION: CWS changefileheader (1.2.320); FILE MERGED 2008/04/01 16:05:18 thb 1.2.320.3: #i85898# Stripping all external header guards 2008/04/01 13:01:11 thb 1.2.320.2: #i85898# Stripping all external header guards 2008/03/28 15:44:13 rt 1.2.320.1: #i87441# Change license header to LPGL v3.

--- vcl/inc/vcl/impfont.hxx
+++ vcl/inc/vcl/impfont.hxx
@@ -1,64 +1,43 @@
-
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: impfont.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: vg $ $Date: 2007-04-11 17:57:41 $
+ * $RCSfile: impfont.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _SV_IMPFONT_HXX
 #define _SV_IMPFONT_HXX
 
-#ifndef _TOOLS_GEN_HXX
 #include <tools/gen.hxx>
-#endif
-#ifndef _TOOLS_STRING_HXX
 #include <tools/string.hxx>
-#endif
-#ifndef INCLUDED_I18NPOOL_LANG_H
 #include <i18npool/lang.h>
-#endif
-#ifndef _TOOLS_COLOR_HXX
 #include <tools/color.hxx>
-#endif
-#ifndef _VCL_DLLAPI_H
 #include <vcl/dllapi.h>
-#endif
-
-#ifndef _VCL_VCLENUM_HXX
 #include <vcl/vclenum.hxx>
-#endif
-#ifndef _VCL_FNTSTYLE_HXX
 #include <vcl/fntstyle.hxx>
-#endif
 
 // ------------
 // - Impl_Font -
