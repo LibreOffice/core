77b6bbe47a476731a09b8d00ad4178ffb5861f75
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:46:28 +0000

    INTEGRATION: CWS changefileheader (1.41.22); FILE MERGED 2008/04/01 21:58:29 thb 1.41.22.4: #i85898# Corrected misspelled header name; removed misspelled include guard 2008/04/01 16:05:56 thb 1.41.22.3: #i85898# Stripping all external header guards 2008/04/01 13:02:06 thb 1.41.22.2: #i85898# Stripping all external header guards 2008/03/28 15:45:51 rt 1.41.22.1: #i87441# Change license header to LPGL v3.

--- vcl/win/source/app/salinst.cxx
+++ vcl/win/source/app/salinst.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: salinst.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.41 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-07 16:48:33 $
+ * $RCSfile: salinst.cxx,v $
+ * $Revision: 1.42 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -45,61 +40,22 @@
 #include <excpt.h>
 #endif
 
-#ifndef _VOS_MUTEX_HXX
 #include <vos/mutex.hxx>
-#endif
-
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-
-#ifndef _SV_WINCOMP_HXX
 #include <wincomp.hxx>
-#endif
-#ifndef _SV_SALIDS_HRC
 #include <salids.hrc>
-#endif
-#ifndef _SV_SALDATA_HXX
 #include <saldata.hxx>
-#endif
-#ifndef _SV_SALINST_H
 #include <salinst.h>
-#endif
-#ifndef _SV_SALFRAME_H
 #include <salframe.h>
-#endif
-#ifndef _SV_SALOBJ_H
 #include <salobj.h>
-#endif
-#ifndef _SV_SALSYS_HXX
 #include <vcl/salsys.hxx>
-#endif
-#ifndef _SV_SALTIMER_H
 #include <saltimer.h>
-#endif
-#ifndef _SV_SALATYPE_HXX
 #include <vcl/salatype.hxx>
-#endif
-#ifndef _SV_SYSDATA_HXX
-#include <sysdata.hxx>
-#endif
-#ifndef _SV_SALOGL_H
 #include <salogl.h>
-#endif
-#ifndef _SV_SALBMP_H
 #include <salbmp.h>
-#endif
-#ifndef _SV_SALIMESTATUS_HXX
 #include <vcl/salimestatus.hxx>
-#endif
-
-#ifndef _SV_TIMER_HXX
 #include <vcl/timer.hxx>
-#endif
-
-#ifndef _SV_WINCOMP_HXX
 #include <wincomp.hxx>  // CS_DROPSHADOW
-#endif
 
 // =======================================================================
 
