3d9b2f93a936aa897d8818a86e37eefbd093adbb
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:58:41 +0000

    INTEGRATION: CWS changefileheader (1.34.70); FILE MERGED 2008/04/01 15:15:18 thb 1.34.70.3: #i85898# Stripping all external header guards 2008/04/01 12:29:50 thb 1.34.70.2: #i85898# Stripping all external header guards 2008/03/31 12:31:43 rt 1.34.70.1: #i87441# Change license header to LPGL v3.

--- extensions/source/propctrlr/formmetadata.hxx
+++ extensions/source/propctrlr/formmetadata.hxx
@@ -1,54 +1,42 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: formmetadata.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.34 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-27 11:52:15 $
+ * $RCSfile: formmetadata.hxx,v $
+ * $Revision: 1.35 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _EXTENSIONS_PROPCTRLR_FORMMETADATA_HXX_
 #define _EXTENSIONS_PROPCTRLR_FORMMETADATA_HXX_
 
-#ifndef _EXTENSIONS_PROPCTRLR_PROPERTYINFO_HXX_
 #include "propertyinfo.hxx"
-#endif
 #ifndef _EXTENSIONS_PROPCTRLR_MODULEPRC_HXX_
 #include "modulepcr.hxx"
 #endif
-#ifndef EXTENSIONS_SOURCE_PROPCTRLR_ENUMREPRESENTATION_HXX
 #include "enumrepresentation.hxx"
-#endif
-
-#ifndef _COMPHELPER_COMPOSEDPROPS_HXX_
 #include <comphelper/composedprops.hxx>
-#endif
 
 //............................................................................
 namespace pcr
