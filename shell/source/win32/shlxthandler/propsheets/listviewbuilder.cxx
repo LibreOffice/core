fddf238b7d2de45d5342b25d9efce41db676797d
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:21:31 +0000

    INTEGRATION: CWS changefileheader (1.8.44); FILE MERGED 2008/04/01 12:41:15 thb 1.8.44.2: #i85898# Stripping all external header guards 2008/03/31 13:17:13 rt 1.8.44.1: #i87441# Change license header to LPGL v3.

--- shell/source/win32/shlxthandler/propsheets/listviewbuilder.cxx
+++ shell/source/win32/shlxthandler/propsheets/listviewbuilder.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: listviewbuilder.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 14:24:13 $
+ * $RCSfile: listviewbuilder.cxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -43,22 +38,10 @@
 //------------------------------------
 // include
 //------------------------------------
-
-#ifndef LISTVIEWBUILDER_HXX_INCLUDED
 #include "listviewbuilder.hxx"
-#endif
-
-#ifndef DOCUMENT_STATISTIC_HXX_INCLUDED
 #include "document_statistic.hxx"
-#endif
-
-#ifndef UTILITIES_HXX_INCLUDED
 #include "internal/utilities.hxx"
-#endif
-
-#ifndef CONFIG_HXX_INCLUDED
 #include "internal/config.hxx"
-#endif
 
 #if defined _MSC_VER
 #pragma warning(push, 1)
@@ -69,10 +52,7 @@
 #endif
 #include <commctrl.h>
 #include <tchar.h>
-
-#ifndef RESOURCE_H_INCLUDED
 #include "internal/resource.h"
-#endif
 
 //------------------------------------
 //
