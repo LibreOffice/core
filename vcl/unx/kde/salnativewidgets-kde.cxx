202c95dde3fb340046cca785d97cbc3e964f88dc
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:45:14 +0000

    INTEGRATION: CWS changefileheader (1.25.22); FILE MERGED 2008/04/01 16:05:48 thb 1.25.22.3: #i85898# Stripping all external header guards 2008/04/01 13:01:57 thb 1.25.22.2: #i85898# Stripping all external header guards 2008/03/28 15:45:23 rt 1.25.22.1: #i87441# Change license header to LPGL v3.

--- vcl/unx/kde/salnativewidgets-kde.cxx
+++ vcl/unx/kde/salnativewidgets-kde.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: salnativewidgets-kde.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.25 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-03-05 16:51:15 $
+ * $RCSfile: salnativewidgets-kde.cxx,v $
+ * $Revision: 1.26 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,20 +32,11 @@
 #include "precompiled_vcl.hxx"
 
 #define _SV_SALNATIVEWIDGETS_KDE_CXX
-
-#ifndef INCLUDED_VCL_KDE_HEADERS_H
 #include "kde_headers.h"
-#endif
 
 #include <salunx.h>
-
-#ifndef _SV_SALDATA_HXX
 #include <saldata.hxx>
-#endif
-
-#ifndef _SV_SALDISP_HXX
 #include <saldisp.hxx>
-#endif
 
 #ifndef _SV_SALGDI_HXX
 #include <salgdi.h>
@@ -59,18 +45,9 @@
 #ifndef _SV_SALGDI_HXX
 #include <salframe.h>
 #endif
-
-#ifndef _SV_SETTINGS_HXX
 #include <vcl/settings.hxx>
-#endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-
-#ifndef _VCL_KDEDATA_HXX
 #include <plugins/kde/kdedata.hxx>
-#endif
 #include <iostream>
 
 #include <pspgraphics.h>
