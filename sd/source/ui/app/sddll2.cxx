111120817ac47077c0f23174232acef7153a1afb
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:09:04 +0000

    INTEGRATION: CWS changefileheader (1.33.12); FILE MERGED 2008/04/01 15:34:12 thb 1.33.12.3: #i85898# Stripping all external header guards 2008/04/01 12:38:37 thb 1.33.12.2: #i85898# Stripping all external header guards 2008/03/31 13:57:15 rt 1.33.12.1: #i87441# Change license header to LPGL v3.

--- sd/source/ui/app/sddll2.cxx
+++ sd/source/ui/app/sddll2.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: sddll2.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.35 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-03 13:26:12 $
+ * $RCSfile: sddll2.cxx,v $
+ * $Revision: 1.36 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,93 +36,36 @@
 #include <svx/editdata.hxx>
 #include "eetext.hxx"
 #include <svx/svxids.hrc>
-
-#ifndef _EEITEM_HXX //autogen
 #include <svx/eeitem.hxx>
-#endif
 
 #include <svx/flditem.hxx>
-#ifndef _IMAPDLG_HXX_ //autogen
 #include <svx/imapdlg.hxx>
-#endif
-#ifndef _BMPMASK_HXX_ //autogen
 #include <svx/bmpmask.hxx>
-#endif
-#ifndef _SVX_GALBRWS_HXX_ //autogen
 #include <svx/galbrws.hxx>
-#endif
-#ifndef _SVX_SRCHDLG_HXX //autogen
 #include <svx/srchdlg.hxx>
-#endif
-#ifndef _SVX_FONTWORK_HXX //autogen
 #include <svx/fontwork.hxx>
-#endif
-#ifndef _SVX_COLRCTRL_HXX //autogen
 #include <svx/colrctrl.hxx>
-#endif
-#ifndef _SVX_VERT_TEXT_TBXCTRL_HXX
 #include <svx/verttexttbxctrl.hxx>
-#endif
-#ifndef _SVX_DLG_HYPERLINK_HXX //autogen
 #include <svx/hyprlink.hxx>
-#endif
-#ifndef _SVX_TAB_HYPERLINK_HXX
 #include <svx/hyperdlg.hxx>
-#endif
-#ifndef _FILLCTRL_HXX //autogen
 #include <svx/fillctrl.hxx>
-#endif
-#ifndef _SVX_LINECTRL_HXX //autogen
 #include <svx/linectrl.hxx>
-#endif
-#ifndef _SVX_TBCONTRL_HXX //autogen
 #include <svx/tbcontrl.hxx>
-#endif
-#ifndef _SVX_ZOOMCTRL_HXX //autogen
 #include <svx/zoomctrl.hxx>
-#endif
-#ifndef _SVX_PSZCTRL_HXX //autogen
 #include <svx/pszctrl.hxx>
-#endif
-#ifndef _SVX_MODCTRL_HXX //autogen
 #include <svx/modctrl.hxx>
-#endif
-#ifndef _SVX_FNTCTL_HXX //autogen
 #include <svx/fntctl.hxx>
-#endif
-#ifndef _SVX_FNTSZCTL_HXX //autogen
 #include <svx/fntszctl.hxx>
-#endif
-#ifndef _SVX_F3DCHILD_HXX //autogen
 #include <svx/f3dchild.hxx>
-#endif
-#ifndef _SVX_GRAFCTRL_HXX
 #include <svx/grafctrl.hxx>
-#endif
-#ifndef _SVX_TBXCUSTOMSHAPES_HXX
 #include <svx/tbxcustomshapes.hxx>
-#endif
-#ifndef _SVX_FONTWORK_GALLERY_DIALOG_HXX
 #include <svx/fontworkgallery.hxx>
-#endif
-#ifndef _SVX_LBOXCTRL_HXX_
 #include <svx/lboxctrl.hxx>
-#endif
-#ifndef _SVX_CLIPBOARDCTL_HXX_
 #include <svx/clipboardctl.hxx>
-#endif
-#ifndef _SVX_EXTRUSION_CONTROLS_HXX
 #include <svx/extrusioncontrols.hxx>
-#endif
-#ifndef _SVX_TBXCOLOR_HXX
 #include <svx/tbxcolor.hxx>
-#endif
-#ifndef _AVMEDIA_MEDIACHILD_HXX
 #include <avmedia/mediaplayer.hxx>
-#endif
-#ifndef _AVMEDIA_MEDIATOOLBOX_HXX
 #include <avmedia/mediatoolbox.hxx>
-#endif
 
 #include <svx/layctrl.hxx>
 #include <svx/subtoolboxcontrol.hxx>
@@ -137,32 +75,20 @@
 #include "diactrl.hxx"
 #include "gluectrl.hxx"
 #include "tbx_ww.hxx"
-#ifndef SD_ANIMATION_CHILD_WINDOW_HXX
 #include "AnimationChildWindow.hxx"
-#endif
 #include "animobjs.hxx"
-#ifndef SD_NAVIGATOR_CHILD_WINDOW_HXX
 #include "NavigatorChildWindow.hxx"
-#endif
 #ifndef SD_LAZER_DIALOG_CHILD_WINDOW_HXX
 #include "LayerDialogChildWindow.hxx"
 #endif
 //#include "3dchld.hxx"
 #include "app.hrc"
 #include "SpellDialogChildWindow.hxx"
-#ifndef SD_DRAW_VIEW_SHELL_HXX
 #include "DrawViewShell.hxx"
-#endif
-#ifndef SD_GRAPHIC_VIEW_SHELL_HXX
 #include "GraphicViewShell.hxx"
-#endif
 #include <svx/xmlsecctrl.hxx>
-#ifndef _SVX_FORMATPAINTBRUSHCTRL_HXX
 #include <svx/formatpaintbrushctrl.hxx>
-#endif
-#ifndef SD_PANE_CHILD_WINDOWS_HXX
 #include "PaneChildWindows.hxx"
-#endif
 
 #include "tmplctrl.hxx"
 
