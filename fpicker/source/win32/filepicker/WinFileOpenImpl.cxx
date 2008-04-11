030236c27c9b6df6aec59213203851107e5c8ed9
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:35:21 +0000

    INTEGRATION: CWS changefileheader (1.29.12); FILE MERGED 2008/04/01 15:17:04 thb 1.29.12.3: #i85898# Stripping all external header guards 2008/04/01 12:30:50 thb 1.29.12.2: #i85898# Stripping all external header guards 2008/03/31 13:13:17 rt 1.29.12.1: #i87441# Change license header to LPGL v3.

--- fpicker/source/win32/filepicker/WinFileOpenImpl.cxx
+++ fpicker/source/win32/filepicker/WinFileOpenImpl.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: WinFileOpenImpl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.29 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2008-03-12 07:33:29 $
+ * $RCSfile: WinFileOpenImpl.cxx,v $
+ * $Revision: 1.30 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,70 +36,25 @@
 //------------------------------------------------------------------------
 
 #include "shared.hxx"
-
-#ifndef _WINFILEOPENIMPL_HXX_
 #include "WinFileOpenImpl.hxx"
-#endif
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
-
-#ifndef _OSL_FILE_HXX_
 #include <osl/file.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_EXTENDEDFILEPICKERELEMENTIDS_HPP_
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_FILEPICKEREVENT_HPP_
 #include <com/sun/star/ui/dialogs/FilePickerEvent.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_EXECUTABLEDIALOGRESULTS_HPP_
 #include <com/sun/star/ui/dialogs/ExecutableDialogResults.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_COMMONFILEPICKERELEMENTIDS_HPP_
 #include <com/sun/star/ui/dialogs/CommonFilePickerElementIds.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_FILEPREVIEWIMAGEFORMATS_HPP_
 #include <com/sun/star/ui/dialogs/FilePreviewImageFormats.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_UI_DIALOGS_LISTBOXCONTROLACTIONS_HPP_
 #include <com/sun/star/ui/dialogs/ListBoxControlActions.hpp>
-#endif
-
-#ifndef _WINIMPLHELPER_HXX_
 #include "..\misc\WinImplHelper.hxx"
-#endif
 
 #ifndef _FILEPICKER_HXX_
 #include "filepicker.hxx"
 #endif
-
-#ifndef _CONTROLACCESS_HXX_
 #include "controlaccess.hxx"
-#endif
-
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-
-#ifndef _RTL_STRING_HXX_
 #include <rtl/string.hxx>
-#endif
-
-#ifndef _THREAD_HXX_
 #include <osl/thread.hxx>
-#endif
-
-#ifndef _FILEPICKERSTATE_HXX_
 #include "filepickerstate.hxx"
-#endif
 
 //------------------------------------------------------------------------
 // namespace directives
