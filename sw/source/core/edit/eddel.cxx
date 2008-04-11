bfc19375611b60b17eecbde41af897926ef3cbcc
Rüdiger Timm <rt@openoffice.org>
2008-04-11 13:00:44 +0000

    INTEGRATION: CWS notes3 (1.14.142); FILE MERGED 2008/03/08 16:18:35 mod 1.14.142.1: ease resync

--- sw/source/core/edit/eddel.cxx
+++ sw/source/core/edit/eddel.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: eddel.cxx,v $
- * $Revision: 1.15 $
+ * $Revision: 1.16 $
  *
  * This file is part of OpenOffice.org.
  *
