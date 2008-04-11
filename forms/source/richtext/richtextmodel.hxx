b6af4fecc3fdc0e6ec18960a9f9dd5820bb3b67c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:47:12 +0000

    INTEGRATION: CWS changefileheader (1.10.68); FILE MERGED 2008/04/01 15:16:39 thb 1.10.68.3: #i85898# Stripping all external header guards 2008/04/01 12:30:30 thb 1.10.68.2: #i85898# Stripping all external header guards 2008/03/31 13:11:41 rt 1.10.68.1: #i87441# Change license header to LPGL v3.

--- forms/source/richtext/richtextmodel.hxx
+++ forms/source/richtext/richtextmodel.hxx
@@ -1,76 +1,48 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: richtextmodel.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.10 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-06 09:57:09 $
+ * $RCSfile: richtextmodel.hxx,v $
+ * $Revision: 1.11 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTMODEL_HXX
 #define FORMS_SOURCE_RICHTEXT_RICHTEXTMODEL_HXX
 
-#ifndef _FORMS_FORMCOMPONENT_HXX_
 #include "FormComponent.hxx"
-#endif
-#ifndef FRM_MODULE_HXX
 #include "frm_module.hxx"
-#endif
-#ifndef FORMS_SOURCE_COMPONENT_FORMCONTROLFONT_HXX
 #include "formcontrolfont.hxx"
-#endif
-#ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTUNOWRAPPER_HXX
 #include "richtextunowrapper.hxx"
-#endif
-
-#ifndef COMPHELPER_PROPERTYCONTAINERHELPER_HXX
 #include <comphelper/propertycontainerhelper.hxx>
-#endif
-#ifndef COMPHELPER_INC_COMPHELPER_LISTENERNOTIFICATION_HXX
 #include <comphelper/listenernotification.hxx>
-#endif
 
 /** === begin UNO includes === **/
-#ifndef _COM_SUN_STAR_AWT_XDEVICE_HPP_
 #include <com/sun/star/awt/XDevice.hpp>
-#endif
-#ifndef _COM_SUN_STAR_UTIL_XMODIFYBROADCASTER_HPP_
 #include <com/sun/star/util/XModifyBroadcaster.hpp>
-#endif
 /** === end UNO includes === **/
-
-#ifndef _CPPUHELPER_IMPLBASE3_HXX_
 #include <cppuhelper/implbase3.hxx>
-#endif
-
-#ifndef _LINK_HXX
 #include <tools/link.hxx>
-#endif
 #include <memory>
 
 class EditEngine;
