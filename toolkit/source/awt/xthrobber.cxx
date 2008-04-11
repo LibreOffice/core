97f2432785b726e86d1ed35e3bc4e8c3c0339bd8
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:23:33 +0000

    INTEGRATION: CWS changefileheader (1.3.148); FILE MERGED 2008/04/01 16:00:57 thb 1.3.148.3: #i85898# Stripping all external header guards 2008/04/01 12:56:49 thb 1.3.148.2: #i85898# Stripping all external header guards 2008/03/28 15:40:15 rt 1.3.148.1: #i87441# Change license header to LPGL v3.

--- toolkit/source/awt/xthrobber.cxx
+++ toolkit/source/awt/xthrobber.cxx
@@ -1,67 +1,46 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: xthrobber.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.4 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-04 10:55:16 $
+ * $RCSfile: xthrobber.cxx,v $
+ * $Revision: 1.5 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_toolkit.hxx"
-
-#ifndef   TOOLKIT_AWT_XTHROBBER_HXX
 #include "toolkit/awt/xthrobber.hxx"
-#endif
-#ifndef  _TOOLKIT_HELPER_PROPERTY_HXX_
 #include "toolkit/helper/property.hxx"
-#endif
-#ifndef  _TOOLKIT_HELPER_TKRESMGR_HXX_
 #include <toolkit/helper/tkresmgr.hxx>
-#endif
-#ifndef  _TOOLKIT_HELPER_THROBBERIMPL_HXX_
 #include <toolkit/helper/throbberimpl.hxx>
-#endif
 
 #ifndef _TOOLKIT_AWT_XTHROBBER_HRC_
 #include "xthrobber.hrc"
 #endif
-
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-#ifndef   _SV_FIXED_HXX
 #include <vcl/fixed.hxx>
-#endif
-#ifndef   _SV_TIMER_HXX
 #include <vcl/timer.hxx>
-#endif
 
 //........................................................................
 namespace toolkit
