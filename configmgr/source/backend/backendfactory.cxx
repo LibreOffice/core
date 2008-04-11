733fe4f371fb35c002abddad0e90ea3f428e1f03
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:15:39 +0000

    INTEGRATION: CWS changefileheader (1.11.72); FILE MERGED 2008/04/01 12:27:15 thb 1.11.72.2: #i85898# Stripping all external header guards 2008/03/31 12:22:39 rt 1.11.72.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/backend/backendfactory.cxx
+++ configmgr/source/backend/backendfactory.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: backendfactory.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.11 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:01:29 $
+ * $RCSfile: backendfactory.cxx,v $
+ * $Revision: 1.12 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -41,33 +36,16 @@
 #ifndef CONFIGMGR_API_FACTORY_HXX_
 #include "confapifactory.hxx"
 #endif
-#ifndef CONFIGMGR_BOOTSTRAPCONTEXT_HXX_
 #include "bootstrapcontext.hxx"
-#endif
-#ifndef CONFIGMGR_BOOTSTRAP_HXX_
 #include "bootstrap.hxx"
-#endif
-#ifndef CONFIGMGR_BACKEND_BACKENDACCESS_HXX_
 #include "backendaccess.hxx"
-#endif
-#ifndef CONFIGMGR_SERVICEINFOHELPER_HXX_
 #include "serviceinfohelper.hxx"
-#endif
-#ifndef CONFIGMGR_WRAPEXCEPTION_HXX
 #include "wrapexception.hxx"
-#endif
 
 
-#ifndef _COM_SUN_STAR_CONFIGURATION_CANNOTLOADCONFIGURATIONEXCEPTION_HPP_
 #include <com/sun/star/configuration/CannotLoadConfigurationException.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XBACKEND_HPP_
 #include <com/sun/star/configuration/backend/XBackend.hpp>
-#endif
-#ifndef _COM_SUN_STAR_CONFIGURATION_BACKEND_XMULTILAYERSTRATUM_HPP_
 #include <com/sun/star/configuration/backend/XMultiLayerStratum.hpp>
-#endif
 
 namespace configmgr
 {
