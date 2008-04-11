58a9db01b5d0db851c9f320b72d247eb8384ff9c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:51:03 +0000

    INTEGRATION: CWS changefileheader (1.2.2); FILE MERGED 2008/04/01 16:07:41 thb 1.2.2.3: #i85898# Stripping all external header guards 2008/04/01 13:02:56 thb 1.2.2.2: #i85898# Stripping all external header guards 2008/03/31 13:04:24 rt 1.2.2.1: #i87441# Change license header to LPGL v3.

--- xmlhelp/source/cxxhelp/provider/bufferedinputstream.hxx
+++ xmlhelp/source/cxxhelp/provider/bufferedinputstream.hxx
@@ -1,15 +1,37 @@
-#ifndef _CPPUHELPER_WEAK_HXX_
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: bufferedinputstream.hxx,v $
+ * $Revision: 1.3 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
 #include <cppuhelper/weak.hxx>
-#endif
-#ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
-#endif
-#ifndef _COM_SUN_STAR_IO_XINPUTSTREAM_HPP_
 #include <com/sun/star/io/XInputStream.hpp>
-#endif
-#ifndef _COM_SUN_STAR_IO_XSEEKABLE_HPP_
 #include <com/sun/star/io/XSeekable.hpp>
-#endif
 
 
 namespace chelp {
