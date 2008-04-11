754ddfecd70b9bbda6de4ab509302f3d384877b8
Rüdiger Timm <rt@openoffice.org>
2008-04-11 02:08:58 +0000

    INTEGRATION: CWS changefileheader (1.60.212); FILE MERGED 2008/04/01 12:50:04 thb 1.60.212.2: #i85898# Stripping all external header guards 2008/03/31 14:23:34 rt 1.60.212.1: #i87441# Change license header to LPGL v3.

--- svx/source/svdraw/svdpagv.cxx
+++ svx/source/svdraw/svdpagv.cxx
@@ -1,71 +1,46 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: svdpagv.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.60 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-11-02 12:19:31 $
+ * $RCSfile: svdpagv.cxx,v $
+ * $Revision: 1.61 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifndef _SVDPAGV_HXX
 #include <svx/svdpagv.hxx>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XWINDOW_HPP_
 #include <com/sun/star/awt/XWindow.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_POSSIZE_HPP_
 #include <com/sun/star/awt/PosSize.hpp>
-#endif
 #ifndef _UNOTOOLS_PROCESSFACTORY_HXX
 #include <comphelper/processfactory.hxx>
 #endif
-#ifndef _SVDOUTL_HXX
 #include <svx/svdoutl.hxx>
-#endif
-#ifndef _XPOLY_HXX
 #include <svx/xpoly.hxx>
-#endif
-
-#ifndef _SVDOUNO_HXX
 #include <svx/svdouno.hxx>
-#endif
-
-#ifndef _SVDPAGE_HXX
 #include <svx/svdpage.hxx>
-#endif
-
-#ifndef _SVDVIEW_HXX
 #include <svx/svdview.hxx>
-#endif
 
 #include <svx/svdedxv.hxx>
 #include <svx/svdpagv.hxx>
@@ -86,35 +61,18 @@
 #include <svx/svdoole2.hxx>
 
 // #110094#
-#ifndef _SDR_CONTACT_OBJECTCONTACTOFPAGEVIEW_HXX
 #include <svx/sdr/contact/objectcontactofpageview.hxx>
-#endif
-
-#ifndef _SVDOGRP_HXX
 #include <svx/svdogrp.hxx>
-#endif
-
-#ifndef _SDR_CONTACT_VIEWOBJECTCONTACTREDIRECTOR_HXX
 #include <svx/sdr/contact/viewobjectcontactredirector.hxx>
-#endif
-
-#ifndef _SVX_FMVIEW_HXX
 #include <svx/fmview.hxx>
-#endif
 
 // for search on vector
 #include <algorithm>
 
 using namespace ::rtl;
 using namespace ::com::sun::star;
-
-#ifndef _SDRPAGEWINDOW_HXX
 #include <svx/sdrpagewindow.hxx>
-#endif
-
-#ifndef _SDRPAINTWINDOW_HXX
 #include <sdrpaintwindow.hxx>
-#endif
 
 TYPEINIT1(SdrPageView, SfxListener);
 DBG_NAME(SdrPageView);
