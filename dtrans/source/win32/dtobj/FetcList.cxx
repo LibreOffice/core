f9e1faac53f196e73327d9e43f5f8e32756bfe33
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:41:11 +0000

    INTEGRATION: CWS changefileheader (1.17.62); FILE MERGED 2008/04/01 15:13:35 thb 1.17.62.3: #i85898# Stripping all external header guards 2008/04/01 12:28:33 thb 1.17.62.2: #i85898# Stripping all external header guards 2008/03/31 13:09:02 rt 1.17.62.1: #i87441# Change license header to LPGL v3.

--- dtrans/source/win32/dtobj/FetcList.cxx
+++ dtrans/source/win32/dtobj/FetcList.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: FetcList.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.17 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 17:01:10 $
+ * $RCSfile: FetcList.cxx,v $
+ * $Revision: 1.18 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,38 +34,17 @@
 //------------------------------------------------------------------------
 // includes
 //------------------------------------------------------------------------
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
-
-#ifndef _FETCLIST_HXX_
 #include "FetcList.hxx"
-#endif
-
-#ifndef _FETC_HXX_
 #include "Fetc.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_DATATRANSFER_XMIMECONTENTTYPEFACTORY_HPP_
 #include <com/sun/star/datatransfer/XMimeContentTypeFactory.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_DATATRANSFER_XMIMECONTENTTYPE_HPP_
 #include <com/sun/star/datatransfer/XMimeContentType.hpp>
-#endif
 
 #ifndef _DATAFORMATTRANSLATOR_HXX_
 #include "DataFmtTransl.hxx"
 #endif
-
-#ifndef _IMPLHELPER_HXX_
 #include "..\misc\ImplHelper.hxx"
-#endif
-
-#ifndef _WINCLIP_HXX_
 #include "..\misc\WinClip.hxx"
-#endif
 
 #include <algorithm>
 
