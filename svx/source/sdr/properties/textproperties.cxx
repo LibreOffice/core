10e16f1a54f2c19326e0b8a64704d10eb2ff5322
Rüdiger Timm <rt@openoffice.org>
2008-04-11 01:43:57 +0000

    INTEGRATION: CWS changefileheader (1.15.34); FILE MERGED 2008/04/01 15:51:33 thb 1.15.34.3: #i85898# Stripping all external header guards 2008/04/01 12:49:44 thb 1.15.34.2: #i85898# Stripping all external header guards 2008/03/31 14:23:15 rt 1.15.34.1: #i87441# Change license header to LPGL v3.

--- svx/source/sdr/properties/textproperties.cxx
+++ svx/source/sdr/properties/textproperties.cxx
@@ -1,114 +1,60 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: textproperties.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.16 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2008-04-02 09:54:22 $
+ * $RCSfile: textproperties.cxx,v $
+ * $Revision: 1.17 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-
-#ifndef _SDR_PROPERTIES_TEXTPROPERTIES_HXX
 #include <svx/sdr/properties/textproperties.hxx>
-#endif
-
-#ifndef _SFXITEMSET_HXX
 #include <svtools/itemset.hxx>
-#endif
-
-#ifndef _SFXSTYLE_HXX
 #include <svtools/style.hxx>
-#endif
-
-#ifndef _SFXITEMITER_HXX
 #include <svtools/itemiter.hxx>
-#endif
-
-#ifndef _SFXSMPLHINT_HXX
 #include <svtools/smplhint.hxx>
-#endif
-
-#ifndef _SVDDEF_HXX
 #include <svx/svddef.hxx>
-#endif
 
 //#ifndef _EEITEM_HXX
 //#include <svx/eeitem.hxx>
 //#endif
-
-#ifndef _SVDOTEXT_HXX
 #include <svx/svdotext.hxx>
-#endif
-
-#ifndef _SVDOUTL_HXX
 #include <svx/svdoutl.hxx>
-#endif
-
-#ifndef _SVX_WRITINGMODEITEM_HXX
 #include <svx/writingmodeitem.hxx>
-#endif
-
-#ifndef _SVDMODEL_HXX
 #include <svx/svdmodel.hxx>
-#endif
-
-#ifndef _OUTLOBJ_HXX
 #include <svx/outlobj.hxx>
-#endif
-
-#ifndef _SVX_XFLCLIT_HXX
 #include <svx/xflclit.hxx>
-#endif
 
 
-#ifndef _SVX_ADJITEM_HXX
 #include <svx/adjitem.hxx>
-#endif
-
-#ifndef _SVDETC_HXX
 #include <svx/svdetc.hxx>
-#endif
-
-#ifndef _MyEDITENG_HXX
 #include <svx/editeng.hxx>
-#endif
-
-#ifndef _SVX_FLDITEM_HXX
 #include <svx/flditem.hxx>
-#endif
 
 // #i25616#
-#ifndef _SVX_XLNWTIT_HXX //autogen
 #include <svx/xlnwtit.hxx>
-#endif
 
 //////////////////////////////////////////////////////////////////////////////
 
