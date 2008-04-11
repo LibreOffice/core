a42c3bea10bd45abcd70d279d24760bcebab002f
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:56:53 +0000

    INTEGRATION: CWS changefileheader (1.2.178); FILE MERGED 2008/04/01 16:00:52 thb 1.2.178.3: #i85898# Stripping all external header guards 2008/04/01 12:56:44 thb 1.2.178.2: #i85898# Stripping all external header guards 2008/03/28 15:40:06 rt 1.2.178.1: #i87441# Change license header to LPGL v3.

--- toolkit/inc/toolkit/controls/tkthrobber.hxx
+++ toolkit/inc/toolkit/controls/tkthrobber.hxx
@@ -1,66 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: tkthrobber.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2006-12-20 13:51:25 $
+ * $RCSfile: tkthrobber.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef TOOLKIT_CONTROLS_TKTHROBBER_HXX
 #define TOOLKIT_CONTROLS_TKTHROBBER_HXX
 
-#ifndef _TOOLKIT_AWT_UNOCONTROLMODEL_HXX_
 #include <toolkit/controls/unocontrolmodel.hxx>
-#endif
-#ifndef _TOOLKIT_HELPER_SERVICENAMES_HXX_
 #include <toolkit/helper/servicenames.hxx>
-#endif
-#ifndef _TOOLKIT_AWT_UNOCONTROLBASE_HXX_
 #include <toolkit/controls/unocontrolbase.hxx>
-#endif
-#ifndef _TOOLKIT_HELPER_MACROS_HXX_
 #include <toolkit/helper/macros.hxx>
-#endif
-
-#ifndef  _COM_SUN_STAR_GRAPHIC_XGRAPHIC_HPP_
 #include <com/sun/star/graphic/XGraphic.hpp>
-#endif
-#ifndef  _COM_SUN_STAR_AWT_XTHROBBER_HPP_
 #include <com/sun/star/awt/XThrobber.hpp>
-#endif
-
-#ifndef _COMPHELPER_UNO3_HXX_
 #include <comphelper/uno3.hxx>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE1_HXX_
 #include <cppuhelper/implbase1.hxx>
-#endif
 
 //........................................................................
 namespace toolkit
