7b849b189de7d8dc50aca656aea441394642a649
Rüdiger Timm <rt@openoffice.org>
2008-04-11 10:49:34 +0000

    INTEGRATION: CWS changefileheader (1.22.12); FILE MERGED 2008/04/01 15:42:27 thb 1.22.12.3: #i85898# Stripping all external header guards 2008/04/01 12:41:46 thb 1.22.12.2: #i85898# Stripping all external header guards 2008/03/31 07:26:11 rt 1.22.12.1: #i87441# Change license header to LPGL v3.

--- stoc/source/registry_tdprovider/tdprovider.cxx
+++ stoc/source/registry_tdprovider/tdprovider.cxx
@@ -1,67 +1,43 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: tdprovider.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.22 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-09-27 13:00:40 $
+ * $RCSfile: tdprovider.cxx,v $
+ * $Revision: 1.23 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_stoc.hxx"
-
-#ifndef _OSL_DIAGNOSE_H_
 #include <osl/diagnose.h>
-#endif
-#ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
-#endif
-
-#ifndef _UNO_DISPATCHER_H_
 #include <uno/dispatcher.h>
-#endif
-#ifndef _UNO_MAPPING_HXX_
 #include <uno/mapping.hxx>
-#endif
-
-#ifndef _CPPUHELPER_FACTORY_HXX_
 #include <cppuhelper/factory.hxx>
-#endif
-#ifndef _CPPUHELPER_COMPBASE4_HXX_
 #include <cppuhelper/compbase4.hxx>
-#endif
-#ifndef _CPPUHELPER_IMPLBASE2_HXX_
 #include <cppuhelper/implbase2.hxx>
-#endif
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
 
 #include <cppuhelper/weakref.hxx>
 
@@ -77,13 +53,8 @@
 
 #include "registry/reader.hxx"
 #include "registry/version.h"
-
-#ifndef _STOC_RDBTDP_BASE_HXX
 #include "base.hxx"
-#endif
-#ifndef _STOC_RDBTDP_TDENUMERATION_HXX
 #include "rdbtdp_tdenumeration.hxx"
-#endif
 #include "structtypedescription.hxx"
 
 #define SERVICENAME "com.sun.star.reflection.TypeDescriptionProvider"
