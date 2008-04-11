29b37f150a457870fdca8c7741339dffc4105ee6
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:47:15 +0000

    INTEGRATION: CWS changefileheader (1.2.2); FILE MERGED 2008/04/01 16:07:36 thb 1.2.2.3: #i85898# Stripping all external header guards 2008/04/01 13:02:55 thb 1.2.2.2: #i85898# Stripping all external header guards 2008/03/31 13:04:23 rt 1.2.2.1: #i87441# Change license header to LPGL v3.

--- xmlhelp/source/cxxhelp/inc/qe/DocGenerator.hxx
+++ xmlhelp/source/cxxhelp/inc/qe/DocGenerator.hxx
@@ -1,61 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: DocGenerator.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.2 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-03-25 15:17:32 $
+ * $RCSfile: DocGenerator.hxx,v $
+ * $Revision: 1.3 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _XMLSEARCH_QE_DOCGENERATOR_HXX_
 #define _XMLSEARCH_QE_DOCGENERATOR_HXX_
 
-#ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
-#endif
 #ifndef _RTL_USTRING_HXX__
 #include  <rtl/ustring.hxx>
 #endif
-#ifndef _XMLSEARCH_EXCEP_XMLSEARCHEXCEPTIONS_HXX_
 #include <excep/XmlSearchExceptions.hxx>
-#endif
-#ifndef _XMLSEARCH_QE_XMLINDEX_HXX_
 #include <qe/XmlIndex.hxx>
-#endif
-#ifndef _XMLSEARCH_QE_CONCEPTDATA_HXX_
 #include <qe/ConceptData.hxx>
-#endif
-#ifndef _XMLSEARCH_UTIL_COMPRESSORITERATOR_HXX_
 #include <util/CompressorIterator.hxx>
-#endif
-#ifndef _XMLSEARCH_UTIL_DECOMPRESSOR_HXX_
 #include <util/Decompressor.hxx>
-#endif
 
 
 namespace xmlsearch {
