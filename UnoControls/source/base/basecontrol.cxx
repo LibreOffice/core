4823b28ace5998ee5ca171be3a2f4a841e5aaac2
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:11:15 +0000

    INTEGRATION: CWS changefileheader (1.5.26); FILE MERGED 2008/04/01 16:03:01 thb 1.5.26.3: #i85898# Stripping all external header guards 2008/04/01 12:59:00 thb 1.5.26.2: #i85898# Stripping all external header guards 2008/03/31 13:07:32 rt 1.5.26.1: #i87441# Change license header to LPGL v3.

--- UnoControls/source/base/basecontrol.cxx
+++ UnoControls/source/base/basecontrol.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: basecontrol.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:08:41 $
+ * $RCSfile: basecontrol.cxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,37 +32,17 @@
 //  my own include
 //____________________________________________________________________________________________________________
 
-#ifndef _UNOCONTROLS_BASECONTROL_CTRL_HXX
 #include "basecontrol.hxx"
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of other projects
 //____________________________________________________________________________________________________________
-
-#ifndef _COM_SUN_STAR_AWT_XDEVICE_HPP_
 #include <com/sun/star/awt/XDevice.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_XDISPLAYBITMAP_HPP_
 #include <com/sun/star/awt/XDisplayBitmap.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_DEVICEINFO_HPP_
 #include <com/sun/star/awt/DeviceInfo.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_WINDOWATTRIBUTE_HPP_
 #include <com/sun/star/awt/WindowAttribute.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_POSSIZE_HPP_
 #include <com/sun/star/awt/PosSize.hpp>
-#endif
-
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of my own project
