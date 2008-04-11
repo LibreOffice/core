1a01148a743a5aacbcf26ffee357652bb364e472
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:48:05 +0000

    INTEGRATION: CWS changefileheader (1.23.86); FILE MERGED 2008/04/01 15:00:38 thb 1.23.86.3: #i85898# Stripping all external header guards 2008/04/01 10:47:46 thb 1.23.86.2: #i85898# Stripping all external header guards 2008/03/28 16:04:48 rt 1.23.86.1: #i87441# Change license header to LPGL v3.

--- basctl/source/basicide/moduldlg.hxx
+++ basctl/source/basicide/moduldlg.hxx
@@ -1,74 +1,49 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: moduldlg.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.23 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:58:40 $
+ * $RCSfile: moduldlg.hxx,v $
+ * $Revision: 1.24 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _MODULDLG_HXX
 #define _MODULDLG_HXX
 
-#ifndef _SVHEADER_HXX
 #include <svheader.hxx>
-#endif
 
 #include <bastype2.hxx>
-
-#ifndef _SV_DIALOG_HXX //autogen
 #include <vcl/dialog.hxx>
-#endif
 
 #ifndef _SV_BUTTON_HXX //autogen
 #include <vcl/button.hxx>
 #endif
-
-#ifndef _SV_FIXED_HXX //autogen
 #include <vcl/fixed.hxx>
-#endif
-
-#ifndef _SVTABBX_HXX //autogen
 #include <svtools/svtabbx.hxx>
-#endif
-
-#ifndef _SV_TABDLG_HXX //autogen
 #include <vcl/tabdlg.hxx>
-#endif
-
-#ifndef _SV_TABPAGE_HXX //autogen
 #include <vcl/tabpage.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_TASK_XINTERACTIONHANDLER_HPP_
 #include "com/sun/star/task/XInteractionHandler.hpp"
-#endif
 
 #include <vcl/tabctrl.hxx>
 #include <vcl/lstbox.hxx>
