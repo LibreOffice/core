dd237d5439eb77cba8296cb11952d278cbc519a5
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:51:24 +0000

    INTEGRATION: CWS changefileheader (1.19.16); FILE MERGED 2008/04/01 12:27:38 thb 1.19.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:56 rt 1.19.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/treemgr/roottree.cxx
+++ configmgr/source/treemgr/roottree.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: roottree.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.19 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:46:15 $
+ * $RCSfile: roottree.cxx,v $
+ * $Revision: 1.20 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,29 +34,12 @@
 
 #include "roottree.hxx"
 #include "roottreeimpl.hxx"
-
-#ifndef CONFIGMGR_VIEWACCESS_HXX_
 #include "viewaccess.hxx"
-#endif
-
-#ifndef CONFIGMGR_NODEACCESS_HXX
 #include "nodeaccess.hxx"
-#endif
-
-#ifndef CONFIGMGR_VIEWBEHAVIORFACTORY_HXX_
 #include "viewfactory.hxx"
-#endif
-
-#ifndef CONFIGMGR_CONFIGNODE_HXX_
 #include "noderef.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGCHANGEINFO_HXX_
 #include "nodechangeinfo.hxx"
-#endif
-
-#ifndef CONFIGMGR_TREECHANGELIST_HXX
 #include "treechangelist.hxx"
-#endif
 
 namespace configmgr
 {
