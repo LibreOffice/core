905990838ce36547ebd3cf53481bd4bbc82edcd8
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:33:01 +0000

    INTEGRATION: CWS changefileheader (1.15.32); FILE MERGED 2008/04/01 15:00:34 thb 1.15.32.3: #i85898# Stripping all external header guards 2008/04/01 10:47:44 thb 1.15.32.2: #i85898# Stripping all external header guards 2008/03/28 16:04:40 rt 1.15.32.1: #i87441# Change license header to LPGL v3.

--- basctl/source/basicide/basicbox.cxx
+++ basctl/source/basicide/basicbox.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: basicbox.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.15 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 16:44:34 $
+ * $RCSfile: basicbox.cxx,v $
+ * $Revision: 1.16 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -47,21 +42,13 @@
 #include <iderid.hxx>
 #include <iderdll.hxx>
 #include <bastypes.hxx>
-
-#ifndef _BASTYPE2_HXX
 #include "bastype2.hxx"
-#endif
-#ifndef _BASDOC_HXX
 #include "basdoc.hxx"
-#endif
 
 #include "localizationmgr.hxx"
 #include "managelang.hxx"
 #include "dlgresid.hrc"
-
-#ifndef _UNO_LINGU_HXX
 #include <svx/unolingu.hxx>
-#endif
 
 #include <svtools/langtab.hxx>
 
