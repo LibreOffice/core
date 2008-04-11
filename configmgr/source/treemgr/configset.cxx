1cbd74763dacda1cdcada980aa22ef066aa146ca
Rüdiger Timm <rt@openoffice.org>
2008-04-11 12:44:29 +0000

    INTEGRATION: CWS changefileheader (1.30.16); FILE MERGED 2008/04/01 15:06:56 thb 1.30.16.3: #i85898# Stripping all external header guards 2008/04/01 12:27:36 thb 1.30.16.2: #i85898# Stripping all external header guards 2008/03/31 12:22:55 rt 1.30.16.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/treemgr/configset.cxx
+++ configmgr/source/treemgr/configset.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: configset.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.30 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2007-11-23 14:40:38 $
+ * $RCSfile: configset.cxx,v $
+ * $Revision: 1.31 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,39 +32,16 @@
 #include "precompiled_configmgr.hxx"
 #include <stdio.h> // needed for Solaris 8
 #include "configset.hxx"
-
-#ifndef CONFIGMGR_CONFIGCHANGE_HXX_
 #include "nodechange.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGCHANGEIMPL_HXX_
 #include "nodechangeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGNODEIMPL_HXX_
 #include "treeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGTEMPLATE_HXX_
 #include "template.hxx"
-#endif
-#ifndef CONFIGMGR_TEMPLATEIMPL_HXX_
 #include "templateimpl.hxx"
-#endif
-#ifndef CONFIGMGR_CONFIGGROUP_HXX_
 #include "configgroup.hxx"
-#endif
-
-#ifndef _CONFIGMGR_TREE_VALUENODE_HXX
 #include "valuenode.hxx"
-#endif
-#ifndef CONFIGMGR_VALUENODEBEHAVIOR_HXX_
 #include "valuenodeimpl.hxx"
-#endif
-#ifndef CONFIGMGR_SETNODEBEHAVIOR_HXX_
 #include "setnodeimpl.hxx"
-#endif
-
-#ifndef _VOS_REFERNCE_HXX_
 #include <vos/refernce.hxx>
-#endif
 
 namespace configmgr
 {
