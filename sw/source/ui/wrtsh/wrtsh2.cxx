fa470e01d84a8b6060ab6aa8e5f14e0a52897f06
Rüdiger Timm <rt@openoffice.org>
2008-04-11 13:05:03 +0000

    INTEGRATION: CWS notes3 (1.30.6); FILE MERGED 2008/03/14 23:41:52 mod 1.30.6.3: WaE 2008/03/13 11:04:56 mba 1.30.6.2: RESYNC: (1.30-1.31); FILE MERGED 2008/02/22 21:00:47 mod 1.30.6.1: move focus stuff to SetActivePostit, fix for undo related bugs

--- sw/source/ui/wrtsh/wrtsh2.cxx
+++ sw/source/ui/wrtsh/wrtsh2.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: wrtsh2.cxx,v $
- * $Revision: 1.32 $
+ * $Revision: 1.33 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -102,8 +102,7 @@ void SwWrtShell::Insert(SwField &rFld)
 
     StartUndo(UNDO_INSERT, &aRewriter);
 
-    USHORT nType = rFld.GetTyp()->Which();
-    if( HasSelection() && nType != RES_POSTITFLD)
+    if( HasSelection() )
         DelRight();
 
     SwEditShell::Insert(rFld);
