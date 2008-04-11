844c8ecf0576738f15a25d47c85299a4f94673b3
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:45:17 +0000

    INTEGRATION: CWS changefileheader (1.3.80); FILE MERGED 2008/04/01 15:03:03 thb 1.3.80.3: #i85898# Stripping all external header guards 2008/04/01 10:49:25 thb 1.3.80.2: #i85898# Stripping all external header guards 2008/03/28 16:34:53 rt 1.3.80.1: #i87441# Change license header to LPGL v3.

--- canvas/inc/canvas/base/spritecanvasbase.hxx
+++ canvas/inc/canvas/base/spritecanvasbase.hxx
@@ -1,58 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: spritecanvasbase.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.3 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: kz $ $Date: 2006-12-13 14:36:31 $
+ * $RCSfile: spritecanvasbase.hxx,v $
+ * $Revision: 1.4 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef INCLUDED_CANVAS_SPRITECANVASBASE_HXX
 #define INCLUDED_CANVAS_SPRITECANVASBASE_HXX
 
-#ifndef _RTL_REF_HXX_
 #include <rtl/ref.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_RENDERING_XSPRITECANVAS_HPP_
 #include <com/sun/star/rendering/XSpriteCanvas.hpp>
-#endif
-#ifndef _COM_SUN_STAR_RENDERING_INTERPOLATIONMODE_HPP_
 #include <com/sun/star/rendering/InterpolationMode.hpp>
-#endif
-
-#ifndef INCLUDED_CANVAS_INTEGERBITMAPBASE_HXX
 #include <canvas/base/integerbitmapbase.hxx>
-#endif
-#ifndef INCLUDED_CANVAS_SPRITEREDRAWMANAGER_HXX
 #include <canvas/spriteredrawmanager.hxx>
-#endif
 
 
 namespace canvas
