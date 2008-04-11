67dcec0f22cfeacb72270b2f351ce003fb9d3adc
Rüdiger Timm <rt@openoffice.org>
2008-04-11 03:22:30 +0000

    INTEGRATION: CWS changefileheader (1.5.368); FILE MERGED 2008/04/01 15:52:06 thb 1.5.368.3: #i85898# Stripping all external header guards 2008/04/01 12:50:29 thb 1.5.368.2: #i85898# Stripping all external header guards 2008/03/31 14:24:23 rt 1.5.368.1: #i87441# Change license header to LPGL v3.

--- svx/workben/msview/msview.cxx
+++ svx/workben/msview/msview.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: msview.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2007-06-27 19:37:37 $
+ * $RCSfile: msview.cxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -40,10 +35,7 @@
 #include <map>
 #include <algorithm>
 #include <boost/shared_ptr.hpp>
-
-#ifndef _SOT_STORAGE_HXX
 #include <sot/storage.hxx>
-#endif
 #ifndef _SVTOOLS_HRC
 #include <svtools/svtools.hrc>
 #endif
@@ -63,18 +55,12 @@
 #include <vcl/unohelp2.hxx>
 #include <svtools/svtreebx.hxx>
 #include <svtools/svmedit.hxx>
-
-#ifndef _FILEDLGHELPER_HXX
 #include <sfx2/filedlghelper.hxx>
-#endif
 
 #include <toolkit/unohlp.hxx>
 
 #include <tools/stream.hxx>
-
-#ifndef _TOOLS_RESMGR_HXX
 #include <tools/resmgr.hxx>
-#endif
 
 #include <comphelper/processfactory.hxx>
 #include <cppuhelper/servicefactory.hxx>
@@ -89,10 +75,7 @@
 #include <com/sun/star/awt/XToolkit.hpp>
 #include <com/sun/star/awt/WindowDescriptor.hpp>
 #include <com/sun/star/awt/WindowAttribute.hpp>
-
-#ifndef _MSDFFDEF_HXX
 #include <svx/msdffdef.hxx>
-#endif
 
 #include <unotools/localfilehelper.hxx>
 
