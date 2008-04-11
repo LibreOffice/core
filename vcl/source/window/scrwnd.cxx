6b245d4d7272c3c6ad5a24e3fc7786599e2ed8be
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:12:15 +0000

    INTEGRATION: CWS changefileheader (1.13.222); FILE MERGED 2008/04/01 16:05:41 thb 1.13.222.3: #i85898# Stripping all external header guards 2008/04/01 13:01:48 thb 1.13.222.2: #i85898# Stripping all external header guards 2008/03/28 15:45:02 rt 1.13.222.1: #i87441# Change license header to LPGL v3.

--- vcl/source/window/scrwnd.cxx
+++ vcl/source/window/scrwnd.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: scrwnd.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2007-07-24 10:21:21 $
+ * $RCSfile: scrwnd.cxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -38,18 +33,13 @@
 
 #include <math.h>
 #include <limits.h>
-
-#ifndef _TOOLS_TIME_HXX
 #include <tools/time.hxx>
-#endif
 #include <tools/debug.hxx>
 
 #ifndef _SV_SVIDS_HRC
 #include <vcl/svids.hrc>
 #endif
-#ifndef _SV_SVDATA_HXX
 #include <vcl/svdata.hxx>
-#endif
 #ifndef _VCL_TIMER_HXX
 #include <vcl/timer.hxx>
 #endif
