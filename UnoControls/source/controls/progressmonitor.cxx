1cb620b51cdb9a7eb9dbe5d7150fe2f9ac57b732
Rüdiger Timm <rt@openoffice.org>
2008-04-11 06:16:08 +0000

    INTEGRATION: CWS changefileheader (1.5.26); FILE MERGED 2008/04/01 16:03:03 thb 1.5.26.3: #i85898# Stripping all external header guards 2008/04/01 12:59:01 thb 1.5.26.2: #i85898# Stripping all external header guards 2008/03/31 13:07:32 rt 1.5.26.1: #i87441# Change license header to LPGL v3.

--- UnoControls/source/controls/progressmonitor.cxx
+++ UnoControls/source/controls/progressmonitor.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: progressmonitor.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 23:09:47 $
+ * $RCSfile: progressmonitor.cxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,53 +32,24 @@
 //  my own includes
 //____________________________________________________________________________________________________________
 
-#ifndef _UNOCONTROLS_PROGRESSMONITOR_CTRL_HXX
 #include "progressmonitor.hxx"
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of other projects
 //____________________________________________________________________________________________________________
-
-#ifndef _COM_SUN_STAR_AWT_GRADIENTSTYLE_HPP_
 #include <com/sun/star/awt/GradientStyle.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_RASTEROPERATION_HPP_
 #include <com/sun/star/awt/RasterOperation.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_GRADIENT_HPP_
 #include <com/sun/star/awt/Gradient.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_XGRAPHICS_HPP_
 #include <com/sun/star/awt/XGraphics.hpp>
-#endif
-
-#ifndef _COM_SUN_STAR_AWT_POSSIZE_HPP_
 #include <com/sun/star/awt/PosSize.hpp>
-#endif
-
-#ifndef _CPPUHELPER_TYPEPROVIDER_HXX_
 #include <cppuhelper/typeprovider.hxx>
-#endif
-
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-
-#ifndef _SOLAR_H
 #include <tools/solar.h>
-#endif
 
 //____________________________________________________________________________________________________________
 //  includes of my project
 //____________________________________________________________________________________________________________
-
-#ifndef _UNOCONTROLS_PROGRESSBAR_CTRL_HXX
 #include "progressbar.hxx"
-#endif
 
 //____________________________________________________________________________________________________________
 //  namespace
