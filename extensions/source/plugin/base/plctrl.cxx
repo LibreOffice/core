b2db032ed00208fbd9ab36941edc66a257ba993a
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:23:23 +0000

    INTEGRATION: CWS changefileheader (1.6.48); FILE MERGED 2008/04/01 12:29:46 thb 1.6.48.2: #i85898# Stripping all external header guards 2008/03/31 12:31:35 rt 1.6.48.1: #i87441# Change license header to LPGL v3.

--- extensions/source/plugin/base/plctrl.cxx
+++ extensions/source/plugin/base/plctrl.cxx
@@ -1,58 +1,41 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: plctrl.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.6 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:51:05 $
+ * $RCSfile: plctrl.cxx,v $
+ * $Revision: 1.7 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_extensions.hxx"
-#ifndef _COM_SUN_STAR_AWT_XADJUSTMENTLISTENER_HPP_
 #include <com/sun/star/awt/XAdjustmentListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XACTIONLISTENER_HPP_
 #include <com/sun/star/awt/XActionListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XTEXTLISTENER_HPP_
 #include <com/sun/star/awt/XTextListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XSPINLISTENER_HPP_
 #include <com/sun/star/awt/XSpinListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XITEMLISTENER_HPP_
 #include <com/sun/star/awt/XItemListener.hpp>
-#endif
-#ifndef _COM_SUN_STAR_AWT_XVCLCONTAINERLISTENER_HPP_
 #include <com/sun/star/awt/XVclContainerListener.hpp>
-#endif
 
 #include <plugin/plctrl.hxx>
 #include <vcl/syschild.hxx>
