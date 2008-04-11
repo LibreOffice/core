2fef17d2703043ca69e519806ea750e6895b5cf4
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:39:05 +0000

    INTEGRATION: CWS changefileheader (1.20.80); FILE MERGED 2008/04/01 15:39:06 thb 1.20.80.3: #i85898# Stripping all external header guards 2008/04/01 12:40:46 thb 1.20.80.2: #i85898# Stripping all external header guards 2008/03/31 13:38:16 rt 1.20.80.1: #i87441# Change license header to LPGL v3.

--- sfx2/source/dialog/macropg.cxx
+++ sfx2/source/dialog/macropg.cxx
@@ -1,44 +1,36 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: macropg.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.20 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2008-01-07 09:03:21 $
+ * $RCSfile: macropg.cxx,v $
+ * $Revision: 1.21 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_sfx2.hxx"
-
-#ifndef _BASMGR_HXX //autogen
 #include <basic/basmgr.hxx>
-#endif
 #ifndef GCC
 #endif
 
@@ -61,13 +53,8 @@
 #include <sfx2/macrconf.hxx>
 #include <sfx2/sfxdefs.hxx>
 #include "helpid.hrc"
-
-#ifndef _HEADERTABLISTBOX_HXX
 #include "headertablistbox.hxx"
-#endif
-#ifndef _MACROPG_IMPL_HXX
 #include "macropg_impl.hxx"
-#endif
 
 
 _SfxMacroTabPage_Impl::_SfxMacroTabPage_Impl( void ) :
