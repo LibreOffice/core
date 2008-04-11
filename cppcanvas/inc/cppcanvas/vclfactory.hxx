406b6340bdf03410418f9f9b3ac5ba553d88aa72
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:07:13 +0000

    INTEGRATION: CWS changefileheader (1.5.80); FILE MERGED 2008/04/01 12:27:50 thb 1.5.80.2: #i85898# Stripping all external header guards 2008/03/31 13:07:07 rt 1.5.80.1: #i87441# Change license header to LPGL v3.

--- cppcanvas/inc/cppcanvas/vclfactory.hxx
+++ cppcanvas/inc/cppcanvas/vclfactory.hxx
@@ -1,65 +1,44 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: vclfactory.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.5 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:16:10 $
+ * $RCSfile: vclfactory.hxx,v $
+ * $Revision: 1.6 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef _CPPCANVAS_VCLFACTORY_HXX
 #define _CPPCANVAS_VCLFACTORY_HXX
 
-#ifndef _CPPCANVAS_CANVAS_HXX
 #include <cppcanvas/canvas.hxx>
-#endif
-#ifndef _CPPCANVAS_BITMAPCANVAS_HXX
 #include <cppcanvas/bitmapcanvas.hxx>
-#endif
-#ifndef _CPPCANVAS_SPRITECANVAS_HXX
 #include <cppcanvas/spritecanvas.hxx>
-#endif
-#ifndef _CPPCANVAS_POLYPOLYGON_HXX
 #include <cppcanvas/polypolygon.hxx>
-#endif
-#ifndef _CPPCANVAS_BITMAP_HXX
 #include <cppcanvas/bitmap.hxx>
-#endif
-#ifndef _CPPCANVAS_RENDERER_HXX
 #include <cppcanvas/renderer.hxx>
-#endif
-#ifndef _CPPCANVAS_TEXT_HXX
 #include <cppcanvas/text.hxx>
-#endif
-#ifndef _CPPCANVAS_SPRITE_HXX
 #include <cppcanvas/sprite.hxx>
-#endif
 
 
 class Window;
