74e829fc91c977c879ed3ee381e67083a827d1bb
Rüdiger Timm <rt@openoffice.org>
2008-04-11 08:34:48 +0000

    INTEGRATION: CWS changefileheader (1.13.48); FILE MERGED 2008/04/01 15:15:02 thb 1.13.48.3: #i85898# Stripping all external header guards 2008/04/01 12:29:41 thb 1.13.48.2: #i85898# Stripping all external header guards 2008/03/31 12:31:23 rt 1.13.48.1: #i87441# Change license header to LPGL v3.

--- extensions/source/bibliography/bibview.cxx
+++ extensions/source/bibliography/bibview.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: bibview.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.13 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: ihi $ $Date: 2008-01-14 14:38:34 $
+ * $RCSfile: bibview.cxx,v $
+ * $Revision: 1.14 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -39,52 +34,23 @@
 #ifndef BIB_HRC
 #include "bib.hrc"
 #endif
-#ifndef ADDRCONT_HXX
 #include "bibcont.hxx"
-#endif
-#ifndef ADRBEAM_HXX
 #include "bibbeam.hxx"
-#endif
-#ifndef BIBMOD_HXX
 #include "bibmod.hxx"
-#endif
-#ifndef _BIB_GENERAL_HXX
 #include "general.hxx"
-#endif
-#ifndef _BIB_VIEW_HXX
 #include "bibview.hxx"
-#endif
-#ifndef _BIB_DATMAN_HXX
 #include "datman.hxx"
-#endif
-#ifndef ADRRESID_HXX
 #include "bibresid.hxx"
-#endif
-#ifndef BIBMOD_HXX
 #include "bibmod.hxx"
-#endif
 #include "sections.hrc"
-#ifndef _BIBCONFIG_HXX
 #include "bibconfig.hxx"
-#endif
 
 
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
-#ifndef _COM_SUN_STAR_SDBC_XRESULTSETUPDATE_HPP_
 #include <com/sun/star/sdbc/XResultSetUpdate.hpp>
-#endif
-#ifndef _COM_SUN_STAR_FORM_XLOADABLE_HPP_
 #include <com/sun/star/form/XLoadable.hpp>
-#endif
-#ifndef _SV_MSGBOX_HXX
 #include <vcl/msgbox.hxx>
-#endif
-
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::form;
