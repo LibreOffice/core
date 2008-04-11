6b28697b78f7c97a2116005843c02da4bbcd90e8
Rüdiger Timm <rt@openoffice.org>
2008-04-11 07:45:22 +0000

    INTEGRATION: CWS changefileheader (1.8.134); FILE MERGED 2008/04/01 15:16:38 thb 1.8.134.3: #i85898# Stripping all external header guards 2008/04/01 12:30:30 thb 1.8.134.2: #i85898# Stripping all external header guards 2008/03/31 13:11:41 rt 1.8.134.1: #i87441# Change license header to LPGL v3.

--- forms/source/richtext/richtextcontrol.cxx
+++ forms/source/richtext/richtextcontrol.cxx
@@ -1,101 +1,63 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: richtextcontrol.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.8 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 23:59:56 $
+ * $RCSfile: richtextcontrol.cxx,v $
+ * $Revision: 1.9 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_forms.hxx"
-
-#ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTCONTROL_HXX
 #include "richtextcontrol.hxx"
-#endif
-#ifndef FRM_MODULE_HXX
 #include "frm_module.hxx"
-#endif
 #ifndef _FRM_PROPERTY_HRC_
 #include "property.hrc"
 #endif
-#ifndef _FRM_SERVICES_HXX_
 #include "services.hxx"
-#endif
 
 #ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTMODEL_CXX
 #include "richtextmodel.hxx"
 #endif
-#ifndef FORMS_SOURCE_RICHTEXT_RICHTEXTVCLCONTROL_HXX
 #include "richtextvclcontrol.hxx"
-#endif
-#ifndef FORMS_SOURCE_RICHTEXT_CLIPBOARDDISPATCHER_HXX
 #include "clipboarddispatcher.hxx"
-#endif
-#ifndef FORMS_SOURCE_RICHTEXT_PARAMETRIZEDATTRIBUTEDISPATCHER_HXX
 #include "parametrizedattributedispatcher.hxx"
-#endif
-#ifndef FORMS_SOURCE_RICHTEXT_SPECIALDISPATCHERS_HXX
 #include "specialdispatchers.hxx"
-#endif
 
 /** === begin UNO includes === **/
-#ifndef _COM_SUN_STAR_AWT_POSSIZE_HPP_
 #include <com/sun/star/awt/PosSize.hpp>
-#endif
 /** === end UNO includes === **/
-
-#ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
-#endif
-
-#ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
-#endif
 
 #ifndef _SVX_SVXIDS_HRC
 #include <svx/svxids.hrc>
 #endif
-#ifndef _MyEDITVIEW_HXX
 #include <svx/editview.hxx>
-#endif
-
-#ifndef _SFXITEMSET_HXX
 #include <svtools/itemset.hxx>
-#endif
-#ifndef _SFXITEMPOOL_HXX
 #include <svtools/itempool.hxx>
-#endif
-
-#ifndef _SFXMSGPOOL_HXX
 #include <sfx2/msgpool.hxx>
-#endif
 
 //--------------------------------------------------------------------------
 extern "C" void SAL_CALL createRegistryInfo_ORichTextControl()
