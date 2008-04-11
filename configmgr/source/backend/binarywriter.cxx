98668f763a5fc69ee4bdb64f5fe0626977223893
Rüdiger Timm <rt@openoffice.org>
2008-04-11 11:22:17 +0000

    INTEGRATION: CWS changefileheader (1.9.72); FILE MERGED 2008/04/01 15:06:36 thb 1.9.72.3: #i85898# Stripping all external header guards 2008/04/01 12:27:16 thb 1.9.72.2: #i85898# Stripping all external header guards 2008/03/31 12:22:39 rt 1.9.72.1: #i87441# Change license header to LPGL v3.

--- configmgr/source/backend/binarywriter.cxx
+++ configmgr/source/backend/binarywriter.cxx
@@ -1,35 +1,30 @@
 /*************************************************************************
 *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: binarywriter.cxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.9 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: obo $ $Date: 2006-09-16 15:03:37 $
+ * $RCSfile: binarywriter.cxx,v $
+ * $Revision: 1.10 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
 ************************************************************************/
 
@@ -37,47 +32,18 @@
 #include "precompiled_configmgr.hxx"
 
 #include "binarywriter.hxx"
-
-#ifndef _CONFIGMGR_TREE_VALUENODE_HXX
 #include "valuenode.hxx"
-#endif
-#ifndef _CONFIGMGR_FILEHELPER_HXX_
 #include "filehelper.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_TYPE_HXX_
 #include <com/sun/star/uno/Type.hxx>
-#endif
-#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
 #include <com/sun/star/uno/Any.hxx>
-#endif
-
-#ifndef _CONFIGMGR_OSLSTREAM_HXX_
 #include "oslstream.hxx"
-#endif
-
-#ifndef _COM_SUN_STAR_IO_IOEXCEPTION_HPP_
 #include <com/sun/star/io/IOException.hpp>
-#endif
-#ifndef _COM_SUN_STAR_IO_XOUTPUTSTREAM_HPP_
 #include <com/sun/star/io/XOutputStream.hpp>
-#endif
-#ifndef _COM_SUN_STAR_IO_XACTIVEDATASOURCE_HPP_
 #include <com/sun/star/io/XActiveDataSource.hpp>
-#endif
-#ifndef _COM_SUN_STAR_IO_XDATAOUTPUTSTREAM_HPP_
 #include <com/sun/star/io/XDataOutputStream.hpp>
-#endif
-
-#ifndef CONFIGMGR_TYPECONVERTER_HXX
 #include "typeconverter.hxx"
-#endif
-#ifndef CONFIGMGR_BINARYTYPE_HXX
 #include "binarytype.hxx"
-#endif
-#ifndef CONFIGMGR_SIMPLETYPEHELPER_HXX
 #include "simpletypehelper.hxx"
-#endif
 
 #define ASCII(x) rtl::OUString::createFromAscii(x)
 namespace configmgr
