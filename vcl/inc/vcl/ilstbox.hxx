fb3786e3215056e20ab3d974c2a7a53e28eff7a4
Vladimir Glazounov <vg@openoffice.org>
2008-04-15 12:22:33 +0000

    INTEGRATION: CWS aqua11y01 (1.2.80); FILE MERGED 2008/01/14 21:16:37 obr 1.2.80.1: #i82877# use dedicated subclass of vclxaccessiblecomponent for the listbox/combobox floater

--- vcl/inc/vcl/ilstbox.hxx
+++ vcl/inc/vcl/ilstbox.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ilstbox.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -497,7 +497,6 @@ private:
 
 protected:
     long            PreNotify( NotifyEvent& rNEvt );
-    virtual ::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > CreateAccessible();
 
 public:
                     ImplListBoxFloatingWindow( Window* pParent );
