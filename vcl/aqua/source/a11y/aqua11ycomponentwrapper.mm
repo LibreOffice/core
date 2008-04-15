c65d43fd6214ac9f00b40c90b39d699ac73b2993
Vladimir Glazounov <vg@openoffice.org>
2008-04-15 12:10:04 +0000

    INTEGRATION: CWS aqua11y01 (1.1.2); FILE ADDED 2008/04/15 07:58:34 rt 1.1.2.3: Update license header to LGPL 3. 2008/03/12 12:27:41 fne 1.1.2.2: #i82877# make toolbox respond to focus changes through a11y, fix for comboboxes 2008/02/06 07:55:46 fne 1.1.2.1: #i82877# renamed cxx to mm

---
+++ vcl/aqua/source/a11y/aqua11ycomponentwrapper.mm
@@ -0,0 +1,115 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: aqua11ycomponentwrapper.mm,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_vcl.hxx"
+
+#include "aqua11ycomponentwrapper.h"
+#include "aqua11yrolehelper.h"
+#include <com/sun/star/accessibility/AccessibleRole.hpp>
+
+using namespace ::com::sun::star::accessibility;
+using namespace ::com::sun::star::awt;
+using namespace ::com::sun::star::uno;
+
+// Wrapper for XAccessibleComponent and XAccessibleExtendedComponent
+
+@implementation AquaA11yComponentWrapper : NSObject
+
++(id)sizeAttributeForElement:(AquaA11yWrapper *)wrapper {
+    Size size = [ wrapper accessibleComponent ] -> getSize();
+    NSSize nsSize = NSMakeSize ( (float) size.Width, (float) size.Height );
+    return [ NSValue valueWithSize: nsSize ];
+}
+
+// TODO: should be merged with AquaSalFrame::VCLToCocoa... to a general helper method
++(id)positionAttributeForElement:(AquaA11yWrapper *)wrapper {
+    // VCL coordinates are in upper-left-notation, Cocoa likes it the Cartesian way (lower-left)
+    NSRect screenRect = [ [ NSScreen mainScreen ] frame ];
+    Size size = [ wrapper accessibleComponent ] -> getSize();
+    Point location = [ wrapper accessibleComponent ] -> getLocationOnScreen();
+    NSPoint nsPoint = NSMakePoint ( (float) location.X, (float) ( screenRect.size.height - size.Height - location.Y ) );
+    return [ NSValue valueWithPoint: nsPoint ];
+}
+
++(id)descriptionAttributeForElement:(AquaA11yWrapper *)wrapper {
+    if ( [ wrapper accessibleExtendedComponent ] != nil ) {
+        return CreateNSString ( [ wrapper accessibleExtendedComponent ] -> getToolTipText() );
+    } else {
+        return nil;
+    }
+}
+
++(void)addAttributeNamesTo:(NSMutableArray *)attributeNames {
+    NSAutoreleasePool * pool = [ [ NSAutoreleasePool alloc ] init ];
+    [ attributeNames addObjectsFromArray: [ NSArray arrayWithObjects: 
+            NSAccessibilitySizeAttribute, 
+            NSAccessibilityPositionAttribute, 
+            NSAccessibilityFocusedAttribute, 
+            NSAccessibilityEnabledAttribute, 
+            nil ] ];
+    [ pool release ];
+}
+
++(MacOSBOOL)isAttributeSettable:(NSString *)attribute forElement:(AquaA11yWrapper *)wrapper {
+    MacOSBOOL isSettable = NO;
+    NSAutoreleasePool * pool = [ [ NSAutoreleasePool alloc ] init ];
+    if ( [ attribute isEqualToString: NSAccessibilityFocusedAttribute ] 
+      && ! [ [ AquaA11yRoleHelper getNativeRoleFrom: [ wrapper accessibleContext ] ] isEqualToString: NSAccessibilityScrollBarRole ] 
+      && ! [ [ AquaA11yRoleHelper getNativeRoleFrom: [ wrapper accessibleContext ] ] isEqualToString: NSAccessibilityStaticTextRole ] ) {
+        isSettable = YES;
+    }
+    [ pool release ];
+    return isSettable;
+}
+
++(void)setFocusedAttributeForElement:(AquaA11yWrapper *)wrapper to:(id)value {
+    if ( [ value boolValue ] == YES ) {
+        if ( [ wrapper accessibleContext ] -> getAccessibleRole() == AccessibleRole::COMBO_BOX ) {
+            // special treatment for comboboxes: find the included text area and set focus to it
+            if ( [ wrapper accessibleContext ] -> getAccessibleChildCount() > 0 ) {
+                for ( int i = 0; i < [ wrapper accessibleContext ] -> getAccessibleChildCount(); i++ ) {
+                    Reference < XAccessible > rxAccessibleChild = [ wrapper accessibleContext ] -> getAccessibleChild ( i );
+                    if ( rxAccessibleChild.is() && rxAccessibleChild -> getAccessibleContext().is() && rxAccessibleChild -> getAccessibleContext() -> getAccessibleRole() == AccessibleRole::TEXT ) {
+                        Reference < XAccessibleComponent > rxAccessibleComponent = Reference < XAccessibleComponent > ( rxAccessibleChild -> getAccessibleContext(), UNO_QUERY );
+                        if ( rxAccessibleComponent.is() ) {
+                            rxAccessibleComponent -> grabFocus();
+                        }
+                    }
+                }
+            }
+        } else {
+            [ wrapper accessibleComponent ] -> grabFocus();
+        }
+    }
+}
+
+@end
