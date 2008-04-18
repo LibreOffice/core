71049acb09243e6c5be782eb0a2a0331dd1b7673
Rüdiger Timm <rt@openoffice.org>
2008-04-18 12:51:33 +0000

    INTEGRATION: CWS xmlfilter04 (1.1.2); FILE ADDED 2008/04/18 11:28:28 rt 1.1.2.8: Change license header to LGPL v3. 2008/03/12 14:21:37 dr 1.1.2.7: more code finetuning 2008/03/11 14:32:53 dr 1.1.2.6: add spPr and txPr to various objects 2008/03/07 13:28:41 dr 1.1.2.5: axis conversion 2008/03/05 11:04:59 dr 1.1.2.4: more source data import 2008/03/03 14:03:31 dr 1.1.2.3: more conversion 2008/02/29 15:27:13 dr 1.1.2.2: basic converter stuff 2008/02/27 11:08:10 dr 1.1.2.1: first steps: chart API conversion

---
+++ oox/inc/oox/drawingml/chart/chartconverter.hxx
@@ -0,0 +1,91 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: chartconverter.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef OOX_DRAWINGML_CHART_CHARTCONVERTER_HXX
+#define OOX_DRAWINGML_CHART_CHARTCONVERTER_HXX
+
+#include <com/sun/star/uno/Reference.hxx>
+
+namespace com { namespace sun { namespace star {
+    namespace chart2 { class XChartDocument; }
+    namespace chart2 { namespace data { class XDataProvider; } }
+    namespace chart2 { namespace data { class XDataSequence; } }
+} } }
+
+namespace oox { namespace core {
+    class FilterBase;
+} }
+
+namespace oox {
+namespace drawingml {
+namespace chart {
+
+// ============================================================================
+
+struct ChartSpaceModel;
+
+class ChartConverter
+{
+public:
+    explicit            ChartConverter();
+    virtual             ~ChartConverter();
+
+    /** Converts the passed OOXML chart model to the passed chart2 document. */
+    void                convertModelToDocument(
+                            ::oox::core::FilterBase& rFilter,
+                            ChartSpaceModel& rModel,
+                            const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XChartDocument >& rxChartDoc );
+
+    /** Creates an internal data provider. Derived classes may override this
+        function to create an external data provider. */
+    virtual void        createDataProvider(
+                            const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::XChartDocument >& rxChartDoc );
+
+    /** Creates a data sequence from a formula. Dummy implementation. Derived
+        classes have to override this function to actually parse the formula. */
+    virtual ::com::sun::star::uno::Reference< ::com::sun::star::chart2::data::XDataSequence >
+                        createDataSequence(
+                            const ::com::sun::star::uno::Reference< ::com::sun::star::chart2::data::XDataProvider >& rxDataProvider,
+                            const ::rtl::OUString& rFormula );
+
+private:
+                        ChartConverter( const ChartConverter& );
+    ChartConverter&     operator=( const ChartConverter& );
+};
+
+// ============================================================================
+
+} // namespace chart
+} // namespace drawingml
+} // namespace oox
+
+#endif
+
