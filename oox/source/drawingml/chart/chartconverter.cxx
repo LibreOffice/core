789a9b6d62dc89028134ccaf3b8086d4269f3d80
Rüdiger Timm <rt@openoffice.org>
2008-04-18 13:20:51 +0000

    INTEGRATION: CWS xmlfilter04 (1.1.2); FILE ADDED 2008/04/18 12:10:37 rt 1.1.2.12: Change license header to LGPL v3. 2008/03/12 14:22:30 dr 1.1.2.11: more code finetuning 2008/03/11 14:33:12 dr 1.1.2.10: add spPr and txPr to various objects 2008/03/11 09:50:33 dr 1.1.2.9: import manual chart title text 2008/03/10 16:11:02 dr 1.1.2.8: move legend to title files 2008/03/10 12:56:14 dr 1.1.2.7: 3D settings 2008/03/07 13:30:22 dr 1.1.2.6: axis conversion 2008/03/05 11:05:05 dr 1.1.2.5: more source data import 2008/03/03 14:04:30 dr 1.1.2.4: more conversion 2008/02/29 15:29:52 dr 1.1.2.3: basic converter stuff 2008/02/29 09:23:29 dr 1.1.2.2: title 2008/02/27 11:13:02 dr 1.1.2.1: first steps: chart API conversion

---
+++ oox/source/drawingml/chart/chartconverter.cxx
@@ -0,0 +1,92 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: chartconverter.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "oox/drawingml/chart/chartconverter.hxx"
+#include <com/sun/star/chart2/XChartDocument.hpp>
+#include "oox/drawingml/chart/chartspaceconverter.hxx"
+
+using ::rtl::OUString;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Exception;
+using ::com::sun::star::chart2::XChartDocument;
+using ::com::sun::star::chart2::data::XDataProvider;
+using ::com::sun::star::chart2::data::XDataSequence;
+using ::oox::core::FilterBase;
+
+namespace oox {
+namespace drawingml {
+namespace chart {
+
+// ============================================================================
+
+ChartConverter::ChartConverter()
+{
+}
+
+ChartConverter::~ChartConverter()
+{
+}
+
+void ChartConverter::convertModelToDocument( FilterBase& rFilter,
+        ChartSpaceModel& rModel, const Reference< XChartDocument >& rxChartDoc )
+{
+    OSL_ENSURE( rxChartDoc.is(), "ChartConverter::convertModelToDocument - missing chart document" );
+    if( rxChartDoc.is() )
+    {
+        ConverterRoot aConvBase( rFilter, *this, rxChartDoc );
+        ChartSpaceConverter aSpaceConv( aConvBase, rModel );
+        aSpaceConv.convertModelToDocument();
+    }
+}
+
+void ChartConverter::createDataProvider( const Reference< XChartDocument >& rxChartDoc )
+{
+    try
+    {
+        if( !rxChartDoc->hasInternalDataProvider() )
+            rxChartDoc->createInternalDataProvider( sal_False );
+    }
+    catch( Exception& )
+    {
+    }
+}
+
+Reference< XDataSequence > ChartConverter::createDataSequence( const Reference< XDataProvider >&, const OUString& )
+{
+    return 0;
+}
+
+// ============================================================================
+
+} // namespace chart
+} // namespace drawingml
+} // namespace oox
+
