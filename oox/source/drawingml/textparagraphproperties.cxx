9b5fbf83137e056039af454e6bcbfdd7d31eb7f6
Rüdiger Timm <rt@openoffice.org>
2008-04-18 13:18:46 +0000

    INTEGRATION: CWS xmlfilter04 (1.2.12); FILE MERGED 2008/02/27 14:40:08 dr 1.2.12.1: add setters to Color class, add gamma to a:scrgbClr

--- oox/source/drawingml/textparagraphproperties.cxx
+++ oox/source/drawingml/textparagraphproperties.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: textparagraphproperties.cxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -357,15 +357,12 @@ void TextParagraphProperties::apply( const TextParagraphPropertiesPtr& rSourceTe
 void TextParagraphProperties::pushToPropSet( const ::oox::core::XmlFilterBase& rFilterBase,
     const Reference < XPropertySet >& xPropSet, PropertyMap& rioBulletMap, sal_Bool bApplyBulletMap ) const
 {
-    PropertySet aPropSet( xPropSet );
-    Sequence< OUString > aNames;
-    Sequence< Any > aValues;
-
 //       maTextParagraphPropertyMap.dump_debug("TextParagraph paragraph props");
-    maTextParagraphPropertyMap.makeSequence( aNames, aValues );
-    aPropSet.setProperties( aNames, aValues );
 
-    maTextCharacterPropertiesPtr->pushToPropSet( rFilterBase, aPropSet.getXPropertySet() );
+    PropertySet aPropSet( xPropSet );
+    aPropSet.setProperties( maTextParagraphPropertyMap );
+
+    maTextCharacterPropertiesPtr->pushToPropSet( rFilterBase, xPropSet );
     maBulletList.pushToPropMap( rFilterBase, rioBulletMap );
 
     if ( maParaTopMargin.bHasValue )
