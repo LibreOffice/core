d306e642286af7ce5f11e1d67d86b6575a7a0dc9
Rüdiger Timm <rt@openoffice.org>
2008-04-18 12:48:01 +0000

    INTEGRATION: CWS xmlfilter04 (1.2.12); FILE MERGED 2008/03/14 13:53:52 dr 1.2.12.2: never call methods of parent context handler 2008/02/21 12:41:52 hbrinkm 1.2.12.1: joined changes from xmlfilter03

--- oox/inc/oox/drawingml/shapepropertiescontext.hxx
+++ oox/inc/oox/drawingml/shapepropertiescontext.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: shapepropertiescontext.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -41,7 +41,6 @@ class ShapePropertiesContext : public ::oox::core::ContextHandler
 public:
     ShapePropertiesContext( ::oox::core::ContextHandler& rParent, Shape& rShape );
 
-    virtual void SAL_CALL endFastElement( ::sal_Int32 Element ) throw (::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeException);
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext( ::sal_Int32 Element, const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastAttributeList >& Attribs ) throw (::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeException);
 
 protected:
