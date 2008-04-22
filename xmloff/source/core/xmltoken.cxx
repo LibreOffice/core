c6fdc3472f419694575550cc0363ea3661a231f7
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 14:30:45 +0000

    INTEGRATION: CWS hr50 (1.116.10); FILE MERGED 2008/04/09 11:05:53 hr 1.116.10.1: #i86574#: fix warning (gcc-4.2.3)

--- xmloff/source/core/xmltoken.cxx
+++ xmloff/source/core/xmltoken.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmltoken.cxx,v $
- * $Revision: 1.125 $
+ * $Revision: 1.126 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -3085,8 +3085,9 @@ namespace xmloff { namespace token {
     {
         if (nRescheduleCount > 0)
             --nRescheduleCount;
-        else
+        else {
             DBG_ERROR("RescheduleCount not increased");
+        }
     }
 
 }
