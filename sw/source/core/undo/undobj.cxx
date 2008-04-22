930a65047fc6b5998afa4492fc7d8654ea4a5381
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 14:02:06 +0000

    INTEGRATION: CWS hr50 (1.23.36); FILE MERGED 2008/04/11 15:54:02 hr 1.23.36.1: #i86574#: fix warning (gcc-4.2.3)

--- sw/source/core/undo/undobj.cxx
+++ sw/source/core/undo/undobj.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: undobj.cxx,v $
- * $Revision: 1.26 $
+ * $Revision: 1.27 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -435,8 +435,9 @@ void SwUndoSaveCntnt::MoveFromUndoNds( SwDoc& rDoc, ULONG nNodeIdx,
         rNds._MoveNodes( aRg, rDoc.GetNodes(), rInsPos.nNode, 0 == pEndNdIdx );
 
     }
-    else
+    else {
         ASSERT( FALSE, "was ist es denn nun?" );
+    }
 
     rDoc.DoUndo( bUndo );
 }
