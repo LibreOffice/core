1d564d54f4d0cd1ccbe7f80d86a31e443b204327
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 14:43:12 +0000

    INTEGRATION: CWS hr50 (1.2.38); FILE MERGED 2008/04/16 10:27:38 hr 1.2.38.1: #i86797#: code is not yet 64 bit clean, disable building

--- soldep/bootstrp/makefile.mk
+++ soldep/bootstrp/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -40,6 +40,12 @@ ENABLE_EXCEPTIONS=true
 
 .INCLUDE :  $(PRJ)$/util$/perl.mk
 .INCLUDE :  settings.mk
+
+# fixme, code is not yet 64 bit clean
+.IF "$(OS)$(CPU)"=="LINUXX"
+all:
+    @echo nothing to do
+.ENDIF
 
 # --- Files --------------------------------------------------------
 
