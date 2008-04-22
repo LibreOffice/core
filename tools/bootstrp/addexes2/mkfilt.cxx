19ddb5877fce5a1e114641d4defb858a7b37d4e1
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 14:08:42 +0000

    INTEGRATION: CWS hr50 (1.9.114); FILE MERGED 2008/03/03 13:41:14 hr 1.9.114.1: #i86574#: fix warning (gcc-4.2.3)

--- tools/bootstrp/addexes2/mkfilt.cxx
+++ tools/bootstrp/addexes2/mkfilt.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: mkfilt.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -192,8 +192,9 @@ void MkFilter::Filter()
             if ( bInTnrList )
                 pTnrLst->Insert( pMkLine, LIST_APPEND );
         }
-        else
+        else {
             /* Zeilen ignorieren */;
+        }
     }   // End Of File
     fprintf( stderr, "\n" );
 
