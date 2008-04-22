fbf41c95259be2ac310a27a2e0d95d8804e131e7
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 13:48:32 +0000

    INTEGRATION: CWS hr50 (1.5.20); FILE MERGED 2008/03/10 15:47:41 hr 1.5.20.1: #i86574#: fix warning (gcc-4.2.3)

--- autodoc/source/inc/luxenum.hxx
+++ autodoc/source/inc/luxenum.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: luxenum.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -78,8 +78,8 @@ class Enum // : public Template_Base
                                                 { nValue = i_nValue; return *this; }
     self &              operator=(
                             intt                i_nValue )
-                                                { if ( CheckIntt(i_nValue) ) nValue = DIFF(i_nValue);
-                                                  else csv_assert(false); return *this; }
+                                                { if ( CheckIntt(i_nValue) ) {nValue = DIFF(i_nValue);}
+                                                  else {csv_assert(false);} return *this; }
     self &              operator=(
                             const self &        i_rEnum )
                                                 { nValue = i_rEnum.nValue; return *this; }
