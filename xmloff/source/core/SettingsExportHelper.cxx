06317984abe3e687f934dadf7aa68321819e424f
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 14:29:53 +0000

    INTEGRATION: CWS hr50 (1.29.172); FILE MERGED 2008/04/09 11:05:52 hr 1.29.172.1: #i86574#: fix warning (gcc-4.2.3)

--- xmloff/source/core/SettingsExportHelper.cxx
+++ xmloff/source/core/SettingsExportHelper.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: SettingsExportHelper.cxx,v $
- * $Revision: 1.30 $
+ * $Revision: 1.31 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -186,8 +186,9 @@ void XMLSettingsExportHelper::CallTypeFunction(const uno::Any& rAny,
                 aAny >>= aProps;
                 exportSymbolDescriptors(aProps, rName);
             }
-            else
+            else {
                 DBG_ERROR("this type is not implemented now");
+            }
         }
         break;
     }
