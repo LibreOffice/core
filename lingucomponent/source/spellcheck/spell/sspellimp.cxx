fc1a6876ae9b0dcbc5de23b896a69140c9c3813e
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 13:47:15 +0000

    INTEGRATION: CWS hr50 (1.18.96); FILE MERGED 2008/04/10 17:39:30 hr 1.18.96.1: #i86574#: fix warning (gcc-4.2.3)

--- lingucomponent/source/spellcheck/spell/sspellimp.cxx
+++ lingucomponent/source/spellcheck/spell/sspellimp.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sspellimp.cxx,v $
- * $Revision: 1.20 $
+ * $Revision: 1.21 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -568,8 +568,9 @@ void SAL_CALL
             xPropHelper = pPropHelper;
             pPropHelper->AddAsPropListener();   //! after a reference is established
         }
-        else
+        else {
             DBG_ERROR( "wrong number of arguments in sequence" );
+        }
 
     }
 }
