5fc06030ac77772f7f6c5e12b2cdbabe50aabf05
Ivo Hinkelmann <ihi@openoffice.org>
2008-04-24 16:03:28 +0000

    INTEGRATION: CWS cmcfixes43 (1.3.6); FILE MERGED 2008/03/07 16:55:32 cmc 1.3.6.1: string.h for memcpy

--- bridges/source/cpp_uno/gcc3_linux_powerpc64/cpp2uno.cxx
+++ bridges/source/cpp_uno/gcc3_linux_powerpc64/cpp2uno.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: cpp2uno.cxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -42,6 +42,7 @@
 
 #include "share.hxx"
 #include <stdio.h>
+#include <string.h>
 
 
 using namespace ::com::sun::star::uno;
