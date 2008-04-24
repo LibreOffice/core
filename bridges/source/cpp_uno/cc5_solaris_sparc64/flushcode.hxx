148937002dd42077f8a1b0aeb1d40c43979f77cd
Ivo Hinkelmann <ihi@openoffice.org>
2008-04-24 15:26:02 +0000

    INTEGRATION: CWS sb85 (1.1.2); FILE ADDED 2008/03/12 10:51:40 sb 1.1.2.1: #i86117# Solaris SPARC64 bridge

---
+++ bridges/source/cpp_uno/cc5_solaris_sparc64/flushcode.hxx
@@ -0,0 +1,50 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: flushcode.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: ihi $ $Date: 2008-04-24 16:26:02 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2008 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_BRIDGES_SOURCE_CPP_UNO_CC5_SOLARIS_SPARC64_FLUSHCODE_HXX
+#define INCLUDED_BRIDGES_SOURCE_CPP_UNO_CC5_SOLARIS_SPARC64_FLUSHCODE_HXX
+
+#include "sal/config.h"
+
+namespace bridges { namespace cpp_uno { namespace cc5_solaris_sparc64 {
+
+/**
+ * Flush a region of memory into which code has been written dynamically.
+ */
+void flushCode(void const * begin, void const * end);
+
+} } }
+
+#endif
