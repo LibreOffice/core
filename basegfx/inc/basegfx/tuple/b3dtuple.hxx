5adc6119a901a11243356e3d586264d081f8f6f3
Ivo Hinkelmann <ihi@openoffice.org>
2008-04-24 14:05:59 +0000

    INTEGRATION: CWS aw055 (1.13.30); FILE MERGED 2008/02/29 04:31:27 aw 1.13.30.1: removed op equal at polygons, added to tooling. Done for 2D and 3D

--- basegfx/inc/basegfx/tuple/b3dtuple.hxx
+++ basegfx/inc/basegfx/tuple/b3dtuple.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: b3dtuple.hxx,v $
- * $Revision: 1.14 $
+ * $Revision: 1.15 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -185,6 +185,14 @@ namespace basegfx
                 ::basegfx::fTools::equal(mfX, rTup.mfX) &&
                 ::basegfx::fTools::equal(mfY, rTup.mfY) &&
                 ::basegfx::fTools::equal(mfZ, rTup.mfZ));
+        }
+
+        bool equal(const B3DTuple& rTup, const double& rfSmallValue) const
+        {
+            return (
+                ::basegfx::fTools::equal(mfX, rTup.mfX, rfSmallValue) &&
+                ::basegfx::fTools::equal(mfY, rTup.mfY, rfSmallValue) &&
+                ::basegfx::fTools::equal(mfZ, rTup.mfZ, rfSmallValue));
         }
 
         // operators
