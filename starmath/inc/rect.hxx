b975b47cb95326a53d6538cbd40133ead34fc2bb
Ivo Hinkelmann <ihi@openoffice.org>
2008-04-24 12:57:29 +0000

    INTEGRATION: CWS tl50 (1.5.198); FILE MERGED 2008/02/27 16:26:55 tl 1.5.198.1: #i86272# unused code removed

--- starmath/inc/rect.hxx
+++ starmath/inc/rect.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: rect.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -215,7 +215,9 @@ public:
     inline  Rectangle   AsRectangle() const;
             SmRect      AsGlyphRect() const;
 
+#ifdef SM_RECT_DEBUG
             void        Draw(OutputDevice &rDev, const Point &rPosition, int nFlags) const;
+#endif
 };
 
 
