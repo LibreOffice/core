5def0623e37bb6931055fcfc5f44be5a8663363e
Ivo Hinkelmann <ihi@openoffice.org>
2008-04-24 16:05:49 +0000

    INTEGRATION: CWS cmcfixes43 (1.4.88); FILE MERGED 2008/03/03 10:52:36 cmc 1.4.88.1: #i86629 unify gcc maps

--- stoc/test/registry_tdprovider/makefile.mk
+++ stoc/test/registry_tdprovider/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -48,8 +48,8 @@ SHL1STDLIBS = $(CPPULIB) $(CPPUHELPERLIB) $(SALLIB)
 SHL1IMPLIB = itestregistrytdprovider
 DEF1NAME = $(SHL1TARGET)
 
-.IF "$(OS)$(CPU)$(COMNAME)" == "LINUXIgcc3"
-SHL1VERSIONMAP = testregistrytdprovider.LINUXIgcc3.map
+.IF "$(COMNAME)" == "gcc3"
+SHL1VERSIONMAP = testregistrytdprovider.gcc3.map
 .ELSE
 SHL1VERSIONMAP = testregistrytdprovider.map
 .ENDIF
