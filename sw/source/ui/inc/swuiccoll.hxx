537d091ecb5038e868aa1352804330f7e1018099
Kurt Zenker <kz@openoffice.org>
2008-05-06 14:24:47 +0000

    INTEGRATION: CWS lcwarnings2 (1.5.182); FILE MERGED 2008/04/17 12:35:18 tl 1.5.182.3: RESYNC: (1.5-1.6); FILE MERGED 2008/03/03 10:09:52 tl 1.5.182.2: #i83458# warning-free code wntmsci11 2008/02/29 14:51:11 tl 1.5.182.1: #i83458# warning free code for wntmsci11

--- sw/source/ui/inc/swuiccoll.hxx
+++ sw/source/ui/inc/swuiccoll.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: swuiccoll.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -72,8 +72,8 @@ class SwCondCollPage : public SfxTabPage
     DECL_LINK( AssignRemoveHdl, PushButton*);
     DECL_LINK( SelectHdl, ListBox* );
 
-    using TabPage::ActivatePage;
-    using TabPage::DeactivatePage;
+    using SfxTabPage::ActivatePage;
+    using SfxTabPage::DeactivatePage;
 
 public:
 
