f71fb5caab70056136e9ddc83b77d2bd9b2656b7
Kurt Zenker <kz@openoffice.org>
2008-05-06 09:34:34 +0000

    INTEGRATION: CWS native147 (1.20.4); FILE MERGED 2008/04/23 20:26:48 obr 1.20.4.1: #i88334#,#i61569#,#i62176# removed /etc indirection and package database check for office install location

--- sysui/desktop/util/makefile.mk
+++ sysui/desktop/util/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.20 $
+# $Revision: 1.21 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -34,47 +34,10 @@ PRJ=..$/..
 PRJNAME=sysui
 TARGET=launcher
 
-TARGETTYPE=CUI
-LIBTARGET=NO
-NO_DEFAULT_STL=TRUE
-LIBSALCPPRT=$(0)
-
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
 .INCLUDE :  ..$/productversion.mk
-
-.IF "$(ENABLE_GNOMEVFS)"!=""
-COMPILER_WARN_ALL=TRUE
-PKGCONFIG_MODULES=gnome-vfs-2.0 ORBit-2.0 ORBit-CosNaming-2.0 gconf-2.0
-.INCLUDE: pkg_config.mk
-
-.IF "$(OS)" == "SOLARIS"
-.IF "$(SYSBASE)"!=""
-.IF "$(COMNAME)"=="sunpro5"
-# soften C restrictions to survive "inline" keyword
-CFLAGSCC!:=$(subst,-xc99=none,-xc99=no_lib $(CFLAGSCC))
-.ENDIF          # "$(COMNAME)"=="sunpro5"
-.ENDIF			# "$(SYSBASE)"!=""
-
-LINKFLAGS+=-z nodefs
-.ENDIF # "$(OS)" == "SOLARIS"
-
-# Disable lazy binding of symbols
-.IF "$(OS)" == "LINUX"
-LINKFLAGS+=-Wl,-z,now
-.ENDIF
-.IF "$(OS)" == "SOLARIS"
-LINKFLAGS+=-z now
-.ENDIF  
-
-.IF "$(OS)" == "LINUX"
-# hack for faked SO environment
-CFLAGS+=-gdwarf-2
-PKGCONFIG_LIBS!:=-Wl,--export-dynamic $(PKGCONFIG_LIBS)
-.ENDIF
-
-.ENDIF          # "$(ENABLE_GNOMEVFS)"!=""
 
 .IF "$(PKGFORMAT)"!="$(PKGFORMAT:s/rpm//)"
 RPMTARFILES=$(BIN)$/rpm$/{$(PRODUCTLIST)}-desktop-integration.tar.gz
@@ -83,17 +46,6 @@ RPMTARFILES=$(BIN)$/rpm$/{$(PRODUCTLIST)}-desktop-integration.tar.gz
 .IF "$(PKGFORMAT)"!="$(PKGFORMAT:s/deb//)"
 DEBTARFILES=$(BIN)$/deb$/{$(PRODUCTLIST)}-desktop-integration.tar.gz
 .ENDIF
-
-# --- Files --------------------------------------------------------
-
-.IF "$(ENABLE_GNOMEVFS)"!=""
-OBJFILES += $(OBJ)$/gnome-set-default-application.obj
-
-APP1TARGET=gnome-set-default-application
-APP1OBJS=$(OBJ)$/gnome-set-default-application.obj
-APP1LIBS=
-APP1STDLIBS+=$(PKGCONFIG_LIBS)
-.ENDIF          # "$(ENABLE_GNOMEVFS)"!=""
 
 # --- Targets ------------------------------------------------------
 
