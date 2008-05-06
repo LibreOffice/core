869ddb3a4bb525c31b48d286fce3cd032a05b163
Kurt Zenker <kz@openoffice.org>
2008-05-06 09:30:47 +0000

    INTEGRATION: CWS native147 (1.16.4); FILE MERGED 2008/04/23 20:26:46 obr 1.16.4.1: #i88334#,#i61569#,#i62176# removed /etc indirection and package database check for office install location

--- sysui/desktop/mandriva/makefile.mk
+++ sysui/desktop/mandriva/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.16 $
+# $Revision: 1.17 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -46,7 +46,7 @@ TARGET=mandriva
  
 .IF "$(PKGFORMAT)"!="$(PKGFORMAT:s/rpm//)"
 
-SPECFILE=$(MISC)$/$(TARGET)-menus.spec
+SPECFILE=$(TARGET)-menus.spec
 RPMFILES=$(foreach,i,{$(PRODUCTLIST)} $(PKGDIR)$/$i-$(SPECFILE:b)-$(PKGVERSION.$i)-$(PKGREV).noarch.rpm)
 
 .ENDIF
