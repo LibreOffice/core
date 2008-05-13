de223cc9ae27c55246831a6a9584c6350fd41656
Vladimir Glazounov <vg@openoffice.org>
2008-05-13 12:03:40 +0000

    INTEGRATION: CWS fwk86 (1.2.282); FILE MERGED 2008/04/30 15:47:24 pb 1.2.282.2: RESYNC: (1.2-1.3); FILE MERGED 2008/04/07 11:46:01 pb 1.2.282.1: fix: #i86924# new member: ULONG m_nSymbolsStyle

--- sfx2/inc/sfx2/tbxctrl.hxx
+++ sfx2/inc/sfx2/tbxctrl.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: tbxctrl.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -387,6 +387,7 @@ private:
     String                  aLastURL;
     BOOL                    bBigImages;
     PopupMenu*              pMenu;
+    ULONG                   m_nSymbolsStyle;
     BOOL                    m_bWasHiContrastMode;
     BOOL                    m_bShowMenuImages;
 };
