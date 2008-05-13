c4b3110a0a604ed6fe478752ea0f6224f7837107
Vladimir Glazounov <vg@openoffice.org>
2008-05-13 13:29:45 +0000

    INTEGRATION: CWS presenterscreen (1.2.4); FILE MERGED 2008/04/30 08:09:09 af 1.2.4.4: #i88853# Added GetCurrentSlide() method.  Made UpdatePaneTitles() public. 2008/04/23 13:13:12 af 1.2.4.3: #i18486# Added HandleMouseClick() method, added support for F1 key. 2008/04/22 08:26:22 af 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/04/16 15:31:35 af 1.2.4.1: #i18486# Improved access to theme, added support for UNO commands.

--- sdext/source/presenter/PresenterController.hxx
+++ sdext/source/presenter/PresenterController.hxx
@@ -8,7 +8,7 @@
  *
  * $RCSfile: PresenterController.hxx,v $
  *
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -32,13 +32,17 @@
 #ifndef SDEXT_PRESENTER_CONTROLLER_HXX
 #define SDEXT_PRESENTER_CONTROLLER_HXX
 
-#include <cppuhelper/compbase5.hxx>
+#include "PresenterTheme.hxx"
+#include "PresenterSprite.hxx"
+#include <cppuhelper/compbase6.hxx>
 #include <cppuhelper/basemutex.hxx>
 #include <com/sun/star/awt/FontDescriptor.hpp>
 #include <com/sun/star/awt/XFocusListener.hpp>
 #include <com/sun/star/awt/XKeyListener.hpp>
 #include <com/sun/star/awt/XMouseListener.hpp>
+#include <com/sun/star/drawing/XPresenterHelper.hpp>
 #include <com/sun/star/frame/XController.hpp>
+#include <com/sun/star/frame/XDispatch.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/presentation/XSlideShowController.hpp>
 #include <com/sun/star/presentation/XSlideShowListener.hpp>
@@ -48,7 +52,9 @@
 #include <com/sun/star/drawing/framework/XPane.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/util/Color.hpp>
+#include <com/sun/star/util/XURLTransformer.hpp>
 #include <rtl/ref.hxx>
+#include <map>
 #include <vector>
 #include <boost/function.hpp>
 #include <boost/shared_ptr.hpp>
@@ -59,6 +65,8 @@ namespace css = ::com::sun::star;
 namespace sdext { namespace presenter {
 
 class PresenterAnimator;
+class PresenterCanvasHelper;
+class PresenterPaintManager;
 class PresenterPaneAnimator;
 class PresenterPaneContainer;
 class PresenterPaneBorderPainter;
@@ -66,12 +74,13 @@ class PresenterTheme;
 class PresenterWindowManager;
 
 namespace {
-    typedef ::cppu::WeakComponentImplHelper5 <
+    typedef ::cppu::WeakComponentImplHelper6 <
         css::drawing::framework::XConfigurationChangeListener,
         css::frame::XFrameActionListener,
         css::awt::XKeyListener,
         css::awt::XFocusListener,
-        css::awt::XMouseListener
+        css::awt::XMouseListener,
+        css::awt::XMouseMotionListener
     > PresenterControllerInterfaceBase;
 }
 
@@ -85,6 +94,9 @@ class PresenterController
       public PresenterControllerInterfaceBase
 {
 public:
+    static ::rtl::Reference<PresenterController> Instance (
+        const css::uno::Reference<css::frame::XFrame>& rxFrame);
+
     PresenterController (
         const css::uno::Reference<css::uno::XComponentContext>& rxContext,
         const css::uno::Reference<css::frame::XController>& rxController,
@@ -97,12 +109,10 @@ public:
 
     void UpdateCurrentSlide (const sal_Int32 nOffset);
 
-    sal_Int32 GetBackgroundColor (void) const;
-    sal_Int32 GetViewBackgroundColor (const ::rtl::OUString& rsViewURL) const;
-    css::uno::Reference<css::rendering::XBitmap> GetViewBackgroundBitmap (const ::rtl::OUString& rsViewURL) const;
-    css::awt::FontDescriptor GetViewFontDescriptor (const ::rtl::OUString& rsViewURL) const;
-    css::rendering::FontRequest GetViewFontRequest (const ::rtl::OUString& rsViewURL) const;
-    css::util::Color GetViewFontColor (const ::rtl::OUString& rsViewURL) const;
+    SharedBitmapDescriptor
+        GetViewBackground (const ::rtl::OUString& rsViewURL) const;
+    PresenterTheme::SharedFontDescriptor
+        GetViewFont (const ::rtl::OUString& rsViewURL) const;
     ::boost::shared_ptr<PresenterTheme> GetTheme (void) const;
     ::rtl::Reference<PresenterWindowManager> GetWindowManager (void) const;
     css::uno::Reference<css::presentation::XSlideShowController>
@@ -111,7 +121,32 @@ public:
     rtl::Reference<PresenterPaneContainer> GetPaneContainer (void) const;
     ::rtl::Reference<PresenterPaneBorderPainter> GetPaneBorderPainter (void) const;
     ::boost::shared_ptr<PresenterAnimator> GetAnimator (void) const;
+    ::boost::shared_ptr<PresenterCanvasHelper> GetCanvasHelper (void) const;
+    css::uno::Reference<css::drawing::XPresenterHelper> GetPresenterHelper (void) const;
+    ::boost::shared_ptr<PresenterPaintManager> GetPaintManager (void) const;
     void HideSlideSorter (void);
+    double GetSlideAspectRatio (void) const;
+    void ShowView (const ::rtl::OUString& rsViewURL);
+    void HideView (const ::rtl::OUString& rsViewURL);
+    bool IsViewVisible (const ::rtl::OUString& rsViewURL) const;
+    void DispatchUnoCommand (const ::rtl::OUString& rsCommand) const;
+    css::uno::Reference<css::frame::XDispatch> GetDispatch (
+        const css::util::URL& rURL) const;
+    css::util::URL CreateURLFromString (const ::rtl::OUString& rsURL) const;
+    css::uno::Reference<css::drawing::framework::XConfigurationController>
+        GetConfigurationController (void) const;
+    css::uno::Reference<css::drawing::XDrawPage> GetCurrentSlide (void) const;
+
+    void HandleMouseClick (const css::awt::MouseEvent& rEvent);
+    void UpdatePaneTitles (void);
+
+    /** Request activation or deactivation of (some of) the views according
+        to the given parameters.
+    */
+    void RequestViews (
+        const bool bIsSlideSorterActive,
+        const bool bIsNotesViewActive,
+        const bool bIsHelpViewActive);
 
     // XConfigurationChangeListener
 
@@ -164,24 +199,42 @@ public:
     virtual void SAL_CALL mouseExited (const css::awt::MouseEvent& rEvent)
         throw (css::uno::RuntimeException);
 
+
+    // XMouseMotionListener
+
+    virtual void SAL_CALL mouseMoved (const css::awt::MouseEvent& rEvent)
+        throw (css::uno::RuntimeException);
+
+    virtual void SAL_CALL mouseDragged (const css::awt::MouseEvent& rEvent)
+        throw (css::uno::RuntimeException);
+
 private:
-    css::uno::Reference<css::uno::XComponentContext> mxContext;
+    typedef ::std::map<css::uno::Reference<css::frame::XFrame>,rtl::Reference<PresenterController> > InstanceContainer;
+    static InstanceContainer maInstances;
+
+    css::uno::Reference<css::uno::XComponentContext> mxComponentContext;
+    css::uno::Reference<css::rendering::XSpriteCanvas> mxCanvas;
     css::uno::Reference<css::frame::XController> mxController;
     css::uno::Reference<css::drawing::framework::XConfigurationController>
         mxConfigurationController;
     css::uno::Reference<css::presentation::XSlideShowController> mxSlideShowController;
     css::uno::Reference<css::drawing::framework::XResourceId> mxMainPaneId;
     rtl::Reference<PresenterPaneContainer> mpPaneContainer;
+    sal_Int32 mnCurrentSlideIndex;
     css::uno::Reference<css::drawing::XDrawPage> mxCurrentSlide;
     css::uno::Reference<css::drawing::XDrawPage> mxNextSlide;
     ::rtl::Reference<PresenterWindowManager> mpWindowManager;
     ::boost::shared_ptr<PresenterPaneAnimator> mpCurrentPaneAnimation;
-    sal_Int32 mnBackgroundColor;
     sal_Int32 mnWindowBackgroundColor;
     ::boost::shared_ptr<PresenterTheme> mpTheme;
     css::uno::Reference<css::awt::XWindow> mxMainWindow;
     ::rtl::Reference<PresenterPaneBorderPainter> mpPaneBorderPainter;
     ::boost::shared_ptr<PresenterAnimator> mpAnimator;
+    ::boost::shared_ptr<PresenterCanvasHelper> mpCanvasHelper;
+    css::uno::Reference<css::drawing::XPresenterHelper> mxPresenterHelper;
+    ::boost::shared_ptr<PresenterPaintManager> mpPaintManager;
+    sal_Int32 mnPendingSlideNumber;
+    css::uno::Reference<css::util::XURLTransformer> mxUrlTransformer;
 
     void InitializePresenterScreen (void);
     void InitializeSlideShowView (const css::uno::Reference<css::uno::XInterface>& rxView);
@@ -189,11 +242,7 @@ private:
     void UpdateViews (void);
     void InitializeMainPane (const css::uno::Reference<css::drawing::framework::XPane>& rxPane);
     void LoadTheme (const css::uno::Reference<css::drawing::framework::XPane>& rxPane);
-    void ToggleHelpWindow (const bool bUseAnimation);
-    enum ToggleState { Toggle, Show, Hide };
-    void ToggleSlideSorter (
-        const ToggleState eState,
-        const sal_Int32 nAppearMode);
+    void UpdatePendingSlideNumber (const sal_Int32 nPendingSlideNumber);
 
     void ThrowIfDisposed (void) const throw (::com::sun::star::lang::DisposedException);
 };
