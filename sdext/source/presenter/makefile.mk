fc73d51b39a5045e01ad95033918478fd5cc2ca8
Vladimir Glazounov <vg@openoffice.org>
2008-05-13 13:42:11 +0000

    INTEGRATION: CWS presenterscreen (1.3.4); FILE MERGED 2008/05/06 09:59:57 af 1.3.4.9: #i89059# Moved XCU and XCS files to their places in the registry/ dir tree. 2008/04/30 12:27:47 af 1.3.4.8: #i18486# Fixed replacement of placeholders. 2008/04/30 08:42:55 af 1.3.4.7: #i18486# Changed identifier prefix from org.openoffice to com.sun.star 2008/04/28 09:20:36 af 1.3.4.6: #i18486# Cleaned up processing of description.xml. 2008/04/22 13:10:34 af 1.3.4.5: #i18486# Made extension identifier platform specific. 2008/04/22 08:37:13 af 1.3.4.4: #i18486# Add runtime libraries to extension. 2008/04/22 08:24:37 af 1.3.4.3: RESYNC: (1.3-1.4); FILE MERGED 2008/04/18 14:55:09 af 1.3.4.2: #i18486# Removed last dependency on non-URE library. 2008/04/16 16:24:51 af 1.3.4.1: #i18486# Changed theme bitmaps.

--- sdext/source/presenter/makefile.mk
+++ sdext/source/presenter/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -39,13 +39,15 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings ----------------------------------
 
+.INCLUDE : rtlbootstrap.mk
 .INCLUDE : settings.mk
 .INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
-.IF "$(ENABLE_PRESENTER_SCREEN)" != "YES"
+.IF "$(ENABLE_PRESENTER_SCREEN)" == "NO"
 @all:
     @echo "Presenter Screen build disabled."
 .ELSE
+
 DLLPRE=
 common_build_zip=
 
@@ -62,7 +64,8 @@ SLOFILES=										\
     $(SLO)$/PresenterAnimation.obj				\
     $(SLO)$/PresenterAnimator.obj				\
     $(SLO)$/PresenterBitmapContainer.obj		\
-    $(SLO)$/PresenterClock.obj					\
+    $(SLO)$/PresenterButton.obj					\
+    $(SLO)$/PresenterCanvasHelper.obj			\
     $(SLO)$/PresenterConfigurationAccess.obj	\
     $(SLO)$/PresenterControlCreator.obj			\
     $(SLO)$/PresenterController.obj				\
@@ -72,6 +75,7 @@ SLOFILES=										\
     $(SLO)$/PresenterHelper.obj					\
     $(SLO)$/PresenterHelpView.obj				\
     $(SLO)$/PresenterNotesView.obj				\
+    $(SLO)$/PresenterPaintManager.obj			\
     $(SLO)$/PresenterPane.obj					\
     $(SLO)$/PresenterPaneAnimator.obj			\
     $(SLO)$/PresenterPaneBase.obj				\
@@ -88,98 +92,154 @@ SLOFILES=										\
     $(SLO)$/PresenterSprite.obj					\
     $(SLO)$/PresenterSpritePane.obj				\
     $(SLO)$/PresenterTheme.obj					\
+    $(SLO)$/PresenterTimer.obj					\
     $(SLO)$/PresenterToolBar.obj				\
+    $(SLO)$/PresenterUIPainter.obj				\
     $(SLO)$/PresenterViewFactory.obj			\
     $(SLO)$/PresenterWindowManager.obj			\
     $(SLO)$/PresenterComponent.obj
-
-#$(SLO)$/PresenterAboutDialog.obj			\
-
-
-DESCRIPTION:=$(MISC)$/$(TARGET)$/description.xml
-
-.IF "$(GUI)" == "WIN" || "$(GUI)" == "WNT"
-PACKLICS:=$(foreach,i,$(alllangiso) $(MISC)$/$(TARGET)$/registry$/license_$i)
-.ELSE
-PACKLICS:=$(foreach,i,$(alllangiso) $(MISC)$/$(TARGET)$/registry$/LICENSE_$i)
-.ENDIF
 
 
 # --- Library -----------------------------------
 
 SHL1TARGET=		$(TARGET).uno
 
-SHL1STDLIBS=	$(COMPHELPERLIB)	\
-                $(CPPUHELPERLIB)	\
+SHL1STDLIBS=	$(CPPUHELPERLIB)	\
                 $(CPPULIB)			\
-                $(SALLIB)			\
-                $(VOSLIB)
+                $(SALLIB)
 SHL1DEPN=
 SHL1IMPLIB=		i$(SHL1TARGET)
 SHL1LIBS=		$(SLB)$/$(TARGET).lib
 SHL1DEF=		$(MISC)$/$(SHL1TARGET).def
 SHL1VERSIONMAP=	exports.map
+SHL1RPATH=      OXT
 DEF1NAME=		$(SHL1TARGET)
 
-COMPONENT_FILES=											\
-    $(MISC)$/$(TARGET)$/configuration$/Jobs.xcu				\
-    $(MISC)$/$(TARGET)$/configuration$/Addons.xcu			\
-    $(MISC)$/$(TARGET)$/configuration$/ProtocolHandler.xcu	\
-    $(MISC)$/$(TARGET)$/configuration$/PresenterScreen.xcs	\
-    $(MISC)$/$(TARGET)$/configuration$/PresenterScreen.xcu
+ZIP1TARGET=		presenter-screen
+ZIP1DIR=		$(MISC)$/$(TARGET)
+ZIP1EXT=		.oxt
+ZIP1FLAGS=-r
+ZIP1LIST=		*
+
+DESCRIPTION:=$(ZIP1DIR)$/description.xml
+
+.IF "$(GUI)" == "WIN" || "$(GUI)" == "WNT"
+PACKLICS:=$(foreach,i,$(alllangiso) $(ZIP1DIR)$/registry$/license_$i)
+.ELSE
+PACKLICS:=$(foreach,i,$(alllangiso) $(ZIP1DIR)$/registry$/LICENSE_$i)
+.ENDIF
+
+
+
+COMPONENT_FILES=																			\
+    $(ZIP1DIR)$/registry$/data$/org$/openoffice$/Office$/Jobs.xcu							\
+    $(ZIP1DIR)$/registry$/data$/org$/openoffice$/Office$/Addons.xcu							\
+    $(ZIP1DIR)$/registry$/data$/org$/openoffice$/Office$/ProtocolHandler.xcu				\
+    $(ZIP1DIR)$/registry$/data$/org$/openoffice$/Office$/extension$/PresenterScreen.xcu		\
+    $(ZIP1DIR)$/registry$/schema/org$/openoffice$/Office$/extension$/PresenterScreen.xcs
 
 COMPONENT_BITMAPS=												\
-    $(MISC)$/$(TARGET)$/bitmaps$/TitleTopPaneBorder.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/TitleTopLeftPaneBorder.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/TitleTopRightPaneBorder.png	\
-    $(MISC)$/$(TARGET)$/bitmaps$/TopPaneBorder.png				\
-    $(MISC)$/$(TARGET)$/bitmaps$/TopLeftPaneBorder.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/TopRightPaneBorder.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/LeftPaneBorder.png				\
-    $(MISC)$/$(TARGET)$/bitmaps$/RightPaneBorder.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/BottomLeftPaneBorder.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/BottomRightPaneBorder.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/BottomPaneBorder.png			\
+    $(ZIP1DIR)$/bitmaps$/BorderTop.png							\
+    $(ZIP1DIR)$/bitmaps$/BorderTopLeft.png						\
+    $(ZIP1DIR)$/bitmaps$/BorderTopRight.png						\
+    $(ZIP1DIR)$/bitmaps$/BorderLeft.png							\
+    $(ZIP1DIR)$/bitmaps$/BorderRight.png						\
+    $(ZIP1DIR)$/bitmaps$/BorderBottomLeft.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderBottomRight.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderBottom.png						\
                                                                 \
-    $(MISC)$/$(TARGET)$/bitmaps$/NextEffectButton.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/NextEffectButtonGlow.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/NextEffectButtonPressed.png	\
-    $(MISC)$/$(TARGET)$/bitmaps$/NextSlideButton.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/NextSlideButtonGlow.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/NextSlideButtonPressed.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/PrevSlideButton.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/PrevSlideButtonGlow.png		\
-    $(MISC)$/$(TARGET)$/bitmaps$/PrevSlideButtonPressed.png		\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveTop.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveTopLeft.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveTopRight.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveLeft.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveRight.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveBottomLeft.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveBottomRight.png			\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveBottom.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderActiveBottomCallout.png			\
                                                                 \
-    $(MISC)$/$(TARGET)$/bitmaps$/Left.png						\
-    $(MISC)$/$(TARGET)$/bitmaps$/Right.png						\
-    $(MISC)$/$(TARGET)$/bitmaps$/Up.png							\
-    $(MISC)$/$(TARGET)$/bitmaps$/Down.png						\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideTop.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideTopLeft.png			\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideTopRight.png			\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideLeft.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideRight.png			\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideBottomLeft.png		\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideBottomRight.png		\
+    $(ZIP1DIR)$/bitmaps$/BorderCurrentSlideBottom.png			\
                                                                 \
-    $(MISC)$/$(TARGET)$/bitmaps$/PagerHorizontal.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/PagerVertical.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarTop.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarTopLeft.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarTopRight.png				\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarLeft.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarRight.png					\
+    $(ZIP1DIR)$/bitmaps$/BorderToolbarBottom.png				\
                                                                 \
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbLeft.png					\
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbRight.png					\
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbTop.png					\
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbBottom.png				\
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbHorizontal.png			\
-    $(MISC)$/$(TARGET)$/bitmaps$/ThumbVertical.png
-
-
-#$(MISC)$/$(TARGET)$/bitmaps$/Clock-Face.png					
-#$(MISC)$/$(TARGET)$/bitmaps$/Clock-Hour-Hand.png
-#$(MISC)$/$(TARGET)$/bitmaps$/Clock-Minute-Hand.png
-#	$(MISC)$/$(TARGET)$/bitmaps$/PagerLeft.png					\
-#	$(MISC)$/$(TARGET)$/bitmaps$/PagerRight.png					\
-#	$(MISC)$/$(TARGET)$/bitmaps$/PagerTop.png					\
-#	$(MISC)$/$(TARGET)$/bitmaps$/PagerBottom.png				\
+    $(ZIP1DIR)$/bitmaps$/Background.png							\
+    $(ZIP1DIR)$/bitmaps$/ViewBackground.png						\
+                                                                \
+    $(ZIP1DIR)$/bitmaps$/ButtonSlidePreviousNormal.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlidePreviousMouseOver.png		\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlidePreviousSelected.png		\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlidePreviousDisabled.png		\
+    $(ZIP1DIR)$/bitmaps$/ButtonEffectNextNormal.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonEffectNextMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonEffectNextSelected.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonEffectNextDisabled.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonNotesNormal.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonNotesMouseOver.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonNotesSelected.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonNotesDisabled.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlideSorterNormal.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlideSorterMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlideSorterSelected.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonSlideSorterDisabled.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonHelpNormal.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonHelpMouseOver.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonHelpSelected.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonHelpDisabled.png					\
+                                                                \
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowUpNormal.png				\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowUpMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowUpSelected.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowUpDisabled.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowDownNormal.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowDownMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowDownSelected.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarArrowDownDisabled.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarPagerMiddleNormal.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarPagerMiddleMouseOver.png		\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbTopNormal.png				\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbTopMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbBottomNormal.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbBottomMouseOver.png		\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbMiddleNormal.png			\
+    $(ZIP1DIR)$/bitmaps/ScrollbarThumbMiddleMouseOver.png		\
+                                                                \
+    $(ZIP1DIR)$/bitmaps$/ButtonPlusNormal.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonPlusMouseOver.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonPlusSelected.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonPlusDisabled.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonMinusNormal.png					\
+    $(ZIP1DIR)$/bitmaps$/ButtonMinusMouseOver.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonMinusSelected.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonMinusDisabled.png				\
+                                                                \
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameLeftNormal.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameCenterNormal.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameRightNormal.png				\
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameLeftMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameCenterMouseOver.png			\
+    $(ZIP1DIR)$/bitmaps$/ButtonFrameRightMouseOver.png			\
+                                                                \
+    $(ZIP1DIR)$/bitmaps$/LabelMouseOverLeft.png					\
+    $(ZIP1DIR)$/bitmaps$/LabelMouseOverCenter.png				\
+    $(ZIP1DIR)$/bitmaps$/LabelMouseOverRight.png
 
 COMPONENT_MANIFEST= 							\
-    $(MISC)$/$(TARGET)$/META-INF$/manifest.xml
+    $(ZIP1DIR)$/META-INF$/manifest.xml
 
 COMPONENT_LIBRARY= 								\
-    $(MISC)$/$(TARGET)$/$(TARGET).uno$(DLLPOST)
+    $(ZIP1DIR)$/$(TARGET).uno$(DLLPOST)
 
 ZIP1DEPS=					\
     $(PACKLICS) 			\
@@ -189,35 +249,62 @@ ZIP1DEPS=					\
     $(COMPONENT_BITMAPS)	\
     $(COMPONENT_LIBRARY)
 
-ZIP1TARGET=		presenter-screen
-ZIP1DIR=		$(MISC)$/$(TARGET)
-ZIP1EXT=		.oxt
-ZIP1FLAGS=-r
-ZIP1LIST=		*
+
+PLATFORMID:=$(RTL_OS:l)_$(RTL_ARCH:l)
+
 
 # --- Targets ----------------------------------
 
 .INCLUDE : target.mk
 
+$(SLO)$/PresenterComponent.obj : $(INCCOM)$/PresenterExtensionIdentifier.hxx
+
+$(INCCOM)$/PresenterExtensionIdentifier.hxx : PresenterExtensionIdentifier.txx
+    $(TYPE) $< | sed s/UPDATED_PLATFORM/$(PLATFORMID)/ > $@
+
 $(COMPONENT_MANIFEST) : $$(@:f)
     @-$(MKDIRHIER) $(@:d)
     +$(TYPE) $< | $(SED) "s/SHARED_EXTENSION/$(DLLPOST)/" > $@
 
-$(COMPONENT_FILES) : $$(@:f)
-    @-$(MKDIRHIER) $(@:d)
-    +$(COPY) $< $@
+#$(COMPONENT_FILES) : $$(@:f)
+#	@-$(MKDIRHIER) $(@:d)
+#    +$(COPY) $< $@
 
 $(COMPONENT_BITMAPS) : bitmaps$/$$(@:f)
     @-$(MKDIRHIER) $(@:d)
     +$(COPY) $< $@
 
-#$(COMPONENT_HELP) : help$/$$(@:f)
-#	@-$(MKDIRHIER) $(@:d)
-#    +$(COPY) $< $@
-
 $(COMPONENT_LIBRARY) : $(DLLDEST)$/$$(@:f)
     @-$(MKDIRHIER) $(@:d)
     +$(COPY) $< $@
+.IF "$(OS)$(CPU)"=="WNTI"
+    .IF "$(PACKMS)"!=""
+        .IF "$(CCNUMVER)" <= "001499999999"
+            $(GNUCOPY) $(PACKMS)$/msvcr80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/msvcp80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/msvcm80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/Microsoft.VC80.CRT.manifest $(ZIP1DIR)
+        .ELSE
+            $(GNUCOPY) $(PACKMS)$/msvcr90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/msvcp90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/msvcm90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(PACKMS)$/Microsoft.VC90.CRT.manifest $(ZIP1DIR)
+        .ENDIF
+    .ELSE        # "$(PACKMS)"!=""
+        .IF "$(CCNUMVER)" <= "001499999999"
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcr80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcp80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcm80.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/Microsoft.VC80.CRT.manifest $(ZIP1DIR)
+        .ELSE
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcr90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcp90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/msvcm90.dll $(ZIP1DIR)
+            $(GNUCOPY) $(SOLARBINDIR)$/Microsoft.VC90.CRT.manifest $(ZIP1DIR)
+        .ENDIF
+    .ENDIF         # "$(PACKMS)"!=""
+.ENDIF
+
 
 .IF "$(GUI)" == "WIN" || "$(GUI)" == "WNT"
 $(PACKLICS) : $(SOLARBINDIR)$/osl$/license$$(@:b:s/_/./:e:s/./_/)$$(@:e).txt
@@ -229,13 +316,27 @@ $(PACKLICS) : $(SOLARBINDIR)$/osl$/LICENSE$$(@:b:s/_/./:e:s/./_/)$$(@:e)
     $(GNUCOPY) $< $@
 .ENDIF
 
-.INCLUDE .IGNORE : $(MISC)$/$(TARGET)_lang_track.mk
+$(ZIP1DIR)/%.xcu : %.xcu
+    @@-$(MKDIRHIER) $(@:d)
+    $(GNUCOPY) $< $@
+
+$(ZIP1DIR)$/%.xcs : %.xcs
+    @@-$(MKDIRHIER) $(@:d)
+    $(GNUCOPY) $< $@
+
+# Temporary file that is used to replace some placeholders in description.xml.
+DESCRIPTION_TMP:=$(ZIP1DIR)$/description.xml.tmp
+
+.INCLUDE .IGNORE : $(ZIP1DIR)_lang_track.mk
 .IF "$(LAST_WITH_LANG)"!="$(WITH_LANG)"
 PHONYDESC=.PHONY
 .ENDIF			# "$(LAST_WITH_LANG)"!="$(WITH_LANG)"
 $(DESCRIPTION) $(PHONYDESC) : $$(@:f)
     @@-$(MKDIRHIER) $(@:d)
-    $(PERL) $(SOLARENV)$/bin$/licinserter.pl description.xml registry/LICENSE_xxx $@
-    @echo LAST_WITH_LANG=$(WITH_LANG) > $(MISC)$/$(TARGET)_lang_track.mk
+    $(PERL) $(SOLARENV)$/bin$/licinserter.pl description.xml registry/LICENSE_xxx $(DESCRIPTION_TMP)
+    @echo LAST_WITH_LANG=$(WITH_LANG) > $(ZIP1DIR)_lang_track.mk
+    $(TYPE) $(DESCRIPTION_TMP) | sed s/UPDATED_PLATFORM/$(PLATFORMID)/ > $@
+    @@-$(RM) $(DESCRIPTION_TMP)	
 
-.ENDIF # "$(ENABLE_PRESENTER_SCREEN)" != "YES"
+
+.ENDIF # "$(ENABLE_PRESENTER_SCREEN)" != "NO"
