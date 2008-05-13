6a5424fa92f878b6cd3db6fa2d2d24f55b2b1e8f
Vladimir Glazounov <vg@openoffice.org>
2008-05-13 13:29:17 +0000

    INTEGRATION: CWS presenterscreen (1.2.4); FILE MERGED 2008/04/30 08:05:43 af 1.2.4.5: #i88853# Extended functionality to allow easier modification of properties. 2008/04/23 11:51:22 af 1.2.4.4: #i18486# Added desctructor. 2008/04/22 11:41:50 rene 1.2.4.3: #i88584# fix double qualification 2008/04/22 08:26:25 af 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/04/16 15:29:16 af 1.2.4.1: #i18486# Added support for accessing configuration nodes as property sets.

--- sdext/source/presenter/PresenterConfigurationAccess.hxx
+++ sdext/source/presenter/PresenterConfigurationAccess.hxx
@@ -8,7 +8,7 @@
  *
  * $RCSfile: PresenterConfigurationAccess.hxx,v $
  *
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,6 +33,7 @@
 #define SDEXT_PRESENTER_CONFIGURATION_ACCESS_HXX
 
 #include <rtl/ustring.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
 #include <com/sun/star/container/XHierarchicalNameAccess.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
@@ -45,17 +46,32 @@ namespace sdext { namespace presenter {
 
 /** This class gives access to the configuration.  Create an object of this
     class for one node of the configuration.  This will be the root node.
-    Its children (on all levels) are then accessible through this new object.
+    From this one you can use this class in two ways.
+
+    <p>In a stateless mode (with exception of the root node) you can use static
+    methods for obtaining child nodes, get values from properties at leaf
+    nodes and iterate over children of inner nodes.</p>
+
+    <p>In a stateful mode use non-static methods like GoToChild() to
+    navigate to children.</p>
+
+    <p>Note to call CommitChanges() after making changes to
+    PresenterConfigurationAccess object that was opened in READ_WRITE mode.</p>
 */
 class PresenterConfigurationAccess
 {
 public:
     enum WriteMode { READ_WRITE, READ_ONLY };
+    typedef ::boost::function<bool(
+        const ::rtl::OUString&,
+        const css::uno::Reference<css::beans::XPropertySet>&)> Predicate;
+    static const ::rtl::OUString msPresenterScreenRootName;
 
     /** Create a new object to access the configuration entries below the
         given root.
         @param rsRootName
-            Name of the root.
+            Name of the root.  You can use msPresenterScreenRootName to
+            access the configuration of the presenter screen.
         @param eMode
             This flag specifies whether to give read-write or read-only
             access.
@@ -65,12 +81,37 @@ public:
         const ::rtl::OUString& rsRootName,
         WriteMode eMode);
 
+    ~PresenterConfigurationAccess (void);
+
     /** Return a configuration node below the root of the called object.
         @param rsPathToNode
             The relative path from the root (as given the constructor) to the node.
     */
     css::uno::Any GetConfigurationNode (
         const ::rtl::OUString& rsPathToNode);
+    css::uno::Reference<css::beans::XPropertySet> GetNodeProperties (
+        const ::rtl::OUString& rsPathToNode);
+
+    /** Return <TRUE/> when opening the configuration (via creating a new
+        PresenterConfigurationAccess object) or previous calls to
+        GoToChild() left the called PresenterConfigurationAccess object in a
+        valid state.
+    */
+    bool IsValid (void) const;
+
+    /** Move the focused node to the (possibly indirect) child specified by the given path.
+    */
+    bool GoToChild (const ::rtl::OUString& rsPathToNode);
+
+    /** Move the focused node to the first direct child that fulfills the the given predicate.
+    */
+    bool GoToChild (const Predicate& rPredicate);
+
+    /** Modify the property child of the currently focused node.  Keep in
+        mind to call CommitChanges() to write the change back to the
+        configuration.
+    */
+    bool SetProperty (const ::rtl::OUString& rsPropertyName, const css::uno::Any& rValue);
 
     /** Return a configuration node below the given node.
         @param rxNode
@@ -86,6 +127,10 @@ public:
         const css::uno::Reference<css::container::XHierarchicalNameAccess>& rxNode,
         const ::rtl::OUString& rsPathToNode);
 
+    static css::uno::Reference<css::beans::XPropertySet> GetNodeProperties (
+        const css::uno::Reference<css::container::XHierarchicalNameAccess>& rxNode,
+        const ::rtl::OUString& rsPathToNode);
+
     /** Write any changes that have been made back to the configuration.
         This call is ignored when the called ConfigurationAccess object was
         not create with read-write mode.
@@ -97,6 +142,9 @@ public:
     typedef ::boost::function<void(
         const ::rtl::OUString&,
         const ::std::vector<css::uno::Any>&) > ItemProcessor;
+    typedef ::boost::function<void(
+        const ::rtl::OUString&,
+        const ::css::uno::Reference<css::beans::XPropertySet>&) > PropertySetProcessor;
 
     /** Execute a functor for all elements of the given container.
         @param rxContainer
@@ -114,6 +162,9 @@ public:
         const css::uno::Reference<css::container::XNameAccess>& rxContainer,
         const ::std::vector<rtl::OUString>& rArguments,
         const ItemProcessor& rProcessor);
+    static void ForAll (
+        const css::uno::Reference<css::container::XNameAccess>& rxContainer,
+        const PropertySetProcessor& rProcessor);
 
     /** Fill a list with the string contents of all sub-elements in the given container.
         @param rxContainer
@@ -126,21 +177,31 @@ public:
         @param rList
             The list to be filled.
     */
-    static void PresenterConfigurationAccess::FillList(
+    static void FillList(
         const css::uno::Reference<css::container::XNameAccess>& rxContainer,
         const ::rtl::OUString& rsArgument,
         ::std::vector<rtl::OUString>& rList);
 
-    typedef ::boost::function<bool(
-        const ::rtl::OUString&,
-        const css::uno::Reference<css::container::XNameAccess>&)> Predicate;
-
-    static css::uno::Reference<css::container::XNameAccess> Find (
+    static css::uno::Any Find (
         const css::uno::Reference<css::container::XNameAccess>& rxContainer,
         const Predicate& rPredicate);
 
+    static bool IsStringPropertyEqual (
+        const ::rtl::OUString& rsValue,
+        const ::rtl::OUString& rsPropertyName,
+        const css::uno::Reference<css::beans::XPropertySet>& rxNode);
+
+    /** This method wraps a call to getPropertyValue() and returns an empty
+        Any instead of throwing an exception when the property does not
+        exist.
+    */
+    static css::uno::Any GetProperty (
+        const css::uno::Reference<css::beans::XPropertySet>& rxProperties,
+        const ::rtl::OUString& rsKey);
+
 private:
     css::uno::Reference<css::uno::XInterface> mxRoot;
+    css::uno::Any maNode;
 };
 
 } } // end of namespace sdext::tools
