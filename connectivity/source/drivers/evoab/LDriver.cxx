030279d56862117005bbba798764e02627a83052
Vladimir Glazounov <vg@openoffice.org>
2008-05-13 12:58:28 +0000

    INTEGRATION: CWS cmcfixes44 (1.9.6); FILE MERGED 2008/04/24 08:13:50 cmc 1.9.6.1: #i88676# osl_freeProcessHandle is missing after a successful call to osl_executeProcess

--- connectivity/source/drivers/evoab/LDriver.cxx
+++ connectivity/source/drivers/evoab/LDriver.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: LDriver.cxx,v $
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -180,6 +180,7 @@ namespace
                 nError = nWaitForProcessError;
                 // TODO: kill the running process?
             }
+            osl_freeProcessHandle( hProcess );
         }
         return nError;
     }
