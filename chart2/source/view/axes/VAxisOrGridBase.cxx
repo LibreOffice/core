696c2ff283f12f524bdf9ed8ee0b54eacd5973e8
Vladimir Glazounov <vg@openoffice.org>
2008-05-15 07:03:52 +0000

    INTEGRATION: CWS chart26_DEV300 (1.2.130); FILE MERGED 2008/04/23 19:23:29 iha 1.2.130.1: #i72994# good automatic placement of walls, grid lines and axes labels dependent on 3D rotation

--- chart2/source/view/axes/VAxisOrGridBase.cxx
+++ chart2/source/view/axes/VAxisOrGridBase.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: VAxisOrGridBase.cxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -50,6 +50,9 @@ using ::com::sun::star::uno::Reference;
 VAxisOrGridBase::VAxisOrGridBase( sal_Int32 nDimensionIndex, sal_Int32 nDimensionCount )
             : PlotterBase( nDimensionCount )
             , m_nDimensionIndex( nDimensionIndex )
+            , m_eLeftWallPos(CuboidPlanePosition_Left)
+            , m_eBackWallPos(CuboidPlanePosition_Back)
+            , m_eBottomPos(CuboidPlanePosition_Bottom)
 {
 }
 
@@ -70,6 +73,13 @@ void VAxisOrGridBase::setTransformationSceneToScreen( const drawing::HomogenMatr
 {
     m_aMatrixScreenToScene = HomogenMatrixToB3DHomMatrix(rMatrix);
     PlotterBase::setTransformationSceneToScreen( rMatrix);
+}
+
+void VAxisOrGridBase::set3DWallPositions( CuboidPlanePosition eLeftWallPos, CuboidPlanePosition eBackWallPos, CuboidPlanePosition eBottomPos )
+{
+    m_eLeftWallPos = eLeftWallPos;
+    m_eBackWallPos = eBackWallPos;
+    m_eBottomPos = eBottomPos;
 }
 
 TickmarkHelper* VAxisOrGridBase::createTickmarkHelper()
