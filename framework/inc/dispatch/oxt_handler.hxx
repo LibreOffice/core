58931ea7dd144ebe04dc82dec1a337571d1db61f
Vladimir Glazounov <vg@openoffice.org>
2008-05-15 08:04:14 +0000

    INTEGRATION: CWS oxtsysint01 (1.1.2); FILE ADDED 2008/04/10 06:41:31 dv 1.1.2.1: #i85856# Office should handle '.oxt' files

---
+++ framework/inc/dispatch/oxt_handler.hxx
@@ -0,0 +1,223 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: oxt_handler.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: vg $ $Date: 2008-05-15 09:04:14 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef __FRAMEWORK_HANDLER_OXT_HANDLER_HXX_
+#define __FRAMEWORK_HANDLER_OXT_HANDLER_HXX_
+
+//_________________________________________________________________________________________________________________
+//  my own includes
+//_________________________________________________________________________________________________________________
+
+#ifndef __FRAMEWORK_THREADHELP_THREADHELPBASE_HXX_
+#include <threadhelp/threadhelpbase.hxx>
+#endif
+
+#ifndef __FRAMEWORK_THREADHELP_RESETABLEGUARD_HXX_
+#include <threadhelp/resetableguard.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XINTERFACE_HXX_
+#include <macros/xinterface.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XTYPEPROVIDER_HXX_
+#include <macros/xtypeprovider.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_XSERVICEINFO_HXX_
+#include <macros/xserviceinfo.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_DEBUG_HXX_
+#include <macros/debug.hxx>
+#endif
+
+#ifndef __FRAMEWORK_MACROS_GENERIC_HXX_
+#include <macros/generic.hxx>
+#endif
+
+#ifndef __FRAMEWORK_GENERAL_H_
+#include <general.h>
+#endif
+
+#ifndef __FRAMEWORK_STDTYPES_H_
+#include <stdtypes.h>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  interface includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _COM_SUN_STAR_LANG_XTYPEPROVIDER_HPP_
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XNOTIFYINGDISPATCH_HPP_
+#include <com/sun/star/frame/XNotifyingDispatch.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XSTATUSLISTENER_HPP_
+#include <com/sun/star/frame/XStatusListener.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_FRAME_XFRAME_HPP_
+#include <com/sun/star/frame/XFrame.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_DOCUMENT_XEXTENDEDFILTERDETECTION_HPP_
+#include <com/sun/star/document/XExtendedFilterDetection.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+
+#ifndef _COM_SUN_STAR_UTIL_URL_HPP_
+#include <com/sun/star/util/URL.hpp>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  other includes
+//_________________________________________________________________________________________________________________
+
+#ifndef _CPPUHELPER_WEAK_HXX_
+#include <cppuhelper/weak.hxx>
+#endif
+
+//_________________________________________________________________________________________________________________
+//  namespace
+//_________________________________________________________________________________________________________________
+
+namespace framework{
+
+//_________________________________________________________________________________________________________________
+//  exported const
+//_________________________________________________________________________________________________________________
+
+//_________________________________________________________________________________________________________________
+//  exported definitions
+//_________________________________________________________________________________________________________________
+
+/*-************************************************************************************************************//**
+    @short          handler to detect and play sounds ("wav" and "au" only!)
+    @descr          Register this implementation as a content handler to detect and/or play wav- and au-sounds.
+                    It doesn't depend from the target platform. But one instance of this class
+                    can play one sound at the same time only. Means every new dispatch request will stop the
+                    might still running one. So we support one operation/one URL/one listener at the same time
+                    only.
+
+    @devstatus      ready
+    @threadsafe     yes
+*//*-*************************************************************************************************************/
+class Oxt_Handler  :   // interfaces
+                        public  css::lang::XTypeProvider
+                    ,   public  css::lang::XServiceInfo
+                    ,   public  css::frame::XNotifyingDispatch // => XDispatch
+                    ,   public  css::document::XExtendedFilterDetection
+                        // baseclasses
+                        // Order is neccessary for right initialization!
+                    ,   private ThreadHelpBase
+                    ,   public  ::cppu::OWeakObject
+{
+    //-------------------------------------------------------------------------------------------------------------
+    //  public methods
+    //-------------------------------------------------------------------------------------------------------------
+    public:
+
+        //---------------------------------------------------------------------------------------------------------
+        //  constructor / destructor
+        //---------------------------------------------------------------------------------------------------------
+                 Oxt_Handler( const css::uno::Reference< css::lang::XMultiServiceFactory >& xFactory );
+        virtual ~Oxt_Handler(                                                                        );
+
+        //---------------------------------------------------------------------------------------------------------
+        //  XInterface, XTypeProvider, XServiceInfo
+        //---------------------------------------------------------------------------------------------------------
+        FWK_DECLARE_XINTERFACE
+        FWK_DECLARE_XTYPEPROVIDER
+            DECLARE_XSERVICEINFO
+
+        //---------------------------------------------------------------------------------------------------------
+        //  XNotifyingDispatch
+        //---------------------------------------------------------------------------------------------------------
+        virtual void SAL_CALL dispatchWithNotification(const css::util::URL&                                             aURL      ,
+                                                       const css::uno::Sequence< css::beans::PropertyValue >&            lArguments,
+                                                       const css::uno::Reference< css::frame::XDispatchResultListener >& xListener ) throw( css::uno::RuntimeException );
+
+        //---------------------------------------------------------------------------------------------------------
+        //  XDispatch
+        //---------------------------------------------------------------------------------------------------------
+        virtual void SAL_CALL dispatch              (  const css::util::URL&                                     aURL        ,
+                                                       const css::uno::Sequence< css::beans::PropertyValue >&    lArguments  ) throw( css::uno::RuntimeException );
+        // not supported !
+        virtual void SAL_CALL addStatusListener     (  const css::uno::Reference< css::frame::XStatusListener >& /*xListener*/   ,
+                                                       const css::util::URL&                                     /*aURL*/        ) throw( css::uno::RuntimeException ) {};
+        virtual void SAL_CALL removeStatusListener  (  const css::uno::Reference< css::frame::XStatusListener >& /*xListener*/   ,
+                                                       const css::util::URL&                                     /*aURL*/        ) throw( css::uno::RuntimeException ) {};
+
+        //---------------------------------------------------------------------------------------------------------
+        //  XExtendedFilterDetection
+        //---------------------------------------------------------------------------------------------------------
+        virtual ::rtl::OUString SAL_CALL detect     (        css::uno::Sequence< css::beans::PropertyValue >&    lDescriptor ) throw( css::uno::RuntimeException );
+
+    //-------------------------------------------------------------------------------------------------------------
+    //  protected methods
+    //-------------------------------------------------------------------------------------------------------------
+    protected:
+
+    //-------------------------------------------------------------------------------------------------------------
+    //  private methods
+    //-------------------------------------------------------------------------------------------------------------
+    private:
+
+    //-------------------------------------------------------------------------------------------------------------
+    //  variables
+    //  (should be private everyway!)
+    //-------------------------------------------------------------------------------------------------------------
+    private:
+
+        css::uno::Reference< css::lang::XMultiServiceFactory >     m_xFactory          ;   /// global uno service factory to create new services
+        css::uno::Reference< css::uno::XInterface >                m_xSelfHold         ;   /// we must protect us against dieing during async(!) dispatch() call!
+        css::uno::Reference< css::frame::XDispatchResultListener > m_xListener         ;
+
+};      //  class Oxt_Handler
+
+}       //  namespace framework
+
+#endif  //  #ifndef __FRAMEWORK_HANDLER_OXT_HANDLER_HXX_
