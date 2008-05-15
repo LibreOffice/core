0dba7844290028c68d0172f4b909a6bd4d5be94a
Vladimir Glazounov <vg@openoffice.org>
2008-05-15 06:59:36 +0000

    INTEGRATION: CWS chart26_DEV300 (1.8.16); FILE MERGED 2008/04/08 15:40:06 iha 1.8.16.1: #i72994# simpler access to 3D settings

--- offapi/com/sun/star/chart/makefile.mk
+++ offapi/com/sun/star/chart/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.11 $
+# $Revision: 1.12 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -92,6 +92,7 @@ IDLFILES=\
     PieDiagram.idl\
     StackableDiagram.idl\
     StockDiagram.idl\
+    X3DDefaultSetter.idl\
     X3DDisplay.idl\
     XAxisXSupplier.idl\
     XAxisYSupplier.idl\
