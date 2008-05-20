332d869eb134d903e00321a523bd6bc63d8f5754
Kurt Zenker <kz@openoffice.org>
2008-05-20 12:10:24 +0000

    INTEGRATION: CWS koheidatapilot01 (1.12.202); FILE MERGED 2008/04/25 21:00:34 kohei 1.12.202.7: RESYNC: (1.12-1.13); FILE MERGED 2008/04/24 23:26:45 kohei 1.12.202.6: * fixed a regression on page field filtering by the empty string value. * fixed an incorrect drill-down table with number groups (i88531). * moved the shared string storage out of ScDPCacheTable to make it more generic. 2008/04/21 03:07:58 kohei 1.12.202.5: code cleanup. 2008/04/17 13:11:10 kohei 1.12.202.4: Used ScDPItemData to store cell values to allow numerical comparison where appropriate.  Also fixed the regression involving drill-down on group names. 2007/12/06 07:06:15 kohei 1.12.202.3: removed all previously invisible code. 2007/11/29 19:34:41 kohei 1.12.202.2: fixed the regression where table didn't rebuild cache table when it's refreshed. 2007/10/29 17:44:40 kohei 1.12.202.1: initial checkin

--- sc/source/core/data/dpsdbtab.cxx
+++ sc/source/core/data/dpsdbtab.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dpsdbtab.cxx,v $
- * $Revision: 1.13 $
+ * $Revision: 1.14 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -50,13 +50,24 @@
 #include <com/sun/star/sdbc/XRowSet.hpp>
 #include <com/sun/star/sdbc/XResultSetMetaDataSupplier.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <com/sun/star/sheet/DataPilotFieldFilter.hpp>
 
 #include "dpsdbtab.hxx"
 #include "collect.hxx"
 #include "global.hxx"
 #include "globstr.hrc"
+#include "dpcachetable.hxx"
+#include "dptabres.hxx"
 
 using namespace com::sun::star;
+
+using ::std::vector;
+using ::std::set;
+using ::std::hash_map;
+using ::com::sun::star::uno::Sequence;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::Any;
+using ::com::sun::star::uno::UNO_QUERY;
 
 #define SC_SERVICE_ROWSET           "com.sun.star.sdb.RowSet"
 #define SC_SERVICE_INTHANDLER       "com.sun.star.sdb.InteractionHandler"
@@ -74,13 +85,11 @@ public:
     ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > xServiceManager;
     ScImportSourceDesc  aDesc;
     long                nColCount;
-    BOOL                bValid;
-    BOOL                bAtStart;
-    String*             pTitles;
-    TypedStrCollection** ppStrings;
     uno::Reference<sdbc::XRowSet> xRowSet;
     sal_Int32*          pTypes;
     SvNumberFormatter*  pFormatter;
+
+    ScDPCacheTable      aCacheTable;
 
     ScDatabaseDPData_Impl() {}
 };
@@ -95,14 +104,11 @@ ScDatabaseDPData::ScDatabaseDPData(
     pImpl->xServiceManager = xSMgr;
     pImpl->aDesc = rImport;
     pImpl->nColCount = 0;
-    pImpl->bValid = FALSE;
-    pImpl->bAtStart = FALSE;
-    pImpl->pTitles = NULL;
-    pImpl->ppStrings = NULL;
     pImpl->pTypes = NULL;
     pImpl->pFormatter = NULL;       // created on demand
 
     OpenDatabase();
+    CreateCacheTable();
 }
 
 ScDatabaseDPData::~ScDatabaseDPData()
@@ -110,13 +116,6 @@ ScDatabaseDPData::~ScDatabaseDPData()
     ::comphelper::disposeComponent( pImpl->xRowSet );
 
     delete[] pImpl->pTypes;
-    if ( pImpl->ppStrings )
-    {
-        for (long i=0; i<pImpl->nColCount; i++)
-            delete pImpl->ppStrings[i];
-        delete[] pImpl->ppStrings;
-    }
-    delete[] pImpl->pTitles;
     delete pImpl->pFormatter;       // NumberFormatter is local for this object
     delete pImpl;
 }
@@ -124,21 +123,7 @@ ScDatabaseDPData::~ScDatabaseDPData()
 void ScDatabaseDPData::DisposeData()
 {
     //! use OpenDatabase here?
-
-    //! column titles ???
-
-    //  collections for column entries
-    if ( pImpl->ppStrings )
-    {
-        for (long i=0; i<pImpl->nColCount; i++)
-        {
-            delete pImpl->ppStrings[i];
-            pImpl->ppStrings[i] = NULL;
-        }
-    }
-
-    //! init entries on demand!
-    InitAllColumnEntries();             //! configurable ???
+    pImpl->aCacheTable.clear();
 }
 
 BOOL ScDatabaseDPData::OpenDatabase()
@@ -194,8 +179,6 @@ BOOL ScDatabaseDPData::OpenDatabase()
             else
                 pImpl->xRowSet->execute();
 
-            pImpl->bAtStart = TRUE;
-
             //
             //  get column descriptions
             //
@@ -211,22 +194,9 @@ BOOL ScDatabaseDPData::OpenDatabase()
             uno::Reference<sdbc::XResultSet> xResSet( pImpl->xRowSet, uno::UNO_QUERY );
             if ( pImpl->nColCount > 0 && xResSet.is() )
             {
-                //  get column titles
-                pImpl->pTitles = new String[pImpl->nColCount];
                 pImpl->pTypes = new sal_Int32[pImpl->nColCount];
                 for (long nCol=0; nCol<pImpl->nColCount; nCol++)
-                {
-                    pImpl->pTitles[nCol] = xMeta->getColumnLabel( nCol+1 );
                     pImpl->pTypes[nCol]  = xMeta->getColumnType( nCol+1 );
-                }
-
-                //  collections for column entries
-                pImpl->ppStrings = new TypedStrCollection*[pImpl->nColCount];
-                for (long i=0; i<pImpl->nColCount; i++)
-                    pImpl->ppStrings[i] = NULL;
-
-                //! init entries on demand!
-                InitAllColumnEntries();             //! configurable ???
 
                 bSuccess = TRUE;
             }
@@ -247,110 +217,12 @@ BOOL ScDatabaseDPData::OpenDatabase()
     if (!bSuccess)
         ::comphelper::disposeComponent( pImpl->xRowSet );
 
-    pImpl->bValid = bSuccess;
     return bSuccess;
 }
 
 long ScDatabaseDPData::GetColumnCount()
 {
     return pImpl->nColCount;
-}
-
-void lcl_FillItemData( ScDPItemData& rData,
-                        const uno::Reference<sdbc::XRow>& xRow, long nRowPos,
-                        long nType, BOOL bStringForVal, ScDatabaseDPData_Impl* pImpl )
-{
-    //! merge with lcl_PutData in dbdocimp.cxx
-
-    rData.aString.Erase();
-    rData.fValue = 0.0;
-    rData.bHasValue = FALSE;
-    short nNumType = NUMBERFORMAT_NUMBER;
-    BOOL bEmptyFlag = FALSE;
-
-    if (!xRow.is())
-        return;
-
-    try
-    {
-        switch ( nType )
-        {
-            case sdbc::DataType::BIT:
-            case sdbc::DataType::BOOLEAN:
-                nNumType = NUMBERFORMAT_LOGICAL;
-                rData.fValue = (xRow->getBoolean(nRowPos) ? 1 : 0);
-                bEmptyFlag = ( rData.fValue == 0.0 ) && xRow->wasNull();
-                rData.bHasValue = TRUE;
-                break;
-
-            case sdbc::DataType::TINYINT:
-            case sdbc::DataType::SMALLINT:
-            case sdbc::DataType::INTEGER:
-            case sdbc::DataType::BIGINT:
-            case sdbc::DataType::FLOAT:
-            case sdbc::DataType::REAL:
-            case sdbc::DataType::DOUBLE:
-            case sdbc::DataType::NUMERIC:
-            case sdbc::DataType::DECIMAL:
-                //! do the conversion here?
-                rData.fValue = xRow->getDouble(nRowPos);
-                bEmptyFlag = ( rData.fValue == 0.0 ) && xRow->wasNull();
-                rData.bHasValue = TRUE;
-                break;
-
-            case sdbc::DataType::CHAR:
-            case sdbc::DataType::VARCHAR:
-            case sdbc::DataType::LONGVARCHAR:
-                rData.aString = xRow->getString(nRowPos);
-                bEmptyFlag = ( rData.aString.Len() == 0 ) && xRow->wasNull();
-                break;
-
-            case sdbc::DataType::DATE:
-                {
-                    nNumType = NUMBERFORMAT_DATE;
-
-                    if (!pImpl->pFormatter)
-                        pImpl->pFormatter = new SvNumberFormatter( pImpl->xServiceManager, ScGlobal::eLnge );
-
-                    util::Date aDate = xRow->getDate(nRowPos);
-                    rData.fValue = Date( aDate.Day, aDate.Month, aDate.Year ) -
-                                                *pImpl->pFormatter->GetNullDate();
-                    bEmptyFlag = xRow->wasNull();
-                    rData.bHasValue = TRUE;
-                }
-                break;
-
-            //! case sdbc::DataType::TIME:
-            //! case sdbc::DataType::TIMESTAMP:
-
-            case sdbc::DataType::SQLNULL:
-                //bEmptyFlag = TRUE;
-                break;
-
-            case sdbc::DataType::BINARY:
-            case sdbc::DataType::VARBINARY:
-            case sdbc::DataType::LONGVARBINARY:
-            default:
-                //bError = TRUE;        // unknown type
-                break;
-        }
-    }
-    catch ( uno::Exception& )
-    {
-        //bError = TRUE;
-    }
-
-    if ( bEmptyFlag )
-        rData.bHasValue = FALSE;        // empty -> empty string
-
-    if ( rData.bHasValue && bStringForVal )
-    {
-        if (!pImpl->pFormatter)
-            pImpl->pFormatter = new SvNumberFormatter( pImpl->xServiceManager, ScGlobal::eLnge );
-
-        ULONG nIndex = pImpl->pFormatter->GetStandardFormat( nNumType, ScGlobal::eLnge );
-        pImpl->pFormatter->GetInputLineString( rData.fValue, nIndex, rData.aString );
-    }
 }
 
 void lcl_Reset( const uno::Reference<sdbc::XRowSet>& xRowSet )
@@ -362,104 +234,10 @@ void lcl_Reset( const uno::Reference<sdbc::XRowSet>& xRowSet )
     xRowSet->execute();     // restart
 }
 
-void ScDatabaseDPData::InitAllColumnEntries()
-{
-    DBG_ASSERT( pImpl->ppStrings, "GetColumnEntries: no entries" );
-    long nCol;
-    for ( nCol=0; nCol<pImpl->nColCount; nCol++ )
-        if (!pImpl->ppStrings[nCol])
-            pImpl->ppStrings[nCol] = new TypedStrCollection;
-
-//  Sound::Beep();      //! Test !!!
-
-    uno::Reference<sdbc::XRowSet> xRowSet = pImpl->xRowSet;
-    uno::Reference<sdbc::XRow> xRow( xRowSet, uno::UNO_QUERY );
-    if ( xRow.is() )
-    {
-        ScDPItemData aItemData;
-
-        try
-        {
-            if ( !pImpl->bAtStart )
-                lcl_Reset( xRowSet );
-
-            pImpl->bAtStart = FALSE;
-            while ( xRowSet->next() )
-            {
-                for ( nCol=0; nCol<pImpl->nColCount; nCol++ )
-                {
-                    //! get string for value data only if value hasn't been inserted yet
-                    lcl_FillItemData( aItemData, xRow, nCol+1, pImpl->pTypes[nCol], TRUE, pImpl );
-                    TypedStrData* pNew = new TypedStrData(
-                            aItemData.aString, aItemData.fValue,
-                            aItemData.bHasValue ? SC_STRTYPE_VALUE : SC_STRTYPE_STANDARD );
-                    if (!pImpl->ppStrings[nCol]->Insert(pNew))
-                        delete pNew;
-                }
-            }
-        }
-        catch ( sdbc::SQLException& rError )
-        {
-            //! store error message
-            InfoBox aInfoBox( 0, String(rError.Message) );
-            aInfoBox.Execute();
-        }
-        catch ( uno::Exception& )
-        {
-            DBG_ERROR("Unexpected exception in database");
-        }
-    }
-}
-
 const TypedStrCollection& ScDatabaseDPData::GetColumnEntries(long nColumn)
 {
-    DBG_ASSERT( pImpl->ppStrings && nColumn < pImpl->nColCount, "GetColumnEntries: no entries" );
-    if (!pImpl->ppStrings[nColumn])
-    {
-        TypedStrCollection* pColl = new TypedStrCollection;
-
-        //! select distinct values directly from database
-
-//      Sound::Beep();      //! Test !!!
-
-        uno::Reference<sdbc::XRowSet> xRowSet = pImpl->xRowSet;
-        uno::Reference<sdbc::XRow> xRow( xRowSet, uno::UNO_QUERY );
-        if ( xRow.is() )
-        {
-            ScDPItemData aItemData;
-
-            try
-            {
-                if ( !pImpl->bAtStart )
-                    lcl_Reset( xRowSet );
-
-                pImpl->bAtStart = FALSE;
-                while ( xRowSet->next() )
-                {
-                    //! get string for value data only if value hasn't been inserted yet
-                    lcl_FillItemData( aItemData, xRow, nColumn+1, pImpl->pTypes[nColumn], TRUE, pImpl );
-                    TypedStrData* pNew = new TypedStrData(
-                            aItemData.aString, aItemData.fValue,
-                            aItemData.bHasValue ? SC_STRTYPE_VALUE : SC_STRTYPE_STANDARD );
-                    if (!pColl->Insert(pNew))
-                        delete pNew;
-                }
-            }
-            catch ( sdbc::SQLException& rError )
-            {
-                //! store error message
-                InfoBox aInfoBox( 0, String(rError.Message) );
-                aInfoBox.Execute();
-            }
-            catch ( uno::Exception& )
-            {
-                DBG_ERROR("Unexpected exception in database");
-            }
-        }
-
-        pImpl->ppStrings[nColumn] = pColl;
-    }
-    return *pImpl->ppStrings[nColumn];
+    CreateCacheTable();
+    return pImpl->aCacheTable.getFieldEntries(nColumn);
 }
 
 String ScDatabaseDPData::getDimensionName(long nColumn)
@@ -470,10 +248,12 @@ String ScDatabaseDPData::getDimensionName(long nColumn)
         //return "Data";
         return ScGlobal::GetRscString(STR_PIVOT_DATA);
     }
-    else if ( pImpl->pTitles && nColumn < pImpl->nColCount )
-    {
-        return pImpl->pTitles[nColumn];
-    }
+
+    CreateCacheTable();
+    const String* pStr = pImpl->aCacheTable.getFieldName(nColumn);
+    if (pStr)
+        return *pStr;
+
     DBG_ERROR("getDimensionName: invalid dimension");
     return String();
 }
@@ -495,106 +275,43 @@ void ScDatabaseDPData::SetEmptyFlags( BOOL /* bIgnoreEmptyRows */, BOOL /* bRepe
     //! disable flags
 }
 
-void ScDatabaseDPData::ResetIterator()
+void ScDatabaseDPData::CreateCacheTable()
 {
-    try
-    {
-        uno::Reference<sdbc::XRowSet> xRowSet = pImpl->xRowSet;
-        if ( xRowSet.is() && !pImpl->bAtStart )
-        {
-            lcl_Reset( xRowSet );
-            pImpl->bAtStart = TRUE;
-        }
-    }
-    catch ( sdbc::SQLException& rError )
-    {
-        //! store error message
-        InfoBox aInfoBox( 0, String(rError.Message) );
-        aInfoBox.Execute();
-    }
-    catch ( uno::Exception& )
-    {
-        DBG_ERROR("Unexpected exception in database");
-    }
+    if (!pImpl->aCacheTable.empty())
+        return;
 
-//  Sound::Beep();      //! Test !!!
+    // Get null date.
+    if (!pImpl->pFormatter)
+        pImpl->pFormatter = new SvNumberFormatter(pImpl->xServiceManager, ScGlobal::eLnge);
+
+    pImpl->aCacheTable.fillTable(pImpl->xRowSet, *pImpl->pFormatter->GetNullDate());
 }
 
-BOOL ScDatabaseDPData::GetNextRow( const ScDPTableIteratorParam& rParam )
+void ScDatabaseDPData::FilterCacheTable(const vector<ScDPDimension*>& rPageDims)
 {
-    BOOL bSuccess = FALSE;
-    uno::Reference<sdbc::XRowSet> xRowSet = pImpl->xRowSet;
-    uno::Reference<sdbc::XRow> xRow( xRowSet, uno::UNO_QUERY );
+    CreateCacheTable();
+    pImpl->aCacheTable.filterByPageDimension(rPageDims);
+}
 
-    try
-    {
-        pImpl->bAtStart = FALSE;
-        if ( xRow.is() && xRowSet->next() )
-        {
-            SCSIZE i;
+void ScDatabaseDPData::GetDrillDownData(const vector<ScDPCacheTable::Criterion>& rCriteria, Sequence< Sequence<Any> >& rData)
+{
+    CreateCacheTable();
+    sal_Int32 nRowSize = pImpl->aCacheTable.getRowSize();
+    if (!nRowSize)
+        return;
 
-            for (i=0; i<rParam.nColCount; i++)
-            {
-                long nDim = rParam.pCols[i];
-                if ( getIsDataLayoutDimension(nDim) )
-                    rParam.pColData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-                else
-                    lcl_FillItemData( rParam.pColData[i], xRow, nDim+1, pImpl->pTypes[nDim], FALSE, pImpl );
-            }
+    pImpl->aCacheTable.filterTable(rCriteria, rData, IsRepeatIfEmpty());
+}
 
-            for (i=0; i<rParam.nRowCount; i++)
-            {
-                long nDim = rParam.pRows[i];
-                if ( getIsDataLayoutDimension(nDim) )
-                    rParam.pRowData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-                else
-                    lcl_FillItemData( rParam.pRowData[i], xRow, nDim+1, pImpl->pTypes[nDim], FALSE, pImpl );
-            }
+void ScDatabaseDPData::CalcResults(CalcInfo& rInfo, bool bAutoShow)
+{
+    CreateCacheTable();
+    CalcResultsFromCacheTable(pImpl->aCacheTable, rInfo, bAutoShow);
+}
 
-            for (i=0; i<rParam.nPageCount; i++)
-            {
-                long nDim = rParam.pPages[i];
-                if ( getIsDataLayoutDimension(nDim) )
-                    rParam.pPageData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-                else
-                    lcl_FillItemData( rParam.pPageData[i], xRow, nDim+1, pImpl->pTypes[nDim], FALSE, pImpl );
-            }
-
-            for (i=0; i<rParam.nDatCount; i++)
-            {
-                //! merge this with lcl_FillItemData, distinguish all SC_VALTYPE_... types
-
-                long nDim = rParam.pDats[i];
-
-                double fVal = 0.0;
-                BYTE nType = SC_VALTYPE_EMPTY;
-                try
-                {
-                    fVal = xRow->getDouble( nDim+1 );
-                }
-                catch ( uno::Exception& )
-                {
-                    //  anything that can't be converted to a number is a string
-                    nType = SC_VALTYPE_STRING;
-                }
-                rParam.pValues[i].Set( fVal, SC_VALTYPE_VALUE );
-            }
-
-            bSuccess = TRUE;
-        }
-    }
-    catch ( sdbc::SQLException& rError )
-    {
-        //! store error message
-        InfoBox aInfoBox( 0, String(rError.Message) );
-        aInfoBox.Execute();
-    }
-    catch ( uno::Exception& )
-    {
-        DBG_ERROR("Unexpected exception in database");
-    }
-
-    return bSuccess;
+const ScDPCacheTable& ScDatabaseDPData::GetCacheTable() const
+{
+    return pImpl->aCacheTable;
 }
 
 // -----------------------------------------------------------------------
