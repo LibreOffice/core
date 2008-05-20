651d16d30afc4b3f1d57f521b7919813f5d93781
Kurt Zenker <kz@openoffice.org>
2008-05-20 13:32:24 +0000

    INTEGRATION: CWS fwk89_DEV300 (1.34.6.2.8); FILE MERGED 2008/04/16 15:47:07 cd 1.34.6.2.8.1: #i87028# Fixed duplicate BASEPROPERTY IDs

--- toolkit/inc/toolkit/helper/property.hxx
+++ toolkit/inc/toolkit/helper/property.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: property.hxx,v $
- * $Revision: 1.37 $
+ * $Revision: 1.38 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -48,136 +48,131 @@ namespace rtl {
 }
 
 
-#define BASEPROPERTY_NOTFOUND                0
+#define BASEPROPERTY_NOTFOUND                        0
 
-#define BASEPROPERTY_TEXT                    1  // ::rtl::OUString
-#define BASEPROPERTY_BACKGROUNDCOLOR         2  // sal_Int32
-#define BASEPROPERTY_FILLCOLOR               3  // sal_Int32
-#define BASEPROPERTY_TEXTCOLOR               4  // sal_Int32
-#define BASEPROPERTY_LINECOLOR               5  // sal_Int32
-#define BASEPROPERTY_BORDER                  6  // sal_Int16
-#define BASEPROPERTY_ALIGN                   7  // sal_Int16
-#define BASEPROPERTY_FONTDESCRIPTOR          8  // ::com::sun::star::awt::FontDescriptor, war mal Font, aber nie gespeichert...
-#define BASEPROPERTY_DROPDOWN                9  // sal_Bool
-#define BASEPROPERTY_MULTILINE              10  // sal_Bool
-#define BASEPROPERTY_STRINGITEMLIST         11  // UStringSequence
-#define BASEPROPERTY_HSCROLL                12  // sal_Bool
-#define BASEPROPERTY_VSCROLL                13  // sal_Bool
-#define BASEPROPERTY_TABSTOP                14  // sal_Bool
-#define BASEPROPERTY_STATE                  15  // sal_Int16
-#define BASEPROPERTY_FONT_TYPE              16  // OLD: Font_Type
-#define BASEPROPERTY_FONT_SIZE              17  // OLD: Font_Size
-#define BASEPROPERTY_FONT_ATTRIBS           18  // OLD: Font_Attribs
-#define BASEPROPERTY_DEFAULTCONTROL         19  // ::rtl::OUString (ServiceName)
-#define BASEPROPERTY_LABEL                  20  // ::rtl::OUString
-#define BASEPROPERTY_LINECOUNT              21  // sal_Int16
-#define BASEPROPERTY_EXTDATEFORMAT          22  // sal_Int16
-#define BASEPROPERTY_DATESHOWCENTURY        23  // sal_Bool
-#define BASEPROPERTY_EXTTIMEFORMAT          24  // sal_Int16
-#define BASEPROPERTY_NUMSHOWTHOUSANDSEP     25  // sal_Bool
-#define BASEPROPERTY_CURRENCYSYMBOL         26  // ::rtl::OUString
-#define BASEPROPERTY_SPIN                   27  // sal_Bool
-#define BASEPROPERTY_STRICTFORMAT           28  // sal_Bool
-#define BASEPROPERTY_DECIMALACCURACY        29  // sal_Int16
-#define BASEPROPERTY_DATE                   30  // sal_Int32
-#define BASEPROPERTY_DATEMIN                31  // sal_Int32
-#define BASEPROPERTY_DATEMAX                32  // sal_Int32
-#define BASEPROPERTY_TIME                   33  // sal_Int32
-#define BASEPROPERTY_TIMEMIN                34  // sal_Int32
-#define BASEPROPERTY_TIMEMAX                35  // sal_Int32
-#define BASEPROPERTY_VALUE_INT32            36  // sal_Int32
-#define BASEPROPERTY_VALUEMIN_INT32         37  // sal_Int32
-#define BASEPROPERTY_VALUEMAX_INT32         38  // sal_Int32
-#define BASEPROPERTY_VALUESTEP_INT32        39  // sal_Int32
-#define BASEPROPERTY_EDITMASK               40  // ::rtl::OUString
-#define BASEPROPERTY_LITERALMASK            41  // ::rtl::OUString
-#define BASEPROPERTY_IMAGEURL               42  // ::rtl::OUString
-#define BASEPROPERTY_READONLY               43  // sal_Bool
-#define BASEPROPERTY_ENABLED                44  // sal_Bool
-#define BASEPROPERTY_PRINTABLE              45  // sal_Bool
-#define BASEPROPERTY_ECHOCHAR               46  // sal_Int16
-#define BASEPROPERTY_MAXTEXTLEN             47  // sal_Int16
-#define BASEPROPERTY_HARDLINEBREAKS         48  // sal_Int16
-#define BASEPROPERTY_AUTOCOMPLETE           49  // sal_Bool
-#define BASEPROPERTY_MULTISELECTION         50  // sal_Bool
-#define BASEPROPERTY_SELECTEDITEMS          51  // INT16Sequence
-#define BASEPROPERTY_VALUE_DOUBLE           52  // DOUBLE
-#define BASEPROPERTY_VALUEMIN_DOUBLE        53  // DOUBLE
-#define BASEPROPERTY_VALUEMAX_DOUBLE        54  // DOUBLE
-#define BASEPROPERTY_VALUESTEP_DOUBLE       55  // DOUBLE
-#define BASEPROPERTY_TRISTATE               56  // sal_Bool
-#define BASEPROPERTY_DEFAULTBUTTON          57  // sal_Bool
-#define BASEPROPERTY_HELPURL                58  // ::rtl::OUString
-#define BASEPROPERTY_AUTOTOGGLE             59  // sal_Bool
-//#define BASEPROPERTY_FOCUSSELECTIONHIDE       60  // sal_Bool
-#define BASEPROPERTY_FORMATKEY              61  // sal_Bool
-#define BASEPROPERTY_FORMATSSUPPLIER        62  // ::com::sun::star::util::XNumberFormatsSupplier
-#define BASEPROPERTY_EFFECTIVE_VALUE        63  // Any (double or string)
-#define BASEPROPERTY_TREATASNUMBER          64  // sal_Bool
-#define BASEPROPERTY_EFFECTIVE_DEFAULT      65  // Any (double or string)
-#define BASEPROPERTY_EFFECTIVE_MIN          66  // Double
-#define BASEPROPERTY_EFFECTIVE_MAX          67  // Double
-#define BASEPROPERTY_CURSYM_POSITION        68  // sal_Bool
-#define BASEPROPERTY_TITLE                  69  // ::rtl::OUString
-#define BASEPROPERTY_MOVEABLE               70  // sal_Bool
-#define BASEPROPERTY_CLOSEABLE              71  // sal_Bool
-#define BASEPROPERTY_SIZEABLE               72  // sal_Bool
-#define BASEPROPERTY_HELPTEXT               73  // ::rtl::OUString
-#define BASEPROPERTY_PROGRESSVALUE          74  // sal_Int32
-#define BASEPROPERTY_PROGRESSVALUE_MIN      75  // sal_Int32
-#define BASEPROPERTY_PROGRESSVALUE_MAX      76  // sal_Int32
-#define BASEPROPERTY_SCROLLVALUE            77  // sal_Int32
-#define BASEPROPERTY_SCROLLVALUE_MAX        78  // sal_Int32
-#define BASEPROPERTY_LINEINCREMENT          79  // sal_Int32
-#define BASEPROPERTY_BLOCKINCREMENT         80  // sal_Int32
-#define BASEPROPERTY_VISIBLESIZE            81  // sal_Int32
-#define BASEPROPERTY_ORIENTATION            82  // sal_Int32
-#define BASEPROPERTY_FONTRELIEF             83  // sal_Int16
-#define BASEPROPERTY_FONTEMPHASISMARK       84  // sal_Int16
-#define BASEPROPERTY_TEXTLINECOLOR          85  // sal_Int32
-#define BASEPROPERTY_IMAGEALIGN             86  // sal_Int16
-#define BASEPROPERTY_SCALEIMAGE             87  // sal_Bool
-#define BASEPROPERTY_PUSHBUTTONTYPE         88  // sal_Int16
-#define BASEPROPERTY_DISPLAYBACKGROUNDCOLOR 89  // sal_Int32
-#define BASEPROPERTY_AUTOMNEMONICS          90  // sal_Bool
-#define BASEPROPERTY_MOUSETRANSPARENT       91  // sal_Bool
-#define BASEPROPERTY_ACCESSIBLENAME         92  // ::rtl::OUString
-#define BASEPROPERTY_PLUGINPARENT           93  // sal_Int64
-#define BASEPROPERTY_SCROLLVALUE_MIN        94  // sal_Int32
-#define BASEPROPERTY_REPEAT_DELAY           95  // sal_Int32
-#define BASEPROPERTY_SYMBOL_COLOR           96  // sal_Int32
-#define BASEPROPERTY_SPINVALUE              97  // sal_Int32
-#define BASEPROPERTY_SPINVALUE_MIN          98  // sal_Int32
-#define BASEPROPERTY_SPINVALUE_MAX          99  // sal_Int32
-#define BASEPROPERTY_SPININCREMENT         100  // sal_Int32
-#define BASEPROPERTY_REPEAT                101  // sal_Bool
-#define BASEPROPERTY_ENFORCE_FORMAT        102  // sal_Bool
-#define BASEPROPERTY_LIVE_SCROLL           103  // sal_Bool
-#define BASEPROPERTY_LINE_END_FORMAT       104  // sal_Int16
-#define BASEPROPERTY_ACTIVATED            105  // sal Bool
-#define BASEPROPERTY_COMPLETE             106  // sal_Bool
-#define BASEPROPERTY_CURRENTITEMID            107  // sal_Int16
-#define BASEPROPERTY_TOGGLE                108  // sal_Bool
-#define BASEPROPERTY_FOCUSONCLICK          109  // sal_Bool
-#define BASEPROPERTY_HIDEINACTIVESELECTION 110  // sal_Bool
-#define BASEPROPERTY_VISUALEFFECT          111  // sal_Int16
-#define BASEPROPERTY_BORDERCOLOR           112  // sal_Int32
-#define BASEPROPERTY_IMAGEPOSITION         113  // sal_Int16
-#define BASEPROPERTY_NATIVE_WIDGET_LOOK    114  // sal_Bool
-#define BASEPROPERTY_VERTICALALIGN         115  // VerticalAlignment
-#define BASEPROPERTY_WHEELWITHOUTFOCUS     116  // sal_Bool
-#define BASEPROPERTY_GRAPHIC               117  // css.graphic.XGraphic
-#define BASEPROPERTY_STEP_TIME             118  // sal_Int32
-#define BASEPROPERTY_DECORATION            119  // sal_Bool
-#define BASEPROPERTY_PAINTTRANSPARENT      120  // sal_Bool
-#define BASEPROPERTY_AUTOHSCROLL           121  // sal_Bool
-#define BASEPROPERTY_AUTOVSCROLL           122  // sal_Bool
-#define BASEPROPERTY_DESKTOP_AS_PARENT     123  // sal_Bool
-#define BASEPROPERTY_UNIT                  124  // ::awt::FieldUnit
-#define BASEPROPERTY_CUSTOMUNITTEXT        125  // ::rtl::OUString
-
-//#define BASEPROPERTY_RESOURCERESOLVER      124  // css.resource.XStringResourceResolver
-
+#define BASEPROPERTY_TEXT                            1  // ::rtl::OUString
+#define BASEPROPERTY_BACKGROUNDCOLOR                 2  // sal_Int32
+#define BASEPROPERTY_FILLCOLOR                       3  // sal_Int32
+#define BASEPROPERTY_TEXTCOLOR                       4  // sal_Int32
+#define BASEPROPERTY_LINECOLOR                       5  // sal_Int32
+#define BASEPROPERTY_BORDER                          6  // sal_Int16
+#define BASEPROPERTY_ALIGN                           7  // sal_Int16
+#define BASEPROPERTY_FONTDESCRIPTOR                  8  // ::com::sun::star::awt::FontDescriptor, war mal Font, aber nie gespeichert...
+#define BASEPROPERTY_DROPDOWN                        9  // sal_Bool
+#define BASEPROPERTY_MULTILINE                       10 // sal_Bool
+#define BASEPROPERTY_STRINGITEMLIST                  11 // UStringSequence
+#define BASEPROPERTY_HSCROLL                         12 // sal_Bool
+#define BASEPROPERTY_VSCROLL                         13 // sal_Bool
+#define BASEPROPERTY_TABSTOP                         14 // sal_Bool
+#define BASEPROPERTY_STATE                           15 // sal_Int16
+#define BASEPROPERTY_FONT_TYPE                       16 // OLD: Font_Type
+#define BASEPROPERTY_FONT_SIZE                       17 // OLD: Font_Size
+#define BASEPROPERTY_FONT_ATTRIBS                    18 // OLD: Font_Attribs
+#define BASEPROPERTY_DEFAULTCONTROL                  19 // ::rtl::OUString (ServiceName)
+#define BASEPROPERTY_LABEL                           20 // ::rtl::OUString
+#define BASEPROPERTY_LINECOUNT                       21 // sal_Int16
+#define BASEPROPERTY_EXTDATEFORMAT                   22 // sal_Int16
+#define BASEPROPERTY_DATESHOWCENTURY                 23 // sal_Bool
+#define BASEPROPERTY_EXTTIMEFORMAT                   24 // sal_Int16
+#define BASEPROPERTY_NUMSHOWTHOUSANDSEP              25 // sal_Bool
+#define BASEPROPERTY_CURRENCYSYMBOL                  26 // ::rtl::OUString
+#define BASEPROPERTY_SPIN                            27 // sal_Bool
+#define BASEPROPERTY_STRICTFORMAT                    28 // sal_Bool
+#define BASEPROPERTY_DECIMALACCURACY                 29 // sal_Int16
+#define BASEPROPERTY_DATE                            30 // sal_Int32
+#define BASEPROPERTY_DATEMIN                         31 // sal_Int32
+#define BASEPROPERTY_DATEMAX                         32 // sal_Int32
+#define BASEPROPERTY_TIME                            33 // sal_Int32
+#define BASEPROPERTY_TIMEMIN                         34 // sal_Int32
+#define BASEPROPERTY_TIMEMAX                         35 // sal_Int32
+#define BASEPROPERTY_VALUE_INT32                     36 // sal_Int32
+#define BASEPROPERTY_VALUEMIN_INT32                  37 // sal_Int32
+#define BASEPROPERTY_VALUEMAX_INT32                  38 // sal_Int32
+#define BASEPROPERTY_VALUESTEP_INT32                 39 // sal_Int32
+#define BASEPROPERTY_EDITMASK                        40 // ::rtl::OUString
+#define BASEPROPERTY_LITERALMASK                     41 // ::rtl::OUString
+#define BASEPROPERTY_IMAGEURL                        42 // ::rtl::OUString
+#define BASEPROPERTY_READONLY                        43 // sal_Bool
+#define BASEPROPERTY_ENABLED                         44 // sal_Bool
+#define BASEPROPERTY_PRINTABLE                       45 // sal_Bool
+#define BASEPROPERTY_ECHOCHAR                        46 // sal_Int16
+#define BASEPROPERTY_MAXTEXTLEN                      47 // sal_Int16
+#define BASEPROPERTY_HARDLINEBREAKS                  48 // sal_Int16
+#define BASEPROPERTY_AUTOCOMPLETE                    49 // sal_Bool
+#define BASEPROPERTY_MULTISELECTION                  50 // sal_Bool
+#define BASEPROPERTY_SELECTEDITEMS                   51 // INT16Sequence
+#define BASEPROPERTY_VALUE_DOUBLE                    52 // DOUBLE
+#define BASEPROPERTY_VALUEMIN_DOUBLE                 53 // DOUBLE
+#define BASEPROPERTY_VALUEMAX_DOUBLE                 54 // DOUBLE
+#define BASEPROPERTY_VALUESTEP_DOUBLE                55 // DOUBLE
+#define BASEPROPERTY_TRISTATE                        56 // sal_Bool
+#define BASEPROPERTY_DEFAULTBUTTON                   57 // sal_Bool
+#define BASEPROPERTY_HELPURL                         58 // ::rtl::OUString
+#define BASEPROPERTY_AUTOTOGGLE                      59 // sal_Bool
+//#define BASEPROPERTY_FOCUSSELECTIONHIDE            60 // sal_Bool
+#define BASEPROPERTY_FORMATKEY                       61 // sal_Bool
+#define BASEPROPERTY_FORMATSSUPPLIER                 62 // ::com::sun::star::util::XNumberFormatsSupplier
+#define BASEPROPERTY_EFFECTIVE_VALUE                 63 // Any (double or string)
+#define BASEPROPERTY_TREATASNUMBER                   64 // sal_Bool
+#define BASEPROPERTY_EFFECTIVE_DEFAULT               65 // Any (double or string)
+#define BASEPROPERTY_EFFECTIVE_MIN                   66 // Double
+#define BASEPROPERTY_EFFECTIVE_MAX                   67 // Double
+#define BASEPROPERTY_CURSYM_POSITION                 68 // sal_Bool
+#define BASEPROPERTY_TITLE                           69 // ::rtl::OUString
+#define BASEPROPERTY_MOVEABLE                        70 // sal_Bool
+#define BASEPROPERTY_CLOSEABLE                       71 // sal_Bool
+#define BASEPROPERTY_SIZEABLE                        72 // sal_Bool
+#define BASEPROPERTY_HELPTEXT                        73 // ::rtl::OUString
+#define BASEPROPERTY_PROGRESSVALUE                   74 // sal_Int32
+#define BASEPROPERTY_PROGRESSVALUE_MIN               75 // sal_Int32
+#define BASEPROPERTY_PROGRESSVALUE_MAX               76 // sal_Int32
+#define BASEPROPERTY_SCROLLVALUE                     77 // sal_Int32
+#define BASEPROPERTY_SCROLLVALUE_MAX                 78 // sal_Int32
+#define BASEPROPERTY_LINEINCREMENT                   79 // sal_Int32
+#define BASEPROPERTY_BLOCKINCREMENT                  80 // sal_Int32
+#define BASEPROPERTY_VISIBLESIZE                     81 // sal_Int32
+#define BASEPROPERTY_ORIENTATION                     82 // sal_Int32
+#define BASEPROPERTY_FONTRELIEF                      83 // sal_Int16
+#define BASEPROPERTY_FONTEMPHASISMARK                84 // sal_Int16
+#define BASEPROPERTY_TEXTLINECOLOR                   85 // sal_Int32
+#define BASEPROPERTY_IMAGEALIGN                      86 // sal_Int16
+#define BASEPROPERTY_SCALEIMAGE                      87 // sal_Bool
+#define BASEPROPERTY_PUSHBUTTONTYPE                  88 // sal_Int16
+#define BASEPROPERTY_DISPLAYBACKGROUNDCOLOR          89 // sal_Int32
+#define BASEPROPERTY_AUTOMNEMONICS                   90 // sal_Bool
+#define BASEPROPERTY_MOUSETRANSPARENT                91 // sal_Bool
+#define BASEPROPERTY_ACCESSIBLENAME                  92  // ::rtl::OUString
+#define BASEPROPERTY_PLUGINPARENT                    93  // sal_Int64
+#define BASEPROPERTY_SCROLLVALUE_MIN                 94  // sal_Int32
+#define BASEPROPERTY_REPEAT_DELAY                    95  // sal_Int32
+#define BASEPROPERTY_SYMBOL_COLOR                    96  // sal_Int32
+#define BASEPROPERTY_SPINVALUE                       97  // sal_Int32
+#define BASEPROPERTY_SPINVALUE_MIN                   98  // sal_Int32
+#define BASEPROPERTY_SPINVALUE_MAX                   99  // sal_Int32
+#define BASEPROPERTY_SPININCREMENT                  100  // sal_Int32
+#define BASEPROPERTY_REPEAT                         101  // sal_Bool
+#define BASEPROPERTY_ENFORCE_FORMAT                 102  // sal_Bool
+#define BASEPROPERTY_LIVE_SCROLL                    103  // sal_Bool
+#define BASEPROPERTY_LINE_END_FORMAT                104  // sal_Int16
+#define BASEPROPERTY_ACTIVATED                      105  // sal Bool
+#define BASEPROPERTY_COMPLETE                       106  // sal_Bool
+#define BASEPROPERTY_CURRENTITEMID                  107  // sal_Int16
+#define BASEPROPERTY_TOGGLE                         108  // sal_Bool
+#define BASEPROPERTY_FOCUSONCLICK                   109  // sal_Bool
+#define BASEPROPERTY_HIDEINACTIVESELECTION          110  // sal_Bool
+#define BASEPROPERTY_VISUALEFFECT                   111  // sal_Int16
+#define BASEPROPERTY_BORDERCOLOR                    112  // sal_Int32
+#define BASEPROPERTY_IMAGEPOSITION                  113  // sal_Int16
+#define BASEPROPERTY_NATIVE_WIDGET_LOOK             114  // sal_Bool
+#define BASEPROPERTY_VERTICALALIGN                  115  // VerticalAlignment
+#define BASEPROPERTY_WHEELWITHOUTFOCUS              116  // sal_Bool
+#define BASEPROPERTY_GRAPHIC                        117  // css.graphic.XGraphic
+#define BASEPROPERTY_STEP_TIME                      118  // sal_Int32
+#define BASEPROPERTY_DECORATION                     119  // sal_Bool
+#define BASEPROPERTY_PAINTTRANSPARENT               120  // sal_Bool
+#define BASEPROPERTY_AUTOHSCROLL                    121 // sal_Bool
+#define BASEPROPERTY_AUTOVSCROLL                    122  // sal_Bool
+#define BASEPROPERTY_DESKTOP_AS_PARENT              123  // sal_Bool
 #define BASEPROPERTY_TREE_START                     124
 #define BASEPROPERTY_TREE_SELECTIONTYPE             124
 #define BASEPROPERTY_TREE_EDITABLE                  125
@@ -189,8 +184,10 @@ namespace rtl {
 #define BASEPROPERTY_TREE_INVOKESSTOPNODEEDITING    131
 #define BASEPROPERTY_TREE_END                       131
 #define BASEPROPERTY_DIALOGSOURCEURL                132
-#define BASEPROPERTY_NOLABEL               133   // ::rtl::OUString  added for issue79712
-#define BASEPROPERTY_URL                   134  // ::rtl::OUString
+#define BASEPROPERTY_NOLABEL                        133  // ::rtl::OUString  added for issue79712
+#define BASEPROPERTY_URL                            134  // ::rtl::OUString
+#define BASEPROPERTY_UNIT                           135  // ::awt::FieldUnit
+#define BASEPROPERTY_CUSTOMUNITTEXT                 136  // ::rtl::OUString
 
 // Keine gebundenen Properties, werden immer aus der Property BASEPROPERTY_FONTDESCRIPTOR entnommen.
 #define BASEPROPERTY_FONTDESCRIPTORPART_START           1000
