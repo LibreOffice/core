0d09f30912cba12278ae40bb483edd2641d279ec
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:58:46 +0000

    INTEGRATION: CWS mba30patches01 (1.32.24); FILE MERGED 2008/04/28 10:32:30 mba 1.32.24.3: #i5226#: change reverted for further investigation 2008/04/23 09:46:25 mba 1.32.24.2: RESYNC: (1.32-1.35); FILE MERGED 2008/03/18 15:53:25 mba 1.32.24.1: #i5226#: saving enabled even if file is not modified

--- sfx2/source/doc/guisaveas.cxx
+++ sfx2/source/doc/guisaveas.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: guisaveas.cxx,v $
- * $Revision: 1.36 $
+ * $Revision: 1.37 $
  *
  * This file is part of OpenOffice.org.
  *
