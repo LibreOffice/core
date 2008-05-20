c99e535d6f7c3e73d389974ff1f0caee81b8ac53
Kurt Zenker <kz@openoffice.org>
2008-05-20 12:10:39 +0000

    INTEGRATION: CWS koheidatapilot01 (1.9.202); FILE MERGED 2008/04/25 21:00:22 kohei 1.9.202.10: RESYNC: (1.9-1.10); FILE MERGED 2008/04/24 23:26:45 kohei 1.9.202.9: * fixed a regression on page field filtering by the empty string value. * fixed an incorrect drill-down table with number groups (i88531). * moved the shared string storage out of ScDPCacheTable to make it more generic. 2008/04/17 13:11:10 kohei 1.9.202.8: Used ScDPItemData to store cell values to allow numerical comparison where appropriate.  Also fixed the regression involving drill-down on group names. 2008/04/15 15:32:13 kohei 1.9.202.7: get the "Identify categories" option to really work.  The previous commit didn't quite do the right thing. 2008/04/15 03:41:18 kohei 1.9.202.6: get the "Identify categories" option to work. 2008/04/15 00:01:11 kohei 1.9.202.5: get the "Ignore empty rows" option to work. 2007/12/06 07:06:15 kohei 1.9.202.4: removed all previously invisible code. 2007/11/29 19:34:41 kohei 1.9.202.3: fixed the regression where table didn't rebuild cache table when it's refreshed. 2007/11/09 02:12:09 kohei 1.9.202.2: renamed one method & some minor code change to make more use of cache table. 2007/10/29 17:44:40 kohei 1.9.202.1: initial checkin

--- sc/source/core/data/dpshttab.cxx
+++ sc/source/core/data/dpshttab.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dpshttab.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -39,10 +39,24 @@
 #include <svtools/zforlist.hxx>
 
 #include "dpshttab.hxx"
+#include "dptabres.hxx"
 #include "document.hxx"
 #include "collect.hxx"
 #include "cell.hxx"
+#include "dpcachetable.hxx"
 #include "globstr.hrc"
+
+#include <com/sun/star/sheet/DataPilotFieldFilter.hpp>
+
+#include <vector>
+#include <set>
+
+using namespace ::com::sun::star;
+using ::com::sun::star::uno::Any;
+using ::com::sun::star::uno::Sequence;
+using ::std::vector;
+using ::std::set;
+using ::std::hash_map;
 
 // -----------------------------------------------------------------------
 
@@ -52,49 +66,47 @@ public:
     ScDocument*     pDoc;
     ScRange         aRange;
     ScQueryParam    aQuery;
-    long            nColCount;
+    BOOL*           pSpecial;           // to flag special handling of query parameters in ValidQuery.
     BOOL            bIgnoreEmptyRows;
     BOOL            bRepeatIfEmpty;
-    TypedStrCollection**    ppStrings;
     BOOL*           pDateDim;
     SCROW           nNextRow;       // for iterator, within range
 
-    ScSheetDPData_Impl() {}
+    ScDPCacheTable  aCacheTable;
+
+    ScSheetDPData_Impl() :
+        pSpecial(NULL)
+    {
+    }
 };
 
 // -----------------------------------------------------------------------
 
 ScSheetDPData::ScSheetDPData( ScDocument* pD, const ScSheetSourceDesc& rDesc )
-    : pSpecial(NULL)
 {
-    long nCount = rDesc.aSourceRange.aEnd.Col() - rDesc.aSourceRange.aStart.Col() + 1;
     pImpl = new ScSheetDPData_Impl;
     pImpl->pDoc = pD;
     pImpl->aRange = rDesc.aSourceRange;
     pImpl->aQuery = rDesc.aQueryParam;
     pImpl->bIgnoreEmptyRows = FALSE;
     pImpl->bRepeatIfEmpty = FALSE;
-    pImpl->nColCount = nCount;
-    pImpl->ppStrings = new TypedStrCollection*[nCount];
     pImpl->pDateDim = NULL;
-    for (long i=0; i<nCount; i++)
-        pImpl->ppStrings[i] = NULL;
 
     pImpl->nNextRow = pImpl->aRange.aStart.Row() + 1;
 
     SCSIZE nEntryCount(pImpl->aQuery.GetEntryCount());
-    pSpecial = new BOOL[nEntryCount];
+    pImpl->pSpecial = new BOOL[nEntryCount];
     for (SCSIZE j = 0; j < nEntryCount; ++j )
     {
         ScQueryEntry& rEntry = pImpl->aQuery.GetEntry(j);
         if (rEntry.bDoQuery)
         {
-            pSpecial[j] = false;
+            pImpl->pSpecial[j] = false;
             if (!rEntry.bQueryByString)
             {
                 if (*rEntry.pStr == EMPTY_STRING &&
                    ((rEntry.nVal == SC_EMPTYFIELDS) || (rEntry.nVal == SC_NONEMPTYFIELDS)))
-                    pSpecial[j] = true;
+                    pImpl->pSpecial[j] = true;
             }
             else
             {
@@ -109,26 +121,20 @@ ScSheetDPData::ScSheetDPData( ScDocument* pD, const ScSheetSourceDesc& rDesc )
 
 ScSheetDPData::~ScSheetDPData()
 {
-    for (long i=0; i<pImpl->nColCount; i++)
-        delete pImpl->ppStrings[i];
-    delete[] pImpl->ppStrings;
     delete[] pImpl->pDateDim;
+    delete[] pImpl->pSpecial;
     delete pImpl;
-    delete[] pSpecial;
 }
 
 void ScSheetDPData::DisposeData()
 {
-    for (long i=0; i<pImpl->nColCount; i++)
-    {
-        delete pImpl->ppStrings[i];
-        pImpl->ppStrings[i] = NULL;
-    }
+    pImpl->aCacheTable.clear();
 }
 
 long ScSheetDPData::GetColumnCount()
 {
-    return pImpl->nColCount;
+    CreateCacheTable();
+    return pImpl->aCacheTable.getColSize();
 }
 
 BOOL lcl_HasQuery( const ScQueryParam& rParam )
@@ -139,81 +145,31 @@ BOOL lcl_HasQuery( const ScQueryParam& rParam )
 
 const TypedStrCollection& ScSheetDPData::GetColumnEntries(long nColumn)
 {
-    DBG_ASSERT(nColumn>=0 && nColumn<pImpl->nColCount, "ScSheetDPData: wrong column");
-
-    if (!pImpl->ppStrings[nColumn])
-    {
-        TypedStrCollection* pColl = new TypedStrCollection;
-
-        //! document must have function to fill collection!!!
-        String aDocStr;
-        SCCOL nDocCol = (SCCOL)(pImpl->aRange.aStart.Col() + nColumn);
-        SCTAB nDocTab = pImpl->aRange.aStart.Tab();
-        SCROW nStartRow = pImpl->aRange.aStart.Row()+1; // start of data
-        SCROW nEndRow = pImpl->aRange.aEnd.Row();
-        SCCOL nStartCol = pImpl->aRange.aStart.Col();
-        SCCOL nEndCol = pImpl->aRange.aEnd.Col();
-        for (SCROW nRow = nStartRow; nRow <= nEndRow; nRow++)
-        {
-            if ( pImpl->bIgnoreEmptyRows &&
-                    pImpl->pDoc->IsBlockEmpty( nDocTab, nStartCol, nRow, nEndCol, nRow ) )
-            {
-                //  ignore this (empty) row
-                //! count and skip empty rows?
-            }
-            else if ( pImpl->bRepeatIfEmpty && nRow > nStartRow &&
-                        !pImpl->pDoc->HasData( nDocCol, nRow, nDocTab ) )
-            {
-                //  ignore empty member (if it's not the first row)
-            }
-            else if ( lcl_HasQuery(pImpl->aQuery) &&
-                        !pImpl->pDoc->ValidQuery( nRow, nDocTab, pImpl->aQuery, pSpecial ) )
-            {
-                //  this row is filtered out
-            }
-            else
-            {
-                TypedStrData* pNew;
-                pImpl->pDoc->GetString( nDocCol, nRow, nDocTab, aDocStr );
-                if ( pImpl->pDoc->HasValueData( nDocCol, nRow, nDocTab ) )
-                {
-                    double fVal = pImpl->pDoc->GetValue(ScAddress(nDocCol, nRow, nDocTab));
-                    pNew = new TypedStrData( aDocStr, fVal, SC_STRTYPE_VALUE );
-                }
-                else
-                    pNew = new TypedStrData( aDocStr );
-
-                if (!pColl->Insert(pNew))
-                    delete pNew;
-            }
-        }
-
-        pImpl->ppStrings[nColumn] = pColl;
-    }
-    return *pImpl->ppStrings[nColumn];
+    DBG_ASSERT(nColumn>=0 && nColumn < pImpl->aCacheTable.getColSize(), "ScSheetDPData: wrong column");
+    CreateCacheTable();
+    return pImpl->aCacheTable.getFieldEntries(nColumn);
 }
 
 String ScSheetDPData::getDimensionName(long nColumn)
 {
+    CreateCacheTable();
     if (getIsDataLayoutDimension(nColumn))
     {
         //! different internal and display names?
         //return "Data";
         return ScGlobal::GetRscString(STR_PIVOT_DATA);
     }
-    else if ( nColumn >= pImpl->nColCount )
+    else if (nColumn >= pImpl->aCacheTable.getColSize())
     {
         DBG_ERROR("getDimensionName: invalid dimension");
         return String();
     }
     else
     {
-        SCCOL nDocCol = (SCCOL)(pImpl->aRange.aStart.Col() + nColumn);
-        SCROW nDocRow = pImpl->aRange.aStart.Row();
-        SCTAB nDocTab = pImpl->aRange.aStart.Tab();
-        String aDocStr;
-        pImpl->pDoc->GetString( nDocCol, nDocRow, nDocTab, aDocStr );
-        return aDocStr;
+        const String* pStr = pImpl->aCacheTable.getFieldName(nColumn);
+        if (pStr)
+            return *pStr;
+        else return String();
     }
 }
 
@@ -230,11 +186,13 @@ BOOL lcl_HasDateFormat( ScDocument* pDoc, const ScRange& rRange )
 
 BOOL ScSheetDPData::IsDateDimension(long nDim)
 {
+    CreateCacheTable();
+    long nColCount = pImpl->aCacheTable.getColSize();
     if (getIsDataLayoutDimension(nDim))
     {
         return FALSE;
     }
-    else if ( nDim >= pImpl->nColCount )
+    else if (nDim >= nColCount)
     {
         DBG_ERROR("IsDateDimension: invalid dimension");
         return FALSE;
@@ -243,9 +201,9 @@ BOOL ScSheetDPData::IsDateDimension(long nDim)
     {
         if (!pImpl->pDateDim)
         {
-            pImpl->pDateDim = new BOOL[pImpl->nColCount];
+            pImpl->pDateDim = new BOOL[nColCount];
             ScRange aTestRange = pImpl->aRange;
-            for (long i=0; i<pImpl->nColCount; i++)
+            for (long i = 0; i < nColCount; ++i)
             {
                 SCCOL nCol = (SCCOL)( pImpl->aRange.aStart.Col() + i );
                 aTestRange.aStart.SetCol(nCol);
@@ -259,11 +217,12 @@ BOOL ScSheetDPData::IsDateDimension(long nDim)
 
 UINT32 ScSheetDPData::GetNumberFormat(long nDim)
 {
+    CreateCacheTable();
     if (getIsDataLayoutDimension(nDim))
     {
         return 0;
     }
-    else if ( nDim >= pImpl->nColCount )
+    else if (nDim >= pImpl->aCacheTable.getColSize())
     {
         DBG_ERROR("GetNumberFormat: invalid dimension");
         return 0;
@@ -281,7 +240,8 @@ UINT32 ScSheetDPData::GetNumberFormat(long nDim)
 
 BOOL ScSheetDPData::getIsDataLayoutDimension(long nColumn)
 {
-    return ( nColumn == pImpl->nColCount );
+    CreateCacheTable();
+    return (nColumn == pImpl->aCacheTable.getColSize());
 }
 
 void ScSheetDPData::SetEmptyFlags( BOOL bIgnoreEmptyRows, BOOL bRepeatIfEmpty )
@@ -290,131 +250,47 @@ void ScSheetDPData::SetEmptyFlags( BOOL bIgnoreEmptyRows, BOOL bRepeatIfEmpty )
     pImpl->bRepeatIfEmpty   = bRepeatIfEmpty;
 }
 
-void ScSheetDPData::ResetIterator()
+bool ScSheetDPData::IsRepeatIfEmpty()
 {
-    pImpl->nNextRow = pImpl->aRange.aStart.Row() + 1;
+    return pImpl->bRepeatIfEmpty;
 }
 
-void lcl_GetStringOrValue( ScDPItemData& rData, ScDocument* pDoc,
-                            SCCOL nCol, SCROW nRow, SCTAB nTab,
-                            BOOL bRepeatIfEmpty, SCROW nFirstDataRow )
+void ScSheetDPData::CreateCacheTable()
 {
-    if ( bRepeatIfEmpty )
-    {
-        //  use first non-empty entry
-        //! just keep old ItemData ????
-        //! otherwise count empty cells and subtract!
+    // Scan and store the data from the source range.
+    if (!pImpl->aCacheTable.empty())
+        // already cached.
+        return;
 
-        while ( !pDoc->HasData( nCol, nRow, nTab ) && nRow > nFirstDataRow )
-            --nRow;
-    }
-
-    BOOL bVal = pDoc->HasValueData( nCol, nRow, nTab );
-    rData.bHasValue = bVal;
-    if (bVal)
-        rData.fValue = pDoc->GetValue( ScAddress( nCol, nRow, nTab ) );
-    else
-        pDoc->GetString( nCol, nRow, nTab, rData.aString );
+    pImpl->aCacheTable.fillTable(pImpl->pDoc, pImpl->aRange, pImpl->aQuery, pImpl->pSpecial,
+                                 pImpl->bIgnoreEmptyRows);
 }
 
-BOOL ScSheetDPData::GetNextRow( const ScDPTableIteratorParam& rParam )
+void ScSheetDPData::FilterCacheTable(const vector<ScDPDimension*>& rPageDims)
 {
-    if ( pImpl->nNextRow > pImpl->aRange.aEnd.Row() )
-        return FALSE;
+    CreateCacheTable();
+    pImpl->aCacheTable.filterByPageDimension(rPageDims);
+}
 
-    SCSIZE i;
-    long nStartCol = pImpl->aRange.aStart.Col();
-    SCTAB nDocTab = pImpl->aRange.aStart.Tab();
-    SCROW nFirstDataRow = pImpl->aRange.aStart.Row() + 1;
+void ScSheetDPData::GetDrillDownData(const vector<ScDPCacheTable::Criterion>& rCriteria, Sequence< Sequence<Any> >& rData)
+{
+    CreateCacheTable();
+    sal_Int32 nRowSize = pImpl->aCacheTable.getRowSize();
+    if (!nRowSize)
+        return;
 
-    BOOL bFilteredOut;
-    do
-    {
-        if ( pImpl->bIgnoreEmptyRows )
-        {
-            SCCOL nEndCol = pImpl->aRange.aEnd.Col();
-            while ( pImpl->pDoc->IsBlockEmpty( nDocTab, (SCCOL)nStartCol, pImpl->nNextRow,
-                                                nEndCol, pImpl->nNextRow ) )
-            {
-                ++pImpl->nNextRow;
-                if ( pImpl->nNextRow > pImpl->aRange.aEnd.Row() )
-                    return FALSE;
-            }
-        }
+    pImpl->aCacheTable.filterTable(rCriteria, rData, IsRepeatIfEmpty());
+}
 
-        bFilteredOut = ( lcl_HasQuery(pImpl->aQuery) &&
-                !pImpl->pDoc->ValidQuery( pImpl->nNextRow, nDocTab, pImpl->aQuery, pSpecial ) );
-        if ( bFilteredOut )
-        {
-            ++pImpl->nNextRow;
-            if ( pImpl->nNextRow > pImpl->aRange.aEnd.Row() )
-                return FALSE;
-        }
-    }
-    while (bFilteredOut);
+void ScSheetDPData::CalcResults(CalcInfo& rInfo, bool bAutoShow)
+{
+    CreateCacheTable();
+    CalcResultsFromCacheTable(pImpl->aCacheTable, rInfo, bAutoShow);
+}
 
-    //! use more efficient iterators
-
-    for (i=0; i<rParam.nColCount; i++)
-    {
-        long nDim = rParam.pCols[i];
-        if ( getIsDataLayoutDimension(nDim) )
-            rParam.pColData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-        else
-            lcl_GetStringOrValue( rParam.pColData[i], pImpl->pDoc,
-                                    (SCCOL)(nStartCol+nDim), pImpl->nNextRow, nDocTab,
-                                    pImpl->bRepeatIfEmpty, nFirstDataRow );
-    }
-
-    for (i=0; i<rParam.nRowCount; i++)
-    {
-        long nDim = rParam.pRows[i];
-        if ( getIsDataLayoutDimension(nDim) )
-            rParam.pRowData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-        else
-            lcl_GetStringOrValue( rParam.pRowData[i], pImpl->pDoc,
-                                    (SCCOL)(nStartCol+nDim), pImpl->nNextRow, nDocTab,
-                                    pImpl->bRepeatIfEmpty, nFirstDataRow );
-    }
-
-    for (i=0; i<rParam.nPageCount; i++)
-    {
-        long nDim = rParam.pPages[i];
-        if ( getIsDataLayoutDimension(nDim) )
-            rParam.pPageData[i].SetString( String::CreateFromAscii(RTL_CONSTASCII_STRINGPARAM("x")) );
-        else
-            lcl_GetStringOrValue( rParam.pPageData[i], pImpl->pDoc,
-                                    (USHORT)(nStartCol+nDim), pImpl->nNextRow, nDocTab,
-                                    pImpl->bRepeatIfEmpty, nFirstDataRow );
-    }
-
-    for (i=0; i<rParam.nDatCount; i++)
-    {
-        long nDim = rParam.pDats[i];
-        ScAddress aPos( (SCCOL)(nStartCol+nDim), pImpl->nNextRow, nDocTab );
-        ScBaseCell* pCell = pImpl->pDoc->GetCell( aPos );
-        if ( !pCell || pCell->GetCellType() == CELLTYPE_NOTE )
-            rParam.pValues[i].Set( 0.0, SC_VALTYPE_EMPTY );
-        else if ( pCell->GetCellType() == CELLTYPE_FORMULA && ((ScFormulaCell*)pCell)->GetErrCode() )
-            rParam.pValues[i].Set( 0.0, SC_VALTYPE_ERROR );
-        else if ( pCell->HasValueData() )
-        {
-            //! GetValue method at BaseCell?
-            DBG_ASSERT( pCell->GetCellType() == CELLTYPE_VALUE ||
-                        pCell->GetCellType() == CELLTYPE_FORMULA, "wrong cell type" );
-            double fVal;
-            if ( pCell->GetCellType() == CELLTYPE_VALUE )
-                fVal = ((ScValueCell*)pCell)->GetValue();
-            else
-                fVal = ((ScFormulaCell*)pCell)->GetValue();
-            rParam.pValues[i].Set( fVal, SC_VALTYPE_VALUE );
-        }
-        else
-            rParam.pValues[i].Set( 0.0, SC_VALTYPE_STRING );
-    }
-
-    pImpl->nNextRow++;
-    return TRUE;
+const ScDPCacheTable& ScSheetDPData::GetCacheTable() const
+{
+    return pImpl->aCacheTable;
 }
 
 // -----------------------------------------------------------------------
