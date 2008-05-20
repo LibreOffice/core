9e8947e87b1420bb8983c452037a0d570d47de2a
Kurt Zenker <kz@openoffice.org>
2008-05-20 18:10:53 +0000

    INTEGRATION: CWS mba30patches01 (1.7.40); FILE MERGED 2008/04/23 10:49:23 mba 1.7.40.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/18 15:40:57 mba 1.7.40.1: #i86365#: remove unused code

--- extensions/source/propctrlr/eformshelper.cxx
+++ extensions/source/propctrlr/eformshelper.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: eformshelper.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -510,12 +510,6 @@ namespace pcr
         {
             OSL_ENSURE( sal_False, "EFormsHelper::setBinding: caught an exception!" );
         }
-    }
-
-    //--------------------------------------------------------------------
-    Reference< XPropertySet > EFormsHelper::createBindingForFormModel( const ::rtl::OUString& _rTargetModel ) const SAL_THROW(())
-    {
-        return implGetOrCreateBinding( _rTargetModel, ::rtl::OUString() );
     }
 
     //--------------------------------------------------------------------
