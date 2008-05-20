5eb1d7b4f359f9d4ea2924aebd42a7067ef24d61
Kurt Zenker <kz@openoffice.org>
2008-05-20 18:19:57 +0000

    INTEGRATION: CWS mba30patches01 (1.5.324); FILE MERGED 2008/04/23 10:50:41 mba 1.5.324.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/18 15:41:02 mba 1.5.324.1: #i86365#: remove unused code

--- extensions/source/scanner/scanner.hxx
+++ extensions/source/scanner/scanner.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: scanner.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -101,11 +101,8 @@ public:
     virtual SEQ( sal_Int8 ) SAL_CALL        getMaskDIB() throw();
 
     // Misc
-    OUString                                getImplementationName() throw();
     static OUString                         getImplementationName_Static() throw();
-    Sequence< OUString >                    getSupportedServiceNames() throw();
     static Sequence< OUString >             getSupportedServiceNames_Static() throw();
-    BOOL                                    supportsService( const OUString& ServiceName ) throw();
 
     void                                    Lock() { maProtector.acquire(); }
     void                                    Unlock() { maProtector.release(); }
