b780282862dd3afefc25e65ae8130af5edff4494
Kurt Zenker <kz@openoffice.org>
2008-05-20 18:11:55 +0000

    INTEGRATION: CWS mba30patches01 (1.5.124); FILE MERGED 2008/04/23 10:49:39 mba 1.5.124.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/18 15:40:59 mba 1.5.124.1: #i86365#: remove unused code

--- extensions/source/propctrlr/handlerhelper.hxx
+++ extensions/source/propctrlr/handlerhelper.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: handlerhelper.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -139,14 +139,6 @@ namespace pcr
             @return
                 the newly created control
         */
-        static ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyControl >
-            createComboBoxControl(
-                const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyControlFactory >& _rxControlFactory,
-                const ::com::sun::star::uno::Sequence< ::rtl::OUString >& _rInitialListEntries,
-                sal_Bool _bReadOnlyControl,
-                sal_Bool _bSorted
-            );
-
         static ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyControl >
             createComboBoxControl(
                 const ::com::sun::star::uno::Reference< ::com::sun::star::inspection::XPropertyControlFactory >& _rxControlFactory,
