9c024d17be3b57a0d9471356bc8492048e6bdb68
Kurt Zenker <kz@openoffice.org>
2008-05-20 18:00:54 +0000

    INTEGRATION: CWS mba30patches01 (1.2.56); FILE MERGED 2008/04/23 09:44:49 mba 1.2.56.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/18 15:40:03 mba 1.2.56.1: #i86353#: remove unused code

--- idl/inc/char.hxx
+++ idl/inc/char.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: char.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -37,7 +37,6 @@
 class SvChar
 {
 public:
-    static BOOL MakeChar( char * pChar, char ** ppStr );
     static char * GetTable( CharSet nSource, CharSet nDest );
 };
 
