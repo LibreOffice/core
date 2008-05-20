898920d8794d0a62fada17becdfcdd7b9176fa48
Kurt Zenker <kz@openoffice.org>
2008-05-20 18:06:56 +0000

    INTEGRATION: CWS mba30patches01 (1.5.40); FILE MERGED 2008/04/23 10:48:30 mba 1.5.40.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/18 15:40:53 mba 1.5.40.1: #i86365#: remove unused code

--- extensions/source/bibliography/loadlisteneradapter.cxx
+++ extensions/source/bibliography/loadlisteneradapter.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: loadlisteneradapter.cxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -113,18 +113,6 @@ namespace bib
     //---------------------------------------------------------------------
     OComponentAdapterBase::~OComponentAdapterBase()
     {
-    }
-
-    //---------------------------------------------------------------------
-    void OComponentAdapterBase::lock()
-    {
-        ++m_nLockCount;
-    }
-
-    //---------------------------------------------------------------------
-    void OComponentAdapterBase::unlock()
-    {
-        --m_nLockCount;
     }
 
     //---------------------------------------------------------------------
