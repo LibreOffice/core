133077077f5cd8e5321fe8767f7c10e8b1af2d08
Kurt Zenker <kz@openoffice.org>
2008-05-21 11:28:33 +0000

    INTEGRATION: CWS extmgrui01 (1.1.2); FILE ADDED 2008/05/21 11:31:39 kz 1.1.2.10: license updated 2008/04/29 08:43:04 dv 1.1.2.9: #i88800# Add new notified packages only once 2008/03/26 14:08:24 dv 1.1.2.8: Don't hide dialog when clicking 'updates available' icon 2008/02/29 08:54:25 dv 1.1.2.7: #i83902# Allow unopkg to quit when closing dialog 2008/02/28 15:16:51 dv 1.1.2.6: #i83902# Added support for updating a single extension via context menu 2008/02/27 12:58:54 dv 1.1.2.5: #i83902# Better handling of external adding of extensions 2008/02/26 07:19:53 dv 1.1.2.4: #i83902# Some strings changed for new dialog 2008/02/22 07:50:19 dv 1.1.2.3: #i83902# Added progress handling 2008/02/12 14:52:04 dv 1.1.2.2: #i83902# new dialog for the extension manager (part 2) 2008/02/04 07:39:06 dv 1.1.2.1: #i83902# new dialog for the extension manager

---
+++ desktop/source/deployment/gui/dp_gui_theextmgr.hxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: dp_gui_theextmgr.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_DP_GUI_THEEXTMGR_HXX
+#define INCLUDED_DP_GUI_THEEXTMGR_HXX
+
+#include "comphelper/sequence.hxx"
+
+#include "cppuhelper/implbase2.hxx"
+
+#include "com/sun/star/container/XNameAccess.hpp"
+#include "com/sun/star/deployment/XPackageManager.hpp"
+#include "com/sun/star/frame/XDesktop.hpp"
+#include "com/sun/star/frame/XTerminateListener.hpp"
+#include "com/sun/star/uno/XComponentContext.hpp"
+#include "com/sun/star/util/XModifyListener.hpp"
+
+#include "dp_gui.h"
+
+//==============================================================================
+namespace dp_gui {
+
+//------------------------------------------------------------------------------
+class ExtMgrDialog;
+class ExtensionCmdQueue;
+
+//------------------------------------------------------------------------------
+class TheExtensionManager :
+    public ::cppu::WeakImplHelper2< ::com::sun::star::frame::XTerminateListener,
+                                    ::com::sun::star::util::XModifyListener >
+{
+private:
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >    m_xContext;
+    ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDesktop >           m_xDesktop;
+    ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager> > m_sPackageManagers;
+    ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameAccess >    m_xNameAccessNodes;
+
+    ::std::auto_ptr< ExtensionCmdQueue > m_pExecuteCmdQueue;
+
+    ExtMgrDialog   *m_pDialog;
+
+    // liste der packages ( xpackage?, mit parent manager, ... )
+
+    void createPackageList();
+    bool createPackageList( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > &xPackageManager );
+
+public:
+    static ::rtl::Reference<TheExtensionManager> s_ExtMgr;
+
+         TheExtensionManager( Window * pParent,
+                              const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > &xContext );
+        ~TheExtensionManager();
+
+    void SetText( const ::rtl::OUString &rTitle );
+    void Show();
+    void ToTop( USHORT nFlags );
+    bool Close();
+    bool isVisible();
+
+    //-----------------
+    bool checkUpdates( bool showUpdateOnly, bool parentVisible );
+    bool enablePackage( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > &xPackage,
+                        bool bEnable );
+    bool removePackage( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > &xPackageManager,
+                        const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > &xPackage );
+    bool updatePackage( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > &xPackageManager,
+                        const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > &xPackage );
+    bool installPackage( const ::rtl::OUString &rPackageURL, bool bWarnUser = false );
+
+    bool queryTermination();
+    void terminateDialog();
+
+    // Tools
+    bool supportsOptions( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > &xPackage ) const;
+    PackageState getPackageState( const ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackage > &xPackage ) const;
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > getContext() const { return m_xContext; }
+    ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > getUserPkgMgr() const { return m_sPackageManagers[0]; }
+    ::com::sun::star::uno::Reference< ::com::sun::star::deployment::XPackageManager > getSharedPkgMgr() const { return m_sPackageManagers[1]; }
+
+    //-----------------
+    static ::rtl::Reference<TheExtensionManager> get(
+        ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext> const & xContext,
+        ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindow> const & xParent = 0,
+        ::rtl::OUString const & view = ::rtl::OUString() );
+
+    // XEventListener
+    virtual void SAL_CALL disposing( ::com::sun::star::lang::EventObject const & evt )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // XTerminateListener
+    virtual void SAL_CALL queryTermination( ::com::sun::star::lang::EventObject const & evt )
+        throw (::com::sun::star::frame::TerminationVetoException, ::com::sun::star::uno::RuntimeException);
+    virtual void SAL_CALL notifyTermination( ::com::sun::star::lang::EventObject const & evt )
+        throw (::com::sun::star::uno::RuntimeException);
+
+    // XModifyListener
+    virtual void SAL_CALL modified( ::com::sun::star::lang::EventObject const & evt )
+        throw (::com::sun::star::uno::RuntimeException);
+};
+
+} // namespace dp_gui
+
+#endif
+
