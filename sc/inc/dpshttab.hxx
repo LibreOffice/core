4e56822a64ed6b8b195449d1493727c811724a74
Kurt Zenker <kz@openoffice.org>
2008-05-22 19:13:59 +0000

    #i10000# warnings fixed

--- sc/inc/dpshttab.hxx
+++ sc/inc/dpshttab.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dpshttab.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -42,7 +42,7 @@ namespace com { namespace sun { namespace star { namespace sheet {
 }}}}
 
 class ScDPDimension;
-class ScDPItemData;
+struct ScDPItemData;
 
 // --------------------------------------------------------------------
 //
