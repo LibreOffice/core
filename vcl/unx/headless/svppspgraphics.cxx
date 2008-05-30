0f1d9bfc3e1bc6917f83ca426e83f5d66a336cf2
Oliver Bolte <obo@openoffice.org>
2008-05-30 09:08:07 +0000

    INTEGRATION: CWS aquabmpfix01 (1.5.28); FILE MERGED 2008/05/13 14:03:27 hdu 1.5.28.1: #100000# remove compile warnings

--- vcl/unx/headless/svppspgraphics.cxx
+++ vcl/unx/headless/svppspgraphics.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svppspgraphics.cxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -230,6 +230,12 @@ BOOL PspGraphics::unionClipRegion( long nX, long nY, long nDX, long nDY )
     return (BOOL)m_pPrinterGfx->UnionClipRegion (nX, nY, nDX, nDY);
 }
 
+bool PspGraphics::unionClipRegion( const ::basegfx::B2DPolyPolygon& )
+{
+        // TODO: implement and advertise OutDevSupport_B2DClip support
+        return false;
+}
+
 void PspGraphics::EndSetClipRegion()
 {
     m_pPrinterGfx->EndSetClipRegion ();
@@ -316,6 +322,18 @@ void PspGraphics::drawPolyPolygon( sal_uInt32           nPoly,
                                    PCONSTSALPOINT  *pPtAry )
 {
     m_pPrinterGfx->DrawPolyPolygon (nPoly, pPoints, (const Point**)pPtAry);
+}
+
+bool PspGraphics::drawPolyPolygon( const ::basegfx::B2DPolyPolygon&, double /*fTransparency*/ )
+{
+        // TODO: implement and advertise OutDevSupport_B2DDraw support
+        return false;
+}
+
+bool PspGraphics::drawPolyLine( const ::basegfx::B2DPolygon&, const ::basegfx::B2DVector& /*rLineWidths*/ )
+{
+        // TODO: implement and advertise OutDevSupport_B2DDraw support
+        return false;
 }
 
 sal_Bool PspGraphics::drawPolyLineBezier( ULONG nPoints, const SalPoint* pPtAry, const BYTE* pFlgAry )
