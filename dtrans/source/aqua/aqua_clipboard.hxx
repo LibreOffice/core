5bcc52e73b46840e72254e36daa60a636811dad4
Oliver Bolte <obo@openoffice.org>
2008-05-30 06:56:05 +0000

    INTEGRATION: CWS aquavcl07 (1.7.8); FILE MERGED 2008/04/17 05:39:42 pl 1.7.8.2: RESYNC: (1.7-1.8); FILE MERGED 2008/04/16 12:07:10 pl 1.7.8.1: #i87595# don not call on destroyed objects

--- dtrans/source/aqua/aqua_clipboard.hxx
+++ dtrans/source/aqua/aqua_clipboard.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: aqua_clipboard.hxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -69,6 +69,7 @@ class AquaClipboard;
 
 -(void)applicationDidBecomeActive:(NSNotification*)aNotification;
 
+-(void)disposing;
 @end
 
 
