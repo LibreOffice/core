437aad3f56af1f00f621b751e92be56080980730
Oliver Bolte <obo@openoffice.org>
2008-05-30 07:00:58 +0000

    INTEGRATION: CWS aquavcl07 (1.3.34); FILE MERGED 2008/04/29 18:14:34 pl 1.3.34.4: #i88447# add dockmenu entries from quickstarter 2008/04/17 05:40:18 pl 1.3.34.3: RESYNC: (1.3-1.4); FILE MERGED 2008/04/10 12:50:21 pl 1.3.34.2: #i86519# #i86522# cmd-w hack 2008/04/07 16:10:08 pl 1.3.34.1: #i86435# remap Command to MOD1 and Ctrl to MOD3

--- vcl/aqua/source/app/vclnsapp.mm
+++ vcl/aqua/source/app/vclnsapp.mm
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: vclnsapp.mm,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -241,6 +241,12 @@
 -(void)removeFallbackMenuItem: (NSMenuItem*)pItem
 {
     AquaSalMenu::removeFallbackMenuItem( pItem );
+}
+
+-(void)addDockMenuItem: (NSMenuItem*)pNewItem
+{
+    NSMenu* pDock = AquaSalInstance::GetDynamicDockMenu();
+    [pDock insertItem: pNewItem atIndex: [pDock numberOfItems]];
 }
 
 @end
