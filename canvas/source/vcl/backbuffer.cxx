a0c7fb2374d392379d7419237896265194619f30
Oliver Bolte <obo@openoffice.org>
2008-05-30 08:57:13 +0000

    INTEGRATION: CWS aquabmpfix01 (1.6.98); FILE MERGED 2008/05/09 11:06:47 hdu 1.6.98.2: RESYNC: (1.6-1.7); FILE MERGED 2008/05/09 10:32:42 hdu 1.6.98.1: #i88759# canvas allows b2d drawing

--- canvas/source/vcl/backbuffer.cxx
+++ canvas/source/vcl/backbuffer.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: backbuffer.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -41,6 +41,8 @@ namespace vclcanvas
         maVDev( new VirtualDevice( rRefDevice,
                                    bMonochromeBuffer ) )
     {
+        if( !bMonochromeBuffer )
+            maVDev->SetAntialiasing( ANTIALIASING_ENABLE_B2DDRAW | maVDev->GetAntialiasing() );
     }
 
     OutputDevice& BackBuffer::getOutDev()
