d963aba064b076d3af33cc6c3ed2b0adfc7252cc
Oliver Bolte <obo@openoffice.org>
2008-05-30 09:10:00 +0000

    INTEGRATION: CWS aquabmpfix01 (1.45.28); FILE MERGED 2008/05/13 14:03:27 hdu 1.45.28.1: #100000# remove compile warnings

--- vcl/unx/inc/salgdi.h
+++ vcl/unx/inc/salgdi.h
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: salgdi.h,v $
- * $Revision: 1.45 $
+ * $Revision: 1.46 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -231,6 +231,7 @@ public:
     virtual void            ResetClipRegion();
     virtual void            BeginSetClipRegion( ULONG nCount );
     virtual BOOL            unionClipRegion( long nX, long nY, long nWidth, long nHeight );
+    virtual bool            unionClipRegion( const ::basegfx::B2DPolyPolygon& );
     virtual void            EndSetClipRegion();
 
     virtual void            SetLineColor();
@@ -285,6 +286,8 @@ public:
     virtual void            drawPolyPolygon( sal_uInt32 nPoly,
                                              const sal_uInt32* pPoints,
                                              PCONSTSALPOINT* pPtAry );
+    virtual bool            drawPolyPolygon( const ::basegfx::B2DPolyPolygon&, double fTransparency );
+    virtual bool            drawPolyLine( const ::basegfx::B2DPolygon&, const ::basegfx::B2DVector& rLineWidths );
     virtual sal_Bool        drawPolyLineBezier( ULONG nPoints,
                                                 const SalPoint* pPtAry,
                                                 const BYTE* pFlgAry );
