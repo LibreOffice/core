daa913bcd6f9566ad3114c468490d2ddad7bccac
Oliver Bolte <obo@openoffice.org>
2008-05-30 08:28:30 +0000

    INTEGRATION: CWS notes4 (1.53.4); FILE MERGED 2008/04/17 10:51:14 mod 1.53.4.1: various small fixes

--- sw/source/core/fields/docufld.cxx
+++ sw/source/core/fields/docufld.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: docufld.cxx,v $
- * $Revision: 1.56 $
+ * $Revision: 1.57 $
  *
  * This file is part of OpenOffice.org.
  *
