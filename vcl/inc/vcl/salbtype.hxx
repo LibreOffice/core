49e7c90ca641767a4f7777a1cee55f7b10e52830
Oliver Bolte <obo@openoffice.org>
2008-05-30 09:04:46 +0000

    INTEGRATION: CWS aquabmpfix01 (1.2.318); FILE MERGED 2008/05/09 11:08:55 hdu 1.2.318.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/13 15:42:17 hdu 1.2.318.1: #i87002# allow grey-only AquaSalBitmaps to avoid unneeded conversions

--- vcl/inc/vcl/salbtype.hxx
+++ vcl/inc/vcl/salbtype.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: salbtype.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -219,6 +219,7 @@ public:
     inline BitmapColor&         operator[]( USHORT nIndex );
 
     inline USHORT               GetBestIndex( const BitmapColor& rCol ) const;
+    bool                        IsGreyPalette() const;
 };
 
 // ---------------
