a3c9b7383eae5410544b5a7906fc7a77e0ce12e5
Oliver Bolte <obo@openoffice.org>
2008-05-30 06:58:58 +0000

    INTEGRATION: CWS aquavcl07 (1.4.4); FILE MERGED 2008/04/29 18:14:33 pl 1.4.4.1: #i88447# add dockmenu entries from quickstarter

--- vcl/aqua/inc/vclnsapp.h
+++ vcl/aqua/inc/vclnsapp.h
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: vclnsapp.h,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -58,6 +58,7 @@
 -(void)scrollbarSettingsChanged: (NSNotification*) pNotification;
 -(void)addFallbackMenuItem: (NSMenuItem*)pNewItem;
 -(void)removeFallbackMenuItem: (NSMenuItem*)pOldItem;
+-(void)addDockMenuItem: (NSMenuItem*)pNewItem;
 @end
 
 
