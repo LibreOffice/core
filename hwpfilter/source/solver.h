b44e7daaa419a6b48bb151b34dcbbc86c9408751
Vladimir Glazounov <vg@openoffice.org>
2008-06-04 09:03:25 +0000

    INTEGRATION: CWS sw30bf04 (1.3.2); FILE MERGED 2008/04/16 14:07:56 ama 1.3.2.1: Patch #i86356#: Remove unused methods

--- hwpfilter/source/solver.h
+++ hwpfilter/source/solver.h
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: solver.h,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,73 +31,6 @@
 #ifndef _SOLVER_H_
 #define _SOLVER_H_
 
-class mgcLinearSystem
-{
-public:
-  mgcLinearSystem() {;}
-
-  float** NewMatrix (int N);
-  void DeleteMatrix (int N, float** A);
-  float* NewVector (int N);
-  void DeleteVector (int N, float* B);
-
-  int Inverse (int N, float** A);
-  // Input:
-  //     A[N][N], entries are A[row][col]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     A[N][N], inverse matrix
-
-  int Solve (int N, float** A, float* b);
-  // Input:
-  //     A[N][N] coefficient matrix, entries are A[row][col]
-  //     b[N] vector, entries are b[row]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     A[N][N] is inverse matrix
-  //     b[N] is solution x to Ax = b
-
-  int SolveTri (int N, float* a, float* b, float* c, float* r, float* u);
-  // Input:
-  //     Matrix is tridiagonal.
-  //     Lower diagonal a[N-1]
-  //     Main  diagonal b[N]
-  //     Upper diagonal c[N-1]
-  //     Right-hand side r[N]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     u[N] is solution
-
-  int SolveConstTri (int N, float a, float b, float c, float* r, float* u);
-  // Input:
-  //     Matrix is tridiagonal.
-  //     Lower diagonal is constant, a
-  //     Main  diagonal is constant, b
-  //     Upper diagonal is constant, c
-  //     Right-hand side r[N]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     u[N] is solution
-
-  int SolveSymmetric (int N, float** A, float* b);
-  // Input:
-  //     A[N][N] symmetric coefficient matrix, entries are A[row][col]
-  //     b[N] vector, entries are b[row]
-  // Output:
-  //     return value is TRUE if successful, FALSE if (nearly) singular
-  //     decomposition A = L D L^t (diagonal terms of L are all 1)
-  //         A[i][i] = entries of diagonal D
-  //         A[i][j] for i > j = lower triangular part of L
-  //     b[N] is solution to x to Ax = b
-
-  int SymmetricInverse (int N, float** A, float** Ainv);
-  // Input:
-  //     A[N][N], entries are A[row][col]
-  // Output:
-  //     return value is TRUE if successful, FALSE if algorithm failed
-  //     Ainv[N][N], inverse matrix
-};
-
 class mgcLinearSystemD
 {
 public:
@@ -106,14 +39,6 @@ public:
   double** NewMatrix (int N);
   void DeleteMatrix (int N, double** A);
   double* NewVector (int N);
-  void DeleteVector (int N, double* B);
-
-  int Inverse (int N, double** A);
-  // Input:
-  //     A[N][N], entries are A[row][col]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     A[N][N], inverse matrix
 
   int Solve (int N, double** A, double* b);
   // Input:
@@ -123,48 +48,6 @@ public:
   //     return value is TRUE if successful, FALSE if pivoting failed
   //     A[N][N] is inverse matrix
   //     b[N] is solution x to Ax = b
-
-  int SolveTri (int N, double* a, double* b, double* c, double* r,
-      double* u);
-  // Input:
-  //     Matrix is tridiagonal.
-  //     Lower diagonal a[N-1]
-  //     Main  diagonal b[N]
-  //     Upper diagonal c[N-1]
-  //     Right-hand side r[N]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     u[N] is solution
-
-  int SolveConstTri (int N, double a, double b, double c, double* r,
-      double* u);
-  // Input:
-  //     Matrix is tridiagonal.
-  //     Lower diagonal is constant, a
-  //     Main  diagonal is constant, b
-  //     Upper diagonal is constant, c
-  //     Right-hand side r[N]
-  // Output:
-  //     return value is TRUE if successful, FALSE if pivoting failed
-  //     u[N] is solution
-
-  int SolveSymmetric (int N, double** A, double* b);
-  // Input:
-  //     A[N][N] symmetric coefficient matrix, entries are A[row][col]
-  //     b[N] vector, entries are b[row]
-  // Output:
-  //     return value is TRUE if successful, FALSE if (nearly) singular
-  //     decomposition A = L D L^t (diagonal terms of L are all 1)
-  //         A[i][i] = entries of diagonal D
-  //         A[i][j] for i > j = lower triangular part of L
-  //     b[N] is solution to x to Ax = b
-
-  int SymmetricInverse (int N, double** A, double** Ainv);
-  // Input:
-  //     A[N][N], entries are A[row][col]
-  // Output:
-  //     return value is TRUE if successful, FALSE if algorithm failed
-  //     Ainv[N][N], inverse matrix
 };
 
 #endif
