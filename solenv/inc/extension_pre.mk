ac0b8fdd04df15e4d4a7c2b7394f771d0f3fdefa
Rüdiger Timm <rt@openoffice.org>
2008-06-06 07:11:07 +0000

    INTEGRATION: CWS ause094 (1.1.2); FILE ADDED 2008/06/03 12:07:08 hjs 1.1.2.6: #i83636# - minor fixes 2008/06/02 10:29:50 hjs 1.1.2.5: #i83636# - more of that 2008/05/26 12:48:23 hjs 1.1.2.4: #i83636# - work on targets 2008/05/19 10:29:16 hjs 1.1.2.3: #i88602# minor changes 2008/05/16 15:22:01 hjs 1.1.2.2: #i88602# changed .xcu handling 2008/05/15 14:59:58 hjs 1.1.2.1: #i88602# common definitions/targets for extension packing

---
+++ solenv/inc/extension_pre.mk
@@ -0,0 +1,57 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: extension_pre.mk,v $
+#
+# $Revision: 1.2 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+EXTENSIONDIR*:=$(MISC)$/$(EXTENSIONNAME)
+
+DESCRIPTION:=$(EXTENSIONDIR)$/description.xml
+DESCRIPTION_SRC*:=description.xml
+
+COMPONENT_MANIFEST*:=$(EXTENSIONDIR)$/META-INF$/manifest.xml
+MANIFEST_SRC*:=manifest.xml
+
+.IF "$(GUI)" == "WIN" || "$(GUI)" == "WNT"
+PACKLICS*:=$(foreach,i,$(alllangiso) $(EXTENSIONDIR)$/registration$/license_$i.txt)
+COMPONENT_LIC_TEMPL*:=registration/license_xxx.txt
+.ELSE
+PACKLICS*:=$(foreach,i,$(alllangiso) $(EXTENSIONDIR)$/registration$/LICENSE_$i)
+COMPONENT_LIC_TEMPL*:=registration/LICENSE_xxx
+.ENDIF
+
+#TODO:  check ZIP9TARGET for previous use!
+common_build_zip=
+MANIFEST_DEPS=	$(PACKLICS) $(DESCRIPTION) $(COMPONENT_FILES) $(COMPONENT_LIBRARIES) $(COMPONENT_JARFILES) $(COMPONENT_MERGED_XCU) $(EXTENSION_PACKDEPS)
+ZIP9DEPS=		$(PACKLICS) $(DESCRIPTION) $(COMPONENT_FILES) $(COMPONENT_LIBRARIES) $(COMPONENT_MANIFEST) $(COMPONENT_JARFILES) $(COMPONENT_MERGED_XCU) $(EXTENSION_PACKDEPS)
+ZIP9TARGET=		$(EXTENSION_ZIPNAME)
+ZIP9DIR=		$(EXTENSIONDIR)
+ZIP9EXT=		.oxt
+ZIP9FLAGS=-r
+ZIP9LIST=		*
+
