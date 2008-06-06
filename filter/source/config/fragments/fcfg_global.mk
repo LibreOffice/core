69aa99f7c442f8a25c552ff97d0b7d69dd0cb465
Rüdiger Timm <rt@openoffice.org>
2008-06-06 08:32:41 +0000

    INTEGRATION: CWS native151 (1.10.362); FILE MERGED 2008/05/06 15:55:26 is 1.10.362.1: #i79800# binfilter into an own package

--- filter/source/config/fragments/fcfg_global.mk
+++ filter/source/config/fragments/fcfg_global.mk
@@ -1,28 +1,8 @@
 all_fragments+=global
 
-
-.IF "$(WITH_BINFILTER)" != "NO"
-T4_GLOBAL_BINFILTER = \
-    writer_StarWriter_30 \
-    writer_StarWriter_40 \
-    writer_globaldocument_StarWriter_40GlobalDocument \
-    writer_StarWriter_50 \
-    writer_globaldocument_StarWriter_50GlobalDocument
-F4_GLOBAL_BINFILTER = \
-    StarWriter_3_0__StarWriter_GlobalDocument_ \
-    StarWriter_4_0__StarWriter_GlobalDocument_ \
-    StarWriter_4_0_GlobalDocument \
-    StarWriter_5_0__StarWriter_GlobalDocument_ \
-    StarWriter_5_0_GlobalDocument
-.ELSE
-T4_GLOBAL_BINFILTER =
-F4_GLOBAL_BINFILTER =
-.ENDIF
-
 # -----------------------------------------------
 # count = 12
 T4_GLOBAL = \
-    $(T4_GLOBAL_BINFILTER) \
     writer_Text \
     writer_StarOffice_XML_Writer \
     writer_globaldocument_StarOffice_XML_Writer_GlobalDocument \
@@ -32,7 +12,6 @@ T4_GLOBAL = \
 # -----------------------------------------------
 # count = 12
 F4_GLOBAL = \
-    $(F4_GLOBAL_BINFILTER) \
     Text__encoded___StarWriter_GlobalDocument_ \
     writer_globaldocument_StarOffice_XML_Writer \
     writer_globaldocument_StarOffice_XML_Writer_GlobalDocument \
@@ -44,8 +23,6 @@ F4_GLOBAL = \
 # -----------------------------------------------
 # count = 12
 F4_UI_GLOBAL = \
-    StarWriter_4_0_GlobalDocument_ui \
-    StarWriter_5_0_GlobalDocument_ui \
     Text__encoded___StarWriter_GlobalDocument__ui \
     writer_globaldocument_StarOffice_XML_Writer_ui \
     writer_globaldocument_StarOffice_XML_Writer_GlobalDocument_ui \
