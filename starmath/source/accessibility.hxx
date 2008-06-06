ba945e7702b1cdec6f2beb92ab5354a65a8e5c0f
Rüdiger Timm <rt@openoffice.org>
2008-06-06 10:46:28 +0000

    INTEGRATION: CWS impressodf12 (1.21.8); FILE MERGED 2008/05/26 16:16:57 cl 1.21.8.2: RESYNC: (1.21-1.22); FILE MERGED 2008/04/28 11:34:51 cl 1.21.8.1: changed depth from USHORT to sal_Int16

--- starmath/source/accessibility.hxx
+++ starmath/source/accessibility.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: accessibility.hxx,v $
- * $Revision: 1.22 $
+ * $Revision: 1.23 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -259,8 +259,8 @@ public:
     virtual sal_Bool        InsertText( const String&, const ESelection& );
     virtual sal_Bool        QuickFormatDoc( BOOL bFull=FALSE );
 
-    virtual USHORT          GetDepth( USHORT nPara ) const;
-    virtual sal_Bool        SetDepth( USHORT nPara, USHORT nNewDepth );
+    virtual sal_Int16       GetDepth( USHORT nPara ) const;
+    virtual sal_Bool        SetDepth( USHORT nPara, sal_Int16 nNewDepth );
 
     virtual const SfxItemSet*   GetEmptyItemSetPtr();
     // implementation functions for XParagraphAppend and XTextPortionAppend
