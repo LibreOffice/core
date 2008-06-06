623483975b9c95b82663562d6249a11109ffc00b
Rüdiger Timm <rt@openoffice.org>
2008-06-06 10:50:37 +0000

    INTEGRATION: CWS shutup2 (1.2.136); FILE MERGED 2008/04/16 14:47:52 rt 1.2.136.2: RESYNC: (1.2-1.3); FILE MERGED 2008/02/27 08:46:13 rt 1.2.136.1: #i86502# SOLARINCDIR/offuh already is part of standard include path, no need to set it here a second time.

--- starmath/inc/makefile.mk
+++ starmath/inc/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -36,7 +36,6 @@ TARGET=inc
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 # --- Files --------------------------------------------------------
 # --- Targets -------------------------------------------------------
