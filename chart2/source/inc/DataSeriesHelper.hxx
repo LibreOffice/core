2afb1638f202c71120fede40f7602d039eecf98d
Rüdiger Timm <rt@openoffice.org>
2008-06-06 08:11:28 +0000

    INTEGRATION: CWS chart25 (1.6.10); FILE MERGED 2008/04/25 14:44:15 iha 1.6.10.1: #i88742# make default lines a bit stronger for line charts

--- chart2/source/inc/DataSeriesHelper.hxx
+++ chart2/source/inc/DataSeriesHelper.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: DataSeriesHelper.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -161,6 +161,9 @@ void switchLinesOnOrOff(
     const ::com::sun::star::uno::Reference<
         ::com::sun::star::beans::XPropertySet > & xSeriesProperties,
     bool bLinesOn );
+
+void makeLinesThickOrThin( const ::com::sun::star::uno::Reference<
+        ::com::sun::star::beans::XPropertySet > & xSeriesProperties, bool bThick );
 
 void setPropertyAlsoToAllAttributedDataPoints(
     const ::com::sun::star::uno::Reference<
