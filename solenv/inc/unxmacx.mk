4ec2761e4905299cfcf0d54854ca4390f6177555
Rüdiger Timm <rt@openoffice.org>
2008-06-09 11:40:04 +0000

    INTEGRATION: CWS sb87 (1.31.40); FILE MERGED 2008/05/07 11:53:33 sb 1.31.40.1: #i88687# added RPATH mode NONE

--- solenv/inc/unxmacx.mk
+++ solenv/inc/unxmacx.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: unxmacx.mk,v $
 #
-# $Revision: 1.32 $
+# $Revision: 1.33 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -198,6 +198,7 @@ LINKFLAGSRUNPATH_UREBIN=
 LINKFLAGSRUNPATH_OOO=-install_name '@__________________________________________________OOO$/$(@:f)'
 LINKFLAGSRUNPATH_BRAND=
 LINKFLAGSRUNPATH_OXT=
+LINKFLAGSRUNPATH_NONE=
 LINKFLAGS=$(LINKFLAGSDEFS)
 
 # [ed] 5/14/02 If we're building for aqua, add in the objc runtime library into our link line
