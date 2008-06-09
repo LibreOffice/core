af88093aa1307a6fbdc2ded8969ada698c117d7c
Rüdiger Timm <rt@openoffice.org>
2008-06-09 11:54:15 +0000

    INTEGRATION: CWS sb87 (1.14.16); FILE MERGED 2008/05/09 15:09:40 sb 1.14.16.1: #i88211# python start wrapper moved to brand layer

--- scp2/source/python/makefile.mk
+++ scp2/source/python/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.14 $
+# $Revision: 1.15 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -57,8 +57,7 @@ PARFILES=\
         module_python.par              \
         module_python_mailmerge.par    \
         profileitem_python.par         \
-        file_python.par                \
-        shortcut_python.par
+        file_python.par
 
 ULFFILES= \
         module_python.ulf              \
