1a937e1d3ca3ae033ca0a6ba650481d70f394212
Rüdiger Timm <rt@openoffice.org>
2008-06-09 11:39:39 +0000

    INTEGRATION: CWS sb87 (1.21.44); FILE MERGED 2008/05/07 11:53:33 sb 1.21.44.1: #i88687# added RPATH mode NONE

--- solenv/inc/unxlngs3904.mk
+++ solenv/inc/unxlngs3904.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: unxlngs3904.mk,v $
 #
-# $Revision: 1.21 $
+# $Revision: 1.22 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -141,6 +141,7 @@ LINKFLAGSRUNPATH_UREBIN=-Wl,-rpath,\''$$ORIGIN/../lib:$$ORIGIN'\'
 LINKFLAGSRUNPATH_OOO=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../ure-link/lib'\'
 LINKFLAGSRUNPATH_BRAND=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../basis-link/program:$$ORIGIN/../basis-link/ure-link/lib'\'
 LINKFLAGSRUNPATH_OXT=
+LINKFLAGSRUNPATH_NONE=
 LINKFLAGS=-Wl,-z,combreloc $(LINKFLAGSDEFS)
 
 # linker flags for linking applications
