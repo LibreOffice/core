1f2cc8c2f38241ff7ad160d025a1cbe61795d4f0
Rüdiger Timm <rt@openoffice.org>
2008-06-13 11:47:06 +0000

    INTEGRATION: CWS qadev32 (1.7.2); FILE MERGED 2008/05/28 12:19:15 cn 1.7.2.3: #i87389# enhance documentation 2008/04/17 11:40:43 cn 1.7.2.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/25 09:33:21 cn 1.7.2.1: #i87389# new parameter: NoCwsAttach

--- qadevOOo/runner/util/PropertyName.java
+++ qadevOOo/runner/util/PropertyName.java
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: PropertyName.java,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -29,49 +29,132 @@
  ************************************************************************/
 package util;
 
+/**
+ * This interfaces describes some key names which are used in <CODE>lib.TestParameters</CODE>.
+ */
+
 public interface PropertyName {
+    /**
+     * parameter name: "AppExecutionCommand"
+     */
     final public static String APP_EXECUTION_COMMAND = "AppExecutionCommand";
+    /**
+     * parameter name: "ConnectionString"
+     */
     final public static String CONNECTION_STRING = "ConnectionString";
+    /**
+     * parameter name: "TestBase"
+     */
     final public static String TEST_BASE = "TestBase";
+    /**
+     * parameter name: "TestDocumentPath"
+     */
     final public static String TEST_DOCUMENT_PATH = "TestDocumentPath";
+    /**
+     * parameter name: "LoggingIsActive"
+     */
     final public static String LOGGING_IS_ACTIVE = "LoggingIsActive";
+    /**
+     * parameter name: "DebugIsActive"
+     */
     final public static String DEBUG_IS_ACTIVE = "DebugIsActive";
+    /**
+     * parameter name: "OutProduceer"
+     */
     final public static String OUT_PRODUCER = "OutProducer";
+    /**
+     * parameter name: "ShortWait"
+     */
     final public static String SHORT_WAIT= "ShortWait";
+    /**
+     * internal only, no parameter
+     */
     final public static String OFFICE_PROVIDER = "OfficeProvider";
+    /**
+     * internal only, no parameter
+     */
     final public static String OFFICE_WATCHER = "Watcher";
+    /**
+     * internal only, no parameter
+     */
     final public static String LOG_WRITER = "LogWriter";
+    /**
+     * parameter name: "TimeOut"<p>
+     * time out given in milli seconds
+     */
     final public static String TIME_OUT = "TimeOut";
+    /**
+     * parameter name: "ThreadTimeOut"
+     */
     final public static String THREAD_TIME_OUT = "ThreadTimeOut";
+    /**
+     * parameter name: "OfficeCloseTimeOut"
+     */
     final public static String OFFICE_CLOSE_TIME_OUT = "OfficeCloseTimeOut";
+    /**
+     * parameter name: "OperatingSystem"
+     */
     final public static String OPERATING_SYSTEM = "OperatingSystem";
+    /**
+     * parameter name: "AutoRestart"
+     */
     final public static String AUTO_RESTART = "AutoRestart";
+    /**
+     * parameter name: "NewOfficeInstance"
+     */
     final public static String NEW_OFFICE_INSTANCE = "NewOfficeInstance";
+    /**
+     * parameter name: "KeepDocument"
+     */
     final public static String KEEP_DOCUMENT = "KeepDocument";
     /**
+     * parameter name: "OOO_EnvSet"<p>
      * path to the configuration file to create an build environment
      */
     final public static String OOO_ENVSET = "OOO_EnvSet";
     /**
+     * parameter name: "SRC_ROOT"<p>
      * path to the source root of OpenOffice.org
      */
     final public static String SRC_ROOT = "SRC_ROOT";
-
+    /**
+     * parameter name: "Version"<p>
+     * the name of the version to test
+     */
     final public static String VERSION = "Version";
     /**
+     * parameter name: "COMP_ENV"<p>
      * like unxsols4, unxsoli4, unxlngi5, wntmsci10
      */
     final public static String COMP_ENV = "COMP_ENV";
 
     /**
-     * Path to a shel
+     * parameter name "Shell"<p>
+     * Path to a shell.
      * This shell is used to run some commands outside of Java
      * example: /bin/tcsh c:\\myShell\\myShell.exe
      */
     final public static String SHELL = "Shell";
-
+    /**
+     * parameter name: "NoCwsAttach"<p>
+     * If this paraeter is set to "true" , a status of CWS-UnoAPI-Tests was not attached to EIS<p>
+     * @see tests.complex.unoapi.CheckModuleAPI
+     */
+    final public static String NO_CWS_ATTACH = "NoCwsAttach";
+    /**
+     * internal only, no parameter
+     */
     final public static String WNTMSCI = "wntmsci";
+    /**
+     * internal only, no parameter
+     */
     final public static String UNXLNGI = "unxlngi";
+    /**
+     * internal only, no parameter
+     */
     final public static String UNXSOLS = "unxsols";
+    /**
+     * internal only, no parameter
+     */
     final public static String UNXSOLI = "unxsoli";
 }
