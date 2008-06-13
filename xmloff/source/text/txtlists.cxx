5d523a1d94826550a03dc7a0068ca4b7d7f0a90d
Rüdiger Timm <rt@openoffice.org>
2008-06-13 08:21:15 +0000

    INTEGRATION: CWS swlists01 (1.1.2); FILE ADDED 2008/05/28 09:14:13 od 1.1.2.6: #i86732# performance improvement by late creation of internal data structures 2008/05/21 12:20:32 od 1.1.2.5: #i86732# further adjustments/changes for new list handling for 	 export to OpenOffice.org file format and ODF 1.0/1.1 2008/05/14 10:31:40 od 1.1.2.4: #i86732# further changes/adjustments for new list handling 2008/05/13 11:39:00 od 1.1.2.3: #i86732# further changes/adjustments for new list handling 2008/05/09 12:36:11 od 1.1.2.2: #i86732# new license header 2008/05/08 14:51:32 od 1.1.2.1: #i86732# implementation of new helper class <XMLTextListsHelper>

---
+++ xmloff/source/text/txtlists.cxx
@@ -0,0 +1,215 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: txtlists.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "precompiled_xmloff.hxx"
+
+#include <txtlists.hxx>
+
+#include <tools/debug.hxx>
+#include <tools/date.hxx>
+#include <tools/time.hxx>
+
+XMLTextListsHelper::XMLTextListsHelper()
+    : mpProcessedLists( 0 ),
+      msLastProcessedListId(),
+      msListStyleOfLastProcessedList(),
+      mpContinuingLists( 0 ),
+      mpListStack( 0 )
+{
+}
+
+XMLTextListsHelper::~XMLTextListsHelper()
+{
+    if ( mpProcessedLists )
+    {
+        mpProcessedLists->clear();
+        delete mpProcessedLists;
+    }
+    if ( mpContinuingLists )
+    {
+        mpContinuingLists->clear();
+        delete mpContinuingLists;
+    }
+    if ( mpListStack )
+    {
+        mpListStack->clear();
+        delete mpListStack;
+    }
+}
+
+void XMLTextListsHelper::KeepListAsProcessed( ::rtl::OUString sListId,
+                                              ::rtl::OUString sListStyleName,
+                                              ::rtl::OUString sContinueListId )
+{
+    if ( IsListProcessed( sListId ) )
+    {
+        DBG_ASSERT( false,
+                    "<XMLTextListsHelper::AddList(..)> - list id already added" );
+        return;
+    }
+
+    if ( mpProcessedLists == 0 )
+    {
+        mpProcessedLists = new tMapForLists();
+    }
+
+    ::std::pair< ::rtl::OUString, ::rtl::OUString >
+                                aListData( sListStyleName, sContinueListId );
+    (*mpProcessedLists)[ sListId ] = aListData;
+
+    msLastProcessedListId = sListId;
+    msListStyleOfLastProcessedList = sListStyleName;
+}
+
+sal_Bool XMLTextListsHelper::IsListProcessed( const ::rtl::OUString sListId ) const
+{
+    if ( mpProcessedLists == 0 )
+    {
+        return sal_False;
+    }
+
+    return mpProcessedLists->find( sListId ) != mpProcessedLists->end();
+}
+
+::rtl::OUString XMLTextListsHelper::GetListStyleOfProcessedList(
+                                            const ::rtl::OUString sListId ) const
+{
+    if ( mpProcessedLists != 0 )
+    {
+        tMapForLists::const_iterator aIter = mpProcessedLists->find( sListId );
+        if ( aIter != mpProcessedLists->end() )
+        {
+            return (*aIter).second.first;
+        }
+    }
+
+    return ::rtl::OUString();
+}
+
+::rtl::OUString XMLTextListsHelper::GetContinueListIdOfProcessedList(
+                                            const ::rtl::OUString sListId ) const
+{
+    if ( mpProcessedLists != 0 )
+    {
+        tMapForLists::const_iterator aIter = mpProcessedLists->find( sListId );
+        if ( aIter != mpProcessedLists->end() )
+        {
+            return (*aIter).second.second;
+        }
+    }
+
+    return ::rtl::OUString();
+}
+
+const ::rtl::OUString& XMLTextListsHelper::GetLastProcessedListId() const
+{
+    return msLastProcessedListId;
+}
+
+const ::rtl::OUString& XMLTextListsHelper::GetListStyleOfLastProcessedList() const
+{
+    return msListStyleOfLastProcessedList;
+}
+
+::rtl::OUString XMLTextListsHelper::GenerateNewListId() const
+{
+    sal_Int64 n = Time().GetTime();
+    n += Date().GetDate();
+    n += rand();
+    ::rtl::OUString sTmpStr( ::rtl::OUString::valueOf( n ) );
+
+    long nHitCount = 0;
+    ::rtl::OUString sNewListId( sTmpStr );
+    if ( mpProcessedLists != 0 )
+    {
+        while ( mpProcessedLists->find( sNewListId ) != mpProcessedLists->end() )
+        {
+            ++nHitCount;
+            sNewListId = sTmpStr;
+            sNewListId += ::rtl::OUString::valueOf( nHitCount );
+        }
+    }
+
+    return sNewListId;
+}
+
+void XMLTextListsHelper::StoreLastContinuingList( ::rtl::OUString sListId,
+                                                  ::rtl::OUString sContinuingListId )
+{
+    if ( mpContinuingLists == 0 )
+    {
+        mpContinuingLists = new tMapForContinuingLists();
+    }
+
+    (*mpContinuingLists)[ sListId ] = sContinuingListId;
+}
+
+::rtl::OUString XMLTextListsHelper::GetLastContinuingListId(
+                                                ::rtl::OUString sListId ) const
+{
+    if ( mpContinuingLists != 0)
+    {
+        tMapForContinuingLists::const_iterator aIter =
+                                                mpContinuingLists->find( sListId );
+        if ( aIter != mpContinuingLists->end() )
+        {
+            return (*aIter).second;
+        }
+    }
+
+    return sListId;
+}
+
+void XMLTextListsHelper::PushListOnStack( ::rtl::OUString sListId,
+                                          ::rtl::OUString sListStyleName )
+{
+    if ( mpListStack == 0 )
+    {
+        mpListStack = new tStackForLists();
+    }
+    ::std::pair< ::rtl::OUString, ::rtl::OUString >
+                                aListData( sListId, sListStyleName );
+    mpListStack->push_back( aListData );
+}
+void XMLTextListsHelper::PopListFromStack()
+{
+    if ( mpListStack != 0 &&
+         mpListStack->size() > 0 )
+    {
+        mpListStack->pop_back();
+    }
+}
+
+sal_Bool XMLTextListsHelper::EqualsToTopListStyleOnStack( const ::rtl::OUString sListId ) const
+{
+    return mpListStack != 0
+           ? sListId == mpListStack->back().second
+           : sal_False;
+}
