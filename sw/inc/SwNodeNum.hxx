58b18a81172732a98339830535a466ed0dfa187b
Rüdiger Timm <rt@openoffice.org>
2008-06-13 08:25:10 +0000

    INTEGRATION: CWS swlists01 (1.7.390); FILE MERGED 2008/05/08 16:16:53 od 1.7.390.8: RESYNC: (1.8-1.9); FILE MERGED 2008/05/07 07:12:40 od 1.7.390.7: #i86732# further changes/adjustments for new list handling 2008/04/16 08:15:44 od 1.7.390.6: #i86732# further changes/adjustments for new list handling 2008/03/20 17:12:14 od 1.7.390.5: correction of merge 2008/03/20 16:12:12 od 1.7.390.4: correction of merge conflict solution 2008/03/20 14:27:16 od 1.7.390.3: RESYNC: (1.7-1.8); FILE MERGED 2008/03/20 07:46:22 od 1.7.390.2: #i86732# class <SwNodeNum>: remove no longer needed methods 2008/03/06 07:48:14 od 1.7.390.1: #i86732# class <SwNumberTreeNode> and <SwNodeNum> 	 - refactoring of class interfaces

--- sw/inc/SwNodeNum.hxx
+++ sw/inc/SwNodeNum.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: SwNodeNum.hxx,v $
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -40,50 +40,16 @@ class SwNumFmt;
 
 class SW_DLLPUBLIC SwNodeNum : public SwNumberTreeNode
 {
-    SwTxtNode * mpTxtNode;
-    SwNumRule * mpNumRule;
-
-    tSwNumTreeNumber mnStart;
-    bool mbRestart;
-
-    // --> OD 2006-03-07 #131436#
-    static void _UnregisterMeAndChildrenDueToRootDelete( SwNodeNum& rNodeNum );
-    // <--
-protected:
-//    void SetTxtNode(SwTxtNode * pTxtNode);
-
-    // --> OD 2006-04-26 #i64010#
-    virtual bool HasCountedChildren() const;
-    virtual bool IsCountedForNumbering() const;
-    // <--
-
 public:
-    SwNodeNum();
-    // --> OD 2007-10-26 #i83479#
+    // --> OD 2008-02-19 #refactorlists#
     explicit SwNodeNum( SwTxtNode* pTxtNode );
-    explicit SwNodeNum( SwNumRule* pRule );
+    explicit SwNodeNum( SwNumRule* pNumRule );
     // <--
-    SwNodeNum( const SwNodeNum & rNodeNum );
-
-    // --> OD 2007-10-26 #i83479# - made public
-    SwTxtNode* GetTxtNode() const;
-    SwNumRule* GetNumRule() const;
-    void SetNumRule(SwNumRule * pRule);
-    // <--
-
     virtual ~SwNodeNum();
 
-    virtual SwNumberTreeNode * Create() const;
-
-    virtual SwNumberTreeNode * Copy() const;
-
-    // --> OD 2007-10-25 #i83479#
-    virtual void AddChild(SwNumberTreeNode * pChild, unsigned int nDepth = 0);
-    // <--
-
-    virtual void RemoveChild(SwNumberTreeNode * pChild);
-
-    virtual bool IsNotifiable() const;
+    SwNumRule* GetNumRule() const;
+    void ChangeNumRule( SwNumRule& rNumRule );
+    SwTxtNode* GetTxtNode() const;
 
     virtual bool IsNotificationEnabled() const;
 
@@ -91,35 +57,15 @@ public:
 
     virtual bool IsCounted() const;
 
-    virtual bool IsCountPhantoms() const;
-
-    virtual void NotifyNode();
-
     virtual bool LessThan(const SwNumberTreeNode & rNode) const;
-
-    void SetRestart(bool bRestart);
 
     virtual bool IsRestart() const;
 
-    void SetStart(tSwNumTreeNumber nStart);
+    virtual SwNumberTree::tSwNumTreeNumber GetStartValue() const;
 
-    virtual tSwNumTreeNumber GetStart() const;
-
-    String ToString() const;
-
-    void SetLevel(unsigned int nLevel);
+//    String ToString() const;
 
     SwPosition GetPosition() const;
-
-//    friend class SwTxtNode;
-//    friend class SwNumRule;
-
-    // --> OD 2005-11-16 #i57919# - direct access on <mnStart>, needed for HTML export
-    inline const tSwNumTreeNumber GetStartValue() const
-    {
-        return mnStart;
-    }
-    // <--
 
     // --> OD 2006-03-07 #131436#
     // The number tree root node is deleted, when the corresponding numbering
@@ -139,6 +85,37 @@ public:
         @author OD
     */
     const SwNodeNum* GetPrecedingNodeNumOf( const SwTxtNode& rTxtNode ) const;
+
+protected:
+    virtual SwNumberTreeNode * Create() const;
+
+    // --> OD 2006-04-26 #i64010#
+    virtual bool HasCountedChildren() const;
+    virtual bool IsCountedForNumbering() const;
+    // <--
+
+    // --> OD 2008-02-19 #refactorlists#
+    // method called before this tree node has been added to the list tree
+    virtual void PreAdd();
+    // method called at a child after this child has been removed from the list tree
+    virtual void PostRemove();
+    // <--
+private:
+    SwTxtNode * mpTxtNode;
+    SwNumRule * mpNumRule;
+
+    // --> OD 2006-03-07 #131436#
+    static void _UnregisterMeAndChildrenDueToRootDelete( SwNodeNum& rNodeNum );
+    // <--
+
+    SwNodeNum( const SwNodeNum& );              // no copy constructor
+    SwNodeNum& operator=( const SwNodeNum& );   // no assignment operator
+
+    virtual bool IsCountPhantoms() const;
+
+    virtual bool IsNotifiable() const;
+
+    virtual void NotifyNode();
 };
 
 #endif // _SW_NODE_NUM_HXX
