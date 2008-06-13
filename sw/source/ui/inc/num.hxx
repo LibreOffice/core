5a822cd89f59405ead4490de16a21c3d7239a062
Rüdiger Timm <rt@openoffice.org>
2008-06-13 10:15:57 +0000

    INTEGRATION: CWS swlists01 (1.10.164); FILE MERGED 2008/05/19 07:55:19 od 1.10.164.5: RESYNC: (1.12-1.13); FILE MERGED 2008/05/14 12:59:10 od 1.10.164.4: #i86732# remove no longer needed code 2008/05/08 16:21:31 od 1.10.164.3: RESYNC: (1.11-1.12); FILE MERGED 2008/03/20 14:18:50 od 1.10.164.2: RESYNC: (1.10-1.11); FILE MERGED 2008/03/06 08:30:39 od 1.10.164.1: #i86732# class <SwNumPositionTabPage> 	 - refactoring: remove unused member <bHasChild>

--- sw/source/ui/inc/num.hxx
+++ sw/source/ui/inc/num.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: num.hxx,v $
- * $Revision: 1.13 $
+ * $Revision: 1.14 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -130,7 +130,6 @@ class SwNumPositionTabPage : public SfxTabPage
     USHORT              nActNumLvl;
 
     BOOL                bModified           : 1;
-    BOOL                bHasChild           : 1;
     BOOL                bPreset             : 1;
     BOOL                bInInintControl     : 1;  //Modify-Fehler umgehen, soll ab 391 behoben sein
     // --> OD 2008-02-01 #newlistlevelattrs#
