d4db5b3a54b692658799b4cc868a0143337a5145
Rüdiger Timm <rt@openoffice.org>
2008-06-13 08:30:27 +0000

    INTEGRATION: CWS swlists01 (1.5.280); FILE MERGED 2008/05/08 16:25:56 od 1.5.280.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/06 09:34:33 od 1.5.280.1: #i86732# refactoring due to revised undo class interfaces

--- sw/source/core/attr/cellatr.cxx
+++ sw/source/core/attr/cellatr.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: cellatr.cxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -207,8 +207,7 @@ void SwTblBoxFormula::ChangeState( const SfxPoolItem* pItem )
                     // und dann in der externen Darstellung
                     aCopy.PtrToBoxNm( &pTblNd->GetTable() );
                     pUpdtFld->pHistory->Add( &aCopy, &aCopy,
-                                pNd->FindTableBoxStartNode()->GetIndex(),
-                                const_cast<SwDoc&>(*(pNd->GetDoc())) );
+                                pNd->FindTableBoxStartNode()->GetIndex() );
                 }
             }
             else
