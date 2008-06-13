312fec95be72e106bbb5c79541d8aef195323115
Rüdiger Timm <rt@openoffice.org>
2008-06-13 08:29:11 +0000

    INTEGRATION: CWS swlists01 (1.61.130); FILE MERGED 2008/05/08 16:16:15 od 1.61.130.2: RESYNC: (1.61-1.62); FILE MERGED 2008/05/07 07:15:00 od 1.61.130.1: #i86732# UNO-API for new list handling

--- sw/inc/unomap.hxx
+++ sw/inc/unomap.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: unomap.hxx,v $
- * $Revision: 1.62 $
+ * $Revision: 1.63 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -318,6 +318,7 @@
 #define WID_RULE_NAME                   2
 #define WID_IS_ABS_MARGINS              3
 #define WID_IS_OUTLINE                  4
+#define WID_DEFAULT_LIST_ID             5
 
 //TextColumns
 #define WID_TXTCOL_LINE_WIDTH           0
