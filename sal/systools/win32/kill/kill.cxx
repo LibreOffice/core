c7a00ab0e8c7c17c30ba22af33dd51f56312375e
Rüdiger Timm <rt@openoffice.org>
2008-06-13 11:36:26 +0000

    INTEGRATION: CWS qadev32 (1.7.2); FILE MERGED 2008/04/18 14:11:32 hro 1.7.2.1: #i86802# Support kill -9

--- sal/systools/win32/kill/kill.cxx
+++ sal/systools/win32/kill/kill.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: kill.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -380,7 +380,10 @@ int _tmain()
 
         if ( IsValidHandle( hProcess ) )
         {
-            RaiseSignalEx( hProcess, sig );
+            if ( SIGKILL == sig )
+                TerminateProcess( hProcess, 255 );
+            else
+                RaiseSignalEx( hProcess, sig );
             _tprintf( _T("Done\n") );
 
             CloseHandle( hProcess );
