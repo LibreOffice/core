4b5f94adb4ca04805a6c21762530acc6dc1dc11c
Rüdiger Timm <rt@openoffice.org>
2008-06-13 13:08:49 +0000

    INTEGRATION: CWS obr09 (1.2.550); FILE MERGED 2008/05/28 08:23:56 obr 1.2.550.1: #i89711# 64 bit dependencies

--- instsetoo_native/inc_openoffice/unix/find-requires-gnome.sh
+++ instsetoo_native/inc_openoffice/unix/find-requires-gnome.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cat > /dev/null
-echo libgnomevfs-2.so.0
-echo libgconf-2.so.4
+[[ "${OUTPATH}" == unxlngx* ]] && mark64="()(64bit)"
+echo "libgnomevfs-2.so.0${mark64}"
+echo "libgconf-2.so.4${mark64}"
 
