01a82c74b650d3430451497217bf3b0130545f0b
Rüdiger Timm <rt@openoffice.org>
2008-06-16 11:56:27 +0000

    INTEGRATION: CWS rptchart02 (1.5.60); FILE MERGED 2008/04/16 06:35:58 oj 1.5.60.2: RESYNC: (1.5-1.6); FILE MERGED 2008/04/15 12:50:19 oj 1.5.60.1: use comphelper copyProperties

--- chart2/source/tools/ImplUndoManager.cxx
+++ chart2/source/tools/ImplUndoManager.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ImplUndoManager.cxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -179,7 +179,7 @@ void UndoElement::applyModelContentToModel(
             xDestinationTitled->setTitleObject( xSourceTitled->getTitleObject());
 
             // page background
-            PropertyHelper::copyProperties(
+            comphelper::copyProperties(
                 xSource->getPageBackground(),
                 xDestination->getPageBackground() );
 
