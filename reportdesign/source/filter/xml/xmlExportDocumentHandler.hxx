c361a7789140673ba6e0272108addd70f61109e6
Rüdiger Timm <rt@openoffice.org>
2008-06-16 12:34:59 +0000

    INTEGRATION: CWS rptchart02 (1.2.4); FILE MERGED 2008/04/16 06:28:51 oj 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/12 09:45:17 oj 1.2.4.1: impl chart handling

--- reportdesign/source/filter/xml/xmlExportDocumentHandler.hxx
+++ reportdesign/source/filter/xml/xmlExportDocumentHandler.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlExportDocumentHandler.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -97,6 +97,7 @@ private:
     sal_Int32                                                                           m_nCurrentCellIndex;
     bool m_bTableRowsStarted;
     bool m_bFirstRowExported;
+    bool m_bExportChar;
 };
 // -----------------------------------------------------------------------------
 } // namespace rptxml
