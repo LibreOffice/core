1c4da6b5549720b8d2458f74a3f0dacaac929af4
Rüdiger Timm <rt@openoffice.org>
2008-06-16 13:05:28 +0000

    INTEGRATION: CWS sb88 (1.4.10); FILE MERGED 2008/06/03 15:29:53 sb 1.4.10.1: #i89553 applied patch by cmc

--- configmgr/source/treecache/timestamp.cxx
+++ configmgr/source/treecache/timestamp.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: timestamp.cxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -48,13 +48,6 @@ namespace configmgr
         const ::TimeValue maxTimeValue = { maxTimeValueSeconds, maxTimeValueNanos };
 
         return TimeStamp( maxTimeValue );
-    }
-// -------------------------------------------------------------------------
-    TimeStamp TimeStamp::always()
-    {
-        const ::TimeValue minTimeValue = { minTimeValueSeconds, minTimeValueNanos };
-
-        return TimeStamp( minTimeValue );
     }
 // -------------------------------------------------------------------------
     TimeStamp TimeStamp::getCurrentTime()
