c8c7fcb74134320491f3d40e669b0cec7fa911cc
Rüdiger Timm <rt@openoffice.org>
2008-06-16 12:35:56 +0000

    INTEGRATION: CWS rptchart02 (1.4.52); FILE MERGED 2008/04/16 06:28:58 oj 1.4.52.2: RESYNC: (1.4-1.5); FILE MERGED 2008/04/03 06:35:19 oj 1.4.52.1: #i86343# remove unused code

--- reportdesign/source/filter/xml/xmlFormatCondition.cxx
+++ reportdesign/source/filter/xml/xmlFormatCondition.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlFormatCondition.cxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -109,11 +109,6 @@ OXMLFormatCondition::~OXMLFormatCondition()
 {
 
     DBG_DTOR( rpt_OXMLFormatCondition,NULL);
-}
-// -----------------------------------------------------------------------------
-ORptFilter& OXMLFormatCondition::GetOwnImport()
-{
-    return static_cast<ORptFilter&>(GetImport());
 }
 // -----------------------------------------------------------------------------
 void OXMLFormatCondition::EndElement()
