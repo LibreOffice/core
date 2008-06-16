fb503f7425697955d93c21f955545a8ad7c5bdfb
Rüdiger Timm <rt@openoffice.org>
2008-06-16 13:01:41 +0000

    INTEGRATION: CWS sb88 (1.9.10); FILE MERGED 2008/06/03 15:29:52 sb 1.9.10.1: #i89553 applied patch by cmc

--- configmgr/source/tree/subtree.hxx
+++ configmgr/source/tree/subtree.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: subtree.hxx,v $
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -103,9 +103,6 @@ namespace configmgr
     // to search in ChildListSet a value
     class SearchNode : public INode
     {
-        // why you want to search with no names.
-        SearchNode();
-
     public:
         SearchNode(OUString const& aName);
         virtual ~SearchNode();
