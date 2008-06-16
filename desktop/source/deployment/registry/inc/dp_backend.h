6923ed26cbeb157574a140c57302a5ab0feced37
Rüdiger Timm <rt@openoffice.org>
2008-06-16 11:56:15 +0000

    INTEGRATION: CWS extmgrui02 (1.16.2); FILE MERGED 2008/06/10 07:27:43 dv 1.16.2.3: RESYNC: (1.16-1.17); FILE MERGED 2008/06/05 07:53:55 dv 1.16.2.2: #i90386# Added support for custom icons 2008/05/27 11:31:50 dv 1.16.2.1: Enabled publisher link

--- desktop/source/deployment/registry/inc/dp_backend.h
+++ desktop/source/deployment/registry/inc/dp_backend.h
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dp_backend.h,v $
- * $Revision: 1.17 $
+ * $Revision: 1.18 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -219,6 +219,7 @@ public:
     getUpdateInformationURLs() throw (css::uno::RuntimeException);
 
     virtual css::beans::StringPair SAL_CALL getPublisherInfo() throw (css::uno::RuntimeException);
+    virtual css::uno::Reference< css::graphic::XGraphic > SAL_CALL getIcon( sal_Bool bHighContrast ) throw (css::uno::RuntimeException);
 
     virtual css::uno::Reference<css::deployment::XPackageTypeInfo> SAL_CALL
     getPackageType() throw (css::uno::RuntimeException);
