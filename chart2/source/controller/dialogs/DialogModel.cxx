2e0bb70c639a8360976e62dc41ef7f3efe2a705c
Rüdiger Timm <rt@openoffice.org>
2008-06-16 11:48:58 +0000

    INTEGRATION: CWS rptchart02 (1.5.4); FILE MERGED 2008/04/16 06:35:27 oj 1.5.4.2: RESYNC: (1.5-1.6); FILE MERGED 2008/04/15 12:50:17 oj 1.5.4.1: use comphelper copyProperties

--- chart2/source/controller/dialogs/DialogModel.cxx
+++ chart2/source/controller/dialogs/DialogModel.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: DialogModel.cxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -736,7 +736,7 @@ void DialogModel::restoreModel(
             xDestinationTitled->setTitleObject( xSourceTitled->getTitleObject());
 
             // page background
-            PropertyHelper::copyProperties(
+            comphelper::copyProperties(
                 xSource->getPageBackground(),
                 xDestination->getPageBackground() );
         }
