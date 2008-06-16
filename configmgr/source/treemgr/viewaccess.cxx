a8c2f4aa5c30c878c45e4cc5d4d846f91f42521d
Rüdiger Timm <rt@openoffice.org>
2008-06-16 13:08:49 +0000

    INTEGRATION: CWS sb88 (1.4.10); FILE MERGED 2008/06/03 15:29:54 sb 1.4.10.1: #i89553 applied patch by cmc

--- configmgr/source/treemgr/viewaccess.cxx
+++ configmgr/source/treemgr/viewaccess.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: viewaccess.cxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -41,12 +41,6 @@ namespace configmgr
 //-----------------------------------------------------------------------------
     namespace view
     {
-//-----------------------------------------------------------------------------
-bool ViewTreeAccess::isValid(configuration::NodeRef const& _aNodeArg) const
-{
-    return _aNodeArg.isValid();
-}
-
 //-----------------------------------------------------------------------------
 configuration::NodeData* ViewTreeAccess::nodeData(configuration::NodeRef const& _aNodeArg) const
 {
