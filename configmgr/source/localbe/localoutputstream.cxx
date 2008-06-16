6e7e23a6f0ce56452fa772547a1920a0ced44e7b
Rüdiger Timm <rt@openoffice.org>
2008-06-16 12:50:21 +0000

    INTEGRATION: CWS sb88 (1.7.10); FILE MERGED 2008/06/03 15:29:50 sb 1.7.10.1: #i89553 applied patch by cmc

--- configmgr/source/localbe/localoutputstream.cxx
+++ configmgr/source/localbe/localoutputstream.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: localoutputstream.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -119,7 +119,6 @@ LocalOutputStream::~LocalOutputStream()
     try
     {
         this->closeOutput();
-    //  if (mWriteFile)  FileHelper::removeFile(mTemporaryFileUrl) ;
     }
     catch (uno::Exception&)
     {
