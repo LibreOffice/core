752081f91e587763c3c00c636e8009035eacd279
Rüdiger Timm <rt@openoffice.org>
2008-06-20 05:56:02 +0000

    INTEGRATION: CWS ooxtablefilter (1.1.2); FILE ADDED 2008/06/20 06:29:21 rt 1.1.2.2: Change license header to LGPL v. 3 2008/03/28 20:07:55 sj 1.1.2.1: initial version

---
+++ oox/source/drawingml/stylematrixreferencecontext.cxx
@@ -0,0 +1,58 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: stylematrixreferencecontext.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "oox/drawingml/stylematrixreferencecontext.hxx"
+#include "oox/drawingml/colorchoicecontext.hxx"
+#include "oox/core/namespaces.hxx"
+#include "tokens.hxx"
+
+using rtl::OUString;
+using namespace ::oox::core;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+
+namespace oox { namespace drawingml {
+
+StyleMatrixReferenceContext::StyleMatrixReferenceContext( ContextHandler& rParent,
+    const ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastAttributeList >& rxAttribs,
+        rtl::OUString& rIdentifier, Color& rColor )
+: ContextHandler( rParent )
+, mrColor( rColor )
+{
+    rIdentifier = rxAttribs->getOptionalValue( XML_idx );
+}
+
+Reference< XFastContextHandler > StyleMatrixReferenceContext::createFastChildContext( sal_Int32 /* aElementToken */, const Reference< XFastAttributeList >& /* xAttribs */ ) throw (SAXException, RuntimeException)
+{
+    return new colorChoiceContext( *this, mrColor );
+}
+
+} }
