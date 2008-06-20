a84b2d0a97de472a5563516665af8292974f5c72
Rüdiger Timm <rt@openoffice.org>
2008-06-20 05:59:02 +0000

    INTEGRATION: CWS ooxtablefilter (1.1.2); FILE ADDED 2008/06/10 11:57:46 sj 1.1.2.3: updating license text 2008/04/02 17:43:49 sj 1.1.2.2: added table import 2008/03/28 20:02:21 sj 1.1.2.1: initial version

---
+++ oox/source/drawingml/table/tablebackgroundstylecontext.cxx
@@ -0,0 +1,90 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: tablebackgroundstylecontext.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include <osl/diagnose.h>
+
+#include "oox/drawingml/stylematrixreferencecontext.hxx"
+#include "oox/drawingml/table/tablebackgroundstylecontext.hxx"
+#include "oox/drawingml/fillpropertiesgroupcontext.hxx"
+#include "oox/core/namespaces.hxx"
+#include "tokens.hxx"
+using namespace ::oox::core;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+using ::rtl::OUString;
+
+namespace oox { namespace drawingml { namespace table {
+
+TableBackgroundStyleContext::TableBackgroundStyleContext( ContextHandler& rParent, TableStyle& rTableStyle )
+: ContextHandler( rParent )
+, mrTableStyle( rTableStyle )
+{
+}
+
+TableBackgroundStyleContext::~TableBackgroundStyleContext()
+{
+}
+
+uno::Reference< xml::sax::XFastContextHandler > SAL_CALL
+TableBackgroundStyleContext::createFastChildContext( ::sal_Int32 aElementToken, const uno::Reference< xml::sax::XFastAttributeList >& xAttribs )
+    throw ( xml::sax::SAXException, uno::RuntimeException)
+{
+    uno::Reference< xml::sax::XFastContextHandler > xRet;
+
+    switch( aElementToken )
+    {
+        // EG_ThemeableFillStyle (choice)
+        case NMSP_DRAWINGML|XML_fill:       // CT_FillProperties
+            {
+                boost::shared_ptr< oox::drawingml::FillProperties >& rFillProperties( mrTableStyle.getBackgroundFillProperties() );
+                rFillProperties = boost::shared_ptr< oox::drawingml::FillProperties > ( new oox::drawingml::FillProperties() );
+                xRet.set( new oox::drawingml::FillPropertiesContext( *this, *rFillProperties.get() ) );
+            }
+            break;
+        case NMSP_DRAWINGML|XML_fillRef:    // CT_StyleMatrixReference
+            xRet.set( new oox::drawingml::StyleMatrixReferenceContext( *this, xAttribs, mrTableStyle.getBackgroundFillStyleRef(), mrTableStyle.getBackgroundFillStyleColor() ) );
+            break;
+
+        // EG_ThemeableEffectStyle (choice)
+        case NMSP_DRAWINGML|XML_effect:     // CT_EffectProperties
+            break;
+        case NMSP_DRAWINGML|XML_effectRef:  // CT_StyleMatrixReference
+            break;
+    }
+    if( !xRet.is() )
+    {
+        uno::Reference<XFastContextHandler> xTmp(this);
+        xRet.set( xTmp );
+    }
+    return xRet;
+}
+
+} } }
