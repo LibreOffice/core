e11abbccb29de8903bdffe7e964a2eba7c01fbef
Rüdiger Timm <rt@openoffice.org>
2008-06-20 05:59:53 +0000

    INTEGRATION: CWS ooxtablefilter (1.1.2); FILE ADDED 2008/06/10 11:57:47 sj 1.1.2.2: updating license text 2008/03/28 20:02:41 sj 1.1.2.1: initial version

---
+++ oox/source/drawingml/table/tablepartstylecontext.cxx
@@ -0,0 +1,80 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: tablepartstylecontext.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include <osl/diagnose.h>
+
+#include "oox/drawingml/table/tablepartstylecontext.hxx"
+#include "oox/drawingml/table/tablestyletextstylecontext.hxx"
+#include "oox/drawingml/table/tablestylecellstylecontext.hxx"
+#include "oox/core/namespaces.hxx"
+#include "tokens.hxx"
+using namespace ::oox::core;
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+using ::rtl::OUString;
+
+namespace oox { namespace drawingml { namespace table {
+
+TablePartStyleContext::TablePartStyleContext( ContextHandler& rParent, TableStylePart& rTableStylePart )
+: ContextHandler( rParent )
+, mrTableStylePart( rTableStylePart )
+{
+}
+
+TablePartStyleContext::~TablePartStyleContext()
+{
+}
+
+// CT_TablePartStyle
+uno::Reference< xml::sax::XFastContextHandler > SAL_CALL
+TablePartStyleContext::createFastChildContext( ::sal_Int32 aElementToken, const uno::Reference< xml::sax::XFastAttributeList >& xAttribs )
+    throw ( xml::sax::SAXException, uno::RuntimeException)
+{
+    uno::Reference< xml::sax::XFastContextHandler > xRet;
+
+    switch( aElementToken )
+    {
+        case NMSP_DRAWINGML|XML_tcTxStyle:  // CT_TableStyleTextStyle
+            xRet.set( new TableStyleTextStyleContext( *this, xAttribs, mrTableStylePart ) );
+            break;
+        case NMSP_DRAWINGML|XML_tcStyle:    // CT_TableStyleCellStyle
+            xRet.set( new TableStyleCellStyleContext( *this,  mrTableStylePart ) );
+            break;
+    }
+    if( !xRet.is() )
+    {
+        uno::Reference<XFastContextHandler> xTmp(this);
+        xRet.set( xTmp );
+    }
+    return xRet;
+}
+
+} } }
