c8a5dd2976f373231fcb89fd5686aa5329dd27ce
Rüdiger Timm <rt@openoffice.org>
2008-06-20 05:46:56 +0000

    INTEGRATION: CWS ooxtablefilter (1.1.2); FILE ADDED 2008/06/20 06:27:18 rt 1.1.2.2: Change license header to LGPL v. 3 2008/03/20 15:32:16 sj 1.1.2.1: initial version

---
+++ oox/inc/oox/drawingml/table/tablerow.hxx
@@ -0,0 +1,65 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: tablerow.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef OOX_DRAWINGML_TABLEROW_HXX
+#define OOX_DRAWINGML_TABLEROW_HXX
+
+#include "oox/drawingml/table/tablecell.hxx"
+#include "oox/helper/propertymap.hxx"
+#include "oox/drawingml/color.hxx"
+
+#include <boost/shared_ptr.hpp>
+#include <boost/optional.hpp>
+#include <vector>
+#include <map>
+
+namespace oox { namespace drawingml { namespace table {
+
+class TableRow
+{
+public:
+
+    TableRow();
+    ~TableRow();
+
+    void setHeight( sal_Int32 nHeight ){ mnHeight = nHeight; };
+    sal_Int32 getHeight() const { return mnHeight; };
+    std::vector< TableCell >& getTableCells() { return mvTableCells; };
+
+private:
+
+    sal_Int32 mnHeight;
+    std::vector< TableCell > mvTableCells;
+};
+
+} } }
+
+#endif  //  OOX_DRAWINGML_TABLEROW_HXX
