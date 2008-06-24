dfec6fc2811f9ccea63c30229324fd5eaec64d3c
Kurt Zenker <kz@openoffice.org>
2008-06-24 10:03:51 +0000

    INTEGRATION: CWS canvas05 (1.2.112); FILE MERGED 2008/04/21 07:28:04 thb 1.2.112.2: RESYNC: (1.2-1.3); FILE MERGED 2007/12/20 22:19:00 thb 1.2.112.1: #i81092# #i78888# #i78925# #i79258# #i79437# #i84784# Large canvas rework, completing various areas such as color spaces, bitmap data access, true sprite and non-sprite implementations, and upstreaming the canvas parts of rodos emf+ rendering

--- canvas/source/vcl/spritecanvashelper.hxx
+++ canvas/source/vcl/spritecanvashelper.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: spritecanvashelper.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -53,7 +53,11 @@ namespace vclcanvas
     public:
         SpriteCanvasHelper();
 
-        void setRedrawManager( ::canvas::SpriteRedrawManager& rManager );
+        void init( const OutDevProviderSharedPtr& rOutDev,
+                   SpriteCanvas&                  rOwningSpriteCanvas,
+                   ::canvas::SpriteRedrawManager& rManager,
+                   bool                           bProtect,
+                   bool                           bHaveAlpha );
 
         /// Dispose all internal references
         void disposing();
@@ -149,6 +153,9 @@ namespace vclcanvas
 
         /// Set from the SpriteCanvas: instance coordinating sprite redraw
         ::canvas::SpriteRedrawManager*                  mpRedrawManager;
+
+        /// Set from the init method. used to generate sprites
+        SpriteCanvas*                                   mpOwningSpriteCanvas;
 
         /** Background compositing surface.
 
