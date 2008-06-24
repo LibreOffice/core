1ad617fd17be9b7ae736a205828d85a5b654d85b
Kurt Zenker <kz@openoffice.org>
2008-06-24 10:53:31 +0000

    INTEGRATION: CWS canvas05 (1.30.4); FILE MERGED 2008/04/21 07:51:13 thb 1.30.4.2: RESYNC: (1.30-1.31); FILE MERGED 2007/10/01 12:21:25 thb 1.30.4.1: #i80285# Merged nonzero winding rule feature from picom

--- basebmp/source/bitmapdevice.cxx
+++ basebmp/source/bitmapdevice.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: bitmapdevice.cxx,v $
- * $Revision: 1.32 $
+ * $Revision: 1.33 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -593,7 +593,8 @@ namespace
                                       maColorLookup( maAccessor,
                                                      col),
                                       rBounds,
-                                      aPoly );
+                                      aPoly,
+                                      basegfx::FillRule_EVEN_ODD );
         }
 
         virtual void fillPolyPolygon_i(const basegfx::B2DPolyPolygon& rPoly,
