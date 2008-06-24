a1c4058d5e1ce91d2cb588e21e9d4b82cc06e52a
Kurt Zenker <kz@openoffice.org>
2008-06-24 15:40:24 +0000

    INTEGRATION: CWS obo30 (1.13.90); FILE MERGED 2008/06/02 12:27:50 obo 1.13.90.1: #i90100# ambigous Reference during ENABLE_PCH build

--- svx/source/dialog/hyphen.cxx
+++ svx/source/dialog/hyphen.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: hyphen.cxx,v $
- * $Revision: 1.13 $
+ * $Revision: 1.14 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -53,7 +53,6 @@
 //#include <usr/ustring.hxx>
 #endif
 
-using namespace ::rtl;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
