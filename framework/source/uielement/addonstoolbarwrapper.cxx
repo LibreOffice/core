b6f7853c98381ac429f952a0265e018e86fcf99e
Kurt Zenker <kz@openoffice.org>
2008-06-24 15:57:22 +0000

    INTEGRATION: CWS obo30 (1.7.40); FILE MERGED 2008/06/05 14:55:31 obo 1.7.40.2: #i90100# missing EOL 2008/05/29 14:43:45 obo 1.7.40.1: #i90100# ambigous Reference during ENABLE_PCH build

--- framework/source/uielement/addonstoolbarwrapper.cxx
+++ framework/source/uielement/addonstoolbarwrapper.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: addonstoolbarwrapper.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -73,7 +73,6 @@
 #include <vcl/toolbox.hxx>
 #include <rtl/logfile.hxx>
 
-using namespace rtl;
 using namespace com::sun::star::uno;
 using namespace com::sun::star::beans;
 using namespace com::sun::star::frame;
@@ -196,3 +195,4 @@ Reference< XInterface > SAL_CALL AddonsToolBarWrapper::getRealInterface() throw 
 }
 
 } // namespace framework
+
