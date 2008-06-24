f9a889105f9d994ebd067f20b1120168720b06ab
Kurt Zenker <kz@openoffice.org>
2008-06-24 09:25:57 +0000

    INTEGRATION: CWS canvas05 (1.5.24); FILE MERGED 2008/06/03 15:39:06 thb 1.5.24.10: #i85116# Revived lost changes in cairocanvas 2008/04/21 07:31:43 thb 1.5.24.9: RESYNC: (1.7-1.8); FILE MERGED 2008/04/07 14:33:55 thb 1.5.24.8: RESYNC: (1.6-1.7); FILE MERGED 2008/04/03 16:02:26 thb 1.5.24.7: Merged in cairoquartz01 changes, reworked into the polymorphic Surface concept now 2008/04/02 22:56:28 thb 1.5.24.6: Reworked Surface class to abstract interface; changed all manual refcount handling to RAII 2008/03/28 23:38:46 thb 1.5.24.5: Backbuffer-less canvas implementation on top of cairo 2008/03/18 22:00:57 thb 1.5.24.4: Implementing non-backbuffered canvas for cairocanvas as well - reworked to share most of the code 2008/01/22 00:50:55 thb 1.5.24.3: RESYNC: (1.5-1.6); FILE MERGED 2007/12/20 22:18:56 thb 1.5.24.2: #i81092# #i78888# #i78925# #i79258# #i79437# #i84784# Large canvas rework, completing various areas such as color spaces, bitmap data access, true sprite and non-sprite implementations, and upstreaming the canvas parts of rodos emf+ rendering 2007/10/01 13:02:01 thb 1.5.24.1: #i78888# #i78925# #i79258# #i79437# Merge from CWS picom

--- canvas/source/cairo/cairo_spritehelper.cxx
+++ canvas/source/cairo/cairo_spritehelper.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: cairo_spritehelper.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,6 +33,7 @@
 
 #include <canvas/debug.hxx>
 #include <canvas/verbosetrace.hxx>
+#include <tools/diagnose_ex.h>
 
 #include <rtl/logfile.hxx>
 #include <rtl/math.hxx>
@@ -62,15 +63,14 @@ namespace cairocanvas
 {
     SpriteHelper::SpriteHelper() :
         mpSpriteCanvas(),
-        mbTextureDirty( true ),
-    mpBufferSurface( NULL )
-    {
-    }
+        mpBufferSurface(),
+        mbTextureDirty(true)
+    {}
 
-    void SpriteHelper::init( const geometry::RealSize2D&    rSpriteSize,
-                             const SpriteCanvasRef&         rSpriteCanvas)
+    void SpriteHelper::init( const geometry::RealSize2D& rSpriteSize,
+                             const SpriteCanvasRef&      rSpriteCanvas )
     {
-        ENSURE_AND_THROW( rSpriteCanvas.get(),
+        ENSURE_OR_THROW( rSpriteCanvas.get(),
                           "SpriteHelper::init(): Invalid device, sprite canvas or surface" );
 
         mpSpriteCanvas     = rSpriteCanvas;
@@ -81,109 +81,106 @@ namespace cairocanvas
                                         rSpriteCanvas.get() );
     }
 
-    void SpriteHelper::setSurface( Surface* pBufferSurface )
+    void SpriteHelper::setSurface( const SurfaceSharedPtr& pBufferSurface )
     {
-        if( mpBufferSurface )
-            mpBufferSurface->Unref();
-
         mpBufferSurface = pBufferSurface;
-        mpBufferSurface->Ref();
     }
 
     void SpriteHelper::disposing()
     {
+        mpBufferSurface.reset();
         mpSpriteCanvas.clear();
 
         // forward to parent
         CanvasCustomSpriteHelper::disposing();
-
-        if( mpBufferSurface ) {
-            mpBufferSurface->Unref();
-            mpBufferSurface = NULL;
-        }
     }
 
-    void SpriteHelper::redraw( Cairo*                     pCairo,
+    void SpriteHelper::redraw( const CairoSharedPtr&    pCairo,
                                const ::basegfx::B2DPoint& rPos,
                                bool&                      /*io_bSurfacesDirty*/,
                                bool                       /*bBufferedUpdate*/ ) const
     {
-    #ifdef CAIRO_CANVAS_PERF_TRACE
-    struct timespec aTimer;
-    mxDevice->startPerfTrace( &aTimer );
-        #endif
+#ifdef CAIRO_CANVAS_PERF_TRACE
+        struct timespec aTimer;
+        mxDevice->startPerfTrace( &aTimer );
+#endif
 
         const double fAlpha( getAlpha() );
-    const ::basegfx::B2DHomMatrix aTransform( getTransformation() );
+        const ::basegfx::B2DHomMatrix aTransform( getTransformation() );
 
-    if( isActive() && !::basegfx::fTools::equalZero( fAlpha ) ) {
-        OSL_TRACE ("CanvasCustomSprite::redraw called");
-        if( pCairo ) {
-        basegfx::B2DVector aSize = getSizePixel();
-        cairo_save( pCairo );
+        if( isActive() && !::basegfx::fTools::equalZero( fAlpha ) ) {
+            OSL_TRACE ("CanvasCustomSprite::redraw called");
+            if( pCairo ) {
+                basegfx::B2DVector aSize = getSizePixel();
+                cairo_save( pCairo.get() );
 
-        double fX, fY;
+                double fX, fY;
 
-        fX = rPos.getX();
-        fY = rPos.getY();
+                fX = rPos.getX();
+                fY = rPos.getY();
 
-         if( !aTransform.isIdentity() ) {
-             cairo_matrix_t aMatrix, aInverseMatrix;
-             cairo_matrix_init( &aMatrix,
-                        aTransform.get( 0, 0 ), aTransform.get( 1, 0 ), aTransform.get( 0, 1 ),
-                        aTransform.get( 1, 1 ), aTransform.get( 0, 2 ), aTransform.get( 1, 2 ) );
+                if( !aTransform.isIdentity() ) {
+                    cairo_matrix_t aMatrix, aInverseMatrix;
+                    cairo_matrix_init( &aMatrix,
+                                       aTransform.get( 0, 0 ), aTransform.get( 1, 0 ), aTransform.get( 0, 1 ),
+                                       aTransform.get( 1, 1 ), aTransform.get( 0, 2 ), aTransform.get( 1, 2 ) );
 
-             aMatrix.x0 = basegfx::fround( aMatrix.x0 );
-             aMatrix.y0 = basegfx::fround( aMatrix.y0 );
+                    aMatrix.x0 = basegfx::fround( aMatrix.x0 );
+                    aMatrix.y0 = basegfx::fround( aMatrix.y0 );
 
-            cairo_matrix_init( &aInverseMatrix, aMatrix.xx, aMatrix.yx, aMatrix.xy, aMatrix.yy, aMatrix.x0, aMatrix.y0 );
-            cairo_matrix_invert( &aInverseMatrix );
-            cairo_matrix_transform_distance( &aInverseMatrix, &fX, &fY );
+                    cairo_matrix_init( &aInverseMatrix, aMatrix.xx, aMatrix.yx, aMatrix.xy, aMatrix.yy, aMatrix.x0, aMatrix.y0 );
+                    cairo_matrix_invert( &aInverseMatrix );
+                    cairo_matrix_transform_distance( &aInverseMatrix, &fX, &fY );
 
-             cairo_set_matrix( pCairo, &aMatrix );
-         }
+                    cairo_set_matrix( pCairo.get(), &aMatrix );
+                }
 
-         fX = basegfx::fround( fX );
-         fY = basegfx::fround( fY );
+                fX = basegfx::fround( fX );
+                fY = basegfx::fround( fY );
 
-        cairo_matrix_t aOrigMatrix;
-        cairo_get_matrix( pCairo, &aOrigMatrix );
-        cairo_translate( pCairo, fX, fY );
+                cairo_matrix_t aOrigMatrix;
+                cairo_get_matrix( pCairo.get(), &aOrigMatrix );
+                cairo_translate( pCairo.get(), fX, fY );
 
                 if( getClip().is() )
                 {
+                    const uno::Reference<rendering::XPolyPolygon2D>& rClip( getClip() );
+
                     ::basegfx::B2DPolyPolygon aClipPoly(
-                        ::canvas::tools::polyPolygonFromXPolyPolygon2D(
-                            getClip() ));
+                        ::basegfx::unotools::b2DPolyPolygonFromXPolyPolygon2D(
+                            rClip ));
 
-            //cairo_reset_clip( pCairo );
-            doPolyPolygonImplementation( aClipPoly, Clip, pCairo );
-        }
+                    doPolyPolygonImplementation( aClipPoly, Clip, pCairo.get(),
+                                                 NULL, SurfaceProviderRef(mpSpriteCanvas.get()),
+                                                 rClip->getFillRule() );
+                }
 
-        OSL_TRACE ("aSize %f x %f position: %f,%f", aSize.getX(), aSize.getY(), fX, fY );
-        cairo_rectangle( pCairo, 0, 0, floor( aSize.getX() ), floor( aSize.getY() ) );
-        cairo_clip( pCairo );
-        cairo_set_matrix( pCairo, &aOrigMatrix );
+                OSL_TRACE ("aSize %f x %f position: %f,%f", aSize.getX(), aSize.getY(), fX, fY );
+                cairo_rectangle( pCairo.get(), 0, 0, floor( aSize.getX() ), floor( aSize.getY() ) );
+                cairo_clip( pCairo.get() );
+                cairo_set_matrix( pCairo.get(), &aOrigMatrix );
 
-        if( isContentFullyOpaque() )
-             cairo_set_operator( pCairo, CAIRO_OPERATOR_SOURCE );
-        cairo_set_source_surface( pCairo, mpBufferSurface->mpSurface, fX, fY );
+                if( isContentFullyOpaque() )
+                    cairo_set_operator( pCairo.get(), CAIRO_OPERATOR_SOURCE );
+                cairo_set_source_surface( pCairo.get(),
+                                          mpBufferSurface->getCairoSurface().get(),
+                                          fX, fY );
                 if( ::rtl::math::approxEqual( fAlpha, 1.0 ) )
-            cairo_paint( pCairo );
-        else
-            cairo_paint_with_alpha( pCairo, fAlpha );
+                    cairo_paint( pCairo.get() );
+                else
+                    cairo_paint_with_alpha( pCairo.get(), fAlpha );
 
-        cairo_restore( pCairo );
+                cairo_restore( pCairo.get() );
+            }
         }
-    }
 
-    #ifdef CAIRO_CANVAS_PERF_TRACE
-    mxDevice->stopPerfTrace( &aTimer, "sprite redraw" );
-        #endif
+#ifdef CAIRO_CANVAS_PERF_TRACE
+        mxDevice->stopPerfTrace( &aTimer, "sprite redraw" );
+#endif
     }
 
     ::basegfx::B2DPolyPolygon SpriteHelper::polyPolygonFromXPolyPolygon2D( uno::Reference< rendering::XPolyPolygon2D >& xPoly ) const
     {
-        return ::canvas::tools::polyPolygonFromXPolyPolygon2D( xPoly );
+        return ::basegfx::unotools::b2DPolyPolygonFromXPolyPolygon2D(xPoly);
     }
 }
