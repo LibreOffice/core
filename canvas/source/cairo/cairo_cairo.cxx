a5e6a7a14d7e12976277af68a162dbd63ec3b098
Kurt Zenker <kz@openoffice.org>
2008-06-24 09:18:21 +0000

    INTEGRATION: CWS canvas05 (1.5.24); FILE MERGED 2008/04/21 07:32:13 thb 1.5.24.8: RESYNC: (1.7-1.8); FILE MERGED 2008/04/16 13:47:38 thb 1.5.24.7: Made cairocanvas build on Win32 again 2008/04/07 14:34:03 thb 1.5.24.6: RESYNC: (1.5-1.7); FILE MERGED 2008/04/03 20:12:35 thb 1.5.24.5: Improved the system-dependent passing-around of the OS handles - now much more opaque 2008/04/03 16:02:25 thb 1.5.24.4: Merged in cairoquartz01 changes, reworked into the polymorphic Surface concept now 2008/04/02 22:56:26 thb 1.5.24.3: Reworked Surface class to abstract interface; changed all manual refcount handling to RAII 2008/03/18 22:00:55 thb 1.5.24.2: Implementing non-backbuffered canvas for cairocanvas as well - reworked to share most of the code 2007/12/18 10:05:52 thb 1.5.24.1: #i84049# Applying patch from jnavrati to fix a crash when xrender is not available

--- canvas/source/cairo/cairo_cairo.cxx
+++ canvas/source/cairo/cairo_cairo.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: cairo_cairo.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -32,100 +32,36 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_canvas.hxx"
 
-/****************************************************************************************
- * Platform independent part of surface backends for OpenOffice.org Cairo Canvas        *
- * For rest of the functions, see platform specific cairo_<platform>_cairo.cxx          *
- ****************************************************************************************/
-
 #include "cairo_cairo.hxx"
+
+#ifdef WNT
+# include <tools/prewin.h>
+# include <windows.h>
+# include <tools/postwin.h>
+#endif
+
+#include <vcl/sysdata.hxx>
+#include <vcl/syschild.hxx>
 
 namespace cairo
 {
+/****************************************************************************************
+ * Platform independent part of surface backends for OpenOffice.org Cairo Canvas        *
+ * For the rest of the functions (and the platform-specific derived                     *
+ *  Surface classes), see platform specific cairo_<platform>_cairo.cxx                  *
+ ****************************************************************************************/
 
-/**
-   * Surface::getCairo:  Create Cairo for the Canvas surface
-   *
-   * @return new Cairo or NULL
-   **/
-    Cairo* Surface::getCairo()
+    const SystemEnvData* GetSysData(const Window *pOutputWindow)
     {
-        Cairo *cr = NULL;
-        if (mpSurface) {
-            cr = cairo_create( mpSurface );
-        }
-        return cr;
+        const SystemEnvData* pSysData = NULL;
+        // check whether we're a SysChild: have to fetch system data
+        // directly from SystemChildWindow, because the GetSystemData
+        // method is unfortunately not virtual
+        const SystemChildWindow* pSysChild = dynamic_cast< const SystemChildWindow* >( pOutputWindow );
+        if( pSysChild )
+            pSysData = pSysChild->GetSystemData();
+        else
+            pSysData = pOutputWindow->GetSystemData();
+        return pSysData;
     }
-
-
-// This is needed to distinguish support for Cairo versions < 1.2.
-#if !defined (USE_CAIRO10_APIS)
-
-  /**
-   * Surface::~Surface:  Destroy the Canvas surface
-   *
-   * Cairo itself takes care of freeing any resources, such as
-   * image data and other references related to the surface.
-   *
-   **/
-    Surface::~Surface()
-    {
-        if( mpSurface )
-        {
-            cairo_surface_destroy( mpSurface );
-            mpSurface = NULL;
-        }
-    }
-
-
-  /**
-   * Surface::getSimilar:  Create new similar Canvas surface
-   * @param aContent format of the new surface (cairo_content_t from cairo/src/cairo.h)
-   * @param width width of the new surface
-   * @param height height of the new surface
-   *
-   * Creates a new Canvas surface. This should create platform native surface, even though
-   * generic function is used.
-   *
-   * Cairo surface from aContent (cairo_content_t)
-   *
-   * @return new surface or NULL
-   **/
-    Surface* Surface::getSimilar( Content aContent, int width, int height )
-    {
-        // This should create platform native Cairo surface on ALL platforms.
-        return new Surface( cairo_surface_create_similar( mpSurface, aContent, width, height ) );
-
-        // For example on Mac OS X, cairo_surface_create_similar() actually results in native surface,
-        // equivalent to cairo_quartz_surface_create()
-    }
-#endif   // !defined (USE_CAIRO10_APIS)
-
-
-  /**
-   * Surface::createVirtualDevice:  Create new VCL virtual device
-   *
-   * Creates a new virtual device in VCL, with the current mpSurface contents as data.
-   * This is used by e.g. cairo_canvashelper_text.cxx to make VCL draw text on the cairo surface.
-   *
-   * @return new virtual device
-   **/
-    VirtualDevice* Surface::createVirtualDevice()
-    {
-        // struct SystemGraphicsData in vcl/inc/sysdata.hxx
-        SystemGraphicsData aSystemGraphicsData;
-
-        aSystemGraphicsData.nSize = sizeof(SystemGraphicsData);
-        fillSystemGraphicsData( aSystemGraphicsData );
-
-        /**
-         *    Contrary to the description of
-         *    VirtualDevice(SystemGraphicsData*, nBitCount);  (in vcl/inc/virdev.hxx)
-         *    at least X11 behaves differently, i.e. nBitCount is color depth, not just 1 or 0.
-        **/
-        return new VirtualDevice( &aSystemGraphicsData,
-                                  sal::static_int_cast<USHORT>(getDepth()) );
-    }
-
-}  // namespace cairo
-
-
+}
