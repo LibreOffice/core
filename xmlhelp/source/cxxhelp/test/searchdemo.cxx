ba0f271e4ff0006e697c573974f80c9dfc99f7a2
Kurt Zenker <kz@openoffice.org>
2008-06-24 15:23:46 +0000

    INTEGRATION: CWS ab52 (1.7.6); FILE MERGED 2008/06/18 09:13:25 ab 1.7.6.1: #i90029# Removed unused code

--- xmlhelp/source/cxxhelp/test/searchdemo.cxx
+++ xmlhelp/source/cxxhelp/test/searchdemo.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: searchdemo.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,7 +33,6 @@
 #include <rtl/string.hxx>
 #include <osl/file.hxx>
 #include <db/Block.hxx>
-#include <db/Schema.hxx>
 #include <db/BtreeDictParameters.hxx>
 #include <db/BtreeDict.hxx>
 #include <util/RandomAccessStream.hxx>
@@ -119,42 +118,3 @@ int main( int argc,char* argv[] )
     }
     return 0;
 }
-
-
-// This is the main line of programming
-
-
-//    Indexccessor indexAcc( rtl::OUString::createFromAscii( "/home/ab106281/work/index" ) );
-//    Schema schema( indexAcc,false );
-//    BtreeDictParameters params( schema,
-//                    rtl::OUString::createFromAscii( "DICTIONARY" ) );
-
-//    blu();
-
-//    std::vector< rtl::OUString > terms( 1 );
-//    terms[0] = rtl::OUString::createFromAscii( "text" );
-//    QueryStatement statement( 100,terms,rtl::OUString::createFromAscii( "" ) );
-//    QueryProcessor queryProcessor( 0 );
-//    QueryResults* results = queryProcessor.processQuery( statement );
-
-
-
-  // rtl::OString bluber = rtl::OString( bla.getStr(),bla.getLength(),RTL_TEXTENCODING_UTF8 );
-  // cout << bluber.getStr() << endl;
-
-
-
-/*
-  public static void main(String[] args) {
-  try {
-  Schema schema = new Schema( new IndexAccessor( args[0] ), false);
-  BtreeDictParameters params = new BtreeDictParameters(schema, "DICTIONARY");
-  BtreeDict source = new BtreeDict(params);
-  source.test();
-  }
-  catch (Exception e) {
-  System.err.println(e);
-  e.printStackTrace();
-  }
-  }
-*/
