211e553b691b766cf5cddf5f825d1adcccb1276f
Kurt Zenker <kz@openoffice.org>
2008-06-24 09:30:37 +0000

    INTEGRATION: CWS canvas05 (1.2.2); FILE MERGED 2008/06/03 23:52:23 thb 1.2.2.4: Removed extra level of indirection for getting a graphics for a surface; removed some unused code 2008/04/21 07:30:26 thb 1.2.2.3: RESYNC: (1.2-1.3); FILE MERGED 2008/02/08 00:26:38 thb 1.2.2.2: #81092# Finishing cooperative canvas output stuff 2008/01/22 00:25:23 thb 1.2.2.1: #i81092# Making gdiplus and dx canvas more independent

--- canvas/source/directx/dx_bitmap.hxx
+++ canvas/source/directx/dx_bitmap.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dx_bitmap.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,52 +33,30 @@
 
 #include <com/sun/star/rendering/XCanvas.hpp>
 #include <com/sun/star/rendering/XIntegerBitmap.hpp>
-#include <canvas/rendering/isurfaceproxy.hxx>
-#include <canvas/rendering/isurfaceproxymanager.hxx>
 #include <boost/shared_ptr.hpp>
 #include <basegfx/vector/b2ivector.hxx>
 #include <basegfx/point/b2dpoint.hxx>
 #include <basegfx/range/b2drange.hxx>
-#include "dx_canvasfont.hxx" //winstuff
+#include "dx_winstuff.hxx"
+#include "dx_ibitmap.hxx"
+#include "dx_graphicsprovider.hxx"
 #include "dx_gdiplususer.hxx"
-#include "dx_rendermodule.hxx"
 
 namespace dxcanvas
 {
-    typedef ::boost::shared_ptr< class SurfaceGraphics > SurfaceGraphicsSharedPtr;
-
-    class DXBitmap
+    class DXBitmap : public IBitmap
     {
     public:
-        DXBitmap( const ::basegfx::B2IVector&                   rSize,
-                  const canvas::ISurfaceProxyManagerSharedPtr&  rMgr,
-                  const IDXRenderModuleSharedPtr&               rRenderModule,
-                  bool                                          bWithAlpha );
+        DXBitmap( const BitmapSharedPtr& rBitmap,
+                  bool                   bWithAlpha );
+        DXBitmap( const ::basegfx::B2IVector& rSize,
+                  bool                        bWithAlpha );
 
-        bool resize( const ::basegfx::B2IVector& rSize );
-        void clear();
-        bool hasAlpha() const;
+        virtual GraphicsSharedPtr         getGraphics();
 
-        BitmapSharedPtr                         getBitmap();
-        SurfaceGraphicsSharedPtr                getGraphics() const;
-        COMReference<surface_type>              getSurface() const { return mpSurface; }
-        ::basegfx::B2IVector                    getSize() { return maSize; }
-
-        bool draw( double                           fAlpha,
-                   const ::basegfx::B2DPoint&       rPos,
-                   const ::basegfx::B2DHomMatrix&   rTransform );
-
-        bool draw( const ::basegfx::B2IRange&       rArea );
-
-        bool draw( double                           fAlpha,
-                   const ::basegfx::B2DPoint&       rPos,
-                   const ::basegfx::B2DRange&       rArea,
-                   const ::basegfx::B2DHomMatrix&   rTransform );
-
-        bool draw( double                           fAlpha,
-                   const ::basegfx::B2DPoint&       rPos,
-                   const ::basegfx::B2DPolyPolygon& rClipPoly,
-                   const ::basegfx::B2DHomMatrix&   rTransform );
+        virtual BitmapSharedPtr           getBitmap() const;
+        virtual ::basegfx::B2IVector      getSize() const;
+        virtual bool                      hasAlpha() const;
 
         ::com::sun::star::uno::Sequence< sal_Int8 > getData(
             ::com::sun::star::rendering::IntegerBitmapLayout&       bitmapLayout,
@@ -99,52 +77,17 @@ namespace dxcanvas
             const ::com::sun::star::geometry::IntegerPoint2D&       pos );
 
     private:
-#ifdef DX_DEBUG_IMAGES
-        void imageDebugger();
-#endif
-        void init();
-
         // Refcounted global GDI+ state container
         GDIPlusUserSharedPtr mpGdiPlusUser;
 
         // size of this image in pixels [integral unit]
         ::basegfx::B2IVector maSize;
 
-        // pointer to the rendermodule, needed to create surfaces
-        // which are used as container for the actual pixel data.
-        // generally we could use any kind of storage, but GDI+
-        // is not willing to render antialiased fonts unless we
-        // use this special kind of container, don't ask me why...
-        IDXRenderModuleSharedPtr mpRenderModule;
-
-        // pointer to the surface manager, needed in case clients
-        // want to resize the bitmap.
-        canvas::ISurfaceProxyManagerSharedPtr mpSurfaceManager;
-
-        // access point to the surface proxy which handles
-        // the hardware-dependent rendering stuff.
-        canvas::ISurfaceProxySharedPtr mpSurfaceProxy;
-
-        // container for pixel data, we need to use a directx
-        // surface since GDI+ sucks...
-        COMReference<surface_type> mpSurface;
-
-        // since GDI+ does not work correctly in case we
-        // run on a 16bit display [don't ask me why] we need
-        // to occasionally render to a native GDI+ bitmap.
-        BitmapSharedPtr mpGDIPlusBitmap;
-
-        // internal implementation of the iColorBuffer interface
-        canvas::IColorBufferSharedPtr mpColorBuffer;
-
-        // indicates wether the associated surface needs
-        // to refresh its contents or not. in other words,
-        // this flag is set iff both representations are
-        // out of sync.
-        mutable bool mbIsSurfaceDirty;
+        BitmapSharedPtr      mpBitmap;
+        GraphicsSharedPtr    mpGraphics;
 
         // true if the bitmap contains an alpha channel
-        bool mbAlpha;
+        bool                 mbAlpha;
     };
 
     typedef ::boost::shared_ptr< DXBitmap > DXBitmapSharedPtr;
