87523929cf9199d2175e31cc37a80122ee78331f
Armin Weiss <aw@openoffice.org>
2008-06-24 14:44:27 +0000

    #i39532# corrections

---
+++ drawinglayer/inc/drawinglayer/processor2d/linegeometryextractor2d.hxx
@@ -0,0 +1,74 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: linegeometryextractor2d.hxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: aw $ $Date: 2008-06-24 15:44:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_DRAWINGLAYER_PROCESSOR2D_LINEGEOMETRYEXTRACTOR2D_HXX
+#define INCLUDED_DRAWINGLAYER_PROCESSOR2D_LINEGEOMETRYEXTRACTOR2D_HXX
+
+#include <drawinglayer/processor2d/baseprocessor2d.hxx>
+#include <basegfx/polygon/b2dpolypolygon.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace drawinglayer
+{
+    namespace processor2d
+    {
+        class LineGeometryExtractor2D : public BaseProcessor2D
+        {
+        private:
+            basegfx::B2DPolyPolygon                 maExtractedHairlines;
+            basegfx::B2DPolyPolygon                 maExtractedLineFills;
+
+            // bitfield
+            unsigned                                mbInLineGeometry : 1;
+
+            // tooling methods
+            void processBasePrimitive2D(const primitive2d::BasePrimitive2D& rCandidate);
+
+        public:
+            LineGeometryExtractor2D(const geometry::ViewInformation2D& rViewInformation);
+            virtual ~LineGeometryExtractor2D();
+
+            const basegfx::B2DPolyPolygon& getExtractedHairlines() const { return maExtractedHairlines; }
+            const basegfx::B2DPolyPolygon& getExtractedLineFills() const { return maExtractedLineFills; }
+        };
+    } // end of namespace processor2d
+} // end of namespace drawinglayer
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // INCLUDED_DRAWINGLAYER_PROCESSOR2D_LINEGEOMETRYEXTRACTOR2D_HXX
+
+// eof
---
+++ drawinglayer/source/processor2d/linegeometryextractor2d.cxx
@@ -0,0 +1,152 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: linegeometryextractor2d.cxx,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *  last change: $Author: aw $ $Date: 2008-06-24 15:44:27 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_drawinglayer.hxx"
+
+#include <drawinglayer/processor2d/linegeometryextractor2d.hxx>
+#include <drawinglayer/primitive2d/drawinglayer_primitivetypes2d.hxx>
+#include <drawinglayer/primitive2d/polygonprimitive2d.hxx>
+#include <drawinglayer/primitive2d/polypolygonprimitive2d.hxx>
+#include <drawinglayer/primitive2d/transformprimitive2d.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace com::sun::star;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace drawinglayer
+{
+    namespace processor2d
+    {
+        LineGeometryExtractor2D::LineGeometryExtractor2D(const geometry::ViewInformation2D& rViewInformation)
+        :   BaseProcessor2D(rViewInformation),
+            maExtractedHairlines(),
+            maExtractedLineFills(),
+            mbInLineGeometry(false)
+        {
+        }
+
+        LineGeometryExtractor2D::~LineGeometryExtractor2D()
+        {
+        }
+
+        void LineGeometryExtractor2D::processBasePrimitive2D(const primitive2d::BasePrimitive2D& rCandidate)
+        {
+            switch(rCandidate.getPrimitiveID())
+            {
+                case PRIMITIVE2D_ID_POLYGONSTROKEPRIMITIVE2D :
+                case PRIMITIVE2D_ID_POLYGONSTROKEARROWPRIMITIVE2D :
+                {
+                    // enter a line geometry group (with or without LineEnds)
+                    bool bOldState(mbInLineGeometry);
+                    mbInLineGeometry = true;
+                    process(rCandidate.get2DDecomposition(getViewInformation2D()));
+                    mbInLineGeometry = bOldState;
+                    break;
+                }
+                case PRIMITIVE2D_ID_POLYGONHAIRLINEPRIMITIVE2D :
+                {
+                    if(mbInLineGeometry)
+                    {
+                        // extract hairline line geometry in world coordinates
+                        const primitive2d::PolygonHairlinePrimitive2D& rPolygonCandidate(static_cast< const primitive2d::PolygonHairlinePrimitive2D& >(rCandidate));
+                        basegfx::B2DPolygon aLocalPolygon(rPolygonCandidate.getB2DPolygon());
+                        aLocalPolygon.transform(getViewInformation2D().getObjectTransformation());
+                        maExtractedHairlines.append(aLocalPolygon);
+                    }
+                    break;
+                }
+                case PRIMITIVE2D_ID_POLYPOLYGONCOLORPRIMITIVE2D :
+                {
+                    if(mbInLineGeometry)
+                    {
+                        // extract filled line geometry (line with width)
+                        const primitive2d::PolyPolygonColorPrimitive2D& rPolygonCandidate(static_cast< const primitive2d::PolyPolygonColorPrimitive2D& >(rCandidate));
+                        basegfx::B2DPolyPolygon aLocalPolyPolygon(rPolygonCandidate.getB2DPolyPolygon());
+                        aLocalPolyPolygon.transform(getViewInformation2D().getObjectTransformation());
+                        maExtractedLineFills.append(aLocalPolyPolygon);
+                    }
+                    break;
+                }
+                case PRIMITIVE2D_ID_TRANSFORMPRIMITIVE2D :
+                {
+                    // remember current transformation and ViewInformation
+                    const primitive2d::TransformPrimitive2D& rTransformCandidate(static_cast< const primitive2d::TransformPrimitive2D& >(rCandidate));
+                    const geometry::ViewInformation2D aLastViewInformation2D(getViewInformation2D());
+
+                    // create new transformations for CurrentTransformation and for local ViewInformation2D
+                    const geometry::ViewInformation2D aViewInformation2D(
+                        getViewInformation2D().getObjectTransformation() * rTransformCandidate.getTransformation(),
+                        getViewInformation2D().getViewTransformation(),
+                        getViewInformation2D().getViewport(),
+                        getViewInformation2D().getVisualizedPage(),
+                        getViewInformation2D().getViewTime(),
+                        getViewInformation2D().getExtendedInformationSequence());
+                    updateViewInformation(aViewInformation2D);
+
+                    // proccess content
+                    process(rTransformCandidate.getChildren());
+
+                    // restore transformations
+                    updateViewInformation(aLastViewInformation2D);
+
+                    break;
+                }
+                case PRIMITIVE2D_ID_SCENEPRIMITIVE2D :
+                case PRIMITIVE2D_ID_WRONGSPELLPRIMITIVE2D :
+                case PRIMITIVE2D_ID_MARKERARRAYPRIMITIVE2D :
+                case PRIMITIVE2D_ID_POINTARRAYPRIMITIVE2D :
+                case PRIMITIVE2D_ID_BITMAPPRIMITIVE2D :
+                case PRIMITIVE2D_ID_METAFILEPRIMITIVE2D :
+                case PRIMITIVE2D_ID_MASKPRIMITIVE2D :
+                {
+                    // ignorable primitives
+                    break;
+                }
+                default :
+                {
+                    // process recursively
+                    process(rCandidate.get2DDecomposition(getViewInformation2D()));
+                    break;
+                }
+            }
+        }
+    } // end of namespace processor2d
+} // end of namespace drawinglayer
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
