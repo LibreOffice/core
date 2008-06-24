5091e51080a7e777f86ee151df122b45be977740
Kurt Zenker <kz@openoffice.org>
2008-06-24 09:41:22 +0000

    INTEGRATION: CWS canvas05 (1.2.2); FILE MERGED 2008/06/03 13:05:56 thb 1.2.2.8: Made DX5 work again as well 2008/05/23 23:02:34 thb 1.2.2.7: RESYNC: (1.6-1.7); FILE MERGED 2008/05/02 17:11:34 thb 1.2.2.6: Missing bits to make dxcanvas actually work again 2008/04/21 07:29:08 thb 1.2.2.5: RESYNC: (1.5-1.6); FILE MERGED 2008/02/08 00:26:39 thb 1.2.2.4: #81092# Finishing cooperative canvas output stuff 2008/01/22 00:51:11 thb 1.2.2.3: RESYNC: (1.2-1.5); FILE MERGED 2008/01/22 00:25:24 thb 1.2.2.2: #i81092# Making gdiplus and dx canvas more independent 2007/12/20 22:18:58 thb 1.2.2.1: #i81092# #i78888# #i78925# #i79258# #i79437# #i84784# Large canvas rework, completing various areas such as color spaces, bitmap data access, true sprite and non-sprite implementations, and upstreaming the canvas parts of rodos emf+ rendering

--- canvas/source/directx/makefile.mk
+++ canvas/source/directx/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.7 $
+# $Revision: 1.8 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -32,14 +32,16 @@
 PRJ=..$/..
 
 PRJNAME=canvas
-TARGET=directxcanvas
-TARGET2=directx9canvas
+TARGET=directx9canvas
+TARGET2=directx5canvas
+TARGET3=gdipluscanvas
 ENABLE_EXCEPTIONS=TRUE
 
 
 # --- Settings -----------------------------------------------------------
 
 .INCLUDE :	settings.mk
+DLLPRE =
 
 # --- Nothing to do if we're compiling with --disable-directx -----------
 .IF "$(ENABLE_DIRECTX)" == ""
@@ -58,43 +60,58 @@ CDEFS+= -DVERBOSE
 CDEFS+= -DDX_DEBUG_IMAGES
 .ENDIF
 
-CDEFS += -DDIRECTX_VERSION=0x0500
-
 # --- This is Windows only! { ----------------------------------------------------------------
 
 .IF "$(GUI)" == "WNT"
 
-SLOFILES = \
-    $(SLO)$/dx_5rm.obj					    \
-    $(SLO)$/dx_9rm.obj					    \
-    $(SLO)$/dx_bitmap.obj				    \
+SHARED_SLOFILES = \
+    $(SLO)$/dx_bitmap.obj			        \
+    $(SLO)$/dx_bitmapcanvashelper.obj		\
     $(SLO)$/dx_canvasbitmap.obj 			\
     $(SLO)$/dx_canvascustomsprite.obj 		\
     $(SLO)$/dx_canvasfont.obj 				\
     $(SLO)$/dx_canvashelper.obj			    \
     $(SLO)$/dx_canvashelper_texturefill.obj \
-    $(SLO)$/dx_config.obj					\
     $(SLO)$/dx_devicehelper.obj				\
     $(SLO)$/dx_gdiplususer.obj				\
     $(SLO)$/dx_impltools.obj 				\
     $(SLO)$/dx_linepolypolygon.obj			\
+    $(SLO)$/dx_textlayout.obj				\
+    $(SLO)$/dx_textlayout_drawhelper.obj    \
+    $(SLO)$/dx_vcltools.obj
+
+DX_SLOFILES = \
+    $(SLO)$/dx_5rm.obj					    \
+    $(SLO)$/dx_9rm.obj					    \
+    $(SLO)$/dx_config.obj					\
     $(SLO)$/dx_spritecanvas.obj 			\
     $(SLO)$/dx_spritecanvashelper.obj		\
+    $(SLO)$/dx_spritedevicehelper.obj		\
     $(SLO)$/dx_spritehelper.obj 			\
-    $(SLO)$/dx_surfacegraphics.obj			\
-    $(SLO)$/dx_textlayout.obj				\
-    $(SLO)$/dx_vcltools.obj					\
-    $(SLO)$/dx_textlayout_drawhelper.obj    \
+    $(SLO)$/dx_surfacebitmap.obj		    \
+    $(SLO)$/dx_surfacegraphics.obj
+DX_SLOFILES += $(SHARED_SLOFILES)
+
+GDIPLUS_SLOFILES = \
+    $(SLO)$/dx_canvas.obj
+GDIPLUS_SLOFILES += $(SHARED_SLOFILES)
+
+STDLIBS= $(CPPULIB) $(TKLIB) $(SALLIB) $(COMPHELPERLIB) $(CPPUHELPERLIB) $(BASEGFXLIB) $(CANVASTOOLSLIB) $(VCLLIB) $(TOOLSLIB) $(UNOTOOLSLIB)
+
 
 ########################################################
-# DX5
+# DX9
 ########################################################
+
+# Indicates the source obj files for the dx5 lib
+LIB1TARGET= $(SLB)$/$(TARGET).lib
+LIB1OBJFILES = $(DX_SLOFILES)
 
 # Indicates the filename of the shared library.
 SHL1TARGET=$(TARGET).uno
 
 # Links import libraries.
-SHL1STDLIBS= $(CPPULIB) $(TKLIB) $(SALLIB) $(COMPHELPERLIB) $(CPPUHELPERLIB) $(BASEGFXLIB) $(CANVASTOOLSLIB) $(VCLLIB) $(TOOLSLIB) $(UNOTOOLSLIB)
+SHL1STDLIBS= $(STDLIBS)
 
 # Specifies an import library to create. For Win32 only.
 SHL1IMPLIB=i$(TARGET)
@@ -102,78 +119,98 @@ SHL1IMPLIB=i$(TARGET)
 # Specifies libraries from the same module to put into the shared library.
 SHL1LIBS=$(SLB)$/$(TARGET).lib
 
-
-
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
 
 DEF1NAME=$(SHL1TARGET)
 DEF1EXPORTFILE=exports.dxp
 
-SHL1STDLIBS += $(GDI32LIB)
-SHL1STDLIBS += $(DDRAWLIB)
-SHL1STDLIBS += $(GDIPLUSLIB)
+CDEFS+=-DDIRECTX_VERSION=0x0900
 
-.IF "$(USE_DIRECTX5)"!=""
+SHL1STDLIBS += $(GDI32LIB)
 .IF "$(COM)" == "GCC"
-SHL1STDLIBS += $(DIRECTXSDK_LIB)/d3dx.lib
+SHL1STDLIBS += $(DIRECTXSDK_LIB)/d3d9.lib
 .ELSE
-SHL1STDLIBS += d3dx.lib
+SHL1STDLIBS += d3d9.lib
 .ENDIF
-.ELSE # "$(USE_DIRECTX5)
-.IF "$(COM)" == "GCC"
-SHL1STDLIBS += $(DIRECTXSDK_LIB)/d3dx9.lib
-SHL1STDLIBS += $(DIRECTXSDK_LIB)/dxguid.lib
-.ELSE
-SHL1STDLIBS += d3dx9.lib     # directx 9
-SHL1STDLIBS += dxguid.lib    # directx 9
-.ENDIF
-.ENDIF # "$(USE_DIRECTX5)
+SHL1STDLIBS += $(GDIPLUSLIB)
 
 .IF "$(dx_debug_images)"!="" || "$(DX_DEBUG_IMAGES)"!=""
 SHL1STDLIBS += imdebug.lib
 .ENDIF
 
+
 ########################################################
-# DX9
+# DX5
 ########################################################
 
-SECOND_BUILD=DX9
-DX9_SLOFILES=$(SLOFILES)
-DX9CDEFS+=-DDIRECTX_VERSION=0x0900
+.IF "$(USE_DIRECTX5)" != ""
+SECOND_BUILD=DX5
+DX5_SLOFILES=$(DX_SLOFILES)
+DX5CDEFS += -DDIRECTX_VERSION=0x0500
 
-LIB1TARGET= $(SLB)$/$(TARGET)dx9.lib
-LIB1OBJFILES = $(REAL_DX9_SLOFILES)
+LIB2TARGET= $(SLB)$/$(TARGET2).lib
+LIB2OBJFILES = $(REAL_DX5_SLOFILES)
 
 # Indicates the filename of the shared library.
 SHL2TARGET=$(TARGET2).uno
 
 # Links import libraries.
-SHL2STDLIBS= $(CPPULIB) $(TKLIB) $(SALLIB) $(COMPHELPERLIB) $(CPPUHELPERLIB) $(BASEGFXLIB) $(CANVASTOOLSLIB) $(VCLLIB) $(TOOLSLIB) $(UNOTOOLSLIB)
+SHL2STDLIBS= $(STDLIBS)
 
 # Specifies an import library to create. For Win32 only.
-SHL2IMPLIB=$(TARGET)dx9.lib
+SHL2IMPLIB=i$(TARGET2).lib
 
 # Specifies libraries from the same module to put into the shared library.
-SHL2LIBS=$(SLB)$/$(TARGET)dx9.lib
+SHL2LIBS=$(SLB)$/$(TARGET2).lib
 SHL2DEF=$(MISC)$/$(SHL2TARGET).def
 
 DEF2NAME=$(SHL2TARGET)
 DEF2EXPORTFILE=exports.dxp
 
 SHL2STDLIBS += $(GDI32LIB)
-.IF "$(COM)" == "GCC"
-SHL2STDLIBS += $(DIRECTXSDK_LIB)/d3d9.lib
-.ELSE
-SHL2STDLIBS += d3d9.lib
-.ENDIF
+SHL2STDLIBS += $(DDRAWLIB)
 SHL2STDLIBS += $(GDIPLUSLIB)
+
+.IF "$(COM)" == "GCC"
+SHL2STDLIBS += $(DIRECTXSDK_LIB)/d3dx.lib
+.ELSE
+SHL2STDLIBS += d3dx.lib
+.ENDIF
 
 .IF "$(dx_debug_images)"!="" || "$(DX_DEBUG_IMAGES)"!=""
 SHL2STDLIBS += imdebug.lib
 .ENDIF
+.ENDIF # IF "$(USE_DIRECTX5)" != ""
+
+
+########################################################
+# GDI+
+########################################################
+
+LIB3TARGET= $(SLB)$/$(TARGET3).lib
+LIB3OBJFILES = $(GDIPLUS_SLOFILES)
+
+# Indicates the filename of the shared library.
+SHL3TARGET=$(TARGET3).uno
+
+# Links import libraries.
+SHL3STDLIBS= $(CPPULIB) $(TKLIB) $(SALLIB) $(COMPHELPERLIB) $(CPPUHELPERLIB) $(BASEGFXLIB) $(CANVASTOOLSLIB) $(VCLLIB) $(TOOLSLIB) $(UNOTOOLSLIB)
+
+# Specifies an import library to create. For Win32 only.
+SHL3IMPLIB=i$(TARGET3).lib
+
+# Specifies libraries from the same module to put into the shared library.
+SHL3LIBS=$(SLB)$/$(TARGET3).lib
+SHL3DEF=$(MISC)$/$(SHL3TARGET).def
+
+DEF3NAME=$(SHL3TARGET)
+DEF3EXPORTFILE=exports.dxp
+
+SHL3STDLIBS += gdi32.lib
+SHL3STDLIBS += gdiplus.lib
 
 .IF "$(dx_debug_images)"!="" || "$(DX_DEBUG_IMAGES)"!=""
-SHL1STDLIBS += imdebug.lib
+SHL3STDLIBS += imdebug.lib
 .ENDIF
 
 .ENDIF # IF "$(GUI)" == "WNT"
