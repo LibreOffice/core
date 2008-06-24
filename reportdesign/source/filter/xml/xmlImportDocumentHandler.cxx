0edbcd3793b03909a64fb1165c5de5bb258646ad
Kurt Zenker <kz@openoffice.org>
2008-06-24 15:46:47 +0000

    INTEGRATION: CWS obo30 (1.3.26); FILE MERGED 2008/06/24 07:01:26 obo 1.3.26.3: #i90100# Precompiled header include missing 2008/06/20 09:57:59 obo 1.3.26.2: RESYNC: (1.3-1.4); FILE MERGED 2008/06/03 09:48:45 obo 1.3.26.1: #i90100# ambigous Reference during ENABLE_PCH build

--- reportdesign/source/filter/xml/xmlImportDocumentHandler.cxx
+++ reportdesign/source/filter/xml/xmlImportDocumentHandler.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlImportDocumentHandler.cxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -27,6 +27,7 @@
  * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
+#include "precompiled_reportdesign.hxx"
 
 #include "xmlImportDocumentHandler.hxx"
 #include <com/sun/star/sdb/CommandType.hpp>
