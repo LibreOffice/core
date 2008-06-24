354f936f4a0619a8588124b79d2f41145bdb3e60
Kurt Zenker <kz@openoffice.org>
2008-06-24 09:18:39 +0000

    INTEGRATION: CWS canvas05 (1.4.24); FILE MERGED 2008/05/23 22:03:43 thb 1.4.24.9: Moving all remaining new files to LGPL 3 2008/04/10 21:18:37 mox 1.4.24.8: Making cairo quartz buildable. Some pieces still missing. 2008/04/07 14:34:21 thb 1.4.24.7: RESYNC: (1.4-1.6); FILE MERGED 2008/04/03 20:12:35 thb 1.4.24.6: Improved the system-dependent passing-around of the OS handles - now much more opaque 2008/04/03 16:02:25 thb 1.4.24.5: Merged in cairoquartz01 changes, reworked into the polymorphic Surface concept now 2008/04/02 23:07:06 thb 1.4.24.4: And now with all missing functions 2008/04/02 22:56:26 thb 1.4.24.3: Reworked Surface class to abstract interface; changed all manual refcount handling to RAII 2007/12/20 22:18:56 thb 1.4.24.2: #i81092# #i78888# #i78925# #i79258# #i79437# #i84784# Large canvas rework, completing various areas such as color spaces, bitmap data access, true sprite and non-sprite implementations, and upstreaming the canvas parts of rodos emf+ rendering 2007/12/18 10:05:52 thb 1.4.24.1: #i84049# Applying patch from jnavrati to fix a crash when xrender is not available

--- canvas/source/cairo/cairo_cairo.hxx
+++ canvas/source/cairo/cairo_cairo.hxx
@@ -1,124 +1,100 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: cairo_cairo.hxx,v $
+ * $Revision: 1.7 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
 #ifndef _CAIROCANVAS_CAIRO_HXX
 #define _CAIROCANVAS_CAIRO_HXX
 
-#ifdef QUARTZ
-// needed because sysdata.hxx contains native NS* classes
-#include "premac.h"
-#include <Cocoa/Cocoa.h>
-#include "postmac.h"
-#endif
+#include <sal/config.h>
+#include <boost/shared_ptr.hpp>
 
-#include <cairo.h>
+struct SystemEnvData;
+struct BitmapSystemData;
+struct SystemGraphicsData;
+class  VirtualDevice;
+class  OutputDevice;
+class  Window;
+class  Size;
 
-#ifdef CAIRO_HAS_WIN32_SURFACE
-// needed because sysdata.hxx contains native types
-#include <tools/prewin.h>
-#include <windows.h>
-#include <tools/postwin.h>
-#undef min
-#undef max
-#endif
-
-#include <vcl/virdev.hxx>
-#include <vcl/sysdata.hxx>
-#include <vcl/bitmap.hxx>
-
-#include <stdio.h>
+#include <cairo.h>  //cannot be inside a namespace, otherwise Quartz fails to compile.
 
 namespace cairo {
-
-// There are some different code paths with Cairo < 1.2.0 (only supported on XLib)
-#if (CAIRO_VERSION < CAIRO_VERSION_ENCODE(1, 2, 0))
-#define USE_CAIRO10_APIS 1
-#endif
-
     typedef cairo_t Cairo;
     typedef cairo_matrix_t Matrix;
     typedef cairo_format_t Format;
     typedef cairo_content_t Content;
     typedef cairo_pattern_t Pattern;
 
-#if defined(CAIRO_HAS_XLIB_SURFACE) && !defined (QUARTZ)
-     bool HasXRender( const void* pSysData );
-#endif
+    typedef boost::shared_ptr<cairo_surface_t> CairoSurfaceSharedPtr;
+    typedef boost::shared_ptr<Cairo>           CairoSharedPtr;
 
-    class Surface {
-        int mnRefCount;
-#if defined QUARTZ
-        // nothing needed
-#elif defined (CAIRO_HAS_XLIB_SURFACE)
-    #ifdef USE_CAIRO10_APIS
-        const void* mpSysData;
-        void* mpDisplay;
-        long mhDrawable;
-        void *mpRenderFormat;
-        bool mbFreePixmap;
-    #endif
-#elif defined (CAIRO_HAS_WIN32_SURFACE)
-        // nothing needed
-#endif
+    const SystemEnvData* GetSysData(const Window *pOutputWindow);
+
+    /** Cairo surface interface
+
+        For each cairo-supported platform, there's an implementation of
+        this interface
+     */
+    struct Surface
+    {
     public:
-        cairo_surface_t* mpSurface;
+        virtual ~Surface() {}
 
-#if defined(USE_CAIRO10_APIS) && defined(CAIRO_HAS_XLIB_SURFACE) && !defined(QUARTZ)
-        Surface( const void* pSysData, void* pDisplay, long hDrawable, void* pRenderFormat, cairo_surface_t* pSurface );
-#endif
+        // Query methods
+        virtual CairoSharedPtr getCairo() const = 0;
+        virtual CairoSurfaceSharedPtr getCairoSurface() const = 0;
+        virtual boost::shared_ptr<Surface> getSimilar( Content aContent, int width, int height ) const = 0;
 
-        explicit Surface( cairo_surface_t* pSurface );
-        Surface( const SystemEnvData* pSysData, int x, int y, int width, int height );
-        Surface( const SystemEnvData* pSysData, const BitmapSystemData* pBmpData, int width, int height );
+        /// factory for VirDev on this surface
+        virtual boost::shared_ptr<VirtualDevice> createVirtualDevice() const = 0;
 
-        ~Surface();
+        /// Resize the surface (possibly destroying content)
+        virtual void Resize( int width, int height ) = 0;
 
-        void Ref()
-        {
-            mnRefCount ++;
-        }
+        /// Flush all pending output to surface
+        virtual void flush() const = 0;
+    };
 
-        void Unref()
-        {
-            mnRefCount --;
-            if( mnRefCount <= 0 )
-                delete this;
-        }
+    typedef boost::shared_ptr<Surface> SurfaceSharedPtr;
 
-        Cairo* getCairo();
+    /// Create Surface from given cairo surface
+    SurfaceSharedPtr createSurface( const CairoSurfaceSharedPtr& rSurface );
+    /// Create surface with given dimensions
+    SurfaceSharedPtr createSurface( const OutputDevice& rRefDevice,
+                                    int x, int y, int width, int height );
+    /// Create Surface for given bitmap data
+    SurfaceSharedPtr createBitmapSurface( const OutputDevice& rRefDevice,
+                                          const BitmapSystemData& rData,
+                                          const Size&             rSize );
 
-        Surface* getSimilar( Content aContent, int width, int height );
-
-        VirtualDevice* createVirtualDevice();
-        void fillSystemGraphicsData( SystemGraphicsData& data );
-
-        int getDepth();
-
-
-#if defined(CAIRO_HAS_XLIB_SURFACE) && !defined (QUARTZ)
-    #if defined USE_CAIRO10_APIS
-        long getPixmap()
-        {
-            return mhDrawable;
-        }
-
-        void* getRenderFormat()
-        {
-            return mpRenderFormat;
-        }
-    #endif
-
-        int getXFormat( Content aContent );
-
-        // use only for surfaces made on X Drawables
-        void Resize( int width, int height );
-#endif
-
-
-#ifdef UNX
-        // Only used by Xlib and the current Mac OS X Quartz implementation
-        void flush(const SystemEnvData* pSysData);
-#endif
-
-    };  // class Surface
-
-} // namespace cairo
+    /// Check whether cairo will work on given window
+    bool IsCairoWorking( OutputDevice* );
+}
 
 #endif
