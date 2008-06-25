5427e2a77c5f94ee196d2ae1676f6bd99613650b
Kurt Zenker <kz@openoffice.org>
2008-06-25 13:15:25 +0000

    INTEGRATION: CWS xformsdocmodify (1.10.38); FILE MERGED 2008/06/03 20:48:46 fs 1.10.38.1: #i90243# +SetDocumentSpecificSettings

--- xmloff/inc/xmloff/xmlimp.hxx
+++ xmloff/inc/xmloff/xmlimp.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlimp.hxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -336,6 +336,8 @@ public:
 
     virtual void SetViewSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aViewProps);
     virtual void SetConfigurationSettings(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aConfigProps);
+    virtual void SetDocumentSpecificSettings(const ::rtl::OUString& _rSettingsGroupName,
+                    const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& _rSettings);
 
     XMLFontStylesContext *GetFontDecls();
     SvXMLStylesContext *GetStyles();
