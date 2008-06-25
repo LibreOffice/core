ca73ab217794a4812784b42cae22c6a7cd7b74d3
Kurt Zenker <kz@openoffice.org>
2008-06-25 13:38:59 +0000

    INTEGRATION: CWS rptwizard01 (1.1.2); FILE ADDED 2008/06/24 14:59:26 kz 1.1.2.2: fixed wrong license header 2008/05/20 10:52:16 lla 1.1.2.1: #i86092#

---
+++ wizards/com/sun/star/wizards/reportbuilder/layout/SectionTextField.java
@@ -0,0 +1,71 @@
+/*
+ * ***********************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: SectionTextField.java,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ * **********************************************************************
+ */
+
+package com.sun.star.wizards.reportbuilder.layout;
+
+import com.sun.star.awt.FontDescriptor;
+import com.sun.star.report.XFormattedField;
+
+/**
+ *
+ * @author ll93751
+ */
+public class SectionTextField  extends SectionObject
+{
+    protected SectionTextField(XFormattedField _aFormattedField)
+    {
+        m_aParentObject = _aFormattedField;
+        // We would like to know, what properties are in this object.
+    }
+
+    public static SectionObject create(XFormattedField _aFormattedField)
+    {
+        SectionTextField a = new SectionTextField(_aFormattedField);
+        return a;
+    }
+
+    public FontDescriptor getFontDescriptor()
+    {
+        FontDescriptor a = null;
+        try
+        {
+            XFormattedField aField = (XFormattedField)getParent();
+            a = aField.getFontDescriptor();
+        }
+        catch (com.sun.star.beans.UnknownPropertyException e)
+        {
+        }
+        return a;
+    }
+}
+
