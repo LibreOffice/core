ce10a1ea60f370155f8d6abba0f93ccb5e422ede
Kurt Zenker <kz@openoffice.org>
2008-06-25 10:53:15 +0000

    INTEGRATION: CWS dba30d (1.15.18); FILE MERGED 2008/05/27 12:28:02 fs 1.15.18.1: #i89657# refactoring, so that our binding's getValue is only called when our mutex is not locked

--- forms/source/component/Date.hxx
+++ forms/source/component/Date.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: Date.hxx,v $
- * $Revision: 1.15 $
+ * $Revision: 1.16 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -85,8 +85,10 @@ protected:
 
     virtual ::com::sun::star::uno::Any
                             translateControlValueToExternalValue( ) const;
+    virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type >
+                            getSupportedBindingTypes();
     virtual ::com::sun::star::uno::Any
-                            translateExternalValueToControlValue( ) const;
+                            translateExternalValueToControlValue( const ::com::sun::star::uno::Any& _rExternalValue ) const;
 
     virtual ::com::sun::star::uno::Any
                             translateControlValueToValidatableValue( ) const;
@@ -95,8 +97,6 @@ protected:
                             getDefaultForReset() const;
 
     virtual void            onConnectedDbColumn( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& _rxForm );
-
-    virtual sal_Bool        approveValueBinding( const ::com::sun::star::uno::Reference< ::com::sun::star::form::binding::XValueBinding >& _rxBinding );
 
 protected:
     DECLARE_XCLONEABLE();
