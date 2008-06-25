64751d4c58dcea1ec2b7afb1eaf1c75d72d59d34
Kurt Zenker <kz@openoffice.org>
2008-06-25 13:37:46 +0000

    INTEGRATION: CWS rptwizard01 (1.1.2); FILE ADDED 2008/06/24 14:58:46 kz 1.1.2.6: fixed wrong license header 2008/05/20 10:53:02 lla 1.1.2.5: #i86092# support templates 2008/05/16 07:05:43 lla 1.1.2.4: #i86092# restructures to work with design templates 2008/03/14 13:50:10 lla 1.1.2.3: #i86092# PMD cleanups 2008/03/11 12:52:29 lla 1.1.2.2: #i86092# cleanups, make labelwidth variable 2008/03/07 07:36:52 lla 1.1.2.1: #i86092#

---
+++ wizards/com/sun/star/wizards/reportbuilder/layout/InBlocksLabelsAbove.java
@@ -0,0 +1,166 @@
+/*
+ * ***********************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: InBlocksLabelsAbove.java,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ * **********************************************************************
+ */
+
+package com.sun.star.wizards.reportbuilder.layout;
+
+import com.sun.star.awt.Rectangle;
+import com.sun.star.report.XSection;
+import com.sun.star.wizards.common.Resource;
+import com.sun.star.wizards.report.IReportDefinitionReadAccess;
+import com.sun.star.wizards.ui.*;
+
+/**
+ *
+ * @author ll93751
+ */
+public class InBlocksLabelsAbove extends ColumnarTwoColumns
+{
+    public InBlocksLabelsAbove(IReportDefinitionReadAccess _xDefinitionAccess, Resource _aResource)
+    {
+        super(_xDefinitionAccess, _aResource);
+    }
+
+    public String getName()
+    {
+        return "InBlocksLabelsAboveLayoutOfData";
+    }
+
+    public String getLocalizedName()
+    {
+        final String sLocalizedName = getResource().getResText(UIConsts.RID_REPORT + 85);
+        return sLocalizedName;
+    }
+
+    protected void insertDetailFields()
+    {
+        copyDetailProperties();
+
+        final String[] aFieldTitleNames = getFieldTitleNames();
+        if (aFieldTitleNames == null)
+        {
+            return;
+        }
+        final String[] aFieldNames = getFieldNames();
+        if (aFieldNames == null)
+        {
+            return;
+        }
+//        int nGroups = getReportDefinition().getGroups().getCount();
+
+        final XSection xSection = getReportDefinition().getDetail();
+
+        Rectangle aRectLabels = new Rectangle();
+        Rectangle aRectFields = new Rectangle();
+
+
+        final int nUsablePageWidth = getPageWidth() - getLeftPageIndent() - getRightPageIndent() - getLeftGroupIndent(getCountOfGroups());
+
+        int i=0;
+        int nCount = aFieldTitleNames.length;
+        // int x = 0;
+        aRectLabels.Y = 0;
+        aRectLabels.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+
+        aRectFields.Y = 500;
+        aRectFields.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+
+        // first run only calculates the width.
+        int nDelta = 0;
+        int nFieldWidth = 3000;
+        while (nCount > 0)
+        {
+            // String sLabel = aFieldTitleNames[i];
+            // int nLabelWidth = 3000;
+            // String sFieldName = convertToFieldName(aFieldNames[i]);
+            aRectFields.X += nFieldWidth;
+            // aRectLabels.X += nFieldWidth;
+
+            final int nNextX = aRectFields.X + nFieldWidth;
+            if (nNextX > (getPageWidth() - getRightPageIndent()) )
+            {
+                // all other values are not from interest.
+                break;
+            }
+
+            ++i;
+            --nCount;
+        }
+        final int nDeltaTotal = nUsablePageWidth - i * nFieldWidth;
+        nDelta = nDeltaTotal;
+        if (i > 0)
+        {
+            nDelta = nDeltaTotal / i;
+        }
+
+        aRectLabels.Y = 0;
+        aRectLabels.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+
+        final SectionObject aSOLabel = getDesignTemplate().getDetailLabel();
+        aSOLabel.setFontToBold();
+        final SectionObject aSOTextField = getDesignTemplate().getDetailTextField();
+
+        aRectFields.Y = aSOLabel.getHeight(500);
+        aRectFields.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+        i = 0;
+        nCount = aFieldTitleNames.length;
+        while (nCount > 0)
+        {
+            final String sLabel = aFieldTitleNames[i];
+            final int nLabelWidth = 3000 + nDelta;
+            aRectLabels = insertLabel(xSection, sLabel, aRectLabels, nLabelWidth, aSOLabel);
+            final String sFieldName = convertToFieldName(aFieldNames[i]);
+            nFieldWidth = 3000 + nDelta;
+            aRectFields = insertFormattedField(xSection, sFieldName, aRectFields, nFieldWidth, aSOTextField);
+
+            int nNextX = aRectFields.X + nFieldWidth;
+            if (nNextX > (getPageWidth() - getRightPageIndent()) & nCount > 1)
+            {
+                aRectLabels.Y += (2 * aSOLabel.getHeight(500)); // 2 * label height
+                aRectLabels.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+
+                aRectFields.Y += (2 * aSOTextField.getHeight(500));
+                aRectFields.X = getLeftPageIndent() + getLeftGroupIndent(getCountOfGroups());
+            }
+            ++i;
+            --nCount;
+        }
+        aRectFields.Y += aSOTextField.getHeight(500);
+        aRectFields.Y += aSOTextField.getHeight(500); // one empty line
+        xSection.setHeight(aRectFields.Y);
+        doNotBreakInTable(xSection);
+    }
+    protected void insertDetailFieldTitles()
+    {
+        // we won't extra field titles
+    }
+}
