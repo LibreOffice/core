caa2fdc06d48e30de24131be36b40b1882fa97f8
Kurt Zenker <kz@openoffice.org>
2008-06-25 13:26:33 +0000

    INTEGRATION: CWS rptwizard01 (1.4.126); FILE MERGED 2008/02/14 13:11:46 lla 1.4.126.1: #i86092# cleanups

--- wizards/com/sun/star/wizards/common/NoValidPathException.java
+++ wizards/com/sun/star/wizards/common/NoValidPathException.java
@@ -2,10 +2,15 @@ package com.sun.star.wizards.common;
 
 import com.sun.star.lang.XMultiServiceFactory;
 
-public class NoValidPathException extends Exception {
-
-    public NoValidPathException(XMultiServiceFactory xMSF) {
-        SystemDialog.showErrorBox(xMSF, "dbwizres", "dbw", 521); // OfficePathnotavailable
+public class NoValidPathException extends Exception
+{
+    public NoValidPathException(XMultiServiceFactory xMSF, String _sText)
+    {
+        super(_sText);
+        // TODO: NEVER open a dialog in an exception
+        if (xMSF != null)
+        {
+            SystemDialog.showErrorBox(xMSF, "dbwizres", "dbw", 521); // OfficePathnotavailable
+        }
     }
-
 }
