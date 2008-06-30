95cecd8e91847030076de25241fdcfe581f5a27d
Vladimir Glazounov <vg@openoffice.org>
2008-06-30 14:45:24 +0000

    INTEGRATION: CWS hub01 (1.8.264); FILE MERGED 2008/06/18 20:45:01 thb 1.8.264.2: RESYNC: (1.8-1.10); FILE MERGED 2008/01/16 18:09:03 hub 1.8.264.1: Issue number: 85274 Submitted by:  hub Reviewed by:   hub Fix warning from gcc 4.2.1 for empty else- statements.

--- svx/source/outliner/paralist.cxx
+++ svx/source/outliner/paralist.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: paralist.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -114,7 +114,9 @@ void ParagraphList::MoveParagraphs( ULONG nStart, ULONG nDest, ULONG _nCount )
         }
     }
     else
+    {
         DBG_ERROR( "MoveParagraphs: Invalid Parameters" );
+    }
 }
 
 Paragraph* ParagraphList::NextVisible( Paragraph* pPara ) const
