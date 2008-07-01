3d2a0a775d051c613d35db89dcec4866e04c7211
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 10:26:30 +0000

    INTEGRATION: CWS xmlfilter05 (1.55.32); FILE MERGED 2008/06/16 08:43:53 dr 1.55.32.3: RESYNC: (1.56-1.57); FILE MERGED 2008/05/02 10:42:23 hbrinkm 1.55.32.2: RESYNC: (1.55-1.56); FILE MERGED 2008/04/01 15:34:02 hbrinkm 1.55.32.1: 'Merged xmlfilter04'

--- sw/source/core/text/frmpaint.cxx
+++ sw/source/core/text/frmpaint.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: frmpaint.cxx,v $
- * $Revision: 1.57 $
+ * $Revision: 1.58 $
  *
  * This file is part of OpenOffice.org.
  *
