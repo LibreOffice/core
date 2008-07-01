2eac83fcca78533dfc14b6f3bb2b4e32a46b4349
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 09:59:59 +0000

    INTEGRATION: CWS xmlfilter05 (1.1.2); FILE ADDED 2008/05/21 13:27:14 dr 1.1.2.2: #i10000# new header 2008/04/24 06:45:58 os 1.1.2.1: OLE handler

---
+++ writerfilter/source/dmapper/OLEHandler.hxx
@@ -0,0 +1,82 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: OLEHandler.hxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+#ifndef INCLUDED_OLEHANDLER_HXX
+#define INCLUDED_OLEHANDLER_HXX
+
+#ifndef INCLUDED_WRITERFILTERDLLAPI_H
+#include <WriterFilterDllApi.hxx>
+#endif
+#include <resourcemodel/WW8ResourceModel.hxx>
+#include <boost/shared_ptr.hpp>
+
+namespace com{ namespace sun{ namespace star{
+    namespace embed{
+        class XEmbeddedObject;
+    }
+    namespace io{
+        class XInputStream;
+    }
+    namespace text{
+        class XTextDocument;
+    }
+    namespace uno{
+        class XComponentContext;
+    }
+}}}
+namespace writerfilter {
+namespace dmapper
+{
+//class PropertyMap;
+/** Handler for OLE objects
+ */
+class WRITERFILTER_DLLPRIVATE OLEHandler : public Properties
+{
+    ::rtl::OUString     m_sObjectType;
+    ::rtl::OUString     m_sProgId;
+    ::rtl::OUString     m_sShapeId;
+    ::rtl::OUString     m_sDrawAspect;
+    ::rtl::OUString     m_sObjectId;
+    ::rtl::OUString     m_sr_id;
+
+    ::com::sun::star::uno::Reference< ::com::sun::star::io::XInputStream > m_xInputStream;
+public:
+    OLEHandler();
+    virtual ~OLEHandler();
+
+    // Properties
+    virtual void attribute(Id Name, Value & val);
+    virtual void sprm(Sprm & sprm);
+
+    ::rtl::OUString copyOLEOStream( ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextDocument > xTextDocument );
+};
+typedef boost::shared_ptr< OLEHandler >  OLEHandlerPtr;
+}}
+
+#endif //
