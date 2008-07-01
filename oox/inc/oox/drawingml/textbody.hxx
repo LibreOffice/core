d7c4efb41301e3295958ed3faf6cc6a1170da3b3
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 10:36:05 +0000

    INTEGRATION: CWS xmlfilter05 (1.3.4); FILE MERGED 2008/06/27 12:09:40 dr 1.3.4.5: RESYNC: (1.5-1.6); FILE MERGED 2008/05/21 11:48:16 dr 1.3.4.4: #i10000# new header 2008/04/25 08:57:32 dr 1.3.4.3: more cleanup 2008/04/02 12:41:45 hbrinkm 1.3.4.2: merged changes from xmlfilter04 to xmlfilter05 2008/04/01 15:37:14 hbrinkm 1.3.4.1: 'Merged xmlfilter04'

--- oox/inc/oox/drawingml/textbody.hxx
+++ oox/inc/oox/drawingml/textbody.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: textbody.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -69,8 +69,6 @@ protected:
     PropertyMap         maTextProperties;
     TextListStyle       maTextListStyle;
 };
-
-typedef boost::shared_ptr< TextBody > TextBodyPtr;
 
 } }
 
