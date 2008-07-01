dc087c146b4c5a2d453d0592fe233a8fdd506276
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 11:05:13 +0000

    INTEGRATION: CWS xmlfilter05 (1.1.2); FILE ADDED 2008/05/21 12:55:44 dr 1.1.2.2: #i10000# new header 2008/04/25 08:57:34 dr 1.1.2.1: more cleanup

---
+++ oox/source/drawingml/transform2dcontext.cxx
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: transform2dcontext.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "oox/drawingml/transform2dcontext.hxx"
+#include "oox/core/namespaces.hxx"
+#include "oox/helper/attributelist.hxx"
+#include "oox/drawingml/shape.hxx"
+
+using ::com::sun::star::awt::Point;
+using ::com::sun::star::awt::Size;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::RuntimeException;
+using ::com::sun::star::xml::sax::SAXException;
+using ::com::sun::star::xml::sax::XFastAttributeList;
+using ::com::sun::star::xml::sax::XFastContextHandler;
+using ::oox::core::ContextHandler;
+
+namespace oox {
+namespace drawingml {
+
+// ============================================================================
+
+/** context to import a CT_Transform2D */
+Transform2DContext::Transform2DContext( ContextHandler& rParent, const Reference< XFastAttributeList >& xAttribs, Shape& rShape ) throw()
+: ContextHandler( rParent )
+, mrShape( rShape )
+{
+    AttributeList aAttributeList( xAttribs );
+    mrShape.setRotation( aAttributeList.getInteger( XML_rot, 0 ) ); // 60000ths of a degree Positive angles are clockwise; negative angles are counter-clockwise
+    mrShape.setFlip( aAttributeList.getBool( XML_flipH, sal_False ), aAttributeList.getBool( XML_flipV, sal_False ) );
+}
+
+Reference< XFastContextHandler > Transform2DContext::createFastChildContext( sal_Int32 aElementToken, const Reference< XFastAttributeList >& xAttribs ) throw (SAXException, RuntimeException)
+{
+    switch( aElementToken )
+    {
+    case NMSP_DRAWINGML|XML_off:        // horz/vert translation
+        mrShape.setPosition( Point( xAttribs->getOptionalValue( XML_x ).toInt32(), xAttribs->getOptionalValue( XML_y ).toInt32() ) );
+        break;
+    case NMSP_DRAWINGML|XML_ext:        // horz/vert size
+        mrShape.setSize( Size( xAttribs->getOptionalValue( XML_cx ).toInt32(), xAttribs->getOptionalValue( XML_cy ).toInt32() ) );
+        break;
+/* todo: what to do?
+    case NMSP_DRAWINGML|XML_chOff:  // horz/vert translation of children
+    case NMSP_DRAWINGML|XML_chExt:  // horz/vert size of children
+        break;
+*/
+    }
+
+    return 0;
+}
+
+// ============================================================================
+
+} // namespace drawingml
+} // namespace oox
+
