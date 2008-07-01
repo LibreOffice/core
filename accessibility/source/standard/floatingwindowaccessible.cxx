f46fc469e096c6dadfb596969c5c450a8cc806c9
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 13:08:39 +0000

    #i88766# solve duplicate include

--- accessibility/source/standard/floatingwindowaccessible.cxx
+++ accessibility/source/standard/floatingwindowaccessible.cxx
@@ -8,7 +8,7 @@
  *
  * $RCSfile: floatingwindowaccessible.cxx,v $
  *
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,7 +31,6 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_accessibility.hxx"
 
-#include "precompiled_accessibility.hxx"
 #include <floatingwindowaccessible.hxx>
 
 
