a550c34f9521da15c05568f76f1f25b0543d33c1
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 13:32:00 +0000

    INTEGRATION: CWS notes6 (1.6.24); FILE MERGED 2008/06/10 18:29:37 mod 1.6.24.1: notes5 import

--- sw/inc/PostItMgr.hxx
+++ sw/inc/PostItMgr.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: PostItMgr.hxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
