056fea9aca1dc3787bd8c3d3e1c4f9b776b42c04
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 11:01:17 +0000

    INTEGRATION: CWS xmlfilter05 (1.3.4); FILE MERGED 2008/05/02 11:16:45 hbrinkm 1.3.4.4: RESYNC: (1.3-1.5); FILE MERGED 2008/04/25 08:57:33 dr 1.3.4.3: more cleanup 2008/04/02 12:41:55 hbrinkm 1.3.4.2: merged changes from xmlfilter04 to xmlfilter05 2008/04/01 15:38:06 hbrinkm 1.3.4.1: 'Merged xmlfilter04'

--- oox/source/drawingml/spdefcontext.cxx
+++ oox/source/drawingml/spdefcontext.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: spdefcontext.cxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -30,6 +30,7 @@
 
 #include "oox/drawingml/spdefcontext.hxx"
 #include "oox/drawingml/shapepropertiescontext.hxx"
+#include "oox/drawingml/textbody.hxx"
 #include "oox/drawingml/textbodypropertiescontext.hxx"
 #include "oox/drawingml/textliststylecontext.hxx"
 #include "oox/core/namespaces.hxx"
