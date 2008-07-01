1bc35eb247010edb098cd7e1ebd1918ae9318d01
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 11:13:09 +0000

    INTEGRATION: CWS xmlfilter05 (1.3.4); FILE MERGED 2008/05/02 11:18:30 hbrinkm 1.3.4.4: RESYNC: (1.3-1.4); FILE MERGED 2008/04/23 15:58:36 dr 1.3.4.3: a little theme cleanup 2008/04/02 12:41:58 hbrinkm 1.3.4.2: merged changes from xmlfilter04 to xmlfilter05 2008/04/01 15:38:19 hbrinkm 1.3.4.1: 'Merged xmlfilter04'

--- oox/source/drawingml/diagram/diagramdefinitioncontext.cxx
+++ oox/source/drawingml/diagram/diagramdefinitioncontext.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: diagramdefinitioncontext.cxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -29,6 +29,7 @@
  ************************************************************************/
 
 #include "oox/core/skipcontext.hxx"
+#include "oox/core/namespaces.hxx"
 #include "layoutnodecontext.hxx"
 #include "diagramdefinitioncontext.hxx"
 #include "oox/drawingml/diagram/datamodelcontext.hxx"
