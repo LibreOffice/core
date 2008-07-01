53a5bea4142d2224e345801f876b11692fceb5c5
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 13:33:21 +0000

    INTEGRATION: CWS notes6 (1.72.102); FILE MERGED 2008/06/18 23:14:08 mod 1.72.102.2: RESYNC: (1.72-1.74); FILE MERGED 2008/06/10 18:29:38 mod 1.72.102.1: notes5 import

--- sw/source/core/crsr/crsrsh.cxx
+++ sw/source/core/crsr/crsrsh.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: crsrsh.cxx,v $
- * $Revision: 1.74 $
+ * $Revision: 1.75 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -566,7 +566,6 @@ void SwCrsrShell::ExtendedSelectAll()
     SwCntntNode* pCNd = rNodes.GoPrevious( &pPos->nNode );
     pPos->nContent.Assign( pCNd, pCNd ? pCNd->Len() : 0 );
 }
-
 
 BOOL SwCrsrShell::MovePage( SwWhichPage fnWhichPage, SwPosPage fnPosPage )
 {
