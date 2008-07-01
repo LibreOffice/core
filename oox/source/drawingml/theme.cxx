35bcd25e7cc2d1777925626db469eed755a9c138
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 11:04:39 +0000

    INTEGRATION: CWS xmlfilter05 (1.2.18); FILE MERGED 2008/05/02 11:17:42 hbrinkm 1.2.18.2: RESYNC: (1.2-1.3); FILE MERGED 2008/04/23 15:58:14 dr 1.2.18.1: a little theme cleanup

--- oox/source/drawingml/theme.cxx
+++ oox/source/drawingml/theme.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: theme.cxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -28,23 +28,23 @@
  *
  ************************************************************************/
 
-#include "oox/drawingml/shape.hxx"
-#include "oox/core/namespaces.hxx"
-#include "tokens.hxx"
+#include "oox/drawingml/theme.hxx"
 
-using namespace ::oox::core;
+namespace oox {
+namespace drawingml {
 
-namespace oox { namespace drawingml {
+// ============================================================================
 
 Theme::Theme()
-: mpClrSchemePtr( new ClrScheme )
-, mpspDefPtr( new Shape )
-, mplnDefPtr( new Shape )
-, mptxDefPtr( new Shape )
 {
 }
+
 Theme::~Theme()
 {
 }
 
-} }
+// ============================================================================
+
+} // namespace drawingml
+} // namespace oox
+
