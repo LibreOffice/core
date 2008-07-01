db445b584e4f381ce62ac0d586262ec6263b5588
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 15:10:12 +0000

    INTEGRATION: CWS odfmetadata (1.1.2); FILE ADDED 2008/06/17 09:57:28 mst 1.1.2.1: - unoxml/prj/build.lst:   + add new directory: source/rdf   + add new dependency: redland - unoxml/source/rdf:   + new library unordf: redland-based RDF repository - unoxml/qa/complex:   + add complex test: RDFRepositoryTest

---
+++ unoxml/qa/complex/makefile.mk
@@ -0,0 +1,61 @@
+#*************************************************************************
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.2 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#***********************************************************************/
+
+PRJ = ..$/..
+TARGET  = unoxml_test
+PRJNAME = $(TARGET)
+PACKAGE = complex$/unoxml
+
+# --- Settings -----------------------------------------------------
+.INCLUDE: settings.mk
+
+
+#----- compile .java files -----------------------------------------
+
+JARFILES        = ridl.jar unoil.jar jurt.jar juh.jar jut.jar java_uno.jar OOoRunner.jar
+JAVAFILES       = RDFRepositoryTest.java \
+
+JAVACLASSFILES	= $(foreach,i,$(JAVAFILES) $(CLASSDIR)$/$(PACKAGE)$/$(i:b).class)
+
+#----- make a jar from compiled files ------------------------------
+
+MAXLINELENGTH = 100000
+
+JARCLASSDIRS    = $(PACKAGE)
+JARTARGET       = $(TARGET).jar
+JARCOMPRESS 	= TRUE
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+
+
+run:
+    +java -cp $(CLASSPATH) org.openoffice.Runner -TestBase java_complex -sce tests.sce -tdoc $(PWD)$/testdocuments
+
