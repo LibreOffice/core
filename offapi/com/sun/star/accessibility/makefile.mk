0681b10f16ea8a1b768154419a485ab3b5f314a0
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 14:13:17 +0000

    INTEGRATION: CWS sw30a11y01 (1.7.52); FILE MERGED 2008/06/02 09:13:10 od 1.7.52.2: RESYNC: (1.7-1.8); FILE MERGED 2008/05/27 14:03:50 od 1.7.52.1: #i71360# new interface XAccessibleTextMarkup

--- offapi/com/sun/star/accessibility/makefile.mk
+++ offapi/com/sun/star/accessibility/makefile.mk
@@ -1,14 +1,14 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.8 $
+# $Revision: 1.9 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -75,6 +75,7 @@ IDLFILES=\
     XAccessibleTable.idl                            \
     XAccessibleText.idl                             \
     XAccessibleTextAttributes.idl                   \
+    XAccessibleTextMarkup.idl                       \
     XAccessibleValue.idl
 
 # ------------------------------------------------------------------
