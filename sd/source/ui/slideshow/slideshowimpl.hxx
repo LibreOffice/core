918d1b5e1b8885be2894b2fd3d2e5395dce8c9ad
Rüdiger Timm <rt@openoffice.org>
2008-07-02 10:53:10 +0000

    INTEGRATION: CWS impress145 (1.27.50); FILE MERGED 2008/06/16 13:37:20 cl 1.27.50.1: #i71026# added support for windows media application commands

--- sd/source/ui/slideshow/slideshowimpl.hxx
+++ sd/source/ui/slideshow/slideshowimpl.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: slideshowimpl.hxx,v $
- * $Revision: 1.27 $
+ * $Revision: 1.28 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -300,6 +300,7 @@ private:
     DECL_LINK( ContextMenuSelectHdl, Menu * );
     DECL_LINK( ContextMenuHdl, void* );
     DECL_LINK( deactivateHdl, Timer* );
+    DECL_LINK( EventListenerHdl, VclSimpleEvent* );
 
     // helper
     void receiveRequest(SfxRequest& rReq);
