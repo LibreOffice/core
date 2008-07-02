50dbfa8bc1464dca866f63ce0dd6bb063a43a0a0
Rüdiger Timm <rt@openoffice.org>
2008-07-02 08:36:58 +0000

    INTEGRATION: CWS dr61 (1.4.16); FILE MERGED 2008/05/19 12:24:04 dr 1.4.16.1: #i10000# removed remaining include guards

--- sc/source/filter/xml/XMLTableShapesContext.hxx
+++ sc/source/filter/xml/XMLTableShapesContext.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: XMLTableShapesContext.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -28,8 +28,8 @@
  *
  ************************************************************************/
 
-#ifndef _SC_XMLTABLESHAPESCONTEXT_HXX
-#define _SC_XMLTABLESHAPESCONTEXT_HXX
+#ifndef SC_XMLTABLESHAPESCONTEXT_HXX
+#define SC_XMLTABLESHAPESCONTEXT_HXX
 
 #include <xmloff/xmlictxt.hxx>
 
