61f02ddf1a35bb0673e1beaacae8ec0f503826db
Vladimir Glazounov <vg@openoffice.org>
2008-07-02 09:14:54 +0000

    INTEGRATION: CWS vcl90 (1.70.54); FILE MERGED 2008/06/06 14:58:56 pl 1.70.54.1: #i61446# workaround icewms halfhearted EWMH support

--- vcl/unx/source/app/wmadaptor.cxx
+++ vcl/unx/source/app/wmadaptor.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: wmadaptor.cxx,v $
- * $Revision: 1.70 $
+ * $Revision: 1.71 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -2036,6 +2036,10 @@ int GnomeWMAdaptor::handlePropertyNotify( X11SalFrame* pFrame, XPropertyEvent* p
                 Rectangle( Point( rGeom.nX, rGeom.nY ),
                            Size( rGeom.nWidth, rGeom.nHeight ) );
         }
+    }
+    else if( pEvent->atom == m_aWMAtoms[ NET_WM_DESKTOP ] )
+    {
+        pFrame->m_nWorkArea = getWindowWorkArea( pFrame->GetShellWindow() );
     }
     else
         nHandled = 0;
