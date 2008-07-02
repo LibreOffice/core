1d188a7a7f71b8f299bafbe3f8c7098182198a64
Rüdiger Timm <rt@openoffice.org>
2008-07-02 08:03:27 +0000

    INTEGRATION: CWS dr61 (1.10.16); FILE MERGED 2008/05/19 12:24:00 dr 1.10.16.1: #i10000# removed remaining include guards

--- sc/source/filter/excel/xename.cxx
+++ sc/source/filter/excel/xename.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xename.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -34,9 +34,7 @@
 
 #include <map>
 
-#ifndef __GLOBSTR_HRC_
 #include "globstr.hrc"
-#endif
 #include "document.hxx"
 #include "rangenam.hxx"
 #include "dbcolect.hxx"
