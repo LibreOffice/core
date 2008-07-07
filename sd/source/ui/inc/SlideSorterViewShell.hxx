c543bbaaf029c63a2b145d968368ad40c810a9e0
Rüdiger Timm <rt@openoffice.org>
2008-07-07 12:33:05 +0000

    INTEGRATION: CWS impress146 (1.13.56); FILE MERGED 2008/06/27 14:52:50 af 1.13.56.1: #i89785# Added GetClipboardState() method.

--- sd/source/ui/inc/SlideSorterViewShell.hxx
+++ sd/source/ui/inc/SlideSorterViewShell.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: SlideSorterViewShell.hxx,v $
- * $Revision: 1.13 $
+ * $Revision: 1.14 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -101,6 +101,7 @@ public:
     void ExecStatusBar (SfxRequest& rRequest);
     virtual void Command (const CommandEvent& rEvent, ::sd::Window* pWindow);
     virtual void GetMenuState (SfxItemSet &rSet);
+    virtual void GetClipboardState (SfxItemSet &rSet);
 
     virtual void ReadFrameViewData (FrameView* pView);
     virtual void WriteFrameViewData (void);
