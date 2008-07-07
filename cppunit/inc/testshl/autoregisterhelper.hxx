9c4a7372c78eb0324aba43829e16bbe64b0b4046
Lars Langhans <lla@openoffice.org>
2008-07-07 05:13:43 +0000

    #86509# cleanups

--- cppunit/inc/testshl/autoregisterhelper.hxx
+++ cppunit/inc/testshl/autoregisterhelper.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- *  $RCSfile: autoregisterhelper.hxx,v $
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $Revision: 1.1 $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  last change: $Author: lla $ $Date: 2008-02-27 16:19:14 $
+ * $RCSfile: autoregisterhelper.hxx,v $
+ * $Revision: 1.2 $
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * This file is part of OpenOffice.org.
  *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
+ * OpenOffice.org is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -52,6 +47,7 @@ class AutomaticRegisterHelper : public DynamicLibraryHelper
 {
     FunctionList m_aFunctionList;
     bool m_bLoadLibraryOK;
+    // GetOpt & m_aOptions; // use getOptions() instead!
 
 public:
     AutomaticRegisterHelper(rtl::OUString const& _sDLLName, GetOpt & _aOptions/* , JobList * _pJobList = NULL */ );
@@ -61,6 +57,8 @@ public:
 
     /// @return true, if the given DLLName library could load and initialised.
     bool isOkToStartTests() const {return m_bLoadLibraryOK;}
+
+    virtual ~AutomaticRegisterHelper();
 };
 
 #endif
--- cppunit/inc/testshl/dynamicregister.hxx
+++ cppunit/inc/testshl/dynamicregister.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
  *  OpenOffice.org - a multi-platform office productivity suite
  *
  *  $RCSfile: dynamicregister.hxx,v $
+ * $Revision: 1.2 $
  *
- *  $Revision: 1.1 $
+ * This file is part of OpenOffice.org.
  *
- *  last change: $Author: lla $ $Date: 2008-02-27 16:20:22 $
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
- *
- *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
+ * OpenOffice.org is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
@@ -37,10 +32,7 @@
 #define DYNAMICREGISTER_HXX
 
 #include <rtl/ustring.hxx>
-
-#ifndef _OSL_MODULE_HXX_
 #include <osl/module.hxx>
-#endif
 
 #include "getopt.hxx"
 #include "cppunit/nocopy.hxx"
@@ -54,9 +46,21 @@ protected:
     rtl::OUString m_suDLLName;
     GetOpt & m_aOptions;
 
+    GetOpt& getOptions() {return m_aOptions;}
+
 public:
     DynamicLibraryHelper(rtl::OUString const& _sDLLName, GetOpt & _aOptions);
     virtual ~DynamicLibraryHelper();
+
+private:
+    void showFilenameIfVerbose();
+    void realLoadLibrary(rtl::OUString const& _sLibToLoad);
+    void loadLibraryFromAbsolutePath();
+    void loadLibraryFromLocalPath();
+
+    rtl::OUString m_suAbsolutePathFile;  // file:///D:/foo/bar/library.dll
+    rtl::OUString m_suAbsolutePath;      // file:///D:/foo/bar
+    rtl::OUString m_suFilename;          // library.dll
 };
 
 #endif
