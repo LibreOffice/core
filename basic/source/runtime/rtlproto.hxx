848c6b2e5981fc6a90ed7947c15d98b342217a27
Rüdiger Timm <rt@openoffice.org>
2008-07-07 10:59:56 +0000

    INTEGRATION: CWS ab53 (1.18.24); FILE MERGED 2008/06/25 09:45:26 tkr 1.18.24.1: #i91017# add CreateInstanceWithArguments methode

--- basic/source/runtime/rtlproto.hxx
+++ basic/source/runtime/rtlproto.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: rtlproto.hxx,v $
- * $Revision: 1.19 $
+ * $Revision: 1.20 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -326,6 +326,7 @@ extern RTLFUNC(GetPathSeparator);
 extern RTLFUNC(ResolvePath);
 extern RTLFUNC(CreateUnoStruct);
 extern RTLFUNC(CreateUnoService);
+extern RTLFUNC(CreateUnoServiceWithArguments);
 extern RTLFUNC(CreateUnoValue);
 extern RTLFUNC(GetProcessServiceManager);
 extern RTLFUNC(GetDefaultContext);
