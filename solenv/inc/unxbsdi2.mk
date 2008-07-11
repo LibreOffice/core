3e688ca5757f0a9f385578185d847d567981c1bc
Rüdiger Timm <rt@openoffice.org>
2008-07-11 13:43:37 +0000

    INTEGRATION: CWS jsc21 (1.15.30); FILE MERGED 2008/06/11 14:44:02 sb 1.15.30.3: RESYNC: (1.16-1.17); FILE MERGED 2008/04/23 09:50:42 jsc 1.15.30.2: RESYNC: (1.15-1.16); FILE MERGED 2008/04/11 12:20:15 jsc 1.15.30.1: #i86121# new rpath option for SDK related tools

--- solenv/inc/unxbsdi2.mk
+++ solenv/inc/unxbsdi2.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: unxbsdi2.mk,v $
 #
-# $Revision: 1.17 $
+# $Revision: 1.18 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -123,6 +123,7 @@ LINKFLAGSRUNPATH_URELIB=-Wl,-rpath,\''$$ORIGIN'\'
 LINKFLAGSRUNPATH_UREBIN=-Wl,-rpath,\''$$ORIGIN/../lib:$$ORIGIN'\'
     #TODO: drop $ORIGIN once no URE executable is also shipped in OOo
 LINKFLAGSRUNPATH_OOO=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../ure-link/lib'\'
+LINKFLAGSRUNPATH_SDK=-Wl,-rpath,\''$$ORIGIN/../../ure-link/lib'\'
 LINKFLAGSRUNPATH_BRAND=-Wl,-rpath,\''$$ORIGIN:$$ORIGIN/../basis-link/program:$$ORIGIN/../basis-link/ure-link/lib'\'
 LINKFLAGSRUNPATH_OXT=
 LINKFLAGSRUNPATH_NONE=
