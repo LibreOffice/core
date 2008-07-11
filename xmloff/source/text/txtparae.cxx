405c6e66ec5b72ef1cebb0baf0c78affe69f6580
Rüdiger Timm <rt@openoffice.org>
2008-07-11 13:08:04 +0000

    INTEGRATION: CWS sw30bf08 (1.148.16); FILE MERGED 2008/07/09 06:49:54 od 1.148.16.2: RESYNC: (1.148-1.149); FILE MERGED 2008/07/02 12:01:37 od 1.148.16.1: #i91295# method <XMLTextParagraphExport::exportListChange(..)> 	 - do not export list element property text:continue-numbering="true" 	   in case that ODF 1.0/1.1 is exported and a list can only continue 	   another list using ODF 1.2 features.

--- xmloff/source/text/txtparae.cxx
+++ xmloff/source/text/txtparae.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: txtparae.cxx,v $
- * $Revision: 1.149 $
+ * $Revision: 1.150 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -717,13 +717,6 @@ void XMLTextParagraphExport::exportListChange(
                                 GetExport().AddAttribute( XML_NAMESPACE_TEXT,
                                                           XML_CONTINUE_LIST,
                                                           sContinueListId );
-                            }
-                            else if ( sListStyleName ==
-                                      mpTextListsHelper->GetListStyleOfProcessedList( sListId ) )
-                            {
-                                GetExport().AddAttribute( XML_NAMESPACE_TEXT,
-                                                          XML_CONTINUE_NUMBERING,
-                                                          XML_TRUE );
                             }
 
                             if ( rNextInfo.IsRestart() &&
