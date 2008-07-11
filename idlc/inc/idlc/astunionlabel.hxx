f6c0995f9df74ff0162f27d9bbefc599bfa7600b
Rüdiger Timm <rt@openoffice.org>
2008-07-11 13:47:49 +0000

    INTEGRATION: CWS jsc21 (1.3.4); FILE MERGED 2008/06/20 11:12:52 jsc 1.3.4.1: #i86349# remove unused methods

--- idlc/inc/idlc/astunionlabel.hxx
+++ idlc/inc/idlc/astunionlabel.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: astunionlabel.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -39,7 +39,6 @@ enum UnionLabel
 class AstUnionLabel
 {
 public:
-    AstUnionLabel();
     AstUnionLabel(UnionLabel labelKind, AstExpression* pExpr);
     virtual ~AstUnionLabel();
 
