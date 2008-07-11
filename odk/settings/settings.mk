fc0ca57f2cd649c6330171445a06b80e2143a0e9
Rüdiger Timm <rt@openoffice.org>
2008-07-11 13:36:35 +0000

    INTEGRATION: CWS jsc21 (1.28.2); FILE MERGED 2008/07/07 13:54:10 jsc 1.28.2.9: #i88797# adapt base link flag 2008/06/27 08:49:55 jsc 1.28.2.8: #i90032# adapt link flags and linking for MacOS 2008/06/16 11:22:13 jsc 1.28.2.7: #i88797# adapt solaris compiler options 2008/06/11 14:38:53 sb 1.28.2.6: RESYNC: (1.28-1.29); FILE MERGED 2008/05/27 17:21:19 jsc 1.28.2.5: #i88797# adapt for new structure 2008/05/23 14:34:18 jsc 1.28.2.4: #i88797# adapted for mac 2008/05/23 11:38:05 jsc 1.28.2.3: #i88797# adapted for mac 2008/05/22 07:07:57 jsc 1.28.2.2: #i88797# adapted to new structure 2008/02/11 11:30:29 jsc 1.28.2.1: #i85448# apply patch

--- odk/settings/settings.mk
+++ odk/settings/settings.mk
@@ -26,6 +26,7 @@ ifeq "$(DEBUG)" "yes"
 JAVAC_FLAGS+=-g
 endif
 
+JAVABIN=bin
 
 ###########################################################################
 #
@@ -77,19 +78,19 @@ EMPTYSTRING=
 PATH_SEPARATOR=;
 
 # use this for release version
-CC_FLAGS=-c -MT -Zm500 -wd4251 -wd4275 -wd4290 -wd4675 -wd4786 -wd4800 -Zc:forScope -GR
+CC_FLAGS=-c -MT -Zm500 -Zc:forScope,wchar_t- -wd4251 -wd4275 -wd4290 -wd4675 -wd4786 -wd4800 -Zc:forScope -GR -EHa
 ifeq "$(CPP_VC8)" "true"
-CC_FLAGS+=-EHa -Zc:wchar_t-
+#CC_FLAGS+=-EHa -Zc:wchar_t-
 LINK_MANIFEST_VC8_ONLY=mt -manifest $@.manifest "-outputresource:$@;2"
 else
-CC_FLAGS+=-GX
+#CC_FLAGS+=
 LINK_MANIFEST_VC8_ONLY=
 endif
 ifeq "$(DEBUG)" "yes"
 CC_FLAGS+=-Zi
 endif
 
-CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I"$(OO_SDK_HOME)/include/stl"
 SDK_JAVA_INCLUDES = -I"$(OO_SDK_JAVA_HOME)/include" -I"$(OO_SDK_JAVA_HOME)/include/win32"
 
@@ -101,7 +102,7 @@ CC_OUTPUT_SWITCH=-Fo
 
 LIBRARY_LINK_FLAGS=/NODEFAULTLIB /DLL /DEBUGTYPE:cv
 COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) /DEF:$(PRJ)/settings/component.uno.def
-EXE_LINK_FLAGS=/MAP /OPT:NOREF /SUBSYSTEM:CONSOLE /BASE:0x1100000 /DEBUGTYPE:cv
+EXE_LINK_FLAGS=/MAP /OPT:NOREF /SUBSYSTEM:CONSOLE /BASE:0x1b000000 /DEBUGTYPE:cv /NODEFAULTLIB  msvcrt.lib kernel32.lib
 ifeq "$(DEBUG)" "yes"
 LIBRARY_LINK_FLAGS+=/DEBUG
 EXE_LINK_FLAGS+=/DEBUG
@@ -186,7 +187,7 @@ CC_FLAGS=-c -KPIC
 ifeq "$(DEBUG)" "yes"
 CC_FLAGS+=-g
 endif
-CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I"$(OO_SDK_HOME)/include/stl"
 SDK_JAVA_INCLUDES = -I"$(OO_SDK_JAVA_HOME)/include" -I"$(OO_SDK_JAVA_HOME)/include/solaris"
 
@@ -203,7 +204,8 @@ LIBRARY_LINK_FLAGS+=-instances=static
 endif
 COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -M $(PRJ)/settings/component.uno.map
 
-LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
+EXE_LINK_FLAGS=-w -mt -z combreloc -PIC -temp=/tmp -norunpath -Bdirect -z defs
+LINK_LIBS=-L$(OUT)/lib -L$(OO_SDK_OUT)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
 LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)"
 
 ifneq "$(OO_SDK_URE_HOME)" ""
@@ -324,7 +326,7 @@ CC_FLAGS+=-fPIC
 endif
 
 SDK_JAVA_INCLUDES = -I"$(OO_SDK_JAVA_HOME)/include" -I"$(OO_SDK_JAVA_HOME)/include/linux"
-CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I"$(OO_SDK_HOME)/include/stl"
 CC_DEFINES=-DUNX -DGCC -DLINUX -DCPPU_ENV=$(CPPU_ENV) -DGXX_INCLUDE_PATH=$(SDK_GXX_INCLUDE_PATH)
 
@@ -334,15 +336,16 @@ CC_DEFINES=-DUNX -DGCC -DLINUX -DCPPU_ENV=$(CPPU_ENV) -DGXX_INCLUDE_PATH=$(SDK_G
 
 CC_OUTPUT_SWITCH=-o
 
-LIBRARY_LINK_FLAGS=-shared -Wl,-rpath,$$ORIGIN
+LIBRARY_LINK_FLAGS=-shared '-Wl,-rpath,$$ORIGIN'
 
 ifeq "$(PROCTYPE)" "ppc"
 LIBRARY_LINK_FLAGS+=-fPIC
 endif
 COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -Wl,--version-script,$(PRJ)/settings/component.uno.map
 
-EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs -Wl,--whole-archive -lsalcpprt -Wl,--no-whole-archive
-LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
+#EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs -Wl,--whole-archive -lsalcpprt -Wl,--no-whole-archive
+EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs -Wl,--no-whole-archive
+LINK_LIBS=-L$(OUT)/lib -L$(OO_SDK_OUT)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
 LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)"
 
 ifneq "$(OO_SDK_URE_HOME)" ""
@@ -357,13 +360,23 @@ endif
 #
 ###########################################################################
 ifneq (,$(findstring darwin,$(PLATFORM)))
-# Settings for MacOSX using gcc 3.3 compiler
+# Settings for MacOSX using gcc 4.0.1 compiler
 
-# Default is MacOSX on a ppc machine    
+PROCTYPE := $(shell $(PRJ)/config.guess | cut -d"-" -f1)
+
+# Default is MacOSX on a Intel machine    
 PLATFORM=macosx
+
+ifeq "$(PROCTYPE)" "i686"
+PACKAGE_LIB_DIR=macosx_x86.plt
+UNOPKG_PLATFORM=MacOSX_x86
+JAVA_PROC_TYPE=x86
+else
 PACKAGE_LIB_DIR=macosx_ppc.plt
 UNOPKG_PLATFORM=MacOSX_PowerPC
 JAVA_PROC_TYPE=ppc
+endif
+JAVABIN=Commands
 
 OS=MACOSX
 PS=/
@@ -405,11 +418,17 @@ CPPUHELPERLIB=-luno_cppuhelper$(COMID)
 SALHELPERLIB=-luno_salhelper$(COMID)
 REGLIB=-lreg
 STORELIB=-lstore
-ifeq "$(STLPORT_VER)" "500"
-STLPORTLIB=-lstlport -lstdc++
-else
-STLPORTLIB=-lstlport_gcc$(STLDEBUG) -lstdc++
-endif
+
+SALDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libuno_sal.dylib.3:$(OO_SDK_URE_LIB_DIR)/libuno_sal.dylib
+CPPUDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libuno_cppu.dylib.3:$(OO_SDK_URE_LIB_DIR)/libuno_cppu.dylib
+CPPUHELPERDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libuno_cppuhelper$(COMID).dylib.3:$(OO_SDK_URE_LIB_DIR)/libuno_cppuhelper$(COMID).dylib
+SALHELPERDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libuno_salhelper$(COMID).dylib.3:$(OO_SDK_URE_LIB_DIR)/libuno_salhelper$(COMID).dylib
+REGDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libreg.dylib.3:$(OO_SDK_URE_LIB_DIR)/libreg.dylib
+STOREDYLIB=-Wl,-dylib_file,@__________________________________________________URELIB/libstore.dylib.3:$(OO_SDK_URE_LIB_DIR)/libstore.dylib
+
+INSTALL_NAME_URELIBS=install_name_tool -change @__________________________________________________URELIB/libuno_sal.dylib.3 @executable_path/urelibs/libuno_sal.dylib.3 -change  @__________________________________________________URELIB/libuno_cppu.dylib.3 @executable_path/urelibs/libuno_cppu.dylib.3 -change @__________________________________________________URELIB/libuno_cppuhelper$(COMID).dylib.3 @executable_path/urelibs/libuno_cppuhelper$(COMID).dylib.3 -change @__________________________________________________URELIB/libuno_salhelper$(COMID).dylib.3 @executable_path/urelibs/libuno_salhelper$(COMID).dylib.3 -change @__________________________________________________URELIB/libreg.dylib.3 @executable_path/urelibs/libreg.dylib.3 -change @__________________________________________________URELIB/libstore.dylib.3 @executable_path/urelibs/libstore.dylib.3
+
+INSTALL_NAME_URELIBS_BIN=install_name_tool -change @__________________________________________________URELIB/libuno_sal.dylib.3 libuno_sal.dylib.3 -change  @__________________________________________________URELIB/libuno_cppu.dylib.3 libuno_cppu.dylib.3 -change @__________________________________________________URELIB/libuno_cppuhelper$(COMID).dylib.3 libuno_cppuhelper$(COMID).dylib.3 -change @__________________________________________________URELIB/libuno_salhelper$(COMID).dylib.3 libuno_salhelper$(COMID).dylib.3 -change @__________________________________________________URELIB/libreg.dylib.3 libreg.dylib.3 -change @__________________________________________________URELIB/libstore.dylib.3 libstore.dylib.3
 
 EMPTYSTRING=
 PATH_SEPARATOR=:
@@ -422,21 +441,22 @@ CC_FLAGS=-malign-natural -c -O -fPIC -fno-common
 endif
 
 SDK_JAVA_INCLUDES = -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -I/System/Library/Frameworks/JavaVM.framework/Headers
-CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I"$(OO_SDK_HOME)/include/stl"
 CC_DEFINES=-DUNX -DGCC -DMACOSX -DCPPU_ENV=$(CPPU_ENV) -DGXX_INCLUDE_PATH=$(SDK_GXX_INCLUDE_PATH)
 
 CC_OUTPUT_SWITCH=-o
 
-LIBRARY_LINK_FLAGS=-dynamiclib -single_module -fPIC -fno-common
+LIBRARY_LINK_FLAGS=-dynamiclib -single_module -Wl,-multiply_defined,suppress
+#-fPIC -fno-common
 
 # install_name '@executable_path$/(@:f)'
-COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) 
-#-Wl,--version-script,$(PRJ)/settings/component.uno.map
+COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS)  -Wl,-exported_symbols_list $(COMP_MAPFILE)
 
 #EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs
-LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
+LINK_LIBS=-L$(OUT)/lib -L$(OO_SDK_OUT)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)"
 LINK_JAVA_LIBS=-framework JavaVM
+#LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/Libraries"
 
 ifneq "$(OO_SDK_URE_HOME)" ""
 URE_MISC=$(OO_SDK_URE_HOME)/share/misc
@@ -522,7 +542,7 @@ CC_FLAGS=-c -O -fPIC -DPIC $(PTHREAD_CFLAGS)
 endif
 
 SDK_JAVA_INCLUDES = -I"$(OO_SDK_JAVA_HOME)/include" -I"$(OO_SDK_JAVA_HOME)/include/freebsd"
-CC_INCLUDES=-I. -I$(OUT)/inc/examples -I$(PRJ)/include
+CC_INCLUDES=-I. -I$(OUT)/inc -I$(OUT)/inc/examples -I$(PRJ)/include
 STL_INCLUDES=-I"$(OO_SDK_HOME)/include/stl"
 CC_DEFINES=-DUNX -DGCC -DFREEBSD -DCPPU_ENV=$(CPPU_ENV) -DGXX_INCLUDE_PATH=$(SDK_GXX_INCLUDE_PATH)
 
@@ -533,7 +553,7 @@ COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS) -Wl,--version-script,$(PRJ)/settings/compo
 
 EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined 
 #EXE_LINK_FLAGS+=-Wl,-export-dynamic -Wl,-z,defs
-LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)" $(PTHREAD_LIBS)
+LINK_LIBS=-L$(OUT)/lib -L$(OO_SDK_OUT)/$(PLATFORM)/lib -L"$(OO_SDK_URE_LIB_DIR)" $(PTHREAD_LIBS)
 LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)"
 
 ifneq "$(OO_SDK_URE_HOME)" ""
