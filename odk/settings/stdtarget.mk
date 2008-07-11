dcf0a2fee68e00fd22208b060725bf180ce614fb
Rüdiger Timm <rt@openoffice.org>
2008-07-11 13:37:13 +0000

    INTEGRATION: CWS jsc21 (1.3.242); FILE MERGED 2008/05/22 07:07:58 jsc 1.3.242.1: #i88797# adapted to new structure

--- odk/settings/stdtarget.mk
+++ odk/settings/stdtarget.mk
@@ -8,3 +8,9 @@ endif
 
 .PHONY: install
 install: $(REGISTERFLAG)
+
+$(SDKTYPEFLAG):  #$(URE_TYPES) $(OFFICE_TYPES)
+    -$(MKDIR) $(subst /,$(PS),$(@D))
+    -$(DEL) $(subst \\,\,$(subst /,$(PS),$(SDKTYPEFLAG)))
+    $(CPPUMAKER) -Gc -BUCR -O$(OUT_INC) $(URE_TYPES) $(OFFICE_TYPES)
+    echo flagged > $@
