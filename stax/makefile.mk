776eefd997ec145a7daa0f7b321c3ee619764894
Hans-Joachim Lankenau <hjs@openoffice.org>
2008-07-14 14:35:25 +0000

    #i91376# - prerequisite to build saxon

---
+++ stax/makefile.mk
@@ -0,0 +1,67 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.1 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+PRJ=.
+
+PRJNAME=stax
+TARGET=stax
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+.INCLUDE : antsettings.mk
+
+.IF "$(SOLAR_JAVA)" != ""
+.IF "$(JAVANUMVER:s/.//)" >= "000100060000" 
+all:
+    @echo "Your java version already contains StAX"
+.ENDIF			# "$(JAVANUMVER:s/.//)" >= "000100060000" 
+.IF "$(SYSTEM_SAXON)" == "YES"
+all:
+    @echo "An already available installation of saxon should exist on your system."
+    @echo "Therefore the files provided here does not need to be built in addition."
+.ENDIF
+
+# --- Files --------------------------------------------------------
+TARFILE_NAME=stax-api-1.0-2-sources
+TARFILE_ROOTDIR=src
+
+BUILD_ACTION=$(JAVAC) javax$/xml$/stream$/*.java && jar -cf jsr173_1.0_api.jar javax$/xml$/stream$/*.class javax$/xml$/stream$/events$/*.class javax$/xml$/stream$/util$/*.class
+
+OUT2CLASS=stax-api-1.0.jar
+
+.ENDIF			# $(SOLAR_JAVA)!= ""
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+.INCLUDE : tg_ext.mk
+
