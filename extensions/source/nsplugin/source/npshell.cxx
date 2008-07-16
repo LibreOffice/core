687ca31a1efbaae0b66d2fb006fecd37a800698a
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-16 19:13:01 +0000

    INTEGRATION: CWS cmcfixes47 (1.17.32); FILE MERGED 2008/07/14 08:11:36 cmc 1.17.32.2: RESYNC: (1.17-1.18); FILE MERGED 2008/06/06 14:44:47 cmc 1.17.32.1: #i82545# fix firefox plugin on 64bit platforms

--- extensions/source/nsplugin/source/npshell.cxx
+++ extensions/source/nsplugin/source/npshell.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: npshell.cxx,v $
- * $Revision: 1.18 $
+ * $Revision: 1.19 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -76,6 +76,27 @@
 #include <errno.h>
 #include "boost/scoped_array.hpp"
 #include "tools/pathutils.hxx"
+
+//http://qa.openoffice.org/issues/show_bug.cgi?id=82545
+//https://bugzilla.mozilla.org/show_bug.cgi?id=241262
+#ifdef UNIX
+#  ifndef _UINT32
+#    if defined(__alpha) || defined(__LP64__)
+       typedef unsigned int uint32;
+#    else  /* __alpha */
+       typedef unsigned long uint32;
+#    endif
+#    define _UINT32
+#  endif
+#  ifndef _INT32
+#    if defined(__alpha) || defined(__LP64__)
+       typedef int int32;
+#    else  /* __alpha */
+       typedef long int32;
+#    endif
+#    define _INT32
+#  endif
+#endif
 
 #include "npapi.h"
 #include "npshell.hxx"
