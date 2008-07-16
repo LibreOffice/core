cf21ceb37ceb55b511c0d98292bc3d514044630a
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-16 12:27:56 +0000

    INTEGRATION: CWS tl55 (1.1.2); FILE ADDED 2008/06/25 12:13:37 ama 1.1.2.2: Fix #i90237#: Sentence start/end in grammar check list 2008/06/25 11:24:46 ama 1.1.2.1: Fix #i90237#: Sentence start/end in grammar check list

---
+++ sw/source/core/text/SwGrammarMarkUp.cxx
@@ -0,0 +1,142 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: SwGrammarMarkUp.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
+#include "SwGrammarMarkUp.hxx"
+
+void SwGrammarMarkUp::MoveGrammar( xub_StrLen nPos, long nDiff )
+{
+    Move( nPos, nDiff );
+    if( !maSentence.size() )
+        return;
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter < nPos )
+        ++pIter;
+    xub_StrLen nEnd = nDiff < 0 ? xub_StrLen(nPos - nDiff) : nPos;
+    while( pIter != maSentence.end() )
+    {
+        if( *pIter >= nEnd )
+            *pIter = xub_StrLen( *pIter + nDiff );
+        else
+            *pIter = nPos;
+        ++pIter;
+    }
+}
+
+SwGrammarMarkUp* SwGrammarMarkUp::SplitGrammarList( xub_StrLen nSplitPos )
+{
+    SwGrammarMarkUp* pNew = (SwGrammarMarkUp*)SplitList( nSplitPos );
+    if( !maSentence.size() )
+        return pNew;
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter < nSplitPos )
+        ++pIter;
+    if( pIter != maSentence.begin() )
+    {
+        pNew->maSentence.insert( pNew->maSentence.begin(), maSentence.begin(), pIter );
+        maSentence.erase( maSentence.begin(), pIter );
+    }
+    return pNew;
+}
+
+void SwGrammarMarkUp::JoinGrammarList( SwGrammarMarkUp* pNext, xub_StrLen nInsertPos )
+{
+    JoinList( pNext, nInsertPos );
+    if( !pNext->maSentence.size() )
+        return;
+    std::vector< xub_StrLen >::iterator pIter = pNext->maSentence.begin();
+    while( pIter != pNext->maSentence.end() )
+    {
+        *pIter = *pIter + nInsertPos;
+        ++pIter;
+    }
+    maSentence.insert( maSentence.end(), pNext->maSentence.begin(), pNext->maSentence.end() );
+}
+
+void SwGrammarMarkUp::ClearGrammarList()
+{
+    ClearList();
+    maSentence.clear();
+}
+
+void SwGrammarMarkUp::setSentence( xub_StrLen nStart )
+{
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter < nStart )
+        ++pIter;
+    if( pIter == maSentence.end() || *pIter > nStart )
+        maSentence.insert( pIter, nStart );
+}
+
+void SwGrammarMarkUp::removeSentence(xub_StrLen nStart, xub_StrLen nLength )
+{
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter < nStart )
+        ++pIter;
+    if( nLength == STRING_LEN )
+        nStart = STRING_LEN;
+    else
+        nStart += nLength;
+    std::vector< xub_StrLen >::iterator pLast = pIter;
+    while( pLast != maSentence.end() && *pLast < nStart )
+        ++pLast;
+    maSentence.erase( pIter, pLast );
+}
+
+xub_StrLen SwGrammarMarkUp::getSentenceStart( xub_StrLen nPos )
+{
+    if( !maSentence.size() )
+        return 0;
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter < nPos )
+        ++pIter;
+    if( pIter != maSentence.begin() )
+        --pIter;
+    xub_StrLen nRet = 0;
+    if( pIter != maSentence.end() && *pIter < nPos )
+        nRet = *pIter;
+    return nRet;
+}
+
+xub_StrLen SwGrammarMarkUp::getSentenceEnd( xub_StrLen nPos )
+{
+    if( !maSentence.size() )
+        return STRING_LEN;
+    std::vector< xub_StrLen >::iterator pIter = maSentence.begin();
+    while( pIter != maSentence.end() && *pIter <= nPos )
+        ++pIter;
+    xub_StrLen nRet = STRING_LEN;
+    if( pIter != maSentence.end() )
+        nRet = *pIter;
+    return nRet;
+}
+
