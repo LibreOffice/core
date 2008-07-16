5c60da14c1ffa0656e7d21920bc4faeb72068765
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-16 12:30:05 +0000

    INTEGRATION: CWS tl55 (1.1.2); FILE ADDED 2008/07/02 07:11:34 tl 1.1.2.5: #i85999# grammar checking framework 2008/06/25 10:51:26 ama 1.1.2.4: Fix #i90237#: Sentence start/end in grammar check list 2008/06/19 13:37:57 ama 1.1.2.3: Fix GrammarChecking during editing 2008/06/17 06:40:23 ama 1.1.2.2: Fix #i90238#: Don't create superfluous lists 2008/06/13 12:25:22 ama 1.1.2.1: Fix #i90238#: Delayed grammar check in active paragraphs

---
+++ sw/source/core/txtnode/SwGrammarContact.cxx
@@ -0,0 +1,213 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: SwGrammarContact.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sw.hxx"
+
+#include <vcl/timer.hxx>
+#include <hints.hxx>
+#include <IGrammarContact.hxx>
+#include <pam.hxx>
+#include <ndtxt.hxx>
+#include <SwGrammarMarkUp.hxx>
+#include <txtfrm.hxx>
+#include <rootfrm.hxx>
+#include <viewsh.hxx>
+
+/* SwGrammarContact
+    This class is responsible for the delayed display of grammar checks when a paragraph is edited
+    It's a client of the paragraph the cursor points to.
+    If the cursor position changes, updateCursorPosition has to be called
+    If the grammar checker wants to set a grammar marker at a paragraph, he has to request
+    the grammar list from this class. If the requested paragraph is not edited, it returns
+    the normal grammar list. But if the paragraph is the active one, a proxy list will be returned and
+    all changes are set in this proxy list. If the cursor leaves the paragraph the proxy list
+    will replace the old list. If the grammar checker has completed the paragraph ('setChecked')
+    then a timer is setup which replaces the old list as well.
+*/
+
+class SwGrammarContact : public IGrammarContact, public SwClient
+{
+    Timer aTimer;
+    SwGrammarMarkUp *mpProxyList;
+    bool mbFinished;
+    SwTxtNode* getMyTxtNode() { return (SwTxtNode*)pRegisteredIn; }
+    void repaintTextFrames( SwModify& rModify ) const;
+      DECL_LINK( TimerRepaint, Timer * );
+
+public:
+    SwGrammarContact();
+    ~SwGrammarContact() { aTimer.Stop(); delete mpProxyList; }
+
+    // (pure) virtual functions of IGrammarContact
+    virtual void updateCursorPosition( const SwPosition& rNewPos );
+    virtual SwGrammarMarkUp* getGrammarCheck( SwTxtNode& rTxtNode, bool bCreate );
+    virtual void finishGrammarCheck( SwTxtNode& rTxtNode );
+
+    // virtual function of SwClient
+    virtual void Modify( SfxPoolItem *pOld, SfxPoolItem *pNew);
+};
+
+SwGrammarContact::SwGrammarContact() : mpProxyList(0), mbFinished( false )
+{
+    aTimer.SetTimeout( 2000 );  // Repaint of grammar check after 'setChecked'
+    aTimer.SetTimeoutHdl( LINK(this, SwGrammarContact, TimerRepaint) );
+}
+
+IMPL_LINK( SwGrammarContact, TimerRepaint, Timer *, pTimer )
+{
+    if( pTimer )
+    {
+        pTimer->Stop();
+        if( pRegisteredIn )
+        {   //Replace the old wrong list by the proxy list and repaint all frames
+            getMyTxtNode()->SetGrammarCheck( mpProxyList, true );
+            mpProxyList = 0;
+            repaintTextFrames( *pRegisteredIn );
+        }
+    }
+    return 0;
+}
+
+/* repaint all text frames of the given text node */
+void SwGrammarContact::repaintTextFrames( SwModify& rModify ) const
+{
+    SwClientIter aIter( rModify );
+    for( const SwTxtFrm *pFrm = (const SwTxtFrm*)aIter.First( TYPE(SwTxtFrm) );
+         pFrm; pFrm = (const SwTxtFrm*)aIter.Next() )
+    {
+        SwRect aRec( pFrm->PaintArea() );
+        const SwRootFrm *pRootFrm = pFrm->FindRootFrm();
+        ViewShell *pCurShell = pRootFrm ? pRootFrm->GetCurrShell() : NULL;
+        if( pCurShell )
+            pCurShell->InvalidateWindows( aRec );
+    }
+}
+
+/* I'm always a client of the current paragraph */
+void SwGrammarContact::updateCursorPosition( const SwPosition& rNewPos )
+{
+    SwTxtNode* pTxtNode = rNewPos.nNode.GetNode().GetTxtNode();
+    if( pTxtNode != GetRegisteredIn() ) // paragraph has been changed
+    {
+        aTimer.Stop();
+        if( GetRegisteredIn() ) // My last paragraph has been left
+        {
+            if( mpProxyList )
+            {   // replace old list by the proxy list and repaint
+                getMyTxtNode()->SetGrammarCheck( mpProxyList, true );
+                repaintTextFrames( *pRegisteredIn );
+            }
+            pRegisteredIn->Remove( this ); // good bye old paragraph
+            mpProxyList = 0;
+        }
+        if( pTxtNode )
+            pTxtNode->Add( this ); // welcome new paragraph
+    }
+}
+
+/* deliver a grammar check list for the given text node */
+SwGrammarMarkUp* SwGrammarContact::getGrammarCheck( SwTxtNode& rTxtNode, bool bCreate )
+{
+    SwGrammarMarkUp *pRet = 0;
+    if( GetRegisteredIn() == &rTxtNode ) // hey, that's my current paragraph!
+    {   // so you will get a proxy list...
+        if( bCreate )
+        {
+            if( mbFinished )
+            {
+                delete mpProxyList;
+                mpProxyList = 0;
+            }
+            if( !mpProxyList )
+                mpProxyList = new SwGrammarMarkUp();
+           mbFinished = false;
+        }
+        pRet = mpProxyList;
+    }
+    else
+    {
+        pRet = rTxtNode.GetGrammarCheck(); // do you have already a list?
+        if( bCreate && !pRet ) // do you want to create a list?
+        {
+            pRet = new SwGrammarMarkUp();
+            rTxtNode.SetGrammarCheck( pRet );
+            rTxtNode.SetGrammarCheckDirty( true );
+        }
+    }
+    return pRet;
+}
+
+void SwGrammarContact::Modify( SfxPoolItem *pOld, SfxPoolItem * )
+{
+    if( !pOld || pOld->Which() != RES_OBJECTDYING )
+        return;
+
+    SwPtrMsgPoolItem *pDead = (SwPtrMsgPoolItem *)pOld;
+    if( pDead->pObject == pRegisteredIn )
+    {    // if my current paragraph dies, I throw the proxy list away
+        aTimer.Stop();
+        pRegisteredIn->Remove( this );
+        delete mpProxyList;
+        mpProxyList = 0;
+    }
+}
+
+void SwGrammarContact::finishGrammarCheck( SwTxtNode& rTxtNode )
+{
+    if( &rTxtNode != pRegisteredIn ) // not my paragraph
+        repaintTextFrames( rTxtNode ); // can be repainted directly
+    else
+    {
+        if( mpProxyList )
+        {
+            mbFinished = true;
+            aTimer.Start(); // will replace old list and repaint with delay
+        }
+        else if( getMyTxtNode()->GetGrammarCheck() )
+        {   // all grammar problems seems to be gone, no delay needed
+            getMyTxtNode()->SetGrammarCheck( 0, true );
+            repaintTextFrames( *pRegisteredIn );
+        }
+    }
+}
+
+IGrammarContact* createGrammarContact()
+{
+    return new SwGrammarContact();
+}
+
+void finishGrammarCheck( SwTxtNode& rTxtNode )
+{
+    IGrammarContact* pGrammarContact = getGrammarContact( rTxtNode );
+    if( pGrammarContact )
+        pGrammarContact->finishGrammarCheck( rTxtNode );
+}
+
