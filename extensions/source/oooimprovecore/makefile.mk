be81c70c82cba8e744a5dc4eaa17e3e55a4827bb
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-17 14:14:59 +0000

    INTEGRATION: CWS logger2 (1.1.2); FILE ADDED 2008/07/03 08:57:54 b_michaelsen 1.1.2.4: #i88653# added extension description, showing options page when invitation was rejected, fixed hang on invite job 2008/06/27 16:02:23 b_michaelsen 1.1.2.3: #i88653# ui moved into the core 2008/06/13 16:14:07 b_michaelsen 1.1.2.2: #i88653# makefile 2008/06/11 10:46:22 b_michaelsen 1.1.2.1: #i88653# liboooimprovecore implements the uno interface for the core components (com.sun.star.oooimprovecore.core)

---
+++ extensions/source/oooimprovecore/makefile.mk
@@ -0,0 +1,75 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.2 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+PRJ=..$/..
+PRJNAME=extensions
+TARGET=oooimprovecore
+
+# --- Settings ----------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files -------------------------------------
+
+EXCEPTIONSFILES= \
+        $(SLO)$/core.obj \
+        $(SLO)$/oooimprovecore_services.obj \
+
+SLOFILES= \
+        $(EXCEPTIONSFILES) \
+        $(SLO)$/oooimprovecore_module.obj \
+
+LIB1TARGET=$(SLB)$/$(TARGET)_t.lib
+LIB1FILES= \
+        $(SLB)$/$(TARGET).lib \
+
+SHL1TARGET=$(TARGET)$(DLLPOSTFIX)
+
+SHL1STDLIBS= \
+        $(COMPHELPERLIB) \
+        $(CPPUHELPERLIB) \
+        $(CPPULIB) \
+        $(SALLIB) \
+        $(SVXLIB) \
+        $(VCLLIB) \
+        $(SFXLIB) \
+        $(TOOLSLIB) \
+
+
+SHL1LIBS=$(LIB1TARGET)
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+SHL1VERSIONMAP=$(TARGET).map
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
+
