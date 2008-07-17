3afda7f56876a32b231ce7562912b614406a0d1c
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-17 14:24:15 +0000

    INTEGRATION: CWS logger2 (1.16.28); FILE MERGED 2008/05/13 14:43:40 b_michaelsen 1.16.28.1: merged logger cws

--- framework/source/uielement/makefile.mk
+++ framework/source/uielement/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.16 $
+# $Revision: 1.17 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -30,61 +30,61 @@
 #*************************************************************************
 PRJ=..$/..
 
-PRJNAME=			framework
-TARGET=				fwk_uielement
-USE_DEFFILE=		TRUE
-ENABLE_EXCEPTIONS=	TRUE
+PRJNAME=            framework
+TARGET=             fwk_uielement
+USE_DEFFILE=        TRUE
+ENABLE_EXCEPTIONS=  TRUE
 
 # --- Settings -----------------------------------------------------
 
-.INCLUDE :  		settings.mk
+.INCLUDE :          settings.mk
 
 # --- Generate -----------------------------------------------------
 
-SLOFILES=			\
-                    $(SLO)$/menubarwrapper.obj              \
-                    $(SLO)$/menubarmanager.obj              \
-                    $(SLO)$/fontmenucontroller.obj          \
-                    $(SLO)$/fontsizemenucontroller.obj      \
-                    $(SLO)$/objectmenucontroller.obj        \
-                    $(SLO)$/headermenucontroller.obj        \
-                    $(SLO)$/footermenucontroller.obj        \
-                    $(SLO)$/controlmenucontroller.obj       \
-                    $(SLO)$/macrosmenucontroller.obj   		\
-                    $(SLO)$/uicommanddescription.obj        \
-                    $(SLO)$/itemcontainer.obj               \
-                    $(SLO)$/constitemcontainer.obj          \
-                    $(SLO)$/rootitemcontainer.obj           \
-                    $(SLO)$/toolbarwrapper.obj              \
-                    $(SLO)$/toolbar.obj                     \
-                    $(SLO)$/toolbarmanager.obj              \
-                    $(SLO)$/addonstoolbarwrapper.obj        \
-                    $(SLO)$/addonstoolbarmanager.obj        \
-                    $(SLO)$/generictoolbarcontroller.obj    \
-                    $(SLO)$/toolbarsmenucontroller.obj      \
-                    $(SLO)$/statusbarwrapper.obj            \
-                    $(SLO)$/statusbar.obj                   \
-                    $(SLO)$/statusbarmanager.obj			\
-                    $(SLO)$/recentfilesmenucontroller.obj   \
-                    $(SLO)$/progressbarwrapper.obj          \
-                    $(SLO)$/statusindicatorinterfacewrapper.obj \
-                    $(SLO)$/logoimagestatusbarcontroller.obj \
-                    $(SLO)$/logotextstatusbarcontroller.obj \
-                    $(SLO)$/newmenucontroller.obj           \
-                    $(SLO)$/simpletextstatusbarcontroller.obj\
-                    $(SLO)$/complextoolbarcontroller.obj     \
-                    $(SLO)$/comboboxtoolbarcontroller.obj    \
-                    $(SLO)$/imagebuttontoolbarcontroller.obj \
-                    $(SLO)$/togglebuttontoolbarcontroller.obj \
-                    $(SLO)$/buttontoolbarcontroller.obj       \
-                    $(SLO)$/spinfieldtoolbarcontroller.obj    \
-                    $(SLO)$/edittoolbarcontroller.obj         \
-                    $(SLO)$/dropdownboxtoolbarcontroller.obj \
-                    $(SLO)$/menubarmerger.obj \
-                    $(SLO)$/toolbarmerger.obj \
+SLOFILES=           \
+                    $(SLO)$/addonstoolbarmanager.obj            \
+                    $(SLO)$/addonstoolbarwrapper.obj            \
+                    $(SLO)$/buttontoolbarcontroller.obj         \
+                    $(SLO)$/comboboxtoolbarcontroller.obj       \
+                    $(SLO)$/complextoolbarcontroller.obj        \
+                    $(SLO)$/constitemcontainer.obj              \
+                    $(SLO)$/controlmenucontroller.obj           \
+                    $(SLO)$/dropdownboxtoolbarcontroller.obj    \
+                    $(SLO)$/edittoolbarcontroller.obj           \
+                    $(SLO)$/fontmenucontroller.obj              \
+                    $(SLO)$/fontsizemenucontroller.obj          \
+                    $(SLO)$/footermenucontroller.obj            \
+                    $(SLO)$/generictoolbarcontroller.obj        \
+                    $(SLO)$/headermenucontroller.obj            \
+                    $(SLO)$/imagebuttontoolbarcontroller.obj    \
+                    $(SLO)$/itemcontainer.obj                   \
+                    $(SLO)$/langselectionmenucontroller.obj     \
                     $(SLO)$/langselectionstatusbarcontroller.obj \
-                    $(SLO)$/langselectionmenucontroller.obj
+                    $(SLO)$/logoimagestatusbarcontroller.obj    \
+                    $(SLO)$/logotextstatusbarcontroller.obj     \
+                    $(SLO)$/macrosmenucontroller.obj            \
+                    $(SLO)$/menubarmanager.obj                  \
+                    $(SLO)$/menubarmerger.obj                   \
+                    $(SLO)$/menubarwrapper.obj                  \
+                    $(SLO)$/newmenucontroller.obj               \
+                    $(SLO)$/objectmenucontroller.obj            \
+                    $(SLO)$/progressbarwrapper.obj              \
+                    $(SLO)$/recentfilesmenucontroller.obj       \
+                    $(SLO)$/rootitemcontainer.obj               \
+                    $(SLO)$/simpletextstatusbarcontroller.obj   \
+                    $(SLO)$/spinfieldtoolbarcontroller.obj      \
+                    $(SLO)$/statusbar.obj                       \
+                    $(SLO)$/statusbarmanager.obj                \
+                    $(SLO)$/statusbarwrapper.obj                \
+                    $(SLO)$/statusindicatorinterfacewrapper.obj \
+                    $(SLO)$/togglebuttontoolbarcontroller.obj   \
+                    $(SLO)$/toolbar.obj                         \
+                    $(SLO)$/toolbarmanager.obj                  \
+                    $(SLO)$/toolbarmerger.obj                   \
+                    $(SLO)$/toolbarsmenucontroller.obj          \
+                    $(SLO)$/toolbarwrapper.obj                  \
+                    $(SLO)$/uicommanddescription.obj            \
 
 # --- Targets ------------------------------------------------------
 
-.INCLUDE :			target.mk
+.INCLUDE :          target.mk
