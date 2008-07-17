7ea16187a79e6e600066210851f16e138a882e40
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-17 14:33:27 +0000

    INTEGRATION: CWS logger2 (1.1.4); FILE ADDED 2008/07/10 08:15:26 b_michaelsen 1.1.4.6: #i88653# fixed extra qualification on member error on gcc4 2008/06/17 12:45:58 b_michaelsen 1.1.4.5: #i88653# enabling by configuration and extension 2008/06/06 09:47:22 b_michaelsen 1.1.4.4: #i88653# eventcount and discarding inconvenient exceptions 2008/05/19 11:24:46 b_michaelsen 1.1.4.3: #i88653# logrotation 2008/05/13 14:42:49 b_michaelsen 1.1.4.2: merged logger cws 2008/04/30 10:35:14 b_michaelsen 1.1.4.1: file uieventslogger.hxx was added on branch cws_dev300_logger2 on 2008-05-13 14:42:49 +0000

---
+++ comphelper/inc/comphelper/uieventslogger.hxx
@@ -0,0 +1,61 @@
+/*************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: uieventslogger.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ ************************************************************************/
+
+#ifndef _COMPHELPER_UIEVENTSLOGGER_HXX
+#define _COMPHELPER_UIEVENTSLOGGER_HXX
+
+#include <boost/shared_ptr.hpp>
+#include <com/sun/star/uno/Sequence.hxx>
+#include <com/sun/star/beans/PropertyValue.hpp>
+#include <com/sun/star/logging/XCsvLogFormatter.hpp>
+#include <com/sun/star/logging/XLogger.hpp>
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/util/URL.hpp>
+#include <comphelper/comphelperdllapi.h>
+#include <rtl/ustring.hxx>
+
+
+namespace comphelper
+{
+    // this class is part of the OOo Improvement Program Core
+    class COMPHELPER_DLLPUBLIC UiEventsLogger
+    {
+        public:
+            static sal_Bool isEnabled();
+            static sal_Int32 getSessionLogEventCount();
+            static void appendDispatchOrigin( ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue>& args, const ::rtl::OUString& origin);
+            static ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue> purgeDispatchOrigin(const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue>& args);
+            static void logDispatch(const ::com::sun::star::util::URL& url, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue>& args);
+            static void logVcl(const ::rtl::OUString& parent_id, sal_Int32 window_type, const ::rtl::OUString& id, const ::rtl::OUString& method, const ::rtl::OUString& param);
+            static void logVcl(const ::rtl::OUString& parent_id, sal_Int32 window_type, const ::rtl::OUString& id, const ::rtl::OUString& method, sal_Int32 param);
+            static void logVcl(const ::rtl::OUString& parent_id, sal_Int32 window_type, const ::rtl::OUString& id, const ::rtl::OUString& method);
+            virtual ~UiEventsLogger() {}
+    };
+}
+#endif
