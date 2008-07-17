ec705f77dc5ee2d0491d234ed68a64ff3eb2e301
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-17 14:28:27 +0000

    INTEGRATION: CWS logger2 (1.5.96); FILE MERGED 2008/06/30 10:56:32 pb 1.5.96.1: fix: #i90370# new class FixedHyperlinkImage

--- svtools/inc/svtools/fixedhyper.hxx
+++ svtools/inc/svtools/fixedhyper.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: fixedhyper.hxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -135,6 +135,97 @@ namespace svt
         virtual void        SetDescription( const String& rNewDescription );
     };
 
+    //=====================================================================
+    //= FixedHyperlinkImage
+    //=====================================================================
+    class SVT_DLLPUBLIC FixedHyperlinkImage : public FixedImage
+    {
+    private:
+        Pointer             m_aOldPointer;
+        Link                m_aClickHdl;
+        String              m_sURL;
+
+        /** initializes the font (link color and underline).
+
+            Called by the Ctors.
+        */
+        void                Initialize();
+
+    protected:
+        /** overwrites Window::MouseMove().
+
+            Changes the pointer only over the text.
+        */
+        virtual void        MouseMove( const MouseEvent& rMEvt );
+
+        /** overwrites Window::MouseButtonUp().
+
+            Calls the set link if the mouse is over the text.
+        */
+        virtual void        MouseButtonUp( const MouseEvent& rMEvt );
+
+        /** overwrites Window::RequestHelp().
+
+            Shows tooltip only if the mouse is over the text.
+        */
+        virtual void        RequestHelp( const HelpEvent& rHEvt );
+
+    public:
+        /** ctors
+
+            With ResId or WinBits.
+        */
+        FixedHyperlinkImage( Window* pParent, const ResId& rId );
+        FixedHyperlinkImage( Window* pParent, WinBits nWinStyle = 0 );
+
+        /** dtor
+
+        */
+        virtual ~FixedHyperlinkImage();
+
+        /** overwrites Window::GetFocus().
+
+            Changes the color of the text and shows a focus rectangle.
+        */
+        virtual void        GetFocus();
+
+        /** overwrites Window::LoseFocus().
+
+            Changes the color of the text and hides the focus rectangle.
+        */
+        virtual void        LoseFocus();
+
+        /** overwrites Window::KeyInput().
+
+            KEY_RETURN and KEY_SPACE calls the link handler.
+        */
+        virtual void        KeyInput( const KeyEvent& rKEvt );
+
+        /** sets <member>m_aClickHdl</member> with <arg>rLink</arg>.
+
+            <member>m_aClickHdl</member> is called if the text is clicked.
+        */
+        inline void         SetClickHdl( const Link& rLink ) { m_aClickHdl = rLink; }
+
+        /** returns <member>m_aClickHdl</member>.
+
+            @return
+                <member>m_aClickHdl</member>
+        */
+        inline const Link&  GetClickHdl() const { return m_aClickHdl; }
+
+        // ::toolkit::FixedHyperbaseLink
+
+        /** sets the URL of the hyperlink and uses it as tooltip. */
+        virtual void        SetURL( const String& rNewURL );
+
+        /** returns the URL of the hyperlink.
+
+            @return
+                <member>m_sURL</member>
+        */
+        virtual String      GetURL() const;
+    };
 //.........................................................................
 } // namespace svt
 //.........................................................................
