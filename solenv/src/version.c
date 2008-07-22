6877f31298da822a8123c45e77e51b46fa10b58b
Oliver Bolte <obo@openoffice.org>
2008-07-22 15:52:47 +0000

    INTEGRATION: CWS os2port03 (1.7.630); FILE MERGED 2008/07/16 12:20:22 obr 1.7.630.2: RESYNC: (1.7-1.8); FILE MERGED 2008/01/14 17:25:27 ydario 1.7.630.1: define version also for os2. Issue number:i85203 Submitted by:ydario

--- solenv/src/version.c
+++ solenv/src/version.c
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: version.c,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -53,11 +53,11 @@ static const struct VersionInfo g_aVersionInfo =
     _INPATH
 };
 
-#ifdef WNT
+#if defined(WNT) || defined(OS2)
 __declspec(dllexport) const struct VersionInfo* GetVersionInfo(void);
 #endif
 
-#ifdef WNT
+#if defined(WNT) || defined(OS2)
 __declspec(dllexport) const struct VersionInfo* GetVersionInfo(void)
 #else
 const struct VersionInfo *GetVersionInfo(void)
