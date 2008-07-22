389a7f0d6e2d7fe53c0c53246f7c1c614a508fa2
Oliver Bolte <obo@openoffice.org>
2008-07-22 12:34:18 +0000

    INTEGRATION: CWS xmlfilter06 (1.3.6); FILE MERGED 2008/06/25 14:58:38 dr 1.3.6.2: import chart data point labels and trendline label 2008/05/27 10:40:34 dr 1.3.6.1: joined changes from CWS xmlfilter05

--- oox/source/core/fragmenthandler2.cxx
+++ oox/source/core/fragmenthandler2.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: fragmenthandler2.cxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -42,8 +42,9 @@ namespace core {
 
 // ============================================================================
 
-FragmentHandler2::FragmentHandler2( XmlFilterBase& rFilter, const OUString& rFragmentPath ) :
-    FragmentHandler( rFilter, rFragmentPath )
+FragmentHandler2::FragmentHandler2( XmlFilterBase& rFilter, const OUString& rFragmentPath, bool bEnableTrimSpace ) :
+    FragmentHandler( rFilter, rFragmentPath ),
+    ContextHandler2Helper( bEnableTrimSpace )
 {
 }
 
