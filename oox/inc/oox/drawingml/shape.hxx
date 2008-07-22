46edd3c6c014de24edb8ff1bc9f5731b515b24fe
Oliver Bolte <obo@openoffice.org>
2008-07-22 12:08:44 +0000

    INTEGRATION: CWS xmlfilter06 (1.5.6); FILE MERGED 2008/06/30 14:50:17 dr 1.5.6.8: #i10000# resync additions 2008/06/30 09:56:42 dr 1.5.6.7: RESYNC: (1.5-1.6); FILE MERGED 2008/06/23 12:55:43 dr 1.5.6.6: final chart font formatting 2008/06/12 15:01:20 dr 1.5.6.5: chart formatting, some more changes 2008/06/10 10:50:23 dr 1.5.6.4: chart area formatting, first steps 2008/06/09 15:16:11 dr 1.5.6.3: more chart line formatting, add line dashs to chart dash container 2008/06/06 16:27:51 dr 1.5.6.2: chart formatting: builtin line/fill/effect styles, manual line formatting 2008/05/27 10:40:27 dr 1.5.6.1: joined changes from CWS xmlfilter05

--- oox/inc/oox/drawingml/shape.hxx
+++ oox/inc/oox/drawingml/shape.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: shape.hxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,10 +33,7 @@
 
 #include "oox/helper/propertymap.hxx"
 #include "oox/drawingml/drawingmltypes.hxx"
-#include "oox/drawingml/lineproperties.hxx"
-#include "oox/drawingml/fillproperties.hxx"
 #include "oox/drawingml/customshapeproperties.hxx"
-#include "oox/drawingml/table/tableproperties.hxx"
 #include "oox/drawingml/textliststyle.hxx"
 
 #include <com/sun/star/frame/XModel.hpp>
@@ -50,15 +47,13 @@ namespace oox { namespace drawingml {
 
 typedef ::std::map< ::rtl::OUString, ShapePtr > ShapeIdMap;
 
-enum ShapeStyle
+struct ShapeStyleRef
 {
-    SHAPESTYLE_ln,
-    SHAPESTYLE_fill,
-    SHAPESTYLE_effect,
-    SHAPESTYLE_font
+    Color               maPhClr;
+    sal_Int32           mnThemedIdx;
 };
-typedef std::map< ShapeStyle, ColorPtr > ShapeStylesColorMap;
-typedef std::map< ShapeStyle, rtl::OUString > ShapeStylesIndexMap;
+
+typedef ::std::map< sal_Int32, ShapeStyleRef > ShapeStyleRefMap;
 
 // ============================================================================
 
@@ -88,30 +83,35 @@ public:
     Shape( const sal_Char* pServiceType = NULL );
     virtual ~Shape();
 
-    rtl::OUString&                  getServiceName(){ return msServiceName; };
+    rtl::OUString&                  getServiceName(){ return msServiceName; }
     void                            setServiceName( const sal_Char* pServiceName );
-    PropertyMap&                    getShapeProperties(){ return maShapeProperties; };
-    LinePropertiesPtr               getLineProperties(){ return mpLinePropertiesPtr; };
-    FillPropertiesPtr               getFillProperties(){ return mpFillPropertiesPtr; };
-    FillPropertiesPtr               getGraphicProperties() { return mpGraphicPropertiesPtr; };
-    CustomShapePropertiesPtr        getCustomShapeProperties(){ return mpCustomShapePropertiesPtr; };
-    table::TablePropertiesPtr       getTableProperties(){ if ( !mpTablePropertiesPtr.get() )
-                                                            mpTablePropertiesPtr.reset( new table::TableProperties() );
-                                                        return mpTablePropertiesPtr; };
 
-    void                            setPosition( com::sun::star::awt::Point nPosition ){ maPosition = nPosition; };
-    void                            setSize( com::sun::star::awt::Size aSize ){ maSize = aSize; };
-    void                            setRotation( sal_Int32 nRotation ) { mnRotation = nRotation; };
-    void                            setFlip( sal_Bool bFlipH, sal_Bool bFlipV ) { mbFlipH = bFlipH; mbFlipV = bFlipV; };
-    void                            addChild( const ShapePtr pChildPtr ) { maChilds.push_back( pChildPtr ); };
-    std::vector< ShapePtr >&        getChilds() { return maChilds; };
+    PropertyMap&                    getShapeProperties(){ return maShapeProperties; }
 
-    void                            setName( const rtl::OUString& rName ) { msName = rName; };
+    inline LineProperties&          getLineProperties() { return *mpLinePropertiesPtr; }
+    inline const LineProperties&    getLineProperties() const { return *mpLinePropertiesPtr; }
+
+    inline FillProperties&          getFillProperties() { return *mpFillPropertiesPtr; }
+    inline const FillProperties&    getFillProperties() const { return *mpFillPropertiesPtr; }
+
+    FillPropertiesPtr               getGraphicProperties() { return mpGraphicPropertiesPtr; }
+    CustomShapePropertiesPtr        getCustomShapeProperties(){ return mpCustomShapePropertiesPtr; }
+
+    table::TablePropertiesPtr       getTableProperties();
+
+    void                            setPosition( com::sun::star::awt::Point nPosition ){ maPosition = nPosition; }
+    void                            setSize( com::sun::star::awt::Size aSize ){ maSize = aSize; }
+    void                            setRotation( sal_Int32 nRotation ) { mnRotation = nRotation; }
+    void                            setFlip( sal_Bool bFlipH, sal_Bool bFlipV ) { mbFlipH = bFlipH; mbFlipV = bFlipV; }
+    void                            addChild( const ShapePtr pChildPtr ) { maChildren.push_back( pChildPtr ); }
+    std::vector< ShapePtr >&        getChildren() { return maChildren; }
+
+    void                            setName( const rtl::OUString& rName ) { msName = rName; }
     ::rtl::OUString                 getName( ) { return msName; }
-    void                            setId( const rtl::OUString& rId ) { msId = rId; };
-    void                            setSubType( sal_uInt32 nSubType ) { mnSubType = nSubType; };
-    sal_Int32                       getSubType() const { return mnSubType; };
-    void                            setIndex( sal_uInt32 nIndex ) { mnIndex = nIndex; };
+    void                            setId( const rtl::OUString& rId ) { msId = rId; }
+    void                            setSubType( sal_uInt32 nSubType ) { mnSubType = nSubType; }
+    sal_Int32                       getSubType() const { return mnSubType; }
+    void                            setIndex( sal_uInt32 nIndex ) { mnIndex = nIndex; }
 
     // setDefaults has to be called if styles are imported (OfficeXML is not storing properties having the default value)
     void                            setDefaults();
@@ -119,10 +119,11 @@ public:
     void                setTextBody(const TextBodyPtr & pTextBody);
     TextBodyPtr         getTextBody();
     void                setMasterTextListStyle( const TextListStylePtr& pMasterTextListStyle );
-    TextListStylePtr    getMasterTextListStyle() const { return mpMasterTextListStyle; };
+    TextListStylePtr    getMasterTextListStyle() const { return mpMasterTextListStyle; }
 
-    ShapeStylesColorMap&    getShapeStylesColor(){ return maShapeStylesColorMap; }
-    ShapeStylesIndexMap&    getShapeStylesIndex(){ return maShapeStylesIndexMap; }
+    inline ShapeStyleRefMap&        getShapeStyleRefs() { return maShapeStyleRefs; }
+    inline const ShapeStyleRefMap&  getShapeStyleRefs() const { return maShapeStyleRefs; }
+    const ShapeStyleRef*            getShapeStyleRef( sal_Int32 nRefType ) const;
 
     inline void         setCreateShapeCallback( CreateShapeCallbackRef xCallback ) { mxCreateCallback = xCallback; }
 
@@ -151,7 +152,7 @@ protected:
                             const ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShapes >& rxShapes,
                             const ::com::sun::star::awt::Rectangle* pShapeRect );
 
-    void                addChilds(
+    void                addChildren(
                             const ::oox::core::XmlFilterBase& rFilterBase,
                             Shape& rMaster,
                             const ThemePtr& rxTheme,
@@ -159,7 +160,7 @@ protected:
                             const ::com::sun::star::awt::Rectangle& rClientRect,
                             ShapeIdMap* pShapeMap );
 
-    std::vector< ShapePtr >     maChilds;               // only used for group shapes
+    std::vector< ShapePtr >     maChildren;               // only used for group shapes
     TextBodyPtr                 mpTextBody;
     LinePropertiesPtr           mpLinePropertiesPtr;
     FillPropertiesPtr           mpFillPropertiesPtr;
@@ -171,23 +172,17 @@ protected:
     ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShape > mxShape;
 
     rtl::OUString   msServiceName;
-    rtl::OUString   msName;
-    rtl::OUString   msId;
+    rtl::OUString    msName;
+    rtl::OUString    msId;
     sal_uInt32      mnSubType;      // if this type is not zero, then the shape is a placeholder
     sal_uInt32      mnIndex;
 
-    ShapeStylesColorMap maShapeStylesColorMap;
-    ShapeStylesIndexMap maShapeStylesIndexMap;
+    ShapeStyleRefMap   maShapeStyleRefs;
 
     com::sun::star::awt::Size       maSize;
     com::sun::star::awt::Point      maPosition;
 
 private:
-
-    void setShapeStyles( const ThemePtr& rxTheme, LineProperties& rLineProperties, FillProperties& rFillProperties );
-    void setShapeStyleColors( const ::oox::core::XmlFilterBase& rFilterBase,
-            LineProperties& rLineProperties, FillProperties& rFillProperties, PropertyMap& rShapeProperties );
-
     CreateShapeCallbackRef          mxCreateCallback;
     sal_Int32                       mnRotation;
     sal_Bool                        mbFlipH;
