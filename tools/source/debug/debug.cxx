f0ac8d2a9ac0da8a8b1e9e90470670e03040f0db
Oliver Bolte <obo@openoffice.org>
2008-07-22 16:38:53 +0000

    INTEGRATION: CWS os2port03 (1.15.24); FILE MERGED 2008/07/16 13:45:54 obr 1.15.24.2: RESYNC: (1.15-1.16); FILE MERGED 2008/04/15 14:41:14 ydario 1.15.24.1: Issue number:  i85203 Submitted by: ydario Reviewed by: ydario

--- tools/source/debug/debug.cxx
+++ tools/source/debug/debug.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: debug.cxx,v $
- * $Revision: 1.16 $
+ * $Revision: 1.17 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -271,7 +271,7 @@ void ImplDbgUnlock()
 
 // =======================================================================
 
-#if defined WNT && !defined SVX_LIGHT
+#if (defined WNT || defined OS2) && !defined SVX_LIGHT
 //#define SV_MEMMGR //
 #endif
 #ifdef SV_MEMMGR
