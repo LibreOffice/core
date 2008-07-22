a1a4cb74ec50da276b88497b08291c99a9c0f738
Oliver Bolte <obo@openoffice.org>
2008-07-22 12:34:44 +0000

    INTEGRATION: CWS xmlfilter06 (1.1.2); FILE ADDED 2008/06/26 14:15:57 dr 1.1.2.1: handle drawing object tables separated for chart import, extended line formatting with own property name sets

---
+++ oox/source/core/modelobjectcontainer.cxx
@@ -0,0 +1,100 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: modelobjectcontainer.cxx,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "oox/core/modelobjectcontainer.hxx"
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <com/sun/star/awt/Gradient.hpp>
+#include <com/sun/star/awt/XBitmap.hpp>
+#include <com/sun/star/drawing/LineDash.hpp>
+#include <com/sun/star/drawing/PolyPolygonBezierCoords.hpp>
+#include "oox/helper/helper.hxx"
+
+using ::rtl::OUString;
+using ::com::sun::star::uno::Any;
+using ::com::sun::star::uno::Reference;
+using ::com::sun::star::uno::UNO_QUERY;
+using ::com::sun::star::lang::XMultiServiceFactory;
+using ::com::sun::star::frame::XModel;
+using ::com::sun::star::awt::Gradient;
+using ::com::sun::star::awt::XBitmap;
+using ::com::sun::star::drawing::LineDash;
+using ::com::sun::star::drawing::PolyPolygonBezierCoords;
+
+namespace oox {
+namespace core {
+
+// ============================================================================
+
+ModelObjectContainer::ModelObjectContainer( const Reference< XModel >& rxModel ) :
+    maMarkerContainer(   Reference< XMultiServiceFactory >( rxModel, UNO_QUERY ), CREATE_OUSTRING( "com.sun.star.drawing.MarkerTable" ) ),
+    maDashContainer(     Reference< XMultiServiceFactory >( rxModel, UNO_QUERY ), CREATE_OUSTRING( "com.sun.star.drawing.DashTable" ) ),
+    maGradientContainer( Reference< XMultiServiceFactory >( rxModel, UNO_QUERY ), CREATE_OUSTRING( "com.sun.star.drawing.GradientTable" ) ),
+    maBitmapContainer(   Reference< XMultiServiceFactory >( rxModel, UNO_QUERY ), CREATE_OUSTRING( "com.sun.star.drawing.BitmapTable" ) ),
+    maDashNameBase( CREATE_OUSTRING( "msLineDash " ) ),
+    maGradientNameBase( CREATE_OUSTRING( "msFillGradient " ) ),
+    maBitmapNameBase( CREATE_OUSTRING( "msFillBitmap " ) )
+{
+}
+
+bool ModelObjectContainer::hasLineMarker( const OUString& rMarkerName ) const
+{
+    return maMarkerContainer.hasObject( rMarkerName );
+}
+
+bool ModelObjectContainer::insertLineMarker( const OUString& rMarkerName, const PolyPolygonBezierCoords& rMarker )
+{
+    OSL_ENSURE( rMarker.Coordinates.hasElements(), "ModelObjectContainer::insertLineMarker - line marker without coordinates" );
+    if( rMarker.Coordinates.hasElements() )
+        return maMarkerContainer.insertObject( rMarkerName, Any( rMarker ), false ).getLength() > 0;
+    return false;
+}
+
+OUString ModelObjectContainer::insertLineDash( const LineDash& rDash )
+{
+    return maDashContainer.insertObject( maDashNameBase, Any( rDash ), true );
+}
+
+OUString ModelObjectContainer::insertFillGradient( const Gradient& rGradient )
+{
+    return maGradientContainer.insertObject( maGradientNameBase, Any( rGradient ), true );
+}
+
+OUString ModelObjectContainer::insertFillBitmap( const Reference< XBitmap >& rxBitmap )
+{
+    if( rxBitmap.is() )
+        return maBitmapContainer.insertObject( maBitmapNameBase, Any( rxBitmap ), true );
+    return OUString();
+}
+
+// ============================================================================
+
+} // namespace core
+} // namespace oox
+
