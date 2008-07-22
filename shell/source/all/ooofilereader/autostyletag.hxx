d031d6abe8ef190d6c3e40670f4a6ea75652d682
Oliver Bolte <obo@openoffice.org>
2008-07-22 16:47:38 +0000

    INTEGRATION: CWS os2port03 (1.4.18); FILE MERGED 2008/07/17 05:25:41 obr 1.4.18.2: RESYNC: (1.4-1.5); FILE MERGED 2008/04/15 14:32:19 ydario 1.4.18.1: Issue number:  i85203 Submitted by: ydario Reviewed by: ydario

--- shell/source/all/ooofilereader/autostyletag.hxx
+++ shell/source/all/ooofilereader/autostyletag.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: autostyletag.hxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
