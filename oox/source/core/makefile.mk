3c0d223fe2f3050538154556a484f445844ffaaa
Oliver Bolte <obo@openoffice.org>
2008-07-22 12:34:30 +0000

    INTEGRATION: CWS xmlfilter06 (1.4.6); FILE MERGED 2008/06/26 14:15:57 dr 1.4.6.2: handle drawing object tables separated for chart import, extended line formatting with own property name sets 2008/05/27 10:40:34 dr 1.4.6.1: joined changes from CWS xmlfilter05

--- oox/source/core/makefile.mk
+++ oox/source/core/makefile.mk
@@ -1,14 +1,14 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -54,6 +54,7 @@ SLOFILES =	\
         $(SLO)$/filterdetect.obj			\
         $(SLO)$/fragmenthandler.obj			\
         $(SLO)$/fragmenthandler2.obj		\
+        $(SLO)$/modelobjectcontainer.obj	\
         $(SLO)$/recordparser.obj			\
         $(SLO)$/relations.obj				\
         $(SLO)$/relationshandler.obj		\
