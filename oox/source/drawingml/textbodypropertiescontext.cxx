917628a1060e528369379eb88301a8a8babb8ac3
Oliver Bolte <obo@openoffice.org>
2008-07-22 12:42:18 +0000

    INTEGRATION: CWS xmlfilter06 (1.5.6); FILE MERGED 2008/06/20 11:58:16 dr 1.5.6.2: line/fill/character properties rework; first steps of chart text formatting and rotation import; make line arrow import work 2008/05/27 10:40:35 dr 1.5.6.1: joined changes from CWS xmlfilter05

--- oox/source/drawingml/textbodypropertiescontext.cxx
+++ oox/source/drawingml/textbodypropertiescontext.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: textbodypropertiescontext.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,6 +31,7 @@
 #include "oox/drawingml/textbodypropertiescontext.hxx"
 
 #include <com/sun/star/text/ControlCharacter.hpp>
+#include "oox/drawingml/textbodyproperties.hxx"
 #include "oox/drawingml/drawingmltypes.hxx"
 #include "oox/helper/attributelist.hxx"
 #include "oox/helper/propertymap.hxx"
@@ -49,16 +50,15 @@ namespace oox { namespace drawingml {
 
 // CT_TextBodyProperties
 TextBodyPropertiesContext::TextBodyPropertiesContext( ContextHandler& rParent,
-    const Reference< XFastAttributeList >& xAttributes, PropertyMap& rTextBodyProp )
+    const Reference< XFastAttributeList >& xAttributes, TextBodyProperties& rTextBodyProp )
 : ContextHandler( rParent )
 , mrTextBodyProp( rTextBodyProp )
 {
-    AttributeList attribs(xAttributes);
+    AttributeList aAttribs( xAttributes );
 
     // ST_TextWrappingType
-    sal_Int32 nWrappingType = xAttributes->getOptionalValueToken( XML_wrap, XML_square );
-    const OUString sTextWordWrap( RTL_CONSTASCII_USTRINGPARAM( "TextWordWrap" ) );
-    mrTextBodyProp[ sTextWordWrap ] <<= (nWrappingType == XML_square);
+    sal_Int32 nWrappingType = aAttribs.getToken( XML_wrap, XML_square );
+    mrTextBodyProp.maPropertyMap[ CREATE_OUSTRING( "TextWordWrap" ) ] <<= (nWrappingType == XML_square);
 
     // ST_Coordinate
     const OUString sTextLeftDistance( RTL_CONSTASCII_USTRINGPARAM( "TextLeftDistance" ) );
@@ -68,29 +68,29 @@ TextBodyPropertiesContext::TextBodyPropertiesContext( ContextHandler& rParent,
     OUString sValue;
     sValue = xAttributes->getOptionalValue( XML_lIns );
     sal_Int32 nLeftInset = ( sValue.getLength() != 0 ? GetCoordinate(  sValue ) : 91440 / 360 );
-    mrTextBodyProp[ sTextLeftDistance ]  <<= static_cast< sal_Int32 >( nLeftInset );
+    mrTextBodyProp.maPropertyMap[ sTextLeftDistance ]  <<= static_cast< sal_Int32 >( nLeftInset );
 
     sValue = xAttributes->getOptionalValue( XML_tIns );
     sal_Int32 nTopInset  = ( sValue.getLength() != 0 ? GetCoordinate(  sValue ) : 91440 / 360 );
-    mrTextBodyProp[ sTextUpperDistance ] <<= static_cast< sal_Int32 >( nTopInset );
+    mrTextBodyProp.maPropertyMap[ sTextUpperDistance ] <<= static_cast< sal_Int32 >( nTopInset );
 
     sValue = xAttributes->getOptionalValue( XML_rIns );
     sal_Int32 nRightInset  = ( sValue.getLength() != 0 ? GetCoordinate(  sValue ) : 91440 / 360 );
-    mrTextBodyProp[ sTextRightDistance ] <<= static_cast< sal_Int32 >( nRightInset );
+    mrTextBodyProp.maPropertyMap[ sTextRightDistance ] <<= static_cast< sal_Int32 >( nRightInset );
 
     sValue = xAttributes->getOptionalValue( XML_bIns );
-    sal_Int32 nBottonInset = ( sValue.getLength() != 0 ? GetCoordinate(  sValue ) : 45720 / 360 );;
-    mrTextBodyProp[ sTextLowerDistance ] <<= static_cast< sal_Int32 >( nBottonInset );
+    sal_Int32 nBottonInset = ( sValue.getLength() != 0 ? GetCoordinate(  sValue ) : 45720 / 360 );
+    mrTextBodyProp.maPropertyMap[ sTextLowerDistance ] <<= static_cast< sal_Int32 >( nBottonInset );
 
 
     // ST_TextAnchoringType
 //   sal_Int32 nAnchoringType = xAttributes->getOptionalValueToken( XML_anchor, XML_t );
 
-//   bool bAnchorCenter = attribs.getBool( XML_anchorCtr, false );
+//   bool bAnchorCenter = aAttribs.getBool( XML_anchorCtr, false );
 
-//   bool bCompatLineSpacing = attribs.getBool( XML_compatLnSpc, false );
-//   bool bForceAA = attribs.getBool( XML_forceAA, false );
-//   bool bFromWordArt = attribs.getBool( XML_fromWordArt, false );
+//   bool bCompatLineSpacing = aAttribs.getBool( XML_compatLnSpc, false );
+//   bool bForceAA = aAttribs.getBool( XML_forceAA, false );
+//   bool bFromWordArt = aAttribs.getBool( XML_fromWordArt, false );
 
   // ST_TextHorzOverflowType
 //   sal_Int32 nHorzOverflow = xAttributes->getOptionalValueToken( XML_horzOverflow, XML_overflow );
@@ -98,17 +98,19 @@ TextBodyPropertiesContext::TextBodyPropertiesContext( ContextHandler& rParent,
 //   sal_Int32 nVertOverflow =  xAttributes->getOptionalValueToken( XML_vertOverflow, XML_overflow );
 
     // ST_TextColumnCount
-//   sal_Int32 nNumCol = attribs.getInteger( XML_numCol, 1 );
+//   sal_Int32 nNumCol = aAttribs.getInteger( XML_numCol, 1 );
 
     // ST_Angle
-//   sal_Int32 nRot = attribs.getInteger( XML_rot, 0 );
-//   bool bRtlCol = attribs.getBool( XML_rtlCol, false );
+    mrTextBodyProp.moRotation = aAttribs.getInteger( XML_rot );
+
+//   bool bRtlCol = aAttribs.getBool( XML_rtlCol, false );
     // ST_PositiveCoordinate
-//   sal_Int32 nSpcCol = attribs.getInteger( XML_spcCol, 0 );
-//   bool bSpcFirstLastPara = attribs.getBool( XML_spcFirstLastPara, 0 );
-//   bool bUpRight = attribs.getBool( XML_upright, 0 );
+//   sal_Int32 nSpcCol = aAttribs.getInteger( XML_spcCol, 0 );
+//   bool bSpcFirstLastPara = aAttribs.getBool( XML_spcFirstLastPara, 0 );
+//   bool bUpRight = aAttribs.getBool( XML_upright, 0 );
+
     // ST_TextVerticalType
-//   sal_Int32 nVert = xAttributes->getOptionalValueToken( XML_vert, XML_horz );
+    mrTextBodyProp.moVert = aAttribs.getToken( XML_vert );
 }
 
 // --------------------------------------------------------------------
@@ -132,11 +134,11 @@ Reference< XFastContextHandler > TextBodyPropertiesContext::createFastChildConte
 
             // EG_TextAutofit
             case NMSP_DRAWINGML|XML_noAutofit:
-                mrTextBodyProp[ sTextAutoGrowHeight ] <<= sal_False;    // CT_TextNoAutofit
+                mrTextBodyProp.maPropertyMap[ sTextAutoGrowHeight ] <<= false;   // CT_TextNoAutofit
                 break;
             case NMSP_DRAWINGML|XML_normAutofit:    // CT_TextNormalAutofit
             case NMSP_DRAWINGML|XML_spAutoFit:
-                mrTextBodyProp[ sTextAutoGrowHeight ] <<= sal_True;
+                mrTextBodyProp.maPropertyMap[ sTextAutoGrowHeight ] <<= true;
                 break;
 
             case NMSP_DRAWINGML|XML_scene3d:        // CT_Scene3D
