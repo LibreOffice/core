70a769f6ec2feafa685a8b40f79c0acbed802146
Oliver Bolte <obo@openoffice.org>
2008-07-22 16:47:50 +0000

    INTEGRATION: CWS os2port03 (1.3.198); FILE MERGED 2008/07/17 05:25:44 obr 1.3.198.2: RESYNC: (1.3-1.4); FILE MERGED 2008/04/15 14:32:19 ydario 1.3.198.1: Issue number:  i85203 Submitted by: ydario Reviewed by: ydario

--- shell/source/all/ooofilereader/dummytag.hxx
+++ shell/source/all/ooofilereader/dummytag.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dummytag.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
