25d73b74582ce52afd6dc9aaa0ff21991d4a224d
Oliver Bolte <obo@openoffice.org>
2008-07-22 13:07:16 +0000

    INTEGRATION: CWS xmlfilter06 (1.4.6); FILE MERGED 2008/06/30 09:57:38 dr 1.4.6.3: RESYNC: (1.4-1.5); FILE MERGED 2008/06/06 16:27:54 dr 1.4.6.2: chart formatting: builtin line/fill/effect styles, manual line formatting 2008/05/27 10:40:42 dr 1.4.6.1: joined changes from CWS xmlfilter05

--- oox/source/shape/ShapeFilterBase.cxx
+++ oox/source/shape/ShapeFilterBase.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ShapeFilterBase.cxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -46,6 +46,11 @@ ShapeFilterBase::ShapeFilterBase
 
 ShapeFilterBase::~ShapeFilterBase()
 {
+}
+
+const ::oox::drawingml::Theme* ShapeFilterBase::getCurrentTheme() const
+{
+    return 0;
 }
 
 sal_Int32 ShapeFilterBase::getSchemeClr(sal_Int32 /*nColorSchemeToken*/ ) const
