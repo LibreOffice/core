0130234d07b14f5340a656d709d4f4aadf631c0c
Oliver Bolte <obo@openoffice.org>
2008-07-25 06:47:51 +0000

    INTEGRATION: CWS dba30g (1.1.4); FILE ADDED 2008/07/22 06:52:18 fs 1.1.4.2: copying following changes from CWS dba30h to CWS dba30g:   2008/07/21 10:08:56 fs 1.1.2.1: base class for complex test cases based on the CRMDatabase 2008/07/21 10:08:56 fs 1.1.4.1: file CRMBasedTestCase.java was added on branch cws_dev300_dba30g on 2008-07-22 06:52:18 +0000

---
+++ dbaccess/qa/complex/dbaccess/CRMBasedTestCase.java
@@ -0,0 +1,91 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: CRMBasedTestCase.java,v $
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+package complex.dbaccess;
+
+import com.sun.star.lang.XMultiServiceFactory;
+import com.sun.star.sdb.XSingleSelectQueryComposer;
+import com.sun.star.uno.UnoRuntime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class CRMBasedTestCase extends complexlib.ComplexTestCase
+{
+    protected   CRMDatabase m_database;
+
+    // --------------------------------------------------------------------------------------------------------
+    protected final XMultiServiceFactory getORB()
+    {
+        return (XMultiServiceFactory)param.getMSF();
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    protected void createTestCase()
+    {
+        try
+        {
+            m_database = new CRMDatabase( getORB() );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace( System.err );
+            assure( "caught an exception (" + e.getMessage() + ") while creating the test case", false );
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    public void before()
+    {
+        createTestCase();
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    public void after()
+    {
+        try
+        {
+            if ( m_database != null )
+                m_database.close();
+        }
+        catch ( Exception ex )
+        {
+            Logger.getLogger( this.getClass().getName() ).log( Level.SEVERE, null, ex );
+        }
+    }
+
+    /** creates a SingleSelectQueryComposer for our connection
+     */
+    protected final XSingleSelectQueryComposer createQueryComposer() throws com.sun.star.uno.Exception
+    {
+        XMultiServiceFactory connectionFactory = (XMultiServiceFactory)UnoRuntime.queryInterface(
+            XMultiServiceFactory.class, m_database.getConnection() );
+        return (XSingleSelectQueryComposer)UnoRuntime.queryInterface(
+            XSingleSelectQueryComposer.class, connectionFactory.createInstance( "com.sun.star.sdb.SingleSelectQueryComposer" ) );
+    }
+}
