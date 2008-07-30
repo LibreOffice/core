ad5e1e91092aacf80a74cf1c0ac3655141239313
Rüdiger Timm <rt@openoffice.org>
2008-07-30 13:21:26 +0000

    INTEGRATION: CWS layoutdialogs (1.2.2); FILE MERGED 2008/07/30 11:29:33 rt 1.2.2.5: Update license header. 2008/05/22 07:14:01 jcn 1.2.2.4: Putting the expatlibs after the toolslibs fixes MacOsX ppc build.
    build breaks on Mac/PPC X11 because of the linker-bug static libraries
    must appear after the libs that use symbols from them.  (aqua is not
    affected, since the added -lobjc misteriously "disables" that bug)
    
    Thanks cloph!
    2008/03/30 13:36:03 cl 1.2.2.3: build tralay by default
    2008/03/26 15:29:33 jcn 1.2.2.2: Fix makefile header thinko.
    2008/03/26 13:05:21 cl 1.2.2.1: fixing warnings

--- transex3/layout/makefile.mk
+++ transex3/layout/makefile.mk
@@ -1,3 +1,34 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.3 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
 PRJ=..
 
 INCPRE=$(MISC)
@@ -20,13 +51,7 @@ CFLAGS+=-DSYSTEM_EXPAT
 
 # --- Files --------------------------------------------------------
 
-CXXFILES=\
-    layoutparse.cxx\
-    tralay.cxx
-
 APP1TARGET=$(TARGET)
-
-.IF "$(ENABLE_LAYOUT)" == "TRUE"
 
 OBJFILES =\
     $(OBJ)/export2.obj\
@@ -39,29 +64,18 @@ OBJFILES =\
 APP1OBJS = $(OBJFILES)
 
 APP1STDLIBS =\
-    $(EXPATASCII3RDLIB)\
     $(TOOLSLIB)\
-    $(VOSLIB)
+    $(EXPATASCII3RDLIB)\
+    $(VOSLIB)\
+    $(CPPULIB) \
+    $(SALLIB)	
 
 # --- Targets ------------------------------------------------------
 
 .INCLUDE :  target.mk
 
 test .PHONY:
-    ../$(INPATH)/bin/tralay -l en-US zoom.xml > out.sdf
+    ../$(INPATH)/bin/tralay -l en-US -o out.sdf zoom.xml
     cat out.sdf > trans.sdf
     sed 's/en-US\t/de\tde:/' out.sdf >> trans.sdf 
-    ../$(INPATH)/bin/tralay -m trans.sdf -l de zoom.xml #> zoom-DE.xml
-
-.ELSE # ENABLE_LAYOUT != TRUE
-
-$(BIN)$/$(APP1TARGET)$(EXECPOST):
-    touch $@
-
-ALLTAR:
-all:
-check:
-clean:
-
-.ENDIF # ENABLE_LAYOUT != TRUE
-
+    ../$(INPATH)/bin/tralay -m trans.sdf -l de -o zoom-DE.xml zoom.xml
