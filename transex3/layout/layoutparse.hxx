8ec8a1f9afb83b8d1448deeb6d44bd87421f4803
Rüdiger Timm <rt@openoffice.org>
2008-07-30 13:21:13 +0000

    INTEGRATION: CWS layoutdialogs (1.2.2); FILE MERGED 2008/07/30 11:29:33 rt 1.2.2.3: Update license header. 2008/03/27 13:09:53 jcn 1.2.2.2: Substitute TABs with 4 spaces. 2008/03/26 13:05:21 cl 1.2.2.1: fixing warnings

--- transex3/layout/layoutparse.hxx
+++ transex3/layout/layoutparse.hxx
@@ -1,3 +1,34 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: layoutparse.hxx,v $
+ *
+ * $Revision: 1.3 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
 #ifndef LAYOUTPARSE_HXX
 #define LAYOUTPARSE_HXX
 
@@ -14,6 +45,9 @@ public:
     BOOL Write( ByteString &aFilename );
     void HandleElement( XMLElement* element );
     void InsertL10NElement( ByteString const& id, XMLElement* element );
+
+    using XMLFile::InsertL10NElement;
+    using XMLFile::Write;
 };
 
 std::vector<XMLAttribute*> interestingAttributes( XMLAttributeList* lst );
