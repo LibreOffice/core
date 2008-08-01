6771aaee9049a98ec4e1dece2304e3a214db9b47
Rüdiger Timm <rt@openoffice.org>
2008-08-01 13:25:49 +0000

    INTEGRATION: CWS rmfreetype (1.5.36); FILE MERGED 2008/06/04 04:52:53 obr 1.5.36.1: #i52026# faked RPM dependency for libfreetype

--- instsetoo_native/inc_openoffice/unix/makefile.mk
+++ instsetoo_native/inc_openoffice/unix/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -41,7 +41,8 @@ TARGET=find-requires
 # ------------------------------------------------------------------
 
 FINDREQUIRES = \
-    $(BIN)$/find-requires-gnome.sh
+    $(BIN)$/find-requires-gnome.sh \
+    $(BIN)$/find-requires-x11.sh
     
 # --- Targets ------------------------------------------------------
 
