dd43a14a40fbba04c07a1d619b7ee21fb8307897
Rüdiger Timm <rt@openoffice.org>
2008-08-01 11:13:34 +0000

    INTEGRATION: CWS helpsearch (1.20.8); FILE MERGED 2008/07/28 08:42:33 jsc 1.20.8.2: #i91750# use explicit cast to the Anaylzer interface 2008/07/23 07:29:54 jsc 1.20.8.1: #i91750# add lang commandline parameter, use CJK analyzer for ja

--- xmlhelp/source/com/sun/star/help/HelpIndexer.java
+++ xmlhelp/source/com/sun/star/help/HelpIndexer.java
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: HelpIndexer.java,v $
- * $Revision: 1.20 $
+ * $Revision: 1.21 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -27,20 +27,19 @@
  * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
-
 package com.sun.star.help;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 import java.util.zip.CRC32;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.cjk.CJKAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexWriter;
 
 import java.io.File;
@@ -48,233 +47,224 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 
+public class HelpIndexer {
 
-public class HelpIndexer
-{
-    public HelpIndexer()
-    {
+    public HelpIndexer() {
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String aDirToZipStr = "";
+        String aLanguageStr = "";
         String aModule = "";
         String aTargetZipFileStr = "";
 
         // Scan arguments
+        boolean bLang = false;
         boolean bMod = false;
         boolean bZipDir = false;
         boolean bOutput = false;
 
         int nArgCount = args.length;
-        for( int i = 0 ; i < nArgCount ; i++ )
-        {
-            if( "-mod".equals(args[i]) )
-            {
-                if( i + 1 < nArgCount ) {
-                    aModule = args[i+1];
+        for (int i = 0; i < nArgCount; i++) {
+            if ("-lang".equals(args[i])) {
+                if (i + 1 < nArgCount) {
+                    aLanguageStr = args[i + 1];
+                    bLang = true;
+                }
+                i++;
+            } else if ("-mod".equals(args[i])) {
+                if (i + 1 < nArgCount) {
+                    aModule = args[i + 1];
                     bMod = true;
                 }
                 i++;
-            }
-            else if( "-zipdir".equals(args[i]) )
-            {
-                if( i + 1 < nArgCount ) {
-                    aDirToZipStr = args[i+1];
+            } else if ("-zipdir".equals(args[i])) {
+                if (i + 1 < nArgCount) {
+                    aDirToZipStr = args[i + 1];
                     bZipDir = true;
                 }
                 i++;
-            }
-            else if( "-o".equals(args[i]) )
-            {
-                if( i + 1 < nArgCount )
-                {
-                    aTargetZipFileStr = args[i+1];
+            } else if ("-o".equals(args[i])) {
+                if (i + 1 < nArgCount) {
+                    aTargetZipFileStr = args[i + 1];
                     bOutput = true;
                 }
                 i++;
             }
         }
 
-        if( !bMod || !bZipDir || !bOutput )
-        {
-            System.out.println( "Usage: HelpIndexer -mod HelpModule -zipdir TempZipDir -o OutputZipFile" );
-            System.exit( -1 );
+        if (!bLang || !bMod || !bZipDir || !bOutput) {
+            System.out.println("Usage: HelpIndexer -lang ISOLangCode -mod HelpModule -zipdir TempZipDir -o OutputZipFile");
+            System.exit(-1);
         }
 
-        File docDir = new File( aDirToZipStr );
+        File docDir = new File(aDirToZipStr);
         String aIndexDirName = aModule + ".idxl";
-        File aIndexDir = new File( aDirToZipStr + File.separator + aIndexDirName );
-        File aCaptionFilesDir = new File( aDirToZipStr + File.separator + "caption" );
-        File aContentFilesDir = new File( aDirToZipStr + File.separator + "content" );
+        File aIndexDir = new File(aDirToZipStr + File.separator + aIndexDirName);
+        File aCaptionFilesDir = new File(aDirToZipStr + File.separator + "caption");
+        File aContentFilesDir = new File(aDirToZipStr + File.separator + "content");
 
-        try
-        {
+        try {
             Date start = new Date();
-            IndexWriter writer = new IndexWriter( aIndexDir, new StandardAnalyzer(), true );
+            Analyzer analyzer = aLanguageStr.equals("ja") ? (Analyzer)new CJKAnalyzer() : (Analyzer)new StandardAnalyzer();
+            IndexWriter writer = new IndexWriter(aIndexDir, analyzer, true);
             System.out.println("Lucene: Indexing to directory '" + aIndexDir + "'...");
-            int nRet = indexDocs( writer, aModule, aCaptionFilesDir, aContentFilesDir );
-            if( nRet != -1 )
-            {
+            int nRet = indexDocs(writer, aModule, aCaptionFilesDir, aContentFilesDir);
+            if (nRet != -1) {
                 System.out.println();
                 System.out.println("Optimizing ...");
                 writer.optimize();
             }
             writer.close();
-            if( nRet == -1 )
-                deleteRecursively( aIndexDir );
+            if (nRet == -1) {
+                deleteRecursively(aIndexDir);
+            }
 
             System.out.println("Zipping ...");
-            File aDirToZipFile = new File( aDirToZipStr );
-            createZipFile( aDirToZipFile, aTargetZipFileStr );
+            File aDirToZipFile = new File(aDirToZipStr);
+            createZipFile(aDirToZipFile, aTargetZipFileStr);
             deleteRecursively( aDirToZipFile );
 
             Date end = new Date();
             System.out.println(end.getTime() - start.getTime() + " total milliseconds");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(" caught a " + e.getClass() +
-            "\n with message: " + e.getMessage());
-            System.exit( -1 );
+                    "\n with message: " + e.getMessage());
+            System.exit(-1);
         }
     }
 
-    private static int indexDocs( IndexWriter writer, String aModule,
-        File aCaptionFilesDir, File aContentFilesDir ) throws IOException
-    {
-        if( !aCaptionFilesDir.canRead() || !aCaptionFilesDir.isDirectory() )
-        {
-            System.out.println( "Not found: " + aCaptionFilesDir );
+    private static int indexDocs(IndexWriter writer, String aModule,
+            File aCaptionFilesDir, File aContentFilesDir) throws IOException {
+        if (!aCaptionFilesDir.canRead() || !aCaptionFilesDir.isDirectory()) {
+            System.out.println("Not found: " + aCaptionFilesDir);
             return -1;
         }
-        if( !aContentFilesDir.canRead() || !aContentFilesDir.isDirectory() )
-        {
-            System.out.println( "Not found: " + aContentFilesDir );
+        if (!aContentFilesDir.canRead() || !aContentFilesDir.isDirectory()) {
+            System.out.println("Not found: " + aContentFilesDir);
             return -1;
         }
 
         String[] aCaptionFiles = aCaptionFilesDir.list();
-        List aCaptionFilesList = Arrays.asList( aCaptionFiles );
-        HashSet aCaptionFilesHashSet = new HashSet( aCaptionFilesList );
+        List aCaptionFilesList = Arrays.asList(aCaptionFiles);
+        HashSet aCaptionFilesHashSet = new HashSet(aCaptionFilesList);
 
         String[] aContentFiles = aContentFilesDir.list();
-        List aContentFilesList = Arrays.asList( aContentFiles );
-        HashSet aContentFilesHashSet = new HashSet( aContentFilesList );
+        List aContentFilesList = Arrays.asList(aContentFiles);
+        HashSet aContentFilesHashSet = new HashSet(aContentFilesList);
 
         // Loop over caption files and find corresponding content file
-        System.out.println( "Indexing, adding files" );
+        System.out.println("Indexing, adding files");
         int nCaptionFilesLen = aCaptionFiles.length;
-        for( int i = 0 ; i < nCaptionFilesLen ; i++ )
-        {
+        for (int i = 0; i < nCaptionFilesLen; i++) {
             String aCaptionFileStr = aCaptionFiles[i];
-            File aCaptionFile = new File( aCaptionFilesDir, aCaptionFileStr );
+            File aCaptionFile = new File(aCaptionFilesDir, aCaptionFileStr);
             File aContentFile = null;
-            if( aContentFilesHashSet.contains( aCaptionFileStr ) )
-            aContentFile = new File( aContentFilesDir, aCaptionFileStr );
+            if (aContentFilesHashSet.contains(aCaptionFileStr)) {
+                aContentFile = new File(aContentFilesDir, aCaptionFileStr);
+            }
 
-            System.out.print( "." );
-            writer.addDocument( HelpFileDocument.Document( aModule, aCaptionFile, aContentFile ) );
+            System.out.print(".");
+            writer.addDocument(HelpFileDocument.Document(aModule, aCaptionFile, aContentFile));
         }
 
         // Loop over content files to find remaining files not mapped to caption files
         int nContentFilesLen = aContentFiles.length;
-        for( int i = 0 ; i < nContentFilesLen ; i++ )
-        {
+        for (int i = 0; i < nContentFilesLen; i++) {
             String aContentFileStr = aContentFiles[i];
-            if( !aCaptionFilesHashSet.contains( aContentFileStr ) ) {
+            if (!aCaptionFilesHashSet.contains(aContentFileStr)) {
                 // Not already handled in caption files loop
                 File aCaptionFile = null;
-                File aContentFile = new File( aContentFilesDir, aContentFileStr );
-                    System.out.print( "." );
-                writer.addDocument( HelpFileDocument.Document( aModule, aCaptionFile, aContentFile ) );
+                File aContentFile = new File(aContentFilesDir, aContentFileStr);
+                System.out.print(".");
+                writer.addDocument(HelpFileDocument.Document(aModule, aCaptionFile, aContentFile));
             }
         }
         return 0;
     }
 
-    public static void createZipFile( File aDirToZip, String aTargetZipFileStr )
-        throws FileNotFoundException, IOException
-    {
-        FileOutputStream fos = new FileOutputStream( aTargetZipFileStr );
-        ZipOutputStream zos = new ZipOutputStream( fos );
+    public static void createZipFile(File aDirToZip, String aTargetZipFileStr)
+            throws FileNotFoundException, IOException {
+        FileOutputStream fos = new FileOutputStream(aTargetZipFileStr);
+        ZipOutputStream zos = new ZipOutputStream(fos);
 
         File[] aChildrenFiles = aDirToZip.listFiles();
         int nFileCount = aChildrenFiles.length;
-        for( int i = 0 ; i < nFileCount ; i++ )
-            addToZipRecursively( zos, aChildrenFiles[i], null );
+        for (int i = 0; i < nFileCount; i++) {
+            addToZipRecursively(zos, aChildrenFiles[i], null);
+        }
 
         zos.close();
     }
 
-    public static void addToZipRecursively( ZipOutputStream zos, File aFile, String aBasePath )
-        throws FileNotFoundException, IOException
-    {
-        if( aFile.isDirectory() )
-        {
+    public static void addToZipRecursively(ZipOutputStream zos, File aFile, String aBasePath)
+            throws FileNotFoundException, IOException {
+        if (aFile.isDirectory()) {
             String aDirName = aFile.getName();
-            if( aDirName.equalsIgnoreCase( "caption" ) || aDirName.equalsIgnoreCase( "content" ) )
+            if (aDirName.equalsIgnoreCase("caption") || aDirName.equalsIgnoreCase("content")) {
                 return;
+            }
 
             File[] aChildrenFiles = aFile.listFiles();
             String aNewBasePath = "";
-            if( aBasePath != null )
+            if (aBasePath != null) {
                 aNewBasePath += aBasePath + File.separator;
+            }
             aNewBasePath += aDirName;
 
             int nFileCount = aChildrenFiles.length;
-            for( int i = 0 ; i < nFileCount ; i++ )
-                addToZipRecursively( zos, aChildrenFiles[i], aNewBasePath );
+            for (int i = 0; i < nFileCount; i++) {
+                addToZipRecursively(zos, aChildrenFiles[i], aNewBasePath);
+            }
 
             return;
         }
 
         // No directory
         // read contents of file we are going to put in the zip
-        int fileLength = (int)aFile.length();
-        FileInputStream fis = new FileInputStream ( aFile );
-        byte[] wholeFile = new byte [fileLength];
-        int bytesRead = fis.read( wholeFile , 0 , fileLength );
+        int fileLength = (int) aFile.length();
+        FileInputStream fis = new FileInputStream(aFile);
+        byte[] wholeFile = new byte[fileLength];
+        int bytesRead = fis.read(wholeFile, 0, fileLength);
         fis.close();
 
         String aFileName = aFile.getName();
         String aEntryName = "";
-        if( aBasePath != null )
+        if (aBasePath != null) {
             aEntryName += aBasePath + "/";
+        }
         aEntryName += aFileName;
-        ZipEntry aZipEntry = new ZipEntry( aEntryName );
-        aZipEntry.setTime( aFile.lastModified() );
-        aZipEntry.setSize( fileLength );
+        ZipEntry aZipEntry = new ZipEntry(aEntryName);
+        aZipEntry.setTime(aFile.lastModified());
+        aZipEntry.setSize(fileLength);
 
-        int nMethod = ( aFileName.toLowerCase().endsWith( ".jar") )
-            ? ZipEntry.STORED : ZipEntry.DEFLATED;
-        aZipEntry.setMethod( nMethod );
+        int nMethod = (aFileName.toLowerCase().endsWith(".jar"))
+                ? ZipEntry.STORED : ZipEntry.DEFLATED;
+        aZipEntry.setMethod(nMethod);
 
         CRC32 tempCRC = new CRC32();
-        tempCRC.update( wholeFile, 0, wholeFile.length );
-        aZipEntry.setCrc( tempCRC.getValue() );
+        tempCRC.update(wholeFile, 0, wholeFile.length);
+        aZipEntry.setCrc(tempCRC.getValue());
 
         // write the contents into the zip element
-        zos.putNextEntry( aZipEntry );
-        zos.write( wholeFile , 0, fileLength );
+        zos.putNextEntry(aZipEntry);
+        zos.write(wholeFile, 0, fileLength);
         zos.closeEntry();
     }
 
-    static public boolean deleteRecursively( File aFile )
-    {
-        if( aFile.isDirectory() )
-        {
+    static public boolean deleteRecursively(File aFile) {
+        if (aFile.isDirectory()) {
             File[] aChildrenFiles = aFile.listFiles();
             int nFileCount = aChildrenFiles.length;
-            for( int i = 0 ; i < nFileCount ; i++ )
-            {
+            for (int i = 0; i < nFileCount; i++) {
                 File aChildrenFile = aChildrenFiles[i];
-                boolean bSuccess = deleteRecursively( aChildrenFile );
-                if( !bSuccess )
+                boolean bSuccess = deleteRecursively(aChildrenFile);
+                if (!bSuccess) {
                     return false;
+                }
             }
         }
 
