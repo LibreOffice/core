872108edbca12363a50fdcb12d0f531ea08da248
Kurt Zenker <kz@openoffice.org>
2008-08-14 16:02:29 +0000

    INTEGRATION: CWS pdf25 (1.1.2); FILE ADDED 2008/08/05 09:59:14 akhva 1.1.2.2: #i90061 2008/07/24 11:41:28 akhva 1.1.2.1: #90061

---
+++ sdext/source/pdfimport/misc/pdfihelper.cxx
@@ -0,0 +1,84 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: pdfihelper.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sdext.hxx"
+
+#include "pdfihelper.hxx"
+
+#include <rtl/ustrbuf.hxx>
+#include <vcl/canvastools.hxx>
+
+using namespace pdfi;
+using namespace com::sun::star;
+
+rtl::OUString pdfi::getColorString( const rendering::ARGBColor& rCol )
+{
+    rtl::OUStringBuffer aBuf( 7 );
+    const sal_uInt8 nRed  ( vcl::unotools::toByteColor(rCol.Red)   );
+    const sal_uInt8 nGreen( vcl::unotools::toByteColor(rCol.Green) );
+    const sal_uInt8 nBlue ( vcl::unotools::toByteColor(rCol.Blue)  );
+    aBuf.append( sal_Unicode('#') );
+    if( nRed < 10 )
+        aBuf.append( sal_Unicode('0') );
+    aBuf.append( sal_Int32(nRed), 16 );
+    if( nGreen < 10 )
+        aBuf.append( sal_Unicode('0') );
+    aBuf.append( sal_Int32(nGreen), 16 );
+    if( nBlue < 10 )
+        aBuf.append( sal_Unicode('0') );
+    aBuf.append( sal_Int32(nBlue), 16 );
+
+    // TODO(F3): respect alpha transparency (polygons etc.)
+    OSL_ASSERT(rCol.Alpha == 1.0);
+
+    return aBuf.makeStringAndClear();
+}
+
+rtl::OUString pdfi::unitMMString( double fMM )
+{
+    rtl::OUStringBuffer aBuf( 32 );
+    aBuf.append( rtl_math_round( fMM, 2, rtl_math_RoundingMode_Floor ) );
+    aBuf.appendAscii( "mm" );
+
+    return aBuf.makeStringAndClear();
+}
+
+rtl::OUString pdfi::convertPixelToUnitString( double fPix )
+{
+    rtl::OUStringBuffer aBuf( 32 );
+    aBuf.append( rtl_math_round( convPx2mm( fPix ), 2, rtl_math_RoundingMode_Floor ) );
+    aBuf.appendAscii( "mm" );
+
+    return aBuf.makeStringAndClear();
+}
+
+
