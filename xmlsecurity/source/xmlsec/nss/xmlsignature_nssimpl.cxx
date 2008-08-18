ccc9fdb0d4edd596a0553c3bd39e190ee12f58c5
Vladimir Glazounov <vg@openoffice.org>
2008-08-18 12:16:48 +0000

    INTEGRATION: CWS cmcfixes48 (1.9.22); FILE MERGED 2008/07/30 09:30:35 cmc 1.9.22.1: #i92277# make this work on 64bit platforms

--- xmlsecurity/source/xmlsec/nss/xmlsignature_nssimpl.cxx
+++ xmlsecurity/source/xmlsec/nss/xmlsignature_nssimpl.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlsignature_nssimpl.cxx,v $
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -52,6 +52,11 @@
 #include "xmlstreamio.hxx"
 #include "errorcallback.hxx"
 
+#include <sal/types.h>
+//For reasons that escape me, this is what xmlsec does when size_t is not 4
+#if SAL_TYPES_SIZEOFPOINTER != 4
+#    define XMLSEC_NO_SIZE_T
+#endif
 #include "xmlsec/xmlsec.h"
 #include "xmlsec/xmldsig.h"
 #include "xmlsec/crypto.h"
