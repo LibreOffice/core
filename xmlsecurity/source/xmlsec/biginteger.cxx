227fc7ccce2f35f596160c92a291a14dc0447b7a
Vladimir Glazounov <vg@openoffice.org>
2008-08-18 12:14:49 +0000

    INTEGRATION: CWS cmcfixes48 (1.7.22); FILE MERGED 2008/07/30 09:30:33 cmc 1.7.22.1: #i92277# make this work on 64bit platforms

--- xmlsecurity/source/xmlsec/biginteger.cxx
+++ xmlsecurity/source/xmlsec/biginteger.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: biginteger.cxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -33,6 +33,11 @@
 
 #include <xmlsecurity/biginteger.hxx>
 
+#include <sal/types.h>
+//For reasons that escape me, this is what xmlsec does when size_t is not 4
+#if SAL_TYPES_SIZEOFPOINTER != 4
+#    define XMLSEC_NO_SIZE_T
+#endif
 #include <xmlsec/xmlsec.h>
 #include <xmlsec/bn.h>
 #include <com/sun/star/uno/Sequence.hxx>
