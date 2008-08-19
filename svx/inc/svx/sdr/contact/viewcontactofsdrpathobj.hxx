f4421da8457f65ab92c49852827dbcde3dd6af80
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:40:35 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:26 cl 1.1.2.9: fixed license files 2008/07/10 12:58:58 aw 1.1.2.8: #i39532# XOutputDevice removed, PrepareDelete removed 2008/05/27 14:48:06 aw 1.1.2.7: #i39532# changes DEV300 m12 resync corrections 2008/01/22 12:29:06 aw 1.1.2.6: adaptions and 1st stripping 2007/07/06 13:42:27 aw 1.1.2.5: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2006/10/19 10:53:45 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:05:51 aw 1.1.2.3: #i39532# 2006/06/02 14:17:45 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:32:43 aw 1.1.2.1: code for primitive support

---
+++ svx/inc/svx/sdr/contact/viewcontactofsdrpathobj.hxx
@@ -0,0 +1,74 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewcontactofsdrpathobj.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFSDRPATHOBJ_HXX
+#define _SDR_CONTACT_VIEWCONTACTOFSDRPATHOBJ_HXX
+
+#include <svx/sdr/contact/viewcontactoftextobj.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class SdrPathObj;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class ViewContactOfSdrPathObj : public ViewContactOfTextObj
+        {
+        protected:
+            // internal access to SdrPathObj
+            SdrPathObj& GetPathObj() const
+            {
+                return (SdrPathObj&)GetSdrObject();
+            }
+
+        public:
+            // basic constructor, used from SdrObject.
+            ViewContactOfSdrPathObj(SdrPathObj& rTextObj);
+            virtual ~ViewContactOfSdrPathObj();
+
+        protected:
+            // This method is responsible for creating the graphical visualisation data
+            // ONLY based on model data
+            virtual drawinglayer::primitive2d::Primitive2DSequence createViewIndependentPrimitive2DSequence() const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWCONTACTOFSDRPATHOBJ_HXX
+
+// eof
