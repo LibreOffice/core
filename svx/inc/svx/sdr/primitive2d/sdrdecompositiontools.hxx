7c32dfa54ff36e184dde2b65b956b84744502648
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:45:56 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:27 cl 1.1.2.4: fixed license files 2008/05/27 14:48:07 aw 1.1.2.3: #i39532# changes DEV300 m12 resync corrections 2008/04/04 06:07:12 aw 1.1.2.2: #i39532# added primitive support for tables 2006/10/19 10:52:09 aw 1.1.2.1: #i39532# primitive

---
+++ svx/inc/svx/sdr/primitive2d/sdrdecompositiontools.hxx
@@ -0,0 +1,90 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: sdrdecompositiontools.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_SDR_PRIMITIVE2D_SDRDECOMPOSITIONTOOLS_HXX
+#define INCLUDED_SDR_PRIMITIVE2D_SDRDECOMPOSITIONTOOLS_HXX
+
+#include <drawinglayer/primitive2d/baseprimitive2d.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predefines
+namespace basegfx {
+    class B2DPolygon;
+    class B2DPolyPolygon;
+    class B2DHomMatrix;
+}
+
+namespace drawinglayer { namespace attribute {
+    class SdrFillAttribute;
+    class SdrLineAttribute;
+    class FillGradientAttribute;
+    class SdrShadowAttribute;
+    class SdrLineStartEndAttribute;
+    class SdrTextAttribute;
+}}
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace drawinglayer
+{
+    namespace primitive2d
+    {
+        Primitive2DReference createPolyPolygonFillPrimitive(
+            const ::basegfx::B2DPolyPolygon& rUnitPolyPolygon,
+            const ::basegfx::B2DHomMatrix& rObjectTransform,
+            const attribute::SdrFillAttribute& rFill,
+            const attribute::FillGradientAttribute* pFillGradient = 0L);
+
+        Primitive2DReference createPolygonLinePrimitive(
+            const ::basegfx::B2DPolygon& rUnitPolygon,
+            const ::basegfx::B2DHomMatrix& rObjectTransform,
+            const attribute::SdrLineAttribute& rLine,
+            const attribute::SdrLineStartEndAttribute* pStroke = 0L);
+
+        Primitive2DReference createTextPrimitive(
+            const ::basegfx::B2DPolyPolygon& rUnitPolyPolygon,
+            const ::basegfx::B2DHomMatrix& rObjectTransform,
+            const attribute::SdrTextAttribute& rText,
+            const attribute::SdrLineAttribute* pStroke,
+            bool bCellText);
+
+        Primitive2DReference createShadowPrimitive(
+            const Primitive2DSequence& rSource,
+            const attribute::SdrShadowAttribute& rShadow);
+
+    } // end of namespace primitive2d
+} // end of namespace drawinglayer
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //INCLUDED_SDR_PRIMITIVE2D_SDRDECOMPOSITIONTOOLS_HXX
+
+// eof
