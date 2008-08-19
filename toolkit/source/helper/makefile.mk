4a14d774fd5c6a7b1b521c3b995c8468f5489f92
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:11:09 +0000

    INTEGRATION: CWS aw033 (1.11.26); FILE MERGED 2008/05/14 13:43:38 aw 1.11.26.5: RESYNC: (1.13-1.14); FILE MERGED 2008/02/13 11:01:39 aw 1.11.26.4: formpdfexport re-added 2008/02/12 11:25:38 aw 1.11.26.3: RESYNC: (1.12-1.13); FILE MERGED 2007/08/09 13:40:54 aw 1.11.26.2: RESYNC: (1.11-1.12); FILE MERGED 2007/08/07 15:41:42 aw 1.11.26.1: #i39532# migrated describePDFControl to toolkit

--- toolkit/source/helper/makefile.mk
+++ toolkit/source/helper/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.14 $
+# $Revision: 1.15 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -58,6 +58,7 @@ SLOFILES=   \
             $(SLO)$/externallock.obj                \
             $(SLO)$/imagealign.obj                  \
             $(SLO)$/throbberimpl.obj                \
+            $(SLO)$/formpdfexport.obj				\
             $(SLO)$/accessibilityclient.obj         \
             $(SLO)$/fixedhyperbase.obj
 
