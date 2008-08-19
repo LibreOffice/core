72b3ac6a64a199cffb75e89d71d0e9caa86c72ee
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:34:04 +0000

    INTEGRATION: CWS aw033 (1.21.58); FILE MERGED 2008/07/10 13:01:00 aw 1.21.58.1: #i39532# XOutputDevice removed, PrepareDelete removed

--- svx/source/xoutdev/xtabhtch.cxx
+++ svx/source/xoutdev/xtabhtch.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xtabhtch.cxx,v $
- * $Revision: 1.21 $
+ * $Revision: 1.22 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -53,11 +53,16 @@
 #include <svx/dialmgr.hxx>
 #include <svx/xtable.hxx>
 #include <svx/xpool.hxx>
-#include <svx/xoutx.hxx>
 #include "dlgutil.hxx"
 #include <svx/xflhtit.hxx>
 #include <svx/xflclit.hxx>
 #include <svx/xfillit0.hxx>
+
+#include <svx/svdorect.hxx>
+#include <svx/svdmodel.hxx>
+#include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
+#include <svx/sdr/contact/displayinfo.hxx>
+#include <svx/xlnclit.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::rtl;
@@ -152,54 +157,115 @@ Bitmap* XHatchTable::CreateBitmapForUI( long /*nIndex*/, BOOL /*bDelete*/)
 // class XHatchList
 // -----------------
 
-/*************************************************************************
-|*
-|* XHatchList::XHatchList()
-|*
-*************************************************************************/
-
-XHatchList::XHatchList( const String& rPath,
-                            XOutdevItemPool* pInPool,
-                            USHORT nInitSize, USHORT nReSize ) :
-                XPropertyList   ( rPath, pInPool, nInitSize, nReSize),
-                pVD             ( NULL ),
-                pXOut           ( NULL ),
-                pXFSet          ( NULL )
+class impXHatchList
 {
-    pBmpList = new List( nInitSize, nReSize );
+private:
+    VirtualDevice*          mpVirtualDevice;
+    SdrModel*               mpSdrModel;
+    SdrObject*              mpBackgroundObject;
+    SdrObject*              mpHatchObject;
+
+public:
+    impXHatchList(VirtualDevice* pV, SdrModel* pM, SdrObject* pB, SdrObject* pH)
+    :   mpVirtualDevice(pV),
+        mpSdrModel(pM),
+        mpBackgroundObject(pB),
+        mpHatchObject(pH)
+    {}
+
+    ~impXHatchList()
+    {
+        delete mpVirtualDevice;
+        SdrObject::Free(mpBackgroundObject);
+        SdrObject::Free(mpHatchObject);
+        delete mpSdrModel;
+    }
+
+    VirtualDevice* getVirtualDevice() const { return mpVirtualDevice; }
+    SdrObject* getBackgroundObject() const { return mpBackgroundObject; }
+    SdrObject* getHatchObject() const { return mpHatchObject; }
+};
+
+void XHatchList::impCreate()
+{
+    if(!mpData)
+    {
+        const Point aZero(0, 0);
+        const StyleSettings& rStyleSettings = Application::GetSettings().GetStyleSettings();
+
+        VirtualDevice* pVirDev = new VirtualDevice;
+        OSL_ENSURE(0 != pVirDev, "XDashList: no VirtualDevice created!" );
+        pVirDev->SetMapMode(MAP_100TH_MM);
+        const Size aSize(pVirDev->PixelToLogic(Size(BITMAP_WIDTH, BITMAP_HEIGHT)));
+        pVirDev->SetOutputSize(aSize);
+        pVirDev->SetDrawMode(rStyleSettings.GetHighContrastMode()
+            ? DRAWMODE_SETTINGSLINE | DRAWMODE_SETTINGSFILL | DRAWMODE_SETTINGSTEXT | DRAWMODE_SETTINGSGRADIENT
+            : DRAWMODE_DEFAULT);
+
+        SdrModel* pSdrModel = new SdrModel();
+        OSL_ENSURE(0 != pSdrModel, "XDashList: no SdrModel created!" );
+        pSdrModel->GetItemPool().FreezeIdRanges();
+
+        const Size aSinglePixel(pVirDev->PixelToLogic(Size(1, 1)));
+        const Rectangle aBackgroundSize(aZero, Size(aSize.getWidth() - aSinglePixel.getWidth(), aSize.getHeight() - aSinglePixel.getHeight()));
+        SdrObject* pBackgroundObject = new SdrRectObj(aBackgroundSize);
+        OSL_ENSURE(0 != pBackgroundObject, "XDashList: no BackgroundObject created!" );
+        pBackgroundObject->SetModel(pSdrModel);
+        pBackgroundObject->SetMergedItem(XFillStyleItem(XFILL_SOLID));
+        pBackgroundObject->SetMergedItem(XFillColorItem(String(), rStyleSettings.GetFieldColor()));
+        pBackgroundObject->SetMergedItem(XLineStyleItem(XLINE_SOLID));
+        pBackgroundObject->SetMergedItem(XLineColorItem(String(), Color(COL_BLACK)));
+
+        SdrObject* pHatchObject = new SdrRectObj(aBackgroundSize);
+        OSL_ENSURE(0 != pHatchObject, "XDashList: no HatchObject created!" );
+        pHatchObject->SetModel(pSdrModel);
+        pHatchObject->SetMergedItem(XFillStyleItem(XFILL_HATCH));
+        pHatchObject->SetMergedItem(XLineStyleItem(XLINE_NONE));
+
+        mpData = new impXHatchList(pVirDev, pSdrModel, pBackgroundObject, pHatchObject);
+        OSL_ENSURE(0 != mpData, "XDashList: data creation went wrong!" );
+    }
 }
 
-/************************************************************************/
+void XHatchList::impDestroy()
+{
+    if(mpData)
+    {
+        delete mpData;
+        mpData = 0;
+    }
+}
+
+XHatchList::XHatchList(const String& rPath, XOutdevItemPool* pInPool, sal_uInt16 nInitSize, sal_uInt16 nReSize)
+:   XPropertyList(rPath, pInPool, nInitSize, nReSize),
+    mpData(0)
+{
+    pBmpList = new List(nInitSize, nReSize);
+}
 
 XHatchList::~XHatchList()
 {
-    if( pVD )    delete pVD;
-    if( pXOut )  delete pXOut;
-    if( pXFSet ) delete pXFSet;
+    if(mpData)
+    {
+        delete mpData;
+        mpData = 0;
+    }
 }
-
-/************************************************************************/
 
 XHatchEntry* XHatchList::Replace(XHatchEntry* pEntry, long nIndex )
 {
     return (XHatchEntry*) XPropertyList::Replace(pEntry, nIndex);
 }
 
-/************************************************************************/
-
 XHatchEntry* XHatchList::Remove(long nIndex)
 {
     return (XHatchEntry*) XPropertyList::Remove(nIndex, 0);
 }
 
-/************************************************************************/
-
 XHatchEntry* XHatchList::GetHatch(long nIndex) const
 {
     return (XHatchEntry*) XPropertyList::Get(nIndex, 0);
 }
-
-/************************************************************************/
 
 BOOL XHatchList::Load()
 {
@@ -226,8 +292,6 @@ BOOL XHatchList::Load()
     return( FALSE );
 }
 
-/************************************************************************/
-
 BOOL XHatchList::Save()
 {
     INetURLObject aURL( aPath );
@@ -247,8 +311,6 @@ BOOL XHatchList::Save()
     return SvxXMLXTableExportComponent::save( aURL.GetMainURL( INetURLObject::NO_DECODE ), xTable );
 }
 
-/************************************************************************/
-
 BOOL XHatchList::Create()
 {
     XubString aStr( SVX_RES( RID_SVXSTR_HATCH ) );
@@ -265,10 +327,10 @@ BOOL XHatchList::Create()
     return( TRUE );
 }
 
-/************************************************************************/
-
 BOOL XHatchList::CreateBitmapsForUI()
 {
+    impCreate();
+
     for( long i = 0; i < Count(); i++)
     {
         Bitmap* pBmp = CreateBitmapForUI( i, FALSE );
@@ -277,81 +339,39 @@ BOOL XHatchList::CreateBitmapsForUI()
         if( pBmp )
             pBmpList->Insert( pBmp, i );
     }
-    // Loeschen, da JOE den Pool vorm Dtor entfernt!
-    if( pVD )   { delete pVD;   pVD = NULL;     }
-    if( pXOut ) { delete pXOut; pXOut = NULL;   }
-    if( pXFSet ){ delete pXFSet; pXFSet = NULL; }
+
+    impDestroy();
 
     return( TRUE );
 }
 
-/************************************************************************/
-
 Bitmap* XHatchList::CreateBitmapForUI( long nIndex, BOOL bDelete )
 {
-    Point   aZero;
+    impCreate();
+    VirtualDevice* pVD = mpData->getVirtualDevice();
+    SdrObject* pHatchObject = mpData->getHatchObject();
 
-    if( !pVD ) // und pXOut und pXFSet
+    pHatchObject->SetMergedItem(XFillStyleItem(XFILL_HATCH));
+    pHatchObject->SetMergedItem(XFillHatchItem(String(), GetHatch(nIndex)->GetHatch()));
+
+    sdr::contact::SdrObjectVector aObjectVector;
+    aObjectVector.push_back(mpData->getBackgroundObject());
+    aObjectVector.push_back(pHatchObject);
+    sdr::contact::ObjectContactOfObjListPainter aPainter(*pVD, aObjectVector, 0);
+    sdr::contact::DisplayInfo aDisplayInfo;
+
+    aPainter.ProcessDisplay(aDisplayInfo);
+
+    const Point aZero(0, 0);
+    Bitmap* pBitmap = new Bitmap(pVD->GetBitmap(aZero, pVD->GetOutputSize()));
+
+    if(bDelete)
     {
-        pVD = new VirtualDevice;
-        DBG_ASSERT( pVD, "XHatchList: Konnte kein VirtualDevice erzeugen!" );
-        //pVD->SetMapMode( MAP_100TH_MM );
-        //pVD->SetOutputSize( pVD->PixelToLogic( Size( BITMAP_WIDTH, BITMAP_HEIGHT ) ) );
-        pVD->SetOutputSizePixel( Size( BITMAP_WIDTH, BITMAP_HEIGHT ) );
-
-        pXOut = new XOutputDevice( pVD );
-        DBG_ASSERT( pVD, "XHatchList: Konnte kein XOutDevice erzeugen!" );
-
-        pXFSet = new XFillAttrSetItem( pXPool );
-        DBG_ASSERT( pVD, "XHatchList: Konnte kein XFillAttrSetItem erzeugen!" );
+        impDestroy();
     }
 
-    if( Application::GetSettings().GetStyleSettings().GetHighContrastMode() != 0 )
-        pVD->SetDrawMode( OUTPUT_DRAWMODE_CONTRAST );
-    else
-        pVD->SetDrawMode( OUTPUT_DRAWMODE_COLOR );
-
-    // Damit die Schraffuren mit Rahmen angezeigt werden:
-    // MapMode-Aenderungen (100th mm <--> Pixel)
-    Size aPixelSize = pVD->GetOutputSizePixel();
-    pVD->SetMapMode( MAP_PIXEL );
-
-    pXFSet->GetItemSet().Put( XFillStyleItem( XFILL_SOLID ) );
-    pXFSet->GetItemSet().Put( XFillColorItem( String(), RGB_Color( COL_WHITE ) ) );
-
-//-/    pXOut->SetFillAttr( *pXFSet );
-    pXOut->SetFillAttr( pXFSet->GetItemSet() );
-
-    // #73550#
-    pXOut->OverrideLineColor( Color( COL_BLACK ) );
-
-    pXOut->DrawRect( Rectangle( aZero, aPixelSize ) );
-
-    pVD->SetMapMode( MAP_100TH_MM );
-    Size aVDSize = pVD->GetOutputSize();
-    // 1 Pixel (Rahmen) abziehen
-    aVDSize.Width() -= (long) ( aVDSize.Width() / aPixelSize.Width() + 1 );
-    aVDSize.Height() -= (long) ( aVDSize.Height() / aPixelSize.Height() + 1 );
-
-    pXFSet->GetItemSet().Put( XFillStyleItem( XFILL_HATCH ) );
-    pXFSet->GetItemSet().Put( XFillHatchItem( String(), GetHatch( nIndex )->GetHatch() ) );
-
-//-/    pXOut->SetFillAttr( *pXFSet );
-    pXOut->SetFillAttr( pXFSet->GetItemSet() );
-
-    pXOut->DrawRect( Rectangle( aZero, aVDSize ) );
-
-    Bitmap* pBitmap = new Bitmap( pVD->GetBitmap( aZero, pVD->GetOutputSize() ) );
-
-    // Loeschen, da JOE den Pool vorm Dtor entfernt!
-    if( bDelete )
-    {
-        if( pVD )   { delete pVD;   pVD = NULL;     }
-        if( pXOut ) { delete pXOut; pXOut = NULL;   }
-        if( pXFSet ){ delete pXFSet; pXFSet = NULL; }
-    }
-
-    return( pBitmap );
+    return pBitmap;
 }
 
+//////////////////////////////////////////////////////////////////////////////
 // eof
