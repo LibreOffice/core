da3e2ae0875ee2f711d72c65588dbd1305b811a5
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:42:04 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:26 cl 1.1.2.6: fixed license files 2008/05/27 14:48:06 aw 1.1.2.5: #i39532# changes DEV300 m12 resync corrections 2008/01/22 12:29:06 aw 1.1.2.4: adaptions and 1st stripping 2007/07/06 13:42:27 aw 1.1.2.3: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2006/10/19 10:53:46 aw 1.1.2.2: #i39532# primitive 2006/08/09 17:04:29 aw 1.1.2.1: #i39532#

---
+++ svx/inc/svx/sdr/contact/viewobjectcontactofe3d.hxx
@@ -0,0 +1,86 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewobjectcontactofe3d.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWOBJECTCONTACTOFE3D_HXX
+#define _SDR_CONTACT_VIEWOBJECTCONTACTOFE3D_HXX
+
+#include <svx/sdr/contact/viewobjectcontactofsdrobj.hxx>
+#include <drawinglayer/primitive3d/baseprimitive3d.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+namespace sdr { namespace contact {
+    class ViewObjectContactOfE3dScene;
+}}
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class ViewObjectContactOfE3d : public ViewObjectContactOfSdrObj
+        {
+        protected:
+            // Primitive3D sequence of the ViewContact. This contains all necessary information
+            // for the graphical visualisation and needs to be supported by all VCs which
+            // can be visualized.
+            drawinglayer::primitive3d::Primitive3DSequence          mxPrimitive3DSequence;
+
+            // This method is responsible for creating the graphical visualisation data which is
+            // stored/cached in the local primitive. Default gets view-independent Primitive3D
+            // from the ViewContact using ViewContact::getViewIndependentPrimitive3DSequence(), takes care of
+            // visibility and ghosted.
+            // This method will not handle included hierarchies and not check geometric visibility.
+            drawinglayer::primitive3d::Primitive3DSequence createPrimitive3DSequence(const DisplayInfo& rDisplayInfo) const;
+
+            // also overload the 2d method to deliver a 2d object with embedd3d 3d and the 3d transformation which is able to
+            // answer the get2DRange question accordingly
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            ViewObjectContactOfE3d(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfE3d();
+
+            // access to the local primitive sequence. This will ensure that the list is
+            // current in comparing the local list content with a fresh created incarnation
+            // This method will not handle included hierarchies or visibility.
+            drawinglayer::primitive3d::Primitive3DSequence getPrimitive3DSequence(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWOBJECTCONTACTOFE3D_HXX
+
+// eof
