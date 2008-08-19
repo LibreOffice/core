98d33a6c644099fa8caef60cabca367fcb7d7138
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:36:12 +0000

    INTEGRATION: CWS aw033 (1.8.14); FILE MERGED 2008/07/10 12:58:58 aw 1.8.14.14: #i39532# XOutputDevice removed, PrepareDelete removed 2008/05/16 10:54:33 aw 1.8.14.13: adaptions after resync 2008/05/14 13:58:24 aw 1.8.14.12: RESYNC: (1.11-1.12); FILE MERGED 2008/04/16 05:30:03 aw 1.8.14.11: diverse optimisations and fixes for primitives. Support for LazyInvalidation. 2008/02/12 10:42:25 aw 1.8.14.10: CVS update from internal development GIT 2008/02/07 14:12:58 aw 1.8.14.9: stable version from working GIT repository for unxlngi6 build 2008/01/29 10:27:14 aw 1.8.14.8: updated refresh for ActionChanged(), diverse removals 2008/01/22 12:29:05 aw 1.8.14.7: adaptions and 1st stripping 2007/08/09 17:14:13 aw 1.8.14.6: RESYNC: (1.10-1.11); FILE MERGED 2007/01/25 12:44:05 aw 1.8.14.5: #i39532# change 3D scene content generation to be usable from API implementation, too 2006/10/19 10:53:41 aw 1.8.14.4: #i39532# primitive 2006/08/09 17:05:47 aw 1.8.14.3: #i39532# 2006/05/12 14:58:52 aw 1.8.14.2: RESYNC: (1.8-1.10); FILE MERGED 2006/05/12 12:44:09 aw 1.8.14.1: code changes for primitive support

--- svx/inc/svx/sdr/contact/objectcontact.hxx
+++ svx/inc/svx/sdr/contact/objectcontact.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: objectcontact.hxx,v $
- * $Revision: 1.12 $
+ * $Revision: 1.13 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,32 +31,32 @@
 #ifndef _SDR_CONTACT_OBJECTCONTACT_HXX
 #define _SDR_CONTACT_OBJECTCONTACT_HXX
 
-#include <svx/sdr/contact/viewobjectcontactlist.hxx>
+#include <svx/sdr/animation/objectanimator.hxx>
 #include "svx/svxdllapi.h"
+#include <drawinglayer/geometry/viewinformation2d.hxx>
 
 //////////////////////////////////////////////////////////////////////////////
 // predeclarations
 
 class SetOfByte;
 class Rectangle;
+class SdrPageView;
+class OutputDevice;
 
-namespace sdr
-{
-    namespace contact
-    {
-        class DisplayInfo;
-        class ViewContact;
-        class ViewObjectContactRedirector;
-    } // end of namespace contact
-    namespace animation
-    {
-        class ObjectAnimator;
-    } // end of namespace animation
-    namespace event
-    {
-        class TimerEventHandler;
-    } // end of namespace event
-} // end of namespace sdr
+namespace sdr { namespace contact {
+    class DisplayInfo;
+    class ViewContact;
+    class ViewObjectContactRedirector;
+}}
+
+namespace sdr { namespace event {
+    class TimerEventHandler;
+}}
+
+namespace basegfx {
+    class B2DRange;
+    class B2DHomMatrix;
+}
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -66,61 +66,15 @@ namespace sdr
     {
         class SVX_DLLPUBLIC ObjectContact
         {
-        protected:
+        private:
+            // make ViewObjectContact a friend to exclusively allow it to use
+            // AddViewObjectContact/RemoveViewObjectContact
+            friend class ViewObjectContact;
+
             // All VOCs which are created using this OC, thus remembering this OC
             // as a reference. All those VOCs need to be deleted when the OC goes down.
             // Registering and de-registering is done in the VOC constructors/destructors.
-            ViewObjectContactList                           maVOCList;
-
-            // Current DrawHierarchy. This may be rebuilt from srcatch anytime and
-            // may not contain all registered objects from maVOCList.
-            ViewObjectContactList                           maDrawHierarchy;
-
-            // the ObjectAnimator if this View and the contained objects
-            // support ObjectAnimations
-            sdr::animation::ObjectAnimator*                 mpObjectAnimator;
-
-            // the EventHandler for asynchronious loading of graphics
-            sdr::event::TimerEventHandler*                  mpEventHandler;
-
-            // The redirector. If set it is used to pipe all supported calls
-            // to the redirector. When one is set at the ViewContact too, the one at
-            // the ViewContact will win.
-            ViewObjectContactRedirector*                    mpViewObjectContactRedirector;
-
-            // bitfield
-            // This flag describes if the DrawHierarchy used by this OC is
-            // in a valid state. It needs to be set to sal_True when the DrawHierarchy is
-            // created and may be invalidated from MarkDrawHierarchyInvalid().
-            // Initialisation is with sal_False of course.
-            unsigned                                        mbDrawHierarchyValid : 1;
-
-            // ## test for preview renderer
-            unsigned                                        mbIsPreviewRenderer : 1;
-
-            // method to create a ObjectAnimator. Needs to give a result.
-            virtual sdr::animation::ObjectAnimator* CreateObjectAnimator();
-
-            // method to create a EventHandler. Needs to give a result.
-            virtual sdr::event::TimerEventHandler* CreateEventHandler();
-
-            // Update Draw Hierarchy data
-            virtual void EnsureValidDrawHierarchy(DisplayInfo& rDisplayInfo) = 0;
-
-        public:
-            // basic constructor
-            ObjectContact();
-
-            // The destructor. When PrepareDelete() was not called before (see there)
-            // warnings will be generated in debug version if there are still contacts
-            // existing.
-            virtual ~ObjectContact();
-
-            // Prepare deletion of this object. This needs to be called always
-            // before really deleting this objects. This is necessary since in a c++
-            // destructor no virtual function calls are allowed. To avoid this problem,
-            // it is required to first call PrepareDelete().
-            virtual void PrepareDelete();
+            std::vector< ViewObjectContact* >               maViewObjectContactVector;
 
             // A new ViewObjectContact was created and shall be remembered.
             void AddViewObjectContact(ViewObjectContact& rVOContact);
@@ -128,85 +82,135 @@ namespace sdr
             // A ViewObjectContact was deleted and shall be forgotten.
             virtual void RemoveViewObjectContact(ViewObjectContact& rVOContact);
 
-            // Test if ViewObjectContact is registered here
-            sal_Bool ContainsViewObjectContact(ViewObjectContact& rVOContact);
+            // the primitiveAnimator which is used if this View and/or the contained primitives
+            // support animatedSwitchPrimitives
+            sdr::animation::primitiveAnimator               maPrimitiveAnimator;
 
-            // Clear Draw Hierarchy data.
-            void ClearDrawHierarchy();
+            // the EventHandler for e.g. asynchronious loading of graphics
+            sdr::event::TimerEventHandler*                  mpEventHandler;
+
+            // The redirector. If set it is used to pipe all supported calls
+            // to the redirector. When one is set at the ViewContact too, the one at
+            // the ViewContact will win.
+            ViewObjectContactRedirector*                    mpViewObjectContactRedirector;
+
+            // the Primitive2DParameters containing view information
+            drawinglayer::geometry::ViewInformation2D       maViewInformation2D;
+
+            // bitfield
+            // flag for preview renderer
+            unsigned                                        mbIsPreviewRenderer : 1;
+
+            // method to create a EventHandler. Needs to give a result.
+            sdr::event::TimerEventHandler* CreateEventHandler();
+
+        protected:
+            // Interface to allow derivates to travel over the registered VOC's
+            sal_uInt32 getViewObjectContactCount() const { return maViewObjectContactVector.size(); }
+            ViewObjectContact* getViewObjectContact(sal_uInt32 a) const { return maViewObjectContactVector[a]; }
+
+            // interface to allow derivates to set PreviewRenderer flag
+            void setPreviewRenderer(bool bNew) { mbIsPreviewRenderer = bNew; }
+
+            // interface to allow derivates to set ViewInformation2D
+            void updateViewInformation2D(const drawinglayer::geometry::ViewInformation2D& rViewInformation2D) { maViewInformation2D = rViewInformation2D; }
+
+        public:
+            // basic constructor
+            ObjectContact();
+            virtual ~ObjectContact();
+
+            // LazyInvalidate request. This is used from the VOCs to mark that they
+            // got invalidated by an ActionCanged() call. An active view needs to remember
+            // this and take action on it. Default implementation directly calls back
+            // triggerLazyInvalidate() wich promptly handles the request
+            virtual void setLazyInvalidate(ViewObjectContact& rVOC);
+
+            // call this to support evtl. preparations for repaint. Default does nothing
+            virtual void PrepareProcessDisplay();
 
             // Process the whole displaying
-            virtual void ProcessDisplay(DisplayInfo& rDisplayInfo) = 0;
+            virtual void ProcessDisplay(DisplayInfo& rDisplayInfo);
 
             // test if visualizing of entered groups is switched on at all. Default
             // implementation returns sal_False.
-            virtual sal_Bool DoVisualizeEnteredGroup() const;
+            virtual bool DoVisualizeEnteredGroup() const;
 
-            // Get the active group (the entered group). To get independent
-            // from the old object/view classes return values use the new
-            // classes. Default returns NULL.
-            virtual ViewContact* GetActiveGroupContact() const;
-
-            // Get info about validity state of DrawHierarchy
-            sal_Bool IsDrawHierarchyValid() const;
-
-            // Take notice of invalidation of DrawHierarchy from this level. This may
-            // be the removal/deletion or insertion of an object. Take preparations for reacting on that.
-            void MarkDrawHierarchyInvalid();
+            // get active group's (the entered group) ViewContact
+            virtual const ViewContact* getActiveViewContact() const;
 
             // Invalidate given rectangle at the window/output which is represented by
             // this ObjectContact. Default does nothing.
-            virtual void InvalidatePartOfView(const Rectangle& rRectangle) const;
+            virtual void InvalidatePartOfView(const basegfx::B2DRange& rRange) const;
 
-            // #i42815#
             // Get info if given Rectangle is visible in this view
-            virtual sal_Bool IsAreaVisible(const Rectangle& rRectangle) const;
-
-            // Take some action when new objects are inserted. This is triggered from
-            // the VOCs, originating from VCs.
-            void ActionChildInserted(const Rectangle& rInitialRectangle);
+            virtual bool IsAreaVisible(const basegfx::B2DRange& rRange) const;
 
             // Get info about the need to visualize GluePoints. The default
             // is that it is not necessary.
-            virtual sal_Bool AreGluePointsVisible() const;
+            virtual bool AreGluePointsVisible() const;
 
-            // method to get the ObjectAnimator. It will
-            // return a existing one or create a new one using CreateObjectAnimator().
-            sdr::animation::ObjectAnimator& GetObjectAnimator() const;
+            // method to get the primitiveAnimator
+            sdr::animation::primitiveAnimator& getPrimitiveAnimator();
 
             // method to get the EventHandler. It will
             // return a existing one or create a new one using CreateEventHandler().
             sdr::event::TimerEventHandler& GetEventHandler() const;
 
-            // delete the ObjectAnimator
-            void DeleteObjectAnimator();
-
             // delete the EventHandler
             void DeleteEventHandler();
 
-            // test if there is an ObjectAnimator without creating one on demand
-            sal_Bool HasObjectAnimator() const;
-
             // test if there is an EventHandler without creating one on demand
-            sal_Bool HasEventHandler() const;
+            bool HasEventHandler() const;
 
             // check if text animation is allowed. Default is sal_true.
-            virtual sal_Bool IsTextAnimationAllowed() const;
+            virtual bool IsTextAnimationAllowed() const;
 
             // check if graphic animation is allowed. Default is sal_true.
-            virtual sal_Bool IsGraphicAnimationAllowed() const;
+            virtual bool IsGraphicAnimationAllowed() const;
 
             // check if asynchronious graphis loading is allowed. Default is sal_False.
-            virtual sal_Bool IsAsynchronGraphicsLoadingAllowed() const;
+            virtual bool IsAsynchronGraphicsLoadingAllowed() const;
 
             // access to ViewObjectContactRedirector
             ViewObjectContactRedirector* GetViewObjectContactRedirector() const;
             void SetViewObjectContactRedirector(ViewObjectContactRedirector* pNew);
 
             // check if buffering of MasterPages is allowed. Default is sal_False.
-            virtual sal_Bool IsMasterPageBufferingAllowed() const;
+            virtual bool IsMasterPageBufferingAllowed() const;
+
+            // print? Default is false
+            virtual bool isOutputToPrinter() const;
+
+            // window? Default is true
+            virtual bool isOutputToWindow() const;
+
+            // VirtualDevice? Default is false
+            virtual bool isOutputToVirtualDevice() const;
+
+            // recording MetaFile? Default is false
+            virtual bool isOutputToRecordingMetaFile() const;
+
+            // gray display mode
+            virtual bool isDrawModeGray() const;
+
+            // gray display mode
+            virtual bool isDrawModeBlackWhite() const;
+
+            // high contrast display mode
+            virtual bool isDrawModeHighContrast() const;
 
             // check if this is a preview renderer. Default is sal_False.
             bool IsPreviewRenderer() const { return mbIsPreviewRenderer; }
+
+            // get Primitive2DParameters for this view
+            const drawinglayer::geometry::ViewInformation2D& getViewInformation2D() const { return maViewInformation2D; }
+
+            // access to SdrPageView. May return 0L like the default implementations do. Needs to be overloaded as needed.
+            virtual SdrPageView* TryToGetSdrPageView() const;
+
+            // access to OutputDevice. May return 0L like the default implementations do. Needs to be overloaded as needed.
+            virtual OutputDevice* TryToGetOutputDevice() const;
         };
     } // end of namespace contact
 } // end of namespace sdr
