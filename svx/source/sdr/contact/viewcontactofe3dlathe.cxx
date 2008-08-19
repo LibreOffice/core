718ead89ccc28147335981baf3bae100565a804d
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:06:26 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:28 cl 1.1.2.12: fixed license files 2008/07/10 13:00:54 aw 1.1.2.11: #i39532# XOutputDevice removed, PrepareDelete removed 2008/05/27 14:49:57 aw 1.1.2.10: #i39532# changes DEV300 m12 resync corrections 2008/05/14 09:55:15 aw 1.1.2.9: #i39532# aw033 progresses from git 2007/08/13 15:34:33 aw 1.1.2.8: #i39532# changes after resync 2007/07/06 13:43:07 aw 1.1.2.7: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/01/23 10:46:30 aw 1.1.2.6: changes after resync 2006/12/11 17:40:57 aw 1.1.2.5: #i39532# changes after resync 2006/10/19 10:59:13 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:12:23 aw 1.1.2.3: #i39532# 2006/06/02 14:17:48 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:39:10 aw 1.1.2.1: code for primitive support

---
+++ svx/source/sdr/contact/viewcontactofe3dlathe.cxx
@@ -0,0 +1,132 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewcontactofe3dlathe.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <svx/sdr/contact/viewcontactofe3dlathe.hxx>
+#include <svx/lathe3d.hxx>
+#include <drawinglayer/primitive3d/sdrlatheprimitive3d.hxx>
+#include <svx/sdr/primitive2d/sdrattributecreator.hxx>
+#include <svx/sdr/primitive3d/sdrattributecreator3d.hxx>
+#include <basegfx/polygon/b2dpolygontools.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        ViewContactOfE3dLathe::ViewContactOfE3dLathe(E3dLatheObj& rLathe)
+        :   ViewContactOfE3d(rLathe)
+        {
+        }
+
+        ViewContactOfE3dLathe::~ViewContactOfE3dLathe()
+        {
+        }
+
+        drawinglayer::primitive3d::Primitive3DSequence ViewContactOfE3dLathe::createViewIndependentPrimitive3DSequence() const
+        {
+            drawinglayer::primitive3d::Primitive3DSequence xRetval;
+            const SfxItemSet& rItemSet = GetE3dLatheObj().GetMergedItemSet();
+            drawinglayer::attribute::SdrLineFillShadowAttribute* pAttribute = drawinglayer::primitive2d::createNewSdrLineFillShadowAttribute(rItemSet, false);
+
+            // for 3D Objects, always create a primitive even when not visible. This is necessary ATM
+            // since e.g. chart geometries rely on the occupied space of non-visible objects
+            if(!pAttribute)
+            {
+                pAttribute = new drawinglayer::attribute::SdrLineFillShadowAttribute(
+                    impCreateFallbackLineAttribute(basegfx::BColor(0.0, 1.0, 0.0)),
+                    0, 0, 0, 0);
+            }
+
+            // get extrude geometry
+            basegfx::B3DHomMatrix aWorldTransform;
+
+            // get polygon (2d)
+            const basegfx::B2DPolyPolygon aPolyPolygon(GetE3dLatheObj().GetPolyPoly2D());
+
+            // add object to world transformation
+            aWorldTransform *= GetE3dLatheObj().GetTransform();
+
+            // get 3D Object Attributes
+            drawinglayer::attribute::Sdr3DObjectAttribute* pSdr3DObjectAttribute = drawinglayer::primitive2d::createNewSdr3DObjectAttribute(rItemSet);
+
+            // calculate texture size. Use the polygon length of the longest polygon for
+            // height and the rotated radius for width (using polygon center) to get a good
+            // texture mapping
+            const sal_uInt32 nPolygonCount(aPolyPolygon.count());
+            double fPolygonMaxLength(0.0);
+
+            for(sal_uInt32 a(0); a < nPolygonCount; a++)
+            {
+                const basegfx::B2DPolygon aCandidate(aPolyPolygon.getB2DPolygon(a));
+                const double fPolygonLength(basegfx::tools::getLength(aCandidate));
+                fPolygonMaxLength = std::max(fPolygonMaxLength, fPolygonLength);
+            }
+
+            const basegfx::B2DRange aPolyPolygonRange(basegfx::tools::getRange(aPolyPolygon));
+            const basegfx::B2DVector aTextureSize(
+                F_PI * fabs(aPolyPolygonRange.getCenter().getX()), // PI * d
+                fPolygonMaxLength);
+
+            // get more data
+            const sal_uInt32 nHorizontalSegments(GetE3dLatheObj().GetHorizontalSegments());
+            const sal_uInt32 nVerticalSegments(GetE3dLatheObj().GetVerticalSegments());
+            const double fDiagonal((double)GetE3dLatheObj().GetPercentDiagonal() / 100.0);
+            const double fBackScale((double)GetE3dLatheObj().GetBackScale() / 100.0);
+            const double fRotation(((double)GetE3dLatheObj().GetEndAngle() / 1800.0) * F_PI);
+            const bool bSmoothNormals(GetE3dLatheObj().GetSmoothNormals()); // Plane itself
+            const bool bSmoothLids(GetE3dLatheObj().GetSmoothLids()); // Front/back
+            const bool bCharacterMode(GetE3dLatheObj().GetCharacterMode());
+            const bool bCloseFront(GetE3dLatheObj().GetCloseFront());
+            const bool bCloseBack(GetE3dLatheObj().GetCloseBack());
+
+            // create primitive and add
+            const drawinglayer::primitive3d::Primitive3DReference xReference(new drawinglayer::primitive3d::SdrLathePrimitive3D(
+                aWorldTransform, aTextureSize, *pAttribute, *pSdr3DObjectAttribute,
+                aPolyPolygon, nHorizontalSegments, nVerticalSegments,
+                fDiagonal, fBackScale, fRotation,
+                bSmoothNormals, true, bSmoothLids, bCharacterMode, bCloseFront, bCloseBack));
+            xRetval = drawinglayer::primitive3d::Primitive3DSequence(&xReference, 1);
+
+            // delete 3D Object Attributes
+            delete pSdr3DObjectAttribute;
+            delete pAttribute;
+
+            return xRetval;
+        }
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
