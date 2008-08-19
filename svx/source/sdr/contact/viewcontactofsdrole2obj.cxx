dcc77796859d3aec76952a70a6f23c8f2910cb79
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:09:02 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:28 cl 1.1.2.13: fixed license files 2008/06/03 13:41:11 aw 1.1.2.12: corrections 2008/05/27 14:49:58 aw 1.1.2.11: #i39532# changes DEV300 m12 resync corrections 2008/02/26 08:40:52 aw 1.1.2.10: removals, OLE, optimizations for primitives 2008/01/22 12:29:30 aw 1.1.2.9: adaptions and 1st stripping 2007/08/13 15:34:33 aw 1.1.2.8: #i39532# changes after resync 2007/07/06 13:43:07 aw 1.1.2.7: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/03/06 12:39:27 aw 1.1.2.6: #i39532# 2007/01/23 10:46:31 aw 1.1.2.5: changes after resync 2006/10/19 10:59:15 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:12:25 aw 1.1.2.3: #i39532# 2006/06/02 14:17:50 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:40:24 aw 1.1.2.1: code for primitive support

---
+++ svx/source/sdr/contact/viewcontactofsdrole2obj.cxx
@@ -0,0 +1,193 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewcontactofsdrole2obj.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <svx/sdr/contact/viewcontactofsdrole2obj.hxx>
+#include <svx/svdoole2.hxx>
+#include <svx/sdr/attribute/sdrallattribute.hxx>
+#include <svx/sdr/contact/viewobjectcontactofsdrole2obj.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+#include <svx/sdr/primitive2d/sdrole2primitive2d.hxx>
+#include <drawinglayer/primitive2d/graphicprimitive2d.hxx>
+#include <basegfx/polygon/b2dpolygontools.hxx>
+#include <drawinglayer/primitive2d/polygonprimitive2d.hxx>
+#include <drawinglayer/primitive2d/bitmapprimitive2d.hxx>
+#include <svtools/colorcfg.hxx>
+#include <svx/sdr/primitive2d/sdrattributecreator.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        // Create a Object-Specific ViewObjectContact, set ViewContact and
+        // ObjectContact. Always needs to return something.
+        ViewObjectContact& ViewContactOfSdrOle2Obj::CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact)
+        {
+            ViewObjectContact* pRetval = new ViewObjectContactOfSdrOle2Obj(rObjectContact, *this);
+            DBG_ASSERT(pRetval, "ViewContact::CreateObjectSpecificViewObjectContact() failed (!)");
+
+            return *pRetval;
+        }
+
+        ViewContactOfSdrOle2Obj::ViewContactOfSdrOle2Obj(SdrOle2Obj& rOle2Obj)
+        :   ViewContactOfSdrRectObj(rOle2Obj)
+        {
+        }
+
+        ViewContactOfSdrOle2Obj::~ViewContactOfSdrOle2Obj()
+        {
+        }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewContactOfSdrOle2Obj::createPrimitive2DSequenceWithGivenGraphic(const Graphic& rOLEGraphic, bool bScaleContent) const
+        {
+            drawinglayer::primitive2d::Primitive2DSequence xRetval;
+            SdrText* pSdrText = GetOle2Obj().getText(0);
+
+            if(pSdrText)
+            {
+                // take unrotated snap rect (direct model data) for position and size
+                const Rectangle& rRectangle = GetOle2Obj().GetGeoRect();
+                const basegfx::B2DRange aObjectRange(rRectangle.Left(), rRectangle.Top(), rRectangle.Right(), rRectangle.Bottom());
+
+                // create object matrix
+                const GeoStat& rGeoStat(GetOle2Obj().GetGeoStat());
+                const double fShearX(rGeoStat.nShearWink ? tan((36000 - rGeoStat.nShearWink) * F_PI18000) : 0.0);
+                const double fRotate(rGeoStat.nDrehWink ? (36000 - rGeoStat.nDrehWink) * F_PI18000 : 0.0);
+                basegfx::B2DHomMatrix aObjectMatrix;
+
+                aObjectMatrix.scale(aObjectRange.getWidth(), aObjectRange.getHeight());
+                aObjectMatrix.shearX(fShearX);
+                aObjectMatrix.rotate(fRotate);
+                aObjectMatrix.translate(aObjectRange.getMinX(), aObjectRange.getMinY());
+
+                // Prepare attribute settings, will be used soon anyways
+                const SfxItemSet& rItemSet = GetOle2Obj().GetMergedItemSet();
+                drawinglayer::attribute::SdrLineFillShadowTextAttribute* pAttribute = drawinglayer::primitive2d::createNewSdrLineFillShadowTextAttribute(rItemSet, *pSdrText);
+
+                if(!pAttribute)
+                {
+                    // force existence, even when not visible
+                    pAttribute = new drawinglayer::attribute::SdrLineFillShadowTextAttribute(0, 0, 0, 0, 0, 0);
+                }
+
+                // Prepare OLE filling. This is normally the metafile describing OLE content, but may also
+                // be the empty OLE bitmap for empty/not loaded OLEs
+                const GraphicObject aGraphicObject(rOLEGraphic);
+                const GraphicAttr aGraphicAttr;
+                drawinglayer::primitive2d::Primitive2DSequence xOLEContent;
+
+                if(bScaleContent)
+                {
+                    // Create outline and placeholder graphic with some scaling
+                    const Size aPrefSize(rOLEGraphic.GetPrefSize());
+                    const double fOffsetX((aObjectRange.getWidth() - aPrefSize.getWidth()) / 2.0);
+                    const double fOffsetY((aObjectRange.getHeight() - aPrefSize.getHeight()) / 2.0);
+
+                    if(basegfx::fTools::moreOrEqual(fOffsetX, 0.0) && basegfx::fTools::moreOrEqual(fOffsetY, 0.0))
+                    {
+                        // if content fits into frame, create it
+                        basegfx::B2DHomMatrix aInnerObjectMatrix;
+
+                        aInnerObjectMatrix.scale(aPrefSize.getWidth(), aPrefSize.getHeight());
+                        aInnerObjectMatrix.translate(fOffsetX, fOffsetY);
+                        aInnerObjectMatrix.shearX(fShearX);
+                        aInnerObjectMatrix.rotate(fRotate);
+                        aInnerObjectMatrix.translate(aObjectRange.getMinX(), aObjectRange.getMinY());
+
+                        drawinglayer::primitive2d::Primitive2DReference xScaledContent(
+                            new drawinglayer::primitive2d::GraphicPrimitive2D(aInnerObjectMatrix, aGraphicObject, aGraphicAttr));
+                        xOLEContent = drawinglayer::primitive2d::Primitive2DSequence(&xScaledContent, 1);
+                    }
+                }
+                else
+                {
+                    // create graphic primitive for content
+                    drawinglayer::primitive2d::Primitive2DReference xDirectContent(
+                        new drawinglayer::primitive2d::GraphicPrimitive2D(aObjectMatrix, aGraphicObject, aGraphicAttr));
+                    xOLEContent = drawinglayer::primitive2d::Primitive2DSequence(&xDirectContent, 1);
+                }
+
+                // create primitive. Use Ole2 primitive here. Prepare attribute settings, will be used soon anyways.
+                const drawinglayer::primitive2d::Primitive2DReference xReference(new drawinglayer::primitive2d::SdrOle2Primitive2D(
+                    xOLEContent,
+                    aObjectMatrix,
+                    *pAttribute));
+                xRetval = drawinglayer::primitive2d::Primitive2DSequence(&xReference, 1);
+                delete pAttribute;
+
+                // a standard gray outline is created for scaled content
+                if(bScaleContent)
+                {
+                    const svtools::ColorConfig aColorConfig;
+                    const svtools::ColorConfigValue aColor(aColorConfig.GetColorValue(svtools::OBJECTBOUNDARIES));
+
+                    if(aColor.bIsVisible)
+                    {
+                        basegfx::B2DPolygon aOutline(basegfx::tools::createPolygonFromRect(basegfx::B2DRange(0, 0, 1, 1)));
+                        const Color aVclColor(aColor.nColor);
+                        aOutline.transform(aObjectMatrix);
+                        const drawinglayer::primitive2d::Primitive2DReference xOutline(
+                            new drawinglayer::primitive2d::PolygonHairlinePrimitive2D(aOutline, aVclColor.getBColor()));
+                        drawinglayer::primitive2d::appendPrimitive2DReferenceToPrimitive2DSequence(xRetval, xOutline);
+                    }
+                }
+            }
+
+            return xRetval;
+        }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewContactOfSdrOle2Obj::createViewIndependentPrimitive2DSequence() const
+        {
+            const Graphic* pOLEGraphic = GetOle2Obj().GetGraphic();
+
+            if(pOLEGraphic)
+            {
+                // there is a graphic set, use it
+                return createPrimitive2DSequenceWithGivenGraphic(*pOLEGraphic, GetOle2Obj().IsEmptyPresObj());
+            }
+            else
+            {
+                // no graphic, use default empty OLE bitmap
+                const Bitmap aEmptyOLEBitmap(GetOle2Obj().GetEmtyOLEReplacementBitmap());
+                const Graphic aEmtyOLEGraphic(aEmptyOLEBitmap);
+
+                return createPrimitive2DSequenceWithGivenGraphic(aEmtyOLEGraphic, true);
+            }
+        }
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
