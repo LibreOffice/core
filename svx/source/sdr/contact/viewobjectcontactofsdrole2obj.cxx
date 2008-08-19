4e67b877b35d127de848085afd9d6a6112d22567
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:11:56 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:29 cl 1.1.2.12: fixed license files 2008/06/03 13:41:12 aw 1.1.2.11: corrections 2008/05/27 14:49:58 aw 1.1.2.10: #i39532# changes DEV300 m12 resync corrections 2008/03/13 08:22:28 aw 1.1.2.9: #i39532# diverse support for chart2 2008/03/06 07:19:10 aw 1.1.2.8: 3i39532# added OLE shading support for OLE Icon and direct embedded modes to visualize outplace edited OLEs 2008/03/05 07:57:32 aw 1.1.2.7: #i39532# prettyprinter support 2008/02/26 08:40:52 aw 1.1.2.6: removals, OLE, optimizations for primitives 2008/02/12 10:42:35 aw 1.1.2.5: CVS update from internal development GIT 2008/01/22 12:29:30 aw 1.1.2.4: adaptions and 1st stripping 2007/01/23 10:46:32 aw 1.1.2.3: changes after resync 2006/08/09 17:12:26 aw 1.1.2.2: #i39532# 2006/05/12 12:41:26 aw 1.1.2.1: code for primitive support

---
+++ svx/source/sdr/contact/viewobjectcontactofsdrole2obj.cxx
@@ -0,0 +1,199 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewobjectcontactofsdrole2obj.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <svx/sdr/contact/viewobjectcontactofsdrole2obj.hxx>
+#include <svx/sdr/contact/viewobjectcontactofsdrobj.hxx>
+#include <svx/sdr/contact/viewcontactofsdrole2obj.hxx>
+#include <svx/svdoole2.hxx>
+#include <svx/sdr/contact/objectcontact.hxx>
+#include <drawinglayer/primitive2d/chartprimitive2d.hxx>
+#include <drawinglayer/attribute/fillattribute.hxx>
+#include <drawinglayer/primitive2d/polypolygonprimitive2d.hxx>
+#include <basegfx/polygon/b2dpolygontools.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace com::sun::star;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        const SdrOle2Obj& ViewObjectContactOfSdrOle2Obj::getSdrOle2Object() const
+        {
+            return static_cast< ViewContactOfSdrOle2Obj& >(GetViewContact()).GetOle2Obj();
+        }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewObjectContactOfSdrOle2Obj::createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const
+        {
+            // this method is overloaded to do some things the old SdrOle2Obj::DoPaintObject did.
+            // In the future, some of these may be solved different, but ATM try to stay compatible
+            // with the old behaviour
+            drawinglayer::primitive2d::Primitive2DSequence xRetval;
+            const SdrOle2Obj& rSdrOle2 = getSdrOle2Object();
+            const bool bIsActive(rSdrOle2.executeOldDoPaintPreparations(GetObjectContact().TryToGetSdrPageView()));
+            const Rectangle& rObjectRectangle(rSdrOle2.GetGeoRect());
+            const basegfx::B2DRange aObjectRange(rObjectRectangle.Left(), rObjectRectangle.Top(), rObjectRectangle.Right(), rObjectRectangle.Bottom());
+
+            // create object transform
+            basegfx::B2DHomMatrix aObjectTransform;
+            aObjectTransform.set(0, 0, aObjectRange.getWidth());
+            aObjectTransform.set(1, 1, aObjectRange.getHeight());
+            aObjectTransform.set(0, 2, aObjectRange.getMinX());
+            aObjectTransform.set(1, 2, aObjectRange.getMinY());
+
+            if(GetObjectContact().isDrawModeHighContrast())
+            {
+                // directly call at the corresponding VC and force OLE Graphic to HighContrast
+                const ViewContactOfSdrOle2Obj& rVC = static_cast< const ViewContactOfSdrOle2Obj& >(GetViewContact());
+                Graphic* pOLEHighContrastGraphic = rSdrOle2.getEmbeddedObjectRef().GetHCGraphic();
+
+                if(pOLEHighContrastGraphic)
+                {
+                    // there is a graphic set, use it
+                    xRetval = rVC.createPrimitive2DSequenceWithGivenGraphic(*pOLEHighContrastGraphic, rSdrOle2.IsEmptyPresObj());
+                }
+                else
+                {
+                    // no HighContrast graphic, use default empty OLE bitmap
+                    const Bitmap aEmptyOLEBitmap(rSdrOle2.GetEmtyOLEReplacementBitmap());
+                    const Graphic aEmtyOLEGraphic(aEmptyOLEBitmap);
+
+                    xRetval = rVC.createPrimitive2DSequenceWithGivenGraphic(aEmtyOLEGraphic, true);
+                }
+            }
+            else
+            {
+                // call parent which will use the regular createViewIndependentPrimitive2DSequence
+                // at the corresponding VC
+                xRetval = ViewObjectContactOfSdrObj::createPrimitive2DSequence(rDisplayInfo);
+            }
+
+            if(rSdrOle2.getEmbeddedObjectRef().IsChart())
+            {
+                // for chart, to not lose the current better quality visualisation which
+                // uses a direct paint, use a primtive wrapper for that exceptional case. The renderers
+                // will then ATM paint it to an OutputDevice directly.
+                // In later versions this should be replaced by getting the Primitive2DSequnce from
+                // the chart and using it.
+                // to be able to render something in non-VCL using renderers, the wrapper is a
+                // GroupPrimitive2D which automatically decomposes to the already created Metafile
+                // content.
+                // For being completely compatible, ATM Window and VDEV PrettyPrinting is suppressed.
+                // It works in the VCL renderers, though. So for activating again with VCL primitive
+                // renderers, change conditions here.
+
+                // determine if embedding and PrettyPrinting shall be done at all
+                uno::Reference< frame::XModel > xChartModel;
+                bool bDoChartPrettyPrinting(true);
+                static bool bPrettyPrintingForBitmaps(false);
+
+                // the original ChartPrettyPainter does not do it for Window
+                if(!bPrettyPrintingForBitmaps && bDoChartPrettyPrinting && GetObjectContact().isOutputToWindow())
+                {
+                    bDoChartPrettyPrinting = false;
+                }
+
+                // the original ChartPrettyPainter does not do it for VDEV
+                if(!bPrettyPrintingForBitmaps && bDoChartPrettyPrinting && GetObjectContact().isOutputToVirtualDevice())
+                {
+                    bDoChartPrettyPrinting = false;
+                }
+
+                // the chart model is needed. Check if it's available
+                if(bDoChartPrettyPrinting)
+                {
+                    // get chart model
+                    xChartModel = rSdrOle2.getXModel();
+
+                    if(!xChartModel.is())
+                    {
+                        bDoChartPrettyPrinting = false;
+                    }
+                }
+
+                if(bDoChartPrettyPrinting)
+                {
+                    // embed MetaFile data in a specialized Wrapper Primitive which holds also the ChartModel needed
+                    // for PrettyPrinting
+                    const drawinglayer::primitive2d::Primitive2DReference xReference(new drawinglayer::primitive2d::ChartPrimitive2D(
+                        xChartModel, aObjectTransform, xRetval));
+                    xRetval = drawinglayer::primitive2d::Primitive2DSequence(&xReference, 1);
+                }
+            }
+
+            if(bIsActive)
+            {
+                // do not shade when printing or PDF exporting
+                if(!GetObjectContact().isOutputToPrinter() && !GetObjectContact().isOutputToRecordingMetaFile())
+                {
+                    // shade the representation if the object is activated outplace
+                    basegfx::B2DPolygon aObjectOutline(basegfx::tools::createPolygonFromRect(basegfx::B2DRange(0.0, 0.0, 1.0, 1.0)));
+                    aObjectOutline.transform(aObjectTransform);
+
+                    // Use a FillHatchPrimitive2D with necessary attributes
+                    const drawinglayer::attribute::FillHatchAttribute aFillHatch(
+                        drawinglayer::attribute::HATCHSTYLE_SINGLE, // single hatch
+                        125.0, // 1.25 mm
+                        45.0 * F_PI180, // 45 degree diagonal
+                        Color(COL_BLACK).getBColor(), // black color
+                        false); // no filling
+
+                    const drawinglayer::primitive2d::Primitive2DReference xReference(new drawinglayer::primitive2d::PolyPolygonHatchPrimitive2D(
+                        basegfx::B2DPolyPolygon(aObjectOutline),
+                        Color(COL_BLACK).getBColor(),
+                        aFillHatch));
+
+                    drawinglayer::primitive2d::appendPrimitive2DReferenceToPrimitive2DSequence(xRetval, xReference);
+                }
+            }
+
+            return xRetval;
+        }
+
+        ViewObjectContactOfSdrOle2Obj::ViewObjectContactOfSdrOle2Obj(ObjectContact& rObjectContact, ViewContact& rViewContact)
+        :   ViewObjectContactOfSdrObj(rObjectContact, rViewContact)
+        {
+        }
+
+        ViewObjectContactOfSdrOle2Obj::~ViewObjectContactOfSdrOle2Obj()
+        {
+        }
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
