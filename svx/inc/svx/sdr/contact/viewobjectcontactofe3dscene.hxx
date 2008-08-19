c0490deb229f862f0043bd8433a0c5b8644368db
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:42:13 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:26 cl 1.1.2.12: fixed license files 2008/07/10 12:58:59 aw 1.1.2.11: #i39532# XOutputDevice removed, PrepareDelete removed 2008/06/10 09:31:31 aw 1.1.2.10: #i39532# changed 3d primitive stuff to use viewinformation3d 2008/05/27 14:48:06 aw 1.1.2.9: #i39532# changes DEV300 m12 resync corrections 2008/03/05 07:57:16 aw 1.1.2.8: #i39532# prettyprinter support 2008/01/22 12:29:06 aw 1.1.2.7: adaptions and 1st stripping 2007/07/06 13:42:27 aw 1.1.2.6: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/01/25 12:44:06 aw 1.1.2.5: #i39532# change 3D scene content generation to be usable from API implementation, too 2006/12/12 10:13:52 aw 1.1.2.4: #i39532# changes after resync 2006/11/28 11:17:46 aw 1.1.2.3: #i39532# 2006/10/19 10:53:46 aw 1.1.2.2: #i39532# primitive 2006/08/09 17:04:47 aw 1.1.2.1: #i39532#

---
+++ svx/inc/svx/sdr/contact/viewobjectcontactofe3dscene.hxx
@@ -0,0 +1,66 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewobjectcontactofe3dscene.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWOBJECTCONTACTOFE3DSCENE_HXX
+#define _SDR_CONTACT_VIEWOBJECTCONTACTOFE3DSCENE_HXX
+
+#include <svx/sdr/contact/viewobjectcontactofsdrobj.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfE3dScene : public ViewObjectContactOfSdrObj
+        {
+        protected:
+            // 2d primitive creator
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            // basic constructor.
+            ViewObjectContactOfE3dScene(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfE3dScene();
+
+            // process this primitive: Eventually also recursively travel an existing hierarchy,
+            // e.g. for group objects, scenes or pages. This method will test geometrical visibility.
+            virtual drawinglayer::primitive2d::Primitive2DSequence getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWOBJECTCONTACTOFE3DSCENE_HXX
+
+// eof
