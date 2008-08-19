8a3b569dff246514884032e1f8d10f878e931fe4
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:43:31 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:26 cl 1.1.2.10: fixed license files 2008/05/27 14:48:07 aw 1.1.2.9: #i39532# changes DEV300 m12 resync corrections 2008/01/29 10:27:14 aw 1.1.2.8: updated refresh for ActionChanged(), diverse removals 2008/01/22 12:29:07 aw 1.1.2.7: adaptions and 1st stripping 2007/07/06 13:42:27 aw 1.1.2.6: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2006/11/28 11:17:46 aw 1.1.2.5: #i39532# 2006/10/19 10:53:47 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:05:52 aw 1.1.2.3: #i39532# 2006/06/02 14:17:45 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:33:35 aw 1.1.2.1: code for primitive support

---
+++ svx/inc/svx/sdr/contact/viewobjectcontactofsdrpage.hxx
@@ -0,0 +1,250 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewobjectcontactofsdrpage.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWOBJECTCONTACTOFSDRPAGE_HXX
+#define _SDR_CONTACT_VIEWOBJECTCONTACTOFSDRPAGE_HXX
+
+#include <svx/sdr/contact/viewobjectcontact.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class SdrPage;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageSubObject : public ViewObjectContact
+        {
+        protected:
+            const SdrPage& getPage() const;
+
+        public:
+            ViewObjectContactOfPageSubObject(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageSubObject();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+            virtual bool isPrimitiveGhosted(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageBackground : public ViewObjectContactOfPageSubObject
+        {
+        protected:
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            ViewObjectContactOfPageBackground(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageBackground();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfMasterPage : public ViewObjectContactOfPageSubObject
+        {
+        protected:
+        public:
+            ViewObjectContactOfMasterPage(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfMasterPage();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageFill : public ViewObjectContactOfPageSubObject
+        {
+        protected:
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            ViewObjectContactOfPageFill(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageFill();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageShadow : public ViewObjectContactOfPageSubObject
+        {
+        public:
+            ViewObjectContactOfPageShadow(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageShadow();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfOuterPageBorder : public ViewObjectContactOfPageSubObject
+        {
+        public:
+            ViewObjectContactOfOuterPageBorder(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfOuterPageBorder();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfInnerPageBorder : public ViewObjectContactOfPageSubObject
+        {
+        public:
+            ViewObjectContactOfInnerPageBorder(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfInnerPageBorder();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageHierarchy : public ViewObjectContactOfPageSubObject
+        {
+        public:
+            ViewObjectContactOfPageHierarchy(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageHierarchy();
+
+            virtual drawinglayer::primitive2d::Primitive2DSequence getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageGrid : public ViewObjectContactOfPageSubObject
+        {
+        protected:
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            ViewObjectContactOfPageGrid(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageGrid();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfPageHelplines : public ViewObjectContactOfPageSubObject
+        {
+        protected:
+            virtual drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
+
+        public:
+            ViewObjectContactOfPageHelplines(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfPageHelplines();
+
+            virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class SVX_DLLPUBLIC ViewObjectContactOfSdrPage : public ViewObjectContact
+        {
+        public:
+            ViewObjectContactOfSdrPage(ObjectContact& rObjectContact, ViewContact& rViewContact);
+            virtual ~ViewObjectContactOfSdrPage();
+
+            virtual drawinglayer::primitive2d::Primitive2DSequence getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo) const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWOBJECTCONTACTOFSDRPAGE_HXX
+
+// eof
