3d5a6a8d1c5046b441c26a01358116e12038616b
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:17:33 +0000

    INTEGRATION: CWS aw033 (1.6.14); FILE MERGED 2008/05/14 14:05:36 aw 1.6.14.11: RESYNC: (1.15-1.16); FILE MERGED 2008/03/18 07:15:24 aw 1.6.14.10: #i39532# changes after resync 2008/03/14 13:50:21 cl 1.6.14.9: RESYNC: (1.14-1.15); FILE MERGED 2008/01/22 12:29:31 aw 1.6.14.8: adaptions and 1st stripping 2007/11/19 11:26:07 aw 1.6.14.7: #i39532# Lot of changes to make polygon stuff bezier-able 2007/08/09 18:55:39 aw 1.6.14.6: RESYNC: (1.13-1.14); FILE MERGED 2007/03/20 14:17:20 aw 1.6.14.5: RESYNC: (1.12-1.13); FILE MERGED 2006/11/28 19:32:49 aw 1.6.14.4: RESYNC: (1.10-1.12); FILE MERGED 2006/09/26 19:29:51 aw 1.6.14.3: RESYNC: (1.7-1.10); FILE MERGED 2006/05/12 16:03:57 aw 1.6.14.2: RESYNC: (1.6-1.7); FILE MERGED 2006/05/12 12:46:27 aw 1.6.14.1: code changes for primitive support

--- svx/source/svdraw/impgrfll.cxx
+++ svx/source/svdraw/impgrfll.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: impgrfll.cxx,v $
- * $Revision: 1.16 $
+ * $Revision: 1.17 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -30,349 +30,13 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svx.hxx"
-#include <svtools/itemset.hxx>
-#include <vcl/salbtype.hxx>     // FRound
-#include <vcl/virdev.hxx>
-#include <vcl/gdimtf.hxx>
-#include <vcl/metaact.hxx>
+#include <svx/rectenum.hxx>
 #include <vcl/svapp.hxx>
-#include <vcl/graphictools.hxx>
-#include <basegfx/polygon/b2dpolygon.hxx>
-#include <basegfx/matrix/b2dhommatrix.hxx>
-#include <basegfx/range/b2drange.hxx>
-#include <basegfx/polygon/b2dpolypolygontools.hxx>
-
-#include <svx/svdobj.hxx>
-#include <svx/xpoly.hxx>
-#include "xattr.hxx"
-#include "xoutbmp.hxx"
-#include <svx/xoutx.hxx>
-
-#include <svx/xenum.hxx>
-#include <svx/xgrad.hxx>
-#include <svx/xhatch.hxx>
-#include <svx/xflhtit.hxx>
-#include <svx/xbtmpit.hxx>
-#include "svdoimp.hxx"
-#include <svx/svdattr.hxx>
+#include <vcl/outdev.hxx>
+#include <vcl/bitmap.hxx>
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#define ITEMVALUE(ItemSet,Id,Cast)  ((const Cast&)(ItemSet).Get(Id)).GetValue()
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// #100127# Bracket filled output with a comment, if recording a Mtf
-ImpGraphicFill::ImpGraphicFill( const SdrObject&        rObj,
-                                const XOutputDevice&    rXOut,
-                                const SfxItemSet&       rFillItemSet,
-                                bool                    bIsShadow       ) :
-    mrXOut( rXOut ),
-    mbCommentWritten( false )
-{
-    basegfx::B2DPolyPolygon aGeometry(rObj.TakeXorPoly(sal_True));
-    prepare( rXOut, rObj.GetMergedItemSet(), aGeometry, rFillItemSet, bIsShadow );
-}
-
-ImpGraphicFill::ImpGraphicFill( const XOutputDevice& rXOut,
-                                const SfxItemSet& rSet,
-                                basegfx::B2DPolyPolygon& aGeometry,
-                                const SfxItemSet& rFillItemSet,
-                                bool bIsShadow ) :
-    mrXOut( rXOut ),
-    mbCommentWritten( false )
-{
-    prepare( rXOut, rSet, aGeometry, rFillItemSet, bIsShadow );
-}
-
-void ImpGraphicFill::prepare( const XOutputDevice&  rXOut,
-                         const SfxItemSet& rSet,
-                         basegfx::B2DPolyPolygon& aGeometry,
-                         const SfxItemSet& rFillItemSet,
-                         bool bIsShadow     )
-{
-    XFillStyle eFillStyle( ITEMVALUE( rFillItemSet, XATTR_FILLSTYLE, XFillStyleItem ) );
-    XGradient aGradient(((const XFillGradientItem&)rFillItemSet.Get(XATTR_FILLGRADIENT)).GetGradientValue());
-    XHatch aHatch(((const XFillHatchItem&)rFillItemSet.Get(XATTR_FILLHATCH)).GetHatchValue());
-
-    sal_Int32 nDX( ((SdrShadowXDistItem&)(rSet.Get(SDRATTR_SHADOWXDIST))).GetValue() );
-    sal_Int32 nDY( ((SdrShadowYDistItem&)(rSet.Get(SDRATTR_SHADOWYDIST))).GetValue() );
-
-    UINT16 nTransp( ((XFillTransparenceItem&)
-                        (rSet.Get(XATTR_FILLTRANSPARENCE))).GetValue() );
-    XFillFloatTransparenceItem aFillFloatTransparence((XFillFloatTransparenceItem&)rSet.Get(XATTR_FILLFLOATTRANSPARENCE));
-
-    GDIMetaFile* pMtf=NULL;
-    if( ( eFillStyle != XFILL_NONE ) && ( !aFillFloatTransparence.IsEnabled() ) && ( !nTransp ) )
-        pMtf=mrXOut.GetOutDev()->GetConnectMetaFile();
-    if( pMtf != NULL )
-    {
-        // #104686# Prune non-closed polygons from geometry
-        basegfx::B2DPolyPolygon aPolyPoly;
-        sal_uInt32 i;
-        for( i=0L; i<aGeometry.count(); ++i )
-        {
-            const basegfx::B2DPolygon aCandidate(aGeometry.getB2DPolygon(i));
-
-            if(aCandidate.isClosed())
-            {
-                aPolyPoly.append(aCandidate);
-            }
-        }
-
-        // #103692# Offset original geometry for shadows
-        if( bIsShadow && (nDX || nDY) )
-        {
-            // transformation necessary
-            basegfx::B2DHomMatrix aMatrix;
-
-            aMatrix.translate(nDX, nDY);
-            aPolyPoly.transform(aMatrix);
-        }
-
-        SvtGraphicFill::FillType eType(SvtGraphicFill::fillSolid);
-        switch( eFillStyle )
-        {
-            case XFILL_NONE:
-            case XFILL_SOLID:
-                eType = SvtGraphicFill::fillSolid;
-                break;
-
-            case XFILL_GRADIENT:
-                eType = SvtGraphicFill::fillGradient;
-                break;
-
-            case XFILL_HATCH:
-                eType = SvtGraphicFill::fillHatch;
-                break;
-
-            case XFILL_BITMAP:
-                eType = SvtGraphicFill::fillTexture;
-                break;
-
-            default:
-                DBG_ERROR( "ImpGraphicFill::ImpGraphicFill invalid fill type");
-                break;
-        }
-
-        SvtGraphicFill::Transform aTransform;
-        SvtGraphicFill::HatchType eHatch(SvtGraphicFill::hatchSingle);
-        // TODO: Set hatch background color. Do that via multi-texturing
-        switch( aHatch.GetHatchStyle() )
-        {
-            case XHATCH_SINGLE:
-                eHatch = SvtGraphicFill::hatchSingle;
-                break;
-
-            case XHATCH_DOUBLE:
-                eHatch = SvtGraphicFill::hatchDouble;
-                break;
-
-            case XHATCH_TRIPLE:
-                eHatch = SvtGraphicFill::hatchTriple;
-                break;
-
-            default:
-                DBG_ERROR( "ImpGraphicFill::ImpGraphicFill invalid hatch type");
-                break;
-        }
-
-        if( SvtGraphicFill::fillHatch == eType )
-        {
-            // scale
-            aTransform.matrix[0] *= aHatch.GetDistance();
-            aTransform.matrix[4] *= aHatch.GetDistance();
-
-            // rotate
-            aTransform.matrix[0] *= cos( (double) aHatch.GetAngle() );
-            aTransform.matrix[1] *= -sin( (double) aHatch.GetAngle() );
-            aTransform.matrix[3] *= sin( (double) aHatch.GetAngle() );
-            aTransform.matrix[4] *= cos( (double) aHatch.GetAngle() );
-        }
-
-        SvtGraphicFill::GradientType eGrad(SvtGraphicFill::gradientLinear);
-        switch( aGradient.GetGradientStyle() )
-        {
-            case XGRAD_LINEAR:
-            case XGRAD_AXIAL:
-                // TODO: setup transformation
-                eGrad = SvtGraphicFill::gradientLinear;
-                break;
-
-            case XGRAD_RADIAL:
-            case XGRAD_ELLIPTICAL:
-                // TODO: setup transformation
-                eGrad = SvtGraphicFill::gradientRadial;
-                break;
-
-            case XGRAD_SQUARE:
-            case XGRAD_RECT:
-                // TODO: setup transformation
-                eGrad = SvtGraphicFill::gradientRectangular;
-                break;
-
-            default:
-                DBG_ERROR( "ImpGraphicFill::ImpGraphicFill invalid gradient type");
-                break;
-        }
-
-        Graphic aFillGraphic;
-        bool bTile( ITEMVALUE( rFillItemSet, XATTR_FILLBMP_TILE, SfxBoolItem ) );
-        if( SvtGraphicFill::fillTexture == eType )
-        {
-            OutputDevice* pOut = rXOut.GetOutDev();
-
-            if( pOut )
-            {
-                Bitmap          aBitmap(((const XFillBitmapItem&)(rSet.Get(XATTR_FILLBITMAP))).GetBitmapValue().GetBitmap());
-                const basegfx::B2DRange aTempRange(basegfx::tools::getRange(basegfx::tools::adaptiveSubdivideByAngle(aPolyPoly)));
-                const Rectangle aPolyRect(FRound(aTempRange.getMinX()), FRound(aTempRange.getMinY()), FRound(aTempRange.getMaxX()), FRound(aTempRange.getMaxY()));
-                MapMode         aMap( pOut->GetMapMode().GetMapUnit() );
-                Size            aStartOffset;
-                Size            aBmpOutputSize;
-
-                // #104609# Call extracted method from XOutputDevice to
-                // determine bitmap size and offset
-                RECT_POINT      eRectPoint = (RECT_POINT) ITEMVALUE( rSet, XATTR_FILLBMP_POS, SfxEnumItem );
-                USHORT          nOffX = ITEMVALUE( rSet, XATTR_FILLBMP_TILEOFFSETX, SfxUInt16Item );
-                USHORT          nOffY = ITEMVALUE( rSet, XATTR_FILLBMP_TILEOFFSETY, SfxUInt16Item );
-                Size            aPosOffset( ITEMVALUE( rSet, XATTR_FILLBMP_POSOFFSETX, SfxUInt16Item ),
-                                            ITEMVALUE( rSet, XATTR_FILLBMP_POSOFFSETY, SfxUInt16Item ) );
-                BOOL            bStretch = ITEMVALUE( rSet, XATTR_FILLBMP_STRETCH, SfxBoolItem );
-                BOOL            bLogSize = ITEMVALUE( rSet, XATTR_FILLBMP_SIZELOG, SfxBoolItem );
-                Size            aSize( labs( ITEMVALUE( rSet, XATTR_FILLBMP_SIZEX, SfxMetricItem ) ),
-                                       labs( ITEMVALUE( rSet, XATTR_FILLBMP_SIZEY, SfxMetricItem ) ) );
-
-                Size            aBmpRenderSize;
-                Size            aBmpPerCentSize;
-                Size            aBmpSizePixel( aBitmap.GetSizePixel() );
-
-                if( bLogSize )
-                    aBmpRenderSize = aSize;
-                else
-                    aBmpPerCentSize = aSize;
-
-                ImpCalcBmpFillSizes( aStartOffset, aBmpOutputSize, aPolyRect,
-                                     aMap, aBitmap, aBmpRenderSize, aBmpPerCentSize,
-                                     aPosOffset, bLogSize, bTile, bStretch, eRectPoint );
-
-                // avoid reimplementation of tiling and offset calculation
-                // -- simply render our texture into a VDev
-                if( bTile && (nOffX || nOffY) )
-                {
-                    // paint texture to VDev, then extract first 2x2
-                    // tiles (to cope with the offset variations)
-
-                    // setup VDev
-                    VirtualDevice   aVDev;
-
-                    aVDev.SetOutputSizePixel( pOut->LogicToPixel( aPolyRect, aMap ).GetSize() );
-                    aVDev.SetMapMode( aMap );
-
-                    // setup XOutDev
-                    XOutputDevice aXOut( &aVDev );
-                    aXOut.SetFillAttr( rFillItemSet );
-
-                    // prepare ItemSet to avoid line drawing
-                    SfxItemSet aEmptySet( *rFillItemSet.GetPool() );
-                    aEmptySet.Put(XLineStyleItem(XLINE_NONE));
-                    aXOut.SetLineAttr( aEmptySet );
-
-                    // render into VDev (note: cannot paint smaller
-                    // area here, because bitmap size might be
-                    // relative to draw rect)
-                    Rectangle aOutRect( aPolyRect );
-                    aOutRect.SetPos( Point(0,0) );
-                    aXOut.DrawRect( aOutRect );
-
-                    // extract first 2x2 tiles
-                    Size aTileBmpSize( aBmpOutputSize );
-                    aTileBmpSize.Width() *= 2;
-                    aTileBmpSize.Height() *= 2;
-                    Size aTileBmpSizePixel( pOut->LogicToPixel( aTileBmpSize ) );
-                    aFillGraphic = Graphic( aVDev.GetBitmap( Point(), aTileBmpSize ) );
-                    aFillGraphic.SetPrefMapMode( MapMode(MAP_PIXEL) );
-                    aFillGraphic.SetPrefSize( aTileBmpSizePixel );
-
-                    if( aTileBmpSizePixel.Width() == 0 )
-                        aTileBmpSizePixel.Width() = 1;
-                    if( aTileBmpSizePixel.Height() == 0 )
-                        aTileBmpSizePixel.Height() = 1;
-
-                    // setup transformation (scale to logical coordinate system. no need for translation)
-                    aTransform.matrix[0] *= (double)2*aBmpOutputSize.Width() / aTileBmpSizePixel.Width();
-                    aTransform.matrix[4] *= (double)2*aBmpOutputSize.Height() / aTileBmpSizePixel.Height();
-                }
-                else
-                {
-                    // setup fill graphic
-                    aFillGraphic = Graphic( aBitmap );
-                    aFillGraphic.SetPrefMapMode( MapMode(MAP_PIXEL) );
-                    aFillGraphic.SetPrefSize( aBmpSizePixel );
-
-                    if( aBmpSizePixel.Width() == 0 )
-                        aBmpSizePixel.Width() = 1;
-                    if( aBmpSizePixel.Height() == 0 )
-                        aBmpSizePixel.Height() = 1;
-
-                    // setup transformation from size and offset values (scale to logical coordinate system)
-                    aTransform.matrix[0] *= (double)aBmpOutputSize.Width() / aBmpSizePixel.Width();
-                    aTransform.matrix[4] *= (double)aBmpOutputSize.Height() / aBmpSizePixel.Height();
-
-                    // translate
-                    aTransform.matrix[2] += aStartOffset.Width();
-                    aTransform.matrix[5] += aStartOffset.Height();
-                }
-            }
-        }
-
-        const Color aColorSolid = ((const XFillColorItem&) (rFillItemSet.Get(XATTR_FILLCOLOR))).GetColorValue();
-        SvtGraphicFill aFill = SvtGraphicFill( PolyPolygon(aPolyPoly),
-                              aColorSolid,
-                              ITEMVALUE( rFillItemSet, XATTR_FILLTRANSPARENCE, XFillTransparenceItem ) / 100.0,
-                              SvtGraphicFill::fillEvenOdd,
-                              eType,
-                              aTransform,
-                              SvtGraphicFill::fillTexture == eType ? bTile : false,
-                              eHatch,
-                              aHatch.GetColor(),
-                              eGrad,
-                              aGradient.GetStartColor(),
-                              aGradient.GetEndColor(),
-                              0 == aGradient.GetSteps() ? ((int)SvtGraphicFill::gradientStepsInfinite) : aGradient.GetSteps(), // 0 means adaptive/infinite step count
-                              aFillGraphic );
-
-#ifdef DBG_UTIL
-        ::rtl::OString aStr( aFill.toString() );
-#endif
-
-        SvMemoryStream  aMemStm;
-
-        aMemStm << aFill;
-
-        pMtf->AddAction( new MetaCommentAction( "XPATHFILL_SEQ_BEGIN", 0,
-                                                static_cast<const BYTE*>(aMemStm.GetData()),
-
-                                                aMemStm.Seek( STREAM_SEEK_TO_END ) ) );
-        mbCommentWritten = true;
-    }
-}
-
-// #100127# Bracket filled output with a comment, if recording a Mtf
-ImpGraphicFill::~ImpGraphicFill()
-{
-    GDIMetaFile* pMtf=NULL;
-    if( mbCommentWritten )
-        pMtf=mrXOut.GetOutDev()->GetConnectMetaFile();
-    if( pMtf != NULL )
-    {
-        pMtf->AddAction( new MetaCommentAction( "XPATHFILL_SEQ_END" ) );
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-// #104609# Extracted from XOutputDevice::ImpCalcBmpFillStartValues
 void ImpCalcBmpFillSizes( Size&            rStartOffset,
                           Size&            rBmpOutputSize,
                           const Rectangle& rOutputRect,
@@ -435,21 +99,21 @@ void ImpCalcBmpFillSizes( Size&            rStartOffset,
                 rBmpOutputSize = rBmpSize;
 
                 if( !rBmpSize.Width() )
-                    rBmpOutputSize.Width() = FRound( (double) rBmpSize.Height() * aBmpPrefSize.Width() / aBmpPrefSize.Height() );
+                    rBmpOutputSize.Width() = basegfx::fround( (double) rBmpSize.Height() * aBmpPrefSize.Width() / aBmpPrefSize.Height() );
                 else
-                    rBmpOutputSize.Height() = FRound( (double) rBmpSize.Width() * aBmpPrefSize.Height() / aBmpPrefSize.Width() );
+                    rBmpOutputSize.Height() = basegfx::fround( (double) rBmpSize.Width() * aBmpPrefSize.Height() / aBmpPrefSize.Width() );
             }
             else
             {
                 if( !rBmpPerCent.Width() )
                 {
-                    rBmpOutputSize.Height() = FRound( (double) rOutputRect.GetHeight() * rBmpPerCent.Height() / 100. );
-                    rBmpOutputSize.Width() = FRound( (double) rBmpOutputSize.Height() * aBmpPrefSize.Width() / aBmpPrefSize.Height() );
+                    rBmpOutputSize.Height() = basegfx::fround( (double) rOutputRect.GetHeight() * rBmpPerCent.Height() / 100. );
+                    rBmpOutputSize.Width() = basegfx::fround( (double) rBmpOutputSize.Height() * aBmpPrefSize.Width() / aBmpPrefSize.Height() );
                 }
                 else
                 {
-                    rBmpOutputSize.Width() = FRound( (double) rOutputRect.GetWidth() * rBmpPerCent.Width() / 100. );
-                    rBmpOutputSize.Height() = FRound( (double) rBmpOutputSize.Width() * aBmpPrefSize.Height() / aBmpPrefSize.Width() );
+                    rBmpOutputSize.Width() = basegfx::fround( (double) rOutputRect.GetWidth() * rBmpPerCent.Width() / 100. );
+                    rBmpOutputSize.Height() = basegfx::fround( (double) rBmpOutputSize.Width() * aBmpPrefSize.Height() / aBmpPrefSize.Width() );
                 }
             }
         }
@@ -461,8 +125,8 @@ void ImpCalcBmpFillSizes( Size&            rStartOffset,
             rBmpOutputSize = rBmpSize;
         else
         {
-            rBmpOutputSize.Width() = FRound( (double) rOutputRect.GetWidth() * rBmpPerCent.Width() / 100. );
-            rBmpOutputSize.Height() = FRound( (double) rOutputRect.GetHeight() * rBmpPerCent.Height() / 100. );
+            rBmpOutputSize.Width() = basegfx::fround( (double) rOutputRect.GetWidth() * rBmpPerCent.Width() / 100. );
+            rBmpOutputSize.Height() = basegfx::fround( (double) rOutputRect.GetHeight() * rBmpPerCent.Height() / 100. );
         }
     }
 
@@ -589,3 +253,6 @@ void ImpCalcBmpFillSizes( Size&            rStartOffset,
         }
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
