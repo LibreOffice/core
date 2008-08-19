5bb6e488c308074608a7328bb0b54e058a763fd5
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 23:21:58 +0000

    INTEGRATION: CWS aw033 (1.13.44); FILE MERGED 2008/05/14 14:52:18 aw 1.13.44.2: RESYNC: (1.13-1.14); FILE MERGED 2008/01/29 10:34:22 aw 1.13.44.1: updated refresh for ActionChanged(), diverse removals

--- sd/source/ui/view/showview.cxx
+++ sd/source/ui/view/showview.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: showview.cxx,v $
- * $Revision: 1.14 $
+ * $Revision: 1.15 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -154,9 +154,9 @@ void ShowView::InvalidateOneWin (::Window& rWin, const Rectangle& rRect)
 |*
 \************************************************************************/
 
-void ShowView::CompleteRedraw(OutputDevice* pOutDev, const Region& rReg, USHORT, ::sdr::contact::ViewObjectContactRedirector* pRedirector /*=0L*/)
+void ShowView::CompleteRedraw(OutputDevice* pOutDev, const Region& rReg, sdr::contact::ViewObjectContactRedirector* pRedirector /*=0L*/)
 {
-    FmFormView::CompleteRedraw(pOutDev, rReg, SDRPAINTMODE_ANILIKEPRN, pRedirector);
+    FmFormView::CompleteRedraw(pOutDev, rReg, pRedirector);
 }
 
 /*************************************************************************
