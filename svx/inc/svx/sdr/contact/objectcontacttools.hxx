ecc4089019e4f6de174c872f70f28467890311c3
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:36:45 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:25 cl 1.1.2.8: fixed license files 2008/05/27 14:48:06 aw 1.1.2.7: #i39532# changes DEV300 m12 resync corrections 2008/02/07 14:12:59 aw 1.1.2.6: stable version from working GIT repository for unxlngi6 build 2007/12/20 14:50:50 aw 1.1.2.5: #i39532# Added simple test-canvas-renderer 2007/07/27 09:17:11 aw 1.1.2.4: #i39532# 2007/07/09 13:25:12 aw 1.1.2.3: #i39532# adaptions to splitted VCLProcessors for meta and pixel targets 2006/10/19 10:53:42 aw 1.1.2.2: #i39532# primitive 2006/08/09 17:04:38 aw 1.1.2.1: #i39532#

---
+++ svx/inc/svx/sdr/contact/objectcontacttools.hxx
@@ -0,0 +1,67 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: objectcontacttools.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_OBJECTCONTACTTOOLS_HXX
+#define _SDR_CONTACT_OBJECTCONTACTTOOLS_HXX
+
+#include <drawinglayer/geometry/viewinformation2d.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class OutputDevice;
+
+namespace drawinglayer { namespace processor2d {
+    class BaseProcessor2D;
+}}
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        // create a mating VCL-Provessor for given OutputDevice. This includes
+        // looking for MetaFile-recording. The returned renderer changes owner,
+        // deletion is duty of the caller
+        drawinglayer::processor2d::BaseProcessor2D* createBaseProcessor2DFromOutputDevice(
+            OutputDevice& rTargetOutDev,
+            const drawinglayer::geometry::ViewInformation2D& rViewInformation2D,
+            bool bTryToTestCanvas = false);
+
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_OBJECTCONTACTTOOLS_HXX
+
+// eof
