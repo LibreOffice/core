64b83cd554fc0c8cdbd209e93ad5b01d7e1a79f7
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:10:39 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:28 cl 1.1.2.12: fixed license files 2008/06/24 15:42:24 aw 1.1.2.11: #i39532# corrections 2008/06/10 09:31:40 aw 1.1.2.10: #i39532# changed 3d primitive stuff to use viewinformation3d 2008/05/27 14:49:58 aw 1.1.2.9: #i39532# changes DEV300 m12 resync corrections 2008/01/22 12:29:30 aw 1.1.2.8: adaptions and 1st stripping 2007/07/06 13:43:08 aw 1.1.2.7: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/01/25 12:47:05 aw 1.1.2.6: #i39532# change 3D scene content generation to be usable from API implementation, too 2007/01/23 10:46:31 aw 1.1.2.5: changes after resync 2006/11/28 11:17:54 aw 1.1.2.4: #i39532# 2006/11/07 15:52:20 aw 1.1.2.3: #i39532# changed various aspevcts of XPrimitive2D implementations 2006/10/19 10:59:16 aw 1.1.2.2: #i39532# primitive 2006/08/09 17:11:51 aw 1.1.2.1: #i39532#

---
+++ svx/source/sdr/contact/viewobjectcontactofe3d.cxx
@@ -0,0 +1,106 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewobjectcontactofe3d.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <svx/sdr/contact/viewobjectcontactofe3d.hxx>
+#include <svx/sdr/contact/viewcontactofe3d.hxx>
+#include <basegfx/color/bcolor.hxx>
+#include <drawinglayer/primitive3d/modifiedcolorprimitive3d.hxx>
+#include <svx/sdr/contact/viewobjectcontactofe3dscene.hxx>
+#include <drawinglayer/primitive2d/embedded3dprimitive2d.hxx>
+#include <svx/sdr/contact/viewcontactofe3dscene.hxx>
+#include <drawinglayer/attribute/sdrattribute3d.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        ViewObjectContactOfE3d::ViewObjectContactOfE3d(ObjectContact& rObjectContact, ViewContact& rViewContact)
+        :   ViewObjectContactOfSdrObj(rObjectContact, rViewContact)
+        {
+        }
+
+        ViewObjectContactOfE3d::~ViewObjectContactOfE3d()
+        {
+        }
+
+        drawinglayer::primitive3d::Primitive3DSequence ViewObjectContactOfE3d::createPrimitive3DSequence(const DisplayInfo& rDisplayInfo) const
+        {
+            // get the view-independent Primitive from the viewContact
+            const ViewContactOfE3d& rViewContactOfE3d(dynamic_cast< const ViewContactOfE3d& >(GetViewContact()));
+            drawinglayer::primitive3d::Primitive3DSequence xRetval(rViewContactOfE3d.getViewIndependentPrimitive3DSequence());
+
+            // handle ghosted
+            if(isPrimitiveGhosted(rDisplayInfo))
+            {
+                const ::basegfx::BColor aRGBWhite(1.0, 1.0, 1.0);
+                const ::basegfx::BColorModifier aBColorModifier(aRGBWhite, 0.5, ::basegfx::BCOLORMODIFYMODE_INTERPOLATE);
+                const drawinglayer::primitive3d::Primitive3DReference xReference(new drawinglayer::primitive3d::ModifiedColorPrimitive3D(xRetval, aBColorModifier));
+                xRetval = drawinglayer::primitive3d::Primitive3DSequence(&xReference, 1);
+            }
+
+            return xRetval;
+        }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewObjectContactOfE3d::createPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const
+        {
+            const ViewContactOfE3d& rViewContact = static_cast< const ViewContactOfE3d& >(GetViewContact());
+
+            // get 3d primitive vector, isPrimitiveVisible() is done in 3d creator
+            return rViewContact.impCreateWithGivenPrimitive3DSequence(getPrimitive3DSequence(rDisplayInfo));
+        }
+
+        drawinglayer::primitive3d::Primitive3DSequence ViewObjectContactOfE3d::getPrimitive3DSequence(const DisplayInfo& rDisplayInfo) const
+        {
+            drawinglayer::primitive3d::Primitive3DSequence xNewPrimitive3DSeq(createPrimitive3DSequence(rDisplayInfo));
+
+            // local up-to-date checks. New list different from local one?
+            if(!drawinglayer::primitive3d::arePrimitive3DSequencesEqual(mxPrimitive3DSequence, xNewPrimitive3DSeq))
+            {
+                // has changed, copy content
+                const_cast< ViewObjectContactOfE3d* >(this)->mxPrimitive3DSequence = xNewPrimitive3DSeq;
+            }
+
+            // return current Primitive2DSequence
+            return mxPrimitive3DSequence;
+        }
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
