7b3fee07bac63e76e0d46340984ea56dbf2d3a8a
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:23:19 +0000

    INTEGRATION: CWS aw033 (1.7.58); FILE MERGED 2008/07/10 12:58:57 aw 1.7.58.1: #i39532# XOutputDevice removed, PrepareDelete removed

--- svx/inc/dragmt3d.hxx
+++ svx/inc/dragmt3d.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: dragmt3d.hxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -101,7 +101,6 @@ public:
     virtual void Brk();
     virtual FASTBOOL End(FASTBOOL bCopy);
 
-    //virtual void DrawXor(XOutputDevice& rXOut, FASTBOOL bFull) const;
     E3dView& Get3DView()  { return (E3dView&)rView;  }
 
     DECL_LINK(TimerInterruptHdl, void*);
