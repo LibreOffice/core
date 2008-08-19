9d11c9cb1f9f5ce08249fd944cec98589304a402
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:33:31 +0000

    INTEGRATION: CWS aw033 (1.22.58); FILE MERGED 2008/07/10 13:01:00 aw 1.22.58.1: #i39532# XOutputDevice removed, PrepareDelete removed

--- svx/source/xoutdev/xtabcolr.cxx
+++ svx/source/xoutdev/xtabcolr.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xtabcolr.cxx,v $
- * $Revision: 1.22 $
+ * $Revision: 1.23 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -50,7 +50,6 @@
 #include <svx/dialmgr.hxx>
 #include <svx/xtable.hxx>
 #include <svx/xpool.hxx>
-#include <svx/xoutx.hxx>
 
 #define GLOBALOVERFLOW
 
