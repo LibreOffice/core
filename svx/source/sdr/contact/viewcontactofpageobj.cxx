dbf1bd0a72dbfd52709a690b80021872087f0517
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:07:44 +0000

    INTEGRATION: CWS aw033 (1.7.16); FILE MERGED 2008/07/10 13:00:54 aw 1.7.16.12: #i39532# XOutputDevice removed, PrepareDelete removed 2008/06/24 15:42:24 aw 1.7.16.11: #i39532# corrections 2008/05/27 14:49:58 aw 1.7.16.10: #i39532# changes DEV300 m12 resync corrections 2008/05/14 14:00:12 aw 1.7.16.9: RESYNC: (1.14-1.15); FILE MERGED 2008/01/22 12:29:30 aw 1.7.16.8: adaptions and 1st stripping 2007/08/13 15:34:33 aw 1.7.16.7: #i39532# changes after resync 2007/08/09 18:46:43 aw 1.7.16.6: RESYNC: (1.12-1.14); FILE MERGED 2007/03/20 14:15:43 aw 1.7.16.5: RESYNC: (1.11-1.12); FILE MERGED 2007/01/19 13:18:56 aw 1.7.16.4: RESYNC: (1.10-1.11); FILE MERGED 2006/11/28 11:17:54 aw 1.7.16.3: #i39532# 2006/09/26 19:21:20 aw 1.7.16.2: RESYNC: (1.7-1.10); FILE MERGED 2006/05/12 12:46:23 aw 1.7.16.1: code changes for primitive support

--- svx/source/sdr/contact/viewcontactofpageobj.cxx
+++ svx/source/sdr/contact/viewcontactofpageobj.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: viewcontactofpageobj.cxx,v $
- * $Revision: 1.15 $
+ * $Revision: 1.16 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -37,135 +37,10 @@
 #include <svx/svdmodel.hxx>
 #include <svx/svdpage.hxx>
 #include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
-#include <svx/xoutx.hxx>
 #include <svx/sdr/contact/viewobjectcontact.hxx>
-
-//////////////////////////////////////////////////////////////////////////////
-// Own PagePainter which takes over initialisation tasks
-
-namespace sdr
-{
-    namespace contact
-    {
-        class OCOfPageObjPagePainter : public ObjectContactOfPagePainter
-        {
-        protected:
-            Rectangle                               maDestinationRectangle;
-            Point                                   maTranslate;
-            Fraction                                maScaleX;
-            Fraction                                maScaleY;
-            ViewContact&                            mrUserViewContact;
-
-        public:
-            // basic constructor
-            OCOfPageObjPagePainter(const SdrPage* pPage, ViewContact& rUserViewContact);
-
-            // The destructor. When PrepareDelete() was not called before (see there)
-            // warnings will be generated in debug version if there are still contacts
-            // existing.
-            virtual ~OCOfPageObjPagePainter();
-
-            // Own paint
-            sal_Bool PaintIt(DisplayInfo& rDisplayInfo, const Rectangle& rDestinationRectangle);
-
-            // Own reaction on changes
-            virtual void InvalidatePartOfView(const Rectangle& rRectangle) const;
-        };
-
-        //////////////////////////////////////////////////////////////////////////////
-        // Implementation
-
-        OCOfPageObjPagePainter::OCOfPageObjPagePainter(const SdrPage* pPage, ViewContact& rUserViewContact)
-        :   ObjectContactOfPagePainter(pPage),
-            mrUserViewContact(rUserViewContact)
-        {
-            // #i71130# Mark this view as preview renderer
-            mbIsPreviewRenderer = true;
-        }
-
-        OCOfPageObjPagePainter::~OCOfPageObjPagePainter()
-        {
-        }
-
-        sal_Bool OCOfPageObjPagePainter::PaintIt(DisplayInfo& rDisplayInfo, const Rectangle& rDestinationRectangle)
-        {
-            // save old clipping from OutDev
-            sal_Bool bRetval(sal_False);
-
-            OutputDevice* pOut = rDisplayInfo.GetOutputDevice();
-            maDestinationRectangle = rDestinationRectangle;
-
-            if(pOut)
-            {
-                // remap sizes. Destination size is size of rPaintRectangle,
-                // source size is the page size.
-                MapMode aOldMapMode = pOut->GetMapMode();
-
-                // calc translation
-                maTranslate = Point(maDestinationRectangle.TopLeft() + aOldMapMode.GetOrigin());
-
-                // calc scaling
-                maScaleX = Fraction(maDestinationRectangle.GetWidth(), mpStartPage->GetWdt());
-                maScaleY = Fraction(maDestinationRectangle.GetHeight(), mpStartPage->GetHgt());
-
-                // save clipping
-                sal_Bool bRememberedClipping(pOut->IsClipRegion());
-                Region aRememberedClipping;
-
-                if(bRememberedClipping)
-                {
-                    aRememberedClipping = pOut->GetClipRegion();
-                }
-
-                // add clipping against object bounds
-                pOut->IntersectClipRegion(maDestinationRectangle);
-
-                // change origin
-                MapMode aNewMapMode(pOut->GetMapMode());
-                aNewMapMode.SetOrigin(maTranslate);
-                pOut->SetMapMode(aNewMapMode);
-
-                // change scaling
-                Point aEmptyPoint;
-                pOut->SetMapMode(MapMode(MAP_RELATIVE, aEmptyPoint, maScaleX, maScaleY));
-
-                // create copy of DisplayInfo. Do copy PageView to have it available at
-                // included page painting.
-                DisplayInfo aDisplayInfo(rDisplayInfo.GetPageView());
-                aDisplayInfo.SetExtendedOutputDevice(rDisplayInfo.GetExtendedOutputDevice());
-                aDisplayInfo.SetPaintInfoRec(rDisplayInfo.GetPaintInfoRec());
-                aDisplayInfo.SetOutputDevice(pOut);
-
-                // make MasterPages visible
-                aDisplayInfo.SetPagePainting(sal_True); // #i72889#
-
-                // do processing
-                ProcessDisplay(aDisplayInfo);
-
-                // restore original MapMode
-                pOut->SetMapMode(aOldMapMode);
-
-                // restore remembered clipping
-                if(bRememberedClipping)
-                {
-                    pOut->SetClipRegion(aRememberedClipping);
-                }
-                else
-                {
-                    pOut->SetClipRegion();
-                }
-            }
-
-            return bRetval;
-        }
-
-        void OCOfPageObjPagePainter::InvalidatePartOfView(const Rectangle& /*rRectangle*/) const
-        {
-            // call user change
-            mrUserViewContact.ActionChanged();
-        }
-    } // end of namespace contact
-} // end of namespace sdr
+#include <svx/sdr/contact/viewobjectcontactofpageobj.hxx>
+#include <basegfx/polygon/b2dpolygontools.hxx>
+#include <drawinglayer/primitive2d/polygonprimitive2d.hxx>
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -173,206 +48,59 @@ namespace sdr
 {
     namespace contact
     {
+        ViewObjectContact& ViewContactOfPageObj::CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact)
+        {
+            ViewObjectContact* pRetval = new ViewObjectContactOfPageObj(rObjectContact, *this);
+            return *pRetval;
+        }
+
         // Access to referenced page
         const SdrPage* ViewContactOfPageObj::GetReferencedPage() const
         {
             return GetPageObj().GetReferencedPage();
         }
 
-        // method to recalculate the PaintRectangle if the validity flag shows that
-        // it is invalid. The flag is set from GetPaintRectangle, thus the implementation
-        // only needs to refresh maPaintRectangle itself.
-        void ViewContactOfPageObj::CalcPaintRectangle()
-        {
-            maPaintRectangle = GetPageObj().GetCurrentBoundRect();
-        }
-
-        // get rid of evtl. remembered PagePainter
-        void ViewContactOfPageObj::GetRidOfPagePainter()
-        {
-            if(mpPagePainter)
-            {
-                mpPagePainter->PrepareDelete();
-                delete mpPagePainter;
-                mpPagePainter = 0L;
-            }
-        }
-
-        // Prepare a PagePainter for current referenced page. This may
-        // refresh, create or delete a PagePainter instance in
-        // mpPagePainter
-        void ViewContactOfPageObj::PreparePagePainter(const SdrPage* pPage)
-        {
-            if(pPage)
-            {
-                if(mpPagePainter)
-                {
-                    mpPagePainter->SetStartPage(pPage);
-                }
-                else
-                {
-                    mpPagePainter = new OCOfPageObjPagePainter(pPage, *this);
-                }
-            }
-            else
-            {
-                GetRidOfPagePainter();
-            }
-        }
-
-        Rectangle ViewContactOfPageObj::GetPageRectangle (void)
-        {
-            CalcPaintRectangle();
-            return maPaintRectangle;
-        }
-
-        // Paint support methods for page content painting
-        sal_Bool ViewContactOfPageObj::PaintPageContents(
-            DisplayInfo& rDisplayInfo,
-            const Rectangle& rPaintRectangle,
-            const ViewObjectContact& /*rAssociatedVOC*/)
-        {
-            // Prepare page painter
-            sal_Bool bRetval(sal_False);
-            const SdrPage* pPage = GetReferencedPage();
-            PreparePagePainter(pPage);
-
-            if(mpPagePainter)
-            {
-                bRetval = mpPagePainter->PaintIt(rDisplayInfo, rPaintRectangle);
-            }
-
-            return bRetval;
-        }
-
-        sal_Bool ViewContactOfPageObj::PaintPageReplacement(
-            DisplayInfo& rDisplayInfo,
-            const Rectangle& rPaintRectangle,
-            const ViewObjectContact& /*rAssociatedVOC*/)
-        {
-            // If painting recursive, paint a replacement visualization
-            OutputDevice* pOut = rDisplayInfo.GetOutputDevice();
-
-            svtools::ColorConfigValue aDocColor(
-                rDisplayInfo.GetColorConfig().GetColorValue(svtools::DOCCOLOR));
-            svtools::ColorConfigValue aBorderColor(
-                rDisplayInfo.GetColorConfig().GetColorValue(svtools::DOCBOUNDARIES));
-
-            pOut->SetFillColor(aDocColor.nColor);
-            pOut->SetLineColor(aBorderColor.bIsVisible ? aBorderColor.nColor: aDocColor.nColor);
-
-            pOut->DrawRect(rPaintRectangle);
-
-            return sal_True;
-        }
-
-        sal_Bool ViewContactOfPageObj::PaintPageBorder(
-            DisplayInfo& rDisplayInfo,
-            const Rectangle& rPaintRectangle,
-            const ViewObjectContact& /*rAssociatedVOC*/)
-        {
-            OutputDevice* pOut = rDisplayInfo.GetOutputDevice();
-            sal_Bool bRetval(sal_False);
-            svtools::ColorConfigValue aFrameColor(
-                rDisplayInfo.GetColorConfig().GetColorValue(svtools::OBJECTBOUNDARIES));
-
-            if( aFrameColor.bIsVisible )
-            {
-                pOut->SetFillColor();
-                pOut->SetLineColor(aFrameColor.nColor);
-                pOut->DrawRect(rPaintRectangle);
-
-                bRetval = sal_True;
-            }
-
-            return bRetval;
-        }
-
-        // On StopGettingViewed the PagePainter can be dismissed.
-        void ViewContactOfPageObj::StopGettingViewed()
-        {
-            // call parent
-            ViewContactOfSdrObj::StopGettingViewed();
-
-            // dismiss PagePainter
-            GetRidOfPagePainter();
-        }
-
         ViewContactOfPageObj::ViewContactOfPageObj(SdrPageObj& rPageObj)
-        :   ViewContactOfSdrObj(rPageObj),
-            mpPagePainter(0L),
-            mbIsPainting(sal_False),
-            mbIsInActionChange(sal_False)
+        :   ViewContactOfSdrObj(rPageObj)
         {
         }
 
         ViewContactOfPageObj::~ViewContactOfPageObj()
         {
-            GetRidOfPagePainter();
-        }
-
-        // Paint this object. This is before evtl. SubObjects get painted. It needs to return
-        // sal_True when something was pained and the paint output rectangle in rPaintRectangle.
-        sal_Bool ViewContactOfPageObj::PaintObject(DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC)
-        {
-            sal_Bool bRetval(sal_False);
-
-            // get page to be displayed
-            const SdrPage* pPage = GetReferencedPage();
-
-            // avoid recursive painting
-            if(mbIsPainting)
-            {
-                // Paint a replacement object
-                const Rectangle aNewRectangle(GetPageRectangle());
-                bRetval |= PaintPageReplacement(rDisplayInfo, aNewRectangle, rAssociatedVOC);
-                rPaintRectangle.Union(aNewRectangle);
-            }
-            else
-            {
-                // something to paint?
-                if(pPage)
-                {
-                    // set recursion flag, see description in *.hxx
-                    mbIsPainting = sal_True;
-
-                    // Paint a replacement object.
-                    const Rectangle aNewRectangle(GetPageRectangle());
-                    bRetval |= PaintPageContents(rDisplayInfo, aNewRectangle, rAssociatedVOC);
-                    rPaintRectangle.Union(aNewRectangle);
-
-                    // reset recursion flag, see description in *.hxx
-                    mbIsPainting = sal_False;
-                }
-            }
-
-            // paint frame, but not when printing and no page available
-            if(!(rDisplayInfo.OutputToPrinter() && !pPage))
-            {
-                // #i54989# fill aNewRectangle before usage
-                const Rectangle aNewRectangle(GetPageRectangle());
-                bRetval |= PaintPageBorder(rDisplayInfo, aNewRectangle, rAssociatedVOC);
-                rPaintRectangle.Union(aNewRectangle);
-            }
-
-            return bRetval;
         }
 
         // #i35972# React on changes of the object of this ViewContact
         void ViewContactOfPageObj::ActionChanged()
         {
-            if(!mbIsInActionChange)
+            static bool bIsInActionChange(false);
+
+            if(!bIsInActionChange)
             {
                 // set recursion flag, see description in *.hxx
-                mbIsInActionChange = sal_True;
+                bIsInActionChange = true;
 
                 // call parent
                 ViewContactOfSdrObj::ActionChanged();
 
                 // reset recursion flag, see description in *.hxx
-                mbIsInActionChange = sal_False;
+                bIsInActionChange = false;
             }
         }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewContactOfPageObj::createViewIndependentPrimitive2DSequence() const
+        {
+            // ceate graphical visualisation data. Since this is the view-independent version which should not be used,
+            // create a replacement graphic visualisation here. Use GetLastBoundRect to access the model data directly
+            // which is aOutRect for SdrPageObj.
+            const Rectangle aModelRectangle(GetPageObj().GetLastBoundRect());
+            const basegfx::B2DRange aModelRange(aModelRectangle.Left(), aModelRectangle.Top(), aModelRectangle.Right(), aModelRectangle.Bottom());
+            const basegfx::B2DPolygon aOutline(basegfx::tools::createPolygonFromRect(aModelRange));
+            const basegfx::BColor aYellow(1.0, 1.0, 0.0);
+            const drawinglayer::primitive2d::Primitive2DReference xReference(new drawinglayer::primitive2d::PolygonHairlinePrimitive2D(aOutline, aYellow));
+
+            return drawinglayer::primitive2d::Primitive2DSequence(&xReference, 1);
+        }
+
     } // end of namespace contact
 } // end of namespace sdr
 
