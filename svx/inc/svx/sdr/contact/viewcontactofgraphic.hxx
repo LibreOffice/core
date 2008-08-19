7b053a35617f527a8335967dad864f7c1695334d
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:38:16 +0000

    INTEGRATION: CWS aw033 (1.3.16); FILE MERGED 2008/05/14 13:59:11 aw 1.3.16.9: RESYNC: (1.4-1.5); FILE MERGED 2008/02/07 14:12:59 aw 1.3.16.8: stable version from working GIT repository for unxlngi6 build 2008/01/22 12:29:05 aw 1.3.16.7: adaptions and 1st stripping 2007/12/03 16:36:33 aw 1.3.16.6: RESYNC: (1.3-1.4); FILE MERGED 2007/07/06 13:42:27 aw 1.3.16.5: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2006/10/19 10:53:43 aw 1.3.16.4: #i39532# primitive 2006/08/09 17:05:49 aw 1.3.16.3: #i39532# 2006/06/02 14:17:43 aw 1.3.16.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:44:10 aw 1.3.16.1: code changes for primitive support

--- svx/inc/svx/sdr/contact/viewcontactofgraphic.hxx
+++ svx/inc/svx/sdr/contact/viewcontactofgraphic.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: viewcontactofgraphic.hxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -39,14 +39,6 @@
 
 class SdrGrafObj;
 
-namespace sdr
-{
-    namespace event
-    {
-        class AsynchGraphicLoadingEvent;
-    } // end of namespace event
-} // end of namespace sdr
-
 //////////////////////////////////////////////////////////////////////////////
 
 namespace sdr
@@ -55,81 +47,26 @@ namespace sdr
     {
         class ViewContactOfGraphic : public ViewContactOfTextObj
         {
-            // Member which takes care for the asynch loading events which may be necessary
-            // for asynch graphics loading.
-            sdr::event::AsynchGraphicLoadingEvent*      mpAsynchLoadEvent;
-
-            // Test graphics state and eventually trigger a SwapIn event or an Asynchronous
-            // load event. Return value gives info if SwapIn was triggered.
-            sal_Bool ImpPrepareForPaint(
-                DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
-
         protected:
-            // internal access to SdrObject
+            // Create a Object-Specific ViewObjectContact, set ViewContact and
+            // ObjectContact. Always needs to return something.
+            virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact);
+
+        public:
+            // access to SdrObject
             SdrGrafObj& GetGrafObject() const
             {
                 return ((SdrGrafObj&)GetSdrObject());
             }
 
-            // method to create a AnimationInfo. Needs to give a result if
-            // SupportsAnimation() is overloaded and returns sal_True.
-            virtual sdr::animation::AnimationInfo* CreateAnimationInfo();
-
-            // For draft object display
-            sal_Bool PaintGraphicDraft(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC);
-
-            // For empty Graphic PresObj display
-            sal_Bool PaintGraphicPresObj(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC);
-
-            // Decide if graphic should be painted as draft
-            sal_Bool DoPaintGraphicDraft() const;
-
-            // Methods to react on start getting viewed or stop getting
-            // viewed. This info is derived from the count of members of
-            // registered ViewObjectContacts. Default does nothing.
-            virtual void StopGettingViewed();
-
-        public:
-            // basic constructor, used from SdrObject.
+            // basic constructor, destructor
             ViewContactOfGraphic(SdrGrafObj& rGrafObj);
-
-            // The destructor. When PrepareDelete() was not called before (see there)
-            // warnings will be generated in debug version if there are still contacts
-            // existing.
             virtual ~ViewContactOfGraphic();
 
-            // Prepare deletion of this object. Tghis needs to be called always
-            // before really deleting this objects. This is necessary since in a c++
-            // destructor no virtual function calls are allowed. To avoid this problem,
-            // it is required to first call PrepareDelete().
-            virtual void PrepareDelete();
-
-            // When ShouldPaintObject() returns sal_True, the object itself is painted and
-            // PaintObject() is called.
-            virtual sal_Bool ShouldPaintObject(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
-
-            // Paint this object. This is before evtl. SubObjects get painted. It needs to return
-            // sal_True when something was pained and the paint output rectangle in rPaintRectangle.
-            virtual sal_Bool PaintObject(DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC);
-
-            // React on removal of the object of this ViewContact,
-            // DrawHierarchy needs to be changed
-            virtual void ActionRemoved();
-
-            // Does this ViewContact support animation?
-            virtual sal_Bool SupportsAnimation() const;
-
-            // This is the call from the asynch graphic loading. This may only be called from
-            // AsynchGraphicLoadingEvent::ExecuteEvent(). Do load the graphics. The event will
-            // be deleted (consumed) and ForgetAsynchGraphicLoadingEvent will be called.
-            void DoAsynchGraphicLoading();
-
-            // This is the call from the destructor of the asynch graphic loading event.
-            // No one else has to call this. It is needed to let this object forget about
-            // the event. The parameter allows checking for the correct event.
-            void ForgetAsynchGraphicLoadingEvent(sdr::event::AsynchGraphicLoadingEvent* pEvent);
+        protected:
+            // This method is responsible for creating the graphical visualisation data
+            // ONLY based on model data
+            virtual drawinglayer::primitive2d::Primitive2DSequence createViewIndependentPrimitive2DSequence() const;
         };
     } // end of namespace contact
 } // end of namespace sdr
