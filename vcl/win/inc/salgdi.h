768afadbb12a7b882aef4ab60bb633721cba25e1
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 23:38:00 +0000

    INTEGRATION: CWS aw033 (1.25.70); FILE MERGED 2008/07/11 11:41:39 aw 1.25.70.8: RESYNC: (1.30-1.31); FILE MERGED 2008/06/25 13:10:13 aw 1.25.70.7: RESYNC: (1.29-1.30); FILE MERGED 2008/05/14 15:09:24 aw 1.25.70.6: RESYNC: (1.27-1.29); FILE MERGED 2008/03/14 14:38:30 cl 1.25.70.5: RESYNC: (1.26-1.27); FILE MERGED 2007/12/13 16:44:29 aw 1.25.70.4: #i39532# AA work for unxlngi6 2007/11/07 17:21:17 aw 1.25.70.3: #i39532# checking in to create a working version for HDU 2007/08/09 20:33:54 aw 1.25.70.2: RESYNC: (1.25-1.26); FILE MERGED 2007/05/29 14:50:31 hdu 1.25.70.1: #i75669# fix warnings after RESYNC to SRC680_m212

--- vcl/win/inc/salgdi.h
+++ vcl/win/inc/salgdi.h
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: salgdi.h,v $
- * $Revision: 1.31 $
+ * $Revision: 1.32 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -185,7 +185,7 @@ protected:
     virtual void        drawPolygon( ULONG nPoints, const SalPoint* pPtAry );
     virtual void        drawPolyPolygon( sal_uInt32 nPoly, const sal_uInt32* pPoints, PCONSTSALPOINT* pPtAry );
     virtual bool        drawPolyPolygon( const ::basegfx::B2DPolyPolygon&, double fTransparency );
-    virtual bool        drawPolyLine( const ::basegfx::B2DPolygon&, const ::basegfx::B2DVector& rLineWidths );
+    virtual bool        drawPolyLine( const ::basegfx::B2DPolygon&, const ::basegfx::B2DVector& rLineWidth, basegfx::B2DLineJoin);
     virtual sal_Bool    drawPolyLineBezier( ULONG nPoints, const SalPoint* pPtAry, const BYTE* pFlgAry );
     virtual sal_Bool    drawPolygonBezier( ULONG nPoints, const SalPoint* pPtAry, const BYTE* pFlgAry );
     virtual sal_Bool    drawPolyPolygonBezier( sal_uInt32 nPoly, const sal_uInt32* pPoints, const SalPoint* const* pPtAry, const BYTE* const* pFlgAry );
