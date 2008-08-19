227ff3818ed385114dc3e1f0e0e2a52464344d3b
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:37:15 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:25 cl 1.1.2.8: fixed license files 2008/07/10 12:58:58 aw 1.1.2.7: #i39532# XOutputDevice removed, PrepareDelete removed 2007/07/06 13:42:26 aw 1.1.2.6: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/01/25 12:44:05 aw 1.1.2.5: #i39532# change 3D scene content generation to be usable from API implementation, too 2006/10/19 10:53:42 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:05:48 aw 1.1.2.3: #i39532# 2006/06/02 14:17:42 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:31:05 aw 1.1.2.1: code for primitive support

---
+++ svx/inc/svx/sdr/contact/viewcontactofe3dcube.hxx
@@ -0,0 +1,75 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewcontactofe3dcube.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFE3DCUBE_HXX
+#define _SDR_CONTACT_VIEWCONTACTOFE3DCUBE_HXX
+
+#ifndef _SDR_CONTACT_VIEWCONTACTOFE3D_HXX
+#include <svx/sdr/contact/viewcontactofe3d.hxx>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// predeclarations
+
+class E3dCubeObj;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        class ViewContactOfE3dCube : public ViewContactOfE3d
+        {
+        public:
+            // basic constructor, used from SdrObject.
+            ViewContactOfE3dCube(E3dCubeObj& rCubeObj);
+            virtual ~ViewContactOfE3dCube();
+
+            // access to SdrObject
+            E3dCubeObj& GetE3dCubeObj() const
+            {
+                return (E3dCubeObj&)GetE3dObject();
+            }
+
+        protected:
+            // This method is responsible for creating the graphical visualisation data which is
+            // stored in the local primitive list. Default creates a yellow replacement rectangle.
+            virtual drawinglayer::primitive3d::Primitive3DSequence createViewIndependentPrimitive3DSequence() const;
+        };
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //_SDR_CONTACT_VIEWCONTACTOFE3DCUBE_HXX
+
+// eof
