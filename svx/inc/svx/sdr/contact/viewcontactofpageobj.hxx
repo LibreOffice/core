fa11382ca742125c76f87183c5d509d0c0462f23
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:38:51 +0000

    INTEGRATION: CWS aw033 (1.6.16); FILE MERGED 2008/07/10 12:58:58 aw 1.6.16.4: #i39532# XOutputDevice removed, PrepareDelete removed 2008/05/14 13:58:37 aw 1.6.16.3: RESYNC: (1.6-1.7); FILE MERGED 2008/01/22 12:29:06 aw 1.6.16.2: adaptions and 1st stripping 2006/11/28 11:17:46 aw 1.6.16.1: #i39532#

--- svx/inc/svx/sdr/contact/viewcontactofpageobj.hxx
+++ svx/inc/svx/sdr/contact/viewcontactofpageobj.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: viewcontactofpageobj.hxx,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -40,14 +40,6 @@
 class SdrPageObj;
 class SdrPage;
 
-namespace sdr
-{
-    namespace contact
-    {
-        class OCOfPageObjPagePainter;
-    } // end of namespace contact
-} // end of namespace sdr
-
 //////////////////////////////////////////////////////////////////////////////
 
 namespace sdr
@@ -56,96 +48,30 @@ namespace sdr
     {
         class SVX_DLLPUBLIC ViewContactOfPageObj : public ViewContactOfSdrObj
         {
-            // The painter for the page. As long as the page does not change,
-            // the incarnation can be reused.
-            OCOfPageObjPagePainter*                     mpPagePainter;
-
-            // bitfield
-            // Flag to avoid recursive displaying of page objects, e.g.
-            // when the page object itself shows a page which displays
-            // page objects.
-            unsigned                                    mbIsPainting : 1;
-
-            // #i35972# flag to avoid recursive ActionChange events
-            unsigned                                    mbIsInActionChange : 1;
-
         protected:
-            // internal access to SdrObject
-            SdrPageObj& GetPageObj() const
-            {
-                return (SdrPageObj&)GetSdrObject();
-            }
+            // Create a Object-Specific ViewObjectContact, set ViewContact and
+            // ObjectContact. Always needs to return something.
+            virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact);
+
+            // create graphical visualisation data
+            virtual drawinglayer::primitive2d::Primitive2DSequence createViewIndependentPrimitive2DSequence() const;
 
             // Access to referenced page
             const SdrPage* GetReferencedPage() const;
 
-            // method to recalculate the PaintRectangle if the validity flag shows that
-            // it is invalid. The flag is set from GetPaintRectangle, thus the implementation
-            // only needs to refresh maPaintRectangle itself.
-            virtual void CalcPaintRectangle();
-
-            /** Return the rectangle that specifies where and how large the
-                page will be painted.  This rectangle will usually be
-                identical to the one returned by GetPaintRectangle().
-                The returned rectangle has to lie completly inside the
-                rectangle returned by GetPaintRectangle().  Making it
-                smaller results in a border arround the page rectangle.
-
-                Note: This method may calculate and store internally the
-                requested rectangle and thus can not be const.
-            */
-            virtual Rectangle GetPageRectangle (void);
-
-            // get rid of evtl. remembered PagePainter
-            void GetRidOfPagePainter();
-
-            // Prepare a PagePainter for current referenced page. This may
-            // refresh, create or delete a PagePainter instance in
-            // mpPagePainter
-            void PreparePagePainter(const SdrPage* pPage);
-
-            /** Paint support methods for page content painting
-                @param rPaintRectangle
-                    The painting of the page content will be transformed so
-                    that it fills exactly this rectangle.  Usually this will
-                    be the paint rectangle.  Making the content rectangle
-                    smaller will result in a border between the outer paint
-                    rectangle (the bounding box) and the page content
-                    rectangle.
-            */
-            sal_Bool PaintPageContents(
-                DisplayInfo& rDisplayInfo,
-                const Rectangle& rPaintRectangle,
-                const ViewObjectContact& rAssociatedVOC);
-            sal_Bool PaintPageReplacement(
-                DisplayInfo& rDisplayInfo,
-                const Rectangle& rPaintRectangle,
-                const ViewObjectContact& rAssociatedVOC);
-            sal_Bool PaintPageBorder(
-                DisplayInfo& rDisplayInfo,
-                const Rectangle& rPaintRectangle,
-                const ViewObjectContact& rAssociatedVOC);
-
-            // On StopGettingViewed the PagePainter can be dismissed.
-            virtual void StopGettingViewed();
-
         public:
             // basic constructor, used from SdrObject.
             ViewContactOfPageObj(SdrPageObj& rPageObj);
-
-            // The destructor. When PrepareDelete() was not called before (see there)
-            // warnings will be generated in debug version if there are still contacts
-            // existing.
             virtual ~ViewContactOfPageObj();
-
-            // Paint this object. This is before evtl. SubObjects get painted. It needs to return
-            // sal_True when something was pained and the paint output rectangle in rPaintRectangle.
-            virtual sal_Bool PaintObject(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle,
-                const ViewObjectContact& rAssociatedVOC);
 
             // #WIP# React on changes of the object of this ViewContact
             virtual void ActionChanged();
+
+            // access to SdrObject
+            SdrPageObj& GetPageObj() const
+            {
+                return (SdrPageObj&)GetSdrObject();
+            }
         };
     } // end of namespace contact
 } // end of namespace sdr
