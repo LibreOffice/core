ad19535a68180d409116e1f364366e3a8f73e8b7
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:09:33 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:28 cl 1.1.2.11: fixed license files 2008/05/27 14:49:58 aw 1.1.2.10: #i39532# changes DEV300 m12 resync corrections 2008/04/04 06:07:49 aw 1.1.2.9: #i39532# added primitive support for tables 2008/01/22 12:29:30 aw 1.1.2.8: adaptions and 1st stripping 2007/08/13 15:34:33 aw 1.1.2.7: #i39532# changes after resync 2007/07/06 13:43:07 aw 1.1.2.6: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/01/23 10:46:31 aw 1.1.2.5: changes after resync 2006/10/19 10:59:15 aw 1.1.2.4: #i39532# primitive 2006/08/09 17:12:25 aw 1.1.2.3: #i39532# 2006/06/02 14:17:50 aw 1.1.2.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:40:45 aw 1.1.2.1: code for primitive support

---
+++ svx/source/sdr/contact/viewcontactofsdrrectobj.cxx
@@ -0,0 +1,122 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: viewcontactofsdrrectobj.cxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_svx.hxx"
+
+#include <svx/sdr/contact/viewcontactofsdrrectobj.hxx>
+#include <svx/svdorect.hxx>
+#include <svx/sdr/primitive2d/sdrattributecreator.hxx>
+#include <svx/sdr/attribute/sdrallattribute.hxx>
+#include <svx/sdr/primitive2d/sdrrectangleprimitive2d.hxx>
+#include <svtools/itemset.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace sdr
+{
+    namespace contact
+    {
+        ViewContactOfSdrRectObj::ViewContactOfSdrRectObj(SdrRectObj& rRectObj)
+        :   ViewContactOfTextObj(rRectObj)
+        {
+        }
+
+        ViewContactOfSdrRectObj::~ViewContactOfSdrRectObj()
+        {
+        }
+
+        drawinglayer::primitive2d::Primitive2DSequence ViewContactOfSdrRectObj::createViewIndependentPrimitive2DSequence() const
+        {
+            drawinglayer::primitive2d::Primitive2DSequence xRetval;
+            const SfxItemSet& rItemSet = GetRectObj().GetMergedItemSet();
+            SdrText* pSdrText = GetRectObj().getText(0);
+
+            if(pSdrText)
+            {
+                drawinglayer::attribute::SdrLineFillShadowTextAttribute* pAttribute = drawinglayer::primitive2d::createNewSdrLineFillShadowTextAttribute(rItemSet, *pSdrText);
+
+                if(pAttribute)
+                {
+                    if(pAttribute->isVisible())
+                    {
+                        // take unrotated snap rect (direct model data) for position and size
+                        const Rectangle& rRectangle = GetRectObj().GetGeoRect();
+                        const ::basegfx::B2DRange aObjectRange(rRectangle.Left(), rRectangle.Top(), rRectangle.Right(), rRectangle.Bottom());
+                        const GeoStat& rGeoStat(GetRectObj().GetGeoStat());
+                        ::basegfx::B2DHomMatrix aObjectMatrix;
+
+                        // fill object matrix
+                        if(!::basegfx::fTools::equalZero(aObjectRange.getWidth()))
+                        {
+                            aObjectMatrix.set(0, 0, aObjectRange.getWidth());
+                        }
+
+                        if(!::basegfx::fTools::equalZero(aObjectRange.getHeight()))
+                        {
+                            aObjectMatrix.set(1, 1, aObjectRange.getHeight());
+                        }
+
+                        if(rGeoStat.nShearWink)
+                        {
+                            aObjectMatrix.shearX(tan((36000 - rGeoStat.nShearWink) * F_PI18000));
+                        }
+
+                        if(rGeoStat.nDrehWink)
+                        {
+                            aObjectMatrix.rotate((36000 - rGeoStat.nDrehWink) * F_PI18000);
+                        }
+
+                        aObjectMatrix.translate(aObjectRange.getMinX(), aObjectRange.getMinY());
+
+                        // calculate corner radius
+                        sal_uInt32 nCornerRadius(((SdrEckenradiusItem&)(rItemSet.Get(SDRATTR_ECKENRADIUS))).GetValue());
+                        double fCornerRadiusX;
+                        double fCornerRadiusY;
+                        drawinglayer::primitive2d::calculateRelativeCornerRadius(nCornerRadius, aObjectRange, fCornerRadiusX, fCornerRadiusY);
+
+                        // create primitive
+                        const drawinglayer::primitive2d::Primitive2DReference xReference(new drawinglayer::primitive2d::SdrRectanglePrimitive2D(
+                            aObjectMatrix, *pAttribute, fCornerRadiusX, fCornerRadiusY));
+                        xRetval = drawinglayer::primitive2d::Primitive2DSequence(&xReference, 1);
+                    }
+
+                    delete pAttribute;
+                }
+            }
+
+            return xRetval;
+        }
+    } // end of namespace contact
+} // end of namespace sdr
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
