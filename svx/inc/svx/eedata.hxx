732f86e696a4565cfffd20fe17727ee6aaae04e7
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:25:55 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:25 cl 1.1.2.3: fixed license files 2007/09/26 11:38:33 aw 1.1.2.2: #i73860# text decomposition extrended 2007/08/02 11:45:42 aw 1.1.2.1: #i39532# added data structures for primitive and metafile comment support

---
+++ svx/inc/svx/eedata.hxx
@@ -0,0 +1,78 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: eedata.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+#ifndef _EEDATA_HXX
+#define _EEDATA_HXX
+
+//////////////////////////////////////////////////////////////////////////////
+// extended data definitions for EditEngine portion stripping (StripPortions()
+// support). These may be mapped to some primitive definitions from Drawinglayer
+// later.
+#include <vector>
+
+namespace EEngineData
+{
+    // spell checking wrong vector containing the redlining data
+    class WrongSpellClass
+    {
+    public:
+        sal_uInt32 nStart;
+        sal_uInt32 nEnd;
+
+        WrongSpellClass(sal_uInt32 nS, sal_uInt32 nE) : nStart(nS), nEnd(nE) {}
+    };
+
+    typedef std::vector< WrongSpellClass > WrongSpellVector;
+
+    // text marking vector containing the EOC, EOW and EOS TEXT definitions
+    enum TextMarkingType
+    {
+        EndOfCaracter = 0,
+        EndOfWord = 1,
+        EndOfSentence = 2
+    };
+
+    class TextMarkingClass
+    {
+    public:
+        TextMarkingType eType;
+        sal_uInt32 nIndex;
+
+        TextMarkingClass(TextMarkingType eT, sal_uInt32 nI) : eType(eT), nIndex(nI) {}
+    };
+
+    typedef std::vector< TextMarkingClass > TextMarkingVector;
+
+} // end of namespace EditEngine
+
+#endif // _EEDATA_HXX
+
+//////////////////////////////////////////////////////////////////////////////
+// eof
