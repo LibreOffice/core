80d5924906087c033fb1080cea9460b15bc3b929
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 23:52:25 +0000

    INTEGRATION: CWS aw033 (1.11.152); FILE MERGED 2008/05/14 15:24:50 aw 1.11.152.5: RESYNC: (1.14-1.15); FILE MERGED 2008/03/14 14:43:30 cl 1.11.152.4: RESYNC: (1.13-1.14); FILE MERGED 2007/08/09 22:44:03 aw 1.11.152.3: RESYNC: (1.12-1.13); FILE MERGED 2007/03/20 16:21:12 aw 1.11.152.2: RESYNC: (1.11-1.12); FILE MERGED 2007/01/22 15:20:32 aw 1.11.152.1: changes after resync

--- sc/source/filter/lotus/lotform.cxx
+++ sc/source/filter/lotus/lotform.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: lotform.cxx,v $
- * $Revision: 1.15 $
+ * $Revision: 1.16 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -43,6 +43,7 @@
 #include "root.hxx"
 #include "ftools.hxx"
 #include "tool.h"
+
 #include <math.h>
 
 extern WKTYP                eTyp;
