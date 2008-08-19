38b923b483df93f5b1113c0ab1ae0ee74eb2ed9d
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:39:52 +0000

    INTEGRATION: CWS aw033 (1.8.16); FILE MERGED 2008/07/10 12:58:58 aw 1.8.16.9: #i39532# XOutputDevice removed, PrepareDelete removed 2008/05/14 13:59:05 aw 1.8.16.8: RESYNC: (1.10-1.11); FILE MERGED 2008/01/22 12:29:06 aw 1.8.16.7: adaptions and 1st stripping 2007/07/06 13:42:27 aw 1.8.16.6: #i39532# moved from Primitive2DReference to Primitive2DSequence where possible to avoid extra-group primitive creations and deeper hierarchies as necessary 2007/05/23 20:15:27 aw 1.8.16.5: RESYNC: (1.8-1.9); FILE MERGED 2007/01/25 18:25:27 aw 1.8.16.4: #i39532# reorganized marker primitives to reduce count 2006/10/19 10:53:44 aw 1.8.16.3: #i39532# primitive 2006/06/02 14:17:44 aw 1.8.16.2: #i39532# adaptions to new primitives, error corrections 2006/05/12 12:44:10 aw 1.8.16.1: code changes for primitive support

--- svx/inc/svx/sdr/contact/viewcontactofsdrobj.hxx
+++ svx/inc/svx/sdr/contact/viewcontactofsdrobj.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: viewcontactofsdrobj.hxx,v $
- * $Revision: 1.11 $
+ * $Revision: 1.12 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -57,71 +57,23 @@ namespace sdr
             SdrObject&                                      mrObject;
 
             // Remember AnimationKind of object. Used to find out if that kind
-            // has changed in ActionChanged(), then it may be necessary to reset
-            // the AnimationInfo. Only used if object type is at least SdrTextObj.
+            // has changed in ActionChanged()
             SdrTextAniKind                                  meRememberedAnimationKind;
-
-            // internal access to SdrObject
-            SdrObject& GetSdrObject() const
-            {
-                return mrObject;
-            }
-
-            // method to create a AnimationInfo. Needs to give a result if
-            // SupportsAnimation() is overloaded and returns sal_True.
-            virtual sdr::animation::AnimationInfo* CreateAnimationInfo();
 
             // Create a Object-Specific ViewObjectContact, set ViewContact and
             // ObjectContact. Always needs to return something.
             virtual ViewObjectContact& CreateObjectSpecificViewObjectContact(ObjectContact& rObjectContact);
 
-            // method to recalculate the PaintRectangle if the validity flag shows that
-            // it is invalid. The flag is set from GetPaintRectangle, thus the implementation
-            // only needs to refresh maPaintRectangle itself.
-            virtual void CalcPaintRectangle();
-
-            // Used from ViewContactOfE3dScene and ViewContactOfGroup when
-            // those groupings are empty.
-            sal_Bool PaintReplacementObject(DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle);
-
-            // New methodology to test for the new SC drawing flags (SDRPAINTMODE_SC_)
-            sal_Bool DoPaintForCalc(DisplayInfo& rDisplayInfo) const;
-
-            // Paint a shadowed frame in object size. Fill it with a default gray if last parameter is sal_True.
-            sal_Bool PaintShadowedFrame(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const Rectangle& rUnrotatedRectangle,
-                const GeoStat& rGeometric, sal_Bool bFilled);
-
-            // Paint draft text in object size.
-            sal_Bool PaintDraftText(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const Rectangle& rUnrotatedRectangle,
-                const GeoStat& rGeometric, const XubString& rDraftString, sal_Bool bUnderline);
-
-            // Paint draft bitmap in object size.
-            sal_Bool PaintDraftBitmap(
-                DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const Rectangle& rUnrotatedRectangle,
-                const GeoStat& rGeometric, const Bitmap& rBitmap);
-
         public:
+            // access to SdrObject
+            SdrObject& GetSdrObject() const
+            {
+                return mrObject;
+            }
+
             // basic constructor, used from SdrObject.
             ViewContactOfSdrObj(SdrObject& rObj);
-
-            // The destructor. When PrepareDelete() was not called before (see there)
-            // warnings will be generated in debug version if there are still contacts
-            // existing.
             virtual ~ViewContactOfSdrObj();
-
-            // When ShouldPaintObject() returns sal_True, the object itself is painted and
-            // PaintObject() is called.
-            virtual sal_Bool ShouldPaintObject(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
-
-            // Paint this object. This is before evtl. SubObjects get painted. It needs to return
-            // sal_True when something was pained and the paint output rectangle in rPaintRectangle.
-            virtual sal_Bool PaintObject(DisplayInfo& rDisplayInfo, Rectangle& rPaintRectangle, const ViewObjectContact& rAssociatedVOC);
-
-            // Paint this objects GluePoints. This is after PaitObject() was called.
-            // This is temporarily as long as GluePoints are no handles yet. The default does nothing.
-            virtual void PaintGluePoints(DisplayInfo& rDisplayInfo, const ViewObjectContact& rAssociatedVOC);
 
             // Access to possible sub-hierarchy
             virtual sal_uInt32 GetObjectCount() const;
@@ -131,11 +83,14 @@ namespace sdr
             // React on changes of the object of this ViewContact
             virtual void ActionChanged();
 
-            // Does this ViewContact support animation?
-            virtual sal_Bool SupportsAnimation() const;
-
             // overload for acessing the SdrObject
             virtual SdrObject* TryToGetSdrObject() const;
+
+            //////////////////////////////////////////////////////////////////////////////
+            // primitive stuff
+
+            // add Gluepoints (if available)
+            virtual drawinglayer::primitive2d::Primitive2DSequence createGluePointPrimitive2DSequence() const;
         };
     } // end of namespace contact
 } // end of namespace sdr
