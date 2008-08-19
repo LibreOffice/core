6cc148a890d105da77daf5f278cc64e870e65301
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:33:19 +0000

    INTEGRATION: CWS aw033 (1.17.58); FILE MERGED 2008/07/10 13:01:00 aw 1.17.58.1: #i39532# XOutputDevice removed, PrepareDelete removed

--- svx/source/xoutdev/xtabbtmp.cxx
+++ svx/source/xoutdev/xtabbtmp.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xtabbtmp.cxx,v $
- * $Revision: 1.17 $
+ * $Revision: 1.18 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -50,7 +50,6 @@
 #include <svx/dialmgr.hxx>
 #include <svx/xtable.hxx>
 #include <svx/xpool.hxx>
-#include <svx/xoutx.hxx>
 
 #define GLOBALOVERFLOW
 
