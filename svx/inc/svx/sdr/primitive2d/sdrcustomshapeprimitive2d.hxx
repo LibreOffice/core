8dfdaee58ce2a0fbad1175622b8b29e3c5e7c207
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:46:06 +0000

    INTEGRATION: CWS aw033 (1.1.2); FILE ADDED 2008/08/19 16:46:27 cl 1.1.2.6: fixed license files 2008/05/27 14:48:07 aw 1.1.2.5: #i39532# changes DEV300 m12 resync corrections 2007/01/25 12:44:07 aw 1.1.2.4: #i39532# change 3D scene content generation to be usable from API implementation, too 2006/11/28 11:17:47 aw 1.1.2.3: #i39532# 2006/11/07 15:52:09 aw 1.1.2.2: #i39532# changed various aspevcts of XPrimitive2D implementations 2006/10/19 10:52:06 aw 1.1.2.1: #i39532# primitive

---
+++ svx/inc/svx/sdr/primitive2d/sdrcustomshapeprimitive2d.hxx
@@ -0,0 +1,83 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: sdrcustomshapeprimitive2d.hxx,v $
+ *
+ * $Revision: 1.2 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_SDR_PRIMITIVE2D_SDCUSTOMSHAPEPRIMITIVE2D_HXX
+#define INCLUDED_SDR_PRIMITIVE2D_SDCUSTOMSHAPEPRIMITIVE2D_HXX
+
+#include <drawinglayer/primitive2d/baseprimitive2d.hxx>
+#include <svx/sdr/attribute/sdrallattribute.hxx>
+#include <basegfx/matrix/b2dhommatrix.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predefines
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace drawinglayer
+{
+    namespace primitive2d
+    {
+        class SdrCustomShapePrimitive2D : public BasePrimitive2D
+        {
+        private:
+            attribute::SdrShadowTextAttribute           maSdrSTAttribute;
+            Primitive2DSequence                         maSubPrimitives;
+            ::basegfx::B2DHomMatrix                     maTextBox;
+
+        protected:
+            // local decomposition.
+            virtual Primitive2DSequence createLocalDecomposition(const geometry::ViewInformation2D& aViewInformation) const;
+
+        public:
+            SdrCustomShapePrimitive2D(
+                const attribute::SdrShadowTextAttribute& rSdrSTAttribute,
+                const Primitive2DSequence& rSubPrimitives,
+                const ::basegfx::B2DHomMatrix& rTextBox);
+
+            // data access
+            const attribute::SdrShadowTextAttribute& getSdrSTAttribute() const { return maSdrSTAttribute; }
+            const Primitive2DSequence& getSubPrimitives() const { return maSubPrimitives; }
+            const ::basegfx::B2DHomMatrix& getTextBox() const { return maTextBox; }
+
+            // compare operator
+            virtual bool operator==(const BasePrimitive2D& rPrimitive) const;
+
+            // provide unique ID
+            DeclPrimitrive2DIDBlock()
+        };
+    } // end of namespace primitive2d
+} // end of namespace drawinglayer
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //INCLUDED_SDR_PRIMITIVE2D_SDCUSTOMSHAPEPRIMITIVE2D_HXX
+
+// eof
