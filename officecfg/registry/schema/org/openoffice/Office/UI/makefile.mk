b2f23014a0d37b038ed19d38990c7d07585cdb85
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 08:22:19 +0000

    INTEGRATION: CWS dba31a (1.15.66); FILE MERGED 2008/07/01 07:20:39 oj 1.15.66.1: #i77916# move config to report builder

--- officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
+++ officecfg/registry/schema/org/openoffice/Office/UI/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.15 $
+# $Revision: 1.16 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -77,9 +77,7 @@ XCSFILES= \
     Effects.xcs \
     Category.xcs \
     GenericCategories.xcs \
-    GlobalSettings.xcs \
-    DbReportWindowState.xcs \
-    ReportCommands.xcs
+    GlobalSettings.xcs
 
 .INCLUDE :  target.mk
 
