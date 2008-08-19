4b2dde4a7898a13d75871cbd67e1aab3a73eb1c3
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 22:01:18 +0000

    INTEGRATION: CWS aw033 (1.8.14); FILE MERGED 2008/07/10 13:00:54 aw 1.8.14.10: #i39532# XOutputDevice removed, PrepareDelete removed 2008/06/24 15:42:24 aw 1.8.14.9: #i39532# corrections 2008/05/14 14:07:17 aw 1.8.14.8: RESYNC: (1.12-1.14); FILE MERGED 2008/03/18 07:15:23 aw 1.8.14.7: #i39532# changes after resync 2008/03/14 13:50:09 cl 1.8.14.6: RESYNC: (1.11-1.12); FILE MERGED 2008/01/22 12:29:28 aw 1.8.14.5: adaptions and 1st stripping 2007/12/03 16:39:55 aw 1.8.14.4: RESYNC: (1.10-1.11); FILE MERGED 2007/08/09 18:31:14 aw 1.8.14.3: RESYNC: (1.9-1.10); FILE MERGED 2006/11/28 19:13:18 aw 1.8.14.2: RESYNC: (1.8-1.9); FILE MERGED 2005/10/28 11:38:17 aw 1.8.14.1: #i39532#

--- svx/source/inc/svdoimp.hxx
+++ svx/source/inc/svdoimp.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svdoimp.hxx,v $
- * $Revision: 1.14 $
+ * $Revision: 1.15 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,57 +31,25 @@
 #ifndef _SVX_SVDOIMP_HXX
 #define _SVX_SVDOIMP_HXX
 
-#include <memory>
-#include <vector>
-#include <cppuhelper/weakref.hxx>
-
-#ifndef _MAPMOD_HXX //autogen
 #include <vcl/mapmod.hxx>
-#endif
-#include <svtools/lstner.hxx>
-#include <vcl/timer.hxx>
-#include <svx/svdsob.hxx>
-#include <svx/svdtypes.hxx> // fuer SdrLayerID
-#include <svx/svdglue.hxx> // Klebepunkte
-#include <svx/xdash.hxx>
-#include <svx/xpoly.hxx>
 
-//#ifndef _POLY3D_HXX
-//#include "poly3d.hxx"
-//#endif
-#include <svx/xenum.hxx>
-#include <basegfx/vector/b2dvector.hxx>
+//#include <svtools/lstner.hxx>
+//#include <vcl/timer.hxx>
+//#include <svx/svdsob.hxx>
+//#include <svx/svdtypes.hxx> // fuer SdrLayerID
+//#include <svx/svdglue.hxx> // Klebepunkte
+//#include <svx/xdash.hxx>
+//#include <svx/xpoly.hxx>
+//#include <svx/xenum.hxx>
+//#include <basegfx/vector/b2dvector.hxx>
 #include <svx/rectenum.hxx>
-#include <basegfx/polygon/b2dpolypolygon.hxx>
+//#include <basegfx/polygon/b2dpolypolygon.hxx>
 
-class SdrObject;
-class XOutputDevice;
-class XFillAttrSetItem;
-class XLineAttrSetItem;
-class SfxItemSet;
 class Bitmap;
 
-
 ///////////////////////////////////////////////////////////////////////////////
 
-// #100127# Bracket filled shapes with a comment, if recording a Mtf
-class ImpGraphicFill
-{
-public:
-    ImpGraphicFill( const SdrObject& rObj, const XOutputDevice& rXOut, const SfxItemSet& rFillItemSet, bool bIsShadow=false );
-    ImpGraphicFill( const XOutputDevice& rXOut, const SfxItemSet& rSet,basegfx::B2DPolyPolygon& aGeometry, const SfxItemSet& rFillItemSet, bool bIsShadow );
-    ~ImpGraphicFill();
-
-private:
-    void prepare( const XOutputDevice&  rXOut, const SfxItemSet& rSet, basegfx::B2DPolyPolygon& aGeometry, const SfxItemSet& rFillItemSet, bool bIsShadow );
-
-    const XOutputDevice&    mrXOut;
-    bool                    mbCommentWritten;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-
-// #104609# Extracted from XOutputDevice::ImpCalcBmpFillStartValues
+// #104609# Extracted from old XOutDev's ImpCalcBmpFillStartValues
 
 /** Calc offset and size for bitmap fill
 
@@ -139,129 +107,7 @@ void ImpCalcBmpFillSizes( Size&            rStartOffset,
                           RECT_POINT       eBmpRectPoint );
 
 
-///////////////////////////////////////////////////////////////////////////////
-
-class ImpLineStyleParameterPack
-{
-    XLineJoint                      meLineJoint;
-    basegfx::B2DPolyPolygon     maStartPolyPolygon;
-    basegfx::B2DPolyPolygon     maEndPolyPolygon;
-
-    sal_Int32                       mnLineWidth;
-    sal_Int32                       mnStartWidth;
-    sal_Int32                       mnEndWidth;
-
-    ::std::vector<double>           maDotDashArray;
-    double                          mfFullDotDashLen;
-    double                          mfDegreeStepWidth;
-
-    // bitfield
-    unsigned                        mbStartCentered : 1;
-    unsigned                        mbEndCentered : 1;
-    unsigned                        mbForceNoArrowsLeft : 1;
-    unsigned                        mbForceNoArrowsRight : 1;
-    unsigned                        mbForceHair : 1;
-
-    // flag for LineStyle. True is XLINE_SOLID, false is XLINE_DASH
-    unsigned                        mbLineStyleSolid : 1;
-
-public:
-    ImpLineStyleParameterPack(const SfxItemSet& rSet, bool bForceHair);
-    ~ImpLineStyleParameterPack();
-
-    sal_Int32 GetLineWidth() const { return mnLineWidth; }
-    sal_Int32 GetDisplayLineWidth() const { return mbForceHair ? 0L : mnLineWidth; }
-    bool IsLineStyleSolid() const { return mbLineStyleSolid; }
-
-    sal_Int32 GetStartWidth() const { return mnStartWidth; }
-    sal_Int32 GetEndWidth() const { return mnEndWidth; }
-
-    const basegfx::B2DPolyPolygon& GetStartPolyPolygon() const { return maStartPolyPolygon; }
-    const basegfx::B2DPolyPolygon& GetEndPolyPolygon() const { return maEndPolyPolygon; }
-
-    double GetDegreeStepWidth() const { return mfDegreeStepWidth; }
-
-    XLineJoint GetLineJoint() const { return meLineJoint; }
-    double GetLinejointMiterMinimumAngle() const { return 15.0; }
-
-    double GetFullDotDashLen() const { return mfFullDotDashLen; }
-    const ::std::vector< double >& GetDotDash() const { return maDotDashArray; }
-
-    bool IsStartCentered() const { return mbStartCentered; }
-    bool IsEndCentered() const { return mbEndCentered; }
-
-    bool IsStartActive() const;
-    bool IsEndActive() const;
-
-    void ForceNoArrowsLeft(bool bNew) { mbForceNoArrowsLeft = bNew; }
-    void ForceNoArrowsRight(bool bNew) { mbForceNoArrowsRight = bNew; }
-};
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class ImpLineGeometryCreator
-{
-    const ImpLineStyleParameterPack&        mrLineAttr;
-    basegfx::B2DPolyPolygon&                maAreaPolyPolygon;
-    basegfx::B2DPolyPolygon&                maLinePolyPolygon;
-
-    // bitfield
-    unsigned                                mbLineDraft : 1;
-
-    // private support functions
-    // help functions for line geometry creation
-    void ImpCreateLineGeometry(
-        const basegfx::B2DPolygon& rSourcePoly);
-
-public:
-    ImpLineGeometryCreator(
-        const ImpLineStyleParameterPack& rAttr,
-        basegfx::B2DPolyPolygon& rPoPo,
-        basegfx::B2DPolyPolygon& rPoLi)
-    :   mrLineAttr(rAttr),
-        maAreaPolyPolygon(rPoPo),
-        maLinePolyPolygon(rPoLi),
-        mbLineDraft(false)
-    {
-    }
-
-    void AddPolygon(const basegfx::B2DPolygon& rPoly) { ImpCreateLineGeometry(rPoly); }
-    const basegfx::B2DPolyPolygon& GetAreaPolyPolygon() const { return maAreaPolyPolygon; }
-    const basegfx::B2DPolyPolygon& GetLinePolyPolygon() const { return maLinePolyPolygon; }
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class SdrLineGeometry
-{
-    basegfx::B2DPolyPolygon             maAreaPolyPolygon;
-    basegfx::B2DPolyPolygon             maLinePolyPolygon;
-    ImpLineStyleParameterPack               maLineAttr;
-
-    // bitfield
-    unsigned                                mbForceOnePixel : 1;
-    unsigned                                mbForceTwoPixel : 1;
-
-public:
-    SdrLineGeometry(
-        const basegfx::B2DPolyPolygon& rAreaPolyPolygon,
-        const basegfx::B2DPolyPolygon& rLinePolyPolygon,
-        const ImpLineStyleParameterPack& rLineAttr,
-        bool bForceOnePixel,
-        bool bForceTwoPixel)
-    :   maAreaPolyPolygon(rAreaPolyPolygon),
-        maLinePolyPolygon(rLinePolyPolygon),
-        maLineAttr(rLineAttr),
-        mbForceOnePixel(bForceOnePixel),
-        mbForceTwoPixel(bForceTwoPixel)
-    {}
-
-    const basegfx::B2DPolyPolygon& GetAreaPolyPolygon() { return maAreaPolyPolygon; }
-    const basegfx::B2DPolyPolygon& GetLinePolyPolygon() { return maLinePolyPolygon; }
-    const ImpLineStyleParameterPack& GetLineAttr() { return maLineAttr; }
-    bool DoForceOnePixel() const { return mbForceOnePixel; }
-    bool DoForceTwoPixel() const { return mbForceTwoPixel; }
-};
 
 #endif // _SVX_SVDOIMP_HXX
 
