1d154a82adb3af9b422144edcc453d8a96e7b161
Rüdiger Timm <rt@openoffice.org>
2008-08-28 10:45:52 +0000

    INTEGRATION: CWS mh30f_DEV300 (1.4.500); FILE MERGED 2008/08/27 12:28:00 mh 1.4.500.1: update copyright year

--- desktop/source/app/copyright_ascii_sun.c
+++ desktop/source/app/copyright_ascii_sun.c
@@ -4,12 +4,12 @@
   *
   */
 
-const char copyright_text_1[] = "Copyright © 2005 Sun Microsystems, Inc., All rights reserved.";
+const char copyright_text_1[] = "Copyright © 2008 Sun Microsystems, Inc., All rights reserved.";
 const char copyright_text_2[] = "Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product that is described in this document. In particular, and without limitation, these intellectual property rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or more additional patents or pending patent applications in the U.S. and in other countries.";
 const char copyright_text_3[] = "U.S. Government Rights - Commercial software. Government users are subject to the Sun Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its supplements.  Use is subject to license terms.";
 const char copyright_text_4[] = "This distribution may include materials developed by third parties.Sun, Sun Microsystems, the Sun logo, Java, Solaris and StarOffice are trademarks or registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the U.S. and other countries.";
 const char copyright_text_5[] = "UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open Company, Ltd.";
-const char copyright_text_21[] = "Copyright © 2005 Sun Microsystems, Tous droits réservés.";
+const char copyright_text_21[] = "Copyright © 2008 Sun Microsystems, Tous droits réservés.";
 const char copyright_text_22[] = "Sun Microsystems, Inc. a les droits de propriété intellectuels relatants à la technologie incorporée dans ce produit. En particulier, et sans la limitation, ces droits de propriété intellectuels peuvent inclure un ou plus des brevets américains énumérés à http://www.sun.com/patents et un ou les brevets plus supplémentaires ou les applications de brevet en attente dans les États - Unis et les autres pays.";
 const char copyright_text_23[] = "Ce produit ou document est protégé par un copyright et distribué avec des licences qui en restreignent l'utilisation, la copie, la distribution, et la décompilation. Aucune partie de ce produit ou document ne peut être reproduite sous aucune forme, par quelque moyen que ce soit, sans l'autorisation préalable et écrite de Sun et de ses bailleurs de licence, s'il y ena.";
 const char copyright_text_24[] = "L'utilisation est soumise aux termes du contrat de licence.";
