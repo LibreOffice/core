1a3eea2880a5763b88b0b4f901db7408fdba29e1
Rüdiger Timm <rt@openoffice.org>
2008-09-04 09:07:10 +0000

    INTEGRATION: CWS swenhancedfields2 (1.18.134); FILE MERGED 2008/08/04 12:37:03 b_michaelsen 1.18.134.2: RESYNC: (1.18-1.21); FILE MERGED 2008/04/23 07:51:18 ama 1.18.134.1: #i33737#: Enhanced fields

--- xmloff/inc/xmlnmspe.hxx
+++ xmloff/inc/xmlnmspe.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: xmlnmspe.hxx,v $
- * $Revision: 1.21 $
+ * $Revision: 1.22 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -92,5 +92,9 @@ XML_OLD_NAMESPACE( STYLE,   3U )
 XML_OLD_NAMESPACE( TEXT,    4U )
 XML_OLD_NAMESPACE( TABLE,   5U )
 XML_OLD_NAMESPACE( META,    6U )
+
+// experimental namespaces
+XML_NAMESPACE( FIELD,           100U )
+
 
 #endif  //  _XMLOFF_XMLNMSPE_HXX
