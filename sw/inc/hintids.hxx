2c58c90bb9e6735a04384140fb1a2e33e46e6579
Rüdiger Timm <rt@openoffice.org>
2008-09-04 09:17:17 +0000

    INTEGRATION: CWS swenhancedfields2 (1.34.332); FILE MERGED 2008/08/04 14:14:12 b_michaelsen 1.34.332.2: RESYNC: (1.34-1.36); FILE MERGED 2008/04/23 11:05:37 ama 1.34.332.1: #i33737#: Enhanced fields

--- sw/inc/hintids.hxx
+++ sw/inc/hintids.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: hintids.hxx,v $
- * $Revision: 1.36 $
+ * $Revision: 1.37 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -40,7 +40,16 @@
 #define CH_TXTATR_INWORD        ((sal_Unicode)0x02)
 #define CH_TXTATR_TAB           ((sal_Unicode)'\t')
 #define CH_TXTATR_NEWLINE       ((sal_Unicode)'\n')
-
+#define CH_TXT_ATR_FIELDSTART ((sal_Unicode)0x04)
+#define CH_TXT_ATR_FIELDEND ((sal_Unicode)0x05)
+#define CH_TXT_ATR_SUBST_FIELDSTART ("[")
+#define CH_TXT_ATR_SUBST_FIELDEND ("]")
+//#define CH_TXT_ATR_FORMELEMENT ((sal_Unicode)'#')
+#define CH_TXT_ATR_FORMELEMENT ((sal_Unicode)0x06)
+/*
+#define FIELD_BOOKMARK_PREFIX "__"
+#define FIELD_FORM_BOOKMARK_PREFIX "__FORM"
+*/
 /*
  * Hier kommen erst mal die enums fuer die Hints
  */
