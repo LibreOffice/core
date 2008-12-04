4c4bb11ad956f84dfb7d824cdd659b097cc3e334
Frank Meies <fme@openoffice.org>
2008-12-04 14:42:57 +0000

    #i5991# Overline support in drawing layer

--- drawinglayer/source/primitive2d/textlayoutdevice.cxx
+++ drawinglayer/source/primitive2d/textlayoutdevice.cxx
@@ -175,6 +175,13 @@ namespace drawinglayer
             setFont(getVclFontFromFontAttributes(rFontAttributes, fFontScaleX, fFontScaleY, 0.0, mrDevice));
         }
 
+        double TextLayouterDevice::getOverlineOffset() const
+        {
+            const ::FontMetric& rMetric = mrDevice.GetFontMetric();
+            double fRet = (rMetric.GetIntLeading() / 2.0) - rMetric.GetAscent();
+            return fRet;
+        }
+
         double TextLayouterDevice::getUnderlineOffset() const
         {
             const ::FontMetric& rMetric = mrDevice.GetFontMetric();
@@ -186,6 +193,13 @@ namespace drawinglayer
         {
             const ::FontMetric& rMetric = mrDevice.GetFontMetric();
             double fRet = (rMetric.GetAscent() - rMetric.GetIntLeading()) / 3.0;
+            return fRet;
+        }
+
+        double TextLayouterDevice::getOverlineHeight() const
+        {
+            const ::FontMetric& rMetric = mrDevice.GetFontMetric();
+            double fRet = rMetric.GetIntLeading() / 2.5;
             return fRet;
         }
 
