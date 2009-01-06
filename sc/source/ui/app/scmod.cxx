6c0208067b950fec47d8a2679e0daa79fc78a2c0
Jens-Heiner Rechtien <hr@openoffice.org>
2009-01-06 12:54:16 +0000

    CWS-TOOLING: integrate CWS locales31 2008-12-10 14:56:44 +0100 er  r265193 : calling convention mismatch 2008-12-08 12:04:56 +0100 er  r264984 : #i96840# classify some extra languages as CTL 2008-12-05 19:54:02 +0100 erack  r264916 : ImpSvNumberformatScan::ScanType: do not miscategorize as automtic currency if currency symbol in General string, such as R in Standard 2008-12-04 12:07:05 +0100 erack  r264831 : CWS-TOOLING: rebase CWS locales31 to trunk@264807 (milestone: DEV300:m37) 2008-12-03 16:14:17 +0100 erack  r264797 : #i93694# update script+language -> unicode digit mapping; patch from <hdu> 2008-11-25 02:01:18 +0100 erack  r264270 : #i83349# apply remaining parts of the patch, now that we use ICU 4.0; contributed by <kstribley> 2008-11-25 01:38:20 +0100 erack  r264269 : #i93694# test the bit, not the constant ... 2008-11-24 10:34:58 +0100 erack  r264211 : #i93694# LANGUAGE_ARABIC is gone 2008-11-24 01:39:25 +0100 erack  r264192 : #i94435# LANGUAGE_SPANISH now is an alias of LANGUAGE_SPANISH_MODERN, need LANGUAGE_SPANISH_DATED in switch case if both are to be used 2008-11-23 22:54:17 +0100 erack  r264190 : CWS-TOOLING: rebase CWS locales31 to trunk@263288 (milestone: DEV300:m35) 2008-11-23 20:23:28 +0100 erack  r264189 : migrate CWS locales31 to SVN

--- sc/source/ui/app/scmod.cxx
+++ sc/source/ui/app/scmod.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: scmod.cxx,v $
- * $Revision: 1.58.32.1 $
+ * $Revision: 1.58.172.1 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -1007,7 +1007,7 @@ USHORT ScModule::GetOptDigitLanguage()
 {
     SvtCTLOptions::TextNumerals eNumerals = GetCTLOptions().GetCTLTextNumerals();
     return ( eNumerals == SvtCTLOptions::NUMERALS_ARABIC ) ? LANGUAGE_ENGLISH_US :
-           ( eNumerals == SvtCTLOptions::NUMERALS_HINDI)   ? LANGUAGE_ARABIC :
+           ( eNumerals == SvtCTLOptions::NUMERALS_HINDI)   ? LANGUAGE_ARABIC_SAUDI_ARABIA :
                                                              LANGUAGE_SYSTEM;
 }
 
