dc63c36a03935ca59595203fe329ea03db4dbabe
Jens-Heiner Rechtien <hr@openoffice.org>
2009-01-14 11:10:41 +0000

    #i10000#: include precompiled headers

--- svx/source/sdr/primitive2d/sdrprimitivetools.cxx
+++ svx/source/sdr/primitive2d/sdrprimitivetools.cxx
@@ -29,6 +29,7 @@
  *
  ************************************************************************/
 
+#include "precompiled_svx.hxx"
 #include <svx/sdr/primitive2d/sdrprimitivetools.hxx>
 #include <vcl/bmpacc.hxx>
 #include <osl/mutex.hxx>
