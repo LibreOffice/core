f554cbf974906bcaa6cc78ee5cef2e60ffdf15d0
Vladimir Glazounov <vg@openoffice.org>
2009-01-21 15:28:14 +0000

    CWS-TOOLING: integrate CWS fwk95 2009-01-15 13:24:14 +0100 cd  r266366 : #i96390# Change method name to use UNO AWT style for mnemonics 2009-01-15 13:23:30 +0100 cd  r266365 : #i96390# Change method name to use UNO AWT style for mnemonics 2009-01-12 17:25:13 +0100 cd  r266174 : #i97983# Check pointer returned by dynamic_cast 2009-01-12 15:31:40 +0100 cd  r266162 : #i97983# Create toolkit popup menus via service manager instead of implementation class 2008-12-11 15:04:34 +0100 cd  r265305 : #i95863# Remove user defined properties 2008-12-11 14:56:45 +0100 cd  r265302 : #i95606# Update documentation for textfields 2008-12-11 10:06:20 +0100 cd  r265250 : #i96390# Fix warning on Solaris compiler 2008-12-09 17:27:32 +0100 cd  r265119 : CWS-TOOLING: rebase CWS fwk95 to trunk@264807 (milestone: DEV300:m37) 2008-12-01 13:29:43 +0100 cd  r264597 : #i96390# New interfaces and structures for complete UNO AWT menu API 2008-12-01 13:25:22 +0100 cd  r264596 : #i96390# Added implementation of the new UNO AWT interfaces and structures for menus 2008-11-28 13:55:19 +0100 cd  r264553 : #i96640# Fixed wrong property type for MenuBarCloser 2008-11-28 13:53:56 +0100 cd  r264552 : #i96621# Fixed ambiguous usage of && and || 2008-11-18 16:05:45 +0100 cd  r263781 : #i95316# Initialize SvtTabAppearanceCfg to enable system settings changed notiifcation code 2008-11-18 16:05:43 +0100 cd  r263780 : #i96155# Fix wrong statement to check for enabled argument 2008-10-20 16:54:37 +0200 cd  r262328 : #i10000# migrate CWS fwk95 to SVN.

--- offapi/com/sun/star/awt/makefile.mk
+++ offapi/com/sun/star/awt/makefile.mk
@@ -1,7 +1,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -325,7 +325,12 @@ IDLFILES=\
     XLayoutFlow.idl\
     XLayoutFlowContainer.idl\
     XLayoutRoot.idl\
-    XLayoutUnit.idl
+    XLayoutUnit.idl \
+    MenuItemType.idl \
+    MenuLogo.idl \
+    XMenuExtended2.idl \
+    XMenuBarExtended.idl \
+    XPopupMenuExtended.idl
 
 # ------------------------------------------------------------------
 
