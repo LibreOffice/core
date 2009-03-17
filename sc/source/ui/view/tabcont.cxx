46dfc893a9f36622e201a8168dc915db9794b695
Vladimir Glazounov <vg@openoffice.org>
2009-03-17 12:35:17 +0000

    CWS-TOOLING: integrate CWS calcsoli2_DEV300 2009-02-24 21:05:58 +0100 nn  r268409 : #i99576# no optimization for tabcont.cxx on unxsoli4

--- sc/source/ui/view/tabcont.cxx
+++ sc/source/ui/view/tabcont.cxx
@@ -324,6 +324,8 @@ void ScTabControl::UpdateStatus()
             if ( rMark.GetTableSelect(i) != IsPageSelected(static_cast<sal_uInt16>(i)+1) )
                 bModified = TRUE;
 
+        // #i99576# the following loop is mis-optimized on unxsoli4 and the reason
+        // why this file is in NOOPTFILES.
         if ( bModified )
             for (i=0; i<nCount; i++)
                 SelectPage( static_cast<sal_uInt16>(i)+1, rMark.GetTableSelect(i) );
