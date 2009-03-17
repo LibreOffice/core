7f490ef8614159ca8be36cc6eb9ed6117e81518e
Vladimir Glazounov <vg@openoffice.org>
2009-03-17 11:49:39 +0000

    CWS-TOOLING: integrate CWS cl12_DEV300 2009-02-28 11:04:48 +0100 cl  r268621 : #i99678# correctly initialize paragraph vector for edit engine text 2009-02-27 16:16:25 +0100 dr  r268605 : #i99710# lost cloning of outlinerparaobject

--- svx/source/outliner/outlobj.cxx
+++ svx/source/outliner/outlobj.cxx
@@ -61,6 +61,8 @@ public:
         mbIsEditDoc(bIsEditDoc),
         mnRefCount(0)
     {
+        if( (maParagraphDataVector.size() == 0) && (pEditTextObject->GetParagraphCount() != 0) )
+            maParagraphDataVector.resize(pEditTextObject->GetParagraphCount());
     }
 
     // destructor
