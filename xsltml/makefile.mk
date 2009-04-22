080a4df7dab593364df0d2155c2054ad6428930a
Ivo Hinkelmann <ihi@openoffice.org>
2009-04-22 17:14:21 +0000

    CWS-TOOLING: integrate CWS mav46 2009-04-03 13:43:02 +0200 mav  r270481 : #i81373# fix delivering 2009-04-01 09:13:45 +0200 hjs  r270299 : #i81373# a bit too specific 2009-03-31 14:55:42 +0200 mav  r270282 : #i81373# the new tarball patching mechanics from hjs 2009-03-31 12:47:18 +0200 mav  r270274 : #i81373# use the new tarball patching mechanics 2009-03-31 12:46:35 +0200 mav  r270273 : #i81373# the change from the version attached to issue 2009-03-29 19:39:21 +0200 mav  r270191 : CWS-TOOLING: rebase CWS mav46 to trunk@270033 (milestone: DEV300:m45) 2009-02-27 09:07:23 +0100 mav  r268552 : #i99703# workaround Ubuntu problem 2009-02-25 12:51:27 +0100 mav  r268434 : #i81373# change the version of the extension 2009-02-25 11:25:15 +0100 mav  r268420 : #i81373# the filter installation needs at least 3.0 to work 2009-02-23 13:04:34 +0100 mav  r268352 : #i88682# use correct representation of CR in xml 2009-02-20 16:40:25 +0100 mav  r268334 : #i88682# show a warning if the page exists already 2009-02-20 15:42:10 +0100 mav  r268330 : #i99462# remove wiki filter from OOo installation 2009-02-20 15:39:57 +0100 mav  r268329 : #i99462# remove wiki filter from OOo installation 2009-02-20 15:36:41 +0100 mav  r268328 : #i99462# remove wiki filter from OOo installation 2009-02-20 15:33:29 +0100 mav  r268326 : #i99462# remove wiki filter from OOo installation 2009-02-20 14:19:45 +0100 mav  r268320 : #i88650# fix the typo 2009-02-20 13:02:38 +0100 mav  r268315 : #i81373# add math transformation into wiki filter 2009-02-20 11:46:20 +0100 mav  r268309 : #i81373# add the tarball 2009-02-19 09:46:10 +0100 mav  r268262 : #i82560# add wiki template into the extension 2009-02-19 07:26:45 +0100 mav  r268258 : #i82560#,#i81373# add wiki filter and template into the extension 2009-02-17 12:47:16 +0100 mav  r268115 : #i81373# commit the patch 2009-02-16 14:32:34 +0100 mav  r267814 : #i82560# rename the template 2009-02-16 14:29:40 +0100 mav  r267812 : #i82560# rename the template 2009-02-16 14:29:15 +0100 mav  r267811 : #i82560# rename the template 2009-02-16 12:16:23 +0100 mav  r267788 : #i90840# integrate the patch 2009-02-16 11:48:28 +0100 mav  r267786 : #i95568#,#i95340# improve parsing of html wiki-pages while looking for the server 2009-02-13 13:46:35 +0100 mav  r267720 : #i95568# fix typos 2009-02-13 13:38:42 +0100 mav  r267719 : #i96279# integrate the patch 2009-02-13 13:37:32 +0100 mav  r267718 : #i95568# check the retrieved URL 2009-02-13 11:39:55 +0100 mav  r267706 : #i95568# allow to connect to Wikipedia successfuly

---
+++ xsltml/makefile.mk
@@ -0,0 +1,69 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# $RCSfile: makefile.mk,v $
+#
+# $Revision: 1.6 $
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+PRJ=.
+
+PRJNAME=xsltml
+TARGET=xsltml
+
+.IF "$(ENABLE_MEDIAWIKI)" == "YES"
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=xsltml_2.1.2
+PATCH_FILES=$(TARFILE_NAME).patch
+TARFILE_IS_FLAT:=TRUE
+
+CONVERTFILES=\
+    README \
+    entities.xsl \
+    glayout.xsl \
+    mmltex.xsl \
+    cmarkup.xsl \
+    scripts.xsl \
+    tables.xsl \
+    tokens.xsl \
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :	set_ext.mk
+.INCLUDE :	target.mk
+.INCLUDE :	tg_ext.mk
+
+.ELSE
+@all:
+    @echo "MediaWiki Publisher extension disabled."
+.ENDIF
+
