49817b8e20f5eef9ac75b0a59fe1e7076f9eae6f
Ivo Hinkelmann <ihi@openoffice.org>
2009-04-22 14:05:37 +0000

    CWS-TOOLING: integrate CWS python26 2009-04-22 11:57:41 +0200 obo  r271091 : #i101079# mingw port (by tono) 2009-04-22 11:56:37 +0200 obo  r271090 : #i101079# mingw port (by tono) 2009-04-22 11:55:55 +0200 obo  r271089 : #i101079# mingw port (by tono) 2009-04-22 11:26:00 +0200 obo  r271084 : #i100492# Patch for FreeBSD (by maho) 2009-04-21 10:19:01 +0200 obo  r271020 : #i92413# make python buildable in Hamburg RE environment 2009-04-17 15:52:57 +0200 obo  r270952 : #i10000# fix for 4NT shell support 2009-04-17 15:16:39 +0200 hjs  r270951 : #i92413# - remove gcc warnings about new struct member. fetched fomr DEV300 2009-04-17 10:30:28 +0200 sb  r270927 : #i92413# adaptions for Solaris and/or bash 2009-04-16 00:36:32 +0200 jbu  r270861 : #i101080# patch for bash environment provided by tono 2009-04-15 13:50:01 +0200 rene  r270842 : #i100997# allow also python < 2.6 again 2009-03-12 14:23:32 +0100 rene  r269410 : cleanup: remove Python 2.3.4 - not referenced anymore 2009-03-11 21:48:58 +0100 jbu  r269356 : native python modules pyuno.dll -> pyuno.pyd (required by 2.6 python runtime) 2009-03-11 11:11:43 +0100 rene  r269307 : fix typo; regen configure 2009-03-10 11:44:54 +0100 rene  r269262 : update _AM_PYTHON_INTERPRETER_LIST 2009-03-10 00:30:09 +0100 jbu  r269233 : python26 now builds on windows (installation set not built yet) 2009-03-06 21:45:20 +0100 jbu  r269050 : python 2.6.1-linuxx86: PYTHONHOME variable removed from set_soenv.in (is now unecessary), added new and removed old python blob, linux x86 builds without patches, windows build still needs to be done and is now broken 2009-03-06 14:08:33 +0100 rene  r269004 : #i92413#

--- python/pyversion.mk
+++ python/pyversion.mk
@@ -1,8 +1,8 @@
 # when you want to change the python version, you must update the d.lst
 # in the python project accordingly !!!
 PYMAJOR=2
-PYMINOR=3
-PYMICRO=4
+PYMINOR=6
+PYMICRO=1
 PYVERSION=$(PYMAJOR).$(PYMINOR).$(PYMICRO)
 
 .IF "$(GUI)" == "UNX"
@@ -13,8 +13,8 @@ PY_FULL_DLL_NAME=libpython$(PYMAJOR).$(PYMINOR).so.1.0
 .ENDIF
 PYTHONLIB=-lpython$(PYMAJOR).$(PYMINOR)
 .ELIF "$(GUI)" == "OS2"
-PY_FULL_DLL_NAME=python24.dll
-PYTHONLIB=python24.lib
+PY_FULL_DLL_NAME=python$(PYMAJOR)$(PYMINOR).dll
+PYTHONLIB=python$(PYMAJOR)$(PYMINOR).lib
 .ELSE
 .IF "$(COM)" == "GCC"
 PY_FULL_DLL_NAME=libpython$(PYMAJOR).$(PYMINOR).dll
