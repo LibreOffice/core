e13174627c0787183487dd67f14d6e4664988693
Ivo Hinkelmann <ihi@openoffice.org>
2009-07-16 16:59:45 +0000

    CWS-TOOLING: integrate CWS mav53_DEV300 2009-06-29 13:42:56 +0200 hbrinkm  r273471 : #i101593# switched off optimization in table layout, that caused frames to be layouted incorrectly 2009-06-25 12:29:11 +0200 cd  r273376 : #i101774# Don't rely on initialize call for VistaFilePicker 2009-06-25 11:48:33 +0200 tl  r273373 : #i101420# Gnome accessibility fix 2009-06-24 18:16:49 +0200 od  r273361 : #i97309 method <XMLTextParagraphExport::exportListChange(..)> 	- export text:start-value at correct list item. 2009-06-23 14:11:44 +0200 b_michaelsen  r273281 : #i103032#: cutting of URL-parameters for usage tracking 2009-06-22 11:49:12 +0200 b_michaelsen  r273207 : #i102805#: Added usage tracking to SfxToolboxControl 2009-06-22 11:46:00 +0200 b_michaelsen  r273205 : whitespace fixes 2009-06-05 14:21:12 +0200 mav  r272683 : #i101741# check the file date only in document file locking context

--- sw/source/core/layout/tabfrm.cxx
+++ sw/source/core/layout/tabfrm.cxx
@@ -2146,7 +2146,9 @@ void SwTabFrm::MakeAll()
 
         if ( !bValidSize || !bValidPrtArea )
         {
-            const BOOL bOptLower = (Frm().*fnRect->fnGetHeight)() == 0;
+            // HB #i101593# no optimization as it leeds to not layouting certain nested tables
+            // const BOOL bOptLower = (Frm().*fnRect->fnGetHeight)() == 0;
+            const BOOL bOptLower = FALSE;
 
             const long nOldPrtWidth = (Prt().*fnRect->fnGetWidth)();
             const long nOldFrmWidth = (Frm().*fnRect->fnGetWidth)();
