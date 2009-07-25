f12ec65fe96fb1a0c5824b78f4a38154431eb184
Philipp Lohmann <pl@openoffice.org>
2009-07-25 10:40:41 +0000

    solve a rebase problem

--- starmath/util/makefile.mk
+++ starmath/util/makefile.mk
@@ -64,6 +64,7 @@ SHL1STDLIBS= \
             $(TKLIB) \
             $(VCLLIB) \
             $(SVLLIB)	\
+            $(I18NPAPERLIB)     \
             $(SOTLIB) \
             $(UNOTOOLSLIB) \
             $(TOOLSLIB) \
