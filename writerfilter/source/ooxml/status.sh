a7d244fab0a92e3cecb558de1a4e628e891fcc6b
Henning Brinkmann <hbrinkm@openoffice.org>
2009-07-27 11:02:24 +0200

    use <stage1> as outer tag. Status comments are located before case statement, not after. Use qname attribute instead of tokenid

--- writerfilter/source/ooxml/status.sh
+++ writerfilter/source/ooxml/status.sh
@@ -3,19 +3,18 @@
 SEARCHIN=$SRC_ROOT/writerfilter/source/dmapper
 
 echo '<?xml version="1.0"?>'
-echo "<analyze>"
+echo "<stage1>"
 
 xsltproc analyzemodel.xsl model.xml | tail -n +2
 
 mdfind -onlyin $SEARCHIN "case NS_" |
-xargs -J % grep -A 1 -n "case NS_" % |
-grep -v "^//" |
+xargs -J % grep -B 1 -n "case NS_" % |
 grep "cxx" |
 sed 's#'$SEARCHIN'##' |
 sed 's#\(^[^:]*\):\([0-9]*\):#<qname file="\1" line="\2"#' |
-sed 's#[/* ]*case \(NS_.*\):.*# tokenid="\1"/>#' |
+sed 's#[/* ]*case \(NS_.*\):.*# qname="\1"/>#' |
 sed 's#.*WRITERFILTERSTATUS: done: \([0-9]*\), planned: \([0-9]*\), spent: \([0-9]*\).*#<status done="\1" planned="\2" spent="\3"/>#' |
 sed 's#^.*-[0-9][0-9]*-.*$#<nostatus/>#' |
 grep -v "^--"
 
-echo "</analyze>"
+echo "</stage1>"
