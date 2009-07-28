7eaee88a1491c66c229ef3c786acd0e02d89e4e5
Henning Brinkmann <hbrinkm@openoffice.org>
2009-07-28 17:36:50 +0200

    - integrated stage2 - status infos DO follow case labels - do not take commented out lines - do not take lines containing "WRITERSTATUS:" into account if they have no "done: " inside

--- writerfilter/source/ooxml/status.sh
+++ writerfilter/source/ooxml/status.sh
@@ -2,19 +2,24 @@
 
 SEARCHIN=$SRC_ROOT/writerfilter/source/dmapper
 
+(
 echo '<?xml version="1.0"?>'
 echo "<stage1>"
 
 xsltproc analyzemodel.xsl model.xml | tail -n +2
 
 mdfind -onlyin $SEARCHIN "case NS_" |
-xargs -J % grep -B 1 -n "case NS_" % |
+xargs -J % grep -A 1 -n "case NS_" % |
 grep "cxx" |
+grep -v "//.*case NS_" |
 sed 's#'$SEARCHIN'##' |
 sed 's#\(^[^:]*\):\([0-9]*\):#<qname file="\1" line="\2"#' |
 sed 's#[/* ]*case \(NS_.*\):.*# qname="\1"/>#' |
-sed 's#.*WRITERFILTERSTATUS: done: \([0-9]*\), planned: \([0-9]*\), spent: \([0-9]*\).*#<status done="\1" planned="\2" spent="\3"/>#' |
+sed 's#.*WRITERFILTERSTATUS: done: \([0-9]*\), planned: \([0-9.]*\), spent: \([0-9.]*\).*#<status done="\1" planned="\2" spent="\3"/>#' |
+grep -v "WRITERFILTERSTATUS:" |
 sed 's#^.*-[0-9][0-9]*-.*$#<nostatus/>#' |
 grep -v "^--"
 
 echo "</stage1>"
+) |
+xsltproc analyzestage2.xsl -
