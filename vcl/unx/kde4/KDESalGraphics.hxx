782ba886a2c93f86acdfa681a52b5315f1380a6e
Release Engineers <releng@openoffice.org>
2009-08-07 10:16:34 +0000

    CWS-TOOLING: integrate CWS kde4int 2009-07-15 10:40:52 +0200 pl  r273993 : #i103288# configure 2009-07-09 14:57:24 +0200 pl  r273867 : #i103284# spin fields 2009-07-09 13:49:13 +0200 pl  r273864 : #i103284# min size for dropdown comboboxes 2009-07-09 13:46:11 +0200 pl  r273860 : #i103284# min size fro dropdown listboxes 2009-07-09 13:01:48 +0200 pl  r273857 : #i103284# fix most radiobuttons and checboxes, also on tabpages 2009-07-08 19:38:12 +0200 pl  r273847 : #i103290# clean up, enable KDE4 fpicker 2009-07-08 15:54:06 +0200 pl  r273837 : #i103284# background mode opaque doesn't look that good 2009-07-07 19:29:01 +0200 pl  r273814 : add kde4 directory and deliverables 2009-07-07 11:18:08 +0200 pl  r273786 : make compile with Qt 4.1 2009-07-02 19:20:47 +0200 pl  r273673 : #i103290# add: KDE4 file picker (thanks Roman Shtylman) 2009-07-02 19:16:07 +0200 pl  r273672 : #i103289# add: KDE4integration (thanks Roman Shtylman) 2009-07-02 19:01:20 +0200 pl  r273671 : #i103286# add: KDE4 checks (thanks Roman Shtylman) 2009-07-02 18:58:15 +0200 pl  r273670 : #i103286# initial import for KDE4 backend (thanks Roman Shtylman) 2009-07-02 18:08:06 +0200 pl  r273668 : #i103284# initial import for KDE4 plugin (thanks Roman Shtylman)

---
+++ vcl/unx/kde4/KDESalGraphics.hxx
@@ -0,0 +1,114 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2009 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#pragma once
+
+#include <rtl/string.hxx>
+#include <saldisp.hxx>
+#include <salgdi.h>
+
+/** handles graphics drawings requests and performs the needed drawing operations */
+class KDESalGraphics : public X11SalGraphics
+{
+    public:
+        KDESalGraphics() {}
+        virtual ~KDESalGraphics() {}
+
+        /**
+            What widgets can be drawn the native way.
+            @param type Type of the widget.
+            @param part Specification of the widget's part if it consists of more than one.
+            @return true if the platform supports native drawing of the widget type defined by part.
+        */
+        virtual BOOL IsNativeControlSupported( ControlType type, ControlPart part );
+
+        /** Test whether the position is in the native widget.
+            If the return value is TRUE, bIsInside contains information whether
+            aPos was or was not inside the native widget specified by the
+            type/part combination.
+        */
+        virtual BOOL hitTestNativeControl( ControlType type, ControlPart part,
+                                        const Region& rControlRegion, const Point& aPos,
+                                        SalControlHandle& rControlHandle, BOOL& rIsInside );
+        /** Draw the requested control described by part/nControlState.
+
+            @param rControlRegion
+            The bounding region of the complete control in VCL frame coordinates.
+
+            @param aValue
+            An optional value (tristate/numerical/string).
+
+            @param rControlHandle
+            Carries platform dependent data and is maintained by the SalFrame implementation.
+
+            @param aCaption
+            A caption or title string (like button text etc.)
+        */
+        virtual BOOL drawNativeControl( ControlType type, ControlPart part,
+                                        const Region& rControlRegion, ControlState nControlState,
+                                        const ImplControlValue& aValue, SalControlHandle& rControlHandle,
+                                        const rtl::OUString& aCaption );
+
+        /** Draw text on the widget.
+            OPTIONAL. Draws the requested text for the control described by part/nControlState.
+            Used if text is not drawn by DrawNativeControl().
+
+            @param rControlRegion The bounding region of the complete control in VCL frame coordinates.
+            @param aValue An optional value (tristate/numerical/string)
+            @param rControlHandle Carries platform dependent data and is maintained by the SalFrame implementation.
+            @param aCaption A caption or title string (like button text etc.)
+        */
+        virtual BOOL drawNativeControlText( ControlType, ControlPart,
+                                            const Region&, ControlState,
+                                            const ImplControlValue&, SalControlHandle&,
+                                            const rtl::OUString& ) { return false; }
+        /** Check if the bounding regions match.
+
+            If the return value is TRUE, rNativeBoundingRegion
+            contains the true bounding region covered by the control
+            including any adornment, while rNativeContentRegion contains the area
+            within the control that can be safely drawn into without drawing over
+            the borders of the control.
+
+            @param rControlRegion
+            The bounding region of the control in VCL frame coordinates.
+
+            @param aValue
+            An optional value (tristate/numerical/string)
+
+            @param rControlHandle
+            Carries platform dependent data and is maintained by the SalFrame implementation.
+
+            @param aCaption
+            A caption or title string (like button text etc.)
+        */
+        virtual BOOL getNativeControlRegion( ControlType type, ControlPart part,
+                                            const Region& rControlRegion, ControlState nControlState,
+                                            const ImplControlValue& aValue, SalControlHandle& rControlHandle,
+                                            const rtl::OUString& aCaption,
+                                            Region &rNativeBoundingRegion, Region &rNativeContentRegion );
+};
---
+++ vcl/unx/kde4/VCLKDEApplication.cxx
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2009 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "VCLKDEApplication.hxx"
+
+#define Region QtXRegion
+#include <QEvent>
+#undef Region
+
+#include "KDESalDisplay.hxx"
+
+VCLKDEApplication::VCLKDEApplication() :
+    KApplication()
+{
+    disp = 0;
+}
+
+bool VCLKDEApplication::x11EventFilter(XEvent* event)
+{
+    //if we have a display and the display consumes the event
+    //do not process the event in qt
+    if (disp && disp->Dispatch(event) > 0)
+    {
+        return true;
+    }
+
+    return false;
+}
