004098681b06be35ad163abf2ae7ef8c39212a5a
Vladimir Glazounov <vg@openoffice.org>
2009-08-26 12:47:18 +0000

    CWS-TOOLING: integrate CWS chart40 2009-08-05 10:28:53 +0200 iha  r274640 : CWS-TOOLING: rebase CWS chart40 to trunk@274622 (milestone: DEV300:m54) 2009-07-31 18:03:11 +0200 iha  r274546 : #i91420# number of lines on chart type dialog is incorrect 2009-07-23 15:51:13 +0200 iha  r274275 : correct detection of old chart files b6700820 2009-07-23 15:10:30 +0200 iha  r274273 : #i103287# some lines vanish on import of some old files (opacity-problem) 2009-07-23 13:42:10 +0200 iha  r274268 : #i103588# Chart in report cause crash 2009-07-23 10:57:14 +0200 iha  r274264 : #i95503# Enter key does not work in source ranges dialog 2009-07-22 10:25:02 +0200 iha  r274221 : #i103713# ambiguous && || 2009-07-20 12:59:02 +0200 iha  r274120 : #i103468# remove warning

--- xmloff/source/chart/SchXMLTools.hxx
+++ xmloff/source/chart/SchXMLTools.hxx
@@ -53,9 +53,13 @@ class SvXMLExport;
 
 namespace SchXMLTools
 {
+    bool isDocumentGeneratedWithOpenOfficeOlderThan2_0( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel);
     bool isDocumentGeneratedWithOpenOfficeOlderThan2_3( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel);
     bool isDocumentGeneratedWithOpenOfficeOlderThan2_4( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel);
     bool isDocumentGeneratedWithOpenOfficeOlderThan3_0( const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >& xChartModel);
+
+    void setBuildIDAtImportInfo( ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel > xModel
+        , ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > xImportInfo );
 
     enum SchXMLChartTypeEnum
     {
