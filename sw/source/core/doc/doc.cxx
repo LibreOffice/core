017ae40dc058017a2e39f2ad3b8d6a14c988ce25
Frank Schönheit <fs@openoffice.org>
2009-09-14 12:24:42 +0000

    #cr6875455# properly pass the reference device to the SdrModel (patch approved by od)

--- sw/source/core/doc/doc.cxx
+++ sw/source/core/doc/doc.cxx
@@ -465,6 +465,8 @@ void SwDoc::setPrinter(/*[in]*/ SfxPrinter *pP,/*[in]*/ bool bDeleteOld,/*[in]*/
         if ( bDeleteOld )
             delete pPrt;
         pPrt = pP;
+        if( pDrawModel )
+            pDrawModel->SetRefDevice( pP );
     }
 
     if ( bCallPrtDataChanged &&
@@ -493,6 +495,8 @@ void SwDoc::setVirtualDevice(/*[in]*/ VirtualDevice* pVd,/*[in]*/ bool bDeleteOl
         if ( bDeleteOld )
             delete pVirDev;
         pVirDev = pVd;
+        if( pDrawModel )
+            pDrawModel->SetRefDevice( pVd );
     }
 }
 
