b0fa467ebb7c8c124ea433b3562fcbae0cbfe197
Mathias Bauer <mba@openoffice.org>
2009-09-19 00:02:22 +0200

    better dependencies for sdi files

--- svx/sdi/makefile.mk
+++ svx/sdi/makefile.mk
@@ -1,7 +1,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -45,10 +45,11 @@ SDI1EXPORT=svx
 # --- Files --------------------------------------------------------
 
 SVSDI1DEPEND= \
+        $(SOLARINCXDIR)$/sfx2/sfx.sdi \
+        $(SOLARINCXDIR)$/sfx2/sfxitems.sdi \
         svxitems.sdi \
     fmslots.sdi \
     svx.sdi	\
-    $(SOLARINCXDIR)$/sfxslots.ilb	\
     xoitems.sdi \
     svxitems.sdi
 
