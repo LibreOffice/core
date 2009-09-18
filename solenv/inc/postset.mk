014467d75e9b12eaae3065449adc12fdcd5a6b8b
Jens-Heiner Rechtien <hr@openoffice.org>
2009-09-18 10:15:03 +0000

    CWS-TOOLING: integrate CWS mba32issues02 2009-09-17 mb93783 merge commit 2009-09-03 tb121644 #i104748 - slot unification to .uno:PasteSpecial 2009-09-01 mba #101455#: code simplification 2009-08-31 mb93783 iso locales for norwegian builds are nb and nn, not no 2009-08-25 mba merge to m55 2009-07-29 mba cleanup after rebase 2009-07-29 mba #i103200#: wrong order of first and last name in CJK UI 2009-07-27 mba adding dictionary changes from broken svn CWS 2009-07-27 mba adding dictionary changes from broken svn CWS 2009-07-27 mba apply patch from broken svn CWS

--- solenv/inc/postset.mk
+++ solenv/inc/postset.mk
@@ -1,7 +1,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -35,6 +35,7 @@
 defaultlangiso=en-US
 
 # Complete list of all supported ISO codes
+
 completelangiso=\
     af \
     ar \
@@ -111,6 +112,7 @@ completelangiso=\
     pl \
     pt \
     pt-BR \
+    ro \
     ru \
     rw \
     sa-IN \
