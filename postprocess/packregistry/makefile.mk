495174f29290f2c01d025c640df8e7e22bf382af
sb <sb@openoffice.org>
2009-10-12 16:34:39 +0200

    #i101955# made binfilter.xcd conditional on WITH_BINFILTER

--- postprocess/packregistry/makefile.mk
+++ postprocess/packregistry/makefile.mk
@@ -38,7 +38,6 @@ MY_MOD = $(SOLARXMLDIR)/registry/spool
 MY_XCDS = \
     $(MISC)/base.xcd \
     $(MISC)/brand.xcd \
-    $(MISC)/binfilter.xcd \
     $(MISC)/calc.xcd \
     $(MISC)/draw.xcd \
     $(MISC)/graphicfilter.xcd \
@@ -72,25 +71,6 @@ MY_FILES_brand = \
 MY_FILES_brand += \
     $(MY_MOD)/org/openoffice/Office/Jobs/Jobs-registration.xcu
 .END
-
-MY_DEPS_binfilter = main
-MY_FILES_binfilter = \
-    $(MY_MOD)/fcfg_chart_bf_filters.xcu \
-    $(MY_MOD)/fcfg_chart_bf_types.xcu \
-    $(MY_MOD)/fcfg_calc_bf_filters.xcu \
-    $(MY_MOD)/fcfg_calc_bf_types.xcu \
-    $(MY_MOD)/fcfg_draw_bf_filters.xcu \
-    $(MY_MOD)/fcfg_draw_bf_types.xcu \
-    $(MY_MOD)/fcfg_global_bf_filters.xcu \
-    $(MY_MOD)/fcfg_global_bf_types.xcu \
-    $(MY_MOD)/fcfg_impress_bf_filters.xcu \
-    $(MY_MOD)/fcfg_impress_bf_types.xcu \
-    $(MY_MOD)/fcfg_math_bf_filters.xcu \
-    $(MY_MOD)/fcfg_math_bf_types.xcu \
-    $(MY_MOD)/fcfg_web_bf_filters.xcu \
-    $(MY_MOD)/fcfg_web_bf_types.xcu \
-    $(MY_MOD)/fcfg_writer_bf_filters.xcu \
-    $(MY_MOD)/fcfg_writer_bf_types.xcu
 
 MY_DEPS_calc = main
 MY_FILES_calc = \
@@ -422,6 +402,28 @@ MY_DEPS_xsltfilter = main
 MY_FILES_xsltfilter = \
     $(MY_MOD)/fcfg_xslt_filters.xcu \
     $(MY_MOD)/fcfg_xslt_types.xcu
+
+.IF "$(WITH_BINFILTER)" != "NO"
+MY_XCDS += $(MISC)/binfilter.xcd
+MY_DEPS_binfilter = main
+MY_FILES_binfilter = \
+    $(MY_MOD)/fcfg_chart_bf_filters.xcu \
+    $(MY_MOD)/fcfg_chart_bf_types.xcu \
+    $(MY_MOD)/fcfg_calc_bf_filters.xcu \
+    $(MY_MOD)/fcfg_calc_bf_types.xcu \
+    $(MY_MOD)/fcfg_draw_bf_filters.xcu \
+    $(MY_MOD)/fcfg_draw_bf_types.xcu \
+    $(MY_MOD)/fcfg_global_bf_filters.xcu \
+    $(MY_MOD)/fcfg_global_bf_types.xcu \
+    $(MY_MOD)/fcfg_impress_bf_filters.xcu \
+    $(MY_MOD)/fcfg_impress_bf_types.xcu \
+    $(MY_MOD)/fcfg_math_bf_filters.xcu \
+    $(MY_MOD)/fcfg_math_bf_types.xcu \
+    $(MY_MOD)/fcfg_web_bf_filters.xcu \
+    $(MY_MOD)/fcfg_web_bf_types.xcu \
+    $(MY_MOD)/fcfg_writer_bf_filters.xcu \
+    $(MY_MOD)/fcfg_writer_bf_types.xcu
+.ENDIF
 
 .IF "$(GUIBASE)" == "unx" && "$(ENABLE_GCONF)" == "TRUE" && \
         "$(ENABLE_LOCKDOWN)" == "YES"
