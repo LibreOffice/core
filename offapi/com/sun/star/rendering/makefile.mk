253dada8bed749112fd2c78ceddfeb7478944598
thb <thb@openoffice.org>
2009-10-16 00:43:16 +0200

    #i105937# Much improved gradient support for canvas/basegfx/drawinglayer.
    See http://blog.thebehrens.net/2009/07/28/hackweek-iv-canvas-convwatch/ for more background information

--- offapi/com/sun/star/rendering/makefile.mk
+++ offapi/com/sun/star/rendering/makefile.mk
@@ -108,7 +108,6 @@ IDLFILES=\
     XIntegerReadOnlyBitmap.idl \
     XLinePolyPolygon2D.idl \
     XParametricPolyPolygon2D.idl \
-    XParametricPolyPolygon2DFactory.idl \
     XPolyPolygon2D.idl \
     XSimpleCanvas.idl \
     XSprite.idl \
