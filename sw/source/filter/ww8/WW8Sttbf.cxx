e0d2085017d182cde44e145f3e8bfc428dec6e21
Kurt Zenker <kz@openoffice.org>
2009-10-15 16:29:40 +0000

    CWS-TOOLING: integrate CWS c29v001a 2009-10-06 hb #160731# use SetReadOnlyUI instead of item at medium to get read only functionality 2009-10-05 hb #160731# added virtual destructor to IDocumentExternal data to make compilable under wntmsci12 2009-10-02 hb #160731# store fReadOnlyRecommended/fWriteReservation in WW8FibData 2009-10-02 hb #160731# handle stepping of offset correctly when reading strings 2009-10-02 hb #160731# write SttbAssoc/fReadOnlyRecommended/fWriteReservation 2009-10-02 hb #160731# Added new class WW8FibData 2009-10-02 hb #160731# new identifier for external type: FIB 2009-10-02 hb #160731# Added new class WW8FibData 2009-10-02 hb dbg_out for OUString 2009-10-01 hb read and write SttbfAssoc 2009-10-01 hb store external data in SwDoc 2009-09-29 hb merged DEV300_m60 2009-09-29 hb merged changes from private repository 2009-09-04 Henning Brinkmann added WW8Sttb for reading the WW8Sttb 2009-09-15 hb137859 #160401# made compilable 2009-09-15 hb137859 merged changes by hb 2009-09-15 sj93757 #160376# fixed shadow problem 2009-09-15 Henning Brinkmann merged changes for #160401# 2009-09-01 Henning Brinkmann #160401# added fReadOnlyRecommended and fWriteReservation to WW8Fib. If one of those is set, the document is read read-only.

---
+++ sw/source/filter/ww8/WW8Sttbf.cxx
@@ -0,0 +1,121 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: ww8par2.hxx,v $
+ * $Revision: 1.47.214.1 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*- */
+
+#include <iostream>
+#include <dbgoutsw.hxx>
+#include "WW8Sttbf.hxx"
+
+namespace ww8
+{
+    WW8Struct::WW8Struct(SvStream& rSt, sal_uInt32 nPos, sal_uInt32 nSize)
+    : mn_offset(0), mn_size(nSize)
+    {
+        rSt.Seek(nPos);
+
+        mp_data.reset(new BYTE[nSize]);
+        rSt.Read(mp_data.get(), nSize);
+    }
+
+    WW8Struct::WW8Struct(WW8Struct * pStruct, sal_uInt32 nPos, sal_uInt32 nSize)
+    : mp_data(pStruct->mp_data), mn_offset(pStruct->mn_offset + nPos),
+    mn_size(nSize)
+    {
+    }
+
+    WW8Struct::~WW8Struct()
+    {
+    }
+
+    sal_uInt8 WW8Struct::getU8(sal_uInt32 nOffset)
+    {
+        sal_uInt8 nResult = 0;
+
+        if (nOffset < mn_size)
+        {
+            nResult = mp_data[mn_offset + nOffset];
+        }
+
+        return nResult;
+    }
+
+    ::rtl::OUString WW8Struct::getUString(sal_uInt32 nOffset,
+                                          sal_uInt32 nCount)
+    {
+        ::rtl::OUString aResult;
+
+        if (nCount > 0)
+        {
+            rtl_uString * pNew = 0;
+            rtl_uString_newFromStr_WithLength
+            (&pNew, reinterpret_cast<const sal_Unicode *>(&mp_data[mn_offset + nOffset]),
+             nCount);
+
+            aResult = rtl::OUString(pNew);
+        }
+
+#ifdef DEBUG
+        char sBuffer[256];
+        snprintf(sBuffer, sizeof(sBuffer), "offset=\"%" SAL_PRIuUINT32 "\" count=\"%" SAL_PRIuUINT32 "\"",
+                 nOffset, nCount);
+        ::std::clog << "<WW8Struct-getUString" << sBuffer << ">"
+                    << dbg_out(aResult) << "</WW8Struct-getUString>"
+                    << ::std::endl;
+#endif
+
+        return aResult;
+
+    }
+
+    ::rtl::OUString WW8Struct::getString(sal_uInt32 nOffset,
+                                         sal_uInt32 nCount)
+    {
+        ::rtl::OUString aResult;
+
+        if (nCount > 0)
+        {
+            ::rtl::OString aOStr(reinterpret_cast<const sal_Char *>(&mp_data[mn_offset + nOffset]),
+                                 nCount);
+            ::rtl::OUString aOUStr(OStringToOUString(aOStr, RTL_TEXTENCODING_ASCII_US));
+            aResult = rtl::OUString(aOUStr);
+        }
+
+#ifdef DEBUG
+        char sBuffer[256];
+        snprintf(sBuffer, sizeof(sBuffer), "offset=\"%" SAL_PRIuUINT32 "\" count=\"%" SAL_PRIuUINT32 "\"",
+                 nOffset, nCount);
+        ::std::clog << "<WW8Struct-getString " << sBuffer << ">"
+                    << dbg_out(aResult) << "</WW8Struct-getUString>"
+                    << ::std::endl;
+#endif
+
+        return aResult;
+    }
+}
