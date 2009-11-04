da23564a52f5c0abd6f60ff60e16f1816c4cc9d9
Oliver Bolte <obo@openoffice.org>
2009-11-04 11:12:54 +0000

    CWS-TOOLING: integrate CWS tkr27 2009-10-28 06:49:21 +0100 tkr  r277234 : #105418# rebase issue SYSSHELLLIB doesn't exist anymore 2009-10-27 12:25:20 +0100 tkr  r277198 : #105418# rebase issue SYSSHELLLIB doesn't exist anymore 2009-10-26 08:06:52 +0100 tkr  r277172 : #105418# rename -lineon to -lneon for mingw port 2009-10-22 14:58:56 +0200 tkr  r277125 : #i105418# rename neon lib name 2009-10-22 14:56:48 +0200 tkr  r277123 : #i105418# rename neon lib name 2009-10-14 13:33:53 +0200 tkr  r276894 : #i105891# windows ifilter relocated 2009-10-12 12:12:43 +0200 tkr  r276826 : CWS-TOOLING: rebase CWS tkr27 to branches/OOO320@276773 (milestone: OOO320:m1) 2009-10-08 12:50:50 +0200 tkr  r276787 : #i105418# introduced new defines for special_lib_file 2009-10-08 12:48:08 +0200 tkr  r276786 : #i105418# build neon as shared library 2009-10-08 12:13:35 +0200 tkr  r276782 : #i105418# build neon as shared library 2009-09-29 12:46:16 +0200 tkr  r276523 : #i105418# build neon as shared library 2009-09-29 12:36:15 +0200 tkr  r276522 : #i105418# build neon as shared library 2009-09-29 12:17:21 +0200 tkr  r276519 : #i105418# build neon as shared library 2009-09-29 11:11:33 +0200 tkr  r276512 : #i105418# build neon as shared library 2009-09-29 10:39:19 +0200 tkr  r276509 : #i105418# build neon as shared library 2009-09-29 10:36:45 +0200 tkr  r276508 : #i105418# build neon as shared library

--- solenv/inc/libs.mk
+++ solenv/inc/libs.mk
@@ -220,12 +220,14 @@ JPEG3RDLIB=-ljpeg
 .ELSE
 JPEG3RDLIB=-ljpeglib
 .ENDIF
-.IF "$(SYSTEM_NEON)" == "YES" || "$(GUI)$(COM)"=="WNTGCC"
+.IF "$(SYSTEM_NEON)" == "YES"
+NEON3RDLIB=-lneon
+.ELIF "$(GUI)$(COM)"=="WNTGCC"
 NEON3RDLIB=-lneon
 .ELIF "$(OS)" == "MACOSX"
-NEON3RDLIB=$(SOLARLIBDIR)/libneon.a
+NEON3RDLIB=$(SOLARLIBDIR)/libneon.dylib
 .ELSE
-NEON3RDLIB=$(STATIC) -lneon $(DYNAMIC)
+NEON3RDLIB=-lneon
 .ENDIF
 .IF "$(SYSTEM_DB)" == "YES"
 BERKELEYLIB=-ldb
@@ -424,6 +426,7 @@ MAILLIB=mail.lib
 DOCMGRLIB=docmgr.lib
 BASICLIB=basic.lib
 TKTLIB=tkt.lib
+SJLIB=sj.lib
 SVXCORELIB=isvxcore.lib
 SVXMSFILTERLIB=isvxmsfilter.lib
 SVXLIB=isvx.lib
@@ -439,7 +442,7 @@ EXPAT3RDLIB=expat_xmltok.lib expat_xmlparse.lib
 EXPATASCII3RDLIB=expat_xmltok.lib ascii_expat_xmlparse.lib
 ZLIB3RDLIB=zlib.lib
 JPEG3RDLIB=jpeglib.lib
-NEON3RDLIB=neon.lib
+NEON3RDLIB=ineon.lib
 BERKELEYLIB=libdb47.lib
 CURLLIB=libcurl.lib
 CHAOSLIB=ichaos.lib
