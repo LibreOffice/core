f949bcea10117e99c731831eae335bf1d6793d0a
Frank Schoenheit [fs] <frank.schoenheit@sun.com>
2009-11-05 14:31:55 +0100

    when creating a new HsqlDatabase, make sure it does not appear in the pick list

--- connectivity/qa/connectivity/tools/HsqlDatabase.java
+++ connectivity/qa/connectivity/tools/HsqlDatabase.java
@@ -30,6 +30,7 @@
 package connectivity.tools;
 
 import com.sun.star.beans.PropertyValue;
+import com.sun.star.beans.PropertyState;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.container.ElementExistException;
 import com.sun.star.frame.XStorable;
@@ -82,9 +83,9 @@ public class HsqlDatabase extends AbstractDatabase
         dsProperties.setPropertyValue("URL", "sdbc:embedded:hsqldb");
 
         final XStorable storable = (XStorable) UnoRuntime.queryInterface(XStorable.class, m_databaseDocument);
-        storable.storeAsURL(m_databaseDocumentFile, new PropertyValue[]
-                {
-                });
+        storable.storeAsURL( m_databaseDocumentFile, new PropertyValue[]
+            {   new PropertyValue( "PickListEntry", 0, false, PropertyState.DIRECT_VALUE )
+            } );
     }
 
     /** drops the table with a given name
