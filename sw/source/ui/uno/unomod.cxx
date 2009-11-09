b45d1411b305728e9f605779c3deb82a9fd74489
os <os@openoffice.org>
2009-11-09 08:41:33 +0100

    #i106342# change reverted

--- sw/source/ui/uno/unomod.cxx
+++ sw/source/ui/uno/unomod.cxx
@@ -576,17 +576,17 @@ void SwXPrintSettings::_getSingleValue( const comphelper::PropertyInfo & rInfo, 
         break;
         case HANDLE_PRINTSET_PROSPECT_RTL:
         {
-            bBoolVal = mpPrtOpt->IsPrintProspect_RTL();
+            rValue <<= mpPrtOpt->IsPrintProspect_RTL();
         }
         break;
         case HANDLE_PRINTSET_PLACEHOLDER:
         {
-            bBoolVal = mpPrtOpt->IsPrintTextPlaceholder();
+            rValue <<= mpPrtOpt->IsPrintTextPlaceholder();
         }
         break;
         case HANDLE_PRINTSET_HIDDEN_TEXT:
         {
-            bBoolVal = mpPrtOpt->IsPrintHiddenText();
+            rValue <<= mpPrtOpt->IsPrintHiddenText();
         }
         break;
         default:
