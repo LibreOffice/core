f2a492bbf52cffbbd146685cf1c5f82202ec53c7
Joachim Lingner <jl@openoffice.org>
2009-11-09 16:50:39 +0100

    jl137: #i103420# better allow tracing in product built depending on bootstrap var XMLSECURITY_TRACE

---
+++ xmlsecurity/source/xmlsec/diagnose.cxx
@@ -0,0 +1,77 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * OpenOffice.org - a multi-platform office productivity suite
+ *
+ * $RCSfile: xmlstreamio.hxx,v $
+ * $Revision: 1.3 $
+ *
+ * This file is part of OpenOffice.org.
+ *
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+
+#include "diagnose.hxx"
+#include <stdio.h>
+#include <stdarg.h>
+#include "rtl/instance.hxx"
+#include "rtl/bootstrap.hxx"
+
+namespace xmlsecurity {
+
+struct UseDiagnose : public rtl::StaticWithInit<
+    const bool, UseDiagnose>
+{
+    const bool operator () ()
+    {
+        ::rtl::OUString value;
+        sal_Bool res = rtl::Bootstrap::get(
+            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("XMLSECURITY_TRACE")), value);
+        return res == sal_True ? true : false;
+    }
+};
+
+/* the function will print the string when
+   - build with debug
+   - the bootstrap variable XMLSECURITY_DIAGNOSE is set.
+ */
+void xmlsec_trace(const char* pszFormat, ...)
+{
+    bool bDebug = false;
+
+#if OSL_DEBUG_LEVEL > 1
+    bDebug = true;
+#endif
+    if (bDebug || UseDiagnose::get())
+    {
+        va_list args;
+        fprintf(stderr, "[xmlsecurity] ");
+        va_start(args, pszFormat);
+        vfprintf(stderr, pszFormat, args);
+        va_end(args);
+
+        fprintf(stderr,"\n");
+        fflush(stderr);
+    }
+}
+
+
+
+}
