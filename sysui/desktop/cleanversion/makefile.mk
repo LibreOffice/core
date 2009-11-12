fcee26267f09e01e126a72236b494525109e955a
Vladimir Glazunov <vg@openoffice.org>
2009-11-12 12:44:18 +0100

    #i106852# fix because of solaris missing grep fuctionality

--- sysui/desktop/cleanversion/makefile.mk
+++ sysui/desktop/cleanversion/makefile.mk
@@ -43,7 +43,7 @@ ALLTAR: $(MISC)$/cleanup.flag
 
 # version changed - bin previous trees
 $(MISC)$/cleanup.flag : ..$/productversion.mk
-    rm -rf `ls $(MISC) | grep -v -x $(MISC)$/logs` && $(TOUCH) $@
+    rm -rf `ls -d ../../unxsols4.pro/misc/* | sed '/\/logs$/d'` && $(TOUCH) $@
     
 .ENDIF			# "$(MISC)"!=""
     
