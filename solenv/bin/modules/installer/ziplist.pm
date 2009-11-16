6c035c5af36c52d3f994976021d14633d13abbaf
Hans-Joachim Lankenau <hjs@openoffice.org>
2009-11-16 16:25:14 +0100

    #i10000# packing without UPDMINOREXT set

--- solenv/bin/modules/installer/ziplist.pm
+++ solenv/bin/modules/installer/ziplist.pm
@@ -493,7 +493,7 @@ sub replace_minor_in_pathes
     {
         my $line = ${$patharrayref}[$i];
 
-        if ((! defined $ENV{CWS_WORK_STAMP}) or (defined $ENV{UPDMINOREXT}) )
+        if ( ! defined $ENV{CWS_WORK_STAMP} and defined $ENV{UPDMINOR} )
 #       if ( $installer::globals::minor )
         {
             $line =~ s/\{minor\}/$installer::globals::minor/g;
