3b932a289f87ee4218fc916fa18aa21e79edda59
Frank Schoenheit [fs] <frank.schoenheit@sun.com>
2009-11-27 21:16:24 +0100

    [CWS autorecovery] merged SfxTopFrame into SfxFrame SfxTopFrame was the only class deriving from the abstract class SfxFrame, so both can effectively be treated as one class.

--- sd/source/ui/slideshow/slideshow.cxx
+++ sd/source/ui/slideshow/slideshow.cxx
@@ -1202,7 +1202,7 @@ void SlideShow::StartFullscreenPresentation( )
         // The new frame is created hidden.  To make it visible and activate the
         // new view shell--a prerequisite to process slot calls and initialize
         // its panes--a GrabFocus() has to be called later on.
-        SfxTopFrame* pNewFrame = SfxTopFrame::Create( mpDoc->GetDocSh(), pWorkWindow, PRESENTATION_FACTORY_ID, TRUE);
+        SfxFrame* pNewFrame = SfxFrame::Create( mpDoc->GetDocSh(), *pWorkWindow, PRESENTATION_FACTORY_ID, true );
         pNewFrame->SetPresentationMode(TRUE);
 
         mpFullScreenViewShellBase = static_cast<ViewShellBase*>(pNewFrame->GetCurrentViewFrame()->GetViewShell());
