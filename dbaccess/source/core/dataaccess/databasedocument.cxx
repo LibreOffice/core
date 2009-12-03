2cf617e18ddb7b98e2288616b5a2ad12c5a5d0da
Mathias Bauer <mba@openoffice.org>
2009-12-03 08:37:11 +0100

    more resync fixes

--- dbaccess/source/core/dataaccess/databasedocument.cxx
+++ dbaccess/source/core/dataaccess/databasedocument.cxx
@@ -73,7 +73,7 @@
 #include <comphelper/storagehelper.hxx>
 #include <cppuhelper/exc_hlp.hxx>
 #include <framework/titlehelper.hxx>
-#include <svtools/saveopt.hxx>
+#include <unotools/saveopt.hxx>
 #include <tools/debug.hxx>
 #include <tools/diagnose_ex.h>
 #include <tools/errcode.hxx>
