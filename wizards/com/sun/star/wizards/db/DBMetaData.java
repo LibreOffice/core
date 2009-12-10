5005736eb6fca43acc38c0660f7beda4a0359bb7
Ocke Janssen [oj] <Ocke.Janssen@sun.com>
2009-12-10 11:39:59 +0100

    dba33d: merge

--- wizards/com/sun/star/wizards/db/DBMetaData.java
+++ wizards/com/sun/star/wizards/db/DBMetaData.java
@@ -597,13 +597,8 @@ public class DBMetaData
 
     public void getDataSourceInterfaces() throws Exception
     {
-<<<<<<< local
-        xCompleted = UnoRuntime.queryInterface( XCompletedConnection.class, m_dataSource );
-        xDataSourcePropertySet = UnoRuntime.queryInterface( XPropertySet.class, m_dataSource );
-=======
-        xCompleted = (XCompletedConnection) UnoRuntime.queryInterface(XCompletedConnection.class, getDataSource());
-        xDataSourcePropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, getDataSource());
->>>>>>> other
+        xCompleted = UnoRuntime.queryInterface(XCompletedConnection.class, getDataSource());
+        xDataSourcePropertySet = UnoRuntime.queryInterface(XPropertySet.class, getDataSource());
         bPasswordIsRequired = ((Boolean) xDataSourcePropertySet.getPropertyValue("IsPasswordRequired")).booleanValue();
     }
 
@@ -686,11 +681,7 @@ public class DBMetaData
     private boolean getConnection(String _DataSourceName)
     {
         setDataSourceByName(_DataSourceName, true);
-<<<<<<< local
-        return getConnection(m_dataSource);
-=======
         return getConnection( getDataSource() );
->>>>>>> other
     }
 
     private boolean getConnection(com.sun.star.sdbc.XConnection _DBConnection)
