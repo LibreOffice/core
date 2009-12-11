8efa42c387b62e668e70502928492ee4ef32d44e
sb <sb@openoffice.org>
2009-12-11 14:29:59 +0100

    sb118: Replace (heavily modified) CppUnit 1.8.0 with (unmodified) latest 1.12.1. - Old modifications were necessary for testshl2, which has simply been excluded   from the build for now. - Tests in basebmp, basegfx, o3tl (that are executed during build) have been   converted from using modified CppUnit/testshl2 to using unmodified CppUnit. - CppUnit's DllPlugInTester has problems with OOo tests on Windows, see   #i107562#, so for now a new cppunittester from sal is used instead.

--- cppunit/makefile.mk
+++ cppunit/makefile.mk
@@ -1,14 +1,13 @@
 #*************************************************************************
-#
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
-# $RCSfile: makefile.mk,v $
+# $RCSfile: makefile,v $
 #
-# $Revision: 1.1.2.3 $
+# $Revision: 1.4 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -26,106 +25,123 @@
 # version 3 along with OpenOffice.org.  If not, see
 # <http://www.openoffice.org/license.html>
 # for a copy of the LGPLv3 License.
-#
-#*************************************************************************
+#***********************************************************************/
 
-PRJ=.
+PRJ = .
+PRJNAME = cppunit
+TARGET = cppunit
 
-PRJNAME=so_cppunit
-TARGET=so_cppunit
+.INCLUDE: settings.mk
 
-# --- Settings -----------------------------------------------------
+TARFILE_NAME = cppunit-1.12.1
+    # from <https://sourceforge.net/projects/cppunit/files/cppunit/1.12.1/
+    #  cppunit-1.12.1.tar.gz/download>
 
-.INCLUDE :	settings.mk
+PATCH_FILES = solarisfinite.patch warnings.patch
+    # solarisfinite.patch: see <https://sourceforge.net/tracker/?func=detail&
+    #  aid=2912590&group_id=11795&atid=311795>
+    # warnings.patch: see <https://sourceforge.net/tracker/?func=detail&
+    #  aid=2912630&group_id=11795&atid=311795>
 
-# --- Files --------------------------------------------------------
+.IF "$(OS)" == "WNT" && "$(COM)" == "MSC"
 
-TARFILE_NAME=cppunit-1.8.0
+# On Windows, CppUnit appears to support either the Unix-style configure/make
+# approach with cygwin and gcc (and libtool fails miserably if gcc is replaced
+# by MSVC cl), or a Visual Studio project (CppUnitLibraries.dsw).  However, the
+# latter is too old to work with vcbuild from a wntmsci12 tool chain.  So, the
+# easiest way appears to inject hand-crafted dmake makefiles to build the
+# relevant parts.  (Another approach would be to manually use the Visual Studio
+# IDE to convert CppUnitLibraries.dsw to modern format, inject the resulting
+# cppunit_dll.vcproj and DllPlugInTester.vcproj and build those with vcbuild.)
 
-MOVED_FILES=\
- include$/cppunit$/result$/SynchronizedObject.h \
- include$/cppunit$/result$/TestListener.h \
- include$/cppunit$/result$/TestResultCollector.h \
- include$/cppunit$/result$/TestResult.h \
- include$/cppunit$/result$/TestSucessListener.h \
- include$/cppunit$/result$/TextTestResult.h \
- include$/cppunit$/portability$/config-bcb5.h \
- include$/cppunit$/portability$/config-msvc6.h \
- src$/result$/SynchronizedObject.cpp \
- src$/result$/TestResultCollector.cpp \
- src$/result$/TestResult.cpp \
- src$/result$/TestSucessListener.cpp \
- src$/result$/TextTestResult.cpp
+CONFIGURE_ACTION = $(SED) -e s:@BACKPATH@:../$(BACK_PATH): \
+    < ../$(BACK_PATH)$(PATH_IN_MODULE)/ooo-cppunit_dll.mk \
+    > src/cppunit/ooo-cppunit_dll.mk && \
+    $(SED) -e s:@BACKPATH@:../$(BACK_PATH): \
+    < ../$(BACK_PATH)$(PATH_IN_MODULE)/ooo-DllPlugInTester.mk \
+    > src/DllPlugInTester/ooo-DllPlugInTester.mk
 
-ADDITIONAL_FILES=\
- $(MOVED_FILES) \
- include$/cppunit$/additionalfunc.hxx \
- include$/cppunit$/autoregister$/callbackfunc_fktptr.h  \
- include$/cppunit$/autoregister$/callbackstructure.h \
- include$/cppunit$/autoregister$/htestresult.h \
- include$/cppunit$/autoregister$/registerfunc.h \
- include$/cppunit$/autoregister$/registertestfunction.h \
- include$/cppunit$/autoregister$/testfunc.h \
- include$/cppunit$/checkboom.hxx \
- include$/cppunit$/cmdlinebits.hxx \
- include$/cppunit$/externcallbackfunc.hxx \
- include$/cppunit$/joblist.hxx \
- include$/cppunit$/nocopy.hxx \
- include$/cppunit$/portability$/config-auto.h \
- include$/cppunit$/result$/callbackfunc.h \
- include$/cppunit$/result$/emacsTestResult.hxx \
- include$/cppunit$/result$/log.hxx \
- include$/cppunit$/result$/optionhelper.hxx \
- include$/cppunit$/result$/outputter.hxx \
- include$/cppunit$/result$/testshlTestResult.h \
- include$/cppunit$/signaltest.h \
- include$/cppunit$/simpleheader.hxx \
- include$/cppunit$/stringhelper.hxx \
- include$/cppunit$/taghelper.hxx \
- include$/cppunit$/tagvalues.hxx \
- include$/testshl$/autoregisterhelper.hxx \
- include$/testshl$/dynamicregister.hxx \
- include$/testshl$/filehelper.hxx \
- include$/testshl$/getopt.hxx \
- include$/testshl$/log.hxx \
- include$/testshl$/tresstatewrapper.h \
- include$/testshl$/tresstatewrapper.hxx \
- include$/testshl$/versionhelper.hxx \
- include$/testshl$/winstuff.hxx \
- include$/makefile.mk \
- makefile.mk \
- src$/makefile.mk \
- src$/cppunit$/cmdlinebits.cxx \
- src$/cppunit$/joblist.cxx \
- src$/cppunit$/makefile.mk \
- src$/cppunit$/registertestfunction.cxx \
- src$/cppunit$/signaltest.cxx \
- src$/cppunit$/t_print.cxx \
- src$/cppunit$/tresregister.cxx \
- src$/cppunit$/tresstatewrapper.cxx \
- src$/result$/emacsTestResult.cxx \
- src$/result$/log.cxx \
- src$/result$/makefile.mk \
- src$/result$/optionhelper.cxx \
- src$/result$/outputter.cxx \
- src$/result$/signal.cxx \
- src$/result$/signal.hxx \
- src$/result$/testshlTestResult.cxx \
- src$/result$/treswrapper.cxx \
- src$/win32$/makefile.mk \
- src$/win32$/winstuff.cxx
+BUILD_ACTION = cd src/cppunit && dmake -f ooo-cppunit_dll.mk && \
+    cd ../DllPlugInTester && dmake -f ooo-DllPlugInTester.mk
 
-PATCH_FILES=cppunit-1.8.0-r1.patch
+OUTDIR2INC = include/cppunit
 
-# CONFIGURE_DIR=
-# CONFIGURE_ACTION=
-# 
-# BUILD_DIR=
-BUILD_ACTION=dmake $(MFLAGS) $(CALLMACROS)
+.INCLUDE: set_ext.mk
+.INCLUDE: target.mk
+.INCLUDE: tg_ext.mk
 
-# --- Targets ------------------------------------------------------
+$(PACKAGE_DIR)/$(CONFIGURE_FLAG_FILE): ooo-cppunit_dll.mk ooo-DllPlugInTester.mk
 
-.INCLUDE : set_ext.mk
-.INCLUDE : target.mk
-.INCLUDE : tg_ext.mk
+.ELSE
 
+.IF "$(USE_SYSTEM_STL)" != "YES"
+
+OOO_STLPORT_CXXFLAGS = -I$(SOLARINCDIR)/stl
+.IF "$(COM)" == "GCC"
+OOO_STLPORT_CXXFLAGS += -DGXX_INCLUDE_PATH=$(GXX_INCLUDE_PATH)
+.ENDIF
+
+OOO_STLPORT_LDFLAGS = -L$(SOLARLIBDIR)
+OOO_STLPORT_LIBS = $(LIBSTLPORT)
+
+# When "checking for C compiler default output file name" configure
+# unfortunately uses "$CC $CFLAGS $CPPFLAGS $LDFLAGS conftest.c $LIBS" to build
+# a C program that links against $(LIBSTLPORT); at least on one unxlngi6
+# machine, this has been observed to fail with "/lib/libm.so.6: undefined
+# reference to `_rtld_global_ro@GLIBC_PRIVATE'" unless -lm is also specified:
+.IF "$(OS)" == "LINUX" && "$(COM)" == "GCC" && "$(CPU)" == "I"
+OOO_STLPORT_LIBS += -lm
+.ENDIF
+
+# And later, when "checking whether the C compiler works" configure tries to
+# execute that program; however, the program would fail to locate the STLport
+# library (another work-around might be to add something like --as-needed around
+# $(LIBSTLPORT)):
+.IF "$(OS)" == "LINUX" || "$(OS)" == "SOLARIS"
+.IF "$(LD_LIBRARY_PATH)" == ""
+LD_LIBRARY_PATH := $(SOLARLIBDIR)
+    # strictly speaking, this is incorrect if the LD_LIBRARY_PATH environment
+    # variable is set to the empty string
+.ELSE
+LD_LIBRARY_PATH := $(LD_LIBRARY_PATH):$(SOLARLIBDIR)
+.ENDIF
+.EXPORT: LD_LIBRARY_PATH
+.END
+
+.END
+
+# At least on Solaris with Sun CC, linking the cppunit dynamic library fails as
+# TestAssert.o uses fabs but -lm is missing from the command line (cppunit's
+# aclocal.m4 contains an unused AC_CHECK_LIBM, maybe using that would be a
+# better fix):
+.IF "$(OS)" == "SOLARIS" && "$(COM)" == "C52"
+MY_LIBS = -lm
+.ENDIF
+
+CONFIGURE_ACTION = ./configure
+CONFIGURE_FLAGS = --prefix=$(shell cd $(PACKAGE_DIR) && \
+    pwd $(PWDFLAGS))/$(TARFILE_ROOTDIR)/ooo-install \
+    --disable-dependency-tracking --disable-static --disable-doxygen \
+    --disable-html-docs --disable-latex-docs CC='$(CC)' CXX='$(CXX)' \
+    CXXFLAGS='$(OOO_STLPORT_CXXFLAGS)' \
+    LDFLAGS='$(LDFLAGS) $(OOO_STLPORT_LDFLAGS)' \
+    LIBS='$(OOO_STLPORT_LIBS) $(MY_LIBS)'
+
+BUILD_ACTION = $(GNUMAKE)
+BUILD_FLAGS = install
+
+OUTDIR2INC = ooo-install/include/cppunit
+
+.IF "$(OS)" == "MACOSX"
+OUT2LIB = ooo-install/lib/libcppunit-1.12.1.dylib
+.ELSE
+OUT2LIB = ooo-install/lib/libcppunit-1.12.so.1
+.ENDIF
+
+OUT2BIN = ooo-install/bin/DllPlugInTester
+
+.INCLUDE: set_ext.mk
+.INCLUDE: target.mk
+.INCLUDE: tg_ext.mk
+
+.ENDIF
