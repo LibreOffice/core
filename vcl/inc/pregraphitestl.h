8bbcf854cf7248384646de2b66f06c21bea227ca
Caolán McNamara <cmc@openoffice.org>
2009-12-17 13:57:53 +0000

    cmcfixes69: #i106157#: workaround graphite stl/stlport issues

---
+++ vcl/inc/pregraphitestl.h
@@ -0,0 +1,29 @@
+#if defined(GRAPHITEADAPTSTL) && defined(std)
+#   define std_was_redefined_as_stlport std
+#   undef std
+#   undef _STLP_CONFIG_H
+#   undef _STLP_STRING
+#   undef _STLP_IOSTREAM
+#   undef _STLP_IOSFWD
+#   undef _STLP_IOMANIP
+#   undef _STLP_ALGORITHM
+#   undef _STLP_VECTOR
+#   undef _STLP_SET
+#   undef _STLP_MAP
+#   define _STLP_DONT_REDEFINE_STD 1
+#   define _STLP_WHOLE_NATIVE_STD 1
+#   include _STLP_NATIVE_HEADER(exception_defines.h)
+#   include _STLP_NATIVE_HEADER(memory)
+#   include _STLP_NATIVE_HEADER(exception)
+#   include _STLP_NATIVE_HEADER(iosfwd)
+#   include _STLP_NATIVE_HEADER(string)
+#   include _STLP_NATIVE_HEADER(streambuf)
+#   include _STLP_NATIVE_HEADER(ios)
+#   include _STLP_NATIVE_HEADER(locale)
+#   include _STLP_NATIVE_HEADER(stdexcept)
+#   include _STLP_NATIVE_HEADER(ostream)
+#   include _STLP_NATIVE_HEADER(istream)
+#   include _STLP_NATIVE_HEADER(iostream)
+#endif
+//sil_std resolves to the std that Graphite was built with
+namespace sil_std = std;
