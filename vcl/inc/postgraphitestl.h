c7fcabef35a9ccf49a5de2c8ff1470aee77c238d
Caolán McNamara <cmc@openoffice.org>
2010-01-12 11:18:27 +0000

    cmcfixes69: #i106157# support system graphite, even with a different stlport

--- vcl/inc/postgraphitestl.h
+++ vcl/inc/postgraphitestl.h
@@ -1,17 +1,9 @@
 #ifdef std_was_redefined_as_stlport
 //  put things back the way they were
 #   define std std_was_redefined_as_stlport
-#   undef _STLP_DONT_REDEFINE_STD
-#   undef _STLP_WHOLE_NATIVE_STD
-#   undef _STLP_STRING
-#   undef _STLP_IOSTREAM
-#   undef _STLP_IOSFWD
-#   undef _STLP_IOMANIP
-#   undef _STLP_ALGORITHM
-#   undef _STLP_VECTOR
-#   undef _STLP_SET
-#   undef _STLP_MAP
+#   undef _STLP_OUTERMOST_HEADER_ID
 //  force config to be re-read
+#   undef _STLP_NOTHROW_INHERENTLY
 #   undef _STLP_CONFIG_H
 #   include <stddef.h>
 #endif
--- vcl/inc/pregraphitestl.h
+++ vcl/inc/pregraphitestl.h
@@ -1,23 +1,20 @@
 #if defined(GRAPHITEADAPTSTL) && defined(std)
+#   include <ostream>
+#   include <istream>
+#   include <fstream>
+#   include <iostream>
+#   include <vector>
+#   include <algorithm>
 #   define std_was_redefined_as_stlport std
 #   undef std
-#   undef _STLP_CONFIG_H
-#   undef _STLP_STRING
-#   undef _STLP_IOSTREAM
-#   undef _STLP_IOSFWD
-#   undef _STLP_IOMANIP
-#   undef _STLP_ALGORITHM
-#   undef _STLP_VECTOR
-#   undef _STLP_SET
-#   undef _STLP_MAP
-#   define _STLP_DONT_REDEFINE_STD 1
-#   define _STLP_WHOLE_NATIVE_STD 1
+#   define _STLP_OUTERMOST_HEADER_ID 0xdeadbeaf
 #   pragma GCC visibility push(default)
 #   include _STLP_NATIVE_HEADER(exception_defines.h)
 #   include _STLP_NATIVE_HEADER(limits)
 #   include _STLP_NATIVE_HEADER(memory)
 #   include _STLP_NATIVE_HEADER(exception)
 #   include _STLP_NATIVE_HEADER(iosfwd)
+#   include _STLP_NATIVE_HEADER(algorithm)
 #   include _STLP_NATIVE_HEADER(string)
 #   include _STLP_NATIVE_HEADER(streambuf)
 #   include _STLP_NATIVE_HEADER(ios)
@@ -26,6 +23,7 @@
 #   include _STLP_NATIVE_HEADER(ostream)
 #   include _STLP_NATIVE_HEADER(istream)
 #   include _STLP_NATIVE_HEADER(iostream)
+#   include _STLP_NATIVE_HEADER(vector)
 #   pragma GCC visibility pop
 #endif
 //sil_std resolves to the std that Graphite was built with
