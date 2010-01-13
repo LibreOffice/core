6bcdfaf6a740250ac39d4fceb3f500b82cb96040
sb <sb@openoffice.org>
2010-01-13 18:56:34 +0100

    sb118: adapted cppunit.mk to cppunittester and no longer require "dmake test" to run tests; changed places back to include _cppunit.mk where it had been taken out before

--- solenv/inc/cppunit.mk
+++ solenv/inc/cppunit.mk
@@ -31,15 +31,12 @@
 
 # Helper makefile additional, to support
 # dmake test
-# for testshl2 (cppunit testing)
+# for cppunittester (cppunit testing)
 # for simple start all generated libraries:
 # dmake test
 #
 # for library 1 & 2:
 # or dmake test1 test2
-#
-# for library 2 with additional options to the testshl2
-# dmake test2 TESTOPT="-dntsf"
 
 # This file have to be unroll by
 # cat cppunit.mk | mkunroll >_cppunit.mk
@@ -47,38 +44,17 @@
 # $(TNR) is the number
 # $(SHL$(TNR)TARGETN) is the same like $(OUT)/bin/$(SHL1TARGET) for every number, within unix, "bin" is replaced by "lib"
 
-#.IF "$(TESTOPT)"==""
-#	TESTOPT="-onlyerrors"
-#.ENDIF
-
 ################################################################################
 # unroll begin
 # ---- create the test libraries ------------------------------------
 
-TEST$(TNR)LIB=$(SHL$(TNR)TARGETN)
-.IF "$(TESTOPT)"==""
-    TEST$(TNR)OPT="-jobexclude"
-    TEST$(TNR)OPT+=$(SHL$(TNR)TARGET).xsce
-    TEST$(TNR)OPT+="-sf"
-    TEST$(TNR)OPT+=$(mktmp "foo")
-    TEST$(TNR)OPT+="-onlyerrors"
-    TEST$(TNR)OPT+=" "
-    TEST$(TNR)OPT+=$(TESTOPTADD)
-.ELSE
-    TEST$(TNR)OPT=$(TESTOPT)
-.ENDIF
-
 .IF "$(SHL$(TNR)TARGET)"!=""
-test : test$(TNR)
-test$(TNR): ALLTAR
+ALLTAR : test$(TNR)
+test$(TNR) .PHONY : $(SHL$(TNR)TARGETN)
         @echo ----------------------------------------------------------
-        @echo - start unit test \#$(TNR) on library $(TEST$(TNR)LIB)
+        @echo - start unit test \#$(TNR) on library $(SHL$(TNR)TARGETN)
         @echo ----------------------------------------------------------
-.IF "$(OS)" == "LINUX" # full path needed by osl_getModuleURLFromAddress:
-        `which testshl2` $(TEST$(TNR)LIB) $(TEST$(TNR)OPT)
-.ELSE
-        testshl2 $(TEST$(TNR)LIB) $(TEST$(TNR)OPT)
-.ENDIF
+        $(CPPUNITTESTER) $(SHL$(TNR)TARGETN)
 .ENDIF
 
 # unroll end
