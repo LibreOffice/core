04815f28a3eb94a6dc6b4e715ccee208e6fb506d
Philipp Lohmann [pl] <Philipp.Lohmann@Sun.COM>
2010-01-13 15:53:07 +0100

    vcl109: #i108290# KDE4 integration updates from shtylman

--- fpicker/source/unx/kde4/KDE4FilePicker.cxx
+++ fpicker/source/unx/kde4/KDE4FilePicker.cxx
@@ -166,8 +166,6 @@ KDE4FilePicker::KDE4FilePicker( const uno::Reference<lang::XMultiServiceFactory>
 
     //default mode
     _dialog->setOperationMode(KFileDialog::Opening);
-
-    _dialog->setStyleSheet("color: black;");
 }
 
 KDE4FilePicker::~KDE4FilePicker()
@@ -212,6 +210,7 @@ sal_Int16 SAL_CALL KDE4FilePicker::execute()
 
     _dialog->clearFilter();
     _dialog->setFilter(_filter);
+    _dialog->filterWidget()->setEditable(false);
 
     //block and wait for user input
     if (_dialog->exec() == KFileDialog::Accepted)
@@ -263,7 +262,7 @@ uno::Sequence< ::rtl::OUString > SAL_CALL KDE4FilePicker::getFiles()
         QCheckBox *cb = dynamic_cast<QCheckBox*> (
             _customWidgets[ExtendedFilePickerElementIds::CHECKBOX_AUTOEXTENSION ]);
 
-        if (cb->isChecked())
+        if (cb && cb->isChecked())
         {
             extension = _dialog->currentFilter(); // assuming filter value is like this *.ext
             extension.replace("*","");
@@ -658,13 +657,13 @@ void SAL_CALL KDE4FilePicker::initialize( const uno::Sequence<uno::Any> &args )
 
         case FILESAVE_AUTOEXTENSION:
             operationMode = KFileDialog::Saving;
-            addCustomControl( ExtendedFilePickerElementIds::CHECKBOX_AUTOEXTENSION );
+            //addCustomControl( ExtendedFilePickerElementIds::CHECKBOX_AUTOEXTENSION );
             break;
 
         case FILESAVE_AUTOEXTENSION_PASSWORD:
         {
             operationMode = KFileDialog::Saving;
-            addCustomControl( ExtendedFilePickerElementIds::CHECKBOX_AUTOEXTENSION );
+            //addCustomControl( ExtendedFilePickerElementIds::CHECKBOX_AUTOEXTENSION );
             addCustomControl( ExtendedFilePickerElementIds::CHECKBOX_PASSWORD );
             break;
         }
@@ -715,6 +714,7 @@ void SAL_CALL KDE4FilePicker::initialize( const uno::Sequence<uno::Any> &args )
     }
 
     _dialog->setOperationMode(operationMode);
+    _dialog->setConfirmOverwrite(true);
 }
 
 void SAL_CALL KDE4FilePicker::cancel()
