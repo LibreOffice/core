71cdbc4bc63e5e62d788d651fe5411c21e3cc424
Kohei Yoshida <kyoshida@novell.com>
2010-01-16 12:01:15 -0500

    kohei04: #i108374# added in-line comment for the line I added.

--- sax/source/expatwrap/saxwriter.cxx
+++ sax/source/expatwrap/saxwriter.cxx
@@ -703,6 +703,7 @@ inline void SaxWriterHelper::clearBuffer() throw( SAXException )
         m_Sequence.realloc(nCurrentPos);
         nCurrentPos = writeSequence();
         m_Sequence.realloc(SEQUENCESIZE);
+        // Be sure to update the array pointer after the reallocation.
         mp_Sequence = m_Sequence.getArray();
     }
 }
