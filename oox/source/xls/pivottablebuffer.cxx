9b9e688bb3430f2a2b88c9e89f7fd44b96a81aa3
Daniel Rentz <dr@openoffice.org>
2010-01-26 13:49:49 +0100

    #i108547# allow modifications in readonly document when loading msooxml

--- oox/source/xls/pivottablebuffer.cxx
+++ oox/source/xls/pivottablebuffer.cxx
@@ -1425,7 +1425,7 @@ void PivotTable::finalizeImport()
             }
             catch( Exception& )
             {
-                OSL_ENSURE( false, "PivotTable::finalizeImport - exception while creating the pivot table" );
+                OSL_ENSURE( false, "PivotTable::finalizeImport - exception while creating the DataPilot table" );
             }
         }
     }
