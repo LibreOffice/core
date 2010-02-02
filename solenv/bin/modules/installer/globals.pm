2937e95667b59e9e8920f3f0813b8c2ccb88fd20
Ingo Schmidt <is@openoffice.org>
2010-02-02 17:23:10 +0100

    native0 #161620# increasing max lang length for 17 languages

--- solenv/bin/modules/installer/globals.pm
+++ solenv/bin/modules/installer/globals.pm
@@ -180,7 +180,7 @@ BEGIN
     $installertypedir = "";
     $controlledmakecabversion = "5";
     $saved_packages_path = "";
-    $max_lang_length = 50;
+    $max_lang_length = 65;
     $globalblock = "Globals";
     $rootmodulegid = "";
     %alllangmodules = ();
