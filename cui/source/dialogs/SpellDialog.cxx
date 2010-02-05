97ed06ff8bed722b12d8037a9f641664e06a3c13
Thomas Lange [tl] <tl@openoffice.org>
2010-02-05 13:45:10 +0100

    #161586# allowing for thesaurus vendor images

--- cui/source/dialogs/SpellDialog.cxx
+++ cui/source/dialogs/SpellDialog.cxx
@@ -476,7 +476,7 @@ IMPL_STATIC_LINK( SpellDialog, InitHdl, SpellDialog *, EMPTYARG )
     }
     else
     {
-        if( SvtLinguConfig().HasAnyVendorImages() )
+        if( SvtLinguConfig().HasVendorImages( "SpellAndGrammarDialogImage" ) )
         {
             pThis->aVendorImageFI.Show();
             Size aVendorSize = pThis->aVendorImageFI.GetSizePixel();
