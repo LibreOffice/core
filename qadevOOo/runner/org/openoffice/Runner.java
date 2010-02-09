af77423da3d540d381ae4cd1996d7e55e2303392
Lars Langhans <lla@openoffice.org>
2010-02-09 11:18:46 +0100

    qadev40: #161119# refactoring

--- qadevOOo/runner/org/openoffice/Runner.java
+++ qadevOOo/runner/org/openoffice/Runner.java
@@ -187,7 +187,7 @@ public class Runner
 
     public static void main(String[] args)
     {
-        System.out.println("OOoRunner Main() version from 20090825 (yyyymmdd)");
+        System.out.println("OOoRunner Main() version from 20100125 (yyyymmdd)");
 
         setStartTime(getTime());
 
