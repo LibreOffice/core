1708f6091bfc05164fe2bd770664ceb26826a6b8
Jens-Heiner Rechtien <hr@openoffice.org>
2010-02-12 15:01:35 +0100

    changefileheader2: #i109125#: change source file copyright notice from Sun Microsystems to Oracle; remove CVS style keywords (RCSfile, Revision)

--- dbaccess/source/ui/misc/TokenWriter.cxx
+++ dbaccess/source/ui/misc/TokenWriter.cxx
@@ -2,12 +2,9 @@
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright 2008 by Sun Microsystems, Inc.
+ * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
- *
- * $RCSfile: TokenWriter.cxx,v $
- * $Revision: 1.36.14.3 $
  *
  * This file is part of OpenOffice.org.
  *
