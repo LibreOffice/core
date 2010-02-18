b5e6c6c56493c84d9823a0d0b7c85b61a23c2be3
Ocke Janssen [oj] <Ocke.Janssen@sun.com>
2010-02-18 11:33:54 +0100

    dba33e: #i108128# check if the found driver also accepts the URL

--- connectivity/source/manager/mdrivermanager.cxx
+++ connectivity/source/manager/mdrivermanager.cxx
@@ -720,7 +720,7 @@ Reference< XDriver > OSDBCDriverManager::implGetDriverForURL(const ::rtl::OUStri
         }
 
         // found something?
-        if ( m_aDriversBS.end() != aFind )
+        if ( m_aDriversBS.end() != aFind && aFind->xDriver.is() && aFind->xDriver->acceptsURL(_rURL) )
             xReturn = aFind->xDriver;
     }
 
