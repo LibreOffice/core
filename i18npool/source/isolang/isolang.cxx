48c79177a1a29420b3d8adfa8e00703ab62789a1
Eike Rathke <erAck@openoffice.org>
2010-03-01 18:40:29 +0100

    locales33: #i109687# added Bafia_Cameroon [ksf-CM] to language list

--- i18npool/source/isolang/isolang.cxx
+++ i18npool/source/isolang/isolang.cxx
@@ -458,6 +458,7 @@ static MsLangId::IsoLangEntry const aImplIsoLangEntries[] =
     { LANGUAGE_USER_BUSHI,                 "buc", "YT" },
     { LANGUAGE_USER_TAHITIAN,               "ty", "PF" },
     { LANGUAGE_USER_MALAGASY_PLATEAU,      "plt", "MG" },
+    { LANGUAGE_USER_BAFIA,                 "ksf", "CM" },
     { LANGUAGE_NONE,                       "zxx", ""   },   // added to ISO 639-2 on 2006-01-11: Used to declare the absence of linguistic information
     { LANGUAGE_DONTKNOW,                    "",   ""   }    // marks end of table
 };
