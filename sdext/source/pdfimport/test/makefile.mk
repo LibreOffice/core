f525a73df30e62519385ba906bbd7f56b99a5597
sb <sb@openoffice.org>
2010-03-04 22:13:08 +0100

    sb118: #i109791# improved CPPUNIT_CFLAGS handling

--- sdext/source/pdfimport/test/makefile.mk
+++ sdext/source/pdfimport/test/makefile.mk
@@ -35,16 +35,14 @@ ENABLE_EXCEPTIONS=TRUE
 
 .INCLUDE: settings.mk
 
-.IF "$(SYSTEM_CPPUNIT)"=="YES"
-CFLAGS+= $(CPPUNIT_CFLAGS)
-.ENDIF
-
 # --- test lib ------------------------------------------------------
 
 .IF "$(ENABLE_PDFIMPORT)" == "NO"
 @all:
         @echo "PDF Import extension disabled."
 .ENDIF
+
+CFLAGSCXX += $(CPPUNIT_CFLAGS)
 
 .IF "$(SYSTEM_ZLIB)" == "YES"
 CFLAGS+=-DSYSTEM_ZLIB
