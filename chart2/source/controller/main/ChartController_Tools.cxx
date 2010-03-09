7822e88a9487258d2a398ac202cbeb045863aa16
Ingrid Halama <iha@openoffice.org>
2010-03-09 16:06:24 +0100

    chart43: #i109977# make axes and grids deletable via delete key

--- chart2/source/controller/main/ChartController_Tools.cxx
+++ chart2/source/controller/main/ChartController_Tools.cxx
@@ -502,8 +502,7 @@ bool ChartController::isObjectDeleteable( const uno::Any& rSelection )
     if( (rSelection >>= aSelObjCID) && aSelObjCID.getLength() > 0 )
     {
         ObjectType aObjectType(ObjectIdentifier::getObjectType( aSelObjCID ));
-        if( (OBJECTTYPE_TITLE == aObjectType) || (OBJECTTYPE_LEGEND == aObjectType)
-                || (OBJECTTYPE_DATA_SERIES == aObjectType) )
+        if( (OBJECTTYPE_TITLE == aObjectType) || (OBJECTTYPE_LEGEND == aObjectType) )
             return true;
         if( (OBJECTTYPE_DATA_SERIES == aObjectType) || (OBJECTTYPE_LEGEND_ENTRY == aObjectType) )
             return true;
@@ -511,6 +510,8 @@ bool ChartController::isObjectDeleteable( const uno::Any& rSelection )
             (OBJECTTYPE_DATA_AVERAGE_LINE == aObjectType) || (OBJECTTYPE_DATA_ERRORS == aObjectType))
             return true;
         if( (OBJECTTYPE_DATA_LABELS == aObjectType) || (OBJECTTYPE_DATA_LABEL == aObjectType) )
+            return true;
+        if( (OBJECTTYPE_AXIS == aObjectType) || (OBJECTTYPE_GRID == aObjectType) || (OBJECTTYPE_SUBGRID == aObjectType) )
             return true;
     }
 
@@ -539,8 +540,6 @@ bool ChartController::executeDispatch_Delete()
             return false;
 
         //remove chart object
-        impl_ClearSelection();
-
         uno::Reference< chart2::XChartDocument > xChartDoc( m_aModel->getModel(), uno::UNO_QUERY );
         if( !xChartDoc.is() )
             return false;
@@ -695,6 +694,24 @@ bool ChartController::executeDispatch_Delete()
                     bReturn = true;
                     aUndoGuard.commitAction();
                 }
+                break;
+            }
+            case OBJECTTYPE_AXIS:
+            {
+                executeDispatch_DeleteAxis();
+                bReturn = true;
+                break;
+            }
+            case OBJECTTYPE_GRID:
+            {
+                executeDispatch_DeleteMajorGrid();
+                bReturn = true;
+                break;
+            }
+            case OBJECTTYPE_SUBGRID:
+            {
+                executeDispatch_DeleteMinorGrid();
+                bReturn = true;
                 break;
             }
 
