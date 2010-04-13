39fb1ee5667028cb9752c4fe84e28ac0148418d3
Ingrid Halama <iha@openoffice.org>
2010-04-13 16:21:41 +0200

    chart45: #i110849# crash after copying chart

--- chart2/source/controller/chartapiwrapper/ChartDataWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/ChartDataWrapper.cxx
@@ -303,7 +303,9 @@ ChartDataWrapper::ChartDataWrapper( ::boost::shared_ptr< Chart2ModelContact > sp
         m_spChart2ModelContact( spChart2ModelContact ),
         m_aEventListenerContainer( m_aMutex )
 {
+    osl_incrementInterlockedCount( &m_refCount );
     initDataAccess();
+    osl_decrementInterlockedCount( &m_refCount );
 }
 
 ChartDataWrapper::ChartDataWrapper( ::boost::shared_ptr< Chart2ModelContact > spChart2ModelContact,
@@ -311,8 +313,10 @@ ChartDataWrapper::ChartDataWrapper( ::boost::shared_ptr< Chart2ModelContact > sp
         m_spChart2ModelContact( spChart2ModelContact ),
         m_aEventListenerContainer( m_aMutex )
 {
+    osl_incrementInterlockedCount( &m_refCount );
     lcl_AllOperator aOperator( xNewData );
     applyData( aOperator );
+    osl_decrementInterlockedCount( &m_refCount );
 }
 
 ChartDataWrapper::~ChartDataWrapper()
