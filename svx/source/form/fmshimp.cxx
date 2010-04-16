2a8198c6ddfc3552f6630a85c1b23a9f697d091e
Frank Schoenheit [fs] <frank.schoenheit@sun.com>
2010-04-16 14:30:41 +0200

    slidecopy: removed some traces which are of no use anymore

--- svx/source/form/fmshimp.cxx
+++ svx/source/form/fmshimp.cxx
@@ -941,7 +941,6 @@ void SAL_CALL FmXFormShell::formDeactivated(const EventObject& rEvent) throw( Ru
 void FmXFormShell::disposing()
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "svx", "Ocke.Janssen@sun.com", "FmXFormShell::disposing" );
-    OSL_TRACE( "--- FmXFormShell::disposing      : %p, ........, ........", this );
     impl_checkDisposed();
 
     FmXFormShell_BASE::disposing();
@@ -3761,7 +3760,6 @@ void FmXFormShell::SetWizardUsing(sal_Bool _bUseThem)
 void FmXFormShell::viewDeactivated( FmFormView& _rCurrentView, sal_Bool _bDeactivateController /* = sal_True */ )
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "svx", "Ocke.Janssen@sun.com", "FmXFormShell::viewDeactivated" );
-    OSL_TRACE( "--- FmXFormShell::viewDeactivated: %p, %p, ........", this, &_rCurrentView );
 
     if ( _rCurrentView.GetImpl() && !_rCurrentView.IsDesignMode() )
     {
@@ -3796,7 +3794,6 @@ void FmXFormShell::viewDeactivated( FmFormView& _rCurrentView, sal_Bool _bDeacti
     // remove callbacks at the page
     if ( pPage )
     {
-        OSL_TRACE( "--- FmXFormShell::resetHandler   : %p, ........, %p", this, pPage );
         pPage->GetImpl().SetFormsCreationHdl( Link() );
     }
     UpdateForms( sal_True );
@@ -3835,7 +3832,6 @@ IMPL_LINK( FmXFormShell, OnFormsCreated, FmFormPage*, /*_pPage*/ )
 void FmXFormShell::viewActivated( FmFormView& _rCurrentView, sal_Bool _bSyncAction /* = sal_False */ )
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "svx", "Ocke.Janssen@sun.com", "FmXFormShell::viewActivated" );
-    OSL_TRACE( "--- FmXFormShell::viewActivated  : %p, %p, ........", this, &_rCurrentView );
 
     FmFormPage* pPage = _rCurrentView.GetCurPage();
 
@@ -3865,7 +3861,6 @@ void FmXFormShell::viewActivated( FmFormView& _rCurrentView, sal_Bool _bSyncActi
     // set callbacks at the page
     if ( pPage )
     {
-        OSL_TRACE( "--- FmXFormShell::setHandler     : %p, ........, %p", this, pPage );
         pPage->GetImpl().SetFormsCreationHdl( LINK( this, FmXFormShell, OnFormsCreated ) );
     }
 
