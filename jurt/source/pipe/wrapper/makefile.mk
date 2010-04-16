462d351e9d4038ebad0fa11c8c77106a3b7bd3c9
sb <sb@openoffice.org>
2010-04-16 16:24:56 +0200

    sb120: #i110944# jpipe.dll wrapper around jpipx.dll

--- jurt/source/pipe/wrapper/makefile.mk
+++ jurt/source/pipe/wrapper/makefile.mk
@@ -25,26 +25,21 @@
 #
 #***********************************************************************/
 
-PRJ = ../..
+PRJ = ../../..
 PRJNAME = jurt
-TARGET = jpipe
+TARGET = wrapper
 
 NO_DEFAULT_STL = TRUE
+UWINAPILIB =
 VISIBILITY_HIDDEN = TRUE
 
 .INCLUDE: settings.mk
 
-.IF "$(OS)" == "WNT"
-SHL1TARGET = jpipx
-.ELSE
-SHL1TARGET = jpipe
-.END
-
 SHL1CODETYPE = C
 SHL1IMPLIB = i$(SHL1TARGET)
-SHL1OBJS = $(SLO)/com_sun_star_lib_connections_pipe_PipeConnection.obj
+SHL1OBJS = $(SLO)/wrapper.obj
 SHL1RPATH = URELIB
-SHL1STDLIBS = $(SALLIB)
+SHL1TARGET = jpipe
 SHL1USE_EXPORTS = name
 DEF1NAME = $(SHL1TARGET)
 
