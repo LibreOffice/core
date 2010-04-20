be4cb22878a67afb375b0b4bc28292fd7486a7dc
Christian Lippka <christian.lippka@sun.com>
2010-04-20 17:23:49 +0200

    #i107213# finalized layout grippies tooltips

--- sd/source/ui/view/viewoverlaymanager.cxx
+++ sd/source/ui/view/viewoverlaymanager.cxx
@@ -83,7 +83,6 @@ BitmapEx ViewOverlayManager::maSmallButtonImages[BMP_PLACEHOLDER_SMALL_END - BMP
 BitmapEx ViewOverlayManager::maLargeButtonImages[BMP_PLACEHOLDER_LARGE_END - BMP_PLACEHOLDER_LARGE_START];
 
 static USHORT gButtonSlots[] = { SID_INSERT_TABLE, SID_INSERT_DIAGRAM, SID_INSERT_GRAPHIC, SID_INSERT_AVMEDIA };
-static const sal_Char* gSlotStr[] = { ".uno:InsertTable", ".uno:InsertObjectChart", ".uno:InsertGraphic", ".uno:InsertVideo" };
 static USHORT gButtonToolTips[] = { STR_RESET_LAYOUT, STR_INSERT_TABLE, STR_INSERT_CHART, STR_INSERT_PICTURE,STR_INSERT_MOVIE };
 
 // --------------------------------------------------------------------
