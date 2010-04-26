96176d231f3737ef4eb96f2e08a5cfd958a12dcb
Philipp Lohmann [pl] <Philipp.Lohmann@Sun.COM>
2010-04-26 11:14:03 +0200

    vcl111: #i111106# simplify XIOErrorHandler (thanks cmc)

--- vcl/unx/source/app/saldata.cxx
+++ vcl/unx/source/app/saldata.cxx
@@ -352,8 +352,9 @@ SalXLib::SalXLib()
         nFDs_ = m_pTimeoutFDS[0] + 1;
     }
 
-    PushXErrorLevel( !!getenv( "SAL_IGNOREXERRORS" ) );
     m_bHaveSystemChildFrames        = false;
+    m_aOrigXIOErrorHandler = XSetIOErrorHandler ( (XIOErrorHandler)X11SalData::XIOErrorHdl );
+    PushXErrorLevel( !!getenv( "SAL_IGNOREXERRORS" ) );
 }
 
 SalXLib::~SalXLib()
@@ -363,6 +364,7 @@ SalXLib::~SalXLib()
     close (m_pTimeoutFDS[1]);
 
     PopXErrorLevel();
+    XSetIOErrorHandler (m_aOrigXIOErrorHandler);
 }
 
 void SalXLib::PushXErrorLevel( bool bIgnore )
@@ -457,8 +459,6 @@ void SalXLib::Init()
         std::fflush( stderr );
         exit(0);
     }
-
-    XSetIOErrorHandler    ( (XIOErrorHandler)X11SalData::XIOErrorHdl );
 
     SalDisplay *pSalDisplay = new SalX11Display( pDisp );
 
