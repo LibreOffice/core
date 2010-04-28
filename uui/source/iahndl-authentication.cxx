ececcc290b431ca40b785afbffa1f3c2396a1bbb
Philipp Lohmann [pl] <Philipp.Lohmann@Sun.COM>
2010-04-28 14:22:16 +0200

    pdfextfix03: #i110871# PasswordRequest should still be supported for backwards compatibility

--- uui/source/iahndl-authentication.cxx
+++ uui/source/iahndl-authentication.cxx
@@ -581,5 +581,17 @@ UUIInteractionHelper::handlePasswordRequest(
                                true /* bool bMSCryptoMode */);
         return true;
     }
+
+    task::PasswordRequest aPasswordRequest;
+    if( aAnyRequest >>= aPasswordRequest )
+    {
+        handlePasswordRequest_(getParentProperty(),
+                               aPasswordRequest.Mode,
+                               rRequest->getContinuations(),
+                               rtl::OUString(),
+                               false /* bool bMSCryptoMode */);
+        return true;
+    }
+
     return false;
 }
