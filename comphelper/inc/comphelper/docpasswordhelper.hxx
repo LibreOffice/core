386eebb4cfa473de8db4a8337050ff20cce521a9
Mikhail Voytenko <mav@openoffice.org>
2010-06-03 00:10:45 +0200

    tl78: #i110383# password to modify support for binary types only

--- comphelper/inc/comphelper/docpasswordhelper.hxx
+++ comphelper/inc/comphelper/docpasswordhelper.hxx
@@ -85,8 +85,38 @@ public:
     // ------------------------------------------------------------------------
 
     /** This helper function generates the hash code based on the algorithm
+        specified by MS for "Password to modify" feature of Word.
+
+        @param aString
+            The string for which the hash should be calculated
+
+        @return
+            The hash represented by sal_uInt32
+      */
+
+    static sal_uInt32 GetWordHashAsUINT32(
+                const ::rtl::OUString& aString );
+
+    // ------------------------------------------------------------------------
+
+    /** This helper function generates the hash code based on the algorithm
+        specified by MS for "Password to modify" feature of Word.
+
+        @param aString
+            The string for which the hash should be calculated
+
+        @return
+            The hash represented by sequence of bytes in BigEndian form
+      */
+
+    static ::com::sun::star::uno::Sequence< sal_Int8 > GetWordHashAsSequence(
+                const ::rtl::OUString& aString );
+
+    // ------------------------------------------------------------------------
+
+    /** This helper function generates the hash code based on the algorithm
         specified by MS for "Password to modify" and passwords related to
-        table protection.
+        table protection of Excel.
 
         @param aString
             The string for which the hash should be calculated
