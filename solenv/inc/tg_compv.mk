5d2ff9dcf5ed11b8d0379fca73a8596890b2ef2b
Caolán McNamara <cmc@openoffice.org>
2010-06-08 10:55:22 +0100

    cmcfixes75: #i111770# fix the so.2 -> so.4 for hppa

--- solenv/inc/tg_compv.mk
+++ solenv/inc/tg_compv.mk
@@ -102,11 +102,17 @@ SHORTSTDCPP3="5"
 .ENDIF
 
 .IF "$(CCNUMVER)">="000300040000"
-.IF "$(OS)$(CPU)" == "LINUX6"
+.IF "$(OS)$(CPU)" == "LINUX6" || "$(OS)$(CPU)" == "LINUXH"
 #for gcc >= 3.4.0 on m68k-linux this is libgcc_s.so.2.
+#for gcc >= 3.4.0 < 4.2.0 on hppa-linux this is libgcc_s.so.2.
 SHORTSTDC3:="2"
-.ELIF "$(OS)$(CPU)" == "LINUXH"
-#for gcc >= 3.4.0 on hppa-linux this is libgcc_s.so.4.
+.ENDIF
+SHORTSTDCPP3="6"
+.ENDIF
+
+.IF "$(CCNUMVER)">="000400020000"
+.IF "$(OS)$(CPU)" == "LINUXH"
+#for gcc >= 4.2.0 on hppa-linux this is libgcc_s.so.4.
 SHORTSTDC3:="4"
 .ENDIF
 SHORTSTDCPP3="6"
