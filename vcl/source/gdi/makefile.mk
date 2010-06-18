00ce3f2859b6fc88402b2321e31e714f84a291ca
Frank Schoenheit [fs] <frank.schoenheit@sun.com>
2010-06-19 00:33:50 +0200

    unoawt: argh ... should remove the name of the removed file from makefile.mk, too

--- vcl/source/gdi/makefile.mk
+++ vcl/source/gdi/makefile.mk
@@ -84,7 +84,6 @@ SLOFILES=	$(EXCEPTIONSFILES)      \
             $(SLO)$/bitmap4.obj 	\
             $(SLO)$/alpha.obj		\
             $(SLO)$/bitmapex.obj	\
-            $(SLO)$/imgcons.obj 	\
             $(SLO)$/bmpacc.obj		\
             $(SLO)$/bmpacc2.obj 	\
             $(SLO)$/bmpacc3.obj 	\
