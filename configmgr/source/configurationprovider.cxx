870203a92a83da9e2e1b8a56e4eab43aa561a564
sb <sb@openoffice.org>
2010-06-21 17:41:23 +0200

    sb126: #i111970# accept and ignore EnableAsync (transplanted from 0c119e327cea31601db9932cf3f85bbab9ee06f5)

--- configmgr/source/configurationprovider.cxx
+++ configmgr/source/configurationprovider.cxx
@@ -241,7 +241,7 @@ Service::createInstanceWithArguments(
     if (nodepath.getLength() == 0) {
         badNodePath();
     }
-    // For backwards compatibility, allow a notepath that misses the leading
+    // For backwards compatibility, allow a nodepath that misses the leading
     // slash:
     if (nodepath[0] != '/') {
         nodepath = rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/")) + nodepath;
@@ -456,7 +456,8 @@ Factory::createInstanceWithArgumentsAndContext(
                             " arguments")),
                     0);
             }
-            // For backwards compatibility, allow "Locale" in any case:
+            // For backwards compatibility, allow "Locale" and (ignored)
+            // "EnableAsync" in any case:
             if (name.equalsIgnoreAsciiCaseAsciiL(
                     RTL_CONSTASCII_STRINGPARAM("locale")))
             {
@@ -471,8 +472,9 @@ Factory::createInstanceWithArgumentsAndContext(
                                 " one, non-empty, string Locale argument")),
                         0);
                 }
-            } else {
-                //TODO
+            } else if (!name.equalsIgnoreAsciiCaseAsciiL(
+                           RTL_CONSTASCII_STRINGPARAM("enableasync")))
+            {
                 throw css::uno::Exception(
                     rtl::OUString(
                         RTL_CONSTASCII_USTRINGPARAM(
