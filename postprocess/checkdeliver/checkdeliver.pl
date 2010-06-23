09c61cc78787eb4c9b5607f695bdaf0550c98f4e
Ruediger Timm <rt@openoffice.org>
2010-06-23 15:26:20 +0200

    masterfix: #i10000# Avoid false alarms in checkdeliver

--- postprocess/checkdeliver/checkdeliver.pl
+++ postprocess/checkdeliver/checkdeliver.pl
@@ -52,6 +52,7 @@ my @exceptionmodlist = (
                         "postprocess",
                         "instset.*native",
                         "smoketest.*native",
+                        "testautomation",
                         "testgraphical"
                        ); # modules not yet delivered
 
@@ -151,6 +152,11 @@ sub check
         return 1;
     }
 
+    if ( -z $listname ) {
+        print_logged( "Warning: empty deliver log file \'$listname\'. Module '$module' not delivered correctly?\n\n" );
+        return 0;
+    }
+
     # read deliver log file
     if ( ! open( DELIVERLOG, "< $listname" ) ) {
         print_logged( "Error: cannot open file \'$listname\'\n$!" );
@@ -177,7 +183,7 @@ sub check
     close( DELIVERLOG );
 
     if ( ! $repository ) {
-        print_logged( "Error parsing \'$listname\': cannot determine repository\n");
+        print_logged( "Error parsing \'$listname\': cannot determine repository. Module '$module' not delivered correctly?\n\n" );
         $error ++;
         return $error;
     }
