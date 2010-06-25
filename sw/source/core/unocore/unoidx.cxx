47a2fe9b7053a569e7a6d5cc090b28985992c50d
Michael Stahl <mst@openoffice.org>
2010-06-25 18:00:25 +0200

    sw33bf06: #i111177#: preventively work around unxsols4 compiler bug everywhere

--- sw/source/core/unocore/unoidx.cxx
+++ sw/source/core/unocore/unoidx.cxx
@@ -367,7 +367,8 @@ public:
             *aSwMapProvider.GetPropertySet(lcl_TypeToPropertyMap_Index(eType)))
         , m_eTOXType(eType)
         , m_ListenerContainer(static_cast< ::cppu::OWeakObject* >(&rThis))
-        , m_bIsDescriptor(0 == pBaseSection)
+        // #i111177# unxsols4 (Sun C++ 5.9 SunOS_sparc) may generate wrong code
+        , m_bIsDescriptor((0 == pBaseSection) ? true : false)
         , m_pDoc(&rDoc)
         , m_pProps((m_bIsDescriptor)
             ? new SwDocIndexDescriptorProperties_Impl(rDoc.GetTOXType(eType, 0))
