13afa75fa7892d82f7eeb47b52fdf17fd2a97681
Mathias Bauer <mba@openoffice.org>
2010-07-19 17:13:56 +0200

    CWS changehid: #i111784#: use mhids.pl to generate conversion list for HIDs

--- sc/source/ui/drawfunc/fuins2.cxx
+++ sc/source/ui/drawfunc/fuins2.cxx
@@ -262,7 +262,6 @@ FuInsertOLE::FuInsertOLE(ScTabViewShell* pViewSh, Window* pWin, ScDrawView* pVie
                 aServerLst.Remove( ScDocShell::Factory().GetClassId() );   // Starcalc nicht anzeigen
                 //TODO/LATER: currently no inserting of ClassId into SfxRequest!
             case SID_INSERT_PLUGIN :
-            case SID_INSERT_APPLET :
             case SID_INSERT_FLOATINGFRAME :
             {
                 SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
