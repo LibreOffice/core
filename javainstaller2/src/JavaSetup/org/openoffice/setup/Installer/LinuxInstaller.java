969f4032e8ee1339cbbc339466f0122f8c1fb8a8
Ingo Schmidt <is@openoffice.org>
2010-08-13 11:15:11 +0200

    native324 #i113835# preparing rpm installation on debian systems

--- javainstaller2/src/JavaSetup/org/openoffice/setup/Installer/LinuxInstaller.java
+++ javainstaller2/src/JavaSetup/org/openoffice/setup/Installer/LinuxInstaller.java
@@ -190,6 +190,20 @@ public class LinuxInstaller extends Installer {
                     if ( sofficeLink.exists() ) { useForce = true; }
                 }
 
+                // On Debian based systems, rpms can be installed with the switch --force-debian, if a rpm
+                // is installed.
+
+                String forceDebianString = "";
+
+                if ( ! data.debianInvestigated() ) {
+                    helper.investigateDebian(data);
+                    data.setDebianInvestigated(true);
+                }
+
+                if ( data.isDebianSystem() ) {
+                    forceDebianString = "--force-debian";
+                }
+
                 String rpmCommand = "";
                 String[] rpmCommandArray;
                 String databasePath = null;
@@ -212,14 +226,73 @@ public class LinuxInstaller extends Installer {
                 if (useForce) {
                     if (useLocalDatabase) {
                         if ( relocations != null ) {
-                            rpmCommand = "rpm --upgrade --ignoresize --force -vh " +
+                            rpmCommand = "rpm --upgrade --ignoresize --force " + forceDebianString + " -vh " +
+                                    "--relocate " + relocations + " " + databaseString +
+                                    " " + databasePath + " " + packageName;
+                            rpmCommandArray = new String[11];
+                            rpmCommandArray[0] = "rpm";
+                            rpmCommandArray[1] = "--upgrade";
+                            rpmCommandArray[2] = "--ignoresize";
+                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[4] = forceDebianString;
+                            rpmCommandArray[5] = "-vh";
+                            rpmCommandArray[6] = "--relocate";
+                            rpmCommandArray[7] = relocations;
+                            rpmCommandArray[8] = databaseString;
+                            rpmCommandArray[9] = databasePath;
+                            rpmCommandArray[10] = packageName;
+                        } else {
+                            rpmCommand = "rpm --upgrade --ignoresize --force " + forceDebianString + " -vh " +
+                                    databaseString + " " + databasePath + " " + packageName;
+                            rpmCommandArray = new String[9];
+                            rpmCommandArray[0] = "rpm";
+                            rpmCommandArray[1] = "--upgrade";
+                            rpmCommandArray[2] = "--ignoresize";
+                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[4] = forceDebianString;
+                            rpmCommandArray[5] = "-vh";
+                            rpmCommandArray[6] = databaseString;
+                            rpmCommandArray[7] = databasePath;
+                            rpmCommandArray[8] = packageName;
+                        }
+                    } else {
+                        if ( relocations != null )
+                        {
+                            rpmCommand = "rpm --upgrade --ignoresize --force " + forceDebianString + " -vh " +
+                                    "--relocate " + relocations + " " + packageName;
+                            rpmCommandArray = new String[9];
+                            rpmCommandArray[0] = "rpm";
+                            rpmCommandArray[1] = "--upgrade";
+                            rpmCommandArray[2] = "--ignoresize";
+                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[4] = forceDebianString;
+                            rpmCommandArray[5] = "-vh";
+                            rpmCommandArray[6] = "--relocate";
+                            rpmCommandArray[7] = relocations;
+                            rpmCommandArray[8] = packageName;
+                        } else {
+                            rpmCommand = "rpm --upgrade --ignoresize --force " + forceDebianString + " -vh " + packageName;
+                            rpmCommandArray = new String[7];
+                            rpmCommandArray[0] = "rpm";
+                            rpmCommandArray[1] = "--upgrade";
+                            rpmCommandArray[2] = "--ignoresize";
+                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[4] = forceDebianString;
+                            rpmCommandArray[5] = "-vh";
+                            rpmCommandArray[6] = packageName;
+                        }
+                    }
+                } else {
+                    if (useLocalDatabase) {
+                        if ( relocations != null ) {
+                            rpmCommand = "rpm --upgrade --ignoresize " + forceDebianString + " -vh " +
                                     "--relocate " + relocations + " " + databaseString +
                                     " " + databasePath + " " + packageName;
                             rpmCommandArray = new String[10];
                             rpmCommandArray[0] = "rpm";
                             rpmCommandArray[1] = "--upgrade";
                             rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[3] = forceDebianString;
                             rpmCommandArray[4] = "-vh";
                             rpmCommandArray[5] = "--relocate";
                             rpmCommandArray[6] = relocations;
@@ -227,13 +300,13 @@ public class LinuxInstaller extends Installer {
                             rpmCommandArray[8] = databasePath;
                             rpmCommandArray[9] = packageName;
                         } else {
-                            rpmCommand = "rpm --upgrade --ignoresize --force -vh " +
+                            rpmCommand = "rpm --upgrade --ignoresize " + forceDebianString + " -vh " +
                                     databaseString + " " + databasePath + " " + packageName;
                             rpmCommandArray = new String[8];
                             rpmCommandArray[0] = "rpm";
                             rpmCommandArray[1] = "--upgrade";
                             rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[3] = forceDebianString;
                             rpmCommandArray[4] = "-vh";
                             rpmCommandArray[5] = databaseString;
                             rpmCommandArray[6] = databasePath;
@@ -242,77 +315,26 @@ public class LinuxInstaller extends Installer {
                     } else {
                         if ( relocations != null )
                         {
-                            rpmCommand = "rpm --upgrade --ignoresize --force -vh " +
+                            rpmCommand = "rpm --upgrade --ignoresize " + forceDebianString + " -vh " +
                                     "--relocate " + relocations + " " + packageName;
                             rpmCommandArray = new String[8];
                             rpmCommandArray[0] = "rpm";
                             rpmCommandArray[1] = "--upgrade";
                             rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[3] = forceDebianString;
                             rpmCommandArray[4] = "-vh";
                             rpmCommandArray[5] = "--relocate";
                             rpmCommandArray[6] = relocations;
                             rpmCommandArray[7] = packageName;
                         } else {
-                            rpmCommand = "rpm --upgrade --ignoresize --force -vh " + packageName;
+                            rpmCommand = "rpm --upgrade --ignoresize " + forceDebianString + " -vh " + packageName;
                             rpmCommandArray = new String[6];
                             rpmCommandArray[0] = "rpm";
                             rpmCommandArray[1] = "--upgrade";
                             rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "--force";
+                            rpmCommandArray[3] = forceDebianString;
                             rpmCommandArray[4] = "-vh";
                             rpmCommandArray[5] = packageName;
-                        }
-                    }
-                } else {
-                    if (useLocalDatabase) {
-                        if ( relocations != null ) {
-                            rpmCommand = "rpm --upgrade --ignoresize -vh " +
-                                    "--relocate " + relocations + " " + databaseString +
-                                    " " + databasePath + " " + packageName;
-                            rpmCommandArray = new String[9];
-                            rpmCommandArray[0] = "rpm";
-                            rpmCommandArray[1] = "--upgrade";
-                            rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "-vh";
-                            rpmCommandArray[4] = "--relocate";
-                            rpmCommandArray[5] = relocations;
-                            rpmCommandArray[6] = databaseString;
-                            rpmCommandArray[7] = databasePath;
-                            rpmCommandArray[8] = packageName;
-                        } else {
-                            rpmCommand = "rpm --upgrade --ignoresize -vh " +
-                                    databaseString + " " + databasePath + " " + packageName;
-                            rpmCommandArray = new String[7];
-                            rpmCommandArray[0] = "rpm";
-                            rpmCommandArray[1] = "--upgrade";
-                            rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "-vh";
-                            rpmCommandArray[4] = databaseString;
-                            rpmCommandArray[5] = databasePath;
-                            rpmCommandArray[6] = packageName;
-                        }
-                    } else {
-                        if ( relocations != null )
-                        {
-                            rpmCommand = "rpm --upgrade --ignoresize -vh " +
-                                    "--relocate " + relocations + " " + packageName;
-                            rpmCommandArray = new String[7];
-                            rpmCommandArray[0] = "rpm";
-                            rpmCommandArray[1] = "--upgrade";
-                            rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "-vh";
-                            rpmCommandArray[4] = "--relocate";
-                            rpmCommandArray[5] = relocations;
-                            rpmCommandArray[6] = packageName;
-                        } else {
-                            rpmCommand = "rpm --upgrade --ignoresize -vh " + packageName;
-                            rpmCommandArray = new String[5];
-                            rpmCommandArray[0] = "rpm";
-                            rpmCommandArray[1] = "--upgrade";
-                            rpmCommandArray[2] = "--ignoresize";
-                            rpmCommandArray[3] = "-vh";
-                            rpmCommandArray[4] = packageName;
                         }
                     }
                 }
@@ -383,20 +405,58 @@ public class LinuxInstaller extends Installer {
                 useLocalDatabase = true;
             }
 
-            if (useLocalDatabase) {
-                rpmCommand = "rpm -ev" + " " + databaseString + " " + databasePath + " " + packageName;
-                rpmCommandArray = new String[5];
-                rpmCommandArray[0] = "rpm";
-                rpmCommandArray[1] = "-ev";
-                rpmCommandArray[2] = databaseString;
-                rpmCommandArray[3] = databasePath;
-                rpmCommandArray[4] = packageName;
+            // On Debian based systems, rpms can be installed with the switch --force-debian, if a rpm
+            // is installed.
+
+            String forceDebianString = "";
+
+            if ( ! data.debianInvestigated() ) {
+                helper.investigateDebian(data);
+                data.setDebianInvestigated(true);
+            }
+
+            if ( data.isDebianSystem() ) {
+                forceDebianString = "--force-debian";
+            }
+
+            // Code duplication for isDebianSystem is necessary, because there is no valid position
+            // for forceDebianString, if it is empty. This is no problem in installPackage.
+
+            if ( data.isDebianSystem() ) {
+
+                if (useLocalDatabase) {
+                    rpmCommand = "rpm " + forceDebianString + " -ev" + " " + databaseString + " " + databasePath + " " + packageName;
+                    rpmCommandArray = new String[6];
+                    rpmCommandArray[0] = "rpm";
+                    rpmCommandArray[1] = forceDebianString;
+                    rpmCommandArray[2] = "-ev";
+                    rpmCommandArray[3] = databaseString;
+                    rpmCommandArray[4] = databasePath;
+                    rpmCommandArray[5] = packageName;
+                } else {
+                    rpmCommand = "rpm " + forceDebianString + " -ev" + " " + packageName;
+                    rpmCommandArray = new String[4];
+                    rpmCommandArray[0] = "rpm";
+                    rpmCommandArray[1] = forceDebianString;
+                    rpmCommandArray[2] = "-ev";
+                    rpmCommandArray[3] = packageName;
+                }
             } else {
-                rpmCommand = "rpm -ev" + " " + packageName;
-                rpmCommandArray = new String[3];
-                rpmCommandArray[0] = "rpm";
-                rpmCommandArray[1] = "-ev";
-                rpmCommandArray[2] = packageName;
+                if (useLocalDatabase) {
+                    rpmCommand = "rpm -ev" + " " + databaseString + " " + databasePath + " " + packageName;
+                    rpmCommandArray = new String[5];
+                    rpmCommandArray[0] = "rpm";
+                    rpmCommandArray[1] = "-ev";
+                    rpmCommandArray[2] = databaseString;
+                    rpmCommandArray[3] = databasePath;
+                    rpmCommandArray[4] = packageName;
+                } else {
+                    rpmCommand = "rpm -ev" + " " + packageName;
+                    rpmCommandArray = new String[3];
+                    rpmCommandArray[0] = "rpm";
+                    rpmCommandArray[1] = "-ev";
+                    rpmCommandArray[2] = packageName;
+                }
             }
 
             Vector returnVector = new Vector();
