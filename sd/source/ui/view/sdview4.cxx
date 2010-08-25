d0ac8f1e7866c637be15f081b59ef456aacb954a
Thorsten Behrens <thb@openoffice.org>
2010-08-25 23:20:11 +0200

    linkwarn-sd-no-dnd-links.diff: Do not link on drag and drop.
    n#348149

--- sd/source/ui/view/sdview4.cxx
+++ sd/source/ui/view/sdview4.cxx
@@ -395,11 +395,7 @@ IMPL_LINK( View, DropInsertFileHdl, Timer*, EMPTYARG )
             if( !pGraphicFilter->ImportGraphic( aGraphic, aURL ) )
             {
                 sal_Int8    nTempAction = ( aIter == maDropFileVector.begin() ) ? mnAction : 0;
-                const bool bLink = ( ( nTempAction & DND_ACTION_LINK ) != 0 );
                 SdrGrafObj* pGrafObj = InsertGraphic( aGraphic, nTempAction, maDropPos, NULL, NULL );
-
-                if( pGrafObj && bLink )
-                    pGrafObj->SetGraphicLink( aCurrentDropFile, String() );
 
                 // return action from first inserted graphic
                 if( aIter == maDropFileVector.begin() )
