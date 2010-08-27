eaa4e44cfee19f81a3a3d41982263a3f09e23f06
Gregor Hartmann <gh <Gregor Hartmann<gh@openoffice.org>
2010-08-27 14:02:22 +0200

    build breakers

--- starmath/inc/helpids.h
+++ starmath/inc/helpids.h
@@ -1,7 +1,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
@@ -323,3 +323,4 @@
 #define HID_SMA_NOSPACE                                   "STARMATH_HID_SMA_NOSPACE"
 
 #endif
+
