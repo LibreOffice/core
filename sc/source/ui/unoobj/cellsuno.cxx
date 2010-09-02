70f55868b448b1e04130fac5552b1476bb564c80
Daniel Rentz [dr] <daniel.rentz@oracle.com>
2010-09-02 16:24:32 +0200

    mib19: #163525# handle missing codenames in XLS files

--- sc/source/ui/unoobj/cellsuno.cxx
+++ sc/source/ui/unoobj/cellsuno.cxx
@@ -8482,8 +8482,7 @@ void ScTableSheetObj::SetOnePropertyValue( const SfxItemPropertySimpleEntry* pEn
             rtl::OUString aCodeName;
             if ( pDocSh && ( aValue >>= aCodeName ) )
             {
-                String sNewName( aCodeName );
-                pDocSh->GetDocument()->SetCodeName( GetTab_Impl(), sNewName );
+                pDocSh->GetDocument()->SetCodeName( GetTab_Impl(), aCodeName );
             }
         }
         else
