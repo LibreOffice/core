f0c824936cfcc6b684898f7c909390a9b5799303
Michael Stahl <mst@openoffice.org>
2010-09-09 11:25:11 +0200

    sw34bf01: #i114162#: xmloff: do not write invalid config-items of type "byte".

--- xmloff/source/core/SettingsExportHelper.cxx
+++ xmloff/source/core/SettingsExportHelper.cxx
@@ -203,6 +203,7 @@ void XMLSettingsExportHelper::exportBool(const sal_Bool bValue, const rtl::OUStr
 
 void XMLSettingsExportHelper::exportByte(const sal_Int8 nValue, const rtl::OUString& rName) const
 {
+#if 0
     DBG_ASSERT(rName.getLength(), "no name");
     m_rContext.AddAttribute( XML_NAME, rName );
     m_rContext.AddAttribute( XML_TYPE, XML_BYTE );
@@ -211,6 +212,13 @@ void XMLSettingsExportHelper::exportByte(const sal_Int8 nValue, const rtl::OUStr
     SvXMLUnitConverter::convertNumber(sBuffer, sal_Int32(nValue));
     m_rContext.Characters( sBuffer.makeStringAndClear() );
     m_rContext.EndElement( sal_False );
+#else
+    (void) nValue; (void) rName;
+    OSL_ENSURE(false, "XMLSettingsExportHelper::exportByte(): #i114162#:\n"
+        "config-items of type \"byte\" are not valid ODF, "
+        "so storing them is disabled!\n"
+        "Use a different type instead (e.g. \"short\").");
+#endif
 }
 void XMLSettingsExportHelper::exportShort(const sal_Int16 nValue, const rtl::OUString& rName) const
 {
