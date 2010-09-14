f8e1d2438199c4a76ceeb36352678c415e25272c
Tor Lillqvist <tlillqvist@novell.com>
2010-09-14 14:42:40 +0200

    win32-dont-attempt-restart.diff: Don't attempt to restart OOo after crash
    n#524250
    
    Don't attempt to restart soffice.bin immediately if it crashes.  At
    such a restart, if the document recovery dialog is cancelled, an
    empty Writer document is opened even if the user was editing some
    other kind of document. Some customers are annoyed by this. And the
    automatic restart doesn't work on Linux anyway, so for consistency
    it doesn't hurt if we force it not to work on Windows, either.

--- desktop/win32/source/officeloader/officeloader.cxx
+++ desktop/win32/source/officeloader/officeloader.cxx
@@ -417,8 +417,7 @@ int WINAPI _tWinMain( HINSTANCE, HINSTANCE, LPTSTR, int )
             CloseHandle( aProcessInfo.hProcess );
             CloseHandle( aProcessInfo.hThread );
         }
-    } while ( fSuccess
-              && ( ::desktop::ExitHelper::E_CRASH_WITH_RESTART == dwExitCode || ::desktop::ExitHelper::E_NORMAL_RESTART == dwExitCode ));
+    } while ( false );
     delete[] lpCommandLine;
 
     return fSuccess ? dwExitCode : -1;
