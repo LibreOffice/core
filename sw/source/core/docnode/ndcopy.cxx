9a11bd42af0db32fd06c84dd955ee563134665ce
Amelia Wang <amwang@novell.com>
2010-09-14 16:20:45 +0200

    sw-outline-numbering-broken-fix.diff: outline numbering fix in master docs
    i#96092, n#445536

--- sw/source/core/docnode/ndcopy.cxx
+++ sw/source/core/docnode/ndcopy.cxx
@@ -258,7 +258,7 @@ SwCntntNode* SwTxtNode::MakeCopy( SwDoc* pDoc, const SwNodeIndex& rIdx ) const
     // kopiere Attribute/Text
     if( !pCpyAttrNd->HasSwAttrSet() )
         // wurde ein AttrSet fuer die Numerierung angelegt, so loesche diesen!
-        pTxtNd->ResetAllAttr();
+        pCpyAttrNd->ResetAllAttr();
 
     // if Copy-Textnode unequal to Copy-Attrnode, then copy first
     // the attributes into the new Node.
