9988d9e42d124ab8faed24a63fd6ad1e7d2e3ee9
Christian Lippka <cl@openoffice.org>
2010-09-17 17:49:45 +0200

    mib19: #163566# do not throw exceptions if path settings is not available

--- framework/source/services/pathsettings.cxx
+++ framework/source/services/pathsettings.cxx
@@ -243,21 +243,24 @@ void PathSettings::impl_readAll()
 OUStringList PathSettings::impl_readOldFormat(const ::rtl::OUString& sPath)
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "framework", "Ocke.Janssen@sun.com", "PathSettings::impl_readOldFormat" );
-    css::uno::Reference< css::container::XNameAccess > xCfg = fa_getCfgOld();
-    css::uno::Any                                      aVal = xCfg->getByName(sPath);
+    css::uno::Reference< css::container::XNameAccess > xCfg( fa_getCfgOld() );
+    OUStringList aPathVal;
 
-    ::rtl::OUString                       sStringVal;
-    css::uno::Sequence< ::rtl::OUString > lStringListVal;
-    OUStringList                          aPathVal;
+    if( xCfg->hasByName(sPath) )
+    {
+        css::uno::Any aVal( xCfg->getByName(sPath) );
 
-    if (aVal >>= sStringVal)
-    {
-        aPathVal.push_back(sStringVal);
-    }
-    else
-    if (aVal >>= lStringListVal)
-    {
-        aPathVal << lStringListVal;
+        ::rtl::OUString                       sStringVal;
+        css::uno::Sequence< ::rtl::OUString > lStringListVal;
+
+        if (aVal >>= sStringVal)
+        {
+            aPathVal.push_back(sStringVal);
+        }
+        else if (aVal >>= lStringListVal)
+        {
+            aPathVal << lStringListVal;
+        }
     }
 
     return aPathVal;
