c1aa8ce95e4823c1361bb2fc0c9aa298c4f5ab9c
Petr Mladek <pmladek@suse.cz>
2010-10-05 15:55:25 +0200

    more removed include guards using fixguard.py

--- testtools/source/bridgetest/currentcontextchecker.hxx
+++ testtools/source/bridgetest/currentcontextchecker.hxx
@@ -32,9 +32,7 @@
 #include "com/sun/star/uno/Reference.hxx"
 #include "com/sun/star/uno/RuntimeException.hpp"
 #include "cppuhelper/implbase1.hxx"
-#ifndef OSL_DIAGNOSE_HXX_INCLUDED
 #include "osl/diagnose.hxx"
-#endif
 #include "sal/types.h"
 #include "test/testtools/bridgetest/XCurrentContextChecker.hpp"
 
