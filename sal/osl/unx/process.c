ca5219d40dd1b3be5de7335bed2f90ce8c41c38a
Thomas Klausner <wiz@NetBSD.org>
2010-10-09 11:02:55 +0200

    Handle NetBSD in the same way as FreeBSD.

--- sal/osl/unx/process.c
+++ sal/osl/unx/process.c
@@ -41,7 +41,7 @@
 #endif
 
 
-#ifdef FREEBSD
+#if defined(FREEBSD) || defined(NETBSD)
 #include <machine/param.h>
 #endif
 
@@ -475,7 +475,7 @@ static void ChildStatusProc(void *pData)
 
             if (! INIT_GROUPS(data.m_name, data.m_gid) || (setuid(data.m_uid) != 0))
                 OSL_TRACE("Failed to change uid and guid, errno=%d (%s)\n", errno, strerror(errno));
-#if defined(LINUX) || defined (FREEBSD)
+#if defined(LINUX) || defined (FREEBSD) || defined(NETBSD)
             unsetenv("HOME");
 #else
             putenv("HOME=");
