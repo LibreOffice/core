af4e38f4d9174bc3f927ad119cab62e95264a5de
Caolán McNamara <caolanm@redhat.com>
2010-10-13 14:50:45 +0100

    update libtooling and config.guess for aix 7

--- redland/raptor/makefile.mk
+++ redland/raptor/makefile.mk
@@ -57,7 +57,7 @@ OOO_PATCH_FILES= \
     $(TARFILE_NAME).patch.ooo_build \
     $(TARFILE_NAME).patch.dmake \
     $(TARFILE_NAME).patch.win32 \
-
+    raptor-aix.patch
 
 PATCH_FILES=$(OOO_PATCH_FILES)
 
@@ -108,6 +108,10 @@ CFLAGS=-m64
 CPPFLAGS+:=-I$(SOLARINCDIR)$/external
 LDFLAGS+:=-L$(SOLARLIBDIR)
 
+.IF "$(OS)"=="AIX"
+LDFLAGS+:=$(LINKFLAGS) $(LINKFLAGSRUNPATH_OOO)
+.ENDIF
+
 .IF "$(SYSBASE)"!=""
 CPPFLAGS+:=-I$(SYSBASE)$/usr$/include
 .IF "$(OS)"=="SOLARIS" || "$(OS)"=="LINUX"
@@ -141,6 +145,9 @@ OUT2INC+=src$/raptor.h
 .IF "$(OS)"=="MACOSX"
 OUT2LIB+=src$/.libs$/libraptor.$(RAPTOR_MAJOR).dylib src$/.libs$/libraptor.dylib
 OUT2BIN+=src/raptor-config
+.ELIF "$(OS)"=="AIX"
+OUT2LIB+=src$/.libs$/libraptor.so.$(RAPTOR_MAJOR) src$/.libs$/libraptor.so
+OUT2BIN+=src/raptor-config
 .ELIF "$(OS)"=="WNT"
 .IF "$(COM)"=="GCC"
 OUT2LIB+=src$/.libs$/*.a
@@ -152,7 +159,7 @@ OUT2BIN+=src/raptor-config
 .ELIF "$(GUI)"=="OS2"
 # if we use dmake, this is done automagically
 .ELSE
-OUT2LIB+=src$/.libs$/libraptor.so.$(RAPTOR_MAJOR) src$/.libs$/libraptor.so
+OUT2LIB+=src$/.libs$/libraptor.so.$(RAPTOR_MAJOR)
 OUT2BIN+=src/raptor-config
 .ENDIF
 
