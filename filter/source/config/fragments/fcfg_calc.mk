244ccc275b58f63625631f512fdea4eb57d14614
Katarina Machalkova <kmachalkova@suse.cz>
2010-10-13 18:07:55 +0200

    Merged ooxml-filter-calc.diff from ooo-build

--- filter/source/config/fragments/fcfg_calc.mk
+++ filter/source/config/fragments/fcfg_calc.mk
@@ -27,7 +27,9 @@ T4_CALC = \
     calc_MS_Excel_2003_XML \
     MS_Excel_2007_XML \
     MS_Excel_2007_XML_Template \
-    MS_Excel_2007_Binary
+    MS_Excel_2007_Binary \
+    calc_OOXML \
+    calc_OOXML_Template
 
 # -----------------------------------------------
 # count = 28
@@ -57,7 +59,9 @@ F4_CALC = \
     MS_Excel_2003_XML \
     calc_MS_Excel_2007_XML \
     calc_MS_Excel_2007_XML_Template \
-    calc_MS_Excel_2007_Binary
+    calc_MS_Excel_2007_Binary \
+    calc_OOXML \
+    calc_OOXML_Template
 
 # -----------------------------------------------
 # count = 12
@@ -76,7 +80,9 @@ F4_UI_CALC = \
     MS_Excel_2003_XML_ui \
     calc_MS_Excel_2007_XML_ui \
     calc_MS_Excel_2007_XML_Template_ui \
-    calc_MS_Excel_2007_Binary_ui
+    calc_MS_Excel_2007_Binary_ui \
+    calc_OOXML_ui \
+    calc_OOXML_Template_ui
 
 # -----------------------------------------------
 # count = 0
