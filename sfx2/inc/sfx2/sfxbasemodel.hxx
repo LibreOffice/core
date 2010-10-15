7d22ff57241f19213453ecd0c98d67fb579a3803
Norbert Thiebaud <nthiebaud@gmail.com>
2010-10-14 23:11:47 -0500

    replace a OClareableGuard on the SolarMutex by a SolarClearableMutex

--- sfx2/inc/sfx2/sfxbasemodel.hxx
+++ sfx2/inc/sfx2/sfxbasemodel.hxx
@@ -1572,7 +1572,7 @@ public:
     };
 
     SfxModelGuard( SfxBaseModel& i_rModel, const AllowedModelState i_eState = E_FULLY_ALIVE )
-        :m_aGuard( Application::GetSolarMutex() )
+        : m_aGuard()
     {
         if ( i_rModel.IsDisposed() )
             throw ::com::sun::star::lang::DisposedException( ::rtl::OUString(), *&i_rModel );
@@ -1589,7 +1589,7 @@ public:
     }
 
 private:
-    ::vos::OClearableGuard  m_aGuard;
+    SolarMutexClearableGuard  m_aGuard;
 };
 
 #undef css
