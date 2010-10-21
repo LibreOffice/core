88b663b7317ef1c6239c9e5ca52b1a9ec14f1993
Miklos Vajna <vmiklos@frugalware.org>
2010-10-21 14:37:56 +0200

    #i114366# fix tables in page styles
    Done at openSUSE Conference 2010

--- sw/source/filter/ww8/rtfexport.cxx
+++ sw/source/filter/ww8/rtfexport.cxx
@@ -460,6 +460,10 @@ void RtfExport::WritePageDescTable()
     }
     Strm() << '}' << sNewLine;
     bOutPageDescs = FALSE;
+
+    // reset table infos, otherwise the depth of the cells will be incorrect,
+    // in case the page style (header or footer) had tables
+    mpTableInfo = ww8::WW8TableInfo::Pointer_t(new ww8::WW8TableInfo());
 }
 
 void RtfExport::ExportDocument_Impl()
