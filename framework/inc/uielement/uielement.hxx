3314bec88951b91c102fb26780cbb30d571b2a3f
Carsten Driesner <cd@openoffice.org>
2010-10-26 14:59:02 +0200

    #i112595# Transition from sal_Bool to bool

--- framework/inc/uielement/uielement.hxx
+++ framework/inc/uielement/uielement.hxx
@@ -60,83 +60,83 @@ struct DockedData
 {
     DockedData() : m_aPos( LONG_MAX, LONG_MAX ),
                    m_nDockedArea( ::com::sun::star::ui::DockingArea_DOCKINGAREA_TOP ),
-                   m_bLocked( sal_False ) {}
+                   m_bLocked( false ) {}
 
-    Point       m_aPos;
-    Size        m_aSize;
-    sal_Int16   m_nDockedArea;
-    sal_Bool    m_bLocked;
+    Point     m_aPos;
+    Size      m_aSize;
+    sal_Int16 m_nDockedArea;
+    bool      m_bLocked;
 };
 
 struct FloatingData
 {
     FloatingData() : m_aPos( LONG_MAX, LONG_MAX ),
                      m_nLines( 1 ),
-                     m_bIsHorizontal( sal_True ) {}
+                     m_bIsHorizontal( true ) {}
 
-    Point       m_aPos;
-    Size        m_aSize;
-    sal_Int16   m_nLines;
-    sal_Bool    m_bIsHorizontal;
+    Point     m_aPos;
+    Size      m_aSize;
+    sal_Int16 m_nLines;
+    bool      m_bIsHorizontal;
 };
 
 struct UIElement
 {
-    UIElement() : m_bFloating( sal_False ),
-                  m_bVisible( sal_True ),
-                  m_bUserActive( sal_False ),
-                  m_bCreateNewRowCol0( sal_False ),
-                  m_bDeactiveHide( sal_False ),
-                  m_bMasterHide( sal_False ),
-                  m_bContextSensitive( sal_False ),
-                  m_bContextActive( sal_True ),
-                  m_bNoClose( sal_False ),
-                  m_bSoftClose( sal_False ),
-                  m_bStateRead( sal_False ),
+    UIElement() : m_bFloating( false ),
+                  m_bVisible( true ),
+                  m_bUserActive( false ),
+                  m_bCreateNewRowCol0( false ),
+                  m_bDeactiveHide( false ),
+                  m_bMasterHide( false ),
+                  m_bContextSensitive( false ),
+                  m_bContextActive( true ),
+                  m_bNoClose( false ),
+                  m_bSoftClose( false ),
+                  m_bStateRead( false ),
                   m_nStyle( BUTTON_SYMBOL )
                   {}
 
     UIElement( const rtl::OUString& rName,
                const rtl::OUString& rType,
                const com::sun::star::uno::Reference< ::com::sun::star::ui::XUIElement >& rUIElement,
-               sal_Bool bFloating = sal_False
+               bool bFloating = false
                ) : m_aType( rType ),
                    m_aName( rName ),
                    m_xUIElement( rUIElement ),
                    m_bFloating( bFloating ),
-                   m_bVisible( sal_True ),
-                   m_bUserActive( sal_False ),
-                   m_bCreateNewRowCol0( sal_False ),
-                   m_bDeactiveHide( sal_False ),
-                   m_bMasterHide( sal_False ),
-                   m_bContextSensitive( sal_False ),
-                   m_bContextActive( sal_True ),
-                   m_bNoClose( sal_False ),
-                   m_bSoftClose( sal_False ),
-                   m_bStateRead( sal_False ),
+                   m_bVisible( true ),
+                   m_bUserActive( false ),
+                   m_bCreateNewRowCol0( false ),
+                   m_bDeactiveHide( false ),
+                   m_bMasterHide( false ),
+                   m_bContextSensitive( false ),
+                   m_bContextActive( true ),
+                   m_bNoClose( false ),
+                   m_bSoftClose( false ),
+                   m_bStateRead( false ),
                    m_nStyle( BUTTON_SYMBOL ) {}
 
     bool operator< ( const UIElement& aUIElement ) const;
     UIElement& operator=( const UIElement& rUIElement );
 
-    rtl::OUString                                                            m_aType;
-    rtl::OUString                                                            m_aName;
-    rtl::OUString                                                            m_aUIName;
-    com::sun::star::uno::Reference< ::com::sun::star::ui::XUIElement >       m_xUIElement;
-    sal_Bool                                                                 m_bFloating,
-                                                                             m_bVisible,
-                                                                             m_bUserActive,
-                                                                             m_bCreateNewRowCol0,
-                                                                             m_bDeactiveHide,
-                                                                             m_bMasterHide,
-                                                                             m_bContextSensitive,
-                                                                             m_bContextActive;
-    sal_Bool                                                                 m_bNoClose,
-                                                                             m_bSoftClose,
-                                                                             m_bStateRead;
-    sal_Int16                                                                m_nStyle;
-    DockedData                                                               m_aDockedData;
-    FloatingData                                                             m_aFloatingData;
+    rtl::OUString                                                      m_aType;
+    rtl::OUString                                                      m_aName;
+    rtl::OUString                                                      m_aUIName;
+    com::sun::star::uno::Reference< ::com::sun::star::ui::XUIElement > m_xUIElement;
+    bool                                                               m_bFloating,
+                                                                       m_bVisible,
+                                                                       m_bUserActive,
+                                                                       m_bCreateNewRowCol0,
+                                                                       m_bDeactiveHide,
+                                                                       m_bMasterHide,
+                                                                       m_bContextSensitive,
+                                                                       m_bContextActive;
+    bool                                                               m_bNoClose,
+                                                                       m_bSoftClose,
+                                                                       m_bStateRead;
+    sal_Int16                                                          m_nStyle;
+    DockedData                                                         m_aDockedData;
+    FloatingData                                                       m_aFloatingData;
 };
 
 typedef std::vector< UIElement > UIElementVector;
