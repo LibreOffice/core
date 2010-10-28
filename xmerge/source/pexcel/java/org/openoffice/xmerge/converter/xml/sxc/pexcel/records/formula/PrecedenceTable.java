b822b0e5619a113d8fce62aa77bb8dfaffcee5a8
Nigel Hawkins <n.hawkins@gmx.com>
2010-10-28 14:02:14 +0100

    Fix javadoc comments in filters/xmerge pexcel

--- xmerge/source/pexcel/java/org/openoffice/xmerge/converter/xml/sxc/pexcel/records/formula/PrecedenceTable.java
+++ xmerge/source/pexcel/java/org/openoffice/xmerge/converter/xml/sxc/pexcel/records/formula/PrecedenceTable.java
@@ -31,7 +31,7 @@ import java.util.HashMap;
 
 /**
  * This class defines the precedence applied to each operator when performing a conversion
- * {@link org.openoffice.xmerge.converter.xml.sxc.pexcel.Records.formula.FormulaCompiler.infix2 from infix to RPN.}.
+ * {@link org.openoffice.xmerge.converter.xml.sxc.pexcel.records.formula.FormulaCompiler.infix2RPN  from infix to RPN}.
  */
 public class PrecedenceTable {
     public static final int DEFAULT_PRECEDENCE  = 0;
