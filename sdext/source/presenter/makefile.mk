06bc09ab3839e0644375d40169882804d4ec76fe
Jan Holesovsky <kendy@suse.cz>
2010-11-05 00:39:43 +0100

    Fix a funny build issue in sdext when not building presenter console.

--- sdext/source/presenter/makefile.mk
+++ sdext/source/presenter/makefile.mk
@@ -42,13 +42,13 @@ MAXLINELENGTH:=80000
 
 PACKAGE=com.sun.PresenterScreen-$(PLATFORMID)
 
-.IF "$(L10N_framework)"==""
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
-
 .IF "$(ENABLE_PRESENTER_SCREEN)" == "NO"
 @all:
     @echo "Presenter Screen build disabled."
 .ELSE
+
+.IF "$(L10N_framework)"==""
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
 
 DLLPRE=
 common_build_zip=
@@ -379,10 +379,9 @@ $(DESCRIPTION) $(PHONYDESC) : $$(@:f)
     $(TYPE) $(DESCRIPTION_TMP) | sed s/UPDATED_PLATFORM/$(PLATFORMID)/ > $@
     @@-$(RM) $(DESCRIPTION_TMP)
 
-
-.ENDIF # "$(ENABLE_PRESENTER_SCREEN)" != "NO"
 .ELSE
 ivo:
     $(ECHO)
 .ENDIF # L10N_framework
 
+.ENDIF # "$(ENABLE_PRESENTER_SCREEN)" != "NO"
