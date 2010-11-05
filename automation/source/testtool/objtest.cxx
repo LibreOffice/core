509d298ac859a487aece51dcd4e23cd3237a3f68
Robert Nagy <robert@openbsd.org>
2010-11-05 11:16:45 +0100

    add OpenBSD platforms

--- automation/source/testtool/objtest.cxx
+++ automation/source/testtool/objtest.cxx
@@ -505,6 +505,10 @@ void TestToolObj::LoadIniFile()             // Laden der IniEinstellungen, die d
     abGP.Append( "23" );  // Linux ALPHA
 #elif defined NETBSD && defined X86_64
     abGP.Append( "24" );  // NetBSD/amd64
+#elif defined OPENBSD && defined X86
+    abGP.Append( "24" );  // OpenBSD/i386
+#elif defined OPENBSD && defined X86_64
+    abGP.Append( "25" );  // OpenBSD/amd64
 #else
 #error ("unknown platform. please request an ID for your platform on qa/dev")
 #endif
