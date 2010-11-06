470e4511e328a90c1f8e1ab631b1c3fb87b7246a
Robert Nagy <robert@openbsd.org>
2010-11-06 18:40:19 +0100

    add OPENBSD specific stuff

--- solenv/bin/mapgen.pl
+++ solenv/bin/mapgen.pl
@@ -135,7 +135,8 @@ sub filter_symbols {
         $env_section = '1' and next if ((/^# SOLARIS #$/) && ($ENV{OS} eq 'SOLARIS'));
         $env_section = '1' and next if ((/^# LINUX #$/) && ($ENV{OS} eq 'LINUX'));
         $env_section = '1' and next if ((/^# FREEBSD #$/) && ($ENV{OS} eq 'FREEBSD'));
-        last if ($env_section && ((/^# SOLARIS #$/) || (/^# FREEBSD #$/) || (/^# LINUX #$/)));
+        $env_section = '1' and next if ((/^# OPENBSD #$/) && ($ENV{OS} eq 'OPENBSD'));
+        last if ($env_section && ((/^# SOLARIS #$/) || (/^# FREEBSD #$/) || (/^# LINUX #$/) || (/^# OPENBSD #$/)));
         next if (!$_ || /^#/);
         push(@filters, $_);
     };
