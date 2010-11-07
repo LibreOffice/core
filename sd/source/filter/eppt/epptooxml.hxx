bbfd27406e70e7fba275882a7ab2c284a479899c
Tor Lillqvist <tlillqvist@novell.com>
2010-11-08 00:33:34 +0200

    Dubious fix for compilation error

--- sd/source/filter/eppt/epptooxml.hxx
+++ sd/source/filter/eppt/epptooxml.hxx
@@ -85,7 +85,7 @@ public:
     virtual oox::vml::Drawing* getVmlDrawing() { return NULL; }
     virtual const oox::drawingml::Theme* getCurrentTheme() const { return NULL; }
     virtual const oox::drawingml::table::TableStyleListPtr getTableStyles() { return oox::drawingml::table::TableStyleListPtr(); }
-    virtual oox::drawingml::chart::ChartConverter* getChartConverter() { return NULL; }
+    virtual oox::drawingml::chart::ChartConverter& getChartConverter() { static oox::drawingml::chart::ChartConverter aConverter; return aConverter; }
 
     static const char* GetSideDirection( sal_uInt8 nDirection );
     static const char* GetCornerDirection( sal_uInt8 nDirection );
