255a95f5cd74867c781051700c54f41bba4d6d95
Mihaela Kedikova <misheto@openoffice.org>
2010-11-08 17:59:39 +0100

    gridcontrol07: bugfixes for gridcontrol

--- svtools/source/table/tablecontrol_impl.cxx
+++ svtools/source/table/tablecontrol_impl.cxx
@@ -948,8 +948,17 @@ namespace svt { namespace table
             //In the case that column headers are defined but data hasn't yet been set,
             //only column headers will be shown
             if(m_pModel->hasColumnHeaders())
+            {
                 if(m_nColHeaderHeightPixel>1)
+                {
                     m_pDataWindow->SetSizePixel( m_rAntiImpl.GetOutputSizePixel());
+                    //fix for 163360 - tests with other examples!!!
+                    if(m_bResizingGrid)
+                    //update column widths to fit in grid - 18.10.10
+                        impl_ni_updateColumnWidths();
+                    m_bResizingGrid = true;
+                }
+            }
             if(m_nColumnCount != 0)
                 impl_ni_updateScrollbars();
         }
@@ -2012,7 +2021,9 @@ namespace svt { namespace table
         impl_ni_getAccVisibleColWidths();
         int newColWidth = m_aColumnWidthsPixel[m_nCurColumn];
         //subtract 1 from m_aAccColumnWidthPixel because right border should be part of the current cell
-        if(m_aVisibleColumnWidthsPixel[resizingColumn]-1 == rPoint.X() && pColumn->isResizable())
+        int nLeft = m_aVisibleColumnWidthsPixel[resizingColumn]-4;
+        int nRight = m_aVisibleColumnWidthsPixel[resizingColumn]-1;
+        if( rPoint.X()> nLeft && rPoint.X()<nRight && pColumn->isResizable())
             aNewPointer = Pointer( POINTER_HSPLIT );
         //MouseButton was pressed but not yet released, mouse is moving
         if(m_bResizing)
@@ -2040,7 +2051,9 @@ namespace svt { namespace table
         m_bResizingGrid = false;
         m_nResizingColumn = m_nCurColumn;
         PColumnModel pColumn = m_pModel->getColumnModel(m_nResizingColumn);
-        if(m_aVisibleColumnWidthsPixel[m_nResizingColumn-m_nLeftColumn]-1 == rPoint.X() && pColumn->isResizable())
+        int nLeft = m_aVisibleColumnWidthsPixel[m_nResizingColumn-m_nLeftColumn]-4;
+        int nRight = m_aVisibleColumnWidthsPixel[m_nResizingColumn-m_nLeftColumn]-1;
+        if(rPoint.X()> nLeft && rPoint.X()<nRight && pColumn->isResizable())
         {
             m_pDataWindow->CaptureMouse();
             m_bResizing = true;
