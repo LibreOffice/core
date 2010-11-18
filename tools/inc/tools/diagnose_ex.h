088f05ac5fce17a04f7f5142bffd04b895d59455
Kevin Hunter <hunteke@earlham.edu>
2010-11-18 17:43:40 -0500

    EasyHack: RTL_CONST macro from createFromAscii

--- tools/inc/tools/diagnose_ex.h
+++ tools/inc/tools/diagnose_ex.h
@@ -91,14 +91,14 @@
                                      OSL_ENSURE(c, m); \
                                      throw ::com::sun::star::lang::IllegalArgumentException( \
                                      ::rtl::OUString::createFromAscii(BOOST_CURRENT_FUNCTION) + \
-                                     ::rtl::OUString::createFromAscii(",\n"m), \
+                                     ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( ",\n"m )), \
                                      ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >(), \
                                      0 ); }
 #define ENSURE_ARG_OR_THROW2(c, m, ifc, arg) if( !(c) ) { \
                                                OSL_ENSURE(c, m); \
                                                throw ::com::sun::star::lang::IllegalArgumentException( \
                                                ::rtl::OUString::createFromAscii(BOOST_CURRENT_FUNCTION) + \
-                                               ::rtl::OUString::createFromAscii(",\n"m), \
+                                               ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( ",\n"m )), \
                                                ifc, \
                                                arg ); }
 
@@ -109,13 +109,13 @@
                                      OSL_ENSURE(c, m); \
                                      throw ::com::sun::star::uno::RuntimeException( \
                                      ::rtl::OUString::createFromAscii(BOOST_CURRENT_FUNCTION) + \
-                                     ::rtl::OUString::createFromAscii(",\n"m), \
+                                     ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( ",\n"m )), \
                                      ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >() ); }
 #define ENSURE_OR_THROW2(c, m, ifc) if( !(c) ) { \
                                           OSL_ENSURE(c, m); \
                                           throw ::com::sun::star::uno::RuntimeException( \
                                           ::rtl::OUString::createFromAscii(BOOST_CURRENT_FUNCTION) + \
-                                          ::rtl::OUString::createFromAscii(",\n"m), \
+                                          ::rtl::OUString::( RTL_CONSTASCII_USTRINGPARAM( ",\n"m )), \
                                           ifc ); }
 
 /** This macro asserts the given condition (in debug mode), and
