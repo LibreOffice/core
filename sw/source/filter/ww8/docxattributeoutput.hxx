93a0a92bc07b541009ccd0e8d536a50dc43c2672
Miklos Vajna <vmiklos@frugalware.org>
2010-11-22 14:30:26 +0100

    Add doxygen comment for DocxAttributeOutput

--- sw/source/filter/ww8/docxattributeoutput.hxx
+++ sw/source/filter/ww8/docxattributeoutput.hxx
@@ -64,6 +64,7 @@ enum DocxColBreakStatus
     COLBRK_WRITE
 };
 
+/// The class that has handlers for various resource types when exporting as DOCX.
 class DocxAttributeOutput : public AttributeOutputBase
 {
 public:
