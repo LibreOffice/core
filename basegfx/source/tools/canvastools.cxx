3f3fe0b4e3dfe8801920e4b0c1d8c2364120fba9
Gert Faller <gertfaller@aliceadsl.fr>
2010-11-23 18:54:05 +0100

    RTL_CONSTASCII_USTRINGPARAM in libs-gui 12

--- basegfx/source/tools/canvastools.cxx
+++ basegfx/source/tools/canvastools.cxx
@@ -331,9 +331,9 @@ namespace basegfx
                     if( !xLinePoly.is() )
                     {
                         throw lang::IllegalArgumentException(
-                            ::rtl::OUString::createFromAscii(
+                            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
                                     "basegfx::unotools::b2DPolyPolygonFromXPolyPolygon2D(): Invalid input"
-                                    "poly-polygon, cannot retrieve vertex data"),
+                                    "poly-polygon, cannot retrieve vertex data")),
                             uno::Reference< uno::XInterface >(),
                             0 );
                     }
