c24d569cb20c98409b40e8a77b7bdbbcc01c4568
Caolán McNamara <caolanm@redhat.com>
2010-11-24 11:39:55 +0000

    cppcheck: Member variable not initialized in the constructor

--- svx/source/dialog/fntctrl.cxx
+++ svx/source/dialog/fntctrl.cxx
@@ -147,7 +147,7 @@ class FontPrevWin_Impl
 public:
     inline FontPrevWin_Impl() :
         pPrinter( NULL ), bDelPrinter( FALSE ),
-        pColor( NULL ), pBackColor( 0 ),
+        pColor( NULL ), pBackColor( 0 ), nAscent( 0 ),
         cStartBracket( 0 ), cEndBracket( 0 ), nFontWidthScale( 100 ),
         bSelection( FALSE ), bGetSelection( FALSE ), bUseResText( FALSE ),
         bTwoLines( FALSE ),
