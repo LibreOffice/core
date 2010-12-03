2e38ae3d6670d72103bfd06c8b1b5d169e618f4e
Luboš Luňák <l.lunak@suse.cz>
2010-12-03 18:07:53 +0100

    oxygen icon set no longer comes as a tarball

--- packimages/pack/makefile.mk
+++ packimages/pack/makefile.mk
@@ -44,7 +44,6 @@ CUSTOM_PREFERRED_FALLBACK_1*=-c $(SOLARSRC)$/ooo_custom_images$/tango
 CUSTOM_PREFERRED_FALLBACK_2*=-c $(SOLARSRC)$/ooo_custom_images$/industrial
 
 CRYSTAL_TARBALL=$(SOLARSRC)$/external_images$/ooo_crystal_images-1.tar.gz
-OXYGEN_TARBALL=$(SOLARSRC)$/external_images$/ooo_oxygen_images-2009-06-17.tar.gz
 
 ALLTAR : $(IMAGES) $(CUSTOM_IMAGES) $(COMMONBIN)$/images_brand.zip
 
@@ -78,15 +77,5 @@ $(MISC)$/crystal.flag : $(CRYSTAL_TARBALL)
 .ENDIF
     @$(TYPE) $@ || echo "ERROR: unpacking $(CRYSTAL_TARBALL) failed"
 
-# unpack the Oxygen icon set
-$(MISC)$/oxygen.flag : $(OXYGEN_TARBALL)
-    cd $(MISC) && gzip -d -c $(OXYGEN_TARBALL) | ( tar -xf - ) && $(TOUCH) $(@:f)
-.IF "$(GUI)"=="UNX"
-    chmod -R g+w $(MISC)$/oxygen
-.ENDIF
-    @$(TYPE) $@ || echo "ERROR: unpacking $(CRYSTAL_TARBALL) failed"
-
 # dependencies
 images_crystal : $(MISC)$/crystal.flag $(RES)$/img$/commandimagelist.ilst
-
-images_oxygen : $(MISC)$/oxygen.flag $(RES)$/img$/commandimagelist.ilst
