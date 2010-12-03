be8d00fe05ad77bdf4aa86e37faab84b35fb8da8
Frank Schoenheit [fs] <frank.schoenheit@oracle.com>
2010-12-03 14:46:39 +0100

    dba34c: #i104347# DisableSelectionOnFocus for multi line edit controls

--- svtools/source/uno/unoiface.cxx
+++ svtools/source/uno/unoiface.cxx
@@ -82,6 +82,7 @@ SAL_DLLPUBLIC_EXPORT Window* CreateWindow( VCLXWindow** ppNewComp, const ::com::
         if ( pParent )
         {
             pWindow = new MultiLineEdit( pParent, nWinBits|WB_IGNORETAB);
+            static_cast< MultiLineEdit* >( pWindow )->DisableSelectionOnFocus();
             *ppNewComp = new VCLXMultiLineEdit;
         }
         else
