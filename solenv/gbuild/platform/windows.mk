e1f8a3e42fb9ae2f3d4804aae05a1fca6ac0c403
Norbert Thiebaud <nthiebaud@gmail.com>
2010-12-15 16:37:48 -0600

    fixing windows compile error output, static libs on windows

--- solenv/gbuild/platform/windows.mk
+++ solenv/gbuild/platform/windows.mk
@@ -226,7 +226,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(6) \
 		-c $(3) \
 		-Fo$(1)" && \
-	E=$$($$C) || (R=$$? && echo $$C && echo $$E 1>&2 && $$(exit $$R)))
+	E=$$($$C) || (R=$$? && echo "$$C" && echo "$$E" 1>&2 && $$(exit $$R)))
 $(call gb_CObject__command_deponcompile,$(1),$(2),$(3),$(4),$(5),$(6))
 endef
 
@@ -269,7 +269,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(6) \
 		-c $(3) \
 		-Fo$(1)" && \
-	E=$$($$C) || (R=$$? && echo $$C && echo $$E 1>&2 && $$(exit $$R)))
+	E=$$($$C) || (R=$$? && echo "$$C" && echo "$$E" 1>&2 && $$(exit $$R)))
 $(call gb_CxxObject__command_deponcompile,$(1),$(2),$(3),$(4),$(5),$(6))
 
 endef
@@ -310,7 +310,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(6) \
 		-c $(3) \
 		-Yc$(notdir $(patsubst %.cxx,%.hxx,$(3))) -Fp$(1)" && \
-	E=$$($$C) || (R=$$? && echo $$C && echo $$E 1>&2 && $$(exit $$R)))
+	E=$$($$C) || (R=$$? && echo "$$C" && echo "$$E" 1>&2 && $$(exit $$R)))
 	$(call gb_PrecompiledHeader__command_deponcompile,$(1),$(2),$(3),$(4),$(5),$(6))
 
 endef
@@ -350,7 +350,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(6) \
 		-c $(3) \
 		-Yc$(notdir $(patsubst %.cxx,%.hxx,$(3))) -Fp$(1)" && \
-	E=$$($$C) || (R=$$? && echo $$C && echo $$E 1>&2 && $$(exit $$R)))
+	E=$$($$C) || (R=$$? && echo "$$C" && echo "$$E" 1>&2 && $$(exit $$R)))
 	$(call gb_NoexPrecompiledHeader__command_deponcompile,$(1),$(2),$(3),$(4),$(5),$(6))
 
 endef
@@ -392,7 +392,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(foreach lib,$(4),$(call gb_Library_get_filename,$(lib))) \
 		$(foreach lib,$(5),$(call gb_StaticLibrary_get_filename,$(lib))) \
 		$(subst -out: -implib:$(1),-out:$(1),-out:$(DLLTARGET) -implib:$(1))" && \
-	E=$$($$C) || (R=$$? && echo $$C && echo $$E 1>&2 && rm -f $(1)); \
+	E=$$($$C) || (R=$$? && echo "$$C" && echo "$$E" 1>&2 && rm -f $(1)); \
 	rm $${RESPONSEFILE} && $$(exit $$R))
 endef
 
@@ -523,7 +523,7 @@ endef
 # StaticLibrary class
 
 gb_StaticLibrary_DEFS :=
-gb_StaticLibrary_TARGETTYPEFLAGS :=
+gb_StaticLibrary_TARGETTYPEFLAGS := -LIB
 gb_StaticLibrary_SYSPRE :=
 gb_StaticLibrary_PLAINEXT := .lib
 gb_StaticLibrary_JPEGEXT := lib$(gb_StaticLibrary_PLAINEXT)
