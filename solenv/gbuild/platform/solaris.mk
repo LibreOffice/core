b2e8ac2e05d012e5cbf1801033bcfec6688079f2
Norbert Thiebaud <nthiebaud@gmail.com>
2010-12-15 16:33:14 -0600

    gb_CxxObject__command_dep is not platform dependant anymore

--- solenv/gbuild/platform/solaris.mk
+++ solenv/gbuild/platform/solaris.mk
@@ -152,12 +152,6 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(6))
 endef
 
-define gb_CxxObject_command_dep
-mkdir -p $(dir $(1)) && \
-	echo '$(call gb_CxxObject_get_target,$(2)) : $$(gb_Helper_PHONY)' > $(1)
-endef
-
-
 # LinkTarget class
 
 define gb_LinkTarget__get_rpath_for_layer
