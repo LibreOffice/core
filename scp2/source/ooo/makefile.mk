956dfd48c13450f62e23bebe89e92f9c30621bd4
Fridrich Štrba <fridrich.strba@bluewin.ch>
2010-12-16 11:40:20 +0100

    Descriptions for helppacks

--- scp2/source/ooo/makefile.mk
+++ scp2/source/ooo/makefile.mk
@@ -316,6 +316,7 @@ ULFFILES=                          \
         folderitem_ooo.ulf         \
         module_ooo.ulf             \
         module_langpack.ulf        \
+        module_helppack.ulf        \
         module_java.ulf            \
         registryitem_ooo.ulf       \
         module_systemint.ulf
