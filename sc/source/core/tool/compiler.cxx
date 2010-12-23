ce346973915d6f3270b00e9f289137b8cd556286
Kohei Yoshida <kyoshida@novell.com>
2010-12-23 12:14:45 -0500

    Remove bogus check for numerical sheet names. (fdo#32570)
    We apparently put numerical sheet name into quotes somewhere else,
    so checking for a number followed by a '.' and flagging it as bad
    is not only necessary, it's not appropriate.  E.g. we do support
    other formula syntax where the sheet-to-reference separator is not
    '.' (Excel A1 and Excel R1C1), thereby making this test even more
    inappropriate.
    
    Signed-off-by: Jan Holesovsky <kendy@suse.cz>

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -2663,8 +2663,7 @@ BOOL ScCompiler::IsValue( const String& rSym )
     double fVal;
     sal_uInt32 nIndex = ( mxSymbols->isEnglish() ?
         pDoc->GetFormatTable()->GetStandardIndex( LANGUAGE_ENGLISH_US ) : 0 );
-//  ULONG nIndex = 0;
-////    ULONG nIndex = pDoc->GetFormatTable()->GetStandardIndex(ScGlobal::eLnge);
+
     if (pDoc->GetFormatTable()->IsNumberFormat( rSym, nIndex, fVal ) )
     {
         USHORT nType = pDoc->GetFormatTable()->GetType(nIndex);
@@ -2685,10 +2684,6 @@ BOOL ScCompiler::IsValue( const String& rSym )
             if (*p == '(')
                 return FALSE;   // Boolean function instead.
         }
-
-        if( aFormula.GetChar(nSrcPos) == '.' )
-            // numerical sheet name?
-            return FALSE;
 
         if( nType == NUMBERFORMAT_TEXT )
             // HACK: number too big!
