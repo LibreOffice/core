feb562dc6f0486e162bd669c46f9941750060b69
Caolán McNamara <caolanm@redhat.com>
2010-12-23 12:51:06 +0000

    cppcheck: prefer prefix variant

--- sw/source/core/fields/flddropdown.cxx
+++ sw/source/core/fields/flddropdown.cxx
@@ -141,7 +141,7 @@ uno::Sequence<OUString> SwDropDownField::GetItemSequence() const
     int i = 0;
     vector<String>::const_iterator aIt;
 
-    for (aIt = aValues.begin(); aIt != aValues.end(); aIt++)
+    for (aIt = aValues.begin(); aIt != aValues.end(); ++aIt)
     {
         pSeq[i] = rtl::OUString(*aIt);
 
