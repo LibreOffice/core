dd42af8c16804d848b1358ea46811fae64088f9f
Frank Schoenheit [fs] <frank.schoenheit@oracle.com>
2010-12-23 15:48:53 +0100

    gridsort: re-factoring the column-resizing thingie, step 1 In the current implementation, this is rather complex, and hardly maintainable. Also, it all happens in TableControl_Impl and TableDataWindow, while in the original design, the InputHandler was intended to care for this kind of tasks (one class - one responsibility).

--- automation/source/server/statemnt.cxx
+++ automation/source/server/statemnt.cxx
@@ -6207,7 +6207,7 @@ protected:
                                                             Point aPos( aSize.Width() / 2, aSize.Height() / 2 );
                                                             long nStep = aSize.Height() / 4;
                                                             ::svt::table::RowPos nLastPos;
-                                                            while ( ( nLastPos = pTC->GetCurrentRow( aPos ) ) != nNr1-1 && nStep > 0 )
+                                                            while ( ( nLastPos = pTC->GetRowAtPoint( aPos ) ) != nNr1-1 && nStep > 0 )
                                                             {
                                                                 if ( nLastPos > nNr1-1 || nLastPos == ROW_INVALID )
                                                                     aPos.Y() -= nStep;
@@ -6215,7 +6215,7 @@ protected:
                                                                     aPos.Y() += nStep;
                                                                 nStep /= 2;
                                                             }
-                                                            if ( pTC->GetCurrentRow( aPos ) == nNr1-1 )
+                                                            if ( pTC->GetRowAtPoint( aPos ) == nNr1-1 )
                                                             {
                                                                 MouseEvent aMEvnt(aPos,1,MOUSE_SIMPLECLICK|MOUSE_SELECT,MOUSE_LEFT,KEY_MOD1);
                                                                 pTC->getSelEngine()->SelMouseButtonDown( aMEvnt );
