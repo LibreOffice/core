085b4a47ca5b0f9b71e5b8994ac7c46bb6628338
Joseph Powers <jpowers27@cox.net>
2011-01-03 08:11:31 -0800

    Revert previous patch

--- svx/inc/svx/gridctrl.hxx
+++ svx/inc/svx/gridctrl.hxx
@@ -47,12 +47,17 @@
 #include <comphelper/propmultiplex.hxx>
 #include <svtools/transfer.hxx>
 #include "svx/svxdllapi.h"
-#include <vector>
 
 class DbGridControl;
 class CursorWrapper;
 
 sal_Bool CompareBookmark(const ::com::sun::star::uno::Any& aLeft, const ::com::sun::star::uno::Any& aRight);
+
+namespace svxform
+{
+    class DataColumn;
+}
+DECLARE_LIST(DbDataColumns, ::svxform::DataColumn*)
 
 enum GridRowStatus
 {
@@ -69,6 +74,7 @@ enum GridRowStatus
 class DbGridRow : public SvRefBase
 {
     ::com::sun::star::uno::Any                      m_aBookmark;        // ::com::sun::star::text::Bookmark of the row, can be set
+    DbDataColumns               m_aVariants;
     GridRowStatus               m_eStatus;
     sal_Bool                        m_bIsNew;
                                                     // row is no longer valid
@@ -79,6 +85,9 @@ public:
     void SetState(CursorWrapper* pCur, sal_Bool bPaintCursor);
 
     ~DbGridRow();
+
+    sal_Bool HasField(sal_uInt32 nPos) const {return nPos < m_aVariants.Count();}
+    const ::svxform::DataColumn& GetField(sal_uInt32 nPos) const { return *m_aVariants.GetObject(nPos); }
 
     void            SetStatus(GridRowStatus _eStat) { m_eStatus = _eStat; }
     GridRowStatus   GetStatus() const               { return m_eStatus; }
