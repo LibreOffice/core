a95e4952297007634302b11e3eaa3d0001a40f47
Caolán McNamara <caolanm@redhat.com>
2011-01-06 14:03:55 +0000

    cppunit: prefer prefix variant

--- desktop/source/deployment/registry/dp_backend.cxx
+++ desktop/source/deployment/registry/dp_backend.cxx
@@ -122,7 +122,7 @@ void PackageRegistryBackend::check()
 void PackageRegistryBackend::disposing()
 {
     try {
-        for ( t_string2ref::const_iterator i = m_bound.begin(); i != m_bound.end(); i++)
+        for ( t_string2ref::const_iterator i = m_bound.begin(); i != m_bound.end(); ++i)
             i->second->removeEventListener(this);
         m_bound.clear();
         m_xComponentContext.clear();
