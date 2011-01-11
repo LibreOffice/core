80ae0dcf217c23ff73b82d0c37274415012942ab
Bjoern Michaelsen <b_michaelsen@openoffice.org>
2011-01-11 21:16:21 +0100

    gnumake3: more work on junit subsequenttest migration

--- sw/JunitTest_sw_unoapi.mk
+++ sw/JunitTest_sw_unoapi.mk
@@ -2,7 +2,7 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 # 
-# Copyright 2009 by Sun Microsystems, Inc.
+# Copyright 2000, 2011 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
@@ -14,34 +14,32 @@
 #
 # OpenOffice.org is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License version 3 for more details
 # (a copy is included in the LICENSE file that accompanied this code).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# version 3 along with OpenOffice.org.	If not, see
+# version 3 along with OpenOffice.org.  If not, see
 # <http://www.openoffice.org/license.html>
 # for a copy of the LGPLv3 License.
 #
 #*************************************************************************
 
-$(eval $(call gb_Module_Module,sw))
+$(eval $(call gb_JunitTest_JunitTest,sw_unoapi))
 
-$(eval $(call gb_Module_add_targets,sw,\
-    AllLangResTarget_sw \
-    Library_msword \
-    Library_sw \
-    Library_swd \
-    Library_swui \
-    Library_vbaswobj \
-    Package_misc \
-    Package_uiconfig \
-    Package_xml \
+$(eval $(call gb_JunitTest_set_defs,sw_unoapi,\
+    $$(DEFS) \
+    -Dorg.openoffice.test.arg.sce=$(SRCDIR)/sw/qa/unoapi/sw.sce \
+    -Dorg.openoffice.test.arg.xcl=$(SRCDIR)/sw/qa/unoapi/knownissues.xcl \
+    -Dorg.openoffice.test.arg.tdoc=$(SRCDIR)/sw/qa/unoapi/testdocuments \
 ))
 
-$(eval $(call gb_Module_add_subsequentcheck_targets,sw,\
-    JunitTest_sw_complex \
-    JunitTest_sw_unoapi \
+$(eval $(call gb_JunitTest_add_sourcefiles,sw_unoapi,\
+    sw/qa/unoapi/Test \
 ))
 
-# vim: set noet ts=4 sw=4:
+$(eval $(call gb_JunitTest_add_classes,sw_unoapi,\
+    org.openoffice.sw.qa.unoapi.Test \
+))
+
+# vim: set noet sw=4 ts=4:
