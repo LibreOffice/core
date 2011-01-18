82d2570a643f82c3405e724533567a61120ce2c0
Thomas Arnhold <thomas@arnhold.org>
2011-01-18 08:27:38 +0100

    Remove obsolete RCS lines.

--- sw/source/ui/docvw/AnchorOverlayObject.cxx
+++ sw/source/ui/docvw/AnchorOverlayObject.cxx
@@ -7,9 +7,6 @@
  *
  * OpenOffice.org - a multi-platform office productivity suite
  *
- * $RCSfile: postit.cxx,v $
- * $Revision: 1.8.42.11 $
- *
  * This file is part of OpenOffice.org.
  *
  * OpenOffice.org is free software: you can redistribute it and/or modify
