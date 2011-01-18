eb05c1e908368564ace6c86fd5e929d432dff249
jonathan aquilina <jaquilina@eagleeyet.net>
2011-01-18 11:24:53 +0100

    removed double line spacing

--- filter/source/xmlfilteradaptor/genericfilter.cxx
+++ filter/source/xmlfilteradaptor/genericfilter.cxx
@@ -28,153 +28,80 @@
 
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_filter.hxx"
-
 #include <stdio.h>
-
-
-
 #include <osl/mutex.hxx>
-
 #include <osl/thread.h>
-
 #include <cppuhelper/factory.hxx>
 
-
-
 #ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
-
 #include <com/sun/star/lang/XSingleServiceFactory.hpp>
-
 #endif
-
-
 
 #include "XmlFilterAdaptor.hxx"
 
 
 
 using namespace ::rtl;
-
 using namespace ::cppu;
-
 using namespace ::com::sun::star::uno;
-
 using namespace ::com::sun::star::lang;
-
 using namespace ::com::sun::star::registry;
 
-
-
 extern "C"
-
 {
-
 //==================================================================================================
-
 void SAL_CALL component_getImplementationEnvironment(
-
     const sal_Char ** ppEnvTypeName, uno_Environment ** /* ppEnv */ )
-
 {
-
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
-
 }
-
 //==================================================================================================
-
 sal_Bool SAL_CALL component_writeInfo(
-
     void * /* pServiceManager */, void * pRegistryKey )
-
 {
-
     if (pRegistryKey)
-
     {
-
         try
-
         {
-
             Reference< XRegistryKey > xNewKey(
-
                 reinterpret_cast< XRegistryKey * >( pRegistryKey )->createKey( XmlFilterAdaptor_getImplementationName() ) );
-
             xNewKey = xNewKey->createKey( OUString( RTL_CONSTASCII_USTRINGPARAM( "/UNO/SERVICES" )) );
 
-
-
             const Sequence< OUString > & rSNL = XmlFilterAdaptor_getSupportedServiceNames();
-
             const OUString * pArray = rSNL.getConstArray();
 
             for ( sal_Int32 nPos = rSNL.getLength(); nPos--; )
-
                 xNewKey->createKey( pArray[nPos] );
-
-
-
             return sal_True;
-
         }
-
         catch (InvalidRegistryException &)
-
         {
-
             OSL_ENSURE( sal_False, "### InvalidRegistryException!" );
-
         }
-
     }
-
     return sal_False;
-
 }
 
 //==================================================================================================
 
 void * SAL_CALL component_getFactory(
-
     const sal_Char * pImplName, void * pServiceManager, void * /* pRegistryKey */ )
-
 {
-
     void * pRet = 0;
-
-
-
     OUString implName = OUString::createFromAscii( pImplName );
-
     if ( pServiceManager && implName.equals(XmlFilterAdaptor_getImplementationName()) )
-
     {
-
         Reference< XSingleServiceFactory > xFactory( createSingleFactory(
-
             reinterpret_cast< XMultiServiceFactory * >( pServiceManager ),
-
             OUString::createFromAscii( pImplName ),
-
             XmlFilterAdaptor_createInstance, XmlFilterAdaptor_getSupportedServiceNames() ) );
-
-
-
         if (xFactory.is())
-
         {
-
             xFactory->acquire();
-
             pRet = xFactory.get();
-
         }
-
     }
-
     return pRet;
-
 }
 
 }
