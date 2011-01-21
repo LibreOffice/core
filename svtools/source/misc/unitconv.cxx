7ba80b24256d49eb765f2943c1495c1511bb7b69
Thomas Arnhold <thomas@arnhold.org>
2011-01-21 14:23:59 +0100

    Remove old RCS lines.

--- svtools/source/misc/unitconv.cxx
+++ svtools/source/misc/unitconv.cxx
@@ -7,9 +7,6 @@
  *
  * OpenOffice.org - a multi-platform office productivity suite
  *
- * $RCSfile: dlgutil.cxx,v $
- * $Revision: 1.17 $
- *
  * This file is part of OpenOffice.org.
  *
  * OpenOffice.org is free software: you can redistribute it and/or modify
