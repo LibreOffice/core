37806a5199bfb6d66ad8edc68a90024f29a413bd
Thomas Arnhold <thomas@arnhold.org>
2011-01-21 14:49:25 +0100

    Remove old RCS lines.

--- slideshow/source/engine/OGLTrans/unx/OGLTrans_TransitionImpl.cxx
+++ slideshow/source/engine/OGLTrans/unx/OGLTrans_TransitionImpl.cxx
@@ -6,9 +6,6 @@
  *
  * OpenOffice.org - a multi-platform office productivity suite
  *
- * $RCSfile: OGLTrans_TransitionImpl.cxx,v $
- * $Revision: 1.5 $
- *
  * This file is part of OpenOffice.org.
  *
  * OpenOffice.org is free software: you can redistribute it and/or modify
--- slideshow/source/engine/OGLTrans/win/OGLTrans_TransitionImpl.hxx
+++ slideshow/source/engine/OGLTrans/win/OGLTrans_TransitionImpl.hxx
@@ -6,9 +6,6 @@
  *
  * OpenOffice.org - a multi-platform office productivity suite
  *
- * $RCSfile: OGLTrans_TransitionImpl.hxx,v $
- * $Revision: 1.5.12.1 $
- *
  * This file is part of OpenOffice.org.
  *
  * OpenOffice.org is free software: you can redistribute it and/or modify
