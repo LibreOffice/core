0c61e01a8a0a181c024f153e1f9fdd73c54fe6cf
Caolán McNamara <caolanm@redhat.com>
2011-01-24 10:41:10 +0000

    WaE: gcc 4.6.0 various warnings

--- writerperfect/source/filter/Style.hxx
+++ writerperfect/source/filter/Style.hxx
@@ -34,7 +34,7 @@
 class TopLevelElementStyle
 {
 public:
-    TopLevelElementStyle() : mpsMasterPageName(NULL) { }
+    TopLevelElementStyle() : mpsMasterPageName(0) { }
     virtual ~TopLevelElementStyle() { if (mpsMasterPageName) delete mpsMasterPageName; }
     void setMasterPageName(WPXString &sMasterPageName) { mpsMasterPageName = new WPXString(sMasterPageName); }
     const WPXString * getMasterPageName() const { return mpsMasterPageName; }
