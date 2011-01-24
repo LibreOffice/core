2b803d5c6f93637239d6ea3fcff2ad47cd3e2dc2
Thomas Arnhold <thomas@arnhold.org>
2011-01-24 11:51:45 +0100

    Remove FUTURE directive.

--- sd/source/filter/ppt/ppt97animations.hxx
+++ sd/source/filter/ppt/ppt97animations.hxx
@@ -129,9 +129,6 @@ private: //private methods
     bool HasAfterEffect() const;
     bool HasAfterEffect_ChangeColor() const;
     bool HasAfterEffect_DimAtNextEffect() const;
-#ifdef FUTURE
-    bool HasAfterEffect_DimAfterEffect() const;
-#endif
     bool HasStopPreviousSound() const;
     bool HasReverseOrder() const; //true if the text paragraphs should be animated in reverse order
     sal_Int32 GetParagraphLevel() const; //paragraph level that is animated ( that paragraph and higher levels )
