a2cf60a2924518d62bfcebc72a0b6354c20f4bc9
David Tardon <dtardon@redhat.com>
2011-01-25 15:41:35 +0100

    Revert "WaE: unused variables"
    This reverts commit d06a2dd024d69dec873f1a59adbf7d5b372c39a1.

--- vcl/source/fontsubset/cff.cxx
+++ vcl/source/fontsubset/cff.cxx
@@ -1751,9 +1751,7 @@ int CffSubsetterContext::getFDSelect( int nGlyphIndex) const
                 const U16 nRangeCount = (pReadPtr[0]<<8) + pReadPtr[1];
                 assert( nRangeCount > 0);
                 assert( nRangeCount <= mnCharStrCount);
-#ifndef NDEBUG
                 U16 nPrev = (pReadPtr[2]<<8) + pReadPtr[3];
-#endif
                 assert( nPrev == 0);
                 (void)nPrev;
                 pReadPtr += 4;
@@ -1765,9 +1763,7 @@ int CffSubsetterContext::getFDSelect( int nGlyphIndex) const
                     if( nGlyphIndex < nNext)
                         return nFDIdx;
                     pReadPtr += 3;
-#ifndef NDEBUG
                     nPrev = nNext;
-#endif
                 }
             } break;
         default:    // invalid FDselect format
