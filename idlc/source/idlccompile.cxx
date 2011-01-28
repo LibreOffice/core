1eb936d24568269908bde10289c789f671d66aad
Thomas Arnhold <thomas@arnhold.org>
2011-01-28 23:45:51 +0100

    Use errno to get the correct error message if mkstemp() fails.

--- idlc/source/idlccompile.cxx
+++ idlc/source/idlccompile.cxx
@@ -41,6 +41,7 @@
 #endif
 
 #ifdef  SAL_UNX
+#include <errno.h>
 #include <unistd.h>
 #if defined(MACOSX) || defined(FREEBSD) || defined(NETBSD) || \
     defined(AIX) || defined(OPENBSD)
@@ -168,7 +169,7 @@ OString makeTempName(const OString& prefix)
     int nDescriptor = mkstemp(tmpFilePattern);
     if( -1 == nDescriptor )
     {
-        fprintf( stderr,"idlc: couldn't create temporary file\n" );
+        fprintf(stderr, "idlc: mkstemp(\"%s\") failed: %s\n", tmpFilePattern, strerror(errno));
         exit( 1 );
     }
     // the file shall later be reopened by stdio functions
