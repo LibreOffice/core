894985ff00c217013cc360a77108fc68cd8647e7
Mathias Bauer <mba@openoffice.org>
2011-01-28 13:59:35 +0100

    CWS gnumake3: kill ENABLE_VBA

--- sc/source/ui/vba/makefile.mk
+++ sc/source/ui/vba/makefile.mk
@@ -37,10 +37,6 @@ VISIBILITY_HIDDEN=TRUE
 .INCLUDE :  settings.mk
 DLLPRE =
 
-.IF "$(ENABLE_VBA)"!="YES"
-dummy:
-        @echo "not building vba..."
-.ENDIF
 .IF "$(L10N_framework)"==""
 INCPRE=$(INCCOM)$/$(TARGET)
 
