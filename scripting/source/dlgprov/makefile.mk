1d1e3298ae77049be883769273c440dedeaeae7d
Mathias Bauer <mba@openoffice.org>
2011-01-28 13:54:12 +0100

    CWS gnumake3: more fixes for problems caused by crappy vbahelper headers

--- scripting/source/dlgprov/makefile.mk
+++ scripting/source/dlgprov/makefile.mk
@@ -58,6 +58,7 @@ SHL1STDLIBS= \
         $(CPPUHELPERLIB) \
         $(COMPHELPERLIB) \
         $(UCBHELPERLIB) \
+        $(VBAHELPERLIB)	\
         $(CPPULIB) \
         $(BASICLIB) \
         $(SALLIB)
