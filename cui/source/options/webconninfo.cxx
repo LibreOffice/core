97608a8a2eb82e7246ddbdd57a72fae42d27e700
Caolán McNamara <caolanm@redhat.com>
2011-01-31 11:14:43 +0000

    WaE: various new gcc 4.6.0 warnings

--- cui/source/options/webconninfo.cxx
+++ cui/source/options/webconninfo.cxx
@@ -209,7 +209,7 @@ void WebConnectionInfoDialog::FillPasswordList()
                     aUIEntry += ::rtl::OUString::valueOf( (sal_Unicode)'\t' );
                     aUIEntry += aURLEntries[nURLInd].UserList[nUserInd].UserName;
                     SvLBoxEntry* pEntry = m_aPasswordsLB.InsertEntry( aUIEntry );
-                    pEntry->SetUserData( (void*)(nCount++) );
+                    pEntry->SetUserData( (void*)(sal_IntPtr)(nCount++) );
                 }
             }
 
@@ -228,7 +228,7 @@ void WebConnectionInfoDialog::FillPasswordList()
                 aUIEntry += ::rtl::OUString::valueOf( (sal_Unicode)'\t' );
                 aUIEntry += ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "*" ) );
                 SvLBoxEntry* pEntry = m_aPasswordsLB.InsertEntry( aUIEntry );
-                pEntry->SetUserData( (void*)(nCount++) );
+                pEntry->SetUserData( (void*)(sal_IntPtr)(nCount++) );
             }
         }
     }
