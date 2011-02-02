c58b67eaf3598c249364982615501eb296bda8be
Thomas Arnhold <thomas@arnhold.org>
2011-02-02 12:30:22 +0100

    Fix CDEFS definition.
    Set it after including settings.mk because this file resets CDEFS.
    
    This one enables VBA_OOBUILD_HACK, so we need nCount in a higher scope.

--- sc/source/ui/vba/makefile.mk
+++ sc/source/ui/vba/makefile.mk
@@ -31,7 +31,7 @@ PRJNAME=sc
 TARGET=vbaobj
 ENABLE_EXCEPTIONS=TRUE
 VISIBILITY_HIDDEN=TRUE
-CDEFS+=-DVBA_OOBUILD_HACK
+
 # --- Settings -----------------------------------------------------
 
 .INCLUDE :  settings.mk
@@ -41,6 +41,9 @@ DLLPRE =
 dummy:
         @echo "not building vba..."
 .ENDIF
+
+CDEFS+=-DVBA_OOBUILD_HACK
+
 .IF "$(L10N_framework)"==""
 INCPRE=$(INCCOM)$/$(TARGET)
 
