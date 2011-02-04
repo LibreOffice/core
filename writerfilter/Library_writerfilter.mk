ee774d5daf59300029a08148feadc5dcd26fc6ab
Henning Brinkmann <hbrinkm@openoffice.org>
2011-02-04 13:47:28 +0100

    xmlfilter10: XPathlogger was missing [hg:333ad0d1384b]

--- writerfilter/Library_writerfilter.mk
+++ writerfilter/Library_writerfilter.mk
@@ -75,13 +75,13 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
 	writerfilter/source/dmapper/DomainMapperTableManager \
 	writerfilter/source/dmapper/DomainMapper_Impl \
 	writerfilter/source/dmapper/FFDataHandler \
-	writerfilter/source/dmapper/FormControlHelper \
 	writerfilter/source/dmapper/FontTable \
+	writerfilter/source/dmapper/FormControlHelper \
 	writerfilter/source/dmapper/GraphicHelpers \
 	writerfilter/source/dmapper/GraphicImport \
-	writerfilter/source/dmapper/NumberingManager  \
 	writerfilter/source/dmapper/MeasureHandler \
 	writerfilter/source/dmapper/ModelEventListener \
+	writerfilter/source/dmapper/NumberingManager  \
 	writerfilter/source/dmapper/OLEHandler \
 	writerfilter/source/dmapper/PageBordersHandler \
 	writerfilter/source/dmapper/PropertyIds \
@@ -94,49 +94,50 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
 	writerfilter/source/dmapper/TablePropertiesHandler \
 	writerfilter/source/dmapper/TblStylePrHandler \
 	writerfilter/source/dmapper/ThemeTable \
-	writerfilter/source/resourcemodel/resourcemodel \
-	writerfilter/source/resourcemodel/util \
-	writerfilter/source/resourcemodel/TagLogger \
-	writerfilter/source/resourcemodel/ResourceModelHelper \
-	writerfilter/source/resourcemodel/WW8Analyzer \
-	writerfilter/source/resourcemodel/Protocol \
-	writerfilter/source/doctok/WW8Annotation \
-	writerfilter/source/doctok/WW8OutputWithDepth \
-	writerfilter/source/doctok/WW8Text \
-	writerfilter/source/doctok/DffImpl \
 	writerfilter/source/doctok/Dff \
-	writerfilter/source/doctok/WW8Picture \
-	writerfilter/source/doctok/WW8Sttbf \
-	writerfilter/source/doctok/WW8Table \
-	writerfilter/source/doctok/WW8StyleSheet \
+	writerfilter/source/doctok/DffImpl \
+	writerfilter/source/doctok/WW8Annotation \
+	writerfilter/source/doctok/WW8BinTableImpl \
+	writerfilter/source/doctok/WW8Clx \
+	writerfilter/source/doctok/WW8CpAndFc \
+	writerfilter/source/doctok/WW8DocumentImpl \
+	writerfilter/source/doctok/WW8FKPImpl \
 	writerfilter/source/doctok/WW8FontTable \
 	writerfilter/source/doctok/WW8LFOTable \
 	writerfilter/source/doctok/WW8ListTable \
-	writerfilter/source/doctok/WW8CpAndFc \
-	writerfilter/source/doctok/WW8StructBase \
-	writerfilter/source/doctok/WW8Clx \
-	writerfilter/source/doctok/WW8FKPImpl \
-	writerfilter/source/doctok/WW8BinTableImpl \
-	writerfilter/source/doctok/WW8StreamImpl \
-	writerfilter/source/doctok/WW8PropertySetImpl \
-	writerfilter/source/doctok/WW8DocumentImpl \
+	writerfilter/source/doctok/WW8OutputWithDepth \
+	writerfilter/source/doctok/WW8Picture \
 	writerfilter/source/doctok/WW8PieceTableImpl \
+	writerfilter/source/doctok/WW8PropertySetImpl \
 	writerfilter/source/doctok/WW8ResourceModelImpl \
-	writerfilter/source/ooxml/OOXMLFactory \
-	writerfilter/source/ooxml/OOXMLBinaryObjectReference\
-	writerfilter/source/ooxml/OOXMLPropertySetImpl \
-	writerfilter/source/ooxml/OOXMLParserState \
-	writerfilter/source/ooxml/Handler \
-	writerfilter/source/ooxml/OOXMLDocumentImpl \
-	writerfilter/source/ooxml/OOXMLStreamImpl \
-	writerfilter/source/ooxml/OOXMLFastDocumentHandler \
-	writerfilter/source/ooxml/OOXMLFastContextHandler \
-	writerfilter/source/ooxml/OOXMLFastTokenHandler \
-	writerfilter/source/rtftok/RTFParseException \
-	writerfilter/source/filter/WriterFilter \
-	writerfilter/source/filter/WriterFilterDetection \
+	writerfilter/source/doctok/WW8StreamImpl \
+	writerfilter/source/doctok/WW8StructBase \
+	writerfilter/source/doctok/WW8Sttbf \
+	writerfilter/source/doctok/WW8StyleSheet \
+	writerfilter/source/doctok/WW8Table \
+	writerfilter/source/doctok/WW8Text \
 	writerfilter/source/filter/ImportFilter \
 	writerfilter/source/filter/RtfFilter \
+	writerfilter/source/filter/WriterFilter \
+	writerfilter/source/filter/WriterFilterDetection \
+	writerfilter/source/ooxml/Handler \
+	writerfilter/source/ooxml/OOXMLBinaryObjectReference\
+	writerfilter/source/ooxml/OOXMLDocumentImpl \
+	writerfilter/source/ooxml/OOXMLFactory \
+	writerfilter/source/ooxml/OOXMLFastContextHandler \
+	writerfilter/source/ooxml/OOXMLFastDocumentHandler \
+	writerfilter/source/ooxml/OOXMLFastTokenHandler \
+	writerfilter/source/ooxml/OOXMLParserState \
+	writerfilter/source/ooxml/OOXMLPropertySetImpl \
+	writerfilter/source/ooxml/OOXMLStreamImpl \
+	writerfilter/source/resourcemodel/Protocol \
+	writerfilter/source/resourcemodel/ResourceModelHelper \
+	writerfilter/source/resourcemodel/TagLogger \
+	writerfilter/source/resourcemodel/WW8Analyzer \
+	writerfilter/source/resourcemodel/XPathLogger \
+	writerfilter/source/resourcemodel/resourcemodel \
+	writerfilter/source/resourcemodel/util \
+	writerfilter/source/rtftok/RTFParseException \
 ))
 
 $(eval $(call gb_Library_add_generated_exception_objects,writerfilter,\
