aed400f208fbf9ba2ce943030f84e6c993f482fb
Mathias Bauer <mba@openoffice.org>
2011-02-05 23:57:43 +0100

    CWS gnumake3: reverted error from cws gnumake2

--- sw/source/filter/html/htmltabw.cxx
+++ sw/source/filter/html/htmltabw.cxx
@@ -338,7 +338,7 @@ void SwHTMLWrtTable::OutTableCell( SwHTMLWriter& rWrt,
 
 #ifndef PURE_HTML
     long nWidth = 0;
-    sal_uInt32 nPrcWidth = ULONG_MAX;
+    sal_uInt32 nPrcWidth = USHRT_MAX;
     if( bOutWidth )
     {
         if( bLayoutExport )
