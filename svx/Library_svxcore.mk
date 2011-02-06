b31ae53bd29953d09dbda1e37e3ec8aeec81807b
Mathias Bauer <mba@openoffice.org>
2011-02-06 22:00:30 +0100

    CWS gnumake3: wrong dll name for component file

--- svx/Library_svxcore.mk
+++ svx/Library_svxcore.mk
@@ -27,7 +27,7 @@
 
 $(eval $(call gb_Library_Library,svxcore))
 
-$(eval $(call gb_Library_set_componentfile,svx,svx/util/svxcore))
+$(eval $(call gb_Library_set_componentfile,svxcore,svx/util/svxcore))
 
 $(eval $(call gb_Library_add_sdi_headers,svx,svx/sdi/svxslots))
 
