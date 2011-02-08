e0bb8b98af592bcb4e7d5450ec2b2eabcddb12e8
Baptiste Daroussin <bapt@freebsd.org>
2011-02-08 21:14:08 +0000

    add freebsd in here

--- desktop/scripts/unopkg.sh
+++ desktop/scripts/unopkg.sh
@@ -45,7 +45,7 @@ cd "$sd_cwd"
 
 # this is a temporary hack until we can live with the default search paths
 case "`uname -s`" in
-NetBSD|OpenBSD)
+NetBSD|OpenBSD|FreeBSD)
     sd_prog1="$sd_prog/../basis-link/program"
     sd_prog2="$sd_prog/../basis-link/ure-link/lib"
     LD_LIBRARY_PATH=$sd_prog1:$sd_prog2${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
