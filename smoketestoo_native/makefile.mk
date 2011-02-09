f918d8477bb448c532c3e2daa792e575443f17aa
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-02-09 16:20:26 +0100

    Trying to remove the stlport mention from the code

--- smoketestoo_native/makefile.mk
+++ smoketestoo_native/makefile.mk
@@ -33,13 +33,6 @@ ENABLE_EXCEPTIONS = TRUE
 
 CFLAGSCXX += $(CPPUNIT_CFLAGS)
 
-#building with stlport, but cppunit was not built with stlport
-.IF "$(USE_SYSTEM_STL)"!="YES"
-.IF "$(SYSTEM_CPPUNIT)"=="YES"
-CFLAGSCXX+=-DADAPT_EXT_STL
-.ENDIF
-.ENDIF
-
 SLOFILES = $(SHL1OBJS)
 
 SHL1TARGET = smoketest
