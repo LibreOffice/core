99f36298cf267dd16124ef6952bd4e30bceff730
Nigel Hawkins <n.hawkins-KK0ffGbhmjU@public.gmane.org>
2011-02-09 14:48:21 +0000

    Remove SvBytes completely

--- svl/source/memtools/svarray.cxx
+++ svl/source/memtools/svarray.cxx
@@ -77,7 +77,6 @@ USHORT SvPtrarrPlain::GetPos( const VoidPtr aElement ) const
 
 
 SV_IMPL_VARARR( SvBools, BOOL )
-SV_IMPL_VARARR( SvBytes, BYTE )
 SV_IMPL_VARARR( SvULongs, ULONG )
 SV_IMPL_VARARR( SvUShorts, USHORT )
 SV_IMPL_VARARR( SvLongs, long)
