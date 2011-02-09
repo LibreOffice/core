053754d986bedee85fd2a5076b5f7561e945c5cd
Thorsten Behrens <tbehrens@novell.com>
2011-02-09 15:18:20 +0100

    Add basebmp to list of doxygen-processed modules

--- solenv/bin/mkdocs.sh
+++ solenv/bin/mkdocs.sh
@@ -27,7 +27,7 @@ BINDIR=`dirname $0`
 
 # get list of modules in build order - bah, blows RAM & disk, static list below
 #INPUT_PROJECTS=`build --all --show | sed -n -e '/Entering module/ s/Entering module // p'`
-INPUT_PROJECTS="o3tl basegfx comphelper svl vcl canvas cppcanvas oox svtools goodies drawinglayer xmloff slideshow sfx2 editeng svx cui chart2 dbaccess sd starmath sc sw"
+INPUT_PROJECTS="o3tl basegfx basebmp comphelper svl vcl canvas cppcanvas oox svtools goodies drawinglayer xmloff slideshow sfx2 editeng svx cui chart2 dbaccess sd starmath sc sw"
 
 # output directory for generated documentation
 BASE_OUTPUT="$1"
