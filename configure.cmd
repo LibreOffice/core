7387cd7edb872da41b0dd0c246f30a9019b83f2e
Thomas Arnhold <thomas@arnhold.org>
2011-02-12 11:58:01 +0100

    Easy hack: remove obsolete gjc aot compilation

--- configure.cmd
+++ configure.cmd
@@ -3,5 +3,5 @@ setlocal
 SET MY_ANT_HOME=e:/java150/apache-ant-1.7.0
 SET JAVA_HOME=e:\java150
 SET PATH=%JAVA_HOME%\bin;%PATH%;%MY_ANT_HOME%\bin;
-sh -c "configure --with-system-openssl --with-system-icu --with-system-libxml --with-system-libxslt --disable-werror --with-use-shell=bash --disable-mozilla --disable-epm --disable-odk --disable-qadevooo --with-x=no --disable-fontconfig --disable-gnome-vfs --disable-gtk --with-java=yes --disable-gcjaot --without-nas --without-fonts --with-ant-home=%MY_ANT_HOME%" | tee configure.log
+sh -c "configure --with-system-openssl --with-system-icu --with-system-libxml --with-system-libxslt --disable-werror --with-use-shell=bash --disable-mozilla --disable-epm --disable-odk --disable-qadevooo --with-x=no --disable-fontconfig --disable-gnome-vfs --disable-gtk --with-java=yes --without-nas --without-fonts --with-ant-home=%MY_ANT_HOME%" | tee configure.log
 endlocal
