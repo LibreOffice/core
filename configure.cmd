a3bdc038d016ee81b519e8b8d7718e77a021849b
Thomas Arnhold <thomas@arnhold.org>
2011-02-12 17:01:06 +0100

    Remove configure.cmd as it's a personal file

--- configure.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-SET MY_ANT_HOME=e:/java150/apache-ant-1.7.0
-SET JAVA_HOME=e:\java150
-SET PATH=%JAVA_HOME%\bin;%PATH%;%MY_ANT_HOME%\bin;
-sh -c "configure --with-system-openssl --with-system-icu --with-system-libxml --with-system-libxslt --disable-werror --with-use-shell=bash --disable-mozilla --disable-epm --disable-odk --disable-qadevooo --with-x=no --disable-fontconfig --disable-gnome-vfs --disable-gtk --with-java=yes --without-nas --without-fonts --with-ant-home=%MY_ANT_HOME%" | tee configure.log
-endlocal
