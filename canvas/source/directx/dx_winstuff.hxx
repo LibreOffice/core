6444836dbd573a224142550205e89f4e5b1665ff
Tor Lillqvist <tlillqvist@novell.com>
2011-02-13 19:56:26 +0200

    Fix x64 Windows build

--- canvas/source/directx/dx_winstuff.hxx
+++ canvas/source/directx/dx_winstuff.hxx
@@ -59,6 +59,7 @@
 #define BOOL win32BOOL
 #define INT32 win32INT32
 #define UINT32 win32UINT32
+#define ULONG win32ULONG
 #define GradientStyle_RECT win32GradientStyle_RECT
 #define Polygon win32Polygon
 #define PolyPolygon win32PolyPolygon
