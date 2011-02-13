f4f7fea877c47e8e95613f770ed0f87d1b2f8464
Thomas Arnhold <thomas@arnhold.org>
2011-02-13 20:35:49 +0100

    Correct variable initialization for 0b58d1d635b33148e6765eb35016b0fb43f214b3

--- sdext/source/minimizer/pppoptimizerdialog.cxx
+++ sdext/source/minimizer/pppoptimizerdialog.cxx
@@ -139,7 +139,8 @@ void SAL_CALL PPPOptimizerDialog::dispatch( const URL& rURL,
         {
             try
             {
-                sal_Int64 nFileSizeSource,nFileSizeDest = 0;
+                sal_Int64 nFileSizeSource = 0;
+                sal_Int64 nFileSizeDest = 0;
                 mpOptimizerDialog = new OptimizerDialog( mxMSF, mxFrame, this );
                 mpOptimizerDialog->execute();
 
