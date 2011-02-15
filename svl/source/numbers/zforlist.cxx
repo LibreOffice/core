ddbef3da193fa0c48e6441c7a106e112a467fdbb
Jan Holesovsky <kendy@noe.suse.cz>
2011-02-15 09:44:21 +0100

    Use contstructor instead of assignment, fixes unittest in sc.

--- svl/source/numbers/zforlist.cxx
+++ svl/source/numbers/zforlist.cxx
@@ -3461,7 +3461,7 @@ const NfCurrencyEntry* SvNumberFormatter::GetCurrencyEntry( BOOL & bFoundBank,
 void SvNumberFormatter::GetCompatibilityCurrency( String& rSymbol, String& rAbbrev ) const
 {
     ::com::sun::star::uno::Sequence< ::com::sun::star::i18n::Currency2 >
-        xCurrencies = xLocaleData->getAllCurrencies();
+        xCurrencies( xLocaleData->getAllCurrencies() );
     sal_Int32 nCurrencies = xCurrencies.getLength();
     sal_Int32 j;
     for ( j=0; j < nCurrencies; ++j )
