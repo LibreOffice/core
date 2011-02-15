eb07114a5c5d144e894b455c8b6f83e67463edf5
Muthu Subramanian K <sumuthu@novell.com>
2011-02-15 20:13:31 +0530

    Adding new operator for duplicate-conditional formatting.

--- offapi/com/sun/star/sheet/makefile.mk
+++ offapi/com/sun/star/sheet/makefile.mk
@@ -66,6 +66,7 @@ IDLFILES=\
     CellsEnumeration.idl\
     ComplexReference.idl\
     ConditionOperator.idl\
+    ConditionOperator2.idl\
     ConsolidationDescriptor.idl\
     DDEItemInfo.idl\
     DDELink.idl\
@@ -314,6 +315,7 @@ IDLFILES=\
     XSheetCellRangeContainer.idl\
     XSheetCellRanges.idl\
     XSheetCondition.idl\
+    XSheetCondition2.idl\
     XSheetConditionalEntries.idl\
     XSheetConditionalEntry.idl\
     XSheetFilterDescriptor.idl\
