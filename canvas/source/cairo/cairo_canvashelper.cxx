74ccf00ef42e67e54b96b270a68c17625c1a040f
Radek Doulik <rodo@novell.com>
2011-02-17 18:45:18 +0100

    Revert "fix cairo canvas regression - do not access polygon points outside range"
     - it is not needed anymore
    
    This reverts commit a898a8cb077c05b1085da8254e66f61aa265b7dc.

--- canvas/source/cairo/cairo_canvashelper.cxx
+++ canvas/source/cairo/cairo_canvashelper.cxx
@@ -1074,7 +1074,7 @@ namespace cairocanvas
                     {
                         const sal_uInt32 nNextIndex((b + 1) % nPointCount);
                         aEdge.setB2DPoint(1, aCandidate.getB2DPoint(nNextIndex));
-                        aEdge.setNextControlPoint(0, aCandidate.getNextControlPoint(b % nPointCount));
+                        aEdge.setNextControlPoint(0, aCandidate.getNextControlPoint(b));
                         aEdge.setPrevControlPoint(1, aCandidate.getPrevControlPoint(nNextIndex));
 
                         doPolyPolygonImplementation( basegfx::B2DPolyPolygon(aEdge),
