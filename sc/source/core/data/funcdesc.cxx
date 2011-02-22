0e2be9d8ee1a4a0faa65becafc8e27b9869304aa
Tor Lillqvist <tlillqvist@novell.com>
2011-02-22 02:02:37 +0200

    Include precompiled_sc.hxx

--- sc/source/core/data/funcdesc.cxx
+++ sc/source/core/data/funcdesc.cxx
@@ -26,6 +26,9 @@
  *
  ************************************************************************/
 
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_sc.hxx"
+
 #include "funcdesc.hxx"
 
 #include "addincol.hxx"
