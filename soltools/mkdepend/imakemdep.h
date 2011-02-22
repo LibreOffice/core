d2a6b1e9a07831ba54a3c6f3604b451b2c5eb5dc
Thomas Arnhold <thomas@arnhold.org>
2011-02-22 22:05:17 +0100

    Remove HPUX from imakedep.h

--- soltools/mkdepend/imakemdep.h
+++ soltools/mkdepend/imakemdep.h
@@ -43,14 +43,6 @@ in this Software without prior written authorization from the X Consortium.
  *     These will be passed to the compile along with the contents of the
  *     make variable BOOTSTRAPCFLAGS.
  */
-#ifdef hpux
-#ifdef hp9000s800
-#define imake_ccflags "-DSYSV"
-#else
-#define imake_ccflags "-Wc,-Nd4000,-Ns3000 -DSYSV"
-#endif
-#endif
-
 #if defined(macII) || defined(_AUX_SOURCE)
 #define imake_ccflags "-DmacII -DSYSV"
 #endif
@@ -208,9 +200,6 @@ in this Software without prior written authorization from the X Consortium.
  *     If use cc -E but want a different compiler, define DEFAULT_CC.
  *     If the cpp you need is not in /lib/cpp, define DEFAULT_CPP.
  */
-#ifdef hpux
-#define USE_CC_E
-#endif
 #ifdef WIN32
 #define USE_CC_E
 #define DEFAULT_CC "cl"
@@ -548,18 +537,6 @@ struct pair predefs[] = {
 #endif
 #ifdef __sparc__
     {"__sparc__", "1", NULL},
-#endif
-#ifdef hpux
-    {"hpux", "1", NULL},
-#endif
-#ifdef __hpux
-    {"__hpux", "1", NULL},
-#endif
-#ifdef __hp9000s800
-    {"__hp9000s800", "1", NULL},
-#endif
-#ifdef __hp9000s700
-    {"__hp9000s700", "1", NULL},
 #endif
 #ifdef vax
     {"vax", "1", NULL},
