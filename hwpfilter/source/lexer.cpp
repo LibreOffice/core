f9c94966c92800ede3d7c7b75722167f1c9c1f7f
Thomas Arnhold <thomas@arnhold.org>
2011-02-24 21:57:44 +0100

    Remove TURBOC support

--- hwpfilter/source/lexer.cpp
+++ hwpfilter/source/lexer.cpp
@@ -60,15 +60,6 @@
 #endif  /* __STDC__ */
 #endif  /* ! __cplusplus */
 
-#ifdef __TURBOC__
- #pragma warn -rch
- #pragma warn -use
-#include <io.h>
-#include <stdlib.h>
-#define YY_USE_CONST
-#define YY_USE_PROTOS
-#endif
-
 #ifdef YY_USE_CONST
 #define yyconst const
 #else
