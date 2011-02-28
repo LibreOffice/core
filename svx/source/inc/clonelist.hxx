20b77e66d03050108c276f9bb191832125447fa8
Rafael Dominguez <venccsralph@gmail.com>
2011-02-27 21:45:37 -0800

    [PATCH] Remove deprecated container list for std::vector.

--- svx/source/inc/clonelist.hxx
+++ svx/source/inc/clonelist.hxx
@@ -28,8 +28,9 @@
 #ifndef _CLONELIST_HXX_
 #define _CLONELIST_HXX_
 
+#include <vector>
+
 #include <sal/types.h>
-#include <tools/list.hxx>
 
 // predeclarations
 class SdrObject;
@@ -39,12 +40,10 @@ class SdrObject;
 // re-creating the connections for contained connectors
 class CloneList
 {
-    List                        maOriginalList;
-    List                        maCloneList;
+    std::vector<const SdrObject*> maOriginalList;
+    std::vector<SdrObject*> maCloneList;
 
 public:
-    CloneList();
-    ~CloneList();
 
     void AddPair(const SdrObject* pOriginal, SdrObject* pClone);
     sal_uInt32 Count() const;
