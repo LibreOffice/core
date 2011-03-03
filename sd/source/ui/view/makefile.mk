6914ac098f2fa13e5e392794a5a26f95217e036f
Rafael Dominguez <venccsralph@gmail.com>
2011-03-02 19:48:50 -0800

    Remove dead code inside sd.

--- sd/source/ui/view/makefile.mk
+++ sd/source/ui/view/makefile.mk
@@ -1,7 +1,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -85,7 +85,6 @@ SLOFILES =	\
         $(SLO)$/presvish.obj \
         $(SLO)$/outlview.obj \
         $(SLO)$/tabcontr.obj \
-        $(SLO)$/bmcache.obj	\
         $(SLO)$/drbezob.obj	\
         $(SLO)$/drtxtob.obj	\
         $(SLO)$/drtxtob1.obj	\
