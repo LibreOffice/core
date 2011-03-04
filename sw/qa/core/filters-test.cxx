f01b5e4351717258b58f359fa3af798dcb8efda7
Caolán McNamara <caolanm@redhat.com>
2011-03-04 15:13:30 +0000

    Regression test for CVE-2009-0200

--- sw/qa/core/filters-test.cxx
+++ sw/qa/core/filters-test.cxx
@@ -136,6 +136,11 @@ void FiltersTest::testCVEs()
         m_aPWDURL + rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/CVE/CVE-2007-0245-1.rtf")));
     CPPUNIT_ASSERT_MESSAGE("CVE-2007-0245 regression", bResult == true);
 
+    bResult = testLoad(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("MS Word 97")),
+        rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("CWW8")),
+        m_aPWDURL + rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/CVE/CVE-2009-0200-1.doc")));
+    CPPUNIT_ASSERT_MESSAGE("CVE-2009-0200 regression", bResult == true);
+
     bResult = testLoad(rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Rich Text Format")),
         rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("RTF")),
         m_aPWDURL + rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("/CVE/CVE-2010-3451-1.rtf")));
