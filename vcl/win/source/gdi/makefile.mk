9a318cf8050143c4b9975e861c41b2ee37de1d19
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-03-16 10:25:08 +0100

    MAKEFILE.MK -> makefile.mk

--- vcl/win/source/gdi/makefile.mk
+++ vcl/win/source/gdi/makefile.mk
@@ -1,7 +1,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -56,7 +56,7 @@ SLOFILES=   $(SLO)$/salgdi.obj   \
             $(SLO)$/winlayout.obj	\
             $(SLO)$/wntgdi.obj      \
             $(SLO)$/salnativewidgets-luna.obj
-            
+
 
 EXCEPTIONSFILES=	$(SLO)$/salprn.obj
 
