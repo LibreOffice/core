d8f1ce1e9cb83ea74e8cf853b24b1c0c8d29921f
Caolán McNamara <caolanm@redhat.com>
2011-03-22 08:52:48 +0000

    WaE: this isn't helpful

--- sfx2/source/dialog/passwd.cxx
+++ sfx2/source/dialog/passwd.cxx
@@ -249,9 +249,7 @@ short SfxPasswordDialog::Execute()
         maPasswordFT.Show();
     if( (mnExtras & SHOWEXTRAS_USER ) )
     {
-#ifndef _MSC_VER
-#warning Inevitably this layout logic will be wrong post merge until we can see the dialog to test it.
-#endif
+        //TODO: Inevitably this layout logic will be wrong post merge until we can see the dialog to test it.
         Size a3Size = LogicToPixel( Size( 3, 3 ), MAP_APPFONT );
         Size a6Size = LogicToPixel( Size( 6, 6 ), MAP_APPFONT );
         long nMinHeight = maHelpBtn.GetPosPixel().Y() +
