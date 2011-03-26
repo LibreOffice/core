241c2ec0075037a438f29da268fc72de8eae7f2b
Norbert Thiebaud <nthiebaud@gmail.com>
2011-03-26 01:03:47 -0500

    fix filter loading: export ScFilterCreate

--- sc/source/filter/ftools/ftools.cxx
+++ sc/source/filter/ftools/ftools.cxx
@@ -398,7 +398,7 @@ ScFormatFilterPluginImpl::ScFormatFilterPluginImpl()
 {
 }
 
-ScFormatFilterPlugin * SAL_CALL ScFilterCreate(void)
+SAL_DLLPUBLIC_EXPORT ScFormatFilterPlugin * SAL_CALL ScFilterCreate(void)
 {
     return new ScFormatFilterPluginImpl();
 }
