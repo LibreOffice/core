bf540443d49a8ceb0071ab7a46f3d0c20866d867
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-03-28 10:27:07 +0200

    One more return of prewin.h/postwin.h hack :(

--- svtools/source/misc/transfer.cxx
+++ svtools/source/misc/transfer.cxx
@@ -29,14 +29,9 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_svtools.hxx"
 #ifdef WNT
-#if defined _MSC_VER
-#pragma warning(push, 1)
-#pragma warning(disable: 4917)
-#endif
+#include <prewin.h>
+#include <postwin.h>
 #include <shlobj.h>
-#if defined _MSC_VER
-#pragma warning(pop)
-#endif
 #endif
 #include <osl/mutex.hxx>
 #include <rtl/memory.h>
