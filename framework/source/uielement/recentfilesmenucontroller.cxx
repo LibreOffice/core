5373c145a2003d8b45eb9228cc6bbfd0de36fed8
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-03-28 12:00:36 +0200

    OOops again :(

--- framework/source/uielement/recentfilesmenucontroller.cxx
+++ framework/source/uielement/recentfilesmenucontroller.cxx
@@ -1,4 +1,4 @@
-2/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
