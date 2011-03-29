c89df72dfcc494a0ed2e9cc956b24a4f84d0d176
Norbert Thiebaud <nthiebaud@gmail.com>
2011-03-29 13:42:24 -0500

    use --xxx instead -f -xxx for command line options

--- sd/qa/unit/makefile.mk
+++ sd/qa/unit/makefile.mk
@@ -107,7 +107,7 @@ test .PHONY: $(SHL1TARGETN) $(MISC)/$(TARGET)/services.rdb
     @echo ----------------------------------------------------------
     @echo - start unit test \#1 on library $(SHL1TARGETN)
     @echo ----------------------------------------------------------
-    $(CPPUNITTESTER) $(SHL1TARGETN) -headless -invisible \
+    $(CPPUNITTESTER) $(SHL1TARGETN) --headless --invisible \
         '-env:UNO_TYPES=$(my_file)$(SOLARBINDIR)/udkapi.rdb $(my_file)$(SOLARBINDIR)$/types.rdb' \
         '-env:UNO_SERVICES=$(my_file)$(SOLARXMLDIR)/ure/services.rdb $(my_file)$(PWD)/$(MISC)/$(TARGET)/services.rdb'\
         -env:URE_INTERNAL_LIB_DIR="$(my_file)$(SOLARSHAREDBIN)" \
