98b98a6ffaab058e4f4a353843bebe27f8c51ff6
David Tardon <dtardon@redhat.com>
2011-03-29 18:36:07 +0200

    WaE: unused variable

--- svl/source/numbers/zforlist.cxx
+++ svl/source/numbers/zforlist.cxx
@@ -3670,13 +3670,6 @@ void SvNumberFormatter::ImpInitCurrencyTable()
     {
         LanguageType eLang = MsLangId::convertLocaleToLanguage(
                 pLocales[nLocale]);
-#if OSL_DEBUG_LEVEL > 1
-        LanguageType eReal = MsLangId::getRealLanguage( eLang );
-        if ( eReal != eLang ) {
-            sal_Bool bBreak;
-            bBreak = sal_True;
-        }
-#endif
         pLocaleData->setLocale( pLocales[nLocale] );
         Sequence< Currency2 > aCurrSeq = pLocaleData->getAllCurrencies();
         sal_Int32 nCurrencyCount = aCurrSeq.getLength();
