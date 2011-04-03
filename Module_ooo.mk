0d963e8dff57dff1124b62f1d8c422d6449fcdbd
David Tardon <dtardon@redhat.com>
2011-04-03 19:23:07 +0200

    convert leading tab to spaces

--- Module_ooo.mk
+++ Module_ooo.mk
@@ -14,12 +14,12 @@
 #
 # OpenOffice.org is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU Lesser General Public License version 3 for more details
 # (a copy is included in the LICENSE file that accompanied this code).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# version 3 along with OpenOffice.org.	If not, see
+# version 3 along with OpenOffice.org.    If not, see
 # <http://www.openoffice.org/license.html>
 # for a copy of the LGPLv3 License.
 #
@@ -28,37 +28,37 @@
 $(eval $(call gb_Module_Module,ooo))
 
 $(eval $(call gb_Module_add_moduledirs,ooo,\
-	comphelper \
-	drawinglayer \
-	editeng \
-	fileaccess \
-	forms \
-	framework \
-	padmin \
-	sfx2 \
-	sot \
-	svl \
-	svtools \
-	svx \
-	sw \
-	toolkit \
-	tools \
-	unotools \
-	unoxml \
-	xmloff \
-	vbahelper \
+    comphelper \
+    drawinglayer \
+    editeng \
+    fileaccess \
+    forms \
+    framework \
+    padmin \
+    sfx2 \
+    sot \
+    svl \
+    svtools \
+    svx \
+    sw \
+    toolkit \
+    tools \
+    unotools \
+    unoxml \
+    xmloff \
+    vbahelper \
 ))
 
 # these have only migrated subsequentcheck for now
 $(eval $(call gb_Module_add_moduledirs,ooo,\
-	chart2 \
-	configmgr \
-	dbaccess \
-	linguistic \
-	qadevOOo \
-	sal \
-	ucb \
-	unotools \
+    chart2 \
+    configmgr \
+    dbaccess \
+    linguistic \
+    qadevOOo \
+    sal \
+    ucb \
+    unotools \
 ))
 
 # vim: set noet ts=4 sw=4:
