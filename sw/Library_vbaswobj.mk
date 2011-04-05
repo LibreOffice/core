9dae7504997bf66a6e1d483b42c586998f67a57d
Robert Nagy <robert@openbsd.org>
2011-04-05 12:00:37 +0200

    set ldflags on *BSD too just like linux

--- sw/Library_vbaswobj.mk
+++ sw/Library_vbaswobj.mk
@@ -136,7 +136,7 @@ $(eval $(call gb_Library_add_exception_objects,vbaswobj,\
     sw/source/ui/vba/wordvbahelper \
 ))
 
-ifeq ($(OS),LINUX)
+ifneq (,$(filter LINUX DRAGONFLY OPENBSD FREEBSD NETBSD, $(OS)))
 $(eval $(call gb_Library_set_ldflags,vbaswobj,\
     $$(LDFLAGS) \
     -Wl$(COMMA)-O1 \
