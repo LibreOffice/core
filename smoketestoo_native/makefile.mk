3721dd9318278f455f31ab2c10b6ec00eac868f9
Caolán McNamara <caolanm@redhat.com>
2011-04-05 11:47:33 +0100

    remove ADAPT_EXT_STL

--- smoketestoo_native/makefile.mk
+++ smoketestoo_native/makefile.mk
@@ -35,13 +35,6 @@ ABORT_ON_ASSERTION = TRUE
 
 CFLAGSCXX += $(CPPUNIT_CFLAGS)
 
-#building with stlport, but cppunit was not built with stlport
-.IF "$(USE_SYSTEM_STL)"!="YES"
-.IF "$(SYSTEM_CPPUNIT)"=="YES"
-CFLAGSCXX+=-DADAPT_EXT_STL
-.ENDIF
-.ENDIF
-
 SLOFILES = $(SHL1OBJS)
 
 SHL1TARGET = smoketest
