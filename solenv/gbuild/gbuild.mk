4620f155b5ec9805e6387b5fbb36f62500c3ce72
David Tardon <dtardon@redhat.com>
2011-04-05 14:28:02 +0200

    do not define NDEBUG on debug level 1

--- solenv/gbuild/gbuild.mk
+++ solenv/gbuild/gbuild.mk
@@ -208,15 +208,13 @@ ifneq ($(strip $(SOLAR_JAVA)),)
 gb_GLOBALDEFS += -DSOLAR_JAVA
 endif
 
-ifeq ($(gb_DEBUGLEVEL),2)
-gb_GLOBALDEFS += \
-	-DDEBUG \
-
-else
+ifeq ($(gb_DEBUGLEVEL),0)
 gb_GLOBALDEFS += \
 	-DOPTIMIZE \
 	-DNDEBUG \
-
+else ifneq ($(gb_DEBUGLEVEL),1) # 2 or more
+gb_GLOBALDEFS += \
+	-DDEBUG \
 endif
 
 ifneq ($(strip $(ENABLE_GTK)),)
