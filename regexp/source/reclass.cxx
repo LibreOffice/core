50bf044cfa87fde9bce2b6870e222f2e42a0519f
Caolán McNamara <caolanm@redhat.com>
2011-04-13 10:51:35 +0100

    catch by const reference

--- regexp/source/reclass.cxx
+++ regexp/source/reclass.cxx
@@ -1689,7 +1689,7 @@ Regexpr::regex_compile()
             sal_Unicode tmp = translit->transliterateChar2Char(c);
             BUF_PUSH(tmp);
             (*pending_exact)++;
-        } catch (::com::sun::star::i18n::MultipleCharsOutputException e) {
+        } catch (const ::com::sun::star::i18n::MultipleCharsOutputException& e) {
             ::rtl::OUString o2( translit->transliterateChar2String( c));
             sal_Int32 len2 = o2.getLength();
             const sal_Unicode * k2 = o2.getStr();
@@ -2934,7 +2934,7 @@ Regexpr::set_list_bit(sal_Unicode c, sal_Unicode *b)
     try {
         sal_Unicode tmp = translit->transliterateChar2Char(c);
         b[tmp / BYTEWIDTH] |= 1 << (tmp % BYTEWIDTH);
-    } catch (::com::sun::star::i18n::MultipleCharsOutputException e) {
+    } catch (const ::com::sun::star::i18n::MultipleCharsOutputException& e) {
         ::rtl::OUString o2( translit->transliterateChar2String( c));
         sal_Int32 len2 = o2.getLength();
         const sal_Unicode * k2 = o2.getStr();
