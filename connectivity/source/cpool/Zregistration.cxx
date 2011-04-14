dbef0ebc20d2a3258aedb98c112ba9ba6ee422e7
Ocke Janssen [oj] <Ocke.Janssen@oracle.com>
2011-04-14 10:11:15 +0200

    gnumake4: added dbaccess and reportdesign [hg:a75d5667430f]

--- connectivity/source/cpool/Zregistration.cxx
+++ connectivity/source/cpool/Zregistration.cxx
@@ -44,13 +44,13 @@ extern "C"
 {
 
 //---------------------------------------------------------------------------------------
-    void SAL_CALL component_getImplementationEnvironment(const sal_Char** _ppEnvTypeName, uno_Environment** /*_ppEnv*/)
+    SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(const sal_Char** _ppEnvTypeName, uno_Environment** /*_ppEnv*/)
 {
     *_ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
 
 //---------------------------------------------------------------------------------------
-void* SAL_CALL component_getFactory(const sal_Char* _pImplName, ::com::sun::star::lang::XMultiServiceFactory* _pServiceManager, void* /*_pRegistryKey*/)
+SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(const sal_Char* _pImplName, ::com::sun::star::lang::XMultiServiceFactory* _pServiceManager, void* /*_pRegistryKey*/)
 {
     void* pRet = NULL;
 
