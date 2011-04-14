5eb46770effb2f5e47ed9b1b7a06ffeb64be3f50
Tor Lillqvist <tlillqvist@novell.com>
2011-04-14 14:11:38 +0300

    Add our normal Emacs and vim mode lines

--- svtools/source/filter.vcl/filter/exportdialog.cxx
+++ svtools/source/filter.vcl/filter/exportdialog.cxx
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -1511,3 +1512,4 @@ IMPL_LINK( ExportDialog, OK, void *, EMPTYARG )
     return 0;
 }
 
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
