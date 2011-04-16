a3228bb5c63654dee14f058ad93f453999382707
Francois Tigeot <ftigeot@wolfpond.org>
2011-04-16 09:21:45 +0200

    Remove OS/2-specific subdirectories

--- desktop/os2/source/applauncher/scalc.cxx
+++
@@ -1,6 +0,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-#include "launcher.hxx"
-
-CHAR APPLICATION_SWITCH[] = ( "-calc" );
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
