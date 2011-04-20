959067af84bf74d324f6751f766a96b6b46649d0
Julien Nabet <serval2412@yahoo.fr>
2011-04-20 14:15:47 +0200

    Just reformat a line

--- dbaccess/source/ext/adabas/Aservices.cxx
+++ dbaccess/source/ext/adabas/Aservices.cxx
@@ -58,8 +58,7 @@ extern "C" void SAL_CALL createRegistryInfo_adabasui()
 
 //---------------------------------------------------------------------------------------
 
-extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL
-component_getImplementationEnvironment(
+extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
                 const sal_Char  **ppEnvTypeName,
                 uno_Environment **
             )
