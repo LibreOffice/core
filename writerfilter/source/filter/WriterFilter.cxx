727c2a2eba9a663fcd596cc07933d63899696a4f
Noel Power <noel.power@novell.com>
2011-04-20 16:36:39 +0100

    make component methods public component fails to load otherwise
    decorate component_getImplementationEnvironment &
    component_getFactor with SAL_DLLPUBLIC_EXPORT, otherwise wrong functions get called

--- writerfilter/source/filter/WriterFilter.cxx
+++ writerfilter/source/filter/WriterFilter.cxx
@@ -60,12 +60,12 @@ static struct ::cppu::ImplementationEntry s_component_entries [] =
     { 0, 0, 0, 0, 0, 0 } // terminate with NULL
 };
 
-void SAL_CALL component_getImplementationEnvironment(const sal_Char ** ppEnvTypeName, uno_Environment ** /*ppEnv*/ )
+SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(const sal_Char ** ppEnvTypeName, uno_Environment ** /*ppEnv*/ )
 {
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
 }
 
-void * SAL_CALL component_getFactory(sal_Char const * implName, ::com::sun::star::lang::XMultiServiceFactory * xMgr, ::com::sun::star::registry::XRegistryKey * xRegistry )
+SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(sal_Char const * implName, ::com::sun::star::lang::XMultiServiceFactory * xMgr, ::com::sun::star::registry::XRegistryKey * xRegistry )
 {
     return ::cppu::component_getFactoryHelper(implName, xMgr, xRegistry, s_component_entries );
 }
