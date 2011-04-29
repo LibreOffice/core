ee0902b50d5043e4b377cbf4556bb8ed39efd6b1
Norbert Thiebaud <nthiebaud@gmail.com>
2011-04-28 21:53:29 -0500

    avoid object double include in starmath

--- starmath/Library_sm.mk
+++ starmath/Library_sm.mk
@@ -74,7 +74,6 @@ $(eval $(call gb_Library_add_exception_objects,sm,\
         starmath/source/dialog \
         starmath/source/document \
         starmath/source/edit \
-        starmath/source/eqnolefilehdr \
         starmath/source/format \
         starmath/source/mathmlexport \
         starmath/source/mathmlimport \
