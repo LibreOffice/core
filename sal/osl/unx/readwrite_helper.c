48227a2047e0ea1b501becf02ebfe30287bab1a9
Julien Chaffraix <julien.chaffraix@gmail.com>
2011-04-30 16:37:49 -0700

    Properly advance the data / buffer pointer.
    Spotted by Michael Meeks.

--- sal/osl/unx/readwrite_helper.c
+++ sal/osl/unx/readwrite_helper.c
@@ -35,10 +35,12 @@
 sal_Bool safeWrite(int fd, void* data, sal_uInt32 dataSize)
 {
     sal_Int32 nToWrite = dataSize;
+    unsigned char* dataToWrite = data;
+
     // Check for overflow as we convert a signed to an unsigned.
     OSL_ASSERT(dataSize == (sal_uInt32)nToWrite);
     while ( nToWrite ) {
-        sal_Int32 nWritten = write(fd, data, nToWrite);
+        sal_Int32 nWritten = write(fd, dataToWrite, nToWrite);
         if ( nWritten < 0 ) {
             if ( errno == EINTR )
                 continue;
@@ -49,6 +51,7 @@ sal_Bool safeWrite(int fd, void* data, sal_uInt32 dataSize)
 
         OSL_ASSERT(nWritten > 0);
         nToWrite -= nWritten;
+        dataToWrite += nWritten;
     }
 
     return sal_True;
@@ -57,10 +60,12 @@ sal_Bool safeWrite(int fd, void* data, sal_uInt32 dataSize)
 sal_Bool safeRead( int fd, void* buffer, sal_uInt32 count )
 {
     sal_Int32 nToRead = count;
+    unsigned char* bufferForReading = buffer;
+
     // Check for overflow as we convert a signed to an unsigned.
     OSL_ASSERT(count == (sal_uInt32)nToRead);
     while ( nToRead ) {
-        sal_Int32 nRead = read(fd, buffer, nToRead);
+        sal_Int32 nRead = read(fd, bufferForReading, nToRead);
         if ( nRead < 0 ) {
             // We were interrupted before reading, retry.
             if (errno == EINTR)
@@ -75,6 +80,7 @@ sal_Bool safeRead( int fd, void* buffer, sal_uInt32 count )
             return sal_False;
 
         nToRead -= nRead;
+        bufferForReading += nRead;
     }
 
     return sal_True;
