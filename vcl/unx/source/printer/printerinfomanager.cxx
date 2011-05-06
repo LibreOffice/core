c313d45a2e75e994b6a35d4d18b308c498967299
Thorsten Behrens <tbehrens@novell.com>
2011-05-06 18:13:35 +0200

    Fix dbglevel=2 build

--- vcl/unx/source/printer/printerinfomanager.cxx
+++ vcl/unx/source/printer/printerinfomanager.cxx
@@ -268,7 +268,7 @@ void PrinterInfoManager::initialize()
         if( aConfig.HasGroup( GLOBAL_DEFAULTS_GROUP ) )
         {
             #if OSL_DEBUG_LEVEL > 1
-            fprintf( stderr, "found global defaults in %s\n", OUStringToOString( aFile.PathToFileName(), RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
+            fprintf( stderr, "found global defaults in %s\n", rtl::OUStringToOString( aFile.PathToFileName(), RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
             #endif
             aConfig.SetGroup( GLOBAL_DEFAULTS_GROUP );
 
@@ -817,8 +817,8 @@ bool PrinterInfoManager::writePrinterConfig()
             subst != it->second.m_aInfo.m_aFontSubstitutes.end(); ++subst )
             {
                 ByteString aKey( "SubstFont_" );
-                aKey.Append( OUStringToOString( subst->first, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
-                pConfig->WriteKey( aKey, OUStringToOString( subst->second, RTL_TEXTENCODING_ISO_8859_1 ) );
+                aKey.Append( rtl::OUStringToOString( subst->first, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
+                pConfig->WriteKey( aKey, rtl::OUStringToOString( subst->second, RTL_TEXTENCODING_ISO_8859_1 ) );
             }
         }
     }
@@ -873,7 +873,7 @@ bool PrinterInfoManager::addPrinter( const OUString& rPrinterName, const OUStrin
         bSuccess = true;
         #if OSL_DEBUG_LEVEL > 1
         fprintf( stderr, "new printer %s, level = %d, pdfdevice = %d, colordevice = %d, depth = %d\n",
-        OUStringToOString( rPrinterName, osl_getThreadTextEncoding() ).getStr(),
+                 rtl::OUStringToOString( rPrinterName, osl_getThreadTextEncoding() ).getStr(),
         m_aPrinters[rPrinterName].m_aInfo.m_nPSLevel,
         m_aPrinters[rPrinterName].m_aInfo.m_nPDFDevice,
         m_aPrinters[rPrinterName].m_aInfo.m_nColorDevice,
@@ -1046,12 +1046,12 @@ void PrinterInfoManager::fillFontSubstitutions( PrinterInfo& rInfo ) const
                     fprintf( stderr,
                     "substitute %s %s %d %d\n"
                     " ->        %s %s %d %d\n",
-                    OUStringToOString( it->m_aFamilyName, RTL_TEXTENCODING_ISO_8859_1 ).getStr(),
+                             rtl::OUStringToOString( it->m_aFamilyName, RTL_TEXTENCODING_ISO_8859_1 ).getStr(),
                     it->m_eItalic == italic::Upright ? "r" : it->m_eItalic == italic::Oblique ? "o" : it->m_eItalic == italic::Italic ? "i" : "u",
                     it->m_eWeight,
                     it->m_eWidth,
 
-                    OUStringToOString( aInfo.m_aFamilyName, RTL_TEXTENCODING_ISO_8859_1 ).getStr(),
+                             rtl::OUStringToOString( aInfo.m_aFamilyName, RTL_TEXTENCODING_ISO_8859_1 ).getStr(),
                     aInfo.m_eItalic == italic::Upright ? "r" : aInfo.m_eItalic == italic::Oblique ? "o" : aInfo.m_eItalic == italic::Italic ? "i" : "u",
                     aInfo.m_eWeight,
                     aInfo.m_eWidth
@@ -1117,7 +1117,7 @@ FILE* PrinterInfoManager::startSpool( const OUString& rPrintername, bool bQuickC
     const PrinterInfo&   rPrinterInfo   = getPrinterInfo (rPrintername);
     const rtl::OUString& rCommand       = (bQuickCommand && rPrinterInfo.m_aQuickCommand.getLength() ) ?
                                           rPrinterInfo.m_aQuickCommand : rPrinterInfo.m_aCommand;
-    rtl::OString aShellCommand  = OUStringToOString (rCommand, RTL_TEXTENCODING_ISO_8859_1);
+    rtl::OString aShellCommand  = rtl::OUStringToOString (rCommand, RTL_TEXTENCODING_ISO_8859_1);
     aShellCommand += rtl::OString( " 2>/dev/null" );
 
     return popen (aShellCommand.getStr(), "w");
@@ -1157,7 +1157,7 @@ void PrinterInfoManager::setDefaultPaper( PPDContext& rContext ) const
     {
         #if OSL_DEBUG_LEVEL > 1
         fprintf( stderr, "not setting default paper, already set %s\n",
-        OUStringToOString( rContext.getValue( pPageSizeKey )->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
+                 rtl::OUStringToOString( rContext.getValue( pPageSizeKey )->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
         #endif
         return;
     }
@@ -1174,12 +1174,12 @@ void PrinterInfoManager::setDefaultPaper( PPDContext& rContext ) const
     if( pPaperVal )
     {
         #if OSL_DEBUG_LEVEL > 1
-        fprintf( stderr, "setting default paper %s\n", OUStringToOString( pPaperVal->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
+        fprintf( stderr, "setting default paper %s\n", rtl::OUStringToOString( pPaperVal->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
         #endif
         rContext.setValue( pPageSizeKey, pPaperVal );
         #if OSL_DEBUG_LEVEL > 1
         pPaperVal = rContext.getValue( pPageSizeKey );
-        fprintf( stderr, "-> got paper %s\n", OUStringToOString( pPaperVal->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
+        fprintf( stderr, "-> got paper %s\n", rtl::OUStringToOString( pPaperVal->m_aOption, RTL_TEXTENCODING_ISO_8859_1 ).getStr() );
         #endif
     }
 }
