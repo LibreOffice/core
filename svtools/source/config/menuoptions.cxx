81d10064500f40ce730168da3b290f98f0116d1b
Caolán McNamara <caolanm@redhat.com>
2011-05-16 15:33:16 +0100

    reduce spew somewhat, one warning is enough

--- svtools/source/config/menuoptions.cxx
+++ svtools/source/config/menuoptions.cxx
@@ -258,6 +258,10 @@ SvtMenuOptions_Impl::SvtMenuOptions_Impl()
         // Safe impossible cases.
         // Check any for valid value.
         DBG_ASSERT( !(seqValues[nProperty].hasValue()==sal_False), "SvtMenuOptions_Impl::SvtMenuOptions_Impl()\nInvalid property value for property detected!\n" );
+
+        if (!seqValues[nProperty].hasValue())
+            continue;
+
         switch( nProperty )
         {
             case PROPERTYHANDLE_DONTHIDEDISABLEDENTRIES :   {
