865b843f62e4121aa6ca1c4290ec87b003183620
Lucas Baudin <xapantu@gmail.com>
2011-05-22 11:04:58 +0200

    Fix some visual glitch with GTK+
    - Fix separators, they weren't properly aligned, and they didn't use
      gtk_paint_box when needed some gtk themes/engine draw lines with box ^^
      (for 3d appearance for instance)
    - Fix pressed toolbar buttons state: it wasn't drawn correctly
    - Fix toolbar buttons background: window background was paint, but the
      background was the toolbar, so, now, the background of the toolbar is paint
    - Use gtk_paint_box instead of gtk_paint_flat_box for buttondefault, it wasn't
      correct and caused some weird things in the corners
    - Code cleanup: remove a function call which set the widgets flags, it is
      useless (and could cause some segfaults) since we must specify widget state
      and shadow in gtk_paint_(flat_)box).
    - Code cleanup: remove duplicated code, toolbar button are now drawn using the
      same function as normal buttons

--- vcl/unx/inc/plugins/gtk/gtkgdi.hxx
+++ vcl/unx/inc/plugins/gtk/gtkgdi.hxx
@@ -95,6 +95,11 @@ protected:
     GdkPixmap* NWGetPixmapFromScreen( Rectangle srcRect );
     sal_Bool NWRenderPixmapToScreen( GdkPixmap* pPixmap, Rectangle dstRect );
 
+    sal_Bool NWPaintGTKButtonReal( GtkWidget* button, GdkDrawable* gdkDrawable, ControlType nType, ControlPart nPart,
+                           const Rectangle& rControlRectangle,
+                           const clipList& rClipList,
+                           ControlState nState, const ImplControlValue& aValue,
+                           const OUString& rCaption );
     sal_Bool NWPaintGTKButton( GdkDrawable* gdkDrawable, ControlType nType, ControlPart nPart,
                            const Rectangle& rControlRectangle,
                            const clipList& rClipList,
