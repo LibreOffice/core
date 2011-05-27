7ec24fbe56590c4d8a49b0360225d41d7b316540
Nigel Hawkins <n.hawkins@gmx.com>
2011-05-27 21:01:54 +0100

    Fix build breakage with debug enabled.

--- sax/source/tools/fastserializer.cxx
+++ sax/source/tools/fastserializer.cxx
@@ -514,7 +514,7 @@ namespace sax_fastparser {
         std::map< sal_Int32, Int8Sequence >::iterator iter = maData.begin();
         while ( iter != maData.end( ) )
         {
-            fprintf( stderr, "pair: %ld, ", iter->first );
+            fprintf( stderr, "pair: %d, ", iter->first );
             for ( sal_Int32 i=0, len=iter->second.getLength(); i < len; ++i )
                 fprintf( stderr, "%c", iter->second[i] );
             fprintf( stderr, "\n" );
