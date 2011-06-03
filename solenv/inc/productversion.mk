55bdcf0c672ff68669c85ee14a7711417b99b5b0
Petr Mladek <pmladek@suse.cz>
2011-06-03 21:48:49 +0200

    Revert "Reverting one Oxygenoffice change until _KAMI_ makes it work"
    This reverts commit 77eb7f58d973eeb397dac0702646fc811894851c.
    
    The original build problems were fixed in libs-core/sysui.
    
    Signed-off-by: pmladek@suse.cz

--- solenv/inc/productversion.mk
+++ solenv/inc/productversion.mk
@@ -25,7 +25,7 @@
 #
 #*************************************************************************
 
-PRODUCTLIST = libreoffice broffice
+PRODUCTLIST = libreoffice broffice oxygenoffice
 
 # default values to minimize maintainance effort
 PRODUCTVERSION = 3.4
