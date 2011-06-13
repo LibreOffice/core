a44330b976452828296fe01cb419d851b7936d20
Tor Lillqvist <tml@iki.fi>
2011-06-13 17:54:21 +0300

    Bypass DLL prefix and extension stuff on iOS for now

--- cppuhelper/source/shlib.cxx
+++ cppuhelper/source/shlib.cxx
@@ -244,6 +244,7 @@ static OUString makeComponentPath(
         if (rPath[ rPath.getLength() -1 ] != '/')
             buf.append( (sal_Unicode) '/' );
     }
+#ifndef IOS
     sal_Int32 nEnd = endsWith( rLibName, OUSTR(SAL_DLLEXTENSION) );
     if (nEnd < 0) // !endsWith
     {
@@ -256,6 +257,7 @@ static OUString makeComponentPath(
         buf.appendAscii( RTL_CONSTASCII_STRINGPARAM(SAL_DLLEXTENSION) );
     }
     else // name is completely pre/postfixed
+#endif
     {
         buf.append( rLibName );
     }
