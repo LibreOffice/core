1ed472827bb0a6a3c640ea892290ea59760f6044
Caolán McNamara <caolanm@redhat.com>
2011-06-14 11:58:53 +0100

    can shrink these buffers to minimum required

--- tools/source/stream/stream.cxx
+++ tools/source/stream/stream.cxx
@@ -1938,7 +1938,7 @@ void SvStream::RefreshBuffer()
 
 SvStream& SvStream::WriteNumber(sal_Int32 nInt32)
 {
-    char buffer[256+12];
+    char buffer[12];
     sal_Size nLen = sprintf(buffer, "%"SAL_PRIdINT32, nInt32);
     Write(buffer, nLen);
     return *this;
@@ -1946,7 +1946,7 @@ SvStream& SvStream::WriteNumber(sal_Int32 nInt32)
 
 SvStream& SvStream::WriteNumber(sal_uInt32 nUInt32)
 {
-    char buffer[256+12];
+    char buffer[11];
     sal_Size nLen = sprintf(buffer, "%"SAL_PRIuUINT32, nUInt32);
     Write(buffer, nLen);
     return *this;
