ec7e1a9608971e9d7fd24d586ef8b1e8df54e84d
Tor Lillqvist <tlillqvist@novell.com>
2011-06-17 23:33:43 +0300

    hunspell-mingw.patch does not even apply

--- hunspell/makefile.mk
+++ hunspell/makefile.mk
@@ -80,9 +80,6 @@ OUT2LIB=$(BUILD_DIR)$/src$/hunspell$/.libs$/libhunspell-1.2.a
 
 .IF "$(GUI)"=="WNT"
 .IF "$(COM)"=="GCC"
-PATCH_FILES=\
-    hunspell-mingw.patch
-
 CONFIGURE_ACTION=configure
 CONFIGURE_FLAGS= --disable-shared --disable-nls --with-pic LDFLAGS=-Wl,--enable-runtime-pseudo-reloc-v2
 BUILD_ACTION=make
