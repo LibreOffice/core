67b392a0621eaf4f9a3b0a99cc37109ca32263e8
Caolán McNamara <caolanm@redhat.com>
2011-06-20 00:36:36 +0100

    totally pointless intermediate object

--- formula/source/core/resource/core_resource.cxx
+++ formula/source/core/resource/core_resource.cxx
@@ -1,4 +1,3 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -56,9 +55,7 @@ namespace formula
 
         ::com::sun::star::lang::Locale aLocale = Application::GetSettings().GetUILocale();
 
-        ByteString sFileName("for");
-
-        m_pImpl = ResMgr::CreateResMgr(sFileName.GetBuffer(), aLocale);
+        m_pImpl = ResMgr::CreateResMgr("for", aLocale);
     }
 
     //------------------------------------------------------------------
