e8de073d6336a2a8e170ecb5e1598c8ff4819cef
Caolán McNamara <caolanm@redhat.com>
2011-06-21 11:33:27 +0100

    use size_t here

--- hwpfilter/source/hwpreader.cxx
+++ hwpfilter/source/hwpreader.cxx
@@ -538,12 +538,12 @@ void HwpReader::makeDrawMiscStyle( HWPDrawingObject *hdo )
                         if( (fd = open( filename , O_CREAT | O_WRONLY , 0666)) >= 0 )
 #endif
                         {
-                            write( fd, emp->data, emp->size );
+                            size_t nWritten = write(fd, emp->data, emp->size);
+                            OSL_VERIFY(nWritten == emp->size);
                             close(fd);
                         }
 #ifdef _WIN32
-                        int j;
-                        for( j = 0 ; j < (int)strlen( dirname ) ; j++)
+                        for(int j = 0 ; j < (int)strlen( dirname ) ; j++)
                         {
                             if( dirname[j] == '\\' ) buf[j] = '/';
                             else buf[j] = dirname[j];
