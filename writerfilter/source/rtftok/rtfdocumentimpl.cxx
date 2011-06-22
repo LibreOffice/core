fe553af4f7a7bfc284f9e4b2fe95dd3efaf334e2
Miklos Vajna <vmiklos@frugalware.org>
2011-06-22 20:48:26 +0200

    implement RTF_COLSX

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1422,6 +1422,13 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
                         NS_ooxml::LN_EG_SectPrContents_cols, NS_ooxml::LN_CT_Columns_num, pValue);
             }
             break;
+        case RTF_COLSX:
+            {
+                RTFValue::Pointer_t pValue(new RTFValue(nParam));
+                lcl_putNestedAttribute(m_aStates.top().aSectionSprms,
+                        NS_ooxml::LN_EG_SectPrContents_cols, NS_ooxml::LN_CT_Columns_space, pValue);
+            }
+            break;
         default:
             OSL_TRACE("%s: TODO handle value '%s'", OSL_THIS_FUNC, m_pCurrentKeyword->getStr());
             bParsed = false;
