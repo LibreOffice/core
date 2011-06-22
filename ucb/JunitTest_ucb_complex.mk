01ea92166c12fe080575f8faba4162779fb46c28
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-06-22 14:32:23 +0200

    gnumake4: adjust ucb to gbuild

--- ucb/JunitTest_ucb_complex.mk
+++ ucb/JunitTest_ucb_complex.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_JunitTest_JunitTest,ucb_complex))
+$(eval $(call gb_JunitTest_JunitTest,ucb_complex,SRCDIR))
 
 $(eval $(call gb_JunitTest_set_defs,ucb_complex,\
     $$(DEFS) \
--- ucb/JunitTest_ucb_unoapi.mk
+++ ucb/JunitTest_ucb_unoapi.mk
@@ -25,7 +25,7 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-$(eval $(call gb_JunitTest_JunitTest,ucb_unoapi))
+$(eval $(call gb_JunitTest_JunitTest,ucb_unoapi,SRCDIR))
 
 $(eval $(call gb_JunitTest_set_defs,ucb_unoapi,\
     $$(DEFS) \
