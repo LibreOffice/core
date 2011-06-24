3c05f4b2fa6f64476f5186cc28fcdb50a0d779db
Caolán McNamara <caolanm@redhat.com>
2011-06-24 10:29:09 +0100

    bad merge, fix to enable extensions to build again

--- odk/settings/settings.mk
+++ odk/settings/settings.mk
@@ -502,11 +502,9 @@ PLATFORM=kfreebsd
 ifeq "$(PROCTYPE)" "x86_64"
 PACKAGE_LIB_DIR=kfreebsd_x86_64.plt
 UNOPKG_PLATFORM=kFreeBSD_x86_64
-STLPORT=no
 else
 PACKAGE_LIB_DIR=kfreebsd_x86.plt
 UNOPKG_PLATFORM=kFreeBSD_x86
-STLPORT=no
 endif
 else
 PLATFORM=freebsd
@@ -570,10 +568,6 @@ CPPUHELPERLIB=-luno_cppuhelper$(COMID)
 SALHELPERLIB=-luno_salhelper$(COMID)
 REGLIB=-lreg
 STORELIB=-lstore
-ifeq "$(STLPORT)" "yes"
-endif
-else
-STLPORTLIB=
 
 EMPTYSTRING=
 PATH_SEPARATOR=:
