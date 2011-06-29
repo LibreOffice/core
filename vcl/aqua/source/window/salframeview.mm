fe26694b03e30c63952e54b1139a6009dc1448bb
Tor Lillqvist <tml@iki.fi>
2011-06-29 20:52:44 +0300

    Add Emacs and vim mode lines to *.mm files, too

--- vcl/aqua/source/window/salframeview.mm
+++ vcl/aqua/source/window/salframeview.mm
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*n***********************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -1728,3 +1729,4 @@ private:
 
 @end
 
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
