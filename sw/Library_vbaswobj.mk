0db4fc4aa99f884c549626098d159df4b0562088
Caolán McNamara <caolanm@redhat.com>
2011-07-05 09:57:57 +0100

    no need for extra noexecstack

--- sw/Library_vbaswobj.mk
+++ sw/Library_vbaswobj.mk
@@ -141,11 +141,4 @@ $(eval $(call gb_Library_add_exception_objects,vbaswobj,\
     sw/source/ui/vba/wordvbahelper \
 ))
 
-ifneq (,$(filter LINUX DRAGONFLY OPENBSD FREEBSD NETBSD, $(OS)))
-$(eval $(call gb_Library_set_ldflags,vbaswobj,\
-    $$(LDFLAGS) \
-    -Wl$(COMMA)-z$(COMMA)noexecstack \
-))
-endif
-
 # vim: set noet sw=4 ts=4:
