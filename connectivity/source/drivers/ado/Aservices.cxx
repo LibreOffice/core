9b8b278d5e38b11f9c819f2b6c2bfa478a35b217
Matúš Kukan <matus.kukan@gmail.com>
2011-07-12 18:25:18 +0200

    Keep this one component_getImplementationEnvironment method

--- connectivity/source/drivers/ado/Aservices.cxx
+++ connectivity/source/drivers/ado/Aservices.cxx
@@ -86,6 +86,14 @@ struct ProviderRequest
 };
 
 //---------------------------------------------------------------------------------------
+extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
+                const sal_Char **ppEnvTypeName,
+                uno_Environment        ** /*ppEnv*/)
+{
+    *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME ":affine";
+}
+
+//---------------------------------------------------------------------------------------
 extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
