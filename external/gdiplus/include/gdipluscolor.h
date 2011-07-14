490b499531f76f07830aa0c4266d0a89cc9b61f8
Tor Lillqvist <tlillqvist@novell.com>
2011-07-14 18:17:37 +0300

    Add constructor and GetValue() method to the Color class

--- external/gdiplus/include/gdipluscolor.h
+++ external/gdiplus/include/gdipluscolor.h
@@ -30,9 +30,17 @@ enum ColorChannelFlags
 
 #ifdef __cplusplus
 
-/* FIXME: missing the methods. */
 class Color
 {
+ public:
+    Color(BYTE a, BYTE r, BYTE g, BYTE b)
+    {
+        Argb = (((a<<24)&0xFF000000) | ((r<<16)&0x00FF0000) | ((g<<8)&0x0000FF00) | (b&0x000000FF));
+    }
+    ARGB GetValue() const
+    {
+        return Argb;
+    }
 protected:
     ARGB Argb;
 };
