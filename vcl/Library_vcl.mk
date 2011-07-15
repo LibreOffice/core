4b282f0eb8e5fa37f23ce3e4cbc5b349aea93925
Tor Lillqvist <tlillqvist@novell.com>
2011-07-15 16:00:42 +0300

    Use gb_Library_win32_OLDNAMES
    No need for the OLDNAMESLIB, we already had gb_Library_win32_OLDNAMES
    for this purpose.

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -571,7 +571,7 @@ $(eval $(call gb_Library_add_linked_libs,vcl,\
     mpr \
     msimg32 \
     msvcrt \
-    $(OLDNAMESLIB) \
+    $(gb_Library_win32_OLDNAMES) \
     ole32 \
     shell32 \
     user32 \
