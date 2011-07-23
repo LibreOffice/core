0efb060f68af62cd7e254cb9d5323b10bd04de89
Michael Stahl <mst@openoffice.org>
2011-07-23 13:36:46 +0200

    add unittests from gnumake4

---
+++ writerfilter/CppunitTest_writerfilter_doctok.mk
@@ -0,0 +1,51 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# Copyright 2009 by Sun Microsystems, Inc.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.	If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,writerfilter_doctok))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,writerfilter_doctok, \
+	writerfilter/qa/cppunittests/doctok/testdoctok \
+))
+
+$(eval $(call gb_CppunitTest_add_linked_libs,writerfilter_doctok, \
+	writerfilter \
+	ucbhelper \
+	cppuhelper \
+	cppu \
+	sal \
+	stl \
+	$(gb_STDLIBS) \
+))
+
+$(eval $(call gb_CppunitTest_set_include,writerfilter_doctok,\
+	$$(INCLUDE) \
+	-I$(OUTDIR)/inc/offuh \
+	-I$(OUTDIR)/inc \
+	-I$(SRCDIR)/writerfilter/inc \
+))
+
+# vim: set noet sw=4 ts=4:
