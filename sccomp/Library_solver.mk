f2f443b3a6cdd715827c44ff86999f9b2195d271
Matúš Kukan <matus.kukan@gmail.com>
2011-07-27 19:49:27 +0200

    Convert sccomp to gbuild

---
+++ sccomp/Library_solver.mk
@@ -0,0 +1,55 @@
+#*************************************************************************
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# Copyright 2000, 2011 Oracle and/or its affiliates.
+#
+# OpenOffice.org - a multi-platform office productivity suite
+#
+# This file is part of OpenOffice.org.
+#
+# OpenOffice.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenOffice.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenOffice.org.  If not, see
+# <http://www.openoffice.org/license.html>
+# for a copy of the LGPLv3 License.
+#
+#*************************************************************************
+
+$(eval $(call gb_Library_Library,solver))
+
+$(eval $(call gb_Library_set_componentfile,solver,sccomp/source/solver/solver))
+
+$(eval $(call gb_Library_set_include,solver,\
+	$$(INCLUDE) \
+	-I$(realpath $(SRCDIR)/sccomp/source/solver) \
+))
+
+$(eval $(call gb_Library_add_api,solver,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_Library_add_linked_libs,solver,\
+	comphelper \
+	cppu \
+	cppuhelper \
+	sal \
+	tl \
+	$(gb_STDLIBS) \
+))
+
+$(eval $(call gb_Library_use_external,solver,lpsolve55))
+
+$(eval $(call gb_Library_add_exception_objects,solver,\
+	sccomp/source/solver/solver \
+))
