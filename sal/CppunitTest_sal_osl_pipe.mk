1ce803704459493291db994daacb32de41277501
Tor Lillqvist <tml@iki.fi>
2011-07-30 23:03:36 +0300

    Add consistent Emacs and vim mode lines

--- sal/CppunitTest_sal_osl_pipe.mk
+++ sal/CppunitTest_sal_osl_pipe.mk
@@ -1,3 +1,4 @@
+# -*- Mode: makefile; tab-width: 4; indent-tabs-mode: t -*-
 # Version: MPL 1.1 / GPLv3+ / LGPLv3+
 #
 # The contents of this file are subject to the Mozilla Public License Version
--- sal/Module_sal.mk
+++ sal/Module_sal.mk
@@ -1,3 +1,4 @@
+# -*- Mode: makefile; tab-width: 4; indent-tabs-mode: t -*-
 # Version: MPL 1.1 / GPLv3+ / LGPLv3+
 #
 # The contents of this file are subject to the Mozilla Public License Version
@@ -35,4 +36,4 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sal,\
 	CppunitTest_sal_rtl_math \
 ))
 
-# vim: set noet ts=4 sw=4:
+# vim: set noet sw=4 ts=4:
