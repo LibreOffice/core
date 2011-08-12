d0ed4ca4715797d2b9d6455ddee7c7851cd3acd5
Norbert Thiebaud <nthiebaud@gmail.com>
2011-08-11 22:35:23 -0500

    sc_ucalc is missing a dep on AllLangRes/sc and others -> build heisenbug

--- sc/CppunitTest_sc_ucalc.mk
+++ sc/CppunitTest_sc_ucalc.mk
@@ -37,6 +37,12 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_ucalc, \
 
 $(eval $(call gb_CppunitTest_add_library_objects,sc_ucalc,sc))
 
+$(call gb_CxxObject_get_target,sc/qa/unit/ucalc): $(WORKDIR)/AllLangRes/sc
+$(call gb_CxxObject_get_target,sc/qa/unit/ucalc): $(WORKDIR)/AllLangRes/frm
+$(call gb_CxxObject_get_target,sc/qa/unit/ucalc): $(WORKDIR)/AllLangRes/for
+$(call gb_CxxObject_get_target,sc/qa/unit/ucalc): $(WORKDIR)/AllLangRes/forui
+
+
 $(eval $(call gb_CppunitTest_add_linked_libs,sc_ucalc, \
     avmedia \
     basegfx \
