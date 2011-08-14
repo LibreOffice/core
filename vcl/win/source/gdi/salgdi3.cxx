7c1a620eca8f24f08d690b65d224ddf56042bdef
Jan Holesovsky <kendy@suse.cz>
2011-08-15 00:01:57 +0200

    Commit fee11cbc61 changed encoding to UTF8, revert that.

--- vcl/win/source/gdi/salgdi3.cxx
+++ vcl/win/source/gdi/salgdi3.cxx
@@ -2812,7 +2812,7 @@ sal_Bool WinSalGraphics::CreateFontSubset( const rtl::OUString& rToFile,
     if( osl_File_E_None != osl_getSystemPathFromFileURL( rToFile.pData, &aSysPath.pData ) )
         return FALSE;
     const rtl_TextEncoding aThreadEncoding = osl_getThreadTextEncoding();
-    const ByteString aToFile( rtl::OUStringToOString(aSysPath, RTL_TEXTENCODING_UTF8));
+    const ByteString aToFile( rtl::OUStringToOString( aSysPath, aThreadEncoding ) );
 
     // check if the font has a CFF-table
     const DWORD nCffTag = CalcTag( "CFF " );
