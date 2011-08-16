8ea6c913a46d7c64398881467269c4b5aee62827
Tor Lillqvist <tlillqvist@novell.com>
2011-08-16 12:04:54 +0300

    Link with the debugging runtime in a dbgutil build here, too

--- solenv/gbuild/platform/windows.mk
+++ solenv/gbuild/platform/windows.mk
@@ -390,6 +390,7 @@ $(call gb_Helper_abbreviate_dirs_native,\
 		$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib))) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_filename,$(lib))) \
 		$(LIBS) \
+		$(if $(gb_PRODUCT),,msvcrtd.lib msvcprtd.lib) \
 		$(if $(DLLTARGET),-out:$(DLLTARGET) -implib:$(1),-out:$(1)); RC=$$?; rm $${RESPONSEFILE} \
 	$(if $(DLLTARGET),; if [ ! -f $(DLLTARGET) ]; then rm -f $(1) && false; fi) ; exit $$RC)
 endef
