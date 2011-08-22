a789d8a487c2bcc9687e859337b6d6a1b6bbf8bc
Miklos Vajna <vmiklos@frugalware.org>
2011-08-22 18:21:31 +0200

    Implement RTF_ANSICPG

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2020,6 +2020,9 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
                 m_aFontEncodings[m_nCurrentFontIndex] = rtl_getTextEncodingFromWindowsCodePage(aRTFEncodings[i].codepage);
             }
             break;
+        case RTF_ANSICPG:
+            m_aStates.top().nCurrentEncoding = rtl_getTextEncodingFromWindowsCodePage(nParam);
+            break;
         case RTF_CF:
             {
                 // NS_sprm::LN_CIco won't work, that would be an index in a static table
