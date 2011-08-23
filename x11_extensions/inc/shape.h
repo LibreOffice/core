8abc0e6679ef7622dc13f3e2006fdbb4f893cda3
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-08-23 15:15:53 +0200

    recreated tag libreoffice-3.3.0.4 which had these commits: commit 271ae244bded9542e8268681bf54878c3b7e18c3 (tag: refs/tags/libreoffice-3.3.0.4, refs/remotes/origin/libreoffice-3-3-0) Author: Petr Mladek <pmladek@suse.cz> Date:   Tue Jan 18 19:01:19 2011 +0100
        Version 3.3.0.4, tag libreoffice-3.3.0.4 (3.3-rc4)
    
    commit 56f7ff0e0d8c754f764a9a4b030e5194198a77d1
    Author: Petr Mladek <pmladek@suse.cz>
    Date:   Tue Jan 11 22:59:37 2011 +0100
    
        Branch libreoffice-3-3-0
    
        This is 'libreoffice-3-3-0' - the stable branch for the 3.3.0 release.
        Only very safe changes, reviewed by three people are allowed.
    
        If you want to commit more complicated fix for the next 3.3.x release,
        please use the 'libreoffice-3-3' branch.
    
        If you want to build something cool, unstable, and risky, use master.

--- x11_extensions/inc/shape.h
+++ x11_extensions/inc/shape.h
@@ -37,115 +37,115 @@ in this Software without prior written authorization from The Open Group.
 #include <X11/Xutil.h>
 
 typedef struct {
-    int type;           /* of event */
+    int	type;		    /* of event */
     unsigned long serial;   /* # of last request processed by server */
-    Bool send_event;        /* true if this came frome a SendEvent request */
-    Display *display;       /* Display the event was read from */
-    Window window;      /* window of event */
-    int kind;           /* ShapeBounding or ShapeClip */
-    int x, y;           /* extents of new region */
+    Bool send_event;	    /* true if this came frome a SendEvent request */
+    Display *display;	    /* Display the event was read from */
+    Window window;	    /* window of event */
+    int kind;		    /* ShapeBounding or ShapeClip */
+    int x, y;		    /* extents of new region */
     unsigned width, height;
-    Time time;          /* server timestamp when region changed */
-    Bool shaped;        /* true if the region exists */
+    Time time;		    /* server timestamp when region changed */
+    Bool shaped;	    /* true if the region exists */
 } XShapeEvent;
 
 _XFUNCPROTOBEGIN
 
 extern Bool XShapeQueryExtension (
-    Display*    /* display */,
-    int*    /* event_base */,
-    int*    /* error_base */
+    Display*	/* display */,
+    int*	/* event_base */,
+    int*	/* error_base */
 );
 
 extern Status XShapeQueryVersion (
-    Display*    /* display */,
-    int*    /* major_version */,
-    int*    /* minor_version */
+    Display*	/* display */,
+    int*	/* major_version */,
+    int*	/* minor_version */
 );
 
 extern void XShapeCombineRegion (
-    Display*    /* display */,
-    Window  /* dest */,
-    int     /* dest_kind */,
-    int     /* x_off */,
-    int     /* y_off */,
-    Region  /* region */,
-    int     /* op */
+    Display*	/* display */,
+    Window	/* dest */,
+    int		/* dest_kind */,
+    int		/* x_off */,
+    int		/* y_off */,
+    Region	/* region */,
+    int		/* op */
 );
 
 extern void XShapeCombineRectangles (
-    Display*    /* display */,
-    Window  /* dest */,
-    int     /* dest_kind */,
-    int     /* x_off */,
-    int     /* y_off */,
-    XRectangle* /* rectangles */,
-    int     /* n_rects */,
-    int     /* op */,
-    int     /* ordering */
+    Display*	/* display */,
+    Window	/* dest */,
+    int		/* dest_kind */,
+    int		/* x_off */,
+    int		/* y_off */,
+    XRectangle*	/* rectangles */,
+    int		/* n_rects */,
+    int		/* op */,
+    int		/* ordering */
 );
 
 extern void XShapeCombineMask (
-    Display*    /* display */,
-    Window  /* dest */,
-    int     /* dest_kind */,
-    int     /* x_off */,
-    int     /* y_off */,
-    Pixmap  /* src */,
-    int     /* op */
+    Display*	/* display */,
+    Window	/* dest */,
+    int		/* dest_kind */,
+    int		/* x_off */,
+    int		/* y_off */,
+    Pixmap	/* src */,
+    int		/* op */
 );
 
 extern void XShapeCombineShape (
-    Display*    /* display */,
-    Window  /* dest */,
-    int     /* dest_kind */,
-    int     /* x_off */,
-    int     /* y_off */,
-    Window  /* src */,
-    int     /* src_kind */,
-    int     /* op */
+    Display*	/* display */,
+    Window	/* dest */,
+    int		/* dest_kind */,
+    int		/* x_off */,
+    int		/* y_off */,
+    Window	/* src */,
+    int		/* src_kind */,
+    int		/* op */
 );
 
 extern void XShapeOffsetShape (
-    Display*    /* display */,
-    Window  /* dest */,
-    int     /* dest_kind */,
-    int     /* x_off */,
-    int     /* y_off */
+    Display*	/* display */,
+    Window	/* dest */,
+    int		/* dest_kind */,
+    int		/* x_off */,
+    int		/* y_off */
 );
 
 extern Status XShapeQueryExtents (
-    Display*        /* display */,
-    Window      /* window */,
-    Bool*       /* bounding_shaped */,
-    int*        /* x_bounding */,
-    int*        /* y_bounding */,
-    unsigned int*   /* w_bounding */,
-    unsigned int*   /* h_bounding */,
-    Bool*       /* clip_shaped */,
-    int*        /* x_clip */,
-    int*        /* y_clip */,
-    unsigned int*   /* w_clip */,
-    unsigned int*   /* h_clip */
+    Display*		/* display */,
+    Window		/* window */,
+    Bool*		/* bounding_shaped */,
+    int*		/* x_bounding */,
+    int*		/* y_bounding */,
+    unsigned int*	/* w_bounding */,
+    unsigned int*	/* h_bounding */,
+    Bool*		/* clip_shaped */,
+    int*		/* x_clip */,
+    int*		/* y_clip */,
+    unsigned int*	/* w_clip */,
+    unsigned int*	/* h_clip */
 );
 
 extern void XShapeSelectInput (
-    Display*        /* display */,
-    Window      /* window */,
-    unsigned long   /* mask */
+    Display*		/* display */,
+    Window		/* window */,
+    unsigned long	/* mask */
 );
 
 extern unsigned long XShapeInputSelected (
-    Display*    /* display */,
-    Window  /* window */
+    Display*	/* display */,
+    Window	/* window */
 );
 
 extern XRectangle *XShapeGetRectangles (
-    Display*    /* display */,
-    Window  /* window */,
-    int     /* kind */,
-    int*    /* count */,
-    int*    /* ordering */
+    Display*	/* display */,
+    Window	/* window */,
+    int		/* kind */,
+    int*	/* count */,
+    int*	/* ordering */
 );
 
 _XFUNCPROTOEND
