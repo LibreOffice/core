3422a02b5e9bd3a50518198c6ae9a9df0008edac
Caolán McNamara <caolanm@redhat.com>
2011-08-28 22:49:47 +0100

    tweaks to analyze .a archives

--- solenv/bin/callcatchEnv.Set.sh
+++ solenv/bin/callcatchEnv.Set.sh
@@ -1,6 +1,7 @@
-CC="callcatcher $CC"
-CXX="callcatcher $CXX"
+CC="callcatcher ${CC:-gcc}"
+CXX="callcatcher ${CXX:-g++}"
+AR="callarchive ${AR:-ar}"
+#old-school ones, can go post-gbuildification is complete
 LINK="callcatcher $CXX"
-LIBMGR="callarchive $AR"
-AR="callarchive $AR"
-export CC CXX LINK LIBMGR AR
+LIBMGR="callarchive ${LIBMGR:-ar}"
+export CC CXX AR LINK LIBMGR
