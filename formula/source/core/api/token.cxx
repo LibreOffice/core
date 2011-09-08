9c5a533adc319d3a5db050f7e2ac98c84edfd8c9
Eike Rathke <erack@apache.org>
2011-09-08 23:38:50 +0000

    calc68: #i117806# fixed negated condition, made jump matrix work again
    Original committer: Eike Rathke [er] <eike.rathke@oracle.com>

--- formula/source/core/api/token.cxx
+++ formula/source/core/api/token.cxx
@@ -1319,7 +1319,7 @@ const FormulaToken* FormulaTokenIterator::GetNonEndOfPathToken( short nIdx ) con
 
 bool FormulaTokenIterator::IsEndOfPath() const
 {
-    return GetNonEndOfPathToken( pCur->nPC + 1) != NULL;
+    return GetNonEndOfPathToken( pCur->nPC + 1) == NULL;
 }
 
 // -----------------------------------------------------------------------------
