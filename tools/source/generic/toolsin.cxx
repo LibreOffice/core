527c515873cd2337e666c817f322fdbc8f4a74bd
Caolán McNamara <caolanm@redhat.com>
2011-09-13 16:40:23 +0100

    include tools/tools.h for visibility markup

--- tools/source/generic/toolsin.cxx
+++ tools/source/generic/toolsin.cxx
@@ -29,7 +29,7 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_tools.hxx"
 
-#define _TOOLS_TOOLSIN_CXX
+#include <tools/tools.h>
 
 #include <string.h>
 #include <tools/debug.hxx>
