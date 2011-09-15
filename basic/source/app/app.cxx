32967e26b54dcb584a84cbfed8ed9281690473d3
Norbert Thiebaud <nthiebaud@gmail.com>
2011-09-15 17:12:02 -0500

    Revert "I think IMPL_GEN_RES_STR is not needed here"
    This reverts commit 9b5c26fe2d9f25dbbe51e92b0f261dd89072dca9.

--- basic/source/app/app.cxx
+++ basic/source/app/app.cxx
@@ -84,6 +84,8 @@ using namespace com::sun::star::beans;
 using ::rtl::OUString;
 using ::rtl::OUStringToOString;
 
+IMPL_GEN_RES_STR;
+
 SttResId::SttResId( sal_uInt32 nId ) :
     ResId( nId, *((*(BasicDLL**)GetAppData(SHL_BASIC))->GetSttResMgr()) )
 {
