f0548b4f96b6d52429596f9a638343c15d42ba81
Stephan Bergmann <sbergman@redhat.com>
2011-09-20 12:51:57 +0200

    Revert "Use PREFIXDIR when possible"
    This reverts commit af37c0a988109b3b38e68591a5e9a7ea39e2c2a7.

--- sysui/desktop/share/create_tree.sh
+++ sysui/desktop/share/create_tree.sh
@@ -42,31 +42,31 @@ if [ "${GNOMEDIR}" ]; then
   chmod 0644 "${DESTDIR}/${GNOMEDIR}/share/application-registry/${PREFIX}".*
 fi
 
-mkdir -p "${DESTDIR}$PREFIXDIR/bin"
+mkdir -p "${DESTDIR}/usr/bin"
 
 test -n "${OFFICE_PREFIX}" && office_prefix="${OFFICE_PREFIX}" || office_prefix=/opt
 office_root=${office_prefix}/${PREFIX}
 
 #this symlink is needed to have the API boostrap functions running right
-ln -sf "${office_root}/program/soffice" "${DESTDIR}$PREFIXDIR/bin/${PREFIX}"
+ln -sf "${office_root}/program/soffice" "${DESTDIR}/usr/bin/${PREFIX}"
 
-cp printeradmin.sh "${DESTDIR}$PREFIXDIR/bin/${PREFIX}-printeradmin"
-chmod 0755 "${DESTDIR}$PREFIXDIR/bin/${PREFIX}-printeradmin"
+cp printeradmin.sh "${DESTDIR}/usr/bin/${PREFIX}-printeradmin"
+chmod 0755 "${DESTDIR}/usr/bin/${PREFIX}-printeradmin"
 
 if test "${PREFIX}" != libreoffice${PRODUCTVERSION} ; then
     # compat symlinks
     mkdir -p "${DESTDIR}${office_prefix}"
     ln -sf libreoffice${PRODUCTVERSION} "${DESTDIR}${office_root}"
-    ln -sf $PREFIXDIR/bin/${PREFIX} "${DESTDIR}$PREFIXDIR/bin/libreoffice${PRODUCTVERSION}"
-    ln -sf $PREFIXDIR/bin/${PREFIX}-printeradmin "${DESTDIR}$PREFIXDIR/bin/libreoffice${PRODUCTVERSION}-printeradmin"
+    ln -sf /usr/bin/${PREFIX} "${DESTDIR}/usr/bin/libreoffice${PRODUCTVERSION}"
+    ln -sf /usr/bin/${PREFIX}-printeradmin "${DESTDIR}/usr/bin/libreoffice${PRODUCTVERSION}-printeradmin"
 fi
 
-mkdir -p "${DESTDIR}$PREFIXDIR/share/mime/packages"
-cp openoffice.org.xml "${DESTDIR}$PREFIXDIR/share/mime/packages/libreoffice${PRODUCTVERSION}.xml"
-chmod 0644 "${DESTDIR}$PREFIXDIR/share/mime/packages/libreoffice${PRODUCTVERSION}.xml"
+mkdir -p "${DESTDIR}/usr/share/mime/packages"
+cp openoffice.org.xml "${DESTDIR}/usr/share/mime/packages/libreoffice${PRODUCTVERSION}.xml"
+chmod 0644 "${DESTDIR}/usr/share/mime/packages/libreoffice${PRODUCTVERSION}.xml"
 
-mkdir -p "${DESTDIR}$PREFIXDIR/share/applications"
+mkdir -p "${DESTDIR}/usr/share/applications"
 for i in `cat launcherlist`; do
-  ln -sf "${office_root}/share/xdg/${i}" "${DESTDIR}$PREFIXDIR/share/applications/${PREFIX}-${i}"
+  ln -sf "${office_root}/share/xdg/${i}" "${DESTDIR}/usr/share/applications/${PREFIX}-${i}"
 done
 
