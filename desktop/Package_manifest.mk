0a5fe3833e03582896ff2db81018ef1dff3a30b8
Jan Holesovsky <kendy@suse.cz>
2011-09-20 14:43:36 +0200

    Fix manifest creation.

--- desktop/Package_manifest.mk
+++ desktop/Package_manifest.mk
@@ -28,9 +28,9 @@ $(eval $(call gb_Package_Package,desktop_manifest,$(WORKDIR)/CustomTarget/deskto
 $(eval $(call gb_Package_add_customtarget,desktop_manifest,desktop/util,SRCDIR))
 
 $(eval $(call gb_CustomTarget_add_outdir_dependencies,desktop/util,\
-    $(OUTDIR)/bin/soffice.exe \
+    $(OUTDIR)/bin/soffice.bin \
 ))
 
-$(eval $(call gb_Package_add_file,desktop_manifest,$(OUTDIR)/bin/soffice.exe.manifest,soffice.exe.manifest))
+$(eval $(call gb_Package_add_file,desktop_manifest,$(OUTDIR)/bin/soffice.bin.manifest,soffice.bin.manifest))
 
 # vim: set ts=4 sw=4 et:
