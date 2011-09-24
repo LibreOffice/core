0f7d6910567f1df22f1706623043554989216a7c
Xisco Fauli <anistenis@gmail.com>
2011-09-24 12:59:08 +0200

    Remote the python code for now.
    The reason I delete it is because this code is not used yet. I still keep working in the feature branch, so when i'll finish there i'll merge it into master. It doesn't make sense to have it in master for now

--- wizards/com/sun/star/wizards/common/prova.py
+++
@@ -1,7 +0,0 @@
-from PropertyNames import PropertyNames
-
-class prova:
-
-    def Imprimir(self):
-        print PropertyNames.PROPERTY_STEP
-
--- wizards/com/sun/star/wizards/ui/XPathSelectionListener.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-class XPathSelectionListener(object):
-
-    @abstractmethod
-    def validatePath(self):
-        pass
