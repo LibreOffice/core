e594b27aa841d2c4a0a1861def8ade092eab6c5f
Tor Lillqvist <tml@iki.fi>
2011-09-26 21:14:13 +0300

    WaE: control reaches end of non-void function

--- cppcanvas/source/mtfrenderer/textaction.cxx
+++ cppcanvas/source/mtfrenderer/textaction.cxx
@@ -2286,12 +2286,8 @@ namespace cppcanvas
                                                     rState ) );
                 }
             }
-#if defined __GNUC__
-#if __GNUC__ == 4 && __GNUC_MINOR__ >= 1
             // Unreachable; to avoid bogus warning:
             return ActionSharedPtr();
-#endif
-#endif
         }
     }
 }
