2fc62f63f93b511db63d73bba720b89300d1c94f
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-10-05 01:04:33 +0200

    reenable accidently disabled sc checks again

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -34,7 +34,7 @@ $(eval $(call gb_Module_add_targets,sc,\
 	Package_xml \
 ))
 
-#$(eval $(call gb_Module_add_check_targets,sc,\
+$(eval $(call gb_Module_add_check_targets,sc,\
 	CppunitTest_sc_filters_test \
 	CppunitTest_sc_ucalc \
 	RdbTarget_sc_filters_test \
