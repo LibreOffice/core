c1d9cbee958a0c39679cd581e6fa66fc68ed2a5d
Norbert Thiebaud <nthiebaud@gmail.com>
2011-10-05 00:42:08 -0500

    accfixes2: TextEngine::GetTextColor

--- svtools/inc/svtools/texteng.hxx
+++ svtools/inc/svtools/texteng.hxx
@@ -120,26 +120,26 @@ private:
 
     Font                maFont;
     Color               maTextColor;
-    sal_uInt16              mnCharHeight;
-    sal_uInt16              mnFixCharWidth100;
+    sal_uInt16          mnCharHeight;
+    sal_uInt16          mnFixCharWidth100;
 
-    sal_uLong               mnMaxTextLen;
-    sal_uLong               mnMaxTextWidth;
-    sal_uLong               mnCurTextWidth;
-    sal_uLong               mnCurTextHeight;
-    sal_uLong               mnDefTab;
+    sal_uLong           mnMaxTextLen;
+    sal_uLong           mnMaxTextWidth;
+    sal_uLong           mnCurTextWidth;
+    sal_uLong           mnCurTextHeight;
+    sal_uLong           mnDefTab;
 
     TxtAlign            meAlign;
 
-    sal_Bool                mbIsFormatting      : 1;    // Semaphore wegen der Hook's
-    sal_Bool                mbFormatted         : 1;
-    sal_Bool                mbUpdate            : 1;
-    sal_Bool                mbModified          : 1;
-    sal_Bool                mbUndoEnabled       : 1;
-    sal_Bool                mbIsInUndo          : 1;
-    sal_Bool                mbDowning           : 1;
-    sal_Bool                mbRightToLeft       : 1;
-    sal_Bool                mbHasMultiLineParas : 1;
+    sal_Bool            mbIsFormatting      : 1;    // Semaphore wegen der Hook's
+    sal_Bool            mbFormatted         : 1;
+    sal_Bool            mbUpdate            : 1;
+    sal_Bool            mbModified          : 1;
+    sal_Bool            mbUndoEnabled       : 1;
+    sal_Bool            mbIsInUndo          : 1;
+    sal_Bool            mbDowning           : 1;
+    sal_Bool            mbRightToLeft       : 1;
+    sal_Bool            mbHasMultiLineParas : 1;
 
                         TextEngine( const TextEngine& ) : SfxBroadcaster()  {}
     TextEngine&         operator=( const TextEngine& )      { return *this; }
@@ -183,10 +183,10 @@ protected:
     void                CheckIdleFormatter();
     void                IdleFormatAndUpdate( TextView* pCurView = 0, sal_uInt16 nMaxTimerRestarts = 5 );
 
-    sal_Bool                CreateLines( sal_uLong nPara );
+    sal_Bool            CreateLines( sal_uLong nPara );
     void                CreateAndInsertEmptyLine( sal_uLong nPara );
     void                ImpBreakLine( sal_uLong nPara, TextLine* pLine, TETextPortion* pPortion, sal_uInt16 nPortionStart, long nRemainingWidth );
-    sal_uInt16              SplitTextPortion( sal_uLong nPara, sal_uInt16 nPos );
+    sal_uInt16          SplitTextPortion( sal_uLong nPara, sal_uInt16 nPos );
     void                CreateTextPortions( sal_uLong nPara, sal_uInt16 nStartPos );
     void                RecalcTextPortion( sal_uLong nPara, sal_uInt16 nStartPos, short nNewChars );
     void                SeekCursor( sal_uLong nNode, sal_uInt16 nPos, Font& rFont, OutputDevice* pOutDev );
@@ -194,18 +194,18 @@ protected:
     void                FormatDoc();
     void                FormatFullDoc();
     void                FormatAndUpdate( TextView* pCurView = 0 );
-    sal_Bool                IsFormatting() const { return mbIsFormatting; }
+    sal_Bool            IsFormatting() const { return mbIsFormatting; }
     void                UpdateViews( TextView* pCurView = 0 );
 
     void                ImpPaint( OutputDevice* pOut, const Point& rStartPos, Rectangle const* pPaintArea, TextSelection const* pPaintRange = 0, TextSelection const* pSelection = 0 );
 
     void                UpdateSelections();
 
-    sal_Bool                IsFormatted() const { return mbFormatted; }
+    sal_Bool            IsFormatted() const { return mbFormatted; }
 
-    sal_uInt16              GetCharPos( sal_uLong nPara, sal_uInt16 nLine, long nDocPosX, sal_Bool bSmart = sal_False );
+    sal_uInt16          GetCharPos( sal_uLong nPara, sal_uInt16 nLine, long nDocPosX, sal_Bool bSmart = sal_False );
     Rectangle           GetEditCursor( const TextPaM& rPaM, sal_Bool bSpecial, sal_Bool bPreferPortionStart = sal_False );
-    sal_uInt16              ImpFindIndex( sal_uLong nPortion, const Point& rPosInPara, sal_Bool bSmart );
+    sal_uInt16          ImpFindIndex( sal_uLong nPortion, const Point& rPosInPara, sal_Bool bSmart );
     long                ImpGetPortionXOffset( sal_uLong nPara, TextLine* pLine, sal_uInt16 nTextPortion );
     long                ImpGetXPos( sal_uLong nPara, TextLine* pLine, sal_uInt16 nIndex, sal_Bool bPreferPortionStart = sal_False );
     long                ImpGetOutputOffset( sal_uLong nPara, TextLine* pLine, sal_uInt16 nIndex, sal_uInt16 nIndex2 );
@@ -213,10 +213,10 @@ protected:
     void                ImpInitLayoutMode( OutputDevice* pOutDev, sal_Bool bDrawingR2LPortion = sal_False );
     TxtAlign            ImpGetAlign() const;
 
-    sal_uLong               CalcTextHeight();
-    sal_uLong               CalcParaHeight( sal_uLong nParagraph ) const;
-    sal_uLong               CalcTextWidth( sal_uLong nPara );
-    sal_uLong               CalcTextWidth( sal_uLong nPara, sal_uInt16 nPortionStart, sal_uInt16 nPortionLen, const Font* pFont = 0 );
+    sal_uLong           CalcTextHeight();
+    sal_uLong           CalcParaHeight( sal_uLong nParagraph ) const;
+    sal_uLong           CalcTextWidth( sal_uLong nPara );
+    sal_uLong           CalcTextWidth( sal_uLong nPara, sal_uInt16 nPortionStart, sal_uInt16 nPortionLen, const Font* pFont = 0 );
     Range               GetInvalidYOffsets( sal_uLong nPortion );
 
     // Fuer Undo/Redo
@@ -238,21 +238,21 @@ public:
     String              GetTextLines( LineEnd aSeparator = LINEEND_LF ) const;
     void                ReplaceText(const TextSelection& rSel, const String& rText);
 
-    sal_uLong               GetTextLen( LineEnd aSeparator = LINEEND_LF ) const;
-    sal_uLong               GetTextLen( const TextSelection& rSel, LineEnd aSeparator = LINEEND_LF ) const;
+    sal_uLong           GetTextLen( LineEnd aSeparator = LINEEND_LF ) const;
+    sal_uLong           GetTextLen( const TextSelection& rSel, LineEnd aSeparator = LINEEND_LF ) const;
 
     void                SetFont( const Font& rFont );
     const Font&         GetFont() const { return maFont; }
 
-    sal_uInt16              GetDefTab() const;
+    sal_uInt16          GetDefTab() const;
 
     void                SetLeftMargin( sal_uInt16 n );
-    sal_uInt16              GetLeftMargin() const;
+    sal_uInt16          GetLeftMargin() const;
 
     void                SetUpdateMode( sal_Bool bUpdate );
-    sal_Bool                GetUpdateMode() const { return mbUpdate; }
+    sal_Bool            GetUpdateMode() const { return mbUpdate; }
 
-    sal_uInt16              GetViewCount() const;
+    sal_uInt16          GetViewCount() const;
     TextView*           GetView( sal_uInt16 nView ) const;
     void                InsertView( TextView* pTextView );
     void                RemoveView( TextView* pTextView );
@@ -260,25 +260,25 @@ public:
     void                SetActiveView( TextView* pView );
 
     void                SetMaxTextLen( sal_uLong nLen );
-    sal_uLong               GetMaxTextLen() const { return mnMaxTextLen; }
+    sal_uLong           GetMaxTextLen() const { return mnMaxTextLen; }
 
     void                SetMaxTextWidth( sal_uLong nWidth );
-    sal_uLong               GetMaxTextWidth() const { return mnMaxTextWidth; }
+    sal_uLong           GetMaxTextWidth() const { return mnMaxTextWidth; }
 
-    sal_uLong               GetTextHeight() const;
-    sal_uLong               CalcTextWidth();
-    sal_uInt16              GetCharHeight() const { return mnCharHeight; }
+    sal_uLong           GetTextHeight() const;
+    sal_uLong           CalcTextWidth();
+    sal_uInt16          GetCharHeight() const { return mnCharHeight; }
 
-    sal_uLong               GetParagraphCount() const;
+    sal_uLong           GetParagraphCount() const;
     String              GetText( sal_uLong nParagraph ) const;
-    sal_uInt16              GetTextLen( sal_uLong nParagraph ) const;
-    sal_uLong               GetTextHeight( sal_uLong nParagraph ) const;
+    sal_uInt16          GetTextLen( sal_uLong nParagraph ) const;
+    sal_uLong           GetTextHeight( sal_uLong nParagraph ) const;
 
-    sal_uInt16              GetLineCount( sal_uLong nParagraph ) const;
-    sal_uInt16              GetLineLen( sal_uLong nParagraph, sal_uInt16 nLine ) const;
+    sal_uInt16          GetLineCount( sal_uLong nParagraph ) const;
+    sal_uInt16          GetLineLen( sal_uLong nParagraph, sal_uInt16 nLine ) const;
 
     void                SetRightToLeft( sal_Bool bR2L );
-    sal_Bool                IsRightToLeft() const { return mbRightToLeft; }
+    sal_Bool            IsRightToLeft() const { return mbRightToLeft; }
 
     sal_Bool            HasUndoManager() const { return mpUndoManager ? sal_True : sal_False; }
     ::svl::IUndoManager&
@@ -291,20 +291,20 @@ public:
     void                ResetUndo();
 
     void                EnableUndo( sal_Bool bEnable );
-    sal_Bool                IsUndoEnabled()             { return mbUndoEnabled; }
+    sal_Bool            IsUndoEnabled()             { return mbUndoEnabled; }
 
     void                SetModified( sal_Bool bModified )   { mbModified = bModified; }
-    sal_Bool                IsModified() const              { return mbModified; }
+    sal_Bool            IsModified() const              { return mbModified; }
 
-    sal_Bool                Read( SvStream& rInput, const TextSelection* pSel = NULL );
+    sal_Bool            Read( SvStream& rInput, const TextSelection* pSel = NULL );
 
-    sal_Bool                Write( SvStream& rOutput, const TextSelection* pSel = NULL, sal_Bool bHTML = sal_False );
+    sal_Bool            Write( SvStream& rOutput, const TextSelection* pSel = NULL, sal_Bool bHTML = sal_False );
 
     TextPaM             GetPaM( const Point& rDocPos, sal_Bool bSmart = sal_True );
     Rectangle           PaMtoEditCursor( const TextPaM& rPaM, sal_Bool bSpecial = sal_False );
     String              GetWord( const TextPaM& rCursorPos, TextPaM* pStartOfWord = 0 );
 
-    sal_Bool                    HasAttrib( sal_uInt16 nWhich ) const;
+    sal_Bool                HasAttrib( sal_uInt16 nWhich ) const;
     const TextAttrib*       FindAttrib( const TextPaM& rPaM, sal_uInt16 nWhich ) const;
     const TextCharAttrib*   FindCharAttrib( const TextPaM& rPaM, sal_uInt16 nWhich ) const;
 
@@ -312,7 +312,7 @@ public:
     void                RemoveAttrib( sal_uLong nPara, const TextCharAttrib& rAttrib );
     void                RemoveAttribs( sal_uLong nPara, sal_Bool bIdleFormatAndUpdate = sal_True );
     void                SetAttrib( const TextAttrib& rAttr, sal_uLong nPara, sal_uInt16 nStart, sal_uInt16 nEnd, sal_Bool bIdleFormatAndUpdate = sal_True );
-
+    Color               GetTextColor() const { return maTextColor; };
     TxtAlign            GetTextAlign() const { return meAlign; }
     void                SetTextAlign( TxtAlign eAlign );
 
