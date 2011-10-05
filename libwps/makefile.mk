5154df8cb9b8dd314976cbd37afa0888513ee7fa
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-10-05 14:51:59 +0200

    fix also libwps build after change in libwp* flags

--- libwps/makefile.mk
+++ libwps/makefile.mk
@@ -55,6 +55,9 @@ TARFILE_MD5=9717505e864a16173afa681bda4bc910
 BUILD_ACTION=dmake $(MFLAGS) $(CALLMACROS)
 BUILD_DIR=src/lib
 
+PATCH_FILES=\
+	libwps-0.2.3-build.patch
+
 # --- Targets ------------------------------------------------------
 
 .INCLUDE : set_ext.mk
