17523de685d0c4b06f9be02c02788583e9fd221e
Jan Holesovsky <kendy@suse.cz>
2011-10-17 11:37:56 +0200

    Skeleton of the unit test for updates.

---
+++ extensions/qa/update/test_update.cxx
@@ -0,0 +1,62 @@
+/*
+ * Version: MPL 1.1 / GPLv3+ / LGPLv3+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * Copyright (C) 2011 Jan Holesovsky <kendy@suse.cz> (initial developer)
+ *
+ * All Rights Reserved.
+ *
+ * For minor contributions see the git repository.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+ * the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+ * in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+ * instead of those above.
+ */
+
+#include <sal/config.h>
+#include <sal/precppunit.hxx>
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+namespace testupdate {
+
+class test : public CppUnit::TestFixture
+{
+public:
+    void setUp() {}
+
+    void tearDown() {}
+
+protected:
+    // setup & test checkForUpdates() method
+    void testUpdateCheck()
+    {
+        //CPPUNIT_FAIL( "This has to be populated with the real test..." );
+    }
+
+    CPPUNIT_TEST_SUITE(test);
+    CPPUNIT_TEST(testUpdateCheck);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(testupdate::test, "update");
+} // namespace testupdate
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
