05a33692a084f59f2fae0a535131133d2a3f6b72
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-10-20 23:19:46 +0200

    experimental countoutdated target

---
+++ solenv/gbuild/extensions/pre_Counters.mk
@@ -0,0 +1,48 @@
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+#       Bjoern Michaelsen, Canonical Ltd. <bjoern.michaelsen@canonical.com>
+# Portions created by the Initial Developer are Copyright (C) 2010 the
+# Initial Developer. All Rights Reserved.
+#
+# Major Contributor(s):
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+
+gb_CountersOutdated_COUNTER:=
+
+.PHONY: countoutdated
+countoutdated: $(filter-out countoutdated,$(MAKECMDGOALS))
+	$(info total outdated files: $(words $(gb_CountersOutdated_COUNTER)))
+	@true
+
+ifneq ($(filter countoutdated,$(MAKECMDGOALS)),)
+
+$(WORKDIR)/%:
+	$(info $* is outdated.)
+	$(eval gb_CountersOutdated_COUNTER+= x)
+	@true
+	
+$(OUTDIR)/%:
+	$(info $* is outdated (OUTDIR).)
+	$(eval gb_CountersOutdated_COUNTER+= x)
+	@true
+
+endif
+# vim: set noet ts=4 sw=4:
