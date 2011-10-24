d781ae9ae1aef10927de6ace368b0cc7d48280ad
Michael Stahl <mstahl@redhat.com>
2011-10-24 18:16:09 +0200

    WinResTarget: remove realpath calls

--- solenv/gbuild/WinResTarget.mk
+++ solenv/gbuild/WinResTarget.mk
@@ -60,13 +60,13 @@ endef
 
 define gb_WinResTarget_add_file
 $(call gb_WinResTarget_get_clean_target,$(1)) : RCFILE=$(gb_Helper_SRCDIR_NATIVE)/$(2).rc
-$(call gb_WinResTarget_get_target,$(1)) : RCFILE=$(realpath $(SRCDIR)/$(strip $(2)).rc)
-$(call gb_WinResTarget_get_target,$(1)) : $(realpath $(SRCDIR)/$(strip $(2)).rc)
+$(call gb_WinResTarget_get_target,$(1)) : RCFILE=$(SRCDIR)/$(strip $(2)).rc
+$(call gb_WinResTarget_get_target,$(1)) : $(SRCDIR)/$(strip $(2)).rc
 
 endef
 
 define gb_WinResTarget_add_dependency
-$(call gb_WinResTarget_get_target,$(1)) : $(foreach file,$(2),$(realpath $(SRCDIR)/$(strip $(file))))
+$(call gb_WinResTarget_get_target,$(1)) : $(foreach file,$(2),$(SRCDIR)/$(file))
 
 endef
 
