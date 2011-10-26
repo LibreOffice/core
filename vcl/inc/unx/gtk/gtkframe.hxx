a608d0664371c21c854e1f91aee2210a17641220
Petr Mladek <pmladek@suse.cz>
2011-10-26 17:16:40 +0200

    renderArea is implemented only with GTK-3
    fixes build on SLED10 with gtk-2.8 and cairo-1.0.2

--- vcl/inc/unx/gtk/gtkframe.hxx
+++ vcl/inc/unx/gtk/gtkframe.hxx
@@ -325,8 +325,9 @@ public:
     // only for gtk3 ...
     void pushIgnoreDamage();
     void popIgnoreDamage();
+#if GTK_CHECK_VERSION(3,0,0)
     void renderArea( cairo_t *cr, cairo_rectangle_t *src );
-
+#endif
     virtual ~GtkSalFrame();
 
     // SalGraphics or NULL, but two Graphics for all SalFrames
