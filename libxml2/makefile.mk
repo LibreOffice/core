e42dfec9ea188dd0c0859a409ca67c5c2cfabb5b
Tor Lillqvist <tlillqvist@suse.com>
2011-10-27 14:57:23 +0300

    Don't build libxml2.dll against debugging runtime for --enable-debug
    Passing the debug=yes option to libxml2's configure.js causes it to be
    built against the debugging runtime, which is not what we want if we
    just have used --enable-debug. It is with --enable-dbgutil that we
    want to use the debugging runtime. So look at $(dbgutil), not
    $(debug).

--- libxml2/makefile.mk
+++ libxml2/makefile.mk
@@ -95,7 +95,7 @@ BUILD_DIR=$(CONFIGURE_DIR)
 CONFIGURE_DIR=win32
 CONFIGURE_ACTION=cscript configure.js
 CONFIGURE_FLAGS=iconv=no sax1=yes
-.IF "$(debug)"!=""
+.IF "$(dbgutil)"!=""
 CONFIGURE_FLAGS+=debug=yes
 .ENDIF
 BUILD_ACTION=nmake
