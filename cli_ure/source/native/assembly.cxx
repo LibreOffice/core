0c9d2b9cb3f1b5bac034ccfee0eb83aa2182f2cf
Joseph Powers <jpowers27@cox.net>
2011-11-02 06:39:08 -0700

    precompiled_cli_ure.hxx is empty, so just remove it

--- cli_ure/source/native/assembly.cxx
+++ cli_ure/source/native/assembly.cxx
@@ -26,9 +26,6 @@
  *
  ************************************************************************/
 
-
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_cli_ure.hxx"
 #using <mscorlib.dll>
 
 [assembly:System::Reflection::AssemblyProduct( "CLI-UNO Language Binding" )];
--- cli_ure/source/uno_bridge/cli_proxy.cxx
+++ cli_ure/source/uno_bridge/cli_proxy.cxx
@@ -26,8 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_cli_ure.hxx"
 #include "typelib/typedescription.h"
 #include "rtl/ustrbuf.hxx"
 #include "com/sun/star/uno/RuntimeException.hpp"
