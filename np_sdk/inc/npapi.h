29f83de2465c6151036b65ea326432fd9b16572c
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-11-08 11:33:31 +0100

    Unbreak windows build

--- np_sdk/inc/npapi.h
+++ np_sdk/inc/npapi.h
@@ -784,10 +784,7 @@ extern "C" {
 
 /* NPP_* functions are provided by the plugin and called by the navigator. */
 
-#if defined(XP_UNIX)
 char* NPP_GetMIMEDescription(void);
-#endif
-
 NPError NP_LOADDS NPP_New(NPMIMEType pluginType, NPP instance,
                           uint16_t mode, int16_t argc, char* argn[],
                           char* argv[], NPSavedData* saved);
