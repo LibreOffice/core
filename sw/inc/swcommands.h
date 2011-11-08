20fe7d11e935b48126c31ff7629fded87e61ce85
Korrawit Pruegsanusak <detective.conan.1412@gmail.com>
2011-11-08 09:17:49 +0000

    Change Windows newline to Unix newline

--- sw/inc/swcommands.h
+++ sw/inc/swcommands.h
@@ -2,7 +2,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
