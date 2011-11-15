e3e77167ad7c0263c124b6327728502b411cebb3
Kohei Yoshida <kohei.yoshida@suse.com>
2011-11-15 13:57:24 -0500

    Fixed the ref button for "copy results to" not starting the ref mode.

--- sc/source/ui/dbgui/sfiltdlg.cxx
+++ sc/source/ui/dbgui/sfiltdlg.cxx
@@ -84,7 +84,7 @@ ScSpecialFilterDlg::ScSpecialFilterDlg( SfxBindings* pB, SfxChildWindow* pCW, Wi
         aBtnCopyResult  ( this, ScResId( BTN_COPY_RESULT ) ),
         aLbCopyArea     ( this, ScResId( LB_COPY_AREA ) ),
         aEdCopyArea     ( this, this, ScResId( ED_COPY_AREA ) ),
-        aRbCopyArea     ( this, ScResId( RB_COPY_AREA ) ),
+        aRbCopyArea     ( this, ScResId( RB_COPY_AREA ), &aEdCopyArea, this ),
         aBtnDestPers    ( this, ScResId( BTN_DEST_PERS ) ),
         aFtDbAreaLabel  ( this, ScResId( FT_DBAREA_LABEL ) ),
         aFtDbArea       ( this, ScResId( FT_DBAREA ) ),
