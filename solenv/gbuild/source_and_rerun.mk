8a7cfed28d15a7e64e897fa309189cce058fc48a
Norbert Thiebaud <nthiebaud@gmail.com>
2011-11-16 16:13:54 -0600

    support for various make goal in source_and_rerun

--- solenv/gbuild/source_and_rerun.mk
+++ solenv/gbuild/source_and_rerun.mk
@@ -1,6 +1,11 @@
 
-all:
+dummy:
 	@if test -f ./config.mk; then . ./config.mk; else if test -f ../config.mk ; then . ../config.mk; fi; fi && \
 	if test -f ./Env.Host.sh; then . ./Env.Host.sh; else if test -f ../Env.Host.sh; then . ../Env.Host.sh; fi ; fi && \
 	if test -z "$${SOLARENV}"; then echo "No environment set!" 2>&1; exit 1; fi && \
 	$(MAKE) $(MAKECMDGOALS)
+
+ifneq ($(strip $(MAKECMDGOALS)),)
+$(eval $(MAKECMDGOALS) : dummy)
+endif
+
