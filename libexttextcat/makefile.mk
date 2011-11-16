34578b7c2706d2cca95eeb266aeb0498078bb55a
Michael Stahl <mstahl@redhat.com>
2011-11-16 10:56:37 +0100

    libexttextcat: conflict in 69b04c22986b03c675cf4925f76721190aa5ba82

--- libexttextcat/makefile.mk
+++ libexttextcat/makefile.mk
@@ -47,9 +47,7 @@ all:
 TARFILE_NAME=libexttextcat-3.2.0
 TARFILE_MD5=e1c178b18f130b40494561f02bc1a948
 TARFILE_ROOTDIR=libexttextcat-3.2.0
-PATCH_FILES=libexttextcat-3.2.0-visibility.patch
-
-PATCH_FILES = android.patch
+PATCH_FILES=libexttextcat-3.2.0-visibility.patch android.patch
 
 .IF "$(GUI)"=="UNX"
 .IF "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
