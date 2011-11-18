339777016344fe463e83c71fc3d130309e99e3d7
August Sodora <augsod@gmail.com>
2011-11-18 00:57:45 -0500

    Add emacs modeline

--- basctl/source/basicide/linenumberwindow.cxx
+++ basctl/source/basicide/linenumberwindow.cxx
@@ -1,3 +1,5 @@
+/* -*- Mode: C++; eval:(c-set-style "bsd"); tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #include "baside2.hxx"
 #include "linenumberwindow.hxx"
 
--- basctl/source/basicide/linenumberwindow.hxx
+++ basctl/source/basicide/linenumberwindow.hxx
@@ -1,3 +1,5 @@
+/* -*- Mode: C++; eval:(c-set-style "bsd"); tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #ifndef BASICIDE_LINENUMBERWINDOW_HXX
 #define BASICIDE_LINENUMBERWINDOW_HXX
 
