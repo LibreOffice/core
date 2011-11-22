5ddab9d8d4ef4553fef0bc76312fb5f3565cf9f2
August Sodora <augsod@gmail.com>
2011-11-21 22:16:15 -0500

    Remove unnecessary includes

--- basctl/source/basicide/ide_pch.hxx
+++ basctl/source/basicide/ide_pch.hxx
@@ -30,21 +30,13 @@
 #define ITEMID_SEARCH           0
 #define ITEMID_SIZE             0
 
-#include <vcl/wrkwin.hxx>
 #include <vcl/msgbox.hxx>
-#include <vcl/menu.hxx>
-
-#include <svl/svarray.hxx>
-#include <svl/itemset.hxx>
 #include <svl/aeitem.hxx>
-#include <svl/stritem.hxx>
 #include <svl/whiter.hxx>
 #include <svl/intitem.hxx>
 #include <svl/srchitem.hxx>
 
 #define _BASIC_TEXTPORTIONS
-#include <basic/sbdef.hxx>
-#include <basic/sbstar.hxx>
 #include <basic/sbmeth.hxx>
 #include <basic/sbmod.hxx>
 #include <basic/basmgr.hxx>
@@ -52,20 +44,12 @@
 #include <sfx2/app.hxx>
 #include <sfx2/objface.hxx>
 #include <sfx2/printer.hxx>
-#include <sfx2/viewsh.hxx>
-#include <sfx2/event.hxx>
-#include <sfx2/bindings.hxx>
 #include <sfx2/request.hxx>
 #include <sfx2/childwin.hxx>
-#include <sfx2/viewfrm.hxx>
-#include <sfx2/objsh.hxx>
-#include <sfx2/genlink.hxx>
 #include <sfx2/dispatch.hxx>
 #include <sfx2/minfitem.hxx>
 #include <sfx2/dinfdlg.hxx>
 #include <sfx2/docfac.hxx>
-#include <sfx2/progress.hxx>
 #include <editeng/sizeitem.hxx>
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
