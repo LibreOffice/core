49a9e8a08cc14003ad5806ba9256c163489f0d02
Norbert Thiebaud <nthiebaud@gmail.com>
2011-11-27 13:12:41 -0600

    remove include of pch header from idl

--- idl/inc/pch/precompiled_idl.cxx
+++ idl/inc/pch/precompiled_idl.cxx
@@ -26,6 +26,5 @@
  *
  ************************************************************************/
 
-#include "precompiled_idl.hxx"
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- idl/source/cmptools/hash.cxx
+++ idl/source/cmptools/hash.cxx
@@ -26,8 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_idl.hxx"
 
 // C and C++ includes
 #include <stdlib.h>
