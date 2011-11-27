f7738e755b6a5332889e6c147bd562f56741a855
Norbert Thiebaud <nthiebaud@gmail.com>
2011-11-27 13:11:50 -0600

    remove include of pch header from hwpfilter

--- hwpfilter/inc/pch/precompiled_hwpfilter.hxx
+++ hwpfilter/inc/pch/precompiled_hwpfilter.hxx
@@ -26,7 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): Generated on 2006-09-01 17:49:45.139559
 
 #ifdef PRECOMPILED_HEADERS
 #endif
--- hwpfilter/source/attributes.cxx
+++ hwpfilter/source/attributes.cxx
@@ -26,8 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_hwpfilter.hxx"
 
 #include <assert.h>
 #include <vector>
