27120fe88c78d31191daf936f8296c08d227c926
Norbert Thiebaud <nthiebaud@gmail.com>
2011-11-27 13:10:27 -0600

    remove include of pch header from fpicker

--- fpicker/inc/pch/precompiled_fpicker.hxx
+++ fpicker/inc/pch/precompiled_fpicker.hxx
@@ -26,7 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): Generated on 2006-09-01 17:49:42.764604
 
 #ifdef PRECOMPILED_HEADERS
 #include <tools/debug.hxx>
--- fpicker/source/win32/filepicker/dialogcustomcontrols.cxx
+++ fpicker/source/win32/filepicker/dialogcustomcontrols.cxx
@@ -26,8 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_fpicker.hxx"
 
 #ifndef _DIALOGCUSTOMCONTROLS_CXX_
 #include "dialogcustomcontrols.hxx"
