06483141ede45f34a7e1a6bfedf7107e40839af9
Norbert Thiebaud <nthiebaud@gmail.com>
2011-11-27 13:23:56 -0600

    remove include of pch header from store

--- store/workben/t_store.cxx
+++ store/workben/t_store.cxx
@@ -26,8 +26,6 @@
  *
  ************************************************************************/
 
-// MARKER(update_precomp.py): autogen include statement, do not remove
-#include "precompiled_store.hxx"
 
 #define _T_STORE_CXX
 #include <sal/types.h>
