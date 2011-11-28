0c428b052fa0599637497f289e57858fcca4c4e2
Eike Rathke <erack@redhat.com>
2011-11-28 02:44:36 +0100

    dr78: #i43040# removed german umlauts and utf8 bom accidentially added by too smart editor
    # HG changeset patch
    # User Daniel Rentz [dr] <daniel.rentz@oracle.com>
    # Date 1295613905 -3600
    # Node ID 65540563b0d1ba68bd3a9d6e3e8111c6c0e1a71d
    # Parent  7c526a3a4dfa801f59eaeeea99e7b7a032bc6fe4

--- scaddins/source/analysis/bessel.cxx
+++ scaddins/source/analysis/bessel.cxx
@@ -57,10 +57,10 @@ const sal_Int32 MAXITER = 100;      // Maximum number of iterations.
     The algorithm follows
     http://www.reference-global.com/isbn/978-3-11-020354-7
     Numerical Mathematics 1 / Numerische Mathematik 1,
-    An algorithm-based introduction / Eine algorithmisch orientierte Einführung
+    An algorithm-based introduction / Eine algorithmisch orientierte Einfuehrung
     Deuflhard, Peter; Hohmann, Andreas
     Berlin, New York (Walter de Gruyter) 2008
-    4. überarb. u. erw. Aufl. 2008
+    4. ueberarb. u. erw. Aufl. 2008
     eBook ISBN: 978-3-11-020355-4
     Chapter 6.3.2 , algorithm 6.24
     The source is in German.
@@ -332,10 +332,10 @@ double BesselK( double fNum, sal_Int32 nOrder ) throw( IllegalArgumentException,
     The algorithm for order 0 and for order 1 follows
     http://www.reference-global.com/isbn/978-3-11-020354-7
     Numerical Mathematics 1 / Numerische Mathematik 1,
-    An algorithm-based introduction / Eine algorithmisch orientierte Einführung
+    An algorithm-based introduction / Eine algorithmisch orientierte Einfuehrung
     Deuflhard, Peter; Hohmann, Andreas
     Berlin, New York (Walter de Gruyter) 2008
-    4. überarb. u. erw. Aufl. 2008
+    4. ueberarb. u. erw. Aufl. 2008
     eBook ISBN: 978-3-11-020355-4
     Chapter 6.3.2 , algorithm 6.24
     The source is in German.
