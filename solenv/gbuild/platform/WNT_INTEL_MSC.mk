c7052a5d6242df174ba21134635ac3d18530bf34
Jan Holesovsky <kendy@suse.cz>
2011-12-08 23:55:45 +0100

    i18npool on Windows needed more fixes.

--- solenv/gbuild/platform/WNT_INTEL_MSC.mk
+++ solenv/gbuild/platform/WNT_INTEL_MSC.mk
@@ -256,7 +256,7 @@ R=$(gb_Helper_REPODIR_NATIVE) && $(subst $(REPODIR)/,$$R/,$(subst $(gb_Helper_RE
 $(subst $(REPODIR)/,$$R/,$(subst $(SRCDIR)/,$$S/,$(subst $(OUTDIR)/,$$O/,$(subst $(WORKDIR)/,$$W/,$(subst $(gb_Helper_REPODIR_NATIVE)/,$$R/,$(subst $(gb_Helper_SRCDIR_NATIVE)/,$$S/,$(subst $(gb_Helper_OUTDIR_NATIVE)/,$$O/,$(subst $(gb_Helper_WORKDIR_NATIVE)/,$$W/,$(1)))))))))
 endef
 
-gb_Helper_set_ld_path := PATH=\"$${PATH}:$(OUTDIR)/bin\"
+gb_Helper_set_ld_path := PATH="$${PATH}:$(OUTDIR)/bin"
 
 # convert parameters filesystem root to native notation
 # does some real work only on windows, make sure not to
