1a658dda70d2e139d124d60d64691356c9414f3e
Lionel Elie Mamane <lionel@mamane.lu>
2011-12-12 23:07:03 +0100

    postgresql-sdbc: do not use GNU ld-specific options on MacOS X

--- connectivity/source/drivers/postgresql/makefile.mk
+++ connectivity/source/drivers/postgresql/makefile.mk
@@ -91,9 +91,16 @@ LIBPQ_DEP_LIBS+=\
 .ENDIF
 .ELSE
 LIBPQ_LINK=$(OUTDIR)/lib/libpq.a
+.IF "$(GUI)"=="MAC"
+LIBPQ_DEP_LIBS += -Wl,-dead_strip_dylibs
+.ELSE
 LIBPQ_DEP_LIBS += -Wl,--as-needed
+.ENDIF
 .INCLUDE : $(OUTDIR_FOR_BUILD)/inc/postgresql/libpq-flags.mk
+.IF "$(GUI)"=="MAC"
+.ELSE
 LIBPQ_DEP_LIBS += -Wl,--no-as-needed
+.ENDIF
 .ENDIF
 POSTGRESQL_INC=-I$(OUTDIR)/inc/postgresql
 POSTGRESQL_LIB=
