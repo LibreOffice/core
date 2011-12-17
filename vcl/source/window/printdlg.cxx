c4ccdda15a66ae6d8751da7401f5ea4f46df0c04
Ivan Timofeev <timofeev.i.s@gmail.com>
2011-12-17 11:53:26 +0400

    fdo#34641: set initial focus to "Number of copies"

--- vcl/source/window/printdlg.cxx
+++ vcl/source/window/printdlg.cxx
@@ -902,6 +902,9 @@ PrintDialog::PrintDialog( Window* i_pParent, const boost::shared_ptr<PrinterCont
     // setup dependencies
     checkControlDependencies();
 
+    // set initial focus to "Number of copies"
+    maJobPage.maCopyCountField.GrabFocus();
+    maJobPage.maCopyCountField.SetSelection( Selection(0, 0xFFFF) );
 }
 
 PrintDialog::~PrintDialog()
