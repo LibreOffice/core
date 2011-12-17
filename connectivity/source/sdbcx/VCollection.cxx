d18daeff771b255207bb30d428540d9ebd28ec32
Ivan Timofeev <timofeev.i.s@gmail.com>
2011-12-17 16:22:42 +0400

    correct diagnostic message

--- connectivity/source/sdbcx/VCollection.cxx
+++ connectivity/source/sdbcx/VCollection.cxx
@@ -535,7 +535,7 @@ void OCollection::renameObject(const ::rtl::OUString _sOldName,const ::rtl::OUSt
     OSL_ENSURE(m_pElements->exists(_sOldName),"Element doesn't exist");
     OSL_ENSURE(!m_pElements->exists(_sNewName),"Element already exists");
     OSL_ENSURE(!_sNewName.isEmpty(),"New name must not be empty!");
-    OSL_ENSURE(!_sOldName.isEmpty(),"New name must not be empty!");
+    OSL_ENSURE(!_sOldName.isEmpty(),"Old name must not be empty!");
 
     if ( m_pElements->rename(_sOldName,_sNewName) )
     {
