9148757cc823efa0ac6dfe0a9a825bae47cb31fa
Matúš Kukan <matus.kukan@gmail.com>
2011-12-20 22:23:33 +0100

    callcather: remove sdr::Comment

--- svx/Library_svxcore.mk
+++ svx/Library_svxcore.mk
@@ -305,7 +305,6 @@ $(eval $(call gb_Library_add_exception_objects,svxcore,\
     svx/source/svdraw/clonelist \
     svx/source/svdraw/gradtrns \
     svx/source/svdraw/polypolygoneditor \
-    svx/source/svdraw/sdrcomment \
     svx/source/svdraw/sdrhittesthelper \
     svx/source/svdraw/sdrmasterpagedescriptor \
     svx/source/svdraw/sdrpagewindow \
