98d75b3506520bd5db7071a2d9379fb7c565b9d9
Caolán McNamara <caolanm@redhat.com>
2011-12-21 16:38:55 +0000

    ditch min/max as macros under msvc

--- sfx2/Library_sfx.mk
+++ sfx2/Library_sfx.mk
@@ -53,7 +53,7 @@ $(eval $(call gb_Library_set_include,sfx,\
 ))
 
 $(eval $(call gb_Library_add_defs,sfx,\
-    -DSFX2_DLLIMPLEMENTATION \
+    -DSFX2_DLLIMPLEMENTATION -DNOMINMAX \
 ))
 
 ifeq ($(ENABLE_SYSTRAY_GTK),TRUE)
