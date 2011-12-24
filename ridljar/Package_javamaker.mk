e54637bf0b85605cc045d34e7ede19a05f7fe7b8
David Tardon <dtardon@redhat.com>
2011-12-24 15:14:26 +0100

    add dep on javamaker

--- ridljar/Package_javamaker.mk
+++ ridljar/Package_javamaker.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_Package_Package,ridljar_javamaker,$(WORKDIR)/CustomTarget/ridlj
 $(eval $(call gb_Package_add_customtarget,ridljar_javamaker,ridljar/javamaker))
 
 $(eval $(call gb_CustomTarget_add_outdir_dependencies,ridljar/javamaker,\
+    $(OUTDIR_FOR_BUILD)/bin/javamaker \
     $(OUTDIR)/bin/udkapi.rdb \
 ))
 
