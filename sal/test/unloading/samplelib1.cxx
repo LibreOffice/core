ac6b7001773a76a210b26a1f2231c1ff5f06e91c
David Tardon <dtardon@redhat.com>
2011-12-27 15:43:29 +0100

    function component_canUnload is SAL_CALL

--- sal/test/unloading/samplelib1.cxx
+++ sal/test/unloading/samplelib1.cxx
@@ -257,7 +257,7 @@ extern "C" {
         return pRet;
     }
 
-    SAL_DLLPUBLIC_EXPORT sal_Bool component_canUnload( TimeValue* libUnused)
+    SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_canUnload( TimeValue* libUnused)
     {
         return globalModuleCount.canUnload( &globalModuleCount, libUnused);
     }
