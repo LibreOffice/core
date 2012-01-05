42527448cfd08cbf1ef16d0e9f03e16a77169cb8
David Tardon <dtardon@redhat.com>
2012-01-05 07:38:45 +0100

    WaE: invalid conversion from 'const ScToken*' to 'ScToken*'
    Signed-off-by: David Tardon <dtardon@redhat.com>

--- sc/source/ui/unoobj/chart2uno.cxx
+++ sc/source/ui/unoobj/chart2uno.cxx
@@ -2212,7 +2212,7 @@ ScChart2DataProvider::createDataSequenceByFormulaTokens(
             case svExternalSingleRef:
             case svExternalDoubleRef:
             {
-                ScTokenRef pNew(static_cast<const ScToken*>(p->Clone()));
+                ScTokenRef pNew(static_cast<ScToken*>(p->Clone()));
                 aRefTokens.push_back(pNew);
             }
             break;
