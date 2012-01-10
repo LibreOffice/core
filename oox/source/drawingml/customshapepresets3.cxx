81e0f1264e2aca3f3f7c55397dfe1f897487addb
Radek Doulik <rodo@novell.com>
2012-01-10 13:09:51 +0100

    pptx: regenerate presets after custom shapes moveto/close fix

--- oox/source/drawingml/customshapepresets3.cxx
+++ oox/source/drawingml/customshapepresets3.cxx
@@ -11808,7 +11808,7 @@ void CustomShapeProperties::initializePresetsMap3()
         }
         {
             aPropSequence [1].Name = CREATE_OUSTRING ("Segments");
-            Sequence< EnhancedCustomShapeSegment > aSegmentSeq (13);
+            Sequence< EnhancedCustomShapeSegment > aSegmentSeq (12);
             {
                 EnhancedCustomShapeSegment aSegment;
                 aSegment.Command = 1;
@@ -11841,51 +11841,45 @@ void CustomShapeProperties::initializePresetsMap3()
             }
             {
                 EnhancedCustomShapeSegment aSegment;
-                aSegment.Command = 4;
-                aSegment.Count = 0;
+                aSegment.Command = 2;
+                aSegment.Count = 1;
                 aSegmentSeq [5] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
-                aSegment.Command = 2;
-                aSegment.Count = 1;
+                aSegment.Command = 6;
+                aSegment.Count = 0;
                 aSegmentSeq [6] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
-                aSegment.Command = 6;
+                aSegment.Command = 5;
                 aSegment.Count = 0;
                 aSegmentSeq [7] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
-                aSegment.Command = 5;
-                aSegment.Count = 0;
-                aSegmentSeq [8] = aSegment;
-            }
-            {
-                EnhancedCustomShapeSegment aSegment;
                 aSegment.Command = 1;
                 aSegment.Count = 1;
-                aSegmentSeq [9] = aSegment;
+                aSegmentSeq [8] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
                 aSegment.Command = 2;
                 aSegment.Count = 3;
-                aSegmentSeq [10] = aSegment;
+                aSegmentSeq [9] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
                 aSegment.Command = 6;
                 aSegment.Count = 0;
-                aSegmentSeq [11] = aSegment;
+                aSegmentSeq [10] = aSegment;
             }
             {
                 EnhancedCustomShapeSegment aSegment;
                 aSegment.Command = 5;
                 aSegment.Count = 0;
-                aSegmentSeq [12] = aSegment;
+                aSegmentSeq [11] = aSegment;
             }
             aPropSequence [1].Value = makeAny (aSegmentSeq);
         }
