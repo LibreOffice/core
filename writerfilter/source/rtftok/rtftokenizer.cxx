b5d9e500ed8562448498cc1135b130123767134c
Miklos Vajna <vmiklos@frugalware.org>
2012-01-11 02:06:35 +0100

    microopt, avoid unnecessary call to Tell()

--- writerfilter/source/rtftok/rtftokenizer.cxx
+++ writerfilter/source/rtftok/rtftokenizer.cxx
@@ -87,8 +87,8 @@ int RTFTokenizer::resolveParse()
     {
         //SAL_INFO("writerfilter", OSL_THIS_FUNC << ": parsing character '" << ch << "'");
 
-        sal_uInt32 nCurrentPos = Strm().Tell();
-        if (m_xStatusIndicator.is() && nCurrentPos > (nLastPos + nPercentSize))
+        sal_uInt32 nCurrentPos;
+        if (m_xStatusIndicator.is() && (nCurrentPos = Strm().Tell()) > (nLastPos + nPercentSize))
             m_xStatusIndicator->setValue(nLastPos = nCurrentPos);
 
         if (m_rImport.getGroup() < 0)
