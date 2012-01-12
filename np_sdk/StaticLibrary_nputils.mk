842523e46f347680ad2648c628c63374524135cd
Matúš Kukan <matus.kukan@gmail.com>
2012-01-12 09:43:26 +0100

    np_sdk: add missing dependency

--- np_sdk/StaticLibrary_nputils.mk
+++ np_sdk/StaticLibrary_nputils.mk
@@ -27,6 +27,8 @@
 
 $(eval $(call gb_StaticLibrary_StaticLibrary,nputils))
 
+$(eval $(call gb_StaticLibrary_add_package_headers,nputils,np_sdk_inc))
+
 $(eval $(call gb_StaticLibrary_use_externals,nputils,\
 	mozilla_headers \
 ))
