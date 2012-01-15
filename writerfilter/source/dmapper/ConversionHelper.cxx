1f4edb5dedd265b5ba155d34df5a802f36d158b4
David Tardon <dtardon@redhat.com>
2012-01-15 18:44:52 +0100

    WaE: narrowing conversion from unsigned int to sal_Int32

--- writerfilter/source/dmapper/ConversionHelper.cxx
+++ writerfilter/source/dmapper/ConversionHelper.cxx
@@ -131,7 +131,9 @@ void MakeBorderLine( sal_Int32 nLineThickness,   sal_Int32 nLineType,
 {
     static const sal_Int32 aBorderDefColor[] =
     {
-        COL_AUTO, COL_BLACK, COL_LIGHTBLUE, COL_LIGHTCYAN, COL_LIGHTGREEN,
+        // The first item means automatic color (COL_AUTO), but we
+        // do not use it anyway (see the next statement) .-)
+        0, COL_BLACK, COL_LIGHTBLUE, COL_LIGHTCYAN, COL_LIGHTGREEN,
         COL_LIGHTMAGENTA, COL_LIGHTRED, COL_YELLOW, COL_WHITE, COL_BLUE,
         COL_CYAN, COL_GREEN, COL_MAGENTA, COL_RED, COL_BROWN, COL_GRAY,
         COL_LIGHTGRAY
