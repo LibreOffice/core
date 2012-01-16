69d91300547f6ed45a9a499ffae71c2c0f91c441
Stephan Bergmann <sbergman@redhat.com>
2012-01-16 21:42:09 +0100

    WaE signed/unsigned comparison.

--- xmloff/source/text/txtparae.cxx
+++ xmloff/source/text/txtparae.cxx
@@ -856,7 +856,7 @@ void XMLTextParagraphExport::exportListChange(
 
         if ( nListLevelsToBeClosed > 0 &&
              pListElements &&
-             pListElements->size() >= ( 2 * nListLevelsToBeClosed ) )
+             pListElements->size() >= ( 2 * sal::static_int_cast< sal_uInt16 >(nListLevelsToBeClosed) ) )
         {
             do {
                 for(size_t j = 0; j < 2; ++j)
