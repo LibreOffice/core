e75308f9099e3baebe7b283241e8b2fd799ef9ff
Stephan Bergmann <sbergman@redhat.com>
2012-01-17 08:55:50 +0100

    Avoid compilation problem.

--- sfx2/source/control/dispatch.cxx
+++ sfx2/source/control/dispatch.cxx
@@ -572,7 +572,7 @@ sal_Bool SfxDispatcher::CheckVirtualStack( const SfxShell& rShell, sal_Bool bDee
     SFX_STACK(SfxDispatcher::CheckVirtualStack);
 
     SfxShellStack_Impl aStack( pImp->aStack );
-    for(std::deque<SfxToDo_Impl>::const_reverse_iterator i = pImp->aToDoStack.rbegin(); i != pImp->aToDoStack.rend(); ++i)
+    for(std::deque<SfxToDo_Impl>::reverse_iterator i = pImp->aToDoStack.rbegin(); i != pImp->aToDoStack.rend(); ++i)
     {
         if(i->bPush)
             aStack.Push(static_cast<SfxShell*>(i->pCluster));
