0e163ead17cb1e6675c72a39c32a0bb4d733857a
Stephan Bergmann <sbergman@redhat.com>
2012-01-17 11:20:02 +0100

    Disable CppunitTest_svl_urihelper for now due to bad dependencies.

--- svl/Module_svl.mk
+++ svl/Module_svl.mk
@@ -38,8 +38,9 @@ $(eval $(call gb_Module_add_targets,svl,\
 
 $(eval $(call gb_Module_add_check_targets,svl,\
 	CppunitTest_svl_lngmisc \
-    CppunitTest_svl_urihelper \
 ))
+#TODO: CppunitTest_svl_urihelper depends on ucb, can only be added once svl is
+# in tail build
 
 $(eval $(call gb_Module_add_subsequentcheck_targets,svl,\
     JunitTest_svl_complex \
