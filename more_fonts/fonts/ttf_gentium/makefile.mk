a5ee64167be9db385758dbd84bb65413196a771f
Pedro Giffuni <pfg@apache.org>
2012-01-18 18:41:52 +0000

    Bring back the Gentium Basic fonts. These are under SIL OFL, and have been temptatively classified as Category-B for now but this may have to be revised or eventually reverted.

---
+++ more_fonts/fonts/ttf_gentium/makefile.mk
@@ -0,0 +1,57 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+
+
+PRJ=..$/..
+
+PRJNAME=more_fonts
+TARGET=ttf_gentium
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=gentiumbasic-fonts-1.10
+TARFILE_MD5=35efabc239af896dfb79be7ebdd6e6b9
+TARFILE_ROOTDIR=gentiumbasic-fonts-1.10
+
+PATCH_FILES=
+
+
+CONFIGURE_DIR=
+CONFIGURE_ACTION=
+
+BUILD_DIR=
+BUILD_ACTION=
+BUILD_FLAGS=
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+
+.IF "$(WITH_FONTS)"!="NO"
+.INCLUDE : tg_ext.mk
+.ENDIF
+
