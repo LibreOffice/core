fe4491ef1a4a695470406bfaeb09930b9df17e3e
Pedro Giffuni <pfg@apache.org>
2012-01-19 20:26:07 +0000

    Bring in Google Chrome OS fonts.
    Tinos, Arimo and Cousine are replacements for the most common fonts
    available on commercial Operating Systems. They are available under
    the SIL OpenFont license.

---
+++ more_fonts/fonts/ttf_croscore/makefile.mk
@@ -0,0 +1,54 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+PRJ=..$/..
+
+PRJNAME=more_fonts
+TARGET=ttf_croscore
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=croscorefonts-1.21.0
+TARFILE_MD5=61f59e4110781cbe66b46449eadac231
+TARFILE_ROOTDIR=.
+
+PATCH_FILES=
+
+CONFIGURE_DIR=
+CONFIGURE_ACTION=
+
+BUILD_DIR=
+BUILD_ACTION=
+BUILD_FLAGS=
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+
+.IF "$(WITH_FONTS)"!="NO"
+.INCLUDE : tg_ext.mk
+.ENDIF
+
