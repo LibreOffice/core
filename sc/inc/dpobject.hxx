a0dc477bfa4460a3091e533eed57fae93f481d5a
Kohei Yoshida <kohei.yoshida@suse.com>
2012-01-23 16:34:15 -0500

    Fix refresh problem on pivot tables whose data cache have not been created.
    Signed-off-by: Petr Mladek <pmladek@suse.cz>

--- sc/inc/dpobject.hxx
+++ sc/inc/dpobject.hxx
@@ -330,6 +330,7 @@ public:
         ScDocument* mpDoc;
     public:
         DBCaches(ScDocument* pDoc);
+        bool hasCache(sal_Int32 nSdbType, const rtl::OUString& rDBName, const rtl::OUString& rCommand) const;
         const ScDPCache* getCache(sal_Int32 nSdbType, const ::rtl::OUString& rDBName, const ::rtl::OUString& rCommand);
         size_t size() const;
 
@@ -385,6 +386,12 @@ public:
 private:
     /** Only to be called from ScDPCache::RemoveReference(). */
     void RemoveCache(const ScDPCache* pCache);
+
+    void GetAllTables(const ScRange& rSrcRange, std::set<ScDPObject*>& rRefs) const;
+    void GetAllTables(const rtl::OUString& rSrcName, std::set<ScDPObject*>& rRefs) const;
+    void GetAllTables(
+        sal_Int32 nSdbType, const ::rtl::OUString& rDBName, const ::rtl::OUString& rCommand,
+        std::set<ScDPObject*>& rRefs) const;
 
 private:
     typedef ::boost::ptr_vector<ScDPObject> TablesType;
