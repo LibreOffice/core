00856ee8107fa2d30e324bd6e5bcf3549c1e56d3
Caolán McNamara <caolanm@redhat.com>
2012-01-25 13:02:41 +0000

    callcatcher: update list

--- autodoc/source/parser/kernel/x_parse.cxx
+++ autodoc/source/parser/kernel/x_parse.cxx
@@ -27,68 +27,7 @@
  ************************************************************************/
 
 #include <precomp.h>
-#include <x_parse.hxx>
-
-// NOT FULLY DECLARED SERVICES
-
-
-
-X_Parser::X_Parser( E_Event             i_eEvent,
-                    const char *        i_sObject,
-                    const String &      i_sCausingFile_FullPath,
-                    uintt               i_nCausingLineNr  )
-    :   eEvent(i_eEvent),
-        sObject(i_sObject),
-        sCausingFile_FullPath(i_sCausingFile_FullPath),
-        nCausingLineNr(i_nCausingLineNr)
-{
-}
-
-X_Parser::~X_Parser()
-{
-}
-
-X_Parser::E_Event
-X_Parser::GetEvent() const
-{
-     return eEvent;
-}
-
-void
-X_Parser::GetInfo( std::ostream & o_rOutputMedium ) const
-{
-    o_rOutputMedium << "Error in file "
-                    << sCausingFile_FullPath
-                    << " in line "
-                    << nCausingLineNr
-                    << ": ";
-
-
-    switch (eEvent)
-    {
-        case x_InvalidChar:
-            o_rOutputMedium << "Unknown character '"
-                            << sObject
-                            << "'";
-            break;
-        case x_UnexpectedToken:
-            o_rOutputMedium << "Unexpected token \""
-                            << sObject
-                            << "\"";
-            break;
-        case x_UnexpectedEOF:
-            o_rOutputMedium << "Unexpected end of file.";
-            break;
-        case x_UnspecifiedSyntaxError:
-            o_rOutputMedium << "Unspecified syntax problem in file.";
-            break;
-        case x_Any:
-        default:
-            o_rOutputMedium << "Unspecified parsing exception.";
-    }   // end switch
-    o_rOutputMedium << Endl();
-}
-
+#include <autodoc/x_parsing.hxx>
 
 std::ostream &
 operator<<( std::ostream &                  o_rOut,
