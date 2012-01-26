2461c1d1deb589930c9078bc4728c6b9120b9a8b
Miklos Vajna <vmiklos@frugalware.org>
2012-01-26 17:27:03 +0100

    solenv: handle error code from ./g
    This avoids "Cannot find the git binary! Is git installed and is in
    PATH?" strings in the about dialog when building from a tarball.
    (cherry picked from commit 439e5279c5d334f849a6ab7b51bad1bb12f13aae)
    
    Conflicts:
    
    	solenv/bin/modules/installer/scriptitems.pm
    (cherry picked from commit 9abb0b6bbe2de65988065f78354ccf5ebde4a7ae)
    
    Signed-off-by: Caolán McNamara <caolanm@redhat.com>
    Signed-off-by: Petr Mladek <pmladek@suse.cz>
    Signed-off-by: Eike Rathke <erack@redhat.com>

--- solenv/bin/modules/installer/scriptitems.pm
+++ solenv/bin/modules/installer/scriptitems.pm
@@ -816,7 +816,7 @@ sub replace_setup_variables
     if ( $localbuild =~ /^\s*(\w+?)(\d+)\s*$/ ) { $localbuild = $2; }   # using "680" instead of "src680"
 
     my $buildidstring = `$ENV{'SRC_ROOT'}/g -s log -n 1 --pretty=format:%h-`;
-    if (!$buildidstring) {
+    if ($? || !$buildidstring) {
         $buildidstring = $localbuild . $localminor . "(Build:" . $installer::globals::buildid . ")";
     }
     else {
