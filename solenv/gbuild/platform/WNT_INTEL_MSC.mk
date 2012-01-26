ed3122a8cbedbab0e22513efd7274e4a10b43dcc
Norbert Thiebaud <nthiebaud@gmail.com>
2012-01-26 02:31:08 -0600

    gb_Helper_convert_native should not introduce superflous spaces
    that broke a case where we had -DMISC=$(call gb_Helper_conver_native,...)
    which transformed in to -DMISC=   d:/foo/bar
    and broke the build

--- solenv/gbuild/platform/WNT_INTEL_MSC.mk
+++ solenv/gbuild/platform/WNT_INTEL_MSC.mk
@@ -251,6 +251,7 @@ gb_Helper_set_ld_path := PATH="$${PATH}:$(OUTDIR)/bin"
 # slashes after file: in a file: URL from being unduplicated by the
 # // -> / substitution.
 define gb_Helper_convert_native
+$(strip \
 $(subst file:!!!,file:///, \
 $(subst //,/, \
 $(subst file:///,file:!!!, \
@@ -258,7 +259,7 @@ $(subst $(REPODIR),$(patsubst %/,%,$(gb_Helper_REPODIR_NATIVE)), \
 $(subst $(SRCDIR),$(gb_Helper_SRCDIR_NATIVE), \
 $(subst $(WORKDIR),$(gb_Helper_WORKDIR_NATIVE), \
 $(subst $(OUTDIR),$(gb_Helper_OUTDIR_NATIVE), \
-$(1))))))))
+$(1)))))))))
 endef
 
 # YaccTarget class
