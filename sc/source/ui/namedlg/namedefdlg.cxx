b15d3b50ba468defd3109e5ec0aef331e222cb5e
Stefan Knorr (astron) <heinzlesspam@gmail.com>
2012-01-26 14:36:16 +0100

    Make named ranges dialog more consistent, localizable (I+II+III)
    Signed-off-by: Petr Mladek <pmladek@suse.cz>

--- sc/source/ui/namedlg/namedefdlg.cxx
+++ sc/source/ui/namedlg/namedefdlg.cxx
@@ -345,7 +345,7 @@ void ScNameDefDlg::MorePushed()
     Size nSize = GetSizePixel();
 
     //depending on the state of the button, move all elements below up/down
-    long nPixel = 75;
+    long nPixel = 65;
     if (!maBtnMore.GetState())
     {
         nPixel *= -1;
