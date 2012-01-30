b446a1fb30b5a7097824acfe6bbbb143f4284b03
Andreas Schierl <openoffice@schierla.de>
2012-01-30 23:34:56 +0100

    ClrContourCache for SwAnchoredDrawObjects

--- sw/source/core/layout/pagechg.cxx
+++ sw/source/core/layout/pagechg.cxx
@@ -2035,6 +2035,9 @@ void lcl_MoveAllLowerObjs( SwFrm* pFrm, const Point& rOffset )
             const Point aNewAnchorPos( ( aCurrAnchorPos + rOffset ) );
             pAnchoredDrawObj->DrawObj()->SetAnchorPos( aNewAnchorPos );
             pAnchoredDrawObj->SetLastObjRect( pAnchoredDrawObj->GetObjRect().SVRect() );
+
+            // clear contour cache
+            ClrContourCache( pAnchoredDrawObj->GetDrawObj() );
         }
         // #i92511#
         // cache for object rectangle inclusive spaces has to be invalidated.
