48453f3d5d8ba0429d814552c7d9c48a595d539e
David Tardon <dtardon@redhat.com>
2012-02-08 15:46:17 +0100

    mark parameter as unused

--- canvas/source/vcl/spritecanvashelper.cxx
+++ canvas/source/vcl/spritecanvashelper.cxx
@@ -486,11 +486,9 @@ namespace vclcanvas
                                         _1 ) );
     }
 
-    void SpriteCanvasHelper::opaqueUpdate( const ::basegfx::B2DRange&                          rTotalArea,
+    void SpriteCanvasHelper::opaqueUpdate( SAL_UNUSED_PARAMETER const ::basegfx::B2DRange& rTotalArea ,
                                            const ::std::vector< ::canvas::Sprite::Reference >& rSortedUpdateSprites )
     {
-        (void)rTotalArea;
-
         ENSURE_OR_THROW( mpOwningSpriteCanvas &&
                          mpOwningSpriteCanvas->getBackBuffer() &&
                          mpOwningSpriteCanvas->getFrontBuffer(),
