9af79da1347910ea0e75ae9e218b0a7a4745fd9c
Pedro Giffuni <pfg@apache.org>
2012-02-12 20:39:34 +0000

    Conditionally bring in the StaX API 1.2.0
    This is only required for Java5 installations when
    not using an external Saxon-B distribution, so it's
    unlikely we will include it in a binary package.
    
    Previous version had issues (see XMLBEANS-308)
    and is not being shipped in othe Apache Projects.
    
    The new version is under an Apache License 2.0.

--- stax/makefile.mk
+++ stax/makefile.mk
@@ -45,19 +45,19 @@ all:
 
 .IF "$(BUILD_STAX)" != "YES"
 
-$(OUT)$/class$/jsr173_1.0_api.jar : $(PRJ)$/download$/jsr173_1.0_api.jar
+$(OUT)$/class$/stax-1.2.0.jar : $(PRJ)$/download$/stax-1.2.0.jar
     +$(COPY) $< $@
 
 .ELSE 			#  "$(BUILD_STAX)" != "YES"
 
 # --- Files --------------------------------------------------------
-TARFILE_NAME=stax-api-1.0-2-sources
-TARFILE_MD5=8294d6c42e3553229af9934c5c0ed997
+TARFILE_NAME=stax-src-1.2.0
+TARFILE_MD5=980143f96b3f6ce45d2e4947da21a5e9
 TARFILE_ROOTDIR=src
 
-BUILD_ACTION=$(JAVAC) javax$/xml$/stream$/*.java && jar -cf jsr173_1.0_api.jar javax$/xml$/stream$/*.class javax$/xml$/stream$/events$/*.class javax$/xml$/stream$/util$/*.class
+BUILD_ACTION=$(JAVAC) javax$/xml$/stream$/*.java && jar -cf stax-1.2.0.jar javax$/xml$/stream$/*.class javax$/xml$/stream$/events$/*.class javax$/xml$/stream$/util$/*.class
 
-OUT2CLASS=jsr173_1.0_api.jar
+OUT2CLASS=stax-1.2.0.jar
 .ENDIF			#  "$(BUILD_STAX)" != "YES"
 
 .ELSE			# $(SOLAR_JAVA)!= ""
