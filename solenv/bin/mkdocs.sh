f9d2a8643548d6bc138f8b36a8fff95d658d5bc1
Thorsten Behrens <tbehrens@suse.com>
2012-02-14 08:50:26 +0100

    Remove accidental comment

--- solenv/bin/mkdocs.sh
+++ solenv/bin/mkdocs.sh
@@ -177,7 +177,7 @@ do
   echo "Ref-Tags:   $DOXYGEN_REF_TAGFILES"
   echo "Title:      $DOXYGEN_PROJECTNAME"
 
-  #nice -15 doxygen "$DOXYGEN_CFG" || exit 1
+  nice -15 doxygen "$DOXYGEN_CFG" || exit 1
 
   # setup referenced tagfiles for next round
   DOXYGEN_REF_TAGFILES="$DOXYGEN_REF_TAGFILES $DOXYGEN_OUR_TAGFILE=$BASE_URL/$PROJECT/html"
