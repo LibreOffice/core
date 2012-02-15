715ac8c1b402e33930317a69bbdd61ad88133740
Stephan Bergmann <sbergman@redhat.com>
2012-02-15 21:55:57 +0100

    Fixed some more apparent misuses of RTL_CONSTASCII_USTRINGPARAM

--- svtools/source/misc/imap2.cxx
+++ svtools/source/misc/imap2.cxx
@@ -536,8 +536,8 @@ sal_uLong ImageMap::ImpDetectFormat( SvStream& rIStm )
             aStr = aStr.toAsciiLowerCase();
 
             if ( (aStr.indexOfL(RTL_CONSTASCII_STRINGPARAM("rect")) != -1) ||
-                 (aStr.indexOfL(RTL_CONSTASCII_USTRINGPARAM("circ")) != -1) ||
-                 (aStr.indexOfL(RTL_CONSTASCII_USTRINGPARAM("poly")) != -1) )
+                 (aStr.indexOfL(RTL_CONSTASCII_STRINGPARAM("circ")) != -1) ||
+                 (aStr.indexOfL(RTL_CONSTASCII_STRINGPARAM("poly")) != -1) )
             {
                 if ( ( aStr.indexOf('(') != -1 ) &&
                      ( aStr.indexOf(')') != -1 ) )
