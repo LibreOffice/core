19ad1f959a9ac01e602be2e3967736cee19d0dd5
Thomas Arnhold <thomas@arnhold.org>
2012-02-18 20:45:48 +0100

    np_sdk: remove unused macros

--- np_sdk/inc/npapi.h
+++ np_sdk/inc/npapi.h
@@ -739,13 +739,6 @@ enum NPEventType {
 #define NPRES_USER_BREAK   (NPRES_BASE + 2)
 
 /*
- * Don't use these obsolete error codes any more.
- */
-#define NP_NOERR  NP_NOERR_is_obsolete_use_NPERR_NO_ERROR
-#define NP_EINVAL NP_EINVAL_is_obsolete_use_NPERR_GENERIC_ERROR
-#define NP_EABORT NP_EABORT_is_obsolete_use_NPRES_USER_BREAK
-
-/*
  * Version feature information
  */
 #define NPVERS_HAS_STREAMOUTPUT             8
