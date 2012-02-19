1cc064f1a414446791903bb84c9f166cd3185e14
Matteo Casalin <matteo.casalin@gmx.com>
2012-02-19 18:02:12 +0100

    ValueSet: fix safety check - failure value of GetItemId is 0, not -1

--- svtools/source/control/valueacc.cxx
+++ svtools/source/control/valueacc.cxx
@@ -436,7 +436,7 @@ uno::Reference< accessibility::XAccessible > SAL_CALL ValueSetAcc::getAccessible
     const sal_uInt16                                    nItemId = mpParent->GetItemId( Point( aPoint.X, aPoint.Y ) );
     uno::Reference< accessibility::XAccessible >    xRet;
 
-    if( ((sal_uInt16)-1) != nItemId )
+    if ( nItemId )
     {
         const size_t nItemPos = mpParent->GetItemPos( nItemId );
 
