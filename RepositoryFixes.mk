31826318dfd2f4c38d6bf34f957fe51edc0a2b14
Tor Lillqvist <tlillqvist@suse.com>
2012-02-20 11:15:02 +0200

    Shared libraries must in practice be called lib*.so on Android
    Otherwise the package creating code (part of the SDK) won't include
    them and/or the package installation code (on the OS itself) won't
    unpack them. (They just silently skip the file.)

--- RepositoryFixes.mk
+++ RepositoryFixes.mk
@@ -40,7 +40,9 @@ gb_Library_FILENAMES := $(patsubst log_uno:liblog_uno%,log_uno:liblog_uno_uno%,$
 gb_Library_FILENAMES := $(patsubst purpenvhelper:libpurpen%,purpenvhelper:libuno_purpen%,$(gb_Library_FILENAMES))
 gb_Library_FILENAMES := $(patsubst salhelper:libsalhelper%,salhelper:libuno_salhelper%,$(gb_Library_FILENAMES))
 gb_Library_FILENAMES := $(patsubst ucbhelper:libucbhelper%,ucbhelper:libucbhelper4%,$(gb_Library_FILENAMES))
+ifneq ($(OS),ANDROID)
 gb_Library_FILENAMES := $(patsubst unoexceptionprotector:libuno%,unoexceptionprotector:uno%,$(gb_Library_FILENAMES))
+endif
 gb_Library_FILENAMES := $(patsubst unsafe_uno:libunsafe_uno%,unsafe_uno:libunsafe_uno_uno%,$(gb_Library_FILENAMES))
 endif
 
