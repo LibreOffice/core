baccf076f4a5a9986ad94f8477996fd44648b6d6
Noel Power <noel.power@novell.com>
2012-02-21 15:13:08 +0000

    Cope with more recent cygwin's process.h
    Signed-off-by: Michael Meeks <michael.meeks@suse.com>

--- dmake/unix/runargv.c
+++ dmake/unix/runargv.c
@@ -125,6 +125,9 @@ _finished_child(pid, status) [unix/runargv] handles the finished child. If
 #endif
 
 #if __CYGWIN__ && ENABLE_SPAWN
+#if HAVE_CYGWIN_PROCESS_H
+#  include <cygwin/process.h>
+#else
 #  include <process.h>
 #endif
 
