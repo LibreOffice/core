da9baeb8c4dad67374b7f9b67de92e1a72d518d0
Petr Mladek <pmladek@suse.cz>
2012-02-21 16:36:24 +0100

    Revert incomplete "Cope with more recent cygwin's process.h"
    This reverts commit baccf076f4a5a9986ad94f8477996fd44648b6d6.

--- dmake/unix/runargv.c
+++ dmake/unix/runargv.c
@@ -125,9 +125,6 @@ _finished_child(pid, status) [unix/runargv] handles the finished child. If
 #endif
 
 #if __CYGWIN__ && ENABLE_SPAWN
-#if HAVE_CYGWIN_PROCESS_H
-#  include <cygwin/process.h>
-#else
 #  include <process.h>
 #endif
 
