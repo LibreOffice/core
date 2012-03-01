2fb1fe6c24a62b954f3a4bbdb5628d94269df946
David Tardon <dtardon@redhat.com>
2012-03-01 13:00:09 +0100

    get redland built with mingw
    Do not ask me how or why this works. There are things man was not meant
    to know.

--- redland/raptor/makefile.mk
+++ redland/raptor/makefile.mk
@@ -102,6 +102,14 @@ BUILD_ACTION=dmake
 BUILD_DIR=$(CONFIGURE_DIR)$/src
 .ENDIF
 
+# Hack to get librdf.dll linked on mingw. I am already wearied of
+# fighting libtool's pseudo-intelligence, so I am just going to go with
+# the flow here.
+ALL : $(OUT)/lib/libraptor.la
+
+$(OUT)/lib/libraptor.la : $(MISC)/build/so_built_so_raptor
+	$(SED) "/^dlname/s@='@='../bin/@" $(MISC)/build/$(TARFILE_NAME)/src/libraptor.la > $@
+
 .ELSE # "WNT"
 
 .IF "$(OS)$(COM)"=="LINUXGCC" || "$(OS)$(COM)"=="FREEBSDGCC"
