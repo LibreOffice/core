b8b088daf8c993e90b86a632c1077ae324414a84
David Tardon <dtardon@redhat.com>
2012-03-01 10:21:36 +0100

    get raptor built with mingw
    I _really_ love how libtool, when called without -no-undefined, silently
    falls back to building a static lib (AFAICS without even trying to check
    if there really will be any undefined symbols in the resulting dll).
    Thank you, libtool author! I cannot express how much I enjoyed the hours
    I spent figuring out what is happening there!

--- redland/raptor/makefile.mk
+++ redland/raptor/makefile.mk
@@ -83,14 +83,16 @@ raptor_CC=$(CC) -mthreads
 raptor_CC+=-shared-libgcc
 .ENDIF
 raptor_LIBS=
-.IF "$(MINGW_SHARED_GXXLIB)"=="YES"
-raptor_LIBS+=$(MINGW_SHARED_LIBSTDCPP)
+
+raptor_LDFLAGS=-Wl,--no-undefined -Wl,--enable-runtime-pseudo-reloc-v2,--export-all-symbols
+.IF "$(ILIB)"!=""
+raptor_LDFLAGS+= -L$(ILIB:s/;/ -L/)
 .ENDIF
 
 CONFIGURE_DIR=
 CONFIGURE_ACTION=.$/configure
 # do not enable grddl parser (#i93768#)
-CONFIGURE_FLAGS=--disable-static --disable-gtk-doc --with-openssl-digests --with-xml-parser=libxml --enable-parsers="rdfxml ntriples turtle trig guess rss-tag-soup" --without-bdb --without-sqlite --without-mysql --without-postgresql --without-threestore       --with-regex-library=posix --with-decimal=none --with-www=xml --build=i586-pc-mingw32 --host=i586-pc-mingw32 lt_cv_cc_dll_switch="-shared" CC="$(raptor_CC)" CPPFLAGS="-nostdinc $(INCLUDE)" LDFLAGS="-no-undefined -Wl,--enable-runtime-pseudo-reloc-v2,--export-all-symbols  -L$(ILIB:s/;/ -L/)" LIBS="$(raptor_LIBS)" OBJDUMP="$(WRAPCMD) objdump" LIBXML2LIB="$(LIBXML2LIB)" XSLTLIB="$(XSLTLIB)"
+CONFIGURE_FLAGS=--disable-static --enable-shared --disable-gtk-doc --with-openssl-digests --with-xml-parser=libxml --enable-parsers="rdfxml ntriples turtle trig guess rss-tag-soup" --without-bdb --without-sqlite --without-mysql --without-postgresql --without-threestore       --with-regex-library=posix --with-decimal=none --with-www=xml --build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) --target=$(HOST_PLATFORM) lt_cv_cc_dll_switch="-shared" CC="$(raptor_CC)" CPPFLAGS="$(INCLUDE)" LDFLAGS="$(raptor_LDFLAGS)" LIBS="$(raptor_LIBS)" OBJDUMP="$(WRAPCMD) $(HOST_PLATFORM)-objdump" LIBXML2LIB="$(LIBXML2LIB)" XSLTLIB="$(XSLTLIB)"
 BUILD_ACTION=$(GNUMAKE)
 BUILD_FLAGS+= -j$(EXTMAXPROCESS)
 BUILD_DIR=$(CONFIGURE_DIR)
