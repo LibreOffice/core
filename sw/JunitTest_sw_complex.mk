24a5db94da34fc40c713d6534b5f741dfee50e44
Michael Stahl <mstahl@redhat.com>
2012-03-05 12:20:48 +0100

    fdo#39694, fdo#42073: add junit test for these (cherry picked from commit 870e5a8e8251967b513b4255231ee9665b085431)
    Signed-off-by: Miklos Vajna <vmiklos@suse.cz>

--- sw/JunitTest_sw_complex.mk
+++ sw/JunitTest_sw_complex.mk
@@ -40,6 +40,7 @@ $(eval $(call gb_JunitTest_add_sourcefiles,sw_complex,\
     sw/qa/complex/writer/CheckBookmarks \
     sw/qa/complex/writer/CheckCrossReferences \
     sw/qa/complex/writer/CheckFlies \
+    sw/qa/complex/writer/CheckFields \
     sw/qa/complex/writer/CheckIndexedPropertyValues \
     sw/qa/complex/writer/CheckNamedPropertyValues \
     sw/qa/complex/writer/LoadSaveTest \
@@ -60,6 +61,7 @@ $(eval $(call gb_JunitTest_add_classes,sw_complex,\
     complex.writer.CheckBookmarks \
     complex.checkColor.CheckChangeColor \
     complex.writer.CheckCrossReferences \
+    complex.writer.CheckFields\
     complex.writer.CheckFlies \
     complex.writer.CheckIndexedPropertyValues \
     complex.writer.CheckNamedPropertyValues \
