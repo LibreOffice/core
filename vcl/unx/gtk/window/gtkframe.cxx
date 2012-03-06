f9a4af41ca574760dbd22c8207661fabb6823e3b
Caolán McNamara <caolanm@redhat.com>
2012-03-06 09:45:08 +0000

    silence SolarMutex not locked spew

--- vcl/unx/gtk/window/gtkframe.cxx
+++ vcl/unx/gtk/window/gtkframe.cxx
@@ -3835,6 +3835,7 @@ void GtkSalFrame::IMHandler::signalIMCommit( GtkIMContext* CONTEXT_ARG, gchar* p
 {
     GtkSalFrame::IMHandler* pThis = (GtkSalFrame::IMHandler*)im_handler;
 
+    SolarMutexGuard aGuard;
     vcl::DeletionListener aDel( pThis->m_pFrame );
     // open a block that will end the GTK_YIELD_GRAB before calling preedit changed again
     {
@@ -4003,6 +4004,7 @@ void GtkSalFrame::IMHandler::signalIMPreeditChanged( GtkIMContext*, gpointer im_
 
     GTK_YIELD_GRAB();
 
+    SolarMutexGuard aGuard;
     vcl::DeletionListener aDel( pThis->m_pFrame );
 
     pThis->m_pFrame->CallCallback( SALEVENT_EXTTEXTINPUT, (void*)&pThis->m_aInputEvent);
@@ -4023,6 +4025,7 @@ void GtkSalFrame::IMHandler::signalIMPreeditEnd( GtkIMContext*, gpointer im_hand
 
     pThis->m_bPreeditJustChanged = true;
 
+    SolarMutexGuard aGuard;
     vcl::DeletionListener aDel( pThis->m_pFrame );
     pThis->doCallEndExtTextInput();
     if( ! aDel.isDeleted() )
