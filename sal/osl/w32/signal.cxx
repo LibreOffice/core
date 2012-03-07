e1eae3267b7775be0cbe719e3a055de3ee343f92
David Tardon <dtardon@redhat.com>
2012-03-07 13:06:12 +0100

    WaE: declaration of '...' shadows a global declaration
    pfn_ADDEREXCLUDEDAPPLICATIONW has been defined in mingw-w64 headers
    since the very beginning. I suppose this was only relevant for mingw32,
    which we do not support anymore.

--- sal/osl/w32/signal.cxx
+++ sal/osl/w32/signal.cxx
@@ -68,9 +68,6 @@ static sal_Bool InitSignal(void)
     hFaultRep = LoadLibrary( "faultrep.dll" );
     if ( hFaultRep )
     {
-#ifdef __MINGW32__
-typedef BOOL (WINAPI *pfn_ADDEREXCLUDEDAPPLICATIONW)(LPCWSTR);
-#endif
         pfn_ADDEREXCLUDEDAPPLICATIONW       pfn = (pfn_ADDEREXCLUDEDAPPLICATIONW)GetProcAddress( hFaultRep, "AddERExcludedApplicationW" );
         if ( pfn )
             pfn( L"SOFFICE.EXE" );
