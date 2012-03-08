706cac767c90281b74fbc27a134b022d27e2ce21
Szabolcs Dezsi <dezsiszabi@hotmail.com>
2012-03-08 14:52:14 +0100

    fdo#43424: Comparing empty document with attached one crashes LO

--- sw/source/core/doc/doccomp.cxx
+++ sw/source/core/doc/doccomp.cxx
@@ -1536,14 +1536,17 @@ void SwCompareData::ShowDelete( const CompareData& rData, sal_uLong nStt,
         ((SwCompareLine*)rData.GetLine( nEnd-1 ))->GetEndNode(), 1 );
 
     sal_uInt16 nOffset = 0;
-    const CompareLine* pLine;
-    if( GetLineCount() == nInsPos )
+    const CompareLine* pLine = 0;
+    if( nInsPos >= 1 )
     {
-        pLine = GetLine( nInsPos-1 );
-        nOffset = 1;
+        if( GetLineCount() == nInsPos )
+        {
+            pLine = GetLine( nInsPos-1 );
+            nOffset = 1;
+        }
+        else
+            pLine = GetLine( nInsPos );
     }
-    else
-        pLine = GetLine( nInsPos );
 
     const SwNode* pLineNd;
     if( pLine )
