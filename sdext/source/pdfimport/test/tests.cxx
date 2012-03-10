df6097ab4fc7008997caa93fb6674b1e2599e227
Luboš Luňák <l.lunak@suse.cz>
2012-03-10 22:27:38 +0100

    operator must be in template or argument namespace
    clang has more strict (and probably more spec compliant) lookup

--- sdext/source/pdfimport/test/tests.cxx
+++ sdext/source/pdfimport/test/tests.cxx
@@ -87,15 +87,18 @@ using namespace ::pdfparse;
 using namespace ::pdfi;
 using namespace ::com::sun::star;
 
-namespace
+namespace rtl
 {
-
     template< typename charT, typename traits > std::basic_ostream<charT, traits> &
         operator <<(
                 std::basic_ostream<charT, traits> & stream, rtl::OString const & string)
         {
             return stream << string.getStr();
         }
+}
+
+namespace
+{
 
     class TestSink : public ContentSink
     {
