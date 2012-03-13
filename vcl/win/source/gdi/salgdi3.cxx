3beb6f66cbead4526ff74e212cd6af1f837f665b
Caolán McNamara <caolanm@redhat.com>
2012-03-13 10:44:46 +0000

    missing namespace

--- vcl/win/source/gdi/salgdi3.cxx
+++ vcl/win/source/gdi/salgdi3.cxx
@@ -551,7 +551,8 @@ namespace
         const com::sun::star::lang::Locale& rLocale )
     {
         // get the default font for a specified locale
-        const DefaultFontConfiguration& rDefaults = utl::DefaultFontConfiguration::get();
+        const utl::DefaultFontConfiguration& rDefaults =
+            utl::DefaultFontConfiguration::get();
         const rtl::OUString aDefault = rDefaults.getUserInterfaceFont(rLocale);
         return rDevFontList.ImplFindByTokenNames(aDefault);
     }
