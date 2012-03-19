260ed4ed0317f4f16a675dde48750bc40923bc96
Caolán McNamara <caolanm@redhat.com>
2012-03-19 11:52:27 +0000

    map of kind+name -> new-name

--- xmloff/source/core/i18nmap.cxx
+++ xmloff/source/core/i18nmap.cxx
@@ -31,96 +31,22 @@
 #include <svl/svarray.hxx>
 #include "xmloff/i18nmap.hxx"
 
-using ::rtl::OUString;
-
-class SvI18NMapEntry_Impl
+bool SvI18NMap::Add( sal_uInt16 nKind, const rtl::OUString& rName,
+                     const rtl::OUString& rNewName )
 {
-    sal_uInt16          nKind;
-    OUString        aName;
-    OUString        aNewName;
-
-public:
-
-    const OUString& GetNewName() const { return aNewName; }
-
-    SvI18NMapEntry_Impl( sal_uInt16 nKnd, const OUString& rName,
-                         const OUString& rNewName ) :
-        nKind( nKnd ),
-        aName( rName ),
-        aNewName( rNewName )
-    {}
-
-    SvI18NMapEntry_Impl( sal_uInt16 nKnd, const OUString& rName ) :
-        nKind( nKnd ),
-        aName( rName )
-    {}
-
-    sal_Bool operator==( const SvI18NMapEntry_Impl& r ) const
-    {
-        return nKind == r.nKind &&
-               aName == r.aName;
-    }
-
-    sal_Bool operator<( const SvI18NMapEntry_Impl& r ) const
-    {
-        return nKind < r.nKind ||
-               ( nKind == r.nKind &&
-                 aName < r.aName);
-    }
-};
-
-typedef SvI18NMapEntry_Impl *SvI18NMapEntry_ImplPtr;
-SV_DECL_PTRARR_SORT_DEL( SvI18NMap_Impl, SvI18NMapEntry_ImplPtr, 20 )
-SV_IMPL_OP_PTRARR_SORT( SvI18NMap_Impl, SvI18NMapEntry_ImplPtr )
-
-// ---------------------------------------------------------------------
-
-SvI18NMapEntry_Impl *SvI18NMap::_Find( sal_uInt16 nKind,
-                                     const OUString& rName ) const
-{
-    SvI18NMapEntry_Impl *pRet = 0;
-    SvI18NMapEntry_Impl aTst( nKind, rName );
-
-    sal_uInt16 nPos;
-    if( pImpl->Seek_Entry( &aTst, &nPos ) )
-    {
-        pRet = (*pImpl)[nPos];
-    }
-
-    return pRet;
+    SvI18NMapEntry_Key aKey(nKind, rName);
+    bool bIsNewInsertion = m_aMap.insert(SvI18NMap_Impl::value_type(aKey, rNewName)).second;
+    DBG_ASSERT( bIsNewInsertion, "SvI18NMap::Add: item registered already" );
+    return bIsNewInsertion;
 }
 
-SvI18NMap::SvI18NMap() :
-    pImpl( 0 )
+const rtl::OUString& SvI18NMap::Get( sal_uInt16 nKind, const rtl::OUString& rName ) const
 {
-    pImpl = new SvI18NMap_Impl;
+    SvI18NMapEntry_Key aKey(nKind, rName);
+    SvI18NMap_Impl::const_iterator aI = m_aMap.find(aKey);
+    if (aI != m_aMap.end())
+        return aI->second;
+    return rName;
 }
-
-SvI18NMap::~SvI18NMap()
-{
-    delete pImpl;
-}
-
-void SvI18NMap::Add( sal_uInt16 nKind, const OUString& rName,
-                     const OUString& rNewName )
-{
-    SvI18NMapEntry_Impl *pEntry = _Find( nKind, rName );
-    DBG_ASSERT( !pEntry, "SvI18NMap::Add: item registered already" );
-    if( !pEntry )
-    {
-        pEntry = new SvI18NMapEntry_Impl( nKind, rName, rNewName );
-        pImpl->Insert( pEntry );
-    }
-}
-
-const OUString& SvI18NMap::Get( sal_uInt16 nKind, const OUString& rName ) const
-{
-    SvI18NMapEntry_Impl *pEntry = _Find( nKind, rName );
-    if( pEntry )
-        return pEntry->GetNewName();
-    else
-        return rName;
-}
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
