14d47f6f6ed9f9e682136cc72d6fe44a9e0c7d9e
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-03-26 16:26:32 -0400

    fdo#46942: Fix a regression caused by List removal.
    3236c2c3af9355157c62f68fcbd429498f7f747b accidentally removed the gallery
    item count which was used to differentiate the gallery symbols from
    normal ones.
    
    Signed-off-by: Stephan Bergmann <sbergman@redhat.com>

--- cui/source/tabpages/tpline.cxx
+++ cui/source/tabpages/tpline.cxx
@@ -1464,6 +1464,7 @@ IMPL_LINK( SvxLineTabPage, MenuCreateHdl_Impl, MenuButton *, pButton )
         String aEmptyStr;
         const String *pUIName = NULL;
         sal_uInt32 i = 0;
+        nNumMenuGalleryItems = aGrfNames.size();
         for(std::vector<String>::iterator it = aGrfNames.begin(); it != aGrfNames.end(); ++it, ++i)
         {
             pUIName = &(*it);
