8627b0a297d6a2b7461bb53da2f434c397cb2603
Ivan Timofeev <timofeev.i.s@gmail.com>
2012-03-27 23:03:03 +0400

    there was Table::Insert, let's use map::insert as a replacement

--- sc/source/core/tool/chartpos.cxx
+++ sc/source/core/tool/chartpos.cxx
@@ -475,7 +475,7 @@ void ScChartPositioner::CreatePositionMap()
             {
                 sal_uLong nKey = it1->first;
                 for (ColumnMap::const_iterator it2 = ++pCols->begin(); it2 != pCols->end(); ++it2 )
-                    (*it2->second)[ nKey ] =  NULL;     // keine Daten
+                    it2->second->insert( RowMap::value_type( nKey, NULL )); // keine Daten
             }
         }
     }
