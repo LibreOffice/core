61d6d08667e03271604007fe602063c08e0e8608
Luboš Luňák <l.lunak@suse.cz>
2012-03-29 16:52:42 +0200

    --headless for hwpfilter test
    Otherwise there's a build dependency missing (and it's apparently
    not needed anyway).

--- hwpfilter/CppunitTest_hwpfilter_test_hwpfilter.mk
+++ hwpfilter/CppunitTest_hwpfilter_test_hwpfilter.mk
@@ -68,6 +68,7 @@ $(eval $(call gb_CppunitTest_add_components,hwpfilter_test_hwpfilter,\
 ))
 
 $(eval $(call gb_CppunitTest_set_args,hwpfilter_test_hwpfilter,\
+    --headless \
     --protector unoexceptionprotector$(gb_Library_DLLEXT) unoexceptionprotector \
     "-env:CONFIGURATION_LAYERS=xcsxcu:$(call gb_CppunitTarget__make_url,$(OUTDIR)/xml/registry)" \
 ))
