8bc439333a970895b0f1dd5f353ac9f8c9031c56
Stephan Bergmann <sbergman@redhat.com>
2012-03-30 17:00:08 +0200

    Updated patch comment

--- berkeleydb/makefile.mk
+++ berkeleydb/makefile.mk
@@ -64,7 +64,9 @@ PATCH_FILES += db-4.7.25.NC-mingw32hack.patch
     # which causes /usr/i686-w64-mingw32/sys-root/mingw/include/_mingw.h to not
     # define _USE_32BIT_TIME_T, so that berkeleydb uses a 64 bit time_t while
     # the rest of LibreOffice uses a 32 bit time_t, which causes problems as
-    # there is e.g. a member time_t timestamp of struct __db in db.h
+    # there is e.g. a member time_t timestamp of struct __db in db.h; see
+    # <http://sourceforge.net/support/tracker.php?aid=3513251> "_mingw.h: size
+    # of time_t depends on _FILE_OFFSET_BITS"
 .ELSE
 PATCH_FILES=\
     db-4.7.25.patch \
