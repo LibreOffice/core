e3cd3c72b20e4e0918e86edf5c9e3c9bd00ae9e3
Michael Stahl <mstahl@redhat.com>
2012-04-04 22:35:08 +0200

    fdo#45562: paint borders in SwFlyFrm::Paint:
    Painting borders of Flys in the heaven layer cannot be done correctly in
    SwRootFrm::Paint, because delaying until then paints over other drawing
    objects that are on top of the frame, so do it in SwFlyFrm::Paint,
    like the old border painting code used to.
    (regression from 804d0a896731629397c5328c13c04a45bc55f459)
    (cherry picked from commit 5913506b2193e93ca2767ab7365ab2e76ed7848f)
    
    Signed-off-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -3031,8 +3031,6 @@ SwRootFrm::Paint(SwRect const& rRect, SwPrintData const*const pPrintData) const
                 if ( bExtraData )
                     pPage->RefreshExtraData( aPaintRect );
 
-                // have to paint frame borders added in heaven layer here...
-                ProcessPrimitives(g_pBorderLines->GetBorderLines_Clear());
                 DELETEZ(g_pBorderLines);
                 pVout->Leave();
 
@@ -3751,12 +3749,28 @@ void SwCellFrm::Paint(SwRect const& rRect, SwPrintData const*const) const
 void MA_FASTCALL lcl_PaintLowerBorders( const SwLayoutFrm *pLay,
                                const SwRect &rRect, const SwPageFrm *pPage );
 
+struct BorderLinesGuard
+{
+    explicit BorderLinesGuard() : m_pBorderLines(g_pBorderLines)
+    {
+        g_pBorderLines = new BorderLines;
+    }
+    ~BorderLinesGuard()
+    {
+        delete g_pBorderLines;
+        g_pBorderLines = m_pBorderLines;
+    }
+private:
+    BorderLines *const m_pBorderLines;
+};
+
 void SwFlyFrm::Paint(SwRect const& rRect, SwPrintData const*const) const
 {
     //wegen der Ueberlappung von Rahmen und Zeichenobjekten muessen die
     //Flys ihre Umrandung (und die der Innenliegenden) direkt ausgeben.
     //z.B. #33066#
     pLines->LockLines(sal_True);
+    BorderLinesGuard blg; // this should not paint borders added from PaintBaBo
 
     SwRect aRect( rRect );
     aRect._Intersection( Frm() );
@@ -3966,6 +3980,8 @@ void SwFlyFrm::Paint(SwRect const& rRect, SwPrintData const*const) const
     // and then unlock other lines.
     pLines->PaintLines( pOut );
     pLines->LockLines( sal_False );
+    // have to paint frame borders added in heaven layer here...
+    ProcessPrimitives(g_pBorderLines->GetBorderLines_Clear());
 
     pOut->Pop();
 
