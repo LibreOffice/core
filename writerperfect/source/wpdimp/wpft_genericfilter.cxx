d77b839868491d2d97081be9592e7860fcdea063
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-04-19 12:00:15 +0200

    Prefix the UNO components in writerperfect

--- writerperfect/source/wpdimp/wpft_genericfilter.cxx
+++ writerperfect/source/wpdimp/wpft_genericfilter.cxx
@@ -40,7 +40,7 @@ using namespace ::com::sun::star::registry;
 
 extern "C"
 {
-    SAL_DLLPUBLIC_EXPORT void *SAL_CALL component_getFactory(
+    SAL_DLLPUBLIC_EXPORT void *SAL_CALL wpft_component_getFactory(
         const sal_Char *pImplName, void *pServiceManager, void * /* pRegistryKey */ )
     {
         void *pRet = 0;
--- writerperfect/source/wpgimp/wpgimport_genericfilter.cxx
+++ writerperfect/source/wpgimp/wpgimport_genericfilter.cxx
@@ -41,7 +41,7 @@ using namespace ::com::sun::star::registry;
 
 extern "C"
 {
-    SAL_DLLPUBLIC_EXPORT void *SAL_CALL component_getFactory(
+    SAL_DLLPUBLIC_EXPORT void *SAL_CALL wpgfilter_component_getFactory(
         const sal_Char *pImplName, void *pServiceManager, void * /* pRegistryKey */ )
     {
         void *pRet = 0;
