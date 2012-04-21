1ac9f91198889cc5cee40ba9ef2b0255183ee195
Tor Lillqvist <tml@iki.fi>
2012-04-22 00:59:45 +0300

    WaE: unused functions

--- l10ntools/source/cfg_yy_wrapper.c
+++ l10ntools/source/cfg_yy_wrapper.c
@@ -2,4 +2,7 @@
 // Helper to suppress warnings in lex generated c code, see #i57362#
 #include "cfg_yy.c"
 
+void (*avoid_unused_yyunput_in_cfg_yy_c)() = yyunput;
+int (*avoid_unused_yy_flex_strlen_in_cfg_yy_c)() = yy_flex_strlen;
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- l10ntools/source/src_yy_wrapper.c
+++ l10ntools/source/src_yy_wrapper.c
@@ -2,4 +2,7 @@
 // Helper to suppress warnings in lex generated c code, see #i57362#
 #include "src_yy.c"
 
+void (*avoid_unused_yyunput_in_src_yy_c)() = yyunput;
+int (*avoid_unused_yy_flex_strlen_in_src_yy_c)() = yy_flex_strlen;
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
