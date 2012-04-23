efc80f71f54d73a50b89721132a9c5668d3da39d
Tor Lillqvist <tml@iki.fi>
2012-04-23 19:02:23 +0300

    Add hopefully helpful comment

--- slideshow/source/engine/OGLTrans/mac/aquaOpenGLView.h
+++ slideshow/source/engine/OGLTrans/mac/aquaOpenGLView.h
@@ -8,6 +8,15 @@
 // the most interesting is there : in OGLTrans, we use an NSView*
 // and subclass the NSView to create an OpenGLView does the trick
 
+// I can't really parse the above
+
+// This is weird code. AquaOpenGLView objects are actually allocated
+// as NSOpenGLView objetcs, and the _openGLContext and _pixelFormat
+// fields are copied from the NSOpenGLView.h header so that it has the
+// same structure as NSOpenGLView . Surely this could be done in a
+// better way? Is this because of some pre-10.4 SDK compatibility,
+// which is irrelevant now?
+
 @interface AquaOpenGLView : NSView
 {
   @private
