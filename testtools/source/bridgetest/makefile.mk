85c2f280ef21780de8aa2aa3c35148afc38f70b7
Stephan Bergmann <sbergman@redhat.com>
2012-04-23 14:18:24 +0200

    Revert "i hope this is fixed now"
    This reverts commit b8044cb828af149a4598b35a4d46d8fe4205ae74.
    
    84dbc4fe2547f8fc341a46d7f000e721c81e63ee fixed the bridge for PPC on Linux, not
    Mac OS X.

--- testtools/source/bridgetest/makefile.mk
+++ testtools/source/bridgetest/makefile.mk
@@ -146,10 +146,14 @@ ALLTAR: \
 runtest : $(DLLDEST)$/uno_types.rdb $(DLLDEST)$/uno_services.rdb makefile.mk \
         $(SHL1TARGETN) $(SHL2TARGETN) $(SHL3TARGETN)
 .IF "$(CROSS_COMPILING)"!="YES"
+.IF "$(COM)$(OS)$(CPU)" == "GCCMACOSXP"
+    @echo "Mac OSX PPC GCC fails this test!, likely broken UNO bridge. Fix me."
+.ELSE
         cd $(DLLDEST) && $(AUGMENT_LIBRARY_PATH) $(SOLARBINDIR)/uno \
         -ro uno_services.rdb -ro uno_types.rdb \
         -s com.sun.star.test.bridge.BridgeTest -- \
         com.sun.star.test.bridge.CppTestObject
+.ENDIF
 .ENDIF
 
 $(DLLDEST)/services.rdb :
