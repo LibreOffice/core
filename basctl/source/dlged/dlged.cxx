aa155d9307bd519a2e615cc8bde29d0d9fb349b6
Tor Lillqvist <tml@iki.fi>
2012-04-23 15:37:51 +0300

    WaE: equality comparison with extraneous parentheses

--- basctl/source/dlged/dlged.cxx
+++ basctl/source/dlged/dlged.cxx
@@ -483,7 +483,7 @@ void DlgEditor::MouseButtonUp( const MouseEvent& rMEvt )
 {
     sal_Bool bRet = pFunc->MouseButtonUp( rMEvt );
 
-    if( (eMode == DLGED_INSERT) )
+    if( eMode == DLGED_INSERT )
         bCreateOK = bRet;
 }
 
