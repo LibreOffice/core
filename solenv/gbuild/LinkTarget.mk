7cc6106badf9bc87ff1d33ed0926795147f52872
David Tardon <dtardon@redhat.com>
2012-05-02 13:44:44 +0200

    let make know about all files created by LinkTarget
    Change-Id: Id8af5f63b7a249949286ed35d26d8f4e71a52742

--- solenv/gbuild/LinkTarget.mk
+++ solenv/gbuild/LinkTarget.mk
@@ -1042,7 +1042,9 @@ $$(call gb_Output_error,\
 endef
 
 define gb_LinkTarget_add_auxtargets
+$(2) :| $(call gb_LinkTarget_get_target,$(1))
 $(call gb_LinkTarget_get_clean_target,$(1)) : AUXTARGETS += $(2)
+
 endef
 
 define gb_LinkTarget__add_internal_headers
