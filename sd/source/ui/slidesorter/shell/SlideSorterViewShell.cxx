LibreOffice/core

commit ec240eafe9b25620383aa54015f5c55e0f64227a

Split construct into bits that depend on showing the window and not
This fixes a horrific a11y problem where Show triggers callbacks
to virtual methods, while the object is not fully constructed,
which means, that the *baseclass* virtual method is called,
not the *derived* virtual method that constructs the a11y wrapper

enable a11y, and run the smoketest with dbg-util before touching
this stuff

sd/source/ui/slidesorter/shell/SlideSorterViewShell.cxx
@@ -135,9 +135,6 @@ SlideSorterViewShell::SlideSorterViewShell (
 {
     meShellType = ST_SLIDE_SORTER;
 
-    SetPool( &GetDoc()->GetPool() );
-    SetUndoManager( GetDoc()->GetDocSh()->GetUndoManager() );
-
     if (pFrameViewArgument != NULL)
         mpFrameView = pFrameViewArgument;
     else
@@ -189,6 +186,11 @@ void SlideSorterViewShell::Initialize (void)
         mpVerticalScrollBar,
         mpScrollBarBox);
     mpView = &mpSlideSorter->GetView();
+
+    ViewShell::doShow();
+
+    SetPool( &GetDoc()->GetPool() );
+    SetUndoManager( GetDoc()->GetDocSh()->GetUndoManager() );
 
     // For accessibility we have to shortly hide the content window.
     // This triggers the construction of a new accessibility object for

sd/source/ui/toolpanel/ToolPanelViewShell.cxx
@@ -483,6 +483,8 @@ ToolPanelViewShell::ToolPanelViewShell( SfxViewFrame* pFrame, ViewShellBase& rVi
     ,mpSubShellManager()
     ,mnMenuId(0)
 {
+    ViewShell::doShow();
+
     meShellType = ST_TASK_PANE;
 
     mpContentWindow->SetCenterAllowed( false );

sd/source/ui/view/drviewsa.cxx
@@ -143,6 +143,8 @@ DrawViewShell::DrawViewShell( SfxViewFrame* pFrame, ViewShellBase& rViewShellBas
 , mbIsLayerModeActive(false)
 , mbIsInSwitchPage(false)
 {
+    ViewShell::doShow();
+
     if (pFrameViewArgument != NULL)
         mpFrameView = pFrameViewArgument;
     else

sd/source/ui/view/outlnvsh.cxx
@@ -225,6 +225,8 @@ OutlineViewShell::OutlineViewShell (
       mbInitialized(false)
 
 {
+    ViewShell::doShow();
+
     if (pFrameViewArgument != NULL)
         mpFrameView = pFrameViewArgument;
     else

sd/source/ui/inc/ViewShell.hxx
@@ -562,6 +562,27 @@ protected:
     virtual void SetZoomFactor( const Fraction &rZoomX,
                                 const Fraction &rZoomY );
 
+    /**
+        This must be called after the ctor, but before anything else.
+        It's the part of construction that is dependent
+        on showing the top-level window.
+
+        Showing a window with a11y enabled causes various callbacks
+        to be triggered.
+
+        Due to the "virtual methods are not virtual during constructors"
+        problem, this is a disaster to call from the ctor
+
+        i.e. construct calls Show, and if a11y is enabled this
+        reenters the not-fully constructed object and calls
+        CreateAccessibleDocumentView, so if construct is called
+        from the ctor then if a derived class is contructed the base-cass
+        CreateAccessibleDocumentView is used, not the derived
+        CreateAccessibleDocumentView. i.e. run smoketest under a11y with
+        debugging assertions enabled
+    */
+    void doShow();
+
 private:
     ::Window* mpParentWindow;
     /** This window updater is used to keep all relevant windows up to date

sd/source/ui/view/viewshel.cxx
@@ -218,11 +218,6 @@ void ViewShell::construct(void)
     mpContentWindow->SetViewShell(this);
     mpContentWindow->SetPosSizePixel(
         GetParentWindow()->GetPosPixel(),GetParentWindow()->GetSizePixel());
-    mpContentWindow->Show();
-    static_cast< ::Window*>(mpContentWindow.get())->Resize();
-    OSL_TRACE("content window has size %d %d",
-        mpContentWindow->GetSizePixel().Width(),
-        mpContentWindow->GetSizePixel().Height());
 
     if ( ! GetDocSh()->IsPreview())
     {
@@ -231,18 +226,12 @@ void ViewShell::construct(void)
         mpHorizontalScrollBar->EnableRTL (sal_False);
         mpHorizontalScrollBar->SetRange(Range(0, 32000));
         mpHorizontalScrollBar->SetScrollHdl(LINK(this, ViewShell, HScrollHdl));
-        mpHorizontalScrollBar->Show();
 
         mpVerticalScrollBar.reset (new ScrollBar(GetParentWindow(), WinBits(WB_VSCROLL | WB_DRAG)));
         mpVerticalScrollBar->SetRange(Range(0, 32000));
         mpVerticalScrollBar->SetScrollHdl(LINK(this, ViewShell, VScrollHdl));
-        mpVerticalScrollBar->Show();
-        maScrBarWH = Size(
-            mpVerticalScrollBar->GetSizePixel().Width(),
-            mpHorizontalScrollBar->GetSizePixel().Height());
 
         mpScrollBarBox.reset(new ScrollBarBox(GetParentWindow(), WB_SIZEABLE));
-        mpScrollBarBox->Show();
     }
 
     String aName( RTL_CONSTASCII_USTRINGPARAM( "ViewShell" ));
@@ -264,12 +253,31 @@ void ViewShell::construct(void)
     // Register the sub shell factory.
     mpImpl->mpSubShellFactory.reset(new ViewShellObjectBarFactory(*this));
     GetViewShellBase().GetViewShellManager()->AddSubShellFactory(this,mpImpl->mpSubShellFactory);
+}
+
+void ViewShell::doShow(void)
+{
+    mpContentWindow->Show();
+    static_cast< ::Window*>(mpContentWindow.get())->Resize();
+    OSL_TRACE("content window has size %d %d",
+        mpContentWindow->GetSizePixel().Width(),
+        mpContentWindow->GetSizePixel().Height());
+
+    if ( ! GetDocSh()->IsPreview())
+    {
+        // Show scroll bars
+        mpHorizontalScrollBar->Show();
+
+        mpVerticalScrollBar->Show();
+        maScrBarWH = Size(
+            mpVerticalScrollBar->GetSizePixel().Width(),
+            mpHorizontalScrollBar->GetSizePixel().Height());
+
+        mpScrollBarBox->Show();
+    }
 
     GetParentWindow()->Show();
 }
-
-
-
 
 void ViewShell::Init (bool bIsMainViewShell)
 {

commit 4ae4c57caab02c5d8fe91cd9956fabba6bd3dc1e

Resolves: rhbz#805743 a11y: call doShow after we have a valid view
Basically a regression from ec240eafe9b25620383aa54015f5c55e0f64227a but things
were broken before that too.

enable a11y (for gnome 3 this is gsettings set org.gnome.desktop.interface
toolkit-accessibility true), and run the smoketest with dbg-util before
touching this stuff

Change-Id: I I55f4e22d8ac61c7ac3ccc37b82794604b28a8f14

sd/source/ui/slidesorter/shell/SlideSorterViewShell.cxx
@@ -186,7 +186,7 @@ void SlideSorterViewShell::Initialize (void)
         mpScrollBarBox);
     mpView = &mpSlideSorter->GetView();
 
-    ViewShell::doShow();
+    doShow();
 
     SetPool( &GetDoc()->GetPool() );
     SetUndoManager( GetDoc()->GetDocSh()->GetUndoManager() );

sd/source/ui/toolpanel/ToolPanelViewShell.cxx
@@ -482,7 +482,7 @@ ToolPanelViewShell::ToolPanelViewShell( SfxViewFrame* pFrame, ViewShellBase& rVi
     ,mpSubShellManager()
     ,mnMenuId(0)
 {
-    ViewShell::doShow();
+    doShow();
 
     meShellType = ST_TASK_PANE;
 

sd/source/ui/view/drviewsa.cxx
@@ -137,13 +137,12 @@ DrawViewShell::DrawViewShell( SfxViewFrame* pFrame, ViewShellBase& rViewShellBas
 , mbIsLayerModeActive(false)
 , mbIsInSwitchPage(false)
 {
-    ViewShell::doShow();
-
     if (pFrameViewArgument != NULL)
         mpFrameView = pFrameViewArgument;
     else
         mpFrameView = new FrameView(GetDoc());
     Construct(GetDocSh(), ePageKind);
+    doShow();
 }
 
 /*************************************************************************

sd/source/ui/view/outlnvsh.cxx
@@ -222,8 +222,6 @@ OutlineViewShell::OutlineViewShell (
       mbInitialized(false)
 
 {
-    ViewShell::doShow();
-
     if (pFrameViewArgument != NULL)
         mpFrameView = pFrameViewArgument;
     else
@@ -232,6 +230,8 @@ OutlineViewShell::OutlineViewShell (
     mpFrameView->Connect();
 
     Construct(GetDocSh());
+
+    doShow();
 }
 
 /*************************************************************************