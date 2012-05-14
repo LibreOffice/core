2368d3403a57be2e13ef478f00b6981212de0db7
Caolán McNamara <caolanm@redhat.com>
2012-05-14 11:55:58 +0100

    this should have been CDEFAULTOPT, not DEFAULTOPT
    Change-Id: Ib119c7bf8e04b8274026159218281cb3f220c8d0

--- solenv/inc/unxlngppc.mk
+++ solenv/inc/unxlngppc.mk
@@ -27,7 +27,7 @@
 
 # mk file for Unix Linux PowerPC using GCC, please make generic modifications to unxlng.mk
 
-DEFAULTOPT=-Os
+CDEFAULTOPT=-Os
 PICSWITCH:=-fPIC
 .INCLUDE : unxlng.mk
 CDEFS+=-DPOWERPC -DPPC
