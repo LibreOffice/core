a11fa1a4ac7ab27328b5e5376ea7597b1e69ddd1
Noel Power <noel.power@novell.com>
2012-05-24 14:47:41 +0100

    Revert "calc69: #i118068# handle all-hidden case in ScDrawUtil::CalcScale"
    This reverts commit 8a838b9fbf46ece9680824cd3a044ab7338bf306.

--- sc/source/ui/view/drawutil.cxx
+++ sc/source/ui/view/drawutil.cxx
@@ -80,18 +80,6 @@ void ScDrawUtil::CalcScale( ScDocument* pDoc, SCTAB nTab,
         nPixelY += ScViewData::ToPixel(nHeight, nPPTY);
     }
 
-    // #i116848# To get a large-enough number for PixelToLogic, multiply the integer values
-    // instead of using a larger number of rows
-    if ( nTwipsY )
-    {
-        long nMultiply = 2000000 / nTwipsY;
-        if ( nMultiply > 1 )
-        {
-            nTwipsY *= nMultiply;
-            nPixelY *= nMultiply;
-        }
-    }
- 
     MapMode aHMMMode( MAP_100TH_MM, Point(), rZoomX, rZoomY );
     Point aPixelLog = pDev->PixelToLogic( Point( nPixelX,nPixelY ), aHMMMode );
 
