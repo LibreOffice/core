73e8772bb92ee3af29660f55b3b06a922efad50e
David Ostrovsky <David.Ostrovsky@gmx.de>
2012-05-27 22:50:41 +0200

    gbuild migration: pyuno module
    Change-Id: I7f923a5622214f7540a789bcdd93bf6fd1d166db

---
+++ pyuno/Library_pythonloader.mk
@@ -0,0 +1,74 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# Major Contributor(s):
+# Copyright (C) 2012 David Ostrovsky <d.ostrovsky@gmx.de> (initial developer)
+#
+# All Rights Reserved.
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+
+$(eval $(call gb_Library_Library,pythonloader))
+
+$(eval $(call gb_Library_set_componentfile,pythonloader,pyuno/source/loader/pythonloader))
+
+$(eval $(call gb_Library_set_include,pythonloader,\
+    -I$(SRCDIR)/pyuno/source/loader \
+    -I$(SRCDIR)/pyuno/inc \
+    $(PYTHON_CFLAGS) \
+    $$(INCLUDE) \
+))
+
+#$(eval $(call gb_Library_add_defs,pythonloader,\
+#	-DPYVERSION=$(PYVERSION) \
+#	-DPYMAJMIN=$(PYMAJOR).$(PYMINOR) \
+#	-DPY_MAJOR_VERSION=$(PYMAJOR) \
+#))
+
+$(eval $(call gb_Library_use_api,pythonloader,\
+    udkapi \
+))
+
+$(eval $(call gb_Library_use_libraries,pythonloader,\
+    cppu \
+    cppuhelper \
+    sal \
+    pyuno \
+))
+
+$(eval $(call gb_Library_add_libs,pythonloader,\
+	$(if $(filter-out MACOSX,$(OS)),-Wl,--as-needed) \
+    $(PYTHON_LIBS) \
+	$(if $(filter-out MACOSX,$(OS)),-Wl,--no-as-needed) \
+))
+
+# TODO: remove it
+# disable -Wl,-z,defs
+# from man ld:
+# Disallows undefined symbols in object files.
+# Undefined symbols in shared libraries are still allowed.
+#$(eval $(call gb_Library_set_ldflags,pythonloader,\
+#    $$(filter-out -Wl$$(COMMA)-z$$(COMMA)defs,$$(T_LDFLAGS)) \
+#))
+
+$(eval $(call gb_Library_add_exception_objects,pythonloader,\
+    pyuno/source/loader/pyuno_loader \
+))
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
