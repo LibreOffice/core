f2154c68d19c3e711d371cf91d37c2b885484059
Ariel Constenla-Haile <arielch@apache.org>
2012-05-28 15:23:23 +0000

    i53749 - Show Pause logo according to presentation settings
    Original author: Tsutomu Uchino <hanya.runo at gmail.com>

--- sd/source/ui/slideshow/slideshowimpl.cxx
+++ sd/source/ui/slideshow/slideshowimpl.cxx
@@ -3320,8 +3320,13 @@ void SAL_CALL SlideshowImpl::gotoNextSlide(  ) throw (RuntimeException)
                     {
                         if( mpShowWindow )
                         {
-                            Graphic aGraphic( SfxApplication::GetApplicationLogo().GetBitmapEx() );
-                            mpShowWindow->SetPauseMode( 0, maPresSettings.mnPauseTimeout, &aGraphic );
+                            if ( maPresSettings.mbShowPauseLogo )
+                            {
+                                Graphic aGraphic( SfxApplication::GetApplicationLogo().GetBitmapEx() );
+                                mpShowWindow->SetPauseMode( 0, maPresSettings.mnPauseTimeout, &aGraphic );
+                            }
+                            else
+                                mpShowWindow->SetPauseMode( 0, maPresSettings.mnPauseTimeout );
                         }
                     }
                     else
