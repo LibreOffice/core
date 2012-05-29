ea22c4fc2570d484dd6a05f3b98204c75895329a
David Tardon <dtardon@redhat.com>
2012-05-29 13:48:00 +0200

    make sure that $(OUTDIR)/lib exists before delivering libs
    Change-Id: I8a4b11c14c615ba47c8e58b5f30145f5e8d77f0d

--- solenv/gbuild/Library.mk
+++ solenv/gbuild/Library.mk
@@ -74,7 +74,8 @@ $(call gb_LinkTarget_add_defs,$(2),\
 	$(gb_Library_DEFS) \
 )
 $(call gb_Library__get_final_target,$(1)) : $(call gb_Library_get_target,$(1))
-$(call gb_Library_get_target,$(1)) : $(call gb_LinkTarget_get_target,$(2))
+$(call gb_Library_get_target,$(1)) : $(call gb_LinkTarget_get_target,$(2)) \
+	| $(dir $(call gb_Library_get_target,$(1))).dir
 $(call gb_Library_get_clean_target,$(1)) : $(call gb_LinkTarget_get_clean_target,$(2))
 $(call gb_Library_Library_platform,$(1),$(2),$(gb_Library_DLLDIR)/$(call gb_Library_get_dllname,$(1)))
 $$(eval $$(call gb_Module_register_target,$(call gb_Library__get_final_target,$(1)),$(call gb_Library_get_clean_target,$(1))))
--- solenv/gbuild/StaticLibrary.mk
+++ solenv/gbuild/StaticLibrary.mk
@@ -66,7 +66,8 @@ $(call gb_LinkTarget_set_targettype,$(2),StaticLibrary)
 $(call gb_LinkTarget_add_defs,$(2),\
 	$(gb_StaticLibrary_DEFS) \
 )
-$(call gb_StaticLibrary_get_target,$(1)) : $(call gb_LinkTarget_get_target,$(2))
+$(call gb_StaticLibrary_get_target,$(1)) : $(call gb_LinkTarget_get_target,$(2)) \
+	| $(dir $(call gb_StaticLibrary_get_target,$(1))).dir
 $(call gb_StaticLibrary_get_clean_target,$(1)) : $(call gb_LinkTarget_get_clean_target,$(2))
 $(call gb_StaticLibrary_StaticLibrary_platform,$(1),$(2))
 $$(eval $$(call gb_Module_register_target,$(call gb_StaticLibrary_get_target,$(1)),$(call gb_StaticLibrary_get_clean_target,$(1))))
