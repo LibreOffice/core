611f45659263b7647e01d9f23ae155d5d7de4818
Norbert Thiebaud <nthiebaud@gmail.com>
2012-06-02 15:10:21 -0500

    targeted string re-work
    Change-Id: I535fd1e4438b3ade78c1867eeb0321e6753ed22b

--- basegfx/source/tools/canvastools.cxx
+++ basegfx/source/tools/canvastools.cxx
@@ -329,9 +329,9 @@ namespace basegfx
                     if( !xLinePoly.is() )
                     {
                         throw lang::IllegalArgumentException(
-                            ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM(
+                            ::rtl::OUString(
                                     "basegfx::unotools::b2DPolyPolygonFromXPolyPolygon2D(): Invalid input"
-                                    "poly-polygon, cannot retrieve vertex data")),
+                                    "poly-polygon, cannot retrieve vertex data"),
                             uno::Reference< uno::XInterface >(),
                             0 );
                     }
