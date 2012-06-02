9b43bf2ce3b947b494e076cad853d09a6e6d7544
Philipp Riemer <ruderphilipp@gmail.com>
2012-06-02 16:18:44 +0200

    re-indent files in sw/source/core/inc
    * re-indentatation by -4 spaces
    * delete ASCII art separators

--- sw/source/core/inc/ascharanchoredobjectposition.hxx
+++ sw/source/core/inc/ascharanchoredobjectposition.hxx
@@ -51,109 +51,104 @@ namespace objectpositioning
 
     class SwAsCharAnchoredObjectPosition : public SwAnchoredObjectPosition
     {
-        private:
-         // data to calculate object position
-            // proposed anchor position, starting point for the calculation
-            // of the object position
-            const Point& mrProposedAnchorPos;
-            // flags that influences the calculation of the anchor position
-            // AS_CHAR_QUICK   : quick formatting - calculated position not set at object
-            // AS_CHAR_ULSPACE : consider upper/lower spacing - adjustment of anchor position
-            // AS_CHAR_INIT    : initial calculation
-            // AS_CHAR_ROTATE  : object is rotated by 90 degrees
-            // AS_CHAR_REVERSE : object is reversed (rotated by 270 degrees)
-            // AS_CHAR_BIDI    : object belongs to a BIDI-multi-portion
-            const AsCharFlags mnFlags;
-            // needed line values for the different alignments.
-            const SwTwips mnLineAscent;
-            const SwTwips mnLineDescent;
-            const SwTwips mnLineAscentInclObjs;
-            const SwTwips mnLineDescentInclObjs;
+    private:
+        // data to calculate object position
+        // Proposed anchor position, starting point for the calculation
+        // of the object position.
+        const Point& mrProposedAnchorPos;
+        // flags that influences the calculation of the anchor position
+        // AS_CHAR_QUICK   : quick formatting - calculated position not set at object
+        // AS_CHAR_ULSPACE : consider upper/lower spacing - adjustment of anchor position
+        // AS_CHAR_INIT    : initial calculation
+        // AS_CHAR_ROTATE  : object is rotated by 90 degrees
+        // AS_CHAR_REVERSE : object is reversed (rotated by 270 degrees)
+        // AS_CHAR_BIDI    : object belongs to a BIDI-multi-portion
+        const AsCharFlags mnFlags;
+        // needed line values for the different alignments.
+        const SwTwips mnLineAscent;
+        const SwTwips mnLineDescent;
+        const SwTwips mnLineAscentInclObjs;
+        const SwTwips mnLineDescentInclObjs;
 
-         // calculated data for object position
-            Point       maAnchorPos;
-            SwTwips     mnRelPos;
-            SwRect      maObjBoundRect;
-            // line alignment relative to line height; gives feedback for line formatting
-            // 0 - no line alignment, 1 - at top, 2 - at center, 3 - at bottom
-            sal_uInt8   mnLineAlignment;
+        // calculated data for object position
+        Point       maAnchorPos;
+        SwTwips     mnRelPos;
+        SwRect      maObjBoundRect;
+        // line alignment relative to line height; gives feedback for line formatting
+        // 0 - no line alignment, 1 - at top, 2 - at center, 3 - at bottom
+        sal_uInt8   mnLineAlignment;
 
-            // method to cast <SwAnchoredObjectPosition::GetAnchorFrm()>
-            const SwTxtFrm& GetAnchorTxtFrm() const;
+        // method to cast <SwAnchoredObjectPosition::GetAnchorFrm()>
+        const SwTxtFrm& GetAnchorTxtFrm() const;
 
-            /** determine the relative position to base line for object position
+        /** determine the relative position to base line for object position
 
-                @param _ObjBoundHeight
-                height including corresponding spacing of the object, for which
-                the Y-position has to be calculated.
+            @param _ObjBoundHeight
+            height including corresponding spacing of the object, for which
+            the Y-position has to be calculated.
 
-                @param _rVert
-                given vertical positioning and alignment
+            @param _rVert
+            given vertical positioning and alignment
 
-                @return relative position to the base line
-            */
-            SwTwips _GetRelPosToBase( const SwTwips          _nObjBoundHeight,
-                                      const SwFmtVertOrient& _rVert );
+            @return relative position to the base line
+        */
+        SwTwips _GetRelPosToBase( const SwTwips          _nObjBoundHeight,
+                                  const SwFmtVertOrient& _rVert );
 
-        // *********************************************************************
-        public:
-            /** construtor; provided object to be positioned and needed data
-                for calculation of the object position
+    public:
+        /** construtor; provided object to be positioned and needed data
+            for calculation of the object position
 
-                @param _rDrawObj
-                input parameter - object, that is be positioned.
+            @param _rDrawObj
+            input parameter - object, that is be positioned.
 
-                @param _rProposedAnchorPos
-                proposed anchor position; starting point for the calculation
-                of the anchor position
+            @param _rProposedAnchorPos
+            proposed anchor position; starting point for the calculation
+            of the anchor position
 
-                @param _nFlags
-                flags that influences the calculation of the anchor position
-                AS_CHAR_QUICK   : quick formatting - calculated position not set at object
-                AS_CHAR_ULSPACE : consider upper/lower spacing - adjustment of anchor position
-                AS_CHAR_INIT    : initial calculation
-                AS_CHAR_ROTATE  : object is rotated by 90 degrees
-                AS_CHAR_REVERSE : object is reversed (rotated by 270 degrees)
-                AS_CHAR_BIDI    : object belongs to a BIDI-multi-portion
+            @param _nFlags
+            flags that influences the calculation of the anchor position
+            AS_CHAR_QUICK   : quick formatting - calculated position not set at object
+            AS_CHAR_ULSPACE : consider upper/lower spacing - adjustment of anchor position
+            AS_CHAR_INIT    : initial calculation
+            AS_CHAR_ROTATE  : object is rotated by 90 degrees
+            AS_CHAR_REVERSE : object is reversed (rotated by 270 degrees)
+            AS_CHAR_BIDI    : object belongs to a BIDI-multi-portion
 
-                @param _nLineAscent, _nLineDescent, _nLineAscentInclObjs,
-                _nLineDescentInclObjs - needed line values for the different
-                alignments.
-            */
-            SwAsCharAnchoredObjectPosition( SdrObject&      _rDrawObj,
-                                            const Point&    _rProposedAnchorPos,
-                                            const AsCharFlags _nFlags,
-                                            const SwTwips     _nLineAscent,
-                                            const SwTwips     _nLineDescent,
-                                            const SwTwips     _nLineAscentInclObjs,
-                                            const SwTwips     _nLineDescentInclObjs );
-            virtual ~SwAsCharAnchoredObjectPosition();
+            @param _nLineAscent, _nLineDescent, _nLineAscentInclObjs,
+            _nLineDescentInclObjs - needed line values for the different
+            alignments.
+        */
+        SwAsCharAnchoredObjectPosition( SdrObject&      _rDrawObj,
+                                        const Point&    _rProposedAnchorPos,
+                                        const AsCharFlags _nFlags,
+                                        const SwTwips     _nLineAscent,
+                                        const SwTwips     _nLineDescent,
+                                        const SwTwips     _nLineAscentInclObjs,
+                                        const SwTwips     _nLineDescentInclObjs );
+        virtual ~SwAsCharAnchoredObjectPosition();
 
-            /** calculate position for object position
+        /** calculate position for object position
 
-                members <maAnchorPos>, <mnRelPos>, <maObjBoundRect> and
-                <mnLineAlignment> are calculated.
-                calculated position is set at the given object.
-            */
-            virtual void CalcPosition();
+            members <maAnchorPos>, <mnRelPos>, <maObjBoundRect> and
+            <mnLineAlignment> are calculated.
+            calculated position is set at the given object.
+        */
+        virtual void CalcPosition();
 
-            // calculated anchored position for object position type AS_CHAR
+        // calculated anchored position for object position type AS_CHAR
+        Point GetAnchorPos() const;
 
-            Point GetAnchorPos() const;
+        // calculated relative position to base line for object position type AS_CHAR
+        SwTwips GetRelPosY() const;
 
-           // calculated relative position to base line for object position type AS_CHAR
+        // determined object rectangle including spacing for object position type AS_CHAR
+        SwRect GetObjBoundRectInclSpacing() const;
 
-            SwTwips GetRelPosY() const;
-
-          // determined object rectangle including spacing for object position type AS_CHAR
-
-            SwRect GetObjBoundRectInclSpacing() const;
-
-          // determined line alignment relative to line height
-
-            sal_uInt8 GetLineAlignment() const;
+        // determined line alignment relative to line height
+        sal_uInt8 GetLineAlignment() const;
     };
-} // namespace objectpositioning
+}
 
 #endif
 
