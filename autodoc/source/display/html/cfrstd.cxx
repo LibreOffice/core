8430f1432e3c2c9e171ff2028148d71f85826d7b
Jürgen Schmidt <jsc@apache.org>
2012-06-04 11:00:33 +0000

    119677: correct copyright footer

--- autodoc/source/display/html/cfrstd.cxx
+++ autodoc/source/display/html/cfrstd.cxx
@@ -330,9 +330,9 @@ MakeCopyRight()
         plt = localtime(&gt);
     int year = 1900 + plt->tm_year;
 
-    cr  << "Copyright &copy; 1995, "
+    cr  << "Copyright &copy; "
         << year
-        << ", Oracle and/or its affiliates. All rights reserved. Oracle and Java are registered trademarks of Oracle and/or its affiliates. Other names may be trademarks of their respective owners.";
+        << ", The Apache Software Foundation, Licensed under the Apache License, Version 2.0. Apache, the Apache feather logo, Apache OpenOffice and OpenOfifce.org are trademarks of The Apache Software Foundation. Other names may be trademarks of their respective owners.";
     return String(cr.c_str());
 
 }
