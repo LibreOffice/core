5572bdb944ccf5ba7915388b7920c88084f6dd37
Michael Meeks <michael.meeks@suse.com>
2012-06-06 18:28:58 +0100

    re-base on ALv2 code.
    Change-Id: I06da546daa2efb23362cdc61c14add3ea761638c

--- connectivity/source/drivers/evoab2/EApi.h
+++ connectivity/source/drivers/evoab2/EApi.h
@@ -16,6 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
+
 #ifndef _CONNECTIVITY_EVOAB_EVOLUTION_API_HXX_
 #define _CONNECTIVITY_EVOAB_EVOLUTION_API_HXX_
 #include <glib-object.h>
