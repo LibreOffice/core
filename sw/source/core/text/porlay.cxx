LibreOffice/core

commit 563df5d0c3c696912f211e74dd4dbda1aa720ae7

n#757905 SwLineLayout::CalcLine: improve line height calculation
Word vs. Writer difference: if the only portion in a line is a fly
(as-character shape) and the height of the fly is smaller than the
height of the line, then Writer sets the height to the height of the
fly. Word takes the max of the two values.

We already have IgnoreTabsAndBlanksForLineCalculation for Word compat
wrt. line height calculation, use that here as well.

Change-Id: Iebe1bc697d6f19a03d15a1e5fd25ab4d5f4898fd

sw/source/core/text/porlay.cxx
@@ -330,6 +330,7 @@ void SwLineLayout::CalcLine( SwTxtFormatter &rLine, SwTxtFormatInfo &rInf )
         }
         else
         {
+            KSHORT nLineHeight = Height();
             Init( GetPortion() );
             SwLinePortion *pPos = pPortion;
             SwLinePortion *pLast = this;
@@ -436,7 +437,15 @@ void SwLineLayout::CalcLine( SwTxtFormatter &rLine, SwTxtFormatInfo &rInf )
                     else if( !pPos->IsFlyPortion() )
                     {
                         if( Height() < nPosHeight )
-                            Height( nPosHeight );
+                        {
+                            // Height is set to 0 when Init() is called.
+                            if (bIgnoreBlanksAndTabsForLineHeightCalculation)
+                                // Compat flag set: take the line height, if it's larger.
+                                Height(std::max(nPosHeight, nLineHeight));
+                            else
+                                // Just care about the portion height.
+                                Height(nPosHeight);
+                        }
                         if( pPos->IsFlyCntPortion() || ( pPos->IsMultiPortion()
                             && ((SwMultiPortion*)pPos)->HasFlyInCntnt() ) )
                             rLine.SetFlyInCntBase();

commit 11a54814be785766d797cc83cfdacd2368ea642c

Related: i#3952 SwLineLayout::CalcLine: fix regression
Commit 563df5d0c3c696912f211e74dd4dbda1aa720ae7 broke the layout of the
bugdoc, fix this.

Change-Id: I7965e9507d4ba130a4f7bea844cdd893ef42ba4a

sw/source/core/text/porlay.cxx
@@ -439,7 +439,7 @@ void SwLineLayout::CalcLine( SwTxtFormatter &rLine, SwTxtFormatInfo &rInf )
                         if( Height() < nPosHeight )
                         {
                             // Height is set to 0 when Init() is called.
-                            if (bIgnoreBlanksAndTabsForLineHeightCalculation)
+                            if (bIgnoreBlanksAndTabsForLineHeightCalculation && pPos->GetWhichPor() == POR_FLYCNT)
                                 // Compat flag set: take the line height, if it's larger.
                                 Height(std::max(nPosHeight, nLineHeight));
                             else