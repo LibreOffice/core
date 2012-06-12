770e990dabcd4f0415da65ef3ace4fc5a3784278
Stephan Bergmann <sbergman@redhat.com>
2012-06-12 10:31:35 +0200

    Missing dependency
    Change-Id: Iffbd7722b7ede8a2824e59f0bfc0241730de2486

--- sw/CppunitTest_sw_subsequent_ww8tok.mk
+++ sw/CppunitTest_sw_subsequent_ww8tok.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sw_subsequent_ww8tok, \
 
 $(eval $(call gb_CppunitTest_use_libraries,sw_subsequent_ww8tok, \
     cppu \
+    cppuhelper \
     sal \
     test \
     unotest \
