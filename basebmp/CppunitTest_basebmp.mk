eccac35fff3c4d90e2fc60fdd67ed8e7a99aad76
Norbert Thiebaud <nthiebaud@gmail.com>
2012-06-16 04:36:43 -0500

    basebmp gbuildification
    Change-Id: I9b24ff3503b1dc1198aab622e36224e75cb31255

---
+++ basebmp/CppunitTest_basebmp.mk
@@ -0,0 +1,36 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,basebmp_cpputest))
+
+$(eval $(call gb_CppunitTest_set_include,basebmp_cpputest,\
+    -I$(SRCDIR)/basebmp/inc/ \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,basebmp_cpputest,\
+    basebmp \
+    basegfx \
+    sal \
+    $(gb_STDLIBS) \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,basebmp_cpputest,\
+    basebmp/test/basictest \
+    basebmp/test/bmpmasktest \
+    basebmp/test/bmptest \
+    basebmp/test/cliptest \
+    basebmp/test/filltest \
+    basebmp/test/linetest \
+    basebmp/test/masktest \
+    basebmp/test/polytest \
+    basebmp/test/tools \
+))
+
+# vim: set noet sw=4 ts=4:
---
+++ basebmp/Library_basebmp.mk
@@ -0,0 +1,40 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Library_Library,basebmp))
+
+$(eval $(call gb_Library_use_package,basebmp,basebmp_inc))
+
+$(eval $(call gb_Library_use_api,basebmp,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Library_use_libraries,basebmp,\
+    basegfx \
+    sal \
+    $(gb_STDLIBS) \
+))
+
+$(eval $(call gb_Library_set_include,basebmp,\
+    -I$(SRCDIR)/basebmp/inc/ \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_Library_add_defs,basebmp,\
+	-DBASEBMP_DLLIMPLEMENTATION \
+))
+
+$(eval $(call gb_Library_add_exception_objects,basebmp,\
+	basebmp/source/bitmapdevice \
+	basebmp/source/debug \
+	basebmp/source/polypolygonrenderer \
+))
+
+# vim: set noet sw=4 ts=4:
