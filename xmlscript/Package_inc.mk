0db1b46a45694a735d0f33f5c5a1facede58e653
Norbert Thiebaud <nthiebaud@gmail.com>
2012-06-16 20:58:28 -0500

    gbuildify xmlscript
    Change-Id: Ia3704b4a7813d45c87e9f7107945fedd73622339

---
+++ xmlscript/Package_inc.mk
@@ -0,0 +1,26 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,xmlscript_inc,$(SRCDIR)/xmlscript))
+
+# headers
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xcrdllapi.h,inc/xmlscript/xcrdllapi.h))
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xml_helper.hxx,inc/xmlscript/xml_helper.hxx))
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xmldlg_imexp.hxx,inc/xmlscript/xmldlg_imexp.hxx))
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xmllib_imexp.hxx,inc/xmlscript/xmllib_imexp.hxx))
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xmlmod_imexp.hxx,inc/xmlscript/xmlmod_imexp.hxx))
+$(eval $(call gb_Package_add_file,xmlscript_inc,inc/xmlscript/xmlns.h,inc/xmlscript/xmlns.h))
+
+# dtds
+$(eval $(call gb_Package_add_file,xmlscript_inc,bin/dialog.dtd,dtd/dialog.dtd))
+$(eval $(call gb_Package_add_file,xmlscript_inc,bin/libraries.dtd,dtd/libraries.dtd))
+$(eval $(call gb_Package_add_file,xmlscript_inc,bin/library.dtd,dtd/library.dtd))
+$(eval $(call gb_Package_add_file,xmlscript_inc,bin/module.dtd,dtd/module.dtd))
+
+# vim: set noet sw=4 ts=4:
