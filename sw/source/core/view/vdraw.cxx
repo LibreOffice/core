e7bbaefa47fa19971151f03531bf1da3658e6d34
Sebastian Spaeth <Sebastian@SSpaeth.de>
2012-06-21 12:12:14 +0200

    Remove commented out code
    Remove old, already commented out code and useless code documentation
    (basically restating the function names). Also translate the few Germany
    comments in that file.
    
    This file still lacks a good doxygen code documentation or a brief
    description of what it actually does.
    
    Change-Id: I339a01dbd4ea894be13293133b8dedbc55c2bbac
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- sw/source/core/view/vdraw.cxx
+++ sw/source/core/view/vdraw.cxx
@@ -35,8 +35,6 @@
 #include <fmtanchr.hxx>
 #include <frmfmt.hxx>
 
-/// OD 29.08.2002 #102450#
-/// include <svx/svdoutl.hxx>
 #include <svx/svdoutl.hxx>
 
 #include "fesh.hxx"
@@ -54,114 +52,49 @@
 #include <IDocumentDrawModelAccess.hxx>
 
 
-/*************************************************************************
-|*
-|*  SwSaveHdl
-|*
-|*  Ersterstellung      MA 14. Feb. 95
-|*  Letzte Aenderung    MA 02. Jun. 98
-|*
-|*************************************************************************/
-//SwSaveHdl::SwSaveHdl( SwViewImp *pI ) :
-//  pImp( pI ),
-//  bXorVis( FALSE )
-//{
-    //if ( pImp->HasDrawView() )
-    //{
-    //  bXorVis = pImp->GetDrawView()->IsShownXorVisible( pImp->GetShell()->GetOut());
-    //  if ( bXorVis )
-    //      pImp->GetDrawView()->HideShownXor( pImp->GetShell()->GetOut() );
-    //}
-//}
-
-
-//SwSaveHdl::~SwSaveHdl()
-//{
-    //if ( bXorVis )
-    //  pImp->GetDrawView()->ShowShownXor( pImp->GetShell()->GetOut() );
-//}
-
-
-/*************************************************************************
-|*
-|*  SwViewImp::StartAction(), EndAction()
-|*
-|*  Ersterstellung      MA 14. Feb. 95
-|*  Letzte Aenderung    MA 14. Sep. 98
-|*
-|*************************************************************************/
 void SwViewImp::StartAction()
 {
     if ( HasDrawView() )
     {
         SET_CURR_SHELL( GetShell() );
         if ( pSh->ISA(SwFEShell) )
-            ((SwFEShell*)pSh)->HideChainMarker();   //Kann sich geaendert haben
-        //bResetXorVisibility = GetDrawView()->IsShownXorVisible( GetShell()->GetOut());
-        //GetDrawView()->HideShownXor( GetShell()->GetOut() );
+            ((SwFEShell*)pSh)->HideChainMarker(); // might have changed
     }
 }
+
 
 void SwViewImp::EndAction()
 {
     if ( HasDrawView() )
     {
         SET_CURR_SHELL( GetShell() );
-        //if ( bResetXorVisibility )
-        //  GetDrawView()->ShowShownXor( GetShell()->GetOut() );
         if ( pSh->ISA(SwFEShell) )
-            ((SwFEShell*)pSh)->SetChainMarker();    //Kann sich geaendert haben
+            ((SwFEShell*)pSh)->SetChainMarker(); // might have changed
     }
 }
 
-/*************************************************************************
-|*
-|*  SwViewImp::LockPaint(), UnlockPaint()
-|*
-|*  Ersterstellung      MA 11. Jun. 96
-|*  Letzte Aenderung    MA 11. Jun. 96
-|*
-|*************************************************************************/
+
 void SwViewImp::LockPaint()
 {
     if ( HasDrawView() )
     {
-        //HMHbShowHdlPaint = GetDrawView()->IsMarkHdlShown();
-        //HMHif ( bShowHdlPaint )
-        //HMH   GetDrawView()->HideMarkHdl();
         bResetHdlHiddenPaint = !GetDrawView()->areMarkHandlesHidden();
         GetDrawView()->hideMarkHandles();
     }
     else
     {
-        //HMHbShowHdlPaint = FALSE;
         bResetHdlHiddenPaint = sal_False;
     }
 }
+
 
 void SwViewImp::UnlockPaint()
 {
     if ( bResetHdlHiddenPaint )
         GetDrawView()->showMarkHandles();
-    //HMHif ( bShowHdlPaint )
-    //HMH   GetDrawView()->ShowMarkHdl();
 }
 
-/*************************************************************************
-|*
-|*  SwViewImp::PaintLayer(), PaintDispatcher()
-|*
-|*  Ersterstellung      MA 20. Dec. 94
-|*  Letzte Aenderung    AMA 04. Jun. 98
-|*
-|*************************************************************************/
-// OD 29.08.2002 #102450#
-// add 3rd paramter <const Color* pPageBackgrdColor> for setting this
-// color as the background color at the outliner of the draw view.
-// OD 09.12.2002 #103045# - add 4th parameter for the horizontal text direction
-// of the page in order to set the default horizontal text direction at the
-// outliner of the draw view for painting layers <hell> and <heaven>.
-// OD 25.06.2003 #108784# - correct type of 1st parameter
+
 void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
                             SwPrintData const*const pPrintData,
                             const SwRect& ,
@@ -182,13 +115,13 @@ void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
                                 DRAWMODE_SETTINGSTEXT | DRAWMODE_SETTINGSGRADIENT );
         }
 
-        // OD 29.08.2002 #102450#
-        // For correct handling of accessibility, high contrast, the page background
-        // color is set as the background color at the outliner of the draw view.
-        // Only necessary for the layers hell and heaven
+        // For correct handling of accessibility, high contrast, the
+        // page background color is set as the background color at the
+        // outliner of the draw view.  Only necessary for the layers
+        // hell and heaven
         Color aOldOutlinerBackgrdColor;
-        // OD 09.12.2002 #103045# - set default horizontal text direction on
-        // painting <hell> or <heaven>.
+        // set default horizontal text direction on painting <hell> or
+        // <heaven>.
         EEHorizontalTextDirection aOldEEHoriTextDir = EE_HTEXTDIR_L2R;
         const IDocumentDrawModelAccess* pIDDMA = GetShell()->getIDocumentDrawModelAccess();
         if ( (_nLayerID == pIDDMA->GetHellId()) ||
@@ -220,9 +153,7 @@ void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
         GetPageView()->DrawLayer( _nLayerID, pOutDev, pRedirector );
         pOutDev->Pop();
 
-        // OD 29.08.2002 #102450#
-        // reset background color of the outliner
-        // OD 09.12.2002 #103045# - reset default horizontal text direction
+        // reset background color of the outliner & default horiz. text dir.
         if ( (_nLayerID == pIDDMA->GetHellId()) ||
              (_nLayerID == pIDDMA->GetHeavenId()) )
         {
@@ -234,14 +165,7 @@ void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
     }
 }
 
-/*************************************************************************
-|*
-|*  SwViewImp::IsDragPossible()
-|*
-|*  Ersterstellung      MA 19. Jan. 93
-|*  Letzte Aenderung    MA 16. Jan. 95
-|*
-|*************************************************************************/
+
 #define WIEDUWILLST 400
 
 sal_Bool SwViewImp::IsDragPossible( const Point &rPoint )
@@ -273,14 +197,6 @@ sal_Bool SwViewImp::IsDragPossible( const Point &rPoint )
     return aRect.IsInside( rPoint );
 }
 
-/*************************************************************************
-|*
-|*  SwViewImp::NotifySizeChg()
-|*
-|*  Ersterstellung      MA 23. Jun. 93
-|*  Letzte Aenderung    MA 05. Oct. 98
-|*
-|*************************************************************************/
 
 void SwViewImp::NotifySizeChg( const Size &rNewSz )
 {
@@ -290,7 +206,7 @@ void SwViewImp::NotifySizeChg( const Size &rNewSz )
     if ( GetPageView() )
         GetPageView()->GetPage()->SetSize( rNewSz );
 
-    //Begrenzung des Arbeitsbereiches.
+    // Limitation of the work area
     const Rectangle aRect( Point( DOCUMENTBORDER, DOCUMENTBORDER ), rNewSz );
     const Rectangle &rOldWork = GetDrawView()->GetWorkArea();
     sal_Bool bCheckDrawObjs = sal_False;
@@ -311,13 +227,11 @@ void SwViewImp::NotifySizeChg( const Size &rNewSz )
         SdrObject *pObj = pPage->GetObj( nObj );
         if( !pObj->ISA(SwVirtFlyDrawObj) )
         {
-            //Teilfix(26793): Objekte, die in Rahmen verankert sind, brauchen
-            //nicht angepasst werden.
+            // Objects not anchored to the frame, do not need to be adjusted
             const SwContact *pCont = (SwContact*)GetUserCall(pObj);
-            //JP - 16.3.00 Bug 73920: this function might be called by the
-            //              InsertDocument, when a PageDesc-Attribute is
-            //              set on a node. Then the SdrObject must not have
-            //              an UserCall.
+            // this function might be called by the InsertDocument, when
+            // a PageDesc-Attribute is set on a node. Then the SdrObject
+            // must not have an UserCall.
             if( !pCont || !pCont->ISA(SwDrawContact) )
                 continue;
 
@@ -329,7 +243,7 @@ void SwViewImp::NotifySizeChg( const Size &rNewSz )
                 continue;
             }
 
-            // OD 19.06.2003 #108784# - no move for drawing objects in header/footer
+            // no move for drawing objects in header/footer
             if ( pAnchor->FindFooterOrHeader() )
             {
                 continue;
@@ -346,7 +260,7 @@ void SwViewImp::NotifySizeChg( const Size &rNewSz )
                 if ( aSz.Width() || aSz.Height() )
                     pObj->Move( aSz );
 
-                //Notanker: Grosse Objekte nicht nach oben verschwinden lassen.
+                // Don't let large objects dissappear to the top
                 aSz.Width() = aSz.Height() = 0;
                 if ( aBound.Bottom() < aRect.Top() )
                     aSz.Width() = (aBound.Bottom() - aRect.Top()) - MINFLY;
@@ -358,7 +272,5 @@ void SwViewImp::NotifySizeChg( const Size &rNewSz )
         }
     }
 }
-
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
