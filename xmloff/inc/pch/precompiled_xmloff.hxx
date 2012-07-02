6a71f121bd4b1ab5567fdb07a048715c4fd38097
Luboš Luňák <l.lunak@suse.cz>
2012-07-02 14:24:05 +0200

    xmloff precompiled headers
    Change-Id: Iae5ec6d6af8ebfa38f560dddc5a93a0386772f4d

---
+++ xmloff/inc/pch/precompiled_xmloff.hxx
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifdef PRECOMPILED_HEADERS
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <list>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/bind.hpp>
+#include <boost/compressed_pair.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/iterator.hpp>
+#include <boost/mem_fn.hpp>
+#include <boost/limits.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/none.hpp>
+#include <boost/optional.hpp>
+#include <boost/ptr_container/ptr_set.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/scoped_array.hpp>
+#include <boost/type.hpp>
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
