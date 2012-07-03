a666d4236cf799777a6af37c218fb6adc285b7a5
Markus Mohrhard <markus.mohrhard@googlemail.com>
2012-07-03 02:52:55 +0200

    check for invalid tab number, fdo#51667
    Change-Id: I9aebfafb68850130088658535f289f66d49f1741

--- sc/source/core/data/documen7.cxx
+++ sc/source/core/data/documen7.cxx
@@ -212,7 +212,7 @@ void ScDocument::StartListeningCell( const ScAddress& rAddress,
 {
     OSL_ENSURE(pListener, "StartListeningCell: pListener Null");
     SCTAB nTab = rAddress.Tab();
-    if (nTab < static_cast<SCTAB>(maTabs.size()) && maTabs[nTab])
+    if (VALIDTAB(nTab) && nTab < static_cast<SCTAB>(maTabs.size()) && maTabs[nTab])
         maTabs[nTab]->StartListening( rAddress, pListener );
 }
 
@@ -221,7 +221,7 @@ void ScDocument::EndListeningCell( const ScAddress& rAddress,
 {
     OSL_ENSURE(pListener, "EndListeningCell: pListener Null");
     SCTAB nTab = rAddress.Tab();
-    if (nTab < static_cast<SCTAB>(maTabs.size()) && maTabs[nTab])
+    if (VALIDTAB(nTab) && nTab < static_cast<SCTAB>(maTabs.size()) && maTabs[nTab])
         maTabs[nTab]->EndListening( rAddress, pListener );
 }
 
