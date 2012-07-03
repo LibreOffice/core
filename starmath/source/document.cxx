19811155689c7878e8118c8d1e4e0640df25de2b
Ariel Constenla-Haile <arielch@apache.org>
2012-07-03 12:26:39 +0000

    i120156 - AutoUpdate on Undo/Redo

--- starmath/source/document.cxx
+++ starmath/source/document.cxx
@@ -1181,6 +1181,10 @@ void SmDocShell::Execute(SfxRequest& rReq)
                     DBG_UNHANDLED_EXCEPTION();
                 }
             }
+
+            SmModule  *pModule = SM_MOD();
+            if ( pModule && pModule->GetConfig()->IsAutoRedraw() )
+                UpdateText();
             Repaint();
             SfxViewFrame* pFrm = SfxViewFrame::GetFirst( this );
             while( pFrm )
