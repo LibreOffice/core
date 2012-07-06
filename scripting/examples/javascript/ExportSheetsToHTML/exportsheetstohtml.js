3f281b6f0ad73eae72b77fa4dcab9008b7e14187
Michael Meeks <michael.meeks@suse.com>
2012-07-06 17:28:37 +0100

    re-base on ALv2 code. Includes:
    remove onlineregistration with dependencies
    Patch contributed by Juergen Schmidt
    http://svn.apache.org/viewvc?view=revision&revision=1249245

--- scripting/examples/javascript/ExportSheetsToHTML/exportsheetstohtml.js
+++ scripting/examples/javascript/ExportSheetsToHTML/exportsheetstohtml.js
@@ -1,7 +1,24 @@
-// When this script is run on an existing, saved, spreadsheet, 
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+// When this script is run on an existing, saved, spreadsheet,
 // eg. /home/testuser/myspreadsheet.sxc, the script will export
-// each sheet to a separate html file, 
-// eg. /home/testuser/myspreadsheet_sheet1.html, 
+// each sheet to a separate html file,
+// eg. /home/testuser/myspreadsheet_sheet1.html,
 // /home/testuser/myspreadsheet_sheet2.html etc
 importClass(Packages.com.sun.star.uno.UnoRuntime);
 importClass(Packages.com.sun.star.sheet.XSpreadsheetDocument);
