9991c0f552bfc9c6ea3e6e6d65fe67cc32f243fd
Michael Stahl <mstahl@redhat.com>
2012-07-10 16:22:29 +0200

    gbuild: sigh... that didn't help... let's try an eval here
    Change-Id: Ief6367a5fb8e2e1000bb94c7a4e6cf2095b24543

--- solenv/gbuild/JunitTest.mk
+++ solenv/gbuild/JunitTest.mk
@@ -70,7 +70,7 @@ $(call gb_JunitTest_get_target,%) :
 define gb_JunitTest_JunitTest
 $(call gb_JunitTest_get_target,$(1)) : T_CP := $(if $(value XCLASSPATH),$(value XCLASSPATH)$(gb_CLASSPATHSEP))$(call gb_JavaClassSet_get_classdir,$(call gb_JunitTest_get_classsetname,$(1)))$(gb_CLASSPATHSEP)$(OOO_JUNIT_JAR)$(gb_CLASSPATHSEP)$(OUTDIR)/lib
 $(call gb_JunitTest_get_target,$(1)) : CLASSES :=
-$(call gb_JunitTest_JunitTest_platform,$(1))
+$(eval $(call gb_JunitTest_JunitTest_platform,$(1)))
 
 $(call gb_JavaClassSet_JavaClassSet,$(call gb_JunitTest_get_classsetname,$(1)))
 $(call gb_JavaClassSet_use_system_jar,$(call gb_JunitTest_get_classsetname,$(1)),$(OOO_JUNIT_JAR))
