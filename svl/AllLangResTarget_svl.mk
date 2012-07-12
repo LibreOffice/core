ae95e31831916df760503bfc2496b7bc55bc638b
Thomas Arnhold <thomas@arnhold.org>
2012-07-12 15:24:35 +0200

    hrc cleanup: Remove unused cstitem.src
    STR_COLUM_DT_AUTO is unused too.
    
    Change-Id: Icd14e697842ec7c78131a76daa683aae1b5dfc3f

--- svl/AllLangResTarget_svl.mk
+++ svl/AllLangResTarget_svl.mk
@@ -31,7 +31,6 @@ $(eval $(call gb_SrsTarget_use_packages,svl/res,\
 
 $(eval $(call gb_SrsTarget_add_files,svl/res,\
     svl/source/misc/mediatyp.src \
-    svl/source/items/cstitem.src \
 ))
 
 # vim: set noet sw=4 ts=4:
