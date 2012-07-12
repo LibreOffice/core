c1aeceb363f188bc6a0de6c5cc1f7a6371b149b4
Michael Stahl <mstahl@redhat.com>
2012-07-12 21:39:42 +0200

    npapi.h: work around idiotic warnings from winnt.h
    Change-Id: I9a685fd0bf2e1285299d99ea39c29501bf8fadcd

--- np_sdk/inc/npapi.h
+++ np_sdk/inc/npapi.h
@@ -51,7 +51,14 @@
 #endif
 
 #if defined(_WIN32) && !defined(__SYMBIAN32__)
+#if _MSC_VER >= 1200
+#pragma warning(push)
+#pragma warning(disable:4201)
+#endif
 #include <windef.h>
+#if _MSC_VER >= 1200
+#pragma warning(pop)
+#endif
 #ifndef XP_WIN
 #define XP_WIN 1
 #endif
