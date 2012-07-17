b480f91968a3df16a655585fba494425aea725dc
David Tardon <dtardon@redhat.com>
2012-07-17 13:53:49 +0200

    fdo#44534 invalidate bound rect after drag&drop
    Change-Id: Ie3319870ae26e9e2cac32641a33be62e4e020c02

--- svx/source/svdraw/svdoedge.cxx
+++ svx/source/svdraw/svdoedge.cxx
@@ -1919,6 +1919,8 @@ bool SdrEdgeObj::applySpecialDrag(SdrDragStat& rDragStat)
     ImpSetEdgeInfoToAttr();
     bEdgeTrackUserDefined = false;
 
+    SetRectsDirty();
+
     if(bOriginalEdgeModified && rDragStat.GetView())
     {
         // hide connect marker helper again when original gets changed.
