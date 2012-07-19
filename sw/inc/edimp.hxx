c10159f3ed4b474736711fc6f15b85454da2669f
Noel Grandin <noel@peralex.com>
2012-07-19 16:19:24 +0200

    Convert SV_DECL_VARARR_SORT(_SwPamRanges) to o3tl::sorted_vector
    Change-Id: I6f3fa6b4940ba17e21e9fc47bcdd61c9761ba477

--- sw/inc/edimp.hxx
+++ sw/inc/edimp.hxx
@@ -30,7 +30,7 @@
 #define _EDIMP_HXX
 
 #include <tools/solar.h>
-#include <svl/svarray.hxx>
+#include <o3tl/sorted_vector.hxx>
 
 class SwPaM;
 class SwNodeIndex;
@@ -56,13 +56,13 @@ struct SwPamRange
     SwPamRange() : nStart( 0 ), nEnd( 0 )   {}
     SwPamRange( sal_uLong nS, sal_uLong nE ) : nStart( nS ), nEnd( nE ) {}
 
-    sal_Bool operator==( const SwPamRange& rRg )
-        { return nStart == rRg.nStart ? sal_True : sal_False; }
-    sal_Bool operator<( const SwPamRange& rRg )
-        { return nStart < rRg.nStart ? sal_True : sal_False; }
+    bool operator==( const SwPamRange& rRg ) const
+        { return nStart == rRg.nStart ? true : false; }
+    bool operator<( const SwPamRange& rRg ) const
+        { return nStart < rRg.nStart ? true : false; }
 };
 
-SV_DECL_VARARR_SORT( _SwPamRanges, SwPamRange, 0 )
+class _SwPamRanges : public o3tl::sorted_vector<SwPamRange> {};
 
 class SwPamRanges : private _SwPamRanges
 {
@@ -73,7 +73,7 @@ public:
     SwPaM& SetPam( sal_uInt16 nArrPos, SwPaM& rPam );
 
     sal_uInt16 Count() const
-                {   return _SwPamRanges::Count(); }
+                {   return _SwPamRanges::size(); }
     SwPamRange operator[]( sal_uInt16 nPos ) const
                 { return _SwPamRanges::operator[](nPos); }
 };
