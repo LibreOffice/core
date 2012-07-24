612a2064986643ba69de4b59d2baab9068987ba3
Andrzej J.R. Hunt <andrzej@ahunt.org>
2012-07-24 11:05:12 +0200

    Improved resizing handle, added visual feedback, limits.
    Change-Id: I6f1f9168dd53f660b676bf5050848ce509e11ae1

--- android/sdremote/src/org/libreoffice/impressremote/PresentationFragment.java
+++ android/sdremote/src/org/libreoffice/impressremote/PresentationFragment.java
@@ -1,6 +1,7 @@
 package org.libreoffice.impressremote;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -9,14 +10,20 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class PresentationFragment extends Fragment {
 
 	private View mTopView;
+	private ImageView mHandle;
+	private View mLayout;
+
+	private Context mContext;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
+		mContext = container.getContext();
+
 		View v = inflater.inflate(R.layout.fragment_presentation, container,
 		                false);
 
@@ -26,9 +33,10 @@ public class PresentationFragment extends Fragment {
 		mWebView.loadData(summary, "text/html", null);
 
 		mTopView = v.findViewById(R.id.imageView1);
+		mLayout = v.findViewById(R.id.presentation_layout);
 
-		v.findViewById(R.id.presentation_handle).setOnTouchListener(
-		                new SizeListener());
+		mHandle = (ImageView) v.findViewById(R.id.presentation_handle);
+		mHandle.setOnTouchListener(new SizeListener());
 
 		return v;
 	}
@@ -41,9 +49,25 @@ public class PresentationFragment extends Fragment {
 			LayoutParams aParams = mTopView.getLayoutParams();
 
 			switch (aEvent.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				mHandle.setImageResource(R.drawable.handle_light);
+				break;
+			case MotionEvent.ACTION_UP:
+				mHandle.setImageResource(R.drawable.handle_default);
+				break;
 			case MotionEvent.ACTION_MOVE:
 				int aHeight = mTopView.getHeight();
 				aParams.height = aHeight + (int) (aEvent.getY());
+
+				final int DRAG_MARGIN = 120;
+
+				int aSize = mLayout.getHeight();
+				if (aParams.height < DRAG_MARGIN) {
+					aParams.height = DRAG_MARGIN;
+				} else if (aParams.height > aSize - DRAG_MARGIN) {
+					aParams.height = aSize - DRAG_MARGIN;
+				}
+
 				mTopView.setLayoutParams(aParams);
 				break;
 			}
