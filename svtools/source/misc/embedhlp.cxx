44bc6b5cac723b52df40fbef026e99b7119d8a69
Caolán McNamara <caolanm@redhat.com>
2012-07-26 16:51:58 +0100

    discard broken embedded object previews
    Change-Id: Idbce67d16ed3e72395e130b1c4aa1d6977acd74b
    
    Signed-off-by: Petr Mladek <pmladek@suse.cz>

--- svtools/source/misc/embedhlp.cxx
+++ svtools/source/misc/embedhlp.cxx
@@ -561,16 +561,24 @@ SvStream* EmbeddedObjectRef::GetGraphicStream( sal_Bool bUpdate ) const
         {
             const sal_Int32 nConstBufferSize = 32000;
             SvStream *pStream = new SvMemoryStream( 32000, 32000 );
-            sal_Int32 nRead=0;
-            uno::Sequence < sal_Int8 > aSequence ( nConstBufferSize );
-            do
+            try
             {
-                nRead = xStream->readBytes ( aSequence, nConstBufferSize );
-                pStream->Write( aSequence.getConstArray(), nRead );
+                sal_Int32 nRead=0;
+                uno::Sequence < sal_Int8 > aSequence ( nConstBufferSize );
+                do
+                {
+                    nRead = xStream->readBytes ( aSequence, nConstBufferSize );
+                    pStream->Write( aSequence.getConstArray(), nRead );
+                }
+                while ( nRead == nConstBufferSize );
+                pStream->Seek(0);
+                return pStream;
             }
-            while ( nRead == nConstBufferSize );
-            pStream->Seek(0);
-            return pStream;
+            catch (const uno::Exception& ex)
+            {
+                delete pStream;
+                xStream.clear();
+            }
         }
     }
 
