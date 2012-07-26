196ec14db1d4f39e25c83517cc7bad4d78ee6572
Armin Le Grand <alg@apache.org>
2012-07-26 08:16:50 +0000

    Resolves: #i118267# Add undo actions for adapted arrowhead sizes...
    when linewidth is changed
    
    (cherry picked from commit 8e63413dbf514303095790630a2d77d854c3da95)
    
    Change-Id: I538805726ca29e3cdb05dc32577b91b7157d0e0f

--- sd/source/ui/view/drviews2.cxx
+++ sd/source/ui/view/drviews2.cxx
@@ -257,8 +257,8 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                     SdrObject* pObj = NULL;
                     const SdrMarkList& rMarkList = mpDrawView->GetMarkedObjectList();
                     sal_uLong nCount = rMarkList.GetMarkCount();
-
-                    sal_Int32 nNewLineWidth = ((const XLineWidthItem&)rReq.GetArgs()->Get(XATTR_LINEWIDTH)).GetValue();
+                    const sal_Int32 nNewLineWidth(((const XLineWidthItem&)rReq.GetArgs()->Get(XATTR_LINEWIDTH)).GetValue());
+                    const bool bUndo(mpDrawView->IsUndoEnabled());
 
                     for (sal_uLong i=0; i<nCount; i++)
                     {
@@ -295,8 +295,27 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                             }
 
                             if(bSetItemSet)
+                            {
+                                if(bUndo)
+                                {
+                                    if(!bMergeUndo)
+                                    {
+                                        pUndoManager->EnterListAction( String(), String() );
+                                        mpDrawView->BegUndo();
+                                        bMergeUndo = sal_True;
+                                    }
+
+                                    mpDrawView->AddUndo(GetDoc()->GetSdrUndoFactory().CreateUndoAttrObject(*pObj));
+                                }
+
                                 pObj->SetMergedItemSet(aAttr);
+                            }
                         }
+                    }
+
+                    if(bMergeUndo)
+                    {
+                        mpDrawView->EndUndo();
                     }
                 }
 
@@ -306,7 +325,6 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                     SdrObject* pObj = NULL;
                     const SdrMarkList& rMarkList = mpDrawView->GetMarkedObjectList();
                     sal_uLong nCount = rMarkList.GetMarkCount();
-
                     const bool bUndo = mpDrawView->IsUndoEnabled();
 
                     for (sal_uLong i=0; i<nCount; i++)
@@ -319,10 +337,10 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                         {
                             aAttr.Put(pObj->GetMergedItemSet());
 
-                            const XFillStyleItem& rFillStyle =
-                            (const XFillStyleItem&) aAttr.Get(XATTR_FILLSTYLE);
+                            const XFillStyleItem& rFillStyle = (const XFillStyleItem&) aAttr.Get(XATTR_FILLSTYLE);
+                            const XLineStyleItem& rLineStyle = (const XLineStyleItem&) aAttr.Get(XATTR_LINESTYLE);
 
-                            if (rFillStyle.GetValue() == XFILL_NONE)
+                            if(XFILL_NONE == rFillStyle.GetValue() && XLINE_NONE == rLineStyle.GetValue())
                             {
                                 if( bUndo )
                                 {
