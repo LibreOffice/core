996e1931e0a57e3c37b75308a7e1c391f83d16b0
Michael Stahl <mstahl@redhat.com>
2012-07-27 21:39:39 +0200

    gbuild: WinResTarget: fix dep-target dependencies:
    Windows specific fallout of 8b5a984d45005d3df1c89eae897d6e04612625d8,
    similar to 236055746ffe92ce3b2a7a9316b1083da046deb9; can be observed by
    building with "make check" from scratch, on the "subsequentcheck" target
    one res file depends on some lngconvex executable by way of a custom
    target in shell, which causes udkapi to rebuild itself before the make
    restart.
    
    Change-Id: I14f370a66f4ac5088da4af7eceede09f5a6bae84
    (cherry picked from commit 8a05f1265e6307c34b2ffdb5fc5279870c8397b7)
    
    Signed-off-by: David Tardon <dtardon@redhat.com>

--- solenv/gbuild/WinResTarget.mk
+++ solenv/gbuild/WinResTarget.mk
@@ -27,7 +27,7 @@ endif
 endef
 
 ifeq ($(gb_FULLDEPS),$(true))
-$(call gb_WinResTarget_get_dep_target,%) : $(call gb_WinResTarget_get_target,%)
+$(call gb_WinResTarget_get_dep_target,%) :
 	$(call gb_WinResTarget__command_dep,$@,$*,$<)
 endif
 
