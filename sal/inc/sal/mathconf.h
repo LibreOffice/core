6247f4889a8b8769c48f05691dcd8db514d0c444
Tor Lillqvist <tml@iki.fi>
2012-07-30 15:41:55 +0300

    Work around isfinite crack with NDK r8b and the 4.6 toolchain
    Change-Id: I66e499d942b34d27e765d00f1ddfb3bbd1f78480

--- sal/inc/sal/mathconf.h
+++ sal/inc/sal/mathconf.h
@@ -63,7 +63,8 @@ extern "C" {
 
 
 /* SAL_MATH_FINITE(d): test double d on INFINITY, NaN et al. */
-#if !defined SOLARIS && defined(__cplusplus) \
+#if !defined SOLARIS && !defined ANDROID \
+                     && defined(__cplusplus) \
                      && ( defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L )
 #define SAL_MATH_FINITE(d) std::isfinite(d)
 #elif defined( WNT)
