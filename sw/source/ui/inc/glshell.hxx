d1a66813a0a4f28af34728efa48a97e4a48a9bf8
Caolán McNamara <caolanm@redhat.com>
2012-08-02 12:06:58 +0100

    strange header guard
    Change-Id: I981e2e9f18b6ebde4d129746fdc30f360956848b

--- sw/source/ui/inc/glshell.hxx
+++ sw/source/ui/inc/glshell.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef _AUTODOC_HXX
-#define _AUTODOC_HXX
+#ifndef _GLSHELL_HXX
+#define _GLSHELL_HXX
 #include "wdocsh.hxx"
 
 class SwGlosDocShell : public SwDocShell
@@ -83,25 +83,5 @@ public:
 };
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
