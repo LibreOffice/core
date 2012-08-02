dac29877d1c92aa55215165976ca87d6a50f6698
Michael Stahl <mst@openoffice.org>
2012-08-02 12:10:01 +0200

    mythes: fix some warnings
    Signed-off-by: Petr Mladek <pmladek@suse.cz>

--- mythes/makefile.mk
+++ mythes/makefile.mk
@@ -43,6 +43,7 @@ ADDITIONAL_FILES += makefile.mk
 
 PATCH_FILES=\
     mythes-1.2.0-vanilla-th-gen-idx.patch \
+    mythes-1.2.2-overflow.patch \
     mythes-1.2.0-makefile-mk.diff \
     mythes-1.2.0-android.patch
 
