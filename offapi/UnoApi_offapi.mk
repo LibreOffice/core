0debd6fd58088612b9992abcddfef55ed2f70ffb
Jean-Tiare LE BIGOT <admin@jtlebi.fr>
2012-08-10 00:00:54 -0400

    Applied fdo#46808 to offapi/auth module
    For auth module, this is more like a cosmetic change :)
    Anyway, all the tests passes and this is one more module
    migrated.
    
    Change-Id: Iaef3b4ab8c29526be8d0a388fc17e6af5d293c81

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -2,7 +2,7 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
@@ -38,9 +38,13 @@ $(eval $(call gb_UnoApi_set_include,offapi,\
 	-I$(OUTDIR)/idl \
 ))
 
-
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/animations,\
 	TargetPropertiesCreator \
+))
+$(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/auth,\
+	SSOExceptions \
+	SSOManagerFactory \
+	SSOPasswordCache \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/awt,\
 	AsyncCallback \
@@ -294,11 +298,6 @@ $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/accessi
 	AccessBridge \
 	Accessible \
 	AccessibleContext \
-))
-$(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/auth,\
-	SSOExceptions \
-	SSOManagerFactory \
-	SSOPasswordCache \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/awt,\
 	AccessibleButton \
