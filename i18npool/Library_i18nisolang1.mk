57c3b583f1f69edd32b2a54253850e1b3b202255
Eike Rathke <erack@redhat.com>
2012-08-11 13:49:25 +0200

    langtag: libxml2 only used for liblangtag
    Change-Id: I4bf7bc4f58bac7675cf694dc206e6ba119a6354e

--- i18npool/Library_i18nisolang1.mk
+++ i18npool/Library_i18nisolang1.mk
@@ -58,8 +58,7 @@ ifeq ($(ENABLE_LIBLANGTAG),YES)
 $(eval $(call gb_Library_add_defs,i18nisolang1,-DENABLE_LIBLANGTAG))
 $(eval $(call gb_Library_use_external,i18nisolang1,glib))
 $(eval $(call gb_Library_use_external,i18nisolang1,liblangtag))
-endif
-
 $(eval $(call gb_Library_use_external,i18nisolang1,libxml2))
+endif
 
 # vim: set noet sw=4 ts=4:
