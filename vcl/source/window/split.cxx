7ecd1c14977110fea447ccee39cc03a15ca42c10
Jean-Tiare LE BIGOT <admin@jtlebi.fr>
2012-08-12 17:08:16 -0400

    Set ide sizer color to dark grey; was dark black
    The color setting was picked from SplitWindow::Paint()
    for visual consistency with the sizers used in Impress
    for example.
    
    This commit also resurects presumably dead code.
    In fact GDB let me see that only the short path
    was taken.
    
    This has an impact on Calc's window splitters and
    on more visibly on the basic IDE which now looks
    much nicer :)
    
    EDITS:
     restricted this commit impact to IDE as a poc
    
    Change-Id: I9a051fd894feccff4bbc7db7efe1160cb2c3a2c6

--- vcl/source/window/split.cxx
+++ vcl/source/window/split.cxx
@@ -158,6 +158,9 @@ Splitter::Splitter( Window* pParent, WinBits nStyle ) :
 {
     ImplInitSplitterData();
     ImplInit( pParent, nStyle );
+
+    SetLineColor();
+    SetFillColor();
 }
 
 // -----------------------------------------------------------------------
@@ -748,46 +751,26 @@ void Splitter::DataChanged( const DataChangedEvent& rDCEvt )
 
 void Splitter::Paint( const Rectangle& rPaintRect )
 {
-    if( HasFocus() || mbKbdSplitting )
+    DrawRect( rPaintRect );
+
+    Polygon aPoly( rPaintRect );
+    PolyPolygon aPolyPoly( aPoly );
+    DrawTransparent( aPolyPoly, 85 );
+
+    if( mbKbdSplitting )
     {
-        Color oldFillCol = GetFillColor();
-        Color oldLineCol = GetLineColor();
+        LineInfo aInfo( LINE_DASH );
+        //aInfo.SetDashLen( 2 );
+        //aInfo.SetDashCount( 1 );
+        aInfo.SetDistance( 1 );
+        aInfo.SetDotLen( 2 );
+        aInfo.SetDotCount( 3 );
 
-        SetLineColor();
-        SetFillColor( GetSettings().GetStyleSettings().GetFaceColor() );
-        DrawRect( rPaintRect );
-
-        Color aSelectionBorderCol( GetSettings().GetStyleSettings().GetActiveColor() );
-        SetFillColor( aSelectionBorderCol );
-        SetLineColor();
-
-        Polygon aPoly( rPaintRect );
-        PolyPolygon aPolyPoly( aPoly );
-        DrawTransparent( aPolyPoly, 85 );
-
-        SetLineColor( aSelectionBorderCol );
-        SetFillColor();
-
-        if( mbKbdSplitting )
-        {
-            LineInfo aInfo( LINE_DASH );
-            //aInfo.SetDashLen( 2 );
-            //aInfo.SetDashCount( 1 );
-            aInfo.SetDistance( 1 );
-            aInfo.SetDotLen( 2 );
-            aInfo.SetDotCount( 1 );
-
-            DrawPolyLine( aPoly, aInfo );
-        }
-        else
-            DrawRect( rPaintRect );
-
-        SetFillColor( oldFillCol);
-        SetLineColor( oldLineCol);
+        DrawPolyLine( aPoly, aInfo );
     }
     else
     {
-        Window::Paint( rPaintRect );
+        DrawRect( rPaintRect );
     }
 }
 
