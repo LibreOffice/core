bfb8f56816206d1ec1f9e3a61c3e9ee2271cdca3
Norbert Thiebaud <nthiebaud@gmail.com>
2012-08-20 22:01:08 -0500

    gridfixes: #i112465# delegate RequestHelp to the base class,
    when we do not have own help to display
    
    Change-Id: I953144aa0921e849df14e9de31010e8904dffc5f

--- svtools/source/table/tabledatawindow.cxx
+++ svtools/source/table/tabledatawindow.cxx
@@ -142,18 +142,26 @@ namespace svt { namespace table
 
         if ( !sHelpText.isEmpty() )
         {
+            // hide the standard (singleton) help window, so we do not have two help windows open at the same time
+            Help::HideBalloonAndQuickHelp();
+
             Rectangle const aControlScreenRect(
                 OutputToScreenPixel( Point( 0, 0 ) ),
                 GetOutputSizePixel()
             );
 
             if ( m_nTipWindowHandle )
+            {
                 Help::UpdateTip( m_nTipWindowHandle, this, aControlScreenRect, sHelpText );
+            }
             else
                 m_nTipWindowHandle = Help::ShowTip( this, aControlScreenRect, sHelpText, nHelpStyle );
         }
         else
+        {
             impl_hideTipWindow();
+            Window::RequestHelp( rHEvt );
+        }
     }
 
     //------------------------------------------------------------------------------------------------------------------
