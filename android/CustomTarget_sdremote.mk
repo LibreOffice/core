fae45f59b85b46b01e6be7f35b7f60260d67e064
Tor Lillqvist <tml@iki.fi>
2012-08-21 13:52:05 +0300

    Add the android module to gbuild
    Just build the sdremote app for now. Note that this is a pure Java
    app with no dependencies on (native) code (or Java code, for that
    matter) from rest of LO.
    
    Probably should drop the separate android/sdremote/Makfile and just do
    what it does in android/CustomTarget_sdremote.mk instead.
    
    Adding other Android apps (well, the LibreOffice4Android one likely)
    to gbuild will require more complexity as they bundle native code, and
    thus should depend on other modules first having been built. If one
    wants to go really fancy, one could of course depend on the specific
    libraries (and other files) being bundled. Let's see...
    
    Change-Id: If10761479f348c4993eec40b7f8346edb77f0e0d

---
+++ android/CustomTarget_sdremote.mk
@@ -0,0 +1,19 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+$(eval $(call gb_CustomTarget_CustomTarget,android/sdremote))
+
+sdremote_DIR := $(call gb_CustomTarget_get_workdir,android/sdremote)
+
+$(call gb_CustomTarget_get_target,android/sdremote) : $(sdremote_DIR)/done
+
+$(sdremote_DIR)/done : $(gb_Helper_PHONY)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),MAK,1)
+	cd $(SRCDIR)/android/sdremote && $(MAKE)
+
+# vim: set noet sw=4 ts=4:
---
+++ android/Module_android.mk
@@ -0,0 +1,19 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+$(eval $(call gb_Module_Module,android))
+
+ifeq ($(OS),ANDROID)
+
+$(eval $(call gb_Module_add_targets,android,\
+    CustomTarget_sdremote \
+))
+
+endif
+
+# vim: set noet sw=4 ts=4:
