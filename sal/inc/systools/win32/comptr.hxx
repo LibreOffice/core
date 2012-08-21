95b26a37eec7800a337f4522c9235f6e6306de32
Luboš Luňák <l.lunak@suse.cz>
2012-08-21 10:24:58 +0200

    mingw doesn't know __uuidof
    Copied from fpicker/source/win32/filepicker/comptr.hxx, which
    otherwise seems to be the same file.
    
    Change-Id: I51ca96c1280e75f9318fcbc74997b2bf755fff95

--- sal/inc/systools/win32/comptr.hxx
+++ sal/inc/systools/win32/comptr.hxx
@@ -174,12 +174,14 @@ class ComPtr
             }
         }
 
+#ifndef __MINGW32__
         //---------------------------------------------------------------------
         template< class T_QUERYINTERFACE >
         HRESULT query(T_QUERYINTERFACE** pQuery)
         {
             return m_pInterface->QueryInterface(__uuidof(T_QUERYINTERFACE), (void**)pQuery);
         }
+#endif
 
         //---------------------------------------------------------------------
         HRESULT query(REFIID rIID  ,
