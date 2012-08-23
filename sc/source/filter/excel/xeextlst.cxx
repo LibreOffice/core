a2cc10e02375aebd32ba0df49d83ee01235a709b
Markus Mohrhard <markus.mohrhard@googlemail.com>
2012-08-23 16:03:38 +0200

    add missing licence header

--- sc/source/filter/excel/xeextlst.cxx
+++ sc/source/filter/excel/xeextlst.cxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "xeextlst.hxx"
 #include "xeroot.hxx"
 #include "xehelper.hxx"
@@ -244,3 +253,5 @@ XclExpExtRef XclExtLst::GetItem( XclExpExtType eType )
 
     return XclExpExtRef();
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
