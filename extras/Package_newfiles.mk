768d02c93c51fb606cf7221d0fd7a0c9dd3e363d
David Tardon <dtardon@redhat.com>
2012-08-24 13:32:40 +0200

    soffice.html has been removed
    Change-Id: Id564b3a34524a754261211d15abaadbdb1898061

--- extras/Package_newfiles.mk
+++ extras/Package_newfiles.mk
@@ -10,7 +10,6 @@
 $(eval $(call gb_Package_Package,extras_newfiles,$(SRCDIR)/extras/source/shellnew))
 
 $(eval $(call gb_Package_add_files,extras_newfiles,pck,\
-    soffice.html \
     soffice.odg \
     soffice.odp \
     soffice.ods \
