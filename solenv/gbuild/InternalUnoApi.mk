cce1368dc1c3af6b18c033694700f66d81ec0be5
Matúš Kukan <matus.kukan@gmail.com>
2012-08-25 13:19:26 +0200

    do not copy these xml files into solver
    Change-Id: I92822085a65a179764b307b1344b09057e1d3b04

--- solenv/gbuild/InternalUnoApi.mk
+++ solenv/gbuild/InternalUnoApi.mk
@@ -113,12 +113,6 @@ endef
 
 define gb_InternalUnoApi_set_xmlfile
 $(call gb_UnoApiTarget_set_xmlfile,$(1)_out,$(2))
-$(call gb_Package_Package_internal,$(1)_xml,$(SRCDIR))
-$(call gb_Package_add_file,$(1)_xml,xml/$(notdir $(2)),$(2))
-$(call gb_InternalUnoApi_get_target,$(1)) :| \
-	$(call gb_Package_get_target,$(1)_xml)
-$(call gb_InternalUnoApi_get_clean_target,$(1)) : \
-	$(call gb_Package_get_clean_target,$(1)_xml)
 
 endef
 
