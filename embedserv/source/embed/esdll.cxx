5084b6dca889e83da13c917a15fafd10a698f326
Peter Foley <pefoley2@verizon.net>
2012-08-25 10:31:14 -0400

    fix embedserv build with Win8 SDK
    Change-Id: I3a51a563fa3c2a57cbbe44087f0f4958c2b41a69
    Reviewed-on: https://gerrit.libreoffice.org/790
    Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
    Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

--- embedserv/source/embed/esdll.cxx
+++ embedserv/source/embed/esdll.cxx
@@ -18,7 +18,7 @@
  */
 
 #define STRICT
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0403
 #define _WIN32_DCOM
 #if defined(_MSC_VER) && (_MSC_VER > 1310)
 #pragma warning(disable : 4917 4555)
