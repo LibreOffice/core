53ca7b480f8fb23b04023faee74fbfa4b77bfa02
Peter Foley <pefoley2@verizon.net>
2012-08-29 19:50:38 -0400

    work-around really weird bug in VS2012
    Change-Id: I7092bbcbddec179c62c2817b069f757863ca5982

--- sc/source/core/data/fillinfo.cxx
+++ sc/source/core/data/fillinfo.cxx
@@ -1,4 +1,5 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
