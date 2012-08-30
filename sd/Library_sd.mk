6285e9da02445cace8a25d5318c01419ad3d717e
Tor Lillqvist <tml@iki.fi>
2012-08-30 12:57:59 +0300

    Use gio only on Linux
    Change-Id: Ice64f084a04b879ccaec66f9a679f82ab9001547

--- sd/Library_sd.mk
+++ sd/Library_sd.mk
@@ -107,9 +107,14 @@ $(eval $(call gb_Library_use_libraries,sd,\
 ))
 
 $(eval $(call gb_Library_use_externals,sd,\
- gio \
  libxml2 \
 ))
+
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Library_use_externals,sd,\
+ gio \
+))
+endif
 
 ifeq ($(OS),WNT)
 $(eval $(call gb_Library_use_libraries,sd,\
