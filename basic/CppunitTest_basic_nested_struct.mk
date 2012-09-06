30affe2f2fde8add8b1e89799e0e09515ccd9e54
Takeshi Abe <tabe@fixedpoint.jp>
2012-09-06 19:09:57 +0900

    Add missing header/footer comment
    Change-Id: I0ca050a8d0e9f92308ecf0525ee7f0c32ddd0492

--- basic/CppunitTest_basic_nested_struct.mk
+++ basic/CppunitTest_basic_nested_struct.mk
@@ -1,3 +1,12 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 $(eval $(call gb_CppunitTest_CppunitTest,basic_nested_struct))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,basic_nested_struct, \
@@ -47,3 +56,5 @@ $(eval $(call gb_CppunitTest_use_components,basic_nested_struct,\
     ucb/source/core/ucb1 \
 ))
 $(eval $(call gb_CppunitTest_use_configuration,basic_nested_struct))
+
+# vim: set noet sw=4 ts=4:
