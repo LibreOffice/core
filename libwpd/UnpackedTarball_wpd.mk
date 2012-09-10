fcf415df476c56a7ed4e5fb30e731a263da61e52
Michael Stahl <mstahl@redhat.com>
2012-09-10 15:24:37 +0200

    libwpd: MSVC warning C4310: cast truncates constant value
    Change-Id: Ibd6c7299b612d05d4c00ddfe47b51f45e3ae07d8

--- libwpd/UnpackedTarball_wpd.mk
+++ libwpd/UnpackedTarball_wpd.mk
@@ -14,6 +14,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,wpd,$(WPD_TARBALL)))
 $(eval $(call gb_UnpackedTarball_add_patches,wpd,\
 	libwpd/libwpd-0.9.4.patch \
 	libwpd/libwpd-0.9.4-warning.patch \
+	libwpd/libwpd-0.9.4-msvc-warning.patch \
 ))
 # libwpd/libwpd-0.9.4-warning.patch: -Werror,-Wunused-private-field
 
