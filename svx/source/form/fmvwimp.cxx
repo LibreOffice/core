d1c1cce4ec0222ac50f9f78ddf5f4ce7aa7377ab
Caolán McNamara <caolanm@redhat.com>
2012-09-10 22:55:02 +0100

    Resolves: rhbz#855972 crash on switching to outline view
    Change-Id: Ic2675e9d1d8ce6bb67e9c9e4db292f79c4240409

--- svx/source/form/fmvwimp.cxx
+++ svx/source/form/fmvwimp.cxx
@@ -938,8 +938,10 @@ IMPL_LINK(FmXFormView, OnAutoFocus, void*, /*EMPTYTAG*/)
     do
     {
 
+    SdrPageView *pPageView = m_pView ? m_pView->GetSdrPageView() : NULL;
+    SdrPage *pSdrPage = pPageView ? pPageView->GetPage() : NULL;
     // get the forms collection of the page we belong to
-    FmFormPage* pPage = m_pView ? PTR_CAST( FmFormPage, m_pView->GetSdrPageView()->GetPage() ) : NULL;
+    FmFormPage* pPage = PTR_CAST( FmFormPage, pSdrPage );
     Reference< XIndexAccess > xForms( pPage ? Reference< XIndexAccess >( pPage->GetForms(), UNO_QUERY ) : Reference< XIndexAccess >() );
 
     const PFormViewPageWindowAdapter pAdapter = m_aPageWindowAdapters.empty() ? NULL : m_aPageWindowAdapters[0];
