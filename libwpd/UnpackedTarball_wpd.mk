b45834bf6479e614353e6ff19b2883bac5b5370f
Caolán McNamara <caolanm@redhat.com>
2012-09-12 23:12:15 +0100

    WaE: with DEBUG defined
    Change-Id: I2bf950b8ac644c911050ce01282ac988167d28f9

--- libwpd/UnpackedTarball_wpd.mk
+++ libwpd/UnpackedTarball_wpd.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,wpd,\
 	libwpd/libwpd-0.9.4.patch \
 	libwpd/libwpd-0.9.4-warning.patch \
 	libwpd/libwpd-0.9.4-msvc-warning.patch \
+	libwpd/libwpd-0.9.4-gcc-osldebug2-warning.patch \
 ))
 # libwpd/libwpd-0.9.4-warning.patch: see upstream
 #  <http://libwpd.git.sourceforge.net/git/gitweb.cgi?p=libwpd/libwpd;a=commit;
@@ -23,5 +24,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,wpd,\
 #  <http://libwpd.git.sourceforge.net/git/gitweb.cgi?p=libwpd/libwpd;a=commit;
 #  h=fb3827aeb5de64ed84eb1140bae9e32e50ca5133> "MSVC warning C4310: cast
 #  truncates constant value"
+# libwpd/libwpd-0.9.4-gcc-osldebug2-warning.patch
+#  mailed to Fridrich cause there's no apparent way to submit a patch
 
 # vim: set noet sw=4 ts=4:
