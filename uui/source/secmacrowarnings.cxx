LibreOffice/core

commit 197a79e57131c976a489882479a679ba2ab61b82

fdo#46808, Adapt DocumentDigitalSignatures UNO service to new style
Rename service from "Decryptor" to "DocumentDigitalSignatures" to be consistent
with the other UNO services, and with it's own header file name.
Add service constructor for passing in arguments.

Change-Id: Iaca0c40bb2f1a1af2ef9ca29361bfd023ade2678

uui/source/secmacrowarnings.cxx
@@ -19,7 +19,7 @@
 
 #include <com/sun/star/xml/crypto/XSecurityEnvironment.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
+#include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <comphelper/sequence.hxx>
 #include "comphelper/documentconstants.hxx"
 #include <comphelper/processfactory.hxx>
@@ -116,7 +116,7 @@ IMPL_LINK_NOARG(MacroWarning, ViewSignsBtnHdl)
     uno::Sequence< uno::Any > aArgs( 1 );
     aArgs[0] = uno::makeAny( maODFVersion );
     uno::Reference< security::XDocumentDigitalSignatures > xD(
-        comphelper::getProcessServiceFactory()->createInstanceWithArguments( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( "com.sun.star.security.DocumentDigitalSignatures" ) ), aArgs ), uno::UNO_QUERY );
+        security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext()) );
     if( xD.is() )
     {
         if( mxCert.is() )
@@ -135,19 +135,16 @@ IMPL_LINK_NOARG(MacroWarning, EnableBtnHdl)
         uno::Sequence< uno::Any > aArgs( 1 );
         aArgs[0] = uno::makeAny( maODFVersion );
         uno::Reference< security::XDocumentDigitalSignatures > xD(
-            comphelper::getProcessServiceFactory()->createInstanceWithArguments( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( "com.sun.star.security.DocumentDigitalSignatures" ) ), aArgs ), uno::UNO_QUERY );
-        if( xD.is() )
+            security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext()) );
+        if( mxCert.is() )
+            xD->addAuthorToTrustedSources( mxCert );
+        else if( mxStore.is() )
         {
-            if( mxCert.is() )
-                xD->addAuthorToTrustedSources( mxCert );
-            else if( mxStore.is() )
-            {
-                DBG_ASSERT( mpInfos, "-MacroWarning::EnableBtnHdl(): no infos, search in nirvana..." );
+            DBG_ASSERT( mpInfos, "-MacroWarning::EnableBtnHdl(): no infos, search in nirvana..." );
 
-                sal_Int32   nCnt = mpInfos->getLength();
-                for( sal_Int32 i = 0 ; i < nCnt ; ++i )
-                    xD->addAuthorToTrustedSources( (*mpInfos)[ i ].Signer );
-            }
+            sal_Int32   nCnt = mpInfos->getLength();
+            for( sal_Int32 i = 0 ; i < nCnt ; ++i )
+                xD->addAuthorToTrustedSources( (*mpInfos)[ i ].Signer );
         }
     }
 

cui/source/options/optinet2.cxx
@@ -64,7 +64,7 @@
 #include <svx/svxids.hrc> // slot ids, mostly for changetracking
 
 // for security TP
-#include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
+#include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 
 #ifdef UNX
 #include <sys/stat.h>
@@ -867,9 +867,8 @@ IMPL_LINK_NOARG(SvxSecurityTabPage, MacroSecPBHdl)
     try
     {
         Reference< security::XDocumentDigitalSignatures > xD(
-            comphelper::getProcessServiceFactory()->createInstance( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( "com.sun.star.security.DocumentDigitalSignatures" ) ) ), UNO_QUERY );
-        if ( xD.is() )
-            xD->manageTrustedSources();
+            security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext() ) );
+        xD->manageTrustedSources();
     }
     catch (const Exception& e)
     {

filter/source/pdf/impdialog.cxx
@@ -43,7 +43,7 @@
 #include "com/sun/star/container/XIndexAccess.hpp"
 #include "com/sun/star/frame/XController.hpp"
 #include "com/sun/star/view/XSelectionSupplier.hpp"
-#include "com/sun/star/security/XDocumentDigitalSignatures.hpp"
+#include "com/sun/star/security/DocumentDigitalSignatures.hpp"
 #include "com/sun/star/security/XCertificate.hpp"
 
 #include <boost/shared_ptr.hpp>
@@ -1709,17 +1709,9 @@ ImpPDFTabSigningPage::~ImpPDFTabSigningPage()
 IMPL_LINK_NOARG( ImpPDFTabSigningPage, ClickmaPbSignCertSelect )
 {
 
-    uno::Sequence< uno::Any > aArgs( 2 );
-    aArgs[0] <<= rtl::OUString("1.2");
-    aArgs[1] <<= sal_False;
-
     Reference< security::XDocumentDigitalSignatures > xSigner(
-        comphelper::getProcessServiceFactory()->createInstanceWithArguments(
-            rtl::OUString( "com.sun.star.security.DocumentDigitalSignatures"  ), aArgs ),
-        uno::UNO_QUERY );
-
-    if ( !xSigner.is() )
-        return 0;
+        security::DocumentDigitalSignatures::createWithVersion(
+            comphelper::getProcessComponentContext(), rtl::OUString("1.2") ) );
 
     maSignCertificate = xSigner->chooseCertificate();
 

offapi/UnoApi_offapi.mk
@@ -214,6 +214,9 @@ $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/resource,\
 	StringResourceWithLocation \
 	StringResourceWithStorage \
 ))
+$(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/security,\
+	DocumentDigitalSignatures \
+))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/scanner,\
 	ScannerManager \
 ))
@@ -1045,9 +1048,6 @@ $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/sdbcx,\
 	UserDescriptor \
 	View \
 	ViewDescriptor \
-))
-$(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/security,\
-	DocumentDigitalSignatures \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/setup,\
 	ProductRegistration \

offapi/com/sun/star/security/DocumentDigitalSignatures.idl
@@ -33,8 +33,22 @@ module com { module sun { module star { module security {
  * Service of DocumentDigitalSignatures
  */
 
-    service Decryptor {
-    interface XDocumentDigitalSignatures ;
+service DocumentDigitalSignatures : XDocumentDigitalSignatures
+{
+    /* default constructor */
+    createDefault();
+
+    /**
+        @param ODFVersion
+            the version of the signature
+     */
+    createWithVersion([in] string ODFVersion);
+
+    /**
+        @param ODFVersion
+            the version of the signature
+     */
+    createWithVersionAndValidSignature([in] string ODFVersion, [in] boolean HasValidDocumentSignature);
 };
 
 } ; } ; } ; } ;

sfx2/source/dialog/dinfdlg.cxx
@@ -33,7 +33,7 @@
 
 #include <comphelper/string.hxx>
 #include <com/sun/star/security/DocumentSignatureInformation.hpp>
-#include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
+#include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <unotools/syslocale.hxx>
 #include <rtl/math.hxx>
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
@@ -938,26 +938,23 @@ void SfxDocumentPage::ImplUpdateSignatures()
         if ( pMedium && !pMedium->GetName().isEmpty() && pMedium->GetStorage().is() )
         {
             Reference< security::XDocumentDigitalSignatures > xD(
-                comphelper::getProcessServiceFactory()->createInstance( rtl::OUString( RTL_CONSTASCII_USTRINGPARAM ( "com.sun.star.security.DocumentDigitalSignatures" ) ) ), uno::UNO_QUERY );
+                security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext()) );
 
-            if ( xD.is() )
+            String s;
+            Sequence< security::DocumentSignatureInformation > aInfos;
+            aInfos = xD->verifyDocumentContentSignatures( pMedium->GetZipStorageToSign_Impl(),
+                                                            uno::Reference< io::XInputStream >() );
+            if ( aInfos.getLength() > 1 )
+                s = aMultiSignedStr;
+            else if ( aInfos.getLength() == 1 )
             {
-                String s;
-                Sequence< security::DocumentSignatureInformation > aInfos;
-                aInfos = xD->verifyDocumentContentSignatures( pMedium->GetZipStorageToSign_Impl(),
-                                                                uno::Reference< io::XInputStream >() );
-                if ( aInfos.getLength() > 1 )
-                    s = aMultiSignedStr;
-                else if ( aInfos.getLength() == 1 )
-                {
-                    rtl::OUString aCN_Id("CN");
-                    const security::DocumentSignatureInformation& rInfo = aInfos[ 0 ];
-                    s = GetDateTimeString( rInfo.SignatureDate, rInfo.SignatureTime );
-                    s.AppendAscii( ", " );
-                    s += GetContentPart( rInfo.Signer->getSubjectName(), aCN_Id );
-                }
-                aSignedValFt.SetText( s );
+                rtl::OUString aCN_Id("CN");
+                const security::DocumentSignatureInformation& rInfo = aInfos[ 0 ];
+                s = GetDateTimeString( rInfo.SignatureDate, rInfo.SignatureTime );
+                s.AppendAscii( ", " );
+                s += GetContentPart( rInfo.Signer->getSubjectName(), aCN_Id );
             }
+            aSignedValFt.SetText( s );
         }
     }
 }

sfx2/source/doc/docfile.cxx
@@ -62,7 +62,7 @@
 #include <cppuhelper/implbase1.hxx>
 #include <com/sun/star/beans/PropertyValue.hpp>
 #include <com/sun/star/security/DocumentSignatureInformation.hpp>
-#include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
+#include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <tools/urlobj.hxx>
 #include <unotools/tempfile.hxx>
 #include <comphelper/processfactory.hxx>
@@ -3371,105 +3371,97 @@ sal_Bool SfxMedium::SignContents_Impl( sal_Bool bScriptingContent, const ::rtl::
     {
         // The component should know if there was a valid document signature, since
         // it should show a warning in this case
-        uno::Sequence< uno::Any > aArgs( 2 );
-        aArgs[0] <<= aODFVersion;
-        aArgs[1] <<= bHasValidDocumentSignature;
-        ::com::sun::star::uno::Reference< ::com::sun::star::security::XDocumentDigitalSignatures > xSigner(
-            comphelper::getProcessServiceFactory()->createInstanceWithArguments(
-                rtl::OUString( "com.sun.star.security.DocumentDigitalSignatures"  ),
-                aArgs ),
-            ::com::sun::star::uno::UNO_QUERY );
+        uno::Reference< security::XDocumentDigitalSignatures > xSigner(
+            security::DocumentDigitalSignatures::createWithVersionAndValidSignature(
+                comphelper::getProcessComponentContext(), aODFVersion, bHasValidDocumentSignature ) );
 
-        if ( xSigner.is() )
+        uno::Reference< embed::XStorage > xWriteableZipStor;
+        if ( !IsReadOnly() )
         {
-            uno::Reference< embed::XStorage > xWriteableZipStor;
-            if ( !IsReadOnly() )
+            // we can reuse the temporary file if there is one already
+            CreateTempFile( false );
+            GetMedium_Impl();
+
+            try
             {
-                // we can reuse the temporary file if there is one already
-                CreateTempFile( false );
-                GetMedium_Impl();
+                if ( !pImp->xStream.is() )
+                    throw uno::RuntimeException();
 
-                try
+                xWriteableZipStor = ::comphelper::OStorageHelper::GetStorageOfFormatFromStream( ZIP_STORAGE_FORMAT_STRING, pImp->xStream );
+                if ( !xWriteableZipStor.is() )
+                    throw uno::RuntimeException();
+
+                uno::Reference< embed::XStorage > xMetaInf = xWriteableZipStor->openStorageElement(
+                                                ::rtl::OUString( "META-INF"  ),
+                                                embed::ElementModes::READWRITE );
+                if ( !xMetaInf.is() )
+                    throw uno::RuntimeException();
+
+                if ( bScriptingContent )
                 {
-                    if ( !pImp->xStream.is() )
-                        throw uno::RuntimeException();
+                    // If the signature has already the document signature it will be removed
+                    // after the scripting signature is inserted.
+                    uno::Reference< io::XStream > xStream(
+                        xMetaInf->openStreamElement( xSigner->getScriptingContentSignatureDefaultStreamName(),
+                                                     embed::ElementModes::READWRITE ),
+                        uno::UNO_SET_THROW );
 
-                    xWriteableZipStor = ::comphelper::OStorageHelper::GetStorageOfFormatFromStream( ZIP_STORAGE_FORMAT_STRING, pImp->xStream );
-                    if ( !xWriteableZipStor.is() )
-                        throw uno::RuntimeException();
-
-                    uno::Reference< embed::XStorage > xMetaInf = xWriteableZipStor->openStorageElement(
-                                                    ::rtl::OUString( "META-INF"  ),
-                                                    embed::ElementModes::READWRITE );
-                    if ( !xMetaInf.is() )
-                        throw uno::RuntimeException();
-
-                    if ( bScriptingContent )
+                    if ( xSigner->signScriptingContent( GetZipStorageToSign_Impl(), xStream ) )
                     {
-                        // If the signature has already the document signature it will be removed
-                        // after the scripting signature is inserted.
-                        uno::Reference< io::XStream > xStream(
-                            xMetaInf->openStreamElement( xSigner->getScriptingContentSignatureDefaultStreamName(),
-                                                         embed::ElementModes::READWRITE ),
-                            uno::UNO_SET_THROW );
+                        // remove the document signature if any
+                        ::rtl::OUString aDocSigName = xSigner->getDocumentContentSignatureDefaultStreamName();
+                        if ( !aDocSigName.isEmpty() && xMetaInf->hasByName( aDocSigName ) )
+                            xMetaInf->removeElement( aDocSigName );
 
-                        if ( xSigner->signScriptingContent( GetZipStorageToSign_Impl(), xStream ) )
-                        {
-                            // remove the document signature if any
-                            ::rtl::OUString aDocSigName = xSigner->getDocumentContentSignatureDefaultStreamName();
-                            if ( !aDocSigName.isEmpty() && xMetaInf->hasByName( aDocSigName ) )
-                                xMetaInf->removeElement( aDocSigName );
+                        uno::Reference< embed::XTransactedObject > xTransact( xMetaInf, uno::UNO_QUERY_THROW );
+                        xTransact->commit();
+                        xTransact.set( xWriteableZipStor, uno::UNO_QUERY_THROW );
+                        xTransact->commit();
 
-                            uno::Reference< embed::XTransactedObject > xTransact( xMetaInf, uno::UNO_QUERY_THROW );
-                            xTransact->commit();
-                            xTransact.set( xWriteableZipStor, uno::UNO_QUERY_THROW );
-                            xTransact->commit();
-
-                            // the temporary file has been written, commit it to the original file
-                            Commit();
-                            bChanges = true;
-                        }
-                    }
-                    else
-                    {
-                         uno::Reference< io::XStream > xStream(
-                            xMetaInf->openStreamElement( xSigner->getDocumentContentSignatureDefaultStreamName(),
-                                                         embed::ElementModes::READWRITE ),
-                            uno::UNO_SET_THROW );
-
-                        if ( xSigner->signDocumentContent( GetZipStorageToSign_Impl(), xStream ) )
-                        {
-                            uno::Reference< embed::XTransactedObject > xTransact( xMetaInf, uno::UNO_QUERY_THROW );
-                            xTransact->commit();
-                            xTransact.set( xWriteableZipStor, uno::UNO_QUERY_THROW );
-                            xTransact->commit();
-
-                            // the temporary file has been written, commit it to the original file
-                            Commit();
-                            bChanges = true;
-                        }
+                        // the temporary file has been written, commit it to the original file
+                        Commit();
+                        bChanges = true;
                     }
                 }
-                catch ( const uno::Exception& )
+                else
                 {
-                    OSL_FAIL( "Couldn't use signing functionality!\n" );
-                }
+                     uno::Reference< io::XStream > xStream(
+                        xMetaInf->openStreamElement( xSigner->getDocumentContentSignatureDefaultStreamName(),
+                                                     embed::ElementModes::READWRITE ),
+                        uno::UNO_SET_THROW );
 
-                CloseAndRelease();
+                    if ( xSigner->signDocumentContent( GetZipStorageToSign_Impl(), xStream ) )
+                    {
+                        uno::Reference< embed::XTransactedObject > xTransact( xMetaInf, uno::UNO_QUERY_THROW );
+                        xTransact->commit();
+                        xTransact.set( xWriteableZipStor, uno::UNO_QUERY_THROW );
+                        xTransact->commit();
+
+                        // the temporary file has been written, commit it to the original file
+                        Commit();
+                        bChanges = true;
+                    }
+                }
             }
-            else
+            catch ( const uno::Exception& )
             {
-                try
-                {
-                    if ( bScriptingContent )
-                        xSigner->showScriptingContentSignatures( GetZipStorageToSign_Impl(), uno::Reference< io::XInputStream >() );
-                    else
-                        xSigner->showDocumentContentSignatures( GetZipStorageToSign_Impl(), uno::Reference< io::XInputStream >() );
-                }
-                catch( const uno::Exception& )
-                {
-                    OSL_FAIL( "Couldn't use signing functionality!\n" );
-                }
+                OSL_FAIL( "Couldn't use signing functionality!\n" );
+            }
+
+            CloseAndRelease();
+        }
+        else
+        {
+            try
+            {
+                if ( bScriptingContent )
+                    xSigner->showScriptingContentSignatures( GetZipStorageToSign_Impl(), uno::Reference< io::XInputStream >() );
+                else
+                    xSigner->showDocumentContentSignatures( GetZipStorageToSign_Impl(), uno::Reference< io::XInputStream >() );
+            }
+            catch( const uno::Exception& )
+            {
+                OSL_FAIL( "Couldn't use signing functionality!\n" );
             }
         }
 

sfx2/source/doc/docmacromode.cxx
@@ -26,7 +26,7 @@
 #include <com/sun/star/task/ErrorCodeRequest.hpp>
 #include <com/sun/star/task/DocumentMacroConfirmationRequest.hpp>
 #include <com/sun/star/task/InteractionClassification.hpp>
-#include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
+#include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <com/sun/star/script/XLibraryQueryExecutable.hpp>
 #include <com/sun/star/script/vba/XVBACompatibility.hpp>
 
@@ -56,6 +56,7 @@ namespace sfx2
     using ::com::sun::star::task::DocumentMacroConfirmationRequest;
     using ::com::sun::star::task::ErrorCodeRequest;
     using ::com::sun::star::uno::Exception;
+    using ::com::sun::star::security::DocumentDigitalSignatures;
     using ::com::sun::star::security::XDocumentDigitalSignatures;
     using ::com::sun::star::security::DocumentSignatureInformation;
     using ::com::sun::star::embed::XStorage;
@@ -224,20 +225,16 @@ namespace sfx2
 
             // get document location from medium name and check whether it is a trusted one
             // the service is created ohne document version, since it is not of interest here
-            ::comphelper::ComponentContext aContext( ::comphelper::getProcessServiceFactory() );
-            Reference< XDocumentDigitalSignatures > xSignatures;
-            if ( aContext.createComponent( "com.sun.star.security.DocumentDigitalSignatures", xSignatures ) )
+            Reference< XDocumentDigitalSignatures > xSignatures(DocumentDigitalSignatures::createDefault(::comphelper::getProcessComponentContext()));
+            INetURLObject aURLReferer( sReferrer );
+
+            ::rtl::OUString aLocation;
+            if ( aURLReferer.removeSegment() )
+                aLocation = aURLReferer.GetMainURL( INetURLObject::NO_DECODE );
+
+            if ( !aLocation.isEmpty() && xSignatures->isLocationTrusted( aLocation ) )
             {
-                INetURLObject aURLReferer( sReferrer );
-
-                ::rtl::OUString aLocation;
-                if ( aURLReferer.removeSegment() )
-                    aLocation = aURLReferer.GetMainURL( INetURLObject::NO_DECODE );
-
-                if ( !aLocation.isEmpty() && xSignatures->isLocationTrusted( aLocation ) )
-                {
-                    return allowMacroExecution();
-                }
+                return allowMacroExecution();
             }
 
             // at this point it is clear that the document is not in the secure location

commit d332c3af32ec3cd5c24618c83261c2f07abd838a

fdo#54858: fix UNO service conversion
regression from 197a79e57131c976a489882479a679ba2ab61b82

Change-Id: I3fbba4da0d293659d8521c768d34de18cd6bc634

uui/source/secmacrowarnings.cxx
@@ -113,10 +113,8 @@ IMPL_LINK_NOARG(MacroWarning, ViewSignsBtnHdl)
 {
     DBG_ASSERT( mxCert.is(), "*MacroWarning::ViewSignsBtnHdl(): no certificate set!" );
 
-    uno::Sequence< uno::Any > aArgs( 1 );
-    aArgs[0] = uno::makeAny( maODFVersion );
     uno::Reference< security::XDocumentDigitalSignatures > xD(
-        security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext()) );
+        security::DocumentDigitalSignatures::createWithVersion(comphelper::getProcessComponentContext(), maODFVersion));
     if( xD.is() )
     {
         if( mxCert.is() )
@@ -132,10 +130,8 @@ IMPL_LINK_NOARG(MacroWarning, EnableBtnHdl)
 {
     if( mbSignedMode && maAlwaysTrustCB.IsChecked() )
     {   // insert path into trusted path list
-        uno::Sequence< uno::Any > aArgs( 1 );
-        aArgs[0] = uno::makeAny( maODFVersion );
         uno::Reference< security::XDocumentDigitalSignatures > xD(
-            security::DocumentDigitalSignatures::createDefault(comphelper::getProcessComponentContext()) );
+            security::DocumentDigitalSignatures::createWithVersion(comphelper::getProcessComponentContext(), maODFVersion));
         if( mxCert.is() )
             xD->addAuthorToTrustedSources( mxCert );
         else if( mxStore.is() )