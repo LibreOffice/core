af875ebebc79023c22c7c78b539ed60e58a57fdf
Stephan Bergmann <sbergman@redhat.com>
2012-09-17 13:26:35 +0200

    Missing dependency
    Change-Id: Iba87094fe3c003f5c49f995a54531462298ea190

--- ucb/Library_ucpodma1.mk
+++ ucb/Library_ucpodma1.mk
@@ -32,6 +32,7 @@ $(eval $(call gb_Library_set_componentfile,ucpodma1,ucb/source/ucp/odma/ucpodma1
 $(eval $(call gb_Library_use_sdk_api,ucpodma1))
 
 $(eval $(call gb_Library_use_libraries,ucpodma1,\
+	comphelper \
 	cppu \
 	cppuhelper \
 	sal \
