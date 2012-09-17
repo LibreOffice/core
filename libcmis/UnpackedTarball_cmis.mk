bcaa72b06e1ce08ff81908609292d54f218ea10e
Cédric Bosdonnat <cedric.bosdonnat@free.fr>
2012-09-17 13:40:36 +0200

    libcmis: silenced some warnings
    Change-Id: I4dd47968b4273804858a47707fdf07a3b5be242a

--- libcmis/UnpackedTarball_cmis.mk
+++ libcmis/UnpackedTarball_cmis.mk
@@ -13,6 +13,10 @@ $(eval $(call gb_UnpackedTarball_set_tarball,cmis,$(CMIS_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,cmis,0))
 
+$(eval $(call gb_UnpackedTarball_add_patches,cmis, \
+	libcmis/libcmis-0.3.0-warnings.patch \
+))
+
 ifeq ($(OS)$(COM),WNTMSC)
 $(eval $(call gb_UnpackedTarball_add_patches,cmis,libcmis/boost-win.patch))
 endif
