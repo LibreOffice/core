d60ea419c57812f611125b0ca51bf897ea7e214b
Michael Stahl <mst@openoffice.org>
2012-09-18 14:19:50 +0200

    rhbz#852128: sw: avoid table undo crash:
    SwUndoTblNdsChg::UndoImpl: to prevent access of deleted table box start
    node, disconnect the SwTableBox from the start node before removing the
    table box nodes.
    This problem was probably introduced with CWS swnewtable
    db4de0817df6906db2743239d45f9f0834ab1e91, which changed the order of the
    removal operations for unknown reasons.
    
    (cherry picked from commit cf842d7c7f9559bfdbb3924cd05a3a50d1dff5e3)
    
    Conflicts:
    	sw/source/core/table/swtable.cxx
    
    Change-Id: Ic59823a84082cc6ff453b2b512cbb8253886ecf3
    Reviewed-on: https://gerrit.libreoffice.org/640
    Reviewed-by: Miklos Vajna <vmiklos@suse.cz>
    Tested-by: Miklos Vajna <vmiklos@suse.cz>

--- sw/inc/swtable.hxx
+++ sw/inc/swtable.hxx
@@ -422,6 +422,7 @@ public:
     SwFrmFmt* ClaimFrmFmt();
     void ChgFrmFmt( SwTableBoxFmt *pNewFmt );
 
+    void RemoveFromTable();
     const SwStartNode *GetSttNd() const { return pSttNd; }
     sal_uLong GetSttIdx() const;
 
