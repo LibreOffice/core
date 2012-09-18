e7649f4ae1751ebcd528ea54f6affa63d34b9b56
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-09-17 22:44:33 -0400

    Updated mdds to 0.6.1.
    Change-Id: Ibc0a67b689e5731afdfe2b9af7d991a998ffb389

--- mdds/UnpackedTarball_mdds.mk
+++ mdds/UnpackedTarball_mdds.mk
@@ -13,10 +13,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,mdds,$(MDDS_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
 	mdds/mdds_0.6.0.patch \
-	mdds/0001-help-compiler-select-the-right-overload-of-vector-in.patch \
-	mdds/mdds_0.6.0-unused-parameter.patch \
-	mdds/mdds_0.6.0-unreachable-code.patch \
-	mdds/0001-fix-linking-error-with-boost-1.50.patch \
 	mdds/0001-Workaround-for-gcc-bug.patch \
 ))
 
