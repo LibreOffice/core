56dcf84da2c4bf37c151610188d8f47715b538e2
Andrzej J.R. Hunt <andrzej@ahunt.org>
2012-09-18 20:40:09 +0200

    Fix mispaste.
    Change-Id: I80908ec6886eb5b7b30d0465bdf4a2f92340ce7c

--- sd/inc/sddll.hxx
+++ sd/inc/sddll.hxx
@@ -1,4 +1,4 @@
-git clone git://anongit.freedesktop.org/libreoffice/core libo/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
