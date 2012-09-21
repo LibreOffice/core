ff7ac2253210bd87631d6031466a3ee47268d252
David Tardon <dtardon@redhat.com>
2012-09-21 17:01:39 +0200

    fix output files of mdds and vigra too
    Change-Id: Ie7b78dbc0e9ac3715149faad0238f4f51a08527e

--- mdds/UnpackedTarball_mdds.mk
+++ mdds/UnpackedTarball_mdds.mk
@@ -16,4 +16,35 @@ $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
 	mdds/0001-Workaround-for-gcc-bug.patch \
 ))
 
+$(eval $(call gb_UnpackedTarball_mark_output_files,mdds,\
+	include/mdds/compat/unique_ptr.hpp \
+	include/mdds/default_deleter.hpp \
+	include/mdds/flat_segment_tree.hpp \
+	include/mdds/flat_segment_tree_def.inl \
+	include/mdds/flat_segment_tree_itr.hpp \
+	include/mdds/global.hpp \
+	include/mdds/hash_container/map.hpp \
+	include/mdds/mixed_type_matrix.hpp \
+	include/mdds/mixed_type_matrix_def.inl \
+	include/mdds/mixed_type_matrix_element.hpp \
+	include/mdds/mixed_type_matrix_flag_storage.hpp \
+	include/mdds/mixed_type_matrix_storage.hpp \
+	include/mdds/mixed_type_matrix_storage_filled_linear.inl \
+	include/mdds/mixed_type_matrix_storage_sparse.inl \
+	include/mdds/multi_type_matrix.hpp \
+	include/mdds/multi_type_matrix_def.inl \
+	include/mdds/multi_type_vector.hpp \
+	include/mdds/multi_type_vector_def.inl \
+	include/mdds/multi_type_vector_itr.hpp \
+	include/mdds/multi_type_vector_macro.hpp \
+	include/mdds/multi_type_vector_trait.hpp \
+	include/mdds/multi_type_vector_types.hpp \
+	include/mdds/node.hpp \
+	include/mdds/point_quad_tree.hpp \
+	include/mdds/quad_node.hpp \
+	include/mdds/rectangle_set.hpp \
+	include/mdds/rectangle_set_def.inl \
+	include/mdds/segment_tree.hpp \
+))
+
 # vim: set noet sw=4 ts=4:
