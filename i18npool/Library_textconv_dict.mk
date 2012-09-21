60fda0b0cd4cb6c867ce7a8f6e55067845043c3c
Peter Foley <pefoley2@verizon.net>
2012-09-21 19:16:52 -0400

    add needed libraries to textconv_dict
    Change-Id: Ic0f55385c34bb253a1f452335ebcd3bba92ca9fa

--- i18npool/Library_textconv_dict.mk
+++ i18npool/Library_textconv_dict.mk
@@ -37,6 +37,9 @@ $(eval $(call gb_Library_use_sdk_api,textconv_dict))
 
 $(eval $(call gb_Library_use_libraries,textconv_dict,\
 	$(gb_UWINAPI) \
+	sal \
+	cppu \
+	cppuhelper \
 ))
 
 $(eval $(call gb_Library_add_generated_exception_objects,textconv_dict,\
