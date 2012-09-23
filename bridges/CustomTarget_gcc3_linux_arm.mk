a55274831642677dae591ac5de75849619e3ec64
David Tardon <dtardon@redhat.com>
2012-09-23 17:27:19 +0200

    fix make syntax
    Change-Id: I6414a49f867c71c7a9a17bbcc9acd5da35e3a292

--- bridges/CustomTarget_gcc3_linux_arm.mk
+++ bridges/CustomTarget_gcc3_linux_arm.mk
@@ -14,7 +14,7 @@ $(call gb_CustomTarget_get_target,bridges/source/cpp_uno/gcc3_linux_arm) : \
 	$(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.objectlist
 
 $(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.o : \
-		$(SRCDIR)/bridges/source/cpp_uno/gcc3_linux_arm/armhelper.S
+		$(SRCDIR)/bridges/source/cpp_uno/gcc3_linux_arm/armhelper.S \
 		| $(dir $(call gb_CustomTarget_get_target,bridges/source/cpp_uno/gcc3_linux_arm)).dir
 	$(gb_CXX) -c -o $@ $< -fPIC
 
