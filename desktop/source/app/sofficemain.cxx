adc68da352923fe6fb19bf3b8677d09d6d656588
Stephan Bergmann <sbergman@redhat.com>
2012-09-24 17:41:51 +0200

    fdo#54385: Displaying help/version early on UNX requires service manager
    This regression was introduced with 6c6358a6822d3562b9b8c7668a7d60d6c644dfe8
    "Related fdo#53006: Do not instantiate service manager too early."
    
    (cherry picked from commit 2f14e2e67c58fe6948501d57a38cd0d2ad84dfff)
    (cherry picked from commit 7b33d564bc0079e5d8309e4b5081b1d44e1efd12)
    
    Conflicts:
    	desktop/inc/app.hxx
    	desktop/source/app/app.cxx
    	desktop/source/app/appinit.cxx
    	desktop/source/app/sofficemain.cxx
    (with ensureProcessServiceManager still available in libreoffice-3-6-2, this
    cherry-pick shrinks up dramatically)
    
    Change-Id: If22ea3ac6474188bf0792246620e5c705a813445

--- desktop/source/app/sofficemain.cxx
+++ desktop/source/app/sofficemain.cxx
@@ -69,11 +69,13 @@ extern "C" int DESKTOP_DLLPUBLIC soffice_main()
     const desktop::CommandLineArgs& rCmdLineArgs = aDesktop.GetCommandLineArgs();
     if ( rCmdLineArgs.IsHelp() )
     {
+        desktop::Desktop::ensureProcessServiceFactory();
         desktop::displayCmdlineHelp();
         return EXIT_SUCCESS;
     }
     else if ( rCmdLineArgs.IsVersion() )
     {
+        desktop::Desktop::ensureProcessServiceFactory();
         desktop::displayVersion();
         return EXIT_SUCCESS;
     }
