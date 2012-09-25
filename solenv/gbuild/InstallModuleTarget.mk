9642ae95a82f3040b0e6af3ef2408a4099ac0362
David Tardon <dtardon@redhat.com>
2012-09-25 17:27:52 +0200

    apparently cpp.lcc does not have .exe ext. on windows
    Change-Id: I87ed6ba66b739638379adea2a137fa1ab6c502ee

--- solenv/gbuild/InstallModuleTarget.mk
+++ solenv/gbuild/InstallModuleTarget.mk
@@ -74,7 +74,9 @@ endef
 
 # ScpPreprocessTarget class
 
-gb_ScpPreprocessTarget_TARGET := $(call gb_Executable_get_target_for_build,cpp.lcc)
+# TODO change to gb_Executable_get_target_for_build after soltools has
+# been gbuildified
+gb_ScpPreprocessTarget_TARGET := $(OUTDIR_FOR_BUILD)/bin/cpp.lcc
 gb_ScpPreprocessTarget_COMMAND := $(gb_Helper_set_ld_path) $(gb_ScpPreprocessTarget_TARGET)
 
 gb_ScpPreprocessTarget_get_source = $(SRCDIR)/$(1).scp
