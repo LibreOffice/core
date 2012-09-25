52807a0daac358ca2472b851070653cdcf3a246f
David Tardon <dtardon@redhat.com>
2012-09-25 16:23:19 +0200

    fix scp2 build producing garbage
    Change-Id: Ia295528fea995c5660f9d50c0426d79d60900ebd

--- solenv/gbuild/InstallModuleTarget.mk
+++ solenv/gbuild/InstallModuleTarget.mk
@@ -115,7 +115,7 @@ gb_ScpMergeTarget_get_source = $(SRCDIR)/$(1).ulf
 define gb_ScpMergeTarget__command
 $(call gb_Output_announce,$(2),$(true),SUM,1)
 $(call gb_Helper_abbreviate_dirs,\
-	$(gb_ScpMergeTarget_COMMAND) -p scp2 -i $(3) -o $(1) -m $(SCP_SDF) -l all \
+	$(gb_ScpMergeTarget_COMMAND) -p scp2 -i $(SCP_SOURCE) -o $(1) -m $(SCP_SDF) -l all \
 )
 endef
 
@@ -123,7 +123,7 @@ $(dir $(call gb_ScpMergeTarget_get_target,%))%/.dir :
 	$(if $(wildcard $(dir $@)),,mkdir -p $(dir $@))
 
 $(call gb_ScpMergeTarget_get_target,%) : $(gb_ScpMergeTarget_TARGET)
-	$(call gb_ScpMergeTarget__command,$@,$*,$<)
+	$(call gb_ScpMergeTarget__command,$@,$*)
 
 .PHONY : $(call gb_ScpMergeTarget_get_clean_target,%)
 $(call gb_ScpMergeTarget_get_clean_target,%) :
@@ -132,6 +132,7 @@ $(call gb_ScpMergeTarget_get_clean_target,%) :
 
 # gb_ScpMergeTarget_ScpMergeTarget(<target>)
 define gb_ScpMergeTarget_ScpMergeTarget
+$(call gb_ScpMergeTarget_get_target,$(1)) : private SCP_SOURCE := $(call gb_ScpMergeTarget_get_source,$(1))
 $(call gb_ScpMergeTarget_get_target,$(1)) : $(call gb_ScpMergeTarget_get_source,$(1))
 $(call gb_ScpMergeTarget_get_target,$(1)) :| $(dir $(call gb_ScpMergeTarget_get_target,$(1))).dir
 $(call gb_ScpMergeTarget_get_target,$(1)) : SCP_SDF := $(gb_SDFLOCATION)/$(dir $(1))/localize.sdf
