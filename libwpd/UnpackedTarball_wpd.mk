21ab2c6d992ff555bbcd2e03add21a743348659b
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-09-25 05:42:03 +0200

    Uploading libwpd 0.9.6 release
    Change-Id: I71a27d94b2e7097c73b633498181e54c15e14deb

--- libwpd/UnpackedTarball_wpd.mk
+++ libwpd/UnpackedTarball_wpd.mk
@@ -11,16 +11,12 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,wpd))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,wpd,$(WPD_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_set_patchlevel,wpd,0))
+# $(eval $(call gb_UnpackedTarball_set_patchlevel,wpd,0))
 
-wpd_patches :=
-# By mistake libwpd-0.9.5 changed ABI, change back
-wpd_patches += libwpd-0.9.5-ABI.patch
-# Link the static library against the dll runtime and not the static one
-wpd_patches += libwpd-0.9.5-msc.patch
+# wpd_patches :=
 
-$(eval $(call gb_UnpackedTarball_add_patches,wpd,\
-	$(foreach patch,$(wpd_patches),libwpd/$(patch)) \
-))
+# $(eval $(call gb_UnpackedTarball_add_patches,wpd,\
+# 	$(foreach patch,$(wpd_patches),libwpd/$(patch)) \
+# ))
 
 # vim: set noet sw=4 ts=4:
