7372bbced8d99d6fd479a464095e4f827c39422e
Michael Stahl <mstahl@redhat.com>
2012-09-26 15:42:02 +0200

    gbuild: split out a common android.mk
    Change-Id: Ie0858cb137aeaeb18815e3085de4d4000c62218b

--- solenv/gbuild/platform/ANDROID_ARM_GCC.mk
+++ solenv/gbuild/platform/ANDROID_ARM_GCC.mk
@@ -25,80 +25,13 @@
 # in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
 # instead of those above.
 
-#please make generic modifications to unxgcc.mk or linux.mk
+# please make generic modifications to unxgcc.mk or android.mk
 gb_CPUDEFS += -DARM32
 gb_COMPILERDEFAULTOPTFLAGS := -Os
 gb_CXXFLAGS += -fno-omit-frame-pointer
 gb_CFLAGS += -fno-omit-frame-pointer
 
 include $(GBUILDDIR)/platform/unxgcc.mk
-
-# Link almost everything with -lgnustl_shared
-gb_STDLIBS := \
-	gnustl_shared
-
-gb_Library_PLAINLIBS_NONE := \
-	android \
-	gnustl_shared \
-
-# No unit testing can be run
-gb_CppunitTest_CPPTESTPRECOMMAND := :
-
-# Re-define this from unxgcc.mk with some small but important
-# changes. Just temporarily done this way, shm_get promised to
-# eventually enable this to be done this in some more elegant and less
-# redundant fashion.
-
-gb_LinkTarget_LDFLAGS += \
-    -Wl,-z,defs \
-	-Wl,--as-needed \
-	-Wl,--no-add-needed
-
-define gb_LinkTarget__command_dynamiclink
-$(call gb_Helper_abbreviate_dirs,\
-	mkdir -p $(dir $(1)) && \
-	$(gb_CXX) \
-		-shared \
-		$(if $(filter Library CppunitTest,$(TARGETTYPE)),$(gb_Library_TARGETTYPEFLAGS)) \
-		$(if $(filter Library,$(TARGETTYPE)),$(gb_Library_LTOFLAGS)) \
-		$(subst \d,$$,$(RPATH)) \
-		$(T_LDFLAGS) \
-		$(foreach object,$(COBJECTS),$(call gb_CObject_get_target,$(object))) \
-		$(foreach object,$(CXXOBJECTS),$(call gb_CxxObject_get_target,$(object))) \
-		$(foreach object,$(ASMOBJECTS),$(call gb_AsmObject_get_target,$(object))) \
-		$(foreach object,$(GENCOBJECTS),$(call gb_GenCObject_get_target,$(object))) \
-		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
-		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
-		-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group \
-		$(LIBS) \
-		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib))))) \
-		-o $(1))
-endef
-
-# Prefix UNO library filenames with "lib"
-gb_Library_FILENAMES := \
-	$(foreach lib,$(gb_Library_OOOLIBS),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_OOOEXT)) \
-	$(foreach lib,$(gb_Library_PLAINLIBS_NONE),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_PLAINEXT)) \
-	$(foreach lib,$(gb_Library_PLAINLIBS_URE),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_PLAINEXT)) \
-	$(foreach lib,$(gb_Library_PLAINLIBS_OOO),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_PLAINEXT)) \
-	$(foreach lib,$(gb_Library_RTLIBS),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_RTEXT)) \
-	$(foreach lib,$(gb_Library_RTVERLIBS),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_RTEXT)) \
-	$(foreach lib,$(gb_Library_UNOLIBS_URE),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_UNOEXT)) \
-	$(foreach lib,$(gb_Library_UNOLIBS_OOO),$(lib):$(gb_Library_SYSPRE)$(lib)$(gb_Library_UNOEXT)) \
-	$(foreach lib,$(gb_Library_UNOVERLIBS),$(lib):$(gb_Library_UNOVERPRE)$(lib)$(gb_Library_PLAINEXT)) \
-
-
-# No DT_RPATH or DT_RUNPATH support in the Bionic dynamic linker so
-# don't bother generating such.
-
-define gb_Library_get_rpath
-endef
-
-define gb_Executable_get_rpath
-endef
-
-gb_LinkTarget_LDFLAGS := $(subst -Wl$(COMMA)-rpath-link$(COMMA)$(SYSBASE)/lib:$(SYSBASE)/usr/lib,,$(gb_LinkTarget_LDFLAGS))
-
-gb_Library__set_soversion_script_platform = 
+include $(GBUILDDIR)/platform/android.mk
 
 # vim: set noet sw=4:
