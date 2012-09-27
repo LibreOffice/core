05f1bf896661cf0127ca9bd9ea282d46b724db75
Tor Lillqvist <tml@iki.fi>
2012-09-27 23:51:50 +0300

    No 64-bit QuickTime
    Change-Id: Ie287d12c2142342fb89650bf715a2e12edabff78

--- scp2/InstallModule_ooo.mk
+++ scp2/InstallModule_ooo.mk
@@ -166,6 +166,9 @@ $(eval $(call gb_InstallModule_add_defs,scp2/ooo,\
 	$(if $(filter-out YES,$(WITH_MYSPELL_DICTS)),\
 		-DWITHOUT_MYSPELL_DICTS \
 	) \
+	$(if $(BITNESS_OVERRIDE),\
+		-DBITNESS_OVERRIDE=$(BITNESS_OVERRIDE) \
+	) \
 ))
 
 ifeq ($(DISABLE_PYTHON),TRUE)
