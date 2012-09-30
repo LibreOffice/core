6976fc91256bdf6224ddfab46c535f319ac8475c
Matúš Kukan <matus.kukan@gmail.com>
2012-09-30 14:10:17 +0200

    soltools: build again ldump4 for Windows
    Change-Id: I8f1f5756ac777cefed52ae72af65931bfa14d580

---
+++ soltools/Executable_ldump4.mk
@@ -0,0 +1,17 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,ldump4))
+
+$(eval $(call gb_Executable_add_exception_objects,ldump4,\
+    soltools/ldump/hashtbl \
+    soltools/ldump/ldump \
+))
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
