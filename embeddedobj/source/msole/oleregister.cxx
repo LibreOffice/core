393588ce8e698203dd87a87eb506afb551457717
Tor Lillqvist <tml@iki.fi>
2012-10-01 22:00:53 +0300

    Use prefix
    Change-Id: I28607f54eb06912ab1d27d80813d54cb6faa7771

--- embeddedobj/source/msole/oleregister.cxx
+++ embeddedobj/source/msole/oleregister.cxx
@@ -29,7 +29,7 @@ using namespace ::com::sun::star;
 
 extern "C" {
 
-SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(
+SAL_DLLPUBLIC_EXPORT void * SAL_CALL emboleobj_component_getFactory(
     const sal_Char * pImplName, void * pServiceManager,
     SAL_UNUSED_PARAMETER void * /*pRegistryKey*/ )
 {
