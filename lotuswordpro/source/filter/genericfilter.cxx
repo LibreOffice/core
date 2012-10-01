645181f46472d459026d87c39b3475e834390761
Tor Lillqvist <tml@iki.fi>
2012-10-01 11:22:51 +0300

    Prefixify lotuswordpro
    Change-Id: I9b30477d94995adef51a76bdf298d1815f511e9e

--- lotuswordpro/source/filter/genericfilter.cxx
+++ lotuswordpro/source/filter/genericfilter.cxx
@@ -16,7 +16,7 @@ using namespace ::com::sun::star::registry;
 
 extern "C"
 {
-SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(
+SAL_DLLPUBLIC_EXPORT void * SAL_CALL lotuswordpro_component_getFactory(
     const sal_Char * pImplName, void * pServiceManager, void * /*pRegistryKey*/ )
 {
     void * pRet = 0;
