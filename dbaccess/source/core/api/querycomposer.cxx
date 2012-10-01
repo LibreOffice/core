afb28ae04ab818721e848e254ae89b2185318435
Norbert Thiebaud <nthiebaud@gmail.com>
2012-10-01 16:08:10 -0500

    missing system header
    Change-Id: Icba29c0ebfd30af36b18eb59a23643287985f8c8

--- dbaccess/source/core/api/querycomposer.cxx
+++ dbaccess/source/core/api/querycomposer.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-
+#include <string.h>
 #include <com/sun/star/lang/DisposedException.hpp>
 #include <com/sun/star/util/XNumberFormatter.hpp>
 #include <com/sun/star/sdbc/ColumnSearch.hpp>
