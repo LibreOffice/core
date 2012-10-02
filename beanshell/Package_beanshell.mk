9fadb7804fb425ab92b1a5e85db9a89207ce2607
Michael Stahl <mstahl@redhat.com>
2012-10-02 13:09:26 +0200

    beanshell: rename package to beanshell
    Change-Id: Ie4979cfb2a20bc0128a550358b0d0adc3f3d209c

--- beanshell/Package_beanshell.mk
+++ beanshell/Package_beanshell.mk
@@ -7,10 +7,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,beanshell_inc,$(call gb_UnpackedTarball_get_dir,beanshell)))
+$(eval $(call gb_Package_Package,beanshell,$(call gb_UnpackedTarball_get_dir,beanshell)))
 
-$(eval $(call gb_Package_use_external_project,beanshell_inc,beanshell))
+$(eval $(call gb_Package_use_external_project,beanshell,beanshell))
 
-$(eval $(call gb_Package_add_file,beanshell_inc,bin/bsh.jar,dist/bsh-2.0b1.jar))
+$(eval $(call gb_Package_add_file,beanshell,bin/bsh.jar,dist/bsh-2.0b1.jar))
 
 # vim: set noet sw=4 ts=4:
