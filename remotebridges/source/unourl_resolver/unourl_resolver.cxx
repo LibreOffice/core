ae53e3e9fc0e0f878e7911732a5dd918a9bec597
Tor Lillqvist <tml@iki.fi>
2012-10-02 11:38:39 +0300

    Use prefix
    Change-Id: Ifa5ea2dd818043b6b5442e580bff51d1e2cb93e1

--- remotebridges/source/unourl_resolver/unourl_resolver.cxx
+++ remotebridges/source/unourl_resolver/unourl_resolver.cxx
@@ -194,13 +194,13 @@ static struct ImplementationEntry g_entries[] =
 
 extern "C"
 {
-SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_canUnload( TimeValue *pTime )
+SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL uuresolver_component_canUnload( TimeValue *pTime )
 {
     return g_moduleCount.canUnload( &g_moduleCount , pTime );
 }
 
 //==================================================================================================
-SAL_DLLPUBLIC_EXPORT void * SAL_CALL component_getFactory(
+SAL_DLLPUBLIC_EXPORT void * SAL_CALL uuresolver_component_getFactory(
     const sal_Char * pImplName, void * pServiceManager, void * pRegistryKey )
 {
     return component_getFactoryHelper( pImplName, pServiceManager, pRegistryKey , g_entries );
