29440bd18985f6ab50029552c5e9d8d1cf643d4c
Peter Foley <pefoley2@verizon.net>
2012-10-02 14:26:12 -0400

    add cppuhelper to sw tests
    Change-Id: Ie50c7adf5bd4ba1b18a8deb3e708bcf83a2777af

--- sw/CppunitTest_sw_subsequent_rtfexport.mk
+++ sw/CppunitTest_sw_subsequent_rtfexport.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sw_subsequent_rtfexport, \
 
 $(eval $(call gb_CppunitTest_use_libraries,sw_subsequent_rtfexport, \
     cppu \
+	cppuhelper \
     sal \
 	sw \
     test \
