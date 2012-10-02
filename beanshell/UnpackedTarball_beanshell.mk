02e400865cb9a51b623a3d866d3236f0d6c899ca
David Ostrovsky <david@ostrovsky.org>
2012-10-02 09:31:35 +0200

    gbuildification of beanshell
    Change-Id: I1b71796ca95500efeffe5e01dba3e73711f920e7
    Reviewed-on: https://gerrit.libreoffice.org/745
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

---
+++ beanshell/UnpackedTarball_beanshell.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,beanshell))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,beanshell,$(BSH_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_add_patches,beanshell,\
+	beanshell/bsh-2.0b1-src.patch \
+))
+
+# vim: set noet sw=4 ts=4:
