b3e011e3384228a737741534709951d0033b15ac
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2012-10-03 18:23:27 +0200

    don't pretend to be 1.4 compatible, when the baseline is 1.5
    also pass java-target-version from environment
    as the source version doesn't change by miracle in external modules,
    keep that in the build.xml (and only there)
    
    Change-Id: Ic09de3c7b15318677afe6d4afbe7c9f7e7f3c14c

--- saxon/ExternalProject_saxon.mk
+++ saxon/ExternalProject_saxon.mk
@@ -28,9 +28,8 @@ $(call gb_ExternalProject_get_state_target,saxon,build) :
 		)\
 		$(if $(filter yes,$(JAVACISGCJ))\
 			,-Dbuild.compiler=gcj \
-			,-Dant.build.javac.source=$(JAVA_SOURCE_VER) \
-				-Dant.build.javac.target=$(JAVA_TARGET_VER) \
 		) \
+		-Dant.build.javac.target=$(JAVA_TARGET_VER) \
 		$(if $(debug),-Dbuild.debug="on") \
 		-Dsolarbindir=$(OUTDIR)/bin jar-bj && \
 	touch $@
