5c19b8701c58fbb49188ce2210879ce6f4431471
Miklos Vajna <vmiklos@suse.cz>
2012-10-05 16:30:37 +0200

    ItemSetToPageDesc / PageDescToItemSet: handle first page footers
    Change-Id: Id480c0e81b79ec48b3ceba566219089d9a408c22

--- sw/source/ui/utlui/uitool.cxx
+++ sw/source/ui/utlui/uitool.cxx
@@ -257,6 +257,8 @@ void ItemSetToPageDesc( const SfxItemSet& rSet, SwPageDesc& rPageDesc )
 
             rPageDesc.ChgFooterShare(((const SfxBoolItem&)
                         rFooterSet.Get(SID_ATTR_PAGE_SHARED)).GetValue());
+            rPageDesc.ChgFirstShare(((const SfxBoolItem&)
+                        rFooterSet.Get(SID_ATTR_PAGE_SHARED_FIRST)).GetValue());
         }
         else
         {   // Footer ausschalten
@@ -439,6 +441,8 @@ void PageDescToItemSet( const SwPageDesc& rPageDesc, SfxItemSet& rSet)
         //
         SfxBoolItem aShared(SID_ATTR_PAGE_SHARED, rPageDesc.IsFooterShared());
         aFooterSet.Put(aShared);
+        SfxBoolItem aFirstShared(SID_ATTR_PAGE_SHARED_FIRST, rPageDesc.IsFirstShared());
+        aFooterSet.Put(aFirstShared);
 
         // Groesse
         SvxSizeItem aSize(SID_ATTR_PAGE_SIZE, Size(rFrmSize.GetSize()));
