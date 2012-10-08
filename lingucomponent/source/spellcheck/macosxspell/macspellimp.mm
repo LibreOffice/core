60337134e492aca378aec5251339f1fa5ea49049
Michael Meeks <michael.meeks@suse.com>
2012-10-08 10:00:18 +0100

    re-base on ALv2 code. Includes:
        linecap: Reintegrating finished LineCap feature
        Patch contributed by Regina Henschel
        http://svn.apache.org/viewvc?view=revision&revision=1232507
    
        disable extended linecap API for now.
    
        gnumake4 work primarily from Mathias Bauer
        http://svn.apache.org/viewvc?view=revision&revision=1394326
    
        misc. dead code cleanups
        Patch contributed by Ariel Constenla-Haile
        http://svn.apache.org/viewvc?view=revision&revision=1204995
        http://svn.apache.org/viewvc?view=revision&revision=1207270

--- lingucomponent/source/spellcheck/macosxspell/macspellimp.mm
+++ lingucomponent/source/spellcheck/macosxspell/macspellimp.mm
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/linguistic2/XSearchableDictionaryList.hpp>
@@ -35,7 +26,6 @@
 #include <tools/debug.hxx>
 #include <osl/mutex.hxx>
 
-#include <dictmgr.hxx>
 #include <macspellimp.hxx>
 
 #include <linguistic/spelldta.hxx>
@@ -58,10 +48,8 @@ using ::rtl::OString;
 using ::rtl::OUStringBuffer;
 using ::rtl::OUStringToOString;
 
-///////////////////////////////////////////////////////////////////////////
-
 MacSpellChecker::MacSpellChecker() :
-    aEvtListeners   ( GetLinguMutex() )
+    aEvtListeners( GetLinguMutex() )
 {
     aDEncs = NULL;
     aDLocs = NULL;
@@ -117,14 +105,10 @@ Sequence< Locale > SAL_CALL MacSpellChecker::getLocales()
 
         int numusr;          // number of user dictionary entries
         int numshr;          // number of shared dictionary entries
-        dictentry * spdict;  // shared dict entry pointer
-        dictentry * updict;  // user dict entry pointer
         SvtPathOptions aPathOpt;
         rtl_TextEncoding aEnc = RTL_TEXTENCODING_UTF8;
 
         std::vector<NSString *> postspdict;
-        std::vector<dictentry *> postupdict;
-
 
     if (!numdict) {
 
@@ -143,7 +127,6 @@ Sequence< Locale > SAL_CALL MacSpellChecker::getLocales()
             }
         }
 
-        numusr = postupdict.size();
         numshr = postspdict.size();
 
         // we really should merge these and remove duplicates but since
@@ -198,11 +181,6 @@ Sequence< Locale > SAL_CALL MacSpellChecker::getLocales()
                 aDNames = NULL;
                 aSuppLocales.realloc(0);
             }
-
-            /* de-allocation of memory is handled inside the DictMgr */
-            updict = NULL;
-            spdict = NULL;
-
         }
 
     return aSuppLocales;
