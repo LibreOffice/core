c60b8ab88a8c65f4db4f90494373c438c97cd8cf
David Tardon <dtardon@redhat.com>
2012-10-09 21:17:22 +0200

    add missing dep on sfx2 headers
    Change-Id: I93b48313501f97c9934b1a7368e289e9a3ca5a76

--- svx/Library_textconversiondlgs.mk
+++ svx/Library_textconversiondlgs.mk
@@ -28,6 +28,10 @@
 
 $(eval $(call gb_Library_Library,textconversiondlgs))
 
+$(eval $(call gb_Library_use_packages,textconversiondlgs,\
+	sfx2_inc \
+))
+
 $(eval $(call gb_Library_set_componentfile,textconversiondlgs,svx/util/textconversiondlgs))
 
 $(eval $(call gb_Library_set_include,textconversiondlgs,\
