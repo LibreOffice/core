31828c94034bafa41beb443632a3f70600185a47
David Tardon <dtardon@redhat.com>
2012-10-09 16:45:59 +0200

    add missing dep on ucbhelper/macros.hxx
    Change-Id: Ib2bd5f44580ac1e6da2f2494c23adfa098694449

--- ucb/Library_srtrs1.mk
+++ ucb/Library_srtrs1.mk
@@ -27,6 +27,10 @@
 
 $(eval $(call gb_Library_Library,srtrs1))
 
+$(eval $(call gb_Library_use_packages,srtrs1,\
+	ucbhelper_inc \
+))
+
 $(eval $(call gb_Library_set_componentfile,srtrs1,ucb/source/sorter/srtrs1))
 
 $(eval $(call gb_Library_use_sdk_api,srtrs1))
