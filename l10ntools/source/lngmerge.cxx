06175e8b19625cb5a43b1ae5ae63e419cee7e053
Zolnai Tamás <zolnaitamas2000@gmail.com>
2012-10-10 15:30:40 +0200

    Correct lngmerge not to work with eof
    Change-Id: Ibf80a31ea2c03ff2882e71a45acb71d9c9a16482

--- l10ntools/source/lngmerge.cxx
+++ l10ntools/source/lngmerge.cxx
@@ -50,10 +50,10 @@ LngParser::LngParser(const rtl::OString &rLngFile,
     if (aStream.is_open())
     {
         bool bFirstLine = true;
+        std::string s;
+        std::getline(aStream, s);
         while (!aStream.eof())
         {
-            std::string s;
-            std::getline(aStream, s);
             rtl::OString sLine(s.data(), s.length());
 
             if( bFirstLine )
@@ -64,6 +64,7 @@ LngParser::LngParser(const rtl::OString &rLngFile,
             }
 
             pLines->push_back( new rtl::OString(sLine) );
+            std::getline(aStream, s);
         }
     }
     else
