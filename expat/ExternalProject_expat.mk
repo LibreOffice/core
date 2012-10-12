9aa7413c801f881ec80cf54541bee11225342bef
David Ostrovsky <david@ostrovsky.org>
2012-10-12 12:28:11 -0700

    gbuildification of expat
    Two different xmlparse libraries are created:
    ascii_expat_xmlparse and expat_xmlparse. One
    without -DXML_UNICODE and one with. Source file
    is copied to be able to add cobjects twice to
    gbuild machinery.
    
    On windows 64 bit additional two librares are created:
    expat_xmlparse_x64 and expat_xmltok_x64. That is due
    the problem with shell/shlxthandler (original comment):
    ------------------------------------------------------
    use UNICODE only because shell/shlxthandler
    doesn't link against ascii_expat_xmlparse
    ------------------------------------------------------
    Source files are copied to be able to add cobjects twice
    to gbuild machinery.
    
    Include files are delivered to $(OUTDIR)/inc/external/expat
    now and not to $(OUTDIR)/inc/external any more.
    set_include call is added in RepositoryExternal.mk.
    
    Change-Id: I3b3aa40f39ef82c70f6f28790b582c83e48bdf76

---
+++ expat/ExternalProject_expat.mk
@@ -0,0 +1,56 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalProject_ExternalProject,expat))
+
+$(eval $(call gb_ExternalProject_use_unpacked,expat,expat))
+
+ifneq ($(OS),WNT)
+$(eval $(call gb_ExternalProject_register_targets,expat,\
+	configure \
+))
+ifeq ($(BUILD_X64),TRUE)
+$(eval $(call gb_ExternalProject_register_targets,expat,\
+	duplicate_x64_files \
+))
+endif
+endif
+
+$(eval $(call gb_ExternalProject_register_targets,expat,\
+	duplicate_unicode_files \
+))
+
+$(call gb_ExternalProject_get_state_target,expat,configure) :
+	cd $(EXTERNAL_WORKDIR) \
+	&& ./configure \
+		$(if $(filter YES,$(CROSS_COMPILING)),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+	&& touch $@
+
+#
+# ugly hack number one: we need to compile it twice:
+# with -DXML_UNICODE and without.
+# We are copying the source to produce two different objects.
+$(call gb_ExternalProject_get_state_target,expat,duplicate_unicode_files) :
+	cd $(EXTERNAL_WORKDIR)/lib \
+	&& $(GNUCOPY) xmlparse.c unicode_xmlparse.c \
+	&& touch $@
+
+#
+# ugly hack number two on windows x64 platform: we need to link it twice:
+# with $(LINK_X64_BINARY) and with $(gb_LINK).
+# We are copying the source to produce two different objects and libraries.
+$(call gb_ExternalProject_get_state_target,expat,duplicate_x64_files) :
+	cd $(EXTERNAL_WORKDIR)/lib \
+	&& $(GNUCOPY) xmlparse.c xmlparse_x64.c \
+	&& $(GNUCOPY) xmltok.c xmltok_x64.c \
+	&& $(GNUCOPY) xmlrole.c xmlrole_x64.c \
+	&& touch $@
+
+
+# vim: set noet sw=4 ts=4:
