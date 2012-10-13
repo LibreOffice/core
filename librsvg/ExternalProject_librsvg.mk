b91b5e98d0a636a549d44a5a62ac13706a7d537c
Norbert Thiebaud <nthiebaud@gmail.com>
2012-10-13 00:09:19 -0500

    fix librsvg gbuildification
    Change-Id: I3c3d5f0284e07384bcd63b6f4d489ec82da55a75

--- librsvg/ExternalProject_librsvg.mk
+++ librsvg/ExternalProject_librsvg.mk
@@ -27,9 +27,9 @@ $(call gb_ExternalProject_get_state_target,librsvg,build) :
 	GDK_PIXBUF_CFLAGS="-I$(OUTDIR)/inc/external/gdk-pixbuf-2.0" \
     GDK_PIXBUF_LIBS="-lgdk_pixbuf-2.0" \
 	LIBRSVG_CFLAGS="-I$(OUTDIR)/inc/external/glib-2.0 -I$(OUTDIR)/inc/external/gdk-pixbuf-2.0 -I$(OUTDIR)/inc/external/pango-1.0 -I$(OUTDIR)/inc/cairo $(if $(filter YES,$(SYSTEM_LIBXML)),$(LIBXML_CFLAGS),-I$(OUTDIR)/inc/externel/libxml)" \
-    LIBRSVG_LIBS="-L$(SOLARLIBDIR) -lgdk_pixbuf-2.0 -lpango-1.0 -lpangocairo-1.0 -lgthread-2.0 -lgio-2.0 -lgmodule-2.0 -lgobject-2.0 -lglib-2.0 $(if $(filter YES,$(SYSTEM_LIBXML)),$(LIBXML_LIBS),-lxml2) -lcairo -lintl" \
+    LIBRSVG_LIBS="-L$(OUTDIR)/lib -lgdk_pixbuf-2.0 -lpango-1.0 -lpangocairo-1.0 -lgthread-2.0 -lgio-2.0 -lgmodule-2.0 -lgobject-2.0 -lglib-2.0 $(if $(filter YES,$(SYSTEM_LIBXML)),$(LIBXML_LIBS),-lxml2) -lcairo -lintl" \
 	CFLAGS="-I$(OUTDIR)/inc/external -I$(OUTDIR)/inc/external/glib-2.0 -I$(OUTDIR)/inc/external/gdk-pixbuf-2.0 -I$(OUTDIR)/inc/external/pango-1.0 -I$(OUTDIR)/inc/cairo" \
-	LDFLAGS="$(foreach lib,cairo.2 gio-2.0.0 glib-2.0.0 gmodule-2.0.0 gobject-2.0.0 gthread-2.0.0 intl.8 pango-1.0.0,-Wl,-dylib_file,@loader_path/lib$(lib).dylib:$(OUTDIR)/lib/lib$(lib).dylib) $(if $(filter NO,$(SYSTEM_LIBXML)), -Wl,-dylib_file,@loader_path/../ure-link/lib/libxml2.2.dylib:$(OUTDIR)/lib/libxml2.2.dylib)"
+	LDFLAGS="$(foreach lib,cairo.2 gio-2.0.0 glib-2.0.0 gmodule-2.0.0 gobject-2.0.0 gthread-2.0.0 intl.8 pango-1.0.0,-Wl,-dylib_file,@loader_path/lib$(lib).dylib:$(OUTDIR)/lib/lib$(lib).dylib) $(if $(filter YES,$(SYSTEM_LIBXML)),,-Wl,-dylib_file,@loader_path/../ure-link/lib/libxml2.2.dylib:$(OUTDIR)/lib/libxml2.2.dylib)" \
 	&& $(GNUMAKE) -j$(EXTMAXPROCESS) \
 	&& touch $@
 # vim: set noet sw=4 ts=4:
