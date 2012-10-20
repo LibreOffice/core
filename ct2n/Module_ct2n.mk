97d83c45faf42ada9993239c3835641d8bbd070f
David Ostrovsky <david@ostrovsky.org>
2012-10-20 10:05:45 -0700

    gbuildification of ct2n
    Conflicts:
    	config_host.mk.in
    
    Change-Id: I98ca1bb2af19d99a7a908991cf27a148ee84c543

---
+++ ct2n/Module_ct2n.mk
@@ -0,0 +1,21 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,ConvertTextToNumber))
+
+ifneq (,$(filter CT2N,$(BUILD_TYPE)))
+
+$(eval $(call gb_Module_add_targets,ConvertTextToNumber,\
+	UnpackedTarball_ct2n \
+	Extension_ct2n \
+))
+
+endif
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
