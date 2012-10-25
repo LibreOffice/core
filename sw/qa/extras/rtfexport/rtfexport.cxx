LibreOffice/core

commit d4069372484f18b242a42a1996767f57b031fff6

fdo#50831 fix RTF export of direct run formatting for empty paragraphs
Change-Id: I5f0e7aefdea80bbb9cf61b991c5b706bd2023dfa
Signed-off-by: Michael Meeks <michael.meeks@suse.com>

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -374,11 +374,13 @@ void RtfAttributeOutput::EndParagraphProperties()
     m_rExport.Strm() << m_aStyles.makeStringAndClear().getStr();
 }
 
-void RtfAttributeOutput::StartRun( const SwRedlineData* pRedlineData )
+void RtfAttributeOutput::StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun )
 {
     OSL_TRACE("%s", OSL_THIS_FUNC);
 
-    m_aRun.append('{');
+    m_bSingleEmptyRun = bSingleEmptyRun;
+    if (!m_bSingleEmptyRun)
+        m_aRun.append('{');
 
     // if there is some redlining in the document, output it
     Redline( pRedlineData );
@@ -391,7 +393,8 @@ void RtfAttributeOutput::EndRun()
     OSL_TRACE("%s", OSL_THIS_FUNC);
     m_aRun.append(m_rExport.sNewLine);
     m_aRun.append(m_aRunText.makeStringAndClear());
-    m_aRun.append('}');
+    if (!m_bSingleEmptyRun)
+        m_aRun.append('}');
 }
 
 void RtfAttributeOutput::StartRunProperties()
@@ -3020,7 +3023,8 @@ RtfAttributeOutput::RtfAttributeOutput( RtfExport &rExport )
     m_bWroteCellInfo( false ),
     m_bHadFieldResult( false ),
     m_bTableRowEnded( false ),
-    m_aCells()
+    m_aCells(),
+    m_bSingleEmptyRun(false)
 {
     OSL_TRACE("%s", OSL_THIS_FUNC);
 }

sw/source/filter/ww8/attributeoutputbase.hxx
@@ -159,7 +159,7 @@ public:
     virtual void EmptyParagraph() = 0;
 
     /// Start of the text run.
-    virtual void StartRun( const SwRedlineData* pRedlineData ) = 0;
+    virtual void StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun = false ) = 0;
 
     /// End of the text run.
     virtual void EndRun() = 0;

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -499,7 +499,7 @@ void DocxAttributeOutput::EndParagraphProperties()
     m_pSerializer->mergeTopMarks( sax_fastparser::MERGE_MARKS_PREPEND );
 }
 
-void DocxAttributeOutput::StartRun( const SwRedlineData* pRedlineData )
+void DocxAttributeOutput::StartRun( const SwRedlineData* pRedlineData, bool /*bSingleEmptyRun*/ )
 {
     // if there is some redlining in the document, output it
     StartRedline( pRedlineData );

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -90,7 +90,7 @@ public:
     virtual void EndParagraphProperties();
 
     /// Start of the text run.
-    virtual void StartRun( const SwRedlineData* pRedlineData );
+    virtual void StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun = false );
 
     /// End of the text run.
     virtual void EndRun();

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -63,7 +63,7 @@ public:
     virtual void EndParagraphProperties();
 
     /// Start of the text run.
-    virtual void StartRun( const SwRedlineData* pRedlineData );
+    virtual void StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun = false );
 
     /// End of the text run.
     virtual void EndRun();
@@ -539,6 +539,9 @@ private:
 
     /// Number of cells from the table definition, by depth.
     std::map<sal_uInt32,sal_uInt32> m_aCells;
+
+    /// If we're in a paragraph that has a single empty run only.
+    bool m_bSingleEmptyRun;
 public:
     RtfAttributeOutput( RtfExport &rExport );
 

sw/source/filter/ww8/wrtw8nds.cxx
@@ -1810,11 +1810,12 @@ void MSWordExportBase::OutputTextNode( const SwTxtNode& rNode )
     do {
         const SwRedlineData* pRedlineData = aAttrIter.GetRedline( nAktPos );
 
-        AttrOutput().StartRun( pRedlineData );
+        xub_StrLen nNextAttr = GetNextPos( &aAttrIter, rNode, nAktPos );
+        // Is this the only run in this paragraph and it's empty?
+        bool bSingleEmptyRun = nAktPos == 0 && nNextAttr == 0;
+        AttrOutput().StartRun( pRedlineData, bSingleEmptyRun );
         if( nTxtTyp == TXT_FTN || nTxtTyp == TXT_EDN )
             AttrOutput().FootnoteEndnoteRefTag();
-
-        xub_StrLen nNextAttr = GetNextPos( &aAttrIter, rNode, nAktPos );
 
         if( nNextAttr > nEnd )
             nNextAttr = nEnd;

sw/source/filter/ww8/ww8atr.cxx
@@ -972,7 +972,7 @@ void WW8AttributeOutput::StartRunProperties()
 }
 
 
-void WW8AttributeOutput::StartRun( const SwRedlineData* pRedlineData )
+void WW8AttributeOutput::StartRun( const SwRedlineData* pRedlineData, bool /*bSingleEmptyRun*/ )
 {
     if (pRedlineData)
     {

sw/source/filter/ww8/ww8attributeoutput.hxx
@@ -55,7 +55,7 @@ public:
 
     /// Start of the text run.
     ///
-    virtual void StartRun( const SwRedlineData* pRedlineData );
+    virtual void StartRun( const SwRedlineData* pRedlineData, bool bSingleEmptyRun = false );
 
     /// End of the text run.
     ///

commit 411f9007e20f8035bd2ab7bbc807c61892a5c64c

fdo#55939 fix RTF export of footnotes
Regression from d4069372484f18b242a42a1996767f57b031fff6.

Change-Id: I58e8d48ac3222b795f7edfd0e74ecd86ea36f380

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -2331,17 +2331,20 @@ void RtfAttributeOutput::TextFootnote_Impl( const SwFmtFtn& rFootnote )
 
     /*
      * The footnote contains a whole paragraph, so we have to:
-     * 1) Reset, then later restore the contents of our run buffer.
+     * 1) Reset, then later restore the contents of our run buffer and run state.
      * 2) Buffer the output of the whole paragraph, as we do so for section headers already.
      */
     const SwNodeIndex* pIndex = rFootnote.GetTxtFtn()->GetStartNode();
     RtfStringBuffer aRun = m_aRun;
     m_aRun.clear();
+    bool bInRunOrig = m_bInRun;
+    m_bInRun = false;
     m_bBufferSectionHeaders = true;
     m_rExport.WriteSpecialText( pIndex->GetIndex() + 1,
             pIndex->GetNode().EndOfSectionIndex(),
             !rFootnote.IsEndNote() ? TXT_FTN : TXT_EDN);
     m_bBufferSectionHeaders = false;
+    m_bInRun = bInRunOrig;
     m_aRun = aRun;
     m_aRun->append(m_aSectionHeaders.makeStringAndClear());
 

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -66,6 +66,7 @@ public:
     void testMathVerticalstacks();
     void testMathRuns();
     void testFdo53113();
+    void testFdo55939();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -106,6 +107,7 @@ void Test::run()
         {"math-vertical-stacks.rtf", &Test::testMathVerticalstacks},
         {"math-runs.rtf", &Test::testMathRuns},
         {"fdo53113.odt", &Test::testFdo53113},
+        {"fdo55939.odt", &Test::testFdo55939},
     };
     // Don't test the first import of these, for some reason those tests fail
     const char* aBlacklist[] = {
@@ -424,6 +426,17 @@ void Test::testFdo53113()
     CPPUNIT_ASSERT_EQUAL(sal_Int32(16), aPairs.getLength());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(535), aPairs[1].First.Value.get<sal_Int32>());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(102), aPairs[1].Second.Value.get<sal_Int32>());
+}
+
+void Test::testFdo55939()
+{
+    // The problem was that the exported RTF was invalid.
+    uno::Reference<text::XTextRange> xParagraph(getParagraph(1));
+    getRun(xParagraph, 1, "Main text before footnote.");
+    // Why the tab has to be removed here?
+    CPPUNIT_ASSERT_EQUAL(OUString("Footnote text."),
+            getProperty< uno::Reference<text::XTextRange> >(getRun(xParagraph, 2), "Footnote")->getText()->getString().replaceAll("\t", ""));
+    getRun(xParagraph, 3, " Text after the footnote."); // However, this leading space is intentional and OK.
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);