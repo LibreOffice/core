1491ec254a56cc7d3f790670e068721596906f98
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2012-10-25 14:58:03 +0200

    WaE: 'xChartType$D57056$_pInterface' may be used uninitialized…
    … in this function"
    declaring those functions static breaks the build on MacPPC / gcc 4.0.1
    
    Change-Id: I3a389530d51ae0ddd38418159ca4234016bf93ba

--- xmloff/source/chart/SchXMLSeriesHelper.cxx
+++ xmloff/source/chart/SchXMLSeriesHelper.cxx
@@ -114,7 +114,8 @@ using ::rtl::OUString;
     return aRet;
 }
 
-static uno::Reference< chart2::XChartType > lcl_getChartTypeOfSeries(
+namespace {
+uno::Reference< chart2::XChartType > lcl_getChartTypeOfSeries(
                                 const uno::Reference< chart2::XDiagram >&   xDiagram
                               , const Reference< chart2::XDataSeries >& xSeries )
 {
@@ -161,6 +162,7 @@ static uno::Reference< chart2::XChartType > lcl_getChartTypeOfSeries(
         }
     }
     return 0;
+}
 }
 
 bool SchXMLSeriesHelper::isCandleStickSeries(
