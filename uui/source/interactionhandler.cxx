LibreOffice/core

commit c25cb8a641723ab098980bb842caf75c0dc9b059

fdo#46808, Adapt task::InteractionHandler UNO service to new style
Since we don't need to expose XInitialisation, we can make the new-style
service implement XInteractionHandler2.

Change-Id: Ib27beed1c12df17592c6472d6f58c233d2c41558

uui/source/interactionhandler.cxx
@@ -19,6 +19,8 @@
 
 #include "iahndl.hxx"
 #include "interactionhandler.hxx"
+#include "comphelper/namedvaluecollection.hxx"
+#include "com/sun/star/awt/XWindow.hpp"
 
 using namespace com::sun::star;
 
@@ -67,7 +69,21 @@ UUIInteractionHandler::initialize(
     throw (uno::Exception)
 {
     delete m_pImpl;
-    m_pImpl = new UUIInteractionHelper(m_xServiceFactory, rArguments);
+
+    uno::Reference< awt::XWindow > xWindow;
+    rtl::OUString aContext;
+    ::comphelper::NamedValueCollection aProperties( rArguments );
+    if ( aProperties.has( "Parent" ) )
+    {
+        OSL_VERIFY( aProperties.get( "Parent" ) >>= xWindow );
+    }
+    if ( aProperties.has( "Context" ) )
+    {
+        OSL_VERIFY( aProperties.get( "Context" ) >>= aContext );
+    }
+
+
+    m_pImpl = new UUIInteractionHelper(m_xServiceFactory, xWindow, aContext);
 }
 
 void SAL_CALL

commit 402eaced7554aaeea75c10015857ea197191ba34

Related fdo#46808: Fix UUIInteractionHandler::initialize argument handling
This was a regression introduced with c25cb8a641723ab098980bb842caf75c0dc9b059
"fdo#46808, Adapt task::InteractionHandler UNO service to new style."

Change-Id: I52e36c4aabbf4b2b4552ed0e8ea164db15071980

uui/source/interactionhandler.cxx
@@ -70,18 +70,26 @@ UUIInteractionHandler::initialize(
 {
     delete m_pImpl;
 
+    // The old-style InteractionHandler service supported a sequence of
+    // PropertyValue, while the new-style service now uses constructors to pass
+    // in Parent and Context values; for backwards compatibility, keep support
+    // for a PropertyValue sequence, too:
     uno::Reference< awt::XWindow > xWindow;
     rtl::OUString aContext;
-    ::comphelper::NamedValueCollection aProperties( rArguments );
-    if ( aProperties.has( "Parent" ) )
+    if (!((rArguments.getLength() == 1 && (rArguments[0] >>= xWindow)) ||
+          (rArguments.getLength() == 2 && (rArguments[0] >>= xWindow) &&
+           (rArguments[1] >>= aContext))))
     {
-        OSL_VERIFY( aProperties.get( "Parent" ) >>= xWindow );
+        ::comphelper::NamedValueCollection aProperties( rArguments );
+        if ( aProperties.has( "Parent" ) )
+        {
+            OSL_VERIFY( aProperties.get( "Parent" ) >>= xWindow );
+        }
+        if ( aProperties.has( "Context" ) )
+        {
+            OSL_VERIFY( aProperties.get( "Context" ) >>= aContext );
+        }
     }
-    if ( aProperties.has( "Context" ) )
-    {
-        OSL_VERIFY( aProperties.get( "Context" ) >>= aContext );
-    }
-
 
     m_pImpl = new UUIInteractionHelper(m_xServiceFactory, xWindow, aContext);
 }