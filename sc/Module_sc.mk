2f8db10bdc1bbf2fcb975665f3d9651b9625cc54
Markus Mohrhard <markus.mohrhard@googlemail.com>
2012-10-26 19:49:47 +0200

    this test should not been disabled
    Change-Id: I21aa786a9f214ba5137d35497318c4f088456845

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -49,6 +49,7 @@ $(eval $(call gb_Module_add_check_targets,sc,\
 ))
 
 $(eval $(call gb_Module_add_slowcheck_targets,sc, \
+    CppunitTest_sc_subsequent_filters_test \
     CppunitTest_sc_subsequent_export_test \
 ))
 
