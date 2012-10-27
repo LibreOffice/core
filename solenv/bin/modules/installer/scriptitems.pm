9a4f81c333f23462f4fa462d8fba539b0b10a9a8
Lior Kaplan <kaplanlior@gmail.com>
2012-10-28 01:44:34 +0200

    ./g doesn't support -s option or log command any more, use git directly
    Change-Id: Icb8fa341fe76abb03ba59cbc57e8b54dbb9acbec
    Reviewed-on: https://gerrit.libreoffice.org/935
    Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
    Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

--- solenv/bin/modules/installer/scriptitems.pm
+++ solenv/bin/modules/installer/scriptitems.pm
@@ -702,7 +702,7 @@ sub replace_setup_variables
 
     if ( $localbuild =~ /^\s*(\w+?)(\d+)\s*$/ ) { $localbuild = $2; }   # using "680" instead of "src680"
 
-    my $buildidstring = join( '-', split( ' ', `$ENV{'SRC_ROOT'}/g -s log -n 1 --pretty=format:"%h "` ));
+    my $buildidstring = join( '-', split( ' ', `git log -n 1 --pretty=format:"%h "` ));
     if ($? || !$buildidstring) {
         $buildidstring = $localbuild . $localminor . "(Build:" . $installer::globals::buildid . ")";
     }
