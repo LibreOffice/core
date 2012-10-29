573af9266c4d17445f0c75e1a71b629c845b83b8
Michael Stahl <mstahl@redhat.com>
2012-10-29 14:49:39 +0100

    libwps: fix gcc warning too [-Wsign-promo]
    Change-Id: I23e32cfc100441530691e0dcdc71eac37b3fbf0d

--- libwps/UnpackedTarball_wps.mk
+++ libwps/UnpackedTarball_wps.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,wps,1))
 
 $(eval $(call gb_UnpackedTarball_add_patches,wps,\
     libwps/libwps.msvc.warnings.patch \
+    libwps/libwps.gcc.warnings.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
