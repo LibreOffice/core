45c7a7f5dc3057b283513e4516b7b59f21e46fde
Michael Stahl <mstahl@redhat.com>
2012-10-29 21:32:02 +0100

    libwps: need to depend on unpacked private headers:
    The generated cxx files that are built in the StaticLibrary depend on
    the header from the UnpackedTarball.  When the tarball gets unpacked
    again in an incremental build, the headers may have changed and the cxx
    files that include them must be rebuilt.  This works for those headers
    that are added to a Package, because they are make targets, but not for
    those that are not delivered and just sit in the unpacked directory.
    These need to be manually enumerated in order to turn them into make
    targets, which is not entirely satisfying.
    
    A little grepping in the .d file for the StaticLibrary can help:
    
     grep "UnpackedTarball.*\.h" workdir/*/Dep/LinkTarget/StaticLibrary/libwpslib.a.d | sed 's,:* \+[^ ]*$,,' | sed 's,^.*UnpackedTarball/[a-zA-Z0-9]*/,,' | sort | uniq
    
    Change-Id: I9fd4ee0cd6646c834118cf172e39a24d2833c439

--- libwps/UnpackedTarball_wps.mk
+++ libwps/UnpackedTarball_wps.mk
@@ -20,6 +20,27 @@ $(eval $(call gb_UnpackedTarball_add_patches,wps,\
 
 $(eval $(call gb_UnpackedTarball_mark_output_files,wps,\
 	$(addsuffix .cpp,$(libwps_SOURCEFILES)) \
+	src/lib/libwps_internal.h \
+	src/lib/libwps_tools_win.h \
+	src/lib/WPS4Graph.h \
+	src/lib/WPS4.h \
+	src/lib/WPS4Text.h \
+	src/lib/WPS8.h \
+	src/lib/WPSCell.h \
+	src/lib/WPSContentListener.h \
+	src/lib/WPSDebug.h \
+	src/lib/WPSEntry.h \
+	src/lib/WPS.h \
+	src/lib/WPSHeader.h \
+	src/lib/WPSList.h \
+	src/lib/WPSOLEParser.h \
+	src/lib/WPSOLEStream.h \
+	src/lib/WPSPageSpan.h \
+	src/lib/WPSParagraph.h \
+	src/lib/WPSParser.h \
+	src/lib/WPSPosition.h \
+	src/lib/WPSSubDocument.h \
+	src/lib/WPSTable.h \
 ))
 
 # vim: set noet sw=4 ts=4:
