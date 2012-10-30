30e6227c6f31bb9273dc4e3a429033c4069e72ad
Matúš Kukan <matus.kukan@gmail.com>
2012-10-30 22:01:39 +0100

    postgresql: forgot to change include path for openldap
    Change-Id: I3ce3906662ff6ce5855e4c20c3d2f927288bec9f

--- postgresql/ExternalProject_postgresql.mk
+++ postgresql/ExternalProject_postgresql.mk
@@ -33,7 +33,11 @@ $(call gb_ExternalProject_get_state_target,postgresql,build) :
 		$(if $(filter YES,$(CROSS_COMPILING)),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		$(if $(filter YES,$(WITH_KRB5)),--with-krb5) \
 		$(if $(filter YES,$(WITH_GSSAPI)),--with-gssapi) \
-		$(if $(filter NO,$(SYSTEM_OPENLDAP)),CPPFLAGS="-I$(OUTDIR)/inc/openldap" LDFLAGS="-L$(OUTDIR)/lib" EXTRA_LDAP_LIBS="-llber -lssl3 -lsmime3 -lnss3 -lnssutil3 -lplds4 -lplc4 -lnspr4") \
+		$(if $(filter NO,$(SYSTEM_OPENLDAP)), \
+			CPPFLAGS="-I$(call gb_UnpackedTarball_get_dir,openldap/include)" \
+			LDFLAGS="-L$(OUTDIR)/lib" \
+			EXTRA_LDAP_LIBS="-llber -lssl3 -lsmime3 -lnss3 -lnssutil3 -lplds4 -lplc4 -lnspr4" \
+		) \
 	&& cd src/interfaces/libpq \
 	&& MAKEFLAGS= && $(MAKE) all-static-lib libpq-flags.mk \
 	&& touch $@
