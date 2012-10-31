75a087c80730bdce257756d863397da8268469b5
Matúš Kukan <matus.kukan@gmail.com>
2012-10-31 23:48:49 +0100

    compile concat-deps in solenv; also put them into $(OUTDIR)
    Change-Id: I4ef21f14a77442b60059c649b83435c775670060

--- solenv/gbuild/LinkTarget.mk
+++ solenv/gbuild/LinkTarget.mk
@@ -386,7 +386,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(8),$(call gb_GenCObject_get_dep_target,$(object))) \
 		$(foreach object,$(9),$(call gb_GenCxxObject_get_dep_target,$(object))) \
 		) && \
-	$(SOLARENV)/bin/concat-deps $${RESPONSEFILE} > $(1)) && \
+	$(call gb_Executable_get_target_for_build,concat-deps) $${RESPONSEFILE} > $(1)) && \
 	rm -f $${RESPONSEFILE}
 
 endef
