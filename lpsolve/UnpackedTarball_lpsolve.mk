a7d34af344900233ba08aee2a56a3daf85ab2421
Norbert Thiebaud <nthiebaud@gmail.com>
2012-11-04 08:05:10 -0600

    lpsolve: missing owner for add_file
    Change-Id: I6cf49b204ca8fd00ba5475cb3effbe72546e3a6f

--- lpsolve/UnpackedTarball_lpsolve.mk
+++ lpsolve/UnpackedTarball_lpsolve.mk
@@ -14,15 +14,23 @@ $(eval $(call gb_UnpackedTarball_set_tarball,lpsolve,$(LPSOLVE_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,lpsolve,3))
 
 ifeq ($(GUI_FOR_BUILD),WNT)
+
 $(eval $(call gb_UnpackedTarball_add_patches,lpsolve,\
 	lpsolve/lp_solve_5.5-windows.patch \
 ))
+
 else
+
 $(eval $(call gb_UnpackedTarball_add_patches,lpsolve,\
 	lpsolve/lp_solve-aix.patch \
 	lpsolve/lp_solve-fixed-warn.patch \
 	lpsolve/lp_solve_5.5.patch \
 ))
-$(eval $(call gb_UnpackedTarball_add_file,lpsolve/lpsolve55/ccc.static,lpsolve/ccc.static))
+
+$(eval $(call gb_UnpackedTarball_add_file,lpsolve,\
+	lpsolve/lpsolve55/ccc.static,lpsolve/ccc.static))
+
 endif
 # vim: set noet sw=4 ts=4:
+
+
