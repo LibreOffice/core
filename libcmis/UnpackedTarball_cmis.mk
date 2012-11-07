b7686e41f1bbf655a4e3a5f142a5520faecfa6bc
Cédric Bosdonnat <cedric.bosdonnat@free.fr>
2012-11-07 14:00:15 +0100

    CMIS: fixed <wsse:Expires> output: c_str() on a temp string object isn't good
    Change-Id: I7de9dd2372cd7b61a3bc03bdca0023a3bc7484bd

--- libcmis/UnpackedTarball_cmis.mk
+++ libcmis/UnpackedTarball_cmis.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,cmis,0))
 $(eval $(call gb_UnpackedTarball_add_patches,cmis, \
 	libcmis/libcmis-0.3.0-warnings.patch \
 	libcmis/libcmis-0.3.0-win.patch \
+	libcmis/libcmis-0.3.0.patch \
 ))
 
 ifeq ($(OS)$(COM),WNTMSC)
