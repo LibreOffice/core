LibreOffice/core

commit f42a9309760565d24619bad9ee1f5561ce89c14b

Use prefixes for these UNO components
Change-Id: I719f10818fd211bfe60e5351a5de53a716fdf20f

connectivity/source/drivers/jdbc/jservices.cxx
@@ -89,7 +89,7 @@ component_getImplementationEnvironment(
     *ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME ":affine";
 }
 
-extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
+extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL jdbc_component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
                     void* /*pRegistryKey*/)

connectivity/source/commontools/FDatabaseMetaDataResultSet.cxx
@@ -884,7 +884,7 @@ extern "C"
 {
 
 //---------------------------------------------------------------------------------------
-SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(const sal_Char* implName, ::com::sun::star::lang::XMultiServiceFactory* serviceManager, void* registryKey)
+SAL_DLLPUBLIC_EXPORT void* SAL_CALL dbtools_component_getFactory(const sal_Char* implName, ::com::sun::star::lang::XMultiServiceFactory* serviceManager, void* registryKey)
 {
     return cppu::component_getFactoryHelper(implName, serviceManager, registryKey, entries);
 }

connectivity/source/cpool/Zregistration.cxx
@@ -34,7 +34,7 @@ extern "C"
 {
 
 //---------------------------------------------------------------------------------------
-SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(const sal_Char* _pImplName, ::com::sun::star::lang::XMultiServiceFactory* _pServiceManager, void* /*_pRegistryKey*/)
+SAL_DLLPUBLIC_EXPORT void* SAL_CALL dbpool2_component_getFactory(const sal_Char* _pImplName, ::com::sun::star::lang::XMultiServiceFactory* _pServiceManager, void* /*_pRegistryKey*/)
 {
     void* pRet = NULL;
 

connectivity/source/cpool/dbpool2.component
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  -->
 
-<component loader="com.sun.star.loader.SharedLibrary"
+<component loader="com.sun.star.loader.SharedLibrary" prefix="dbpool2"
     xmlns="http://openoffice.org/2010/uno-components">
   <implementation name="com.sun.star.sdbc.OConnectionPool">
     <service name="com.sun.star.sdbc.ConnectionPool"/>

connectivity/source/dbtools/dbtools.component
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  -->
 
-<component loader="com.sun.star.loader.SharedLibrary"
+<component loader="com.sun.star.loader.SharedLibrary" prefix="dbtools"
     xmlns="http://openoffice.org/2010/uno-components">
   <implementation name="org.openoffice.comp.helper.DatabaseMetaDataResultSet">
     <service name="com.sun.star.sdbc.ResultSet"/>

connectivity/source/drivers/ado/Aservices.cxx
@@ -75,7 +75,7 @@ struct ProviderRequest
 };
 
 //---------------------------------------------------------------------------------------
-extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnvironment(
+extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL ado_component_getImplementationEnvironment(
                 const sal_Char **ppEnvTypeName,
                 uno_Environment        ** /*ppEnv*/)
 {
@@ -83,7 +83,7 @@ extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL component_getImplementationEnviron
 }
 
 //---------------------------------------------------------------------------------------
-extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
+extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL ado_component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
                     void* /*pRegistryKey*/)

connectivity/source/drivers/ado/ado.component
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  -->
 
-<component loader="com.sun.star.loader.SharedLibrary"
+<component loader="com.sun.star.loader.SharedLibrary" prefix="ado"
     xmlns="http://openoffice.org/2010/uno-components">
   <implementation name="com.sun.star.comp.sdbc.ado.ODriver">
     <service name="com.sun.star.sdbc.Driver"/>

connectivity/source/drivers/calc/Cservices.cxx
@@ -75,7 +75,7 @@ struct ProviderRequest
 };
 
 //---------------------------------------------------------------------------------------
-extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL component_getFactory(
+extern "C" SAL_DLLPUBLIC_EXPORT void* SAL_CALL connectivity_calc_component_getFactory(
                     const sal_Char* pImplementationName,
                     void* pServiceManager,
                     void* /*pRegistryKey*/)

commit b3df643ffe8f6c17fd12032a75e2584799e3c115

connectivity: fix re-introduced bug fdo 35023
regression from f42a9309760565d24619bad9ee1f5561ce89c14b
which forgot to adapt getImplementationEnvironment to new prefix

Change-Id: If5a6f804b8f9f91e22f093a76f6b81a2b62991ab

connectivity/source/drivers/jdbc/jservices.cxx
@@ -75,7 +75,7 @@ struct ProviderRequest
 };
 
 extern "C" SAL_DLLPUBLIC_EXPORT void SAL_CALL
-component_getImplementationEnvironment(
+jdbc_component_getImplementationEnvironment(
     char const ** ppEnvTypeName, uno_Environment **)
 {
     // Recent Java 6 VMs make calls to JNI Attach/DetachCurrentThread (which