f730320bad9f4c9c7a02b55641ed0604c589f1e1
jailletc36 <christophe.jaillet@wanadoo.fr>
2012-11-09 14:57:13 +0100

    cppCheck: remove dead assignment + a little formatting
    Change-Id: I78f7f7ae7373e00e0750abc4566c71a8b9c1640a
    Signed-off-by: jailletc36 <christophe.jaillet@wanadoo.fr>

--- sc/source/ui/dbgui/tpsubt.cxx
+++ sc/source/ui/dbgui/tpsubt.cxx
@@ -275,20 +275,19 @@ void ScTpSubTotalGroup::FillListBoxes()
         SCTAB   nTab        = pViewData->GetTabNo();
         SCCOL   nMaxCol     = rSubTotalData.nCol2;
         SCCOL   col;
-        sal_uInt16  i=0;
+        sal_uInt16  i;
         rtl::OUString  aFieldName;
 
         aLbGroup.Clear();
         aLbColumns.Clear();
         aLbGroup.InsertEntry( aStrNone, 0 );
 
-        i=0;
-        for ( col=nFirstCol; col<=nMaxCol && i<SC_MAXFIELDS; col++ )
+        for ( i=0, col=nFirstCol; col<=nMaxCol && i<SC_MAXFIELDS; col++ )
         {
             pDoc->GetString( col, nFirstRow, nTab, aFieldName );
             if ( aFieldName.isEmpty() )
             {
-               rtl::OUStringBuffer aBuf;
+                rtl::OUStringBuffer aBuf;
                 aBuf.append(aStrColumn);
                 aFieldName = aBuf.makeStringAndClear().replaceAll("%1", ScColToAlpha( col ));
             }
