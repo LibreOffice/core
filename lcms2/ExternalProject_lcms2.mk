0d08bef014415c37c9914ca1e369e2a436a31e1c
Caolán McNamara <caolanm@redhat.com>
2012-11-12 11:09:51 +0000

    fix to make MacOSX run again
    Change-Id: I7a021c38e1c2e39641a28d95cc9cc7e3d5ee8225

--- lcms2/ExternalProject_lcms2.mk
+++ lcms2/ExternalProject_lcms2.mk
@@ -37,6 +37,7 @@ $(call gb_ExternalProject_get_state_target,lcms2,build):
 	CPPFLAGS=" $(SOLARINC)" \
 	$(if $(filter-out WNTGCC,$(OS)$(COM)),,CPPFLAGS=" -DCMS_DLL_BUILD") \
 	$(if $(filter IOS ANDROID,$(OS)), --disable-shared --enable-static, --enable-shared --disable-static) \
+	$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 	&& cd src \
 	&& $(MAKE) \
 	&& touch $@
