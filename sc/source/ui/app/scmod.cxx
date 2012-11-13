3ee088ece2abbdff699f44aa4a5b7e3c8b82aa48
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-11-13 14:14:30 -0500

    fdo#56776: Change in formula options should cause repaint.
    Else Calc would fail to update the column headers due to A1 to R1C1
    change (and vise versa).
    
    Change-Id: I2c403212f5bc539aea92370e60da96f480549d6d

--- sc/source/ui/app/scmod.cxx
+++ sc/source/ui/app/scmod.cxx
@@ -1068,6 +1068,11 @@ void ScModule::ModifyOptions( const SfxItemSet& rOptSet )
     if (rOptSet.HasItem(SID_SCFORMULAOPTIONS, &pItem))
     {
         const ScFormulaOptions& rOpt = ((const ScTpFormulaItem*)pItem)->GetFormulaOptions();
+
+        if (!pFormulaCfg || (*pFormulaCfg != rOpt))
+            // Formula options have changed. Repaint the column headers.
+            bRepaint = true;
+
         SetFormulaOptions( rOpt );
 
         if ( pDocSh )
