9d510d2b9e1ef45cd84eb3c88072bed1dd949670
Enrico Weigelt, metux ITS <metuxitservice@googlemail.com>
2012-11-16 04:40:36 +0100

    Xlib import via generic pkg-config
    Generic importing Xlib+friends via pkg-config, instead of scanning
    through a list of directories. This is very helpful for non-standard
    installation pathes and crosscompiling, as the generic pkg-config
    infrastructure will handle it all.
    
    Also dropping the obsolete bundled Xext headers.
    
    Change-Id: I6ee381030ff9f1d2d83062a17ab55ad3d847a4c6

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -2083,25 +2083,9 @@ endif # SYSTEM_LIBORCUS
 
 ifeq ($(GUIBASE),unx)
 
-# TODO: do we really need these X11 headers in the repo?
-ifneq ($(filter X11_EXTENSIONS,$(BUILD_TYPE)),)
-
-define gb_LinkTarget__use_x11extensions
-$(call gb_LinkTarget_use_packages,$(1),\
-	x11_extensions_inc \
-)
-endef
-
-else # !X11_EXTENSIONS
-
-gb_LinkTarget__use_x11extensions :=
-
-endif # X11_EXTENSIONS
-
 ifeq ($(XRANDR_DLOPEN),FALSE)
 
 define gb_LinkTarget__use_Xrandr
-$(call gb_LinkTarget__use_x11extensions,$(1))
 
 $(call gb_LinkTarget_set_include,$(1),\
 	$$(INCLUDE) \
@@ -2116,7 +2100,6 @@ endef
 else # XRANDR_DLOPEN
 
 define gb_LinkTarget__use_Xrandr
-$(call gb_LinkTarget__use_x11extensions,$(1))
 
 $(call gb_LinkTarget_add_defs,$(1),\
 	-DXRANDR_DLOPEN \
@@ -2126,7 +2109,6 @@ endef
 endif # XRANDR_DLOPEN
 
 define gb_LinkTarget__use_Xrender
-$(call gb_LinkTarget__use_x11extensions,$(1))
 
 $(call gb_LinkTarget_set_include,$(1),\
 	$$(INCLUDE) \
