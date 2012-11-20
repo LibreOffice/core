d5d7232fb5eac4a449cd35ab605075a30c7324d3
Andras Timar <atimar@suse.com>
2012-11-20 15:09:35 +0100

    fix readlicense_oo makefile, po file moved
    Change-Id: I1ff2085b773c3e46915140d816e940ddf048998f

--- readlicense_oo/CustomTarget_readme.mk
+++ readlicense_oo/CustomTarget_readme.mk
@@ -36,7 +36,7 @@ readlicense_oo_README_XRM := $(readlicense_oo_DIR)/readme.xrm
 $(readlicense_oo_DIR)/readme.xrm : \
 		$(SRCDIR)/readlicense_oo/docs/readme.xrm \
 		$(readlicense_XRMEXTARGET) \
-		$(foreach lang,$(filter-out en-US,$(gb_WITH_LANG)),$(gb_POLOCATION)/$(lang)/readlicense_oo/docs/readme.po) \
+		$(foreach lang,$(filter-out en-US,$(gb_WITH_LANG)),$(gb_POLOCATION)/$(lang)/readlicense_oo/docs.po) \
 		| $(readlicense_oo_DIR)/.dir
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),XRM,1)
 	$(call gb_Helper_abbreviate_dirs, \
