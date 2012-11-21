eae732641ff2a60bb5bc50e60f8404aae7a7decb
Michael Meeks <michael.meeks@suse.com>
2012-11-21 14:51:27 +0000

    fix day of week naming in sal unit test.

--- sal/qa/osl/file/osl_File.cxx
+++ sal/qa/osl/file/osl_File.cxx
@@ -220,8 +220,8 @@ inline void printTime( TimeValue *tv )
         case 0: printf("Sun. "); break;
         case 1: printf("Mon. "); break;
         case 2: printf("Tue. "); break;
-        case 3: printf("Thr. "); break;
-        case 4: printf("Wen. "); break;
+        case 3: printf("Wed. "); break;
+        case 4: printf("Thu. "); break;
         case 5: printf("Fri. "); break;
         case 6: printf("Sat. "); break;
     }
