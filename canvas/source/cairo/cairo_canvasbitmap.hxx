50a7098b2ee7047cda459e60720e5206eb3d4963
Ricardo Montania <ricardo@linuxafundo.com.br>
2012-11-26 13:36:00 -0200

    More ::rtl::OUString -> OUString in canvas
    Change-Id: If99dfc98ace1bf62cd5ab05c596f4f6ffc5fbd2f
    Reviewed-on: https://gerrit.libreoffice.org/1175
    Tested-by: Olivier Hallot <olivier.hallot@alta.org.br>
    Reviewed-by: Olivier Hallot <olivier.hallot@alta.org.br>

--- canvas/source/cairo/cairo_canvasbitmap.hxx
+++ canvas/source/cairo/cairo_canvasbitmap.hxx
@@ -88,9 +88,9 @@ namespace cairocanvas
         DECLARE_UNO3_XCOMPONENT_AGG_DEFAULTS( CanvasBitmap, CanvasBitmapBase_Base, ::cppu::WeakComponentImplHelperBase );
 
         // XServiceInfo
-        virtual ::rtl::OUString SAL_CALL getImplementationName(  ) throw (::com::sun::star::uno::RuntimeException);
-        virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName ) throw (::com::sun::star::uno::RuntimeException);
-        virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual OUString SAL_CALL getImplementationName(  ) throw (::com::sun::star::uno::RuntimeException);
+        virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName ) throw (::com::sun::star::uno::RuntimeException);
+        virtual ::com::sun::star::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw (::com::sun::star::uno::RuntimeException);
 
         // SurfaceProvider
         virtual SurfaceSharedPtr getSurface();
