7226672d4b894192d5312885c1c13a6394c27187
Rob Snelders <programming@ertai.nl>
2012-11-28 18:35:45 +0100

    rename the URL for feedback to something more positive
    Change-Id: Iea248eeb3c3eacf9b0f123daa52516f5c1f0259c

--- sfx2/source/appl/appserv.cxx
+++ sfx2/source/appl/appserv.cxx
@@ -424,7 +424,7 @@ void SfxApplication::MiscExec_Impl( SfxRequest& rReq )
         case SID_SEND_FEEDBACK:
         {
             ::rtl::OUString module = SfxHelp::GetCurrentModuleIdentifier();
-            ::rtl::OUString sURL("http://hub.libreoffice.org/file-a-bug/?version=" + utl::ConfigManager::getAboutBoxProductVersion() +
+            ::rtl::OUString sURL("http://hub.libreoffice.org/send-feedback/?version=" + utl::ConfigManager::getAboutBoxProductVersion() +
 				 "&locale=" + utl::ConfigManager::getLocale() + "&module=" + module.copy(module.lastIndexOf(".") + 1 )  );
             try
             {
