66308dd049b98476127265e9cc9ac32f19dfccaf
Eike Rathke <erack@redhat.com>
2012-11-28 18:30:08 +0100

    changes to Congolese locales
    Changes to commit a81a9109ed43e0b3880c9bd88e58f2b21065226b
    
    The ..._CONGO identifier and "... (Congo)" resource string changes are
    only to match the current convention that if a locale is not of the
    default country of a language we mention the country. This helps in case
    we later add the other locale of a language. Having two Lang-IDs then
    with different sublanguage and adding the ISO locales is good practice.
    
    For axk-CG it should be LANGUAGE_USER_YAKA_CONGO instead of
    LANGUAGE_USER_AKA and the resource string should be "Aka (Congo)",
    because it is not the default axk-CF (Central African Republic), see
    http://www.ethnologue.com/show_language.asp?code=axk
    
    For kng-CG it should be LANGUAGE_USER_KIKONGO_CONGO instead of
    LANGUAGE_USER_KIKONGO and the resource string should be "Kikongo
    (Congo)", because it is not the default kng-CD (Democratic Republic of
    the Congo), see http://www.ethnologue.com/show_language.asp?code=kng
    
    However, changed name to Koongo instead of Kikongo because that is the
    default name even for Congo, see
    http://www.ethnologue.com/show_language.asp?code=kng, and used
    LANGUAGE_USER_KOONGO_CONGO instead.
    
    For njy-CG it should be LANGUAGE_USER_NJYEM_CONGO instead of
    LANGUAGE_USER_NJYEM and the resource string should be "Njyem (Congo)",
    because it is not the default njy-CM (Cameroon), see
    http://www.ethnologue.com/show_language.asp?code=njy
    
    For puu-CG it should be LANGUAGE_USER_PUNU_CONGO instead of
    LANGUAGE_USER_PUNU and the resource string should be "Punu (Congo)",
    because it is not the default puu-GA (Gabon), see
    http://www.ethnologue.com/show_language.asp?code=puu
    
    For yom-CG it should be LANGUAGE_USER_YOMBE_CONGO instead of
    LANGUAGE_USER_YOMBE and the resource string should be "Yombe (Congo)",
    because it is not the default yom-CD (Democratic Republic of the Congo),
    see http://www.ethnologue.com/show_language.asp?code=yom
    
    For each of these ..._CONGO lang-IDs introduced the default ID and made
    the ..._CONGO ID with sublanguage 0x20, adapted corresponding IDs in
    *.xml files.
    
    All IDs in those *_CG.xml files needed to be corrected anyway as they
    all had a replaceTo="[$CFA-63A]" copied from sg_CF.xml, the ID needs to
    match that of the LangID instead.
    
    As all LC_FORMAT element section were identical, made them refer to
    kng_CG instead of having duplicated copies, same for LC_CURRENCY.
    
    Removed the executable permission bits from *_CG.xml files.
    
    Change-Id: Iae06af5fbf19e3a3c83a4f4b4dcb857213df629e

--- i18npool/inc/i18npool/lang.h
+++ i18npool/inc/i18npool/lang.h
@@ -547,19 +547,24 @@ typedef unsigned short LanguageType;
 #define LANGUAGE_USER_PAPIAMENTU_CURACAO    0x8479  /* makeLangID( 0x21, getPrimaryLanguage( LANGUAGE_PAPIAMENTU)) */
 #define LANGUAGE_USER_PAPIAMENTU_BONAIRE    0x8879  /* makeLangID( 0x22, getPrimaryLanguage( LANGUAGE_PAPIAMENTU)) */
 #define LANGUAGE_USER_AKA                   0x0677
+#define LANGUAGE_USER_AKA_CONGO             0x8277  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_AKA)) */
 #define LANGUAGE_USER_DIBOLE                0x0678
 #define LANGUAGE_USER_DOONDO                0x0679
 #define LANGUAGE_USER_KAAMBA                0x067A
-#define LANGUAGE_USER_KIKONGO               0x067B
+#define LANGUAGE_USER_KOONGO                0x067B
+#define LANGUAGE_USER_KOONGO_CONGO          0x827B  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_KOONGO)) */
 #define LANGUAGE_USER_KUNYI                 0x067C
 #define LANGUAGE_USER_NGUNGWEL              0x067D
 #define LANGUAGE_USER_NJYEM                 0x067E
+#define LANGUAGE_USER_NJYEM_CONGO           0x827E  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_NJYEM)) */
 #define LANGUAGE_USER_PUNU                  0x067F
+#define LANGUAGE_USER_PUNU_CONGO            0x827F  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_PUNU)) */
 #define LANGUAGE_USER_SUUNDI                0x0680
 #define LANGUAGE_USER_TEKE_KUKUYA           0x0681
 #define LANGUAGE_USER_TSAANGI               0x0682
 #define LANGUAGE_USER_YAKA                  0x0683
 #define LANGUAGE_USER_YOMBE                 0x0684
+#define LANGUAGE_USER_YOMBE_CONGO           0x8284  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_YOMBE)) */
 #define LANGUAGE_USER_SYSTEM_CONFIG         0xFFFE  /* not a locale, to be used only in configuration context to obtain system default, primary 0x3fe, sub 0x3f */
 
 #endif /* INCLUDED_I18NPOOL_LANG_H */
