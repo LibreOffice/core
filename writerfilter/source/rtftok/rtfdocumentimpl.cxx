fb17de573c582eca7548bf8cd0a0ea86cfad17df
Miklos Vajna <vmiklos@suse.cz>
2012-11-30 16:32:56 +0100

    fdo#57708 fix RTF import of pictures right before text frames
    Change-Id: Ibfeaadd5b48fd48c2c1f20de5dc272d9a326bfc4
    (cherry picked from commit 8063e36115a11ddf3db05928db9287947beee74d)
    Reviewed-on: https://gerrit.libreoffice.org/1215
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -280,7 +280,8 @@ RTFDocumentImpl::RTFDocumentImpl(uno::Reference<uno::XComponentContext> const& x
     m_aHexBuffer(),
     m_bIgnoreNextContSectBreak(false),
     m_bNeedSect(true),
-    m_bWasInFrame(false)
+    m_bWasInFrame(false),
+    m_bHadPicture(false)
 {
     OSL_ASSERT(xInputStream.is());
     m_pInStream.reset(utl::UcbStreamHelper::CreateStream(xInputStream, sal_True));
@@ -479,6 +480,8 @@ void RTFDocumentImpl::parBreak()
     runBreak();
     Mapper().endCharacterGroup();
     Mapper().endParagraphGroup();
+
+    m_bHadPicture = false;
 
     // start new one
     Mapper().startParagraphGroup();
@@ -789,6 +792,8 @@ int RTFDocumentImpl::resolvePict(bool bInline)
         m_pCurrentBuffer->push_back(make_pair(BUFFER_PROPS, pValue));
     }
 
+    // Make sure we don't loose these properties with a too early reset.
+    m_bHadPicture = true;
     return 0;
 }
 
@@ -1926,6 +1931,8 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
             }
             break;
         case RTF_PARD:
+            if (m_bHadPicture)
+                dispatchSymbol(RTF_PAR);
             m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
             m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
             m_aStates.top().resetFrame();
