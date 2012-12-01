794c1f97f57710846c22c21234e9adacedecd24a
Miklos Vajna <vmiklos@suse.cz>
2012-12-01 11:09:18 +0100

    writerfilter: remove unused WW8Analyzer
    Change-Id: I711bc4d6b7100a1b089ed5bfeec7ff14f2727138

--- writerfilter/Library_writerfilter.mk
+++ writerfilter/Library_writerfilter.mk
@@ -168,7 +168,6 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
     writerfilter/source/resourcemodel/Protocol \
     writerfilter/source/resourcemodel/ResourceModelHelper \
     writerfilter/source/resourcemodel/TagLogger \
-    writerfilter/source/resourcemodel/WW8Analyzer \
     writerfilter/source/resourcemodel/XPathLogger \
     writerfilter/source/resourcemodel/qnametostrcore \
     writerfilter/source/resourcemodel/resourcemodel \
