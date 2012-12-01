1611806226329e5532e6393d0ac2275d7e99eb61
José Guilherme Vanz <guilherme.sft@gmail.com>
2012-11-30 22:24:01 -0200

    Removal ::rtl:: prefixes and macros in oxx
    This commit removes some ::rtl:: prefixes and RTL_CONSTASCII_STRINGPARAM,
     RTL_CONSTASCII_USTRINGPARAM macros in oxx.
    
    Change-Id: I12bfb2528e8a9aaf3f119a1e6f0ae0246d16a772
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/1211
    Reviewed-by: Olivier Hallot <olivier.hallot@alta.org.br>
    Tested-by: Olivier Hallot <olivier.hallot@alta.org.br>

--- oox/source/core/fragmenthandler2.cxx
+++ oox/source/core/fragmenthandler2.cxx
@@ -67,7 +67,7 @@ bool FragmentHandler2::prepareMceContext( sal_Int32 nElement, const AttributeLis
 
         case MCE_TOKEN( Choice ):
             {
-                OUString aRequires = rAttribs.getString( ( XML_Requires ), OUString(RTL_CONSTASCII_USTRINGPARAM("none")) );
+                OUString aRequires = rAttribs.getString( ( XML_Requires ), OUString("none") );
                 aRequires = getFilter().getNamespaceURL( aRequires );
                 if( getFilter().getNamespaceId( aRequires ) > 0 && !aMceState.empty() && aMceState.back() == MCE_STARTED )
                     aMceState.back() = MCE_FOUND_CHOICE;
