a75a79e32718f98bf1f84199c944a831bec75a6a
Stephan Bergmann <sbergman@redhat.com>
2012-12-04 13:57:53 +0100

    Adapt scp2 to WITH_MOZAB4WIN
    Change-Id: Ibef1ceb454eafba7129c774b8ced8c743ea3ce3d

--- scp2/InstallModule_ooo.mk
+++ scp2/InstallModule_ooo.mk
@@ -66,7 +66,6 @@ $(eval $(call gb_InstallModule_define_if_set,scp2/ooo,\
 	SYSTEM_LIBXSLT \
 	SYSTEM_LPSOLVE \
 	SYSTEM_LUCENE \
-	SYSTEM_MOZILLA \
 	SYSTEM_MYTHES \
 	SYSTEM_NEON \
 	SYSTEM_NSS \
@@ -77,6 +76,7 @@ $(eval $(call gb_InstallModule_define_if_set,scp2/ooo,\
 	SYSTEM_REDLAND \
 	SYSTEM_SNDFILE \
 	SYSTEM_STDLIBS \
+	WITH_MOZAB4WIN \
 ))
 
 $(eval $(call gb_InstallModule_define_value_if_set,scp2/ooo,\
@@ -148,9 +148,6 @@ $(eval $(call gb_InstallModule_add_defs,scp2/ooo,\
 	) \
 	$(if $(filter YES,$(SYSTEM_HSQLDB)),\
 		-DHSQLDB_JAR=\""$(call gb_Helper_make_path,$(HSQLDB_JAR))"\" \
-	) \
-	$(if $(filter-out YES,$(WITH_MOZILLA)),\
-		-DWITHOUT_MOZILLA \
 	) \
 	$(if $(filter-out YES,$(WITH_MYSPELL_DICTS)),\
 		-DWITHOUT_MYSPELL_DICTS \
