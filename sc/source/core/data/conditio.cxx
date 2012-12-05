664baba931bafd4e36fb822291b1d42c4554b88b
Markus Mohrhard <markus.mohrhard@googlemail.com>
2012-12-05 02:25:33 +0100

    adapt cond format range when copying sheets, fdo#56742
    Change-Id: I9b28ac161f326ebeeaf0fa22e0a96915187d7c94
    (cherry picked from commit 1c60abfdb617039cedc53982c7c8eca640e28cac)
    
    Signed-off-by: Eike Rathke <erack@redhat.com>

--- sc/source/core/data/conditio.cxx
+++ sc/source/core/data/conditio.cxx
@@ -1547,7 +1547,11 @@ void ScConditionalFormat::CompileXML()
 void ScConditionalFormat::UpdateReference( UpdateRefMode eUpdateRefMode,
                                 const ScRange& rRange, SCsCOL nDx, SCsROW nDy, SCsTAB nDz )
 {
-    maRanges.UpdateReference( eUpdateRefMode, pDoc, rRange, nDx, nDy, nDz );
+    if( eUpdateRefMode == URM_COPY && nDz != 0 )
+        maRanges.UpdateReference( URM_MOVE, pDoc, rRange, nDx, nDy, nDz );
+    else
+        maRanges.UpdateReference( eUpdateRefMode, pDoc, rRange, nDx, nDy, nDz );
+
     for(CondFormatContainer::iterator itr = maEntries.begin(); itr != maEntries.end(); ++itr)
         itr->UpdateReference(eUpdateRefMode, rRange, nDx, nDy, nDz);
 }
