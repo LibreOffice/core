c95f6b679a548f7b79d10005b0415d7a0233e4b4
Michael Meeks <michael.meeks@suse.com>
2012-12-05 11:35:41 +0000

    remove obsolete stdidlclass.

--- cppuhelper/Library_cppuhelper.mk
+++ cppuhelper/Library_cppuhelper.mk
@@ -69,7 +69,6 @@ $(eval $(call gb_Library_add_exception_objects,cppuhelper,\
 	cppuhelper/source/propshlp \
 	cppuhelper/source/servicefactory \
 	cppuhelper/source/shlib \
-	cppuhelper/source/stdidlclass \
 	cppuhelper/source/supportsservice \
 	cppuhelper/source/tdmgr \
 	cppuhelper/source/typeprovider \
