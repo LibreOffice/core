f2299ef98c9db7018c413b06b08baa6e2c9dd257
Ariel Constenla-Haile <arielch@apache.org>
2012-12-06 12:30:14 +0000

    Related: #i121442# Remove unused UNO StatusbarControllers
    (cherry picked from commit 06b91eb4cc81dde86875445c5acaa5c57111333b)
    
    Conflicts:
    	framework/inc/uielement/logoimagestatusbarcontroller.hxx
    	framework/inc/uielement/logotextstatusbarcontroller.hxx
    	framework/inc/uielement/simpletextstatusbarcontroller.hxx
    	framework/source/classes/resource.src
    	framework/source/register/registerservices.cxx
    	framework/source/uielement/logoimagestatusbarcontroller.cxx
    	framework/source/uielement/logotextstatusbarcontroller.cxx
    	framework/source/uielement/simpletextstatusbarcontroller.cxx
    	officecfg/registry/data/org/openoffice/Office/UI/Controller.xcu
    
    Change-Id: I799bd0fa29191efce4fe9a09f74247e38d8f8873
    (cherry picked from commit 4fcda14828d2fa65cc2541ed3e3cb93dd7bc9ac8)

--- framework/source/register/registertemp.cxx
+++ framework/source/register/registertemp.cxx
@@ -42,9 +42,6 @@
 #include <recording/dispatchrecordersupplier.hxx>
 #include <services/license.hxx>
 #include <services/uriabbreviation.hxx>
-#include <uielement/simpletextstatusbarcontroller.hxx>
-#include <uielement/logoimagestatusbarcontroller.hxx>
-#include <uielement/logotextstatusbarcontroller.hxx>
 #include <uielement/fontmenucontroller.hxx>
 #include <uielement/fontsizemenucontroller.hxx>
 #include <uielement/footermenucontroller.hxx>
@@ -59,14 +56,11 @@ COMPONENTGETFACTORY ( fwl,
                         IFFACTORY( ::framework::MediaTypeDetectionHelper        )
                         IFFACTORY( ::framework::MailToDispatcher                        ) else
                         IFFACTORY( ::framework::ServiceHandler                          )   else
-                        IFFACTORY( ::framework::LogoTextStatusbarController             )   else
-                        IFFACTORY( ::framework::LogoImageStatusbarController            )   else
                         IFFACTORY( ::framework::License                                 )   else
                         IFFACTORY( ::framework::PopupMenuDispatcher                     )   else
                         IFFACTORY( ::framework::DispatchHelper                          )   else
                         IFFACTORY( ::framework::DispatchRecorder                        )   else
                         IFFACTORY( ::framework::DispatchRecorderSupplier                )   else
-                        IFFACTORY( ::framework::SimpleTextStatusbarController           )   else
                         IFFACTORY( ::framework::ToolbarsMenuController                  )   else
                         IFFACTORY( ::framework::FontMenuController                      )   else
                         IFFACTORY( ::framework::MacrosMenuController                    )   else
