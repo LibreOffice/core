879f0d057dfe6ebabbf1290a09a183c610cdb3fa
Arnaud Versini <arnaud.versini@gmail.com>
2012-12-08 11:35:00 +0100

    API CHANGE Remove useless deprecated rtl/memory.h
    Change-Id: Iac156b004464018225bbfda24f0a234f9ebcb19f

--- sal/osl/all/compat.cxx
+++ sal/osl/all/compat.cxx
@@ -37,6 +37,29 @@ SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL osl_tryToAcquireSemaphore(void *) {
     for (;;) { std::abort(); } // avoid "must return a value" warnings
 }
 
+SAL_DLLPUBLIC_EXPORT void SAL_CALL rtl_zeroMemory(void *, sal_Size) {
+    std::abort();
+}
+
+SAL_DLLPUBLIC_EXPORT void SAL_CALL rtl_fillMemory(void *, sal_Size, sal_uInt8) {
+    std::abort();
+}
+
+SAL_DLLPUBLIC_EXPORT void SAL_CALL rtl_copyMemory(void *, const void *, sal_Size) {
+    std::abort();
+}
+
+SAL_DLLPUBLIC_EXPORT void SAL_CALL rtl_moveMemory(void *, const void *, sal_Size) {
+    std::abort();
+}
+
+SAL_DLLPUBLIC_EXPORT sal_Int32 SAL_CALL rtl_compareMemory(const void *, const void *, sal_Size) {
+    std::abort();
+}
+
+SAL_DLLPUBLIC_EXPORT void* SAL_CALL rtl_findInMemory(const void *, sal_uInt8, sal_Size) {
+    std::abort();
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
