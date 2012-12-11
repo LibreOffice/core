c8915f93a498a9d4e9c096f8ade5be98da6d0e1e
Andras Timar <atimar@suse.com>
2012-12-11 17:20:55 +0100

    enable MSP creation and bump version number for 1st PTF
    Change-Id: I18295f2c52f7cb4461e8efbd1f40121ac56e0c5e

--- solenv/bin/modules/installer/globals.pm
+++ solenv/bin/modules/installer/globals.pm
@@ -388,7 +388,7 @@ BEGIN
     $cab_file_per_component = 0;
     $cabfilecompressionlevel = 21; # Using LZX compression, possible values are: 15 | 16 | ... | 21 (best compression)
     $number_of_cabfiles = 1;    # only for $fix_number_of_cab_files = 1
-    $include_cab_in_msi = 1;
+    $include_cab_in_msi = 0;
     $use_packages_for_cabs = 0;
     $msidatabasename = "";
     $prepare_winpatch = 0;
