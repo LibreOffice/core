b1ed6678735b30a62ed329e82ea1e7e573e93484
Luboš Luňák <l.lunak@suse.cz>
2012-12-11 08:37:33 +0100

    this class is apparently a dupe of one in editeng

--- xmloff/inc/xmloff/xmlcnitm.hxx
+++ xmloff/inc/xmloff/xmlcnitm.hxx
@@ -20,63 +20,8 @@
 #ifndef _XMLOFF_XMLCNITM_HXX
 #define _XMLOFF_XMLCNITM_HXX
 
-#include <svl/poolitem.hxx>
-
-class SvXMLNamespaceMap;
-namespace rtl { class OUString; }
-
-//============================================================================
-
-class SvXMLAttrContainerItem_Impl;
-
-class SvXMLAttrContainerItem: public SfxPoolItem
-{
-    SvXMLAttrContainerItem_Impl *pImpl;
-
-public:
-    TYPEINFO();
-
-    SvXMLAttrContainerItem( sal_uInt16 nWhich = 0 );
-    SvXMLAttrContainerItem( const SvXMLAttrContainerItem& );
-    virtual ~SvXMLAttrContainerItem();
-
-    virtual int operator==( const SfxPoolItem& ) const;
-    using SfxPoolItem::Compare;
-    virtual int Compare( const SfxPoolItem &rWith ) const;
-
-    virtual SfxItemPresentation GetPresentation(
-                                SfxItemPresentation ePresentation,
-                                SfxMapUnit eCoreMetric,
-                                SfxMapUnit ePresentationMetric,
-                                XubString &rText,
-                                const IntlWrapper *pIntlWrapper = 0 ) const;
-
-    virtual sal_uInt16 GetVersion( sal_uInt16 nFileFormatVersion ) const;
-
-    virtual bool QueryValue( com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 ) const;
-    virtual bool PutValue( const com::sun::star::uno::Any& rVal, sal_uInt8 nMemberId = 0 );
-
-    virtual SfxPoolItem *Clone( SfxItemPool * = 0) const
-    { return new SvXMLAttrContainerItem( *this ); }
-
-    sal_Bool AddAttr( const ::rtl::OUString& rLName,
-                          const ::rtl::OUString& rValue );
-    sal_Bool AddAttr( const ::rtl::OUString& rPrefix,
-                          const ::rtl::OUString& rNamespace,
-                          const ::rtl::OUString& rLName,
-                          const ::rtl::OUString& rValue );
-
-    sal_uInt16 GetAttrCount() const;
-    ::rtl::OUString GetAttrNamespace( sal_uInt16 i ) const;
-    ::rtl::OUString GetAttrPrefix( sal_uInt16 i ) const;
-    const ::rtl::OUString& GetAttrLName( sal_uInt16 i ) const;
-    const ::rtl::OUString& GetAttrValue( sal_uInt16 i ) const;
-
-    sal_uInt16 GetFirstNamespaceIndex() const;
-    sal_uInt16 GetNextNamespaceIndex( sal_uInt16 nIdx ) const;
-    const ::rtl::OUString& GetNamespace( sal_uInt16 i ) const;
-    const ::rtl::OUString& GetPrefix( sal_uInt16 i ) const;
-};
+// This has class been moved to editeng, or why was there a dupe?
+#include <editeng/xmlcnitm.hxx>
 
 #endif //  _XMLOFF_XMLCNITM_HXX
 
