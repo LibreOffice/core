87e1be0120ef3073238a4308525c564dd6f8a94e
Oliver-Rainer Wittmann <orw@apache.org>
2012-12-12 13:41:59 +0000

    #121466# - replace BUILD_SPECIAL by ENABLE_ONLINE_UPDATE - left over from Ariel's change regarding the Online Update function

--- scp2/util/makefile.mk
+++ scp2/util/makefile.mk
@@ -327,7 +327,7 @@ SCP2FILES += \
 SCP2FILES += layout.par
 .ENDIF # ENABLE_LAYOUT == TRUE
 
-.IF "$(BUILD_SPECIAL)"!=""
+.IF "$(ENABLE_ONLINE_UPDATE)"!=""
 SCP2FILES += \
              module_onlineupdate.par   \
              file_onlineupdate.par
