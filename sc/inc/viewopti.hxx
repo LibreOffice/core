LibreOffice/core

commit 6ea8ea456cf5df267284278ecda42aa9b089a682

Remove Large Handles option
This patch removes the option Large Handles everywhere and makes LibO use the larger 9px handles always.

sc/inc/viewopti.hxx
@@ -56,8 +56,7 @@ enum ScViewOption
     VOPT_HELPLINES,
     VOPT_ANCHOR,
     VOPT_PAGEBREAKS,
-    VOPT_CLIPMARKS,
-    VOPT_BIGHANDLES
+    VOPT_CLIPMARKS
 };
 
 enum ScVObjType
@@ -67,7 +66,7 @@ enum ScVObjType
     VOBJ_TYPE_DRAW
 };
 
-#define MAX_OPT             (sal_uInt16)VOPT_BIGHANDLES+1
+#define MAX_OPT             (sal_uInt16)VOPT_CLIPMARKS+1
 #define MAX_TYPE            (sal_uInt16)VOBJ_TYPE_DRAW+1
 
 #define SC_STD_GRIDCOLOR    COL_LIGHTGRAY

sc/source/core/tool/viewopti.cxx
@@ -152,7 +152,6 @@ void ScViewOptions::SetDefaults()
     aOptArr[ VOPT_SYNTAX      ] =
     aOptArr[ VOPT_HELPLINES   ] =
     aOptArr[ VOPT_GRID_ONTOP  ] =
-    aOptArr[ VOPT_BIGHANDLES  ] = false;
     aOptArr[ VOPT_NOTES       ] =
     aOptArr[ VOPT_NULLVALS    ] =
     aOptArr[ VOPT_VSCROLL     ] =
@@ -299,8 +298,6 @@ SfxPoolItem* ScTpViewItem::Clone( SfxItemPool * ) const
 #define SCLAYOUTOPT_GRIDCOLOR       1
 #define SCLAYOUTOPT_PAGEBREAK       2
 #define SCLAYOUTOPT_GUIDE           3
-#define SCLAYOUTOPT_SIMPLECONT      4
-#define SCLAYOUTOPT_LARGECONT       5
 #define SCLAYOUTOPT_COLROWHDR       6
 #define SCLAYOUTOPT_HORISCROLL      7
 #define SCLAYOUTOPT_VERTSCROLL      8
@@ -345,8 +342,6 @@ Sequence<OUString> ScViewCfg::GetLayoutPropertyNames()
         "Line/GridLineColor",       // SCLAYOUTOPT_GRIDCOLOR
         "Line/PageBreak",           // SCLAYOUTOPT_PAGEBREAK
         "Line/Guide",               // SCLAYOUTOPT_GUIDE
-        "Line/SimpleControlPoint",  // SCLAYOUTOPT_SIMPLECONT
-        "Line/LargeControlPoint",   // SCLAYOUTOPT_LARGECONT
         "Window/ColumnRowHeader",   // SCLAYOUTOPT_COLROWHDR
         "Window/HorizontalScroll",  // SCLAYOUTOPT_HORISCROLL
         "Window/VerticalScroll",    // SCLAYOUTOPT_VERTSCROLL
@@ -453,9 +448,6 @@ ScViewCfg::ScViewCfg() :
                         break;
                     case SCLAYOUTOPT_GUIDE:
                         SetOption( VOPT_HELPLINES, ScUnoHelpFunctions::GetBoolFromAny( pValues[nProp] ) );
-                        break;
-                    case SCLAYOUTOPT_LARGECONT:
-                        SetOption( VOPT_BIGHANDLES, ScUnoHelpFunctions::GetBoolFromAny( pValues[nProp] ) );
                         break;
                     case SCLAYOUTOPT_COLROWHDR:
                         SetOption( VOPT_HEADER, ScUnoHelpFunctions::GetBoolFromAny( pValues[nProp] ) );
@@ -620,9 +612,6 @@ IMPL_LINK( ScViewCfg, LayoutCommitHdl, void *, EMPTYARG )
                 break;
             case SCLAYOUTOPT_GUIDE:
                 ScUnoHelpFunctions::SetBoolInAny( pValues[nProp], GetOption( VOPT_HELPLINES ) );
-                break;
-            case SCLAYOUTOPT_LARGECONT:
-                ScUnoHelpFunctions::SetBoolInAny( pValues[nProp], GetOption( VOPT_BIGHANDLES ) );
                 break;
             case SCLAYOUTOPT_COLROWHDR:
                 ScUnoHelpFunctions::SetBoolInAny( pValues[nProp], GetOption( VOPT_HEADER ) );

filter/source/xslt/import/uof/uof2odf_presentation.xsl
@@ -37,7 +37,6 @@
 							<config:config-item config:name="PageKind" config:type="short">0</config:config-item>
 							<config:config-item config:name="SelectedPage" config:type="short">0</config:config-item>
 							<config:config-item config:name="IsLayerMode" config:type="boolean">false</config:config-item>
-							<config:config-item config:name="IsBigHandles" config:type="boolean">false</config:config-item>
 							<config:config-item config:name="IsDoubleClickTextEdit" config:type="boolean">true</config:config-item>
 							<config:config-item config:name="IsClickChangeRotation" config:type="boolean">false</config:config-item>
 							<config:config-item config:name="SlidesPerRow" config:type="short">4</config:config-item>

officecfg/registry/data/org/openoffice/Office/UI/DrawImpressCommands.xcu
@@ -1525,15 +1525,6 @@
           <value xml:lang="en-US">24 Bit True Color</value>
         </prop>
       </node>
-      <node oor:name=".uno:BigHandles" oor:op="replace">
-        <prop oor:name="Label" oor:type="xs:string">
-
-          <value xml:lang="en-US">Large Handles</value>
-        </prop>
-        <prop oor:name="Properties" oor:type="xs:int">
-          <value>1</value>
-        </prop>
-      </node>
       <node oor:name=".uno:DoubleClickTextEdit" oor:op="replace">
         <prop oor:name="Label" oor:type="xs:string">
 

officecfg/registry/schema/org/openoffice/Office/Draw.xcs
@@ -364,17 +364,6 @@
 				</info>
 				<value>true</value>
 			</prop>
-      <prop oor:name="BigHandles" oor:type="xs:boolean">
-				<!-- OldPath: Draw/Other -->
-				<!-- OldLocation: soffice.cfg -->
-				<!-- UIHints: Optionbar -->
-				<info>
-					<author>AF</author>
-					<desc>Indicates whether to show big (true) or small (false) handles.</desc>
-					<label>Big Handles</label>
-				</info>
-                <value>true</value>
-			</prop>
 			<prop oor:name="ModifyWithAttributes" oor:type="xs:boolean">
 				<!-- OldPath: Draw/Other -->
 				<!-- OldLocation: soffice.cfg -->

officecfg/registry/schema/org/openoffice/Office/Impress.xcs
@@ -408,16 +408,6 @@
         </info>
         <value>true</value>
       </prop>
-      <prop oor:name="BigHandles" oor:type="xs:boolean">
-        <!-- OldPath: Impress/Other -->
-        <!-- OldLocation: soffice.cfg -->
-        <!-- UIHints: Optionbar -->
-        <info>
-          <desc>Indicates whether to show big (true) or small (false) handles.</desc>
-          <label>Big Handles</label>
-        </info>
-        <value>true</value>
-      </prop>
       <prop oor:name="ModifyWithAttributes" oor:type="xs:boolean">
         <!-- OldPath: Impress/Other -->
         <!-- OldLocation: soffice.cfg -->

commit 2d754521853b9ae89f4d9621150857f6592603b9

Fixed accidentally modified default view options.
It was unintentionally caused by
6ea8ea456cf5df267284278ecda42aa9b089a682.

Also made the default values easier to see.

Let's not do

  foo1 =
  foo2 =
  foo3 = true;

type of assignment which may give the reader the wrong impression.

Let's do

  foo1 = true;
  foo2 = true;
  foo3 = true;

instead.

Change-Id: I181b80d2aae96d65b662b187bc884913fec836db

sc/inc/viewopti.hxx
@@ -86,7 +86,7 @@ public:
     void                    SetDefaults();
 
     void                    SetOption( ScViewOption eOpt, sal_Bool bNew = sal_True )    { aOptArr[eOpt] = bNew; }
-    sal_Bool                    GetOption( ScViewOption eOpt ) const                { return aOptArr[eOpt]; }
+    bool                    GetOption( ScViewOption eOpt ) const                { return aOptArr[eOpt]; }
 
     void                    SetObjMode( ScVObjType eObj, ScVObjMode eMode ) { aModeArr[eObj] = eMode; }
     ScVObjMode              GetObjMode( ScVObjType eObj ) const             { return aModeArr[eObj]; }
@@ -103,7 +103,7 @@ public:
     int                     operator!= ( const ScViewOptions& rOpt ) const { return !(operator==(rOpt)); }
 
 private:
-    sal_Bool            aOptArr     [MAX_OPT];
+    bool            aOptArr     [MAX_OPT];
     ScVObjMode      aModeArr    [MAX_TYPE];
     Color           aGridCol;
     String          aGridColName;

sc/source/core/tool/viewopti.cxx
@@ -134,24 +134,24 @@ ScViewOptions::~ScViewOptions()
 
 void ScViewOptions::SetDefaults()
 {
-    aOptArr[ VOPT_FORMULAS    ] =
-    aOptArr[ VOPT_SYNTAX      ] =
-    aOptArr[ VOPT_HELPLINES   ] =
-    aOptArr[ VOPT_GRID_ONTOP  ] =
-    aOptArr[ VOPT_NOTES       ] =
-    aOptArr[ VOPT_NULLVALS    ] =
-    aOptArr[ VOPT_VSCROLL     ] =
-    aOptArr[ VOPT_HSCROLL     ] =
-    aOptArr[ VOPT_TABCONTROLS ] =
-    aOptArr[ VOPT_OUTLINER    ] =
-    aOptArr[ VOPT_HEADER      ] =
-    aOptArr[ VOPT_GRID        ] =
-    aOptArr[ VOPT_ANCHOR      ] =
-    aOptArr[ VOPT_PAGEBREAKS  ] =
-    aOptArr[ VOPT_CLIPMARKS   ] = sal_True;
+    aOptArr[ VOPT_FORMULAS    ] = false;
+    aOptArr[ VOPT_SYNTAX      ] = false;
+    aOptArr[ VOPT_HELPLINES   ] = false;
+    aOptArr[ VOPT_GRID_ONTOP  ] = false;
+    aOptArr[ VOPT_NOTES       ] = true;
+    aOptArr[ VOPT_NULLVALS    ] = true;
+    aOptArr[ VOPT_VSCROLL     ] = true;
+    aOptArr[ VOPT_HSCROLL     ] = true;
+    aOptArr[ VOPT_TABCONTROLS ] = true;
+    aOptArr[ VOPT_OUTLINER    ] = true;
+    aOptArr[ VOPT_HEADER      ] = true;
+    aOptArr[ VOPT_GRID        ] = true;
+    aOptArr[ VOPT_ANCHOR      ] = true;
+    aOptArr[ VOPT_PAGEBREAKS  ] = true;
+    aOptArr[ VOPT_CLIPMARKS   ] = true;
 
-    aModeArr[VOBJ_TYPE_OLE ]  =
-    aModeArr[VOBJ_TYPE_CHART] =
+    aModeArr[VOBJ_TYPE_OLE ]  = VOBJ_MODE_SHOW;
+    aModeArr[VOBJ_TYPE_CHART] = VOBJ_MODE_SHOW;
     aModeArr[VOBJ_TYPE_DRAW ] = VOBJ_MODE_SHOW;
 
     aGridCol     = Color( SC_STD_GRIDCOLOR );