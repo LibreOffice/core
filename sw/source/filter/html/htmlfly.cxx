LibreOffice/core

commit 3852a6f54880af8ed9161227baa555580c7d4517

ByteString->rtl::OString

sw/source/filter/html/htmlfly.cxx
@@ -542,11 +542,12 @@ void SwHTMLWriter::OutFrmFmt( sal_uInt8 nMode, const SwFrmFmt& rFrmFmt,
 }
 
 
-void SwHTMLWriter::OutFrmFmtOptions( const SwFrmFmt &rFrmFmt,
+rtl::OString SwHTMLWriter::OutFrmFmtOptions( const SwFrmFmt &rFrmFmt,
                                      const String& rAlternateTxt,
-                                     ByteString &rEndTags,
-                                     sal_uInt32 nFrmOpts )
+                                     sal_uInt32 nFrmOpts,
+                                     const rtl::OString &rEndTags )
 {
+    rtl::OString sRetEndTags;
     rtl::OStringBuffer sOut;
     const SfxPoolItem* pItem;
     const SfxItemSet& rItemSet = rFrmFmt.GetAttrSet();
@@ -820,9 +821,10 @@ void SwHTMLWriter::OutFrmFmtOptions( const SwFrmFmt &rFrmFmt,
             sOut.append('<').append(OOO_STRING_SVTOOLS_HTML_linebreak).
                 append(' ').append(OOO_STRING_SVTOOLS_HTML_O_clear).
                 append('=').append(pStr).append('>').append(rEndTags);
-            rEndTags = sOut.makeStringAndClear().getStr();
+            sRetEndTags = sOut.makeStringAndClear();
         }
     }
+    return sRetEndTags;
 }
 
 
@@ -995,7 +997,7 @@ Writer& OutHTML_Image( Writer& rWrt, const SwFrmFmt &rFrmFmt,
 
     // Attribute die ausserhelb der Grafik geschreiben werden muessen sammeln
     rtl::OStringBuffer sOut;
-    ByteString aEndTags;
+    rtl::OString aEndTags;
 
     // implizite Sprungmarke -> <A NAME=...></A>...<IMG ...>
     if( pMarkType && rFrmFmt.GetName().Len() )
@@ -1168,7 +1170,7 @@ Writer& OutHTML_Image( Writer& rWrt, const SwFrmFmt &rFrmFmt,
     }
 
     // ALT, ALIGN, WIDTH, HEIGHT, HSPACE, VSPACE
-    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, rAlternateTxt, aEndTags, nFrmOpts );
+    aEndTags = rHTMLWrt.OutFrmFmtOptions( rFrmFmt, rAlternateTxt, nFrmOpts, aEndTags );
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_FLY ) )
         rHTMLWrt.OutCSS1_FrmFmtOptions( rFrmFmt, nFrmOpts );
 
@@ -1195,8 +1197,8 @@ Writer& OutHTML_Image( Writer& rWrt, const SwFrmFmt &rFrmFmt,
 
     rHTMLWrt.Strm() << '>';
 
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     if( rHTMLWrt.aINetFmts.Count() )
     {
@@ -1437,8 +1439,7 @@ static Writer & OutHTML_FrmFmtAsMulticol( Writer& rWrt,
                                 : HTML_FRMOPTS_MULTICOL;
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_FLY ) && !bInCntnr )
         nFrmFlags |= HTML_FRMOPTS_MULTICOL_CSS1;
-    ByteString aEndTags;
-    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, aEndTags, nFrmFlags );
+    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, nFrmFlags );
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_FLY ) && !bInCntnr )
         rHTMLWrt.OutCSS1_FrmFmtOptions( rFrmFmt, nFrmFlags );
 
@@ -1486,13 +1487,11 @@ static Writer& OutHTML_FrmFmtAsSpacer( Writer& rWrt, const SwFrmFmt& rFrmFmt )
     rWrt.Strm() << sOut.makeStringAndClear().getStr();
 
     // ALIGN, WIDTH, HEIGHT
-    ByteString aEndTags;
-    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, aEndTags,
-                                HTML_FRMOPTS_SPACER );
+    rtl::OString aEndTags = rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, HTML_FRMOPTS_SPACER );
 
     rWrt.Strm() << '>';
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     return rWrt;
 }
@@ -1522,11 +1521,10 @@ static Writer& OutHTML_FrmFmtAsDivOrSpan( Writer& rWrt,
     sOut.append('<').append(pStr);
 
     rWrt.Strm() << sOut.makeStringAndClear().getStr();
-    ByteString aEndTags;
     sal_uLong nFrmFlags = HTML_FRMOPTS_DIV;
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_BORDER_NONE ) )
        nFrmFlags |= HTML_FRMOPT_S_NOBORDER;
-    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, aEndTags, nFrmFlags );
+    rtl::OString aEndTags = rHTMLWrt.OutFrmFmtOptions( rFrmFmt, aEmptyStr, nFrmFlags );
     rHTMLWrt.OutCSS1_FrmFmtOptions( rFrmFmt, nFrmFlags );
     rWrt.Strm() << '>';
 
@@ -1557,8 +1555,8 @@ static Writer& OutHTML_FrmFmtAsDivOrSpan( Writer& rWrt,
         rHTMLWrt.OutNewLine();
     HTMLOutFuncs::Out_AsciiTag( rWrt.Strm(), pStr, sal_False );
 
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     return rWrt;
 }

sw/source/filter/html/htmldraw.cxx
@@ -837,11 +837,10 @@ Writer& OutHTML_DrawFrmFmtAsMarquee( Writer& rWrt,
         rWrt.Strm() << sOut.makeStringAndClear().getStr();
 
     // und nun noch ALIGN, HSPACE und VSPACE
-    ByteString aEndTags;
     sal_uInt32 nFrmFlags = HTML_FRMOPTS_MARQUEE;
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_DRAW ) )
         nFrmFlags |= HTML_FRMOPTS_MARQUEE_CSS1;
-    rHTMLWrt.OutFrmFmtOptions( rFmt, aEmptyStr, aEndTags, nFrmFlags );
+    rtl::OString aEndTags = rHTMLWrt.OutFrmFmtOptions( rFmt, aEmptyStr, nFrmFlags );
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_DRAW ) )
         rHTMLWrt.OutCSS1_FrmFmtOptions( rFmt, nFrmFlags, &rSdrObject );
 
@@ -859,8 +858,8 @@ Writer& OutHTML_DrawFrmFmtAsMarquee( Writer& rWrt,
 
     HTMLOutFuncs::Out_AsciiTag( rWrt.Strm(), OOO_STRING_SVTOOLS_HTML_marquee, sal_False );
 
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     return rWrt;
 }

sw/source/filter/html/htmlforw.cxx
@@ -1114,9 +1114,9 @@ Writer& OutHTML_DrawFrmFmtAsControl( Writer& rWrt,
                             ? HTML_FRMOPTS_IMG_CONTROL_CSS1
                             : HTML_FRMOPTS_CONTROL_CSS1);
     }
-    ByteString aEndTags;
+    rtl::OString aEndTags;
     if( nFrmOpts != 0 )
-        rHTMLWrt.OutFrmFmtOptions( rFmt, aEmptyStr, aEndTags, nFrmOpts );
+        aEndTags = rHTMLWrt.OutFrmFmtOptions( rFmt, aEmptyStr, nFrmOpts );
 
     if( rHTMLWrt.bCfgOutStyles )
     {
@@ -1363,8 +1363,8 @@ Writer& OutHTML_DrawFrmFmtAsControl( Writer& rWrt,
         }
     }
 
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     // Controls sind nicht absatz-gebunden, deshalb kein LF mehr ausgeben!
     rHTMLWrt.bLFPossible = sal_False;

sw/source/filter/html/htmlplug.cxx
@@ -1017,7 +1017,6 @@ Writer& OutHTML_FrmFmtOLENode( Writer& rWrt, const SwFrmFmt& rFrmFmt,
         return rWrt;
     }
 
-    ByteString aEndTags;
     sal_uLong nFrmOpts;
 
     // wenn meoglich vor dem "Objekt" einen Zeilen-Umbruch ausgeben
@@ -1154,8 +1153,7 @@ Writer& OutHTML_FrmFmtOLENode( Writer& rWrt, const SwFrmFmt& rFrmFmt,
     // ALT, WIDTH, HEIGHT, HSPACE, VSPACE, ALIGN
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_FLY ) && !bHiddenEmbed )
         nFrmOpts |= HTML_FRMOPTS_OLE_CSS1;
-    rHTMLWrt.OutFrmFmtOptions( rFrmFmt, pOLENd->GetTitle(),
-                               aEndTags, nFrmOpts );
+    rtl::OString aEndTags = rHTMLWrt.OutFrmFmtOptions( rFrmFmt, pOLENd->GetTitle(), nFrmOpts );
     if( rHTMLWrt.IsHTMLMode( HTMLMODE_ABS_POS_FLY ) && !bHiddenEmbed )
         rHTMLWrt.OutCSS1_FrmFmtOptions( rFrmFmt, nFrmOpts );
 
@@ -1255,8 +1253,8 @@ Writer& OutHTML_FrmFmtOLENode( Writer& rWrt, const SwFrmFmt& rFrmFmt,
         HTMLOutFuncs::Out_AsciiTag( rWrt.Strm(), OOO_STRING_SVTOOLS_HTML_iframe, sal_False );
     }
 
-    if( aEndTags.Len() )
-        rWrt.Strm() << aEndTags.GetBuffer();
+    if( aEndTags.getLength() )
+        rWrt.Strm() << aEndTags.getStr();
 
     return rWrt;
 }

sw/source/filter/html/wrthtml.hxx
@@ -408,8 +408,8 @@ public:
     // ALT/ALIGN/WIDTH/HEIGHT/HSPACE/VSPACE-Optionen des aktuellen
     // Frame-Formats ausgeben und ggf. ein <BR CLEAR=...> vorne an
     // rEndTags anhaengen
-    void OutFrmFmtOptions( const SwFrmFmt& rFrmFmt, const String& rAltTxt,
-                            ByteString &rEndTags, sal_uInt32 nFrmOpts );
+    rtl::OString OutFrmFmtOptions( const SwFrmFmt& rFrmFmt, const String& rAltTxt,
+        sal_uInt32 nFrmOpts, const rtl::OString& rEndTags = rtl::OString() );
     void OutCSS1_TableFrmFmtOptions( const SwFrmFmt& rFrmFmt );
     void OutCSS1_SectionFmtOptions( const SwFrmFmt& rFrmFmt );
     void OutCSS1_FrmFmtOptions( const SwFrmFmt& rFrmFmt, sal_uInt32 nFrmOpts,

commit a923084f872b8ce13f6213827fe5b1c711e6b15f

fdo#58286: sw HTML export: fix missing end tags on FrmFmts
SwHTMLWriter::OutFrmFmtOptions must return a value that includes the
parameter rEndTags.

(regression from 3852a6f54880af8ed9161227baa555580c7d4517)

Change-Id: I280a0de57e254f75b69d08d93e23bf7c765058ec

sw/source/filter/html/htmlfly.cxx
@@ -534,7 +534,7 @@ rtl::OString SwHTMLWriter::OutFrmFmtOptions( const SwFrmFmt &rFrmFmt,
                                      sal_uInt32 nFrmOpts,
                                      const rtl::OString &rEndTags )
 {
-    rtl::OString sRetEndTags;
+    rtl::OString sRetEndTags(rEndTags);
     rtl::OStringBuffer sOut;
     const SfxPoolItem* pItem;
     const SfxItemSet& rItemSet = rFrmFmt.GetAttrSet();
@@ -811,6 +811,7 @@ rtl::OString SwHTMLWriter::OutFrmFmtOptions( const SwFrmFmt &rFrmFmt,
             sRetEndTags = sOut.makeStringAndClear();
         }
     }
+    assert(sRetEndTags.endsWith(rEndTags)); // fdo#58286
     return sRetEndTags;
 }
 