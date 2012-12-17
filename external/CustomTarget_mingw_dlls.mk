e4edbc9583ad1dcfe35adcdc43a70439fb20b612
Peter Foley <pefoley2@verizon.net>
2012-12-17 17:48:00 -0500

    convert external to gbuild
    Change-Id: I7127db6aad9e97a0f3e758aa83b22ac8465d3f43

---
+++ external/CustomTarget_mingw_dlls.mk
@@ -0,0 +1,83 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,external/mingw-dlls))
+
+MINGW_DLLS := \
+    $(MINGW_BOOST_DATE_TIME_DLL) \
+    $(MINGW_CAIRO_DLL) \
+    $(MINGW_CRYPTO_DLL) \
+    $(MINGW_CURL_DLL) \
+    $(MINGW_EXPAT_DLL) \
+    $(MINGW_FONTCONFIG_DLL) \
+    $(MINGW_FREETYPE_DLL) \
+    $(MINGW_GCRYPT_DLL) \
+    $(MINGW_GLIB_DLL) \
+    $(MINGW_GMODULE_DLL) \
+    $(MINGW_GNUTLS_DLL) \
+    $(MINGW_GPG_ERROR_DLL) \
+    $(MINGW_GRAPHITE2_DLL) \
+    $(MINGW_HUNSPELL_DLL) \
+    $(MINGW_HYPHEN_DLL) \
+    $(MINGW_ICONV_DLL) \
+    $(MINGW_ICUDATA_DLL) \
+    $(MINGW_ICUI18N_DLL) \
+    $(MINGW_ICUUC_DLL) \
+    $(MINGW_IDN_DLL) \
+    $(MINGW_INTL_DLL) \
+    $(MINGW_JPEG_DLL) \
+    $(MINGW_LCMS2_DLL) \
+    $(MINGW_LIBEXSLT_DLL) \
+    $(MINGW_LIBLANGTAG_DLL) \
+    $(MINGW_LIBXML_DLL) \
+    $(MINGW_LIBXSLT_DLL) \
+    $(MINGW_LPSOLVE_DLL) \
+    $(MINGW_MYTHES_DLL) \
+    $(MINGW_NEON_DLL) \
+    $(MINGW_NSPR4_DLL) \
+    $(MINGW_NSS3_DLL) \
+    $(MINGW_NSSUTIL3_DLL) \
+    $(MINGW_PIXMAN_DLL) \
+    $(MINGW_PYTHON_DLL) \
+    $(MINGW_PYTHON_EXE) \
+    $(MINGW_PLC4_DLL) \
+    $(MINGW_PLDS4_DLL) \
+    $(MINGW_PNG15_DLL) \
+    $(MINGW_POPPLER_DLL) \
+    $(MINGW_RAPTOR_DLL) \
+    $(MINGW_RASQAL_DLL) \
+    $(MINGW_READLINE_DLL) \
+    $(MINGW_REDLAND_DLL) \
+    $(MINGW_SQLITE3_DLL) \
+    $(MINGW_SMIME3_DLL) \
+    $(MINGW_SSH2_DLL) \
+    $(MINGW_SSL3_DLL) \
+    $(MINGW_SSL_DLL) \
+    $(MINGW_TASN1_DLL) \
+    $(MINGW_TERMCAP_DLL) \
+    $(MINGW_ZLIB_DLL) \
+    $(if $(filter YES,$(MINGW_SHARED_GCCLIB)),$(MINGW_GCCDLL)) \
+    $(if $(filter YES,$(MINGW_SHARED_GXXLIB)),$(MINGW_GXXDLL))
+
+POTENTIAL_MINGW_RUNTIME_BINDIRS := \
+	$(COMPATH)/$(HOST_PLATFORM)/sys-root/mingw/bin \
+	/usr/$(HOST_PLATFORM)/sys-root/mingw/bin
+
+$(eval $(call gb_CustomTarget_register_targets,external/wine,\
+	$(MINGW_DLLS) \
+))
+
+$(call gb_CustomTarget_get_workdir,external/mingw-dlls)/%.dll: $(firstword $(wildcard $(foreach dir,$(POTENTIAL_MINGW_RUNTIME_BINDIRS), $(dir)/%)))
+	if ! test -f $<; then \
+		echo Could not find $*.dll in any of $(POTENTIAL_MINGW_RUNTIME_BINDIRS); \
+	fi
+	$(call gb_Output_announce,$*,$(true),CPY,1)
+	cp $< $*
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab:
---
+++ external/Package_jawt.mk
@@ -0,0 +1,14 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,jawt,$(call gb_CustomTarget_get_workdir,external/jawt)))
+
+$(eval $(call gb_Package_add_file,jawt,libjawt.dll.a,lib/libjawt.dll.a))
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab:
