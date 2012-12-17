82446092f2594c65c5b250b20433fb0dc91d921b
Peter Foley <pefoley2@verizon.net>
2012-12-17 17:48:00 -0500

    convert external to gbuild
    Change-Id: I6b9073313e674807d519bf9ee95a50893c2bf46d

---
+++ external/Package_msvc_dlls.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,msvc_dlls,$(call gb_CustomTarget_get_workdir,external/msvc_dlls)))
+
+$(eval $(call gb_Package_add_files,msvc_dlls,bin,\
+	msvcp$(VCVER).dll \
+	msvcr$(VCVER).dll \
+	$(if $(filter 90,$(VCVER)),msvcm90.dll Microsoft.VC90.CRT.manifest) \
+))
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab:
