5f189199021929a50be244498cca05bc3dcf0816
Michael Stahl <mstahl@redhat.com>
2012-12-18 13:52:19 +0100

    DiscoveryService: include errno.h
    Change-Id: I8bf8ce8324e6ebe89dca1c58f02d973b8998ff2a

--- sd/source/ui/remotecontrol/DiscoveryService.cxx
+++ sd/source/ui/remotecontrol/DiscoveryService.cxx
@@ -6,13 +6,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <algorithm>
 #include <vector>
 
 #include <comphelper/processfactory.hxx>
 #include <rtl/strbuf.hxx>
-#include <string.h>
 
 #include "DiscoveryService.hxx"
 
