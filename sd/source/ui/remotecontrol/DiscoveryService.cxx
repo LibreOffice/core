1f5401e6c9c260354022aeab6a02512bdbfa7a4b
Michael Stahl <mstahl@redhat.com>
2012-12-18 12:47:21 +0100

    DiscoveryService: -Werror=unused-but-set-variable
    Change-Id: If0175642ebcbe1f4f6cc7640d87385c96e097180

--- sd/source/ui/remotecontrol/DiscoveryService.cxx
+++ sd/source/ui/remotecontrol/DiscoveryService.cxx
@@ -49,6 +49,12 @@ DiscoveryService::DiscoveryService() :
 
     rc = bind( mSocket, (sockaddr*) &aAddr, sizeof(sockaddr_in) );
 
+    if (rc)
+    {
+        SAL_WARN("sd", "DiscoveryService: bind failed: " << errno);
+        return; // would be better to throw, but unsure if caller handles that
+    }
+
     struct ip_mreq multicastRequest;
 
     multicastRequest.imr_multiaddr.s_addr = inet_addr( "239.0.0.1" );
@@ -59,6 +65,12 @@ DiscoveryService::DiscoveryService() :
         (const char*)
 #endif
         &multicastRequest, sizeof(multicastRequest));
+
+    if (rc)
+    {
+        SAL_WARN("sd", "DiscoveryService: setsockopt failed: " << errno);
+        return; // would be better to throw, but unsure if caller handles that
+    }
 }
 
 DiscoveryService::~DiscoveryService()
